2da5aba89b8a1bd7d20cae541f6a495f503752aa
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1991-08-28 10:26:23 +0000

    Initial revision
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@33 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/machmode.h
@@ -0,0 +1,111 @@
+/* Machine mode definitions for GNU C-Compiler; included by rtl.h and tree.h.
+   Copyright (C) 1990-1991 Free Software Foundation, Inc.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+
+#ifndef HAVE_MACHINE_MODES
+
+/* Strictly speaking, this isn't the proper place to include these definitions,
+   but this file is included by every GCC file.
+
+   Some systems define these in, e.g., param.h.  We undefine these names
+   here to avoid the warnings.  We prefer to use our definitions since we
+   know they are correct.  */
+
+#undef MIN
+#undef MAX
+
+#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+#define MAX(X,Y) ((X) > (Y) ? (X) : (Y))
+
+/* Make an enum class that gives all the machine modes.  */
+
+#define DEF_MACHMODE(SYM, NAME, TYPE, SIZE, UNIT, WIDER)  SYM,
+
+enum machine_mode {
+#include "machmode.def"
+
+#ifdef EXTRA_CC_MODES
+  EXTRA_CC_MODES,
+#endif
+MAX_MACHINE_MODE };
+
+#undef DEF_MACHMODE
+
+#define HAVE_MACHINE_MODES
+
+#ifndef NUM_MACHINE_MODES
+#define NUM_MACHINE_MODES (int) MAX_MACHINE_MODE
+#endif
+
+/* Get the name of mode MODE as a string.  */
+
+extern char *mode_name[];
+#define GET_MODE_NAME(MODE)		(mode_name[(int)(MODE)])
+
+enum mode_class { MODE_RANDOM, MODE_INT, MODE_FLOAT, MODE_PARTIAL_INT, MODE_CC,
+		  MODE_COMPLEX_INT, MODE_COMPLEX_FLOAT };
+
+/* Get the general kind of object that mode MODE represents
+   (integer, floating, complex, etc.)  */
+
+extern enum mode_class mode_class[];
+#define GET_MODE_CLASS(MODE)		(mode_class[(int)(MODE)])
+
+/* Get the size in bytes of an object of mode MODE.  */
+
+extern int mode_size[];
+#define GET_MODE_SIZE(MODE)		(mode_size[(int)(MODE)])
+
+/* Get the size in bytes of the basic parts of an object of mode MODE.  */
+
+extern int mode_unit_size[];
+#define GET_MODE_UNIT_SIZE(MODE)	(mode_unit_size[(int)(MODE)])
+
+/* Get the number of units in the object.  */
+
+#define GET_MODE_NUNITS(MODE)  \
+  (GET_MODE_SIZE ((MODE)) / GET_MODE_UNIT_SIZE ((MODE)))
+
+/* Get the size in bits of an object of mode MODE.  */
+
+#define GET_MODE_BITSIZE(MODE)  (BITS_PER_UNIT * mode_size[(int)(MODE)])
+
+/* Get a bitmask containing 1 for all bits in a word
+   that fit within mode MODE.  */
+
+#define GET_MODE_MASK(MODE)  \
+   ((GET_MODE_BITSIZE (MODE) >= HOST_BITS_PER_INT)  \
+    ? -1 : ((1 << GET_MODE_BITSIZE (MODE)) - 1))
+
+/* Get the next wider natural mode (eg, QI -> HI -> SI -> DI -> TI).  */
+
+extern enum machine_mode mode_wider_mode[];
+#define GET_MODE_WIDER_MODE(MODE)	(mode_wider_mode[(int)(MODE)])
+
+/* Find the best mode to use to access a bit field.  */
+
+extern enum machine_mode get_best_mode ();
+
+/* Determine alignment, 1<=result<=BIGGEST_ALIGNMENT.  */
+
+#define GET_MODE_ALIGNMENT(MODE)   \
+  MIN (BIGGEST_ALIGNMENT, 	   \
+       MAX (1, (GET_MODE_UNIT_SIZE (MODE) * BITS_PER_UNIT)))
+
+#endif /* not HAVE_MACHINE_MODES */
