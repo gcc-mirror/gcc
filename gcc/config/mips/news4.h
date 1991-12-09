2db1a89d47fff4928eb4741e274482e6c397cb05
Michael Meissner <meissner@gcc.gnu.org>
1991-12-09 21:21:46 +0000

    Initial revision
    From-SVN: r108

---
+++ gcc/config/mips/news4.h
@@ -0,0 +1,53 @@
+/* Definitions of target machine for GNU compiler.  Sony RISC NEWS (mips)
+   Copyright (C) 1991 Free Software Foundation, Inc.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#define MIPS_NEWS
+
+#define CPP_PREDEFINES "-Dr3000 -Dnews3700 -DLANGUAGE_C -DMIPSEB -DSYSTYPE_BSD -Dsony_news -Dsony -Dunix -Dmips -Dhost_mips"
+
+#define ASM_SPEC	"%{!mgas:					\
+				%{!mrnames: -nocpp}			\
+			 	%{pipe:%e:-pipe not supported}		\
+				%{EB} %{!EB:-EB}			\
+				%{EL: %e-EL not supported}		\
+				%{O:-O2} %{O1:-O2} %{O2:-O2} %{O3:-O3}	\
+				%{g} %{g1} %{g2} %{g3} %{g0} %{v} %{K}	\
+			 %{G*}"
+
+#define CPP_SPEC	"-I/usr/include2.0				\
+			 %{.S:	-D__LANGUAGE_ASSEMBLY__			\
+				-D_LANGUAGE_ASSEMBLY			\
+				%{!ansi: -DLANGUAGE_ASSEMBLY}}		\
+			 %{!.S:	-D__LANGUAGE_C__			\
+				-D_LANGUAGE_C				\
+				%{!ansi:-DLANGUAGE_C}}"
+
+#define LINK_SPEC	"%{G*}						\
+			 %{!mgas:					\
+				%{EB} %{!EB:-EB}			\
+				%{EL: %e-EL not supported}		\
+				%{bestGnum}}"
+
+#define LIB_SPEC "%{p:-lprof1} %{pg:-lprof1} -lc"
+
+#define STARTFILE_SPEC "%{pg:gcrt0.o%s}%{!pg:%{p:mcrt0.o%s}%{!p:crt0.o%s}}"
+
+#define MACHINE_TYPE "RISC NEWS-OS"
+
+#include "mips.h"
