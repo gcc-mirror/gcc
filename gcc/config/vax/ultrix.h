4c808cdd915449373f720e32382628b61e70df5a
rms <rms@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-01-21 18:29:25 +0000

    entered into RCS
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@225 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/vax/ultrix.h
@@ -0,0 +1,13 @@
+#include "vax.h"
+
+#undef CPP_PREDEFINES
+#define CPP_PREDEFINES " -Dvax -Dunix -Dultrix -Dbsd4_2 -D__vax -D__unix -D__ultrix -D__bsd4_2"
+
+/* By default, allow $ to be part of an identifier.  */
+#define DOLLARS_IN_IDENTIFIERS 1
+
+/* These are as defined in /usr/include/sys/stdtypes.h */
+#define SIZE_TYPE "unsigned int"
+#define PTRDIFF_TYPE "int"
+#define WCHAR_TYPE "short unsigned int"
+#define WCHAR_TYPE_SIZE 16
