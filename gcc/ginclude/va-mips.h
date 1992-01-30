91a8b2ec7fb6d48647034acf20534011a5927ec5
rms <rms@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-01-30 20:37:09 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@254 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ginclude/va-mips.h
+++ gcc/ginclude/va-mips.h
@@ -28,15 +28,25 @@
 typedef char * __va___list;
 #endif
 
+#define __va_rounded_size(TYPE)  \
+  (((sizeof (TYPE) + sizeof (int) - 1) / sizeof (int)) * sizeof (int))
+
+#ifdef _STDARG_H
+#define va_start(AP, LASTARG) 						\
+ (AP = ((char *) &(LASTARG) + __va_rounded_size (LASTARG)))
+#else
 #define va_start(AP)  AP = (char *) &__builtin_va_alist
+#endif
+
 #define va_end(AP)
 
 #ifdef lint	/* complains about constant in conditional context */
 #define va_arg(list, mode) ((mode *)(list += sizeof(mode)))[-1]
 
 #else		/* !lint */
-#define va_arg(AP, mode) ((mode *)(AP = \
-	(char *) (sizeof(mode) > 4 ? ((int)AP + 2*8 - 1) & -8 \
-				   : ((int)AP + 2*4 - 1) & -4)))[-1]
+#define va_arg(AP, mode)						\
+  ((mode *)(AP = (char *) (__alignof(mode) > 4				\
+				? ((int)AP + 2*8 - 1) & -8		\
+				: ((int)AP + 2*4 - 1) & -4)))[-1]
 #endif		/* lint */
 
