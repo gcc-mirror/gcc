9cfac0142314b09caf197380b3ac79eeb89fdf59
Richard Kenner <kenner@gcc.gnu.org>
1992-02-01 09:17:42 -0500

    entered into RCS
    From-SVN: r266

---
+++ gcc/config/rs6000/xm-mach.h
@@ -0,0 +1,23 @@
+/* Configuration for GNU C-compiler for IBM RS/6000 on MACH.
+   Copyright (C) 1992 Free Software Foundation, Inc.
+   Contributed by Richard Kenner (kenner@nyu.edu).
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+
+#include "xm-rs6000.h"
+#undef USG
