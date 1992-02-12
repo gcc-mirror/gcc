b2ecd59945215e54a94e5cf05a42fe5871fbecfb
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-02-12 15:45:01 +0000

    Initial revision
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@306 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/mips/svr3-4.h
@@ -0,0 +1,55 @@
+/* Definitions of target machine for GNU compiler.  MIPS RISC-OS System V version.
+   Copyright (C) 1991 Free Software Foundation, Inc.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#define MIPS_SYSV
+
+#define CPP_PREDEFINES "-Dmips -Dunix -Dhost_mips -DMIPSEB -DR3000"
+
+#define MD_STARTFILE_PREFIX "/usr/lib/cmplrs/cc"
+
+#define CPP_SPEC "\
+%{!ansi: %{!ZBSD43: -DSYSTYPE_SYSV} \
+	 %{ZBSD43:  -DSYSTYPE_BSD43}} \
+%{!ZBSD43: -D__SYSTYPE_SYSV__} \
+%{ZBSD43:  -D__SYSTYPE_BSD43__} \
+%{!nostdinc:	%{!ZBSD43: -I/sysv/usr/include} \
+		%{ZBSD43:  -I/bsd43/usr/include}} \
+%{.cc:	-D__LANGUAGE_C_PLUS_PLUS -D_LANGUAGE_C_PLUS_PLUS} \
+%{.cxx:	-D__LANGUAGE_C_PLUS_PLUS -D_LANGUAGE_C_PLUS_PLUS} \
+%{.C:	-D__LANGUAGE_C_PLUS_PLUS -D_LANGUAGE_C_PLUS_PLUS} \
+%{.m:	-D__LANGUAGE_OBJECTIVE_C -D_LANGUAGE_OBJECTIVE_C} \
+%{.S:	-D__LANGUAGE_ASSEMBLY -D_LANGUAGE_ASSEMBLY %{!ansi:-DLANGUAGE_ASSEMBLY}} \
+%{!.S:	-D__LANGUAGE_C -D_LANGUAGE_C %{!ansi:-DLANGUAGE_C}}"
+
+#define LINK_SPEC "\
+%{G*} \
+%{!mgas: \
+	%{EB} %{!EB:-EB} \
+	%{EL: %e-EL not supported} \
+	%{bestGnum} \
+	%{!ZBSD43: -systype /sysv/} \
+	%{ZBSD43:  -systype /bsd43/}}"
+		    
+#define LIB_SPEC "%{p:-lprof1} %{pg:-lprof1} -lc crtn.o%s"
+
+#define STARTFILE_SPEC "%{pg:gcrt0.o%s}%{!pg:%{p:mcrt0.o%s}%{!p:crt1.o%s}}"
+
+#define MACHINE_TYPE "RISC-OS System V Mips"
+
+#include "mips.h"
