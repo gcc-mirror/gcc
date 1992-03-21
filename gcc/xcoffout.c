72cc682a17b8f92d75286a43f79ef87dc684f5d5
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-03-21 23:58:13 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@558 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/xcoffout.c
+++ gcc/xcoffout.c
@@ -38,7 +38,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #include "xcoff.h"
 
-#ifdef USG
+#if defined (USG) || defined (NO_STAB_H)
 #include "gstab.h"
 #else
 #include <stab.h>
