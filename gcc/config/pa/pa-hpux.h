fa6fad36a8896085277c0cfb15bdd35934620d25
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-04-18 17:59:35 +0000

    Initial revision
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@778 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/pa/pa-hpux.h
@@ -0,0 +1,14 @@
+#include "hppa.h"
+
+/* Make GCC agree with types.h.  */
+#define SIZE_TYPE "unsigned int"
+
+/* HPUX doesn't use any debugging format that GCC knows about.  */
+#undef DBX_DEBUGGING_INFO
+
+/* Like the default, except no -lg.  */
+#define LIB_SPEC "%{!p:%{!pg:-lc}}%{p:-lc_p}%{pg:-lc_p}"
+
+/* Control how gcc finds libgcc.a and how it passes -L options.  */
+#define LINK_LIBGCC_SPECIAL
+#define RELATIVE_PREFIX_NOT_LINKDIR
