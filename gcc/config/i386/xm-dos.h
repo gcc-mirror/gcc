994719f9c844efb9dde1ab2862ad703f3f955d88
rms <rms@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-05-03 21:39:39 +0000

    entered into RCS
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@869 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/xm-dos.h
+++ gcc/config/i386/xm-dos.h
@@ -2,3 +2,9 @@
 
 /* Inhibit cccp.c's definition of putenv.  */
 #define HAVE_PUTENV
+
+/* Use semicolons to separate elements of a path.  */
+#define PATH_SEPARATOR ';'
+
+/* Suffix for executable file names.  */
+#define EXECUTABLE_SUFFIX ".exe"
