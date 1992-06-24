bedd5a66a6cd142b016a8b8d44a8a4c002cea8a6
mycroft <mycroft@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-06-24 04:16:49 +0000

    entered into RCS
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@1269 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/m88k/xm-sysv3.h
@@ -0,0 +1,29 @@
+/* Configuration for GNU C-compiler.
+   Motorola m88100 running the AT&T/Unisoft/Motorola V.3 reference port.
+   Copyright (C) 1990 Free Software Foundation, Inc.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#include "xm-m88k.h"
+
+#define R_OK 4
+#define W_OK 2
+#define X_OK 1
+#define F_OK 0
+
+/* for the emacs version of alloca */
+#define STACK_DIRECTION -1
