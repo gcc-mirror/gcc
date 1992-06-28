a840ff962cb1c672191a7ef616132d1c8af81512
mycroft <mycroft@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-06-28 15:34:25 +0000

    entered into RCS
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@1327 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/limitx.h
+++ gcc/limitx.h
@@ -1,5 +1,8 @@
 /* This administrivia gets added to the beginning of limits.h
    if the system has its own version of limits.h.  */
 
+#ifndef _LIMITS_H_  /* Terminated in limity.h.  */
+#define _LIMITS_H_
+
 #include_next <limits.h>
 
