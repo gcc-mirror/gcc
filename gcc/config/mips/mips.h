dc0e5255a5527bfb5d64cce8a409f0cf743640c9
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-07-12 02:00:15 +0000

    Bump meissner version number
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@1563 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -435,7 +435,7 @@ while (0)
 
 /* Print subsidiary information on the compiler version in use.  */
 
-#define MIPS_VERSION "[AL 1.1, MM 20]"
+#define MIPS_VERSION "[AL 1.1, MM 21]"
 
 #ifndef MACHINE_TYPE
 #define MACHINE_TYPE "BSD Mips"
