882e061d827479d886f558d4ab3f01edeee6418e
Richard Stallman <rms@gnu.org>
1992-08-12 07:23:53 +0000

    (TARGET_DEFAULT): Make value 0.
    From-SVN: r1804

--- gcc/config/m68k/hp2bsd.h
+++ gcc/config/m68k/hp2bsd.h
@@ -21,7 +21,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 /* See m68k.h.  0 means 68000 with no 68881.  */
 
-#define TARGET_DEFAULT -0102
+#define TARGET_DEFAULT 0
 
 /* Define __HAVE_68881 in preprocessor only if -m68881 is specified.
    This will control the use of inline 68881 insns in certain macros.
