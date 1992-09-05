dac45ea36e2d1e5997848ef0e39976a04ea85153
Richard Stallman <rms@gnu.org>
1992-09-05 02:03:04 +0000

    Comment change for meaning of TREE_CONSTANT_OVERFLOW.
    From-SVN: r2054

--- gcc/tree.h
+++ gcc/tree.h
@@ -228,8 +228,7 @@ struct tree_common
    implicitly and should not lead to an "unused value" warning.  */
 #define TREE_NO_UNUSED_WARNING(NODE) ((NODE)->common.static_flag)
 
-/* In a NON_LVALUE_EXPR, this means there was overflow in folding.
-   The folded constant is inside the NON_LVALUE_EXPR.  */
+/* In an INTEGER_CST, this means there was overflow in folding.  */
 #define TREE_CONSTANT_OVERFLOW(NODE) ((NODE)->common.static_flag)
 
 /* Nonzero for a TREE_LIST or TREE_VEC node means that the derivation
