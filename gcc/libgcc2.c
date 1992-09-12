9bd23d2c954318dfe201afc37c9dbca98856040b
Richard Stallman <rms@gnu.org>
1992-09-12 08:45:46 +0000

    (__gcc_bcmp): New function, in cond on L__gcc_bcmp.
    From-SVN: r2110

--- gcc/libgcc2.c
+++ gcc/libgcc2.c
@@ -975,7 +975,30 @@ __fixunssfsi (SFtype a)
 #undef unsigned
 #undef float
 #undef double
+
+#ifdef L__gcc_bcmp
 
+/* Like bcmp except the sign is meaningful.
+   Reult is negative if S1 is less than S2,
+   positive if S1 is greater, 0 if S1 and S2 are equal.  */
+
+int
+__gcc_bcmp (s1, s2, size)
+     char *s1, *s2;
+     size_t size;
+{
+  while (size > 0)
+    {
+      char c1 = *s1++, c2 = *s2++;
+      if (c1 != c2)
+	return c1 - c2;
+      size--;
+    }
+  return 0;
+}
+
+#endif
+
 #ifdef L_varargs
 #ifdef __i860__
 #if defined(__svr4__) || defined(__alliant__)
