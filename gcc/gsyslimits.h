dc24dc0a611473f5f48f22122d625ac0d52eca6e
Richard Stallman <rms@gnu.org>
1992-09-24 06:58:57 +0000

    Add comment.
    From-SVN: r2226

--- gcc/gsyslimits.h
+++ gcc/gsyslimits.h
@@ -1,1 +1,6 @@
+/* syslimits.h stands for the system's own limits.h file.
+   If we can use it ok unmodified, then we install this text.
+   If fixincludes fixes it, then the fixed version is installed
+   instead of this text.  */
+
 #include_next <limits.h>
