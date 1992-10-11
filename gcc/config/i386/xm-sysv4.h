d1deac2539c7dbf50f1429c8bd768698f81b75a9
jrv <jrv@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-10-11 12:56:41 +0000

    (USE_C_ALLOCA): New macro.  Define if not using GNU C's builtin alloca.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@2397 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/xm-sysv4.h
+++ gcc/config/i386/xm-sysv4.h
@@ -2,3 +2,11 @@
 
 #include "xm-i386.h"
 #include "xm-svr4.h"
+
+/* If compiled with GNU C, use the built-in alloca.  */
+#undef alloca
+#ifdef __GNUC__
+#define alloca __builtin_alloca
+#else
+#define USE_C_ALLOCA
+#endif
