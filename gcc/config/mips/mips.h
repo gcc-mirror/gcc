46cca58cf74ed4d610dea10379befde5cfb099a0
Richard Stallman <rms@gnu.org>
1992-10-15 05:10:32 +0000

    (DEBUG_REGISTER_NAMES): New macro definition.
    From-SVN: r2466

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -2823,6 +2823,21 @@ while (0)
   &mips_reg_names[66][0],						\
 }
 
+/* print-rtl.c can't use REGISTER_NAMES, since it depends on mips.c.
+   So define this for it.  */
+#define DEBUG_REGISTER_NAMES						\
+{									\
+  "$0",   "at",   "v0",   "v1",   "a0",   "a1",   "a2",   "a3",		\
+  "t0",   "t1",   "t2",   "t3",   "t4",   "t5",   "t6",   "t7",		\
+  "s0",   "s1",   "s2",   "s3",   "s4",   "s5",   "s6",   "s7",		\
+  "t8",   "t9",   "k0",   "k1",   "gp",   "sp",   "$fp",   "ra",	\
+  "$f0",  "$f1",  "$f2",  "$f3",  "$f4",  "$f5",  "$f6",  "$f7",	\
+  "$f8",  "$f9",  "$f10", "$f11", "$f12", "$f13", "$f14", "$f15",	\
+  "$f16", "$f17", "$f18", "$f19", "$f20", "$f21", "$f22", "$f23",	\
+  "$f24", "$f25", "$f26", "$f27", "$f28", "$f29", "$f30", "$f31",	\
+  "hi",   "lo",   "$fcr31"						\
+}
+
 /* If defined, a C initializer for an array of structures
    containing a name and a register number.  This macro defines
    additional names for hard registers, thus allowing the `asm'
