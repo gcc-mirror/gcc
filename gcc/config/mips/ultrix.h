9dbc4d4c997b89cebfb4617e7c7b2d4d7eceade5
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-10-16 18:22:19 +0000

    Protect all defines with ifndef.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@2492 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/mips/ultrix.h
+++ gcc/config/mips/ultrix.h
@@ -24,6 +24,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 -DMIPSEL -DR3000 -DSYSTYPE_BSD -D_SYSTYPE_BSD -Dbsd4_2 -Dhost_mips -Dmips -Dultrix -Dunix"
 #endif
 
+#ifndef ASM_SPEC
 #define ASM_SPEC "\
 %{!mgas: \
 	%{!mrnames: %{!.s:-nocpp} %{.s: %{cpp} %{nocpp}}} \
@@ -34,7 +35,9 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 	%{O:-O2} %{O1:-O2} %{O2:-O2} %{O3:-O3} \
 	%{g} %{g0} %{g1} %{g2} %{g3} %{v} %{K}} \
 %{G*}"
+#endif
 
+#ifndef CPP_SPEC
 #define CPP_SPEC "\
 %{.S:	-D__LANGUAGE_ASSEMBLY__ -D__LANGUAGE_ASSEMBLY %{!ansi:-DLANGUAGE_ASSEMBLY}} \
 %{.cc:	-D__LANGUAGE_C_PLUS_PLUS__ -D__LANGUAGE_C_PLUS_PLUS} \
@@ -42,7 +45,9 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 %{.C:	-D__LANGUAGE_C_PLUS_PLUS__ -D__LANGUAGE_C_PLUS_PLUS} \
 %{.m:	-D__LANGUAGE_OBJECTIVE_C__ -D__LANGUAGE_OBJECTIVE_C} \
 %{!.S:	-D__LANGUAGE_C__  -D__LANGUAGE_C %{!ansi:-DLANGUAGE_C}}"
+#endif
 
+#ifndef LINK_SPEC
 #define LINK_SPEC "\
 %{G*} \
 %{!mgas: \
@@ -50,15 +55,24 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 	%{EB: %e-EB not supported} \
 	%{mips1} %{mips2} %{mips3} \
 	%{bestGnum}}"
+#endif
 
+#ifndef LIB_SPEC
 #define LIB_SPEC "%{p:-lprof1} %{pg:-lprof1} -lc"
+#endif
 
+#ifndef STARTFILE_SPEC
 #define STARTFILE_SPEC "%{pg:gcrt0.o%s}%{!pg:%{p:mcrt0.o%s}%{!p:crt0.o%s}}"
+#endif
 
 /* For compatibility with types.h.  */
+#ifndef STARTFILE_SPEC
 #define SIZE_TYPE "unsigned int"
+#endif
 
+#ifndef MACHINE_TYPE
 #define MACHINE_TYPE "DECstation running ultrix"
+#endif
 
 /* Use atexit for static constructors/destructors, instead of defining
    our own exit function.  Ultrix 4.x has this, 3.x probably does not.  */
