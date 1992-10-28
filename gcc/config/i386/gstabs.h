7aeb7aebc60606ff63057b36fa0be900a3af30df
Richard Stallman <rms@gnu.org>
1992-10-28 09:30:48 +0000

    Initial revision
    From-SVN: r2626

---
+++ gcc/config/i386/gstabs.h
@@ -0,0 +1,9 @@
+#include "i386gas.h"
+
+/* We do not want to output SDB debugging information.  */
+
+#undef SDB_DEBUGGING_INFO
+
+/* We want to output DBX debugging information.  */
+
+#define DBX_DEBUGGING_INFO
