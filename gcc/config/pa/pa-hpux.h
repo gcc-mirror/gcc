32ef4b19908442b9eccbf2fad9afd96d48a92934
tege <tege@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-11-09 17:33:01 +0000

    Move definition of TARGET_DEFAULT to before inclusion of other pa*h files. Only define TARGET_DEFAULT if not already defined. Get rid of hpux8 conditionals.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@2721 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/pa/pa-hpux.h
+++ gcc/config/pa/pa-hpux.h
@@ -1,3 +1,26 @@
+/* Definitions of target machine for GNU compiler, for HP-UX.
+   Copyright (C) 1991 Free Software Foundation, Inc.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#ifndef TARGET_DEFAULT
+#define TARGET_DEFAULT 8	/* TARGET_SHARED_LIBS */
+#endif
+
 #include "pa.h"
 
 /* Make GCC agree with types.h.  */
@@ -26,10 +49,5 @@
 #undef CPP_PREDEFINES
 #define CPP_PREDEFINES "-Dhppa -Dhp9000s800 -D__hp9000s800 -Dhp9k8 -DPWB -Dhpux -Dunix -D_HPUX_SOURCE"
 
-/* Link against shared libraries */
-#ifdef hpux8
-#undef TARGET_DEFAULT
-#define TARGET_DEFAULT 8 
 #undef LINK_SPEC
 #define LINK_SPEC "-u main %{g*:-a archive} %{p:-a archive} %{pg:-a archive}"
-#endif
