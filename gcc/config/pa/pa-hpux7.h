52ef876432b6b72bcd1c31b634b955a86abd04f0
Torbjorn Granlund <tege@gnu.org>
1992-11-21 23:17:25 +0000

    entered into RCS
    From-SVN: r2769

---
+++ gcc/config/pa/pa-hpux7.h
@@ -0,0 +1,50 @@
+/* Definitions of target machine for GNU compiler, for HP-UX.
+   Copyright (C) 1991 Free Software Foundation, Inc.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#ifndef TARGET_DEFAULT
+#define TARGET_DEFAULT 0
+#endif
+
+#include "pa.h"
+
+/* Make GCC agree with types.h.  */
+#undef SIZE_TYPE
+#undef PTRDIFF_TYPE
+#undef WCHAR_TYPE
+#undef WCHAR_TYPE_SIZE
+
+#define SIZE_TYPE "unsigned int"
+#define PTRDIFF_TYPE "int"
+#define WCHAR_TYPE "unsigned int"
+#define WCHAR_TYPE_SIZE 32
+
+/* HPUX doesn't use any debugging format that GCC knows about.  */
+#undef DBX_DEBUGGING_INFO
+
+/* Like the default, except no -lg.  */
+#define LIB_SPEC "%{!p:%{!pg:-lc}}%{p:-lc_p}%{pg:-lc_p}"
+
+/* Control how gcc finds libgcc.a and how it passes -L options.  */
+#define LINK_LIBGCC_SPECIAL
+#define RELATIVE_PREFIX_NOT_LINKDIR
+
+#define HAVE_ATEXIT
+
+#undef CPP_PREDEFINES
+#define CPP_PREDEFINES "-Dhppa -Dhp9000s800 -D__hp9000s800 -Dhp9k8 -DPWB -Dhpux -Dunix -D_HPUX_SOURCE"
