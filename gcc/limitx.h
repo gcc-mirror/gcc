30c08fcc6b8b6823b156b1b4b23420b8b2715152
Richard Stallman <rms@gnu.org>
1992-11-30 23:08:48 +0000

    Test and define _GCC_LIMITS_H_, not _LIMITS_H_.
    From-SVN: r2822

--- gcc/limitx.h
+++ gcc/limitx.h
@@ -1,8 +1,10 @@
 /* This administrivia gets added to the beginning of limits.h
    if the system has its own version of limits.h.  */
 
-#ifndef _LIMITS_H_  /* Terminated in limity.h.  */
-#define _LIMITS_H_
+/* We use _GCC_LIMITS_H_ because we want this not to match
+   any macros that the system's limits.h uses for its own purposes.  */
+#ifndef _GCC_LIMITS_H_  /* Terminated in limity.h.  */
+#define _GCC_LIMITS_H_
 
 #ifndef _LIBC_LIMITS_H_
 /* Use <...> so that we find syslimits.h only in system include dirs.  */
