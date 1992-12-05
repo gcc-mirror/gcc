09b6dfc1765a33579616ad48fca5a36d5a631a9e
Richard Kenner <kenner@gcc.gnu.org>
1992-12-04 19:13:25 -0500

    (ASM_OUTPUT_ADDR_DIFF_ELT, JUMP_TABLES_IN_TEXT_SECTION): Copy definitions from i386v4.h to get PIC code correct.
    From-SVN: r2833

--- gcc/config/i386/386bsd.h
+++ gcc/config/i386/386bsd.h
@@ -129,3 +129,19 @@
 									\
   return ret;								\
 }
+
+/* The following macros are stolen from i386v4.h */
+/* These have to be defined to get PIC code correct */
+
+/* This is how to output an element of a case-vector that is relative.
+   This is only used for PIC code.  See comments by the `casesi' insn in
+   i386.md for an explanation of the expression this outputs. */
+
+#undef ASM_OUTPUT_ADDR_DIFF_ELT
+#define ASM_OUTPUT_ADDR_DIFF_ELT(FILE, VALUE, REL) \
+  fprintf (FILE, "\t.long _GLOBAL_OFFSET_TABLE_+[.-%s%d]\n", LPREFIX, VALUE)
+
+/* Indicate that jump tables go in the text section.  This is
+   necessary when compiling PIC code.  */
+
+#define JUMP_TABLES_IN_TEXT_SECTION
