7aee14a2a77c79ff95947c3f66b846c4de1bccd5
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-12-16 12:33:01 +0000

    (assembler_trampoline_template): Always put template in text section.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@2883 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/varasm.c
+++ gcc/varasm.c
@@ -1071,6 +1071,11 @@ assemble_trampoline_template ()
   char *name;
   int align;
 
+  /* Some assemblers don't like instructions in data segment, so always
+     put trampoline template in text segment.  */
+
+  text_section ();
+
   /* Write the assembler code to define one.  */
   align = floor_log2 (FUNCTION_BOUNDARY / BITS_PER_UNIT);
   if (align > 0)
