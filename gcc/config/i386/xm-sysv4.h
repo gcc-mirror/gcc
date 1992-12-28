f1b63b839f98fc266c4d5adb6e851c215342bc66
jrv <jrv@138bc75d-0d04-0410-961f-82ee72b054a4>
1992-12-28 02:30:13 +0000

    Renamed from xm-i38v4.h. Use new file names for included 386 files.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@2964 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/xm-sysv4.h
+++ gcc/config/i386/xm-sysv4.h
@@ -1,6 +1,6 @@
 /* Configuration for GCC for Intel i386 running System V Release 4.  */
 
-#include "xm-i386.h"
+#include "i386/xm-i386.h"
 #include "xm-svr4.h"
 
 /* If compiled with GNU C, use the built-in alloca.  */
