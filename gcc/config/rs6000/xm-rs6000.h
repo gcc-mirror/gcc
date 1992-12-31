57933edc91fa0f7afb1d47e9b08580b329266793
Richard Kenner <kenner@gcc.gnu.org>
1992-12-31 08:52:40 -0500

    (REAL_NM_FILE_NAME): Delete from here.
    From-SVN: r3009

--- gcc/config/rs6000/xm-rs6000.h
+++ gcc/config/rs6000/xm-rs6000.h
@@ -56,8 +56,5 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 /* AIX is a flavor of System V */
 #define	USG
 
-/* This is the only version of nm that collect2 can work with.  */
-#define REAL_NM_FILE_NAME "/usr/ucb/nm"
-
 /* Big buffers improve performance.  */
 #define IO_BUFFER_SIZE (0x8000 - 4096)
