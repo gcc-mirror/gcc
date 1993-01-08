60c9835ecc279cb9511b2ed79a3770e3be542d1e
James Van Artsdalen <jrv@gnu.org>
1993-01-08 09:52:03 +0000

    (perform_*): Move to i386/perform.h.
    Include i386/perform.h.
    
    From-SVN: r3155

--- gcc/config/i386/osfrose.h
+++ gcc/config/i386/osfrose.h
@@ -24,6 +24,9 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #include "halfpic.h"
 #include "i386/gstabs.h"
 
+/* Get perform_* macros to build libgcc.a.  */
+#include "i386/perform.h"
+
 #define OSF_OS
 
 #undef  WORD_SWITCH_TAKES_ARG
@@ -372,76 +375,3 @@ while (0)
 
 /* Generate calls to memcpy, etc., not bcopy, etc. */
 #define TARGET_MEM_FUNCTIONS
-
-
-
-/* Defines to be able to build libgcc.a with GCC.  */
-
-/* It might seem that these are not important, since gcc 2 will never
-   call libgcc for these functions.  But programs might be linked with
-   code compiled by gcc 1, and then these will be used.  */
-
-#define perform_udivsi3(a,b)						\
-{									\
-  register int dx asm("dx");						\
-  register int ax asm("ax");						\
-									\
-  dx = 0;								\
-  ax = a;								\
-  asm ("divl %3" : "=a" (ax), "=d" (dx) : "a" (ax), "g" (b), "d" (dx));	\
-  return ax;								\
-}
-
-#define perform_divsi3(a,b)						\
-{									\
-  register int dx asm("dx");						\
-  register int ax asm("ax");						\
-									\
-  ax = a;								\
-  asm ("cltd\n\tidivl %3" : "=a" (ax), "=d" (dx) : "a" (ax), "g" (b));	\
-  return ax;								\
-}
-
-#define perform_umodsi3(a,b)						\
-{									\
-  register int dx asm("dx");						\
-  register int ax asm("ax");						\
-									\
-  dx = 0;								\
-  ax = a;								\
-  asm ("divl %3" : "=a" (ax), "=d" (dx) : "a" (ax), "g" (b), "d" (dx));	\
-  return dx;								\
-}
-
-#define perform_modsi3(a,b)						\
-{									\
-  register int dx asm("dx");						\
-  register int ax asm("ax");						\
-									\
-  ax = a;								\
-  asm ("cltd\n\tidivl %3" : "=a" (ax), "=d" (dx) : "a" (ax), "g" (b));	\
-  return dx;								\
-}
-
-#define perform_fixdfsi(a)						\
-{									\
-  auto unsigned short ostatus;						\
-  auto unsigned short nstatus;						\
-  auto int ret;								\
-  auto double tmp;							\
-									\
-  &ostatus;			/* guarantee these land in memory */	\
-  &nstatus;								\
-  &ret;									\
-  &tmp;									\
-									\
-  asm volatile ("fnstcw %0" : "=m" (ostatus));				\
-  nstatus = ostatus | 0x0c00;						\
-  asm volatile ("fldcw %0" : /* no outputs */ : "m" (nstatus));		\
-  tmp = a;								\
-  asm volatile ("fldl %0" : /* no outputs */ : "m" (tmp));		\
-  asm volatile ("fistpl %0" : "=m" (ret));				\
-  asm volatile ("fldcw %0" : /* no outputs */ : "m" (ostatus));		\
-									\
-  return ret;								\
-}
