cec64920ac1731485dfe5880300673909452f3c7
jrv <jrv@138bc75d-0d04-0410-961f-82ee72b054a4>
1993-01-11 02:43:11 +0000

    Include i386/xm-i386.h. Remove redundant defines.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@3189 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/xm-aix.h
+++ gcc/config/i386/xm-aix.h
@@ -17,42 +17,15 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-
-/* #defines that need visibility everywhere.  */
-#ifdef FALSE
-#undef FALSE
-#endif
-#ifdef TRUE
-#undef TRUE
-#endif
-#define FALSE 0
-#define TRUE 1
-
-/* This describes the machine the compiler is hosted on.  */
-#define HOST_BITS_PER_CHAR 8
-#define HOST_BITS_PER_SHORT 16
-#define HOST_BITS_PER_INT 32
-#define HOST_BITS_PER_LONG 32
-#define HOST_BITS_PER_LONGLONG 64
-
-/* Arguments to use with `exit'.  */
-#define SUCCESS_EXIT_CODE 0
-#define FATAL_EXIT_CODE 33
-
 #define USG
 
-/* target machine dependencies.
-   tm.h is a symbolic link to the actual target specific file.   */
-#include "tm.h"
+#include "i386/xm-i386.h"
 
 #define bcopy(a,b,c) memcpy (b,a,c)
 #define bzero(a,b) memset (a,0,b)
 #define bcmp(a,b,c) memcmp (a,b,c)
 
-/* If compiled with GNU C, use the built-in alloca.  */
-#undef alloca
-#ifdef __GNUC__
-#define alloca __builtin_alloca
-#else
+/* If not compiled with GNU C, use the portable alloca.  */
+#ifndef __GNUC__
 #define USE_C_ALLOCA
 #endif
