1c31414ee7d6ab8a9538300814d8da9d3fe71730
wilson <wilson@138bc75d-0d04-0410-961f-82ee72b054a4>
1993-02-19 01:26:56 +0000

    (STARTING_FRAME_OFFSET): Set to -8.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@3492 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sparc/sparc.h
+++ gcc/config/sparc/sparc.h
@@ -661,7 +661,7 @@ extern char leaf_reg_backmap[];
    If FRAME_GROWS_DOWNWARD, this is the offset to the END of the
    first local allocated.  Otherwise, it is the offset to the BEGINNING
    of the first local allocated.  */
-#define STARTING_FRAME_OFFSET (-16)
+#define STARTING_FRAME_OFFSET (-8)
 
 /* If we generate an insn to push BYTES bytes,
    this says how many the stack pointer really advances by.
