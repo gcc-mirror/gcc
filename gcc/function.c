4752d3bc315f5c0230c3c94abe15d5dfb237ad3d
Richard Kenner <kenner@gcc.gnu.org>
1993-03-27 13:41:59 -0500

    (expand_function_start): When evaluating sizes of variable types, do so just for side-effects (the SAVE_EXPRs).
    From-SVN: r3896

--- gcc/function.c
+++ gcc/function.c
@@ -4396,7 +4396,7 @@ expand_function_start (subr, parms_have_cleanups)
 
   /* Evaluate now the sizes of any types declared among the arguments.  */
   for (tem = nreverse (get_pending_sizes ()); tem; tem = TREE_CHAIN (tem))
-    expand_expr (TREE_VALUE (tem), NULL_RTX, VOIDmode, 0);
+    expand_expr (TREE_VALUE (tem), const0_rtx, VOIDmode, 0);
 
   /* Make sure there is a line number after the function entry setup code.  */
   force_next_line_note ();
