20e389ef93f2903cfe37759459647db1677573f3
Richard Stallman <rms@gnu.org>
1993-04-09 21:40:24 +0000

    Initial revision
    From-SVN: r4055

---
+++ gcc/config/i386/go32.h
@@ -0,0 +1,10 @@
+/* Configuration for an i386 running MS-DOS with djgpp/go32 */
+
+#define YES_UNDERSCORES
+
+#include "i386/gas.h"
+
+#ifdef CPP_PREDEFINES
+#undef CPP_PREDEFINES
+#endif
+#define CPP_PREDEFINES "-Dunix -Di386 -DGO32 -DMSDOS"
