a3ee62e0065a4ab960ff9c1fdeea92cde1515ba9
Jim Wilson <wilson@gcc.gnu.org>
1993-05-06 11:07:34 -0700

    Include "clipper/clipper.h" instead of "clipper.h".
    From-SVN: r4369

--- gcc/config/clipper/clix.h
+++ gcc/config/clipper/clix.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-#include "clipper.h"
+#include "clipper/clipper.h"
 
 #include "svr3.h"
 
