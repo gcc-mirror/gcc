ecd023b66232bb57c7b65b8022f8b5a5992d7de0
Richard Stallman <rms@gnu.org>
1993-05-23 19:33:36 +0000

    (objc_protocol): New field `class_pointer'.
    From-SVN: r4543

--- gcc/objc/objc.h
+++ gcc/objc/objc.h
@@ -117,6 +117,7 @@ struct objc_class {
 
 #ifndef __OBJC__
 typedef struct objc_protocol {
+  struct objc_class* class_pointer;
   char *protocol_name;
   struct objc_protocol_list *protocol_list;
   struct objc_method_description_list *instance_methods, *class_methods; 
