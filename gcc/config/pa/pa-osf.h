47f3e1052817c46c311b5f254d9cdb14d9e82b65
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1993-07-14 16:26:07 +0000

    * pa-utahmach.h (CPP_PREDEFINES): Do not define HPUX_SOURCE, hp700, 	or HP700.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@4917 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/pa/pa-osf.h
+++ gcc/config/pa/pa-osf.h
@@ -22,7 +22,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #include "pa/pa.h"
 #undef CPP_PREDEFINES
-#define CPP_PREDEFINES "-Dhppa -Dhp9000s800 -D__hp9000s800 -Dhp9k8 -Dunix -D_HPUX_SOURCE -Dhp9000 -Dhp800 -Dspectrum -DREVARGV -Dhp700 -DHP700 -Dparisc -D__pa_risc -DPARISC -DBYTE_MSF -DBIT_MSF"
+#define CPP_PREDEFINES "-Dhppa -Dhp9000s800 -D__hp9000s800 -Dhp9k8 -Dunix -Dhp9000 -Dhp800 -Dspectrum -DREVARGV -Dparisc -D__pa_risc -DPARISC -DBYTE_MSF -DBIT_MSF"
 
 /* Don't default to pcc-struct-return, because gcc is the only compiler, and
    we want to retain compatibility with older gcc versions.  */
