1ea2a61b83335685086285c9ed0a3255d64b46fa
wilson <wilson@138bc75d-0d04-0410-961f-82ee72b054a4>
1993-07-21 21:19:38 +0000

    New File.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@4955 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/m68k/lynx.h
@@ -0,0 +1,35 @@
+/* Definitions for Motorola 680X0 running LynxOS.
+   Copyright (C) 1993 Free Software Foundation, Inc.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#include "m68k/m68k.h"
+#include "lynx.h"
+
+/* See m68k.h.  7 means 68020 with 68881.  */
+
+#ifndef TARGET_DEFAULT
+#define TARGET_DEFAULT 7
+#endif
+
+/* Names to predefine in the preprocessor for this target machine.  */
+#undef CPP_PREDEFINES
+#define CPP_PREDEFINES "-Dunix -Dmc68000 -DM68K -DLynx -DIBITS32"
+
+/* Every structure or union's size must be a multiple of 2 bytes.  */
+
+#define STRUCTURE_SIZE_BOUNDARY 16
