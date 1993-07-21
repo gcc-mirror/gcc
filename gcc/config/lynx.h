e4e135320bc9c40dca5c958dd7617c1f1933052c
wilson <wilson@138bc75d-0d04-0410-961f-82ee72b054a4>
1993-07-21 21:22:31 +0000

    New file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@4957 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/lynx.h
@@ -0,0 +1,77 @@
+/* Target independent definitions for LynxOS.  */
+
+/* ??? The -C option may need to change to whatever option the GNU assembler
+   actually accepts.  */
+#undef ASM_SPEC
+#define ASM_SPEC "%{mcoff:-C}"
+
+#undef CPP_SPEC
+#define CPP_SPEC "%{mthreads:-D_MULTITHREADED} %{mposix:-D_POSIX_SOURCE} %{msystem-v:-I/usr/include-v}"
+
+/* Provide required defaults for linker switches.  */
+/* ??? The -k option may need to change to whatever option the GNU linker
+   actually accepts.  This is to produce COFF output.  */
+/* ??? The -V option may need to change to whatever option the GNU linker
+   actually accepts.  This is to produce System-V magic numbers.  */
+#undef LINK_SPEC
+#define LINK_SPEC "%{!e*:-e __main} %{msystem-v:-V} %{mcoff:k}"
+
+#undef LIB_SPEC
+#define LIB_SPEC "%{mthreads:-L/lib/thread/}%{msystem-v:-lc_v}%{!msystem-v:%{mposix:-lc_p} -lc}"
+
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "%{p:%{mcoff:pinit1.o%s}%{!mcoff:pinit.o%s}}%{!p:%{msystem-v:%{mcoff:vinit1.o%s}%{!mcoff:vinit.o%s}}%{!msystem-v:%{mcoff:init1.o%s}%{!mcoff:init.o%s}}}"
+
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC "%{mcoff:initn.o%s} %{p:_etext.o%s}"
+
+#undef SIZE_TYPE
+#define SIZE_TYPE "unsigned int"
+
+#undef WCHAR_TYPE
+#define WCHAR_TYPE "int"
+
+#undef PTRDIFF_TYPE
+#define PTRDIFF_TYPE "long int"
+
+/* We want to output DBX debugging information.  */
+
+#define DBX_DEBUGGING_INFO
+
+/* Generate calls to memcpy, memcmp and memset.  */
+
+#define TARGET_MEM_FUNCTIONS
+
+/* Handle #pragma pack and sometimes #pragma weak.  */
+
+#define HANDLE_SYSV_PRAGMA
+
+#define TARGET_THREADS	(target_flags & MASK_THREADS)
+#define MASK_THREADS	0x40000000
+
+#define TARGET_POSIX	(target_flags & MASK_POSIX)
+#define MASK_POSIX	0x20000000
+
+#define TARGET_SYSTEM_V	(target_flags & MASK_SYSTEM_V)
+#define MASK_SYSTEM_V	0x10000000
+
+#define TARGET_COFF	(target_flags & MASK_COFF)
+#define MASK_COFF	0x08000000
+
+#undef SUBTARGET_SWITCHES
+#define SUBTARGET_SWITCHES \
+    {"threads",		MASK_THREADS},		\
+    {"posix",		MASK_POSIX},		\
+    {"system-v",	MASK_SYSTEM_V},		\
+    {"coff",		MASK_COFF},
+
+#undef SUBTARGET_OVERRIDE_OPTIONS
+#define SUBTARGET_OVERRIDE_OPTIONS \
+{ if (TARGET_SYSTEM_V && profile_flag)			\
+    warning ("-msystem-v and -p are incompatible");		\
+  if (TARGET_SYSTEM_V && TARGET_THREADS)			\
+    warning ("-msystem-v and -mthreads are incompatible"); }
+
+/* Define this so that C++ destructors will use atexit.  */
+
+#define HAVE_ATEXIT
---
+++ gcc/config/sparc/lynx.h
@@ -0,0 +1,30 @@
+/* Definitions for SPARC running LynxOS.
+   Copyright (C) 1993 Free Software Foundation, Inc.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#include "sparc/sparc.h"
+#include "lynx.h"
+
+/* ??? Must redefine to get sparclite and v8 defines.  Can this be done
+   differently?  */
+#undef CPP_SPEC
+#define CPP_SPEC "%{mthreads:-D_MULTITHREADED} %{msparclite:-D__sparclite__} %{mv8:-D__sparc_v8__} %{mposix:-D_POSIX_SOURCE} %{msystem-v:-lynx-sysv}"
+
+/* Names to predefine in the preprocessor for this target machine.  */
+#undef CPP_PREDEFINES
+#define CPP_PREDEFINES "-Dunix -Dsparc -DLynx -DIBITS32"
