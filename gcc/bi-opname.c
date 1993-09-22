c6b9e095c3e27774d0e47f03d3a9ed2920d968f0
bson <bson@138bc75d-0d04-0410-961f-82ee72b054a4>
1993-09-22 15:48:59 +0000

    Initial revision
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@5389 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/bi-opname.c
@@ -0,0 +1,35 @@
+/* Utility to generate opcode name list from bytecode definition file.
+   Copyright (C) 1993 Free Software Foundation, Inc.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#include "bi-defs.h"
+
+int
+main()
+{
+  struct def *d;
+  struct variation *v;
+
+  yyparse();
+  reverse();
+
+  for (d = defs; d; d = d->next)
+    for (v = d->variations; v; v = v->next)
+      printf("\"%s%s\",\n", d->basename, v->name);
+  return 0;
+}
