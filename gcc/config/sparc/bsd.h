052b29215f8f1bb3ea9e61918d752e95298f621b
wilson <wilson@138bc75d-0d04-0410-961f-82ee72b054a4>
1993-09-28 22:35:19 +0000

    New file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@5507 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/sparc/bsd.h
@@ -0,0 +1,7 @@
+#include "sparc/sparc.h"
+
+#undef LIB_SPEC
+#define LIB_SPEC	"%{!p:%{!pg:-lc}}%{p:-lc_p}%{pg:-lc_p}"
+ 
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC	"%{pg:gcrt0.o%s}%{!pg:%{p:gcrt0.o%s}%{!p:crt0.o%s}}"
