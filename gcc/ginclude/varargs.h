c8cdb42bc6c1050b7f687b987275e8d160e25eb7
Richard Stallman <rms@gnu.org>
1993-10-02 02:11:53 +0000

    Test __sequent__ like __BSD_NET2__.
    From-SVN: r5556

--- gcc/ginclude/varargs.h
+++ gcc/ginclude/varargs.h
@@ -147,20 +147,23 @@ typedef __gnuc_va_list va_list;
    But on BSD NET2 we must not test or define or undef it.
    (Note that the comments in NET 2's ansi.h
    are incorrect for _VA_LIST_--see stdio.h!)  */
-#if !defined (_VA_LIST_) || defined (__BSD_NET2__) || defined (____386BSD____)
+/* Michael Eriksson <mer@sics.se> at Thu Sep 30 11:00:57 1993:
+   Sequent defines _VA_LIST_ in <machine/machtypes.h> to be the type to
+   use for va_list (``typedef _VA_LIST_ va_list'') */
+#if !defined (_VA_LIST_) || defined (__BSD_NET2__) || defined (____386BSD____) || defined (__sequent__)
 /* The macro _VA_LIST is used in SCO Unix 3.2.  */
 #ifndef _VA_LIST
 /* The macro _VA_LIST_T_H is used in the Bull dpx2  */
 #ifndef _VA_LIST_T_H
 #define _VA_LIST_T_H
-#if !(defined (__BSD_NET2__) || defined (____386BSD____))
+#if !(defined (__BSD_NET2__) || defined (____386BSD____) || defined (__sequent__))
 #define _VA_LIST_
 #endif
 #define _VA_LIST
 typedef __gnuc_va_list va_list;
 #endif /* not _VA_LIST_T_H */
 #endif /* not _VA_LIST */
-#endif /* not _VA_LIST_ */
+#endif /* not _VA_LIST_, except on certain systems */
 
 #endif /* not __svr4__ */
 
