abeb85d65f04f6a142cd3d95fc3b219907bfec17
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1993-10-02 18:03:56 +0000

    (build_string): Make lifetime of string the same as tree node.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@5558 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree.c
+++ gcc/tree.c
@@ -1254,7 +1254,7 @@ build_string (len, str)
 {
   register tree s = make_node (STRING_CST);
   TREE_STRING_LENGTH (s) = len;
-  TREE_STRING_POINTER (s) = obstack_copy0 (saveable_obstack, str, len);
+  TREE_STRING_POINTER (s) = obstack_copy0 (expression_obstack, str, len);
   return s;
 }
 
