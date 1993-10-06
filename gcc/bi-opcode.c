c9b9e07c7a42e52020dce9e4a1779e501bd146c1
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1993-10-06 16:12:38 +0000

    Include hconfig.h.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@5634 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/bi-opcode.c
+++ gcc/bi-opcode.c
@@ -18,6 +18,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #include <stdio.h>
+#include "hconfig.h"
 #include "bi-defs.h"
 
 int
