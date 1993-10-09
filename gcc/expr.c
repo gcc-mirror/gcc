37568125833520ba34d3723db4c888013ae0d725
Richard Kenner <kenner@gcc.gnu.org>
1993-10-09 15:19:52 -0400

    (expand_expr, case ABS_EXPR): Don't copy twice into volatile MEM.
    From-SVN: r5698

--- gcc/expr.c
+++ gcc/expr.c
@@ -5001,6 +5001,7 @@ expand_expr (exp, target, tmode, modifier)
       target = original_target;
       temp = gen_label_rtx ();
       if (target == 0 || ! safe_from_p (target, TREE_OPERAND (exp, 0))
+	  || (GET_CODE (target) == MEM && MEM_VOLATILE_P (target))
 	  || (GET_CODE (target) == REG
 	      && REGNO (target) < FIRST_PSEUDO_REGISTER))
 	target = gen_reg_rtx (mode);
