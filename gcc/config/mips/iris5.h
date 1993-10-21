3c7914d4c4db5f19f9824580a95a3d34e2798848
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1993-10-21 18:36:59 +0000

    (ASM_SPEC, LINK_SPEC, CPP_PREDEFINES): Update.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@5863 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/mips/iris5.h
+++ gcc/config/mips/iris5.h
@@ -29,6 +29,15 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #undef CPP_PREDEFINES
 #define CPP_PREDEFINES	"\
+-D_MIPS_FPSET=16 -D_MIPS_ISA=_MIPS_ISA_MIPS1 -D_MIPS_SIM_ABI32 \
 -Dunix -Dmips -Dsgi -DSVR3 -Dhost_mips -DMIPSEB -DSYSTYPE_SYSV \
--D_MIPS_SZLONG=32 \
+-D_SYSTYPE_SVR4 -D_LONGLONG -D_MIPSEB -D_SGI_SOURCE -D__SDO__  \
+-D_MIPS_SZLONG=32 -D_MIPS_SZINT=32 -D_MIPS_SZPTR=32 -D_SVR4_SOURCE \
 -Asystem(unix) -Asystem(svr3) -Acpu(mips) -Amachine(mips)"
+
+#undef ASM_SPEC
+#define ASM_SPEC "-elf -KPIC -EB %{O*:-O2 -fullasopt} %{!O*:-O1}"
+
+#undef LINK_SPEC
+#define LINK_SPEC "-elf -_SYSTYPE_SVR4 -require_dynamic_link \
+ _rld_new_interface -no_unresolved -KPIC -call_shared -transitive_link"
