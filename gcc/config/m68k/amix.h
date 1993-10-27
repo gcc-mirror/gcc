4cfe753245520015bac7524c24b6a09556745a17
Richard Stallman <rms@gnu.org>
1993-10-27 04:01:32 +0000

    (NO_DOLLAR_IN_LABEL): Defined.
    From-SVN: r5914

--- gcc/config/m68k/amix.h
+++ gcc/config/m68k/amix.h
@@ -21,6 +21,9 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #include "m68k/m68kv4.h"
 
+/* ler@lerami.lerctr.org reports as doesn't accept $.  */
+#define NO_DOLLAR_IN_LABEL
+
 /* Alter assembler syntax for fsgldiv and fsglmul.
    It is highly likely that this is a generic SGS m68k assembler dependency.
    If so, it should eventually be handled in the m68k/sgs.h ASM_OUTPUT_OPCODE
