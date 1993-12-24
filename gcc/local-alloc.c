2d19a71c10fd27e6bd565150104a81541b039945
Jim Wilson <wilson@gcc.gnu.org>
1993-12-23 17:35:17 -0800

    (optimize_reg_copy_1): After decreasing sregno's reg_live_length, correct it if it is now obviously too small.
    From-SVN: r6291

--- gcc/local-alloc.c
+++ gcc/local-alloc.c
@@ -791,6 +791,11 @@ optimize_reg_copy_1 (insn, dest, src)
 	      if (sregno >= FIRST_PSEUDO_REGISTER)
 		{
 		  reg_live_length[sregno] -= length;
+		  /* reg_live_length is only an approximation after combine
+		     if sched is not run, so make sure that we still have
+		     a reasonable value.  */
+		  if (reg_live_length[sregno] < 2)
+		    reg_live_length[sregno] = 2;
 		  reg_n_calls_crossed[sregno] -= n_calls;
 		}
 
