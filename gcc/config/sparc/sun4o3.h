10a78a0a2aa98632bc19607f1c2e52f955d7de80
Jim Wilson <wilson@gcc.gnu.org>
1994-01-31 12:34:29 -0800

    (TARGET_DEFAULT): Delete.
    From-SVN: r6439

--- gcc/config/sparc/sun4o3.h
+++ gcc/config/sparc/sun4o3.h
@@ -1,9 +1,5 @@
 #include "sparc/sparc.h"
 
-/* Define the Sun-asm flag, which is necessary for Sun 4 with os version 3.  */
-#undef TARGET_DEFAULT
-#define TARGET_DEFAULT 7
-
 #undef FUNCTION_PROFILER
 #define FUNCTION_PROFILER(FILE, LABELNO)  \
   fprintf (FILE, "\tsethi %%hi(LP%d),%%o0\n\tcall .mcount\n\tor %%lo(LP%d),%%o0,%%o0\n", \
