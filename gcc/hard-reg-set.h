896389552ba444585e6718a98d1d6a4267b7b17e
Richard Kenner <kenner@gcc.gnu.org>
1994-02-03 13:24:04 -0500

    (GO_IF_HARD_REG_EQUAL): Fix typo.
    From-SVN: r6476

--- gcc/hard-reg-set.h
+++ gcc/hard-reg-set.h
@@ -1,5 +1,5 @@
 /* Sets (bit vectors) of hard registers, and operations on them.
-   Copyright (C) 1987, 1992 Free Software Foundation, Inc.
+   Copyright (C) 1987, 1992, 1994 Free Software Foundation, Inc.
 
 This file is part of GNU CC
 
@@ -181,7 +181,7 @@ do { register HARD_REG_ELT_TYPE *scan_xp_ = (X), *scan_yp_ = (Y); \
 do { register HARD_REG_ELT_TYPE *scan_xp_ = (X), *scan_yp_ = (Y); \
      register int i;						\
      for (i = 0; i < HARD_REG_SET_LONGS; i++)			\
-       if (*scan_xp_++ != ~ *scan_yp_++)) break;		\
+       if (*scan_xp_++ != *scan_yp_++)) break;		\
      if (i == HARD_REG_SET_LONGS) goto TO; } while (0)
 
 #endif
