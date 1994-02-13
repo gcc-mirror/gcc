d935b239a709c741dd784b3425a6dbcfdd4b7376
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
1994-02-13 00:48:58 +0000

    Use i386/xm-osf.h as OSF/1 xm file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@6544 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/i386/xm-osf.h
@@ -0,0 +1,31 @@
+/* Configuration for GNU C-compiler for 386 running OSF/1
+   Copyright (C) 1994 Free Software Foundation, Inc.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#undef TRUE
+#undef FALSE
+
+#include "i386/xm-i386.h"
+
+#define bcopy(a,b,c) memcpy (b,a,c)
+#define bzero(a,b) memset (a,0,b)
+#define bcmp(a,b,c) memcmp (a,b,c)
+
+#define HAVE_PUTENV
+#define HAVE_VPRINTF
+
