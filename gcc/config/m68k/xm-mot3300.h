0d920bbf1a91445ee1b3f9948c16d2a98fda4e7f
Richard Kenner <kenner@gcc.gnu.org>
1994-04-16 19:19:06 -0400

    Add comment (and copyright notice).
    From-SVN: r7053

--- gcc/config/m68k/xm-mot3300.h
+++ gcc/config/m68k/xm-mot3300.h
@@ -1,3 +1,24 @@
+/* Configuration for GNU C-compiler for Motorola 68000 family.
+   SysV68 Motorola 3300 Delta Series
+   Copyright (C) 1994 Free Software Foundation, Inc.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+
 #define USG
 
 #include "m68k/xm-m68k.h"
@@ -11,6 +32,7 @@
 
 #define NO_SYS_SIGLIST
 
+/* do not use alloca from -lPW with cc, because function epilogues use %sp */
 #ifndef __GNUC__
 #define USE_C_ALLOCA
 #endif
