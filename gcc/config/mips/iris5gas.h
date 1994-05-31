54a53d8cf50eaa45201e59a6a209e04bfc126148
Jim Wilson <wilson@gcc.gnu.org>
1994-05-31 16:37:39 -0700

    New file.
    From-SVN: r7402

---
+++ gcc/config/mips/iris5gas.h
@@ -0,0 +1,31 @@
+/* Definitions of target machine for GNU compiler.  Irix version 5 with gas. */
+
+#include "mips/iris5.h"
+
+/* Enable debugging.  */
+#define DBX_DEBUGGING_INFO
+#define SDB_DEBUGGING_INFO
+#define MIPS_DEBUGGING_INFO
+
+/* Irix 5 does not have some strange restrictions that Irix 3 had.  */
+#undef SET_FILE_NUMBER
+#define SET_FILE_NUMBER() ++num_source_filenames
+#undef LABEL_AFTER_LOC
+#define LABEL_AFTER_LOC(STREAM)
+
+/* We need to use .esize and .etype instead of .size and .type to
+   avoid conflicting with ELF directives.  These are only recognized
+   by gas, anyhow, not the native assembler.  */
+#undef PUT_SDB_SIZE
+#define PUT_SDB_SIZE(a)                                       \
+do {                                                  \
+  extern FILE *asm_out_text_file;                     \
+  fprintf (asm_out_text_file, "\t.esize\t%d;", (a));  \
+} while (0)
+
+#undef PUT_SDB_TYPE
+#define PUT_SDB_TYPE(a)                                       \
+do {                                                  \
+  extern FILE *asm_out_text_file;                     \
+  fprintf (asm_out_text_file, "\t.etype\t0x%x;", (a));        \
+} while (0)
