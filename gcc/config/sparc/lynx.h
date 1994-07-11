2f737c95e5392b3f736c52d69e103d38c72c13df
rms <rms@138bc75d-0d04-0410-961f-82ee72b054a4>
1994-07-11 20:58:36 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@7718 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sparc/lynx.h
+++ gcc/config/sparc/lynx.h
@@ -18,13 +18,35 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #include "sparc/sparc.h"
+
+#undef ASM_OUTPUT_IDENT
+#undef SELECT_SECTION
+#undef SELECT_RTX_SECTION
+
+#define BSS_SECTION_ASM_OP	".section\t\".bss\""
+
 #include "lynx.h"
 
 /* ??? Must redefine to get sparclite and v8 defines.  Can this be done
    differently?  */
+
 #undef CPP_SPEC
-#define CPP_SPEC "%{mthreads:-D_MULTITHREADED} %{msparclite:-D__sparclite__} %{mv8:-D__sparc_v8__} %{mposix:-D_POSIX_SOURCE} %{msystem-v:-lynx-sysv}"
+#define CPP_SPEC "%{mthreads:-D_MULTITHREADED}  \
+  %{mposix:-D_POSIX_SOURCE}  \
+  %{msystem-v:-I/usr/include_v}  \
+  %{msparclite:-D__sparclite__} %{mv8:-D__sparc_v8__}"
 
 /* Names to predefine in the preprocessor for this target machine.  */
+
 #undef CPP_PREDEFINES
-#define CPP_PREDEFINES "-Dunix -Dsparc -DLynx -DIBITS32 -Asystem(unix) -Asystem(lynx) -Acpu(sparc) -Amachine(sparc)"
+#define CPP_PREDEFINES "-Dunix -Dsparc -DSPARC -DLynx -DLYNX -DIBITS32 -Asystem(unix) -Asystem(lynx) -Acpu(sparc) -Amachine(sparc)"
+
+#undef LINK_SPEC
+
+/* Sparc version of libc.a has references to libm.a (printf calls pow for
+   instance), so we must always link both. */
+
+#undef LIB_SPEC
+#define LIB_SPEC "%{mthreads:-L/lib/thread/}  \
+  %{msystem-v:-lc_v -lm_v -lc_v}  \
+  %{!msystem-v:%{mposix:-lc_p} -lc -lm -lc}"
