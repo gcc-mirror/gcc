d996122c30e9572f56f71ba19adfedb928596b0d
Doug Evans <dje@gnu.org>
1994-07-16 20:45:33 +0000

    Add libgcc copyright.
    From-SVN: r7773

--- gcc/config/h8300/lib1funcs.asm
+++ gcc/config/h8300/lib1funcs.asm
@@ -1,7 +1,37 @@
 ;; libgcc1 routines for the Hitachi h8/300 cpu.
 ;; Contributed by Steve Chamberlain.
 ;; sac@cygnus.com
-;; This file is in the public domain.
+
+/* Copyright (C) 1994 Free Software Foundation, Inc.
+
+This file is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
+
+In addition to the permissions in the GNU General Public License, the
+Free Software Foundation gives you unlimited permission to link the
+compiled version of this file with other programs, and to distribute
+those programs without any restriction coming from the use of this
+file.  (The General Public License restrictions do apply in other
+respects; for example, they cover modification of the file, and
+distribution when not linked into another program.)
+
+This file is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+/* As a special exception, if you link this library with other files,
+   some of which are compiled with GCC, to produce an executable,
+   this library does not by itself cause the resulting executable
+   to be covered by the GNU General Public License.
+   This exception does not however invalidate any other reasons why
+   the executable file might be covered by the GNU General Public License.  */
 
 /* Assembler register definitions.  */
 
