6dd49058f55c734005ad40d3202a9ee58e3a0bc5
Richard Kenner <kenner@gcc.gnu.org>
1994-09-07 18:11:06 -0400

    (simplify_set): Only convert IF_THEN_ELSE to logical expression if mode of object being compared is result mode.
    From-SVN: r8033

--- gcc/combine.c
+++ gcc/combine.c
@@ -4369,6 +4369,7 @@ simplify_set (x)
       && GET_MODE_CLASS (GET_MODE (src)) == MODE_INT
       && (GET_CODE (XEXP (src, 0)) == EQ || GET_CODE (XEXP (src, 0)) == NE)
       && XEXP (XEXP (src, 0), 1) == const0_rtx
+      && GET_MODE (src) == GET_MODE (XEXP (XEXP (src, 0), 0))
       && (num_sign_bit_copies (XEXP (XEXP (src, 0), 0),
 			       GET_MODE (XEXP (XEXP (src, 0), 0)))
 	  == GET_MODE_BITSIZE (GET_MODE (XEXP (XEXP (src, 0), 0))))
