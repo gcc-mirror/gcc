e2d05e0f58f598bc176b70638e4309b8b0f70e77
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1994-10-17 23:21:51 +0000

    Initial revision
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@8293 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/rs6000/aix41.h
@@ -0,0 +1,26 @@
+/* Definitions of target machine for GNU compiler,
+   for IBM RS/6000 POWER running AIX version 4.1.
+   Copyright (C) 1994 Free Software Foundation, Inc.
+   Contributed by David Edelsohn (edelsohn@npac.syr.edu).
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+
+#include "rs6000/rs6000.h"
+
+#undef ASM_SPEC
+#define ASM_SPEC "-u -mpwr"
