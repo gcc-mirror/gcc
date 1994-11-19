15568776420762900c270bdaa268103d69ae8fbb
Jason Merrill <merrill@gnu.org>
1994-11-19 04:16:08 +0000

    Initial revision
    From-SVN: r8520

---
+++ gcc/config/sparc/sunos4.h
@@ -0,0 +1,22 @@
+/* Definitions of target machine for GNU compiler, for SunOS 4.x
+   Copyright (C) 1994 Free Software Foundation, Inc.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#define SUNOS4_SHARED_LIBRARIES 1
+
+#include "sparc/sparc.h"
