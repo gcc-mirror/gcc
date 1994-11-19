03f3609810ce73d30c5ace972765dffc2f33124e
Jim Wilson <wilson@gcc.gnu.org>
1994-11-19 15:37:10 -0800

    (expand_shift): Re-enable truncation of immediate shift count.
    From-SVN: r8527

--- gcc/expmed.c
+++ gcc/expmed.c
@@ -1653,7 +1653,7 @@ expand_shift (code, mode, shifted, amount, target, unsignedp)
 
   op1 = expand_expr (amount, NULL_RTX, VOIDmode, 0);
 
-#if 0 && SHIFT_COUNT_TRUNCATED
+#if SHIFT_COUNT_TRUNCATED
   if (SHIFT_COUNT_TRUNCATED
       && GET_CODE (op1) == CONST_INT
       && (unsigned HOST_WIDE_INT) INTVAL (op1) >= GET_MODE_BITSIZE (mode))
