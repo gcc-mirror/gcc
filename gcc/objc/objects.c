d5e4fa5e173ff180b66524db80e4392051dfa479
Richard Kenner <kenner@gcc.gnu.org>
1994-11-18 19:10:21 -0500

    (class_create_instance): Replace memchr with memset.
    From-SVN: r8515

--- gcc/objc/objects.c
+++ gcc/objc/objects.c
@@ -43,7 +43,7 @@ class_create_instance(Class* class)
     new = (*_objc_object_alloc)(class);
   if (new!=nil)
     {
-      memchr (new, 0, class->instance_size);
+      memset (new, 0, class->instance_size);
       new->class_pointer = class;
     }
   return new;
