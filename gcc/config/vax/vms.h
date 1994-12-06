3d8fa28356046e647007e6d22c07dddfcf6406c1
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1994-12-06 23:42:11 +0000

    (CPP_PREDEFINES): Set __GNUC_MINOR__ to 7.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@8617 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/vax/vms.h
+++ gcc/config/vax/vms.h
@@ -41,7 +41,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
    and GNU CPP is not used on VMS except with GNU C.  */
 #define CPP_PREDEFINES \
 "-Dvax -Dvms -DVMS -D__vax__ -D__vms__ -D__VMS__\
- -D__GNUC__=2 -D__GNUC_MINOR__=6 -Asystem(vms) -Acpu(vax) -Amachine(vax)"
+ -D__GNUC__=2 -D__GNUC_MINOR__=7 -Asystem(vms) -Acpu(vax) -Amachine(vax)"
 
 /* These match the definitions used in VAXCRTL, the VMS C run-time library */
 
