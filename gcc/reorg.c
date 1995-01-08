175a444d3f1456071b956446cf50237157e71f29
Richard Kenner <kenner@gcc.gnu.org>
1995-01-07 20:43:06 -0500

    Fix up copyright notice.
    From-SVN: r8726

--- gcc/reorg.c
+++ gcc/reorg.c
@@ -1,5 +1,5 @@
 /* Perform instruction reorganizations for delay slot filling.
-   Copyright (C) 1992, 1993, 1994. 1995 Free Software Foundation, Inc.
+   Copyright (C) 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
    Contributed by Richard Kenner (kenner@vlsi1.ultra.nyu.edu).
    Hacked by Michael Tiemann (tiemann@cygnus.com).
 
