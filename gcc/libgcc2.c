4e06d41e83ca82be1f7235e713691336b50bdc8c
sac <sac@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-01-12 16:18:12 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@8736 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/libgcc2.c
+++ gcc/libgcc2.c
@@ -1,6 +1,6 @@
 /* More subroutines needed by GCC output code on some machines.  */
 /* Compile this one with gcc.  */
-/* Copyright (C) 1989, 1992, 1993, 1994 Free Software Foundation, Inc.
+/* Copyright (C) 1989, 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
