5048d76c9af52aae733ed9aa0304fe757691aaa5
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-01-28 01:56:50 +0000

    (__lshldi3): Deleted.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@8831 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/libgcc2.c
+++ gcc/libgcc2.c
@@ -145,38 +145,6 @@ __negdi2 (u)
 }
 #endif
 
-#ifdef L_lshldi3
-DItype
-__lshldi3 (u, b)
-     DItype u;
-     word_type b;
-{
-  DIunion w;
-  word_type bm;
-  DIunion uu;
-
-  if (b == 0)
-    return u;
-
-  uu.ll = u;
-
-  bm = (sizeof (SItype) * BITS_PER_UNIT) - b;
-  if (bm <= 0)
-    {
-      w.s.low = 0;
-      w.s.high = (USItype)uu.s.low << -bm;
-    }
-  else
-    {
-      USItype carries = (USItype)uu.s.low >> bm;
-      w.s.low = (USItype)uu.s.low << b;
-      w.s.high = ((USItype)uu.s.high << b) | carries;
-    }
-
-  return w.ll;
-}
-#endif
-
 #ifdef L_lshrdi3
 DItype
 __lshrdi3 (u, b)
