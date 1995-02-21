a07190d44a054ef365d517f809f795e6a90ce747
Richard Kenner <kenner@gcc.gnu.org>
1995-02-21 08:15:01 -0500

    Initial revision
    From-SVN: r8987

---
+++ gcc/ginclude/iso646.h
@@ -0,0 +1,13 @@
+/* Macros for C programs written in national variants of ISO 646.  */
+
+#define and	&&
+#define and_eq	&=
+#define bitand	&
+#define bitor	|
+#define compl	~
+#define not	!
+#define not_eq	!=
+#define or	||
+#define or_eq	|=
+#define xor	^
+#define xor_eq	^=
