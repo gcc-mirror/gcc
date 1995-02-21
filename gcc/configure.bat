1f12ca9d5bfc8a55b3c2a53349f08dd66522a3ab
Richard Kenner <kenner@gcc.gnu.org>
1995-02-21 18:33:40 -0500

    Take arg specifying msdos or winnt batch file config.
    From-SVN: r9005

--- gcc/configure.bat
+++ gcc/configure.bat
@@ -1,2 +1,15 @@
 @echo off
-config\msdos\configure %1 %2 %3 %4
+if %1.==msdos. goto call_msdos
+if %1.==winnt. goto call_winnt
+echo Usage: configure msdos or configure winnt
+goto END
+
+:call_msdos
+call config\msdos\configure %1 %2 %3 %4
+goto END
+
+:call_winnt
+call config\i386\config-nt %1 %2 %3 %4
+goto END
+
+:END
