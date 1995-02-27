6ddaea1c2f7ea7ff29a0dd9efbe9b11227f890dd
Roland McGrath <roland@gcc.gnu.org>
1995-02-27 05:43:24 +0000

    (ASM_SPEC): Macro removed.
    From-SVN: r9088

--- gcc/config/gnu.h
+++ gcc/config/gnu.h
@@ -11,11 +11,6 @@
 #undef CPP_SPEC
 #define CPP_SPEC "%{posix:-D_POSIX_SOURCE} %{bsd:-D_BSD_SOURCE}"
 
-/* Provide an ASM_SPEC appropriate for GNU.  Currently we only deal
-   with the options for generating PIC code.  */
-#undef ASM_SPEC
-#define ASM_SPEC " %| %{fpic:-k} %{fPIC:-k -K}"
-
 /* Default C library spec.  Use -lbsd-compat for gcc -bsd.  */
 #undef LIB_SPEC
 #define LIB_SPEC "%{bsd:-lbsd-compat} %{!p:%{!pg:-lc}}%{p:-lc_p}%{pg:-lc_p}"
