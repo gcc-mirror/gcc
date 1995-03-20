ac6b994fe01af38989e75ae3394884fd92051c9c
Richard Kenner <kenner@gcc.gnu.org>
1995-03-20 08:03:42 -0500

    Put Alpha-specific decls before #include of runtime.h.
    From-SVN: r9212

--- gcc/objc/misc.c
+++ gcc/objc/misc.c
@@ -1,22 +1,23 @@
 /* GNU Objective C Runtime Miscellanious 
-   Copyright (C) 1993, 1994 Free Software Foundation, Inc.
+   Copyright (C) 1993, 1994, 1995 Free Software Foundation, Inc.
 
 Author: Kresten Krab Thorup
 
 This file is part of GNU CC.
 
-GNU CC is free software; you can redistribute it and/or modify it under the
-   terms of the GNU General Public License as published by the Free Software
-   Foundation; either version 2, or (at your option) any later version.
+GNU CC is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
 
-GNU CC is distributed in the hope that it will be useful, but WITHOUT ANY
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-   details.
+GNU CC is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
 
-You should have received a copy of the GNU General Public License along with
-   GNU CC; see the file COPYING.  If not, write to the Free Software
-   Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to the Free
+Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 /* As a special exception, if you link this library with files compiled with
    GCC to produce an executable, this does not cause the resulting executable
@@ -24,17 +25,17 @@ You should have received a copy of the GNU General Public License along with
    however invalidate any other reasons why the executable file might be
    covered by the GNU General Public License.  */
 
-#include "runtime.h"
-
-void objc_error(id object, const char* fmt, va_list);
-
-void (*_objc_error)(id, const char*, va_list) = objc_error;
-
 #ifdef __alpha__
 #include <stdlib.h>
 extern int write (int, const char*, int);
 extern size_t strlen (const char*);
 #endif
+
+#include "runtime.h"
+
+void objc_error(id object, const char* fmt, va_list);
+
+void (*_objc_error)(id, const char*, va_list) = objc_error;
 
 void
 objc_error(id object, const char* fmt, va_list ap)
