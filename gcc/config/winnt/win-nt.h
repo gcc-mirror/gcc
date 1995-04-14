bf4b1344e8a30b4ed553b17848d51b304cc3634b
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-04-14 13:05:11 +0000

    Fix typo with comment.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@9380 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/winnt/win-nt.h
+++ gcc/config/winnt/win-nt.h
@@ -17,7 +17,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #define TARGET_MEM_FUNCTIONS
 
