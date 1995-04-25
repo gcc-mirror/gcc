7d41fad55b1a03b074680a6b2045d844c8fe2ece
Richard Kenner <kenner@gcc.gnu.org>
1995-04-25 19:07:43 -0400

    Add copyright for 1995.
    From-SVN: r9460

--- gcc/reg-stack.c
+++ gcc/reg-stack.c
@@ -1,5 +1,5 @@
 /* Register to Stack convert for GNU compiler.
-   Copyright (C) 1992, 1993, 1994 Free Software Foundation, Inc.
+   Copyright (C) 1992, 1993, 1994, 1995 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
