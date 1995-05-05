02f45919b852ef48fe91f941082ffa8ee33c1d2b
dje <dje@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-05-05 17:32:07 +0000

    NXConstantString.h renamed to NXConstStr.h.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@9579 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/objc/NXConstStr.m
+++ gcc/objc/NXConstStr.m
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
  
-#include "objc/NXConstantString.h"
+#include "objc/NXConstStr.h"
 
 @implementation NXConstantString
 
