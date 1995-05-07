5b1a1a267b890478fce8a169614c8df9de7ee0de
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-05-07 21:27:58 +0000

    Use "go32" instead of "msdos" for future expansion. Add ^M to end of each line.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@9587 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/configure.bat
+++ gcc/configure.bat
@@ -1,10 +1,10 @@
 @echo off
-if %1.==msdos. goto call_msdos
+if %1.==go32. goto call_go32
 if %1.==winnt. goto call_winnt
-echo Usage: configure msdos or configure winnt
+echo Usage: configure go32 or configure winnt cpu
 goto END
 
-:call_msdos
+:call_go32
 call config\msdos\configure %1 %2 %3 %4
 goto END
 
@@ -13,3 +13,4 @@ call config\%2\config-nt %1 %2 %3 %4
 goto END
 
 :END
+
