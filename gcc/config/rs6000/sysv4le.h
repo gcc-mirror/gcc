13902e161ab71b495f1d067d96bc4b9147aff88a
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-05-10 21:37:55 +0000

    Little endian support.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@9615 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/rs6000/sysv4le.h
@@ -0,0 +1,25 @@
+/* Target definitions for GNU compiler for a little endian PowerPC
+   running System V.4
+   Copyright (C) 1995, Free Software Foundation, Inc.
+   Contributed by Cygnus Support.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#include "rs6000/sysv4.h"
+
+#undef TARGET_DEFAULT
+#define TARGET_DEFAULT (MASK_POWERPC | MASK_NEW_MNEMONICS | MASK_LITTLE_ENDIAN)
