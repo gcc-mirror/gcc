37aac87dabbabde69f7baab5cd6901f185995652
wilson <wilson@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-05-12 06:19:57 +0000

    (MD_STARTFILE_PREFIX, MD_EXEC_PREFIX, STARTFILE_SPEC, LINK_SPEC): Define.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@9636 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/mips/svr4-t.h
+++ gcc/config/mips/svr4-t.h
@@ -10,3 +10,21 @@
 /* Use the default values in mips.h.  */
 #undef MD_STARTFILE_PREFIX
 #undef MD_EXEC_PREFIX
+#define MD_STARTFILE_PREFIX "/usr/lib/cmplrs/cc/"
+#define MD_EXEC_PREFIX "/usr/lib/cmplrs/cc/"
+
+/* These are the same as the ones in svr4-5.h, except that references to
+   /svr4/ have been removed.  */
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "%{pg:gcrt0.o%s}%{!pg:%{p:mcrt0.o%s}%{!p:crt1.o%s}}\
+	%{ansi:/usr/lib/values-Xc.o%s} \
+                          %{!ansi: \
+                           %{traditional:/usr/lib/values-Xt.o%s} \
+                           %{!traditional:/usr/lib/values-Xa.o%s}}"
+
+#undef LINK_SPEC
+#define LINK_SPEC "\
+%{G*} %{EB} %{EL} %{mips1} %{mips2} %{mips3} \
+%{bestGnum} %{shared} %{non_shared} \
+%{call_shared} %{no_archive} %{exact_version} \
+%{!shared: %{!non_shared: %{!call_shared: -non_shared}}}"
