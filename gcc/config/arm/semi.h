7a830785f42d5c48e23d8fa0d4b895911b9bd6a7
Richard Earnshaw <erich@gnu.org>
1995-05-13 09:35:22 +0000

    Initial revision
    From-SVN: r9672

---
+++ gcc/config/arm/semi.h
@@ -0,0 +1,37 @@
+/* Definitions of target machine for GNU compiler.  ARM on semi-hosted platform
+   Copyright (C) 1994 Free Software Foundation, Inc.
+   Contributed by Richard Earnshaw (richard.earnshaw@armltd.co.uk)
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#define STARTFILE_SPEC  "crt0.o%s"
+
+#define LIB_SPEC "-lc"
+
+#define LINK_SPEC "-X"
+
+#define CPP_PREDEFINES \
+    "-Darm -Dsemi -Acpu(arm) -Amachine(arm)"
+
+#define TARGET_VERSION fputs (" (ARM/semi-hosted)", stderr);
+
+#define TARGET_DEFAULT 8	/* APCS-32 (ARM 6) */
+
+/* Currently we have no support for atexit.  */
+#define EXIT_BODY {}
+
+#include "arm/arm.h"
