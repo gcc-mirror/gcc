bbda4250979729d37c70077b735648cf20cb3b9b
Jason Merrill <merrill@gnu.org>
1995-05-24 21:46:55 +0000

    *** empty log message ***
    From-SVN: r9811

--- gcc/tree.c
+++ gcc/tree.c
@@ -3414,10 +3414,9 @@ type_list_equal (l1, l2)
   for (t1 = l1, t2 = l2; t1 && t2; t1 = TREE_CHAIN (t1), t2 = TREE_CHAIN (t2))
     if (TREE_VALUE (t1) != TREE_VALUE (t2)
 	|| (TREE_PURPOSE (t1) != TREE_PURPOSE (t2)
-	    && ! ((TREE_TYPE (TREE_PURPOSE (t1))
-		   == TREE_TYPE (TREE_PURPOSE (t2)))
-		  && 1 == simple_cst_equal (TREE_PURPOSE (t1),
-					    TREE_PURPOSE (t2)))))
+	    && ! (1 == simple_cst_equal (TREE_PURPOSE (t1), TREE_PURPOSE (t2))
+		  && (TREE_TYPE (TREE_PURPOSE (t1))
+		      == TREE_TYPE (TREE_PURPOSE (t2))))))
       return 0;
 
   return t1 == t2;
