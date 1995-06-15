054c11961d749ef72a77262dcc3c30221e5f7d3f
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-06-15 12:44:04 +0000

    Update FSF address.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@9970 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/objc/misc.c
+++ gcc/objc/misc.c
@@ -17,7 +17,8 @@ for more details.
 
 You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to the Free
-Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
 
 /* As a special exception, if you link this library with files compiled with
    GCC to produce an executable, this does not cause the resulting executable
