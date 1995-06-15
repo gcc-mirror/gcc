c58d4270afd64d680a7f9c705bb205efda8e56a4
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-06-15 12:29:51 +0000

    Update FSF address.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@9966 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/scan.h
+++ gcc/scan.h
@@ -13,7 +13,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #include <stdio.h>
 
