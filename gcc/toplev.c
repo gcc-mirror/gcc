e9fa0c7c6f01e606ae1957d5a17398b8e30d78f4
Richard Kenner <kenner@gcc.gnu.org>
1995-06-15 08:29:51 -0400

    Update FSF address.
    From-SVN: r9966

--- gcc/toplev.c
+++ gcc/toplev.c
@@ -15,7 +15,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
 
 /* This is the top level of cc1/c++.
    It parses command args, opens files, invokes the various passes
