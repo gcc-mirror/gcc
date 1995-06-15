5129e1716b2ffa7da7a4d1eb7001daf3aeb07d95
Richard Kenner <kenner@gcc.gnu.org>
1995-06-15 15:00:58 -0400

    Update header.
    From-SVN: r9978

--- gcc/config/m68k/3b1g.h
+++ gcc/config/m68k/3b1g.h
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
-the Free Software Foundation, 59 Temple Place - Suit 330,
+the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
 #include "m68k/m68k.h"
