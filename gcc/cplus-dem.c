8d62a21cd3d8c4d8311bab2e02530218e76db6a7
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-06-15 11:33:25 +0000

    Update FSF address.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@9959 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cplus-dem.c
+++ gcc/cplus-dem.c
@@ -16,8 +16,8 @@ Library General Public License for more details.
 
 You should have received a copy of the GNU Library General Public
 License along with libiberty; see the file COPYING.LIB.  If
-not, write to the Free Software Foundation, Inc., 675 Mass Ave,
-Cambridge, MA 02139, USA.  */
+not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
 
 /* This file exports two functions; cplus_mangle_opname and cplus_demangle.
 
