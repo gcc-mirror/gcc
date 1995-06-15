77b724562b322cb0c60060e909fbb79e60367df1
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-06-15 13:10:54 +0000

    Update FSF address.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@9973 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/alpha/win-nt.h
+++ gcc/config/alpha/win-nt.h
@@ -17,7 +17,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
 
 /* Say this is Windows/NT for the other config files.  */
 #define WINDOWS_NT 1
