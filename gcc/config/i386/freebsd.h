97aadbb900b6a63b00f850ee8abb11c7d2d3b4be
Richard Kenner <kenner@gcc.gnu.org>
1995-06-15 09:57:32 -0400

    Update FSF address.
    From-SVN: r9975

--- gcc/config/i386/freebsd.h
+++ gcc/config/i386/freebsd.h
@@ -17,7 +17,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
 
 /* This goes away when the math-emulator is fixed */
 #define TARGET_CPU_DEFAULT 0400		/* TARGET_NO_FANCY_MATH_387 */
