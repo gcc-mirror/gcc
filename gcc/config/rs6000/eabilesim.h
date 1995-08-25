7a8dd0d320fb71a8d5985177729a2236d996f86f
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-08-25 23:20:52 +0000

    Initial revision
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@10279 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/rs6000/eabilesim.h
@@ -0,0 +1,48 @@
+/* Support for GCC on simulated PowerPC systems targeted to embedded ELF
+   systems.
+   Copyright (C) 1995 Free Software Foundation, Inc.
+   Contributed by Cygnus Support.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+#include "rs6000/eabile.h"
+
+/* Right now, the simulator doesn't handle floating point, so disable it
+   by default.  */
+#undef TARGET_DEFAULT
+#define TARGET_DEFAULT (MASK_POWERPC | MASK_NEW_MNEMONICS | MASK_LITTLE_ENDIAN | MASK_SOFT_FLOAT)
+
+#undef TARGET_VERSION
+#define TARGET_VERSION fprintf (stderr, " (PowerPC Simulated)");
+
+#undef CPP_PREDEFINES
+#define CPP_PREDEFINES \
+  "-DPPC -D__embedded__ -D__simulator__ -Asystem(embedded) -Asystem(simulator) -Acpu(powerpc) -Amachine(powerpc)"
+
+/* Use the simulator crt0 and libgloss/newlib libraries */
+#undef  STARTFILE_SPEC
+#define	STARTFILE_SPEC "sim-crt0.o%s"
+
+#undef	LIB_SPEC
+#define	LIB_SPEC "-lsim -lc -lsim"
+
+#undef	LIBGCC_SPEC
+#define	LIBGCC_SPEC "libgcc.a%s"
+
+#undef	ENDFILE_SPEC
+#define	ENDFILE_SPEC ""
