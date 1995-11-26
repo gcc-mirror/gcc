4ac3710629d15b0fda9db16bda3d75c7d4619e57
Richard Kenner <kenner@gcc.gnu.org>
1995-11-26 14:40:30 -0500

    Reformat and add some new functions for gnat1.
    From-SVN: r10603

--- gcc/config/winnt/oldnames.c
+++ gcc/config/winnt/oldnames.c
@@ -1,24 +1,47 @@
-int open (const char *filename, int oflag, int pmode)
-{
-  return _open (filename, oflag, pmode);
-} 
-
-int read (int handle, void *buffer, unsigned int count)
-{
-  return _read (handle, buffer, count);
-}
-
-int close (int handle)
-{
-  return _close (handle);
-}
-
-int access (const char *path, int mode)
+int
+access (const char *path, int mode)
 {
   return _access (path, mode);
 }
 
-char *mktemp (char *template)
+int
+chmod (const char *filename, int pmode)
+{
+  return _chmod (filename, pmode);
+}
+
+int
+close (int handle)
+{
+  return _close (handle);
+}
+
+char *
+mktemp (char *template)
 {
    return (char *) _mktemp (template);
+}
+
+int
+open (const char *filename, int oflag, int pmode)
+{
+  return _open (filename, oflag, pmode);
+} 
+
+int
+read (int handle, void *buffer, unsigned int count)
+{
+  return _read (handle, buffer, count);
+}
+
+int
+unlink (const char *path)
+{
+  return _unlink (path);
+}
+
+int
+write (int handle, void *buffer, unsigned int count)
+{
+  return _write (handle, buffer, count);
 }
