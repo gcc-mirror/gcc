4b0c5859302c48a3af844497e8d26c8f9c5fe748
wilson <wilson@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-12-19 23:25:43 +0000

    (return_address_pointer_rtx): New global variable. (gen_rtx, init_emit_once): Add support for it.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@10817 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/emit-rtl.c
+++ gcc/emit-rtl.c
@@ -159,6 +159,10 @@ rtx static_chain_rtx;		/* (REG:Pmode STATIC_CHAIN_REGNUM) */
 rtx static_chain_incoming_rtx;	/* (REG:Pmode STATIC_CHAIN_INCOMING_REGNUM) */
 rtx pic_offset_table_rtx;	/* (REG:Pmode PIC_OFFSET_TABLE_REGNUM) */
 
+/* This is used to implement __builtin_return_address for some machines.
+   See for instance the MIPS port.  */
+rtx return_address_pointer_rtx;	/* (REG:Pmode RETURN_ADDRESS_POINTER_REGNUM) */
+
 rtx virtual_incoming_args_rtx;	/* (REG:Pmode VIRTUAL_INCOMING_ARGS_REGNUM) */
 rtx virtual_stack_vars_rtx;	/* (REG:Pmode VIRTUAL_STACK_VARS_REGNUM) */
 rtx virtual_stack_dynamic_rtx;	/* (REG:Pmode VIRTUAL_STACK_DYNAMIC_REGNUM) */
@@ -344,6 +348,11 @@ gen_rtx VPROTO((enum rtx_code code, enum machine_mode mode, ...))
       if (arg_pointer_rtx && regno == ARG_POINTER_REGNUM && mode == Pmode
 	  && ! reload_in_progress)
 	return arg_pointer_rtx;
+#endif
+#ifdef RETURN_ADDRESS_POINTER_REGNUM
+      if (return_address_pointer_rtx && regno == RETURN_ADDRESS_POINTER_REGNUM
+	  && mode == Pmode && ! reload_in_progress)
+	return return_address_pointer_rtx;
 #endif
       if (stack_pointer_rtx && regno == STACK_POINTER_REGNUM && mode == Pmode
 	  && ! reload_in_progress)
@@ -3391,6 +3400,11 @@ init_emit_once (line_numbers)
     arg_pointer_rtx = stack_pointer_rtx;
   else
     arg_pointer_rtx = gen_rtx (REG, Pmode, ARG_POINTER_REGNUM);
+
+#ifdef RETURN_ADDRESS_POINTER_REGNUM
+  return_address_pointer_rtx = gen_rtx (REG, Pmode,
+					RETURN_ADDRESS_POINTER_REGNUM);
+#endif
 
   /* Create the virtual registers.  Do so here since the following objects
      might reference them.  */
