a254e3f8ccd0be4af1c86f93aa85cd7dde3c6c94
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1995-12-25 03:22:10 +0000

    * pa.h (ASM_OUTPUT_FUNCTION_PREFIX): Strip any name encoding         on the section name.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@10860 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/pa/pa.h
+++ gcc/config/pa/pa.h
@@ -1015,11 +1015,15 @@ extern enum cmp_type hppa_branch_type;
 #endif
 
 #define ASM_OUTPUT_FUNCTION_PREFIX(FILE, NAME) \
-  if (!TARGET_PORTABLE_RUNTIME && TARGET_GAS && in_section == in_text) \
-    fputs ("\t.NSUBSPA $CODE$,QUAD=0,ALIGN=8,ACCESS=44,CODE_ONLY\n", FILE); \
-  else if (! TARGET_PORTABLE_RUNTIME && TARGET_GAS)			\
-    fprintf (FILE,							\
-	     "\t.SUBSPA %s\n", NAME);
+  {									\
+    char *name;								\
+    STRIP_NAME_ENCODING (name, NAME);					\
+    if (!TARGET_PORTABLE_RUNTIME && TARGET_GAS && in_section == in_text) \
+      fputs ("\t.NSUBSPA $CODE$,QUAD=0,ALIGN=8,ACCESS=44,CODE_ONLY\n", FILE); \
+    else if (! TARGET_PORTABLE_RUNTIME && TARGET_GAS)			\
+      fprintf (FILE,							\
+	       "\t.SUBSPA %s\n", name);				\
+  }
     
 #define ASM_DECLARE_FUNCTION_NAME(FILE, NAME, DECL) \
     do { tree fntype = TREE_TYPE (TREE_TYPE (DECL));			\
