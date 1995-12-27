3051365e5660fa68a8f0672f9422b7823b0c2ac3
Richard Kenner <kenner@gcc.gnu.org>
1995-12-27 15:26:58 -0500

    (struct inline_remap): New fields regno_pointer_flag and regno_pointer_align.
    From-SVN: r10876

--- gcc/integrate.h
+++ gcc/integrate.h
@@ -1,5 +1,5 @@
 /* Function integration definitions for GNU C-Compiler
-   Copyright (C) 1990 Free Software Foundation, Inc.
+   Copyright (C) 1990, 1995 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
@@ -92,6 +92,10 @@ struct inline_remap
   rtvec copy_asm_operands_vector;
   /* Likewise, this is the copied constraints vector.  */
   rtvec copy_asm_constraints_vector;
+
+  /* Indications for regs being pointers and their alignment.  */
+  char *regno_pointer_flag;
+  char *regno_pointer_align;
 
   /* The next few fields are used for subst_constants to record the SETs
      that it saw.  */
