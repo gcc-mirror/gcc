09a8970ac866148cf40c4bfd8a50da01a4b621da
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1996-01-16 01:44:08 +0000

    (ASM_SPEC): New macro.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@11005 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/m68k/netbsd.h
+++ gcc/config/m68k/netbsd.h
@@ -15,6 +15,9 @@
 #undef CPP_SPEC
 #define CPP_SPEC "%{!msoft-float:-D__HAVE_68881__ -D__HAVE_FPU__} %{posix:-D_POSIX_SOURCE}"
 
+#undef ASM_SPEC
+#define ASM_SPEC " %| %{m68030} %{m68040} %{fpic:-k} %{fPIC:-k -K}"
+
 /* Names to predefine in the preprocessor for this target machine.  */
 
 #define CPP_PREDEFINES "-Dunix -Dm68k -Dmc68000 -Dmc68020 -D__NetBSD__ -Asystem(unix) -Asystem(NetBSD) -Acpu(m68k) -Amachine(m68k)"
