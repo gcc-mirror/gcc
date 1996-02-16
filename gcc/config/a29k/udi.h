217dcd5da6f727d8c831e5af04951ea96e4baacf
Richard Kenner <kenner@gcc.gnu.org>
1996-02-16 08:36:43 -0500

    Renamed from a29k-udi.h.
    Don't include a29k.h or dbxcoff.h.
    
    From-SVN: r11282

--- gcc/config/a29k/udi.h
+++ gcc/config/a29k/udi.h
@@ -19,12 +19,6 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-#include "a29k/a29k.h"
-
-/* Support -gstabs.  */
-
-#include "dbxcoff.h"
-
 /* Support the ctors and dtors sections for g++.  */
 
 #define CTORS_SECTION_ASM_OP	"\t.use .ctors"
