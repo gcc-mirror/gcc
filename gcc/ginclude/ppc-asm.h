17ed6b0a8a9dc7a40689e0b4713a4a0d4d60fb8a
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
1996-02-26 14:32:34 +0000

    Change copyright.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@11347 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ginclude/ppc-asm.h
+++ gcc/ginclude/ppc-asm.h
@@ -1,18 +1,4 @@
-/*
- * Copyright (c) 1995 Cygnus Support
- *
- * The authors hereby grant permission to use, copy, modify, distribute,
- * and license this software and its documentation for any purpose, provided
- * that existing copyright notices are retained in all copies and that this
- * notice is included verbatim in any distributions. No written agreement,
- * license, or royalty fee is required for any of the authorized uses.
- * Modifications to this software may be copyrighted by their authors
- * and need not follow the licensing terms described here, provided that
- * the new terms are clearly indicated on the first page of each file where
- * they apply.
- */
-
-
+/* PowerPC asm definitions for GNU C.  */
 /* Under winnt, 1) gas suppports the following as names and 2) in particular
    defining "toc" breaks the FUNC_START macro as ".toc" becomes ".2" */
 
