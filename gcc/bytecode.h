d3b1294bec4c3319aedfd54d25858768e187392c
dje <dje@138bc75d-0d04-0410-961f-82ee72b054a4>
1996-03-04 20:18:45 +0000

    Update copyright.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@11418 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/bytecode.h
+++ gcc/bytecode.h
@@ -1,5 +1,5 @@
 /* Bytecode definitions for GNU C-compiler.
-   Copyright (C) 1993, 1994 Free Software Foundation, Inc.
+   Copyright (C) 1993, 1994, 1996 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
