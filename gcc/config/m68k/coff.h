eb1cb30dc4844d6cd80c2ac795075865b5dff1cf
Richard Kenner <kenner@gcc.gnu.org>
1996-03-05 18:51:38 -0500

    (ASM_OUTPUT_SECTION_NAME): New define.
    From-SVN: r11479

--- gcc/config/m68k/coff.h
+++ gcc/config/m68k/coff.h
@@ -94,6 +94,14 @@ Boston, MA 02111-1307, USA.  */
 #define ASM_OUTPUT_ALIGNED_BSS(FILE, NAME, SIZE, ALIGN) \
   asm_output_aligned_bss ((FILE), (NAME), (SIZE), (ALIGN))
 
+/* Support generic sections */
+
+#undef ASM_OUTPUT_SECTION_NAME
+#define ASM_OUTPUT_SECTION_NAME(FILE, DECL, NAME) \
+  fprintf((FILE), ".section\t%s,\"%c\"\n", (NAME), \
+	  TREE_CODE (DECL) == FUNCTION_DECL || \
+	  TREE_READONLY (DECL) ? 'x' : 'd')
+
 /* Support the ctors and dtors sections for g++.  */
 
 #define CTORS_SECTION_ASM_OP	".section\t.ctors,\"x\""
