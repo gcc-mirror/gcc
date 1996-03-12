672a6c5facfcfbab4fd4f1d826a4a76e43260c7e
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1996-03-12 22:24:57 +0000

    (__LONG_MAX__): on Alpha, use 64 bit value.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@11521 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/glimits.h
+++ gcc/glimits.h
@@ -63,7 +63,11 @@
 /* Minimum and maximum values a `signed long int' can hold.
    (Same as `int').  */
 #ifndef __LONG_MAX__
+#ifndef __alpha__
 #define __LONG_MAX__ 2147483647L
+#else
+#define __LONG_MAX__ 9223372036854775807L
+# endif /* __alpha__ */
 #endif
 #undef LONG_MIN
 #define LONG_MIN (-LONG_MAX-1)
