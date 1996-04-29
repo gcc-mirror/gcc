32806d5e19bba90efc7a6835ea44aab5a78daf5d
Doug Evans <dje@gnu.org>
1996-04-29 17:02:49 +0000

    * sparc/vxsparc.h: sparc-aout.h renamed to aout.h.
    From-SVN: r11903

--- gcc/config/sparc/vxsparc.h
+++ gcc/config/sparc/vxsparc.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler.  Vxworks SPARC version.
-   Copyright (C) 1994 Free Software Foundation, Inc.
+   Copyright (C) 1994, 1996 Free Software Foundation, Inc.
    Contributed by David Henkel-Wallace (gumby@cygnus.com)
 
 This file is part of GNU CC.
@@ -19,7 +19,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-#include "sparc/sparc-aout.h"
+#include "sparc/aout.h"
 
 /* Specify what to link with.  */
 /* VxWorks does all the library stuff itself.  */
