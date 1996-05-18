8d0ebe11bacb3bd379001cb0a44a95ad45939d9c
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1996-05-18 18:08:05 +0000

    No longer include clipper.h and svr3.h.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@12024 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/clipper/clix.h
+++ gcc/config/clipper/clix.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler.  Clipper/Clix version.
-   Copyright (C) 1988, 1993 Free Software Foundation, Inc.
+   Copyright (C) 1988, 1993, 1996 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
@@ -17,10 +17,6 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
-
-#include "clipper/clipper.h"
-
-#include "svr3.h"
 
 /* Names to predefine in the preprocessor for this target machine.  */
 
