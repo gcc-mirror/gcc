0ebfc1dc06703b62900bfc2510b91489d9932b7b
Richard Kenner <kenner@gcc.gnu.org>
1996-05-19 22:59:47 -0400

    No longer include pa-hpux.h.
    From-SVN: r12050

--- gcc/config/pa/pa-hiux.h
+++ gcc/config/pa/pa-hiux.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler, for HI-UX.
-   Copyright (C) 1993, 1995 Free Software Foundation, Inc.
+   Copyright (C) 1993, 1995, 1996 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
@@ -20,7 +20,6 @@ Boston, MA 02111-1307, USA.  */
 
 /* HIUX is just a HPUX variant.  We can simply use the HPUX configuration
    for just about everything.  */
-#include "pa/pa-hpux.h"
 
 /* Predefines are the one noteworthy difference between HPUX and HIUX.  */
 #undef CPP_PREDEFINES
