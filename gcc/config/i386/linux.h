5021dfa01eae352e7d8f9fbe36ffe65512f65060
dje <dje@138bc75d-0d04-0410-961f-82ee72b054a4>
1996-05-24 16:29:41 +0000

    * varasm.c (asm_output_bss): New argument DECL. 	Use ASM_DECLARE_OBJECT_NAME if defined. 	(asm_output_aligned_bss): Likewise. 	(assemble_variable): Pass DECL to ASM_OUTPUT{,_ALIGNED}_BSS. 	* i386/linux.h (ASM_OUTPUT_ALIGNED_BSS): Update.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@12090 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/linux.h
+++ gcc/config/i386/linux.h
@@ -196,9 +196,9 @@ Boston, MA 02111-1307, USA.  */
 #include "i386/perform.h"
 
 /* A C statement (sans semicolon) to output to the stdio stream
-   FILE the assembler definition of an uninitialized global label named
+   FILE the assembler definition of uninitialized global DECL named
    NAME whose size is SIZE bytes and alignment is ALIGN bytes.
    Try to use asm_output_aligned_bss to implement this macro.  */
 
-#define ASM_OUTPUT_ALIGNED_BSS(FILE, NAME, SIZE, ALIGN) \
-  asm_output_aligned_bss (FILE, NAME, SIZE, ALIGN)
+#define ASM_OUTPUT_ALIGNED_BSS(FILE, DECL, NAME, SIZE, ALIGN) \
+  asm_output_aligned_bss (FILE, DECL, NAME, SIZE, ALIGN)
