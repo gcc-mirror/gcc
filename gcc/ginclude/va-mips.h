c9f99b6a232ac41d3ea3270e2886bc9552d2d9a4
Ian Lance Taylor <ian@gcc.gnu.org>
1996-10-22 22:30:50 +0000

    Fix MIPS EABI when using -mips1 or -msoft-float
    From-SVN: r12998

--- gcc/ginclude/va-mips.h
+++ gcc/ginclude/va-mips.h
@@ -24,11 +24,16 @@ typedef struct {
   char *__gp_regs;
 } __gnuc_va_list;
 
-#ifdef __mips64
-#define __va_reg_size 8
-#else
-#define __va_reg_size 4
-#endif
+#else /* ! (defined (__mips_eabi) && ! defined (__mips_soft_float)) */
+
+typedef char * __gnuc_va_list;
+
+#endif /* ! (defined (__mips_eabi) && ! defined (__mips_soft_float)) */
+#endif /* not __GNUC_VA_LIST */
+
+/* If this is for internal libc use, don't define anything but
+   __gnuc_va_list.  */
+#if defined (_STDARG_H) || defined (_VARARGS_H)
 
 enum {
   __no_type_class = -1,
@@ -53,17 +58,6 @@ enum {
   __lang_type_class
 };
 
-#else /* ! (defined (__mips_eabi) && ! defined (__mips_soft_float)) */
-
-typedef char * __gnuc_va_list;
-
-#endif /* ! (defined (__mips_eabi) && ! defined (__mips_soft_float)) */
-#endif /* not __GNUC_VA_LIST */
-
-/* If this is for internal libc use, don't define anything but
-   __gnuc_va_list.  */
-#if defined (_STDARG_H) || defined (_VARARGS_H)
-
 /* In GCC version 2, we want an ellipsis at the end of the declaration
    of the argument list.  GCC version 1 can't parse it.  */
 
@@ -81,13 +75,21 @@ typedef char * __gnuc_va_list;
   (((sizeof (__TYPE) + sizeof (int) - 1) / sizeof (int)) * sizeof (int))
 #endif
 
+#ifdef __mips64
+#define __va_reg_size 8
+#else
+#define __va_reg_size 4
+#endif
+
 /* Get definitions for _MIPS_SIM_ABI64 etc.  */
 #ifdef _MIPS_SIM
 #include <sgidefs.h>
 #endif
 
 #ifdef _STDARG_H
-#if defined (__mips_eabi) && ! defined (__mips_soft_float)
+#if defined (__mips_eabi)
+#ifndef __mips_soft_float
+#ifdef __mips64
 #define va_start(__AP, __LASTARG)					\
   (__AP.__gp_regs = ((char *) __builtin_next_arg (__LASTARG)		\
 		     - (__builtin_args_info (2) < 8			\
@@ -95,7 +97,23 @@ typedef char * __gnuc_va_list;
 			: 0)),						\
    __AP.__fp_left = 8 - __builtin_args_info (3),			\
    __AP.__fp_regs = __AP.__gp_regs - __AP.__fp_left * __va_reg_size)
-#else /* ! (defined (__mips_eabi) && ! defined (__mips_soft_float)) */
+#else /* ! defined (__mips64) */
+#define va_start(__AP, __LASTARG)					\
+  (__AP.__gp_regs = ((char *) __builtin_next_arg (__LASTARG)		\
+		     - (__builtin_args_info (2) < 8			\
+			? (8 - __builtin_args_info (2)) * __va_reg_size	\
+			: 0)),						\
+   __AP.__fp_left = (8 - __builtin_args_info (3)) / 2,			\
+   __AP.__fp_regs = __AP.__gp_regs - __AP.__fp_left * 8,		\
+   __AP.__fp_regs = (char *) ((int) __AP.__fp_regs & -8))
+#endif /* ! defined (__mips64) */
+#else /* defined (__mips_soft_float) */
+#define va_start(__AP, __LASTARG)					\
+  (__AP = ((__gnuc_va_list) __builtin_next_arg (__LASTARG)		\
+	   - (__builtin_args_info (2) >= 8 ? 0				\
+	      : (8 - __builtin_args_info (2)) * __va_reg_size)))
+#endif /* defined (__mips_soft_float) */
+#else /* ! defined (__mips_eabi) */
 #define va_start(__AP, __LASTARG) \
   (__AP = (__gnuc_va_list) __builtin_next_arg (__LASTARG))
 #endif /* ! (defined (__mips_eabi) && ! defined (__mips_soft_float)) */
@@ -107,14 +125,32 @@ typedef char * __gnuc_va_list;
 #else
 #define va_dcl    int __builtin_va_alist; __va_ellipsis
 #endif
-#if defined (__mips_eabi) && ! defined (__mips_soft_float)
+#if defined (__mips_eabi)
+#ifndef __mips_soft_float
+#ifdef __mips64
 #define va_start(__AP)							\
   (__AP.__gp_regs = ((char *) __builtin_next_arg ()			\
 		     - (__builtin_args_info (2) < 8			\
 			? (8 - __builtin_args_info (2)) * __va_reg_size	\
-			: 8)),						\
+			: __va_reg_size)),				\
    __AP.__fp_left = 8 - __builtin_args_info (3),			\
    __AP.__fp_regs = __AP.__gp_regs - __AP.__fp_left * __va_reg_size)
+#else /* ! defined (__mips64) */
+#define va_start(__AP)							\
+  (__AP.__gp_regs = ((char *) __builtin_next_arg ()			\
+		     - (__builtin_args_info (2) < 8			\
+			? (8 - __builtin_args_info (2)) * __va_reg_size	\
+			: __va_reg_size)),				\
+   __AP.__fp_left = (8 - __builtin_args_info (3)) / 2,			\
+   __AP.__fp_regs = __AP.__gp_regs - __AP.__fp_left * 8,		\
+   __AP.__fp_regs = (char *) ((int) __AP.__fp_regs & -8))
+#endif /* ! defined (__mips64) */
+#else /* defined (__mips_soft_float) */
+#define va_start(__AP)							\
+  (__AP = ((__gnuc_va_list) __builtin_next_arg ()			\
+	   - (__builtin_args_info (2) >= 8 ? __va_reg_size		\
+	      : (8 - __builtin_args_info (2)) * __va_reg_size)))
+#endif /* defined (__mips_soft_float) */
 /* Need alternate code for _MIPS_SIM_ABI64.  */
 #elif defined(_MIPS_SIM) && (_MIPS_SIM == _MIPS_SIM_ABI64 || _MIPS_SIM == _MIPS_SIM_NABI32)
 #define va_start(__AP)							\
@@ -130,8 +166,9 @@ void va_end (__gnuc_va_list);		/* Defined in libgcc.a */
 #endif
 #define va_end(__AP)	((void)0)
 
-#if defined (__mips_eabi) && ! defined (__mips_soft_float)
+#if defined (__mips_eabi)
 
+#ifndef __mips_soft_float
 #ifdef __mips64
 #define __va_next_addr(__AP, __type)					\
   ((__builtin_classify_type (*(__type *) 0) == __real_type_class	\
@@ -143,11 +180,31 @@ void va_end (__gnuc_va_list);		/* Defined in libgcc.a */
   ((__builtin_classify_type (*(__type *) 0) == __real_type_class	\
     && __AP.__fp_left > 0)						\
    ? (--__AP.__fp_left, (__AP.__fp_regs += 8) - 8)			\
-   : (((__builtin_classify_type (* (__type *) 0) < record_type_class	\
+   : (((__builtin_classify_type (* (__type *) 0) < __record_type_class	\
 	&& __alignof__ (__type) > 4)					\
-       ? __AP.__gp_regs = (__AP.__gp_regs + 8 - 1) & -8),		\
-      (__AP.__gp_regs += __va_reg_size) - __va_reg_size))
+       ? __AP.__gp_regs = (char *) (((int) __AP.__gp_regs + 8 - 1) & -8) \
+       : (char *) 0),							\
+      (__builtin_classify_type (* (__type *) 0) >= __record_type_class	\
+       ? (__AP.__gp_regs += __va_reg_size) - __va_reg_size		\
+       : ((__AP.__gp_regs += __va_rounded_size (__type))		\
+	  - __va_rounded_size (__type)))))
 #endif
+#else /* defined (__mips_soft_float) */
+#ifdef __mips64
+#define __va_next_addr(__AP, __type)					\
+  ((__AP += __va_reg_size) - __va_reg_size)
+#else
+#define __va_next_addr(__AP, __type)					\
+  (((__builtin_classify_type (* (__type *) 0) < __record_type_class	\
+     && __alignof__ (__type) > 4)					\
+    ? __AP = (char *) (((int) __AP + 8 - 1) & -8)			\
+    : (char *) 0),							\
+   (__builtin_classify_type (* (__type *) 0) >= __record_type_class	\
+    ? (__AP += __va_reg_size) - __va_reg_size				\
+    : ((__AP += __va_rounded_size (__type))				\
+       - __va_rounded_size (__type))))
+#endif
+#endif /* defined (__mips_soft_float) */
 
 #ifdef __MIPSEB__
 #define va_arg(__AP, __type)						\
@@ -167,7 +224,7 @@ void va_end (__gnuc_va_list);		/* Defined in libgcc.a */
    : *(__type *) (void *) __va_next_addr (__AP, __type))
 #endif
 
-#else /* ! (defined (__mips_eabi) && ! defined (__mips_soft_float)) */
+#else /* ! defined (__mips_eabi) */
 
 /* We cast to void * and then to TYPE * because this avoids
    a warning about increasing the alignment requirement.  */
@@ -206,6 +263,6 @@ void va_end (__gnuc_va_list);		/* Defined in libgcc.a */
 					 + __va_rounded_size(__type))))[-1]
 #endif
 #endif
-#endif /* ! (defined (__mips_eabi) && ! defined (__mips_soft_float)) */
+#endif /* ! defined (__mips_eabi)  */
 
 #endif /* defined (_STDARG_H) || defined (_VARARGS_H) */
