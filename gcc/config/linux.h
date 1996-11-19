370c6b99ce73fa7dafa5ef6e3806786369367b8d
coxs <coxs@138bc75d-0d04-0410-961f-82ee72b054a4>
1996-11-19 22:25:12 +0000

    (COMMENT_BEGIN): Delete.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@13206 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/linux.h
+++ gcc/config/linux.h
@@ -34,11 +34,6 @@ Boston, MA 02111-1307, USA.  */
 #define MULTIBYTE_CHARS 1
 #endif
 
-/* There are conflicting reports about whether this system uses
-   a different assembler syntax.  wilson@cygnus.com says # is right.  */
-#undef COMMENT_BEGIN
-#define COMMENT_BEGIN "#"
-
 #undef ASM_APP_ON
 #define ASM_APP_ON "#APP\n"
 
