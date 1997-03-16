d584d9cd40e4961c673a83a94154364015f6feb6
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1997-03-16 13:41:08 +0000

    Correct include path.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@13716 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/objc/thr-pthreads.c
+++ gcc/objc/thr-pthreads.c
@@ -24,7 +24,7 @@ Boston, MA 02111-1307, USA.  */
    however invalidate any other reasons why the executable file might be
    covered by the GNU General Public License.  */
 
-#include <pthreads/pthread.h>
+#include <pthread.h>
 #include <objc/thr.h>
 #include "runtime.h"
 
