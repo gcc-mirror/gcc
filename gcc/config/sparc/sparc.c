7bdc039d3000c116e86a7de80ee1d19ee3143f7b
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
1997-04-13 13:47:11 +0000

    Clean up copyright notice.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@13867 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sparc/sparc.c
+++ gcc/config/sparc/sparc.c
@@ -1,5 +1,5 @@
 /* Subroutines for insn-output.c for Sun SPARC.
-   Copyright (C) 1987, 88, 89, 92-95, 1996, 1997 Free Software Foundation, Inc.
+   Copyright (C) 1987, 88, 89, 92-96, 1997 Free Software Foundation, Inc.
    Contributed by Michael Tiemann (tiemann@cygnus.com)
    64 bit SPARC V9 support by Michael Tiemann, Jim Wilson, and Doug Evans,
    at Cygnus Support.
