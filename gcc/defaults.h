19bce5764e35a015f8a29d889d9f6a260cdb8e7b
merrill <merrill@138bc75d-0d04-0410-961f-82ee72b054a4>
1997-07-04 18:49:39 +0000

    x
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@14390 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/defaults.h
+++ gcc/defaults.h
@@ -133,3 +133,9 @@ do { fprintf (FILE, "\t%s\t", ASM_LONG);				\
 #define SUPPORTS_WEAK 0
 #endif
 #endif
+
+/* If we have a definition of INCOMING_RETURN_ADDR_RTX, assume that
+   the rest of the DWARF 2 frame unwind support is also provided.  */
+#ifdef INCOMING_RETURN_ADDR_RTX
+#define DWARF2_UNWIND_INFO
+#endif
