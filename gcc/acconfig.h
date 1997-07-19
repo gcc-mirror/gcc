2f51182ac3209817ffcea613aa84a0a60de287ae
Doug Evans <dje@gnu.org>
1997-07-19 02:37:36 +0000

    Initial revision
    From-SVN: r14486

---
+++ gcc/acconfig.h
@@ -0,0 +1,14 @@
+
+/* Include the old config.h as config2.h to simplify the transition
+   to autoconf.  */
+#include "config2.h"
+
+/* Whether malloc must be declared even if <stdlib.h> is included.  */
+#undef NEED_DECLARATION_MALLOC
+
+/* Whether realloc must be declared even if <stdlib.h> is included.  */
+#undef NEED_DECLARATION_REALLOC
+
+/* Whether free must be declared even if <stdlib.h> is included.  */
+#undef NEED_DECLARATION_FREE
+@TOP@
