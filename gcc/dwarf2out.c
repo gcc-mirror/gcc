293a9337aa37971cb016f6c1557a01169fa4aa77
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1997-08-29 15:33:54 +0000

    * dwarfout2.c (build_abbrev_table): Use xrealloc, not xmalloc         to reallocate abbrev_die_table.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@14997 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/dwarf2out.c
+++ gcc/dwarf2out.c
@@ -3955,7 +3955,7 @@ build_abbrev_table (die)
 	{
 	  n_alloc = abbrev_die_table_allocated + ABBREV_DIE_TABLE_INCREMENT;
 	  abbrev_die_table 
-	    = (dw_die_ref *) xmalloc (abbrev_die_table,
+	    = (dw_die_ref *) xrealloc (abbrev_die_table,
 				      sizeof (dw_die_ref) * n_alloc);
 
 	  bzero ((char *) &abbrev_die_table[abbrev_die_table_allocated],
