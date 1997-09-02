cc36c19670317cc57fee167477105d8c68c05fd8
Richard Kenner <kenner@gcc.gnu.org>
1997-09-02 09:28:27 -0400

    (ROUND_TYPE_ALIGN): Don't blow up if no fields.
    From-SVN: r15032

--- gcc/config/rs6000/rs6000.h
+++ gcc/config/rs6000/rs6000.h
@@ -604,6 +604,7 @@ extern int rs6000_debug_arg;		/* debug argument handling */
   ((TREE_CODE (STRUCT) == RECORD_TYPE			\
     || TREE_CODE (STRUCT) == UNION_TYPE			\
     || TREE_CODE (STRUCT) == QUAL_UNION_TYPE)		\
+   && TYPE_FIELDS (STRUCT) != 0				\
    && DECL_MODE (TYPE_FIELDS (STRUCT)) == DFmode	\
    ? MAX (MAX ((COMPUTED), (SPECIFIED)), BIGGEST_ALIGNMENT) \
    : MAX ((COMPUTED), (SPECIFIED)))
