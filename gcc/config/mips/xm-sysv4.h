068f1cb8d7eb2a676cf8023f33b33a7569fa8d1e
Jeffrey A Law <law@cygnus.com>
1997-09-03 04:39:26 +0000

    xm-svr4.h (SYS_SIGLIST_DECLARED): Define.
            * xm-svr4.h (SYS_SIGLIST_DECLARED): Define.
            * xm-news.h (SYS_SIGLIST_DECLARED): Likewise.
            * gcc.texi: Note that if you define sys_siglist that you should
            also define SYS_SIGLIST_DECLARED.
    autoconf doesn't handle finding sys_siglist under different names.
    
    From-SVN: r15052

--- gcc/config/mips/xm-sysv4.h
+++ gcc/config/mips/xm-sysv4.h
@@ -3,3 +3,5 @@
 /* SVR4 provides no sys_siglist,
    but does offer the same data under another name.  */
 #define sys_siglist _sys_siglist
+#undef SYS_SIGLIST_DECLARED
+#define SYS_SIGLIST_DECLARED
