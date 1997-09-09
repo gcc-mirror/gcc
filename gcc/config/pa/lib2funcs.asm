c82af01654489f0da3e85b80a8920220d3496c0a
Jeff Law <law@gcc.gnu.org>
1997-09-09 17:33:35 -0600

    Add libgcc2 exception to copyright notice.
    From-SVN: r15205

--- gcc/config/pa/lib2funcs.asm
+++ gcc/config/pa/lib2funcs.asm
@@ -14,6 +14,14 @@
 ;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;  GNU General Public License for more details.
 
+; In addition to the permissions in the GNU General Public License, the
+; Free Software Foundation gives you unlimited permission to link the
+; compiled version of this file with other programs, and to distribute
+; those programs without any restriction coming from the use of this
+; file.  (The General Public License restrictions do apply in other
+; respects; for example, they cover modification of the file, and
+; distribution when not linked into another program.)
+
 ;  You should have received a copy of the GNU General Public License
 ;  along with GNU CC; see the file COPYING.  If not, write to
 ;  the Free Software Foundation, 59 Temple Place - Suite 330,
