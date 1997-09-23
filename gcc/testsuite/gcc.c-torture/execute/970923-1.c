cc8fe0a15d1e036a6e4cf82a97f787ffded76069
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1997-09-23 21:20:32 +0000

    new test (already in c-torture)
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@15685 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/970923-1.c
@@ -0,0 +1,27 @@
+int
+ts(a)
+     int a;
+{
+  if (a < 1000 && a > 2000)
+    return 1;
+  else
+    return 0;
+}
+
+int
+tu(a)
+     unsigned int a;
+{
+  if (a < 1000 && a > 2000)
+    return 1;
+  else
+    return 0;
+}
+
+
+main()
+{
+  if (ts (0) || tu (0))
+    abort ();
+  exit (0);
+}
