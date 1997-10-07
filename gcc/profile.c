496651db26666e15ffa3af1a7b86732a83645ee8
Michael Meissner <meissner@cygnus.com>
1997-10-07 16:07:46 +0000

    Declare get_file_function_name
    From-SVN: r15855

--- gcc/profile.c
+++ gcc/profile.c
@@ -1,5 +1,5 @@
 /* Calculate branch probabilities, and basic block execution counts. 
-   Copyright (C) 1990, 1991, 1992, 1993, 1994, 1996, 1997 Free Software Foundation, Inc.
+   Copyright (C) 1990, 91, 92, 93, 94, 96, 1997 Free Software Foundation, Inc.
    Contributed by James E. Wilson, UC Berkeley/Cygnus Support;
    based on some ideas from Dain Samples of UC Berkeley.
    Further mangling by Bob Manson, Cygnus Support.
@@ -53,7 +53,6 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 extern char * xmalloc ();
 extern void free ();
-extern tree get_file_function_name ();
 
 /* One of these is dynamically created whenever we identify an arc in the
    function.  */
