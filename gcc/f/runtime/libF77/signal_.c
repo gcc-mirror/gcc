0e52c1777d3095e5fb9016de8508b249aa0e3a5b
Richard Henderson <rth@cygnus.com>
1997-10-15 10:02:53 -0600

    signal_.c (G77_signal_0): Make return type sig_pf as well.
            * libF77/signal_.c (G77_signal_0): Make return type sig_pf as well.
            * libI77/fio.h: Include <string.h> if STDC_HEADERS.
            * libU77/chmod_.c: Likewise.
    
    From-SVN: r15911

--- gcc/f/runtime/libF77/signal_.c
+++ gcc/f/runtime/libF77/signal_.c
@@ -2,13 +2,13 @@
 #include "signal1.h"
 
 #ifdef KR_headers
-ftnint G77_signal_0 (sigp, proc) integer *sigp; sig_pf proc;
+sig_pf G77_signal_0 (sigp, proc) integer *sigp; sig_pf proc;
 #else
-ftnint G77_signal_0 (integer *sigp, sig_pf proc)
+sig_pf G77_signal_0 (integer *sigp, sig_pf proc)
 #endif
 {
 	int sig;
 	sig = (int)*sigp;
 
-	return (ftnint)signal(sig, proc);
+	return signal(sig, proc);
 	}
