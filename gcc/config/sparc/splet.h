202fd1d57e51defdd6c6eb14c85c8130badb787e
Doug Evans <devans@gcc.gnu.org>
1997-10-20 21:28:27 +0000

    Update copyright year.
    From-SVN: r16124

--- gcc/config/sparc/splet.h
+++ gcc/config/sparc/splet.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler, for SPARClet.
-   Copyright (C) 1996 Free Software Foundation, Inc.
+   Copyright (C) 1996, 1997 Free Software Foundation, Inc.
    Contributed by Doug Evans (dje@cygnus.com).
 
 This file is part of GNU CC.
