c2c7141abd75f723841adb15a2bfb542424be722
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1997-11-02 01:02:05 +0000

    * libF77/signal_.c: Undo last change until we can fix it right.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@16264 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/f/runtime/libF77/signal_.c
+++ gcc/f/runtime/libF77/signal_.c
@@ -2,13 +2,13 @@
 #include "signal1.h"
 
 #ifdef KR_headers
-sig_pf G77_signal_0 (sigp, proc) integer *sigp; sig_pf proc;
+ftnint G77_signal_0 (sigp, proc) integer *sigp; sig_pf proc;
 #else
-sig_pf G77_signal_0 (integer *sigp, sig_pf proc)
+ftnint G77_signal_0 (integer *sigp, sig_pf proc)
 #endif
 {
 	int sig;
 	sig = (int)*sigp;
 
-	return signal(sig, proc);
+	return (ftnint)signal(sig, proc);
 	}
