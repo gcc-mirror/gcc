f8db4bd1531f8a7f370b9dd5bf8eddca3205d372
Jeff Law <law@gcc.gnu.org>
1997-11-02 23:05:40 -0700

    *** empty log message ***
    From-SVN: r16290

---
+++ gcc/testsuite/g77.f-torture/execute/971102-1.f
@@ -0,0 +1,11 @@
+       i=3
+       j=0
+       do i=i,5
+         j = j+i
+       end do
+       do i=3,i
+         j = j+i
+       end do
+       if (i.ne.7) call abort()
+       print *, i,j
+       end
