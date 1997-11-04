492c23b2bafe52f4f6abfa3764aeedff2333a452
Paul Eggert <eggert@gnu.org>
1997-11-04 23:28:40 +0000

    (HAVE_INTTYPES_H): Undef.
    From-SVN: r16324

--- gcc/acconfig.h
+++ gcc/acconfig.h
@@ -1,3 +1,6 @@
+/* Define if you have a working <inttypes.h> header file.  */
+#undef HAVE_INTTYPES_H
+
 /* Whether malloc must be declared even if <stdlib.h> is included.  */
 #undef NEED_DECLARATION_MALLOC
 
