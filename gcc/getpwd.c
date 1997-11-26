d8f7fd1f51a958c3d78c6627f130bd2ac345e1cf
Richard Kenner <kenner@gcc.gnu.org>
1997-11-26 12:52:00 -0500

    (getpwd): Use VMS implementaion of _WIN32 unless cygwin32.
    From-SVN: r16734

--- gcc/getpwd.c
+++ gcc/getpwd.c
@@ -32,7 +32,7 @@ extern char *getcwd ();
 char *getenv ();
 char *xmalloc ();
 
-#ifndef VMS
+#if !(defined (VMS) || (defined(_WIN32) && !defined(__CYGWIN32__)))
 
 /* Get the working directory.  Use the PWD environment variable if it's
    set correctly, since this is faster and gives more uniform answers
@@ -80,7 +80,7 @@ getpwd ()
   return p;
 }
 
-#else	/* VMS */
+#else	/* VMS || _WIN32 && !__CYGWIN32__ */
 
 #ifndef MAXPATHLEN
 #define MAXPATHLEN 255
@@ -96,4 +96,4 @@ getpwd ()
   return pwd;
 }
 
-#endif	/* VMS */
+#endif	/* VMS || _WIN32 && !__CYGWIN32__ */
