ca6d8cae45369624280a6c821e4a0c0090f4a480
Jason Merrill <jason@gcc.gnu.org>
1997-12-03 15:29:19 -0500

    new
    From-SVN: r16930

---
+++ gcc/testsuite/g++.old-deja/g++.eh/new2.C
@@ -0,0 +1,44 @@
+// Test that a throw in B's constructor destroys the A and frees the memory.
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <new.h>
+
+struct A {
+  A();
+  ~A();
+};
+
+struct B {
+  B (A);
+};
+
+void foo (B*);
+
+int newed, created;
+
+main ()
+{
+  try {
+    foo (new B (A ()));
+  } catch (...) { }
+
+  return !(!newed && !created);
+}
+
+A::A() { created = 1; }
+A::~A() { created = 0; }
+B::B(A) { throw 1; }
+void foo (B*) { }
+
+void* operator new (size_t size) throw (std::bad_alloc)
+{
+  ++newed;
+  return (void *) malloc (size);
+}
+
+void operator delete (void *p) throw ()
+{
+  --newed;
+  free (p);
+}
---
+++ gcc/testsuite/g++.old-deja/g++.pt/enum2.C
@@ -0,0 +1,17 @@
+// Build don't link:
+
+struct U {
+  static int STATIC;
+};
+
+template <int* x> class FOO {
+public:
+  enum { n = 0 };
+};
+
+template <class A> class BAR {
+public:
+  enum { n = FOO<&A::STATIC>::n };
+};
+
+int n = BAR<U>::n;
