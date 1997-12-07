80aacca38904d96064087d5a5841563cec3aa872
Richard Kenner <kenner@gcc.gnu.org>
1997-12-07 18:02:58 -0500

    (__do_global_ctors): Add missing arg to __register_frame.
    From-SVN: r17000

--- gcc/crtstuff.c
+++ gcc/crtstuff.c
@@ -387,7 +387,8 @@ __do_global_ctors ()
 {
   func_ptr *p;
 #ifdef EH_FRAME_SECTION_ASM_OP
-  __register_frame (__EH_FRAME_BEGIN__);
+  static struct oobject object;
+  __register_frame (__EH_FRAME_BEGIN__, &object);
 #endif
   for (p = __CTOR_END__ - 1; *p != (func_ptr) -1; p--)
     (*p) ();
