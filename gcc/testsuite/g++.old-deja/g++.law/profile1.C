a24ebe1badf00553185c196c79f1e756e26a6a57
Jeff Law <law@gcc.gnu.org>
1997-12-16 15:45:45 -0700

    profile1.C (XFAIL): Expect it to fail on m68k-motorola-sysv and m88k-motorola-sysv3.
    	* g++.old-deja/g++.law/profile1.C (XFAIL): Expect it to fail on
    	m68k-motorola-sysv and m88k-motorola-sysv3.
    	* gcc.c-torture/execute/loop-2e.cexp: New file to indicate expected
    	compile failures on m68k-motorola-sysv and m88k-motorola-sysv3.
    	* gcc.c-torture/execute/loop-2f.cexp: Likewise.
    
    From-SVN: r17119

--- gcc/testsuite/g++.old-deja/g++.law/profile1.C
+++ gcc/testsuite/g++.old-deja/g++.law/profile1.C
@@ -1,7 +1,7 @@
 // Special g++ Options: -pg
 // GROUPS passed profiling
 // Skip if not native
-// excess errors test - XFAIL mips*-*-* alpha*-*-* i[3456]86-*-sco3.2v5*
+// excess errors test - XFAIL m68k-motorola-sysv m88k-motorola-sysv3 mips*-*-* alpha*-*-* i[3456]86-*-sco3.2v5*
 #include <stdio.h>
 main()
 {
