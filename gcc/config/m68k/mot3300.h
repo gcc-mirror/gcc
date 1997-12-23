b0b11d17b73edcb57a117a1790612fef0c0a5d76
Richard Kenner <kenner@gcc.gnu.org>
1997-12-22 19:13:56 -0500

    (ASM_BYTE_OP): Don't include '\t' in  definition.
    (ASM_OUTPUT_ASCII): Prefix ASM_BYTE_OP by one single '\t'.
    
    From-SVN: r17204

--- gcc/config/m68k/mot3300.h
+++ gcc/config/m68k/mot3300.h
@@ -351,7 +351,7 @@ dtors_section ()							\
 #undef ASM_BYTE
 #define ASM_BYTE	".byte"
 #undef ASM_BYTE_OP
-#define ASM_BYTE_OP	"\t.byte"
+#define ASM_BYTE_OP	".byte"
 #else
 #undef ASM_LONG
 #define ASM_LONG	"long"
@@ -362,7 +362,7 @@ dtors_section ()							\
 #undef ASM_BYTE
 #define ASM_BYTE	"byte"
 #undef ASM_BYTE_OP
-#define ASM_BYTE_OP	"\tbyte"
+#define ASM_BYTE_OP	"byte"
 #endif /* USE_GAS */
 
 /* The sysV68 as doesn't know about double's and float's.  */
@@ -471,7 +471,7 @@ do { long l;					\
 #undef ASM_OUTPUT_ASCII
 #define ASM_OUTPUT_ASCII(FILE,PTR,LEN) \
   do { register int sp = 0, lp = 0;				\
-    fprintf ((FILE), "%s\t", ASM_BYTE_OP);			\
+    fprintf ((FILE), "\t%s\t", ASM_BYTE_OP);			\
   loop:								\
     if ((PTR)[sp] > ' ' && ! ((PTR)[sp] & 0x80) && (PTR)[sp] != '\\')	\
       { lp += 3;						\
