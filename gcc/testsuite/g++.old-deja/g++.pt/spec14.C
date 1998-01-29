98e10ea1cf2910fe866a71687f181276fc55548a
Jason Merrill <jason@gcc.gnu.org>
1998-01-29 06:07:01 -0500

    new
    From-SVN: r17553

---
+++ gcc/testsuite/g++.old-deja/g++.pt/spec14.C
@@ -0,0 +1,9 @@
+class X
+{
+public:
+  template <typename A, typename B, typename C>
+  X() {}
+  
+  template <typename A, typename B>
+  X::X<A, void, B>() {} // ERROR - non-template type used as a template
+};
