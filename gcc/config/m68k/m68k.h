0d6ff084f27023c4a72d0436b2b6506d15deb837
Jim Wilson <wilson@cygnus.com>
1998-02-18 07:32:06 -0500

    (MACHINE_STATE_{SAVE,RESTORE} [MOTOROLA]): Add %# and %/; add : to make them into extended asms.
    From-SVN: r18076

--- gcc/config/m68k/m68k.h
+++ gcc/config/m68k/m68k.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler.  Sun 68000/68020 version.
-   Copyright (C) 1987, 88, 93, 94, 95, 96, 1997 Free Software Foundation, Inc.
+   Copyright (C) 1987, 88, 93-97, 1998 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
@@ -1078,23 +1078,23 @@ while(0)
 #if defined(__mcf5200__)
 #define MACHINE_STATE_SAVE(id)		\
     {					\
-      asm ("subl 20,sp");		\
-      asm ("movml d0/d1/a0/a1,sp@(4)");	\
-      asm ("movew cc,d0");		\
-      asm ("movml d0,sp@");		\
+      asm ("subl %#20,%/sp" : );	\
+      asm ("movml %/d0/%/d1/%/a0/%/a1,%/sp@(4)" : ); \
+      asm ("movew %/cc,%/d0" : );	\
+      asm ("movml %/d0,%/sp@" : );	\
     }
 #else /* !__mcf5200__ */
 #if defined(MACHINE_STATE_m68010_up)
 #define MACHINE_STATE_SAVE(id)		\
     {					\
-      asm ("movew cc,sp@-");		\
-      asm ("moveml d0/d1/a0/a1,sp@-");	\
+      asm ("movew %/cc,%/sp@-" : );	\
+      asm ("moveml %/d0/%/d1/%/a0/%/a1,%/sp@-" : ); \
     }
 #else /* !MACHINE_STATE_m68010_up */
 #define MACHINE_STATE_SAVE(id)		\
     {					\
-      asm ("movew sr,sp@-");		\
-      asm ("moveml d0/d1/a0/a1,sp@-");	\
+      asm ("movew %/sr,%/sp@-" : );	\
+      asm ("moveml %/d0/%/d1/%/a0/%/a1,%/sp@-" : ); \
     }
 #endif /* MACHINE_STATE_m68010_up */
 #endif /* __mcf5200__ */
@@ -1131,16 +1131,16 @@ while(0)
 #if defined(__mcf5200__)
 #define MACHINE_STATE_RESTORE(id)	\
     {					\
-      asm ("movml sp@,d0");		\
-      asm ("movew d0,cc");		\
-      asm ("movml sp@(4),d0/d1/a0/a1");	\
-      asm ("addl 20,sp");		\
+      asm ("movml %/sp@,%/d0" : );	\
+      asm ("movew %/d0,%/cc" : );	\
+      asm ("movml %/sp@(4),%/d0/%/d1/%/a0/%/a1" : ); \
+      asm ("addl %#20,%/sp" : );	\
     }
 #else /* !__mcf5200__ */
 #define MACHINE_STATE_RESTORE(id)	\
     {					\
-      asm ("moveml sp@+,d0/d1/a0/a1");	\
-      asm ("movew sp@+,cc");		\
+      asm ("moveml %/sp@+,%/d0/%/d1/%/a0/%/a1" : ); \
+      asm ("movew %/sp@+,%/cc" : );	\
     }
 #endif /* __mcf5200__ */
 #endif /* MOTOROLA */
