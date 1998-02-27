dcf347a7fb7cb3754b103174619adb2b0fc891f8
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-02-27 02:53:00 +0000

    new
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@18283 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.pt/memclass1.C
@@ -0,0 +1,13 @@
+template <class T> struct A {
+  template <class U> struct B {
+    template <class V> void f (V) { }
+    void g () { }
+  };
+};
+
+main ()
+{
+  A<int>::B<char> b;
+  b.f (42);
+  b.g ();
+}
