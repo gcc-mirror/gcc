e335c5414bcba9210401b6d6607252f36e4f0140
Alexandre Oliva <oliva@dcc.unicamp.br>
1998-03-09 22:38:00 +0000

    g++.old-deja/g++.other/friend1.C: New test.
    From-SVN: r18454

---
+++ gcc/testsuite/g++.old-deja/g++.other/friend1.C
@@ -0,0 +1,29 @@
+// Build don't link:
+// f() should be able to access B::j, as of FDIS [class.protected]/1
+
+// Subject: Re: [bug] Inheritance and friend access control broken
+// References: <199803032141.WAA09332@piano.dptmaths.ens-cachan.fr>
+// <orhg5ff544.fsf@iguacu.dcc.unicamp.br>
+// <199803041125.MAA06937@cor.dptmaths.ens-cachan.fr>
+// <orn2f6ek92.fsf@iguacu.dcc.unicamp.br> <19980304102900.46897@dgii.com>
+// From: Alexandre Oliva <oliva@dcc.unicamp.br>
+// Date: 06 Mar 1998 01:43:18 -0300
+
+
+class B {
+protected:
+    int i;
+    static int j;
+};
+
+class D : public B {
+    friend void f();
+};
+
+void f()
+{
+    ((B*)0)->i = 3; // ERROR - protected
+    ((D*)0)->i = 4;
+    B::j = 5;
+    D::j = 6;
+}
