18a86e2c99a2ab37bff8f187099db18262617d75
Richard Kenner <kenner@gcc.gnu.org>
1998-03-13 07:53:54 -0500

    Don't include xm-mips.h.
    Don't define USG.
    
    From-SVN: r18541

--- gcc/config/mips/xm-iris5.h
+++ gcc/config/mips/xm-iris5.h
@@ -1,7 +1,3 @@
-#include "mips/xm-mips.h"
-
-#define USG
-
 /* On SGI IRIX 5.3, inttypes.h clashes with sys/types.h, but the clash
    (when compiled with GCC) is a warning, so configure.in thinks it's OK
    to use it.  Work around this problem.  */
