14b5955855fad21a3ac5015af6296c03b3d43d67
Richard Kenner <kenner@gcc.gnu.org>
1998-03-14 07:29:21 -0500

    (m68k/xm-m68k.h): Don't define.
    (USE_C_ALLOCA, USG): Don't include.
    
    From-SVN: r18574

--- gcc/config/m68k/xm-plexus.h
+++ gcc/config/m68k/xm-plexus.h
@@ -1,12 +1,5 @@
 /* Host environment for 68000's running System V.  */
 
-#include "m68k/xm-m68k.h"
-
-#define USG
-#ifndef __GNUC__
-#define USE_C_ALLOCA
-#endif
-
 #ifndef _SIZE_T_
 typedef int size_t;
 #define _SIZE_T_
