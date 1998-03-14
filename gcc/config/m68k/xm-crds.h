9b80d359849c8d3caa66e291d379f05289df701d
Richard Kenner <kenner@gcc.gnu.org>
1998-03-14 07:13:47 -0500

    (m68k/xm-m68k.h): Don't include.
    (USE_C_ALLOCA, unos, USG): Don't define.
    
    From-SVN: r18571

--- gcc/config/m68k/xm-crds.h
+++ gcc/config/m68k/xm-crds.h
@@ -1,17 +1,5 @@
-#define USG
-
-#ifndef unos
-#define unos
-#endif
-
-#include "m68k/xm-m68k.h"
-
 /* Avoid conflict with C library by changing name of this symbol.  */
 #define gettime gcc_gettime
-
-#ifndef __GNUC__
-#define USE_C_ALLOCA
-#endif
 
 /* Override part of the obstack macros.  */
 
