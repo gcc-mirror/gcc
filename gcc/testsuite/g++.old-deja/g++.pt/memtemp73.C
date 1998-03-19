75d028ffdd8795c48acca596836cb0ff694b9046
Jason Merrill <jason@gcc.gnu.org>
1998-03-19 06:53:41 -0500

    new
    From-SVN: r18709

---
+++ gcc/testsuite/g++.old-deja/g++.pt/memtemp73.C
@@ -0,0 +1,18 @@
+template <class T> struct A {
+  template <class U> void f(U);
+};
+
+template <int i> struct B { };
+
+template <class T> template <class U>
+void A<T>::f (U)
+{
+  enum { foo };
+  B<foo> b;
+}
+
+int main ()
+{
+  A<char> a;
+  a.f (42);
+}
