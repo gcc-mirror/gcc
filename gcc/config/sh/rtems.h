4375fc7bf580b6ea8ab66b082bee994c684c3a2b
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-03-23 21:54:40 +0000

    * config/sh/rtems.h: Switched from ELF to COFF.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@18781 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sh/rtems.h
+++ gcc/config/sh/rtems.h
@@ -19,7 +19,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-#include "sh/elf.h"
+#include "sh/sh.h"
 
 /* Specify predefined symbols in preprocessor.  */
 
