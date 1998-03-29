5c926150eb114eff2c36a02273825b9b8a33820f
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-03-29 23:22:49 +0000

    * genemit.c (DONE): Rework so that it works in the true arm if         an if-else conditional.         (FAIL): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@18906 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/genemit.c
+++ gcc/genemit.c
@@ -749,8 +749,8 @@ from the machine description file `md'.  */\n\n");
   printf ("extern char *insn_operand_constraint[][MAX_RECOG_OPERANDS];\n\n");
   printf ("extern rtx recog_operand[];\n");
   printf ("#define operands emit_operand\n\n");
-  printf ("#define FAIL {end_sequence (); return _val;}\n");
-  printf ("#define DONE {_val = gen_sequence (); end_sequence (); return _val;}\n");
+  printf ("#define FAIL do {end_sequence (); return _val;} while (0)\n");
+  printf ("#define DONE do {_val = gen_sequence (); end_sequence (); return _val;} while (0)\n");
 
   /* Read the machine description.  */
 
