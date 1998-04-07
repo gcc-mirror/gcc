8d234bf4079e13a990b101f4bde39161d722230b
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-04-07 23:30:05 +0000

    New test from Joern.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@19037 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/loop-4.c
@@ -0,0 +1,16 @@
+int
+f()
+{
+  int j = 1;
+  long i;
+  for (i = -0x70000000L; i < 0x60000000L; i += 0x10000000L) j <<= 1;
+  return j;
+}
+
+int
+main ()
+{
+  if (f () != 8192)
+    abort ();
+  return 0;
+}
