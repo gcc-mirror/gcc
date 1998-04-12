d1f11193560a12cb37145c741e07491e902d3078
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-04-12 19:52:16 +0000

    This change is from an idea suggested by Arthur David Olson. 	* c-common.c (decl_attributes, record_function_format, 	check_format_info, init_function_format_info): 	Add support for strftime format checking. 	(enum format_type): New type. 	(record_function_format): Now static, and takes value of type 	enum format_type instead of int. 	(time_char_table): New constant. 	(struct function_format_info): format_type member renamed from is_scan. 	(check_format_info): Use `warning' rather than sprintf followed by 	`warning', to avoid mishandling `%' in warnings. 	Change `pedwarn' to `warning', since these warnings do not necessarily 	mean the program does not conform to the C Standard, as the code 	need not be executed. 	* c-tree.h (record_function_format): Remove decl; no longer extern. 	* extend.texi: Add documentation for strftime format checking.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@19151 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-tree.h
+++ gcc/c-tree.h
@@ -162,7 +162,6 @@ extern void gen_aux_info_record                 PROTO((tree, int, int, int));
 extern void declare_function_name               PROTO((void));
 extern void decl_attributes                     PROTO((tree, tree, tree));
 extern void init_function_format_info		PROTO((void));
-extern void record_function_format		PROTO((tree, tree, int, int, int));
 extern void check_function_format		PROTO((tree, tree, tree));
 /* Print an error message for invalid operands to arith operation CODE.
    NOP_EXPR is used as a special case (see truthvalue_conversion).  */
