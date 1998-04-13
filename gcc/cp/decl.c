d7ace80972153cd445ff4eac9439a7b755db2d74
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-04-13 11:13:36 +0000

    x Fri Apr 10 12:16:49 1998  Benjamin Kosnik  <bkoz@loony.cygnus.com> 	* decl.c (duplicate_decls): Don't warn for redundant decls if  	friend: let add_friend take care of it. Fixes g++/15307
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@19198 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/decl.c
+++ gcc/cp/decl.c
@@ -2786,7 +2786,9 @@ duplicate_decls (newdecl, olddecl)
       && ! DECL_ARTIFICIAL (olddecl)
       && !(new_defines_function && DECL_INITIAL (olddecl) == NULL_TREE)
       /* Don't warn about extern decl followed by (tentative) definition.  */
-      && !(DECL_EXTERNAL (olddecl) && ! DECL_EXTERNAL (newdecl)))
+      && !(DECL_EXTERNAL (olddecl) && ! DECL_EXTERNAL (newdecl))
+      /* Don't warn about friends, let add_friend take care of it. */
+      && ! DECL_FRIEND_P (newdecl))
     {
       cp_warning ("redundant redeclaration of `%D' in same scope", newdecl);
       cp_warning_at ("previous declaration of `%D'", olddecl);
