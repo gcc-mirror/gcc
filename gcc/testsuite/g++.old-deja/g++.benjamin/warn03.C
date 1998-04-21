a147412ac137e60a3e2fe2a5bea6d4af04068199
Benjamin Kosnik <bkoz@gcc.gnu.org>
1998-04-21 19:45:53 +0000

    / tests for g++/15307 and -Wredundant-decls
    From-SVN: r19365

---
+++ gcc/testsuite/g++.old-deja/g++.benjamin/warn03.C
@@ -0,0 +1,10 @@
+// 980420 bkoz 
+// from g++/15307, tests for -Wredundant-decls for decls
+// Build don't link: Special g++ Options:
+// -Wredundant-decls
+
+//shouldn't crash
+extern unsigned char *foo5[]; 
+extern unsigned char *foo5[]; 
+
+
