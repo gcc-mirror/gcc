f5710887ad5b831a63fcd08babf0a2ae171f09e4
Mark Mitchell <mmitchel@gcc.gnu.org>
1998-04-22 21:02:15 +0000

    *** empty log message ***
    From-SVN: r19379

---
+++ gcc/testsuite/g++.old-deja/g++.pt/spec17.C
@@ -0,0 +1,44 @@
+// Build don't link:
+
+template<class T>
+struct Foo { };
+
+template<class T1, class T2>
+struct BT { };
+
+template<class T1, class T2>
+struct BT< Foo<T1>, Foo<T2> > { static const int i = 1; };
+
+template<class T1, class T2>
+struct BT< T1, Foo<T2> > { static const int i = 2; };
+
+template<class T1, class T2>
+struct BT< Foo<T1>, T2 > { static const int i = 3; };
+
+template<class T1, class T2>
+int foo(Foo<T1>, Foo<T2>)
+{
+  return 1;
+}
+
+template<class T1, class T2>
+int foo(T1, Foo<T2>)
+{
+  return 2;
+}
+
+template<class T1, class T2>
+int foo(Foo<T1>, T2)
+{
+  return 3;
+}
+
+void f()
+{
+  BT< double, Foo<int> >::i;
+  BT< Foo<int>, Foo<int> >::i;
+  BT< Foo<int>, float >::i;
+  foo(1.0, Foo<int>());
+  foo(Foo<int>(), Foo<int>());
+  foo(Foo<int>(), 1.0);
+}
