5645adf0a2e0dc6ceac2c6fe1fc26e2f210ea5bb
Dave Love <fx@gcc.gnu.org>
1998-04-27 11:19:46 +0000

    Comment about fix.
    From-SVN: r19431

--- gcc/testsuite/g77.f-torture/compile/980419-1.f
+++ gcc/testsuite/g77.f-torture/compile/980419-1.f
@@ -1,3 +1,8 @@
+CCC Abort fixed by:
+CCC1998-04-21  Jim Wilson  <wilson@cygnus.com>
+CCC
+CCC        * stmt.c (check_seenlabel): When search for line number note for
+CCC        warning, handle case where there is no such note.
       logical l(10)
       integer i(10)
       goto (10,20),l
