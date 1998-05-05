9d1bcb4667cf94ae02b3b3015694a55175be9c9f
Jeff Law <law@gcc.gnu.org>
1998-05-05 16:31:25 -0600

    New test from Joern.
    From-SVN: r19552

---
+++ gcc/testsuite/gcc.c-torture/execute/980505-2.c
@@ -0,0 +1,22 @@
+typedef unsigned short Uint16;
+typedef unsigned int Uint;
+
+Uint f ()
+{
+        Uint16 token;
+        Uint count;
+        static Uint16 values[1] = {0x9300};
+
+        token = values[0];
+        count = token >> 8;
+
+        return count;
+}
+
+int
+main ()
+{
+  if (f () != 0x93)
+    abort ();
+  exit (0);
+}
