45a455c9a6e7bcd78f24bb35a741ad3b62d40065
Dave Love <fx@gcc.gnu.org>
1998-05-06 06:57:39 +0000

    Comment on fix.
    From-SVN: r19570

--- gcc/testsuite/g77.f-torture/compile/980427-0.f
+++ gcc/testsuite/g77.f-torture/compile/980427-0.f
@@ -1,4 +1,5 @@
 c ../../egcs/gcc/f/com.c:938: failed assertion `TREE_CODE (TREE_TYPE (e)) == REAL_TYPE'
+c     Fixed by 28-04-1998 global.c (ffeglobal_ref_progunit_) change.
       external b
       call y(b)
       end
