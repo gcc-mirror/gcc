e83015a9b5df840bb010174df94c170c842676cd
Richard Henderson <rth@cygnus.com>
1998-05-11 01:31:20 -0700

    alpha.h (PRINT_OPERAND_PUNCT_VALID_P): Add '`'.
            * alpha.h (PRINT_OPERAND_PUNCT_VALID_P): Add '`'.
            * alpha.c (print_operand): Handle it.
            * alpha.md (fix_truncdfsi2, fix_truncsfsi2): New patterns and
            related define_splits.  Also add peepholes for SImode reload
            plus sign_extend lossage.
    
    From-SVN: r19664

--- gcc/config/alpha/alpha.c
+++ gcc/config/alpha/alpha.c
@@ -2496,6 +2496,23 @@ print_operand (file, x, code)
 	fputs ("su", file);
       break;
 
+    case '`':
+      /* Generates trap-mode suffix for instructions that accept the
+	 v and sv suffix.  The only instruction that needs this is cvtql.  */
+      switch (alpha_fptm)
+	{
+	case ALPHA_FPTM_N:
+	  break;
+	case ALPHA_FPTM_U:
+	  fputs ("v", file);
+	  break;
+	case ALPHA_FPTM_SU:
+	case ALPHA_FPTM_SUI:
+	  fputs ("sv", file);
+	  break;
+	}
+      break;
+
     case '(':
       /* Generates trap-mode suffix for instructions that accept the
 	 v, sv, and svi suffix.  The only instruction that needs this
@@ -2541,7 +2558,8 @@ print_operand (file, x, code)
 	 suffix (cvtqt and cvtqs).  */
       switch (alpha_fptm)
 	{
-	case ALPHA_FPTM_N: case ALPHA_FPTM_U:
+	case ALPHA_FPTM_N:
+	case ALPHA_FPTM_U:
 	case ALPHA_FPTM_SU:	/* cvtqt/cvtqs can't cause underflow */
 	  break;
 	case ALPHA_FPTM_SUI:
