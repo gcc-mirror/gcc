0bdd1fc893deda06d107ce5613464ec37e1e0c59
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-05-19 15:43:24 +0000

    Add test
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@19884 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.other/overload2.C
@@ -0,0 +1,24 @@
+// Build don't run:
+
+template <class T>
+class ConstArray {
+};
+
+template <class T1, class T2>
+void operator+(const ConstArray<T1>&, const ConstArray<T2>&)
+{
+}
+
+template <class T1, class T2>
+void operator+(const ConstArray<T1>&, T2);
+
+template <class T1, class T2>
+void operator+(T1, const ConstArray<T2>&);
+
+const ConstArray<int> cai() { return ConstArray<int>(); }
+const ConstArray<double> cad() { return ConstArray<double>(); }
+
+int main()
+{
+  cai () + cad ();
+}
