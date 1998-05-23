7b6a7d3b5fa58b19cd0cf8d5321922ccc31de386
Dave Love <fx@gcc.gnu.org>
1998-05-23 09:51:54 +0000

    More commentary.
    From-SVN: r19971

--- gcc/testsuite/g77.f-torture/execute/980520-1.f
+++ gcc/testsuite/g77.f-torture/execute/980520-1.f
@@ -1,4 +1,6 @@
 c     Produced a link error through not eliminating the unused statement
-c     function.  It's in `execute' since it needs to link.
+c     function after 1998-05-15 change to gcc/toplev.c.  It's in
+c     `execute' since it needs to link.
+c     Fixed by 1998-05-23 change to f/com.c.
       values(i,j) = val((i-1)*n+j)
       end
