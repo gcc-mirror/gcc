e857289138844dba7f723df1a982904f361e8d2b
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-05-24 23:55:20 +0000

    1998-05-24  Mark Mitchell  <mark@markmitchell.com> 	* call.c (maybe_handle_implicit_object): Handle QUAL_CONVs.  Make 	sure the type of the REF_BIND is a reference type. 	(maybe_handle_ref_bind, compare_ics): Rename reference_type* to 	ref_to_type* for clarity.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@20037 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.other/overload6.C
@@ -0,0 +1,18 @@
+extern "C" void abort();
+
+struct S1
+{
+  int f() { return 0; }
+  int f() const { return 1; }
+};
+
+struct S2 : public S1
+{
+};
+
+int main()
+{
+  S2 s2;
+  if (s2.f() != 0)
+    abort ();
+}
