dbe9d4e126fcccee95c7aea8be74203c2778aaaf
fx <fx@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-05-26 14:08:56 +0000

    Add runtime test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@20063 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g77.f-torture/execute/alpha2.f
+++ gcc/testsuite/g77.f-torture/execute/alpha2.f
@@ -1,3 +1,4 @@
+c     This was originally a compile test.
       IMPLICIT REAL*8 (A-H,O-Z)
       COMMON /C/   A(9), INT
       DATA A      /
@@ -5,5 +6,14 @@
      2                 0.29996921166596490D01, 0.20016917082678680D01,
      3                 0.99126390351864390D00, 0.97963256554443300D-01,
      4                -0.87360964813570100D-02, 0.16917082678692080D-02,
-     5                -0.26013651283774820D-05                         /
+     5                7./
+C     Data values were once mis-compiled on (OSF/1 ?) Alpha with -O2
+c     such that, for instance, `7.' appeared as `4.' in the assembler
+c     output.
+      call test(a(9), 7)
       END
+      subroutine test(r, i)
+      double precision r
+      if (nint(r)/=i) call abort
+      end
+
