6fa6cd566295d4759ae83dffa626bb5a167c7b49
robertl <robertl@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-05-28 23:06:53 +0000

    g++ now recognizes the error.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@20127 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.robertl/eb67.C
+++ gcc/testsuite/g++.old-deja/g++.robertl/eb67.C
@@ -1,15 +1,6 @@
 // Build don't link: 
 /* 
-The 971114 "gcc/cp/parse.y" doesn't properly identify non-aggregate
-types used as base classes.
-
-First, the rule:
-
-        base_class: base_class_access_list see_typename base_class.1
-
-uses "IS_AGGR_TYPE" instead of "is_aggr_type" to check "base_class.1",
-so no error is reported for code like:
-
+Check whether a typedef for a basic type as a baseclass is diagnosed.
 */
-        typedef int an_int;
-        class bar : public an_int {};
+typedef int an_int;
+class bar : public an_int {}; // ERROR -
