222e22a6692442535b25dc2ad485f4df8689eeb0
robertl <robertl@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-05-28 23:04:32 +0000

    Martin says: This makes a good namespace test case :-) we should leave it.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@20126 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.robertl/eb124.C
+++ gcc/testsuite/g++.old-deja/g++.robertl/eb124.C
@@ -1,7 +1,3 @@
+//Build don't link:
 #include <stdexcept>
-#if WORK_AROUND
-typedef std::runtime_error std_runtime_error;
-class X : public std_runtime_error {};
-#else
 class X : public std::runtime_error {};
-#endif
