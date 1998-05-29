2879ca2a89e81ec127e2accb87bd3884a78d6ad4
Robert Lipe <robertl@gcc.gnu.org>
1998-05-29 08:10:44 +0000

    Pick up a few scraggling files that missed my first add/commit cycle.
    From-SVN: r20138

---
+++ gcc/testsuite/g++.old-deja/g++.robertl/ebnull.C
@@ -0,0 +1,17 @@
+class null {
+    null (null const&);
+    void operator& ();
+
+  public:
+    null () {}
+
+    template <typename T>
+    operator T* () const { return 0; }
+} const null;
+
+int main ()
+{
+    int *p = null;
+
+    return 0;
+}
