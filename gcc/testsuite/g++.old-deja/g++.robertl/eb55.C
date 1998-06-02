6edccec7bc98e47963f464b66730808967cc5d58
robertl <robertl@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-06-02 20:10:23 +0000

    Per Martin.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@20191 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.robertl/eb55.C
+++ gcc/testsuite/g++.old-deja/g++.robertl/eb55.C
@@ -1,9 +1,12 @@
 // Build don't link: 
+// Since the constructor is in streambuf.h, additional diagnostics are
+// produced, which are not really supported in the old-deja framework
+// excess errors test - XFAIL *-*-*
 #include <strstream.h>
 
 void
 t( char* buf )
 {
-        istrstream str = buf;
+  istrstream str = buf;  //ERROR - inaccessible copy constructor
 }
 
