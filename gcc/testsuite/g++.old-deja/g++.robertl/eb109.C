a7c2e816b708f6997e547535defd93f3803657f5
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-06-04 01:49:32 +0000

    update
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@20220 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.robertl/eb109.C
+++ gcc/testsuite/g++.old-deja/g++.robertl/eb109.C
@@ -39,19 +39,19 @@ template<class VertexType, class EdgeType>
 ostream& operator<<(ostream& os, Graph<VertexType,EdgeType>& G)
 {
     // display of vertices with successors
-    for(int i = 0; i < G.size(); ++i)
+  for(int i = 0; i < G.size(); ++i)  // ERROR - no size function
     {
-        os << G[i].first << " <";
+      os << G[i].first << " <";      // ERROR - no index operator
 
         // The compiler does not like this line!!!!!!
         typename Graph<VertexType, EdgeType>::Successor::iterator
-                   startN = G[i].second.begin(),
-                   endN   = G[i].second.end();
+	  startN = G[i].second.begin(), // ERROR - no index operator
+	  endN   = G[i].second.end();  // ERROR - no index operator
 
         while(startN != endN)
         {
             os << G[(*startN).first].first << ' ' // vertex
-               << (*startN).second << ' ';        // edge value
+               << (*startN).second << ' ';        // ERROR - no index operator
             ++startN;
         }
         os << ">\n";
@@ -62,7 +62,7 @@ ostream& operator<<(ostream& os, Graph<VertexType,EdgeType>& G)
 int main()
 {
     // no edge weighting, therefore type Empty:
-    Graph<string, Empty> V(true);        // directed
+    Graph<string, Empty> V(true);        // ERROR - no bool constructor
     // ReadGraph(V, "gra1.dat");
 
     // display of vertices with successors
