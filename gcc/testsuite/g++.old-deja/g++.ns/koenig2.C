19333f700395dd63c5507534e6a9b23035a82864
Jason Merrill <jason@yorick.cygnus.com>
1998-06-05 02:32:42 +0000

    x
    From-SVN: r20237

--- gcc/testsuite/g++.old-deja/g++.ns/koenig2.C
+++ gcc/testsuite/g++.old-deja/g++.ns/koenig2.C
@@ -1,9 +1,10 @@
-//Build don't link:
-//Check association of {error} for Koenig lookup
-class QString { };
+// Build don't link:
+// Check association of {error} for Koenig lookup
+// Special g++ Options:
+
+struct QString { operator void * (); };
 int foo()
 {
-	QString bar;
-	if (bar == __null ) {
-    }
+  QString bar;
+  return (bar == __null );
 }
