b3a558fd95a7a61787c731bc017f5b99c4bd1ab5
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-06-13 23:45:18 +0000

    * cp-tree.h (complete_type_or_else): Declare. 	* init.c (build_new_1, build_delete): Use it. 	* typeck.c (require_complete_type): Use complete_type, rather than 	expanding it inline. 	(complete_type_or_else): New function. 	(build_component_ref): Use it. 	(pointer_int_sum): Make sure the type pointed to is complete. 	(pointer_diff): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@20499 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.pt/pointer1.C
@@ -0,0 +1,17 @@
+// Build don't link:
+
+template <class T>
+struct S1
+{
+};
+
+template <class T>
+struct S2
+{
+  typedef T* pointer_t;
+};
+
+int f(S2<S1<int> >::pointer_t p1, S2<S1<int> >::pointer_t p2)
+{
+  return (int) (p1 - p2);
+}
