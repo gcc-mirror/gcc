77f194c29905bf0b828f3867a7d44b7368fe0228
Jeffrey A Law <law@cygnus.com>
1998-06-17 17:56:02 +0000

    * gcc.c-torture/execute/980617-1.c: New test.
    From-SVN: r20546

---
+++ gcc/testsuite/gcc.c-torture/execute/980617-1.c
@@ -0,0 +1,14 @@
+void foo (unsigned int * p)
+{
+  if ((signed char)(*p & 0xFF) == 17 || (signed char)(*p & 0xFF) == 18)
+    return;
+  else
+    abort ();
+}
+
+int main ()
+{
+  int i = 0x30011;
+  foo(&i);
+  exit (0);
+}
