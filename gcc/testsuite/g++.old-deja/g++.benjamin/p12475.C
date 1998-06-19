eb4ac092409fd515cd0009785c59eadca0813c0b
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-06-19 17:40:09 +0000

    � 64 bit targets don't overflow
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@20595 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.benjamin/p12475.C
+++ gcc/testsuite/g++.old-deja/g++.benjamin/p12475.C
@@ -1,5 +1,5 @@
 // Build don't link:
 // prms-id: 12475
+// excess errors test - XFAIL alpha*-*-* mips64*-*-*
 
-enum huh { start =-2147483648, next };		// WARNING - 
-
+enum huh { start =-2147483648, next };		// WARNING - , XFAIL sparc64-*-* alpha*-*-*
