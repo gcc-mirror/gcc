d12eff3a6ddce05b2f8adb939daae26a2ac146ba
Jeffrey A Law <law@cygnus.com>
1998-06-26 16:59:06 +0000

    * gcc.dg/980626-1.c: New test.
    From-SVN: r20742

---
+++ gcc/testsuite/gcc.dg/980626-1.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-options -Wimplicit-int } */
+
+int main()
+{
+  typedef SFtype __attribute__ ((mode (SF)));
+  typedef DFtype __attribute__ ((mode (DF)));
+  typedef HItype __attribute__ ((mode (HI)));
+  typedef SItype __attribute__ ((mode (SI)));
+  typedef DItype __attribute__ ((mode (DI)));
+  typedef UHItype __attribute__ ((mode (HI)));
+  typedef USItype __attribute__ ((mode (SI)));
+  typedef UDItype __attribute__ ((mode (DI)));
+  return 0;
+}
