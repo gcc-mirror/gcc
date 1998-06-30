87835f767120a113ba64696081bbb44c7456612d
robertl <robertl@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-06-30 08:31:49 +0000

    Changes from Alexandre and Klaus-Georg.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@20820 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.robertl/eb131.C
+++ gcc/testsuite/g++.old-deja/g++.robertl/eb131.C
@@ -1,30 +1,21 @@
 // Build don't link
-// The call to a::foo() generates an error:
-// eb131.C: In method `a::a()':
-// eb131.C:26: no matching function for call to `a::foo (void (a::*)(double))'
-// eb131.C:15: candidates are: a::foo(void (a::*)(float))
-// According to [over.over] in the CD2, &junk should resolve in this context.
 // From: Klaus-Georg Adams <Klaus-Georg.Adams@chemie.uni-karlsruhe.de> 
 // Reported against EGCS snaps 98/06/18.
 
 struct a {
-       a();
-       void junk();
-       void junk( int );
-       void junk( float );
-       void junk( double );
+	a();
+	void junk( float );
+	void junk( double );
 
-       void foo( void (a::*member)(float) );
-       void bar( void (*function)(float) );
+	void bar( double );
+	void bar( float );
+
+	void foo( void (a::*member)(float) );
 };
-
-void baz();
-void baz( int );
-void baz( float );
-void baz( double );
 
 a::a()
 {
-       foo( &junk );
-       bar( &baz );
+	foo( &junk ); // ERROR: junk is an unqualified-id.
+	foo( &bar );  // ERROR: bar is an unqualified-id.
 }
+
