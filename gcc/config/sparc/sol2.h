2cf504a1fc4d3ec625afd018cca41f9bfcf7eda6
davem <davem@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-07-03 03:19:41 +0000

    * sparc.c (sparc_operand, move_operand,	arith_operand, 	arith11_operand, arith10_operand, arith_double_operand, 	arith11_double_operand, arith10_double_operand, small_int, 	uns_small_int): Recognize CONSTANT_P_RTX. 	(output_sized_memop, output_move_with_extension, 	output_load_address, output_size_for_block_move, 	output_block_move, delay_operand): Remove, has not been 	enabled or referenced for years. 	* sparc.md (movstrsi, block_move_insn): Likewise. 	* sparc.h (PREDICATE_CODES): Define. 	* linux-aout.h (MACHINE_STATE_{SAVE,RESTORE}): Override with 	version which uses getcc/setcc traps to save/restore condition 	codes. 	* linux64.h: Likewise. 	* sunos4.h: Likewise. 	* linux.h: Likewise. 	* sol2.h: Likewise. 	* sun4o3.h: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@20917 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sparc/sol2.h
+++ gcc/config/sparc/sol2.h
@@ -210,4 +210,23 @@ Boston, MA 02111-1307, USA.  */
    sparc_override_options will disable V8+ if not generating V9 code.  */
 #undef TARGET_DEFAULT
 #define TARGET_DEFAULT (MASK_APP_REGS + MASK_EPILOGUE + MASK_FPU + MASK_V8PLUS)
+
+/* Override MACHINE_STATE_{SAVE,RESTORE} because we have special
+   traps available which can get and set the condition codes
+   reliably.  */
+#undef MACHINE_STATE_SAVE
+#define MACHINE_STATE_SAVE(ID)				\
+  unsigned long int ms_flags, ms_saveret;		\
+  asm volatile("ta	0x20\n\t"			\
+	       "mov	%%g1, %0\n\t"			\
+	       "mov	%%g2, %1\n\t"			\
+	       : "=r" (ms_flags), "=r" (ms_saveret));
+
+#undef MACHINE_STATE_RESTORE
+#define MACHINE_STATE_RESTORE(ID)			\
+  asm volatile("mov	%0, %%g1\n\t"			\
+	       "mov	%1, %%g2\n\t"			\
+	       "ta	0x21\n\t"			\
+	       : /* no outputs */			\
+	       : "r" (ms_flags), "r" (ms_saveret));
 
