3c578ce64d77e9a7a5b181764cc3e7ac1ef234c5
schwab <schwab@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-07-10 03:36:24 +0000

    * config/m68k/m68k.c (const_uint32_operand): Recognize 	CONSTANT_P_RTX. 	(const_sint32_operand): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@21049 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/m68k/m68k.c
+++ gcc/config/m68k/m68k.c
@@ -3233,6 +3233,8 @@ const_uint32_operand (op, mode)
      rtx op;
      enum machine_mode mode;
 {
+  if (GET_CODE (op) == CONSTANT_P_RTX)
+    return 1;
 #if HOST_BITS_PER_WIDE_INT > 32
   /* All allowed constants will fit a CONST_INT.  */
   return (GET_CODE (op) == CONST_INT
@@ -3252,6 +3254,8 @@ const_sint32_operand (op, mode)
      rtx op;
      enum machine_mode mode;
 {
+  if (GET_CODE (op) == CONSTANT_P_RTX)
+    return 1;
   /* All allowed constants will fit a CONST_INT.  */
   return (GET_CODE (op) == CONST_INT
 	  && (INTVAL (op) >= (-0x7fffffff - 1) && INTVAL (op) <= 0x7fffffff));
