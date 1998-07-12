3375639a487355a905ff8316c52692e04ddf23cf
Dave Love <fx@gcc.gnu.org>
1998-07-12 08:54:18 +0000

    New test.
    From-SVN: r21084

---
+++ gcc/testsuite/g77.f-torture/noncompile/980616-0.f
@@ -0,0 +1,8 @@
+* Fixed by 1998-07-11 equiv.c change.
+* ../../gcc/f/equiv.c:666: failed assertion `ffebld_op (subscript) == FFEBLD_opCONTER'
+
+* Date: Mon, 15 Jun 1998 21:54:32 -0500
+* From: Ian A Watson <WATSON_IAN_A@lilly.com>
+* Subject: Mangler Crash
+      EQUIVALENCE(I,glerf(P))
+      COMMON /foo/ glerf(3)
