2d2ad82da4fdd52b434a7202f77a63ebc330366d
Martin v. Löwis <loewis@gcc.gnu.org>
1998-07-15 14:38:06 +0000

    Add aliases in nested blocks
    From-SVN: r21197

--- gcc/testsuite/g++.old-deja/g++.ns/alias6.C
+++ gcc/testsuite/g++.old-deja/g++.ns/alias6.C
@@ -1,16 +1,35 @@
+// Check namespace aliases inside blocks
 namespace A { 
   int i;
-  void f(){}
+  void f(){
+    i = 0;
+  }
 }
 
-main ()
+int g();
+
+int main ()
 {
   namespace B = A;
   B::i=42;
   B::f();
   using namespace B;
   f();
+  // A::i is now 0, B::i is 1
+  return g();
 }
 
-namespace B {}
+namespace B {
+  int i = 1;
+}
 
+int g()
+{
+  namespace x = A;
+  if (x::i)
+  {
+    namespace x = B;
+    return x::i;
+  }
+  return x::i;
+}
