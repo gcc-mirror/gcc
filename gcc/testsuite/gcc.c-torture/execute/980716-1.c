a7b47808a0e06c9162950aa99624d3fe7c0ebb46
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-07-16 20:47:24 +0000

    * gcc.c-torture/execute/980716-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@21222 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/980716-1.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+
+void
+stub(int num, ...)
+{
+    va_list ap;
+    char *end;
+    int i;
+
+    for (i = 0; i < 2; i++) {
+        va_start(ap, num);
+        while ( 1 ) {
+            end = va_arg(ap, char *);
+            if (!end) break;
+        }
+        va_end(ap);
+    }
+}
+
+int
+main()
+{
+    stub(1, "ab", "bc", "cx", 0);
+    exit (0);
+}
+
