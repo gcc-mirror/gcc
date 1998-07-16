1ccb8f57d5258a26d493e9658edc0acae3bb545d
Nick Clifton <nickc@cygnus.com>
1998-07-16 16:45:49 -0600

    gcc.c (do_spec_1): Cope with %g/%u/%U options which do not have a suffix.
    8
    	* gcc.c (do_spec_1): Cope with %g/%u/%U options which do not have
    	a suffix.
    
    From-SVN: r21229

--- gcc/gcc.c
+++ gcc/gcc.c
@@ -3653,7 +3653,7 @@ do_spec_1 (spec, inswitch, soft_matched_part)
 			t->length = p - suffix;
 			t->suffix = save_string (suffix, p - suffix);
 			t->unique = (c != 'g');
-			temp_filename = make_temp_file (suffix);
+			temp_filename = make_temp_file (t->suffix);
 		      }
 		    temp_filename_length = strlen (temp_filename);
 		    t->filename = temp_filename;
