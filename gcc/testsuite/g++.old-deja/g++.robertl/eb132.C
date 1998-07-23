5e3d1da5d3f5c64041751797861c8184c7b91d9c
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-07-23 19:11:58 +0000

    update
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@21354 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.robertl/eb132.C
+++ gcc/testsuite/g++.old-deja/g++.robertl/eb132.C
@@ -3,10 +3,13 @@
 // Reported against EGCS snaps 98/06/28.
 // Special g++ Options: -O -Wall  -fgcse -frerun-loop-opt
 //
-// Compilation of this program with the flags g++ -Wall -O -fgcse -frerun-loop-opt
-// or -O2 produces spurious warnings in the standard header <std/bastring.h>.
+// Compilation of this program with the flags g++ -Wall -O -fgcse
+// -frerun-loop-opt or -O2 produces spurious warnings in the standard
+// header <std/bastring.h>.
 //
 // They vanish if the declaration of a::b is taken out.
+
+// excess errors test - XFAIL *-*-*
 
 #include <string>
 
