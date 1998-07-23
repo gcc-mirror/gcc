136dc16a52166cff22037db7282e7c5e4093450f
Jason Merrill <jason@gcc.gnu.org>
1998-07-23 15:11:58 -0400

    update
    From-SVN: r21354

--- gcc/testsuite/g++.old-deja/g++.robertl/eb56.C
+++ gcc/testsuite/g++.old-deja/g++.robertl/eb56.C
@@ -1,9 +1,9 @@
-
 // Error: Internal compiler error on 1998/05/28 snapshot.
+// Build don't link:
 
-        class foo {
-                typedef int sometype;
-        };
+class foo {
+        typedef int sometype;
+};
 
-        struct die : public foo::sometype {
-        };
+struct die : public foo::sometype { // ERROR - invalid base type
+};
