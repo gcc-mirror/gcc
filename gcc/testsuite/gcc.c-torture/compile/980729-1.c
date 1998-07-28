57c478d61b12695ee7aae93f760e2a6dd02a8db8
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-07-28 23:18:04 +0000

    * gcc.c-torture/compile/980729-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@21463 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/980729-1.c
@@ -0,0 +1,14 @@
+static int
+regex_compile ()
+{
+  int  c, c1;
+  char str[6  + 1];
+  c1 = 0;
+  for (;;)
+    {
+      do { } while (0) ;
+      if (c1 == 6 )
+        break;
+      str[c1++] = c;
+    }
+}  
