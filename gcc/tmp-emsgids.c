d70283ce50dc333f6b2e8650e2f85981d8c34a2f
Jeff Law <law@gcc.gnu.org>
1998-08-02 00:46:43 -0600

    Initial revision
    From-SVN: r21536

---
+++ gcc/tmp-emsgids.c
@@ -0,0 +1,96 @@
+#line 59 "config/arc/arc.h"
+_("may not use both -EB and -EL")
+#line 176 "config/arm/arm.h"
+_("-mapcs-26 and -mapcs-32 may not be used together")
+#line 187 "config/arm/arm.h"
+_("-msoft-float and -mhard_float may not be used together")
+#line 198 "config/arm/arm.h"
+_("-mbig-endian and -mlittle-endian may not be used together")
+#line 72 "config/arm/riscix.h"
+_("-mbsd and -pedantic incompatible")
+#line 73 "config/arm/riscix.h"
+_("-mbsd and -mxopen incompatible")
+#line 74 "config/arm/riscix.h"
+_("-mxopen and -pedantic incompatible")
+#line 133 "config/arm/riscix.h"
+_("-mbsd and -pedantic incompatible")
+#line 134 "config/arm/riscix.h"
+_("-mbsd and -mxopen incompatible")
+#line 135 "config/arm/riscix.h"
+_("-mxopen and -pedantic incompatible")
+#line 126 "config/dsp16xx/dsp16xx.h"
+_("A -ifile option requires a -map option")
+#line 135 "config/dsp16xx/dsp16xx.h"
+_("A -ifile option requires a -map option")
+#line 809 "config/i386/sco5.h"
+_("-static not valid with -mcoff")
+#line 810 "config/i386/sco5.h"
+_("-shared not valid with -mcoff")
+#line 811 "config/i386/sco5.h"
+_("-symbolic not valid with -mcoff")
+#line 847 "config/i386/sco5.h"
+_("-fpic is not valid with -mcoff")
+#line 848 "config/i386/sco5.h"
+_("-fPIC is not valid with -mcoff")
+#line 882 "config/i386/sco5.h"
+_("-static not valid with -mcoff")
+#line 883 "config/i386/sco5.h"
+_("-shared not valid with -mcoff")
+#line 884 "config/i386/sco5.h"
+_("-symbolic not valid with -mcoff")
+#line 885 "config/i386/sco5.h"
+_("-fpic not valid with -mcoff")
+#line 886 "config/i386/sco5.h"
+_("-fPIC not valid with -mcoff")
+#line 305 "config/i860/fx2800.h"
+_("-p option not supported: use -pg instead")
+#line 674 "config/mips/mips.h"
+_("-pipe is not supported.")
+#line 811 "config/mips/mips.h"
+_("may not use both -mfp64 and -msingle-float")
+#line 812 "config/mips/mips.h"
+_("may not use both -mfp64 and -m4650")
+#line 815 "config/mips/mips.h"
+_("may not use both -EB and -EL")
+#line 27 "config/mips/r3900.h"
+_("-mhard-float not supported.")
+#line 29 "config/mips/r3900.h"
+_("-msingle-float and -msoft-float can not both be specified.")
+#line 41 "config/mips/r3900.h"
+_("-mhard-float not supported.")
+#line 43 "config/mips/r3900.h"
+_("-msingle-float and -msoft-float can not both be specified.")
+#line 166 "config/nextstep.h"
+_("-p profiling is no longer supported.  Use -pg instead.")
+#line 169 "config/nextstep.h"
+_("-p profiling is no longer supported.  Use -pg instead.")
+#line 36 "config/vax/vax.h"
+_("profiling not supported with -mg\n")
+#line 37 "config/vax/vax.h"
+_("profiling not supported with -mg\n")
+#line 32 "cp/lang-specs.h"
+_("GNU C++ does not support -C without using -E")
+#line 45 "cp/lang-specs.h"
+_("-pg and -fomit-frame-pointer are incompatible")
+#line 56 "cp/lang-specs.h"
+_("-pg and -fomit-frame-pointer are incompatible")
+#line 603 "gcc.c"
+_("GNU C does not support -C without using -E")
+#line 617 "gcc.c"
+_("-pg and -fomit-frame-pointer are incompatible")
+#line 624 "gcc.c"
+_("GNU C does not support -C without using -E")
+#line 633 "gcc.c"
+_("-E required when input is from standard input")
+#line 637 "gcc.c"
+_("GNU C does not support -C without using -E")
+#line 652 "gcc.c"
+_("-pg and -fomit-frame-pointer are incompatible")
+#line 659 "gcc.c"
+_("Compilation of header file requested")
+#line 661 "gcc.c"
+_("GNU C does not support -C without using -E")
+#line 676 "gcc.c"
+_("-pg and -fomit-frame-pointer are incompatible")
+#line 689 "gcc.c"
+_("GNU C does not support -C without using -E")
