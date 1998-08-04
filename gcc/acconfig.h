c64d8c0ae5a88fbb5f8005aac88266235c5c4946
Paul Eggert <eggert@gnu.org>
1998-08-04 04:45:35 +0000

    (ENABLE_NLS, HAVE_CATGETS, HAVE_GETTEXT,
    HAVE_LC_MESSAGES, HAVE_STPCPY, PACKAGE, VERSION): New macros.
    
    From-SVN: r21557

--- gcc/acconfig.h
+++ gcc/acconfig.h
@@ -1,5 +1,23 @@
+/* Define if you want expensive run-time checks. */
+#undef ENABLE_CHECKING
+
+/* Define to 1 if NLS is requested.  */
+#undef ENABLE_NLS
+
+/* Define as 1 if you have catgets and don't want to use GNU gettext.  */
+#undef HAVE_CATGETS
+
+/* Define as 1 if you have gettext and don't want to use GNU gettext.  */
+#undef HAVE_GETTEXT
+
 /* Define if you have a working <inttypes.h> header file.  */
 #undef HAVE_INTTYPES_H
+
+/* Define if your locale.h file contains LC_MESSAGES.  */
+#undef HAVE_LC_MESSAGES
+
+/* Define as 1 if you have the stpcpy function.  */
+#undef HAVE_STPCPY
 
 /* Whether malloc must be declared even if <stdlib.h> is included.  */
 #undef NEED_DECLARATION_MALLOC
@@ -25,6 +43,9 @@
 /* Whether sbrk must be declared even if <stdlib.h> is included.  */
 #undef NEED_DECLARATION_SBRK
 
-/* Define if you want expensive run-time checks. */
-#undef ENABLE_CHECKING
+/* Define to the name of the distribution.  */
+#undef PACKAGE
+
+/* Define to the version of the distribution.  */
+#undef VERSION
 @TOP@
