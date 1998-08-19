e76be4654d954a5e36b1d58cb0e1cb1e7e2f106c
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-08-19 16:31:30 +0000

    � 	From Nick Clifton         * execute/ieee/930529-1.c (main): Check to see if __thumb__ is         defined, and test for ARM style doubles if so.         * execute/comp-goto-1.c (main): Make main() return an int.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@21860 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/ieee/930529-1.c
+++ gcc/testsuite/gcc.c-torture/execute/ieee/930529-1.c
@@ -15,7 +15,7 @@ main ()
       if (d.c[7] == 0x92 && d.c[6] == 0x24 && d.c[5] == 0x49 && d.c[4] == 0x92
 	  && d.c[3] == 0x24 && d.c[2] == 0x49 && d.c[1] == 0xc2 && d.c[0] == 0x3f)
 	exit (0);
-#ifdef __arm__
+#if defined __arm__ || defined __thumb__
       if (d.c[4] == 0x92 && d.c[5] == 0x24 && d.c[6] == 0x49 && d.c[7] == 0x92
 	  && d.c[0] == 0x24 && d.c[1] == 0x49 && d.c[2] == 0xc2 && d.c[3] == 0x3f)
 	exit (0);
