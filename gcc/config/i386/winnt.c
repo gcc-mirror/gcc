9a9b13226d8e9896fb32b3cc589098fc302da814
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-08-20 11:00:20 +0000

    Include system.h, not stdio.h
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@21874 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/winnt.c
+++ gcc/config/i386/winnt.c
@@ -20,7 +20,7 @@ the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
 #include "config.h"
-#include <stdio.h>
+#include "system.h"
 #include "rtl.h"
 #include "regs.h"
 #include "hard-reg-set.h"
