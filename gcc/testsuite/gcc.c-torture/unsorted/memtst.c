13e76d70e5b97df3bf19a8f44c29580d7a57e95b
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-08-21 23:30:36 +0000

    * gcc.c-torture/unsorted/memtst.c: Reduce size of array when         STACK_SIZE is defined.         * gcc.c-torture/unsorted/stuct.c: Similarly. Makes targets with 16bit address spaces happy.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@21900 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/unsorted/memtst.c
+++ gcc/testsuite/gcc.c-torture/unsorted/memtst.c
@@ -1,3 +1,9 @@
+#ifdef STACK_SIZE
+#define SIZE STACK_SIZE / 8
+#else
+#define SIZE 65536
+#endif
+
 memtst (int *p, int a)
 {
   do
@@ -10,11 +16,11 @@ memtst (int *p, int a)
 
 main ()
 {
-  int a[65536];
+  int a[SIZE];
   int i;
-  bzero (a, 65536 * 4);
+  bzero (a, SIZE * 4);
   for (i = 0;  i < 100;  i++)
     {
-      memtst (a, 65536);
+      memtst (a, SIZE);
     }
 }
