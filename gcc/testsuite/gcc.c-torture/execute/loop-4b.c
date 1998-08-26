ef75736785c9ea83d2f9b01a03aeef0b45501e72
J"orn Rennecke <amylaar@cygnus.co.uk>
1998-08-26 08:19:18 +0000

    Document overflow bug in check_dbra_loop:
    	* execute/loop-4b.c: New test.
    
    From-SVN: r21999

---
+++ gcc/testsuite/gcc.c-torture/execute/loop-4b.c
@@ -0,0 +1,21 @@
+int
+f()
+{
+  int j = 1;
+  long i;
+  i = 0x60000000L;
+  do
+    {
+      j <<= 1;
+      i += 0x10000000L;
+    } while (i < -0x60000000L);
+  return j;
+}
+
+int
+main ()
+{
+  if (f () != 2)
+    abort ();
+  exit (0);
+}
