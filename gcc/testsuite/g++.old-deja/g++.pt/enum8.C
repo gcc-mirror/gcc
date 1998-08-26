dbfe21241f2cd4a39b35ef393710166ad3c4b42b
Mark Mitchell <mark@markmitchell.com>
1998-08-26 13:55:47 +0000

    cp-tree.h (finish_enum): Change prototype.
    	* cp-tree.h (finish_enum): Change prototype.
    	* decl.c (finish_enum): Use TYPE_VALUES, rather than taking a
    	VALUES parameter.  Don't try to compute mins/maxs if
    	processing_template_decl.
    	* parse.y (structsp): Use new calling sequence for finish_enum.
    	* pt.c (tsubst_enum): Likewise.  Take the new type as input.
    	(lookup_template_class): Remove unused variables.  Tweak.
    	Register enums on instantiation list before substituting
    	enumeration constants.
    	(tsubst_decl): Remove unused variables.
    	(regenerate_decl_from_template): Likewise.
    	* decl.c (duplicate_decls): Don't obliterate the
    	DECL_TEMPLATE_INFO for a template if we're not replacing it with
    	anything.
    
    From-SVN: r22004

---
+++ gcc/testsuite/g++.old-deja/g++.pt/enum8.C
@@ -0,0 +1,18 @@
+// Build don't link:
+
+template <int I>
+void f();
+
+template <>
+void f<4>() {}
+
+template <class T>
+struct S
+{
+  enum E { a = 1, b = a + 3 };
+};
+
+int main()
+{
+  f<S<int>::b>();
+}
