43ccffb6fd159b6ec48fdaa7f280a84450c0f2b3
bothner <bothner@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-08-27 20:51:39 +0000

    Migrate from devo/gcc/ch.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22037 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/ch/runtime/gettextrecord.c
@@ -0,0 +1,30 @@
+/* Implement Input/Output runtime actions for CHILL.
+   Copyright (C) 1992,1993 Free Software Foundation, Inc.
+   Author: Wilfried Moser, et al
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#include "fileio.h"
+
+VarString*
+__gettextrecord( Text_Mode* the_text, char* file, int line )
+{
+  if( !the_text )
+    CHILLEXCEPTION( file, line, EMPTY, NULL_TEXT );
+
+  return the_text->text_record;
+}
