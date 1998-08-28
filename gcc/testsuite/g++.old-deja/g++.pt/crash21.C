a09125d5756c70e86bb53295a152c91873459c86
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-08-28 18:11:36 +0000

    New test
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22067 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.pt/crash21.C
@@ -0,0 +1,27 @@
+// Build don't link:
+// Special g++ Options:
+// crash test - XFAIL
+
+class Pooled
+{
+};
+
+class RefCounted
+{
+};
+
+class BrickExpressionBase : public RefCounted, public Pooled
+{
+};
+
+template<unsigned Dim, class LHS, class RHS, class OP>
+class BrickExpression : public BrickExpressionBase
+{
+};
+
+template <unsigned Dim, class T>
+void f()
+{
+  typedef BrickExpression<Dim, T, T, T> ExprT;
+  ExprT(3).apply;
+}
