91d2afc3b4d0a8b3858225a7a3249639cf4d025f
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-08-30 21:18:48 +0000

    # 	* Makefile.in (INTERFACE): New, set to 2. 	(ARLIB): Set to libstdc++.a.$(VERSION) 	(ARLINK, MARLINK): New macros. 	(LIBS): Add $(ARLINK). 	($(ARLINK), marlink): New targets. 	(install): Don't check $(libsubdir). Handle versioned libraries. 	* config/linux.ml (ARLIB, MARLINK, SHLIB, MSHLINK): New macros. 	(LIBS): Add marlink $(ARLINK). 	* config/aix.ml (ARLIB): Set to libstdc++-ar.a.$(VERSION) 	(ARLINK): New macros. 	(BUILD_LIBS): Add $(ARLINK). 	* config/dec-osf.ml (LIBS): Add $(ARLINK). 	* config/elf.ml (LIBS): Likewise. 	* config/elfshlibm.ml (LIBS): Likewise. 	* config/hpux.ml (LIBS): Likewise. 	* config/iris5.ml (LIBS): Likewise. 	* config/sol2shm.ml (LIBS): Likewise. 	* config/sunos4.ml (LIBS): Likewise. 	* configure.in: Use ${topsrcdir}/config.if to put 	LIBSTDCXX_INTERFACE, CXX_INTERFACE, LIBC_INTERFACE in 	${package_makefile_frag}.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22117 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++/config/elf.ml
+++ libstdc++/config/elf.ml
@@ -1,7 +1,7 @@
 # Elf without shared libm -- we have to link with the archive library, even
 # for programs that don't use complex.
 
-LIBS    = $(ARLIB) $(SHLIB) $(SHLINK)
+LIBS    = $(ARLIB) $(ARLINK) $(SHLIB) $(SHLINK)
 SHFLAGS = -h $(SHLIB)
 DEPLIBS = ../$(SHLIB)
 LDLIBS  = -L.. -lstdc++ -lm
--- libstdc++/config/sol2shm.ml
+++ libstdc++/config/sol2shm.ml
@@ -1,6 +1,6 @@
 # Solaris2 with shared libm, so we can link it into the shared libstdc++.
 
-LIBS    = $(ARLIB) $(SHLIB) $(SHLINK)
+LIBS    = $(ARLIB) $(ARLINK) $(SHLIB) $(SHLINK)
 SHFLAGS = -h $(SHLIB)
 SHDEPS  = -lm
 DEPLIBS = ../$(SHLIB)
