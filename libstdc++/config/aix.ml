a3c665a4a9ea70bbed2f0cf74a5bf8248a521922
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-08-30 21:18:44 +0000

    e 	* Makefile.in (INTERFACE): New, set to 2. 	(ARLIB): Set to libstdc++.a.$(VERSION) 	(ARLINK, MARLINK): New macros. 	(LIBS): Add $(ARLINK). 	($(ARLINK), marlink): New targets. 	(install): Don't check $(libsubdir). Handle versioned libraries. 	* config/linux.ml (ARLIB, MARLINK, SHLIB, MSHLINK): New macros. 	(LIBS): Add marlink $(ARLINK). 	* config/aix.ml (ARLIB): Set to libstdc++-ar.a.$(VERSION) 	(ARLINK): New macros. 	(BUILD_LIBS): Add $(ARLINK). 	* config/dec-osf.ml (LIBS): Add $(ARLINK). 	* config/elf.ml (LIBS): Likewise. 	* config/elfshlibm.ml (LIBS): Likewise. 	* config/hpux.ml (LIBS): Likewise. 	* config/iris5.ml (LIBS): Likewise. 	* config/sol2shm.ml (LIBS): Likewise. 	* config/sunos4.ml (LIBS): Likewise. 	* configure.in: Use ${topsrcdir}/config.if to put 	LIBSTDCXX_INTERFACE, CXX_INTERFACE, LIBC_INTERFACE in 	${package_makefile_frag}.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22116 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++/config/aix.ml
+++ libstdc++/config/aix.ml
@@ -1,8 +1,9 @@
 # AIX has wierd shared/non-shared libraries.
 
-ARLIB	 = libstdc++-ar.a 
+ARLIB	 = libstdc++-ar.a.$(VERSION)
+ARLINK	 = libstdc++-ar.a
 SHLINK   = libstdc++.a
-LIBS     = $(ARLIB) $(SHLIB) $(SHLINK)
+LIBS     = $(ARLIB) $(ARLINK) $(SHLIB) $(SHLINK)
 DEPLIBS  = ../$(SHLIB)
 SHDEPS   = -lm
 SHFLAGS  = -Wl,-unix
