25957c46d49f4985d042dc762e7a3cd5c8f453d9
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-09-01 10:03:49 +0000

    Fix typo
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22157 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.pt/crash24.C
+++ gcc/testsuite/g++.old-deja/g++.pt/crash24.C
@@ -1,11 +1,11 @@
 // Build don't link:
 
 template<typename T, template <class> class U> void template_fn (T);
-template<typename T, typename U> void callme ( void (*)(T));
+template<typename T> void callme ( void (*)(T));
 
 template<typename T> struct S1;
 
 int main()
 {
-  callme( template_fn<double, S1>); // ERROR - no matching function
+  callme(&template_fn<double, S1>);
 }
