027a08dffa5dfff93534fb7d5c9029a59bd22567
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-09-02 11:42:40 +0000

    � typo
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22170 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.benjamin/warn03.C
+++ gcc/testsuite/g++.old-deja/g++.benjamin/warn03.C
@@ -1,7 +1,7 @@
 // 980420 bkoz 
 // from g++/15307, tests for -Wredundant-decls for decls
-// Build don't link: Special g++ Options:
-// -Wredundant-decls
+// Build don't link: 
+// Special g++ Options: -Wredundant-decls
 
 //shouldn't crash
 extern unsigned char *foo5[]; 
