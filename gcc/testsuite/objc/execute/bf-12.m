ff79b0cf7660dfd068a1b0a08a865c881d55259d
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-09-02 23:43:53 +0000

    * lib/{objc.exp,objc-torture.exp}: New files for objc testing harness.         * objc: ObjC testsuite.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22198 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/objc/execute/bf-12.m
@@ -0,0 +1,23 @@
+#include <objc/objc.h>
+#include <objc/objc-api.h>
+#include <objc/Object.h>
+
+@interface MyObject
+{
+  Class isa;
+  float f;
+  char a[3];
+  int i:2;
+  int j:6;
+  int s;
+  int k:12;
+  char d;
+  void *pointer;
+}
+@end
+
+@implementation MyObject
+@end
+
+#include "bf-common.h"
+
