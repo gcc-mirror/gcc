c9f81ec25b3f1914d082e12d117efda4d76c130b
Alexandre Oliva <oliva@dcc.unicamp.br>
1998-09-15 17:05:28 +0000

    static2.C: invocation of static data member of type pointer-to-function denoted as...
    	* g++.old-deja/g++.other/static2.C: invocation of static data
     	member of type pointer-to-function denoted as non-static member
    
    From-SVN: r22434

---
+++ gcc/testsuite/g++.old-deja/g++.other/static2.C
@@ -0,0 +1,10 @@
+// Build don't link:
+// Based on a test case by Koos Vriezen <koos@polder.ubc.kun.nl>
+
+struct foo {
+    static void (*mystatic) ();
+};
+
+void bar(foo& t) {
+    t.mystatic ();
+}
