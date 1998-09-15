4f0523b7c746fb551eaee80845689e6a4dfff32e
oliva <oliva@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-09-15 14:36:22 +0000

    * g++.old-deja/g++.other/typedef5.C: add some more tests involving 	checks involving function types and aliases
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22430 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.other/typedef5.C
+++ gcc/testsuite/g++.old-deja/g++.other/typedef5.C
@@ -4,5 +4,13 @@
 
 typedef int t;
 typedef t* u;
+typedef u v;
+typedef v* (*w)(t);
 typedef int t;
 typedef t* u;
+typedef u v;
+typedef v* (*w)(t const); // this is ok
+typedef v* (*w)(t); // ERROR - covers message `previously declared here'
+typedef v* (*const w)(t); // ERROR - invalid redeclaration
+typedef v const* (*w)(t); // ERROR - invalid redeclaration
+typedef v* const (*w)(t); // ERROR - invalid redeclaration
