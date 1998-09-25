5be0affa1275a3a5971be7dde1cdcfee26adb6a2
loewis <loewis@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-09-25 11:17:12 +0000

    New test case.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22590 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.ns/template6.C
@@ -0,0 +1,24 @@
+//Build don't link:
+//reported by Theodore Papadopoulo (Theodore.Papadopoulo@sophia.inria.fr)
+
+namespace A {
+
+   namespace B {
+
+      template <class T1,class T2>
+         class B {
+               static const unsigned count = 0;
+               template <class ForwardIter>
+               void SetError(ForwardIter it,const T1& p1,const T2& p2) const { }
+         };
+
+      template <>
+         const unsigned B<int,int>::count = 2;
+   }
+};
+
+int
+main()
+{
+   unsigned kk =  A::B<int,int>::count; // ERROR - not a template: syntax error
+}
