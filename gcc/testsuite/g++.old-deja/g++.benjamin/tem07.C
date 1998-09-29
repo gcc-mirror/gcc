92d217b6587641fd02d7d92186d7a47aca0cb96b
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-09-29 16:13:35 +0000

    � new test
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22642 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.benjamin/tem07.C
@@ -0,0 +1,37 @@
+
+template <class T>
+class Foo
+{
+public:
+    Foo(const T&);
+    Foo(const T&, const T&);
+};
+
+template <class T>
+Foo<T>::Foo(const T& t0)
+{
+}
+
+template <class T>
+Foo<T>::Foo(const T& t0, const T& t1)
+{
+}
+
+template Foo<int>::Foo(const int& t0);
+
+
+int main (void) {
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
