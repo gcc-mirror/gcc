e173ff3c65df83e9102dc6a975dfe5fbfbe0e308
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-09-30 10:10:02 +0000

    * function.c (gen_mem_addressof): If the address REG is 	REG_USERVAR_P make the new REG be so also. 	* loop.c (scan_loop): Apply DeMorgan's laws and add documentation 	in an attempt to clarify slightly.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22667 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/980929-1.c
@@ -0,0 +1,21 @@
+void f(int i)
+{
+  if (i != 1000)
+    abort ();
+}
+
+
+int main()
+{
+  int n=1000;
+  int i;
+
+  f(n);
+  for(i=0; i<1; ++i) {
+    f(n);
+    n=666;
+    &n;
+  }
+
+  exit (0);
+}
