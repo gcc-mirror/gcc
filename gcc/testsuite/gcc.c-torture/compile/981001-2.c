fcdd68890f6ea78cd21e10f09ca499acf9de4059
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-10-01 18:06:54 +0000

    * gcc.c-torture/compile/981001-2.c: New test.         * gcc.c-torture/compile/981001-3.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22723 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/981001-2.c
@@ -0,0 +1,12 @@
+#define weak_alias(func, aliasname) \
+	extern __typeof (func) aliasname __attribute__ ((weak, alias (#func)));
+
+#define add3(d, m, c) ((d) + (m) + (c))
+
+int
+__add3(int d, int m, int c)
+{
+  return d + m + c;
+}
+
+weak_alias (__add3, add3)
