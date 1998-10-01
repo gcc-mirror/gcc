4102f62733e77afa2885292beb43a5738241c5ed
Robert Lipe <robertl@dgii.com>
1998-10-01 21:35:22 +0000

    Makefile.in (INCLUDES): Reference gcc via $MULTIBUILDTOP.
            * Makefile.in (INCLUDES): Reference gcc via $MULTIBUILDTOP.
            (FLAGS_TO_PASS): Added.
            (runtime-info.h): Reference cc1ibj via $MULTIBUILDTOP.
            * archive.c: Change config.h to tconfig.h.
            * configure.in: Find gcc's object directory even for multilibs.
    
    Co-Authored-By: Jeffrey A Law <law@cygnus.com>
    
    From-SVN: r22725

--- libobjc/archive.c
+++ libobjc/archive.c
@@ -24,7 +24,7 @@ Boston, MA 02111-1307, USA.  */
    however invalidate any other reasons why the executable file might be
    covered by the GNU General Public License.  */
 
-#include "config.h"
+#include "tconfig.h"
 #include "runtime.h"
 #include "typedstream.h"
 #include "encoding.h"
