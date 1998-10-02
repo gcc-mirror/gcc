0de03383e7e72cb15d8cdc095996e7bb6912347c
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-10-02 01:21:18 +0000

    * gcc.c-torture/compile/981001-4.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22739 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/981001-4.c
@@ -0,0 +1,14 @@
+#define P(a,b) P1(a,b)
+#define P1(a,b) a##b
+
+#define ONCE(x, y) (x ?: x = y())
+#define PREFIX
+
+extern int P(PREFIX, init) (void);
+
+int
+fun(void)
+{
+  static int memo;
+  return ONCE(memo, P(PREFIX, init));
+}
