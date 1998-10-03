c6aaeb648d0bf8f7731f330e36055c25c51286f9
oliva <oliva@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-10-03 10:55:59 +0000

    mark as crash failure
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22773 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.other/decl2.C
+++ gcc/testsuite/g++.old-deja/g++.other/decl2.C
@@ -1,7 +1,9 @@
 // Build don't link:
 // Based on a test-case by Maciej Radziejewski <maciejr@iws.uni-stuttgart.de>
 
-int i(0)(1); // ERROR - multiple initialization - XCRASH *-*-*
+// crash test - XFAIL *-*-*
+
+int i(0)(1); // ERROR - multiple initialization
 int j(2) = 3; // ERROR - multiple initialization
 int k(4)(5)(6); // ERROR - multiple initialization
 int m, n(7)(8); // ERROR - multiple initialization
