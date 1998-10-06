bef6a7a43b4016bb121ace79c4ae5a4138b8c753
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-10-06 12:38:05 +0000

    * call.c (resolve_args): Resolve template specializations, if 	possible.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22867 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.pt/overload4.C
+++ gcc/testsuite/g++.old-deja/g++.pt/overload4.C
@@ -1,14 +1,12 @@
 // Build don't link:
 
-// crash test - XFAIL *-*-*
-
 template <class T> void foo(T);
 
 template <class T> void bar(void (*)(T), T);
 
 void baz() {
   bar<int>(foo, 1);
-  bar(foo<int>, 1); // explicit args for foo don't help
-  bar<int>(foo<int>, 1); // not even here
-  bar(foo, 1);
+  bar(foo<int>, 1);
+  bar<int>(foo<int>, 1);
+  bar(foo, 1); 
 }
