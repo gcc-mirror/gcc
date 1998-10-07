9e1352519ce635d93e1d684a0370e86fc1dc77e8
oliva <oliva@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-10-07 20:59:13 +0000

    * g++.old-deja/g++.ns/extern1.C: New test.  Extern declarations  	within functions should introduce names into the innermost  	enclosing namespace
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22902 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.ns/extern1.C
@@ -0,0 +1,18 @@
+// Build don't run:
+
+// Based on a testcase by eyal.ben-david@aks.com
+
+// An extern declaration of an undeclared object within a function
+// introduces the object into the enclosing namespace [basic.link]/7
+
+namespace {
+  void foo() {
+    extern int xx; // causes linker error - XFAIL *-*-*
+    xx = 0;
+  }
+  int xx = 1;
+}
+
+int main() {
+  xx = 2;
+}
