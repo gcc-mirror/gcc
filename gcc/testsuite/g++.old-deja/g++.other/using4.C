a3d33572daedc27b663aa7d25aa9a2f3b6c8cda8
oliva <oliva@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-10-07 21:22:16 +0000

    * g++.old-deja/g++.other/using4.C: New test.  Test using  	declarations of methods from base classes.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@22903 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.other/using4.C
@@ -0,0 +1,20 @@
+// Build don't link:
+
+// Based on a testcase by Martin Bachtold <martinb@coyotesystems.com>
+
+// excess errors test - XFAIL *-*-*
+
+struct foo {
+  void m();
+};
+
+struct bar : foo {
+  using foo::m;
+  void m(int);
+};
+
+void f() {
+  bar b;
+  b.m();
+  b.m(1);
+}
