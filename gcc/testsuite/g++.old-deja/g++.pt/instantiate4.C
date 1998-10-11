0126020166c4f3bc6a262125ec51a99de8379c4b
Alexandre Oliva <oliva@dcc.unicamp.br>
1998-10-11 11:12:06 +0000

    instantiate4.C: New test: -frepo does not generate needed virtual table
    	* g++.old-deja/g++.pt/instantiate4.C: New test: -frepo does not
    	generate needed virtual table
    	* lib/old-dejagnu.exp (old-dejagnu): support `Build then link:'
    
    From-SVN: r22984

---
+++ gcc/testsuite/g++.old-deja/g++.pt/instantiate4.C
@@ -0,0 +1,29 @@
+// Build then link:
+
+// Special g++ Options: -frepo -Werror
+
+// Submitted by Melissa O'Neill <oneill@cs.sfu.ca>
+// the vtable of Foo<int> wouldn't be generated
+
+// excess errors test - XFAIL *-*-*
+
+template <typename A>
+struct Foo {
+   virtual void foo() {}
+};
+
+template <typename A>
+struct Bar {   
+   void bar();
+};
+
+template <typename A> 
+void Bar<A>::bar() {
+   Foo<A> oof;
+}
+
+int main () {
+    Bar<int> rab;
+    
+    rab.bar();
+}
