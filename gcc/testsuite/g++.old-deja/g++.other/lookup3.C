54a9eb0b62b447b33e8e21808a5942ebb017f995
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-10-15 19:44:51 +0000

    new
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@23118 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.other/lookup3.C
@@ -0,0 +1,17 @@
+// Test to make sure that the use of __typeof__ in WIFEXITED works.
+
+int main ()
+{
+  int stat_loc = 0;
+  (__extension__
+   ({
+     union
+     {
+       __typeof__ (stat_loc) __in;
+       int 		__i;
+     } __u;
+     __u.__in = (stat_loc);
+     __u.__i;
+   })
+   );
+}
