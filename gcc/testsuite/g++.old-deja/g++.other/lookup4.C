ea4d3ff6e081929a77d3306137d175505106de58
Jason Merrill <jason@gcc.gnu.org>
1998-10-19 16:13:26 -0400

    new
    From-SVN: r23188

---
+++ gcc/testsuite/g++.old-deja/g++.other/lookup4.C
@@ -0,0 +1,21 @@
+// Test for proper handling of references to overloaded member functions.
+
+struct A {
+  static void f (int) { }
+  void f ();
+};
+
+void (*p)(int) = &A::f;
+
+void A::f ()
+{
+  p = f;
+}
+
+int main()
+{
+  A a;
+  p = &a.f;
+  (a.f)();
+  (a.f)(42);
+}
