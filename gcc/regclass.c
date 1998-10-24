765e958f6bcbbeca84a5f4cf6c5baf5b448de5fb
Richard Kenner <kenner@gcc.gnu.org>
1998-10-24 13:37:55 -0400

    (record_reg_classes): Allow matching up to operand number 9.
    From-SVN: r23281

--- gcc/regclass.c
+++ gcc/regclass.c
@@ -1225,6 +1225,7 @@ record_reg_classes (n_alts, n_ops, ops, modes, constraints, insn)
 
 	      case '?':  case '!':  case '#':
 	      case '0':  case '1':  case '2':  case '3':  case '4':
+	      case '5':  case '6':  case '7':  case '8':  case '9':
 	      case 'p':
 		break;
 
