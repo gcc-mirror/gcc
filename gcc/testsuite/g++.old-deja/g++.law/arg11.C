3ae273dc9250656c8ba4f601165bc4d28074b042
Jason Merrill <jason@gcc.gnu.org>
1998-10-25 21:09:40 -0500

    up
    From-SVN: r23345

--- gcc/testsuite/g++.old-deja/g++.law/arg11.C
+++ gcc/testsuite/g++.old-deja/g++.law/arg11.C
@@ -1,6 +1,5 @@
 // Build don't link: 
 // GROUPS passed arg-matching
-// excess errors test - XFAIL *-*-*
 // From: gustavo@cpqd.br (Gustavo Chaves)
 // Date:     Wed, 25 May 94 09:38:00 EST
 // Subject:  problem with user defined conversions in initialization
@@ -10,7 +9,7 @@ struct String { String(const char*); };
 
 struct Ack { Ack(String); };
 
-struct S { void method(Ack); };
+struct S { void method(Ack); };	// ERROR - referenced below
 
 void function(Ack);
 
