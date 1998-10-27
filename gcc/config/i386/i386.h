c47f5ea57b48aec483b5e0566817143411100171
Bernd Schmidt <crux@pool.informatik.rwth-aachen.de>
1998-10-27 23:35:41 +0000

    reload1.c (verify_initial_offsets): New function.
            * reload1.c (verify_initial_offsets): New function.
            (reload): Call it after reload_as_needed.  Also verify that the frame
            size stays constant during reload_as_needed.
            * i386.h (CONST_DOUBLE_OK_FOR_LETTER_P): Undo Jul 26 change.
    
    From-SVN: r23378

--- gcc/config/i386/i386.h
+++ gcc/config/i386/i386.h
@@ -891,19 +891,10 @@ enum reg_class
 /* Similar, but for floating constants, and defining letters G and H.
    Here VALUE is the CONST_DOUBLE rtx itself.  We allow constants even if
    TARGET_387 isn't set, because the stack register converter may need to
-   load 0.0 into the function value register.
-
-   We disallow these constants when -fomit-frame-pointer and compiling
-   PIC code since reload might need to force the constant to memory.
-   Forcing the constant to memory changes the elimination offsets after
-   the point where they must stay constant.
-
-   However, we must allow them after reload as completed as reg-stack.c
-   will create insns which use these constants.  */
+   load 0.0 into the function value register.  */
 
 #define CONST_DOUBLE_OK_FOR_LETTER_P(VALUE, C)  \
-  (((reload_completed || !flag_pic || !flag_omit_frame_pointer) && (C) == 'G') \
-   ? standard_80387_constant_p (VALUE) : 0)
+  ((C) == 'G' ? standard_80387_constant_p (VALUE) : 0)
 
 /* Place additional restrictions on the register class to use when it
    is necessary to be able to hold a value of mode MODE in a reload
