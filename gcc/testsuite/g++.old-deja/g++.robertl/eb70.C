5959e9bdf5d5fc52ce6a28bdf96f9f1e0063bb1e
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-10-28 01:57:15 +0000

    up
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@23388 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.robertl/eb70.C
+++ gcc/testsuite/g++.old-deja/g++.robertl/eb70.C
@@ -1,18 +1,16 @@
 // conversion ops should be treated as coming from the most derived class
 // for overload resolution.  See [over.match.funcs].
 // Build don't link:
-// excess errors test - XFAIL *-*-*
 
 class X {
 public:
-  inline operator bool() const { return true; }
+  operator bool() const;
 };
 
 class Y : public X {
 private:
-  inline operator void*() const { return 0; }
+  operator void*() const;
 };
-
 
 int f(Y const& y) {
   return bool(y);
