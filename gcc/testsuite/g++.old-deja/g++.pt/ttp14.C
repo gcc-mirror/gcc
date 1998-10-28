714b05f35cc8cb0a20427aca4bece30581806e8a
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-10-28 11:40:53 +0000

    fix
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@23400 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.pt/ttp14.C
+++ gcc/testsuite/g++.old-deja/g++.pt/ttp14.C
@@ -17,7 +17,7 @@ template<class E,template<class> class DD = D> class C
 		int f();
 };
 
-template<class E,template<class> class DD = D> int C<E,DD>::f()
+template<class E,template<class> class DD> int C<E,DD>::f()
 {
 	DD<E> d2;
 	return d2.f();
