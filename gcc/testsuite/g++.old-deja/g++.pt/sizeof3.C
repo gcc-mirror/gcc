79134f62201619915d834d4d533d512b73df2366
oliva <oliva@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-10-31 02:06:21 +0000

    * g++.old-deja/g++.pt/sizeof3.C: a similar testcase not involving 	base classes
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@23469 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.pt/sizeof3.C
@@ -0,0 +1,27 @@
+// Adapted from testcase by Oskar Enoksson <osken393@student.liu.se>
+
+// execution test - XFAIL *-*-*
+
+extern "C" void abort();
+
+template<class T0>
+class A {
+public:
+  typedef T0 T;
+};
+
+template<int K>
+class B {
+  typedef A<char[K]> BC;
+};
+
+template<int N, int M>
+class C { 
+public:
+  typedef A<char[M]> AC;
+};
+
+int main() {
+  if (sizeof(C<3,7>::AC::T) != 7) // gets bogus error - XFAIL *-*-*
+    abort();
+}
