2301bd7d7684944718f636cb72d581610ea10afa
ghazi <ghazi@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-11-07 12:54:34 +0000

    * cp-tree.h: Don't include gansidecl.h.         * exception.cc: Include gansidecl.h (since we don't include config.h)         * g++spec.c: Don't include gansidecl.h.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@23557 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/g++spec.c
+++ gcc/cp/g++spec.c
@@ -19,10 +19,7 @@ the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
 #include "config.h"
-
 #include "system.h"
-
-#include "gansidecl.h"
 
 /* This bit is set if we saw a `-xfoo' language specification.  */
 #define LANGSPEC	(1<<1)
