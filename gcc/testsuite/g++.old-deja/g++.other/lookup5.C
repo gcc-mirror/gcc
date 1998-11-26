ea84ebc2bcd02ffee299c303ef33f182b524ae46
Alexandre Oliva <oliva@gcc.gnu.org>
1998-11-26 20:57:42 +0000

    added crash test
    From-SVN: r23915

--- gcc/testsuite/g++.old-deja/g++.other/lookup5.C
+++ gcc/testsuite/g++.old-deja/g++.other/lookup5.C
@@ -1,9 +1,11 @@
 // Build don't link:
 // Simplified from bug report by Paris Smaragdis <paris@media.mit.edu>
 
+// crash test - XFAIL *-*-*
+
 template <class T> class vector {};
 class foo {};
 int main() {
         foo f;
-        f.vector(); // gets bogus error - ICE - XFAIL *-*-*
+        f.vector(); // ERROR - not a method
 }
