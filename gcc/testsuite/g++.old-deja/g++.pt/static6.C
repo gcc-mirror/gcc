c6cb68654882867731c7f7d35f0bfee35bb4ac1e
oliva <oliva@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-11-26 19:54:16 +0000

    * g++.old-deja/g++.pt/static6.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@23910 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.pt/static6.C
@@ -0,0 +1,14 @@
+// Build don't run:
+
+// Simplified from testcase by Erez Louidor Lior <s3824888@techst02.technion.ac.il>
+
+// causes linker error - XFAIL *-*-*
+
+template <class T> struct A {
+ static const int l[1];
+};
+
+template<class T>
+const int A<T>::l[1] = {1};
+
+int i = A<int>::l[0];
