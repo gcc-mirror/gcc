6715688eaddf5f8d74379023fa6917eff7e96f59
Alexandre Oliva <oliva@gcc.gnu.org>
1998-11-26 20:39:54 +0000

    added crash test XFAIL
    From-SVN: r23913

--- gcc/testsuite/g++.old-deja/g++.pt/decl2.C
+++ gcc/testsuite/g++.old-deja/g++.pt/decl2.C
@@ -2,6 +2,8 @@
 
 // Simplified from testcase by Christophe Boyanique <boyan@imac.u-paris2.fr>
 
+// crash test - XFAIL *-*-*
+
 template <class T> struct foo { foo(); };
 template<class T> foo<T>::foo() {}
-T // ERROR - currently an ICE - XFAIL *-*-*
+T // ERROR - currently an ICE
