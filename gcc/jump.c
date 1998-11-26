3ec75256a55ef86fdcc69ce740e277eda68f4462
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-11-26 00:21:27 +0000

    * rtlanal.c (regs_set_between_p): New function.         * rtl.h (regs_set_between_p): Prototype it.         * jump.c (jump_optimize): Use it instead of modified_between_p         in the Sep 2 change.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@23888 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/jump.c
+++ gcc/jump.c
@@ -521,7 +521,7 @@ jump_optimize (f, cross_jump, noop_moves, after_regscan)
 		      || ! modified_between_p (SET_SRC (temp4), p, temp2))
 		  /* Verify that registers used by the jump are not clobbered
 		     by the instruction being moved.  */
-		  && ! modified_between_p (PATTERN (temp), temp2,
+		  && ! regs_set_between_p (PATTERN (temp), temp2,
 					   NEXT_INSN (temp2)))
 		{
 		  emit_insn_after_with_line_notes (PATTERN (temp2), p, temp2);
@@ -622,7 +622,7 @@ jump_optimize (f, cross_jump, noop_moves, after_regscan)
 		  && ! modified_between_p (SET_SRC (temp4), insert_after, temp)
 		  /* Verify that registers used by the jump are not clobbered
 		     by the instruction being moved.  */
-		  && ! modified_between_p (PATTERN (temp), temp3,
+		  && ! regs_set_between_p (PATTERN (temp), temp3,
 					   NEXT_INSN (temp3))
 		  && invert_jump (temp, JUMP_LABEL (insn)))
 		{
