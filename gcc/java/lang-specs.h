076b41ee483989ab61d0b54510d7dd31b4df1d47
Alexandre Oliva <oliva@dcc.unicamp.br>
1998-11-26 16:20:19 +0000

    jcf-path.c (add_entry): recognize ".jar" too
    	* jcf-path.c (add_entry): recognize ".jar" too
    	* lang-specs.h: ditto
    
    From-SVN: r23907

--- gcc/java/lang-specs.h
+++ gcc/java/lang-specs.h
@@ -27,6 +27,7 @@ The Free Software Foundation is independent of Sun Microsystems, Inc.  */
   {".java",   {"@java"} },
   {".class",  {"@java"} },
   {".zip",    {"@java"} },
+  {".jar",    {"@java"} },
   {"@java",
    {"%{!E:jc1 %i %1 %{!Q:-quiet} %{d*} %{m*} %{a}\
 		    %{g*} %{O*} %{W*} %{w} %{pedantic*} %{ansi}\
