f81c5bd5fc0d118c72f658f854750294f853772b
oliva <oliva@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-11-26 20:57:42 +0000

    added crash test
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@23915 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.pt/friend37.C
+++ gcc/testsuite/g++.old-deja/g++.pt/friend37.C
@@ -1,6 +1,8 @@
 // Build don't link:
 // Simplified from report by Volker Dobler <volker@hugo.physik.uni-konstanz.de>
 
+// crash test - XFAIL *-*-*
+
 template <class T> class A {
-  friend int ice<>( int k=0 ); // gets bogus error - ICE - XFAIL *-*-*
+  friend int ice<>( int k=0 ); // ERROR - undeclared
 };
