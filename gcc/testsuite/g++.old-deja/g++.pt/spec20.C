174a498bcadd6c3410875a810c615511f6b9fa44
Alexandre Oliva <oliva@dcc.unicamp.br>
1998-12-03 11:24:23 +0000

    * g++.old-deja/g++.pt/spec20.C: ERROR marks were bogus
    From-SVN: r24070

--- gcc/testsuite/g++.old-deja/g++.pt/spec20.C
+++ gcc/testsuite/g++.old-deja/g++.pt/spec20.C
@@ -1,11 +1,13 @@
 // Build don't link:
 
+// According to [temp.class.spec.mfunc]/2, these are valid
+
 template <class T> 
 struct S {
   template <class U> void f(U);
-  template <> void f<int>(int); // ERROR - specialization
+  template <> void f<int>(int); // gets bogus error - XFAIL *-*-*
 
   template <class V> struct I {};
   template <class V> struct I<V*> {};
-  template <> struct I<int>; // ERROR - specialization
+  template <> struct I<int>; // gets bogus error - XFAIL *-*-*
 };
