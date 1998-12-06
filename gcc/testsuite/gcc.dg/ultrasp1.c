7b590b2b2031e3623bfc76cae9cd505b9467fd39
oliva <oliva@138bc75d-0d04-0410-961f-82ee72b054a4>
1998-12-06 02:16:21 +0000

    * gcc.dg/ultrasp1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@24127 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/ultrasp1.c
@@ -0,0 +1,8 @@
+/* Simplified from testcase by David Staepelaere <staapa@ultimatech.com> */
+
+/* { dg-do compile { xfail sparc-*-* } } */
+/* { dg-options -mcpu=ultrasparc { target sparc-*-*-* } } */
+
+int foo(long long y) {
+  return -1 * y;
+}
