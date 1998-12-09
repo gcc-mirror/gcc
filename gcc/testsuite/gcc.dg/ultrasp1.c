752e02e4fc8a9379b6f7c808a9c9dc04c6b4815b
Alexandre Oliva <oliva@dcc.unicamp.br>
1998-12-09 00:37:27 +0000

    * gcc.dg/ultrasp1.c: empty dg-options, avoid `long long' warnings
    From-SVN: r24201

--- gcc/testsuite/gcc.dg/ultrasp1.c
+++ gcc/testsuite/gcc.dg/ultrasp1.c
@@ -1,6 +1,7 @@
 /* Simplified from testcase by David Staepelaere <staapa@ultimatech.com> */
 
 /* { dg-do compile { xfail sparc-*-* } } */
+/* { dg-options "" } */
 /* { dg-options -mcpu=ultrasparc { target sparc-*-*-* } } */
 
 int foo(long long y) {
