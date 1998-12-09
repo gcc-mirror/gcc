33964bf4af75b5e6de8ef1b51979a6d96032b54b
Mark Mitchell <mark@markmitchell.com>
1998-12-09 11:46:02 +0000

    decl.c (grokdeclarator): Update the name of the TEMPLATE_DECL...
    	* decl.c (grokdeclarator): Update the name of the TEMPLATE_DECL, as
    	well as the TYPE_DECL, when a typedef name is assigned to a
    	previously anonymous type.
    
    From-SVN: r24218

---
+++ gcc/testsuite/g++.old-deja/g++.pt/derived1.C
@@ -0,0 +1,24 @@
+// Build don't link:
+
+class A
+{
+public:
+  typedef int Info;
+};
+
+template <class T>
+class B : public A
+{
+public:
+  typedef struct{
+    int a; 
+    int b;
+  } Info;
+};
+
+void f()
+{
+  B<A>::Info ie;
+  ie.a=1;
+  ie.b=2;
+}
