7d91fc530a176c8831245bfa38b758d6bfe79591
Jeffrey A Law <law@cygnus.com>
1998-12-11 09:28:51 +0000

    * g++.old-deja/g++.benjamin/p12475.C: Expect failure on mips64 targets.
    From-SVN: r24264

--- gcc/testsuite/g++.old-deja/g++.benjamin/p12475.C
+++ gcc/testsuite/g++.old-deja/g++.benjamin/p12475.C
@@ -2,4 +2,4 @@
 // prms-id: 12475
 // excess errors test - XFAIL alpha*-*-* mips64*-*-*
 
-enum huh { start =-2147483648, next };		// WARNING - , XFAIL sparc64-*-* alpha*-*-*
+enum huh { start =-2147483648, next };		// WARNING - , XFAIL sparc64-*-* alpha*-*-* mips64*-*-*
