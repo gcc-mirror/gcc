ebc9d4b79c148644abd7f418f23b7dc2c10110e3
Alexandre Oliva <oliva@gcc.gnu.org>
1998-12-21 10:39:08 +0000

    require diagnostic even without -pedantic
    From-SVN: r24389

--- gcc/testsuite/g++.old-deja/g++.other/conv5.C
+++ gcc/testsuite/g++.old-deja/g++.other/conv5.C
@@ -2,5 +2,7 @@
 
 // Based on bug report by Thomas Kunert <kunert@physik.tu-dresden.de>
 
-const int foo();
-int (*bar)() = foo; // ERROR - discarding const - XFAIL *-*-*
+// Special g++ Options:
+
+int foo();
+const int (*bar)() = foo; // ERROR - adding const - XFAIL *-*-*
