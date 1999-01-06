fdf004cfcbbc7278a3e44e740f6d125022b14ef3
Jeff Law <law@gcc.gnu.org>
1999-01-06 13:02:36 -0700

    Fix copyrights.
    From-SVN: r24531

--- gcc/expr.c
+++ gcc/expr.c
@@ -1,5 +1,5 @@
 /* Convert tree expression to rtl instructions, for GNU compiler.
-   Copyright (C) 1988, 92-97, 1998 Free Software Foundation, Inc.
+   Copyright (C) 1988, 92-98, 1999 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
