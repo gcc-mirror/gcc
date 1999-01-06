2f197d2fea50781416712f02b730cfae2d3c4bbd
Mark Elbrecht <snowball3@usa.net>
1999-01-05 19:25:57 -0700

    configure.in (pc-msdosdjgpp): Set x_make to x-go32.
            * configure.in (pc-msdosdjgpp): Set x_make to x-go32.
            * configure: Rebuilt.
            * i386/xm-go32.h: Define LIBSTDCXX.
            * i386/x-go32: New.
            * i386/go32.h (MD_EXEC_PREFIX): Define.
            (FILE_NAME_ABSOLUTE_P): Define.
            (LINK_COMMAND_SPEC): Define.
    
    From-SVN: r24510

--- gcc/config/i386/go32.h
+++ gcc/config/i386/go32.h
@@ -14,11 +14,42 @@
 
 #include "i386/gas.h"
 
+/* Search for as.exe and ld.exe in DJGPP's binary directory. */ 
+#define MD_EXEC_PREFIX "$DJDIR/bin/"
+
+/* Correctly handle absolute filename detection in cp/xref.c */
+#define FILE_NAME_ABSOLUTE_P(NAME) \
+        (((NAME)[0] == '/') || ((NAME)[0] == '\\') || \
+        (((NAME)[0] >= 'A') && ((NAME)[0] <= 'z') && ((NAME)[1] == ':')))
+
 #ifdef CPP_PREDEFINES
 #undef CPP_PREDEFINES
 #endif
 #define CPP_PREDEFINES "-Dunix -Di386 -DGO32 -DMSDOS \
   -Asystem(unix) -Asystem(msdos) -Acpu(i386) -Amachine(i386)"
+
+/* We need to override link_command_spec in gcc.c so support -Tdjgpp.djl.
+   This cannot be done in LINK_SPECS as that LINK_SPECS is processed
+   before library search directories are known by the linker.
+   This avoids problems when specs file is not available. An alternate way,
+   suggested by Robert Hoehne, is to use SUBTARGET_EXTRA_SPECS instead.
+*/ 
+
+#undef LINK_COMMAND_SPEC
+#define LINK_COMMAND_SPEC \
+"%{!fsyntax-only: \
+%{!c:%{!M:%{!MM:%{!E:%{!S:%(linker) %l %X %{o*} %{A} %{d} %{e*} %{m} %{N} %{n} \
+\t%{r} %{s} %{t} %{u*} %{x} %{z} %{Z}\
+\t%{!A:%{!nostdlib:%{!nostartfiles:%S}}}\
+\t%{static:} %{L*} %D %o\
+\t%{!nostdlib:%{!nodefaultlibs:%G %L %G}}\
+\t%{!A:%{!nostdlib:%{!nostartfiles:%E}}}\
+\t-Tdjgpp.djl %{T*}}}}}}}\n\
+%{!c:%{!M:%{!MM:%{!E:%{!S:stubify %{v} %{o*:%*} %{!o*:a.out} }}}}}"
+ 
+/* Make sure that gcc will not look for .h files in /usr/local/include 
+   unless user explicitly requests it.  */
+#undef LOCAL_INCLUDE_DIR
 
 #undef EXTRA_SECTIONS
 #define EXTRA_SECTIONS in_ctor, in_dtor
