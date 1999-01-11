5ad98185114ec29301d1befe714e74b387e4b4d6
Jeff Law <law@gcc.gnu.org>
1999-01-11 16:40:03 -0700

    Opps.  Copied in the wrong file for initial commit.
    From-SVN: r24625

--- gcc/testsuite/gcc.c-torture/execute/990106-2.c
+++ gcc/testsuite/gcc.c-torture/execute/990106-2.c
@@ -1,5 +1,21 @@
-foo(char *bufp)
+unsigned calc_mp(unsigned mod)
 {
-    int x = 80;
-    return (*bufp++ = x ? 'a' : 'b');
+      unsigned a,b,c;
+      c=-1;
+      a=c/mod;
+      b=0-a*mod;
+      if (b > mod) { a += 1; b-=mod; }
+      return b;
 }
+
+int main(int argc, char *argv[])
+{
+      unsigned x = 1234;
+      unsigned y = calc_mp(x);
+
+      if (y != 680)
+	abort ();
+      exit (0);
+}
+
+
