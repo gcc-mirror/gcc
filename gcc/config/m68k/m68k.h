27d0138b8c2fbd801f3c702a22f75a1587175e58
Andreas Schwab <schwab@gcc.gnu.org>
1999-01-11 02:00:26 +0000

    Update copyright.
    From-SVN: r24610

--- gcc/config/m68k/m68k.h
+++ gcc/config/m68k/m68k.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler.  Sun 68000/68020 version.
-   Copyright (C) 1987, 88, 93-97, 1998 Free Software Foundation, Inc.
+   Copyright (C) 1987, 88, 93-98, 1999 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
