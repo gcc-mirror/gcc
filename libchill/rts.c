2892309926aa9effd05ad151c0dd8f4b0bae2f6c
Jeff Law <law@gcc.gnu.org>
1999-01-11 07:06:22 -0700

    More address fixes.
    From-SVN: r24618

--- libchill/rts.c
+++ libchill/rts.c
@@ -1,21 +1,21 @@
 /* GNU CHILL compiler regression test file
  Copyright (C) 1992, 1993 Free Software Foundation, Inc.
  
- This file is part of GNU CC.
+This file is part of GNU CC.
 
- GNU CC is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2, or (at your option)
- any later version.
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
 
- GNU CC is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with GNU CC; see the file COPYING.  If not, write to
- the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. */
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. */
 
 /* As a special exception, if you link this library with other files,
    some of which are compiled with GCC, to produce an executable,
