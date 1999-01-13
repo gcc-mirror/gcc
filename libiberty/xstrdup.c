7e4311a31b379a90fbdcdc5f71ed9b27aac29aea
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
1999-01-13 11:30:56 +0000

    * xstrdup.c (xstrdup): Switch from strcpy to memcpy for speed.
    From-SVN: r24651

--- libiberty/xstrdup.c
+++ libiberty/xstrdup.c
@@ -2,16 +2,21 @@
    This trivial function is in the public domain.
    Ian Lance Taylor, Cygnus Support, December 1995.  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
 #include "ansidecl.h"
 #include "libiberty.h"
 
 char *
 xstrdup (s)
-     const char *s;
+  const char *s;
 {
-  char *ret;
-
-  ret = xmalloc (strlen (s) + 1);
-  strcpy (ret, s);
+  register size_t len = strlen (s) + 1;
+  register char *ret = xmalloc (len);
+  memcpy (ret, s, len);
   return ret;
 }
