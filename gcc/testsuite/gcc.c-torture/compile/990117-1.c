a3076648faa136b9a2554b626ea18e501b1b98da
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-01-16 23:09:13 +0000

    * gcc.c-torture/compile/990117-1.c: New test from Horst von Brand. Derived from glibc & egcs code.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@24709 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/990117-1.c
@@ -0,0 +1,13 @@
+static void
+__bb_init_prg ()
+{
+  const char *p;
+
+      {
+	unsigned long l;
+
+	(__extension__ (__builtin_constant_p (p) && __builtin_constant_p (l)
+			? 5 : 2));
+      }
+
+}
