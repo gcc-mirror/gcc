9e3685d5f463193242bece7e381f467165adf66c
Jason Merrill <jason@gcc.gnu.org>
1999-01-16 11:34:32 -0500

    new
    From-SVN: r24702

---
+++ gcc/testsuite/g++.old-deja/g++.pt/memtemp82.C
@@ -0,0 +1,13 @@
+// Build don't link:
+// excess errors test - XFAIL *-*-*
+
+struct foo {
+       template<typename T> T bar() { return staticbar<T>( this ); }
+       template<typename T> static T staticbar( foo* ) { return 0; }
+};
+
+void f()
+{
+       foo t;
+       int k = t.bar<int>();
+}
