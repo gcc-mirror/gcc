f573e6fc7ffe7b4b6534078f7b85320e01aa31b7
Kaveh Ghazi <ghazi@gcc.gnu.org>
1999-01-23 21:24:02 +0000

    Update copyrights
    From-SVN: r24836

--- gcc/scan.h
+++ gcc/scan.h
@@ -1,5 +1,5 @@
 /* scan.h - Utility declarations for scan-decls and fix-header programs.
-   Copyright (C) 1993, 1998 Free Software Foundation, Inc.
+   Copyright (C) 1993, 1998, 1999 Free Software Foundation, Inc.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the
