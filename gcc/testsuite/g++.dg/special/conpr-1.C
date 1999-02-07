962dee4bd440e37c66761ddb9e6857992e03a091
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-02-07 15:40:17 +0000

    new
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@25071 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/special/conpr-1.C
@@ -0,0 +1,18 @@
+/* { dg-do run } */
+
+class foo_t {
+    int x;
+public:
+    foo_t(void) { x=1; }
+    int get(void) { return x; }
+};
+
+static foo_t foo __attribute__((init_priority(5000)));
+
+int main(void) {
+
+    if (foo.get())
+        exit(0);
+    else
+        abort();
+}
