3e6a6fa8eb6f1b9cb75122f4e573468a643cfa8c
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-02-16 23:52:06 +0000

    * i386/freebsd-elf.h, i386/gas.h, i386/linux.h: Fix minor spacing         errors.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@25257 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/gas.h
+++ gcc/config/i386/gas.h
@@ -94,8 +94,8 @@ Boston, MA 02111-1307, USA.  */
 
 #ifdef HAVE_GAS_MAX_SKIP_P2ALIGN
 #  define ASM_OUTPUT_MAX_SKIP_ALIGN(FILE,LOG,MAX_SKIP) \
-     if ((LOG)!=0) \
-       if ((MAX_SKIP)==0) fprintf ((FILE), "\t.p2align %d\n", (LOG)); \
+     if ((LOG) != 0) \
+       if ((MAX_SKIP) == 0) fprintf ((FILE), "\t.p2align %d\n", (LOG)); \
        else fprintf ((FILE), "\t.p2align %d,,%d\n", (LOG), (MAX_SKIP))
 #endif
 
