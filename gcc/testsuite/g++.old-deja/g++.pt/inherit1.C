3fd71a527ef1afd65299554c36d47e509b824abe
Mark Mitchell <mark@markmitchell.com>
1999-02-17 18:58:59 +0000

    decl.c (xref_basetypes): Comment.
    	* decl.c (xref_basetypes): Comment.
    	* pt.c (instantiate_class_template): Use xref_basetypes.
    
    From-SVN: r25272

---
+++ gcc/testsuite/g++.old-deja/g++.pt/inherit1.C
@@ -0,0 +1,25 @@
+// Origin: Wolfgang Bangerth <wolf@gaia.iwr.uni-heidelberg.de>
+
+int i = 1;
+
+struct Base1 {  int local1;  };
+struct Base2 {  int local2;  };
+
+template <int dim> class Derived;
+
+template <>
+class Derived<1> : public Base1, public Base2 {};
+
+template <int dim>
+class FinalClass :  public Derived<dim> {
+public:
+  FinalClass () {
+    if (&local1 != &local2)
+      i = 0;
+  }
+};
+
+int main () {
+  FinalClass<1> a1;
+  return i;
+}
