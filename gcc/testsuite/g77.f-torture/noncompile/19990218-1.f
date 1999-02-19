c72593c9a010d8f3bfc8d5735b240b467c43ee1b
burley <burley@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-02-19 09:15:46 +0000

    new g77 tests
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@25304 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g77.f-torture/noncompile/19990218-1.f
@@ -0,0 +1,13 @@
+        double precision function fun(a,b)
+        double precision a,b
+        print*,'in sub: a,b=',a,b
+        fun=a*b
+        print*,'in sub: fun=',fun
+        return
+        end
+        program test
+        double precision a,b,c
+        data a,b/1.0d-46,1.0d0/
+        c=fun(a,b)
+        print*,'in main: fun=',c
+        end
