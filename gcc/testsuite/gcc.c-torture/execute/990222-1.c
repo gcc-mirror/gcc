001f9148fdb46d8ddaf33d5dab57dc8f9d22442b
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-02-22 00:17:46 +0000

    * gcc.c-torture/execute/990222-1.c: New test. New test from Herman ten Brugge
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@25361 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/990222-1.c
@@ -0,0 +1,11 @@
+char line[4] = { '1', '9', '9', '\0' };
+
+int main()
+{
+  char *ptr = line + 3;
+
+  while ((*--ptr += 1) > '9') *ptr = '0';
+  if (line[0] != '2' || line[1] != '0' || line[2] != '0')
+    abort();
+  return 0;
+}
