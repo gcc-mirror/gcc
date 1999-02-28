d498b23758bfab62301ae80fd8aa38655f1ed3ba
loewis <loewis@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-02-28 09:07:01 +0000

    New test cases.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@25498 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.martin/sts_vectini.C
@@ -0,0 +1,41 @@
+// Special g++ Options: -O2
+// egcs-bugs 1999-02-22 14:24 Stefan Schwarzer
+// sts@ica1.uni-stuttgart.de
+// optimizer problem in egcs <= 1.1.1
+
+struct XTVec{
+  XTVec(){x[0]=x[1] =x[2] =0;}
+  XTVec(int ax,int y=0.,int z=0.){x[0]=ax;x[1]=y; x[2]=z; }
+  int& operator[](int);
+
+  int x[3];
+};
+
+inline 
+int & XTVec::operator[](int i){
+  return x[i];
+}
+
+inline 
+XTVec& operator+=(XTVec& lhs, XTVec& rhs){
+  lhs[0]+=rhs[0];
+  lhs[1]+=rhs[1];
+  lhs[2]+=rhs[2];
+  return lhs;
+}
+
+inline 
+XTVec operator+(XTVec& lhs, XTVec& rhs){
+  XTVec result(lhs);
+  return result += rhs;
+}
+
+int main()
+{
+  XTVec ur(4.,0.,1.);
+  XTVec ll(0.,2.,0.);
+  XTVec initsum(ur + ll);
+
+  // sum of components should be 7
+  return (initsum[0] + initsum[1] + initsum[2] - 7);
+}
