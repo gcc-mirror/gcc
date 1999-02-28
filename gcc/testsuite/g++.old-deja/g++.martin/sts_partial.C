994de9dd7102d8514765062a56ac6646fbf29e83
Martin v. Löwis <loewis@gcc.gnu.org>
1999-02-28 09:07:01 +0000

    New test cases.
    From-SVN: r25498

---
+++ gcc/testsuite/g++.old-deja/g++.martin/sts_partial.C
@@ -0,0 +1,15 @@
+// excess errors test - XFAIL 
+// ecgs-bugs 1999-02-22 14:26 Stefan Schwarzer
+// sts@ica1.uni-stuttgart.de
+// partial ordering problem in egcs <= 1.1.1
+
+template<class T>
+int f(T &){ return 1; }
+
+template<class T>
+int f( T[] ){ return 0; }
+
+int main(){
+  int d[] ={2};
+  return f(d);
+}
