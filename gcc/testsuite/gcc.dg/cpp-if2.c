77ecaf67d7fdba660f6764416f2e1261beb55db1
zack <zack@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-02-28 15:05:41 +0000

    Minor tweaks to testing harness
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@25508 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cpp-if2.c
+++ gcc/testsuite/gcc.dg/cpp-if2.c
@@ -1,12 +1,12 @@
 /* { dg-do preprocess } */
 /* { dg-options -pedantic-errors } */
 
-#if 'a' == 'a' && '\001' == 1 && '\x12' == 0x12
-#error yes /* { dg-error "#error yes" "basic charconst recognition" } */
+#if 'a' != 'a' || '\001' != 1 || '\x12' != 0x12
+#error a,1,0x12 /* { dg-bogus "#error" "basic charconst recognition" } */
 #endif
 
-#if 'a' == L'a' && L'\xfeed' == 0xfeed
-#error yes /* { dg-error "#error yes" "wide charconst recognition" } */
+#if 'a' != L'a' || L'\xfeed' != 0xfeed
+#error L'a',0xfeed /* { dg-bogus "#error" "wide charconst recognition" } */
 #endif
 
 #if 'abcd' /* { dg-warning "multi-character character constant" "multi-character charconst" } */
