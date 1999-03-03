67395f4d9a8c56aef0b2d9e1e84074d4d7e369b9
Richard Kenner <kenner@gcc.gnu.org>
1999-03-03 18:33:35 -0500

    Fix FSF address.
    From-SVN: r25572

--- gcc/config/i386/dgux.h
+++ gcc/config/i386/dgux.h
@@ -16,7 +16,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
 
 /* for now, we are just like the sysv4 version with a
    few hacks
@@ -25,7 +26,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #include "i386/sysv4.h"
 
 #ifndef VERSION_INFO2
-#define VERSION_INFO2   "$Revision: 1.7 $"
+#define VERSION_INFO2   "$Revision: 1.8 $"
 #endif
 
 #ifndef VERSION_STRING
