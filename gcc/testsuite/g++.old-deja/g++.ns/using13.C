4cdea27381507e52775c8d844ebc9522ded33f5f
Martin v. Löwis <loewis@gcc.gnu.org>
1999-03-03 11:23:47 +0000

    New test case
    From-SVN: r25561

---
+++ gcc/testsuite/g++.old-deja/g++.ns/using13.C
@@ -0,0 +1,23 @@
+namespace A{
+ void foo(int){}
+}
+namespace B{
+  void foo(bool){}
+}
+
+void bar()
+{
+  using B::foo;
+  using A::foo;
+  foo(true);
+}
+
+namespace Foo {
+  template<class N> void Hello(N) {}
+}
+
+int main() {
+  using Foo::Hello;
+  Hello(4);
+  bar();
+}
