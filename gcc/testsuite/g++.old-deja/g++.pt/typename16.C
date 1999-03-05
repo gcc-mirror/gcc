7d4bdeed81155efff35f8713d203d86c5733f791
Mark Mitchell <mark@markmitchell.com>
1999-03-05 16:38:54 +0000

    cp-tree.h (ANON_UNION_TYPE_P): Robustify.
    	* cp-tree.h (ANON_UNION_TYPE_P): Robustify.
    	* decl.c (make_typename_type): Don't issue an error if an
    	immediate lookup fails; it migt be resolved later.
    	* friend.c (is_friend): Add comment.
    	* search.c (breadth_first_search): Add POSTFN and DATA
    	parameters.  Tidy.  All callers changed.
    	(lookup_field_queue_p): New function.
    	(lookup_field_r): Likewise.
    	(lookup_field_post): Likewise.
    	(lookup_field): Use them, via breadth_first_search, instead of
    	duplicating logic.
    	(compute_access): Robustify.
    	(lookup_fnfield_info): New structure.
    
    From-SVN: r25607

---
+++ gcc/testsuite/g++.old-deja/g++.pt/typename16.C
@@ -0,0 +1,31 @@
+// Build don't run:
+// Special g++ Options:
+
+struct B {
+  typedef int I;
+};
+
+template <class T>
+struct D1 : public B {
+};
+
+template <class T>
+struct D2 : public D1<T> {
+  I i;
+};
+
+template <>
+struct D1<int> {
+  typedef double I;
+};
+
+template <class T>
+void f(T);
+template <>
+void f(double) {}
+
+int main()
+{
+  D2<int> d2i;
+  f(d2i.i);
+}
