e3cd08a5d7ec96a83e7135fdcd4be554b7644f50
wilson <wilson@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-03-22 15:11:34 +0000

    Fix bf-sign-2.c failure on 64-bit host caused by my last patch. 	* bf-sign-2.c (main): Replace struct sizeof test with 4.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@25902 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/bf-sign-2.c
+++ gcc/testsuite/gcc.c-torture/execute/bf-sign-2.c
@@ -40,7 +40,9 @@ main ()
   if ((x.u15 - 2) >= 0)		/* promoted value should be signed */
     abort ();
 
-  if (sizeof (struct { unsigned long u32:32;}) <= sizeof (int))
+  /* Conditionalize check on whether integers are 4 bytes or larger, i.e.
+     larger than a 31 bit bitfield.  */
+  if (sizeof (int) >= 4)
     {
       if ((x.u31 - 2) >= 0)	/* promoted value should be signed */
 	abort ();
