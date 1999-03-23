16b3094bb6a9168e401f2352a88bb8cbf4dc2dec
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-03-23 19:17:08 +0000

    tweak
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@25930 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.pt/ptrmem7.C
+++ gcc/testsuite/g++.old-deja/g++.pt/ptrmem7.C
@@ -1,21 +1,14 @@
 // Build don't link:
 // Origin: Jason Merrill <jason@cygnus.com>
 
-struct A
-{
-  A() : x(123) { }
-  int x;
-};
-A a;
-
-template<const int A::*PX>
-struct B
-{
-  static int g() { return a.*PX; }
+struct A {
+  int i;
 };
 
+template<const int A::* P>
+struct B { };
 
-int main(int argc, char *argv[])
+int main()
 {
-  int n = B<&A::x>::g();
+  B<&A::i> b;
 }
