aff3985ee9eb15a0db47ae966546b7ce44995c16
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-03-25 03:28:05 +0000

    new
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@25968 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.other/ptrmem4.C
@@ -0,0 +1,12 @@
+// Test to make sure g++ can handle target types that aren't identical
+// with pointers to members.
+
+struct A { int i; };
+struct B : public A { };
+
+int main ()
+{
+  int A::*p = &A::i;
+  const int B::*q = &A::i;
+  return p != q;
+}
