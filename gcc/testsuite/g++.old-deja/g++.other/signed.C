e17815e1708d289bd4dcb6bde8244489cba33e19
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-03-25 03:50:54 +0000

    new
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@25971 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.other/signed.C
@@ -0,0 +1,16 @@
+// Build don't link:
+
+// Copyright (C) 1999 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 24 Mar 1999 <nathan@acm.org>
+
+// Determine that function style casts are groked
+
+void fn()
+{
+  +char(5);
+  +short(5);
+  +int(5);
+  +long(5);
+  +signed(5);
+  +unsigned(5);
+}
