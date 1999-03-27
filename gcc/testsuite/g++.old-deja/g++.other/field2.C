f3516f2d9ceb0e12650d7f01f902ee96d06e556e
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-03-27 01:12:06 +0000

    new
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26011 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.other/field2.C
@@ -0,0 +1,20 @@
+// Test for proper handling of field calls.
+// Contributed by Jason Merrill <jason@cygnus.com>
+
+struct A {
+  inline A* operator()() { return this; }
+};
+
+struct B {
+  int i;
+  union { A a; };
+};
+
+int
+main ()
+{
+  B b;
+  A* ap = &b.a;
+  A* ap2 = b.a();
+  return (ap != ap2);
+}
