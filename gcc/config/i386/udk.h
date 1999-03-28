5f47c350d15fe0ec5c55a88d4da600e47a5c4474
robertl <robertl@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-03-28 10:28:29 +0000

    � 	* i386/sysv5.h: New file to describe UnixWare7/SVR5. 	* configure.in (i?86-UnixWare7*-sysv): Use it. 	* i386/udk.h: Use sysv5.h.   Now uses Dwarf-2.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26039 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/udk.h
+++ gcc/config/i386/udk.h
@@ -6,18 +6,13 @@
 /* We're very much the SVR4 target with "/udk" prepended to everything that's
    interesting */
 
-#include "i386/sysv4.h"
+#include "i386/sysv5.h"
 
 #undef MD_EXEC_PREFIX
 #define MD_EXEC_PREFIX 		"/udk/usr/ccs/bin/" 
 
 #undef MD_STARTFILE_PREFIX
 #define MD_STARTFILE_PREFIX 	"/udk/usr/ccs/lib/" 
-
-#if 0 /* Later. */
-#undef PREFERRED_DEBUGGING_TYPE
-#define PREFERRED_DEBUGGING_TYPE DWARF2_DEBUG 
-#endif
 
 #define STANDARD_INCLUDE_DIR	"/udk/usr/include"
 
