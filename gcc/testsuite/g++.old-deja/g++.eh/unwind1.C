5728af7d874a156e27b994afda6a7d552af56f22
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-03-30 15:04:45 +0000

    new
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26067 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.eh/unwind1.C
@@ -0,0 +1,24 @@
+// Test that unwinding properly restores SP.
+// Contributed by Jason Merrill <jason@cygnus.com>
+
+int f (int i)
+{
+  throw i;
+}
+
+int main ()
+{  
+  void *sp1 = __builtin_alloca (0);
+
+  try
+    {
+      f (0);
+    }
+  catch (int)
+    {
+    }
+
+  void *sp2 = __builtin_alloca (0);
+
+  return (sp1 != sp2);
+}
