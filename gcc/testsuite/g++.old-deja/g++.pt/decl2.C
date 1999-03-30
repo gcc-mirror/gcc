eecaaf43fac2244f645bbd36e4f8b3c9b9e57e57
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-03-30 23:22:28 +0000

    update
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26070 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.pt/decl2.C
+++ gcc/testsuite/g++.old-deja/g++.pt/decl2.C
@@ -2,8 +2,6 @@
 
 // Simplified from testcase by Christophe Boyanique <boyan@imac.u-paris2.fr>
 
-// crash test - XFAIL *-*-*
-
 template <class T> struct foo { foo(); };
 template<class T> foo<T>::foo() {}
 T // ERROR - currently an ICE
