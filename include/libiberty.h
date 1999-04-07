64f62168ef931afaa0a57fadf9b4410cadcbe249
Jeff Law <law@gcc.gnu.org>
1999-04-06 22:24:20 -0600

    Sync 	* Add symcat file. 	* libiberty.h expose basename prototype on more systems
    From-SVN: r26238

--- include/libiberty.h
+++ include/libiberty.h
@@ -36,7 +36,7 @@ extern char **dupargv PARAMS ((char **));
    across different systems, sometimes as "char *" and sometimes as
    "const char *" */
 
-#if defined(__GNU_LIBRARY__ ) || defined (__linux__)
+#if defined (__GNU_LIBRARY__ ) || defined (__linux__) || defined (__FreeBSD__) || defined (__CYGWIN__) || defined (__CYGWIN32__)
 extern char *basename PARAMS ((const char *));
 #else
 extern char *basename ();
