6ed173d03b97ef5657f2d316235674ae0f61f812
Catherine Moore <clm@cygnus.com>
1999-04-07 12:59:53 +0000

    elf.h (MAKE_DECL_ONE_ONLY): Define.
           * config/mips/elf.h (MAKE_DECL_ONE_ONLY): Define.
            (UNIQUE_SECTION_P): Define.
    
    From-SVN: r26260

--- gcc/config/mips/elf.h
+++ gcc/config/mips/elf.h
@@ -169,6 +169,8 @@ do {									 \
   fputc ('\n', FILE);				\
  } while (0)
 
+#define MAKE_DECL_ONE_ONLY(DECL) (DECL_WEAK (DECL) = 1)
+#define UNIQUE_SECTION_P(DECL) (DECL_ONE_ONLY (DECL))
 #define UNIQUE_SECTION(DECL,RELOC)					   \
 do {									   \
   int len, size, sec;							   \
