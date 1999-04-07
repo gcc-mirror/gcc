18719e69a3764e48004296e1654d66570de534c0
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-04-07 04:46:54 +0000

    Fix copyright date.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26242 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/v850/v850.h
+++ gcc/config/v850/v850.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler. NEC V850 series
-   Copyright (C) 1996, 1997, 1998 Free Software Foundation, Inc.
+   Copyright (C) 1996, 1997, 1998, 1999 Free Software Foundation, Inc.
    Contributed by Jeff Law (law@cygnus.com).
 
 This file is part of GNU CC.
