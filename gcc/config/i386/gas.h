a059f76b597bf95c29cd2f79474853eb4421183e
Krister Walfridsson <cato@df.lth.se>
1999-04-09 22:31:32 +0200

    gas.h (ASM_OUTPUT_MAX_SKIP_ALIGN): Fix typo.
            * i386/gas.h (ASM_OUTPUT_MAX_SKIP_ALIGN): Fix typo.
            * i386/freebsd-elf.h (ASM_OUTPUT_MAX_SKIP_ALIGN): Likewise.
    
    From-SVN: r26321

--- gcc/config/i386/gas.h
+++ gcc/config/i386/gas.h
@@ -96,7 +96,7 @@ Boston, MA 02111-1307, USA.  */
 #  define ASM_OUTPUT_MAX_SKIP_ALIGN(FILE,LOG,MAX_SKIP) \
      if ((LOG) != 0) {\
        if ((MAX_SKIP) == 0) fprintf ((FILE), "\t.p2align %d\n", (LOG)); \
-       else fprintf ((FILE), "\t.p2align %d,,%d\n", (LOG), (MAX_SKIP)) \
+       else fprintf ((FILE), "\t.p2align %d,,%d\n", (LOG), (MAX_SKIP)); \
      }
 #endif
 
