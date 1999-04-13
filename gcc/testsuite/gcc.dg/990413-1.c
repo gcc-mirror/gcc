78c45660ff322714605c833a8b312061e1339ec7
zack <zack@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-04-13 23:09:35 +0000

    Test for cpp line numbering bug
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26431 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/990413-1.c
@@ -0,0 +1,13 @@
+/* Verify that cpp doesn't screw up the line numbering when a macro argument
+   extends over multiple lines.  */
+/* { dg-do compile } */
+
+#define FOO(x)  /* nothing */
+
+void
+func(void)
+{
+  FOO(i
+      = 4)
+  else;  /* { dg-error "syntax error" "error on this line" { target native } { 12 } } */ 
+}
