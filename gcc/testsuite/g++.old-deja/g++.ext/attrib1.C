510710badd940cb5bf5a4fe5b6284047b7f61e2e
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-04-15 05:49:12 +0000

    new
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26471 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.ext/attrib1.C
+++ gcc/testsuite/g++.old-deja/g++.ext/attrib1.C
@@ -1,6 +1,11 @@
-// Test for using prefix attributes in an abstract declarator.
+// Test for using prefix attributes in a parameter decl.
 // Contributed by Jason Merrill <jason@cygnus.com>
 // Skip if not target: i?86-*-*
 // Build don't link:
 
-void f (void (__attribute__((__stdcall__)) *)());
+#define _stdcall __attribute__ ((__stdcall__))
+
+typedef void (_stdcall* pfn)();
+
+void f (void (_stdcall*)    ());
+void f (void (_stdcall* pfn)());
