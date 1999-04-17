25aab5d0addefe0e3b814a7ec540a1fa883cd11f
Mark Mitchell <mark@codesourcery.com>
1999-04-17 14:15:29 +0000

    decl.c (xref_tag): Revise handling of nested template declarations.
    	* decl.c (xref_tag): Revise handling of nested template
    	declarations.
    	* pt.c (check_explicit_specialization): Tweak handling of friend
    	templates in template classes.
    	(tsubst_friend_class): Handle friend declarations for nested
    	member template classes.
    
    From-SVN: r26520

---
+++ gcc/testsuite/g++.old-deja/g++.pt/friend41.C
@@ -0,0 +1,21 @@
+// Build don't link:
+// Origin: Mark Mitchell <mark@codesourcery.com>
+
+template <class T>
+class S {
+public:
+  template <class U>
+  class C {
+  public:
+    void f() { S::i = 3; }
+  };
+
+  template <class U>
+  friend class C;
+
+private:
+  static int i;
+};
+
+
+template void S<int>::C<double>::f();
