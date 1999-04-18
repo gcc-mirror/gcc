fac618987174fd5030aea66b11c81633c10b409a
Anthony Green <green@cygnus.com>
1999-04-18 08:24:30 +0000

    Makefile.in: Rebuilt.
    	* Makefile.in: Rebuilt.
    	* Makefile.am (ordinary_java_source_files): Add new security files.
    	* java/security/NoSuchAlgorithmException.java,
    	java/security/MessageDigest.java: New files.
    	* include/javaprims.h: Add security namespace.
    
    From-SVN: r26536

---
+++ libjava/java/security/NoSuchAlgorithmException.java
@@ -0,0 +1,22 @@
+/* Copyright (C) 1999  Cygnus Solutions
+
+   This file is part of libgcj.
+
+This software is copyrighted work licensed under the terms of the
+Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
+details.  */
+ 
+package java.security;
+ 
+public class NoSuchAlgorithmException extends Exception
+{
+  public NoSuchAlgorithmException()
+  {
+    super();
+  }
+
+  public NoSuchAlgorithmException(String msg)
+  {
+    super(msg);
+  }
+}
