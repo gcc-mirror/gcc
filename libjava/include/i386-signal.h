95ef33d1772a9cc5a602b9a671ed84e45558eb3c
aph <aph@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-04-20 06:27:11 +0000

    1999-04-20  Andrew Haley  <aph@cygnus.com>         * include/sparc-signal.h: new file.         * configure.in: include/sparc-signal.h added.         * configure: regenerated.         * prims.cc (JvRunMain): signal handling code rewritten to be more         portable.         (catch_segv): ditto.         (catch_fpe): ditto.         * include/i386-signal.h: reorganized.         * include/default-signal.h: reorganized.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26560 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/include/i386-signal.h
+++ libjava/include/i386-signal.h
@@ -9,8 +9,8 @@ Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
 details.  */
 
 /* This technique should work for all i386 based Unices which conform
-   to iBCS2.  This includes all versions of Linux more recent than
-   version 1.3 */
+ * to iBCS2.  This includes all versions of Linux more recent than 1.3 
+ */
 
 
 #ifndef JAVA_SIGNAL_H
@@ -21,8 +21,10 @@ details.  */
 #define HANDLE_SEGV 1
 #define HANDLE_FPE 1
 
+#define SIGNAL_HANDLER(_name)	\
+static void _name (int _dummy)
 
-#define MAKE_THROW_FRAME(_dummy)					\
+#define MAKE_THROW_FRAME						\
 {									\
   void **_p = (void **)&_dummy;						\
   struct sigcontext_struct *_regs = (struct sigcontext_struct *)++_p;	\
@@ -33,6 +35,30 @@ details.  */
   asm volatile ("mov %0, (%%ebp); mov %1, 4(%%ebp)"			\
 		: : "r"(_ebp), "r"(_eip));				\
 }
+
+#define INIT_SEGV						\
+do								\
+  {								\
+    nullp = new java::lang::NullPointerException ();    	\
+    struct sigaction act;					\
+    act.sa_handler = catch_segv;				\
+    sigemptyset (&act.sa_mask);					\
+    act.sa_flags = 0;						\
+    sigaction (SIGSEGV, &act, NULL);				\
+  }								\
+while (0)  
+
+#define INIT_FPE						\
+do								\
+  { 								\
+    arithexception = new java::lang::ArithmeticException ();	\
+    struct sigaction act;					\
+    act.sa_handler = catch_fpe;					\
+    sigemptyset (&act.sa_mask);					\
+    act.sa_flags = 0;						\
+    sigaction (SIGFPE, &act, NULL);				\
+  }								\
+while (0)  
 
 #endif /* JAVA_SIGNAL_H */
   
