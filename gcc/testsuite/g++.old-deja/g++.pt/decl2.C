94ff101fbd669d656b0120207cc2bf3ca311ece1
Zack Weinberg <zack@gcc.gnu.org>
1999-04-28 06:46:53 +0000

    Adjust so it actually tests something other than a preprocessor quirk; mark XFAIL
    From-SVN: r26687

--- gcc/testsuite/g++.old-deja/g++.pt/decl2.C
+++ gcc/testsuite/g++.old-deja/g++.pt/decl2.C
@@ -1,7 +1,8 @@
 // Build don't link:
+// crash test - XFAIL *-*-*
 
 // Simplified from testcase by Christophe Boyanique <boyan@imac.u-paris2.fr>
 
 template <class T> struct foo { foo(); };
 template<class T> foo<T>::foo() {}
-T // ERROR - currently an ICE
+T; // ERROR - ICE - XFAIL *-*-*
