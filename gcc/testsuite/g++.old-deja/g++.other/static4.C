58a8a2dae160e37dbb7be5e1ca364ed7db70df9f
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-04-29 13:41:32 +0000

    Fix test to correctly reflect problem
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26695 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.other/static4.C
+++ gcc/testsuite/g++.old-deja/g++.other/static4.C
@@ -1,8 +1,9 @@
 // Origin: Andrew Pollard <andrew@odie.demon.co.uk>
+// Special g++ Options: -O
 
 struct A {
-        A(int);
+        A(int, int);
 };
-A::A(int) {}
-static A _A(0);
+A::A(int, int) {}
+static A _A(0, 0);
 int main() { return(0); }
