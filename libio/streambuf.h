1e332c6fe5855dda65d612c7491028025f997673
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-04-29 08:32:13 +0000

    * streambuf.h (ios::~ios): Use operator delete[] to remove _arrays.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26691 138bc75d-0d04-0410-961f-82ee72b054a4

--- libio/streambuf.h
+++ libio/streambuf.h
@@ -482,7 +482,9 @@ inline ios::~ios() {
 #ifndef _IO_NEW_STREAMS
     if (!(_flags & (unsigned int)ios::dont_close)) delete rdbuf();
 #endif
-    if (_arrays) delete [] _arrays;
+    // It is safe to use naked operator delete[] as we know elements have no
+    // dtor, and g++ does not add a new[] cookie for such cases.
+    operator delete[] (_arrays);
 }
 } // extern "C++"
 #endif /* _STREAMBUF_H */
