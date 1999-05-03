b5884f105f38d87e1878d80b15cbdbe3df6ccb50
loewis <loewis@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-05-03 23:55:28 +0000

    New test case
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26763 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.martin/typedef2.C
@@ -0,0 +1,7 @@
+// Build don't link:
+// Testcase from Alexander Zvyagin <zvyagin@mx.ihep.su>
+// Check implicit conversion from string constants into typedefs
+
+typedef char CHAR;
+void f2(CHAR *s="");
+
