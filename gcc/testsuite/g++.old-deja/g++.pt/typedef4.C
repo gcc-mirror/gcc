aa65d1a2adda3ca65e8e4e71295074b3ad647c65
Mark Mitchell <mark@codesourcery.com>
1999-05-03 15:08:33 +0000

    search.c (lookup_field_r): Set the TREE_TYPE of an ambiguous lookup to error_mark_node here.
    	* search.c (lookup_field_r): Set the TREE_TYPE of an ambiguous
    	lookup to error_mark_node here.
    	(lookup_member): Revise documentation.  Add comments.  Don't set
    	the TREE_TYPE to error_mark_node here, and don't build up an extra
    	TREE_LIST for ambiguous lookups.
    	(setup_class_bindings): Adjust accordingly.
    	(push_class_decls): Revise out-of-date comments.
    
    From-SVN: r26754

---
+++ gcc/testsuite/g++.old-deja/g++.pt/typedef4.C
@@ -0,0 +1,17 @@
+// Build don't link:
+// Origin: Mark Mitchell <mark@codesourcery.com>
+
+struct B1 {
+  typedef int X;
+};
+
+struct B2 {
+  typedef int X;
+};
+
+template <class T>
+struct D : public B1, public B2 {
+  typedef int X;
+};
+
+template struct D<int>;
