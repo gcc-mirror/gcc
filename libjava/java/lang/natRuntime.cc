468c97e2155ac10eeaf87b636f372ddc4a67d685
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-05-05 14:19:24 +0000

    * Makefile.in: Rebuilt. 	* Makefile.am (CLEANFILES): Don't mention $(class_files). 	(clean-local): New target 	* java/lang/natRuntime.cc: Include <ltdl.h> if required. 	(load, loadLibrary): Now native. 	(init): New method. 	* java/lang/Runtime.java (load, loadLibrary): Now native. 	(init): New native method. 	(Runtime): Use init. 	* prims.cc: Include <ltdl.h> if required. 	(JvRunMain): Call LTDL_SET_PRELOADED_SYMBOLS.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26785 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/lang/natRuntime.cc
+++ libjava/java/lang/natRuntime.cc
@@ -15,6 +15,12 @@ details.  */
 #include <cni.h>
 #include <jvm.h>
 #include <java/lang/Runtime.h>
+#include <java/lang/UnknownError.h>
+#include <java/lang/UnsatisfiedLinkError.h>
+
+#ifdef USE_LTDL
+#include <ltdl.h>
+#endif
 
 void
 java::lang::Runtime::exit (jint status)
@@ -41,6 +47,55 @@ void
 java::lang::Runtime::gc (void)
 {
   _Jv_RunGC ();
+}
+
+void
+java::lang::Runtime::load (jstring path)
+{
+  JvSynchronize sync (this);
+  checkLink (path);
+  using namespace java::lang;
+#ifdef USE_LTDL
+  // FIXME: make sure path is absolute.
+  lt_dlhandle h = lt_dlopen (FIXME);
+  if (h == NULL)
+    {
+      const char *msg = lt_dlerror ();
+      _Jv_Throw (new UnsatisfiedLinkError (JvNewStringLatin1 (msg)));
+    }
+#else
+  _Jv_Throw (new UnknownError
+	     (JvNewStringLatin1 ("Runtime.load not implemented")));
+#endif /* USE_LTDL */
+}
+
+void
+java::lang::Runtime::loadLibrary (jstring lib)
+{
+  JvSynchronize sync (this);
+  checkLink (lib);
+  using namespace java::lang;
+#ifdef USE_LTDL
+  // FIXME: make sure path is absolute.
+  lt_dlhandle h = lt_dlopenext (FIXME);
+  if (h == NULL)
+    {
+      const char *msg = lt_dlerror ();
+      _Jv_Throw (new UnsatisfiedLinkError (JvNewStringLatin1 (msg)));
+    }
+#else
+  _Jv_Throw (new UnknownError
+	     (JvNewStringLatin1 ("Runtime.loadLibrary not implemented")));
+#endif /* USE_LTDL */
+}
+
+void
+java::lang::Runtime::init (void)
+{
+  finalize_on_exit = false;
+#ifdef USE_LTDL
+  lt_dlinit ();
+#endif
 }
 
 void
