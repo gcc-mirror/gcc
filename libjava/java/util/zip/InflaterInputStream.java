9b987e331504cd6027812cefc4ff1aa3f6a6e128
bothner <bothner@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-05-06 00:15:43 +0000

    u 	* InflaterInputStream.java:  New stub class. 	* ZipInputStream.java:  New class.  Partly works. 	* ZipConstants.java:  Add two (internal) constants. 	* ZipEntry.java (timeFromDOS):  New static (non-public) method. 	* ZipFile.java:  Make it mostly work, except for compression. 	* ZipOutputStream.java:  Start implementation.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26792 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/java/util/zip/InflaterInputStream.java
@@ -0,0 +1,12 @@
+package java.util.zip;
+import java.io.*;
+
+/** Placefolder - very incomplete. */
+
+public class InflaterInputStream extends FilterInputStream
+{
+  public InflaterInputStream(InputStream in)
+  {
+    super(in);
+  }
+}
