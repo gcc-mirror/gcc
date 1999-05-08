1ba68f550956bf29a20e6fa5103bd29709474749
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-05-08 16:39:18 +0000

    * g++.old-deja/g++.benjamin/p12475.C: Use LONG_MAX to find         the constant to test.  Don't XFAIL 64-bit targets.         * g++.old-deja/g++.jason/thunk3.C: Don't XFAIL Alpha.         * g++.old-deja/g++.law/profile1.C: Likewise.         * g++.old-deja/g++.other/delete4.C: Define size_t; use it in all         operator new instances.         * g++.old-deja/g++.other/delete5.C: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26840 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.other/delete5.C
+++ gcc/testsuite/g++.old-deja/g++.other/delete5.C
@@ -1,16 +1,18 @@
 // Copyright (C) 1999 Free Software Foundation, Inc.
 // Contributed by Nathan Sidwell 15 Apr 1999 <nathan@acm.org>
 
+typedef __SIZE_TYPE__ size_t;
+
 struct X
 {
   int i;
   
   X():i(){}
-  void *operator new(unsigned)
+  void *operator new(size_t)
   {
     return 0; // WARNING - cannot return NULL
   }
-  void *operator new[](unsigned)
+  void *operator new[](size_t)
   {
     return 0; // WARNING - cannot return NULL
   }
@@ -21,11 +23,11 @@ struct Y
   int i;
   
   Y():i(){}
-  void *operator new(unsigned) throw()
+  void *operator new(size_t) throw()
   {
     return 0; // ok
   }
-  void *operator new[](unsigned) throw()
+  void *operator new[](size_t) throw()
   {
     return 0; // ok
   }
