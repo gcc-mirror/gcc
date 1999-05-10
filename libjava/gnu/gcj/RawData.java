a31b081d1116f0158b70c4a7e9eb04943d1a1349
Tom Tromey <tromey@cygnus.com>
1999-05-10 08:42:28 +0000

    RawData.java: Added copyright header.
    	* gnu/gcj/RawData.java: Added copyright header.
    	* include/jni.h (_Jv_va_list): Always define as va_list.
    
    From-SVN: r26855

--- libjava/gnu/gcj/RawData.java
+++ libjava/gnu/gcj/RawData.java
@@ -1,3 +1,11 @@
+/* Copyright (C) 1999  Cygnus Solutions
+
+   This file is part of libgcj.
+
+This software is copyrighted work licensed under the terms of the
+Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
+details.  */
+
 package gnu.gcj;
 
 /** A type uses to indicate pointers to non-Java data. */
