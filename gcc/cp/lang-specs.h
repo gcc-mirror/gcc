938fb460b073963d2d0d28ad35711de3ae1ea360
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-05-17 23:28:09 +0000

    * lang-specs.h: Define __GNU_CC and __GNUC_MINOR__ only if -no-gcc         was not given.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26984 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/lang-specs.h
+++ gcc/cp/lang-specs.h
@@ -33,7 +33,8 @@ Boston, MA 02111-1307, USA.  */
      "%{E|M|MM:cpp -lang-c++ %{nostdinc*} %{C} %{v} %{A*} %{I*} %{P} %{$} %I\
 	%{C:%{!E:%eGNU C++ does not support -C without using -E}}\
 	%{M} %{MM} %{MD:-MD %b.d} %{MMD:-MMD %b.d} %{MG}\
-	-D__GNUC__=%v1 -D__GNUG__=%v1 -D__GNUC_MINOR__=%v2 -D__cplusplus\
+	%{!no-gcc:-D__GNUC__=%v1 -D__GNUG__=%v1 -D__GNUC_MINOR__=%v2}\
+	-D__cplusplus\
 	%{ansi:-trigraphs -D__STRICT_ANSI__} %{!undef:%{!ansi:%p} %P}\
 	%{!fno-exceptions:-D__EXCEPTIONS}\
         %c %{Os:-D__OPTIMIZE_SIZE__} %{O*:%{!O0:-D__OPTIMIZE__}} %{trigraphs}\
@@ -43,8 +44,8 @@ Boston, MA 02111-1307, USA.  */
       %{!E:%{!M:%{!MM:cc1plus %i %1 %2\
                             -lang-c++ %{nostdinc*} %{C} %{A*} %{I*} %{P} %{$} %I\
                             %{MD:-MD %b.d} %{MMD:-MMD %b.d} %{MG}\
-                            -D__GNUC__=%v1 -D__GNUG__=%v1\
-                            -D__GNUC_MINOR__=%v2 -D__cplusplus\
+                            %{!no-gcc:-D__GNUC__=%v1 -D__GNUG__=%v1\
+                            -D__GNUC_MINOR__=%v2} -D__cplusplus\
                             %{ansi:-trigraphs -D__STRICT_ANSI__} %{!undef:%{!ansi:%p} %P}\
                             %{!fno-exceptions:-D__EXCEPTIONS}\
                             %c %{Os:-D__OPTIMIZE_SIZE__} %{O*:%{!O0:-D__OPTIMIZE__}}\
@@ -64,7 +65,8 @@ Boston, MA 02111-1307, USA.  */
    {"cpp -lang-c++ %{nostdinc*} %{C} %{v} %{A*} %{I*} %{P} %{$} %I\
 	%{C:%{!E:%eGNU C++ does not support -C without using -E}}\
 	%{M} %{MM} %{MD:-MD %b.d} %{MMD:-MMD %b.d} %{MG}\
-	-D__GNUC__=%v1 -D__GNUG__=%v1 -D__GNUC_MINOR__=%v2 -D__cplusplus\
+	%{!no-gcc:-D__GNUC__=%v1 -D__GNUG__=%v1 -D__GNUC_MINOR__=%v2}\
+	-D__cplusplus\
 	%{ansi:-trigraphs -D__STRICT_ANSI__} %{!undef:%{!ansi:%p} %P}\
 	%{!fno-exceptions:-D__EXCEPTIONS}\
         %c %{Os:-D__OPTIMIZE_SIZE__} %{O*:%{!O0:-D__OPTIMIZE__}} %{trigraphs}\
