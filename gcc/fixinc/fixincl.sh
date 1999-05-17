5c8553add6b7ddf71c70056fc051b2013966e7ce
korbb <korbb@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-05-17 12:41:23 +0000

    Removed no-op pipe
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@26973 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fixinc/fixincl.sh
+++ gcc/fixinc/fixincl.sh
@@ -395,7 +395,7 @@ echo 'Removing unneeded directories:'
 cd $LIB
 all_dirs=`find . -type d -print | sort -r`
 for file in $all_dirs; do
-  rmdir $LIB/$file > /dev/null 2>&1 | :
+  rmdir $LIB/$file > /dev/null 2>&1
 done
 
 # # # # # # # # # # # # # # # # # # # # #
