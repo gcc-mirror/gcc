4d49638c24e1f1e9f0f363913f20109b6829dbe7
Mark Mitchell <mark@codesourcery.com>
1999-05-22 01:24:58 +0000

    * expr.h (lang_expand_constant): Guard with #ifdef TREE_CODE.
    From-SVN: r27090

--- gcc/expr.h
+++ gcc/expr.h
@@ -1015,11 +1015,13 @@ extern rtx (*lang_expand_expr) PROTO ((union tree_node *, rtx,
 				       enum machine_mode,
 				       enum expand_modifier modifier));
 
+#ifdef TREE_CODE
 /* Hook called by output_constant for language-specific tree codes.
    It is up to the language front-end to install a hook if it has any
    such codes that output_constant needs to know about.  Returns a
    language-independent constant equivalent to its input.  */
-extern union tree_node * (*lang_expand_constant) PROTO((tree));
+extern tree (*lang_expand_constant) PROTO((tree));
+#endif
 
 extern void init_all_optabs			PROTO ((void));
 extern void init_mov_optab			PROTO ((void));
