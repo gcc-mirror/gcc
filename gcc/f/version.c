6f84cd7959ace3dbab71e7f5c1337dad98b6d010
burley <burley@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-05-26 12:48:41 +0000

    rename version numbers, etc
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@27174 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-const char *ffe_version_string = "0.5.24-19990515";
+const char *ffe_version_string = "0.5.25 19990526 (prerelease)";
--- libf2c/libI77/Version.c
+++ libf2c/libI77/Version.c
@@ -3,7 +3,7 @@ static char junk[] = "\n@(#) LIBI77 VERSION pjw,dmg-mods 19990503\n";
 /*
 */
 
-char __G77_LIBI77_VERSION__[] = "0.5.24-19981012";
+char __G77_LIBI77_VERSION__[] = "0.5.25 19981012 (prerelease)";
 
 /*
 2.01	$ format added
