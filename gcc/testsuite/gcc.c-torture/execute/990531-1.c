e14cdc191e5b4262b37fcfb68ceb8a0495c9cb2a
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-05-30 23:40:07 +0000

    * gcc.c-torture/execute/990531-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@27261 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/990531-1.c
@@ -0,0 +1,23 @@
+   unsigned long bad(int reg, unsigned long inWord)
+   {
+       union {
+           unsigned long word;
+           unsigned char byte[4];
+       } data;
+
+       data.word = inWord;
+       data.byte[reg] = 0;
+
+       return data.word;
+   }
+
+main()
+{
+  /* XXX This test could be generalized.  */
+  if (sizeof (long) != 4)
+    exit (0);
+
+  if (bad (0, 0xdeadbeef) == 0xdeadbeef)
+    abort ();
+  exit (0);
+}
