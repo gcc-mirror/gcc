f8bc613916612ca12f0a2ab35e28b588c6dad681
Robert Lipe <robertlipe@usa.net>
1999-05-30 13:45:17 +0000

    fixincl.c: Replace local include scheme with #includes of gansidecl.h and system.h.
            * fixincl.c: Replace local include scheme with #includes of
            gansidecl.h and system.h.
            * procopen.c:  Likewise.
            * server.c:  Likewise.
    
    From-SVN: r27251

--- gcc/fixinc/server.c
+++ gcc/fixinc/server.c
@@ -46,17 +46,9 @@
  * If you do not wish that, delete this exception notice.
  */
 #include "auto-host.h"
-
-#include <sys/types.h>
-#ifdef HAVE_FCNTL_H
-#include <fcntl.h>
-#endif
-#include <errno.h>
+#include "gansidecl.h"
+#include "system.h"
 #include <signal.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <sys/param.h>
 
 #include "server.h"
 
