21022457e3492961c5259b2c620e048f38c57a53
amylaar <amylaar@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-06-03 20:00:16 +0000

    * sh.c (fixup_addr_diff_vecs): Emit braf reference label. 	(braf_label_ref_operand): Delete. 	* sh.h (PREDICATE_CODES): Remove braf_label_ref_operand. 	* sh.md (casesi_jump_2): Operand1 is now the inside of a 	label_ref, and has no predicate. 	The patten has a predicate to guard against invalid substitutions. 	(dummy_jump): Delete. 	(casesi): Update use of casesi_jump_2.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@27329 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sh/sh.h
+++ gcc/config/sh/sh.h
@@ -2129,7 +2129,6 @@ extern struct rtx_def *get_fpscr_rtx ();
   {"arith_reg_operand", {SUBREG, REG}},					\
   {"arith_reg_or_0_operand", {SUBREG, REG, CONST_INT}},			\
   {"binary_float_operator", {PLUS, MULT}},				\
-  {"braf_label_ref_operand", {LABEL_REF}},				\
   {"commutative_float_operator", {PLUS, MULT}},				\
   {"fp_arith_reg_operand", {SUBREG, REG}},				\
   {"fp_extended_operand", {SUBREG, REG, FLOAT_EXTEND}},			\
