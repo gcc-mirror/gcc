048f60dcb1e7663c2fcd5e1d5f0aaad621e786c4
Jeffrey A Law <law@cygnus.com>
1999-06-17 12:50:51 +0000

    * gcc.c-torture/compile/990617-1.c: New test.
    From-SVN: r27572

---
+++ gcc/testsuite/gcc.c-torture/compile/990617-1.c
@@ -0,0 +1,17 @@
+
+int main()
+{
+    do {
+        long l;
+        long *p = &l;
+        
+        *p = 0x0000000070000000L;
+        p += 2;
+        {
+            unsigned int *addr = (unsigned int *)0x70000000;
+            printf("%d, %d\n", addr[1], addr[0]);
+        }
+        
+    } while (1);
+}
+
