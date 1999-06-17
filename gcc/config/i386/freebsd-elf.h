415177981b6f292824578b84fedd2db7fbb7162a
David O'Brien <obrien@FreeBSD.org>
1999-06-17 12:33:32 +0000

    * i386/freebsd-elf.h (LINK_SPEC): Fix typo.
    From-SVN: r27571

--- gcc/config/i386/freebsd-elf.h
+++ gcc/config/i386/freebsd-elf.h
@@ -239,7 +239,7 @@ Boston, MA 02111-1307, USA.  */
       %{!static: \
         %{rdynamic:-export-dynamic} \
 	%{!dynamic-linker:-dynamic-linker /usr/libexec/ld-elf.so.1}} \
-    %{static:-Bstatic}}} \
+    %{static:-Bstatic}} \
   %{symbolic:-Bsymbolic}"
 
 /* A C statement to output to the stdio stream FILE an assembler
