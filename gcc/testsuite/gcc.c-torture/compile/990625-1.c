3fb36cbd2963839fa95263f339f2a08c651bd3c8
Jeffrey A Law <law@cygnus.com>
1999-06-25 04:29:41 +0000

    * gcc.c-torture/compile/990625-1.c: New test.
    From-SVN: r27756

---
+++ gcc/testsuite/gcc.c-torture/compile/990625-1.c
@@ -0,0 +1,12 @@
+#define __USE_STRING_INLINES
+#include <string.h>
+
+void test()
+{
+        char *p, *a;
+        const char *s;
+
+        while ( (s = a) )
+          p = strcpy(strcpy(p,"/"), s);
+}
+
