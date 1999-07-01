b537d12084f7aba41c814b0bcf0edf5d0ac3a159
Mark Mitchell <mark@codesourcery.com>
1999-07-01 16:36:59 +0000

    decl.c (grokdeclarator): Don't give names "for linkage purposes" to anonymous cv-qualified types.
    	* decl.c (grokdeclarator): Don't give names "for linkage purposes"
    	to anonymous cv-qualified types.
    
    From-SVN: r27891

---
+++ gcc/testsuite/g++.old-deja/g++.other/debug4.C
@@ -0,0 +1,6 @@
+// Build don't link:
+// Special g++ Options: -g
+
+typedef volatile struct {
+	int i;
+} mutex_t;
--- gcc/testsuite/g++.old-deja/g++.other/typedef1.C
+++ gcc/testsuite/g++.old-deja/g++.other/typedef1.C
@@ -4,9 +4,9 @@ typedef const struct {
    int x;
 } Test;
 
-void foo(Test);
+static void foo(Test);
 
-void foo(Test t)
+static void foo(Test t)
 {
   t.x = 0; // ERROR - assignment of read-only member
   return;
