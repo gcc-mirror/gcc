090cf5ce08a53e9d3acc0d8190557fe0a7ae28ce
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-07-02 15:16:02 +0000

    * gcc/configure.in (mips-sgi-irix6*): Handle --with-gnu-ld. 	* gcc/config/mips/t-iris6gld: New file. 	* gcc/config/mips/iris6gld.h: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@27924 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/mips/iris6gld.h
@@ -0,0 +1,48 @@
+/* Definitions of target machine for GNU compiler.  Iris version 6 with
+   GNU ld.
+   Copyright (C) 1999 Free Software Foundation, Inc.
+   Written by Mark Mitchell <mark@codesourcery.com>.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+#undef LIB_SPEC
+#define LIB_SPEC \
+  "%{mabi=n32: %{mips4:-L/usr/lib32/mips4} %{!mips4:-L/usr/lib32/mips3} \
+     -L/usr/lib32} \
+   %{mabi=64: %{mips4:-L/usr/lib64/mips4} %{!mips4:-L/usr/lib64/mips3} \
+     -L/usr/lib64} \
+   %{!mabi*: %{mips4:-L/usr/lib32/mips4} %{!mips4:-L/usr/lib32/mips3} \
+     -L/usr/lib32} \
+   %{!shared: \
+     %{p:libprof1.a%s}%{pg:libprof1.a%s} -lc}"
+
+/* Use the default libgcc spec.  */
+#undef LIBGCC_SPEC
+
+/* ??? If no mabi=X option give, but a mipsX option is, then should depend
+   on the mipsX option.  */
+#undef LINK_SPEC
+#define LINK_SPEC "\
+%{G*} %{EB} %{EL} %{mips1} %{mips2} %{mips3} %{mips4} \
+%{bestGnum} %{shared} %{non_shared} \
+%{call_shared} %{no_archive} %{exact_version} \
+%{static: -non_shared} \
+%{!static: \
+  %{!shared: %{!non_shared: %{!call_shared: -call_shared}}}} \
+%{rpath} -init __do_global_ctors -fini __do_global_dtors \
+%{mabi=32: -melf32bsmip}%{mabi=n32: -melfbmipn32}%{mabi=64: -melfbmip64}%{!mabi*: -melf32bmipn32}"
