cb4fe216b239021cd2532d45cc0bad75819e27a5
oliva <oliva@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-07-03 11:03:01 +0000

    * template3.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@27933 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.oliva/template3.C
@@ -0,0 +1,11 @@
+// Build don't link:
+
+// Copyright (C) 1999 Free Software Foundation
+
+// by Alexandre Oliva <oliva@dcc.unicamp.br>
+// based on bug report by Ulf Larsson <ulf.larsson@mbow337.swipnet.se>
+
+// crash test - XFAIL *-*-*
+
+template <class T> class C {};
+class foo {} bar = bar.C();
