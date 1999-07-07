c0772678a981c077876304b152a5c82c82dba915
Franz Sirl <Franz.Sirl-kernel@lauterbach.com>
1999-07-07 01:05:39 +0000

    reload1.c (gen_reload): When synthesizing a 3 operand add sequence...
            * reload1.c (gen_reload): When synthesizing a 3 operand add
            sequence, improve test for when to reload OP1 into the reload
            register instead of OP0.
    
    From-SVN: r27988

--- gcc/reload1.c
+++ gcc/reload1.c
@@ -7819,9 +7819,13 @@ gen_reload (out, in, opnum, type)
 	 DEFINE_PEEPHOLE should be specified that recognizes the sequence
 	 we emit below.  */
 
+      code = (int) add_optab->handlers[(int) GET_MODE (out)].insn_code;
+
       if (CONSTANT_P (op1) || GET_CODE (op1) == MEM || GET_CODE (op1) == SUBREG
 	  || (GET_CODE (op1) == REG
-	      && REGNO (op1) >= FIRST_PSEUDO_REGISTER))
+             && REGNO (op1) >= FIRST_PSEUDO_REGISTER)
+         || (code != CODE_FOR_nothing
+             && ! (*insn_operand_predicate[code][2]) (op1, insn_operand_mode[code][2])))
 	tem = op0, op0 = op1, op1 = tem;
 
       gen_reload (out, op0, opnum, type);
