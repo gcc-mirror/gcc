70ceca9bf0624ff620cafaf96a7330c1bce4bebb
Alexandre Oliva <oliva@dcc.unicamp.br>
1999-07-17 14:26:13 +0000

    * template6.C, delete1.C, template7.C: New test.
    From-SVN: r28140

---
+++ gcc/testsuite/g++.old-deja/g++.oliva/template6.C
@@ -0,0 +1,11 @@
+// Build don't link:
+
+// Copyright (C) 1999 Free Software Foundation
+
+// by Alexandre Oliva <oliva@dcc.unicamp.br>
+// simplified from bug report by Meenaradchagan Vishnu <mvishnu@fore.com>
+
+// crash test - XFAIL *-*-*
+
+template <typename> struct foo {};
+template <> void foo();
