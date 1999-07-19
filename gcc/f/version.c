f524a24177b8581bbb742615c6815f106524770d
Jeff Law <law@gcc.gnu.org>
1999-07-19 01:46:09 -0600

    Daily bump.
    From-SVN: r28168

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-const char *ffe_version_string = "0.5.25 19990718 (prerelease)";
+const char *ffe_version_string = "0.5.25 19990719 (prerelease)";
--- gcc/version.c
+++ gcc/version.c
@@ -1,1 +1,1 @@
-char *version_string = "2.95 19990718 (prerelease)";
+char *version_string = "2.95 19990719 (prerelease)";
