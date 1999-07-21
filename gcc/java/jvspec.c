d9ccfd0af306444b1f8b1acaed2ffa3782be7aca
aph <aph@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-07-21 14:51:24 +0000

    1999-07-15  Andrew Haley  <aph@cygnus.com>
            * lang.c (flag_use_divide_subroutine): New variable.
            * typeck.c: (convert_ieee_real_to_integer): Bounds check
            fp-to-integer conversion.
            (convert): Call convert_ieee_real_to_integer when flag_fast_math
            is not set.
    
            * expr.c (build_java_soft_divmod): New function.
            (build_java_binop): Call build_java_soft_divmod if
            flag_use_divide_subroutine is set.
            * decl.c: soft_idiv_node, soft_irem_node, soft_ldiv_node, tree
            soft_lrem_node: new builtin functions.
            (init_decl_processing) Initialize the new builtins.
            * java-tree.h soft_idiv_node, soft_irem_node, soft_ldiv_node, tree
            soft_lrem_node: new builtin functions.
            (build_java_soft_divmod): New function.
            * parse.y: Call build_java_soft_divmod if
            flag_use_divide_subroutine is set.
            * parse.c: Rebuilt.
    
            * jvspec.c (lang_specific_driver): Always allow an extra arg (for
            a --specs= arg) even if not linking.
            * lang-options.h (DEFINE_LANG_NAME ("Java")): Add
            -fuse-divide-subroutine
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@28209 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/java/jvspec.c
+++ gcc/java/jvspec.c
@@ -376,8 +376,7 @@ lang_specific_driver (fn, in_argc, in_argv, in_added_libraries)
     }
   if (saw_g + saw_O == 0)
     num_args++;
-  if (will_link)
-    num_args++;
+  num_args++;
   arglist = (char **) xmalloc ((num_args + 1) * sizeof (char *));
 
   for (i = 0, j = 0; i < argc; i++, j++)
@@ -408,7 +407,7 @@ lang_specific_driver (fn, in_argc, in_argv, in_added_libraries)
 	  continue;
 	}
 
-      if (will_link && spec_file == NULL && strncmp (argv[i], "-L", 2) == 0)
+      if (spec_file == NULL && strncmp (argv[i], "-L", 2) == 0)
 	spec_file = find_spec_file (argv[i] + 2);
 
       if (strncmp (argv[i], "-fmain=", 7) == 0)
@@ -453,10 +452,10 @@ lang_specific_driver (fn, in_argc, in_argv, in_added_libraries)
   if (saw_g + saw_O == 0)
     arglist[j++] = "-g1";
 
-  /* Read the specs file corresponding to libgcj, but only if linking.
+  /* Read the specs file corresponding to libgcj.
      If we didn't find the spec file on the -L path, then we hope it
      is somewhere in the standard install areas.  */
-  if (will_link)
+  if (! saw_C)
     arglist[j++] = spec_file == NULL ? "-specs=libgcj.spec" : spec_file;
 
   if (saw_C)
