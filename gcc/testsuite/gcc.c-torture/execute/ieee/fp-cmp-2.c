1fccef3b12b4151760f16dbe646135c06c52cd10
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-07-27 15:03:03 +0000

    Add clones of fp-cmp-1 for float/long double; Change name of unknown register
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@28296 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/ieee/fp-cmp-2.c
@@ -0,0 +1,41 @@
+#include <signal.h>
+
+float nan = 1.0f/0.0f - 1.0f/0.0f;
+float x = 1.0f;
+
+void leave ()
+{
+  exit (0);
+}
+
+main ()
+{
+#if ! defined (__vax__) && ! defined (_CRAY)
+  /* Move this line earlier, for architectures (like alpha) that issue 
+     SIGFPE on the first comparisons. */
+#ifndef SIGNAL_SUPPRESS
+  /* Some machines catches a SIGFPE when a NaN is compared.
+     Let this test succeed o such machines.  */
+  signal (SIGFPE, leave);
+#endif
+  /* NaN is an IEEE unordered operand.  All these test should be false.  */
+  if (nan == nan)
+    abort ();
+  if (nan != x)
+    x = 1.0;
+  else
+    abort ();
+
+  if (nan < x)
+    abort ();
+  if (nan > x)
+    abort ();
+  if (nan <= x)
+    abort ();
+  if (nan >= x)
+    abort ();
+  if (nan == x)
+    abort ();
+#endif
+  exit (0);
+}
