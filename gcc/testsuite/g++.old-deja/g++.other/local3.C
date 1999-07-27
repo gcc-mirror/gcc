6b6e55ef26209f2087addf85c06114aa9153b92c
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-07-27 01:11:10 +0000

    new
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@28279 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.other/local3.C
@@ -0,0 +1,17 @@
+// Bug: g++ lies about DECL_CONTEXT, so the backend thinks B::f is not
+// function-local.
+// Contributed by Jason Merrill <jason@cygnus.com>
+// excess errors test - XFAIL *-*-*
+
+struct A {
+  virtual void f () = 0;
+};
+
+int main()
+{
+   struct B : public A {
+     void f () { }
+   }; 
+
+   B b;
+}
