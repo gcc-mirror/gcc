6bfd5083bbda841256d11ada370b9acff6d72642
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-08-02 23:23:45 +0000

    Jakub Jelinek  <jj@ultra.linux.cz>
            * real.c (PUT_REAL): Clear unused bytes if long double is IEEE quad.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@28422 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/real.c
+++ gcc/real.c
@@ -260,7 +260,12 @@ do {						\
 #define MAXDECEXP 4932
 #define MINDECEXP -4977
 #define GET_REAL(r,e) bcopy ((char *) r, (char *) e, 2*NE)
-#define PUT_REAL(e,r) bcopy ((char *) e, (char *) r, 2*NE)
+#define PUT_REAL(e,r)				\
+do {						\
+  if (2*NE < sizeof(*r))			\
+    bzero((char *)r, sizeof(*r));		\
+  bcopy ((char *) e, (char *) r, 2*NE);		\
+} while (0)
 #else
 #define NE 6
 #define MAXDECEXP 4932
