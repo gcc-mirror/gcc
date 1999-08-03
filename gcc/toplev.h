e1c49ab40e7b8d8dbd2a08a4b1b1b683d507ed1b
Mark Mitchell <mark@codesourcery.com>
1999-08-03 00:07:52 +0000

    toplev.h (errorcount): Declare.
    	* toplev.h (errorcount): Declare.
    	(warningcount): Likewise.
    	(sorrycount): Likewise.
    	* c-lex.c (errorcount): Don't declare.
    	* dwarf2out.c (errorcount): Don't declare.
    
    From-SVN: r28426

--- gcc/toplev.h
+++ gcc/toplev.h
@@ -115,4 +115,7 @@ extern void fnotice			PROTO ((FILE *, const char *, ...))
 extern const char *trim_filename	PROTO ((const char *));
 extern int wrapup_global_declarations   PROTO ((union tree_node **, int));
 extern void check_global_declarations   PROTO ((union tree_node **, int));
+extern int errorcount;
+extern int warningcount;
+extern int sorrycount;
 #endif /* __GCC_TOPLEV_H */
