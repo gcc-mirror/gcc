246833ac171aa132fee598a93a86a819c6eaf4f4
Richard Henderson <rth@gcc.gnu.org>
1999-08-02 17:58:44 -0700

    expr.h (PROMOTE_PROTOTYPES): Move ...
            * expr.h (PROMOTE_PROTOTYPES): Move ...
            * defaults.h: ... to here.
            * c-decl.c: Include defaults.h instead of expr.h.
            * c-typeck.c: Include defaults.h.
    
    From-SVN: r28433

--- gcc/expr.h
+++ gcc/expr.h
@@ -251,11 +251,6 @@ enum direction {none, upward, downward};  /* Value has this type.  */
 #define PRETEND_OUTGOING_VARARGS_NAMED 0
 #endif
 
-/* Supply a default definition for PROMOTE_PROTOTYPES.  */
-#ifndef PROMOTE_PROTOTYPES
-#define PROMOTE_PROTOTYPES	0
-#endif
-
 /* Nonzero if we do not know how to pass TYPE solely in registers.
    We cannot do so in the following cases:
 
