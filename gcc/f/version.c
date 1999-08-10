4c71b22f2d4ba07cf8d55f2b8edbcb355df44bef
Jeff Law <law@gcc.gnu.org>
1999-08-10 01:45:08 -0600

    Daily bump.
    From-SVN: r28643

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-const char *ffe_version_string = "0.5.25 19990809 (experimental)";
+const char *ffe_version_string = "0.5.25 19990810 (experimental)";
--- gcc/version.c
+++ gcc/version.c
@@ -1,1 +1,1 @@
-char *version_string = "2.96 19990809 (experimental)";
+char *version_string = "2.96 19990810 (experimental)";
