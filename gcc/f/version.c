0c661084e88af2cd9b68311edaf471b1f09926d8
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-08-11 07:45:14 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@28663 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-const char *ffe_version_string = "0.5.25 19990810 (experimental)";
+const char *ffe_version_string = "0.5.25 19990811 (experimental)";
--- gcc/version.c
+++ gcc/version.c
@@ -1,1 +1,1 @@
-char *version_string = "2.96 19990810 (experimental)";
+char *version_string = "2.96 19990811 (experimental)";
