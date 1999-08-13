a9729c43a0c0b7313ab463d10f2e797b3d6463ed
Martin v. Löwis <loewis@gcc.gnu.org>
1999-08-13 07:25:31 +0000

    New test case
    From-SVN: r28693

---
+++ gcc/testsuite/g++.old-deja/g++.martin/eval1.c
@@ -0,0 +1,21 @@
+// Postfix expression must be evaluated even if accessing a static member.
+// execution test - XFAIL *-*-*
+
+struct S
+{
+	static int i;
+	S* foo();
+};
+
+S* S::foo(){
+  i = 0;
+  return this;
+};
+
+int S::i = 1;
+int main(void)
+{
+	S * s = new S;
+	int k=(s->foo())->i;
+	return k;
+}
