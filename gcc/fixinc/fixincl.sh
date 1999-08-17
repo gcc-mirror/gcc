4a291c1c3a07b211124c7558183a5dcfdd99e1ac
korbb <korbb@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-08-17 18:32:32 +0000

    Do not trash single-line C-style comments
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@28735 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fixinc/fixincl.sh
+++ gcc/fixinc/fixincl.sh
@@ -6,7 +6,7 @@
 # files which are fixed to work correctly with ANSI C and placed in a
 # directory that GNU C will search.
 #
-# This script contains 105 fixup scripts.
+# This script contains 106 fixup scripts.
 #
 # See README-fixinc for more information.
 #
