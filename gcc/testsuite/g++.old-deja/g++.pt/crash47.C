dfe2b0b3b51f0596fe3bd27a3adc8bf432babacd
Mark Mitchell <mark@codesourcery.com>
1999-08-18 22:11:10 +0000

    search.c (setup_class_bindings): Robustify.
    	* search.c (setup_class_bindings): Robustify.
    	* typeck.c (common_type): Use same_type_p, not pointer equality,
    	to compare types.
    
    From-SVN: r28756

---
+++ gcc/testsuite/g++.old-deja/g++.pt/crash47.C
@@ -0,0 +1,17 @@
+// Build don't link:
+// Origin: Rick Campbell <rick.campbell@db.com>
+
+template <class Owner, typename Type>
+struct DataMember
+{
+    inline DataMember (Type Owner::* data_member);
+
+    Type Owner::* _data_member;
+};
+
+template <class Owner, typename Type>
+inline
+DataMember<Owner,Type>::DataMember (Type Owner::* data_member)
+    : _data_member (data_member)
+{
+}
