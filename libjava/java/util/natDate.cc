f033810d2265f5fac109f29cb23d1d6ef2fe71fb
Alexandre Oliva <oliva@gcc.gnu.org>
1999-08-21 11:56:21 +0000

    natSystem.cc (getpwuid_adaptor): New overloaded function that detects the signature of getpwuid_r.
    * java/lang/natSystem.cc (getpwuid_adaptor): New overloaded
    function that detects the signature of getpwuid_r.
    (init_properties): Use it.
    * java/util/natDate.cc (ctime_adaptor): Likewise for ctime_r.
    (toString): Use it.
    
    From-SVN: r28789

--- libjava/java/util/natDate.cc
+++ libjava/java/util/natDate.cc
@@ -1,45 +1,23 @@
-/* Copyright (C) 1998, 1999  Cygnus Solutions
+#if HAVE_CTIME_R
+/* Use overload resolution to find out the signature of ctime_r.  */
 
-   This file is part of libgcj.
-
-This software is copyrighted work licensed under the terms of the
-Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
-details.  */
-
-#include <config.h>
-
-// We want to make sure to pick up the POSIX ctime_r.  Some systems,
-// such as Solaris 2.6, have their own version as well.
-#ifdef HAVE_CTIME_R
-#define _POSIX_PTHREAD_SEMANTICS
-#endif
-
-#include <cni.h>
-#include <java/util/Date.h>
-#include <java/lang/String.h>
-
-#include <time.h>
-
-#ifdef HAVE_SYS_TYPES_H
-#include <sys/types.h>
-#endif
-
-#ifdef HAVE_SYS_TIME_H
-#include <sys/time.h>
-#endif
-
-jstring
-java::util::Date::toString()
+  /* This is Posix ctime_r().  */
+template <typename T_clock, typename T_buf, size_t buflen>
+static inline char *
+ctime_adaptor (char* (*ctime_r)(T_clock *clock, T_buf *buf),
+	       time_t *clock, char (&buf)[buflen])
 {
-#ifdef HAVE_CTIME_R
-  time_t t = millis / 1000;
-  char buf[30];
-  return JvNewStringLatin1 (ctime_r (&t, buf));
-#elif defined (HAVE_CTIME)
-  // FIXME: this isn't thread-safe.
-  time_t t = millis / 1000;
-  return JvNewStringLatin1 (ctime (&t));
-#else
-  return NULL;
-#endif
+  return ctime_r(clock, buf);
 }
+
+/* This is an old-style ctime_r, used on IRIX 5.2.  */
+template <typename T_clock, typename T_buf, typename T_buflen, size_t buflen>
+static inline char *
+ctime_adaptor (char* (*ctime_r)(T_clock *clock, T_buf *buf, T_buflen len),
+	       time_t *clock, char (&buf)[buflen])
+{
+  return ctime_r(clock, buf, buflen);
+}
+#endif
+
+  return JvNewStringLatin1 (ctime_adaptor (ctime_r, &t, buf));
