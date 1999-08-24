952d33b8db3341dd48dc4e73186720f33a8525db
Jason Merrill <jason@gcc.gnu.org>
1999-08-24 17:25:13 -0400

    new
    From-SVN: r28827

---
+++ gcc/testsuite/g++.old-deja/g++.eh/cond1.C
@@ -0,0 +1,31 @@
+// Build don't link:
+
+// Copyright (C) 1999 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 11 Apr 1999 <nathan@acm.org>
+// Derived from bug report from Gabriel Dos Reis
+// <Gabriel.Dos-Reis@cmla.ens-cachan.fr>
+// http://egcs.cygnus.com/ml/egcs-bugs/1999-03/msg00883.html
+
+// conditional exprs have some funny rules when one of the types is void.
+// [expr.cond] 5.16, make sure we do the right things
+// We implement an extension, allowing one side to be void, check we
+// pedantically moan.
+
+struct X {};
+void fn(int i)
+{
+  int j;
+
+  j = (i ? throw X() : 1); // ok, int
+  j = (i ? 1 : throw X()); // ok, int
+  
+  (i ? throw X() : throw X());  // ok, void
+  
+  (i ? i : j) = 1; // ok, int &
+  (i ? throw X() : j) = 1; // ERROR - non-lvalue
+  (i ? j : throw X()) = 1; // ERROR - non-lvalue
+  (i ? throw X() : throw X()) = 1;  // ERROR - void
+  
+  (i ? (void)1 : i++); // ERROR - ANSI forbids
+  (i ? i++ : (void)1); // ERROR - ANSI forbids
+}
