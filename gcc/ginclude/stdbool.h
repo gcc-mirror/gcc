18d113ce3dab0cc67ae9c42e916028a30afcacea
zack <zack@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-08-29 15:46:14 +0000

    1999-08-29 08:38 -0700  Zack Weinberg  <zack@bitmover.com>
    	* stdbool.h: Make the typedef name _Bool, with bool a #defined
    	alias.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@28972 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ginclude/stdbool.h
+++ gcc/ginclude/stdbool.h
@@ -2,17 +2,20 @@
 #ifndef __STDBOOL_H__
 #define __STDBOOL_H__	1
 
-/* The type `bool' must promote to `int' or `unsigned int'.  The constants
+/* The type `_Bool' must promote to `int' or `unsigned int'.  The constants
    `true' and `false' must have the value 0 and 1 respectively.  */
 typedef enum
   {
     false = 0,
     true = 1
-  } bool;
+  } _Bool;
 
 /* The names `true' and `false' must also be made available as macros.  */
 #define false	false
 #define true	true
+
+/* The macro `bool', which may be undefined, expands to _Bool.  */
+#define bool _Bool
 
 /* Signal that all the definitions are present.  */
 #define __bool_true_false_are_defined	1
