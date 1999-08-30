44696e7830dcf9593f53d729c47a943758b60abe
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-08-30 05:22:45 +0000

    * pt.c (push_template_decl_real): Use template declaration from 	class type if it exists.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@28978 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.ns/template14.C
@@ -0,0 +1,14 @@
+// Build don't link:
+// Origin: Alex Samuel <samuel@codesourcery.com>
+
+namespace Outer 
+{
+template <class T> class Inner;
+}
+
+template <class T>
+class Outer::Inner  
+{
+public:
+  Inner ();
+};
