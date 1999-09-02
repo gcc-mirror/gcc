492de4b7d9b991829897f8ebe38bd3da17357f67
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-09-02 09:23:14 +0000

    * g++.old-deja/g++.other/ambig2.C: New test. 	* g++.old-deja/g++.other/cond5.C: New test. 	* g++.old-deja/g++.other/lookup16.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@29057 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.other/lookup16.C
@@ -0,0 +1,30 @@
+// Build don't link:
+// Copyright (C) 1999 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 25 Aug 1999 <nathan@acm.org>
+
+// typenames are not injected early enough, [basic.scope.pdecl]3.3.1/4
+// indicates this should compile.
+
+struct A {
+};
+
+struct B : A {
+  typedef A Parent;
+  struct F {
+  };
+};
+
+struct C : B {
+  typedef B Parent;
+  struct G {};
+  struct F : C::Parent::F {
+    typedef C::Parent::F Parent;
+  };
+};
+
+struct D : B {
+  typedef B Parent;
+  struct F : D::Parent::F {
+    typedef D::Parent::F Parent;
+  };
+};
