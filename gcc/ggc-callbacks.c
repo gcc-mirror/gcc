3252bc51296378f1b40734e863a230307b0b0064
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-09-04 22:34:20 +0000

    * Makefile.in (C_AND_C_OBJS): Add ggc-callbacks.o. 	(OBJS): Add $(GGC). 	(ggc-callbacks.o): New target. 	* ggc-callbacks.c: New file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@29113 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/ggc-callbacks.c
@@ -0,0 +1,43 @@
+/* Empty GC callbacks to be used by languages that don't support GC.
+   Copyright (C) 1998 Free Software Foundation, Inc.
+
+   This file is part of GNU CC.
+
+   GNU CC is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   GNU CC is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GNU CC; see the file COPYING.  If not, write to
+   the Free Software Foundation, 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include "config.h"
+#include "system.h"
+#include "rtl.h"
+#include "tree.h"
+#include "ggc.h"
+
+void 
+lang_mark_tree (t)
+     union tree_node *t ATTRIBUTE_UNUSED;
+{
+  /* If this function is called, we are doing GC.  But, this file is
+     only included in compilers for languages that don't support GC.  */
+  abort ();
+}
+
+void
+lang_cleanup_tree (t)
+     union tree_node *t ATTRIBUTE_UNUSED;
+{
+  /* If this function is called, we are doing GC.  But, this file is
+     only included in compilers for languages that don't support GC.  */
+  abort ();
+}
