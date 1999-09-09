c82dbd958872662840fa7e33e9c0a47c7608938e
Mark Mitchell <mark@codesourcery.com>
1999-09-09 00:29:22 +0000

    decl.c (layout_var_decl): Change prototype.
    	* decl.c (layout_var_decl): Change prototype.  Don't complete
    	types for external objects.
    	(check_initializer): Likewise.  Tidy.
    	(initialize_local_var): Complete types here.
    	(cp_finish_decl): Not here.  Reorganize a little.
    	(grokvardecl): Don't complete types here.
    
    From-SVN: r29220

---
+++ gcc/testsuite/g++.old-deja/g++.pt/poi1.C
@@ -0,0 +1,15 @@
+// Build don't link:
+// Origin: Gerald Pfeifer <pfeifer@dbai.tuwien.ac.at>
+
+template <class T>
+class TLITERAL : public T 
+    {
+    int x;
+    };
+
+class GATOM;
+
+typedef TLITERAL<GATOM> x;
+extern TLITERAL<GATOM> y;
+
+
