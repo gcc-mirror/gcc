5b0b5a84004c1a237c35b83a08bf28aed19f781c
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-09-10 11:05:30 +0000

    * g++.old-deja/g++.jason/rfg4.C: Revert erroneous change, add -w. 	* g++.old-deja/g++.jason/rfg5.C: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@29268 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.jason/rfg4.C
+++ gcc/testsuite/g++.old-deja/g++.jason/rfg4.C
@@ -1,5 +1,6 @@
 // Bug: f1 and f2 are treated as overloaded when they aren't.
 // Build don't link:
+// Special g++ Options: -pedantic -errors -w
 
 int i;
 void f1(double) { }
@@ -8,6 +9,5 @@ void f2(double) { }
 void
 test ()
 {
-  void (*ptr) (double);
-  ptr = i ? f1 : f2;		// gets bogus error - improper overloading
+  i ? f1 : f2;		// gets bogus error - improper overloading
 }
