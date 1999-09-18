57b524174e514e6df90c7d973c43b78b5a5c5004
Mark Mitchell <mark@codesourcery.com>
1999-09-18 23:56:18 +0000

    decl.c (layout_var_decl): Change prototype.
    	* decl.c (layout_var_decl): Change prototype.  Call layout_decl
    	even when the declaration is external.
    	(cp_finish_decl): Adjust call to layout_var_decl.
    	* pt.c (tsubst_expr): Make sure to initialize stmt before using it.
    
    From-SVN: r29504

---
+++ gcc/testsuite/g++.old-deja/g++.other/crash11.C
@@ -0,0 +1,21 @@
+// Origin: Alfred Minarik <a8601248@unet.univie.ac.at>
+// Build don't link:
+
+template <typename T>
+struct allocator
+{
+  typedef int size_type;
+};
+
+template <typename T>
+struct string
+{
+  typedef typename allocator<T>::size_type size_type;
+
+  static size_type size;
+
+  size_type
+  max_size() const { return size; }
+};
+
+template string <char>;
