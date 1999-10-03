e192c1020f20455e953b7d73c7a1f84bf7f0fb8e
Kaveh Ghazi <ghazi@gcc.gnu.org>
1999-10-03 17:01:59 +0000

    Update copyright date
    From-SVN: r29785

--- gcc/bitmap.c
+++ gcc/bitmap.c
@@ -1,5 +1,5 @@
 /* Functions to support general ended bitmaps.
-   Copyright (C) 1997, 1998 Free Software Foundation, Inc.
+   Copyright (C) 1997, 1998, 1999 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
