a5055ecd30cd7609dcb4f11654a7524beb45e647
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-10-09 21:06:03 +0000

    * cp-tree.h (make_rtl_for_local_static): New function. 	* decl.c (make_rtl_for_nonlocal_decl): Move code to create RTL for 	local statics ... 	(make_rtl_for_local_static): Here. 	* semantics.c (expand_stmt): Use make_rtl_for_local_static.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@29879 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.other/static10.C
@@ -0,0 +1,16 @@
+// Build don't link:
+// Origin: Ulrich Drepper <drepper@cygnus.com>
+
+struct st
+{
+  int a;
+};
+
+int
+foo (int a)
+{
+  static const st i = { 0 };
+
+  if (i.a == a)
+    return 0;
+}
