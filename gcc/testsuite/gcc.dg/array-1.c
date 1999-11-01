d4202c992b1b9c039556c00d40de03add0be7bec
oliva <oliva@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-11-01 05:18:17 +0000

    * gcc.dg/array-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@30309 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/array-1.c
@@ -0,0 +1,15 @@
+/* Simplified from ncurses 5.0's pad.c
+   by Alexandre Oliva <oliva@lsd.ic.unicamp.br>
+
+   Copyright (C) 1999 Free Software Foundation  */
+
+/* { dg-do compile { xfail alpha-*-* } } */
+
+extern char foo[1];
+char foo[] = "";
+
+int
+bar()
+{
+  return foo[0];
+}
