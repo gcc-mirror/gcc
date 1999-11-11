f185457abf8902bc01032208a56c13cd2c79d40b
Alexandre Oliva <oliva@lsd.ic.unicamp.br>
1999-11-11 14:39:01 +0000

    * template9.C: New test.
    From-SVN: r30489

---
+++ gcc/testsuite/g++.old-deja/g++.oliva/template9.C
@@ -0,0 +1,11 @@
+// Build don't link:
+
+// Copyright (C) 1999 Free Software Foundation
+
+// by Alexandre Oliva <oliva@lsd.ic.unicamp.br>
+// based on bug report by Philippe Bouchard <boucp00@DMI.USherb.CA>
+
+struct foo {
+  template <class>
+  void bar() = 0; // ERROR - invalid initializer - XFAIL *-*-*
+};
