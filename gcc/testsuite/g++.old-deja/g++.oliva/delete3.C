250ba2372cdb135d47cb4a47227d0457fb73ce9a
Alexandre Oliva <oliva@lsd.ic.unicamp.br>
1999-11-21 17:49:59 +0000

    * delete2.C, delete3.C, delete4.C, delete5.C: New tests.
    From-SVN: r30607

---
+++ gcc/testsuite/g++.old-deja/g++.oliva/delete3.C
@@ -0,0 +1,36 @@
+// Copyright (C) 1999 Free Software Foundation
+
+// by Alexandre Oliva <oliva@lsd.ic.unicamp.br>
+
+// Test whether dtors of vbases are called on throw within new[].
+// Variant of delete2.C.
+
+extern "C" void abort();
+extern "C" void exit(int);
+
+struct Foo {
+  static bool first;
+
+  Foo() {
+    if (first)
+      first = false;
+    else
+      throw first;
+  }
+
+  ~Foo() {
+    std::exit(0);
+  }
+};
+
+bool Foo::first = true;
+
+struct Bar : virtual Foo {
+};
+
+int main() {
+  delete [] new Bar[2];
+  std::abort();
+}
+
+
