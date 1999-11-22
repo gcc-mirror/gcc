7cffd12d38191aa05ef9049d80d4714d6e52711a
Nathan Sidwell <nathan@acm.org>
1999-11-22 15:51:14 +0000

    cleanup2.C: New test.
    	* g++.old-deja/g++.eh/cleanup2.C: New test.
    	* g++.old-deja/g++.ext/pretty2.C: New test.
    	* g++.old-deja/g++.ext/pretty3.C: New test.
    	* g++.old-deja/g++.other/debug6.C: New test.
    
    From-SVN: r30615

---
+++ gcc/testsuite/g++.old-deja/g++.eh/cleanup2.C
@@ -0,0 +1,54 @@
+//  Copyright (C) 1999 Free Software Foundation, Inc.
+//  Contributed by Nathan Sidwell 21 Nov 1999 <nathan@acm.org>
+
+// make sure we don't call base dtors, if we failed to call the
+// base ctor due to exception throwing
+
+// execution test - XFAIL *-*-*
+
+#include <stdio.h>
+
+static bool bad = false;
+
+static int thrower ()
+{
+  printf ("in %s\n", __PRETTY_FUNCTION__);
+  throw 0;
+  return 0;
+}
+
+struct X
+{
+  X (int) throw (int);
+  ~X () throw ();
+};
+
+X::X (int) throw (int)
+  {printf ("in ctor X %s\n", __PRETTY_FUNCTION__); bad = true;}
+X::~X () throw ()
+  {printf ("in dtor X %s\n", __PRETTY_FUNCTION__); bad = true;}
+
+struct X1 {};
+struct Y : X
+{
+  Y() throw (int);
+  ~Y() throw ();
+};
+Y::Y() throw (int)
+  : X(thrower ())   // throws, so X::X is never called
+  {printf ("in ctor Y%s\n", __PRETTY_FUNCTION__); bad = true;}
+Y::~Y() throw ()
+  {printf ("in dtor Y%s\n", __PRETTY_FUNCTION__); bad = true;}
+
+int main ()
+{
+  try
+    {
+      Y y;
+    }
+  catch (...)
+    {
+      printf ("caught\n");
+    }
+  return bad;
+}
