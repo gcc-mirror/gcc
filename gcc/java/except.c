9a260e9924e983c75b69431c202c960e8671829b
Mark Mitchell <mark@codesourcery.com>
1999-11-25 02:17:23 +0000

    * except.c (emit_handlers): Zero catch_clauses after emitting them.
    From-SVN: r30657

--- gcc/java/except.c
+++ gcc/java/except.c
@@ -393,6 +393,7 @@ emit_handlers ()
       emit_jump (funcend);
 
       emit_insns (catch_clauses);
+      catch_clauses = NULL_RTX;
       expand_leftover_cleanups ();
 
       emit_label (funcend);
