97c1c80b37a9f61753836c4dd059e9bf4a06f099
Bernd Schmidt <bernds@cygnus.co.uk>
1999-11-29 12:18:56 +0000

    New test: gcc.dg/991129-1.c
    From-SVN: r30703

---
+++ gcc/testsuite/gcc.dg/991129-1.c
@@ -0,0 +1,15 @@
+/* Test against a problem in push_reload.  */
+
+/* { dg-do compile { target i?86-*-* } } */
+/* { dg-options "-O2" } */
+
+unsigned long foo (unsigned long long x, unsigned long y)
+{
+    unsigned long a;
+
+    x += y;
+
+    asm ("" : "=a" (a) : "A" (x), "rm" (y));
+
+    return a;
+}
