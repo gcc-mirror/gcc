763fb6cb2a3852a6aa303d7174ab6c73205af0c6
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-12-10 01:09:59 +0000

    * pa/elf.h (MAX_OFILE_ALIGNMENT): Define.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@30854 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/pa/elf.h
+++ gcc/config/pa/elf.h
@@ -89,3 +89,8 @@ do {  \
        fputs (",ENTRY\n", FILE);					\
      } while (0)
 
+/* Biggest alignment supported by the object file format of this
+   machine.  Use this macro to limit the alignment which can be
+   specified using the `__attribute__ ((aligned (N)))' construct.  If
+   not defined, the default value is `BIGGEST_ALIGNMENT'.  */
+#define MAX_OFILE_ALIGNMENT (32768 * 8)
