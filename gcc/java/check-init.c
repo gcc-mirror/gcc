ea4403dcb07785e81e82c93c9573b3dee637f0d4
green <green@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-12-12 23:19:04 +0000

    1999-12-15  Anthony Green  <green@cygnus.com>
    	* check-init.c (check_init): Take into account both types of
            `throw's when checking for uninitialized variables.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@30879 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/java/check-init.c
+++ gcc/java/check-init.c
@@ -674,7 +674,8 @@ check_init (exp, before)
 
 	for ( ;  x != NULL_TREE;  x = TREE_CHAIN (x))
 	  check_init (TREE_VALUE (x), before);
-	if (func == throw_node)
+	if (func == throw_node[0]
+	    || func == throw_node[1])
 	  goto never_continues;
       }
       break;
