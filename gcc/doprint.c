613c97a563a8a2abf345d97e290236a523b237bf
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-12-13 08:01:44 +0000

    * doprnt.c: Remove incorrect comment closure.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@30887 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/doprint.c
+++ gcc/doprint.c
@@ -16,8 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
- */
-
 #include "config.h"
 #include "system.h"
 #undef _doprnt
