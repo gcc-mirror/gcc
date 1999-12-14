b13d32a8b2b441612b72ef1dc6b7b289a45a5409
Jeffrey A Law <law@cygnus.com>
1999-12-14 06:21:32 +0000

    * gcc.c-torture/compile/991213-2.c: New test.
    From-SVN: r30898

---
+++ gcc/testsuite/gcc.c-torture/compile/991213-2.c
@@ -0,0 +1,15 @@
+typedef long		__kernel_time_t;
+typedef __kernel_time_t		time_t;
+time2(
+    void (* const  (funcp)(time_t)),
+    const long offset, int * const okayp)
+{
+	register int			bits;
+	time_t				t;
+	for (bits = 0, t = 1; t > 0; ++bits, t <<= 1)
+		;
+	t = (t < 0) ? 0 : ((time_t) 1 << bits);
+	for ( ; ; ) {
+		(*funcp)((time_t)&t);
+	}
+}
