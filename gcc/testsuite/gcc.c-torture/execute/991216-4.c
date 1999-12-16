ae88b73e697f679e3747d2a4919c261094fa7764
crux <crux@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-12-16 10:45:50 +0000

    New testcase.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@30977 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/991216-4.c
@@ -0,0 +1,16 @@
+/* Test against a problem with loop reversal.  */
+static void bug(int size, int tries)
+{
+    int i;
+    int num = 0;
+    while (num < size)
+    {
+        for (i = 1; i < tries; i++) num++;
+    }
+}
+
+int main()
+{
+    bug(5, 10);
+    exit (0);
+}
