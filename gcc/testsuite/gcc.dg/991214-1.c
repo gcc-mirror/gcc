5ba280dae8c8d278b77979ff64dacd29d497ebe0
Bernd Schmidt <bernds@cygnus.co.uk>
1999-12-16 10:20:44 +0000

    Fix an older test
    From-SVN: r30975

--- gcc/testsuite/gcc.dg/991214-1.c
+++ gcc/testsuite/gcc.dg/991214-1.c
@@ -1,7 +1,10 @@
 /* { dg-do compile { target i?86-*-* } } */
+/* { dg-options "-O2" } */
 
+/* Test against a problem with the combiner substituting explicit hard reg
+   references when it shouldn't.  */
 int foo (int, int) __attribute__ ((regparm (3)));
 int foo (int x, int y)
 {
-  asm volatile("" : : "d" (x), "r" (y));
+  __asm__ __volatile__("" : : "d" (x), "r" (y));
 }
