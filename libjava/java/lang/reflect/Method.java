8c5764afb6f837b47c43d4c8db90fc4616cc6f81
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-12-20 23:05:21 +0000

    * java/lang/reflect/Modifier.java (STRICT): New constant. 	(isStrict): New method. 	(toString): Added `strict'.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@31040 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/lang/reflect/Method.java
+++ libjava/java/lang/reflect/Method.java
@@ -17,8 +17,7 @@ package java.lang.reflect;
 /* Written using "Java Class Libraries", 2nd edition, ISBN 0-201-31002-3
  * "The Java Language Specification", ISBN 0-201-63451-1
  * plus online API docs for JDK 1.2 beta from http://www.javasoft.com.
- * Status:  Incomplete: needs a private constructor, and
- *          invoke() needs to be finished.
+ * Status:  Incomplete: invoke() needs to be finished.
  */
 
 public final class Method extends AccessibleObject implements Member
