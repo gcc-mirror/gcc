1fa5d7a02c5a84b777d5a429e68dcf9ebdd70b7b
green <green@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-12-20 02:18:11 +0000

    1999-12-19  Anthony Green  <green@cygnus.com>
            * libgcjdata.c: New file.
            * libgcj.spec.in: Use @LIBDATASTARTSPEC@ in startfile.
            * configure: Rebuilt.
            * configure.in (LIBDATASTARTSPEC): Force data with a known name
            into the program.
            * Makefile.in: Rebuilt.
            * Makefile.am: Build libgcjdata.a.
    
            * libgcj.spec.in: Use @FORCELIBGCCSPEC@ in startfile.
            * configure: Rebuilt.
            * configure.in (FORCELIBGCCSPEC): Force important parts of libgcc
            into every program.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@31027 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/libgcjdata.c
@@ -0,0 +1,12 @@
+/* Copyright (C) 1998, 1999  Cygnus Solutions
+
+   This file is part of libgcj.
+
+This software is copyrighted work licensed under the terms of the
+Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
+details.  */
+
+/* Some systems need data_start defined so the GC be built as a shared
+   library.  */
+
+int data_start = 0;
