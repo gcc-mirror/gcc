eda190c54117bdd0ff2fdec02dd71275c51631b3
Martin v. Löwis <loewis@gcc.gnu.org>
1999-12-21 09:49:39 +0000

    New test case.
    From-SVN: r31048

---
+++ gcc/testsuite/gcc.c-torture/execute/991221-1.c
@@ -0,0 +1,13 @@
+int main( void )
+{
+   unsigned long totalsize = 80;
+   unsigned long msize = 64;
+
+   if (sizeof(long) != 4)
+     exit(0);
+   
+   if ( totalsize > (2147483647L   * 2UL + 1)  
+        || (msize != 0 && ((msize - 1) > (2147483647L   * 2UL + 1) )))
+      abort();
+   exit( 0 );
+}
