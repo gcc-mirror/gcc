ecd6d976ecf887a3ef27c7a97af1628beca00c56
oliva <oliva@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-12-22 08:41:27 +0000

    * nameret1.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@31064 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.oliva/nameret1.C
@@ -0,0 +1,14 @@
+// Copyright (C) 1999 Free Software Foundation
+
+// by Alexandre Oliva <oliva@lsd.ic.unicamp.br>
+
+// distilled from libg++'s Rational.cc
+
+// Special g++ Options:
+// crash test - XFAIL sparc64*-*-* sparcv9*-*-* alpha*-*-*
+
+inline int bar () return r {}
+
+int foo () {
+  return bar ();
+}
