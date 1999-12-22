99f3f6cc7e240299e3f82f1b751ee8592adcf4d3
Alexandre Oliva <oliva@lsd.ic.unicamp.br>
1999-12-22 09:21:44 +0000

    * nameret2.C: New test.
    From-SVN: r31069

---
+++ gcc/testsuite/g++.old-deja/g++.oliva/nameret2.C
@@ -0,0 +1,15 @@
+// Copyright (C) 1999 Free Software Foundation
+
+// by Alexandre Oliva <oliva@lsd.ic.unicamp.br>
+
+// distilled from libg++'s Integer.cc
+
+// Special g++ Options:
+// crash test - XFAIL *-*-*
+
+inline int bar () return r {}
+
+int& foo (int& x) {
+  bar ();
+  return x;
+}
