81b25f804227f1d011a9e0d77e57738793b3dec4
Alexandre Oliva <oliva@gcc.gnu.org>
1999-12-22 09:25:27 +0000

    oops, not an ICE, just a bogus error
    From-SVN: r31071

--- gcc/testsuite/g++.old-deja/g++.oliva/nameret2.C
+++ gcc/testsuite/g++.old-deja/g++.oliva/nameret2.C
@@ -5,11 +5,10 @@
 // distilled from libg++'s Integer.cc
 
 // Special g++ Options: -O1
-// crash test - XFAIL *-*-*
 
 inline int bar () return r {}
 
 int& foo (int& x) {
   bar ();
   return x;
-}
+} // gets bogus error - XFAIL *-*-*
