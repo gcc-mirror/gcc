7a555f64433fa8b31044ac5c95206aec4446a68c
Martin v. Löwis <loewis@gcc.gnu.org>
1999-12-27 11:36:24 +0000

    New test case
    From-SVN: r31098

---
+++ gcc/testsuite/gcc.c-torture/execute/991227-1.c
@@ -0,0 +1,11 @@
+char* doit(int flag)
+{
+  return 1 + (flag ? "\0wrong\n" : "\0right\n");
+}
+int main()
+{
+  char *result = doit(0);
+  if (*result == 'r' && result[1] == 'i')
+    exit(0);
+  abort();
+}
