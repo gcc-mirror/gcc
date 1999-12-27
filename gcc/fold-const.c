71145810908ff31ea6c2b98c0964e661d5b91c66
Martin v. Löwis <loewis@informatik.hu-berlin.de>
1999-12-27 11:35:07 +0000

    fold-const.c (operand_equal_p): Use memcmp to compare string constants.
    	* fold-const.c (operand_equal_p): Use memcmp to compare string
    	constants.
    
    From-SVN: r31097

--- gcc/fold-const.c
+++ gcc/fold-const.c
@@ -2250,7 +2250,7 @@ operand_equal_p (arg0, arg1, only_const)
 
       case STRING_CST:
 	return (TREE_STRING_LENGTH (arg0) == TREE_STRING_LENGTH (arg1)
-		&& ! strncmp (TREE_STRING_POINTER (arg0),
+		&& ! memcmp (TREE_STRING_POINTER (arg0),
 			      TREE_STRING_POINTER (arg1),
 			      TREE_STRING_LENGTH (arg0)));
 
