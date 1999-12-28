b09a61f45e124a138260308c9629ff3bd89f75b5
Jeff Law <law@gcc.gnu.org>
1999-12-28 01:45:07 -0700

    Daily bump.
    From-SVN: r31106

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-const char *ffe_version_string = "0.5.25 19991227 (experimental)";
+const char *ffe_version_string = "0.5.25 19991228 (experimental)";
--- libf2c/libF77/Version.c
+++ libf2c/libF77/Version.c
@@ -3,7 +3,7 @@ static char junk[] = "\n@(#)LIBF77 VERSION 19990503\n";
 /*
 */
 
-char __G77_LIBF77_VERSION__[] = "0.5.25 19991227 (prerelease)";
+char __G77_LIBF77_VERSION__[] = "0.5.25 19991228 (prerelease)";
 
 /*
 2.00	11 June 1980.  File version.c added to library.
