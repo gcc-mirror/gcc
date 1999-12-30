b8f4daeb4e24eb42df7dbb799ef8663e4dc6ecea
crux <crux@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-12-30 12:40:53 +0000

    New testcase
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@31137 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/991230-1.c
@@ -0,0 +1,20 @@
+/* { dg-do run { target i?86-*-* } } */
+/* { dg-options "-O -ffast-math -mcpu=i486" } */
+
+/* Test that floating point greater-than tests are compiled correctly with
+   -ffast-math.  */
+static int gt (double a, double b)
+{
+  if (a > b)
+    return 4;
+  return 0;
+}
+
+static double zero = 0.0;
+
+int main ()
+{
+  if (gt (zero, zero))
+    abort ();
+  return 0;
+}
