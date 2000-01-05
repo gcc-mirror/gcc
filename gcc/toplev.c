b23a1cac4095fbb6341239621b2f076ce697f782
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-01-05 06:29:24 +0000

    Update copyright date.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@31226 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/toplev.c
+++ gcc/toplev.c
@@ -1,5 +1,5 @@
 /* Top level of GNU C compiler
-   Copyright (C) 1987, 88, 89, 92-98, 1999 Free Software Foundation, Inc.
+   Copyright (C) 1987, 88, 89, 92-98, 1999, 2000 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
