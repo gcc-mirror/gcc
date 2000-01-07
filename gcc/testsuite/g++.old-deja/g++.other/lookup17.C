fbda03059d519370ebc553bdf56e41309659c53a
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-01-07 05:57:58 +0000

    fix
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@31268 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.other/lookup17.C
+++ gcc/testsuite/g++.old-deja/g++.other/lookup17.C
@@ -1,13 +1,14 @@
+// Bug: t->B is resolved to the type instead of the field.
+
 struct A {
   struct B { } *B;
   int i, j, k, l, m;
 };
 
-struct A *t;
+struct A a;
 
 int
 main ()
 {
-  void *p = t
-    ->B;
+  void *p = a.B;
 }
