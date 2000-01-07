85367bc5e30068a84b02705c0f2a87de06d5c2d0
dje <dje@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-01-07 22:30:22 +0000

    - Update copyright, oops.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@31279 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/collect2.c
+++ gcc/collect2.c
@@ -1,6 +1,6 @@
 /* Collect static initialization info into data structures that can be
    traversed by C++ initialization and finalization routines.
-   Copyright (C) 1992, 93-98, 1999 Free Software Foundation, Inc.
+   Copyright (C) 1992, 93-99, 2000 Free Software Foundation, Inc.
    Contributed by Chris Smith (csmith@convex.com).
    Heavily modified by Michael Meissner (meissner@cygnus.com),
    Per Bothner (bothner@cygnus.com), and John Gilmore (gnu@cygnus.com).
