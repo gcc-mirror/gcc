69d4f56db177ddbacc02c21a6b69735fa70f7c81
Alexandre Oliva <oliva@lsd.ic.unicamp.br>
2000-01-08 19:53:48 +0000

    * gcc.dg/20000108-1.c: New test.
    From-SVN: r31286

---
+++ gcc/testsuite/gcc.dg/20000108-1.c
@@ -0,0 +1,17 @@
+/* Copyright (C) 2000  Free Software Foundation.
+
+   by Alexandre Oliva  <oliva@lsd.ic.unicamp.br>  */
+
+/* { dg-do run } */
+/* { dg-options "-O3" } */
+
+void foo () {} /* unused, but essential to trigger the bug */
+
+int main () {
+  int i;
+  /* use asms to prevent optimizations */
+  /* i = -1; */ asm ("" : "=r" (i) : "0" (-1));
+  /* i =  1; */ asm ("" : "=r" (i) : "0" (i ? 1 : 2));
+  if (i != 1)
+    abort();
+}
