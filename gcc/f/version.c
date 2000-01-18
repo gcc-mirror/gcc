c399e76da6fe124f13dc45c5d85df8138bff7284
Jeff Law <law@gcc.gnu.org>
2000-01-18 01:45:07 -0700

    Daily bump.
    From-SVN: r31484

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-const char *ffe_version_string = "0.5.25 20000117 (experimental)";
+const char *ffe_version_string = "0.5.25 20000118 (experimental)";
--- gcc/version.c
+++ gcc/version.c
@@ -1,1 +1,1 @@
-char *version_string = "2.96 20000117 (experimental)";
+char *version_string = "2.96 20000118 (experimental)";
