f7cf9dd67dc1865b6ccfe30786edd9ffa1b7a80a
Jason Merrill <jason@gcc.gnu.org>
2000-01-25 12:15:58 -0500

    new
    From-SVN: r31612

---
+++ gcc/testsuite/g++.old-deja/g++.pt/dynarray.C
@@ -0,0 +1,21 @@
+// Build don't link:
+// Origin: Theo Papadopoulo <Theodore.Papadopoulo@sophia.inria.fr>
+
+inline const unsigned& f(unsigned const& a) {
+        return a;
+}
+
+template <class T>
+void
+g(const unsigned n)
+{
+        double D[f(n)];
+}
+
+template <class T,class U>
+void g(unsigned const int) { }
+
+int main()
+{
+        g<double>(18);
+}
