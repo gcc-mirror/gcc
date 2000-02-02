cf8e13bbc889b696915cef8dbdce555d8f1617a6
Krister Walfridsson <cato@df.lth.se>
2000-02-03 00:27:03 +0100

    * i386/netbsd.h (INT_ASM_OP): Define.
    From-SVN: r31766

--- gcc/config/i386/netbsd.h
+++ gcc/config/i386/netbsd.h
@@ -86,3 +86,7 @@
    and initialization stuff better.  */
 #define DWARF2_UNWIND_INFO 0
 
+/* This is the pseudo-op used to generate a 32-bit word of data with a
+   specific value in some section.  */
+
+#define INT_ASM_OP ".long"
