cc6c6c234f50d95507baf0fb77e67cb90dee3905
Alan Modra <alan@SPRI.Levels.UniSA.Edu.Au>
2000-02-06 05:39:32 +0000

    Alan Modra <alan@SPRI.Levels.UniSA.Edu.Au>
    	Alan Modra  <alan@SPRI.Levels.UniSA.Edu.Au>
            * gcc.c-torture/execute/20000205-1.c: New.
    
    From-SVN: r31815

---
+++ gcc/testsuite/gcc.c-torture/execute/20000205-1.c
@@ -0,0 +1,17 @@
+static int f (int a)
+{
+  if (a == 0)
+    return 0;
+  do
+    if (a & 128)
+      return 1;
+  while (f (0));
+  return 0;
+}
+
+int main(void)
+{
+  if (f (~128))
+    abort ();
+  exit (0);
+}
