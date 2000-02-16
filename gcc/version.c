4746ee266f25b302b0b2976096106b00da13aeee
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2000-02-16 16:22:47 +0000

    configure (gcc_version): When setting, narrow search to lines containing `version_string'.
    	* configure (gcc_version): When setting, narrow search to
    	lines containing `version_string'.
    
    gcc:
    	* version.c: Include gansidecl.h and version.h.
    
    	* version.h: Wrap entire file in macro __GCC_VERSION_H__.
    
    	* configure.in (gcc_version): When setting, narrow search to
    	lines containing `version_string'.
    
    	* Makefile.in (mainversion): Likewise.
    	(GCC_H): New variable.
    	(gcc.h): Delete target.
    	(gcc.o, gccspec.o, cppspec.o): Depend on $(GCC_H), not gcc.h.
    	(version.o): Depend on version.h.
    	(dbxout.o): Don't depend on gcc.h.
    
    gcc/cp:
    	* Make-lang.in (g++spec.o): Depend on $(GCC_H), not gcc.h.
    
    gcc/f:
    	* Make-lang.in (g77spec.o): Depend on $(GCC_H), not gcc.h.
    
    gcc/java:
    	* Make-lang.in (jvspec.o): Depend on $(GCC_H), not gcc.h.
    
    libf2c:
    	* configure.in (gcc_version): When setting, narrow search to
    	lines containing `version_string'.
    
    From-SVN: r32009

--- gcc/version.c
+++ gcc/version.c
@@ -1,1 +1,4 @@
+#include "gansidecl.h"
+#include "version.h"
+
 const char *const version_string = "2.96 20000216 (experimental)";
--- gcc/version.h
+++ gcc/version.h
@@ -1,1 +1,4 @@
+#ifndef __GCC_VERSION_H__
+#define __GCC_VERSION_H__
 extern const char *const version_string;
+#endif /* __GCC_VERSION_H__ */
