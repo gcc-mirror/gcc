430469de49a4681e6800578d1744e9a1b6fb9d07
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-02-20 08:45:07 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32077 138bc75d-0d04-0410-961f-82ee72b054a4

--- libf2c/libU77/Version.c
+++ libf2c/libU77/Version.c
@@ -1,6 +1,6 @@
 static char junk[] = "\n@(#) LIBU77 VERSION 19980709\n";
 
-char __G77_LIBU77_VERSION__[] = "0.5.25 20000219 (prerelease)";
+char __G77_LIBU77_VERSION__[] = "0.5.25 20000220 (prerelease)";
 
 #include <stdio.h>
 
