ee6436e0734d0d42d3584376d400e9d33660c37b
sirl <sirl@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-02-23 23:16:36 +0000

    * execute/va-arg-13.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32127 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/va-arg-13.c
@@ -0,0 +1,38 @@
+/* derived from mozilla source code */
+
+#include <stdarg.h>
+
+typedef struct {
+  void *stream;
+  va_list ap;
+  int nChar;  
+} ScanfState;
+
+void dummy (va_list vap)
+{
+  if (va_arg (vap, int) != 1234) abort();
+  return;
+}
+
+void test (int fmt, ...)
+{
+  ScanfState state, *statep;
+
+  statep = &state;
+
+  va_start (statep->ap, fmt);
+  dummy (statep->ap);
+  va_end (statep->ap);
+  
+  va_start (state.ap, fmt);
+  dummy (state.ap);
+  va_end (state.ap);
+  
+  return;
+}
+
+int main (void)
+{
+  test (456, 1234);
+  exit (0);
+}
