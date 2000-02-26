6ffae6e79072c716d8c3b160574876bed032b209
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-02-26 13:51:58 +0000

    Fix copyrights
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32177 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ch/grant.c
+++ gcc/ch/grant.c
@@ -1,5 +1,6 @@
 /* Implement grant-file output & seize-file input for CHILL.
-   Copyright (C) 1992, 93-96, 98, 99, 2000 Free Software Foundation, Inc.
+   Copyright (C) 1992, 1993, 1994, 1995, 1996, 1998,
+   1999, 2000 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
