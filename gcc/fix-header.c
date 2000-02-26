3b708058ed6d316edf86d18e0bf90eb0abf7499d
Jeff Law <law@gcc.gnu.org>
2000-02-25 23:23:30 -0700

    Fix copyrights.
    From-SVN: r32175

--- gcc/fix-header.c
+++ gcc/fix-header.c
@@ -1,5 +1,6 @@
 /* fix-header.c - Make C header file suitable for C++.
-   Copyright (C) 1993, 94-99, 2000 Free Software Foundation, Inc.
+   Copyright (C) 1993, 1994, 1995, 1996, 1997, 1998,
+   1999, 2000 Free Software Foundation, Inc.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the
