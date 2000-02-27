eaf4a9247025a00648caf54e0d83667b9e010025
zack <zack@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-02-27 17:17:01 +0000

    Test for proper handling of 8-bit characters in strings. Reported by Per Hedbor <per@idonex.se>.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32221 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20000227-1.c
@@ -0,0 +1,15 @@
+static const unsigned char f[] = "\0\377";
+static const unsigned char g[] = "\0ÿ";
+
+int main(void)
+{
+  if (sizeof f != 3 || sizeof g != 3)
+    abort ();
+  if (f[0] != g[0])
+    abort ();
+  if (f[1] != g[1])
+    abort ();
+  if (f[2] != g[2])
+    abort ();
+  return 0;
+}
