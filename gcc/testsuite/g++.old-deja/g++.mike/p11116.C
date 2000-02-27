199e398cd206981ca7a1b4ce60a16e86aaf005b4
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-02-27 21:30:14 +0000

    p11116.C
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32224 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.mike/p11116.C
+++ gcc/testsuite/g++.old-deja/g++.mike/p11116.C
@@ -2,6 +2,12 @@
 // Special g++ Options: -Wno-pmf-conversions
 // prms-id: 11116
 
+#if defined (__GXX_ABI_VERSION) && __GXX_ABI_VERSION >= 100
+// This test tests the rather strange property afforded one by G++ to
+// peek inside a pointer-to-member, as if it were a structure.  We
+// probably shouldn't allow that.  In any case, under the new ABI,
+// the fields don't have the same names.
+#else
 class Bar {
 public:
   int f(int a) { val = a; return val; }
@@ -20,3 +26,4 @@ void foo() {
   p = (int (*)(void*,int))((void (*)())((bp).__pfn_or_delta2.__pfn));
   a = (*p)(&bar, 4);
 }
+#endif
