e7fcfdf7f5fe1c4b10f69aaf615d2bcdc9bd8349
Chandra Chavva <cchavva@gcc.gnu.org>
2000-03-01 17:42:36 -0500

    *** empty log message ***
    From-SVN: r32288

---
+++ gcc/testsuite/gcc.c-torture/execute/20000224-1.c
@@ -0,0 +1,28 @@
+int loop_1 = 100;
+int loop_2 = 7;
+int flag = 0;
+
+int test (void)
+{
+    int i;
+    int counter  = 0;
+
+    while (loop_1 > counter) {
+        if (flag & 1) {
+            for (i = 0; i < loop_2; i++) {
+                counter++;
+            }
+        }
+        flag++;
+    }
+    return 1;
+}
+
+int main()
+{
+    if (test ())
+      abort ();
+    
+    exit (0);
+}
+
