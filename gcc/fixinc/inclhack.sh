90cca551dfcdf63f4e63462acea680dbd5005f8b
korbb <korbb@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-03-01 20:43:02 +0000

    remove unused symlinks from generated include dir
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32284 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fixinc/inclhack.sh
+++ gcc/fixinc/inclhack.sh
@@ -3473,8 +3473,26 @@ then echo 'Cleaning up unneeded directories:' ; fi
 cd $LIB
 all_dirs=`find . -type d \! -name '.' -print | sort -r`
 for file in $all_dirs; do
-  rmdir $LIB/$file > /dev/null 2>&1
-done
+  if rmdir $LIB/$file > /dev/null
+  then
+    test $VERBOSE -gt 3 && echo "  removed $file"
+  fi
+done 2> /dev/null
+
+test $VERBOSE -gt 2 && echo "Removing unused symlinks"
+
+all_dirs=`find . -type l -print`
+for file in $all_dirs
+do
+  if ls -lLd $file > /dev/null
+  then :
+  else rm -f $file
+       test $VERBOSE -gt 3 && echo "  removed $file"
+       rmdir `dirname $file` > /dev/null && \
+         test $VERBOSE -gt 3 && \
+         echo "  removed `dirname $file`"
+  fi
+done 2> /dev/null
 
 if test $VERBOSE -gt 0
 then echo fixincludes is done ; fi
