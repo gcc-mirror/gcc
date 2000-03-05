18ca9ce7234bb110f3a8aaaae334955cf49d17e5
Anthony Green <green@redhat.com>
2000-03-05 09:07:27 +0000

    re GNATS gcj/129 (Static array length access bug in gcj)
    	* libjava.compile/PR129_B.java: New file.
    	* libjava.compile/support/PR129_A.java: New file.
    	For PR gcj/129.
    
    From-SVN: r32340

---
+++ libjava/testsuite/libjava.compile/PR129_B.java
@@ -0,0 +1,9 @@
+import support.PR129_A;
+
+public class PR129_B { 
+  public static void main ( String[] args ) { 
+    int length = PR129_A.strArr.length; 
+    System.out.println ( "Array length: " + length ); 
+  } 
+} 
+
