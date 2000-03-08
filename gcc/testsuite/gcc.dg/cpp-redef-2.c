ee499b6bc155f53c699cf6b0337d9630f194ab99
zack <zack@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-03-08 20:38:06 +0000

    cpp-mi.c: Fix switches. cpp-redef-2.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32431 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/cpp-redef-2.c
@@ -0,0 +1,27 @@
+/* Test for redefining macros with significant differences.  */
+
+/* { dg-do preprocess }
+   { dg-options "-pedantic -Wall" } */
+
+#define mac(a, b) (a) + (b)
+#define mac(a, b) (a) * (b)
+#define mac(a, b) (a) * (x)
+
+#define ro(x) foo x bar
+#define ro(x, b) foo x bar
+
+#define va(a...) a
+#define va(...) __VA_ARGS__
+
+/* { dg-warning "redefined" "redef mac"     { target *-*-* } 7  }
+   { dg-warning "redefined" "redef mac"     { target *-*-* } 8  }
+   { dg-warning "redefined" "redef ro"      { target *-*-* } 11 }
+   { dg-warning "redefined" "redef va"      { target *-*-* } 14 }
+
+   { dg-warning "previous"  "prev def mac"  { target *-*-* } 6  }
+   { dg-warning "previous"  "prev def mac"  { target *-*-* } 7  }
+   { dg-warning "previous"  "prev def ro"   { target *-*-* } 10 }
+   { dg-warning "previous"  "prev def va"   { target *-*-* } 13 }
+
+   { dg-warning "varargs"   "named varargs" { target *-*-* } 13 }
+   { dg-warning "varargs"   "anon varargs"  { target *-*-* } 14 } */ 
