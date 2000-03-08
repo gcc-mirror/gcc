98eddaf72b64cf32c7f0b71bcb22bd245aa55f83
Nathan Sidwell <nathan@codesourcery.com>
2000-03-08 11:22:15 +0000

    misc12.C: Removed.
    	* g++.old-deja/g++.brendan/misc12.C: Removed.
    	* g++.old-deja/g++.pt/memtemp77.C: Constify.
    	* g++.old-deja/g++.ext/pretty4.C: New test.
    
    From-SVN: r32419

--- gcc/testsuite/g++.old-deja/g++.pt/memtemp77.C
+++ gcc/testsuite/g++.old-deja/g++.pt/memtemp77.C
@@ -4,21 +4,21 @@ template <class T>
 struct S3
 {
   template <class U>
-  static char* h(U);
+  static const char* h(U);
 };
 
 template <>
 template <>
-char* S3<double>::h(int) { return __PRETTY_FUNCTION__; }
+const char* S3<double>::h(int) { return __PRETTY_FUNCTION__; }
 
 template <>
 template <>
-char* S3<char>::h(int) { return __PRETTY_FUNCTION__; }
+const char* S3<char>::h(int) { return __PRETTY_FUNCTION__; }
 
 int main()
 {
   if (strcmp (S3<double>::h(7), 
-	      "char *S3<T>::h (U) [with U = int, T = double]") == 0)
+	      "const char *S3<T>::h (U) [with U = int, T = double]") == 0)
     return 0;
   else 
     return 1;
