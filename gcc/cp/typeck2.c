bfa397b1d42921f25b98e237f782f0e835dbe119
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-03-08 15:56:16 +0000

    * typeck2.c (process_init_constructor): Set TREE_PURPOSE of         array CONSTRUCTOR elements.  Don't use expr_tree_cons.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32422 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/typeck2.c
+++ gcc/cp/typeck2.c
@@ -811,7 +811,7 @@ process_init_constructor (type, init, elts)
 	    allconstant = 0;
 	  else if (! initializer_constant_valid_p (next1, TREE_TYPE (next1)))
 	    allsimple = 0;
-	  members = expr_tree_cons (NULL_TREE, next1, members);
+	  members = tree_cons (size_int (i), next1, members);
 	}
     }
   else if (TREE_CODE (type) == RECORD_TYPE)
@@ -844,7 +844,7 @@ process_init_constructor (type, init, elts)
 	{
 	  if (! DECL_NAME (field) && DECL_C_BIT_FIELD (field))
 	    {
-	      members = expr_tree_cons (field, integer_zero_node, members);
+	      members = tree_cons (field, integer_zero_node, members);
 	      continue;
 	    }
 
@@ -920,7 +920,7 @@ process_init_constructor (type, init, elts)
 	    allconstant = 0;
 	  else if (! initializer_constant_valid_p (next1, TREE_TYPE (next1)))
 	    allsimple = 0;
-	  members = expr_tree_cons (field, next1, members);
+	  members = tree_cons (field, next1, members);
 	}
     }
   else if (TREE_CODE (type) == UNION_TYPE
@@ -991,7 +991,7 @@ process_init_constructor (type, init, elts)
 	allconstant = 0;
       else if (initializer_constant_valid_p (next1, TREE_TYPE (next1)) == 0)
 	allsimple = 0;
-      members = expr_tree_cons (field, next1, members);
+      members = tree_cons (field, next1, members);
     }
 
   /* If arguments were specified as a list, just remove the ones we used.  */
