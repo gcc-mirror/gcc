8ce5ef1923e1bc826b4159ecee825db5fc9b8cd9
Nathan Sidwell <nathan@codesourcery.com>
2000-03-08 10:26:40 +0000

    * g++.old-deja/g++.other/string2.C: New test.
    From-SVN: r32417

---
+++ gcc/testsuite/g++.old-deja/g++.other/string2.C
@@ -0,0 +1,17 @@
+// Copyright (C) 2000 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 6 Mar 2000 <nathan@codesourcery.com>
+
+// A char const array should never be confused for a string literal.
+
+int main ()
+{
+  static const char ary[] = "wibble";
+  void const *ptr = 0;
+  
+  ptr = ary;
+  if (ptr == "wibble")
+    return 1;
+  if (ptr != ary)
+    return 1;
+  return 0;
+}
