aa77ac5f237e2b0a646631ec7088b8c7bdd14fd3
Zack Weinberg <zack@gcc.gnu.org>
2000-03-09 23:04:50 +0000

    Add another case
    From-SVN: r32458

--- gcc/testsuite/gcc.dg/cpp-redef-2.c
+++ gcc/testsuite/gcc.dg/cpp-redef-2.c
@@ -6,6 +6,7 @@
 #define mac(a, b) (a) + (b)
 #define mac(a, b) (a) * (b)
 #define mac(a, b) (a) * (x)
+#define mac(a, g) (a) * (x)
 
 #define ro(x) foo x bar
 #define ro(x, b) foo x bar
@@ -15,13 +16,15 @@
 
 /* { dg-warning "redefined" "redef mac"     { target *-*-* } 7  }
    { dg-warning "redefined" "redef mac"     { target *-*-* } 8  }
-   { dg-warning "redefined" "redef ro"      { target *-*-* } 11 }
-   { dg-warning "redefined" "redef va"      { target *-*-* } 14 }
+   { dg-warning "redefined" "redef mac"     { target *-*-* } 9  }
+   { dg-warning "redefined" "redef ro"      { target *-*-* } 12 }
+   { dg-warning "redefined" "redef va"      { target *-*-* } 15 }
 
    { dg-warning "previous"  "prev def mac"  { target *-*-* } 6  }
    { dg-warning "previous"  "prev def mac"  { target *-*-* } 7  }
-   { dg-warning "previous"  "prev def ro"   { target *-*-* } 10 }
-   { dg-warning "previous"  "prev def va"   { target *-*-* } 13 }
+   { dg-warning "previous"  "prev def mac"  { target *-*-* } 8  }
+   { dg-warning "previous"  "prev def ro"   { target *-*-* } 11 }
+   { dg-warning "previous"  "prev def va"   { target *-*-* } 14 }
 
-   { dg-warning "varargs"   "named varargs" { target *-*-* } 13 }
-   { dg-warning "varargs"   "anon varargs"  { target *-*-* } 14 } */ 
+   { dg-warning "varargs"   "named varargs" { target *-*-* } 14 }
+   { dg-warning "varargs"   "anon varargs"  { target *-*-* } 15 } */ 
