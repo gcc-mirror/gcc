563ee39fb0fac2ea7d4817926c6bf0ae726611b6
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-03-09 08:45:07 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32444 138bc75d-0d04-0410-961f-82ee72b054a4

--- libf2c/libI77/Version.c
+++ libf2c/libI77/Version.c
@@ -3,7 +3,7 @@ static char junk[] = "\n@(#) LIBI77 VERSION pjw,dmg-mods 19990627\n";
 /*
 */
 
-char __G77_LIBI77_VERSION__[] = "0.5.25 20000308 (prerelease)";
+char __G77_LIBI77_VERSION__[] = "0.5.25 20000309 (prerelease)";
 
 /*
 2.01	$ format added
