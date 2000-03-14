eb61c012cf91e52c66be45f649b7592f968ffb42
Jeffrey A Law <law@cygnus.com>
2000-03-14 16:10:27 +0000

    * gcc.c-torture/execute/2000314-1.c: New test.
    From-SVN: r32530

---
+++ gcc/testsuite/gcc.c-torture/execute/20000314-1.c
@@ -0,0 +1,17 @@
+int main ()
+{
+  long winds = 0;
+
+  while (winds != 0)
+    {
+      if (*(char *) winds)
+	break;
+    }
+
+  if (winds == 0 || winds != 0 || *(char *) winds)
+    exit (0);
+
+  abort ();
+}
+
+
