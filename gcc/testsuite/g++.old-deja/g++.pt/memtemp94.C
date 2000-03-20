6962d2ec2751291fb9f989dcb52c3e8148d06485
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-03-20 21:37:40 +0000

    new
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32652 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.pt/memtemp94.C
@@ -0,0 +1,9 @@
+// Build don't link:
+// Origin: Theodore Papadopoulo <Theodore.Papadopoulo@sophia.inria.fr>
+
+struct H {
+  template <class T> void k() const { }
+  typedef void (H::*pmf)() const;
+
+  pmf f() const { return &H::k<int>; }
+};
