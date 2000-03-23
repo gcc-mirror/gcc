f2492d2ebbd862ae86b9c2cf8e61177359aa5e94
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-03-23 09:23:16 +0000

    * g++.old-deja/g++.ext/array4.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32703 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.ext/array4.C
@@ -0,0 +1,32 @@
+// Build don't link:
+
+// Special g++ Options:
+
+// Copyright (C) 2000 Free Software Foundation, Inc.
+// Contributed by Mark Mitchell 19 Mar 2000 <mark@codesourcery.com>
+//                Nathan Sidwell 19 Mar 2000 <nathan@codesourcery.com>
+
+// [nathan] We have a zero sized array extension, and (unfortunately) allow it
+// to be the sole member of a struct (rather than the trailing member of a
+// non-empty struct as C99 is/will allow). Such a type will have a size of
+// zero. Internally, we also use a TYPE_SIZE of zero to indicate an
+// incompletable type. We must keep the two zeroes distinct -- arrgh!
+
+struct A
+{
+  int m[0];
+};
+
+void foo ()
+{
+  A a;
+}
+
+template <class T>
+struct S
+{
+  int x[0];
+};
+
+template struct S<int>;
+
