5f11c812702cfb08dca2003f4cd43a2219a08fc3
Tom Tromey <tromey@cygnus.com>
2000-03-27 19:37:16 +0000

    test_long.out: New file.
    	* libjava.lang/test_long.out: New file.
    	* libjava.lang/test_long.java: New file.
    
    From-SVN: r32769

---
+++ libjava/testsuite/libjava.lang/test_long.java
@@ -0,0 +1,13 @@
+class test_long {
+
+  public static void main (String[] arg)
+  {
+    long a, b, c, d, e;
+    e = 234L;
+    d = 0x8888888888888888L;
+    a = 0x0123456789ABCDEFL;
+    b = 0xFEDCBA9876543210L;
+    c = 0xF096A5B4C3B2E187L;
+    System.out.println (a+" "+b+" "+c+" "+d+" "+e);
+  }
+}
