5ae419e6a7568e7fb0a9a99cd214505a38accf72
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-03-27 11:03:26 +0000

    * libgcc2.h: Use MIN_UNITS_PER_WORD, not UNITS_PER_WORD.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32762 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/libgcc2.h
+++ gcc/libgcc2.h
@@ -87,11 +87,11 @@ typedef		 int QItype	__attribute__ ((mode (QI)));
 typedef unsigned int UQItype	__attribute__ ((mode (QI)));
 typedef		 int HItype	__attribute__ ((mode (HI)));
 typedef unsigned int UHItype	__attribute__ ((mode (HI)));
-#if UNITS_PER_WORD > 1
+#if MIN_UNITS_PER_WORD > 1
 /* These typedefs are usually forbidden on dsp's with UNITS_PER_WORD 1 */
 typedef 	 int SItype	__attribute__ ((mode (SI)));
 typedef unsigned int USItype	__attribute__ ((mode (SI)));
-#if UNITS_PER_WORD > 2
+#if MIN_UNITS_PER_WORD > 2
 /* These typedefs are usually forbidden on archs with UNITS_PER_WORD 2 */
 typedef		 int DItype	__attribute__ ((mode (DI)));
 typedef unsigned int UDItype	__attribute__ ((mode (DI)));
@@ -149,7 +149,7 @@ typedef int word_type __attribute__ ((mode (__word__)));
 #define float bogus_type
 #define double bogus_type
 
-#if UNITS_PER_WORD > 2
+#if MIN_UNITS_PER_WORD > 2
 #define W_TYPE_SIZE (4 * BITS_PER_UNIT)
 #define Wtype	SItype
 #define UWtype	USItype
@@ -159,7 +159,7 @@ typedef int word_type __attribute__ ((mode (__word__)));
 #define UDWtype	UDItype
 #define __NW(a,b)	__ ## a ## si ## b
 #define __NDW(a,b)	__ ## a ## di ## b
-#elif UNITS_PER_WORD > 1
+#elif MIN_UNITS_PER_WORD > 1
 #define W_TYPE_SIZE (2 * BITS_PER_UNIT)
 #define Wtype	HItype
 #define UWtype	UHItype
