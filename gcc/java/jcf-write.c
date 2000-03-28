0981a85961c671c04e797252ad30949a5037b1ce
Alexandre Petit-Bianco <apbianco@cygnus.com>
2000-03-28 08:33:45 +0000

    [multiple changes]
    2000-03-28  Alexandre Petit-Bianco  <apbianco@cygnus.com>
    
    	* parse-scan.y (pop_class_context): Reset `inner_qualifier_length'
    	to 0 when it reaches -1.
    
    2000-03-27  Alexandre Petit-Bianco  <apbianco@cygnus.com>
    
    	* jcf-parse.c (get_constant): Properly cast `num' during the
    	invocation of `add_double call'.
    	* jcf-write.c (push_long_const): Properly cast `lo' before
    	comparing it to short bounds.
    	* parse-scan.y (interface_declaration:): Rule re-arrange so that
    	`interface_body:' is reduced after the current interface is
    	pushed.
    
    2000-02-28  Alexandre Petit-Bianco  <apbianco@cygnus.com>
    
    	* parse-scan.y (inner_qualifier, inner_qualifier_length): New
    	static globals.
    	(push_class_context, pop_class_context): New function.
    	(class_body:): Call pop_class_context.
    	(interface_body:): Likewise.
    	(INNER_QUALIFIER): New macro.
    	(report_class_declaration): Changed output format and use
    	INNER_QUALIFIER. Call push_class_context.
    
    From-SVN: r32779

--- gcc/java/jcf-write.c
+++ gcc/java/jcf-write.c
@@ -845,7 +845,8 @@ push_long_const (lo, hi, state)
       RESERVE(1);
       OP1(OPCODE_lconst_0 + lo);
     }
-  else if ((hi == 0 && lo < 32768) || (hi == -1 && lo >= -32768))
+  else if ((hi == 0 && (jword)(lo  & 0xFFFFFFFF) < 32768) 
+          || (hi == -1 && (jword)(lo & 0xFFFFFFFF) >= -32768))
       {
         push_int_const (lo, state);
         RESERVE (1);
