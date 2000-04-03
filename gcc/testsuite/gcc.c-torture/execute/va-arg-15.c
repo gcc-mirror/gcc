cbdbfaecebeecf2aec95a52634b74142d500dde8
dlindsay <dlindsay@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-04-03 21:05:03 +0000

    More va-arg variations. OKed by Jeff Law.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32887 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/va-arg-15.c
@@ -0,0 +1,46 @@
+#include <varargs.h>
+
+void vafunction (dummy, va_alist)
+  char *dummy;
+  va_dcl
+{
+  double darg;
+  int iarg;
+  int flag = 0;
+  int i;
+  va_list ap;
+
+  va_start(ap);
+  for (i = 1; i <= 18; i++, flag++) 
+    {
+      if (flag & 1)
+	{
+	  darg = va_arg (ap, double);	
+	  if (darg != (double)i)
+	    abort();
+	}
+      else
+	{
+	  iarg = va_arg (ap, int);
+	  if (iarg != i)
+	    abort();
+	}
+    }
+    va_end(ap);
+}
+
+int main (void)
+{
+  vafunction( "", 
+	1, 2., 
+	3, 4., 
+	5, 6., 
+	7, 8., 
+	9, 10.,
+	11, 12.,
+	13, 14.,
+	15, 16.,
+	17, 18. );
+  exit(0);
+  return 0;
+}
