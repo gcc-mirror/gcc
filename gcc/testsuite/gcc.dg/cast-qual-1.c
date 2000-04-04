f1c0416f8ff262867ffdd0b52026d8dcc4513555
ghazi <ghazi@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-04-04 15:13:11 +0000

    * gcc.dg/cast-qual-1.c: Check casts through a pointer typedef.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@32903 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cast-qual-1.c
+++ gcc/testsuite/gcc.dg/cast-qual-1.c
@@ -28,3 +28,33 @@ bad_assign(const void *bar)
 {
   const char **foo = bar; /* { dg-warning "initialization discards" "discarding `const' warning" } */
 }
+
+typedef struct rtx_def * rtx;
+
+void
+typedef_cast(const void *bar)
+{
+  (const rtx)bar; /* { dg-bogus "cast discards" "discarding `const' warning" } */
+  (const rtx *)bar; /* { dg-warning "cast discards" "discarding `const' warning" } */
+}
+
+void
+typedef_cast2(const rtx bar)
+{
+  (const void *)bar; /* { dg-bogus "cast discards" "discarding `const' warning" } */
+  (const void **)bar; /* { dg-warning "cast discards" "discarding `const' warning" } */
+}
+
+void
+typedef_assign(const void *bar)
+{
+  rtx const *foo1 = bar; /* { dg-bogus "initialization discards" "discarding `const' warning" } */
+  const rtx *foo2 = bar; /* { dg-warning "initialization discards" "discarding `const' warning" } */
+}
+
+void
+typdef_assign2(const rtx bar)
+{
+  void *const *foo1 = bar; /* { dg-bogus "initialization discards" "discarding `const' warning" } */
+  const void **foo2 = bar; /* { dg-warning "initialization discards" "discarding `const' warning" } */
+}
