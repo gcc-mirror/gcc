5b57fe03b23d32e841dc81b879adc1bba0b83c37
wilson <wilson@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-04-07 18:51:44 +0000

    Fix nit. 	* config/ia64/ia64.c: Delete stdio.h and ctype.h includes.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@33007 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/ia64/ia64.c
+++ gcc/config/ia64/ia64.c
@@ -20,8 +20,6 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-#include <stdio.h>
-#include <ctype.h>
 #include "config.h"
 #include "system.h"
 #include "rtl.h"
