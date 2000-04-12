9ddb9a6b6185dabc4ecb88f0c389d36cc36d948a
hp <hp@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-04-12 20:54:03 +0000

    * execute/20000412-6.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@33126 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20000412-6.c
@@ -0,0 +1,22 @@
+unsigned bug (unsigned short value, unsigned short *buffer,
+              unsigned short *bufend);
+
+unsigned short buf[] = {1, 4, 16, 64, 256};
+int main()
+{
+  if (bug (512, buf, buf + 3) != 491)
+    abort ();
+
+  exit (0);
+}
+
+unsigned
+bug (unsigned short value, unsigned short *buffer, unsigned short *bufend)
+{
+  unsigned short *tmp;
+
+  for (tmp = buffer; tmp < bufend; tmp++)
+    value -= *tmp;
+
+  return value;
+}
