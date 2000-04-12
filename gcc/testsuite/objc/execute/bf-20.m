5d6057c59d61e0738290464ba1cb139052c1bbc9
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-04-12 15:58:38 +0000

    * objc/execute/bf-20.m: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@33111 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/objc/execute/bf-20.m
@@ -0,0 +1,15 @@
+#include <objc/objc.h>
+#include <objc/objc-api.h>
+#include <objc/Object.h>
+
+@interface MyObject
+{
+  short s;
+  char c;
+}
+@end
+
+@implementation MyObject
+@end
+
+#include "bf-common.h"
