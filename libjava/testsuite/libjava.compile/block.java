cd5aae11244da49d3619287d047949a1ddafa29f
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-04-21 00:53:14 +0000

    * libjava.compile/block.java: New file. 	* libjava.compile/block.out: New file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@33298 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/testsuite/libjava.compile/block.java
@@ -0,0 +1,14 @@
+// Class block
+// Generated on Thu Apr 20 15:03:23 PDT 2000
+// It is illegal to use modifiers for in-block (local) classes
+
+class block {
+  public void foo ()
+  {
+    public class xxx {}
+  }
+  public static void main (String[] arg)
+  {
+    System.out.println ("Testing class `block'...");
+  }
+}
