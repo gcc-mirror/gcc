a6a69fc2ad1062fab2801e42aca04af23470365b
Zack Weinberg <zack@wolery.cumb.org>
2000-04-23 21:50:59 +0000

    cpphash.c (collect_objlike_expansion): Add sanity check.
    	* cpphash.c (collect_objlike_expansion): Add sanity check.
    	(special_symbol): Remove case T_CONST, T_XCONST, T_MCONST.  If
    	the buffer is the empty string, return.  Mark __DATE__ and
    	__TIME__ as XCONST nodes, not MCONST.
    	(_cpp_macroexpand): Avoid pushing an empty buffer.
    	(funlike_macroexpand): Don't pop token_buffer here.
    
    From-SVN: r33360

---
+++ gcc/testsuite/gcc.dg/undef-2.c
@@ -0,0 +1,23 @@
+/* C99 6.10.8 para 4: None of [the predefined macro names] shall be the
+   subject of a #define or an #undef preprocessing directive.  */
+
+/* { dg-do preprocess } */
+
+#undef __DATE__		/* { dg-warning "undefining" "__DATE__" } */
+#undef __TIME__		/* { dg-warning "undefining" "__TIME__" } */
+#undef __FILE__		/* { dg-warning "undefining" "__FILE__" } */
+#undef __LINE__		/* { dg-warning "undefining" "__LINE__" } */
+#undef __STDC__		/* { dg-warning "undefining" "__STDC__" } */
+
+/* These should be protected from #undef, but aren't, because they
+   are set with normal #define commands - and on top of that, some
+   of them are library properties, outside our control.  To consider:
+   warn about undefining/redefining any identifier beginning with
+   __STDC_ .
+
+   __STDC_HOSTED__
+   __STDC_VERSION__
+   __STDC_IEC_559__
+   __STDC_IEC_559_COMPLEX__
+   __STDC_ISO_10646__
+ */
