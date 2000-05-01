2a290850875743c71df2145e1cd92c6e7462175f
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-05-01 17:17:55 +0000

    Update copyright.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@33574 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/1750a/1750a.c
+++ gcc/config/1750a/1750a.c
@@ -7,7 +7,7 @@ This file is part of GNU CC.
 
 GNU CC is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 1, or (at your option)
+the Free Software Foundation; either version 2, or (at your option)
 any later version.
 
 GNU CC is distributed in the hope that it will be useful,
