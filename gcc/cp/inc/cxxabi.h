f8a31625b132e2b9d22cb12d197c6acabf6bf3cb
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-05-02 01:25:15 +0000

    * exception.cc: Update license notice. 	* new.cc: Likewise. 	* new1.cc: Likewise. 	* new2.cc: Likewise. 	* tinfo.cc: Likewise. 	* tinfo2.cc: Likewise. 	* vec.cc: Likewise. 	* inc/cxxabi.h: Likewise. 	* inc/exception: Likewise. 	* inc/new: Likewise. 	* inc/new.h: Likewise. 	* inc/typeinfo: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@33601 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/inc/cxxabi.h
+++ gcc/cp/inc/cxxabi.h
@@ -1,9 +1,35 @@
 /* new abi support -*- C++ -*-
    Copyright (C) 2000
    Free Software Foundation, Inc.
-   Written by Nathan Sidwell, Codesourcery LLC, <nathan@codesourcery.com>
+   Written by Nathan Sidwell, Codesourcery LLC, <nathan@codesourcery.com>  */
 
-   This file declares the new abi entry points into the runtime. It is not
+// This file is part of GNU CC.
+//
+// GNU CC is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+// 
+// GNU CC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with GNU CC; see the file COPYING.  If not, write to
+// the Free Software Foundation, 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+
+// As a special exception, you may use this file as part of a free software
+// library without restriction.  Specifically, if other files instantiate
+// templates or use macros or inline functions from this file, or you compile
+// this file and link it with other files to produce an executable, this
+// file does not by itself cause the resulting executable to be covered by
+// the GNU General Public License.  This exception does not however
+// invalidate any other reasons why the executable file might be covered by
+// the GNU General Public License.
+
+/* This file declares the new abi entry points into the runtime. It is not
    normally necessary for user programs to include this header, or use the
    entry points directly. However, this header is available should that be
    needed.
