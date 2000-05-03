4bd048efabd5718ffd5297b2c658d39bcb28ea84
Rodney Brown <RodneyBrown@pmsc.com>
2000-05-03 18:49:17 +0000

    Replace inclusion of <stdio.h> with "system.h"
    From-SVN: r33639

--- gcc/config/mcore/mcore.c
+++ gcc/config/mcore/mcore.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-#include <stdio.h>
+#include "system.h"
 #include "assert.h"
 #include "gansidecl.h"
 
