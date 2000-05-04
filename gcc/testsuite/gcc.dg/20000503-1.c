187cdc0268474120a8ce52c455f8a25f7ff372eb
robertl <robertl@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-05-04 03:02:24 +0000

    * gcc.dg/20000503-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@33656 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20000503-1.c
@@ -0,0 +1,17 @@
+/* { dg-do compile { target i?86-*-linux* i?86-*-sco3.2v5* i?86-*-sysv* } } */
+/* { dg-options "-O2 -gdwarf -g2" } */
+
+/* Distilled from GCC's stmt.c.  Caused abort in dwarf-1 code. */
+
+static void
+expand_nl_goto_receiver ()
+{
+{
+static struct elims {int from, to;} elim_regs[] = {{ 16, 7}, { 16, 6}, { 20, 7},{ 20, 6}};
+      int i;
+
+      for (i = 0; i < sizeof elim_regs / sizeof elim_regs[0]; i++)
+	if (elim_regs[i].from == 16 && elim_regs[i].to == 6)
+	break;
+      }
+}
