d207c3f7ffa7bdece0ba89c054c969359379342b
Benjamin Kosnik <bkoz@cygnus.com>
2000-05-08 17:23:37 +0000

    fstream.tcc (filebuf::_M_init_filebuf): Don't set _M_buf_size based on macro, instead use _M_buf_size_opt.
    2000-05-08  Benjamin Kosnik  <bkoz@cygnus.com>
    
    	* bits/fstream.tcc (filebuf::_M_init_filebuf): Don't set
    	_M_buf_size based on macro, instead use _M_buf_size_opt.
    	* bits/std_streambuf.h (basic_streambuf): Add _M_buf_size_opt.
    	(basic_streambuf()): Set _M_buf_size_opt.
    	* testsuite/27_io/filebuf.cc (filebuf): Use _M_buf_size_opt
    	instead of _M_buf_size.
    
    From-SVN: r33768

--- libstdc++-v3/testsuite/27_io/filebuf.cc
+++ libstdc++-v3/testsuite/27_io/filebuf.cc
@@ -1,6 +1,6 @@
 // 990117 bkoz test functionality of basic_filebuf for char_type == char
 
-// Copyright (C) 1997-1999 Free Software Foundation, Inc.
+// Copyright (C) 1997-1999, 2000 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -37,7 +37,7 @@ class derived_filebuf: public std::filebuf
 {
  public:
   void
-  set_size(int_type __size) { _M_buf_size = __size; }
+  set_size(int_type __size) { _M_buf_size_opt = __size; }
 };
 
 derived_filebuf fb_01; // in 
