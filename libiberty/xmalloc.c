650f30687e05ee0e8d44521b58cdb4ec10801af3
J. David Anglin <dave@hiauly1.hia.nrc.ca>
2000-05-18 22:48:34 +0000

    * xmalloc.c: Include config.h for HAVE_SBRK definition.
    From-SVN: r34003

--- libiberty/xmalloc.c
+++ libiberty/xmalloc.c
@@ -17,6 +17,9 @@ License along with libiberty; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 #include "ansidecl.h"
 #include "libiberty.h"
 
