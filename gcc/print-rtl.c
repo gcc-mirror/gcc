0e7ebb06661d8b0ab00c16bc347a8b914b539c49
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-05-18 19:26:57 +0000

    * print-rtl.c (print_rtx): Only print note line number as string 	if between NOTE_INSN_BIAS and NOTE_INSN_MAX. 	* final.c (final_scan_insn): Do nothing for note line number 0.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@33997 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/print-rtl.c
+++ gcc/print-rtl.c
@@ -328,7 +328,8 @@ print_rtx (in_rtx)
       /* Print NOTE_INSN names rather than integer codes.  */
 
       case 'n':
-	if (XINT (in_rtx, i) <= 0)
+	if (XINT (in_rtx, i) >= NOTE_INSN_BIAS
+	    && XINT (in_rtx, i) < NOTE_INSN_MAX)
 	  fprintf (outfile, " %s", GET_NOTE_INSN_NAME (XINT (in_rtx, i)));
 	else
 	  fprintf (outfile, " %d", XINT (in_rtx, i));
