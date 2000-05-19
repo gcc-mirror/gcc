6e95df84d1be816955443d07c4935189b0341c63
zack <zack@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-05-19 17:43:38 +0000

    * cpphash.c (funlike_macroexpand): Make sure not to walk p1 	past l1 when deleting whitespace and markers.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@34027 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20000519-1.c
@@ -0,0 +1,10 @@
+/* Regression test for preprocessor crash.
+   Reported by Mathias Froehlich <frohlich@na.uni-tuebingen.de>.  */
+/* { dg-do preprocess } */
+#define foo
+
+#define __CAT__(a,b,c,d) a##b##c##d
+#define CAT(a,b,c,d) __CAT__(a,b,c,d)
+
+#define bar CAT(,foo,bar,)
+bar
