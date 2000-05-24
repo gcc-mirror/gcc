53ba789fa904445647ae5533a9ace0f768e3ef19
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-05-24 07:45:07 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@34130 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
 #include "gansidecl.h"
 #include "version.h"
 
-const char *const version_string = "2.96 20000523 (experimental)";
+const char *const version_string = "2.96 20000524 (experimental)";
