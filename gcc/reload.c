2c6630701996e679b15e1d495e7999c68a4b43bd
amylaar <amylaar@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-05-25 01:11:22 +0000

    * rtl.h (loc_mentioned_in_p): Declare. 	* reload.c (loc_mentioned_in_p): Moved from here... 	* rtlanal.c (loc_mentioned_in_p): to here.  No longer static. 	Fix loop increment for 'E' handling. 	* loop.c (strength_reduce): When doing biv->giv conversion, 	take multi-insn biv increments into account.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@34150 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/reload.c
+++ gcc/reload.c
@@ -272,7 +272,6 @@ static void find_reloads_address_part PARAMS ((rtx, rtx *, enum reg_class,
 static rtx find_reloads_subreg_address PARAMS ((rtx, int, int, enum reload_type,
 					      int, rtx));
 static int find_inc_amount	PARAMS ((rtx, rtx));
-static int loc_mentioned_in_p	PARAMS ((rtx *, rtx));
 extern void debug_reload_to_stream PARAMS ((FILE *));
 extern void debug_reload PARAMS ((void));
 
@@ -1556,32 +1555,6 @@ remove_address_replacements (in_rtx)
 	}
     }
   return something_changed;
-}
-
-/* Return non-zero if IN contains a piece of rtl that has the address LOC */
-static int
-loc_mentioned_in_p (loc, in)
-     rtx *loc, in;
-{
-  enum rtx_code code = GET_CODE (in);
-  const char *fmt = GET_RTX_FORMAT (code);
-  int i, j;
-
-  for (i = GET_RTX_LENGTH (code) - 1; i >= 0; i--)
-    {
-      if (loc == &in->fld[i].rtx)
-	return 1;
-      if (fmt[i] == 'e')
-	{
-	  if (loc_mentioned_in_p (loc, XEXP (in, i)))
-	    return 1;
-	}
-      else if (fmt[i] == 'E')
-	for (j = XVECLEN (in, i) - 1; i >= 0; i--)
-	  if (loc_mentioned_in_p (loc, XVECEXP (in, i, j)))
-	    return 1;
-    }
-  return 0;
 }
 
 /* If there is only one output reload, and it is not for an earlyclobber
--- gcc/rtlanal.c
+++ gcc/rtlanal.c
@@ -2358,3 +2358,29 @@ insns_safe_to_move_p (from, to, new_to)
   
   return 0;
 }
+
+/* Return non-zero if IN contains a piece of rtl that has the address LOC */
+int
+loc_mentioned_in_p (loc, in)
+     rtx *loc, in;
+{
+  enum rtx_code code = GET_CODE (in);
+  const char *fmt = GET_RTX_FORMAT (code);
+  int i, j;
+
+  for (i = GET_RTX_LENGTH (code) - 1; i >= 0; i--)
+    {
+      if (loc == &in->fld[i].rtx)
+	return 1;
+      if (fmt[i] == 'e')
+	{
+	  if (loc_mentioned_in_p (loc, XEXP (in, i)))
+	    return 1;
+	}
+      else if (fmt[i] == 'E')
+	for (j = XVECLEN (in, i) - 1; j >= 0; j--)
+	  if (loc_mentioned_in_p (loc, XVECEXP (in, i, j)))
+	    return 1;
+    }
+  return 0;
+}
