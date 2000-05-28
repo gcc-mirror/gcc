0b88c5fca0484f6528ade8d89673c972a04539c9
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-05-28 14:20:27 +0000

    * expr2.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@34234 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.oliva/expr2.C
@@ -0,0 +1,18 @@
+// Copyright (C) 2000 Free Software Foundation
+
+// by Alexandre Oliva <aoliva@cygnus.com>
+
+// execution test - XFAIL *-*-*
+
+int i, j;
+
+const int &f(const int& I, const int& J) {
+  // this must not be optimized to I because it's an lvalue
+  return (I != J) ? I : J;
+}
+
+int main () {
+  if (&f(i, j) != &j)
+    abort ();
+  exit (0);
+}
