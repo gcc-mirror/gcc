e087aeb2b9641000a819a86ef856f7c152f959f8
Richard Henderson <rth@cygnus.com>
2000-05-27 20:24:57 -0700

    print-rtl.c (reg_names): Remove const.
            * print-rtl.c (reg_names): Remove const.
            * regclass.c (reg_names): Likewise.
            * regs.h (reg_names): Likewise.
            * hard-reg-set.h (reg_names): Likewise.
    
    From-SVN: r34225

--- gcc/hard-reg-set.h
+++ gcc/hard-reg-set.h
@@ -471,4 +471,4 @@ extern int n_non_fixed_regs;
 
 /* Vector indexed by hardware reg giving its name.  */
 
-extern const char * const reg_names[FIRST_PSEUDO_REGISTER];
+extern const char * reg_names[FIRST_PSEUDO_REGISTER];
