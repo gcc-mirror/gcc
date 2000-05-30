54ae96de89f391cd76bbd0bfab208b54e71804ec
Mark Mitchell <mark@codesourcery.com>
2000-05-30 17:39:59 +0000

    * call.c (joust): Fix handling of overloaded builtin operators.
    From-SVN: r34269

---
+++ gcc/testsuite/g++.old-deja/g++.other/overload13.C
@@ -0,0 +1,13 @@
+// Build don't link:
+// Origin: Nathan Sidwell <nathan@codesourcery.com>
+
+struct A {
+  bool operator== (A const &);
+  operator bool () const;
+  operator int * () const;
+};
+
+bool foo (A &a1, A &a2)
+{
+  return a1 == a2;
+}
