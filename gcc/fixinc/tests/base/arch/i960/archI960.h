5d7d28c26575f668be3e1646f87dfd0bf24b886f
Bruce Korb <bkorb@gnu.org>
2000-05-31 14:26:57 +0000

    rework "make check" for fixinc
    From-SVN: r34298

---
+++ gcc/fixinc/tests/base/arch/i960/archI960.h
@@ -0,0 +1,8 @@
+
+
+#if defined( VA_I960_MACRO_CHECK )
+extern int __vxvsiz vsiz;
+extern int __vxvali vali;
+extern int __vxvpad vpad;
+#define __vxalignof__(x) ...
+#endif  /* VA_I960_MACRO_CHECK */
