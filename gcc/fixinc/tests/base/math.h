ff84bb1888b6acc653c5b8edbd75740925383a9b
Bruce Korb <korbb@gcc.gnu.org>
2000-06-02 16:04:10 +0000

    fix test result for ultrix_atof_param
    From-SVN: r34359

--- gcc/fixinc/tests/base/math.h
+++ gcc/fixinc/tests/base/math.h
@@ -92,6 +92,11 @@ extern int matherr();
 #define __P(a) a
 #endif
 #endif  /* SVR4__P_CHECK */
+
+
+#if defined( ULTRIX_ATOF_PARAM_CHECK )
+extern double atof( const char *);
+#endif  /* ULTRIX_ATOF_PARAM_CHECK */
 #ifdef __cplusplus
 #undef exception
 #endif
