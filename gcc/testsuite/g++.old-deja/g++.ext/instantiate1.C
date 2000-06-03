b9d969b7d40904b673662ffb656b90e184655456
Jason Merrill <jason@gcc.gnu.org>
2000-06-02 20:02:48 -0400

    new
    From-SVN: r34366

---
+++ gcc/testsuite/g++.old-deja/g++.ext/instantiate1.C
@@ -0,0 +1,23 @@
+// Test that 'extern template' suppresses instantiations.
+// Special g++ Options: -g -O
+
+// Ignore the 'ld returned 1' message from collect2.
+// excess errors test - XFAIL *-*-*
+
+template <class T> void f (T) { }
+extern template void f (int);
+
+template <class T> struct A {
+  void f () { }
+};
+extern template struct A<int>;
+
+int main ()
+{
+  f (42);			// ERROR - not instantiated
+  A<int> a;
+  a.f ();			// ERROR - not instantiated
+  f (2.0);			// gets bogus error
+  A<double> b;
+  b.f ();			// gets bogus error
+}
---
+++ gcc/testsuite/g++.old-deja/g++.ext/instantiate2.C
@@ -0,0 +1,17 @@
+// Test that 'static template' instantiates statics.
+// Special g++ Options: -g -fno-implicit-templates
+
+// Ignore the 'ld returned 1' message from collect2.
+// excess errors test - XFAIL *-*-*
+
+template <class T> struct A {
+  static T t;
+};
+template <class T> T A<T>::t = 0;
+static template struct A<int>;
+
+int main ()
+{
+  A<int>::t = 42;		// gets bogus error
+  A<char>::t = 42;		// ERROR - not instantiated
+}
