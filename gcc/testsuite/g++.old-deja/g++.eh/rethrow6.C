eb0a9cd2a91690797accfce718072698a65813cc
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-06-03 02:07:51 +0000

    new
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@34374 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.eh/rethrow6.C
@@ -0,0 +1,75 @@
+// Testcase for proper handling of rethrow.
+
+#include <stdio.h>
+
+int c, d;
+int wrong;
+
+struct A
+{
+  int i;
+  A () { i = c++; printf ("A() %d\n", i); }
+  A (const A&) { i = c++; printf ("A(const A&) %d\n", i); }
+  ~A() { printf ("~A() %d\n", i); ++d; }
+};
+
+struct B
+{
+  ~B () {
+    try
+      {
+	printf ("Rethrowing III...\n");
+	throw;
+      }
+    catch (A& a)
+      {
+	printf ("Caught III %d...\n", a.i);
+	if (a.i != 1)
+	  {
+	    printf ("** rethrew uncaught exception **\n");
+	    wrong = 1;
+	  }
+      }
+    printf ("continuing to unwind II...\n");
+  }
+};
+
+int
+main ()
+{
+  {
+    A a;
+
+    try
+      {
+	try
+	  {
+	    printf ("Throwing I...\n");
+	    throw a;
+	  }
+	catch (A& a)
+	  {
+	    printf ("Caught I %d...\n", a.i);
+	    try
+	      {
+		B b;
+		printf ("Throwing II...\n");
+		throw a;
+	      }
+	    catch (A& a)
+	      {
+		printf ("Caught II %d...\n", a.i);
+		printf ("Throwing IV...\n");
+		throw;
+	      }
+	  }
+      }
+    catch (A& a)
+      {
+	printf ("Caught IV %d.\n", a.i);
+      }
+  }
+
+  printf ("c == %d, d == %d\n", c, d);
+  return c != d || wrong;
+}
