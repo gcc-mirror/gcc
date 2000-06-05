c598ac99acb4d02052fb117145906fe17f76dc7c
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-06-05 13:44:29 +0000

    * gcc.c-torture/compile/20000605-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@34403 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/20000605-1.c
@@ -0,0 +1,22 @@
+/* Copyright (C) 2000 Free Software Foundation */
+
+/* make sure we don't get confused by various flavours of void */
+
+/* Origin:  Jakub Jelinek  <jakub@redhat.com>
+ *          Joel Sherrill <joel.sherrill@OARcorp.com>
+ */
+
+typedef void foo;
+foo bar(void);
+void baz(void)
+{
+  bar();
+}
+
+void volatile f();
+
+int x()
+{
+  f();
+}
+
