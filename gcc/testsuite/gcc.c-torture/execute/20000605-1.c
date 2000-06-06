f40162723047b08af9de8f4d1defde78b9f800f1
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-06-06 00:19:52 +0000

    New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@34418 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20000605-1.c
@@ -0,0 +1,48 @@
+typedef struct _RenderInfo RenderInfo;
+struct _RenderInfo
+{
+    int y;
+    float scaley;
+    int src_y;
+};
+
+static void bar(void) { }
+
+static int
+render_image_rgb_a (RenderInfo * info)
+{
+  int y, ye;
+  float error;
+  float step;
+
+  y = info->y;
+  ye = 256;
+
+  step = 1.0 / info->scaley;
+
+  error = y * step;
+  error -= ((int) error) - step;
+
+  for (; y < ye; y++) {
+      if (error >= 1.0) {
+	  info->src_y += (int) error;
+	  error -= (int) error;
+	  bar();
+      }
+      error += step;
+  }
+  return info->src_y;
+}
+
+int main (void)
+{
+    RenderInfo info;
+
+    info.y = 0;
+    info.src_y = 0;
+    info.scaley = 1.0;
+
+    if (render_image_rgb_a(&info) != 256)
+       abort ();
+    exit(0);
+}
