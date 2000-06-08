16b72afedd80e27cdd507ef17922a9dd65915893
Mark Mitchell <mark@codesourcery.com>
2000-06-08 17:35:44 +0000

    libioP.h: Remove old ABI mangling code.
    	* libioP.h: Remove old ABI mangling code.
    	* stdstrbufs.cc: Likewise.
    
    From-SVN: r34459

--- libio/libioP.h
+++ libio/libioP.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1997, 2000 Free Software Foundation, Inc.
    This file is part of the GNU IO Library.
 
    This library is free software; you can redistribute it and/or
@@ -593,18 +593,8 @@ extern int _IO_vscanf __P ((const char *, _IO_va_list));
 /* VTABLE_LABEL defines NAME as of the CLASS class.
    CNLENGTH is strlen(#CLASS).  */
 #ifdef __GNUC__
-# if (!defined(__GXX_ABI_VERSION) || __GXX_ABI_VERSION < 100)
-#  if _G_VTABLE_LABEL_HAS_LENGTH
-#   define VTABLE_LABEL(NAME, CLASS, CNLENGTH) \
-  extern char NAME[] asm (_G_VTABLE_LABEL_PREFIX #CNLENGTH #CLASS);
-#  else
-#   define VTABLE_LABEL(NAME, CLASS, CNLENGTH) \
-  extern char NAME[] asm (_G_VTABLE_LABEL_PREFIX #CLASS);
-#  endif
-# else
-#  define VTABLE_LABEL(NAME, CLASS, CNLENGTH) \
-  extern char NAME[] asm ("_ZN" #CNLENGTH #CLASS "TVE");
-# endif /* (!defined(__GXX_ABI_VERSION) || __GXX_ABI_VERSION < 100) */
+# define VTABLE_LABEL(NAME, CLASS, CNLENGTH) \
+  extern char NAME[] asm ("_ZTV" #CNLENGTH #CLASS );
 #endif /* __GNUC__ */
 
 #if !defined(builtinbuf_vtable) && defined(__cplusplus)
