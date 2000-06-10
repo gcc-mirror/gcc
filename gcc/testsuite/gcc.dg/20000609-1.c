27985b669a096e686ab206be177d7e3df114e207
Clinton Popetz <cpopetz@cygnus.com>
2000-06-10 02:57:20 +0000

    * gcc.dg/20000609-1.c: New test.
    From-SVN: r34481

---
+++ gcc/testsuite/gcc.dg/20000609-1.c
@@ -0,0 +1,20 @@
+/* { dg-do run { target i?86-*-* } } */
+/* { dg-options "-O1 -ffast-math -march=i686" } */
+
+/* Sanity check for fp_jcc_* with TARGET_CMOVE.  */
+
+extern void abort (void);
+
+static int test(double a)
+{
+  if (a)
+    return 0;
+}
+
+static double zero = 0.0;
+
+int main ()
+{
+  test (zero);
+  return 0;
+}
