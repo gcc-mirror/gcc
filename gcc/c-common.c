e79b3b6bd189e156a245ae85feca135797d82d26
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-06-12 19:14:08 +0000

    * c-common.c (lang_get_alias_set): Fix typo.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@34500 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-common.c
+++ gcc/c-common.c
@@ -3340,7 +3340,7 @@ lang_get_alias_set (t)
     }
   /* It's not yet safe to use alias sets for classes in C++ because
      the TYPE_FIELDs list for a class doesn't mention base classes.  */
-  else if (c_language == clk_cplusplus && AGGREGATE_TYPE_P (t));
+  else if (c_language == clk_cplusplus && AGGREGATE_TYPE_P (t))
     return 0;
 
   return -1;
