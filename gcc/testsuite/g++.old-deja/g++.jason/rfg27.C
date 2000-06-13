a032eacb1b30960ed3cbc0fe6de47bd97ec816cc
Jason Merrill <jason@gcc.gnu.org>
2000-06-13 19:47:37 -0400

    complain about long long
    From-SVN: r34533

--- gcc/testsuite/g++.old-deja/g++.jason/rfg27.C
+++ gcc/testsuite/g++.old-deja/g++.jason/rfg27.C
@@ -1,3 +1,6 @@
+// Don't compile this one with -Wno-long-long...
+// Special g++ Options: -pedantic-errors
+
 short volatile short var_0_2;              /* ERROR - duplicate short */
 long volatile long var_0_3;                /* ERROR - duplicate long */
 signed volatile signed var_0_7;            /* ERROR - duplicate signed */
