22c40c28b39c88652b3f25a51dfbf6b044f157d6
Jakub Jelinek <jakub@redhat.com>
2000-06-16 13:14:56 +0200

    * gcc.c-torture/execute/loop-6.c: New test.
    From-SVN: r34573

---
+++ gcc/testsuite/gcc.c-torture/execute/loop-6.c
@@ -0,0 +1,15 @@
+main()
+{
+  char c;
+  char d;
+  int nbits;
+  c = -1;
+  for (nbits = 1 ; nbits < 100; nbits++) {
+    d = (1 << nbits) - 1;
+    if (d == c)
+      break;
+  }
+  if (nbits == 100)
+    abort();
+  exit(0);
+}
