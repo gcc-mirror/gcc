7be97c2c2327d43e42d677da11c4effed6ce483c
Bruce Korb <korbb@gcc.gnu.org>
2000-06-17 19:48:12 +0000

    new test result files
    From-SVN: r34583

---
+++ gcc/fixinc/tests/base/ctrl-quotes-def-1.h
@@ -0,0 +1,5 @@
+
+
+#if defined( CTRL_QUOTES_DEF_CHECK_1 )
+#define _CTRL(c) (c&037)
+#endif  /* CTRL_QUOTES_DEF_CHECK_1 */
---
+++ gcc/fixinc/tests/base/sys/ioctl.h
@@ -0,0 +1,6 @@
+
+
+#if defined( ULTRIX_NESTED_IOCTL_CHECK )
+/* #define SIOCSCREENON _IOWR('i', 49, int)*//* screend, net/gw_screen.h */
+
+#endif  /* ULTRIX_NESTED_IOCTL_CHECK */
