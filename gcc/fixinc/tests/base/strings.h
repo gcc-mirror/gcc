3bbea996a262371c8a2d58f9549148d990644529
korbb <korbb@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-06-17 19:54:04 +0000

    modified make check test output
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@34585 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fixinc/tests/base/strings.h
+++ gcc/fixinc/tests/base/strings.h
@@ -1,5 +1,16 @@
+#ifndef FIXINC_ULTRIX_STRINGS_CHECK
+#define FIXINC_ULTRIX_STRINGS_CHECK 1
+
 
 
 #if defined( SUNOS_STRLEN_CHECK )
  __SIZE_TYPE__ strlen(); /* string length */
 #endif  /* SUNOS_STRLEN_CHECK */
+
+
+#if defined( ULTRIX_STRINGS_CHECK )
+@(#)strings.h   6.1     (ULTRIX)
+
+#endif  /* ULTRIX_STRINGS_CHECK */
+
+#endif  /* FIXINC_ULTRIX_STRINGS_CHECK */
--- gcc/fixinc/tests/base/sys/limits.h
+++ gcc/fixinc/tests/base/sys/limits.h
@@ -1,8 +1,20 @@
 
 
+#if defined( LIMITS_IFNDEFS_CHECK )
+#ifndef DBL_DIG
+ #	define	DBL_DIG 	 0  /* somthin' */
+#endif
+#endif  /* LIMITS_IFNDEFS_CHECK */
+
+
 #if defined( NESTED_SYS_LIMITS_CHECK )
 /*
 #define CHILD_MAX 20  Max, Max, ... */ /*
 #define OPEN_MAX  20  Max, Max, ... */
 
 #endif  /* NESTED_SYS_LIMITS_CHECK */
+
+
+#if defined( ULTRIX_MATH_IFDEF_CHECK )
+#if     defined(__GFLOAT) 
+#endif  /* ULTRIX_MATH_IFDEF_CHECK */
