5840d6e4f57a38f2c250927b4cd65c6e933f522c
Jason Merrill <jason@gcc.gnu.org>
2000-06-24 18:12:16 -0400

    new
    From-SVN: r34685

---
+++ gcc/testsuite/g++.old-deja/g++.other/friend7.C
@@ -0,0 +1,21 @@
+// Origin: Martin v. Löwis  <loewis@informatik.hu-berlin.de>
+// Test that a friend declaration with an explicit :: finds the right fn.
+// Build don't link:
+
+namespace M {
+class S; 
+}
+void foo(M::S *);
+
+namespace M {
+class S {
+  friend void (::foo)(S *);
+  void Fn(); 
+  static S s;
+};
+} 
+
+void (::foo)(M::S *ptr) {
+  M::S::s.Fn();
+  ptr->Fn();
+} 
