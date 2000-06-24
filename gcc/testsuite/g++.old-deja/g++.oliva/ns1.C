0b44a7e9ece73a006e45866aee5d9b5f67df8cc8
Jason Merrill <jason@gcc.gnu.org>
2000-06-24 18:27:43 -0400

    fixed
    From-SVN: r34688

--- gcc/testsuite/g++.old-deja/g++.oliva/ns1.C
+++ gcc/testsuite/g++.old-deja/g++.oliva/ns1.C
@@ -1,6 +1,6 @@
 // Build don't link:
 
-// Copyright (C) 1999 Free Software Foundation
+// Copyright (C) 1999, 2000 Free Software Foundation
 
 // by Alexandre Oliva <oliva@dcc.unicamp.br>
 // based on bug report by Dima Volodin <dvv@dvv.ru>
@@ -8,6 +8,6 @@
 void foo();
 namespace bar {
   class baz {
-    friend void ::foo(); // gets bogus error - parse error - XFAIL *-*-*
+    friend void ::foo(); // gets bogus error - parse error
   };
 }
