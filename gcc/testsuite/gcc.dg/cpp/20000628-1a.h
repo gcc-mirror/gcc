122168d7b2905b6a7f57f43b3ca54bbc8ece5dce
Zack Weinberg <zack@gcc.gnu.org>
2000-06-29 19:30:34 +0000

    20000628-1a.h: On each of the first three inclusions, declare one variable.
    	* gcc.dg/cpp/20000628-1a.h: On each of the first three inclusions,
    	declare one variable.  On the fourth, error.
    	* gcc.dg/cpp/20000628-1.c (main): Reference the three variables
    	declared by 20000628-1a.h.
    
    From-SVN: r34790

--- gcc/testsuite/gcc.dg/cpp/20000628-1a.h
+++ gcc/testsuite/gcc.dg/cpp/20000628-1a.h
@@ -1,2 +1,15 @@
-/* Included from 20000628-1.h */
-extern int x;
+/* Included from 20000628-1.h.  This header is supposed to be seen
+   exactly three times.  */
+
+#if !defined A
+#define A
+int a;
+#elif !defined B
+#define B
+int b;
+#elif !defined C
+#define C
+int c;
+#else
+#error Included a fourth time
+#endif
