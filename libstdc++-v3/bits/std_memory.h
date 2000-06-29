c011e2ad8adc191c6c38089cf05ae9f3daaf5a08
Branko Cibej <branko.cibej@hermes.si>
2000-06-29 21:07:04 +0000

    std_memory.h (auto_ptr_ref): Reworked and defined only if _GLIBCPP_RESOLVE_LIB_DEFECTS.
    2000-06-29  Branko Cibej  <branko.cibej@hermes.si>
    
            * bits/std_memory.h (auto_ptr_ref): Reworked and defined only if
            _GLIBCPP_RESOLVE_LIB_DEFECTS.
            (auto_ptr): _M_ptr changed to void*.
            (suto_ptr::get): Cast _M_ptr to element type.
            (auto_ptr::auto_ptr(auto_ptr<Tp1>)): Test implicit convetsion.
            (auto_ptr::~auto_ptr): Use this->get() instead of _M_ptr.
            (auto_ptr::operator*): Likewise.
            (auto_ptr::operator->): Likewise.
            (auto_ptr::release): Likewise.
            (auto_ptr::reset): Likewise.
            (auto_ptr::auto_ptr(auto_ptr_ref)): Initialize from __ref._M_release.
            (auto_ptr::operator auto_ptr_rev<_Tp1>): Updated.
            Define nested auto_ptr_ref unless _GLIBCPP_RESOLVE_LIB_DEFECTS.
            Define operator=(auto_ptr_ref) if _GLIBCPP_RESOLVE_LIB_DEFECTS.
    
    From-SVN: r34792

--- libstdc++-v3/bits/std_memory.h
+++ libstdc++-v3/bits/std_memory.h
@@ -25,19 +25,28 @@
 
 __STL_BEGIN_NAMESPACE
 
-#if defined(__SGI_STL_USE_AUTO_PTR_CONVERSIONS) && \
-     defined(__STL_MEMBER_TEMPLATES)
- 
- template<class _Tp1> struct auto_ptr_ref {
-   _Tp1* _M_ptr;
-   auto_ptr_ref(_Tp1* __p) : _M_ptr(__p) {}
+#ifdef _GLIBCPP_RESOLVE_LIB_DEFECTS
+# if defined(__SGI_STL_USE_AUTO_PTR_CONVERSIONS) && \
+       defined(__STL_MEMBER_TEMPLATES)
+
+template<typename _Tp> class auto_ptr_ref {
+  template<typename _Tp1> friend class auto_ptr;
+  _Tp* _M_ptr;
+  void*& _M_ptr_ref;
+  explicit auto_ptr_ref(_Tp* __p, void*& __r) __STL_NOTHROW
+    : _M_ptr(__p), _M_ptr_ref(__r) {}
+  _Tp* _M_release() const __STL_NOTHROW {
+    _M_ptr_ref = 0;
+    return _M_ptr;
+  }
 };
 
-#endif
+# endif /* auto ptr conversions && member templates */
+#endif /* _GLIBCPP_RESOLVE_LIB_DEFECTS */
 
 template <class _Tp> class auto_ptr {
 private:
-  _Tp* _M_ptr;
+  void* _M_ptr;
 
 public:
   typedef _Tp element_type;
@@ -46,8 +55,10 @@ public:
   auto_ptr(auto_ptr& __a) __STL_NOTHROW : _M_ptr(__a.release()) {}
 
 #ifdef __STL_MEMBER_TEMPLATES
-  template <class _Tp1> auto_ptr(auto_ptr<_Tp1>& __a) __STL_NOTHROW
-    : _M_ptr(__a.release()) {}
+  template <class _Tp1> auto_ptr(auto_ptr<_Tp1>& __a) __STL_NOTHROW {
+    _Tp* const __tmp = __a.release(); // Must have implicit conversion
+    _M_ptr = __tmp;
+  }
 #endif /* __STL_MEMBER_TEMPLATES */
 
   auto_ptr& operator=(auto_ptr& __a) __STL_NOTHROW {
@@ -67,23 +78,24 @@ public:
   // specification here, but omitting it is standard conforming.  Its 
   // presence can be detected only if _Tp::~_Tp() throws, but (17.4.3.6/2)
   // this is prohibited.
-  ~auto_ptr() { delete _M_ptr; }
+  ~auto_ptr() { delete this->get(); }
  
   _Tp& operator*() const __STL_NOTHROW {
-    return *_M_ptr;
+    return  this->get();
   }
   _Tp* operator->() const __STL_NOTHROW {
-    return _M_ptr;
+    return static_cast<_Tp*>(_M_ptr);
   }
   _Tp* get() const __STL_NOTHROW {
-    return _M_ptr;
+    return static_cast<_Tp*>(_M_ptr);
   }
   _Tp* release() __STL_NOTHROW {
-    _Tp* __tmp = _M_ptr;
+    _Tp* const __tmp = this->get();
     _M_ptr = 0;
     return __tmp;
   }
   void reset(_Tp* __p = 0) __STL_NOTHROW {
+    _Tp* const __tmp = this->get();
     if (__p != _M_ptr) {
       delete _M_ptr;
       _M_ptr = __p;
@@ -98,23 +110,35 @@ public:
 #if defined(__SGI_STL_USE_AUTO_PTR_CONVERSIONS) && \
     defined(__STL_MEMBER_TEMPLATES)
 
+# ifndef _GLIBCPP_RESOLVE_LIB_DEFECTS
+private:
+  template<typename _Tp1> struct auto_ptr_ref {
+    _Tp1* _M_ptr;
+    void*& _M_ptr_ref;
+    explicit auto_ptr_ref(_Tp1* __p, void*& __r) __STL_NOTHROW
+    : _M_ptr(__p), _M_ptr_ref(__r) {}
+    _Tp1* _M_release() const __STL_NOTHROW {
+      _M_ptr_ref = 0;
+      return _M_ptr;
+    }
+  };
+# endif /* !_GLIBCPP_RESOLVE_LIB_DEFECTS */
+
 public:
   auto_ptr(auto_ptr_ref<_Tp> __ref) __STL_NOTHROW
-    : _M_ptr(__ref._M_ptr) {}
-
+  : _M_ptr(__ref._M_release()) {}
+  
+# ifdef _GLIBCPP_RESOLVE_LIB_DEFECTS
   auto_ptr& operator=(auto_ptr_ref<_Tp> __ref) __STL_NOTHROW {
-    if (__ref._M_ptr != this->get()) {
-      delete _M_ptr;
-      _M_ptr = __ref._M_ptr;
-    }
-    return *this;
+    reset(__ref._M_release());
+    return *this; 
   }
-
-  template <class _Tp1> operator auto_ptr_ref<_Tp1>() __STL_NOTHROW 
-    { return auto_ptr_ref<_Tp>(this->release()); }
+# endif /* _GLIBCPP_RESOLVE_LIB_DEFECTS */
+  
+  template <class _Tp1> operator auto_ptr_ref<_Tp1>() __STL_NOTHROW
+  { return auto_ptr_ref<_Tp1>(this->get(), _M_ptr); }
   template <class _Tp1> operator auto_ptr<_Tp1>() __STL_NOTHROW
-    { return auto_ptr<_Tp1>(this->release()); }
-
+  { return auto_ptr<_Tp1>(this->release()); }
 #endif /* auto ptr conversions && member templates */
 };
 
