ff4cb867867d61bffbb8c8f1633264d7a277b36d
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-07-06 15:34:53 +0000

    * g++.old-deja/g++.pt/instantiate7.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@34889 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.pt/instantiate7.C
@@ -0,0 +1,18 @@
+// Build don't link:
+
+// Copyright (C) 2000 Free Software Foundation
+// Contributed by Nathan Sidwell 6 July 2000 <nathan@codesourcery.com>
+
+template <class T>
+void Wibble (void (*fn) (), T *const __restrict__ &p2)
+{}
+template<class T1, class T2>
+void Wibble (T1 *const __restrict__ &p1, T2 *const __restrict__ &p2)
+{}
+
+void Baz ();
+
+void Foo (void const *ptr)
+{
+  Wibble (&Baz, ptr);
+}
