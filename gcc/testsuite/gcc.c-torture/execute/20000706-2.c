f5afd9e924c198f8a01a92bb40ff48be87525ab1
Jakub Jelinek <jakub@redhat.com>
2000-07-07 21:55:29 +0200

    sibcall.c (uses_addressof): Add INMEM argument...
    	* sibcall.c (uses_addressof): Add INMEM argument, check for
    	current_function_internal_arg_pointer outside of MEM rtxs in addition
    	to ADDRESSOFs.
    	(sequence_uses_addressof): Update caller.
    
    	* gcc.c-torture/execute/20000706-1.c: New test.
    	* gcc.c-torture/execute/20000706-2.c: New test.
    	* gcc.c-torture/execute/20000706-3.c: New test.
    	* gcc.c-torture/execute/20000706-4.c: New test.
    	* gcc.c-torture/execute/20000706-5.c: New test.
    
    From-SVN: r34906

---
+++ gcc/testsuite/gcc.c-torture/execute/20000706-2.c
@@ -0,0 +1,31 @@
+extern void abort(void);
+extern void exit(int);
+
+struct baz {
+  int a, b, c, d, e;
+};
+
+void bar(struct baz *x, int f, int g, int h, int i, int j)
+{
+  if (x->a != 1 || x->b != 2 || x->c != 3 || x->d != 4 || x->e != 5 ||
+      f != 6 || g != 7 || h != 8 || i != 9 || j != 10)
+    abort();
+}
+
+void foo(char *z, struct baz x, char *y)
+{
+  bar(&x,6,7,8,9,10);
+}
+
+int main()
+{
+  struct baz x;
+
+  x.a = 1;
+  x.b = 2;
+  x.c = 3;
+  x.d = 4;
+  x.e = 5;
+  foo((char *)0,x,(char *)0);
+  exit(0);
+}
