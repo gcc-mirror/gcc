261985c30c196783d037149572b01af0e769a137
Tom Tromey <tromey@cygnus.com>
2000-07-07 20:51:48 +0000

    stringconst2.out: New file.
    	* libjava.lang/stringconst2.out: New file.
    	* libjava.lang/stringconst2.java: New file.
    
    From-SVN: r34909

---
+++ libjava/testsuite/libjava.lang/stringconst2.java
@@ -0,0 +1,21 @@
+// Test to make sure a string constant is correctly initialized.
+
+import java.lang.reflect.*;
+
+public class stringconst2
+{
+  public static final String q = "zardoz";
+
+  public static void main (String[] args)
+  {
+    try
+      {
+	Class k = Class.forName ("stringconst2");
+	Field f = k.getField ("q");
+	System.out.println (f.get (null));
+      }
+    catch (Throwable _)
+      {
+      }
+  }
+}
