3f2efc377f1df666d53ad56c19630812d2b5b5ac
Jeff Law <law@gcc.gnu.org>
2000-07-10 01:45:10 -0600

    Daily bump.
    From-SVN: r34941

--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
 #include "gansidecl.h"
 #include "version.h"
 
-const char *const version_string = "2.96 20000709 (experimental)";
+const char *const version_string = "2.96 20000710 (experimental)";
