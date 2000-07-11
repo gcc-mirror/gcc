fb5715bd9852e48aab66f0d316afad3e9459dfe2
korbb <korbb@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-07-11 22:29:52 +0000

    new tests results from changes
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@34971 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fixinc/tests/base/testing.h
+++ gcc/fixinc/tests/base/testing.h
@@ -23,11 +23,9 @@ BSD43__IOWR('T', 1) /* Some are multi-line */
 
 
 #if defined( LIBC1_IFDEFD_MEMX_CHECK )
-#if defined(__STDC__) && __GNUC__ < 2 || defined(__cplusplus)
 /* Copy N bytes of SRC to DEST.  */
 extern __ptr_t memcpy __P ((__ptr_t __dest, __const __ptr_t __src,
                          size_t __n));
-#endif
 #endif  /* LIBC1_IFDEFD_MEMX_CHECK */
 
 
