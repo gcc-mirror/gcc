268afb999d5d67e4083d42c64d32d49df28dee0e
Bruce Korb <korbb@gcc.gnu.org>
2000-07-11 22:29:52 +0000

    new tests results from changes
    From-SVN: r34971

--- gcc/fixinc/tests/base/sys/types.h
+++ gcc/fixinc/tests/base/sys/types.h
@@ -1,11 +1,5 @@
 
 
-#if defined( IRIX_MULTILINE_CMNT_CHECK )
-/* we check the type of the result */
-// /* of the sizeof something.  This is a bad test :-( */
-#endif  /* IRIX_MULTILINE_CMNT_CHECK */
-
-
 #if defined( SYSTYPES_STDLIB_SIZE_T_CHECK )
 #ifndef __SIZE_TYPE__
 #define __SIZE_TYPE__ long unsigned int
