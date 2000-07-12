c6be190f2480a6dd77b390e21c552c50a56e2aa1
bryce <bryce@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-07-12 21:13:51 +0000

    Add missing files from last check-in: 	* java/awt/image/ImageConsumer.java: New file. 	* java/awt/image/ImageProducer.java: New file. 	* java/awt/image/ImageObserver.java: New file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@35000 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/java/awt/image/ImageProducer.java
@@ -0,0 +1,16 @@
+/* Copyright (C) 2000  Free Software Foundation
+
+   This file is part of libgcj.
+
+This software is copyrighted work licensed under the terms of the
+Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
+details.  */
+
+public interface ImageProducer
+{
+  void addConsumer(ImageConsumer ic);
+  boolean isConsumer(ImageConsumer ic);
+  void removeConsumer(ImageConsumer ic);
+  void startProduction(ImageConsumer ic);
+  void requestTopDownLeftRightResend(ImageConsumer ic);
+}
