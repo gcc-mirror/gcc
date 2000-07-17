c06f8b152d2521c58111f1ccb99b3db35fda4201
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-07-17 08:34:27 +0000

    * gcc.dg/c90-digraph-1.c, gcc.dg/c94-digraph.c, 	gcc.dg/c99-digraph.c: New tests.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@35078 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/c94-digraph-1.c
@@ -0,0 +1,24 @@
+/* Test for recognition of digraphs: should be recognised in C94 and C99
+   mode, but not in C90 mode.  Also check correct stringizing.
+*/
+/* Origin: Joseph Myers <jsm28@cam.ac.uk> */
+/* { dg-do run } */
+/* { dg-options "-std=iso9899:199409 -pedantic-errors" } */
+
+#define str(x) xstr(x)
+#define xstr(x) #x
+#define foo(p, q) str(p %:%: q)
+
+extern void abort (void);
+extern int strcmp (const char *, const char *);
+
+int
+main (void)
+{
+  const char *t = foo (1, 2);
+  const char *u = str (<:);
+  if (strcmp (t, "12") || strcmp (u, "<:"))
+    abort ();
+  else
+    return 0;
+}
