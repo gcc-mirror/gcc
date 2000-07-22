f0f4cb1b04bfa88b5a7e8a32f35043a4040980c2
Michael Meissner <meissner@redhat.com>
2000-07-22 02:46:24 +0000

    Add gcc.c-torture/execute/ieee/hugeval.c
    From-SVN: r35184

---
+++ gcc/testsuite/gcc.c-torture/execute/ieee/hugeval.c
@@ -0,0 +1,27 @@
+#include <math.h>
+
+static const double zero =  0.0;
+static const double pone =  1.0;
+static const double none = -1.0;
+static const double pinf =  1.0 / 0.0;
+static const double ninf = -1.0 / 0.0;
+
+int
+main ()
+{
+  if (pinf != pone/zero)
+    abort ();
+
+  if (ninf != none/zero)
+    abort ();
+
+#ifdef HUGE_VAL
+  if (HUGE_VAL != pinf)
+    abort ();
+
+  if (-HUGE_VAL != ninf)
+    abort ();
+#endif
+
+  exit (0);
+}
