5e1a2fc70a27a62081cfd9a14aef12c806866cee
Jakub Jelinek <jakub@redhat.com>
2000-07-25 21:35:45 +0200

    i386.md (andsi_1+1): Allow HImode.
    	* config/i386/i386.md (andsi_1+1): Allow HImode.
    	(andsi_1+2): Require q_regs_operand.
    	* gcc.dg/20000720-1.c: New test.
    
    From-SVN: r35249

---
+++ gcc/testsuite/gcc.dg/20000720-1.c
@@ -0,0 +1,39 @@
+/* { dg-do compile { target i?86-*-* } } */
+/* { dg-options "-mpreferred-stack-boundary=2 -march=i586 -O2 -fomit-frame-pointer" } */
+
+extern void *foo(void *a, const void *b, unsigned c);
+
+extern inline void *
+bar(void *a, const void *b, unsigned c)
+{
+  int d0, d1, d2;
+  __asm__ __volatile__(
+    "" :
+    "=&c" (d0), "=&D" (d1), "=&S" (d2) :
+    "0" (c/4), "q" (c), "1" (a), "2" (b) :
+    "memory");
+  return a;
+}
+
+typedef struct {
+  unsigned char a;
+  unsigned b : 2;
+  unsigned c : 4;
+  unsigned d : 2;
+} *baz;
+
+static int
+dead(unsigned short *v, char *w, unsigned char *x, int y, int z)
+{
+  int i = 0;
+  unsigned short j = *v;
+
+  while (y > 0) {
+    ((baz)x)->a = j;
+    ((baz)x)->b = 0;
+    ((baz)x)->c = 0;
+    ((baz)x)->d = 0;
+    __builtin_constant_p(i) ? foo(x, w, i) : bar(x, w, i);
+  }
+  return z - y;
+}
