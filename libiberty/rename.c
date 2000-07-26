c2f275e484523e8a63d6751473d59c6fc63e0f74
Rodney Brown <RodneyBrown@pmsc.com>
2000-07-26 17:23:24 -0600

    Makefile.in (rename.o, waitpid.o): Depend on config.h
            * Makefile.in (rename.o, waitpid.o): Depend on config.h
            * rename.c: include config.h, unistd.h
            * waitpid.c: include config.h, sys/wait.h
    
    Co-Authored-By: Jeffrey A Law <law@cygnus.com>
    
    From-SVN: r35272

--- libiberty/rename.c
+++ libiberty/rename.c
@@ -3,7 +3,13 @@
 
 /* Rename a file.  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 #include <errno.h>
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
 
 int
 rename (zfrom, zto)
--- libiberty/waitpid.c
+++ libiberty/waitpid.c
@@ -1,3 +1,10 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#ifdef HAVE_SYS_WAIT_H
+#include <sys/wait.h>
+#endif
+
 int
 waitpid (pid, stat_loc, options)
 	int pid, *stat_loc, options;
