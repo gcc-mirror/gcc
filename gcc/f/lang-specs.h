be7d4ea4ff91751cbd3e05f5e470e5f838a8f64d
toon <toon@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-07-26 12:37:12 +0000

    2000-07-26  Toon Moene  <toon@moene.indiv.nluug.nl>
    	* lang-specs.h: Remove one /dev/null from tradcpp invocation.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@35264 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/f/lang-specs.h
+++ gcc/f/lang-specs.h
@@ -45,7 +45,7 @@ the Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
      as %(asm_options) %{!pipe:%g.s} %A }}}}\n"},
   /* XXX This is perverse and should not be necessary.  */
   {"@f77-version",
-   "tradcpp -lang-fortran %(cpp_options) /dev/null /dev/null \n\
+   "tradcpp -lang-fortran %(cpp_options) /dev/null \n\
     f771 -fnull-version %1 \
       %{!Q:-quiet} -dumpbase g77-version.f %{d*} %{m*} %{a*} \
       %{g*} %{O*} %{W*} %{w} %{pedantic*} \
