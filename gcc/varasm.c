ec1bff50af97c7cab92ba2c29ff841e88e697cb6
Hans-Peter Nilsson <hp@axis.com>
2000-07-27 01:35:17 +0000

    * varasm.c (assemble_variable) [! defined(ASM_OUTPUT_ALIGNED_COMMON)         && ! defined (ASM_OUTPUT_ALIGNED_BSS]): Also test         ! defined(ASM_OUTPUT_ALIGNED_DECL_COMMON) before complaining about         lack of implemented alignment.
    From-SVN: r35277

--- gcc/varasm.c
+++ gcc/varasm.c
@@ -1499,7 +1499,8 @@ assemble_variable (decl, top_level, at_end, dont_output_data)
       rounded = (rounded / (BIGGEST_ALIGNMENT / BITS_PER_UNIT)
 		 * (BIGGEST_ALIGNMENT / BITS_PER_UNIT));
       
-#if !defined(ASM_OUTPUT_ALIGNED_COMMON) && !defined(ASM_OUTPUT_ALIGNED_BSS)
+/* Don't continue this line--convex cc version 4.1 would lose.  */
+#if !defined(ASM_OUTPUT_ALIGNED_COMMON) && !defined(ASM_OUTPUT_ALIGNED_DECL_COMMON) && !defined(ASM_OUTPUT_ALIGNED_BSS)
       if ((unsigned HOST_WIDE_INT) DECL_ALIGN (decl) / BITS_PER_UNIT > rounded)
          warning_with_decl 
            (decl, "requested alignment for %s is greater than implemented alignment of %d.",rounded);
