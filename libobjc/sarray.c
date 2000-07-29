6d09d1f2a3b182a8ee8d5aeb9ad827521825e90b
ovidiu <ovidiu@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-07-29 07:11:04 +0000

    Removed the explicit prototypes for strlen and memcpy on 64-bit platforms (Suggested by Rodney Brown <rdb@cup.hp.com>).
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@35326 138bc75d-0d04-0410-961f-82ee72b054a4

--- libobjc/sarray.c
+++ libobjc/sarray.c
@@ -44,10 +44,6 @@ const char* __objc_sparse2_id = "2 level sparse indices";
 const char* __objc_sparse3_id = "3 level sparse indices";
 #endif
 
-#if defined(__alpha__) || (defined(__sparc__) && (defined(__sparcv9) || defined(__arch64__))) || (defined(__ia64__) && defined(__LP64__))
-const void *memcpy (void*, const void*, size_t);
-#endif
-
 /* This function removes any structures left over from free operations
    that were not safe in a multi-threaded environment. */
 void
