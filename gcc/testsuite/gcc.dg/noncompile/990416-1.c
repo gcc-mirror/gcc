5f1240b6bac53ded27c90f910dd2b930dac157a0
Zack Weinberg <zack@wolery.cumb.org>
2000-07-31 18:19:28 +0000

    c90-digraph-1.c: Restore { dg-do run } marker.
    	* gcc.dg/c90-digraph-1.c: Restore { dg-do run } marker.
    	* gcc.dg/noncompile/990416-1.c: Do not expect an implicit-
    	declaration warning for __builtin_dwarf_reg_size.
    
    From-SVN: r35387

--- gcc/testsuite/gcc.dg/noncompile/990416-1.c
+++ gcc/testsuite/gcc.dg/noncompile/990416-1.c
@@ -7,5 +7,5 @@ copy_reg (unsigned int reg, frame_state *udata,	/* { dg-error "parse" } */
   word_type *preg = get_reg_addr (reg, udata, 0);	/* { dg-error "undeclared|function|without a cast" } */
   word_type *ptreg = get_reg_addr (reg, target_udata, 0); /* { dg-error "undeclared|without a cast" } */
    
-  memcpy (ptreg, preg, __builtin_dwarf_reg_size (reg));	/* { dg-warning "implicit declaration" } */
+  memcpy (ptreg, preg, __builtin_dwarf_reg_size (reg));
 }
