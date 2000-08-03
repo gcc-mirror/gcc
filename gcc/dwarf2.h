254aa3380d9e92cde8ebb6301493a0864279a62f
nickc <nickc@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-08-03 22:41:38 +0000

    Change java language number to 0x000b
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@35462 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/dwarf2.h
+++ gcc/dwarf2.h
@@ -534,8 +534,8 @@ enum dwarf_source_language
     DW_LANG_Fortran90 = 0x0008,
     DW_LANG_Pascal83 = 0x0009,
     DW_LANG_Modula2 = 0x000a,
-    DW_LANG_Mips_Assembler = 0x8001,
-    DW_LANG_Java = 0x9af4
+    DW_LANG_Java = 0x000b,
+    DW_LANG_Mips_Assembler = 0x8001
   };
 
 
