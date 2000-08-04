4f449834ce9ec09ed80726b565e281942d419718
korbb <korbb@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-08-04 14:20:42 +0000

    Removed conditional #defines of macros GCC always defines
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@35480 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fixinc/tests/base/sys/types.h
+++ gcc/fixinc/tests/base/sys/types.h
@@ -10,25 +10,16 @@
 
 
 #if defined( GNU_TYPES_CHECK )
-#ifndef __PTRDIFF_TYPE__
-#define __PTRDIFF_TYPE__
-#endif
 #if !defined(_GCC_PTRDIFF_T)
 #define _GCC_PTRDIFF_T
 typedef __PTRDIFF_TYPE__ ptrdiff_t;
 #endif
  /* long int */
-#ifndef __SIZE_TYPE__
-#define __SIZE_TYPE__
-#endif
 #if !defined(_GCC_SIZE_T)
 #define _GCC_SIZE_T
 typedef __SIZE_TYPE__ size_t;
 #endif
  /* uint_t */
-#ifndef __WCHAR_TYPE__
-#define __WCHAR_TYPE__
-#endif
 #if !defined(_GCC_WCHAR_T) && ! defined(__cplusplus)
 #define _GCC_WCHAR_T
 typedef __WCHAR_TYPE__ wchar_t;
