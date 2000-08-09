91b57e101e3215bd6e6d906aba5fe8987175a8df
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-08-09 18:55:42 +0000

    * pt.c (tsubst_aggr_type): Bail if creating the argvec fails.         (tsubst_template_arg_vector): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@35592 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.pt/explarg1.C
@@ -0,0 +1,21 @@
+// Bug: g++ generates an error trying to generate the first foo<int>, when
+// it should silently fail and go on to the next one.
+
+template<class T, typename U> class A { };
+
+template<class T> void
+foo(const A<T,typename T::N>&);
+
+template<typename T>
+class B { };
+
+template<typename T> void
+foo(B<T> const &) { }
+
+int
+main(void)
+{
+  B<int> sa;
+
+  foo<int> (sa);
+}
