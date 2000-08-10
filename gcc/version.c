12629520b693e2ca20788783c11b8894a5bb6576
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-08-10 07:45:17 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@35601 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
 #include "gansidecl.h"
 #include "version.h"
 
-const char *const version_string = "2.96 20000809 (experimental)";
+const char *const version_string = "2.96 20000810 (experimental)";
--- libf2c/libU77/Version.c
+++ libf2c/libU77/Version.c
@@ -1,6 +1,6 @@
 static char junk[] = "\n@(#) LIBU77 VERSION 19980709\n";
 
-char __G77_LIBU77_VERSION__[] = "0.5.26 20000809 (prerelease)";
+char __G77_LIBU77_VERSION__[] = "0.5.26 20000810 (prerelease)";
 
 #include <stdio.h>
 
