c98a2029dcd44cec6903a0c14880ce9218ab7316
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-08-18 09:21:31 +0000

    * g++.old-deja/g++.pt/typename27.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@35776 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.pt/typename27.C
@@ -0,0 +1,21 @@
+// Build don't link:
+// 
+// Copyright (C) 2000 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 9 Aug 2000 <nathan@codesourcery.com>
+
+// bugs 173, 174 & 406 all ICE'd due to Koenig lookup involving
+// typename T::t.
+
+struct A
+{
+  typedef int type;
+};
+
+template<typename T> void same_key (T, typename T::type);
+
+template <class T> void foo (T *, void (*) (T, int));
+
+void baz (A *ptr)
+{
+  foo (ptr, same_key);
+}
