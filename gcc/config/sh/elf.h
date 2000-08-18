ffe51c8a82160ef337ab46d3aac2da15851704dd
Alexandre Oliva <aoliva@redhat.com>
2000-08-18 19:39:52 +0000

    elf.h: Do not include sh/sh.h.
    * config/sh/elf.h: Do not include sh/sh.h.
    * config/sh/rtems.h: Likewise.
    * config/sh/rtemself.h: Do not include sh/elf.h.
    * configure.in: Get them included with `tm_file's.
    * configure: Rebuilt.
    
    From-SVN: r35793

--- gcc/config/sh/elf.h
+++ gcc/config/sh/elf.h
@@ -19,9 +19,6 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-/* Mostly like the regular SH configuration.  */
-#include "sh/sh.h"
-
 /* No SDB debugging info.  */
 #undef SDB_DEBUGGING_INFO
 
