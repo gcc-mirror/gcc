92f8096ad961d138a9de5c717c1c2eae46e6f2ec
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-08-22 08:41:02 +0000

    New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@35872 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20000822-1.c
@@ -0,0 +1,24 @@
+int f0(int (*fn)(int *), int *p)
+{
+  return (*fn) (p);
+}
+
+int f1(void)
+{
+  int i = 0;
+
+  int f2(int *p)
+  {
+    i = 1;
+    return *p + 1;
+  }
+
+  return f0(f2, &i);
+}
+
+int main()
+{
+  if (f1() != 2)
+    abort ();
+  return 0;
+}
