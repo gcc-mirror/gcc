0551c32d8bd642833f411c7cea2b870b3d87a70d
Richard Henderson <rth@cygnus.com>
2000-08-24 14:40:24 -0700

    stmt.c (expand_asm_operands): Treat EXTRA_CONSTRAINT like g or X.
    	* stmt.c (expand_asm_operands): Treat EXTRA_CONSTRAINT like g or X.
    
    	* config/ia64/ia64-protos.h: Update.
    	* config/ia64/ia64.c (gr_register_operand): New.
    	(fr_register_operand, grfr_register_operand): New.
    	(gr_nonimmediate_operand, grfr_nonimmediate_operand): New.
    	(grfr_reg_or_8bit_operand): New.
    	(gr_reg_or_0_operand): Rename from reg_or_0_operand and
    	use gr_register_operand.
    	(gr_reg_or_5bit_operand, gr_reg_or_6bit_operand): Likewise.
    	(gr_reg_or_8bit_operand, gr_reg_or_8bit_adjusted_operand): Likewise.
    	(gr_reg_or_8bit_and_adjusted_operand): Likewise.
    	(gr_reg_or_14bit_operand, gr_reg_or_22bit_operand): Likewise.
    	(fr_reg_or_fp01_operand): Likewise.
    	(not_postinc_memory_operand): New.
    	(ia64_split_timode): Remove unused variables.
    	(rtx_needs_barrier): Check arguments to cmpxchg.
    	(builtin_description): Remove.
    	(bdesc_2argsi, bdesc_2argdi): Remove.
    	(ia64_init_builtins): Declare all builtins directly.
    	(ia64_expand_fetch_and_op): Rewrite to be called from
    	ia64_expand_builtin directly.  Use expand_binop and co.
    	(ia64_expand_op_and_fetch): Likewise.
    	(ia64_expand_compare_and_swap): Likewise.
    	(ia64_expand_binop_builtin): Remove.
    	(ia64_expand_lock_test_and_set): New.
    	(ia64_expand_lock_release): New.
    	(ia64_expand_builtin): Use them.
    	* config/ia64/ia64.h (CONSTRAINT_OK_FOR_S): New.
    	(EXTRA_CONSTRAINT): Use it.
    	(PREDICATE_CODES): Update.
    	* config/ia64/ia64.md (*): Use gr_register_operand and co.
    	(mf): Indicate that we set memory as well as use it.
    	(fetchadd_acq_si): Show memory being modified as well.
    	(fetchadd_acq_di, cmpxchg_acq_si, cmpxchg_acq_di): Likewise.
    	(val_compare_and_swap_si, val_compare_and_swap_di): Remove.
    	(lock_test_and_set_si, lock_test_and_set_di): Remove.
    	(fetch_and_add_si, fetch_and_sub_si, fetch_and_or_si): Remove.
    	(fetch_and_and_si, fetch_and_xor_si, fetch_and_nand_si): Remove.
    	(fetch_and_add_di, fetch_and_sub_di, fetch_and_or_di): Remove.
    	(fetch_and_and_di, fetch_and_xor_di, fetch_and_nand_di): Remove.
    	(add_and_fetch_di, sub_and_fetch_di, or_and_fetch_di): Remove.
    	(and_and_fetch_di, xor_and_fetch_di, nand_and_fetch_di): Remove.
    	(add_and_fetch_si, sub_and_fetch_si, or_and_fetch_si): Remove.
    	(and_and_fetch_si, xor_and_fetch_si, nand_and_fetch_si): Remove.
    	* config/ia64/ia64intrin.h (*): Cast result to the appropriate
    	return type.  Pretty print definitions.
    
    From-SVN: r35956

--- gcc/config/ia64/ia64intrin.h
+++ gcc/config/ia64/ia64intrin.h
@@ -1,60 +1,132 @@
 #ifndef _IA64INTRIN_H_INCLUDED
 #define _IA64INTRIN_H_INCLUDED
 
-void __sync_synchronize (void);
+/* Actually, everything is a compiler builtin, but just so
+   there's no confusion... */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-int  __sync_val_compare_and_swap_si (int *, int, int);
-long __sync_val_compare_and_swap_di (long *, long, long);
-#define __sync_val_compare_and_swap(A,B,C) ((sizeof (*(A)) == sizeof(int)) ? __sync_val_compare_and_swap_si((int *)(A),(int)(B),(int)(C)) : __sync_val_compare_and_swap_di((long *)(A),(long)(B),(long)(C)))
+extern void __sync_synchronize (void);
 
-int  __sync_bool_compare_and_swap_si (int *, int, int);
-long __sync_bool_compare_and_swap_di (long *, long, long);
-#define __sync_bool_compare_and_swap(A,B,C) ((sizeof (*(A)) == sizeof(int)) ? __sync_bool_compare_and_swap_si((int *)(A),(int)(B),(int)(C)) : __sync_bool_compare_and_swap_di((long *)(A),(long)(B),(long)(C)))
+extern int __sync_val_compare_and_swap_si (int *, int, int);
+extern long __sync_val_compare_and_swap_di (long *, long, long);
+#define __sync_val_compare_and_swap(PTR, OLD, NEW)			\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR)))						\
+    __sync_val_compare_and_swap_si((int *)(PTR),(int)(OLD),(int)(NEW))	\
+  : (__typeof__(*(PTR)))						\
+    __sync_val_compare_and_swap_di((long *)(PTR),(long)(OLD),(long)(NEW)))
 
-void __sync_lock_release_si (int *);
-void __sync_lock_release_di (long *);
-#define __sync_lock_release(A) ((sizeof (*(A)) == sizeof(int)) ? __sync_lock_release_si((int *)(A)) : __sync_lock_release_di((long *)(A)))
+extern int __sync_bool_compare_and_swap_si (int *, int, int);
+extern long __sync_bool_compare_and_swap_di (long *, long, long);
+#define __sync_bool_compare_and_swap(PTR, OLD, NEW)			\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR)))						\
+    __sync_bool_compare_and_swap_si((int *)(PTR),(int)(OLD),(int)(NEW))	\
+  : (__typeof__(*(PTR)))						\
+    __sync_bool_compare_and_swap_di((long *)(PTR),(long)(OLD),(long)(NEW)))
 
-int  __sync_lock_test_and_set_si (int *, int);
-long __sync_lock_test_and_set_di (long *, long);
-#define __sync_lock_test_and_set(A,B) ((sizeof (*(A)) == sizeof(int)) ? __sync_lock_test_and_set_si((int *)(A),(int)(B)) : __sync_lock_test_and_set_di((long *)(A),(long)(B)))
+extern void __sync_lock_release_si (int *);
+extern void __sync_lock_release_di (long *);
+#define __sync_lock_release(PTR)		\
+  ((sizeof (*(PTR)) == sizeof(int))		\
+   ? __sync_lock_release_si((int *)(PTR))	\
+   : __sync_lock_release_di((long *)(PTR)))
 
-int  __sync_fetch_and_add_si  (int *, int);
-int  __sync_fetch_and_sub_si  (int *, int);
-int  __sync_fetch_and_and_si  (int *, int);
-int  __sync_fetch_and_or_si   (int *, int);
-int  __sync_fetch_and_xor_si  (int *, int);
-int  __sync_fetch_and_nand_si (int *, int);
-long __sync_fetch_and_add_di  (long *, long);
-long __sync_fetch_and_sub_di  (long *, long);
-long __sync_fetch_and_and_di  (long *, long);
-long __sync_fetch_and_or_di   (long *, long);
-long __sync_fetch_and_xor_di  (long *, long);
-long __sync_fetch_and_nand_di (long *, long);
-#define __sync_fetch_and_add(A,B) ((sizeof (*(A)) == sizeof(int)) ? __sync_fetch_and_add_si((int *)(A),(int)(B)) : __sync_fetch_and_add_di((long *)(A),(long)(B)))
-#define __sync_fetch_and_sub(A,B) ((sizeof (*(A)) == sizeof(int)) ? __sync_fetch_and_sub_si((int *)(A),(int)(B)) : __sync_fetch_and_sub_di((long *)(A),(long)(B)))
-#define __sync_fetch_and_and(A,B) ((sizeof (*(A)) == sizeof(int)) ? __sync_fetch_and_and_si((int *)(A),(int)(B)) : __sync_fetch_and_and_di((long *)(A),(long)(B)))
-#define __sync_fetch_and_or(A,B) ((sizeof (*(A)) == sizeof(int)) ? __sync_fetch_and_or_si((int *)(A),(int)(B)) : __sync_fetch_and_or_di((long *)(A),(long)(B)))
-#define __sync_fetch_and_xor(A,B) ((sizeof (*(A)) == sizeof(int)) ? __sync_fetch_and_xor_si((int *)(A),(int)(B)) : __sync_fetch_and_xor_di((long *)(A),(long)(B)))
-#define __sync_fetch_and_nand(A,B) ((sizeof (*(A)) == sizeof(int)) ? __sync_fetch_and_nand_si((int *)(A),(int)(B)) : __sync_fetch_and_nand_di((long *)(A),(long)(B)))
+extern int __sync_lock_test_and_set_si (int *, int);
+extern long __sync_lock_test_and_set_di (long *, long);
+#define __sync_lock_test_and_set(PTR,VAL)				\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR))) __sync_lock_test_and_set_si((int *)(PTR),(int)(VAL)) \
+  : (__typeof__(*(PTR))) __sync_lock_test_and_set_di((long *)(PTR),(long)(VAL)))
 
-int  __sync_add_and_fetch_si  (int *, int);
-int  __sync_sub_and_fetch_si  (int *, int);
-int  __sync_and_and_fetch_si  (int *, int);
-int  __sync_or_and_fetch_si   (int *, int);
-int  __sync_xor_and_fetch_si  (int *, int);
-int  __sync_nand_and_fetch_si (int *, int);
-long __sync_add_and_fetch_di  (long *, long);
-long __sync_sub_and_fetch_di  (long *, long);
-long __sync_and_and_fetch_di  (long *, long);
-long __sync_or_and_fetch_di   (long *, long);
-long __sync_xor_and_fetch_di  (long *, long);
-long __sync_nand_and_fetch_di (long *, long);
-#define __sync_add_and_fetch(A,B) ((sizeof (*(A)) == sizeof(int)) ? __sync_add_and_fetch_si((int *)(A),(int)(B)) : __sync_add_and_fetch_di((long *)(A),(long)(B)))
-#define __sync_sub_and_fetch(A,B) ((sizeof (*(A)) == sizeof(int)) ? __sync_sub_and_fetch_si((int *)(A),(int)(B)) : __sync_sub_and_fetch_di((long *)(A),(long)(B)))
-#define __sync_and_and_fetch(A,B) ((sizeof (*(A)) == sizeof(int)) ? __sync_and_and_fetch_si((int *)(A),(int)(B)) : __sync_and_and_fetch_di((long *)(A),(long)(B)))
-#define __sync_or_and_fetch(A,B) ((sizeof (*(A)) == sizeof(int)) ? __sync_or_and_fetch_si((int *)(A),(int)(B)) : __sync_or_and_fetch_di((long *)(A),(long)(B)))
-#define __sync_xor_and_fetch(A,B) ((sizeof (*(A)) == sizeof(int)) ? __sync_xor_and_fetch_si((int *)(A),(int)(B)) : __sync_xor_and_fetch_di((long *)(A),(long)(B)))
-#define __sync_nand_and_fetch(A,B) ((sizeof (*(A)) == sizeof(int)) ? __sync_nand_and_fetch_si((int *)(A),(int)(B)) : __sync_nand_and_fetch_di((long *)(A),(long)(B)))
+extern int __sync_fetch_and_add_si (int *, int);
+extern long __sync_fetch_and_add_di (long *, long);
+#define __sync_fetch_and_add(PTR,VAL)					\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR))) __sync_fetch_and_add_si((int *)(PTR),(int)(VAL)) \
+  : (__typeof__(*(PTR))) __sync_fetch_and_add_di((long *)(PTR),(long)(VAL)))
+
+extern int __sync_fetch_and_sub_si (int *, int);
+extern long __sync_fetch_and_sub_di (long *, long);
+#define __sync_fetch_and_sub(PTR,VAL)					\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR))) __sync_fetch_and_sub_si((int *)(PTR),(int)(VAL)) \
+  : (__typeof__(*(PTR))) __sync_fetch_and_sub_di((long *)(PTR),(long)(VAL)))
+
+extern int __sync_fetch_and_and_si (int *, int);
+extern long __sync_fetch_and_and_di (long *, long);
+#define __sync_fetch_and_and(PTR,VAL)					\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR))) __sync_fetch_and_and_si((int *)(PTR),(int)(VAL)) \
+  : (__typeof__(*(PTR))) __sync_fetch_and_and_di((long *)(PTR),(long)(VAL)))
+
+extern int __sync_fetch_and_or_si (int *, int);
+extern long __sync_fetch_and_or_di (long *, long);
+#define __sync_fetch_and_or(PTR,VAL)					\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR))) __sync_fetch_and_or_si((int *)(PTR),(int)(VAL)) \
+  : (__typeof__(*(PTR))) __sync_fetch_and_or_di((long *)(PTR),(long)(VAL)))
+
+extern int __sync_fetch_and_xor_si (int *, int);
+extern long __sync_fetch_and_xor_di (long *, long);
+#define __sync_fetch_and_xor(PTR,VAL)					\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR))) __sync_fetch_and_xor_si((int *)(PTR),(int)(VAL)) \
+  : (__typeof__(*(PTR))) __sync_fetch_and_xor_di((long *)(PTR),(long)(VAL)))
+
+extern int __sync_fetch_and_nand_si (int *, int);
+extern long __sync_fetch_and_nand_di (long *, long);
+#define __sync_fetch_and_nand(PTR,VAL)					\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR))) __sync_fetch_and_nand_si((int *)(PTR),(int)(VAL)) \
+  : (__typeof__(*(PTR))) __sync_fetch_and_nand_di((long *)(PTR),(long)(VAL)))
+
+extern int __sync_add_and_fetch_si (int *, int);
+extern long __sync_add_and_fetch_di (long *, long);
+#define __sync_add_and_fetch(PTR,VAL)					\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR))) __sync_add_and_fetch_si((int *)(PTR),(int)(VAL)) \
+  : (__typeof__(*(PTR))) __sync_add_and_fetch_di((long *)(PTR),(long)(VAL)))
+
+extern int __sync_sub_and_fetch_si (int *, int);
+extern long __sync_sub_and_fetch_di (long *, long);
+#define __sync_sub_and_fetch(PTR,VAL)					\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR))) __sync_sub_and_fetch_si((int *)(PTR),(int)(VAL)) \
+  : (__typeof__(*(PTR))) __sync_sub_and_fetch_di((long *)(PTR),(long)(VAL)))
+
+extern int __sync_and_and_fetch_si (int *, int);
+extern long __sync_and_and_fetch_di (long *, long);
+#define __sync_and_and_fetch(PTR,VAL)					\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR))) __sync_and_and_fetch_si((int *)(PTR),(int)(VAL)) \
+  : (__typeof__(*(PTR))) __sync_and_and_fetch_di((long *)(PTR),(long)(VAL)))
+
+extern int __sync_or_and_fetch_si (int *, int);
+extern long __sync_or_and_fetch_di (long *, long);
+#define __sync_or_and_fetch(PTR,VAL)					\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR))) __sync_or_and_fetch_si((int *)(PTR),(int)(VAL)) \
+  : (__typeof__(*(PTR))) __sync_or_and_fetch_di((long *)(PTR),(long)(VAL)))
+
+extern int __sync_xor_and_fetch_si (int *, int);
+extern long __sync_xor_and_fetch_di (long *, long);
+#define __sync_xor_and_fetch(PTR,VAL)					\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR))) __sync_xor_and_fetch_si((int *)(PTR),(int)(VAL)) \
+  : (__typeof__(*(PTR))) __sync_xor_and_fetch_di((long *)(PTR),(long)(VAL)))
+
+extern int __sync_nand_and_fetch_si (int *, int);
+extern long __sync_nand_and_fetch_di (long *, long);
+#define __sync_nand_and_fetch(PTR,VAL)					\
+ ((sizeof (*(PTR)) == sizeof(int))					\
+  ? (__typeof__(*(PTR))) __sync_nand_and_fetch_si((int *)(PTR),(int)(VAL)) \
+  : (__typeof__(*(PTR))) __sync_nand_and_fetch_di((long *)(PTR),(long)(VAL)))
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
