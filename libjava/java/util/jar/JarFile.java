cf3e246c38c1755fb0054ce57b4bc573c3312402
mark <mark@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-08-27 22:26:27 +0000

    * java/util/zip/ZipFile.java: Implement OPEN_DELETE mode, new constructor,     close can delete the file, finalize calls close.     * java/util/jar/JarFile.java: Constructor that takes mode now calls super.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@36007 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/util/jar/JarFile.java
+++ libjava/java/util/jar/JarFile.java
@@ -129,15 +129,17 @@ public class JarFile extends ZipFile {
     }
 
     /**
-     * XXX - not yet implemented in java.util.zip.ZipFile
+     * Creates a new JarFile with the indicated mode, tries to read the
+     * manifest and if the manifest exists and verify is true verfies it.
      *
      * @param file the file to open to open as a jar file
      * @param verify checks manifest and entries when true and a manifest
      * exists, when false no checks are made
-     * @param mode XXX - see ZipFile
-     * @exception FileNotFoundException XXX
-     * @exception IOException XXX
-     * @exception IllegalArgumentException XXX
+     * @param mode either ZipFile.OPEN_READ or
+     *             (ZipFile.OPEN_READ | ZipFile.OPEN_DELETE)
+     * @exception FileNotFoundException if the file does not exist
+     * @exception IOException if another IO exception occurs while reading
+     * @exception IllegalArgumentException when given an illegal mode
      * 
      * @since 1.3
      */
@@ -145,12 +147,10 @@ public class JarFile extends ZipFile {
                                                     FileNotFoundException,
                                                     IOException,
                                                     IllegalArgumentException {
-        // XXX - For now don't use super(file, mode)
-        this(file, verify);
-        /* super(file, mode);
-           manifest = readManifest();
-           if (verify)
-               verify(); */
+        super(file, mode);
+        manifest = readManifest();
+        if (verify)
+            verify();
     }
 
     // Methods
