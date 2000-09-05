a0d2701931106b2273fb1d37ab376d419d6b51a0
ghazi <ghazi@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-09-05 15:52:44 +0000

    * gcc.dg/wtr-int-type-1.c: Fix for 64-bit platforms.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@36160 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/wtr-int-type-1.c
+++ gcc/testsuite/gcc.dg/wtr-int-type-1.c
@@ -9,16 +9,37 @@ testfunc (void)
 {
   long long i;
   
+  /* Octal and hex values shouldn't issue -Wtraditional warnings. */
   i = 0x80000000;
+  i = 0xFFFFFFFF;
+  i = 037777777777;
+
   i = 0x8000000000000000;
-  i = 9223372036854775807; /* { dg-warning "integer constant.*with -traditional" "integer constant" } */
-  i = 4294967295; /* { dg-warning "decimal constant|integer constant.*with -traditional" "integer constant" } */
+  i = 0xFFFFFFFFFFFFFFFF;
+  i = 01777777777777777777777;
+
+  /* We expect to get either a "width of integer constant changes with
+     -traditional" warning or an "integer constant is unsigned in ISO
+     C, signed with -traditional" warning depending on whether we are
+     testing on a 32 or 64 bit platform.  Either warning means the
+     test passes and both matched by checking for "integer constant".  */
+  i = 18446744073709551615; /* { dg-warning "integer constant" "integer constant" } */
   
-#line 18 "sys-header.h" 3
+#line 29 "sys-header.h" 3
 /* We are in system headers now, no -Wtraditional warnings should issue.  */
 
   i = 0x80000000;
+  i = 0xFFFFFFFF;
+  i = 037777777777;
+  
   i = 0x8000000000000000;
+  i = 0xFFFFFFFFFFFFFFFF;
+  i = 01777777777777777777777;
+  
   i = 9223372036854775807;
-  i = 4294967295; /* { dg-warning "decimal constant" "decimal constant" } */
+  i = 18446744073709551615;
 }
+
+/* Ignore "decimal constant is so large that it is unsigned" warnings.  */
+/* { dg-warning "decimal constant" "decimal constant" { target *-*-* } 26 } */
+/* { dg-warning "decimal constant" "decimal constant" { target *-*-* } 40 } */
