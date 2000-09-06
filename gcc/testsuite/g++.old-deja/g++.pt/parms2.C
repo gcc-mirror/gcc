8bec73885bf82c84b4d9e72d5fcc3865189b1bb4
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-09-06 09:22:08 +0000

    * g++.old-deja/g++.pt/parms2.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@36194 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.pt/parms2.C
@@ -0,0 +1,39 @@
+// Build don't link:
+// 
+// Copyright (C) 2000 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 5 Sept 2000 <nathan@codesourcery.com>
+
+// bug 73. We failed to compare explicit arguments for a TEMPLATE_ID_EXPR in a
+// TYPENAME_TYPE.
+
+struct Plus {
+  template <class T>
+  struct Sig { typedef int Third;};
+};
+
+
+template <class T>
+struct Ethel {
+  typedef int WrappedType;
+};
+
+struct Fred {
+   
+  template <class Q, class LA, class LB>
+  Ethel<typename Q::Sig<typename LA::WrappedType>::Third> baz ();
+
+
+  template <class Z, class A, class B>
+  Ethel<typename Z::Sig<A>::Third>
+  foo ( const Z&, const Ethel<A>&, const Ethel<B>&) const;
+};
+
+int main() {
+  Fred f;
+  Ethel<int> e;
+  Plus p;
+ 
+  f.foo (p, e, e);
+
+  return 0;
+}
