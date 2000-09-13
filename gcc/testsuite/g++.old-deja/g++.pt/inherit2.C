634dd0ca7756a22847e2327b6270792cbd042495
Jason Merrill <jason@gcc.gnu.org>
2000-09-12 20:59:16 -0400

    new
    From-SVN: r36381

---
+++ gcc/testsuite/g++.old-deja/g++.pt/inherit2.C
@@ -0,0 +1,20 @@
+// Test that we warn about unqualified references to implicit typenames.
+// Bug: g++ is confused by the binding for ::AN and crashes.
+// Special g++ Options:
+// crash test - XFAIL *-*-*
+
+template <class T> struct A {
+  struct AA { };
+  struct AB { };
+  struct AC { };
+};
+
+template <class T> struct B: public A<T> {
+  friend struct B::AA;		// OK
+  friend AB;			// WARNING - needs class-key
+  friend struct AC;		// WARNING - refers to ::AC
+};
+
+B<int> b;
+
+int main () { }
