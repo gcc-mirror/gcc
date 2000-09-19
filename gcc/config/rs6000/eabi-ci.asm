ed3ef5d3ba6d15c5b1fa789566bf75d37b151e24
geoffk <geoffk@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-09-19 01:25:31 +0000

    * config/rs6000/eabi-cn.asm: Use /**/ to delimit comments, not #. 	* config/rs6000/eabi-ci.asm: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@36521 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/rs6000/eabi-ci.asm
+++ gcc/config/rs6000/eabi-ci.asm
@@ -1,40 +1,40 @@
-# crti.s for eabi
+/* crti.s for eabi
+   Copyright (C) 1996, 2000 Free Software Foundation, Inc.
+   Written By Michael Meissner
 
-#   Copyright (C) 1996, 2000 Free Software Foundation, Inc.
-#   Written By Michael Meissner
-# 
-# This file is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation; either version 2, or (at your option) any
-# later version.
-# 
-# In addition to the permissions in the GNU General Public License, the
-# Free Software Foundation gives you unlimited permission to link the
-# compiled version of this file with other programs, and to distribute
-# those programs without any restriction coming from the use of this
-# file.  (The General Public License restrictions do apply in other
-# respects; for example, they cover modification of the file, and
-# distribution when not linked into another program.)
-# 
-# This file is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; see the file COPYING.  If not, write to
-# the Free Software Foundation, 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
-# 
-#    As a special exception, if you link this library with files
-#    compiled with GCC to produce an executable, this does not cause
-#    the resulting executable to be covered by the GNU General Public License.
-#    This exception does not however invalidate any other reasons why
-#    the executable file might be covered by the GNU General Public License.
-# 
+This file is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
 
-# This file just supplies labeled starting points for the .got* and other
-# special sections.  It is linked in first before other modules.
+In addition to the permissions in the GNU General Public License, the
+Free Software Foundation gives you unlimited permission to link the
+compiled version of this file with other programs, and to distribute
+those programs without any restriction coming from the use of this
+file.  (The General Public License restrictions do apply in other
+respects; for example, they cover modification of the file, and
+distribution when not linked into another program.)
+
+This file is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+   As a special exception, if you link this library with files
+   compiled with GCC to produce an executable, this does not cause
+   the resulting executable to be covered by the GNU General Public License.
+   This exception does not however invalidate any other reasons why
+   the executable file might be covered by the GNU General Public License.
+
+ */
+
+/* This file just supplies labeled starting points for the .got* and other
+   special sections.  It is linked in first before other modules.  */
  
 	.file	"crti.s"
 	.ident	"GNU C crti.s"
@@ -107,7 +107,7 @@ __EXCEPT_START__:
 	.type	__EH_FRAME_BEGIN__,@object
 __EH_FRAME_BEGIN__:
 
-# Head of __init function used for static constructors
+/* Head of __init function used for static constructors.  */
 	.section ".init","ax"
 	.align 2
 FUNC_START(__init)
@@ -115,7 +115,7 @@ FUNC_START(__init)
 	mflr 0
 	stw 0,20(1)
 
-# Head of __fini function used for static destructors
+/* Head of __fini function used for static destructors.  */
 	.section ".fini","ax"
 	.align 2
 FUNC_START(__fini)
--- gcc/config/rs6000/eabi-cn.asm
+++ gcc/config/rs6000/eabi-cn.asm
@@ -1,40 +1,40 @@
-# crtn.s for eabi
+/* crtn.s for eabi
+   Copyright (C) 1996, 2000 Free Software Foundation, Inc.
+   Written By Michael Meissner
 
-#   Copyright (C) 1996, 2000 Free Software Foundation, Inc.
-#   Written By Michael Meissner
-# 
-# This file is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation; either version 2, or (at your option) any
-# later version.
-# 
-# In addition to the permissions in the GNU General Public License, the
-# Free Software Foundation gives you unlimited permission to link the
-# compiled version of this file with other programs, and to distribute
-# those programs without any restriction coming from the use of this
-# file.  (The General Public License restrictions do apply in other
-# respects; for example, they cover modification of the file, and
-# distribution when not linked into another program.)
-# 
-# This file is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; see the file COPYING.  If not, write to
-# the Free Software Foundation, 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
-# 
-#    As a special exception, if you link this library with files
-#    compiled with GCC to produce an executable, this does not cause
-#    the resulting executable to be covered by the GNU General Public License.
-#    This exception does not however invalidate any other reasons why
-#    the executable file might be covered by the GNU General Public License.
-# 
+This file is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
 
-# This file just supplies labeled ending points for the .got* and other
-# special sections.  It is linked in last after other modules.
+In addition to the permissions in the GNU General Public License, the
+Free Software Foundation gives you unlimited permission to link the
+compiled version of this file with other programs, and to distribute
+those programs without any restriction coming from the use of this
+file.  (The General Public License restrictions do apply in other
+respects; for example, they cover modification of the file, and
+distribution when not linked into another program.)
+
+This file is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+   As a special exception, if you link this library with files
+   compiled with GCC to produce an executable, this does not cause
+   the resulting executable to be covered by the GNU General Public License.
+   This exception does not however invalidate any other reasons why
+   the executable file might be covered by the GNU General Public License.
+
+  */
+
+/* This file just supplies labeled ending points for the .got* and other
+   special sections.  It is linked in last after other modules.  */
  
 	.file	"crtn.s"
 	.ident	"GNU C crtn.s"
@@ -100,14 +100,14 @@ __EXCEPT_END__:
 __EH_FRAME_END__:
         .long   0
 
-# Tail of __init used for static constructors in Solaris
+/* Tail of __init function used for static constructors.  */
 	.section ".init","ax"
 	lwz 0,20(1)
 	mtlr 0
 	addi 1,1,16
 	blr
 
-# Tail of __fini used for static destructors in Solaris
+/* Tail of __fini function used for static destructors.  */
 	.section ".fini","ax"
 	lwz 0,20(1)
 	mtlr 0
