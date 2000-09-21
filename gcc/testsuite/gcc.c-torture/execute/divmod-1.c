d02cbd6922138f1219073b7dff467338edbe147d
nickc <nickc@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-09-21 17:30:11 +0000

    Add extra tests (for modulos of very large dividends by very small divisors)
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@36561 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/divmod-1.c
+++ gcc/testsuite/gcc.c-torture/execute/divmod-1.c
@@ -50,6 +50,22 @@ mod4 (x, y)
   return x % y;
 }
 
+signed long
+mod5 (x, y)
+     signed long x;
+     signed long y;
+{
+  return x % y;
+}
+     
+unsigned long
+mod6 (x, y)
+     unsigned long x;
+     unsigned long y;
+{
+  return x % y;
+}
+     
 main ()
 {
   if (div1 (-(1 << 7)) != 1 << 7)
@@ -68,5 +84,10 @@ main ()
     abort ();
   if (mod4 (-(1 << 15), -1) != 0)
     abort ();
+  if (mod5 (0x50000000, 2) != 0)
+    abort ();
+  if (mod6 (0x50000000, 2) != 0)
+    abort ();
+  
   exit (0);
 }
