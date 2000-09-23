87cc8a1d1d8bc6977cb595a2f0ff063252e9f061
Neil Booth <neil@gcc.gnu.org>
2000-09-23 11:47:05 +0000

    20000510-1.S: Rename assembler.S.
            * gcc.dg/cpp/20000510-1.S: Rename assembler.S.  Add test
            that we continue to preprocess after an unknown directive.
    	* gcc.dg/cpp/tr-str.c: Run-time test for traditional
    	stringification.
    
    From-SVN: r36577

--- gcc/testsuite/gcc.dg/cpp/tr-str.c
+++ gcc/testsuite/gcc.dg/cpp/tr-str.c
@@ -1,4 +1,17 @@
 /* Test whether traditional stringify works.  */
-/* { dg-do preprocess } */
+/* { dg-do run } */
 /* { dg-options "-traditional" } */
 #define foo(a, b) c="a"; d="b";
+
+extern void abort ();
+
+int main ()
+{
+  char *c, *d;
+
+  foo (p, q);
+  if (c[0] != 'p' || d[0] != 'q')
+    abort ();
+
+  exit (0);
+}
