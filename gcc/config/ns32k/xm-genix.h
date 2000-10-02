776749c5f2d9a2e3e1fc34d2ae1738012c773ba0
Mark Mitchell <mark@codesourcery.com>
2000-10-02 06:50:52 +0000

    genix.h: Remove.
    	* config/ns32k/genix.h: Remove.
    	* config/ns32k/x-genix: Likewise.
    	* config/ns32k/xm-genix.h: Likewise.
    	* config/fx80: Remove all filee in directory.
    	* config/pyr: Likewise.
    	* config/tahoe: Likewise.
    	* config/gmicro: Likewise.
    	* config/spur: Likewise.
    	* configure.in: Remove configury bits for above targets.
    	* configure: Regenerated.
    
    From-SVN: r36691

--- gcc/config/ns32k/xm-genix.h
+++
@@ -1,7 +0,0 @@
-/* Config file for ns32k running system V.  */
-
-#define memcpy(src,dst,len) bcopy ((dst),(src),(len))
-#define memset gcc_memset
-#define memcmp(left,right,len) bcmp ((left),(right),(len))
-
-#define USG
