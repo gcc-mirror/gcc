f7b9c39dc89744b6362eb2db35c082f68a1429bd
William Cohen <wcohen@gcc.gnu.org>
2000-10-04 17:57:03 +0000

    A test to verify the patch to warn of initializers for zero-length arrays is installed in the compiler.
    From-SVN: r36711

---
+++ gcc/testsuite/gcc.dg/20000926-1.c
@@ -0,0 +1,27 @@
+/* Copyright (C) 2000  Free Software Foundation.
+
+   by William Cohen  <wcohen@redhat.com>  */
+
+/* { dg-do compile } */
+/* { dg-options "" } */
+
+struct PDATA
+{
+    unsigned int  Dummy:32;
+    const char*   PName;
+};
+
+typedef struct PDATA    P_DATA;
+
+struct PLAYBOOK {
+        const char * BookName;
+        P_DATA       Play[0];
+};
+
+struct PLAYBOOK playbook  =
+{
+  "BookName",
+  {
+    { 1, "PName0" }, /* { dg-warning "excess elements in array initializer|(near initialization for `playbook.Play')" } */
+  }
+};
