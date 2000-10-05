b2f9816ffd08532b0f6a809d6e074300226e9a79
Nathan Sidwell <nathan@codesourcery.com>
2000-10-05 08:31:39 +0000

    * g++.old-deja/g++.pt/crash59.C: New test.
    From-SVN: r36721

---
+++ gcc/testsuite/g++.old-deja/g++.pt/crash59.C
@@ -0,0 +1,20 @@
+// Build don't link:
+// Copyright (C) 2000 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 4 Oct 2000 <nathan@codesourcery.com>
+
+// Bug 532. We failed to bail out when tsubsting a _DECL failed
+
+class ATOMSET
+{
+};
+
+template <class T>
+void addConstsTo(const T &container)
+{
+typename T::const_iterator l = 0; // ERROR - no type const_iterator
+}
+
+void tallyConstants()
+{
+addConstsTo(ATOMSET());
+}
