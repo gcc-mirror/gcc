c2e3a3f170240abe46588ad1737db8bdb6a6cb1d
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-10-06 09:13:11 +0000

    * g++.old-deja/g++.pt/enum14.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@36756 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.pt/crash59.C
+++ gcc/testsuite/g++.old-deja/g++.pt/crash59.C
@@ -1,6 +1,7 @@
 // Build don't link:
 // Copyright (C) 2000 Free Software Foundation, Inc.
 // Contributed by Nathan Sidwell 4 Oct 2000 <nathan@codesourcery.com>
+// Origin: Bug 543 Gerald Pfeifer <pfeifer@dbai.tuwien.ac.at>
 
 // Bug 532. We failed to bail out when tsubsting a _DECL failed
 
