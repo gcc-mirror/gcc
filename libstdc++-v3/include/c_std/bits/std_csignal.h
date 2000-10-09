22aef5143dec8081980621d99f929d4b888711ce
Benjamin Kosnik <bkoz@gcc.gnu.org>
2000-10-09 08:43:31 +0000

    configopts.html: Edit.
    2000-10-10  Benjamin Kosnik  <bkoz@purist.soma.redhat.com>
    
    	* docs/configopts.html: Edit.
    
    	* libio/Makefile.am (INCLUDES): Remove duplicates, simplify.
    	* libio/Makefile.in: Regenerate.
    	* libmath/Makefile.am (INCLUDES): Remove duplicates, simplify.
    	* libmath/Makefile.in: Regenerate.
    	* libsupc++/Makefile.am (INCLUDES): Remove duplicate.
    	* libsupc++/Makefile.in: Regenerate.
    
    	* Makefile.am (AM_MAKEFLAGS): Add GLIBCPP_INCLUDES,
    	TOPLEVEL_INCLUDES, LIBMATH_INCLUDES, LIBIO_INCLUDES,
    	CSHADOW_INCLUDES up here.
    	* Makefile.in: Regenerate.
    	* src/Makefile.am (CSHADOW_INCLUDES): Add include/c_std, include/c
    	for shadow headers.
    	(myinstallheaders): Change glibcpp_includedir to GLIBCPP_INCLUDES.
    	Move GLIBCPP_INCLUDES, TOPLEVEL_INCLUDES, LIBMATH_INCLUDES,
    	LIBIO_INCLUDES, CSHADOW_INCLUDES up Makefile hierarchy.
    	* src/Makefile.in: Regenerate.
    
    	* include/c: New directory.
    	* include/c/bits/(std_cassert.h, std_cctype.h, std_cerrno.h,
    	std_cfloat.h, std_climits.h, std_clocale.h, std_cmath.h,
    	std_csetjmp.h, std_csignal.h, std_cstdarg.h, std_cstddef.h,
    	std_cstdio.h, std_cstdlib.h, std_cstring.h, std_ctime.h,
    	std_cwchar.h, std_cwctype.h): Add, moving from...
    	* include/bits: ...here.
    	* include/c_std: New directory.
    	* include/c_std/bits/*: Populate from...
    	* shadow: ...here. Remove directory.
    
    From-SVN: r36802

---
+++ libstdc++-v3/include/c_std/bits/std_csignal.h
@@ -0,0 +1,90 @@
+// -*- C++ -*- header wrapper.
+
+// Copyright (C) 1997-1999, 2000 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// As a special exception, you may use this file as part of a free software
+// library without restriction.  Specifically, if other files instantiate
+// templates or use macros or inline functions from this file, or you compile
+// this file and link it with other files to produce an executable, this
+// file does not by itself cause the resulting executable to be covered by
+// the GNU General Public License.  This exception does not however
+// invalidate any other reasons why the executable file might be covered by
+// the GNU General Public License.
+
+//
+// ISO C++ 14882: 20.4.6  C library
+//
+
+#ifndef _CPP_CSIGNAL
+#define _CPP_CSIGNAL 1
+
+# ifdef _IN_C_LEGACY_  /* sub-included by a C header */
+      // get out of the "legacy"
+    } // close extern "C"
+  }   // close namespace _C_legacy::
+#  undef _IN_C_LEGACY_
+#  define _SIGNAL_NEED_C_LEGACY_
+# endif
+
+namespace _C_legacy {
+  extern "C" {
+#   define _IN_C_LEGACY_
+    // XXX
+#   undef __need_sig_atomic_t
+#   undef __need_sigset_t
+
+#   pragma GCC system_header
+#   include_next <signal.h>
+  }
+} // namespace _C_legacy
+
+#  undef sig_atomic_t
+#  undef raise
+#  undef signal
+
+namespace std {
+  // Adopt C names into std::
+  using _C_legacy::sig_atomic_t;
+  using _C_legacy::raise;
+  using _C_legacy::signal;
+} // namespace std
+  
+# undef _IN_C_LEGACY_
+
+  // Expose global C names, including non-standard ones, but shadow
+  // some names and types with the std:: C++ version.
+  using std::sig_atomic_t;
+
+  using std::raise;
+  using std::signal;
+
+# ifdef _SIGNAL_NEED_C_LEGACY_
+  // dive back into the "swamp"
+  namespace _C_legacy {
+    extern "C" {
+#  define _IN_C_LEGACY_
+#  undef _SIGNAL_NEED_C_LEGACY_
+#  endif /* _SIGNAL_NEED_C_LEGACY_ */
+
+#endif  /*_CPP_CSIGNAL*/
+
+
+
+
+
