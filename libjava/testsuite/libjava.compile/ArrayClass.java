f2c436967fcbbb66092d08121bde1df28cb2a948
Anthony Green <green@redhat.com>
2000-10-09 02:20:28 +0000

    New test code
    From-SVN: r36796

---
+++ libjava/testsuite/libjava.compile/ArrayClass.java
@@ -0,0 +1,11 @@
+public class ArrayClass
+{
+  public static void main (String[] args)
+  {
+    System.out.println (ArrayClass.class);
+    System.out.println (ArrayClass[].class);
+    System.out.println (ArrayClass[][].class);
+    System.out.println (ArrayClass[][][].class);
+    System.out.println (ArrayClass[][][][].class);
+  }
+}
