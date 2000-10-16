a5ad11e16e82932d577a3c5fdf9fac6b6dcfa4d7
Jakub Jelinek <jakub@redhat.com>
2000-10-16 21:55:27 +0200

    * gcc.c-torture/execute/20001013-1.c: New test.
    From-SVN: r36891

---
+++ gcc/testsuite/gcc.c-torture/execute/20001013-1.c
@@ -0,0 +1,17 @@
+struct x {
+	int a, b;
+} z = { -4028, 4096 };
+
+int foo(struct x *p, int y)
+{
+  if ((y & 0xff) != y || -p->b >= p->a)
+    return 1;
+  return 0;
+}
+
+main()
+{
+  if (foo (&z, 10))
+    abort ();
+  exit (0);
+}
