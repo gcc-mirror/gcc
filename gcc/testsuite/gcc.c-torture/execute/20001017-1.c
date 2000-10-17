60bd162b50d7461c6c8f641e1ad9ce76ba805c21
sirl <sirl@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-10-17 22:04:51 +0000

    2000-10-17  Franz Sirl  <Franz.Sirl-kernel@lauterbach.com>
    	* gcc.c-torture/execute/20001017-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@36913 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20001017-1.c
@@ -0,0 +1,17 @@
+ 
+void bug (double *Cref, char transb, int m, int n, int k,
+	  double a, double *A, int fdA, double *B, int fdB,
+	  double b, double *C, int fdC)
+{
+  if (C != Cref) abort ();
+}
+ 
+int main (void)
+{
+  double A[1], B[1], C[1];
+   
+  bug (C, 'B', 1, 2, 3, 4.0, A, 5, B, 6, 7.0, C, 8);
+   
+  return 0;
+}
+                                                                                                                                       
