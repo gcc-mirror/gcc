15edc0cb96924c94cda82a9cfa425f92a7a5ed5e
Matthew Hiller <hiller@redhat.com>
2000-10-18 02:27:57 +0000

    * gcc.c-torture/execute/20001017-2.c: New test.
    From-SVN: r36924

---
+++ gcc/testsuite/gcc.c-torture/execute/20001017-2.c
@@ -0,0 +1,17 @@
+void
+fn_4parms (unsigned char a, long *b, long *c, unsigned int *d)
+{
+  if (*b != 1 || *c != 2 || *d != 3)
+    abort ();
+}
+
+int
+main ()
+{
+  unsigned char a = 0;
+  unsigned long b = 1, c = 2;
+  unsigned int d = 3;
+
+  fn_4parms (a, &b, &c, &d);
+  return 0;
+}
