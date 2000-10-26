f023efe4d380f64ec1d816b83b426e789684ecac
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-10-26 14:56:05 +0000

    2000-10-26  Benjamin Kosnik  <bkoz@redhat.com>
    	* acinclude.m4 (GLIBCPP_ENABLE_LONG_DOUBLE): Remove.
    	* aclocal.m4: Regenerate.
    	* configure.in: Remove GLIBCPP_ENABLE_LONG_DOUBLE.
    	* configure: Regenerate.
    	* docs/configopts.html: Remove.
    	* src/complexl.cc: Revert.
    	* config.h.in: Remove.
    	* include/c_std/bits/std_cmath.h: Remove guards based on
    	_GLIBCPP_USE_LONG_DOUBLE.
    	* include/c/bits/std_cmath.h: Same. Format. Match c_std behavior
    	with respect to long double signatures.
    
    	* config/os/aix/bits/ctype_noninline.h (ctype): Remove throw
    	specification.
    	* config/os/newlib/bits/ctype_noninline.h (ctype): And here.
    	* config/os/solaris/solaris2.6/bits/ctype_noninline.h (ctype): Same.
    	* config/os/solaris/solaris2.5/bits/ctype_noninline.h (ctype): Same.
    	* config/os/generic/bits/ctype_noninline.h (ctype): Same.
    	* config/os/bsd/bits/ctype_noninline.h (ctype): Same.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@37073 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/config/os/solaris/solaris2.6/bits/ctype_noninline.h
+++ libstdc++-v3/config/os/solaris/solaris2.6/bits/ctype_noninline.h
@@ -34,7 +34,7 @@
 // Information as gleaned from /usr/include/ctype.h
   
   ctype<char>::ctype(const mask* __table = 0, bool __del = false, 
-	size_t __refs = 0) throw()
+	size_t __refs = 0) 
     : _Ctype_nois<char>(__refs), _M_del(__table != 0 && __del), 
       _M_toupper(__trans_upper), _M_tolower(__trans_lower),
       _M_ctable(__ctype_mask), _M_table(__table == 0 ? _M_ctable: __table) 
