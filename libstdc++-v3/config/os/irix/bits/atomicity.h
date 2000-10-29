37ab89b9cf948efe11e7f3a44e02ddc62842de50
Mark Mitchell <mmitchel@gcc.gnu.org>
2000-10-29 22:31:16 +0000

    Fix copyright date
    From-SVN: r37133

--- libstdc++-v3/config/os/irix/bits/atomicity.h
+++ libstdc++-v3/config/os/irix/bits/atomicity.h
@@ -1,5 +1,5 @@
 /* Low-level functions for atomic operations.  IRIX version.
-   Copyright (C) 1997 Free Software Foundation, Inc.
+   Copyright (C) 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
