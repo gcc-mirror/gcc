6f268adf3b95994c68ba45ad1ceaa18b776f6b29
Michael Hayes <mhayes@redhat.com>
2000-10-29 09:14:22 +0000

    * integrate.c (copy_insn_list): Copy the unchanging flag for calls.
    From-SVN: r37121

--- gcc/integrate.c
+++ gcc/integrate.c
@@ -1468,6 +1468,7 @@ copy_insn_list (insns, map, static_chain_value)
 	  copy = emit_call_insn (pattern);
 
 	  SIBLING_CALL_P (copy) = SIBLING_CALL_P (insn);
+	  CONST_CALL_P (copy) = CONST_CALL_P (insn);
 
 	  /* Because the USAGE information potentially contains objects other
 	     than hard registers, we need to copy it.  */
