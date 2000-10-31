0bcef9f8db69d7a20614f48b5984367ee2dfbd47
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-10-31 20:38:04 +0000

    * c-typeck.c (build_unary_op): If pedantic, pedwarn for increment 	and decrement of complex types.
    testsuite:
    	* gcc.dg/c99-complex-2.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@37164 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/c99-complex-2.c
@@ -0,0 +1,22 @@
+/* Test for _Complex: in C99 only.  Test for increment and decrement.  */
+/* Origin: Joseph Myers <jsm28@cam.ac.uk> */
+/* { dg-do compile } */
+/* { dg-options "-std=iso9899:1999 -pedantic-errors" } */
+
+/* Use of ++ and -- on complex types (both prefix and postfix) is a
+   C99 constraint violation (6.5.2.4p1, 6.5.3.1p1).
+*/
+
+_Complex double
+foo (_Complex double z)
+{
+  z++; /* { dg-bogus "warning" "warning in place of error" } */
+  /* { dg-error "complex" "postinc" { target *-*-* } 13 } */
+  ++z; /* { dg-bogus "warning" "warning in place of error" } */
+  /* { dg-error "complex" "preinc" { target *-*-* } 15 } */
+  z--; /* { dg-bogus "warning" "warning in place of error" } */
+  /* { dg-error "complex" "postdec" { target *-*-* } 17 } */
+  --z; /* { dg-bogus "warning" "warning in place of error" } */
+  /* { dg-error "complex" "predec" { target *-*-* } 19 } */
+  return z;
+}
