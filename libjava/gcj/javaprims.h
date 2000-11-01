4547105f643233da44000b0c7f0a2f5b83548f28
Warren Levy <warrenl@cygnus.com>
2000-11-01 00:18:03 +0000

    jni.cc: Added include of java/lang/ThreadGroup.h.
    	* jni.cc: Added include of java/lang/ThreadGroup.h.
    	* gcj/javaprims.h: Removed Replaceable and Resolvable from namespace
    	per change of 2000-10-05.
    
    From-SVN: r37173

--- libjava/gcj/javaprims.h
+++ libjava/gcj/javaprims.h
@@ -363,8 +363,6 @@ extern "Java"
       class PushbackReader;
       class RandomAccessFile;
       class Reader;
-      class Replaceable;
-      class Resolvable;
       class SequenceInputStream;
       class Serializable;
       class SerializablePermission;
