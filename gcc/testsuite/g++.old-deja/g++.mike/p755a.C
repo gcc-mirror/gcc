0b0b1f137058986849740a580b41f5cd8f3964b9
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-11-04 20:00:52 +0000

    * g++.old-deja/g++.mike/p755.C: Tweak handling of exit. 	* g++.old-deja/g++.mike/p755a.C: Likewise. 	* g++.old-deja/g++.mike/p9706.C: Don't assume that the standard 	library makes no calls to `operator new' during initialization.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@37257 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.mike/p755a.C
+++ gcc/testsuite/g++.old-deja/g++.mike/p755a.C
@@ -1,10 +1,10 @@
 // It checks to see if you can define your own global delete operator.
 // prms-id: 755
 
-extern "C" void exit(int);
+extern "C" void _exit(int);
 
 void operator delete(void *p) throw() {
-  exit(0);
+  _exit(0);
 }
 
 int main () {
