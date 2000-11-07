665d66d647a24dd78c881d9473b06d861468f06f
Jeff Law <law@gcc.gnu.org>
2000-11-07 01:45:08 -0700

    Daily bump.
    From-SVN: r37288

--- libf2c/libU77/Version.c
+++ libf2c/libU77/Version.c
@@ -1,6 +1,6 @@
 static char junk[] = "\n@(#) LIBU77 VERSION 19980709\n";
 
-char __G77_LIBU77_VERSION__[] = "0.5.26 20001106 (prerelease)";
+char __G77_LIBU77_VERSION__[] = "0.5.26 20001107 (prerelease)";
 
 #include <stdio.h>
 
