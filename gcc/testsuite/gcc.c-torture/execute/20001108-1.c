93ae2b3e0669b306a2bf33778637275bd7f4971d
Nick Clifton <nickc@redhat.com>
2000-11-10 19:27:35 +0000

    Add test of unsigned long long multuiple and accumulate.
    From-SVN: r37375

--- gcc/testsuite/gcc.c-torture/execute/20001108-1.c
+++ gcc/testsuite/gcc.c-torture/execute/20001108-1.c
@@ -1,14 +1,24 @@
 long long
-poly (long long sum, long x)
+signed_poly (long long sum, long x)
 {
   sum += (long long) (long) sum * (long long) x;
+  return sum;
+}
+
+unsigned long long
+unsigned_poly (unsigned long long sum, unsigned long x)
+{
+  sum += (unsigned long long) (unsigned long) sum * (unsigned long long) x;
   return sum;
 }
 
 int
 main (void)
 {
-  if (poly (2LL, 3) != 8LL)
+  if (signed_poly (2LL, -3) != -4LL)
+    abort ();
+  
+  if (unsigned_poly (2ULL, 3) != 8ULL)
     abort ();
 
   exit (0);
