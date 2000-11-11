5696467825b85328f6b808fdc20c50c1ce97a60d
Mark Mitchell <mark@codesourcery.com>
2000-11-11 23:59:50 +0000

    err-msg8.C: Avoid capricious line-number issues with error messages.
    	* g++.brendan/err-msg8.C: Avoid capricious line-number issues with
    	error messages.
    
    From-SVN: r37395

--- gcc/testsuite/g++.old-deja/g++.brendan/err-msg8.C
+++ gcc/testsuite/g++.old-deja/g++.brendan/err-msg8.C
@@ -1,4 +1,3 @@
 // Build don't link: 
 // GROUPS passed error-messages
-operator int ;
-// ERROR -  declaration of `operator int' as non-function.*
+operator int ; int j; // ERROR -  declaration of `operator int' as non-function.*
