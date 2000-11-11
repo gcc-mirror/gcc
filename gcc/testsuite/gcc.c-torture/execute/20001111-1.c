3e5c33c0b76fa0a5c5161aefd243ad00f9a7fcd1
Bernd Schmidt <bernds@redhat.co.uk>
2000-11-11 14:09:42 +0000

    New execution test.
    From-SVN: r37387

---
+++ gcc/testsuite/gcc.c-torture/execute/20001111-1.c
@@ -0,0 +1,41 @@
+
+static int next_buffer = 0;
+void bar (void);
+
+static int t = 1, u = 0;
+
+long
+foo (unsigned int offset)
+{
+  unsigned i, buffer;
+  int x;
+  char *data;
+
+  i = u;
+  if (i)
+    return i * 0xce2f;
+
+  buffer = next_buffer;
+  data = buffer * 0xce2f;
+  for (i = 0; i < 2; i++)
+    bar ();
+  buffer = next_buffer;
+  return buffer * 0xce2f + offset;
+
+}
+
+void
+bar (void)
+{
+}
+
+int
+main ()
+{
+  if (foo (3) != 3)
+    abort ();
+  next_buffer = 1;
+  if (foo (2) != 0xce2f + 2)
+    abort ();
+  exit (0);
+}
