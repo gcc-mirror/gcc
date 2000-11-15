c7e700dffc2c5edd254411c2ece935cb9cad1cf9
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-11-15 11:59:07 +0000

    cp/ 	* typeck.c (lookup_anon_field): Cope with qv qualifiers. testsuite/ 	* g++.old-deja/g++.other/anon5.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@37478 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.other/anon5.C
@@ -0,0 +1,17 @@
+// Build don't link:
+// Copyright (C) 2000 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 14 Nov 2000 <nathan@codesourcery.com>
+
+// Bug 649. A cv qualified anonymous union would cause confusion.
+
+struct X
+{
+  int fn () const
+  {
+    return member;
+  }
+  const union
+  {
+    int member;
+  };
+};
