d21ec08dec45555d58f207a3776121c8e0870282
zack <zack@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-11-17 16:45:34 +0000

    New test - regression of ICE
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@37521 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20001116-1.c
@@ -0,0 +1,11 @@
+/* This looks like a warning test, but it's actually a regression test for a
+   nasty ICE due to messed up parser context.  Problem originally found
+   during bootstrap; this is synthetic.  -zw  */
+/* { dg-do compile } 
+   { dg-options -W } */
+
+void foo (int x)
+{
+  if (x)	/* { dg-warning "empty body in an if-statement" } */
+    ;
+}
