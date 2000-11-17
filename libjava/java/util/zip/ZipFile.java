4f21aedbf4c7661626f49e7ca74ecc2a80892262
Mark Wielaard <mark@klomp.org>
2000-11-17 21:42:28 +0000

    * java/util/zip/*.java: Javadoc and copyright updates.
    From-SVN: r37526

--- libjava/java/util/zip/ZipFile.java
+++ libjava/java/util/zip/ZipFile.java
@@ -1,14 +1,31 @@
-// ZipFile.java - Read contents of a ZIP file.
+/* ZipFile.java - Read contents of a ZIP file
+   Copyright (C) 1999, 2000 Free Software Foundation, Inc.
 
-/* Copyright (C) 1999, 2000  Free Software Foundation
+This file is part of GNU Classpath.
 
-   This file is part of libgcj.
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
 
-This software is copyrighted work licensed under the terms of the
-Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
-details.  */
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+As a special exception, if you link this library with other files to
+produce an executable, this library does not by itself cause the
+resulting executable to be covered by the GNU General Public License.
+This exception does not however invalidate any other reasons why the
+executable file might be covered by the GNU General Public License. */
 
 package java.util.zip;
+
 import java.io.*;
 
 /* Written using on-line Java Platform 1.2 API Specification
@@ -166,6 +183,12 @@ public class ZipFile implements ZipConstants
     return name;
   }
 
+  /**
+   * Returns the number of entries in this ZipFile.
+   * @exception IllegalStateException if the ZipFile has been closed.
+   *
+   * @since 1.2
+   */
   public int size ()
   {
     if (entries == null)
