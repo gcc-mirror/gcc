7667a9788c346cccfcd8f043b4651c5a1eccac16
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-11-21 21:58:28 +0000

    2000-11-21  Branko Cibej  <brankoe@xbc.nu>
            * config/os/solaris/solaris2.6/bits/os_defines.h: Define
            _XOPEN_SOURCE, _LARGEFILE64_SOURCE, __EXTENSIONS__, __off_t,
            __off64_t and __ssize_t.
    	* config/os/solaris/solaris2.5/bits/os_defines.h: For now, use the
    	same configuration as above.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@37628 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/config/os/solaris/solaris2.6/bits/os_defines.h
+++ libstdc++-v3/config/os/solaris/solaris2.6/bits/os_defines.h
@@ -31,10 +31,21 @@
 #ifndef _GLIBCPP_OS_DEFINES
 #  define _GLIBCPP_OS_DEFINES
 
+// Need these to get sane definitions, esp. of 64-bit types and typedefs.
+#define _XOPEN_SOURCE        500
+#define _LARGEFILE64_SOURCE  1
+#define __EXTENSIONS__
 
 /* System-specific #define, typedefs, corrections, etc, go here.  This
    file will come before all others. */
+// These are typedefs which libio assumes are already in place (because
+// they really are, under Linux).
+#define __off_t     off_t
+#define __off64_t   off64_t
+#define __ssize_t   ssize_t
 
+// Without this all the libio vtbls are offset wrongly.
+#define _G_USING_THUNKS 0
 
 #endif
 
