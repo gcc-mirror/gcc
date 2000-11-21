fa1996f36e066619a4767dbaefca7c8a0ebc7ab3
Ovidiu Predescu <ovidiu@gcc.gnu.org>
2000-11-21 08:32:12 +0000

    Added.
    From-SVN: r37609

---
+++ gcc/testsuite/objc/execute/fdecl.m
@@ -0,0 +1,13 @@
+/* Bug report submitted by igorkh@hotbot.com on submit@bugs.debian.org
+   Thu, 13 Apr 2000 09:42:08 -0400 */
+
+@class AClass;
+
+@interface test
+{
+        AClass foo;
+}
+@end
+
+@implementation test
+@end
