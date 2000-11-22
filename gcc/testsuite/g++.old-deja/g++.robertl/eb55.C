d6f1cd05303be2872c43ddb8626443ec9cfaff50
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-11-22 23:10:04 +0000

    * g++.old-deja/g++.mike/p6610a.C: This test no longer fails. 	* g++.old-deja/g++.robertl/eb55.C: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@37674 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.robertl/eb55.C
+++ gcc/testsuite/g++.old-deja/g++.robertl/eb55.C
@@ -1,7 +1,7 @@
 // Build don't link: 
 // Since the constructor is in streambuf.h, additional diagnostics are
 // produced, which are not really supported in the old-deja framework
-// excess errors test - XFAIL *-*-*
+
 #include <strstream.h>
 
 void
