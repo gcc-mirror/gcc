cac78ea2814a35a8e797ba0ed6ab1f24531df18a
Mark Mitchell <mark@codesourcery.com>
2000-11-22 22:43:49 +0000

    pt.c (do_decl_instantiate): Explicitly clone constructors and destructors that haven't already been cloned.
    	* pt.c (do_decl_instantiate): Explicitly clone constructors and
    	destructors that haven't already been cloned.
    
    From-SVN: r37673

---
+++ gcc/testsuite/g++.old-deja/g++.other/ctor1.C
@@ -0,0 +1,14 @@
+// Additional sources: ctor1-aux.cc
+// Origin: Mark Mitchell <mark@codesourcery.com>
+
+template <class T>
+struct S {
+  template <class U>
+  S (U);
+};
+
+template <class T>
+template <class U>
+S<T>::S (U) {}
+
+template S<int>::S (double);
