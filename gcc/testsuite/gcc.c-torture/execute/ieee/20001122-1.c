e5a7acddf687a7c538d53108942ff3d0161ef6aa
Jakub Jelinek <jakub@redhat.com>
2000-11-22 17:37:24 +0100

    * gcc.c-torture/execute/ieee/20001122-1.c: New test.
    From-SVN: r37657

---
+++ gcc/testsuite/gcc.c-torture/execute/ieee/20001122-1.c
@@ -0,0 +1,22 @@
+volatile double a, *p;
+
+int main ()
+{
+  double c, d;
+  volatile double b;
+
+  d = 1.0;
+  p = &b;
+  do
+  {
+    c = d;
+    d = c * 0.5;
+    b = 1 + d;
+  } while (b != 1.0);
+
+  a = 1.0 + c;
+  if (a == 1.0)
+    abort();
+
+  exit (0);
+}
