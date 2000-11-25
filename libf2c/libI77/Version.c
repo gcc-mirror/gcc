f7da5f3d4672ee802897caf1de7c9099948e7e9d
Jeff Law <law@gcc.gnu.org>
2000-11-25 01:45:08 -0700

    Daily bump.
    From-SVN: r37735

--- libf2c/libI77/Version.c
+++ libf2c/libI77/Version.c
@@ -3,7 +3,7 @@ static char junk[] = "\n@(#) LIBI77 VERSION pjw,dmg-mods 19991115\n";
 /*
 */
 
-char __G77_LIBI77_VERSION__[] = "0.5.26 20001124 (prerelease)";
+char __G77_LIBI77_VERSION__[] = "0.5.26 20001125 (prerelease)";
 
 /*
 2.01	$ format added
