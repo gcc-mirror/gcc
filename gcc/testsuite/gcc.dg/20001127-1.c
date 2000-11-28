9c63a1e0a2b1adbb829dcbd8a485cb405f32dc04
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-11-28 09:04:27 +0000

    * config/i386/i386.md (truncxfsf2_2): Fix predicate.
    	* gcc.dg/20001127-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@37812 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20001127-1.c
@@ -0,0 +1,16 @@
+/* { dg-do compile { target i?86-*-* } } */
+/* { dg-options "-O2" } */
+
+extern inline float bar (float x)
+{
+  register long double value;
+  asm volatile ("frndint" : "=t" (value) : "0" (x));
+  return value;
+}
+
+float a;
+
+float foo (float b)
+{
+  return a + bar (b);
+}
