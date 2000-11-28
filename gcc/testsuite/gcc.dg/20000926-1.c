70cf82bcf5c19dbfc10aab5914426dcf5764d799
Geoffrey Keating <geoffk@redhat.com>
2000-11-28 22:25:51 +0000

    940510-1.c: Update to test c89 functionality.
    	* gcc.dg/noncompile/940510-1.c: Update to test c89 functionality.
    	Move from here ...
    	* gcc.dg/940510-1.c: ... to here.
    
    	* gcc.dg/20000926-1.c: GNU C now allows initializations of
    	zero-size arrays in toplevel structures.
    
    From-SVN: r37830

--- gcc/testsuite/gcc.dg/20000926-1.c
+++ gcc/testsuite/gcc.dg/20000926-1.c
@@ -22,6 +22,6 @@ struct PLAYBOOK playbook  =
 {
   "BookName",
   {
-    { 1, "PName0" }, /* { dg-warning "excess elements in array initializer|(near initialization for `playbook.Play')" } */
+    { 1, "PName0" },
   }
 };
