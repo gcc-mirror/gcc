2c92b94d0f1c7c54182770327784e8cb84e65b95
Nathan Sidwell <nathan@codesourcery.com>
2000-11-30 09:36:29 +0000

    call.c (build_over_call): Use VOID_TYPE_P.
    cp:
    	* call.c (build_over_call): Use VOID_TYPE_P. Don't die on
    	incomplete return type.
    testsuite:
    	* g++.old-deja/g++.other/crash38.C: New test.
    
    From-SVN: r37872

---
+++ gcc/testsuite/g++.old-deja/g++.other/crash38.C
@@ -0,0 +1,19 @@
+// Build don't link:
+
+// Copyright (C) 2000 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 28 Nov 2000 <nathan@codesourcery.com>
+
+// Bug 611. We ICEd when calling a member function returning an incomplete
+// type by value.
+
+struct X;   // ERROR - forward ref
+
+struct Y
+{
+  X foo ();
+};
+
+void baz (Y *p)
+{
+  p->foo ();    // ERROR - incomplete
+}
