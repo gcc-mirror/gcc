7ae52912d0979e919945a582207dab5e264d2af9
neil <neil@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-12-02 15:28:08 +0000

    * g++.old-deja/g++.other/instan2.C         * g++.old-deja/g++.other/instan3.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@37952 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.other/instan2.C
@@ -0,0 +1,15 @@
+// Build don't link:
+// Origin: Neil Booth, from bug report #44
+
+#include <iterator>
+
+template<class T>
+struct X
+{
+};
+
+template<class T>
+X<T> operator+(const X<T>&, const X<T>&);
+
+template<>
+X<int> operator+<int>(const X<int>&, const X<int>&);
