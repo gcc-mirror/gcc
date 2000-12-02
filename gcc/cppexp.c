87da14dfd8c03ead40e38c2738b25344287b7f47
neil <neil@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-12-02 10:13:49 +0000

    * cppexp.c (parse_number): Update diagnostic test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@37947 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cppexp.c
+++ gcc/cppexp.c
@@ -205,8 +205,9 @@ parse_number (pfile, tok)
 
       if (CPP_WTRADITIONAL (pfile) && sufftab[i].u)
 	cpp_warning (pfile, "traditional C rejects the `U' suffix");
-      if (sufftab[i].l == 2 && !CPP_OPTION (pfile, extended_numbers))
-	SYNTAX_ERROR ("too many 'l' suffixes in integer constant");
+      if (sufftab[i].l == 2 && CPP_OPTION (pfile, pedantic)
+	  && ! CPP_OPTION (pfile, c99))
+	cpp_pedwarn (pfile, "too many 'l' suffixes in integer constant");
     }
   
   if (base <= largest_digit)
