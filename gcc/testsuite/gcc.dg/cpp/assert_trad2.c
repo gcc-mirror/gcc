723e8d057a6ad527021a951d9a049c692f130c9f
neil <neil@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-12-04 22:06:41 +0000

    * gcc.dg/cpp/assert_trad1.c, assert_trad2.c, assert_trad3.c:         New tests.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38012 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/cpp/assert_trad2.c
@@ -0,0 +1,24 @@
+/* Malformed assertion tests.  */
+/* { dg-do preprocess } */
+/* { dg-options "-traditional" } */
+
+#assert		/* { dg-error "without predicate" "assert w/o predicate" } */
+#assert %	/* { dg-error "an identifier" "assert punctuation" } */
+#assert 12	/* { dg-error "an identifier" "assert number" } */
+#assert abc	/* { dg-error "missing" "assert w/o answer" } */
+
+#if #		/* { dg-error "without predicate" "test w/o predicate" } */
+#endif
+
+#if #%		/* { dg-error "an identifier" "test punctuation" } */
+#endif		/* { dg-error "parse error" "" { target *-*-* } 13 } */
+
+#if #12		/* { dg-error "an identifier" "test number" } */
+#endif		/* { dg-error "parse error" "" { target *-*-* } 16 } */
+
+#if #abc
+#error		/* { dg-bogus "error" "test w/o answer" } */
+#endif
+
+#if #abc[def]	/* { dg-error "parse error" "bad syntax" } */
+#endif
