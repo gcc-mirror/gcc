e3813b92e269d36550dff21f0e87719c9df1f33d
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-12-05 23:25:08 +0000

    2000-12-05  Benjamin Kosnik  <bkoz@fillmore.redhat.com>
    	* acinclude.m4: Change up to reflect new directory organization.
    	Add in bits for NetBSD.
    	* aclocal.m4: Regenerate.
    	* configure: Regenerate.
    	* config/os/bsd: New directory.
    	* config/os/netbsd: Move to...
    	* config/os/bsd/netbsd: ...here.
    	* config/os/freebsd: Move to...
    	* config/os/bsd/freebsd: ...here.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38055 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/config/os/bsd/freebsd/bits/ctype_base.h
@@ -0,0 +1,77 @@
+// Locale support -*- C++ -*-
+
+// Copyright (C) 2000 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// As a special exception, you may use this file as part of a free software
+// library without restriction.  Specifically, if other files instantiate
+// templates or use macros or inline functions from this file, or you compile
+// this file and link it with other files to produce an executable, this
+// file does not by itself cause the resulting executable to be covered by
+// the GNU General Public License.  This exception does not however
+// invalidate any other reasons why the executable file might be covered by
+// the GNU General Public License.
+
+//
+// ISO C++ 14882: 22.1  Locales
+//
+  
+// Information as gleaned from /usr/include/ctype.h on FreeBSD 3.4,
+// 4.0 and all versions of the CVS managed file at:
+// :pserver:anoncvs@anoncvs.freebsd.org:/home/ncvs/src/include/ctype.h
+  
+  struct ctype_base
+  {
+    typedef unsigned long 	mask;
+    // Non-standard typedefs.
+    typedef const int* 		__to_type;
+
+    enum
+    {
+#ifdef _CTYPE_S
+      // FreeBSD 4.0 uses this style of define.
+      space = _CTYPE_S,
+      print = _CTYPE_R,
+      cntrl = _CTYPE_C,
+      upper = _CTYPE_U,
+      lower = _CTYPE_L,
+      alpha = _CTYPE_A,
+      digit = _CTYPE_D,
+      punct = _CTYPE_P,
+      xdigit = _CTYPE_X,
+      alnum = _CTYPE_A | _CTYPE_D,
+      graph = _CTYPE_G
+#else
+      // Older versions, including Free BSD 3.4, use this style of define.
+      space = _S,
+      print = _R,
+      cntrl = _C,
+      upper = _U,
+      lower = _L,
+      alpha = _A,
+      digit = _D,
+      punct = _P,
+      xdigit = _X,
+      alnum = _A | _D,
+      graph = _G
+#endif
+    };
+  };
+
+
+
