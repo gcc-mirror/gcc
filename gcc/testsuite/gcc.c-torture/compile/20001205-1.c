7d3615831e1495c3181b108a3dd510382a997abc
Richard Henderson <rth@redhat.com>
2000-12-05 00:13:27 -0800

    New test.
    From-SVN: r38029

---
+++ gcc/testsuite/gcc.c-torture/compile/20001205-1.c
@@ -0,0 +1,21 @@
+static inline unsigned long rdfpcr(void)
+{
+        unsigned long tmp, ret;
+        __asm__ ("" : "=r"(tmp), "=r"(ret));
+        return ret;
+}
+
+static inline unsigned long
+swcr_update_status(unsigned long swcr, unsigned long fpcr)
+{
+	swcr &= ~0x7e0000ul;
+        swcr |= (fpcr >> 3) & 0x7e0000ul;
+        return swcr;
+}
+
+unsigned long osf_getsysinfo(unsigned long flags)
+{
+        unsigned long w;
+	w = swcr_update_status(flags, rdfpcr());
+	return w;
+}
