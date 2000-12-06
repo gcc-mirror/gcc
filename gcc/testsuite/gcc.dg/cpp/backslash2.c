a72f7c07c0a796184be49b1e52e1de5f266f2713
neil <neil@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-12-06 20:14:32 +0000

    * gcc.dg/cpp/backslash2.c
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38073 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/cpp/backslash2.c
@@ -0,0 +1,14 @@
+/* Copyright (C) 2000 Free Software Foundation, Inc.  */
+
+/* { dg-do preprocess } */
+
+/* Test warnings for backslash-space-newline.
+   Source: Neil Booth. 6 Dec 2000.  */
+
+foo \  
+bar
+/* { dg-warning "separated by space" "" { target *-*-* } 8 } */
+
+/* foo \   
+   bar */
+/* { dg-bogus "separated by space" "" { target *-*-* } 12 } */
