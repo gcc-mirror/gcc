742d48c23dfdbc4fc090b4e7fbbd17e87852fbb2
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-12-07 02:42:02 +0000

    * config.gcc (a29k*-*-rtems*, arm*-*-rtems*, c4x-*-rtems*,         h8300-*-rtems*, hppa1.1-*-rtems, *mips*-*-rtems*): New targets.         * config.gcc (*-rtems*): Add support for gthr-rtems.h.         * gthr-rtems.h, config/a29k/rtems.h, config/arm/rtems-elf.h,         config/h8300/rtems.h, config/mips/rtems.h: New files.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38094 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/h8300/rtems.h
@@ -0,0 +1,37 @@
+/* Definitions for rtems targeting a H8
+   Copyright (C) 1996, 1997, 2000 Free Software Foundation, Inc.
+   Contributed by Joel Sherrill (joel@OARcorp.com).
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+#include "h8300/h8300.h"
+
+/* Specify predefined symbols in preprocessor.  */
+
+#undef CPP_PREDEFINES
+#define CPP_PREDEFINES "-Dh8300 \
+  -D__LONG_MAX__=2147483647L -D__LONG_LONG_MAX__=2147483647L \
+  -Drtems -D__rtems__ -Asystem(rtems) -Acpu(h8300) -Amachine(h8300)"
+
+/* Generate calls to memcpy, memcmp and memset.  */
+#ifndef TARGET_MEM_FUNCTIONS
+#define TARGET_MEM_FUNCTIONS
+#endif
+
+/* Get machine-independent configuration parameters for RTEMS.  */
+#include <rtems.h>
