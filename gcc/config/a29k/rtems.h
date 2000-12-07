bc98ef7f6c0cb3ee14b28c3ca7655261138fe0a1
Joel Sherrill <joel@OARcorp.com>
2000-12-07 02:42:02 +0000

    config.gcc (a29k*-*-rtems*, [...]): New targets.
            * config.gcc (a29k*-*-rtems*, arm*-*-rtems*, c4x-*-rtems*,
            h8300-*-rtems*, hppa1.1-*-rtems, *mips*-*-rtems*): New targets.
            * config.gcc (*-rtems*): Add support for gthr-rtems.h.
            * gthr-rtems.h, config/a29k/rtems.h, config/arm/rtems-elf.h,
            config/h8300/rtems.h, config/mips/rtems.h: New files.
    
    From-SVN: r38094

---
+++ gcc/config/a29k/rtems.h
@@ -0,0 +1,36 @@
+/* Definitions for rtems targeting a AMD A29K using COFF.
+   Copyright (C) 1997, 1998, 2000 Free Software Foundation, Inc.
+   Contributed by Joel Sherrill (joel@OARcorp.com).
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+#include "a29k/a29k.h"
+
+/* Specify predefined symbols in preprocessor.  */
+
+#undef CPP_PREDEFINES
+#define CPP_PREDEFINES "-D_AM29K -D_AM29000 -D_EPI -Drtems -D__rtems__ \
+  -Asystem(rtems) -Acpu(a29k) -Amachine(a29k)"
+
+/* Generate calls to memcpy, memcmp and memset.  */
+#ifndef TARGET_MEM_FUNCTIONS
+#define TARGET_MEM_FUNCTIONS
+#endif
+
+/* Get machine-independent configuration parameters for RTEMS.  */
+#include <rtems.h>
