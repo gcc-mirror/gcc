bd8757b313518d151d28fff18f4d7958b5cb7ba3
Alexandre Petit-Bianco <apbianco@cygnus.com>
2000-12-09 03:08:23 +0000

    fastjar: Imported.
    2000-12-08  Alexandre Petit-Bianco  <apbianco@cygnus.com>
    
            * fastjar: Imported.
    
    From-SVN: r38145

---
+++ fastjar/pushback.h
@@ -0,0 +1,32 @@
+/*
+  pushback.h - header for pushback.c
+  Copyright (C) 1999  Bryan Burns
+  
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+struct pb_file {
+  unsigned int buff_amt;
+  ub1 pb_buff[RDSZ];
+  int fd;
+  ub1 *next;
+};
+
+typedef struct pb_file pb_file;
+
+
+void pb_init(pb_file *, int);
+int pb_push(pb_file *, void *, int);
+int pb_read(pb_file *, void *, int);
