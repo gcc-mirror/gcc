04fd40b9d15b6374d3508259c86ed88d773bd988
Neil Booth <neil@daikokuya.demon.co.uk>
2000-12-13 19:47:04 +0000

    Tests by Zack Weinberg <zackw@stanford.edu>.
    	* gcc.dg/cpp/defined.c: Update.
    	* gcc.dg/cpp/defined_trad.c: New tests.
    
    From-SVN: r38229

--- gcc/testsuite/gcc.dg/cpp/defined_trad.c
+++ gcc/testsuite/gcc.dg/cpp/defined_trad.c
@@ -3,9 +3,10 @@
 /* { dg-do preprocess } */
 /* { dg-options "-traditional" } */
 
-/* Tests tradcpp0 with defined.  */
+/* Tests tradcpp0 with defined.  The defined operator in traditional C
+   works just the same as the defined operator in Standard C.  */
 
-/* Source: Glibc, 11 Dec 2000.  */
+/* Source: Zack Weinberg, glibc, Neil Booth 11 Dec 2000.  */
 
 #if defined REGPARMS
 #error REGPARMS should not be defined
@@ -14,4 +15,65 @@
 #define REGPARMS 1
 #if !defined REGPARMS
 #error REGPARMS should be defined
+#endif
+
+#define defined			/* { dg-error "defined" } */
+
+/* No diagnostics, though you could argue there should be.  */
+#if defined defined
+#error defined is defined!
+#endif
+
+#define is_Z_defined defined Z
+
+#if defined Z
+#error Z is not defined
+#endif
+
+/* The behaviour of "defined" when it comes from a macro expansion is
+   now documented.  */
+#if is_Z_defined
+#error Macro expanding into defined operator test 1
+#endif
+
+#define Z
+
+#if !defined Z
+#error Z is defined
+#endif
+
+#if !is_Z_defined
+#error Macro expanding into defined operator test 2
+#endif
+
+#undef is_Z_defined
+#undef Z
+
+/* Do all the tests over again with the () form of defined.  */
+
+/* No diagnostics, though you could argue there should be.  */
+#if defined(defined)
+#error defined is defined!
+#endif
+
+#define is_Z_defined defined ( Z )
+
+#if defined(Z)
+#error Z is not defined
+#endif
+
+/* The behaviour of "defined" when it comes from a macro expansion is
+   now documented.  */
+#if is_Z_defined
+#error Macro expanding into defined operator test 1
+#endif
+
+#define Z
+
+#if !defined(Z)
+#error Z is defined
+#endif
+
+#if !is_Z_defined
+#error Macro expanding into defined operator test 2
 #endif
