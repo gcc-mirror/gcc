47bf5e3a3327688b007a6f038bc35cbb4e27f1c8
Mike Stump <mrs@gcc.gnu.org>
2000-12-13 22:25:21 +0000

    Fix typo
    From-SVN: r38235

--- gcc/fixinc/fixincl.c
+++ gcc/fixinc/fixincl.c
@@ -1,4 +1,3 @@
-:1066
 /* Install modified versions of certain ANSI-incompatible system header
    files which are fixed to work correctly with ANSI C and placed in a
    directory that GNU C will search.
