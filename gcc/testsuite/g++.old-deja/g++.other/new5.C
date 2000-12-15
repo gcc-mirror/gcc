8d42565b847d588e6c04e9c3cd5a3157859e2c6a
Jason Merrill <jason@redhat.com>
2000-12-15 11:23:26 -0500

    * init.c (build_new_1): Don't strip quals from type.
    From-SVN: r38291

---
+++ gcc/testsuite/g++.old-deja/g++.other/new5.C
@@ -0,0 +1,12 @@
+// Test that const-correctness is observed when using new.
+
+struct A {
+  A() { }
+  int f () { return 1; }
+  int f () const { return 0; }
+};
+
+int main ()
+{
+  return (new const A)->f ();
+}
