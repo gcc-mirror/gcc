a892d100850e69a5646a81a58e5eae95886cea3b
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-12-15 18:45:09 +0000

    * jargrep.c: Include getopt.h if it exists. 	(optind): Declare. 	* configure, config.h: Rebuilt. 	* configure.in: Check for getopt.h.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38294 138bc75d-0d04-0410-961f-82ee72b054a4

--- fastjar/jargrep.c
+++ fastjar/jargrep.c
@@ -1,6 +1,6 @@
 /*
   jargrep.c - main functions for jargrep utility
-  Copyright (C) 1999 Bryan Burns
+  Copyright (C) 1999, 2000 Bryan Burns
   Copyright (C) 2000 Cory Hollingsworth 
  
   Parts of this program are base on Bryan Burns work with fastjar 
@@ -21,9 +21,37 @@
   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-/* $Id: jargrep.c,v 1.2 2000/12/11 02:59:55 apbianco Exp $
+/* $Id: jargrep.c,v 1.3 2000/12/14 18:45:35 ghazi Exp $
 
 $Log: jargrep.c,v $
+Revision 1.3  2000/12/14 18:45:35  ghazi
+Warning fixes:
+
+	* compress.c: Include stdlib.h and compress.h.
+	(rcsid): Delete.
+	(report_str_error): Make static.
+	(ez_inflate_str): Delete unused variable.  Add parens in if-stmt.
+	(hrd_inflate_str): Likewise.
+
+	* compress.h (init_compression, end_compression, init_inflation,
+	end_inflation): Prototype void arguments.
+
+	* dostime.c (rcsid): Delete.
+
+	* jargrep.c: Include ctype.h, stdlib.h, zlib.h and compress.h.
+	Make functions static.  Cast ctype function argument to `unsigned
+	char'.  Add parens in if-stmts.  Constify.
+	(Usage): Change into a macro.
+	(jargrep): Remove unused parameter.
+
+	* jartool.c: Constify.  Add parens in if-stmts.  Align
+	signed/unsigned char pointers in functions calls using casts.
+	(rcsid): Delete.
+	(list_jar): Fix printf format specifier.
+	(usage): Chop long string into bits.  Reformat.
+
+	* pushback.c (rcsid): Delete.
+
 Revision 1.2  2000/12/11 02:59:55  apbianco
 2000-12-10  Robert Lipe <robertlipe@usa.net>
 
@@ -71,10 +99,14 @@ will test some other platforms later.
 #include "zipfile.h"
 #include "zlib.h"
 #include "compress.h"
+#ifdef HAVE_GETOPT_H
+#include <getopt.h>
+#endif
 
 #define Usage "Usage: %s [-bcinsw] <-e regexp | regexp> file(s)\n"
 
 extern char *optarg;
+extern int optind;
 
 /*
 Function name: opt_valid
