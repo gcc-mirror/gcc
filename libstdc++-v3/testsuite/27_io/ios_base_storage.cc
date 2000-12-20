713c16f8601f76998538ee6999b5cc33eabf7208
Benjamin Kosnik <bkoz@gcc.gnu.org>
2000-12-20 08:24:40 +0000

    [multiple changes]
    2000-12-20  Benjamin Kosnik  <bkoz@fillmore.constant.com>
    
    	* testsuite/17_intro/headers_c.cc (main): New file.
    
    	* acinclude.m4 (SECTION_FLAGS): Only set if --enable-debug is not
    	passed.
    	(OPT_LDFLAGS): And here.
    	* aclocal.m4: Regenerate.
    	* configure: Regenerate.
    
    2000-12-19  Curtis Janssen  <cljanss@ca.sandia.gov>
    
    	* src/ios.cc (ios_base::_M_grow_words(int ix)): Fix libstdc++/1089.
    	* testsuite/27_io/ios_base_storage.cc (test01): New file.
    
    From-SVN: r38392

---
+++ libstdc++-v3/testsuite/27_io/ios_base_storage.cc
@@ -0,0 +1,54 @@
+// 2000-12-19 bkoz
+
+// Copyright (C) 2000 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 27.4.2.5 ios_base storage functions
+
+#include <sstream>
+#include <iostream>
+
+#include <debug_assert.h>
+
+// http://gcc.gnu.org/ml/gcc-bugs/2000-12/msg00413.html
+void test01() 
+{
+  bool test = true;
+  
+  using namespace std;
+
+  long x1 = ios::xalloc();
+  long x2 = ios::xalloc();
+  long x3 = ios::xalloc();
+  long x4 = ios::xalloc();
+
+  ostringstream out("the element of crime, lars von trier");
+  out.pword(++x4); // should not crash
+}
+
+
+int main(void)
+{
+  test01();
+
+  return 0;
+}
+
+
+
+
