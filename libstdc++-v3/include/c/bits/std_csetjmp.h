9582cacddbbb5929381936d4122c78a47c8b015c
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-12-22 00:04:33 +0000

    2000-12-21  Benjamin Kosnik  <bkoz@redhat.com>
    	* configure.in: Change GLIBCPP_ENABLE_SHADOW to
    	GLIBCPP_ENABLE_CHEADERS.
    	* acinclude.m4 (GLIBCPP_ENABLE_SHADOW): Change to
    	ENABLE_CHEADERS.  C_INCLUDE_DIR defaults to c_std from c.
    	* aclocal.m4: Regenerate.
    	* configure: Regenerate.
    	* include/c: Move to ...
    	* include/c_std: ...here.
    	* include/c_std: Move to ...
    	* include/c_shadow: ...here.
    	* include/c: Repopulate, with simple headers.
    	* docs/html/configopts.html: Adjust docs.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38438 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/c/bits/std_csetjmp.h
+++ libstdc++-v3/include/c/bits/std_csetjmp.h
@@ -1,6 +1,6 @@
 // -*- C++ -*- forwarding header.
 
-// Copyright (C) 2000 Free Software Foundation, Inc.
+// Copyright (C) 1997-1999, 2000 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -38,5 +38,11 @@
 
 #pragma GCC system_header
 #include <setjmp.h>
+
+namespace std
+{
+  using ::jmp_buf;
+  extern "C" void longjmp(jmp_buf, int);
+}
 
 #endif
