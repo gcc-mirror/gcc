3e730fb8dbfc6d44894bee940d2ef7b61d8aa0f7
Bernd Schmidt <bernds@redhat.co.uk>
2000-12-22 14:55:10 +0000

    Fix a vax failure
    From-SVN: r38462

--- gcc/calls.c
+++ gcc/calls.c
@@ -405,7 +405,15 @@ emit_call_1 (funexp, fndecl, funtype, stack_size, rounded_stack_size,
 
 #ifndef ACCUMULATE_OUTGOING_ARGS
 #if defined (HAVE_call_pop) && defined (HAVE_call_value_pop)
-  if (HAVE_call_pop && HAVE_call_value_pop && n_popped > 0)
+/* If the target has "call" or "call_value" insns, then prefer them
+   if no arguments are actually popped.  If the target does not have
+   "call" or "call_value" insns, then we must use the popping versions
+   even if the call has no arguments to pop.  */
+  if (HAVE_call_pop && HAVE_call_value_pop
+#if defined (HAVE_call) && defined (HAVE_call_value)
+      && (n_popped > 0 || ! HAVE_call || ! HAVE_call_value)
+#endif
+      )
     {
       rtx n_pop = GEN_INT (n_popped);
       rtx pat;
