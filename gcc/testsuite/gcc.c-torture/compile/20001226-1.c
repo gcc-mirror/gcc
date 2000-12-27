6b8bb84f31faddc860ff89aa0495fbb35375eb56
geoffk <geoffk@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-12-27 11:01:03 +0000

    2000-12-27  Geoffrey Keating  <geoffk@redhat.com>
    	* config/rs6000/rs6000.md (define_attr "length"): Correct
    	calculation.
    
    2000-12-26  Geoffrey Keating  <geoffk@redhat.com>
    
    	* gcc.c-torture/compile/20001226-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38495 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/20001226-1.c
@@ -0,0 +1,26 @@
+/* This testcase exposed two branch shortening bugs on powerpc.  */
+
+#define C(a,b) \
+  if (a > b)  goto gt; \
+  if (a < b)  goto lt;
+
+#define C4(x,b) C((x)[0], b) C((x)[1],b) C((x)[2],b) C((x)[3],b)
+#define C16(x,y) C4(x, (y)[0]) C4(x, (y)[1]) C4(x, (y)[2]) C4(x, (y)[3])
+
+#define C64(x,y) C16(x,y) C16(x+4,y) C16(x+8,y) C16(x+12,y)
+#define C256(x,y) C64(x,y) C64(x,y+4) C64(x,y+8) C64(x,y+12)
+
+#define C1024(x,y) C256(x,y) C256(x+16,y) C256(x+32,y) C256(x+48,y)
+#define C4096(x,y) C1024(x,y) C1024(x,y+16) C1024(x,y+32) C1024(x,y+48)
+
+unsigned foo(int x[64], int y[64])
+{
+  C4096(x,y);
+  
+  return 0x01234567;
+ gt:
+  return 0x12345678;
+ lt:
+  return 0xF0123456;
+}
+
