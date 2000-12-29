fbf66769761c914c46066f0581ae0a49d493209c
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2000-12-29 09:27:22 +0000

    * gcc.dg/20001117-1.c (__cyg_profile_func_enter): Define.         (__cyg_profile_func_exit): Define.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38531 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/20001117-1.c
+++ gcc/testsuite/gcc.dg/20001117-1.c
@@ -20,3 +20,6 @@ int main ()
     abort ();
   exit (0);
 }
+
+void __attribute__((no_instrument_function)) __cyg_profile_func_enter() { }
+void __attribute__((no_instrument_function)) __cyg_profile_func_exit() { } 
