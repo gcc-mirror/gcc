b74ec6e7061a8ac5d0c7f69671cdad3b6ea9a11b
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-01-01 13:19:20 +0000

    * config.gcc (sparc-*-elf): Include sparc/sol2.h. 	(sparc-*-rtems*): Include sparc/sol2.h and sparc/elf.h. 	(sparclite-*-elf*, sparc86x-*-elf*): Likewise. 	* config/sparc/elf.h: No longer include sol2.h. 	* config/sparc/lifeelf.h: No longer include sparc/elf.h. 	* config/sparc/rtemself.h, config/sparc/sp86x-elf.h: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38587 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sparc/liteelf.h
+++ gcc/config/sparc/liteelf.h
@@ -19,8 +19,6 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-#include "sparc/elf.h"
-
 #undef CPP_PREDEFINES
 #define CPP_PREDEFINES "-D__sparc__ -D__sparclite__ -Acpu=sparc -Amachine=sparc"
 
