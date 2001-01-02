19a4efe71ed22ede45376172f18deda683aab5e6
cchavva <cchavva@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-01-02 16:37:39 +0000

    Initial version.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38628 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20001227-1.c
@@ -0,0 +1,32 @@
+typedef struct {
+  char y;
+  char x[32];
+} X;
+
+int z (void)
+{
+  X xxx;
+  xxx.x[0] =
+  xxx.x[31] = '0';
+  xxx.y = 0xf;
+  return f (xxx, xxx);
+}
+
+int main (void)
+{
+  int val;
+
+  val = z ();
+  if (val != 0x60)
+    abort ();
+  exit (0);
+}
+
+int f(X x, X y)
+{
+  if (x.y != y.y)
+    return 'F';
+
+  return x.x[0] + y.x[0];
+}
+
