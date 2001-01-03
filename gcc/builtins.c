1515f92ff456e0479c0f4358519f0f1af5aae3d3
ghazi <ghazi@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-01-03 16:54:46 +0000

    update copyrights
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38665 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/builtins.c
+++ gcc/builtins.c
@@ -1,6 +1,6 @@
 /* Expand builtin functions.
    Copyright (C) 1988, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000 Free Software Foundation, Inc.
+   1999, 2000, 2001 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
