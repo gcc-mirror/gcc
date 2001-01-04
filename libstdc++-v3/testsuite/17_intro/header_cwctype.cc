e9049f5741d04553d9a5de0ddc003c05048ee823
Benjamin Kosnik <bkoz@gcc.gnu.org>
2001-01-04 21:57:30 +0000

    std_cwctype.h: Include std_cwchar.h for wint_t.
    2001-01-03  Benjamin Kosnik  <bkoz@redhat.com>
    
    	* include/c_std/bits/std_cwctype.h: Include std_cwchar.h for wint_t.
    	* testsuite/17_intro/header_cwctype.cc (main): New file.
    
    	* src/Makefile.am (base_headers): Change.
    	* include/bits/std_string.h: And here.
    	* include/bits/string.tcc: Tweaks, move to...
    	* include/bits/basic_string.tcc: ...Here.
    	* src/string-inst.cc: Simplify, just instantiate the whole class,
    	not member-by-member.
    
    From-SVN: r38693

---
+++ libstdc++-v3/testsuite/17_intro/header_cwctype.cc
@@ -0,0 +1,36 @@
+// 2000-01-03 bkoz
+
+// Copyright (C) 2001 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 17.4.1.2 Headers, cwctype
+
+#if _GLIBCPP_USE_WCHAR_T
+#include <cwctype>
+  // Make sure wint_t is in namespace std
+  std::wint_t i = 5;
+#endif
+
+int main(void)
+{
+  return 0;
+}
+
+
+
+
