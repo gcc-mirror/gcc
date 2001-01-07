45b8ddb4915e571fd5a71c46cd5c7ee10de0b3e9
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2001-01-07 23:03:53 +0000

    * expr.c (cplus_expand_expr): Don't reset `target'.
    From-SVN: r38787

--- gcc/cp/expr.c
+++ gcc/cp/expr.c
@@ -1,7 +1,7 @@
 /* Convert language-specific tree expression to rtl instructions,
    for GNU compiler.
    Copyright (C) 1988, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   2000 Free Software Foundation, Inc.
+   2000, 2001 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
@@ -100,10 +100,6 @@ cplus_expand_expr (exp, target, tmode, modifier)
   tree type = TREE_TYPE (exp);
   register enum machine_mode mode = TYPE_MODE (type);
   register enum tree_code code = TREE_CODE (exp);
-  int ignore = target == const0_rtx;
-
-  if (ignore)
-    target = 0;
 
   /* No sense saving up arithmetic to be done
      if it's all in the wrong mode to form part of an address.
