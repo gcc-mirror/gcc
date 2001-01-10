11186a20e9a9cb37769d0d1f35497e74b9541b1c
alehotsky <alehotsky@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-01-10 01:14:28 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38846 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/920730-1.c
+++ gcc/testsuite/gcc.c-torture/execute/920730-1.c
@@ -1,27 +1,27 @@
 /* 920730-1.c */
-
+#include <limits.h>
 f1()
 {
-	int b=0x80000000;
-	return b>=0x80000000;
+	int b=INT_MIN;
+	return b>=INT_MIN;
 }
 
 f2()
 {
-	int b=0x80000001;
-	return b>=0x80000001;
+	int b=INT_MIN+1;
+	return b>= (unsigned)(INT_MAX+2);
 }
 
 f3()
 {
-	int b=0x7fffffff;
-	return b>=0x7fffffff;
+	int b=INT_MAX;
+	return b>=INT_MAX;
 }
 
 f4()
 {
-	int b=0xffffffff;
-	return b>=0xffffffff;
+	int b=-1;
+	return b>=UINT_MAX;
 }
 
 main ()
