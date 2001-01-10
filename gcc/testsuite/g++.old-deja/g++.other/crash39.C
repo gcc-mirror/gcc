b1244a687c2e4fb232060d08a5c71285621d8748
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-01-10 15:30:59 +0000

    cp: 	* class.c (duplicate_tag_error): Clear more flags. testsuite: 	* g++.old_deja/g++.other/crash39.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38867 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.other/crash39.C
@@ -0,0 +1,18 @@
+// Build don't link:
+
+// Copyright (C) 2000 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 10 Jan 2001 <nathan@codesourcery.com>
+
+// Bug 595. We failed to clear out some things after seeing a duplicate
+// struct definition. That caused us to become inconsistent.
+
+struct X
+{
+  ~X ();
+};
+struct S { X a; };  // ERROR - previous defn
+struct S { X a; };  // ERROR - redefinition
+
+void c1(S s)
+{
+}
