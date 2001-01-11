9e0e4679a884a1212e2d942793e2c682c22b0ec0
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-01-11 12:50:28 +0000

    testsuite: 	* g++.old-deja/g++.pt/instantiate13.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38904 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.pt/instantiate13.C
@@ -0,0 +1,25 @@
+// Build don't link:
+
+// Copyright (C) 2000 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 11 Jan 2001 <nathan@codesourcery.com>
+
+// Bug 1551. We were accessing some uninitialized memory, causing us
+// to reject this.
+
+template <typename T>
+struct base
+{
+base();
+base(unsigned);
+};
+
+template <typename V>
+struct Y
+{
+Y(unsigned = 0);
+};
+
+template <>
+Y<char>::Y(unsigned) { }
+
+base<double> x;
