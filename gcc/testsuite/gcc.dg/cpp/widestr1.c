8d7e08bd25626e9dbf6cbb7c2c963d4c763d53d1
neil <neil@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-01-13 18:40:49 +0000

    * gcc.dg/cpp/widestr1.c: Update.         * gcc.dg/cpp/prag-imp.c: Remove.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@38988 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cpp/widestr1.c
+++ gcc/testsuite/gcc.dg/cpp/widestr1.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2000  Free Software Foundation.
+/* Copyright (C) 2000, 2001  Free Software Foundation.
 
    by Alexandre Oliva  <oliva@lsd.ic.unicamp.br>  */
 
@@ -6,4 +6,4 @@
 
 #line 1 L"foo" /* { dg-error "not a valid filename" "wide string in #line" } */
 #include L"stdio.h" /* { dg-error "expects" "wide string in #include" } */
-#pragma implementation L"test.h" /* { dg-error "malformed" "wide string in #pragma implementation" } */
+
