fa7aa1feb6fde728a03c5275bacde8073d6e50eb
Jakub Jelinek <jakub@redhat.com>
2001-01-19 19:03:44 +0100

    * gcc.c-torture/execute/20010119-1.c: New test.
    From-SVN: r39138

---
+++ gcc/testsuite/gcc.c-torture/execute/20010119-1.c
@@ -0,0 +1,23 @@
+#ifdef __OPTIMIZE__
+extern void undef (void);
+
+void bar (unsigned x) { }
+void baz (unsigned x) { }
+
+extern inline void foo (int a, int b)
+{
+  int c = 0;
+  while (c++ < b)
+    (__builtin_constant_p (a) ? ((a) > 20000 ? undef () : bar (a)) : baz (a));
+}
+#else
+void foo (int a, int b)
+{
+}
+#endif
+
+int main (void)
+{
+  foo(10, 100);
+  exit (0);
+}
