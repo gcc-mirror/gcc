445c435a52d25ed61ed9698e56d09309c5d949c6
Joseph Myers <jsm28@cam.ac.uk>
2001-01-24 19:47:47 +0000

    texi2pod.pl: Handle "\,".
    contrib:
    	* texi2pod.pl: Handle "\,".
    
    gcc:
    	* gcc.1, cpp.1: Regenerate.
    
    From-SVN: r39244

--- contrib/texi2pod.pl
+++ contrib/texi2pod.pl
@@ -144,6 +144,7 @@ while(<STDIN>)
     s/\@TeX\{\}/TeX/g;
     s/\@pounds\{\}/\#/g;
     s/\@minus(?:\{\})?/-/g;
+    s/\\,/,/g;
 
     # Now the ones that have to be replaced by special escapes
     # (which will be turned back into text by unmunge())
