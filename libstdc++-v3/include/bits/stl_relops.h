69f1ee0e7e19cab89735d0369672db6583ebcc85
pme <pme@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-01-25 15:35:10 +0000

    2001-01-25  Phil Edwards  <pme@sources.redhat.com>
    	* include/bits/std_iterator.h:  Do not include stl_relops.h.
    	* include/bits/std_numeric.h:  Ditto.
    	* include/bits/stl_algobase.h:  Ditto.
    	* include/bits/stl_relops.h:  Add comment warning about problems.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@39267 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/stl_relops.h
+++ libstdc++-v3/include/bits/stl_relops.h
@@ -28,6 +28,16 @@
  *   You should not attempt to use it directly.
  */
 
+/**** libstdc++-v3 note:  Inclusion of this file has been removed from
+ * all of the other STL headers for safety reasons, except std_utility.h.
+ * For more information, see the thread of about twenty messages starting
+ * with <URL:http://gcc.gnu.org/ml/libstdc++/2001-01/msg00223.html>, or the
+ * FAQ at <URL:http://gcc.gnu.org/onlinedocs/libstdc++/faq/index.html#4_4>.
+ *
+ * Short summary:  the rel_ops operators cannot be made to play nice.
+ * Don't use them.
+*/
+
 #ifndef _CPP_BITS_STL_RELOPS_H
 #define _CPP_BITS_STL_RELOPS_H 1
 
