33d1e63a77e6a2d69ea5b1d0dab5eabcd0bf608c
neil <neil@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-01-31 07:50:11 +0000

    * gcc.dg/cpp/avoidpaste1.c: Update.         * gcc.dg/cpp/paste4.c: Update.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@39367 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cpp/paste4.c
+++ gcc/testsuite/gcc.dg/cpp/paste4.c
@@ -1,9 +1,11 @@
 /* Copyright (C) 2000 Free Software Foundation, Inc.  */
 /* { dg-do compile } */
+/* { dg-options -save-temps } */
 
 /* Since 1.0e and + form the pasted token, 1 is a separate token and
    so should be output with a preceding space.  The old preprocessor
-   gets this wrong.  */
+   gets this wrong.  We use -save-temps to avoid direct use of the
+   integrated preprocessor.  */
 
 #define glue(x, y) x ## y
 
