9a3bcffd72bd546edf923d22039b25bce3b3b490
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-02-07 13:27:09 +0000

    * gcc.c-torture/execute/20010206-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@39517 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20010206-1.c
@@ -0,0 +1,16 @@
+int foo (void)
+{
+  int i;
+#line 1 "20010206-1.c"
+  if (0) i = 1; else i
+#line 1 "20010206-1.c"
+    = 26;
+  return i;
+}
+
+int main ()
+{
+  if (foo () != 26)
+    abort ();
+  exit (0);
+}
