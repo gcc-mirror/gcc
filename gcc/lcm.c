b25a2bca64005e2439fc27e2aff218000eb079b1
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-02-07 17:44:47 +0000

    Update Copyright notice
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@39519 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/lcm.c
+++ gcc/lcm.c
@@ -1,5 +1,5 @@
 /* Generic partial redundancy elimination with lazy code motion support.
-   Copyright (C) 1998, 1999, 2000 Free Software Foundation, Inc.
+   Copyright (C) 1998, 1999, 2000, 2001 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
