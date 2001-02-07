f219fbbfe00cd568d3fb5cf1a474956adb7c62c0
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-02-07 22:04:10 +0000

    * c-decl.c (finish_enum): Revert part of 2000-01-05 change.
    	* gcc.dg/20010202-1.c: New test.
    	* gcc.dg/991209-1.c: Compile on whole ia32 family, not just i386.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@39527 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20010202-1.c
@@ -0,0 +1,24 @@
+/* { dg-do compile { target i?86-*-* sparc*-*-* } } */
+/* { dg-options "-O2" } */
+
+typedef enum { false, true } __attribute__ ((packed)) boolean;
+typedef struct {
+  enum {
+    A0 = 0, A1 = 1, A2 = 2
+  } __attribute__((packed)) A:3;
+  enum {
+    B0 = 0, B1 = 1, B2 = 2
+  } __attribute__((packed)) B:3;
+  boolean C:1;
+  boolean D:1;
+  unsigned char :8;
+} foo;
+foo x = { A2, B1, false, true };
+
+int main(void)
+{
+  if (sizeof (foo) != 2 || __alignof__ (foo) != 1)
+    abort ();
+
+  exit (0);
+}
