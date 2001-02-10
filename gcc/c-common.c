3220116ffb472e3330fcebaf30246c74a271dfe0
Anthony Green <green@redhat.com>
2001-02-10 23:30:55 +0000

    Doc and warning fixes.
    From-SVN: r39580

--- gcc/c-common.c
+++ gcc/c-common.c
@@ -466,7 +466,7 @@ combine_strings (strings)
   nchars = wide_flag ? length / wchar_bytes : length;
 
   if (pedantic && nchars - 1 > nchars_max && c_language == clk_c)
-    pedwarn ("string length `%d' is greater than the minimum length `%d' ISO C%d is required to support",
+    pedwarn ("string length `%d' is greater than the length `%d' ISO C%d compilers are required to support",
 	     nchars - 1, nchars_max, flag_isoc99 ? 99 : 89);
 
   /* Create the array type for the string constant.
