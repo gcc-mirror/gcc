fe375cf108993c9afcef778e4b2def2ddf43160e
Jakub Jelinek <jakub@gcc.gnu.org>
2001-02-14 08:57:49 +0100

    stor-layout.c (is_pending_size, [...]): New functions.
    	* stor-layout.c (is_pending_size, put_pending_size): New functions.
    	(variable_size): Call put_pending_size.
    	* tree.h (is_pending_size, put_pending_size): Add prototypes.
    	* fold-const.c (extract_muldiv): If SAVE_EXPR is on the pending
    	sizes list, put newly created SAVE_EXPR there as well.
    
    	* gcc.c-torture/execute/20010209-1.c: New test.
    
    	* config/ia64/ia64.c (last_group): Only 2 entries are needed.
    	(errata_find_address_regs): load_group has only 2 entries.
    	(errata_emit_nops): Likewise.  shladd is not problematic.
    	Clear last_group if nop was emitted.
    	(fixup_errata): load_group has only 2 entries.
    	Optimize.
    
    From-SVN: r39663

---
+++ gcc/testsuite/gcc.c-torture/execute/20010209-1.c
@@ -0,0 +1,21 @@
+int b;
+int foo (void)
+{
+  int x[b];
+  int bar (int t[b])
+  {
+    int i;
+    for (i = 0; i < b; i++)
+      t[i] = i + (i > 0 ? t[i-1] : 0);
+    return t[b-1];
+  }
+  return bar (x);
+}
+
+int main ()
+{
+  b = 6;
+  if (foo () != 15)
+    abort ();
+  exit (0);
+}
