fe00962b8a892eb5eb599b2143800ecd8c00c285
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-02-15 12:54:38 +0000

    * friend1.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@39714 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.oliva/friend1.C
@@ -0,0 +1,12 @@
+// Build don't link:
+
+// Copyright (C) 2001 Free Software Foundation
+
+// by Alexandre Oliva <aoliva@redhat.com>
+
+// We shouldn't warn about bar referring to a non-template in this case.
+
+template <typename T>
+class foo {
+  friend int bar(int);
+};
