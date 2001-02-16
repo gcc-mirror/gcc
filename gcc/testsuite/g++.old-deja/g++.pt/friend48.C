dc38c36b628b2f66cd4d4dceb51d9d79897967ef
Mark Mitchell <mark@codesourcery.com>
2001-02-16 07:52:44 +0000

    pt.c (push_template_decl_real): Don't remangle the name of a class template.
    	* pt.c (push_template_decl_real): Don't remangle the name of a
    	class template.
    
    From-SVN: r39751

---
+++ gcc/testsuite/g++.old-deja/g++.pt/friend48.C
@@ -0,0 +1,17 @@
+// Build don't link:
+// Origin: Mark Mitchell <mark@codesourcery.com>
+
+template <class T>
+class C {
+  template <class U>
+  friend class ::C;
+};
+
+namespace N 
+{
+template <class T>
+class D {
+  template <class U>
+  friend class N::D;
+};
+};
