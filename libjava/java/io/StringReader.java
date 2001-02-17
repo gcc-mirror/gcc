35c833bf998c89b83111d903660e72c1ea463dee
mark <mark@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-02-17 13:59:13 +0000

    * java/io/DataInputStream.java: update copyright notice     * java/io/PrintWriter.java: idem     * java/io/Reader.java: idem     * java/io/StreamTokenizer.java: idem     * java/io/StringReader.java: idem     * java/lang/reflect/ReflectPermission.java: idem
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@39798 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/io/StringReader.java
+++ libjava/java/io/StringReader.java
@@ -1,11 +1,29 @@
-/* Copyright (C) 1998, 1999, 2000  Free Software Foundation
+/* StringReader.java -- permits a String to be read as a character input stream
+   Copyright (C) 1998, 1999, 2000  Free Software Foundation
 
-   This file is part of libgcj.
+This file is part of GNU Classpath.
 
-This software is copyrighted work licensed under the terms of the
-Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
-details.  */
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
  
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+As a special exception, if you link this library with other files to
+produce an executable, this library does not by itself cause the
+resulting executable to be covered by the GNU General Public License.
+This exception does not however invalidate any other reasons why the
+executable file might be covered by the GNU General Public License. */
+
 package java.io;
 
 /* Written using "Java Class Libraries", 2nd edition, ISBN 0-201-31002-3
--- libjava/java/lang/reflect/ReflectPermission.java
+++ libjava/java/lang/reflect/ReflectPermission.java
@@ -1,17 +1,28 @@
-// ReflectPermission.java - Process modifier values.
+/* ReflectPermission.java - named permission for reflaction
+   Copyright (C) 2000, 2001  Free Software Foundation
 
-/* Copyright (C) 2000  Free Software Foundation
+This file is part of GNU Classpath.
 
-   This file is part of libgcj.
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
 
-This software is copyrighted work licensed under the terms of the
-Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
-details.  */
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
 
-/**
- * @author Tom Tromey <tromey@redhat.com>
- * @date November 18, 2000
- */
+As a special exception, if you link this library with other files to
+produce an executable, this library does not by itself cause the
+resulting executable to be covered by the GNU General Public License.
+This exception does not however invalidate any other reasons why the
+executable file might be covered by the GNU General Public License. */
 
 /* Written using "Java Class Libraries", 2nd edition, ISBN 0-201-31002-3
  * "The Java Language Specification", ISBN 0-201-63451-1
@@ -26,6 +37,9 @@ import java.security.BasicPermission;
 /**
  * This class implements permissions for reflection.  This is a named
  * permission, and the only defined name is suppressAccessChecks.
+ *
+ * @author Tom Tromey <tromey@redhat.com>
+ * @date November 18, 2000
  */
 public final class ReflectPermission extends BasicPermission
 {
