a714e5c5d34f921252d147a23ff3bc0642413824
Jason Merrill <jason@redhat.com>
2001-02-17 19:00:29 -0500

    decl2.c (build_artificial_parm): Set TREE_READONLY.
            * decl2.c (build_artificial_parm): Set TREE_READONLY.
    
            * decl.c (bad_specifiers): Allow throw specs on things with
            pointer-to-function or -member-function type.
            * init.c (build_default_init): Don't use a CONSTRUCTOR to initialize
            a pmf.
    
    From-SVN: r39810

---
+++ gcc/testsuite/g++.old-deja/g++.eh/spec7.C
@@ -0,0 +1,19 @@
+// Test that we allow simple throw specs on pointers.
+
+void f() throw () { }
+void (*pf)() throw () = f;
+
+struct A
+{
+  void g() throw () { }
+  static void (A::*pmf)() throw ();
+};
+
+void (A::* A::pmf)() = &A::g;
+
+int main()
+{
+  pf ();
+  A a;
+  (a.*A::pmf)();
+}
