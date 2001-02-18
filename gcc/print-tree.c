84011a0731c563454e69dfba736c5294c2dd6276
Mark Mitchell <mmitchel@gcc.gnu.org>
2001-02-18 06:03:45 +0000

    print-tree.c (print_node): Do not use BLOCK_CHAIN when we're not looking at a BLOCK.
    	* print-tree.c (print_node): Do not use BLOCK_CHAIN when we're not
    	looking at a BLOCK.
    
    From-SVN: r39819

--- gcc/print-tree.c
+++ gcc/print-tree.c
@@ -641,7 +641,7 @@ print_node (file, prefix, node, indent)
 		    EXPR_WFL_FILENAME (node) : "(no file info)"),
 		   EXPR_WFL_LINENO (node), EXPR_WFL_COLNO (node));
 	}
-      print_node (file, "chain", BLOCK_CHAIN (node), indent + 4);
+      print_node (file, "chain", TREE_CHAIN (node), indent + 4);
       break;
 
     case 'c':
