7539316943c17368ce5ef876a9baba6b8c2e64f6
Joseph Myers <jsm28@cam.ac.uk>
2001-02-19 20:03:42 +0000

    Version.c, [...]: Update G77 version number to 0.5.27.
    libf2c:
    	* libF77/Version.c, libI77/Version.c, libU77/Version.c: Update G77
    	version number to 0.5.27.
    
    gcc:
    	* README, cpp.texi, gcc.texi, version.c: Update version number to
    	3.1.
    	* cpp.1, gcov.1, gcc.1: Regenerate.
    
    gcc/f:
    	* version.c, root.texi: Update GCC version number to 3.1.  Update
    	G77 version number to 0.5.27.
    	* BUGS, NEWS: Regenerate.
    
    From-SVN: r39901

--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
 #include "gansidecl.h"
 #include "version.h"
 
-const char *const version_string = "2.97 20010219 (experimental)";
+const char *const version_string = "3.1 20010219 (experimental)";
--- libf2c/libU77/Version.c
+++ libf2c/libU77/Version.c
@@ -1,6 +1,6 @@
 static char junk[] = "\n@(#) LIBU77 VERSION 19980709\n";
 
-char __G77_LIBU77_VERSION__[] = "0.5.26 20010219 (experimental)";
+char __G77_LIBU77_VERSION__[] = "0.5.27 20010219 (experimental)";
 
 #include <stdio.h>
 
