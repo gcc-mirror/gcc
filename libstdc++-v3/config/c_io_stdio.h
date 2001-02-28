e529189ccc80855a7d273bcb5346f7d3a7ca8018
Benjamin Kosnik <bkoz@redhat.com>
2001-02-28 06:45:36 +0000

    basic_file.h: Include basic_file_model.h.
    2001-02-27  Benjamin Kosnik  <bkoz@redhat.com>
    
    	libstdc++/1886
    	* include/bits/basic_file.h: Include basic_file_model.h.
    	* config/c_io_libio.cc: Move to...
    	* config/basic_file_libio.cc: Here.
    	* config/basic_file_libio.h: New file.
    	* config/c_io_stdio.cc: Move to...
    	* config/basic_file_stdio.cc: Here.
    	* config/basic_file_stdio.h: New file.
    	* config/c_io_libio.h: Tweak.
    	* config/c_io_stdio.h: Tweak.
    	* src/Makefile.am (build_headers): Add basic_file_model.h.
    	(sources): Add basic_file.cc, remove c++io.cc.
    	* src/Makefile.in: Regenerate.
    	* acinclude.m4 (GLIBCPP_ENABLE_CSTDIO): Add support for
    	basic_file_model.h and basic_file.cc.
    	* aclocal.m4: Regenerate.
    	* configure: Regenerate.
    	* testsuite/27_io/filebuf.cc (test05): Add regression.
    
    From-SVN: r40112

--- libstdc++-v3/config/c_io_stdio.h
+++ libstdc++-v3/config/c_io_stdio.h
@@ -1,6 +1,6 @@
 // underlying io library  -*- C++ -*-
 
-// Copyright (C) 2000 Free Software Foundation, Inc.
+// Copyright (C) 2000, 2001 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -36,8 +36,8 @@
 #include <stddef.h>
 #include <bits/c++threads.h>
 
-namespace std {
-
+namespace std 
+{
 // from fpos.h
   typedef long  	streamoff;
   typedef ptrdiff_t	streamsize; // Signed integral type
@@ -59,42 +59,41 @@ namespace std {
 
 // from ios_base.h
   struct __ios_flags
- {
-   typedef short __int_type;
+  {
+    typedef short __int_type;
 
-   static const __int_type _S_boolalpha =	0x0001;
-   static const __int_type _S_dec =		0x0002;
-   static const __int_type _S_fixed = 		0x0004;
-   static const __int_type _S_hex =		0x0008;
-   static const __int_type _S_internal = 	0x0010;
-   static const __int_type _S_left =   		0x0020;
-   static const __int_type _S_oct =		0x0040;
-   static const __int_type _S_right =		0x0080;
-   static const __int_type _S_scientific =	0x0100;
-   static const __int_type _S_showbase =       	0x0200;
-   static const __int_type _S_showpoint =	0x0400;
-   static const __int_type _S_showpos =		0x0800;
-   static const __int_type _S_skipws =		0x1000;
-   static const __int_type _S_unitbuf =		0x2000;
-   static const __int_type _S_uppercase	=	0x4000;
-   static const __int_type _S_adjustfield =	0x0020 | 0x0080 | 0x0010;
-   static const __int_type _S_basefield =	0x0002 | 0x0040 | 0x0008;
-   static const __int_type _S_floatfield =	0x0100 | 0x0004;
+    static const __int_type _S_boolalpha =	0x0001;
+    static const __int_type _S_dec =		0x0002;
+    static const __int_type _S_fixed = 		0x0004;
+    static const __int_type _S_hex =		0x0008;
+    static const __int_type _S_internal = 	0x0010;
+    static const __int_type _S_left =   	0x0020;
+    static const __int_type _S_oct =		0x0040;
+    static const __int_type _S_right =		0x0080;
+    static const __int_type _S_scientific =	0x0100;
+    static const __int_type _S_showbase =       0x0200;
+    static const __int_type _S_showpoint =	0x0400;
+    static const __int_type _S_showpos =	0x0800;
+    static const __int_type _S_skipws =		0x1000;
+    static const __int_type _S_unitbuf =	0x2000;
+    static const __int_type _S_uppercase =	0x4000;
+    static const __int_type _S_adjustfield =	0x0020 | 0x0080 | 0x0010;
+    static const __int_type _S_basefield =	0x0002 | 0x0040 | 0x0008;
+    static const __int_type _S_floatfield =	0x0100 | 0x0004;
 
     // 27.4.2.1.3  Type ios_base::iostate
-   static const __int_type _S_badbit =		0x01;
-   static const __int_type _S_eofbit =		0x02;
-   static const __int_type _S_failbit =		0x04;
+    static const __int_type _S_badbit =		0x01;
+    static const __int_type _S_eofbit =		0x02;
+    static const __int_type _S_failbit =       	0x04;
 
     // 27.4.2.1.4  Type openmode
-   static const __int_type _S_app =		0x01;
-   static const __int_type _S_ate =		0x02;
-   static const __int_type _S_bin =		0x04;
-   static const __int_type _S_in =		0x08;
-   static const __int_type _S_out =		0x10;
-   static const __int_type _S_trunc =		0x20;
+    static const __int_type _S_app =		0x01;
+    static const __int_type _S_ate =		0x02;
+    static const __int_type _S_bin =		0x04;
+    static const __int_type _S_in =		0x08;
+    static const __int_type _S_out =		0x10;
+    static const __int_type _S_trunc =		0x20;
   };
-
 }
 
 #endif // _CPP_IO_STDIO_H
