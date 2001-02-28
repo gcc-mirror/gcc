ddd48f501b0dd06b2e1916f13cfef87ac6a05875
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-02-28 00:14:16 +0000

    2001-02-27  Benjamin Kosnik  <bkoz@redhat.com>
    	Correct license.
    	* config/cpu/alpha/bits/atomicity.h: Change.
    	* config/cpu/arm/bits/atomicity.h
    	* config/cpu/generic/bits/atomicity.h
    	* config/cpu/i386/bits/atomicity.h
    	* config/cpu/i486/bits/atomicity.h
    	* config/cpu/ia64/bits/atomicity.h
    	* config/cpu/powerpc/bits/atomicity.h
    	* config/cpu/sparc/sparc32/bits/atomicity.h
    	* config/cpu/sparc/sparc64/bits/atomicity.h
    	* config/os/aix/bits/atomicity.h
    	* config/os/irix/bits/atomicity.h
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@40103 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/config/cpu/i486/bits/atomicity.h
+++ libstdc++-v3/config/cpu/i486/bits/atomicity.h
@@ -1,21 +1,31 @@
-/* Low-level functions for atomic operations.  ix86 version, x >= 4.
-   Copyright (C) 1997 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
+// Low-level functions for atomic operations: x86, x >= 4 version  -*- C++ -*-
 
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
+// Copyright (C) 1999, 2000, 2001 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
 
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
-   License along with the GNU C Library; see the file COPYING.LIB.  If not,
-   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// As a special exception, you may use this file as part of a free software
+// library without restriction.  Specifically, if other files instantiate
+// templates or use macros or inline functions from this file, or you compile
+// this file and link it with other files to produce an executable, this
+// file does not by itself cause the resulting executable to be covered by
+// the GNU General Public License.  This exception does not however
+// invalidate any other reasons why the executable file might be covered by
+// the GNU General Public License.
 
 #ifndef _BITS_ATOMICITY_H
 #define _BITS_ATOMICITY_H	1
