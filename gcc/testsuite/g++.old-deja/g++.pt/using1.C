fbfe8c9e5ab2168a5776947525909c485afe39f3
Nathan Sidwell <nathan@codesourcery.com>
2001-03-01 14:01:39 +0000

    Implement using decls inside template functions.
    cp:
    	Implement using decls inside template functions.
    	* decl2.c (validate_nonmember_using_decl): Don't special case
    	fake_std_node in the global namespace. Don't reject early when
    	processing a template.
    	(do_local_using_decl): Add to statement tree. Don't do further
    	processing when building a template.
    	* pt.c (tsubst_expr, DECL_STMT case): Deal with USING_DECLs.
    testsuite:
    	* g++.old-deja/g++.pt/using1.C: New test.
    
    From-SVN: r40151

---
+++ gcc/testsuite/g++.old-deja/g++.pt/using1.C
@@ -0,0 +1,36 @@
+// Copyright (C) 2001 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 26 Feb 2001 <nathan@codesourcery.com>
+
+// Bug 1981. using declarations in namespace scope were not remembered.
+
+namespace A
+{
+  void swap () {}
+};
+
+template <class T> void f()
+{
+  using A::swap;
+}
+
+template void f<float> ();
+
+namespace B
+{
+  int foo (int) { return 1;}
+  
+  template <class T> int baz ()
+  {
+    using ::foo;
+    
+    return foo (1);
+  }
+  template int baz<float> ();
+};
+
+int foo (int) { return 0;}
+
+int main ()
+{
+  return B::baz<float> ();
+}
