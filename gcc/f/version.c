6b451f90f8bb83ff1a85c6d8cbd67bda10de5ded
gccadmin <gccadmin@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-03-05 08:16:41 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@40246 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-const char *ffe_version_string = "0.5.27 20010304 (experimental)";
+const char *ffe_version_string = "0.5.27 20010305 (experimental)";
--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
 #include "gansidecl.h"
 #include "version.h"
 
-const char *const version_string = "3.1 20010304 (experimental)";
+const char *const version_string = "3.1 20010305 (experimental)";
