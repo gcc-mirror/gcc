2f6572cd7a1f7652c020512e5180c777e38318c9
aph <aph@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-03-06 20:45:06 +0000

    2001-03-06  Andrew Haley  <aph@redhat.com>
    	* typeck.c (convert_arguments): Check that we have an fndecl.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@40265 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/typeck.c
+++ gcc/cp/typeck.c
@@ -3207,7 +3207,7 @@ convert_arguments (typelist, values, fndecl, flags)
 	  if (TREE_CODE (TREE_TYPE (val)) == REFERENCE_TYPE)
 	    val = convert_from_reference (val);
 
-	  if (DECL_BUILT_IN (fndecl)
+	  if (fndecl && DECL_BUILT_IN (fndecl)
 	      && DECL_FUNCTION_CODE (fndecl) == BUILT_IN_CONSTANT_P)
 	    /* Don't do ellipsis conversion for __built_in_constant_p
 	       as this will result in spurious warnings for non-POD
