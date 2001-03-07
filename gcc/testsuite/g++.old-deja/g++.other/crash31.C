a17bbbd8b0d9a20eab8fe2778d560588888bc4a5
neil <neil@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-03-07 22:17:36 +0000

    * g++.old-deja/g++.other/crash31.C: XFAIL. 	* g77.f-torture/execute/20010116.x: Conditional XFAIL. 	* g77.f-torture/execute/short.x: XFAIL -Os on x86. 	* gcc.c-torture/execute/20010123-1.x: XFAIL. 	* gcc.c-torture/execute/20010124-1.x: XFAIL. 	* gcc.dg/sequence-pt-1.c: XFAIL one test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@40302 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.other/crash31.C
+++ gcc/testsuite/g++.old-deja/g++.other/crash31.C
@@ -1,11 +1,12 @@
 // Build don't link:
 // Origin: Jakub Jelinek <jakub@redhat.com>
 
-// crash test - XFAIL *-*-*
+// excess errors test - XFAIL *-*-*
 
 namespace bar
 {
 struct foo
 {
   foo();
-};			// ERROR - parse error
+};
+			// ERROR - parse error XFAIL *-*-*
