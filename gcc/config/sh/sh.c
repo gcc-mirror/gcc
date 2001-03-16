47890aff1eff51068db7d9677d8be444e72ba511
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-03-16 12:34:13 +0000

    * config/sh/sh.c (sh_expand_prologue): Insns that set up the PIC register may be dead.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@40550 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sh/sh.c
+++ gcc/config/sh/sh.c
@@ -3987,7 +3987,23 @@ sh_expand_prologue ()
   push_regs (live_regs_mask, live_regs_mask2);
 
   if (flag_pic && regs_ever_live[PIC_OFFSET_TABLE_REGNUM])
-    emit_insn (gen_GOTaddr2picreg ());
+    {
+      rtx insn = get_last_insn ();
+      rtx last = emit_insn (gen_GOTaddr2picreg ());
+
+      /* Mark these insns as possibly dead.  Sometimes, flow2 may
+	 delete all uses of the PIC register.  In this case, let it
+	 delete the initialization too.  */
+      do
+	{
+	  insn = NEXT_INSN (insn);
+
+	  REG_NOTES (insn) = gen_rtx_EXPR_LIST (REG_MAYBE_DEAD,
+						const0_rtx,
+						REG_NOTES (insn));
+	}
+      while (insn != last);
+    }
 
   if (target_flags != save_flags)
     {
