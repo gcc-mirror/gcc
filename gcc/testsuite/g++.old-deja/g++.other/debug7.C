110c3568a23b529419ca8d51c17ae4104c84e6d0
Mark Mitchell <mark@codesourcery.com>
2001-03-16 06:34:17 +0000

    * dwarf2out.c (rtl_for_decl_location): Use DECL_RTL_IF_SET.
    From-SVN: r40544

---
+++ gcc/testsuite/g++.old-deja/g++.other/debug7.C
@@ -0,0 +1,11 @@
+// Build don't run:
+// Origin: Loren James Rittle <rittle@latour.rsch.comm.mot.com>
+// Special g++ Options: -g -O2
+
+namespace std {
+  const int __stl_chunk_size = 7;
+};
+
+int main ()
+{
+}
