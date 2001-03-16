418f81eefb6540582e4818225404d4a647ad67eb
Bernd Schmidt <bernds@redhat.com>
2001-03-16 12:52:21 +0000

    Update ChangeLogs and version numbers for 2.95.3 release
    From-SVN: r40552

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-const char *ffe_version_string = "0.5.25 19991030 (prerelease)";
+const char *ffe_version_string = "0.5.25 20010315 (release)";
--- gcc/version.c
+++ gcc/version.c
@@ -1,1 +1,1 @@
-char *version_string = "2.95.3 20010312 (prerelease)";
+char *version_string = "2.95.3 20010315 (release)";
