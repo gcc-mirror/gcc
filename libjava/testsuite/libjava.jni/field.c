ae8a67c4c027244f570ea099d365ae93ab49c59f
Tom Tromey <tromey@redhat.com>
2001-03-23 05:57:00 +0000

    field.out: New file.
    	* libjava.jni/field.out: New file.
    	* libjava.jni/field.c: New file.
    	* libjava.jni/field.java: New file.
    
    From-SVN: r40776

---
+++ libjava/testsuite/libjava.jni/field.c
@@ -0,0 +1,24 @@
+#include <jni.h>
+#include <field.h>
+
+jobjectArray
+Java_field_fetch (JNIEnv *env, jobject this)
+{
+  jclass cls;
+  jfieldID fid;
+  jobjectArray obj;
+
+  cls = (*env)->GetObjectClass (env, this);
+  if (! cls)
+    return 0;
+
+  fid = (*env)->GetFieldID (env, cls, "F", "[Ljava/lang/Object;");
+  if (! fid)
+    return 0;
+
+  obj = (*env)->GetObjectField (env, this, fid);
+
+  return obj;
+}
+
+
