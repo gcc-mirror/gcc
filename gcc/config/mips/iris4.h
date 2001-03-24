26a0b4aff422646ffc4ed01e15ac88e66c512036
Richard Henderson <rth@gcc.gnu.org>
2001-03-23 17:21:11 -0800

    Update copyright.
    From-SVN: r40805

--- gcc/config/mips/iris4.h
+++ gcc/config/mips/iris4.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler.  Iris version 4.
-   Copyright (C) 1991, 1993, 1999 Free Software Foundation, Inc.
+   Copyright (C) 1991, 1993, 1999, 2001 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
