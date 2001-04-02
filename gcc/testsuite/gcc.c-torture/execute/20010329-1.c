fd03fc5d4d51651da4cea79357bd750e88f828d5
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-04-02 08:08:22 +0000

    * fold-const.c (fold): Before optimizing unsigned comparison with 	0x7fffffffU, make sure arg0 is integral type.
    	* gcc.c-torture/execute/20010329-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@41000 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20010329-1.c
@@ -0,0 +1,14 @@
+#include <limits.h>
+
+int main (void)
+{
+  void *x = ((void *)((unsigned int)INT_MAX + 2));
+  void *y = ((void *)((unsigned long)LONG_MAX + 2));
+  if (x >= ((void *)((unsigned int)INT_MAX + 1))
+      && x <= ((void *)((unsigned int)INT_MAX + 6))
+      && y >= ((void *)((unsigned long)LONG_MAX + 1))
+      && y <= ((void *)((unsigned long)LONG_MAX + 6)))
+    exit (0);
+  else
+    abort ();
+}
