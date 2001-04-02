5a9a8530f61faae07d0b94b58961aa70e47c588f
No Author <no-author@gcc.gnu.org>
2001-04-02 21:29:03 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_0-branch'.
    From-SVN: r41016

---
+++ gcc/testsuite/g77.f-torture/compile/20010321-1.f
@@ -0,0 +1,8 @@
+# 1 "20010321-1.f"
+      SUBROUTINE TWOEXP
+# 1 "include/implicit.h" 1 3
+      IMPLICIT DOUBLE PRECISION (A-H)
+# 3 "20010321-1.f" 2 3
+      LOGICAL ANTI
+      ANTI = .FALSE.
+      END
