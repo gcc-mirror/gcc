96fee988a9afafc33705ce0816e62b3ee2825ca0
Mark Mitchell <mmitchel@gcc.gnu.org>
2001-04-16 00:27:44 +0000

    Fix typo in test case
    From-SVN: r41365

--- gcc/testsuite/g++.old-deja/g++.other/ptrmem6.C
+++ gcc/testsuite/g++.old-deja/g++.other/ptrmem6.C
@@ -1,7 +1,7 @@
 // Build don't link:
 // crash test - XFAIL *-*-*
 
-// Copyright (c) 2000 Free Software Foundation.
+// Copyright (c) 2000, 2001 Free Software Foundation.
 // Distilled from a bug report by Eric Ford <eford@princeton.edu>
 
 extern double *y;
@@ -12,5 +12,5 @@ void SetInitCond(void)
 {
   int i;
   for(i = 2; i < nPoints; ++i)
-    y[i] = y[nPoints] .* (x[i]-x[1]) / (x[nPoints]-x[1]);  // ERROR
+    y[i] = y[nPoints] .* (x[i]-x[1]) / (x[nPoints]-x[1]);  // ERROR - 
 }
