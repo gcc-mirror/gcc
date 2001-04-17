5854680c2ce7745d1dad96fa10b0343d3e4f0cc9
John David Anglin <dave@hiauly1.hia.nrc.ca>
2001-04-17 17:54:22 +0000

    vax.h (PROMOTE_PROTOTYPES): Define for compatibility with system libraries and native K&R compilers.
    	* vax.h (PROMOTE_PROTOTYPES): Define for compatibility with system
    	libraries and native K&R compilers.
    
    From-SVN: r41391

--- gcc/config/vax/vax.h
+++ gcc/config/vax/vax.h
@@ -1,6 +1,6 @@
 /* Definitions of target machine for GNU compiler.  Vax version.
    Copyright (C) 1987, 1988, 1991, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000 Free Software Foundation, Inc.
+   1999, 2000, 2001 Free Software Foundation, Inc.
 
 This file is part of GNU CC.
 
@@ -864,6 +864,10 @@ enum reg_class { NO_REGS, ALL_REGS, LIM_REG_CLASSES };
 /* Value is 1 if truncating an integer of INPREC bits to OUTPREC bits
    is done just by pretending it is already truncated.  */
 #define TRULY_NOOP_TRUNCATION(OUTPREC, INPREC) 1
+
+/* When a prototype says `char' or `short', really pass an `int'.
+   (On the vax, this is required for system-library compatibility.)  */
+#define PROMOTE_PROTOTYPES 1
 
 /* Specify the machine mode that pointers have.
    After generation of rtl, the compiler makes no further distinction
