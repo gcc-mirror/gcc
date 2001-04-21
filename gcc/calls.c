3fc31190eb6e08da198174db3e154198fa11b614
Kazu Hirata <kazu@hxi.com>
2001-04-21 21:34:02 +0000

    calls.c (expand_call): Fix a comment typo.
    2001-04-21  Kazu Hirata  <kazu@hxi.com>
    
    	* calls.c (expand_call): Fix a comment typo.
    
    From-SVN: r41487

--- gcc/calls.c
+++ gcc/calls.c
@@ -2384,8 +2384,8 @@ expand_call (exp, target, ignore)
   args = (struct arg_data *) alloca (num_actuals * sizeof (struct arg_data));
   memset ((char *) args, 0, num_actuals * sizeof (struct arg_data));
 
-  /* Build up entries inthe ARGS array, compute the size of the arguments
-     into ARGS_SIZE, etc.  */
+  /* Build up entries in the ARGS array, compute the size of the
+     arguments into ARGS_SIZE, etc.  */
   initialize_argument_information (num_actuals, args, &args_size,
 				   n_named_args, actparms, fndecl,
 				   &args_so_far, reg_parm_stack_space,
