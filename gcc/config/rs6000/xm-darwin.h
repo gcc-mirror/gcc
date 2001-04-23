86648ad2c2cd6de60065736c07b291b278cc5818
shebs <shebs@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-04-23 01:14:03 +0000

    * config.gcc (powerpc-*-darwin*): Move the flag in xm-darwin.h         into xm_defines.         * config/rs6000/xm-darwin.h: Remove file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@41496 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/rs6000/xm-darwin.h
+++
@@ -1,5 +0,0 @@
-/* Override the usual setting, since Apple's GCC has lame bugs and
-   can't handle the initializers.  Someday the bugs will be fixed and
-   we can get rid of this silliness.  */
-
-#define HAVE_DESIGNATED_INITIALIZERS 0
