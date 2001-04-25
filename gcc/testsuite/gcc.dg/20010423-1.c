475fa9bdf917fd95ad208f807ff7379c8aa9be32
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-04-25 09:57:00 +0000

    * emit-rtl.c (subreg_hard_regno): Only do HARD_REGNO_MODE_OK check 	if check_mode is non-zero.
    	* gcc.dg/20010423-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@41537 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20010423-1.c
@@ -0,0 +1,17 @@
+/* { dg-do compile { target ia64-*-* } } */
+/* { dg-options "-O2" } */
+
+int __sync_fetch_and_add_si (int *, int);
+
+inline unsigned int
+bar (volatile unsigned int *mem, unsigned int val)
+{
+  return __sync_fetch_and_add_si((int *)mem, (int)val);
+}
+
+volatile unsigned int x;
+
+void foo (unsigned short *a)
+{
+  *a = bar (&x, 1) + 1;
+}
