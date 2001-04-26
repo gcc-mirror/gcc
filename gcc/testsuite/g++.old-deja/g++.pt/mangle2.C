0576ec13087498afc8beda9cda8da33f07ecdbeb
Nathan Sidwell <nathan@codesourcery.com>
2001-04-26 08:34:06 +0000

    mangle.c (write_chars): New macro.
    cp:
    	* mangle.c (write_chars): New macro.
    	(hwint_to_ascii): New function
    	(write_number): Use it.
    	(write_integer_cst): Deal with really big numbers.
    testsuite:
    	* g++.old-deja/g++.pt/mangle2.C: New test.
    
    From-SVN: r41584

---
+++ gcc/testsuite/g++.old-deja/g++.pt/mangle2.C
@@ -0,0 +1,40 @@
+// Build don't link:
+// 
+// Copyright (C) 2001 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 25 April 2001 <nathan@codesourcery.com>
+// Origin:pcarlini@unitus.it
+
+// Bug 2559. We hadn't implemented code to mangle numbers bigger than
+// HOST_WIDE_INT.
+
+template<class T, T min_val, T max_val>
+class integer_traits_base
+{
+public:
+static const bool is_integral = true;
+};
+
+template<class T>
+class integer_traits
+{
+public:
+static const bool is_integral = false;
+};
+
+template<>
+class integer_traits<long long>
+: public integer_traits_base<long long, (-9223372036854775807LL - 1),
+9223372036854775807LL>
+{ };
+
+integer_traits<long long> f;
+
+template <class T, T value> T foo () 
+{
+  return value;
+}
+
+void x ()
+{
+  foo<long long, -9223372036854775807LL> ();
+}
