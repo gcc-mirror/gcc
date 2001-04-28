940283950073eb708ba4380e9632b30d1ff80d58
GCC Administrator <gccadmin@gcc.gnu.org>
2001-04-28 07:16:40 +0000

    Bump dates.
    From-SVN: r41654

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-const char *ffe_version_string = "0.5.26 20010427 (experimental)";
+const char *ffe_version_string = "0.5.26 20010428 (experimental)";
--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
 #include "gansidecl.h"
 #include "version.h"
 
-const char *const version_string = "3.0 20010427 (prerelease)";
+const char *const version_string = "3.0 20010428 (prerelease)";
