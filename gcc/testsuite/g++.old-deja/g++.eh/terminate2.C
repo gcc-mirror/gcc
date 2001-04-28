ad49920b70288edcf10b93a0c1598c90cf0b4bdc
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-04-28 05:11:21 +0000

    new
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@41652 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.old-deja/g++.eh/terminate2.C
@@ -0,0 +1,16 @@
+// Test that an unhandled exception causes us to call terminate.
+
+#include <exception>
+#include <cstdlib>
+
+void my_terminate ()
+{
+  std::exit (0);
+}
+
+int main (void)
+{
+  std::set_terminate (my_terminate);
+  throw 1;
+  return 1;
+}
