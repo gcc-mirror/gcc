901d43bcd00712e558ce9d408c788980b6823c13
Richard Henderson <rth@redhat.com>
2001-04-30 16:28:23 -0700

    Revert last change.
    From-SVN: r41708

--- gcc/testsuite/gcc.dg/20000724-1.c
+++ gcc/testsuite/gcc.dg/20000724-1.c
@@ -51,7 +51,7 @@ NT	"ret"
 
 extern inline void do_asm(struct s * x)
 {
-  asm volatile("call ___checkme" : : "c" (x) : "esp", "memory");
+  asm volatile("call ___checkme" : : "c" (x) : "memory");
 }
 
 int foo(void)
