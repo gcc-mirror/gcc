3f35c23689f0dd0384168f590d62e2266b5b8bf1
toon <toon@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-04-30 21:51:57 +0000

    2001-04-30  Toon Moene  <toon@moene.indiv.nluug.nl>
    	* g77.f-torture/execute/20010430.f: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@41695 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g77.f-torture/execute/20010430.f
@@ -0,0 +1,20 @@
+      REAL DAT(2,5)
+      DO I = 1, 5
+         DAT(1,I) = I*1.6356-NINT(I*1.6356)
+         DAT(2,I) = I
+      ENDDO
+      DO I = 1, 4
+         DO J = I+1, 5
+            IF (DAT(1,J) - DAT(1,I) .LT. 0.0) THEN
+               DO K = 1, 2
+                  TMP = DAT(K,I)
+                  DAT(K,I) = DAT(K,J)
+                  DAT(K,J) = TMP
+               ENDDO
+            ENDIF
+         ENDDO
+      ENDDO
+      DO I = 1, 4
+         IF (DAT(1,I) .GT. DAT(1,I+1)) CALL ABORT
+      ENDDO
+      END
