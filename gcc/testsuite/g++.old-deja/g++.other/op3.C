36730e3d6e9d0010abba097eaeae49430df1a164
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-05-07 18:03:52 +0000

    * g++.old-deja/g++.other/op2.C: Don't use `glob' as a variable. 	* g++.old-deja/g++.other/op3.C: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@41901 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.other/op3.C
+++ gcc/testsuite/g++.old-deja/g++.other/op3.C
@@ -1,4 +1,4 @@
-// Copyright (C) 2000 Free Software Foundation, Inc.
+// Copyright (C) 2000, 2001 Free Software Foundation, Inc.
 // Contributed by Nathan Sidwell 28 Nov 2000 <nathan@codesourcery.com>
 
 // Related to bug 91. We'd not preserve constness accessing a member of the
@@ -6,23 +6,23 @@
 
 #include <stdio.h>
 
-int glob = 0;
+int value = 0;
 
 struct A
 {
   A() {}
 
   A( A& arg) 
-  { printf ("%s\n", __PRETTY_FUNCTION__); glob = 1;}
+  { printf ("%s\n", __PRETTY_FUNCTION__); value = 1;}
 
   A( const A& arg)
-  { printf ("%s\n", __PRETTY_FUNCTION__); glob = 2;}
+  { printf ("%s\n", __PRETTY_FUNCTION__); value = 2;}
 
   A& operator=( A& ) 
-  { printf ("%s\n", __PRETTY_FUNCTION__); glob = 3; return *this; }
+  { printf ("%s\n", __PRETTY_FUNCTION__); value = 3; return *this; }
 
   A& operator=( const A& ) 
-  { printf ("%s\n", __PRETTY_FUNCTION__); glob = 4; return *this; }
+  { printf ("%s\n", __PRETTY_FUNCTION__); value = 4; return *this; }
 };
 
 struct B
@@ -33,31 +33,31 @@ struct B
 
 void foo( A& )
 {
-  printf ("%s\n", __PRETTY_FUNCTION__); glob = 5;
+  printf ("%s\n", __PRETTY_FUNCTION__); value = 5;
 }
 
 void foo( const A& )
 {
- printf ("%s\n", __PRETTY_FUNCTION__); glob = 6;
+ printf ("%s\n", __PRETTY_FUNCTION__); value = 6;
 }
 
 int main()
 {
   const A a0;
-  glob = 0; printf ("A(cA) : ");  A a1(a0); if (glob != 2) return 1;
-  glob = 0; printf ("A(A ) : ");  A a2(a1); if (glob != 1) return 2;
+  value = 0; printf ("A(cA) : ");  A a1(a0); if (value != 2) return 1;
+  value = 0; printf ("A(A ) : ");  A a2(a1); if (value != 1) return 2;
   
   const B b0;
-  glob = 0; printf ("B(cB) : ");  B b1(b0); if (glob != 2) return 3;
-  glob = 0; printf ("B(B ) : ");  B b2(b1); if (glob != 2) return 4;
+  value = 0; printf ("B(cB) : ");  B b1(b0); if (value != 2) return 3;
+  value = 0; printf ("B(B ) : ");  B b2(b1); if (value != 2) return 4;
 
-  glob = 0; printf ("A= cA : ");  a1 = a0; if (glob != 4) return 5;
-  glob = 0; printf ("A= A : ");   a1 = a2; if (glob != 3) return 6;
-  glob = 0; printf ("B= cB : ");  b1 = b0; if (glob != 4) return 7;
-  glob = 0; printf ("B= B : ");   b1 = b2; if (glob != 4) return 8;
+  value = 0; printf ("A= cA : ");  a1 = a0; if (value != 4) return 5;
+  value = 0; printf ("A= A : ");   a1 = a2; if (value != 3) return 6;
+  value = 0; printf ("B= cB : ");  b1 = b0; if (value != 4) return 7;
+  value = 0; printf ("B= B : ");   b1 = b2; if (value != 4) return 8;
 
-  glob = 0; printf ("foo(cB): "); foo(b0.a); if (glob != 6) return 9;
-  glob = 0; printf ("foo(B ): "); foo(b2.a); if (glob != 5) return 10;
+  value = 0; printf ("foo(cB): "); foo(b0.a); if (value != 6) return 9;
+  value = 0; printf ("foo(B ): "); foo(b2.a); if (value != 5) return 10;
 
   return 0;
 }
