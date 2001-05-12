90e0c4105558b573e458ab2944ec51f69a9cbffb
Neil Booth <neil@cat.daikokuya.demon.co.uk>
2001-05-12 16:16:23 +0000

    dwarfout.c (dwarfout_init): Add cast.
    	* dwarfout.c (dwarfout_init): Add cast.
    	* sched-deps.c (sched_analyze_insn): Remove unused vars.
    	* unwind-dw2-fde.c (_Unwind_Find_FDE): Flag unused parameter so.
    cp:
    	* method.c (build_mangled_C99_name): Remove unused prototype.
    
    From-SVN: r41996

--- gcc/dwarfout.c
+++ gcc/dwarfout.c
@@ -5806,7 +5806,7 @@ dwarfout_init (asm_out_file, main_input_filename)
   fputc ('\n', asm_out_file);
   ASM_OUTPUT_PUSH_SECTION (asm_out_file, DEBUG_SECTION);
   ASM_OUTPUT_LABEL (asm_out_file, DEBUG_BEGIN_LABEL);
-  output_die (output_compile_unit_die, main_input_filename);
+  output_die (output_compile_unit_die, (PTR) main_input_filename);
   ASM_OUTPUT_POP_SECTION (asm_out_file);
 
   fputc ('\n', asm_out_file);
