2f45f144b810e8c81cc53792123a7c15588fd530
Richard Henderson <rth@redhat.com>
2001-05-12 13:55:36 -0700

    lang.c (flag_new_exceptions): Don't declare it.
            * lang.c (flag_new_exceptions): Don't declare it.
            (java_init_options): Or set it.
    
    From-SVN: r42013

--- gcc/java/lang.c
+++ gcc/java/lang.c
@@ -149,7 +149,6 @@ int flag_force_classes_archive_check;
 
 /* From gcc/flags.h, and indicates if exceptions are turned on or not.  */
 
-extern int flag_new_exceptions;
 extern int flag_exceptions;
 
 /* Table of language-dependent -f options.
@@ -700,7 +699,6 @@ void lang_init_source (level)
 static void
 java_init_options ()
 {
-  flag_new_exceptions = 1;
   flag_bounds_check = 1;
 }
 
