5a3979b8091797c9eae548b107c3dddb4346836d
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-05-21 15:54:33 +0000

    * class.c (build_vtable): Set DECL_ASSEMBLER_NAME for vtables here.         (get_vtable_decl, build_vtt): Not here.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@42392 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/mangle1.C
@@ -0,0 +1,29 @@
+// Test for mangling of simple testcase involving construction vtables.
+
+// { dg-do compile }
+// { dg-options "-fno-inline" }
+
+struct A {
+  virtual void f () { }
+};
+
+struct B: public virtual A { };
+struct C: public B { };
+
+C c;
+
+// { dg-final { scan-assembler mangle1.C "\n_ZN1A1fEv:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZN1AC2Ev:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZN1BC2Ev:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZN1CC1Ev:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZTC1C0_1B:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZTI1A:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZTI1B:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZTI1C:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZTS1A:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZTS1B:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZTS1C:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZTT1C:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZTV1A:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZTV1C:" } }
+// { dg-final { scan-assembler mangle1.C "\n_ZTv0_n12_N1A1fEv:" } }
