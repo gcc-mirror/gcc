719f0fde70cd6182862533567af5e005ca512180
Joseph Myers <jsm28@cam.ac.uk>
2001-05-23 19:10:30 +0100

    jv-scan.c (version): Update copyright year.
    gcc/java:
    	* jv-scan.c (version): Update copyright year.
    
    libjava:
    	* gij.cc (version): Update copyright year.
    
    From-SVN: r42505

--- libjava/gij.cc
+++ libjava/gij.cc
@@ -1,4 +1,4 @@
-/* Copyright (C) 1999, 2000  Free Software Foundation
+/* Copyright (C) 1999, 2000, 2001  Free Software Foundation
 
    This file is part of libgcj.
 
@@ -41,7 +41,7 @@ static void
 version ()
 {
   printf ("gij (GNU libgcj) version %s\n\n", VERSION);
-  printf ("Copyright (C) 1999, 2000 Free Software Foundation.\n");
+  printf ("Copyright (C) 2001 Free Software Foundation.\n");
   printf ("This is free software; see the source for copying conditions.  There is NO\n");
   printf ("warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n");
   exit (0);
