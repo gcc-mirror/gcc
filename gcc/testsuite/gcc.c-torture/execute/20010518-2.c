d60e607e7a23433f3b7ae751dc1f89b34ecb5504
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-05-25 19:38:52 +0000

    2001-05-25  Diego Novillo  <dnovillo@redhat.com>
    	* gcc.c-torture/compile/20010518-2.c: New file.
    	* gcc.c-torture/execute/20010518-1.c: New file.
    	* gcc.c-torture/execute/20010518-2.c: New file.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@42580 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20010518-2.c
@@ -0,0 +1,38 @@
+/* Mis-aligned packed structures.  */
+
+typedef struct
+{
+  char b0;
+  char b1;
+  char b2;
+  char b3;
+  char b4;
+  char b5;
+} __attribute__ ((packed)) b_struct;
+
+
+typedef struct
+{
+  short a;
+  long b;
+  short c;
+  short d;
+  b_struct e;
+} __attribute__ ((packed)) a_struct;
+
+
+int
+main(void)
+{
+  volatile a_struct *a;
+  volatile a_struct b;
+
+  a = &b;
+  *a = (a_struct){1,2,3,4};
+  a->e.b4 = 'c';
+
+  if (a->a != 1 || a->b != 2 || a->c != 3 || a->d != 4 || a->e.b4 != 'c')
+    abort ();
+
+  exit (0);
+}
