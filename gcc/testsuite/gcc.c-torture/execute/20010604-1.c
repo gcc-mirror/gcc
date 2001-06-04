4cbf3711d32f400b665337f12a92b87da106c51a
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-06-04 20:30:45 +0000

    * gcc.c-torture/execute/20010604-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@42883 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20010604-1.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+
+int f (int a, int b, int c, _Bool d, _Bool e, _Bool f, char g)
+{
+  if (g != 1 || d != true || e != true || f != true) abort ();
+  return a + b + c;
+}
+
+int main (void)
+{
+  if (f (1, 2, -3, true, true, true, '\001'))
+    abort ();
+  exit (0);
+}
