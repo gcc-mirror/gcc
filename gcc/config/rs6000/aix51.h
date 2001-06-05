47a8dbf00ecf570921f8c43f15e6aeb4232e541e
David Edelsohn <edelsohn@gnu.org>
2001-06-05 18:09:53 +0000

    aix51.h (CPP_SPEC, [...]): Fix typo in __WCHAR_TYPE__ definition.
    	* rs6000/aix51.h (CPP_SPEC, CPLUSPLUS_CPP_SPEC): Fix typo in
    	__WCHAR_TYPE__ definition.
    	(WCHAR_TYPE_SIZE): #undef before #define.
    
    From-SVN: r42912

--- gcc/config/rs6000/aix51.h
+++ gcc/config/rs6000/aix51.h
@@ -21,7 +21,7 @@ the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
 
-/* AIX V5 and above support 64-bit executables.  */
+/* AIX 4.3 and above support 64-bit executables.  */
 #undef  SUBSUBTARGET_SWITCHES
 #define SUBSUBTARGET_SWITCHES					\
   {"aix64", 		MASK_64BIT | MASK_POWERPC64 | MASK_POWERPC,	\
@@ -103,13 +103,13 @@ do {									\
 -D_AIX -D_AIX32 -D_AIX41 -D_AIX43 -D_AIX51 -Asystem=unix -Asystem=aix"
 
 #undef CPP_SPEC
-#define CPP_SPEC "%{posix: -D_POSIX_SOURCE} \
-  %{ansi: -D_ANSI_C_SOURCE} \
-  %{!maix64: -D__WCHAR_TYPE__="short unsigned int"} \
-  %{maix64: -D__64BIT__ -D_ARCH_PPC -D__WCHAR_TYPE__="unsigned int" \
-    -D__LONG_MAX__=9223372036854775807L} \
-  %{mpe: -I/usr/lpp/ppe.poe/include} \
-  %{pthread: -D_THREAD_SAFE} \
+#define CPP_SPEC "%{posix: -D_POSIX_SOURCE}\
+  %{ansi: -D_ANSI_C_SOURCE}\
+  %{!maix64: -D__WCHAR_TYPE__=short\\ unsigned\\ int}\
+  %{maix64: -D__64BIT__ -D_ARCH_PPC -D__LONG_MAX__=9223372036854775807L \
+    -D__WCHAR_TYPE__=unsigned\\ int}\
+  %{mpe: -I/usr/lpp/ppe.poe/include}\
+  %{pthread: -D_THREAD_SAFE}\
   %(cpp_cpu)"
 
 /* The GNU C++ standard library requires that these macros be 
@@ -120,9 +120,9 @@ do {									\
    -D_XOPEN_SOURCE_EXTENDED=1                   \
    -D_LARGE_FILE_API                            \
    -D_ALL_SOURCE                                \
-   %{!maix64: -D__WCHAR_TYPE__="short unsigned int"} \
-   %{maix64: -D__64BIT__ -D_ARCH_PPC -D__WCHAR_TYPE__="unsigned int" \
-     -D__LONG_MAX__=9223372036854775807L} \
+   %{!maix64: -D__WCHAR_TYPE__=short\\ unsigned\\ int}\
+   %{maix64: -D__64BIT__ -D_ARCH_PPC -D__LONG_MAX__=9223372036854775807L \
+     -D__WCHAR_TYPE__=unsigned\\ int}\
    %{mpe: -I/usr/lpp/ppe.poe/include}\
    %{pthread: -D_THREAD_SAFE}\
    %(cpp_cpu)"
@@ -214,6 +214,7 @@ do {									\
 /* __WCHAR_TYPE__ is dynamic, so do not define it statically.  */
 #define NO_BUILTIN_WCHAR_TYPE
 #undef WCHAR_TYPE
+#undef WCHAR_TYPE_SIZE
 
 /* Width of wchar_t in bits.  */
 #define WCHAR_TYPE_SIZE (!TARGET_64BIT ? 16 : 32)
