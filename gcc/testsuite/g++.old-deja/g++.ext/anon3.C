fcebb2ddda6b7ed8726b6a04348b3f8d99eb580f
Nathan Sidwell <nathan@codesourcery.com>
2001-06-07 16:15:45 +0000

    re PR c++/2914 (gcc 3.0 crashes on xalan DoubleSupport.cpp)
    cp:
    	PR c++/2914
    	* decl.c (pushtag): Don't push into a complete type's scope.
    testsuite:
    	* g++.old-deja/g++.ext/anon3.C: New test.
    
    From-SVN: r42962

---
+++ gcc/testsuite/g++.old-deja/g++.ext/anon3.C
@@ -0,0 +1,36 @@
+// Build don't link:
+
+// Copyright (C) 2001 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 6 Jun 2001 <nathan@codesourcery.com>
+
+// Bug 2914. New types can be created in a static member
+// initializer. These should not be injected into the member's class's
+// scope.
+
+class DoubleSupport
+{
+  public:
+  static void toDouble();
+  
+  static const double s_NaN;
+  static const double s_positiveInfinity;
+  static const double s_negativeInfinity;
+  static const double s_positiveZero;
+  static const double s_negativeZero;
+  static const unsigned long* s_NaNFirstDWORD;
+  static const unsigned long* s_NaNSecondDWORD;
+};
+
+const double DoubleSupport::s_positiveInfinity =
+(__extension__ ((union { unsigned char __c[8]; double __d; })
+  { __c: { 0, 0, 0, 0, 0, 0, 0xf0, 0x7f } }).__d);
+
+struct other 
+{
+};
+
+
+void
+DoubleSupport::toDouble()
+{
+}
