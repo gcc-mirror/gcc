bcde99fabbc69e8390d89705314e6c92c743a55e
Jakub Jelinek <jakub@redhat.com>
2001-06-08 19:09:11 +0200

    tradcpp.c (do_define): Make sure we don't walk past limit.
    	* tradcpp.c (do_define): Make sure we don't walk past limit.
    
    	* gcc.dg/cpp/tr-define.c: New test.
    
    From-SVN: r43023

--- gcc/tradcpp.c
+++ gcc/tradcpp.c
@@ -2760,8 +2760,8 @@ do_define (buf, limit, op)
       }
     }
 
-    ++bp;			/* skip paren */
-    while (is_nvspace (*bp))	/* and leading whitespace */
+    ++bp;					/* skip paren */
+    while (is_nvspace (*bp) && bp < limit)	/* and leading whitespace */
       ++bp;
     /* now everything from bp before limit is the definition. */
     defn = collect_expansion (bp, limit, argno, arg_ptrs);
@@ -2788,7 +2788,7 @@ do_define (buf, limit, op)
     }
   } else {
     /* simple expansion or empty definition; skip leading whitespace */
-    while (is_nvspace (*bp))
+    while (is_nvspace (*bp) && bp < limit)
       ++bp;
     /* now everything from bp before limit is the definition. */
     defn = collect_expansion (bp, limit, -1, 0);
