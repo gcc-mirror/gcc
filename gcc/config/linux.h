281e78ae1c0c6cb25149f08e21d15ff5556f0271
Richard Henderson <rth@redhat.com>
2001-06-11 12:51:02 -0700

    linux.h (CPLUSPLUS_CPP_SPEC): Use %(cpp) not $(cpp_cpu).
            * config/linux.h (CPLUSPLUS_CPP_SPEC): Use %(cpp) not $(cpp_cpu).
            * config/alpha/linux.h (CPLUSPLUS_CPP_SPEC): Likewise.
    
    From-SVN: r43210

--- gcc/config/linux.h
+++ gcc/config/linux.h
@@ -75,12 +75,9 @@ Boston, MA 02111-1307, USA.  */
 #define CC1_SPEC "%{profile:-p}"
 #endif
 
-/* The GNU C++ standard library requires that these macros be
-   defined.  */
+/* The GNU C++ standard library requires that these macros be defined.  */
 #undef CPLUSPLUS_CPP_SPEC
-#define CPLUSPLUS_CPP_SPEC                      \
-  "-D_GNU_SOURCE                                \
-   %(cpp_cpu)"
+#define CPLUSPLUS_CPP_SPEC "-D_GNU_SOURCE %(cpp)"
 
 #ifndef USE_GNULIBC_1
 #undef DEFAULT_VTABLE_THUNKS
