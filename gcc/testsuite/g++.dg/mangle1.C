eaea542ac23577f776afa97cdc13d1901d0db7c2
John David Anglin <dave@hiauly1.hia.nrc.ca>
2001-06-16 18:30:32 +0000

    mangle1.C: Modify regexp to test for `[: \t\n]' at end of label name.
    	* g++.dg/mangle1.C: Modify regexp to test for `[: \t\n]' at end
    	of label name.
    
    From-SVN: r43422

--- gcc/testsuite/g++.dg/mangle1.C
+++ gcc/testsuite/g++.dg/mangle1.C
@@ -12,17 +12,17 @@ struct C: public B { };
 
 C c;
 
-// { dg-final { scan-assembler mangle1.C "\n_?_ZN1A1fEv:" } }
-// { dg-final { scan-assembler mangle1.C "\n_?_ZN1AC2Ev:" } }
-// { dg-final { scan-assembler mangle1.C "\n_?_ZN1BC2Ev:" } }
-// { dg-final { scan-assembler mangle1.C "\n_?_ZN1CC1Ev:" } }
-// { dg-final { scan-assembler mangle1.C "\n_?_ZTC1C0_1B:" } }
-// { dg-final { scan-assembler mangle1.C "\n_?_ZTI1A:" } }
-// { dg-final { scan-assembler mangle1.C "\n_?_ZTI1B:" } }
-// { dg-final { scan-assembler mangle1.C "\n_?_ZTI1C:" } }
-// { dg-final { scan-assembler mangle1.C "\n_?_ZTS1A:" } }
-// { dg-final { scan-assembler mangle1.C "\n_?_ZTS1B:" } }
-// { dg-final { scan-assembler mangle1.C "\n_?_ZTS1C:" } }
-// { dg-final { scan-assembler mangle1.C "\n_?_ZTT1C:" } }
-// { dg-final { scan-assembler mangle1.C "\n_?_ZTV1A:" } }
-// { dg-final { scan-assembler mangle1.C "\n_?_ZTV1C:" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZN1A1fEv\[: \t\n\]" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZN1AC2Ev\[: \t\n\]" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZN1BC2Ev\[: \t\n\]" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZN1CC1Ev\[: \t\n\]" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZTC1C0_1B\[: \t\n\]" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZTI1A\[: \t\n\]" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZTI1B\[: \t\n\]" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZTI1C\[: \t\n\]" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZTS1A\[: \t\n\]" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZTS1B\[: \t\n\]" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZTS1C\[: \t\n\]" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZTT1C\[: \t\n\]" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZTV1A\[: \t\n\]" } }
+// { dg-final { scan-assembler mangle1.C "\n_?_ZTV1C\[: \t\n\]" } }
