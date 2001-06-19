414d0697b2172cea72711c611f26929e8e4f1c38
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-06-19 01:51:33 +0000

    2001-06-18  Benjamin Kosnik  <bkoz@redhat.com>
    	* include/bits/stl_raw_storage_iter.h: Format. Correct derivation.
    	* testsuite/20_util/raw_storage_iterator.cc: Same.
    
            * include/bits/stl_alloc.h (_S_chunk_alloc): Change malloc to
            operator new.
            (__mem_interface): New typedef for switching between malloc and new.
    	* testsuite/20_util/allocator_members.cc: New file.
    
    	* testsuite/20_util/comparisons.cc: New file.
    	* testsuite/20_util/pairs.cc: New file.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@43451 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/stl_raw_storage_iter.h
+++ libstdc++-v3/include/bits/stl_raw_storage_iter.h
@@ -33,39 +33,45 @@
 
 namespace std
 {
+  template <class _ForwardIterator, class _Tp>
+  class raw_storage_iterator 
+    : public iterator<output_iterator_tag, void, void, void, void>
+    {
+    protected:
+      _ForwardIterator _M_iter;
 
-template <class _ForwardIterator, class _Tp>
-class raw_storage_iterator {
-protected:
-  _ForwardIterator _M_iter;
-public:
-  typedef output_iterator_tag iterator_category;
-  typedef void                value_type;
-  typedef void                difference_type;
-  typedef void                pointer;
-  typedef void                reference;
+    public:
+      explicit 
+      raw_storage_iterator(_ForwardIterator __x) : _M_iter(__x) {}
 
-  explicit raw_storage_iterator(_ForwardIterator __x) : _M_iter(__x) {}
-  raw_storage_iterator& operator*() { return *this; }
-  raw_storage_iterator& operator=(const _Tp& __element) {
-    construct(&*_M_iter, __element);
-    return *this;
-  }        
-  raw_storage_iterator<_ForwardIterator, _Tp>& operator++() {
-    ++_M_iter;
-    return *this;
-  }
-  raw_storage_iterator<_ForwardIterator, _Tp> operator++(int) {
-    raw_storage_iterator<_ForwardIterator, _Tp> __tmp = *this;
-    ++_M_iter;
-    return __tmp;
-  }
-};
+      raw_storage_iterator& 
+      operator*() { return *this; }
 
+      raw_storage_iterator& 
+      operator=(const _Tp& __element) 
+      {
+	construct(&*_M_iter, __element);
+	return *this;
+      }        
 
+      raw_storage_iterator<_ForwardIterator, _Tp>& 
+      operator++() 
+      {
+	++_M_iter;
+	return *this;
+      }
+
+      raw_storage_iterator<_ForwardIterator, _Tp> 
+      operator++(int) 
+      {
+	raw_storage_iterator<_ForwardIterator, _Tp> __tmp = *this;
+	++_M_iter;
+	return __tmp;
+      }
+    };
 } // namespace std
 
-#endif /* _CPP_BITS_STL_RAW_STORAGE_ITERATOR_H */
+#endif
 
 // Local Variables:
 // mode:C++
