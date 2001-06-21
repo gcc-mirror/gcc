111b096e564f3c600825fc7232d922eddfd64955
pme <pme@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-06-21 23:03:00 +0000

    2001-06-21  Phil Edwards  <pme@sources.redhat.com>
    	* include/backward/algo.h:  Add "GPL plus runtime exception" comment.
    	* include/backward/algobase.h:  Likewise.
    	* include/backward/alloc.h:  Likewise.
    	* include/backward/bvector.h:  Likewise.
    	* include/backward/defalloc.h:  Likewise.
    	* include/backward/deque.h:  Likewise.
    	* include/backward/function.h:  Likewise.
    	* include/backward/hash_map.h:  Likewise.
    	* include/backward/hash_set.h:  Likewise.
    	* include/backward/hashtable.h:  Likewise.
    	* include/backward/heap.h:  Likewise.
    	* include/backward/iterator.h:  Likewise.
    	* include/backward/list.h:  Likewise.
    	* include/backward/map.h:  Likewise.
    	* include/backward/multimap.h:  Likewise.
    	* include/backward/multiset.h:  Likewise.
    	* include/backward/pair.h:  Likewise.
    	* include/backward/rope.h:  Likewise.
    	* include/backward/set.h:  Likewise.
    	* include/backward/slist.h:  Likewise.
    	* include/backward/stack.h:  Likewise.
    	* include/backward/strstream:  Likewise.
    	* include/backward/tempbuf.h:  Likewise.
    	* include/backward/tree.h:  Likewise.
    	* include/backward/vector.h:  Likewise.
    	* include/bits/pthread_allocimpl.h:  Likewise.
    	* include/bits/std_algorithm.h:  Likewise.
    	* include/bits/std_bitset.h:  Likewise.
    	* include/bits/std_deque.h:  Likewise.
    	* include/bits/std_functional.h:  Likewise.
    	* include/bits/std_iterator.h:  Likewise.
    	* include/bits/std_list.h:  Likewise.
    	* include/bits/std_map.h:  Likewise.
    	* include/bits/std_memory.h:  Likewise.
    	* include/bits/std_numeric.h:  Likewise.
    	* include/bits/std_queue.h:  Likewise.
    	* include/bits/std_set.h:  Likewise.
    	* include/bits/std_stack.h:  Likewise.
    	* include/bits/std_utility.h:  Likewise.
    	* include/bits/std_vector.h:  Likewise.
    	* include/bits/stl_algo.h:  Likewise.
    	* include/bits/stl_algobase.h:  Likewise.
    	* include/bits/stl_alloc.h:  Likewise.
    	* include/bits/stl_bvector.h:  Likewise.
    	* include/bits/stl_construct.h:  Likewise.
    	* include/bits/stl_deque.h:  Likewise.
    	* include/bits/stl_function.h:  Likewise.
    	* include/bits/stl_heap.h:  Likewise.
    	* include/bits/stl_iterator.h:  Likewise.
    	* include/bits/stl_iterator_base_funcs.h:  Likewise.
    	* include/bits/stl_iterator_base_types.h:  Likewise.
    	* include/bits/stl_list.h:  Likewise.
    	* include/bits/stl_map.h:  Likewise.
    	* include/bits/stl_multimap.h:  Likewise.
    	* include/bits/stl_multiset.h:  Likewise.
    	* include/bits/stl_numeric.h:  Likewise.
    	* include/bits/stl_pair.h:  Likewise.
    	* include/bits/stl_pthread_alloc.h:  Likewise.
    	* include/bits/stl_queue.h:  Likewise.
    	* include/bits/stl_raw_storage_iter.h:  Likewise.
    	* include/bits/stl_relops.h:  Likewise.
    	* include/bits/stl_set.h:  Likewise.
    	* include/bits/stl_stack.h:  Likewise.
    	* include/bits/stl_tempbuf.h:  Likewise.
    	* include/bits/stl_threads.h:  Likewise.
    	* include/bits/stl_tree.h:  Likewise.
    	* include/bits/stl_uninitialized.h:  Likewise.
    	* include/bits/stl_vector.h:  Likewise.
    	* include/bits/type_traits.h:  Likewise.
    	* include/ext/hash_map:  Likewise.
    	* include/ext/hash_set:  Likewise.
    	* include/ext/rope:  Likewise.
    	* include/ext/ropeimpl.h:  Likewise.
    	* include/ext/slist:  Likewise.
    	* include/ext/stl_hash_fun.h:  Likewise.
    	* include/ext/stl_hashtable.h:  Likewise.
    	* include/ext/stl_rope.h:  Likewise.
    	* libsupc++/unwind-cxx.h:  Likewise.
    	* src/bitset.cc:  Likewise.
    	* src/strstream.cc:  Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@43496 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/libsupc++/unwind-cxx.h
+++ libstdc++-v3/libsupc++/unwind-cxx.h
@@ -18,6 +18,15 @@
 // the Free Software Foundation, 59 Temple Place - Suite 330,
 // Boston, MA 02111-1307, USA.
 
+// As a special exception, you may use this file as part of a free software
+// library without restriction.  Specifically, if other files instantiate
+// templates or use macros or inline functions from this file, or you compile
+// this file and link it with other files to produce an executable, this
+// file does not by itself cause the resulting executable to be covered by
+// the GNU General Public License.  This exception does not however
+// invalidate any other reasons why the executable file might be covered by
+// the GNU General Public License.
+
 // This is derived from the C++ ABI for IA-64.  Where we diverge
 // for cross-architecture compatibility are noted with "@@@".
 
