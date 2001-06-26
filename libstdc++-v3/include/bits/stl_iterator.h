9483461dc9bd390cfbc4053435e3934ae39f46c6
Gabriel Dos Reis <gdr@merlin.codesourcery.com>
2001-06-26 06:36:33 +0000

    stl_iterator.h (__normal_iterator<>): Qualify dependent names with `typename'.
    	* include/bits/stl_iterator.h (__normal_iterator<>): Qualify
    	dependent names with `typename'.  Bring in various required
    	iterator_traits members.  Can't imagine why that used to work.
    
    From-SVN: r43568

--- libstdc++-v3/include/bits/stl_iterator.h
+++ libstdc++-v3/include/bits/stl_iterator.h
@@ -443,18 +443,24 @@ private:
 // _Iterator parameter is the same.
 template<typename _Iterator, typename _Container>
 class __normal_iterator
-  : public iterator<iterator_traits<_Iterator>::iterator_category,
-                    iterator_traits<_Iterator>::value_type,
-                    iterator_traits<_Iterator>::difference_type,
-                    iterator_traits<_Iterator>::pointer,
-                    iterator_traits<_Iterator>::reference>
+  : public iterator<typename iterator_traits<_Iterator>::iterator_category,
+                    typename iterator_traits<_Iterator>::value_type,
+                    typename iterator_traits<_Iterator>::difference_type,
+                    typename iterator_traits<_Iterator>::pointer,
+                    typename iterator_traits<_Iterator>::reference>
 {
-
+  typedef iterator_traits<_Iterator> _Traits;
+  
 protected:
   _Iterator _M_current;
 
 public:
   typedef __normal_iterator<_Iterator, _Container> normal_iterator_type;
+  typedef typename _Traits::iterator_category iterator_category;
+  typedef typename _Traits::value_type value_type;
+  typedef typename _Traits::difference_type difference_type;
+  typedef typename _Traits::pointer pointer;
+  typedef typename _Traits::reference reference;
 
   __normal_iterator() : _M_current(_Iterator()) { }
 
