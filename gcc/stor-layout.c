40f54c9570c09a1647935e5d3b39fb539addf4b8
kenner <kenner@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-07-02 10:27:17 +0000

    * stor-layout.c (layout_decl): Revert change to handling of alignment 	in packed types.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@43686 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/stor-layout.c
+++ gcc/stor-layout.c
@@ -381,7 +381,7 @@ layout_decl (decl, known_align)
       DECL_BIT_FIELD_TYPE (decl) = DECL_BIT_FIELD (decl) ? type : 0;
       if (maximum_field_alignment != 0)
 	DECL_ALIGN (decl) = MIN (DECL_ALIGN (decl), maximum_field_alignment);
-      else if (DECL_PACKED (decl) && known_align % DECL_ALIGN (decl) != 0)
+      else if (DECL_PACKED (decl))
 	{
 	  DECL_ALIGN (decl) = MIN (DECL_ALIGN (decl), BITS_PER_UNIT);
 	  DECL_USER_ALIGN (decl) = 0;
