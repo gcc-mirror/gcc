4c1de8c08ffca7852891e08667aacec6d9b6bc73
jsturm <jsturm@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-07-03 23:52:27 +0000

    * libjava.lang/II.java: New file. * libjava.lang/II.out: New file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@43744 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/testsuite/libjava.lang/II.java
@@ -0,0 +1,16 @@
+public class II {
+  {
+    System.out.println("ii");
+  }
+  public II()
+  {
+  }
+  public II(int x)
+  {
+    this();
+  }
+  public static void main(String[] args)
+  {
+    new II(0);
+  }
+}
