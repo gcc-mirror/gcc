b94b03fb700c24f4d2c913aaa644f9ab74da655f
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-07-06 22:39:17 +0000

    2001-07-06  Diego Novillo  <dnovillo@redhat.com>
    	* combine.c (combine_simplify_rtx): Also recompute 'mode' if the
    	call to simplify_binary_operation returns a new pattern.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@43826 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/combine.c
+++ gcc/combine.c
@@ -3702,7 +3702,12 @@ combine_simplify_rtx (x, op0_mode, last, in_dest)
     }
 
   if (temp)
-    x = temp, code = GET_CODE (temp), op0_mode = VOIDmode;
+    {
+      x = temp;
+      code = GET_CODE (temp);
+      op0_mode = VOIDmode;
+      mode = GET_MODE (temp);
+    }
 
   /* First see if we can apply the inverse distributive law.  */
   if (code == PLUS || code == MINUS
