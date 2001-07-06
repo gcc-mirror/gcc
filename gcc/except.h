00bf78bcf900da6b848593f480ab89d84deeb417
Richard Henderson <rth@redhat.com>
2001-07-06 16:17:41 -0700

    except.h (MUST_USE_SJLJ_EXCEPTIONS): Examine the value of DWARF2_UNWIND_INFO not just whether it is defined.
            * except.h (MUST_USE_SJLJ_EXCEPTIONS): Examine the value of
            DWARF2_UNWIND_INFO not just whether it is defined.
    
    From-SVN: r43830

--- gcc/except.h
+++ gcc/except.h
@@ -180,7 +180,7 @@ extern tree (*lang_eh_runtime_type) PARAMS ((tree));
 
 #if ! (defined (EH_RETURN_DATA_REGNO)			\
        && (defined (IA64_UNWIND_INFO)			\
-	   || (defined (DWARF2_UNWIND_INFO)		\
+	   || (DWARF2_UNWIND_INFO			\
 	       && defined (EH_RETURN_STACKADJ_RTX)	\
 	       && (defined (EH_RETURN_HANDLER_RTX)	\
 		   || defined (HAVE_eh_return)))))
