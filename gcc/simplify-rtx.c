3c25a678eadcad18714d0bf3eccb843725582602
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-07-11 09:41:34 +0000

    * simplify-rtx.c (simplify_gen_subreg): Return null for QUEUED rtxes.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@43933 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/simplify-rtx.c
+++ gcc/simplify-rtx.c
@@ -2446,6 +2446,9 @@ simplify_gen_subreg (outermode, op, innermode, byte)
       || byte >= GET_MODE_SIZE (innermode))
     abort ();
 
+  if (GET_CODE (op) == QUEUED)
+    return NULL_RTX;
+
   new = simplify_subreg (outermode, op, innermode, byte);
   if (new)
     return new;
