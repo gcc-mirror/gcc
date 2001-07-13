abe2b927912c1000b9d89eca2bb3a0738bfba79b
Tom Tromey <tromey@redhat.com>
2001-07-13 23:12:35 +0000

    jvspec.c (jvgenmain_spec): Remove -ffilelist-file from cc1 invocation.
    	* jvspec.c (jvgenmain_spec): Remove -ffilelist-file from cc1
    	invocation.
    
    From-SVN: r43999

--- gcc/java/jvspec.c
+++ gcc/java/jvspec.c
@@ -63,6 +63,7 @@ const char jvgenmain_spec[] =
 		   %{<fuse-boehm-gc} %{<fhash-synchronization} %{<fjni}\
 		   %{<fclasspath*} %{<fCLASSPATH*} %{<foutput-class-dir}\
 		   %{<fuse-divide-subroutine} %{<fno-use-divide-subroutine}\
+		   %{<ffilelist-file}\
 		   %{f*} -fdollars-in-identifiers\
 		   %{aux-info*}\
 		   %{pg:%{fomit-frame-pointer:%e-pg and -fomit-frame-pointer are incompatible}}\
