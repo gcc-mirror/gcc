3461a998f83f6aba603802fcc1031a5898e9956f
korbb <korbb@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-07-17 03:59:18 +0000

    update the sample output
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@44060 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fixinc/tests/base/math.h
+++ gcc/fixinc/tests/base/math.h
@@ -35,11 +35,19 @@ extern double floor(), ceil(), fmod(), fabs _PARAMS((double));
 #endif  /* FIX_HEADER_BREAKAGE_CHECK */
 
 
+#if defined( HPUX10_CPP_POW_INLINE_CHECK )
+
+#endif  /* HPUX10_CPP_POW_INLINE_CHECK */
+
+
+#if defined( HPUX11_CPP_POW_INLINE_CHECK )
+
+#endif  /* HPUX11_CPP_POW_INLINE_CHECK */
+
+
 #if defined( HPUX11_FABSF_CHECK )
 #ifdef _PA_RISC
-#ifndef __cplusplus
 #  define fabsf(x) ((float)fabs((double)(float)(x)))
-#endif
 #endif
 #endif  /* HPUX11_FABSF_CHECK */
 
