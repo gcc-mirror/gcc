e109d16f8c56fa61f9b4c15a2426ad07ac88cbd6
Tom Tromey <tromey@redhat.com>
2001-07-23 20:01:29 +0000

    javaprims.h: Rebuilt class list.
    	* gcj/javaprims.h: Rebuilt class list.
    	* Makefile.in: Rebuilt.
    	* Makefile.am (core_java_source_files): Added VMClassLoader.
    	* java/lang/VMClassLoader.java: New file.
    	* java/lang/Boolean.java: Merged with Classpath.
    	* java/lang/Byte.java: Merged with Classpath.
    	* java/lang/Integer.java: Merged with Classpath.
    	* java/lang/Long.java: Merged with Classpath.
    	* java/lang/Number.java: Merged with Classpath.
    	* java/lang/Short.java: Merged with Classpath.
    
    From-SVN: r44274

--- libjava/java/lang/Byte.java
+++ libjava/java/lang/Byte.java
@@ -1,88 +1,193 @@
-/* Copyright (C) 1998, 1999, 2000, 2001  Free Software Foundation
+/* Byte.java -- object wrapper for byte
+   Copyright (C) 1998, 2001 Free Software Foundation, Inc.
 
-   This file is part of libgcj.
+This file is part of GNU Classpath.
 
-This software is copyrighted work licensed under the terms of the
-Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
-details.  */
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+As a special exception, if you link this library with other files to
+produce an executable, this library does not by itself cause the
+resulting executable to be covered by the GNU General Public License.
+This exception does not however invalidate any other reasons why the
+executable file might be covered by the GNU General Public License. */
+
 
 package java.lang;
 
 /**
- * @author Per Bothner <bothner@cygnus.com>
- * @date April 17, 1998.  
+ * Instances of class <code>Byte</code> represent primitive <code>byte</code>
+ * values.
+ *
+ * Additionally, this class provides various helper functions and variables
+ * useful to bytes.
+ *
+ * @author Paul Fisher
+ * @author John Keiser
+ * @author Per Bothner
+ * @since JDK 1.0
  */
-/* Written using "Java Class Libraries", 2nd edition, plus online
- * API docs for JDK 1.2 beta from http://www.javasoft.com.
- * Status:  Believed complete and correct.
- *	    Includes JDK 1.2 methods.
- */
-
-public final class Byte extends Number implements Comparable
+public final class Byte extends Number implements Comparable 
 {
-  byte value;
+  static final long serialVersionUID = -7183698231559129828L;
 
-  public final static byte MIN_VALUE = -128;
-  public final static byte MAX_VALUE = 127;
+  /**
+   * The minimum value a <code>byte</code> can represent is -128.
+   */
+  public static final byte MIN_VALUE = -128;
 
-  // This initialization is seemingly circular, but it is accepted
-  // by javac, and is handled specially by gcc.
-  public static final Class TYPE = byte.class;
+  /**
+   * The maximum value a <code>byte</code> can represent is 127.
+   */
+  public static final byte MAX_VALUE = 127;
 
-  private static final long serialVersionUID = -7183698231559129828L;
+  /**
+   * The primitive type <code>byte</code> is represented by this 
+   * <code>Class</code> object.
+   */
+  public static final Class TYPE = VMClassLoader.getPrimitiveClass("byte");
 
-  public Byte(byte value)
+  /**
+   * The immutable value of this Byte.
+   */
+  private final byte value;
+
+  /**
+   * Create a <code>Byte</code> object representing the value of the 
+   * <code>byte</code> argument.
+   *
+   * @param value the value to use
+   */     
+  public Byte(byte value) 
   {
     this.value = value;
   }
 
-  public Byte(String str) 
-    throws NumberFormatException
+  /**
+   * Create a <code>Byte</code> object representing the value specified 
+   * by the <code>String</code> argument.
+   *
+   * @param s the string to convert.
+   */
+  public Byte(String s) throws NumberFormatException 
   {
-    this.value = parseByte(str, 10);
+    value = parseByte(s, 10);
   }
 
-  public byte byteValue()
-  {
-    return value;
-  }
-
-  public short shortValue()
-  {
-    return value;
-  }
-
-  public int intValue()
-  {
-    return value;
-  }
-
-  public long longValue ()
+  /**
+   * Return a hashcode representing this Object.
+   *
+   * <code>Byte</code>'s hash code is calculated by simply returning its
+   * value.
+   *
+   * @return this Object's hash code.
+   */
+  public int hashCode() 
   {
     return value;
   }
 
-  public float floatValue ()
+  /**
+   * Returns <code>true</code> if <code>obj</code> is an instance of
+   * <code>Byte</code> and represents the same byte value.
+   * @return whether these Objects are semantically equal.
+   */    
+  public boolean equals(Object obj) 
   {
-    return (float) value;
+    return ((obj instanceof Byte) && (value == ((Byte)obj).byteValue()));
   }
 
-  public double doubleValue ()
+  /**
+   * Converts the <code>byte</code> to a <code>String</code> and assumes
+   * a radix of 10.
+   * @param i the <code>byte</code> to convert to <code>String</code>
+   * @return the <code>String</code> representation of the argument.
+   */    
+  public static String toString(byte i) 
   {
-    return (double) value;
+    return Integer.toString ((int) i);
   }
 
-  public static Byte decode(String str)
-    throws NumberFormatException
+  /**
+   * Converts the <code>Byte</code> value to a <code>String</code> and
+   * assumes a radix of 10.
+   * @return the <code>String</code> representation of this <code>Byte</code>.
+   */    
+  public String toString() 
   {
-    int i = (Integer.decode(str)).intValue();
-    if (i < MIN_VALUE || i > MAX_VALUE)
-      throw new NumberFormatException();
-    return new Byte((byte) i);
+    return Integer.toString ((int) value);
+  }
+    
+  /**
+   * Creates a new <code>Byte</code> object using the <code>String</code>,
+   * assuming a radix of 10.
+   * @param s the <code>String</code> to convert.
+   * @return the new <code>Byte</code>.
+   * @see #Byte(java.lang.String)
+   * @see #parseByte(java.lang.String)
+   * @exception NumberFormatException thrown if the <code>String</code> 
+   * cannot be parsed as a <code>byte</code>.
+   */
+  public static Byte valueOf(String s) throws NumberFormatException 
+  {
+    return new Byte(parseByte(s));
   }
 
-  public static byte parseByte(String str, int radix)
-    throws NumberFormatException
+  /**
+   * Creates a new <code>Byte</code> object using the <code>String</code>
+   * and specified radix (base).
+   * @param s the <code>String</code> to convert.
+   * @param radix the radix (base) to convert with.
+   * @return the new <code>Byte</code>.
+   * @see #parseByte(java.lang.String,int)
+   * @exception NumberFormatException thrown if the <code>String</code> 
+   * cannot be parsed as a <code>byte</code>.
+   */
+  public static Byte valueOf(String s, int radix) 
+    throws NumberFormatException 
+  {
+    return new Byte(parseByte(s, radix));
+  }
+
+  /**
+   * Converts the specified <code>String</code> into a <code>byte</code>.
+   * This function assumes a radix of 10.
+   *
+   * @param s the <code>String</code> to convert
+   * @return the <code>byte</code> value of the <code>String</code>
+   *         argument.
+   * @exception NumberFormatException thrown if the <code>String</code> 
+   * cannot be parsed as a <code>byte</code>.
+   */
+  public static byte parseByte(String s) throws NumberFormatException 
+  {
+    return parseByte(s, 10);
+  }
+
+  /**
+   * Converts the specified <code>String</code> into a <code>byte</code>
+   * using the specified radix (base).
+   *
+   * @param str the <code>String</code> to convert
+   * @param radix the radix (base) to use in the conversion
+   * @return the <code>String</code> argument converted to </code>byte</code>.
+   * @exception NumberFormatException thrown if the <code>String</code> 
+   * cannot be parsed as a <code>byte</code>.    
+   */
+  public static byte parseByte(String str, int radix) 
+    throws NumberFormatException 
   {
     int i = Integer.parseInt(str, radix);
     if (i < MIN_VALUE || i > MAX_VALUE)
@@ -90,55 +195,109 @@ public final class Byte extends Number implements Comparable
     return (byte) i;
   }
 
-  public static byte parseByte(String str)
-    throws NumberFormatException
+  /**
+   * Convert the specified <code>String</code> into a <code>Byte</code>.
+   * The <code>String</code> may represent decimal, hexadecimal, or 
+   * octal numbers.
+   *
+   * The <code>String</code> argument is interpreted based on the leading
+   * characters.  Depending on what the String begins with, the base will be
+   * interpreted differently:
+   *
+   * <table>
+   * <tr><th>Leading<br>Characters</th><th>Base</th></tr>
+   * <tr><td>#</td><td>16</td></tr>
+   * <tr><td>0x</td><td>16</td></tr>
+   * <tr><td>0X</td><td>16</td></tr>
+   * <tr><td>0</td><td>8</td></tr>
+   * <tr><td>Anything<br>Else</td><td>10</td></tr>
+   * </table>
+   *
+   * @param str the <code>String</code> to interpret.
+   * @return the value of the String as a <code>Byte</code>.
+   * @exception NumberFormatException thrown if the <code>String</code> 
+   * cannot be parsed as a <code>byte</code>.    
+   */
+  public static Byte decode(String str) throws NumberFormatException 
   {
-    return parseByte(str, 10);
+    int i = (Integer.decode(str)).intValue();
+    if (i < MIN_VALUE || i > MAX_VALUE)
+      throw new NumberFormatException();
+    return new Byte((byte) i);
   }
-
-  public static Byte valueOf(String str, int radix)
-    throws NumberFormatException
-  {
-    return new Byte(parseByte(str, radix));
-  }
-
-  public static Byte valueOf(String str)
-    throws NumberFormatException
-  {
-    return valueOf(str, 10);
-  }
-
-  // Added in JDK 1.2
-  public int compareTo(Byte anotherByte)
-  {
-    return this.value - anotherByte.value;
-  }
-
-  // Added in JDK 1.2
-  /** @throws ClassCastException */
-  public int compareTo(Object o)
-  {
-    return this.value - ((Byte) o).value;
-  }
-
-  public boolean equals(Object obj)
-  {
-    return (obj instanceof Byte) && ((Byte)obj).value == value;
-  }
-
-  // Verified that hashCode is returns plain value (see Boolean_1 test).
-  public int hashCode()
+    
+  /** Return the value of this <code>Byte</code> as an <code>short</code>.
+   ** @return the value of this <code>Byte</code> as an <code>short</code>.
+   **/
+  public byte byteValue()
   {
     return value;
   }
 
-  public String toString()
+  /** Return the value of this <code>Byte</code> as an <code>short</code>.
+   ** @return the value of this <code>Byte</code> as an <code>short</code>.
+   **/
+  public short shortValue()
   {
-    return Integer.toString((int) value);
+    return value;
   }
 
-  public static String toString(byte value)
+  /** Return the value of this <code>Byte</code> as an <code>int</code>.
+   ** @return the value of this <code>Byte</code> as an <code>int</code>.
+   **/
+  public int intValue()
   {
-    return Integer.toString((int) value);
+    return value;
+  }
+
+  /** Return the value of this <code>Byte</code> as a <code>long</code>.
+   ** @return the value of this <code>Byte</code> as a <code>long</code>.
+   **/
+  public long longValue()
+  {
+    return value;
+  }
+
+  /** Return the value of this <code>Byte</code> as a <code>float</code>.
+   ** @return the value of this <code>Byte</code> as a <code>float</code>.
+   **/
+  public float floatValue()
+  {
+    return value;
+  }
+
+  /** Return the value of this <code>Byte</code> as a <code>double</code>.
+   ** @return the value of this <code>Byte</code> as a <code>double</code>.
+   **/
+  public double doubleValue()
+  {
+    return value;
+  }
+    
+  /**
+   * Compare two Bytes numerically by comparing their
+   * <code>byte</code> values.
+   * @return a positive value if this <code>Byte</code> is greater
+   * in value than the argument <code>Byte</code>; a negative value
+   * if this <code>Byte</code> is smaller in value than the argument
+   * <code>Byte</code>; and <code>0</code>, zero, if this
+   * <code>Byte</code> is equal in value to the argument
+   * <code>Byte</code>.  
+   */
+  public int compareTo(Byte b)
+  {
+    return (int)(value - b.byteValue());
+  }
+    
+  /**
+   * Behaves like <code>compareTo(java.lang.Byte)</code> unless the Object
+   * is not a <code>Byte</code>.  Then it throws a 
+   * <code>ClassCastException</code>.
+   * @exception ClassCastException if the argument is not a
+   * <code>Byte</code>.  
+   */
+  public int compareTo(Object o)
+  {
+    return compareTo((Byte)o);
   }
 }
--- libjava/java/lang/Short.java
+++ libjava/java/lang/Short.java
@@ -1,144 +1,307 @@
-/* Copyright (C) 1998, 1999, 2000, 2001  Free Software Foundation
+/* java.lang.Short
+   Copyright (C) 1998, 2001 Free Software Foundation, Inc.
 
-   This file is part of libgcj.
+This file is part of GNU Classpath.
 
-This software is copyrighted work licensed under the terms of the
-Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
-details.  */
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+As a special exception, if you link this library with other files to
+produce an executable, this library does not by itself cause the
+resulting executable to be covered by the GNU General Public License.
+This exception does not however invalidate any other reasons why the
+executable file might be covered by the GNU General Public License. */
+
 
 package java.lang;
 
 /**
- * @author Per Bothner <bothner@cygnus.com>
- * @date April 17, 1998.  
+ * Instances of class <code>Short</code> represent primitive
+ * <code>short</code> values.
+ *
+ * Additionally, this class provides various helper functions and variables
+ * related to shorts.
+ *
+ * @author Paul Fisher
+ * @author John Keiser
+ * @since JDK 1.0
  */
-/* Written using "Java Class Libraries", 2nd edition, plus online
- * API docs for JDK 1.2 beta from http://www.javasoft.com.
- * Status:  Believed complete and correct.
- *	    Includes JDK 1.2 methods.
- */
-
 public final class Short extends Number implements Comparable
 {
-  short value;
+  static final long serialVersionUID = 7515723908773894738L;
 
-  public final static short MIN_VALUE = -32768;
-  public final static short MAX_VALUE = 32767;
+  /**
+   * The minimum value a <code>short</code> can represent is -32768.
+   */
+  public static final short MIN_VALUE = -32768;
 
-  // This initialization is seemingly circular, but it is accepted
-  // by javac, and is handled specially by gcc.
-  public static final Class TYPE = short.class;
+  /**
+   * The minimum value a <code>short</code> can represent is 32767.
+   */
+  public static final short MAX_VALUE =  32767;
 
-  private static final long serialVersionUID = 7515723908773894738L;
+  /**
+   * The primitive type <code>short</code> is represented by this 
+   * <code>Class</code> object.
+   */
+  public static final Class TYPE = VMClassLoader.getPrimitiveClass("short");
 
+  /**
+   * The immutable value of this Short.
+   */
+  private final short value;
+
+  /**
+   * Create a <code>Short</code> object representing the value of the 
+   * <code>short</code> argument.
+   *
+   * @param value the value to use
+   */
   public Short(short value)
   {
     this.value = value;
   }
 
-  public Short(String str) 
-    throws NumberFormatException
+  /**
+   * Create a <code>Short</code> object representing the value of the 
+   * argument after conversion to a <code>short</code>.
+   *
+   * @param s the string to convert.
+   */
+  public Short(String s) throws NumberFormatException
   {
-    this.value = parseShort(str, 10);
+    value = parseShort(s, 10);
   }
 
-  public byte byteValue()
-  {
-    return (byte) value;
-  }
-
-  public short shortValue()
-  {
-    return value;
-  }
-
-  public int intValue()
-  {
-    return value;
-  }
-
-  public long longValue ()
-  {
-    return value;
-  }
-
-  public float floatValue ()
-  {
-    return (float) value;
-  }
-
-  public double doubleValue ()
-  {
-    return (double) value;
-  }
-
-  public static Short decode(String str)
-    throws NumberFormatException
-  {
-    int i = (Integer.decode(str)).intValue();
-    if (i < MIN_VALUE || i > MAX_VALUE)
-      throw new NumberFormatException();
-    return new Short((short) i);
-  }
-
-  public static short parseShort(String str, int radix)
-    throws NumberFormatException
-  {
-    int i = Integer.parseInt(str, radix);
-    if (i < MIN_VALUE || i > MAX_VALUE)
-      throw new NumberFormatException();
-    return (short) i;
-  }
-
-  public static short parseShort(String str)
-    throws NumberFormatException
-  {
-    return parseShort(str, 10);
-  }
-
-  public static Short valueOf(String str, int radix)
-    throws NumberFormatException
-  {
-    return new Short(parseShort(str, radix));
-  }
-
-  public static Short valueOf(String str)
-    throws NumberFormatException
-  {
-    return valueOf(str, 10);
-  }
-
-  // Added in JDK 1.2
-  public int compareTo(Short anotherShort)
-  {
-    return this.value - anotherShort.value;
-  }
-
-  // Added in JDK 1.2
-  /** @throws ClassCastException */
-  public int compareTo(Object o)
-  {
-    return this.value - ((Short) o).value;
-  }
-
-  public boolean equals(Object obj)
-  {
-    return (obj instanceof Short) && ((Short) obj).value == value;
-  }
-
-  // Verified that hashCode is returns plain value (see Short_1 test).
+  /**
+   * Return a hashcode representing this Object.
+   *
+   * <code>Short</code>'s hash code is calculated by simply returning its
+   * value.
+   *
+   * @return this Object's hash code.
+   */
   public int hashCode()
   {
     return value;
   }
 
-  public String toString()
+  /**
+   * If the <code>Object</code> is not <code>null</code>, is an
+   * <code>instanceof</code> <code>Short</code>, and represents
+   * the same primitive <code>short</code> value return 
+   * <code>true</code>.  Otherwise <code>false</code> is returned.
+   */
+  public boolean equals(Object obj)
   {
-    return Integer.toString((int) value);
+    return obj instanceof Short && ((Short)obj).value == value;
   }
 
-  public static String toString(short value)
+  /**
+   * Converts the <code>short</code> to a <code>String</code> and assumes
+   * a radix of 10.
+   * @param i the <code>short</code> to convert to <code>String</code>
+   * @return the <code>String</code> representation of the argument.
+   */    
+  public static String toString(short i)
   {
-    return Integer.toString((int) value);
+    return Integer.toString((int) i);
+  }
+
+  /**
+   * Converts the <code>Short</code> value to a <code>String</code> and
+   * assumes a radix of 10.
+   * @return the <code>String</code> representation of this <code>Short</code>.
+   */    
+  public String toString()
+  {
+    return Integer.toString ((int) value);
+  }
+
+  /**
+   * Creates a new <code>Short</code> object using the <code>String</code>,
+   * assuming a radix of 10.
+   * @param s the <code>String</code> to convert.
+   * @return the new <code>Short</code>.
+   * @see #Short(java.lang.String)
+   * @see #parseShort(java.lang.String)
+   * @exception NumberFormatException thrown if the <code>String</code> 
+   * cannot be parsed as a <code>short</code>.
+   */
+  public static Short valueOf(String s) throws NumberFormatException
+  {
+    return new Short(parseShort(s));
+  }
+
+  /**
+   * Creates a new <code>Short</code> object using the <code>String</code>
+   * and specified radix (base).
+   * @param s the <code>String</code> to convert.
+   * @param radix the radix (base) to convert with.
+   * @return the new <code>Short</code>.
+   * @see #parseShort(java.lang.String,int)
+   * @exception NumberFormatException thrown if the <code>String</code> 
+   * cannot be parsed as a <code>short</code>.
+   */
+  public static Short valueOf(String s, int radix)
+    throws NumberFormatException
+  {
+    return new Short(parseShort(s, radix));
+  }
+
+  /**
+   * Converts the specified <code>String</code> into a <code>short</code>.
+   * This function assumes a radix of 10.
+   *
+   * @param s the <code>String</code> to convert
+   * @return the <code>short</code> value of the <code>String</code>
+   *         argument.
+   * @exception NumberFormatException thrown if the <code>String</code> 
+   * cannot be parsed as a <code>short</code>.
+   */
+  public static short parseShort(String s) throws NumberFormatException
+  {
+    return parseShort(s, 10);
+  }
+
+  /**
+   * Converts the specified <code>String</code> into a <code>short</code>
+   * using the specified radix (base).
+   *
+   * @param s the <code>String</code> to convert
+   * @param radix the radix (base) to use in the conversion
+   * @return the <code>String</code> argument converted to </code>short</code>.
+   * @exception NumberFormatException thrown if the <code>String</code> 
+   * cannot be parsed as a <code>short</code>.
+   */
+  public static short parseShort(String s, int radix) 
+    throws NumberFormatException
+  {
+    int i = Integer.parseInt(s, radix);
+    if (i < MIN_VALUE || i > MAX_VALUE)
+      throw new NumberFormatException();
+    return (short) i;
+  }
+
+  /**
+   * Convert the specified <code>String</code> into a <code>Short</code>.
+   * The <code>String</code> may represent decimal, hexadecimal, or 
+   * octal numbers.
+   *
+   * The <code>String</code> argument is interpreted based on the leading
+   * characters.  Depending on what the String begins with, the base will be
+   * interpreted differently:
+   *
+   * <table>
+   * <tr><th>Leading<br>Characters</th><th>Base</th></tr>
+   * <tr><td>#</td><td>16</td></tr>
+   * <tr><td>0x</td><td>16</td></tr>
+   * <tr><td>0X</td><td>16</td></tr>
+   * <tr><td>0</td><td>8</td></tr>
+   * <tr><td>Anything<br>Else</td><td>10</td></tr>
+   * </table>
+   *
+   * @param s the <code>String</code> to interpret.
+   * @return the value of the String as a <code>Short</code>.
+   * @exception NumberFormatException thrown if the <code>String</code> 
+   * cannot be parsed as a <code>short</code>.    
+   */
+  public static Short decode(String s) throws NumberFormatException
+  {
+    int i = (Integer.decode(s)).intValue();
+    if (i < MIN_VALUE || i > MAX_VALUE)
+      throw new NumberFormatException();
+    return new Short((short) i);
+  }
+
+  /** Return the value of this <code>Short</code> as an <code>short</code>.
+   ** @return the value of this <code>Short</code> as an <code>short</code>.
+   **/
+  public byte byteValue()
+  {
+    return (byte) value;
+  }
+
+  /** Return the value of this <code>Short</code> as an <code>short</code>.
+   ** @return the value of this <code>Short</code> as an <code>short</code>.
+   **/
+  public short shortValue()
+  {
+    return value;
+  }
+
+  /** Return the value of this <code>Short</code> as an <code>int</code>.
+   ** @return the value of this <code>Short</code> as an <code>int</code>.
+   **/
+  public int intValue()
+  {
+    return value;
+  }
+
+  /** Return the value of this <code>Short</code> as a <code>long</code>.
+   ** @return the value of this <code>Short</code> as a <code>long</code>.
+   **/
+  public long longValue()
+  {
+    return value;
+  }
+
+  /** Return the value of this <code>Short</code> as a <code>float</code>.
+   ** @return the value of this <code>Short</code> as a <code>float</code>.
+   **/
+  public float floatValue()
+  {
+    return value;
+  }
+
+  /** Return the value of this <code>Short</code> as a <code>double</code>.
+   ** @return the value of this <code>Short</code> as a <code>double</code>.
+   **/
+  public double doubleValue()
+  {
+    return value;
+  }
+
+  /**
+   * Compare two Shorts numerically by comparing their
+   * <code>short</code> values.
+   * @return a positive value if this <code>Short</code> is greater
+   * in value than the argument <code>Short</code>; a negative value
+   * if this <code>Short</code> is smaller in value than the argument
+   * <code>Short</code>; and <code>0</code>, zero, if this
+   * <code>Short</code> is equal in value to the argument
+   * <code>Short</code>.  
+   *
+   * @since 1.2
+   */
+  public int compareTo(Short s)
+  {
+    return value - s.value;
+  }
+    
+  /**
+   * Behaves like <code>compareTo(java.lang.Short)</code> unless the Object
+   * is not a <code>Short</code>.  Then it throws a 
+   * <code>ClassCastException</code>.
+   * @exception ClassCastException if the argument is not a
+   * <code>Short</code>.
+   *
+   * @since 1.2
+   */
+  public int compareTo(Object o)
+  {
+    return compareTo((Short)o);
   }
 }
