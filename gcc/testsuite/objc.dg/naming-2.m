af43fcd223b3629c489159d5b49dd3d72524611d
shebs <shebs@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-08-01 08:15:11 +0000

    2001-08-01  Ziemowit Laski  <zlaski@apple.com>
    	* objc.dg/fwd-proto-1.m: New.
    	* objc.dg/local-decl-1.m: New.
    	* objc.dg/naming-1.m: New.
    	* objc.dg/naming-2.m: New.
    	* objc.dg/proto-hier-1.m: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@44537 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/objc.dg/naming-2.m
@@ -0,0 +1,12 @@
+/* Test for collision of @interfaces with global vars.  */
+/* Author: Ziemowit Laski <zlaski@apple.com>.  */
+/* { dg-do compile } */
+
+@interface Foo
+@end
+float Foo;  /* { dg-error "parse error before" } */
+
+double Bar;
+@interface Bar
+@end  /* { dg-error "redeclared as different kind of symbol" } */
+/* { dg-error "previous declaration of" "" { target *-*-* } 9 } */
