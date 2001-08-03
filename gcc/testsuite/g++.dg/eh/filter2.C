98668546eed7e5aedf3b77ffa222ded06d3774a2
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-08-03 23:15:51 +0000

    * except.c (collect_one_action_chain): Add an explicit cleanup         action if regions surrounding a catch were encoded entirely         within the call-site entry.
            * g++.dg/eh/filter1.C, g++.dg/eh/filter2.C: New tests.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@44616 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/eh/filter2.C
@@ -0,0 +1,59 @@
+// Test that terminate gets run when a catch filter fails to match while
+// running destructors.  Original bug depended on a::~a being inlined.
+// { dg-do run }
+// { dg-options -O }
+
+#include <exception>
+#include <cstdlib>
+
+struct e1 {};
+struct e2 {};
+
+struct a
+{
+  a () { }
+
+  ~a ()
+    {
+      try
+	{
+	  throw e1();
+	}
+      catch (e2 &)
+	{
+        }
+    }
+};
+
+void
+ex_test ()
+{
+  a aa;
+  try
+    {
+      throw e1 ();
+    }
+  catch (e2 &)
+    {
+    }
+}
+
+void my_terminate ()
+{
+  std::exit (0);
+}
+
+int
+main ()
+{
+  std::set_terminate (my_terminate);
+
+  try
+    {
+      ex_test ();
+    }
+  catch (...)
+    {
+    }
+  abort ();
+}
