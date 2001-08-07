1b81b7891b82b0d5bbb5ff760423f3d6ad3c701e
Jason Merrill <jason@redhat.com>
2001-08-07 07:13:54 -0400

    alias.c (get_alias_set): Return a previously calculated alias set for a VAR_DECL.
            * alias.c (get_alias_set): Return a previously calculated
            alias set for a VAR_DECL.
    
    From-SVN: r44683

---
+++ gcc/testsuite/g++.dg/opt/alias1.C
@@ -0,0 +1,25 @@
+// Test that type punning using an anonymous union works with strict aliasing.
+// { dg-do run }
+// { dg-options -O2 -fstrict-aliasing }
+
+extern "C" void abort ();
+
+void f (int i)
+{
+  union
+  {
+    int ui;
+    float uf[2];
+  };
+
+  ui = i;
+  if (uf[0] != 42.0)
+    abort ();
+}
+
+int main ()
+{
+  union U { int i; float f[2]; } u;
+  u.f[0] = 42.0;
+  f (u.i);
+}
