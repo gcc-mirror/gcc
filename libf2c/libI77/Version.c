8fd78854e646b2a6fd1ce40eb7d4a48889972545
GCC Administrator <gccadmin@gcc.gnu.org>
2001-08-07 07:16:42 +0000

    Daily bump.
    From-SVN: r44682

--- libf2c/libI77/Version.c
+++ libf2c/libI77/Version.c
@@ -3,7 +3,7 @@ static char junk[] = "\n@(#) LIBI77 VERSION pjw,dmg-mods 20001205\n";
 /*
 */
 
-char __G77_LIBI77_VERSION__[] = "0.5.26 20010806 (experimental)";
+char __G77_LIBI77_VERSION__[] = "0.5.26 20010807 (experimental)";
 
 /*
 2.01	$ format added
