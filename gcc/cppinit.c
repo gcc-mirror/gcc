5475a1654d48bc8a96067a11299abb874ee38495
neil <neil@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-08-07 20:37:26 +0000

    * cppinit.c (cpp_finish): Pop the final buffer without comment. 	* cpplex.c (_cpp_lex_token): Don't pop the final buffer; and 	take care to avoid multiple no-newline at EOF warnings in that 	case.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@44701 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cppinit.c
+++ gcc/cppinit.c
@@ -1010,12 +1010,13 @@ void
 cpp_finish (pfile)
      cpp_reader *pfile;
 {
-  if (CPP_BUFFER (pfile))
-    {
-      cpp_ice (pfile, "buffers still stacked in cpp_finish");
-      while (CPP_BUFFER (pfile))
-	_cpp_pop_buffer (pfile);
-    }
+  /* cpplex.c leaves the final buffer on the stack.  This it so that
+     it returns an unending stream of CPP_EOFs to the client.  If we
+     popped the buffer, we'd derefence a NULL buffer pointer and
+     segfault.  It's nice to allow the client to do worry-free excess
+     cpp_get_token calls.  */
+  while (pfile->buffer)
+    _cpp_pop_buffer (pfile);
 
   /* Don't write the deps file if preprocessing has failed.  */
   if (CPP_OPTION (pfile, print_deps) && pfile->errors == 0)
