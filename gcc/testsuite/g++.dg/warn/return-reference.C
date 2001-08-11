3c147435cf7229cb2fb804322a76731085802b37
gdr <gdr@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-08-11 20:21:55 +0000

    New test from PR #3230
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@44807 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/warn/return-reference.C
@@ -0,0 +1,21 @@
+// { dg-do compile }
+
+const int* bar();
+
+const int&
+foo1()
+{
+  static int empty;
+  const int* x = bar();
+  return (x ? *x : empty);
+}
+
+const int&
+foo2()
+{
+  static int empty;
+  const int* x = bar();
+  const int& r = (x ? *x : empty);
+  return (r);
+}
+
