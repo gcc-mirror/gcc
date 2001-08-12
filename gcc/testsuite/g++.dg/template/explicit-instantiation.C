ad8ede806262bef22615b373ad4294115400bebe
gdr <gdr@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-08-12 08:26:01 +0000

    Testcase from PR #3381
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@44822 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/explicit-instantiation.C
@@ -0,0 +1,13 @@
+// Contributed by Gabriel Dos Reis <gdr@codesourcery.com>
+// Origin: Jens.Maurer@gmx.net
+// { dg-do compile }
+
+namespace N
+{
+  template<class T>
+  class A { };
+}
+
+template class ::N::A<int>;
+
+
