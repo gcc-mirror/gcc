4e982a225fbdb1370f0c9e67479b23b073a3f9f6
Gabriel Dos Reis <gdr@gcc.gnu.org>
2001-08-12 13:04:39 +0000

    Testcase from PR #3625
    From-SVN: r44825

---
+++ gcc/testsuite/g++.dg/template/init-list.C
@@ -0,0 +1,17 @@
+// Contributed by Gabriel Dos Reis <gdr@codesourcery.com>
+// { dg-do compile }
+
+template<typename T>
+struct Base {
+  Base(int) { }
+};
+
+template<typename T>
+struct Derived : Base<T> {
+  Derived();
+};
+
+template<typename T>
+Derived<T>::Derived() : Base(4) { } // { dg-error "field" "" }
+
+
