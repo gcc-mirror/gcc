160fb9bc16f709eaa4a1c3b28d151118d44097d1
Gabriel Dos Reis <gdr@gcc.gnu.org>
2001-08-13 13:59:49 +0000

    New testcase from PR #3816
    From-SVN: r44850

---
+++ gcc/testsuite/g++.dg/other/anon-struct.C
@@ -0,0 +1,9 @@
+// Contributed by Gabriel Dos Reis <gdr@codesourcery.com>
+// { dg-do compile }
+
+namespace N { }
+
+namespace M
+{
+  typedef struct { } N;         // { dg-bogus ".*" "" { xfail *-*-* } }
+}
