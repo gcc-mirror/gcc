7a6ad18813de51b44a79384ff439af24b3ff4292
GCC Administrator <gccadmin@gcc.gnu.org>
2001-08-15 07:16:45 +0000

    Daily bump.
    From-SVN: r44918

--- libf2c/libU77/Version.c
+++ libf2c/libU77/Version.c
@@ -1,6 +1,6 @@
 static char junk[] = "\n@(#) LIBU77 VERSION 19980709\n";
 
-char __G77_LIBU77_VERSION__[] = "0.5.26 20010814 (experimental)";
+char __G77_LIBU77_VERSION__[] = "0.5.26 20010815 (experimental)";
 
 #include <stdio.h>
 
