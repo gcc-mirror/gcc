32fa4d4a5d61299e741e147e46580cef298ad639
Richard Henderson <rth@redhat.com>
2001-08-17 12:40:03 -0700

    elf.h, [...]: Fix typos last change.
            * config/mips/elf.h, config/mips/elf64.h, config/mips/netbsd.h,
            config/mips/openbsd.h: Fix typos last change.
    
    From-SVN: r44977

--- gcc/config/mips/netbsd.h
+++ gcc/config/mips/netbsd.h
@@ -210,12 +210,12 @@ do {									 \
    TARGET_ASM_NAMED_SECTION too.  */
 
 #undef TEXT_SECTION
-#define TEXT_SECTION()
-do {
-  if (TARGET_FILE_SWITCHING)
-    abort ();
-  fputs (TEXT_SECTION_ASM_OP, asm_out_file);
-  fputc ('\n', asm_out_file);
+#define TEXT_SECTION()				\
+do {						\
+  if (TARGET_FILE_SWITCHING)			\
+    abort ();					\
+  fputs (TEXT_SECTION_ASM_OP, asm_out_file);	\
+  fputc ('\n', asm_out_file);			\
 } while (0)
 
 /* Since gas and gld are standard on NetBSD, we don't need these */
