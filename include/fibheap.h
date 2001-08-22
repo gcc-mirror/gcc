d7a0e799d357c10f8fb0312c34b9176c026ca7d9
Richard Henderson <rth@redhat.com>
2001-08-21 18:53:38 -0700

    fibheap.h: Tidy formatting.
            * fibheap.h: Tidy formatting.
            (fibnode_t): Limit degree to 31 bits to avoid warning.
    
    From-SVN: r45088

--- include/fibheap.h
+++ include/fibheap.h
@@ -19,13 +19,12 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-/* Fibonacci heaps are somewhat complex, but, there's an
-   article in DDJ on them  that explains them pretty well:
+/* Fibonacci heaps are somewhat complex, but, there's an article in
+   DDJ that explains them pretty well:
 
    http://www.ddj.com/articles/1997/9701/9701o/9701o.htm?topic=algoritms
 
-   Introduction to algorithms by Corman and Rivest also goes over
-   them.
+   Introduction to algorithms by Corman and Rivest also goes over them.
 
    The original paper that introduced them is "Fibonacci heaps and their
    uses in improved network optimization algorithms" by Tarjan and
@@ -36,15 +35,14 @@ Boston, MA 02111-1307, USA.  */
    ExtractMin: O(lg n) amortized. O(n) worst case.
    DecreaseKey: O(1) amortized.  O(lg n) worst case. 
    Insert: O(2) amortized. O(1) actual.  
-   Union: O(1) amortized. O(1) actual.
-
-  
-*/
+   Union: O(1) amortized. O(1) actual.  */
 
 #ifndef _FIBHEAP_H_
 #define _FIBHEAP_H_
 
 #include <ansidecl.h>
+
+typedef long fibheapkey_t;
 
 typedef struct fibheap
 {
@@ -52,29 +50,32 @@ typedef struct fibheap
   struct fibnode *min;
   struct fibnode *root;
 } *fibheap_t;
-typedef long fibheapkey_t;
+
 typedef struct fibnode
 {
   struct fibnode *parent;
   struct fibnode *child;
   struct fibnode *left;
   struct fibnode *right;
-  unsigned int degree : sizeof(size_t) * CHAR_BIT - 2;
-  unsigned int mark:1;
   fibheapkey_t key;
   void *data;
+  unsigned int degree : 31;
+  unsigned int mark : 1;
 } *fibnode_t;
 
 extern fibheap_t fibheap_new PARAMS ((void));
 extern fibnode_t fibheap_insert PARAMS ((fibheap_t, fibheapkey_t, void *));
 extern int fibheap_empty PARAMS ((fibheap_t));
 extern fibheapkey_t fibheap_min_key PARAMS ((fibheap_t));
-extern fibheapkey_t fibheap_replace_key PARAMS ((fibheap_t, fibnode_t, fibheapkey_t));
-extern void *fibheap_replace_key_data PARAMS ((fibheap_t, fibnode_t, fibheapkey_t, void *));
+extern fibheapkey_t fibheap_replace_key PARAMS ((fibheap_t, fibnode_t,
+						 fibheapkey_t));
+extern void *fibheap_replace_key_data PARAMS ((fibheap_t, fibnode_t,
+					       fibheapkey_t, void *));
 extern void *fibheap_extract_min PARAMS ((fibheap_t));
 extern void *fibheap_min PARAMS ((fibheap_t));
 extern void *fibheap_replace_data PARAMS ((fibheap_t, fibnode_t, void *));
 extern void *fibheap_delete_node PARAMS ((fibheap_t, fibnode_t));
 extern void fibheap_delete PARAMS ((fibheap_t));
 extern fibheap_t fibheap_union PARAMS ((fibheap_t, fibheap_t));
+
 #endif /* _FIBHEAP_H_ */
