f12b58b3384f7b4faba39eaa3ebc67c3049f2a3c
gerald <gerald@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-08-22 14:35:51 +0000

    * Makefile.in, alias.c, basic-block.h, bb-reorder.c, bitmap.c, 	bitmap.h, builtin-types.def, builtins.c, builtins.def, 	c-aux-info.c, c-common.c, c-common.def, c-common.h, 	c-convert.c, c-decl.c, c-dump.c, c-dump.h, c-errors.c, 	c-format.c, c-lang.c, c-lex.c, c-lex.h, c-parse.in, 	c-pragma.c, c-pragma.h, c-semantics.c, c-tree.h, c-typeck.c, 	caller-save.c, calls.c, collect2.c, collect2.h, combine.c, 	conditions.h, config.gcc, configure.frag, configure.in, 	conflict.c, convert.c, convert.h, cppspec.c, crtstuff.c, 	cse.c, cselib.c, cselib.h, dbxout.c, dbxout.h, defaults.h, 	dependence.c, df.c, df.h, diagnostic.c, diagnostic.h, 	doloop.c, dominance.c, dwarf.h, dwarf2.h, dwarf2asm.c, 	dwarf2asm.h, dwarf2out.c, dwarf2out.h, dwarfout.c, 	emit-rtl.c, errors.c, errors.h, except.c, except.h, 	exgettext, explow.c, expmed.c, expr.c, expr.h, final.c, 	fixproto, flags.h, flow.c, fold-const.c, fp-test.c, 	function.c, function.h, gbl-ctors.h, gcc.c, gcc.h, gcc.hlp, 	gccspec.c, gcov-io.h, gcse.c, genattr.c, genattrtab.c, 	gencheck.c, gencodes.c, genconfig.c, genemit.c, 	genextract.c, genflags.c, gengenrtl.c, genmultilib, 	genopinit.c, genoutput.c, genpeep.c, genrecog.c, 	gensupport.c, gensupport.h, ggc-callbacks.c, ggc-common.c, 	ggc-none.c, ggc-page.c, ggc-simple.c, ggc.h, global.c, 	graph.c, graph.h, gthr-aix.h, gthr-dce.h, gthr-posix.h, 	gthr-rtems.h, gthr-single.h, gthr-solaris.h, gthr-vxworks.h, 	gthr-win32.h, gthr.h, haifa-sched.c, halfpic.c, halfpic.h, 	hard-reg-set.h, hwint.h, ifcvt.c, input.h, insn-addr.h, 	integrate.c, integrate.h, jump.c, lcm.c, libgcc2.c, 	libgcc2.h, lists.c, local-alloc.c, loop.c, loop.h, 	machmode.def, machmode.h, main.c, mbchar.c, mbchar.h, 	mips-tdump.c, mips-tfile.c, mklibgcc.in, mkmap-flat.awk, 	mkmap-symver.awk, optabs.c, output.h, params.c, params.def, 	params.h, predict.c, predict.def, predict.h, prefix.c, 	prefix.h, print-rtl.c, print-tree.c, profile.c, protoize.c, 	read-rtl.c, real.c, real.h, recog.c, recog.h, reg-stack.c, 	regclass.c, regmove.c, regrename.c, regs.h, reload.c, 	reload.h, reload1.c, reorg.c, resource.c, resource.h, rtl.c, 	rtl.def, rtl.h, rtlanal.c, sbitmap.c, sbitmap.h, 	sched-deps.c, sched-ebb.c, sched-int.h, sched-rgn.c, 	sched-vis.c, sdbout.c, sdbout.h, sibcall.c, simplify-rtx.c, 	ssa-ccp.c, ssa-dce.c, ssa.c, ssa.h, stmt.c, stor-layout.c, 	stringpool.c, system.h, timevar.c, timevar.def, timevar.h, 	tlink.c, toplev.c, toplev.h, tree.c, tree.def, tree.h, 	tsystem.h, unroll.c, unwind-dw2-fde.c, unwind-dw2-fde.h, 	unwind-dw2.c, unwind-pe.h, unwind-sjlj.c, unwind.h, 	unwind.inc, varasm.c, varray.c, varray.h, xcoffout.c, 	xcoffout.h: replace "GNU CC" with "GCC".
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@45105 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/prefix.c
+++ gcc/prefix.c
@@ -1,12 +1,12 @@
 /* Utility to update paths from internal to external forms.
    Copyright (C) 1997, 1998, 1999, 2000 Free Software Foundation, Inc.
 
-This file is part of GNU CC.
+This file is part of GCC.
 
-GNU CC is free software; you can redistribute it and/or
-modify it under the terms of the GNU Library General Public
-License as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later version.
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
 
 GCC is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
