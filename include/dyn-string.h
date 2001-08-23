59f598943ff10a79b25cf412288e59a2ea2cbba5
gerald <gerald@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-08-23 12:07:58 +0000

    * dyn-string.h, fibheap.h, partition.h, sort.h, splay-tree.h: 	replace "GNU CC" with "GCC".
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@45130 138bc75d-0d04-0410-961f-82ee72b054a4

--- include/dyn-string.h
+++ include/dyn-string.h
@@ -2,20 +2,20 @@
    Copyright (C) 1998, 1999, 2000 Free Software Foundation, Inc.
    Contributed by Mark Mitchell (mark@markmitchell.com).
 
-This file is part of GNU CC.
+This file is part of GCC.
    
-GNU CC is free software; you can redistribute it and/or modify
+GCC is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2, or (at your option)
 any later version.
 
-GNU CC is distributed in the hope that it will be useful,
+GCC is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with GNU CC; see the file COPYING.  If not, write to
+along with GCC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
