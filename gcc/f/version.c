c4788ff65e6018b09222b666ceab88b188d0fe7d
Mark Mitchell <mark@codesourcery.com>
2001-08-24 20:16:10 +0000

    * gcc_release: Correct quoting.
    From-SVN: r45160

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-const char *ffe_version_string = "0.5.26 20010824 (release)";
+const char *ffe_version_string = "0.5.26 20010824 (prerelease)";
--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
 #include "gansidecl.h"
 #include "version.h"
 
-const char *const version_string = "3.0.1";
+const char *const version_string = "3.0.2 20010824 (prerelease)";
