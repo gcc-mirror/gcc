d3b75f2624bcec149271d0f11f64b6cfc0ad48d2
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-08-31 17:11:27 +0000

    * java/io/ByteArrayOutputStream.java: Re-merged with Classpath. 	* java/io/OptionalDataException.java: Merged with Classpath.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@45327 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/io/OptionalDataException.java
+++ libjava/java/io/OptionalDataException.java
@@ -1,39 +1,87 @@
-/* Copyright (C) 2000  Free Software Foundation
+/* OptionalDataException.java -- indicates unexpected data in serialised stream
+   Copyright (C) 1998, 2000, 2001 Free Software Foundation, Inc.
 
-   This file is part of libgcj.
+This file is part of GNU Classpath.
 
-This software is copyrighted work licensed under the terms of the
-Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
-details.  */
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+ 
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+As a special exception, if you link this library with other files to
+produce an executable, this library does not by itself cause the
+resulting executable to be covered by the GNU General Public License.
+This exception does not however invalidate any other reasons why the
+executable file might be covered by the GNU General Public License. */
+
 
 package java.io;
-
-/**
- * @author Warren Levy <warrenl@cygnus.com>
- * @date February 7, 2000.
- */
 
 /* Written using on-line Java Platform 1.2 API Specification.
  * Status:  Believed complete and correct.
  */
 
+/**
+  * This exception is thrown when unexpected data appears in the input
+  * stream from which a serialized object is being read.  The field
+  * <code>eof</code> will always be set to true (***Why even have it?***) and 
+  * the <code>count</code> field will contain the number of valid bytes
+  * available to be read.
+  *
+  * @version 0.0
+  *
+  * @author Aaron M. Renn (arenn@urbanophile.com)
+  * @author Warren Levy <warrenl@cygnus.com>
+  * @date February 7, 2000. 
+  */
 public class OptionalDataException extends ObjectStreamException
 {
-  // FIXME: Need to set these fields per the doc in a constructor.
-  public boolean eof;
-  public int length;
 
-  // FIXME: This can probably go away once the right signatures of
-  // these package private constructors is determined.
-  private static final long serialVersionUID = -8011121865681257820L;
+private static final long serialVersionUID = -8011121865681257820L;
 
-  OptionalDataException()
-  {
-    super();
-  }
+/*
+ * Instance Variables
+ */
 
-  OptionalDataException(String msg)
-  {
-    super(msg);
-  }
+/**
+  * Whether or not the end of the stream has been reached
+  */
+public boolean eof;
+
+/**
+  * The number of valid bytes that can be read
+  */
+public int length;
+
+/*************************************************************************/
+
+/*
+ * Constructors
+ */
+
+/**
+  * Create a new OptionalDataException with an eof parameter indicating
+  * whether or not the end of stream is reached and the number of valid
+  * bytes that may be read.
+  *
+  * @param eof 'true' if end of stream reached, 'false' otherwise
+  * @param count The number of valid bytes to be read.
+  */
+OptionalDataException(boolean eof, int count)
+{
+  this.eof = eof;
+  this.length = count;
 }
+
+} // class OptionalDataException
+
