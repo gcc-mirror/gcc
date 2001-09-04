8518f8e15139f7a0f10d76e6105a4e2e290f3b22
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-09-04 17:18:20 +0000

    Fix for PR libgcj/4213: 	* Makefile.am (ordinary_java_source_files): Added new file. 	* gnu/gcj/text/LocaleData.java: New file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@45381 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/gnu/gcj/text/LocaleData.java
@@ -0,0 +1,25 @@
+// Generic locale data for java.text.
+
+/* Copyright (C) 2001  Free Software Foundation
+
+   This file is part of libgcj.
+
+This software is copyrighted work licensed under the terms of the
+Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
+details.  */
+
+package gnu.gcj.text;
+
+import java.util.ListResourceBundle;
+
+public class LocaleData extends ListResourceBundle
+{
+  private static final Object[][] contents =
+  {
+  };
+
+  protected Object[][] getContents ()
+  {
+    return contents;
+  }
+}
