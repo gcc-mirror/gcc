f660683a337c9a50c0949ed79f0d43eceb19fbeb
nickc <nickc@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-09-14 11:05:39 +0000

    Fix typo in FSF copyright statement
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@45595 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/sched-vis.c
+++ gcc/sched-vis.c
@@ -17,8 +17,8 @@ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 for more details.
 
 You should have received a copy of the GNU General Public License
-along with GCC; see the file COPYING.  If not, write to the Free the
-Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+along with GCC; see the file COPYING.  If not, write to the Free
+Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 02111-1307, USA.  */
 
 #include "config.h"
