c2de8f0d525dd5eeab1e2469ceb81173ffb9622f
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-09-15 18:36:40 +0000

    * 980223.c: Change type of addr from long to char *.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@45635 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/980223.c
+++ gcc/testsuite/gcc.c-torture/execute/980223.c
@@ -1,4 +1,4 @@
-typedef struct { long addr; long type; } object;
+typedef struct { char *addr; long type; } object;
 
 object bar (object blah)
 {
@@ -19,13 +19,13 @@ object foo (object x, object y)
 }
 
 int nil;
-object cons1[2] = { {(long) &nil, 0}, {(long) &nil, 0} };
-object cons2[2] = { {(long) &cons1, 64}, {(long) &nil, 0} };
+object cons1[2] = { {(char *) &nil, 0}, {(char *) &nil, 0} };
+object cons2[2] = { {(char *) &cons1, 64}, {(char *) &nil, 0} };
 
 main()
 {
-  object x = {(long) &cons2, 64};
-  object y = {(long) &nil, 0};
+  object x = {(char *) &cons2, 64};
+  object y = {(char *) &nil, 0};
   object three = foo(x,y);
   return 0;
 }
