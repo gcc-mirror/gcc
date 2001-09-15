3617ff1e5d2e4a91a2d3e28055b5ff9bca5d5d8f
Neil Booth <neil@daikokuya.demon.co.uk>
2001-09-15 18:38:46 +0000

    * gcc.dg/cpp/macro9.c: New test.
    From-SVN: r45636

---
+++ gcc/testsuite/gcc.dg/cpp/macro9.c
@@ -0,0 +1,14 @@
+/* Copyright (C) 2001 Free Software Foundation, Inc.  */
+
+/* { dg-do compile } */
+
+/* Source: Neil Booth, 15 Sep 2001.
+
+   A silly test to check that if a function-like macro name is
+   immediately followed by a directive, then we process the directive
+   properly.  */
+
+#define int()
+int
+#define main main () { return 0; }
+main
