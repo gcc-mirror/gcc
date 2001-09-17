b611e9ad2a95154d3f4cff6b0902f9e2b77b0fde
No Author <no-author@gcc.gnu.org>
2001-09-17 19:56:41 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_0-branch'.
    From-SVN: r45658

---
+++ gcc/testsuite/gcc.dg/array-5.c
@@ -0,0 +1,42 @@
+/* { dg-do compile } */
+/* { dg-options "" } */
+
+/* Check compatibility of array declarations.  */
+
+/* Incomplete decl matches.  */
+extern char arr0[];
+char arr0[1];
+
+/* Two integral expressions must be the same.  Note that 0 is
+   a gcc extension, but it should work like any other constant.  */
+extern char arr1[1];
+char arr1[1];
+extern char arr2[0];
+char arr2[0];
+extern char arr3[0];            /* { dg-error "previous declaration" } */
+char arr3[1];                   /* { dg-error "conflicting types" } */
+
+/* Variable size matches.  */
+void func(int n, int m)
+{
+  /* The next two are from the example in c99 6.7.5.2/9.  */
+  {
+    /* Invalid: not compatible because 4 != 6.  */
+    int a[n][6][m];
+    int (*p)[4][n+1];
+    p = a;			/* { dg-error "incompatible" } */
+  }
+  {
+    /* Compatible, but defined behavior only if n == 6 and m == n+1.  */
+    int c[n][n][6][m];
+    int (*r)[n][n][n+1];
+    r = c;
+  }
+  {
+    /* Compatible, but undefined behavior; (2, 2) is not a constant
+       expression, and thus A is a VLA.  */
+    int a[6][(2, 2)];
+    int (*p)[3];
+    p = a; /* { dg-bogus "incompatible" "bad vla handling" { xfail *-*-* } } */
+  }
+}
