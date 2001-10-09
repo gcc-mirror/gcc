12eac81ab0ea8146723271aca4838a4e09fc645d
Jason Merrill <jason@gcc.gnu.org>
2001-10-09 11:44:25 -0400

    reorganize
    From-SVN: r46125

---
+++ gcc/testsuite/g++.dg/init/array1.C
@@ -0,0 +1,26 @@
+// Test that completing an array declared with a typedef doesn't change
+// the typedef.
+
+// { dg-do run }
+
+typedef int iArr[];
+
+const iArr array4={
+  {1},{2},{3},{4}
+};
+
+const iArr array3={
+  {1},{2},{3}
+};
+
+const iArr array5={
+  {1},{2},{3},{4},{5}
+};
+
+int main()
+{
+  if (sizeof (array4)/sizeof (array4[0]) != 4
+      || sizeof (array3)/sizeof (array3[0]) != 3
+      || sizeof (array5)/sizeof (array5[0]) != 5)
+    return 1;
+}
