ab17bb727725899888b456cc8a79fff658a7a98c
GCC Administrator <gccadmin@gcc.gnu.org>
2001-10-12 07:41:42 +0000

    Daily bump.
    From-SVN: r46225

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-const char *ffe_version_string = "0.5.26 20011011 (prerelease)";
+const char *ffe_version_string = "0.5.26 20011012 (prerelease)";
--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
 #include "gansidecl.h"
 #include "version.h"
 
-const char *const version_string = "3.0.2 20011011 (prerelease)";
+const char *const version_string = "3.0.2 20011012 (prerelease)";
