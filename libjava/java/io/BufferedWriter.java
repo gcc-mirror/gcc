b458bf6b319b47fdb32ebb680a94c63172a4df4b
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-10-18 23:43:59 +0000

    * java/io/BufferedWriter.java (write(String,int,int)): Correctly 	check bounds.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@46338 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/io/BufferedWriter.java
+++ libjava/java/io/BufferedWriter.java
@@ -1,5 +1,5 @@
 /* BufferedWriter.java -- Buffer output into large blocks before writing
-   Copyright (C) 1998, 1999, 2000 Free Software Foundation, Inc.
+   Copyright (C) 1998, 1999, 2000, 2001 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
@@ -199,7 +199,7 @@ public class BufferedWriter extends Writer
    */
   public void write (String str, int offset, int len) throws IOException
   {
-    if (offset < 0 || len < 0 || offset + len < str.length())
+    if (offset < 0 || len < 0 || offset + len > str.length())
       throw new ArrayIndexOutOfBoundsException ();
 
     synchronized (lock)
