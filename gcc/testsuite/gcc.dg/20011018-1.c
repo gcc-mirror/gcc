5bdbf61420856238528e0f4bcbaf4534e9debc1b
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-10-19 19:34:44 +0000

    * config/alpha/alpha.c (summarize_insn): Don't abort on ASM_INPUT.
    	* gcc.dg/20011018-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@46357 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20011018-1.c
@@ -0,0 +1,15 @@
+/* { dg-do compile { target alpha*-*-* } } */
+/* { dg-options "-O2 -mieee" } */
+
+double foo (void);
+void bar (float, float);
+
+void test (void)
+{
+  float f, g;
+
+  f = foo();
+  g = foo();
+  asm ("");
+  bar (f, g);
+}
