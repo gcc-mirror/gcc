e6a7e62eb3f46044027a4b7f2217d5f87a3e8bf3
Robert Dewar <dewar@gnat.com>
2001-10-25 22:51:49 +0000

    * s-tpinop.ads: Add 2001 to copyright notice. Fix header format.
    From-SVN: r46499

--- gcc/ada/s-tpinop.ads
+++ gcc/ada/s-tpinop.ads
@@ -2,14 +2,13 @@
 --                                                                          --
 --                GNU ADA RUN-TIME LIBRARY (GNARL) COMPONENTS               --
 --                                                                          --
---                S Y S T E M . T A S K _ P R I M I T I V E S .             --
---                   I N T E R R U P T _ O P E R A T I O N S                --
+--                SYSTEM.TASK_PRIMITIVES.INTERRUPT_OPERATIONS               --
 --                                                                          --
 --                                  S p e c                                 --
 --                                                                          --
---                             $Revision: 1.5 $                             --
+--                             $Revision$
 --                                                                          --
---              Copyright (C) 1998 Free Software Foundation, Inc.           --
+--           Copyright (C) 1998-2001 Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNARL is free software; you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
