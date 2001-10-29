fdfc3540c096bc36865395e853a006eba622b35c
nicola <nicola@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-10-29 21:00:46 +0000

    New objc tests
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@46614 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/objc/execute/class_self-2.m
@@ -0,0 +1,65 @@
+/* Contributed by Nicola Pero - Fri Oct 26 22:39:32 BST 2001 */
+#include <objc/objc.h>
+
+/* Test calling a class method on self where self has been redefined
+   to be another class - the call requires a cast */
+
+
+/* The first class */
+struct d
+{
+  int a;
+};
+
+@interface ClassA
+{
+  Class isa;
+}
++ (Class) class;
++ (struct d) method;
+@end
+
+@implementation ClassA
++ (Class) class
+{
+  return self;
+}
+
++ (struct d) method
+{
+  struct d u;
+  u.a = 5;
+  
+  return u;
+}
+@end
+
+/* The second class */
+@interface TestClass
+{
+  Class isa;
+}
++ (void) test;
+@end
+
+@implementation TestClass
++ (void) test
+{
+  self = [ClassA class];
+  
+
+  if ([(Class)self method].a != 5)
+    {
+      abort ();
+    }
+}
+
+@end
+
+
+int main (void)
+{
+  [TestClass test];
+
+  return 0;
+}
