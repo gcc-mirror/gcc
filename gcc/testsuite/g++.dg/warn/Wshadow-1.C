8ac9ea616e01991beb4164c1a44647763d33302c
Neil Booth <neil@daikokuya.demon.co.uk>
2001-11-08 19:04:43 +0000

    decl.c (shadow_warning): New function.
    	* cp/decl.c (shadow_warning): New function.
    	(pushdecl): Improve -Wshadow warnings.  Don't give both a warning
    	and an error when a block scope decl shadows a parameter.
    
    	* g++.dg/warn/Wshadow-1.C: New tests.
    	* g++.old-deja/g++.mike/for3.C: Update.
    
    From-SVN: r46852

---
+++ gcc/testsuite/g++.dg/warn/Wshadow-1.C
@@ -0,0 +1,42 @@
+/* Copyright (C) 2001 Free Software Foundation, Inc.  */
+
+/* { dg-do compile } */
+/* { dg-options -Wshadow } */
+
+/* Source: Neil Booth, 3 Nov 2001, and PR 16, 713.  -Wshadow was
+   giving a bunch of warnings we didn't want, and wasn't giving the
+   location of the shadowed variable.  */
+
+struct status			// { dg-bogus "shadowed declaration" }
+{
+  int member;
+  void foo2 ();
+
+  inline static int foo3 (int member) // { dg-bogus "shadows" }
+  {
+    return member;
+  }
+};
+
+int decl1;			// { dg-warning "shadowed declaration" }
+int decl2;			// { dg-warning "shadowed declaration" }
+void foo (struct status &status,// { dg-bogus "shadows a global decl" }
+	  double decl1)
+{				// { dg-warning "shadows a global decl" }
+}
+
+void foo1 (int d)
+{
+  double d;			// { dg-error "shadows a parameter" }
+}
+
+// { dg-error "In member function" "ignored" { target *-*-* } 0 }
+void status::foo2 ()
+{
+  int member;			// { dg-warning "shadows a member" }
+  int decl2;			// { dg-warning "shadows a global decl" }
+  int local;			// { dg-warning "shadowed declaration" }
+  {
+    int local;			// { dg-warning "shadows a previous local" }
+  }
+}
--- gcc/testsuite/g++.old-deja/g++.mike/for3.C
+++ gcc/testsuite/g++.old-deja/g++.mike/for3.C
@@ -1,9 +1,9 @@
 // Special g++ Options: -Wshadow
 
 int
-main(int i) {
-  for(int i=1; i < 3; i++);	// WARNING - shadows parm
-  for(int i=1; i < 3; i++);	// WARNING - shadows parm
+main(int i) {			// WARNING - shadowed decl
+  for(int i=1; i < 3; i++);	// WARNING - declaration of
+  for(int i=1; i < 3; i++);	// WARNING - declaration of
   for(int j=1; j < 3; j++);
   for(int j=1; j < 3; j++);
 }
