49b2e77d7a2fd0c2cae150076d28c3e443e825f5
obrien <obrien@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-11-10 01:07:17 +0000

    2001-11-07  David O'Brien  <obrien@FreeBSD.org> 	* config.gcc: Add FreeBSD/IA-64 target. Expand config/i64/elf.h headers. 	* config/i64/ia64.h (TARGET_VERSION,ASM_SPEC,LINK_SPEC): Remove, 	they are OS specific.  Use CPP_CPU_SPEC rather than CPP_PREDEFINES, 	so OS specific files and use CPP_PREDEFINES for their needs. 	* config/i64/elf.h (TARGET_VERSION): Add.  Also, move inclusion of 	target headers to tm_file. 	* config/ia64/aix.h (TARGET_VERSION): Likewise. 	* config/ia64/hpux.h (TARGET_VERSION): Likewise. 	* config/ia64/linux.h (TARGET_VERSION): Likewise. 	(FUNCTION_PROFILER): Remove. 	* config/ia64/ia64.h (FUNCTION_PROFILER): Update defintion using the 	version in config/ia64/linux.h. 	* config/ia64/freebsd.h: New file. 	* config/freebsd.h: Tidy up. 	(_USING_CONFIG_FREEBSD): Define. 	(FBSD_CPP_PREDEFINES): Add __KPRINTF_ATTRIBUTE__. 	(FBSD_CPP_SPEC): Define generic FreeBSD spec. 	(FBSD_STARTFILE_SPEC): Likewise. 	(FBSD_ENDFILE_SPEC): Likewise. 	(FBSD_LIB_SPEC): Likewise. 	(WCHAR_UNSIGNED): Undefine, FreeBSD's wchars are signed. 	(USER_LABEL_PREFIX): Define to ELF compatible value as some CPU headers 	set an ELF-improper one.
    Approved by:  Richard Henderson <rth@redhat.com>
                  Message-ID: <20011109100352.A6186@redhat.com>
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@46909 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/ia64/freebsd.h
@@ -0,0 +1,66 @@
+/* Definitions for Intel IA-64 running FreeBSD using the ELF format
+   Copyright (C) 2001 Free Software Foundation, Inc.
+   Contributed by David E. O'Brien <obrien@FreeBSD.org> and BSDi.
+
+This file is part of GNU CC.
+
+GNU CC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU CC; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+
+#undef  LINK_SPEC
+#define LINK_SPEC							\
+ "%{p:%e`-p' not supported; use `-pg' and gprof(1)}			\
+  %{Wl,*:%*}								\
+  %{assert*} %{R*} %{rpath*} %{defsym*}					\
+  %{shared:-Bshareable %{h*} %{soname*}}				\
+  %{symbolic:-Bsymbolic}						\
+  %{!shared:								\
+    %{!static:								\
+      %{rdynamic:-export-dynamic}					\
+      %{!dynamic-linker:-dynamic-linker /usr/libexec/ld-elf.so.1}}	\
+    %{static:-Bstatic}}"
+
+#undef  ASM_SPEC
+#define ASM_SPEC "-x %{mconstant-gp} %{mauto-pic}"
+
+
+/************************[  Target stuff  ]***********************************/
+
+/* Define the actual types of some ANSI-mandated types.  
+   Needs to agree with <machine/ansi.h>.  GCC defaults come from c-decl.c,
+   c-common.c, and config/<arch>/<arch>.h.  */
+
+/* Earilier headers may get this wrong for FreeBSD.
+   We use the GCC defaults instead.  */
+#undef WCHAR_TYPE
+
+#undef  WCHAR_UNSIGNED
+#define WCHAR_UNSIGNED 0
+
+#undef  WCHAR_TYPE_SIZE
+#define WCHAR_TYPE_SIZE 32
+
+#undef  TARGET_VERSION
+#define TARGET_VERSION fprintf (stderr, " (FreeBSD/IA-64 ELF)");
+
+#define TARGET_ELF		1
+
+#define DONT_USE_BUILTIN_SETJMP
+#define JMP_BUF_SIZE  76
+
+/* Output any profiling code before the prologue.  */
+
+#undef  PROFILE_BEFORE_PROLOGUE
+#define PROFILE_BEFORE_PROLOGUE 1
--- gcc/config/ia64/ia64.h
+++ gcc/config/ia64/ia64.h
@@ -31,11 +31,11 @@ Boston, MA 02111-1307, USA.  */
 
 /* Run-time target specifications */
 
-/* Define this to be a string constant containing `-D' options to define the
-   predefined macros that identify this machine and system.  These macros will
-   be predefined unless the `-ansi' option is specified.  */
-/* ??? This is undefed in svr4.h.  */
-#define CPP_PREDEFINES "-Dia64 -Amachine=ia64"
+#define CPP_CPU_SPEC "\
+  -Acpu=ia64 -Amachine=ia64 \
+  %{!ansi:%{!std=c*:%{!std=i*:-Dia64}}} -D__ia64 -D__ia64__"
+
+#define CC1_SPEC "%(cc1_cpu) "
 
 /* This declaration should be present.  */
 extern int target_flags;
@@ -176,11 +176,6 @@ extern const char *ia64_fixed_range_string;
       N_("Specify range of registers to make fixed.")},			\
 }
 
-/* This macro is a C statement to print on `stderr' a string describing the
-   particular machine description choice.  */
-
-#define TARGET_VERSION fprintf (stderr, " (IA-64)");
-
 /* Sometimes certain combinations of command options do not make sense on a
    particular target machine.  You can define a macro `OVERRIDE_OPTIONS' to
    take account of this.  This macro, if defined, is executed once just after
@@ -242,36 +237,6 @@ extern const char *ia64_fixed_range_string;
    into options for GNU CC to pass to the `cc1plus'.  */
 
 /* #define CC1PLUS_SPEC "" */
-
-/* A C string constant that tells the GNU CC driver program options to pass to
-   the assembler.  It can also specify how to translate options you give to GNU
-   CC into options for GNU CC to pass to the assembler.  */
-
-#if ((TARGET_CPU_DEFAULT | TARGET_DEFAULT) & MASK_GNU_AS) != 0
-/* GNU AS.  */
-#define ASM_SPEC \
-  "%{mno-gnu-as:-N so} %{!mno-gnu-as:-x} %{mconstant-gp} %{mauto-pic}"
-#else
-/* Intel ias.  */
-#define ASM_SPEC \
-  "%{!mgnu-as:-N so} %{mgnu-as:-x} %{mconstant-gp:-M const_gp}\
-   %{mauto-pic:-M no_plabel}"
-#endif
-
-/* A C string constant that tells the GNU CC driver program options to pass to
-   the linker.  It can also specify how to translate options you give to GNU CC
-   into options for GNU CC to pass to the linker.  */
-
-/* The Intel linker does not support dynamic linking, so we need -dn.
-   The Intel linker gives annoying messages unless -N so is used.  */
-#if ((TARGET_CPU_DEFAULT | TARGET_DEFAULT) & MASK_GNU_LD) != 0
-/* GNU LD.  */
-#define LINK_SPEC "%{mno-gnu-ld:-dn -N so}"
-#else
-/* Intel ild.  */
-#define LINK_SPEC "%{!mgnu-ld:-dn -N so}"
-#endif
-
 
 /* Storage Layout */
 
@@ -1570,21 +1535,24 @@ do {									\
 /* A C statement or compound statement to output to FILE some assembler code to
    call the profiling subroutine `mcount'.  */
 
-/* ??? Unclear if this will actually work.  No way to test this currently.  */
-
-#define FUNCTION_PROFILER(FILE, LABELNO) \
+#undef FUNCTION_PROFILER
+#define FUNCTION_PROFILER(FILE, LABELNO)				\
 do {									\
   char buf[20];								\
   ASM_GENERATE_INTERNAL_LABEL (buf, "LP", LABELNO);			\
-  fputs ("\taddl r16 = @ltoff(", FILE);					\
+  fputs ("\talloc out0 = ar.pfs, 8, 0, 4, 0\n", FILE);			\
+  if (TARGET_AUTO_PIC)							\
+    fputs ("\tmovl out3 = @gprel(", FILE);				\
+  else									\
+    fputs ("\taddl out3 = @ltoff(", FILE);				\
   assemble_name (FILE, buf);						\
-  fputs ("), gp\n", FILE);						\
-  fputs ("\tmov r17 = r1;;\n", FILE);					\
-  fputs ("\tld8 out0 = [r16]\n", FILE);					\
-  fputs ("\tmov r18 = b0\n", FILE);					\
-  fputs ("\tbr.call.sptk.many rp = mcount;;\n", FILE);			\
-  fputs ("\tmov b0 = r18\n", FILE);					\
-  fputs ("\tmov r1 = r17;;\n", FILE);					\
+  if (TARGET_AUTO_PIC)							\
+    fputs (");;\n", FILE);						\
+  else									\
+    fputs ("), r1;;\n", FILE);						\
+  fputs ("\tmov out1 = r1\n", FILE);					\
+  fputs ("\tmov out2 = b0\n", FILE);					\
+  fputs ("\tbr.call.sptk.many b0 = _mcount;;\n", FILE);			\
 } while (0)
 
 /* A C statement or compound statement to output to FILE some assembler code to
