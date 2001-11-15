52703367b83f8fd235d20e17620f8b3f1a329c8a
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-11-15 17:47:40 +0000

    avoid includes
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@47064 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.other/null2.C
+++ gcc/testsuite/g++.old-deja/g++.other/null2.C
@@ -1,6 +1,6 @@
 // Based on a testcase by Eric Dumazet <Eric.Dumazet@COSMOSBAY.COM>
 
-#include <cstdlib>
+extern "C" void abort ();
 
 const char * const foo = ""; // foo is not NULL
 
@@ -8,5 +8,5 @@ int main() {
   if ((foo == 0) ? 0 : foo)  // so this should evaluate to `foo'
     return 0;
   else
-    std::abort();
+    abort();
 }
