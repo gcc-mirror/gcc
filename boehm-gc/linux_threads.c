38100f51f3b2e49a169d8ab82db1ffbab8260ff8
ljrittle <ljrittle@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-11-26 17:40:34 +0000

    * linux_threads.c (WRAP_FUNC(pthread_join)): Remove special 	case for GC_FREEBSD_THREADS. 	* configure.in (*-*-freebsd*): Clarify warning. 	* configure: Rebuilt.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@47347 138bc75d-0d04-0410-961f-82ee72b054a4

--- boehm-gc/linux_threads.c
+++ boehm-gc/linux_threads.c
@@ -1268,17 +1268,6 @@ int WRAP_FUNC(pthread_join)(pthread_t thread, void **retval)
     /* cant have been recycled by pthreads.				*/
     UNLOCK();
     result = REAL_FUNC(pthread_join)(thread, retval);
-# if defined (GC_FREEBSD_THREADS)
-    /* On FreeBSD, the wrapped pthread_join() sometimes returns (what
-       appears to be) a spurious EINTR which caused the test and real code
-       to gratuitously fail.  Having looked at system pthread library source
-       code, I see how this return code may be generated.  In one path of
-       code, pthread_join() just returns the errno setting of the thread
-       being joined.  This does not match the POSIX specification or the
-       local man pages thus I have taken the liberty to catch this one
-       spurious return value properly conditionalized on GC_FREEBSD_THREADS. */
-    if (result == EINTR) result = 0;
-# endif
     if (result == 0) {
         LOCK();
         /* Here the pthread thread id may have been recycled. */
