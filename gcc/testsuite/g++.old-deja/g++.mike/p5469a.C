c7447b1989facf1dfcf6ad0930cb1b5766509264
Mark Mitchell <mmitchel@gcc.gnu.org>
2001-11-29 21:44:11 +0000

    re PR c++/3471 (gcc 3.01 reports error about a private copy constructor that shouldn't get called.)
    	PR c++/3471
    	* call.c (convert_like_real): Do not build additional temporaries
    	for rvalues of class type.
    
    From-SVN: r47454

--- gcc/testsuite/g++.old-deja/g++.mike/p5469a.C
+++ gcc/testsuite/g++.old-deja/g++.mike/p5469a.C
@@ -4,7 +4,7 @@ int count;
 
 class A {
   A();
-  A(const A&);			// ERROR - referenced below
+  A(const A&);
 public:
   A(int) { ++count; }
   ~A() { --count; }
@@ -14,7 +14,7 @@ public:
 int main() {
   {
     A a (1);
-    if (a == 2 || a == 1)	// ERROR - private copy ctor
+    if (a == 2 || a == 1)
       ;
   }
   return count;
