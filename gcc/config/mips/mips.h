dfabc2e70a829a868b61c5a27036bfc1bf0f4e39
echristo <echristo@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-12-04 06:30:02 +0000

    2001-12-03  Eric Christopher  <echristo@redhat.com>
    	* config/mips/mips.h (CALL_REALLY_USED_REGISTERS): New macro.
    	* config/mips/mips.md: Check TARGET_DEBUG_D_MODE before split
    	patterns.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@47590 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -1716,6 +1716,25 @@ do {							\
   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1					\
 }
 
+/* Like `CALL_USED_REGISTERS' but used to overcome a historical
+   problem which makes CALL_USED_REGISTERS *always* include
+   all the FIXED_REGISTERS.  Until this problem has been
+   resolved this macro can be used to overcome this situation.
+   In particular, block_propagate() requires this list
+   be acurate, or we can remove registers which should be live.
+   This macro is used in regs_invalidated_by_call.  */
+
+
+#define CALL_REALLY_USED_REGISTERS                                      \
+{ /* General registers.  */                                             \
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,                       \
+  0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1,                       \
+  /* Floating-point registers.  */                                      \
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,			\
+  1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,			\
+  /* Others.  */                                                        \
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1					\
+}
 
 /* Internal macros to classify a register number as to whether it's a
    general purpose register, a floating point register, a
