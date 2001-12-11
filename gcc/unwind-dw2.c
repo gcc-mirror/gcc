f0451e26f99676ad8fc7069b8788446e9eec2c3b
Jakub Jelinek <jakub@redhat.com>
2001-12-11 18:27:14 +0100

    * unwind-dw2.c (execute_cfa_program): Fix DW_CFA_restore handling.
    From-SVN: r47881

--- gcc/unwind-dw2.c
+++ gcc/unwind-dw2.c
@@ -711,9 +711,9 @@ execute_cfa_program (const unsigned char *insn_ptr,
       _Unwind_Word reg, utmp;
       _Unwind_Sword offset, stmp;
 
-      if (insn & DW_CFA_advance_loc)
+      if ((insn & 0xc0) == DW_CFA_advance_loc)
 	fs->pc += (insn & 0x3f) * fs->code_align;
-      else if (insn & DW_CFA_offset)
+      else if ((insn & 0xc0) == DW_CFA_offset)
 	{
 	  reg = insn & 0x3f;
 	  insn_ptr = read_uleb128 (insn_ptr, &utmp);
@@ -721,7 +721,7 @@ execute_cfa_program (const unsigned char *insn_ptr,
 	  fs->regs.reg[reg].how = REG_SAVED_OFFSET;
 	  fs->regs.reg[reg].loc.offset = offset;
 	}
-      else if (insn & DW_CFA_restore)
+      else if ((insn & 0xc0) == DW_CFA_restore)
 	{
 	  reg = insn & 0x3f;
 	  fs->regs.reg[reg].how = REG_UNSAVED;
