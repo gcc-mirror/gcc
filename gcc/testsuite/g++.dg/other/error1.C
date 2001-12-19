01b5d7867b2c90c6ebb04076710dc7f4dea26d74
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-12-19 11:52:59 +0000

    cp: 	PR c++/90 	* typeck.c (build_function_call_real): Use original function 	expression for errors. testsuite: 	* g++.dg/other/error1.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@48176 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/other/error1.C
@@ -0,0 +1,15 @@
+// { dg-do compile }
+
+// Copyright (C) 2001 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 18 Dec 2001 <nathan@nathan@codesourcery.com>
+
+// PR 90, stupid error message `(this + 160)'
+
+class foo {
+  public:
+  int fudge[40];
+  int bar [40];
+  inline int access(int i) {
+    return bar(i);  // { dg-error "`this->foo::bar' cannot" "" }
+  }
+};
