0ba28a6d484b7aab09c57af8d36e3b789a443c3f
Richard Henderson <rth@redhat.com>
2001-12-21 11:14:46 -0800

    wtr-aggr-init-1.c: Test that __extension__ disables then re-enables the warning.
            * gcc.dg/wtr-aggr-init-1.c: Test that __extension__ disables then
            re-enables the warning.
    
    From-SVN: r48248

--- gcc/testsuite/gcc.dg/wtr-aggr-init-1.c
+++ gcc/testsuite/gcc.dg/wtr-aggr-init-1.c
@@ -20,19 +20,29 @@ testfunc1 (void)
   static struct foo f4 = { 0, 0 };
   
   f3 = f4;
+
+  __extension__ ({
+    struct foo f5 = { 0, 0 }; /* { dg-bogus "traditional C rejects automatic" "__extension__ disables warnings" } */
+    f5.i = 0;
+  });
+
+  {
+    struct foo f6 = { 0, 0 }; /* { dg-warning "traditional C rejects automatic" "__extension__ reenables warnings" } */
+    f6.i = 0;
+  }
 }
   
-# 26 "sys-header.h" 3
+# 35 "sys-header.h" 3
 /* We are in system headers now, no -Wtraditional warnings should issue.  */
 
-struct foo f5 = { 0, 0 };
-static struct foo f6 = { 0, 0 };
+struct foo f7 = { 0, 0 };
+static struct foo f8 = { 0, 0 };
 
 void
 testfunc2 (void)
 {
-  struct foo f7 = { 0, 0 };
-  static struct foo f8 = { 0, 0 };
+  struct foo f9 = { 0, 0 };
+  static struct foo f10 = { 0, 0 };
   
-  f7 = f8;
+  f9 = f10;
 }
