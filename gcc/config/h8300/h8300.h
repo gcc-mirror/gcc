4873dbcaf4010fc9f518d6def524cf418e0e6303
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-12-22 02:41:38 +0000

    * config/h8300/h8300.h: Fix comment formatting.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@48262 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/h8300/h8300.h
+++ gcc/config/h8300/h8300.h
@@ -181,7 +181,7 @@ extern int target_flags;
 /* Target machine storage layout */
 
 /* Define to use software floating point emulator for REAL_ARITHMETIC and
-   decimal <-> binary conversion. */
+   decimal <-> binary conversion.  */
 #define REAL_ARITHMETIC
 
 /* Define this if most significant bit is lowest numbered
