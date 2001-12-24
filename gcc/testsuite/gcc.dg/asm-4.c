f9477390b376afc765528e92fc3f115f898cab93
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-12-24 06:15:31 +0000

    * stmt.c (resolve_operand_names): Handle operand modifiers.
            * gcc.dg/asm-4.c: Test operand modifiers.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@48299 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/asm-4.c
+++ gcc/testsuite/gcc.dg/asm-4.c
@@ -3,12 +3,14 @@
 
 int main()
 {
-  int x;
+  int x, y, z;
 
   asm volatile ("test0 X%0Y%[arg]Z" : [arg] "=g" (x));
-  asm volatile ("test1 X%[out]Y%[in]Z" : [out] "=g" (x) : [in] "0"(x));
+  asm volatile ("test1 X%[out]Y%[in]Z" : [out] "=g" (y) : [in] "0"(y));
+  asm volatile ("test2 X%a0Y%a[arg]Z" : : [arg] "p" (&z));
 }
 
 /* ??? Someone explain why the back reference dosn't work.  */
 /* { dontdg-final { scan-assembler "test0 X(.*)Y\1Z" } } */
 /* { dontdg-final { scan-assembler "test1 X(.*)Y\1Z" } } */
+/* { dontdg-final { scan-assembler "test2 X(.*)Y\1Z" } } */
