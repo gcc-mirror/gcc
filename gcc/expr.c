9413de453da4c94d53f3fa21592643a7d6c311bc
Richard Kenner <kenner@vlsi1.ultra.nyu.edu>
2001-12-29 21:05:22 +0000

    * expr.c (convert_move): If -fforce-mem, force FROM to not be memory.
    From-SVN: r48385

--- gcc/expr.c
+++ gcc/expr.c
@@ -1052,6 +1052,9 @@ convert_move (to, from, unsignedp)
       if ((code = can_extend_p (to_mode, from_mode, unsignedp))
 	  != CODE_FOR_nothing)
 	{
+	  if (flag_force_mem)
+	    from = force_not_mem (from);
+
 	  emit_unop_insn (code, to, from, equiv_code);
 	  return;
 	}
