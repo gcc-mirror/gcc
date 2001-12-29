545614570cb5ee234331fc93c562eb146dbba579
Jakub Jelinek <jakub@redhat.com>
2001-12-29 21:01:15 +0100

    cfglayout.c (insert_intra_before_1): New.
    	* cfglayout.c (insert_intra_before_1): New.
    	(insert_inter_bb_scope_notes): Emit sibling block notes which don't
    	span multiple basic blocks.
    
    	* gcc.dg/debug-3.c: New test.
    	* gcc.dg/debug-4.c: New test.
    	* gcc.dg/debug-5.c: New test.
    
    From-SVN: r48380

---
+++ gcc/testsuite/gcc.dg/debug-3.c
@@ -0,0 +1,35 @@
+/* This testcase failed, because scope containing baz was deleted
+   (spanned 0 basic blocks) and DWARF-2 couldn't find baz origin.  */
+/* { dg-do compile } */
+/* { dg-options "-O3 -g" } */
+
+struct A { char *a, *b, *c, *d; };
+
+static int
+bar (struct A *x)
+{
+  return x->c - x->b;
+}
+
+void fnptr (void (*fn) (void));
+
+void
+foo (void)
+{
+  struct A e;
+
+  {
+    void baz (void)
+      {
+	bar (&e);
+      }
+    fnptr (baz);
+  }
+  {
+    struct A *f;
+
+    f = &e;
+    if (f->c - f->a > f->d - f->a)
+      f->c = f->d;
+  }
+}
