2da2b957da8722b43b5ffdd1bdff2b653c8d371e
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-01-08 04:52:36 +0000

    * testsuite/gcc.dg/altivec-4.c: Test altivec predicates.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@48623 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/altivec-4.c
+++ gcc/testsuite/gcc.dg/altivec-4.c
@@ -1,10 +1,33 @@
 /* { dg-do compile { target powerpc-*-* } } */
 /* { dg-options "-maltivec -O0 -Wall" } */
 
+#define vector __attribute__((vector_size(16)))
+
 int __attribute__((mode(V4SI))) x, y;
+
+vector int i,j,k;
+vector short s,t,u;
+vector char c,d,e;
+vector float f,g,h;
 
 void
 b()
 {
   __builtin_altivec_vadduwm (x, y);
+
+  /* Make sure the predicates accept correct argument types.  */
+  
+  k = __builtin_altivec_vcmpbfp_p (f, g);
+  k = __builtin_altivec_vcmpeqfp_p (f, g);
+  k = __builtin_altivec_vcmpequb_p (c, d);
+  k = __builtin_altivec_vcmpequh_p (s, t);
+  k = __builtin_altivec_vcmpequw_p (i, j);
+  k = __builtin_altivec_vcmpgefp_p (f, g);
+  k = __builtin_altivec_vcmpgtfp_p (f, g);
+  k = __builtin_altivec_vcmpgtsb_p (c, d);
+  k = __builtin_altivec_vcmpgtsh_p (s, t);
+  k = __builtin_altivec_vcmpgtsw_p (i, j);
+  k = __builtin_altivec_vcmpgtub_p (c, d);
+  k = __builtin_altivec_vcmpgtuh_p (s, t);
+  k = __builtin_altivec_vcmpgtuw_p (i, j);
 }
