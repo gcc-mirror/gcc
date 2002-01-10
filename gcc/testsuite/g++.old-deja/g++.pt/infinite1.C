6c21456ca24d42a4ec2c20131945d13b8f075f6d
Jason Merrill <jason@gcc.gnu.org>
2002-01-10 16:03:45 -0500

    specify template depth
    From-SVN: r48753

--- gcc/testsuite/g++.old-deja/g++.pt/infinite1.C
+++ gcc/testsuite/g++.old-deja/g++.pt/infinite1.C
@@ -1,6 +1,8 @@
 // Test for catching infinitely recursive instantiations.
 // Origin: Jason Merrill <jason@redhat.com>
 
+// Special g++ Options: -ftemplate-depth-10
+
 template <int i> void f()
 {
   f<i+1>();			// ERROR - excessive recursion
@@ -10,7 +12,7 @@ template <int i> void f()
 // error first about the recursive template instantions.  But, in case
 // the compiler fails to catch the error, this will keep it from
 // running forever instantiating more and more templates.
-template <> void f<100>();
+template <> void f<11>();
 
 int main()
 {
