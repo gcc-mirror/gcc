10bffebae1d05faebac69d6dbe3ffa0720507c97
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-01-10 02:54:12 +0000

    2002-01-08  Aldy Hernandez  <aldyh@redhat.com>
            * testuite/gcc.dg/altivec-4.c: Add test for mtvscr, dssall,
            mfvscr, dss, lvsl, lvsr, dstt, dstst, dststt, dst.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@48709 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/altivec-4.c
+++ gcc/testsuite/gcc.dg/altivec-4.c
@@ -3,12 +3,18 @@
 
 #define vector __attribute__((vector_size(16)))
 
-int __attribute__((mode(V4SI))) x, y;
+static int __attribute__((mode(V4SI))) x, y;
 
-vector int i,j,k;
-vector short s,t,u;
-vector signed char c,d,e;
-vector float f,g,h;
+static vector signed int i,j,k;
+static vector signed short s,t;
+static vector signed char c,d;
+static vector float f,g;
+
+static vector unsigned char uc;
+
+static vector signed int *pi;
+
+static int int1, int2;
 
 void
 b()
@@ -30,4 +36,17 @@ b()
   k = __builtin_altivec_vcmpgtub_p (c, d);
   k = __builtin_altivec_vcmpgtuh_p (s, t);
   k = __builtin_altivec_vcmpgtuw_p (i, j);
+
+  __builtin_altivec_mtvscr (i);
+  __builtin_altivec_dssall ();
+  s = __builtin_altivec_mfvscr ();
+  __builtin_altivec_dss (3);
+
+  __builtin_altivec_dst (pi, int1 + int2, 3);
+  __builtin_altivec_dstst (pi, int1 + int2, 3);
+  __builtin_altivec_dststt (pi, int1 + int2, 3);
+  __builtin_altivec_dstt (pi, int1 + int2, 3);
+
+  uc = (vector unsigned char) __builtin_altivec_lvsl (int1 + 69, pi);
+  uc = (vector unsigned char) __builtin_altivec_lvsr (int1 + 69, pi);
 }
