651673c26671b23ff40b4ee35b488a8de6766b00
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-01-14 23:55:52 +0000

    * config/h8300/h8300-protos.h: Remove the prototype for 	eq_operator. 	* config/h8300/h8300.c (eq_operator): Remove.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@48857 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/h8300/h8300-protos.h
+++ gcc/config/h8300/h8300-protos.h
@@ -54,7 +54,6 @@ extern int small_call_insn_operand PARAMS ((rtx, enum machine_mode));
 extern int jump_address_operand PARAMS ((rtx, enum machine_mode));
 extern int bit_operand PARAMS ((rtx, enum machine_mode));
 extern int bit_memory_operand PARAMS ((rtx, enum machine_mode));
-extern int eq_operator PARAMS ((rtx, enum machine_mode));
 extern int bit_operator PARAMS ((rtx, enum machine_mode));
 extern int nshift_operator PARAMS ((rtx, enum machine_mode));
 
