61448eb733648aefa46b59c46075eccc5dd6b081
janis <janis@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-01-15 17:30:28 +0000

    * gcc.misc-tests/i386-prefetch.exp: New. 	* gcc.misc-tests/i386-pf-3dnow-1.c: New test. 	* gcc.misc-tests/i386-pf-athlon-1.c: New test. 	* gcc.misc-tests/i386-pf-none-1.c: New test. 	* gcc.misc-tests/i386-pf-sse-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@48876 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.misc-tests/i386-pf-none-1.c
@@ -0,0 +1,26 @@
+/* Test that data prefetch instructions are not generated for i386 variants
+   that do not support those instructions.  */
+
+/* { dg-do compile { target i?86-*-* } } */
+
+char *msg = "howdy there";
+
+void foo (char *p)
+{
+  __builtin_prefetch (p, 0, 0);
+  __builtin_prefetch (p, 0, 1);
+  __builtin_prefetch (p, 0, 2);
+  __builtin_prefetch (p, 0, 3);
+  __builtin_prefetch (p, 1, 0);
+  __builtin_prefetch (p, 1, 1);
+  __builtin_prefetch (p, 1, 2);
+  __builtin_prefetch (p, 1, 3);
+}
+
+int main ()
+{
+  foo (msg);
+  exit (0);
+}
+
+/* { dg-final { scan-assembler-not "fetch" } } */
