def9746c1382cf005fed7a99ac2d4f00277e363a
Benjamin Kosnik <bkoz@redhat.com>
2002-01-16 06:11:41 +0000

    num_put_members_wchar_t.cc (test02): Same.
    2002-01-15  Benjamin Kosnik  <bkoz@redhat.com>
    
    	* testsuite/22_locale/num_put_members_wchar_t.cc (test02): Same.
    	* testsuite/22_locale/num_put_members_char.cc (test01): Tweak.
    	* testsuite/22_locale/ctype_members_char.cc: Tweak.
    
    From-SVN: r48902

--- libstdc++-v3/testsuite/22_locale/ctype_members_char.cc
+++ libstdc++-v3/testsuite/22_locale/ctype_members_char.cc
@@ -1,6 +1,6 @@
 // 2000-02-16 bkoz
 
-// Copyright (C) 2000 Free Software Foundation, Inc.
+// Copyright (C) 2000, 2001, 2002 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -31,8 +31,6 @@
 
 #include <locale>
 #include <vector>
-//#include <iostream>
-//#include <iomanip>
 #include <testsuite_hooks.h>
 
 // XXX This test (test02) is not working for non-glibc locale models.
