4cd533411e92177f1e3a997b58cdbe7a825cf215
Tom Tromey <tromey@redhat.com>
2002-01-16 23:56:25 +0000

    Canvas.java (serialVersionUID): New constant.
    	* java/awt/Canvas.java (serialVersionUID): New constant.
    	Updated copyright.  Added javadoc from Classpath.
    	* java/awt/ItemSelectable.java: Replaced with Classpath version.
    
    From-SVN: r48934

--- libjava/java/awt/Canvas.java
+++ libjava/java/awt/Canvas.java
@@ -1,21 +1,42 @@
-/* Copyright (C) 2000  Free Software Foundation
+/* Copyright (C) 1999, 2000, 2002  Free Software Foundation
 
-   This file is part of libgcj.
+This file is part of GNU Classpath.
 
-This software is copyrighted work licensed under the terms of the
-Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
-details.  */
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+As a special exception, if you link this library with other files to
+produce an executable, this library does not by itself cause the
+resulting executable to be covered by the GNU General Public License.
+This exception does not however invalidate any other reasons why the
+executable file might be covered by the GNU General Public License. */
+
 
 package java.awt;
 
 import java.awt.peer.ComponentPeer;
 
-public class Canvas extends Component
+public class Canvas extends Component implements java.io.Serializable
 {
   transient GraphicsConfiguration graphicsConfiguration;
-  
+
+  /**
+   * Initializes a new instance of <code>Canvas</code>.
+   */
   public Canvas() { }
-  
+
   public Canvas(GraphicsConfiguration graphicsConfiguration)
   {
     this.graphicsConfiguration = graphicsConfiguration;
@@ -28,17 +49,21 @@ public class Canvas extends Component
     return super.getGraphicsConfigurationImpl();
   }
 
+  /**
+   * Creates the native peer for this object.
+   */
   public void addNotify()
   {
     if (peer == null)
-      {
-	peer = (ComponentPeer) getToolkit().createCanvas(this);
-      }
+      peer = (ComponentPeer) getToolkit().createCanvas(this);
     super.addNotify();
   }
 
-  /** Override this to create components with custom painting.
-      Defaults to filling the component with the background color. */
+  /**
+   * Repaints the canvas window.  This method should be overriden by 
+   * a subclass to do something useful, as this method simply paints
+   * the window with the background color.
+   */
   public void paint(Graphics gfx)
   {
     /* This implementation doesn't make much sense since the filling
@@ -49,4 +74,7 @@ public class Canvas extends Component
     Dimension size = getSize();
     gfx.fillRect(0, 0, size.width, size.height);
   }
+
+  // Serialization constant
+  private static final long serialVersionUID = -2284879212465893870L;
 }
