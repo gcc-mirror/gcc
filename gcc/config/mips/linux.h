3cbae491002e462f1037720e9e739fbb17fecbae
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-01-17 01:37:52 +0000

    2002-01-16  H.J. Lu <hjl@gnu.org>
    	* config/mips/linux.h: Include "mips/abi64.h".
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@48937 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/mips/linux.h
+++ gcc/config/mips/linux.h
@@ -19,6 +19,7 @@ the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
 #include "mips/mips.h"
+#include "mips/abi64.h"
 
 #undef WCHAR_TYPE
 #define WCHAR_TYPE "int"
