c56664e09e68fda5815501bbfe0cbbb206c2b691
Tom Tromey <tromey@redhat.com>
2002-01-17 17:35:28 +0000

    AWTError.java: Replaced with Classpath version.
    	* java/awt/AWTError.java: Replaced with Classpath version.
    	* java/awt/AWTException.java: Replaced with Classpath version.
    	* java/awt/IllegalComponentStateException.java: Replaced with
    	Classpath version.
    
    From-SVN: r48961

--- libjava/java/awt/AWTError.java
+++ libjava/java/awt/AWTError.java
@@ -1,25 +1,51 @@
-/* Copyright (C) 2000  Free Software Foundation
+/* AWTError.java -- A serious AWT error occurred.
+   Copyright (C) 1999 Free Software Foundation, Inc.
 
-   This file is part of libjava.
+This file is part of GNU Classpath.
 
-This software is copyrighted work licensed under the terms of the
-Libjava License.  Please consult the file "LIBJAVA_LICENSE" for
-details.  */
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+As a special exception, if you link this library with other files to
+produce an executable, this library does not by itself cause the
+resulting executable to be covered by the GNU General Public License.
+This exception does not however invalidate any other reasons why the
+executable file might be covered by the GNU General Public License. */
+
 
 package java.awt;
 
-/** An exception of this type is thrown by AWT in some situations,
- * usually when major problems are encountered.
- * @author Tom Tromey <tromey@cygnus.com>
- * @date April 8, 2000
- */
+/**
+  * This error is thrown when a critical AWT error occurs.
+  *
+  * @author Aaron M. Renn (arenn@urbanophile.com)
+  */
 public class AWTError extends Error
 {
-  /** Make a new instance of AWTError
-   * @param s Text associated with the exception
-   */
-  public AWTError (String s)
-  {
-    super (s);
-  }
+
+/**
+  * Initializes a new instance of <code>AWTError</code> with the specified
+  * descriptive error message.
+  *
+  * @param message The descriptive error message.
+  */
+public
+AWTError(String message)
+{
+  super(message);
 }
+
+} // class AWTError 
+
--- libjava/java/awt/IllegalComponentStateException.java
+++ libjava/java/awt/IllegalComponentStateException.java
@@ -1,30 +1,64 @@
-/* Copyright (C) 2000  Free Software Foundation
+/* IllegalComponentStateException.java -- Bad component state
+   Copyright (C) 1999 Free Software Foundation, Inc.
 
-   This file is part of libjava.
+This file is part of GNU Classpath.
 
-This software is copyrighted work licensed under the terms of the
-Libjava License.  Please consult the file "LIBJAVA_LICENSE" for
-details.  */
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+As a special exception, if you link this library with other files to
+produce an executable, this library does not by itself cause the
+resulting executable to be covered by the GNU General Public License.
+This exception does not however invalidate any other reasons why the
+executable file might be covered by the GNU General Public License. */
+
 
 package java.awt;
 
 /**
- * @author Tom Tromey <tromey@cygnus.com>
- * @date April 8, 2000
- */
-
-/* Status: Believed complete and correct to JDK 1.2.  */
-
-
+  * This exception is thrown when the requested operation failed because
+  * a component was not in the proper state.
+  *
+  * @author Aaron M. Renn (arenn@urbanophile.com)
+  */
 public class IllegalComponentStateException extends IllegalStateException
 {
-  public IllegalComponentStateException ()
-  {
-    super ();
-  }
 
-  public IllegalComponentStateException (String s)
-  {
-    super (s);
-  }
+/**
+  * Initializes a new instance of <code>IllegalComponentStateException</code>
+  * with the specified detailed error message.
+  *
+  * @param message The detailed error message.
+  */
+public
+IllegalComponentStateException(String message)
+{
+  super(message);
 }
+
+/*************************************************************************/
+
+/**
+  * Initializes a new instance of <code>IllegalComponentStateException</code>
+  * with no detailed error message.
+  */
+public
+IllegalComponentStateException()
+{
+  super();
+}
+
+} // class IllegalComponentStateException
+
