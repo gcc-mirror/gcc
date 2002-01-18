97e988000d1e839e3094435856cea94a1b1185be
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-01-18 13:24:04 +0000

    also test with inlining
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@48984 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/opt/nrv2.C
@@ -0,0 +1,28 @@
+// Test for the named return value optimization, this time with inlining.
+// { dg-do run }
+// { dg-options -O2 }
+
+int c;
+int d;
+
+struct A
+{
+  A() { ++c; }
+  A(const A&) { ++c; };
+  ~A() { ++d; }
+};
+
+inline A f ()
+{
+  A a;
+  return a;
+}
+
+int main ()
+{
+  {
+    A a = f ();
+  }
+
+  return !(c == 1 && c == d);
+}
