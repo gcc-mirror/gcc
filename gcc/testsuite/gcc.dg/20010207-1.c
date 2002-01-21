32abfc26ab362c2ae6b6a2180503d987776f195d
David Billinghurst <David.Billinghurst@riotinto.com>
2002-01-21 22:58:30 +0000

    20010207-1.c: Restrict to i?86-*-[eflno]*
    2002-01-22  David Billinghurst <David.Billinghurst@riotinto.com>
    
    	* gcc.dg/20010207-1.c: Restrict to i?86-*-[eflno]*
    
    From-SVN: r49059

--- gcc/testsuite/gcc.dg/20010207-1.c
+++ gcc/testsuite/gcc.dg/20010207-1.c
@@ -1,4 +1,6 @@
-/* { dg-do compile { target i?86-*-* } } */
+/* { dg-do compile { target i?86-*-[eflno]* } } */
+/* linux, (free|net|open)bsd and elf only */
+/* cygwin and mingw32, for example, don't support dwarf-2 */
 /* { dg-options "-O3 -gdwarf-2" } */
 
 extern inline int f1 (void) {return f2();}
