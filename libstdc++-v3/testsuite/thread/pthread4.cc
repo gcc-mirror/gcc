a68d024dfcfb284d0ab6ce809f1ff691049a893b
David Billinghurst <David.Billinghurst@riotinto.com>
2002-01-25 16:05:48 +0000

    pthread1.cc: Enable on cygwin.
    	* testsuite/thread/pthread1.cc: Enable on cygwin.
    	* testsuite/thread/pthread2.cc: Likewise.
    	* testsuite/thread/pthread3.cc: Likewise.
    	* testsuite/thread/pthread4.cc: Likewise.
    	* testsuite/thread/pthread5.cc: Likewise.
    	* testsuite/thread/pthread6.cc: Likewise.
    
    From-SVN: r49214

--- libstdc++-v3/testsuite/thread/pthread4.cc
+++ libstdc++-v3/testsuite/thread/pthread4.cc
@@ -20,7 +20,7 @@
 // Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
 // USA.
 
-// { dg-do run { target *-*-freebsd* *-*-linux* *-*-solaris* } }
+// { dg-do run { target *-*-freebsd* *-*-linux* *-*-solaris* *-*-cygwin } }
 // { dg-options "-pthread" { target *-*-freebsd* *-*-linux* } }
 // { dg-options "-pthreads" { target *-*-solaris* } }
 
@@ -36,7 +36,11 @@ using namespace std;
 static list<string> foo;
 static pthread_mutex_t fooLock = PTHREAD_MUTEX_INITIALIZER;
 static unsigned max_size = 10;
+#if defined(__CYGWIN__)
+static int iters = 10000;
+#else
 static int iters = 1000000;
+#endif
 
 void*
 produce (void*)
--- libstdc++-v3/testsuite/thread/pthread5.cc
+++ libstdc++-v3/testsuite/thread/pthread5.cc
@@ -20,7 +20,7 @@
 // Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
 // USA.
 
-// { dg-do run { target *-*-freebsd* *-*-linux* *-*-solaris* } }
+// { dg-do run { target *-*-freebsd* *-*-linux* *-*-solaris* *-*-cygwin } }
 // { dg-options "-pthread" { target *-*-freebsd* *-*-linux* } }
 // { dg-options "-pthreads" { target *-*-solaris* } }
 
