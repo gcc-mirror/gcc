1bf8640cba0953e0ec177f5273601ddd95fc1a8c
David Billinghurst <David.Billinghurst@riotinto.com>
2002-01-27 01:39:36 +0000

    f77-edit-i-in.f: New test
    2002-01-26  David Billinghurst  <David.Billinghurst@riotinto.com>
    
    	* g77.dg/f77-edit-i-in.f: New test
    	* g77.dg/f77-edit-i-out.f: New test
    
    From-SVN: r49257

---
+++ gcc/testsuite/g77.dg/f77-edit-i-in.f
@@ -0,0 +1,24 @@
+C Test Fortran 77 I edit descriptor for input
+C      (ANSI X3.9-1978 Section 13.5.9.1)
+C
+C Origin: David Billinghurst <David.Billinghurst@riotinto.com>
+C
+C { dg-do run }
+      integer i,j
+
+      open(unit=10,status='SCRATCH')
+      write(10,'(A)') '1'
+      write(10,'(A)') ' '
+      write(10,'(A)') '   -1'
+
+      rewind(10)
+
+      read(10,'(I1)') i
+      if ( i.ne.1 ) call abort()
+      read(10,'(I1)') i
+      if ( i.ne.0 ) call abort()
+      read(10,'(I2,X,I2)') i,j
+      if ( i.ne.0 ) call abort()
+      if ( j.ne.-1 ) call abort()
+
+      end
