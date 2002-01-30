84d553151f7bccfef1fa50de4c2883bd6a9c3cae
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-01-30 03:39:56 +0000

    * gcc.c-torture/execute/991118-1.c: Cast various constants back         to long long.         * gcc.c-torture/execute/bf64-1.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@49336 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/bf64-1.c
+++ gcc/testsuite/gcc.c-torture/execute/bf64-1.c
@@ -26,15 +26,15 @@ sub2 (struct tmp2 tmp2)
 
 main()
 {
-  struct tmp tmp = {0x123, 0xFFF000FFF000FLL};
-  struct tmp2 tmp2 = {0xFFF000FFF000FLL, 0x123};
+  struct tmp tmp = {0x123, (long long)0xFFFFFF000FFF000F};
+  struct tmp2 tmp2 = {(long long)0xFFFFFF000FFF000F, 0x123};
 
   tmp = sub (tmp);
   tmp2 = sub2 (tmp2);
 
-  if (tmp.pad != 0x123 || tmp.field != 0xFFFFFF541FFF567FLL)
+  if (tmp.pad != 0x123 || tmp.field != (long long)0xFFFFFF541FFF567F)
     abort ();
-  if (tmp2.pad != 0x123 || tmp2.field != 0xFFFFFF541FFF567FLL)
+  if (tmp2.pad != 0x123 || tmp2.field != (long long)0xFFFFFF541FFF567F)
     abort ();
   exit (0);
 }
