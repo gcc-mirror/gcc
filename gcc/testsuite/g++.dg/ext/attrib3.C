08ef898c1dfd9f164a555c908444189f11a149ec
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-02-01 19:15:58 +0000

    use unused attribute for the parm
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@49403 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ext/attrib3.C
+++ gcc/testsuite/g++.dg/ext/attrib3.C
@@ -1,16 +1,17 @@
 // Test that attributes work in a variety of situations.
+// { dg-options -Wunused }
 // { dg-do run }
 
 #define attrib __attribute ((mode (QI)))
+#define attrib2 __attribute ((unused))
 
 attrib signed int a;		// attributes before type are broken
-attrib unsigned int b;
+static attrib unsigned int b;
 
-int foo(attrib int o)		// attribute arguments are broken
+int foo(attrib2 int o)		// attribute arguments are broken
 {
   return (sizeof (a) != 1
 	  || sizeof (b) != 1
-	  || sizeof (o) != 1
 	  || sizeof ((attrib signed int) b) != 1);
 }
 
