f17d060eb7e43a8893aac95aaffe015a192b03b5
Jason Merrill <jason@gcc.gnu.org>
2002-02-01 14:19:50 -0500

    link, expect link errors
    From-SVN: r49404

--- gcc/testsuite/g++.old-deja/g++.brendan/union1.C
+++ gcc/testsuite/g++.old-deja/g++.brendan/union1.C
@@ -1,7 +1,10 @@
-// Build don't link: 
 // GROUPS passed unions
 // This is fixed by finish_anon_union doing a string_cst_equal check on
 // the DECL_SIZE of the decls, instead of comparing the DECL_SIZE nodes.
+
+// The compiler currently fails to allocate space for the static union.
+// excess errors test - XFAIL *-*-*
+
      unsigned
      hash(const double d)
      {
@@ -12,3 +15,8 @@
         asdouble = d;
         return asint[0] ^ asint[1];
      }
+
+int main ()
+{
+  hash (3.1415);
+}
