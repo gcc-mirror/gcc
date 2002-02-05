ca33b868ea1fe9b7202647264b15a35afd1b32eb
pme <pme@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-02-05 00:14:37 +0000

    2002-02-04  Phil Edwards  <pme@gcc.gnu.org>
    	* docs/doxygen/TODO:  Impl-defined behavior now documented...
    	* docs/html/17_intro/howto.html:  ...here.
    	* docs/doxygen/mainpage.doxy:  Remove, rename...
    	* docs/doxygen/mainpage.html:  ...to this.  Tweak HTML, add license.
    	* docs/doxygen/style.css:  Add small text.
    	* docs/doxygen/run_doxygen:  Adjust for new mainpage.
    	* docs/doxygen/user.cfg.in:  Likewise.
    
    2002-02-04  Stephan Buys  <s.buys@icon.co.za>
    
    	* include/bits/stl_map.h:  Initial doxygen markup.
    	* include/std/std_fstream.h:  Initial doxygen markup.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@49502 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/stl_map.h
+++ libstdc++-v3/include/bits/stl_map.h
@@ -1,6 +1,6 @@
 // Map implementation -*- C++ -*-
 
-// Copyright (C) 2001 Free Software Foundation, Inc.
+// Copyright (C) 2001, 2002 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -66,6 +66,14 @@
 namespace std
 {
 
+/**
+ *  @brief A standard container made up of pairs (see std::pair in <utility>)
+ *         which can be retrieved based on a key.
+ *
+ *  This is an associative container.  Values contained within it can be
+ *  quickly retrieved through a key element.  Example:  MyMap["First"] would
+ *  return the data associated with the key "First".
+*/
 template <class _Key, class _Tp, class _Compare = less<_Key>,
           class _Alloc = allocator<pair<const _Key, _Tp> > >
 class map
@@ -81,7 +89,7 @@ public:
   typedef _Tp                   mapped_type;
   typedef pair<const _Key, _Tp> value_type;
   typedef _Compare             key_compare;
-    
+
   class value_compare
     : public binary_function<value_type, value_type, bool> {
   friend class map<_Key,_Tp,_Compare,_Alloc>;
@@ -95,7 +103,7 @@ public:
   };
 
 private:
-  typedef _Rb_tree<key_type, value_type, 
+  typedef _Rb_tree<key_type, value_type,
                    _Select1st<value_type>, key_compare, _Alloc> _Rep_type;
   _Rep_type _M_t;  // red-black tree representing map
 public:
@@ -133,7 +141,7 @@ public:
   operator=(const map<_Key, _Tp, _Compare, _Alloc>& __x)
   {
     _M_t = __x._M_t;
-    return *this; 
+    return *this;
   }
 
   // accessors:
@@ -142,17 +150,73 @@ public:
   value_compare value_comp() const { return value_compare(_M_t.key_comp()); }
   allocator_type get_allocator() const { return _M_t.get_allocator(); }
 
+  /**
+   *  Returns a read/write iterator that points to the first pair in the map.
+   *  Iteration is done in ascending order according to the keys.
+  */
   iterator begin() { return _M_t.begin(); }
+
+  /**
+   *  Returns a read-only (constant) iterator that points to the first pair
+   *  in the map.  Iteration is done in ascending order according to the keys.
+  */
   const_iterator begin() const { return _M_t.begin(); }
+
+  /**
+   *  Returns a read/write iterator that points one past the last pair in the
+   *  map.  Iteration is done in ascending order according to the keys.
+  */
   iterator end() { return _M_t.end(); }
+
+  /**
+   *  Returns a read-only (constant) iterator that points one past the last
+   *  pair in the map.  Iteration is done in ascending order according to the
+   *  keys.
+  */
   const_iterator end() const { return _M_t.end(); }
+
+  /**
+   *  Returns a read/write reverse iterator that points to the last pair in
+   *  the map.  Iteration is done in descending order according to the keys.
+  */
   reverse_iterator rbegin() { return _M_t.rbegin(); }
+
+  /**
+   *  Returns a read-only (constant) reverse iterator that points to the last
+   *  pair in the map.  Iteration is done in descending order according to
+   *  the keys.
+  */
   const_reverse_iterator rbegin() const { return _M_t.rbegin(); }
+
+  /**
+   *  Returns a read/write reverse iterator that points to one before the
+   *  first pair in the map.  Iteration is done in descending order according
+   *  to the keys.
+  */
   reverse_iterator rend() { return _M_t.rend(); }
+
+  /**
+   *  Returns a read-only (constant) reverse iterator that points to one
+   *  before the first pair in the map.  Iteration is done in descending order
+   *  according to the keys.
+  */
   const_reverse_iterator rend() const { return _M_t.rend(); }
+
+  /** Returns true if the map is empty.  (Thus begin() would equal end().)  */
   bool empty() const { return _M_t.empty(); }
+  /** Returns the size of the map.  */
   size_type size() const { return _M_t.size(); }
+  /** Returns the maximum size of the map.  */
   size_type max_size() const { return _M_t.max_size(); }
+
+  /**
+   *  @brief Subscript ( [] ) access to map data.
+   *  @param  k  The key for which data should be retrieved.
+   *  Allows for easy lookup with the subscript ( [] ) operator.  Returns the
+   *  data associated with the key specified in subscript.  If the key does
+   *  not exist a pair with that key is created with a default value, which
+   *  is then returned.
+  */
   _Tp& operator[](const key_type& __k) {
     iterator __i = lower_bound(__k);
     // __i->first is greater than or equivalent to __k.
@@ -160,44 +224,216 @@ public:
       __i = insert(__i, value_type(__k, _Tp()));
     return (*__i).second;
   }
+
   void swap(map<_Key,_Tp,_Compare,_Alloc>& __x) { _M_t.swap(__x._M_t); }
 
   // insert/erase
-
-  pair<iterator,bool> insert(const value_type& __x) 
+  /**
+   *  @brief Attempts to insert a std::pair into the map.
+   *  @param  x  Pair to be inserted (see std::make_pair for easy creation of
+   *             pairs).
+   *  @return  A pair of which the first element is an iterator that points
+   *           to the possibly inserted pair, a second element of type bool
+   *           to show if the pair was actually inserted.
+   *
+   *  This function attempts to insert a (key, value) pair into the map.  A
+   *  map relies on unique keys and thus a pair is only inserted if its first
+   *  element (the key) is not already present in the map.
+  */
+  pair<iterator,bool> insert(const value_type& __x)
     { return _M_t.insert_unique(__x); }
+
+  /**
+   *  @brief Attempts to insert a std::pair into the map.
+   *  @param  position  An iterator that serves as a hint as to where the
+   *                    pair should be inserted.
+   *  @param  x  Pair to be inserted (see std::make_pair for easy creation of
+   *             pairs).
+   *  @return  An iterator that points to the inserted (key,value) pair.
+   *
+   *  This function is not concerned about whether the insertion took place
+   *  or not and thus does not return a boolean like the single-argument
+   *  insert() does.  Note that the first parameter is only a hint and can
+   *  potentially improve the performance of the insertion process.  A bad
+   *  hint would cause no gains in efficiency.
+  */
   iterator insert(iterator position, const value_type& __x)
     { return _M_t.insert_unique(position, __x); }
+
+  /**
+   *  @brief A template function that attemps to insert elements from
+   *         another range (possibly another map).
+   *  @param  first  Iterator pointing to the start of the range to be inserted.
+   *  @param  last  Iterator pointing to the end of the range.
+  */
   template <class _InputIterator>
   void insert(_InputIterator __first, _InputIterator __last) {
     _M_t.insert_unique(__first, __last);
   }
 
+  /**
+   *  @brief Erases an element from a map.
+   *  @param  position  An iterator pointing to the element to be erased.
+   *
+   *  This function erases an element, pointed to by the given iterator, from
+   *  a map.  Note that this function only erases the element, and that if
+   *  the element is itself a pointer, the pointed-to memory is not touched
+   *  in any way.  That is the user's responsibilty.
+  */
   void erase(iterator __position) { _M_t.erase(__position); }
+
+  /**
+   *  @brief Erases an element according to the provided key.
+   *  @param  x  Key of element to be erased.
+   *  @return  Doc me!
+   *
+   *  This function erases an element, located by the given key, from a map.
+   *  Note that this function only erases the element, and that if
+   *  the element is itself a pointer, the pointed-to memory is not touched
+   *  in any way.  That is the user's responsibilty.
+  */
   size_type erase(const key_type& __x) { return _M_t.erase(__x); }
+
+  /**
+   *  @brief Erases a [first,last) range of elements from a map.
+   *  @param  first  Iterator pointing to the start of the range to be erased.
+   *  @param  last  Iterator pointing to the end of the range to be erased.
+   *
+   *  This function erases a sequence of elements from a map.
+   *  Note that this function only erases the element, and that if
+   *  the element is itself a pointer, the pointed-to memory is not touched
+   *  in any way.  That is the user's responsibilty.
+  */
   void erase(iterator __first, iterator __last)
     { _M_t.erase(__first, __last); }
+
+  /** Erases all elements in a map.  */
   void clear() { _M_t.clear(); }
 
   // map operations:
 
+  /**
+   *  @brief Tries to locate an element in a map.
+   *  @param  x  Key of (key, value) pair to be located.
+   *  @return  Iterator pointing to sought-after element, or end() if not
+   *           found.
+   *
+   *  This function takes a key and tries to locate the element with which
+   *  the key matches.  If successful the function returns an iterator
+   *  pointing to the sought after pair. If unsuccessful it returns the
+   *  one past the end ( end() ) iterator.
+  */
   iterator find(const key_type& __x) { return _M_t.find(__x); }
+
+  /**
+   *  @brief Tries to locate an element in a map.
+   *  @param  x  Key of (key, value) pair to be located.
+   *  @return  Read-only (constant) iterator pointing to sought-after
+   *           element, or end() if not found.
+   *
+   *  This function takes a key and tries to locate the element with which
+   *  the key matches.  If successful the function returns a constant iterator
+   *  pointing to the sought after pair. If unsuccessful it returns the
+   *  one past the end ( end() ) iterator.
+  */
   const_iterator find(const key_type& __x) const { return _M_t.find(__x); }
+
+  /**
+   *  @brief Finds the number of elements with given key.
+   *  @param  x  Key of (key, value) pairs to be located.
+   *  @return Number of elements with specified key.
+   *
+   *  This function only makes sense for multimaps.
+  */
   size_type count(const key_type& __x) const {
-    return _M_t.find(__x) == _M_t.end() ? 0 : 1; 
+    return _M_t.find(__x) == _M_t.end() ? 0 : 1;
   }
+
+  /**
+   *  @brief Finds the beginning of a subsequence matching given key.
+   *  @param  x  Key of (key, value) pair to be located.
+   *  @return  Iterator pointing to first element matching given key, or
+   *           end() if not found.
+   *
+   *  This function is useful only with std::multimap.  It returns the first
+   *  element of a subsequence of elements that matches the given key.  If
+   *  unsuccessful it returns an iterator pointing to the first element that
+   *  has a greater value than given key or end() if no such element exists.
+  */
   iterator lower_bound(const key_type& __x) {return _M_t.lower_bound(__x); }
+
+  /**
+   *  @brief Finds the beginning of a subsequence matching given key.
+   *  @param  x  Key of (key, value) pair to be located.
+   *  @return  Read-only (constant) iterator pointing to first element
+   *           matching given key, or end() if not found.
+   *
+   *  This function is useful only with std::multimap.  It returns the first
+   *  element of a subsequence of elements that matches the given key.  If
+   *  unsuccessful the iterator will point to the next greatest element or,
+   *  if no such greater element exists, to end().
+  */
   const_iterator lower_bound(const key_type& __x) const {
-    return _M_t.lower_bound(__x); 
+    return _M_t.lower_bound(__x);
   }
+
+  /**
+   *  @brief Finds the end of a subsequence matching given key.
+   *  @param  x  Key of (key, value) pair to be located.
+   *  @return Iterator pointing to last element matching given key.
+   *
+   *  This function only makes sense with multimaps.
+  */
   iterator upper_bound(const key_type& __x) {return _M_t.upper_bound(__x); }
+
+  /**
+   *  @brief Finds the end of a subsequence matching given key.
+   *  @param  x  Key of (key, value) pair to be located.
+   *  @return  Read-only (constant) iterator pointing to last element matching
+   *           given key.
+   *
+   *  This function only makes sense with multimaps.
+  */
   const_iterator upper_bound(const key_type& __x) const {
-    return _M_t.upper_bound(__x); 
+    return _M_t.upper_bound(__x);
   }
-  
+
+  /**
+   *  @brief Finds a subsequence matching given key.
+   *  @param  x  Key of (key, value) pairs to be located.
+   *  @return  Pair of iterators that possibly points to the subsequence
+   *           matching given key.
+   *
+   *  This function improves on lower_bound() and upper_bound() by giving a more
+   *  elegant and efficient solution.  It returns a pair of which the first
+   *  element possibly points to the first element matching the given key
+   *  and the second element possibly points to the last element matching the
+   *  given key.  If unsuccessful the first element of the returned pair will
+   *  contain an iterator pointing to the next greatest element or, if no such
+   *  greater element exists, to end().
+   *
+   *  This function only makes sense for multimaps.
+  */
   pair<iterator,iterator> equal_range(const key_type& __x) {
     return _M_t.equal_range(__x);
   }
+
+  /**
+   *  @brief Finds a subsequence matching given key.
+   *  @param  x  Key of (key, value) pairs to be located.
+   *  @return  Pair of read-only (constant) iterators that possibly points to
+   *           the subsequence matching given key.
+   *
+   *  This function improves on lower_bound() and upper_bound() by giving a more
+   *  elegant and efficient solution.  It returns a pair of which the first
+   *  element possibly points to the first element matching the given key
+   *  and the second element possibly points to the last element matching the
+   *  given key.  If unsuccessful the first element of the returned pair will
+   *  contain an iterator pointing to the next greatest element or, if no such
+   *  a greater element exists, to end().
+   *
+   *  This function only makes sense for multimaps.
+  */
   pair<const_iterator,const_iterator> equal_range(const key_type& __x) const {
     return _M_t.equal_range(__x);
   }
@@ -211,43 +447,43 @@ public:
 };
 
 template <class _Key, class _Tp, class _Compare, class _Alloc>
-inline bool operator==(const map<_Key,_Tp,_Compare,_Alloc>& __x, 
+inline bool operator==(const map<_Key,_Tp,_Compare,_Alloc>& __x,
                        const map<_Key,_Tp,_Compare,_Alloc>& __y) {
   return __x._M_t == __y._M_t;
 }
 
 template <class _Key, class _Tp, class _Compare, class _Alloc>
-inline bool operator<(const map<_Key,_Tp,_Compare,_Alloc>& __x, 
+inline bool operator<(const map<_Key,_Tp,_Compare,_Alloc>& __x,
                       const map<_Key,_Tp,_Compare,_Alloc>& __y) {
   return __x._M_t < __y._M_t;
 }
 
 template <class _Key, class _Tp, class _Compare, class _Alloc>
-inline bool operator!=(const map<_Key,_Tp,_Compare,_Alloc>& __x, 
+inline bool operator!=(const map<_Key,_Tp,_Compare,_Alloc>& __x,
                        const map<_Key,_Tp,_Compare,_Alloc>& __y) {
   return !(__x == __y);
 }
 
 template <class _Key, class _Tp, class _Compare, class _Alloc>
-inline bool operator>(const map<_Key,_Tp,_Compare,_Alloc>& __x, 
+inline bool operator>(const map<_Key,_Tp,_Compare,_Alloc>& __x,
                       const map<_Key,_Tp,_Compare,_Alloc>& __y) {
   return __y < __x;
 }
 
 template <class _Key, class _Tp, class _Compare, class _Alloc>
-inline bool operator<=(const map<_Key,_Tp,_Compare,_Alloc>& __x, 
+inline bool operator<=(const map<_Key,_Tp,_Compare,_Alloc>& __x,
                        const map<_Key,_Tp,_Compare,_Alloc>& __y) {
   return !(__y < __x);
 }
 
 template <class _Key, class _Tp, class _Compare, class _Alloc>
-inline bool operator>=(const map<_Key,_Tp,_Compare,_Alloc>& __x, 
+inline bool operator>=(const map<_Key,_Tp,_Compare,_Alloc>& __x,
                        const map<_Key,_Tp,_Compare,_Alloc>& __y) {
   return !(__x < __y);
 }
 
 template <class _Key, class _Tp, class _Compare, class _Alloc>
-inline void swap(map<_Key,_Tp,_Compare,_Alloc>& __x, 
+inline void swap(map<_Key,_Tp,_Compare,_Alloc>& __x,
                  map<_Key,_Tp,_Compare,_Alloc>& __y) {
   __x.swap(__y);
 }
