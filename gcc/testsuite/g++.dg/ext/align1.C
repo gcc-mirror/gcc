23fb1469b574e85a4f39ee3f48286d9ab6317056
Nick Clifton <nickc@cambridge.redhat.com>
2002-02-06 16:18:33 +0000

    Do not use an explicit alignment value as certain file formats cannot support particularly large alignments.
    From-SVN: r49545

--- gcc/testsuite/g++.dg/ext/align1.C
+++ gcc/testsuite/g++.dg/ext/align1.C
@@ -1,19 +1,20 @@
 // Test that __attribute__ ((aligned)) is preserved.
-// Cygwin does not support alignment greater than 16
+// The alignment used to be 64 but Cygwin does not
+// support an alignment greater than 16 and COFF 
+// not support an alignment greater than 4.
 
 extern "C" int printf (const char *, ...);
 
-typedef float at[4][4] __attribute__ ((aligned (64)));
+typedef float at[4][4] __attribute__ ((aligned));
 
 float dummy[4][4][15];
 
 static volatile at a1[15];
-// { dg-warning "object file alignment" "" { target i?86-pc-cygwin } 10 }
 
-float f1 __attribute__ ((aligned (64)));
-// { dg-warning "object file alignment" "" { target i?86-pc-cygwin } 13 }
+float f1 __attribute__ ((aligned));
 
-int main()
+int
+main (void)
 {
   printf ("%d %d\n", __alignof (a1), __alignof (f1));
   return (__alignof (a1) < __alignof (f1));
