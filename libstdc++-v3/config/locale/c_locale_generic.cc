a36f8f2b0b584be3ae7194bbc2e959a89aa9f022
ljrittle <ljrittle@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-02-06 17:31:43 +0000

    * acinclude.m4: Add C++ linkage check for strtof. 	* aclocal.m4: Rebuilt. 	* config.h.in: Rebuilt. 	* configure: Rebuilt. 	* config/locale/c_locale_generic.cc: Conditionally include 	<ieeefp.h>.  Improve handling and error checking of float 	and long double input for non-C99 configurations.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@49546 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/config/locale/c_locale_generic.cc
+++ libstdc++-v3/config/locale/c_locale_generic.cc
@@ -35,6 +35,10 @@
 
 #include <locale>
 
+#ifdef _GLIBCPP_HAVE_IEEEFP_H
+#include <ieeefp.h>
+#endif
+
 namespace std 
 {
   // Specializations for all types used in num_get.
@@ -119,10 +123,24 @@ namespace std
 	  const char* __old = setlocale(LC_ALL, "C");
 	  char* __sanity;
 	  errno = 0;
-#ifdef _GLIBCPP_USE_C99
+#if defined(_GLIBCPP_USE_C99) || defined(_GLIBCPP_HAVE_STRTOF)
 	  float __f = strtof(__s, &__sanity);
 #else
-	  float __f = static_cast<float>(strtod(__s, &__sanity));
+	  double __d = strtod(__s, &__sanity);
+	  float __f = static_cast<float>(__d);
+#ifdef _GLIBCPP_HAVE_FINITEF
+	  if (!finitef (__f))
+	    errno = ERANGE;
+#elif defined (_GLIBCPP_HAVE_FINITE)
+	  if (!finite (static_cast<double> (__f)))
+	    errno = ERANGE;
+#elif defined (_GLIBCPP_HAVE_ISINF)
+	  if (isinf (static_cast<double> (__f)))
+	    errno = ERANGE;
+#else
+	  if (fabs(__d) > numeric_limits<float>::max())
+	    errno = ERANGE;
+#endif
 #endif
           if (__sanity != __s && *__sanity == '\0' && errno != ERANGE)
 	    __v = __f;
@@ -161,7 +179,7 @@ namespace std
 	{
 	  // Assumes __s formatted for "C" locale.
 	  const char* __old = setlocale(LC_ALL, "C");
-#if defined(_GLIBCPP_USE_C99) && !defined(__hpux)
+#if defined(_GLIBCPP_USE_C99) || defined(_GLIBCPP_HAVE_STRTOLD)
 	  char* __sanity;
 	  errno = 0;
 	  long double __ld = strtold(__s, &__sanity);
@@ -170,7 +188,14 @@ namespace std
 #else
 	  typedef char_traits<char>::int_type int_type;
 	  long double __ld;
+	  errno = 0;
 	  int __p = sscanf(__s, "%Lf", &__ld);
+	  if (errno == ERANGE)
+	    __p = 0;
+#ifdef _GLIBCPP_HAVE_FINITEL
+	  if ((__p == 1) && !finitel (__ld))
+	    __p = 0;
+#endif
 	  if (__p && static_cast<int_type>(__p) != char_traits<char>::eof())
 	    __v = __ld;
 #endif
