acc4f70c946b82509366092a86a415cc001c6937
nickc <nickc@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-02-06 12:15:53 +0000

    Expect the compilation to fail because the COFF format does not support the weak attribute.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@49544 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/warn/weak1.C
+++ gcc/testsuite/g++.dg/warn/weak1.C
@@ -1,4 +1,6 @@
 // { dg-do run }
+// { dg-do compile { target *-*-coff } }
+// { dg-warning "weak declaration" "COFF format does not support weak" { target *-*-coff } 5 }
 
 extern void foo (void) __attribute__ ((weak));
 
