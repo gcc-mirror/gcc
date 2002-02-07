dae051417b720a488b8ad03f9359e2908cdc8c2a
echristo <echristo@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-02-07 23:43:23 +0000

    2002-02-07  Eric Christopher  <echristo@redhat.com>
    	* config/mips/crtn.asm: Cleanup #ifdefs.
    
    Add ChangeLog entry for previous patch that didn't seem to make it.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@49595 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/mips/crtn.asm
+++ gcc/config/mips/crtn.asm
@@ -2,41 +2,38 @@
    Return spill offset of 40 and 20.  Aligned to 16 bytes for n32.  */
 
 	.section .init,"ax",@progbits
+#ifdef	__mips16
+/* The mips16 uses $7 for a return address. We use that here too.  */
+	lw	$7,20($sp)
+	addu	$sp,$sp,32
+
+	j	$7
+#else
 #ifdef __mips64
 	ld      $31,40($sp)
 	daddu	$sp,$sp,48
 #else
-#ifndef __mips16
 	lw	$31,20($sp)
 	addu	$sp,$sp,32
-#else
-/* The mips16 uses $7 for a return address. We use that here too.  */
-	lw	$7,20($sp)
-	addu	$sp,$sp,32
-
-	j	$7
 #endif
-#endif
-#ifndef __mips16
 	j	$31
+
 #endif
 
 	.section .fini,"ax",@progbits
-#ifdef __mips64
-	ld	$31,40($sp)
-	daddu	$sp,$sp,48
-#else
-#ifndef __mips16
-	lw	$31,20($sp)
-	addu	$sp,$sp,32
-#else
+#ifdef	__mips16
 /* The mips16 uses $7 for a return address. We use that here too.  */
 	lw	$7,20($sp)
 	addu	$sp,$sp,32
 
 	j	$7
+#else
+#ifdef	__mips64
+	ld	$31,40($sp)
+	daddu	$sp,$sp,48
+#else
+	lw	$31,20($sp)
+	addu	$sp,$sp,32
 #endif
-#endif
-#ifndef __mips16
 	j	$31
 #endif
