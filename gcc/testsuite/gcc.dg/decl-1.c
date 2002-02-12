3bb91036a0175ec8a83614db24f7b0a69e4f84f0
Neil Booth <neil@daikokuya.demon.co.uk>
2002-02-12 07:36:38 +0000

    * gcc.dg/decl-1.c: New test.
    From-SVN: r49702

---
+++ gcc/testsuite/gcc.dg/decl-1.c
@@ -0,0 +1,15 @@
+/* Copyright (C) 2002 Free Software Foundation, Inc.
+
+   Source: Neil Booth, 12 Feb 2002.
+
+   In the declaration of proc, x must be parsed as a typedef name
+   (6.7.5.3 p11).  */
+
+typedef int x;
+int proc(int (x));	/* x is a typedef, param to proc is a function.  */
+int proc2(int x);	/* x is an identifier, param is an int.  */
+
+int main ()
+{
+  return proc (proc2);		/* { dg-bogus "integer from pointer" } */
+}
