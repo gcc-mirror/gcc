658311e053abebc6fa24f2863d81e802e776057e
Richard Smith <richard@ex-parrot.com>
2002-02-13 20:59:37 +0000

    * g++.old-deja/g++.other/thunk1.C: New test.
    From-SVN: r49743

---
+++ gcc/testsuite/g++.old-deja/g++.other/thunk1.C
@@ -0,0 +1,35 @@
+extern "C" void abort();
+
+int ic;
+
+struct X 
+{
+  X() { ++ic; }
+  X( const X & ) { ++ic; }
+ ~X() { if (--ic < 0) abort(); }
+};
+
+struct V 
+{
+  virtual ~V() {}
+};
+
+struct A : public virtual V 
+{
+}; 
+
+struct B : public virtual V 
+{
+  virtual void foo( X ) = 0;
+}; 
+
+struct D : public A, public virtual B 
+{
+  virtual void foo( X ) {}
+}; 
+
+int main()
+{
+  B *b = new D;
+  b->foo( X() );
+}
