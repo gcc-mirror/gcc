3c6ed5ba7af8a6f6015f7320e2b07c9e3aec2c6a
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-02-13 21:46:22 +0000

    PR optimization/5547: 	* config/i386/i386.c (i386_simplify_dwarf_addr): Simplify 	all valid IA-32 address modes involving non-scaled %ebx and 	GOT/GOTOFF as displacement.
    	* g++.dg/other/debug3.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@49745 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/other/debug3.C
@@ -0,0 +1,47 @@
+// PR optimization/5547
+// This testcase caused ICE on IA-32, since DWARF-2 was unable
+// to emit location expression for parameter a of operator+.
+// { dg-do compile }
+// { dg-options "-O2 -g -fpic" }
+
+struct A { char *s; };
+
+inline A operator+ (char a, const A &b)
+{
+  A s;
+  s.s = new char[12];
+  s.s[0] = a;
+  return s;
+}
+
+int b (const A &);
+
+void test1 (const A &x, int y)
+{
+  int j = b ("012345"[y] + x);
+  for (int i = 0; i < y; i++);
+}
+
+void test2 (const A &x, int y)
+{
+  int j = b ("012345678"[y + 2] + x);
+  for (int i = 0; i < y; i++);
+}
+
+void test3 (const A &x, int y)
+{
+  int j = b ("012345678"[y - 6] + x);
+  for (int i = 0; i < y; i++);
+}
+
+void test4 (const A &x, int y)
+{
+  int j = b ("012345678"[2 * y - 10] + x);
+  for (int i = 0; i < y; i++);
+}
+
+void test5 (const A &x, int y)
+{
+  int j = b ("012345678"[4 * y] + x);
+  for (int i = 0; i < y; i++);
+}
