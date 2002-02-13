8c8d0a565ed726d5e3e8a17cc4ecb54f78e93871
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-02-13 18:29:12 +0000

    2002-02-13  Benjamin Kosnik  <bkoz@redhat.com>
    	* src/localename.cc: Remove stdexcept include.
    	* src/locale-inst.cc: Same.
    	* src/locale.cc: Same.
    	* src/functexcept.cc: Remove string includes.
    	* include/std/std_bitset.h: Remove stdexcept include.
    	* testsuite/23_containers/bitset_members.cc: Add stdexcept.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@49740 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/src/functexcept.cc
+++ libstdc++-v3/src/functexcept.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2001 Free Software Foundation, Inc.
+// Copyright (C) 2001, 2002 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -28,11 +28,10 @@
 #include <bits/functexcept.h>
 #include <cstdlib>
 #include <exception>
+#include <stdexcept>
 #include <new>
 #include <typeinfo>
-#include <stdexcept>
 #include <ios>
-#include <string>
 
 namespace std 
 {
