c1de4682feb4d9d6342b022d5ac3d0f17c3e3769
megacz <megacz@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-02-24 23:17:26 +0000

    2002-02-24  Adam Megacz  <adam@xwt.org>
            * java/lang/Win32Process.java: Created a dummy class to allow
            build process to run to completion.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@50013 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/java/lang/Win32Process.java
@@ -0,0 +1,63 @@
+// Win32Process.java - Subclass of Process for Win32 systems.
+
+/* Copyright (C) 2002  Free Software Foundation
+
+   This file is part of libgcj.
+
+This software is copyrighted work licensed under the terms of the
+Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
+details.  */
+
+package java.lang;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+/**
+ * @author Adam Megacz
+ * @date Feb 24, 2002
+ */
+
+// This is entirely internal to our implementation.
+
+// This file is copied to `ConcreteProcess.java' before compilation.
+// Hence the class name apparently does not match the file name.
+final class ConcreteProcess extends Process
+{
+  public void destroy ()
+  {
+    throw new Error("not implemented");
+  }
+  
+  public int exitValue ()
+  {
+    throw new Error("not implemented");
+  }
+
+  public InputStream getErrorStream ()
+  {
+    throw new Error("not implemented");
+  }
+
+  public InputStream getInputStream ()
+  {
+    throw new Error("not implemented");
+  }
+
+  public OutputStream getOutputStream ()
+  {
+    throw new Error("not implemented");
+  }
+
+  public int waitFor () throws InterruptedException
+  {
+    throw new Error("not implemented");
+  }
+
+  public ConcreteProcess (String[] progarray, String[] envp) throws IOException
+  {
+    throw new Error("not implemented");
+  }
+
+}
