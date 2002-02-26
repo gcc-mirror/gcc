64ee51e0ba631964e33c56902d9cc07ee9c6316c
GCC Administrator <gccadmin@gcc.gnu.org>
2002-02-26 08:17:53 +0000

    Daily bump.
    From-SVN: r50053

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,4 +1,4 @@
-        #include "ansidecl.h"
+                #include "ansidecl.h"
 #include "f/version.h"
 
-const char *const ffe_version_string = "3.2 20020225 (experimental)";
+const char *const ffe_version_string = "3.2 20020226 (experimental)";
