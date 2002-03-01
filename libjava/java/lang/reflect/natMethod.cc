21956ee22085bcc0960ffecb0a63419182934cec
Anthony Green <green@redhat.com>
2002-03-01 13:41:16 +0000

    FFI_TYPE_POINTER fix
    From-SVN: r50195

--- libjava/java/lang/reflect/natMethod.cc
+++ libjava/java/lang/reflect/natMethod.cc
@@ -426,6 +426,7 @@ _Jv_CallAnyMethodA (jobject obj,
   union
   {
     ffi_arg i;
+    jobject o;
     jlong l;
     jfloat f;
     jdouble d;
@@ -448,37 +449,43 @@ _Jv_CallAnyMethodA (jobject obj,
   // a narrowing conversion for jbyte, jchar, etc. results.
   // Note that boolean is handled either by the FFI_TYPE_SINT8 or
   // FFI_TYPE_SINT32 case.
-  switch (rtype->type)
-    {
-    case FFI_TYPE_VOID:
-      break;
-    case FFI_TYPE_SINT8:
-      result->b = (jbyte)ffi_result.i;
-      break;
-    case FFI_TYPE_SINT16:
-      result->s = (jshort)ffi_result.i;
-      break;
-    case FFI_TYPE_UINT16:
-      result->c = (jchar)ffi_result.i;
-      break;
-    case FFI_TYPE_SINT32:
-      result->i = (jint)ffi_result.i;
-      break;
-    case FFI_TYPE_SINT64:
-      result->j = (jlong)ffi_result.l;
-      break;
-    case FFI_TYPE_FLOAT:
-      result->f = (jfloat)ffi_result.f;
-      break;
-    case FFI_TYPE_DOUBLE:
-      result->d = (jdouble)ffi_result.d;
-      break;
-    default:
-      JvFail ("Unknown ffi_call return type");
-      break;
-    }
   if (is_constructor)
     result->l = obj;
+  else
+    {
+      switch (rtype->type)
+	{
+	case FFI_TYPE_VOID:
+	  break;
+	case FFI_TYPE_SINT8:
+	  result->b = (jbyte)ffi_result.i;
+	  break;
+	case FFI_TYPE_SINT16:
+	  result->s = (jshort)ffi_result.i;
+	  break;
+	case FFI_TYPE_UINT16:
+	  result->c = (jchar)ffi_result.i;
+	  break;
+	case FFI_TYPE_SINT32:
+	  result->i = (jint)ffi_result.i;
+	  break;
+	case FFI_TYPE_SINT64:
+	  result->j = (jlong)ffi_result.l;
+	  break;
+	case FFI_TYPE_FLOAT:
+	  result->f = (jfloat)ffi_result.f;
+	  break;
+	case FFI_TYPE_DOUBLE:
+	  result->d = (jdouble)ffi_result.d;
+	  break;
+	case FFI_TYPE_POINTER:
+	  result->l = (jobject)ffi_result.o;
+	  break;
+	default:
+	  JvFail ("Unknown ffi_call return type");
+	  break;
+	}
+    }
 
   return ex;
 #else
