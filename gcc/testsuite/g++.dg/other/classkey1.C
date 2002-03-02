fd87b7a7099bc82b2542b169c592c0dfc997029e
Nathan Sidwell <nathan@codesourcery.com>
2002-03-02 19:51:57 +0000

    re PR c++/775 (2.97 error declaring nested class a friend)
    cp:
            PR c++/775
            * decl.c (lookup_tag): Only reject enum/class mismatch, not
            class/union mismatch.
            * parse.y (check_class_key): New function.
            (structsp): Call it.Read from remote host gcc.gnu.org: Connection reset by peer
    testsuite:
            * g++.dg/other/classkey1.C: New test.
    
    From-SVN: r50230

---
+++ gcc/testsuite/g++.dg/other/classkey1.C
@@ -0,0 +1,17 @@
+// { dg-do compile }
+
+// Copyright (C) 2002 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 28 Feb 2002 <nathan@codesourcery.com>
+
+// PR 775. Some mismatches which were broken.
+
+template <class T> struct A {};
+union A<int> a; // { dg-error "`union' tag" "" }
+
+template <> union A<float> {}; // { dg-error "`union' tag" "" }
+
+struct B {};
+union B b;	// { dg-error "`union' tag" "" }
+
+union C {};
+class C c;	// { dg-error "`class' tag" "" }
