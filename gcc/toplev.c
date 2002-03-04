974a7f567236f76df9e78fa0346b92039e49e0ed
Jan Hubicka <jh@suse.cz>
2002-03-04 15:37:45 +0100

    toplev.c (rest_of_compilation): Cleanup CFG after dead jumptables removal
    	* toplev.c (rest_of_compilation): Cleanup CFG after dead jumptables
    	removal
    
    From-SVN: r50277

--- gcc/toplev.c
+++ gcc/toplev.c
@@ -2877,6 +2877,7 @@ rest_of_compilation (decl)
     {
       timevar_push (TV_LOOP);
       delete_dead_jumptables ();
+      cleanup_cfg (CLEANUP_EXPENSIVE | CLEANUP_PRE_LOOP);
       open_dump_file (DFI_loop, decl);
       /* CFG is no longer maintained up-to-date.  */
       free_bb_for_insn ();
