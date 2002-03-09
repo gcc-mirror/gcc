a0a283d05d96daeef4015ecc066378b9ab7fc061
Richard Kenner <kenner@vlsi1.ultra.nyu.edu>
2002-03-09 12:25:15 +0000

    * combine.c (make_extraction): Don't make extension of CONST_INT.
    From-SVN: r50490

--- gcc/combine.c
+++ gcc/combine.c
@@ -6027,6 +6027,9 @@ make_extraction (mode, inner, pos, pos_rtx, len,
       if (mode == tmode)
 	return new;
 
+      if (GET_CODE (new) == CONST_INT)
+	return trunc_int_for_mode (INTVAL (new), mode);
+
       /* If we know that no extraneous bits are set, and that the high
 	 bit is not set, convert the extraction to the cheaper of
 	 sign and zero extension, that are equivalent in these cases.  */
