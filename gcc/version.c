5256865b1ecda357c262b7454a3cb1d15c220308
GCC Administrator <gccadmin@gcc.gnu.org>
2002-03-10 08:16:46 +0000

    Daily bump.
    From-SVN: r50530

--- gcc/version.c
+++ gcc/version.c
@@ -1,1 +1,1 @@
-                                                                                        char *version_string = "2.95.4 20020309 (prerelease)";
+                                                                                        char *version_string = "2.95.4 20020310 (prerelease)";
--- libf2c/libI77/Version.c
+++ libf2c/libI77/Version.c
@@ -3,7 +3,7 @@
 /*
 */
 
-char __G77_LIBI77_VERSION__[] = "0.5.25 20020309 (prerelease)";
+char __G77_LIBI77_VERSION__[] = "0.5.25 20020310 (prerelease)";
 
 /*
 2.01	$ format added
