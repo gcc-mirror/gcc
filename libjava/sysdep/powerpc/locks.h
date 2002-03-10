aa7543832a1a981c69c4c3f08cd6ebee77828519
Bryce McKinlay <bryce@gcc.gnu.org>
2002-03-10 03:31:08 +0000

    configure.in: Define SLOW_PTHREAD_SELF if configure.host set slow_pthread_self.
    libjava:
    	* configure.in: Define SLOW_PTHREAD_SELF if configure.host set
    	slow_pthread_self. Set up symlink for sysdeps directory.
    	* configure.host: Document more shell variables. Set sysdeps_dir
    	for most platforms. Set slow_pthread_self for i686. Set
    	enable_hash_synchronization_default and slow_pthread_self for PowerPC.
    	* posix-threads.cc (_Jv_ThreadSelf_out_of_line): Use release_set so
    	that memory barrier is emitted where required.
    	* include/posix-threads.h (_Jv_ThreadSelf for SLOW_PTHREAD_SELF): Add
    	read_barrier() to enforce ordering of reads.
    	* sysdep/powerpc/locks.h: New file. Implementation of synchronization
    	primitives for PowerPC.
    	* sysdep/i386/locks.h: New file. Synchronization primitives for i386
    	moved from natObject.cc.
    	* sysdep/alpha/locks.h: Likewise.
    	* sysdep/ia64/locks.h: Likewise.
    	* sysdep/generic/locks.h: Likewise.
    	* java/lang/natObject.cc: Move thread synchronization primitives to
    	system-dependent headers.
    
    gcc/java:
    	* decl.c (java_init_decl_processing): Make sure class_type_node
    	alignment is not less than 64 bits if hash synchronization is enabled.
    
    boehm-gc:
    	* include/gc_priv.h: Define ALIGN_DOUBLE on 32 bit targets if GCJ
    	support is enabled, for hash synchronization.
    
    [[Split portion of a mixed commit.]]
    
    From-SVN: r50518.2

---
+++ libjava/sysdep/powerpc/locks.h
@@ -0,0 +1,78 @@
+// locks.h - Thread synchronization primitives. PowerPC implementation.
+
+/* Copyright (C) 2002  Free Software Foundation
+
+   This file is part of libgcj.
+
+This software is copyrighted work licensed under the terms of the
+Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
+details.  */
+
+#ifndef __SYSDEP_LOCKS_H__
+#define __SYSDEP_LOCKS_H__
+
+typedef size_t obj_addr_t;	/* Integer type big enough for object	*/
+				/* address.				*/
+
+inline static bool
+compare_and_swap(volatile obj_addr_t *addr,
+		  			      obj_addr_t old,
+					      obj_addr_t new_val) 
+{
+  int ret;
+
+  __asm__ __volatile__ (
+	   "0:    lwarx %0,0,%1 ;"
+	   "      xor. %0,%3,%0;"
+	   "      bne 1f;"
+	   "      stwcx. %2,0,%1;"
+	   "      bne- 0b;"
+	   "1:    "
+	: "=&r"(ret)
+	: "r"(addr), "r"(new_val), "r"(old)
+	: "cr0", "memory");
+  /* This version of __compare_and_swap is to be used when acquiring
+     a lock, so we don't need to worry about whether other memory
+     operations have completed, but we do need to be sure that any loads
+     after this point really occur after we have acquired the lock.  */
+  __asm__ __volatile__ ("isync" : : : "memory");
+  return ret == 0;
+}
+
+inline static void
+release_set(volatile obj_addr_t *addr, obj_addr_t new_val)
+{
+  __asm__ __volatile__ ("sync" : : : "memory");
+  *(addr) = new_val;
+}
+
+inline static bool
+compare_and_swap_release(volatile obj_addr_t *addr,
+		  				     obj_addr_t old,
+						     obj_addr_t new_val)
+{
+  int ret;
+
+  __asm__ __volatile__ ("sync" : : : "memory");
+  __asm__ __volatile__ (
+	   "0:    lwarx %0,0,%1 ;"
+	   "      xor. %0,%3,%0;"
+	   "      bne 1f;"
+	   "      stwcx. %2,0,%1;"
+	   "      bne- 0b;"
+	   "1:    "
+	: "=&r"(ret)
+	: "r"(addr), "r"(new_val), "r"(old)
+	: "cr0", "memory");
+  return ret == 0;
+}
+
+// Ensure that subsequent instructions do not execute on stale
+// data that was loaded from memory before the barrier.
+inline static void
+read_barrier()
+{
+  __asm__ __volatile__ ("isync" : : : "memory");
+}
+
+#endif
