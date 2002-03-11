f680e7ebf1a4c86812b0b7d239e75ae7e0790659
No Author <no-author@gcc.gnu.org>
2002-03-11 20:44:29 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_1-branch'.
    From-SVN: r50593

---
+++ gcc/testsuite/g++.dg/overload/pmf1.C
@@ -0,0 +1,21 @@
+// { dg-do compile }
+
+// Copyright (C) 2002 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 04 Mar 2002 <nathan@codesourcery.com>
+//		  Jason Merrill <jason@redhat.com>
+
+struct A { int i; };
+struct B: private A {};
+struct C {
+  C (int A::*);
+};
+
+int A::*aip = &A::i;
+
+void f (int B::*) {}  // should choose this, even though it's ill-formed
+void f (C) {}         // even though this would be well-formed
+
+int main ()
+{
+  f (aip);  // { dg-error "`A' is an inaccessible base of `B'" "" }
+}
