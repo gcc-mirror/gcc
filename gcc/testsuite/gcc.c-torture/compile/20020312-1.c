0d7a164505e45b59cbebc4a49e6df9e81d28d5d5
Jakub Jelinek <jakub@redhat.com>
2002-03-13 08:48:03 +0100

    re PR rtl-optimization/5892 (ICE in gen_nop_type during 2.4.17 ia64 kernel build)
    	PR optimization/5892
    	* config/ia64/ia64.c (rotate_one_bundle): Update current packet.
    
    	* gcc.c-torture/compile/20020312-1.c: New test.
    
    From-SVN: r50722

---
+++ gcc/testsuite/gcc.c-torture/compile/20020312-1.c
@@ -0,0 +1,19 @@
+/* PR optimization/5892 */
+typedef struct { unsigned long a; unsigned int b, c; } A;
+typedef struct { unsigned long a; A *b; int c; } B;
+
+static inline unsigned int
+bar (unsigned int x)
+{
+  unsigned long r;
+  asm ("" : "=r" (r) : "0" (x));
+  return r >> 31;
+}
+
+int foo (B *x)
+{
+  A *y;
+  y = x->b;
+  y->b = bar (x->c);
+  y->c = ({ unsigned int z = 1; (z << 24) | (z >> 24); });
+}
