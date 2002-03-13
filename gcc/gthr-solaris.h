7a8de19b4c54f6920786b650892e3bd591b53f81
Jason Merrill <jason@redhat.com>
2002-03-13 12:15:46 -0500

    gthr-posix.h (__gthread_active_p): Move __gthread_active_ptr into the function and constify it.
            * gthr-posix.h (__gthread_active_p): Move __gthread_active_ptr
            into the function and constify it.
            * gthr-dce.h, gthr-solaris.h: Likewise.
    
    From-SVN: r50741

--- gcc/gthr-solaris.h
+++ gcc/gthr-solaris.h
@@ -81,11 +81,10 @@ typedef mutex_t __gthread_mutex_t;
 /* This will not actually work in Solaris 2.5, since libc contains
    dummy symbols of all thr_* routines.  */
 
-static void *__gthread_active_ptr = (void *) &thr_create;
-
 static inline int
 __gthread_active_p (void)
 {
+  static void *const __gthread_active_ptr = (void *) &thr_create;
   return __gthread_active_ptr != 0;
 }
 
