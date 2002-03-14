02cd118b8b949af686494bfa15ade5d027f1abc5
No Author <no-author@gcc.gnu.org>
2002-03-14 15:51:59 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_1-branch'.
    From-SVN: r50774

---
+++ gcc/testsuite/gcc.c-torture/execute/20020314-1.c
@@ -0,0 +1,36 @@
+void f(void * a, double y)
+{
+}
+
+double g (double a, double b, double c, double d)
+{
+  double x, y, z;
+  void *p;
+
+  x = a + b;
+  y = c * d;
+
+  p = alloca (16);
+
+  f(p, y);
+  z = x * y * a;
+
+  return z + b;
+}
+
+main ()
+{
+  double a, b, c, d;
+  a = 1.0;
+  b = 0.0;
+  c = 10.0;
+  d = 0.0;
+
+  if (g (a, b, c, d) != 0.0)
+    abort ();
+
+  if (a != 1.0 || b != 0.0 || c != 10.0 || d != 0.0)
+    abort ();
+
+  exit (0);
+}
