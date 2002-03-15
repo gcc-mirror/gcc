08bcc159d9e63cd48eb12895f027e5d84d2ccd65
Eric Blake <ebb9@email.byu.edu>
2002-03-15 19:17:39 +0000

    For PR java/5902:
    2002-03-15  Eric Blake  <ebb9@email.byu.edu>
    
    	For PR java/5902:
    	* libjava.compile/PR5902.java: Does not need to execute.
    	For PR java/5913:
    	* libjava.compile/PR5913.java: Ditto.
    
    From-SVN: r50829

--- libjava/testsuite/libjava.compile/PR5913.java
+++ libjava/testsuite/libjava.compile/PR5913.java
@@ -1,10 +1,6 @@
 class PR5913
 {
-  public static void main(String[] args)
-  {
-    boolean test1 = ("" + 1) instanceof String;
-    // This also tests literal parsing, as mentioned in PR 5902.
-    boolean test2 = "" + 0x1instanceof String;
-    System.exit((test1 && test2) ? 0 : 1);
-  }
+  boolean test1 = ("" + 1) instanceof String;
+  // This also tests literal parsing, as mentioned in PR 5902.
+  boolean test2 = "" + 0x1instanceof String;
 }
