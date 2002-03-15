c4c3877b17746aa4fd869af459e729dd1870ee38
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-03-15 09:54:42 +0000

    * toplev.c (wrapup_global_declarations): Clarify variable handling.         -fkeep-static-consts doesn't apply to comdats.
    cp/
            * decl.c (make_rtl_for_nonlocal_decl): Also defer COMDAT
            variables.
            * decl2.c (maybe_make_one_only): Also mark the decl as needed.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@50802 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/abi/vague1.C
@@ -0,0 +1,18 @@
+// Test that we don't emit unneeded copies of static data member template
+// instantiations.
+
+// Disable debug info so we don't get confused by the symbol name there.
+// { dg-options "-g0" }
+// { dg-final { scan-assembler-not "_ZN1AIiE1tE" } }
+
+template <class T> struct A {
+  static const T t = 0;
+};
+
+template <class T> const T A<T>::t;
+
+int i;
+int main ()
+{
+  i = A<int>::t;		// Should just use the value
+}
