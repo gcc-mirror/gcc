32135a8c0ca88b46200a10ada7e987b3e5bbd34b
Richard Earnshaw <rearnsha@arm.com>
2002-03-15 18:32:37 +0000

    * gcc.dg/weak-1.c: Fix scan tests.
    From-SVN: r50821

--- gcc/testsuite/gcc.dg/weak-1.c
+++ gcc/testsuite/gcc.dg/weak-1.c
@@ -2,14 +2,14 @@
 /* COFF does not support weak, and dg doesn't support UNSUPPORTED.  */
 /* { dg-do compile { xfail *-*-coff i?86-pc-cygwin } } */
 
-/* { dg-final { scan-assembler "weak[^ 	]*[ 	]_?a" } } */
-/* { dg-final { scan-assembler "weak[^ 	]*[ 	]_?b" } } */
-/* { dg-final { scan-assembler "weak[^ 	]*[ 	]_?c" } } */
-/* { dg-final { scan-assembler "weak[^ 	]*[ 	]_?d" } } */
-/* { dg-final { scan-assembler "weak[^ 	]*[ 	]_?e" } } */
-/* { dg-final { scan-assembler "weak[^ 	]*[ 	]_?g" } } */
-/* { dg-final { scan-assembler-not "weak[^ 	]*[ 	]_?i" } } */
-/* { dg-final { scan-assembler "weak[^ 	]*[ 	]_?j" } } */
+/* { dg-final { scan-assembler "weak\[^ \t\]*\[ \t]_?a" } } */
+/* { dg-final { scan-assembler "weak\[^ \t\]*\[ \t]_?b" } } */
+/* { dg-final { scan-assembler "weak\[^ \t\]*\[ \t]_?c" } } */
+/* { dg-final { scan-assembler "weak\[^ \t\]*\[ \t]_?d" } } */
+/* { dg-final { scan-assembler "weak\[^ \t\]*\[ \t]_?e" } } */
+/* { dg-final { scan-assembler "weak\[^ \t\]*\[ \t]_?g" } } */
+/* { dg-final { scan-assembler-not "weak\[^ \t\]*\[ \t]_?i" } } */
+/* { dg-final { scan-assembler "weak\[^ \t\]*\[ \t]_?j" } } */
 
 #pragma weak a
 int a;
