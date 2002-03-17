beb9c8a2348bfc9095cf35684aee67cdfd7461ef
Jason Merrill <jason@redhat.com>
2002-03-17 08:54:04 -0500

    re PR c++/5757 (Throwing exception in constructor, causes segfault during unwind)
            PR c++/5757
            * init.c (build_new_1): Pass the right pointer to op delete.
    
    From-SVN: r50917

---
+++ gcc/testsuite/g++.dg/eh/new1.C
@@ -0,0 +1,40 @@
+// PR c++/5757
+// Test that when a constructor throws in a new-expression, we pass the
+// right pointer to operator delete.
+
+// { dg-do run }
+
+#include <new>
+
+int ret = 1;
+
+void *ptr;
+void * operator new[] (size_t s) throw (std::bad_alloc)
+{
+  ptr = operator new (s);
+  return ptr;
+}
+
+void operator delete[] (void *p) throw ()
+{
+  if (p == ptr)
+    ret = 0;
+  operator delete (p);
+}
+
+struct A
+{
+  A() { throw 1; }
+  ~A() {}
+};
+
+int
+main ()
+{
+  try
+    {
+      A *p = new A[4];
+    }
+  catch (...) {}
+  return ret;
+}
