2effc1a3fb4b64b61bc90d59c0eb8494d779154f
GCC Administrator <gccadmin@gcc.gnu.org>
2002-03-18 08:16:39 +0000

    Daily bump.
    From-SVN: r50947

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,4 +1,4 @@
                                                                                         #include "ansidecl.h"
 #include "f/version.h"
 
-const char *const ffe_version_string = "3.1 20020317 (prerelease)";
+const char *const ffe_version_string = "3.1 20020318 (prerelease)";
--- libf2c/libF77/Version.c
+++ libf2c/libF77/Version.c
@@ -3,7 +3,7 @@
 /*
 */
 
-char __G77_LIBF77_VERSION__[] = "3.1 20020317 (prerelease)";
+char __G77_LIBF77_VERSION__[] = "3.1 20020318 (prerelease)";
 
 /*
 2.00	11 June 1980.  File version.c added to library.
