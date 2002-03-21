c497919065688aaa4335cba7bc237f8b9455cf8a
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-03-21 00:29:09 +0000

    * lib/prune.exp (prune_gcc_output): Ignore Cygwin warning about         ignoring -fpic.         * lib/old-dejagnu.exp (old-dejagnu): Also pass compiler output         through prune_gcc_output.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@51102 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.jason/template18.C
+++ gcc/testsuite/g++.old-deja/g++.jason/template18.C
@@ -4,6 +4,10 @@
 
 // We mark this XFAIL because we can't test for expected linker errors.
 // If we get an XPASS for this testcase, that's a bug.
+
+// Suppressed-instantiation tests like this are known to break on
+// Cygwin, because of the MULTIPLE_SYMBOL_SPACES stuff.  This is OK.
+
 // (OK) excess errors test - XFAIL *-*-*
 
 #pragma implementation "irrelevant_file"
