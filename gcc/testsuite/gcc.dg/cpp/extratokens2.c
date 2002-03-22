a5e039de1f6e976dfb1fef516d93ee2f3caf75c0
pme <pme@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-03-22 21:44:30 +0000

    2002-03-22  Phil Edwards  <pme@gcc.gnu.org>
    	* gcc.dg/cpp/extratokens2.c:  New file.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@51188 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/cpp/extratokens2.c
@@ -0,0 +1,22 @@
+/* Copyright (C) 2002 Free Software Foundation, Inc.  */
+
+/* { dg-do preprocess } */
+/* { dg-options "-fno-show-column -Wno-endif-labels" } */
+
+/* Tests that -Wno-endif-labels correctly disables the checks done by
+   default (and tested in extratokens.c).  */
+
+/* Source: Phil Edwards, 21 Mar 2002.  Copied from extratokens.c and
+   modified.  */
+
+#if 1 
+#if 0
+#else foo	/* { dg-bogus "extra tokens" "bad warning" } */
+#endif /	/* { dg-bogus "extra tokens" "bad warning" } */
+#endif
+
+# 36 "file.c" 3
+
+/* ... but in a system header, it's acceptable.  */
+#ifdef KERNEL
+#endif KERNEL  /* { dg-bogus "extra tokens" "bad warning" } */
