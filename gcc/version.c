0e0150b2988d834f1e12dde83c2eebd2c85db9d3
GCC Administrator <gccadmin@gcc.gnu.org>
2002-03-24 08:16:56 +0000

    Daily bump.
    From-SVN: r51254

--- gcc/version.c
+++ gcc/version.c
@@ -1,1 +1,1 @@
-                                                                                        char *version_string = "2.95.4 20020323 (prerelease)";
+                                                                                        char *version_string = "2.95.4 20020324 (prerelease)";
