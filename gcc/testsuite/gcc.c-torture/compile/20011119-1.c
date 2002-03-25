8018b36d0b35fa05f43d58c960626852c7fd13ff
Richard Henderson <rth@redhat.com>
2002-03-25 12:55:20 -0800

    20011119-1.c (xxx): New.
            * gcc.c-torture/compile/20011119-1.c (xxx): New.
            * gcc.c-torture/compile/20011119-2.c (xxx): New.
    
    From-SVN: r51337

--- gcc/testsuite/gcc.c-torture/compile/20011119-1.c
+++ gcc/testsuite/gcc.c-torture/compile/20011119-1.c
@@ -1,5 +1,4 @@
-extern inline int foo (void)
-{
-  return 23;
-}
+extern inline int foo (void) { return 23; }
+int xxx(void) __asm__("xxx");
+int xxx(void) { return 23; }
 extern int foo (void) __attribute__ ((weak, alias ("xxx")));
