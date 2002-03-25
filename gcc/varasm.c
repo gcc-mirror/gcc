1bd6476f2eacdfdda0bc413f404b4b8bd778f802
Richard Henderson <rth@redhat.com>
2002-03-24 16:44:36 -0800

    varasm.c (asm_emit_uninitialised): Revert 2002-03-14 change.
            * varasm.c (asm_emit_uninitialised): Revert 2002-03-14 change.
            Do resolve_unique_section before shared data clause.
    
            * gcc.dg/weak-1.c: Use -fno-common.
    
    From-SVN: r51289

--- gcc/varasm.c
+++ gcc/varasm.c
@@ -1395,16 +1395,9 @@ asm_emit_uninitialised (decl, name, size, rounded)
 	destination = asm_dest_common;
     }
 
-  switch (destination)
-    {
-    case asm_dest_common:
-      if (! DECL_WEAK (decl))
-	break;
-    case asm_dest_bss:
-      globalize_decl (decl);
-    default:
-      break;
-    }
+  if (destination == asm_dest_bss)
+    globalize_decl (decl);
+  resolve_unique_section (decl, 0);
 
   if (flag_shared_data)
     {
@@ -1429,8 +1422,6 @@ asm_emit_uninitialised (decl, name, size, rounded)
 	  break;
 	}
     }
-
-  resolve_unique_section (decl, 0);
 
   switch (destination)
     {
