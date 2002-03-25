3c6603faf93b15083b874aa146d221f9973b62e0
bwilson <bwilson@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-03-25 17:57:03 +0000

    * g++.old-deja/g++.jason/thunk3.C: Add xtensa-*-* to list of         targets using generic thunk support.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@51320 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.jason/thunk3.C
+++ gcc/testsuite/g++.old-deja/g++.jason/thunk3.C
@@ -2,7 +2,7 @@
 // Note that this will break on any target that uses the generic thunk
 //  support, because it doesn't support variadic functions.
 
-// excess errors test - XFAIL mips*-*-* rs6000-*-* powerpc-*-eabi m68k-*-coff m68k-motorola-sysv m88k-motorola-sysv3  mn10300-*-* mn10200-*-* v850-*-* sh-*-* sh64-*-* h8*-*-*
+// excess errors test - XFAIL mips*-*-* rs6000-*-* powerpc-*-eabi m68k-*-coff m68k-motorola-sysv m88k-motorola-sysv3  mn10300-*-* mn10200-*-* v850-*-* sh-*-* sh64-*-* h8*-*-* xtensa-*-*
 
 #include <stdarg.h>
 
