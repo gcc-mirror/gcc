d1db77180273516350abc351a65f78d17855f291
David Billinghurst <David.Billinghurst@riotinto.com>
2002-03-25 23:49:06 +0000

    g++.exp: Fix handling of LD_LIBRARY_PATH et al
    	* lib/g++.exp:  Fix handling of LD_LIBRARY_PATH et al
    	* lib/g77.exp:  Likewise
    	* lib/objc.exp: Likewise
    	* gcc.dg/weak-1.c:  Early return from dg-final on platforms
    	that do not support weak symbols
    
    From-SVN: r51356

--- gcc/testsuite/gcc.dg/weak-1.c
+++ gcc/testsuite/gcc.dg/weak-1.c
@@ -4,6 +4,9 @@
 /* COFF does not support weak, and dg doesn't support UNSUPPORTED.  */
 /* { dg-do compile { xfail *-*-coff i?86-pc-cygwin } } */
 
+/* { dg-final { global target_triplet } } */
+/* { dg-final { if [string match i?86-pc-cygwin $target_triplet ] {return} } } */
+/* { dg-final { if [string match *-*-coff $target_triplet ] {return} } } */
 /* { dg-final { scan-assembler "weak\[^ \t\]*\[ \t]_?a" } } */
 /* { dg-final { scan-assembler "weak\[^ \t\]*\[ \t]_?b" } } */
 /* { dg-final { scan-assembler "weak\[^ \t\]*\[ \t]_?c" } } */
