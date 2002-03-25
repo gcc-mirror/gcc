13fcad7cc0df074dd9b137241f5faa470c5d277b
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-03-25 20:55:20 +0000

    * gcc.c-torture/compile/20011119-1.c (xxx): New.         * gcc.c-torture/compile/20011119-2.c (xxx): New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@51337 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/compile/20011119-2.c
+++ gcc/testsuite/gcc.c-torture/compile/20011119-2.c
@@ -1,13 +1,6 @@
-extern inline int foo (void)
-{
-  return 23;
-}
-int bar (void)
-{
-  return foo ();
-}
+extern inline int foo (void) { return 23; }
+int bar (void) { return foo (); }
 extern int foo (void) __attribute__ ((weak, alias ("xxx")));
-int baz (void)
-{
-  return foo ();
-}
+int baz (void) { return foo (); }
+int xxx(void) __asm__("xxx");
+int xxx(void) { return 23; }
