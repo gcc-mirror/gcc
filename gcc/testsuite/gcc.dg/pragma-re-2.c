f4674fbeab8070834148bdac4e3c4d88509b14fe
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-03-27 02:30:23 +0000

    * gcc.dg/pragma-re-2.c: Avoid empty source file warning.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@51426 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pragma-re-2.c
+++ gcc/testsuite/gcc.dg/pragma-re-2.c
@@ -4,3 +4,5 @@
 #pragma redefine_extname foo		/* { dg-warning "malformed" } */
 #pragma redefine_extname foo 1		/* { dg-warning "malformed" } */
 #pragma redefine_extname foo bar 2	/* { dg-warning "junk" } */
+
+int bar; /* silence `ISO C forbids an empty source file' warning */
