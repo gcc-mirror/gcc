1b58660a9d302afe516eca5023066585ae6ceb85
Richard Henderson <rth@redhat.com>
2002-03-27 17:52:55 -0800

    instantiate1.C: Use scan-assembler, not link errors.
            * g++.dg/ext/instantiate1.C: Use scan-assembler, not link errors.
            * g++.dg/ext/instantiate2.C, g++.dg/ext/instantiate3.C: Likewise;
            move from g++.old-deja/g++.ext/.
    
    From-SVN: r51491

---
+++ gcc/testsuite/g++.dg/ext/instantiate3.C
@@ -0,0 +1,14 @@
+// Test that 'inline template' instantiates the vtable.
+// { dg-do compile }
+// { dg-options "-O -fno-implicit-templates" }
+
+template <class T> struct A {
+  virtual void f () { }
+};
+inline template struct A<int>;
+
+// { dg-final { scan-assembler "\n_?_ZTV1AIiE(:|\n)" } }
+A<int> a;
+
+// { dg-final { scan-assembler-not "\n_?_ZTV1AIcE(:|\n)" } }
+A<char> b;
