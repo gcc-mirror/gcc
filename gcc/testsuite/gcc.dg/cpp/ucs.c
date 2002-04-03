9d7013b8ac6b072089612f9bb48461b1cd3df259
Richard Henderson <rth@redhat.com>
2002-04-03 13:59:03 -0800

    * gcc.dg/cpp/ucs.c: Improved long UCS test.
    From-SVN: r51815

--- gcc/testsuite/gcc.dg/cpp/ucs.c
+++ gcc/testsuite/gcc.dg/cpp/ucs.c
@@ -5,13 +5,43 @@
 
 /* This tests universal character sequences.
 
-   Neil Booth, 22 May 2001.  */
+   Neil Booth, 22 May 2001.
+   Richard Henderson, 3 Apr 2002.  */
+
+#include <limits.h>
+
+#define unsigned        +0
+#define int             +0 
+#define char            +1
+#define short           +2 
+#define long            +3
+
+#if __WCHAR_TYPE__ == 0
+# define WCHAR_MAX      INT_MAX
+#elif __WCHAR_TYPE__ == 1
+# define WCHAR_MAX      CHAR_MAX
+#elif __WCHAR_TYPE__ == 2
+# define WCHAR_MAX      SHRT_MAX
+#elif __WCHAR_TYPE__ == 3
+# define WCHAR_MAX      LONG_MAX
+#else
+# error wacky wchar_t
+#endif
+
+#undef unsigned
+#undef int
+#undef char
+#undef short
+#undef long
 
 #if L'\u1234' != 0x1234
 #error bad short ucs	/* { dg-bogus "bad" "bad \u1234 evaluation" } */
 #endif
-#if L'\U1234abcd' != 0x1234abcd
-#error bad long ucs	/* { dg-bogus "bad" "bad \U1234abcd evaluation" } */
+
+#if WCHAR_MAX >= 0x7ffffff
+# if L'\U1234abcd' != 0x1234abcd
+#  error bad long ucs	/* { dg-bogus "bad" "bad \U1234abcd evaluation" } */
+# endif
 #endif
 
 void foo ()
@@ -24,7 +54,7 @@ void foo ()
   c = L'\u000x';	/* { dg-error "non-hex" "non-hex digit in UCN" } */
   /* If sizeof(HOST_WIDE_INT) > sizeof(wchar_t), we can get a multi-character
      constant warning even for wide characters.  */
-  /* { dg-warning "too long|multi-character" "" { target *-*-* } 24 } */
+  /* { dg-warning "too long|multi-character" "" { target *-*-* } 54 } */
 
   c = '\u0024';		/* { dg-bogus "invalid" "0024 is a valid UCN" } */
   c = "\u0040"[0];	/* { dg-bogus "invalid" "0040 is a valid UCN" } */
