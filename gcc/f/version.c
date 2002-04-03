13f39b6ddf03a18bf7b6dcff4e60a66655907cbb
GCC Administrator <gccadmin@gcc.gnu.org>
2002-04-03 08:17:15 +0000

    Daily bump.
    From-SVN: r51802

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,4 +1,4 @@
 #include "ansidecl.h"
 #include "f/version.h"
 
-const char *const ffe_version_string = "3.2 20020402 (experimental)";
+const char *const ffe_version_string = "3.2 20020403 (experimental)";
--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
 #include "ansidecl.h"
 #include "version.h"
 
-const char *const version_string = "3.2 20020402 (experimental)";
+const char *const version_string = "3.2 20020403 (experimental)";
