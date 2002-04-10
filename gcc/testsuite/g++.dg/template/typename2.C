ba3307c092fce730120657ab5c89ff4d0936244e
Nathan Sidwell <nathan@codesourcery.com>
2002-04-10 09:40:18 +0000

    re PR c++/5507 (Overzealous implicit typename warning)
    cp:
            PR c++/5507
            * decl.c (make_typename_type): Remove implicit typenameness.
    testsuite:
            PR c++/5507
            * g++.dg/template/typename2.C: New test.
    
    From-SVN: r52124

---
+++ gcc/testsuite/g++.dg/template/typename2.C
@@ -0,0 +1,25 @@
+// { dg-do compile }
+// { dg-options "" }
+
+// Copyright (C) 2001 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 21 Mar 2002 <nathan@codesourcery.com>
+
+// PR 5507. Overzealous implicit typename warning
+
+template<typename _CharT>
+class __ctype_abstract_base
+{
+  typedef int mask;
+};
+
+template<typename _CharT>
+class ctype : public __ctype_abstract_base<_CharT>
+{
+  typedef typename ctype::mask mask;
+};
+
+template<typename _CharT>
+class ctype2 : public __ctype_abstract_base<_CharT>
+{
+  typedef mask mask; // { dg-warning "(`typename )|(implicit typename)" "" }
+};
