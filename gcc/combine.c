886bca0ae323169b3cf97f2c3a103256bd3f80e0
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-04-11 20:56:29 +0000

    PR c/6223 	* combine.c (if_then_else_cond): Use trunc_int_for_mode on nz.
    	* gcc.dg/20020411-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@52176 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/combine.c
+++ gcc/combine.c
@@ -7382,7 +7382,7 @@ if_then_else_cond (x, ptrue, pfalse)
 	   && GET_MODE_BITSIZE (mode) <= HOST_BITS_PER_WIDE_INT
 	   && exact_log2 (nz = nonzero_bits (x, mode)) >= 0)
     {
-      *ptrue = GEN_INT (nz), *pfalse = const0_rtx;
+      *ptrue = gen_int_mode (nz, mode), *pfalse = const0_rtx;
       return x;
     }
 
