ce476d48d5a3b7ce4501d7fa4394739ae1ef7056
Hans-Peter Nilsson <hp@bitrange.com>
2002-04-13 12:27:48 +0000

    980628-4.x, [...]: New, xfail cross-endian tests; powerpc-*-* and mmix hosted on i[34567]86-*-*.
    	* g77.f-torture/execute/980628-4.x,
    	g77.f-torture/execute/980628-5.x,
    	g77.f-torture/execute/980628-6.x, g77.f-torture/execute/alpha2.x,
    	g77.f-torture/execute/auto0.x, g77.f-torture/execute/auto1.x,
    	g77.f-torture/compile/alpha1.x: New, xfail cross-endian tests;
    	powerpc-*-* and mmix hosted on i[34567]86-*-*.
    	* g77.dg/f77-edit-i-in.f: xfail for mmix-knuth-mmixware.
    	* g77.f-torture/execute/io0.x, g77.f-torture/execute/io1.x,
    	g77.f-torture/execute/20001201.x,
    	g77.f-torture/execute/u77-test.x: New files.
    	* gcc.dg/wint_t-1.c: Mark excess errors for mmix-knuth-mmixware.
    	* gcc.dg/wchar_t-1.c: Ditto.
    
    From-SVN: r52275

--- gcc/testsuite/gcc.dg/wchar_t-1.c
+++ gcc/testsuite/gcc.dg/wchar_t-1.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-Wall" } */
+/* { dg-excess-errors "Newlib has no wchar.h" { target mmix-knuth-mmixware } } */
 
 /* Compile with -Wall to get a warning if built-in and system wchar_t don't
    match.  */
