cf56e373c4b75dd96e6521637217d47261cb6b8f
David Billinghurst <David.Billinghurst@riotinto.com>
2002-04-14 11:52:25 +0000

    re PR c/6277 (Failure of test gcc.dg/i386-387-1.c on cygwin)
    2002-04-14  David Billinghurst <David.Billinghurst@riotinto.com>
    
    	PR c/6277
    	* gcc.dg/i386-387-1.c:  Allow optional leading underscore
    	in scan-assembler function name
    
    From-SVN: r52292

--- gcc/testsuite/gcc.dg/i386-387-1.c
+++ gcc/testsuite/gcc.dg/i386-387-1.c
@@ -1,9 +1,9 @@
 /* Verify that -mno-fancy-math-387 works.  */
 /* { dg-do compile { target "i?86-*-*" } } */
 /* { dg-options "-O -ffast-math -mfpmath=387 -mno-fancy-math-387" } */
-/* { dg-final { scan-assembler "call\tsin" } } */
-/* { dg-final { scan-assembler "call\tcos" } } */
-/* { dg-final { scan-assembler "call\tsqrt" } } */
+/* { dg-final { scan-assembler "call\t_?sin" } } */
+/* { dg-final { scan-assembler "call\t_?cos" } } */
+/* { dg-final { scan-assembler "call\t_?sqrt" } } */
 
 double f1(double x) { return __builtin_sin(x); }
 double f2(double x) { return __builtin_cos(x); }
