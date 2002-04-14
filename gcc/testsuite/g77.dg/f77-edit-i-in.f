4b27e132f1d20f084ec35439c5bee9345da5a883
David Billinghurst <David.Billinghurst@riotinto.com>
2002-04-14 13:46:24 +0000

    f77-edit-i-in.f: Use internal buffer in place of scratch file.
    2002-04-12  David Billinghurst <David.Billinghurst@riotinto.com>
    
    	* g77.dg/f77-edit-i-in.f: Use internal buffer in place
    	of scratch file.
    
    From-SVN: r52294

--- gcc/testsuite/g77.dg/f77-edit-i-in.f
+++ gcc/testsuite/g77.dg/f77-edit-i-in.f
@@ -3,23 +3,18 @@ C      (ANSI X3.9-1978 Section 13.5.9.1)
 C
 C Origin: David Billinghurst <David.Billinghurst@riotinto.com>
 C
-C Scratch files aren't implemented for mmixware: stubs will fail at run time.
-C { dg-do run { xfail mmix-knuth-mmixware } }
       integer i,j
+      character*10 buf
 
-      open(unit=10,status='SCRATCH')
-      write(10,'(A)') '1'
-      write(10,'(A)') ' '
-      write(10,'(A)') '   -1'
+      write(buf,'(A)') '1  -1'
 
-      rewind(10)
-
-      read(10,'(I1)') i
+      read(buf,'(I1)') i
       if ( i.ne.1 ) call abort()
-      read(10,'(I1)') i
+
+      read(buf,'(X,I1)') i
       if ( i.ne.0 ) call abort()
-      read(10,'(I2,X,I2)') i,j
-      if ( i.ne.0 ) call abort()
-      if ( j.ne.-1 ) call abort()
+
+      read(buf,'(X,I1,X,I2)') i,j
+      if ( i.ne.0 .and. j.ne.-1 ) call abort()
 
       end
