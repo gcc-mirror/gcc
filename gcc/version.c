d4b772182260418bfc0ccb9eb176923201a9eaab
GCC Administrator <gccadmin@gcc.gnu.org>
2002-04-15 07:16:12 +0000

    Daily bump.
    From-SVN: r52314

--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
 #include "ansidecl.h"
 #include "version.h"
 
-const char *const version_string = "3.1 20020414 (prerelease)";
+const char *const version_string = "3.1 20020415 (prerelease)";
--- libf2c/libI77/Version.c
+++ libf2c/libI77/Version.c
@@ -3,7 +3,7 @@ static char junk[] = "\n@(#) LIBI77 VERSION pjw,dmg-mods 20001205\n";
 /*
 */
 
-char __G77_LIBI77_VERSION__[] = "3.1 20020414 (prerelease)";
+char __G77_LIBI77_VERSION__[] = "3.1 20020415 (prerelease)";
 
 /*
 2.01	$ format added
