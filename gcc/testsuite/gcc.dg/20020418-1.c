a48632153d7f6013126d9a73125fbef7c05268c4
Jakub Jelinek <jakub@redhat.com>
2002-04-18 22:10:48 +0200

    PR optimization/5887, PR optimization/6010
            PR optimization/5887, PR optimization/6010
    	* gcc.dg/20020418-1.c: New test.
    	* gcc.dg/20020418-2.c: New test.
    
    From-SVN: r52492

---
+++ gcc/testsuite/gcc.dg/20020418-1.c
@@ -0,0 +1,26 @@
+/* PR optimization/5887 */
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+/* { dg-options "-O2 -msse -ffast-math" { target i?86-*-* } } */
+
+void bar (float *a, float *b);
+
+void foo (char *x)
+{
+  float a, b;
+  char c[256];
+  int i, j;
+
+  bar (&a, &b);
+  for (i = 0; i < 256; i++)
+    {
+      float v = a;
+      if (v < 0.0f) v = 0.0f;
+      if (v < 255.0f) v = 255.0f;
+      c[i] = v;
+      a += b;
+    }
+
+  for (j = 0; j < 256; j++)
+    x[j] = c[j];
+}
