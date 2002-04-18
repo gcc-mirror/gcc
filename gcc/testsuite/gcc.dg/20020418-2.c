3a69e0d573a31b39d36c98a939a52da26c47cde6
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-04-18 20:10:48 +0000

    PR optimization/5887, PR optimization/6010 	* gcc.dg/20020418-1.c: New test. 	* gcc.dg/20020418-2.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@52492 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20020418-2.c
@@ -0,0 +1,21 @@
+/* PR optimization/6010 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -funroll-all-loops" } */
+/* { dg-options "-O2 -funroll-all-loops -march=pentium3" { target i?86-*-* } } */
+
+void bar (float);
+
+void foo (float y, unsigned long z)
+{
+  int b;
+  float c = y;
+
+  for (b = 0; b < z; b++)
+    {
+      bar (c);
+      if (c == y)
+	c = -y;
+      else
+	c = y;
+    }
+}
