a3acf46d445fffc0d691bf1acb80d9a70dbd082c
Jakub Jelinek <jakub@redhat.com>
2002-04-18 21:10:11 +0200

    re PR c/6358 (GCC 3.1 ICE on statement expressions)
    	PR c/6358
    	* function.c (assign_parms): Assign hard current_function_return_rtx
    	register here...
    	(expand_function_end): ...not here.
    
    	* gcc.c-torture/compile/20020418-1.c: New test.
    
    From-SVN: r52485

---
+++ gcc/testsuite/gcc.c-torture/compile/20020418-1.c
@@ -0,0 +1,18 @@
+/* PR c/6358
+   This testcase ICEd on IA-32 in foo, because current_function_return_rtx
+   was assigned a hard register only after expand_null_return was called,
+   thus return pseudo was clobbered twice and the hard register not at
+   all.  */
+
+void baz (void);
+                       
+double foo (void)
+{
+  baz ();
+  return;
+}
+
+double bar (void)
+{
+  baz ();
+}
