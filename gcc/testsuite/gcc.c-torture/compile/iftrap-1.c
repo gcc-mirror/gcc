fe70d0e306045b920df0feaf869e83b5106e8098
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-04-18 20:02:18 +0000

    * ifcvt.c: Include except.h.         (block_has_only_trap): Break out from find_cond_trap.         (find_cond_trap): Use it.  Always delete the trap block.         (merge_if_block): Allow then block null.  Be less simplistic about         what insns can end a block.         * Makefile.in (ifcvt.o): Depend on except.h.
            * gcc.c-torture/compile/iftrap-1.c: New.
            * gcc.dg/iftrap-1.c: Adjust for ia64.
            * gcc.dg/iftrap-2.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@52489 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/iftrap-1.c
@@ -0,0 +1,99 @@
+/* Verify that ifcvt doesn't crash under a number of interesting conditions. */
+
+void f1(int p)
+{
+  if (p)
+    __builtin_trap();
+}
+
+void f2(int p)
+{
+  if (p)
+    __builtin_trap();
+  else
+    bar();
+}
+
+void f3(int p)
+{
+  if (p)
+    bar();
+  else
+    __builtin_trap();
+}
+
+void f4(int p, int q)
+{
+  if (p)
+    {
+      bar();
+      if (q)
+	bar();
+    }
+  else
+    __builtin_trap();
+}
+
+void f5(int p)
+{
+  if (p)
+    __builtin_trap();
+  else
+    abort();
+}
+
+void f6(int p)
+{
+  if (p)
+    abort();
+  else
+    __builtin_trap();
+}
+
+void f7(int p)
+{
+  if (p)
+    __builtin_trap();
+  else
+    __builtin_trap();
+}
+
+void f8(int p)
+{
+  if (p)
+    __builtin_trap();
+  else
+    {
+      bar();
+      __builtin_trap();
+    }
+}
+
+void f9(int p)
+{
+  if (p)
+    {
+      bar();
+      __builtin_trap();
+    }
+  else
+    __builtin_trap();
+}
+
+void f10(int p)
+{
+  if (p)
+    __builtin_trap();
+  while (1)
+    bar();
+}
+
+void f11(int p)
+{
+  if (p)
+    __builtin_trap();
+  else
+    bar();
+  while (1)
+    baz();
+}
