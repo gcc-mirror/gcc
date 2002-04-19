aad17deeed34dfb4c7e9403732baf29fcad933d7
gccadmin <gccadmin@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-04-19 07:19:16 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@52516 138bc75d-0d04-0410-961f-82ee72b054a4

--- libf2c/libU77/Version.c
+++ libf2c/libU77/Version.c
@@ -1,6 +1,6 @@
 static char junk[] = "\n@(#) LIBU77 VERSION 19980709\n";
 
-char __G77_LIBU77_VERSION__[] = "3.2 20020418 (experimental)";
+char __G77_LIBU77_VERSION__[] = "3.2 20020419 (experimental)";
 
 #include <stdio.h>
 
