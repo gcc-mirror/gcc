596f6d22a9054cdda67442cec86c3aa290f600a3
Mark Mitchell <mark@codesourcery.com>
2002-04-23 08:34:45 +0000

    re PR c++/6256 (Seg fault for template friends in namespaces, regression from 2.95)
    	PR c++/6256:
    	* g++.dg/template/friend5.C: New test.
    
    	PR c++/6331:
    	* g++.dg/template/qual1.C: Likewise.
    
    	PR c++/6256:
    	* pt.c (tsubst_friend_class): Handle templates with explicit
    	nested names.
    
    	PR c++/6331:
    	* typeck.c (merge_types): Remember the cv-qualification of pointer
    	types when merging them.
    
    From-SVN: r52660

---
+++ gcc/testsuite/g++.dg/template/qual1.C
@@ -0,0 +1,21 @@
+// { dg-do compile }
+
+template<class T>
+class Link_array
+{
+public:
+  void sort (int (*compare) (T *const&,T *const&));
+};
+
+int shift_compare (int *const &, int *const &) {};
+
+template<class T> void
+Link_array<T>::sort (int (*compare) (T *const&,T *const&)) 
+{
+}
+
+void f ()
+{
+  Link_array<int> clashes;
+  clashes.sort (shift_compare);
+}
