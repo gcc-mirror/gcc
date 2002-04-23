736020b4a73cd6ea6deaed6239ca6b274c01f9d8
Jason Merrill <jason@redhat.com>
2002-04-23 17:56:10 -0400

    fstream.tcc (basic_filebuf::seekoff): Fix for output-only filebufs.
            * include/bits/fstream.tcc (basic_filebuf::seekoff): Fix for
            output-only filebufs.
            * include/std/std_fstream.h (basic_filebuf::_M_set_indeterminate):
            Likewise.
            (basic_filebuf::_M_set_determinate): Likewise.
    
    From-SVN: r52685

--- libstdc++-v3/testsuite/27_io/ostream_seeks.cc
+++ libstdc++-v3/testsuite/27_io/ostream_seeks.cc
@@ -167,6 +167,13 @@ void test04(void)
   pos06 = is03.tellp(); 
   VERIFY( pos05 == pos06 ); 
 
+  // libstdc++/6414
+  if01.seekg(0, std::ios_base::beg);
+  pos01 = if01.tellg();
+  if01.peek();
+  pos02 = if01.tellg();
+  VERIFY( pos02 == pos01 );
+
 #ifdef DEBUG_ASSERT
   assert(test);
 #endif
