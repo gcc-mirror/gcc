20d9838a825bce30a6d822f7eda5a081ba798661
David O'Brien <obrien@FreeBSD.org>
2002-04-23 16:37:16 +0000

    g++spec.c: Use profiled libstdc++ and libm with -p/-pg.
    2002-04-23  David O'Brien  <obrien@FreeBSD.org>
    	* cp/g++spec.c: Use profiled libstdc++ and libm with -p/-pg.
    	* config/freebsd.h (MATH_LIBRARY_PROFILE): Use the _p verions of
    	these libraries.
    
    Approved by:  Richard Henderson <rth@redhat.com>
                  Message-ID: <20020423092356.C632@redhat.com>
    
    From-SVN: r52670

--- gcc/cp/g++spec.c
+++ gcc/cp/g++spec.c
@@ -32,9 +32,15 @@ Boston, MA 02111-1307, USA.  */
 #ifndef MATH_LIBRARY
 #define MATH_LIBRARY "-lm"
 #endif
+#ifndef MATH_LIBRARY_PROFILE
+#define MATH_LIBRARY_PROFILE "-lm"
+#endif
 
 #ifndef LIBSTDCXX
 #define LIBSTDCXX "-lstdc++"
+#endif
+#ifndef LIBSTDCXX_PROFILE
+#define LIBSTDCXX_PROFILE "-lstdc++"
 #endif
 
 void
@@ -44,6 +50,9 @@ lang_specific_driver (in_argc, in_argv, in_added_libraries)
      int *in_added_libraries;
 {
   int i, j;
+
+  /* If non-zero, the user gave us the `-p' or `-pg' flag.  */ 
+  int saw_profile_flag = 0;
 
   /* If non-zero, the user gave us the `-v' flag.  */ 
   int saw_verbose_flag = 0;
@@ -137,6 +146,8 @@ lang_specific_driver (in_argc, in_argv, in_added_libraries)
 	    }
 	  else if (strcmp (argv[i], "-lc") == 0)
 	    args[i] |= WITHLIBC;
+	  else if (strcmp (argv[i], "-pg") == 0 || strcmp (argv[i], "-p") == 0)
+	    saw_profile_flag++;
 	  else if (strcmp (argv[i], "-v") == 0)
 	    {
 	      saw_verbose_flag = 1;
@@ -259,14 +270,14 @@ lang_specific_driver (in_argc, in_argv, in_added_libraries)
   /* Add `-lstdc++' if we haven't already done so.  */
   if (library)
     {
-      arglist[j++] = LIBSTDCXX;
+      arglist[j++] = saw_profile_flag ? LIBSTDCXX_PROFILE : LIBSTDCXX;
       added_libraries++;
     }
   if (saw_math)
     arglist[j++] = saw_math;
   else if (library && need_math)
     {
-      arglist[j++] = MATH_LIBRARY;
+      arglist[j++] = saw_profile_flag ? MATH_LIBRARY_PROFILE : MATH_LIBRARY;
       added_libraries++;
     }
   if (saw_libc)
