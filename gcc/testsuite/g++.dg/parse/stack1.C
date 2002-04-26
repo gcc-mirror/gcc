deae487ab287f34088fdbdd50375dbfc954ad24f
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-04-26 05:35:13 +0000

    * c-parse.in (yyoverflow): New. 	* cp/parse.y (yyoverflow): New. 	* g++.dg/parse/stack1.C: New. 	* gcc.dg/20020425-1.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@52779 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/stack1.C
@@ -0,0 +1,14 @@
+/* PR c/2161: parser stack overflow.  */
+/* { dg-do compile } */
+
+#define ONE	else if (0) { }
+#define TEN	ONE ONE ONE ONE ONE ONE ONE ONE ONE ONE
+#define HUN	TEN TEN TEN TEN TEN TEN TEN TEN TEN TEN
+#define THOU	HUN HUN HUN HUN HUN HUN HUN HUN HUN HUN
+
+void foo()
+{
+  if (0) { }
+  /* 11,000 else if's.  */
+  THOU THOU THOU THOU THOU THOU THOU THOU THOU THOU THOU
+}
