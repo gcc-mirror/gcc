48ba95387db27bf3d27d866b05b81f283ca9c5bd
Alexandre Oliva <aoliva@redhat.com>
2002-04-26 20:09:56 +0000

    * g++.dg/abi/enum1.C: New test.
    From-SVN: r52804

---
+++ gcc/testsuite/g++.dg/abi/enum1.C
@@ -0,0 +1,16 @@
+// { dg-do run }
+
+#include <cstdlib>
+
+enum foo
+{
+  foo1   = 0,
+  foo2   = 0xffffffffffffffffULL,
+  foo3   = 0xf0fffffffffffffeULL
+};
+
+int main (int i)
+{
+  if (sizeof (enum foo) != sizeof (unsigned long long))
+    std::abort ();
+}
