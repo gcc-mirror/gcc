82f637d15d8fa12277e8f6b5a79959ce9bc4d790
Benjamin Kosnik <bkoz@redhat.com>
2002-04-28 00:42:12 +0000

    os_defines.h: Remove _G_USING_THUNKS.
    2002-04-27  Benjamin Kosnik  <bkoz@redhat.com>
    
    	* config/os/solaris/solaris2.5/bits/os_defines.h: Remove
    	_G_USING_THUNKS.
    	* config/os/solaris/solaris2.6/bits/os_defines.h: Same.
    	* config/os/solaris/solaris2.7/bits/os_defines.h: Same.
    
    From-SVN: r52838

--- libstdc++-v3/config/os/solaris/solaris2.6/bits/os_defines.h
+++ libstdc++-v3/config/os/solaris/solaris2.6/bits/os_defines.h
@@ -1,6 +1,6 @@
 // Specific definitions for Solaris 2.6  -*- C++ -*-
 
-// Copyright (C) 2000 Free Software Foundation, Inc.
+// Copyright (C) 2000, 2002 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -38,8 +38,5 @@
 #define __off_t     off_t
 #define __off64_t   off64_t
 #define __ssize_t   ssize_t
-
-// Without this all the libio vtbls are offset wrongly.
-#define _G_USING_THUNKS 0
 
 #endif
