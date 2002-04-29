d675fcc0e6b94c43f894e443b997ac26529b57a9
gccadmin <gccadmin@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-04-29 07:17:01 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@52883 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,4 +1,4 @@
 #include "ansidecl.h"
 #include "f/version.h"
 
-const char *const ffe_version_string = "3.2 20020428 (experimental)";
+const char *const ffe_version_string = "3.2 20020429 (experimental)";
