792066cd4ffac169975abd230527e8fdfeb2c01f
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-05-01 23:17:57 +0000

    2002-05-01  Jeff Law  <law@redhat.com>
    	* pa.h (EXTRA_CONSTRAINT): Don't accept PIC addresses for the
    	'T' constraint.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@53027 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/pa/pa.h
+++ gcc/config/pa/pa.h
@@ -1240,6 +1240,7 @@ extern int may_call_alloca;
 			     ? GET_MODE (OP)		\
 			     : DFmode),			\
 			    XEXP (OP, 0))		\
+       && GET_CODE (XEXP (OP, 0)) != LO_SUM		\
        && !(GET_CODE (XEXP (OP, 0)) == PLUS		\
 	    && (GET_CODE (XEXP (XEXP (OP, 0), 0)) == MULT\
 		|| GET_CODE (XEXP (XEXP (OP, 0), 1)) == MULT)))\
