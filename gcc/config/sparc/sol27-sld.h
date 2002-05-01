233b817811b60907a525133f87c92734ad3ef743
Richard Henderson <rth@redhat.com>
2002-04-30 19:47:56 -0700

    sol27-sld.h: Rename from sol26-sld.h.
            * config/sparc/sol27-sld.h: Rename from sol26-sld.h.
            (PREFERRED_DEBUGGING_TYPE, ASM_DEBUG_SPEC): Undef.
            * config.gcc (sparc-solaris): Adjust test for native ld bug.
            Add it after the biarch headers.
    
    From-SVN: r52987

--- gcc/config/sparc/sol27-sld.h
+++ gcc/config/sparc/sol27-sld.h
@@ -1,6 +1,8 @@
-/* Up through Solaris 2.6, the system linker does not work with DWARF
+/* Up through Solaris 2.7, the system linker does not work with DWARF
    or DWARF2, since it does not have working support for relocations
    to unaligned data.  */
 
 #undef DWARF_DEBUGGING_INFO
 #undef DWARF2_DEBUGGING_INFO
+#undef PREFERRED_DEBUGGING_TYPE
+#undef ASM_DEBUG_SPEC
