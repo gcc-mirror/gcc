0d6d6892299f322c30f4e3df9ac9ec6f8a07f415
Aldy Hernandez <aldyh@redhat.com>
2002-05-02 02:03:14 +0000

    altivec-8.c: New.
    2002-05-02  Aldy Hernandez  <aldyh@redhat.com>
    
            * gcc.dg/altivec-8.c: New.
    
            * config/rs6000/rs6000.c (rs6000_legitimate_address): Disallow
            PRE_INC and PRE_DEC for altivec modes.
    
    From-SVN: r53031

---
+++ gcc/testsuite/gcc.dg/altivec-8.c
@@ -0,0 +1,18 @@
+/* Origin: Aldy Hernandez  <aldyh@redhat.com>  */
+/* Test rs6000_legitimate_address.  PRE_INC should be invalid.  */
+
+/* { dg-do compile { target powerpc-*-* } } */
+/* { dg-options "-maltivec" } */
+
+#include <altivec.h>
+
+vector signed short *hannah;
+
+int
+main ()
+{
+  *hannah++ = __builtin_altivec_vspltish (5);
+  *hannah++ = __builtin_altivec_vspltish (6);
+
+  return 0;
+}
