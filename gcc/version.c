2a8ed41ae42a65eb17c877ce349c06eb269a387f
GCC Administrator <gccadmin@gcc.gnu.org>
2002-05-04 07:17:40 +0000

    Daily bump.
    From-SVN: r53149

--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
                                                                                         #include "gansidecl.h"
 #include "version.h"
 
-const char *const version_string = "3.0.5 20020503 (prerelease)";
+const char *const version_string = "3.0.5 20020504 (prerelease)";
--- libf2c/libU77/Version.c
+++ libf2c/libU77/Version.c
@@ -1,6 +1,6 @@
                                                                                         static char junk[] = "\n@(#) LIBU77 VERSION 19980709\n";
 
-char __G77_LIBU77_VERSION__[] = "0.5.26 20020503 (prerelease)";
+char __G77_LIBU77_VERSION__[] = "0.5.26 20020504 (prerelease)";
 
 #include <stdio.h>
 
