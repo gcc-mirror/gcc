10302a5cec7eb3c25663108757dfd2f0598790e6
GCC Administrator <gccadmin@gcc.gnu.org>
2002-05-08 07:16:49 +0000

    Daily bump.
    From-SVN: r53282

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-                                                                                        const char *ffe_version_string = "0.5.25 20020507 (prerelease)";
+                                                                                        const char *ffe_version_string = "0.5.25 20020508 (prerelease)";
--- libf2c/libI77/Version.c
+++ libf2c/libI77/Version.c
@@ -3,7 +3,7 @@
 /*
 */
 
-char __G77_LIBI77_VERSION__[] = "0.5.25 20020507 (prerelease)";
+char __G77_LIBI77_VERSION__[] = "0.5.25 20020508 (prerelease)";
 
 /*
 2.01	$ format added
