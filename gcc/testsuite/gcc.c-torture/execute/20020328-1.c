7d35abf335a6f6d4858fafe935c6df21789f4a71
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-05-09 13:33:58 +0000

    Forgot to commit long long ago.  See my message to gcc-patches from Mar 28 :-)  Testcase for PR 3311.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@53328 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20020328-1.c
@@ -0,0 +1,25 @@
+int b = 0;
+
+func () { }
+
+void
+testit(int x)
+{
+  if (x != 20)
+    abort ();
+}
+
+int
+main()
+
+{
+  int a = 0;
+
+  if (b)
+    func();
+
+  /* simplify_and_const_int would incorrectly omit the mask in
+     the line below.  */
+  testit ((a + 23) & 0xfffffffc);
+  exit (0);
+}
