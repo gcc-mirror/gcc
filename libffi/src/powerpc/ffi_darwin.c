859f7aaf4122e4a2560153d0458371e556da2dc7
Andreas Tobler <a.tobler@schweiz.ch>
2002-05-13 22:19:10 +0200

    ffi_darwin.c (ffi_prep_closure): Declare fd at beginning of function (for older apple cc).
    2002-05-13  Andreas Tobler  <a.tobler@schweiz.ch>
    
            * src/powerpc/ffi_darwin.c (ffi_prep_closure): Declare fd at
            beginning of function (for older apple cc).
    
    From-SVN: r53430

--- libffi/src/powerpc/ffi_darwin.c
+++ libffi/src/powerpc/ffi_darwin.c
@@ -6,7 +6,7 @@
    Darwin ABI support (c) 2001 John Hornkvist
    AIX ABI support (c) 2002 Free Software Foundation, Inc.
 
-   $Id: ffi_darwin.c,v 1.3 2002/02/21 19:14:28 dje Exp $
+   $Id: ffi_darwin.c,v 1.4 2002/03/07 18:24:42 dje Exp $
 
    Permission is hereby granted, free of charge, to any person obtaining
    a copy of this software and associated documentation files (the
@@ -455,6 +455,7 @@ ffi_prep_closure (ffi_closure* closure,
 {
   unsigned int *tramp;
   struct ffi_aix_trampoline_struct *tramp_aix;
+  aix_fd *fd;
  
   switch (cif->abi)
     {  
@@ -486,7 +487,7 @@ ffi_prep_closure (ffi_closure* closure,
     case FFI_AIX:
 
       tramp_aix = (struct ffi_aix_trampoline_struct *) (closure->tramp);
-      aix_fd *fd = (aix_fd *)(void *)ffi_closure_ASM;
+      fd = (aix_fd *)(void *)ffi_closure_ASM;
 
       FFI_ASSERT (cif->abi == FFI_AIX);
 
