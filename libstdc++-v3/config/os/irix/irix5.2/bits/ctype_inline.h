a8b432f4def6548c398a660a2880517f20802fcc
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2002-05-13 13:52:08 +0000

    ctype_inline.h (scan_is, scan_not): Fix typo, use this->is() rather than manually (and perhaps incorrectly) inlining it.
    	* config/os/irix/irix5.2/bits/ctype_inline.h (scan_is, scan_not):
    	Fix typo, use this->is() rather than manually (and perhaps
    	incorrectly) inlining it.
    	* config/os/irix/irix6.5/bits/ctype_inline.h (scan_is, scan_not):
    	Likewise.
    	* testsuite/22_locale/ctype_scan_char.cc: New file.
    
    From-SVN: r53422

--- libstdc++-v3/config/os/irix/irix5.2/bits/ctype_inline.h
+++ libstdc++-v3/config/os/irix/irix5.2/bits/ctype_inline.h
@@ -52,8 +52,7 @@
   ctype<char>::
   scan_is(mask __m, const char* __low, const char* __high) const
   {
-    while (__low < __high 
-	   && !((_M_table)[static_cast<unsigned char>(*__low)] & __m))
+    while (__low < __high && ! this->is(__m, *__low))
       ++__low;
     return __low;
   }
@@ -62,8 +61,7 @@
   ctype<char>::
   scan_not(mask __m, const char* __low, const char* __high) const
   {
-    while (__low < __high 
-	   && ((_M_table + 1)[static_cast<unsigned char>(*__low)] & __m) != 0)
+    while (__low < __high && this->is(__m, *__low))
       ++__low;
     return __low;
   }
