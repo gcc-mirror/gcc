57529d080886722c2da5413c340a5cc71ab1dd3c
David Billinghurst <David.Billinghurst@riotinto.com>
2002-05-17 04:04:29 +0000

    weak-3.c: Add dg-excess-error comment for cygwin, coff and h8300-*-hms targets
    2002-05-16  David.Billinghurst <David.Billinghurst@riotinto.com>
    
    	* gcc.dg/weak-3.c: Add dg-excess-error comment for
    	cygwin, coff and h8300-*-hms targets
    	* gcc.dg/weak-5.c: Likewise
    	* gcc.dg/weak-7.c: Likewise
    
    From-SVN: r53540

--- gcc/testsuite/gcc.dg/weak-7.c
+++ gcc/testsuite/gcc.dg/weak-7.c
@@ -1,4 +1,5 @@
 /* { dg-do compile } */
+/* { dg-excess-errors "COFF does not support weak symbols" { target *-*-coff i?86-pc-cygwin h8300-*-hms } } */
 
 extern void * foo (void);
 void * foo (void) { return (void *)foo; } /* { dg-error "precede" } */
