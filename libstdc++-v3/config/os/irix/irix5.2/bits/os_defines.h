70bb012bdb090dddea0455aa3cd756b4f5880575
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2002-05-19 15:13:03 +0000

    * config/os/irix/irix5.2/bits/os_defines.h 	(__glibcpp_long_double_bits, __glibcpp_long_bits): Define.
    From-SVN: r53627

--- libstdc++-v3/config/os/irix/irix5.2/bits/os_defines.h
+++ libstdc++-v3/config/os/irix/irix5.2/bits/os_defines.h
@@ -1,6 +1,6 @@
 // Specific definitions for IRIX  -*- C++ -*-
 
-// Copyright (C) 2001 Free Software Foundation, Inc.
+// Copyright (C) 2001, 2002 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -48,7 +48,14 @@
 #define __off64_t off64_t
 #define __ssize_t ssize_t
 
-// GCC does not use thunks on IRIX.
+// GCC does not use thunks on IRIX. 
 #define _G_USING_THUNKS 0
 
+#define __glibcpp_long_double_bits 64
+
+#if __LONG_MAX__ > 2147483647
+#define __glibcpp_long_bits 64
 #endif
+
+#endif
+
