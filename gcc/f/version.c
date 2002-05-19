1c83a0bfc2a432e524b08b9e76f015cb73fa033d
GCC Administrator <gccadmin@gcc.gnu.org>
2002-05-19 07:21:02 +0000

    Daily bump.
    From-SVN: r53611

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,4 +1,4 @@
 #include "ansidecl.h"
 #include "f/version.h"
 
-const char *const ffe_version_string = "3.1.1 20020518 (prerelease)";
+const char *const ffe_version_string = "3.1.1 20020519 (prerelease)";
--- libf2c/libI77/Version.c
+++ libf2c/libI77/Version.c
@@ -3,7 +3,7 @@ static char junk[] = "\n@(#) LIBI77 VERSION pjw,dmg-mods 20001205\n";
 /*
 */
 
-char __G77_LIBI77_VERSION__[] = "3.1.1 20020518 (prerelease)";
+char __G77_LIBI77_VERSION__[] = "3.1.1 20020519 (prerelease)";
 
 /*
 2.01	$ format added
