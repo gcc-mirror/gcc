2271b1d49e1c3048cb87ee8ed09df1e20eba357c
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2002-05-19 15:16:30 +0000

    ctype_noninline.h (classic_table): Return __ctype + 1.
    	* config/os/irix/irix5.2/bits/ctype_noninline.h (classic_table):
    	Return __ctype + 1.
    	(ctype:_M_table): Initialize to classic_table(), not __ctype.
    
    From-SVN: r53628

--- libstdc++-v3/config/os/irix/irix5.2/bits/ctype_noninline.h
+++ libstdc++-v3/config/os/irix/irix5.2/bits/ctype_noninline.h
@@ -36,19 +36,19 @@
 
   const ctype_base::mask*
   ctype<char>::classic_table() throw()
-  { return 0; }
+  { return __ctype + 1; }
 
   ctype<char>::ctype(__c_locale, const mask* __table, bool __del, 
 		     size_t __refs)
   : __ctype_abstract_base<char>(__refs), _M_del(__table != 0 && __del), 
   _M_toupper(NULL), _M_tolower(NULL), 
-  _M_table(!__table ? __ctype : __table)
+  _M_table(!__table ? classic_table() : __table)
   { }
 
   ctype<char>::ctype(const mask* __table, bool __del, size_t __refs)
   : __ctype_abstract_base<char>(__refs), _M_del(__table != 0 && __del), 
   _M_toupper(NULL), _M_tolower(NULL), 
-  _M_table(!__table ? __ctype : __table)
+  _M_table(!__table ? classic_table() : __table)
   { }
 
   char
