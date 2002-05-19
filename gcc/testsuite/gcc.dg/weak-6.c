210142e0e23349d6494b9c500add989d0582674c
David Billinghurst <David.Billinghurst@riotinto.com>
2002-05-19 01:20:53 +0000

    weak-2.c: xfail warnings for platforms that don't support weak symbols.
    2002-05-19  David Billinghurst <David.Billinghurst@riotinto.com>
    
    	* gcc.dg/weak-2.c:  xfail warnings for platforms that don't
    	support weak symbols.
    	* gcc.dg/weak-4.c: Likewise
    	* gcc.dg/weak-6.c: Likewise
    
    From-SVN: r53601

--- gcc/testsuite/gcc.dg/weak-6.c
+++ gcc/testsuite/gcc.dg/weak-6.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 
 extern void * foo (void);
-void * foo (void) { return (void *)foo; } /* { dg-error "precede" } */
+void * foo (void) { return (void *)foo; } /* { dg-error "precede" "" { xfail *-*-coff i?86-pc-cygwin h8300-*-hms } } */
 
 #pragma weak foo
