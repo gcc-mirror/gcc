32d3f6346aa1a54bf186cf7498ecb46ce1b4a8d1
Brian R. Gaeke <brg@dgate.ORG>
2002-05-21 17:26:50 +0000

    * g++.dg/other/copy2.C: New test.
    From-SVN: r53692

---
+++ gcc/testsuite/g++.dg/other/copy2.C
@@ -0,0 +1,32 @@
+// { dg-do run }
+
+// Test that A's copy assignment method is called when B's instance
+// member array of A is assigned.
+
+// Contributed by Brian Gaeke, public domain.
+int status = 1;
+
+class A
+{
+public:
+  int i;
+  A &operator =(const A &i)
+  {
+    status = 0;
+  }
+};
+
+class B
+{
+public:
+  A arr[10];
+};
+
+int main (int argc, char **argv)
+{
+  B b;
+  b.arr[0].i = 15;
+  B a;
+  a = b; // trigger copy assignment
+  return status;
+}
