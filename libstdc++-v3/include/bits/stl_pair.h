d476dcad7f5d7b4c0a03d5a053e713c6b1b3a528
Phil Edwards <pme@gcc.gnu.org>
2002-05-21 21:09:11 +0000

    stl_pair.h: Tweak comment markup.
    2002-05-21  Phil Edwards  <pme@gcc.gnu.org>
    
    	* include/bits/stl_pair.h:  Tweak comment markup.
    
    From-SVN: r53700

--- libstdc++-v3/include/bits/stl_pair.h
+++ libstdc++-v3/include/bits/stl_pair.h
@@ -95,7 +95,7 @@ inline bool operator==(const pair<_T1, _T2>& __x, const pair<_T1, _T2>& __y)
   return __x.first == __y.first && __x.second == __y.second; 
 }
 
-/// http://gcc.gnu.org/onlinedocs/libstdc++/20_util/howto.html#pairlt
+/// <http://gcc.gnu.org/onlinedocs/libstdc++/20_util/howto.html#pairlt>
 template <class _T1, class _T2>
 inline bool operator<(const pair<_T1, _T2>& __x, const pair<_T1, _T2>& __y)
 { 
