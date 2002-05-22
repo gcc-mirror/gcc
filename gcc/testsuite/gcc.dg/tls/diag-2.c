7ce3d0dfc55205b03efb54a9154c202f2ea6a41c
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-05-22 01:12:13 +0000

    * gcc.dg/tls/tls.exp, gcc.dg/tls/trivial.c, gcc.dg/tls/diag-1.c,         gcc.dg/tls/diag-2.c, gcc.dg/tls/init-1.c: New directory and files.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@53716 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tls/diag-2.c
@@ -0,0 +1,21 @@
+/* Invalid __thread specifiers.  */
+
+__thread extern int g1;		/* { dg-error "`__thread' before `extern'" } */
+__thread static int g2;		/* { dg-error "`__thread' before `static'" } */
+__thread __thread int g3;	/* { dg-error "duplicate `__thread'" } */
+typedef __thread int g4;	/* { dg-error "multiple storage classes" } */
+
+void foo()
+{
+  __thread int l1;		/* { dg-error "implicitly auto and declared `__thread'" } */
+  auto __thread int l2;		/* { dg-error "multiple storage classes" } */
+  __thread extern int l3;	/* { dg-error "`__thread' before `extern'" } */
+  register __thread int l4;	/* { dg-error "multiple storage classes" } */
+}
+
+__thread void f1 ();		/* { dg-error "invalid storage class for function" } */
+extern __thread void f2 ();	/* { dg-error "invalid storage class for function" } */
+static __thread void f3 ();	/* { dg-error "invalid storage class for function" } */
+__thread void f4 () { }		/* { dg-error "function definition declared `__thread'" } */
+
+void bar(__thread int p1);	/* { dg-error "storage class specified for parameter" } */
---
+++ gcc/testsuite/gcc.dg/tls/trivial.c
@@ -0,0 +1,1 @@
+__thread int i;
