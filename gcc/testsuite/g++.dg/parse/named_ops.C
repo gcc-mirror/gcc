51d0da03d9dd675f734fca6a0ac9a93457bbf846
neil <neil@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-05-23 22:09:13 +0000

    testsuite: 	* gcc.dg/cpp/testsuite/named_ops.c: Remove. 	* g++.dg/parse/named_ops.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@53814 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/parse/named_ops.C
+++ gcc/testsuite/g++.dg/parse/named_ops.C
@@ -1,11 +1,13 @@
 /* Copyright (C) 2002 Free Software Foundation, Inc.  */
 
-/* { dg-do preprocess } */
+/* { dg-do compile } */
 /* { dg-options -fpreprocessed } */
 
 /* Tests that C++ named ops are still there with -fpreprocessed.  */
 
 /* Source: Neil Booth, 23 May 2002.  */
 
-#if 2 xor 2
-#endif
+int main ()
+{
+  return 2 xor 2;
+}
