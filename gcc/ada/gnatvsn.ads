73b85c375ceaa095487326e76e9710e43d367cc6
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-05-26 10:29:30 +0000

    gcc/ada: 	* gnatvsn.ads (Gnat_Version_String): Change to "3.2 20020526 	(experimental)".
    maintainer-scripts:
    	* gcc_release: Add gcc/ada/gnatvsn.ads to list of files in which
    	release status is updated.
    	* update_version (textstring_FILES): Add gcc/ada/gnatvsn.ads.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@53892 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/gnatvsn.ads
+++ gcc/ada/gnatvsn.ads
@@ -37,7 +37,7 @@
 
 package Gnatvsn is
 
-   Gnat_Version_String : constant String := "3.1 (20020212)";
+   Gnat_Version_String : constant String := "3.2 20020526 (experimental)";
    --  Version output when GNAT (compiler), or its related tools, including
    --  GNATBIND, GNATCHOP, GNATFIND, GNATLINK, GNATMAKE, GNATXREF, are run
    --  (with appropriate verbose option switch set).
