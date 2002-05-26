13c2f1da0b15269c6a858f34565af100f87e76ca
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-05-26 02:40:46 +0000

    New test for pr 6788
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@53881 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/opt/thunk1.C
@@ -0,0 +1,42 @@
+// PR 6788
+// Test that the thunk adjusts the this pointer properly.
+// { dg-do run }
+
+extern "C" void abort ();
+
+struct A
+{
+  virtual void foo() = 0;
+  char large[33*1024];
+};
+
+struct B
+{
+  virtual void foo() = 0;
+};
+
+struct C : public A, public B
+{
+  virtual void foo();
+};
+
+static C *match;
+
+void C::foo()
+{
+  if (this != match)
+    abort ();
+}
+
+void bar(B *x)
+{
+  x->foo();
+}
+
+int main()
+{
+  C obj;
+  match = &obj;
+  bar(&obj);
+  return 0;
+}
