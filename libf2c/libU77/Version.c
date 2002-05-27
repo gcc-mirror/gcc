6cc2e2dfd8cab46f546d9279a506d5f53a8ea7e3
GCC Administrator <gccadmin@gcc.gnu.org>
2002-05-27 07:17:43 +0000

    Daily bump.
    From-SVN: r53916

--- libf2c/libU77/Version.c
+++ libf2c/libU77/Version.c
@@ -1,6 +1,6 @@
                                                                                         static char junk[] = "\n@(#) LIBU77 VERSION 19980709\n";
 
-char __G77_LIBU77_VERSION__[] = "0.5.25 20020526 (prerelease)";
+char __G77_LIBU77_VERSION__[] = "0.5.25 20020527 (prerelease)";
 
 #include <stdio.h>
 
