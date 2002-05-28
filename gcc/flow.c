1da8586af0e0891fc526e6b33ff073242838c551
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-05-28 20:01:29 +0000

    * flow.c (calculate_global_regs_live): Rename call_used to         invalidated_by_call.  Initialize from regs_invalidated_by_call         instead of call_used_regs.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@53962 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/flow.c
+++ gcc/flow.c
@@ -1065,8 +1065,8 @@ calculate_global_regs_live (blocks_in, blocks_out, flags)
      int flags;
 {
   basic_block *queue, *qhead, *qtail, *qend, bb;
-  regset tmp, new_live_at_end, call_used;
-  regset_head tmp_head, call_used_head;
+  regset tmp, new_live_at_end, invalidated_by_call;
+  regset_head tmp_head, invalidated_by_call_head;
   regset_head new_live_at_end_head;
   int i;
 
@@ -1080,12 +1080,12 @@ calculate_global_regs_live (blocks_in, blocks_out, flags)
 
   tmp = INITIALIZE_REG_SET (tmp_head);
   new_live_at_end = INITIALIZE_REG_SET (new_live_at_end_head);
-  call_used = INITIALIZE_REG_SET (call_used_head);
+  invalidated_by_call = INITIALIZE_REG_SET (invalidated_by_call_head);
 
   /* Inconveniently, this is only readily available in hard reg set form.  */
   for (i = 0; i < FIRST_PSEUDO_REGISTER; ++i)
-    if (call_used_regs[i])
-      SET_REGNO_REG_SET (call_used, i);
+    if (TEST_HARD_REG_BIT (regs_invalidated_by_call, i))
+      SET_REGNO_REG_SET (invalidated_by_call, i);
 
   /* Create a worklist.  Allocate an extra slot for ENTRY_BLOCK, and one
      because the `head == tail' style test for an empty queue doesn't
@@ -1167,7 +1167,7 @@ calculate_global_regs_live (blocks_in, blocks_out, flags)
 	    if (e->flags & EDGE_EH)
 	      {
 		bitmap_operation (tmp, sb->global_live_at_start,
-				  call_used, BITMAP_AND_COMPL);
+				  invalidated_by_call, BITMAP_AND_COMPL);
 		IOR_REG_SET (new_live_at_end, tmp);
 	      }
 	    else
@@ -1335,7 +1335,7 @@ calculate_global_regs_live (blocks_in, blocks_out, flags)
 
   FREE_REG_SET (tmp);
   FREE_REG_SET (new_live_at_end);
-  FREE_REG_SET (call_used);
+  FREE_REG_SET (invalidated_by_call);
 
   if (blocks_out)
     {
