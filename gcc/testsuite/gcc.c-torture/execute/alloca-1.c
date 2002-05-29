e2c77eaf4a9c58d1aa71ed20ad4f556ca225ae11
No Author <no-author@gcc.gnu.org>
2002-05-29 20:34:28 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_1-branch'.
    From-SVN: r54019

---
+++ gcc/testsuite/gcc.c-torture/execute/alloca-1.c
@@ -0,0 +1,21 @@
+/* Verify that alloca storage is sufficiently aligned.  */
+/* ??? May fail if BIGGEST_ALIGNMENT > STACK_BOUNDARY.  Which, I guess
+   can only happen on !STRICT_ALIGNMENT targets.  */
+
+typedef __SIZE_TYPE__ size_t;
+
+struct dummy { int x __attribute__((aligned)); };
+#define BIGGEST_ALIGNMENT __alignof__(struct dummy)
+
+_Bool foo(void)
+{
+  char *p = __builtin_alloca(32);
+  return ((size_t)p & (BIGGEST_ALIGNMENT - 1)) == 0;
+}
+
+int main()
+{
+  if (!foo())
+    abort ();
+  return 0;
+}
