61b78ce13ff63efb2f669ec2c872c423ef4ad245
Richard Henderson <rth@redhat.com>
2002-06-02 11:11:21 -0700

    * gcc.dg/uninit-A.c: Remove xfail markers.
    From-SVN: r54177

--- gcc/testsuite/gcc.dg/uninit-A.c
+++ gcc/testsuite/gcc.dg/uninit-A.c
@@ -49,8 +49,8 @@ qualify_ambiguous_name (id)
      tree id;
 {
   tree qual, qual_wfl, decl;
-  tree name;	 /* { dg-bogus "name" "uninitialized variable warning" { xfail *-*-* } } */
-  tree ptr_type; /* { dg-bogus "ptr_type" "uninitialized variable warning" { xfail *-*-* } } */
+  tree name;	 /* { dg-bogus "name" "uninitialized variable warning" } */
+  tree ptr_type; /* { dg-bogus "ptr_type" "uninitialized variable warning" } */
   int again, new_array_found = 0;
   int super_found = 0, this_found = 0;
 
