3eb82013e62ddc46156ae95d0ae0eddc40a4988b
neil <neil@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-06-02 20:13:46 +0000

    * c-lex.c: Update copyright and file description.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@54185 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-lex.c
+++ gcc/c-lex.c
@@ -1,6 +1,6 @@
-/* Lexical analyzer for C and Objective C.
+/* Mainly the interface between cpplib and the C front ends.
    Copyright (C) 1987, 1988, 1989, 1992, 1994, 1995, 1996, 1997
-   1998, 1999, 2000 Free Software Foundation, Inc.
+   1998, 1999, 2000, 2001, 2002 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
