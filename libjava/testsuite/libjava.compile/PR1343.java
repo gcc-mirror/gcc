d6ee617dd7edc5cb721ad5cd320c47acd18e7ea8
Tom Tromey <tromey@redhat.com>
2002-06-04 19:39:02 +0000

    re PR libgcj/6389 (System.getProperty("") should always throw an IllegalArgumentException)
    	* libjava.compile/T20020604.java: New file.
    
    	For PR libgcj/6389:
    	* libjava.mauve/xfails: Now passes System.getProperty test.
    
    	For PR java/1343 and PR java/6336:
    	* libjava.compile/PR1343.java: New file.
    
    	For PR java/5913:
    	* libjava.compile/PR5913.xfail: Removed.
    
    From-SVN: r54259

---
+++ libjava/testsuite/libjava.compile/PR1343.java
@@ -0,0 +1,17 @@
+import java.util.Vector;
+
+public class PR1343
+{
+    void f(Object[] l)
+    {
+        class x1
+        {
+        };
+    }
+    void f(Vector l)
+    {
+        class x1
+        {
+        };
+    }
+}
