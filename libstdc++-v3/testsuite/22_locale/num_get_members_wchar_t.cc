36592c9008f5cce377af3217ab8129046a3a8426
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-06-04 13:38:07 +0000

    2002-06-04  Paolo Carlini  <pcarlini@unitus.it>
    	* testsuite/22_locale/money_get_members_char.cc
    	(test02): Add decimal point to long double constants.
    	* testsuite/22_locale/money_get_members_wchar_t.cc
    	(test02): Likewise.
    	* testsuite/22_locale/money_put_members_char.cc
    	(test02, test03, test06): Likewise.
    	* testsuite/22_locale/money_put_members_wchar_t.cc:
    	(test02, test03, test06): Likewise.
    	* testsuite/22_locale/num_get_members_char.cc:
    	(test02, test01): Likewise; suffix long long constants with LL.
    	* testsuite/22_locale/num_get_members_wchar_t.cc:
    	(test02, test01): Likewise.
    	* testsuite/22_locale/num_put_members_char.cc:
    	(test02, test01): Likewise.
    	* testsuite/22_locale/num_put_members_wchar_t.cc:
    	(test02, test01): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@54243 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/22_locale/num_get_members_wchar_t.cc
+++ libstdc++-v3/testsuite/22_locale/num_get_members_wchar_t.cc
@@ -223,8 +223,8 @@ void test01()
 
 
 #ifdef _GLIBCPP_USE_LONG_LONG
-  long long ll1 = 9223372036854775807;
-  long long ll2 = -9223372036854775807;
+  long long ll1 = 9223372036854775807LL;
+  long long ll2 = -9223372036854775807LL;
   long long ll;
 
   iss.str(L"9.223.372.036.854.775.807");
@@ -275,7 +275,7 @@ void test02()
   VERIFY( rem1 == L" Elizabeth Durack" );
 
   // 02 get(long double)
-  long double ld = 0;
+  long double ld = 0.0;
   err = goodbit;
   iter_type end2 = ng.get(str.begin(), str.end(), iss, err, ld);
   wstring rem2(end2, str.end());
