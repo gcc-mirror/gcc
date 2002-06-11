958c2bb36eeee8b349c021ffc679374988600a4f
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-06-11 03:50:30 +0000

    * libjava.lang/PR6520.java: New file.  For PR java/6520.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@54482 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/testsuite/libjava.lang/PR6520.java
@@ -0,0 +1,13 @@
+public class pr6520
+{
+   public static void check (double x, double y)
+   {
+     System.out.println (x == y);
+   }
+ 
+   public static void main(String[] args)
+   {
+     check (Math.min (2.0f, Float.NaN), Float.NaN);
+   }
+ }
+
