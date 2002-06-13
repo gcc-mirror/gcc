2acf0fa92efaabede6cee1b5ff4732b4c325043c
David S. Miller <davem@redhat.com>
2002-06-13 02:37:19 -0700

    expmed.c (init_expmed): Remove duplicate init of 'reg'.
    2002-06-13  David S. Miller  <davem@redhat.com>
    
    	* expmed.c (init_expmed): Remove duplicate init of 'reg'.
    
    From-SVN: r54580

--- gcc/expmed.c
+++ gcc/expmed.c
@@ -104,16 +104,15 @@ static int mul_highpart_cost[NUM_MACHINE_MODES];
 void
 init_expmed ()
 {
-  /* This is "some random pseudo register" for purposes of calling recog
-     to see what insns exist.  */
-  rtx reg = gen_rtx_REG (word_mode, 10000);
-  rtx shift_insn, shiftadd_insn, shiftsub_insn;
+  rtx reg, shift_insn, shiftadd_insn, shiftsub_insn;
   int dummy;
   int m;
   enum machine_mode mode, wider_mode;
 
   start_sequence ();
 
+  /* This is "some random pseudo register" for purposes of calling recog
+     to see what insns exist.  */
   reg = gen_rtx_REG (word_mode, 10000);
 
   zero_cost = rtx_cost (const0_rtx, 0);
