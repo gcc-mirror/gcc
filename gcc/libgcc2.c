912ce2971a2fc65ca9684e11b23300d23e8ffbc8
Jeff Law <law@redhat.com>
2002-06-17 15:08:12 -0600

    * libgcc2.c: Do not include symcat.h or machmode.h.
    From-SVN: r54717

--- gcc/libgcc2.c
+++ gcc/libgcc2.c
@@ -36,9 +36,6 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #include "tconfig.h"
 #include "tsystem.h"
 
-#include "symcat.h"
-#include "machmode.h"
-
 /* Don't use `fancy_abort' here even if config.h says to use it.  */
 #ifdef abort
 #undef abort
