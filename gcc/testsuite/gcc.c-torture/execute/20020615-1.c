92ea7f4201cb140cecbc5b0a2ed4fc03fedc52cf
hp <hp@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-06-17 23:29:39 +0000

    * gcc.c-torture/execute/20020615-1.c: Correct bug-description.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@54723 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/20020615-1.c
+++ gcc/testsuite/gcc.c-torture/execute/20020615-1.c
@@ -1,8 +1,8 @@
-/* PR target/7042.  In cris.md the return pattern and its condition
-   had a bug uncovered by the combination of saved registers and
-   filled delay slot, where the delay-slot insn set the
-   return-value register.  Derived from ghostscript-6.52 (GPL)
-   by hp@axis.com.  */
+/* PR target/7042.  When reorg.c changed branches into return insns, it
+   completely forgot about any current_function_epilogue_delay_list and
+   drops those insns.  Uncovered on cris-axis-elf, where an insn in an
+   epilogue delay-slot set the return-value register with the test-case
+   below.  Derived from ghostscript-6.52 (GPL) by hp@axis.com.  */
 
 typedef struct font_hints_s {
   int axes_swapped;
