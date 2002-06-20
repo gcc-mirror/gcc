12d0271cfd53d04e04009cc251793c9af2a30d7e
Benjamin Kosnik <bkoz@redhat.com>
2002-06-20 20:13:46 +0000

    ext-inst.cc (__gnu_cxx): Use instead of std:: for extensions.
    2002-06-20  Benjamin Kosnik  <bkoz@redhat.com>
    
            * src/ext-inst.cc (__gnu_cxx): Use instead of std:: for extensions.
            Use size_type instead of unsigned long.
    
    From-SVN: r54854

--- libstdc++-v3/src/ext-inst.cc
+++ libstdc++-v3/src/ext-inst.cc
@@ -34,34 +34,29 @@
 #include <ext/rope>
 #include <ext/stdio_filebuf.h>
 
-namespace std
+namespace __gnu_cxx
 {
-  using __gnu_cxx::stdio_filebuf;
-
   template
     const unsigned long 
-    __gnu_cxx::rope<char, std::allocator<char> >::_S_min_len;
+    rope<char, std::allocator<char> >::_S_min_len;
 
   template
     char
-    __gnu_cxx::rope<char, std::allocator<char> >::
-    _S_fetch(__gnu_cxx::_Rope_RopeRep<char, std::allocator<char> >*,
-	     unsigned long);
+    rope<char, std::allocator<char> >::
+    _S_fetch(_Rope_RopeRep<char, std::allocator<char> >*, size_type);
 
   template class stdio_filebuf<char>;
 
 #ifdef _GLIBCPP_USE_WCHAR_T
   template
     const unsigned long
-    __gnu_cxx::rope<wchar_t, std::allocator<wchar_t> >::_S_min_len;
+    rope<wchar_t, std::allocator<wchar_t> >::_S_min_len;
 
   template
     wchar_t
-    __gnu_cxx::rope<wchar_t, std::allocator<wchar_t> >::
-    _S_fetch(__gnu_cxx::_Rope_RopeRep<wchar_t, std::allocator<wchar_t> >*,
-	     unsigned long);
+    rope<wchar_t, std::allocator<wchar_t> >::
+    _S_fetch(_Rope_RopeRep<wchar_t, std::allocator<wchar_t> >*, size_type);
 
   template class stdio_filebuf<wchar_t>;
 #endif
-
-} // namespace std
+} // namespace __gnu_cxx
