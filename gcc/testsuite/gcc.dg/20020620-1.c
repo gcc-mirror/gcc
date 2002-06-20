9164f47a9dbbd98fc19182716f1f69f2ab69ce9f
Richard Sandiford <rsandifo@redhat.com>
2002-06-20 18:07:29 +0000

    * gcc.dg/20020620-1.c: New test.
    From-SVN: r54847

---
+++ gcc/testsuite/gcc.dg/20020620-1.c
@@ -0,0 +1,7 @@
+/* { dg-do compile { target mips64*-*-* } } */
+/* { dg-options "-O2 -mlong64" } */
+int foo (int *x, int i)
+{
+  return x[i] + i;
+}
+/* { dg-final { scan-assembler-not "move" } } */
