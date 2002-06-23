f4e62fcfd3d22a51bc5945e6be8b9ac38e5acdc6
aj <aj@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-06-23 05:30:14 +0000

    * gcc.c-torture/execute/complex-6.c: New.
    	* reg-stack.c (convert_regs_exit): Push the registers to stack in
    	proper order.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@54915 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/complex-6.c
@@ -0,0 +1,59 @@
+/* This test tests complex conjugate and passing/returning of
+   complex parameter.  */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int err;
+
+#define TEST(TYPE, FUNC)					\
+__complex__ TYPE						\
+ctest_ ## FUNC (__complex__ TYPE x)				\
+{								\
+  __complex__ TYPE res;						\
+								\
+  res = ~x;							\
+								\
+  return res;							\
+}								\
+								\
+void								\
+test_ ## FUNC (void)						\
+{								\
+  __complex__ TYPE res, x;					\
+								\
+  x = 1.0 + 2.0i;						\
+								\
+  res = ctest_ ## FUNC (x);					\
+								\
+  if (res != 1.0 - 2.0i)					\
+    {								\
+      printf ("test_" #FUNC " failed\n");			\
+      ++err;							\
+    }								\
+}
+
+
+TEST(float, float)
+TEST(double, double)
+TEST(long double, long_double)
+TEST(int, int)
+TEST(long int, long_int)
+
+int
+main (void)
+{
+
+  err = 0;
+
+  test_float ();
+  test_double ();
+  test_long_double ();
+  test_int ();
+  test_long_int ();
+
+  if (err != 0)
+    abort ();
+
+  return 0;
+}
