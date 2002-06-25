d7afb332a58bacfbef31115c7cc9c4c02e4367bd
schwab <schwab@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-06-25 13:27:19 +0000

    * expr.c (JSR): Avoid undefined operation on PC.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@54987 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/java/expr.c
+++ gcc/java/expr.c
@@ -2946,7 +2946,11 @@ process_jvm_instruction (PC, byte_ops, length)
   }
 
 #define JSR(OPERAND_TYPE, OPERAND_VALUE) \
-  build_java_jsr (oldpc+OPERAND_VALUE, PC);
+  {						    \
+    /* OPERAND_VALUE may have side-effects on PC */ \
+    int opvalue = OPERAND_VALUE;		    \
+    build_java_jsr (oldpc + opvalue, PC);	    \
+  }
 
 /* Push a constant onto the stack. */
 #define PUSHC(OPERAND_TYPE, OPERAND_VALUE) \
