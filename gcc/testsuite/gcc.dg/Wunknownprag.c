5ae4caef64b121ef89043f62438b048d88f7a7e0
neil <neil@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-06-27 05:59:53 +0000

    PR preprocessor/7070 	* c-lex.c (cb_def_pragma): Don't try to spell CPP_EOF. testsuite: 	* gcc.dg/Wunknownprag.c: New tests.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@55021 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/Wunknownprag.c
@@ -0,0 +1,11 @@
+/* Copyright (C) 2002 Free Software Foundation, Inc.  */
+
+/* { dg-do compile } */
+/* { dg-options "-Wunknown-pragmas" } */
+
+/* We used to get "unspellable token: CPP_EOF" warnings.  */
+
+#pragma				/* { dg-warning "ignoring #pragma" } */
+#pragma ~			/* { dg-warning "ignoring #pragma" } */
+#pragma baz			/* { dg-warning "ignoring #pragma" } */
+#pragma baz baz			/* { dg-warning "ignoring #pragma" } */
