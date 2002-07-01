7098b619a0ed14eb3255417bf33923564035329e
Neil Booth <neil@daikokuya.co.uk>
2002-07-01 19:41:52 +0000

    cmdlne-dD.c, [...]: New tests.
    	* gcc.dg/cpp/trad/cmdlne-dD.c, gcc.dg/cpp/trad/cmdlne-dM.c,
    	gcc.dg/cpp/trad/include.c: New tests.
    
    From-SVN: r55146

---
+++ gcc/testsuite/gcc.dg/cpp/trad/cmdlne-dM.c
@@ -0,0 +1,9 @@
+/* Copyright (C) 2000, 2002 Free Software Foundation, Inc.  */
+
+/* { dg-do preprocess } */
+/* { dg-options "-dM -traditional-cpp" } */
+
+/* Test -dM does not fail.  */
+
+#define objlike obj like
+#define funlike(like) fun like
