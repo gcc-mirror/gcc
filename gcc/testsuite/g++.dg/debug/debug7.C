9833f6792a3fc80658167415883160d47ddd68b4
Mark Mitchell <mark@codesourcery.com>
2002-07-05 16:40:01 +0000

    re PR c++/6706 (ICE with variable-sized arrays and DWARF-1 output)
    	PR c++/6706
    	* dwarfout.c (output_reg_number): Fix warning message.
    	(output_bound_representation): Check SAVE_EXPR_RTL is not NULL
    	before using it.
    
    	PR c++/6706
    	* g++.dg/debug/debug6.C: New test.
    	* g++.dg/debug/debug7.C: New test.
    
    From-SVN: r55264

---
+++ gcc/testsuite/g++.dg/debug/debug7.C
@@ -0,0 +1,18 @@
+// { dg-do compile }
+
+void f (int);
+
+int
+main() {
+
+  int a = 4;
+  int b = 5;
+  int (*x)[b] = new int[a][b];
+
+  x[2][1] = 7;
+
+  for (int i = 0; i < a; ++i)
+    for (int j = 0; j < b; ++j)
+      f (x[i][j]);
+  delete [] x;
+}
