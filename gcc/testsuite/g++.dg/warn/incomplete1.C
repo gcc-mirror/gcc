41c7e3cbd716c3fcc46c205be3c0e2c77aa69d78
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-07-06 16:58:39 +0000

    * g++.dg/warn/incomplete1.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@55289 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/warn/incomplete1.C
@@ -0,0 +1,21 @@
+// { dg-do compile }
+
+// Contributed by Brian Gaeke; public domain.
+
+// 5 If the object being deleted has incomplete class type at the
+// point of deletion and the complete class has a non-trivial
+// destructor or a deallocation function, the behavior is undefined.
+
+// (But the deletion does not constitute an ill-formed program. So the
+// program should nevertheless compile, but it should give a warning.)
+
+class A;	// { dg-warning "forward declaration of `struct A'" "" }
+
+A *a;		// { dg-warning "`a' has incomplete type" "" }
+
+int
+main (int argc, char **argv)
+{
+  delete a;	// { dg-warning "delete" "" { xfail *-*-* } }
+  return 0;
+}
