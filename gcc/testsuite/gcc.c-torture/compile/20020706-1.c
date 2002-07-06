e66c864962a7b8c2564e802fa630283b0353082e
Alexandre Oliva <aoliva@redhat.com>
2002-07-06 17:19:51 +0000

    20020706-1.c: New test.
    * gcc.c-torture/compile/20020706-1.c: New test.
    * gcc.c-torture/compile/20020706-2.c: New test.
    
    From-SVN: r55290

---
+++ gcc/testsuite/gcc.c-torture/compile/20020706-1.c
@@ -0,0 +1,50 @@
+// Contributed by Alexandre Oliva <aoliva@redhat.com>
+// From Red Hat case 106165.
+
+typedef struct s1
+{
+  unsigned short v1;
+  unsigned char *v2;
+} S1;
+
+extern void bar(const struct s1 *const hdb);
+extern unsigned char* foo ();
+
+unsigned int sn;
+S1 *hdb;
+S1 *pb;
+unsigned short len;
+
+unsigned int crashIt()
+{
+  unsigned char *p;
+  unsigned int nsn;
+  unsigned short cnt;
+
+  if (sn != 0) return 1;
+
+  if ((len < 12) || ((p = (((pb->v1) >= 8) ? pb->v2 : foo() )) == 0))
+    return 1;
+
+  nsn = (
+	 (((*(unsigned int*)p) & 0x000000ff) << 24) |
+	 (((*(unsigned int*)p) & 0x0000ff00) << 8)  |
+	 (((*(unsigned int*)p) & 0x00ff0000) >> 8)  |
+	 (((*(unsigned int*)p) & 0xff000000) >> 24)  );
+  p += 4;
+
+  cnt = (unsigned short) ((
+			   (((*(unsigned int*)p) & 0x000000ff) << 24) |
+			   (((*(unsigned int*)p) & 0x0000ff00) << 8)  |
+			   (((*(unsigned int*)p) & 0x00ff0000) >> 8)  |
+			   (((*(unsigned int*)p) & 0xff000000) >> 24)  ) &
+			  0xffff);
+
+  if ((len != 12 + (cnt * 56)) || (nsn == 0))
+    {
+      bar(hdb);
+      return 1;
+    }
+
+  return 0;
+}
