8d65dd46a7e839e857a3110a5fa99010d90d9d2d
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-07-06 17:19:51 +0000

    * gcc.c-torture/compile/20020706-1.c: New test. * gcc.c-torture/compile/20020706-2.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@55290 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/20020706-2.c
@@ -0,0 +1,26 @@
+// Contributed by Alexandre Oliva <aoliva@redhat.com>
+// From Red Hat case 106165.
+
+typedef unsigned short (FUNC_P) (void *, unsigned char *, unsigned short);
+
+void crashIt(int id, FUNC_P *func, unsigned char *funcparm)
+{
+  unsigned char buff[5], reverse[4];
+  unsigned char *bp = buff;
+  unsigned char *rp = reverse;
+  unsigned short int count = 0;
+  unsigned short cnt;
+  while (id > 0)
+    {
+      *rp++ = (unsigned char) (id & 0x7F);
+      id >>= 7;
+      count++;
+    }
+  cnt = count + 1;
+  while ((count--) > 1)
+    {
+      *bp++ = (unsigned char)(*(--rp) | 0x80);
+    }
+  *bp++ = *(--rp);
+  (void)(*func)(funcparm, buff, cnt);
+}
