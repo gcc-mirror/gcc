07bffcb8aae4fc2af555d2b1bf0a21b9463f7c82
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-07-06 00:25:30 +0000

    2002-07-05  Kaveh R. Ghazi  <ghazi@caip.rutgers.edu>
            PR c++/7099
            * g++.dg/warn/noreturn1.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@55277 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/warn/noreturn-1.C
@@ -0,0 +1,71 @@
+// Test that noreturn attributes are properly set.
+// Origin: Kaveh Ghazi <ghazi@caip.rutgers.edu> 2002-06-18.
+// { dg-do compile }
+// { dg-options "-Wall -O2" }
+
+#include <cstdlib>
+
+int foo1 (int i)
+{
+  switch (i)
+    {
+    case 1:
+    case 2:
+      return i;
+    }
+  abort();
+}
+
+int foo2 (int i)
+{
+  switch (i)
+    {
+    case 1:
+    case 2:
+      return i;
+    }
+  std::abort();
+}
+
+int foo3 (int i)
+{
+  switch (i)
+    {
+    case 1:
+    case 2:
+      return i;
+    }
+  exit(1);
+}
+
+int foo4 (int i)
+{
+  switch (i)
+    {
+    case 1:
+    case 2:
+      return i;
+    }
+  std::exit(1);
+}
+
+void __attribute__ ((__noreturn__)) foo5 ()
+{
+  abort();
+}
+
+void __attribute__ ((__noreturn__)) foo6 ()
+{
+  std::abort();
+}
+
+void __attribute__ ((__noreturn__)) foo7 ()
+{
+  exit(1);
+}
+
+void __attribute__ ((__noreturn__)) foo8 ()
+{
+  std::exit(1);
+}
+
