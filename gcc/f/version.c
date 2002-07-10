186728e5ec02e08ebaec90960b27f4154a6fbb07
GCC Administrator <gccadmin@gcc.gnu.org>
2002-07-10 07:17:19 +0000

    Daily bump.
    From-SVN: r55362

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-                                                                                        const char *ffe_version_string = "0.5.25 20020709 (prerelease)";
+                                                                                        const char *ffe_version_string = "0.5.25 20020710 (prerelease)";
