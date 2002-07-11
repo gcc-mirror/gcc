b70fc53b64a55140d7a3077b7914002e173ec401
GCC Administrator <gccadmin@gcc.gnu.org>
2002-07-11 07:16:52 +0000

    Daily bump.
    From-SVN: r55394

--- gcc/ada/gnatvsn.ads
+++ gcc/ada/gnatvsn.ads
@@ -37,7 +37,7 @@
 
 package Gnatvsn is
 
-   Gnat_Version_String : constant String := "3.2 20020710 (experimental)";
+   Gnat_Version_String : constant String := "3.2 20020711 (experimental)";
    --  Version output when GNAT (compiler), or its related tools, including
    --  GNATBIND, GNATCHOP, GNATFIND, GNATLINK, GNATMAKE, GNATXREF, are run
    --  (with appropriate verbose option switch set).
