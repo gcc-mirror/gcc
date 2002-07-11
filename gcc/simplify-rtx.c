226cfe61eadd70c97919b77040d8a81b9f65ecb9
J"orn Rennecke <joern.rennecke@superh.com>
2002-07-11 23:53:01 +0000

    simplify-rtx.c (simplify_subreg): Handle floating point CONST_DOUBLEs.
    gcc:
    Thu Jul 11 15:39:21 2002  J"orn Rennecke <joern.rennecke@superh.com>
    
    	* simplify-rtx.c (simplify_subreg): Handle floating point
    	CONST_DOUBLEs.  When an integer subreg of a smaller mode than
    	the element mode is requested, compute a subreg with an
    	integer mode of the same size as the element mode first.
    
    testsuite:
    Thu Jul 11 15:39:21 2002  J"orn Rennecke <joern.rennecke@superh.com>
                              Andrew Pinski  <pinskia@physics.uc.edu>
    
    	gcc.c-torture/compile/simd-2.c: New testcase.
    	gcc.c-torture/compile/simd-3.c: Likewise.
    
    Co-Authored-By: Andrew Pinski <pinskia@physics.uc.edu>
    
    From-SVN: r55410

--- gcc/simplify-rtx.c
+++ gcc/simplify-rtx.c
@@ -2307,6 +2307,14 @@ simplify_subreg (outermode, op, innermode, byte)
 	  for (; n_elts--; i += step)
 	    {
 	      elt = CONST_VECTOR_ELT (op, i);
+	      if (GET_CODE (elt) == CONST_DOUBLE
+		  && GET_MODE_CLASS (GET_MODE (elt)) == MODE_FLOAT)
+		{
+		  elt = gen_lowpart_common (int_mode_for_mode (GET_MODE (elt)),
+					    elt);
+		  if (! elt)
+		    return NULL_RTX;
+		}
 	      if (GET_CODE (elt) != CONST_INT)
 		return NULL_RTX;
 	      high = high << shift | sum >> (HOST_BITS_PER_WIDE_INT - shift);
@@ -2318,6 +2326,18 @@ simplify_subreg (outermode, op, innermode, byte)
 	    return immed_double_const (high, sum, outermode);
 	  else
 	    return NULL_RTX;
+	}
+      else if (GET_MODE_CLASS (outermode) == MODE_INT
+	       && (elt_size % GET_MODE_SIZE (outermode) == 0))
+	{
+	  enum machine_mode new_mode
+	    = int_mode_for_mode (GET_MODE_INNER (innermode));
+	  int subbyte = byte % elt_size;
+
+	  op = simplify_subreg (new_mode, op, innermode, byte - subbyte);
+	    if (! op)
+	      return NULL_RTX;
+	  return simplify_subreg (outermode, op, new_mode, subbyte);
 	}
       else if (GET_MODE_CLASS (outermode) != MODE_VECTOR_INT
 	       && GET_MODE_CLASS (outermode) != MODE_VECTOR_FLOAT)
