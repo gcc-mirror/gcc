58c1eac972da676c119d4687176c5c8bb4a712ab
mark <mark@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-07-14 22:18:35 +0000

    2002-07-14� Mark Wielaard� <mark@klomp.org>
        * gnu/java/security/der/DEREncodingException.java,
        gnu/java/security/provider/DERReader.java,
        gnu/java/security/provider/DERWriter.java,
        gnu/java/security/provider/DSAKeyPairGenerator.java,
        gnu/java/security/provider/DSAParameterGenerator.java,
        gnu/java/security/provider/DSAParameters.java,
        gnu/java/security/provider/DSASignature.java,
        gnu/java/security/provider/GnuDSAPrivateKey.java,
        gnu/java/security/provider/GnuDSAPublicKey.java,
        gnu/java/security/provider/MD5.java,
        gnu/java/security/util/Prime.java: New files from Classpath.
        * Makefile.am (ordinary_java_source_files): Add new files.
        * Makefile.in: Regenerate.
    
    2002-07-14� C. Brian Jones <cbj@gnu.org>
    
        * gnu/java/security/provider/DefaultPolicy.java
        (getPermissions): do not maintain static class variable of
        Permissions
        * gnu/java/security/provider/SHA.java
        (engineUpdate): algorithm change
        (engineDigest): algorithm change
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@55444 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/gnu/java/security/der/DEREncodingException.java
+++ libjava/gnu/java/security/der/DEREncodingException.java
@@ -1,5 +1,5 @@
-/* DefaultPolicy.java 
-   Copyright (C) 2001, 2002 Free Software Foundation, Inc.
+/* DEREncodingException.java --- DER Encoding Exception
+   Copyright (C) 1999 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
@@ -35,29 +35,19 @@ this exception to your version of the library, but you are not
 obligated to do so.  If you do not wish to do so, delete this
 exception statement from your version. */
 
-package gnu.java.security.provider;
 
-import java.security.*;
+package gnu.java.security.der;
 
-/** 
-  * This is just a stub policy implementation which grants all permissions
-  * to any code source. FIXME: This should be replaced with a real
-  * implementation that reads the policy configuration from a file, like
-  * $JAVA_HOME/jre/lib/security/java.security.
-  */
-public class DefaultPolicy extends Policy
+public class DEREncodingException extends Exception
 {
-  static Permission allPermission = new AllPermission();
 
-  public PermissionCollection getPermissions(CodeSource codesource)
+  public DEREncodingException()
   {
-    Permissions perms = new Permissions();
-    perms.add(allPermission);
-    return perms;
+    super ();
   }
   
-  public void refresh()
+  public DEREncodingException (String msg)
   {
-    // Nothing.
+    super (msg);
   }
 }
