db50171f40e718db91d21f13828a2e74f879c04c
Jason Merrill <jason@gcc.gnu.org>
2002-07-17 12:43:42 -0400

    mangle.c (write_unqualified_name): Handle template parm subtitution.
    * mangle.c (write_unqualified_name): Handle template parm
    subtitution.
    * class.c (build_base_field): Set DECL_PACKED.
    (layout_class_type): Don't use tail padding of PODs.
    
    From-SVN: r55535

---
+++ gcc/testsuite/g++.dg/abi/mangle8.C
@@ -0,0 +1,16 @@
+// Red Hat bugzilla 65035
+// Bug: We were encoding the name of the instantiation as 'operator int'
+// rather than 'operator T'.
+// { dg-do compile }
+
+struct C {
+    template <class T>
+    operator T ();
+};
+
+template <class T>
+C::operator T () { return 0; }
+
+template C::operator int ();
+
+// { dg-final { scan-assembler _ZN1CcvT_IiEEv } }
