ba59df78eb56fa25667da86a5e6ae401a2960a22
Kriang Lerdsuwanakij <lerdsuwa@users.sourceforge.net>
2002-07-17 13:56:46 +0000

    PR c++/4802, c++/5387
    	PR c++/4802, c++/5387
    	* decl.c (make_typename_type): Use enforce_access.
    
    	* g++.dg/template/access2.C: New test.
    	* g++.dg/template/access3.C: New test.
    
    From-SVN: r55517

---
+++ gcc/testsuite/g++.dg/template/access3.C
@@ -0,0 +1,17 @@
+// { dg-do compile }
+
+// PR c++/5387
+// Enforcing access of typename type.
+
+template <class T> struct A {
+  typename T::X<int> x;			// { dg-error "this context" }
+};
+
+class B {
+  template <class T> class X {};	// { dg-error "private" }
+};
+
+int main()
+{
+  A<B> ab;				// { dg-error "instantiated" }
+}
