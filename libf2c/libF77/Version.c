14219b37d30e3f7d373037aaba43f3720b14705d
GCC Administrator <gccadmin@gcc.gnu.org>
2002-07-18 07:17:37 +0000

    Daily bump.
    From-SVN: r55550

--- libf2c/libF77/Version.c
+++ libf2c/libF77/Version.c
@@ -3,7 +3,7 @@
 /*
 */
 
-char __G77_LIBF77_VERSION__[] = "0.5.26 20020717 (prerelease)";
+char __G77_LIBF77_VERSION__[] = "0.5.26 20020718 (prerelease)";
 
 /*
 2.00	11 June 1980.  File version.c added to library.
