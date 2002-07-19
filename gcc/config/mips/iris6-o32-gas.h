59267987dbc7bc330557f4fdfdb574fc797f6c84
Rainer Orth <ro@TechFak.Uni-Bielefeld.DE>
2002-07-19 21:58:12 +0000

    iris5gas.h (DWARF2_DEBUGGING_INFO): Define.
    	* config/mips/iris5gas.h (DWARF2_DEBUGGING_INFO): Define.
    	(PREFERRED_DEBUGGING_TYPE): Use DWARF2_DEBUG.
    	(LINK_SPEC): Define.
    	(STARTFILE_SPEC): Define.
    	(ENDFILE_SPEC): Define.
    
    	* config/mips/iris6-o32.h (LINK_SPEC): Move ...
    	* config/mips/iris6-o32-as.h (LINK_SPEC): ... here.
    
    	* config/mips/iris6-o32-gas.h: New file.
    	* config.gcc (mips-sgi-irix6*o32): Use it.
    
    	* config/mips/t-iris5-gas: New file.
    	* config.gcc (mips-sgi-irix6*o32, mips-sgi-irix5*): Use it.
    
    From-SVN: r55596

--- gcc/config/mips/iris6-o32-gas.h
+++ gcc/config/mips/iris6-o32-gas.h
@@ -1,21 +1,10 @@
 /* Definitions of target machine for GNU compiler, for MIPS running IRIX 6
-   (O32 ABI) using the SGI assembler.  */
-
-/* Override mips.h default: the IRIX 6 O32 assembler warns about -O3:
-
-   as: Warning: -O3 is not supported for assembly compiles for ucode
-   compilers; changing to -O2.
-   
-   So avoid passing it in the first place.  */
-#undef SUBTARGET_ASM_OPTIMIZING_SPEC
-#define SUBTARGET_ASM_OPTIMIZING_SPEC "\
-%{noasmopt:-O0} \
-%{!noasmopt:%{O|O1|O2|O3:-O2}}"
+   (O32 ABI) using the GNU assembler.  */
 
 /* Enforce use of O32 linker, irrespective of SGI_ABI environment variable
    and machine type (e.g., R8000 systems default to -64).  Copied from
-   iris5.h, only adding -32.  The default options -call_shared -no_unresolved
-   are only passed if not invoked with -r.  */
+   iris5gas.h, only adding -32.  The default options -call_shared
+   -no_unresolved are only passed if not invoked with -r.  */
 #undef LINK_SPEC
 #define LINK_SPEC "\
 %{G*} %{EB} %{EL} %{mips1} %{mips2} %{mips3} \
@@ -24,6 +13,7 @@
 %{static: -non_shared} \
 %{!static: \
   %{!shared:%{!non_shared:%{!call_shared:%{!r: -call_shared -no_unresolved}}}}} \
-%{rpath} \
+%{rpath} -init __do_global_ctors -fini __do_global_dtors \
+%{shared:-hidden_symbol __do_global_ctors,__do_global_ctors_1,__do_global_dtors} \
 -_SYSTYPE_SVR4 \
 -32"
