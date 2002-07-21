ced41e58e0e3ae4bbd0bfcb0103dc4b6b9573060
GCC Administrator <gccadmin@gcc.gnu.org>
2002-07-21 07:16:33 +0000

    Daily bump.
    From-SVN: r55620

--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
                                                                                         #include "gansidecl.h"
 #include "version.h"
 
-const char *const version_string = "3.0.5 20020720 (prerelease)";
+const char *const version_string = "3.0.5 20020721 (prerelease)";
