9504d8787d55093489415d001cd600c8f21c1b4f
Mark Mitchell <mmitchel@gcc.gnu.org>
2002-07-25 23:40:41 +0000

    Update version
    From-SVN: r55765

--- gcc/ada/gnatvsn.ads
+++ gcc/ada/gnatvsn.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---                            $Revision: 1.2.12.61 $
+--                            $Revision: 1.2.12.62 $
 --                                                                          --
 --          Copyright (C) 1992-2002 Free Software Foundation, Inc.          --
 --                                                                          --
@@ -38,7 +38,7 @@
 
 package Gnatvsn is
 
-   Gnat_Version_String : constant String := "3.1.1 20020725 (prerelease)";
+   Gnat_Version_String : constant String := "3.1.1 20020725 (release)";
    --  Version output when GNAT (compiler), or its related tools, including
    --  GNATBIND, GNATCHOP, GNATFIND, GNATLINK, GNATMAKE, GNATXREF, are run
    --  (with appropriate verbose option switch set).
