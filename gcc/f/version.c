eb7a93b0f7bc5a459635b6fcb2f6e2e7165a1778
GCC Administrator <gccadmin@gcc.gnu.org>
2002-07-31 07:16:21 +0000

    Daily bump.
    From-SVN: r55896

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,4 +1,4 @@
 #include "ansidecl.h"
 #include "f/version.h"
 
-const char *const ffe_version_string = "3.2 20020730 (prerelease)";
+const char *const ffe_version_string = "3.2 20020731 (prerelease)";
