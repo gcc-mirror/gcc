efd092f3c57ca1acecabadf41c0804ec2a2cc4f3
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-08-02 14:55:08 +0000

    * config.gcc: Don't include mips/abi64.h in $tm_file. 	* hard-reg-set.h (call_really_used_regs): Declare. 	* config/mips/abi64.h: Remove file. 	* config/mips/linux.h, 	* config/mips/iris6.h: Don't include it. 	* config/mips/mips-protos.h (mips_conditional_register_usage): Declare. 	* config/mips/mips.h (CONDITIONAL_REGISTER_USAGE): Use it. 	(REG_PARM_STACK_SPACE, STACK_BOUNDARY, STRICT_ARGUMENT_NAMING, 	FUNCTION_ARG_PASS_BY_REFERENCE, FUNCTION_ARG_PADDING, 	FUNCTION_ARG_CALLEE_COPIES, MUST_PASS_IN_STACK, MIPS_STACK_ALIGN): 	Bring across definitions from abi64.h. 	(GP_ARG_LAST, FP_ARG_LAST): Use MAX_ARGS_IN_REGISTERS. 	(BIGGEST_MAX_ARGS_IN_REGISTERS): New. 	(struct mips_args): Use it. 	* config/mips/mips.c (mips_conditional_register_usage): Define.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@55986 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/mips/linux.h
+++ gcc/config/mips/linux.h
@@ -18,8 +18,6 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-#include "mips/abi64.h"
-
 #undef WCHAR_TYPE
 #define WCHAR_TYPE "int"
 
