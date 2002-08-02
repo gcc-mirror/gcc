509da48a87fc1c7d2c31b687138f8ebfdd6802e2
GCC Administrator <gccadmin@gcc.gnu.org>
2002-08-02 07:17:14 +0000

    Daily bump.
    From-SVN: r55978

--- libf2c/libF77/Version.c
+++ libf2c/libF77/Version.c
@@ -3,7 +3,7 @@ static char junk[] = "\n@(#)LIBF77 VERSION 20000929\n";
 /*
 */
 
-char __G77_LIBF77_VERSION__[] = "3.3 20020801 (experimental)";
+char __G77_LIBF77_VERSION__[] = "3.3 20020802 (experimental)";
 
 /*
 2.00	11 June 1980.  File version.c added to library.
