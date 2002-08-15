492fa7d852133f6b24f8619cc84420f7c66e92b9
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-08-15 21:17:16 +0000

    * gcc.dg/typeof-1.c: New test. * g++.dg/ext/typeof2.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@56360 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/ext/typeof2.C
@@ -0,0 +1,29 @@
+// Test typeof with __asm redirection
+// { dg-do compile }
+// { dg-options "-O2" }
+
+extern "C" {
+  extern int foo1;
+  extern int foo1 __asm ("bar1");
+  int foo1 = 1;
+
+  extern int foo2 (int);
+  extern int foo2 (int) __asm ("bar2");
+  int foo2 (int x)
+  {
+    return x;
+  }
+
+  extern int foo3;
+  extern __typeof (foo3) foo3 __asm ("bar3");
+  int foo3 = 1;
+
+  extern int foo4 (int);
+  extern __typeof (foo4) foo4 __asm ("bar4");
+  int foo4 (int x)
+  {
+    return x;
+  }
+}
+
+// { dg-final { scan-assembler-not "foo" } }
