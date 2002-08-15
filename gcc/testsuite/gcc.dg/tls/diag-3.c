55ae9079933065d00adfac121e8b5b6a18b61939
Richard Henderson <rth@redhat.com>
2002-08-14 17:16:11 -0700

    * gcc.dg/tls/diag-3.c: Fix expected message strings.
    From-SVN: r56336

--- gcc/testsuite/gcc.dg/tls/diag-3.c
+++ gcc/testsuite/gcc.dg/tls/diag-3.c
@@ -1,10 +1,10 @@
 /* Report invalid extern and __thread combinations.  */
 
 extern int j;		/* { dg-error "previous declaration" } */
-__thread int j;		/* { dg-error "thread-local declaration for" } */
+__thread int j;		/* { dg-error "follows non thread-local" } */
 
 extern __thread int i;	/* { dg-error "previous declaration" } */
-int i;			/* { dg-error "non thread-local" } */
+int i;			/* { dg-error "follows thread-local" } */
 
 extern __thread int k;	/* This is fine.  */
 __thread int k;
