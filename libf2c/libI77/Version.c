6274a3f5268cb8a3f763eb0594e79e34f0e73140
gccadmin <gccadmin@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-08-17 07:17:10 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@56408 138bc75d-0d04-0410-961f-82ee72b054a4

--- libf2c/libI77/Version.c
+++ libf2c/libI77/Version.c
@@ -3,7 +3,7 @@ static char junk[] = "\n@(#) LIBI77 VERSION pjw,dmg-mods 20001205\n";
 /*
 */
 
-char __G77_LIBI77_VERSION__[] = "3.3 20020816 (experimental)";
+char __G77_LIBI77_VERSION__[] = "3.3 20020817 (experimental)";
 
 /*
 2.01	$ format added
