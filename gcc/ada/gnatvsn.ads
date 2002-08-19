579c9e7c527dd65b6685b67ccca61c5d7d7172a9
gccadmin <gccadmin@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-08-19 07:17:42 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@56436 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/gnatvsn.ads
+++ gcc/ada/gnatvsn.ads
@@ -37,7 +37,7 @@
 
 package Gnatvsn is
 
-   Gnat_Version_String : constant String := "3.3 20020818 (experimental)";
+   Gnat_Version_String : constant String := "3.3 20020819 (experimental)";
    --  Version output when GNAT (compiler), or its related tools, including
    --  GNATBIND, GNATCHOP, GNATFIND, GNATLINK, GNATMAKE, GNATXREF, are run
    --  (with appropriate verbose option switch set).
