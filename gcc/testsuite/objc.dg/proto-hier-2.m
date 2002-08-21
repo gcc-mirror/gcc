92b5aacd3300c2154c62528c6a8914a749c7a663
Devang Patel <dpatel@apple.com>
2002-08-20 18:01:14 -0700

    tree.c (get_qualified_type): Add TYPE_CONTEXT check.
    2002-08-20  Devang Patel  <dpatel@apple.com>
            * tree.c (get_qualified_type): Add TYPE_CONTEXT check.
    
    testsuite:
    	* objc.dg/proto-hier-2.m: New test.
    
    From-SVN: r56475

---
+++ gcc/testsuite/objc.dg/proto-hier-2.m
@@ -0,0 +1,49 @@
+/* Test protocol warning.  */
+/* Contributed by Devang Patel <dpatel@apple.com>.  */
+/* { dg-do compile } */
+
+typedef struct objc_object { struct objc_class *class_pointer; } *id;
+
+@protocol Bar
+@end
+
+id <Bar> Foo_Bar () { }
+
+typedef struct
+{
+        int i;
+} MyStruct;
+
+@interface Foo
+{
+  id _mainData;
+  MyStruct *_anotherData;
+}
+
+-(id) mainDataSource;
+-(id) anotherDataSource;
+-(id) my_method: (int) i;
+@end
+
+@implementation Foo
+-(id) anotherDataSource
+{
+        return (id)_anotherData;
+}
+
+-(id) mainDataSource
+{
+        return _mainData;
+}
+
+-(id) my_method: (int) i
+{
+  id one = [self anotherDataSource];
+
+  i = i - 1;
+  // Do not issue warning about my_method not implemented by protocol
+  return [(one ? [self mainDataSource] : one) my_method:i];
+}
+
+@end
+
