d241f75b9693bad3e4990196c2e3e785e73e7e70
Andre Leis <a.leis@gmx.net>
2002-08-25 00:59:31 +0000

    David Billinghurst (David.Billinghurst@riotinto.com>
    2002-08-25  Andre Leis <a.leis@gmx.net>
                David Billinghurst (David.Billinghurst@riotinto.com>
    
            * sysdep.c (__gnat_ttyname): include <termios.h> on cygwin
    
    Co-Authored-By: David Billinghurst <David.Billinghurst@riotinto.com>
    
    From-SVN: r56558

--- gcc/ada/sysdep.c
+++ gcc/ada/sysdep.c
@@ -294,7 +294,8 @@ __gnat_ttyname (filedes)
 #if defined (linux) || defined (sun) || defined (sgi) || defined (__EMX__) \
   || (defined (__osf__) && ! defined (__alpha_vxworks)) || defined (WINNT) \
   || defined (__MACHTEN__) || defined (hpux) || defined (_AIX) \
-  || (defined (__svr4__) && defined (i386)) || defined (__Lynx__)
+  || (defined (__svr4__) && defined (i386)) || defined (__Lynx__) \
+  || defined (__CYGWIN__)
 #include <termios.h>
 
 #else
@@ -349,7 +350,7 @@ getc_immediate_common (stream, ch, end_of_file, avail, waiting)
 {
 #if defined (linux) || defined (sun) || defined (sgi) || defined (__EMX__) \
     || (defined (__osf__) && ! defined (__alpha_vxworks)) \
-    || defined (__CYGWIN32__) || defined (__MACHTEN__) || defined (hpux) \
+    || defined (__CYGWIN__) || defined (__MACHTEN__) || defined (hpux) \
     || defined (_AIX) || (defined (__svr4__) && defined (i386)) \
     || defined (__Lynx__)
   char c;
