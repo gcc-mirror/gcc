5f4442bc925142e053cc9f3fe5a6d1d27d3b523c
bwilson <bwilson@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-09-04 16:22:51 +0000

    * config/xtensa/elf.h (TARGET_SECTION_TYPE_FLAGS): Define to         xtensa_multibss_section_type_flags.         * config/xtensa/xtensa.c (xtensa_multibss_section_type_flags): Define.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@56797 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/xtensa/elf.h
+++ gcc/config/xtensa/elf.h
@@ -24,6 +24,8 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
     builtin_define ("__ELF__");					\
   } while (0)
 
+#define TARGET_SECTION_TYPE_FLAGS xtensa_multibss_section_type_flags
+
 /* Don't assume anything about the header files. */
 #define NO_IMPLICIT_EXTERN_C
 
