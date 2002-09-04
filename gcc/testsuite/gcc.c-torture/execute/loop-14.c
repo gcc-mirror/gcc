ed6cc1f5cb3b6cc316f2f67583b62b92a5de4e4b
J"orn Rennecke <joern.rennecke@superh.com>
2002-09-04 23:38:25 +0000

    loop.c (scan_loop): Don't mark separate insns out of a libcall for moving.
    gcc:
    	* loop.c (scan_loop): Don't mark separate insns out of a libcall
    	for moving.
    	(move_movables): Abort if we see the first insn of a libcall.
    gcc/testsuite
    	* gcc.c-torture/execute/loop-14.c: New test.
    
    From-SVN: r56823

---
+++ gcc/testsuite/gcc.c-torture/execute/loop-14.c
@@ -0,0 +1,20 @@
+int a3[3];
+
+void f(int *a)
+{
+  int i;
+
+  for (i=3; --i;)
+    a[i] = 42 / i;
+}
+
+int
+main ()
+{
+  f(a3);
+
+  if (a3[1] != 42 || a3[2] != 21)
+    abort ();
+
+  exit (0);
+}
