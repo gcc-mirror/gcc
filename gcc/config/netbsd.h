5ccae5bfd969aaddcc0e4e4c4e640cfc1c8f0417
thorpej <thorpej@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-09-15 18:14:18 +0000

    * config/netbsd.h (LIB_SPEC): Include the appropriate pthread library if -pthread is specified.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@57164 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/netbsd.h
+++ gcc/config/netbsd.h
@@ -77,14 +77,23 @@ Boston, MA 02111-1307, USA.  */
 #endif /* NETBSD_NATIVE */
 
 
-/* Provide a LIB_SPEC appropriate for NetBSD.  Just select the appropriate
-   libc, depending on whether we're doing profiling; if `-posix' is specified,
-   link against the appropriate libposix first.  Don't include libc when
-   linking a shared library.  */
+/* Provide a LIB_SPEC appropriate for NetBSD.  Here we:
+
+   1. Select the appropriate set of libs, depending on whether we're
+      profiling.
+
+   2. Include the pthread library if -pthread is specified.
+
+   3. Include the posix library if -posix is specified.  */
 
 #undef LIB_SPEC
 #define LIB_SPEC		\
-  "%{posix:			\
+  "%{pthread:			\
+     %{!p:			\
+       %{!pg:-lpthread}}	\
+     %{p:-lpthread_p}		\
+     %{pg:-lpthread_p}}		\
+   %{posix:			\
      %{!p:			\
        %{!pg:-lposix}}		\
      %{p:-lposix_p}		\
