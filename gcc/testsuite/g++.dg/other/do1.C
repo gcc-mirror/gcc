045f0656692c557b58aa639fef96adc70ae1180d
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-09-16 20:13:07 +0000

    .: 	PR c++/7640 	* c-semantics.c (genrtl_do_stmt): Cope with NULL cond. testsuite: 	* g++.dg/other/do1.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@57212 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/other/do1.C
@@ -0,0 +1,12 @@
+// { dg-do compile }
+
+// Copyright (C) 2002 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 16 Sep 2002 <nathan@codesourcery.com>
+
+// PR 7640. ICE.
+
+void init ()
+{
+  do {  } while (0)
+  obj = 0;
+}
