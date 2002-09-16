e34fcbe4ff4eaaf0f5188e80b1a69ecd62d8b6ef
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-09-16 15:15:36 +0000

    cp: 	* parse.y (parse_finish_call_expr): Check lookup_member result. testsuite: 	* g++.dg/lookup/scoped2.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@57195 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/lookup/scoped2.C
@@ -0,0 +1,19 @@
+// { dg-do compile }
+
+// Copyright (C) 2002 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 16 Sep 2002 <nathan@codesourcery.com>
+
+// Seg faulted.
+
+struct Base 
+{
+};
+
+struct Derived : Base
+{
+  void Foo ()
+  {
+    Base::Baz ();  // { dg-error "has no member" "" }
+      
+  };
+};
