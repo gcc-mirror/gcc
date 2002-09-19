068fc551ab26da1d9b68a2190e9976b411532caf
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-09-19 01:07:10 +0000

    * ifcvt.c (noce_process_if_block): Correctly detect X modified         with INSN_B before COND_EARLIEST.  Don't check A and B for         modification in condition range.  Reorder INSN_B for A==B properly.         (if_convert): Iterate until no matches for a block.
    	* gcc.c-torture/execute/20020916-1.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@57294 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20020916-1.c
@@ -0,0 +1,19 @@
+/* Distilled from try_pre_increment in flow.c.  If-conversion inserted
+   new instructions at the wrong place on ppc.  */
+
+int foo(int a)
+{
+  int x;
+  x = 0;
+  if (a > 0) x = 1;
+  if (a < 0) x = 1;
+  return x;
+}
+
+int main()
+{
+  if (foo(1) != 1)
+    abort();
+  return 0;
+}
+
