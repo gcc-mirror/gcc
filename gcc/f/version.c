aa5efba3c04ce4c167dfbf84f5f68fb77e823b1d
GCC Administrator <gccadmin@gcc.gnu.org>
2002-09-20 07:17:02 +0000

    Daily bump.
    [[Split portion of a mixed commit.]]
    
    From-SVN: r57341.1

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-                                                                                        const char *ffe_version_string = "0.5.25 20020919 (prerelease)";
+                                                                                        const char *ffe_version_string = "0.5.25 20020920 (prerelease)";
--- libf2c/libU77/Version.c
+++ libf2c/libU77/Version.c
@@ -1,6 +1,6 @@
                                                                                         static char junk[] = "\n@(#) LIBU77 VERSION 19980709\n";
 
-char __G77_LIBU77_VERSION__[] = "0.5.25 20020919 (prerelease)";
+char __G77_LIBU77_VERSION__[] = "0.5.25 20020920 (prerelease)";
 
 #include <stdio.h>
 
