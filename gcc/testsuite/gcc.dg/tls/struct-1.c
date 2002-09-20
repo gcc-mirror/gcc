2db7f8d113db10e3c5fe9ab7b98a9a929e376f8e
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-09-20 20:03:40 +0000

    * gcc.dg/tls/struct-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@57367 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tls/struct-1.c
@@ -0,0 +1,32 @@
+/* This testcase ICEd on IA-32 because the backend was inconsistent whether
+   to allow addends for @dtpoff relocs or not.  */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fpic" } */
+
+struct S {
+  int s0, s1, s2, s3;
+};
+static __thread struct S x;
+extern void abort (void);
+extern void exit (int);
+
+void
+foo (struct S *s)
+{
+  s->s2 = 231;
+}
+
+void
+bar (void)
+{
+  if (x.s0 == 231 || x.s2 != 231)
+    abort ();
+}
+
+int
+main ()
+{
+  foo (&x);
+  bar ();
+  exit (0);
+}
