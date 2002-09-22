319e9548e93fc8de3bbbfc07cea1dba3a7525e46
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-09-22 18:59:08 +0000

    * gcc.dg/20020219-1.c: Add "-mdisable-indexing" option for target 	hppa*-*-hpux*.  Update test comment.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@57415 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/20020219-1.c
+++ gcc/testsuite/gcc.dg/20020219-1.c
@@ -1,8 +1,13 @@
 /* PR c/4389
    This testcase failed because host_integerp (x, 0) was returning
-   1 even for constants bigger than 2^31.  */
+   1 even for constants bigger than 2^31.  It fails under under hpux
+   without -mdisable-indexing because the pointer x - 1 is used as
+   the base address of an indexed load.  Because the struct A is not
+   actually allocated, x - 1 lies in the text segment and this causes
+   the wrong space register to be selected for the load.  */
 /* { dg-do run } */
 /* { dg-options "-O2" } */
+/* { dg-options "-O2 -mdisable-indexing" { target hppa*-*-hpux* } } */
 
 extern void abort (void);
 extern void exit (int);
