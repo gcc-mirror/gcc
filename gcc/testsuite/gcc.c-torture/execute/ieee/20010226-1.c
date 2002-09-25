6190e14740643bdcd67fc4ff0a69c2207d0c0bf7
Richard Henderson <rth@redhat.com>
2002-09-25 10:14:48 -0700

    20010226-1.c: Early exit for too-small long double.
            * gcc.c-torture/execute/ieee/20010226-1.c: Early exit for
            too-small long double.
    
    From-SVN: r57502

--- gcc/testsuite/gcc.c-torture/execute/ieee/20010226-1.c
+++ gcc/testsuite/gcc.c-torture/execute/ieee/20010226-1.c
@@ -1,17 +1,24 @@
+#include <float.h>
+
 long double dfrom = 1.1;
 long double m1;
 long double m2;
 unsigned long mant_long;
 
-int main( )
+int main()
 {
-  m1 = dfrom / 2.0;
+  /* Some targets don't support a conforming long double type.  This is
+     common with very small parts which set long double == float.   Look
+     to see if the type has at least 32 bits of precision.  */
+  if (LDBL_EPSILON > 0x1p-31L)
+    return 0;
 
+  m1 = dfrom / 2.0;
   m2 = m1 * 4294967296.0;
   mant_long = ((unsigned long) m2) & 0xffffffff;
 
-  if ( mant_long == 0x8ccccccc)
-    exit (0);
+  if (mant_long == 0x8ccccccc)
+    return 0;
   else
     abort();
 }
