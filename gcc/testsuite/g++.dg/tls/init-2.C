871e97f72002d01c3a98f7e198f5dbbcd9157b09
Mark Mitchell <mark@codesourcery.com>
2002-10-01 18:27:33 +0000

    cp
    From-SVN: r57695

--- gcc/testsuite/g++.dg/tls/init-2.C
+++ gcc/testsuite/g++.dg/tls/init-2.C
@@ -1,13 +1,13 @@
 /* Invalid initializations.  */
 
 extern __thread int i;
-__thread int *p = &i;	/* { dg-error "run-time initialization" } */
+__thread int *p = &i;	/* { dg-error "dynamically initialized" } */
 
 extern int f();
-__thread int j = f();	/* { dg-error "run-time initialization" } */
+__thread int j = f();	/* { dg-error "dynamically initialized" } */
 
 struct S
 {
   S();
 };
-__thread S s;		/* { dg-error "run-time initialization" } */
+__thread S s;		/* { dg-error "non-POD" } */
