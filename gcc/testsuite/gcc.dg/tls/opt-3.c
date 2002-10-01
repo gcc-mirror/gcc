e80b44630607ead2a1a21df43fc1e0492888aeb1
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-10-01 20:49:39 +0000

    * calls.c (precompute_register_parameters): Force non-legitimate         constants into pseudos. 	* gcc.dg/tls/opt-3.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@57709 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tls/opt-3.c
@@ -0,0 +1,10 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fpic } */
+/* { dg-options "-O2 -fpic -mregparm=3" { target i?86-*-* } } */
+
+extern __thread int i, j, k;
+extern void bar(int *, int *, int *);
+void foo(void)
+{
+  bar(&i, &j, &k);
+}
