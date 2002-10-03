c652b99a584d1ee2da7305211690e3d4c14adc52
GCC Administrator <gccadmin@gcc.gnu.org>
2002-10-03 07:16:52 +0000

    Daily bump.
    [[Split portion of a mixed commit.]]
    
    From-SVN: r57769.1

--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
                                                                                         #include "gansidecl.h"
 #include "version.h"
 
-const char *const version_string = "3.0.5 20021002 (prerelease)";
+const char *const version_string = "3.0.5 20021003 (prerelease)";
