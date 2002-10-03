f50df1d58f9ae1f46cf23bc5446eb91175f67851
John David Anglin <dave@hiauly1.hia.nrc.ca>
2002-10-03 04:05:54 +0000

    pa-linux.h (FUNCTION_OK_FOR_SIBCALL): Delete macro.
    	* pa-linux.h (FUNCTION_OK_FOR_SIBCALL): Delete macro.
    	* pa32-linux.h (FUNCTION_OK_FOR_SIBCALL): Define.
    
    From-SVN: r57758

--- gcc/config/pa/pa32-linux.h
+++ gcc/config/pa/pa32-linux.h
@@ -21,3 +21,11 @@ Boston, MA 02111-1307, USA.  */
 /* Turn off various SOM crap we don't want.  */
 #undef TARGET_ELF32
 #define TARGET_ELF32 1
+
+/* Sibcalls are ok when ld is used in single subspace mode.  The
+   multiple subspace mode is not compatible with sibcalls to external
+   functions because the linker generated stubs store the return
+   pointer into the frame.  This target does not need multiple
+   subspace stubs, so we allow sibcalls to all functions.  */
+#undef FUNCTION_OK_FOR_SIBCALL
+#define FUNCTION_OK_FOR_SIBCALL(DECL) 1
