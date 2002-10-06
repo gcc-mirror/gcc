f75369c87a1f5f514b6f2642749c285e1a3d0513
Andreas Jaeger <aj@suse.de>
2002-10-06 21:30:37 +0200

    re PR target/7559 (kdelibs miscompilation)
    	PR target/7559
    	* gcc.dg/20021006-1.c: New test.
    
    From-SVN: r57867

---
+++ gcc/testsuite/gcc.dg/20021006-1.c
@@ -0,0 +1,27 @@
+/* PR target/7559
+   This testcase was miscompiled on x86-64 due to wrong access to the struct
+   members.  */
+
+extern void abort();
+
+struct A {
+  long x;
+};
+
+struct R {
+  struct A a, b;
+};
+
+struct R R = {
+  {100}, {200}
+};
+
+void f(struct R r) {
+  if (r.a.x != R.a.x || r.b.x != R.b.x)
+    abort();
+}
+
+int main() {
+  f(R);
+  return 0;
+}
