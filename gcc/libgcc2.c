f9c9dcd7f514c6c7cac606ce93d185aa0194c191
amodra <amodra@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-10-08 23:50:56 +0000

    * libgcc2.c (__floatdisf): Properly cure double rounding.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@57965 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/libgcc2.c
+++ gcc/libgcc2.c
@@ -1091,7 +1091,10 @@ __floatdisf (DWtype u)
 	     && u < ((DWtype) 1 << DF_SIZE)))
 	{
 	  if ((UDWtype) u & (REP_BIT - 1))
-	    u |= REP_BIT;
+	    {
+	      u &= ~ (REP_BIT - 1);
+	      u |= REP_BIT;
+	    }
 	}
     }
   f = (Wtype) (u >> WORD_SIZE);
