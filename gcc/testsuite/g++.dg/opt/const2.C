25763d80f298a3ff04571a01db5d6f215a936145
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-10-14 21:19:05 +0000

    PR optimization/6631 	* Makefile.in (function.o): Depend on langhooks.h. 	* alias.c (objects_must_conflict_p): Check honor_readonly when 	examining TYPE_READONLY. 	* function.c (assign_stack_temp_for_type): Likewise.
    	PR optimization/6631
    	* g++.dg/opt/const2.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@58136 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/opt/const2.C
@@ -0,0 +1,40 @@
+// { dg-do run }
+// { dg-options "-O" }
+
+extern "C" void abort (void);
+
+struct QSize
+{
+  QSize();
+  QSize( int w, int h );
+  int wd, ht;
+  friend inline const QSize operator+( const QSize &, const QSize & );
+};
+
+inline QSize::QSize()
+{ wd = ht = -1; }
+
+inline QSize::QSize( int w, int h )
+{ wd = w; ht = h; }
+
+inline const QSize operator+( const QSize & s1, const QSize & s2 )
+{ return QSize(s1.wd+s2.wd, s1.ht+s2.ht); }
+
+QSize minimumSize()
+{
+  return QSize (100, 200);
+}
+
+QSize totalMinimumSize()
+{
+    QSize s = minimumSize();
+    return s + QSize( 0, 0 );
+}
+
+int main()
+{
+  QSize s = totalMinimumSize();
+  if (s.wd != 100 || s.ht != 200)
+    abort ();
+}
+
