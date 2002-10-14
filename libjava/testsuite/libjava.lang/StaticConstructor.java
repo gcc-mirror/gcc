a9d3a3a3ca11d7ca4fec80abd982aca6358a358a
aph <aph@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-10-14 19:02:56 +0000

    2002-10-14  Andrew Haley  <aph@redhat.com>
            * libjava.lang/StaticConstructor.java: New.
            * libjava.lang/StaticConstructor.out: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@58131 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/testsuite/libjava.lang/StaticConstructor.java
@@ -0,0 +1,29 @@
+g// Test to make sure static initializers are called
+
+class bar
+{
+  public static int zog;
+  public static int zag;
+
+  static
+  {
+    zog = 12;
+    zag = 2;
+  }
+
+  public bar() { } 
+}
+
+public class StaticConstructor
+{
+  static int foo ()
+  {
+    return new bar().zog;
+  }
+
+  public static void main(String args[])
+  {
+    System.out.println ("" + (foo() + bar.zag));
+  }
+}
+
