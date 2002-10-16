6e2ac1aeef6fcb06ae053814bbc6057afe679959
Ulrich Weigand <uweigand@de.ibm.com>
2002-10-16 21:25:26 +0000

    * gcc.dg/weak/typeof-2.c: For S/390 targets, return before baz3 test.
    From-SVN: r58222

--- gcc/testsuite/gcc.dg/weak/typeof-2.c
+++ gcc/testsuite/gcc.dg/weak/typeof-2.c
@@ -31,4 +31,6 @@ int bar3 (int x)
 // { dg-final { global target_triplet } }
 // { dg-final { if [string match sh-*-* $target_triplet ] {return} } }
 // { dg-final { if [string match {sh[elb1-9]*-*-*} $target_triplet ] {return} } }
+// Likewise for S/390 targets
+// { dg-final { if [string match s390-*-* $target_triplet ] {return} } }
 // { dg-final { scan-assembler "baz3.*baz3.*baz3.*baz3.*baz3.*baz3" } }
