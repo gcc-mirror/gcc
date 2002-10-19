e3a66141b13f7c95a851e4fa9d1e530b1d6e85c4
GCC Administrator <gccadmin@gcc.gnu.org>
2002-10-19 07:17:05 +0000

    Daily bump.
    [[Split portion of a mixed commit.]]
    
    From-SVN: r58304.1

--- libf2c/libF77/Version.c
+++ libf2c/libF77/Version.c
@@ -3,7 +3,7 @@
 /*
 */
 
-char __G77_LIBF77_VERSION__[] = "0.5.25 20021018 (prerelease)";
+char __G77_LIBF77_VERSION__[] = "0.5.25 20021019 (prerelease)";
 
 /*
 2.00	11 June 1980.  File version.c added to library.
