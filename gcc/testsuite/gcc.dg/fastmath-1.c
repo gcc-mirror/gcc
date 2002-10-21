de40e1df94369fb1bc02f5f89571157e39b5e0da
Dale Johannesen <dalej@gcc.gnu.org>
2002-10-21 21:56:40 +0000

    Fix fastmath-specific crash PR 7133.
    From-SVN: r58389

---
+++ gcc/testsuite/gcc.dg/fastmath-1.c
@@ -0,0 +1,23 @@
+/* { dg-do run } */
+/* { dg-options "-O2 -ffast-math" } */
+
+int foo ( float* dists,  int k) 
+{ 
+if ( ( dists [ 0 ] > 0 ) == ( dists [ 1 ] > 0 ) ) 
+  return k;
+return 0;
+} 
+main() {
+  float dists[16] = { 0., 1., 1., 0., 0., -1., -1., 0.,
+		     1., 1., 1., -1., -1., 1., -1., -1. };
+  if ( foo(&dists[0], 1) +
+       foo(&dists[2], 2) +
+       foo(&dists[4], 4) +
+       foo(&dists[6], 8) +
+       foo(&dists[8], 16) +
+       foo(&dists[10], 32) +
+       foo(&dists[12], 64) +
+       foo(&dists[14], 128)
+	!= 156)
+    abort();
+}
