73062b4413130b9f375a46ecb97cd81a68d1cf69
amodra <amodra@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-10-22 07:59:41 +0000

    * output.h (SECTION_NOTYPE): Define. 	* varasm.c (default_section_type_flags_1): Set SECTION_NOTYPE for 	init array sections. 	(default_elf_asm_named_section): Mind SECTION_NOTYPE. 	* config/arm/arm.c (arm_elf_asm_named_section): Likewise.  Also 	merge TLS support.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@58404 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/output.h
+++ gcc/output.h
@@ -485,7 +485,8 @@ extern void no_asm_to_stream PARAMS ((FILE *));
 					   embedded zeros */
 #define SECTION_OVERRIDE 0x20000	/* allow override of default flags */
 #define SECTION_TLS	 0x40000	/* contains thread-local storage */
-#define SECTION_MACH_DEP 0x80000	/* subsequent bits reserved for target */
+#define SECTION_NOTYPE	 0x80000	/* don't output @progbits */
+#define SECTION_MACH_DEP 0x100000	/* subsequent bits reserved for target */
 
 extern unsigned int get_named_section_flags PARAMS ((const char *));
 extern bool set_named_section_flags	PARAMS ((const char *, unsigned int));
