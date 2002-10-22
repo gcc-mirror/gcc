fb7dc860cf134fdafcb6ad4fb3a106c8d48b0735
geoffk <geoffk@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-10-22 20:02:49 +0000

    * btest-gcc.sh: Add gdb.sum to TESTLOGS only when GDB testsuite is run.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@58415 138bc75d-0d04-0410-961f-82ee72b054a4

--- contrib/regression/btest-gcc.sh
+++ contrib/regression/btest-gcc.sh
@@ -99,8 +99,7 @@ H_REAL_TARGET=`$SOURCE/config.sub $H_TARGET || exit 1`
 TESTLOGS="gcc/testsuite/gcc.sum
 gcc/testsuite/g++.sum
 gcc/testsuite/g77.sum
-gcc/testsuite/objc.sum
-test-gdb/gdb.sum"
+gcc/testsuite/objc.sum"
 # $H_TARGET/libstdc++-v3/testsuite/libstdc++-v3.sum
 
 # Build.
@@ -126,25 +125,24 @@ make -k check-gcc
 make check-target-libstdc++-v3
 
 # Test the just-built GCC with the GDB testsuite.
-mkdir test-gdb || exit 1
 if [ -d $GDB_TESTSUITE ] ; then
-cd $GDB_TESTSUITE || exit 1
-for i in gdb.* ; do
-  if [ -d $i ] ; then
-    mkdir $BUILD/test-gdb/$i
-  fi
-done
-cd $BUILD/test-gdb || exit 1
-echo "set host_alias $H_HOST" > site.exp
-echo "set host_triplet $H_HOST" >> site.exp
-echo "set target_alias $H_TARGET" >> site.exp
-echo "set target_triplet $H_REAL_TARGET" >> site.exp
-echo "set build_alias $H_BUILD" >> site.exp
-echo "set build_triplet $H_BUILD" >> site.exp
-echo "set srcdir $GDB_TESTSUITE" >> site.exp
-runtest --tool gdb
-else
-  echo 'gdb tests not run' > $BUILD/test-gdb/gdb.sum
+  mkdir test-gdb || exit 1
+  cd $GDB_TESTSUITE || exit 1
+  for i in gdb.* ; do
+    if [ -d $i ] ; then
+      mkdir $BUILD/test-gdb/$i
+    fi
+  done
+  cd $BUILD/test-gdb || exit 1
+  echo "set host_alias $H_HOST" > site.exp
+  echo "set host_triplet $H_HOST" >> site.exp
+  echo "set target_alias $H_TARGET" >> site.exp
+  echo "set target_triplet $H_REAL_TARGET" >> site.exp
+  echo "set build_alias $H_BUILD" >> site.exp
+  echo "set build_triplet $H_BUILD" >> site.exp
+  echo "set srcdir $GDB_TESTSUITE" >> site.exp
+  runtest --tool gdb
+  TESTLOGS="$TESTLOGS test-gdb/gdb.sum"
 fi
 
 # Sanity-check the testlogs.  They should contain at least one PASS.
