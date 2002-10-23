c7dbbc3e4563d18d6e3129a061469d6684a70334
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-10-23 22:56:13 +0000

    For PR java/6388: 	* libjava.lang/pr6388.java: New file. 	* libjava.lang/pr6388.out: New file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@58475 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/testsuite/libjava.lang/pr6388.java
@@ -0,0 +1,10 @@
+public class pr6388
+{
+  public static void main (String[] args)
+  {
+    System.out.println (Integer.MIN_VALUE);
+    System.out.println (0x80000000);
+    System.out.println (Integer.MIN_VALUE == 0x80000000);
+    System.out.println (0x80000000 == 0x80000000);
+  }
+}
