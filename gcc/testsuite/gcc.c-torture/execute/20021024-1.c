0c6da4e47f8a3c12675f3ba694b0cb6e25275d03
Jakub Jelinek <jakub@gcc.gnu.org>
2002-10-24 19:11:56 +0200

    re PR target/6981 (wrong code in 64-bit manipulation on x86)
    	* config/i386/i386.md (subdi3_1): Add call to ix86_binary_operator_ok.
    	PR target/6981
    	* config/i386/i386.md (adddi3_1): Add call to ix86_binary_operator_ok.
    	* gcc.c-torture/execute/20021024-1.c: New test.
    
    From-SVN: r58497

---
+++ gcc/testsuite/gcc.c-torture/execute/20021024-1.c
@@ -0,0 +1,43 @@
+/* Origin: PR target/6981 from Mattias Engdegaard <mattias@virtutech.se>.  */
+
+void exit (int);
+void abort (void);
+
+unsigned long long *cp, m;
+
+void foo (void)
+{
+}
+
+void bar (unsigned rop, unsigned long long *r)
+{
+  unsigned rs1, rs2, rd;
+
+top:
+  rs2 = (rop >> 23) & 0x1ff;
+  rs1 = (rop >> 9) & 0x1ff;
+  rd = rop & 0x1ff;
+
+  *cp = 1;
+  m = r[rs1] + r[rs2];
+  *cp = 2;
+  foo();
+  if (!rd)
+    goto top;
+  r[rd] = 1;
+}
+
+int main(void)
+{
+  static unsigned long long r[64];
+  unsigned long long cr;
+  cp = &cr;
+
+  r[4] = 47;
+  r[8] = 11;
+  bar((8 << 23) | (4 << 9) | 15, r);
+
+  if (m != 47 + 11)
+    abort ();
+  exit (0);
+}
