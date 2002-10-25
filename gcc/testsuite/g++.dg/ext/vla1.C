f3763a442e854320fd233fd63b7871f301f723be
Zack Weinberg <zack@codesourcery.com>
2002-10-25 17:26:52 +0000

    re PR c++/6994 (ICE in find_function_data)
    	PR middle-end/6994
    
    	* c-objc-common.c (inline_forbidden_p): Can not inline
    	functions containing structures or unions containing VLAs.
    	* tree-inline.c (walk_tree): For all class 't' nodes, walk
    	TYPE_SIZE and TYPE_SIZE_UNIT.
    	(copy_tree_r): Copy types if they are variably modified.
    
    	* g++.dg/ext/vla1.C, gcc.dg/vla-2.c: New tests.
    
    From-SVN: r58535

---
+++ gcc/testsuite/g++.dg/ext/vla1.C
@@ -0,0 +1,26 @@
+// { dg-do compile }
+
+// Crash tests from PR middle-end/6994.  See also gcc.dg/vla-2.c.
+// A::A is acceptable extended C++ (VLA types brought over from C99);
+// B::B is not, but is closely related to acceptable extended C, though
+// not to acceptable C99.
+
+class A { A (int); };
+
+A::A (int i)
+{
+  int ar[1][i];    // { dg-error "variable-size array" }
+
+  ar[0][0] = 0;
+}
+
+class B { B (int); };
+
+B::B (int i)
+{
+  struct S {
+    int ar[1][i];  // { dg-error "variable-size|variably modified" }
+  } s;
+
+  s.ar[0][0] = 0;  // { dg-error "no member" }
+}
