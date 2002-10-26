f0dd122b47336c311afb102bf23a369d096a82be
Mike Stump <mrs@apple.com>
2002-10-26 01:15:16 +0000

    Add gcc.dg/warn-1.c
    From-SVN: r58549

---
+++ gcc/testsuite/gcc.dg/warn-1.c
@@ -0,0 +1,16 @@
+/* The compiler used to say passing arg 0 of, which is wrong.  */
+/* Radar 3069179 */
+
+/* { dg-options "-O3" } */
+
+static void foo (p)
+     int p;
+{	/* { dg-warning "passing arg of" } */
+}
+
+static void bar (void)
+{
+  void *vp;
+
+  foo (vp);	/* { dg-warning "" } */
+}
