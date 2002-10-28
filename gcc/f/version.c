11a122247578bafc19173fa675a774696d2bfb17
GCC Administrator <gccadmin@gcc.gnu.org>
2002-10-28 08:16:27 +0000

    Daily bump.
    From-SVN: r58590

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,4 +1,4 @@
 #include "ansidecl.h"
 #include "f/version.h"
 
-const char *const ffe_version_string = "3.2.1 20021027 (prerelease)";
+const char *const ffe_version_string = "3.2.1 20021028 (prerelease)";
--- libf2c/libU77/Version.c
+++ libf2c/libU77/Version.c
@@ -1,6 +1,6 @@
 static char junk[] = "\n@(#) LIBU77 VERSION 19980709\n";
 
-char __G77_LIBU77_VERSION__[] = "3.2.1 20021027 (prerelease)";
+char __G77_LIBU77_VERSION__[] = "3.2.1 20021028 (prerelease)";
 
 #include <stdio.h>
 
