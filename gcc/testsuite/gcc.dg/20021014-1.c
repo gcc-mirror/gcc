73e51da4d423b25b06e05f82e26cb7a4cdff75ad
Kaveh Ghazi <ghazi@gcc.gnu.org>
2002-10-28 18:17:22 +0000

    20021014-1.c: Bypass errors on solaris2 and irix6.
           * gcc.dg/20021014-1.c: Bypass errors on solaris2 and irix6.
    
           * gcc.dg/20021014-1.c: Use dg-error, not dg-build.
           * gcc.dg/20021014-1.c: Expect build failure on mmix-*-*.
    
    From-SVN: r58601

--- gcc/testsuite/gcc.dg/20021014-1.c
+++ gcc/testsuite/gcc.dg/20021014-1.c
@@ -1,5 +1,14 @@
 /* { dg-do run } */
 /* { dg-options "-O2 -p" } */
+/* { dg-error "profiler" "No profiler support" { target mmix-*-* } 0 } */
+/* Support for -p on solaris2 relies on mcrt1.o which comes with the
+   vendor compiler.  We cannot reiably predict the directory where the
+   vendor compiler (and thus mcrt1.o) is installed so we can't
+   necessarily find mcrt1.o even if we have it.  */
+/* { dg-error "mcrt1.o" "Optional vendor profiler support missing" { target *-*-solaris2* } 0 } */
+/* Support for -p on irix relies on libprof1.a which doesn't appear to
+   exist on any irix6 system currently posting testsuite results.  */
+/* { dg-error "libprof1.a" "Profiler support missing" { target mips*-*-irix* } 0 } */
 
 extern void abort (void);
 extern void exit (int);
