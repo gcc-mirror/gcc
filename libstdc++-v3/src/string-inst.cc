a3c80cd26332f3e835e6f5cbb29b38070fd22e03
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-11-02 02:18:18 +0000

    2002-11-01  Benjamin Kosnik  <bkoz@redhat.com>
    	PR libstdc++/8172
    	* src/string-inst.cc: Add instantiation.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@58738 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/src/string-inst.cc
+++ libstdc++-v3/src/string-inst.cc
@@ -22,7 +22,8 @@
 // As a special exception, you may use this file as part of a free software
 // library without restriction.  Specifically, if other files instantiate
 // templates or use macros or inline functions from this file, or you compile
-// this file and link it with other files to produce an executable, this// file does not by itself cause the resulting executable to be covered by
+// this file and link it with other files to produce an executable, this
+// file does not by itself cause the resulting executable to be covered by
 // the GNU General Public License.  This exception does not however
 // invalidate any other reasons why the executable file might be covered by
 // the GNU General Public License.
@@ -48,6 +49,7 @@ namespace std
   template class basic_string<C>;
   template S operator+(const C*, const S&);
   template S operator+(C, const S&);
+  template S operator+(const S&, const S&);
 } // namespace std
 
 namespace __gnu_cxx
