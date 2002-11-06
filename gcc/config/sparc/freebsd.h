08329eb1f9c5ebb79d45df9e59d1549ee0a63ea4
obrien <obrien@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-11-06 21:29:42 +0000

    2002-11-06  David O'Brien  <obrien@FreeBSD.org> 	* config/sparc/freebsd: Fix typo.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@58865 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sparc/freebsd.h
+++ gcc/config/sparc/freebsd.h
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-/* FreeBSD needs's the platform name (sparc64) defined.
+/* FreeBSD needs the platform name (sparc64) defined.
    Emacs needs to know if the arch is 64 or 32-bits.  */
 
 #undef  CPP_CPU64_DEFAULT_SPEC
