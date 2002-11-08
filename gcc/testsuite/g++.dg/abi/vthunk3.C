a1dbcdb9b41ccf0c3b1f9940343de76653c63088
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-11-08 02:16:48 +0000

    * class.c (add_vcall_offset_vtbl_entries_1): Correct ordering of 	vcall offfsets.  Split out ... 	(add_vcall_offset): ... new function.
    	* g++.dg/abi/vthunk3.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@58912 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/abi/vthunk3.C
@@ -0,0 +1,26 @@
+// { dg-do compile }
+// { dg-options "-fabi-version=0" }
+
+struct A {
+  virtual void a ();
+};
+
+struct B : virtual public A {
+  virtual void b ();
+  virtual void a ();
+};
+
+struct C {
+  virtual void c ();
+};
+
+struct D : public C, public B {
+};
+
+struct E : virtual public D {
+  void b ();
+};
+
+void E::b () {}
+
+// { dg-final { scan-assembler _ZTvn4_n20_N1E1bEv } }
