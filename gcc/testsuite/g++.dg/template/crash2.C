4f2c9d7ec6409e570be8df243c57e4e3298b26e6
Mark Mitchell <mark@codesourcery.com>
2002-11-08 01:42:23 +0000

    re PR c++/8338 (Infinite loop on illegal code)
    	PR c++/8338
    	* pt.c (for_each_template_parm): Add htab parameter.
    	(process_partial_specialization): Adjust call.
    	(push_template_decl_real): Likewise.
    	(pair_fn_data): Add visited.
    	(for_each_template_parm_r): Avoid walking duplicates more than
    	once.
    	(uses_template_parms): Adjust call to for_each_template_parm.
    
    	PR c++/8338
    	* g++.dg/template/crash2.C: New test.
    
    From-SVN: r58911

---
+++ gcc/testsuite/g++.dg/template/crash2.C
@@ -0,0 +1,31 @@
+// { dg-options "" }
+
+template <class EnumType>
+class A
+{
+public:
+  static const EnumType size = max; // { dg-error "" }
+  int table[size];
+};
+template <class EnumType>
+const EnumType A<EnumType>::size;
+ 
+ 
+namespace N
+{
+enum E { max = 5 };
+ 
+struct B
+{
+  A<E> a; // { dg-error "" }
+};
+ 
+}
+ 
+int
+main()
+{
+  N::B b;
+ 
+  return 0;
+}
