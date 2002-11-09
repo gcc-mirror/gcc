ff207dc842f3240d553ec500ec56db60815b4048
lerdsuwa <lerdsuwa@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-11-09 11:53:18 +0000

    PR c++/8389 	* pt.c (instantiate_template): Push class scope for member 	functions. 	(get_mostly_instantiated_function_type): Likewise.  Don't call 	tsubst on context.  Remove CONTEXTP and TPARMSP parameters. 	* cp-tree.h (get_mostly_instantiated_function_type): Adjust. 	* mangle.c (write_encoding, write_unqualified_name): Adjust.
    	PR c++/8389
    	* g++.dg/template/access6.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@58950 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/access6.C
@@ -0,0 +1,17 @@
+// { dg-do compile }
+// Origin: Detlef Vollmann <dv@vollmann.ch>
+
+// PR c++/8389
+// Access control ICE for typename during instantiation and name mangling
+
+template <class> class Base {
+  protected:
+    typedef int Type;
+};
+    
+template <class T> struct Derived : public Base<T> {
+  typedef typename Base<T>::Type Type;
+  template <class Arg> void f(Type = Type()) {};
+};
+    
+template void Derived<char>::f<int> (Type);
