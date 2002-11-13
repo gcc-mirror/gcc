097b5fed53a896cab0b922126fbd7bf9d3342238
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-11-13 23:06:03 +0000

    * g++.dg/abi/vague1.C (dg-final): Return if target is hppa*-*-hpux*.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@59094 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/abi/vague1.C
+++ gcc/testsuite/g++.dg/abi/vague1.C
@@ -2,7 +2,9 @@
 // instantiations.
 
 // Disable debug info so we don't get confused by the symbol name there.
+// The test fails on hppa*-*-hpux* because the symbol _ZN1AIiE1tE is imported.
 // { dg-options "-g0" }
+// { dg-final { if { [istarget hppa*-*-hpux*] } { return } } }
 // { dg-final { scan-assembler-not "_ZN1AIiE1tE" } }
 
 template <class T> struct A {
