7ecda9dee9ff402e2667393de88d862195b18f6f
GCC Administrator <gccadmin@gcc.gnu.org>
2002-11-15 08:16:58 +0000

    Daily bump.
    [[Split portion of a mixed commit.]]
    
    From-SVN: r59122.1

--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
                                                                                         #include "gansidecl.h"
 #include "version.h"
 
-const char *const version_string = "3.0.5 20021114 (prerelease)";
+const char *const version_string = "3.0.5 20021115 (prerelease)";
--- libf2c/libI77/Version.c
+++ libf2c/libI77/Version.c
@@ -3,7 +3,7 @@
 /*
 */
 
-char __G77_LIBI77_VERSION__[] = "0.5.26 20021114 (prerelease)";
+char __G77_LIBI77_VERSION__[] = "0.5.26 20021115 (prerelease)";
 
 /*
 2.01	$ format added
