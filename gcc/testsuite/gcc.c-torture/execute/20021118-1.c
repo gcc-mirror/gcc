6977cd6403fab095dc112eec04754dccfeffee6c
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-11-18 12:35:15 +0000

    * config/sh/sh.c (calc_live_regs): Update check for PIC liveness 	in compact code.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@59212 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20021118-1.c
@@ -0,0 +1,15 @@
+struct s { int f[4]; };
+
+int foo (struct s s, int x1, int x2, int x3, int x4, int x5, int x6, int x7)
+{
+  return s.f[3] + x7;
+}
+
+int main ()
+{
+  struct s s = { 1, 2, 3, 4 };
+
+  if (foo (s, 100, 200, 300, 400, 500, 600, 700) != 704)
+    abort ();
+  exit (0);
+}
