9407d5e44b5f395b623e63fe75eec84d2ad47361
toon <toon@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-11-19 23:17:36 +0000

    2002-11-19  Toon Moene  <toon@moene.indiv.nluug.nl>
    	PR fortran/8587
    	* libF77/pow_zz.c: Handle (0.0, 0.0) ** power.
    
    2002-11-19  Toon Moene  <toon@moene.indiv.nluug.nl>
    
    	PR fortran/8587
    	* news.texi: Show PR fortran/8587 fixed.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@59280 138bc75d-0d04-0410-961f-82ee72b054a4

--- libf2c/libF77/pow_zz.c
+++ libf2c/libF77/pow_zz.c
@@ -8,6 +8,12 @@ pow_zz (doublecomplex * r, doublecomplex * a, doublecomplex * b)
 {
   double logr, logi, x, y;
 
+  if (a->r == 0.0 && a->i == 0.0)
+    {
+	/* Algorithm below doesn't cope.  */
+        r->r = r->i = 0.0;
+        return;
+    }
   logr = log (f__cabs (a->r, a->i));
   logi = atan2 (a->i, a->r);
 
