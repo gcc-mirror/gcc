12470812b1a69d16d4cf7247c6cb6e9002b4679b
gccadmin <gccadmin@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-11-19 08:18:06 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@59255 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/version.c
+++ gcc/version.c
@@ -6,7 +6,7 @@
    please modify this string to indicate that, e.g. by putting your
    organization's name in parentheses at the end of the string.  */
 
-const char version_string[] = "3.3 20021118 (experimental)";
+const char version_string[] = "3.3 20021119 (experimental)";
 
 /* This is the location of the online document giving instructions for
    reporting bugs.  If you distribute a modified version of GCC,
