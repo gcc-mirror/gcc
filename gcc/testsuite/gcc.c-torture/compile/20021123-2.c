5066422418cf6b0b365295861e1fd991f2a320d2
Eric Botcazou <ebotcazou@libertysurf.fr>
2002-11-23 08:22:13 +0000

    20021123-2.c: New test.
    	* gcc.c-torture/compile/20021123-2.c: New test.
    	* gcc.c-torture/compile/20021123-3.c: New test.
    
    From-SVN: r59412

---
+++ gcc/testsuite/gcc.c-torture/compile/20021123-2.c
@@ -0,0 +1,9 @@
+/* PR c/8518 */
+/* Contributed by Volker Reichelt. */
+
+/* Verify that GCC doesn't get confused by the
+   redefinition of an extern inline function. */
+
+extern int inline foo () { return 0; }
+extern int inline bar () { return 0; }
+static int inline bar () { return foo(); }
