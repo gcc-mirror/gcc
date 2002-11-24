9833f23897323326b74eebca32979a90f6c14b89
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-11-24 23:26:13 +0000

    * class.c (layout_class_type): Reuse tail padding when laying out 	virtual bases.
    	* g++.dg/abi/empty10.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@59443 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/abi/empty10.C
@@ -0,0 +1,27 @@
+// { dg-do run }
+// { dg-options "-fabi-version=0 -w" }
+
+struct E {};
+struct E2 : public E {};
+
+struct A {
+  int i;
+};
+
+struct B {
+  int j;
+};
+
+struct C :
+  public E, 
+  public A, 
+  public E2, 
+  virtual public B {
+};
+
+C c;
+
+int main () {
+  if (((char*)(B*)&c - (char*)&c) != 8)
+    return 1;
+}
