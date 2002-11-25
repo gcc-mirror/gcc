c4f6640537ff392ad466a83796e68fb79b512bee
Mark Mitchell <mark@codesourcery.com>
2002-11-25 23:17:31 +0000

    Makefile.am (sources): Add guard.cc.
    	* libsupc++/Makefile.am (sources): Add guard.cc.
    	* libsupc++/Makefile.in: Regenerated.
    	* libsupc++/cxxabi.h (__cxa_guard_acquire): New function.
    	(__cxa_guard_release): Likewise.
    	(__cxa_guard_abort): Likewise.
    	* libsupc++/guard.cc: New file.
    
    From-SVN: r59475

--- libstdc++-v3/libsupc++/cxxabi.h
+++ libstdc++-v3/libsupc++/cxxabi.h
@@ -500,6 +500,20 @@ void __cxa_vec_delete3 (void *__array_address,
                         __SIZE_TYPE__ __padding_size,
                         void (*__destructor) (void *),
                         void (*__dealloc) (void *, __SIZE_TYPE__));
+
+/* guard variables */
+
+/* The ABI requires a 64-bit type.  */
+__extension__ typedef int __guard __attribute__((mode (__DI__)));
+
+extern "C"
+int __cxa_guard_acquire (__guard *);
+
+extern "C"
+void __cxa_guard_release (__guard *);
+
+extern "C"
+void __cxa_guard_abort (__guard *);
                   
 /* demangling routines */
 
