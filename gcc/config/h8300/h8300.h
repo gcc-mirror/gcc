8a81154bd0d672fc2ff581fe96d59ee4cb1141df
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-11-27 22:41:36 +0000

    * config/h8300/h8300.h (OK_FOR_U): Remove extra parentheses.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@59591 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/h8300/h8300.h
+++ gcc/config/h8300/h8300.h
@@ -869,10 +869,10 @@ struct cum_arg
        && REG_OK_FOR_BASE_P (XEXP (OP, 0)))				\
    || (GET_CODE (OP) == MEM && GET_CODE (XEXP (OP, 0)) == SYMBOL_REF	\
        && TARGET_H8300S)						\
-   || ((GET_CODE (OP) == MEM && GET_CODE (XEXP (OP, 0)) == CONST	\
-	&& GET_CODE (XEXP (XEXP (OP, 0), 0)) == PLUS			\
-	&& GET_CODE (XEXP (XEXP (XEXP (OP, 0), 0), 0)) == SYMBOL_REF	\
-	&& GET_CODE (XEXP (XEXP (XEXP (OP, 0), 0), 1)) == CONST_INT)	\
+   || (GET_CODE (OP) == MEM && GET_CODE (XEXP (OP, 0)) == CONST		\
+       && GET_CODE (XEXP (XEXP (OP, 0), 0)) == PLUS			\
+       && GET_CODE (XEXP (XEXP (XEXP (OP, 0), 0), 0)) == SYMBOL_REF	\
+       && GET_CODE (XEXP (XEXP (XEXP (OP, 0), 0), 1)) == CONST_INT	\
        && (TARGET_H8300S						\
 	   || SYMBOL_REF_FLAG (XEXP (XEXP (XEXP (OP, 0), 0), 0))))	\
    || (GET_CODE (OP) == MEM						\
