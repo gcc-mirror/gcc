ddd90e806df8ebbe46e6fcbadc88d1d497e01039
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-12-03 20:13:43 +0000

    * java/lang/Character.java (forDigit): Formatting fix.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@59781 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/lang/Character.java
+++ libjava/java/lang/Character.java
@@ -2079,8 +2079,8 @@ public final class Character implements Serializable, Comparable
    */
   public static char forDigit(int digit, int radix)
   {
-    if (radix < MIN_RADIX || radix > MAX_RADIX ||
-        digit < 0 || digit >= radix)
+    if (radix < MIN_RADIX || radix > MAX_RADIX
+	|| digit < 0 || digit >= radix)
       return '\0';
     return (char) (digit < 10 ? ('0' + digit) : ('a' - 10 + digit));
   }
