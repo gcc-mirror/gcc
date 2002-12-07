29d4fdf2c3db9a86238783c10950045383e27283
Gabriel Dos Reis <gdr@gcc.gnu.org>
2002-12-07 18:39:05 +0000

    New test.
    From-SVN: r59920

---
+++ gcc/testsuite/g++.dg/parse/parameter-declaration-1.C
@@ -0,0 +1,6 @@
+// Contributed by Gabriel Dos Reis <gdr@integrable-solutions.net>
+// Origin: Robert Schiele; PR C++/8799
+// { dg-do compile }
+
+struct {
+   a(void = 0; a(0), a(0)                 // { dg-error "" }
