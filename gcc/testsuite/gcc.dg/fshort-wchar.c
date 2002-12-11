81e5d5a7451b777272f4fb9018081af5e88386af
neil <neil@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-12-11 06:36:17 +0000

    * c-common.c (builtin_define_type_max): Handle unsigned 	types too. testsuite: 	* gcc.dg/fshort-wchar: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@60023 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/fshort-wchar.c
@@ -0,0 +1,18 @@
+/* Copyright (C) 2002 Free Software Foundation, Inc.  */
+
+/* { dg-do run } */
+/* { dg-options "-fshort-wchar" } */
+
+/* Source: Neil Booth, 10 Dec 2002.
+
+   Test that __WCHAR_MAX__ is correct with -fshort-wchar.  */
+
+int main ()
+{
+  __WCHAR_TYPE__ w = ~(__WCHAR_TYPE__) 0;
+
+  if (w != __WCHAR_MAX__)
+    abort ();
+ 
+  return 0;
+}
