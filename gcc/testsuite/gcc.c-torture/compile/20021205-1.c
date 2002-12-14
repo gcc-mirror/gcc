69db9b990380290644638ead1edf292f5413a1ee
Dale Johannesen <dalej@gcc.gnu.org>
2002-12-14 00:54:02 +0000

    Fix test for LP64, move it to gcc.c-torture/compile (per Kaveh Ghazi)
    From-SVN: r60111

--- gcc/testsuite/gcc.c-torture/compile/20021205-1.c
+++ gcc/testsuite/gcc.c-torture/compile/20021205-1.c
@@ -1,8 +1,9 @@
 /* dg-do compile */
 /* dg-options "-O3" */
 typedef struct x x;
+extern void *baz(char *);
 struct x { char * (*bar) (int); };
-static x **foo() { return ((x**)baz()); }
+static x **foo() { return ((x**)baz(0)); }
 int xyzzy()
 {
     baz((*foo())->bar(0));
