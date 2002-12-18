539aacfe8ca5403ac29eea3bf009824f5c60a69a
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-12-18 16:42:23 +0000

    PR c++/6255         * decl.c (lookup_name_real): Build a new TYPENAME_TYPE rather than         modifying the old one.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@60259 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/ext/typename1.C
@@ -0,0 +1,7 @@
+// PR c++/6255
+// { dg-options "-fpermissive -w" }
+
+template <class T> struct A { typedef int X; };
+template <class T> struct B { typedef A<T> Y; void f (Y::X); };
+template <class T, class T1, class T2, class T3> struct C : public B<T> { void g (Y::X); };
+template class B<int>;
