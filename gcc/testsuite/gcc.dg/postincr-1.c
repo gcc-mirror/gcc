b197bc017286cbddb7ddbb071cbd79b6cee18e9c
Jason Merrill <jason@gcc.gnu.org>
2002-12-18 15:00:28 -0500

    new test
    From-SVN: r60263

---
+++ gcc/testsuite/gcc.dg/postincr-1.c
@@ -0,0 +1,19 @@
+/* Simple test for proper postincrement semantics.  */
+/* { dg-do run } */
+
+int i;
+int c;
+int *f ()
+{
+  ++c;
+  return &i;
+}
+
+int main ()
+{
+  int r;
+  r = (*f())++;
+  if (!(r == 0 && i == 1 && c == 1))
+    abort ();
+  return 0;
+}
