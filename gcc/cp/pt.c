89992ce057ea705e4c25b3a82d4ec647020e0ffc
Kriang Lerdsuwanakij <lerdsuwa@users.sourceforge.net>
2002-12-18 16:35:12 +0000

    re PR c++/8442 (rejects nested template classes)
    	PR c++/8442
    	* decl2.c (handle_class_head): Verify if the looked up name is a
    	type or template.
    	* pt.c (convert_template_argument): Fix type or template template
    	parameter decision logic.
    
    	* g++.dg/template/type2.C: New test.
    	* g++.dg/template/ttp3.C: Change expected error message.
    
    From-SVN: r60255

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -3341,17 +3341,12 @@ convert_template_argument (parm, arg, args, complain, i, in_decl)
 		     && TREE_CODE (DECL_TEMPLATE_RESULT (arg)) == TYPE_DECL)
 		    || TREE_CODE (arg) == TEMPLATE_TEMPLATE_PARM
 		    || TREE_CODE (arg) == UNBOUND_CLASS_TEMPLATE);
-  else if (CLASSTYPE_TEMPLATE_INFO (arg) && !CLASSTYPE_USE_TEMPLATE (arg)
-	   && PRIMARY_TEMPLATE_P (CLASSTYPE_TI_TEMPLATE (arg)))
-    {
-      if (is_base_of_enclosing_class (arg, current_class_type))
-	/* This is a template name used within the scope of the
-	   template. It could be the template, or it could be the
-	   instantiation. Choose whichever makes sense.  */
-	is_tmpl_type = requires_tmpl_type;
-      else
-	is_tmpl_type = 1;
-    }
+  else if (CLASSTYPE_IS_TEMPLATE (arg)
+	   && is_base_of_enclosing_class (arg, current_class_type))
+    /* This is a template name used within the scope of the
+       template. It could be the template, or it could be the
+       instantiation. Choose whichever makes sense.  */
+    is_tmpl_type = requires_tmpl_type;
   else
     /* It is a non-template class, or a specialization of a template
        class, or a non-template member of a template class.  */
