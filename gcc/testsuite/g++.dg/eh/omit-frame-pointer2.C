914fc8d6cadb552979ea7cf3c3cbe94b0ababbdd
Jason Merrill <jason@gcc.gnu.org>
2002-12-18 11:41:11 -0500

    new test
    From-SVN: r60258

---
+++ gcc/testsuite/g++.dg/eh/omit-frame-pointer2.C
@@ -0,0 +1,35 @@
+// Reduced from PR c++/5246, PR c++/2447
+// { dg-options "-O -fomit-frame-pointer" }
+// { dg-options "-O -fomit-frame-pointer -mno-accumulate-outgoing-args" { target i?86-*-* } }
+// { dg-do run }
+
+void step (int)
+{
+  void *sp = __builtin_alloca (0);
+}
+
+void f2 (void)
+{
+  step (2);
+  throw int();
+}
+
+void f1 (void)
+{
+  try
+    {
+      step (1);
+      f2 ();
+      step (-1);
+    }
+  catch (int)
+    {
+      step (3);
+    }
+}
+
+int main ()
+{
+  f1 ();
+  return 0;
+}
