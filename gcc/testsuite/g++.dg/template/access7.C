cab7a9a3b95c833df6d0175a9a287a9a767879c8
Kriang Lerdsuwanakij <lerdsuwa@users.sourceforge.net>
2002-12-19 14:45:19 +0000

    re PR c++/3663 (G++ doesn't check access control during template instanation)
    	PR c++/3663
    	* pt.c (lookup_template_class): Copy TREE_PRIVATE and
    	TREE_PROTECTED to created decl nodes.
    
    	* g++.dg/template/access7.C: New test.
    
    From-SVN: r60307

---
+++ gcc/testsuite/g++.dg/template/access7.C
@@ -0,0 +1,18 @@
+// { dg-do compile }
+
+// PR c++/3663
+// Enforce access of nested type.
+
+template <typename A>
+class S {
+  class T {};				// { dg-error "private" }
+};
+
+template <typename A>
+typename A::T* f (A) {			// { dg-error "this context" }
+  return 0;
+}
+
+void g () {
+  f (S<int> ());			// { dg-error "context|instantiated" }
+}
