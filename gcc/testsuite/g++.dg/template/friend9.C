c6086df0386437dcda976383c9603cea7307234e
No Author <no-author@gcc.gnu.org>
2002-12-19 15:30:15 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_3-branch'.
    From-SVN: r60309

---
+++ gcc/testsuite/g++.dg/template/friend9.C
@@ -0,0 +1,18 @@
+// { dg-do compile }
+// Origin: Wolfgang Bangerth <bangerth@ticam.utexas.edu>
+
+// PR c++/8099
+// Partial specialization as friend class
+
+template <int N, typename T> struct X;
+template <typename T>        struct X<1,T>;
+
+template <typename P> class Y {
+    static int i;
+    template <int N, typename T> friend struct X;
+    friend struct X<1,P>;
+};
+
+template <typename T> struct X<1,T> {
+    X () { Y<T>::i; };     // access private field
+};
