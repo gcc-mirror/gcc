3ab68120e2c7f48ff4de37f103800d0a2385d31c
Devang Patel <dpatel@gcc.gnu.org>
2002-12-19 15:46:49 -0800

    Add support for darwin linker option -dynamic.
    From-SVN: r60341

--- gcc/config/rs6000/darwin.h
+++ gcc/config/rs6000/darwin.h
@@ -58,7 +58,9 @@ Boston, MA 02111-1307, USA.  */
 /* We want -fPIC by default, unless we're using -static to compile for
    the kernel or some such.  */
 
-#define CC1_SPEC "%{!static:-fPIC}"
+#define CC1_SPEC "\
+%{static: %{Zdynamic: %e conflicting code gen style switches are used}}\
+%{!static:-fPIC}"
 
 /* Make both r2 and r3 available for allocation.  */
 #define FIXED_R2 0
