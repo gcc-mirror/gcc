51d6eed48eace41bef16aa1d4401e0fb081e7d5d
Andrew Haley <aph@redhat.com>
2002-12-20 02:06:21 +0000

    natResourceBundle.cc: Include ArrayIndexOutOfBoundsException.h.
    2002-12-19  Andrew Haley  <aph@redhat.com>
    
    	* java/util/natResourceBundle.cc: Include
    	ArrayIndexOutOfBoundsException.h.
    	(getCallingClassLoader): Don't put upper bound on stack search.
    	Catch ArrayIndexOutOfBoundsException.
    
    From-SVN: r60348

--- libjava/java/util/natResourceBundle.cc
+++ libjava/java/util/natResourceBundle.cc
@@ -16,17 +16,24 @@ details.  */
 #include <java/lang/SecurityManager.h>
 #include <java/lang/ClassLoader.h>
 #include <java/lang/Class.h>
+#include <java/lang/ArrayIndexOutOfBoundsException.h>
 #include <gnu/gcj/runtime/StackTrace.h>
 
 java::lang::ClassLoader *
 java::util::ResourceBundle::getCallingClassLoader ()
 {
   gnu::gcj::runtime::StackTrace *t = new gnu::gcj::runtime::StackTrace(6);
-  for (int i = 3; i < 6; ++i)
+  try
     {
-      jclass klass = t->classAt(i);
-      if (klass != NULL)
-	return klass->getClassLoaderInternal();
+      for (int i = 3; ; ++i)
+	{
+	  jclass klass = t->classAt(i);
+	  if (klass != NULL)
+	    return klass->getClassLoaderInternal();
+	}
+    }
+  catch (::java::lang::ArrayIndexOutOfBoundsException *e)
+    {
     }
   return NULL;
 }
