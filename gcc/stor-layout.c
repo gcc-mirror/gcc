4975da72b5d11b5c621f6bc7f3138941c91bb520
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-12-23 16:39:36 +0000

    * stor-layout.c (update_alignment_for_field): Correct handling of 	unnamed bitfields on PCC_BITFIELD_TYPE_MATTERS machines. 	* doc/tm.texi (PCC_BITFIELD_TYPE_MATTERS): Note that an unnamed 	bitfield does not affect alignment.
    	* testsuite/gcc.dg/i386-bitfield3.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@60439 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/stor-layout.c
+++ gcc/stor-layout.c
@@ -715,13 +715,9 @@ update_alignment_for_field (rli, field, known_align)
       && DECL_BIT_FIELD_TYPE (field)
       && ! integer_zerop (TYPE_SIZE (type)))
     {
-      /* For these machines, a zero-length field does not
-	 affect the alignment of the structure as a whole.
-	 It does, however, affect the alignment of the next field
-	 within the structure.  */
-      if (! integer_zerop (DECL_SIZE (field)))
-	rli->record_align = MAX (rli->record_align, desired_align);
-      else if (! DECL_PACKED (field))
+      /* A zero-length bit-field affects the alignment of the next
+	 field.  */
+      if (!DECL_PACKED (field) && integer_zerop (DECL_SIZE (field)))
 	{
 	  desired_align = TYPE_ALIGN (type);
 #ifdef ADJUST_FIELD_ALIGN
@@ -729,8 +725,8 @@ update_alignment_for_field (rli, field, known_align)
 #endif
 	}
 
-      /* A named bit field of declared type `int'
-	 forces the entire structure to have `int' alignment.  */
+      /* Named bit-fields cause the entire structure to have the
+	 alignment implied by their type.  */
       if (DECL_NAME (field) != 0)
 	{
 	  unsigned int type_align = TYPE_ALIGN (type);
@@ -745,7 +741,14 @@ update_alignment_for_field (rli, field, known_align)
 	  else if (DECL_PACKED (field))
 	    type_align = MIN (type_align, BITS_PER_UNIT);
 
+	  /* The alignment of the record is increased to the maximum
+	     of the current alignment, the alignment indicated on the
+	     field (i.e., the alignment specified by an __aligned__
+	     attribute), and the alignment indicated by the type of
+	     the field.  */
+	  rli->record_align = MAX (rli->record_align, desired_align);
 	  rli->record_align = MAX (rli->record_align, type_align);
+
 	  rli->unpadded_align = MAX (rli->unpadded_align, DECL_ALIGN (field));
 	  if (warn_packed)
 	    rli->unpacked_align = MAX (rli->unpacked_align, TYPE_ALIGN (type));
---
+++ gcc/testsuite/gcc.dg/i386-bitfield3.c
@@ -0,0 +1,24 @@
+// Test for bitfield alignment in structs on IA-32
+// { dg-do run { target i?86-*-* } }
+// { dg-options "-O2" }
+// { dg-options "-mno-align-double -mno-ms-bitfields" { target *-*-interix* } }
+
+extern void abort (void);
+extern void exit (int);
+
+struct X {
+  int : 32;
+};
+
+struct Y {
+  int i : 32;
+};
+
+int main () {
+  if (__alignof__(struct X) != 1)
+    abort ();
+  if (__alignof__(struct Y) != 4)
+    abort ();
+  
+  exit (0);
+}
