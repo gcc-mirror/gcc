670567924db16cb51d7b6a1cb2da62e92b083a01
GCC Administrator <gccadmin@gcc.gnu.org>
2002-12-30 08:17:21 +0000

    Daily bump.
    [[Split portion of a mixed commit.]]
    
    From-SVN: r60625.1

--- gcc/version.c
+++ gcc/version.c
@@ -1,1 +1,1 @@
-                                                                                        char *version_string = "2.95.4 20021229 (prerelease)";
+                                                                                        char *version_string = "2.95.4 20021230 (prerelease)";
