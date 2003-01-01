f309ff0a12fac773dfa22cc957fa0da6650be2ce
Steven Bosscher <s.bosscher@student.tudelft.nl>
2003-01-01 16:10:03 +0100

    Make-lang.in, [...]: Replace "GNU CC" with "GCC" in the copyright header.
    	* Make-lang.in, boehm.c, buffer.c,
    	  buffer.h, builtins.c, class.c,
    	  config-lang.in, constants.c,
    	  convert.h, decl.c, except.c,
    	  expr.c, java-except.h,
    	  java-tree.h, javaop.def,
    	  jcf-parse.c, jcf-write.c,
    	  jv-scan.c, jvgenmain.c,
    	  jvspec.c, keyword.gperf,
    	  keyword.h, lang-options.h,
    	  lang-specs.h, lang.c, lex.c,
    	  lex.h, mangle.c, mangle_name.c,
    	  parse-scan.y, parse.h, parse.y,
    	  typeck.c, verify.c, xref.c,
    	  xref.h: Replace "GNU CC" with
    	  "GCC" in the copyright header.
    
    	* check-init.c, gjavah.c, javaop.h,
    	  jcf-depend.c, jcf-dump.c, jcf-io.c,
    	  jcf-path.c, jcf-reader.c, jcf.h,
    	  zextract.c, zipfile.h: These files are
    	  "part of GCC". Also say "GCC" not "GNU CC".
    
    From-SVN: r60747

--- gcc/java/jcf-reader.c
+++ gcc/java/jcf-reader.c
@@ -1,21 +1,23 @@
 /* This file read a Java(TM) .class file.
    It is not stand-alone:  It depends on tons of macros, and the
    intent is you #include this file after you've defined the macros.
+   Copyright (C) 1996, 1997, 1998, 1999, 2000, 2003
+   Free Software Foundation, Inc.
 
-   Copyright (C) 1996, 1997, 1998, 1999, 2000  Free Software Foundation, Inc.
+This file is part of GCC.
 
-This program is free software; you can redistribute it and/or modify
+GCC is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2, or (at your option)
 any later version.
 
-This program is distributed in the hope that it will be useful,
+GCC is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with GNU CC; see the file COPYING.  If not, write to
+along with GCC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  
 
