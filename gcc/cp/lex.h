f5adbb8d5cffe99b80082efe900de830c2971aeb
Steven Bosscher <s.bosscher@student.tudelft.nl>
2003-01-01 12:58:58 +0100

    call.c, [...]: Replace "GNU CC" with "GCC" in the copyright header.
    	* call.c, class.c, cp-lang.c, cp-tree.h, cvt.c, dump.c, error.c,
    	  except.c, expr.c friend.c, g++spec.c, init.c, lang-options.h,
    	  lang-specs.h, lex.c, mangle.c, method.c, optimize.c, parser.c,
    	  pt.c, ptree.c, repo.c, rtti.c, search.c, semantics.c, tree.c,
    	  typeck.c, typeck2.c: Replace "GNU CC" with "GCC" in the
    	  copyright header.
    	* lex.h: parse.y is dead, so don't mention it.  Also replace the
    	  copyright header with the default GNU copyright header.
    
    From-SVN: r60738

--- gcc/cp/lex.h
+++ gcc/cp/lex.h
@@ -1,25 +1,26 @@
-/* Define constants and variables for communication with parse.y.
+/* Define constants and variables for communication with the parser.
    Copyright (C) 1987, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
    2000 Free Software Foundation, Inc.
    Hacked by Michael Tiemann (tiemann@cygnus.com)
    and by Brendan Kehoe (brendan@cygnus.com).
 
-This file is part of GNU CC.
+   This file is part of GCC.
 
-GNU CC is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY.  No author or distributor
-accepts responsibility to anyone for the consequences of using it
-or for whether it serves any particular purpose or works at all,
-unless he says so in writing.  Refer to the GNU CC General Public
-License for full details.
+   GCC is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
-Everyone is granted permission to copy, modify and redistribute
-GNU CC, but only under the conditions described in the
-GNU CC General Public License.   A copy of this license is
-supposed to have been given to you along with GNU CC so you
-can know your rights and responsibilities.  It should be in a
-file named COPYING.  Among other things, the copyright notice
-and this notice must be preserved on all copies.  */
+   GCC is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GCC; see the file COPYING.  If not, write to the Free
+   Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.  */
+
 
 #ifndef GCC_CP_LEX_H
 #define GCC_CP_LEX_H
--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -7,20 +7,20 @@
    Written by Mark Mitchell (mmitchell@usa.net) based on code found
    formerly in parse.y and pt.c.  
 
-   This file is part of GNU CC.
+   This file is part of GCC.
 
-   GNU CC is free software; you can redistribute it and/or modify it
+   GCC is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
    any later version.
    
-   GNU CC is distributed in the hope that it will be useful, but
+   GCC is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    General Public License for more details.
    
    You should have received a copy of the GNU General Public License
-   along with GNU CC; see the file COPYING.  If not, write to the Free
+   along with GCC; see the file COPYING.  If not, write to the Free
    Software Foundation, 59 Temple Place - Suite 330, Boston, MA
    02111-1307, USA.  */
 
