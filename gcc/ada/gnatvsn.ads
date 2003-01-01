05a008daf1fce233f290ef1788fa0d3240d34cb2
GCC Administrator <gccadmin@gcc.gnu.org>
2003-01-01 08:17:01 +0000

    Daily bump.
    [[Split portion of a mixed commit.]]
    
    From-SVN: r60735.2

--- gcc/ada/gnatvsn.ads
+++ gcc/ada/gnatvsn.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---                            $Revision: 1.2.12.222 $
+--                            $Revision: 1.2.12.223 $
 --                                                                          --
 --          Copyright (C) 1992-2002 Free Software Foundation, Inc.          --
 --                                                                          --
@@ -38,7 +38,7 @@
 
 package Gnatvsn is
 
-   Gnat_Version_String : constant String := "3.2 20021231 (prerelease)";
+   Gnat_Version_String : constant String := "3.2 20030101 (prerelease)";
    --  Version output when GNAT (compiler), or its related tools, including
    --  GNATBIND, GNATCHOP, GNATFIND, GNATLINK, GNATMAKE, GNATXREF, are run
    --  (with appropriate verbose option switch set).
