175938bee4a50ca84a0824b4843f2f88ba58b985
neroden <neroden@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-01-01 21:03:50 +0000

    g++.dg/lookup/exception1.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@60763 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/lookup/exception1.C
@@ -0,0 +1,33 @@
+/* PR 2743 */
+/* {dg-do compile} */
+
+namespace ns {
+  class Exception
+  {
+  };
+}
+
+namespace ns
+{
+  class Test {
+    public:
+      inline Test() throw( Exception );
+      inline Test(int n ) throw( Exception );
+    private:
+      int i;
+  };
+}
+
+// This line used to fail because Exception wasn't looked up in the
+// right scope.
+ns::Test::Test() throw( Exception ) : i( 1 )
+{
+}
+
+ns::Test::Test( int n ) throw( Exception ) : i( n )
+{
+}
+
+int main(int argc, char* argv) {
+  ns::Test test;
+}
