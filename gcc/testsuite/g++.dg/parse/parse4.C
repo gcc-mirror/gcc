79cc29861692f8e55c2bb599315356e1c162dd14
Neil Booth <neil@daikokuya.co.uk>
2003-01-01 00:49:36 +0000

    * g++.dg/parse/parse4.C: New test.
    From-SVN: r60728

---
+++ gcc/testsuite/g++.dg/parse/parse4.C
@@ -0,0 +1,25 @@
+/* PR c++/88 */
+/* { dg-do compile } */
+
+class Hvec
+{
+public:
+  Hvec(double x, double y, double z);
+  ~Hvec();
+};
+
+
+class Camera1
+{
+public:
+  Camera1(const Hvec &basepos=Hvec(0.0,0.0,0.0));
+  ~Camera1();
+};
+
+class Camera2
+{
+public:
+  Camera2(const Hvec &basepos);
+  inline Camera2() { Camera2(Hvec(0.0,0.0,0.0)); };
+  ~Camera2();
+};
