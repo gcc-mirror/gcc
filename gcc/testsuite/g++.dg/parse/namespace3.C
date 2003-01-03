c50dc701fcdec3d84e1346a140c659a9e01d9002
neroden <neroden@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-01-03 12:52:34 +0000

    * g++.dg/parse/namespace3.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@60828 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/namespace3.C
@@ -0,0 +1,12 @@
+/* PR c+/3816 */
+/* { dg-do compile } */
+namespace A {};
+ 
+namespace OtherNamespace {
+ 
+  typedef struct {
+     int member;
+   } A; // used to conflict with A namespace
+ 
+}; // end of namespace
+ 
