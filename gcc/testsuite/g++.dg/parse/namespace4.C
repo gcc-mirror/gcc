5611a3af19ad8eb0a01ba885a43b8664f1fe39c5
neroden <neroden@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-01-03 15:50:11 +0000

    * g++.dg/parse/namespace4.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@60832 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/namespace4.C
@@ -0,0 +1,18 @@
+/* PR c++/4652 */
+/* { dg-do compile } */
+/* Another conflict between namespace IDs and other things. */
+
+namespace A { };
+
+class B {
+
+   struct {
+      int x;
+   } A;
+
+};
+
+int main() {
+   B b;
+   return 0;
+}
