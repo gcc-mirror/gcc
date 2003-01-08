4298c66b588698054310f031cb6daae4a9f1ffa1
Jan Hubicka <jh@suse.cz>
2003-01-08 12:51:42 +0100

    i386-cadd.c: New test.
    	* gcc.dg/i386-cadd.c: New test.
    	* gcc.dg/i386-cmov4.c: Likewise.
    
    From-SVN: r61039

---
+++ gcc/testsuite/gcc.dg/i386-cmov4.c
@@ -0,0 +1,19 @@
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -march=athlon" } */
+/* { dg-final { scan-assembler "cmov" } } */
+
+/* Verify that if conversion happends for memory references.  */
+int ARCHnodes;
+int *nodekind;
+float *nodekindf;
+t()
+{
+int i;
+/* Redefine nodekind to be 1 for all surface nodes */
+
+  for (i = 0; i < ARCHnodes; i++) {
+    nodekind[i] = (int) nodekindf[i];
+    if (nodekind[i] == 3)
+      nodekind[i] = 1;
+  }
+}
