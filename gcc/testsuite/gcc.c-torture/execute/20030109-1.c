0b2bf35a6c05a687945b6b19922d0b2f1c1aa343
No Author <no-author@gcc.gnu.org>
2003-01-09 09:27:02 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_3-branch'.
    From-SVN: r61093

---
+++ gcc/testsuite/gcc.c-torture/execute/20030109-1.c
@@ -0,0 +1,20 @@
+/* PR c/8032 */
+/* Verify that an empty initializer inside a partial
+   parent initializer doesn't confuse GCC.  */
+
+struct X
+{
+  int a;
+  int b;
+  int z[];
+};
+
+struct X x = { .b = 40, .z = {} };
+
+int main ()
+{
+  if (x.b != 40)
+    abort ();
+
+  return 0;
+}
