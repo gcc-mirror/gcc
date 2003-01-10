f68d1b24ca398d904eed470f00ccd38f0b719706
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-01-10 23:34:44 +0000

    new test
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@61177 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/init/bitfield1.C
@@ -0,0 +1,24 @@
+// Check that we handle bitfields as complex lvalues properly.
+
+struct A
+{
+  int i: 2;
+  int j: 2;
+  int k: 2;
+};
+
+A a, a2;
+bool b;
+void f ();
+
+int main ()
+{
+  (f(), a.j) = 1;
+  (f(), a).j = 2;
+  (b ? a.j : a2.k) = 3;
+  (b ? a : a2).j = 0;
+  ++(a.j) = 1;
+  (a.j = 2) = 3;
+}
+
+    
