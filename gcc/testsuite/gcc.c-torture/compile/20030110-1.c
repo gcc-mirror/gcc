7558f66e3d289267da3142a374f85e3482a33d79
Josef Zlomek <zlomekj@suse.cz>
2003-01-10 14:54:08 +0100

    20030110-1.c: New test.
    	* gcc.c-torture/compile/20030110-1.c: New test.
    Test for http://gcc.gnu.org/ml/gcc-patches/2003-01/msg00519.html
    
    From-SVN: r61153

---
+++ gcc/testsuite/gcc.c-torture/compile/20030110-1.c
@@ -0,0 +1,39 @@
+extern char bpp;
+
+void foo()
+{
+  if (bpp == 32)
+    {
+      if (2 < 8)
+	{
+	  do
+	    {
+	      while (inb(0x9ae8) & (0x0100 >> (2 +1)));
+	    }
+	  while(0);
+	}
+      else
+	{
+	  do
+	    {
+	      while (inb(0x9ae8) & (0x0100 >> (2)));
+	    }
+	  while(0);
+	}
+    }
+  else
+    do
+      { 
+	while (inb(0x9ae8) & (0x0100 >> (1)));
+      }
+    while(0);
+  if (8 < 8)
+    {
+      do
+	{
+	  while (inb(0x9ae8) & (0x0100 >> (8 +1)));
+	}
+      while(0);
+    }
+}
+
