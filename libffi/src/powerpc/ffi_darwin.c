5f7c78d8a4df096dacfa2d9de367a6174dd631ba
Geoffrey Keating <geoffk@apple.com>
2003-01-10 00:01:32 +0000

    ffi_darwin.c: Remove RCS version string.
    	* src/powerpc/ffi_darwin.c: Remove RCS version string.
    	* src/powerpc/darwin.S: Remove RCS version string.
    
    From-SVN: r61135

--- libffi/src/powerpc/ffi_darwin.c
+++ libffi/src/powerpc/ffi_darwin.c
@@ -6,8 +6,6 @@
    Darwin ABI support (c) 2001 John Hornkvist
    AIX ABI support (c) 2002 Free Software Foundation, Inc.
 
-   $Id: ffi_darwin.c,v 1.4 2002/03/07 18:24:42 dje Exp $
-
    Permission is hereby granted, free of charge, to any person obtaining
    a copy of this software and associated documentation files (the
    ``Software''), to deal in the Software without restriction, including
