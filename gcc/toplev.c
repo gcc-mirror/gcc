6674394eda2b9bee4e1941de5d6f1b605b54ed21
geoffk <geoffk@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-01-11 00:45:47 +0000

    * toplev.c (documented_lang_options): Document -Winvalid-pch.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@61181 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/toplev.c
+++ gcc/toplev.c
@@ -1,6 +1,6 @@
 /* Top level of GNU C compiler
    Copyright (C) 1987, 1988, 1989, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002 Free Software Foundation, Inc.
+   1999, 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
@@ -1308,6 +1308,8 @@ documented_lang_options[] =
   { "-Wimport",
     N_("Warn about the use of the #import directive") },
   { "-Wno-import", "" },
+  { "-Winvalid-pch",
+    N_("Warn about PCH files that are found but not used") },
   { "-Wlong-long","" },
   { "-Wno-long-long",
     N_("Do not warn about using 'long long' when -pedantic") },
