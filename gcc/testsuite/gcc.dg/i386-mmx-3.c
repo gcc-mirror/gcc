bc3444909736cac647fe2ddabeec81a378cd55f3
No Author <no-author@gcc.gnu.org>
2003-01-14 11:47:24 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_3-branch'.
    From-SVN: r61276

---
+++ gcc/testsuite/gcc.dg/i386-mmx-3.c
@@ -0,0 +1,17 @@
+/* PR target/8870 */
+/* Originator: otaylor@redhat.com */
+/* { dg-do compile { target i?86-*-* } } */
+/* { dg-options "-O1 -mmmx -march=i686" } */
+
+typedef int v4hi __attribute__ ((mode (V4HI)));
+
+static inline v4hi cvtsi_v4hi (int i)
+{
+  long long tmp = i;
+  return (v4hi) tmp;
+}
+
+v4hi bar (unsigned short a)
+{
+  return cvtsi_v4hi (a);
+}
