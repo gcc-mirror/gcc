d0faeb9de68a22b42cf5e0f2d362bf1b6991760c
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-01-19 13:04:24 +0000

    * config.gcc (mips64*-*-linux*): Added. * config/mips/linux64.h, config/mips/t-linux64: New file. * config/mips/iris6.h (MIPS_TFMODE_FORMAT): Define. * config/mips/mips.c (override_options): Use it. * config/mips/mips.h (TARGET_SWITCHES): Added... (SUBTARGET_TARGET_SWITCHES): New, empty by default. * Makefile.in (SPECS): New. (STAGESTUFF, specs, mostlyclean, install-common): Use it. * gcc.c (process_command): Move self-spec processing past spec file loading. * doc/tm.texi (DRIVER_SELF_SPECS): Document the change. * doc/fragments.texi (MULTILIB_EXTRA_OPTS): Document need for CRTSTUFF_T_CFLAGS. (SPECS): Document. * doc/invoke.texi (-mabi-fake-default): Document.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@61481 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/mips/linux64.h
@@ -0,0 +1,99 @@
+/* Definitions for MIPS running Linux-based GNU systems with ELF format
+   using n32/64 abi.
+   Copyright 2002, 2003 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+/* This sets the post-install default ABI to n32.  This must NOT be
+   kept in sync with the default ABI in gcc/config.gcc; it's actually
+   meant to override that.  However, for correct behavior at build
+   time, we also need t-linux64 to get the build-time specs in line
+   with the setting in config.gcc.  */
+#define DRIVER_DEFAULT_ABI_SELF_SPEC "%{!mabi=*:-mabi=n32}"
+#define SUBTARGET_EXTRA_SPECS \
+  { "driver_default_abi_self_spec", DRIVER_DEFAULT_ABI_SELF_SPEC },
+#define DRIVER_SELF_SPECS \
+"%{mabi-fake-default:%{!mabi=*:-mabi=32}}", \
+"%(driver_default_abi_self_spec)", \
+"%{!mips*:%{mabi=32:-mips1}%{mabi=n32:-mips3}%{mabi=64:-mips4}}"
+#undef SUBTARGET_TARGET_SWITCHES
+#define SUBTARGET_TARGET_SWITCHES \
+  { "abi-fake-default", 0, N_("Same as -mabi=32, just trickier") },
+
+#undef SUBTARGET_ASM_SPEC
+#define SUBTARGET_ASM_SPEC "\
+%{!fno-PIC:%{!fno-pic:-KPIC}} \
+%{fno-PIC:-non_shared} %{fno-pic:-non_shared} \
+%{mabi=64:-64} %{mabi=n32:-n32} \
+%{!mips*: %{mabi=n32|mabi=32:-mips3} %{mabi=64:-mips4}}"
+
+#undef LIB_SPEC
+#define LIB_SPEC "\
+%{!static: \
+ %{mabi=n32:-rpath-link %R/lib32:%R/usr/lib32} \
+ %{mabi=64:-rpath-link %R/lib64:%R/usr/lib64} \
+ %{mabi=32:-rpath-link %R/lib:%R/usr/lib}} \
+%{!shared: %{pthread:-lthread} %{profile:-lc_p} %{!profile: -lc}}"
+
+#undef LINK_SPEC
+#define LINK_SPEC "\
+%{G*} %{EB} %{EL} %{mips1} %{mips2} %{mips3} %{mips4} \
+%{bestGnum} %{shared} %{non_shared} \
+%{call_shared} %{no_archive} %{exact_version} \
+ %(endian_spec) \
+  %{!shared: \
+    %{!ibcs: \
+      %{!static: \
+        %{rdynamic:-export-dynamic} \
+        %{!dynamic-linker: \
+	  %{mabi=n32: -dynamic-linker /lib32/ld.so.1} \
+	  %{mabi=64: -dynamic-linker /lib64/ld.so.1} \
+	  %{mabi=32: -dynamic-linker /lib/ld.so.1}}} \
+      %{static:-static}}} \
+%{mabi=n32: -melf32btsmipn32} \
+%{mabi=64: -melf64btsmip} \
+%{mabi=32: -melf32btsmip}"
+
+#undef STARTFILE_PREFIX_SPEC
+#define STARTFILE_PREFIX_SPEC "\
+%{mabi=n32: /lib32/ /usr/lib32/} \
+%{mabi=64: /lib64/ /usr/lib64/} \
+%{mabi=32: /lib/ /usr/lib/}"
+
+#undef LOCAL_LABEL_PREFIX
+#define LOCAL_LABEL_PREFIX ((mips_abi == ABI_32 || mips_abi == ABI_O64) \
+			    ? "$" : ".")
+
+/* The size in bytes of a DWARF field indicating an offset or length
+   relative to a debug info section, specified to be 4 bytes in the DWARF-2
+   specification.  The SGI/MIPS ABI defines it to be the same as PTR_SIZE.  */
+#define DWARF_OFFSET_SIZE PTR_SIZE
+
+#undef ASM_PREFERRED_EH_DATA_FORMAT
+#if 0  /* We can't use relative addressing modes on NEWABI :-(  */
+#define ASM_PREFERRED_EH_DATA_FORMAT(CODE, GLOBAL)       		\
+   (flag_pic								\
+    ? ((GLOBAL) ? DW_EH_PE_indirect : 0) | DW_EH_PE_sdata4		\
+    : DW_EH_PE_absptr)
+#endif
+
+/* GNU/Linux doesn't use the same floating-point format that IRIX uses
+   for long double.  There's no need to override this here, since
+   ieee_quad_format is the default, but let's put this here to make
+   sure nobody thinks we just forgot to set it to something else.  */
+#define MIPS_TFMODE_FORMAT ieee_quad_format
