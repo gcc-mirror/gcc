d99c740f5832aa30a8250cbc236838f507c1767f
Kazu Hirata <kazu@cs.umass.edu>
2003-01-20 06:42:58 +0000

    h8300.c (notice_update_cc): Don't assume that recog_data.operands[0] is always associated with cc0.
    	* config/h8300/h8300.c (notice_update_cc): Don't assume that
    	recog_data.operands[0] is always associated with cc0.
    
    	* gcc.c-torture/execute/20030120-1.c: New.
    
    From-SVN: r61498

---
+++ gcc/testsuite/gcc.c-torture/execute/20030120-1.c
@@ -0,0 +1,50 @@
+/* On H8/300 port, NOTICE_UPDATE_CC had a bug that causes the final
+   pass to remove test insns that should be kept.  */
+
+unsigned short
+test1 (unsigned short w)
+{
+  if ((w & 0xff00) == 0)
+    {
+      if (w == 0)
+	w = 2;
+    }
+  return w;
+}
+
+unsigned long
+test2 (unsigned long w)
+{
+  if ((w & 0xffff0000) == 0)
+    {
+      if (w == 0)
+	w = 2;
+    }
+  return w;
+}
+
+int
+test3 (unsigned short a)
+{
+  if (a & 1)
+    return 1;
+  else if (a)
+    return 1;
+  else
+    return 0;
+}
+
+int
+main ()
+{
+  if (test1 (1) != 1)
+    abort ();
+
+  if (test2 (1) != 1)
+    abort ();
+
+  if (test3 (2) != 1)
+    abort ();
+
+  exit (0);
+}
