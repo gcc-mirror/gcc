1a4195e6d47cea698568f1b47618a0030a82ef3d
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-01-21 20:53:31 +0000

    2003-01-21  Ranjit Mathew  <rmathew@hotmail.com>
    	* jartool.c (list_jar): Terminate the 'ascii_date' buffer
    	with a '\0' to guard against the case where the formatted
    	time string is more than the size allowed by the buffer.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@61571 138bc75d-0d04-0410-961f-82ee72b054a4

--- fastjar/jartool.c
+++ fastjar/jartool.c
@@ -1564,7 +1564,7 @@ int list_jar(int fd, char **files, int file_num){
   int i, j;
   time_t tdate;
   struct tm *s_tm;
-  char ascii_date[30];
+  char ascii_date[31];
   zipentry ze;
 
 #ifdef DEBUG
@@ -1655,6 +1655,7 @@ int list_jar(int fd, char **files, int file_num){
         tdate = dos2unixtime(mdate);
         s_tm = localtime(&tdate);
         strftime(ascii_date, 30, "%a %b %d %H:%M:%S %Z %Y", s_tm);
+        ascii_date[30] = '\0';
       }
 
       if(filename_len < fnlen + 1){
@@ -1781,6 +1782,7 @@ int list_jar(int fd, char **files, int file_num){
           free(filename);
         
         filename = malloc(sizeof(ub1) * (fnlen + 1));
+        ascii_date[30] = '\0';
         filename_len = fnlen + 1;
       }
       
