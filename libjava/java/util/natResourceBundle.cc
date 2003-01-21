5eb1a9023471d13018bda42df4c80cef90571895
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-01-21 21:16:46 +0000

    * java/util/natResourceBundle.cc (getCallingClassLoader): Start 	search at 2, not 3.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@61574 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/util/natResourceBundle.cc
+++ libjava/java/util/natResourceBundle.cc
@@ -1,4 +1,4 @@
-/* Copyright (C) 2002  Free Software Foundation
+/* Copyright (C) 2002, 2003  Free Software Foundation
 
    This file is part of libgcj.
 
@@ -25,7 +25,10 @@ java::util::ResourceBundle::getCallingClassLoader ()
   gnu::gcj::runtime::StackTrace *t = new gnu::gcj::runtime::StackTrace(6);
   try
     {
-      for (int i = 3; ; ++i)
+      /* Frame 0 is this method, frame 1 is getBundle, so starting at
+	 frame 2 we might see the user's class.  FIXME: should account
+	 for reflection, JNI, etc, here.  */
+      for (int i = 2; ; ++i)
 	{
 	  jclass klass = t->classAt(i);
 	  if (klass != NULL)
