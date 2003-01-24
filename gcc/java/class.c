fd0447cd965c0a7519ef243ad208a4213eb28d23
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-01-24 17:44:03 +0000

    * class.c (build_static_field_ref): Only a String or numeric field 	can fold to a constant.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@61723 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/java/class.c
+++ gcc/java/class.c
@@ -922,8 +922,18 @@ build_static_field_ref (tree fdecl)
   tree fclass = DECL_CONTEXT (fdecl);
   int is_compiled = is_compiled_class (fclass);
 
-  /* Allow static final fields to fold to a constant.  */
-  if (is_compiled || FIELD_FINAL (fdecl))
+  /* Allow static final fields to fold to a constant.  When using
+     -fno-assume-compiled, gcj will sometimes try to fold a field from
+     an uncompiled class.  This is required when the field in question
+     meets the appropriate criteria for a compile-time constant.
+     However, currently sometimes gcj is too eager and will end up
+     returning the field itself, leading to an incorrect external
+     reference being generated.  */
+  if (is_compiled
+      || (FIELD_FINAL (fdecl) && DECL_INITIAL (fdecl) != NULL_TREE
+	  && (JSTRING_TYPE_P (TREE_TYPE (fdecl))
+	      || JNUMERIC_TYPE_P (TREE_TYPE (fdecl)))
+	  && TREE_CONSTANT (DECL_INITIAL (fdecl))))
     {
       if (!DECL_RTL_SET_P (fdecl))
 	{
