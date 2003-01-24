1a6ebfa45664a0bb3fc2f452e4a367635007ac08
GCC Administrator <gccadmin@gcc.gnu.org>
2003-01-24 00:17:00 +0000

    Daily bump.
    [[Split portion of a mixed commit.]]
    
    From-SVN: r61687.1

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-                                                                                        const char *ffe_version_string = "0.5.25 20030123 (prerelease)";
+                                                                                        const char *ffe_version_string = "0.5.25 20030124 (prerelease)";
