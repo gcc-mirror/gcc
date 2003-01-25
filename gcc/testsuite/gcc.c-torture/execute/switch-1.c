315e4c10f9ffea1a45962cb21e291b1afcf7f00c
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-01-25 17:30:29 +0000

    * stmt.c (emit_case_bit_tests): New routine to implement suitable 	switch statements using the equivalent of "if ((1<<x) & cst) ... ". 	(case_bit_test_cmp): New comparison function for "qsort" to order 	case_bit_tests by decreasing number of destination nodes. 	(lshift_cheap_p): New function to determine if "1 << x" is cheap. 	(expand_end_case_type): Use emit_case_bit_tests to implement 	suitable switch statments. 	(CASE_USE_BIT_TESTS): New target macro to disable the above. 	* Makefile.in (stmt.o): Add dependency on optab.h. 	* doc/tm.texi (CASE_USE_BIT_TESTS): Document new target macro.
    	* gcc.c-torture/execute/switch-1.c: New test case.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@61784 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/switch-1.c
@@ -0,0 +1,57 @@
+/* Copyright (C) 2003  Free Software Foundation.
+
+   Test that switch statements suitable using case bit tests are
+   implemented correctly.
+
+   Written by Roger Sayle, 01/25/2001.  */
+
+extern void abort (void);
+
+int
+foo (int x)
+{
+  switch (x)
+    {
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+    }
+  return 31;
+}
+
+int
+main (int argc)
+{
+  int i, r;
+
+  for (i=-1; i<66; i++)
+    {
+      r = foo (i);
+      if (i == 4)
+	{
+	  if (r != 30)
+	    abort ();
+	}
+      else if (i == 6)
+	{
+	  if (r != 30)
+	    abort ();
+	}
+      else if (i == 9)
+	{
+	  if (r != 30)
+	    abort ();
+	}
+      else if (i == 11)
+	{
+	  if (r != 30)
+	    abort ();
+	}
+      else if (r != 31)
+	abort ();
+    }
+  return 0;
+}
+
