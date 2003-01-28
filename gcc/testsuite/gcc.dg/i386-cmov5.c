b96f4a6b7d3149f608506db8aede7ce2b5587419
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-01-28 19:16:01 +0000

    * gcc.dg/i386-cmov5.c:  New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@62002 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/i386-cmov5.c
@@ -0,0 +1,9 @@
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -march=athlon" } */
+/* { dg-final { scan-assembler "sbb" } } */
+
+int
+t(float a, float b)
+{
+	return a<=b?0:-1;
+}
