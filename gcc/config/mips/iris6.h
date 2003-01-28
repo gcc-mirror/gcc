8b3c2cf1419c16dd687faef2d0978ac2fb3fe385
Alexandre Oliva <aoliva@redhat.com>
2003-01-28 02:18:38 +0000

    mips.h (UNITS_PER_HWFPVALUE): Renamed from...
    * config/mips/mips.h (UNITS_PER_HWFPVALUE): Renamed from...
    (UNITS_PER_FPVALUE): Defined as the width of a long double, or
    zero if no hardware floating point.
    (LONG_DUBLE_TYPE_SIZE): Set to 128 on N32 and N64.
    (MAX_FIXED_MODE_SIZE): Define to LONG_DOUBLE_TYPE_SIZE.
    (LIBGCC2_LONG_DOUBLE_TYPE_SIZE): Define.
    (BIGGEST_ALIGNMENT): Same as LONG_DOUBLE_TYPE_SIZE.
    (FUNCTION_VALUE_REGNO_P): Set for FP_RETURN+2 on N32 and N64.
    * config/mips/iris6.h (MIPS_TFMODE_FORMAT): Define.
    * config/mips/mips.c (override_options): Use it.
    (mips_arg_info): Pass TFmode values in even FP registers on N32
    and N64.
    (mips_setup_incoming_varargs): Use UNITS_PER_HWFPVALUE.
    (mips_va_start): Adjust alignment of ARG_POINTER_REGNUM.
    (mips_va_arg): Use UNITS_PER_HWFPVALUE.  Impose additional
    even-register-like alignment to 128-bit arguments.
    (save_restore_insns): Use UNITS_PER_HWFPVALUE.
    (mips_function_value): Likewise.  Return TFmode in $f0 and $f2
    on N32 or N64.
    * config/mips/_tilib.c (__negti2, __ashlti3, __lshrti3): New.
    * config/mips/t-iris6 (LIB2FUNCS_EXTRA): Add _tilib.c.
    (TPBIT): Set to tp-bit.c.
    (tp-bit.c): Create out of fp-bit.c.
    
    From-SVN: r61944

--- gcc/config/mips/iris6.h
+++ gcc/config/mips/iris6.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler.  Iris version 6.
-   Copyright (C) 1994, 1995, 1996, 1997, 1998, 2000, 2001, 2002
+   Copyright (C) 1994, 1995, 1996, 1997, 1998, 2000, 2001, 2002, 2003
    Free Software Foundation, Inc.
 
 This file is part of GNU CC.
@@ -509,3 +509,5 @@ do {									 \
 %{shared:-hidden_symbol __do_global_ctors,__do_global_ctors_1,__do_global_dtors} \
 -_SYSTYPE_SVR4 -woff 131 \
 %{mabi=32: -32}%{mabi=n32: -n32}%{mabi=64: -64}%{!mabi*: -n32}"
+
+#define MIPS_TFMODE_FORMAT ibm_extended_format
