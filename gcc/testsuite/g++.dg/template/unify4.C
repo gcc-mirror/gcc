b2190c1addde6b01bd8abe56c43efdaab8836426
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-01-29 11:35:33 +0000

    cp: 	PR c++/9437 	* pt.c (unify): Don't unify '*T' with 'U C::*'. testsuite: 	PR c++/9437 	* g++.dg/template/unify4.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@62070 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/unify4.C
@@ -0,0 +1,18 @@
+// { dg-do compile }
+
+// Copyright (C) 2003 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 28 Jan 2003 <nathan@codesourcery.com>
+
+// PR 9437. We'd unify 'T *' with 'U C::*', which is obviously broken
+
+struct X
+{
+  template <typename T>
+  operator T* () const { return static_cast<T*> (0); }
+} null;
+
+struct A { int i; };
+
+static void f (int A::* pmi) { }
+
+int main () { f (null); } // { dg-error "cannot convert" "" }
