d2b7331b5c3ba058c80f2025a19664be827b5686
Joel Sherrill <joel@OARcorp.com>
2003-01-29 15:52:11 +0000

    re PR bootstrap/9292 (hppa1.1-rtems configurery problems)
    2003-01-29	Joel Sherrill <joel@OARcorp.com>
    
    	* config.gcc (hppa1.1-rtems):  Did not include t-rtems nor enable
    	RTEMS threads.
    	* config/pa/rtems.h (LIB_SPEC): Use -N when linking.
    	This issue was tracked as PR9292.
    
    From-SVN: r62085

--- gcc/config/pa/rtems.h
+++ gcc/config/pa/rtems.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler, for PRO.
-   Copyright (C) 1997, 2000, 2002 Free Software Foundation, Inc.
+   Copyright (C) 1997, 2000, 2002, 2003 Free Software Foundation, Inc.
    Contributed by Joel Sherrill (joel@OARcorp.com).
 
 This file is part of GNU CC.
@@ -24,3 +24,6 @@ Boston, MA 02111-1307, USA.  */
 #undef CPP_PREDEFINES
 #define CPP_PREDEFINES "-Dhppa -DPWB -Acpu=hppa -Amachine=hppa \
    -D__rtems__ -Asystem=rtems"
+
+#undef LIB_SPEC
+#define LIB_SPEC "%{!p:%{!pg:-lc}}%{p: -lc}%{pg: -lc} -N"
