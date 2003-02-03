4360fe0caf10a555ff7681be2e311ffada570e9b
Volker Reichelt <reichelt@igpm.rwth-aachen.de>
2003-02-03 10:27:08 +0000

    re PR libstdc++/9234 (unary minus for valarrays broken)
    	PR libstdc++/9234
    	* include/bits/valarray_meta.h (_UnBase::operator[]): Apply unary
    	operator.
    
    	* include/bits/valarray_meta.h (__not_equal_to): Use != instead of ==.
    
    	* testsuite/26_numerics/valarray_operators.cc: New test.
    
    From-SVN: r62308

--- libstdc++-v3/include/bits/valarray_meta.h
+++ libstdc++-v3/include/bits/valarray_meta.h
@@ -268,7 +268,7 @@ namespace std
   {
     template<typename _Tp>
       bool operator()(const _Tp& __x, const _Tp& __y) const
-      { return __x == __y; }
+      { return __x != __y; }
   };
 
   struct __less
@@ -457,7 +457,7 @@ namespace std
       _UnBase(const _Arg& __e) : _M_expr(__e) {}
 
       value_type operator[](size_t __i) const
-      { return _M_expr[__i]; }
+      { return _Oper()(_M_expr[__i]); }
 
       size_t size() const { return _M_expr.size(); }
 
