63ea2265f9c6caf89dcfdc520d03be84198c3c36
Jan Hubicka <jh@suse.cz>
2003-02-04 22:04:58 +0100

    i386-ssetype-?.c: Compile using -march=k8; fix for register passing convetions.
    	* gcc.dg/i386-ssetype-?.c:  Compile using -march=k8; fix for register
    	passing convetions.
    
    From-SVN: r62400

--- gcc/testsuite/gcc.dg/i386-ssetype-1.c
+++ gcc/testsuite/gcc.dg/i386-ssetype-1.c
@@ -1,32 +1,35 @@
 /* { dg-do compile { target i?86-*-* x86_64-*-* } } */
-/* { dg-options "-O2 -msse2 -march=athlon" } */
-/* { dg-final { scan-assembler "andpd.*\[bs\]p" } } */
-/* { dg-final { scan-assembler "andnpd.*\[bs\]p" } } */
-/* { dg-final { scan-assembler "xorpd.*\[bs\]p" } } */
-/* { dg-final { scan-assembler "orpd.*\[bs\]p" } } */
+/* { dg-options "-O2 -msse2 -march=k8" } */
+/* { dg-final { scan-assembler "andpd.*magic" } } */
+/* { dg-final { scan-assembler "andnpd.*magic" } } */
+/* { dg-final { scan-assembler "xorpd.*magic" } } */
+/* { dg-final { scan-assembler "orpd.*magic" } } */
 /* { dg-final { scan-assembler-not "movdqa" } } */
-/* { dg-final { scan-assembler "movapd.*\[bs\]p" } } */
+/* { dg-final { scan-assembler "movapd.*magic" } } */
 
 /* Verify that we generate proper instruction with memory operand.  */
 
 #include <xmmintrin.h>
+
+__m128d magic_a, magic_b;
+
 __m128d
-t1(__m128d a, __m128d b)
+t1(void)
 {
-return _mm_and_pd (a,b);
+return _mm_and_pd (magic_a,magic_b);
 }
 __m128d
-t2(__m128d a, __m128d b)
+t2(void)
 {
-return _mm_andnot_pd (a,b);
+return _mm_andnot_pd (magic_a,magic_b);
 }
 __m128d
-t3(__m128d a, __m128d b)
+t3(void)
 {
-return _mm_or_pd (a,b);
+return _mm_or_pd (magic_a,magic_b);
 }
 __m128d
-t4(__m128d a, __m128d b)
+t4(void)
 {
-return _mm_xor_pd (a,b);
+return _mm_xor_pd (magic_a,magic_b);
 }
--- gcc/testsuite/gcc.dg/i386-ssetype-2.c
+++ gcc/testsuite/gcc.dg/i386-ssetype-2.c
@@ -1,11 +1,10 @@
 /* { dg-do compile { target i?86-*-* x86_64-*-* } } */
-/* { dg-options "-O2 -msse2 -march=athlon" } */
+/* { dg-options "-O2 -msse2 -march=k8" } */
 /* { dg-final { scan-assembler "andpd" } } */
 /* { dg-final { scan-assembler "andnpd" } } */
 /* { dg-final { scan-assembler "xorpd" } } */
 /* { dg-final { scan-assembler "orpd" } } */
 /* { dg-final { scan-assembler-not "movdqa" } } */
-/* { dg-final { scan-assembler "movapd" } } */
 
 /* Verify that we generate proper instruction without memory operand.  */
 
