4f6ae35d12e716c1b596cbe979066730be91fc58
Jakub Jelinek <jakub@redhat.com>
2003-02-05 12:29:15 +0100

    re PR rtl-optimization/8555 (ICE in gen_split_1231)
    	PR optimization/8555
    	* config/i386/i386.md (sse_mov?fcc split): Handle op2 == op3 case
    	instead of aborting.
    
    	* gcc.dg/20030204-1.c: New test.
    
    From-SVN: r62438

---
+++ gcc/testsuite/gcc.dg/20030204-1.c
@@ -0,0 +1,16 @@
+/* PR optimization/8555 */
+/* { dg-do compile } */
+/* { dg-options "-O -ffast-math -funroll-loops" } */
+/* { dg-options "-march=pentium3 -O -ffast-math -funroll-loops" { target i?86-*-* } } */
+
+float foo (float *a, int i)
+{
+  int j;
+  float x = a[j = i - 1], y;
+
+  for (j = i; --j >= 0; )
+    if ((y = a[j]) > x)
+      x = y;
+
+  return x;
+}
