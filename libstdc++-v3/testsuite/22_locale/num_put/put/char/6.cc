5dc911525c4b8d4366eb7f9270b8af1fb1dedb66
Paolo Carlini <pcarlini@unitus.it>
2003-02-06 19:08:51 +0100

    re PR libstdc++/9548 (Incorrect results from setf(ios::fixed) and precision(-1) [DR231])
    2003-02-06  Paolo Carlini  <pcarlini@unitus.it>
    
    	PR libstdc++/9548
    	Implement resolution of DR 231 (Ready)
    	* include/bits/locale_facets.h (__num_base::_S_format_float):
    	Change declaration: return void, remove __prec parameter.
    	* src/locale.cc (__num_base::_S_format_float): Implement
    	resolution of DR 231.
    	* include/bits/locale_facets.tcc (num_put::_M_convert_float):
    	Tweak uses. Check for negative precision.
    	* testsuite/22_locale/num_put/put/char/6.cc: Add
    	* testsuite/22_locale/num_put/put/wchar_t/6.cc: Likewise.
    
    From-SVN: r62492

---
+++ libstdc++-v3/testsuite/22_locale/num_put/put/char/6.cc
@@ -0,0 +1,56 @@
+// 2003-02-05 Paolo Carlini <pcarlini@unitus.it>
+
+// Copyright (C) 2003 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 22.2.2.2.1  num_put members
+
+#include <locale>
+#include <sstream>
+#include <testsuite_hooks.h>
+
+// libstdc++/9548 and DR 231
+void test01()
+{
+  using namespace std;
+  bool test = true;
+
+  ostringstream oss1, oss2;
+  const num_put<char>& np1 = use_facet<num_put<char> >(oss1.getloc());
+  const num_put<char>& np2 = use_facet<num_put<char> >(oss2.getloc());
+
+  string result1, result2;
+
+  oss1.precision(-1);
+  oss1.setf(ios_base::fixed, ios_base::floatfield);
+  np1.put(oss1.rdbuf(), oss1, '+', 30.5);
+  result1 = oss1.str();
+  VERIFY( result1 == "30.500000" );
+
+  oss2.precision(0);
+  oss2.setf(ios_base::scientific, ios_base::floatfield);
+  np2.put(oss2.rdbuf(), oss2, '+', 1.0);
+  result2 = oss2.str();
+  VERIFY( result2 == "1e+00" );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/22_locale/num_put/put/wchar_t/6.cc
@@ -0,0 +1,56 @@
+// 2003-02-05 Paolo Carlini <pcarlini@unitus.it>
+
+// Copyright (C) 2003 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 22.2.2.2.1  num_put members
+
+#include <locale>
+#include <sstream>
+#include <testsuite_hooks.h>
+
+// libstdc++/9548 and DR 231
+void test01()
+{
+  using namespace std;
+  bool test = true;
+
+  wostringstream woss1, woss2;
+  const num_put<wchar_t>& np1 = use_facet<num_put<wchar_t> >(woss1.getloc());
+  const num_put<wchar_t>& np2 = use_facet<num_put<wchar_t> >(woss2.getloc());
+
+  wstring result1, result2;
+
+  woss1.precision(-1);
+  woss1.setf(ios_base::fixed, ios_base::floatfield);
+  np1.put(woss1.rdbuf(), woss1, '+', 30.5);
+  result1 = woss1.str();
+  VERIFY( result1 == L"30.500000" );
+
+  woss2.precision(0);
+  woss2.setf(ios_base::scientific, ios_base::floatfield);
+  np2.put(woss2.rdbuf(), woss2, '+', 1.0);
+  result2 = woss2.str();
+  VERIFY( result2 == L"1e+00" );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
