8aa063fb462948401678a3b86e8c1512d57e15ba
Kazu Hirata <kazu@cs.umass.edu>
2003-02-08 18:27:12 +0000

    clzsi2.c: Replace "GNU CC" with "GCC".
    	* config/h8300/clzsi2.c: Replace "GNU CC" with "GCC".
    	* config/h8300/crti.asm: Likewise.
    	* config/h8300/crtn.asm: Likewise.
    	* config/h8300/ctzsi2.c: Likewise.
    	* config/h8300/fixunssfsi.c: Likewise.
    	* config/h8300/h8300-protos.h: Likewise.
    	* config/h8300/h8300.c: Likewise.
    	* config/h8300/h8300.h: Likewise.
    	* config/h8300/h8300.md: Likewise.
    	* config/h8300/paritysi2.c: Likewise.
    	* config/h8300/popcountsi2.c: Likewise.
    	* config/h8300/rtems.h: Likewise.
    
    From-SVN: r62581

--- gcc/config/h8300/h8300.h
+++ gcc/config/h8300/h8300.h
@@ -5,20 +5,20 @@
    Contributed by Steve Chamberlain (sac@cygnus.com),
    Jim Wilson (wilson@cygnus.com), and Doug Evans (dje@cygnus.com).
 
-This file is part of GNU CC.
+This file is part of GCC.
 
-GNU CC is free software; you can redistribute it and/or modify
+GCC is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2, or (at your option)
 any later version.
 
-GNU CC is distributed in the hope that it will be useful,
+GCC is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with GNU CC; see the file COPYING.  If not, write to
+along with GCC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
@@ -1236,7 +1236,7 @@ struct cum_arg
 #define FINAL_PRESCAN_INSN(insn, operand, nop)	\
   final_prescan_insn (insn, operand, nop)
 
-/* Define this macro if GNU CC should generate calls to the System V
+/* Define this macro if GCC should generate calls to the System V
    (and ANSI C) library functions `memcpy' and `memset' rather than
    the BSD functions `bcopy' and `bzero'.  */
 
