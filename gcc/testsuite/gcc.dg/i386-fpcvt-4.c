637df4f4ba4066794c61f17133d820400fa34c8a
Jan Hubicka <jh@suse.cz>
2003-02-10 13:12:56 +0100

    * gcc.dg/i386-fpcvt-1.c:  New test.
    From-SVN: r62627

---
+++ gcc/testsuite/gcc.dg/i386-fpcvt-4.c
@@ -0,0 +1,10 @@
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -march=k8 -mfpmath=sse" } */
+/* { dg-final { scan-assembler "cvtsi2sd" } } */
+/* Check that conversions will get folded.  */
+double
+t(short a)
+{
+  float b=a;
+  return b;
+}
