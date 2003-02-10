68060abb2c38caa677007b7aaba287c058c73c76
Eric Botcazou <ebotcazou@libertysurf.fr>
2003-02-10 14:01:50 +0100

    re PR c/7741 (ICE on conflicting types (make_decl_rtl at varasm.c:834))
    	PR c/7741
    	* c-decl.c (duplicate_decls): Discard the initializer of the
    	new decl when the types are conflicting.
    
    Co-Authored-By: Christian Ehrhardt <ehrhardt@mathematik.uni-ulm.de>
    
    From-SVN: r62636

--- gcc/c-decl.c
+++ gcc/c-decl.c
@@ -1665,6 +1665,12 @@ duplicate_decls (newdecl, olddecl, different_binding_level)
 	    }
 	}
       error_with_decl (olddecl, "previous declaration of `%s'");
+
+      /* This is safer because the initializer might contain references
+	 to variables that were declared between olddecl and newdecl. This
+	 will make the initializer invalid for olddecl in case it gets
+	 assigned to olddecl below.  */
+      DECL_INITIAL (newdecl) = 0;
     }
   else
     {
