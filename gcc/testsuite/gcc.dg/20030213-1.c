f44b98103568b1b96412381bcdf56f1b43f60282
No Author <no-author@gcc.gnu.org>
2003-02-13 23:05:34 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_3-branch'.
    From-SVN: r62870

---
+++ gcc/testsuite/gcc.dg/20030213-1.c
@@ -0,0 +1,36 @@
+/* Testcase for http://gcc.gnu.org/ml/gcc-patches/2003-02/msg01017.html */
+/* { dg-do link { target *-*-* } } */
+/* { dg-options "-O -fpic" } */
+
+int *g;
+
+int main (void)
+{
+  switch (*g)
+    {
+    case 0:
+      {
+        switch (*g)
+          {
+          case 0: *g = 1; break;
+          case 1:
+          case 2: *g = 1; break;
+          case 3:
+          case 4: *g = 1; break;
+          }
+        break;
+      }
+    case 1:
+      {
+        switch (*g)
+          {
+          case 0: *g = 1; break;
+          case 1:
+          case 2: *g = 1; break;
+          case 3:
+          case 4: *g = 1; break;
+          }
+      }
+    }
+  return 0;
+}
