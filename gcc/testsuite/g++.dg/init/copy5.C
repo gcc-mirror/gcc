07fd0523d2117f7a35d1c7cc4d3b1eccfffd4cfc
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-02-18 19:17:28 +0000

    PR c++/9704 	* class.c (layout_class_type): In the 3.2 ABI, take into account 	trailing bit fields when computing CLASSTYPE_SIZE_UNIT.
    	PR c++/9704
    	* g++.dg/init/copy5.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@63054 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/init/copy5.C
@@ -0,0 +1,29 @@
+// { dg-options "-O2" }
+
+struct BOOL {
+    int nVal:1, bSet:1;
+    BOOL (int i) : nVal(i!=0), bSet(1) {}
+};
+struct Fill {
+    void *d;
+    Fill() : d(0) {}
+    Fill( const Fill& ) {}
+};
+struct SvMetaSlot {
+    Fill aGroupId;
+    BOOL a8;
+    SvMetaSlot() :
+      a8(1) {}
+    SvMetaSlot* MakeClone() const;
+};
+
+SvMetaSlot* SvMetaSlot::MakeClone() const { return new SvMetaSlot( *this ); }
+
+extern "C" void abort(void);
+int main()
+{
+  SvMetaSlot s; SvMetaSlot s2(s);
+  if (s.a8.bSet != s2.a8.bSet)
+    abort ();
+  return 0;
+}
