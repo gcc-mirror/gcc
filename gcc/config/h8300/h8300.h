590734b6544d296f3d4cf74713f7a1e723d03fbe
Kazu Hirata <kazu@cs.umass.edu>
2003-02-19 12:53:32 +0000

    h8300-protos.h: Update the prototype for split_adds_subs.
    	* config/h8300/h8300-protos.h: Update the prototype for
    	split_adds_subs.  Remove the prototypes for
    	const_int_le_2_operand and const_int_le_6_operand.
    	* config/h8300/h8300.c (split_adds_sub): Don't output inc/dec.
    	(const_int_le_2_operand): Remove.
    	(const_int_le_6_operand): Likewise.
    	* config/h8300/h8300.h (PREDICATE_CODES): Remove the entries
    	for const_int_le_2_operand and const_int_le_6_operand.
    	* config/h8300/h8300.md: Update all uses of split_adds_subs.
    	(a peephole2): New.
    
    From-SVN: r63103

--- gcc/config/h8300/h8300.h
+++ gcc/config/h8300/h8300.h
@@ -1284,8 +1284,6 @@ struct cum_arg
   {"bit_operand", {REG, SUBREG, MEM}},					\
   {"bit_memory_operand", {MEM}},					\
   {"stack_pointer_operand", {REG}},					\
-  {"const_int_le_2_operand", {CONST_INT}},				\
-  {"const_int_le_6_operand", {CONST_INT}},				\
   {"const_int_gt_2_operand", {CONST_INT}},				\
   {"const_int_ge_8_operand", {CONST_INT}},				\
   {"const_int_qi_operand", {CONST_INT}},				\
