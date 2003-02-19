5b145e0c42451e7245048bc1228227f64c7b5224
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-02-19 04:36:44 +0000

    * config/h8300/h8300.c (general_operand_src): Always check 	MODE. 	(general_operand_dst): Likewise. 	* gcc.c-torture/execute/20030218-1.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@63091 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/h8300/h8300.c
+++ gcc/config/h8300/h8300.c
@@ -723,7 +723,9 @@ general_operand_src (op, mode)
      rtx op;
      enum machine_mode mode;
 {
-  if (GET_CODE (op) == MEM && GET_CODE (XEXP (op, 0)) == POST_INC)
+  if (GET_MODE (op) == mode
+      && GET_CODE (op) == MEM
+      && GET_CODE (XEXP (op, 0)) == POST_INC)
     return 1;
   return general_operand (op, mode);
 }
@@ -736,7 +738,9 @@ general_operand_dst (op, mode)
      rtx op;
      enum machine_mode mode;
 {
-  if (GET_CODE (op) == MEM && GET_CODE (XEXP (op, 0)) == PRE_DEC)
+  if (GET_MODE (op) == mode
+      && GET_CODE (op) == MEM
+      && GET_CODE (XEXP (op, 0)) == PRE_DEC)
     return 1;
   return general_operand (op, mode);
 }
