64c7e556ab3dd5af040d393383a557696522ecaa
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2003-02-21 20:15:39 +0000

    configure.in: Check for sys/sysmp.h and sysmp.
    	* configure.in: Check for sys/sysmp.h and sysmp.
    	* physmem.c: Pull upstream copy, add support for irix6.
    
    	* config.in, configure: Regenerated.
    
    From-SVN: r63237

--- libiberty/physmem.c
+++ libiberty/physmem.c
@@ -1,5 +1,5 @@
 /* Calculate the size of physical memory.
-   Copyright 2000, 2001 Free Software Foundation, Inc.
+   Copyright 2000, 2001, 2003 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-/* Written by Paul Eggert.  */
+/* Written by Paul Eggert and Jim Meyering.  */
 
 #if HAVE_CONFIG_H
 # include <config.h>
@@ -28,6 +28,12 @@
 #if HAVE_SYS_PSTAT_H
 # include <sys/pstat.h>
 #endif
+
+#if HAVE_SYS_SYSMP_H
+#include <sys/sysmp.h>
+#endif
+
+#include "libiberty.h"
 
 /* Return the total amount of physical memory.  */
 double
@@ -43,7 +49,7 @@ physmem_total ()
 #endif
 
 #if HAVE_PSTAT_GETSTATIC
-  {
+  { /* This works on hpux11.  */
     struct pst_static pss;
     if (0 <= pstat_getstatic (&pss, sizeof pss, 1, 0))
       {
@@ -51,6 +57,19 @@ physmem_total ()
 	double pagesize = pss.page_size;
 	if (0 <= pages && 0 <= pagesize)
 	  return pages * pagesize;
+      }
+  }
+#endif
+
+#if HAVE_SYSMP && defined MP_SAGET && defined MPSA_RMINFO && defined _SC_PAGESIZE
+  { /* This works on irix6. */
+    struct rminfo realmem;
+    if (sysmp(MP_SAGET, MPSA_RMINFO, &realmem, sizeof(realmem)) == 0)
+      {
+	double pagesize = sysconf (_SC_PAGESIZE);
+	double pages = realmem.physmem;
+	if (0 <= pages && 0 <= pagesize)
+          return pages * pagesize;
       }
   }
 #endif
@@ -73,7 +92,7 @@ physmem_available ()
 #endif
 
 #if HAVE_PSTAT_GETSTATIC && HAVE_PSTAT_GETDYNAMIC
-  {
+  { /* This works on hpux11.  */
     struct pst_static pss;
     struct pst_dynamic psd;
     if (0 <= pstat_getstatic (&pss, sizeof pss, 1, 0)
@@ -87,6 +106,40 @@ physmem_available ()
   }
 #endif
 
+#if HAVE_SYSMP && defined MP_SAGET && defined MPSA_RMINFO && defined _SC_PAGESIZE
+  { /* This works on irix6. */
+    struct rminfo realmem;
+    if (sysmp(MP_SAGET, MPSA_RMINFO, &realmem, sizeof(realmem)) == 0)
+      {
+	double pagesize = sysconf (_SC_PAGESIZE);
+	double pages = realmem.availrmem;
+	if (0 <= pages && 0 <= pagesize)
+          return pages * pagesize;
+      }
+  }
+#endif
+
   /* Guess 25% of physical memory.  */
   return physmem_total () / 4;
 }
+
+
+#if DEBUG
+
+# include <stdio.h>
+# include <stdlib.h>
+
+int
+main ()
+{
+  printf ("%12.f %12.f\n", physmem_total (), physmem_available ());
+  exit (0);
+}
+
+#endif /* DEBUG */
+
+/*
+Local Variables:
+compile-command: "gcc -DDEBUG -DHAVE_CONFIG_H -I.. -g -O -Wall -W physmem.c"
+End:
+*/
