48e082b90065ab7597361797711b116c180b491b
Jan Hubicka <jh@suse.cz>
2003-02-22 20:14:59 +0100

    * gcc.dg/i386-mul.c: New test.
    From-SVN: r63297

---
+++ gcc/testsuite/gcc.dg/i386-mul.c
@@ -0,0 +1,10 @@
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -march=k8" } */
+/* { dg-final { scan-assembler "and.*magic" } } */
+
+/* Should be done as "andw $32767, magic".  */
+unsigned short magic;
+t()
+{
+	magic%=(unsigned short)0x8000U;
+}
