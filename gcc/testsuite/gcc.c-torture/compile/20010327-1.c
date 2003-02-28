e459243b87904cd66d4bb85892eb831d9e006c2c
Alexandre Oliva <aoliva@redhat.com>
2003-02-28 09:38:40 +0000

    20010327-1.c: Use __SIZE_TYPE__ instead of unsigned long.
    * gcc.c-torture/compile/20010327-1.c: Use __SIZE_TYPE__ instead of
    unsigned long.
    
    From-SVN: r63551

--- gcc/testsuite/gcc.c-torture/compile/20010327-1.c
+++ gcc/testsuite/gcc.c-torture/compile/20010327-1.c
@@ -1,2 +1,6 @@
 extern void _text;
-static unsigned long x = (unsigned long) &_text - 0x10000000L - 1;
+/* We use __SIZE_TYPE__ here because it's as wide as a pointer, so we
+   know we won't have a non-constant because of extension or
+   truncation of the pointer to fit.  */
+static __SIZE_TYPE__ x
+  = (__SIZE_TYPE__) &_text - (__SIZE_TYPE__) 0x10000000L - 1;
