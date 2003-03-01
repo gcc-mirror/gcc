05e643de59279f10908cafad1db5c0d6a1ef1f4c
Richard Earnshaw <rearnsha@gcc.gnu.org>
2003-03-01 12:54:56 +0000

    Update copyright.
    From-SVN: r63608

--- gcc/predict.c
+++ gcc/predict.c
@@ -1,5 +1,5 @@
 /* Branch prediction routines for the GNU compiler.
-   Copyright (C) 2000, 2001, 2002 Free Software Foundation, Inc.
+   Copyright (C) 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
