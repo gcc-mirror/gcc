01952d37e3c7bfeacfb3186ddc8ab7cb59c65c3e
Alexandre Oliva <aoliva@redhat.com>
2003-03-04 05:55:20 +0000

    * gcc.c-torture/execute/20030222-1.c: New test.
    From-SVN: r63767

---
+++ gcc/testsuite/gcc.c-torture/execute/20030222-1.c
@@ -0,0 +1,27 @@
+/* Verify that we get the low part of the long long as an int.  We
+   used to get it wrong on big-endian machines, if register allocation
+   succeeded at all.  We use volatile to make sure the long long is
+   actually truncated to int, in case a single register is wide enough
+   for a long long.  */
+
+#include <limits.h>
+
+void
+ll_to_int (long long x, volatile int *p)
+{
+  int i;
+  asm ("" : "=r" (i) : "0" (x));
+  *p = i;
+}
+
+int val = INT_MIN + 1;
+
+int main() {
+  volatile int i;
+
+  ll_to_int ((long long)val, &i);
+  if (i != val)
+    abort ();
+  
+  exit (0);
+}
