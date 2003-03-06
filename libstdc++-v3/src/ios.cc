a7749d8767d13ac06a6df868ba6753f0953e4c11
jlquinn <jlquinn@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-03-06 18:10:26 +0000

    2003-03-06  Jerry Quinn  <jlquinn@optonline.net>
    	* src/ios.cc (ios_base::_M_init): Remove _M_word_size.
    	(ios_base::ios_base): Set _M_word, _M_word_size.
    	(ios_base::~ios_base): Remove redundant test.
    	* testsuite/27_io/ios_base_storage.cc (test03): New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@63896 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/src/ios.cc
+++ libstdc++-v3/src/ios.cc
@@ -288,7 +288,6 @@ namespace std
     _M_width = 0;
     _M_flags = skipws | dec;
     _M_callbacks = 0;
-    _M_word_size = 0;
     _M_ios_locale = locale();
   }  
   
@@ -302,7 +301,8 @@ namespace std
     return __old;
   }
 
-  ios_base::ios_base() : _M_callbacks(0), _M_word(0), _M_locale_cache(0)
+  ios_base::ios_base() : _M_callbacks(0), _M_word(_M_local_word),
+			 _M_word_size(_S_local_word_size), _M_locale_cache(0)
   {
     // Do nothing: basic_ios::init() does it.  
     // NB: _M_callbacks and _M_word must be zero for non-initialized
@@ -314,7 +314,7 @@ namespace std
   {
     _M_call_callbacks(erase_event);
     _M_dispose_callbacks();
-    if (_M_word && _M_word != _M_local_word) 
+    if (_M_word != _M_local_word) 
       {
 	delete [] _M_word;
 	_M_word = 0;
