16099427134a9724c4a818f6ea4beac140580108
sirl <sirl@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-03-10 20:42:23 +0000

    2003-03-10  Franz Sirl  <Franz.Sirl-kernel@lauterbach.com>
    	* gcc.dg/ppc-sdata-1.c: New test.
    	* gcc.dg/ppc-sdata-2.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@64119 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/ppc-sdata-2.c
@@ -0,0 +1,15 @@
+/* { dg-do compile { target powerpc-*-linux* powerpc-*-sysv* powerpc-*-eabi* } } */
+/* { dg-options "-O2 -fno-common -G 8 -msdata=sysv" } */
+/* { dg-final { scan-assembler "\\.section\[ \t\]\\.sdata," } } */
+/* { dg-final { scan-assembler-not "\\.section\[ \t\]\\.sdata2," } } */
+/* { dg-final { scan-assembler "sdat@sdarel\\(13\\)" } } */
+/* { dg-final { scan-assembler "sdat2@sdarel\\(13\\)" } } */
+
+
+int sdat = 2;
+const char sdat2[] = "1234";
+
+const char * test (void)
+{
+  return sdat ? sdat2 : 0;
+}
