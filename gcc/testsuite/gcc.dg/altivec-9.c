ebb780f29653177f3913296b6965058dc7b420bb
Segher Boessenkool <segher@koffie.nl>
2003-03-10 21:52:31 +0100

    altivec-9.c: New file.
    2003-03-10  Segher Boessenkool  <segher@koffie.nl>
    
            * testsuite/gcc.dg/altivec-9.c: New file.
    
    From-SVN: r64120

---
+++ gcc/testsuite/gcc.dg/altivec-9.c
@@ -0,0 +1,13 @@
+/* { dg-do compile { target powerpc-*-* } } */
+/* { dg-options "-maltivec -mabi=altivec -g" } */
+
+/* PR9564 */
+
+extern int vfork(void);
+
+void
+boom (void)
+{
+  char buf[65536];
+  vfork();
+}
