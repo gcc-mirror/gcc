bb74afdd74731efb02d95665d9ece9e8cf66c86e
steven <steven@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-03-11 20:00:39 +0000

    2003-03-11  Steven Bosscher  <s.bosscher@student.tudelft.nl>
            * gcc.dg/return-type-3.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@64181 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/return-type-3.c
@@ -0,0 +1,14 @@
+/* PR optimization/7189
+   This was a missing warning caused by a cfg cleanup after sibling
+   call optimization.  The return clobber insn was cleaned up and
+   the warning was never issued.  */
+/* { dg-do compile } */
+/* { dg-options "-foptimize-sibling-calls -Wreturn-type" } */
+
+extern void foo(void);
+
+int
+bar (void)
+{
+  foo();
+} /* { dg-warning "control reaches end of non-void function" "warning for falling off end of non-void function" } */
