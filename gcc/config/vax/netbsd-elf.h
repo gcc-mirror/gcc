672345546c8269a4e338d3b9b425790cf9c25466
neroden <neroden@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-03-13 03:23:55 +0000

    * config/vax/bsd.h, config/vax/elf.h, config/vax/netbsd-elf.h, 	config/vax/netbsd.h, config/vax/openbsd.h, config/vax/openbsd1.h, 	config/vax/ultrix.h, config/vax/vax-protos.h, config/vax/vax.c, 	config/vax/vax.h, config/vax/vax.md, config/vax/vaxv.h: GCC, not 	GNU CC.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@64294 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/vax/netbsd-elf.h
+++ gcc/config/vax/netbsd-elf.h
@@ -2,20 +2,20 @@
    for NetBSD/vax ELF systems.
    Copyright (C) 2002 Free Software Foundation, Inc.
 
-This file is part of GNU CC.
+This file is part of GCC.
 
-GNU CC is free software; you can redistribute it and/or modify
+GCC is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2, or (at your option)
 any later version.
 
-GNU CC is distributed in the hope that it will be useful,
+GCC is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with GNU CC; see the file COPYING.  If not, write to
+along with GCC; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 
