301ee2f3e5b728793f5d9a9859080380ce0bebef
Nathanael Nerode <neroden@gcc.gnu.org>
2003-03-13 03:30:00 +0000

    lib1funcs.asm, [...]: GCC, not GNU CC.
    	* config/v850/lib1funcs.asm, config/v850/rtems.h,
    	config/v850/v850-c.c, config/v850/v850-protos.h,
    	config/v850/v850.c, config/v850/v850.h, config/v850/v850.md:
    	GCC, not GNU CC.
    
    From-SVN: r64296

--- gcc/config/v850/lib1funcs.asm
+++ gcc/config/v850/lib1funcs.asm
@@ -1,9 +1,9 @@
 /* libgcc routines for NEC V850.
    Copyright (C) 1996, 1997, 2002 Free Software Foundation, Inc.
 
-This file is part of GNU CC.
+This file is part of GCC.
 
-GNU CC is free software; you can redistribute it and/or modify it
+GCC is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the
 Free Software Foundation; either version 2, or (at your option) any
 later version.
