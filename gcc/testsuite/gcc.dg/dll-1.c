0d1fc2442ff502fc9a98a49062431904137a811e
dannysmith <dannysmith@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-03-13 22:21:21 +0000

    * gcc.dg/dll-1.c: Remove thumb target. Change exp to _exp. 	* gcc.dg/dll-2.c: Enable for cygwin and mingw. Remove 	thumb target, 	* gcc.dg/dll-3.c: Likewise. Adjust scan-assembler 	to accept newer _imp__	prefix and additional 	newline in .drectve section. 	* gcc.dg/dll-4.c: Likewise. 	* gcc.dg/dll-5.c: New file to test -mnop-fun-dllimport 	switch.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@64337 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/dll-1.c
+++ gcc/testsuite/gcc.dg/dll-1.c
@@ -1,10 +1,10 @@
 /* { dg-do compile { target arm*-*-pe* } } */
-/* { dg-do compile { target thumb*-*-pe* } } */
 /* { dg-options -mno-nop-fun-dllimport } */
 
 __declspec (dllimport) void imp ();
 
-__declspec (dllexport) void exp () { imp (); }
+__declspec (dllexport) void _exp () { imp (); }
 
-/* { dg-final { scan-assembler "\.section\[ \t\]*.drectve\n\[^\n\]*-export:exp.*__imp_imp" } } */
-/* { dg-final { scan-assembler-not "__imp_exp" } } */
+/* { dg-final { scan-assembler "\.section\[ \t\]*.drectve\n\[^\n\]*-export:_exp.*__imp_imp" } } */
+/* { dg-final { scan-assembler-not "__imp__exp" } } */
+
--- gcc/testsuite/gcc.dg/dll-4.c
+++ gcc/testsuite/gcc.dg/dll-4.c
@@ -1,5 +1,6 @@
 /* { dg-do compile { target arm*-*-pe* } } */
-/* { dg-do compile { target thumb*-*-pe* } } */
+/* { dg-do compile { target i?86-pc-cygwin } } */
+/* { dg-do compile { target i?86-pc-mingw* } } */
 
 __declspec (dllimport) int foo1;
 int foo1;
@@ -11,4 +12,4 @@ int f () { return foo1 + foo2; }
 
 /* FIXME: We should scan the output of nm for this case.  */
 /* { dg-final { scan-assembler "(foo2:.*\.comm\[ \t_\]*foo1)" } } */
-/* { dg-final { scan-assembler-not "__imp_" } } */
+/* { dg-final { scan-assembler-not "(__imp_|_imp__)" } } */
