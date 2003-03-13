73f45a5caa438cce1f2fccbfd5195b35e73b5760
neroden <neroden@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-03-13 18:26:30 +0000

    * config/ia64/fde-glibc.c, config/ia64/freebsd.h, 	config/ia64/hpux.h, config/ia64/hpux_longdouble.h, 	config/ia64/ia64-c.c, config/ia64/ia64-modes.def, 	config/ia64/ia64-protos.h, config/ia64/ia64.c, 	config/ia64/ia64.h, config/ia64/ia64.md, 	config/ia64/itanium1.md, config/ia64/itanium2.md, 	config/ia64/quadlib.c, config/ia64/unwind-ia64.c, 	config/ia64/unwind-ia64.h: It's GCC, not GNU CC.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@64313 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/ia64/quadlib.c
+++ gcc/config/ia64/quadlib.c
@@ -1,9 +1,9 @@
 /* Subroutines for long double support.
    Copyright (C) 2000, 2001, 2002 Free Software Foundation, Inc.
 
-This file is part of GNU CC.
+This file is part of GCC.
 
-GNU CC is free software; you can redistribute it and/or modify
+GCC is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2, or (at your option)
 any later version.
@@ -17,13 +17,13 @@ do apply in other respects; for example, they cover modification of
 the file, and distribution when not linked into a combine
 executable.)
 
-GNU CC is distributed in the hope that it will be useful,
+GCC is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with GNU CC; see the file COPYING.  If not, write to
+along with GCC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
