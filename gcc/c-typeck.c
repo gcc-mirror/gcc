018ca9198d9cbbb72f36242ec4b60860ddd8e151
Roger Sayle <roger@eyesopen.com>
2003-03-16 16:43:43 +0000

    c-typeck.c (build_component_ref): Turn "for" into "do ..
    	* c-typeck.c (build_component_ref):  Turn "for" into "do .. while"
    	to avoid "may be used uninitialized" warning on ia64-hpux.
    	* config/ia64/ia64-c.c: Include "tm_p.h" for function prototypes.
    
    From-SVN: r64439

--- gcc/c-typeck.c
+++ gcc/c-typeck.c
@@ -1144,7 +1144,7 @@ build_component_ref (datum, component)
 	 end does it - by giving the anonymous entities each a
 	 separate name and type, and then have build_component_ref
 	 recursively call itself.  We can't do that here.  */
-      for (; field; field = TREE_CHAIN (field))
+      do
 	{
 	  tree subdatum = TREE_VALUE (field);
 
@@ -1161,7 +1161,10 @@ build_component_ref (datum, component)
 	    warn_deprecated_use (subdatum);
 
 	  datum = ref;
+
+	  field = TREE_CHAIN (field);
 	}
+      while (field);
 
       return ref;
     }
