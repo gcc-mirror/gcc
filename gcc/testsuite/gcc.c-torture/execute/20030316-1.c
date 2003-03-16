e44f00b4fcd298899234ab2b56246dbb358a3374
No Author <no-author@gcc.gnu.org>
2003-03-16 19:47:36 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_2-branch'.
    From-SVN: r64443

---
+++ gcc/testsuite/gcc.c-torture/execute/20030316-1.c
@@ -0,0 +1,12 @@
+/* PR target/9164 */
+/* The comparison operand was sign extended erraneously.  */
+
+int
+main (void)
+{
+    long j = 0x40000000;
+    if ((unsigned int) (0x40000000 + j) < 0L)
+ 	abort ();
+
+    return 0;
+}
