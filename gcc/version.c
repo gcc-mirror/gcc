5665afc5d2f90dedd66df5ffd4ab99c124e4ff2a
GCC Administrator <gccadmin@gcc.gnu.org>
2003-03-18 00:17:06 +0000

    Daily bump.
    [[Split portion of a mixed commit.]]
    
    From-SVN: r64516.1

--- gcc/version.c
+++ gcc/version.c
@@ -1,1 +1,1 @@
-                                                                                        char *version_string = "2.95.4 20030317 (prerelease)";
+                                                                                        char *version_string = "2.95.4 20030318 (prerelease)";
