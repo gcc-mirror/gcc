8cccd283d23f26d115a54fdabb9221d226c66073
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-03-18 16:32:12 +0000

    * i386.md (cvtts?2si peep2): New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@64537 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/i386-cvt-1.c
@@ -0,0 +1,12 @@
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -march=k8 -mfpmath=sse" } */
+/* { dg-final { scan-assembler "cvttsd2si.*xmm" } } */
+/* { dg-final { scan-assembler "cvttss2si.*xmm" } } */
+int a,a1;
+double b;
+float b1;
+t()
+{
+	a=b;
+	a1=b1;
+}
