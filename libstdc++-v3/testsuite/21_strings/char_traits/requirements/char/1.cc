fac38f66e17607d66331eb9538d8b03bc8a7fb8d
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-03-19 12:17:49 +0000

    2003-03-19  Paolo Carlini  <pcarlini@unitus.it>
    	* testsuite/21_strings/char_traits/requirements/char/1.cc:
    	Test char not wchar_t.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@64576 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/21_strings/char_traits/requirements/char/1.cc
+++ libstdc++-v3/testsuite/21_strings/char_traits/requirements/char/1.cc
@@ -26,8 +26,8 @@
 void test01(void)
 {
   bool test = true;
-  const std::wstring str_01(L"zuma beach");
-  const std::wstring str_02(L"montara and ocean beach");
+  const std::string str_01("zuma beach");
+  const std::string str_02("montara and ocean beach");
  
   // 21.1.1 character traits requirements
 
@@ -41,46 +41,46 @@ void test01(void)
   // pos      == X::pos_type
   // state    == X::state_type
 
-  // void X::assign(wchar_t c, wchar_t d)
+  // void X::assign(char c, char d)
   // assigns c = d;
-  wchar_t c1 = L'z';
-  wchar_t c2 = L'u';
+  char c1 = 'z';
+  char c2 = 'u';
   VERIFY( c1 != c2 );
-  std::char_traits<wchar_t>::assign(c1,c2);
-  VERIFY( c1 == L'u' );
+  std::char_traits<char>::assign(c1,c2);
+  VERIFY( c1 == 'u' );
 
   // char* X::move(char* s, const char* p, size_t n)
   // for each i in [0,n) performs X::assign(s[i], p[i]). Copies
   // correctly even where p is in [s, s + n), and yields s.   
-  wchar_t array1[] = {L'z', L'u', L'm', L'a', L' ', L'b', L'e', L'a', L'c', L'h',  0};
-  const wchar_t str_lit1[] = L"montara and ocean beach";
+  char array1[] = {'z', 'u', 'm', 'a', ' ', 'b', 'e', 'a', 'c', 'h',  0};
+  const char str_lit1[] = "montara and ocean beach";
   int len = sizeof(str_lit1) + sizeof(array1) - 1; // two terminating chars
-  wchar_t array2[len];
+  char array2[len];
 
   VERIFY( str_lit1[0] == 'm' );
   c1 = array2[0];
   c2 = str_lit1[0];
-  wchar_t c3 = array2[1];
-  wchar_t c4 = str_lit1[1];
-  std::char_traits<wchar_t>::move(array2, str_lit1, 0);
+  char c3 = array2[1];
+  char c4 = str_lit1[1];
+  std::char_traits<char>::move(array2, str_lit1, 0);
   VERIFY( array2[0] == c1 );
   VERIFY( str_lit1[0] == c2 );
-  std::char_traits<wchar_t>::move(array2, str_lit1, 1);
+  std::char_traits<char>::move(array2, str_lit1, 1);
   VERIFY( array2[0] == c2 );
   VERIFY( str_lit1[0] == c2 );
   VERIFY( array2[1] == c3 );
   VERIFY( str_lit1[1] == c4 );
-  std::char_traits<wchar_t>::move(array2, str_lit1, 2);
+  std::char_traits<char>::move(array2, str_lit1, 2);
   VERIFY( array2[0] == c2 );
   VERIFY( str_lit1[0] == c2 );
   VERIFY( array2[1] == c4 );
   VERIFY( str_lit1[1] == c4 );
  
-  wchar_t* pc1 = array1 + 1;
+  char* pc1 = array1 + 1;
   c1 = pc1[0];
   c2 = array1[0];
   VERIFY( c1 != c2 );
-  wchar_t* pc2 = std::char_traits<wchar_t>::move(array1, pc1, 0);
+  char* pc2 = std::char_traits<char>::move(array1, pc1, 0);
   c3 = pc1[0];
   c4 = array1[0];
   VERIFY( c1 == c3 );
@@ -89,11 +89,11 @@ void test01(void)
 
   c1 = pc1[0];
   c2 = array1[0];
-  wchar_t* pc3 = pc1;
-  pc2 = std::char_traits<wchar_t>::move(array1, pc1, 10);
+  char* pc3 = pc1;
+  pc2 = std::char_traits<char>::move(array1, pc1, 10);
   c3 = pc1[0];
   c4 = array1[0];
-  VERIFY( c1 != c3 ); // underlying wchar_t array changed.
+  VERIFY( c1 != c3 ); // underlying char array changed.
   VERIFY( c4 != c3 );
   VERIFY( pc2 == array1 );
   VERIFY( pc3 == pc1 ); // but pointers o-tay
