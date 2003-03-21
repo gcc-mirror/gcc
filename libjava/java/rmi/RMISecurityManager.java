025f5843ab4401f904dcf7bda555bb50ff849964
Michael Koch <konqueror@gmx.de>
2003-03-21 08:48:14 +0000

    2003-03-21  Michael Koch  <konqueror@gmx.de>
    	* java/rmi/RMISecurityManager.java
    	(checkAccept): Removed.
    	(checkAccess): Likewise.
    	(checkAccess): Likewise.
    	(checkAwtEventQueueAccess): Likewise.
    	(checkConnect): Likewise.
    	(checkCreateClassLoader): Likewise.
    	(checkDelete): Likewise.
    	(checkExec): Likewise.
    	(checkExit): Likewise.
    	(checkLink): Likewise.
    	(checkListen): Likewise.
    	(checkMemberAccess): Likewise.
    	(checkMulticast): Likewise.
    	(checkPackageAccess): Likewise.
    	(checkPackageDefinition): Likewise.
    	(checkPermission): Likewise.
    	(checkPrintJobAccess): Likewise.
    	(checkPropertiesAccess): Likewise.
    	(checkPropertyAccess): Likewise.
    	(checkRead): Likewise.
    	(checkSecurityAccess): Likewise.
    	(checkSetFactory): Likewise.
    	(checkSystemClipboardAccess): Likewise.
    	(checkTopLevelWindow): Likewise.
    	(checkWrite): Likewise.
    
    From-SVN: r64653

--- libjava/java/rmi/RMISecurityManager.java
+++ libjava/java/rmi/RMISecurityManager.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 1996, 1997, 1998, 1999 Free Software Foundation, Inc.
+  Copyright (c) 1996, 1997, 1998, 1999, 2003 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
@@ -37,113 +37,12 @@ exception statement from your version. */
 
 package java.rmi;
 
-import java.io.FileDescriptor;
-import java.lang.Thread;
-import java.lang.Class;
-import java.lang.SecurityManager;
-import java.net.InetAddress;
-import java.security.Permission;
-
-public class RMISecurityManager extends SecurityManager {
-
-public RMISecurityManager() {
-}
-
-public void checkAccept(String host, int port) {
-}
-
-public void checkAccess(Thread g) {
-}
-
-public void checkAccess(ThreadGroup g) {
-}
-
-public void checkAwtEventQueueAccess() {
-}
-
-public void checkConnect(String host, int port) {
-}
-
-public void checkConnect(String host, int port, Object context) {
-}
-
-public void checkCreateClassLoader() {
-}
-
-public void checkDelete(String file) {
-}
-
-public void checkExec(String cmd) {
-}
-
-public void checkExit(int status) {
-}
-
-public void checkLink(String lib) {
-}
-
-public void checkListen(int port) {
-}
-
-public void checkMemberAccess ( Class clazz, int which ) {
-}
-
-public void checkMulticast(InetAddress maddr) {
-}
-
-public void checkMulticast(InetAddress maddr, byte ttl) {
-}
-
-public void checkPackageAccess(String pkg) {
-}
-
-public void checkPackageDefinition(String pkg) {
-}
-
-public void checkPermission(Permission perm) {
-}
-
-public void checkPermission(Permission perm, Object context) {
-}
-
-public void checkPrintJobAccess() {
-}
-
-public void checkPropertiesAccess() {
-}
-
-public void checkPropertyAccess(String key) {
-}
-
-/* public void checkPropertyAccess(String key, String def) {
-}*/
-
-public void checkRead(FileDescriptor fd) {
-}
-
-public void checkRead(String file) {
-}
-
-public void checkRead(String file, Object context) {
-}
-
-public void checkSecurityAccess(String action) {
-}
-
-public void checkSetFactory() {
-}
-
-public void checkSystemClipboardAccess() {
-}
-
-public boolean checkTopLevelWindow(Object window) {
-	return (true);
-}
-
-public void checkWrite(FileDescriptor fd) {
-}
-
-public void checkWrite(String file) {
-}
-
+/**
+ * @since 1.1
+ */
+public class RMISecurityManager extends SecurityManager
+{
+  public RMISecurityManager()
+  {
+  }
 }
