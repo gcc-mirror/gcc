30b3f142281e886b688b852374172d3abeb3239a
Michael Koch <konqueror@gmx.de>
2003-03-21 09:18:31 +0000

    2003-03-21  Michael Koch  <konqueror@gmx.de>
    	* javax/swing/Action.java
    	(ACCELERATOR_KEY): New constant.
    	(ACTION_COMMAND_KEY): Likewise.
    	(MNEMONIC_KEY): Likewise.
    	* javax/swing/UnsupportedLookAndFeelException.java
    	(UnsupportedLookAndFeelException): Must be public.
    	* javax/swing/WindowConstants.java
    	(EXIT_ON_CLOSE): New constant.
    	* javax/swing/text/BadLocationException.java
    	(offset): New member variable.
    	(BadLocationException): New implementation, documentation added.
    	(offsetRequested): New method.
    	* javax/swing/text/Caret.java:
    	Reformated.
    	* javax/swing/text/Document.java:
    	Reformated.
    
    From-SVN: r64656

--- libjava/javax/swing/text/Caret.java
+++ libjava/javax/swing/text/Caret.java
@@ -42,21 +42,37 @@ import javax.swing.event.*;
 
 public interface Caret
 {
-    void addChangeListener(ChangeListener l);
-    void deinstall(JTextComponent c);
-    int getBlinkRate();
-    int getDot();
-    Point getMagicCaretPosition();
-    int getMark();
-    void install(JTextComponent c);
-    boolean isSelectionVisible();
-    boolean isVisible();
-    void moveDot(int dot);
-    void paint(Graphics g);
-    void removeChangeListener(ChangeListener l);
-    void setBlinkRate(int rate);
-    void setDot(int dot);
-    void setMagicCaretPosition(Point p);
-    void setSelectionVisible(boolean v);
-    void setVisible(boolean v);
+  void addChangeListener(ChangeListener l);
+  
+  void deinstall(JTextComponent c);
+  
+  int getBlinkRate();
+  
+  int getDot();
+  
+  Point getMagicCaretPosition();
+  
+  int getMark();
+  
+  void install(JTextComponent c);
+  
+  boolean isSelectionVisible();
+  
+  boolean isVisible();
+  
+  void moveDot(int dot);
+  
+  void paint(Graphics g);
+  
+  void removeChangeListener(ChangeListener l);
+  
+  void setBlinkRate(int rate);
+  
+  void setDot(int dot);
+  
+  void setMagicCaretPosition(Point p);
+  
+  void setSelectionVisible(boolean v);
+  
+  void setVisible(boolean v);
 }
