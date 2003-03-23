6b8880e93112633ac45052912a6411a5d01ad09e
Richard Henderson <rth@redhat.com>
2003-03-23 12:18:55 -0800

    re PR rtl-optimization/10116 (ce2: invalid merge of "join_bb" in the context of switch statements)
            PR opt/10116
            * ifcvt.c (find_if_block): Disallow tablejump insns outgoing
            from then_bb or else_bb after flow2.
    
    From-SVN: r64752

--- gcc/ifcvt.c
+++ gcc/ifcvt.c
@@ -2045,7 +2045,8 @@ find_if_block (test_bb, then_edge, else_edge)
   /* The THEN block of an IF-THEN combo must have zero or one successors.  */
   if (then_succ != NULL_EDGE
       && (then_succ->succ_next != NULL_EDGE
-          || (then_succ->flags & EDGE_COMPLEX)))
+          || (then_succ->flags & EDGE_COMPLEX)
+	  || (flow2_completed && tablejump_p (then_bb->end))))
     return FALSE;
 
   /* If the THEN block has no successors, conditional execution can still
@@ -2092,7 +2093,8 @@ find_if_block (test_bb, then_edge, else_edge)
 	   && then_succ->dest == else_succ->dest
 	   && else_bb->pred->pred_next == NULL_EDGE
 	   && else_succ->succ_next == NULL_EDGE
-	   && ! (else_succ->flags & EDGE_COMPLEX))
+	   && ! (else_succ->flags & EDGE_COMPLEX)
+	   && ! (flow2_completed && tablejump_p (else_bb->end)))
     join_bb = else_succ->dest;
 
   /* Otherwise it is not an IF-THEN or IF-THEN-ELSE combination.  */
