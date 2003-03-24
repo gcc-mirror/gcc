67c6e557337c07f9e762fd4540a26e23d25b6345
toon <toon@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-03-24 21:20:20 +0000

    2003-03-24  Bud Davis  <bdavis9659@comcast.net>
    	PR fortran/10197
    	* libI77/open.c (f_open): A DIRECT ACCESS file is
    	UNFORMATTED by default.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@64821 138bc75d-0d04-0410-961f-82ee72b054a4

--- libf2c/libI77/open.c
+++ libf2c/libI77/open.c
@@ -148,7 +148,10 @@ f_open (olist * a)
   b->url = (int) a->orl;
   b->ublnk = a->oblnk && (*a->oblnk == 'z' || *a->oblnk == 'Z');
   if (a->ofm == 0)
-    b->ufmt = 1;
+    if ((a->oacc) && (*a->oacc == 'D' || *a->oacc == 'd'))
+      b->ufmt = 0;
+    else
+      b->ufmt = 1;
   else if (*a->ofm == 'f' || *a->ofm == 'F')
     b->ufmt = 1;
   else
