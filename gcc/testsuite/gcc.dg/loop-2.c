a82119e49ef3ab1cca3e6b472878d8ffc4b7b724
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-03-25 20:24:42 +0000

    PR optimization/10171         * unroll.c (unroll_loop): Don't delete the jump at the end unless         we also delete a jump at the beginning.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@64863 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/loop-2.c
@@ -0,0 +1,20 @@
+/* PR optimization/10171 */
+/* Bug: unroll_loop misoptimized the function so that we got
+   0 iterations of the loop rather than the correct 1.  */
+/* { dg-do run } */
+
+inline int tag() { return 0; }
+
+void f ();
+
+int main() {
+  int i;
+  for (i = 0; i < (tag() ? 2 : 1); i++)
+    f();
+  abort ();
+}
+
+void f ()
+{
+  exit (0);
+}
