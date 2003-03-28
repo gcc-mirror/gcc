0c85dbfd1251f34624e17cae810de6ddd4257cc4
Eric Botcazou <ebotcazou@libertysurf.fr>
2003-03-28 10:10:43 +0100

    re PR target/10067 (GCC-3.2.2 outputs invalid asm on sparc64)
    	PR target/10067
    	* config/sparc/sparc.md (jump pattern): Correct order
    	when issuing the annuling marker.
    
    From-SVN: r64964

---
+++ gcc/testsuite/gcc.dg/ultrasp8.c
@@ -0,0 +1,39 @@
+/* PR target/10067 */
+/* Originator: <dat94ali@ludat.lth.se> */
+/* { dg-do compile { target sparc-*-* } } */
+/* { dg-options "-O2 -m64 -mtune=supersparc" } */
+
+struct _reent;
+
+extern unsigned long __malloc_trim_threshold;
+extern unsigned long __malloc_top_pad;
+
+int _mallopt_r(struct _reent *reent_ptr, int param_number, int value)
+{
+  __malloc_lock(reent_ptr);
+
+  switch(param_number)
+  {
+    case -1:
+      __malloc_trim_threshold = value;
+      __malloc_unlock(reent_ptr);
+      return 1;
+
+    case -2:
+      __malloc_top_pad = value;
+      __malloc_unlock(reent_ptr);
+      return 1;
+
+    case -3:
+      __malloc_unlock(reent_ptr);
+      return 1;
+
+    case -4:
+      __malloc_unlock(reent_ptr);
+      return value == 0;
+
+    default:
+      __malloc_unlock(reent_ptr);
+      return 0;
+  }
+}
