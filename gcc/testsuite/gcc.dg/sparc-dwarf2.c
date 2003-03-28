095ec610b0fb76da1f9997ec563885f1ac19521f
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-03-28 07:47:57 +0000

    PR target/10114 and PR target/10084 	* dwarf2out.c (mem_loc_descriptor): Handle LO_SUM.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@64947 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/sparc-dwarf2.c
@@ -0,0 +1,31 @@
+/* PR target/10114 */
+/* Originator: James Troup <james@nocrew.org> */
+/* { dg-do compile { target sparc-*-linux* } } */
+/* { dg-options "-g -O1" } */
+
+extern __inline double sqrt (double __x)
+{
+  register double __r;
+  __asm ("fsqrtd %1,%0" : "=f" (__r) : "f" (__x));
+  return __r;
+}
+
+static double our_skew, max_update_skew;
+
+static double Sqr(double x)
+{
+  return x*x;
+}
+
+void REF_SetReference(double skew)
+{
+  double previous_skew, new_skew;
+  double old_weight, new_weight, sum_weight;
+  double delta_freq1, delta_freq2;
+  double skew1, skew2;
+
+  previous_skew = our_skew;
+  skew1 = sqrt((Sqr(delta_freq1) * old_weight + Sqr(delta_freq2) * new_weight) / sum_weight);
+  skew2 = (previous_skew * old_weight + new_skew * new_weight) / sum_weight;
+  our_skew = skew1 + skew2;
+}
