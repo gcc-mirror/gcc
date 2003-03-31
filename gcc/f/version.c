9cd29341e3146e00fa33fb26cb7a11e99178022a
GCC Administrator <gccadmin@gcc.gnu.org>
2003-03-31 00:16:50 +0000

    Daily bump.
    [[Split portion of a mixed commit.]]
    
    From-SVN: r65067.1

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,1 +1,1 @@
-                                                                                        const char *ffe_version_string = "0.5.26 20030330 (prerelease)";
+                                                                                        const char *ffe_version_string = "0.5.26 20030331 (prerelease)";
--- libf2c/libI77/Version.c
+++ libf2c/libI77/Version.c
@@ -3,7 +3,7 @@
 /*
 */
 
-char __G77_LIBI77_VERSION__[] = "0.5.26 20030330 (prerelease)";
+char __G77_LIBI77_VERSION__[] = "0.5.26 20030331 (prerelease)";
 
 /*
 2.01	$ format added
