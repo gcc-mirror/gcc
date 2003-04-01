0f4f87cf3349e9c9e038ee77f468272c17082b0f
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-04-01 19:18:24 +0000

    2003-04-01  Aldy Hernandez  <aldyh@redhat.com>
            * g++.dg/eh/simd-1.C: New.
    
            * g++.dg/eh/simd-2.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@65138 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/eh/simd-1.C
@@ -0,0 +1,49 @@
+// Test EH when V2SI SIMD registers are involved.
+// Contributed by Aldy Hernandez (aldy@quesejoda.com).
+// { dg-options "-O" }
+// { dg-do run }
+
+typedef int __attribute__((mode(V2SI))) vecint;
+
+vecint vecfunc (vecint beachbum)
+{
+  return vecint;
+}
+
+void f3 (void)
+{
+  /* Force a use of a V2SI register if available.  On the PPC/E500,
+     this will cause the compiler to save the registers in this
+     function in 64-bits.  */
+  vecint foobar = (vecint) {0, 0};
+  foobar = vecfunc (foobar);
+
+  throw int();
+}
+
+void f2 (void)
+{
+  vecint foobar = (vecint) {0, 0};
+  foobar = vecfunc (foobar);
+
+  f3 ();
+}
+
+void f1 (void)
+{
+  int i;
+  try
+    {
+      f2 ();
+    }
+  catch (int)
+    {
+      i = 9;
+    }
+}
+
+int main ()
+{
+  f1 ();
+  return 0;
+}
---
+++ gcc/testsuite/g++.dg/eh/simd-2.C
@@ -0,0 +1,46 @@
+// Test EH when V4SI SIMD registers are involved.
+// Contributed by Aldy Hernandez (aldy@quesejoda.com).
+// { dg-options "-O" }
+// { dg-do run }
+
+typedef int __attribute__((mode(V4SI))) vecint;
+
+vecint vecfunc (vecint beachbum)
+{
+  return vecint;
+}
+
+void f3 (void)
+{
+  vecint foobar = (vecint) {0, 0};
+  foobar = vecfunc (foobar);
+
+  throw int();
+}
+
+void f2 (void)
+{
+  vecint foobar = (vecint) {0, 0};
+  foobar = vecfunc (foobar);
+
+  f3 ();
+}
+
+void f1 (void)
+{
+  int i;
+  try
+    {
+      f2 ();
+    }
+  catch (int)
+    {
+      i = 9;
+    }
+}
+
+int main ()
+{
+  f1 ();
+  return 0;
+}
