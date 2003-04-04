7ac84b8aae82d9ecba4aee9294dd4d599012890f
nickc <nickc@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-04-04 13:00:09 +0000

    config/sparc/sol2-bi.h: Revert previous delta. config/sparc/sol2-gas-bi.h (AS_SPARC64_FLAG): Prepend -TSO.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@65237 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sparc/sol2-gas-bi.h
+++ gcc/config/sparc/sol2-gas-bi.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler, for bi-arch SPARC
    running Solaris 2 using the GNU assembler.  */
 
-#undef AS_SPARC64_FLAG
-#define AS_SPARC64_FLAG	"-64 -Av9"
+#undef  AS_SPARC64_FLAG
+#define AS_SPARC64_FLAG	"-TSO -64 -Av9"
