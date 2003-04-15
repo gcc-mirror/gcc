55adc7bd57e426ac615874ce723406e11f341388
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2003-04-15 14:24:26 +0000

    re PR target/10338 ([3.3 regression?] [Cygwin -> tic4x | avr] cross target compilation error)
    gcc:
    	PR target/10338
    	PR bootstrap/10198
    	PR bootstrap/10140
    	* fixinc/gnu-regex.c (regerror): Use mempcpy not __mempcpy.
    
    libiberty:
    	PR target/10338
    	PR bootstrap/10198
    	PR bootstrap/10140
    	* getopt.c (exchange, _getopt_initialize): Use mempcpy not
    	__mempcpy.
    	* regex.c (regerror): Likewise.
    
    From-SVN: r65634

--- libiberty/regex.c
+++ libiberty/regex.c
@@ -8200,7 +8200,7 @@ regerror (errcode, preg, errbuf, errbuf_size)
       if (msg_size > errbuf_size)
         {
 #if defined HAVE_MEMPCPY || defined _LIBC
-	  *((char *) __mempcpy (errbuf, msg, errbuf_size - 1)) = '\0';
+	  *((char *) mempcpy (errbuf, msg, errbuf_size - 1)) = '\0';
 #else
           memcpy (errbuf, msg, errbuf_size - 1);
           errbuf[errbuf_size - 1] = 0;
