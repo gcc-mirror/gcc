88702c45a68e768f8d5537f2c6c77c9c7b7e6d58
Roger Sayle <roger@eyesopen.com>
2003-04-15 02:11:43 +0000

    * strdup.c (strdup): Tweak implementation to use memcpy.
    From-SVN: r65616

--- libiberty/strdup.c
+++ libiberty/strdup.c
@@ -9,13 +9,24 @@ Returns a pointer to a copy of @var{s} in memory obtained from
 
 */
 
+#include <ansidecl.h>
+#ifdef ANSI_PROTOTYPES
+#include <stddef.h>
+#else
+#define size_t unsigned long
+#endif
+
+extern size_t	strlen PARAMS ((const char*));
+extern PTR	malloc PARAMS ((size_t));
+extern PTR	memcpy PARAMS ((PTR, const PTR, size_t));
+
 char *
 strdup(s)
      char *s;
 {
-    char *result = (char*)malloc(strlen(s) + 1);
-    if (result == (char*)0)
-	return (char*)0;
-    strcpy(result, s);
-    return result;
+  size_t len = strlen (s) + 1;
+  char *result = (char*) malloc (len);
+  if (result == (char*) 0)
+    return (char*) 0;
+  return (char*) memcpy (result, s, len);
 }
