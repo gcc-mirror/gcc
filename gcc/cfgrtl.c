dee7f1be30a66b48744adaf23f8954e25d1749f9
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-04-16 20:39:12 +0000

    PR middle-end/8866 	* cfgtrl.c (try_redirect_by_replacing_jump): Do not delete 	jumptables.
    	PR middle-end/8866
    	* libjava.lang/PR8866.java: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@65698 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cfgrtl.c
+++ gcc/cfgrtl.c
@@ -731,7 +731,7 @@ try_redirect_by_replacing_jump (e, target)
   else
     {
       rtx target_label = block_label (target);
-      rtx barrier, label, table;
+      rtx barrier;
 
       emit_jump_insn_after (gen_jump (target_label), insn);
       JUMP_LABEL (src->end) = target_label;
@@ -740,14 +740,9 @@ try_redirect_by_replacing_jump (e, target)
 	fprintf (rtl_dump_file, "Replacing insn %i by jump %i\n",
 		 INSN_UID (insn), INSN_UID (src->end));
 
-
+      /* Remove the original jump.  If INSN is a tablejump, the jump
+	 table will be removed later, if it is no longer needed.  */
       delete_insn_chain (kill_from, insn);
-
-      /* Recognize a tablejump that we are converting to a
-	 simple jump and remove its associated CODE_LABEL
-	 and ADDR_VEC or ADDR_DIFF_VEC.  */
-      if (tablejump_p (insn, &label, &table))
-	delete_insn_chain (label, table);
 
       barrier = next_nonnote_insn (src->end);
       if (!barrier || GET_CODE (barrier) != BARRIER)
