f9e40cfb02b2c24c031a9053c86ce6f85ceb913b
jkj <jkj@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-04-19 21:23:20 +0000

    Part 2 of the SCO port update. This is everything I have approval for. There are two outstanding patches I await approval to check in.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@65832 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/unwind-dw2.c
+++ gcc/unwind-dw2.c
@@ -178,7 +178,7 @@ _Unwind_GetGR (struct _Unwind_Context *context, int index)
 _Unwind_Word
 _Unwind_GetCFA (struct _Unwind_Context *context)
 {
-  return context->cfa;
+  return (_Unwind_Word)context->cfa;
 }
 
 /* Overwrite the saved value for register REG in CONTEXT with VAL.  */
