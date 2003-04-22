77c915d8799fae4eaef052af7e99d98d9637ab5d
David Turner <novalis@gnu.org>
2003-04-22 17:21:04 +0000

    gbl-ctors.h: Add special license exception.
    	* gbl-ctors.h: Add special license exception.
    	* libgcc2.h: Likewise.
    	* tsystem.h: Likewise.
    	* gcov-io.h: Likewise.
    
    From-SVN: r65949

--- gcc/gbl-ctors.h
+++ gcc/gbl-ctors.h
@@ -23,6 +23,13 @@ along with GCC; see the file COPYING.  If not, write to the Free
 Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 02111-1307, USA.  */
 
+/* As a special exception, if you link this library with other files,
+   some of which are compiled with GCC, to produce an executable,
+   this library does not by itself cause the resulting executable
+   to be covered by the GNU General Public License.
+   This exception does not however invalidate any other reasons why
+   the executable file might be covered by the GNU General Public License.  */
+
 /*	This file contains definitions and declarations of things
 	relating to the normal start-up-time invocation of C++
 	file-scope static object constructors.  These declarations
