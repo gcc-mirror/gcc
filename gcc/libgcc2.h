b5c673fc200fdd02f27e9a3a42f53eca2b3b6ae9
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-04-22 17:21:04 +0000

    * gbl-ctors.h: Add special license exception. 	* libgcc2.h: Likewise. 	* tsystem.h: Likewise. 	* gcov-io.h: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@65949 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/libgcc2.h
+++ gcc/libgcc2.h
@@ -19,6 +19,14 @@ along with GCC; see the file COPYING.  If not, write to the Free
 Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 02111-1307, USA.  */
 
+/* As a special exception, if you link this library with other files,
+   some of which are compiled with GCC, to produce an executable,
+   this library does not by itself cause the resulting executable
+   to be covered by the GNU General Public License.
+   This exception does not however invalidate any other reasons why
+   the executable file might be covered by the GNU General Public License.  */
+
+
 #ifndef GCC_LIBGCC2_H
 #define GCC_LIBGCC2_H
 
