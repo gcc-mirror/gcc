d7ed5e55a2d3ad13cf58bf98343a0a002ee779ca
Kriang Lerdsuwanakij <lerdsuwa@users.sourceforge.net>
2003-04-30 17:51:41 +0000

    PR c++/9432, c++/9528
    	PR c++/9432, c++/9528
    	* decl2.c (validate_nonmember_using_decl): Handle SCOPE_REF.
    
    	* g++.dg/lookup/using4.C: New test.
    
    From-SVN: r66298

---
+++ gcc/testsuite/g++.dg/lookup/using4.C
@@ -0,0 +1,15 @@
+// { dg-do compile }
+
+// Origin: Richard Guenther <rguenth@tat.physik.uni-tuebingen.de>
+
+// PR c++/9432: ICE in validate_nonmember_using_decl when decl is a 
+// SCOPE_REF.
+
+template <class T> struct Foo;
+template <class T>
+struct Bar : public Foo<T> {
+        void foo()
+        {
+                using Foo<T>::i;	// { dg-error "not a namespace" }
+        }
+};
