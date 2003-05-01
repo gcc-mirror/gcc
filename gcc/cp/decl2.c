220eacb44245a14438739c5f28020e5de2f3f9a1
Mark Mitchell <mark@codesourcery.com>
2003-05-01 15:47:02 +0000

    decl2.c (comdat_linkage): Don't externalize explicit instantiations.
    	* decl2.c (comdat_linkage): Don't externalize explicit
    	instantiations.
    
    From-SVN: r66332

--- gcc/cp/decl2.c
+++ gcc/cp/decl2.c
@@ -1629,7 +1629,7 @@ comdat_linkage (decl)
 	  DECL_COMMON (decl) = 1;
 	  DECL_INITIAL (decl) = error_mark_node;
 	}
-      else
+      else if (!DECL_EXPLICIT_INSTANTIATION (decl))
 	{
 	  /* We can't do anything useful; leave vars for explicit
 	     instantiation.  */
