b4db1af89ef7607e747f1d151a353ef12a360db9
neil <neil@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-05-04 21:45:08 +0000

    * doc/cppopts.texi: Enhance -Wtrigraphs documentation
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@66462 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cpp/Wtrigraphs-2.c
+++ gcc/testsuite/gcc.dg/cpp/Wtrigraphs-2.c
@@ -1,14 +1,14 @@
 /* { dg-do preprocess } */
 /* { dg-options "-std=c99 -Wtrigraphs -fno-show-column" } */
 
-/* Test we don't double warn for trigraphs immediately after preceding
-   text.  Source Neil Booth.  4 May 2003.  */
+/* Test warnings for trigraphs in comments, with trigraphs enabled.
+   Neil Booth.  4 May 2003.  */
 
-/* { dg-bogus "ignored" } Test ??< ??= a few ??/ random things in
+/* { dg-bogus "converted" } Test ??< ??= a few ??/ random things in
    { dg-warning "converted" } some ??/
-   { dg-bogus "ignored" } ??< comments.  */
+   { dg-bogus "converted" } ??< comments.  */
 
-// { dg-bogus "ignored" } More ??/ comment ??> tests.
+// { dg-bogus "converted" } More ??/ comment ??> tests.
 
 // { dg-warning "converted" } Another ??/
    Test
@@ -16,5 +16,5 @@
 // { dg-warning "converted" } And another with space after ??/  
    the escape
 
-// { dg-bogus "ignored" } A tricky one ??/\
+// { dg-bogus "converted" } A tricky one ??/\
 
