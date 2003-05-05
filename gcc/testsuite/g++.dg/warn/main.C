577a5d3436d4d2f52759d8d89068ccd13185edc7
lerdsuwa <lerdsuwa@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-05-05 12:19:12 +0000

    PR c++/4494 	* decl.c (start_function): Use same_type_p to check return type 	of main.
    	* g++.dg/warn/main.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@66476 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/warn/main.C
@@ -0,0 +1,11 @@
+// { dg-do compile }
+
+// Origin: Donald W. Gillies <dgillies@graviton.com>
+
+// PR c++/4494: Incorrect diagnostics about return type of main.
+
+typedef int int_t;
+
+int_t main() {
+
+}
