4ee51feef93099568721f35e640bdc8a8455c9af
lerdsuwa <lerdsuwa@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-05-05 14:35:58 +0000

    PR c++/10496 	* typeck.c (build_unary_op): Don't output const qualifier when 	output invalid pointer-to-member diagnostics.
    	* g++.dg/warn/pmf1.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@66481 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/warn/pmf1.C
@@ -0,0 +1,18 @@
+// { dg-do compile }
+
+// Origin: benko@sztaki.hu
+
+// PR c++/10496: Incorrect pointer to member function diagnostics
+// for constant member functions.
+
+struct a
+{
+  int f() const;
+};
+
+
+int
+a::f() const
+{
+  int (a::* b)() const = &f; // { dg-error "&a::f" }
+}
