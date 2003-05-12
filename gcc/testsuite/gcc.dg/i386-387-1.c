26398ccb63dde50f8039ed075b55280a2521f4d3
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-05-12 02:51:40 +0000

    * config/i386/i386.md (logsf2, logdf2, logxf2, logdf2): New patterns 	to implement log, logf and logl built-ins as inline x87 intrinsics. 	(UNSPEC_FYL2X): New unspec to represent x87's "fyl2x" instruction. 	(*fyl2x_sfxf3, *fyl2x_dfxf3, *fyl2x_xf3, *fyl2x_tfxf3): New insn 	patterns for x87's "fyl2x" instruction, used by log?f2 patterns.
    	* reg-stack.c (subst_stack_regs_pat): Handle UNSPEC_FYL2X like
    	UNSPEC_FPATAN, i.e. replaces two stack operands with single result.
    
    	* gcc.dg/i386-387-1.c: Update to also test log.
    	* gcc.dg/i386-387-2.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@66709 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/i386-387-1.c
+++ gcc/testsuite/gcc.dg/i386-387-1.c
@@ -5,8 +5,10 @@
 /* { dg-final { scan-assembler "call\t_?cos" } } */
 /* { dg-final { scan-assembler "call\t_?sqrt" } } */
 /* { dg-final { scan-assembler "call\t_?atan2" } } */
+/* { dg-final { scan-assembler "call\t_?log" } } */
 
 double f1(double x) { return __builtin_sin(x); }
 double f2(double x) { return __builtin_cos(x); }
 double f3(double x) { return __builtin_sqrt(x); }
 double f4(double x, double y) { return __builtin_atan2(x,y); }
+double f5(double x) { return __builtin_log(x); }
