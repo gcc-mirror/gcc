671cc19bba67c6c40b17701f25b80cfa89847591
Mark Mitchell <mmitchel@gcc.gnu.org>
2003-05-13 20:24:38 +0000

    Update version
    From-SVN: r66783

--- gcc/version.c
+++ gcc/version.c
@@ -6,7 +6,7 @@
    please modify this string to indicate that, e.g. by putting your
    organization's name in parentheses at the end of the string.  */
 
-const char version_string[] = "3.3 20030513 (prerelease)";
+const char version_string[] = "3.3";
 
 /* This is the location of the online document giving instructions for
    reporting bugs.  If you distribute a modified version of GCC,
@@ -15,4 +15,4 @@ const char version_string[] = "3.3 20030513 (prerelease)";
    forward us bugs reported to you, if you determine that they are
    not bugs in your modifications.)  */
 
-const char bug_report_url[] = "<URL:http://gcc.gnu.org/bugs.html>";
+const char bug_report_url[] = "3.3";
