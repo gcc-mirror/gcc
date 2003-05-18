895bc69140fa92ab577dfeffb3b614e8081cd87c
lerdsuwa <lerdsuwa@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-05-18 09:42:12 +0000

    * semantics.c (perform_deferred_access_checks): Don't discard 	checked access.
    	* g++.dg/parse/access2.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@66925 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/access2.C
@@ -0,0 +1,15 @@
+// Copyright (C) 2003 Free Software Foundation
+// Contributed by Kriang Lerdsuwanakij <lerdsuwa@users.sourceforge.net>
+// { dg-do compile }
+
+// Deferred access checking of variable declaration.
+
+class A {
+  typedef int X;	// { dg-error "private" }
+  static X a, b, c;
+};
+
+A::X A::a;
+A::X A::b, x;		// { dg-error "this context" }
+A::X y, A::c;		// { dg-error "this context" }
+A::X z;			// { dg-error "this context" }
