1c745a02699797b52f1978dd00ac46b3b149938c
Eric Christopher <echristo@gcc.gnu.org>
2003-05-19 07:00:34 +0000

    mips.h: Remove definition of g_switch_value.
    2003-05-18  Eric Christopher  <echristo@redhat.com>
    
    	* config/mips/mips.h: Remove definition of g_switch_value.
    
    From-SVN: r66947

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -139,7 +139,7 @@ extern int file_in_function_warning;	/* warning given about .file in func */
 extern int sdb_label_count;		/* block start/end next label # */
 extern int sdb_begin_function_line;     /* Starting Line of current function */
 extern int mips_section_threshold;	/* # bytes of data/sdata cutoff */
-extern int g_switch_value;		/* value of the -G xx switch */
+/* extern unsigned HOST_WIDE_INT  g_switch_value; */ /* value of the -G xx switch */
 extern int g_switch_set;		/* whether -G xx was passed.  */
 extern int sym_lineno;			/* sgi next label # for each stmt */
 extern int set_noreorder;		/* # of nested .set noreorder's  */
