38709cad91e97b8e0ed0663f22c50663686ae1ae
Nathanael Nerode <neroden@gcc.gnu.org>
2003-05-23 20:25:39 +0000

    hash.c, [...]: Replace "GNU CC" with "GCC".
    	* hash.c, init.c, libobjc.def, libobjc_entry.c, linking.m,
    	makefile.dos, misc.c, nil_method.c, objects.c, sarray.c,
    	selector.c, sendmsg.c, thr-dce.c, thr-decosf1.c, thr-irix.c,
    	thr-mach.c, thr-objc.c, thr-os2.c, thr-posix.c, thr-pthreads.c,
    	thr-rtems.c, thr-single.c, thr-solaris.c, thr-vxworks.c,
    	thr-win32.c, thr.c: Replace "GNU CC" with "GCC".
    
    From-SVN: r67134

--- libobjc/thr-solaris.c
+++ libobjc/thr-solaris.c
@@ -3,19 +3,19 @@
    Contributed by Galen C. Hunt (gchunt@cs.rochester.edu)
    Conditions added by Mircea Oancea (mircea@first.elcom.pub.ro)
       
-This file is part of GNU CC.
+This file is part of GCC.
 
-GNU CC is free software; you can redistribute it and/or modify it under the
+GCC is free software; you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
 Foundation; either version 2, or (at your option) any later version.
 
-GNU CC is distributed in the hope that it will be useful, but WITHOUT ANY
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 details.
 
 You should have received a copy of the GNU General Public License along with
-GNU CC; see the file COPYING.  If not, write to the Free Software
+GCC; see the file COPYING.  If not, write to the Free Software
 Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
