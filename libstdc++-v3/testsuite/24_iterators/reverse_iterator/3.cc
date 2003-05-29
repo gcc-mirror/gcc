481fca456a283bde417a135ec974a4cb31998f26
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-05-29 12:21:26 +0000

    2003-05-29  Sylvain Pion  <Sylvain.Pion@mpi-sb.mpg.de>
    	PR libstdc++/10783
    	* include/bits/stl_iterator.h (class __normal_iterator):
    	Don't inherit from iterator, add missing typedefs.
    
    2003-05-29  Paolo Carlini <pcarlini@unitus.it>
    
    	* testsuite/24_iterators/reverse_iterator.cc: Split up, as follows.
    	* testsuite/24_iterators/reverse_iterator/1.cc: New.
    	* testsuite/24_iterators/reverse_iterator/2.cc: New.
    	* testsuite/24_iterators/reverse_iterator/3.cc: New, from
    	PR libstdc++/10783.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@67223 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/24_iterators/reverse_iterator/3.cc
@@ -0,0 +1,40 @@
+// 2003-05-29  Paolo Carlini  <pcarlini@unitus.it>
+
+// Copyright (C) 2003 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 24.4.1.2 Reverse iterators
+
+#include <iterator>
+#include <vector>
+#include <testsuite_hooks.h>
+
+// libstdc++/10783
+void test03()
+{
+  bool test = true;
+
+  typedef std::vector<int> V;
+  VERIFY( sizeof(V::iterator) == sizeof(V::reverse_iterator) );
+}
+
+int main() 
+{ 
+  test03();
+  return 0;
+}
