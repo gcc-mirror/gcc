7237deda570136e5dd45f80d1992e61868712892
aj <aj@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-06-01 16:04:55 +0000

    * toplev.c: Use ISO C90 prototypes.
            * toplev.h: Use ISO C90 prototypes.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@67299 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/toplev.h
+++ gcc/toplev.h
@@ -1,5 +1,5 @@
 /* toplev.h - Various declarations for functions found in toplev.c
-   Copyright (C) 1998, 1999, 2000, 2001 Free Software Foundation, Inc.
+   Copyright (C) 1998, 1999, 2000, 2001, 2003 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
@@ -26,23 +26,23 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #define skip_leading_substring(whole,  part) \
    (strncmp (whole, part, strlen (part)) ? NULL : whole + strlen (part))
 
-extern int toplev_main			PARAMS ((int, char **));
-extern int read_integral_parameter	PARAMS ((const char *, const char *,
-						const int));
-extern void strip_off_ending		PARAMS ((char *, int));
-extern void print_time			PARAMS ((const char *, long));
-extern const char *trim_filename	PARAMS ((const char *));
-extern void internal_error		PARAMS ((const char *, ...))
-					       ATTRIBUTE_NORETURN;
-extern void _fatal_insn_not_found	PARAMS ((struct rtx_def *,
-						const char *, int,
-						const char *))
-					       ATTRIBUTE_NORETURN;
-extern void _fatal_insn			PARAMS ((const char *,
-						struct rtx_def *,
-						const char *, int,
-						const char *))
-					      ATTRIBUTE_NORETURN;
+extern int toplev_main			(int, char **);
+extern int read_integral_parameter	(const char *, const char *,
+					 const int);
+extern void strip_off_ending		(char *, int);
+extern void print_time			(const char *, long);
+extern const char *trim_filename	(const char *);
+extern void internal_error		(const char *, ...)
+     ATTRIBUTE_NORETURN;
+extern void _fatal_insn_not_found	(struct rtx_def *,
+  					 const char *, int,
+					 const char *)
+     ATTRIBUTE_NORETURN;
+extern void _fatal_insn			(const char *,
+					 struct rtx_def *,
+					 const char *, int,
+					 const char *)
+     ATTRIBUTE_NORETURN;
 
 #define fatal_insn(msgid, insn) \
 	_fatal_insn (msgid, insn, __FILE__, __LINE__, __FUNCTION__)
@@ -52,48 +52,48 @@ extern void _fatal_insn			PARAMS ((const char *,
 /* None of these functions are suitable for ATTRIBUTE_PRINTF, because
    each language front end can extend them with its own set of format
    specifiers.  */
-extern void warning			PARAMS ((const char *, ...));
-extern void error			PARAMS ((const char *, ...));
-extern void fatal_error			PARAMS ((const char *, ...))
-					       ATTRIBUTE_NORETURN;
-extern void pedwarn			PARAMS ((const char *, ...));
-extern void sorry			PARAMS ((const char *, ...));
+extern void warning			(const char *, ...);
+extern void error			(const char *, ...);
+extern void fatal_error			(const char *, ...)
+     ATTRIBUTE_NORETURN;
+extern void pedwarn			(const char *, ...);
+extern void sorry			(const char *, ...);
 
-extern void rest_of_decl_compilation	PARAMS ((union tree_node *,
-						const char *, int, int));
-extern void rest_of_type_compilation	PARAMS ((union tree_node *, int));
-extern void rest_of_compilation		PARAMS ((union tree_node *));
+extern void rest_of_decl_compilation	(union tree_node *,
+					 const char *, int, int);
+extern void rest_of_type_compilation	(union tree_node *, int);
+extern void rest_of_compilation		(union tree_node *);
 
-extern void pedwarn_with_decl		PARAMS ((union tree_node *,
-						 const char *, ...));
-extern void warning_with_decl		PARAMS ((union tree_node *,
-						 const char *, ...));
-extern void error_with_decl		PARAMS ((union tree_node *,
-						 const char *, ...));
+extern void pedwarn_with_decl		(union tree_node *,
+					 const char *, ...);
+extern void warning_with_decl		(union tree_node *,
+					 const char *, ...);
+extern void error_with_decl		(union tree_node *,
+					 const char *, ...);
 
-extern void announce_function		PARAMS ((union tree_node *));
+extern void announce_function		(union tree_node *);
 
-extern void error_for_asm		PARAMS ((struct rtx_def *,
-						 const char *, ...));
-extern void warning_for_asm		PARAMS ((struct rtx_def *,
-						 const char *, ...));
-extern void warn_deprecated_use		PARAMS ((union tree_node *));
+extern void error_for_asm		(struct rtx_def *,
+					 const char *, ...);
+extern void warning_for_asm		(struct rtx_def *,
+					 const char *, ...);
+extern void warn_deprecated_use		(union tree_node *);
 
-extern void output_clean_symbol_name    PARAMS ((FILE *, const char *));
+extern void output_clean_symbol_name    (FILE *, const char *);
 #ifdef BUFSIZ
-extern void output_quoted_string	PARAMS ((FILE *, const char *));
-extern void output_file_directive	PARAMS ((FILE *, const char *));
+extern void output_quoted_string	(FILE *, const char *);
+extern void output_file_directive	(FILE *, const char *);
 #endif
 
 #ifdef BUFSIZ
   /* N.B. Unlike all the others, fnotice is just gettext+fprintf, and
      therefore it can have ATTRIBUTE_PRINTF.  */
-extern void fnotice			PARAMS ((FILE *, const char *, ...))
-					       ATTRIBUTE_PRINTF_2;
+extern void fnotice			(FILE *, const char *, ...)
+     ATTRIBUTE_PRINTF_2;
 #endif
 
-extern int wrapup_global_declarations   PARAMS ((union tree_node **, int));
-extern void check_global_declarations   PARAMS ((union tree_node **, int));
+extern int wrapup_global_declarations   (union tree_node **, int);
+extern void check_global_declarations   (union tree_node **, int);
 
 extern const char *progname;
 extern const char *dump_base_name;
@@ -106,10 +106,10 @@ extern struct ht *ident_hash;
 /* This function can be used by targets to set the flags originally
     implied by -ffast-math and -fno-fast-math.  */
 
-extern void set_fast_math_flags         PARAMS ((int));
+extern void set_fast_math_flags         (int);
 
 /* Return true iff flags are set as if -ffast-math.  */
-extern bool fast_math_flags_set_p	PARAMS ((void));
+extern bool fast_math_flags_set_p	(void);
 
 /* The following functions accept a wide integer argument.  Rather
    than having to cast on every function call, we use a macro instead.  */
@@ -118,7 +118,7 @@ extern bool fast_math_flags_set_p	PARAMS ((void));
 #define exact_log2(N) exact_log2_wide ((unsigned HOST_WIDE_INT) (N))
 #define floor_log2(N) floor_log2_wide ((unsigned HOST_WIDE_INT) (N))
 #endif
-extern int exact_log2_wide             PARAMS ((unsigned HOST_WIDE_INT));
-extern int floor_log2_wide             PARAMS ((unsigned HOST_WIDE_INT));
+extern int exact_log2_wide             (unsigned HOST_WIDE_INT);
+extern int floor_log2_wide             (unsigned HOST_WIDE_INT);
 
 #endif /* ! GCC_TOPLEV_H */
