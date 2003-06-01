901ebec29c582285316268209eed743f18fb2690
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-06-01 11:35:45 +0000

    2003-06-01  Aldy Hernandez  <aldyh@redhat.com>
            * config/rs6000/rs6000.h (CANNOT_CHANGE_MODE_CLASS): Require
            TARGET_SPE for SPE_VECTOR_MODE.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@67289 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/rs6000/rs6000.h
+++ gcc/config/rs6000/rs6000.h
@@ -1406,7 +1406,7 @@ enum reg_class
 #define CANNOT_CHANGE_MODE_CLASS(FROM, TO, CLASS)			\
   (GET_MODE_SIZE (FROM) != GET_MODE_SIZE (TO)				\
    ? reg_classes_intersect_p (FLOAT_REGS, CLASS)			\
-   : (SPE_VECTOR_MODE (FROM) + SPE_VECTOR_MODE (TO)) == 1		\
+   : (TARGET_SPE && (SPE_VECTOR_MODE (FROM) + SPE_VECTOR_MODE (TO)) == 1) \
    ? reg_classes_intersect_p (GENERAL_REGS, CLASS) 			\
    : 0)
 
