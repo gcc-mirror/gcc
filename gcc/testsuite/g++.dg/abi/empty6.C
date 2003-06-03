66778a93ebe23cac9934e3a3d8f7dac2010ae563
amylaar <amylaar@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-06-03 19:10:09 +0000

    * g++.dg/abi/empty6.C (B): Request alignment of 8 bytes.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@67398 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/abi/empty6.C
+++ gcc/testsuite/g++.dg/abi/empty6.C
@@ -5,4 +5,9 @@ struct A {};
 struct B {
   A a; // { dg-warning "empty" }
   virtual void f () {}
-};
+} __attribute__((aligned(8)));
+/* The preceding attribute is necessary on targets with
+   BIGGEST_ALIGNMENT <= 32 to trigger the warning, as otherwise a 32 bit
+   offset is split into DECL_FIELD_OFFSET 4 and DECL_FIELD_BIT_OFFSET 0,
+   and then there is no discrepancy between DECL_FIELD_OFFSET and
+   byte_position to warn about.  */
