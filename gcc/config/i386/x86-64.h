a8191cadf6122c07f8f6f09004721b9a056b4ae7
neroden <neroden@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-06-03 16:40:42 +0000

    * config/i386/x86-64.h: Remove two target-independent comments; 	 replace "GNU CC" with "GCC".
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@67381 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/x86-64.h
+++ gcc/config/i386/x86-64.h
@@ -2,20 +2,20 @@
    Copyright (C) 2001 Free Software Foundation, Inc.
    Contributed by Bo Thorsen <bo@suse.de>.
 
-This file is part of GNU CC.
+This file is part of GCC.
 
-GNU CC is free software; you can redistribute it and/or modify
+GCC is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2, or (at your option)
 any later version.
 
-GNU CC is distributed in the hope that it will be useful,
+GCC is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with GNU CC; see the file COPYING.  If not, write to
+along with GCC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
@@ -51,19 +51,10 @@ Boston, MA 02111-1307, USA.  */
 #define ASM_SPEC "%{v:-V} %{Qy:} %{!Qn:-Qy} %{n} %{T} %{Ym,*} %{Yd,*} \
  %{Wa,*:%*} %{m32:--32}"
 
-/* A C statement (sans semicolon) to output to the stdio stream
-   FILE the assembler definition of uninitialized global DECL named
-   NAME whose size is SIZE bytes and alignment is ALIGN bytes.
-   Try to use asm_output_aligned_bss to implement this macro.  */
-
 #define ASM_OUTPUT_ALIGNED_BSS(FILE, DECL, NAME, SIZE, ALIGN) \
   asm_output_aligned_bss (FILE, DECL, NAME, SIZE, ALIGN)
 
-/* A C statement to output to the stdio stream FILE an assembler
-   command to advance the location counter to a multiple of 1<<LOG
-   bytes if it is within MAX_SKIP bytes.
-
-   This is used to align code labels according to Intel recommendations.  */
+/* This is used to align code labels according to Intel recommendations.  */
 
 #define ASM_OUTPUT_MAX_SKIP_ALIGN(FILE,LOG,MAX_SKIP)			\
   do {									\
