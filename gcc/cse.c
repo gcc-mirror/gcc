77aefdbb03577fa9814adac19c9b147e23b08e40
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-06-04 21:05:20 +0000

    * cse.c (find_best_addr): Consider binary operators even if second         argument is not CONST_INT.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@67463 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cse.c
+++ gcc/cse.c
@@ -3009,10 +3009,9 @@ find_best_addr (insn, loc, mode)
   if (flag_expensive_optimizations
       && (GET_RTX_CLASS (GET_CODE (*loc)) == '2'
 	  || GET_RTX_CLASS (GET_CODE (*loc)) == 'c')
-      && GET_CODE (XEXP (*loc, 0)) == REG
-      && GET_CODE (XEXP (*loc, 1)) == CONST_INT)
+      && GET_CODE (XEXP (*loc, 0)) == REG)
     {
-      rtx c = XEXP (*loc, 1);
+      rtx op1 = XEXP (*loc, 1);
 
       do_not_record = 0;
       hash = HASH (XEXP (*loc, 0), Pmode);
@@ -3054,7 +3053,7 @@ find_best_addr (insn, loc, mode)
 		    || exp_equiv_p (p->exp, p->exp, 1, 0)))
 	      {
 		rtx new = simplify_gen_binary (GET_CODE (*loc), Pmode,
-					       p->exp, c);
+					       p->exp, op1);
 		int new_cost;
 		new_cost = address_cost (new, mode);
 
