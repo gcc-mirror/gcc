30378fe2b8a3509c0d49973cc8821084f9bdcb1c
Richard Henderson <rth@redhat.com>
2003-06-04 09:56:13 -0700

    c-common.c (handle_cleanup_attribute): New.
            * c-common.c (handle_cleanup_attribute): New.
            (c_common_attributes): Add it.
            * c-decl.c (finish_decl): Honor the cleanup attribute.
            * doc/extend.texi (Variable Attributes): Document it.
    
            * unwind-c.c: New file.
            * Makefile.in (LIB2ADDEH): Add it.
            * config/t-darwin, config/t-linux, config/t-linux-gnulibc1,
            config/ia64/t-ia64: Likewise.
    
            * gcc.dg/cleanup-1.c: New.
            * gcc.dg/cleanup-2.c: New.
            * gcc.dg/cleanup-3.c: New.
            * gcc.dg/cleanup-4.c: New.
            * gcc.dg/cleanup-5.c: New.
            * gcc.dg/cleanup-6.c: New.
            * gcc.dg/cleanup-7.c: New.
    
    From-SVN: r67448

---
+++ gcc/testsuite/gcc.dg/cleanup-1.c
@@ -0,0 +1,36 @@
+/* { dg-do compile } */
+/* { dg-options "-Wall" } */
+/* Validate expected warnings and errors.  */
+
+#define U	__attribute__((unused))
+#define C(x)	__attribute__((cleanup(x)))
+
+static int f1(void *x U) { return 0; }
+static void f2() { }
+static void f3(void) { }
+static void f4(void *x U) { }
+static void f5(int *x U) { }
+static void f6(double *x U) { }
+static void f7(const int *x U) { }
+static void f8(const int *x U, int y U) { }
+static void f9(int x U) { }
+
+void test(void)
+{
+  int o1 C(f1);
+  int o2 C(f2);
+  int o3 C(f3);		/* { dg-error "too many arguments" } */
+  int o4 C(f4);
+  int o5 C(f5);
+  int o6 C(f6);		/* { dg-warning "incompatible pointer type" } */
+  int o7 C(f7);
+  int o8 C(f8);		/* { dg-error "too few arguments" } */
+  int o9 C(f9);		/* { dg-warning "from pointer without a cast" } */
+  int o10 U C(undef);	/* { dg-error "not a function" } */
+  int o11 U C(o1);	/* { dg-error "not a function" } */
+  int o12 U C("f1");	/* { dg-error "not an identifier" } */
+  static int o13 U C(f1); /* { dg-warning "attribute ignored" } */
+}
+
+int o14 C(f1);		/* { dg-warning "attribute ignored" } */
+void t15(int o U C(f1)) {} /* { dg-warning "attribute ignored" } */
---
+++ gcc/testsuite/gcc.dg/cleanup-5.c
@@ -0,0 +1,50 @@
+/* { dg-do run } */
+/* { dg-options "-fexceptions" } */
+/* Verify that cleanups work with exception handling.  */
+
+#include <unwind.h>
+#include <stdlib.h>
+
+static _Unwind_Reason_Code
+force_unwind_stop (int version, _Unwind_Action actions,
+                   _Unwind_Exception_Class exc_class,
+                   struct _Unwind_Exception *exc_obj,
+                   struct _Unwind_Context *context,
+                   void *stop_parameter)
+{
+  if (actions & _UA_END_OF_STACK)
+    abort ();
+  return _URC_NO_REASON;
+}
+
+static void force_unwind ()
+{
+  struct _Unwind_Exception *exc = malloc (sizeof (*exc));
+  exc->exception_class = 0;
+  exc->exception_cleanup = 0;
+                   
+#ifndef __USING_SJLJ_EXCEPTIONS__
+  _Unwind_ForcedUnwind (exc, force_unwind_stop, 0);
+#else
+  _Unwind_SjLj_ForcedUnwind (exc, force_unwind_stop, 0);
+#endif
+                   
+  abort ();
+}
+
+static void handler (void *p __attribute__((unused)))
+{
+  exit (0);
+}
+
+static void doit ()
+{
+  char dummy __attribute__((cleanup (handler)));
+  force_unwind ();
+}
+
+int main()
+{ 
+  doit ();
+  abort ();
+}
