a5ac368a91e1ff65018f05ed7436c9ef578b5d9f
Dan Kegel <dank@kegel.com>
2003-06-06 02:30:59 +0000

    re PR target/10331 ([x86 -> SH] can't compile c++ crosscompiler part of gcc crosscompiler for sh-elf)
    	PR target/10331
    	* config/sh/t-linux (STMP_FIXPROTO): Define.
    
    	PR target/11096
    	* config/sh/linux.h (CPLUSPLUS_CPP_SPEC): Redefine so to include
    	-D_GNU_SOURCE.
    
    Co-Authored-By: Kaz Kojima <kkojima@gcc.gnu.org>
    
    From-SVN: r67525

--- gcc/config/sh/linux.h
+++ gcc/config/sh/linux.h
@@ -52,6 +52,10 @@ do { \
      %{!rpath:-rpath /lib}} \
    %{static:-static}"
 
+/* The GNU C++ standard library requires that these macros be defined.  */
+#undef CPLUSPLUS_CPP_SPEC
+#define CPLUSPLUS_CPP_SPEC "-D_GNU_SOURCE %(cpp)"
+
 #undef LIB_SPEC
 #define LIB_SPEC \
   "%{shared: -lc} \
