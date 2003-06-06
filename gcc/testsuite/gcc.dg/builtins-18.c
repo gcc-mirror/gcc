07bae5ad21964fd180a8f85e300cf823dfa04b53
Roger Sayle <sayle@gcc.gnu.org>
2003-06-06 12:36:26 +0000

    fold-const.c (fold <ABS_EXPR>): Re-fold the result of folding fabs(-x) into fabs(x).
    	* fold-const.c (fold <ABS_EXPR>):  Re-fold the result of folding
    	fabs(-x) into fabs(x).  Use tree_expr_nonnegative_p to determine
    	when the ABS_EXPR (fabs or abs) is not required.
      	(tree_expr_nonnegative_p): Move the logic that sqrt and exp are
    	always nonnegative from fold to here.  Additionally, cabs and fabs
    	are always non-negative, and pow and atan are non-negative if
    	their first argument is non-negative.
    
    	* builtins.c (fold_builtin_cabs): New function to fold cabs{,f,l}.
    	Evaluate cabs of a constant at compile-time.  Convert cabs of a
    	non-complex argument into fabs.  Convert cabs(z) into
    	sqrt(z.r*z.r + z.i*z.i) at the tree-level with -ffast-math or
    	-funsafe-math-optimizations or -ffast-math.
    	(fold_builtin): Convert BUILT_IN_FABS{,F,L} into an ABS_EXPR.
    	Fold BUILT_IN_CABS{,F,L} using fold_builtin_cabs.
    
    	* gcc.dg/builtins-2.c: Add some more tests.
    	* gcc.dg/builtins-18.c: New test case.
    	* gcc.dg/builtins-19.c: New test case.
    
    From-SVN: r67541

---
+++ gcc/testsuite/gcc.dg/builtins-18.c
@@ -0,0 +1,58 @@
+/* Copyright (C) 2003 Free Software Foundation.
+
+   Check that built-in cabs, cabsf and cabsl functions don't
+   break anything and produces the expected results.
+
+   Written by Roger Sayle, 1st June 2003.  */
+
+/* { dg-do link } */
+/* { dg-options "-O2 -ffast-math" } */
+
+extern void link_error(void);
+
+extern float cabsf (float _Complex);
+extern double cabs (double _Complex);
+extern long double cabsl (long double _Complex);
+
+int
+main (void)
+{
+  /* For each type, test both runtime and compile time (constant folding)
+     optimization.  */
+  float _Complex fc = 3.0F + 4.0iF;
+  double _Complex dc = 3.0 + 4.0i;
+  long double _Complex ldc = 3.0L + 4.0iL;
+
+  /* Test floats.  */
+  if (cabsf (fc) != 5.0F)
+    link_error ();
+  if (__builtin_cabsf (fc) != 5.0F)
+    link_error ();
+  if (cabsf (3.0F + 4.0iF) != 5.0F)
+    link_failure ();
+  if (__builtin_cabsf (3.0F + 4.0iF) != 5.0F)
+    link_failure ();
+
+  /* Test doubles.  */
+  if (cabs (dc) != 5.0)
+    link_error ();
+  if (__builtin_cabs (dc) != 5.0)
+    link_error ();
+  if (cabs (3.0 + 4.0i) != 5.0)
+    link_failure ();
+  if (__builtin_cabs (3.0 + 4.0i) != 5.0)
+    link_failure ();
+
+  /* Test long doubles.  */
+  if (cabsl (ldc) != 5.0L)
+    link_error ();
+  if (__builtin_cabsl (ldc) != 5.0L)
+    link_error ();
+  if (cabsl (3.0L + 4.0iL) != 5.0L)
+    link_failure ();
+  if (__builtin_cabsl (3.0L + 4.0iL) != 5.0L)
+    link_failure ();
+
+  return 0;
+}
+
---
+++ gcc/testsuite/gcc.dg/builtins-19.c
@@ -0,0 +1,41 @@
+/* Copyright (C) 2003  Free Software Foundation.
+
+   Check that cabs of a non-complex argument is converted into fabs.
+
+   Written by Roger Sayle, 1st June 2003.  */
+
+/* { dg-do link } */
+/* { dg-options "-O2 -ansi" } */
+
+double cabs (__complex__ double);
+float cabsf (__complex__ float);
+long double cabsl (__complex__ long double);
+
+void link_error (void);
+
+void test (double x)
+{
+  if (cabs (x) != fabs (x))
+    link_error ();
+}
+
+void testf (float x)
+{
+  if (cabsf (x) != fabsf (x))
+    link_error ();
+}
+
+void testl (long double x)
+{
+  if (cabsl (x) != fabsl (x))
+    link_error ();
+}
+
+int main ()
+{
+  test (1.0);
+  testf (1.0f);
+  testl (1.0l);
+  return 0;
+}
+
