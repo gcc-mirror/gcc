960a4fe6e5b33029e023f423b20608584943ba24
Alan Modra <amodra@bigpond.net.au>
2003-06-07 06:21:30 +0000

    * config/rs6000/linux64.h (CRT_CALL_STATIC_FUNCTION): Define.
    From-SVN: r67579

--- gcc/config/rs6000/linux64.h
+++ gcc/config/rs6000/linux64.h
@@ -207,6 +207,18 @@ Boston, MA 02111-1307, USA.  */
 #undef  RS6000_MCOUNT
 #define RS6000_MCOUNT "_mcount"
 
+#ifdef __powerpc64__
+/* _init and _fini functions are built from bits spread across many
+   object files, each potentially with a different TOC pointer.  For
+   that reason, place a nop after the call so that the linker can
+   restore the TOC pointer if a TOC adjusting call stub is needed.  */
+#define CRT_CALL_STATIC_FUNCTION(SECTION_OP, FUNC)	\
+  asm (SECTION_OP "\n"					\
+"	bl ." #FUNC "\n"				\
+"	nop\n"						\
+"	.previous");
+#endif
+
 /* FP save and restore routines.  */
 #undef  SAVE_FP_PREFIX
 #define SAVE_FP_PREFIX "._savef"
