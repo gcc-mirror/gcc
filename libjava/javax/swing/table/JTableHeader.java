bfd7362dba1866334d3ff14f93412ecd2fa6b528
mkoch <mkoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-06-11 16:36:11 +0000

    Forgot to add this file to cvs. Sorry.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@67781 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/javax/swing/table/JTableHeader.java
@@ -0,0 +1,82 @@
+/* JTableHeader.java
+   Copyright (C) 2003 Free Software Foundation, Inc.
+
+This file is part of GNU Classpath.
+
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+Linking this library statically or dynamically with other modules is
+making a combined work based on this library.  Thus, the terms and
+conditions of the GNU General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this library give you
+permission to link this library with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module.  An independent module is a module which is not derived from
+or based on this library.  If you modify this library, you may extend
+this exception to your version of the library, but you are not
+obligated to do so.  If you do not wish to do so, delete this
+exception statement from your version. */
+
+
+package javax.swing.table;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.FocusListener;
+import java.util.Locale;
+import javax.accessibility.Accessible;
+import javax.accessibility.AccessibleComponent;
+import javax.accessibility.AccessibleContext;
+import javax.accessibility.AccessibleRole;
+import javax.accessibility.AccessibleStateSet;
+import javax.swing.JComponent;
+import javax.swing.JTable;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.TableColumnModelEvent;
+import javax.swing.event.TableColumnModelListener;
+import javax.swing.plaf.TableHeaderUI;
+
+public class JTableHeader
+{
+  protected class AccessibleJTableHeader
+  {
+    protected class AccessibleJTableHeaderEntry
+    {
+    }
+  }
+
+  private static final long serialVersionUID = 5144633983372967710L;
+
+  protected TableColumnModel columnModel;
+  protected TableColumn draggedColumn;
+  protected int draggedDistance;
+  protected boolean reorderingAllowed;
+  protected boolean resizingAllowed;
+  protected TableColumn resizingColumn;
+  protected JTable table;
+  protected boolean updateTableInRealTime;
+}
