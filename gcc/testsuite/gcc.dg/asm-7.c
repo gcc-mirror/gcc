65e084edcd1429740bce41942d3987cae6808800
Richard Henderson <rth@redhat.com>
2003-06-11 18:05:48 -0700

    stmt.c (expand_asm_operands): Don't warn for memories with queued addresses.
            * stmt.c (expand_asm_operands): Don't warn for memories with
            queued addresses.
    
    From-SVN: r67809

--- gcc/testsuite/gcc.dg/asm-7.c
+++ gcc/testsuite/gcc.dg/asm-7.c
@@ -7,12 +7,14 @@ void test(void)
   register int r2;
   int i;
   static int m;
+  int *p;
 
   __asm__ ("" : : "m"(r));	/* { dg-warning "address of register" } */
   __asm__ ("" : : "m"(i));
   __asm__ ("" : : "m"(m));
   __asm__ ("" : : "m"(0));	/* { dg-warning "input without lvalue" } */
   __asm__ ("" : : "m"(i+1));	/* { dg-warning "input without lvalue" } */
+  __asm__ ("" : : "m"(*p++));
 
   __asm__ ("" : : "g"(r));
   __asm__ ("" : : "g"(i));
