578f5dbf5d86014a106bdd5cc2f226a052f34a03
wilson <wilson@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-06-12 05:18:12 +0000

    Kaos support from Uwe Stieber * config.gcc (arm*-*-kaos*, i[34567]86-*-kaos*, powerpc-*-kaos*, powerpcle-*-kaos*, strongarm-*-kaos*): New targets. (sh-*-elf*): Add sh*-*-kaos* support. * config/kaos.h, config/arm/kaos-strongarm.h, config/arm/kaos-arm.h, config/i386/kaos-i386.h, config/rs6000/kaos-ppc.h, config/sh/kaos-sh.h: New files.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@67819 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/arm/kaos-arm.h
@@ -0,0 +1,24 @@
+/* Definitions of target machine for GNU compiler.
+   kaOS on arm architecture version.
+   Copyright (C) 2003 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+#undef TARGET_VERSION
+#define TARGET_VERSION fputs (" (ARM/kaOS[ELF])", stderr);
+
