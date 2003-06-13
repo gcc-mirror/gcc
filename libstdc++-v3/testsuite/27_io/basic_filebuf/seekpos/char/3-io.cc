ed24293523ef9e1deb47edfd87349d0724d6cd04
Paolo Carlini <pcarlini@unitus.it>
2003-06-13 23:06:37 +0200

    1.cc: Split and fix for missing seeks between gets and puts into...
    2003-06-13  Paolo Carlini  <pcarlini@unitus.it>
    
    	* testsuite/27_io/basic_filebuf/seekoff/char/1.cc: Split and
    	fix for missing seeks between gets and puts into...
    	* testsuite/27_io/basic_filebuf/seekoff/char/3-io.cc: New.
    	* testsuite/27_io/basic_filebuf/seekoff/char/4-io.cc: New.
    	* testsuite/27_io/basic_filebuf/seekpos/char/1.cc: Same, into...
    	* testsuite/27_io/basic_filebuf/seekpos/char/3-io.cc: New.
    	* testsuite/27_io/basic_filebuf/seekpos/char/4-io.cc: New.
    	* testsuite/27_io/basic_filebuf/sputbackc/char/1-io.cc: Fix
    	for missing seeks between gets and puts.
    	* testsuite/27_io/basic_filebuf/sputbackc/char/2-io.cc: Ditto.
    	* testsuite/data/seekoff-1.tst: New.
    	* testsuite/data/seekoff-2.tst: New.
    	* testsuite/data/seekpos-1.tst: New.
    	* testsuite/data/seekpos-2.tst: New.
    
    From-SVN: r67913

---
+++ libstdc++-v3/testsuite/27_io/basic_filebuf/seekpos/char/3-io.cc
@@ -0,0 +1,98 @@
+// 2001-05-21 Benjamin Kosnik  <bkoz@redhat.com>
+
+// Copyright (C) 2001, 2002, 2003 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 27.8.1.4 Overridden virtual functions
+
+#include <fstream>
+#include <testsuite_hooks.h>
+#include <testsuite_io.h>
+
+// @require@ %-*.tst %-*.txt
+// @diff@ %-*.tst %*.txt
+
+const char name_01[] = "seekpos-1.tst"; // file with data in it
+
+void test05() 
+{
+  using namespace std;
+  using namespace __gnu_cxx_test;
+
+  typedef filebuf::int_type 	int_type;
+  typedef filebuf::pos_type 	pos_type;
+  typedef filebuf::off_type 	off_type;
+
+  bool 				test = true;
+  streamsize 			strmsz_1, strmsz_2;
+  streamoff  			strmof_1, strmof_2;
+
+  int_type c1;
+  int_type c2;
+  int_type c3;
+
+  pos_type pt_1(off_type(-1));
+  pos_type pt_2(off_type(0));
+  pos_type pt_3;
+  off_type off_1 = 0;
+  off_type off_2 = 0;
+
+  // seekpos
+  // pubseekpos(pos_type sp, ios_base::openmode)
+  // alters the stream position to sp
+
+  // in | out
+  {
+    constraint_filebuf fb_03;
+    fb_03.open(name_01, ios_base::out | ios_base::in);
+    VERIFY( fb_03.write_position() );
+    VERIFY( !fb_03.read_position() );
+    pt_1 = fb_03.pubseekoff(78, ios_base::beg);
+    off_1 = pt_1;
+    VERIFY( off_1 > 0 );
+    c1 = fb_03.snextc(); 		//current in pointer +1
+    VERIFY( c1 == 't' );
+    pt_3 = fb_03.pubseekoff(0, ios_base::cur);
+    fb_03.pubseekpos(pt_3);
+    c2 = fb_03.sputc('\n');  	//test current out pointer
+    pt_3 = fb_03.pubseekoff(0, ios_base::cur);
+    fb_03.pubseekpos(pt_3);
+    c3 = fb_03.sgetc();
+    fb_03.pubsync(); 		//resets pointers
+    pt_2 = fb_03.pubseekpos(pt_1);
+    off_2 = pt_2;
+    VERIFY( off_1 == off_2 );
+    c3 = fb_03.snextc(); 		//current in pointer +1
+    VERIFY( c2 == c3 );
+    pt_1 = fb_03.pubseekoff(0, ios_base::end);
+    off_1 = pt_1;
+    VERIFY( off_1 > off_2 );
+    fb_03.sputn("\nof the wonderful things he does!!\nok", 37);
+    fb_03.pubsync();
+    VERIFY( fb_03.write_position() );
+    VERIFY( !fb_03.read_position() );
+    fb_03.close();
+    VERIFY( !fb_03.is_open() );
+  }
+}
+
+main() 
+{
+  test05();
+  return 0;
+}
