81d2d258f5b5578131c9add593df10bd4758f219
Michael Koch <konqueror@gmx.de>
2003-06-14 11:53:02 +0000

    2003-06-14  Michael Koch  <konqueror@gmx.de>
    	* gnu/java/nio/FileChannelImpl.java
            (map_address): Removed incorrect comment.
    	* gnu/java/nio/SelectorImpl.java
            (register): Remove code duplication and code for file channel handling.
    	* gnu/java/nio/ServerSocketChannelImpl.java
            (serverSocket): Renamed from sock_object.
            (ServerSocketChannel): Initialize serverSocket.
            (socket): Return serverSocket.
    	* gnu/java/nio/SocketChannelImpl.java
            (socket): Renamed from sock_object.
            (isConnectionPenging): Simplified.
            (socket): Return socket.
    
    From-SVN: r67940

--- libjava/gnu/java/nio/FileChannelImpl.java
+++ libjava/gnu/java/nio/FileChannelImpl.java
@@ -35,6 +35,7 @@ this exception to your version of the library, but you are not
 obligated to do so.  If you do not wish to do so, delete this
 exception statement from your version. */
 
+
 package gnu.java.nio;
 
 import java.io.EOFException;
@@ -64,8 +65,6 @@ import gnu.gcj.RawData;
 
 public class FileChannelImpl extends FileChannel
 {
-  // GCJ LOCAL: This variable stores a pointer to the memory
-  // where the file is mapped.
   RawData map_address;
   
   int length;
