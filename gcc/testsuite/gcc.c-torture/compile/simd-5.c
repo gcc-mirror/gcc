e4f69bd0e9f4c5c191991be49ec510a07e97a012
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-06-16 19:04:36 +0000

    * gcc.c-torture/compile/simd-5.c: Correct SPARC XFAIL indications. 	Remove spurious PowerPC-64 XFAIL indications.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@68033 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/compile/simd-5.c
+++ gcc/testsuite/gcc.c-torture/compile/simd-5.c
@@ -1,10 +1,10 @@
+/* On SPARC64/SPARC-V9 it fails, except with -m32.  On regular SPARC
+   it doesn't fail, except with -m64.  In other words, this test fails
+   on 64-bit SPARC.  Unfortunately, there's no way to encode that
+   information in the dg framework, so the test will XPASS on 32-bit
+   SPARC.  */
 /* h8300 does not have long long */
-/* PowerPC-64 doesn't handle this; see PR target/9680 */
-/* { dg-do assemble { xfail h8300-*-* powerpc64-*-* } } */
-/* On SPARC64/SPARC-V9 it fails, except with -m32. */
-/* { dg-xfail-if "PR target/9200" { "sparc64-*-*" "sparcv9-*-*" } { "*" } { "-m32" } } */
-/* On regular SPARC it doesn't fail, except with -m64. */
-/* { dg-xfail-if "PR target/9200" { "sparc*-*-*" } { "-m64" } { "" } } */
+/* { dg-excess-errors "PR target/9200" { xfail "sparc*-*-*" "h8300-*-*" } } */
 
 #define vector64 __attribute__((vector_size(8)))
 
