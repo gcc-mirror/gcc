7a79e0cecdf343311188ca8f6dbd0f2febdb1722
mkoch <mkoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-06-17 12:01:37 +0000

    2003-06-17  Michael Koch  <konqueror@gmx.de>
    	* gnu/java/lang/ArrayHelper.java,
    	gnu/java/lang/ClassHelper.java:
    	Reformatted to match classpath's versions.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@68078 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/gnu/java/lang/ClassHelper.java
+++ libjava/gnu/java/lang/ClassHelper.java
@@ -1,5 +1,5 @@
-/* gnu.java.lang.ClassHelper
-   Copyright (C) 1998 Free Software Foundation, Inc.
+/* ClassHelper.java -- Utility methods to augment java.lang.Class
+   Copyright (C) 1998, 2002 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
@@ -7,7 +7,7 @@ GNU Classpath is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2, or (at your option)
 any later version.
- 
+
 GNU Classpath is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -42,202 +42,144 @@ import java.util.*;
 import java.lang.reflect.*;
 
 /**
- ** ClassHelper has various methods that ought to have been
- ** in class.
- **
- ** @author John Keiser
- ** @version 1.1.0, 29 Jul 1998
- **/
+ * ClassHelper has various methods that ought to have been in Class.
+ *
+ * @author John Keiser
+ * @author Eric Blake <ebb9@email.byu.edu>
+ */
+public class ClassHelper
+{
+  /**
+   * Strip the package part from the class name.
+   *
+   * @param clazz the class to get the truncated name from
+   * @return the truncated class name
+   */
+  public static String getTruncatedClassName(Class clazz)
+  {
+    return getTruncatedName(clazz.getName());
+  }
 
-public class ClassHelper {
-	/** Strip the package part from the class name.
-	 ** @param clazz the class to get the truncated name from
-	 ** @return the truncated class name.
-	 **/
-	public static String getTruncatedClassName(Class clazz) {
-		return getTruncatedName(clazz.getName());
-	}
-	/** Strip the package part from the class name, or the
-	 ** class part from the method or field name.
-	 ** @param name the name to truncate.
-	 ** @return the truncated name.
-	 **/
-	public static String getTruncatedName(String name) {
-		int lastInd = name.lastIndexOf('.');
-		if(lastInd == -1) {
-			return name;
-		} else {
-			return name.substring(lastInd+1);
-		}
-	}
+  /**
+   * Strip the package part from the class name, or the class part from
+   * the method or field name.
+   *
+   * @param name the name to truncate
+   * @return the truncated name
+   */
+  public static String getTruncatedName(String name)
+  {
+    int lastInd = name.lastIndexOf('.');
+    if (lastInd == -1)
+      return name;
+    return name.substring(lastInd + 1);
+  }
 
-	/** Strip the last portion of the name (after the last
-	 ** dot).
-	 ** @param name the name to get package of.
-	 ** @return the package name.  "" if no package.
-	 **/
-	public static String getPackagePortion(String name) {
-		int lastInd = name.lastIndexOf('.');
-		if(lastInd == -1) {
-			return "";
-		} else {
-			return name.substring(0,lastInd);
-		}
-	}
+  /**
+   * Strip the last portion of the name (after the last dot).
+   *
+   * @param name the name to get package of
+   * @return the package name, or "" if no package
+   */
+  public static String getPackagePortion(String name)
+  {
+    int lastInd = name.lastIndexOf('.');
+    if (lastInd == -1)
+      return "";
+    return name.substring(0, lastInd);
+  }
 
-	static Hashtable allMethods = new Hashtable();
-	static Hashtable allMethodsAtDeclaration = new Hashtable();
+  /** Cache of methods found in getAllMethods(). */
+  private static Map allMethods = new HashMap();
 
-	/** Get all the methods, public, private and
-	 ** otherwise, from the class, getting them
-	 ** from the most recent class to find them.
-	 **/
-	public static Method[] getAllMethods(Class clazz) {
-		Method[] retval = (Method[])allMethods.get(clazz);
-		if(retval == null) {
-			Method[] superMethods;
-			if(clazz.getSuperclass() != null) {
-				superMethods = getAllMethods(clazz.getSuperclass());
-			} else {
-				superMethods = new Method[0];
-			}
-			Vector v = new Vector();
-			Method[] currentMethods = clazz.getDeclaredMethods();
-			for(int i=0;i<currentMethods.length;i++) {
-				v.addElement(currentMethods[i]);
-			}
-			for(int i=0;i<superMethods.length;i++) {
-				boolean addOK = true;
-				for(int j=0;j<currentMethods.length;j++) {
-					if(getTruncatedName(superMethods[i].getName()).equals(getTruncatedName(currentMethods[j].getName()))
-					   && ArrayHelper.equalsArray(superMethods[i].getParameterTypes(),currentMethods[j].getParameterTypes())) {
-						addOK = false;
-					}
-				}
-				if(addOK) {
-					v.addElement(superMethods[i]);
-				}
-			}
+  /**
+   * Get all the methods, public, private and otherwise, from the class,
+   * getting them from the most recent class to find them. This may not
+   * be quite the correct approach, as this includes methods that are not
+   * inherited or accessible from clazz, so beware.
+   *
+   * @param clazz the class to start at
+   * @return all methods declared or inherited in clazz
+   */
+  public static Method[] getAllMethods(Class clazz)
+  {
+    Method[] retval = (Method[]) allMethods.get(clazz);
+    if (retval == null)
+      {
+        Set methods = new HashSet();
+        Class c = clazz;
+        while (c != null)
+          {
+            Method[] currentMethods = c.getDeclaredMethods();
+          loop:
+            for (int i = 0; i < currentMethods.length; i++)
+              {
+                Method current = currentMethods[i];
+                int size = methods.size();
+                Iterator iter = methods.iterator();
+                while (--size >= 0)
+                  {
+                    Method override = (Method) iter.next();
+                    if (current.getName().equals(override.getName())
+                        && Arrays.equals(current.getParameterTypes(),
+                                         override.getParameterTypes())
+                        && current.getReturnType() == override.getReturnType())
+                      continue loop;
+                  }
+                methods.add(current);
+              }
+            c = c.getSuperclass();
+          }
+        retval = new Method[methods.size()];
+        methods.toArray(retval);
+        allMethods.put(clazz, retval);
+      }
+    return retval;
+  }
 
-			retval = new Method[v.size()];
-			v.copyInto(retval);
-			allMethods.put(clazz,retval);
-		}
-		return retval;
-	}
+  /** Cache of fields found in getAllFields(). */
+  private static Map allFields = new HashMap();
 
-	/** Get all the methods, public, private and
-	 ** otherwise, from the class, and get them from
-	 ** their point of declaration.
-	 **/
-	public static Method[] getAllMethodsAtDeclaration(Class clazz) {
-		Method[] retval = (Method[])allMethodsAtDeclaration.get(clazz);
-		if(retval == null) {
-			Method[] superMethods;
-			if(clazz.getSuperclass() != null) {
-				superMethods = getAllMethodsAtDeclaration(clazz.getSuperclass());
-			} else {
-				superMethods = new Method[0];
-			}
-			Vector v = new Vector();
-			Method[] currentMethods = clazz.getDeclaredMethods();
-			for(int i=0;i<superMethods.length;i++) {
-				v.addElement(superMethods[i]);
-			}
-			for(int i=0;i<superMethods.length;i++) {
-				boolean addOK = true;
-				for(int j=0;j<currentMethods.length;j++) {
-					if(getTruncatedName(superMethods[i].getName()).equals(getTruncatedName(currentMethods[j].getName()))
-					   && ArrayHelper.equalsArray(superMethods[i].getParameterTypes(),currentMethods[j].getParameterTypes())) {
-						addOK = false;
-					}
-				}
-				if(addOK) {
-					v.addElement(superMethods[i]);
-				}
-			}
-
-			retval = new Method[v.size()];
-			v.copyInto(retval);
-			allMethodsAtDeclaration.put(clazz,retval);
-		}
-		return retval;
-	}
-
-	static Hashtable allFields = new Hashtable();
-	static Hashtable allFieldsAtDeclaration = new Hashtable();
-
-	/** Get all the fields, public, private and
-	 ** otherwise, from the class, getting them
-	 ** from the most recent class to find them.
-	 **/
-	public static Field[] getAllFields(Class clazz) {
-		Field[] retval = (Field[])allFields.get(clazz);
-		if(retval == null) {
-			Field[] superFields;
-			if(clazz.getSuperclass() != null) {
-				superFields = getAllFields(clazz.getSuperclass());
-			} else {
-				superFields = new Field[0];
-			}
-			Vector v = new Vector();
-			Field[] currentFields = clazz.getDeclaredFields();
-			for(int i=0;i<currentFields.length;i++) {
-				v.addElement(currentFields[i]);
-			}
-			for(int i=0;i<superFields.length;i++) {
-				boolean addOK = true;
-				for(int j=0;j<currentFields.length;j++) {
-					if(getTruncatedName(superFields[i].getName()).equals(getTruncatedName(currentFields[j].getName()))) {
-						addOK = false;
-					}
-				}
-				if(addOK) {
-					v.addElement(superFields[i]);
-				}
-			}
-
-			retval = new Field[v.size()];
-			v.copyInto(retval);
-			allFields.put(clazz,retval);
-		}
-		return retval;
-	}
-
-	/** Get all the fields, public, private and
-	 ** otherwise, from the class, and get them from
-	 ** their point of declaration.
-	 **/
-	public static Field[] getAllFieldsAtDeclaration(Class clazz) {
-		Field[] retval = (Field[])allFieldsAtDeclaration.get(clazz);
-		if(retval == null) {
-			Field[] superFields;
-			if(clazz.getSuperclass() != null) {
-				superFields = getAllFieldsAtDeclaration(clazz.getSuperclass());
-			} else {
-				superFields = new Field[0];
-			}
-			Vector v = new Vector();
-			Field[] currentFields = clazz.getDeclaredFields();
-			for(int i=0;i<superFields.length;i++) {
-				v.addElement(superFields[i]);
-			}
-			for(int i=0;i<superFields.length;i++) {
-				boolean addOK = true;
-				for(int j=0;j<currentFields.length;j++) {
-					if(getTruncatedName(superFields[i].getName()).equals(getTruncatedName(currentFields[j].getName()))) {
-						addOK = false;
-					}
-				}
-				if(addOK) {
-					v.addElement(superFields[i]);
-				}
-			}
-
-			retval = new Field[v.size()];
-			v.copyInto(retval);
-			allFieldsAtDeclaration.put(clazz,retval);
-		}
-		return retval;
-	}
+  /**
+   * Get all the fields, public, private and otherwise, from the class,
+   * getting them from the most recent class to find them. This may not
+   * be quite the correct approach, as this includes fields that are not
+   * inherited or accessible from clazz, so beware.
+   *
+   * @param clazz the class to start at
+   * @return all fields declared or inherited in clazz
+   */
+  public static Field[] getAllFields(Class clazz)
+  {
+    Field[] retval = (Field[]) allFields.get(clazz);
+    if (retval == null)
+      {
+        Set fields = new HashSet();
+        Class c = clazz;
+        while (c != null)
+          {
+            Field[] currentFields = c.getDeclaredFields();
+          loop:
+            for (int i = 0; i < currentFields.length; i++)
+              {
+                Field current = currentFields[i];
+                int size = fields.size();
+                Iterator iter = fields.iterator();
+                while (--size >= 0)
+                  {
+                    Field override = (Field) iter.next();
+                    if (current.getName().equals(override.getName())
+                        && current.getType() == override.getType())
+                      continue loop;
+                  }
+                fields.add(current);
+              }
+            c = c.getSuperclass();
+          }
+        retval = new Field[fields.size()];
+        fields.toArray(retval);
+        allFields.put(clazz, retval);
+      }
+    return retval;
+  }
 }
