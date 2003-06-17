efc5eec6f356dd9c7028bed9a51190ad5b029eed
Michael Koch <konqueror@gmx.de>
2003-06-17 12:07:56 +0000

    ZipEntry.java, [...]: Reworked imports, only import used classes.
    2003-06-17  Michael Koch  <konqueror@gmx.de>
    
    	* java/util/zip/ZipEntry.java,
    	java/util/zip/ZipFile.java,
    	java/util/zip/ZipInputStream.java,
    	java/util/zip/ZipOutputStream.java:
    	Reworked imports, only import used classes.
    
    From-SVN: r68079

--- libjava/java/util/zip/ZipInputStream.java
+++ libjava/java/util/zip/ZipInputStream.java
@@ -35,11 +35,12 @@ this exception to your version of the library, but you are not
 obligated to do so.  If you do not wish to do so, delete this
 exception statement from your version. */
 
+
 package java.util.zip;
+
 import java.io.EOFException;
 import java.io.InputStream;
 import java.io.IOException;
-import java.util.Enumeration;
 
 /**
  * This is a FilterInputStream that reads the files in an zip archive
