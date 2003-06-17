d32fad6487989bba67e555421ee56832ed167291
Kazu Hirata <kazu@cs.umass.edu>
2003-06-17 17:15:31 +0000

    * config/h8300/h8300-protos.h: Update a comment.
    From-SVN: r68101

--- gcc/config/h8300/h8300-protos.h
+++ gcc/config/h8300/h8300-protos.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler.
-   Hitachi H8/300 version generating coff
+   Hitachi H8/300 version
    Copyright (C) 2000, 2002, 2003 Free Software Foundation, Inc.
    Contributed by Steve Chamberlain (sac@cygnus.com),
    Jim Wilson (wilson@cygnus.com), and Doug Evans (dje@cygnus.com).
