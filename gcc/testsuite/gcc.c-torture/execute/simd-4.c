254d52a91266eda05fa7a0e958bff2d44e4ba34b
J"orn Rennecke <joern.rennecke@superh.com>
2003-06-19 12:57:34 +0000

    simd-4.c (main): Make expected value endian-dependent.
    	* gcc.c-torture/execute/simd-4.c (main): Make expected value
    	endian-dependent.
    
    From-SVN: r68194

--- gcc/testsuite/gcc.c-torture/execute/simd-4.c
+++ gcc/testsuite/gcc.c-torture/execute/simd-4.c
@@ -9,8 +9,11 @@ __ev_convert_s64 (v2si a)
 
 int main()
 {
+  union { long long ll; int i[2] } endianness_test;
+  endianness_test.ll = 1;
+  int little_endian = endianness_test.i[0];
   s64 = __ev_convert_s64 ((v2si){1,0xffffffff});
-  if (s64 != 0x1ffffffffLL)
+  if (s64 != (little_endian ? 0xffffffff00000001LL : 0x1ffffffffLL))
     abort ();
   return 0;
 }
