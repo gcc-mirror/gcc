53a373dd8a9644fb36b2f7c33c7f078bc1df3392
mkoch <mkoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-06-20 12:07:22 +0000

    2003-06-20  Michael Koch  <konqueror@gmx.de>
    	* java/net/URLStreamHandler.java
    	(hostsEqual): Rewritten.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@68260 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/net/URLStreamHandler.java
+++ libjava/java/net/URLStreamHandler.java
@@ -380,12 +380,20 @@ public abstract class URLStreamHandler
    * @exception UnknownHostException If an unknown host is found
    */
   protected boolean hostsEqual (URL url1, URL url2)
-    throws UnknownHostException
   {
-    InetAddress addr1 = InetAddress.getByName (url1.getHost ());
-    InetAddress addr2 = InetAddress.getByName (url2.getHost ());
+    InetAddress addr1 = getHostAddress (url1);
+    InetAddress addr2 = getHostAddress (url2);
 
-    return addr1.equals (addr2);
+    if (addr1 != null || addr2 != null)
+      return addr1.equals (addr2);
+
+    String host1 = url1.getHost();
+    String host2 = url2.getHost();
+    
+    if (host1 != null && host2 != null)
+      return host1.equalsIgnoreCase (host2);
+
+    return host1 == null && host2 == null;
   }
 
   /**
