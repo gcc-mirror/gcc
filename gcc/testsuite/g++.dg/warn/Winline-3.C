5b90de76509696c04edd0e0e2a5f6e7bbdbe2b4b
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-06-20 19:55:29 +0000

    PR c++/10888 	* tree-inline.c (expand_call_inline): Do not warn about failing to 	inline functions declared in system headers. 	* doc/invoke.texi (-Winline): Expand on documentation.
    	PR c++/10888
    	* g++.dg/warn/Winline-3.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@68281 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/warn/Winline-3.C
@@ -0,0 +1,10 @@
+// { dg-options "-Winline -O" }
+
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+  vector<int> v(10);
+}
--- gcc/tree-inline.c
+++ gcc/tree-inline.c
@@ -1201,7 +1201,8 @@ expand_call_inline (tp, walk_subtrees, data)
        || !cgraph_global_info (fn)->inline_once)
       && !inlinable_function_p (fn, id, 0))
     {
-      if (warn_inline && DECL_INLINE (fn) && !DID_INLINE_FUNC (fn))
+      if (warn_inline && DECL_INLINE (fn) && !DID_INLINE_FUNC (fn)
+	  && !DECL_IN_SYSTEM_HEADER (fn))
 	{
 	  warning_with_decl (fn, "inlining failed in call to `%s'");
 	  warning ("called from here");
