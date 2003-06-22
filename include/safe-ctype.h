b60500ad5a5c373761b4a8ea73ca980c75229dc7
zack <zack@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-06-22 15:59:49 +0000

    include: 	* safe-ctype.h (HC_UNKNOWN, HC_ASCII, HC_EBCDIC): Rename to 	HOST_CHARSET_UNKNOWN, HOST_CHARSET_ASCII, HOST_CHARSET_EBCDIC 	respectively. libiberty: 	* safe-ctype.c: Use HOST_CHARSET_ASCII and HOST_CHARSET_EBCDIC, 	not HC_ASCII and HC_EBCDIC. 	Add documentation in form expected by gather-docs. 	* hex.c: Use HOST_CHARSET, not hand-coded check of character set. 	* Makefile.in, functions.texi: Regenerate. gcc: 	* config/i370/i370.c, config/i370/i370.h: Use HOST_CHARSET_ASCII 	and HOST_CHARSET_EBCDIC, not HC_ASCII and HC_EBCDIC.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@68335 138bc75d-0d04-0410-961f-82ee72b054a4

--- include/safe-ctype.h
+++ include/safe-ctype.h
@@ -40,19 +40,19 @@ Boston, MA 02111-1307, USA.  */
 #endif
 
 /* Determine host character set.  */
-#define HC_UNKNOWN 0
-#define HC_ASCII   1
-#define HC_EBCDIC  2
+#define HOST_CHARSET_UNKNOWN 0
+#define HOST_CHARSET_ASCII   1
+#define HOST_CHARSET_EBCDIC  2
 
 #if  '\n' == 0x0A && ' ' == 0x20 && '0' == 0x30 \
    && 'A' == 0x41 && 'a' == 0x61 && '!' == 0x21
-#  define HOST_CHARSET HC_ASCII
+#  define HOST_CHARSET HOST_CHARSET_ASCII
 #else
 # if '\n' == 0x15 && ' ' == 0x40 && '0' == 0xF0 \
    && 'A' == 0xC1 && 'a' == 0x81 && '!' == 0x5A
-#  define HOST_CHARSET HC_EBCDIC
+#  define HOST_CHARSET HOST_CHARSET_EBCDIC
 # else
-#  define HOST_CHARSET HC_UNKNOWN
+#  define HOST_CHARSET HOST_CHARSET_UNKNOWN
 # endif
 #endif
 
