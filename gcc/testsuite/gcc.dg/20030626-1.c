6dc208a1ed88e40e359173ece8a3f9441001fd6c
Eric Botcazou <ebotcazou@libertysurf.fr>
2003-06-26 12:37:35 +0200

    re PR rtl-optimization/11210 (optimizer drops conditional with typecast from signed to unsigned char)
    	PR optimization/11210
    	* fold-const (decode_field_reference): Strip only NOPs that
    	don't affect the sign.
    
    From-SVN: r68524

---
+++ gcc/testsuite/gcc.dg/20030626-1.c
@@ -0,0 +1,18 @@
+/* PR optimization/11210 */
+/* Originator: Guido Classen <guido@clagi.de> */
+/* Reduced testcase by Falk Hueffner <falk@debian.org> */
+/* { dg-do compile } */
+/* { dg-options "-O" } */
+
+/* Verify that the constant expressions folder doesn't
+   throw away the cast operation in the comparison.  */
+
+struct str { 
+  int head; 
+  char data[8];
+};
+
+int foo(struct str t)
+{
+  return t.data[0] || (unsigned char) t.data[2] != 130; /* { dg-bogus "comparison is always 1" } */
+}
