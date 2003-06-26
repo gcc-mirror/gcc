4c43bcb0e1abcab77882342071cc039183cbeef6
Joern Rennecke <amylaar@gcc.gnu.org>
2003-06-26 14:10:55 +0100

    Added missing semicolon at end of union.
    From-SVN: r68529

--- gcc/testsuite/gcc.c-torture/execute/simd-4.c
+++ gcc/testsuite/gcc.c-torture/execute/simd-4.c
@@ -9,7 +9,7 @@ __ev_convert_s64 (v2si a)
 
 int main()
 {
-  union { long long ll; int i[2] } endianness_test;
+  union { long long ll; int i[2]; } endianness_test;
   endianness_test.ll = 1;
   int little_endian = endianness_test.i[0];
   s64 = __ev_convert_s64 ((v2si){1,0xffffffff});
