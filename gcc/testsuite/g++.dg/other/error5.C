5806e0404cd14a22f9575ce24095766c4f8f495f
Giovanni Bajo <giovannibajo@libero.it>
2003-06-30 19:07:07 +0200

    re PR c++/11106 (Error message gives partially mangled operator name)
    2003-06-30  Giovanni Bajo <giovannibajo@libero.it>
    
            PR c++/11106
            * g++.dg/other/error5.C: New test.
    
    From-SVN: r68712

---
+++ gcc/testsuite/g++.dg/other/error5.C
@@ -0,0 +1,13 @@
+// { dg-do compile }
+// Origin: Wolfgang Bangerth <bangerth@ices.utexas.edu>
+// PR c++/11106: Error message gives partially mangled operator name
+
+template <typename T> struct S {
+    struct I {};
+};
+
+template <typename T> struct S2 : S<T> {
+    using S<T>::operator typename S<T>::I*; // { dg-error "operator S\\<T\\>" "" }
+};
+
+template struct S2<int>;  // { dg-error "instantiated" "" }
