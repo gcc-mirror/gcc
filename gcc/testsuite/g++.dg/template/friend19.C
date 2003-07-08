e7d2adb9f64cf5b3fd75424d865b0c47f060ad66
lerdsuwa <lerdsuwa@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-07-08 15:35:53 +0000

    PR c++/11030 	* pt.c (instantiate_class_template): Don't call xref_tag to 	inject name when the friend class is a specialization.
    	* g++.dg/template/friend19.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@69088 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/friend19.C
@@ -0,0 +1,28 @@
+// { dg-do compile }
+
+// Origin: Benjamin Li <benxbli@yahoo.com>
+
+// PR c++/11030: Template substition of friend class that is
+// a specialization.
+
+template <int S>
+struct A
+{
+  void func(void);
+};
+
+template <class T>
+class C
+{
+  static void private_func(void) {}
+public:
+  friend class A<512>;
+};
+
+template <int S>
+void A<S>::func(void)
+{
+  C<void>::private_func();
+}
+
+template class A<512>;
