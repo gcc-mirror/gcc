c6976b580a332f8b261546e8f8bcb784a183fbf8
zack <zack@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-07-08 05:55:11 +0000

    top: 	* Makefile.tpl (configure-gcc): Depend on maybe-configure-intl. 	* Makefile.in: Regenerate. config: 	* gettext.m4: Delete all former contents. 	(ZW_GNU_GETTEXT_SISTER_DIR): New macro. 	* progtest.m4: New file. gcc: 	* Makefile.in (top_builddir): Set to "..", not ".". 	(INTLLIBS, INTLDEPS): Delete. 	(LIBINTL, LIBINTL_DEP, LIBICONV_DEP): New variables to be substituted. 	(LIBDEPS): Add $(LIBICONV_DEP). 	(LIBS): Take out $(INTLLIBS), add $(LIBINTL) and $(LIBICONV). 	(INCLUDES): Replace -I../intl with @INCINTL@. 	($(top_builddir)/intl/libintl.a): Delete rule. 	(stage2-start, stage3-start, stage4-start, stageprofile-start, 	stagefeedback-start): Use $$ for variable to be evaluated by 	shell, not make. 	* acconfig.h (ENABLE_NLS, HAVE_CATGETS, HAVE_GETTEXT, 	HAVE_LC_MESSAGES, HAVE_STPCPY): Delete. 	* aclocal.m4: sinclude ../config/progtest.m4.  Add 	contents of lcmessage.m4 from gettext distro. 	* configure.in: Check for wchar.h and setlocale.  Set 	LIBICONV_DEP to the empty string and substitute it. 	Call AM_LC_MESSAGES.  Delete AC_ARG_ENABLE for --enable-nls; 	this is handled elsewhere.  Use ZW_GNU_GETTEXT_SISTER_DIR, 	not CY_GNU_GETTEXT.  Clear $LIBICONV if its text is included 	in $LIBINTL, to avoid linking it twice. 	* configure, config.in: Regenerate. intl: 	* README: Update. 	* Makefile.in (INSTALL, INSTALL_DATA, MKINSTALLDIRS, 	mkinstalldirs, gettextsrcdir, l): Delete. 	(COMPILE): Add $(DEFS-$@), remove $(XCFLAGS). 	(HEADERS): libgnuintl.h not libgnuintl.h.in.  Remove os2compat.h. 	(SOURCES): Remove os2compat.c. 	(DEFS-dcigettext.o, DEFS-localealias.o, DEFS-localcharset.o, 	DEFS-relocatable.o): New. 	(all-yes): Add config.intl. 	(libintl.h): Use cp, not cat. 	(INCLUDES): Remove -I.. 	(TAGS, CTAGS, ID): Word wrap. 	(mostlyclean, distclean): Remove junk. 	(config.intl): New rule. 	* aclocal.m4: sinclude ../config/progtest.m4 instead of 	including it inline. 	* config.intl.in: New file. 	* configure.in: Take out unnecessary AC_CONFIG_AUX_DIR. 	Take out AC_DEFINEs for LOCALEDIR, LOCALE_ALIAS_PATH, 	LIBDIR, INSTALLDIR.  Set LIBINTL_DEP and INCINTL and AC_SUBST 	them.  Add config.intl to AC_OUTPUT. 	* os2compat.c, os2compat.h: Delete, unused.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@69071 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/acconfig.h
+++ gcc/acconfig.h
@@ -1,18 +1,3 @@
-/* Define to 1 if NLS is requested.  */
-#undef ENABLE_NLS
-
-/* Define as 1 if you have catgets and don't want to use GNU gettext.  */
-#undef HAVE_CATGETS
-
-/* Define as 1 if you have gettext and don't want to use GNU gettext.  */
-#undef HAVE_GETTEXT
-
-/* Define if your locale.h file contains LC_MESSAGES.  */
-#undef HAVE_LC_MESSAGES
-
-/* Define as 1 if you have the stpcpy function.  */
-#undef HAVE_STPCPY
-
 /* Define if your assembler supports specifying the maximum number
    of bytes to skip when using the GAS .p2align command.  */
 #undef HAVE_GAS_MAX_SKIP_P2ALIGN
