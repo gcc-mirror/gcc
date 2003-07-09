0b47487a7bc80ce16026635bfccf952957b81d28
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-07-09 03:02:17 +0000

    * gcc.c-torture/compile/20030704-1.c: Add a comment.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@69122 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/compile/20030704-1.c
+++ gcc/testsuite/gcc.c-torture/compile/20030704-1.c
@@ -1,5 +1,11 @@
 /* PR c/11428.  */
 
+/* fold_single_bit_test() failed to return a tree of the type that the
+   outer expression was looking for.  Specifically, it returned a tree
+   whose type corresponded to QImode for !p->m, but the desired result
+   type was int, which corresponded to SImode.  emit_move_insn() later
+   tried to copy a reg:QI to reg:SI, causing an ICE.  */
+
 struct s {
   int m : 1;
 };
