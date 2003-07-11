1fa7ba3eaafdacad0339ac6a767d2e28a3f290f7
Danny Smith <dannysmith@users.sourceforge.net>
2003-07-11 12:14:42 +0000

    Backport PRs 5287, 7910, 9738, 11021
    	Backport from mainline.
    
    	2003-05-21  Danny Smith  <dannysmith@users.sourceforge.net>
    
    	PR c++/9738
    	* g++.dg/ext/dllimport2.C: New file.
    	* g++.dg/ext/dllimport3.C: New file.
    
    	2003-07-04  Danny Smith  <dannysmith@users.sourceforge.net>
    
    	PR c++/5287, PR c++/7910,  PR c++/11021
    	* testsuite/g++.dg/ext/dllimport1.C: Add mingw32 as target. Add
    	tests for warnings.
    	* g++.dg/ext/dllimport2.C: Add tests for warnings.
    	* g++.dg/ext/dllimport3.C: Likewise.
    	* g++.dg/ext/dllimport4.C: New file.
    	* g++.dg/ext/dllimport5.C: New file.
    	* g++.dg/ext/dllimport6.C: New file.
    	* g++.dg/ext/dllimport7.C: New file.
    	* g++.dg/ext/dllimport8.C: New file.
    	* g++.dg/ext/dllimport9.C: New file.
    	* g++.dg/ext/dllimport10.C: New file.
    	* g++.dg/ext/dllexport1.C: New file.
    
    From-SVN: r69233

--- gcc/testsuite/g++.dg/ext/dllimport1.C
+++ gcc/testsuite/g++.dg/ext/dllimport1.C
@@ -1,11 +1,19 @@
-// { dg-do compile { target i?86-*-cygwin* } }
+//  PR c++/7910
+// { dg-do compile { target i?86-*-cygwin* i?86-*-mingw*} }
+// { dg-options { -Wall -W } }
 
 class __attribute__((dllimport)) Foo
 {
  public:
   virtual void dummy_foo_func(void)
-    {}
+    {}	// { dg-warning "inline function" }
+  void Foo::dummy_foo_fun2();
+  virtual ~Foo();  //  avoid warning  
 };
+
+void Foo::dummy_foo_fun2()
+{	//  { dg-warning "defined" }
+}
 
 class Bar : public Foo
 {
@@ -19,3 +27,5 @@ Bar::~Bar()
 
 void Bar::dummy_bar_func()
 {}
+
+// { dg-final { scan-assembler-not "__imp___ZN3Foo14dummy_foo_fun" } }
