1dc9efda9178487c316f04470064772aa59acc1c
Kazu Hirata <kazu@cs.umass.edu>
2003-07-11 03:28:56 +0000

    * gcc.dg/20021018-1.c: Disable if the sizeof (int) < 4.
    From-SVN: r69210

--- gcc/testsuite/gcc.dg/20021018-1.c
+++ gcc/testsuite/gcc.dg/20021018-1.c
@@ -1,10 +1,10 @@
 /* { dg-do run } */
 /* { dg-options "-O2 -fpic" } */
-/* { dg-error "overflow" "16-bit target" { target xstormy16-*-* } 0 } */
 
 extern void abort (void);
 extern void exit (int);
 
+#if __INT_MAX__ >= 2147483647L
 static const long foo [10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 long __attribute__((noinline))
@@ -20,3 +20,10 @@ main (void)
     abort ();
   exit (0);
 }
+#else
+int
+main (void)
+{
+  exit (0);
+}
+#endif
