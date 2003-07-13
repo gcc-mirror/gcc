c34a67b6043927de03cbbecca3a68d745ffd0b18
mkoch <mkoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-07-13 15:00:20 +0000

    2003-07-13  Michael Koch  <konqueror@gmx.de>
    	* gnu/java/locale/LocaleInformation_de.java
    	* gnu/java/locale/LocaleInformation_en.java
    	* gnu/java/locale/LocaleInformation_nl.java
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@69289 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/gnu/java/locale/LocaleInformation_nl.java
+++ libjava/gnu/java/locale/LocaleInformation_nl.java
@@ -62,7 +62,7 @@ public class LocaleInformation_nl extends ListResourceBundle
   */
 private static final String collation_rules = 
   "<0<1<2<3<4<5<6<7<8<9<A,a<b,B<c,C<d,D<e,E<f,F<g,G<h,H<i,I<j,J<k,K" +
-  "<l,L<m,M<n,N<o,O<p,P<q,Q<r,R<s,S<t,T<u,U<v,V<w,W<x,X<y,Y,z<Z";
+  "<l,L<m,M<n,N<o,O<p,P<q,Q<r,R<s,S<t,T<u,U<v,V<w,W<x,X<y,Y<z,Z";
 
 /**
   * This is the list of months, fully spelled out
