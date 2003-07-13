b51bd761df741877bc701fc9668e1f8dc8cbbc97
mkoch <mkoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-07-13 16:53:05 +0000

    2003-07-13  Michael Koch  <konqueror@gmx.de>
    	* gnu/java/nio/FileChannelImpl.java,
    	gnu/java/nio/natFileChannelImpl.cc: Removed.
    	* java/io/FileInputStream.java,
    	java/io/FileOutputStream.java,
    	java/io/RandomAccessFile.java,
    	java/nio/MappedByteBufferImpl.java:
    	Import java.nio.channels.FileChannelImpl instead of
    	gnu.java.nio.FileChannelImpl.
    	* java/nio/channels/FileChannelImpl.java,
    	java/nio/channels/natFileChannelImpl.cc:
    	New files.
    	* Makefile.am
    	(ordinary_java_source_files):
    	Removed gnu/java/nio/FileChannelImpl.java and added
    	java/nio/channels/FileChannelImpl.java.
    	(nat source_files):
    	Removed gnu/java/nio/natFileChannelImpl.cc and added
    	java/nio/channels/natFileChannelImpl.cc.
    	* Makefile.in: Regenerated.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@69296 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/nio/channels/natFileChannelImpl.cc
+++ libjava/java/nio/channels/natFileChannelImpl.cc
@@ -25,70 +25,70 @@ details.  */
 #endif
 
 #include <gnu/gcj/RawData.h>
-#include <gnu/java/nio/FileChannelImpl.h>
 #include <java/io/FileDescriptor.h>
 #include <java/io/IOException.h>
 #include <java/nio/ByteBuffer.h>
 #include <java/nio/channels/FileChannel.h>
+#include <java/nio/channels/FileChannelImpl.h>
 
 jlong
-gnu::java::nio::FileChannelImpl::size ()
+java::nio::channels::FileChannelImpl::size ()
 {
   return fd->getLength ();
 }
 
 jlong
-gnu::java::nio::FileChannelImpl::implPosition ()
+java::nio::channels::FileChannelImpl::implPosition ()
 {
   return fd->getFilePointer ();
 }
 
 java::nio::channels::FileChannel*
-gnu::java::nio::FileChannelImpl::implPosition (jlong newPosition)
+java::nio::channels::FileChannelImpl::implPosition (jlong newPosition)
 {
   fd->seek (newPosition, ::java::io::FileDescriptor::SET, true);
   return this;
 }
 
 jint
-gnu::java::nio::FileChannelImpl::implRead (JArray<jbyte>* buffer,
-                                           jint offset, jint len)
+java::nio::channels::FileChannelImpl::implRead (JArray<jbyte>* buffer,
+                                                jint offset, jint len)
 {
   return fd->read (buffer, offset, len);
 }
 
 jint
-gnu::java::nio::FileChannelImpl::implWrite (JArray<jbyte>* buffer,
-                                            jint offset, jint len)
+java::nio::channels::FileChannelImpl::implWrite (JArray<jbyte>* buffer,
+                                                 jint offset, jint len)
 {
   fd->write (buffer, offset, len);
   return len;
 }
 
 java::nio::channels::FileChannel*
-gnu::java::nio::FileChannelImpl::implTruncate (jlong size)
+java::nio::channels::FileChannelImpl::implTruncate (jlong size)
 {
   fd->setLength (size);
   return this;
 }
 
 gnu::gcj::RawData*
-gnu::java::nio::FileChannelImpl::nio_mmap_file (jlong /*pos*/, jlong /*size*/,
-                                                jint /*mode*/)
+java::nio::channels::FileChannelImpl::nio_mmap_file (jlong /*pos*/, jlong /*size*/,
+                                                     jint /*mode*/)
 {
   throw new ::java::io::IOException (JvNewStringUTF ("mmap not implemented"));
 }
 
 void
-gnu::java::nio::FileChannelImpl::nio_unmmap_file (gnu::gcj::RawData* /*address*/,
-						  jint /*size*/)
+java::nio::channels::FileChannelImpl::nio_unmmap_file (gnu::gcj::RawData* /*address*/,
+				                       jint /*size*/)
 {
   throw new ::java::io::IOException (JvNewStringUTF ("munmap not implemented"));
 }
 
 void
-gnu::java::nio::FileChannelImpl::nio_msync (gnu::gcj::RawData* /*map_address*/,
-					    jint /*length*/)
+java::nio::channels::FileChannelImpl::nio_msync (gnu::gcj::RawData* /*map_address*/,
+                                                 jint /*length*/)
 {
   throw new ::java::io::IOException (JvNewStringUTF ("msync not implemented"));
 }
