c61dce3a814a535c2d8c0ce1b8d7dd7e476116f8
Gabriel Dos Reis <gdr@integrable-solutions.net>
2003-07-16 01:43:26 +0000

    re PR c++/10903 ([3.3 only] g++ says: "error: object ``type_decl' not supported by dump_expr")
            PR c++/10903
            * pt.c (convert_nontype_argument): Fix thinko in diagnostic.
            Improve.
    
    From-SVN: r69435

---
+++ gcc/testsuite/g++.dg/template/non-type-template-argument-1.C
@@ -0,0 +1,12 @@
+struct A { static const bool b=false; };
+
+struct B { typedef A X; };
+
+template <bool> struct C {};
+
+template <typename T> struct D
+{
+   C<T::X> c;                   // { dg-error "invalid use" }
+};
+
+D<B> d;                         // { dg-error "" }
