1b5e83e3125575a01fe598726438f4861f2ba7d4
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-07-16 18:23:32 +0000

    2003-07-16  Andrew Pinski  <pinskia@physics.uc.edu>
            PR target/11008
            * gcc.dg/i386-pentium4-not-mull.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@69469 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/i386-pentium4-no-mull.c
@@ -0,0 +1,9 @@
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -march=pentium4" } */
+/* { dg-final { scan-assembler-not "imull" } } */
+
+/* Should be done not using imull.  */
+int t(int x)
+{
+  return x*29;
+}
