8fa26784d1ab239d01fc1f5ab8cd293c8cb5f06d
fitzsim <fitzsim@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-07-23 22:26:35 +0000

    * gnu/java/awt/peer/gtk/GtkTextComponentPeer.java 	(connectHooks): New method. 	(handleEvent): Remove. 	* gnu/java/awt/peer/gtk/GtkTextFieldPeer.java 	(createHooks): Remove declaration. 	* jni/gtk-peer/gnu_java_awt_peer_gtk_GtkEvents.c 	(generates_key_typed_event): Change to handle only certain 	keyvals. 	(awt_event_handler): Add special handling for GtkTextView. 	* jni/gtk-peer/gnu_java_awt_peer_gtk_GtkTextComponentPeer.c 	(textcomponent_commit_cb): New function. 	(textcomponent_changed_cb): Likewise. 	* jni/gtk-peer/gnu_java_awt_peer_gtk_GtkTextFieldPeer.c 	(connectHooks): Remove.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@69728 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/gnu/java/awt/peer/gtk/GtkTextFieldPeer.java
+++ libjava/gnu/java/awt/peer/gtk/GtkTextFieldPeer.java
@@ -52,7 +52,6 @@ public class GtkTextFieldPeer extends GtkTextComponentPeer
 //    native void create (ComponentPeer parent, String text);
 
   native void create ();
-  native void createHooks ();
 
   native void gtkEntryGetSize (int cols, int dims[]);
 
