4b7f0943a248a88ce11c339ac4eefcab186dfe5b
austern <austern@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-07-25 05:32:23 +0000

    * /include/bits/char_traits.h (class char_traits): Put all the 	real work into the new class template __gnu_cxx::char_traits. 	Gave generic definitions for member functions.  Types are taken 	from the new class template __gnu_cxx::_Char_types. 	* testsuite/21_strings/char_traits/requirements/short/1.cc: New 	file.  Test of std::char_traits<short>, which serves as a test of 	the char_traits primary template.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@69772 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/char_traits.h
+++ libstdc++-v3/include/bits/char_traits.h
@@ -43,7 +43,169 @@
 #pragma GCC system_header
 
 #include <cstring>            // For memmove, memset, memchr
+#include <bits/stl_algobase.h>// For copy, lexicographical_compare, fill_n
 #include <bits/fpos.h>        // For streampos
+
+namespace __gnu_cxx
+{
+
+  /**
+   *  @brief  Mapping from character type to associated types.
+   * 
+   *
+   *  @note This is an implementation class for the generic version
+   *  of char_traits.  It defines int_type, off_type, pos_type, and
+   *  state_type.  By default these are unsigned long, streamoff,
+   *  streampos, and mbstate_t.  Users who need a different set of 
+   *  types, but who don't need to change the definitions of any function
+   *  defined in char_traits, can specialize __gnu_cxx::_Char_types
+   *  while leaving __gnu_cxx::char_traits alone. */
+  template <class _CharT>
+    struct _Char_types
+    {
+      typedef unsigned long   int_type;
+      typedef std::streampos  pos_type;
+      typedef std::streamoff  off_type;
+      typedef std::mbstate_t  state_type;
+    };
+
+
+  /**
+   *  @brief  Base class used to implement std::char_traits.
+   *
+   *  @note For any given actual character type, this definition is
+   *  probably wrong.  (Most of the member functions are likely to be
+   *  right, but the int_type and state_type typedefs, and the eof()
+   *  member function, are likely to be wrong.)  The reason this class
+   *  exists is so users can specialize it.  Classes in namespace std
+   *  may not be specialized for fundamentl types, but classes in
+   *  namespace __gnu_cxx may be.
+   *
+   *  See http://gcc.gnu.org/onlinedocs/libstdc++/21_strings/howto.html#5
+   *  for advice on how to make use of this class for "unusual" character
+   *  types. Also, check out include/ext/pod_char_traits.h.  */
+  template<typename _CharT>
+    struct char_traits
+    {
+      typedef _CharT                                    char_type;
+      typedef typename _Char_types<_CharT>::int_type    int_type;
+      typedef typename _Char_types<_CharT>::pos_type    pos_type;
+      typedef typename _Char_types<_CharT>::off_type    off_type;
+      typedef typename _Char_types<_CharT>::state_type  state_type;
+      
+      static void 
+      assign(char_type& __c1, const char_type& __c2)
+      { __c1 = __c2; }
+
+      static bool 
+      eq(const char_type& __c1, const char_type& __c2)
+      { return __c1 == __c2; }
+
+      static bool 
+      lt(const char_type& __c1, const char_type& __c2)
+      { return __c1 < __c2; }
+
+      static int 
+      compare(const char_type* __s1, const char_type* __s2, std::size_t __n);
+
+      static std::size_t
+      length(const char_type* __s);
+
+      static const char_type* 
+      find(const char_type* __s, std::size_t __n, const char_type& __a);
+
+      static char_type* 
+      move(char_type* __s1, const char_type* __s2, std::size_t __n);
+
+      static char_type* 
+      copy(char_type* __s1, const char_type* __s2, std::size_t __n);
+
+      static char_type* 
+      assign(char_type* __s, std::size_t __n, char_type __a);
+
+      static char_type 
+      to_char_type(const int_type& __c)
+      { return static_cast<char_type>(__c); }
+
+      static int_type 
+      to_int_type(const char_type& __c)
+      { return static_cast<int_type>(__c); }
+
+      static bool 
+      eq_int_type(const int_type& __c1, const int_type& __c2)
+      { return __c1 == __c2; }
+
+      static int_type 
+      eof()
+      { return static_cast<int_type>(EOF); }
+
+      static int_type 
+      not_eof(const int_type& __c)
+      { return !eq_int_type(__c, eof()) ? __c : char_type(); }
+    };
+
+  template<typename _CharT>
+    int
+    char_traits<_CharT>::
+    compare(const char_type* __s1, const char_type* __s2, std::size_t __n)
+    {
+      for (size_t __i = 0; __i < __n; ++__i)
+	if (lt(__s1[__i], __s2[__i]))
+	  return -1;
+	else if (lt(__s2[__i], __s1[__i]))
+	  return 1;
+      return 0;
+    }
+
+  template<typename _CharT>
+    std::size_t
+    char_traits<_CharT>::
+    length(const char_type* __p)
+    {
+      std::size_t __i = 0;
+      while (!eq(__p[__i], char_type()))
+        ++__i;
+      return __i;
+    }
+
+  template<typename _CharT>
+    const typename char_traits<_CharT>::char_type* 
+    char_traits<_CharT>::
+    find(const char_type* __s, std::size_t __n, const char_type& __a)
+    {
+      for (std::size_t __i = 0; __i < __n; ++__i)
+        if (eq(__s[__i], __a))
+          return __s + __i;
+      return 0;
+    }
+
+  template<typename _CharT>
+    typename char_traits<_CharT>::char_type*
+    char_traits<_CharT>::
+    move(char_type* __s1, const char_type* __s2, std::size_t __n)
+    {
+      return static_cast<_CharT*>(std::memmove(__s1, __s2,
+					       __n * sizeof(char_type)));
+    }
+
+  template<typename _CharT>
+    typename char_traits<_CharT>::char_type* 
+    char_traits<_CharT>::
+    copy(char_type* __s1, const char_type* __s2, std::size_t __n)
+    {
+      std::copy(__s2, __s2 + __n, __s1);
+      return __s1;
+    }
+
+  template<typename _CharT>
+    typename char_traits<_CharT>::char_type* 
+    char_traits<_CharT>::
+    assign(char_type* __s, std::size_t __n, char_type __a)
+    {
+      std::fill_n(__s, __n, __a);
+      return __s;
+    }
+}
 
 namespace std 
 {
@@ -52,7 +214,9 @@ namespace std
    *  @brief  Basis for explicit traits specializations.
    *
    *  @note  For any given actual character type, this definition is
-   *  probably wrong.
+   *  probably wrong.  Since this is just a thin wrapper around
+   *  __gnu_cxx::char_traits, it is possible to achieve a more
+   *  appropriate definition by specializing __gnu_cxx::char_traits.
    *
    *  See http://gcc.gnu.org/onlinedocs/libstdc++/21_strings/howto.html#5
    *  for advice on how to make use of this class for "unusual" character
@@ -60,66 +224,19 @@ namespace std
   */
   template<class _CharT>
     struct char_traits
-    {
-      typedef _CharT 		char_type;
-      typedef unsigned long  	int_type;
-      typedef streampos 	pos_type;
-      typedef streamoff 	off_type;
-      typedef mbstate_t 	state_type;
-      
-      static void 
-      assign(char_type& __c1, const char_type& __c2);
-
-      static bool 
-      eq(const char_type& __c1, const char_type& __c2);
-
-      static bool 
-      lt(const char_type& __c1, const char_type& __c2);
-
-      static int 
-      compare(const char_type* __s1, const char_type* __s2, size_t __n);
-
-      static size_t
-      length(const char_type* __s);
-
-      static const char_type* 
-      find(const char_type* __s, size_t __n, const char_type& __a);
-
-      static char_type* 
-      move(char_type* __s1, const char_type* __s2, size_t __n);
-
-      static char_type* 
-      copy(char_type* __s1, const char_type* __s2, size_t __n);
-
-      static char_type* 
-      assign(char_type* __s, size_t __n, char_type __a);
-
-      static char_type 
-      to_char_type(const int_type& __c);
-
-      static int_type 
-      to_int_type(const char_type& __c);
-
-      static bool 
-      eq_int_type(const int_type& __c1, const int_type& __c2);
-
-      static int_type 
-      eof(); 
-
-      static int_type 
-      not_eof(const int_type& __c);
-    };
+      : public __gnu_cxx::char_traits<_CharT>
+    { };
 
 
   /// 21.1.3.1  char_traits specializations
   template<>
     struct char_traits<char>
     {
-      typedef char 		char_type;
-      typedef int 	        int_type;
-      typedef streampos 	pos_type;
-      typedef streamoff 	off_type;
-      typedef mbstate_t 	state_type;
+      typedef char              char_type;
+      typedef int               int_type;
+      typedef streampos         pos_type;
+      typedef streamoff         off_type;
+      typedef mbstate_t         state_type;
 
       static void 
       assign(char_type& __c1, const char_type& __c2)
@@ -185,11 +302,11 @@ namespace std
   template<>
     struct char_traits<wchar_t>
     {
-      typedef wchar_t 		char_type;
-      typedef wint_t 		int_type;
-      typedef streamoff 	off_type;
-      typedef wstreampos 	pos_type;
-      typedef mbstate_t 	state_type;
+      typedef wchar_t           char_type;
+      typedef wint_t            int_type;
+      typedef streamoff         off_type;
+      typedef wstreampos        pos_type;
+      typedef mbstate_t         state_type;
       
       static void 
       assign(char_type& __c1, const char_type& __c2)
