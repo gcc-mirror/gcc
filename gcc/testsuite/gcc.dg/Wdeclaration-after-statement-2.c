b736595713974eff2b013c37de2e27b80d77474d
Hans-Peter Nilsson <hp@bitrange.com>
2003-07-28 20:15:44 +0000

    Wdeclaration-after-statement-1.c, [...]: New tests.
    	* gcc.dg/Wdeclaration-after-statement-1.c,
    	gcc.dg/Wdeclaration-after-statement-2.c: New tests.
    
    From-SVN: r69900

---
+++ gcc/testsuite/gcc.dg/Wdeclaration-after-statement-2.c
@@ -0,0 +1,26 @@
+/* Test for C99 mixed declarations and code giving warnings, not error with
+   -Wdeclaration-after-statement.  See also c9?-mixdecl-*.  */
+/* Origin: Joseph Myers <jsm28@cam.ac.uk> */
+/* { dg-do run } */
+/* { dg-options "-std=c99 -pedantic-errors -Wdeclaration-after-statement" } */
+
+extern void abort (void);
+extern void exit (int);
+
+int
+main (void)
+{
+  int i = 0;
+  if (i != 0)
+    abort ();
+  i++;
+  if (i != 1)
+    abort ();
+  int j = i;				/* { dg-warning "warning" "declaration-after-statement" } */
+  if (j != 1)
+    abort ();
+  struct foo { int i0; } k = { 4 };	/* { dg-warning "warning" "declaration-after-statement" } */
+  if (k.i0 != 4)
+    abort ();
+  exit (0);
+}
