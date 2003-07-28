9dbc39271ded1571c28b2cb7ae486dc3afea50f5
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-07-28 15:00:50 +0000

    2003-07-28  Aldy Hernandez  <aldyh@redhat.com>
    	* gcc.dg/20030505.c: Only run for SPE.
    	Remove definition of opaque type.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@69884 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/20030505.c
+++ gcc/testsuite/gcc.dg/20030505.c
@@ -1,10 +1,9 @@
-/* { dg-do compile { target powerpc-*-eabi* } } */
+/* { dg-do compile { target powerpc-*-eabispe* } } */
 /* { dg-options "-W" } */
 
 #define __vector __attribute__((vector_size(8)))
 
 typedef float                   __vector __ev64_fs__;
-typedef int                     __vector __ev64_opaque__;
 
 __ev64_opaque__ *p1;
 __ev64_fs__ *p2;
