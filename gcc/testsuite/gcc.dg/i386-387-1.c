e0ac482659944aac3a617d8f0b4c1432e4f7bb8a
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-07-30 22:48:45 +0000

    2003-07-29  Andrew Pinski <pinskia@physics.uc.edu>
            PR target/11565
            * gcc.dg/i386-387-1.c (dg-options): Add -march=i386.
            * gcc.dg/i386-387-5.c (dg-options): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@69974 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/i386-387-1.c
+++ gcc/testsuite/gcc.dg/i386-387-1.c
@@ -1,6 +1,6 @@
 /* Verify that -mno-fancy-math-387 works.  */
 /* { dg-do compile { target "i?86-*-*" } } */
-/* { dg-options "-O -ffast-math -mfpmath=387 -mno-fancy-math-387" } */
+/* { dg-options "-O -ffast-math -mfpmath=387 -mno-fancy-math-387 -march=i386" } */
 /* { dg-final { scan-assembler "call\t_?sin" } } */
 /* { dg-final { scan-assembler "call\t_?cos" } } */
 /* { dg-final { scan-assembler "call\t_?sqrt" } } */
