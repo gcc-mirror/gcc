a132b6a818c35da81002527bfc680e8eea3f24a8
Jakub Jelinek <jakub@redhat.com>
2003-08-01 00:54:30 +0200

    i386.c (legitimate_pic_address_disp_p): Disallow TLS SYMBOL_REFs not inside UNSPEC even in PLUS rtx.
    	* config/i386/i386.c (legitimate_pic_address_disp_p): Disallow TLS
    	SYMBOL_REFs not inside UNSPEC even in PLUS rtx.
    
    	* gcc.dg/tls/opt-7.c: New test.
    
    From-SVN: r70022

---
+++ gcc/testsuite/gcc.dg/tls/opt-7.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fPIC" } */
+
+static __thread void *baz [4] __attribute__((tls_model ("initial-exec")));
+void foo (void)
+{
+  void **u = (void **) baz;
+  
+  u[0] = 0;
+  u[1] = 0;
+}
+
+/* { dg-final { scan-assembler-not "\[48\]\\+baz" { target i?86-*-* x86_64-*-* } } } */
