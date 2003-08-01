f8e41922ce62264092516ad1693b0daf603ac1a9
lerdsuwa <lerdsuwa@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-08-01 15:41:27 +0000

    PR c++/7983 	* g++.dg/parse/typedef4.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@70051 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/typedef4.C
@@ -0,0 +1,12 @@
+// { dg-do compile }
+
+// Origin: Matt Austern <austern@apple.com>
+
+// PR c++/7983: ICE typedef to typename as friend.
+
+template<class T> class smart_ptr2 {
+    T* real_ptr;
+ public:
+    typedef typename T::subT  td;
+    friend class td; // { dg-error "typename|not name a class" }
+};
