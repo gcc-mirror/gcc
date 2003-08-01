3d6fb44808e13a4adc17b213009ea80ad29d89e3
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-08-01 09:10:29 +0000

    cp: 	PR c++/11525 	* parser.c (cp_parser_primary_expression): Do not set 	non-constant-p merely because it is a dependent scope. testsuite: 	PR c++/11525 	* g++.dg/parse/constant4.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@70041 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/constant4.C
@@ -0,0 +1,40 @@
+// { dg-do compile }
+
+// Copyright (C) 2003 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 31 Jul 2003 <nathan@codesourcery.com>
+
+// PR c++/11525 incorrect error about non-constant initalizer
+
+template<typename> class X;
+template<unsigned> class Y {};
+
+
+template<typename T>
+void Foo ()
+{
+  static const unsigned I = X<T>::I;
+  
+  Y<I> i;
+  
+  static const unsigned J = X<T>::J;
+  
+  Y<J> j; // { dg-error "non-constant" "" }
+}
+
+struct A 
+{
+  operator unsigned () const;
+};
+
+template <typename> struct X 
+{
+  enum {I};
+  static A const J;
+};
+
+void Baz ()
+{
+  Foo<int> (); // { dg-error "instantiated" "" }
+}
+
+  
