8e9dd1eb3ded7ed491b4c392635ec713c342f5d5
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2003-08-02 19:14:25 +0000

    builtins-1.c: Add macro helpers.
    	* gcc.dg/builtins-1.c: Add macro helpers.  Add missing math
    	builtins.  Move cases from builtins-4.c here.
    
    	* gcc.dg/torture/builtin-math-1.c: New test taken from
    	bits of gcc.dg/builtins-3.c, gcc.dg/builtins-5.c and also some
    	additional cases.
    
    	* gcc.dg/builtins-3.c, gcc.dg/builtins-4.c, gcc.dg/builtins-5.c:
    	Delete.
    
    From-SVN: r70108

--- gcc/testsuite/gcc.dg/builtins-1.c
+++ gcc/testsuite/gcc.dg/builtins-1.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2002  Free Software Foundation.
+/* Copyright (C) 2002, 2003  Free Software Foundation.
 
    Verify that all the __builtin_ math functions are recognized
    by the compiler.
@@ -8,27 +8,51 @@
 /* { dg-do compile } */
 /* { dg-final { scan-assembler-not "__builtin_" } } */
 
-double test1(double x) { return __builtin_sqrt(x); }
-double test2(double x) { return __builtin_cos(x); }
-double test3(double x) { return __builtin_sin(x); }
-double test4(double x) { return __builtin_exp(x); }
-double test5(double x) { return __builtin_log(x); }
-double test6(double x) { return __builtin_tan(x); }
-double test7(double x) { return __builtin_atan(x); }
+/* These helper macros ensure we also check the float and long double
+   cases.  */
 
-float test1f(float x) { return __builtin_sqrtf(x); }
-float test2f(float x) { return __builtin_cosf(x); }
-float test3f(float x) { return __builtin_sinf(x); }
-float test4f(float x) { return __builtin_expf(x); }
-float test5f(float x) { return __builtin_logf(x); }
-float test6f(float x) { return __builtin_tanf(x); }
-float test7f(float x) { return __builtin_atanf(x); }
+/* Test FP functions taking void.  */
+#define FPTEST0(FN) \
+double test_##FN(void) { return __builtin_##FN(); } \
+float test_##FN##f(void) { return __builtin_##FN##f(); } \
+long double test_##FN##l(void) { return __builtin_##FN##l(); } 
 
-long double test1l(long double x) { return __builtin_sqrtl(x); }
-long double test2l(long double x) { return __builtin_cosl(x); }
-long double test3l(long double x) { return __builtin_sinl(x); }
-long double test4l(long double x) { return __builtin_expl(x); }
-long double test5l(long double x) { return __builtin_logl(x); }
-long double test6l(long double x) { return __builtin_tanl(x); }
-long double test7l(long double x) { return __builtin_atanl(x); }
+/* Test FP functions taking one FP argument.  */
+#define FPTEST1(FN) \
+double test_##FN(double x) { return __builtin_##FN(x); } \
+float test_##FN##f(float x) { return __builtin_##FN##f(x); } \
+long double test_##FN##l(long double x) { return __builtin_##FN##l(x); } 
 
+/* Test FP functions taking one argument of a supplied type.  */
+#define FPTEST1TYPE(FN, TYPE) \
+double test_##FN(TYPE x) { return __builtin_##FN(x); } \
+float test_##FN##f(TYPE x) { return __builtin_##FN##f(x); } \
+long double test_##FN##l(TYPE x) { return __builtin_##FN##l(x); } 
+
+/* Test FP functions taking two FP arguments.  */
+#define FPTEST2(FN) \
+double test_##FN(double x, double y) { return __builtin_##FN(x, y); } \
+float test_##FN##f(float x, float y) { return __builtin_##FN##f(x, y); } \
+long double test_##FN##l(long double x, long double y) { return __builtin_##FN##l(x, y); } 
+
+/* Keep this list sorted alphabetically by function name.  */
+FPTEST1     (atan)
+FPTEST2     (atan2)
+FPTEST1     (ceil)
+FPTEST1     (cos)
+FPTEST1     (exp)
+FPTEST1     (fabs)
+FPTEST1     (floor)
+FPTEST2     (fmod)
+FPTEST0     (huge_val)
+FPTEST0     (inf)
+FPTEST1     (log)
+FPTEST1TYPE (nan, char *)
+FPTEST1TYPE (nans, char *)
+FPTEST1     (nearbyint)
+FPTEST2     (pow)
+FPTEST1     (round)
+FPTEST1     (sin)
+FPTEST1     (sqrt)
+FPTEST1     (tan)
+FPTEST1     (trunc)
