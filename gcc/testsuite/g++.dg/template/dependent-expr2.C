6cb893080b617bc70a23ba35907ea79cc8c25a80
Nathan Sidwell <nathan@codesourcery.com>
2003-08-03 14:23:34 +0000

    re PR c++/11704 (ICE in type_dependent_expression_p with wrong method call in template class)
    cp:
    	PR c++/11704
    	* pt.c (type_dependent_expression_p): Cope with COMPONENT_REF with
    	unknown type.
    testsuite:
    	PR c++/11704
    	* g++.dg/template/dependent-expr2.C: New test.
    
    From-SVN: r70119

---
+++ gcc/testsuite/g++.dg/template/dependent-expr2.C
@@ -0,0 +1,23 @@
+// { dg-do compile }
+
+// Copyright (C) 2003 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 2 Aug 2003 <nathan@codesourcery.com>
+
+// PR 11704. ICE
+
+struct A 
+{
+  int foo() 
+  {
+    return 5;
+  }
+};
+
+template <class T> // If B is not template it works
+struct B
+{
+  bool bar(A& a)
+  {
+    return a.foo == 0; // { dg-error "insufficient context" "" }
+  }
+};
