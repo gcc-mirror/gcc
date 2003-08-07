3adee96c46c8f70fb52de86775c1524654f10f22
Kriang Lerdsuwanakij <lerdsuwa@users.sourceforge.net>
2003-08-07 14:36:39 +0000

    re PR c++/5767 (ICE with forwarding template template parameters)
    	PR c++/5767
    	* parser.c (cp_parser_class_name): Return immediately when scope
    	is error_mark_node.
    
    	* g++.dg/parse/crash11.C: New test.
    
    From-SVN: r70226

---
+++ gcc/testsuite/g++.dg/parse/crash11.C
@@ -0,0 +1,34 @@
+// { dg-do compile }
+
+// Origin: priesnit@math.uni-goettingen.de
+
+// PR c++/5767: ICE parsing typename with invalid scope.
+
+template <template <typename> class TP>
+struct A
+{
+  template <typename T>
+  struct Template
+  {
+    typedef typename TP<T>::Type Type;
+  };
+};
+template <template <typename> class TP>
+struct B
+{
+  template <typename T>
+  struct Template
+  {
+    typedef typename A<A<TP>::Template>
+      ::template Template<T>::Type Type; { dg-error "mismatch|class template|unqualified-id" }
+  };
+};
+template <typename T>
+struct C
+{
+  typedef void Type;
+};
+int main()
+{
+  typedef B<C>::Template<void>::Type Type; { dg-error "init-declarator|;" }
+}
