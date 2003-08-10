86306a6b111cf8e6bef4c451761f3a458212a756
Nathan Sidwell <nathan@codesourcery.com>
2003-08-10 14:54:22 +0000

    re PR c++/10530 (Cannot access non-dependent type within nested template)
    cp:
    	PR c++/10530
    	* pt.c (dependent_type_p_r): A dependent template-id is a class
    	type with dependent template arguments, or a bound template
    	template parameter.
    	(type_dependent_expression_p): A template function decl cannot
    	have a dependent context.
    testsuite:
    	PR c++/10530
    	* g++.dg/template/dependent-name2.C: New test.
    
    From-SVN: r70293

---
+++ gcc/testsuite/g++.dg/template/dependent-name2.C
@@ -0,0 +1,24 @@
+// { dg-do compile }
+
+// Copyright (C) 2003 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 1 Aug 2003 <nathan@codesourcery.com>
+
+// PR 10530. Thought a type was dependent.
+
+template <typename T>
+struct Foo {
+  struct Inner {
+    typedef int type;
+  };
+};
+
+template <typename A> struct Bar {
+  typedef typename Foo<int>::Inner::type type;
+};
+
+template <template <typename T> class TPL> void Foo ()
+{
+  TPL<int> x;
+
+  f (x);
+}
