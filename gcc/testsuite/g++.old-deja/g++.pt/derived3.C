68ea098a059c352835b2090a02a163f47560ef17
Nathan Sidwell <nathan@codesourcery.com>
2003-08-15 10:46:51 +0000

    cp:	* pt.c (instantiate_class_template): Set location before substuting bases.
    cp:	* pt.c (instantiate_class_template): Set location before
    	substuting bases.
    testsuite:
    	* g++.dg/template/error2.C: New test.
    	* g++.dg/lookup/using7.C: Adjust errors
    	* g++.old-deja/g++.pt/crash36.C: Likewise.
    	* g++.old-deja/g++.pt/derived3.C: Likewise.
    
    From-SVN: r70476

--- gcc/testsuite/g++.old-deja/g++.pt/derived3.C
+++ gcc/testsuite/g++.old-deja/g++.pt/derived3.C
@@ -1,14 +1,16 @@
-// { dg-do assemble  }
+// { dg-do compile  }
 
 // by Paul Burchard <burchard@pobox.com>, Level Set Systems, Inc.
 // Copyright (C) 1999, 2002 Free Software Foundation
 
 template<class T>
 class X {
-	class Y : public T {};
-	Y y;			// { dg-error "" } invalid base type
+	class Y : public T
+	{ // { dg-error "base type .* fails to be" "" }
+	};
+	Y y;			// { dg-error "instantiated" "" }
 };
 int main() {
-	X<int> x;		// { dg-error "" } instantiated
+	X<int> x;		// { dg-error "instantiated" "" }
 }
 
