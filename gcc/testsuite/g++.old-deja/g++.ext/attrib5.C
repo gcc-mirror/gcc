4a3386d0c3a41cb486e098b0cc18b55f7c8370a0
Mark Mitchell <mark@codesourcery.com>
2003-08-23 01:52:42 +0000

    * g++.old-deja/g++.ext/attrib5.C: XFAIL on hppa2*-hp-hpux11*.
    From-SVN: r70716

--- gcc/testsuite/g++.old-deja/g++.ext/attrib5.C
+++ gcc/testsuite/g++.old-deja/g++.ext/attrib5.C
@@ -1,5 +1,5 @@
 // Test that attributes weak and alias coexist.
-// excess errors test - XFAIL alpha*-dec-osf* *-*-hms i?86-pc-cygwin *-*-coff
+// excess errors test - XFAIL alpha*-dec-osf* *-*-hms i?86-pc-cygwin *-*-coff hppa2*-hp-hpux11*
 
 extern "C" {
   void f () __attribute__((weak, alias ("_f")));
