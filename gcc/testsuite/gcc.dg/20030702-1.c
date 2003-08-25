85fa5679f4d5c68845265ef7815972e9f2c51379
uweigand <uweigand@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-08-25 20:14:45 +0000

    * combine.c (distribute_notes): Handle REG_ALWAYS_RETURN. 	* gcc.dg/20030702-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@70779 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20030702-1.c
@@ -0,0 +1,10 @@
+/* This tests whether REG_ALWAYS_RETURN notes are handled
+   correctly in combine.  */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fpic -fprofile-arcs" } */
+
+void test (void)
+{
+  fork ();
+}
+
