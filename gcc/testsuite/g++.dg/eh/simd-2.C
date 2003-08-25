22c50c9e00321bc7bf7a7b37b5854ff06b5a8e21
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-08-25 03:17:02 +0000

    * config/i386.i386.c (ix86_return_in_memory): Reformat.  Return true         for 16-byte vector modes if sse not enabled; warn for abi change.         (ix86_value_regno): Only return xmm0 for 16-byte vector types.         * g++.dg/eh/simd-2.C: Add -w for x86.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@70771 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/eh/simd-2.C
+++ gcc/testsuite/g++.dg/eh/simd-2.C
@@ -1,6 +1,7 @@
 // Test EH when V4SI SIMD registers are involved.
 // Contributed by Aldy Hernandez (aldy@quesejoda.com).
 // { dg-options "-O" }
+// { dg-options "-O -w" { target i?86-*-* } }
 // { dg-do run }
 
 typedef int __attribute__((mode(V4SI))) vecint;
