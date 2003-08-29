d560a41c5c43524d82795ac4421eab2df6764e30
Richard Henderson <rth@redhat.com>
2003-08-29 15:44:05 -0700

    function.h (struct function): Add rtl_inline_init, saved_for_inline.
            * function.h (struct function): Add rtl_inline_init, saved_for_inline.
            * integrate.c (save_for_inline): Set saved_for_inline.
            * c-semantics.c (genrtl_scope_stmt): Check it.
            * toplev.c (wrapup_global_declarations): Check it.
            (rest_of_handle_inlining): Set and check rtl_inline_init.
            (rest_of_compilation): Remove out of date comment.
    
    From-SVN: r70931

--- gcc/c-semantics.c
+++ gcc/c-semantics.c
@@ -645,6 +645,7 @@ genrtl_scope_stmt (tree t)
 	  if (TREE_CODE (fn) == FUNCTION_DECL
 	      && DECL_CONTEXT (fn) == current_function_decl
 	      && DECL_SAVED_INSNS (fn)
+	      && DECL_SAVED_INSNS (fn)->saved_for_inline
 	      && !TREE_ASM_WRITTEN (fn)
 	      && TREE_ADDRESSABLE (fn))
 	    {
