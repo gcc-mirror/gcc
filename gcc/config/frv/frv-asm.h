3922e7c1ddbd1646799b61ceb7c22255a220ab24
Eric Christopher <echristo@gcc.gnu.org>
2003-09-05 00:26:26 +0000

    t-frv: Fix path for frv-abi.h.
    2003-09-04  Eric Christopher  <echristo@redhat.com>
    
    	* config/frv/t-frv: Fix path for frv-abi.h.
    	* config/frv/frv-asm.h: Fix string concatenation.
    
    From-SVN: r71099

--- gcc/config/frv/frv-asm.h
+++ gcc/config/frv/frv-asm.h
@@ -1,19 +1,19 @@
 /* Assembler Support.
    Copyright (C) 2000 Free Software Foundation, Inc.
    Contributed by Red Hat, Inc.
-  
+
    This file is part of GNU CC.
-  
+
    GNU CC is free software ; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation * either version 2, or (at your option)
    any later version.
-  
+
    GNU CC is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY ; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-  
+
    You should have received a copy of the GNU General Public License
    along with GNU CC; see the file COPYING.  If not, write to
    the Free Software Foundation, 59 Temple Place - Suite 330,
@@ -23,7 +23,7 @@
    P2(INSN): Emit INSN.P on the FR500 and above, otherwise emit plain INSN.  */
 #ifdef __FRV_VLIW__
 #ifdef __STDC__
-#define P(A) A##.p
+#define P(A) A.p
 #else
 #define P(A) A/**/.p
 #endif
