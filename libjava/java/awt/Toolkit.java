3df7c71c2300d123998ea6534843fd9fe9c2dfb3
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-09-11 16:45:10 +0000

    2003-09-11  Sascha Brawer  <brawer@dandelis.ch>
    	* java/awt/Toolkit.java (getSystemEventQueue): Call SecurityManager
    	if one is installed. Improve Javadoc.
    	(getSystemEventQueueImpl): Improve Javadoc.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@71308 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/awt/Toolkit.java
+++ libjava/java/awt/Toolkit.java
@@ -803,22 +803,57 @@ public abstract class Toolkit
     return props.getProperty(key, def);
   }
 
+
   /**
-   * Returns the event queue for the applet.  Despite the word "System"
-   * in the name of this method, there is no guarantee that the same queue
-   * is shared system wide.
+   * Returns the event queue that is suitable for the calling context.
    *
-   * @return The event queue for this applet (or application)
+   * <p>Despite the word “System” in the name of this
+   * method, a toolkit may provide different event queues for each
+   * applet. There is no guarantee that the same queue is shared
+   * system-wide.
+   *
+   * <p>The implementation first checks whether a
+   * SecurityManager has been installed. If so, its {@link
+   * java.lang.SecurityManager#checkAwtEventQueueAccess()} method gets
+   * called. The security manager will throw a SecurityException if it
+   * does not grant the permission to access the event queue.
+   *
+   * <p>Next, the call is delegated to {@link
+   * #getSystemEventQueueImpl()}.
+   *
+   * @return The event queue for this applet (or application).
+   *
+   * @throws SecurityException if a security manager has been
+   * installed, and it does not grant the permission to access the
+   * event queue.
    */
   public final EventQueue getSystemEventQueue()
   {
+    SecurityManager sm;
+
+    sm = System.getSecurityManager();
+    if (sm != null)
+      sm.checkAwtEventQueueAccess();
+
     return getSystemEventQueueImpl();
   }
 
+
   /**
-   * // FIXME: What does this do?
+   * Returns the event queue that is suitable for the calling context.
+   *
+   * <p>Despite the word “,System”, in the name of this
+   * method, a toolkit may provide different event queues for each
+   * applet. There is no guarantee that the same queue is shared
+   * system-wide.
+   *
+   * <p>No security checks are performed, which is why this method
+   * may only be called by Toolkits.
+   *
+   * @see #getSystemEventQueue()
    */
   protected abstract EventQueue getSystemEventQueueImpl();
+
 
   /**
    * @since 1.3
