8eed96ae3aafd2b724b7b1c1c62cd50763dbe535
bothner <bothner@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-09-12 05:03:22 +0000

    * c-lex.c (fe_file_change): Don't transform to_line with SOURCE_LINE.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@71328 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-lex.c
+++ gcc/c-lex.c
@@ -212,8 +212,6 @@ cb_dir_change (cpp_reader *pfile ATTRIBUTE_UNUSED, const char *dir)
 void
 fe_file_change (const struct line_map *new_map)
 {
-  unsigned int to_line = SOURCE_LINE (new_map, new_map->to_line);
-
   if (new_map->reason == LC_ENTER)
     {
       /* Don't stack the main buffer on the input stack;
@@ -250,13 +248,13 @@ fe_file_change (const struct line_map *new_map)
 #endif
       pop_srcloc ();
 
-      (*debug_hooks->end_source_file) (to_line);
+      (*debug_hooks->end_source_file) (new_map->to_line);
     }
 
   update_header_times (new_map->to_file);
   in_system_header = new_map->sysp != 0;
   input_filename = new_map->to_file;
-  input_line = to_line;
+  input_line = new_map->to_line;
   map = new_map;
 
   /* Hook for C++.  */
