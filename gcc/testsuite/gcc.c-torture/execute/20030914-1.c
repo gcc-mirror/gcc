1cd50c9a646243b140ab1e7923e6c990839eb655
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-09-14 10:07:51 +0000

    * function.c (STACK_BYTES): Move definition to head of file. 	(assign_parms): Don't pass current_function_pretend_args_size 	directly to SETUP_INCOMING_VARARGS.  For partial register arguments, 	round current_function_pretend_args_size up to STACK_BYTES.  Skip any 	excess before laying out the argument.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@71372 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20030914-1.c
@@ -0,0 +1,26 @@
+/* On IRIX 6, PB is passed partially in registers and partially on the
+   stack, with an odd number of words in the register part.  Check that
+   the long double stack argument (PC) is still accessed properly.  */
+
+struct s { int val[16]; };
+
+long double f (int pa, struct s pb, long double pc)
+{
+  int i;
+
+  for (i = 0; i < 16; i++)
+    pc += pb.val[i];
+  return pc;
+}
+
+int main ()
+{
+  struct s x;
+  int i;
+
+  for (i = 0; i < 16; i++)
+    x.val[i] = i + 1;
+  if (f (1, x, 10000.0L) != 10136.0L)
+    abort ();
+  exit (0);
+}
---
+++ gcc/testsuite/gcc.c-torture/execute/20030914-2.c
@@ -0,0 +1,21 @@
+/* On IRIX 6, PA is passed partially in registers and partially on the
+   stack.  We therefore have two potential uses of pretend_args_size:
+   one for the partial argument and one for the varargs save area.
+   Make sure that these uses don't conflict.  */
+
+struct s { int i[18]; };
+
+int f (struct s pa, int pb, ...)
+{
+  return pb;
+}
+
+struct s gs;
+
+int main ()
+{
+  if (f (gs, 0x1234) != 0x1234)
+    abort ();
+
+  exit (0);
+}
