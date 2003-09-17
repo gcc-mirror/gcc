34543ed442a0759e3bd12b847fab5aab850b6811
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-09-17 17:40:05 +0000

    * config/mips/iris5.h (TARGET_OS_CPP_BUILTINS): Define _LONGLONG. 	Define _ABIO32. 	Use it for _MIPS_SIM. 	* config/mips/iris6-o32.h (TARGET_OS_CPP_BUILTINS): Removed.
    	* config/mips/iris6-o32-as.h (SUBTARGET_ASM_OPTIMIZING_SPEC):
    	Moved ...
    	* config/mips/iris5.h (SUBTARGET_ASM_OPTIMIZING_SPEC): ... here,
    	updating comment.
    	Fixes PR target/10190.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@71465 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/mips/iris6-o32-as.h
+++ gcc/config/mips/iris6-o32-as.h
@@ -1,17 +1,6 @@
 /* Definitions of target machine for GNU compiler, for MIPS running IRIX 6
    (O32 ABI) using the SGI assembler.  */
 
-/* Override mips.h default: the IRIX 6 O32 assembler warns about -O3:
-
-   as: Warning: -O3 is not supported for assembly compiles for ucode
-   compilers; changing to -O2.
-   
-   So avoid passing it in the first place.  */
-#undef SUBTARGET_ASM_OPTIMIZING_SPEC
-#define SUBTARGET_ASM_OPTIMIZING_SPEC "\
-%{noasmopt:-O0} \
-%{!noasmopt:%{O|O1|O2|O3:-O2}}"
-
 /* Enforce use of O32 linker, irrespective of SGI_ABI environment variable
    and machine type (e.g., R8000 systems default to -64).  Copied from
    iris5.h, only adding -32.  The default options -call_shared -no_unresolved
--- gcc/config/mips/iris6-o32.h
+++ gcc/config/mips/iris6-o32.h
@@ -18,48 +18,6 @@
 #undef WINT_TYPE_SIZE
 #define WINT_TYPE_SIZE 32
 
-/* Copied from iris5.h, with _MIPS_SIM definition adapted to SGI cc usage
-   and -D_LONGLONG added as in iris6.h.  */
-#undef TARGET_OS_CPP_BUILTINS
-#define TARGET_OS_CPP_BUILTINS()			\
-    do {						\
-	builtin_define_std ("host_mips");		\
-	builtin_define_std ("sgi");			\
-	builtin_define_std ("unix");			\
-	builtin_define_std ("SYSTYPE_SVR4");		\
-	builtin_define ("_LONGLONG");			\
-	builtin_define ("_MODERN_C");			\
-	builtin_define ("_SVR4_SOURCE");		\
-	builtin_define ("__DSO__");			\
-	builtin_define ("_ABIO32=1");			\
-	builtin_define ("_MIPS_SIM=_ABIO32");		\
-	builtin_define ("_MIPS_SZPTR=32");		\
-	builtin_assert ("system=unix");			\
-	builtin_assert ("system=svr4");			\
-	builtin_assert ("machine=sgi");			\
-							\
-     if (!TARGET_FLOAT64)                               \
-        builtin_define ("_MIPS_FPSET=16");              \
-     else                                               \
-        builtin_define ("_MIPS_FPSET=32");              \
-							\
-     if (!TARGET_INT64)                                 \
-        builtin_define ("_MIPS_SZINT=32");              \
-     else                                               \
-        builtin_define ("_MIPS_SZINT=64");              \
-							\
-     if (!TARGET_LONG64)				\
-	builtin_define ("_MIPS_SZLONG=32");		\
-     else						\
-	builtin_define ("_MIPS_SZLONG=64");		\
-							\
-     if (!flag_iso)					\
-       {						\
-	 builtin_define ("__EXTENSIONS__");		\
-	 builtin_define ("_SGI_SOURCE");		\
-       }						\
-} while (0);
-
 /* Enforce use of O32 assembler, irrespective of SGI_ABI environment variable
    and machine type (e.g., R8000 systems default to -64).  Gas doesn't need
    this, but doesn't hurt either.  Need to pass -mips2 to gas which defaults
