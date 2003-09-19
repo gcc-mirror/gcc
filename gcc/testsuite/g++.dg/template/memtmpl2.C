8306960d1e631851fed807bd8ea5098a6ec93b3e
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-09-19 09:28:05 +0000

    cp: 	PR c++/12232 	* pt.c (instantiate_class_template): Increment 	processing_template_decl around the tsubst of a template member 	function. testsuite: 	PR c++/12232 	* g++.dg/template/memtmpl2.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@71563 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/memtmpl2.C
@@ -0,0 +1,15 @@
+// { dg-do compile }
+
+// Copyright (C) 2003 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 18 Sep 2003 <nathan@codesourcery.com>
+
+// PR c++/12232. ICE
+
+template <unsigned D> class TPL;
+
+template <typename T> struct X {
+  template <template <typename> class V>
+  V<TPL<V<int>::d> > operator () ();
+};
+
+void Foo (X<int> x) {}
