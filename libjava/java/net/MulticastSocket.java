65f070242b4206f88edf3c53000520f3f5c0224a
Michael Koch <konqueror@gmx.de>
2003-09-19 07:24:59 +0000

    DatagramSocket.java (getLocalAddress): Renamed result variable to localAddr.
    2003-09-19  Michael Koch  <konqueror@gmx.de>
    
    	* java/net/DatagramSocket.java (getLocalAddress):
    	Renamed result variable to localAddr.
    	* java/net/MulticastSocket.java:
    	No need to import gnu.java.net.PlainDatagramSocketImpl.
    
    From-SVN: r71561

--- libjava/java/net/MulticastSocket.java
+++ libjava/java/net/MulticastSocket.java
@@ -36,10 +36,8 @@ this exception to your version of the library, but you are not
 obligated to do so.  If you do not wish to do so, delete this
 exception statement from your version. */
 
-
 package java.net;
 
-import gnu.java.net.PlainDatagramSocketImpl;
 import java.io.IOException;
 import java.util.Enumeration;
 
