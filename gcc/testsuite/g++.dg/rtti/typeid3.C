c5744534c77854d3d494f78bb2c0980051d2cb83
lerdsuwa <lerdsuwa@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-09-20 16:32:18 +0000

    * g++.dg/rtti/typeid3.C: Correct expected error message.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@71606 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/rtti/typeid3.C
+++ gcc/testsuite/g++.dg/rtti/typeid3.C
@@ -2,10 +2,10 @@
  
 template <template <class> class T> struct A { 
     void error() { 
-      typeid(T).name(); // { dg-error "" }
+      typeid(T).name(); // { dg-error "missing" }
     } 
 }; 
  
 template <class T> struct B {}; 
  
-template void A<B>::error(); // { dg-error "instantiated" }
+template void A<B>::error();
