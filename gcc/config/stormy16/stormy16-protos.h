51c16b7e8a7415eba4863cafbc5e5ff717351f3c
Steven Bosscher <steven@gcc.gnu.org>
2003-09-28 09:51:52 +0000

    stormy16.c, [...]: Convert to ISO C90 function declarations and definitions.
    	* config/stormy16/stormy16.c, config/stormy16/stormy16-protos.h:
    	Convert to ISO C90 function declarations and definitions.
    
    From-SVN: r71878

--- gcc/config/stormy16/stormy16-protos.h
+++ gcc/config/stormy16/stormy16-protos.h
@@ -21,76 +21,69 @@ Boston, MA 02111-1307, USA.  */
 
 
 
-extern struct xstormy16_stack_layout 
-		    xstormy16_compute_stack_layout PARAMS((void));
-extern void         xstormy16_expand_prologue      PARAMS ((void));
-extern void         xstormy16_expand_epilogue      PARAMS ((void));
-extern int	xstormy16_initial_elimination_offset PARAMS ((int, int));
-extern int	direct_return PARAMS ((void));
-extern int	xstormy16_interrupt_function_p PARAMS ((void));
-extern int	xstormy16_epilogue_uses PARAMS ((int));
-extern void	xstormy16_function_profiler PARAMS ((void));
+extern struct xstormy16_stack_layout xstormy16_compute_stack_layout (void);
+extern void xstormy16_expand_prologue (void);
+extern void xstormy16_expand_epilogue (void);
+extern int xstormy16_initial_elimination_offset (int, int);
+extern int direct_return (void);
+extern int xstormy16_interrupt_function_p (void);
+extern int xstormy16_epilogue_uses (int);
+extern void xstormy16_function_profiler (void);
 
 #if defined (TREE_CODE)
 #  if defined (HAVE_MACHINE_MODES)
 extern CUMULATIVE_ARGS xstormy16_function_arg_advance 
-  PARAMS ((CUMULATIVE_ARGS, enum machine_mode, tree, int));
+ (CUMULATIVE_ARGS, enum machine_mode, tree, int);
 extern rtx xstormy16_function_arg
-  PARAMS ((CUMULATIVE_ARGS, enum machine_mode, tree, int));
+ (CUMULATIVE_ARGS, enum machine_mode, tree, int);
 #  endif
-extern void    xstormy16_setup_incoming_varargs 
-  PARAMS ((CUMULATIVE_ARGS, int, tree, int *));
-extern tree    xstormy16_build_va_list PARAMS ((void));
+extern void xstormy16_setup_incoming_varargs 
+ (CUMULATIVE_ARGS, int, tree, int *);
+extern tree xstormy16_build_va_list (void);
 #endif
 
 #if defined (TREE_CODE) && defined (RTX_CODE)
-extern void    xstormy16_expand_builtin_va_start PARAMS ((tree, rtx));
-extern rtx     xstormy16_expand_builtin_va_arg PARAMS ((tree, tree));
-extern void    xstormy16_initialize_trampoline PARAMS ((rtx, rtx, rtx));
-extern rtx     xstormy16_function_value PARAMS ((tree, tree));
+extern void xstormy16_expand_builtin_va_start (tree, rtx);
+extern rtx xstormy16_expand_builtin_va_arg (tree, tree);
+extern void xstormy16_initialize_trampoline (rtx, rtx, rtx);
+extern rtx xstormy16_function_value (tree, tree);
 #endif
 
 #ifdef RTX_CODE
-extern void xstormy16_emit_cbranch PARAMS ((enum rtx_code, rtx));
-extern char * xstormy16_output_cbranch_hi PARAMS ((rtx, const char *, int, 
-						  rtx));
-extern char * xstormy16_output_cbranch_si PARAMS ((rtx, const char *, int, 
-						  rtx));
-extern int xstormy16_mode_dependent_address_p PARAMS ((rtx));
-extern int xstormy16_extra_constraint_p PARAMS ((rtx, int));
+extern void xstormy16_emit_cbranch (enum rtx_code, rtx);
+extern char *xstormy16_output_cbranch_hi (rtx, const char *, int, rtx);
+extern char *xstormy16_output_cbranch_si (rtx, const char *, int, rtx);
+extern int xstormy16_mode_dependent_address_p (rtx);
+extern int xstormy16_extra_constraint_p (rtx, int);
 
-extern void xstormy16_print_operand          PARAMS ((FILE *, rtx, int));
-extern void xstormy16_print_operand_address  PARAMS ((FILE *, rtx));
+extern void xstormy16_print_operand (FILE *, rtx, int);
+extern void xstormy16_print_operand_address (FILE *, rtx);
 
-extern void xstormy16_expand_casesi PARAMS ((rtx, rtx, rtx, rtx, rtx));
-extern void xstormy16_output_addr_vec PARAMS ((FILE *, rtx, rtx));
-extern void xstormy16_expand_call PARAMS ((rtx, rtx, rtx));
+extern void xstormy16_expand_casesi (rtx, rtx, rtx, rtx, rtx);
+extern void xstormy16_output_addr_vec (FILE *, rtx, rtx);
+extern void xstormy16_expand_call (rtx, rtx, rtx);
 #endif
 
 #if defined (HAVE_MACHINE_MODES) && defined (RTX_CODE)
-extern int xstormy16_ineqsi_operator PARAMS ((rtx, enum machine_mode));
-extern int equality_operator PARAMS ((rtx, enum machine_mode));
-extern int inequality_operator PARAMS ((rtx, enum machine_mode));
-extern void xstormy16_split_cbranch PARAMS ((enum machine_mode, 
-					   rtx, rtx, rtx, rtx));
-extern int  short_memory_operand PARAMS ((rtx, enum machine_mode));
-extern int  nonimmediate_nonstack_operand PARAMS ((rtx, enum machine_mode));
+extern int xstormy16_ineqsi_operator (rtx, enum machine_mode);
+extern int equality_operator (rtx, enum machine_mode);
+extern int inequality_operator (rtx, enum machine_mode);
+extern void xstormy16_split_cbranch (enum machine_mode, rtx, rtx, rtx, rtx);
+extern int  short_memory_operand (rtx, enum machine_mode);
+extern int  nonimmediate_nonstack_operand (rtx, enum machine_mode);
 extern enum reg_class xstormy16_secondary_reload_class 
-  PARAMS ((enum reg_class, enum machine_mode, rtx));
-extern int  xstormy16_carry_plus_operand PARAMS ((rtx, enum machine_mode));
-extern int  xs_hi_general_operand PARAMS ((rtx, enum machine_mode));
-extern int  xs_hi_nonmemory_operand PARAMS ((rtx, enum machine_mode));
-extern enum reg_class xstormy16_preferred_reload_class 
-  PARAMS ((rtx, enum reg_class));
-extern int xstormy16_legitimate_address_p 
-   PARAMS ((enum machine_mode, rtx, int));
-extern void xstormy16_split_move PARAMS ((enum machine_mode, rtx, rtx));
-extern void xstormy16_expand_move PARAMS ((enum machine_mode, rtx, rtx));
-extern void xstormy16_expand_arith PARAMS ((enum machine_mode, enum rtx_code, 
-					   rtx, rtx, rtx, rtx));
-extern int  shift_operator PARAMS ((rtx, enum machine_mode));
-extern const char * xstormy16_output_shift PARAMS ((enum machine_mode, 
-						   enum rtx_code, 
-						   rtx, rtx, rtx));
+ (enum reg_class, enum machine_mode, rtx);
+extern int  xstormy16_carry_plus_operand (rtx, enum machine_mode);
+extern int  xs_hi_general_operand (rtx, enum machine_mode);
+extern int  xs_hi_nonmemory_operand (rtx, enum machine_mode);
+extern enum reg_class xstormy16_preferred_reload_class (rtx, enum reg_class);
+extern int xstormy16_legitimate_address_p (enum machine_mode, rtx, int);
+extern void xstormy16_split_move (enum machine_mode, rtx, rtx);
+extern void xstormy16_expand_move (enum machine_mode, rtx, rtx);
+extern void xstormy16_expand_arith (enum machine_mode, enum rtx_code, 
+				    rtx, rtx, rtx, rtx);
+extern int  shift_operator (rtx, enum machine_mode);
+extern const char * xstormy16_output_shift (enum machine_mode, enum rtx_code, 
+					    rtx, rtx, rtx);
 #endif
 
