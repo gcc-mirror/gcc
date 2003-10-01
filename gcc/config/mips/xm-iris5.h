04074c50daed0fc0aed4011330a71ed17ac991e3
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-10-01 23:51:10 +0000

    * config/mips/xm-iris5.h: Remove, unnecessary. 	* config.build (mips-sgi-irix5*): Remove. 	(mips-sgi-irix6*o32): Likewise. 	* config.gcc (mips-sgi-irix6*o32): Remove xm_file. 	(mips-sgi-irix5cross64): Likewise. 	(mips-sgi-irix5*): Likewise. 	* config.host (mips-sgi-irix5*): Remove. 	(mips-sgi-irix6*o32): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@72003 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/mips/xm-iris5.h
+++
@@ -1,4 +0,0 @@
-/* On SGI IRIX 5.3, inttypes.h clashes with sys/types.h, but the clash
-   (when compiled with GCC) is a warning, so configure.in thinks it's OK
-   to use it.  Work around this problem.  */
-#undef HAVE_INTTYPES_H
