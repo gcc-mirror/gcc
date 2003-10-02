11b5269c49d5849aa9909a0147b38cc8d44d7d90
bothner <bothner@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-10-02 07:20:38 +0000

    * cpplex.c (_cpp_get_fresh_line):  Revert my no-longer-needed 	08-28 change, since we're never called with a NULL buffer. 	(_cpp_lex_direct):  Likewise. 	* cpptrad.c (_cpp_read_logical_line_trad):  Likewise. 	Return false if buffer is NULL at end.
    	* cpplex.c (_cpp_get_fresh_line):  Return value now just depends on
    	whether pfile->buffer is NULL after pop, ignoring return_at_eof.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@72013 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cpptrad.c
+++ gcc/cpptrad.c
@@ -299,13 +299,12 @@ _cpp_read_logical_line_trad (cpp_reader *pfile)
 {
   do
     {
-      if ((pfile->buffer == NULL || pfile->buffer->need_line)
-	  && !_cpp_get_fresh_line (pfile))
+      if (pfile->buffer->need_line && !_cpp_get_fresh_line (pfile))
 	return false;
     }
   while (!_cpp_scan_out_logical_line (pfile, NULL) || pfile->state.skipping);
 
-  return true;
+  return pfile->buffer != NULL;
 }
 
 /* Set up state for finding the opening '(' of a function-like
