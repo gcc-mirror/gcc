b494fd9851d66bf76000364d05bfca1acbf74066
Eric Botcazou <ebotcazou@libertysurf.fr>
2003-10-04 20:02:32 +0200

    re PR c/12446 (ICE in emit_move_insn on complicated array reference)
    	PR c/12446
    	* c-typeck.c (convert_for_assignment): Issue an error for
    	array to pointer assignment after default conversion.
    	(digest_init): Likewise.
    
    From-SVN: r72096

---
+++ gcc/testsuite/gcc.dg/c90-array-lval-6.c
@@ -0,0 +1,16 @@
+/* PR c/12446 */
+/* Origin: Keith Thompson <kst@cts.com> */
+
+/* { dg-do compile } */
+/* { dg-options "-std=iso9899:1990 -pedantic-errors" } */
+
+
+struct s { char c[1]; };
+
+extern struct s foo(void);
+
+void bar(void)
+{
+  char *ptr = foo().c; /* { dg-bogus "warning" "warning in place of error" } */
+}
+/* { dg-error "non-lvalue" "array not decaying to lvalue" { target *-*-* } 14 } */
