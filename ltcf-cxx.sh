fffeb871a114219c8aa729064632af142b1bf6c6
Rainer Orth <ro@TechFak.Uni-Bielefeld.DE>
2003-10-06 15:13:13 +0000

    ltcf-cxx.sh (irix5*, irix6*): Prefix -soname and its arg with ${wl}.
    	* ltcf-cxx.sh (irix5*, irix6*): Prefix -soname and its arg with ${wl}.
    	* ltconfig (irix5*, irix6*): Don't override version_type.
    
    From-SVN: r72152

--- ltcf-cxx.sh
+++ ltcf-cxx.sh
@@ -326,7 +326,7 @@ case $host_os in
           if test "$with_gnu_ld" = no; then
             archive_cmds='$CC -shared -nostdlib $predep_objects $libobjs $deplibs $postdep_objects $compiler_flags ${wl}-soname ${wl}$soname `test -n "$verstring" && echo ${wl}-set_version ${wl}$verstring` ${wl}-update_registry ${wl}${objdir}/so_locations -o $lib'
           else
-            archive_cmds='$CC -shared -nostdlib $predep_objects $libobjs $deplibs $postdep_objects $compiler_flags -soname $soname `test -n "$verstring" && echo -set_version $verstring` -o $lib'
+            archive_cmds='$CC -shared -nostdlib $predep_objects $libobjs $deplibs $postdep_objects $compiler_flags ${wl}-soname ${wl}$soname `test -n "$verstring" && echo -set_version $verstring` -o $lib'
           fi
         fi
         hardcode_libdir_flag_spec='${wl}-rpath ${wl}$libdir'
