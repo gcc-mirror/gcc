1b288fecdeb9929c72515dd2471e4c7bec264d2a
Jason Merrill <jason@redhat.com>
2003-10-07 18:10:37 -0400

    re PR c++/12519 (ICE tree check: in genrtl_cleanup_stmt, at c-semantics.c:761)
            PR c++/12519
            * c-semantics.c (genrtl_cleanup_stmt): Ignore the CLEANUP_DECL if
            it isn't a decl.
    
    From-SVN: r72206

--- gcc/c-semantics.c
+++ gcc/c-semantics.c
@@ -758,7 +758,8 @@ void
 genrtl_cleanup_stmt (tree t)
 {
   tree decl = CLEANUP_DECL (t);
-  if (!decl || (DECL_SIZE (decl) && TREE_TYPE (decl) != error_mark_node))
+  if (!decl || !DECL_P (decl)
+      || (DECL_SIZE (decl) && TREE_TYPE (decl) != error_mark_node))
     expand_decl_cleanup_eh (decl, CLEANUP_EXPR (t), CLEANUP_EH_ONLY (t));
 }
 
