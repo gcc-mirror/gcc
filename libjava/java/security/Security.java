c0815127d0226a50633530ca650b0bbd2724fdf5
Michael Koch <konqueror@gmx.de>
2003-10-08 21:01:50 +0000

    Security.java: Reformated.
    2003-10-08  Michael Koch  <konqueror@gmx.de>
    
    	* java/security/Security.java: Reformated.
    
    From-SVN: r72240

--- libjava/java/security/Security.java
+++ libjava/java/security/Security.java
@@ -65,32 +65,35 @@ public final class Security extends Object
 
   private static Vector providers = new Vector();
   private static Properties secprops = new Properties();
+  
   static
-  {
-    String base = System.getProperty("gnu.classpath.home.url");
-    String vendor = System.getProperty("gnu.classpath.vm.shortname");
+    {
+      String base = System.getProperty ("gnu.classpath.home.url");
+      String vendor = System.getProperty ("gnu.classpath.vm.shortname");
     
-    // Try VM specific security file
-    boolean loaded = loadProviders(base, vendor);
+      // Try VM specific security file
+      boolean loaded = loadProviders (base, vendor);
     
-    // Append classpath standard provider if possible
-    if (!loadProviders(base, "classpath") && !loaded && providers.size() == 0)
-      {
-	// No providers found and both security files failed to load properly.
-	System.err.println
-	  ("WARNING: could not properly read security provider files:");
-	System.err.println
-	  ("         " + base + "/security/" + vendor + ".security");
-	System.err.println
-	  ("         " + base + "/security/" + "classpath" + ".security");
-	System.err.println
-	  ("         Falling back to standard GNU security provider");
-	providers.addElement(new gnu.java.security.provider.Gnu());
-      }
+      // Append classpath standard provider if possible
+      if (!loadProviders (base, "classpath")
+	  && !loaded
+	  && providers.size() == 0)
+        {
+	  // No providers found and both security files failed to load properly.
+	  System.err.println
+	    ("WARNING: could not properly read security provider files:");
+	  System.err.println
+	    ("         " + base + "/security/" + vendor + ".security");
+	  System.err.println
+	    ("         " + base + "/security/" + "classpath" + ".security");
+	  System.err.println
+	    ("         Falling back to standard GNU security provider");
+	  providers.addElement (new gnu.java.security.provider.Gnu());
+        }
   }
 
   // This class can't be instantiated.
-  private Security ()
+  private Security()
   {
   }
 
