6233cbbc8894869291cb257cb37a615d6ff91243
Michael Koch <konqueror@gmx.de>
2003-10-11 18:56:54 +0000

    ProtocolConstants.java, DER.java: Removing redundant modifiers.
    2003-10-11  Michael Koch  <konqueror@gmx.de>
    
    	* gnu/java/rmi/server/ProtocolConstants.java,
    	gnu/java/security/der/DER.java:
    	Removing redundant modifiers.
    
    From-SVN: r72359

--- libjava/gnu/java/security/der/DER.java
+++ libjava/gnu/java/security/der/DER.java
@@ -45,46 +45,42 @@ package gnu.java.security.der;
  */
 public interface DER
 {
+  int UNIVERSAL   = 0x00;
+  int APPLICATION = 0x40;
+  int CONTEXT     = 0x80;
+  int PRIVATE     = 0xC0;
 
-  // Constants.
-  // ------------------------------------------------------------------------
+  int CONSTRUCTED = 0x20;
 
-  public static final int UNIVERSAL   = 0x00;
-  public static final int APPLICATION = 0x40;
-  public static final int CONTEXT     = 0x80;
-  public static final int PRIVATE     = 0xC0;
+  int ANY               = 0x00;
+  int BOOLEAN           = 0x01;
+  int INTEGER           = 0x02;
+  int BIT_STRING        = 0x03;
+  int OCTET_STRING      = 0x04;
+  int NULL              = 0x05;
+  int OBJECT_IDENTIFIER = 0x06;
+  int REAL              = 0x09;
+  int ENUMERATED        = 0x0a;
+  int RELATIVE_OID      = 0x0d;
 
-  public static final int CONSTRUCTED = 0x20;
+  int SEQUENCE = 0x10;
+  int SET      = 0x11;
 
-  public static final int ANY               = 0x00;
-  public static final int BOOLEAN           = 0x01;
-  public static final int INTEGER           = 0x02;
-  public static final int BIT_STRING        = 0x03;
-  public static final int OCTET_STRING      = 0x04;
-  public static final int NULL              = 0x05;
-  public static final int OBJECT_IDENTIFIER = 0x06;
-  public static final int REAL              = 0x09;
-  public static final int ENUMERATED        = 0x0a;
-  public static final int RELATIVE_OID      = 0x0d;
+  Object CONSTRUCTED_VALUE = new Object();
 
-  public static final int SEQUENCE = 0x10;
-  public static final int SET      = 0x11;
+  int NUMERIC_STRING   = 0x12;
+  int PRINTABLE_STRING = 0x13;
+  int T61_STRING       = 0x14;
+  int VIDEOTEX_STRING  = 0x15;
+  int IA5_STRING       = 0x16;
+  int GRAPHIC_STRING   = 0x19;
+  int ISO646_STRING    = 0x1A;
+  int GENERAL_STRING   = 0x1B;
 
-  public static final Object CONSTRUCTED_VALUE = new Object();
+  int UTF8_STRING      = 0x0C;
+  int UNIVERSAL_STRING = 0x1C;
+  int BMP_STRING       = 0x1E;
 
-  public static final int NUMERIC_STRING   = 0x12;
-  public static final int PRINTABLE_STRING = 0x13;
-  public static final int T61_STRING       = 0x14;
-  public static final int VIDEOTEX_STRING  = 0x15;
-  public static final int IA5_STRING       = 0x16;
-  public static final int GRAPHIC_STRING   = 0x19;
-  public static final int ISO646_STRING    = 0x1A;
-  public static final int GENERAL_STRING   = 0x1B;
-
-  public static final int UTF8_STRING      = 0x0C;
-  public static final int UNIVERSAL_STRING = 0x1C;
-  public static final int BMP_STRING       = 0x1E;
-
-  public static final int UTC_TIME         = 0x17;
-  public static final int GENERALIZED_TIME = 0x18;
+  int UTC_TIME         = 0x17;
+  int GENERALIZED_TIME = 0x18;
 }
