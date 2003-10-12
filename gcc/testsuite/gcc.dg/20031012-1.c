58fffe70e7197e8d511b7c6c59bdc1fa87625cea
steven <steven@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-10-12 22:16:04 +0000

    * gcc.dg/20031012-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@72410 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20031012-1.c
@@ -0,0 +1,17 @@
+/* Origin: Andrew Morton <akpm@osdl.org> */
+/* Warn if a function addres of a non-weak function is used
+   as a truth value.  */
+/* See thread starting at http://gcc.gnu.org/ml/gcc/2003-10/msg00414.html  */
+
+void foo(void)
+{}
+
+void bar(void)
+{}
+
+int main() {
+	if (foo) /* { dg-warning "" } */
+		bar();
+	return 0;
+}
+
