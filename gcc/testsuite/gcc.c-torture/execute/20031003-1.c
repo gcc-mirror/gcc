2c667a0cbd4b6e0b37c7de16f17563f2a7973972
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-10-14 13:31:27 +0000

    PR optimization/9325 	* gcc.c-torture/execute/20031003-1.c: Remove non-portable tests 	for overflowing floating point to integer conversion during RTL 	simplification.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@72467 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/20031003-1.c
+++ gcc/testsuite/gcc.c-torture/execute/20031003-1.c
@@ -12,31 +12,12 @@ int f2()
   return (int)(float)(2147483647);
 }
 
-int f3()
-{
-  float a = 2147483648.0f;
-  return (int)a;
-}
-
-int f4()
-{
-  int a = 2147483647;
-  float b = (float)a;
-  return (int)b;
-}
-
 int main()
 {
   if (f1() != 2147483647)
     abort ();
   if (f2() != 2147483647)
     abort ();
-#ifdef __OPTIMIZE__
-  if (f3() != 2147483647)
-    abort ();
-  if (f4() != 2147483647)
-    abort ();
-#endif
   return 0;
 }
 
