29ee7b16fe9113fc6644ae48d4b94f1ee02fedfd
Zdenek Dvorak <rakdver@atrey.karlin.mff.cuni.cz>
2003-10-19 20:43:12 +0200

    * gcc.dg/old-style-asm-1.c: Use scan-assembler-times.
    From-SVN: r72682

--- gcc/testsuite/gcc.dg/old-style-asm-1.c
+++ gcc/testsuite/gcc.dg/old-style-asm-1.c
@@ -18,10 +18,9 @@ void foo(int v)
 
 /* The purpose of the test below is to check that there are two branches
    in the generated code, supposedly corresponding to the if-statements.
-   Warning: this is fragile and assumes that one of the generated labels
-   for the branches matches the string "L2", or as with
-   mmix-knuth-mmixware, "L:2".  That assumption is generally invalid,
+   Warning: this is fragile and assumes that the generated labels for the
+   branches contain letter "L".  That assumption is generally invalid,
    because for example it depends on the target macro
-   ASM_GENERATE_INTERNAL_LABEL to generate a name matching this regexp (as
-   with the default definition).  */
-/* { dg-final { scan-assembler "L(:|\\\$0*)?2" } } */
+   ASM_GENERATE_INTERNAL_LABEL to generate such a name (as with the default
+   definition).  */
+/* { dg-final { scan-assembler-times "L" 4 } } */
