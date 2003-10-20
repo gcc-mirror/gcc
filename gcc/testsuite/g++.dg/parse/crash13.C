f39591652e2fb0f59819f8394bd50e146b96d502
lerdsuwa <lerdsuwa@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-10-20 12:42:37 +0000

    PR c++/9781, c++/10583, c++/11862 	* decl.c (cp_finish_decl): Exit immediately if decl is an 	error_mark_node. 	* pt.c (push_template_decl_real): Return error_mark_node for 	invalid template declaration of variable.
    	* g++.dg/parse/crash13.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@72701 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/crash13.C
@@ -0,0 +1,22 @@
+// { dg-do compile }
+
+// Origin: Giovanni Bajo <giovannibajo@libero.it>
+
+// PR c++/10583: ICE using template function with invalid signature.
+
+template <typename> 
+struct A 
+{
+    struct B 
+    {};
+};
+
+template <typename T> 
+void func(A<T>::B* )	// { dg-error "variable|template|expression" }
+{			// { dg-error ";" }
+}
+
+int main() 
+{
+  func<void>(0);	// { dg-error "undeclared|expression|;" }
+}
