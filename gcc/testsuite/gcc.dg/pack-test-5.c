244b1c60bab6c63d71fc88969398ad1c8422c826
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-10-20 22:01:58 +0000

    PR c/11446         * stor-layout.c (layout_decl): Fix alignment handling.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@72725 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pack-test-5.c
@@ -0,0 +1,19 @@
+/* PR c/11446: packed on a struct takes precedence over aligned on the type
+   of a field.  */
+/* { dg-do run } */
+
+struct A {
+  double d;
+} __attribute__ ((aligned));
+
+struct B {
+  char c;
+  struct A a;
+} __attribute__ ((packed));
+
+int main ()
+{
+  if (sizeof (struct B) != sizeof (char) + sizeof (struct A))
+    abort ();
+  return 0;
+}
