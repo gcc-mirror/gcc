8d9f9c57e2f659ca9577a2d6195f0a470e257b5b
pme <pme@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-10-20 20:05:20 +0000

    2003-10-20  Phil Edwards  <phil@codesourcery.com>
    	* gcc.dg/20021014-1.c:  XFAIL for *-*-windiss targets.
    	* gcc.dg/nest.c:  Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@72715 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/20021014-1.c
+++ gcc/testsuite/gcc.dg/20021014-1.c
@@ -1,7 +1,7 @@
 /* { dg-do run } */
 /* { dg-options "-O2 -p" } */
 /* { dg-options "-O2 -p -static" { target hppa*-*-hpux* } } */
-/* { dg-error "profiler" "No profiler support" { target mmix-*-* xstormy16-*-* } 0 } */
+/* { dg-error "profiler" "No profiler support" { target mmix-*-* xstormy16-*-* *-*-windiss } 0 } */
 /* { dg-error "" "consider using `-pg' instead of `-p' with gprof(1)" { target *-*-freebsd* } 0 } */
 /* Support for -p on solaris2 relies on mcrt1.o which comes with the
    vendor compiler.  We cannot reiably predict the directory where the
