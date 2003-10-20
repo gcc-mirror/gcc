adccacc41df4afbdaaa6a5b45364a51708e56088
Zdenek Dvorak <rakdver@atrey.karlin.mff.cuni.cz>
2003-10-20 14:15:27 +0200

    * gcc.dg/old-style-asm-1.c: Count jump_insns instead of labels.
    From-SVN: r72698

--- gcc/testsuite/gcc.dg/old-style-asm-1.c
+++ gcc/testsuite/gcc.dg/old-style-asm-1.c
@@ -1,6 +1,6 @@
 /* PR inline-asm/8832 */
 /* { dg-do compile } */
-/* { dg-options "-O2" } */
+/* { dg-options "-O2 -dP" } */
 
 /* Verify that GCC doesn't optimize
    old style asm instructions.  */
@@ -18,9 +18,6 @@ void foo(int v)
 
 /* The purpose of the test below is to check that there are two branches
    in the generated code, supposedly corresponding to the if-statements.
-   Warning: this is fragile and assumes that the generated labels for the
-   branches contain letter "L".  That assumption is generally invalid,
-   because for example it depends on the target macro
-   ASM_GENERATE_INTERNAL_LABEL to generate such a name (as with the default
-   definition).  */
-/* { dg-final { scan-assembler-times "L" 4 } } */
+   It tries to check for jump_insn (set (pc) pattern, so that jump_insns
+   corresponding to return are not taken into account.  */
+/* { dg-final { scan-assembler-times "(?n)jump_insn.*set.*pc" 2 } } */
