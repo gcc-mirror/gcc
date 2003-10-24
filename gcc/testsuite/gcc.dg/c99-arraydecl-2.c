88682ff62d51b96c6278e30862068780f5eb6f1d
Joseph Myers <jsm@polyomino.org.uk>
2003-10-24 16:30:37 +0100

    re PR c/11943 (Accepts invalid declaration "int x[2, 3];" in C99 mode)
    	* c-parse.in (array_declarator): Use expr_no_commas.
    	Fixes PR c/11943.
    
    testsuite:
    	* gcc.dg/c99-arraydecl-2.c: New test.  PR c/11943.
    
    From-SVN: r72900

---
+++ gcc/testsuite/gcc.dg/c99-arraydecl-2.c
@@ -0,0 +1,16 @@
+/* Test for C99 array declarators: expression must be an
+   assignment-expression.  PR 11943.  */
+/* Origin: Joseph Myers <jsm@polyomino.org.uk> */
+/* { dg-do compile } */
+/* { dg-options "-std=iso9899:1999 -pedantic-errors" } */
+
+void
+foo (void)
+{
+  int a[2, 3]; /* { dg-error "parse|syntax" "bad array declarator" } */
+  void b(int x[2, 3]); /* { dg-error "parse|syntax" "bad array declarator" } */
+  void c(int [2, 3]); /* { dg-error "parse|syntax" "bad array declarator" } */
+  void d(int *x[restrict 2, 3]); /* { dg-error "parse|syntax" "bad array declarator" } */
+  void e(int *x[static restrict 2, 3]); /* { dg-error "parse|syntax" "bad array declarator" } */
+  void f(int *x[restrict static 2, 3]); /* { dg-error "parse|syntax" "bad array declarator" } */
+}
