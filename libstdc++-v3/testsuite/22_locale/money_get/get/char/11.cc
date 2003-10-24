e646c6e0648eae7a5806bd3cb2e6aebfbdd121ca
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-10-24 22:51:15 +0000

    2003-10-24  Paolo Carlini  <pcarlini@suse.de>
    	* include/bits/locale_facets.tcc (money_get::do_get(...,
    	long double&): Properly size the temporary buffer.
    	* testsuite/22_locale/money_get/get/char/11.cc: New.
    	* testsuite/22_locale/money_get/get/wchar_t/11.cc: Ditto.
    
    	* include/bits/locale_facets.tcc (num_put::_M_group_int,
    	num_put::_M_group_float, money_put::do_put(..., const
    	string_type&), collate::do_compare, collate::do_transform):
    	Prefer basic_string::data() to c_str() when the '\0'
    	terminator is not really needed.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@72911 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/22_locale/money_get/get/char/11.cc
@@ -0,0 +1,63 @@
+// 2003-10-24  Paolo Carlini  <pcarlini@suse.de>
+
+// Copyright (C) 2003 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 22.2.6.1.1 money_get members
+
+#include <locale>
+#include <sstream>
+#include <limits>
+#include <testsuite_hooks.h>
+
+void test01()
+{
+  using namespace std;
+  typedef istreambuf_iterator<char> iterator_type;
+
+  bool test __attribute__((unused)) = true;
+  
+  // basic construction
+  locale loc_de = __gnu_test::try_named_locale("de_DE@euro");
+
+  iterator_type end;
+  istringstream iss;
+  iss.imbue(loc_de);
+
+  // cache the money_get facet
+  const money_get<char>& mon_get = use_facet<money_get<char> >(iss.getloc()); 
+
+  // A _very_ big amount.
+  string str = "1";
+  for (int i = 0; i < 2 * numeric_limits<long double>::digits10; ++i)
+    str += ".000";
+  str += ",00 ";
+
+  iss.str(str);
+  iterator_type is_it01(iss);
+  long double result1;
+  ios_base::iostate err01 = ios_base::goodbit;
+  mon_get.get(is_it01, end, true, iss, err01, result1);
+  VERIFY( err01 == ios_base::eofbit );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
