02da88032cce17c466c10795943664d1df6a56ac
korbb <korbb@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-10-25 18:45:39 +0000

    Results are re-alphabetized, too
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@72932 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fixinc/tests/base/stdio.h
+++ gcc/fixinc/tests/base/stdio.h
@@ -26,16 +26,16 @@ int vfscanf(FILE *, const char *, __builtin_va_list) __asm__ (_BSD_STRING(__USER
 #endif  /* BSD_STDIO_ATTRS_CONFLICT_CHECK */
 
 
-#if defined( HPUX11_VSNPRINTF_CHECK )
-extern int vsnprintf(char *, _hpux_size_t, const char *, __gnuc_va_list);
-#endif  /* HPUX11_VSNPRINTF_CHECK */
-
-
 #if defined( HPUX11_SNPRINTF_CHECK )
 extern int snprintf(char *, size_t, const char *, ...);
 extern int snprintf(char *, _hpux_size_t, const char *, ...);
 extern int snprintf(char *, _hpux_size_t, const char *, ...);
 #endif  /* HPUX11_SNPRINTF_CHECK */
+
+
+#if defined( HPUX11_VSNPRINTF_CHECK )
+extern int vsnprintf(char *, _hpux_size_t, const char *, __gnuc_va_list);
+#endif  /* HPUX11_VSNPRINTF_CHECK */
 
 
 #if defined( IRIX_STDIO_DUMMY_VA_LIST_CHECK )
