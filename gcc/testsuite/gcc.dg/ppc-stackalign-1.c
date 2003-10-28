581c0acefc3d8756fdf0c0f70a010167ba1303a1
No Author <no-author@gcc.gnu.org>
2003-10-28 21:15:17 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_3-branch'.
    From-SVN: r73020

---
+++ gcc/testsuite/gcc.dg/ppc-stackalign-1.c
@@ -0,0 +1,34 @@
+/* { dg-do run { target powerpc-*-linux* powerpc-*-sysv* } } */
+
+/* Test stack pointer alignment against variable alloca.  */
+/* Inspired by PR libgcj/10610.  */
+/* Origin: Franz Sirl <Franz.Sirl-kernel@lauterbach.com>.  */
+
+extern void abort (void);
+extern void exit (int);
+
+register unsigned long sp __asm__ ("r1");
+
+void g (int * val __attribute__ ((unused)))
+{
+  if (sp & 0xf)
+    abort ();
+}
+
+void f (int val)
+{
+  int *val1 = __builtin_alloca (val);
+
+  g (val1);
+  return;
+}
+
+int main (void)
+{
+  int i;
+
+  for (i = 1; i < 32; i++)
+    f (i);
+  
+  exit (0);
+}
