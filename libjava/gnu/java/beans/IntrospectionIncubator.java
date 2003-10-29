2035efe7fbfe3205e24f476402d7504650999530
Dalibor Topic <robilad@kaffe.org>
2003-10-29 15:22:01 +0000

    IntrospectionIncubator.java (addMethod): Add public static methods.
    2003-10-29  Dalibor Topic  <robilad@kaffe.org>
    
    	* gnu/java/beans/IntrospectionIncubator.java (addMethod): Add public
    	static methods.
    
    From-SVN: r73043

--- libjava/gnu/java/beans/IntrospectionIncubator.java
+++ libjava/gnu/java/beans/IntrospectionIncubator.java
@@ -67,7 +67,7 @@ public class IntrospectionIncubator {
 
 	/* Paving the way for automatic Introspection */
 	public void addMethod(Method method) {
-		if(Modifier.isPublic(method.getModifiers()) && !Modifier.isStatic(method.getModifiers())) {
+		if(Modifier.isPublic(method.getModifiers())) {
 			String name = ClassHelper.getTruncatedName(method.getName());
 			Class retType = method.getReturnType();
 			Class[] params = method.getParameterTypes();
