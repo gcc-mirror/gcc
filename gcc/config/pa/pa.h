8c05c4bc18309d0a708a149feb8aa5ebd1697542
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-10-29 20:17:50 +0000

    * pa.h (ASM_COMMENT_START): Define.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@73059 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/pa/pa.h
+++ gcc/config/pa/pa.h
@@ -1673,6 +1673,12 @@ do { 									\
 
 /* Control the assembler format that we output.  */
 
+/* A C string constant describing how to begin a comment in the target
+   assembler language.  The compiler assumes that the comment will end at
+   the end of the line.  */
+
+#define ASM_COMMENT_START ";"
+
 /* Output to assembler file text saying following lines
    may contain character constants, extra white space, comments, etc.  */
 
