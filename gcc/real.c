930177d9f0ab0015f02f4961cda28ab63dfd468a
Alexander Kabaev <ak03@gte.com>
2003-11-03 23:27:51 +0000

    * real.c (encode_ieee_single): Ensure proper promotion.
    From-SVN: r73232

--- gcc/real.c
+++ gcc/real.c
@@ -2536,9 +2536,10 @@ encode_ieee_single (const struct real_format *fmt, long *buf,
 		    const REAL_VALUE_TYPE *r)
 {
   unsigned long image, sig, exp;
+  unsigned long sign = r->sign;
   bool denormal = (r->sig[SIGSZ-1] & SIG_MSB) == 0;
 
-  image = r->sign << 31;
+  image = sign << 31;
   sig = (r->sig[SIGSZ-1] >> (HOST_BITS_PER_LONG - 24)) & 0x7fffff;
 
   switch (r->class)
