aad626f71c5a3a3eac7e2946fa1e89655c271bbb
Kriang Lerdsuwanakij <lerdsuwa@users.sourceforge.net>
2003-11-05 14:37:57 +0000

    re PR c++/11616 (Error message "instantiated from here" gives wrong location)
    	PR c++/11616
    	* pt.c (instantiate_pending_templates): Save and restore
    	input_location.
    
    	* g++.dg/template/instantiate5.C: New test.
    
    From-SVN: r73269

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -11037,6 +11037,7 @@ instantiate_pending_templates (void)
   tree last = NULL_TREE;
   int instantiated_something = 0;
   int reconsider;
+  location_t saved_loc = input_location;
   
   do 
     {
@@ -11111,6 +11112,7 @@ instantiate_pending_templates (void)
     } 
   while (reconsider);
 
+  input_location = saved_loc;
   return instantiated_something;
 }
 
