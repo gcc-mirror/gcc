76c7f39df919be6dde5a11f205a9e6eda17aa0d1
lerdsuwa <lerdsuwa@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-11-05 14:37:57 +0000

    PR c++/11616 	* pt.c (instantiate_pending_templates): Save and restore 	input_location.
    	* g++.dg/template/instantiate5.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@73269 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/instantiate5.C
@@ -0,0 +1,24 @@
+// { dg-do compile }
+
+// Origin: Volker Reichelt <reichelt@gcc.gnu.org>
+
+// PR c++/11616: Incorrect line number in diagnostics
+
+template <int> struct A
+{
+  static const int i=0;
+};
+
+int baz() { return A<0>::i; }
+
+struct B
+{
+  static void foo (int);	// { dg-error "candidates" }
+};
+
+template <typename T> struct C
+{
+  virtual void bar() const { T::foo(); } // { dg-error "no matching function" }
+};
+
+C<B> c;				// { dg-error "instantiated" }
