5ca6eb4aa2b10c1f8bdf703451656075ca59bd98
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-11-09 10:47:58 +0000

    2003-11-09  Paolo Carlini  <pcarlini@suse.de>
    	PR libstdc++/12971
    	* include/bits/locale_facets.tcc
    	(money_put::do_put(..., long double)): Fix conversion
    	specification as per DR 328 [WP].
    	* testsuite/22_locale/money_put/put/char/12971.cc: Add.
    	* testsuite/22_locale/money_put/put/wchar_t/12971.cc: Ditto.
    	* docs/html/ext/howto.html: Add entry for DR 328.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@73389 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/22_locale/money_put/put/char/12971.cc
@@ -0,0 +1,50 @@
+// 2003-11-09 Paolo Carlini <pcarlini@suse.de>
+
+// Copyright (C) 2003 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 22.2.6.2.1 money_put members
+
+#include <locale>
+#include <sstream>
+#include <testsuite_hooks.h>
+
+// libstdc++/12971
+void test01()
+{
+  using namespace std;
+  bool test __attribute__((unused)) = true;
+
+  typedef ostreambuf_iterator<char> iterator_type;
+
+  long double amount = 10.8L;
+  
+  // cache the money_put facet
+  ostringstream oss;
+  const money_put<char>& mon_put = use_facet<money_put<char> >(oss.getloc()); 
+
+  iterator_type os_it01 = mon_put.put(oss.rdbuf(), true, oss, ' ', amount);
+  string result = oss.str();
+  VERIFY( result == "11" );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
