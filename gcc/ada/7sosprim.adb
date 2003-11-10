8ab65b8f0f06d7137ee19d17894c805c279ff502
aj <aj@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-11-10 15:54:35 +0000

    * 7sosprim.adb: tv_usec of struct_timeval and time_t are long 	integer.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@73419 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/7sosprim.adb
+++ gcc/ada/7sosprim.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                  B o d y                                 --
 --                                                                          --
---          Copyright (C) 1998-2002 Free Software Foundation, Inc.          --
+--          Copyright (C) 1998-2003 Free Software Foundation, Inc.          --
 --                                                                          --
 -- GNARL is free software; you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -47,11 +47,11 @@ package body System.OS_Primitives is
    pragma Convention (C, struct_timezone);
    type struct_timezone_ptr is access all struct_timezone;
 
-   type time_t is new Integer;
+   type time_t is new Long_Integer;
 
    type struct_timeval is record
       tv_sec       : time_t;
-      tv_usec      : Integer;
+      tv_usec      : Long_Integer;
    end record;
    pragma Convention (C, struct_timeval);
 
