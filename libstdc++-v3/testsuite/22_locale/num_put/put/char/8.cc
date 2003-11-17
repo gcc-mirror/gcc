6a7ee0d62ed3a4aabfbbaa1700d75dcc2a07c9d7
Paolo Carlini <pcarlini@suse.de>
2003-11-17 08:39:57 +0000

    8.cc: New test, summarizing the discussion ensuing libstdc++/12988.
    2003-11-17  Paolo Carlini  <pcarlini@suse.de>
    
    	* testsuite/22_locale/num_put/put/char/8.cc: New test,
    	summarizing the discussion ensuing libstdc++/12988.
    	* testsuite/22_locale/num_put/put/wchar_t/8.cc: Ditto.
    
    From-SVN: r73665

---
+++ libstdc++-v3/testsuite/22_locale/num_put/put/char/8.cc
@@ -0,0 +1,68 @@
+// Copyright (C) 2003 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 22.2.2.2.1  num_put members
+
+#include <locale>
+#include <sstream>
+#include <testsuite_hooks.h>
+
+struct Ctype: std::ctype<char>
+{
+  char
+  do_widen(char c) const 
+  { return 'A' + c % 26; }
+
+  const char*
+  do_widen(const char* lo, const char* hi, char* to) const
+  {
+    for (; lo != hi; *to++ = Ctype::do_widen(*lo++));
+    return hi;
+  } 
+};
+
+// See http://gcc.gnu.org/ml/libstdc++/2003-11/msg00154.html
+void test01()
+{
+  using namespace std;
+  bool test __attribute__((unused)) = true;
+
+  ostringstream oss;
+  oss.imbue(locale(locale::classic(), new Ctype));
+  const num_put<char>& np = use_facet<num_put<char> >(oss.getloc());
+
+  const string empty;
+  string result;
+  long inum = 123;
+  double fnum = 123.456;
+
+  np.put(oss.rdbuf(), oss, '+', inum);
+  result = oss.str();
+  VERIFY( result == "XYZ" );
+
+  oss.clear();
+  oss.str(empty);
+  np.put(oss.rdbuf(), oss, '+', fnum);
+  result = oss.str();
+  VERIFY( result == "XYZ.ABC" );
+}
+
+int main()
+{
+  test01();
+}
