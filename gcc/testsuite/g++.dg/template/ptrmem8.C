1c59130c86e8a30c7767ca22b45ef4a95b0133d0
Giovanni Bajo <giovannibajo@gcc.gnu.org>
2003-12-02 15:50:44 +0000

    re PR c++/10126 (ICE in convert_nontype_argument on using overload resolution in template args)
    	PR c++/10126
    	* g++.dg/template/ptrmem8.C: New test.
    
    From-SVN: r74185

---
+++ gcc/testsuite/g++.dg/template/ptrmem8.C
@@ -0,0 +1,20 @@
+// { dg-do compile }
+// Origin: <marco dot franzen at bigfoot dot com> 
+// PR c++/10126: Handle ptmf default conversions while matching a template 
+//  argument
+
+struct B 
+{
+  int I () const;
+  int I ();
+};
+
+struct D : B {};
+
+template <int (D::*fun)() const> int Get();
+
+int main () 
+{
+  Get<&B::I>();   // { dg-error "no matching function" }
+  Get<&D::I>();   // { dg-error "no matching function" }
+}
