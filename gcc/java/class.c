f8e17ceaf23a78d3697f3a71d5b514a5ad9a0d74
mkoch <mkoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-12-03 21:26:26 +0000

    2003-12-03  Michael Koch  <konqueror@gmx.de>
    	* class.c (make_class_data):
    	Push field value to 'hack_signers' instead of 'signers'.
    	* decl.c (java_init_decl_processing):
    	Push field 'hack_signers' instead of 'signers'.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@74246 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/java/class.c
+++ gcc/java/class.c
@@ -1568,7 +1568,7 @@ make_class_data (tree type)
   PUSH_FIELD_VALUE (cons, "idt", null_pointer_node);
   PUSH_FIELD_VALUE (cons, "arrayclass", null_pointer_node);
   PUSH_FIELD_VALUE (cons, "protectionDomain", null_pointer_node);
-  PUSH_FIELD_VALUE (cons, "signers", null_pointer_node);
+  PUSH_FIELD_VALUE (cons, "hack_signers", null_pointer_node);
   PUSH_FIELD_VALUE (cons, "chain", null_pointer_node);
 
   FINISH_RECORD_CONSTRUCTOR (cons);
