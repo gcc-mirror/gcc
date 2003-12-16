1cd6548d24fbcdffbdc3b852dbf9306d6d276893
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-12-16 19:50:54 +0000

    cp: 	PR c++/9043 	C++ ABI change: Mangling array indices in templates. 	* decl.c (compute_array_index_type): Reorganize for earlier 	template errors. Use value_dependent_expression_p for abi-2. 	* mangle.c (write_array_type): Check broken mangling for 	expression indices on abi-1 testsuite: 	PR c++/9043 	* g++.dg/abi/mangle20-1.C: New test. 	* g++.dg/abi/mangle20-2.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@74707 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/abi/mangle20-1.C
@@ -0,0 +1,19 @@
+// { dg-do compile }
+// { dg-options "-fabi-version=2" }
+
+// Copyright (C) 2003 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 15 Dec 2003 <nathan@codesourcery.com>
+
+// PR 9043
+// mangled array types in templates
+
+template <int I> void f(int (*)[2]) {}
+template <int I> void g(int (*)[I+2]) {}
+
+static const int I=1;
+static const int J=2;
+
+template void f<1>(int (*)[2]);
+//  { dg-final { scan-assembler "\n_Z1fILi1EEvPA2_i:" } }
+template void g<1>(int (*)[3]);
+//  { dg-final { scan-assembler "\n_Z1gILi1EEvPAplT_Li2E_i:" } }
---
+++ gcc/testsuite/g++.dg/abi/mangle20-2.C
@@ -0,0 +1,16 @@
+// { dg-do compile }
+// { dg-options "-fabi-version=1 -Wabi" }
+
+// Copyright (C) 2003 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 15 Dec 2003 <nathan@codesourcery.com>
+
+// PR 9043
+// mangled array types in templates
+
+template <int I> void f(int (*)[2]) {}
+template <int I> void g(int (*)[I+2]) {}
+
+template void f<1>(int (*)[2]);  // { dg-warning "mangled name" }
+//  { dg-final { scan-assembler "\n_Z1fILi1EEvPALi2E_i:" } }
+template void g<1>(int (*)[3]);
+//  { dg-final { scan-assembler "\n_Z1gILi1EEvPAplT_Li2E_i:" } }
