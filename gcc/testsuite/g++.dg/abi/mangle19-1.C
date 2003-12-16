25f4fc1e63e00966fd60eace8fc99eaa6c695f38
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-12-16 10:03:54 +0000

    cp: 	PR c++/13242 	C++ ABI change. Mangling template parameters of reference type 	* mangle.c (write_template_args): Remove unreachable code. 	(write_template_arg): Look through an argument of reference type. testsuite: 	PR c++/13242 	* g++.dg/abi/mangle19-1.C: New test. 	* g++.dg/abi/mangle19-2.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@74682 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/abi/mangle19-1.C
@@ -0,0 +1,13 @@
+// { dg-do compile }
+// { dg-options "-fabi-version=2" }
+
+// Copyright (C) 2003 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 15 Dec 2003 <nathan@codesourcery.com>
+
+// PR 13242
+// mangled template arguments that are external objects incorrectly
+
+extern int N;
+template <int &> struct S {};
+void n (S<N>) {}
+// { dg-final { scan-assembler "\n_Z1n1SILZ1NEE:" } }
