b240b5a250761c80f9f2116fff5ffe06f9a09a43
Fariborz Jahanian <fjahanian@gcc.gnu.org>
2003-12-19 23:35:40 +0000

    Test to test -mdynamic-no-pic -O2 -mpowerpc64 on Darwin.
    From-SVN: r74858

---
+++ gcc/testsuite/gcc.dg/macho-lo-sum.c
@@ -0,0 +1,29 @@
+/* { dg-do compile { target powerpc*-*-darwin* } } */
+/* { dg-options "-O2 -force_cpusubtype_ALL -mpowerpc64 -mdynamic-no-pic" } */
+
+long long knight_attacks[64];
+long long InitializeAttackBoards(void);
+
+int main()
+{
+	return InitializeAttackBoards();
+}
+
+long long InitializeAttackBoards(void)
+{
+
+  int i,j;
+
+  for(i=0;i<64;i++) { }
+
+  for(i=0;i<64;i++) {
+    knight_attacks[i]=0;
+    for(j=0;j<8;j++) {
+      knight_attacks[i]= 0;
+    }
+  }
+
+  return knight_attacks[0];
+
+}
+
