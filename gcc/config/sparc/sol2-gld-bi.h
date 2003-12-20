1713a69f0a1af440caa41ab9e60490ecd7247922
Eric Botcazou <ebotcazou@libertysurf.fr>
2003-12-20 08:40:24 +0100

    re PR other/7956 (unreadable error on -m64 with --disable-multilib)
    	PR other/7956
    	* genmultilib: New variable disable_multilib.  Set it to 'yes'
    	if enable_multilib was set to 'no'.  Emit DISABLE_MULTILIB
    	if disable_multilib was set to 'yes'.
    	* gcc.c: Include multilib.h before tm.h.
    	* config/sparc/sol2-bi.h (LINK_ARCH_SPEC): Emit an error
    	message for multiarch options if DISABLE_MULTILIB is set.
    	* config/sparc/sol2-gld-bi.h (LINK_ARCH_SPEC): Likewise.
    
    From-SVN: r74875

--- gcc/config/sparc/sol2-gld-bi.h
+++ gcc/config/sparc/sol2-gld-bi.h
@@ -10,9 +10,25 @@
   LINK_ARCH64_SPEC_BASE "%{!static: -rpath-link %R/usr/lib/sparcv9}"
 
 #undef LINK_ARCH_SPEC
+#if DISABLE_MULTILIB
+#if DEFAULT_ARCH32_P
+#define LINK_ARCH_SPEC "\
+%{m32:-m elf32_sparc %(link_arch32)} \
+%{m64:%edoes not support multilib} \
+%{!m32:%{!m64:%(link_arch_default)}} \
+"
+#else
+#define LINK_ARCH_SPEC "\
+%{m32:%edoes not support multilib} \
+%{m64:-m elf64_sparc %(link_arch64)} \
+%{!m32:%{!m64:%(link_arch_default)}} \
+"
+#endif
+#else
 #define LINK_ARCH_SPEC "\
 %{m32:-m elf32_sparc %(link_arch32)} \
 %{m64:-m elf64_sparc %(link_arch64)} \
 %{!m32:%{!m64:%(link_arch_default)}} \
 "
+#endif
 
