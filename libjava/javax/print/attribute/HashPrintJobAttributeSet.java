cd5ad6e5d869f94105e614a86060e213a1172a73
mkoch <mkoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-12-21 19:54:52 +0000

    2003-12-21  Michael Koch  <konqueror@gmx.de>
    	* javax/print/attribute/AttributeSetUtilities.java,
    	javax/print/attribute/HashAttributeSet.java,
    	javax/print/attribute/HashDocAttributeSet.java,
    	javax/print/attribute/HashPrintJobAttributeSet.java,
    	javax/print/attribute/HashPrintRequestAttributeSet.java,
    	javax/print/attribute/HashPrintServiceAttributeSet.java: New files.
    	* Makefile.am (ordinary_java_source_files): Added new files.
    	* Makefile.in: Regenerated.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@74914 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/javax/print/attribute/HashPrintJobAttributeSet.java
@@ -0,0 +1,94 @@
+/* HashPrintJobAttributeSet.java -- 
+   Copyright (C) 2003 Free Software Foundation, Inc.
+
+This file is part of GNU Classpath.
+
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+Linking this library statically or dynamically with other modules is
+making a combined work based on this library.  Thus, the terms and
+conditions of the GNU General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this library give you
+permission to link this library with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module.  An independent module is a module which is not derived from
+or based on this library.  If you modify this library, you may extend
+this exception to your version of the library, but you are not
+obligated to do so.  If you do not wish to do so, delete this
+exception statement from your version. */
+
+package javax.print.attribute;
+
+import java.io.Serializable;
+
+public class HashPrintJobAttributeSet extends HashAttributeSet
+  implements Serializable, PrintJobAttributeSet
+{
+  private static final long serialVersionUID = -4204473656070350348L;
+  
+  /**
+   * Creates an empty <code>HashPrintJobAttributeSet</code> object.
+   */
+  public HashPrintJobAttributeSet()
+  {
+    super(PrintJobAttribute.class);
+  }
+  
+  /**
+   * Creates a <code>HashPrintJobAttributeSet</code> object with the given
+   * attribute in it.
+   *
+   * @param attribute the attriute tu put into the attribute set
+   *
+   * @exception NullPointerException if attribute is null
+   */
+  public HashPrintJobAttributeSet(PrintJobAttribute attribute)
+  {
+    super(attribute, PrintJobAttribute.class);
+  }
+  
+  /**
+   * Creates a <code>HashPrintJobAttributeSet</code> object with the given
+   * attributes in it.
+   *
+   * @param attributes the attributes to put into the attribute set
+   *
+   * @exception NullPointerException if attributes is null
+   */
+  public HashPrintJobAttributeSet(PrintJobAttribute[] attributes)
+  {
+    super(attributes, PrintJobAttribute.class);
+  }
+  
+  /**
+   * Creates a <code>HashPrintJobAttributeSet</code> object with the given
+   * attributes in it.
+   *
+   * @param attributes the attributes to put into the attribute set
+   *
+   * @exception ClassCastException if any element of attributes is not
+   * an instance of <code>PrintJobAttribute</code>
+   */
+  public HashPrintJobAttributeSet(PrintJobAttributeSet attributes)
+  {
+    super(attributes, PrintJobAttribute.class);
+  }
+}
