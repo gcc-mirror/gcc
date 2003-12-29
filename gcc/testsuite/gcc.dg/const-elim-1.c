8b7c5b072e7408e91a1553a77a0e09c8e992d9ba
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-12-29 02:33:15 +0000

    * gcc.dg/const-elim-1.c: XFAIL on arm-*-*.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@75191 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/const-elim-1.c
+++ gcc/testsuite/gcc.dg/const-elim-1.c
@@ -7,7 +7,10 @@
 
 /* { dg-do compile } */
 /* { dg-options "-O2 -std=c99" } */
-/* { dg-final { scan-assembler-not "L\\\$?C\[^A-Z\]" } } */
+/* This test fails on ARM cores because we use a block move to
+   initialize "S" in test2, and GCC is not clever enough to eliminate
+   the block move.  */
+/* { dg-final { scan-assembler-not "L\\\$?C\[^A-Z\]" { xfail arm-*-* } } } */
 
 #define I (__extension__ 1.0iF)
 
