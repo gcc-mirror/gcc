14852613aba9c899214235e6e9fead1f0e8f2121
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-12-30 19:36:51 +0000

    * g++.dg/abi/vbase10.C: XFAIL on arm*-*-*.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@75241 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/abi/vbase10.C
+++ gcc/testsuite/g++.dg/abi/vbase10.C
@@ -3,5 +3,11 @@
 
 struct A { virtual void f(); char c1; };
 struct B { B(); char c2; };
-struct C : public A, public virtual B {}; // { dg-warning "ABI" }
+// On ARM processors, the alignment of B will be 4 even though it
+// contains only a single "char".  That avoids the situation that the
+// warning below is designed to catch.  On ARM NetBSD, the alignment
+// of B will be only 1 -- but there is no way to tell DejaGNU that a
+// failure is expected on all ARM targets except arm*-*-netbsd*.
+// Therefore, this test will XPASS on arm*-*-netbsd*.
+struct C : public A, public virtual B {}; // { dg-warning "ABI" "" { xfail arm*-*-* } }
 
