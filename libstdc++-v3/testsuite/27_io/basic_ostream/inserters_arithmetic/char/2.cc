082e1a1b81a54bc1266c75bce7d52d2c20148eb3
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-04 23:19:00 +0000

    2003-01-04  Paolo Carlini  <pcarlini@suse.de>
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/01.cc:
    	Remove redundant #includes.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/02.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/03.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/06.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/07.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/08.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/09.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/10.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/11.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/13.cc:
    	Likewise.
    	* testsuite/27_io/basic_ostream/inserters_arithmetic/char/1.cc:
    	Likewise.
    	* testsuite/27_io/basic_ostream/inserters_arithmetic/char/2.cc:
    	Likewise.
    	* testsuite/27_io/basic_ostream/inserters_arithmetic/wchar_t/1.cc:
    	Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@75398 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/27_io/basic_ostream/inserters_arithmetic/char/2.cc
+++ libstdc++-v3/testsuite/27_io/basic_ostream/inserters_arithmetic/char/2.cc
@@ -19,7 +19,6 @@
 // Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
 // USA.
 
-#include <cstdio> // for sprintf
 #include <iostream>
 #include <iomanip>
 #include <locale>
--- libstdc++-v3/testsuite/27_io/basic_ostream/inserters_arithmetic/wchar_t/1.cc
+++ libstdc++-v3/testsuite/27_io/basic_ostream/inserters_arithmetic/wchar_t/1.cc
@@ -19,9 +19,6 @@
 // Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
 // USA.
 
-#include <cstdio> // for sprintf
-#include <cmath> // for abs
-#include <cfloat> // for DBL_EPSILON
 #include <iostream>
 #include <iomanip>
 #include <sstream>
