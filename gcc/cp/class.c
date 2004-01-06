962c0823997cba18cca85efec200b19fac8e2c87
Mark Mitchell <mark@codesourcery.com>
2004-01-06 00:52:10 +0000

    re PR c++/12815 (Code compiled with optimization behaves unexpectedly)
    	PR c++/12816
    	* class.c (build_vtbl_ref_1): Do not unconditionally mark vtable
    	references as constant.
    
    	PR c++/12815
    	* g++.dg/rtti/typeid4.C: New test.
    
    From-SVN: r75457

--- gcc/cp/class.c
+++ gcc/cp/class.c
@@ -455,7 +455,7 @@ build_vtbl_ref_1 (tree instance, tree idx)
   assemble_external (vtbl);
 
   aref = build_array_ref (vtbl, idx);
-  TREE_CONSTANT (aref) = 1;
+  TREE_CONSTANT (aref) |= TREE_CONSTANT (vtbl) && TREE_CONSTANT (idx);
 
   return aref;
 }
