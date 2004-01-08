72827ed7fca83bc09c52813ff8f9de0f89151bad
Mark Mitchell <mark@codesourcery.com>
2004-01-08 03:32:11 +0000

    * g++.dg/abi/vbase10.C: Use -mstructure-size-boundary=8 on ARM.
    From-SVN: r75529

--- gcc/testsuite/g++.dg/abi/vbase10.C
+++ gcc/testsuite/g++.dg/abi/vbase10.C
@@ -1,13 +1,12 @@
 // { dg-do compile }
 // { dg-options "-Wabi -fabi-version=1" }
+// On ARM processors, the alignment of B will be 4 even though it
+// contains only a single "char".  That would avoids the situation
+// that the warning below is designed to catch.  We therefore
+// explicitly set the default structure alignment to 1.
+// { dg-options "-Wabi -fabi-version=1 -mstructure-size-boundary=8" { target arm*-*-* } }
 
 struct A { virtual void f(); char c1; };
 struct B { B(); char c2; };
-// On ARM processors, the alignment of B will be 4 even though it
-// contains only a single "char".  That avoids the situation that the
-// warning below is designed to catch.  On ARM NetBSD, the alignment
-// of B will be only 1 -- but there is no way to tell DejaGNU that a
-// failure is expected on all ARM targets except arm*-*-netbsd*.
-// Therefore, this test will XPASS on arm*-*-netbsd*.
-struct C : public A, public virtual B {}; // { dg-warning "ABI" "" { xfail arm*-*-* } }
+struct C : public A, public virtual B {}; // { dg-warning "ABI" }
 
