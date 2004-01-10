fc339d020ffe792a4e8253a8977b6423133c9a3e
Nathanael Nerode <neroden@gcc.gnu.org>
2004-01-10 00:02:25 +0000

    backport: [multiple changes]
    2004-01-02  Nathanael Nerode  <neroden@gcc.gnu.org>
    
    	PR target/13069
    	Backport the following from mainline:
    
    	2003-03-08  Neil Booth  <neil@daikokuya.co.uk>
            * config/sh/rtemself.h (TARGET_OS_CPP_BUILTINS): Use instead of
            CPP_PREDEFINES.
            * config/sh/rtems.h (TARGET_OS_CPP_BUILTINS): Use instead of
            CPP_PREDEFINES.
    
    	2003-09-27  Kelley Cook  <kcook@gcc.gnu.org>
    	* config/sh/rtemself.h, config/sh/rtems.h: GNU CC -> GCC.
    
    From-SVN: r75612

--- gcc/config/sh/rtemself.h
+++ gcc/config/sh/rtemself.h
@@ -2,25 +2,26 @@
    Copyright (C) 1997, 1998, 2000, 2002 Free Software Foundation, Inc.
    Contributed by Joel Sherrill (joel@OARcorp.com).
 
-This file is part of GNU CC.
+This file is part of GCC.
 
-GNU CC is free software; you can redistribute it and/or modify
+GCC is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2, or (at your option)
 any later version.
 
-GNU CC is distributed in the hope that it will be useful,
+GCC is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with GNU CC; see the file COPYING.  If not, write to
+along with GCC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
 /* Specify predefined symbols in preprocessor.  */
 
-#undef CPP_PREDEFINES
-#define CPP_PREDEFINES "-D__sh__ -D__ELF__ -D__rtems__ \
-  -Asystem=rtems -Acpu=sh -Amachine=sh"
+#define TARGET_OS_CPP_BUILTINS() do {		\
+  builtin_define( "__rtems__" );		\
+  builtin_assert( "system=rtems" );		\
+} while (0)
