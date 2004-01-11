473ba0af9702922a066c820ebce00d0525e6741f
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-11 20:33:56 +0000

    PR c++/3478 	* g++.dg/parse/error10.C: New test. 	* g++.dg/template/arg2.C: Accept "invalid type" error.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@75689 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/error10.C
@@ -0,0 +1,15 @@
+// PR c++/3478
+// { dg-options "" }
+
+template <typename> struct A
+{
+    enum E {};
+};
+
+template <typename T> void foo()
+{
+  enum          A<void>::E e1;
+  typename      A<T>::E    e2;
+  enum          A<T>::E    e3;
+  enum typename A<T>::E    e4; // { dg-error "" }
+}
