6d758274dd736f60168d67ee4d9539fa9a7cf033
Richard Henderson <rth@gcc.gnu.org>
2004-01-11 15:58:28 -0800

    New test.
    From-SVN: r75696

---
+++ gcc/testsuite/gcc.dg/tls/alpha-1.c
@@ -0,0 +1,9 @@
+/* Make sure that we honor initial-exec.  */
+/* { dg-do compile { target alpha*-*-* } } */
+/* { dg-options "" } */
+
+static __thread int xyzzy __attribute__ ((tls_model ("initial-exec")));
+int foo(void) { return xyzzy; }
+
+/* { dg-final { scan-assembler "gottprel" } } */
+/* { dg-final { scan-assembler-not "tprel(lo|hi|16)" } } */
