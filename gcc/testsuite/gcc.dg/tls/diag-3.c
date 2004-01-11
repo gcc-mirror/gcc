68d4e2bcb6b856a24b6f596642547d6471e7a8e1
zack <zack@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-11 22:35:15 +0000

    * gcc.dg/tls/diag-3.c: Tweak dg-error regexp.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@75690 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tls/diag-3.c
+++ gcc/testsuite/gcc.dg/tls/diag-3.c
@@ -1,7 +1,7 @@
 /* Report invalid extern and __thread combinations.  */
 
 extern int j;		/* { dg-error "previous declaration" } */
-__thread int j;		/* { dg-error "follows non thread-local" } */
+__thread int j;		/* { dg-error "follows non-thread-local" } */
 
 extern __thread int i;	/* { dg-error "previous declaration" } */
 int i;			/* { dg-error "follows thread-local" } */
