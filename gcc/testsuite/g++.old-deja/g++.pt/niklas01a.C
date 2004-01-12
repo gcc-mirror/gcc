59147a629a7a3c98e43be16e325d46ef07e45e93
Ian Lance Taylor <ian@gcc.gnu.org>
2004-01-12 16:15:19 +0000

    re PR c++/4100 ([parser] friend qualifier accepted in definition of nested class)
    	PR c++/4100
    	* g++.dg/parse/friend4.C: New test.
    	* g++.old-deja/g++.pt/niklas01a.C: Mark an error where a class
    	definition is called a friend.
    
    From-SVN: r75727

--- gcc/testsuite/g++.old-deja/g++.pt/niklas01a.C
+++ gcc/testsuite/g++.old-deja/g++.pt/niklas01a.C
@@ -3,6 +3,6 @@
 struct A { // { dg-error "" } forward declaration
   friend struct B : A {		// { dg-error "" } 
     int x;
-  };
+  };	// { dg-error "" } class definition cannot be a friend
   int y;
 };
