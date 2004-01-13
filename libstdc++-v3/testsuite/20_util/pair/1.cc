cb029d0cedb26b2ecc43b609242ceafadfe29b69
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-13 18:57:45 +0000

    2004-01-13  Benjamin Kosnik  <bkoz@redhat.com>
    	* testsuite/performance/ifstream_extract_float.cc: New.
    	* testsuite/performance/ofstream_insert_float.cc: Float generation
    	matches above.
    
    	* 20_util/auto_ptr.cc, auto_ptr_neg.cc: Break into...
    	* 20_util/auto_ptr/1.cc: ...this.
    	* 20_util/auto_ptr/2.cc: Same.
    	* 20_util/auto_ptr/3.cc: Same.
    	* 20_util/auto_ptr/3946.cc: Same.
    	* 20_util/auto_ptr/4.cc: Same.
    	* 20_util/auto_ptr/5.cc: Same.
    	* 20_util/auto_ptr/6.cc: Same.
    	* 20_util/auto_ptr/7.cc: Same.
    	* 20_util/auto_ptr/assign_neg.cc
    	* 20_util/pairs.cc: Break into...
    	* 20_util/pair/1.cc: ...this.
    	* 20_util/pair/2.cc: Same.
    	* 20_util/pair/3.cc: Same.
    	* 20_util/pair/4.cc: Same.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@75811 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/20_util/pair/1.cc
+++ libstdc++-v3/testsuite/20_util/pair/1.cc
@@ -1,6 +1,6 @@
 // 2001-06-18  Benjamin Kosnik  <bkoz@redhat.com>
 
-// Copyright (C) 2001 Free Software Foundation, Inc.
+// Copyright (C) 2001, 2004 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -72,69 +72,8 @@ void test01()
   VERIFY( !(p_st_1 < p_st_2) );
 }
 
-// homogeneous
-void test02()
-{
-  bool test __attribute__((unused)) = true;
-
-  std::pair<bool, bool> p_bb_1(true, false);
-  std::pair<bool, bool> p_bb_2 = std::make_pair(true, false);
-  VERIFY( p_bb_1 == p_bb_2 );
-  VERIFY( !(p_bb_1 < p_bb_2) );
-}
-
-
-// const
-void test03()
-{
-  bool test __attribute__((unused)) = true;
-
-  const std::pair<bool, long> p_bl_1(true, 433);
-  const std::pair<bool, long> p_bl_2 = std::make_pair(true, 433);
-  VERIFY( p_bl_1 == p_bl_2 );
-  VERIFY( !(p_bl_1 < p_bl_2) );
-
-  const std::pair<const char*, float> p_sf_1("total enlightenment", 433.00);
-  const std::pair<const char*, float> p_sf_2 = 
-    std::make_pair("total enlightenment", 433.00);
-  VERIFY( p_sf_1 == p_sf_2 );
-  VERIFY( !(p_sf_1 < p_sf_2) );
-
-  const std::pair<const char*, gnu_obj> p_sg_1("enlightenment", gnu_obj(5));
-  const std::pair<const char*, gnu_obj> p_sg_2 = 
-    std::make_pair("enlightenment", gnu_obj(5));
-  VERIFY( p_sg_1 == p_sg_2 );
-  VERIFY( !(p_sg_1 < p_sg_2) );
-
-  const std::pair<gnu_t<long>, gnu_obj> p_st_1(gnu_t<long>(false), gnu_obj(5));
-  const std::pair<gnu_t<long>, gnu_obj> p_st_2 = 
-    std::make_pair(gnu_t<long>(false), gnu_obj(5));
-  VERIFY( p_st_1 == p_st_2 );
-  VERIFY( !(p_st_1 < p_st_2) );
-}
-
-// const&
-void test04()
-{
-  bool test __attribute__((unused)) = true;
-  const gnu_obj& obj1 = gnu_obj(5);
-  const std::pair<const char*, gnu_obj> p_sg_1("enlightenment", obj1);
-  const std::pair<const char*, gnu_obj> p_sg_2 = 
-    std::make_pair("enlightenment", obj1);
-  VERIFY( p_sg_1 == p_sg_2 );
-  VERIFY( !(p_sg_1 < p_sg_2) );
-
-  const gnu_t<long>& tmpl1 = gnu_t<long>(false);
-  const std::pair<gnu_t<long>, gnu_obj> p_st_1(tmpl1, obj1);
-  const std::pair<gnu_t<long>, gnu_obj> p_st_2 = std::make_pair(tmpl1, obj1);
-  VERIFY( p_st_1 == p_st_2 );
-  VERIFY( !(p_st_1 < p_st_2) );
-}
-
 int main() 
 { 
   test01(); 
-  test02();
-  test03();
-  test04();
+  return 0;
 }
