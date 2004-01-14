ac324f33382003d5841dce4913575786d99c1759
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-14 03:10:51 +0000

    * config/iq2000/iq2000-protos.h: Fix comment formatting. 	* config/iq2000/iq2000.c: Likewise. 	* config/iq2000/iq2000.md: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@75840 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/iq2000/iq2000-protos.h
+++ gcc/config/iq2000/iq2000-protos.h
@@ -16,7 +16,7 @@
    You should have received a copy of the GNU General Public License
    along with GCC; see the file COPYING.  If not, write to
    the Free Software Foundation, 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA. */
+   Boston, MA 02111-1307, USA.  */
 
 #ifndef GCC_IQ2000_PROTOS_H
 #define GCC_IQ2000_PROTOS_H
