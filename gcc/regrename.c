abbe8578d63babc4d94ecd35a70c281386dfd0ce
Kazu Hirata <kazu@cs.umass.edu>
2004-01-14 17:55:20 +0000

    * regrename.c (find_oldest_value_reg): Fix a warning.
    From-SVN: r75871

--- gcc/regrename.c
+++ gcc/regrename.c
@@ -1345,8 +1345,8 @@ find_oldest_value_reg (enum reg_class class, rtx reg, struct value_data *vd)
 	if (!TEST_HARD_REG_BIT (reg_class_contents[class], last))
 	  return NULL_RTX;
 
-      if (new = maybe_mode_change (oldmode, vd->e[regno].mode, mode, i,
-				   regno))
+      new = maybe_mode_change (oldmode, vd->e[regno].mode, mode, i, regno);
+      if (new)
 	{
 	  ORIGINAL_REGNO (new) = ORIGINAL_REGNO (reg);
 	  REG_ATTRS (new) = REG_ATTRS (reg);
