d22c417920998fe96973c74e5068cff8f3a47811
Danny Smith <dannysmith@users.sourceforge.net>
2004-01-14 07:31:39 +0000

    * g++.dg/ext/attrib9.C: Add dg-warnings.
    From-SVN: r75847

--- gcc/testsuite/g++.dg/ext/attrib9.C
+++ gcc/testsuite/g++.dg/ext/attrib9.C
@@ -1,5 +1,5 @@
-class __attribute__((unused)) C;
-struct __attribute__((unused)) S;
-union __attribute__((unused)) U;
+class __attribute__((unused)) C;	//  { dg-warning "type attributes" }
+struct __attribute__((unused)) S;	//  { dg-warning "type attributes" }
+union __attribute__((unused)) U;	//  { dg-warning "type attributes" }
 enum e {};
-enum __attribute__((unused)) e;
+enum __attribute__((unused)) e;		//  { dg-warning "type attributes" }
