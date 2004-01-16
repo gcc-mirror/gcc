a8e6c82ab09c4c7a8490bc3861049b1941b84c75
Mark Mitchell <mark@codesourcery.com>
2004-01-16 19:28:11 +0000

    re PR c++/13574 (array default initializer in class lets gcc consume all memory and die)
    	PR c++/13574
    	* decl.c (compute_array_index_type): Fix grammar in comment.
    	* init.c (build_zero_init): Handle zero-sized arrays correctly.
    
    	PR c++/13574
    	* g++.dg/ext/array1.C: New test.
    
    From-SVN: r75991

--- gcc/cp/decl.c
+++ gcc/cp/decl.c
@@ -6079,9 +6079,8 @@ compute_array_index_type (tree name, tree size)
 	    error ("size of array is negative");
 	  size = integer_one_node;
 	}
-      /* Except that an extension we allow zero-sized arrays.  We
-	 always allow them in system headers because glibc uses
-	 them.  */
+      /* As an extension we allow zero-sized arrays.  We always allow
+	 them in system headers because glibc uses them.  */
       else if (integer_zerop (size) && pedantic && !in_system_header)
 	{
 	  if (name)
