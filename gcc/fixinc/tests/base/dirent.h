fb2c26082deb6a8c52efe232e420462da9d45d52
Rainer Orth <ro@TechFak.Uni-Bielefeld.DE>
2004-01-16 20:58:27 +0000

    inclhack.def (alpha___extern_prefix): Renamed to ...
    	* fixinc/inclhack.def (alpha___extern_prefix): Renamed to ...
    	(alpha___extern_prefix_sys_stat): ... this.
    	Apply to <sys/mount.h>, too.
    	Tweak to match more variations.
    	* fixinc/tests/base/sys/stat.h: Adapt for new hackname.
    
    	* fixinc/inclhack.def (alpha___extern_prefix,
    	alpha___extern_prefix_standards): New hacks to obey
     	__PRAGMA_EXTERN_PREFIX.
    	* fixinc/tests/base/testing.h [ALPHA___EXTERN_PREFIX_CHECK]: New
    	test.
    	* fixinc/tests/base/standards.h: Likewise.
    
    	* fixincl/inclhack.def (alpha_pthread): Tweak to match more
    	variations.
    	New testcase.
    	* fixinc/tests/base/pthread.h: Handle it.
    
    	* fixincl/inclhack.def (bad_lval): Sort file list.
    	Add many missing files up to Tru64 UNIX V5.1B.
    	* gcc/fixinc/tests/base/libgen.h: Renamed to ...
    	* gcc/fixinc/tests/base/dirent.h: ... this to match new file list
    	order.
    
    	* fixinc/fixincl.x: Regenerate.
    
    From-SVN: r75994

--- gcc/fixinc/tests/base/dirent.h
+++ gcc/fixinc/tests/base/dirent.h
@@ -2,7 +2,7 @@
 
     It has been auto-edited by fixincludes from:
 
-	"fixinc/tests/inc/libgen.h"
+	"fixinc/tests/inc/dirent.h"
 
     This had to be done to correct non-standard usages in the
     original, manufacturer supplied header file.  */
