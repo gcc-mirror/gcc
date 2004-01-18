efcecd78557ae33ed1c838206ab1bbaba9b067f1
No Author <no-author@gcc.gnu.org>
2004-01-18 11:25:58 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_4-branch'.
    From-SVN: r76095

---
+++ libstdc++-v3/testsuite/21_strings/basic_string/element_access/wchar_t/4.cc
@@ -0,0 +1,50 @@
+// 2004-01-18  Paolo Carlini  <pcarlini@suse.de>
+
+// Copyright (C) 2004 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 21.3.4 basic_string element access
+
+#include <string>
+#include <testsuite_hooks.h>
+
+// http://gcc.gnu.org/ml/libstdc++/2004-01/msg00184.html
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace std;
+
+  for (int i = 0; i < 2000; ++i)
+    {
+      wstring str_01;
+
+      for (int j = 0; j < i; ++j)
+	str_01 += L'a';
+
+      str_01.reserve(i + 10);
+
+      const wstring str_02(str_01);
+      VERIFY( str_02[i] == L'\0' );
+    }
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
