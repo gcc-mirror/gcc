947b705a23ee35a84bae1df604b1540232618f74
aph <aph@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-20 15:18:33 +0000

    2004-01-20  Andrew Haley  <aph@redhat.com>
            * include/private/gcconfig.h (USE_MMAP): Define for all Linux.
    
            * configure.in: Comment change.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@76210 138bc75d-0d04-0410-961f-82ee72b054a4

--- boehm-gc/include/private/gcconfig.h
+++ boehm-gc/include/private/gcconfig.h
@@ -617,7 +617,8 @@
 #       define OS_TYPE "LINUX"
 #       define STACKBOTTOM ((ptr_t)0xf0000000)
 #       define USE_GENERIC_PUSH_REGS
-		/* We never got around to the assembly version. */
+#	     define USE_MMAP
+	      /* We never got around to the assembly version. */
 /* #       define MPROTECT_VDB - Reported to not work  9/17/01 */
 #       ifdef __ELF__
 #            define DYNAMIC_LOADING
