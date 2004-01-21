abfbb9391e96f95acfd84b1ad92383c71302a32d
No Author <no-author@gcc.gnu.org>
2004-01-21 02:40:59 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_3-branch'.
    From-SVN: r76251

---
+++ gcc/testsuite/g++.dg/template/instantiate6.C
@@ -0,0 +1,16 @@
+// { dg-do compile }
+
+// Origin: gianni@mariani.ws
+//       Wolfgang Bangerth <bangerth@ticam.utexas.edu>
+
+// PR c++/13289: ICE recursively instantiate static member data.
+
+template <int N> struct S { 
+    static const int C; 
+}; 
+ 
+template <int N> 
+const int S<N>::C = S<(N+1)%2>::C;
+ 
+template struct S<1>;
+
