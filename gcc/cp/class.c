3de6f3470ef2f9b605e3b3ebd6daacbd3da497f8
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-21 20:52:28 +0000

    * Make-lang.in, call.c, class.c, decl2.c, except.c, expr.c, 	init.c, mangle.c, typeck.c: Update copyright.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@76304 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/class.c
+++ gcc/cp/class.c
@@ -1,6 +1,6 @@
 /* Functions related to building classes and their related objects.
    Copyright (C) 1987, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002, 2003  Free Software Foundation, Inc.
+   1999, 2000, 2001, 2002, 2003, 2004  Free Software Foundation, Inc.
    Contributed by Michael Tiemann (tiemann@cygnus.com)
 
 This file is part of GCC.
