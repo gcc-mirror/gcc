bd915f51cfc98e3ad406f30b9aa67b85c57f8b9c
Andrew Pinski <pinskia@gcc.gnu.org>
2004-01-24 22:03:25 -0800

    Remove for real.
    From-SVN: r76542

--- gcc/testsuite/gcc.dg/20030121-1.c
+++
@@ -1,8 +0,0 @@
-/* { dg-do compile { target powerpc*-*-darwin* } } */
-/* { dg-options "-O2 -force_cpusubtype_ALL -mpowerpc64" } */
-
-long long (*y)(int t);
-long long get_alias_set (int t)
-{
-  return y(t);
-}
