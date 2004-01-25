9e4a734a2f1fe8d69220c3906c26dba08ceb0af7
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-25 17:28:19 +0000

    * config/mips/mips-protos.h: Remove the prototypes for 	mips_setup_incoming_varargs and mips_return_in_memory. 	* config/mips/mips.c (TARGET_PROMOTE_FUNCTION_ARGS): New. 	(TARGET_PROMOTE_FUNCTION_RETURN): Likewise. 	(TARGET_PROMOTE_PROTOTYPES): Likewise. 	(TARGET_STRUCT_VALUE_RTX): Likewise. 	(TARGET_RETURN_IN_MEMORY): Likewise. 	(TARGET_SETUP_INCOMING_VARARGS): Likewise. 	(TARGET_STRICT_ARGUMENT_NAMING): Likewise. 	(mips_setup_incoming_varargs): Match the prototype for 	TARGET_SETUP_INCOMING_VARARGS. 	(mips_return_in_memory): Make it static.  Add argument fntype. 	(mips_strict_argument_naming): New. 	* config/mips/mips.h (PROMOTE_PROTOTYPES): Remove. 	(PROMOTE_FUNCTION_ARGS): Likewise. 	(PROMOTE_FUNCTION_RETURN): Likewise. 	(STRUCT_VALUE): Likewise. 	(RETURN_IN_MEMORY): Likewise. 	(SETUP_INCOMING_VARARGS): Likewise. 	(STRICT_ARGUMENT_NAMING): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@76569 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/mmix/mmix.h
+++ gcc/config/mmix/mmix.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler, for MMIX.
-   Copyright (C) 2000, 2001, 2002 Free Software Foundation, Inc.
+   Copyright (C) 2000, 2001, 2002, 2004 Free Software Foundation, Inc.
    Contributed by Hans-Peter Nilsson (hp@bitrange.com)
 
 This file is part of GCC.
@@ -292,14 +292,6 @@ extern int target_flags;
      if (0) (UNSIGNEDP) = 0;			\
    }						\
  } while (0)
-
-#define PROMOTE_FUNCTION_ARGS
-
-#if 0
-/* Apparently not doing TRT if int < register-size.  FIXME: Perhaps
-   FUNCTION_VALUE and LIBCALL_VALUE needs tweaking as some ports say.  */
-#define PROMOTE_FUNCTION_RETURN
-#endif
 
 /* I'm a little bit undecided about this one.  It might be beneficial to
    promote all operations.  */
@@ -779,11 +771,6 @@ typedef struct { int regs; int lib; } CUMULATIVE_ARGS;
  mmix_function_value_regno_p (REGNO)
 
 
-/* Node: Aggregate Return */
-
-#define STRUCT_VALUE_REGNUM MMIX_STRUCT_VALUE_REGNUM
-
-
 /* Node: Caller Saves */
 /* (empty) */
 
@@ -805,12 +792,6 @@ typedef struct { int regs; int lib; } CUMULATIVE_ARGS;
  mmix_function_profiler (FILE, LABELNO)
 
 /* Node: Varargs */
-
-/* For the moment, let's stick to pushing argument registers on the stack.
-   Later, we can parse all arguments in registers, to improve
-   performance.  */
-#define SETUP_INCOMING_VARARGS(A, M, T, P, S)	\
- mmix_setup_incoming_varargs(&(A), M, T, &(P), S)
 
 /* FIXME: This and other EXPAND_BUILTIN_VA_... target macros are not
    documented, although used by several targets.  */
