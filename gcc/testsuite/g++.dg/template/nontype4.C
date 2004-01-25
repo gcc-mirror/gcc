5d6080a0858f5b5ebe32fb842afba7bb2a0f7321
lerdsuwa <lerdsuwa@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-25 14:18:19 +0000

    PR c++/13797 	* pt.c (instantiate_class_template): Add an error_mark_node 	check. 	(tsubst_decl) <TEMPLATE_DECL case>: Likewise.
    	* g++.dg/template/nontype4.C: New test.
    	* g++.dg/template/nontype5.C: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@76556 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/nontype4.C
@@ -0,0 +1,14 @@
+// { dg-do compile }
+
+// Origin: Ivan Godard <igodard@pacbell.net>
+//	   Volker Reichelt <reichelt@gcc.gnu.org>
+
+// PR c++/13797: ICE invalid nontype template parameter
+
+template <int> struct A
+{
+    typedef A<0> B;		// { dg-error "not a valid type|conflict" }
+    template <B> struct B {};	// { dg-error "not a valid type|declaration" }
+};
+
+A<0> a;				// { dg-error "instantiated" }
