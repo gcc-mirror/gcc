d546072452527139d9b829c89eda87cbd783b8cb
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-29 16:47:14 +0000

    PR c++/13883 	* mangle.c (write_encoding): Correct encoding of member template 	constructors.
    	PR c++/13883
    	* g++.dg/template/ctor3.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@76868 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/mangle.c
+++ gcc/cp/mangle.c
@@ -688,18 +688,29 @@ write_encoding (const tree decl)
   if (TREE_CODE (decl) == FUNCTION_DECL)
     {
       tree fn_type;
+      tree d;
 
       if (decl_is_template_id (decl, NULL))
-	fn_type = get_mostly_instantiated_function_type (decl);
+	{
+	  fn_type = get_mostly_instantiated_function_type (decl);
+	  /* FN_TYPE will not have parameter types for in-charge or
+	     VTT parameters.  Therefore, we pass NULL_TREE to
+	     write_bare_function_type -- otherwise, it will get
+	     confused about which artificial parameters to skip.  */
+	  d = NULL_TREE;
+	}
       else
-	fn_type = TREE_TYPE (decl);
+	{
+	  fn_type = TREE_TYPE (decl);
+	  d = decl;
+	}
 
       write_bare_function_type (fn_type, 
 				(!DECL_CONSTRUCTOR_P (decl)
 				 && !DECL_DESTRUCTOR_P (decl)
 				 && !DECL_CONV_FN_P (decl)
 				 && decl_is_template_id (decl, NULL)),
-				decl);
+				d);
     }
 }
 
