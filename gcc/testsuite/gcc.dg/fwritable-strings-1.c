2e35579598fcc5d7415c179d3abc2a439b3890a9
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-30 14:16:43 +0000

    PR c/12818 	* varasm.c (const_hash_1) <STRING_CST>: Use the 	address to compute the hash value if flag_writable_strings. 	(compare_constant) <STRING_CST>: Compare the addresses 	if flag_writable_strings. 	(build_constant_desc): Do not copy the expression for a 	STRING_CST if flag_writable_strings.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@76958 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/fwritable-strings-1.c
@@ -0,0 +1,17 @@
+/* PR c/12818 */
+/* Origin: <fnf@ninemoons.com> */
+
+/* { dg-do run } */
+/* { dg-options "-fwritable-strings" } */
+
+extern void abort(void);
+
+char *names[] = {"alice", "bob", "john"};
+
+int main (void)
+{
+  if (names[1][0] != 'b')
+    abort();
+
+  return 0;
+}
