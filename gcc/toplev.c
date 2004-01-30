5c34460d5911eb08d82d8108a3ba2b20b1453e0b
Andrew Pinski <pinskia@physics.uc.edu>
2004-01-30 17:42:47 +0000

    toplev.c: Include alloc-pool.h.
    2004-01-30  Andrew Pinski <pinskia@physics.uc.edu>
    
            * toplev.c: Include alloc-pool.h.
            * Makefile.in (toplev.c): Update dependencies.
    
    From-SVN: r76975

--- gcc/toplev.c
+++ gcc/toplev.c
@@ -78,6 +78,7 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #include "opts.h"
 #include "coverage.h"
 #include "value-prof.h"
+#include "alloc-pool.h"
 
 #if defined (DWARF2_UNWIND_INFO) || defined (DWARF2_DEBUGGING_INFO)
 #include "dwarf2out.h"
