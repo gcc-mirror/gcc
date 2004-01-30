b987c7e73488d44e7e196a316c7b9b222fa4701f
matz <matz@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-30 17:05:09 +0000

    * g++.dg/ext/case-range1.C, g++.dg/ext/case-range2.C,        g++.dg/ext/case-range3.C: New tests.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@76973 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/ext/case-range2.C
@@ -0,0 +1,27 @@
+// { dg-do compile }
+// Tests if case ranges (a GNU extension) correctly emit messages
+// about overlapping ranges.
+// { dg-options "" }
+
+const int low = -2;
+const int high = 15;
+
+template <typename T>
+T f2 (T i)
+{
+  switch (i)
+  {
+    case low ... high : return i + 1;  // { dg-error "" }
+    case 5 : return i + 2;             // { dg-error "duplicate" }
+    default : return 0;
+  }
+}
+
+int f (int i)
+{
+  switch (i) {
+    case 1 ... 10: return i + 1;       // { dg-error "" }
+    case 3 ... 5 : return i + 3;       // { dg-error "duplicate" }
+    default: return f2 (i);            // { dg-error "" }
+  }
+}
