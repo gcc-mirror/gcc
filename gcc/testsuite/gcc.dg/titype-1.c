d560b0aa2ab4f07365674f3a7d2e4a4626c363c5
Eric Botcazou <ebotcazou@libertysurf.fr>
2004-02-02 13:20:52 +0100

    sparc.c (function_arg_slotno): Align TImode arguments on a 16-byte boundary in the parameter array if ARCH64.
    	* config/sparc/sparc.c (function_arg_slotno): Align TImode
    	arguments on a 16-byte boundary in the parameter array if ARCH64.
    	Split handling of TFmode.
    
    From-SVN: r77107

---
+++ gcc/testsuite/gcc.dg/titype-1.c
@@ -0,0 +1,34 @@
+/* { dg-do compile } */
+
+/* Not all platforms support TImode integers.  */
+#if defined(__LP64__) || defined(__sparc__)
+typedef int TItype __attribute__ ((mode (TI)));  /* { dg-error "no data type for mode" "TI" { target sparc-sun-solaris2.[0-6]* } } */
+#else
+typedef long TItype;
+#endif
+
+#include <stdarg.h>
+
+extern void abort(void);
+
+
+void foo(int i, ...)
+{
+  TItype q;
+  va_list va;
+
+  va_start(va, i);
+  q = va_arg(va, TItype);
+  va_end(va);
+
+  if (q != 5)
+    abort();
+}
+
+int main(void)
+{
+  TItype q = 5;
+
+  foo(1, q);
+  return 0;
+}
