9febc4e8def2dbed004c56126ccd6218ffb6fd38
Giovanni Bajo <giovannibajo@gcc.gnu.org>
2004-02-02 01:11:03 +0000

    re PR c++/13957 (Insufficient diagnostic for dependent expression parsed as non-type but instantiated as type)
    	PR c++/13957
    	* g++.dg/template/non-type-template-argument-1.C,
    	g++.dg/template/qualified-id1.C: Update	dg-error marks.
    	* g++.dg/template/nontype6.C: New test.
    
    From-SVN: r77091

---
+++ gcc/testsuite/g++.dg/template/nontype6.C
@@ -0,0 +1,19 @@
+// { dg-do compile }
+// Origin: <v dot haisman at sh dot cvut dot cz>
+// PR c++/13957: Improved error message for type in template (when non-type
+//  is expected).
+
+template <class T>
+struct A
+{
+  typedef int type;
+};
+
+template <class T>
+void func(void)
+{
+  (void)A<T>::type();	 // { dg-error "if a type is meant" }
+// { dg-error "parsed as a non-type" "" { target *-*-* } 15 }
+}
+
+template void func<float>(void);    // { dg-error "instantiated from here" }
