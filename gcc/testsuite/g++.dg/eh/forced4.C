03121740c92265eb836c4d43dae4ef2d01ed5908
Zack Weinberg <zack@codesourcery.com>
2004-02-03 08:20:16 +0000

    * g++.dg/eh/forced1.C, g++.dg/eh/forced2.C, g++.dg/eh/forced3.C 	* g++.dg/eh/forced4.C: XFAIL ia64-hp-hpux11.*. 	* g++.dg/eh/ia64-1.C: Test branch regs only #ifdef __LP64__. 	* gcc.dg/cleanup-5.c: Run only on Linux targets.
    From-SVN: r77165

--- gcc/testsuite/g++.dg/eh/forced4.C
+++ gcc/testsuite/g++.dg/eh/forced4.C
@@ -1,4 +1,5 @@
-// { dg-do run }
+// HP-UX libunwind.so doesn't provide _Unwind_ForcedUnwind.
+// { dg-do run { xfail "ia64-hp-hpux11.*" } }
 
 // Test that forced unwinding does not call std::unexpected going 
 // throw a function with a non-empty exception spec.
