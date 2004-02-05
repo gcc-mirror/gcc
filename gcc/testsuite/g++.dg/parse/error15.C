21391f6718b6130e9700842db37c375b52bce98d
giovannibajo <giovannibajo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-05 16:50:57 +0000

    PR c++/14008 	* g++.dg/parse/error15.C: New test. 	* g++.dg/parse/crash11.C: Update dg-error mark.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@77324 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/error15.C
@@ -0,0 +1,39 @@
+// { dg-do compile }
+// Contributed by Volker Reichelt <reichelt at gcc dot gnu dot org> 
+// PR c++/14008: Improve diagnostic on invalid types in declarators.
+
+namespace N
+{ 
+  template<int> struct A {};
+  struct C {};
+  int K;
+}
+
+N::A f2;              // { dg-error "without an argument list" }
+N::INVALID f3;        // { dg-error "in namespace `N' does not name a type" }
+N::C::INVALID f4;     // { dg-error "in class `N::C' does not name a type" }
+N::K f6;              // { dg-error "in namespace `N' does not name a type" }
+typename N::A f7;     // { dg-error "without an argument list|outside of template" }
+
+struct B
+{
+  N::A f2;            // { dg-error "without an argument list" }
+  N::INVALID f3;      // { dg-error "in namespace `N' does not name a type" }
+  N::C::INVALID f4;   // { dg-error "in class `N::C' does not name a type" }
+  N::K f6;            // { dg-error "in namespace `N' does not name a type" }
+  typename N::A f7;   // { dg-error "without an argument list|outside of template" }
+};
+
+template <int>
+struct C
+{
+  N::A f2;            // { dg-error "without an argument list" }
+  N::INVALID f3;      // { dg-error "in namespace `N' does not name a type" }
+  N::C::INVALID f4;   // { dg-error "in class `N::C' does not name a type" }
+  N::K f6;            // { dg-error "in namespace `N' does not name a type" }
+  typename N::A f7;   // { dg-error "without an argument list" }
+};
+
+// { dg-bogus "" "bogus excess errors in declaration" { xfail *-*-* } 16 }
+// { dg-bogus "" "bogus excess errors in declaration" { xfail *-*-* } 24 }
+// { dg-bogus "" "bogus excess errors in declaration" { xfail *-*-* } 34 }
