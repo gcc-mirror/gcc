007f9998410773f204964208acc9ad18ccc98f64
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-05 05:09:38 +0000

    * lib/file-format.exp (gcc_target_object_format): Use 	${tool}_target_compile, not gcc_target_compile. 	* lib/target-supports.exp (check_alias_available): Likewise. 	(check_gc_sections_available): Likewise. 	* g++.dg/ext/attrib10.C: Use dg-require-alias. 	* g++.old-deja/g++.ext/attrib5.C: Use dg-require-weak and 	dg-require-alias.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@77295 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.ext/attrib5.C
+++ gcc/testsuite/g++.old-deja/g++.ext/attrib5.C
@@ -1,5 +1,7 @@
-// { dg-do run { xfail alpha*-dec-osf* *-*-hms i?86-pc-cygwin *-*-coff hppa2*-hp-hpux11* } }
+// { dg-do run { xfail alpha*-dec-osf* *-*-hms i?86-pc-cygwin *-*-coff } }
 // Test that attributes weak and alias coexist.
+// { dg-require-weak "" }
+// { dg-require-alias "" }
 
 extern "C" {
   void f () __attribute__((weak, alias ("_f")));
