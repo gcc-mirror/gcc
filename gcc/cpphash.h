33336f4083766255a8b5d2b026b0368ddf4e192b
Kazu Hirata <kazu@cs.umass.edu>
2004-02-07 14:33:18 +0000

    c-opts.c, [...]: Update copyright.
    	gcc/
    	* c-opts.c, c-ppoutput.c, cppfiles.c, cpphash.h, cppinit.c,
    	cpplib.h, diagnostic.h, fix-header.c, config/fp-bit.c,
    	config/mips/iris5.h, doc/makefile.texi: Update copyright.
    
    	cp/
    	* rtti.c, tree.c: Update copyright.
    
    	java/
    	* typeck.c: Update copyright.
    
    From-SVN: r77451

--- gcc/cpphash.h
+++ gcc/cpphash.h
@@ -1,5 +1,5 @@
 /* Part of CPP library.
-   Copyright (C) 1997, 1998, 1999, 2000, 2001, 2002, 2003
+   Copyright (C) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004
    Free Software Foundation, Inc.
 
 This program is free software; you can redistribute it and/or modify it
--- gcc/java/typeck.c
+++ gcc/java/typeck.c
@@ -1,5 +1,5 @@
 /* Handle types for the GNU compiler for the Java(TM) language.
-   Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2003
+   Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2003, 2004
    Free Software Foundation, Inc.
 
 This file is part of GCC.
