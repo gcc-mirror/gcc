093936a98e5c8c71174def8366e8cce4b5a17d57
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-07 14:14:54 +0000

    gcc/ 	* c-ppoutput.c, cfganal.c, diagnostic.h, print-rtl.c, 	config/darwin.c, config/darwin.h, config/ia64/ia64-c.c, 	config/m32r/linux.h, config/rs6000/ppc64-fp.c, 	config/sparc/openbsd.h, doc/makefile.texi, doc/passes.texi: 	Update copyright.
    	cp/
    	* rtti.c: Update copyright.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@77449 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-ppoutput.c
+++ gcc/c-ppoutput.c
@@ -1,5 +1,5 @@
 /* Preprocess only, using cpplib.
-   Copyright (C) 1995, 1997, 1998, 1999, 2000, 2001, 2002, 2003
+   Copyright (C) 1995, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004
    Free Software Foundation, Inc.
    Written by Per Bothner, 1994-95.
 
