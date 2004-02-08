e7c7b5152e3cf5e9bf22a374f153446548e5a491
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-08 18:32:35 +0000

    * g++.dg/eh/simd-2.C: Adjust line numbers for SPARC.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@77495 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/eh/simd-2.C
+++ gcc/testsuite/g++.dg/eh/simd-2.C
@@ -3,7 +3,8 @@
 // { dg-options "-O" }
 // { dg-options "-O -w" { target i?86-*-* } }
 // { dg-do run }
-// { dg-error "" "PR target/12916" { target sparc*-*-* } 11 }
+// { dg-error "" "PR target/12916" { target sparc64-*-* sparcv9-*-* } 12 }
+// { dg-error "" "PR target/12916" { target sparc-*-* } 14 }
 
 typedef int __attribute__((mode(V4SI))) vecint;
 
