213d3e2d536eac39e07eb0e1e525b741c99d9d11
Joseph Myers <jsm@polyomino.org.uk>
2004-02-08 20:56:54 +0000

    README.Portability: Change "ISO C89" to "ISO C90".
    	* README.Portability: Change "ISO C89" to "ISO C90".
    	* c-parse.in (primary, initelt): Likewise.
    
    testsuite:
    	* gcc.dg/c90-init-1.c: Adjust expected error messages.
    
    From-SVN: r77500

--- gcc/testsuite/gcc.dg/c90-init-1.c
+++ gcc/testsuite/gcc.dg/c90-init-1.c
@@ -7,10 +7,10 @@ struct A {
   int B;
   short C[2];
 };
-int a[10] = { 10, [4] = 15 };			/* { dg-error "ISO C89 forbids specifying subobject to initialize" } */
-struct A b = { .B = 2 };			/* { dg-error "ISO C89 forbids specifying subobject to initialize" } */
-struct A c[] = { [3].C[1] = 1 };		/* { dg-error "ISO C89 forbids specifying subobject to initialize" } */
-struct A d[] = { [4 ... 6].C[0 ... 1] = 2 };	/* { dg-error "(forbids specifying range of elements to initialize)|(ISO C89 forbids specifying subobject to initialize)" } */
+int a[10] = { 10, [4] = 15 };			/* { dg-error "ISO (C89|C90) forbids specifying subobject to initialize" } */
+struct A b = { .B = 2 };			/* { dg-error "ISO (C89|C90) forbids specifying subobject to initialize" } */
+struct A c[] = { [3].C[1] = 1 };		/* { dg-error "ISO (C89|C90) forbids specifying subobject to initialize" } */
+struct A d[] = { [4 ... 6].C[0 ... 1] = 2 };	/* { dg-error "(forbids specifying range of elements to initialize)|(ISO (C89|C90) forbids specifying subobject to initialize)" } */
 int e[] = { [2] 2 };				/* { dg-error "use of designated initializer without" } */
 struct A f = { C: { 0, 1 } };			/* { dg-error "use of designated initializer with " } */
 int g;
