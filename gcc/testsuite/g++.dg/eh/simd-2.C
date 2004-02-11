5f013ee9ee215402cacf65fa34d09d7d0314c950
Hartmut Penner <hpenner@de.ibm.com>
2004-02-11 15:43:28 +0000

    * g++.dg/simd-2.C: Set -maltivec for ppc64.
    From-SVN: r77664

--- gcc/testsuite/g++.dg/eh/simd-2.C
+++ gcc/testsuite/g++.dg/eh/simd-2.C
@@ -2,10 +2,21 @@
 // Contributed by Aldy Hernandez (aldy@quesejoda.com).
 // { dg-options "-O" }
 // { dg-options "-O -w" { target i?86-*-* } }
+// { dg-options "-O -maltivec" { target powerpc64-*-linux* } }
 // { dg-do run }
-// { dg-error "" "PR target/12916" { target sparc64-*-* sparcv9-*-* } 12 }
-// { dg-error "" "PR target/12916" { target sparc-*-* } 14 }
+// { dg-error "" "PR target/12916" { target sparc64-*-* sparcv9-*-* } 23 }
+// { dg-error "" "PR target/12916" { target sparc-*-* } 25 }
+#ifdef __powerpc64__
+#include <signal.h>
+extern void
+exit (int);
 
+void 
+sig_ill_handler (int sig)
+{
+    exit(0);
+}
+#endif
 typedef int __attribute__((mode(V4SI))) vecint;
 
 vecint vecfunc (vecint beachbum)
@@ -44,6 +55,12 @@ void f1 (void)
 
 int main ()
 {
+#ifdef __powerpc64__
+  /* Exit on systems without altivec.  */
+  signal (SIGILL, sig_ill_handler);
+  asm volatile (".long 0x10000484");  
+  signal (SIGILL, SIG_DFL);
+#endif
   f1 ();
   return 0;
 }
