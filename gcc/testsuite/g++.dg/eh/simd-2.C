79abfe5ff215041ba46acca8990cd9b9de185236
hpenner <hpenner@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-12 10:24:16 +0000

    * g++.dg/simd-2.C: xfail on ppc64-linux.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@77700 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/eh/simd-2.C
+++ gcc/testsuite/g++.dg/eh/simd-2.C
@@ -2,21 +2,11 @@
 // Contributed by Aldy Hernandez (aldy@quesejoda.com).
 // { dg-options "-O" }
 // { dg-options "-O -w" { target i?86-*-* } }
-// { dg-options "-O -maltivec" { target powerpc64-*-linux* } }
-// { dg-do run }
-// { dg-error "" "PR target/12916" { target sparc64-*-* sparcv9-*-* } 23 }
-// { dg-error "" "PR target/12916" { target sparc-*-* } 25 }
-#ifdef __powerpc64__
-#include <signal.h>
-extern void
-exit (int);
+// { dg-options "-O -w -maltivec" { target powerpc64-*-linux* } }
+// { dg-do run { xfail "powerpc64-*-linux*"}  }
+// { dg-error "" "PR target/12916" { target sparc64-*-* sparcv9-*-* } 13 }
+// { dg-error "" "PR target/12916" { target sparc-*-* } 15 }
 
-void 
-sig_ill_handler (int sig)
-{
-    exit(0);
-}
-#endif
 typedef int __attribute__((mode(V4SI))) vecint;
 
 vecint vecfunc (vecint beachbum)
@@ -55,11 +45,9 @@ void f1 (void)
 
 int main ()
 {
-#ifdef __powerpc64__
-  /* Exit on systems without altivec.  */
-  signal (SIGILL, sig_ill_handler);
-  asm volatile (".long 0x10000484");  
-  signal (SIGILL, SIG_DFL);
+#if defined(__powerpc64__) && defined(__linux__)
+  // Don't run on ppc64-linux, since not always AltiVec regs available   
+  return -1;  
 #endif
   f1 ();
   return 0;
