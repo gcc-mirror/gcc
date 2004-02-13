99dfc771e4d6f0f065fc0f63016300642ddc38f7
amodra <amodra@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-13 02:02:32 +0000

    * gcc.dg/debug/20020327-1.c: Disable for powerpc64.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@77747 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/debug/20020327-1.c
+++ gcc/testsuite/gcc.dg/debug/20020327-1.c
@@ -1,4 +1,12 @@
-/* { dg-do link { xfail powerpc64*-*-* } } */
+/* { dg-do link } */
 
+#ifndef __powerpc64__
+/* Fails on powerpc64-linux due to the function Letext using a global
+   .Letext symbol that conflicts with .Letext emitted by gcc with
+   -gstabs.  Some day the linker will be fixed to not require global
+   "dot" syms, but for now disable this test entirely for powerpc64.
+   Using xfail doesn't work, nor does dg-excess-errors because some
+   combinations of command line options won't cause this test to fail.  */
 void Letext (void) { }
+#endif
 int main() { return 0; }
