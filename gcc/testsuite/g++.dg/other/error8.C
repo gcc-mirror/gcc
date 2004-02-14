32bb574bf52ca7e673a4eabf3ab37a50550316cb
Giovanni Bajo <giovannibajo@gcc.gnu.org>
2004-02-14 02:15:20 +0000

    re PR c++/13927 (duplicated error message on bad redeclaration of anon union)
    	PR c++/13927
    	* g++.dg/other/error8.C: New test.
    
    From-SVN: r77788

---
+++ gcc/testsuite/g++.dg/other/error8.C
@@ -0,0 +1,26 @@
+// { dg-do compile }
+// Contributed by: Michael Elizabeth Chastain 
+//   <mec dot gnu at mindspring dot com>
+// PR c++/13927: Wrong error message for redeclartion of type from union
+
+void foo(void)
+{
+  union { int alpha; int beta; }; // { dg-error "previous declaration `int alpha'" }
+  double alpha;  // { dg-error "declaration of" }
+}
+
+// This checks both the templated version, and the position of the diagnostic
+//  (which is currently wrong).
+template <int>
+void tfoo(void)
+{
+  union { 
+    int alpha;  // { dg-error "" "" { xfail *-*-* } }
+    int beta; 
+  }; // { dg-bogus "" "misplaced position of the declaration" { xfail *-*-* } }
+  double alpha; // { dg-error "" "" }
+}
+
+// The duplicated error messages are xfailed for now (tracked in the PR)
+// { dg-bogus "" "duplicate error messages" { xfail *-*-* } 8 }
+// { dg-bogus "" "duplicate error messages" { xfail *-*-* } 9 }
