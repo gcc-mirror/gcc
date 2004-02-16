3570d468c6f08de5b1b6a8b5786386d9e23b7a1f
Eric Botcazou <ebotcazou@libertysurf.fr>
2004-02-16 20:30:25 +0100

    * config/sparc/sparc.c (get_pc_symbol_name): Mark with GTY(()).
    From-SVN: r77915

--- gcc/config/sparc/sparc.c
+++ gcc/config/sparc/sparc.c
@@ -3104,7 +3104,7 @@ static GTY(()) rtx global_offset_table;
 
 /* The function we use to get at it.  */
 static GTY(()) rtx get_pc_symbol;
-static char get_pc_symbol_name[256];
+static GTY(()) char get_pc_symbol_name[256];
 
 /* Ensure that we are not using patterns that are not OK with PIC.  */
 
