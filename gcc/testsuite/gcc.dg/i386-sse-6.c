b3289ccb2d53e2c04bfff6bf7b3bc7765e19198c
Jakub Jelinek <jakub@redhat.com>
2004-02-17 18:16:27 +0100

    i386-cpuid.h (bit_CMOV): Define.
    	* gcc.dg/i386-cpuid.h (bit_CMOV): Define.
    	(i386_cpuid): No need to test if cpuid is available on AMD64.
    	Fix assembly, so that it works onboth i386 and AMD64.
    	* gcc.dg/i386-sse-6.c: Include stdio.h, stdlib.h and string.h.
    	(vecInLong): Fix s[] member type to unsigned int.
    	(vecInWord): Remove type.
    	(mmx_tests, sse_tests, dump64_16, dump64_32, dump64_64): Remove.
    	(a64, b64, c64, d64, e64): Remove.
    	(main): Pass if CPU has no MMX, SSE, SSE2 or CMOV support.
    	Remove unused variable.  Remove initialization of removed variables.
    	Don't call mmx_tests nor sse_tests.
    	(reference_mmx, reference_sse): Remove.
    	(check): Add return stmt.
    	* gcc.dg/i386-sse-7.c: New test.
    	* gcc.dg/i386-mmx-4.c: New test.
    
    From-SVN: r77957

--- gcc/testsuite/gcc.dg/i386-sse-6.c
+++ gcc/testsuite/gcc.dg/i386-sse-6.c
@@ -1,6 +1,9 @@
 /* { dg-do run { target i?86-*-* x86_64-*-* } } */
 /* { dg-options "-O2 -msse2" } */
 #include <xmmintrin.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "i386-cpuid.h"
 
 #ifndef NOINLINE
@@ -11,26 +14,13 @@
 
 typedef union {
   __m128i v;
-  unsigned long s[4];
+  unsigned int s[4];
   unsigned short int t[8];
   unsigned long long u[2];
   unsigned char c[16];
 }vecInLong;
 
-typedef union {
-  __m64 v;
-  unsigned char c[8];
-  unsigned short int s[4];
-  unsigned long long t;
-  unsigned int u[2];
-}vecInWord;
-
-void mmx_tests (void) NOINLINE;
-void sse_tests (void) NOINLINE;
 void sse2_tests (void) NOINLINE;
-void dump64_16 (char *, char *, vecInWord);
-void dump64_32 (char *, char *, vecInWord);
-void dump64_64 (char *, char *, vecInWord);
 void dump128_16 (char *, char *, vecInLong);
 void dump128_32 (char *, char *, vecInLong);
 void dump128_64 (char *, char *, vecInLong);
@@ -42,34 +32,8 @@ char comparison[8000];
 static int errors = 0;
 
 vecInLong a128, b128, c128, d128, e128, f128;
-vecInWord a64, b64, c64, d64, e64;
 __m128i m128_16, m128_32, s128, m128_64, m128_128;
 __m64 m64_16, s64, m64_32, m64_64;
-
-const char *reference_mmx[] = {
-  "_mm_srai_pi16 0012 0012 0012 0012 \n",
-  "_mm_sra_pi16 0012 0012 0012 0012 \n",
-  "_mm_srai_pi32 00123456 00123456 \n",
-  "_mm_sra_pi32 00123456 00123456 \n",
-  "_mm_srli_pi16 0012 0012 0012 0012 \n",
-  "_mm_srl_pi16 0012 0012 0012 0012 \n",
-  "_mm_srli_pi32 00123456 00123456 \n",
-  "_mm_srl_pi32 00123456 00123456 \n",
-  "_mm_srli_si64 00123456789abcde\n",
-  "_mm_srl_si64 00123456789abcde\n",
-  "_mm_slli_pi16 1230 1230 1230 1230 \n",
-  "_mm_sll_pi16 1230 1230 1230 1230 \n",
-  "_mm_slli_pi32 12345670 12345670 \n",
-  "_mm_sll_pi32 12345670 12345670 \n",
-  "_mm_slli_si64 123456789abcdef0\n",
-  "_mm_sll_si64 123456789abcdef0\n",
-  ""
-};
-
-const char *reference_sse[] = {
-  "_mm_shuffle_pi16 0123 4567 89ab cdef \n",
-  ""
-};
 
 const char *reference_sse2[] = {
   "_mm_srai_epi16 0012 0012 0012 0012 0012 0012 0012 0012 \n",
@@ -98,12 +62,13 @@ const char *reference_sse2[] = {
 
 int main()
 {
-  unsigned long *p;
   unsigned long cpu_facilities;
 
   cpu_facilities = i386_cpuid ();
 
-  if (!cpu_facilities)	/* If host has no vector support, pass.  */
+  if ((cpu_facilities & (bit_MMX | bit_SSE | bit_SSE2 | bit_CMOV))
+      != (bit_MMX | bit_SSE | bit_SSE2 | bit_CMOV))
+    /* If host has no vector support, pass.  */
     exit (0);
 
   a128.s[0] = 0x01234567;
@@ -136,61 +101,12 @@ int main()
 
   m128_128 = e128.v;
 
-  d64.u[0]  = 0x01234567;
-  d64.u[1]  = 0x01234567;
-
-  m64_32 = d64.v;
-
-  e64.t  = 0x0123456789abcdefULL;
-
-  m64_64 = e64.v;
-
-  a64.s[0] = 0x0123;
-  a64.s[1] = 0x0123;
-  a64.s[2] = 0x0123;
-  a64.s[3] = 0x0123;
-
-  m64_16 = a64.v;
-
   b128.s[0] = SHIFT;
   b128.s[1] = 0;
   b128.s[2] = 0;
   b128.s[3] = 0;
 
   s128 = b128.v;
-
-  b64.s[0] = SHIFT;
-  b64.s[1] = 0;
-  b64.s[2] = 0;
-  b64.s[3] = 0;
-
-  s64 = b64.v;
-
-  if (cpu_facilities & bit_MMX)
-    {
-      mmx_tests();
-      check (buf, reference_mmx);
-#ifdef DEBUG
-      printf ("mmx testing:\n");
-      printf (buf);
-      printf ("\ncomparison:\n");
-      printf (comparison);
-#endif
-      buf[0] = '\0';
-    }
-
-  if (cpu_facilities & bit_SSE)
-    {
-      sse_tests();
-      check (buf, reference_sse);
-#ifdef DEBUG
-      printf ("sse testing:\n");
-      printf (buf);
-      printf ("\ncomparison:\n");
-      printf (comparison);
-#endif
-      buf[0] = '\0';
-    }
 
   if (cpu_facilities & bit_SSE2)
     {
@@ -208,66 +124,6 @@ int main()
   if (errors != 0)
     abort ();
   exit (0);
-}
-
-void NOINLINE
-mmx_tests (void)
-{
-  /* psraw */
-  c64.v = _mm_srai_pi16 (m64_16, SHIFT);
-  dump64_16 (buf, "_mm_srai_pi16", c64);
-  c64.v  = _mm_sra_pi16 (m64_16, s64);
-  dump64_16 (buf, "_mm_sra_pi16", c64);
-
-  /* psrad */
-  c64.v  = _mm_srai_pi32 (m64_32, SHIFT);
-  dump64_32 (buf, "_mm_srai_pi32", c64);
-  c64.v = _mm_sra_pi32 (m64_32, s64);
-  dump64_32 (buf, "_mm_sra_pi32", c64);
-
-  /* psrlw */
-  c64.v = _mm_srli_pi16 (m64_16, SHIFT);
-  dump64_16 (buf, "_mm_srli_pi16", c64);
-  c64.v = _mm_srl_pi16 (m64_16, s64);
-  dump64_16 (buf, "_mm_srl_pi16", c64);
-
-  /* psrld */
-  c64.v = _mm_srli_pi32 (m64_32, SHIFT);
-  dump64_32 (buf, "_mm_srli_pi32", c64);
-  c64.v = _mm_srl_pi32 (m64_32, s64);
-  dump64_32 (buf, "_mm_srl_pi32", c64);
-
-  /* psrlq */
-  c64.v = _mm_srli_si64 (m64_64, SHIFT);
-  dump64_64 (buf, "_mm_srli_si64", c64);
-  c64.v = _mm_srl_si64 (m64_64, s64);
-  dump64_64 (buf, "_mm_srl_si64", c64);
-
-  /* psllw */
-  c64.v = _mm_slli_pi16 (m64_16, SHIFT);
-  dump64_16 (buf, "_mm_slli_pi16", c64);
-  c64.v = _mm_sll_pi16 (m64_16, s64);
-  dump64_16 (buf, "_mm_sll_pi16", c64);
-
-  /* pslld */
-  c64.v = _mm_slli_pi32 (m64_32, SHIFT);
-  dump64_32 (buf, "_mm_slli_pi32", c64);
-  c64.v = _mm_sll_pi32 (m64_32, s64);
-  dump64_32 (buf, "_mm_sll_pi32", c64);
-
-  /* psllq */
-  c64.v = _mm_slli_si64 (m64_64, SHIFT);
-  dump64_64 (buf, "_mm_slli_si64", c64);
-  c64.v = _mm_sll_si64 (m64_64, s64);
-  dump64_64 (buf, "_mm_sll_si64", c64);
-}
-
-void NOINLINE
-sse_tests (void)
-{
-  /* pshufw */
-  c64.v = _mm_shuffle_pi16 (m64_64, 0x1b);
-  dump64_16 (buf, "_mm_shuffle_pi16", c64);
 }
 
 void NOINLINE
@@ -342,52 +198,6 @@ sse2_tests (void)
   /* pshufhw */
   c128.v = _mm_shufflehi_epi16 (m128_128, 0x1b);
   dump128_16 (buf, "_mm_shuffehi_epi16", c128);
-}
-
-void
-dump64_16 (char *buf, char *name, vecInWord x)
-{
-  int i;
-  char *p = buf + strlen (buf);
-
-  sprintf (p, "%s ", name);
-  p += strlen (p);
-
-  for (i=0; i<4; i++)
-    {
-      sprintf (p, "%4.4x ", x.s[i]);
-      p += strlen (p);
-    }
-  strcat (p, "\n");
-}
-
-void
-dump64_32 (char *buf, char *name, vecInWord x)
-{
-  int i;
-  char *p = buf + strlen (buf);
-
-  sprintf (p, "%s ", name);
-  p += strlen (p);
-
-  for (i=0; i<2; i++)
-    {
-      sprintf (p, "%8.8x ", x.u[i]);
-      p += strlen (p);
-    }
-  strcat (p, "\n");
-}
-
-void
-dump64_64 (char *buf, char *name, vecInWord x)
-{
-  int i;
-  char *p = buf + strlen (buf);
-
-  sprintf (p, "%s ", name);
-  p += strlen (p);
-
-  sprintf (p, "%16.16llx\n", x.t);
 }
 
 void
@@ -502,4 +312,5 @@ check (const char *input, const char *reference[])
   *p_comparison = '\0';
   strcat (p_comparison, new_errors ? "failure\n\n" : "O.K.\n\n") ;
   errors += new_errors;
+  return 0;
 }
