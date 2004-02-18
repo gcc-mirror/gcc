c5fe50362f8f923bdbcf3900025b5e93cb24705d
Paul Brook <paul@codesourcery.com>
2004-02-18 11:11:24 +0000

    Fix previous checkin.
    	PR debug/12934
    	* dwarf2out.c (loc_descriptor_from_tree): Handle
    	EXPR_WITH_FILE_LOCATION.
    testsuite/
    	* gcc.dg/debug/debug-7.c: New test.
    
    From-SVN: r78018

--- gcc/testsuite/gcc.dg/debug/debug-7.c
+++ gcc/testsuite/gcc.dg/debug/debug-7.c
@@ -1,17 +1,16 @@
 /* { dg-do compile } */
 /* { dg-options "-dA" } */
+/* PR debug/12934.  */
 
-typedef __SIZE_TYPE__ size_t;
-
-static inline size_t foo (int n)
+static inline int foo ()
 {
-  return (n + sizeof (int) * 8 - 1) / (sizeof (int) * 8);
+  return 42;
 }
 
-void bar (int, int *);
+void bar (int *);
 
-void baz (int n)
+void baz ()
 {
-  int a[foo (n)];
-  bar (n, a);
+  int a[foo ()];
+  bar (a);
 }
