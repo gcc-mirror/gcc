aa335b7641d7a40f4d0ec62d25bd5b0c60619ed8
Kazu Hirata <kazu@cs.umass.edu>
2004-02-18 15:05:17 +0000

    cfgloop.h, [...]: Update copyright.
    gcc/
    	* cfgloop.h, cfgloopanal.c, cpplex.c, except.h, loop-init.c,
    	loop-unroll.c, scan-decls.c, scan.h, stor-layout.c,
    	xcoffout.c, xcoffout.h, config/arm/mmintrin.h,
    	config/mips/linux64.h, config/pa/pa-64.h,
    	config/rs6000/aix51.h, config/rs6000/aix52.h,
    	config/rs6000/spe.md, config/sparc/linux.h,
    	config/sparc/linux64.h: Update copyright.
    
    gcc/cp/
    	* cp-tree.def, cvt.c: Update copyright.
    
    From-SVN: r78032

--- gcc/except.h
+++ gcc/except.h
@@ -1,5 +1,5 @@
 /* Exception Handling interface routines.
-   Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003
+   Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004
    Free Software Foundation, Inc.
    Contributed by Mike Stump <mrs@cygnus.com>.
 
--- gcc/xcoffout.c
+++ gcc/xcoffout.c
@@ -1,5 +1,5 @@
 /* Output xcoff-format symbol table information from GNU compiler.
-   Copyright (C) 1992, 1994, 1995, 1997, 1998, 1999, 2000, 2002, 2003
+   Copyright (C) 1992, 1994, 1995, 1997, 1998, 1999, 2000, 2002, 2003, 2004
    Free Software Foundation, Inc.
 
 This file is part of GCC.
