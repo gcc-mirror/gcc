ca6b7cecf68ef7ff90916a3ff3deca8e71a14c30
Eric Botcazou <ebotcazou@libertysurf.fr>
2004-02-19 10:23:22 +0100

    simd-4.x: Remove.
    	* gcc.c-torture/execute/simd-4.x: Remove.
    	* gcc.c-torture/execute/va-arg-25.x: Likewise.
    	* gcc.dg/compat/vector-1_x.c: Do not XFAIL on SPARC.
    	Guard with #ifndef SKIP_ATTRIBUTE.
    	* gcc.dg/compat/vector-1_y.c: Likewise.
    	* gcc.dg/compat/vector-2_x.c: Likewise.
    	* gcc.dg/compat/vector-2_y.c: Likewise.
    	* g++.dg/eh/simd-1.C: Do not XFAIL on SPARC.
    	* g++.dg/eh/simd-2.C: Likewise.
    
    From-SVN: r78089

--- gcc/testsuite/g++.dg/eh/simd-1.C
+++ gcc/testsuite/g++.dg/eh/simd-1.C
@@ -3,7 +3,6 @@
 // { dg-options "-O" }
 // { dg-options "-O -w" { target i?86-*-* } }
 // { dg-do run }
-// { dg-error "" "PR target/12916" { target sparc*-*-* } 11 }
 
 typedef int __attribute__((mode(V2SI))) vecint;
 
