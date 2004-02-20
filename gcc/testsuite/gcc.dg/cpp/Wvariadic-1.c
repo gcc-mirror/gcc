7c4d376d4fa0dac8d3039a01c3c8b6fde1eb201d
Richard Henderson <rth@redhat.com>
2004-02-20 10:42:51 -0800

    * doc/invoke.texi: Add -Wvariadic-macros.
    From-SVN: r78179

---
+++ gcc/testsuite/gcc.dg/cpp/Wvariadic-1.c
@@ -0,0 +1,6 @@
+/* { dg-do compile } */
+/* { dg-options "-std=iso9899:1990 -pedantic -Werror" } */
+
+#define f(x,...)	/* { dg-error "variadic" } */
+#define g(x,y...)	/* { dg-error "variadic" } */
+int not_empty;
