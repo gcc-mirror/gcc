d2546dec45eee3ac2e8c0f44d581938c972f7bbf
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-20 23:10:33 +0000

    PR c++/12007 	* g++.dg/other/vthunk1.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@78193 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/other/vthunk1.C
@@ -0,0 +1,26 @@
+// PR c++/12007 Multiple inheritance float pass by value fails
+// { dg-do run }
+
+extern "C" void abort (void);
+
+class gvImpl
+{
+public:
+  virtual void PutVal(float value){}
+};
+
+class foo { public: virtual void Bar(){} };
+
+class myGv: public foo, public gvImpl
+{
+  void PutVal(float value){ if (value != 3.14159f) abort (); }
+};
+
+myGv x;
+gvImpl* object = &x;
+
+int main()
+{
+  object->PutVal(3.14159f);
+  return 0;
+}
