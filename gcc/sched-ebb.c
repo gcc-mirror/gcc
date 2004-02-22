287b0573d219b52539c6ea2633bebee990a656cb
James E Wilson <wilson@specifixinc.com>
2004-02-22 17:56:49 +0000

    re PR target/13918 ([ia64] bootstrap comparision failure)
    2004-02-22  James E Wilson  <wilson@specifixinc.com>
    
    	PR target/13918
    	* sched-ebb.c (ebb_sched_info): Disable use of cselib.
    
    From-SVN: r78267

--- gcc/sched-ebb.c
+++ gcc/sched-ebb.c
@@ -1,6 +1,6 @@
 /* Instruction scheduling pass.
    Copyright (C) 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002 Free Software Foundation, Inc.
+   1999, 2000, 2001, 2002, 2004 Free Software Foundation, Inc.
    Contributed by Michael Tiemann (tiemann@cygnus.com) Enhanced by,
    and currently maintained by, Jim Wilson (wilson@cygnus.com)
 
@@ -203,7 +203,7 @@ static struct sched_info ebb_sched_info =
 
   NULL, NULL,
   NULL, NULL,
-  0, 1
+  0, 0
 };
 
 /* Schedule a single extended basic block, defined by the boundaries HEAD
