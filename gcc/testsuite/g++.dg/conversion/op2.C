d73ff859943b668ab90402b94e458a898a20e777
Gabriel Dos Reis <gdr@integrable-solutions.net>
2004-02-22 16:05:49 +0000

    backport: re PR c++/13907 (valid conversion rejected unless -pedantic is set)
    	    Backport from mainline
    	    2004-02-02  Mark Mitchell  <mark@codesourcery.com>
    	    PR c++/13907
    	    * call.c (convert_class_to_reference): Keep better track of
    	    pedantically invalid user-defined conversions.
    
    From-SVN: r78263

--- gcc/testsuite/g++.dg/conversion/op2.C
+++ gcc/testsuite/g++.dg/conversion/op2.C
@@ -5,10 +5,8 @@ struct A {
   operator const int & () const;
 };
                                                                                
-          
 void f(int &);
 void f(const int &);
-                                                                               
           
 int main() {
   const A x = A();
