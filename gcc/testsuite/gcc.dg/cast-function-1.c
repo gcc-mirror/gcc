82e18817eb8cff09cdbe413cf9cf4562a9611ca7
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-23 12:41:43 +0000

    PR c/14188 	* builtins.c (expand_builtin_va_arg): Emit an informative message 	if a trap is generated. 	* c-typeck.c (build_function_call): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@78294 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cast-function-1.c
+++ gcc/testsuite/gcc.dg/cast-function-1.c
@@ -22,19 +22,19 @@ void bar(void)
   int i;
   str_t s;
 
-  d = ((double (*) (int)) foo1) (i);  /* { dg-warning "non-compatible" } */
-  i = ((int (*) (double)) foo1) (d);  /* { dg-warning "non-compatible" } */
-  s = ((str_t (*) (int)) foo1) (i);   /* { dg-warning "non-compatible" } */
-  ((void (*) (int)) foo1) (d);        /* { dg-warning "non-compatible" } */
-  i = ((int (*) (int)) foo1) (i);     /* { dg-bogus "non-compatible" } */
-  (void) foo1 (i);                    /* { dg-bogus "non-compatible" } */
+  d = ((double (*) (int)) foo1) (i);  /* { dg-warning "non-compatible|abort" } */
+  i = ((int (*) (double)) foo1) (d);  /* { dg-warning "non-compatible|abort" } */
+  s = ((str_t (*) (int)) foo1) (i);   /* { dg-warning "non-compatible|abort" } */
+  ((void (*) (int)) foo1) (d);        /* { dg-warning "non-compatible|abort" } */
+  i = ((int (*) (int)) foo1) (i);     /* { dg-bogus "non-compatible|abort" } */
+  (void) foo1 (i);                    /* { dg-bogus "non-compatible|abort" } */
 
-  d = ((double (*) (int)) foo2) (i);  /* { dg-warning "non-compatible" } */
-  i = ((int (*) (double)) foo2) (d);  /* { dg-bogus "non-compatible" } */
-  s = ((str_t (*) (int)) foo2) (i);   /* { dg-warning "non-compatible" } */
-  ((void (*) (int)) foo2) (d);        /* { dg-warning "non-compatible" } */
-  i = ((int (*) (int)) foo2) (i);     /* { dg-bogus "non-compatible" } */
-  (void) foo2 (i);                    /* { dg-bogus "non-compatible" } */
+  d = ((double (*) (int)) foo2) (i);  /* { dg-warning "non-compatible|abort" } */
+  i = ((int (*) (double)) foo2) (d);  /* { dg-bogus "non-compatible|abort" } */
+  s = ((str_t (*) (int)) foo2) (i);   /* { dg-warning "non-compatible|abort" } */
+  ((void (*) (int)) foo2) (d);        /* { dg-warning "non-compatible|abort" } */
+  i = ((int (*) (int)) foo2) (i);     /* { dg-bogus "non-compatible|abort" } */
+  (void) foo2 (i);                    /* { dg-bogus "non-compatible|abort" } */
 }
 
 int foo1(int arg)
--- gcc/testsuite/gcc.dg/va-arg-1.c
+++ gcc/testsuite/gcc.dg/va-arg-1.c
@@ -7,7 +7,7 @@ volatile int i;
 
 void foo()
 {
-  i = va_arg(v, char); /* { dg-warning "is promoted to|so you should" "char" } */
-  i = va_arg(v, short); /* { dg-warning "is promoted to" "short" } */
-  i = va_arg(v, float); /* { dg-warning "is promoted to" "float" } */
+  i = va_arg(v, char); /* { dg-warning "is promoted to|so you should|abort" "char" } */
+  i = va_arg(v, short); /* { dg-warning "is promoted to|abort" "short" } */
+  i = va_arg(v, float); /* { dg-warning "is promoted to|abort" "float" } */
 }
