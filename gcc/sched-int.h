2a9f2ad3a598ad316d84fa1b2e49033f7a9a2e00
Kazu Hirata <kazu@cs.umass.edu>
2004-02-23 17:02:51 +0000

    convert.c, [...]: Update copyright.
    	* convert.c, gcov-io.c, libgcov.c, sched-int.h, sibcall.c,
    	config/rs6000/linux.h, config/rs6000/rs6000-c.c: Update
    	copyright.
    
    From-SVN: r78318

--- gcc/sched-int.h
+++ gcc/sched-int.h
@@ -1,7 +1,7 @@
 /* Instruction scheduling pass.  This file contains definitions used
    internally in the scheduler.
    Copyright (C) 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2003 Free Software Foundation, Inc.
+   1999, 2000, 2001, 2003, 2004 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
