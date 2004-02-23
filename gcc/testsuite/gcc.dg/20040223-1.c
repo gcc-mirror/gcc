2be57c8857a6cb8ce48743bea7812269dce5263f
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-23 15:27:49 +0000

    2004-02-23  Andrew Pinski  <pinskia@physics.uc.edu>
            PR c/14156
            * gcc.dg/20040223-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@78313 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20040223-1.c
@@ -0,0 +1,41 @@
+/* GCC was not warning about taking the address of paramaters or
+   fields of struct paramaters when returning them.  */
+/* PR c/14156 */
+
+/* { dg-do compile  } */
+
+
+int * f( int a)
+{
+	return &a;/* { dg-warning "address" "" } */
+}
+
+int * g()
+{
+	int b = 0;
+	return &b;/* { dg-warning "address" "" } */
+}
+
+struct ll
+{
+  int i;
+};
+
+int *h(struct ll c)
+{
+  return &c.i;/* { dg-warning "address" "" } */
+}
+
+
+struct ll d;
+
+int *i()
+{
+  return &d.i;/* { dg-bogus "address" "" } */
+}
+
+
+int *j(struct ll *c)
+{
+  return &c->i; /* { dg-bogus "address" "" } */
+}
