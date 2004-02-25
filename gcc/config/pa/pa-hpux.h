cacee710b3bbe9d010c6dce2e7ede23a1329941a
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-25 19:45:13 +0000

    * config.gcc (hppa*-*-*, parisc*-*-*): Add MASK_BIG_SWITCH to all 	target_cpu_default defines. 	* pa-hpux.h (TARGET_DEFAULT): Add MASK_BIG_SWITCH to define. 	* pa.h (TARGET_DEFAULT): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@78446 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/pa/pa-hpux.h
+++ gcc/config/pa/pa-hpux.h
@@ -19,7 +19,7 @@ the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
 #undef TARGET_DEFAULT
-#define TARGET_DEFAULT 0
+#define TARGET_DEFAULT MASK_BIG_SWITCH
 
 /* Make GCC agree with types.h.  */
 #undef SIZE_TYPE
