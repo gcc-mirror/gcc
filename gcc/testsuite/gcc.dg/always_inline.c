c5b6f18e7a3d5a9269704287ca91483d60476e66
Mark Mitchell <mark@codesourcery.com>
2004-02-29 23:43:29 +0000

    re PR middle-end/13448 (gcc 3.3.2, internal error when -O3 and trying to modify function const arg)
    	PR middle-end/13448
    	* c-tree.h (readonly_warning): Rename to ...
    	(readonly_error): ... this.
    	* c-typeck.c (build_unary_op): Adjust accordingly.
    	(readonly_warning): Rename to ...
    	(readonly_error): ... this and issue errors, not warnings.
    	(build_modify_expr): Call readonly_error, not readonly_warning.
    	(c_expand_asm_operands): Likewise.
    	* tree-inline.c (optimize_inline_calls): Do not inline functions
    	after errors have occurred.
    
    	PR middle-end/13448
    	* gcc.dg/inline-5.c: New test.
    	* gcc.dg/always-inline.c: Split out tests into ...
    	* gcc.dg/always-inline2.c: ... this and ...
    	* gcc.dg/always-inline3.c: ... this.
    
    From-SVN: r78682

--- gcc/testsuite/gcc.dg/always_inline.c
+++ gcc/testsuite/gcc.dg/always_inline.c
@@ -1,18 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-Winline -O2" } */
 #include <stdarg.h>
-inline __attribute__ ((always_inline)) void t(void); /* { dg-error "body not available" "" } */
-void
-q(void)
-{
-  t(); 				/* { dg-error "called from here" "" } */
-}
-inline __attribute__ ((always_inline)) void
-q2(void)
-{ 				/* { dg-error "recursive" "" } */
-  q2(); 			/* { dg-error "called from here" "" } */
-  q2(); 			/* { dg-error "called from here" "" } */
-}
 inline __attribute__ ((always_inline)) void
 e(int t, ...)
 {				/* { dg-error "variable argument" "" } */
