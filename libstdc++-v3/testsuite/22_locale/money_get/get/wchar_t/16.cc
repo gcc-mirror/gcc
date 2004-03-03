c5cd338a49deb99ef091b0b060185d8e97ebb982
No Author <no-author@gcc.gnu.org>
2004-03-03 00:39:27 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_4-branch'.
    From-SVN: r78802

---
+++ libstdc++-v3/testsuite/22_locale/money_get/get/wchar_t/16.cc
@@ -0,0 +1,75 @@
+// 2004-03-02  Paolo Carlini  <pcarlini@suse.de>
+
+// Copyright (C) 2004 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 22.2.6.1.1 money_get members
+
+#include <locale>
+#include <sstream>
+#include <testsuite_hooks.h>
+
+// Fail as soon as value digits are not present.
+void test01()
+{
+  using namespace std;
+  bool test __attribute__((unused)) = true;
+
+  typedef istreambuf_iterator<wchar_t> iterator_type;
+
+  // basic construction
+  locale loc_c = locale::classic();
+  locale loc_de = __gnu_test::try_named_locale("de_DE@euro");
+  locale loc_hk = __gnu_test::try_named_locale("en_HK");
+  VERIFY( loc_hk != loc_de );
+  VERIFY( loc_c != loc_hk );
+
+  iterator_type end01, end02;
+  wistringstream iss01, iss02;
+  iss01.imbue(loc_de);
+  iss02.imbue(loc_hk);
+
+  // cache the money_get facet
+  const money_get<wchar_t>& mon_get_01 =
+    use_facet<money_get<wchar_t> >(iss01.getloc());
+  const money_get<wchar_t>& mon_get_02 =
+    use_facet<money_get<wchar_t> >(iss02.getloc());
+
+  iss01.setf(ios_base::showbase);
+  iss01.str(L"EUR ");
+  iterator_type is_it01(iss01);
+  long double result1;
+  ios_base::iostate err01 = ios_base::goodbit;
+  end01 = mon_get_01.get(is_it01, end01, true, iss01, err01, result1);
+  VERIFY( err01 == ios_base::failbit );
+  VERIFY( *end01 == L'E' );
+  
+  iss02.str(L"(HKD )");
+  iterator_type is_it02(iss02);
+  long double result2;
+  ios_base::iostate err02 = ios_base::goodbit;
+  end02 = mon_get_02.get(is_it02, end02, true, iss02, err02, result2);
+  VERIFY( err02 == ios_base::failbit );
+  VERIFY( *end02 == L')' );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/27_io/fpos/14320-5.cc
@@ -0,0 +1,45 @@
+// 2004-03-02  Petur Runolfsson  <peturr02@ru.is>
+
+// Copyright (C) 2004 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 27.4.3 fpos
+
+#include <iterator>
+#include <sstream>
+#include <testsuite_hooks.h>
+
+// libstdc++/14320
+void test01()
+{
+  using namespace std;
+  bool test __attribute__((unused)) = true;
+
+  typedef istreambuf_iterator<char>::difference_type Distance;
+	
+  Distance d;
+  istringstream in("5");
+  in >> d;
+  VERIFY( d == 5 );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
