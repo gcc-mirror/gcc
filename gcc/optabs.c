d28040e26793e96bb6df6f45a00741e5f0978584
Richard Henderson <rth@redhat.com>
2004-03-02 16:45:06 -0800

    re PR middle-end/11767 (different code behaviour with -fnon-call-exceptions -fprofile-arcs)
            PR middle-end/11767
            * coverage.c (coverage_counter_ref): Set MEM_NOTRAP_P.
            * optabs.c (prepare_cmp_insn): Force trapping memories to registers
            before the compare, if flag_non_call_exceptions.
    	* g++.dg/other/profile1.C: New.
    
    From-SVN: r78804

--- gcc/optabs.c
+++ gcc/optabs.c
@@ -3651,6 +3651,16 @@ prepare_cmp_insn (rtx *px, rtx *py, enum rtx_code *pcomparison, rtx size,
       return;
     }
 
+  /* Don't allow operands to the compare to trap, as that can put the
+     compare and branch in different basic blocks.  */
+  if (flag_non_call_exceptions)
+    {
+      if (may_trap_p (x))
+	x = force_reg (mode, x);
+      if (may_trap_p (y))
+	y = force_reg (mode, y);
+    }
+
   *px = x;
   *py = y;
   if (can_compare_p (*pcomparison, mode, purpose))
