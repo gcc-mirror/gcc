aa42f99db9e8d53813493550e4237db403cbedd2
Steve Ellcey <sje@cup.hp.com>
2004-03-04 21:21:36 +0000

    ia64.md (divdf3_internal_thr): Fix algorithm.
    	* config/ia64/ia64.md (divdf3_internal_thr): Fix algorithm.
    	* testsuite/gcc.dg/20040303-1.c: New test.
    
    From-SVN: r78934

---
+++ gcc/testsuite/gcc.dg/20040303-1.c
@@ -0,0 +1,20 @@
+/* Test floating point division on ia64.  There was a bug in the
+   max-throughput version of the inline division code.  Expecting an
+   exact value from a floating point expression is unwise but GCC
+   depends on it in allocno_compare.  */
+
+/* { dg-do run { target ia64-*-* } } */
+/* { dg-options "-minline-float-divide-max-throughput" { target ia64-*-* } } */
+
+extern void abort (void);
+
+volatile int i = 24;
+volatile int j = 30;
+volatile int k = 1;
+
+int main()
+{
+        int pri2 = (((double) i / j) * (10000 / 1000) * k);
+        if (pri2 != 8) abort();
+	return 0;
+}
