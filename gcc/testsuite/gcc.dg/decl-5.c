64a3ee6bd9f60bc86472b21fadef0c1287a3d314
Eric Botcazou <ebotcazou@libertysurf.fr>
2004-03-06 10:28:51 +0100

    re PR c/14114 ([unit-at-a-time] ICE in gen_subprogram_die)
    	PR c/14114
    	* gcc.dg/decl-5.c: New test.
    
    From-SVN: r79012

---
+++ gcc/testsuite/gcc.dg/decl-5.c
@@ -0,0 +1,20 @@
+/* PR c/14114 */
+/* Origin: <snyder@fnal.gov> */
+/* { dg-do compile } */
+/* { dg-options "-O2 -g" } */
+
+/* This used to fail because the compiler thought that the
+   declaration of 'c' from 'b' was shadowing that from 'a'.  */
+
+void a()
+{
+  void c();
+  c();
+} /* { dg-bogus "error" "PR c/14114" { xfail *-*-* } } */
+
+void b()
+{
+  void c();
+}
+
+void c() {}
