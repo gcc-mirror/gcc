0e2fb02d035c180b801107fe237e0fe96b16dfbc
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-03-07 14:45:42 +0000

    * fold-const.c (fold) <EQ_EXPR>: Rewrite optimization to transform 	"foo++ == const" into "++foo == const+incr".
    	* gcc.c-torture/execute/20040307-1.c: New test case.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@79064 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20040307-1.c
@@ -0,0 +1,26 @@
+int main()
+{
+  int b = 0;
+
+  struct {
+    unsigned int bit0:1;
+    unsigned int bit1:1;
+    unsigned int bit2:1;
+    unsigned int bit3:1;
+    unsigned int bit4:1;
+    unsigned int bit5:1;
+    unsigned int bit6:1;
+    unsigned int bit7:1;
+    unsigned int ubyte:24;
+  } sdata = {0x01};
+
+  while ( sdata.bit0-- > 0 ) {
+    b++ ;
+    if ( b > 100 ) break;
+  }
+
+  if (b != 1)
+    abort ();
+  return 0;
+}
+
