3d8dc12357833708007270416b14d7e76362c9b4
No Author <no-author@gcc.gnu.org>
2004-03-08 06:58:29 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_4-branch'.
    From-SVN: r79102

---
+++ gcc/testsuite/gcc.c-torture/execute/20040308-1.c
@@ -0,0 +1,21 @@
+/* This used to fail on SPARC with an unaligned memory access.  */
+
+void foo(int n)
+{
+  struct S {
+    int i[n];
+    unsigned int b:1;
+    int i2;
+  } __attribute__ ((packed)) __attribute__ ((aligned (4)));
+
+  struct S s;
+
+  s.i2 = 0;
+}
+
+int main(void)
+{
+  foo(4);
+  
+  return 0;
+}
