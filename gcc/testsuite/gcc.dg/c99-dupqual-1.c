a7b87e06a19a85fe04e6c1e381956ac37f4e0a15
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-03-11 00:45:37 +0000

    PR c/14517         * c-decl.c (grokdeclarator): Don't warn for duplicate qualifiers         except for pedantic c90 mode.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@79286 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/c99-dupqual-1.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-options "-std=iso9899:1999 -pedantic-errors" } */
+
+typedef const int CI;
+const const int c1;		/* { dg-bogus "duplicate" } */
+const CI c2;			/* { dg-bogus "duplicate" } */
+const CI *c3;			/* { dg-bogus "duplicate" } */
+
+typedef volatile int VI;
+volatile volatile int v1;	/* { dg-bogus "duplicate" } */
+volatile VI v2;			/* { dg-bogus "duplicate" } */
+volatile VI *v3;		/* { dg-bogus "duplicate" } */
