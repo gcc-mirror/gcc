fd0c8c8d00232673f01aeab099b9f69ffcfb6e85
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2004-03-12 13:33:42 +0000

    builtins.c (expand_builtin_mathfn): Add pow10* to the existing exp10* case.
    	* builtins.c (expand_builtin_mathfn): Add pow10* to the
    	existing exp10* case.
    	(expand_builtin): Likewise.
    
    testsuite:
    	* gcc.dg/builtins-34.c: Also check pow10*.
    
    From-SVN: r79383

--- gcc/testsuite/gcc.dg/builtins-34.c
+++ gcc/testsuite/gcc.dg/builtins-34.c
@@ -1,7 +1,7 @@
 /* Copyright (C) 2004 Free Software Foundation.
 
-   Check that exp10, exp10f, exp10l, exp2, exp2f and exp2l
-   built-in functions compile.
+   Check that exp10, exp10f, exp10l, exp2, exp2f, exp2l, pow10, pow10f
+   and pow10l built-in functions compile.
 
    Written by Uros Bizjak, 13th February 2004.  */
 
@@ -10,10 +10,13 @@
 
 extern double exp10(double);
 extern double exp2(double);
+extern double pow10(double);
 extern float exp10f(float);
 extern float exp2f(float);
+extern float pow10f(float);
 extern long double exp10l(long double);
 extern long double exp2l(long double);
+extern long double pow10l(long double);
 
 
 double test1(double x)
@@ -26,6 +29,11 @@ double test2(double x)
   return exp2(x);
 }
 
+double test3(double x)
+{
+  return pow10(x);
+}
+
 float test1f(float x)
 {
   return exp10f(x);
@@ -36,6 +44,11 @@ float test2f(float x)
   return exp2f(x);
 }
 
+float test3f(float x)
+{
+  return pow10f(x);
+}
+
 long double test1l(long double x)
 {
   return exp10l(x);
@@ -44,5 +57,10 @@ long double test1l(long double x)
 long double test2l(long double x)
 {
   return exp2l(x);
+}
+
+long double test3l(long double x)
+{
+  return pow10l(x);
 }
 
