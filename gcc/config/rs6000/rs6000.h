c53bdcf59be1f9b9c8bc10ec3388564f47d62acf
Alan Modra <amodra@gcc.gnu.org>
2004-03-13 17:44:12 +1030

    re PR target/14567 ([3.4 only] long double and va_arg complex args)
    	PR target/14567
    	* config/rs6000/rs6000.h (UNITS_PER_ARG, RS6000_ARG_SIZE): Delete.
    	(HARD_REGNO_MODE_OK): Disallow TFmode for fp31.
    	* config/rs6000/rs6000.c (rs6000_arg_size): New function.
    	Update all users of RS6000_ARG_SIZE.
    	(function_arg_advance): Count fregno using mode size.
    	(function_arg): Handle long double split over regs and memory.
    	(function_arg_partial_nregs): Likewise.
    	(rs6000_va_arg): Repackage complex args.
    
    From-SVN: r79436

--- gcc/config/rs6000/rs6000.h
+++ gcc/config/rs6000/rs6000.h
@@ -1065,7 +1065,8 @@ extern const char *rs6000_warn_altivec_long_switch;
   (INT_REGNO_P (REGNO) ?						\
      INT_REGNO_P (REGNO + HARD_REGNO_NREGS (REGNO, MODE) - 1)	        \
    : FP_REGNO_P (REGNO) ?						\
-     (GET_MODE_CLASS (MODE) == MODE_FLOAT				\
+     ((GET_MODE_CLASS (MODE) == MODE_FLOAT				\
+       && FP_REGNO_P (REGNO + HARD_REGNO_NREGS (REGNO, MODE) - 1))	\
       || (GET_MODE_CLASS (MODE) == MODE_INT				\
 	  && GET_MODE_SIZE (MODE) == UNITS_PER_FP_WORD))		\
    : ALTIVEC_REGNO_P (REGNO) ? ALTIVEC_VECTOR_MODE (MODE)		\
@@ -1761,16 +1762,6 @@ typedef struct rs6000_args
   int call_cookie;		/* Do special things for this call */
   int sysv_gregno;		/* next available GP register */
 } CUMULATIVE_ARGS;
-
-/* Define intermediate macro to compute the size (in registers) of an argument
-   for the RS/6000.  */
-
-#define UNITS_PER_ARG (TARGET_32BIT ? 4 : 8)
-
-#define RS6000_ARG_SIZE(MODE, TYPE)					\
-((MODE) != BLKmode							\
- ? (GET_MODE_SIZE (MODE) + (UNITS_PER_ARG - 1)) / UNITS_PER_ARG		\
- : (int_size_in_bytes (TYPE) + (UNITS_PER_ARG - 1)) / UNITS_PER_ARG)
 
 /* Initialize a variable CUM of type CUMULATIVE_ARGS
    for a call to a function whose data type is FNTYPE.
