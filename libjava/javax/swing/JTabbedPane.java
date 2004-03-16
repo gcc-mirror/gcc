8c7dca8f62aedd623486b18614ad38e2108890d7
Michael Koch <konqueror@gmx.de>
2004-03-16 11:51:23 +0000

    2004-03-16  Michael Koch  <konqueror@gmx.de>
    	* javax/swing/JTabbedPane.java
    	(serialVersionUID): New field.
    
    From-SVN: r79531

--- libjava/javax/swing/JTabbedPane.java
+++ libjava/javax/swing/JTabbedPane.java
@@ -65,6 +65,8 @@ public class JTabbedPane extends JComponent implements Accessible, SwingConstant
 	    this.descr = descr;
 	}
     }
+
+  private static final long serialVersionUID = 1614381073220130939L;
     
     private Vector tabs = new Vector();
 
