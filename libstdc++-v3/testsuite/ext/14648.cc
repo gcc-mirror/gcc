ddacde3a1253c1873c32ac88fa10693677a919c3
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-03-19 16:08:15 +0000

    2004-03-19  Paolo Carlini  <pcarlini@suse.de>
    	PR libstdc++/14648
    	* include/ext/ropeimpl.h (rope<>::_S_apply_to_pieces): Fix
    	memory allocation/deallocation calls.
    	* testsuite/ext/14648.cc: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@79687 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/ext/14648.cc
@@ -0,0 +1,40 @@
+// Copyright (C) 2004 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+#include <iostream>
+#include <ext/hash_map> 
+#include <ext/rope> 
+
+// libstdc++/14648
+void test01() 
+{ 
+  using namespace std; 
+  using namespace __gnu_cxx;
+
+  typedef hash_map<char, crope, hash<char>, equal_to<char> > maptype; 
+  maptype m; 
+  m['l'] = "50";
+  m['x'] = "10";
+  cout << "m['x'] = " << m['x'] << endl; 
+}  
+
+int main()
+{
+  test01();
+  return 0;
+}
