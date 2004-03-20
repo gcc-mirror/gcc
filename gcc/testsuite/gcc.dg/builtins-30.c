b1c2d04a286edce934491c686702cd0f65a04e64
Joseph Myers <jsm@polyomino.org.uk>
2004-03-20 10:19:11 +0000

    re PR c/14635 (nan functions should not be C90 built-ins)
    	PR c/14635
    	* builtins.def (nan, nanf, nanl, nans, nansf, nansl): Change to
    	DEF_GCC_BUILTIN.
    
    testsuite:
    	* builtins-1.c (nan, nans): Don't test.
    	* builtins-30.c: Don't use nan, nanf, nanl.
    	* builtins-35.c: New test.
    
    From-SVN: r79736

--- gcc/testsuite/gcc.dg/builtins-30.c
+++ gcc/testsuite/gcc.dg/builtins-30.c
@@ -6,21 +6,21 @@ extern double strtod (const char *, char **);
 
 /* A built-in function may be overridden by an old-style definition
    specifying too few arguments... */
-double nan ()
+double cos ()
 {  /* { dg-warning "shadowing built-in" } */
   return strtod ("nan", 0);
 }
 
 /* the right number, but the wrong type, arguments... */
-float nanf (foo)
+double sin (foo)
      int foo UNUSED;  /* { dg-warning "shadowing built-in" } */
 {
   return strtod ("nan", 0);
 }
 
 /* or too many arguments.  */
-long double nanl (foo, bar)
-     const char *foo UNUSED;  /* { dg-warning "shadowing built-in" } */
+long double cosl (foo, bar)
+     long double foo UNUSED;  /* { dg-warning "shadowing built-in" } */
      int bar UNUSED;
 {
   return strtod ("nan", 0);
