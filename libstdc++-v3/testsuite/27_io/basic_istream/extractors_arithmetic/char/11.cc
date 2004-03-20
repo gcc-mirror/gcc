4914bf310e7208fb2f5404c06f191c6a2919f799
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-03-20 12:54:35 +0000

    2004-03-20  Paolo Carlini  <pcarlini@suse.de>
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/2.cc:
    	Remove junk.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/3.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/6.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/7.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/8.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/9.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/10.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/11.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/12.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/13.cc:
    	Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@79742 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/27_io/basic_istream/extractors_arithmetic/char/11.cc
+++ libstdc++-v3/testsuite/27_io/basic_istream/extractors_arithmetic/char/11.cc
@@ -25,22 +25,6 @@
 #include <locale>
 #include <testsuite_hooks.h>
 
-std::string str_01;
-std::string str_02("true false 0 1 110001");
-std::string str_03("-19999999 777777 -234234 233 -234 33 1 66300.25 .315 1.5");
-std::string str_04("0123");
-
-std::stringbuf isbuf_01(std::ios_base::in);
-std::stringbuf isbuf_02(str_02, std::ios_base::in);
-std::stringbuf isbuf_03(str_03, std::ios_base::in);
-std::stringbuf isbuf_04(str_04, std::ios_base::in);
-
-std::istream is_01(NULL);
-std::istream is_02(&isbuf_02);
-std::istream is_03(&isbuf_03);
-std::istream is_04(&isbuf_04);
-std::stringstream ss_01(str_01);
- 
 // In the presence of no fmtflags, the input operator should behave
 // like strtol(x, y, 0)
 // libstdc++/90
