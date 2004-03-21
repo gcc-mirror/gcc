e6d8a6d58af6a8df4fe5070f9a02d36997939d71
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-03-21 17:48:39 +0000

    PR c++/14616 	* decl.c (cp_finish_decl): Compute the size of arrays declared in 	templates, if their type is non-dependent.
    	PR c++/14616
    	* g++.dg/init/array13.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@79788 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/init/array13.C
@@ -0,0 +1,9 @@
+template <typename DisjointSet> 
+struct test_disjoint_set { 
+  static void do_test() 
+  { 
+    unsigned int elts[] 
+        = { 0, 1, 2, 3 }; 
+    const int N = sizeof(elts)/sizeof(*elts); 
+  } 
+}; 
