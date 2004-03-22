07ef69611a8c1821ca8c5ee638576097b40b861a
Ulrich Weigand <uweigand@de.ibm.com>
2004-03-22 02:31:24 +0000

    * gcc.dg/i386-sse-8.c: Reset default options.
    From-SVN: r79808

--- gcc/testsuite/gcc.dg/i386-sse-8.c
+++ gcc/testsuite/gcc.dg/i386-sse-8.c
@@ -2,6 +2,7 @@
 /* Origin: <Pawe Sikora <pluto@ds14.agh.edu.pl> */
 
 /* { dg-do compile } */
+/* { dg-options "" } */
 /* { dg-options "-march=pentium3" { target i?86-*-* x86_64-*-* } } */
 
 int main() 
