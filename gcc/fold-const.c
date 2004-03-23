2d742ce75d7f037b53b47d64ddf32ace14cc76c8
Kazu Hirata <kazu@gcc.gnu.org>
2004-03-23 23:31:00 +0000

    re PR rtl-optimization/14669 (Wrong code with -O for enum values expression E4 <= t && t <= E6)
    gcc/
    	PR optimization/14669
    	* fold-const.c (fold): Only unwiden integer comparisons for equality
    	and inequality operators, or when the signedness doesn't change.
    
    gcc/testsuite/
    	PR optimization/14669
    	* g++.dg/opt/fold2.C: New test case.
    
    From-SVN: r79882

--- gcc/fold-const.c
+++ gcc/fold-const.c
@@ -7478,6 +7478,9 @@ fold (tree expr)
       else if (TREE_CODE (TREE_TYPE (arg0)) == INTEGER_TYPE
 	       && TREE_CODE (arg0) == NOP_EXPR
 	       && (tem = get_unwidened (arg0, NULL_TREE)) != arg0
+	       && (code == EQ_EXPR || code == NE_EXPR
+		   || TREE_UNSIGNED (TREE_TYPE (arg0))
+		      == TREE_UNSIGNED (TREE_TYPE (tem)))
 	       && (t1 = get_unwidened (arg1, TREE_TYPE (tem))) != 0
 	       && (TREE_TYPE (t1) == TREE_TYPE (tem)
 		   || (TREE_CODE (t1) == INTEGER_CST
