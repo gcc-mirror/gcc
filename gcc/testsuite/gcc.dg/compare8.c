2f3ce1268cda53b9d9e952049d82c86afcc7edb5
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-03-24 00:19:06 +0000

    * gcc.dg/compare8.c: New test case.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@79891 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/compare8.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-options "-Wsign-compare" } */
+
+int
+f(unsigned short a1, unsigned short a2, unsigned int b)
+{
+  return ((a1+a2)|5) > b ? 2 : 3;  /* { dg-bogus "signed and unsigned" } */
+}
+
+int
+g(unsigned short a1, unsigned short a2, unsigned int b)
+{
+  return ((a1+a2)&5) > b ? 2 : 3;  /* { dg-bogus "signed and unsigned" } */
+}
+
