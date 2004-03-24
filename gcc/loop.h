ef335eb8927b7a2018936f62cc745765344f2bd8
Kazu Hirata <kazu@cs.umass.edu>
2004-03-24 23:45:50 +0000

    dwarf2asm.c, [...]: Update copyright.
    gcc/
    	* dwarf2asm.c, loop.h, pretty-print.c, pretty-print.h,
    	config/i386/mmintrin.h: Update copyright.
    
    cp/
    	* cxx-pretty-print.c, cxx-pretty-print.h, decl.h, friend.c:
    	Update copyright.
    
    From-SVN: r79938

--- gcc/loop.h
+++ gcc/loop.h
@@ -1,5 +1,5 @@
 /* Loop optimization definitions for GCC
-   Copyright (C) 1991, 1995, 1998, 1999, 2000, 2001, 2002, 2003
+   Copyright (C) 1991, 1995, 1998, 1999, 2000, 2001, 2002, 2003, 2004
    Free Software Foundation, Inc.
 
 This file is part of GCC.
