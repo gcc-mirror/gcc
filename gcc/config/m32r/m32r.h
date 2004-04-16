d6b2e5a0ecc06fc359b35d34e4728ec647238039
nickc <nickc@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-04-16 10:30:32 +0000

    * config/m32r/m32r.h (BIG_ENDIAN_BIT): Deleted to fix endian bug. (TARGET_LITTLE_ENDIAN, TARGET_BIG_ENDIAN, TARGET_DEFAULT): Changed. Ditto. (LITTLE_ENDIAN_BIT, TARGET_CPU_DEFAULT,	TARGET_ENDIAN_DEFAULT): Added. Ditto. * config/m32r/little.h (TARGET_LITTLE_ENDIAN): Deleted. (TARGET_ENDIAN_DEFAULT): Added.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@80747 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/m32r/m32r.h
+++ gcc/config/m32r/m32r.h
@@ -246,14 +246,19 @@ extern int target_flags;
 #undef  TARGET_M32R
 #define TARGET_M32R             (! TARGET_M32RX && ! TARGET_M32R2)
 
-/* Big Endian Flag.  */
-#define BIG_ENDIAN_BIT 		(1 << 7)
-#define TARGET_BIG_ENDIAN       (target_flags & BIG_ENDIAN_BIT)
-
 /* Little Endian Flag.  */
-#define LITTLE_ENDIAN_BIT 	(1 <<  8)
-#ifndef TARGET_LITTLE_ENDIAN 	/* See little.h */
-#define TARGET_LITTLE_ENDIAN    (target_flags & LITTLE_ENDIAN_BIT)
+#define LITTLE_ENDIAN_BIT 	(1 << 7)
+#define TARGET_LITTLE_ENDIAN	(target_flags & LITTLE_ENDIAN_BIT)
+#define TARGET_BIG_ENDIAN       (! TARGET_LITTLE_ENDIAN)
+
+/* This defaults us to big-endian.  */
+#ifndef TARGET_ENDIAN_DEFAULT
+#define TARGET_ENDIAN_DEFAULT 0
+#endif
+
+/* This defaults us to m32r.  */
+#ifndef TARGET_CPU_DEFAULT
+#define TARGET_CPU_DEFAULT 0
 #endif
 
 /* Macro to define tables used to set the flags.
@@ -267,7 +272,7 @@ extern int target_flags;
 #endif
 
 #ifndef TARGET_DEFAULT
-#define TARGET_DEFAULT 0
+#define TARGET_DEFAULT (TARGET_CPU_DEFAULT | TARGET_ENDIAN_DEFAULT)
 #endif
 
 #define TARGET_SWITCHES							\
