838a4849fab52af1c5d54703b4591d403544dd7f
Michael Koch <mkoch@gcc.gnu.org>
2004-04-22 18:02:54 +0000

    [multiple changes]
    2004-04-22  Mark Wielaard  <mark@klomp.org>
    
    	* native/jni/gtk-peer/gnu_java_awt_peer_gtk_GtkComponentPeer.c:
    	Changed C++ comments into C comments.
    	* native/jni/gtk-peer/gnu_java_awt_peer_gtk_GtkMenuPeer.c:
    	Likewise.
    
    2004-04-22  Michael Koch  <konqueror@gmx.de>
    
    	* jni/gtk-peer/gnu_java_awt_peer_gtk_GtkPopupMenuPeer.c:
    	Merged copyright year with GNU classpath.
    
    From-SVN: r81042

--- libjava/jni/gtk-peer/gnu_java_awt_peer_gtk_GtkPopupMenuPeer.c
+++ libjava/jni/gtk-peer/gnu_java_awt_peer_gtk_GtkPopupMenuPeer.c
@@ -1,5 +1,5 @@
 /* gtkpopupmenupeer.c -- Native implementation of GtkPopupMenuPeer
-   Copyright (C) 1999 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2004 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
