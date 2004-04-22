ef30ee6ea313013d102c66e7f463b6165da2cedc
Ulrich Weigand <uweigand@de.ibm.com>
2004-04-22 21:31:27 +0000

    expr.c (store_constructor): Use gen_int_mode to correctly sign-extend CONST_INT value.
    	* expr.c (store_constructor): Use gen_int_mode to correctly
    	sign-extend CONST_INT value.
    
    From-SVN: r81055

--- gcc/expr.c
+++ gcc/expr.c
@@ -5115,7 +5115,7 @@ store_constructor (tree exp, rtx target, int cleared, HOST_WIDE_INT size)
 		{
 		  if (word != 0 || ! cleared)
 		    {
-		      rtx datum = GEN_INT (word);
+		      rtx datum = gen_int_mode (word, mode);
 		      rtx to_rtx;
 
 		      /* The assumption here is that it is safe to use
