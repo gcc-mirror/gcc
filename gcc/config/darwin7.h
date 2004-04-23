196771fa67aeea60e48cd9cd500a2b26ebc59470
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-04-23 17:56:08 +0000

    2004-04-23  Andrew Pinski  <pinskia@physics.uc.edu>
            * config/darwin7.h: New file.
            * config.gcc (*-*-darwin*): Add darwin7.h if the
            version is greater than 6.
            * config/darwin.h (TARGET_C99_FUNCTIONS): Define.
            (MATH_LIBRARY): Wrap in ifdefs.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@81100 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/darwin7.h
@@ -0,0 +1,28 @@
+/* Target definitions for Darwin 7.0 and above (Mac OS X) systems.
+   Copyright (C) 2004
+   Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+/* Darwin 7.0 and above have C99 functions.   */
+#define TARGET_C99_FUNCTIONS 1
+
+/* But for some reason they are located in libmx so have it
+   be included when asked for and automatically when linking
+   with gfortran and g++.   */
+#define MATH_LIBRARY "-lmx"
