c26fa757dc8e41cdc141aac1cb54aaf4549c6bc1
Paolo Carlini <pcarlini@suse.de>
2004-04-25 12:56:41 +0000

    10.cc: New.
    2004-04-25  Paolo Carlini  <pcarlini@suse.de>
    
    	* testsuite/21_strings/basic_string/inserters_extractors/char/10.cc:
    	New.
    	* testsuite/21_strings/basic_string/inserters_extractors/wchar_t/10.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/getline/char/5.cc: Likewise.
    
    From-SVN: r81159

---
+++ libstdc++-v3/testsuite/21_strings/basic_string/inserters_extractors/wchar_t/10.cc
@@ -0,0 +1,80 @@
+// Copyright (C) 2004 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 21.3.7.9 inserters and extractors
+
+#include <istream>
+#include <string>
+#include <fstream>
+#include <testsuite_hooks.h>
+
+using namespace std;
+
+wstring prepare(wstring::size_type len, unsigned nchunks, wchar_t delim)
+{
+  wstring ret;
+  for (unsigned i = 0; i < nchunks; ++i)
+    {
+      for (wstring::size_type j = 0; j < len; ++j)
+	ret.push_back(L'a' + rand() % 26);
+      len *= 2;
+      ret.push_back(delim);
+    }
+  return ret;
+}
+
+void check(wistream& stream, const wstring& str, wchar_t delim)
+{
+  bool test __attribute__((unused)) = true;
+
+  wstring chunk;
+  wstring::size_type index = 0, index_new = 0;
+
+  while (getline(stream, chunk, delim))
+    {
+      index_new = str.find(delim, index);
+      VERIFY( !str.compare(index, index_new - index, chunk) );
+      index = index_new + 1;
+    }
+  VERIFY( stream.eof() );
+}
+
+// istream& getline(istream&, string&, char)
+void test01()
+{
+  const char filename[] = "inserters_extractors-2.txt";
+
+  const wchar_t delim = L'|';
+  const wstring data = prepare(777, 10, delim);
+
+  wofstream ofstrm;
+  ofstrm.open(filename);
+  ofstrm.write(data.data(), data.size());
+  ofstrm.close();
+
+  wifstream ifstrm;
+  ifstrm.open(filename);
+  check(ifstrm, data, delim);
+  ifstrm.close();
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
