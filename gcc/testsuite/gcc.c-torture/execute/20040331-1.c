2fe8b9c8f908a4ea5adcd2fd502bfdd793c5630d
Kazu Hirata <kazu@cs.umass.edu>
2004-04-30 15:30:55 +0000

    20040331-1.c: Don't use too wide a bit-field on 16-bit targets.
    	* gcc.c-torture/execute/20040331-1.c: Don't use too wide a
    	bit-field on 16-bit targets.
    
    From-SVN: r81343

--- gcc/testsuite/gcc.c-torture/execute/20040331-1.c
+++ gcc/testsuite/gcc.c-torture/execute/20040331-1.c
@@ -5,8 +5,16 @@ extern void exit (int);
 int
 main (void)
 {
+#if __INT_MAX__ >= 2147483647
   struct { int count: 31; } s = { 0 };
   while (s.count--)
     abort ();
+#elif __INT_MAX__ >= 32767
+  struct { int count: 15; } s = { 0 };
+  while (s.count--)
+    abort ();
+#else
+  /* Don't bother because __INT_MAX__ is too small.  */
+#endif
   exit (0);
 }
