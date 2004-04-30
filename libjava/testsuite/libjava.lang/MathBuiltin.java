91da6229e67e97942829c7e50696df0de7a84221
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-04-30 14:15:08 +0000

    * builtins.c (java_builtins): Add acos, asin, ceil and floor. 	(initialize_builtins): Likewise, define acos, asin, ceil and floor.
    	* testsuite/libjava.lang/MathBuiltin.java: Add tests for acos, asin,
    	ceil and floor.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@81341 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/testsuite/libjava.lang/MathBuiltin.java
+++ libjava/testsuite/libjava.lang/MathBuiltin.java
@@ -5,6 +5,16 @@ class MathBuiltin
     return Math.abs(x);
   }
 
+  static double acos(double x)
+  {
+    return Math.acos(x);
+  }
+
+  static double asin(double x)
+  {
+    return Math.asin(x);
+  }
+
   static double atan(double x)
   {
     return Math.atan(x);
@@ -15,6 +25,11 @@ class MathBuiltin
     return Math.atan2(x,y);
   }
 
+  static double ceil(double x)
+  {
+    return Math.ceil(x);
+  }
+
   static double cos(double x)
   {
     return Math.cos(x);
@@ -23,6 +38,11 @@ class MathBuiltin
   static double exp(double x)
   {
     return Math.exp(x);
+  }
+
+  static double floor(double x)
+  {
+    return Math.floor(x);
   }
 
   static double log(double x)
@@ -62,8 +82,9 @@ class MathBuiltin
 
   public static void main(String argv[])
   {
-    double sum = abs (1.0) + atan (1.0) + atan2 (1.0, 1.0) + cos (1.0)
-                 + exp (1.0) + log(1.0) + max(1.0, 1.0) + min (1.0, 1.0)
+    double sum = abs (1.0) + acos (1.0) + asin (1.0) + atan (1.0)
+		 + atan2 (1.0, 1.0) + ceil (1.0) + cos (1.0) + exp (1.0)
+		 + floor (1.0) + log(1.0) + max(1.0, 1.0) + min (1.0, 1.0)
 		 + pow (1.0, 1.0) + sin (1.0) + sqrt(1.0) + tan(1.0);
   }
 }
