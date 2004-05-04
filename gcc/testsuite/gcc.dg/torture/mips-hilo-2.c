9318b36873d2cb12dc5b90dcf84369b2adb27d1b
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-05-04 17:57:39 +0000

    * reload1.c (inherit_piecemeal_p): New function. 	(emit_reload_insns): When reloading a group of hard registers, use 	inherit_piecemeal_p to decide whether the values of individual hard 	registers can be inherited.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@81480 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/mips-hilo-2.c
@@ -0,0 +1,24 @@
+/* Due to a reload inheritance bug, the asm statement in f() would be passed
+   the low part of u.ll on little-endian 32-bit targets.  */
+/* { dg-do run { target mips*-*-* } } */
+
+unsigned int g;
+
+unsigned long long f (unsigned int x)
+{
+  union { unsigned long long ll; unsigned int parts[2]; } u;
+
+  u.ll = ((unsigned long long) x * x);
+  asm ("mflo\t%0" : "=r" (g) : "l" (u.parts[1]));
+  return u.ll;
+}
+
+int main ()
+{
+  union { unsigned long long ll; unsigned int parts[2]; } u;
+
+  u.ll = f (0x12345678);
+  if (g != u.parts[1])
+    abort ();
+  exit (0);
+}
