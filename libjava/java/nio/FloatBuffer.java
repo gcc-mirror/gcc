a221385cec60e1ba526d292898aeb99953891407
mkoch <mkoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-05-04 21:31:30 +0000

    2004-05-04  Michael Koch  <konqueror@gmx.de>
    	* java/nio/ByteBuffer.java,
    	java/nio/CharBuffer.java,
    	java/nio/DoubleBuffer.java,
    	java/nio/FloatBuffer.java,
    	java/nio/IntBuffer.java,
    	java/nio/LongBuffer.java,
    	java/nio/ShortBuffer.java:
    	(compareTo): Fixed bogus implementation in all buffer classes.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@81489 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/nio/FloatBuffer.java
+++ libjava/java/nio/FloatBuffer.java
@@ -1,5 +1,5 @@
 /* FloatBuffer.java -- 
-   Copyright (C) 2002, 2003 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2003, 2004  Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
@@ -265,32 +265,27 @@ public abstract class FloatBuffer extends Buffer
    */
   public int compareTo (Object obj)
   {
-    FloatBuffer a = (FloatBuffer) obj;
+    FloatBuffer other = (FloatBuffer) obj;
 
-    if (a.remaining () != remaining ())
-      return 1;
-
-    if (! hasArray () ||
-        ! a.hasArray ())
+    int num = Math.min(remaining(), other.remaining());
+    int pos_this = position();
+    int pos_other = other.position();
+    
+    for (int count = 0; count < num; count++)
       {
-        return 1;
+	float a = get(pos_this++);
+	float b = other.get(pos_other++);
+      	 
+	if (a == b)
+	  continue;
+      	   
+	if (a < b)
+	  return -1;
+      	   
+	return 1;
       }
-
-    int r = remaining ();
-    int i1 = position ();
-    int i2 = a.position ();
-
-    for (int i = 0; i < r; i++)
-      {
-        int t = (int) (get (i1) - a.get (i2));
-
-        if (t != 0)
-          {
-            return (int) t;
-          }
-      }
-
-    return 0;
+      
+    return remaining() - other.remaining();
   }
 
   /**
--- libjava/java/nio/LongBuffer.java
+++ libjava/java/nio/LongBuffer.java
@@ -1,5 +1,5 @@
 /* LongBuffer.java -- 
-   Copyright (C) 2002, 2003 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2003, 2004  Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
@@ -265,32 +265,27 @@ public abstract class LongBuffer extends Buffer
    */
   public int compareTo (Object obj)
   {
-    LongBuffer a = (LongBuffer) obj;
+    LongBuffer other = (LongBuffer) obj;
 
-    if (a.remaining () != remaining ())
-      return 1;
-
-    if (! hasArray () ||
-        ! a.hasArray ())
+    int num = Math.min(remaining(), other.remaining());
+    int pos_this = position();
+    int pos_other = other.position();
+    
+    for (int count = 0; count < num; count++)
       {
-        return 1;
+	 long a = get(pos_this++);
+	 long b = other.get(pos_other++);
+      	 
+	 if (a == b)
+	   continue;
+      	   
+	 if (a < b)
+	   return -1;
+      	   
+	 return 1;
       }
-
-    int r = remaining ();
-    int i1 = position ();
-    int i2 = a.position ();
-
-    for (int i = 0; i < r; i++)
-      {
-        int t = (int) (get (i1) - a.get (i2));
-
-        if (t != 0)
-          {
-            return (int) t;
-          }
-      }
-
-    return 0;
+      
+     return remaining() - other.remaining();
   }
 
   /**
