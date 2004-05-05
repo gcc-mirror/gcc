a54f498a27ec92d0552cd38390e7798708cb9fe2
Tom Tromey <tromey@redhat.com>
2004-05-05 08:24:13 +0000

    BufferedReader.java (skip): Removed unused variable.
    2004-05-05  Tom Tromey  <tromey@redhat.com>
    
    	* java/io/BufferedReader.java (skip): Removed unused
    	variable.
    
    From-SVN: r81511

--- libjava/java/io/BufferedReader.java
+++ libjava/java/io/BufferedReader.java
@@ -533,12 +533,13 @@ public class BufferedReader extends Reader
 	// skip the '\n' for us).  By doing this, we'll have to back up pos.
 	// That's easier than trying to keep track of whether we've skipped
 	// one element or not.
-	int ch;
 	if (pos > limit)
-	  if ((ch = read()) < 0)
-	    return 0;
-	  else
-	    --pos; 
+	  {
+	    if (read() < 0)
+	      return 0;
+	    else
+	      --pos; 
+	  }
 
 	int avail = limit - pos;
 
