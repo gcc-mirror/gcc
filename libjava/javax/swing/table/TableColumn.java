c839552495b8333dedf3efa233681d2d1192e159
Michael Koch <konqueror@gmx.de>
2004-05-06 13:04:07 +0000

    TableColumn.java: Reformated.
    2004-05-06  Michael Koch  <konqueror@gmx.de>
    
    	* javax/swing/table/TableColumn.java:
    	Reformated.
    
    From-SVN: r81561

--- libjava/javax/swing/table/TableColumn.java
+++ libjava/javax/swing/table/TableColumn.java
@@ -218,9 +218,10 @@ public class TableColumn implements Serializable
 	 * @param oldValue TODO
 	 * @param newValue TODO
 	 */
-	private void firePropertyChange(String property, boolean oldValue, boolean newValue) {
+	private void firePropertyChange(String property, boolean oldValue, boolean newValue)
+	{
 		firePropertyChange(property, new Boolean(oldValue), new Boolean(newValue));
-	} // firePropertyChange()
+	}
 
 	/**
 	 * setModelIndex
