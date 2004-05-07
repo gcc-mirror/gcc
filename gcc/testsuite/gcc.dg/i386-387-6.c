1251ad9a6f26699283f43e5fd3f2e9e49956a037
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-05-07 05:55:06 +0000

    * gcc.dg/i386-387-1.c: Add new test for __builtin_fmod. 	* gcc.dg/i386-387-2.c: Likewise.
    	* gcc.dg/i386-387-5.c: Add new tests for __builtin_log1p and
    	__builtin_drem.
    	* gcc.dg/i386-387-6.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@81607 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/i386-387-6.c
+++ gcc/testsuite/gcc.dg/i386-387-6.c
@@ -2,5 +2,9 @@
 /* { dg-do compile { target "i?86-*-*" } } */
 /* { dg-options "-O -ffast-math -mfpmath=387 -march=i686 -mno-fancy-math-387" } */
 /* { dg-final { scan-assembler "fpatan" } } */
+/* { dg-final { scan-assembler "fyl2xp1" } } */
+/* { dg-final { scan-assembler "fprem1" } } */
 
 double f1(double x) { return __builtin_atan(x); }
+double f2(double x) { return __builtin_log1p(x); }
+double f3(double x, double y) { return __builtin_drem(x,y); }
