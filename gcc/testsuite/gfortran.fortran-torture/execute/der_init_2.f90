60b93f922aa5caab5206936061e19e2accc995a3
pbrook <pbrook@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-05-13 23:22:13 +0000

    PR fortran/15314 	* gfortran.fortran-torture/execute/der_init_s.f90: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@81818 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.fortran-torture/execute/der_init_2.f90
@@ -0,0 +1,15 @@
+! PR 15314
+! We were looking at the type of the initialization expression, not the type
+! of the field.
+program der_init_2
+  implicit none
+  type foo
+    integer :: a(3) = 42
+    integer :: b = 123
+  end type
+
+  type (foo) :: v
+
+  if ((v%b .ne. 123) .or. any (v%a .ne. 42)) call abort ();
+end program
+
