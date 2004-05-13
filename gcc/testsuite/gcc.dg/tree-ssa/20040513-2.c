91581bccd5c8d82d2a6e06236f346dceab8ae86e
Jeff Law <law@redhat.com>
2004-05-13 14:55:06 -0600

    tree-ssa-forwprop.c (record_single_argument_cond_exprs): Accept new parameters for the statement and variable worklist as well as a...
    	* tree-ssa-forwprop.c (record_single_argument_cond_exprs): Accept
    	new parameters for the statement and variable worklist as well
    	as a bitmap of interesting SSA_NAMEs.  Walk over the statement
    	worklist recording interesting variables in the variable worklist
    	and bitmap.  Handle casts between integral and boolean types.
    	(substitute_single_use_vars): Accept new parameters for the statement
    	and variable worklist.  When a substitution is made add a new
    	entry to the statement worklist.  Handle casts between integral
    	and boolean types.
    	(tree_ssa_forward_propagate_single_use_vars): Rework to pass
    	worklists to children.  Iterate until the statement worklist
    	is empty.
    
    	* gcc.dg/tree-ssa/20040513-1.c: New test.
    	* gcc.dg/tree-ssa/20040513-2.c: New test.
    
    From-SVN: r81803

---
+++ gcc/testsuite/gcc.dg/tree-ssa/20040513-2.c
@@ -0,0 +1,24 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-dom2" } */
+int link_error(void);
+int s(void);
+
+int t(int i)
+{
+  _Bool g = i == 4;
+ int h = g;
+ _Bool j = h;
+ int k = j;
+ _Bool l = k == 0;
+ _Bool o = !l;
+ int m = o;
+
+ if (m)
+  if (i != 4)
+   return link_error();
+ return 0;
+}
+
+/* There should be no link_error calls, if there is any, the
+   optimization has failed */
+/* { dg-final { scan-tree-dump-times "link_error" 0 "dom2"} } */
