9fc4d79ba4e0abc41cb6ee48debc6a2308f22a75
Tobias Schlüter <tobi@gcc.gnu.org>
2004-05-14 15:00:04 +0200

    Make-lang.in, [...]: Update copyright years and boilerplate.
    * Make-lang.in, arith.c, arith.h, array.c, bbt.c, check.c,
    decl.c, dependency.c, dependency.h, dump-parse-tree.c, error.c,
    expr.c, f95-lang.c, gfortran.h, interface.c, intrinsic.c,
    intrinsic.h, io.c, iresolve.c, lang-specs.h, match.c, match.h,
    matchexp.c, misc.c, module.c, options.c, parse.c, parse.h,
    primary.c, resolve.c, scanner.c, simplify.c, st.c, symbol.c,
    trans-array.c, trans-array.h, trans-common.c, trans-const.c,
    trans-const.h, trans-decl.c, trans-expr.c, trans-intrinsic.c,
    trans-io.c, trans-stmt.c, trans-stmt.h, trans-types.c,
    trans-types.h, trans.c, trans.h: Update copyright years and
    boilerplate.
    * data.c: Likewise, also removed two whitespace-only lines.
            * gfortranspec.c, lang.opt: Update copyright years.
    
    From-SVN: r81839

--- gcc/fortran/array.c
+++ gcc/fortran/array.c
@@ -2,22 +2,22 @@
    Copyright (C) 2000, 2001, 2002 Free Software Foundation, Inc.
    Contributed by Andy Vaught
 
-This file is part of GNU G95.
+This file is part of GCC.
 
-GNU G95 is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 2, or (at your option) any later
+version.
 
-GNU G95 is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
 
 You should have received a copy of the GNU General Public License
-along with GNU G95; see the file COPYING.  If not, write to
-the Free Software Foundation, 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.  */
+along with GCC; see the file COPYING.  If not, write to the Free
+Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+02111-1307, USA.  */
 
 #include "config.h"
 #include "gfortran.h"
