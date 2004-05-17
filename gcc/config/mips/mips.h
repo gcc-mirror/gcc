646ca712a1d3d4b6642b8d9c3f0d24e463102b76
Richard Sandiford <rsandifo@redhat.com>
2004-05-17 06:52:43 +0000

    mips.h (MASK_DEBUG_G, [...]): Delete.
    	* config/mips/mips.h (MASK_DEBUG_G, TARGET_DEBUG_G_MODE): Delete.
    	(TARGET_SWITCHES): Remove debugg.
    	* config/mips/mips.md (adddi3, ashldi3, ashrdi3, lshrdi3): Only handle
    	TARGET_64BIT.
    	(subdi3): Replace the define_expand with a define_insn, the latter
    	renamed from subdi3_internal_3.
    	(negdi2): Likewise negdi2_internal_2.
    	(adddi3_internal_[12], subdi3_internal, ashldi3_internal{,2,3})
    	(ashrdi3_internal{,2,3}, lshrdi3_internal{,2,3}): Remove patterns
    	and associated define_splits.
    	(adddi3_internal): Renamed from adddi3_internal_3.
    	(ashldi3_internal): Likewise ashldi3_internal4.
    	(ashrdi3_internal): Likewise ashrdi3_internal4.
    	(lshrdi3_internal): Likewise lshrdi3_internal4.
    
    From-SVN: r81934

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -178,7 +178,6 @@ extern const struct mips_cpu_info *mips_tune_info;
 #define MASK_DEBUG	0		/* unused */
 #define MASK_DEBUG_C	0		/* don't expand seq, etc.  */
 #define MASK_DEBUG_D	0		/* don't do define_split's */
-#define MASK_DEBUG_G	0		/* don't support 64 bit arithmetic */
 
 					/* Dummy switches used only in specs */
 #define MASK_MIPS_TFILE	0		/* flag for mips-tfile usage */
@@ -200,7 +199,6 @@ extern const struct mips_cpu_info *mips_tune_info;
 #define TARGET_DEBUG_MODE	(target_flags & MASK_DEBUG)
 #define TARGET_DEBUG_C_MODE	(target_flags & MASK_DEBUG_C)
 #define TARGET_DEBUG_D_MODE	(target_flags & MASK_DEBUG_D)
-#define TARGET_DEBUG_G_MODE	(target_flags & MASK_DEBUG_G)
 
 					/* Reg. Naming in .s ($21 vs. $a0) */
 #define TARGET_NAME_REGS	(target_flags & MASK_NAME_REGS)
@@ -677,8 +675,6 @@ extern const struct mips_cpu_info *mips_tune_info;
   {"debugc",		  MASK_DEBUG_C,					\
      NULL},								\
   {"debugd",		  MASK_DEBUG_D,					\
-     NULL},								\
-  {"debugg",		  MASK_DEBUG_G,					\
      NULL},								\
   {"",			  (TARGET_DEFAULT				\
 			   | TARGET_CPU_DEFAULT				\
