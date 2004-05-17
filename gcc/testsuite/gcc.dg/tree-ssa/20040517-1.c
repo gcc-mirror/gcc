6944d9ddb0be2b586fb48eece8591bb62a5d5b66
Steven Bosscher <stevenb@suse.de>
2004-05-17 21:25:32 +0000

    tree-ssa-operands.c (get_expr_operands): Do not treat malloc attributed functions as pure or const.
    	* tree-ssa-operands.c (get_expr_operands): Do not treat malloc
    	attributed functions as pure or const.
    
    From-SVN: r81951

---
+++ gcc/testsuite/gcc.dg/tree-ssa/20040517-1.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-ssa-vops" } */
+int a; 
+ 
+extern void __attribute__ ((malloc)) *foo ();
+ 
+void bar (void) 
+{ 
+  a = 1; 
+  foo (); 
+  if (a) 
+    abort (); 
+} 
+
+/* We used to treat malloc functions like pure and const functions, but
+   malloc functions may clobber global memory.  Only the function result
+   does not alias any other pointer.
+   Hence, we must have a VDEF for a before and after the call to foo().  */
+/* { dg-final { scan-tree-dump-times "VDEF" 2 "ssa"} } */
+
