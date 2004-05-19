d3a7ef9aacb50cbcd85a34966da10304f048a576
Falk Hueffner <falk@debian.org>
2004-05-20 01:43:20 +0200

    re PR other/15526 (-ftrapv aborts on 0 * (-1))
    	PR other/15526
    	* libgcc2.c (__mulvsi3): Fix overflow test.
    	* gcc.dg/ftrapv-1.c: New test case.
    
    From-SVN: r82042

---
+++ gcc/testsuite/gcc.dg/ftrapv-1.c
@@ -0,0 +1,25 @@
+/* Copyright (C) 2004 Free Software Foundation.
+
+   PR other/15526
+   Verify correct overflow checking with -ftrapv.
+
+   Written by Falk Hueffner, 20th May 2004.  */
+
+/* { dg-do run } */
+/* { dg-options "-ftrapv" } */
+
+__attribute__((noinline)) int
+mulv(int a, int b)
+{
+  return a * b;
+}
+
+int
+main()
+{
+  mulv( 0,  0);
+  mulv( 0, -1);
+  mulv(-1,  0);
+  mulv(-1, -1);
+  return 0;
+}
