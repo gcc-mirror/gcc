cc52902d97894d218cf6acfd2aa1b904cb4c62e8
Andrew Pinski <pinskia@physics.uc.edu>
2004-05-19 03:35:19 +0000

    tree-ssa-phiopt.c (abs_replacement): New function.
            * tree-ssa-phiopt.c (abs_replacement): New function.
            (empty_block_p): New function extracted from...
            (candidate_bb_for_phi_optimization): Break out empty block test.
            (conditional_replacement): Use empty_block_p.
            (value_replacement): Similarly.
    
            * gcc.dg/tree-ssa/20040514-2.c: Update expected output.
            * gcc.dg/tree-ssa/20040518-2.c: New test.
    
    Co-Authored-By: Jeff Law <law@redhat.com>
    
    From-SVN: r82017

---
+++ gcc/testsuite/gcc.dg/tree-ssa/20040518-2.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-phiopt1-details" } */
+
+int t( int i)
+{
+   int j;
+   if(i>=0)
+    j = i;
+   else
+    j = -i;
+   return j;
+}
+
+/* We should convert one COND_EXPRs into straightline code with ABS.  */
+/* { dg-final { scan-tree-dump-times "straightline" 1 "phiopt1"} } */
+/* { dg-final { scan-tree-dump-times "ABS_EXPR" 1 "phiopt1"} } */
