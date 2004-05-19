0fa13661a9b9ba37df8bcdec4fa30ea91d19c878
Anthony Green <green@localhost.localdomain>
2004-05-19 21:49:04 +0000

    Makefile.am (awt_java_source_files): Remove javax.rmi and gnu.javax.rmi code.
    2004-05-19  Anthony Green  <green@redhat.com>
    
    	* Makefile.am (awt_java_source_files): Remove javax.rmi and
    	gnu.javax.rmi code.
    	* Makefile.in: Rebuilt.
    	* javax/rmi/ClassDesc.java, javax/rmi/StubDelegate.java,
    	javax/rmi/UtilDelegate.java, javax/rmi/Stub.java,
    	javax/rmi/Util.java, javax/rmi/ObjectImpl.java,
    	javax/rmi/SystemException.java, javax/rmi/ValueHandler.java,
    	javax/rmi/PortableRemoteObjectDelegate.java, javax/rmi/Tie.java,
    	gnu/javax/rmi/CORBA/DelegateFactory.java,
    	gnu/javax/rmi/CORBA/GetDelegateInstanceException.java,
    	gnu/javax/rmi/CORBA/PortableRemoteObjectDelegateImpl.java,
    	gnu/javax/rmi/CORBA/StubDelegateImpl.java,
    	gnu/javax/rmi/CORBA/UtilDelegateImpl.java,
    	gnu/javax/rmi/CORBA/ValueHandlerImpl.java,
    	gnu/javax/rmi/PortableServer.java: Remove files.
    
    From-SVN: r82036

--- libjava/javax/rmi/CORBA/ObjectImpl.java
+++
@@ -1,9 +0,0 @@
-package javax.rmi.CORBA;
-
-/** XXX - Stub till we have org.omg.CORBA */
-public class ObjectImpl
-{
-  public ObjectImpl _orb() { return null; }
-  public String object_to_string(ObjectImpl o) 
-    throws javax.rmi.BAD_OPERATION { return null; }
-}
