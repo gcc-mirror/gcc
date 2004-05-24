3d64cd6e4f157825f876dde80a53600d01d6b514
dberlin <dberlin@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-05-24 13:20:52 +0000

    2004-05-24  Daniel Berlin  <dberlin@dberlin.org>
    	PR other/15627
    	* engine/util.h: Include ansidecl.h.
    	Make max and min static inline.
    	* engine/util.c: Remove max and min from here.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@82202 138bc75d-0d04-0410-961f-82ee72b054a4

--- libbanshee/engine/util.c
+++ libbanshee/engine/util.c
@@ -196,6 +196,4 @@ int ptr_cmp(const void *ptr1, const void *ptr2)
   return (char *) ptr1 - (char *) ptr2;
 }
 
-int min(int a, int b) { if (a < b) return a; else return b; }
-int max(int a, int b) { if (a < b) return b; else return a; }
 /* int abs(int a) { if (a < 0) return -a; else return a; } */
