1bfc8f675bee787718c1f5d24a300d7b86107fea
Janis Johnson <janis187@us.ibm.com>
2004-05-24 17:57:09 +0000

    altivec-7.c: Don't use 'vector long'.
    2004-05-24  Janis Johnson  <janis187@us.ibm.com>
    
    	* gcc.dg/altivec-7.c: Don't use 'vector long'.
    	* gcc.dg/altivec-14.c: New test.
    	* g++.dg/ext/altivec-7.C: Don't use 'vector long'.
    
    From-SVN: r82213

--- gcc/testsuite/gcc.dg/altivec-7.c
+++ gcc/testsuite/gcc.dg/altivec-7.c
@@ -5,15 +5,15 @@
 
 #include <altivec.h>
 
-long **longp;
+int **intp;
 int *var_int;
-unsigned long **ulongp;
+unsigned int **uintp;
 vector pixel *varpixel;
 vector signed char *vecchar;
-vector signed long *vecint;       /* { dg-warning "use of .long. in AltiVec types is deprecated; use .int." } */
+vector signed int *vecint;
 vector signed short *vecshort;
 vector unsigned char *vecuchar;
-vector unsigned long *vecuint;    /* { dg-warning "use of .long. in AltiVec types is deprecated; use .int." } */
+vector unsigned int *vecuint;
 vector unsigned short *vecushort;
 vector float *vecfloat;
 
@@ -26,18 +26,18 @@ int main ()
   *varpixel++ = vec_packpx(vecuint[0], vecuint[1]);
   *varpixel++ = vec_vpkpx(vecuint[0], vecuint[1]);
   *vecshort++ = vec_vmulosb(vecchar[0], vecchar[1]);
-  *vecint++ = vec_ld(var_int[0], longp[1]);
-  *vecint++ = vec_lde(var_int[0], longp[1]);
-  *vecint++ = vec_ldl(var_int[0], longp[1]);
-  *vecint++ = vec_lvewx(var_int[0], longp[1]);
+  *vecint++ = vec_ld(var_int[0], intp[1]);
+  *vecint++ = vec_lde(var_int[0], intp[1]);
+  *vecint++ = vec_ldl(var_int[0], intp[1]);
+  *vecint++ = vec_lvewx(var_int[0], intp[1]);
   *vecint++ = vec_unpackh(vecshort[0]);
   *vecint++ = vec_unpackl(vecshort[0]);
   *vecushort++ = vec_andc((vector bool short)vecshort[0], vecushort[1]);
   *vecushort++ = vec_andc(vecushort[0], (vector bool short)vecshort[1]);
   *vecushort++ = vec_vxor((vector bool short)vecshort[0], vecushort[1]);
   *vecushort++ = vec_vxor(vecushort[0], (vector bool short)vecshort[1]);
-  *vecuint++ = vec_ld(var_int[0], ulongp[1]);
-  *vecuint++ = vec_lvx(var_int[0], ulongp[1]);
+  *vecuint++ = vec_ld(var_int[0], uintp[1]);
+  *vecuint++ = vec_lvx(var_int[0], uintp[1]);
   *vecuint++ = vec_vmsumubm(vecuchar[0], vecuchar[1], vecuint[2]);
   *vecuchar++ = vec_xor(vecuchar[0], (vector unsigned char)vecchar[1]);
 
