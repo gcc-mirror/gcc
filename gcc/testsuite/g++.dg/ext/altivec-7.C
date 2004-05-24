b0a309b25cdcff60f332a21043d584943c2e384c
janis <janis@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-05-24 17:57:09 +0000

    2004-05-24  Janis Johnson  <janis187@us.ibm.com>
    	* gcc.dg/altivec-7.c: Don't use 'vector long'.
    	* gcc.dg/altivec-14.c: New test.
    	* g++.dg/ext/altivec-7.C: Don't use 'vector long'.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@82213 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ext/altivec-7.C
+++ gcc/testsuite/g++.dg/ext/altivec-7.C
@@ -10,9 +10,9 @@ void foo(vector bool     char) { }
 void foo(vector unsigned short) { }
 void foo(vector signed short) { }
 void foo(vector bool short) { }
-void foo(vector unsigned long) { } /* { dg-warning "use of .long. in AltiVec types is deprecated. use .int." } */
-void foo(vector signed long) { }   /* { dg-warning "use of .long. in AltiVec types is deprecated. use .int." } */
-void foo(vector bool long) { }     /* { dg-warning "use of .long. in AltiVec types is deprecated. use .int." } */
+void foo(vector unsigned int) { }
+void foo(vector signed int) { }
+void foo(vector bool int) { }
 void foo(vector float) { }
 void foo(vector pixel) { }
 void foo(int) { }
