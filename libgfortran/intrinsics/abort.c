ebeb17c7bb8d07d412b5f02786fb79b0ab0ad2b9
Andreas Jaeger <aj@gcc.gnu.org>
2004-05-30 11:53:10 +0200

    random.c: Include unistd.h for close and read prototypes...
    	* intrinsics/random.c: Include unistd.h for close and read
    	prototypes, remove unneeded inclusion of assert.h.
    
    	* intrinsics/abort.c: Include stdlib.h for abort prototype, remove
    	unneeded inclusion of assert.h.
    
    From-SVN: r82442

--- libgfortran/intrinsics/abort.c
+++ libgfortran/intrinsics/abort.c
@@ -1,5 +1,5 @@
 /* Implementation of the ABORT intrinsic.
-   Copyright (C) 2003 Free Software Foundation, Inc.
+   Copyright (C) 2003, 2004 Free Software Foundation, Inc.
 
 This file is part of the GNU Fortran 95 runtime library (libgfortran).
 
@@ -19,7 +19,7 @@ write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
 #include "libgfortran.h"
-#include <assert.h>
+#include <stdlib.h>
 
 
 void prefix(abort) (void);
