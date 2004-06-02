cb7c16030217d150a1bf98f204ee1536e43ce12c
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-06-02 18:56:54 +0000

    2004-06-02  Andrew Pinski  <pinskia@physics.uc.edu>
            PR tree-optimization/14736
            * g++.dg/tree-ssa/ssa-cast-1.C: New Test.
    
            PR tree-optimization/14042
            * g++.dg/tree-ssa/ssa-sra-1.C: New Test.
    
            PR tree-optimization/14729
            * g++.dg/tree-ssa/ssa-sra-2.C: New Test.
    
    2004-06-02  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR tree-optimization/14042
            PR tree-optimization/14729
            PR tree-optimization/14736
            * tree-ssa.c: Check the type which the pointer points to
            instead of the pointer types.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@82573 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa.c
+++ gcc/tree-ssa.c
@@ -555,7 +555,8 @@ tree_ssa_useless_type_conversion_1 (tree outer_type, tree inner_type)
      so strip conversions that just switch between them.  */
   else if (POINTER_TYPE_P (inner_type)
            && POINTER_TYPE_P (outer_type)
-           && lang_hooks.types_compatible_p (inner_type, outer_type))
+           && lang_hooks.types_compatible_p (TREE_TYPE (inner_type),
+					     TREE_TYPE (outer_type)))
     return true;
 
   /* If both the inner and outer types are integral types, then the
