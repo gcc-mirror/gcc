f922641833ebe869c4ae2342f3bf2de995ffbf77
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-06-03 22:15:08 +0000

    2004-06-03  Andrew Pinski  <pinskia@physics.uc.edu>
            * config/darwin.c (machopic_indirect_data_reference): Copy
            the SYMBOL_REF_DECL from the original RTX for the new
            non-lazy pointer RTX.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@82606 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/darwin.c
+++ gcc/config/darwin.c
@@ -499,6 +499,8 @@ machopic_indirect_data_reference (rtx orig, rtx reg)
       ptr_ref = gen_rtx_SYMBOL_REF (Pmode,
 				    machopic_non_lazy_ptr_name (name));
 
+     SYMBOL_REF_DECL (ptr_ref) = SYMBOL_REF_DECL (orig);
+
       ptr_ref = gen_rtx_MEM (Pmode, ptr_ref);
       RTX_UNCHANGING_P (ptr_ref) = 1;
 
