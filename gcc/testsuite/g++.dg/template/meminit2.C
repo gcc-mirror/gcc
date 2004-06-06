b804bf0273a02650a4898212567d69d96ca1b8ab
No Author <no-author@gcc.gnu.org>
2004-06-06 03:22:56 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_4-branch'.
    From-SVN: r82661

---
+++ gcc/testsuite/g++.dg/template/meminit2.C
@@ -0,0 +1,21 @@
+// { dg-do compile }
+// Origin: Mark Anders <mark dot a dot anders at intel dot com>
+// PR c++/15503: disambiguators in base classes and mem-initializers
+
+template <typename K1> struct O {  
+  template <typename K2> struct I {}; 
+}; 
+
+template <typename T> 
+struct A : typename O<T>::template I<int> {   // { dg-error "keyword `typename' not allowed" }
+  A() :    typename O<T>::template I<int>()   // { dg-error "keyword `typename' not allowed" }
+  {};
+};
+
+template <typename T> 
+struct B : O<T>::template I<int> {
+  B() :    O<T>::I<int>()   // { dg-error "used as template|it is a template" "" }
+  {};
+};
+
+// { dg-bogus "end of input" "bogus token skipping in the parser" { xfail *-*-* } 17 }
