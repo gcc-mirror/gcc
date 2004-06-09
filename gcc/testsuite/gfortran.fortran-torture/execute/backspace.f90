31b6f11ab0530374184676979781ba81b06ae38f
Bud Davis <bdavis9659@comcast.net>
2004-06-09 00:55:04 +0000

    re PR libfortran/15755 (gfortran: backspace call causes bus error)
    2004-06-08  Bud Davis  <bdavis9659@comcast.net>
    
    	PR gfortran/15755
    	* gfortran.fortran-torture/execute/backspace.c : New test.
    
    	* io/backspace.c(st_backspace): call correct routine for
    	formatted and un-formatted units.
    
    From-SVN: r82806

---
+++ gcc/testsuite/gfortran.fortran-torture/execute/backspace.f90
@@ -0,0 +1,14 @@
+! pr 15755
+        implicit none
+        character*1 C
+        open(10)
+        write(10,*)'a'
+        write(10,*)'b'
+        write(10,*)'c'
+        rewind(10)
+        read(10,*)C
+        backspace(10) 
+        read(10,*) C
+        if (C.ne.'b') call abort
+        close(10,STATUS='DELETE')
+        end
