f51d8522f761fab8df285b2fa3aa8c6a931509c3
Tobias Schlüter <tobias.schlueter@physik.uni-muenchen.de>
2004-06-14 21:25:33 +0200

    Fix ChangeLog, fix parentheses from previous patch to trans-intrinsic.c
    From-SVN: r83128

--- gcc/fortran/trans-intrinsic.c
+++ gcc/fortran/trans-intrinsic.c
@@ -1874,8 +1874,9 @@ gfc_conv_intrinsic_len (gfc_se * se, gfc_expr * expr)
       break;
 
     default:
-	if (arg->expr_type == EXPR_VARIABLE && arg->ref == NULL
-	    || (arg->ref->next == NULL && arg->ref->type == REF_ARRAY))
+	if (arg->expr_type == EXPR_VARIABLE 
+	    && (arg->ref == NULL || (arg->ref->next == NULL 
+				     && arg->ref->type == REF_ARRAY)))
 	  {
 	    /* This doesn't catch all cases. 
 	       See http://gcc.gnu.org/ml/fortran/2004-06/msg00165.html
