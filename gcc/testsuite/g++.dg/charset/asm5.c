6bf3d92c78fc0736054b7dc42b5414b3caec08fe
Eric Christopher <echristo@redhat.com>
2004-06-15 23:09:24 +0000

    asm5.c: New.
    2004-06-15  Eric Christopher  <echristo@redhat.com>
    
            * g++.dg/charset/asm5.c: New.
            * gcc.dg/charset/asm6.c: New.
    
    From-SVN: r83215

---
+++ gcc/testsuite/g++.dg/charset/asm5.c
@@ -0,0 +1,13 @@
+/* Test for string translation.  */
+/* { dg-do compile }
+   { dg-require-iconv "IBM-1047" } 
+   { dg-final { scan-assembler "foo" } } */
+int main()
+{
+  unsigned long int *ptr;
+  ptr = ((unsigned long int *)
+         ( { void *stack_ptr;
+           __asm__ __volatile__ ( "foo %0" : "=r" (stack_ptr) );
+           (stack_ptr); } ) );
+  return 0;
+}
