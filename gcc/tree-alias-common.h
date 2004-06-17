d96f6b597e22d1667bdf50ec71dd2b0fef9d1a15
Ben Elliston <bje@au.ibm.com>
2004-06-17 04:49:59 +0000

    tree-alias-ander.h: Add standard top-of-file comment.
    	* tree-alias-ander.h: Add standard top-of-file comment.
    	* tree-alias-common.h: Likewise.
    	* tree-alias-type.h: Likewise.
    
    From-SVN: r83285

--- gcc/tree-alias-common.h
+++ gcc/tree-alias-common.h
@@ -1,6 +1,29 @@
+/* Tree based points-to analysis
+   Copyright (C) 2002, 2003 Free Software Foundation, Inc.
+   Contributed by Daniel Berlin <dberlin@dberlin.org>
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #ifndef TREE_ALIAS_COMMON
 #define TREE_ALIAS_COMMON
+
 #include "tree-alias-type.h"
+
 /* Alias analysis function pointers.
    Functions implemented by the actual alias analysis algorithms in
    order for them to work with the common points-to structure.  */
@@ -96,4 +119,5 @@ extern bool ptr_may_alias_var (tree, tree);
 extern bool same_points_to_set (tree, tree);
 extern bool empty_points_to_set (tree);
 extern const char *alias_get_name (tree);
-#endif
+
+#endif /* TREE_ALIAS_COMMON */
