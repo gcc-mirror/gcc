fe15db91783d3885fec8ecd39f212848800648e0
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-06-17 04:49:59 +0000

    * tree-alias-ander.h: Add standard top-of-file comment. 	* tree-alias-common.h: Likewise. 	* tree-alias-type.h: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@83285 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-alias-ander.h
+++ gcc/tree-alias-ander.h
@@ -1,7 +1,29 @@
+/* Tree based Andersen points-to analysis
+   Copyright (C) 2002, 2003 Free Software Foundation, Inc.
+   Contributed by Daniel Berlin <dberlin@dberlin.org>
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #ifndef TREE_ALIAS_ANDER
 #define TREE_ALIAS_ANDER
+
 #include "tree-alias-common.h"
 
-
 extern struct tree_alias_ops *andersen_alias_ops;
-#endif
+
+#endif /* TREE_ALIAS_ANDER */
--- gcc/tree-alias-type.h
+++ gcc/tree-alias-type.h
@@ -1,3 +1,24 @@
+/* Tree based linear points-to analysis
+   Copyright (C) 2002, 2003 Free Software Foundation, Inc.
+   Contributed by Daniel Berlin <dberlin@dberlin.org>
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #ifndef TREE_ALIAS_TYPE_H
 #define TREE_ALIAS_TYPE_H
 
@@ -6,27 +27,32 @@
 union alias_var_def;
 struct aterm_;
 struct aterm_list_a;
+
 enum alias_var_kind
 {
   ATERM_AVAR
 };
+
 struct alias_var_common  GTY (())
 {
   enum alias_var_kind kind;
   unsigned int varnum;
   tree decl;
 };
+
 struct alias_var_aterm GTY (())
 {
   struct alias_var_common common;
   struct aterm_ * GTY((skip (""))) term;
   struct aterm_list_a *GTY ((skip (""))) ptset;
 };
+
 union alias_var_def GTY ((desc ("%0.common.kind")))
 {
   struct alias_var_common GTY ((tag ("-1"))) common;
   struct alias_var_aterm GTY ((tag ("ATERM_AVAR"))) aterm;
 };
+
 typedef union alias_var_def *alias_var;
 
 #define ALIAS_VAR_KIND(x) ((x)->common.kind)
@@ -38,4 +64,5 @@ union alias_type_def;
 typedef union alias_type_def *alias_type;
 
 alias_var alias_var_new_with_aterm (tree, struct aterm_ *);
-#endif
+
+#endif /* TREE_ALIAS_TYPE_H */
