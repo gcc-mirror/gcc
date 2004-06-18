cbd8cc2500741da3f8a6988169ad173d7f251f01
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-06-18 17:13:35 +0000

    2004-06-18  Paolo Carlini  <pcarlini@suse.de>
    	PR libstdc++/16020
    	* include/debug/safe_base.h
    	(_Safe_sequence_base::_Safe_sequence_base(const _Safe_sequence_base&),
    	_Safe_sequence_base::operator=): Provide definitions.
    	* testsuite/23_containers/bitset/cons/16020.cc: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@83357 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/23_containers/bitset/cons/16020.cc
@@ -0,0 +1,42 @@
+// Copyright (C) 2004 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+#include <debug/bitset>
+#include <testsuite_hooks.h>
+
+// libstdc++/16020
+void test01()
+{
+  using __gnu_debug::bitset;
+  bool test __attribute__((unused)) = true;
+
+  bitset<5> b(7);
+  bitset<5> c;
+
+  bitset<5> bb(b);
+  c = bb;
+
+  VERIFY( bb == b );
+  VERIFY( c == bb );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
