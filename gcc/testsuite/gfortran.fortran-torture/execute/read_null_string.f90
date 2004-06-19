04b0faec0878e8b47e732369bad771e8297a6baa
Bud Davis <bdavis9659@comcast.net>
2004-06-19 16:42:05 +0000

    re PR libfortran/16080 (segmentation fault when reading empty string)
    2004-06-19  Bud Davis  <bdavis9659@comcast.net>
    
          PR gfortran/16080
          * gfortran.fortran-torture/execute/read_null_string.f90: New file.
          * io/list_read.c(set_value): don't copy if the string is null.
    
    From-SVN: r83388

---
+++ gcc/testsuite/gfortran.fortran-torture/execute/read_null_string.f90
@@ -0,0 +1,15 @@
+! pr 16080, segfault on reading an empty string 
+      implicit none 
+      integer t 
+      character*20   temp_name 
+      character*2 quotes
+      open(unit=7,status='SCRATCH')
+      quotes = '""""'  ! "" in the file
+      write(7,*)1
+      write(7,'(A)')quotes
+      temp_name = 'hello'  ! make sure the read overwrites it
+      rewind(7)
+      read(7, *) t 
+      read(7, *) temp_name 
+      if (temp_name.ne.'') call abort
+      end
