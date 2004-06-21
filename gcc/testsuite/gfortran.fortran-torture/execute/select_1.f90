63110e4e74f1c2bec7469c131b097b4ab5bedf25
Tobias Schlüter <tobias.schlueter@physik.uni-muenchen.de>
2004-06-22 00:56:34 +0200

    select_1.f90: Rename function to fix type error.
    * gfortran.fortran-torture/execute/select_1.f90: Rename function
    to fix type error.
    * gfortran.fortran-torture/execute/intrinsic_associated.f90:
    Remove trailing spaces.
    
    From-SVN: r83459

--- gcc/testsuite/gfortran.fortran-torture/execute/select_1.f90
+++ gcc/testsuite/gfortran.fortran-torture/execute/select_1.f90
@@ -1,17 +1,17 @@
 ! from PR 15962, we used to require constant expressions instead of
 ! initialization expressions in case-statements
-function x(k)
+function j(k)
 integer :: k
-integer :: x
+integer :: j
 integer, parameter :: i(2) = (/1,2/)
 
 select case(k)
 case (1:size(i))
-    x = i(k)
+    j = i(k)
 case default
-    x = 0
+    j = 0
 end select
 end function
 
-if (x(2).NE.2 .OR. x(11).NE.0) call abort()
+if (j(2).NE.2 .OR. j(11).NE.0) call abort()
 end
