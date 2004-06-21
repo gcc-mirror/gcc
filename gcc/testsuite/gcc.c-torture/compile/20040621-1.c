628c697a30a509261ca5812aa378d0dde44c88c2
dberlin <dberlin@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-06-21 21:59:38 +0000

    New test, extracted from 15982
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@83454 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/20040621-1.c
@@ -0,0 +1,17 @@
+/* This test woudl cause partial redundancies too complex for PRE
+   to insert using a single temporary due to them not being GIMPLE
+   expressions.  */
+int ssbgst (int c, int k)
+{
+  int a, i, j;
+
+  a = 0;
+  i = 1;
+  j = k;
+  while (j)
+    {
+      a += (j + i) * (k + i + c) + (j + i + c);
+      j = j - 1;
+    }
+  return a;
+}
