cae80939b64ac547c253fbb53f4c91c67d9e8ad5
John David Anglin <dave.anglin@nrc-cnrc.gc.ca>
2004-06-21 23:49:04 +0000

    re PR rtl-optimization/14782 (produces an unaligned data access at -O2)
    	PR rtl-optimization/14782
    	* pa.c (emit_move_sequence): Use SFmode for 4-byte modes when doing
    	the address checks for secondary reloads for loads from and stores
    	to floating-point registers.
    	* pa.h (EXTRA_CONSTRAINT, case T): Use SFmode for 4-byte modes
    	in the address check.  Move work around for ELF32 targets to
    	GO_IF_LEGITIMATE_ADDRESS.
    	(GO_IF_LEGITIMATE_ADDRESS): Require constant offsets to be
    	correctly aligned for DImode loads and stores.  Don't allow long
    	SFmode displacements on ELF32.
    
    From-SVN: r83464

--- gcc/config/pa/pa.c
+++ gcc/config/pa/pa.c
@@ -1562,15 +1562,18 @@ emit_move_sequence (rtx *operands, enum machine_mode mode, rtx scratch_reg)
     operand1 = gen_rtx_MEM (GET_MODE (operand1), tem);
 
   /* Handle secondary reloads for loads/stores of FP registers from
-     REG+D addresses where D does not fit in 5 bits, including
+     REG+D addresses where D does not fit in 5 or 14 bits, including
      (subreg (mem (addr))) cases.  */
   if (scratch_reg
       && fp_reg_operand (operand0, mode)
       && ((GET_CODE (operand1) == MEM
-	   && !memory_address_p (DFmode, XEXP (operand1, 0)))
+	   && !memory_address_p ((GET_MODE_SIZE (mode) == 4 ? SFmode : DFmode),
+				 XEXP (operand1, 0)))
 	  || ((GET_CODE (operand1) == SUBREG
 	       && GET_CODE (XEXP (operand1, 0)) == MEM
-	       && !memory_address_p (DFmode, XEXP (XEXP (operand1, 0), 0))))))
+	       && !memory_address_p ((GET_MODE_SIZE (mode) == 4
+				      ? SFmode : DFmode),
+				     XEXP (XEXP (operand1, 0), 0))))))
     {
       if (GET_CODE (operand1) == SUBREG)
 	operand1 = XEXP (operand1, 0);
@@ -1600,10 +1603,13 @@ emit_move_sequence (rtx *operands, enum machine_mode mode, rtx scratch_reg)
   else if (scratch_reg
 	   && fp_reg_operand (operand1, mode)
 	   && ((GET_CODE (operand0) == MEM
-		&& ! memory_address_p (DFmode, XEXP (operand0, 0)))
+		&& !memory_address_p ((GET_MODE_SIZE (mode) == 4
+					? SFmode : DFmode),
+				       XEXP (operand0, 0)))
 	       || ((GET_CODE (operand0) == SUBREG)
 		   && GET_CODE (XEXP (operand0, 0)) == MEM
-		   && !memory_address_p (DFmode,
+		   && !memory_address_p ((GET_MODE_SIZE (mode) == 4
+					  ? SFmode : DFmode),
 			   		 XEXP (XEXP (operand0, 0), 0)))))
     {
       if (GET_CODE (operand0) == SUBREG)
