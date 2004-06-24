0d656e8b32f0d480d9f1a1112c09cd2ccb4a48d8
uweigand <uweigand@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-06-24 00:49:07 +0000

    * config/s390/s390-protos.h (s390_emit_compare): Add prototype. 	(s390_emit_jump): Likewise. 	* config/s390/s390.c (s390_emit_compare): New function. 	(s390_emit_jump): Likewise. 	* config/s390/s390.md ("beq", "bne", "bgt", "bgtu", "blt", "bltu", 	"bge", "bgeu", "ble", "bleu", "bunordered", "bordered", "buneq", 	"bungt", "bunlt", "bunge", "bunle", "bltgt"): Use s390_emit_compare 	and s390_emit_jump. 	("cjump"): Remove, replace by ... 	("*cjump_64", "*cjump_31"): ... these insns.  Improve length default. 	("icjump", "*icjump_64", "*icjump_31"): Likewise. 	("trap"): Fix type attribute. 	("conditional_trap"): Use s390_emit_compare. 	("doloop_si"): Remove, replace by ... 	("doloop_si64", "doloop_si31"): ... these new insn_and_split. 	Merge existing splitter into insn_and_split.  Improve length default. 	("doloop_di"): Merge with existing splitter into insn_and_split. 	("doloop"): Adapt. 	("jump"): Convert to expander.  Use s390_emit_jump. 	("*jump_64", "*jump_31"): New insns.  Improve length default.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@83574 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/s390/s390-protos.h
+++ gcc/config/s390/s390-protos.h
@@ -52,6 +52,8 @@ extern enum machine_mode s390_tm_ccmode (rtx, rtx, int);
 extern enum machine_mode s390_select_ccmode (enum rtx_code, rtx, rtx);
 extern int s390_alc_comparison (rtx op, enum machine_mode mode);
 extern int s390_slb_comparison (rtx op, enum machine_mode mode);
+extern rtx s390_emit_compare (enum rtx_code, rtx, rtx);
+extern void s390_emit_jump (rtx, rtx);
 extern int symbolic_reference_mentioned_p (rtx);
 extern int tls_symbolic_reference_mentioned_p (rtx);
 extern int legitimate_la_operand_p (rtx);
