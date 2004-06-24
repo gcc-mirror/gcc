88e623662a61f4a7423e5c07961c11681f2803d1
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-06-24 06:51:56 +0000

    2004-06-23  Andrew Pinski  <apinski@apple.com>
            PR middle-end/15988
            * fold-const.c (fold_convert): Types which are compatible
            can be converted with only a NOP_EXPR.
    
    2004-06-23  Andrew Pinski  <apinski@apple.com>
    
            PR middle-end/15988
            * g++.dg/opt/ptrmem4.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@83585 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/opt/ptrmem4.C
@@ -0,0 +1,12 @@
+// { dg-do run }
+// { dg-options "-O3" }
+
+struct X { void foo (); }; 
+ 
+template <typename> 
+inline void spawn (void (X::*fun_ptr)()) {} 
+ 
+void bar () { 
+  void (X::*const comp)() = &X::foo; 
+  spawn<int> (comp); 
+} 
