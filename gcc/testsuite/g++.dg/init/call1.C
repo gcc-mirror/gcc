3db4a9b428b760809fdff522272b95c378949e9b
Jason Merrill <jason@redhat.com>
2004-06-24 16:07:23 -0400

    re PR tree-optimization/16115 (double-destruction problem with argument passing via temporary (breaks auto_ptr))
            PR c++/16115
            * decl.c (grokparms): Give the PARM_DECL reference type if the
            parameter is passed by invisible reference.
    
    From-SVN: r83609

---
+++ gcc/testsuite/g++.dg/init/call1.C
@@ -0,0 +1,35 @@
+// Bug c++/16115
+// { dg-do run }
+
+extern "C" void abort(); 
+ 
+int count = 0; 
+ 
+struct T { 
+    T() { count++; } 
+    T(const T&) { count++; } 
+    ~T() { if (count==0) abort(); --count; } 
+}; 
+ 
+struct auto_ptr { 
+    T* p; 
+ 
+    auto_ptr(T* __p) : p(__p) { } 
+    ~auto_ptr() { delete p; } 
+ 
+    T* release() { 
+      T* t = p; 
+      p = 0; 
+      return t; 
+    } 
+}; 
+ 
+void destroy (auto_ptr a) { 
+  delete a.release(); 
+} 
+ 
+ 
+int main () 
+{ 
+  destroy (new T); 
+}
