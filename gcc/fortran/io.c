50d78f96d060bfbcdc39633b28df1143dd7150d2
Tobias Schlüter <tobias.schlueter@physik.uni-muenchen.de>
2004-06-29 20:55:11 +0200

    * io.c: Add 2004 to copyright years.
    From-SVN: r83870

--- gcc/fortran/io.c
+++ gcc/fortran/io.c
@@ -1,5 +1,6 @@
 /* Deal with I/O statements & related stuff.
-   Copyright (C) 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
+   Copyright (C) 2000, 2001, 2002, 2003, 2004 Free Software Foundation,
+   Inc.
    Contributed by Andy Vaught
 
 This file is part of GCC.
