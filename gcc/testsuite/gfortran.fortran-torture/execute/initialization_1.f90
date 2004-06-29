8e77051c24dbe0ae95b364a934b837f9e2cf3218
tobi <tobi@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-06-29 17:01:35 +0000

    fortran/ PR fortran/15963 * expr.c (check_intrinsic_op): Allow comparison of characters. Make logic easier.
    testsuite/
    PR fortran/15963
    * gfortran.fortran-torture/execute/initialization_1.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@83859 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.fortran-torture/execute/initialization_1.f90
@@ -0,0 +1,10 @@
+! PR 15963 -- checks character comparison in initialization expressions
+character(8), parameter :: a(5) = (/ "H", "E", "L", "L", "O" /)
+call x(a)
+contains
+subroutine x(a)
+character(8), intent(in) :: a(:)
+integer :: b(count(a < 'F'))
+if (size(b) /= 1) call abort()
+end subroutine x
+end
