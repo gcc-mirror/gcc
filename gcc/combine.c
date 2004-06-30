533b9c83063396e96dcca4d31926d41716fce3e1
zack <zack@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-06-30 03:28:31 +0000

    * combine.c (distribute_notes): Don't look at global_regs for 	pseudos.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@83889 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/combine.c
+++ gcc/combine.c
@@ -11839,8 +11839,9 @@ distribute_notes (rtx notes, rtx from_insn, rtx i3, rtx i2)
 		     TEM is doing.  If so, delete TEM.  Otherwise, make this
 		     into a REG_UNUSED note instead. Don't delete sets to
 		     global register vars.  */
-		  if (reg_set_p (XEXP (note, 0), PATTERN (tem))
-		      && !global_regs [REGNO(XEXP (note, 0))])
+		  if ((REGNO (XEXP (note, 0)) >= FIRST_PSEUDO_REGISTER
+		       || !global_regs[REGNO (XEXP (note, 0))])
+		      && reg_set_p (XEXP (note, 0), PATTERN (tem)))
 		    {
 		      rtx set = single_set (tem);
 		      rtx inner_dest = 0;
