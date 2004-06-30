11012cd6977d7a42778d474b494c1431b49a3945
Nitin Yewale <nitiny@kpitcummins.com>
2004-06-30 16:36:47 +0000

    * gcc.dg/h8300-ice2.c : New test
    From-SVN: r83912

---
+++ gcc/testsuite/gcc.dg/h8300-ice2.c
@@ -0,0 +1,12 @@
+/*{dg-do compile target {h8300-*-*}} */
+/* ICE for signed/unsigned long and signed char comparison */
+int main()
+{
+       unsigned long ul = 4;
+       long sl = 2;
+       signed char sch = -1;
+       if (ul <= sch);
+               return 0;
+       if (sl <= sch)
+               return 1;
+}
