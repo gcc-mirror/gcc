a5f882db520f9b526fb47833ca5f3e0c3e6431cb
No Author <no-author@gcc.gnu.org>
2004-07-07 06:32:33 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_4-branch'.
    From-SVN: r84193

---
+++ gcc/testsuite/gcc.c-torture/execute/20040706-1.c
@@ -0,0 +1,9 @@
+int main ()
+{
+  int i;
+  for (i = 0; i < 10; i++)
+    continue;
+  if (i < 10)
+    abort ();
+  exit (0);
+}
---
+++ gcc/testsuite/gcc.dg/20040625-1.c
@@ -0,0 +1,37 @@
+/* { dg-do run } */
+/* { dg-options "-O2" } */
+/* { dg-options "-mminimal-toc" { target powerpc64-*-* powerpc-ibm-aix* } } */
+
+extern void abort (void);
+extern void exit (int);
+
+double __attribute__((noinline))
+foo (void)
+{
+  return 16441577472.0;
+}
+
+double __attribute__((noinline))
+bar (double x)
+{
+  return x;
+}
+
+int __attribute__((noinline))
+test (void)
+{
+  double x = foo ();
+  x = bar (x);
+  x /= 1024 * 1024 * 1024;
+  x *= 70;
+  x = x < 70 ? x : 70;
+  x += 30;
+  return x;
+}
+
+int main (void)
+{
+  if (test () != 100)
+    abort ();
+  exit (0);
+}
