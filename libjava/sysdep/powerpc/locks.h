45d7a972fd9395b7bb23674569d350c2344a47a5
andreast <andreast@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-07-07 18:24:00 +0000

    2004-07-07  Andreas Tobler  <a.tobler@schweiz.ch>
    	* configure.host: Enable hash synchronization on Darwin.
    	* sysdep/powerpc/locks.h (compare_and_swap): Use '\n' instead of
    	';', since this is a comment on Darwin.
    	(compare_and_swap_release): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@84214 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/sysdep/powerpc/locks.h
+++ libjava/sysdep/powerpc/locks.h
@@ -33,12 +33,12 @@ compare_and_swap (volatile obj_addr_t *addr, obj_addr_t old,
   int ret;
 
   __asm__ __volatile__ (
-	   "0:    " _LARX "%0,0,%1 ;"
-	   "      xor. %0,%3,%0;"
-	   "      bne 1f;"
-	   "      " _STCX "%2,0,%1;"
-	   "      bne- 0b;"
-	   "1:    "
+	   "0:    " _LARX "%0,0,%1 \n"
+	   "      xor. %0,%3,%0\n"
+	   "      bne 1f\n"
+	   "      " _STCX "%2,0,%1\n"
+	   "      bne- 0b\n"
+	   "1:   \n"
 	: "=&r" (ret)
 	: "r" (addr), "r" (new_val), "r" (old)
 	: "cr0", "memory");
@@ -67,12 +67,12 @@ compare_and_swap_release (volatile obj_addr_t *addr, obj_addr_t old,
   __asm__ __volatile__ ("sync" : : : "memory");
 
   __asm__ __volatile__ (
-	   "0:    " _LARX "%0,0,%1 ;"
-	   "      xor. %0,%3,%0;"
-	   "      bne 1f;"
-	   "      " _STCX "%2,0,%1;"
-	   "      bne- 0b;"
-	   "1:    "
+	   "0:    " _LARX "%0,0,%1 \n"
+	   "      xor. %0,%3,%0\n"
+	   "      bne 1f\n"
+	   "      " _STCX "%2,0,%1\n"
+	   "      bne- 0b\n"
+	   "1:   \n"
 	: "=&r" (ret)
 	: "r" (addr), "r" (new_val), "r" (old)
 	: "cr0", "memory");
