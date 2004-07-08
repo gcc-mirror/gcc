4592df6bea44cadd44224dc4fa159c3bd47f859f
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-07-08 22:17:48 +0000

    * config/cris/cris-protos.h (cris_expand_builtin_va_arg): Remove.         * config/cris/cris.c (TARGET_GIMPLIFY_VA_ARG_EXPR): New.         (cris_gimplify_va_arg_expr): Rewrite from cris_expand_builtin_va_arg.         * config/cris/cris.h (EXPAND_BUILTIN_VA_ARG): Remove.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@84321 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/cris/cris-protos.h
+++ gcc/config/cris/cris-protos.h
@@ -45,10 +45,6 @@ extern int cris_symbol (rtx);
 extern void cris_output_addr_const (FILE *, rtx);
 extern int cris_cfun_uses_pic_table (void);
 extern void cris_target_asm_named_section (const char *, unsigned int);
-
-# ifdef TREE_CODE
-extern rtx cris_expand_builtin_va_arg (tree, tree);
-# endif
 #endif /* RTX_CODE */
 
 extern void cris_pragma_expand_mul (struct cpp_reader *);
