40e6dfb15a8420967736db2c7410e78687d84543
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-07-08 04:58:11 +0000

    2004-07-07  H.J. Lu  <hongjiu.lu@intel.com>
    	PR c++/16276
    	* g++.dg/rtti/tinfo1.C: New file.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@84261 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/rtti/tinfo1.C
@@ -0,0 +1,40 @@
+// Test if a local definition is in a linkonce/comdat section.
+// { dg-do compile }
+// { dg-final { scan-assembler "_ZTIP9CTemplateIhE:" } }
+// { dg-final { scan-assembler-not ".globl\[ 	\]+_ZTIP9CTemplateIhE" } }
+// { dg-final { scan-assembler-not ".section\[^\n\r\]*_ZTIP9CTemplateIhE\[^\n\r\]*" } }
+
+
+namespace std
+{
+  class type_info
+  {
+  protected:
+    const char *__name;
+
+  protected:
+    explicit type_info(const char *__n): __name(__n) { }
+
+  public:
+    const char* name() const
+    { return __name; }
+  };
+}
+
+template<class TYPE>
+class CTemplate
+{
+};
+
+class CSecondModule {
+public:
+  CSecondModule();
+
+private:
+  const CTemplate<unsigned char> *m_variable; typedef CTemplate<unsigned char> m_variable_type;
+};
+
+CSecondModule::CSecondModule()
+{
+  typeid(const_cast<m_variable_type *>(m_variable)).name() != 0;
+}
