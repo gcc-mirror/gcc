7bb09f12fd3469f340a18730b12376abe3a70267
billingd <billingd@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-07-08 23:43:13 +0000

    2004-07-09  David Billinghurst (David.Billinghurst@riotinto.com)
    	* gfortran.dg/g77/f77-edit-i-in.f: Copy from g77.dg and
    	change X to 1X in format.
    	* gfortran.dg/g77/20010216-1.f:  Copy from g77.dg and
    	add data statements to conform to standard.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@84328 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/g77/f77-edit-i-in.f
@@ -0,0 +1,22 @@
+C Test Fortran 77 I edit descriptor for input
+C      (ANSI X3.9-1978 Section 13.5.9.1)
+C
+C Origin: David Billinghurst <David.Billinghurst@riotinto.com>
+C
+C { dg-do run }
+
+      integer i,j
+      character*10 buf
+
+      write(buf,'(A)') '1  -1'
+
+      read(buf,'(I1)') i
+      if ( i.ne.1 ) call abort()
+
+      read(buf,'(1X,I1)') i
+      if ( i.ne.0 ) call abort()
+
+      read(buf,'(1X,I1,1X,I2)') i,j
+      if ( i.ne.0 .and. j.ne.-1 ) call abort()
+
+      end
