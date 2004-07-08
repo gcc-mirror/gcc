5f0e85625f8ad6e9a5c2feb48190b9ed8fb34ef8
pbrook <pbrook@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-07-08 19:58:56 +0000

    PR fortran/16291 	* libgfortran/io/write.c: (write_float): Added length check. 	Remove pointless memset calls.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@84308 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgfortran/io/write.c
+++ libgfortran/io/write.c
@@ -535,10 +535,10 @@ write_float (fnode *f, const char *source, int len)
                memcpy(p + nb - 8, "Infinity", 8); 
             else
                memcpy(p + nb - 3, "Inf", 3);
-            if (nb < 8)
-               memset(p + nb - 4, fin, 1);
+            if (nb < 8 && nb > 3)
+               p[nb - 4] = fin;
             else if (nb > 8)
-               memset(p + nb - 9, fin, 1); 
+               p[nb - 9] = fin; 
           }
          else
              memcpy(p + nb - 3, "NaN", 3);
