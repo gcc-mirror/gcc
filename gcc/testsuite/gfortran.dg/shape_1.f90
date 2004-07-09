32e89bebc67451505f07ba857cf96157f618c7e9
Tobias Schlüter <tobias.schlueter@physik.uni-muenchen.de>
2004-07-09 23:32:57 +0200

    re PR fortran/13201 (PARAMETER variables of nonconstant shape are accepted)
    PR fortran/13201
    * gfortran.dg/shape_1.f90: New test.
    
    From-SVN: r84403

---
+++ gcc/testsuite/gfortran.dg/shape_1.f90
@@ -0,0 +1,6 @@
+! { dg-do compile }
+! PR 13201 we used to not give an error in those cases
+subroutine foo(n) 
+  integer, parameter :: a(n) = 1 ! { dg-error "cannot be automatic" "automatic shape" }
+  integer, parameter :: z(:) = (/ 1,2,3 /) ! { dg-error "cannot be automatic" "assumed shape" }
+end subroutine
