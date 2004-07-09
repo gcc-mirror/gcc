4ba30a0a943a89103edfb15065d456d2463c0608
tobi <tobi@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-07-09 22:44:22 +0000

    PR fortran/14077 * gfortran.fortran-torture/execute/module_init_1.f90: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@84412 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.fortran-torture/execute/module_init_1.f90
@@ -0,0 +1,9 @@
+! PR 13077: we used to fail when reading the module
+module m1
+real, dimension(3) :: a
+data a(1:3) /2*1.0/
+end module m1
+use m1
+if (a(1).NE.1.) call abort()
+if (a(1).NE.a(3)) call abort()
+end
