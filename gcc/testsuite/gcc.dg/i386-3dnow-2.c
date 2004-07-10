0c2a4754bc69f5642d1db07ecde1fa610404b745
Jan Beulich <jbeulich@novell.com>
2004-07-10 00:27:59 +0000

    mm3dnow.h: New.
            * config/i386/mm3dnow.h: New.
            * config.gcc: Add mm3dnow.h to extra_headers for i?86 and x86-64.
    
    From-SVN: r84428

---
+++ gcc/testsuite/gcc.dg/i386-3dnow-2.c
@@ -0,0 +1,12 @@
+/* { dg-do assemble { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O0 -Werror-implicit-function-declaration -m3dnow" } */
+
+/* Test that the intrinsics compile without optimization.  All of them are
+   defined as inline functions in mmintrin.h that reference the proper
+   builtin functions.  Defining away "static" and "__inline" results in
+   all of them being compiled as proper functions.  */
+
+#define static
+#define __inline
+
+#include <mm3dnow.h>
