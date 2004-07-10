3d302c2cd2ab4de08b59745590b902376205d983
tobi <tobi@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-07-10 09:53:53 +0000

    * gfortran.fortran-torture/execute/module_init_1.f90: Fix test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@84451 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.fortran-torture/execute/module_init_1.f90
+++ gcc/testsuite/gfortran.fortran-torture/execute/module_init_1.f90
@@ -1,7 +1,7 @@
 ! PR 13077: we used to fail when reading the module
 module m1
-real, dimension(3) :: a
-data a(1:3) /2*1.0/
+real, dimension(4) :: a
+data a(1:3:2) /2*1.0/
 end module m1
 use m1
 if (a(1).NE.1.) call abort()
