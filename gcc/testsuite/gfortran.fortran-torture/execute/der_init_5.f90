50389094cc83594333d124581d737d35c8527c98
Tobias Schlüter <tobias.schlueter@physik.uni-muenchen.de>
2004-07-10 19:54:35 +0200

    re PR fortran/15969 (ICE and assertion failure in trans-expr.c)
    PR fortran/15969
    * gfortran.fortran-torture/execute/der_init_5.f90: New test.
    
    From-SVN: r84466

---
+++ gcc/testsuite/gfortran.fortran-torture/execute/der_init_5.f90
@@ -0,0 +1,16 @@
+! Check that null initialization of pointer components works.
+! PR 15969 prompted these
+! the commented out tests are cases where we still fail
+program der_init_5
+  type t
+     type(t), pointer :: a => NULL()
+     real, pointer :: b => NULL()
+!     character, pointer :: c => NULL()
+!     integer, pointer, dimension(:) :: d => NULL()
+  end type t
+  type (t) :: p
+  if (associated(p%a)) call abort()
+  if (associated(p%b)) call abort()
+!  if (associated(p%c)) call abort()
+!  if (associated(p%d)) call abort()
+end
