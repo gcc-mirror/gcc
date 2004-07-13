dcc00e867421f8b0f967ca6b6b99e7f24e116466
billingd <billingd@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-07-13 13:27:52 +0000

    2004-07-13  David Billinghurst (David.Billinghurst@riotinto.com)
    	* gfortran.dg/g77/README: New file
    	* gfortran.dg/g77/19981216-0.f:  Copy from
    	g77.f-torture/noncompile.  Add "dg-do compile" and
    	dg-error as required.
    	* gfortran.dg/g77/19990218-1.f: Likewise
    	* gfortran.dg/g77/19990905-1.f: Likewise
    	* gfortran.dg/g77/9263.f: Likewise
    	* gfortran.dg/g77/980615-0.f: Likewise
    	* gfortran.dg/g77/980616-0.f: Likewise
    	* gfortran.dg/g77/check0.f: Likewise
    	* gfortran.dg/g77/select_no_compile.f: Likewise
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@84622 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/g77/19990905-1.f
@@ -0,0 +1,20 @@
+c { dg-do compile }
+c
+c  g77 gave error
+c  19990905-1.f: In subroutine `x':
+c  19990905-1.f:15: 
+c           common /foo/n
+c                   1
+c  19990905-1.f:18: (continued):
+c           call foo(a(1))
+c                2
+c  Invalid declaration of or reference to symbol `foo' at (2) [initially seen at (1)]
+* =foo7.f in Burley's g77 test suite.
+      subroutine x
+      real a(n)
+      common /foo/n
+      continue
+      entry y(a)
+      call foo(a(1))
+      end
+c { dg-excess-errors "" }  PR 16520
---
+++ gcc/testsuite/gfortran.dg/g77/check0.f
@@ -0,0 +1,12 @@
+c { dg-do compile }
+CCC Abort fixed by:
+CCC1998-04-21  Jim Wilson  <wilson@cygnus.com>
+CCC
+CCC        * stmt.c (check_seenlabel): When search for line number note for
+CCC        warning, handle case where there is no such note.
+      logical l(10)
+      integer i(10)
+      goto (10,20),l ! { dg-error "Selection expression in computed GOTO" "" }
+      goto (10,20),i ! { dg-error "Selection expression in computed GOTO" "" }
+ 10   stop
+ 20   end
