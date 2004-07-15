d529383ecd8208de3bc5df588a8615931312780b
wilson <wilson@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-07-15 00:35:28 +0000

    Fix MIPS SPEC95 FP 146.wave5 -fprofile-generate failure. PR target/16325 * config/mips/mips.h (STARTING_FRAME_OFFSET): When flag_profile_value and ! TARGET_64BIT, include REG_PARM_STACK_SPACE. * gcc.dg/profile-generate-1.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@84727 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/profile-generate-1.c
@@ -0,0 +1,33 @@
+/* Bug 16325.  */
+/* { dg-options "-O -fprofile-generate" } */
+
+int *p1;
+int *p2;
+int *p3;
+
+int ga = 100;
+
+int
+sub (int i, int j)
+{
+  int k;
+  int l;
+  int m;
+  int n;
+  p1 = &k;
+  p2 = &l;
+  p3 = &m;
+  k = 20;
+  l = 30;
+  m = 40;
+  n = i / j;
+  return n + ga;
+}
+
+int
+main(void)
+{
+  if (sub (99, 33) != 103)
+    abort ();
+  return 0;
+}
