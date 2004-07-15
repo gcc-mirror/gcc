610bff8401024f41616666692cba8b0909f97fe5
Tobias Schlüter <tobias.schlueter@physik.uni-muenchen.de>
2004-07-15 16:59:15 +0200

    re PR fortran/15324 (segfault / ICE with array of assumed length characters)
    PR fortran/15324
    * gfortran.dg/pr15324.f90: New test.
    
    From-SVN: r84754

---
+++ gcc/testsuite/gfortran.dg/pr15324.f90
@@ -0,0 +1,22 @@
+! { dg-do run }
+! PR 15234
+! tests for passing arrays of assumed length characters
+program strarray_6
+character(5), dimension(:), allocatable :: c
+n = 3
+allocate(c(-1:n-1))
+c = "BLUBB"
+call foo(c)
+call bar(c,n)
+deallocate(c)
+contains
+subroutine foo(x)
+  character (len = *), dimension(:) :: x
+  if (any (x .ne. "BLUBB")) CALL abort()
+end subroutine foo
+end
+
+subroutine bar(x,n)
+  character (len = *), dimension(n) :: x
+  if (any (x .ne. "BLUBB")) CALL abort()
+end subroutine bar
