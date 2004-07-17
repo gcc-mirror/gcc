655109c8a29eb7ea5c4254f01a6367dc07d921b0
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-07-17 09:20:51 +0000

    * c-typeck.c (parser_build_binary_op): Condition warnings for 	X<=Y<=Z on -Wparentheses instead of -Wextra. 	* doc/invoke.texi: Update.  Document that most of -Wparentheses is 	supported for C only.
    testsuite:
    	* gcc.dg/Wparentheses-2.c, gcc.dg/Wparentheses-3.c,
    	gcc.dg/Wparentheses-4.c, Wparentheses-5.c, Wparentheses-6.c,
    	Wparentheses-7.c, Wparentheses-8.c, Wparentheses-9.c: New tests.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@84860 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/Wparentheses-2.c
@@ -0,0 +1,67 @@
+/* Test operation of -Wparentheses.  Warnings for X<=Y<=Z should be
+   there rather than hidden in -Wextra.  */
+/* Origin: Joseph Myers <jsm@polyomino.org.uk> */
+
+/* { dg-do compile } */
+/* { dg-options "-Wparentheses" } */
+
+int foo (int);
+
+int
+bar (int a, int b, int c)
+{
+  foo (a <= b <= c); /* { dg-warning "comparison" "correct warning" } */
+  foo ((a <= b) <= c);
+  foo (a <= (b <= c));
+  foo (1 <= 2 <= c); /* { dg-warning "comparison" "correct warning" } */
+  foo ((1 <= 2) <= c);
+  foo (1 <= (2 <= c));
+  foo (1 <= 2 <= 3); /* { dg-warning "comparison" "correct warning" } */
+  foo ((1 <= 2) <= 3);
+  foo (1 <= (2 <= 3));
+  foo (a > b > c); /* { dg-warning "comparison" "correct warning" } */
+  foo ((a > b) > c);
+  foo (a > (b > c));
+  foo (1 > 2 > c); /* { dg-warning "comparison" "correct warning" } */
+  foo ((1 > 2) > c);
+  foo (1 > (2 > c));
+  foo (1 > 2 > 3); /* { dg-warning "comparison" "correct warning" } */
+  foo ((1 > 2) > 3);
+  foo (1 > (2 > 3));
+  foo (a < b <= c); /* { dg-warning "comparison" "correct warning" } */
+  foo ((a < b) <= c);
+  foo (a < (b <= c));
+  foo (1 < 2 <= c); /* { dg-warning "comparison" "correct warning" } */
+  foo ((1 < 2) <= c);
+  foo (1 < (2 <= c));
+  foo (1 < 2 <= 3); /* { dg-warning "comparison" "correct warning" } */
+  foo ((1 < 2) <= 3);
+  foo (1 < (2 <= 3));
+  foo (a <= b > c); /* { dg-warning "comparison" "correct warning" } */
+  foo ((a <= b) > c);
+  foo (a <= (b > c));
+  foo (1 <= 2 > c); /* { dg-warning "comparison" "correct warning" } */
+  foo ((1 <= 2) > c);
+  foo (1 <= (2 > c));
+  foo (1 <= 2 > 3); /* { dg-warning "comparison" "correct warning" } */
+  foo ((1 <= 2) > 3);
+  foo (1 <= (2 > 3));
+  foo (a <= b == c); /* { dg-warning "comparison" "correct warning" } */
+  foo ((a <= b) == c);
+  foo (a <= (b == c));
+  foo (1 <= 2 == c); /* { dg-warning "comparison" "correct warning" } */
+  foo ((1 <= 2) == c);
+  foo (1 <= (2 == c));
+  foo (1 <= 2 == 3); /* { dg-warning "comparison" "correct warning" } */
+  foo ((1 <= 2) == 3);
+  foo (1 <= (2 == 3));
+  foo (a != b != c); /* { dg-warning "comparison" "correct warning" } */
+  foo ((a != b) != c);
+  foo (a != (b != c));
+  foo (1 != 2 != c); /* { dg-warning "comparison" "correct warning" } */
+  foo ((1 != 2) != c);
+  foo (1 != (2 != c));
+  foo (1 != 2 != 3); /* { dg-warning "comparison" "correct warning" } */
+  foo ((1 != 2) != 3);
+  foo (1 != (2 != 3));
+}
---
+++ gcc/testsuite/gcc.dg/Wparentheses-7.c
@@ -0,0 +1,121 @@
+/* Test operation of -Wparentheses.  Precedence warnings.  & or + or -
+   or comparison inside ^.  */
+/* Origin: Joseph Myers <jsm@polyomino.org.uk> */
+
+/* { dg-do compile } */
+/* { dg-options "-Wparentheses" } */
+
+int foo (int);
+
+int
+bar (int a, int b, int c)
+{
+  foo (a & b ^ c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((a & b) ^ c);
+  foo (a & (b ^ c));
+  foo (1 & 2 ^ c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 & 2) ^ c);
+  foo (1 & (2 ^ c));
+  foo (1 & 2 ^ 3); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 & 2) ^ 3);
+  foo (1 & (2 ^ 3));
+  foo (a ^ b & c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((a ^ b) & c);
+  foo (a ^ (b & c));
+  foo (1 ^ 2 & c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 ^ 2) & c);
+  foo (1 ^ (2 & c));
+  foo (1 ^ 2 & 3); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 ^ 2) & 3);
+  foo (1 ^ (2 & 3));
+  foo (a + b ^ c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((a + b) ^ c);
+  foo (a + (b ^ c));
+  foo (1 + 2 ^ c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 + 2) ^ c);
+  foo (1 + (2 ^ c));
+  foo (1 + 2 ^ 3); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 + 2) ^ 3);
+  foo (1 + (2 ^ 3));
+  foo (a ^ b + c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((a ^ b) + c);
+  foo (a ^ (b + c));
+  foo (1 ^ 2 + c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 ^ 2) + c);
+  foo (1 ^ (2 + c));
+  foo (1 ^ 2 + 3); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 ^ 2) + 3);
+  foo (1 ^ (2 + 3));
+  foo (a - b ^ c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((a - b) ^ c);
+  foo (a - (b ^ c));
+  foo (1 - 2 ^ c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 - 2) ^ c);
+  foo (1 - (2 ^ c));
+  foo (1 - 2 ^ 3); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 - 2) ^ 3);
+  foo (1 - (2 ^ 3));
+  foo (a ^ b - c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((a ^ b) - c);
+  foo (a ^ (b - c));
+  foo (1 ^ 2 - c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 ^ 2) - c);
+  foo (1 ^ (2 - c));
+  foo (1 ^ 2 - 3); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 ^ 2) - 3);
+  foo (1 ^ (2 - 3));
+  foo (a >= b ^ c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((a >= b) ^ c);
+  foo (a >= (b ^ c));
+  foo (1 >= 2 ^ c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 >= 2) ^ c);
+  foo (1 >= (2 ^ c));
+  foo (1 >= 2 ^ 3); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 >= 2) ^ 3);
+  foo (1 >= (2 ^ 3));
+  foo (a ^ b >= c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((a ^ b) >= c);
+  foo (a ^ (b >= c));
+  foo (1 ^ 2 >= c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 ^ 2) >= c);
+  foo (1 ^ (2 >= c));
+  foo (1 ^ 2 >= 3); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 ^ 2) >= 3);
+  foo (1 ^ (2 >= 3));
+  foo (a == b ^ c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((a == b) ^ c);
+  foo (a == (b ^ c));
+  foo (1 == 2 ^ c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 == 2) ^ c);
+  foo (1 == (2 ^ c));
+  foo (1 == 2 ^ 3); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 == 2) ^ 3);
+  foo (1 == (2 ^ 3));
+  foo (a ^ b == c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((a ^ b) == c);
+  foo (a ^ (b == c));
+  foo (1 ^ 2 == c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 ^ 2) == c);
+  foo (1 ^ (2 == c));
+  foo (1 ^ 2 == 3); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 ^ 2) == 3);
+  foo (1 ^ (2 == 3));
+  foo (a < b ^ c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((a < b) ^ c);
+  foo (a < (b ^ c));
+  foo (1 < 2 ^ c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 < 2) ^ c);
+  foo (1 < (2 ^ c));
+  foo (1 < 2 ^ 3); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 < 2) ^ 3);
+  foo (1 < (2 ^ 3));
+  foo (a ^ b < c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((a ^ b) < c);
+  foo (a ^ (b < c));
+  foo (1 ^ 2 < c); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 ^ 2) < c);
+  foo (1 ^ (2 < c));
+  foo (1 ^ 2 < 3); /* { dg-warning "parentheses" "correct warning" } */
+  foo ((1 ^ 2) < 3);
+  foo (1 ^ (2 < 3));
+}
