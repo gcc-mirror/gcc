73b71f3e137fb50881429d16ee8fe6b983cc59e8
Mark Mitchell <mark@codesourcery.com>
2004-07-22 21:37:06 +0000

    * g++.dg/parse/attr2.C: Simplify.
    From-SVN: r85060

--- gcc/testsuite/g++.dg/parse/attr2.C
+++ gcc/testsuite/g++.dg/parse/attr2.C
@@ -1,10 +1,7 @@
 // PR c++/16337
-// On i686-pc-linux-gnu, without options, we get:
-//   warning: SSE vector return without SSE enabled changes the ABI
-// { dg-options "-w" }
 
 #define vector __attribute__((vector_size(16)))
-vector signed int foo (void)
+void foo (void)
 {
   vector signed int v1 = { 1, 2, 3, 4 };
   vector signed int v2, v3;
