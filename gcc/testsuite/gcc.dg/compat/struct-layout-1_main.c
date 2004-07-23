5aa33bdb4f75939b9467a4580aa13bb6997f192c
Jakub Jelinek <jakub@redhat.com>
2004-07-24 00:36:47 +0200

    Makefile.in (site.exp): Add HOSTCC and HOSTCFLAGS to site.exp.
    	* Makefile.in (site.exp): Add HOSTCC and HOSTCFLAGS to site.exp.
    testsuite/
    	* README.compat: Add RUN_ALL_COMPAT_TESTS documentation.
    	* lib/compat.exp: Replace ^$tmpdir/? in testcase names with tmpdir-.
    	* gcc.dg/compat/struct-layout-1.exp: New testsuite driver.
    	* gcc.dg/compat/struct-layout-1_generate.c: New testcase generator.
    	* gcc.dg/compat/generate-random.c: New file.
    	* gcc.dg/compat/generate-random.h: Likewise.
    	* gcc.dg/compat/generate-random_r.c: Likewise.
    	* gcc.dg/compat/struct-layout-1.h: Likewise.
    	* gcc.dg/compat/struct-layout-1_x1.h: Likewise.
    	* gcc.dg/compat/struct-layout-1_x2.h: Likewise.
    	* gcc.dg/compat/struct-layout-1_y1.h: Likewise.
    	* gcc.dg/compat/struct-layout-1_y2.h: Likewise.
    	* gcc.dg/compat/struct-layout-1_main.c: New test.
    	* gcc.dg/compat/struct-layout-1_x.c: New file.
    	* gcc.dg/compat/struct-layout-1_y.c: New file.
    	* gcc.dg/compat/struct-layout-1_test.h: New file.
    
    From-SVN: r85098

---
+++ gcc/testsuite/gcc.dg/compat/struct-layout-1_main.c
@@ -0,0 +1,15 @@
+#include "struct-layout-1.h"
+
+#define TX(n, type, attrs, fields, ops) extern void test##n (void);
+#include "struct-layout-1_test.h"
+#undef TX
+
+int main (void)
+{
+#define TX(n, type, attrs, fields, ops)   test##n ();
+#include "struct-layout-1_test.h"
+#undef TX
+  if (fails)
+    abort ();
+  exit (0);
+}
---
+++ gcc/testsuite/gcc.dg/compat/struct-layout-1_y.c
@@ -0,0 +1,5 @@
+/* { dg-options "-w" } */
+#include "struct-layout-1_y1.h"
+#include "struct-layout-1_test.h"
+#include "struct-layout-1_y2.h"
+#include "struct-layout-1_test.h"
