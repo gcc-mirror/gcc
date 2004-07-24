0c6d842886b9dce2a675dd484526522ddfe7dd12
Mike Stump <mrs@apple.com>
2004-07-24 07:32:15 +0000

    c-typeck.c (convert_for_assignment): Tightened up pointer converstions that differ in signedness.
            * c-typeck.c (convert_for_assignment): Tightened up pointer converstions
            that differ in signedness.
    Apple Radar 2535328.
    
    From-SVN: r85114

---
+++ gcc/testsuite/gcc.dg/conv-2.c
@@ -0,0 +1,26 @@
+/* { dg-do compile } */
+/* { dg-options "" } */
+
+void f1(long *);
+void f2(unsigned long *);
+
+int main()
+{
+  long *lp;
+  unsigned long *ulp;
+  char *cp;
+  unsigned char *ucp;
+  signed char *scp;
+
+  ulp = lp;	/* { dg-warning " pointer targets in assignment differ in signedness" } */
+  lp = ulp;	/* { dg-warning " pointer targets in assignment differ in signedness" } */
+  f1(ulp);	/* { dg-warning " differ in signedness" } */
+  f2(lp);	/* { dg-warning " differ in signedness" } */
+
+  cp = ucp;	/* { dg-warning " pointer targets in assignment differ in signedness" } */
+  cp = scp;	/* { dg-warning " pointer targets in assignment differ in signedness" } */
+  ucp = scp;	/* { dg-warning " pointer targets in assignment differ in signedness" } */
+  ucp = cp;	/* { dg-warning " pointer targets in assignment differ in signedness" } */
+  scp = ucp;	/* { dg-warning " pointer targets in assignment differ in signedness" } */
+  scp = cp;	/* { dg-warning " pointer targets in assignment differ in signedness" } */
+}
