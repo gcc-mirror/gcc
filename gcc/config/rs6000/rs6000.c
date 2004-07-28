0ae7a632035e3c3642bda5ee0672ed324670fa77
Alan Modra <amodra@bigpond.net.au>
2004-07-28 03:39:47 +0000

    * config/rs6000/rs6000.c (function_arg_padding): Pad SFmode upwards.
    From-SVN: r85251

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -4029,6 +4029,10 @@ function_arg_padding (enum machine_mode mode, tree type)
 	return upward;
     }
 
+  /* SFmode parameters are padded upwards.  */
+  if (mode == SFmode)
+    return upward;
+
   /* Fall back to the default.  */
   return DEFAULT_FUNCTION_ARG_PADDING (mode, type);
 }
