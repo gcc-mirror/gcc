31c2995d302e4610bd7c93604576aa0a227a7b18
austern <austern@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-07-28 23:57:28 +0000

    * config/darwin.c (darwin_one_byte_bool): New global variable.         * config/darwin.h (darwin_one_byte_bool): Declare.         (SUBTARGET_OPTIONS): Define macro.  (for -mone-byte-bool flag.)         * config/rs6000/darwin.h (BOOL_TYPE_SIZE): Conditionalize on         value of darwin_one_byte_bool.         * doc/invoke.texi: Document -mone-byte-bool flag.         * testsuite/gcc.dg/darwin-bool-1.c: New test.         * testsuite/gcc.dg/darwin-bool-2.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@85277 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/rs6000/darwin.h
+++ gcc/config/rs6000/darwin.h
@@ -321,8 +321,10 @@ do {									\
 #define DOUBLE_INT_ASM_OP "\t.quad\t"
 
 /* For binary compatibility with 2.95; Darwin C APIs use bool from
-   stdbool.h, which was an int-sized enum in 2.95.  */
-#define BOOL_TYPE_SIZE INT_TYPE_SIZE
+   stdbool.h, which was an int-sized enum in 2.95.  Users can explicitly
+   choose to have sizeof(bool)==1 with the -mone-byte-bool switch. */
+extern const char *darwin_one_byte_bool;
+#define BOOL_TYPE_SIZE (darwin_one_byte_bool ? CHAR_TYPE_SIZE : INT_TYPE_SIZE)
 
 #undef REGISTER_TARGET_PRAGMAS
 #define REGISTER_TARGET_PRAGMAS DARWIN_REGISTER_TARGET_PRAGMAS
---
+++ gcc/testsuite/gcc.dg/darwin-bool-1.c
@@ -0,0 +1,11 @@
+/* Check that sizeof(bool) is 4 if we don't use special options. */
+/* Matt Austern  <austern@apple.com> */
+/* { dg-do run { target powerpc*-*-darwin* } } */
+
+int dummy1[sizeof(_Bool) - 3];
+int dummy2[5 - sizeof(_Bool)];
+
+int main()
+{
+  return sizeof(_Bool) == 4 ? 0 : 1;
+}
