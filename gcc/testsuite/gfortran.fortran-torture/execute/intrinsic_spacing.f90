1fd0ec3576405da64e9c50637b459c4d65d03c8f
Richard Henderson <rth@redhat.com>
2004-07-28 15:04:28 -0700

    intrinsic_spacing.f90: Pass arguments in the correct type.
            * gfortran.fortran-torture/execute/intrinsic_spacing.f90: Pass
            arguments in the correct type.  Don't write to constant arguments.
    
    From-SVN: r85272

--- gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_spacing.f90
+++ gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_spacing.f90
@@ -4,15 +4,16 @@ program test_spacing
   call test_real4(3.0)
   call test_real4(33.0)
   call test_real4(-3.)
-  call test_real4(0)
-  call test_real8(0)
+  call test_real4(0.0)
+  call test_real8(0.0_8)
   call test_real8(3.0_8)
   call test_real8(33.0_8)
   call test_real8(-33._8)
 end
-subroutine test_real4(x)
-  real x,y,t
+subroutine test_real4(orig)
+  real x,y,t,orig
   integer p
+  x = orig
   p = 24
   y = 2.0 ** (exponent (x) - p)
   t = tiny(x)
@@ -21,9 +22,10 @@ subroutine test_real4(x)
     .and. (abs (x - t) .gt. abs(x * 1e-6)))call abort
 end
 
-subroutine test_real8(x)
-  real*8 x,y,t
+subroutine test_real8(orig)
+  real*8 x,y,t,orig
   integer p
+  x = orig
   p = 53
   y = 2.0 ** (exponent (x) - p)
   t = tiny (x)
