587530637932f8e7cf65de50ec063ae0fb2e301f
Paolo Carlini <pcarlini@suse.de>
2004-07-29 10:52:49 +0000

    re PR libstdc++/16813 (compiler error in DEBUG version of range insertion std::map::insert)
    2004-07-29  Paolo Carlini  <pcarlini@suse.de>
    
    	PR libstdc++/16813
    	* include/debug/map.h (insert(_InputIterator, _InputIterator)):
    	Fix typo.
    	* testsuite/23_containers/map/insert/16813.cc: New.
    
    From-SVN: r85295

---
+++ libstdc++-v3/testsuite/23_containers/map/insert/16813.cc
@@ -0,0 +1,42 @@
+// Copyright (C) 2004 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+#include <debug/map>
+#include <testsuite_hooks.h>
+
+// libstdc++/16813
+void test01()
+{
+  using __gnu_debug::map;
+  bool test __attribute__((unused)) = true;
+
+  map<int, float> m1, m2;
+
+  m1[3] = 3.0f;
+  m1[11] = -67.0f;
+
+  m2.insert(m1.begin(), m1.end());
+
+  VERIFY( m1 == m2 );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
