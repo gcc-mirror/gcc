a4e41fdeecdeadee4f4b221ef38ab38122d1fc18
Ben Elliston <bje@au.ibm.com>
2004-08-02 01:05:47 +0000

    Patch for PR target/16155.
    2004-08-02  Ben Elliston  <bje@au.ibm.com>
    
    	PR target/16155
    	* config/rs6000/altivec.h: Replace typeof with __typeof__.
    
    [testsuite]
    2004-08-02  Ben Elliston  <bje@au.ibm.com>
    
    	* gcc.dg/pr16155.c: New test.
    
    From-SVN: r85418

---
+++ gcc/testsuite/gcc.dg/pr16155.c
@@ -0,0 +1,17 @@
+/* { dg-do compile { target powerpc*-*-* } } */
+/* { dg-options "-maltivec -ansi" } */
+
+/* PR 16155
+ * Compilation of a simple altivec test program fails if the -ansi flag is
+ * given to gcc, when compiling with -maltivec.
+ */
+
+#include <altivec.h>
+
+void foo(void)
+{
+  vector unsigned short a, b;
+  a = vec_splat(b, 0);
+}
+
+/* { dg-bogus "parse error before \"typeof\"" "-maltivec -mansi" { target powerpc*-*-* } 0 } */
