010228b0b78bd9bfaa55ce086a2433c2bebd5bd8
zack <zack@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-02 03:23:01 +0000

    * config/ia64/ia64.c (got_symbolic_operand): Do require a 	symbol+offset operand to have its offset be zero mod 0x3fff 	when GOT entries are in use.  Clarify logic in SYMBOL_REF 	case.  Clarify comments. 	(ia64_expand_load_address): Split a symbol+offset load when 	the offset is nonzero mod 0x3fff, not 0x1fff. testsuite: 	* gcc.dg/ia64-got-1.c: New test case.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@85426 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/ia64-got-1.c
@@ -0,0 +1,23 @@
+/* { dg-do compile { target ia64*-*-* } } */
+/* { dg-options "-O2 -fPIC" } */
+
+/* { dg-final { scan-assembler "@ltoffx\\(object#\\)" } } */
+/* { dg-final { scan-assembler "@ltoffx\\(object#\[-+\]16384\\)" } } */
+/* { dg-final { scan-assembler-not "@ltoffx\\(object#\[-+\]1\\)" } } */
+/* { dg-final { scan-assembler-not "@ltoffx\\(object#\[-+\]8191\\)" } } */
+/* { dg-final { scan-assembler-not "@ltoffx\\(object#\[-+\]8192\\)" } } */
+/* { dg-final { scan-assembler-not "@ltoffx\\(object#\[-+\]8193\\)" } } */
+/* { dg-final { scan-assembler-not "@ltoffx\\(object#\[-+\]16383\\)" } } */
+/* { dg-final { scan-assembler-not "@ltoffx\\(object#\[-+\]16385\\)" } } */
+
+/* must not be in sdata */
+extern char object[];
+
+#define r(n) char *r_##n (void) { return &object[n]; }
+#define R(n) char *R_##n (void) { return &object[-n]; }
+
+#define t(n) r(n) R(n)
+
+t(0) t(1)
+t(8191) t(8192) t(8193)
+t(16383) t(16384) t(16385)
