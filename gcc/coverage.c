e9ee282e1a7ffb499b706caad9ad0f3b8cef676e
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-03 14:05:15 +0000

    * coverage.c (tree_coverage_counter_ref): Fix computation of the new 	ARRAY_REF argument.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@85477 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/coverage.c
+++ gcc/coverage.c
@@ -437,7 +437,7 @@ tree_coverage_counter_ref (unsigned counter, unsigned no)
 		 fold_convert (domain_type, build_int_2 (no, 0)),
 		 TYPE_MIN_VALUE (domain_type),
 		 size_binop (EXACT_DIV_EXPR, TYPE_SIZE_UNIT (GCOV_TYPE_NODE),
-			     size_int (TYPE_ALIGN (GCOV_TYPE_NODE))));
+			     size_int (TYPE_ALIGN_UNIT (GCOV_TYPE_NODE))));
 }
 
 /* Generate a checksum for a string.  CHKSUM is the current
