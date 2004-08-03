5cd9021030cf3325522575b0bdb3e1dbb923a7f3
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-03 14:19:39 +0000

    * gcc.dg/array-7.c, gcc.dg/c99-tag-2.c: New tests.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@85479 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/array-7.c
@@ -0,0 +1,14 @@
+/* Test for array of incomplete structure type - Zack Weinberg in
+   <http://gcc.gnu.org/ml/gcc-patches/2004-08/msg00108.html>.  */
+/* { dg-do compile } */
+/* { dg-options "" } */
+
+struct foo;
+
+void
+f (void)
+{
+  struct foo { int a; int b; };
+}
+
+struct foo array[5]; /* { dg-error "storage size" } */
