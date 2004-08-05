b17e42301f9d1dfa8879d66fce0c87ac7722ae5d
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-05 07:13:56 +0000

    * config/i386/i386.c (ix86_expand_prologue): If the function uses a 	frame pointer, restore eax with an ebp-relative address.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@85595 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20040805-1.c
@@ -0,0 +1,31 @@
+#if __INT_MAX__ < 32768 || (defined(STACK_SIZE) && STACK_SIZE < 0x12000)
+int main () { exit (0); }
+#else
+int a[2] = { 2, 3 };
+
+static int __attribute__((noinline))
+bar (int x, void *b)
+{
+  a[0]++;
+  return x;
+}
+
+static int __attribute__((noinline))
+foo (int x)
+{
+  char buf[0x10000];
+  int y = a[0];
+  a[1] = y;
+  x = bar (x, buf);
+  y = bar (y, buf);
+  return x + y;
+}
+
+int
+main ()
+{
+  if (foo (100) != 102)
+    abort ();
+  exit (0);
+}
+#endif
