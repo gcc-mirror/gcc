3304fb1c75b739d6f91161842c6e0e0a4c7abaa3
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-06 11:23:23 +0000

    PR c/13282 	* c.opt (Wmissing-field-initializers): New option. 	* c-opts.c (c_common_post_options): Make -Wextra turn it on by default. 	* c-typeck.c (pop_init_level): Guard the missing field warning with 	warn_missing_field_initializers rather than extra_warnings. 	* doc/invoke.texi (-Wmissing-field-initializers): Document, moving 	some of the explanation from... 	(-Wextra): ...here.  Say that the missing field warning can be 	seperately controlled by -Wmissing-field-initializers.
    cp/
    	* typeck2.c (process_init_constructor): Guard the missing field warning
    	with warn_missing_field_initializers rather than extra_warnings.
    
    testsuite/
    	* gcc.dg/missing-field-init-[12].c: New tests.
    	* g++.dg/warn/missing-field-init-[12].C: New tests.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@85638 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/missing-field-init-1.c
@@ -0,0 +1,10 @@
+/* { dg-do compile } */
+/* { dg-options "-Wextra -Wno-missing-field-initializers -std=c99" } */
+
+struct s { int a, b, c; };
+struct s s1 = { 1, 2, 3 };
+struct s s2 = { 1, 2 }; /* { dg-bogus "missing initializer" } */
+struct s s3[] = { { 1, 2 }, { 4, 5 } }; /* { dg-bogus "missing initializer" } */
+struct s s4[] = { 1, 2, 3, 4, 5 }; /* { dg-bogus "missing initializer" } */
+struct s s5[] = { 1, 2, 3, 4, 5, 6 };
+struct s s6 = { .a = 1 }; /* { dg-bogus "missing initializer" } */
