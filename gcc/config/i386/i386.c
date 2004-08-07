d6a7d5f6c83b124774eb22d84a47cd17bfd4e29c
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-07 16:34:00 +0000

    2004-08-07  H.J. Lu  <hongjiu.lu@intel.com>
    	* config/i386/i386.c (ix86_expand_clrmem): Properly call
    	gen_strset for 64bit target.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@85673 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -11569,7 +11569,10 @@ ix86_expand_clrmem (rtx dst, rtx count_exp, rtx align_exp)
 		rtx mem = adjust_automodify_address_nv (dst,
 							GET_MODE (zeroreg),
 							destreg, offset);
-		emit_insn (gen_strset (destreg, mem, zeroreg));
+		emit_insn (gen_strset (destreg, mem,
+				       (TARGET_64BIT
+					? gen_rtx_SUBREG (SImode, zeroreg, 0)
+					: zeroreg)));
 		offset += size;
 	      }
 	  else
