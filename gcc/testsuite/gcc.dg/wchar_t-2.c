5dacbe5333f7a9a3ee0860f89f0ba1262470b278
bonzini <bonzini@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-09 07:24:23 +0000

    2004-08-09  Paolo Bonzini  <bonzini@gnu.org>
    	* gcc.dg/wchar_t-2.c: Fix typo.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@85713 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/wchar_t-2.c
+++ gcc/testsuite/gcc.dg/wchar_t-2.c
@@ -1,6 +1,6 @@
 /* { dg-options "-fshort-wchar" } */
 /* Check that "-fshort-wchar" makes wchar_t the same size as "unsigned
-   short".  /
+   short".  */
 
 extern int i[sizeof (L'a')];
 int i[sizeof (unsigned short)];
