300523852fb45755cb9c9e0f8132b4bdfa7adf27
Janis Johnson <janis187@us.ibm.com>
2004-08-12 18:08:12 +0000

    ops-long-2.c: Add dg-error directives.
    2004-08-12  Janis Johnson  <janis187@us.ibm.com>
    
    	* gcc.dg/vmx/ops-long-2.c: Add dg-error directives.
    
    From-SVN: r85889

--- gcc/testsuite/gcc.dg/vmx/ops-long-2.c
+++ gcc/testsuite/gcc.dg/vmx/ops-long-2.c
@@ -17,18 +17,18 @@ extern vector unsigned int * *var_vec_u32_ptr;
 extern vector unsigned int *var_vec_u32;
 
 void f36() {
-  vec_st(var_vec_s32[0], var_int[1], var_long_ptr[2]);
-  vec_st(var_vec_u32[0], var_int[1], var_unsigned_long_ptr[2]);
+  vec_st(var_vec_s32[0], var_int[1], var_long_ptr[2]); /* { dg-error "invalid parameter combination" } */
+  vec_st(var_vec_u32[0], var_int[1], var_unsigned_long_ptr[2]); /* { dg-error "invalid parameter combination" } */
 }
 void f37() {
-  vec_ste(var_vec_s32[0], var_int[1], var_long_ptr[2]);
-  vec_ste(var_vec_u32[0], var_int[1], var_unsigned_long_ptr[2]);
-  vec_stl(var_vec_s32[0], var_int[1], var_long_ptr[2]);
-  vec_stl(var_vec_u32[0], var_int[1], var_unsigned_long_ptr[2]);
-  vec_stvewx(var_vec_s32[0], var_int[1], var_long_ptr[2]);
-  vec_stvewx(var_vec_u32[0], var_int[1], var_unsigned_long_ptr[2]);
-  vec_stvx(var_vec_s32[0], var_int[1], var_long_ptr[2]);
-  vec_stvx(var_vec_u32[0], var_int[1], var_unsigned_long_ptr[2]);
-  vec_stvxl(var_vec_s32[0], var_int[1], var_long_ptr[2]);
-  vec_stvxl(var_vec_u32[0], var_int[1], var_unsigned_long_ptr[2]);
+  vec_ste(var_vec_s32[0], var_int[1], var_long_ptr[2]); /* { dg-error "invalid parameter combination" } */
+  vec_ste(var_vec_u32[0], var_int[1], var_unsigned_long_ptr[2]); /* { dg-error "invalid parameter combination" } */
+  vec_stl(var_vec_s32[0], var_int[1], var_long_ptr[2]); /* { dg-error "invalid parameter combination" } */
+  vec_stl(var_vec_u32[0], var_int[1], var_unsigned_long_ptr[2]); /* { dg-error "invalid parameter combination" } */
+  vec_stvewx(var_vec_s32[0], var_int[1], var_long_ptr[2]); /* { dg-error "invalid parameter combination" } */
+  vec_stvewx(var_vec_u32[0], var_int[1], var_unsigned_long_ptr[2]); /* { dg-error "invalid parameter combination" } */
+  vec_stvx(var_vec_s32[0], var_int[1], var_long_ptr[2]); /* { dg-error "invalid parameter combination" } */
+  vec_stvx(var_vec_u32[0], var_int[1], var_unsigned_long_ptr[2]); /* { dg-error "invalid parameter combination" } */
+  vec_stvxl(var_vec_s32[0], var_int[1], var_long_ptr[2]); /* { dg-error "invalid parameter combination" } */
+  vec_stvxl(var_vec_u32[0], var_int[1], var_unsigned_long_ptr[2]); /* { dg-error "invalid parameter combination" } */
 }
