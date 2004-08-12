dfca499a8fd96b41f52b648ceec82f0710eeb2bd
janis <janis@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-12 21:28:12 +0000

    2004-08-12  Janis Johnson  <janis187@us.ibm.com>
    	* g++.dg/ext/altivec-12.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@85907 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/ext/altivec-12.C
@@ -0,0 +1,19 @@
+/* Test vec_dst* functions with float pointer as first argument.  */
+/* { dg-do compile { target powerpc*-*-* } } */
+/* { dg-xfail-if "" { "powerpc-ibm-aix*" } { "-maltivec" } { "" } } */
+/* { dg-options "-maltivec" } */
+
+#include <altivec.h>
+
+extern int i;
+extern float *fp;
+extern vector float vf;
+
+void
+foo ()
+{
+  vec_dst (fp, i, 1);
+  vec_dstst (fp, i, 1);
+  vec_dststt (fp, i, 1);
+  vec_dstt (fp, i, 1);
+}
