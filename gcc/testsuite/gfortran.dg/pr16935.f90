d01f196ea81712ea65500ddf35b30aff987d5bb4
bdavis <bdavis@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-13 22:01:02 +0000

    2004-08-13  Bud Davis  <bdavis9659@comcast.net>
            PR gfortran/13965
            * gfortran.fortran-torture/execute/open_rewind.f90: New test.
            * io/open.c (st_open): use flags instead of the unit structure.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@85966 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr16935.f90
@@ -0,0 +1,7 @@
+! { dg-do run }
+! pr16935
+! segfault at run time on open statement
+       program bug2
+       implicit none
+       open( 1 , file = "str_500.txt", position = "REWIND" )
+       end
