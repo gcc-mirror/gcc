3fee54be329cad6ec896c9b243073e54b7556c85
Janis Johnson <janis187@us.ibm.com>
2004-08-13 00:46:54 +0000

    gcc.dg/vmx/cw-bug-2.c: Remove.
    2004-08-12  Janis Johnson  <janis187@us.ibm.com>
    
    	gcc.dg/vmx/cw-bug-2.c: Remove.
    	gcc.dg/vmx/vprint-1.c: Remove.
    	gcc.dg/vmx/vscan-1.c: Remove.
    
    From-SVN: r85923

--- gcc/testsuite/gcc.dg/vmx/cw-bug-2.c
+++
@@ -1,9 +0,0 @@
-#include <altivec.h>
-#include <stdlib.h>
-
-int main(void)
-{
-  if (vec_any_ne((vector unsigned short)(((vector unsigned char){0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0})),
-		 vec_mfvscr()))
-    abort ();
-}
