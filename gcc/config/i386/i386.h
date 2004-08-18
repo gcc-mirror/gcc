697bc1129a090ced8dae1a4ea71be81c3e16b024
dorit <dorit@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-18 11:09:36 +0000

    Part of the vectorizer patch that was accidently left out...:         * config/i386/i386.h (UNITS_PER_SIMD_WORD): Define.         * config/rs6000/rs6000.h (UNITS_PER_SIMD_WORD): Define.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@86180 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.h
+++ gcc/config/i386/i386.h
@@ -1080,6 +1080,9 @@ do {									\
      : VALID_MMX_REG_MODE (MODE) && TARGET_MMX ? 1			\
      : VALID_MMX_REG_MODE_3DNOW (MODE) && TARGET_3DNOW ? 1 : 0)
 
+#define UNITS_PER_SIMD_WORD \
+    (TARGET_SSE ? 16 : TARGET_MMX || TARGET_3DNOW ? 8 : 0)
+
 #define VALID_FP_MODE_P(MODE)						\
     ((MODE) == SFmode || (MODE) == DFmode || (MODE) == XFmode		\
      || (MODE) == SCmode || (MODE) == DCmode || (MODE) == XCmode)	\
