0fa1b65cad6cb3ccbf48f992e1710a6953604842
Bud Davis <bdavis9659@comcast.net>
2004-08-21 11:20:29 +0000

    re PR libfortran/16908 (Segfault in libgfortran/io/transfer.c)
    2004-08-21  Bud Davis  <bdavis9659@comcast.net>
    
    	PR 16908
    	* io/transfer.c (next_record_w): Do not blank pad.
    	* io/transfer.c (next_record): Take into account partial records.
    testsuite/
    	* gfortran.dg/direct_io.f90: New test.
    
    From-SVN: r86361

---
+++ gcc/testsuite/gfortran.dg/direct_io_1.f90
@@ -0,0 +1,31 @@
+! PR 16908
+! Segfaulted on second set of writes.  We weren't handling partial records
+! properly when calculating the file position.
+program direct_io_1
+  implicit none
+
+  integer n, nt, mt, m
+  real dt, tm, w
+  real, allocatable :: p(:)
+
+  nt =  2049 ! if nt < 2049, then everything works.
+
+  allocate(p(nt))
+  p  = 0.e0
+
+  inquire(iolength=mt) (p(m), m=1, nt)
+
+  open(unit=12, file='syn.sax', access='direct', recl=mt)
+  n = 1
+  write(12, rec=n) mt, nt
+  write(12, rec=n+1) (p(m), m=1, nt)
+  close(12)
+
+  inquire(iolength=mt) (p(m), m=1, nt)
+
+  open(unit=12, file='syn.sax', access='direct', recl=mt)
+  n = 1
+  write(12, rec=n) mt, nt
+  write(12, rec=n+1) (p(m), m=1, nt)
+  close(12)
+end program
