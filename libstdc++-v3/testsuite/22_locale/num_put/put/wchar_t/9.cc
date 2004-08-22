bdb308868d0122cbe761014c3f865c822b213d3a
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-22 17:55:51 +0000

    2004-08-22  Paolo Carlini  <pcarlini@suse.de>
    	* include/bits/locale_facets.tcc (num_put<>::do_put(bool)): Cast
    	to a signed type, long according to the resolution of DR 359.
    	* testsuite/22_locale/num_put/put/char/9.cc: New.
    	* testsuite/22_locale/num_put/put/wchar_t/9.cc: New.
    
    	* include/bits/locale_facets.tcc (num_put<>::do_put(const void*)):
    	Simplify a bit: no need to clear showpos.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@86382 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/22_locale/num_put/put/wchar_t/9.cc
@@ -0,0 +1,62 @@
+// 2004-08-22  Paolo Carlini  <pcarlini@suse.de>
+
+// Copyright (C) 2004 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 22.2.2.2.1  num_put members
+
+#include <locale>
+#include <sstream>
+#include <testsuite_hooks.h>
+
+// According to 22.2.2.2.2, p23 and the resolution of DR 359
+// val must be casted to a signed type: this can be revealed
+// by ios_base::showpos, which is effective only for signed
+// types (also see libstdc++/15565 about this).
+void test01()
+{
+  using namespace std;
+  bool test __attribute__((unused)) = true;
+
+  // basic construction
+  locale loc_c = locale::classic();
+
+  // sanity check the data is correct.
+  const wstring empty;
+
+  // cache the num_put facet
+  wostringstream oss;
+  oss.imbue(loc_c);
+  const num_put<wchar_t>& np = use_facet<num_put<wchar_t> >(oss.getloc());
+
+  bool b = true;
+  np.put(oss.rdbuf(), oss, L' ', b);
+  VERIFY( oss.str() == L"1" );
+
+  oss.str(empty);
+  oss.clear();
+  oss.setf(ios_base::showpos);
+  np.put(oss.rdbuf(), oss, L' ', b);
+  VERIFY( oss.str() == L"+1" );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
