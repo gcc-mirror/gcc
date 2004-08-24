f8ad2d219e145753abb13bf84b606a41df1ad5bb
Nathan Sidwell <nathan@codesourcery.com>
2004-08-24 16:06:22 +0000

    re PR c++/16889 (ambiguity is not detected)
    cp:
    	PR c++/16889
    	* (is_subobject_of_p): Resurrect & optimize.
    	(lookup_field_r): Use it.
    testsuite:
    	PR c++/16889
    	* g++.dg/lookup/ambig[12].C: New.
    
    From-SVN: r86488

---
+++ gcc/testsuite/g++.dg/lookup/ambig1.C
@@ -0,0 +1,17 @@
+// { dg-do compile }
+
+// Copyright (C) 2004 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 24 Aug 2004 <nathan@codesourcery.com>
+// Origin: Wolfgang Bangerth  <bangerth@dealii.org>
+
+// Bug 16889:Undetected ambiguity.
+
+struct B { 
+  int f(); // { dg-error "int B::f" "" }
+}; 
+ 
+struct B1 : virtual B {}; 
+struct B2 : B {}; 
+struct BB : B1, B2 {}; 
+ 
+int i = BB().f();  // { dg-error "ambiguous" "" }
