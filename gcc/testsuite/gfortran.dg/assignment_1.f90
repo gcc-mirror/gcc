6d1c50cce40910b7015fefa7ce6c632c750bda22
Tobias Schlüter <tobias.schlueter@physik.uni-muenchen.de>
2004-08-26 00:07:32 +0200

    expr.c (gfc_check_assign): Add comment.
    fortran/
    * expr.c (gfc_check_assign): Add comment. Add new warning.
    * trans-expr.c (gfc_conv_function_call): Correctly dereference
     result of pointer valued function when not in pointer assignment.
    
    testsuite/
    * gfortran.dg/assignment_1.f90: New test.
    
    From-SVN: r86585

---
+++ gcc/testsuite/gfortran.dg/assignment_1.f90
@@ -0,0 +1,36 @@
+! { dg-do run }
+! { dg-options -Wsurprising }
+integer, pointer :: p
+integer, target :: t, s
+
+! The tests for character pointers are currently commented out,
+! because they don't yet work correctly.
+! This is PR 17192
+!!$character*5, pointer :: d
+!!$character*5, target :: c, e
+
+t = 1
+p => s
+! We didn't dereference the pointer in the following line.
+p = f() ! { dg-warning "POINTER valued function" "" }
+p = p+1
+if (p.ne.2) call abort()
+if (p.ne.s) call abort()
+
+!!$! verify that we also dereference correctly the result of a function 
+!!$! which returns its result by reference
+!!$c = "Hallo"
+!!$d => e
+!!$d = g() !  dg-warning "POINTER valued function" "" 
+!!$if (d.ne."Hallo") call abort()
+
+contains
+function f()
+integer, pointer :: f
+f => t
+end function f
+!!$function g()
+!!$character, pointer :: g
+!!$g => c
+!!$end function g
+end
