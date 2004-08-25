84129158d185f832f990b2ea8cdd36f577bdd410
bdavis <bdavis@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-25 00:31:33 +0000

    2004-08-24  Bud Davis  <bdavis9659@comcast.net>
            PR fortran/17143
            * runtime/error.c (itoa): keep from overflowing during
            mod operation by using unsigned variable.
    
            * gfortran.dg/pr17143.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@86532 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr17143.f90
@@ -0,0 +1,16 @@
+! pr17143
+! does not print 2*63 correctly
+       character*25 l
+       integer*8 i
+       data i /1/
+       do j = 1,63
+          i = i * 2
+       end do
+       write(l,*)i
+       if (l.ne.' -9223372036854775808') then
+!                ^
+!         the space is required before a number
+          call abort
+       endif 
+       end
+
