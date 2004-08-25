a9b8c2ffe4bf6b6613d3a390c1d6a3ff5131b665
zack <zack@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-25 07:32:52 +0000

    * ifcvt.c (find_if_case_1): Remove ??? comment.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@86539 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ifcvt.c
+++ gcc/ifcvt.c
@@ -2909,7 +2909,6 @@ find_if_case_1 (basic_block test_bb, edge then_edge, edge else_edge)
     {
       new_bb->index = then_bb_index;
       BASIC_BLOCK (then_bb_index) = new_bb;
-      /* ??? Should be then_bb? */
       BB_COPY_PARTITION (new_bb, test_bb);
     }
   /* We've possibly created jump to next insn, cleanup_cfg will solve that
