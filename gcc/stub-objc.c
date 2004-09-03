c41e5f2707102c83157bd831a5de3de47e5dff92
Ziemowit Laski <zlaski@apple.com>
2004-09-03 20:17:50 +0000

    stub-objc.c (objc_is_id, [...]): New stubs.
    [gcc/ChangeLog]
    2004-09-03  Ziemowit Laski  <zlaski@apple.com>
    
    	* stub-objc.c (objc_is_id, objc_declare_alias, objc_declare_class,
    	objc_declare_protocols, objc_start_protocol,
    	objc_start_class_interface, objc_start_category_interface,
    	objc_continue_interface, objc_finish_interface,
    	objc_add_instance_variable, objc_set_visibility, objc_set_method_type,
    	objc_start_class_implementation, objc_start_category_implementation,
    	objc_continue_implementation, objc_finish_implementation,
    	objc_add_method_declaration, objc_start_method_definition,
    	objc_finish_method_definition, objc_build_keyword_decl,
    	objc_build_method_signature, objc_build_encode_expr,
    	objc_build_protocol_expr, objc_build_selector_expr,
    	objc_build_message_expr, objc_build_string_object,
    	objc_get_class_reference, objc_get_protocol_qualified_type,
    	objc_static_init_needed_p, objc_generate_static_init_call): New stubs.
    
    From-SVN: r87051

--- gcc/stub-objc.c
+++ gcc/stub-objc.c
@@ -34,6 +34,12 @@ objc_is_class_name (tree ARG_UNUSED (arg))
 }
 
 tree
+objc_is_id (tree ARG_UNUSED (arg))
+{
+  return 0;
+}
+
+tree
 objc_is_object_ptr (tree ARG_UNUSED (arg))
 {
   return 0;
@@ -59,7 +65,7 @@ objc_is_reserved_word (tree ARG_UNUSED (ident))
 int
 objc_comptypes (tree ARG_UNUSED (lhs), tree ARG_UNUSED (rhs),
                 int ARG_UNUSED (reflexive))
-{ 
+{
   return -1;
 }
 
@@ -70,8 +76,176 @@ objc_message_selector (void)
 }
 
 void
+objc_declare_alias (tree ARG_UNUSED (alias), tree ARG_UNUSED (orig))
+{
+}
+
+void
+objc_declare_class (tree ARG_UNUSED (list))
+{
+}
+
+void
+objc_declare_protocols (tree ARG_UNUSED (list))
+{
+}
+
+void
+objc_start_protocol (tree ARG_UNUSED (proto),
+		     tree ARG_UNUSED (protorefs))
+{
+}
+
+void
+objc_start_class_interface (tree ARG_UNUSED (name),
+			    tree ARG_UNUSED (super),
+			    tree ARG_UNUSED (protos))
+{
+}
+
+void
+objc_start_category_interface (tree ARG_UNUSED (name),
+			       tree ARG_UNUSED (categ),
+			       tree ARG_UNUSED (protos))
+{
+}
+
+void
+objc_continue_interface (void)
+{
+}
+
+void
+objc_finish_interface (void)
+{
+}
+
+void
+objc_add_instance_variable (tree ARG_UNUSED (decl))
+{
+}
+
+void
+objc_set_visibility (int ARG_UNUSED (vis))
+{
+}
+
+void
+objc_set_method_type (enum tree_code ARG_UNUSED (code))
+{
+}
+
+void
+objc_start_class_implementation (tree ARG_UNUSED (name),
+				 tree ARG_UNUSED (super))
+{
+}
+
+void
+objc_start_category_implementation (tree ARG_UNUSED (name),
+				    tree ARG_UNUSED (categ))
+{
+}
+
+void
+objc_continue_implementation (void)
+{
+}
+
+void
 objc_clear_super_receiver (void)
 {
+}
+
+void
+objc_finish_implementation (void)
+{
+}
+
+void
+objc_add_method_declaration (tree ARG_UNUSED (signature))
+{
+}
+
+void
+objc_start_method_definition (tree ARG_UNUSED (signature))
+{
+}
+
+void
+objc_finish_method_definition (tree ARG_UNUSED (fndecl))
+{
+}
+
+tree
+objc_build_keyword_decl (tree ARG_UNUSED (selector),
+			 tree ARG_UNUSED (typename),
+			 tree ARG_UNUSED (identifier))
+{
+  return 0;
+}
+
+tree
+objc_build_method_signature (tree ARG_UNUSED (rettype),
+			     tree ARG_UNUSED (selectors),
+			     tree ARG_UNUSED (optparms))
+{
+  return 0;
+}
+
+tree
+objc_build_encode_expr (tree ARG_UNUSED (expr))
+{
+  return 0;
+}
+
+tree
+objc_build_protocol_expr (tree ARG_UNUSED (expr))
+{
+  return 0;
+}
+
+tree
+objc_build_selector_expr (tree ARG_UNUSED (expr))
+{
+  return 0;
+}
+
+tree
+objc_build_message_expr (tree ARG_UNUSED (expr))
+{
+  return 0;
+}
+
+tree
+objc_build_string_object (tree ARG_UNUSED (str))
+{
+  return 0;
+}
+
+tree
+objc_get_class_reference (tree ARG_UNUSED (name))
+{
+  return 0;
+}
+
+tree
+objc_get_protocol_qualified_type (tree ARG_UNUSED (name),
+				  tree ARG_UNUSED (protos))
+{
+  return 0;
+}
+
+int
+objc_static_init_needed_p (void)
+{
+  return 0;
+}
+
+tree
+objc_generate_static_init_call (tree ARG_UNUSED (ctors))
+{
+  return 0;
 }
 
 int
