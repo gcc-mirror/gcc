c509784d64f2f711d1fb9d69958ecaa63b37ef33
Ziemowit Laski <zlaski@apple.com>
2004-09-08 18:48:56 +0000

    bitfield-2.m: Make 'id' definition a typedef.
    [gcc/testsuite/ChangeLog]
    2004-09-08  Ziemowit Laski  <zlaski@apple.com>
    
    	* objc.dg/bitfield-2.m: Make 'id' definition a typedef.
    	* obj.dg/bitfield-4.m: Allow 'unsigned' in addition to 'unsigned int'
    	in error message.
    	* objc.dg/id-1.m: Attempt to define 'id' in an incompatible fashion.
    	* objc.dg/method-6.m: Allow 'unsigned' in addition to 'unsigned int'
    	in error message.
    	* objc.dg/proto-qual-1.m: Protocol qualifiers now appear before the
    	types they qualify.
    	* objc.dg/type-size-2.m: Fix wording in comment.
    	* objc.dg/va-meth-1.m: New test case.
    
    From-SVN: r87196

--- gcc/testsuite/objc.dg/id-1.m
+++ gcc/testsuite/objc.dg/id-1.m
@@ -1,6 +1,7 @@
-/* Test the id type warning.  */
+/* Test attempt to redefine 'id' in an incompatible fashion.  */
 /* { dg-do compile } */
 
-typedef int id;
+typedef int id;  /* { dg-error "conflicting types for .id." } */
+/* { dg-error "previous declaration of .id. was here" "" { target *-*-* } 0 } */
 
-id b; /* { dg-warning "nexpected type for `id'" } */
+id b;
