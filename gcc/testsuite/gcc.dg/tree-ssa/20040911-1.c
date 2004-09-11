a17ea89bb15f7e9668860609b8953e94b69d0763
Jakub Jelinek <jakub@redhat.com>
2004-09-11 20:57:06 +0200

    tree-ssa-alias.c (collect_points_to_info_r): Handle pointing to PTR + OFFSET as PTR...
    	* tree-ssa-alias.c (collect_points_to_info_r): Handle
    	pointing to PTR + OFFSET as PTR, not as pointing to PTR or pointing
    	to OFFSET.
    
    	* gcc.dg/tree-ssa/20040911-1.c: New test.
    
    From-SVN: r87357

---
+++ gcc/testsuite/gcc.dg/tree-ssa/20040911-1.c
@@ -0,0 +1,21 @@
+/* Verify that points-to information is handled properly for PTR + OFFSET
+   pointer arithmetics.  */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-alias1-vops" } */
+
+char buf[4], *q;
+int foo (int i)
+{
+  char c, *p;
+  q = &c;
+  p = buf;
+  if (i)
+    p = p + 3;
+  else
+    p = p + 2;
+  *p = 6;
+  c = 8;
+  return *p;
+}
+
+/* { dg-final { scan-tree-dump-not "VUSE <c" "alias1" } } */
