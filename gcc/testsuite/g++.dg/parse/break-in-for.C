f7eb22bbe7555a8aba6fba3abee7bc3e58785633
steven <steven@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-12 10:33:07 +0000

    * g++.dg/parse/break-in-for.C: Fix for -ansi -pedantic-errors.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@87387 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/parse/break-in-for.C
+++ gcc/testsuite/g++.dg/parse/break-in-for.C
@@ -1,6 +1,7 @@
-/* PR17412
-   fold-const would try to fold the operands of the break statement.  */
+/* PR17412 - ICE in fold_const.c during parsing.
+   fold would try to fold the operands of the break statement.  */
 /* { dg-do compile } */
+/* { dg-options "" } */
  
 
 void foo ()
