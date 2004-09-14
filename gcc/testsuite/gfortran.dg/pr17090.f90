c72477d6a6c9da1770910622684d16e7e0bc60a9
Bud Davis <bdavis9659@comcast.net>
2004-09-14 00:56:46 +0000

    re PR libfortran/17090 (Runtime I/O error)
    2004-09-13  Bud Davis  <bdavis9659@comcast.net>
    
            PR fortran/17090
    
            * io/list_read.c (list_formatted_read): Handle trailing spaces
            at end of line.
    
    
    2004-09-13  Bud Davis  <bdavis9659@comcast.net>
    
            PR fortran/17090
            * gfortran.dg/pr17090.f90: New test.
    
    From-SVN: r87474

---
+++ gcc/testsuite/gfortran.dg/pr17090.f90
@@ -0,0 +1,21 @@
+! pr 17090 Runtime I/O error
+! bdavis9659@comcast.net 
+! 9/12/2004
+! list directed read with spaces between the last data item and the
+! eoln cause missed data items.
+! this is a libgfortran test case
+       implicit none
+       integer i,sum
+       real a(14)
+       data sum / 0 /
+       open(unit=9,status='SCRATCH')
+       write(9,*)1.0,2.0,3.0,4.0,'      '
+       write(9,*)5.0,6.0,7.0,8.0,'      '
+       write(9,*)9.0,10.0,11.0,12.0,13.0,14.0,'      '
+       rewind(9)
+       read(9,*)a
+       do i = 1,14
+          sum = sum + a(i)
+       end do
+       if (sum.ne.105) call abort
+       end
