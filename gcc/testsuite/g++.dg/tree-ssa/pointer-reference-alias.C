a702ac2f8cfe4b37e51e17fa519cab7ab39e3fd5
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-15 01:18:01 +0000

    2004-09-14  Andrew Pinski  <apinski@apple.com>
            * g++.dg/tree-ssa/pointer-reference-alias.C: New test.
    
    2004-09-14  Andrew Pinski  <apinski@apple.com>
    
            * tree-ssa-copy.c (may_propagate_copy): Don't check the aliasing
            sets of the pointers but the aliasing sets of what they point to.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@87527 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/tree-ssa/pointer-reference-alias.C
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-vars" } */
+
+int f(int *a)
+{
+  int &b = *a;
+  b = 0;
+  return *a;
+}
+
+/* There should be only one dereferencing of a. */
+/* { dg-final { scan-tree-dump-times "\\*a" 1 "vars"} } */
+
