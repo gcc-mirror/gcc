7d32f35df7eb0e4731ff04d04baf131ce550048d
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-18 18:28:14 +0000

    PR c/17424 	* c-typeck.c (c_expr_sizeof_expr): Check for error_mark_node.
    testsuite:
    	* gcc.dg/pr17424-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@87705 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-typeck.c
+++ gcc/c-typeck.c
@@ -1855,9 +1855,18 @@ struct c_expr
 c_expr_sizeof_expr (struct c_expr expr)
 {
   struct c_expr ret;
-  ret.value = c_sizeof (TREE_TYPE (expr.value));
-  ret.original_code = ERROR_MARK;
-  pop_maybe_used (C_TYPE_VARIABLE_SIZE (TREE_TYPE (expr.value)));
+  if (expr.value == error_mark_node)
+    {
+      ret.value = error_mark_node;
+      ret.original_code = ERROR_MARK;
+      pop_maybe_used (false);
+    }
+  else
+    {
+      ret.value = c_sizeof (TREE_TYPE (expr.value));
+      ret.original_code = ERROR_MARK;
+      pop_maybe_used (C_TYPE_VARIABLE_SIZE (TREE_TYPE (expr.value)));
+    }
   return ret;
 }
 
