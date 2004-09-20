ae02be01e3f10620f4f0883457229663660af584
dorit <dorit@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-20 07:17:06 +0000

    2004-09-20  Ira Rosen  <irar@il.ibm.com>
            * gcc.dg/vect/vect-13.c: Added xfail for non ppc platforms.
            * gcc.dg/vect/vect-74.c: Split into 3 tests (vect-79.c, vect-80.c).
            * gcc.dg/vect/vect-62.c: New testcase.
            * gcc.dg/vect/vect-63.c: New testcase.
            * gcc.dg/vect/vect-64.c: New testcase.
            * gcc.dg/vect/vect-65.c: New testcase.
            * gcc.dg/vect/vect-66.c: New testcase.
            * gcc.dg/vect/vect-67.c: New testcase.
            * gcc.dg/vect/vect-68.c: New testcase.
            * gcc.dg/vect/vect-69.c: New testcase.
            * gcc.dg/vect/vect-79.c: New testcase.
            * gcc.dg/vect/vect-80.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@87745 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/vect-63.c
@@ -0,0 +1,47 @@
+/* { dg-do run { target powerpc*-*-* } } */
+/* { dg-do run { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -ftree-vectorize -fdump-tree-vect-stats -maltivec" { target powerpc*-*-* } } */
+/* { dg-options "-O2 -ftree-vectorize -fdump-tree-vect-stats -msse2" { target i?86-*-* x86_64-*-* } } */
+
+#include <stdarg.h>
+#include "tree-vect.h"
+
+#define N 16
+
+int main1 ()
+{
+  int i, j;
+  int ib[N] = {0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45};
+  int ia[N*2][4][N];
+
+  /* Multidimensional array. Aligned. 
+     The first dimension depends on j: not vectorizable. */
+  for (i = 0; i < N; i++)
+    {
+      for (j = 0; j < N; j++)
+        {
+           ia[i + j][1][j] = ib[i];
+        }
+    }
+
+  /* check results: */  
+  for (i = 0; i < N; i++)
+    {
+      for (j = 0; j < N; j++)
+        {
+           if (ia[i + j][1][j] != ib[i])
+              abort();
+        }
+    }
+
+  return 0;
+}
+
+int main (void)
+{ 
+  check_vect ();
+
+  return main1 ();
+}
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { xfail *-*-* } } } */
--- gcc/testsuite/gcc.dg/vect/vect-79.c
+++ gcc/testsuite/gcc.dg/vect/vect-79.c
@@ -14,8 +14,9 @@ afloat a[N];
 afloat b[N+4] = {0.2, 1.3, 2.3, 3.4, 4.5, 5.6, 7.8, 9.0, 10.11, 11.12, 12.13, 13.14, 14.15, 15.16, 16.17, 17.18, 18.19, 19.20}; 
 afloat c[N] = {0.2, 1.3, 2.3, 3.4, 4.5, 5.6, 7.8, 9.0, 10.11, 11.12, 12.13, 13.14, 14.15, 15.16};
 
+/* Not vectorizable. Alias. */
 int
-main1 (afloat *__restrict__  pa, afloat * __restrict__ pb, afloat * __restrict__ pc)
+main2 (afloat *pa, afloat *pb, afloat *pc)
 {
   int i;
   afloat *q = pb + 4;
@@ -34,13 +35,14 @@ main1 (afloat *__restrict__  pa, afloat * __restrict__ pb, afloat * __restrict__
   return 0;
 }
 
+
 int main (void)
 { 
   check_vect ();
 
-  main1 (a, b, c);
+  main2 (a, b, c);
 
   return 0;	
 }
 
-/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect"  { xfail *-*-* } } } */
