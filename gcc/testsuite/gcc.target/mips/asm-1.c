073cbad3bb0ebe7a29e11acc51d08543f5b1dbd7
Richard Sandiford <rsandifo@redhat.com>
2004-09-20 06:54:52 +0000

    re PR target/17565 ([3.3 only] asms in delay slots)
    	PR target/17565
    	* config/mips/mips.md (define_asm_attributes): Set can_delay to no.
    
    From-SVN: r87744

---
+++ gcc/testsuite/gcc.target/mips/asm-1.c
@@ -0,0 +1,14 @@
+/* PR target/17565.  GCC used to put the asm into the delay slot
+   of the call.  */
+/* { dg-do assemble } */
+/* { dg-options "-O" } */
+int foo (int n)
+{
+  register int k asm ("$16") = n;
+  if (k > 0)
+    {
+      bar ();
+      asm ("li %0,0x12345678" : "=r" (k));
+    }
+  return k;
+}
