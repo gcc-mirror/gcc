2c67c2b3205659036e28c2e6518301db1025b0b6
tobi <tobi@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-20 11:09:47 +0000

    PR fortran/15750 * gfortran.fortran-torture/execute/iolength_2.f90: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@87752 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.fortran-torture/execute/iolength_2.f90
@@ -0,0 +1,24 @@
+! Test that IOLENGTH works for derived types containing arrays
+module iolength_2_mod
+  integer, parameter ::  &
+       ! 32 bit, i.e. 4 byte integer (every gcc architecture should have this?)
+       int32 = selected_int_kind(9), &
+       ! IEEE double precision, i.e. 8 bytes
+       dp = selected_real_kind(15, 307)
+  type foo
+     ! This type should take up 5*4+4+8=32 bytes
+     integer(int32) :: a(5), b
+     real(dp) :: c
+  end type foo
+end module iolength_2_mod
+
+program iolength_2
+  use iolength_2_mod
+  implicit none
+  integer :: iol
+  type(foo) :: d
+  inquire (iolength = iol) d
+  if ( 32 /= iol) then
+     call abort
+  end if
+end program iolength_2
