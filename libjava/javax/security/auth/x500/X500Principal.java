936f1f064115f94425c521a5c94f0957f67a480d
Andreas Tobler <andreast@gcc.gnu.org>
2004-09-21 13:25:26 +0200

    [multiple changes]
    2004-09-21  Andreas Tobler  <a.tobler@schweiz.ch>
    
    	* javax/security/auth/x500/X500Principal.java: Fix some merge glitches.
    
    2004-09-21  Michael Koch  <konqueror@gmx.de>
    
    	* javax/net/ssl/SSLServerSocket.java:
    	Removed comment about "Do not edit this file, it's generated.".
    
    From-SVN: r87800

--- libjava/javax/security/auth/x500/X500Principal.java
+++ libjava/javax/security/auth/x500/X500Principal.java
@@ -7,7 +7,7 @@ GNU Classpath is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2, or (at your option)
 any later version.
- 
+
 GNU Classpath is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -121,7 +121,7 @@ public final class X500Principal implements Principal, Serializable
         iae.initCause (ioe);
         throw iae;
       }
-      }
+  }
 
   public X500Principal (byte[] encoded)
   {
