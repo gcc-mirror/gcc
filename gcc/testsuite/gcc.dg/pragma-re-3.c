f3e217918794e816bcd53ca70c55d62b821b8b48
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-22 02:24:06 +0000

    * c-decl.c (implicitly_declare): Call maybe_apply_renaming_pragma. 	(finish_decl): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@87840 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pragma-re-3.c
@@ -0,0 +1,18 @@
+/* { dg-do link { target *-*-solaris* } } */
+
+#pragma redefine_extname f1 f
+#pragma redefine_extname g1 g
+
+void f() {
+  extern int f1();
+  f1();
+}
+
+void g() {
+  g1();
+}
+
+int main () {
+  f();
+  g();
+}
