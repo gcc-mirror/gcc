b28b16008e1771360fa4419827ce110159e3949a
Jakub Jelinek <jakub@gcc.gnu.org>
2004-09-23 23:01:02 +0200

    tree-flow.h (find_new_referenced_vars): Add prototype.
    2004-09-23  Diego Novillo  <dnovillo@redhat.com>
    	    Jakub Jelinek  <jakub@redhat.com>
    
    	* tree-flow.h (find_new_referenced_vars): Add prototype.
    	* tree-sra.c (find_new_referenced_vars_1, find_new_referenced_vars):
    	Move to...
    	* tree-dfa.c (find_new_referenced_vars_1, find_new_referenced_vars):
    	... here.
    	(mark_new_vars_to_rename): Walk through all operands.
    	* tree-ssa-ccp.c (convert_to_gimple_builtin): New function.
    	(execute_fold_all_builtins): Use it.
    	(pass_fold_builtins): Add TODO_rename_vars to todo_flags_finish.
    
    	* gcc.c-torture/execute/builtins/strcpy-2.c: New test.
    	* gcc.c-torture/execute/builtins/strcpy-2-lib.c: New.
    
    From-SVN: r87983

--- gcc/tree-flow.h
+++ gcc/tree-flow.h
@@ -541,6 +541,8 @@ extern void free_df_for_stmt (tree);
 extern tree get_virtual_var (tree);
 extern void add_referenced_tmp_var (tree var);
 extern void mark_new_vars_to_rename (tree, bitmap);
+extern void find_new_referenced_vars (tree *);
+
 extern void redirect_immediate_uses (tree, tree);
 extern tree make_rename_temp (tree, const char *);
 
