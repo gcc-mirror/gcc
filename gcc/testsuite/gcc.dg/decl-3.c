085e33aa0ad674c2680e6216b7d84737b935351c
Joseph Myers <jsm@polyomino.org.uk>
2004-09-24 18:22:20 +0100

    re PR c/17188 (struct Foo { } redefinition)
    	PR c/17188
    	* c-decl.c (diagnose_mismatched_decls): Check for duplicate
    	declarations of enumerators.
    	(start_struct): Check TYPE_SIZE rather than TYPE_FIELDS to check
    	for redefinition.  Check for nested redefinition.
    	(finish_struct): Don't check for nested redefinition.
    	(start_enum): Check for nested redefinition.
    
    testsuite:
    	* gcc.dg/nested-redef-1.c, gcc.dg/pr17188-1.c: New tests.
    	* gcc.dg/decl-3.c: Adjust expected message.
    
    From-SVN: r88063

--- gcc/testsuite/gcc.dg/decl-3.c
+++ gcc/testsuite/gcc.dg/decl-3.c
@@ -2,4 +2,4 @@
 /* { dg-do compile } */
 
 enum { CODES }; /* { dg-error "previous definition" } */
-enum { CODES }; /* { dg-error "conflicting types" } */
+enum { CODES }; /* { dg-error "conflicting types|redeclaration of enumerator" } */
