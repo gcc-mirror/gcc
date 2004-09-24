5d865dace2bb4f577bbd3a9b640cf5a55788c153
Eric Botcazou <ebotcazou@libertysurf.fr>
2004-09-24 08:17:07 +0200

    config.gcc (sparc64-*-solaris2*): Include sparc/sol2-gas.h if the GNU assembler is used.
    	* config.gcc (sparc64-*-solaris2*): Include sparc/sol2-gas.h
    	if the GNU assembler is used.
    	(sparc-*-solaris2*): Likewise.
    	* dbxout.c (NO_DBX_BNSYM_ENSYM): Default to zero.
    	(dbxout_function_end): Protect N_ENSYM with it.
    	(dbxout_begin_prologue): Protect N_BNSYM with it.
    	* doc/tm.texi (NO_DBX_BNSYM_ENSYM): Document it.
    	* config/sparc/sol2.h (NO_DBX_BNSYM_ENSYM): Define to 1.
    	* config/sparc/sol2-gas.h: New file.
    
    From-SVN: r88013

---
+++ gcc/config/sparc/sol2-gas.h
@@ -0,0 +1,5 @@
+/* Definitions of target machine for GCC, for SPARC running Solaris 2
+   using the GNU assembler.  */
+
+/* Undefine this so that BNSYM/ENSYM pairs are emitted by STABS+.  */
+#undef NO_DBX_BNSYM_ENSYM
