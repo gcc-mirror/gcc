aedd07a7130263ca331092f00ae6248017b69f2a
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-26 14:58:34 +0000

    PR middle-end/17112 	* stor-layout.c (compute_record_mode): For records with a single 	field, only use the field's mode if its size matches what we'd 	have choosen for the record ourselves.  This forces the use of 	BLKmode for packed records that don't completely fill a mode.
    	* gcc.dg/pr17112-1.c: New test case.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@88130 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr17112-1.c
@@ -0,0 +1,32 @@
+/* PR middle-end/17112 */
+/* { dg-do run } */
+/* { dg-options "-O2" } */
+
+extern void abort(void);
+
+typedef struct {
+  int int24:24  __attribute__ ((packed));
+} myint24;
+
+myint24 x[3] = {
+  0x123456,
+  0x789abc,
+  0xdef012
+};
+
+myint24 y[3];  // starts out as zeros
+
+void foo()
+{
+  y[1] = x[1];
+}
+
+int main()
+{
+  foo();
+
+  if (y[0].int24 != 0 || y[2].int24 != 0)
+    abort();
+  return 0;
+}
+
