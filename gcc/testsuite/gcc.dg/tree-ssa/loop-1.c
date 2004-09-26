f4dbad2364543b9e23f3a4fc795db2311e5fdf89
Ulrich Weigand <uweigand@de.ibm.com>
2004-09-26 22:31:26 +0000

    * gcc.dg/tree-ssa/loop-1.c: Use -msmall-exec on s390-*-* targets.
    From-SVN: r88151

--- gcc/testsuite/gcc.dg/tree-ssa/loop-1.c
+++ gcc/testsuite/gcc.dg/tree-ssa/loop-1.c
@@ -1,6 +1,11 @@
 /* { dg-do compile } */
 /* { dg-options "-O1 -ftree-loop-ivcanon -funroll-loops -fdump-tree-ivcanon-details" } */
 
+/* On 31-bit S/390 the function address will be stored (once) in the literal pool,
+   so scan-assembler-times "foo" will return 1 even if the loop is fully unrolled.
+   -msmall-exec avoids this by enabling a call instruction with immediate operand.  */
+/* { dg-options "-O1 -ftree-loop-ivcanon -funroll-loops -fdump-tree-ivcanon-details -msmall-exec" { target s390-*-* } } */
+
 void xxx(void)
 {
   int x = 45;
