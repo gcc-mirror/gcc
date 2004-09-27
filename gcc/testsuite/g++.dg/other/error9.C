c81f61b2506d111b638e6aa329f87f78a289e379
Nathan Sidwell <nathan@codesourcery.com>
2004-09-27 12:10:16 +0000

    re PR c++/17681 (Bad message about constructor ordering (-Wall))
    cp:
    	PR c++/17681
    	* error.c (dump_type): Change TREE_VEC case into TREE_BINFO.
    testsuite:
    	PR c++/17681
    	* g++.dg/other/error9.C: New test.
    
    From-SVN: r88170

---
+++ gcc/testsuite/g++.dg/other/error9.C
@@ -0,0 +1,20 @@
+// { dg-options -Wall }
+
+// Copyright (C) 2004 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 27 Sept 2004 <nathan@codesourcery.com>
+
+// Origin:   	 v.haisman@sh.cvut.cz
+// Bug 17681: bad diagnostic text.
+
+struct A
+{ };
+
+struct B
+{ };
+
+struct C : public B, public A
+{
+  C ()  // { dg-warning "when initialized" "" }
+    : A(), B()  // { dg-warning "base .\[AB\]." "" }
+  { }
+};
