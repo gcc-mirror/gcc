06450f587e08804bbb126066c1458f8965e154f7
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-28 08:58:35 +0000

    2004-09-28  Paolo Carlini  <pcarlini@suse.de>
    	PR libstdc++/16612
    	* include/bits/basic_string.h (_M_dispose, _M_refcopy,
    	basic_string()): When _GLIBCXX_FULLY_DYNAMIC_STRING is defined,
    	don't deal with _S_empty_rep.
    	* include/bits/basic_string.tcc (_S_construct, _M_destroy,
    	_M_leak_hard, _M_mutate): Likewise.
    	* acinclude.m4 (GLIBCXX_ENABLE_FULLY_DYNAMIC_STRING): New.
    	* acconfig.h: Add corresponding undef.
    	* configure.ac: Use GLIBCXX_ENABLE_FULLY_DYNAMIC_STRING.
    	* docs/html/configopts.html: Document --enable-fully-dynamic-string.
    	* configure: Regenerate.
    	* config.h.in: Likewise.
    
    2004-09-28  Benjamin Kosnik  <bkoz@redhat.com>
                Paolo Carlini  <pcarlini@suse.de>
    
    	* testsuite/21_strings/basic_string/operations/char/1.cc: New.
    	* testsuite/21_strings/basic_string/operations/wchar_t/1.cc: New.
    	* testsuite/21_strings/basic_string/element_access/char/empty.cc: New.
    	* testsuite/21_strings/basic_string/element_access/wchar_t/empty.cc:
    	New.
    
    2004-09-28  Paolo Carlini  <pcarlini@suse.de>
    
    	* README: Remove obsolete entry about include/c_shadow.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@88225 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/21_strings/basic_string/operations/char/1.cc
@@ -0,0 +1,42 @@
+// Copyright (C) 2004 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 21.3.6 string operations
+
+#include <string>
+#include <testsuite_hooks.h>
+
+int test01(void)
+{
+  bool test __attribute__((unused)) = true;
+
+  std::string empty;
+
+  // data() for size == 0 is non-NULL.
+  VERIFY( empty.size() == 0 );
+  const std::string::value_type* p = empty.data();
+  VERIFY( p != NULL );
+
+  return 0;
+}
+
+int main()
+{ 
+  test01();
+  return 0;
+}
