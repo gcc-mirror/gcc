c35f61cb2e04c68afa6d063d2fcb6cf16672c9f1
Richard Henderson <rth@redhat.com>
2004-09-30 12:33:48 -0700

    * config/alpha/qrnnd.asm: Mark for noexecstack.
    From-SVN: r88350

--- gcc/config/alpha/qrnnd.asm
+++ gcc/config/alpha/qrnnd.asm
@@ -26,6 +26,10 @@
  # Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
  # MA 02111-1307, USA.
 
+#ifdef __ELF__
+.section .note.GNU-stack,""
+#endif
+
         .set noreorder
         .set noat
 
