4efeac21f40dc86db94dc16c776405c832b73417
uweigand <uweigand@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-02 04:19:16 +0000

    * gcc.dg/tree-ssa/loop-1.c: Merge changed dg-options for s390-*-*.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@88420 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/loop-1.c
+++ gcc/testsuite/gcc.dg/tree-ssa/loop-1.c
@@ -4,7 +4,7 @@
 /* On 31-bit S/390 the function address will be stored (once) in the literal pool,
    so scan-assembler-times "foo" will return 1 even if the loop is fully unrolled.
    -msmall-exec avoids this by enabling a call instruction with immediate operand.  */
-/* { dg-options "-O1 -ftree-loop-ivcanon -funroll-loops -fdump-tree-ivcanon-details -msmall-exec" { target s390-*-* } } */
+/* { dg-options "-O1 -ftree-loop-ivcanon -funroll-loops -fdump-tree-ivcanon-details -fdump-tree-cunroll-details -fdump-tree-vars -msmall-exec" { target s390-*-* } } */
 
 void xxx(void)
 {
