a8a21067d8f6737b9b30fbef8a5906427e874835
No Author <no-author@gcc.gnu.org>
2004-10-02 08:57:09 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_4-branch'.
    From-SVN: r88422

---
+++ libstdc++-v3/testsuite/27_io/basic_stringbuf/seekoff/char/10975.cc
@@ -0,0 +1,57 @@
+// 2004-09-30  Paolo Carlini  <pcarlini@suse.de>
+
+// Copyright (C) 2004 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 27.7.1.3 Overridden virtual functions
+
+#include <sstream>
+#include <testsuite_hooks.h>
+
+// libstdc++/10975
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace std;
+  typedef streambuf::pos_type pos_type;
+  typedef streambuf::off_type off_type;
+
+  const pos_type good = pos_type(off_type(0));
+  const pos_type bad = pos_type(off_type(-1));
+  pos_type p;
+
+  stringbuf sbuf;
+  
+  p = sbuf.pubseekoff(0, ios_base::cur, ios_base::in);
+  VERIFY( p == good );
+
+  p = sbuf.pubseekoff(0, ios_base::beg, ios_base::out);
+  VERIFY( p == good );
+
+  p = sbuf.pubseekoff(0, ios_base::end);
+  VERIFY( p == good );
+
+  p = sbuf.pubseekoff(0, ios_base::cur);
+  VERIFY( p == bad );
+}
+
+int main() 
+{
+  test01();
+  return 0;
+}
