dbb4f0b5d591db0213bb22f1dddbecfc212d76b2
amodra <amodra@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-03 03:31:38 +0000

    * config/rs6000/linux.h (TARGET_C99_FUNCTIONS): Define. 	(OS_MISSING_POWERPC64): Move, and comment. 	* config/rs6000/linux64.h (TARGET_C99_FUNCTIONS): Define. 	(OS_MISSING_POWERPC64): Move, and comment.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@88440 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/rs6000/linux64.h
+++ gcc/config/rs6000/linux64.h
@@ -287,6 +287,14 @@ extern int dot_symbols;
 #undef MD_EXEC_PREFIX
 #undef MD_STARTFILE_PREFIX
 
+/* Linux doesn't support saving and restoring 64-bit regs in a 32-bit
+   process.  */
+#define OS_MISSING_POWERPC64 !TARGET_64BIT
+
+/* glibc has float and long double forms of math functions.  */
+#undef  TARGET_C99_FUNCTIONS
+#define TARGET_C99_FUNCTIONS 1
+
 #undef  TARGET_OS_CPP_BUILTINS
 #define TARGET_OS_CPP_BUILTINS()            		\
   do							\
@@ -559,5 +567,3 @@ while (0)
 #endif
 
 #define MD_UNWIND_SUPPORT "config/rs6000/linux-unwind.h"
-
-#define OS_MISSING_POWERPC64 !TARGET_64BIT
