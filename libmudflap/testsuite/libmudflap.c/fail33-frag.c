a269bb719d2654b21c8f2fa75ce870b872de68e5
fche <fche@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-03 21:17:29 +0000

    * fixing typos in accidental checkin
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@88463 138bc75d-0d04-0410-961f-82ee72b054a4

--- libmudflap/testsuite/libmudflap.c/fail33-frag.c
+++ libmudflap/testsuite/libmudflap.c/fail33-frag.c
@@ -7,14 +7,14 @@ char a[SIZE];
 
 int main ()
 {
-  int i, j=0;
+  int i, j=0, k;
   int a_before_b = (& a[0] < & b[0]);
   /* Rather than iterating linearly, which would allow loop unrolling
      and mapping to pointer manipulation, we traverse the "joined"
      arrays in some random order.  */
   for (i=0; i<SIZE*2; i++)
     {
-      k=rand()%(SIZE*2))
+      k = rand() % (SIZE*2);
       j += (a_before_b ? a[k] : b[k]);
     }
   return j;
