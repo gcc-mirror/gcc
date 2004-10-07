add2402ef09ed864788f5501ad8bfc54fd48913b
Geoffrey Keating <geoffk@apple.com>
2004-10-07 21:16:28 +0000

    Radar 3813796
    2004-10-07  Geoffrey Keating  <geoffk@apple.com>
    
    	Radar 3813796
    	* config/rs6000/rs6000.c (rs6000_generate_compare): When
    	flag_trapping_math is in effect, don't generate subtract
    	instructions.
    
    Index: testsuite/ChangeLog
    2004-10-07  Geoffrey Keating  <geoffk@apple.com>
    
    	* gcc.dg/ppc-fsel-3.c: New file.
    	* gcc.dg/ppc-fsel-1.c: Add -fno-trapping-math, update comment.
    
    From-SVN: r88707

--- gcc/testsuite/gcc.dg/ppc-fsel-1.c
+++ gcc/testsuite/gcc.dg/ppc-fsel-1.c
@@ -1,8 +1,8 @@
 /* { dg-do compile { target powerpc*-*-* } } */
-/* { dg-options "-O -mpowerpc-gfxopt" } */
+/* { dg-options "-O -mpowerpc-gfxopt -fno-trapping-math" } */
 /* { dg-final { scan-assembler "fsel" } } */
 
-/* Check that fsel can be generated even without -ffast-math.  */
+/* If the user doesn't care about signals, fsel can be used in many cases.  */
 
 double foo(double a, double b, double c, double d)
 {
