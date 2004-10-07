d0e75c71c9a2586b0de06873febcdddd0eb7f01b
Paul Brook <paul@codesourcery.com>
2004-10-07 17:45:40 +0000

    * gfortran.dg/empty_format_1.f90: Remove stray commas.
    From-SVN: r88696

--- gcc/testsuite/gfortran.dg/empty_format_1.f90
+++ gcc/testsuite/gfortran.dg/empty_format_1.f90
@@ -7,9 +7,9 @@ program main
   character*20 str
   io_unit = 10
   open (unit=io_unit,status='scratch',form='formatted')
-  write (io_unit, '(A)'), "Line1"
-  write (io_unit, '(A)'), "Line2"
-  write (io_unit, '(A)'), "Line3"
+  write (io_unit, '(A)') "Line1"
+  write (io_unit, '(A)') "Line2"
+  write (io_unit, '(A)') "Line3"
   rewind (io_unit)
   read (io_unit,'(A)') str
   if (str .ne. "Line1") call abort
