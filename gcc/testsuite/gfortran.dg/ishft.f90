4d6b4af65ce0546fcdb9d0b96f9ab18c0c805b40
tobi <tobi@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-07 15:12:06 +0000

    fortran/ * primary.c (match_boz_constant): Allow kind parameter suffixes. Move standard warning further to the front.
    testsuite/
    * gfortran.fortran-torture/execute/intrinsic_mvbits.f90,
    gfortran.dg/ishft.f90: Add more tests.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@88690 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/ishft.f90
+++ gcc/testsuite/gfortran.dg/ishft.f90
@@ -5,7 +5,7 @@ if (ishft (1_1, 1) /= 2) call abort
 if (ishft (3_1, 1) /= 6) call abort
 if (ishft (-1_1, 1) /= -2) call abort
 if (ishft (-1_1, -1) /= 127) call abort
-if (ishft (96_1, 2) /= -128_2) call abort
+if (ishft (96_1, 2) /= -128) call abort
 
 if (ishft (1_2, 0) /= 1) call abort
 if (ishft (1_2, 1) /= 2) call abort
@@ -20,6 +20,12 @@ if (ishft (3_4, 1) /= 6) call abort
 if (ishft (-1_4, 1) /= -2) call abort
 if (ishft (-1_4, -1) /= 2147483647) call abort
 if (ishft (1073741824_4 + 536870912_4, 2) /= -2147483648_8) call abort
+
+if (ishft (1_8, 0) /= 1) call abort
+if (ishft (1_8, 1) /= 2) call abort
+if (ishft (3_8, 1) /= 6) call abort
+if (ishft (-1_8, 1) /= -2) call abort
+if (ishft (-1_8, -60) /= z'F'_8) call abort
 
 if (ishftc (1_1, 0) /= 1) call abort
 if (ishftc (1_1, 1) /= 2) call abort
@@ -41,4 +47,13 @@ if (ishftc (3_4, 1) /= 6) call abort
 if (ishftc (-1_4, 1) /= -1) call abort
 if (ishftc (-1_4, -1) /= -1) call abort
 if (ishftc (ishftc (1325876_4, 2), -2) /= 1325876) call abort
+
+if (ishftc (1_8, 0) /= 1) call abort
+if (ishftc (1_8, 1) /= 2) call abort
+if (ishftc (3_8, 1) /= 6) call abort
+if (ishftc (-1_8, 1) /= -1) call abort
+if (ishftc (-1_8, -1) /= -1) call abort
+if (ishftc (ishftc (1325876_8, 2), -2) /= 1325876) call abort
 end
+
+
