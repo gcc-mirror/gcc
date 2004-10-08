3589783147494a0b186d0dfdc20559827da39f49
Andrew Pinski <pinskia@physics.uc.edu>
2004-10-08 23:29:35 +0000

    re PR tree-optimization/17902 (ICE from tree_verify_flow_info)
    2004-10-08  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR tree-opt/17902
            * g++.dg/opt/pr17902.C: New test.
    
    From-SVN: r88802

---
+++ gcc/testsuite/g++.dg/opt/pr17902.C
@@ -0,0 +1,26 @@
+/* { dg-options "-O3" } */
+/* { dg-do compiler } */
+
+void foo();
+struct A { ~A(){ foo(); } };
+struct B { A a; };
+void bar()
+{
+    A a;
+    bool b = false;
+    int i, j;
+
+
+    for (j=0; j<i; j++)
+    {
+        if (i) b=true;
+        if (j && i) foo();
+        if (j && i) i++;
+    }
+
+
+    for (j=0; j<i; j++)
+        if ( !j || (j==1 && b && i) )
+            B x;
+}
+
