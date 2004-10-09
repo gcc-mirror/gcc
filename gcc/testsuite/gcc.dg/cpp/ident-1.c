d994737160896286d4eba36b2ffbe43a0c568e79
No Author <no-author@gcc.gnu.org>
2004-10-09 00:25:11 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_4-branch'.
    From-SVN: r88807

---
+++ gcc/testsuite/gcc.dg/cpp/ident-1.c
@@ -0,0 +1,8 @@
+/* { dg-do preprocess } */
+/* { dg-options "" } */ /* shut off -pedantic */
+
+/* Based on PR 16999 */
+
+#ident "this is an ident"
+
+/* { dg-final { scan-file "ident-1.i" "(^|\\n)#ident \"this is an ident\"($|\\n)" } } */
