7731405b8d365cbb97512a9e4010f2ec20e289a2
Andreas Jaeger <aj@suse.de>
2004-10-11 07:19:48 +0200

    makedepend.c: Include mkdeps.h for prototype of deps_add_vpath.
    	* makedepend.c: Include mkdeps.h for prototype of deps_add_vpath.
    	* Makefile.in (makedepend.o): Add dependency on mkdeps.h.
    
    From-SVN: r88870

--- libcpp/makedepend.c
+++ libcpp/makedepend.c
@@ -25,6 +25,7 @@ Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include "line-map.h"
 #include "cpplib.h"
 #include "getopt.h"
+#include "mkdeps.h"
 
 const char *progname;
 const char *vpath;
