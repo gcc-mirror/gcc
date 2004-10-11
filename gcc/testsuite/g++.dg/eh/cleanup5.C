56dfff01254b52777564235746dabcdee1465c12
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-11 03:16:47 +0000

    2004-10-10  Andrew Pinski  <pinskia@physics.uc.edu>
            PR c++/17907
            * semantics.c (add_decl_expr): If the decl has a size which
            has side effects then the decl expression needs a cleanup point.
    2004-10-10  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR c++/17907
            * g++.dg/eh/cleanup5.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@88867 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/eh/cleanup5.C
@@ -0,0 +1,16 @@
+// PR 17907
+// { dg-do compile }
+// We lost a CLEANUP_POINT_EXPR, leading to a crash destroying temp of A.
+
+
+struct String {
+  ~String();
+  int size() const;
+};
+struct CodingSystem {
+  String convertOut() const;
+};
+void inputOpened(CodingSystem *outputCodingSystem)
+{
+   char filePath[outputCodingSystem->convertOut().size()];
+}
