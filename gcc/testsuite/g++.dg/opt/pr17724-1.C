1bc1f3f6618aa4afb6067a189235ea8d32fc9847
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-13 11:27:01 +0000

    PR tree-optimization/17724 	* tree-cfg.c (tree_purge_dead_eh_edges): Free dominance info.
    	* g++.dg/opt/pr17724-1.C: New test.
    	* g++.dg/opt/pr17724-2.C: New test.
    	* g++.dg/opt/pr17724-3.C: New test.
    	* g++.dg/opt/pr17724-4.C: New test.
    	* g++.dg/opt/pr17724-5.C: New test.
    	* g++.dg/opt/pr17724-6.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@88979 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/opt/pr17724-1.C
@@ -0,0 +1,23 @@
+// PR tree-optimization/17724
+// { dg-do compile }
+// { dg-options "-O2" }
+
+namespace N { char *strcpy (char *, const char *); }
+extern "C" char *strcpy (char *, const char *) throw ();
+inline char *N::strcpy (char *s, const char *t) { return ::strcpy (s, t); }
+
+struct S { ~S (); };
+int foo ();
+
+int
+main ()
+{
+  S s;
+  int a;
+  char b[64];
+  N::strcpy (b, "ABCDEFGHIJKLM");
+  while ((a = foo ()) != -1)
+    if (a)
+      return -1;
+  return 0;
+}
