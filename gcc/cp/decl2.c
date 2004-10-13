e68ba323fd543681236c6e56702da2e290d30c87
fche <fche@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-13 18:18:18 +0000

    2004-10-13  Frank Ch. Eigler  <fche@redhat.com>
    	* toplev.c (compile_file): Call mudflap_finish_file from here ...
    	* c-decl.c (c_write_global_declarations): ... instead of here ...
    	* cp/decl.c (cp_finish_file): ... and here.
    	* tree-mudflap.c (mudflap_enqueue_decl): Reword a warning message.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@88995 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/decl2.c
+++ gcc/cp/decl2.c
@@ -3066,11 +3066,6 @@ cp_finish_file (void)
   cgraph_finalize_compilation_unit ();
   cgraph_optimize ();
 
-  /* Emit mudflap static registration function.  This must be done
-     after all the user functions have been expanded.  */
-  if (flag_mudflap)
-    mudflap_finish_file ();
-
   /* Now, issue warnings about static, but not defined, functions,
      etc., and emit debugging information.  */
   walk_namespaces (wrapup_globals_for_namespace, /*data=*/&reconsider);
