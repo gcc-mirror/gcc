700b0d81d235a049d7f5a674290d3d6d760e5319
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-14 00:56:22 +0000

    libcpp: 	* errors.c (_cpp_begin_message): Print "error: " for errors.
    gcc/testsuite:
    	* gcc.dg/cpp/error-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@89025 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/cpp/error-1.c
@@ -0,0 +1,5 @@
+/* Test that error messages include "error:" in the text.  */
+/* Origin: Joseph Myers <jsm@polyomino.org.uk> */
+/* { dg-do preprocess } */
+
+/* { dg-error "error: unterminated comment" }
