19702c1d34daddd799073407407ff47a44d0084c
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-14 00:34:01 +0000

    * c-tree.h (enum c_typespec_kind, struct c_typespec, 	parser_xref_tag): New. 	(struct c_declspecs): Add tag_defined_p.  Adjust definition of 	typedef_p. 	(declspecs_add_type): Adjust prototypes. 	* c-parse.in (%union): Add tstype. 	(typespec_nonattr, typespec_attr, typespec_reserved_nonattr, 	typespec_reserved_attr, typespec_nonreserved_nonattr, 	structsp_attr, structsp_nonattr): Change to tstype.  Update 	actions. 	* c-decl.c (build_null_declspecs): Initialize tag_defined_p. 	(declspecs_add_type): Update to take struct c_typespec argument. 	Set tag_defined_p and typedef_p as appropriate. 	(xref_tag): Rename to parser_xref_tag and replace by wrapper. 	Update to return struct c_typespec. 	(shadow_tag_warned): Don't let empty declarations with qualifiers 	or storage class specifiers redeclare a tag if a previous 	declaration is visible.
    testsuite:
    	* gcc.dg/c99-tag-3.c, gcc.dg/declspec-14.c: New tests.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@89021 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/declspec-14.c
@@ -0,0 +1,11 @@
+/* Test that "typeof(struct foo)" and similar as declaration
+   specifiers act like typedef.  */
+/* Origin: Joseph Myers <jsm@polyomino.org.uk> */
+/* { dg-do compile } */
+/* { dg-options "" } */
+
+typeof(struct foo); /* { dg-warning "warning: useless type name in empty declaration" } */
+
+struct bar { int a; } x;
+
+typeof(x); /* { dg-warning "warning: useless type name in empty declaration" } */
