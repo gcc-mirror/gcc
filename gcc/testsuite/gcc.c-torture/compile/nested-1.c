f0602b16b700a5fd705646b711b8a563c9c711cf
Andrew Pinski <pinskia@physics.uc.edu>
2004-10-15 13:49:02 +0000

    re PR middle-end/16567 (Nested function and variable-sized structure ICE)
    2004-10-14  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR middle-end/16567
            * gcc.c-torture/compile/nested-1.c: New test.
    
    From-SVN: r89092

---
+++ gcc/testsuite/gcc.c-torture/compile/nested-1.c
@@ -0,0 +1,19 @@
+typedef __SIZE_TYPE__ size_t;
+extern int printf (const char *, ...);
+extern void *memset (void *, int, size_t);
+
+int bar (int (*)(), int, void *);
+
+int
+main(int argc, char **argv)
+{
+  struct s { int a; char b[argc]; };
+  int nested (struct s x) { return x.a + sizeof(x); }
+  struct s t;
+  memset (&t, 0, sizeof(t));
+  t.a = 123;
+  printf("%d\n", bar (nested, argc, &t));
+  return 0;
+}
+
+
