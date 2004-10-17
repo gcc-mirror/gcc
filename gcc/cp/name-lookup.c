6ec448f338a32c12b2e8746647457929936071a5
Andrew Pinski <pinskia@physics.uc.edu>
2004-10-17 17:41:00 +0000

    re PR c++/16301 (ICE with strong using)
    2004-10-17  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR c++/16301
            * namelookup.c (parse_using_directive): If we have a
            error_mark_node, do not set the decl namespace associations
            on it.
    
    2004-10-17  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR c++/16301
            * g++.dg/lookup/strong-using-4.C: New test.
    
    From-SVN: r89178

--- gcc/cp/name-lookup.c
+++ gcc/cp/name-lookup.c
@@ -3384,7 +3384,7 @@ parse_using_directive (tree namespace, tree attribs)
 	{
 	  if (!toplevel_bindings_p ())
 	    error ("strong using only meaningful at namespace scope");
-	  else
+	  else if (namespace != error_mark_node)
 	    DECL_NAMESPACE_ASSOCIATIONS (namespace)
 	      = tree_cons (current_namespace, 0,
 			   DECL_NAMESPACE_ASSOCIATIONS (namespace));
