6e7a355c164ab9adb03365f83f864b8e2c201c23
Eric Botcazou <ebotcazou@libertysurf.fr>
2004-10-19 00:45:01 +0200

    re PR middle-end/18045 (signed integer remainder for power of 2 broken)
    	PR middle-end/18045
    	* expmed.c (expand_smod_pow2): Handle modes whose size
    	is greater than that of HOST_WIDE_INT.
    
    From-SVN: r89253

---
+++ gcc/testsuite/gcc.dg/smod-1.c
@@ -0,0 +1,25 @@
+/* PR middle-end/18045 */
+/* Contributed by Eric Botcazou <ebotcazou@libertysurf.fr> */
+
+/* { dg-do run } */
+/* { dg-options "-std=c99" } */
+/* { dg-options "-std=c99 -mtune=i486" { target i?86-*-* x86_64-*-* } } */
+
+#include <limits.h>
+
+extern void abort(void);
+
+long long smod16(long long x)
+{
+  return x % 16;
+}
+
+int main(void)
+{
+#if LLONG_MAX > 2147483647L
+  if (smod16 (0xFFFFFFFF) != 0xF)
+    abort ();
+#endif
+
+  return 0;
+}
