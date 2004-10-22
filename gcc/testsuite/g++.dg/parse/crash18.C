da4e72c7fe35d5895fbc8b90d4c777d1a703143c
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-22 11:42:30 +0000

    cp: 	PR c++/18095 	* parser.c (eof_token): Make const, correctly initialize rid and 	location fields. 	(struct cp_lexer): Replace buffer_end pointer with buffer_length 	count. Adjust. 	(cp_lexer_new_main): Directly grow lexer's buffer here.  Don't 	zero it out. 	(cp_lexer_new_from_tokens): Adjust. 	(cp_lexer_grow_buffer): Remove. 	(cp_lexer_peek_nth_token, cp_lexer_consume_token, 	cp_lexer_purge_token): Add const casts. testsuite: 	PR c++/18095 	* g++.dg/parse/crash18.C: New. 	* g++.dg/parse/crash19.C: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@89439 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/crash18.C
@@ -0,0 +1,7 @@
+// Copyright (C) 2004 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 20 Oct 2004 <nathan@codesourcery.com>
+
+// PR 18095: ICE
+// Origin: Volker Reichelt <reichelt@gcc.gnu.org>
+
+struct A // { dg-error "expected" "" }
