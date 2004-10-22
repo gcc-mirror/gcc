aed305a929bbaff9bed857638b90899e1ec37622
Benjamin Kosnik <bkoz@redhat.com>
2004-10-22 05:32:16 +0000

    array (array): Make safe for zero-sized arrays.
    2004-10-21  Benjamin Kosnik  <bkoz@redhat.com>
    
    	* include/tr1/array (array): Make safe for zero-sized arrays.
    	(array::end): Return one past the end.
    	(array::at): Use __throw_out_of_range, include functexcept.h.
    	(operator==): Implement.
    	(operator!=): Same.
    	(operator<): Same.
    	(operator>): Same.
    	(operator>=): Same.
    	(operator<=): Same.
    	* testsuite/tr1/6_containers/array/capacity/(empty.cc,
    	max_size.cc, size.cc): New.
    	* testsuite/tr1/6_containers/array/comparison_operators/(equal.cc,
    	greater.cc, greater_or_equal.cc, less.cc, less_or_equal.cc,
    	not_equal): New.
    	* testsuite/tr1/6_containers/array/cons/aggregate_initialization.cc:
    	New.
    	* testsuite/tr1/6_containers/array/element_access/at_out_of_range.cc:
    	New.
    	* testsuite/tr1/6_containers/array/iterators/end_is_one_past.cc: New.
    	* testsuite/tr1/6_containers/array/requirements/(contiguous.cc,
    	instantiate, typedefs, zero_size_arrays): New.
    
    From-SVN: r89429

---
+++ libstdc++-v3/testsuite/tr1/6_containers/array/iterators/end_is_one_past.cc
@@ -0,0 +1,46 @@
+// 2004-10-20  Benjamin Kosnik  <bkoz@redhat.com>
+//
+// Copyright (C) 2004 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 6.2.2 Class template array
+
+#include <tr1/array>
+#include <stdexcept>
+#include <testsuite_hooks.h>
+
+void
+test01() 
+{ 
+  const size_t len = 5;
+  typedef std::tr1::array<int, len> array_type;
+  bool test __attribute__((unused)) = true;
+  array_type a = { 0, 1, 2, 3, 4 };
+
+  array_type::iterator b = a.begin();
+  array_type::iterator e = a.end();
+
+  VERIFY( e != (b + a.size() - 1));
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
+
