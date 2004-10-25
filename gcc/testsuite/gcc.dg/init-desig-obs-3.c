de296e5aaa7a148c3862edb2b71bfd514e907518
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-25 22:23:08 +0000

    PR c/16667 	* c-parse.in (array_designator): New. 	(designator): Use it. 	(initelt): Only permit array_designator without '=', not ".foo".
    testsuite:
    	* gcc.dg/init-desig-obs-1.c, gcc.dg/init-desig-obs-2.c,
    	gcc.dg/init-desig-obs-3.c: New tests.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@89560 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/init-desig-obs-3.c
@@ -0,0 +1,11 @@
+/* Test obsolete forms of designated initializers.  Test with
+   -pedantic-errors.  */
+/* Origin: Joseph Myers <jsm@polyomino.org.uk> */
+/* { dg-do compile } */
+/* { dg-options "-std=gnu99 -pedantic-errors" } */
+struct s { int a; };
+struct s s0 = { .a = 1 };
+struct s s1 = { a: 1 }; /* { dg-error "error: obsolete use of designated initializer with ':'" } */
+
+int x0[] = { [0] = 1 };
+int x1[] = { [0] 1 }; /* { dg-error "error: obsolete use of designated initializer without '='" } */
