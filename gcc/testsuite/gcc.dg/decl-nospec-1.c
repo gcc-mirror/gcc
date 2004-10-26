663174d3f5b4114e970566789a0a5840642c9ad7
Joseph Myers <jsm@polyomino.org.uk>
2004-10-26 22:01:51 +0100

    c-parse.in (datadef): Use pedwarn rather than error and warning for diagnostic in case of no...
    	* c-parse.in (datadef): Use pedwarn rather than error and warning
    	for diagnostic in case of no declaration specifiers.
    
    testsuite:
    	* gcc.dg/decl-nospec-1.c, gcc.dg/decl-nospec-2.c,
    	gcc.dg/decl-nospec-3.c: New tests.
    
    From-SVN: r89605

---
+++ gcc/testsuite/gcc.dg/decl-nospec-1.c
@@ -0,0 +1,8 @@
+/* Data definition with no type or storage class should receive a
+   pedwarn, rather than a warning which becomes an error with
+   -pedantic.  Test with no options.  */
+/* Origin: Joseph Myers <jsm@polyomino.org.uk> */
+/* { dg-do compile } */
+/* { dg-options "" } */
+
+foo(); /* { dg-warning "warning: data definition has no type or storage class" } */
---
+++ gcc/testsuite/gcc.dg/decl-nospec-3.c
@@ -0,0 +1,8 @@
+/* Data definition with no type or storage class should receive a
+   pedwarn, rather than a warning which becomes an error with
+   -pedantic.  Test with -pedantic-errors.  */
+/* Origin: Joseph Myers <jsm@polyomino.org.uk> */
+/* { dg-do compile } */
+/* { dg-options "-pedantic-errors" } */
+
+foo(); /* { dg-error "error: data definition has no type or storage class" } */
