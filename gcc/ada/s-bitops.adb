dafa9f54df92551daf72e7ed8dec5752b47825a8
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-27 13:39:44 +0000

    2004-10-26  Robert Dewar  <dewar@gnat.com>
    	* s-bitops.adb (Bit_Eq): Remove redundant check.
    
    	* s-bitops.ads: Minor comment updates
    	Change some occurrences of Address to System.Address
    
    	* s-carsi8.ads: Fix minor cut-and-paste error in comments
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@89664 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/s-bitops.adb
+++ gcc/ada/s-bitops.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---         Copyright (C) 1996-2002 Free Software Foundation, Inc.           --
+--         Copyright (C) 1996-2004 Free Software Foundation, Inc.           --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -123,10 +123,7 @@ package body System.Bit_Ops is
             Bitc : constant Natural := Llen mod 8;
 
          begin
-            if Llen /= Rlen then
-               return False;
-
-            elsif LeftB (1 .. BLen) /= RightB (1 .. BLen) then
+            if LeftB (1 .. BLen) /= RightB (1 .. BLen) then
                return False;
 
             elsif Bitc /= 0 then
