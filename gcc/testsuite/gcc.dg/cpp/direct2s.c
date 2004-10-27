9b2e15fae11324d3bbfe0ba9587fb471b78638ac
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-27 10:20:55 +0000

    * gcc.dg/cpp/direct2.c: Cope with different bison 'parse' or 	'syntax' error messages. 	* gcc.dg/cpp/direct2s.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@89635 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cpp/direct2s.c
+++ gcc/testsuite/gcc.dg/cpp/direct2s.c
@@ -14,7 +14,7 @@
 
 HASH include "somerandomfile" /*{ dg-error "stray" "non-include" }*/
 /*{ dg-bogus "No such" "don't execute non-include" { target *-*-* } 15 }*/
-int resync_parser_1; /*{ dg-error "parse" "" }*/
+int resync_parser_1; /*{ dg-error "parse|syntax" "" }*/
 
 HASHINCLUDE <somerandomfile> /*{ dg-error "stray" "non-include 2" }*/
 /*{ dg-bogus "No such" "don't execute non-include 2" { target *-*-* } 18 }*/
@@ -22,13 +22,13 @@ int resync_parser_2;
 
 void g1 ()
 {
-HASH define X 1 /* { dg-error "stray|undeclared|parse|for each" "# from macro" } */
+HASH define X 1 /* { dg-error "stray|undeclared|parse|syntax|for each" "# from macro" } */
   int resync_parser_3;
 }
 
 void g2 ()
 {
-HASHDEFINE  Y 1 /* { dg-error "stray|undeclared|parse|for each" "#define from macro" } */
+HASHDEFINE  Y 1 /* { dg-error "stray|undeclared|parse|syntax|for each" "#define from macro" } */
   int resync_parser_4;
 }
 
