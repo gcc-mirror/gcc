03c123a681a8116cafd525c96e723799fb892866
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-28 13:07:00 +0000

    * gcc.dg/i386-rotate-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@89755 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/i386-rotate-1.c
@@ -0,0 +1,16 @@
+/* Verify that rolb instruction is emitted on IA-32/x86-64.  */
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+
+void foo (unsigned char *);
+
+int
+main (void)
+{
+  unsigned char c = 0;
+  foo (&c);
+  c = c >> 1 | c << 7;
+  return c;
+}
+
+/* { dg-final { scan-assembler "rolb" { target i?86-*-* x86_64-*-* } } } */
