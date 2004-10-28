6aa2f2dc70ac5542790255aad076cf6406cb1893
nemet <nemet@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-28 19:31:25 +0000

    PR middle-end/18160 	* gcc.dg/register-var-1.c: New test. 	* gcc.dg/register-var-2.c: New test. 	* gcc.dg/asm-7.c: Taking the address of a register variable is an 	error now.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@89773 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/asm-7.c
+++ gcc/testsuite/gcc.dg/asm-7.c
@@ -9,7 +9,7 @@ void test(void)
   static int m;
   int *p;
 
-  __asm__ ("" : : "m"(r));	/* { dg-warning "address of register" } */
+  __asm__ ("" : : "m"(r));	/* { dg-error "" } */
   __asm__ ("" : : "m"(i));
   __asm__ ("" : : "m"(m));
   __asm__ ("" : : "m"(0));	/* { dg-error "" } */
@@ -22,7 +22,7 @@ void test(void)
   __asm__ ("" : : "g"(0));
   __asm__ ("" : : "g"(i+1));
 
-  __asm__ ("" : "=m"(r2));	/* { dg-warning "address of register" } */
+  __asm__ ("" : "=m"(r2));	/* { dg-error "" } */
   __asm__ ("" : "=m"(i));
   __asm__ ("" : "=m"(m));
 }
---
+++ gcc/testsuite/gcc.dg/register-var-2.c
@@ -0,0 +1,14 @@
+/* PR/18160 */
+
+/* { dg-do compile } */
+
+/* This should yield an error even without -pedantic.  */
+/* { dg-options "-ansi" } */
+
+void g(int *);
+
+void f(void) 
+{ 
+  register int x;
+  g(&x);	/* { dg-error "error: address of register variable" } */
+} 
