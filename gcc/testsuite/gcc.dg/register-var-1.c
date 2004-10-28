fcf750eec2b7e0d938300820a1605501d4b5cfb1
Adam Nemet <anemet@lnxw.com>
2004-10-28 19:31:25 +0000

    re PR middle-end/18160 (ICE on taking register variable address)
    	PR middle-end/18160
    	* gcc.dg/register-var-1.c: New test.
    	* gcc.dg/register-var-2.c: New test.
    	* gcc.dg/asm-7.c: Taking the address of a register variable is an
    	error now.
    
    From-SVN: r89773

---
+++ gcc/testsuite/gcc.dg/register-var-1.c
@@ -0,0 +1,14 @@
+/* PR/18160 */
+
+/* { dg-do compile { target i?86-*-* } } */
+
+/* This should yield an error even without -pedantic.  */
+/* { dg-options "-ansi" } */
+
+void g(int *);
+
+void f(void) 
+{ 
+  register int x __asm ("eax");
+  g(&x);	/* { dg-error "error: address of register variable" } */
+} 
