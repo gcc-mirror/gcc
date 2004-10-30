82b612f0eeb620ea76d41025f01b0cb2126f3d24
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-30 21:44:04 +0000

    2004-10-30  Paolo Carlini  <pcarlini@suse.de>
    	* include/ext/bitmap_allocator.h (allocate(size_type)): Minor tweak.
    
    	* testsuite/ext/bitmap_allocator/check_delete.cc: New.
    	* testsuite/ext/bitmap_allocator/check_new.cc: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@89899 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/ext/bitmap_allocator/check_delete.cc
@@ -0,0 +1,54 @@
+// Copyright (C) 2004 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 20.4.1.1 allocator members
+
+#include <cstdlib>
+#include <ext/bitmap_allocator.h>
+#include <testsuite_hooks.h>
+#include <testsuite_allocator.h>
+
+using __gnu_cxx::bitmap_allocator;
+
+void* 
+operator new(std::size_t n) throw(std::bad_alloc)
+{
+  new_called = true;
+  return std::malloc(n);
+}
+
+void
+operator delete(void *v) throw()
+{
+  delete_called = true;
+  return std::free(v);
+}
+
+// These just help tracking down error messages.
+void test01() 
+{ 
+  bool test __attribute__((unused)) = true;
+  typedef bitmap_allocator<unsigned int> allocator_type;
+  VERIFY( bool(__gnu_test::check_delete<allocator_type, true>()) ); 
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
