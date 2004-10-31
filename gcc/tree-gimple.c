7870d4f8ba60f20187140e365b88064dd875f874
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-31 09:17:40 +0000

    PR middle-end/17526         * tree-gimple.c (is_gimple_mem_rhs): Also require a val for         aggregate types that are not BLKmode.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@89915 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-gimple.c
+++ gcc/tree-gimple.c
@@ -111,9 +111,12 @@ is_gimple_reg_rhs (tree t)
 bool
 is_gimple_mem_rhs (tree t)
 {
-  /* If we're dealing with a renamable type, either source or dest
-     must be a renamed variable.  */
-  if (is_gimple_reg_type (TREE_TYPE (t)))
+  /* If we're dealing with a renamable type, either source or dest must be
+     a renamed variable.  Also force a temporary if the type doesn't need
+     to be stored in memory, since it's cheap and prevents erroneous
+     tailcalls (PR 17526).  */
+  if (is_gimple_reg_type (TREE_TYPE (t))
+      || TYPE_MODE (TREE_TYPE (t)) != BLKmode)
     return is_gimple_val (t);
   else
     return is_gimple_formal_tmp_rhs (t);
