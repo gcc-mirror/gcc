7236f3f6d8414827d760d167225fd8570da42df6
Richard Sandiford <rsandifo@redhat.com>
2004-11-01 20:10:13 +0000

    mangle6.C: Run for all MIPS targets.
    	* g++.dg/abi/mangle6.C: Run for all MIPS targets.  Provide dummy
    	strings for 32-bit code.
    
    From-SVN: r89957

--- gcc/testsuite/g++.dg/abi/mangle6.C
+++ gcc/testsuite/g++.dg/abi/mangle6.C
@@ -1,6 +1,7 @@
 /* Check that __int128 types are mangled.  */
-/* { dg-do compile { target mips64*-*-* mipsisa64*-*-* } } */
+/* { dg-do compile { target mips*-*-* } } */
 
+#ifdef __mips64
 typedef int int128 __attribute__ ((mode(TI)));
 typedef unsigned int uint128 __attribute__ ((mode(TI)));
 
@@ -13,6 +14,10 @@ struct S
 
 int128 (S::*ptr1) (int128) const = &S::func1;
 uint128 (S::*ptr2) (uint128) const = &S::func2;
+#else
+const char *str1 = "_ZNK1S5func1En";
+const char *str2 = "_ZNK1S5func2Eo";
+#endif
 
 /* { dg-final { scan-assembler _ZNK1S5func1En } } */
 /* { dg-final { scan-assembler _ZNK1S5func2Eo } } */
