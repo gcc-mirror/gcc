36f4d1449ca5c83321e28cdffaf0182373b1e8d7
Tobias Schlüter <tobias.schlueter@physik.uni-muenchen.de>
2004-11-03 01:54:02 +0100

    re PR fortran/17535 (gfortran with module procedures)
    fortran/
    PR fortran/17535
    PR fortran/17583
    PR fortran/17713
    * module.c (write_symbol1): Set module_name for dummy arguments.
    
    testsuite/
    PR fortran/17535
    PR fortran/17583
    PR fortran/17713
    * gfortran.dg/generic_[123].f90: New testcases.
    
    From-SVN: r90011

---
+++ gcc/testsuite/gfortran.dg/generic_3.f90
@@ -0,0 +1,30 @@
+! { dg-do compile }
+! Testcase from PR 17713
+module fit_functions
+  implicit none
+contains
+  subroutine gauss( x, a, y, dy, ma )
+    double precision, intent(in)     :: x
+    double precision, intent(in)     :: a(:)
+    double precision, intent(out)    :: y
+    double precision, intent(out)    :: dy(:)
+    integer,          intent(in)     :: ma
+  end subroutine gauss
+end module fit_functions
+
+subroutine mrqcof( x, y, sig, ndata, a, ia, ma )
+  use fit_functions
+  
+  implicit none
+  double precision, intent(in)   :: x(:), y(:), sig(:)
+  integer,   intent(in)          :: ndata
+  double precision, intent(in)   :: a(:)
+  integer,   intent(in)          :: ia(:), ma
+  
+  integer                           i
+  double precision                  yan, dyda(ma)
+  
+  do i = 1, ndata
+     call gauss( x(i), a, yan, dyda, ma )
+  end do
+end subroutine mrqcof
