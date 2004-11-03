318627d5fc55180b3bb8619c408799eb14394cc1
Ben Elliston <bje@au.ibm.com>
2004-11-03 22:28:59 +0000

    enum5.C (enum conditions): Move the packed attribute to the definition to satisfy the new C++ parser.
    	* g++.old-deja/g++.other/enum5.C (enum conditions): Move the
    	packed attribute to the definition to satisfy the new C++ parser.
    
    From-SVN: r90039

--- gcc/testsuite/g++.old-deja/g++.other/enum5.C
+++ gcc/testsuite/g++.old-deja/g++.other/enum5.C
@@ -1,14 +1,14 @@
 // { dg-do run }
-// Copyright (C) 2001, 2003 Free Software Foundation, Inc.
+// Copyright (C) 2001, 2003, 2004 Free Software Foundation, Inc.
 // Contributed by Ben Elliston <bje@redhat.com>
 
 // PR 80: Packed enums use minimum required storage.
 
 extern "C" void abort();
 
-enum numbers { one, two, three } __attribute__ ((packed)) nums; // { dg-bogus "" "" { xfail *-*-* } } 
-enum colours { red = 1000, green, blue } __attribute__ ((packed)) cols; // { dg-bogus "" "" { xfail *-*-* } } 
-enum __attribute__ ((packed)) conditions { fine, rain, cloudy } forecast; // { dg-bogus "" "" { xfail *-*-* } } 
+enum numbers { one, two, three } __attribute__ ((packed)) nums;
+enum colours { red = 1000, green, blue } __attribute__ ((packed)) cols;
+enum conditions { fine, rain, cloudy } __attribute__ ((packed)) forecast;
 
 int
 main()
@@ -19,7 +19,7 @@ main()
   if (sizeof (cols) != 2)
     abort ();
 
-  if (sizeof (forecast) != 1) // { dg-bogus "" "" { xfail *-*-* } }
+  if (sizeof (forecast) != 1)
     abort ();
 
   return 0;
