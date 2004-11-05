68a89c3d7c83837135240a33fc739700c0a685fc
No Author <no-author@gcc.gnu.org>
2004-11-05 03:35:02 +0000

    This commit was manufactured by cvs2svn to create tag 'gcc_3_4_3_release'.
    From-SVN: r90112

--- gcc/testsuite/gcc.dg/pr11459-1.c
+++
@@ -1,6 +0,0 @@
-/* -ansi -std=c99 should mean -std=c99, but the specs reordered the
-    options.  Bug 11459.  */
-/* { dg-do compile } */
-/* { dg-options "-ansi -std=c99 -pedantic" } */
-
-long long i;
