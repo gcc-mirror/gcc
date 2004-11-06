e78980b724dc1ad0689c40f5936c26356033797a
Eric Botcazou <ebotcazou@libertysurf.fr>
2004-11-06 19:58:02 +0100

    * g++.dg/other/pragma-re-1.C (p, q): Fix type and remove cast.
    From-SVN: r90185

--- gcc/testsuite/g++.dg/other/pragma-re-1.C
+++ gcc/testsuite/g++.dg/other/pragma-re-1.C
@@ -11,10 +11,10 @@
 /* This one is expected to work.  */
 #pragma redefine_extname foo bar
 extern "C" int foo(void);
-void *p = (void *)foo;
+int (*p)(void) = foo;
 
 /* This one is expected not to work (redefine_extname
    can only be applied to extern "C" names).  */
 #pragma redefine_extname baz baq
 extern int baz(void);
-void *q = (void *)baz;
+int (*q)(void) = baz;
