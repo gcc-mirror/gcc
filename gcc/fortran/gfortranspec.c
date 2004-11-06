ffac9d613410069c7640bd9bd092c95797df8d67
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-11-06 03:49:32 +0000

    * gfortranspec.c: Replace GNU CC with GCC.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@90163 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/gfortranspec.c
+++ gcc/fortran/gfortranspec.c
@@ -2,7 +2,7 @@
    Copyright (C) 1997, 1999, 2000, 2001, 2002, 2003, 2004 Free Software
    Foundation, Inc.
 
-This file is part of GNU CC.
+This file is part of GCC.
 
 GNU CC is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
