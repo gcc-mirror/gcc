e221ef54b8cb4feb7e3b3def11afae562be2c559
Ulrich Weigand <uweigand@de.ibm.com>
2004-11-09 20:46:19 +0000

    s390-protos.h (s390_pool_operand): Remove.
    ChangeLog:
    
    	* config/s390/s390-protos.h (s390_pool_operand): Remove.
    	* config/s390/s390.c (s390_pool_operand): Likewise.
    	(s390_extra_constraint_str): Handle 'B' constraints.
    	* config/s390/s390.h (CONSTRAINT_LEN): Handle 'B' constraints.
    	* config/s390/s390.md: Document 'B' constraints.
    	("*cmpdi_cct", "*cmpsi_cct"): Use 'B' constraint instead of
    	s390_pool_operand to prevent insns with two literal pool
    	references.  Make pattern commutative.
    	("*cmpdi_ccu", "*cmpsi_ccu", "*cmphi_ccu", "*cmpqi_ccu"): Use
    	'B' constraint instead of s390_pool_operand.
    
    testsuite/ChangeLog:
    
    	* gcc.dg/20041109-1.c: New test.
    
    From-SVN: r90364

---
+++ gcc/testsuite/gcc.dg/20041109-1.c
@@ -0,0 +1,21 @@
+/* This used to ICE due to a literal pool handling bug on s390x.  */
+
+/* { dg-do compile { target s390*-*-* } } */
+/* { dg-options "-O2 -fno-omit-frame-pointer" } */
+
+static struct table { int x; } table[3];
+
+int test (void)
+{
+  struct table *t;
+
+  for (t = table; t < &table[3]; t++)
+    asm volatile ("" : : : "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "12");
+
+  for (t = table; t < &table[3]; t++)
+    if (t->x)
+      return 1;
+
+  return 0;
+}
+
