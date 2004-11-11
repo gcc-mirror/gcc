60934f9cf4eec9feef395961c9dda139fa325845
Nathan Sidwell <nathan@gcc.gnu.org>
2004-11-11 08:40:43 +0000

    re PR target/16458 (PowerPC - redundant compare)
    .:	PR target/16458
    	* config/rs6000/rs6000.c (rs6000_generate_compare): Generate an
    	unsigned equality compare when we know the operands are unsigned.
    testsuite:
    	PR target/16458
    	* gcc.dg/ppc-compare-1.c: New.
    
    From-SVN: r90475

---
+++ gcc/testsuite/gcc.dg/ppc-compare-1.c
@@ -0,0 +1,16 @@
+/* { dg-do compile { target powerpc64-*-* } } */
+/* { dg-options "-m64 -O2" } */
+
+/* { dg-final { scan-assembler-not "cmpw" } } */
+
+/* Origin:Pete Steinmetz <steinmtz@us.ibm.com> */
+
+/* PR 16458: Extraneous compare.  */
+
+int foo (unsigned a, unsigned b)
+{
+  if (a == b) return 1;
+  if (a > b)  return 2;
+  if (a < b)  return 3;
+  return 0;
+}
