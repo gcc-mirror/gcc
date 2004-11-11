e1e2e653d7fdcf6c9f1cafb76fbb016c1879029d
Nathan Sidwell <nathan@codesourcery.com>
2004-11-11 17:03:36 +0000

    re PR target/16457 (PowerPC - Combine two rldicr instructions into a single rlwinm.)
    .:	PR target/16457
    	* config/rs6000/rs6000.c (mask64_2_operand): Stub to call
    	mask64_1or2_operand.
    	(mask64_1or_2_operand): Broken out of mask64_2_operand, add flag
    	to spot rlwinm opportunities.
    	(and64_2_operand): Use mask_1or2_operand.
    	* config/rs6000/rs6000.md (anddi3): Use rlwinm when possible.
    testsuite:
    	PR target/16457
    	* gcc.dg/ppc-and-1: New
    
    From-SVN: r90481

---
+++ gcc/testsuite/gcc.dg/ppc-and-1.c
@@ -0,0 +1,20 @@
+/* { dg-do compile { target powerpc64-*-* } } */
+/* { dg-options "-m64 -O2" } */
+
+/* { dg-final { scan-assembler "rlwinm \[0-9\]+,\[0-9\]+,0,0,30"  } } */
+/* { dg-final { scan-assembler "rlwinm \[0-9\]+,\[0-9\]+,0,29,30"  } } */
+/* { dg-final { scan-assembler-not "rldicr" } } */
+
+/* Origin:Pete Steinmetz <steinmtz@us.ibm.com> */
+
+/* PR 16457 - use rlwinm insn.  */
+
+char *foo1 (char *p, unsigned int x)
+{
+  return p - (x & ~1);
+}
+
+char *foo2 (char *p, unsigned int x)
+{
+  return p - (x & 6);
+}
