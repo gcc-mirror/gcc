7e9b8f341c4a72c407e98cce297e64b96d906c59
schwab <schwab@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-11-12 17:09:10 +0000

    * gcc.dg/pr16286.c: Fix last change.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@90533 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr16286.c
+++ gcc/testsuite/gcc.dg/pr16286.c
@@ -2,10 +2,9 @@
 /* { dg-options "-maltivec" } */
 
 /* PR 16286
- * Compilation of a simple Altivec test program fails if vector is #undefined
- * when compiling with -maltivec.  This may be done for building C++ programs
- * that use the STL <vector>.
- */
+   Compilation of a simple Altivec test program fails if vector, pixel
+   and/or bool are #undefined when compiling with -maltivec.  This may be
+   done for building C++ programs that use the STL <vector>.  */
 
 #include <altivec.h>
 #undef vector
@@ -20,5 +19,5 @@ void test(void)
 }
 
 /* { dg-bogus "(syntax|parse) error before \"vector\"" "-maltivec" { target powerpc*-*-* } 0 } */
-/* { dg-bogus "(syntax|parse) error before \"vector\"" "-maltivec" { target powerpc*-*-* } 0 } */
-/* { dg-bogus "(syntax|parse) error before \"vector\"" "-maltivec" { target powerpc*-*-* } 0 } */
+/* { dg-bogus "(syntax|parse) error before \"pixel\"" "-maltivec" { target powerpc*-*-* } 0 } */
+/* { dg-bogus "(syntax|parse) error before \"bool\"" "-maltivec" { target powerpc*-*-* } 0 } */
