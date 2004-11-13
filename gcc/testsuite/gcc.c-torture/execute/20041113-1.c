62bcd115ff9a30cb9f52000d60c91539c1d940fd
bernds <bernds@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-11-13 17:13:37 +0000

    * gcc.c-torture/execute/20041113-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@90588 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20041113-1.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+
+void test (int x, ...)
+{
+    va_list ap;
+    int i;
+    va_start (ap, x);
+    if (va_arg (ap, int) != 1)
+	abort ();
+    if (va_arg (ap, int) != 2)
+	abort ();
+    if (va_arg (ap, int) != 3)
+	abort ();
+    if (va_arg (ap, int) != 4)
+	abort ();
+}
+
+double a = 40.0;
+
+int main(int argc, char *argv[])
+{
+    test(0, 1, 2, 3, (int)(a / 10.0));
+    exit (0);
+}
