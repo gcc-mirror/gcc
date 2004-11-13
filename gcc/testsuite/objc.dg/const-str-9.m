5dea86897277316778ac91cd98b2ed698937109b
zlaski <zlaski@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-11-13 02:08:44 +0000

    [gcc/ChangeLog] 2004-11-12  Ziemowit Laski  <zlaski@apple.com>
           * varasm.c (output_addressed_constants): For CONST_DECLs,
           output the initializer if present.
    
    [gcc/testsuite/ChangeLog]
    2004-11-12  Ziemowit Laski  <zlaski@apple.com>
    
           * objc.dg/const-str-8.m, objc.dg/const-str-9.m:
           New tests.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@90569 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/objc.dg/const-str-9.m
@@ -0,0 +1,20 @@
+/* Test if ObjC constant strings get placed in the correct section.  */
+/* Contributed by Ziemowit Laski <zlaski@apple.com>  */
+
+/* { dg-options "-fnext-runtime" } */
+/* { dg-do compile { target *-*-darwin* } } */
+
+#include <objc/Object.h>
+
+@interface NSConstantString: Object {
+  char *cString;
+  unsigned int len;
+}
+@end
+
+extern struct objc_class _NSConstantStringClassReference;
+
+static const NSConstantString *appKey = @"MyApp";
+
+/* { dg-final { scan-assembler ".section __OBJC, __cstring_object" } } */
+/* { dg-final { scan-assembler ".long\t__NSConstantStringClassReference\n\t.long\t.*\n\t.long\t5\n\t.data" } } */
