f7f021251dcffa472029790464fc4b6b338454ca
Dorit Naishlos <dorit@il.ibm.com>
2004-11-14 20:44:52 +0000

    re PR tree-optimization/18400 (wrong unrolling after vectorization due to invalid loop->nb_iterations)
    2004-11-14  Dorit Naishlos  <dorit@il.ibm.com>
    
            PR tree-opt/18400
            * gcc.dg/vect/pr18400.c: New test.
    
    2004-11-14  Dorit Naishlos  <dorit@il.ibm.com>
                Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR tree-opt/18425
            * gcc.dg/vect/pr18425.c: New test.
    
    Co-Authored-By: Andrew Pinski <pinskia@physics.uc.edu>
    
    From-SVN: r90631

---
+++ gcc/testsuite/gcc.dg/vect/pr18400.c
@@ -0,0 +1,39 @@
+/* { dg-do run { target powerpc*-*-* } } */
+/* { dg-do run { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-funroll-loops -O2 -ftree-vectorize -fdump-tree-vect-stats -maltivec" { target powerpc*-*-* } } */
+/* { dg-options "-funroll-loops -O2 -ftree-vectorize -fdump-tree-vect-stats -msse2" { target i?86-*-* x86_64-*-* } } */
+
+#include <stdarg.h>
+#include "tree-vect.h"
+
+#define N 8
+ 
+int main1 ()
+{  
+  int b[N] = {0,3,6,9,12,15,18,21};
+  int a[N];
+  int i;
+
+  for (i = 0; i < N; i++)
+    {
+      a[i] = b[i];
+    }
+
+  /* check results:  */
+  for (i = 0; i < N; i++)
+    {
+      if (a[i] != b[i])
+        abort ();
+    }
+
+  return 0;
+}
+
+int main (void)
+{ 
+  check_vect ();
+  
+  return main1 ();
+} 
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } } */
