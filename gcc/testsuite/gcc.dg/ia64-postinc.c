29a1e32f048a4566a808fba0ea95e466290e09de
sje <sje@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-11-15 16:33:04 +0000

    * gcc.dg/ia64-postinc.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@90677 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/ia64-postinc.c
@@ -0,0 +1,23 @@
+/* { dg-do compile { target ia64-*-* } } */
+/* { dg-options "-O2" } */
+/* { dg-options "-O2 -mlp64" { target ia64-*-hpux* } } */
+
+void copy_loop_ldouble (void *xdest,
+                        const void *xsrc,
+                        long roff,
+                        long soff,
+                        long len,
+                        long shift)
+{ __float128 *dest = xdest;
+  const long double *src;
+  long i;
+  roff /= sizeof (__float128);
+  soff /= sizeof (__float128);
+  src = xsrc;
+  src += shift * soff;
+  for (i = 0; i < len - shift; ++i) {
+        *dest = *src;
+        dest += roff;
+        src += soff;
+  }
+}
