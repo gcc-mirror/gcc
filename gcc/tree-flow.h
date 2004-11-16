91688ed7e8021a979dd9e97efa3291995d3623a7
dalej <dalej@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-11-16 01:49:08 +0000

    2004-11-15  Dale Johannesen  <dalej@apple.com>
            * tree-flow.h (add_referenced_temp_var):  Remove name from prototype.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@90712 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-flow.h
+++ gcc/tree-flow.h
@@ -531,7 +531,7 @@ extern void compute_immediate_uses (int, bool (*)(tree));
 extern void free_df (void);
 extern void free_df_for_stmt (tree);
 extern tree get_virtual_var (tree);
-extern void add_referenced_tmp_var (tree var);
+extern void add_referenced_tmp_var (tree);
 extern void mark_new_vars_to_rename (tree, bitmap);
 extern void find_new_referenced_vars (tree *);
 
