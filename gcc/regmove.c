d281a1f1bd19c6a8b0f363649f426c945ee8a9ce
Ulrich Weigand <uweigand@de.ibm.com>
2004-11-16 21:11:33 +0000

    regmove.c (regmove_optimize): Use lowpart_subreg instead of gen_rtx_SUBREG with incorrect offset to compute...
    	* regmove.c (regmove_optimize): Use lowpart_subreg instead of
    	gen_rtx_SUBREG with incorrect offset to compute SRC_SUBREG.
    
    From-SVN: r90762

--- gcc/regmove.c
+++ gcc/regmove.c
@@ -1150,10 +1150,11 @@ regmove_optimize (rtx f, int nregs, FILE *regmove_dump_file)
 		  && GET_MODE_SIZE (GET_MODE (dst))
 		     >= GET_MODE_SIZE (GET_MODE (SUBREG_REG (dst))))
 		{
-		  src_subreg
-		    = gen_rtx_SUBREG (GET_MODE (SUBREG_REG (dst)),
-				      src, SUBREG_BYTE (dst));
 		  dst = SUBREG_REG (dst);
+		  src_subreg = lowpart_subreg (GET_MODE (dst),
+					       src, GET_MODE (src));
+		  if (!src_subreg)
+		    continue;
 		}
 	      if (!REG_P (dst)
 		  || REGNO (dst) < FIRST_PSEUDO_REGISTER)
