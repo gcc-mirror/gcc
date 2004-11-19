3a7ccce8faf93e8ad48cfdb1f685331b31c4621b
fjahanian <fjahanian@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-11-19 18:01:54 +0000

    No darwin non-lazy-ptr for address calculatation when not needed. OKed by Geoff Keating.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@90925 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/non-lazy-ptr-test.c
@@ -0,0 +1,40 @@
+/* { dg-do compile { target powerpc*-apple-darwin* } } */
+/* { dg-options "-S" } */
+
+typedef void PF (void);
+
+static void f(void) {
+}
+
+void f1(void) {
+}
+
+extern void f2(void) {
+}
+
+static void f3(void);
+
+__private_extern__ void pe(void)
+{
+}
+
+PF* g (void) { f(); return f; }
+PF* x (void) { return f1; }
+PF* y (void) { f2(); return f2; }
+PF* z (void) { return f3; }
+PF* w (void) { pe(); return pe; }
+
+int main()
+{
+	(*g())();
+	(*x())();
+	(*y())();
+	(*z())();
+	(*w())();
+	return 0;
+}
+
+void f3(void) {
+}
+
+/* { dg-final { scan-assembler-not "non_lazy_ptr" } } */
