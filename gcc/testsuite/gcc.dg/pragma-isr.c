d23eb341468c6ce1f0bf1708d379c8fbc3e81aec
Nitin Yewale <nitiny@kpitcummins.com>
2004-11-21 22:54:00 +0000

    Move from root of repository to intented place.
    From-SVN: r90988

---
+++ gcc/testsuite/gcc.dg/pragma-isr.c
@@ -0,0 +1,17 @@
+/* { dg-do compile { target h8300-*-* sh-*-*} } */
+/* { dg-options "-O3" } */
+/* Test case will check whether rte is generated for two ISRs*/
+extern void foo();
+#pragma interrupt
+void  isr1(void)
+{
+		foo();
+}
+
+#pragma interrupt
+void  isr2(void)
+{
+		foo();
+}
+
+/* { dg-final { scan-assembler-times "rte" 2} } */
