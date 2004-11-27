23022ed941da1387b948e2795c08badd984cd32b
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-11-27 09:54:10 +0000

    * gcc.dg/mips-movcc-[123].c: Pass the ?: expressions to other 	functions.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@91371 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/mips-movcc-1.c
+++ gcc/testsuite/gcc.dg/mips-movcc-1.c
@@ -4,20 +4,22 @@
 /* { dg-final { scan-assembler "movn" } } */
 /* { dg-final { scan-assembler "movt" } } */
 
+void ext_int (int);
+
 int
 sub1 (int i, int j, int k)
 {
-  return k ? i : j;
+  ext_int (k ? i : j);
 }
 
 int
 sub2 (int i, int j, long l)
 {
-  return !l ? i : j;
+  ext_int (!l ? i : j);
 }
 
 int
 sub3 (int i, int j, float f)
 {
-  return f ? i : j;
+  ext_int (f ? i : j);
 }
