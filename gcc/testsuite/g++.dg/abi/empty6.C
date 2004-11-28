a8344e3213b1ed04a9b32f2c263f2df61ea687f6
Hans-Peter Nilsson <hp@bitrange.com>
2004-11-28 23:28:05 +0000

    re PR target/18484 (mmix-knuth-mmixware testsuite failure: g++.dg/abi/empty6.C)
    	PR target/18484
    	* g++.dg/abi/empty6.C: Xfail warning for mmix-*-*.
    
    From-SVN: r91434

--- gcc/testsuite/g++.dg/abi/empty6.C
+++ gcc/testsuite/g++.dg/abi/empty6.C
@@ -3,11 +3,13 @@
 struct A {};
 
 struct B {
-  A a; // { dg-warning "empty" }
+  A a; // { dg-warning "empty" "" { xfail mmix-*-* } }
   virtual void f () {}
 } __attribute__((aligned(8)));
 /* The preceding attribute is necessary on targets with
    BIGGEST_ALIGNMENT <= 32 to trigger the warning, as otherwise a 32 bit
    offset is split into DECL_FIELD_OFFSET 4 and DECL_FIELD_BIT_OFFSET 0,
    and then there is no discrepancy between DECL_FIELD_OFFSET and
-   byte_position to warn about.  */
+   byte_position to warn about.
+   On the other hand 64-bit targets for example, generally need a larger
+   requested alignment to get the intended warning.  */
