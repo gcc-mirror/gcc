22bbc492b2e73d9132cd5bbfd6771f0d8e087895
Hans-Peter Nilsson <hp@bitrange.com>
2004-11-29 13:27:10 +0000

    re PR middle-end/18164 (ICE on trivial invalid asm)
    	PR middle-end/18164
    	* gcc.dg/pr18164.c: Add dg-options "".  Correct typo in dg-error.
    
    From-SVN: r91468

--- gcc/testsuite/gcc.dg/pr18164.c
+++ gcc/testsuite/gcc.dg/pr18164.c
@@ -1,7 +1,8 @@
 /* { dg-do compile } */
+/* { dg-options "" } */
 void
 f (void)
 {
   int x;
-  asm ("" :  "" (x)); /* {dg-error "output operand constraint lacks" } */
+  asm ("" :  "" (x)); /* { dg-error "output operand constraint lacks" } */
 }
