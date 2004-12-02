1a06672a3b58bef4a4386ca3e24cc17c13d5959a
Nick Clifton <nickc@redhat.com>
2004-12-02 11:10:38 +0000

    xstormy16: New test directory.
    * gcc.target/xstormy16: New test directory.
    * gcc.target/xstormy16/xstormy16.exp: Run the new tests.
    * gcc.target/xstormy16/bp.c: Check the generation of bp	instructions.
    * gcc.target/xstormy16/below_100.c: Check the generation of bp and bn
    instructions and the BELOW100 attribute.
    
    From-SVN: r91627

---
+++ gcc/testsuite/gcc.target/xstormy16/below_100.c
@@ -0,0 +1,30 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+/* { dg-final { scan-assembler "bn " } } */
+/* { dg-final { scan-assembler "bp " } } */
+
+unsigned short   a_below __attribute__((__BELOW100__));
+unsigned short   b_below __attribute__((__BELOW100__));
+unsigned short * a_ptr = & a_below;
+unsigned short * b_ptr = & b_below;
+
+char *
+foo (void)
+{
+  if (a_below & 0x0100)
+    {
+      if (b_below & 0x0100)
+	return "Fail";
+      return "Success";
+    }
+
+  return "Fail";
+}
+
+char *
+bar (void)
+{
+  *a_ptr = 0x0100;
+  *b_ptr = 0xfeff;
+  return foo ();
+}
