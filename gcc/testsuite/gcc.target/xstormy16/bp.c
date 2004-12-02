64aa5bba65a53b647f1a344b6a054d114fc3063b
nickc <nickc@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-12-02 11:10:38 +0000

    * gcc.target/xstormy16: New test directory. * gcc.target/xstormy16/xstormy16.exp: Run the new tests. * gcc.target/xstormy16/bp.c: Check the generation of bp	instructions. * gcc.target/xstormy16/below_100.c: Check the generation of bp and bn instructions and the BELOW100 attribute.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@91627 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/xstormy16/bp.c
@@ -0,0 +1,34 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+/* { dg-final { scan-assembler "bp " } } */
+
+#define a_val (*((volatile unsigned char *) 0x7f14))
+#define b_val (*((volatile unsigned char *) 0x7f10))
+
+unsigned char * a_ptr = (unsigned char *) 0x7f14;
+unsigned char * b_ptr = (unsigned char *) 0x7f10;
+
+int
+foo (void)
+{
+  if (a_val & 0x08)
+    {
+      if (b_val & 0x08)
+	return -1;
+
+      return 0;
+    }
+
+  return -1;
+}
+
+int
+bar (void)
+{
+  *a_ptr = 0x08;
+  *b_ptr = 0xf7;
+
+  return foo ();
+}
+
+
