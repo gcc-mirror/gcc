f177a087002cef5c40eab070151d6dee4e39d346
Steven G. Kargl <kargls@comcast.net>
2004-12-02 04:20:09 +0000

    flush.c: New file.
    2004-12-02  Steven G. Kargl  <kargls@comcast.net>
    	Paul Brook  <paul@codesourcery.com>
    
    libgfortran/
    	* intrinsics/flush.c: New file.
    	* intrinsics/fnum.c: ditto
    	* intrinsics/stat.c: ditto
    	* io/io.h (unit_to_fd): Add prototype.
    	* io/unix.c (unit_to_fd): New function.
    	* configure.ac: Add test for members of struct stat.  Check for
    	sys/types.h and sys/stat.h
    	* Makefile.am: Add intrinsics/{flush.c,fnum.c,stat.c}
    	* configure.in: Regenerate.
    	* config.h.in: Regenerate.
    	* Makefile.in: Regenerate.
    fortran/
    	* check.c (gfc_check_flush, gfc_check_fnum): New functions.
    	(gfc_check_fstat, gfc_check_fstat_sub): New functions.
    	(gfc_check_stat, gfc_check_stat_sub): New functions.
    	* gfortran.h (GFC_ISYM_FNUM,GFC_ISYM_FSTAT,GFC_ISYM_STAT): New symbols
    	* intrinsic.c (add_functions,add_subroutines): Add flush, fnum,
    	fstat, and stat to intrinsics symbol tables.
    	* intrinsic.h (gfc_check_flush, gfc_resolve_stat_sub): Add prototypes.
    	(gfc_resolve_fstat_sub, gfc_resolve_stat): Ditto.
    	* iresolve.c (gfc_resolve_fnum, gfc_resolve_fstat): New functions.
    	(gfc_resolve_stat, gfc_resolve_flush): New functions.
    	(gfc_resolve_stat_sub,gfc_resolve_fstat_sub): New functions
    	* trans-intrinsic.c (gfc_conv_intrinsic_function): Add new intrinsics.
    
    From-SVN: r91611

---
+++ libgfortran/intrinsics/flush.c
@@ -0,0 +1,66 @@
+/* Implementation of the FLUSH intrinsic.
+   Copyright (C) 2004 Free Software Foundation, Inc.
+   Contributed by Steven G. Kargl <kargls@comcast.net>.
+
+This file is part of the GNU Fortran 95 runtime library (libgfortran).
+
+Libgfortran is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+Libgfortran is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with libgfor; see the file COPYING.LIB.  If not,
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
+
+
+#include "config.h"
+#include "libgfortran.h"
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#include "../io/io.h"
+
+/* SUBROUTINE FLUSH(UNIT)
+   INTEGER, INTENT(IN), OPTIONAL :: UNIT  */
+
+static void
+recursive_flush (gfc_unit *us)
+{
+  /* There can be no open files.  */
+  if (us == NULL)
+    return;
+
+  flush (us->s);
+  recursive_flush (us->left);
+  recursive_flush (us->right);
+}
+
+
+void
+prefix(flush_i4) (GFC_INTEGER_4 * unit)
+{
+
+  gfc_unit *us;
+
+  /* flush all streams */
+  if (unit == NULL)
+    {
+      us = g.unit_root;
+      recursive_flush(us);
+    }
+  else
+    {
+      us = find_unit(*unit);
+      if (us != NULL)
+        flush (us->s);
+    }
+}
