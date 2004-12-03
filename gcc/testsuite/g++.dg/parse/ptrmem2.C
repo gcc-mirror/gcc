05d851a15224454173b94f261a5806264fc72d3e
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-12-03 10:51:13 +0000

    cp: 	PR c++/18782 	* decl.c (grokdeclarator): Make sure class in pointer to member is 	not a namespace. testsuite: 	PR c++/18782 	* g++.dg/parse/ptrmem2.C: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@91681 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/ptrmem2.C
@@ -0,0 +1,11 @@
+// { dg-do compile }
+
+// Copyright (C) 2004 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 2 Dec 2004 <nathan@codesourcery.com>
+
+// PR 18782: ICE with ptr-to-member
+// Origin:   Volker Reichelt <reichelt@gcc.gnu.org>
+
+namespace A {}
+
+int A::* p; // { dg-error "is a namespace" "" }
