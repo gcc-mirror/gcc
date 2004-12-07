c7baa14599b771e8d118b5cd33423c84a40e5097
Ziemowit Laski <zlaski@apple.com>
2004-12-07 23:46:11 +0000

    c-tree.h (struct lang_type): Rename 'objc_protocols' field to 'objc_info'.
    [gcc/ChangeLog]
    2004-12-07  Ziemowit Laski  <zlaski@apple.com>
    
            * c-tree.h (struct lang_type): Rename 'objc_protocols' field
            to 'objc_info'.
    
    [gcc/cp/ChangeLog]
    2004-12-07  Ziemowit Laski  <zlaski@apple.com>
    
            * cp-tree.h (struct lang_type_class): Rename 'objc_protocols'
            field to 'objc_info'.
    
    From-SVN: r91833

--- gcc/c-tree.h
+++ gcc/c-tree.h
@@ -73,9 +73,10 @@ struct lang_type GTY(())
   /* In an ENUMERAL_TYPE, the min and max values.  */
   tree enum_min;
   tree enum_max;
-  /* In a RECORD_TYPE, a list of Objective-C protocols that this type
-     adopts.  This is used only in Objective-C.  */
-  tree objc_protocols;
+  /* In a RECORD_TYPE, information specific to Objective-C, such
+     as a list of adopted protocols or a pointer to a corresponding
+     @interface.  See objc/objc-act.h for details.  */
+  tree objc_info;
 };
 
 /* Record whether a type or decl was written with nonconstant size.
