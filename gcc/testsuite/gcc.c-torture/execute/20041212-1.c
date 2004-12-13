66858d3fe479ca9e985a3557412f124b7e665411
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-12-13 04:08:39 +0000

    PR middle-end/17564 	* gcc.c-torture/execute/20041212-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@92076 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20041212-1.c
@@ -0,0 +1,15 @@
+/* A function pointer compared with a void pointer should not be canonicalized.
+   See PR middle-end/17564.  */
+void *f (void) __attribute__ ((__noinline__));
+void *
+f (void)
+{
+  return f;
+}
+int
+main (void)
+{
+  if (f () != f)
+    abort ();
+  exit (0);
+}
