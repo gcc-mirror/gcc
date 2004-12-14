58e96975d38923a12d70c7cfc98e193b4afed18d
Richard Henderson <rth@redhat.com>
2004-12-13 21:33:01 -0800

    * gcc.dg/i386-sse-10.c: Fix typo in options.
    From-SVN: r92127

--- gcc/testsuite/gcc.dg/i386-sse-10.c
+++ gcc/testsuite/gcc.dg/i386-sse-10.c
@@ -1,6 +1,6 @@
 /* PR 17930 */
 /* { dg-do run { target i?86-*-* x86_64-*-* } } */
-/* { dg-options "-O1 -msse2 -mfpmode=sse -mno-accumulate-outgoing-args" } */
+/* { dg-options "-O1 -msse2 -mfpmath=sse -mno-accumulate-outgoing-args" } */
 
 #include "i386-cpuid.h"
 
