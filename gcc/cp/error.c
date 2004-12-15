14bce7cdf5be21eeeff274830f328370a02e61bc
Volker Reichelt <reichelt@igpm.rwth-aachen.de>
2004-12-15 16:45:23 +0000

    re PR c++/16806 (Internal compiler error: Error reporting routines re-entered.)
    	PR c++/16806
    	* error.c (dump_expr) [BASELINK]: Use dump_expr.
    
    From-SVN: r92203

--- gcc/cp/error.c
+++ gcc/cp/error.c
@@ -2071,7 +2071,7 @@ dump_expr (t, flags)
       break;
 
     case BASELINK:
-      print_tree_identifier (scratch_buffer, DECL_NAME (get_first_fn (t)));
+      dump_expr (get_first_fn (t), flags & ~TFF_EXPR_IN_PARENS);
       break;
 
     case TREE_LIST:
