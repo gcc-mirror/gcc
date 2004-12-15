f67254bef2578549f383aabdc379c6d1731822e1
Eric Botcazou <ebotcazou@gcc.gnu.org>
2004-12-15 19:17:57 +0000

    re PR c++/17972 (const/pure functions result in bad asm)
    	PR c++/17972
    	* tree-inline.c (expand_call_inline): Set TREE_SIDE_EFFECTS
    	on the STMT_EXPR wrapping up the inlined body.
    
    From-SVN: r92213

--- gcc/tree-inline.c
+++ gcc/tree-inline.c
@@ -1548,8 +1548,11 @@ expand_call_inline (tree *tp, int *walk_subtrees, void *data)
   splay_tree_delete (id->decl_map);
   id->decl_map = st;
 
-  /* The new expression has side-effects if the old one did.  */
-  TREE_SIDE_EFFECTS (expr) = TREE_SIDE_EFFECTS (t);
+  /* Although, from the semantic viewpoint, the new expression has
+     side-effects only if the old one did, it is not possible, from
+     the technical viewpoint, to evaluate the body of a function
+     multiple times without serious havoc.  */
+  TREE_SIDE_EFFECTS (expr) = 1;
 
   /* Replace the call by the inlined body.  Wrap it in an
      EXPR_WITH_FILE_LOCATION so that we'll get debugging line notes
