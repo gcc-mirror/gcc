4c97972bf0cd32db2685ec708a89e31bcc750c5b
lerdsuwa <lerdsuwa@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-12-15 15:21:11 +0000

    PR c++/18825 	* pt.c (instantiate_class_template): Set input_location for 	friend function. 	(tsubst_friend_function): Don't set input_location here. 	Make sure the context is complete if necessary.
    	* g++.dg/template/friend32.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@92200 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/friend32.C
@@ -0,0 +1,21 @@
+// { dg-do compile }
+
+// Origin: Steven Bosscher <steven@gcc.gnu.org>
+//	   Serge Belyshev <belyshev@lubercy.com>
+
+// PR c++/18825: ICE member as friend
+
+template<class T> class A
+{
+  void f ();			// { dg-error "private" }
+};
+
+template<class T> class B
+{
+  friend void A<T>::f ();	// { dg-error "this context" }
+};
+
+int f ()
+{
+  B<int> b;			// { dg-error "instantiated" }
+}
