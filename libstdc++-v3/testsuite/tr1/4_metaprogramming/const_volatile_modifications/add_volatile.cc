7576fda61518c0b1b02288f61171ca493286c17b
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-12-16 15:00:37 +0000

    2004-12-16  Paolo Carlini  <pcarlini@suse.de>
    	* include/tr1/type_traits: Implement add_const, add_volatile,
    	and add_cv.
    	* testsuite/tr1/4_metaprogramming/const_volatile_modifications/
    	add_const.cc: New.
    	* testsuite/tr1/4_metaprogramming/const_volatile_modifications/
    	add_cv.cc: Likewise.
    	* testsuite/tr1/4_metaprogramming/const_volatile_modifications/
    	add_volatile.cc: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@92265 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/tr1/4_metaprogramming/const_volatile_modifications/add_volatile.cc
@@ -0,0 +1,56 @@
+// 2004-12-16  Paolo Carlini  <pcarlini@suse.de>
+//
+// Copyright (C) 2004 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 4.7.1 Const-volatile modifications
+
+#include <tr1/type_traits>
+#include <testsuite_hooks.h>
+#include <testsuite_tr1.h>
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  using std::tr1::add_volatile;
+  using std::tr1::is_same;
+  using namespace __gnu_test;
+
+  VERIFY( (is_same<add_volatile<int>::type, volatile int>::value) );
+  VERIFY( (is_same<add_volatile<const int>::type, const volatile int>::value) );
+  VERIFY( (is_same<add_volatile<int*>::type, int* volatile>::value) );
+  VERIFY( (is_same<add_volatile<int&>::type, int&>::value) );
+  VERIFY( (is_same<add_volatile<int (int)>::type, int (int)>::value) );
+  VERIFY( (is_same<add_volatile<volatile int>::type, volatile int>::value) );
+  VERIFY( (is_same<add_volatile<ClassType>::type, volatile ClassType>::value) );
+  VERIFY( (is_same<add_volatile<const ClassType>::type,
+	   const volatile ClassType>::value) );
+  VERIFY( (is_same<add_volatile<ClassType*>::type,
+	   ClassType* volatile>::value) );
+  VERIFY( (is_same<add_volatile<ClassType&>::type, ClassType&>::value) );
+  VERIFY( (is_same<add_volatile<ClassType (ClassType)>::type,
+	   ClassType (ClassType)>::value) );
+  VERIFY( (is_same<add_volatile<volatile ClassType>::type,
+	   volatile ClassType>::value) );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
