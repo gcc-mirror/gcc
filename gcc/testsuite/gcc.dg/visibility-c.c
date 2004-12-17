a565cf4de49d154b8b3e84ee55f24dfb3cccfc30
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-12-17 00:54:49 +0000

    2004-12-16  Andrew Pinski  <pinskia@physics.uc.edu>
            * gcc.dg/visibility-c.c: Change compile to
            assemble for the test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@92293 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/visibility-c.c
+++ gcc/testsuite/gcc.dg/visibility-c.c
@@ -1,7 +1,6 @@
 /* Test that visibility works on common symbols also. */
-/* { dg-do compile } */
+/* { dg-do assemble } */
 /* { dg-require-visibility "" } */
-/* { dg-final { scan-hidden "options" } } */
 
 int options  __attribute__((__visibility__("hidden")));
 
