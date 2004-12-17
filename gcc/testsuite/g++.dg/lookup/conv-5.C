b54a07e8d266a3f49d7cd7aa8e8318af506b4ab1
Nathan Sidwell <nathan@codesourcery.com>
2004-12-17 15:58:04 +0000

    re PR c++/17821 (Poor diagnostic for using . instead of ->)
    cp:
    	PR c++/17821
    	* class.c (add_method): Do not push conversion operators into a
    	binding level.
    
    	* cp-tree.h (CLASSTYPE_PRIMARY_TEMPLATE_TYPE): Reformat.
    	* error.c (dump_decl): <TYPE_DECL case> Remove extraneous braces.
    testsuite:
    	PR c++/17821
    	* g++.dg/lookup/conv-5.C: New.
    
    From-SVN: r92316

---
+++ gcc/testsuite/g++.dg/lookup/conv-5.C
@@ -0,0 +1,15 @@
+// { dg-do compile }
+
+// Copyright (C) 2004 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 17 Dec 2004 <nathan@codesourcery.com>
+
+// PR 17821. bogus error
+// Origin:  Mikael Kilpel?inen <belz@kolumbus.fi>
+
+struct A {
+  template<typename T>
+  operator T() const;
+  
+  operator float() const;
+};
+
