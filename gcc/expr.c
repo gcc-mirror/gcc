51bc37ca52fe4514e0646213bdb855263c19afd3
Roger Sayle <roger@eyesopen.com>
2004-12-19 20:01:36 +0000

    re PR middle-end/19068 ([3.3 only] Wrong code for MIN_EXPR and MAX_EXPR)
    	PR middle-end/19068
    	* expr.c (expand_expr_real_1) <MAX_EXPR>: Ensure that target, op0
    	and op1 are all registers (or constants) before expanding the RTL
    	comparison sequence [to avoid reg_overlap_mentioned (target, op1)].
    
    	* g++.dg/opt/max1.C: New test case.
    
    From-SVN: r92386

--- gcc/expr.c
+++ gcc/expr.c
@@ -8107,7 +8107,7 @@ expand_expr_real (tree exp, rtx target, enum machine_mode tmode,
       /* At this point, a MEM target is no longer useful; we will get better
 	 code without it.  */
 
-      if (GET_CODE (target) == MEM)
+      if (! REG_P (target))
 	target = gen_reg_rtx (mode);
 
       /* If op1 was placed in target, swap op0 and op1.  */
@@ -8117,6 +8117,11 @@ expand_expr_real (tree exp, rtx target, enum machine_mode tmode,
 	  op0 = op1;
 	  op1 = tem;
 	}
+
+      /* We generate better code and avoid problems with op1 mentioning
+	 target by forcing op1 into a pseudo if it isn't a constant.  */
+      if (! CONSTANT_P (op1))
+	op1 = force_reg (mode, op1);
 
       if (target != op0)
 	emit_move_insn (target, op0);
