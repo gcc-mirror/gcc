afdc9016cd75c0ac0c9f439dc7fd4d5a4a4392d7
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-12-19 20:11:15 +0000

    PR middle-end/19068 	* g++.dg/opt/max1.C: New test case.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@92388 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/opt/max1.C
@@ -0,0 +1,29 @@
+/* PR middle-end/19068 */
+/* Test case by Andrew Pinski <pinskia@physics.uc.edu> */
+/* { dg-do run } */
+/* { dg-options "-O2" } */
+
+extern "C" void abort (void);
+
+long fff[10];
+
+void f(long a)
+{
+  int i;
+  a =  *((long*)(a+5)) >? *((long*)(a+1)); 
+
+  for(i=0;i<10;i++)
+   fff[i] = a;
+}
+
+int main(void)
+{
+  int i;
+  long a[2] = {10,5};
+  f((long)(&a)-1);
+  for(i = 0;i<10;i++)
+   if (fff[i]!=10)
+    abort ();
+  return 0;
+}
+
