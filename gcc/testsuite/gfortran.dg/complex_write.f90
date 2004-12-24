c1c42f20d6c2ac53feefd4ba02febe6c3ff4d992
Bud Davis <bdavis@gcc.gnu.org>
2004-12-24 03:17:13 +0000

    complex_write.f90: removed extraneous comma.
    2004-12-23  Bud Davis  <bdavis9659@comcast.net>
    
    	* gfortran.dg/complex_write.f90: removed extraneous comma.
    
    From-SVN: r92584

--- gcc/testsuite/gfortran.dg/complex_write.f90
+++ gcc/testsuite/gfortran.dg/complex_write.f90
@@ -7,7 +7,7 @@
        real r1,r2      
        a = cmplx(1.0, 2.0)      
        open(unit=74,status='scratch')      
-       write(74,'(1P,E13.5)'),a      
+       write(74,'(1P,E13.5)')a      
        rewind(74)
 !  can read the complex in as two reals, one on each line
        read(74,'(E13.5)')r1,r2      
