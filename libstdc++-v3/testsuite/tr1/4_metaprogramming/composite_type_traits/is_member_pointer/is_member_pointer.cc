186e6683b09474fa421314f683f7385585ec0d58
Paolo Carlini <pcarlini@suse.de>
2004-12-24 20:33:56 +0000

    type_traits: Implement is_member_object_pointer, is_member_function_pointer.
    2004-12-24  Paolo Carlini  <pcarlini@suse.de>
    
    	* include/tr1/type_traits: Implement is_member_object_pointer,
    	is_member_function_pointer. N.B. Due to c++/19076, the latter
    	doesn't really work at the moment (a rather ugly work around
    	will be provided in case the front-end bug doesn't get fixed
    	soon); generalize and extend the _DEFINE_SPEC macros.
    	* testsuite/tr1/4_metaprogramming/composite_type_traits/
    	is_member_pointer/is_member_pointer.cc: New.
    	* testsuite/tr1/4_metaprogramming/composite_type_traits/
    	is_member_pointer/typedefs.cc: Likewise.
    	* testsuite/tr1/4_metaprogramming/primary_type_categories/
    	is_member_function_pointer/is_member_function_pointer.cc: Likewise.
    	* testsuite/tr1/4_metaprogramming/primary_type_categories/
    	is_member_function_pointer/typedefs.cc: Likewise.
    	* testsuite/tr1/4_metaprogramming/primary_type_categories/
    	is_member_object_pointer/is_member_object_pointer.cc: Likewise.
    	* testsuite/tr1/4_metaprogramming/primary_type_categories/
    	is_member_object_pointer/typedefs.cc: Likewise.
    
    From-SVN: r92593

---
+++ libstdc++-v3/testsuite/tr1/4_metaprogramming/composite_type_traits/is_member_pointer/is_member_pointer.cc
@@ -0,0 +1,56 @@
+// 2004-12-24  Paolo Carlini  <pcarlini@suse.de>
+//
+// Copyright (C) 2004 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 4.5.2 Composite type traits
+
+#include <tr1/type_traits>
+#include <testsuite_hooks.h>
+#include <testsuite_tr1.h>
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  using std::tr1::is_member_pointer;
+  using namespace __gnu_test;
+
+  VERIFY( (test_category<is_member_pointer, int (ClassType::*)>(true)) );
+  VERIFY( (test_category<is_member_pointer, const int (ClassType::*)>(true)) );
+  VERIFY( (test_category<is_member_pointer, ClassType (ClassType::*)>(true)) );
+
+  // Temporarily disabled because of c++/19076 :-(
+
+  //VERIFY( (test_category<is_member_pointer,
+  //   int (ClassType::*) (int)>(true)) );
+  //VERIFY( (test_category<is_member_pointer,
+  //   int (ClassType::*) (int) const>(true)) );
+  //VERIFY( (test_category<is_member_function_pointer,
+  //   ClassType (ClassType::*) (ClassType)>(true)) );
+  //VERIFY( (test_category<is_member_pointer,
+  //   float (ClassType::*) (int, float, int[], int&)>(true)) );
+  
+  // Sanity check.
+  VERIFY( (test_category<is_member_pointer, ClassType>(false)) );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/tr1/4_metaprogramming/composite_type_traits/is_member_pointer/typedefs.cc
@@ -0,0 +1,36 @@
+// 2004-12-24  Paolo Carlini  <pcarlini@suse.de>
+//
+// Copyright (C) 2004 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 
+// NB: This file is for testing tr1/type_traits with NO OTHER INCLUDES.
+
+#include <tr1/type_traits>
+
+// { dg-do compile }
+
+void test01()
+{
+  // Check for required typedefs
+  typedef std::tr1::is_member_pointer<int>    test_type;
+  typedef test_type::value_type               value_type;
+  typedef test_type::type                     type;
+  typedef test_type::type::value_type         type_value_type;
+  typedef test_type::type::type               type_type;
+}
