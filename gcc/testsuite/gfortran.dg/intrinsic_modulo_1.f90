8241ce4ff22686fb7bb7a23e5a50f7561e705907
tobi <tobi@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-12-27 17:13:07 +0000

    fortran/ PR fortran/19032 * trans-intrinsic.c (gfc_conv_intrinsic_mod): Update comment in front of function to match the standard.  Correct handling of MODULO.
    testsuite/
    PR fortran/19032
    * gfortran.dg/intrinsic_modulo_1.f90: New.
    * gfortran.fortran-torture/execute/intrinsic_mod_ulo.f90: Add
    tests with divisor -1.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@92645 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/intrinsic_modulo_1.f90
@@ -0,0 +1,37 @@
+! { dg-do run }
+! testcase from PR 19032 adapted for testsuite
+! Our implementation of modulo was wrong for P = 1 and P = -1,
+! both in the real and the integer case
+program main
+  integer, parameter :: n=16
+  real, dimension(n) :: ar, br, modulo_result, floor_result
+  integer, dimension(n) :: ai, bi , imodulo_result, ifloor_result
+
+  ai(1:4) = 5
+  ai(5:8) = -5
+  ai(9:12) = 1
+  ai(13:16) = -1
+  bi(1:4) = (/ 3,-3, 1, -1/)
+  bi(5:8) = bi(1:4)
+  bi(9:12) = bi(1:4)
+  bi(13:16) = bi(1:4)
+  ar = ai
+  br = bi
+  modulo_result = modulo(ar,br)
+  imodulo_result = modulo(ai,bi)
+  floor_result = ar-floor(ar/br)*br
+  ifloor_result = nint(real(ai-floor(real(ai)/real(bi))*bi))
+
+  do i=1,n
+     if (modulo_result(i) /= floor_result(i) ) then
+!        print "(A,4F5.0)" ,"real case failed: ", &
+!             ar(i),br(i), modulo_result(i), floor_result(i)
+        call abort()
+    end if
+    if (imodulo_result(i) /= ifloor_result(i)) then
+!       print "(A,4I5)", "int case failed: ", &
+!            ai(i), bi(i), imodulo_result(i), ifloor_result(i)
+       call abort ()
+     end if
+  end do
+end program main
