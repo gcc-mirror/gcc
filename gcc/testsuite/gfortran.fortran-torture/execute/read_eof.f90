9fad9bbe061016d9cdc198f0b10e08364dd2bd21
dje <dje@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-12-30 17:16:39 +0000

    * gfortran.fortran-torture/execute/read_eof.f90: Open scratch         file, not /dev/null.  Abort on error.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@92737 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.fortran-torture/execute/read_eof.f90
+++ gcc/testsuite/gfortran.fortran-torture/execute/read_eof.f90
@@ -1,5 +1,6 @@
 ! PR 13919, segfault when file is empty
-      open(unit=8,file='/dev/null')
+      open(unit=8,status='scratch')
       read(8,*,end=1)i
+      call abort
 1     continue
       end
