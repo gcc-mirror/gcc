bedb9fc04bb56b56d8af671bdfe28cc9052622ec
Richard Henderson <rth@redhat.com>
2005-01-01 23:52:31 -0800

    re PR c/19031 (#pragma weak handling changes in 4.0.0)
            PR c/19031
            * c-decl.c (pop_file_scope): Call maybe_apply_pending_pragma_weaks.
            * c-lang.c (finish_file): Don't do it here.
            * objc/objc-act.c (objc_finish_file): Likewise.
    
            * cgraph.c (decl_assembler_name_equal): New.
            (cgraph_node_for_asm, cgraph_varpool_node_for_asm): New.
            (cgraph_varpool_node): Actually link up cgraph_varpool_nodes.
            * cgraph.h (struct cgraph_varpool_node): Add next.
            (cgraph_node_for_asm, cgraph_varpool_node_for_asm): Declare.
            * varasm.c (assemble_alias): Mark the target as needed.
    
    From-SVN: r92803

---
+++ gcc/testsuite/gcc.dg/attr-alias-2.c
@@ -0,0 +1,20 @@
+/* PR 19031 */
+/* { dg-do link } */
+/* { dg-require-alias } */
+/* { dg-options "-funit-at-a-time" } */
+
+static int f1 (void) { return 0; }
+extern int g1 (void) __attribute__((__alias__("f1")));
+
+#define STR(x) STR1(__USER_LABEL_PREFIX__, x)
+#define STR1(x,y) STR2(x, y)
+#define STR2(x,y) #x #y
+
+static int f2 (void) __asm__(STR(a2));
+static int f2 (void) { return 0; }
+extern int g2 (void) __attribute__((__alias__("a2")));
+
+int main ()
+{
+  return g1() + g2();
+}
