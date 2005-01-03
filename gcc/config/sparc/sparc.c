884c8202275f2bd1762d566d27762f62bf30c097
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-01-03 08:23:58 +0000

    * config/sparc/sparc.h (SPARC_RELAXED_ORDERING): Define to false. 	* config/sparc/linux.h (SPARC_RELAXED_ORDERING): Define to true. 	* config/sparc/linux64.h (SPARC_RELAXED_ORDERING): Likewise. 	* config/sparc/sparc.c (TARGET_RELAXED_ORDERING): Define to 	SPARC_RELAXED_ORDERING.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@92824 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sparc/sparc.c
+++ gcc/config/sparc/sparc.c
@@ -498,20 +498,8 @@ enum processor_type sparc_cpu;
 #define TARGET_ATTRIBUTE_TABLE sparc_attribute_table
 #endif
 
-/* The SPARC v9 architecture defines a relaxed memory ordering model (RMO)
-   which requires this if enabled.  Prior to v9, there are no instructions
-   to even talk about memory syncronization.  The Ultra3 processor doesn't
-   implement RMO.
-
-   Solaris never enables RMO; only ever uses total memory ordering (TMO.
-
-   Linux currently uses RMO in uniprocessor mode, which is equivalent to
-   TMO, and TMO in multiprocessor mode.  But they reserve the right to
-   change their minds.  */
-/* ??? Getting the configury correct is harder than it's worth.  Just
-   enable it all the time.  */
 #undef TARGET_RELAXED_ORDERING
-#define TARGET_RELAXED_ORDERING true
+#define TARGET_RELAXED_ORDERING SPARC_RELAXED_ORDERING
 
 struct gcc_target targetm = TARGET_INITIALIZER;
 
