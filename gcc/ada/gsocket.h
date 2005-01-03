05c3e0e9708795d88a1f0b2f7e653f5bd16dc17c
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-01-03 15:30:02 +0000

    * gen-soccon.c: New utility program to generate g-soccon.ads 	automatically.
    	* socket.c, gsocket.h: Split inclusion of system header files into a
    	gsocket.h file separated from socket.c, to allow reuse in gen-soccon.c.
    
    	* g-soccon.ads, g-soccon-unixware.ads, g-soccon-tru64.ads,
    	g-soccon-aix.ads, g-soccon-irix.ads, g-soccon-hpux.ads,
    	g-soccon-interix.ads, g-soccon-solaris.ads, g-soccon-mingw.ads,
    	g-soccon-vxworks.ads, g-soccon-freebsd.ads: Note that these files
    	should not be edited by hand anymore, but should be regenerated using
    	gen-soccon.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@92828 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/ada/gsocket.h
@@ -0,0 +1,67 @@
+/****************************************************************************
+ *                                                                          *
+ *                         GNAT COMPILER COMPONENTS                         *
+ *                                                                          *
+ *                               S O C K E T                                *
+ *                                                                          *
+ *                              C Header File                               *
+ *                                                                          *
+ *            Copyright (C) 2004, Free Software Foundation, Inc.            *
+ *                                                                          *
+ * GNAT is free software;  you can  redistribute it  and/or modify it under *
+ * terms of the  GNU General Public License as published  by the Free Soft- *
+ * ware  Foundation;  either version 2,  or (at your option) any later ver- *
+ * sion.  GNAT is distributed in the hope that it will be useful, but WITH- *
+ * OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License *
+ * for  more details.  You should have  received  a copy of the GNU General *
+ * Public License  distributed with GNAT;  see file COPYING.  If not, write *
+ * to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, *
+ * MA 02111-1307, USA.                                                      *
+ *                                                                          *
+ * As a  special  exception,  if you  link  this file  with other  files to *
+ * produce an executable,  this file does not by itself cause the resulting *
+ * executable to be covered by the GNU General Public License. This except- *
+ * ion does not  however invalidate  any other reasons  why the  executable *
+ * file might be covered by the  GNU Public License.                        *
+ *                                                                          *
+ * GNAT was originally developed  by the GNAT team at  New York University. *
+ * Extensive contributions were provided by Ada Core Technologies Inc.      *
+ *                                                                          *
+ ****************************************************************************/
+
+#ifdef __vxworks
+#include "vxWorks.h"
+#endif
+
+#ifdef IN_RTS
+#include "tconfig.h"
+#include "tsystem.h"
+
+#if defined (WINNT)
+#define FD_SETSIZE 1024
+#include <windows.h>
+
+#ifdef __MINGW32__
+#include "mingw32.h"
+#if STD_MINGW
+#include <winsock.h>
+#else
+#include <windows32/sockets.h>
+#endif
+#endif
+#endif
+
+#if defined (VMS)
+#define FD_SETSIZE 4096
+#include <sys/time.h>
+#endif
+
+#else
+#include "config.h"
+#include "system.h"
+#endif
+
+#if !(defined (VMS) || defined (__MINGW32__))
+# include <sys/socket.h>
+#endif
