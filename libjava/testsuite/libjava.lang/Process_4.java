cde7853d20991c6fd1d5294ebf7b5b524df25608
Eric Botcazou <ebotcazou@libertysurf.fr>
2005-01-03 20:13:12 +0100

    Process_4.java (Process_4): Expect only a non-zero exit status from 'false'.
    	* testsuite/libjava.lang/Process_4.java (Process_4): Expect only
    	a non-zero exit status from 'false'.
    
    From-SVN: r92860

--- libjava/testsuite/libjava.lang/Process_4.java
+++ libjava/testsuite/libjava.lang/Process_4.java
@@ -9,7 +9,8 @@ public class Process_4
 	String[] a = { "false" };
 	Process p = r.exec(a);
 	int c = p.waitFor();
-	System.out.println(c == 1 ? "ok" : "bad");
+	// Solaris' false doesn't return 1.
+	System.out.println(c != 0 ? "ok" : "bad");
       }
     catch (Exception ex)
       {
