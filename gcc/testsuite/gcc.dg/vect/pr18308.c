8ea9d0c7c932328745976c01943afafaa8c729ee
Paolo Bonzini <bonzini@gnu.org>
2005-01-04 14:37:23 +0000

    re PR tree-optimization/18308 (ICE in do_jump, at dojump.c:274)
    gcc:
    2005-01-04  Paolo Bonzini  <bonzini@gnu.org>
    	    Devang Patel  <dpatel@apple.com>
    
    	PR tree-optimization/18308
    
    	* tree-if-conv.c (add_to_dst_predicate_list): Gimplify
    	the operands before creating a new expression.
    	* dojump.c (do_jump): Make drop_through_label available
    	for all cases.  Add expansion of COND_EXPR.
    
    gcc/testsuite:
    2005-01-04  Paolo Bonzini  <bonzini@gnu.org>
    
    	* gcc.dg/vect/pr18308.c: New testcase.
    
    Co-Authored-By: Devang Patel <dpatel@apple.com>
    
    From-SVN: r92895

---
+++ gcc/testsuite/gcc.dg/vect/pr18308.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-O -ftree-vectorize -funroll-loops" } */
+void foo();
+
+void bar(int j)
+{
+  int i, k=0;
+  for (i = 0; i < 2; ++i)
+    if (j) k = 2;
+
+  if (k) foo();
+}
+
