1aae12a2d9286bedfc5a8b5d6c4fc828ef5d760a
Andreas Jaeger <aj@suse.de>
2005-01-04 07:15:28 +0100

    * gcc.target/i386/amd64-abi-1.c: Only run as 64-bit tests.
    From-SVN: r92887

--- gcc/testsuite/gcc.target/i386/amd64-abi-1.c
+++ gcc/testsuite/gcc.target/i386/amd64-abi-1.c
@@ -1,5 +1,6 @@
 /* { dg-do compile { target x86_64-*-* } } */
 /* { dg-options "-mno-sse" } */
+/* { dg-require-effective-target lp64 } */
 
 double foo(void) { return 0; }	/* { dg-error "SSE disabled" } */
 void bar(double x) { }
