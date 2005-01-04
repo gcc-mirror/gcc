e1f36aa7624c05a914f1de93467a6460ff7f1ece
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2005-01-04 00:20:35 +0000

    Backport:
    	2003-10-13  Geoffrey Keating  <geoffk@apple.com>
    
    	* gcc.dg/asm-names.c: Use scan-assembler-not rather
    	than linker trickery.
    
    From-SVN: r92878

--- gcc/testsuite/gcc.dg/asm-names.c
+++ gcc/testsuite/gcc.dg/asm-names.c
@@ -2,12 +2,11 @@
    to have an underscore prefixed, even if normal symbols are.
    Problem reported by Krister Walfridsson <cato@df.lth.se>.  */
 
-/* { dg-do link } */
+/* { dg-do compile } */
 /* { dg-options "-fleading-underscore" } */
+/* { dg-final { scan-assembler-not "____frob14" } } */
 
 extern void frobnicate (void) asm ("___frob14");  /* three underscores */
-
-void __frob14 (void) {} /* two underscores */
 
 int
 main (void)
@@ -15,13 +14,3 @@ main (void)
   frobnicate ();
   return 0;
 }
-
-/* In case built where the runtime expects no leading underscore on
-   main(). */
-extern int xmain (void) asm ("main");
-
-int xmain (void) { return main(); }
-
-/* In case built where the runtime calls __main.  */
-extern int ymain (void) asm ("___main");
-int ymain (void) { return main(); }
