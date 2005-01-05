e7783f3ed60338810e05489e103908224dfe2158
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-01-05 15:27:26 +0000

    * c-parse.in (asm_string): New.  Don't allow wide strings in 	'asm'. 	(simple_asm_expr, asm_argument, asm_operand, asm_clobbers): Use 	asm_string instead of STRING.
    testsuite:
    	* gcc.dg/asm-wide-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@92952 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/asm-wide-1.c
@@ -0,0 +1,35 @@
+/* Wide string literals should not be allowed in asm.  */
+/* Origin: Joseph Myers <joseph@codesourcery.com> */
+/* { dg-do compile } */
+/* { dg-options "" } */
+
+int foo asm (L"bar"); /* { dg-error "error: wide string literal in 'asm'" } */
+
+asm (L"foo"); /* { dg-error "error: wide string literal in 'asm'" } */
+
+void
+f (void)
+{
+  int x = 1;
+  asm (L"foo"); /* { dg-error "error: wide string literal in 'asm'" } */
+  asm ("foo" :
+       L"=g" (x)); /* { dg-error "error: wide string literal in 'asm'" } */
+  asm ("foo" : [x]
+       L"=g" (x)); /* { dg-error "error: wide string literal in 'asm'" } */
+  asm ("foo" : [x] "=g" (x),
+       L"=g" (x)); /* { dg-error "error: wide string literal in 'asm'" } */
+  asm ("foo" : :
+       L"g" (x)); /* { dg-error "error: wide string literal in 'asm'" } */
+  asm ("foo" : : :
+       L"memory"); /* { dg-error "error: wide string literal in 'asm'" } */
+  asm ("foo" : : : "memory",
+       L"memory"); /* { dg-error "error: wide string literal in 'asm'" } */
+}
+
+/* Extra errors from the substitution of "" for wide strings: */
+/* { dg-error "output" "output" { target *-*-* } 16 } */
+/* { dg-error "output" "output" { target *-*-* } 18 } */
+/* { dg-error "output" "output" { target *-*-* } 20 } */
+/* { dg-warning "match" "match" { target *-*-* } 21 } */
+/* { dg-error "register" "register" { target *-*-* } 23 } */
+/* { dg-error "register" "register" { target *-*-* } 25 } */
