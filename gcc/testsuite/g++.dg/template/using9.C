461cdeb308831293f70f01e5506be07cf7e84959
lerdsuwa <lerdsuwa@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-01-06 16:04:05 +0000

    PR c++/17154 	* search.c (lookup_field_1): Handle using declaration in 	class template partial specialization.
    	* g++.dg/template/using9.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@92994 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/using9.C
@@ -0,0 +1,12 @@
+// { dg-do compile }
+
+// Origin: stefaandr@hotmail.com
+
+// PR c++/17154: Using declaration in partial class template specialization.
+
+template <int numrows, class T> struct A { void test_A() {}; };
+template <int numrows, class T> struct B {};
+template <class T> struct B <3, T> : public A <3, T> {
+	using A <3, T>::test_A;
+	void test_B_spec() { test_A(); };
+};
