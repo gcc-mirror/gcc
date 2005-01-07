59d7d7678602bb8dcbeff337a44efc76a3451f01
Geoffrey Keating <geoffk@apple.com>
2005-01-07 00:19:23 +0000

    c-cppbuiltin.c (builtin_define_float_constants): Set __*_EPSILON__ for IBM long double format correctly.
    	* c-cppbuiltin.c (builtin_define_float_constants): Set __*_EPSILON__
    	for IBM long double format correctly.
    
    From-SVN: r93021

---
+++ gcc/testsuite/gcc.dg/rs6000-ldouble-2.c
@@ -0,0 +1,22 @@
+/* { dg-do run { target powerpc*-*-darwin* powerpc*-*-aix* powerpc64-*-linux rs6000-*-* } } */
+/* { dg-options "-mlong-double-128" } */
+
+/* Check that LDBL_EPSILON is right for 'long double'.  */
+
+#include <float.h>
+
+extern void abort (void);
+
+int main(void)
+{
+  volatile long double ee = 1.0;
+  long double eps = ee;
+  while (ee + 1.0 != 1.0)
+    {
+      eps = ee;
+      ee = eps / 2;
+    }
+  if (eps != LDBL_EPSILON)
+    abort ();
+  return 0;
+}
