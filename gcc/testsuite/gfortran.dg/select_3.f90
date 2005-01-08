1dba11822e73daaaf1afc31ab17d616d85c217e3
Steven G. Kargl <kargls@comcast.net>
2005-01-08 18:31:40 +0100

    select_2.f90, [...]: New.
    * gfortran.dg/select_2.f90, gfortran.dg/select_3.f90,
    gfortran.dg/select_4.f90: New.
    
    From-SVN: r93087

---
+++ gcc/testsuite/gfortran.dg/select_3.f90
@@ -0,0 +1,19 @@
+! [dg-do run }
+!  Simple test program to see if gfortran eliminates the 'case (3:2)'
+!  statement.  This is an unreachable CASE because the range is empty.
+!
+program select_3
+  integer i
+  do i = 1, 4
+     select case(i)
+     case (1)
+       if (i /= 1) call abort
+     case (3:2)
+       call abort
+     case (4)
+       if (i /= 4) call abort
+     case default
+       if (i /= 2 .and. i /= 3) call abort
+     end select
+  end do
+end program select_3
