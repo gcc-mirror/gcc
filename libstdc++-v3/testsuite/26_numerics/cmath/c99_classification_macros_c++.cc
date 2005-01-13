46d927bc300f9379515446568736c430625c6122
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-01-13 19:33:40 +0000

    2005-01-13  Paolo Carlini  <pcarlini@suse.de>
    	* testsuite/26_numerics/cmath/c99_classification_macros_c++.cc:
    	Re-enable commented-out test for double type.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@93609 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/26_numerics/cmath/c99_classification_macros_c++.cc
+++ libstdc++-v3/testsuite/26_numerics/cmath/c99_classification_macros_c++.cc
@@ -1,6 +1,6 @@
 // 2001-04-06 gdr
 
-// Copyright (C) 2001 Free Software Foundation, Inc.
+// Copyright (C) 2001, 2005 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -85,7 +85,7 @@ int main()
 {
 #if _GLIBCXX_USE_C99
   test_c99_classify<float>();
-  //test_c99_classify<double>();
+  test_c99_classify<double>();
 #endif
   return 0;
 }
