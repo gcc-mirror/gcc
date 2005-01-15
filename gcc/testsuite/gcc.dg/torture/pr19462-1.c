01c4f82a926915f1aaa7733ac026984eb5787e56
Hans-Peter Nilsson <hp@axis.com>
2005-01-15 21:30:41 +0000

    re PR rtl-optimization/19462 (generating return insns while current_function_epilogue_delay_list nonempty)
    	PR rtl-optimization/19462
    	* gcc.dg/torture/pr19462-1.c: New test.
    
    From-SVN: r93709

---
+++ gcc/testsuite/gcc.dg/torture/pr19462-1.c
@@ -0,0 +1,56 @@
+/* { dg-do compile { xfail cris-*-* } } */
+/* PR rtl-optimization/19462
+   Delayed branch bug: return insns generated in presence of
+   current_function_epilogue_delay_list.  See also PR target/7042.  */
+
+/* The test is from newlib/libc/string/wcscspn.c, which has the following
+   copyright and licensing notice, which by inclusion this file supposedly
+   complies with:
+
+ * Copyright (c)1999 Citrus Project,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.  */
+
+
+unsigned long
+wcscspn(const long *s, const long *set)
+{
+  const long *p;
+  const long *q;
+
+  p = s;
+  while (*p)
+    {
+      q = set;
+      while (*q)
+        {
+          if (*p == *q)
+            goto done;
+          q++;
+        }
+      p++;
+    }
+
+done:
+  return (p - s);
+}
