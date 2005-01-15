de2a399ddc1d2141952a4e928a2ebf64b9ad453b
John David Anglin <danglin@gcc.gnu.org>
2005-01-15 22:57:16 +0000

    Update copyrights to 2005.
    From-SVN: r93714

--- gcc/config/pa/pa-protos.h
+++ gcc/config/pa/pa-protos.h
@@ -1,5 +1,6 @@
 /* Prototypes for pa.c functions used in the md file & elsewhere.
-   Copyright (C) 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
+   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation,
+   Inc.
 
 This file is part of GCC.
 
