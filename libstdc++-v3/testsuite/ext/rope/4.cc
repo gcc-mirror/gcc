6659f3e5ff27f2454e905b7bc0aaca166396cde2
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-01-16 19:16:27 +0000

    2005-01-16  Jonathan Wakely  <redi@gcc.gnu.org>
    	* include/ext/rope: Qualify calls to std::copy() by sequence_buffer.
    	* testsuite/ext/rope/4.cc: Add.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@93736 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/ext/rope/4.cc
@@ -0,0 +1,40 @@
+// Copyright (C) 2005 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// sequence_buffer (SGI extension)
+
+#include <ext/rope>
+#include <algorithm>
+#include <testsuite_hooks.h>
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  __gnu_cxx::crope r1("wibble");
+  __gnu_cxx::crope r2;
+  std::copy( r1.begin(), r1.end(),
+             __gnu_cxx::sequence_buffer<__gnu_cxx::crope>(r2) );
+  VERIFY( r1 == r2 );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
