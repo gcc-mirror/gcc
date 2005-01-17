ac317859d409434b0f5ef8ec89a7abcacb10bd3e
Paolo Carlini <pcarlini@suse.de>
2005-01-17 14:14:26 +0000

    re PR libstdc++/19433 (set, multiset, map, multimap misuse hint on insert)
    2005-01-17  Paolo Carlini  <pcarlini@suse.de>
    
    	PR libstdc++/19433
    	* include/bits/stl_tree.h (_Rb_tree<>::insert_unique(iterator,
    	const _Val&), _Rb_tree<>::insert_equal(iterator, const _Val&)):
    	Obtain amortized constant complexity if t is inserted right after
    	p - not before p - as per Table 69.
    	* testsuite/performance/23_containers/set_insert_from_sorted.cc: New.
    
    	* testsuite/23_containers/multiset/insert/2.cc: New.
    	* testsuite/23_containers/set/insert/1.cc: Likewise.
    
    	* testsuite/performance/23_containers/set_create_from_sorted.cc:
    	Simplify.
    
    	* include/bits/stl_tree.h: Add a few missing std:: qualifications.
    
    From-SVN: r93761

--- libstdc++-v3/testsuite/23_containers/multiset/insert/2.cc
+++ libstdc++-v3/testsuite/23_containers/multiset/insert/2.cc
@@ -1,3 +1,5 @@
+// 2005-01-17  Paolo Carlini  <pcarlini@suse.de>
+
 // Copyright (C) 2005 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
@@ -5,17 +7,17 @@
 // terms of the GNU General Public License as published by the
 // Free Software Foundation; either version 2, or (at your option)
 // any later version.
-
+//
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-
+//
 // You should have received a copy of the GNU General Public License along
 // with this library; see the file COPYING.  If not, write to the Free
 // Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
 // USA.
-
+//
 // As a special exception, you may use this file as part of a free software
 // library without restriction.  Specifically, if other files instantiate
 // templates or use macros or inline functions from this file, or you compile
@@ -25,40 +27,71 @@
 // invalidate any other reasons why the executable file might be covered by
 // the GNU General Public License.
 
-#include <vector>
 #include <set>
-#include <sstream>
-#include <testsuite_performance.h>
+#include <testsuite_hooks.h>
 
-static const unsigned max_size = 1000000; // avoid excessive swap file use!
-static const unsigned iterations = 10;    // make results less random while
-static const unsigned step = 50000;       // keeping the total time reasonable
-
-// libstdc++/19422
-int main()
+// A few tests for insert with hint, in the occasion of libstdc++/19422
+// and libstdc++/19433.
+void test01()
 {
+  bool test __attribute__((unused)) = true;
   using namespace std;
-  using namespace __gnu_test;
-  time_counter time;
-  resource_counter resource;
 
-  typedef set<unsigned>  the_set;
+  multiset<int> ms0, ms1;
+  multiset<int>::iterator iter1;
+  
+  ms0.insert(1);
+  ms1.insert(ms1.end(), 1);
+  VERIFY( ms0 == ms1 );
 
-  vector<unsigned> v(max_size, 0);
-  for (unsigned i = 0; i != max_size; ++i)
-    v[i] = i; // initialize sorted array
+  ms0.insert(3);
+  ms1.insert(ms1.begin(), 3);
+  VERIFY( ms0 == ms1 );
 
-  for (unsigned count = step; count <= max_size; count += step)
-    {
-      ostringstream oss;
-      oss << count;
+  ms0.insert(4);
+  iter1 = ms1.insert(ms1.end(), 4);
+  VERIFY( ms0 == ms1 );
 
-      // measure set construction time (linear in count (Table 69))
-      start_counters(time, resource);
-      for (unsigned i = 0; i != iterations; ++i)
-	the_set(v.begin(), v.begin() + count);
-      stop_counters(time, resource);
-      report_performance(__FILE__, oss.str(), time, resource);
-      clear_counters(time, resource);
-    }
+  ms0.insert(6);
+  ms1.insert(iter1, 6);
+  VERIFY( ms0 == ms1 );
+
+  ms0.insert(2);
+  ms1.insert(ms1.begin(), 2);
+  VERIFY( ms0 == ms1 );
+
+  ms0.insert(7);
+  ms1.insert(ms1.end(), 7);
+  VERIFY( ms0 == ms1 );
+
+  ms0.insert(5);
+  ms1.insert(ms1.find(4), 5);
+  VERIFY( ms0 == ms1 );
+
+  ms0.insert(0);
+  ms1.insert(ms1.end(), 0);
+  VERIFY( ms0 == ms1 );
+
+  ms0.insert(8);
+  ms1.insert(ms1.find(3), 8);
+  VERIFY( ms0 == ms1 );
+  
+  ms0.insert(9);
+  ms1.insert(ms1.end(), 9);
+  VERIFY( ms0 == ms1 );
+
+  ms0.insert(10);
+  ms1.insert(ms1.begin(), 10);
+  VERIFY( ms0 == ms1 );
+}
+
+#if !__GXX_WEAK__ && _MT_ALLOCATOR_H
+// Explicitly instantiate for systems with no COMDAT or weak support.
+template class __gnu_cxx::__mt_alloc<std::_Rb_tree_node<int> >;
+#endif
+
+int main ()
+{
+  test01();
+  return 0;
 }
