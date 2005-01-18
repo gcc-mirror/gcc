bb133e7a1d3529f4ab9e1b6b2b76595ec5db4869
kkojima <kkojima@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-01-18 23:50:28 +0000

    * config/sh/linux.h (TARGET_C99_FUNCTIONS): Define.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@93868 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sh/linux.h
+++ gcc/config/sh/linux.h
@@ -1,5 +1,6 @@
 /* Definitions for SH running Linux-based GNU systems using ELF
-   Copyright (C) 1999, 2000, 2002, 2003, 2004 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2000, 2002, 2003, 2004, 2005
+   Free Software Foundation, Inc.
    Contributed by Kazumoto Kojima <kkojima@rr.iij4u.or.jp>
 
 This file is part of GCC.
@@ -109,6 +110,10 @@ do { \
 #ifdef HAVE_LD_AS_NEEDED
 #define USE_LD_AS_NEEDED 1
 #endif
+
+/* Determine whether the the entire c99 runtime
+   is present in the runtime library.  */
+#define TARGET_C99_FUNCTIONS 1
 
 /* Output assembler code to STREAM to call the profiler.  */
 
