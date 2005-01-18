f7ef2277cc99710d6da4ef3469425062a0e555c4
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-01-18 11:45:39 +0000

    * Make-lang.in, call.c, cvt.c, init.c, rtti.c, tree.c, 	typeck2.c: Update copyright.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@93828 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/rtti.c
+++ gcc/cp/rtti.c
@@ -1,5 +1,6 @@
 /* RunTime Type Identification
-   Copyright (C) 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004
+   Copyright (C) 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
+   2005
    Free Software Foundation, Inc.
    Mostly written by Jason Merrill (jason@cygnus.com).
 
