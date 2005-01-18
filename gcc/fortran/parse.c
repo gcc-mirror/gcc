ec3781807e43bb3ea027ba72514918aca1915f27
Kazu Hirata <kazu@cs.umass.edu>
2005-01-18 12:11:56 +0000

    arith.c, [...]: Update copyright.
    	* arith.c, array.c, check.c, decl.c, expr.c, f95-lang.c,
    	gfortran.h, interface.c, intrinsic.c, io.c, iresolve.c,
    	match.c, matchexp.c, misc.c, module.c, options.c, parse.c,
    	scanner.c, simplify.c, symbol.c, trans-array.c, trans-expr.c,
    	trans-io.c, trans-stmt.c, trans.c: Update copyright.
    
    From-SVN: r93830

--- gcc/fortran/parse.c
+++ gcc/fortran/parse.c
@@ -1,5 +1,5 @@
 /* Main parser.
-   Copyright (C) 2000, 2001, 2002, 2003, 2004 Free Software Foundation, 
+   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, 
    Inc.
    Contributed by Andy Vaught
 
