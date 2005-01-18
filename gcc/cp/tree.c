3638a282e4e76c72f6a8d61041e564b165e1a261
Kazu Hirata <kazu@cs.umass.edu>
2005-01-18 11:45:39 +0000

    Make-lang.in, [...]: Update copyright.
    	* Make-lang.in, call.c, cvt.c, init.c, rtti.c, tree.c,
    	typeck2.c: Update copyright.
    
    From-SVN: r93828

--- gcc/cp/tree.c
+++ gcc/cp/tree.c
@@ -1,6 +1,6 @@
 /* Language-dependent node constructors for parse phase of GNU compiler.
    Copyright (C) 1987, 1988, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
+   1999, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
    Hacked by Michael Tiemann (tiemann@cygnus.com)
 
 This file is part of GCC.
