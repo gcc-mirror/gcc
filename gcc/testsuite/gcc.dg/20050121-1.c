15c8b2d40aa9a338654a3647a427f64ff111bca6
Ian Lance Taylor <ian@airs.com>
2005-01-21 19:23:06 +0000

    * gcc.dg/20050121-1.c: New test.
    From-SVN: r94029

---
+++ gcc/testsuite/gcc.dg/20050121-1.c
@@ -0,0 +1,9 @@
+/* I accidentally broke this while developing a patch for PR 13000,
+   and didn't notice since the testsuite didn't catch it -- ian  */
+/* { dg-do-compile } */
+
+void foo()
+{
+  return;
+  break;	/* { dg-warning "break statement not within" } */
+}
