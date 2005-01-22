d7f3fc19903a41f07d22b0b7b38d06e16cff55b8
Richard Sandiford <rsandifo@redhat.com>
2005-01-22 17:52:44 +0000

    re PR tree-optimization/19484 (function pointer propagation fails for noreturn functions)
    	PR tree-optimization/19484
    	* tree-cfg.c (remove_fallthru_edge): New function.
    	(cleanup_control_flow): Remove fallthru edges from calls that are
    	now known not to return.
    
    From-SVN: r94070

---
+++ gcc/testsuite/gcc.c-torture/compile/20050122-2.c
@@ -0,0 +1,19 @@
+/* Related to PR 19484.  */
+extern void foo (void) __attribute__((noreturn));
+int n;
+
+void
+g (void)
+{
+  __label__ lab;
+  void h (void) { if (n == 2) goto lab; }
+  void (*f1) (void) = foo;
+  void (*f2) (void) = h;
+
+  f2 ();
+  if (n)
+    f1 ();
+  n = 1;
+ lab:
+  n++;
+}
