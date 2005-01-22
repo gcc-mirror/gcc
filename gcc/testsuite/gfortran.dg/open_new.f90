d02b2c6474afab3e4d5748eca254bcf9aa12fa18
Thomas Koenig <Thomas.Koenig@online.de>
2005-01-22 19:49:18 +0000

    re PR libfortran/18982 (open(status="new") does not generate an error if the file exists)
    2005-01-22  Thomas Koenig  <Thomas.Koenig@online.de>
    
    	PR libfortran/18982
    	* io/unix.c (regular_file):  No need to change flags->action
    	if an error occurs.  Document this.
    	No need to call stat() for STATUS_OLD, open() will
    	fail anyway.
    	For ACTION_UNSPECIFIED, try open for read-write, then for
    	read-only if open fails with EACCES, then for write-only
    	if that fails with EACCES again.
    	* io/unix.c (open_external): Document changed behavior of
    	regular_file.
    testsuite/
    	* gfortran.dg/open_new.f90: New file.
    
    From-SVN: r94076

---
+++ gcc/testsuite/gfortran.dg/open_new.f90
@@ -0,0 +1,11 @@
+! { dg do-run }
+! PR 18982:  verifies that opening an existing file with
+!            status="new" is an error
+program main
+  nout = 10
+  open(nout, file="foo.dat", status="replace")     ! make sure foo.dat exists
+  close(nout)
+  open(nout, file="foo.dat", status="new",err=100)
+  call abort                 ! This should never happen
+100 continue
+end program main
