d64f69365ac6e3b3cba4921fc1936af226066ba7
Roger Sayle <roger@eyesopen.com>
2005-01-23 05:16:14 +0000

    re PR target/18402 (ICE in gen_split_1204 on i686-pc-linux-gnu target)
    	PR target/18402
    	Backport from mainline
    	2003-02-05  Jakub Jelinek  <jakub@redhat.com>
    
     	PR optimization/8555
     	* config/i386/i386.md (sse_mov?fcc split): Handle op2 == op3 case
     	instead of aborting.
    
    	* gcc.dg/20030204-1.c: New test.
    
    From-SVN: r94103

--- gcc/testsuite/gcc.dg/20030204-1.c
+++ gcc/testsuite/gcc.dg/20030204-1.c
@@ -2,7 +2,6 @@
 /* { dg-do compile } */
 /* { dg-options "-O -ffast-math -funroll-loops" } */
 /* { dg-options "-march=pentium3 -O -ffast-math -funroll-loops" { target i?86-*-* } } */
-/* { dg-skip-if "" { i?86-*-* } { "-m64" } { "" } } */
 
 float foo (float *a, int i)
 {
