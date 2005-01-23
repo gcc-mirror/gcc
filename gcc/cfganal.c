708bde14eaf5568ecc2fdb23a4cb15762f117f7c
Kazu Hirata <kazu@cs.umass.edu>
2005-01-23 20:13:00 +0000

    * cfganal.c: Fix a reference to Harvey's paper.
    From-SVN: r94126

--- gcc/cfganal.c
+++ gcc/cfganal.c
@@ -936,7 +936,7 @@ dfs_enumerate_from (basic_block bb, int reverse,
 /* Compute dominance frontiers, ala Harvey, Ferrante, et al.
    
    This algorithm can be found in Timothy Harvey's PhD thesis, at
-   http://www.cs.rice.edu/~harv/thesis.pdf in the section on iterative
+   http://www.cs.rice.edu/~harv/dissertation.pdf in the section on iterative
    dominance algorithms.
 
    First, we identify each join point, j (any node with more than one
