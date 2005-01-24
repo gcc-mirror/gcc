c0fd73a9ad4a2bc312fe470fcf7b8924f919c1e0
Richard Sandiford <rsandifo@redhat.com>
2005-01-24 21:42:50 +0000

    irix-csr.c: New file.
    	* config/mips/irix-csr.c: New file.
    	* config/mips/t-iris6 (irix-csr.o): New rule to build it.
    	(EXTRA_MULTILIB_PARTS): Add irix-csr.o.
    	* config/mips/iris6.h (ENDFILE_SPEC): Include it in n32 and n64
    	executables.
    
    From-SVN: r94186

--- gcc/config/mips/iris6.h
+++ gcc/config/mips/iris6.h
@@ -97,7 +97,8 @@ Boston, MA 02111-1307, USA.  */
 
 #undef ENDFILE_SPEC
 #define ENDFILE_SPEC \
-  "crtend.o%s irix-crtn.o%s \
+  "%{!shared:%{mabi=n32|mabi=64:irix-csr.o%s}} \
+   crtend.o%s irix-crtn.o%s \
    %{!shared: \
      %{mabi=32:crtn.o%s}\
      %{mabi=n32:%{mips4:/usr/lib32/mips4/crtn.o%s}\
