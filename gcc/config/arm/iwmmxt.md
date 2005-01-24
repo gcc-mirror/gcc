deeec1d8a85b6a2d6a2b05153ccb18df0a6df905
Kazu Hirata <kazu@cs.umass.edu>
2005-01-24 11:59:45 +0000

    c-opts.c, [...]: Update copyright.
    	* c-opts.c, expmed.c, gengtype.c, tree-complex.c,
    	config/arm/iwmmxt.md, config/avr/avr.c, config/s390/s390.h:
    	Update copyright.
    
    From-SVN: r94154

--- gcc/config/arm/iwmmxt.md
+++ gcc/config/arm/iwmmxt.md
@@ -1,5 +1,5 @@
 ;; Patterns for the Intel Wireless MMX technology architecture.
-;; Copyright (C) 2003, 2004 Free Software Foundation, Inc.
+;; Copyright (C) 2003, 2004, 2005 Free Software Foundation, Inc.
 ;; Contributed by Red Hat.
 
 ;; This file is part of GCC.
--- gcc/config/avr/avr.c
+++ gcc/config/avr/avr.c
@@ -1,5 +1,5 @@
 /* Subroutines for insn-output.c for ATMEL AVR micro controllers
-   Copyright (C) 1998, 1999, 2000, 2001, 2002, 2004
+   Copyright (C) 1998, 1999, 2000, 2001, 2002, 2004, 2005
    Free Software Foundation, Inc.
    Contributed by Denis Chertykov (denisc@overta.ru)
 
