a607ffb19c99cfa6acf803b51544b334c9f2fef7
uweigand <uweigand@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-01-27 01:49:01 +0000

    ChangeLog: 	* dbxout.c (dbxout_symbol_location): Resolve constant pool references 	even for variables with NULL DECL_INITIAL.
    testsuite/ChangeLog:
    	* gcc.dg/20041216-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@94303 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20041216-1.c
@@ -0,0 +1,23 @@
+/* This test case would get an unresolved symbol during link
+   because stabs referred to an optimized-away literal pool
+   entry.  */
+
+/* { dg-do run { target s390*-*-* } } */
+/* { dg-options "-O2 -fno-omit-frame-pointer -gstabs" } */
+
+int main (void)
+{
+  static char buf[4096];
+  char *p;
+
+  do
+    {
+      p = buf;
+      asm volatile ("" : : : "memory", "0", "1", "2", "3", "4", "5", "6",
+				       "7", "8", "9", "10", "12");
+    }
+  while (*p);
+
+  return 0;
+}
+
