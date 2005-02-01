f4a11c6009b0eaa86403b03faae77555465e1126
Janis Johnson <janis187@us.ibm.com>
2005-02-01 21:27:16 +0000

    ppc64-toc.c: Don't explicitly use -m64.
    	* gcc.dg/ppc64-toc.c: Don't explicitly use -m64.
    	* gcc.dg/ppc-eabi.c: Don't explicity use -m32.
    
    From-SVN: r94562

--- gcc/testsuite/gcc.dg/ppc-eabi.c
+++ gcc/testsuite/gcc.dg/ppc-eabi.c
@@ -1,4 +1,4 @@
 /* PR target/16952 */
-/* { dg-do compile { target powerpc*-*-linux* } } */
-/* { dg-options "-m32 -meabi -mrelocatable" } */
+/* { dg-do compile { target { powerpc*-*-linux* && ilp32 } } } */
+/* { dg-options "-meabi -mrelocatable" } */
 char *s = "boo";
