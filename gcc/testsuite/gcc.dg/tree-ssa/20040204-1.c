bf996cfe67f50fd386605f07dcc4076da604026f
Janis Johnson <janis187@us.ibm.com>
2005-02-01 21:35:18 +0000

    * gcc.dg/tree-ssa/20040204-1.c: Remove powerpc from xfail list.
    From-SVN: r94566

--- gcc/testsuite/gcc.dg/tree-ssa/20040204-1.c
+++ gcc/testsuite/gcc.dg/tree-ssa/20040204-1.c
@@ -33,4 +33,4 @@ void test55 (int x, int y)
    that the && should be emitted (based on BRANCH_COST).  Fix this
    by teaching dom to look through && and register all components
    as true.  */
-/* { dg-final { scan-tree-dump-times "link_error" 0 "optimized" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-times "link_error" 0 "optimized" { xfail { ! powerpc*-*-* } } } } */
