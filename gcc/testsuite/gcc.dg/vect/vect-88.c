cb2ce56c15eda096f8463b60113bf7cfb39d3b9f
dorit <dorit@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-02-03 08:19:46 +0000

    * gcc.dg/vect/vect-85.c: Remove xfail.         * gcc.dg/vect/vect-86.c: Remove xfail.         * gcc.dg/vect/vect-87.c: Remove xfail.         * gcc.dg/vect/vect-88.c: Remove xfail.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@94654 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/vect/vect-88.c
+++ gcc/testsuite/gcc.dg/vect/vect-88.c
@@ -49,8 +49,7 @@ int main (void)
   return 0;
 }
 
-/* Fails due to invariant phi introduced by PRE.
-   Fails for targets that don't vectorize PLUS (e.g alpha).  */
-/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { xfail *-*-* } } } */
+/* Fails for targets that don't vectorize PLUS (e.g alpha).  */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { xfail alpha*-*-* } } } */
 /* { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 0 "vect" } } */
-/* { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 1 "vect" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 1 "vect" { xfail alpha*-*-* } } } */
