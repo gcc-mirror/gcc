c1105188df0343cdb77572c90ae81a4c25c10960
Geoffrey Keating <geoffk@gcc.gnu.org>
2005-02-06 08:51:32 +0000

    * gcc.dg/altivec-19.c: New.
    From-SVN: r94670

---
+++ gcc/testsuite/gcc.dg/altivec-19.c
@@ -0,0 +1,12 @@
+/* { dg-do compile { target powerpc*-*-* } } */
+/* { dg-options "-maltivec" } */
+/* { dg-final { scan-assembler "dst" } } */
+
+void foo ( char* image )
+{
+  while ( 1 )
+    {
+      __builtin_altivec_dst( (void *)( (long)image & ~0x0f ), 0, 0 );
+      image += 48;
+    }
+}
