d5328f2af761ead752e63ac5c10f919084f5631a
daney <daney@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-02-11 01:12:57 +0000

    2005-02-10  David Daney  <ddaney@avtrex.com>
    	* config/alpha/linux-unwind.h: Add exception clause to copyright.
    	* config/i386/linux-unwind.h: Likewise.
    	* config/ia64/linux-unwind.h: Likewise.
    	* config/mips/linux-unwind.h: Likewise.
    	* config/pa/linux-unwind.h: Likewise.
    	* config/rs6000/linux-unwind.h: Likewise.
    	* config/s390/linux-unwind.h: Likewise.
    	* config/sh/linux-unwind.h: Likewise.
    	* config/sparc/linux-unwind.h: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@94868 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/ia64/linux-unwind.h
+++ gcc/config/ia64/linux-unwind.h
@@ -1,5 +1,5 @@
 /* DWARF2 EH unwinding support for IA64 Linux.
-   Copyright (C) 2004 Free Software Foundation, Inc.
+   Copyright (C) 2004, 2005 Free Software Foundation, Inc.
 
    This file is part of GCC.
 
@@ -7,6 +7,14 @@
    under the terms of the GNU General Public License as published
    by the Free Software Foundation; either version 2, or (at your
    option) any later version.
+
+   In addition to the permissions in the GNU General Public License,
+   the Free Software Foundation gives you unlimited permission to link
+   the compiled version of this file with other programs, and to
+   distribute those programs without any restriction coming from the
+   use of this file.  (The General Public License restrictions do
+   apply in other respects; for example, they cover modification of
+   the file, and distribution when not linked into another program.)
 
    GCC is distributed in the hope that it will be useful, but WITHOUT
    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
