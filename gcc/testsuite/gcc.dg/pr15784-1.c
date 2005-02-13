6f49fdcc5efbe3d098c49b62c94646bc503b4d3d
James A. Morrison <phython@gcc.gnu.org>
2005-02-13 06:41:01 +0000

    pr15784-1.c, [...]: New tests.
    2005-02-13  James A. Morrison  <phython@gcc.gnu.org>
    
           * gcc.dg/pr15784-1.c, gcc.dg/pr15784-2.c, gcc.dg/pr15784-3.c: New tests.
    
    From-SVN: r94978

---
+++ gcc/testsuite/gcc.dg/pr15784-1.c
@@ -0,0 +1,42 @@
+/* { dg-do compile } */
+/* { dg-options "-fdump-tree-generic" } */
+/* Test for folding abs(x) where appropriate.  */
+#define abs(x) x > 0 ? x : -x
+extern double fabs (double);
+
+int a (int x) {
+	return (abs(x)) >= 0;
+}
+
+int b (int x) {
+	return (abs(x)) == 0;
+}
+
+int c (int x) {
+	return (abs(x)) != 0;
+}
+
+int d (int x) {
+	return 0 != (abs(x));
+}
+
+int e (int x) {
+	return 0 == (abs(x));
+}
+
+int f (int x) {
+	return 0 <= (abs(x));
+}
+
+int g (int x) {
+	return 0 > (abs(x));
+}
+
+int h (float x) {
+	return 0.0 > fabs(x);
+}
+
+int i (float x) {
+	return fabs(x) == -0.0;
+}
+/* { dg-final { scan-tree-dump-times "ABS_EXPR" 0 "generic" } } */
