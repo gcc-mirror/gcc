a1212c35003bd4ab215b2710a357346e6563488d
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-02-14 12:01:15 +0000

    gcc/cp/ChangeLog: PR c++/17816 * decl.c (redeclaration_error_message): Report redefinition of pure virtual function. gcc/testsuite/ChangeLog: PR c++/17816 * g++.dg/other/redecl2.C: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95008 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/other/redecl2.C
@@ -0,0 +1,11 @@
+// PR c++/17816
+// We failed to report duplicate definitions of pure virtual ns.
+
+// { dg-do compile }
+
+struct S {
+  virtual int foo() = 0;
+};
+ 
+int S::foo() { return 0; } // { dg-error "defined here" }
+int S::foo() { return 0; } // { dg-error "redefinition" }
