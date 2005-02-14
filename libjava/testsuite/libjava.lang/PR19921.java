5728cb66879693b5487590579e0022b1e1e5175c
Tom Tromey <tromey@redhat.com>
2005-02-14 20:56:11 +0000

    * testsuite/libjava.lang/PR19921.java: Use correct class name.
    From-SVN: r95046

--- libjava/testsuite/libjava.lang/PR19921.java
+++ libjava/testsuite/libjava.lang/PR19921.java
@@ -2,7 +2,7 @@ interface I {
     void f(Object x);
 }
 
-class PR19929 {
+class PR19921 {
     static void g(I i) {
       // gcj used to create invalid bytecode for this.
       i.f(new Object[1][1]);
