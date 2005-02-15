8440403e21f9fddf4c61e591873cf871b28e2007
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-02-15 19:37:13 +0000

    * decl2.c (mark_used): Set the source location of the used decl to the current input location here... * method.c (synthesize_method): ... not here.  Set input_location from the decl instead.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95070 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/method.c
+++ gcc/cp/method.c
@@ -705,12 +705,15 @@ synthesize_method (tree fndecl)
   tree context = decl_function_context (fndecl);
   bool need_body = true;
   tree stmt;
+  location_t save_input_location = input_location;
 
   /* If we've been asked to synthesize a clone, just synthesize the
      cloned function instead.  Doing so will automatically fill in the
      body for the clone.  */
   if (DECL_CLONED_FUNCTION_P (fndecl))
     {
+      DECL_SOURCE_LOCATION (DECL_CLONED_FUNCTION (fndecl)) =
+	DECL_SOURCE_LOCATION (fndecl);
       synthesize_method (DECL_CLONED_FUNCTION (fndecl));
       return;
     }
@@ -724,13 +727,7 @@ synthesize_method (tree fndecl)
   else if (nested)
     push_function_context_to (context);
 
-  /* Put the function definition at the position where it is needed,
-     rather than within the body of the class.  That way, an error
-     during the generation of the implicit body points at the place
-     where the attempt to generate the function occurs, giving the
-     user a hint as to why we are attempting to generate the
-     function.  */
-  DECL_SOURCE_LOCATION (fndecl) = input_location;
+  input_location = DECL_SOURCE_LOCATION (fndecl);
 
   start_preparsed_function (fndecl, NULL_TREE, SF_DEFAULT | SF_PRE_PARSED);
   stmt = begin_function_body ();
@@ -760,6 +757,8 @@ synthesize_method (tree fndecl)
 
   finish_function_body (stmt);
   expand_or_defer_fn (finish_function (0));
+
+  input_location = save_input_location;
 
   if (! context)
     pop_from_top_level ();
