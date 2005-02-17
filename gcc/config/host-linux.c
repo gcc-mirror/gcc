3bb6785a76262a13038c5e439549d1615c7fb361
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-02-17 22:41:35 +0000

    * bt-load.c, cfgloop.c, convert.c, dominance.c, global.c, 	loop-invariant.c, stmt.c, tree-ssa-forwprop.c, 	tree-ssa-live.c, tree-ssanames.c, tree-vn.c, 	config/host-linux.c, config/arm/fpa.md, config/avr/avr.h: 	Update copyright.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95202 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/host-linux.c
+++ gcc/config/host-linux.c
@@ -1,5 +1,5 @@
 /* Linux host-specific hook definitions.
-   Copyright (C) 2004 Free Software Foundation, Inc.
+   Copyright (C) 2004, 2005 Free Software Foundation, Inc.
 
    This file is part of GCC.
 
--- gcc/convert.c
+++ gcc/convert.c
@@ -1,6 +1,6 @@
 /* Utility routines for data type conversion for GCC.
    Copyright (C) 1987, 1988, 1991, 1992, 1993, 1994, 1995, 1997, 1998,
-   2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
+   2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
