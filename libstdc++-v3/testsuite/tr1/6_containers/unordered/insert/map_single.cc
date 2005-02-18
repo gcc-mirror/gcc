180ecd6aa2fde01f16168bfa6ad17452bbf2a306
Matt Austern <austern@apple.com>
2005-02-18 07:50:08 +0000

    functional (hash): New function object.
            * include/tr1/functional (hash): New function object.
            * include/tr1/hashtable: New file.
            * include/tr1/unordered_set: New file.
            * include/tr1/unordered_map: New file.
            * include/Makefile.am: Add three new TR1 headers.
            * include/Makefile.in: Likewise.
            * testsuite/tr1/6_containers/unordered/insert/array_syntax.cc: New test.
            * testsuite/tr1/6_containers/unordered/insert/map_single.cc: New test.
            * testsuite/tr1/6_containers/unordered/insert/multimap_single.cc: New test.
            * testsuite/tr1/6_containers/unordered/insert/multiset_single.cc: New test.
            * testsuite/tr1/6_containers/unordered/insert/set_single.cc: New test.
            * testsuite/tr1/6_containers/unordered/instantiate/hash.cc: New test.
            * testsuite/tr1/6_containers/unordered/instantiate/map.cc: New test.
            * testsuite/tr1/6_containers/unordered/instantiate/multimap.cc: New test.
            * testsuite/tr1/6_containers/unordered/instantiate/multiset.cc: New test.
            * testsuite/tr1/6_containers/unordered/instantiate/set.cc: New test.
    
    From-SVN: r95219

---
+++ libstdc++-v3/testsuite/tr1/6_containers/unordered/insert/map_single.cc
@@ -0,0 +1,74 @@
+// { dg-do run }
+
+// 2005-2-17  Matt Austern  <austern@apple.com>
+//
+// Copyright (C) 2005 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 6.3.4.4 unordered_map
+// Single-element insert
+
+#include <string>
+#include <iterator>
+#include <tr1/unordered_map>
+#include "testsuite_hooks.h"
+
+bool test __attribute__((unused)) = true;
+
+void test01()
+{
+  typedef std::tr1::unordered_map<std::string, int> Map;
+  typedef std::pair<const std::string, int> Pair;
+
+  Map m;
+  VERIFY(m.empty());
+
+  std::pair<Map::iterator, bool> p = m.insert(Pair("abcde", 3));
+  VERIFY(p.second);
+  VERIFY(m.size() == 1);
+  VERIFY(std::distance(m.begin(), m.end()) == 1);
+  VERIFY(p.first == m.begin());
+  VERIFY(p.first->first == "abcde");
+  VERIFY(p.first->second == 3);
+}
+
+void test02()
+{
+  typedef std::tr1::unordered_map<std::string, int> Map;
+  typedef std::pair<const std::string, int> Pair;
+
+  Map m;
+  VERIFY(m.empty());
+
+  std::pair<Map::iterator, bool> p1 = m.insert(Pair("abcde", 3));
+  std::pair<Map::iterator, bool> p2 = m.insert(Pair("abcde", 7));
+
+  VERIFY(p1.second);
+  VERIFY(!p2.second);
+  VERIFY(m.size() == 1);
+  VERIFY(p1.first == p2.first);
+  VERIFY(p1.first->first == "abcde");
+  VERIFY(p2.first->second == 3);
+}
+
+int main()
+{
+  test01();
+  test02();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/tr1/6_containers/unordered/insert/multimap_single.cc
@@ -0,0 +1,78 @@
+// { dg-do run }
+
+// 2005-2-17  Matt Austern  <austern@apple.com>
+//
+// Copyright (C) 2005 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 6.3.4.6 unordered_multimap
+// Single-element insert
+
+#include <string>
+#include <iterator>
+#include <tr1/unordered_map>
+#include "testsuite_hooks.h"
+
+bool test __attribute__((unused)) = true;
+
+void test01()
+{
+  typedef std::tr1::unordered_multimap<std::string, int> Map;
+  typedef std::pair<const std::string, int> Pair;
+
+  Map m;
+  VERIFY(m.empty());
+
+  Map::iterator i = m.insert(Pair("abcde", 3));
+  VERIFY(m.size() == 1);
+  VERIFY(std::distance(m.begin(), m.end()) == 1);
+  VERIFY(i == m.begin());
+  VERIFY(i->first == "abcde");
+  VERIFY(i->second == 3);
+}
+
+void test02()
+{
+  typedef std::tr1::unordered_multimap<std::string, int> Map;
+  typedef std::pair<const std::string, int> Pair;
+
+  Map m;
+  VERIFY(m.empty());
+
+  m.insert(Pair("abcde", 3));
+  m.insert(Pair("abcde", 7));
+
+  VERIFY(m.size() == 2);
+  VERIFY(std::distance(m.begin(), m.end()) == 2);
+
+  Map::iterator i1 = m.begin();
+  Map::iterator i2 = i1;
+  ++i2;
+
+  VERIFY(i1->first == "abcde");
+  VERIFY(i2->first == "abcde");
+  VERIFY((i1->second == 3 && i2->second == 7) ||
+	 (i1->second == 7 && i2->second == 3));
+}
+
+int main()
+{
+  test01();
+  test02();
+  return 0;
+}
