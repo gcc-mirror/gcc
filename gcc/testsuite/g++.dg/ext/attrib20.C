e4ac42e243f6cb0cb92f4bf19560eddfda62bba0
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-02-18 19:11:58 +0000

    2005-01-20  Giovanni Bajo  <giovannibajo@gcc.gnu.org>
            PR c++/19508
            * g++.dg/ext/attrib20.C: New test.
    
    2005-01-20  Giovanni Bajo  <giovannibajo@gcc.gnu.org>
    
            PR c++/19508
            * decl2.c (grokfield): Do not apply attributes to template parameters
            as they are ignored by tsubst anyway.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95230 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/ext/attrib20.C
@@ -0,0 +1,23 @@
+// { dg-do compile }
+// { dg-options "-g" }
+// Origin: <jan at etpmod dot phys dot tue dot nl>
+// PR c++/19508: avoid attributes for template parameters
+
+template <typename T>
+struct BVector
+{
+  typedef T T2;
+  typedef T value_type __attribute__ ((aligned(8)));    // { dg-bogus "attribute" "attribute" { xfail *-*-* } }
+  typedef T2 value_type2 __attribute__ ((aligned(8)));  // { dg-bogus "attribute" "attribute" { xfail *-*-* } }
+  value_type v;
+};
+BVector<int> m;
+
+template <template <class> class T>
+struct BV2
+{
+  typedef T<float> value_type __attribute__((aligned(8))); // { dg-bogus "attribute" "attribute" { xfail *-*-* } }
+  value_type v;
+};
+BV2<BVector> m2;
+
