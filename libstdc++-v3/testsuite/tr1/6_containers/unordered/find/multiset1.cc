7551ecd5ab66c585de668cf9f27a7aa1ebd22af3
austern <austern@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-02-19 23:57:23 +0000

    * include/tr1/functional (tr1_hashtable_define_trivial_hash): Make hash<T>::operator() a const member function for T a fundamental type * include/tr1/hashtable (extract1st::operator()): Declare const. (hash_code_base): Declare all member functions const (hashtable::find): fix call to this->bucket_count() (hashtable::count): Likewise. (hashtable::equal_range): m_incr_bucket applies to iterator, not node. * testsuite/tr1/6_containers/unordered/find/set1.cc: New test. * testsuite/tr1/6_containers/unordered/find/map1.cc: New test. * testsuite/tr1/6_containers/unordered/find/multimap1.cc: New test. * testsuite/tr1/6_containers/unordered/find/multiset1.cc: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95293 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/tr1/6_containers/unordered/find/multiset1.cc
@@ -0,0 +1,67 @@
+// { dg-do run }
+
+// 2005-2-18  Matt Austern  <austern@apple.com>
+//
+// Copyright (C) 2005 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 6.3.4.5 unordered_set
+// find, equal_range, count
+
+#include <string>
+#include <iterator>
+#include <algorithm>
+#include <tr1/unordered_set>
+#include "testsuite_hooks.h"
+
+bool test __attribute__((unused)) = true;
+
+void test01()
+{
+  typedef std::tr1::unordered_multiset<std::string> Set;
+  Set s;
+  VERIFY(s.empty());
+
+  s.insert("grape");
+  s.insert("banana");
+  s.insert("grape");
+
+  Set::iterator i2 = s.find("banana");
+  VERIFY(i2 != s.end());
+  VERIFY(*i2 == "banana");
+  
+  std::pair<Set::iterator, Set::iterator> p = s.equal_range("grape");
+  VERIFY(std::distance(p.first, p.second) == 2);
+  Set::iterator i3 = p.first;
+  ++i3;
+  VERIFY(*p.first == "grape");
+  VERIFY(*i3 == "grape");
+
+  Set::iterator i4 = s.find("lime");
+  VERIFY(i4 == s.end());  
+
+  VERIFY(s.count("grape") == 2);
+  VERIFY(s.count("banana") == 1);
+  VERIFY(s.count("lime") == 0);
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
