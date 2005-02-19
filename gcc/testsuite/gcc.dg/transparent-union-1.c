58cb41e617c5af9a01dff6d1e6cb80907a48f49d
Jakub Jelinek <jakub@redhat.com>
2005-02-19 01:26:38 +0100

    re PR c/20043 (transparent_union doesn't allow restrict qualifier removal)
    	PR c/20043
    	* c-typeck.c (composite_type): Handle quals in transparent unions.
    	(type_lists_compatible_p): Likewise.
    
    	* gcc.dg/transparent-union-1.c: New test.
    	* gcc.dg/transparent-union-2.c: New test.
    
    From-SVN: r95255

---
+++ gcc/testsuite/gcc.dg/transparent-union-1.c
@@ -0,0 +1,83 @@
+/* PR c/20043 */
+/* { dg-compile } */
+/* { dg-options "-std=gnu99" } */
+
+extern void f0 (int *);
+extern void f0 (int *__restrict);
+
+extern void f1 (int *__restrict);
+extern void f1 (int *);
+
+typedef union { int *i; long *l; } U2
+  __attribute__((transparent_union));
+extern void f2 (U2);
+extern void f2 (int *);
+
+typedef union { int *__restrict i; long *__restrict l; } U3
+  __attribute__((transparent_union));
+extern void f3 (U3);
+extern void f3 (int *__restrict);
+
+extern void f4 (U3);
+extern void f4 (int *);
+
+extern void f5 (U2);
+extern void f5 (int *__restrict);
+
+typedef union { long *l; int *i; } U6
+  __attribute__((transparent_union));
+extern void f6 (U6);
+extern void f6 (int *);
+
+typedef union { long *__restrict l; int *__restrict i; } U7
+  __attribute__((transparent_union));
+extern void f7 (U7);
+extern void f7 (int *__restrict);
+
+extern void f8 (U7);
+extern void f8 (int *);
+
+extern void f9 (U6);
+extern void f9 (int *__restrict);
+
+extern void f10 (U2);
+extern void f11 (U3);
+extern void f12 (U6);
+extern void f13 (U7);
+
+int i;
+long l;
+
+int
+main (void)
+{
+  f0 (&i);
+  f0 (&l);	/* { dg-warning "warning: passing argument 1 of 'f0' from incompatible pointer type" } */
+  f1 (&i);
+  f1 (&l);	/* { dg-warning "warning: passing argument 1 of 'f1' from incompatible pointer type" } */
+  f2 (&i);
+  f2 (&l);	/* { dg-warning "warning: passing argument 1 of 'f2' from incompatible pointer type" } */
+  f3 (&i);
+  f3 (&l);	/* { dg-warning "warning: passing argument 1 of 'f3' from incompatible pointer type" } */
+  f4 (&i);
+  f4 (&l);	/* { dg-warning "warning: passing argument 1 of 'f4' from incompatible pointer type" } */
+  f5 (&i);
+  f5 (&l);	/* { dg-warning "warning: passing argument 1 of 'f5' from incompatible pointer type" } */
+  f6 (&i);
+  f6 (&l);	/* { dg-warning "warning: passing argument 1 of 'f6' from incompatible pointer type" } */
+  f7 (&i);
+  f7 (&l);	/* { dg-warning "warning: passing argument 1 of 'f7' from incompatible pointer type" } */
+  f8 (&i);
+  f8 (&l);	/* { dg-warning "warning: passing argument 1 of 'f8' from incompatible pointer type" } */
+  f9 (&i);
+  f9 (&l);	/* { dg-warning "warning: passing argument 1 of 'f9' from incompatible pointer type" } */
+  f10 (&i);
+  f10 (&l);
+  f11 (&i);
+  f11 (&l);
+  f12 (&i);
+  f12 (&l);
+  f13 (&i);
+  f13 (&l);
+  return 0;
+}
---
+++ gcc/testsuite/gcc.dg/transparent-union-2.c
@@ -0,0 +1,18 @@
+/* PR c/20043 */
+/* { dg-compile } */
+/* { dg-options "-std=gnu99" } */
+
+typedef union { int *i; long *l; } U
+  __attribute__((transparent_union));
+
+extern void f0 (U);		/* { dg-error "previous declaration" } */
+extern void f0 (void *);	/* { dg-error "conflicting types" } */
+
+extern void f1 (U);		/* { dg-error "previous declaration" } */
+extern void f1 (unsigned long);	/* { dg-error "conflicting types" } */
+
+extern void f2 (void *);	/* { dg-error "previous declaration" } */
+extern void f2 (U);		/* { dg-error "conflicting types" } */
+
+extern void f3 (unsigned long);	/* { dg-error "previous declaration" } */
+extern void f3 (U);		/* { dg-error "conflicting types" } */
