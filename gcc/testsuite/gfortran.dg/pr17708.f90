91e42dbbe4c292b2e26bbbcc1659933a3c27eb9c
tobi <tobi@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-02-21 13:03:06 +0000

    * gfortran.dg/do_iterator: Split loop, markup all lines for which an error locus is printed. * gfortran.dg/pr17708.f90: Markup all lines for which an error locus is printed. * gfortran.dg/select_4.f90: Likewise. * lib/gfortran-dg.exp (gfortran-dg-test): Handle gfortran error locuses in the various permutations.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95329 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/pr17708.f90
+++ gcc/testsuite/gfortran.dg/pr17708.f90
@@ -2,8 +2,8 @@
       program test
         j = 0
         do 10 i=1,3
-           if(i == 2) goto 10 ! { dg-warning "" "" }
+           if(i == 2) goto 10 ! { dg-warning "jumps to END" }
            j = j+1
-10      enddo
+10      enddo                 ! { dg-warning "jumps to END" }
         if (j/=2) call abort
       end
