cb9d8cea5f96209228f3c67fbff95356e5fb253d
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-02-22 06:57:03 +0000

    * gcc.dg/charset/builtin1.c: Use dg-require-iconv "IBM1047". 	* gcc.dg/charset/asm1.c: Check for IBM1047 code set, not IBM-1047. 	* gcc.dg/charset/asm2.c: Likewise. 	* gcc.dg/charset/asm3.c: Likewise. 	* gcc.dg/charset/asm4.c: Likewise. 	* gcc.dg/charset/asm5.c: Likewise. 	* gcc.dg/charset/asm6.c: Likewise. 	* gcc.dg/charset/attribute1.c: Likewise. 	* gcc.dg/charset/attribute2.c: Likewise. 	* gcc.dg/charset/builtin1.c: Likewise. 	* gcc.dg/charset/extern.c: Likewise. 	* gcc.dg/charset/function.c: Likewise. 	* gcc.dg/charset/string.c: Likewise. 	* gcc.dg/charset/charset.exp:Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95385 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/charset/string.c
+++ gcc/testsuite/gcc.dg/charset/string.c
@@ -1,5 +1,5 @@
 /* Simple character translation test.  */
 /* { dg-do compile }
-   { dg-require-iconv "IBM-1047" }
+   { dg-require-iconv "IBM1047" }
    { dg-final { scan-assembler-not "string foobar" } } */
 char *foo = "string foobar";
