2dae816dabcbc1c1bb8b1ba7bc58215e42d4e640
bdavis <bdavis@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-02-23 10:45:07 +0000

    2005-02-23  Bud Davis  <bdavis@gfortran.org>
            * gfortran.dg/list_read_4.f90: Change to f90 comments.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95449 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/list_read_4.f90
+++ gcc/testsuite/gfortran.dg/list_read_4.f90
@@ -1,25 +1,25 @@
-C { dg-do run }
-C Test of gfortran list directed read> check delimiters are correctly
-C treated.  Written in f77 so that g77 will run for comparison.
-C
-C f , e and i edit reads are terminated separately by read_real.c
-C
-C PThomas                                                   Jan 2005
-C BDavis        
+! { dg-do run }
+! Test of gfortran list directed read> check delimiters are correctly
+! treated.  Written in f77 so that g77 will run for comparison.
+!
+! f , e and i edit reads are terminated separately by read_real.c
+!
+! PThomas                                                   Jan 2005
+! BDavis        
       program list_read_4
       integer i(10),l(10),k,j 
       real x(10),y(10)
-C expected results
+! expected results
       data y / 1.0,2.0,3.0,-1.0,-1.0,-1.0,4.0,4.0,99.0,99.0 /
       data l /1,2,3,-1,-1,-1,4,4,99,99/
-C put them in a file
+! put them in a file
       open (10,status="scratch")
       write (10,*) " 1.0, 2.0 , 3.0,, 2* , 2*4.0 , 5*99.0"
       write (10,*) " 1.0e0, 2.0e0 , 3.0e0,, 2* , 2*4.0e0 , 5*99.0e0"
       write (10,*) " 1,   2 ,   3,,   2* , 2*4 ,   5*99"
       write (10,*) " 1,   2 ,   3,,   2* , 2*4 ,   5*99"
       rewind (10)
-C
+!
       do k = 1,10
         x(k) = -1.0
       enddo
@@ -41,7 +41,7 @@ C
          if (x(k).ne.y(k)) call abort
          x(k) = -1
       end do
-C integer
+! integer
       do k = 1,10
         i(k) = -1
       end do 
