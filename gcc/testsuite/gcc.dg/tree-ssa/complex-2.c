189b339816238133988b6e976a05256f9d2d3426
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-02-24 20:00:09 +0000

    PR middle-end/19953         * builtins.c (fold_builtin_complex_mul, fold_builtin_complex_div): New.         (fold_builtin_1): Call them.         * fold-const.c (fold_complex_mult_parts): Split out from ...         (fold_complex_mult): ... here.  Fix typo in both imaginary case.         (fold_complex_div_parts, fold_complex_div): New.         (fold): Use them.         * tree.h (fold_complex_mult_parts, fold_complex_div_parts): Declare.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95511 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/complex-2.c
@@ -0,0 +1,12 @@
+/* Verify that we don't libcall for complex / real.  */
+/* { dg-do compile } */
+/* { dg-options "-std=c99 -O -fdump-tree-optimized" } */
+
+typedef _Complex float C;
+
+C foo(C x, float y)
+{
+  return x / y;
+}
+
+/* { dg-final { scan-tree-dump-times "__div" 0 "optimized" } } */
