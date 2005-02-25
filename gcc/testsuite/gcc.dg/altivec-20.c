78796ad54b18ad6aa7433cb0a45f6665f85f9743
Fariborz Jahanian <fjahanian@apple.com>
2005-02-25 01:16:17 +0000

    config/rs6000/rs6000.c (rs6000_legitimize_reload_address): Check for vector...
    config/rs6000/rs6000.c (rs6000_legitimize_reload_address): Check for
    vector types if to use base reg for misaligned non-word ld/std.
    OKed by David Edelsohn.
    
    From-SVN: r95529

---
+++ gcc/testsuite/gcc.dg/altivec-20.c
@@ -0,0 +1,23 @@
+/* { dg-do compile { target powerpc*-*-* } } */
+/* { dg-options "-maltivec -mcpu=G5 -O2" } */
+
+#include <altivec.h>
+
+void foo( float scalar)
+{
+    unsigned long width;
+    unsigned long x;
+    vector float vColor;
+    vector unsigned int selectMask;
+    vColor = vec_perm( vec_ld( 0, &scalar), vec_ld( 3, &scalar), vec_lvsl( 0, &scalar) );
+
+    float *destRow;
+    vector float store, load0;
+
+    for( ; x < width; x++)
+    {
+            load0 = vec_sel( vColor, load0, selectMask );
+            vec_st( store, 0, destRow );
+            store = load0;
+    }
+}
