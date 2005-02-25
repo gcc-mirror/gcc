c9a1f6909e5065535300ac09b184127959629215
Diego Novillo <dnovillo@redhat.com>
2005-02-25 21:10:30 +0000

    re PR tree-optimization/20204 (miscompilation of asm-declared registers)
    	PR tree-optimization/20204
    	* tree-into-ssa.c (insert_phi_nodes_for): Do not use
    	REWRITE_THIS_STMT markers on PHI nodes.
    	(rewrite_initialize_block): Likewise.
    
    testsuite/ChangeLog
    
    	PR tree-optimization/20204
    	* testsuite/gcc.dg/pr20204.c: New test.
    
    From-SVN: r95546

---
+++ gcc/testsuite/gcc.dg/pr20204.c
@@ -0,0 +1,34 @@
+/* { dg-do run { target i?86-*-* } } */
+/* { dg-options "-O2" } */
+
+void *x (void *pdst, const void *psrc, unsigned int pn)
+{
+  register void *return_dst = pdst;
+  register unsigned char *dst = pdst;
+  register unsigned const char *src = psrc;
+  register int n __asm__ ("ebx") = pn;
+
+  if (src < dst && dst < src + n)
+    {
+      src += n;
+      dst += n;
+      while (n--)
+        *--dst = *--src;
+      return return_dst;
+    }
+
+  while (n >= 16) n--;
+
+  return return_dst;
+}
+extern void abort ();
+extern void exit (int);
+char xx[30] = "abc";
+int main (void)
+{
+  char yy[30] = "aab";
+
+  if (x (xx + 1, xx, 2) != xx + 1 || memcmp (xx, yy, sizeof (yy)) != 0)
+    abort ();
+  exit (0);
+}
