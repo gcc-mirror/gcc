c529e0fe2e37607ee722881c877570fbc97143bd
Andrew Pinski <pinskia@physics.uc.edu>
2005-02-26 16:15:25 +0000

    re PR tree-optimization/20188 (asm and memory operands does not add a V_MAY_DEF)
    2005-02-26  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR tree-opt/20188
            * gcc.dg/tree-ssa/inline_asm-1.c: New test.
            * gcc.dg/tree-ssa/inline_asm-2.c: New test.
            * gcc.dg/asm-b.c: New test.
    
    2005-02-26  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR tree-opt/20188
            * tree-ssa-alias.c (count_uses_and_derefs): If we have TREE_LIST
            for the lhs, also walk over the tree.  Likewise for rhs.
    
    From-SVN: r95586

---
+++ gcc/testsuite/gcc.dg/asm-b.c
@@ -0,0 +1,39 @@
+/* { dg-do run  { target powerpc-*-* i?386-*-* x86_64-*-* } } */
+/* { dg-options "-O1" } */
+/* Test to make sure that inline-asm causes the tree optimizators get the
+   V_MAY_DEFs and clober memory.  */
+/* Test from Jakub Jelinek, modified by Andrew Pinski to work on all powerpc targets. */
+extern void abort (void);
+
+unsigned short v = 0x0300;
+
+void
+foo (unsigned short *p)
+{
+  *p = v;
+}
+
+int
+bar (void)
+{
+  unsigned short x;
+  volatile unsigned short *z;
+  foo (&x);
+  const unsigned int y = x;
+  z = &x;
+#if defined (__powerpc__) || defined (__PPC__) || defined (__ppc__) || defined (_POWER)
+  __asm __volatile ("sthbrx %1,0,%2" : "=m" (*z) : "r" (y), "r" (z));
+#elif defined __i386__ || defined __x86_64__
+  __asm __volatile ("movb %b1,1(%2); movb %h1,(%2)" : "=m" (*z) : "r" (y), "r"
+(z));
+#endif
+  return (x & 1) == 0;
+}
+
+int
+main (void)
+{
+  if (bar ())
+    abort ();
+  return 0;
+}
