5591e5f9af6f09455a1f11d8e822785e34647186
Jakub Jelinek <jakub@redhat.com>
2005-02-27 18:13:28 +0100

    re PR c++/20206 (COMDAT broken for C++ thunks)
    	PR c++/20206
    	* decl.c (cxx_comdat_group): Put thunks for
    	TARGET_USE_LOCAL_THUNK_ALIAS_P (function) functions into the same
    	comdat group as the thunk target.
    
    	* g++.dg/opt/thunk2.C: New test.
    	* g++.dg/opt/covariant1.C: New test.
    
    From-SVN: r95619

---
+++ gcc/testsuite/g++.dg/opt/covariant1.C
@@ -0,0 +1,47 @@
+// PR c++/20206
+// { dg-do run }
+// { dg-options "-O0" }
+
+void
+bar (int x)
+{
+  asm ("" : : "g" (x));
+}
+
+struct S { S () {}; virtual ~S () {}; };
+struct T { virtual T *foo (int) {}; };
+struct V : virtual S, virtual T {};
+struct V v;
+struct U : public S, public T
+{
+  bool a;
+  U () {}
+  virtual ~U () {}
+  virtual V *foo (int x)
+  {
+    switch (x)
+      {
+      case 12:
+	break;
+      case 9:
+	bar (7);
+	break;
+      case 10:
+	bar (12);
+	break;
+      case 4:
+	bar (18);
+	break;
+      case 2:
+	bar (26);
+	break;
+      }
+    return &v;
+  }
+};
+U u;
+
+int
+main ()
+{
+}
