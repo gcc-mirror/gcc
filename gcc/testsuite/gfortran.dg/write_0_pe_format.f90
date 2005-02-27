1a579c52699a788ee3f118530f9721161ef33e35
tobi <tobi@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-02-27 19:47:12 +0000

    libgfortran/ 2005-02-27  Fran��ois-Xavier Coudert  <coudert@clipper.ens.fr>
     PR libfortran/20101
    * write.c (output_float): Adding special check for value 0.0 in
    PE format.
    
    gcc/testsuite/
    2005-02-27  Fran��ois-Xavier Coudert  <coudert@clipper.ens.fr>
    
    PR libfortran/20101
    * gfortran.dg/write_0_pe_format.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95628 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/write_0_pe_format.f90
@@ -0,0 +1,9 @@
+! { dg-do run }
+! PR libfortran/20101
+! With format "PE", 0.0 must still have "+00" as exponent
+character(len=10) :: c1, c2
+write(c1,"(1pe9.2)") 0.0
+write(c2,"(1pe9.2)") 1.0
+if (trim(adjustl(c1)) .ne. "0.00E+00") call abort()
+if (trim(adjustl(c2)) .ne. "1.00E+00") call abort()
+end
