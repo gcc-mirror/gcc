44535114620c0f959f1b47db7c783b03e22bce62
kargl <kargl@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-02-28 00:40:03 +0000

    * gfortran.dg/ishft.f90:  Remove kind suffix from BOZ constant * gfortran.fortran-torture/execute/intrinsic_mvbits.f90: ditto
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95644 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_mvbits.f90
+++ gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_mvbits.f90
@@ -1,6 +1,6 @@
 ! Test the MVBITS intrinsic subroutine
 INTEGER*4 :: from, to, result
-integer*8 :: to8
+integer*8 :: from8, to8
 
 DATA from / z'0003FFFC' /
 DATA to / z'77760000' /
@@ -9,7 +9,8 @@ DATA result / z'7777FFFE' /
 CALL mvbits(from, 2, 16, to, 1)
 if (to /= result) CALL abort()
 
-to8 = 0
-call mvbits (b'1011'_8*2_8**32, 33, 3, to8, 2) ! { dg-warning "" "" }
-if (to8 /= b'10100'_8) call abort ! { dg-warning "" "" }
+to8 = 0_8
+from8 = b'1011'*2_8**32
+call mvbits (from8, 33, 3, to8, 2)
+if (to8 /= b'10100') call abort
 end
