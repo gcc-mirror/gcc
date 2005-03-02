bb4c7a44377ffbc0967d80916cae0cd8dc3b2f85
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-02 11:09:48 +0000

    2005-03-02  Richard Guenther  <rguenth@gcc.gnu.org>
    	* cgraph.h (struct cgraph_edge): Add prev_caller and
    	prev_callee fields.
    	(cgraph_node_remove_callees): Export.
    	* cgraph.c (cgraph_create_edge): Initialize prev_caller
    	and prev_callee.
    	(cgraph_edge_remove_callee): New function.
    	(cgraph_edge_remove_caller): Likewise.
    	(cgraph_remove_edge): Use.
    	(cgraph_redirect_edge_callee): Likewise.
    	(cgraph_node_remove_callees): New function.
    	(cgraph_node_remove_callers): Likewise.
    	(cgraph_remove_node): Use.
    	* tree-optimize.c (tree_rest_of_compilation): Use
    	cgraph_node_remove_callees instead of manual loop.
    	* cgraphunit.c (cgraph_finalize_function): Likewise.
    	(cgraph_expand_function): Likewise.
    	(cgraph_remove_unreachable_nodes): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95777 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cgraphunit.c
+++ gcc/cgraphunit.c
@@ -356,8 +356,7 @@ cgraph_finalize_function (tree decl, bool nested)
 	      cgraph_remove_node (n);
 	}
 
-      while (node->callees)
-	cgraph_remove_edge (node->callees);
+      cgraph_node_remove_callees (node);
 
       /* We may need to re-queue the node for assembling in case
          we already proceeded it and ignored as not needed.  */
@@ -843,8 +842,7 @@ cgraph_expand_function (struct cgraph_node *node)
       DECL_INITIAL (node->decl) = error_mark_node;
       /* Eliminate all call edges.  This is important so the call_expr no longer
 	 points to the dead function body.  */
-      while (node->callees)
-	cgraph_remove_edge (node->callees);
+      cgraph_node_remove_callees (node);
     }
 }
 
@@ -1006,8 +1004,7 @@ cgraph_remove_unreachable_nodes (void)
 		      DECL_STRUCT_FUNCTION (node->decl) = NULL;
 		      DECL_INITIAL (node->decl) = error_mark_node;
 		    }
-		  while (node->callees)
-		    cgraph_remove_edge (node->callees);
+		  cgraph_node_remove_callees (node);
 		  node->analyzed = false;
 		}
 	      else
