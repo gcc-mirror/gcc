1cfaed023ea4e439a198ac90e08c379969102b56
No Author <no-author@gcc.gnu.org>
2005-03-02 13:19:48 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r95779

---
+++ libstdc++-v3/testsuite/tr1/3_function_objects/reference_wrapper/invoke.cc
@@ -0,0 +1,103 @@
+// 2005-02-27 Douglas Gregor <doug.gregor -at- gmail.com>
+//
+// Copyright (C) 2005 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 2.1 reference wrappers
+#include <tr1/functional>
+#include <tr1/type_traits>
+#include <testsuite_hooks.h>
+#include <testsuite_tr1.h>
+
+using namespace __gnu_test;
+
+bool test __attribute__((unused)) = true;
+
+struct X
+{
+  typedef int result_type;
+
+  X() : bar(17) {}
+
+  int foo(float x)                   { return truncate_float(x); }
+  int foo_c(float x)  const          { return truncate_float(x); }
+  int foo_v(float x)  volatile       { return truncate_float(x); }
+  int foo_cv(float x) const volatile { return truncate_float(x); }
+
+  int operator()(float x)
+  {
+    return foo(x) + 1;
+  }
+
+  int operator()(float x) const
+  {
+    return foo_c(x) + 2;
+  }
+
+  int bar;
+
+ private:
+  X(const X&);
+  X& operator=(const X&);
+};
+
+void test01()
+{
+  using std::tr1::ref;
+  using std::tr1::cref;
+
+  ::X x;
+  ::X* xp = &x;
+  int (::X::* p_foo)(float) = &::X::foo;
+  int (::X::* p_foo_c)(float) const = &::X::foo_c;
+  int (::X::* p_foo_v)(float) volatile = &::X::foo_v;
+  int (::X::* p_foo_cv)(float) const volatile = &::X::foo_cv;
+  int ::X::* p_bar = &::X::bar;
+
+  const float pi = 3.14;
+
+  // Functions
+  VERIFY(ref(truncate_float)(pi) == 3);
+
+  // Function pointers
+  VERIFY(cref(&truncate_float)(pi) == 3);
+
+  // Member function pointers
+  VERIFY(ref(p_foo)(x, pi) == 3);
+  VERIFY(ref(p_foo)(xp, pi) == 3);
+  VERIFY(ref(p_foo_c)(x, pi) == 3);
+  VERIFY(ref(p_foo_c)(xp, pi) == 3);
+  VERIFY(ref(p_foo_v)(x, pi) == 3);
+  VERIFY(ref(p_foo_v)(xp, pi) == 3);
+  VERIFY(ref(p_foo_cv)(x, pi) == 3);
+  VERIFY(ref(p_foo_cv)(xp, pi) == 3);
+
+  // Member data pointers
+  VERIFY(ref(p_bar)(x) == 17);
+  VERIFY(ref(p_bar)(xp) == 17);
+
+  // Function objects
+  VERIFY(ref(x)(pi) == 4);
+  VERIFY(cref(x)(pi) == 5);
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
