0875e2d5acec793f2f9374650c497c58497e0230
dgregor <dgregor@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-02 12:56:28 +0000

    2005-03-02  Douglas Gregor  <doug.gregor@gmail.com>
            * include/tr1/functional (_Has_result_type): Cleanup.
            (_Result_of_impl): Handle member data pointers correctly.
            (reference_wrapper): Support invocation.
            Move repetition code into new file include/tr1/repeat.h.
            * include/tr1/functional_iterate.h (reference_wrapper): Support
            invocation.  Cleanup long lines.
            * include/tr1/ref_wrap_iterate.h (reference_wrapper): Declare
            invocation operators.
            * include/tr1/repeat.h: Code repetition header.
            * include/Makefile.am: Add ref_wrap_iterate.h, repeat.h
            * include/Makefile.in: Add ref_wrap_iterate.h, repeat.h
            * testsuite/tr1/3_function_objects/reference_wrapper/invoke.cc:
            New test of reference_wrapper invocation.
            * testsuite/tr1/3_function_objects/reference_wrapper/typedefs.cc:
            New test of reference_wrapper typedefs and base classes.
            * testsuite/tr1/3_function_objects/result_of.cc: Trivial cleanup
            (e-mail address).
    
    2005-03-02  Douglas Gregor  <doug.gregor@gmail.com>
    
            * include/tr1/function (result_of): New class template.
            * include/tr1/functional/iterator.h: Implementation of TR1
            result_of.
            * testsuite/tr1/3_function_objects/result_of.cc: New test
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95778 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/tr1/3_function_objects/result_of.cc
@@ -0,0 +1,75 @@
+// 2005-01-26 Douglas Gregor <doug.gregor -at- gmail.com>
+//
+// Copyright (C) 2005 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// 3.4 function return types
+#include <tr1/functional>
+#include <tr1/type_traits>
+#include <testsuite_hooks.h>
+#include <testsuite_tr1.h>
+
+class X {};
+
+struct int_result_type { typedef int result_type; };
+
+struct int_result_of
+{
+  template<typename F> struct result { typedef int type; };
+};
+
+struct int_result_type_and_float_result_of
+{
+  typedef int result_type;
+  template<typename F> struct result { typedef float type; };
+};
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  using std::tr1::result_of;
+  using std::tr1::is_same;
+  using namespace __gnu_test;
+
+  typedef int (*func_ptr)(float, double);
+  typedef int (&func_ref)(float, double);
+  typedef int (::X::*mem_func_ptr)(float);
+  typedef int (::X::*mem_func_ptr_c)(float) const;
+  typedef int (::X::*mem_func_ptr_v)(float) volatile;
+  typedef int (::X::*mem_func_ptr_cv)(float) const volatile;
+
+  VERIFY((is_same<result_of<int_result_type(float)>::type, int>::value));
+  VERIFY((is_same<result_of<int_result_of(double)>::type, int>::value));
+  VERIFY((is_same<result_of<int_result_of(void)>::type, void>::value));
+  VERIFY((is_same<result_of<const int_result_of(double)>::type, int>::value));
+  VERIFY((is_same<result_of<volatile int_result_of(void)>::type, void>::value));
+  VERIFY((is_same<result_of<int_result_type_and_float_result_of(char)>::type, int>::value));
+  VERIFY((is_same<result_of<func_ptr(char, float)>::type, int>::value));
+  VERIFY((is_same<result_of<func_ref(char, float)>::type, int>::value));
+  VERIFY((is_same<result_of<mem_func_ptr(::X,char)>::type, int>::value));
+  VERIFY((is_same<result_of<mem_func_ptr_c(::X,char)>::type, int>::value));
+  VERIFY((is_same<result_of<mem_func_ptr_v(::X,char)>::type, int>::value));
+  VERIFY((is_same<result_of<mem_func_ptr_cv(::X,char)>::type, int>::value));
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
