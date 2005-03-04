35db3ec168fe37033e35bb171191d3f707af8c0d
geoffk <geoffk@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-04 01:38:56 +0000

    * gcc.c-torture/execute/pr17133.c: New. 	* gcc.c-torture/compile/20050303-1.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@95865 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/20050303-1.c
@@ -0,0 +1,10 @@
+void crc()
+{
+    int  toread;
+    long long nleft;
+    unsigned char buf[(128 * 1024)];
+
+    nleft = 0;
+    while (toread = (nleft < (2147483647 * 2U + 1U)) ? nleft: (2147483647 * 2U + 1U) )
+	;
+}
