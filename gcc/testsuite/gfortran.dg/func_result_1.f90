496989685fb5b4ae061d4ed3465fcec7823b009c
No Author <no-author@gcc.gnu.org>
2005-03-04 22:21:16 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r95904

---
+++ gcc/testsuite/gfortran.dg/func_result_1.f90
@@ -0,0 +1,19 @@
+! { dg-do run }
+! From PR 19673 : We didn't dereference the the result from POINTER
+! functions with a RESULT clause
+program ret_ptr
+  if (foo(99) /= bar(99)) call abort ()
+contains
+  function foo (arg) result(ptr)
+    integer :: arg
+    integer, pointer :: ptr
+    allocate (ptr)
+    ptr = arg
+  end function foo
+  function bar (arg)
+    integer :: arg
+    integer, pointer :: bar
+    allocate (bar)
+    bar = arg
+  end function bar
+end  program ret_ptr
