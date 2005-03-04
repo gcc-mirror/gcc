8d06c80914d7374d16887b0e817eadb41d898aa8
James A. Morrison <phython@gcc.gnu.org>
2005-03-04 02:48:30 +0000

    re PR tree-optimization/15784 (fold misses binary optimization)
    2005-03-03  James A. Morrison  <phython@gcc.gnu.org>
    
            PR tree-optimization/15784
            * fold-const.c (fold): Fold ~A + 1 to -1.  Fold -A - 1
            and -1 - A to ~A.
    
    From-SVN: r95870

---
+++ gcc/testsuite/gcc.dg/pr15784-4.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+int a (int x) {
+	return ~x + 1; /* -x */
+}
+
+int b (int x) {
+	return -x -1; /* ~x */
+}
+
+/* { dg-final { scan-tree-dump "~x;" "optimized" } } */
+/* { dg-final { scan-tree-dump "-x;" "optimized" } } */
