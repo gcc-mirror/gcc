b50e299bc8c37ac551f6b54bbfb00838fc931237
Joseph Myers <joseph@codesourcery.com>
2005-03-05 01:56:53 +0000

    2.cc, [...]: XFAIL on *-*-hpux11.23.
    	* testsuite/22_locale/collate/compare/wchar_t/2.cc,
    	testsuite/22_locale/collate/compare/wchar_t/wrapped_env.cc,
    	testsuite/22_locale/collate/compare/wchar_t/wrapped_locale.cc,
    	testsuite/22_locale/collate/hash/wchar_t/2.cc,
    	testsuite/22_locale/collate/hash/wchar_t/wrapped_env.cc,
    	testsuite/22_locale/collate/hash/wchar_t/wrapped_locale.cc,
    	testsuite/22_locale/collate/transform/wchar_t/2.cc,
    	testsuite/22_locale/collate/transform/wchar_t/wrapped_env.cc,
    	testsuite/22_locale/collate/transform/wchar_t/wrapped_locale.cc:
    	XFAIL on *-*-hpux11.23.
    
    From-SVN: r95919

--- libstdc++-v3/testsuite/22_locale/collate/transform/wchar_t/wrapped_env.cc
+++ libstdc++-v3/testsuite/22_locale/collate/transform/wchar_t/wrapped_env.cc
@@ -21,6 +21,7 @@
 // 22.2.4.1.1 collate members
 
 // { dg-do run }
+// { dg-xfail-if "" { "*-*-hpux11.23" } { "*" } { "" } } */
 // { dg-options "-finput-charset=ISO8859-1" }
 // { dg-require-iconv "ISO8859-1" }
 
