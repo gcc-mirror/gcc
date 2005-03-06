40c22b7ca54be578cff9e772a90f27becb20a3a9
John David Anglin <dave.anglin@nrc-cnrc.gc.ca>
2005-03-06 18:08:32 +0000

    file-format.exp (gcc_target_object_format): Add check for som format and hardcode object formats on hppa*-*-hpux*.
    	* lib/file-format.exp (gcc_target_object_format): Add check for som
            format and hardcode object formats on hppa*-*-hpux*.
    	* lib/target-supports.exp (check_weak_available): Add check for som
    	object format.  Always return 0 on hppa*-*-hpux10*.
    	* gcc.dg/titype-1.c, gcc.dg/uninit-C.c: Don't use TImode if __hppa__
    	is defined.
    	* objc.dg/stabs-1.m: hppa*64*-*-* doesn't have stabs.
    
    From-SVN: r95981

--- gcc/testsuite/gcc.dg/titype-1.c
+++ gcc/testsuite/gcc.dg/titype-1.c
@@ -1,7 +1,7 @@
 /* { dg-do run } */
 
 /* Not all platforms support TImode integers.  */
-#if defined(__LP64__)
+#if defined(__LP64__) && !defined(__hppa__)
 typedef int TItype __attribute__ ((mode (TI)));
 #else
 typedef long TItype;
