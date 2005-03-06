1ddc032813b17e24ecdbe87b5f31792fd78095aa
Kazu Hirata <kazu@cs.umass.edu>
2005-03-06 12:57:57 +0000

    jcf-dump.c, [...]: Update copyright.
    	* jcf-dump.c, jcf-io.c, jcf-reader.c, lang.c, parse.h,
    	typeck.c: Update copyright.
    
    From-SVN: r95967

--- gcc/java/jcf-dump.c
+++ gcc/java/jcf-dump.c
@@ -1,7 +1,7 @@
 /* Program to dump out a Java(TM) .class file.
    Functionally similar to Sun's javap.
 
-   Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004
+   Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005
    Free Software Foundation, Inc.
 
 This file is part of GCC.
