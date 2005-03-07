941b4534a35bd3edbf078cb38f253b4e79ee00a1
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-07 17:48:46 +0000

    PR rtl-optimization/19683 	* reload1.c (choose_reload_regs): Pass the number of bits, not the 	number of bytes, to smallest_int_for_mode.  Fix arguments to 	REG_CANNOT_CHANGE_MODE_P.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@96033 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/pr19683-1.c
@@ -0,0 +1,42 @@
+/* From PR rtl-optimization/19683.  On little-endian MIPS targets,
+   reload would incorrectly inherit the high part of the multiplication
+   result.  */
+/* { dg-do run { target mips*-*-* } } */
+
+extern void abort (void);
+extern void exit (int);
+
+#define REPEAT10(X, Y)					\
+  X(Y##0); X(Y##1); X(Y##2); X(Y##3); X(Y##4);		\
+  X(Y##5); X(Y##6); X(Y##7); X(Y##8); X(Y##9)
+
+#define REPEAT30(X) REPEAT10 (X, 0); REPEAT10 (X, 1); REPEAT10 (X, 2)
+#define IN(X) unsigned int x##X = ptr[0]
+#define OUT(X) ptr[0] = x##X
+
+union u { unsigned long long ll; unsigned int i[2]; };
+
+unsigned int
+foo (volatile unsigned int *ptr)
+{
+  union u u;
+  int result;
+
+  u.ll = (unsigned long long) ptr[0] * ptr[0];
+  REPEAT30 (IN);
+  REPEAT30 (OUT);
+  asm ("#" : "=l" (result) : "l" (u.i[1]));
+  return result;
+}
+
+int
+main (void)
+{
+  unsigned int array[] = { 1000 * 1000 * 1000 };
+  union u u;
+
+  u.ll = (unsigned long long) array[0] * array[0];
+  if (foo (array) != u.i[1])
+    abort ();
+  exit (0);
+}
