c384605c898eb1534d21789b541a9f091a60070e
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-08 12:49:01 +0000

    * c-semantics.c, cfghooks.c, diagnostic.h, loop-iv.c, 	loop-unswitch.c, modulo-sched.c, stor-layout.c, tree-gimple.h, 	config/i386/cygwin1.c, doc/cfg.texi, doc/objc.texi: Update 	copyright.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@96100 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-gimple.h
+++ gcc/tree-gimple.h
@@ -1,5 +1,5 @@
 /* Functions to analyze and validate GIMPLE trees.
-   Copyright (C) 2002, 2003 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2003, 2005 Free Software Foundation, Inc.
    Contributed by Diego Novillo <dnovillo@redhat.com>
 
 This file is part of GCC.
