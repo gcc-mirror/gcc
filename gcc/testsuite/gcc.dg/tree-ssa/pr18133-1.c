e4bddc02286f7422cc93f9df8b524661c3ed751b
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-08 03:40:58 +0000

    * gcc.dg/tree-ssa/20030821-1.c: Verify that the computed goto         was folded away.         * gcc.dg/tree-ssa/pr18133-1.c: New test.         * gcc.dg/tree-ssa/pr18133-2.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@96085 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr18133-1.c
@@ -0,0 +1,27 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-optimized-blocks" } */
+
+void foo (void)
+{
+void *p;
+p = &&L0;
+goto *p;
+L0:
+return;
+}
+
+/* The goto &L0 should have been optimized away during CFG
+   cleanups.  */
+/* { dg-final { scan-tree-dump-times "goto &L0" 0 "optimized" } } */
+
+/* There should not be any abnormal edges as DOM removed the
+   computed goto.  */
+
+/* { dg-final { scan-tree-dump-times "ab" 0 "optimized" } } */
+
+/* And verify that we have fixed the fallthru flag as well. 
+   After DOM we will have two fallthru edges (e->0, 0->1),
+   but in the dump files we mention the 0->1 two times.  So
+   scan for 3 instances of "fallthru".  */
+
+/* { dg-final { scan-tree-dump-times "fallthru" 3 "optimized" } } */
