6e9a32219ba643ca53c2b68822f0eddbf3280503
Alexey Neyman <alex.neyman@auriga.ru>
2005-03-08 13:19:40 +0000

    re PR c/14411 (Request for setjmp/longjmp attributes)
            PR c/14411
            * calls.c (flags_from_decl_or_type): Handle eturns_twice' attribute.
            * c-common.c (handle_returns_twice): New function.
            (c_common_attribute_table): Declare eturns_twice' attribute.
            * doc/extend.texi: Document eturns_twice' attribute.
            * tree.h (DECL_IS_RETURNS_TWICE): New macro.
            (struct tree_decl): Add returns_twice_flag.
    
    From-SVN: r96101

---
+++ gcc/testsuite/gcc.dg/attr-returns_twice-1.c
@@ -0,0 +1,23 @@
+/* { dg-do compile { target i?86-*-* } } */
+/* { dg-options "-W" } */
+
+int newsetjmp(void) __attribute__((returns_twice));
+void g(int);
+
+int
+main (void)
+{
+  register int reg asm ("esi") = 1; /* { dg-warning "might be clobbered" "" } */
+
+  if (!newsetjmp ())
+    {
+      reg = 2;
+      g (reg);
+    }
+  else
+    {
+      g (reg);
+    }
+
+  return 0;
+}
