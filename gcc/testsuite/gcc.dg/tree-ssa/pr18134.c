de2f99ddbb1405d57de7d6df8d214aaabb79248f
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-08 03:51:26 +0000

    * gcc.dg/tree-ssa/pr18134.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@96087 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr18134.c
@@ -0,0 +1,25 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-optimized" } */
+
+int  foo (int a)
+{
+void *p;
+if (a!=0)
+  p = &&L0;
+else
+  p = &&L1;
+goto *p;
+L0:
+return 1;
+L1:
+return 0;
+}
+
+/* Everything should have been cleaned up leaving a simple
+   return statement.  */
+/* { dg-final { scan-tree-dump-times "return.*a != 0" 1 "optimized" } } */
+
+/* There should not be any abnormal edges as DOM removed the
+   computed gotos.  */
+
+/* { dg-final { scan-tree-dump-times "ab" 0 "optimized" } } */
