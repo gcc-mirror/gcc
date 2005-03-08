6cd3dd5b400118501adb120511128e772494bfda
Ira Rosen <irar@il.ibm.com>
2005-03-08 11:12:00 +0000

    tree-vect-analyze.c (vect_object_analysis): Analyze initial condition of access function instead of base.
            * tree-vect-analyze.c (vect_object_analysis): Analyze initial
            condition of access function instead of base.
    
    From-SVN: r96092

---
+++ gcc/testsuite/gcc.dg/vect/pr20122.c
@@ -0,0 +1,38 @@
+/* { dg-require-effective-target vect_int } */
+
+#include <stdarg.h>
+#include "tree-vect.h"
+
+typedef short ashort __attribute__ ((__aligned__(16)));
+ashort Kernshort[24];
+static void VecBug(ashort Kernel[8][24]) __attribute__((noinline));
+static void VecBug(ashort Kernel[8][24]);
+
+/* Doesn't occur of only inner-loop. */
+static void VecBug(ashort Kernel[8][24])
+{
+  int k,i;
+    for (k = 0; k<8; k++)
+        for (i = 0; i<24; i++)
+            Kernshort[i] = Kernel[k][i];
+}
+
+int main (int argc, char **argv)
+{
+    check_vect ();
+
+    ashort Kernel[8][24];
+    int k,i;
+
+    for (k = 0; k<8; k++)
+        for (i = 0; i<24; i++)
+            Kernel[k][i] = 0;
+
+    VecBug(Kernel);
+
+    return 0;
+}
+
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 2 "vect" } } */
+
