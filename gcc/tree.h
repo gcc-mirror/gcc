685ae8c92a7b73a690865b28be92a742f26a7aae
Per Bothner <per@bothner.com>
2005-03-07 17:28:39 -0800

    * tree.h (DECL_IS_BUILTIN): Reverted my patch from earlier today.
    From-SVN: r96075

--- gcc/tree.h
+++ gcc/tree.h
@@ -1952,11 +1952,7 @@ struct tree_binfo GTY (())
 #define DECL_SOURCE_LOCATION(NODE) (DECL_CHECK (NODE)->decl.locus)
 #define DECL_SOURCE_FILE(NODE) LOCATION_FILE (DECL_SOURCE_LOCATION (NODE))
 #define DECL_SOURCE_LINE(NODE) LOCATION_LINE (DECL_SOURCE_LOCATION (NODE))
-#if 0
-/* Should be: #ifdef USE_MAPPED_LOCATION
-   However, there appears to be a jc1 bug that this would expose.
-   (It prevents bookstrapping because dwarf2out tries to emit a
-   non-found class.) FIXME. */
+#ifdef USE_MAPPED_LOCATION
 #define DECL_IS_BUILTIN(DECL) \
   (DECL_SOURCE_LOCATION (DECL) <= BUILTINS_LOCATION)
 #else
