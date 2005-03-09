9c90a97e936f00e67938ba115719fe7dda5c545e
Ulrich Weigand <uweigand@de.ibm.com>
2005-03-09 22:05:15 +0000

    s390.c (s390_secondary_output_reload_class): Adapt check for non-offsettable memory references to cope with outstanding...
    	* config/s390/s390.c (s390_secondary_output_reload_class): Adapt check
    	for non-offsettable memory references to cope with outstanding reload
    	replacements.
    	* config/s390/s390.md ("reload_outti"): Call find_replacement to
    	avoid losing outstanding address reloads.
    	("reload_outdi", "reload_outdf"): Likewise.
    
    testsuite/ChangeLog:
    
    	* gcc.dg/20050309-1.c: New test.
    
    From-SVN: r96214

---
+++ gcc/testsuite/gcc.dg/20050309-1.c
@@ -0,0 +1,37 @@
+/* This caused an ICE on s390 due to incorrect secondary
+   output reloads.  */
+
+/* { dg-do compile } */
+/* { dg-options "-O2 -fprofile-generate" } */
+
+char *
+test(char *ret, int *counter, void *schema,
+     const char* name, const char *namespace,
+     void *node, int topLevel)
+{
+  char buf[30];
+  int val;
+
+  if (counter == 0) return 0;
+  if (schema == 0) return 0;
+  if (name == 0) return 0;
+
+  __builtin_memset (ret, 0, 100);
+  lookup (schema, name, -1);
+  val = hash (schema, name, namespace, name, ret);
+  if (val == 0) return ret;
+
+  if (topLevel != 0)
+    {
+      error (1, 0, 0, node, "%s", name);
+      return 0;
+    }
+
+  __snprintf_chk (buf, 29, 1, 30, "#eCont %d", ++*counter);
+  val = hash (schema, name, buf, namespace, ret);
+  if (val == 0) return ret;
+
+  error (1, 0, 0, node, "%s", name);
+  return 0;
+}
+
