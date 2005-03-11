668c3bf83e9e751cac1d5334957bdb47f5fe79b3
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-11 00:29:37 +0000

    * objc.dg/stabs-1.m (dg-final): Change regexp pattern for hppa SOM.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@96278 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/objc.dg/stabs-1.m
+++ gcc/testsuite/objc.dg/stabs-1.m
@@ -15,4 +15,4 @@
 }
 @end
 
-/* { dg-final { scan-assembler ".text\"?\n\t.stabs.*100,0,0,(\\.)?L?Letext\[0-9\]*\n(\\.)?L?Letext" } } */
+/* { dg-final { scan-assembler "(.SUBSPA.*\[\$\]CODE\[\$\]|.text\"?)\n\t.stabs.*100,0,0,(\\.)?L?L\[\$\]?etext\[0-9\]*\n(\\.)?L?L\[\$\]?etext" } } */
