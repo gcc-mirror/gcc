7c6d34c729ac9aacc47aaaa081f3ae5459f1fe70
Per Bothner <per@bothner.com>
2005-03-12 07:40:35 -0800

    decl-2.c: Add extra line breaks.
    	* gcc.dg/decl-2.c:  Add extra line breaks.  That way we can verify
    	that diagnotics refer to correct token.
    	* gcc.dg/unused-3.c: Likewise.
    
    From-SVN: r96339

--- gcc/testsuite/gcc.dg/decl-2.c
+++ gcc/testsuite/gcc.dg/decl-2.c
@@ -1,10 +1,16 @@
 /* PR c/7411 */
 /* Contributed by Christian Ehrhardt */
+/* Added extra line-breaks to check that diagnostics refer to correct token.
+   --Per Bothner. */
 /* { dg-do compile } */
 
 void foo(void)
 {
-  char c;  /* { dg-error "previous declaration" } */
+  char
+    c	/* { dg-error "previous declaration" } */
+    ;
   int i;
-  int c = i;  /* { dg-error "conflicting types" } */
+  int
+    c	/* { dg-error "conflicting types" } */
+    = i;
 }
