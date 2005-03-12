b7119a5a93da0cc44218f28bc3046de61adbc1c1
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-12 00:34:07 +0000

    * cfglayout.c, loop-doloop.c, profile.c, target-def.h, 	target.h, targhooks.c, tracer.c, tree-ssa-loop-ch.c, 	tree-ssa-loop-unswitch.c, tree-ssa-loop.c, vec.c, vmsdbgout.c, 	config/m32r/m32r.md, config/xtensa/xtensa.h: Update copyright.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@96320 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/vmsdbgout.c
+++ gcc/vmsdbgout.c
@@ -1,6 +1,6 @@
 /* Output VMS debug format symbol table information from GCC.
    Copyright (C) 1987, 1988, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
+   1999, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
    Contributed by Douglas B. Rupp (rupp@gnat.com).
    Updated by Bernard W. Giroud (bgiroud@users.sourceforge.net).
 
