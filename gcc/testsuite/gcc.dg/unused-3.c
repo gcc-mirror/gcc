5db3e84328a14a9cd27e18becaeab012728c26ba
bothner <bothner@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-12 15:40:35 +0000

    * gcc.dg/decl-2.c:  Add extra line breaks.  That way we can verify 	that diagnotics refer to correct token. 	* gcc.dg/unused-3.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@96339 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/unused-3.c
+++ gcc/testsuite/gcc.dg/unused-3.c
@@ -1,10 +1,14 @@
 /* Copyright (C) 2000  Free Software Foundation.  */
+/* Added extra line-breaks to check that diagnostics refer to correct token.
+   --Per Bothner. */
 /* { dg-do compile } */
 /* { dg-options "-Wunused" } */
 
 typedef short unused_type __attribute__ ((unused));
 main ()
 {
-  short x;  /* { dg-warning "unused variable" "unused variable warning" } */
+  short
+    x   /* { dg-warning "unused variable" "unused variable warning" } */
+    ;
   unused_type y;
 }
