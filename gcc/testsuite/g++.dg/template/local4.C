e32bc24c11467027ae1bd3e66302f7b23bb112db
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-15 02:16:25 +0000

    * g++.dg/other/cv_func.C, g++.dg/other/offsetof3.C, 	g++.dg/parse/error11.C, g++.dg/tc1/dr108.C, 	g++.dg/template/local4.C, g++.dg/template/nontype6.C, 	g++.dg/warn/deprecated.C: Ensure uniqueness of test assertion 	names.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@96463 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/template/local4.C
+++ gcc/testsuite/g++.dg/template/local4.C
@@ -6,6 +6,6 @@ int main () {
   struct S {};
   // We do not simply use "local|match" on line 10 because we want to
   // make sure that "local" appears.
-  // { dg-error "local" "" { target *-*-* } 10 }
+  // { dg-error "local" "local" { target *-*-* } 10 }
   foo<S> (); // { dg-error "trying|match" } 
 }
--- gcc/testsuite/g++.dg/template/nontype6.C
+++ gcc/testsuite/g++.dg/template/nontype6.C
@@ -13,7 +13,7 @@ template <class T>
 void func(void)
 {
   (void)A<T>::type();	 // { dg-error "if a type is meant" }
-// { dg-error "parsed as a non-type" "" { target *-*-* } 15 }
+// { dg-error "parsed as a non-type" "non-type" { target *-*-* } 15 }
 }
 
 template void func<float>(void);    // { dg-error "instantiated from here" }
