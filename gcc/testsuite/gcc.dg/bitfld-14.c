aaceaa0f0578483b127c56992aa95d23561a078c
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-20 01:52:35 +0000

    * c-decl.c (check_bitfield_type_and_width): Require bit-field 	width to have integer type. 	(build_enumerator): Require enumerator value to have integer type.
    testsuite:
    	* gcc.dg/bitfld-14.c, gcc.dg/enum3.c: New tests.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@96755 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/bitfld-14.c
@@ -0,0 +1,11 @@
+/* Test for non-integer bit-field widths.  */
+/* Origin: Joseph Myers <joseph@codesourcery.com> */
+/* { dg-do compile } */
+/* { dg-options "" } */
+
+enum e { E, F };
+struct s {
+  int a : (void *)4; /* { dg-error "error: bit-field 'a' width not an integer constant" } */
+  int b : (enum e)F;
+  int c : (_Bool)1;
+};
