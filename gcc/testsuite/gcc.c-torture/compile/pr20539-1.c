d83877e2a3e7a3374500341001d5c98d794ee6cf
No Author <no-author@gcc.gnu.org>
2005-03-21 12:55:15 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r96792

---
+++ gcc/testsuite/gcc.c-torture/compile/pr20539-1.c
@@ -0,0 +1,10 @@
+char l7_en;
+long long l6_data_Z_0th;
+int t;
+void f()
+{
+  if (((char )(l6_data_Z_0th>>1 & 1U)) & ((l6_data_Z_0th & 1U)
+     | !(((char )(l6_data_Z_0th>>35 & 15U))==14U)))
+    t = 0ULL;
+}
+
