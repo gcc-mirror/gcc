49dcfcc430fe1edb15db73f772635077ab2b72bd
reichelt <reichelt@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-22 15:07:04 +0000

    PR c++/19980 	* decl.c (start_preparsed_function): Robustify.
    	* g++.dg/template/redecl3.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@96872 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/redecl3.C
@@ -0,0 +1,7 @@
+// PR c++/19980
+// Origin: Volker Reichelt <reichelt@igpm.rwth-aachen.de>
+
+// { dg-do compile }
+
+int foo;                    // { dg-error "previous declaration" }
+template<int> void foo() {} // { dg-error "redeclared" }
