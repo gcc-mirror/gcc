74c673150349ce3fbfabbedbbace6794ed06514b
Mohan Embar <gnustuff@thisiscool.com>
2005-03-22 22:56:19 +0000

    natPlainSocketImplWin32.cc (read): Handle count == 0 case.
    	* gnu/java/net/natPlainSocketImplWin32.cc (read): Handle
    	count == 0 case.
    
    Co-Authored-By: David Daney <ddaney@avtrex.com>
    
    From-SVN: r96897

--- libjava/gnu/java/net/natPlainSocketImplWin32.cc
+++ libjava/gnu/java/net/natPlainSocketImplWin32.cc
@@ -491,6 +491,11 @@ jint
 gnu::java::net::PlainSocketImpl$SocketInputStream::read(jbyteArray buffer,
   jint offset, jint count)
 {
+  // If zero bytes were requested, short circuit so that recv
+  // doesn't signal EOF.
+  if (count == 0)
+    return 0;
+
   if (! buffer)
     throw new ::java::lang::NullPointerException;
 
