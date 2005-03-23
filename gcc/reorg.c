81e974f22ec3484facb70bcfa892c16b0b20e3c1
Ian Lance Taylor <ian@airs.com>
2005-03-23 14:53:52 +0000

    * reorg.c (dbr_schedule): Remove #if 0 code to call final.
    From-SVN: r96931

--- gcc/reorg.c
+++ gcc/reorg.c
@@ -3582,17 +3582,6 @@ dbr_schedule (rtx first, FILE *file)
 {
   rtx insn, next, epilogue_insn = 0;
   int i;
-#if 0
-  int old_flag_no_peephole = flag_no_peephole;
-
-  /* Execute `final' once in prescan mode to delete any insns that won't be
-     used.  Don't let final try to do any peephole optimization--it will
-     ruin dataflow information for this pass.  */
-
-  flag_no_peephole = 1;
-  final (first, 0, NO_DEBUG, 1, 1);
-  flag_no_peephole = old_flag_no_peephole;
-#endif
 
   /* If the current function has no insns other than the prologue and
      epilogue, then do not try to fill any delay slots.  */
