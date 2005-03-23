b9fba50abc3d6dc4178b1140cb9ecd6d313ffab2
hp <hp@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-23 04:18:54 +0000

    * gcc.dg/torture/cris-asm-mof-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@96924 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/cris-asm-mof-1.c
@@ -0,0 +1,35 @@
+/* { dg-do compile { target cris-*-* } } */
+/* { dg-options "-O2 -march=v10" } */
+/* { dg-final { scan-assembler "in-asm: .mof" } } */
+/* { dg-final { scan-assembler "out-asm: .mof" } } */
+/* { dg-final { scan-assembler "in2-asm: .mof" } } */
+/* { dg-final { scan-assembler "out2-asm: .mof" } } */
+
+unsigned int
+in (unsigned int i)
+{
+  register int i0 asm ("mof") = i;
+  asm ("in-asm: %0" : : "x" (i0));
+}
+
+unsigned int
+out (void)
+{
+  register int o asm ("mof");
+  asm ("out-asm: %0" : "=x" (o));
+  return o;
+}
+
+unsigned int
+in2 (unsigned int i)
+{
+  asm ("in2-asm: %0" : : "h" (i));
+}
+
+unsigned int
+out2 (void)
+{
+  unsigned int o;
+  asm ("out2-asm: %0" : "=h" (o));
+  return o;
+}
