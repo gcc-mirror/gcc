70952ee73f4ca4dc9fdf317a7e0290a42caa18ad
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-23 21:42:10 +0000

    PR libfortran/18025         * write.c (output_float): Handling the "F0.d" format similarly as         commercial compilers.
            * pr18025.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@96953 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr18025.f90
@@ -0,0 +1,8 @@
+! PR libfortran/18025  <coudert@clipper.ens.fr>
+! {dg-do run}
+  character(len=80) :: c
+  write(c, "('#',F0.2,'#')") 1.23
+  if (c /= '#1.23#') call abort
+  write(c, "('#',F0.2,'#')") -1.23
+  if (c /= '#-1.23#') call abort
+  end
