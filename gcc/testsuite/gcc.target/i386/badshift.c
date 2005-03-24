fab2f52cf844f20ec3b575d41ad7e5c2d817190d
Alexandre Oliva <aoliva@redhat.com>
2005-03-24 05:57:52 +0000

    re PR rtl-optimization/20532 (Bad code for DImode left shifts by 31 and then 1)
    gcc/ChangeLog:
    PR rtl-optimization/20532
    * simplify-rtx.c (simplify_binary_operation_1): Protect from
    overflow when adding coefficients for PLUS or MINUS.
    (simplify_binary_operation_1): Handle CONST_DOUBLE exact power of
    two as multiplier.
    gcc/testsuite/ChangeLog:
    PR rtl-optimization/20532
    * gcc.target/i386/badshift.c: New.
    
    From-SVN: r96982

---
+++ gcc/testsuite/gcc.target/i386/badshift.c
@@ -0,0 +1,28 @@
+/* PR rtl-optimization/20532 */
+
+/* { dg-do run } */
+/* { dg-options "-m32 -march=i386 -O1" } */
+
+/* We used to optimize the DImode shift-by-32 to zero because in combine
+   we turned:
+
+     (v << 31) * (v << 31)
+
+   into:
+
+     (v * (((HOST_WIDE_INT)1 << 31) + ((HOST_WIDE_INT)1 << 31)))
+
+   With a 32-bit HOST_WIDE_INT, the coefficient overflowed to zero.  */
+
+unsigned long long int badshift(unsigned long long int v)
+{
+        return v << 31 << 1;
+}
+
+extern void abort ();
+
+int main() {
+  if (badshift (1) == 0)
+    abort ();
+  return 0;
+}
