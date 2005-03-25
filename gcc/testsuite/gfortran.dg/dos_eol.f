85255ed00a19f6e0f5bbadb131ee9e32d7b9ee8a
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-25 13:35:29 +0000

    PR libfortran/19678         PR libfortran/19679         * gfortran.dg/dos_eol.f: New test.
            PR libfortran/19678
            * list_read.c (next_char, eat_separator, finish_separator, read_real)
            (namelist_read): Add support for '\r' as well as '\n' as EOL
            character.
    
            PR libfortran/19679
            * list_read.c (read_sf): Add a '\r' in a test to support DOS
            line-endings when line length is exceeded.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@97041 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/dos_eol.f
@@ -0,0 +1,19 @@
+! PR libfortran/19678 and PR libfortran/19679
+! { dg-do run }
+      integer i, j
+      
+      open (10,status='scratch')
+      write (10,'(2A)') '1', achar(13)
+      rewind (10)
+      read (10,*) i
+      if (i .ne. 1) call abort
+      close (10)
+
+      open (10,status='scratch')
+      write (10,'(2A)') '   1', achar(13)
+      write (10,'(2A)') '   2', achar(13)
+      rewind (10)
+      read (10,'(I4)') i
+      read (10,'(I5)') j
+      if ((i .ne. 1) .or. (j .ne. 2)) call abort
+      end
