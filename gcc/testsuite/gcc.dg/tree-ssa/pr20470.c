90faeb2812dd06581e64a06d62e59c5ea084d926
Pat Haugen <pthaugen@us.ibm.com>
2005-03-25 20:10:38 +0000

    re PR tree-optimization/20470 (Branching sequence generated for ABS(x-y))
    2005-03-25  Pat Haugen  <pthaugen@us.ibm.com>
    
            PR tree-optimization/20470
            * gcc.dg/tree-ssa/pr20470.c: New test.
    
    From-SVN: r97063

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr20470.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-options "-fdump-tree-generic" } */
+
+/* PR tree-optimization/20470:
+    Missing fold to abs(x) when x == MINUS_EXPR.  */
+#define abs(x)  ((x) >= 0 ? (x) : -(x))
+
+int i,j,k;
+void f1()
+{
+  i = abs(j-k);
+}
+
+/* { dg-final { scan-tree-dump-times "ABS_EXPR" 1 "generic" } } */
