b982b5f11cbf61457a04d5d5ef27fb4d139c1e70
Steve Ellcey <sje@cup.hp.com>
2005-03-28 18:19:12 +0000

    re PR target/19890 (gcc.dg/20020219-1.c execution test fails on ia64-hpux with -milp32)
    	PR target/19890
    	* gcc.dg/20020219-1.c: Skip on IA64 HP-UX in ILP32 mode.
    
    From-SVN: r97136

--- gcc/testsuite/gcc.dg/20020219-1.c
+++ gcc/testsuite/gcc.dg/20020219-1.c
@@ -1,13 +1,18 @@
 /* PR c/4389
    This testcase failed because host_integerp (x, 0) was returning
-   1 even for constants bigger than 2^31.  It fails under under hpux
-   without -mdisable-indexing because the pointer x - 1 is used as
-   the base address of an indexed load.  Because the struct A is not
+   1 even for constants bigger than 2^31.  It fails under under hppa
+   hpux without -mdisable-indexing because the pointer x - 1 is used
+   as the base address of an indexed load.  Because the struct A is not
    actually allocated, x - 1 lies in the text segment and this causes
-   the wrong space register to be selected for the load.  */
+   the wrong space register to be selected for the load.  It fails on
+   IA64 hpux in ILP32 mode because extending x - 1 before adding the
+   array offset gives a different answer then adding first and then
+   extending.  The underlying problem is the same as with hppa, x - 1 is
+   not a legal data address.  */
 /* { dg-do run } */
 /* { dg-options "-O2" } */
 /* { dg-options "-O2 -mdisable-indexing" { target hppa*-*-hpux* } } */
+/* { dg-skip-if "" { "ia64-*-hpux*" } "*" "-mlp64" } */
 
 /* Disable the test entirely for 16-bit targets.  */
 #if __INT_MAX__ > 32767
