cff9bd7dba49ac01c42906be70e395481b764d7e
kargl <kargl@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-30 00:35:23 +0000

    New test for -fdefault-integer-8 and -fdefault-real-8
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@97222 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/promotion.f90
@@ -0,0 +1,12 @@
+! { dg-do run { target i?86-*-* } }
+! { dg-options "-fdefault-integer-8 -fdefault-real-8" }
+program a
+   logical l
+   integer i
+   real x
+   double precision d
+   if (kind(l) /= 8) call abort
+   if (kind(i) /= 8) call abort
+   if (kind(x) /= 8) call abort
+   if (kind(d) /= 8) call abort
+end program a
