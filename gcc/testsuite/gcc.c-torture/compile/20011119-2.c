6bdf702952fe658d15dd5ec1e5a4de93756eb784
amylaar <amylaar@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-31 13:02:36 +0000

    Fix fallout from PR middle-end/15700: 	* gcc.c-torture/compile/20011119-1.c: Take 	__USER_LABEL_PREFIX__ into account. 	* gcc.c-torture/compile/20011119-2.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@97317 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/compile/20011119-2.c
+++ gcc/testsuite/gcc.c-torture/compile/20011119-2.c
@@ -1,6 +1,10 @@
+#define ASMNAME(cname)  ASMNAME2 (__USER_LABEL_PREFIX__, cname)
+#define ASMNAME2(prefix, cname) STRING (prefix) cname
+#define STRING(x)    #x
+
 extern inline int foo (void) { return 23; }
 int bar (void) { return foo (); }
 extern int foo (void) __attribute__ ((weak, alias ("xxx")));
 int baz (void) { return foo (); }
-int xxx(void) __asm__("xxx");
+int xxx(void) __asm__(ASMNAME ("xxx"));
 int xxx(void) { return 23; }
