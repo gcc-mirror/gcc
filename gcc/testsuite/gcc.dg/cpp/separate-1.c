42d601653239960959751966f4b3120c63b68903
janis <janis@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-31 18:21:38 +0000

    * gcc.dg/altivec-16.c: Use cleanup-saved-temps. 	* gcc.dg/cpp/direct2s.c: Ditto. 	* gcc.dg/cpp/paste4.c: Ditto. 	* gcc.dg/cpp/separate-1.c: Ditto. 	* gcc.dg/pch/save-temps-1.c: Ditto. 	* g++.dg/ext/altivec-10.C: Ditto. 	* g++.dg/pch/pch.C: Ditto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@97341 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cpp/separate-1.c
+++ gcc/testsuite/gcc.dg/cpp/separate-1.c
@@ -13,3 +13,5 @@ int FOO( /* { dg-error "parse error|syntax error|expected" "error on this line" 
 
 int baz FOO /* { dg-error "parse error|syntax error|expected" "error on this line" } */
 ;
+
+/* { dg-final { cleanup-saved-temps } } */
