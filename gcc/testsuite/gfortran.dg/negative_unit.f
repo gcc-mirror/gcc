71ff4bdf51ed7c41c4c04d1e197404c6cd2895b8
No Author <no-author@gcc.gnu.org>
2005-03-31 15:36:09 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r97328

---
+++ gcc/testsuite/gfortran.dg/negative_unit.f
@@ -0,0 +1,22 @@
+! {dg-do run}
+!
+! PR libfortran/20660 and other bugs (not filed in bugzilla) relating
+! to negative units
+!
+! Bugs submitted by Walt Brainerd
+      integer i
+      logical l
+      
+      i = 0
+! gfortran created a 'fort.-1' file and wrote "Hello" in it
+      write (unit=-1, fmt=*, iostat=i) "Hello"
+      if (i <= 0) call abort
+      
+      i = 0
+      open (unit=-11, file="xxx", iostat=i)
+      if (i <= 0) call abort
+
+      inquire (unit=-42, exist=l)
+      if (l) call abort
+
+      end
