fd544fc8139ee76cbb18a2e547b90429b6d991ff
dje <dje@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-31 16:01:53 +0000

    * tree-ssa-loop-im.c (stmt_cost): Add RDIV_EXPR to list of 	expensive operations.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@97330 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-loop-im.c
+++ gcc/tree-ssa-loop-im.c
@@ -436,6 +436,7 @@ stmt_cost (tree stmt)
     case FLOOR_MOD_EXPR:
     case ROUND_MOD_EXPR:
     case TRUNC_MOD_EXPR:
+    case RDIV_EXPR:
       /* Division and multiplication are usually expensive.  */
       cost += 20;
       break;
