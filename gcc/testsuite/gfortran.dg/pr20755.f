25e308459affa949d6066f6fb56ea3b3be1e017a
No Author <no-author@gcc.gnu.org>
2005-04-05 14:24:36 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r97629

---
+++ gcc/testsuite/gfortran.dg/pr20755.f
@@ -0,0 +1,10 @@
+! PR libfortran/20755
+! { dg-do run }
+      character*30 s
+      
+      write (s,2000) 0.0, 0.02
+      if (s .ne. "    0.00       2.000E-02") call abort
+      write (s,2000) 0.01, 0.02
+      if (s .ne. "   1.000E-02   2.000E-02") call abort
+ 2000 format (1P2G12.3)
+      end
