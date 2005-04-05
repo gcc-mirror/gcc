3ce9c82463e3066170e8d8d4177f785e83537966
Ian Lance Taylor <ian@airs.com>
2005-04-05 04:08:56 +0000

    re PR debug/9963 ([CygWin] g++ -gcoff report "C_EFCN symbol out of scope")
    	PR debug/9963
    	* config/i386/cygming.h (ASM_OUTPUT_EXTERNAL): Pass DECL to
    	i386_pe_record_external_function.
    	(i386_pe_record_external_function): Update declaration.
    	* config/i386/winnt.c (struct extern_list): Add decl field.
    	(i386_pe_record_external_function): Add decl parameter.
    	(i386_pe_file_end): Check TREE_ASM_WRITTEN on decl, not
    	identifier.
    	* config/i386/i386-protos.h (i386_pe_record_external_function):
    	Update declaration.
    
    From-SVN: r97602

--- gcc/config/i386/winnt.c
+++ gcc/config/i386/winnt.c
@@ -719,6 +719,7 @@ i386_pe_declare_function_type (FILE *file, const char *name, int public)
 struct extern_list GTY(())
 {
   struct extern_list *next;
+  tree decl;
   const char *name;
 };
 
@@ -731,12 +732,13 @@ static GTY(()) struct extern_list *extern_head;
    for it then.  */
 
 void
-i386_pe_record_external_function (const char *name)
+i386_pe_record_external_function (tree decl, const char *name)
 {
   struct extern_list *p;
 
   p = (struct extern_list *) ggc_alloc (sizeof *p);
   p->next = extern_head;
+  p->decl = decl;
   p->name = name;
   extern_head = p;
 }
@@ -785,10 +787,11 @@ i386_pe_file_end (void)
     {
       tree decl;
 
-      decl = get_identifier (p->name);
+      decl = p->decl;
 
       /* Positively ensure only one declaration for any given symbol.  */
-      if (! TREE_ASM_WRITTEN (decl) && TREE_SYMBOL_REFERENCED (decl))
+      if (! TREE_ASM_WRITTEN (decl)
+	  && TREE_SYMBOL_REFERENCED (DECL_ASSEMBLER_NAME (decl)))
 	{
 	  TREE_ASM_WRITTEN (decl) = 1;
 	  i386_pe_declare_function_type (asm_out_file, p->name,
