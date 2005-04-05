3e0de9d1880ccba36323b5d6c81bc2a0bae912f8
Devang Patel <dpatel@apple.com>
2005-04-05 09:33:29 -0700

    altivec.md (altivec_vsr<VI_char>): Rename to ..
             * config/rs600/altivec.md (altivec_vsr<VI_char>): Rename to ..
             (lhsr<mode>3): ... new name.
             (altivec_vsra<VI_char>): Rename to ..
             (ashr<mode>3): ... new name.
             * config/rs6000/rs6000.c (builtin_description): Rename shift
             operations.
    
             testsuite:
             * lib/target-supports.exp (check_effective_target_vect_shif): New.
             * gcc.dg/vect/vect-shift-1.c: New test.
    
    From-SVN: r97636

---
+++ gcc/testsuite/gcc.dg/vect/vect-shift-1.c
@@ -0,0 +1,28 @@
+/* { dg-require-effective-target vect_shift } */
+
+#include <stdarg.h>
+#include <signal.h>
+
+#define N 16
+#define MAX 42
+
+extern void abort(void); 
+
+int main ()
+{  
+  unsigned int A[4] =  {0x08000000,0x08000001,0xff0000ff,0xf0000001};
+  unsigned int Answer[4] = {0x01000000,0x01000000,0x01fe0001f,0x1e000000};
+  unsigned int B[4];
+  int i, j;
+  
+  for (i=0; i<4; i++)
+    B[i] = A[i] >> 3;
+  for (i=0; i<4; i++)
+    if (B[i] != Answer[i])
+      abort ();
+  return 0;
+}
+
+
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { xfail i?86-*-* x86_64-*-* } } } */
