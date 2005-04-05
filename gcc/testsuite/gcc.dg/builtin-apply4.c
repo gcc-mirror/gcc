4b2841117bd44ef5b9179c02151a507dc1bd23f5
Jakub Jelinek <jakub@redhat.com>
2005-04-05 22:10:13 +0200

    re PR tree-optimization/20076 (__builtin_return(__builtin_apply()) inlined incorrectly)
    	PR tree-optimization/20076
    	* tree-inline.c (inline_forbidden_p_1): Prevent inlining functions
    	that call __builtin_return or __builtin_apply_args.
    
    	* gcc.dg/builtin-apply4.c: New test.
    
    From-SVN: r97653

---
+++ gcc/testsuite/gcc.dg/builtin-apply4.c
@@ -0,0 +1,30 @@
+/* PR tree-optimization/20076 */
+/* { dg-options "-O2" } */
+/* { dg-do run } */
+
+extern void abort (void);
+
+double
+foo (int arg)
+{
+  if (arg != 116)
+    abort();
+  return arg + 1;
+}
+
+inline double
+bar (int arg)
+{
+  foo (arg);
+  __builtin_return (__builtin_apply ((void (*) ()) foo,
+				     __builtin_apply_args (), 16));
+}
+
+int
+main (int argc, char **argv)
+{
+  if (bar (116) != 117.0)
+    abort ();
+
+  return 0;
+}
