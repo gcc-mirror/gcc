1cb8ea17a4f5f39fc75b2200ca4108760701ebdd
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-05 17:35:54 +0000

    PR c++/20673 	* decl.c (grokdeclarator): Correct attribute handling.
    	PR c++/20673
    	* g++.dg/ext/attrib21.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@97640 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/ext/attrib21.C
@@ -0,0 +1,17 @@
+// PR c++/20673
+
+typedef void *voidp;
+
+struct S
+{
+  char a;
+  voidp __attribute__ ((aligned (16))) b;
+};
+
+struct T
+{
+  char a;
+  void *__attribute__ ((aligned (16))) b;
+};
+
+int f[sizeof (struct S) != sizeof (struct T) ? -1 : 1];
