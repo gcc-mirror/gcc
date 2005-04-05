cce7d16e8c6047c02f61bf8e7ab2a5b4cafd5fa0
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-05 19:45:41 +0000

    * gfortran.dg/pr20755.f: Fixed bad format.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@97650 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/pr20755.f
+++ gcc/testsuite/gfortran.dg/pr20755.f
@@ -6,5 +6,5 @@
       if (s .ne. "    0.00       2.000E-02") call abort
       write (s,2000) 0.01, 0.02
       if (s .ne. "   1.000E-02   2.000E-02") call abort
- 2000 format (1P2G12.3)
+ 2000 format (1PG12.3,G12.3)
       end
