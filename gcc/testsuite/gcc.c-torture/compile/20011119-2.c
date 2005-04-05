bfcf81bf390640642434733e4bd9f16a1d374724
John David Anglin <dave.anglin@nrc-cnrc.gc.ca>
2005-04-05 00:17:25 +0000

    20011119-1.c: Require weak and alias support.
    	* gcc.c-torture/compile/20011119-1.c: Require weak and alias support.
    	* gcc.c-torture/compile/20011119-2.c: Likewise.
    	* gcc.c-torture/compile/981001-2.c: Likewise.
    
    From-SVN: r97589

--- gcc/testsuite/gcc.c-torture/compile/20011119-2.c
+++ gcc/testsuite/gcc.c-torture/compile/20011119-2.c
@@ -1,3 +1,5 @@
+/* { dg-require-weak "" } */
+/* { dg-require-alias "" } */
 #define ASMNAME(cname)  ASMNAME2 (__USER_LABEL_PREFIX__, cname)
 #define ASMNAME2(prefix, cname) STRING (prefix) cname
 #define STRING(x)    #x
