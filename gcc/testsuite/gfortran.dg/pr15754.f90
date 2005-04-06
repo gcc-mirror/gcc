15b2e925fa0738eee7d1c0d7a91c230a15b29c0f
tobi <tobi@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-06 18:03:09 +0000

    fortran/ * expr.c (gfc_check_assign): Don't allow NULL as rhs in a non-pointer assignment.
    testsuite/
    * gfortran.dg/pr15754.f90: Change annotations to dg-error.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@97740 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/pr15754.f90
+++ gcc/testsuite/gfortran.dg/pr15754.f90
@@ -1,7 +1,7 @@
 ! we didn't give a warning if the RHS of an assignment was NULL
 ! { dg-do-compile }
 INTEGER, POINTER :: P
-I = NULL() ! { dg-warning "NULL appears" "Assignment non-pointer = NULL" }
-P = NULL() ! { dg-warning "NULL appears" "Assignment pointer = NULL" }
+I = NULL() ! { dg-error "NULL appears" "Assignment non-pointer = NULL" }
+P = NULL() ! { dg-error "NULL appears" "Assignment pointer = NULL" }
 P => NULL()
 END
