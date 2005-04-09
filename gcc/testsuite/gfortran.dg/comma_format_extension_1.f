53d8a8ac22f1da70288ba496a0f63ca26f3e70dd
Andrew Pinski <pinskia@physics.uc.edu>
2005-04-09 18:44:07 +0000

    re PR fortran/13257 ([4.0 only] Error instead of warning for missing comma in format string)
    PR fortran/13257
    
    fortran/ChangeLog:
    	* io.c (check_format): Allow an optional comma
    	between descriptors.
    
    libgfortran/ChangeLog:
    	* format.c (parse_format_list): Allow an optional comma
    	between descriptors.
    
    testsuite/ChangeLog:
    	* comma_format_extension_[1234].f: New tests.
    
    From-SVN: r97919

---
+++ gcc/testsuite/gfortran.dg/comma_format_extension_1.f
@@ -0,0 +1,11 @@
+! { dg-do compile }
+! { dg-options "" }
+! test that the extension for a missing comma is accepted
+
+      subroutine mysub
+      dimension ibar(5)
+      write (3,1001) ( ibar(m), m = 1, 5 )
+
+ 1001 format (/5x,' ',i4' '/ )
+      return
+      end
---
+++ gcc/testsuite/gfortran.dg/comma_format_extension_2.f
@@ -0,0 +1,10 @@
+! { dg-do compile }
+! test that the extension for a missing comma is accepted
+
+      subroutine mysub
+      dimension ibar(5)
+      write (3,1001) ( ibar(m), m = 1, 5 )
+
+ 1001 format (/5x,' ',i4' '/ ) ! { dg-warning "Missing comma" }
+      return
+      end
