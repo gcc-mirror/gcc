5f115a4b06980023032379c4677c16251ed1b1b5
Steven G. Kargl <kargls@comcast.net>
2005-04-09 22:44:24 +0000

    * gfortran.dg/nint_1.f90: New test.
    From-SVN: r97931

---
+++ gcc/testsuite/gfortran.dg/nint_1.f90
@@ -0,0 +1,14 @@
+program nint_1
+  if (int(anint(8388609.0)) /= 8388609) call abort
+  if (int(anint(0.49999997)) /= 0) call abort
+  if (nint(8388609.0) /= 8388609) call abort
+  if (nint(0.49999997) /= 0) call abort
+  if (int(dnint(4503599627370497.0d0),8) /= 4503599627370497_8) call abort
+  if (int(dnint(0.49999999999999994d0)) /= 0) call abort
+  if (int(anint(-8388609.0)) /= -8388609) call abort
+  if (int(anint(-0.49999997)) /= 0) call abort
+  if (nint(-8388609.0) /= -8388609) call abort
+  if (nint(-0.49999997) /= 0) call abort
+  if (int(dnint(-4503599627370497.0d0),8) /= -4503599627370497_8) call abort
+  if (int(dnint(-0.49999999999999994d0)) /= 0) call abort
+end program nint_1
