59afe4b424016454a2a119a26743d0b75c46e9bc
Thomas Koenig <Thomas.Koenig@online.de>
2005-04-10 08:35:39 +0000

    [multiple changes]
    2005-04-10  Thomas Koenig <Thomas.Koenig@online.de>
    
            PR libfortran/17992
            PR libfortran/19568
            PR libfortran/19595
            PR libfortran/20005
            PR libfortran/20092
            PR libfortran/20131
            PR libfortran/20138
            PR libfortran/20661
            PR libfortran/20744
            * io/transfer.c (top level): eor_condition: New static variable.
            (read_sf): Remove unnecessary zeroing of buffer (there is enough
            information in its length).
            Return a string of length 0 (to be padded by caller) if EOR was
            seen previously.
            Remove erroneous special casing of EOR for standard input.
            Set eor_condition for non-advancing I/O if an end of line was
            detected.
            Increment ioparm.size if necessary.
            (formatted_transfer):  Skip the function if there is an EOR condition.
            (data_transfer_init):  Initialize eor_condition to zero (false).
            (next_record_r):  Clear sf_seen_eor if a \n has been seen already.
            (finalize_transfer):  If there is an EOR condition, raise the error.
    
    2005-04-10   Thomas Koenig  <Thomas.Koenig@online.de>
    
            * eor_handling_1.f90: New test case.
            * eor_handling_2.f90: New test case.
            * eor_handling_3.f90: New test case.
            * eor_handling_4.f90: New test case.
            * eor_handling_5.f90: New test case.
            * noadv_size.f90: New test case.
            * pad_no.f90: New test case.
    
    From-SVN: r97943

---
+++ gcc/testsuite/gfortran.dg/pad_no.f90
@@ -0,0 +1,15 @@
+! { dg-do run }
+! Test correct operation for pad='no'.
+program main
+  character(len=1) line(2)
+  line = 'x'
+  open(77,status='scratch',pad='no')
+  write(77,'(A)') 'a','b'
+  rewind(77)
+  read(77,'(2A)',iostat=i) line(1)
+  if (line(1) /= 'a' .or. line(2) /= 'x') call abort
+  rewind(77)
+  line = 'y'
+  read(77,'(2A)',iostat=i,advance='no') line
+  if (line(1) /= 'a' .or. line(2) /= 'y') call abort
+end program main
