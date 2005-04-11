0702744d3d81583923218f05236b092cbf9a4aaa
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-11 18:25:08 +0000

    2005-04-11  Andrew Pinski  <pinskia@physics.uc.edu>
            * gcc.dg/tree-ssa/alias-1.c: New test.
    2005-04-11  Andrew Pinski  <pinskia@physics.uc.edu>
    
            * tree-ssa-alias.c (may_alias_p): If the variable
            is a global variable and the pointer is parameter
            and -fargument-noalias-global is used, then
            the pointer cannot alias the variable.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@97978 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-alias.c
+++ gcc/tree-ssa-alias.c
@@ -1708,6 +1708,16 @@ may_alias_p (tree ptr, HOST_WIDE_INT mem_alias_set,
       alias_stats.simple_resolved++;
       return false;
     }
+  
+  /* If -fargument-noalias-global is >1, pointer arguments may
+     not point to global variables.  */
+  if (flag_argument_noalias > 1 && is_global_var (var)
+      && TREE_CODE (ptr) == PARM_DECL)
+    {
+      alias_stats.alias_noalias++;
+      alias_stats.simple_resolved++;
+      return false;
+    }
 
   m_ann = var_ann (mem);
 
