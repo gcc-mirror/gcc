ac14875119315347b004b4b1a55fa4f7915396a2
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-11 06:16:39 +0000

    * builtins.def (BUILT_IN_LCEIL, BUILT_IN_LCEILF, BUILT_IN_LCEILL) 	(BUILT_IN_LLCEIL, BUILT_IN_LLCEILF, BUILT_IN_LLCEILL): New. 	* optabs.h (enum optab_index): Add new OTI_lceil. 	(lceil_optab): Define corresponding macro. 	* optabs.c (init_optabs): Initialize lceil_optab. 	* genopinit.c (optabs): Implement lceil_optab using lceilsi2 	and lceildi2 patterns.
    	* builtins.c (expand_builtin_int_roundingfn): Handle
    	BUILT_IN_LCEIL{,F,L} and BUILT_IN_LLCEIL{,F,L}.
    	(fold_builtin_int_roundingfn): Handle BUILT_IN_LCEIL{,F,L} and
    	BUILT_IN_LLCEIL{,F,L}.
    	(fold_builtin_1): Fold BUILT_IN_LCEIL{,F,L} and
    	BUILT_IN_LLCEIL{,F,L} using fold_builtin_int_roundingfn.
    	(mathfn_built_in): Handle BUILT_IN LCEIL and BUILT_IN_LLCEIL.
    	(expand_builtin): Expand BUILT_IN_LCEIL{,F,L} and
    	BUILT_IN_LLCEIL{,F,L} using expand_builtin_int_roundingfn.
    
    	* convert.c (convert_to_integer): Convert (long int)ceil{,f,l},
    	into lceil built-in function and (long long int)ceil{,f,l} into
    	llceil built-in function.
    	* fold-const.c (tree_expr_nonnegative_p): Add BUILT_IN_LCEIL and
    	BUILT_IN_LLCEIL.
    testsuite:
    
    	* gcc.dg/builtins-53.c: Also check (int)ceil* and
    	(long long int)ceil*.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@97964 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/builtins-53.c
+++ gcc/testsuite/gcc.dg/builtins-53.c
@@ -1,7 +1,9 @@
 /* Copyright (C) 2005 Free Software Foundation.
 
    Check that (long)floor, (long)floorf, (long)floorl,
-   (long long)floor, (long long)floorf and (long long)floorl
+   (long long)floor, (long long)floorf, (long long)floorl,
+   (long)ceil, (long)ceilf, (long)ceill,
+   (long long)ceil, (long long)ceilf, (long long)ceill
    built-in functions compile.
 
    Written by Uros Bizjak, 5th April 2005.  */
@@ -10,8 +12,13 @@
 /* { dg-options "-O2 -ffast-math" } */
 
 extern double floor(double);
+extern double ceil(double);
+
 extern float floorf(float);
+extern float ceilf(float);
+
 extern long double floorl(long double);
+extern long double ceill(long double);
 
 
 long int test1(double x)
@@ -24,6 +31,16 @@ long long int test2(double x)
   return floor(x);
 }
 
+long int test3(double x)
+{
+  return ceil(x);
+}
+
+long long int test4(double x)
+{
+  return ceil(x);
+}
+
 long int test1f(float x)
 {
   return floorf(x);
@@ -34,6 +51,16 @@ long long int test2f(float x)
   return floorf(x);
 }
 
+long int test3f(float x)
+{
+  return ceilf(x);
+}
+
+long long int test4f(float x)
+{
+  return ceilf(x);
+}
+
 long int test1l(long double x)
 {
   return floorl(x);
@@ -42,4 +69,14 @@ long int test1l(long double x)
 long long int test2l(long double x)
 {
   return floorl(x);
+}
+
+long int test3l(long double x)
+{
+  return ceill(x);
+}
+
+long long int test4l(long double x)
+{
+  return ceill(x);
 }
