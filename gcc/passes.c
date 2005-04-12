85d31d705d4d77cfb1c655aed6556157ee07d66c
ctice <ctice@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-12 22:38:06 +0000

    Temporary fix for partitioning problems; real fix coming soon.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@98051 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/passes.c
+++ gcc/passes.c
@@ -328,8 +328,7 @@ rest_of_handle_final (void)
 
   timevar_push (TV_SYMOUT);
   (*debug_hooks->function_decl) (current_function_decl);
-  if (unlikely_text_section_name)
-    free (unlikely_text_section_name);
+
   timevar_pop (TV_SYMOUT);
 
   ggc_collect ();
