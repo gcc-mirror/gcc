f60a8a2d98f326c78993eff12ae7867b7125cb72
geoffk <geoffk@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-12 02:10:57 +0000

    * gcc.dg/i386-darwin-fpmath.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@98003 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/i386-darwin-fpmath.c
@@ -0,0 +1,9 @@
+/* { dg-do compile { target i?86-*-darwin* } } */
+/* { dg-final { scan-assembler "addsd" } } */
+/* Do not add -msse or -msse2 or -mfpmath=sse to the options.  GCC is
+   supposed to use SSE math on Darwin by default, and libm won't work
+   right if it doesn't.  */
+double foo(double x, double y)
+{
+  return x + y;
+}
