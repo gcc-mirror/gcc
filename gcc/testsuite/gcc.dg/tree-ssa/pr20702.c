60b4ccde85cd8823678f3f424508fe19f9d302e4
Kazu Hirata <kazu@cs.umass.edu>
2005-04-13 15:33:17 +0000

    re PR tree-optimization/20913 (copy-prop does not fold conditionals)
    gcc/
    	PR tree-optimization/20913
    	* tree-ssa-copy.c (copy_prop_visit_cond_stmt): Fold COND_EXPR.
    
    testsuite/
    	PR tree-optimization/20913
    	* gcc.dg/tree-ssa/pr20913.c: New.
    
    From-SVN: r98091

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr20702.c
@@ -0,0 +1,28 @@
+/* PR tree-optimization/20702
+   VRP did not insert ASSERT_EXPRs into dominator dominator children
+   of a basic block ending with COND_EXPR unless the children are also
+   immediate successors of the basic block.  */
+
+/* { dg-do compile } */
+/* { dg-options "-O2 -fno-tree-dominator-opts -fdump-tree-vrp-details" } */
+
+extern void bar (int);
+
+int
+foo (int *p, int b)
+{
+  int a;
+
+  if (b)
+    bar (123);
+  else
+    bar (321);
+
+  a = *p;
+  if (p == 0)
+    return 0;
+
+  return a;
+}
+
+/* { dg-final { scan-tree-dump-times "Folding predicate" 1 "vrp"} } */
