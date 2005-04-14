a0ce797889cd14d04b87cdacb089fcee6f73415e
Kazu Hirata <kazu@cs.umass.edu>
2005-04-14 13:34:57 +0000

    re PR tree-optimization/20657 ([tcb] VRP does not get rid of a redundant "if" statement.)
    gcc/
    	PR tree-optimization/20657
    	* tree-vrp.c (extract_range_from_expr): Notice INTEGER_CST to
    	create an appropriate range from it.
    
    testsuite/
    	PR tree-optimization/20657
    	* gcc.dg/tree-ssa/pr20657.c: New.
    
    From-SVN: r98134

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr20657.c
@@ -0,0 +1,17 @@
+/* PR tree-optimization/20657
+   VRP did not pick up a conditional equivalence from the first "if"
+   statement, which was needed to eliminate the second "if" statement.  */
+
+/* { dg-do compile } */
+/* { dg-options "-O2 -fno-tree-dominator-opts -fdump-tree-vrp-details" } */
+
+int
+foo (int a)
+{
+  if (a == 0)
+    if (a == 0)
+      return 1;
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "Folding predicate" 1 "vrp"} } */
