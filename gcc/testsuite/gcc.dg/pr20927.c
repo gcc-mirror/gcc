559be41a519baec62f81099ba904338607c687dd
Ulrich Weigand <uweigand@de.ibm.com>
2005-04-14 08:25:00 +0000

    re PR target/20927 (ICE in smallest_mode_for_size, at stor-layout.c:221 (s390x))
    ChangeLog:
    
    	PR target/20927
    	* config/s390/s390-modes.def: Define TFmode.
    
    testsuite/ChangeLog:
    
    	PR target/20927
    	* gcc.dg/pr20927.c: New test.
    
    From-SVN: r98128

---
+++ gcc/testsuite/gcc.dg/pr20927.c
@@ -0,0 +1,23 @@
+/* This caused an ICE on s390x due to a reload inheritance bug.  */
+
+/* { dg-do compile { target s390*-*-* } } */
+/* { dg-options "-O2" } */
+
+struct point { double x, y; };
+extern void use (struct point);
+
+void test (struct point *pc, struct point p1)
+{
+  struct point p0 = *pc;
+
+  if (p0.x == p1.x && p0.y == p1.y)
+    use (p0);
+
+  asm ("" : : : "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+
+  p1.y -= p0.y;
+
+  use (p0);
+  use (p1);
+}
+
