3858668b7f79b15e2225861184fee21aca855066
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-17 11:56:30 +0000

    2005-04-17  Richard Guenther  <rguenth@gcc.gnu.org>
    	* scanner.c (gfc_next_char_literal): Reset truncation flag
    	for lines ending in a comment for both fixed and free form.
    	(load_line): Do not set truncated flag if only truncating
    	the EOL marker.
    
    	* gfortran.dg/wtruncate.f: New testcase.
    	* gfortran.dg/wtruncate.f90: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@98269 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/wtruncate.f
@@ -0,0 +1,10 @@
+! { dg-do compile }
+! { dg-options "-Wall" }
+
+! This long comment line should not trigger a line-truncation warning with -Wall
+
+      PROGRAM foo
+      WRITE (*,*) "Test"           ! Neither this comment which exceeds the 72 character limit, too
+      WRITE (*,*) "This exactly 72 character long soruce line not, too."
+      END
+
