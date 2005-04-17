2be2fbdf11f527ca99612f65680ea3522502c971
tkoenig <tkoenig@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-17 20:26:57 +0000

    005-04-17  Thomas Koenig  <Thomas.Koenig@online.de>
            PR libfortran/21075
            * m4/reshape.m4 (reshape_`'rtype_kind):  Change dimension
            of auxiliary arrays from GFC_MAX_DIMENSIONS - 1 to
            GFC_MAX_DIMENSIONS.
            * intrinsics/reshape_generic.c (reshape_generic):  Likewise.
            * generated/reshape_i4.c:  Regenerated.
            * generated/reshape_i8.c:  Regenerated.
    
    2005-04-17  Thomas Koenig  <Thomas.Koenig@online.de>
    
            PR libfortran/21075
            * gfortran.dg/reshape_rank7.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@98288 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/reshape_rank7.f90
@@ -0,0 +1,27 @@
+! { dg-do run}
+! PR 21075:  Reshape with rank 7 used to segfault.
+program main
+  integer :: a(256), b(2,2,2,2,2,2,2)
+  do i=1,256
+     a(i) = i
+  end do
+  b = reshape(a(1:256:2), shape(b))
+  do i1=1,2
+     do i2=1,2
+        do i3=1,2
+           do i4=1,2
+              do i5=1,2
+                 do i6=1,2
+                    do i7=1,2
+                       if (b(i1,i2,i3,i4,i5,i6,i7) /= &
+                            2*((i1-1)+(i2-1)*2+(i3-1)*4+(i4-1)*8+&
+                            (i5-1)*16+(i6-1)*32+(i7-1)*64)+1) &
+                            call abort
+              end do
+            end do
+          end do
+        end do
+      end do
+    end do
+  end do
+end program main
