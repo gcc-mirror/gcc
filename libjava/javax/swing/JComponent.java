615af6564e786fc91c09bafd1c60bf421d88f1f1
Roman Kennke <roman@kennke.org>
2005-04-19 05:28:38 +0000

    JComponent.java: Modified alignmentX and alignmentY to be 0.5 instead of 0.0.
    2005-04-19  Roman Kennke  <roman@kennke.org>
    
    	* javax/swing/JComponent.java:
    	Modified alignmentX and alignmentY to be 0.5 instead of 0.0.
    	Untabified the file.
    
    From-SVN: r98377

--- libjava/javax/swing/JComponent.java
+++ libjava/javax/swing/JComponent.java
@@ -175,7 +175,7 @@ public abstract class JComponent extends Container implements Serializable
    * @see javax.swing.OverlayLayout
    * @see javax.swing.BoxLayout
    */
-  float alignmentX = 0.0f;
+  float alignmentX = 0.5f;
 
   /**
    * A value between 0.0 and 1.0 indicating the preferred vertical
@@ -190,7 +190,7 @@ public abstract class JComponent extends Container implements Serializable
    * @see javax.swing.OverlayLayout
    * @see javax.swing.BoxLayout
    */
-  float alignmentY = 0.0f;
+  float alignmentY = 0.5f;
 
   /** 
    * The border painted around this component.
@@ -1137,9 +1137,9 @@ public abstract class JComponent extends Container implements Serializable
    */
   public JToolTip createToolTip()
   {
-	JToolTip toolTip = new JToolTip();
-	toolTip.setComponent(this);
-	toolTip.setTipText(toolTipText);
+        JToolTip toolTip = new JToolTip();
+        toolTip.setComponent(this);
+        toolTip.setTipText(toolTipText);
     
     return toolTip;
   }
@@ -1173,14 +1173,14 @@ public abstract class JComponent extends Container implements Serializable
       toolTipText = null;
       return;
     }
-		
+                
     // XXX: The tip text doesn't get updated unless you set it to null
     // and then to something not-null. This is consistent with the behaviour
     // of Sun's ToolTipManager.
-			
+                        
     String oldText = toolTipText;
     toolTipText = text;
-		
+                
     if (oldText == null)
       ToolTipManager.sharedInstance().registerComponent(this);
   }
@@ -1428,7 +1428,7 @@ public abstract class JComponent extends Container implements Serializable
             g2 = doubleBuffer.getGraphics();
             g2.setClip(g.getClipBounds());
           }
-	  
+          
         g2 = getComponentGraphics(g2);
         paintComponent(g2);
         paintBorder(g2);
@@ -2273,7 +2273,7 @@ public abstract class JComponent extends Container implements Serializable
 
     this.verifyInputWhenFocusTarget = verifyInputWhenFocusTarget;
     firePropertyChange("verifyInputWhenFocusTarget",
-		       ! verifyInputWhenFocusTarget,
-		       verifyInputWhenFocusTarget);
+                       ! verifyInputWhenFocusTarget,
+                       verifyInputWhenFocusTarget);
   }
 }
