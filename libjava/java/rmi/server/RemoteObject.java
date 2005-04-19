69ae1f74fe940ceff1fcafb00b1f3b477a34f60d
Michael Koch <konqueror@gmx.de>
2005-04-19 10:34:38 +0000

    RemoteObject.java: Removed unused import statement.
    2005-04-19  Michael Koch  <konqueror@gmx.de>
    
    	* java/rmi/server/RemoteObject.java:
    	Removed unused import statement.
    
    From-SVN: r98400

--- libjava/java/rmi/server/RemoteObject.java
+++ libjava/java/rmi/server/RemoteObject.java
@@ -7,7 +7,7 @@ GNU Classpath is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2, or (at your option)
 any later version.
- 
+
 GNU Classpath is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -41,7 +41,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.lang.reflect.Constructor;
 import java.rmi.NoSuchObjectException;
 import java.rmi.Remote;
 import java.rmi.UnmarshalException;
