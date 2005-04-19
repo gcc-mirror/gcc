009e94d4e654ec6b009ef69d27818bdcf2915246
Francois-Xavier Coudert <coudert@clipper.ens.fr>
2005-04-19 09:10:05 +0200

    re PR fortran/16861 ([4.0 only] segfault with doubly used module)
    	PR fortran/16861
    	* resolve.c (resolve_variable): If e->symtree is not set, this
    	ought to be a FAILURE, and not a segfault.
    	* gfortran.dg/pr16861.f90: New test.
    
    From-SVN: r98391

---
+++ gcc/testsuite/gfortran.dg/pr16861.f90
@@ -0,0 +1,32 @@
+! PR fortran/16861
+! { dg-do run }
+module foo
+  integer :: i
+end module foo
+
+module bar
+contains
+  subroutine baz(j)
+    use foo
+    integer, dimension(i) :: j
+    integer :: n
+
+    do n = 1, i
+      if (j(n) /= n**2) call abort
+    end do
+  end subroutine baz
+end module bar
+
+subroutine quus()
+  use foo
+  use bar
+
+  i = 2
+  call baz ((/1,4/))
+  i = 7
+  call baz ((/1,4,9,16,25,36,49/))
+end subroutine quus
+
+program test
+  call quus
+end program test
