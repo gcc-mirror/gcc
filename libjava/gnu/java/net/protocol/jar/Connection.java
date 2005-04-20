9338a13356500af80d0ceb859e8a354e61e754e0
mkoch <mkoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-20 09:25:25 +0000

    2005-04-20  Michael Koch  <konqueror@gmx.de>
    	* gnu/java/net/protocol/file/Connection.java,
    	gnu/java/net/protocol/jar/Connection.java:
    	Reorganized import statements. Fixed @author tags.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@98450 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/gnu/java/net/protocol/jar/Connection.java
+++ libjava/gnu/java/net/protocol/jar/Connection.java
@@ -42,8 +42,8 @@ import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.JarURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -64,7 +64,7 @@ import java.util.zip.ZipFile;
  * This subclass of java.net.JarURLConnection models a URLConnection via
  * the "jar" protocol.
  *
- * @author Kresten Krab Thorup <krab@gnu.org>
+ * @author Kresten Krab Thorup (krab@gnu.org)
  */
 public final class Connection extends JarURLConnection
 {
