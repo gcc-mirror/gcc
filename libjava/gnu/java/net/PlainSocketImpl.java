48ad7977452a698de4807c65502abe69d8f1da90
mkoch <mkoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-20 05:58:46 +0000

    2005-04-20  Michael Koch  <konqueror@gmx.de>
    	* gnu/java/net/PlainDatagramSocketImpl.java:
    	Merged copyright header.
    	* gnu/java/net/PlainSocketImpl.java:
    	Reorganized import statements. Fixed @author tags.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@98443 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/gnu/java/net/PlainSocketImpl.java
+++ libjava/gnu/java/net/PlainSocketImpl.java
@@ -39,8 +39,10 @@ exception statement from your version. */
 
 package gnu.java.net;
 
-import java.io.InputStream;
+import gnu.classpath.Configuration;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -48,7 +50,6 @@ import java.net.SocketAddress;
 import java.net.SocketException;
 import java.net.SocketImpl;
 import java.net.SocketOptions;
-import gnu.classpath.Configuration;
 
 /**
  * Written using on-line Java Platform 1.2 API Specification, as well
@@ -62,9 +63,9 @@ import gnu.classpath.Configuration;
  * combination of Java and native routines to implement standard BSD
  * style sockets of family AF_INET and types SOCK_STREAM and SOCK_DGRAM
  *
- * @author Per Bothner <bothner@cygnus.com>
- * @author Nic Ferrier <nferrier@tapsellferrier.co.uk>
- * @author Aaron M. Renn <arenn@urbanophile.com>
+ * @author Per Bothner (bothner@cygnus.com)
+ * @author Nic Ferrier (nferrier@tapsellferrier.co.uk)
+ * @author Aaron M. Renn (arenn@urbanophile.com)
  */
 public final class PlainSocketImpl extends SocketImpl
 {
