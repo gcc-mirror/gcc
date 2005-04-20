53a2494eea417253a25732b427963ac080923edd
Joseph Myers <jsm28@gcc.gnu.org>
2005-04-20 02:10:00 +0100

    c.opt (Wint-to-pointer-cast, [...]): New options.
    2005-04-20  Michael Pogue  <michael.pogue@sun.com>
                Joseph S. Myers  <joseph@codesourcery.com>
    
    	* c.opt (Wint-to-pointer-cast, Wpointer-to-int-cast): New options.
    	* c-typeck.c (build_c_cast): Check these options.
    	* doc/invoke.texi: Document these options.
    
    testsuite:
    	* gcc.dg/Wint-to-pointer-cast-1.c,
    	gcc.dg/Wint-to-pointer-cast-2.c, gcc.dg/Wint-to-pointer-cast-3.c,
    	gcc.dg/Wpointer-to-int-cast-1.c, gcc.dg/Wpointer-to-int-cast-2.c,
    	gcc.dg/Wpointer-to-int-cast-3.c: New tests.
    
    From-SVN: r98429

---
+++ gcc/testsuite/gcc.dg/Wint-to-pointer-cast-3.c
@@ -0,0 +1,20 @@
+/* Test -Wno-int-to-pointer-cast.  */
+/* Origin: Joseph Myers <joseph@codesourcery.com> */
+/* { dg-do compile } */
+/* { dg-options "-Wno-int-to-pointer-cast" } */
+
+char c;
+
+void *
+f (void)
+{
+  return (void *) c;
+}
+
+void *p;
+
+char
+g (void)
+{
+  return (char) p; /* { dg-warning "warning: cast from pointer to integer of different size" } */
+}
