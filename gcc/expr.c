f0cf03cb877c7e3dfe2903f3e4720b4d44d1dc34
fjahanian <fjahanian@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-21 15:36:01 +0000

    Fix for latent bug in splitting arguments exposed by rs6000_arg_partial_bytes rewrite. Oked by Richard Henderson.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@98507 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/expr.c
+++ gcc/expr.c
@@ -3544,7 +3544,7 @@ emit_push_insn (rtx x, enum machine_mode mode, tree type, rtx size,
       int not_stack;
       /* # bytes of start of argument
 	 that we must make space for but need not store.  */
-      int offset = partial % (PARM_BOUNDARY / BITS_PER_WORD);
+      int offset = partial % (PARM_BOUNDARY / BITS_PER_UNIT);
       int args_offset = INTVAL (args_so_far);
       int skip;
 
@@ -3562,8 +3562,9 @@ emit_push_insn (rtx x, enum machine_mode mode, tree type, rtx size,
 	offset = 0;
 
       /* Now NOT_STACK gets the number of words that we don't need to
-	 allocate on the stack.  */
+	 allocate on the stack. Convert OFFSET to words too. */
       not_stack = (partial - offset) / UNITS_PER_WORD;
+      offset /= UNITS_PER_WORD;
 
       /* If the partial register-part of the arg counts in its stack size,
 	 skip the part of stack space corresponding to the registers.
