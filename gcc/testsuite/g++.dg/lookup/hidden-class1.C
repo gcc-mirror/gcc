d89dab3130e93bf415da3719355e24ef4cd039ca
No Author <no-author@gcc.gnu.org>
2005-04-21 05:20:58 +0000

    This commit was manufactured by cvs2svn to create tag 'gcc_4_0_0_release'.
    From-SVN: r98492

--- gcc/testsuite/g++.dg/lookup/hidden-class1.C
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2005 Free Software Foundation
-// Contributed by Kriang Lerdsuwanakij <lerdsuwa@users.sourceforge.net>
-// { dg-do compile }
-
-class A {
-  friend class B;
-  B *b;		// { dg-error "no type|expected" }
-};
--- gcc/testsuite/g++.dg/lookup/hidden-class2.C
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2005 Free Software Foundation
-// Contributed by Kriang Lerdsuwanakij <lerdsuwa@users.sourceforge.net>
-// { dg-do compile }
-
-class A {
-  friend class B;
-};
-
-class B* b;
