5e49f16b8090faaf53469c51bdadb8ca5c0e115e
Joseph Myers <joseph@codesourcery.com>
2005-04-27 00:58:04 +0100

    re PR c/20740 (cc1 segfaults)
    	PR c/20740
    	* c-format.c (init_dynamic_asm_fprintf_info): Give errors, not
    	assertion failures, if __gcc_host_wide_int__ is not properly
    	defined.
    	(init_dynamic_diag_info): Give errors, not assertion failures, if
    	location_t, tree or __gcc_host_wide_int__ are not properly
    	defined.
    
    testsuite:
    	* gcc.dg/format/asm_fprintf-2.c, gcc.dg/format/asm_fprintf-3.c,
    	gcc.dg/format/asm_fprintf-4.c, gcc.dg/format/asm_fprintf-5.c,
    	gcc.dg/format/gcc_diag-2.c, gcc.dg/format/gcc_diag-3.c,
    	gcc.dg/format/gcc_diag-4.c, gcc.dg/format/gcc_diag-5.c,
    	gcc.dg/format/gcc_diag-6.c, gcc.dg/format/gcc_diag-7.c ,
    	gcc.dg/format/gcc_diag-8.c, gcc.dg/format/gcc_diag-9.c: New tests.
    
    From-SVN: r98800

---
+++ gcc/testsuite/gcc.dg/format/gcc_diag-8.c
@@ -0,0 +1,10 @@
+/* Test for ICE handling internal formats: bug 20740.  The code did
+   not check that, if the required typedef names had been used as
+   identifiers, they were defined to suitable types.  Test
+   "__gcc_host_wide_int__", not a type.  */
+/* Origin: Joseph Myers <joseph@codesourcery.com> */
+/* { dg-do compile } */
+/* { dg-options "-Wformat" } */
+
+int __gcc_host_wide_int__;
+void bar (const char *, ...) __attribute__ ((__format__ (__gcc_diag__, 1, 2))); /* { dg-error "error: '__gcc_host_wide_int__' is not defined as a type" } */
