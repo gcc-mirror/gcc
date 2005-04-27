281baea1860d7905220684c93ab27bf875de07af
rakdver <rakdver@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-27 14:28:11 +0000

    PR tree-optimization/21171 	* tree-ssa-loop-ivopts.c (find_interesting_uses_address): Do not 	record address uses if the reference is volatile.
    	* gcc.dg/tree-ssa/pr21171.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@98828 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr21171.c
@@ -0,0 +1,29 @@
+/* PR 21171.  Ivopts should not rewrite references to volatile memory.  */
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-vars" } */
+
+typedef	unsigned int u_int32_t;
+typedef	unsigned char u_int8_t;
+
+#define	AIC_VECTORS	32
+
+typedef volatile struct AT91RM9200_regs {
+  u_int32_t	SVR[AIC_VECTORS];
+} AT91RM9200_regs_t;
+
+#define CPUReg  ((AT91RM9200_regs_t*)0xFFF00000)
+
+extern const u_int32_t __IntTable[AIC_VECTORS];
+
+int main()
+{
+  int c;
+
+  for (c = 0; c < AIC_VECTORS; c++)
+    CPUReg->SVR[c] = __IntTable[c];
+
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "SVR" 1 "vars"} } */
+/* { dg-final { cleanup-tree-dump "vars" } } */
