5cb9d0d850f8e082972dbacacf5b92740e5915b2
pbrook <pbrook@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-29 02:34:11 +0000

    2005-04-29  Paul Brook   <paul@codesourcery.com>
    	* trans-expr.c (gfc_conv_expr_present): Fix broken assert.  Update
    	comment.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@98968 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/entry_3.f90
@@ -0,0 +1,25 @@
+! { dg-do run }
+! Test assumed shape arrays in procedures with multiple entry points.
+! Arguments that aren't present in all entry points must be treated like
+! optional arguments.
+module entry_4
+contains
+subroutine foo(a)
+  integer, dimension(:) :: a
+  integer, dimension(:) :: b
+  a = (/1, 2/)
+  return
+entry bar(b)
+  b = (/3, 4/)
+end subroutine
+end module
+
+program entry_4_prog
+  use entry_4
+  integer :: a(2)
+  a = 0
+  call foo(a)
+  if (any (a .ne. (/1, 2/))) call abort
+  call bar(a)
+  if (any (a .ne. (/3, 4/))) call abort
+end program
