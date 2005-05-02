fda05890956b883a25ea35edfa65db76a50c3da3
Kazu Hirata <kazu@cs.umass.edu>
2005-05-02 18:06:27 +0000

    re PR tree-optimization/21294 (Missed removal of null pointer check)
    gcc/
    	PR tree-optimization/21294
    	* tree-vrp.c (vrp_expr_computes_nonzero): New.
    	(extract_range_from_expr): Call vrp_expr_computes_nonzero.
    
    testsuite/
    	PR tree-optimization/21294
    	* gcc.dg/tree-ssa/pr21294.c: New.
    
    From-SVN: r99111

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr21294.c
@@ -0,0 +1,23 @@
+/* PR tree-optimization/21294
+   VRP did not notice that an address of the form &p->i is nonnull
+   when p is known to be nonnull.  In this testcase, noticing that
+   allows us to eliminate the second "if" statement.  */
+
+/* { dg-do compile } */
+/* { dg-options "-O2 -fno-tree-dominator-opts -fdump-tree-vrp-details" } */
+
+struct f {
+  int i;
+};
+
+int
+foo (struct f *p)
+{
+  if (p != 0)
+    if (&p->i != 0)
+      return 123;
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "Folding predicate" 1 "vrp"} } */
+/* { dg-final { cleanup-tree-dump "vrp" } } */
