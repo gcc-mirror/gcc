d3fe97365282576442fd0dc75022fe714988d61d
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-03 18:47:49 +0000

    * constants.c, jvgenmain.c, lang.opt, resource.c: Update 	copyright.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@99173 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/java/constants.c
+++ gcc/java/constants.c
@@ -1,5 +1,5 @@
 /* Handle the constant pool of the Java(TM) Virtual Machine.
-   Copyright (C) 1997, 1998, 1999, 2000, 2001, 2003, 2004
+   Copyright (C) 1997, 1998, 1999, 2000, 2001, 2003, 2004, 2005
    Free Software Foundation, Inc.
 
 This file is part of GCC.
--- gcc/java/resource.c
+++ gcc/java/resource.c
@@ -1,5 +1,5 @@
 /* Functions related to building resource files.
-   Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004
+   Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005
    Free Software Foundation, Inc.
 
 This file is part of GCC.
