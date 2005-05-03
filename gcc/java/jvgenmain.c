f384d65856a244b10696cc7b422a60156717cc9a
Kazu Hirata <kazu@cs.umass.edu>
2005-05-03 18:47:49 +0000

    constants.c, [...]: Update copyright.
    	* constants.c, jvgenmain.c, lang.opt, resource.c: Update
    	copyright.
    
    From-SVN: r99173

--- gcc/java/jvgenmain.c
+++ gcc/java/jvgenmain.c
@@ -1,5 +1,5 @@
 /* Program to generate "main" a Java(TM) class containing a main method.
-   Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004
+   Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005
    Free Software Foundation, Inc.
 
 This file is part of GCC.
