cad6c4371f26eb0e9d5ff5c6237752e4f556b23c
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-03 18:40:36 +0000

    * Make-lang.in, dump-parse-tree.c, invoke.texi, lang.opt, 	match.h, trans-array.h: Update copyright.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@99172 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/match.h
+++ gcc/fortran/match.h
@@ -1,5 +1,5 @@
 /* All matcher functions.
-   Copyright (C) 2003 Free Software Foundation, Inc.
+   Copyright (C) 2003, 2005 Free Software Foundation, Inc.
    Contributed by Steven Bosscher
 
 This file is part of GCC.
