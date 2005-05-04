66787d4fb278e88a3ed279cc4f6fe95751f4286b
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-04 01:52:06 +0000

    2004-05-03  Andrew Pinski  <pinskia@physics.uc.edu>
            PR middle-end/15618
            * fold-const.c (fold_widened_comparison): Treat BOOLEAN_TYPE
            the same as INTEGER_TYPE.
            (fold_binary): Fold "bool_var != 0" to bool_var.
            Fold "bool_var == 1" to bool_var.
    2005-05-03  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR middle-end/15618
            * gcc.dg/tree-ssa/bool-[1-9].c: New tests.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@99207 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/bool-3.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-optimized" } */
+
+int f(_Bool x)
+{
+  int y;
+  if (!x)
+    y = 0;
+  else
+    y = 1;
+  return y;
+}
+
+/* There should be no == 0. Though PHI-OPT or invert_truth does not
+   fold its tree.  */
+/* { dg-final { scan-tree-dump-times "== 0" 0 "optimized" { xfail *-*-* } } } */
+
+/* { dg-final { cleanup-tree-dump "optimized" } } */
