ed4c3ffea0a6298f863b8b3f8e165d178560e8c1
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-08 12:06:55 +0000

    Use dg-mips-options instead of dg-options.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@99391 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/mips/fpcmp-2.c
+++ gcc/testsuite/gcc.target/mips/fpcmp-2.c
@@ -1,5 +1,5 @@
 /* We used to use c.le.fmt instead of c.ult.fmt here.  */
-/* { dg-options "-mhard-float -O2" } */
+/* { dg-mips-options "-mhard-float -O2" } */
 int f1 (float x, float y) { return __builtin_islessequal (x, y); }
 int f2 (double x, double y) { return __builtin_islessequal (x, y); }
 /* { dg-final { scan-assembler "c\\.ult\\.s" } } */
