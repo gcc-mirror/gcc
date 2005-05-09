4581ba9d4f3767e4b0dea0bc101b0e865cfcd85d
Francois-Xavier Coudert <coudert@clipper.ens.fr>
2005-05-09 13:21:01 +0200

    re PR libfortran/19155 ([4.0 only] blanks not treated as zeros in 'E' format read (NIST FM110.FOR))
    	PR libfortran/19155
    	* io/read.c (read_f): Accept 'e', 'E', 'd' and 'D' as first
    	non-blank characters of a real number.
    	* gfortran.dg/pr19155.f: New test.
    
    From-SVN: r99424

---
+++ gcc/testsuite/gfortran.dg/pr19155.f
@@ -0,0 +1,15 @@
+! { dg-do run }
+!
+! PR libfortran/19155
+! We accept 'E+00' as a valid real number. The standard says it is not,
+! but doesn't require us to issue an error. Since g77 accepts this as zero,
+! we do the same.
+      real a
+      a = 42
+      open (19,status='scratch')
+      write (19,'(A15)') 'E+00'
+      rewind (19)
+      read (19,'(E15.8)') a
+      if (a .ne. 0) call abort
+      close (19)
+      end
