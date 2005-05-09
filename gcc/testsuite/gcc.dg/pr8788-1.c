3bcb100d435198326198cea3af9341f44e3ceaee
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-09 05:15:50 +0000

    PR inline-asm/8788 	* stmt.c (expand_asm_operands): Avoid calling force_reg on BLKmode 	operands.
    	* gcc.dg/pr8788-1.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@99422 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr8788-1.c
@@ -0,0 +1,20 @@
+/* PR inline-asm/8788 */
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+
+typedef struct {
+    long x[6];
+} myjmp_buf;
+
+typedef struct {
+    myjmp_buf regs;
+} my_stack;
+
+void switch_to_stack (my_stack *stack){
+    asm (  /* { dg-error "impossible constraint" } */
+/* { dg-warning "asm operand 1" "asm operand 1" { target *-*-* } 14 } */
+        "\n"
+        : "+r" (stack->regs)
+    );
+}
+
