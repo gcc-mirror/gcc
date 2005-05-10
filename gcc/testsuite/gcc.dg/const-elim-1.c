64552cd79032ccee03175488798bd12e22062e6a
Bob Wilson <bob.wilson@acm.org>
2005-05-10 16:27:48 +0000

    * gcc.dg/const-elim-1.c: Remove xfail for xtensa-*-*.
    From-SVN: r99526

--- gcc/testsuite/gcc.dg/const-elim-1.c
+++ gcc/testsuite/gcc.dg/const-elim-1.c
@@ -12,8 +12,8 @@
    to eliminate the block move, so the constant gets emitted.
    Currently known targets with this problem: all ARM; PA32 ("hppa*.*"
    matches "hppa2.0w" but not "hppa64"); PPC if string instructions
-   are enabled (notably under AIX); Xtensa.  */
-/* { dg-final { scan-assembler-not "L\\\$?C\[^A-Z\]" { xfail arm-*-* strongarm-*-* xscale-*-* hppa-*-* hppa*.*-*-* powerpc*-*-aix* xtensa-*-* } } } */
+   are enabled (notably under AIX).  */
+/* { dg-final { scan-assembler-not "L\\\$?C\[^A-Z\]" { xfail arm-*-* strongarm-*-* xscale-*-* hppa-*-* hppa*.*-*-* powerpc*-*-aix* } } } */
 
 #define I (__extension__ 1.0iF)
 
