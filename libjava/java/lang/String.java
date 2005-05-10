0f78940a0f759f8b50fc3f549405ff2eafacf77f
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-10 18:28:31 +0000

    * gcj/javaprims.h: Updated. 	* java/lang/String.java (String(StringBuilder)): New constructor. 	* java/lang/natStringBuilder.cc: New file. 	* java/lang/StringBuilder.java: New file. 	* Makefile.in: Rebuilt. 	* Makefile.am (core_java_source_files): Added StringBuilder.java. 	(nat_source_files): Added natStringBuilder.cc.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@99535 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/lang/String.java
+++ libjava/java/lang/String.java
@@ -1,5 +1,5 @@
 /* String.java -- immutable character sequences; the object of string literals
-   Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004
+   Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005
    Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
@@ -395,6 +395,18 @@ public final class String implements Serializable, Comparable, CharSequence
 	  buffer.shared = true;
 	init (buffer.value, 0, buffer.count, ! should_copy);
       }
+  }
+
+  /**
+   * Creates a new String using the character sequence represented by
+   * the StringBuilder. Subsequent changes to buf do not affect the String.
+   *
+   * @param buffer StringBuilder to copy
+   * @throws NullPointerException if buffer is null
+   */
+  public String(StringBuilder buffer)
+  {
+    this(buffer.value, 0, buffer.count);
   }
 
   /**
