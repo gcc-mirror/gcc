ee58dffdbc2874e053c60629b2f1c04384e2248d
Nick Clifton <nickc@redhat.com>
2005-05-10 15:33:18 +0000

    Update the address and phone number of the FSF organization.
    From-SVN: r99519

--- libiberty/xexit.c
+++ libiberty/xexit.c
@@ -14,8 +14,8 @@ Library General Public License for more details.
 
 You should have received a copy of the GNU Library General Public
 License along with libiberty; see the file COPYING.LIB.  If not, write
-to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.  */
+to the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+Boston, MA 02110-1301, USA.  */
 
 /*
 
