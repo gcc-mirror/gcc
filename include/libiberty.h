dbaef7e22b947b3b8aaa226d6dbc4bb05a6c40de
Steve Ellcey <sje@cup.hp.com>
2005-05-12 19:29:53 +0000

    libiberty.h: Do not define empty basename prototype.
    From-SVN: r99626

--- include/libiberty.h
+++ include/libiberty.h
@@ -97,7 +97,10 @@ extern char **dupargv (char **) ATTRIBUTE_MALLOC;
 #if defined (__GNU_LIBRARY__ ) || defined (__linux__) || defined (__FreeBSD__) || defined (__OpenBSD__) || defined(__NetBSD__) || defined (__CYGWIN__) || defined (__CYGWIN32__) || defined (__MINGW32__) || defined (HAVE_DECL_BASENAME)
 extern char *basename (const char *);
 #else
-extern char *basename ();
+/* Do not allow basename to be used if there is no prototype seen.  We
+   either need to use the above prototype or have one from
+   autoconf which would result in HAVE_DECL_BASENAME being set.  */
+#define basename basename_cannot_be_used_without_a_prototype
 #endif
 #endif
 
