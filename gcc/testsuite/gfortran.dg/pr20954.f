bcc112e76b57877beecac4491b325f034404ed9c
No Author <no-author@gcc.gnu.org>
2005-05-18 10:00:56 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r99894

---
+++ gcc/testsuite/gfortran.dg/pr20954.f
@@ -0,0 +1,11 @@
+      ! { dg-do run }
+      ! { dg-options "-fdefault-integer-8" }
+      ! Program to test character length type
+      Program pr20954
+      character*16 string (5)
+      character*5 filename
+      character*80 line
+      filename = 'input'
+      open (2,file=filename)
+      write (line, '(5a16)') (string(i),i=1,5)
+      end
