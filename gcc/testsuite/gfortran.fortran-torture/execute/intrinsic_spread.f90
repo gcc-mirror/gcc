1bc528755f0d4df30b74c913adb8196b82f8afcb
Thomas Koenig <Thomas.Koenig@online.de>
2005-05-24 22:22:10 +0000

    backport: re PR libfortran/18495 ([4.0 only] Intrinisc function SPREAD is broken)
    2005-05-25  Thomas Koenig  <Thomas.Koenig@online.de>
    
    	Backport from mainline:
    	PR libfortran/18495
    	* intrinsics/spread_generic.c (spread):  Remove const from
    	return array descriptor.
    	New variables: rrank (rank of return array),  rs (for
    	calculating the size of the return array), srank (rank
    	of the source array).
    	Generate runtime error if the dim= argument is larger than
    	the rank of the return array.
    	Generate runtime error if the needed rank of the return
    	array is larger than 7.
    	If ret->data is null, populate the return array descriptor
    	and initialize the variables for the actual operation.
    	Otherwise, set ret->dim[0].stride to one if it is zero.
    	Change second, independent use of variable dim to srank.
    
    2005-05-25  Thomas Koenig  <Thomas.Koenig@online.de>
    
    	Backport from mainline:
    	PR libfortran/18495
    	* gfortran.fortran-torture/execute/intrinsic_spread.f90:
    	Test callee-allocated version of return array with a write
    	statement.
    	Test spread with a temporary with another write statement.
    
    From-SVN: r100124

--- gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_spread.f90
+++ gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_spread.f90
@@ -1,10 +1,17 @@
 program foo
    integer, dimension (2, 3) :: a
    integer, dimension (2, 2, 3) :: b
+   character (len=80) line1, line2, line3
 
    a = reshape ((/1, 2, 3, 4, 5, 6/), (/2, 3/))
    b = spread (a, 1, 2)
    if (any (b .ne. reshape ((/1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6/), &
                             (/2, 2, 3/)))) &
       call abort
+   write(line1, 9000) b
+   write(line2, 9000) spread (a, 1, 2)
+   if (line1 /= line2) call abort
+   write(line3, 9000) spread (a, 1, 2) + 0
+   if (line1 /= line3) call abort
+9000 format(12I3)
 end program
