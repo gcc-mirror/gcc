909d1a8e03f07c741c8ed964694e2fe4b45e4e88
Eric Botcazou <ebotcazou@libertysurf.fr>
2005-05-25 09:14:22 +0200

    re PR target/21412 (ICE loading TLS address)
    	PR target/21412
    	* gcc.dg/tls/opt-9.c: XFAIL on SPARC.
    
    From-SVN: r100138

--- gcc/testsuite/gcc.dg/tls/opt-9.c
+++ gcc/testsuite/gcc.dg/tls/opt-9.c
@@ -1,6 +1,7 @@
 /* PR 21412 */
 /* { dg-do compile */
 /* { dg-options "-O2 -fPIC" } */
+/* { dg-error "" "unrecognizable insn" { target sparc*-*-* } 0 } */
 
 struct S { int x[10]; };
 extern __thread struct S s;
