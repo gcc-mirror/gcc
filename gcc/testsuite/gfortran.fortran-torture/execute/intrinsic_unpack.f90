e6165b150c6c4deb1856432b431599701e80fa2a
Thomas Koenig <Thomas.Koenig@online.de>
2005-05-26 06:40:42 +0000

    re PR fortran/17283 (UNPACK issues)
    2005-05-26  Thomas Koenig  <Thomas.Koenig@online.de>
    
            PR libfortran/17283
            * gfortran.fortran-torture/execute/intrinsic_unpack.f90:
            Test callee-allocated memory with write statements.
    
    2005-05-26  Thomas Koenig  <Thomas.Koenig@online.de>
    
            PR libfortran/17283
            * intrinsics/unpack_generic.c:  Fix name of routine
            on top.  Update copyright years.
            (unpack1):  Remove const from return array descriptor.
            rs:  New variable, for calculating return sizes.
            Populate return array descriptor if ret->data is NULL.
    
    From-SVN: r100191

--- gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_unpack.f90
+++ gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_unpack.f90
@@ -2,6 +2,7 @@
 program intrinsic_unpack
    integer, dimension(3, 3) :: a, b
    logical, dimension(3, 3) :: mask;
+   character(len=50) line1, line2
    integer i
 
    mask = reshape ((/.false.,.true.,.false.,.true.,.false.,.false.,&
@@ -10,6 +11,9 @@ program intrinsic_unpack
    b = unpack ((/2, 3, 4/), mask, a)
    if (any (b .ne. reshape ((/1, 2, 0, 3, 1, 0, 0, 0, 4/), (/3, 3/)))) &
       call abort
+   write (line1,'(10I4)') b
+   write (line2,'(10I4)') unpack((/2, 3, 4/), mask, a)
+   if (line1 .ne. line2) call abort
    b = -1
    b = unpack ((/2, 3, 4/), mask, 0)
    if (any (b .ne. reshape ((/0, 2, 0, 3, 0, 0, 0, 0, 4/), (/3, 3/)))) &
