b412edf2966303855dc62d331f5322886fbb36f2
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-26 07:10:21 +0000

    2005-05-25  Benjamin Kosnik  <bkoz@redhat.com>
    	* config/linker-map.gnu: Add linkage support for no extern templates.
    	(std::ios_base::_M_call_callbacks): Add.
    	(std::ios_base::_M_dispose_callbacks): Add.
    	(std::locale::facet::_S_get_c_name): Add.
    	(std::__copy_streambufs): Add.
    	* configure.ac (libtool_VERSION): To 6:5:0.
    	* configure: Regenerate.
    	* testsuite/testsuite_abi.cc (check_version): Add GLIBCXX_3.4.5.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@100193 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/testsuite_abi.cc
+++ libstdc++-v3/testsuite/testsuite_abi.cc
@@ -166,6 +166,7 @@ check_version(const symbol& test, bool added)
       known_versions.push_back("GLIBCXX_3.4.2");
       known_versions.push_back("GLIBCXX_3.4.3");
       known_versions.push_back("GLIBCXX_3.4.4");
+      known_versions.push_back("GLIBCXX_3.4.5");
       known_versions.push_back("CXXABI_1.2");
       known_versions.push_back("CXXABI_1.2.1");
       known_versions.push_back("CXXABI_1.3");
