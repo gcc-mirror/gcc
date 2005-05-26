d915d6377633ef9a2733dd4bcd559b34f6c54dbe
zlaski <zlaski@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-26 00:51:53 +0000

    [gcc/testsuite/ChangeLog] 2005-05-25  Ziemowit Laski  <zlaski@apple.com>
            * obj-c++.dg/selector-1.mm: New.
            * obj-c++.dg/selector-2.mm: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@100179 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/obj-c++.dg/selector-2.mm
@@ -0,0 +1,17 @@
+/* Test that we don't ICE when issuing a -Wselector warning.  */
+/* { dg-options "-Wselector" } */
+/* { dg-do compile } */
+
+#include <objc/Object.h>
+
+@interface Foo
+@end
+@implementation Foo
+-(void) foo
+{
+  SEL a;
+  a = @selector(b1ar);
+}
+@end
+/* { dg-warning "creating selector for nonexistent method .b1ar." "" { target *-*-* } 0 } */
+
