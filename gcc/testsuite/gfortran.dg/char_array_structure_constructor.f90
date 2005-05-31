fba53b18ab956e028e1a710f0029404408888c43
Paul Thomas <pault@gcc.gnu.org>
2005-05-31 17:39:03 +0000

    char_initialiser_actual.f90: Test character initialisers as actual arguments.
    2005-05-30  Paul Thomas  <pault@gcc.gnu.org>
    
    	* gfortran.dg/char_initialiser_actual.f90:
    	Test character initialisers as actual arguments.
    	* gfortran.dg/char_pointer_comp_assign.f90:
    	Test character pointer structure component assignments.
    	* gfortran.dg/char_array_structure_constructor.f90:
    	Test character components in structure constructors.
    
    From-SVN: r100401

---
+++ gcc/testsuite/gfortran.dg/char_array_structure_constructor.f90
@@ -0,0 +1,28 @@
+! { dg-do run }
+! This test the fix of PR19107, where character array actual
+! arguments in derived type constructors caused an ICE.
+! It also checks that the scalar counterparts are OK.
+! Contributed by Paul Thomas  pault@gcc.gnu.org
+!
+MODULE global
+  TYPE :: dt
+    CHARACTER(4) a
+    CHARACTER(4) b(2)
+  END TYPE
+  TYPE (dt), DIMENSION(:), ALLOCATABLE, SAVE :: c
+END MODULE global
+program char_array_structure_constructor
+  USE global
+  call alloc (2)
+  if ((any (c%a /= "wxyz")) .OR. &
+      (any (c%b(1) /= "abcd")) .OR. &
+      (any (c%b(2) /= "efgh"))) call abort ()
+contains
+  SUBROUTINE alloc (n)
+    USE global
+    ALLOCATE (c(n), STAT=IALLOC_FLAG)
+    DO i = 1,n
+      c (i) = dt ("wxyz",(/"abcd","efgh"/))
+    ENDDO
+  end subroutine alloc
+END program char_array_structure_constructor
