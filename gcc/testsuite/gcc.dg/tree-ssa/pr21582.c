909992de2897cb1574cac62f165513d8660bb4a2
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-02 18:26:07 +0000

    PR 21582 	* tree-vrp.c (nonnull_arg_p): New. 	(get_value_range): Call it.
    testsuite/ChangeLog
    
    	PR 21582
    	* gcc.dg/tree-ssa/pr21582.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@100505 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr21582.c
@@ -0,0 +1,28 @@
+/* { dg-do link }  */
+/* { dg-options "-O2 -fdump-tree-vrp" }  */
+
+static inline void do_thing(char *s, int *p, char *q)
+{
+  	/* This should be folded away.  */
+        if (s == 0 || q == 0)
+                link_error ();
+
+	/* This should not be folded as 'p' is not marked nonnull.  */
+	if (p)
+	  	*p = 3;
+}
+
+void __attribute__((nonnull (1, 3))) do_other_thing(char *s, int *p, char *q)
+{
+        do_thing(s, p, q);
+}
+
+int i;
+
+main()
+{
+  do_other_thing ("xxx", &i, "yyy");
+}
+
+/* { dg-final { scan-tree-dump-times "Folding predicate p_.*" 0 "vrp" } } */
+/* { dg-final { cleanup-tree-dump "vrp" } } */
