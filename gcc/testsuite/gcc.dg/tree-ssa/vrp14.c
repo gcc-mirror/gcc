80096ea0e8da0110d9d5af05e304963377b1c685
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-03 21:07:11 +0000

    * tree-ssa-dom.c (record_edge_info): Use last_basic_block to 	allocate info array. 	* tree-vrp.c (extract_range_from_unary_expr): Set resulting 	range to varying in cast expressions that change 	TYPE_PRECISION.
    testsuite/ChangeLog
    
    	* gcc.dg/tree-ssa/vrp14.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@100554 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp14.c
@@ -0,0 +1,46 @@
+/* { dg-do run } */
+/* { dg-options "-O2" } */
+
+struct tree_decl
+{
+  union tree_decl_u1 {
+    int f;
+    long i;
+    struct tree_decl_u1_a {
+      unsigned int align : 24;
+      unsigned int off_align : 8;
+    } a;
+  } u1;
+};
+
+extern void abort (void);
+
+unsigned int
+assemble_variable (struct tree_decl decl)
+{
+  unsigned int align;
+
+  align = decl.u1.a.align;
+
+  if (align > (1 << ((8 * 4) < 64 ? (8 * 4) - 2 : 62)))
+    align = (1 << ((8 * 4) < 64 ? (8 * 4) - 2 : 62));
+
+  /* VRP should not be propagating 0 to the RHS of this assignment.
+     But it was erroneously applying a cast operation between types of
+     different precision.  align is an unsigned int with range [0,
+     0x4000000] but the .align field holds only 24 bits.  So the cast
+     was returning a [0, 0] range.  */
+  decl.u1.a.align = align;
+
+  return decl.u1.a.align;
+}
+
+main ()
+{
+  struct tree_decl decl;
+  decl.u1.a.align = 13;
+  unsigned int x = assemble_variable (decl);
+  if (x == 0)
+    abort ();
+  return 0;
+}
