2261c5d9e6f4d7bbcbcceae33c61e06a91c0a48c
tobi <tobi@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-04 10:26:57 +0000

    fortran/ 2005-06-03  Tobias Schl"uter  <tobias.schlueter@physik.uni-muenchen.de> 	Erik Edelmann  <erik.edelmann@iki.fi>
    	* array.c (gfc_match_array_constructor): Disallow empty array
    	constructor.
    testsuite/
    2005-06-04  Erik Edelmann  <erik.edelmann@iki.fi>
    
    	* gfortran.dg/array_constructor_3.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@100579 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/array.c
+++ gcc/fortran/array.c
@@ -887,7 +887,10 @@ gfc_match_array_constructor (gfc_expr ** result)
   head = tail = NULL;
 
   if (gfc_match (end_delim) == MATCH_YES)
-    goto empty;			/* Special case */
+    {
+      gfc_error ("Empty array constructor at %C is not allowed");
+      goto cleanup;
+    }
 
   for (;;)
     {
@@ -911,7 +914,6 @@ gfc_match_array_constructor (gfc_expr ** result)
   if (gfc_match (end_delim) == MATCH_NO)
     goto syntax;
 
-empty:
   expr = gfc_get_expr ();
 
   expr->expr_type = EXPR_ARRAY;
---
+++ gcc/testsuite/gfortran.dg/array_constructor_3.f90
@@ -0,0 +1,5 @@
+! { dg-do compile }
+! Check that empty array constructors are rejected
+program hum
+    print *, (//)  { dg-error "Empty array constructor" }
+end program hum
