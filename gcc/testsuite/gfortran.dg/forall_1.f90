e046bcf91c0c8f8d18be5bd724dc648a4961805d
No Author <no-author@gcc.gnu.org>
2005-06-04 17:31:40 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r100593

---
+++ gcc/testsuite/gfortran.dg/forall_1.f90
@@ -0,0 +1,27 @@
+! { dg-do run }
+! tests FORALL statements with a mask
+dimension i2(15,10), i1(15)
+type a
+   sequence
+   integer k
+end type a
+type(a) :: a1(10), a2(5,5)
+
+forall (i=1:15, i1(i) /= 0)
+   i1(i) = 0
+end forall
+if (any(i1 /= 0)) call abort
+
+a1(:)%k = i1(1:10)
+forall (i=1:10, a1(i)%k == 0)
+   a1(i)%k = i
+end forall
+if (any (a1(:)%k /= (/ (i, i=1,10) /))) call abort
+
+forall (i=1:15, j=1:10, a1(j)%k <= j)
+   i2(i,j) = j + i*11
+end forall
+do i=1,15
+   if (any (i2(i,:) /= (/ (i*11 + j, j=1,10) /))) call abort
+end do
+end
