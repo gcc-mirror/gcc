28da59e8c4f9e8f45a8d28a98f966c98304c3520
Hans-Peter Nilsson <hp@axis.com>
2005-06-04 03:24:39 +0000

    * g++.dg/opt/crossjump1.C: Gate -fPIC usage on target fpic.
    From-SVN: r100574

--- gcc/testsuite/g++.dg/opt/crossjump1.C
+++ gcc/testsuite/g++.dg/opt/crossjump1.C
@@ -1,6 +1,7 @@
 // PR middle-end/21492
 // { dg-do compile }
-// { dg-options "-Os -fPIC" }
+// { dg-options "-Os" }
+// { dg-options "-Os -fPIC" { target fpic } }
 
 extern char *bar (const char *, const char *);
 extern char *baz (char *, const char *);
