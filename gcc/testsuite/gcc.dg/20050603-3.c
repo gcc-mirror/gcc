3120c026f255ccefb397aa85e8bb063bf3ef9c3b
dalej <dalej@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-05 01:22:31 +0000

    2005-06-04  Dale Johannesen  <dalej@apple.com>
            * gcc.c-torture/execute/20050603-1.c:  New.
            * gcc.c-torture/execute/20050603-2.c:  New.
            * gcc.dg/20050603-3.c:  New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@100611 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20050603-3.c
@@ -0,0 +1,15 @@
+/* { dg-do compile { target "powerpc-*-*" } } */
+/* { dg-options "-O2" } */
+struct Q 
+{
+  long x:20;
+  long y:4;
+  long z:8;
+}b;
+/* This should generate a single rl[w]imi. */
+void rotins (unsigned int x)
+{
+  b.y = (x<<12) | (x>>20);
+}
+
+/* { dg-final { scan-assembler-not "inm" } } */
