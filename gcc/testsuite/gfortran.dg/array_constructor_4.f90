86403f0f329ea996b6323d4011d5da0111ab80d8
Tobias Schlüter <tobias.schlueter@physik.uni-muenchen.de>
2005-06-05 20:03:47 +0200

    re PR fortran/21912 (Wrong implied do-loop)
    fortran/
    	PR fortran/21912
    	* trans-array.c (gfc_trans_array_constructor_value): Slightly reorder.
    	Generate correct exit condition in case of negative steps in
    	implied-do loops.
    testsuite/
    	PR fortran/21912
    	* gfortran.dg/array_constructor_4.f90: New test.
    
    From-SVN: r100630

---
+++ gcc/testsuite/gfortran.dg/array_constructor_4.f90
@@ -0,0 +1,23 @@
+! { dg-do run }
+! PR 21912
+! We didn't adapt the exit condition to negative steps in array constructors,
+! leaving the resulting arrays uninitialized.
+integer :: i(5), n, m, l, k
+
+n = 5
+i = (/ (m, m = n, 1, -1) /)
+if (any (i /= (/ 5, 4, 3, 2, 1 /))) call abort
+
+k = 1
+
+i(5:1:-1) = (/ (m, m = n, k, -1) /)
+if (any (i /= (/ 1, 2, 3, 4, 5 /))) call abort
+
+l = -1
+
+i = (/ (m, m = n, 1, l) /)
+if (any (i /= (/ 5, 4, 3, 2, 1 /))) call abort
+
+i(5:1:-1) = (/ (m, m = n, k, l) /)
+if (any (i /= (/ 1, 2, 3, 4, 5 /))) call abort
+end
