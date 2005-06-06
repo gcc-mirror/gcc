86ab20d90a1cb64be334c60b703013eaece15719
billingd <billingd@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-06 03:00:10 +0000

    2005-06-05  David Billinghurst  <David.Billinghurst@riotinto.com>
    	* gfortran.dg/f2c_5.f90: Add -w to dg-options
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@100646 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/f2c_5.f90
+++ gcc/testsuite/gfortran.dg/f2c_5.f90
@@ -1,6 +1,6 @@
 ! { dg-do run }
 ! { dg-additional-sources f2c_5.c }
-! { dg-options "-fno-f2c" }
+! { dg-options "-fno-f2c -w" }
 ! Check calling conventions without -ff2c
 program f2c_5  
   call f2c_5a()
