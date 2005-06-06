43be3be866fe6355fb0347568602cfbb355c9f4f
bernds <bernds@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-06 09:46:39 +0000

    Add bfin-uclinux configuration.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@100656 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/bfin/uclinux.h
@@ -0,0 +1,10 @@
+#undef  STARTFILE_SPEC
+#define STARTFILE_SPEC \
+  "crt1%O%s crti%O%s crtbegin%O%s"
+
+#undef  ENDFILE_SPEC
+#define ENDFILE_SPEC \
+  "crtend%O%s crtn%O%s"
+
+#undef  LIB_SPEC
+#define LIB_SPEC "%{pthread:-lpthread} -lc"
