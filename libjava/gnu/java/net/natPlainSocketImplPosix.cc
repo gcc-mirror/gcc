c8891bef494c6868b9a26a2ce8f553c3e04f9b00
Anthony Green <green@redhat.com>
2005-06-06 06:17:28 +0000

    natPlainSocketImplPosix.cc (setOption): Fix case fall-through bug.
    2005-06-05  Anthony Green  <green@redhat.com>
    
            * gnu/java/net/natPlainSocketImplPosix.cc (setOption): Fix case
            fall-through bug.
    
    From-SVN: r100650

--- libjava/gnu/java/net/natPlainSocketImplPosix.cc
+++ libjava/gnu/java/net/natPlainSocketImplPosix.cc
@@ -646,6 +646,7 @@ gnu::java::net::PlainSocketImpl::setOption (jint optID, ::java::lang::Object *va
 	if (::setsockopt (native_fd, SOL_SOCKET, SO_REUSEADDR, (char *) &val,
 	    val_len) != 0)
 	  goto error;
+	return;
 #else
         throw new ::java::lang::InternalError (
           JvNewStringUTF ("SO_REUSEADDR not supported"));
