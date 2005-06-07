efb169b09d08c84445a5705d7895a32180445bc8
Nathan Sidwell <nathan@codesourcery.com>
2005-06-07 08:45:32 +0000

    parser.c (cp_parser_late_parsing_default_args): Fix overeager assert in previous patch.
    cp:
    	* parser.c (cp_parser_late_parsing_default_args): Fix overeager
    	assert in previous patch.
    testsuite:
    	* g++.dg/parse/defarg10.C: New.
    
    From-SVN: r100702

---
+++ gcc/testsuite/g++.dg/parse/defarg10.C
@@ -0,0 +1,11 @@
+// Copyright (C) 2005 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 7 Jun 2005 <nathan@codesourcery.com>
+
+// From java library.
+
+void  Foo (int = 0);
+
+class Class
+{
+  friend void Foo (int);
+};
