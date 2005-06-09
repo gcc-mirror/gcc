5878b92f9edefa7c60d5508fbba8441dc1358df9
Nathan Sidwell <nathan@codesourcery.com>
2005-06-09 11:36:12 +0000

    c-typeck.c (build_c_cast): Check type punning on COMPONENT_REF too.
    /:
    	* c-typeck.c (build_c_cast): Check type punning on COMPONENT_REF
    	too.
    testsuite:
    	* gcc.dg/alias-8.c: New.
    
    From-SVN: r100799

---
+++ gcc/testsuite/gcc.dg/alias-8.c
@@ -0,0 +1,12 @@
+// { dg-do compile }
+// { dg-options "-Wstrict-aliasing=2 -fstrict-aliasing" }
+
+struct s {
+  char    *p;
+};
+
+void
+func(struct s *ptr)
+{
+  *(void **)&ptr->p = 0; /* { dg-warning "type-punned pointer" } */
+}
