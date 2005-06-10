f8103b9fe2ecc084b253fd0780ff5344703dfb7a
Aldy Hernandez <aldyh@gcc.gnu.org>
2005-06-10 17:04:09 +0000

    re PR middle-end/17961 (ICE for operation on small vector with altivec enabled)
    	PR 17961
    	* config/rs6000/sysv4.h (ROUND_TYPE_ALIGN): Remove.
    	* config/rs6000/linux64.h (ROUND_TYPE_ALIGN): Remove vector check.
    	* testsuite/gcc.dg/simd-3.c: New.
    
    From-SVN: r100820

---
+++ gcc/testsuite/gcc.dg/simd-3.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-options "-maltivec" { target powerpc-*-* } } */
+
+__attribute__ ((vector_size (2))) signed char v1, v2, v3;
+void
+one (void)
+{
+  v1 = v2 + v3;
+}
+
+__attribute__ ((vector_size (8))) signed char v4, v5, v6;
+void
+two (void)
+{
+  v4 = v5 + v6;
+}
