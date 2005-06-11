c2f4f5f29f425520babb118a8b4c2374e7a6a8cf
David Edelsohn <edelsohn@gnu.org>
2005-06-11 20:21:40 +0000

    backport: re PR libfortran/20930 (gfortran.dg/backspace.f execution test)
            Backport from mainline:
            PR libfortran/20930
            * io/rewind.c (st_rewind): Flush the stream when resetting the mode
            from WRITING to READING.
    
    From-SVN: r100844

--- libgfortran/io/rewind.c
+++ libgfortran/io/rewind.c
@@ -54,9 +54,13 @@ st_rewind (void)
 	  /* If we have been writing to the file, the last written record
 	     is the last record in the file, so truncate the file now.
 	     Reset to read mode so two consecutive rewind statements
-	     don't delete the file contents.  */
-          if (u->mode==WRITING)
-            struncate(u->s);
+	     don't delete the file contents.  Flush buffer when switching
+	     mode.  */
+          if (u->mode == WRITING)
+	    {
+	      flush (u->s);
+	      struncate (u->s);
+	    }
 	  u->mode = READING;
 	  u->last_record = 0;
 	  if (sseek (u->s, 0) == FAILURE)
