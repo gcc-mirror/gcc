0222d4de74e42cfc1de5479e641f262cd57afd9b
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-12 19:59:17 +0000

    PR libfortran/19155 	* io/read.c (read_f): Take care of spaces after initial sign. 	* gfortran.dg/pr19155.f: Add test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@100861 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/pr19155.f
+++ gcc/testsuite/gfortran.dg/pr19155.f
@@ -5,6 +5,7 @@
 ! but doesn't require us to issue an error. Since g77 accepts this as zero,
 ! we do the same.
       real a
+      character*10 c
       a = 42
       open (19,status='scratch')
       write (19,'(A15)') 'E+00'
@@ -12,4 +13,8 @@
       read (19,'(E15.8)') a
       if (a .ne. 0) call abort
       close (19)
+
+      c = "+         "
+      read (c,"(F10.4)") a
+      if (a /= 0) call abort
       end
