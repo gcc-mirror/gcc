a165e746471b32ea442c325732f5c2907034db45
James A. Morrison <phython@gcc.gnu.org>
2005-06-13 00:18:41 +0000

    re PR tree-optimization/14796 ([tree-ssa] combine two shifts into one)
    2005-06-12  James A. Morrison  <phython@gcc.gnu.org>
    
            PR tree-optimization/14796
            * fold-const (fold_binary): Transform (X << C) >> C into X & (-1>>C)
            for unsigned types.
    
    From-SVN: r100869

--- gcc/testsuite/gcc.dg/pr14796-1.c
+++ gcc/testsuite/gcc.dg/pr14796-1.c
@@ -9,6 +9,16 @@ int g (int b) {
 	return (b >> 5) << 5;
 }
 
+unsigned long long h (unsigned long long c) {
+	return (c << 60) >> 60;
+}
+
+int l (int d) {
+	return (d << 6) >> 6;
+}
+
 /* { dg-final { scan-tree-dump "a << 9" "gimple" } } */
 /* { dg-final { scan-tree-dump "b & -32" "gimple" } } */
+/* { dg-final { scan-tree-dump "c & 15" "gimple" } } */
+/* { dg-final { scan-tree-dump "d << 6" "gimple" } } */
 /* { dg-final { cleanup-tree-dump "gimple" } } */
