0ea49890a80cdddc5f79f1b56a3e8b508bdd2e86
Frank Ch. Eigler <fche@redhat.com>
2005-06-14 19:12:24 +0000

    re PR libmudflap/21023 (mudflap reports errors for external array variable with no size specified)
    2005-06-14  Frank Ch. Eigler  <fche@redhat.com>
    
    	PR mudflap/21023
    	* tree-mudflap.c (mudflap_finish_file): Exclude non-public
    	rather than static objects (!) from libmudflap registration.
    
    	* testsuite/libmudflap.c/externs.exp, externs-{1,2}.c: New test files.
    	* testsuite/libmudflap.c/cfrags.exp: Bypass new sources.
    
    From-SVN: r100950

---
+++ libmudflap/testsuite/libmudflap.c/externs-1.c
@@ -0,0 +1,14 @@
+typedef struct { char *name; } dummy;
+extern dummy d[];
+
+int
+main (void)
+{
+  dummy *pd = d;
+
+  while (pd->name)
+    {
+      printf ("%s\n", pd->name);
+      pd++;
+    }
+}
