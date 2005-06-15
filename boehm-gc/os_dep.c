1c2711243b68bceb1438c1b1067a2d593f7bca43
andreast <andreast@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-15 21:25:54 +0000

    2005-06-15  Andreas Tobler  <a.tobler@schweiz.ch>
    	* os_dep.c: Add FreeBSD/PowerPC bits.
    	(GC_SysVGetDataStart): Likewise.
    	* include/private/gcconfig.h: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@100997 138bc75d-0d04-0410-961f-82ee72b054a4

--- boehm-gc/os_dep.c
+++ boehm-gc/os_dep.c
@@ -80,7 +80,7 @@
 #   define NEED_FIND_LIMIT
 # endif
 
-#if defined(FREEBSD) && defined(I386)
+#if defined(FREEBSD) && (defined(I386) || defined(powerpc) || defined(__powerpc__))
 #  include <machine/trap.h>
 #  if !defined(PCR)
 #    define NEED_FIND_LIMIT
@@ -1387,7 +1387,7 @@ int * etext_addr;
 }
 # endif
 
-# if defined(FREEBSD) && defined(I386) && !defined(PCR)
+# if defined(FREEBSD) && (defined(I386) || defined(powerpc) || defined(__powerpc__)) && !defined(PCR)
 /* Its unclear whether this should be identical to the above, or 	*/
 /* whether it should apply to non-X86 architectures.			*/
 /* For now we don't assume that there is always an empty page after	*/
