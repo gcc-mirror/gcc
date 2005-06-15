3eee5bf485a0f505356e138b7275b5169b5cff1d
Goffredo Baroncelli <kreijack@inwind.it>
2005-06-15 22:08:27 +0200

    re PR libgcj/21074 (Trivial bug in the method  getHeaderFieldKey() in the file java/net/protocol/http/HTTPURLConnection.java)
    2005-06-15  Goffredo Baroncelli  <kreijack@inwind.it>
    
    	PR libgcj/21074:
    	* gnu/java/net/protocol/http/HTTPURLConnection.java
    	(getHeaderFieldKey): Check index.
    
    From-SVN: r100994

--- libjava/gnu/java/net/protocol/http/HTTPURLConnection.java
+++ libjava/gnu/java/net/protocol/http/HTTPURLConnection.java
@@ -1,5 +1,5 @@
 /* HTTPURLConnection.java --
-   Copyright (C) 2004 Free Software Foundation, Inc.
+   Copyright (C) 2004, 2005 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
@@ -572,6 +572,10 @@ public class HTTPURLConnection
     int count = 1;
     do
       {
+        if (!i.hasNext())
+          {
+            return null;
+          }
         entry = (Map.Entry) i.next();
         count++;
       }
