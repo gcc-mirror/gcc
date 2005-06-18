3feaa17e413960a88bb8383a0fdf02540feefdc6
kargl <kargl@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-18 17:59:31 +0000

    * intrinsic.c (gfc_intrinsic_func_interface): Enable errors for generic   functions whose simplification routine return FAILURE.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@101160 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/intrinsic.c
+++ gcc/fortran/intrinsic.c
@@ -3007,16 +3007,13 @@ got_specific:
   expr->value.function.isym = specific;
   gfc_intrinsic_symbol (expr->symtree->n.sym);
 
+  gfc_suppress_error = 0;
   if (do_simplify (specific, expr) == FAILURE)
-    {
-      gfc_suppress_error = 0;
-      return MATCH_ERROR;
-    }
+    return MATCH_ERROR;
 
   /* TODO: We should probably only allow elemental functions here.  */
   flag |= (expr->ts.type != BT_INTEGER && expr->ts.type != BT_CHARACTER);
 
-  gfc_suppress_error = 0;
   if (pedantic && gfc_init_expr
       && flag && gfc_init_expr_extensions (specific))
     {
