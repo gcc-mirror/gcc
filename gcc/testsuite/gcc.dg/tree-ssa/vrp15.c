b8f8bd676fdaad83fc346061846bc381acc8fde4
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-21 18:46:33 +0000

    * tree-vrp.c (extract_range_from_unary_expr): Handle type         conversions better.
            * gcc.dg/tree-ssa/vrp15.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@101232 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp15.c
@@ -0,0 +1,34 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-vrp" } */
+
+
+extern void abort (void) __attribute__ ((__noreturn__));
+union tree_node;
+typedef union tree_node *tree;
+enum tree_code
+{
+  ERROR_MARK,
+  COND_EXPR = 42,
+};
+extern const unsigned char tree_code_length[];
+struct tree_common
+{
+  enum tree_code code:8;
+};
+union tree_node
+{
+  struct tree_common common;
+};
+void
+blah (tree t)
+{
+  if (t->common.code != COND_EXPR)
+    abort ();
+  if (1 >= tree_code_length[t->common.code])
+    abort ();
+
+}
+
+/* { dg-final { scan-tree-dump-times "tree_code_length.42." 1 "vrp" } } */
+/* { dg-final { cleanup-tree-dump "vrp" } } */
+
