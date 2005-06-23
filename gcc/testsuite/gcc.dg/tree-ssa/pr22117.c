e57f2b4144b1346f1461faaf0d610699e794a8f2
Kazu Hirata <kazu@codesourcery.com>
2005-06-23 16:04:09 +0000

    re PR tree-optimization/22117 (VRP thinks <ptr type> + <ptr type> is always nonnull.)
    gcc/
    	PR tree-optimization/22117
    	* tree-vrp.c (extract_range_from_binary_expr): Compute a
    	correct range when adding two pointers.
    
    testsuite/
    	PR tree-optimization/22117
    	* gcc.dg/tree-ssa/pr22117.c: New.
    
    From-SVN: r101272

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr22117.c
@@ -0,0 +1,23 @@
+/* PR tree-optimization/22117
+   VRP used think that &p[q] is nonzero even though p and q are both
+   known to be zero after entering the first two "if" statements.  */
+
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-vrp" } */
+
+void
+foo (int *p, int q)
+{
+  if (p == 0)
+    {
+      if (q == 0)
+	{
+	  int *r = &p[q];
+	  if (r != 0)
+	    link_error ();
+	}
+    }
+}
+
+/* { dg-final { scan-tree-dump-times "Folding predicate r_.* != 0B to 0" 1 "vrp" } } */
+/* { dg-final { cleanup-tree-dump "vrp" } } */
