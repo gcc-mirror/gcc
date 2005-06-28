9009820b36c750c043ddf1cd885ef5ad51304350
Richard Henderson <rth@redhat.com>
2005-06-28 00:33:11 -0700

    tree-vect-transform.c (get_initial_def_for_reduction): Use correct type for DEF and INIT_VAL.
            * tree-vect-transform.c (get_initial_def_for_reduction): Use correct
            type for DEF and INIT_VAL.  Pretend MIN/MAX need epilogue adjustment.
    
            * gcc.dg/vect/vect-reduc-1.c: Adjust test to properly validate MIN.
            * gcc.dg/vect/vect-reduc-1char.c: Likewise.
            * gcc.dg/vect/vect-reduc-1short.c: Likewise.
            * gcc.dg/vect/vect-reduc-2.c: Likewise.
            * gcc.dg/vect/vect-reduc-2char.c: Likewise.
            * gcc.dg/vect/vect-reduc-2short.c: Likewise.
    
    From-SVN: r101374

--- gcc/testsuite/gcc.dg/vect/vect-reduc-1short.c
+++ gcc/testsuite/gcc.dg/vect/vect-reduc-1short.c
@@ -6,14 +6,15 @@
 #define N 16
 #define DIFF 242
 
-int main1 (unsigned short x, unsigned short max_result)
+void
+main1 (unsigned short x, unsigned short max_result, unsigned short min_result)
 {
   int i;
-  unsigned short ub[N] = {0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45};
-  unsigned short uc[N] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+  unsigned short ub[N] = {1,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45};
+  unsigned short uc[N] = {1,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
   unsigned short  udiff = 2;
   unsigned short umax = x;
-  unsigned short umin = 10;
+  unsigned short umin = x;
 
   for (i = 0; i < N; i++) {
     udiff += (unsigned short)(ub[i] - uc[i]);
@@ -32,18 +33,16 @@ int main1 (unsigned short x, unsigned short max_result)
     abort ();
   if (umax != max_result)
     abort ();
-  if (umin != 0)
+  if (umin != min_result)
     abort ();
-
-  return 0;
 }
 
 int main (void)
 { 
   check_vect ();
   
-  main1 (100, 100);
-  main1 (0, 15);
+  main1 (100, 100, 1);
+  main1 (0, 15, 0);
   return 0;
 }
 
