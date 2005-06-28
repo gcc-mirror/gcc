68c35eb8e6a605a0db9d18d697ec13d7ae413060
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-28 00:52:35 +0000

    PR 21959 	* tree-ssa-loop-niter.c (scev_probably_wraps_p): Handle type 	casts between unsigned and signed types with different size 	or precision.
    testsuite/ChangeLog
    
    	PR 21959
    	* gcc.dg/tree-ssa/pr21959.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@101365 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr21959.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-vrp" } */
+
+unsigned char c[0xFF];
+void f(void)
+{
+  unsigned char i;
+  c[128] = 128;
+  i = 0;
+  while (1)
+  {
+    /* This predicate should not be folded out.  */
+    if (((signed char) i) < 0) break;
+    c[i] = ' ';
+    i++;
+  }
+}
+
+/* { dg-final { scan-tree-dump-times "Folding predicate " 0 "vrp" } } */
+/* { dg-final { cleanup-tree-dump "vrp" } } */
