917b04f4923bdb692362b09ce5c4730d073c4c00
Kelley Cook <kcook@gcc.gnu.org>
2005-06-30 03:22:09 +0000

    Update FSF address
    From-SVN: r101453

--- libjava/scripts/MakeCharTables.java
+++ libjava/scripts/MakeCharTables.java
@@ -16,8 +16,8 @@ General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with GNU Classpath; see the file COPYING.  If not, write to the
-Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-02111-1307 USA.
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301 USA.
 
 Linking this library statically or dynamically with other modules is
 making a combined work based on this library.  Thus, the terms and
@@ -60,8 +60,8 @@ public class MakeCharTables implements CharData
                        + "\n"
                        + "You should have received a copy of the GNU General Public License\n"
                        + "along with GNU Classpath; see the file COPYING.  If not, write to the\n"
-                       + "Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA\n"
-                       + "02111-1307 USA.\n"
+                       + "Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA\n"
+                       + "02110-1301 USA.\n"
                        + "\n"
                        + "Linking this library statically or dynamically with other modules is\n"
                        + "making a combined work based on this library.  Thus, the terms and\n"
