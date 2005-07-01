f27cea3abf8ded22456f5f46a812cc3915969815
kcook <kcook@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-01 01:29:17 +0000

    Update FSF address
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@101492 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/gen-soccon.c
+++ gcc/ada/gen-soccon.c
@@ -14,8 +14,8 @@
 ** or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License **
 ** for  more details.  You should have  received  a copy of the GNU General **
 ** Public License  distributed with GNAT;  see file COPYING.  If not, write **
-** to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, **
-** MA 02111-1307, USA.                                                      **
+** to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, **
+** Boston, MA 02110-1301, USA.                                              **
 **                                                                          **
 ** GNAT was originally developed  by the GNAT team at  New York University. **
 ** Extensive contributions were provided by Ada Core Technologies Inc.      **
@@ -94,8 +94,8 @@ TXT("-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY
 TXT("-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --")
 TXT("-- for  more details.  You should have  received  a copy of the GNU General --")
 TXT("-- Public License  distributed with GNAT;  see file COPYING.  If not, write --")
-TXT("-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --")
-TXT("-- MA 02111-1307, USA.                                                      --")
+TXT("-- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --")
+TXT("-- Boston, MA 02110-1301, USA.                                              --")
 TXT("--                                                                          --")
 TXT("-- As a special exception,  if other files  instantiate  generics from this --")
 TXT("-- unit, or you link  this unit with other files  to produce an executable, --")
--- gcc/ada/prj-util.ads
+++ gcc/ada/prj-util.ads
@@ -16,8 +16,8 @@
 -- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
 -- for  more details.  You should have  received  a copy of the GNU General --
 -- Public License  distributed with GNAT;  see file COPYING.  If not, write --
--- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
--- MA 02111-1307, USA.                                                      --
+-- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
+-- Boston, MA 02110-1301, USA.                                              --
 --                                                                          --
 -- GNAT was originally developed  by the GNAT team at  New York University. --
 -- Extensive contributions were provided by Ada Core Technologies Inc.      --
