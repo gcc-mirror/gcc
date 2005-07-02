70a9e64b3cc4c27d53dd895e5318cae00245b22c
Andrew Pinski <pinskia@physics.uc.edu>
2005-07-02 16:24:31 +0000

    re PR tree-optimization/14490 ([tree-ssa] Simplify "a - 10 > 150" into "a > 160")
    2005-07-02  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR middle-end/14490
            * fold-const.c (fold_binary): Handle the return value of
            fold_to_nonsharp_ineq_using_bound if we get back the same operand back.
            Implement "X +- C1 CMP C2" folding to "X CMP C2 -+ C1".
    
    From-SVN: r101535

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr14490-2.c
@@ -0,0 +1,10 @@
+/* { dg-do compile } */
+/* { dg-options "-fdump-tree-gimple -fwrapv" } */
+int g(int x)
+{
+   return (x - 10) < 0;
+}
+/* There should be no x >= 9 and one x - 10. */
+/* { dg-final { scan-tree-dump-times ">= 9" 0 "gimple"} } */
+/* { dg-final { scan-tree-dump-times "- 10" 1 "gimple"} } */
+/* { dg-final { cleanup-tree-dump "gimple" } } */
