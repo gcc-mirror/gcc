2d0dab7f2bb2acbc9a9fffc7278be44c9a1dcd43
Jeff Law <law@redhat.com>
2005-07-02 08:15:11 -0600

    tree-ssa-dom.c (find_equivalent_equality_comparison): Do not a eliminate type conversion which feeds an equality comparison if...
            * tree-ssa-dom.c (find_equivalent_equality_comparison): Do not
            a eliminate type conversion which feeds an equality comparison
            if the original type or either operand in the comparison is a
            function pointer.
    
    	* gcc.dg/tree-ssa/pr22051-1.c: New test.
    	* gcc.dg/tree-ssa/pr22051-2.c: New test.
    
    From-SVN: r101534

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr22051-2.c
@@ -0,0 +1,25 @@
+/* { dg-do compile }  */
+/* { dg-options "-O2 -fdump-tree-optimized -w" }  */
+
+
+
+
+void *arf ();
+int
+foo()
+{
+  void (*q)(void);
+  int r = q;
+
+  if (r != 0)
+    return 1;
+  else
+    return 2;
+}
+
+/* The cast to an int type must remain after all optimizations are complete
+   so that we do not try to canonicalize a function pointer for the
+   comparison when no such canonicalization is wanted.  */
+/* { dg-final { scan-tree-dump-times "if \\(\\(int\\) q" 1 "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
+
