1966af04b7dd0d550d4672e9007874110c5eac23
Gabriel Dos Reis <gdr@integrable-solutions.net>
2005-07-03 06:50:51 +0000

    tree-dump.h (dump_string_field): Declare.
            * tree-dump.h (dump_string_field): Declare.
            * tree-dump.c: Use it instead of dump_string.
            (dump_string_field): Make non-static.
    
    cp/
            * dump.c: Use dump_string_field.
    
    From-SVN: r101547

--- gcc/tree-dump.h
+++ gcc/tree-dump.h
@@ -82,6 +82,7 @@ struct dump_info
 extern void dump_pointer (dump_info_p, const char *, void *);
 extern void dump_int (dump_info_p, const char *, int);
 extern void dump_string (dump_info_p, const char *);
+extern void dump_string_field (dump_info_p, const char *, const char *);
 extern void dump_stmt (dump_info_p, tree);
 extern void queue_and_dump_index (dump_info_p, const char *, tree, int);
 extern void queue_and_dump_type (dump_info_p, tree);
