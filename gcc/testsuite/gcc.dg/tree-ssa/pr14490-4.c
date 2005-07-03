6040cccf8bf007f4cca590398c788f8dbbce3b0e
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-03 23:55:04 +0000

    2005-07-03  Andrew Pinski  <pinskia@physics.uc.edu>
            PR testsuite/22288
            * gcc.dg/tree-ssa/pr14490-[1-4].c: Escape "+".
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@101562 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/pr14490-4.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr14490-4.c
@@ -6,5 +6,5 @@ int g(int x)
 }
 /* There should be no x < -10 and one x + 10. */
 /* { dg-final { scan-tree-dump-times "< -10" 0 "gimple"} } */
-/* { dg-final { scan-tree-dump-times "+ 10" 1 "gimple"} } */
+/* { dg-final { scan-tree-dump-times "\\+ 10" 1 "gimple"} } */
 /* { dg-final { cleanup-tree-dump "gimple" } } */
