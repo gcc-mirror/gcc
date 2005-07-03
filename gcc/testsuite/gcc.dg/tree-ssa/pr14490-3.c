bbef4d454798e7aad1886c96144ed3f72890194f
Andrew Pinski <pinskia@physics.uc.edu>
2005-07-03 23:55:04 +0000

    re PR testsuite/22288 (dg-final: couldn't compile regular expression pattern)
    2005-07-03  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR testsuite/22288
            * gcc.dg/tree-ssa/pr14490-[1-4].c: Escape "+".
    
    From-SVN: r101562

--- gcc/testsuite/gcc.dg/tree-ssa/pr14490-3.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr14490-3.c
@@ -6,5 +6,5 @@ int g(int x)
 }
 /* There should be only x < -10 and no x + 10. */
 /* { dg-final { scan-tree-dump-times "< -10" 1 "gimple"} } */
-/* { dg-final { scan-tree-dump-times "+ 10" 0 "gimple"} } */
+/* { dg-final { scan-tree-dump-times "\\+ 10" 0 "gimple"} } */
 /* { dg-final { cleanup-tree-dump "gimple" } } */
