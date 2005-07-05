40d790a36b5ecc26952f3a6c83cb8ac641244c25
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-05 14:16:06 +0000

    * config/m32r/m32r-protos.h: Remove m32r_finalize_pic. 	* config/m32r/m32r.c (m32r_compute_frame_size, 	m32r_expand_prologue): Take current_function_profile into 	account whenever we reference 	current_function_uses_pic_offset_table. 	(m32r_finalize_pic): Remove. 	* config/m32r/m32r.h (FINALIZE_PIC): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@101624 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/m32r/m32r-protos.h
+++ gcc/config/m32r/m32r-protos.h
@@ -29,7 +29,6 @@ extern void   m32r_init (void);
 extern void   m32r_init_expanders (void);
 extern unsigned m32r_compute_frame_size (int);
 extern void   m32r_expand_prologue (void);
-extern void   m32r_finalize_pic (void);
 extern int    direct_return (void);
 extern void   m32r_load_pic_register (void);
 
