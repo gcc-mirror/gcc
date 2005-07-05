d8828f5588041b10e148608e265e4fcc54e05f7c
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-05 17:34:29 +0000

    PR c/22308 	* c-decl.c (finish_struct): Also copy C_TYPE_FIELDS_READONLY, 	C_TYPE_FIELDS_VOLATILE and C_TYPE_VARIABLE_SIZE to type variants.
    testsuite:
    	* gcc.dg/pr22308-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@101629 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr22308-1.c
@@ -0,0 +1,13 @@
+/* Bug 22308: C_TYPE_FIELDS_READONLY not updated on type variants.  */
+/* { dg-do compile } */
+/* { dg-options "" } */
+
+struct foo s;
+volatile struct foo t;
+struct foo { const int z; };
+
+void
+bar (void)
+{
+  t = s; /* { dg-error "error: assignment of read-only variable 't'" } */
+}
