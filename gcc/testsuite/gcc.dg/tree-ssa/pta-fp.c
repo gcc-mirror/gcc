47bcb538b497203e13bcf965302e5af682f2bf12
Daniel Berlin <dberlin@dberlin.org>
2005-07-05 15:31:17 +0000

    tree-ssa-structalias.c (get_constraint_exp_from_ssa_var): Only fall back to saying it points to readonly memory if we can't do better.
    2005-07-04  Daniel Berlin  <dberlin@dberlin.org>
    
    	* tree-ssa-structalias.c (get_constraint_exp_from_ssa_var):
    	Only fall back to saying it points to readonly memory if
    	we can't do better.
    
    From-SVN: r101625

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pta-fp.c
@@ -0,0 +1,26 @@
+/* { dg-do compile } */ 
+/* { dg-options "-O2 -fdump-tree-pta1" } */
+extern double cos (double);
+extern double sin (double);
+double f(double a)
+{
+  double b;
+  double c,d;
+  double (*fp) (double);
+  if (a < 2.0)
+    {
+      fp = sin;
+      c = fp (a);
+    }
+  else
+    {
+      c = 1.0; 
+      fp = cos;
+    }
+  d = fp (a);
+  return d + c;
+}
+/* The points-to set of the final function pointer should be "sin cos" */
+
+/* { dg-final { scan-tree-dump-times "sin cos" 1 "pta1"} } */
+/* { dg-final { cleanup-tree-dump "pta1" } } */
