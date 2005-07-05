4e89ef6bb4a975089b255edc643ae950a16259b2
Diego Novillo <dnovillo@redhat.com>
2005-07-05 00:32:36 +0000

    * tree-ssa-structalias.c: Don't include expr.h.
    From-SVN: r101609

--- gcc/tree-ssa-structalias.c
+++ gcc/tree-ssa-structalias.c
@@ -34,7 +34,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include "basic-block.h"
 #include "output.h"
 #include "errors.h"
-#include "expr.h"
 #include "diagnostic.h"
 #include "tree.h"
 #include "c-common.h"
