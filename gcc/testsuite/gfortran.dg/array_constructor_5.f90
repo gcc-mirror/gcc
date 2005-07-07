9f9d4c5f5030a9631efd7d00354dae8c62434f25
Feng Wang <fengwang@nudt.edu.cn>
2005-07-07 01:56:04 +0000

    re PR fortran/22327 ([4.0 only] Wrong code in array constructor)
    2005-07-07  Feng Wang  <fengwang@nudt.edu.cn>
    
    	PR fortran/22327
    	* trans-array.c (gfc_trans_array_constructor_value): Fix index of data.
    
    2005-07-07  Feng Wang  <fengwang@nudt.edu.cn>
    
    	PR fortran/22327
    	* gfortran.dg/array_constructor_5.f90: New test.
    
    From-SVN: r101682

---
+++ gcc/testsuite/gfortran.dg/array_constructor_5.f90
@@ -0,0 +1,11 @@
+! { dg-do run }
+! PR22327
+program array_constructor
+    implicit none
+    integer :: a(6), i
+    i = 6
+    a = (/ 1, 2, 3, 4, 5, i /)
+    do i = 1, 6
+        if (a(i) /= i) call abort()
+    end do
+end program array_constructor
