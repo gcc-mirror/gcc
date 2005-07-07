f5d21c4ca15a9423443ce33efb3c9b8ebcde051e
Ziemowit Laski <zlaski@apple.com>
2005-07-07 06:25:49 +0000

    re PR objc/22274 (string[1-4].m, const-str-5.m fails with the gnu runtime)
    /objc/ChangeLog]
    2005-07-07  Ziemowit Laski  <zlaski@apple.com>
    
            PR objc/22274
            * objc-act.c (objc_build_string_object): For GNU-style constants,
            use the @interface type rather than the built-in type.
    
    [gcc/testsuite/ChangeLog]
    2005-07-07  Ziemowit Laski  <zlaski@apple.com>
    
            PR objc/22274
            * obj-c++.dg/gnu-runtime-[1-2].mm: New.
            * objc.dg/gnu-runtime-2.m: New.
    
    From-SVN: r101686

---
+++ gcc/testsuite/obj-c++.dg/gnu-runtime-1.mm
@@ -0,0 +1,19 @@
+/* Test that compiling for the GNU runtime works (regardless of
+   the system runtime used).  */
+/* Author: Ziemowit Laski <zlaski@apple.com>  */
+/* { dg-do compile } */
+/* { dg-options "-fgnu-runtime" } */
+
+#include <objc/Object.h>
+
+@interface FooBar: Object
+- (void)boo;
+@end
+
+int main ()
+{
+  id fooBarInst = [[FooBar alloc] init];
+  [fooBarInst boo];
+  return 0;
+}
+
---
+++ gcc/testsuite/objc.dg/gnu-runtime-2.m
@@ -0,0 +1,30 @@
+/* Sanity check for GNU-runtime version of constant strings,
+   regardless of runtime used on target system.  */
+
+/* { dg-do run } */
+/* { dg-options "-fgnu-runtime" } */
+
+#include <objc/Object.h>
+#include <string.h>
+#include <stdlib.h>
+
+@interface NXConstantString: Object
+{
+  char *c_string;
+  unsigned int len;                                                     
+}
+-(const char *) cString;
+-(unsigned int) length;
+@end
+
+@implementation NXConstantString
+-(const char *) cString { return c_string; }
+-(unsigned int) length  { return len; }
+@end
+
+int main(int argc, void **args)
+{
+  if (strcmp ([@"this is a string" cString], "this is a string"))
+    abort ();
+  return 0;
+}
