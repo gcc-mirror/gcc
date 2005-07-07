47b3a403a5b5424d15829d47abd4cb1a8a87e93e
Thomas Koenig <Thomas.Koenig@online.de>
2005-07-07 22:01:41 +0000

    re PR fortran/21594 ([4.0 only] FAIL: gfortran.dg/eoshift.f90  -O0  execution test)
    2005-07-07  Thomas Koenig  <Thomas.Koenig@online.de>
    
    	PR libfortran/21594
    	* intrinsics/eoshift0.c:  If abs(shift) > len, fill the
    	the whole array with the boundary value, but don't overrun it.
    	* intrinsics/eoshift2.c:  Likewise.
    	* m4/eoshift1.m4:  Likewise.
    	* m4/eoshift3.m4:  Likewise.
    	* generated/eoshift1_4.c:  Regenerated.
    	* generated/eoshift1_8.c:  Regenerated.
    	* generated/eoshift3_4.c:  Regenerated.
    	* generated/eoshift3_8.c:  Regenerated.
    
    2005-07-07  Thomas Koenig  <Thomas.Koenig@online.de>
    
    	PR libfortran/21594
    	* gfortran.fortran-torture/execute/intrinsic_eoshift.f90:
    	Add test cases where the shift length is greater than the
    	array length.
    
    From-SVN: r101738

--- gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_eoshift.f90
+++ gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_eoshift.f90
@@ -11,14 +11,27 @@ program intrinsic_eoshift
       call abort
 
    a = reshape ((/1, 2, 3, 4, 5, 6, 7, 8, 9/), (/3, 3/))
+   a = eoshift (a, 9999, 99, 1)
+   if (any (a .ne. 99)) call abort
+
+   a = reshape ((/1, 2, 3, 4, 5, 6, 7, 8, 9/), (/3, 3/))
    a = eoshift (a, -2, dim = 2)
    if (any (a .ne. reshape ((/0, 0, 0, 0, 0, 0, 1, 2, 3/), (/3, 3/)))) &
       call abort
+
+   a = reshape ((/1, 2, 3, 4, 5, 6, 7, 8, 9/), (/3, 3/))
+   a = eoshift (a, -9999, 99, 1)
+   if (any (a .ne. 99)) call abort
 
    ! Array shift and scalar bound.
    a = reshape ((/1, 2, 3, 4, 5, 6, 7, 8, 9/), (/3, 3/))
    a = eoshift (a, (/1, 0, -1/), 99, 1)
    if (any (a .ne. reshape ((/2, 3, 99, 4, 5, 6, 99, 7, 8/), (/3, 3/)))) &
+      call abort
+
+   a = reshape ((/1, 2, 3, 4, 5, 6, 7, 8, 9/), (/3, 3/))
+   a = eoshift (a, (/9999, 0, -9999/), 99, 1)
+   if (any (a .ne. reshape ((/99, 99, 99, 4, 5, 6, 99, 99, 99/), (/3, 3/)))) &
       call abort
 
    a = reshape ((/1, 2, 3, 4, 5, 6, 7, 8, 9/), (/3, 3/))
@@ -31,6 +44,16 @@ program intrinsic_eoshift
    a = eoshift (a, 1, (/99, -1, 42/), 1)
    if (any (a .ne. reshape ((/2, 3, 99, 5, 6, -1, 8, 9, 42/), (/3, 3/)))) &
       call abort
+
+   a = reshape ((/1, 2, 3, 4, 5, 6, 7, 8, 9/), (/3, 3/))
+   a = eoshift (a, 9999, (/99, -1, 42/), 1)
+   if (any (a .ne. reshape ((/99, 99, 99, -1, -1, -1, 42, 42, 42/), &
+	(/3, 3/)))) call abort
+
+   a = reshape ((/1, 2, 3, 4, 5, 6, 7, 8, 9/), (/3, 3/))
+   a = eoshift (a, -9999, (/99, -1, 42/), 1)
+   if (any (a .ne. reshape ((/99, 99, 99, -1, -1, -1, 42, 42, 42/), &
+	(/3, 3/)))) call abort
 
    a = reshape ((/1, 2, 3, 4, 5, 6, 7, 8, 9/), (/3, 3/))
    a = eoshift (a, -2, (/99, -1, 42/), 2)
@@ -60,6 +83,11 @@ program intrinsic_eoshift
    a = eoshift(a, shift=sh, boundary=bo)
    if (any (a .ne. reshape ((/ -999, -999, -999, -99, 4, 5, -9, -9, -9 /), &
         shape(a)))) call abort
+
+   a = reshape ((/1, 2, 3, 4, 5, 6, 7, 8, 9/), (/3, 3/))
+   a = eoshift (a, (/9999, -9999, 0/), (/99, -1, 42/), 2)
+   if (any (a .ne. reshape ((/99, -1, 3, 99, -1, 6, 99, -1, 9/), (/3, 3/)))) &
+      call abort
 
    ! Test arrays > rank 2
    b(:, :, 1) = reshape ((/1, 2, 3, 4, 5, 6, 7, 8, 9/), (/3, 3/))
--- libgfortran/generated/eoshift3_8.c
+++ libgfortran/generated/eoshift3_8.c
@@ -167,7 +167,14 @@ eoshift3_8 (gfc_array_char *ret, gfc_array_char *array,
     {
       /* Do the shift for this dimension.  */
       sh = *hptr;
-      delta = (sh >= 0) ? sh: -sh;
+      if (( sh >= 0 ? sh : -sh ) > len)
+	{
+	  delta = len;
+	  sh = len;
+	}
+      else
+	delta = (sh >= 0) ? sh: -sh;
+
       if (sh > 0)
         {
           src = &sptr[delta * soffset];
