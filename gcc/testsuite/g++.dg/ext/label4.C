25ab86387da37dfdf377a11bbe4a93fae8ca13b4
No Author <no-author@gcc.gnu.org>
2005-07-07 18:41:54 +0000

    This commit was manufactured by cvs2svn to create tag 'gcc_4_0_1_release'.
    From-SVN: r101728

--- gcc/testsuite/g++.dg/ext/label4.C
+++
@@ -1,6 +0,0 @@
-// PR c++/20563: ICE (--enable-checking), infinite loop (--disable-checking)
-// Origin:       Giovanni Bajo <giovannibajo@libero.it>
-
-// { dg-do compile }
-
-__label__ *l;  // { dg-error "before" }
