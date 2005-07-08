a0e71127bb2ab3450437010387edd33404590de8
Ziemowit Laski <zlaski@apple.com>
2005-07-08 01:53:37 +0000

    objc-act.c (objc_build_struct): Pass in an actual @interface instead of its name...
    [gcc/objc/ChangeLog]
    2005-07-07  Ziemowit Laski  <zlaski@apple.com>
    
            * objc-act.c (objc_build_struct): Pass in an actual @interface
            instead of its name, and annotate the struct created (and all
            existing variants thereof) with the @interface.
            (objc_compare_types): Treat forward-declared ObjC classes
            as stand-alone (root) classes for purposes of type comparisons.
            (build_private_template): Move some code to objc_build_struct().
    
    [gcc/testsuite/ChangeLog]
    2005-07-07  Ziemowit Laski  <zlaski@apple.com>
    
            * obj-c++.dg/proto-lossage-6.mm: New.
            * objc.dg/proto-lossage-6.m: New.
    
    From-SVN: r101750

---
+++ gcc/testsuite/obj-c++.dg/proto-lossage-6.mm
@@ -0,0 +1,17 @@
+@class Base;
+@protocol _Protocol;
+
+@interface ClassA {
+}
+-(void) func1:(Base<_Protocol> *)inTarget;
+@end
+
+int main()
+{
+	ClassA* theA = 0;
+	Base<_Protocol>* myBase = 0;
+	[theA func1:myBase];
+
+	return 0;
+}
+
