fbd74ce075e0c0453498cee02166204ea423bb62
dalej <dalej@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-12 20:33:02 +0000

    2005-07-10  Dale Johannesen  <dalej@apple.com>
    	* gcc.target/i386/compress-float-sse.c:  New.
    	* gcc.target/i386/compress-float-sse-pic.c:  New.
    	* gcc.target/i386/compress-float-387.c:  New.
    	* gcc.target/i386/compress-float-387-pic.c:  New.
    	* gcc.dg/compress-float-ppc.c:  New.
    	* gcc.dg/compress-float-ppc-pic.c:  New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@101939 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/compress-float-sse.c
@@ -0,0 +1,6 @@
+/* { dg-do compile { target i?86*-*-* } } */
+/* { dg-options "-O2 -march=pentium4 -mtune=prescott -mfpmath=sse" } */
+double foo (double x) {
+  return x + 1.75;
+}
+/* { dg-final { scan-assembler "movsd" } } */
