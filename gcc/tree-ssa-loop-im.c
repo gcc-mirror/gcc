99aec8dad088c96431699fb17d58bacf05bc7e09
Andrew Pinski <pinskia@physics.uc.edu>
2005-07-12 21:04:19 +0000

    re PR tree-optimization/22105 (ACATS ICE cxg2018 for_each_index, at tree-ssa-loop-im.c:219)
    2005-07-12  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR tree-opt/22105
            * tree-ssa-loop-im.c (for_each_index): Handle COMPLEX_CST.
    
    From-SVN: r101944

--- gcc/tree-ssa-loop-im.c
+++ gcc/tree-ssa-loop-im.c
@@ -195,6 +195,7 @@ for_each_index (tree *addr_p, bool (*cbck) (tree, tree *, void *), void *data)
 	case STRING_CST:
 	case RESULT_DECL:
 	case VECTOR_CST:
+	case COMPLEX_CST:
 	  return true;
 
 	default:
