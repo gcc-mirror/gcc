16bf25cdb684c9923605efbd6dcdc528a5ce912a
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-13 16:31:23 +0000

    * fold-const.c (fold_binary): When comparing two simple ADDR_EXPR         expressions, test their _DECL operands for pointer equality rather         than using operand_equal_p.
            * gcc.dg/tree-ssa/pr22321.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@101974 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr22321.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fno-tree-vrp -fno-tree-copy-prop -fno-tree-ccp -fdump-tree-optimized" } */
+
+
+volatile int x;
+
+int main ()
+{
+  volatile int *vip;
+  vip = &x;
+  volatile int *cvip;
+  cvip = vip;
+
+  if (vip != cvip) return -1;
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "return 0" 1 "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
+
