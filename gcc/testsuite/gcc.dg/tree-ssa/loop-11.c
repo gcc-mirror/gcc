2824a5c3b11d3131ee7f95938a869339aa863e57
Zdenek Dvorak <dvorakz@suse.cz>
2005-07-13 12:37:25 +0200

    re PR tree-optimization/22442 (scev cprop causes wrong code)
    	PR tree-optimizatio/22442
    	* gcc.dg/tree-ssa/loop-11.c: New test.
    
    From-SVN: r101963

---
+++ gcc/testsuite/gcc.dg/tree-ssa/loop-11.c
@@ -0,0 +1,28 @@
+/* A test for final value replacement and higher-order ivs,
+   see PR 22442.  */
+
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-vars" } */
+
+void bar (unsigned);
+
+void foo (void)
+{
+  unsigned i, a;
+
+  for (i = 0; i < 5; i++)
+    a = i * i;
+
+  bar (a);
+}
+
+/* Final value of a gets replaced.  */
+
+/* { dg-final { scan-tree-dump-times "\\(16\\)" 1 "vars" } } */
+
+/* And the empty loop is removed.  */
+
+/* { dg-final { scan-tree-dump-times "if " 0 "vars" } } */
+
+/* { dg-final { cleanup-tree-dump "vars" } } */
+
