17716b74d77eda5c4bc28b5dae0a94ab5f0a95f7
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-14 06:21:59 +0000

    2005-07-12 Paul Thomas  <pault@gcc.gnu.org>
    	PR libfortran/16435
    	* transfer.c (formatted_transfer): Correct the problems
    	with X- and T-editting that caused TLs followed by TRs
    	to overwrite data, which caused NIST FM908.FOR to fail
    	on many tests.
    	(data_transfer_init): Zero X- and T-editting counters at
    	the start of formatted IO.
    	* write.c (write_x): Write specified number of skips with
    	specified number of spaces at the end.
    
    2005-07-12  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR libfortran/16435
    	* gfortran.dg/tl_editting.f90: New.
    	* gfortran.dg/g77/f77-edit-x-out.f: Remove XFAIL.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@102008 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/g77/f77-edit-x-out.f
+++ gcc/testsuite/gfortran.dg/g77/f77-edit-x-out.f
@@ -8,5 +8,5 @@ C ( dg-output "^" }
       write(*,'(I1,1X,I1,2X,I1)') 1,2,3    ! { dg-output "1 2  3(\n|\r\n|\r)" }
 C Section 13.5.3 explains why there are no trailing blanks
       write(*,'(I1,1X,I1,2X,I1,3X)') 1,2,3 ! { dg-output "1 2  3(\n|\r\n|\r)" }
-C { dg-output "\$" {xfail *-*-*} } gfortran PR 16435
+C { dg-output "\$" }
       end
