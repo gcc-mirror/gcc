79dc2448d4341231e064f3262f29169ea00bbf17
Andrew Pinski <pinskia@physics.uc.edu>
2005-07-16 20:46:23 +0000

    re PR fortran/13257 ([4.0 only] Error instead of warning for missing comma in format string)
    2005-07-16  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR fortran/13257
            * io.c (check_format): Allow an optional comma
            between descriptors.
    
    2005-07-16  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR fortran/13257
            * format.c (parse_format_list): Allow an optional comma
            between descriptors.
    
    2005-07-16  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR fortran/13257
            * comma_format_extension_[1234].f: New tests.
    
    From-SVN: r102099

--- gcc/fortran/io.c
+++ gcc/fortran/io.c
@@ -433,6 +433,7 @@ check_format (void)
 format_item:
   /* In this state, the next thing has to be a format item.  */
   t = format_lex ();
+format_item_1:
   switch (t)
     {
     case FMT_POSINT:
@@ -705,8 +706,10 @@ between_desc:
       goto syntax;
 
     default:
-      error = "Missing comma";
-      goto syntax;
+      if (gfc_notify_std (GFC_STD_GNU, "Extension: Missing comma at %C")
+	  == FAILURE)
+	return FAILURE;
+      goto format_item_1;
     }
 
 optional_comma:
--- libgfortran/io/format.c
+++ libgfortran/io/format.c
@@ -452,6 +452,7 @@ parse_format_list (void)
   /* Get the next format item */
  format_item:
   t = format_lex ();
+ format_item_1:
   switch (t)
     {
     case FMT_POSINT:
@@ -854,8 +855,8 @@ parse_format_list (void)
       goto finished;
 
     default:
-      error = "Missing comma in format";
-      goto finished;
+      /* Assume a missing comma, this is a GNU extension */
+      goto format_item_1;
     }
 
   /* Optional comma is a weird between state where we've just finished
