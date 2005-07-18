419e6a7080fb9f0f00ecf063acc88e51326b89fe
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-18 23:35:15 +0000

    2005-07-18  Andrew Pinski  <pinskia@physics.uc.edu>
            * gcc.dg/tree-ssa/alias-2.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@102143 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/alias-2.c
@@ -0,0 +1,27 @@
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+static int a;
+int f;
+void bar (void)  __attribute__((noinline));
+void bar (void)
+{
+  f = 9;
+}
+
+void link_error ();
+
+int foo()
+{
+  int b, c;
+  a = 5;
+  b = a;
+  bar ();
+  b = b + a;
+  if (b != 10)
+    link_error ();
+  return b;
+}
+
+/* We should have removed the link_error on the tree level as GCC can tell that
+   a is not touched by the calling bar at all. */
+/* { dg-final { scan-tree-dump-times "link_error" 0 "optimized"} } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
