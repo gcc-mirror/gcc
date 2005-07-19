70aa69fa0011ecb56ff2226e7161b7f88da81a02
Steve Ellcey <sje@cup.hp.com>
2005-07-19 20:28:00 +0000

    backport: re PR middle-end/21969 (ICE on float __attribute__((vector_size(2048))))
    	Backport from mainline.
    	PR testsuite/21969
    	* gcc.dg/torture/pr21817.c: Use -fno-common for hppa*-*-* targets.
    
    From-SVN: r102170

--- gcc/testsuite/gcc.dg/torture/pr21817-1.c
+++ gcc/testsuite/gcc.dg/torture/pr21817-1.c
@@ -1,4 +1,8 @@
+/* hppa*-*-hpux* needs -fno-common so that value can be given a 16
+   byte alignment.  */
+
 /* { dg-do compile } */
+/* { dg-options "-fno-common" { target { hppa*-*-hpux* } } } */
 
 typedef float v4sf __attribute__((vector_size(16)));
 v4sf value;
