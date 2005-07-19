0219cf3d1b62af497f47d15991c6285b1563d557
Gerald Pfeifer <gerald@pfeifer.com>
2005-07-19 13:42:12 +0000

    config.gcc: Add support for *-*-freebsd7, *-*-freebsd8, and *-*-freebsd9.
    	* config.gcc: Add support for *-*-freebsd7, *-*-freebsd8,
    	and *-*-freebsd9.
    	* config/freebsd-spec.h (FBSD_TARGET_OS_CPP_BUILTINS): Ditto.
    
    From-SVN: r102157

--- gcc/config/freebsd-spec.h
+++ gcc/config/freebsd-spec.h
@@ -51,7 +51,13 @@ Boston, MA 02110-1301, USA.  */
 #define FBSD_TARGET_OS_CPP_BUILTINS()					\
   do									\
     {									\
-	if (FBSD_MAJOR == 6)						\
+	if (FBSD_MAJOR == 9)						\
+	  builtin_define ("__FreeBSD__=9");			       	\
+	else if (FBSD_MAJOR == 8)					\
+	  builtin_define ("__FreeBSD__=8");			       	\
+	if (FBSD_MAJOR == 7)						\
+	  builtin_define ("__FreeBSD__=7");			       	\
+	else if (FBSD_MAJOR == 6)					\
 	  builtin_define ("__FreeBSD__=6");			       	\
 	else if (FBSD_MAJOR == 5)	       				\
 	  builtin_define ("__FreeBSD__=5");			       	\
