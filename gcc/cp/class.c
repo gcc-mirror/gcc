070a40616d1f652e969d3c7ca3203c2fc7e6c44c
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-21 19:43:47 +0000

    2005-07-21  Andrew Pinski  <pinskia@physics.uc.edu>
            * class.c (build_base_path): Fix typo.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@102245 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/class.c
+++ gcc/cp/class.c
@@ -365,8 +365,8 @@ build_base_path (enum tree_code code,
 			 build2 (EQ_EXPR, boolean_type_node,
 				 current_in_charge_parm, integer_zero_node),
 			 v_offset,
-			 convet_to_integer (ptrdiff_type_node,
-					    BINFO_OFFSET (binfo)));
+			 convert_to_integer (ptrdiff_type_node,
+					     BINFO_OFFSET (binfo)));
       else
 	offset = v_offset;
     }
