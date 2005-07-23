b0d5240ebbb5fd63c1e30eb774890fc102e4d6d7
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-23 02:20:30 +0000

    2005-07-22  Andrew Pinski  <pinskia@physics.uc.edu>
            PR tree-opt/22625
            * g++.dg/opt/vrp2.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@102303 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/opt/vrp2.C
@@ -0,0 +1,20 @@
+/* { dg-do run } */
+/* { dg-options "-O2" } */
+
+/* VRP was miscompiling the following as it thought &a->b was a dereference
+   and therfore a was non-null.  
+   Reduced from Mozilla by Serge Belyshev <belyshev@depni.sinp.msu.ru>.  */
+
+extern "C" void abort (void);
+struct T { int i; } t;
+struct A : T { int j; } q, *p = &q;
+
+int main (void)
+{
+  if (p == &t)
+    return 0;
+  if (p)
+    abort ();
+  return 0;
+}
+
