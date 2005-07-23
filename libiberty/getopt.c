3f4ccd4448ec86e848260e321e2cc2b94fb9ee51
ghazi <ghazi@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-23 14:57:03 +0000

    * getopt.c: Include ansidecl.h before system headers.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@102308 138bc75d-0d04-0410-961f-82ee72b054a4

--- libiberty/getopt.c
+++ libiberty/getopt.c
@@ -42,8 +42,8 @@
 # endif
 #endif
 
-#include <stdio.h>
 #include "ansidecl.h"
+#include <stdio.h>
 
 /* Comment out all this code if we are using the GNU C Library, and are not
    actually compiling the library itself.  This code is part of the GNU C
