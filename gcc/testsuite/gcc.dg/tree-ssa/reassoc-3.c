c72f711f8aa995eef830a3189fd0ca65c663544e
Andrew Pinski <pinskia@physics.uc.edu>
2005-07-25 20:23:50 +0000

    tree-ssa-reassoc.c (reassociate_expr): Allow scaler floating point types when flag_unsafe_math_optimizations is true.
    2005-07-25  Andrew Pinski  <pinskia@physics.uc.edu>
    
            * tree-ssa-reassoc.c (reassociate_expr): Allow scaler floating point
            types when flag_unsafe_math_optimizations is true.
    
    2005-07-25  Andrew Pinski  <pinskia@physics.uc.edu>
    
            * gcc.dg/tree-ssa/reassoc-3.c: New test.
            * gcc.dg/tree-ssa/reassoc-4.c: New test.
    
    From-SVN: r102368

---
+++ gcc/testsuite/gcc.dg/tree-ssa/reassoc-3.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */ 
+/* { dg-options "-O2 -fdump-tree-optimized -ffast-math" } */
+float a, b, c, d;
+extern int printf (const char *, ...);
+int main(void)
+{
+  float e;
+  float f;
+  /* We should be able to transform these into the same expression, and only have two additions.  */
+  e = a + b;
+  e = e + c;
+  f = c + a;
+  f = f + b;
+  printf ("%f %f\n", e, f);
+}
+
+/* { dg-final { scan-tree-dump-times "\\\+" 2 "optimized"} } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
