7f0dae42e1e3b6b5d12f340842eb8477ced6887f
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-26 14:27:33 +0000

    2005-07-26  Andrew Pinski  <pinskia@physics.uc.edu>
            PR libobjc/22606
            * Makefile.in (ALL_CFLAGS): Add -fexceptions.
    2005-07-26  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR libobjc/22606
            * objc.dg/try-catch-11.m: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@102386 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/objc.dg/try-catch-11.m
@@ -0,0 +1,42 @@
+/* Check that throwing an exception from a -forward:: works.  */
+/* Developed by Marcin Koziej <creep@desk.pl>.  */
+
+/* { dg-do run } */
+/* { dg-options "-fobjc-exceptions -w" } */
+
+#import <objc/Object.h>
+#import <objc/objc-api.h>
+#include <stdlib.h>
+
+static int i;
+
+@interface Thrower : Object
+- forward: (SEL) s : (void*) a;
+@end
+
+@implementation Thrower
+- forward: (SEL) s : (void*) a
+{
+  i++;
+  @throw [Object new];
+}
+@end
+int
+main()
+{
+  id t = [Thrower new];
+  @try
+  {
+    [t doesnotexist];
+  }
+  @catch (id error)
+  {
+    i++;
+    [error free];
+  }
+  
+  if (i != 2)
+    abort ();
+  
+  return 0;
+}
