69ce48b2736f87d545f37562d6b712628517bba3
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-27 11:15:32 +0000

    2005-07-27  Richard Guenther  <rguenther@suse.de>
    	* tree-ssa-structalias.c (push_fields_onto_fieldstack):
    	Avoid pushing again if current struct contains only
    	fields we decomposed.
    
    	* gcc.dg/tree-ssa/salias-1.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@102424 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/salias-1.c
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-salias" } */
+
+struct {
+  struct {
+    struct {
+	int i, j;
+    } c;
+  } b;
+} a;
+
+int foo(void)
+{
+  a.b.c.i = 0;
+  return a.b.c.j;
+}
+
+/* { dg-final { scan-tree-dump-times "SFT" 2 "salias" } } */
+/* { dg-final { cleanup-tree-dump "salias" } } */
