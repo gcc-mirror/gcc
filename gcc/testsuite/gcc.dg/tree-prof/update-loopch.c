1e2f6f3248337c9e4de47646e9684ab4f16f7197
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-28 21:40:14 +0000

    * gcc.dg/tree-prof/update-loopch.c: Fix thinkos.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@102520 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-prof/update-loopch.c
+++ gcc/testsuite/gcc.dg/tree-prof/update-loopch.c
@@ -1,4 +1,4 @@
-/* { dg-options "-O2 -fdump-tree_profile -fdump-tree-optimized" } */
+/* { dg-options "-O2 -fdump-tree-tree_profile-blocks -fdump-tree-optimized-blocks" } */
 int max = 33333;
 int a[8];
 int
@@ -14,7 +14,7 @@ main ()
 /* Loop header copying will peel away the initial conditional, so the loop body
    is once reached directly from entry point of function, rest via loopback
    edge.  */
-/* { dg-final-use { scan-tree-dump-not "count:33333" "tree_profile"} } */
+/* { dg-final-use { scan-tree-dump "count:33333" "tree_profile"} } */
 /* { dg-final-use { scan-tree-dump "count:33332" "optimized"} } */
 /* { dg-final-use { scan-tree-dump-not "Invalid sum" "optimized"} } */
 /* { dg-final-use { cleanup-tree-dump "tree_profile" } } */
