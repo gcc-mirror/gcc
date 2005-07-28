f2b6b003c18093c3be3b15faa0f2bdc7f652a3fd
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-28 20:51:18 +0000

    PR target/17692         * config/i386/i386.c (ix86_split_sse_movcc): Emit DELETED note         when expanding to nothing.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@102509 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr17692.c
@@ -0,0 +1,21 @@
+/* { dg-do compile } */
+/* { dg-options "-O -mfpmath=sse -msse2" } */
+/* The fact that t1 and t2 are uninitialized is critical.  With them
+   uninitialized, the register allocator is free to put them in the same
+   hard register, which results in
+
+	xmm0 = xmm0 >= xmm0 ? xmm0 : xmm0
+
+   Which is of course a nop, but one for which we would ICE splitting the
+   pattern.  */
+   
+double out;
+
+static void foo(void)
+{
+    double t1, t2, t3, t4;
+    
+    t4 = t1 >= t2 ? t1 : t2;
+    t4 = t4 >= t3 ? t4 : t3;
+    out = t4;
+}
