7c47d6e984423f92f6f7d40e37026ae9402d579a
Joseph Myers <joseph@codesourcery.com>
2005-07-30 00:58:37 +0100

    re PR c/529 (-Wshadow warns on function prototypes vs. global vars)
    	PR c/529
    	* c-decl.c (warn_if_shadowing): Don't check for PARM_DECL in
    	nested function declarators.
    	(pushdecl): Don't call warn_if_shadowing for PARM_DECL.
    	(grokparms): Call warn_if_shadowing for parameters used within the
    	parameter list.
    	(store_parm_decls_newstyle): Call warn_if_shadowing for parameters
    	not used within the parameter list.
    	(store_parm_decls_oldstyle): Call warn_if_shadowing for parameters.
    
    testsuite:
    	* gcc.dg/Wshadow-3.c: New test.
    
    From-SVN: r102571

---
+++ gcc/testsuite/gcc.dg/Wshadow-3.c
@@ -0,0 +1,21 @@
+/* Test warnings for shadowing in function prototype scope: generally
+   useless but of use if the parameter is used within the scope.  Bug
+   529.  */
+/* Origin: Joseph Myers <joseph@codesourcery.com> */
+/* { dg-do compile } */
+/* { dg-options "-std=gnu89 -Wshadow" } */
+
+int v; /* { dg-warning "shadowed declaration" } */
+int f1(int v);
+int f2(int v, int x[v]); /* { dg-warning "warning: declaration of 'v' shadows a global declaration" } */
+int f3(int v, int y[sizeof(v)]); /* { dg-warning "warning: declaration of 'v' shadows a global declaration" } */
+int f4(int v) { return 0; } /* { dg-warning "warning: declaration of 'v' shadows a global declaration" } */
+int f5(int v, int x[v]) { return 0; } /* { dg-warning "warning: declaration of 'v' shadows a global declaration" } */
+int f6(int x) { return 0; }
+int f7(v) int v; { return 0; } /* { dg-warning "warning: declaration of 'v' shadows a global declaration" } */
+int f8(v, w) int v; int w[v]; { return 0; } /* { dg-warning "warning: declaration of 'v' shadows a global declaration" } */
+int f9(x) int x; { return 0; }
+int f10(v) { return 0; } /* { dg-warning "warning: declaration of 'v' shadows a global declaration" } */
+int f11(int a, int b(int a));
+int f12(int a, int b(int a, int x[a])); /* { dg-warning "warning: declaration of 'a' shadows a parameter" } */
+/* { dg-warning "shadowed declaration" "outer parm" { target *-*-* } 20 } */
