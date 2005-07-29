d2f3ffba9a32449f59973e11b846de08c2683583
James A. Morrison <phython@gcc.gnu.org>
2005-07-29 15:22:07 +0000

    tree-vrp.c (compare_range_with_value): Return true or false for ~[VAL_1...
    2005-07-29  James A. Morrison  <phython@gcc.gnu.org>
    
            * tree-vrp.c (compare_range_with_value): Return true or false
            for ~[VAL_1, VAL_2] OP VAL if VAL_1 <= VAL <= VAL_2 for NE_EXPR and
            EQ_EXPR respectively.
    
    From-SVN: r102558

---
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp19.c
@@ -0,0 +1,27 @@
+/* { dg-do compile } */
+/* { dg-options "-fwrapv -O1 -ftree-vrp -fdump-tree-vrp" } */
+
+#include <limits.h>
+extern void abort ();
+extern void exit (int);
+
+int f (int a) {
+	if (a != INT_MIN) {
+		a = a > 0 ? a : -a;
+		if (a < 0)
+		  return 1;
+	}
+	return 0;
+}
+
+int g (int b) {
+	if (b != INT_MIN) {
+		b = b > 0 ? b : -b;
+		if (b >= 0)
+		  return 0;
+	}
+	return 1;
+}
+/* { dg-final { scan-tree-dump "Folding predicate a_. < 0 to 0" "vrp" } } */
+/* { dg-final { scan-tree-dump "Folding predicate b_. >= 0 to 1" "vrp" } } */
+/* { dg-final { cleanup-tree-dump "vrp" } } */
