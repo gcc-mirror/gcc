37ecfd3b3dc80ee7814b105185b9a59d00b274db
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-30 22:46:25 +0000

    2005-07-30  Andrew Pinski  <pinskia@physics.uc.edu>
            * scripts/make_exports.pl: Pass --strip-underscores to c++filt.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@102595 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/scripts/make_exports.pl
+++ libstdc++-v3/scripts/make_exports.pl
@@ -92,7 +92,7 @@ my %export_hash = ();
 print STDERR 'nm -P '.(join ' ',@ARGV).'|';
 open NM,'nm -P '.(join ' ',@ARGV).'|' or die $!;
 # Talk to c++filt through a pair of file descriptors.
-open2(*FILTIN, *FILTOUT, "c++filt") or die $!;
+open2(*FILTIN, *FILTOUT, "c++filt --strip-underscores") or die $!;
 NAME: while (<NM>) {
     my $i;
     chomp;
