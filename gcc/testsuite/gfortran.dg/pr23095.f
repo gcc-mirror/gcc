a3f4b7d835009918910989ea72092a11d5b21d9f
Steven Bosscher <stevenb@suse.de>
2005-07-31 21:45:44 +0000

    re PR target/23095 (ICE in regstack compensate_edge)
    	PR target/23095
    	* common.opt (flag_gcse_after_reload): Don't initialize to 2.
    	(flag_rerun_cse_after_loop): Initialize this to 2 instead.
    	* postreload-gcse.c (hash_scan_set): Do not consider stack regs.
    
    testsuite/
    	PR target/23095
    	* gfortran.dg/pr23095.f: New test.
    
    	PR c++/22003
    	* g++.dg/other/pr22003.C: New test.
    
    From-SVN: r102610

---
+++ gcc/testsuite/gfortran.dg/pr23095.f
@@ -0,0 +1,22 @@
+      ! { dg-do compile { target i?86-*-* x86_64-*-* } }
+      ! { dg-options "-w -m32 -O2 -ffloat-store -fgcse-after-reload" }
+      !
+      ! GCSE after reload made a stack register live across an abnormal
+      ! edges for one of the computed jumps.  This bombed in reg-stack.
+      function foo(n) 
+      real*8 foo 
+      integer ix, n, next 
+      real*8 xmax, absx 
+      foo  = 0.0d0 
+      assign 20 to next 
+      do ix = 1,n 
+         go to next,(10, 30) 
+   10    assign 40 to next 
+         go to 40 
+   20    if (absx .gt. 8.232d-11) go to 40 
+   30    if (absx .le. xmax) go to 40 
+         xmax = absx 
+   40    go to next,(10, 30) 
+      end do 
+      return 
+      end 
