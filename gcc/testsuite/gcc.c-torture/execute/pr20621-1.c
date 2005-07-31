e355794ca1d5a73e44841de364f1f10ed8c8ed8f
No Author <no-author@gcc.gnu.org>
2005-07-31 08:35:15 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_4-branch'.
    From-SVN: r102604

---
+++ gcc/testsuite/gcc.c-torture/execute/pr20621-1.c
@@ -0,0 +1,6 @@
+/* When generating o32 MIPS PIC, main's $gp save slot was out of range
+   of a single load instruction.  */
+struct big { int i[sizeof (int) >= 4 && sizeof (void *) >= 4 ? 0x4000 : 4]; };
+struct big gb;
+int foo (struct big b, int x) { return b.i[x]; }
+int main (void) { return foo (gb, 0) + foo (gb, 1); }
