27b016fb33afae44c2dea54fe07a6a338890bded
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-08-03 14:05:02 +0000

    PR target/18582 	* testsuite/gcc.dg/torture/pr18582-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@102690 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/pr18582-1.c
@@ -0,0 +1,24 @@
+/* { dg-do compile { target i?86-*-* } } */
+/* { dg-options "-msse3" } */
+typedef char v16qi __attribute__((vector_size (16)));
+typedef float v4sf __attribute__((vector_size (16)));
+typedef double v2df __attribute__((vector_size (16)));
+
+extern char ca[];
+extern float fa[];
+extern double da[];
+
+extern v16qi cva[];
+extern v4sf fva[];
+extern v2df dva[];
+
+void
+foo (void)
+{
+  cva[0] = __builtin_ia32_loaddqu (ca);
+  cva[0] = __builtin_ia32_lddqu (ca);
+
+  fva[0] = __builtin_ia32_loadups (fa);
+
+  dva[0] = __builtin_ia32_loadupd (da);
+}
