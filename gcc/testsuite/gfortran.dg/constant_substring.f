8d47c4d75dd9f50a70629aa7880776c04237eb7f
dje <dje@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-08-03 01:58:30 +0000

    * gfortran.dg/constant_substring.f: New test.         * gfortran.dg/pr22491.f: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@102677 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/constant_substring.f
@@ -0,0 +1,11 @@
+! Simplify constant substring
+! { dg-do run }
+      character*2 a
+      character*4 b
+      character*6 c
+      parameter (a="12")
+      parameter (b = a(1:2))
+      write (c,'("#",A,"#")') b
+      if (c .ne. '#12  #') call abort
+      end
+
---
+++ gcc/testsuite/gfortran.dg/pr22491.f
@@ -0,0 +1,11 @@
+! PR fortran/21730
+! { dg-do run }
+      character*2 a (1)
+      character*4 b (1)
+      character*6 c
+      parameter (a="12")
+      parameter (b = a)
+      write (c,'("#",A,"#")') b
+      if (c .ne. '#12  #') call abort
+      end
+
