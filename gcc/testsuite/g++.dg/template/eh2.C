80b41fd3457db4121f908211dcb27d9894241f6e
No Author <no-author@gcc.gnu.org>
2005-08-06 22:48:08 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r102820

---
+++ gcc/testsuite/g++.dg/template/eh2.C
@@ -0,0 +1,10 @@
+// PR c++/23191
+// Origin: Volker Reichelt  <reichelt@igpm.rwth-aachen.de>
+// { dg-do compile }
+
+template<typename T> struct A
+{
+    void foo() throw(typename T::X);  // { dg-error "not a class" }
+};
+
+A<void> a;                            // { dg-error "instantiated" }
