88178f713a1ef555a82a797f829d4a4ca9603f51
reichelt <reichelt@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-08-06 22:22:47 +0000

    PR c++/19498 	* pt.c (tsubst_decl) <case TEMPLATE_DECL>: Return ERROR_MARK_NODE 	if substitution of template args did not succeed.
    	* g++.dg/template/instantiate7.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@102818 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/instantiate7.C
@@ -0,0 +1,10 @@
+// PR c++/19498
+// Origin: Volker Reichelt  <reichelt@igpm.rwth-aachen.de>
+// { dg-do compile }
+
+template<typename T> struct A
+{
+    template<T&> struct B;  // { dg-error "reference to void" }
+};
+
+A<void> a;                  // { dg-error "instantiated" }
