18a6e93f239e2ca2996e73d3c03b045f3142d01b
Andrew Pinski <pinskia@physics.uc.edu>
2005-08-09 17:51:27 +0000

    c99_classification_macros_c.cc: xfail on *-*-darwin*.
    2005-08-09  Andrew Pinski  <pinskia@physics.uc.edu>
    
            * testsuite/26_numerics/cmath/c99_classification_macros_c.cc:
            xfail on *-*-darwin*.
    
    From-SVN: r102919

--- libstdc++-v3/testsuite/26_numerics/cmath/c99_classification_macros_c.cc
+++ libstdc++-v3/testsuite/26_numerics/cmath/c99_classification_macros_c.cc
@@ -27,8 +27,8 @@
 // invalidate any other reasons why the executable file might be covered by
 // the GNU General Public License.
 
-// { dg-do compile { xfail *-*-linux-gnu } }
-// { dg-excess-errors "" { target *-*-linux-gnu } }
+// { dg-do compile { xfail *-*-linux-gnu *-*-darwin* } }
+// { dg-excess-errors "" { target *-*-linux-gnu *-*-darwin* } }
 
 #include <math.h>
 
