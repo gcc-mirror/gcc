b946251f603541388621d470a08a25e382a03620
No Author <no-author@gcc.gnu.org>
2005-08-11 22:55:20 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r103007

---
+++ gcc/testsuite/g++.dg/inherit/pure1.C
@@ -0,0 +1,19 @@
+// PR c++/23266
+// Origin: Volker Reichelt  <reichelt@igpm.rwth-aachen.de>
+// { dg-do compile }
+
+void foo0() = 0;                   // { dg-error "like a variable" }
+virtual void foo1() = 0;           // { dg-error "outside class|variable" }
+
+struct A
+{
+  void foo2() = 0;                 // { dg-error "non-virtual" }
+  static void foo3() = 0;          // { dg-error "static member" }
+  virtual static void foo4() = 0;  // { dg-error "both virtual and static" }
+  virtual void foo5() = 0;         // { dg-error "base class" }
+};
+
+struct B : A
+{
+  static void foo5() = 0;          // { dg-error "static member|declared" }
+};
