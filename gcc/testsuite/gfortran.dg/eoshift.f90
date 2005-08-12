68c41a6cc47494d33bd07ea414ba9e42f8b96352
Thomas Koenig <Thomas.Koenig@online.de>
2005-08-12 20:02:43 +0000

    eoshift.f90: Correct format.
    2005-08-12  Thomas Koenig  <Thomas.Koenig@online.de>
    
    	* gfortran.dg/eoshift.f90:  Correct format.
    
    From-SVN: r103045

--- gcc/testsuite/gfortran.dg/eoshift.f90
+++ gcc/testsuite/gfortran.dg/eoshift.f90
@@ -2,5 +2,5 @@
 ! PR 18958:  We used to segfault for eoshifting off the end of an array.
 program main
   character(len=20) line
-  write (line,'(I4)') eoshift((/1, 3/), 3)
+  write (line,'(2I4)') eoshift((/1, 3/), 3)
 end program main
