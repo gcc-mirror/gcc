96010094bdf67de48831fd1c6b3e96bf3da93aa5
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-08-14 16:15:40 +0000

    2005-08-14 Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/21432.
    	* io.c (match_io): Add code to implement PRINT namelist.
    
    
    2005-08-14 Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/21432.
    	* gfortran.dg/namelist_print_1.f: New test of functionality of
    	PRINT namelist.
    	* gfortran.dg/namelist_print_2.f: New test to check that PRINT
    	namelist generates error with -std=f95.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@103079 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/namelist_print_1.f
@@ -0,0 +1,13 @@
+! Test Non standard PRINT namelist - PR21432
+!
+! Contributor Paul Thomas  <pault@gcc.gnu.org>
+!
+! { dg-do run }
+! { dg-options "-std=gnu" }
+
+      real  x
+      namelist /mynml/ x
+      x = 1
+! ( dg-output "^" }
+      print mynml ! { dg-output "&MYNML(\n|\r\n|\r) X=  1.000000    ,  /(\n|\r\n|\r)" }
+      end
