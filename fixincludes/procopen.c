896306971a8c31fa956069c8e3e4d5821b8d5566
kcook <kcook@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-08-15 00:50:43 +0000

    2005-08-14  Kelley Cook  <kcook@gcc.gnu.org>
    	* All files: Update with new FSF address.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@103096 138bc75d-0d04-0410-961f-82ee72b054a4

--- fixincludes/procopen.c
+++ fixincludes/procopen.c
@@ -17,8 +17,8 @@
  *  You should have received a copy of the GNU General Public License
  *  along with Server Handling.  See the file "COPYING".  If not,
  *  write to:  The Free Software Foundation, Inc.,
- *             59 Temple Place - Suite 330,
- *             Boston,  MA  02111-1307, USA.
+ *             51 Franklin Street, Fifth Floor,
+ *             Boston,  MA  02110-1301, USA.
  *
  * As a special exception, The Free Software Foundation gives
  * permission for additional uses of the text contained in his release
