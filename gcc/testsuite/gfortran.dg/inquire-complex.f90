8be7baa02ec323ce796275cf2ddee7d214a244fb
No Author <no-author@gcc.gnu.org>
2005-08-16 21:38:28 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r103174

---
+++ gcc/testsuite/gfortran.dg/inquire-complex.f90
@@ -0,0 +1,14 @@
+! { dg-do run }
+! PR 23428:  Inquire(iolength) used to give the wrong result.
+program main
+  implicit none
+  integer s4, s8
+  
+  complex(kind=8) c8
+  complex(kind=4) c4
+  
+  inquire (iolength=s4) c4
+  inquire (iolength=s8) c8
+  if (s4 /= 8 .or. s8 /= 16) call abort
+
+end program main
