b62656d9f7039c8a41ad3cca07c2a1570b08782f
James E Wilson <wilson@specifixinc.com>
2005-08-16 13:34:19 -0700

    Fix ICE that occurs with too large array.
    PR tree-optimization/21105
    * c-decl.c (grokdeclarator): Use TYPE_SIZE_UNIT not TYPE_SIZE in
    TREE_OVERFLOW check.
    
    From-SVN: r103169

--- gcc/c-decl.c
+++ gcc/c-decl.c
@@ -4376,7 +4376,7 @@ grokdeclarator (const struct c_declarator *declarator,
 
   if (TREE_CODE (type) == ARRAY_TYPE
       && COMPLETE_TYPE_P (type)
-      && TREE_OVERFLOW (TYPE_SIZE (type)))
+      && TREE_OVERFLOW (TYPE_SIZE_UNIT (type)))
     {
       error ("size of array %qs is too large", name);
       /* If we proceed with the array type as it is, we'll eventually
