e89b6c1c1ec815a9eaedf6e10daf5b8d07f4d54f
Mark Kettenis <kettenis@gnu.org>
2005-08-17 17:30:47 +0000

    floatformat.c (floatformat_always_valid): Change type of last argument to `void *'.
    [libiberty]
    * floatformat.c (floatformat_always_valid): Change type of last
    argument to `void *'.
    (floatformat_i387_ext_is_valid): Likewise.
    (floatformat_to_double): Change type of second argument to `const
    void *'.
    (floatformat_from_double): Change type of last argument to `void
    *'.
    (floatformat_is_valid): Change type of last argument to `const
    void *'.
    (ieee_test): Remove redundant casts.
    [include]
    * floatformat.h (struct floatformat): Change type of large
    argument for is_valid member to `const void *'.
    (floatformat_to_double): Change type of second argument to `const
    void *'.
    (floatformat_from_double): Change type of last argument to `void
    *'.
    (floatformat_is_valid): Change type of last argument to `const
    void *'.
    
    From-SVN: r103218

--- include/floatformat.h
+++ include/floatformat.h
@@ -1,5 +1,6 @@
 /* IEEE floating point support declarations, for GDB, the GNU Debugger.
-   Copyright 1991, 1994, 1995, 1997, 2000, 2003 Free Software Foundation, Inc.
+   Copyright 1991, 1994, 1995, 1997, 2000, 2003, 2005
+   Free Software Foundation, Inc.
 
 This file is part of GDB.
 
@@ -82,7 +83,7 @@ struct floatformat
   const char *name;
 
   /* Validator method.  */
-  int (*is_valid) (const struct floatformat *fmt, const char *from);
+  int (*is_valid) (const struct floatformat *fmt, const void *from);
 };
 
 /* floatformats for IEEE single and double, big and little endian.  */
@@ -116,17 +117,17 @@ extern const struct floatformat floatformat_ia64_quad_little;
    Store the double in *TO.  */
 
 extern void
-floatformat_to_double (const struct floatformat *, const char *, double *);
+floatformat_to_double (const struct floatformat *, const void *, double *);
 
 /* The converse: convert the double *FROM to FMT
    and store where TO points.  */
 
 extern void
-floatformat_from_double (const struct floatformat *, const double *, char *);
+floatformat_from_double (const struct floatformat *, const double *, void *);
 
 /* Return non-zero iff the data at FROM is a valid number in format FMT.  */
 
 extern int
-floatformat_is_valid (const struct floatformat *fmt, const char *from);
+floatformat_is_valid (const struct floatformat *fmt, const void *from);
 
 #endif	/* defined (FLOATFORMAT_H) */
