fe2ae685a1624456ecb22eba91f154226ede1146
Kelley Cook <kcook@gcc.gnu.org>
2005-08-17 02:49:08 +0000

    All files: Update FSF address.
    2005-08-17  Kelley Cook  <kcook@gcc.gnu.org>
    
    	* All files: Update FSF address.
    
    From-SVN: r103194

--- libgfortran/intrinsics/system.c
+++ libgfortran/intrinsics/system.c
@@ -25,8 +25,8 @@ for more details.
 
 You should have received a copy of the GNU General Public License
 along with libgfortran; see the file COPYING.  If not, write to the Free
-Software Foundation, 59 Temple Place - Suite 330, Boston, MA
-02111-1307, USA.  */
+Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.  */
 
 #include "config.h"
 
