d08fb6dc989f9dfcb03c0e4dcaee9bf32124c805
No Author <no-author@gcc.gnu.org>
2005-08-22 09:54:42 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r103340

---
+++ gcc/testsuite/g++.dg/template/param1.C
@@ -0,0 +1,12 @@
+// PR c++/22233
+// Origin: Volker Reichelt  <reichelt@igpm.rwth-aachen.de>
+// { dg-do compile }
+
+template<int> struct A
+{
+  A();
+};
+
+template<int N, char> A<N>::A() {}  // { dg-error "got 2|but 1 required" }
+
+A<0> a;
