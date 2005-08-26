ba34341baa14414c1df48b2f3ccec26a611df6bf
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-08-26 16:26:50 +0000

    PR c/23506 	* c-common.c (c_common_nodes_and_builtins): Increase builtin_types 	array by one element, initialize the BT_LAST element with NULL.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@103526 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-common.c
+++ gcc/c-common.c
@@ -2951,7 +2951,7 @@ c_common_nodes_and_builtins (void)
 
   typedef enum builtin_type builtin_type;
 
-  tree builtin_types[(int) BT_LAST];
+  tree builtin_types[(int) BT_LAST + 1];
   int wchar_type_size;
   tree array_domain_type;
   tree va_list_ref_type_node;
@@ -3311,6 +3311,7 @@ c_common_nodes_and_builtins (void)
 #undef DEF_FUNCTION_TYPE_VAR_4
 #undef DEF_FUNCTION_TYPE_VAR_5
 #undef DEF_POINTER_TYPE
+  builtin_types[(int) BT_LAST] = NULL_TREE;
 
   c_init_attributes ();
 
