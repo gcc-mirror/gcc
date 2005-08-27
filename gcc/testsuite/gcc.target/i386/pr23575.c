b2266391e5763f27c76aae7f9201e4a3ecb7fc54
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-08-27 12:04:28 +0000

    2005-08-27  Richard Guenther  <rguenther@gcc.gnu.org>
    	PR target/23575
    	* config/i386/sse.md (sse2_movsd): Add missing closing
    	braces.
    
    	* gcc.target/i386/pr23575.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@103559 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr23575.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-options "-march=pentium4 -O2" } */
+
+/* We used to ICE because of a bogous pattern.  */
+
+typedef double __v2df __attribute__ ((__vector_size__ (16)));
+typedef __v2df __m128d;
+static __inline __m128d __attribute__((__always_inline__)) _mm_set1_pd (double __F) {
+  return __extension__ (__m128d){__F, __F};
+}
+static __inline __m128d __attribute__((__always_inline__)) _mm_move_sd (__m128d __A, __m128d __B) {
+  return (__m128d) __builtin_ia32_movsd ((__v2df)__A, (__v2df)__B);
+}
+void g(__m128d b);
+__m128d cross(__m128d tmp9)
+{
+  __m128d t1 = _mm_set1_pd(1.0);
+  __m128d tmp10 = _mm_move_sd(t1, tmp9);
+  return tmp10;
+}
