55b806069f8eb4fccbe16fcbddfe3010f26b0515
dalej <dalej@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-08-31 17:28:17 +0000

    2005-08-31  Dale Johannesen  <dalej@apple.com>
            * gcc.dg/20050830-1.c:  New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@103691 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20050830-1.c
@@ -0,0 +1,13 @@
+/* Make sure the doloop optimization is done for this loop. */
+/* { dg-do compile { target powerpc-*-* } } */
+/* { dg-options "-O2" } */
+/* { dg-final { scan-assembler "bdn" } } */
+extern int a[];
+int foo(int w) {
+  int n = w;
+  while (n >= 512)
+    {
+    a[n] = 42;
+    n -= 256;
+    }
+  }
