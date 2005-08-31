65d8810dea559edbbcb290453827ec44e28656ca
Volker Reichelt <reichelt@igpm.rwth-aachen.de>
2005-08-31 08:56:11 +0000

    re PR c++/23639 (Bad error message: not a member of '<declaration error>')
    	PR c++/23639
    	* semantics.c (qualified_name_lookup_error): Do not complain again
    	on invalid scope.
    
    	* g++.dg/template/ttp5.C: Adjust error markers.
    
    From-SVN: r103664

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -2295,7 +2295,9 @@ check_multiple_declarators (void)
 void
 qualified_name_lookup_error (tree scope, tree name)
 {
-  if (TYPE_P (scope))
+  if (scope == error_mark_node)
+    ; /* We already complained.  */
+  else if (TYPE_P (scope))
     {
       if (!COMPLETE_TYPE_P (scope))
 	error ("incomplete type `%T' used in nested name specifier", scope);
