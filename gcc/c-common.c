5f8072bb0b6aafd2668df04bf73e73c0d9d19cb5
Jakub Jelinek <jakub@redhat.com>
2005-09-01 18:46:46 +0200

    re PR c/23506 (Bad array access in DEF_GCC_BUILTIN)
    	PR c/23506
    	* c-common.c (c_common_nodes_and_builtins): Increase builtin_types
    	array by one element, initialize the BT_LAST element with NULL.
    
    From-SVN: r103737

--- gcc/c-common.c
+++ gcc/c-common.c
@@ -2942,7 +2942,7 @@ c_common_nodes_and_builtins (void)
 
   typedef enum builtin_type builtin_type;
 
-  tree builtin_types[(int) BT_LAST];
+  tree builtin_types[(int) BT_LAST + 1];
   int wchar_type_size;
   tree array_domain_type;
   tree va_list_ref_type_node;
@@ -3230,6 +3230,7 @@ c_common_nodes_and_builtins (void)
 #undef DEF_FUNCTION_TYPE_VAR_2
 #undef DEF_FUNCTION_TYPE_VAR_3
 #undef DEF_POINTER_TYPE
+  builtin_types[(int) BT_LAST] = NULL_TREE;
 
   c_init_attributes ();
 
