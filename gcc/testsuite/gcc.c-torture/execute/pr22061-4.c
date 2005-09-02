734eca190ee96d5e3a0066ad0cabd5c3180ced94
No Author <no-author@gcc.gnu.org>
2005-09-02 12:34:58 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_4-branch'.
    From-SVN: r103775

---
+++ gcc/testsuite/gcc.c-torture/execute/pr22061-4.c
@@ -0,0 +1,22 @@
+void
+bar (int N)
+{
+  void foo (int a[2][N++]) {}
+  int a[2][N];
+  foo (a);
+  int b[2][N];
+  foo (b);
+  if (sizeof (a) != sizeof (int) * 2 * 1)
+    abort ();
+  if (sizeof (b) != sizeof (int) * 2 * 2)
+    abort ();
+  if (N != 3)
+    abort ();
+}
+
+int
+main (void)
+{
+  bar (1);
+  exit (0);
+}
