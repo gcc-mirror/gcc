bd87b2831c3c4192f44414f6315e5d202eba8ab8
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-03 18:54:55 +0000

    * io/read.c (read_x): Take int argument instead of fnode * and 	digging the N from F->u.n. 	* io/io.h (read_x): Adjust prototype. 	* io/transfer.c (formatted_transfer): Adjust callers.  Don't clobber 	f->u.n for FMT_T.
    	* gfortran.dg/fmt_t_1.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@103811 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/fmt_t_1.f90
@@ -0,0 +1,12 @@
+! { dg-do run }
+      integer nrow, vec(15)
+      open (10, status="scratch")
+      write (10, fmt='(a)') '001    1 2 3 4 5 6'
+      write (10, fmt='(a)') '000000 7 8 9101112'
+      write (10, fmt='(a)') '000000131415'
+      rewind (10)
+      read (10, fmt='(i6, (t7, 6i2))') nrow, (vec(i), i=1,15)
+      close (10)
+      if (nrow.ne.1) call abort
+      if (any (vec.ne.(/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15/))) call abort
+      end
