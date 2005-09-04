bd7380acd0fbea5430a1b59ad18dbb0e26d69998
No Author <no-author@gcc.gnu.org>
2005-09-04 17:07:46 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r103833

---
+++ gcc/testsuite/objc.dg/type-stream-1.m
@@ -0,0 +1,25 @@
+/* { dg-options "-fgnu-runtime" } */
+/* { dg-do run } */
+#include <objc/typedstream.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+  FILE *f; TypedStream *ts;
+  struct T { int a, b; } x = { 1, 2 };
+  f = fopen ("foo", "w"); ts = objc_open_typed_stream (f, OBJC_WRITEONLY);
+  objc_write_type (ts, @encode(struct T), &x);
+  objc_close_typed_stream (ts); fclose (f);
+  f = fopen ("foo", "r"); ts = objc_open_typed_stream (f, OBJC_READONLY);
+  struct T y;
+  objc_read_type (ts, @encode(struct T), &y);
+  if (y.a != 1)
+   abort ();
+  if (y.b != 2)
+   abort ();
+  objc_close_typed_stream (ts); fclose (f);
+  remove ("foo");
+  return 0;
+}
+
