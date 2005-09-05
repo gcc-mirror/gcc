cb9a1932082ed898f401b71f3de7375156f876b8
J"orn Rennecke <joern.rennecke@st.com>
2005-09-05 14:20:27 +0000

    * gcc.dg/pr21255-1.c: Match different pattern for sh64.
    From-SVN: r103905

--- gcc/testsuite/gcc.dg/pr21255-1.c
+++ gcc/testsuite/gcc.dg/pr21255-1.c
@@ -1,12 +1,14 @@
 /* { dg-do compile { target "sh*-*-*" } } */
 /* { dg-options "-O2 -fomit-frame-pointer" } */
-/* { dg-final { scan-assembler "mov fr4,fr.; mov fr5,fr." } } */
+/* { dg-final { scan-assembler "mov fr4,fr.; mov fr5,fr." { target sh-*-* } } } */
+/* { dg-final { scan-assembler "mov fr4,fr.; mov fr5,fr." { target sh[1234lb]*-*-* } } } */
+/* { dg-final { scan-assembler "mov fr0,fr.; mov fr1,fr." { target sh[56]*-*-* } } } */
 double
 f (double d)
 {
   double r;
 
-#if defined (__SH_FPU_DOUBLE__) && !TARGET_SHMEDIA
+#if defined (__SH_FPU_DOUBLE__)
   asm ("mov %S1,%S0; mov %R1,%R0" : "=f" (r) : "f" (d));
 #else
   asm ("mov fr4,fr4; mov fr5,fr5");
