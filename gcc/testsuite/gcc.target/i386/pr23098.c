48346d17656dc98397dba13818b241a6dece10bd
No Author <no-author@gcc.gnu.org>
2005-09-06 19:44:44 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r103960

---
+++ gcc/testsuite/gcc.target/i386/pr23098.c
@@ -0,0 +1,25 @@
+/* PR rtl-optimization/23098 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fPIC" } */
+/* { dg-final { scan-assembler-not "\.LC\[0-9\]" } } */
+/* { dg-require-effective-target ilp32 } */
+
+double foo (float);
+
+double
+f1 (void)
+{
+  return foo (1.0);
+}
+
+double
+f2 (void)
+{
+  return foo (0.0);
+}
+
+void
+f3 (float *x, float t)
+{
+  *x = 0.0 + t;
+}
