d65aa0ee4cbf1e1df72f420f42779db27b61b24a
Jakub Jelinek <jakub@redhat.com>
2005-09-06 11:22:49 +0200

    dwarf-char1.c: Accept more assembler comment start strings.
    	* gcc.dg/debug/dwarf2/dwarf-char1.c: Accept more assembler comment
    	start strings.
    	* gcc.dg/debug/dwarf2/dwarf-char2.c: Likewise.
    	* gcc.dg/debug/dwarf2/dwarf-char3.c: Likewise.
    
    From-SVN: r103938

--- gcc/testsuite/gcc.dg/debug/dwarf2/dwarf-char1.c
+++ gcc/testsuite/gcc.dg/debug/dwarf2/dwarf-char1.c
@@ -1,8 +1,8 @@
 /* PR debug/7241 */
 /* { dg-do compile } */
 /* { dg-options "-O2 -gdwarf-2 -dA" } */
-/* { dg-final { scan-assembler "0x\[68\]\[ \t\]# DW_AT_encoding" } } */
-/* { dg-final { scan-assembler-not "0x\[57\]\[ \t\]# DW_AT_encoding" } } */
+/* { dg-final { scan-assembler "0x\[68\]\[ \t\]\[#@;!/|\]+ +DW_AT_encoding" } } */
+/* { dg-final { scan-assembler-not "0x\[57\]\[ \t\]\[#@;!/|\]+ +DW_AT_encoding" } } */
 
 char a;
 const char b;
--- gcc/testsuite/gcc.dg/debug/dwarf2/dwarf-char3.c
+++ gcc/testsuite/gcc.dg/debug/dwarf2/dwarf-char3.c
@@ -1,7 +1,7 @@
 /* PR debug/7241 */
 /* { dg-do compile } */
 /* { dg-options "-O2 -gdwarf-2 -dA" } */
-/* { dg-final { scan-assembler "0x\[68\]\[ \t\]# DW_AT_encoding" } } */
-/* { dg-final { scan-assembler-not "0x\[57\]\[ \t\]# DW_AT_encoding" } } */
+/* { dg-final { scan-assembler "0x\[68\]\[ \t\]\[#@;!/|\]+ +DW_AT_encoding" } } */
+/* { dg-final { scan-assembler-not "0x\[57\]\[ \t\]\[#@;!/|\]+ +DW_AT_encoding" } } */
 
 const char *p = "abc";
