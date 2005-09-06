97232bfad89e11b831a5cedf983d5473083cfd81
Tom Tromey <tromey@redhat.com>
2005-09-06 23:53:44 +0000

    re PR libgcj/22211 ([4.0 only] Thread.interrupt sometimes causes abort if thread is already dead)
    	PR libgcj/22211:
    	* java/lang/natThread.cc (finish_): Synchronize when updating
    	alive_flag.
    	(_Jv_AttachCurrentThread): Likewise.
    	(interrupt): Only call _Jv_ThreadInterrupt if thread is alive.
    	* java/lang/Thread.java (isAlive): Now synchronized.
    
    From-SVN: r103973

--- libjava/java/lang/Thread.java
+++ libjava/java/lang/Thread.java
@@ -16,8 +16,8 @@ General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with GNU Classpath; see the file COPYING.  If not, write to the
-Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-02111-1307 USA.
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301 USA.
 
 Linking this library statically or dynamically with other modules is
 making a combined work based on this library.  Thus, the terms and
@@ -550,7 +550,7 @@ public class Thread implements Runnable
    *
    * @return whether this Thread is alive
    */
-  public final boolean isAlive()
+  public final synchronized boolean isAlive()
   {
     return alive_flag;
   }
