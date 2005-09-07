f9b320550f8384ee7a3fc56c07af4ebc2d619bdc
Thomas Koenig <Thomas.Koenig@online.de>
2005-09-07 21:34:26 +0000

    parameter+save.f90: Rename to
    2005-09-07  Thomas Koenig  <Thomas.Koenig@online.de>
    
    	* gfortran.dg/parameter+save.f90:  Rename to
    	* gfortran.dg/parameter_save.f90: .. this.
    
    From-SVN: r104013

--- gcc/testsuite/gfortran.dg/parameter+save.f90
+++
@@ -1,6 +0,0 @@
-! { dg-do compile }
-! PR 20848 - parameter and save should conflict.
-  integer, parameter, save :: x=0 ! { dg-error "conflicts" }
-  integer, save :: y 
-  parameter (y=42)   ! { dg-error "conflicts" }
-end
