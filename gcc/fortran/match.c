0ffe0b3ed9842419d44e053fa2b4dda0d3e97ef9
Tobias Schlüter <tobias.schlueter@physik.uni-muenchen.de>
2005-09-09 00:07:13 +0200

    re PR fortran/23765 (segfault with syntactically wrong common declaration)
    fortran/
    	PR fortran/23765
    	* match.c (gfc_match_common): Remove unnecessary / wrong special
    	cases for end-of-statement.
    testsuite/
    	PR fortran/23765
    	* gfortran.dg/common_6.f90: New test.
    
    From-SVN: r104048

--- gcc/fortran/match.c
+++ gcc/fortran/match.c
@@ -2244,9 +2244,6 @@ gfc_match_common (void)
 
   as = NULL;
 
-  if (gfc_match_eos () == MATCH_YES)
-    goto syntax;
-
   for (;;)
     {
       m = match_common_name (name);
@@ -2276,9 +2273,6 @@ gfc_match_common (void)
 	}
 
       /* Grab the list of symbols.  */
-      if (gfc_match_eos () == MATCH_YES)
-	goto done;
-  
       for (;;)
 	{
 	  m = gfc_match_symbol (&sym, 0);
