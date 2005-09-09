41306174c8b7e4680a11d6faf4a03261bbcb243b
Paul Thomas <pault@gcc.gnu.org>
2005-09-09 00:27:21 +0000

    19990905-0.f: Remove XFAIL...
    2005-09-09  Paul Thomas  <pault@gcc.gnu.org>
    
    	* gfortran.dg/g77/19990905-0.f: Remove XFAIL, rearrange
    	equivalences and add comment to connect the test with
    	the PR. - forgotten in previous.
    
    From-SVN: r104061

--- gcc/testsuite/gfortran.dg/g77/19990905-0.f
+++ gcc/testsuite/gfortran.dg/g77/19990905-0.f
@@ -1,8 +1,12 @@
 c { dg-do compile }
 * =foo0.f in Burley's g77 test suite.
+! Used to give "Variable 'm' cannot appear" "Variable 'm' cannot appear"
+! after REAL a(m,n), as described in PR 16511.
+!
       subroutine sub(a)
+      equivalence (m,iarray(100))
       common /info/ iarray(1000)
-      equivalence (m,iarray(100)), (n,iarray(200))
-      real a(m,n) ! { dg-bogus "Variable 'm' cannot appear" "Variable 'm' cannot appear" { xfail *-*-* } } PR 16511 
+      equivalence (n,iarray(200))
+      real a(m,n)
       a(1,1) = a(2,2)
       end
