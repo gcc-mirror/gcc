09ee8f721054e55134ee4fa755a0f3654795b0a2
Craig Rodrigues <rodrigc@gcc.gnu.org>
2005-09-09 00:21:01 +0000

    backport: c-decl.c (diagnose_mismatched_decls): With -Wredundant-decls...
    	Backport from mainline
    	2005-09-01  Craig Rodrigues  <rodrigc@gcc.gnu.org>
    
    	* c-decl.c (diagnose_mismatched_decls):  With -Wredundant-decls,
    	do not issue warning for a variable definition following
    	a declaration.
    
    From-SVN: r104059

--- gcc/c-decl.c
+++ gcc/c-decl.c
@@ -1271,7 +1271,10 @@ diagnose_mismatched_decls (tree newdecl, tree olddecl,
       && !(DECL_EXTERNAL (olddecl) && !DECL_EXTERNAL (newdecl))
       /* Don't warn about forward parameter decls.  */
       && !(TREE_CODE (newdecl) == PARM_DECL
-	   && TREE_ASM_WRITTEN (olddecl) && !TREE_ASM_WRITTEN (newdecl)))
+	   && TREE_ASM_WRITTEN (olddecl) && !TREE_ASM_WRITTEN (newdecl))
+      /* Don't warn about a variable definition following a declaration.  */
+      && !(TREE_CODE (newdecl) == VAR_DECL
+	   && DECL_INITIAL (newdecl) && !DECL_INITIAL (olddecl)))
     {
       warning ("%Jredundant redeclaration of '%D'", newdecl, newdecl);
       warned = true;
