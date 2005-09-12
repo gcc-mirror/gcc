f439f9a536087071b4593eb0dda40eb5cc592c1c
J"orn Rennecke <joern.rennecke@st.com>
2005-09-12 13:49:56 +0000

    re PR middle-end/23290 (Layout changed for structure with single complex field)
    	PR middle-end/23290
    	* stor-layout.c (compute_record_mode): For records with a single
    	field, actually check the field's mode size against the type size.
    
    From-SVN: r104171

--- gcc/stor-layout.c
+++ gcc/stor-layout.c
@@ -1367,14 +1367,15 @@ compute_record_mode (tree type)
 #endif /* MEMBER_TYPE_FORCES_BLK  */
     }
 
-  TYPE_MODE (type) = mode_for_size_tree (TYPE_SIZE (type), MODE_INT, 1);
-
   /* If we only have one real field; use its mode if that mode's size
      matches the type's size.  This only applies to RECORD_TYPE.  This
      does not apply to unions.  */
   if (TREE_CODE (type) == RECORD_TYPE && mode != VOIDmode
-      && GET_MODE_SIZE (mode) == GET_MODE_SIZE (TYPE_MODE (type)))
+      && host_integerp (TYPE_SIZE (type), 1)
+      && GET_MODE_BITSIZE (mode) == TREE_INT_CST_LOW (TYPE_SIZE (type)))
     TYPE_MODE (type) = mode;
+  else
+    TYPE_MODE (type) = mode_for_size_tree (TYPE_SIZE (type), MODE_INT, 1);
 
   /* If structure's known alignment is less than what the scalar
      mode would need, and it matters, then stick with BLKmode.  */
