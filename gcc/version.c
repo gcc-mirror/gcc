7497b08d8bf74ac1390ed1c6fe31f1015d4526dd
GCC Administrator <gccadmin@gcc.gnu.org>
2005-09-13 00:16:08 +0000

    Daily bump.
    From-SVN: r104199

--- gcc/version.c
+++ gcc/version.c
@@ -5,7 +5,7 @@
    please modify this string to indicate that, e.g. by putting your
    organization's name in parentheses at the end of the string.  */
 
-const char version_string[] = "4.0.2 20050912 (prerelease)";
+const char version_string[] = "4.0.2 20050913 (prerelease)";
 
 /* This is the location of the online document giving instructions for
    reporting bugs.  If you distribute a modified version of GCC,
