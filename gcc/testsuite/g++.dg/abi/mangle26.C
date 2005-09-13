3beac9100a2cb243f88591bab2ae74f36be71a7d
Bastian Blank <waldi@debian.org>
2005-09-13 15:15:37 +0000

    re PR c++/16171 ([ABI] Problems with standard names in different namespaces)
    	PR c++/16171
    	* mangle.c (find_substitution): Do not use special substitutions
    	for identifiers not in std::.
    
    	PR c++/16171
    	* g++.dg/abi/mangle26.C: New test.
    	* g++.dg/abi/mangle27.C: New test.
    	* g++.dg/abi/mangle28.C: New test.
    	* g++.dg/abi/mangle29.C: New test.
    
    From-SVN: r104228

---
+++ gcc/testsuite/g++.dg/abi/mangle26.C
@@ -0,0 +1,14 @@
+// Test of std mangling
+
+// { dg-do compile }
+// { dg-options "-fno-inline" }
+
+namespace std {
+  struct A {
+    virtual void f () { }
+  };
+}
+
+std::A a;
+
+// { dg-final { scan-assembler "\n_?_ZNSt1AC1Ev\[: \t\n\]" } }
