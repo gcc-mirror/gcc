79e72538bf5172eca78af35f62d5f75dedfd1f61
Uros Bizjak <uros@kss-loka.si>
2005-09-13 21:02:04 +0200

    re PR target/23816 (ICE in extract_insn, at recog.c:2084)
            PR target/23816
            * config/i386/sse.md (*ieee_sminv4sf3, *ieee_smaxv4sf3)
            (*ieee_sminv2df3, *ieee_smaxv2df3): New insn patterns.
    
    From-SVN: r104236

---
+++ gcc/testsuite/gcc.dg/vect/pr23816-1.c
@@ -0,0 +1,9 @@
+/* { dg-do compile } */
+
+void
+foo (float a[32], float b[2][32])
+{
+  int i;
+  for (i = 0; i < 32; i++)
+    a[i] = (b[0][i] > b[1][i]) ? b[0][i] : b[1][i];
+}
---
+++ gcc/testsuite/gcc.dg/vect/pr23816-2.c
@@ -0,0 +1,9 @@
+/* { dg-do compile } */
+
+void
+foo (double a[32], double b[2][32])
+{
+  int i;
+  for (i = 0; i < 32; i++)
+    a[i] = (b[0][i] > b[1][i]) ? b[0][i] : b[1][i];
+}
