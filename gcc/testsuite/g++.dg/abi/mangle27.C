290031a32c94782a793778c5e628eaa8ae2cdf56
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-13 15:15:37 +0000

    PR c++/16171 	* mangle.c (find_substitution): Do not use special substitutions 	for identifiers not in std::.
    	PR c++/16171
    	* g++.dg/abi/mangle26.C: New test.
    	* g++.dg/abi/mangle27.C: New test.
    	* g++.dg/abi/mangle28.C: New test.
    	* g++.dg/abi/mangle29.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@104228 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/abi/mangle27.C
@@ -0,0 +1,14 @@
+// Test of std::basic_iostream<char, std::char_traits<char> > mangling
+
+// { dg-do compile }
+// { dg-options "-fno-inline" }
+
+namespace std {
+  template<typename> struct char_traits;
+
+  template<typename, typename> struct basic_iostream { basic_iostream(){} };
+}
+
+std::basic_iostream<char,std::char_traits<char> > s1;
+
+// { dg-final { scan-assembler "\n_?_ZNSdC1Ev\[: \t\n\]" } }
