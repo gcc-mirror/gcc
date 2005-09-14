d91a309555ac21f2db1c4709552144391330f54a
jconner <jconner@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-14 00:25:01 +0000

    PR c++/23180         * g++.dg/init/struct2.C: New test.         * g++.dg/init/struct3.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@104254 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/init/struct2.C
@@ -0,0 +1,22 @@
+/* PR c++/23180.  */
+/* Initialize a local structure with an expression that attempts to use
+   pointer arithmetic to calculate another structure field offset.  */
+
+void saveLoadEntries(const void *);
+
+void saveOrLoad() {
+    struct Track {
+        char soundName[15];
+    };
+    struct SaveLoadEntry {
+        int offs;
+        int type;
+        int size;
+    };    
+
+    SaveLoadEntry trackEntries = {
+	((long) (&((Track *) 42)->soundName[0])) - 42,
+        0, 1
+    };
+    saveLoadEntries(&trackEntries);
+}
