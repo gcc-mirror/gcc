2c84302b67210bbea74396659502027c2ec210b9
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-14 05:12:04 +0000

    2005-09-13  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/19358
    	* trans-array.c (gfc_trans_dummy_array_bias): correct the typo
    	which uses dim[i].upper for lbound, rather than dim[i].lower.
    
    2005-09-13  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/19358
    	* gfortran.fortran-torture/assumed_dummy_1.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@104259 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/assumed_dummy_1.f90
@@ -0,0 +1,46 @@
+! { dg do-run}
+! Tests the fix for PRs 19358, 19477, 21211 and 21622.
+!
+! Note that this tests only the valid cases with explicit interfaces.
+!
+! Contributed by Paul Thomas  <pault@gcc.gnu.org>
+!
+module global
+contains
+  SUBROUTINE goo (x, i)
+    REAL, DIMENSION(i:)     :: x
+    integer                 :: i
+    x (3) = 99.0
+  END SUBROUTINE goo
+end module global
+
+SUBROUTINE foo (x, i)
+  REAL, DIMENSION(i:)       :: x
+  integer                   :: i
+  x (4) = 42.0
+END SUBROUTINE foo
+
+program test
+  use global
+  real, dimension(3)        :: y = 0
+  integer                   :: j = 2
+
+interface
+  SUBROUTINE foo (x, i)
+    REAL, DIMENSION(i:)     :: x
+    integer                 :: i
+  END SUBROUTINE foo
+end interface
+  call foo (y, j)
+  call goo (y, j)
+  call roo (y, j)
+  if (any(y.ne.(/21.0, 99.0, 42.0/))) call abort ()
+contains
+  SUBROUTINE roo (x, i)
+    REAL, DIMENSION(i:)     :: x
+    integer                 :: i
+    x (2) = 21.0
+  END SUBROUTINE roo
+end program test
+
+
