a325b0be7e695e310454331adb668288af8dd2a9
Josh Conner <jconner@apple.com>
2005-09-14 21:34:37 +0000

    re PR tree-optimization/23584 (ipa-pure-const pass ignores dereferencing a volatile pointer type)
        PR middle-end/23584
        * gcc.dg/pr23584.c: New test.
    
    From-SVN: r104284

---
+++ gcc/testsuite/gcc.dg/pr23584.c
@@ -0,0 +1,21 @@
+/* Regression test for PR middle-end/23584 */
+/* Verify that dereferencing an absolute address inside of a function
+   makes that function impure.  */
+
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-ipa-pure-const" } */
+
+int test1 (void)
+{
+  return * (volatile int *) 0x1234;
+}
+
+int test2 (void)
+{
+  int local = * (volatile int *) 0x1234;
+  return local;
+}
+
+/* { dg-final { scan-ipa-dump-not "found to be pure: test1" "pure-const" } } */
+/* { dg-final { scan-ipa-dump-not "found to be pure: test2" "pure-const" } } */
+/* { dg-final { cleanup-ipa-dump "pure-const" } } */
