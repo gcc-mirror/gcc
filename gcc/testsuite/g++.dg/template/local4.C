0f35bd1d8e0cd95e2d18542e0363afce78fddfbd
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-16 15:41:45 +0000

    PR c++/21514 	* pt.c (check_instantiated_args): Treat uses of anonymous types as 	causing type-deduction failure.
    	PR c++/21514
    	* g++.dg/template/crash19.C: Remove dg-error marker.
    	* g++.dg/template/local4.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@104344 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/template/local4.C
+++ gcc/testsuite/g++.dg/template/local4.C
@@ -4,8 +4,5 @@ template <typename T> void foo() {}
 
 int main () {
   struct S {};
-  // We do not simply use "local|match" on line 10 because we want to
-  // make sure that "local" appears.
-  // { dg-error "local" "local" { target *-*-* } 10 }
-  foo<S> (); // { dg-error "trying|match" } 
+  foo<S> (); // { dg-error "match" } 
 }
