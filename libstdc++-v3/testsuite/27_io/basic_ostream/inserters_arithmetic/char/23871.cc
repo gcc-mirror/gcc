10d6edf84c350e224ed8b917f1d3f0087b7e5277
Janis Johnson <janis187@us.ibm.com>
2005-09-16 22:45:33 +0000

    23871.cc: New.
    	* testsuite/27_io/basic_ostream/inserters_arithmetic/char/23871.cc:
    	New.
    
    From-SVN: r104358

---
+++ libstdc++-v3/testsuite/27_io/basic_ostream/inserters_arithmetic/char/23871.cc
@@ -0,0 +1,38 @@
+// 2005-09-15  Janis Johnson  <janis187@us.ibm.com>
+
+// Copyright (C) 2005 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+// MA 02110-1301, USA.
+
+// { dg-do compile }
+
+// libstdc++/23871
+
+#include <iostream>
+
+class mytest {
+public:
+  mytest (int);
+  unsigned char operator|| (const mytest&) const;
+  friend unsigned char operator|| (const int&, const mytest&);
+};
+
+inline unsigned char operator|| (const int& lhs, const mytest& rhs)
+{
+  std::cout << 1 << std::endl;
+  return (mytest)lhs || rhs;
+}
