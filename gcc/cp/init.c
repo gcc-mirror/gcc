368490ddf1c19ac151eedaf8caa0fe7d05ba222d
Mark Mitchell <mark@codesourcery.com>
2005-09-22 00:10:41 +0000

    re PR c++/23993 (Mysterious compiler error when accessing a 2d-array in a template class)
    	PR c++/23993
    	* init.c (integral_constant_value): Use DECL_INTEGRAL_CONSTANT_VAR_P.
    
    	PR c++/23993
    	* g++.dg/template/array14.C: New test.
    
    From-SVN: r104509

--- gcc/cp/init.c
+++ gcc/cp/init.c
@@ -1566,12 +1566,8 @@ build_offset_ref (tree type, tree name, bool address_p)
 tree
 integral_constant_value (tree decl)
 {
-  while ((TREE_CODE (decl) == CONST_DECL
-	  || (TREE_CODE (decl) == VAR_DECL
-	      /* And so are variables with a 'const' type -- unless they
-		 are also 'volatile'.  */
-	      && CP_TYPE_CONST_NON_VOLATILE_P (TREE_TYPE (decl))
-	      && DECL_INITIALIZED_BY_CONSTANT_EXPRESSION_P (decl))))
+  while (TREE_CODE (decl) == CONST_DECL
+	 || DECL_INTEGRAL_CONSTANT_VAR_P (decl))
     {
       tree init;
       /* If DECL is a static data member in a template class, we must
