196067027a74e57aba9c5d78870d4971b1591fce
No Author <no-author@gcc.gnu.org>
2005-09-22 00:10:41 +0000

    This commit was manufactured by cvs2svn to create tag 'gcc_4_0_2_release'.
    From-SVN: r104510

--- gcc/testsuite/gcc.dg/inline2.c
+++
@@ -1,5 +0,0 @@
-/* { dg-do compile } */
-/* { dg-options "-Wall -std=gnu89" } */
-/* This test should compile successfully.  */
-extern inline int foo (void) { return 0; }
-inline int foo (void) { return 1; }
