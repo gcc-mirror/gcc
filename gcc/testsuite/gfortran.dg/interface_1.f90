821bde0f832740ac9466302792809819ef1217cc
Steven G. Kargl <kargls@comcast.net>
2005-09-22 22:47:45 +0000

    re PR fortran/24005 (Ambiguous INTERFACE leads to seg fault)
    PR fortran/24005
    gfortran.dg/interface_1.f90: New test.
    
    From-SVN: r104545

---
+++ gcc/testsuite/gfortran.dg/interface_1.f90
@@ -0,0 +1,40 @@
+! { dg-do compile }
+! This program would segfault without the patch for PR fortran/24005.
+module y
+  !
+  ! If private statement is removed, then we get a bunch of errors
+  !
+  private f
+  !
+  ! If we rename 'f' in module y to say 'g', then gfortran correctly
+  ! identifies ambiguous as being ambiguous.
+  !
+  interface ambiguous
+    module procedure f
+  end interface
+
+  contains
+
+    real function f(a)
+      real a
+      f = a
+    end function
+
+end module y
+
+module z
+
+  use y
+
+  interface ambiguous
+    module procedure f    ! { dg-error "in generic interface" "" }
+  end interface
+
+  contains
+
+    real function f(a)
+      real a
+      f = a
+    end function
+
+end module z
