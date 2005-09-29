954ea6adb1acbcf8a3cac4f7c48a3cc73c8ce8ec
dberlin <dberlin@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-29 19:38:00 +0000

    2005-09-29  Daniel Berlin  <dberlin@dberlin.org>
    	Fix PR tree-optimization/24117
    	* tree-ssa-structalias.c (find_func_aliases): Strip nops
    	before considering whether to use anyoffset.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@104791 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr24117.c
@@ -0,0 +1,24 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+typedef struct  {
+  int x;
+  int z;
+} Foo_t;
+
+char *xm;
+void bar(void);
+
+void foo(void)
+{
+  Foo_t x;
+  x.x = 1;
+  x.z = 2;
+  xm = (char *)&x;
+  bar();
+  /* We can't propagate x.z past bar, so this link_error should still be there.  */
+  if (x.z != 2)
+    link_error ();
+}
+/* { dg-final { scan-tree-dump-times "link_error" 1 "optimized"} } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
