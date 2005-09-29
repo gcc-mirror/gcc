c2c4436e9cf2375b3e75b46df23eda9574ae556b
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-29 17:42:42 +0000

    2005-09-29  Chris Jefferson <chris@bubblescope.net>
    	PR libstdc++/23978
    	* include/tr1/functional (ref, cref): Make inline.
    	* include/tr1/tuple_iterate.h (tuple): Add operator=(std::pair).
    	(tie): Correct formatting.
    	(make_tuple): Make inline.
    	* testsuite/tr1/6_containers/tuple/creation_functions/23978.cc: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@104784 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/tr1/6_containers/tuple/creation_functions/23978.cc
@@ -0,0 +1,47 @@
+// 2005-09-29  Chris Jefferson  <chris@bubblescope.net>
+//
+// Copyright (C) 2005 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// Tuple
+
+#include <tr1/tuple>
+#include <tr1/utility>
+#include <testsuite_hooks.h>
+
+using namespace std;
+using namespace tr1;
+
+// libstdc++/23978
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  pair<int, int> p(1, 2);
+  int x = 0;
+  int y = 0;
+  tie(x, y) = p;
+  VERIFY( x == 1 && y == 2 );
+}
+
+int
+main()
+{
+  test01();
+  return 0;
+}
