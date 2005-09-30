7a6a7d975bd7643be654af588793d528ba70b57d
Uros Bizjak <uros@kss-loka.si>
2005-09-30 08:17:54 +0200

    fast-math-vect-reduc-7.c: Clean up temp files.
    	* gcc.dg/vect/fast-math-vect-reduc-7.c: Clean up temp files.
    	* gcc.dg/vect/pr22480.c: Clean up temp files.
    	* gcc.dg/vect/pr23831.c: Clean up temp files.
    	* gcc.dg/vect/pr23816-1.c: Require effective-target vect_condition.
    	Clean up temp files.
    	* gcc.dg/vect/pr23816-2.c: Require effective-target vect_condition.
    	Clean up temp files.
    
    	* gcc.dg/20050309-1.c: Clean up temp files.
    	* gcc.dg/20050325-1.c: Clean up temp files.
    
    From-SVN: r104813

--- gcc/testsuite/gcc.dg/vect/pr23816-1.c
+++ gcc/testsuite/gcc.dg/vect/pr23816-1.c
@@ -1,4 +1,5 @@
 /* { dg-do compile } */
+/* { dg-require-effective-target vect_condition } */
 
 void
 foo (float a[32], float b[2][32])
@@ -7,3 +8,5 @@ foo (float a[32], float b[2][32])
   for (i = 0; i < 32; i++)
     a[i] = (b[0][i] > b[1][i]) ? b[0][i] : b[1][i];
 }
+
+/* { dg-final { cleanup-tree-dump "vect" } } */
