8dd0dafee8cd2f161826d270bcae8d841ed4394d
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-10-04 03:02:19 +0000

    PR 23445 	* tree-vrp.c (extract_range_from_assert): If the new numeric 	range created out of the assertion contradicts the existing 	numeric range of the ASSERT_EXPR variable, make the new range 	varying.
    testsuite/
    	PR 23445
    	* gcc.c-torture/compile/pr23445.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@104936 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr23445.c
@@ -0,0 +1,10 @@
+ struct buffer_head {
+   char *b_data;
+ };
+   void asfs_deletebnode( struct buffer_head *bhsec)  {
+     if (bhsec == 0)   {
+     void *bnc2 = (void *) bhsec->b_data;
+     if (bnc2)       return;
+     if (bhsec)       __brelse(bhsec);
+   }
+ }
