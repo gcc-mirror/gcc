a10f494b5b8a71293fb9eb347191a272b2212c98
geoffk <geoffk@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-10-05 01:04:30 +0000

    * gcc.dg/builtins-config.h: Only PowerPC Mac OS shipped without 	C99 functions.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@104972 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/builtins-config.h
+++ gcc/testsuite/gcc.dg/builtins-config.h
@@ -19,13 +19,13 @@
 /* FreeBSD before version 5 doesn't have the entire C99 runtime. */
 #elif defined(__netware__)
 /* NetWare doesn't have the entire C99 runtime.  */
-#elif (defined(__APPLE__) \
+#elif (defined(__APPLE__) && defined(__ppc__) \
        && ! defined (__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__))
 /* MacOS versions before 10.3 don't have many C99 functions.  
    But, if you're including this file, you probably want to test the
    newer behaviour, so: */
 #error forgot to set -mmacosx-version-min.
-#elif (defined(__APPLE__) \
+#elif (defined(__APPLE__) && defined(__ppc__) \
        && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1030)
 /* MacOS versions before 10.3 don't have many C99 functions.  */
 #else
