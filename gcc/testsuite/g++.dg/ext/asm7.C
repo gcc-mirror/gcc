227f3818fb395ea759fadb67d4e5aef40466ff0c
No Author <no-author@gcc.gnu.org>
2005-10-05 13:39:51 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r104990

---
+++ gcc/testsuite/g++.dg/ext/asm7.C
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+const int i = 0;
+
+void f(void)
+{
+  __asm__ __volatile__ ("" : "=m" (i)); /* { dg-error "read-only variable" } */
+}
+
+void g(const int set)
+{
+  __asm__ __volatile__ ("" : "=r" (set)); /* { dg-error "read-only parameter" } */
+}
+
+
