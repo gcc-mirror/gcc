31131ac1e6325565c3927b57ef3d4bd3bf6c042b
Paolo Carlini <pcarlini@suse.de>
2005-10-05 19:11:18 +0000

    re PR libstdc++/24198 (Remove from the testsuite gnu_char_type (or fix it))
    2005-10-05  Paolo Carlini  <pcarlini@suse.de>
    
    	PR libstdc++/24198
    	* testsuite/27_io/basic_filebuf/3.cc: Use __gnu_test::pod_ushort
    	instead.
    	* testsuite/27_io/basic_filebuf/seekoff/10132-2.cc: Likewise.
    	* testsuite/27_io/basic_filebuf/seekpos/10132-3.cc: Likewise.
    	* testsuite/27_io/basic_fstream/3.cc: Likewise; run the test.
    	* testsuite/27_io/basic_ifstream/3.cc: Likewise; run the test.
    	* testsuite/27_io/basic_ios/3.cc: Likewise.
    	* testsuite/27_io/basic_iostream/3.cc: Likewise; run the test.
    	* testsuite/27_io/basic_istream/3.cc: Likewise; run the test.
    	* testsuite/27_io/basic_istringstream/3.cc: Likewise; run the test.
    	* testsuite/27_io/basic_ofstream/3.cc: Likewise; run the test.
    	* testsuite/27_io/basic_ostream/3.cc: Likewise; run the test.
    	* testsuite/27_io/basic_ostringstream/3.cc: Likewise; run the test.
    	* testsuite/27_io/basic_streambuf/3.cc: Likewise.
    	* testsuite/27_io/basic_stringbuf/3.cc: Likewise.
    	* testsuite/27_io/basic_stringstream/3.cc: Likewise; run the test.
    
    From-SVN: r105010

--- libstdc++-v3/testsuite/27_io/basic_filebuf/seekpos/10132-3.cc
+++ libstdc++-v3/testsuite/27_io/basic_filebuf/seekpos/10132-3.cc
@@ -1,6 +1,6 @@
 // 2003-04-24 bkoz
 
-// Copyright (C) 2003 Free Software Foundation, Inc.
+// Copyright (C) 2003, 2004, 2005 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -23,98 +23,13 @@
 
 #include <fstream>
 #include <testsuite_hooks.h>
+#include <testsuite_character.h>
 
 // libstdc++/10132, add on
-class gnu_char_type
-{
-  unsigned long character;
-public:
-  // operator ==
-  bool
-  operator==(const gnu_char_type& __lhs) 
-  { return character == __lhs.character; }
-
-  // operator <
-  bool
-  operator<(const gnu_char_type& __lhs) 
-  { return character < __lhs.character; }
-
-  // default ctor
-  gnu_char_type() { }
-
-  // to_char_type
-  gnu_char_type(const unsigned long& __l) : character(__l) { } 
-
-  // to_int_type
-  operator unsigned long() const { return character; }
-};
-
-// char_traits specialization
-struct gnu_char_traits
-{
-  typedef gnu_char_type	char_type;
-  typedef long  		int_type;
-  typedef std::streamoff 	off_type;
-  typedef long   		state_type;
-  typedef std::fpos<state_type>	pos_type;
-  
-  static void 
-  assign(char_type& __c1, const char_type& __c2) { }
-  
-  static bool 
-  eq(const char_type& __c1, const char_type& __c2) { return true; }
-  
-  static bool 
-  lt(const char_type& __c1, const char_type& __c2) { return true; }
-  
-  static int 
-  compare(const char_type* __s1, const char_type* __s2, size_t __n)
-  { return 0; }
-  
-  static size_t
-  length(const char_type* __s) { return 0; }
-  
-  static const char_type* 
-  find(const char_type* __s, size_t __n, const char_type& __a)
-  { return __s; }
-  
-  static char_type* 
-  move(char_type* __s1, const char_type* __s2, size_t __n)
-  { return __s1; }
-  
-  static char_type* 
-  copy(char_type* __s1, const char_type* __s2, size_t __n)
-  { return __s1; }
-  
-  static char_type* 
-  assign(char_type* __s, size_t __n, char_type __a)
-  { return __s; }
-  
-  static char_type 
-  to_char_type(const int_type& __c)
-  { return char_type(); }
-  
-  static int_type 
-  to_int_type(const char_type& __c)
-  { return int_type(); }
-  
-  static bool 
-  eq_int_type(const int_type& __c1, const int_type& __c2)
-  { return true; }
-  
-  static int_type 
-  eof()
-  { return int_type(); }
-  
-  static int_type 
-  not_eof(const int_type& __c)
-  { return int_type(); }
-};
-
 void test07()
 {
   bool test __attribute__((unused)) = true;
-  typedef std::basic_filebuf<gnu_char_type, gnu_char_traits> gnu_filebuf;
+  typedef std::basic_filebuf<__gnu_test::pod_ushort> gnu_filebuf;
   
   try
     { 
