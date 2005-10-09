23e02b4464200c0218ce48750a4bae1aa9de56e3
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-10-09 10:34:47 +0000

    2005-10-09  Paolo Carlini  <pcarlini@suse.de>
    	PR libstdc++/24061 (issue 6.19)
    	* include/tr1/hashtable (struct node_const_iterator, struct
    	hashtable_const_iterator): New, add const variants to enable separate
    	overloadings for iterator and const_iterator in unordered_set and
    	unordered_multiset (as required by issue 6.19).
    	(class hashtable): Change the mutable_iterators template parameter
    	to constant_iterators and adjust throughout the logic.
    	(hashtable::insert(iterator, const value_type&), erase(iterator)
    	erase(iterator, iterator)): New, as per issue 6.19.
    	(hashtable::m_erase(node*, node**)): New, called by erase(iterator)
    	and erase(const_iterator).
    	(hashtable::Insert_Conv_Type): New, used by insert(iterator,
    	const value_type&) and insert(const_iterator, const value_type&)
    	to delegate the work to insert(const value_type&).
    	* include/tr1/unordered_map (class unordered_map, unordered_multimap):
    	Adjust typedefs.
    	* include/tr1/unordered_set (class unordered_set, unordered_multiset):
    	Likewise.
    	* testsuite/tr1/6_containers/unordered/erase/24061-map.cc: New.
    	* testsuite/tr1/6_containers/unordered/erase/24061-multimap.cc: New.
    	* testsuite/tr1/6_containers/unordered/erase/24061-multiset.cc: New.
    	* testsuite/tr1/6_containers/unordered/erase/24061-set.cc: New.
    	* testsuite/tr1/6_containers/unordered/insert/24061-map.cc: New.
    	* testsuite/tr1/6_containers/unordered/insert/24061-multimap.cc: New.
    	* testsuite/tr1/6_containers/unordered/insert/24061-multiset.cc: New.
    	* testsuite/tr1/6_containers/unordered/insert/24061-set.cc: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@105138 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/tr1/6_containers/unordered/insert/24061-multiset.cc
@@ -0,0 +1,58 @@
+// 2005-10-08  Paolo Carlini  <pcarlini@suse.de> 
+//
+// Copyright (C) 2005 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 6.3.4.5  Class template unordered_multiset
+
+#include <tr1/unordered_set>
+#include <string>
+#include <testsuite_hooks.h>
+
+// libstdc++/24061
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  
+  typedef std::tr1::unordered_multiset<std::string> Mset;
+  typedef Mset::iterator       iterator;
+  typedef Mset::const_iterator const_iterator;
+
+  Mset ms1;
+  
+  iterator it1 = ms1.insert(ms1.begin(), "all the love in the world");
+  VERIFY( ms1.size() == 1 );
+  VERIFY( *it1 == "all the love in the world" );
+  
+  const_iterator cit1(it1);
+  const_iterator cit2 = ms1.insert(cit1, "you know what you are?");
+  VERIFY( ms1.size() == 2 );
+  VERIFY( cit2 != cit1 );
+  VERIFY( *cit2 == "you know what you are?" );
+
+  iterator it2 = ms1.insert(it1, "all the love in the world");
+  VERIFY( ms1.size() == 3 );
+  VERIFY( it2 != it1 );
+  VERIFY( *it2 == "all the love in the world" );
+}
+  
+int main()
+{
+  test01();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/tr1/6_containers/unordered/insert/24061-set.cc
@@ -0,0 +1,58 @@
+// 2005-10-08  Paolo Carlini  <pcarlini@suse.de> 
+//
+// Copyright (C) 2005 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 6.3.4.3  Class template unordered_set
+
+#include <tr1/unordered_set>
+#include <string>
+#include <testsuite_hooks.h>
+
+// libstdc++/24061
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  
+  typedef std::tr1::unordered_set<std::string> Set;
+  typedef Set::iterator       iterator;
+  typedef Set::const_iterator const_iterator;
+
+  Set s1;
+  
+  iterator it1 = s1.insert(s1.begin(), "all the love in the world");
+  VERIFY( s1.size() == 1 );
+  VERIFY( *it1 == "all the love in the world" );
+  
+  const_iterator cit1(it1);
+  const_iterator cit2 = s1.insert(cit1, "you know what you are?");
+  VERIFY( s1.size() == 2 );
+  VERIFY( cit2 != cit1 );
+  VERIFY( *cit2 == "you know what you are?" );
+
+  iterator it2 = s1.insert(it1, "all the love in the world");
+  VERIFY( s1.size() == 2 );
+  VERIFY( it2 == it1 );
+  VERIFY( *it2 == "all the love in the world" );
+}
+  
+int main()
+{
+  test01();
+  return 0;
+}
