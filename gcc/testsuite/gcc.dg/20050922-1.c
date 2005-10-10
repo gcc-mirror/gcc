4d0e9d3b4e3a853e1d3ff1fb987caf20920b6880
No Author <no-author@gcc.gnu.org>
2005-10-10 14:43:10 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r105174

---
+++ gcc/testsuite/gcc.dg/20050922-1.c
@@ -0,0 +1,42 @@
+/* This revealed a bug when rotates are expanded into
+   two shifts.  */
+
+/* { dg-do run } */
+/* { dg-options "-O1 -std=c99" } */
+/* { dg-skip-if "" { *-*-solaris2.5.1 *-*-solaris2.[5-9] } "*" "" } */
+
+#include <stdint.h>
+
+extern void abort (void);
+
+uint32_t
+f (uint32_t *S, int j)
+{
+  uint32_t A, B, k, L[2] = {1234, 5678};
+  int i, m;
+
+  A = B = 0;
+  for (i = 0; i < j; i++)
+    {
+      k = (S[i] + A + B) & 0xffffffffL;
+      A = S[i] =
+      ((k << (3 & 0x1f)) | ((k & 0xffffffff) >> (32 - (3 & 0x1f)))); 
+
+      m = (int) (A + B);
+      k = (L[i] + A + B) & 0xffffffffL;
+      B = L[i] =
+	((k << (m & 0x1f)) | ((k & 0xffffffff) >> (32 - (m & 0x1f))));
+    }
+  return L[0] + L[1];
+}
+
+int
+main ()
+{
+  uint32_t S[2] = {0xffff, 0xffffff};
+
+  if (f (S,2)!= 1392607300)
+    abort();
+
+  return 0;
+}
