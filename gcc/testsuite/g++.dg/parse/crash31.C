dbe91deb9a7ab4b7952a2ae7e897e92b9b90915a
Nathan Sidwell <nathan@gcc.gnu.org>
2005-10-12 10:59:27 +0000

    re PR c++/19964 (ICE on invalid member declaration)
    	PR c++/19964
    	* stor-layout.c (place_field): Set DECL_FIELD_OFFSET and
    	DECL_FIELD_BIT_OFFSET of FIELD_DECLs, even if they have an invalid
    	type.
    cp:
    	PR c++/19964
    	* cp/class.c (walk_subobject_offsets): Don't walk error_mark_node.
    testsuite:
    	PR c++/19964
    	* g++.dg/parse/crash31.C: New.
    
    From-SVN: r105293

---
+++ gcc/testsuite/g++.dg/parse/crash31.C
@@ -0,0 +1,9 @@
+struct A
+{ // { dg-error "forward declaration" }
+  A : A; // { dg-error "expected|incomplete" }
+  A : B; // { dg-error "not declared|incomplete" }
+  A : A(); // { dg-error "undefined type|incomplete" }
+  A : B(); // { dg-error "function call|incomplete" }
+  A : A[]; // { dg-error "expected|array reference|incomplete" }
+  A : B[]; // { dg-error "not declared|expected|array reference|incomplete" }
+};
