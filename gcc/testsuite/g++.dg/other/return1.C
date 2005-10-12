89d74b4cf7710acabe7956c45f0618819b2229af
No Author <no-author@gcc.gnu.org>
2005-10-12 18:23:17 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r105316

---
+++ gcc/testsuite/g++.dg/other/return1.C
@@ -0,0 +1,15 @@
+// Copyright (C) 2005 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 12 Oct 2005 <nathan@codesourcery.com>
+
+// PR 21117:ICE after error
+// Origin: Andrew Pinski <pinskia@gcc.gnu.org>
+
+struct wxString;
+struct wxString* wxGetEmptyString();
+
+struct wxString GetHeader() // { dg-error "return type" "" }
+{
+  return *wxGetEmptyString();
+}
+
+
