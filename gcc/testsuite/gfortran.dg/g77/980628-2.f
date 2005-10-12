6c9ba145db17358a97145914013313040b8e87b5
Paul Thomas <pault@gcc.gnu.org>
2005-10-12 05:43:08 +0000

    re PR fortran/16404 (should reject invalid code with -pedantic -std=f95 ? (x8))
    2005-10-12  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/16404
    	PR fortran/20835
    	PR fortran/20890
    	PR fortran/20899
    	PR fortran/20900
    	PR fortran/20901
    	PR fortran/20902
    	PR fortran/24207
    	* gfortran.h: Prototype for gfc_add_in_equivalence.
    	* match.c (gfc_match_equivalence): Make a structure component
    	an explicit,rather than a syntax, error in an equivalence
    	group.  Call gfc_add_in_equivalence to add the constraints
    	imposed in check_conflict.
    	* resolve.c (resolve_symbol): Add constraints: No public
    	structures with private-type components and no public
    	procedures with private-type dummy arguments. Exclude use and
    	host associated symbols from the test for private objects in
    	a public namelist.
    	(resolve_equivalence_derived): Add constraint that prevents
    	a structure equivalence member from having a default
    	initializer.
    	(sequence_type): New static function to determine whether an
    	object is default numeric, default character, non-default
    	or mixed sequence. Add corresponding enum typespec.
    	(resolve_equivalence): Add constraints to equivalence groups
    	or their members: No more than one initialized member and
    	that different types are not equivalenced for std=f95.  All
    	the simple constraints have been moved to check_conflict.
    	* symbol.c (check_conflict): Simple equivalence constraints
    	added, including those removed from resolve_symbol.
    	(gfc_add_in_equivalence): New function to interface calls
    	match_equivalence to check_conflict.
    
    2005-10-12  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/16404
    	PR fortran/20835
    	PR fortran/20890
    	PR fortran/20899
    	PR fortran/20900
    	PR fortran/20901
    	PR fortran/20902
    	PR fortran/24207
    	gfortran.dg/equiv_constraint_1.f90: New test.
    	gfortran.dg/equiv_constraint_2.f90: New test.
    	gfortran.dg/equiv_constraint_3.f90: New test.
    	gfortran.dg/equiv_constraint_4.f90: New test.
    	gfortran.dg/equiv_constraint_5.f90: New test.
    	gfortran.dg/equiv_constraint_6.f90: New test.
    	gfortran.dg/equiv_constraint_7.f90: New test.
    	gfortran.dg/equiv_constraint_8.f90: New test.
    	gfortran.dg/private_type_1.f90: New test.
    	gfortran.dg/private_type_2.f90: New test.
    	gfortran.dg/private_type_3.f90: New test.
    	gfortran.dg/g77/980628-2.f, 980628-3.f, 980628-9.f,
    	980628-10.f: Assert std=gnu to permit mixing of
    	types in equivalence statements.
    
    From-SVN: r105287

--- gcc/testsuite/gfortran.dg/g77/980628-2.f
+++ gcc/testsuite/gfortran.dg/g77/980628-2.f
@@ -1,4 +1,5 @@
 c { dg-do run }
+c { dg-options "-std=gnu" }
 * g77 0.5.23 and previous had bugs involving too little space
 * allocated for EQUIVALENCE and COMMON areas needing initial
 * padding to meet alignment requirements of the system.
---
+++ gcc/testsuite/gfortran.dg/private_type_3.f90
@@ -0,0 +1,32 @@
+! { dg-do compile }
+! { dg-options "-O0" }
+! Tests the fix for PR24207 and the problems associated
+! with the fix for PR21986. In two cases, use associated
+! public symbols were taking on the default private access
+! attribute of the local namespace. In the third, a private
+! symbol was not available to a namelist in contained 
+! procedure in the same module.
+!
+! Based on the example in PR24207.
+!
+module a
+  implicit none
+  real b
+  type :: mytype
+    integer :: c
+  end type mytype
+end module a
+module c
+  use a
+  implicit none
+  public d
+  private
+  real x
+  contains
+     subroutine d (arg_t)   ! This would cause an error
+        type (mytype) :: arg_t
+        namelist /e/ b, x   ! .... as would this.
+        read(5,e)
+	arg_t%c = 42
+     end subroutine d
+end module c
