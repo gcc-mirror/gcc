3a3dd865127b059efd2c5503a57b5ac4d8232543
Razya Ladelsky <razya@gcc.gnu.org>
2005-10-12 09:31:34 +0000

    ipa-1.c: New test.
            * gcc.dg/ipa/ipa-1.c: New test.
            * gcc.dg/ipa/ipa-2.c: New test.
            * gcc.dg/ipa/ipa-3.c: New test.
            * gcc.dg/ipa/ipa-4.c: New test.
            * gcc.dg/ipa/ipa-5.c: New test.
            * gcc.dg/ipa/ipa.exp: New file.
    
    From-SVN: r105291

---
+++ gcc/testsuite/gcc.dg/ipa/ipa-2.c
@@ -0,0 +1,24 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 -fipa-cp -fdump-ipa-cp"  } */
+
+#include <stdio.h>
+int g (int b, int c)
+{
+  printf ("%d %d\n", b, c);
+}
+int f (int a)
+{
+  /* a is modified.  */
+  if (a++ > 0)
+    g (a, 3);
+}
+int main ()
+{
+  f (7);
+  return 0;	
+}
+
+
+/* { dg-final { scan-ipa-dump-times "versioned function" 2 "cp"  } } */
+/* { dg-final { scan-ipa-dump-times "propagating const" 2 "cp"  } } */
+/* { dg-final { cleanup-ipa-dump "cp" } } */
---
+++ gcc/testsuite/gcc.dg/ipa/ipa-4.c
@@ -0,0 +1,27 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 -fipa-cp -fdump-ipa-cp"  } */
+
+#include <stdio.h>
+int g (int b, int c)
+{
+  printf ("%d %d\n", b, c);
+}
+int f (int a)
+{
+  /* First and second parameter of g gets different values.  */ 
+     
+  if (a > 0)
+    g (a, 3);
+  else
+    g (a+1, 5); 	
+}
+int main ()
+{
+  f (7);
+  return 0;	
+}
+
+
+/* { dg-final { scan-ipa-dump-times "versioned function" 1 "cp"  } } */
+/* { dg-final { scan-ipa-dump-times "propagating const" 1 "cp"  } } */
+/* { dg-final { cleanup-ipa-dump "cp" } } */
