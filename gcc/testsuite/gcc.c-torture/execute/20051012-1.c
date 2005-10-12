8adac33500f774ff06f2900186eb09b42cb5e9c5
Adrian Straetling <straetling@de.ibm.com>
2005-10-12 20:30:46 +0000

    combine.c (make_extraction): Correct offset computation.
    	* combine.c (make_extraction): Correct offset computation.
    	* gcc.c-torture/execute/20051012-1.c: New test.
    
    From-SVN: r105330

---
+++ gcc/testsuite/gcc.c-torture/execute/20051012-1.c
@@ -0,0 +1,24 @@
+extern void abort (void);
+
+struct type 
+{
+  int *a;
+  
+  int b:16;
+  unsigned int p:9;
+} t;
+
+unsigned int 
+foo ()
+{
+  return t.p;
+}
+
+int 
+main (void)
+{
+  t.p = 8;
+  if (foo (t) != 8)
+    abort ();
+  return 0;
+}
