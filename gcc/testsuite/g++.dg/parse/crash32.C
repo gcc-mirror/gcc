e4785e43ff275f044ed193434c3df80f34825856
David Edelsohn <edelsohn@gnu.org>
2005-10-14 02:33:11 +0000

    re PR c++/23730 (ICE instead of reporting a call to a non-existent member function)
            PR c++/23730
            * g++.dg/parse/crash32.C: New test.
    
    From-SVN: r105389

---
+++ gcc/testsuite/g++.dg/parse/crash32.C
@@ -0,0 +1,13 @@
+// { dg-do compile }
+struct Visitor;
+
+struct Ast
+{
+  virtual void accept (Visitor& v);
+};
+
+void
+Ast::accept (Visitor& v)
+{
+  v (*this); // { dg-error "no match for call" "" }
+}
