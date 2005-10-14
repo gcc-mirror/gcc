ceaac04ea0dcb86aa6f96ff351a77c93bcc0a872
Nathan Sidwell <nathan@codesourcery.com>
2005-10-14 08:48:02 +0000

    re PR java/23620 (Segfault compiling inner interfaces)
    	PR java/23620
    	* class.c (make_class): Create empty binfo here.
    	(set_super_info): Only create binfo if we have superclasses.
    
    Co-Authored-By: Wil Mahan <wmahan@gmail.com>
    
    From-SVN: r105400

--- gcc/java/class.c
+++ gcc/java/class.c
@@ -316,6 +316,9 @@ make_class (void)
 {
   tree type;
   type = make_node (RECORD_TYPE);
+  /* Unfortunately we must create the binfo here, so that class
+     loading works.  */
+  TYPE_BINFO (type) = make_tree_binfo (0);
   MAYBE_CREATE_TYPE_TYPE_LANG_SPECIFIC (type);
 
   return type;
@@ -464,7 +467,8 @@ set_super_info (int access_flags, tree this_class,
   if (super_class)
     total_supers++;
 
-  TYPE_BINFO (this_class) = make_tree_binfo (total_supers);
+  if (total_supers)
+    TYPE_BINFO (this_class) = make_tree_binfo (total_supers);
   TYPE_VFIELD (this_class) = TYPE_VFIELD (object_type_node);
   if (super_class)
     {
