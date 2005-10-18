7edfc016601e65335667d922b70ad7f31434c312
Danny Smith <dannysmith@users.sourceforge.net>
2005-10-18 22:05:57 +0000

    re PR target/23589 (internal compiler error: in rest_of_handle_final, at toplev.c:2067)
    	PR target/23589
    	* g++.dg/ext/dllimport11.C: New.
    
    From-SVN: r105580

---
+++ gcc/testsuite/g++.dg/ext/dllimport11.C
@@ -0,0 +1,15 @@
+// PR target/23589
+// Template member functions do not get dllimport status of class.
+// { dg-do compile { target i?86-*-cygwin* i?86-*-mingw*} }
+
+struct __attribute__((dllimport)) Foo
+{
+  template <class T> Foo (T);
+};
+
+void a (int i)
+{
+  Foo f(i);
+}
+
+template <class T>  Foo::Foo (T) {}   // no dllimport warnings on definition.
