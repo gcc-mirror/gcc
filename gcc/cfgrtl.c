d80cae5d731af68b0d9e471cbae788c526712b4d
Eric Botcazou <ebotcazou@libertysurf.fr>
2005-10-19 16:45:48 +0200

    re PR middle-end/23199 (ICE in int_mode_for_mode, at stor-layout.c:251)
    	PR middle-end/23199
    	* cfgrtl.c (safe_insert_insn_on_edge): Use can_copy_p to detect
    	whether registers live on the edge can be saved/restored.
    
    From-SVN: r105620

--- gcc/cfgrtl.c
+++ gcc/cfgrtl.c
@@ -1459,15 +1459,8 @@ safe_insert_insn_on_edge (rtx insn, edge e)
   regset killed;
   rtx save_regs = NULL_RTX;
   unsigned regno;
-  int noccmode;
   enum machine_mode mode;
   reg_set_iterator rsi;
-
-#ifdef AVOID_CCMODE_COPIES
-  noccmode = true;
-#else
-  noccmode = false;
-#endif
 
   killed = ALLOC_REG_SET (&reg_obstack);
 
@@ -1494,7 +1487,8 @@ safe_insert_insn_on_edge (rtx insn, edge e)
       if (mode == VOIDmode)
 	return false;
 
-      if (noccmode && mode == CCmode)
+      /* Avoid copying in CCmode if we can't.  */
+      if (!can_copy_p (mode))
 	return false;
 	
       save_regs = alloc_EXPR_LIST (0,
