a1b1867a269955cf4b7a75f6e89ffa5705126061
Paul Thomas <pault@gcc.gnu.org>
2005-10-21 04:37:40 +0000

    re PR fortran/24440 (gfortran failed to build 187.facerec)
    2005-10-21  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/24440
    	* gfortran.dg/assumed_size_dt_dummy.f90: Add test for scalar case.
    
    From-SVN: r105716

--- gcc/testsuite/gfortran.dg/assumed_size_dt_dummy.f90
+++ gcc/testsuite/gfortran.dg/assumed_size_dt_dummy.f90
@@ -1,13 +1,17 @@
 ! { dg-do compile }
 ! PR20853 - No array size information for initializer.
+! PR24440 - patch for PR20853 caused a segfault at line 12.
 ! Contributed by Joost VandeVondele <jv244@cam.ac.uk>
 MODULE TEST
-TYPE init
-INTEGER :: I=0
-END TYPE init
+  TYPE init
+    INTEGER :: I=0
+  END TYPE init
 CONTAINS
-SUBROUTINE try(A) ! { dg-error "cannot have a default initializer" }
-  TYPE(init), DIMENSION(*), INTENT(OUT) :: A
-END SUBROUTINE try
+  SUBROUTINE try (A, B) ! { dg-error "cannot have a default initializer" }
+    TYPE(init), DIMENSION(*), INTENT(OUT) :: A
+    TYPE(init)              , INTENT(OUT) :: B ! PR24440 => segfault
+  END SUBROUTINE try
 END MODULE TEST
-END
+
+end
+
