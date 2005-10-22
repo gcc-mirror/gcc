0707f1299e781e4fb27075cb6286c859f8f7d290
No Author <no-author@gcc.gnu.org>
2005-10-22 17:09:04 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-4_0-branch'.
    From-SVN: r105793

---
+++ gcc/testsuite/gfortran.dg/der_pointer_4.f90
@@ -0,0 +1,11 @@
+! {dg-do compile }
+! PR 24426
+! Pointer-components of derived type with initialized components
+module crash
+  implicit none
+  type foo
+    integer :: i = 0
+    type (foo), pointer :: next
+  end type foo
+  type (foo) :: bar
+end module crash
