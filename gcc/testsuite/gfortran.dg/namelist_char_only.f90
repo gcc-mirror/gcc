b501210e44fed62d75857634d4add6c58e694f64
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-10-24 20:20:40 +0000

    2005-10-24  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/24416
    	* libgfortran/io/list_read.c (namelist_read): Exit with call to
    	free_saved () so that character strings do not accumulate.
    
    2005-10-24  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/24416
    	gfortran.dg/namelist_char_only.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@105862 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/namelist_char_only.f90
@@ -0,0 +1,29 @@
+! { dg-do run }
+! { dg-options "-O0" }
+! Test patch for PR24416.f90 - a used to come back from the read with var
+! prepended.
+!
+  IMPLICIT NONE
+  CHARACTER(len=10)  :: var = "hello"
+  character(len=10)  :: a = ""
+  NAMELIST /inx/ var
+
+  OPEN(unit=11, status='scratch')
+  write (11, *) "&INX"
+  write (11, *) "  var = 'goodbye'"
+  write (11, *) "&END"
+  rewind (11)
+
+  READ(11,NML=inx)
+  CLOSE(11)
+
+  OPEN(unit=11, status='scratch')
+  write (11, *) "alls_well"
+  rewind (11)
+
+  READ(11,*) a
+  CLOSE(11)
+
+  if (a /= "alls_well") call abort ()
+
+END
