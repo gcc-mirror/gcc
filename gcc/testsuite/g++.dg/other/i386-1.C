a22056479c1ec82073209cb070e310f3bddc7a59
Uros Bizjak <uros@kss-loka.si>
2005-10-25 15:01:12 +0200

    i386-1.C: Include i386-cpuid.h.
    	* g++.dg/other/i386-1.C: Include i386-cpuid.h.  Pass if
    	CPU has no SSE support.
    
    From-SVN: r105885

--- gcc/testsuite/g++.dg/other/i386-1.C
+++ gcc/testsuite/g++.dg/other/i386-1.C
@@ -3,13 +3,23 @@
 
 #include <xmmintrin.h>
 #include <stdio.h>
+#include "../../gcc.dg/i386-cpuid.h"
 
 int main(int argc, char** argv) {
   float a = 1.0f;
   float b = 2.0f;
   float c = 3.0f;
   float r;
-  
+
+  unsigned long cpu_facilities;
+
+  cpu_facilities = i386_cpuid ();
+
+  if ((cpu_facilities & (bit_MMX | bit_SSE | bit_CMOV))
+      != (bit_MMX | bit_SSE | bit_CMOV))
+    /* If host has no vector support, pass.  */
+    return 0;
+
   __m128 v = _mm_set_ps(a, b, c, 0);
   
   v = (__m128)_mm_srli_si128((__m128i)v, 4);
