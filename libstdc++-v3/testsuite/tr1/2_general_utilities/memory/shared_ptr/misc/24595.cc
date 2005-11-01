2747345455a558f5ac0c6742e219715b062d19a9
Paolo Carlini <pcarlini@suse.de>
2005-11-01 11:01:40 +0000

    re PR libstdc++/24595 (std::tr1::get_deleter not declared)
    2005-11-01  Paolo Carlini  <pcarlini@suse.de>
    
    	PR libstdc++/24595
    	* include/tr1/boost_shared_ptr.h (shared_ptr<>::get_deleter):
    	Move out of shared_ptr.
    	* testsuite/tr1/2_general_utilities/memory/shared_ptr/misc/24595.cc:
    	New.
    
    From-SVN: r106321

---
+++ libstdc++-v3/testsuite/tr1/2_general_utilities/memory/shared_ptr/misc/24595.cc
@@ -0,0 +1,39 @@
+// Copyright (C) 2005 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// TR1 2.2.2 Template class shared_ptr [tr.util.smartptr.shared]
+
+#include <tr1/memory>
+#include <testsuite_hooks.h>
+
+using std::tr1::get_deleter;
+
+// libstdc++/24595
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  std::tr1::shared_ptr<int> sp;
+  VERIFY( !get_deleter<void(*)(int*)>(sp) );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
