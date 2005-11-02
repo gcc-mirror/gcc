fb530c72edd80bfdb02dd3a2f17e8df61b097859
Jan Hubicka <jh@suse.cz>
2005-11-03 00:21:22 +0100

    re PR target/23303 (4.1 generates sall + addl instead of leal)
    	PR target/23303
    	* i386.md: Add peep2 for simplyfing array accesses.
    	* gcc.dg/i386-lea.c: New test
    
    From-SVN: r106406

---
+++ gcc/testsuite/gcc.dg/i386-lea.c
@@ -0,0 +1,14 @@
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-require-effective-target ilp32 } */
+/* { dg-options "-O2 -march=i686" } */
+/* { dg-final { scan-assembler "leal" } } */
+typedef struct {
+  char **visbuf;
+  char **allbuf;
+} TScreen;
+
+void
+VTallocbuf(TScreen *screen, unsigned long savelines)
+{
+  screen->visbuf = &screen->allbuf[savelines];
+}
