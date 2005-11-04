cc9012f7c74cf3452cdb1b6cde3cf6455e889cd3
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-11-04 00:23:01 +0000

    2005-11-03  Andrew Pinski  <pinskia@physics.uc.edu>
            PR preprocessor/22042
            * macro.c (_cpp_builtin_macro_text): Lower the needed max
            buffer size.
            (cpp_quote_string): Don't octalify non printable
            charactors.
    2005-11-03  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR preprocessor/22042
            * gcc.dg/cpp/strify4.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@106463 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/cpp/strify4.c
@@ -0,0 +1,25 @@
+/* { dg-do run } */
+/* Tests we stringify without changing unprintable characts.  
+
+   Andrew Pinski */
+
+extern int strcmp (const char *, const char *);
+extern int puts (const char *);
+extern void abort (void);
+#define err(str) do { puts(str); abort(); } while (0)
+
+
+#define S(X) S2(X)
+#define S2(X) #X
+#define TAB "	" /* Note there is a tab character here. */
+
+int main (int argc, char *argv[])
+{
+  /* The space before "bar" here is vital.  */
+  char a[] = S(S(TAB));
+
+  if (strcmp (a, "\"\\\"	\\\"\""))
+    err ("stringification caused octal");
+
+  return 0;
+}
