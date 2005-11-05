ae915c82ab340223fecf56c9c4d130f9b3e71eec
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-11-05 20:34:08 +0000

    * decl2.c, init.c, typeck.c: Fix comment typos.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@106533 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/init.c
+++ gcc/cp/init.c
@@ -1604,7 +1604,7 @@ constant_value_1 (tree decl, bool integral_p)
 	      : (!TREE_CONSTANT (init)
 		 /* Do not return an aggregate constant (of which
 		    string literals are a special case), as we do not
-		    want to make inadvertant copies of such entities,
+		    want to make inadvertent copies of such entities,
 		    and we must be sure that their addresses are the
 		    same everywhere.  */
 		 || TREE_CODE (init) == CONSTRUCTOR
