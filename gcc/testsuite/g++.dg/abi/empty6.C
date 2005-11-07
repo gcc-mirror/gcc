7888e995f8d95f5c9a2f8ac188a9b390d1f2ed49
Ulrich Weigand <uweigand@de.ibm.com>
2005-11-07 11:59:20 +0000

    * g++.dg/abi/empty6.C (B): Align to twice pointer size.
    From-SVN: r106590

--- gcc/testsuite/g++.dg/abi/empty6.C
+++ gcc/testsuite/g++.dg/abi/empty6.C
@@ -5,11 +5,10 @@ struct A {};
 struct B {
   A a; // { dg-warning "empty" "" { xfail mmix-*-* } }
   virtual void f () {}
-} __attribute__((aligned(8)));
+} __attribute__((aligned(2 * sizeof (void *))));
 /* The preceding attribute is necessary on targets with
-   BIGGEST_ALIGNMENT <= 32 to trigger the warning, as otherwise a 32 bit
-   offset is split into DECL_FIELD_OFFSET 4 and DECL_FIELD_BIT_OFFSET 0,
+   BIGGEST_ALIGNMENT <= POINTER_SIZE to trigger the warning, as
+   otherwise the offset of 'a' (i.e. POINTER_SIZE) is split into a
+   non-zero DECL_FIELD_OFFSET and a zero DECL_FIELD_BIT_OFFSET,
    and then there is no discrepancy between DECL_FIELD_OFFSET and
-   byte_position to warn about.
-   On the other hand 64-bit targets for example, generally need a larger
-   requested alignment to get the intended warning.  */
+   byte_position to warn about.  */
