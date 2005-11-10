400de984ee70100d15425d5019de2ccab6fd5c79
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-11-10 22:24:28 +0000

    2005-11-10  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/24655
    	PR fortran/24755
    	* match.c (recursive_stmt_fcn): Add checks that symtree exists
    	for the expression to weed out inline intrinsic functions and
    	parameters.
    
    	PR fortran/24409
    	* module.c (mio_symtree_ref): Correct the patch of 0923 so that
    	a symbol is not substituted for by a the symbol for the module
    	itself and to prevent the promotion of a formal argument.
    
    2005-11-10  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/24655
    	PR fortran/24755
    	* gfortran.dg/recursive_statement_functions.f90: Add statement 
    	functions using inline intrinsic functions and parameters to test
    	that they no longer seg-fault.
    
    	PR fortran/24409
    	gfortran.dg/nested_modules_4.f90: New test.
    	gfortran.dg/nested_modules_5.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@106756 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/nested_modules_5.f90
@@ -0,0 +1,26 @@
+! { dg-do compile }
+!
+! Test for supplementary fix to PR24409 - the name clash between the module
+! variable and the interface formal argument would cause an ICE.
+!
+! Contributed by Paul Thomas  <pault@gcc.gnu.org>
+!
+module anything 
+  interface
+    function  lc(string )
+      character(len=*), intent(in)  :: string 
+      character(len=len(string ))    :: lc
+    end function lc
+  end interface
+  character(len=12) :: string
+end module anything
+
+module serial
+  use anything
+end module serial
+
+  use serial
+  use anything
+  character*15  :: buffer  
+  buffer = lc ("Have a Nice DAY")
+  end
--- gcc/testsuite/gfortran.dg/recursive_statement_functions.f90
+++ gcc/testsuite/gfortran.dg/recursive_statement_functions.f90
@@ -1,16 +1,32 @@
 ! { dg-do compile }
 ! PR20866 - A statement function cannot be recursive.
 ! Contributed by Joost VandeVondele <jv244@cam.ac.uk>
-  INTEGER :: i, st1, st2, st3
+!
+! Modified 20051110 to check that regressions PR24655 and PR24755
+! are fixed. Thanks to pavarini@pv.infn.it and tdeutsch@cea.fr for
+! the tests.
+!
+  INTEGER :: i, st1, st2, st3, lambda, n
   REAL :: x, z(2,2)
-  character*8 :: ch
+  character(8) :: ch
+  real(8)   :: fi, arg, sigma, dshpfunc
+  real(8), parameter :: one=1d0
 !
 ! Test check for recursion via other statement functions, string
 ! length references, function actual arguments and array index
 ! references.
-  st1(i)=len(ch(st2(1):8))
-  st2(i)=max (st3(1), 4)
-  st3(i)=2 + cos (z(st1 (1), i)) ! { dg-error "is recursive" }
-  write(6,*) st1(1)
+!
+  st1 (i) = len (ch(st2 (1):8))
+  st2 (i) = max (st3 (1), 4)
+  st3 (i) = 2 + cos (z(st1 (1), i)) ! { dg-error "is recursive" }
+!
+! Test the two regressions.
+!
+  fi (n) = n *one
+  dshpfunc (arg)=-lambda/sigma*(arg/sigma)**(lambda-1)*exp(-(arg/sigma)**lambda)
+!
+! References to each statement function.
+!
+  write(6,*) st1 (1), fi (2), dshpfunc (1.0_8)
   END
 
