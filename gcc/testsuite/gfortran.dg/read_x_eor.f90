2999dd1780b2f85394f71982085dbe149c69e9ab
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2005-11-11 20:31:06 +0000

    re PR libfortran/24719 (Nonadvancing read does not read more than 1 line)
    2005-11-11  Jerry DeLisle <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/24719
    	gfortran.dg/read_noadvance.f90: New test.
    	
    	PR libgfortran/24699
    	gfortran.dg/fmt_t_2.f90: New test.
    	
    	PR libgfortran/24785
    	gfortran.dg/read_x_eor.f90: New test.
    	
    	PR libgfortran/24584
    	gfortran.dg/namelist_empty.f90: Rename test.
    
    	PR libgfortran/24489
    	gfortran/read_eor.f90: Rename test.
    
    From-SVN: r106798

---
+++ gcc/testsuite/gfortran.dg/read_x_eor.f90
@@ -0,0 +1,10 @@
+! { dg-do run }
+! ( dg-output "^" }
+!
+! Test fix for pr24785 - EOR used to scrub the 2X.
+! Reduced from PR example submitted by Harald Anlauf <anlauf@gmx.de>
+!
+     program x_with_advance_bug
+     write (*,'(A,2X)',  advance="no") "<"
+     write (*,'(A)') ">" ! { dg-output "<  >" }
+     end
