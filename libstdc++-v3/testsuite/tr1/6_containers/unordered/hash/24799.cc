b297d58a98b1bd7bb2961b51ea311e1e7ff79375
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-11-11 20:10:20 +0000

    2005-11-11  Paolo Carlini  <pcarlini@suse.de>
    	PR libstdc++/24799
    	* include/tr1/functional (hash): Inherit from std::unary_function.
    	* testsuite/tr1/6_containers/unordered/hash/24799.cc: New.
    
    	PR libstdc++/24805
    	* include/tr1/boost_shared_ptr.h (swap(shared_ptr<>&, shared_ptr<>&),
    	swap(weak_ptr<>&, weak_ptr<>&)): Move inside namespace tr1.
    	* testsuite/tr1/2_general_utilities/memory/shared_ptr/modifiers/
    	24805.cc: New.
    
    	PR libstdc++/24809
    	* include/tr1/type_traits (__is_polymorhpic_helper): Adjust destructor.
    	* testsuite/tr1/4_metaprogramming/type_properties/is_polymorphic/
    	24809.cc: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@106796 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/tr1/6_containers/unordered/hash/24799.cc
@@ -0,0 +1,74 @@
+// 2005-11-11  Paolo Carlini  <pcarlini@suse.de>
+//
+// Copyright (C) 2005 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 6.3.3 Class template hash
+
+#include <tr1/functional>
+#include <string>
+#include <tr1/type_traits>
+#include <testsuite_tr1.h>
+#include <testsuite_hooks.h>
+
+template<typename T>
+  void
+  do_test()
+  {
+    bool test __attribute__((unused)) = true;
+
+    using std::tr1::is_same;
+    using __gnu_test::test_relationship;
+    
+    typedef typename std::tr1::hash<T>::argument_type  argument_type;
+    typedef typename std::tr1::hash<T>::result_type    result_type;
+    
+    VERIFY( (test_relationship<is_same, argument_type, T>(true)) );
+    VERIFY( (test_relationship<is_same, result_type, std::size_t>(true)) );
+  }
+  
+// libstdc++/24799
+void test01()
+{
+  do_test<bool>();
+  do_test<char>();
+  do_test<signed char>();
+  do_test<unsigned char>();
+  do_test<short>();
+  do_test<int>();
+  do_test<long>();
+  do_test<unsigned short>();
+  do_test<unsigned int>();
+  do_test<unsigned long>();
+  do_test<int*>();
+  do_test<std::string>();
+  do_test<float>();
+  do_test<double>();
+  do_test<long double>();
+
+#ifdef _GLIBCXX_USE_WCHAR_T
+  do_test<wchar_t>();
+  do_test<std::wstring>();
+#endif
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
