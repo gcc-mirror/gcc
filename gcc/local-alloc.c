301a8f9577f730dcb680760b84e258e62cafd01f
Zdenek Dvorak <dvorakz@suse.cz>
2005-11-11 14:38:07 +0100

    re PR rtl-optimization/22509 (elemental.f90 testsuite failure (-fweb))
    	PR rtl-optimization/22509
    	* local-alloc.c (memref_used_between_p): Check whether a function call
    	could not reference the memref.
    
    From-SVN: r106783

--- gcc/local-alloc.c
+++ gcc/local-alloc.c
@@ -762,8 +762,19 @@ memref_used_between_p (rtx memref, rtx start, rtx end)
 
   for (insn = NEXT_INSN (start); insn != NEXT_INSN (end);
        insn = NEXT_INSN (insn))
-    if (INSN_P (insn) && memref_referenced_p (memref, PATTERN (insn)))
-      return 1;
+    {
+      if (!INSN_P (insn))
+	continue;
+      
+      if (memref_referenced_p (memref, PATTERN (insn)))
+	return 1;
+
+      /* Nonconst functions may access memory.  */
+      if (CALL_P (insn)
+	  && (! CONST_OR_PURE_CALL_P (insn)
+	      || pure_call_p (insn)))
+	return 1;
+    }
 
   return 0;
 }
