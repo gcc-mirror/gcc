1f02bd26116d0716ceb5cbc2740bd37ae0592736
Kaz Kojima <kkojima@gcc.gnu.org>
2005-11-12 09:28:24 +0000

    pr24445.c: Fix for 64-bit targets.
    	* gcc.dg/pr24445.c: Fix for 64-bit targets.
    
    From-SVN: r106822

--- gcc/testsuite/gcc.dg/pr24445.c
+++ gcc/testsuite/gcc.dg/pr24445.c
@@ -1,6 +1,6 @@
 /* { dg-do compile { target fpic } } */
 /* { dg-options "-O1 -fpic" } */
-extern int bar (void) __attribute__ ((__pure__));
+extern int *bar (void) __attribute__ ((__pure__));
 extern char *baz;
 void
 foo (void)
