1a89bebe7c3ee8da5b95ceba5c4ae76ac7bd2322
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2005-11-12 22:31:18 +0000

    Backport PRs 24584, 24699, 24719, 24785
    2005-11-12  Jerry DeLisle <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/24719
    	*gfortran.dg/read_noadvance.f90: New test.
    	
    	PR libgfortran/24699
    	*gfortran.dg/fmt_t_2.f90: New test.
    	
    	PR libgfortran/24785
    	*gfortran.dg/read_x_eor.f90: New test.
    	
    	PR libgfortran/24584
    	*gfortran.dg/namelist_empty.f90: Rename test.
    	
    	*gfortran.dg/f77/1832.f: Backport from 4.1.
    
    From-SVN: r106839

--- gcc/testsuite/gfortran.dg/g77/1832.f
+++ gcc/testsuite/gfortran.dg/g77/1832.f
@@ -1,9 +1,7 @@
 c { dg-do run }
-      character*120 file
       character*5   string
-      file = "c:/dos/adir/bdir/cdir/text.doc"
-      write(string, *) "a ", file
+      write(string, *) "a "
       if (string .ne. ' a') call abort
 C-- The leading space is normal for list-directed output
-C-- "file" is not printed because it would overflow "string".
+
       end
---
+++ gcc/testsuite/gfortran.dg/read_noadvance.f90
@@ -0,0 +1,21 @@
+! { dg-do run }
+! pr24719, non-advancing read should read more than one line
+! test contributed by jerry delisle <jvdelisle@gcc.gnu.org>
+      implicit none
+      character(1) :: chr
+      character(20) :: correct = 'foo: bar 123abc'
+      integer :: i
+      open(unit = 11, status = "scratch", action="readwrite")
+      write(11,'(a)') "foo: bar"
+      write(11,'(a)') "123abc"
+      rewind(11)
+      i = 0
+      do
+        i = i + 1
+10      read(unit = 11, fmt = '(a)', advance = 'no', end = 99, eor = 11) chr
+        if (chr.ne.correct(i:i)) call abort()
+        cycle
+11      continue
+      end do
+99    close(11)
+      end
