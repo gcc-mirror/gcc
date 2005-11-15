32e209e4f9619ea1c18a56d758d445326c5421b4
Cyrille Comar <comar@adacore.com>
2005-11-15 15:05:45 +0100

    gnat_ugn.texi: Document that -fstack-check is needed for strict compliance with the Ada 95...
    2005-11-14  Cyrille Comar  <comar@adacore.com>
    	    Robert Dewar  <dewar@adacore.com>
    	    Vincent Celier  <celier@adacore.com>
    	    Ben Brosgol  <brosgol@adacore.com>
    	    Jose Ruiz  <ruiz@adacore.com>
    	    Pascal Obry  <obry@adacore.com>
    
    	* gnat_ugn.texi: 
    	Document that -fstack-check is needed for strict compliance with the
    	Ada 95 Reference Manual.
    	Correct reference to VAX systems to meet HP guidelines
    	Add documentation for new gnatmake switches -vl, -vm and -vh
    	Replace DEC Ada by HP Ada
    	Replace DIGITAL by HP
    	Remove empty section on tools in compatibility section
    	Clarify the Windows convention semantics.
    	Document the Win32 calling convention.
    	The Stdcall, Win32 and DLL convention are synonyms.
    	Add a note in -gnatR description about zero size record components
    	Note on new >= and <= warnings for -gnatwc
    	Document that equal sign after -gnatm is optional.
    	Note that strip is working fine on DLL built with a Library
    	Project. The restriction apply only to DLL built with gnatdll.
    	Update section about the way to debug a DLL.
    	Update information about the DLL convention.
    	Document -C switch for gnatprep
    	Document new attribute Library_ALI_Dir
    	Update elaboration doc to include implicit Elaborate pragmas now
    	generated for subprogram instantiations.
    	Document limitation on executable names that include spaces for --GCC,
    	--GNATBIND, and --GNATLINK switches.
    	Document that -w causes -gnatws to be added at start of gcc switches
    
    	* gnat_rm.texi: Document that -mieee is needed for generating infinite
    	and NaN values in case of overflow on machines that are not fully
    	compliant with the IEEE floating-point standard.
    	Create a section describing the set of compiler options needed for
    	strict compliance with the Ada 95 Reference Manual.
    	Add documentation for pragma Obsolescent applied to a package
    	Clarify potential issues of mixed language programs related to the
    	I/O buffering enabling in the elaboration of the GNAT runtime.
    	Add extra documentation for pragma Restrictions (No_Elaboration_Code)
    	This documentation only patch adds extra documentsion for pragma
    	Restrictions (No_Elaboration_Code), explaining why it is not possible
    	to document this restriction in terms of allowed source constructs.
    	Document string literal form of pragma Warnings
    	Document new attribute Library_ALI_Dir
    	Add documentation on stable attributes in project files that was missing
    
    	* gnat-style.texi: Indicate that paragraphs within a single comment
    	should be separated by empty comment lines
    
    	* ug_words: Added replacements for -gnat95 and -gnat05 (/95 and
    	/05 respectively)
    
    	* bindusg.adb: Minor cleanup, put -m before -M for consistency
    
    From-SVN: r107015

--- gcc/ada/bindusg.adb
+++ gcc/ada/bindusg.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                B o d y                                   --
 --                                                                          --
---          Copyright (C) 1992-2005 Free Software Foundation, Inc.          --
+--          Copyright (C) 1992-2005, Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -132,15 +132,15 @@ begin
    Write_Str ("renamed to xyzinit/final, implies -n");
    Write_Eol;
 
-   --  Line for -M switch
-
-   Write_Str ("  -Mxyz     Rename generated main program from main to xyz");
-   Write_Eol;
-
    --  Line for -m switch
 
    Write_Str ("  -mnnn     Limit number of detected error");
    Write_Str ("s to nnn (1-999999)");
+   Write_Eol;
+
+   --  Line for -M switch
+
+   Write_Str ("  -Mxyz     Rename generated main program from main to xyz");
    Write_Eol;
 
    --  Line for -n switch
