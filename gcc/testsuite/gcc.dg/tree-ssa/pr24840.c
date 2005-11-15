4a34401c65d86992657daf94f06c023e98e446d4
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-11-15 02:59:03 +0000

    PR 24840 	* tree-vrp.c (infer_value_range): Return false if STMT is a 	block terminator and its basic block has no successors.
    testsuite/
    
    	PR 24840
    	* gcc.dg/tree-ssa/pr24840.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@106930 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr24840.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+
+typedef struct
+{
+  int tag, y;
+} Object;
+
+void Err_Handler () __attribute__ ((__noreturn__));
+
+int
+P_Error (int argc, Object * argv)
+{
+    if (((argv[1]).tag >> 1) != 11)
+      Wrong_Type (argv[1], 11);
+  Err_Handler (argv[0], argv[1], argc - 2, argv + 2);
+}
