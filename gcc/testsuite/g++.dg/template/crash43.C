9438c9cd72c095b6d1225ccc90463417b6bfb241
Mark Mitchell <mark@codesourcery.com>
2005-11-15 17:44:28 +0000

    re PR c++/24687 (ICE after error)
    	PR c++/24687
    	* g++.dg/template/crash43.C: Remove duplicate copy of test.
    
    From-SVN: r107030

--- gcc/testsuite/g++.dg/template/crash43.C
+++ gcc/testsuite/g++.dg/template/crash43.C
@@ -6,11 +6,3 @@ extern "C" {
     enum {
       __value = (sizeof(__gnu_internal::__test_type<_Tp>(0)))}; // { dg-error "declared|expected" }
 
-// PR c++/24687
-
-extern "C" {
-  template<typename _Tp>  // { dg-error "C" }   
-  struct __is_pod {
-    enum {
-      __value = (sizeof(__gnu_internal::__test_type<_Tp>(0)))}; // { dg-error "declared|expected" }
-
