1f28ca87c1aa7f2c529c5eb92ce9f8d1289d7a55
Kean Johnston <jkj@sco.com>
2005-11-16 06:05:55 +0000

    ansidecl.h: Back-ported ATTRIBUTE_FPTR_PRINTF macros from head.
    include:
    	* ansidecl.h: Back-ported ATTRIBUTE_FPTR_PRINTF macros from head.
    
    libcpp:
    	* include/cpplib.h (struct cpp_callbacks): Make the (*error)
    	function pointer use ATTRIBUTE_FPTR_PRINTF so that versions
    	of gcc prior to 3.1 can bootstrap again.
    
    From-SVN: r107067

--- libcpp/include/cpplib.h
+++ libcpp/include/cpplib.h
@@ -454,7 +454,7 @@ struct cpp_callbacks
   /* Called to emit a diagnostic if client_diagnostic option is true.
      This callback receives the translated message.  */
   void (*error) (cpp_reader *, int, const char *, va_list *)
-       ATTRIBUTE_PRINTF(3,0);
+       ATTRIBUTE_FPTR_PRINTF(3,0);
 };
 
 /* Chain of directories to look for include files in.  */
