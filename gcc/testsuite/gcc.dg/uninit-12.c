f9102bb90bedf3530062aabff746853679144ada
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-11-16 23:43:39 +0000

    PR middle-end/23497         * tree-ssa.c (warn_uninitialized_var): Skip real and imaginary         parts of an SSA_NAME.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@107107 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/uninit-12.c
@@ -0,0 +1,12 @@
+/* PR 23497 */
+/* { dg-do compile } */
+/* { dg-options "-O -Wuninitialized" } */
+
+typedef _Complex float C;
+C foo()
+{
+  C f;
+  __real__ f = 0;
+  __imag__ f = 0;
+  return f;
+}
