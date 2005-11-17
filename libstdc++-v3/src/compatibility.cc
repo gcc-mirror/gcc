069fa9335fb98c207811e6968223d8475f92909e
geoffk <geoffk@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-11-17 20:10:51 +0000

    /Network/Servers/hills/Volumes/capanna/gkeating/l
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@107131 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/src/compatibility.cc
+++ libstdc++-v3/src/compatibility.cc
@@ -361,3 +361,28 @@ namespace std
 #undef _GLIBCXX_APPLY_SYMVER
 
 #endif
+
+#ifdef __APPLE__
+#if (defined(__ppc__) || defined (__ppc64__)) && defined (PIC)
+/* __eprintf shouldn't have been made visible from libstdc++, or
+   anywhere, but on Mac OS X 10.4 it was defined in
+   libstdc++.6.0.3.dylib; so on that platform we have to keep defining
+   it to keep binary compatibility.  We can't just put the libgcc
+   version in the export list, because that doesn't work; once a
+   symbol is marked as hidden, it stays that way.  */
+
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+extern "C" void
+__eprintf (const char *string, const char *expression,
+	   unsigned int line, const char *filename)
+{
+  fprintf (stderr, string, expression, line, filename);
+  fflush (stderr);
+  abort ();
+}
+#endif
+#endif /* __APPLE__ */
