925196ed525e3f05b74d54391edd3a198986e035
Zdenek Dvorak <dvorakz@suse.cz>
2005-11-18 11:27:50 +0100

    tree-scalar-evolution.c (expression_expensive_p): New function.
    	* tree-scalar-evolution.c (expression_expensive_p): New function.
    	(scev_const_prop): Use compute_overall_effect_of_inner_loop.
    
    	* gcc.dg/tree-ssa/loop-14.c: New test.
    
    From-SVN: r107170

---
+++ gcc/testsuite/gcc.dg/tree-ssa/loop-14.c
@@ -0,0 +1,19 @@
+/* A test for final value replacement.  */
+
+/* { dg-options "-O2 -fdump-tree-vars" } */
+
+int foo(void);
+
+int bla(void)
+{
+  int i, j = foo ();
+
+  for (i = 0; i < 100; i++, j++)
+    foo ();
+
+  /* Should be replaced with return j0 + 100;  */
+  return j;
+}
+
+/* { dg-final { scan-tree-dump-times "\\+ 100" 1 "vars" } } */
+/* { dg-final { cleanup-tree-dump "vars" } } */
