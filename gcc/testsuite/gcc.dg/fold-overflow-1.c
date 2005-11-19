7e3537fb67ae445bbb9e5cf8ec6e49900f3f87d5
hp <hp@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-11-19 08:39:25 +0000

    * gcc.dg/fold-overflow-1.c: Adjust for float output for mmix-*-*.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@107216 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/fold-overflow-1.c
+++ gcc/testsuite/gcc.dg/fold-overflow-1.c
@@ -15,4 +15,5 @@ float foo2(void)
   return 1.0f/0.0f;
 }
 
-/* { dg-final { scan-assembler-times "2139095040" 2 } } */
+/* { dg-final { scan-assembler-times "2139095040" 2 { target { ! mmix-*-* } } } } */
+/* { dg-final { scan-assembler-times "#7f800000" 2 { target mmix-*-* } } } */
