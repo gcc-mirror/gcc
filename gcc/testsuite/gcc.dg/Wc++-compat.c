b8b47f42511415601638a9efde76b54c39729c32
Gabriel Dos Reis <gdr@integrable-solutions.net>
2005-11-23 07:07:33 +0000

    re PR c/21668 (gratuitous warning about "extern const" with initializer)
            PR c/21668
            * c-decl.c (grokdeclarator): Don't warn for 'extern const' when
            compiling at the intersection of C and C++.
    
    testsuite/
            PR c/21668
            * gcc.dg/Wc++-compat.c: New.
            * gcc.dg/Wno-c++-compat.c: New.
    
    From-SVN: r107401

---
+++ gcc/testsuite/gcc.dg/Wc++-compat.c
@@ -0,0 +1,9 @@
+/* Copyright (C) 2005  Free Software Foundation.
+
+   by Gabriel Dos Reis  <gdr@integrable-solutions.net>  */
+
+/* { dg-do compile }  */
+/* { dg-options "-Wc++-compat" } */
+
+extern const int foo = 42;
+
