931c6823a52096054e26c6bc9b11adc4b9abfbb0
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-11-25 05:05:26 +0000

    2005-11-25  Andrew Pinski  <pinskia@physics.uc.edu>
            PR middle-end/24989
            * fold-const.c (fold_build): Convert bool_var != 1 and
            bool_var == 0 to !bool_var.
    
    2005-11-24  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR middle-end/24989
            * gcc.dg/tree-ssa/bool-10.c: New test.
            * gcc.dg/tree-ssa/bool-11.c: New test.
            * gcc.dg/tree-ssa/bool-7.c: Un-xfail.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@107488 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/bool-10.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-optimized" } */
+
+int f(_Bool x)
+{
+  return (x != 1);
+}
+
+/* There should be no != 1 which is produced by the front-end as
+   bool_var != 1 is the same as !bool_var. */
+/* { dg-final { scan-tree-dump-times "!= 1" 0 "optimized"} } */
+/* { dg-final { scan-tree-dump-times "!x" 1 "optimized"} } */
+
+/* { dg-final { cleanup-tree-dump "optimized" } } */
--- gcc/testsuite/gcc.dg/tree-ssa/bool-7.c
+++ gcc/testsuite/gcc.dg/tree-ssa/bool-7.c
@@ -11,8 +11,7 @@ int f(_Bool x)
   return y;
 }
 
-/* There should be no != 1. Though PHI-OPT or invert_truth does not
-   fold its tree.  */
-/* { dg-final { scan-tree-dump-times "!= 1" 0 "optimized" { xfail *-*-* } }  }*/
+/* There should be no != 1. Fold changes x != 1 to ! x.  */
+/* { dg-final { scan-tree-dump-times "!= 1" 0 "optimized" } } */
 
 /* { dg-final { cleanup-tree-dump "optimized" } } */
