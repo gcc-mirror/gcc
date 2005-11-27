8edbdaf7352ccfcf7a18650dca29695038d2978c
Eric Christopher <echristo@gcc.gnu.org>
2005-11-27 03:25:42 +0000

    intmax_t-1.c: Remove mips xfail.
    2005-11-26  Eric Christopher  <echristo@apple.com>
    
            * gcc.dg/intmax_t-1.c: Remove mips xfail.
    
    From-SVN: r107562

--- gcc/testsuite/gcc.dg/intmax_t-1.c
+++ gcc/testsuite/gcc.dg/intmax_t-1.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-Wall" } */
-/* { dg-error "" "" { target *-*-solaris2.5.1 mips*-*-elf* *arm*-*-*elf* xscale*-*-elf* xtensa-*-elf* } 0 } */
+/* { dg-error "" "" { target *-*-solaris2.5.1 *arm*-*-*elf* xscale*-*-elf* xtensa-*-elf* } 0 } */
 
 /* Compile with -Wall to get a warning if built-in and system intmax_t don't
    match.  */
