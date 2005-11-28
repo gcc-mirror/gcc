29f9683a5f2242786773eaa8f066001a39f18c82
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-11-28 07:29:43 +0000

    PR middle-end/20219 	* fold-const.c (fold binary) <RDIV_EXPR>: Optimize 	sin(x)/tan(x) as cos(x) and tan(x)/sin(x) as 1.0/cos(x) 	when flag_unsafe_math_optimizations is set and 	we don't care about NaNs or Infinities.
    	Move x/expN(y) and x/pow(y,z) transformation into common
    	flag_unsafe_math_optimizations section.
    
    testsuite/
    
    	* gcc.dg/builtins-20.c: Add checks for sin(x)/tan(x) and
    	tan(x)/sin(x) transformations.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@107597 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/builtins-20.c
+++ gcc/testsuite/gcc.dg/builtins-20.c
@@ -39,6 +39,12 @@ void test1(double x)
 
   if (cos(x)*tan(x) != sin(x))
     link_error ();
+
+  if (sin(x)/tan(x) != cos(x))
+    link_error ();
+
+  if (tan(x)/sin(x) != 1.0/cos(x))
+    link_error ();
 }
 
 void test2(double x, double y)
@@ -66,6 +72,12 @@ void test1f(float x)
     link_error ();
 
   if (cosf(x)*tanf(x) != sinf(x))
+    link_error ();
+
+  if (sinf(x)/tanf(x) != cosf(x))
+    link_error ();
+
+  if (tanf(x)/sinf(x) != 1.0f/cosf(x))
     link_error ();
 #endif
 }
@@ -96,6 +108,12 @@ void test1l(long double x)
     link_error ();
 
   if (cosl(x)*tanl(x) != sinl(x))
+    link_error ();
+
+  if (sinl(x)/tanl(x) != cosl(x))
+    link_error ();
+
+  if (tanl(x)/sinl(x) != 1.0l/cosl(x))
     link_error ();
 #endif
 }
