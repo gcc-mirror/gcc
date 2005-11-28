c1e31251a86f9b7ac347347626c41b3ffc7a6dd6
Kean Johnston <jkj@sco.com>
2005-11-28 18:10:05 +0000

    backport: re PR testsuite/19231 (Execute failure in gcc.c-torture/execute/builtins/strlen-3.c with -fpic/-fPIC)
    	(Back-ported from HEAD)
    	* g++.dg/opt/range-test-1.C: Prevent re-inclusion of system
    	headers.
    	* g++.old-deja/g++.law/profile1.C: No longer expect fail on OSR5.
    	* gcc.c-torture/compile/20000804-1.c: Skip if using -fPIC on X86.
    	* gcc.target/i386/asm-3.c: Ditto.
    	* gcc.target/i386/clobbers.c: Ditto.
    	* gcc.dg/tree-ssa/ssa-dce-2.c: Make t() static and prototype it.
    	* gcc.target/i386/cmov1.c: Make magic* static so it matches the
    	test regexp.
    	* gcc.target/i386/mul.c: Ditto.
    	* gcc.target/i386/ssetype-1.c: Ditto.
    	* gcc.dg/20040813-1.c: Skip on SVR5 systems.
    	* gcc.c-torture/execute/builtins/strlen-3.c: Declare variables as
    	static so the test works with -fPIC mode too. Based on comment in
    	PR19231 by Pinski.
    	* gcc.dg/nest.c: No longer fail on SCO systems as it has -pg again.
    
    From-SVN: r107609

--- gcc/testsuite/gcc.c-torture/compile/20000804-1.c
+++ gcc/testsuite/gcc.c-torture/compile/20000804-1.c
@@ -1,6 +1,7 @@
 /* This does not work on m68hc11 or h8300 due to the use of an asm
    statement to force a 'long long' (64-bits) to go in a register.  */
 /* { dg-do assemble { xfail m6811-*-* m6812-*-* h8300-*-* } } */
+/* { dg-skip-if "" { i?86-*-* } { "-fpic" "-fPIC" } { "" } } */
 
 /* Copyright (C) 2000, 2003 Free Software Foundation */
 __complex__ long long f ()
--- gcc/testsuite/gcc.target/i386/mul.c
+++ gcc/testsuite/gcc.target/i386/mul.c
@@ -3,8 +3,8 @@
 /* { dg-final { scan-assembler "and\[^\\n\]*magic" } } */
 
 /* Should be done as "andw $32767, magic".  */
-unsigned short magic;
-t()
+static unsigned short magic;
+void t(void)
 {
 	magic%=(unsigned short)0x8000U;
 }
