b15b83fb3bf49df836c84730431b68d498082825
Jakub Jelinek <jakub@redhat.com>
2005-11-30 09:32:16 +0100

    ia64.c (ia64_expand_tls_address): Add ORIG_OP1 argument.
    	* config/ia64/ia64.c (ia64_expand_tls_address): Add ORIG_OP1 argument.
    	Move ADDEND_{HI,LO} computation into TLS_MODEL_INITIAL_EXEC case.
    	(ia64_expand_move): Adjust caller.
    
    	* gcc.dg/tls/opt-11.c: New test.
    
    From-SVN: r107704

---
+++ gcc/testsuite/gcc.dg/tls/opt-11.c
@@ -0,0 +1,32 @@
+/* { dg-do run } */
+
+extern void abort (void);
+extern void *memset (void *, int, __SIZE_TYPE__);
+
+struct A
+{
+  char pad[48];
+  int i;
+  int pad2;
+  int j;
+};
+__thread struct A a;
+
+int *
+__attribute__((noinline))
+foo (void)
+{
+  return &a.i;
+}
+
+int
+main (void)
+{
+  int *p = foo ();
+  memset (&a, 0, sizeof (a));
+  a.i = 6;
+  a.j = 8;
+  if (p[0] != 6 || p[1] != 0 || p[2] != 8)
+    abort ();
+  return 0;
+}
