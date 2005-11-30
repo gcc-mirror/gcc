9cc6a07cc3327861c572ea0b5d2c0a74656d005b
Jakub Jelinek <jakub@redhat.com>
2005-11-30 09:35:24 +0100

    ia64.c (ia64_expand_tls_address): Add ORIG_OP1 argument.
    	* config/ia64/ia64.c (ia64_expand_tls_address): Add ORIG_OP1 argument.
    	Move ADDEND_{HI,LO} computation into TLS_MODEL_INITIAL_EXEC case.
    	(ia64_expand_move): Adjust caller.
    
    	* gcc.dg/tls/opt-11.c: New test.
    
    From-SVN: r107706

--- gcc/config/ia64/ia64.c
+++ gcc/config/ia64/ia64.c
@@ -891,14 +891,11 @@ gen_thread_pointer (void)
 
 static rtx
 ia64_expand_tls_address (enum tls_model tls_kind, rtx op0, rtx op1,
-			 HOST_WIDE_INT addend)
+			 rtx orig_op1, HOST_WIDE_INT addend)
 {
   rtx tga_op1, tga_op2, tga_ret, tga_eqv, tmp, insns;
-  rtx orig_op0 = op0, orig_op1 = op1;
+  rtx orig_op0 = op0;
   HOST_WIDE_INT addend_lo, addend_hi;
-
-  addend_lo = ((addend & 0x3fff) ^ 0x2000) - 0x2000;
-  addend_hi = addend - addend_lo;
 
   switch (tls_kind)
     {
@@ -959,6 +956,9 @@ ia64_expand_tls_address (enum tls_model tls_kind, rtx op0, rtx op1,
       break;
 
     case TLS_MODEL_INITIAL_EXEC:
+      addend_lo = ((addend & 0x3fff) ^ 0x2000) - 0x2000;
+      addend_hi = addend - addend_lo;
+
       op1 = plus_constant (op1, addend_hi);
       addend = addend_lo;
 
@@ -1023,7 +1023,7 @@ ia64_expand_move (rtx op0, rtx op1)
 
       tls_kind = tls_symbolic_operand_type (sym);
       if (tls_kind)
-	return ia64_expand_tls_address (tls_kind, op0, sym, addend);
+	return ia64_expand_tls_address (tls_kind, op0, sym, op1, addend);
 
       if (any_offset_symbol_operand (sym, mode))
 	addend = 0;
