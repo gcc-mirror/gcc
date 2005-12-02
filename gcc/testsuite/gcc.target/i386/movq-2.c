00aa5a525e164553d4f1fbf60aadf2075a8d37a4
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-12-02 22:55:35 +0000

    PR target/25199 	* config/i386/i386.md (movqi_1): Only force imovx for alternative 	5 if operand 1 is not aligned.  Undo previous constraint change. 	* config/i386/predicates.md (aligned_operand): Use MEM_ALIGN. testsuite/ 	* gcc.target/i386/movq-2.c: New test. 	* gcc.target/i386/movq.c: Remove target i?86, instead add 	dg-require-effective-target ilp32.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@107955 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/movq-2.c
@@ -0,0 +1,26 @@
+/* PR target/25199 */
+/* { dg-do compile } */
+/* { dg-options "-Os" } */
+/* { dg-require-effective-target ilp32 } */
+
+struct S
+{
+  void *p[30];
+  unsigned char c[4];
+};
+
+unsigned char d;
+
+void
+foo (struct S *x)
+{
+  register unsigned char e __asm ("esi");
+  e = x->c[3];
+  __asm __volatile ("" : : "r" (e));
+  e = x->c[0];
+  __asm __volatile ("" : : "r" (e));
+}
+
+/* { dg-final { scan-assembler-not "movl\[ \t\]*123" } } */
+/* { dg-final { scan-assembler "movzbl\[ \t\]*123" } } */
+/* { dg-final { scan-assembler "movl\[ \t\]*120" } } */
