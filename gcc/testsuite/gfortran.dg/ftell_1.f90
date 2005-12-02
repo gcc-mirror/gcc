23401c66835b10484f8954d9964db8d330323032
Francois-Xavier Coudert <coudert@clipper.ens.fr>
2005-12-02 16:55:14 +0100

    ftell_1.f90: Modify testcase so that it doesn't fail on CRLF platforms.
    	* gfortran.dg/ftell_1.f90: Modify testcase so that it doesn't
    	fail on CRLF platforms.
    	* gfortran.dg/ftell_2.f90: Likewise.
    	* gfortran.dg/modulo_1.f90: New test.
    
    From-SVN: r107898

--- gcc/testsuite/gfortran.dg/ftell_1.f90
+++ gcc/testsuite/gfortran.dg/ftell_1.f90
@@ -1,12 +1,15 @@
 ! { dg-do run }
-  integer*8 o
+  integer*8 o, o2
 
   open (10, status="scratch")
   call ftell (10, o)
   if (o /= 0) call abort
   write (10,"(A)") "1234567"
   call ftell (10, o)
-  if (o /= 8) call abort
+  if (o /= 8 .and. o /= 9) call abort
+  write (10,"(A)") "1234567"
+  call ftell (10, o2)
+  if (o2 /= 2 * o) call abort
   close (10)
   call ftell (10, o)
   if (o /= -1) call abort
