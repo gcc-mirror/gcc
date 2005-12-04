1344f9a3189ed48181c844c7d2cc1f70511ab898
Joseph Myers <joseph@codesourcery.com>
2005-12-04 21:45:41 +0000

    c-common.c (binary_op_error): Do not allow NOP_EXPR.
    	* c-common.c (binary_op_error): Do not allow NOP_EXPR.
    	(c_common_truthvalue_conversion): Handle NOP_EXPR the same as
    	CONVERT_EXPR.
    	(check_function_arguments_recurse): Allow both NOP_EXPR and
    	CONVERT_EXPR but check conversions preserve precision.
    	* c-typeck.c (pointer_diff): Allow both NOP_EXPR and CONVERT_EXPR
    	but check conversions preserve precision.
    	(build_unary_op): Don't allow NOP_EXPR.  Use gcc_unreachable () in
    	default case.
    
    testsuite:
    	* gcc.dg/format/cast-1.c: New test.
    
    From-SVN: r108020

---
+++ gcc/testsuite/gcc.dg/format/cast-1.c
@@ -0,0 +1,16 @@
+/* Test for strings cast through integer types: should not be treated
+   as format strings unless the types are of the same width as
+   pointers (intptr_t or similar).  */
+/* Origin: Joseph Myers <joseph@codesourcery.com> */
+/* { dg-do compile } */
+/* { dg-options "-Wformat" } */
+
+#include "format.h"
+
+void
+f (int x)
+{
+  printf("%s", x); /* { dg-warning "format" } */
+  printf((char *)(size_t)"%s", x); /* { dg-warning "format" } */
+  printf((char *)(char)"%s", x);
+}
