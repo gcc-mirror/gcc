c01731362eb341db3e47782873d718756a0c6c65
Ben Elliston <bje@au.ibm.com>
2005-12-04 23:50:48 +0000

    Makefile.in (clean): Remove stray reference to libcpp.a.
    	* Makefile.in (clean): Remove stray reference to libcpp.a.
    
    	* decimal128.h, decContext.c, decRound.c, decimal32.c,
    	decNumber.c, decContext.h, decimal64.c, decimal32.h, decNumber.h,
    	decimal64.h, decUtility.c, decLibrary.c, configure.ac,
    	decNumberLocal.h, decUtility.h, decDPD.h, decimal128.c: Update FSF
    	office address.
    
    From-SVN: r108024

--- libdecnumber/decLibrary.c
+++ libdecnumber/decLibrary.c
@@ -1,22 +1,22 @@
 /* Temporary library support for decimal floating point.
    Copyright (C) 2005 Free Software Foundation, Inc.
 
-This file is part of GCC.
+   This file is part of GCC.
 
-GCC is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free
-Software Foundation; either version 2, or (at your option) any later
-version.
+   GCC is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
-GCC is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-for more details.
+   GCC is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
 
-You should have received a copy of the GNU General Public License
-along with GCC; see the file COPYING.  If not, write to the Free
-Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
-02110-1301, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with GCC; see the file COPYING.  If not, write to the Free
+   Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.  */
 
 #include "config.h"
 #include "decContext.h"
