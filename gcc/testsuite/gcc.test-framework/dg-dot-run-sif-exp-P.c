c44ca1626c8e2b6519a6ae58780a55a741fd42e2
Janis Johnson <janis187@us.ibm.com>
2005-12-05 19:06:11 +0000

    re PR testsuite/25247 (syntax error in target selector for gcc.dg/torture/fp-int-convert-float128-timode.c)
    	PR testsuite/25247
    	* lib/target-supports-dg.exp (dg-skip-if): Fix target selector for
    	use in selector expressions.
    	(dg-xfail-if): Ditto.
    	* gcc.test-framework/dg-dot-run-sif-exp-P.c: New test.
    	* gcc.test-framework/dg-dot-run-sif-exp-U.c: New test.
    	* gcc.test-framework/dg-dot-run-xif-exp-P.c: New test.
    	* gcc.test-framework/dg-dot-run-xif-exp-XP.c: New test.
    	* gcc.test-framework/test-framework.exp: Skip generated tests if
    	CHECK_TEST_FRAMEWORK is not 1.
    
    From-SVN: r108069

---
+++ gcc/testsuite/gcc.test-framework/dg-dot-run-sif-exp-P.c
@@ -0,0 +1,8 @@
+/* { dg-do run { target { *-*-* && yes } } } */
+/* { dg-skip-if "comment" { no && yes } { "*" } { "" } } */
+
+int
+main ()
+{
+  return 0;
+}
---
+++ gcc/testsuite/gcc.test-framework/dg-dot-run-xif-exp-P.c
@@ -0,0 +1,8 @@
+/* { dg-do run { target { *-*-* && yes } } } */
+/* { dg-xfail-if "comment" { empty-*-* && yes } { "*" } { "" } } */
+
+int
+main ()
+{
+  return 0;
+}
