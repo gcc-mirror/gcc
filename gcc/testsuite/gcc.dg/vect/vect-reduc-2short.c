6ef6ea5ea2df85f63a31793f6c2b0ed01ad5bdbd
Eric Botcazou <ebotcazou@libertysurf.fr>
2005-12-05 19:53:04 +0100

    re PR tree-optimization/24963 (gcc.dg/vect/vect-62.c scan-tree-dump-times not vectorized: redundant loop. no profit to vectorize. 1 fails)
    	PR tree-optimization/24963
    	* gcc.dg/vect/vect-62.c: Remove outdated dg-line.
    
    	PR target/24108
    	* gcc.dg/vect/vect-76.c: Do not XFAIL on vect_no_align platforms.
    
    	PR target/18580
    	* gcc.dg/vect/vect-reduc-1char.c: XFAIL on vect_no_int_max platforms.
    	* gcc.dg/vect/vect-reduc-2short.c: Likewise.
    	* gcc.dg/vect/vect-reduc-1short.c: Likewise.
    	* gcc.dg/vect/vect-reduc-2char.c: Likewise.
    
    From-SVN: r108067

--- gcc/testsuite/gcc.dg/vect/vect-reduc-2short.c
+++ gcc/testsuite/gcc.dg/vect/vect-reduc-2short.c
@@ -44,5 +44,5 @@ int main (void)
   return 0;
 }
 
-/* { dg-final { scan-tree-dump-times "vectorized 3 loops" 1 "vect" } } */
+/* { dg-final { scan-tree-dump-times "vectorized 3 loops" 1 "vect" { xfail vect_no_int_max } } } */
 /* { dg-final { cleanup-tree-dump "vect" } } */
