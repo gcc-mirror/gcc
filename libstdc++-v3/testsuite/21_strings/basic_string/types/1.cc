3af22b23b32d69bd7cc0d444429f33e4edae008d
Paolo Carlini <pcarlini@suse.de>
2005-12-06 17:19:30 +0000

    basic_string.h (insert(iterator, _CharT), [...]): Avoid troubles with ADL, user defined operators and __normal_iterator.
    2005-12-06  Paolo Carlini  <pcarlini@suse.de>
    
    	* include/bits/basic_string.h (insert(iterator, _CharT),
    	erase(iterator), erase(iterator, iterator)): Avoid troubles
    	with ADL, user defined operators and __normal_iterator.
    	* include/bits/stl_iterator.h (operator-(const __normal_iterator
    	<_Iterator, _Container>&, const __normal_iterator<_Iterator,
    	_Container>&)): Add overload for left and right iterators of
    	the same type.
    	* include/debug/safe_iterator.h (operator-(const _Safe_iterator
    	<_Iterator, _Sequence>&, const _Safe_iterator<_Iterator,
    	_Sequence>&)): Likewise.
    	* testsuite/21_strings/basic_string/types/1.cc: New.
    
    From-SVN: r108123

---
+++ libstdc++-v3/testsuite/21_strings/basic_string/types/1.cc
@@ -0,0 +1,48 @@
+// 2005-12-01  Paolo Carlini  <pcarlini@suse.de>
+
+// Copyright (C) 2005 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// { dg-do compile }
+
+#include <string>
+
+namespace N
+{
+  struct X { };
+
+  template<typename T>
+    X operator+(T, std::size_t)
+    { return X(); }
+
+  template<typename T>
+    X operator-(T, T)
+    { return X(); }
+}
+
+int main()
+{ 
+  std::basic_string<N::X> s(5, N::X());
+
+  s.erase(s.begin());
+  s.erase(s.begin(), s.end());
+  s.insert(s.begin(), N::X());
+  s.replace(s.begin(), s.end(), s.begin(), s.end());
+
+  return 0;
+}
