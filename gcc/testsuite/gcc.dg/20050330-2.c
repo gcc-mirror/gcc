88a6bf5e1929ae2f1243c0039cd25092451de3dd
Jakub Jelinek <jakub@redhat.com>
2005-12-08 22:44:27 +0100

    * gcc.dg/20050330-2.c: New test.
    From-SVN: r108243

---
+++ gcc/testsuite/gcc.dg/20050330-2.c
@@ -0,0 +1,21 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fprofile-generate" } */
+
+struct S
+{
+  int a;
+  void **b;
+};
+
+void
+foo (struct S *x, int y)
+{
+  if (!x)
+    return;
+  if (y >= x->a)
+    return;
+  x->a--;
+  for (; y < x->a; y++)
+    x->b[y] = x->b[y + 1];
+  x->b[x->a] = (void *) 0;
+}
