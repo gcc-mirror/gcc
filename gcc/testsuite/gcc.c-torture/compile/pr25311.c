f01cf809285f7d659f1b4ae1541041a0257df0bb
Ulrich Weigand <uweigand@de.ibm.com>
2005-12-09 11:20:40 +0000

    re PR target/25311 (ICE in reload_cse_simplify_operands, at postreload.c:393)
    	PR target/25311
    	* config/s390/s390.c (struct s390_address): New field literal_pool.
    	(s390_decompose_address): Compute literal_pool field.  Do not 
    	assume register %r13 is always (and solely) used as pool base.
    	(s390_extra_constraint_str): Use literal_pool field.
    
    	PR target/25311
    	* gcc.c-torture/compile/pr25311.c: New test.
    
    From-SVN: r108278

---
+++ gcc/testsuite/gcc.c-torture/compile/pr25311.c
@@ -0,0 +1,47 @@
+
+struct w
+{
+  int top;
+  int left;
+  int height;
+  int width;
+  struct w *next;
+  struct w *parent;
+  struct w *child;
+};
+
+extern struct w *Qnil;
+
+void
+set_size (struct w *w, int new_size, int nodelete, int set_height)
+{
+  int old_size = set_height? w->height : w->width;
+
+  if (nodelete || w->parent == Qnil)
+    {
+      int last_pos, last_old_pos, pos, old_pos, first;
+      int div_val = old_size << 1;
+      struct w *c;
+
+      last_pos = first = set_height? w->top : w->left;
+      last_old_pos = 0;
+
+      for (c = w->child; c != Qnil; c = c->next)
+	{
+	  if (set_height)
+	    old_pos = last_old_pos + c->height;
+	  else
+	    old_pos = last_old_pos + c->width;
+
+	  pos = (((old_pos * new_size) << 1) + old_size) / div_val;
+	  set_size (c, pos + first - last_pos, 1, set_height);
+	  last_pos = pos + first;
+	  last_old_pos = old_pos;
+	}
+
+      if (!nodelete)
+	for (c = w->child; c != Qnil; c = c->next)
+	  use (c);
+    }
+}
+
