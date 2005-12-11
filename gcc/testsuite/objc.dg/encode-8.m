39be2506a02b1b4af479d8cc961077eb623d3497
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-12-11 06:59:12 +0000

    2005-12-11  Andrew Pinski  <pinskia@physics.uc.edu>
            PR libobjc/25347
            * encoding.c (objc_sizeof_type): Don't handle _C_UNION_B special
            but use the struct layout functions.
            (objc_alignof_type): Likewise.
            (objc_layout_structure): Handle _C_UNION_B also.
            (objc_layout_structure_next_member): Likewise.
            (objc_layout_finish_structure): Likewise.
    
    2005-12-11  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR libobjc/25347
            * objc.dg/encode-8.m: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@108379 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/objc.dg/encode-8.m
@@ -0,0 +1,31 @@
+/* { dg-options "-fgnu-runtime" } */
+/* { dg-do run } */
+
+#include <objc/encoding.h>
+#include <stdlib.h>
+
+union f
+{
+  char i;
+  double f1;
+  short t;
+};
+
+union g
+{
+  int i;
+};
+
+
+int main(void)
+{
+  if (objc_sizeof_type (@encode (union f)) != sizeof(union f))
+   abort ();
+  if (objc_alignof_type (@encode (union f)) != __alignof__(union f))
+   abort ();
+  if (objc_sizeof_type (@encode (union g)) != sizeof(union g))
+   abort ();
+  if (objc_alignof_type (@encode (union g)) != __alignof__(union g))
+   abort ();
+  return 0;
+}
