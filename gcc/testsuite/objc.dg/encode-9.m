c4e3ffb1f916d8501f39d1beb7f65160c939afe5
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-12-12 23:58:16 +0000

    2005-12-12  Andrew Pinski  <pinskia@physics.uc.edu>
            PR objc/25348
            * objc-act.c (encode_array): Handle arrays to zero sized types.
    2005-12-12  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR objc/25348
            * objc.dg/encode-9.m: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@108432 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/objc.dg/encode-9.m
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-fgnu-runtime " } */
+
+/* There was an ICE due to diving by zero in the objc front-end. */
+
+struct f
+{
+  int i;
+  struct{} g[4];
+  int tt;
+};
+
+char *e = @encode(struct f);
