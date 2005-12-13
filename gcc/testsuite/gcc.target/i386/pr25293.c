3cb7a12915b19dc2f21263f46764df080e75aab8
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-12-13 08:08:46 +0000

    PR debug/25023 	PR target/25293 	* expr.c (emit_move_resolve_push): Handle PRE_MODIFY 	and POST_MODIFY with CONST_INT adjustment equal to PUSH_ROUNDING. 	Fix POST_INC/POST_DEC handling if PUSH_ROUNDING is not identity. 	* config/i386/i386.md (pushhi2, pushqi2): Use pushl instead of pushw. 	Set mode to SI, adjust constraints. 	(pushhi2_rex64, pushqi2_rex64): Set mode to DI. 	* config/i386/i386.h (PUSH_ROUNDING): Round up to 4 instead of 2 for 	32-bit code.
    	* gcc.target/i386/pr25293.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@108463 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr25293.c
@@ -0,0 +1,51 @@
+/* PR target/25293 */
+/* { dg-do compile } */
+/* { dg-options "-mpreferred-stack-boundary=2 -mtune=i586 -O2 -fomit-frame-pointer -g" } */
+/* { dg-require-effective-target ilp32 } */
+
+struct T { unsigned short t1, t2, t3, t4, t5, t6, t7; };
+struct S { struct T s1; unsigned short s2, s3; };
+unsigned short v1;
+int f1 (void);
+int f2 (struct T);
+int f3 (const char *);
+
+int
+foo (struct S *x, struct T y)
+{
+  unsigned short a, b, c;
+  unsigned long d, e;
+  int f = 0;
+  y.t6 = 6;
+  a = y.t7;
+  b = y.t6;
+  c = y.t7;
+  switch (a)
+    {
+    case 8:
+    case 7:
+      c = 9;
+      break;
+    case 1:
+    case 6:
+    case 3:
+      b = 16;
+      c = 9;
+      break;
+    }
+  if ((f = f1 ()))
+    goto error;
+  if ((f = f2 (y)))
+    goto error;
+  d = (long) &y;
+  e = (long) &x->s1;
+  __asm __volatile ("" : "+D" (e), "+S" (d) :: "memory");
+  x->s2 = b;
+  x->s3 = c;
+  f3 ("foo");
+  return 0;
+error:
+  if (v1 >= 1)
+    f3 ("bar");
+  return f;
+}
