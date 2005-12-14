9d1a0fdbcd69aa88d654d491c9d5726cd5c69357
Kean Johnston <jkj@sco.com>
2005-12-14 17:30:34 +0000

    sibcall-6.c: Skip in -fPIC mode on X86 ilp32.
    	* gcc.dg/sibcall-6.c: Skip in -fPIC mode on X86 ilp32.
    	* gcc.target/i386/20020523-1.c: Ditto.
    	* gcc.target/i386/tailcall-1.c: Ditto.
    	* gcc.target/i386/ssetype-3.c: Make magic* static to work with -fPIC.
    	* gcc.target/i386/ssetype-5.c: Ditto.
    
    
    Co-Authored-By: Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
    
    From-SVN: r108521

--- gcc/testsuite/gcc.target/i386/20020523-1.c
+++ gcc/testsuite/gcc.target/i386/20020523-1.c
@@ -2,6 +2,7 @@
    This testcase was miscompiled because sse_mov?fcc_const0*
    patterns were missing earlyclobber.  */
 /* { dg-do run { target i?86-*-* x86_64-*-* } } */
+/* { dg-skip-if "" { ilp32 } { "-fpic" "-fPIC" } { "" } } */
 /* { dg-require-effective-target ilp32 } */
 /* { dg-options "-march=pentium3 -msse -ffast-math -O2" } */
 
