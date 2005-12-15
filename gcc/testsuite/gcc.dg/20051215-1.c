e6add59b161628111dff015502f88d6459927493
Richard Sandiford <richard@codesourcery.com>
2005-12-15 16:42:10 +0000

    predicates.md (call_memory_operand): New.
    	* config/arm/predicates.md (call_memory_operand): New.
    	* config/arm/arm.md (*call_mem, *call_value_mem): Use it.
    
    testsuite/
    	* gcc.dg/20051215-1.c: New file.
    
    From-SVN: r108583

---
+++ gcc/testsuite/gcc.dg/20051215-1.c
@@ -0,0 +1,36 @@
+/* ARM's load-and-call patterns used to allow automodified addresses.
+   This was wrong, because if the modified register were spilled,
+   the call would need an output reload.  */
+/* { dg-do run { target arm*-*-* } } */
+/* { dg-options "-O2 -fno-omit-frame-pointer" } */
+extern void abort (void);
+typedef void (*callback) (void);
+
+static void
+foo (callback *first, callback *p)
+{
+  while (p > first)
+    {
+      (*--p) ();
+#ifndef __thumb__
+      asm ("" : "=r" (p) : "0" (p)
+	   : "r4", "r5", "r6", "r7", "r8", "r9", "r10");
+#endif
+    }   
+}
+
+static void
+dummy (void)
+{
+  static int count;
+  if (count++ == 1)
+    abort ();
+}
+
+int
+main (void)
+{
+  callback list[1] = { dummy };
+  foo (&list[0], &list[1]);
+  return 0;
+}
