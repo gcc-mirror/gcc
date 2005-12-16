de4048cd211ca084fc47bdeb0c31bb0a37f6ecd1
Tom Tromey <tromey@redhat.com>
2005-12-16 17:24:42 +0000

    re PR java/25429 (can't see private static final int CREATE = 1)
    gcc/java:
    	PR java/25429
    	* parse.y (resolve_expression_name): Don't generate accessor
    	methods for constant fields.
    libjava:
    	* testsuite/libjava.jacks/jacks.xfail
    	(8.5.2-accessible-static-member-usage-3): Removed.
    
    	* testsuite/libjava.compile/rh175833.java: New file.
    	* testsuite/libjava.compile/pr25429.java: New file.
    
    Co-Authored-By: Andrew Haley <aph@redhat.com>
    
    From-SVN: r108661

---
+++ libjava/testsuite/libjava.compile/pr25429.java
@@ -0,0 +1,13 @@
+public class pr25429
+{
+  private static final int CONST  = 0;
+  class I {
+    public void f () {
+      switch(0) {
+      case CONST:
+      }
+    }
+  }
+
+  public static void main(String[] args) { }
+}
