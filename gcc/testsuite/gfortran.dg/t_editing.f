59cd610fba0fb8df5fa234e5e2550f2afb92522b
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2005-12-18 08:32:09 +0000

    re PR fortran/25264 (write to internal unit from the string itself gives wrong result ?)
    2005-12-17  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/25264
    	PR libgfortran/25349
    	* gfortran.dg/tl_editing.f90: Added additional checks.
    	* gfortran.dg/t_editing.f: New test.
    	* gfortran.dg/write_padding.f90: New test
    
    From-SVN: r108745

---
+++ gcc/testsuite/gfortran.dg/t_editing.f
@@ -0,0 +1,8 @@
+! { dg-do run }     
+! PR25349 Check T editing. Test case from PR submitted by Thomas Koenig
+! Contributed by Jerry DeLisle <jvdelisle@gcc.gnu.org>
+      program main
+      character(len=10) line
+      write (line,'(1X,A,T1,A)') 'A','B'
+      if (line.ne.'BA') call abort()
+      end
