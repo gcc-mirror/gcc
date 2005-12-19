2803e9970df940bf450c41e68cb15238382f4db2
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2005-12-19 07:02:05 +0000

    re PR libfortran/25463 (T edit descriptor and ADVANCE="no")
    2005-12-18  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/25463
    	* gfortran.dg/advance.f90: New test.
    
    From-SVN: r108785

---
+++ gcc/testsuite/gfortran.dg/advance.f90
@@ -0,0 +1,12 @@
+! { dg-do run }
+! PR25463 Check that advance='no' works correctly.
+! Derived from example given in PR by Thomas Koenig
+! Contributed by Jerry DeLisle  <jvdelisle@gcc.gnu.org> 
+program pr25463
+  character(10) :: str
+  write (10,'(A)',advance="no") 'ab'
+  write (10,'(TL2,A)') 'c'
+  rewind (10)
+  read (10, '(a)') str
+  if (str.ne.'abc') call abort()
+end
