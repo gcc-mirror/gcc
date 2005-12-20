034f16cd3195850629666c0d03bd06171a774099
mrs <mrs@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-12-20 23:46:18 +0000

    * config/darwin.h (TARGET_OPTION_TRANSLATE_TABLE): Handle the         ambiguity between -seg_addr_table and -seg_addr_table_filename.         (WORD_SWITCH_TAKES_ARG): Likewise.         (LINK_SPEC): Likewise. Radar 3941990
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@108876 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/darwin.h
+++ gcc/config/darwin.h
@@ -123,7 +123,7 @@ Boston, MA 02110-1301, USA.  */
   { "-segs_read_only_addr", "-Zsegs_read_only_addr" }, \
   { "-segs_read_write_addr", "-Zsegs_read_write_addr" }, \
   { "-seg_addr_table", "-Zseg_addr_table" }, \
-  { "-seg_addr_table_filename", "-Zseg_addr_table_filename" }, \
+  { "-seg_addr_table_filename", "-Zfn_seg_addr_table_filename" }, \
   { "-filelist", "-Xlinker -filelist -Xlinker" },  \
   { "-framework", "-Xlinker -framework -Xlinker" },  \
   { "-flat_namespace", "-Zflat_namespace" },  \
@@ -167,7 +167,7 @@ Boston, MA 02110-1301, USA.  */
    !strcmp (STR, "Zsegs_read_only_addr") ? 1 :  \
    !strcmp (STR, "Zsegs_read_write_addr") ? 1 : \
    !strcmp (STR, "Zseg_addr_table") ? 1 :       \
-   !strcmp (STR, "Zseg_addr_table_filename") ?1 :\
+   !strcmp (STR, "Zfn_seg_addr_table_filename") ? 1 :\
    !strcmp (STR, "seg1addr") ? 1 :              \
    !strcmp (STR, "segprot") ? 3 :               \
    !strcmp (STR, "sub_library") ? 1 :           \
@@ -272,7 +272,7 @@ Boston, MA 02110-1301, USA.  */
    %{Zsegs_read_only_addr*:-segs_read_only_addr %*} \
    %{Zsegs_read_write_addr*:-segs_read_write_addr %*} \
    %{Zseg_addr_table*: -seg_addr_table %*} \
-   %{Zseg_addr_table_filename*:-seg_addr_table_filename %*} \
+   %{Zfn_seg_addr_table_filename*:-seg_addr_table_filename %*} \
    %{sub_library*} %{sub_umbrella*} \
    %{isysroot*:-syslibroot %*} \
    %{twolevel_namespace} %{twolevel_namespace_hints} \
