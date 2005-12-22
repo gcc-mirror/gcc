58ccc8618ad472ab8e8948947e07f5f900db6242
Paul Brook <paul@codesourcery.com>
2005-12-22 19:24:07 +0000

    typeof-2.c: Skip assembly test on m68k.
    2005-12-22  Paul Brook  <paul@codesourcery.com>
    
    	* gcc.dg/weak/typeof-2.c: Skip assembly test on m68k.
    
    From-SVN: r108968

--- gcc/testsuite/gcc.dg/weak/typeof-2.c
+++ gcc/testsuite/gcc.dg/weak/typeof-2.c
@@ -35,4 +35,6 @@ int bar3 (int x)
 // { dg-final { if [string match {sh[elb1-9]*-*-*} $target_triplet ] {return} } }
 // Likewise for S/390 targets
 // { dg-final { if [string match s390*-*-* $target_triplet ] {return} } }
+// Likewise for m68k targets
+// { dg-final { if [string match m68k*-*-* $target_triplet ] {return} } }
 // { dg-final { scan-assembler "baz3.*baz3.*baz3.*baz3.*baz3.*baz3" } }
