5a4a8e04ada922e80a95ea56d68d9b1d84962163
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-12-26 23:00:18 +0000

    gcc/ 	PR tree-optimization/25125 	* convert.c (convert_to_integer): Don't narrow the type of a 	PLUX_EXPR or MINUS_EXPR if !flag_wrapv and the unwidened type 	is signed.
    gcc/testsuite/
    	PR tree-optimization/25125
    	* gcc.dg/vect/vect-7.c, gcc.dg/vect/vect-reduc-2char.c,
    	gcc.dg/vect/vect-reduc-2short.c: XFAIL.
    	* gcc.c-torture/execute/pr25125.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@109065 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/vect/vect-reduc-2short.c
+++ gcc/testsuite/gcc.dg/vect/vect-reduc-2short.c
@@ -44,5 +44,5 @@ int main (void)
   return 0;
 }
 
-/* { dg-final { scan-tree-dump-times "vectorized 3 loops" 1 "vect" { xfail vect_no_int_max } } } */
+/* { dg-final { scan-tree-dump-times "vectorized 3 loops" 1 "vect" { xfail *-*-* } } } */
 /* { dg-final { cleanup-tree-dump "vect" } } */
