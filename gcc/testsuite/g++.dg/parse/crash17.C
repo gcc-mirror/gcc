d85d3d578a5c2abcf2b071ef42a558a4ed18b55c
Mark Mitchell <mark@codesourcery.com>
2005-12-27 09:05:17 +0000

    re PR c++/25439 ("template<> int A<0>" accepted)
    	PR c++/25439
    	* decl.c (grokdeclarator): Remove dead code.
    	* ptree.c (cxx_print_xnode): Handle BASELINK.
    	* parser.c (make_id_declarator): Add sfk parameter.  
    	(cp_parser_direct_declarator): Do not pass TYPE_DECLs to
    	make_id_declarator.
    	(cp_parser_declarator_id): Simplify BASELINKs here.
    	(cp_parser_member_declaration): Adjust calls to
    	make_id_declarator.
    	PR c++/25439
    	* g++.dg/parse/crash17.C: Adjust error markers.
    	* g++.dg/template/error20.C: New test.
    
    From-SVN: r109079

--- gcc/testsuite/g++.dg/parse/crash17.C
+++ gcc/testsuite/g++.dg/parse/crash17.C
@@ -5,5 +5,5 @@ template <typename> class allocator;
 template<typename T> class vector { 
   // With the dg-error on the next line, we are really just trying to
   // check that the message is not an ICE message.
-  typedef typename allocator<T> allocator_type; // { dg-error "expected|forbids" }
+  typedef typename allocator<T> allocator_type; // { dg-error "expected|invalid" }
 }; 
