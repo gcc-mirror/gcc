b5918dea9c512bd13382d3fce512b725fe78aa24
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2005-12-28 07:20:19 +0000

    re PR libfortran/25550 (file data corrupted after reading end of file)
    2005-12-28  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/25550
    	* io/file_pos.c (st_rewind): Reset bytes left so no error occurs in
    	next_record_r.
    
    From-SVN: r109101

--- libgfortran/io/file_pos.c
+++ libgfortran/io/file_pos.c
@@ -255,6 +255,7 @@ st_rewind (st_parameter_filepos *fpp)
 
 	  u->endfile = NO_ENDFILE;
 	  u->current_record = 0;
+	  u->bytes_left = 0;
 	  test_endfile (u);
 	}
       /* Update position for INQUIRE.  */
