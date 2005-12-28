03345deb5e9e0b9c4442338366467a8050164a53
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2005-12-28 21:12:52 +0000

    re PR libfortran/25139 ("Invalid argument" error on I/O)
    2005-12-28  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/25139
    	* gfortran.dg/backspace_2.f: New test.
    
    From-SVN: r109123

---
+++ gcc/testsuite/gfortran.dg/backspace_2.f
@@ -0,0 +1,21 @@
+! { dg-do run }
+! PR25139 Repeated backspaces and reads.
+! Derived from example given in PR by Dale Ranta and FX Coudert
+! Contributed by Jerry DeLisle  <jvdelisle@gcc.gnu.org> 
+      integer dat(5)
+      dat = (/ 0, 0, 0, 0, 1 /)
+      write(11) dat,dat,dat,dat
+      rewind 11
+      write(11) dat
+      read(11,end=1008) dat
+      call abort()
+ 1008 continue
+      backspace 11
+      write(11) dat
+      read(11,end=1011) dat
+      call abort()
+ 1011 continue
+      backspace 11
+      backspace 11
+      end
+
