f8edf7c28c678558f5d9014c15a5075f86d67a40
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-12-28 07:21:20 +0000

    2005-12-28  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR libgfortran/25550
    	* gfortran.dg/endfile.f: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@109102 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/endfile.f
@@ -0,0 +1,18 @@
+! { dg-do run }
+! PR25550 file data corrupted after reading end of file.
+! Derived from example given in PR from Dale Ranta.
+! Contributed by Jerry DeLisle  <jvdelisle@gcc.gnu.org> 
+      integer data
+      data=-1
+      open(unit=11,status='scratch',form='unformatted')
+      write(11)data
+      read(11,end=1000 )data
+      call abort()
+ 1000 continue
+      rewind (11)
+      read(11)data
+ 1001 continue
+      if(data.ne.-1) call abort
+      end
+
+
