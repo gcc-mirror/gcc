f7741faae58623a7c92d833dcb3b23b055c7bf58
Erik Edelmann <eedelman@gcc.gnu.org>
2005-12-30 15:07:48 +0000

    re PR fortran/22607 (Elemental character functions don't work)
    fortran/
    2005-12-30  Erik Edelmann  <eedelman@gcc.gnu.org>
    
            PR fortran/22607
            * trans-decl.c(gfc_get_extern_function_decl): Don't set
            DECL_IS_PURE (fndecl) = 1 for return-by-reference
            functions.
    
            fortran/PR 25396
            * interface.c (gfc_extend_expr): Initialize
            e->value.function.name to NULL.
    
    
    
    testsuite/
    2005-12-30  Erik Edelmann  <eedelman@gcc.gnu.org>
    
            PR fortran/22607
            * gfortran-dg/pure_byref_3.f90: New.
    
            fortran/PR 25396
            * gfortran.dg/userdef_operator_1.f90: New.
    
    From-SVN: r109172

---
+++ gcc/testsuite/gfortran.dg/pure_byref_3.f90
@@ -0,0 +1,33 @@
+! { dg-do run }
+! PR 22607: External/module pure return-by-reference functions
+
+pure function hoj()
+    integer :: hoj(3)
+    hoj = (/1, 2, 3/)
+end function hoj
+
+module huj_mod
+contains
+    pure function huj()
+        integer :: huj(3)
+        huj = (/1, 2, 3/)
+    end function huj
+end module huj_mod
+
+program pure_byref_3
+    use huj_mod
+    implicit none
+
+    interface
+        pure function hoj()
+            integer :: hoj(3)
+        end function hoj
+    end interface
+    integer :: a(3)
+
+    a = huj()
+    if (.not. all(a == (/1, 2, 3/))) call abort()
+
+    a = hoj()
+    if (.not. all(a == (/1, 2, 3/))) call abort()
+end program pure_byref_3
