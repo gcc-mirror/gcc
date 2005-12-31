084c091588aeb20328123a872ce1623c157b9dc8
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2005-12-31 01:00:54 +0000

    re PR testsuite/25214 (-fpic/-fPIC failures in gcc.dg/i386-local2.c)
    	PR testsuite/25214
    	* gcc.dg/i386-local2.c: Skip if ilp32 and -fpic/-fpic.
    
    From-SVN: r109196

--- gcc/testsuite/gcc.dg/i386-local2.c
+++ gcc/testsuite/gcc.dg/i386-local2.c
@@ -1,5 +1,6 @@
 /* { dg-do compile { target i?86-*-* x86_64-*-* } } */
 /* { dg-options "-O2 -funit-at-a-time -fomit-frame-pointer" } */
+/* { dg-skip-if "PR 25214" { ilp32 } { "-fpic" "-fPIC" } { "" } } */
 /* { dg-final { scan-assembler-not "sub\[^\\n\]*sp" } } */
 
 static __attribute__ ((noinline)) q ();
