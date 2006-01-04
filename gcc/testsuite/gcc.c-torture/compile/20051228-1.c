118e1d6e40b61924a4f4a82751e42b185bce25c3
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-01-04 08:15:06 +0000

    PR target/25554 	* config/i386/i386.md (testqi_ext_3): Ensure len is positive 	and pos non-negative and pos + len <= 32. 	(testqi_ext_3_rex64): Ensure len is positive and pos non-negative, 	drop pos + len < HOST_BITS_PER_WIDE_INT test. 	(testqi_ext_3* splitter): Handle pos + len == HOST_BITS_PER_WIDE_INT.
    	* gcc.c-torture/compile/20051228-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@109317 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/20051228-1.c
@@ -0,0 +1,10 @@
+/* PR target/25554 */
+/* Bitwise shift with negative shift count has undefined behavior,
+   but we shouldn't ICE on it.  */
+
+void
+foo (long x)
+{
+  if (((x >> -2) & 1) != 0)
+    bar ();
+}
