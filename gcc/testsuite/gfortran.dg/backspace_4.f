2f94c68c513e623fa96d18c705643ecf67a7ab79
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2006-01-06 01:44:31 +0000

    re PR libfortran/25598 (gfortran - Fortran runtime error: Invalid argument)
    2005-01-05  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR fortran/25598
    	* gfortran.dg/backspace_3.f: New test.
    	* gfortran.dg/backspace_4.f: New test.
    
    From-SVN: r109406

---
+++ gcc/testsuite/gfortran.dg/backspace_4.f
@@ -0,0 +1,18 @@
+! { dg-do run }
+! PR25598 Error on repeated backspaces.
+! Derived from example given in PR by Dale Ranta
+! Contributed by Jerry DeLisle  <jvdelisle@gcc.gnu.org> 
+       integer data
+       data=-1
+       open(unit=11,status='scratch',form='unformatted')
+       write(11)data
+       read(11,end=        1000 )data
+       call abort()
+ 1000  continue
+       backspace 11
+       backspace 11
+       read(11,end=        1001 )data
+ 1001  continue
+       if (data.ne.-1) call abort
+       close(11)
+       end
