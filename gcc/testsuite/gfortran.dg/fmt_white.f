c53d038d21a28dfd393a11981b902549bdec8cf9
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-01-06 01:28:40 +0000

    2005-01-05  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR fortran/24268
    	* gfortran.dg/fmt_white.f: Update test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@109404 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/fmt_white.f
+++ gcc/testsuite/gfortran.dg/fmt_white.f
@@ -1,13 +1,18 @@
 ! { dg-do run }
 ! PR24268 Test case derived from example given by Iwan Kawrakow
+! Embedded spaces in format strings should be ignored.
 ! Prepared by Jerry DeLisle  <jvdelisle@gcc.gnu.org>
       program pr24268
       real x
-      character*11 line
+      character*13 line
       line = "12.34"
       read(line,*) x
       write(line,10) x
  10                                                            format(g1
      *      1.4)
       if (line.ne."  12.34") call abort()
+      line = ""
+      write(line,20) x
+ 20   format(t r 2 , g 1 1 . 4)
+      if (line.ne."    12.34") call abort()
       end
