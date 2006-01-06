8474dcf50b14757467877985bf95c4c9c40d064b
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-01-06 19:11:06 +0000

    2006-01-06  Paolo Carlini  <pcarlini@suse.de>
    	* include/bits/stl_tree.h (_Rb_tree<>::insert_unique): Rename
    	to _M_insert_unique.
    	(_Rb_tree<>::insert_equal): Rename to _M_insert_equal.
    	* include/bits/stl_map.h (class map<>): Update callers.
    	* include/bits/stl_set.h (class set<>): Likewise.
    	* include/bits/stl_multimap.h (class multimap<>): Likewise.
    	* include/bits/stl_multiset.h (class multiset<>): Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@109424 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/stl_multiset.h
+++ libstdc++-v3/include/bits/stl_multiset.h
@@ -1,6 +1,6 @@
 // Multiset implementation -*- C++ -*-
 
-// Copyright (C) 2001, 2002, 2004, 2005 Free Software Foundation, Inc.
+// Copyright (C) 2001, 2002, 2004, 2005, 2006 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -167,7 +167,7 @@ _GLIBCXX_BEGIN_NESTED_NAMESPACE(std, _GLIBCXX_STD)
       template <class _InputIterator>
         multiset(_InputIterator __first, _InputIterator __last)
 	: _M_t(_Compare(), allocator_type())
-        { _M_t.insert_equal(__first, __last); }
+        { _M_t._M_insert_equal(__first, __last); }
 
       /**
        *  @brief  Builds a %multiset from a range.
@@ -185,7 +185,7 @@ _GLIBCXX_BEGIN_NESTED_NAMESPACE(std, _GLIBCXX_STD)
 		 const _Compare& __comp,
 		 const allocator_type& __a = allocator_type())
 	: _M_t(__comp, __a)
-        { _M_t.insert_equal(__first, __last); }
+        { _M_t._M_insert_equal(__first, __last); }
 
       /**
        *  @brief  %Multiset copy constructor.
@@ -306,7 +306,7 @@ _GLIBCXX_BEGIN_NESTED_NAMESPACE(std, _GLIBCXX_STD)
        */
       iterator
       insert(const value_type& __x)
-      { return _M_t.insert_equal(__x); }
+      { return _M_t._M_insert_equal(__x); }
 
       /**
        *  @brief Inserts an element into the %multiset.
@@ -330,7 +330,7 @@ _GLIBCXX_BEGIN_NESTED_NAMESPACE(std, _GLIBCXX_STD)
        */
       iterator
       insert(iterator __position, const value_type& __x)
-      { return _M_t.insert_equal(__position, __x); }
+      { return _M_t._M_insert_equal(__position, __x); }
 
       /**
        *  @brief A template function that attemps to insert a range of elements.
@@ -343,7 +343,7 @@ _GLIBCXX_BEGIN_NESTED_NAMESPACE(std, _GLIBCXX_STD)
       template <class _InputIterator>
         void
         insert(_InputIterator __first, _InputIterator __last)
-        { _M_t.insert_equal(__first, __last); }
+        { _M_t._M_insert_equal(__first, __last); }
 
       /**
        *  @brief Erases an element from a %multiset.
