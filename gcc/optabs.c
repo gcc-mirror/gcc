bc3839706c7259aa5e30b388e58fb555fd1f6b13
Ian Lance Taylor <ian@airs.com>
2006-01-08 20:52:57 +0000

    - Fix up date in ChangeLog.  Update copyrights in changed files.
    Co-Authored-By: David Edelsohn <edelsohn@gnu.org>
    
    From-SVN: r109475

--- gcc/optabs.c
+++ gcc/optabs.c
@@ -1,6 +1,6 @@
 /* Expand the basic unary and binary arithmetic operations, for GNU compiler.
    Copyright (C) 1987, 1988, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
+   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
