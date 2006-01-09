62479b5aadf80f84365f7d305fcd1ae7da1a73d5
hp <hp@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-01-09 04:47:07 +0000

    * gcc.dg/Wpadded.c: Gate alignment warning on ! default_packed. 	Fix typo in comment.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@109496 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/Wpadded.c
+++ gcc/testsuite/gcc.dg/Wpadded.c
@@ -2,10 +2,12 @@
 
 /* { dg-do compile } */
 /* -fpack-struct is necessary because the warning expected requires the initial
-   packing to be larger than 1, which cannot ge guaranteed for all targets. */
+   packing to be larger than 1, which cannot be guaranteed for all targets.
+   We won't get a warning anyway if the target has "packed" structure
+   layout.  */
 /* { dg-options "-Wpadded -fpack-struct=8" } */
 
 struct foo {
   char bar;
-  long baz;                   /* { dg-warning "padding struct to align" } */
+  long baz; /* { dg-warning "padding struct to align" ""  { target { ! default_packed } } } */
 } futz;
