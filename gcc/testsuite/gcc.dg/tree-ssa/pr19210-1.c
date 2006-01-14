7f17528ab58fa0a0f627b94bacbae686438b9bd4
Zdenek Dvorak <dvorakz@suse.cz>
2006-01-14 13:29:06 +0100

    tree-ssa-loop-niter.c (number_of_iterations_cond): Split into several functions.
    	* tree-ssa-loop-niter.c (number_of_iterations_cond): Split into several
    	functions.
    	(number_of_iterations_ne, number_of_iterations_lt_to_ne,
    	assert_no_overflow_lt, assert_loop_rolls_lt, number_of_iterations_lt,
    	number_of_iterations_le): New functions.
    	(number_of_iterations_special): Removed.
    	(number_of_iterations_exit): Do not use number_of_iterations_special.
    	* tree.c (unsigned_type_for): Always return integer type.
    
    	* gcc.dg/tree-ssa/pr19210-1.c: Update outcome.  Add new test loop.
    	* gcc.dg/tree-ssa/pr19210-2.c: Ditto.
    
    From-SVN: r109702

--- gcc/testsuite/gcc.dg/tree-ssa/pr19210-1.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr19210-1.c
@@ -12,9 +12,18 @@ f (unsigned n)
   for(k = 0;k <= n;k += 4) /* { dg-warning "cannot optimize.*overflow" } */
     g();
 
-  for(k = 5;k <= n;k += 5) /* { dg-warning "cannot optimize.*overflow" } */
+  /* We used to get warning for this loop.  However, since then # of iterations
+     analysis improved, and we can now prove that this loop does not verflow.
+     This is because the only case when it would overflow is if n = ~0 (since
+     ~0 is divisible by 5), and this cannot be the case, since when we got
+     here, the previous loop exited, thus there exists k > n.  */
+  for(k = 5;k <= n;k += 5)
     g();
 
+  /* So we need the following loop, instead.  */
+  for(k = 4;k <= n;k += 5) /* { dg-warning "cannot optimize.*overflow" } */
+    g();
+  
   for(k = 15;k >= n;k--) /* { dg-warning "cannot optimize.*infinite" } */
     g();
 }
