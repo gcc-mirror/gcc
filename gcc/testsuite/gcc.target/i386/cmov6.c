268de9b99fd9a7440549cf09781af0566ff076a1
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-01-15 02:49:17 +0000

    ./: 	* ifcvt.c (noce_init_if_info): New static function, broken out of 	noce_process_if_block. 	(noce_process_if_block): Call noce_init_if_info. 	(check_cond_move_block): New static function. 	(cond_move_process_if_block): New static function. 	(process_if_block): Call cond_move_process_if_block. testsuite/: 	* gcc.target/i386/cmov6.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@109717 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/cmov6.c
@@ -0,0 +1,22 @@
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -march=i686" } */
+/* { dg-final { scan-assembler "cmov\[^6\]" } } */
+
+/* Verify that blocks are converted to conditional moves.  */
+extern int bar (int, int);
+int foo (int c, int d, int e)
+{
+  int a, b;
+
+  if (c)
+    {
+      a = 10;
+      b = d;
+    }
+  else
+    {
+      a = e;
+      b = 20;
+    }
+  return bar (a, b);
+}
