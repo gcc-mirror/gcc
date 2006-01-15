02f08d219a3133e58fbac5d4e5f0ae1b96aeda21
Ian Lance Taylor <ian@airs.com>
2006-01-15 17:06:14 +0000

    re PR testsuite/25796 (gcc.target/i386/cmov6.c fails on x86_64-linux when supplied -m64)
    	PR testsuite/25796
    	* gcc.target/i386/cmov6.c: Use -march=k8.
    
    From-SVN: r109723

--- gcc/testsuite/gcc.target/i386/cmov6.c
+++ gcc/testsuite/gcc.target/i386/cmov6.c
@@ -1,5 +1,5 @@
 /* { dg-do compile { target i?86-*-* x86_64-*-* } } */
-/* { dg-options "-O2 -march=i686" } */
+/* { dg-options "-O2 -march=k8" } */
 /* { dg-final { scan-assembler "cmov\[^6\]" } } */
 
 /* Verify that blocks are converted to conditional moves.  */
