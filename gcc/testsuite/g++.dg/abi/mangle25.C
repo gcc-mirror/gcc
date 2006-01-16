d0f0488156b486f8f40d9951759945d4252d3424
Andreas Jaeger <aj@suse.de>
2006-01-16 08:06:06 +0100

    mangle25.C: Only run on 64-bit systems since 32-bit x86 does not have __float128.
            * g++.dg/abi/mangle25.C: Only run on 64-bit systems since 32-bit
            x86 does not have __float128.
    
    From-SVN: r109743

--- gcc/testsuite/g++.dg/abi/mangle25.C
+++ gcc/testsuite/g++.dg/abi/mangle25.C
@@ -4,9 +4,8 @@
 // ia64-hpux where "long double" is "e" and __float128 is synonymous with
 // "long double".
 // Origin: Joseph Myers <joseph@codesourcery.com>
-// { dg-do compile { target i?86-*-* x86_64-*-* ia64-*-* } } */
+// { dg-do compile { target { { i?86-*-* x86_64-*-* ia64-*-* } && lp64 } } }
 // { dg-options "" } */
-// { dg-options "-mmmx" { target { { i?86-*-* x86_64-*-* } && ilp32 } } } */
 // { dg-final { scan-assembler "_Z1fg" { target i?86-*-* x86_64-*-* } } } */
 // { dg-final { scan-assembler "_Z1fg" { target { ia64-*-* && { ! "ia64-*-hpux*" } } } } } */
 // { dg-final { scan-assembler "_Z1fe" { target ia64-*-hpux* } } } */
