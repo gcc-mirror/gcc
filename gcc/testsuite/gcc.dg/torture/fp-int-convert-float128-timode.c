badb0b073471d32b94f57fcce96d94827a25f8a0
Andreas Jaeger <aj@suse.de>
2006-01-17 19:45:06 +0100

    mangle25.C: Run always on ia64 but only on
    2006-01-17  Andreas Jaeger  <aj@suse.de>
                Janis Johnson <janis187@us.ibm.com>
    
            * g++.dg/abi/mangle25.C: Run always on ia64 but only on
            * x86/x86-64
            in 64-bit.
            * gcc.dg/torture/fp-int-convert-float128-timode.c: Likewise.
            * gcc.dg/torture/fp-int-convert-float128.c: Likewise.
    
    
    Co-Authored-By: Janis Johnson <janis187@us.ibm.com>
    
    From-SVN: r109832

--- gcc/testsuite/gcc.dg/torture/fp-int-convert-float128-timode.c
+++ gcc/testsuite/gcc.dg/torture/fp-int-convert-float128-timode.c
@@ -1,10 +1,9 @@
 /* Test floating-point conversions.  __float128 type with TImode.  */
 /* Origin: Joseph Myers <joseph@codesourcery.com> */
-/* { dg-do run { target i?86-*-* x86_64-*-* ia64-*-* } } */
+/* { dg-do run { target { { i?86-*-* x86_64-*-* ia64-*-* } && lp64 } } } */
 /* { dg-xfail-if "" { i?86-*-* x86_64-*-* } { "*" } { "" } } */
 /* { dg-xfail-if "" { ia64-*-* && lp64 } { "*" } { "" } } */
 /* { dg-options "" } */
-/* { dg-options "-mmmx" { target { { i?86-*-* x86_64-*-* } && ilp32 } } } */
 
 #include "fp-int-convert.h"
 
