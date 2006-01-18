c637eb15a2b0df85f831b3a38dd20e189533684d
Eric Christopher <echristo@gcc.gnu.org>
2006-01-18 02:13:02 +0000

    Update copyright from last checkin.
    From-SVN: r109866

--- gcc/cp/cvt.c
+++ gcc/cp/cvt.c
@@ -1,6 +1,6 @@
 /* Language-level data type conversion for GNU C++.
    Copyright (C) 1987, 1988, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
+   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006 Free Software Foundation, Inc.
    Hacked by Michael Tiemann (tiemann@cygnus.com)
 
 This file is part of GCC.
