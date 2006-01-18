1a890409547698eb67bf3bb59a4ecd2268af20b8
kazu <kazu@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-01-18 15:09:43 +0000

    * tree.c (find_tree_t, find_tree): Remove. 	* cp-tree.h: Remove the prototype for find_tree.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@109892 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/tree.c
+++ gcc/cp/tree.c
@@ -46,7 +46,6 @@ static cp_lvalue_kind lvalue_p_1 (tree, int);
 static tree build_target_expr (tree, tree);
 static tree count_trees_r (tree *, int *, void *);
 static tree verify_stmt_tree_r (tree *, int *, void *);
-static tree find_tree_r (tree *, int *, void *);
 static tree build_local_temp (tree);
 
 static tree handle_java_interface_attribute (tree *, tree, tree, int, bool *);
@@ -1035,27 +1034,6 @@ verify_stmt_tree (tree t)
   statements = htab_create (37, htab_hash_pointer, htab_eq_pointer, NULL);
   walk_tree (&t, verify_stmt_tree_r, &statements, NULL);
   htab_delete (statements);
-}
-
-/* Called from find_tree via walk_tree.  */
-
-static tree
-find_tree_r (tree* tp,
-	     int* walk_subtrees ATTRIBUTE_UNUSED ,
-	     void* data)
-{
-  if (*tp == (tree) data)
-    return (tree) data;
-
-  return NULL_TREE;
-}
-
-/* Returns X if X appears in the tree structure rooted at T.  */
-
-tree
-find_tree (tree t, tree x)
-{
-  return walk_tree_without_duplicates (&t, find_tree_r, x);
 }
 
 /* Check if the type T depends on a type with no linkage and if so, return
