084adcf22735a7ff0be2bf110cf1f8a6380e59fa
Volker Reichelt <reichelt@igpm.rwth-aachen.de>
2006-01-19 17:33:07 +0000

    re PR c++/25854 (Bogus diagnostic with '<type error>')
    	PR c++/25854
    	* pt.c (maybe_process_partial_specialization): Return early on
    	error_mark_node.
    
    	* g++.dg/template/spec28.C: New test.
    
    From-SVN: r109976

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -695,8 +695,12 @@ check_explicit_instantiation_namespace (tree spec)
 void
 maybe_process_partial_specialization (tree type)
 {
-  /* TYPE maybe an ERROR_MARK_NODE.  */
-  tree context = TYPE_P (type) ? TYPE_CONTEXT (type) : NULL_TREE;
+  tree context;
+
+  if (type == error_mark_node)
+    return;
+
+  context = TYPE_CONTEXT (type);
 
   if (CLASS_TYPE_P (type) && CLASSTYPE_USE_TEMPLATE (type))
     {
