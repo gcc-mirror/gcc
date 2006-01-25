bb27328b679b26765e73e4d797e6e3eb4d63b5ee
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-01-25 01:29:14 +0000

    2006-01-24  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR fortran/25835
    	* gfortran.dg/read_eof_1.f90: Rename from read_eof_1.f90.
    	* gfortran.dg/read_eof_2.f90: New test.
    	* gfortran.dg/read_eof_3.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@110201 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/read_eof_2.f90
@@ -0,0 +1,21 @@
+! { dg-do run }
+! PR25835 Check that reading from a file that is at end-of-file does not
+! segfault or give error.  Test case derived from example in PR from Dale Ranta.
+! Contributed by Jerry DeLisle  <jvdelisle@gcc.gnu.org>
+       integer data(2045) ! Exceed internal buffer size
+       data=-1
+       open(unit=11,status='scratch', form='unformatted')
+       write(11)data
+       read(11,end=        1000 )data
+       call abort()
+ 1000  continue
+       backspace 11
+       backspace 11
+       data = 0
+       read(11)data
+       if (.not. all(data == -1)) call abort()
+       read(11,end=        1002 )data
+       call abort()
+ 1002  continue
+       close(11)
+       end
