ff346fdbb168b9b03eb0fd496a598da20165db6c
Tobias Schlüter <tobias.schlueter@physik.uni-muenchen.de>
2006-01-25 23:34:17 +0100

    re PR fortran/18540 (Jumping into blocks gives error rather than warning)
    PR fortran/18540
    * gfortran.dg/goto_1.f: New.
    
    From-SVN: r110228

---
+++ gcc/testsuite/gfortran.dg/goto_1.f
@@ -0,0 +1,15 @@
+! { dg-do run }
+! { dg-options "-std=legacy" }
+! PR 18540
+! Verify that old-style cross-block GOTOs work
+      I = 1
+      GO TO 2
+      IF (I .EQ. 0) THEN
+ 2       IF (I .NE. 1) CALL ABORT
+         I = 0
+         GOTO 3
+      ELSE
+ 3       I = 2
+      END IF
+      IF (I .NE. 2) CALL ABORT
+      END
