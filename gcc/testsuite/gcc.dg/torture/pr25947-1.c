2ec31ce6355795a61cf6a0a4b5a6402da38cc839
Hans-Peter Nilsson <hp@axis.com>
2006-01-26 02:53:15 +0000

    re PR target/25947 (Unrecognizable insn ICE for readline-5.1 in rl_vi_complete)
    	PR target/25947
    	* gcc.dg/torture/pr25947-1.c: New test.
    
    From-SVN: r110244

---
+++ gcc/testsuite/gcc.dg/torture/pr25947-1.c
@@ -0,0 +1,21 @@
+/* PR target/25947: define_split in cris.md caused unrecognized insn.  */
+/* { dg-options "-fpic" { target fpic } } */
+
+extern char *rl_line_buffer;
+extern int rl_point;
+extern int rl_end;
+static const char *vi_motion = " hl^$0ftFT;,%wbeWBE|";
+void
+rl_vi_complete (int ignore, int key)
+{
+  if ((rl_point < rl_end)
+      &&
+      (!(((rl_line_buffer[rl_point]) == ' ')
+	 || ((rl_line_buffer[rl_point]) == '\t'))))
+    {
+      if (!
+	  (((rl_line_buffer[rl_point + 1]) == ' ')
+	   || ((rl_line_buffer[rl_point + 1]) == '\t')))
+	rl_vi_end_word (1, 'E');
+    }
+}
