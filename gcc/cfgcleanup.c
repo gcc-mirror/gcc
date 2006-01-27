dccb60e93b6fe1b6acf55c4795815177981595e6
Gabor Loki <loki@gcc.gnu.org>
2006-01-27 10:59:40 +0000

    cfgcleanup.c (try_crossjump_to_edge): Fix comment typo.
    2006-01-27 Gabor Loki <loki@gcc.gnu.org>
    
    	* cfgcleanup.c (try_crossjump_to_edge): Fix comment typo.
    
    From-SVN: r110297

--- gcc/cfgcleanup.c
+++ gcc/cfgcleanup.c
@@ -1701,7 +1701,7 @@ try_crossjump_to_edge (int mode, edge e1, edge e2)
       && (newpos1 != BB_HEAD (src1)))
     return false;
 
-  /* Avoid deleting preseve label when redirecting ABNORMAL edeges.  */
+  /* Avoid deleting preserve label when redirecting ABNORMAL edeges.  */
   if (block_has_preserve_label (e1->dest)
       && (e1->flags & EDGE_ABNORMAL))
     return false;
