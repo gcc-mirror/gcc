5c31de1247721a9db589ddfd5d74740b7288306e
Ben Elliston <bje@au.ibm.com>
2006-01-30 23:11:35 +0000

    * mangle.c: Comment fix.
    From-SVN: r110413

--- gcc/cp/mangle.c
+++ gcc/cp/mangle.c
@@ -1,7 +1,7 @@
 /* Name mangling for the 3.0 C++ ABI.
    Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005
    Free Software Foundation, Inc.
-   Written by Alex Samuel <sameul@codesourcery.com>
+   Written by Alex Samuel <samuel@codesourcery.com>
 
    This file is part of GCC.
 
