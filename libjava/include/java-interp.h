e939885ff55dc2a789f35c222ae2ef8066da91f5
Keith Seitz <keiths@redhat.com>
2006-01-30 21:20:40 +0000

    java-interp.h (insn_index): New declaration.
            * include/java-interp.h (insn_index): New declaration.
            (num_insn_slots): New private variable.
            (get_line_table): New declaration.
            * interpret.cc (insn_index): New function.
            (get_line_table): New function.
    
    From-SVN: r110411

--- libjava/include/java-interp.h
+++ libjava/include/java-interp.h
@@ -145,6 +145,7 @@ class _Jv_InterpMethod : public _Jv_MethodBase
   _Jv_LineTableEntry *line_table;
 
   void *prepared;
+  int number_insn_slots;
 
   unsigned char* bytecode () 
   {
@@ -182,8 +183,23 @@ class _Jv_InterpMethod : public _Jv_MethodBase
   // number info is unavailable.
   int get_source_line(pc_t mpc);
 
+#ifdef DIRECT_THREADED
+  // Convenience function for indexing bytecode PC/insn slots in
+  // line tables for JDWP
+  jlong insn_index (pc_t pc);
+#endif
+
  public:
   static void dump_object(jobject o);
+
+  /* Get the line table for this method.
+   * start  is the lowest index in the method
+   * end    is the  highest index in the method
+   * line_numbers is an array to hold the list of source line numbers
+   * code_indices is an array to hold the corresponding list of code indices
+   */
+  void get_line_table (jlong& start, jlong& end, jintArray& line_numbers,
+		       jlongArray& code_indices);
 
 #ifdef DIRECT_THREADED
   friend void _Jv_CompileMethod (_Jv_InterpMethod*);
