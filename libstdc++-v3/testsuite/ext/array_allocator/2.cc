f84d5bf1f86a94f2e954a8be3fc103b920ae9fca
Paolo Carlini <pcarlini@suse.de>
2006-02-01 21:57:48 +0000

    re PR libstdc++/21554 (ext/array_allocator/2.cc execution fails)
    2006-02-01  Paolo Carlini  <pcarlini@suse.de>
    
    	PR libstdc++/21554
    	* include/tr1/array (array<>::_M_instance): Maximally align.
    	* testsuite/ext/array_allocator/2.cc: Do not xfail for powerpc.
    
    From-SVN: r110482

--- libstdc++-v3/testsuite/ext/array_allocator/2.cc
+++ libstdc++-v3/testsuite/ext/array_allocator/2.cc
@@ -1,7 +1,4 @@
-// Expected execution error for PR19495.
-// { dg-do run { xfail powerpc*-*-linux* } }
-
-// Copyright (C) 2004, 2005 Free Software Foundation, Inc.
+// Copyright (C) 2004, 2005, 2006 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
