512abe51e107f173b227e5935f5ac14fe0dac955
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-02-02 19:45:05 +0000

    2006-02-02  Paolo Carlini  <pcarlini@suse.de>
    	* include/tr1/cfloat: New.
    	* include/tr1/climits: Likewise.		
    	* include/tr1/cstdarg: Likewise.
    	* include/tr1/ctype.h: Likewise.
    	* include/tr1/fenv.h: Likewise.
    	* include/tr1/float.h: Likewise.
    	* include/tr1/inttypes.h: Likewise.
    	* include/tr1/limits.h: Likewise.
    	* include/tr1/stdint.h: Likewise.
    	* include/tr1/stdarg.h: Likewise.
    	* include/Makefile.am: Add.
    	* include/Makefile.in: Regenerate.
    	* docs/html/ext/tr1.html: Update.
    	* testsuite/tr1/headers.cc: New.
    
    	* include/tr1/cctype: Include <cctype> instead.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@110519 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/tr1/headers.cc
@@ -0,0 +1,46 @@
+// { dg-do compile }
+
+// 2006-02-02  Paolo Carlini  <pcarlini@suse.de>
+//
+// Copyright (C) 2006 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#include <tr1/array>
+#include <tr1/cctype>
+#include <tr1/cfenv>
+#include <tr1/cfloat>
+#include <tr1/cinttypes>
+#include <tr1/climits>
+#include <tr1/complex>
+#include <tr1/cstdarg>
+#include <tr1/cstdint>
+#include <tr1/ctype.h>
+#include <tr1/fenv.h>
+#include <tr1/float.h>
+#include <tr1/functional>
+#include <tr1/hashtable>
+#include <tr1/inttypes.h>
+#include <tr1/limits.h>
+#include <tr1/memory>
+#include <tr1/stdarg.h>
+#include <tr1/stdint.h>
+#include <tr1/tuple>
+#include <tr1/type_traits>
+#include <tr1/unordered_set>
+#include <tr1/unordered_map>
+#include <tr1/utility>
