6094df5ef4215bb2020b9960814dd512e400e252
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-02-05 19:52:35 +0000

    2006-02-05  H.J. Lu  <hongjiu.lu@intel.com>
    	PR fortran/26041
    	PR fortran/26064
    	* gfortran.dg/sibling_dummy_procedure_1.f90: New file.
    	* gfortran.dg/sibling_dummy_procedure_2.f90: Likewise.
    	* gfortran.dg/sibling_dummy_procedure_3.f90: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@110618 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/sibling_dummy_procedure_2.f90
@@ -0,0 +1,32 @@
+! { dg-do compile }
+! This checks the fix for PR 26041.
+!
+! Contributed by H.J. Lu  <hongjiu.lu@intel.com>
+module foo
+   public    bar_
+   interface bar_
+      module procedure bar
+   end interface
+   public    xxx_
+   interface xxx_
+      module procedure xxx
+   end interface
+contains
+   subroutine bar(self, z)
+      interface
+         function self(z) result(res)
+	    real z
+            real(kind=kind(1.0d0)) :: res
+         end function
+      end interface
+   end subroutine
+   subroutine xxx(self,z)
+      interface
+         function self(z) result(res)
+	    real z
+            real(kind=kind(1.0d0)) :: res
+         end function
+      end interface
+      call bar_(self, z)
+   end subroutine
+end
