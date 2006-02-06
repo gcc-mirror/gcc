b5140ad4701dbf7e46a97294fb2f9aced01455f9
Anthony Green <green@redhat.com>
2006-02-06 22:06:21 +0000

    XmlParser.java: Add missing break;.
    2006-02-06  Anthony Green  <green@redhat.com>
    
    	* gnu/xml/aelfred2/XmlParser.java: Add missing break;.
    
    From-SVN: r110667

--- libjava/classpath/gnu/xml/aelfred2/XmlParser.java
+++ libjava/classpath/gnu/xml/aelfred2/XmlParser.java
@@ -1,5 +1,5 @@
 /* XmlParser.java -- 
-   Copyright (C) 1999,2000,2001 Free Software Foundation, Inc.
+   Copyright (C) 1999,2000,2001, 2006 Free Software Foundation, Inc.
 
 This file is part of GNU Classpath.
 
@@ -2185,6 +2185,7 @@ loop:
                   {
                     nest++;
                   }
+		break;
               case ']':
                 if (tryRead("]>"))
                   {
