814a372047ba168eb75ccf3944bc7fbc4d9bb195
Tobias Schlüter <tobi@gcc.gnu.org>
2006-02-08 14:14:43 +0100

    re PR libfortran/25577 (gfortran routine mvbits returns wrong answer.)
    fortran/
    2006-02-07  Dale Ranta  <dir@lanl.gov>
    
    	PR fortran/25577
    	* intrinsics/mvbits.c: Shift '(TYPE)1' type when building 'lenmask'.
    
    testsuite/
    2006-02-08  Tobias Schlueter  <tobias.schlueter@physik.uni-muenchen.de>
    
    	PR fortran/25577
    	* gfortran.dg/mvbits_1.f90: New.
    
    From-SVN: r110756

--- libgfortran/intrinsics/mvbits.c
+++ libgfortran/intrinsics/mvbits.c
@@ -1,6 +1,6 @@
 /* Implementation of the MVBITS intrinsic
-   Copyright (C) 2004 Free Software Foundation, Inc.
-   Contributed by Tobias SchlÃ¼ter
+   Copyright (C) 2004, 2006 Free Software Foundation, Inc.
+   Contributed by Tobias Schlüter
 
 This file is part of the GNU Fortran 95 runtime library (libgfortran).
 
@@ -48,7 +48,7 @@ SUB_NAME (const TYPE *from, const GFC_INTEGER_4 *frompos,
 {
   TYPE oldbits, newbits, lenmask;
 
-  lenmask = (*len == sizeof (TYPE)*8) ? ~(TYPE)0 : (1 << *len) - 1;
+  lenmask = (*len == sizeof (TYPE)*8) ? ~(TYPE)0 : ((TYPE)1 << *len) - 1;
   newbits = (((UTYPE)(*from) >> *frompos) & lenmask) << *topos;
   oldbits = *to & (~(lenmask << *topos));
 
