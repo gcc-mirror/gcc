2a5af6bfe776fbcd5d4e5de33072bd8a36d1bcea
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-02-10 19:22:58 +0000

    PR tree-optimization/26213 	* tree-ssa-threadedge.c (simplify_control_stmt_condition): Do not 	loop trying to follow SSA_NAME_VALUE chains.
    	* gcc.c-torture/compile/pr26213.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@110846 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr26213.c
@@ -0,0 +1,15 @@
+void
+xnanosleep (_Bool overflow)
+{
+  struct { int tv_nsec; } ts_sleep;
+  if (0 <= ts_sleep.tv_nsec)
+    overflow |= 1;
+
+  for (;;)
+    {
+      if (overflow)
+        ts_sleep.tv_nsec = 0;
+      if (foo (ts_sleep))
+        break;
+    }
+}
