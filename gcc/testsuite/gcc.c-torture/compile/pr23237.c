4c553323a6f4d90e48002ea23b7f779c106ad620
Josh Conner <jconner@apple.com>
2006-02-13 18:12:17 +0000

    re PR target/25376 (section attribute doesn't work on darwin)
    PR target/25376
    * varasm.c (function_section): Check for section name before
    calling select_section on targets that define
    USE_SELECT_SECTION_FOR_FUNCTIONS.  On other targets, use
    unlikely_text_section instead of hot_function_section if
    first_function_block_is_cold.
    * gcc.dg/pr25376.c: New test.
    
    * gcc.c-torture/compile/pr23237.c: Only compile test -- don't
    assemble.
    * lib/target-supports.exp (check_named_sections_available):
    Only compile named-sections test -- don't assemble.
    
    From-SVN: r110917

--- gcc/testsuite/gcc.c-torture/compile/pr23237.c
+++ gcc/testsuite/gcc.c-torture/compile/pr23237.c
@@ -1,5 +1,9 @@
 /* { dg-require-effective-target named_sections } */
 
+/* Don't assemble, as this section syntax may not be valid on all platforms
+   (e.g., Darwin).  */
+/* { dg-do compile } */
+
 static __attribute__ ((__section__ (".init.data"))) char *message;
 static __attribute__ ((__section__ (".init.data"))) int (*actions[])(void) = {};
 void unpack_to_rootfs(void)
