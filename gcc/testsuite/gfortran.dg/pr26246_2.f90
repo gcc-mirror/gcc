9904271428c90429e4b522cf6f4d5c51ca33e20c
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-02-13 21:31:18 +0000

    2006-02-13  Andrew Pinski  <pinskia@physics.uc.edu> 	    Jakub Jelinek  <jakub@redhat.com>
    	PR fortran/26246
    	* trans-decl.c (gfc_get_symbol_decl, gfc_get_fake_result_decl): Use
    	gfc_add_decl_to_function rather than gfc_finish_var_decl on length.
    
    	* gfortran.dg/pr26246_1.f90: New test.
    	* gfortran.dg/pr26246_2.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@110929 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr26246_2.f90
@@ -0,0 +1,13 @@
+! PR fortran/26246
+! { dg-options "-fdump-tree-original -fno-automatic" }
+! { dg-do compile }
+
+subroutine foo(string, n)
+  implicit none
+  integer :: n
+  character(len=n + 6), intent(in) :: string
+  if (string .eq. 'abc') call abort
+end subroutine foo
+
+! { dg-final { scan-tree-dump-times "static int" 0 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
