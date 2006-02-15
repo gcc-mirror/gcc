126d39beacc8e4e8e7610769b769e89cd3dbdc3d
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-02-15 09:33:28 +0000

    2006-02-13  Robert Dewar  <dewar@adacore.com>
    	* a-ticoio.ads, a-ticoio.adb: Add use clause (moved here from spec)
    
    	* a-coteio.ads, a-lcteio.ads, a-llctio.ads, a-scteio.ads: New files.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@111038 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/ada/a-scteio.ads
@@ -0,0 +1,24 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                         GNAT RUN-TIME COMPONENTS                         --
+--                                                                          --
+--             A D A . S H O R T _ C O M P L E X _ T E X T _ I O            --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+-- This specification is adapted from the Ada Reference Manual for use with --
+-- GNAT.  In accordance with the copyright of that document, you can freely --
+-- copy and modify this specification,  provided that if you redistribute a --
+-- modified version,  any changes that you have made are clearly indicated. --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  Ada 2005 AI-328
+
+with Ada.Text_IO.Complex_IO;
+with Ada.Numerics.Short_Complex_Types;
+
+pragma Elaborate_All (Ada.Text_IO.Complex_IO);
+
+package Ada.Short_Complex_Text_IO is
+  new Ada.Text_IO.Complex_IO (Ada.Numerics.Short_Complex_Types);
