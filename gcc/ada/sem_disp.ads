3bcd6930a9492b00032d996ab3c700272d878f1e
Javier Miranda <miranda@adacore.com>
2006-02-15 10:43:23 +0100

    restrict.ads (No_Dispatching_Calls): New GNAT restriction.
    2006-02-13  Javier Miranda  <miranda@adacore.com>
    	    Gary Dismukes  <dismukes@adacore.com>
    	    Robert Dewar  <dewar@adacore.com>
    
    	* restrict.ads (No_Dispatching_Calls): New GNAT restriction.
    
    	* sem_disp.adb (Override_Dispatching_Operation): Traverse the list of
    	aliased entities to look for the overriden abstract interface
    	subprogram.
    	(Is_Interface_Subprogram): Complete documentation.
    	(Check_Dispatching_Operation): Do not generate code to register the
    	operation in the dispatch table if the source is compiled with
    	restriction No_Dispatching_Calls.
    	(Override_Dispatching_Operation): Check for illegal attempt to override
    	No_Return procedure with procedure that is not No_Return
    	(Check_Dispatching_Call): Suppress the check for an abstract operation
    	when the original node of an actual is a tag-indeterminate attribute
    	call, since the attribute, which must be 'Input, can never be abstract.
    	(Is_Tag_Indeterminate): Handle checking of tag indeterminacy of a
    	call to the Input attribute (even when rewritten).
    	(Propagate_Tag): Augment comment to indicate the possibility of a call
    	to an Input attribute.
    
    	* sem_disp.ads (Override_Dispatching_Operation): Moved to spec to allow
    	calling it from Exp_Ch3.Make_Controlling_Function_Wrappers.
    
    	* s-rident.ads: (No_Dispatching_Calls): New GNAT restriction.
    	No_Wide_Characters is no longer partition-wide
    	No_Implementation_Attributes/Pragmas are now Ada 2005 (AI-257)
    	rather than GNAT
    
    From-SVN: r111086

--- gcc/ada/sem_disp.ads
+++ gcc/ada/sem_disp.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---          Copyright (C) 1992-2002 Free Software Foundation, Inc.          --
+--          Copyright (C) 1992-2005, Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -79,6 +79,14 @@ package Sem_Disp is
    --  An expression is tag-indeterminate if it is a call that dispatches
    --  on result, and all controlling operands are also indeterminate.
    --  Such a function call may inherit a tag from an enclosing call.
+
+   procedure Override_Dispatching_Operation
+     (Tagged_Type : Entity_Id;
+      Prev_Op     : Entity_Id;
+      New_Op      : Entity_Id);
+   --  Replace an implicit dispatching operation with an explicit one.
+   --  Prev_Op is an inherited primitive operation which is overridden
+   --  by the explicit declaration of New_Op.
 
    procedure Propagate_Tag (Control : Node_Id; Actual : Node_Id);
    --  If a function call is tag-indeterminate,  its controlling argument is
