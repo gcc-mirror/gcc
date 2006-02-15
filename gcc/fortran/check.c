8f320bb0973118594dc10fe9d11ba2f854c6eda9
Tobias Schlüter <tobias.schlueter@physik.uni-muenchen.de>
2006-02-15 13:42:16 +0100

    check.c: Update copyright years.
    	* check.c: Update copyright years.
    
    From-SVN: r111110

--- gcc/fortran/check.c
+++ gcc/fortran/check.c
@@ -1,5 +1,5 @@
 /* Check functions
-   Copyright (C) 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2003, 2004, 2005, 2006 Free Software Foundation, Inc.
    Contributed by Andy Vaught & Katherine Holcomb
 
 This file is part of GCC.
