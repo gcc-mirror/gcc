3000c6f36a67beab50ec808eb1a0af5f113ffc0d
victork <victork@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-02-16 09:59:00 +0000

    testsuite/ 2006-02-16  Victor Kaplansky  <victork@il.ibm.com>
    	PR tree-opt/25918
    	* lib/target-supports.exp
    	(check_effective_target_vect_short_mult): New.
    	(check_effective_target_vect_char_mult): New.
    	(check_effective_target_vect_widen_sum_qi_to_si): New.
    	(check_effective_target_vect_widen_sum_qi_to_hi): New.
    	(check_effective_target_vect_widen_sum_hi_to_si): New.
    	* gcc.dg/vect/vect-reduc-dot-s16.c: Remove, split into
    	vect-reduc-dot-s16a.c and vect-reduc-dot-s16b.c
    	* vect-reduc-dot-s16a.c: New, split from vect-reduc-dot-s16.c.
    	* vect-reduc-dot-s16b.c: New, split from vect-reduc-dot-s16.c.
    	* gcc.dg/vect/vect-reduc-pattern-2.c: Remove, split into
    	vect-reduc-pattern-2a.c, vect-reduc-pattern-2b.c and
    	vect-reduc-pattern-2c.c
    	* gcc.dg/vect/vect-reduc-pattern-1.c: Remove, split into
    	vect-reduc-pattern-1a.c, vect-reduc-pattern-1b.c and
    	vect-reduc-pattern-1c.c
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@111135 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/vect-reduc-pattern-2c.c
@@ -0,0 +1,48 @@
+/* { dg-require-effective-target vect_int } */
+
+#include <stdarg.h>
+#include "tree-vect.h"
+
+#define N 16
+signed char data_ch[N] =
+  { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28 };
+#define SUM 210
+
+int
+foo ()
+{
+  int i;
+  signed short shortsum = 0;
+
+  /* widenning sum: sum chars into short.
+     The widening-summation pattern is currently not detected because of this
+     patch:
+
+     2005-12-26  Kazu Hirata  <kazu@codesourcery.com>
+                                                                                
+        PR tree-optimization/25125
+   */
+
+  for (i = 0; i < N; i++)
+    {
+      shortsum += data_ch[i];
+    }
+
+  /* check results:  */
+  if (shortsum != SUM)
+    abort ();
+
+  return 0;
+}
+
+int
+main (void)
+{
+  check_vect ();
+  return foo ();
+}
+
+/* { dg-final { scan-tree-dump-times "vect_recog_widen_sum_pattern: detected" 1 "vect" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 0 "vect" { target { ! vect_widen_sum_qi_to_hi } } } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
