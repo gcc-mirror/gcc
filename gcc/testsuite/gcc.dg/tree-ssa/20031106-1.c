c8ca29f104d4e1294ecd13288d2e02b5f5e29e42
Andrew Pinski <pinskia@physics.uc.edu>
2006-02-18 21:04:57 +0000

    20031106-1.c: Fix the final scan of the variable to take into account the variable name in...
    2006-02-18  Andrew Pinski  <pinskia@physics.uc.edu>
    
            * gcc.dg/tree-ssa/20031106-1.c: Fix the final scan of the
            variable to take into account the variable name in the
            function header.
            * gcc.dg/tree-ssa/20031106-2.c: Likewise.
    
    From-SVN: r111250

--- gcc/testsuite/gcc.dg/tree-ssa/20031106-1.c
+++ gcc/testsuite/gcc.dg/tree-ssa/20031106-1.c
@@ -13,8 +13,8 @@ void foo (int testarray[])
     link_error ();
 }
 
-/* There should be only one reference to "testarray".  */
-/* { dg-final { scan-tree-dump-times "testarray" 1 "optimized" { xfail *-*-* } } } */
+/* There should be only one reference to "testarray" and one in the function header.  */
+/* { dg-final { scan-tree-dump-times "testarray" 2 "optimized" } } */
 
 /* There should be no link_error calls.  */
 /* { dg-final { scan-tree-dump-times "link_error" 0 "optimized"} } */
--- gcc/testsuite/gcc.dg/tree-ssa/20031106-2.c
+++ gcc/testsuite/gcc.dg/tree-ssa/20031106-2.c
@@ -20,8 +20,8 @@ void foo (struct s* teststruct)
     link_error ();
 }
 
-/* There should be only one reference to "teststruct".  */
-/* { dg-final { scan-tree-dump-times "teststruct" 1 "optimized" { xfail *-*-* } } } */
+/* There should be only one reference to "teststruct" and one in the function header.  */
+/* { dg-final { scan-tree-dump-times "teststruct" 2 "optimized" } } */
 
 /* There should be no link_error calls.  */
 /* { dg-final { scan-tree-dump-times "link_error" 0 "optimized"} } */
