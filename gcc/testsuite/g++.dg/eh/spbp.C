269dcd9d696b8606469c8b306fcba0a2553aff40
geoffk <geoffk@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-02-21 20:36:35 +0000

    * config/i386/darwin.h (DWARF2_FRAME_REG_OUT): New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@111344 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/eh/spbp.C
@@ -0,0 +1,26 @@
+/* { dg-do run } */
+/* { dg-options "-gdwarf-2" } */
+
+/* This was a bug on x86-darwin, where the register numbering for SP
+   and BP was swapped (it's easy to do because on that port it's
+   different for eh_frame and debug_frame).  */
+
+#include <stdlib.h>
+
+void f1(int t)
+{
+  char u[t];
+  throw 1;
+}
+
+int main()
+{
+  bool b = true;
+  try {
+    f1(100);
+  } catch (int x) {
+    if (b)
+      exit (0);
+  }
+  abort ();
+}
