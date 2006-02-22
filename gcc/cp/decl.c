33b91f7e4881d2c9149c0aa62066d668e4353f3d
reichelt <reichelt@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-02-22 17:22:08 +0000

    PR c++/26291 	* decl.c (grok_op_properties): Check for ellipsis in arguments of         operators.
    	* g++.dg/other/ellipsis1.C: New test.
    	* g++.dg/parse/operator4.C: Adjust error marker.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@111367 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/decl.c
+++ gcc/cp/decl.c
@@ -8828,13 +8828,15 @@ grok_op_properties (tree decl, bool complain)
   tree name = DECL_NAME (decl);
   enum tree_code operator_code;
   int arity;
+  bool ellipsis_p;
   tree class_type;
 
-  /* Count the number of arguments.  */
+  /* Count the number of arguments and check for ellipsis.  */
   for (argtype = argtypes, arity = 0;
        argtype && argtype != void_list_node;
        argtype = TREE_CHAIN (argtype))
     ++arity;
+  ellipsis_p = !argtype;
 
   class_type = DECL_CONTEXT (decl);
   if (class_type && !CLASS_TYPE_P (class_type))
@@ -9000,11 +9002,14 @@ grok_op_properties (tree decl, bool complain)
 		     "conversion operator",
 		     ref ? "a reference to " : "", what);
 	}
+
       if (operator_code == COND_EXPR)
 	{
 	  /* 13.4.0.3 */
 	  error ("ISO C++ prohibits overloading operator ?:");
 	}
+      else if (ellipsis_p)
+	error ("%qD must not have variable number of arguments", decl);
       else if (ambi_op_p (operator_code))
 	{
 	  if (arity == 1)
