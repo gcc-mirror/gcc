f064ebd17debbb055d5cb445176fea22ea1eedbb
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-02-23 22:47:58 +0000

    PR tree-optimization/26425 	* tree-vrp.c (vrp_visit_assignment): If the LHS's type has a NULL 	min/max, then assume its varying.
    	* gcc.c-torture/compile/pr26425.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@111399 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr26425.c
@@ -0,0 +1,9 @@
+struct keyring_list {
+ struct key *keys[0];
+};
+void keyring_destroy(struct keyring_list *keyring, unsigned short a)
+{
+ int loop;
+  for (loop = a - 1; loop >= 0; loop--)
+   key_put(keyring->keys[loop]);
+}
