eb8dffe0fbff242efc573fba9eff7a1da7eb893f
Roger Sayle <roger@eyesopen.com>
2006-02-25 22:27:54 +0000

    re PR middle-end/23673 (fold does not fold (a^b) != 0 to a != b)
    	PR middle-end/23673
    	* fold-const.c (fold_binary) <EQ_EXPR>:  Fold (X^Y) == 0 as X == Y
    	and (X^Y) != 0 as X != Y.  Fold (X^Y) == Y as X == 0, and some
    	symmetry related transformations.  Fold (X^C1) == C2 as
    	X == (C1^C2).
    
    	* gcc.dg/fold-eqxor-1.c: New test case.
    	* gcc.dg/fold-eqxor-2.c: Likewise.
    	* gcc.dg/fold-eqxor-3.c: Likewise.
    
    From-SVN: r111442

---
+++ gcc/testsuite/gcc.dg/fold-eqxor-2.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-original" } */
+
+int test1(int a, int b)
+{
+  return (a ^ b) == a;
+}
+
+int test2(int c, int d)
+{
+  return (c ^ d) != c;
+}
+
+int test3(int e, int f)
+{
+  return (e ^ f) == f;
+}
+
+int test4(int g, int h)
+{
+  return (g ^ h) != h;
+}
+
+/* { dg-final { scan-tree-dump-times "b == 0" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "d != 0" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "e == 0" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "g != 0" 1 "original" } } */
+/* { dg-final { cleanup-tree-dump "original" } } */
