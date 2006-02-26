1d8ff0d259a49c3a172fb5ef215cb01dcec2605e
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-02-26 15:36:52 +0000

    2006-02-26  Roger Sayle  <roger@eyesopen.com> 	    James A. Morrison  <phython@gcc.gnu.org>
    	PR middle-end/21137
    	* fold-const.c (fold_binary) <EQ_EXPR>:  Fold ((X>>C1)&C2) eq/ne 0,
    	when C2 is a power of two, as either (X&(C2<<C1)) eq/ne 0 if the
    	new constant C2<<C1, or as (X<0) or (X,false) depending upon the
    	signedness of the shift operation.
    
    	* gcc.dg/fold-eqandshift-1.c: New test case.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@111453 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/fold-eqandshift-1.c
@@ -0,0 +1,46 @@
+/* PR middle-end/21137  */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-original" } */
+extern void foo();
+
+void test1(int a)
+{
+  if ((a >> 3) & 1)
+    foo ();
+}
+
+void test2(int b)
+{
+  if ((b >> 3) & 4)
+    foo ();
+}
+
+int test3(int c)
+{
+  return (c >> 3) & 1;
+}
+
+int test4(int d)
+{
+  return (d >> 3) & 4;
+}
+
+#if 0
+void test5(int e)
+{
+  if ((e >> 31) & 64)
+    foo();
+}
+
+void test6(unsigned int f)
+{
+  if ((f >> 31) & 64)
+    foo();
+}
+#endif
+
+/* { dg-final { scan-tree-dump-times "\\(a \& 8\\) != 0" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "\\(b \& 32\\) != 0" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "c >> 3 \& 1" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "d >> 3 \& 4" 1 "original" } } */
+/* { dg-final { cleanup-tree-dump "original" } } */
