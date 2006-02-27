124656c573d45c24bab4a768c2c4b90f3703aa92
Steven Bosscher <stevenb.gcc@gmail.com>
2006-02-27 21:12:54 +0000

    backport: re PR rtl-optimization/25196 (i386: wrong arguments passed)
    	Backport from mainline and the GCC 4.1 branch:
    	PR rtl-optimization/25196
    	* postreload-gcse.c (record_last_set_info): Notice stack pointer
    	changes in push insns without REG_INC notes.
    
    From-SVN: r111490

---
+++ gcc/testsuite/gcc.dg/pr25196.c
@@ -0,0 +1,34 @@
+/* { dg-do run { target i?86-*-* x86_64-*-* } } */
+/* { dg-require-effective-target ilp32 } */
+/* { dg-options "-march=i386 -O3 -fomit-frame-pointer" } */
+
+/* For this test case, we used to do an invalid load motion after
+   reload, because we missed autoincrements of the stack pointer.  */
+
+extern void abort (void);
+
+static int j;
+
+static void __attribute__((noinline))
+f1 (int a, int b, int c, int d, int e)
+{
+  j = a;
+}
+
+int __attribute__((noinline))
+f2 (int a, int b, int c, int d, int e)
+{
+  if ((b & 0x1111) != 1)
+    f1 (a, b, c, d, e);
+  return 0;
+}
+
+int
+main (void)
+{
+  f2 (123, 0, 0, 0, 0);
+  if (j != 123)
+    abort ();
+  return 0;
+}
+
