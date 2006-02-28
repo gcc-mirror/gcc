c457fcec8e54df75ee6c133a79cee1417523af59
Roger Sayle <roger@eyesopen.com>
2006-02-28 21:31:29 +0000

    re PR tree-optimization/14752 ([tree-ssa] "~a" should be changed to "a" if used in the condition of an if statement)
    	PR middle-end/14752
    	* c-common.c (c_common_truthvalue_conversion) <MINUS_EXPR,
    	BIT_XOR_EXPR, BIT_AND_EXPR>: Delete.  Let fold optimize these
    	cases via the construction of "expr != 0".
    
    	* gcc.dg/fold-eqandshift-2.c: New test case.
    
    From-SVN: r111575

---
+++ gcc/testsuite/gcc.dg/fold-eqandshift-2.c
@@ -0,0 +1,22 @@
+/* PR middle-end/14752 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-original" } */
+
+void bar (void);
+
+void foo (unsigned int a)
+{
+  if ((a >> 2) & 1)
+    bar ();
+}
+
+void baz (unsigned int b)
+{
+  if ((~b >> 2) & 1)
+    bar ();
+}
+
+/* { dg-final { scan-tree-dump-times "\\(a \& 4\\) != 0" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "\\(b \& 4\\) == 0" 1 "original" } } */
+/* { dg-final { cleanup-tree-dump "original" } } */
+
