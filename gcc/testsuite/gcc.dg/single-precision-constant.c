d5c557a568420526faa2ffdde3fa7e597f51849c
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-03-02 14:41:47 +0000

    * gcc.dg/single-precision-constant.c: Tweak for non-C99 runtimes.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@111638 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/single-precision-constant.c
+++ gcc/testsuite/gcc.dg/single-precision-constant.c
@@ -6,13 +6,17 @@
 #include <math.h>
 #include <float.h>
 
+#include "builtins-config.h"
+
 int main (void)
 {
   int result = 0;
   double local_DBL_MAX = DBL_MAX; 
   double local_DBL_MIN = DBL_MIN;
+#ifdef HAVE_C99_RUNTIME
   if (isinf (local_DBL_MAX))
     result |= 1;
+#endif
   if (local_DBL_MIN <= 0.0)
     result |= 1;
   return result;
