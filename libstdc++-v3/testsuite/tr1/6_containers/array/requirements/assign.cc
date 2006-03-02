be32484be40641010606b19aba8acefd3a4b9ac0
Paolo Carlini <pcarlini@suse.de>
2006-03-02 01:20:52 +0000

    array (array<>::swap, assign): Implement.
    2006-03-01  Paolo Carlini  <pcarlini@suse.de>
    
    	* include/tr1/array (array<>::swap, assign): Implement.
    	* include/tr1/array (operator==, operator!=, operator<,
    	operator>, operator>=, operator<=, swap, get): Inline.
    	* testsuite/tr1/6_containers/array/requirements/member_swap.cc: New.
    	* testsuite/tr1/6_containers/array/requirements/assign.cc: Likewise.
    	* testsuite/tr1/6_containers/array/specialized_algorithms/swap.cc:
    	Likewise.
    
    From-SVN: r111631

---
+++ libstdc++-v3/testsuite/tr1/6_containers/array/requirements/assign.cc
@@ -0,0 +1,47 @@
+// 2006-02-24  Paolo Carlini  <pcarlini@suse.de>
+//
+// Copyright (C) 2006 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 6.2.2 Class template array
+
+#include <tr1/array>
+#include <testsuite_hooks.h>
+
+void
+test01() 
+{ 
+  bool test __attribute__((unused)) = true;
+
+  const size_t len = 3;
+  typedef std::tr1::array<int, len> array_type;
+
+  array_type a = { 0, 1, 2 };
+  const int value = 5;
+
+  a.assign(value);
+  VERIFY( a[0] == value );
+  VERIFY( a[1] == value );
+  VERIFY( a[2] == value );  
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
