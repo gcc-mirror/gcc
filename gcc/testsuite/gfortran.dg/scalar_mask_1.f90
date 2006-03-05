d23e98a293a9ee37154af3c9fa3c2ea532dbec05
Thomas Koenig <Thomas.Koenig@online.de>
2006-03-05 20:03:08 +0000

    re PR fortran/23092 ([4.1 only] scalar mask for minval/maxval/sum/product)
    2006-03-05  Thomas Koenig <Thomas.Koenig@online.de>
    
    	PR fortran/23092
    	backport from mainline
    	* trans-intrinsic.c (gfc_conv_intrinsic_minmaxloc):
    	If the mask expression exists and has rank 0, enclose the
    	generated loop in an "if (mask)".  Put the default
    	initialization into the else branch.
    	* trans-intrinsic.c (gfc_conv_intrinsic_arith):  If the
    	mask expression exists and has rank 0, enclose the generated
    	loop in an "if (mask)".
    	* (gfc_conv_intrinsic_minmaxloc):  Likewise.
    
    2006-03-05  Thomas Koenig  <Thomas.Koenig@online.de>
    
    	PR fortran/23092
    	backport from mainline
    	* gfortran.dg/scalar_mask_1.f90:  New test.
    
    From-SVN: r111742

---
+++ gcc/testsuite/gfortran.dg/scalar_mask_1.f90
@@ -0,0 +1,15 @@
+! { dg-do run }
+program main
+  implicit none
+  real, dimension(2) :: a
+  a(1) = 2.0
+  a(2) = 3.0
+  if (product (a, .false.) /= 1.0) call abort
+  if (product (a, .true.) /= 6.0) call abort
+  if (sum (a, .false.) /= 0.0) call abort
+  if (sum (a, .true.) /= 5.0) call abort
+  if (maxval (a, .true.) /= 3.0) call abort
+  if (maxval (a, .false.) > -1e38) call abort
+  if (maxloc (a, 1, .true.) /= 2) call abort
+  if (maxloc (a, 1, .false.) /= 1) call abort
+end program main
