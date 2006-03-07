3c2aae8b66d0a01374f246a85d3e79f2d4648cf9
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-03-07 16:23:38 +0000

    2006-03-07  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/26587
    	* tree-ssa-structalias.c (handle_ptr_arith): Handle MINUS_EXPR.
    
    	* gcc.dg/torture/pr26587.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@111808 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/pr26587.c
@@ -0,0 +1,28 @@
+/* { dg-do run } */
+
+extern void abort(void);
+typedef unsigned int BF_word;
+typedef BF_word BF_key[16 + 2];
+static struct {
+    BF_key P;
+} BF_current;
+int main(void)
+{   
+    BF_word L;
+    BF_word tmp4, *ptr;
+    BF_word i;
+    for (i = 0; i < 16 + 2; i++)
+        BF_current.P[i] = i * 0x98765432;
+    L = 0;
+    ptr = BF_current.P;
+    do {
+        ptr += 2;
+        L ^= BF_current.P[0];
+        tmp4 = L >> 24;
+        L = tmp4 ^ BF_current.P[16 + 1];
+        *(ptr - 2) = L;
+    } while (ptr < &BF_current.P[16 + 2]);
+    if (L != 0x1fdb9752)
+        abort();
+    return 0;
+}
