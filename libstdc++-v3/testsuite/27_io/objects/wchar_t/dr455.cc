5f437987881042dfa95d092e16595e60f482e791
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-03-08 09:44:56 +0000

    2006-03-08  Paolo Carlini  <pcarlini@suse.de>
    	Implement the resolution of DR 455, [DR].
    	* src/ios_init.cc (ios_base::Init::Init()): Do it.
    	* testsuite/27_io/objects/char/dr455.cc: New.
    	* testsuite/27_io/objects/wchar_t/dr455.cc: Likewise.
    	* docs/html/ext/howto.html: Add an entry for DR 455.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@111842 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/27_io/objects/wchar_t/dr455.cc
@@ -0,0 +1,37 @@
+// 2006-03-08  Paolo Carlini  <pcarlini@suse.de>
+
+// Copyright (C) 2006 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#include <iostream>
+#include <testsuite_hooks.h>
+
+// DR 455. cerr::tie() and wcerr::tie() are overspecified.
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  VERIFY( std::wcerr.tie() == &std::wcout );
+}
+
+int 
+main()
+{
+  test01();
+  return 0;
+}
