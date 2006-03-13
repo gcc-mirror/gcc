08f1b53457f9cb9b6dc83b5cd869ecdcc5cb49b6
Richard Guenther <rguenther@suse.de>
2006-03-13 09:09:13 +0000

    re PR middle-end/26630 (Incorrect result when subtracting, casting to short and back to int, adding and multiplying)
    2006-03-13  Richard Guenther  <rguenther@suse.de>
    
    	PR middle-end/26630
    	* gcc.dg/torture/pr26630.c: New testcase.
    
    From-SVN: r111992

---
+++ gcc/testsuite/gcc.dg/torture/pr26630.c
@@ -0,0 +1,11 @@
+/* { dg-do run } */
+
+extern void abort(void);
+int main()
+{
+  int a1 = 40000;
+  int c1 = ( ((int)(short)(a1-10000)) + 10000)*2;
+  if (c1 != 80000)
+    abort();
+  return 0;
+}
