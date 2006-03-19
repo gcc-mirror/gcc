98cb5a5439fa61ed6d27630f617514d209c7a36a
Paul Thomas <pault@gcc.gnu.org>
2006-03-19 10:36:09 +0000

    re PR fortran/26716 (gfortran: incorrect choice of overloaded function)
    2006-03-19  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/26716
    	*expr.c (external_spec_function): Permit elemental functions.
    
    	PR fortran/26716
    	*interface.c (compare_actual_formal): Detect call for procedure
    	usage and require rank checking, in this case, for assumed shape
    	and deferred shape arrays.
    	(gfc_procedure_use): Revert to pre-PR25070 call to
    	compare_actual_formal that does not require rank checking..
    
    2006-03-19  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/26716
    	* gfortran.dg/elemental_initializer_1.f90: New test.
    
    	PR fortran/26716
    	* gfortran.dg/assumed_shape_ranks_2: New test.
    
    From-SVN: r112210

---
+++ gcc/testsuite/gfortran.dg/assumed_shape_ranks_2.f90
@@ -0,0 +1,45 @@
+! { dg-do run }
+! Tests the fix for the regression PR26716.
+! Test contributed by Martin Reinecke  <martin@mpa-garching.mpg.de>
+!
+module mod1
+  implicit none
+
+  interface foo
+     module procedure foo1, foo2
+  end interface
+
+contains
+
+  subroutine foo1(bar, i)
+    real bar
+    integer i
+    i = 1
+   end subroutine
+
+  subroutine foo2(bar, i)
+    real bar(3)
+    integer i
+    i = 2
+  end subroutine
+
+end module mod1
+
+  use mod1
+  implicit none
+
+  real bar(3)
+  integer i
+
+  i = 0
+  call foo (1e0, i)
+  if (i .ne. 1) call abort ()
+
+  i = 0
+  call foo (bar(1), i)
+  if (i .ne. 1) call abort ()
+
+  i = 0
+  call foo (bar, i)
+  if (i .ne. 2) call abort ()
+end
