4a282de4b36221c7168bf10ad0dc8836b374cfb8
Jason Merrill <jason@gcc.gnu.org>
2006-03-20 23:00:36 -0500

    use scan-hidden
    From-SVN: r112241

--- gcc/testsuite/g++.dg/ext/visibility/local1.C
+++ gcc/testsuite/g++.dg/ext/visibility/local1.C
@@ -2,10 +2,10 @@
 // Test that hidden visibility on an inline function is inherited by static
 // local variables and local classes.
 
-// { dg-do compile { target i?86-*-linux* x86_64-*-linux* powerpc*-*-linux* } }
-// { dg-final { scan-assembler "hidden\[ \t\]*_Z1fv" } }
-// { dg-final { scan-assembler "hidden\[ \t\]*_ZZ1fvE1i" } }
-// { dg-final { scan-assembler "hidden\[ \t\]*_ZZ1fvEN1A1fEv" } }
+// { dg-require-visibility "" }
+// { dg-final { scan-hidden "_Z1fv" } }
+// { dg-final { scan-hidden "_ZZ1fvE1i" } }
+// { dg-final { scan-hidden "_ZZ1fvEN1A1fEv" } }
 
 __attribute ((visibility ("hidden"))) inline int
 f()
