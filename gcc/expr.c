740a5be36212cc53bae256f838321a9a0e037535
Jason Merrill <jason@redhat.com>
2006-03-21 23:50:31 -0500

    re PR middle-end/20297 (#pragma GCC visibility isn't properly handled for builtin functions)
            PR middle-end/20297
            * expr.c (init_block_move_fn): Force default visibility.
            (init_block_clear_fn): Likewise.
            * builtins.c (expand_builtin_fork_or_exec): Likewise.
    
    From-SVN: r112273

--- gcc/expr.c
+++ gcc/expr.c
@@ -1375,6 +1375,8 @@ init_block_move_fn (const char *asmspec)
       TREE_PUBLIC (fn) = 1;
       DECL_ARTIFICIAL (fn) = 1;
       TREE_NOTHROW (fn) = 1;
+      DECL_VISIBILITY (fn) = VISIBILITY_DEFAULT;
+      DECL_VISIBILITY_SPECIFIED (fn) = 1;
 
       block_move_fn = fn;
     }
@@ -2578,6 +2580,8 @@ init_block_clear_fn (const char *asmspec)
       TREE_PUBLIC (fn) = 1;
       DECL_ARTIFICIAL (fn) = 1;
       TREE_NOTHROW (fn) = 1;
+      DECL_VISIBILITY (fn) = VISIBILITY_DEFAULT;
+      DECL_VISIBILITY_SPECIFIED (fn) = 1;
 
       block_clear_fn = fn;
     }
