0535d6d75b7dd27343da2f6fcea8af9725305721
David Edelsohn <edelsohn@gnu.org>
2006-03-26 17:15:43 +0000

    dwarf-1.m: Skip on AIX.
            * objc.dg/dwarf-1.m: Skip on AIX.
            * objc.dg/dwarf-2.m: Skip on AIX.
    
    From-SVN: r112398

--- gcc/testsuite/objc.dg/dwarf-1.m
+++ gcc/testsuite/objc.dg/dwarf-1.m
@@ -1,5 +1,6 @@
 /* { dg-options "-gdwarf-2 -dA" } */
 /* { dg-final { scan-assembler "\"id.0\".*DW_AT_name" } } */
+/* { dg-skip-if "No Dwarf" { *-*-aix* } { "*" } { "" } } */
 @interface foo
   id x;
 @end
