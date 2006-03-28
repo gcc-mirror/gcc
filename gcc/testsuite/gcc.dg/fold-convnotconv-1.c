c348f27f02731704ee3e33e5d541060f865a75b7
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-03-28 17:06:19 +0000

    * fold-const.c (fold_unary) <NOP_EXPR>: Fold (T1)(~(T2)X) as 	~(T1)X, when T1 and T2 are integer types of the same precision 	and (T2)X isn't an extension.
    	* gcc.dg/fold-convnotconv-1.c: New test case.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@112455 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/fold-convnotconv-1.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-original" } */
+
+int test1(int a)
+{
+  return ~(unsigned int)a;
+}
+
+unsigned int test2(unsigned int b)
+{
+  return ~(int)b;
+}
+
+/* { dg-final { scan-tree-dump-times "~a" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "~b" 1 "original" } } */
+/* { dg-final { cleanup-tree-dump "original" } } */
+
