9ccab91c6fb6b96c77e1aafd5ef240b965c897b2
Roger Sayle <roger@eyesopen.com>
2006-04-01 19:16:01 +0000

    dependency.c (gfc_is_inside_range): Delete.
    	* dependency.c (gfc_is_inside_range): Delete.
    	(gfc_check_element_vs_section): Significant rewrite.
    
    	* gfortran.dg/dependencency_17.f90: New test case.
    
    From-SVN: r112607

---
+++ gcc/testsuite/gfortran.dg/dependency_17.f90
@@ -0,0 +1,12 @@
+! { dg-do compile }
+! { dg-options "-O2 -fdump-tree-original" }
+subroutine foo(a,i)
+  integer, dimension (3,3,4) :: a
+  integer :: i
+
+  where (a(1,1:2,1:3) .ne. 0)
+    a(2:3,3,2:4) = 1
+  endwhere
+end subroutine
+! { dg-final { scan-tree-dump-times "malloc" 0 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
