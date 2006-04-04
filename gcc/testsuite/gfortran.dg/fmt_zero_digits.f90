c5c28809ab18e2448ea90ada2cd1f68000142dfb
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2006-04-04 03:04:58 +0000

    fmt_zero_digits.f90: New test for no error when zero decimal digits specified in format.
    2006-04-03  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	* gfortran.dg/fmt_zero_digits.f90: New test for no error when
    	zero decimal digits specified in format.
    
    From-SVN: r112657

---
+++ gcc/testsuite/gfortran.dg/fmt_zero_digits.f90
@@ -0,0 +1,10 @@
+! { dg-do run }
+! Verify that when decimal precision is zero, no error.
+! Submitted by Jerry DeLisle  <jvdelisle@gcc.gnu.org>
+program test
+  character(20) :: astr
+  50 FORMAT (d20.0)
+  astr = ""
+  write(astr,50) -8.0D0
+  if (astr.ne."              0.D+01") call abort()
+end program test
