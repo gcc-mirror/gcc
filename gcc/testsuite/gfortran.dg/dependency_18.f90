3f4feb44c143eefd987a2c0dffb10c7be75c691a
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-04-06 02:08:27 +0000

    * dependency.c (get_no_elements): Delete function. 	(get_deps): Delete function. 	(transform_sections): Delete function. 	(gfc_check_section_vs_section): Significant rewrite.
    	* gfortran.dg/dependency_18.f90: New test case.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@112731 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/dependency_18.f90
@@ -0,0 +1,20 @@
+! { dg-do compile }
+! { dg-options "-O2 -fdump-tree-original" }
+subroutine foo(a,i,j,k)
+  integer, dimension (10) :: a
+  integer :: i, j, k
+
+  a(1:5:2) = a(8:6:-1)
+
+  a(1:8) = a(2:9)
+
+  a(4:7) = a(4:1:-1)
+
+  a(i:i+2) = a(i+4:i+6)
+
+  a(j:1:-1) = a(j:5)
+
+  a(k:k+2) = a(k+1:k+3)
+end subroutine
+! { dg-final { scan-tree-dump-times "malloc" 0 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
