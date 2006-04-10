acef130fabdabd9c37b27e7ae1ad6c943a34a405
Roger Sayle <roger@eyesopen.com>
2006-04-10 21:01:19 +0000

    i386.c (ix86_expand_vector_init_one_nonzero): Renamed from ix86_expand_vector_init_low_nonzero.
    	* config/i386/i386.c (ix86_expand_vector_init_one_nonzero): Renamed
    	from ix86_expand_vector_init_low_nonzero.  Take an additional
    	one_var argument indicating which element is non-zero.  Support
    	one_var != 0 for V4SFmode and V4SImode by permuting the result.
    	(ix86_expand_vector_init): Call ix86_expand_vector_init_one_nonzero
    	with one_var instead of ix86_expand_vector_init_low_nonzero.
    
    	* gcc.target/i386/vecinit-1.c: New test case.
    	* gcc.target/i386/vecinit-2.c: Likewise.
    
    From-SVN: r112832

---
+++ gcc/testsuite/gcc.target/i386/vecinit-1.c
@@ -0,0 +1,11 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -msse2" } */
+#define vector __attribute__((vector_size(16)))
+
+float a;
+vector float f1(void) { return (vector float){ a, 0.0, 0.0, 0.0}; }
+vector float f2(void) { return (vector float){ 0.0, a, 0.0, 0.0}; }
+vector float f3(void) { return (vector float){ 0.0, 0.0, a, 0.0}; }
+vector float f4(void) { return (vector float){ 0.0, 0.0, 0.0, a}; }
+/* { dg-final { scan-assembler-not "movaps" } } */
+/* { dg-final { scan-assembler-not "xor" } } */
