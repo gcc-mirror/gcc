8a54a1de62c96c2bda407ecea440434916c8914d
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2006-04-13 06:30:04 +0000

    re PR fortran/26766 ([F2003] Recursive I/O still (again) broken)
    2006-04-12  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/26766
    	* gfortran.dg/write_recursive.f90: New test.
    
    From-SVN: r112915

---
+++ gcc/testsuite/gfortran.dg/write_recursive.f90
@@ -0,0 +1,39 @@
+! { dg-do run }
+! PR26766 Recursive I/O with internal units
+! Test case derived from example in PR
+! Submitted by Jerry DeLisle  <jvdelisle@gcc.gnu.org>
+program pr26766
+  implicit none
+  character (len=8) :: str, tmp
+  write (str, '(a)')  bar (1234)
+  if (str.ne."abcd") call abort()
+  str = "wxyz"
+  write (str, '(2a4)') foo (1), bar (1)
+  if (str.ne."abcdabcd") call abort()
+
+contains
+
+  function foo (i) result (s)
+    integer, intent(in) :: i
+    character (len=4)   :: s, t
+    if (i < 0) then
+       s = "1234"
+    else
+       ! Internal I/O, allowed recursive in f2003, see section 9.11
+       write (s, '(a)') "abcd"
+    end if
+  end function foo
+  
+  function bar (i) result (s)
+    integer, intent(in) :: i
+    character (len=4)   :: s, t
+    if (i < 0) then
+      s = "4567"
+    else
+      write (s, '(a)') foo(i)
+    end if
+  end function bar
+
+end program pr26766
+
+
