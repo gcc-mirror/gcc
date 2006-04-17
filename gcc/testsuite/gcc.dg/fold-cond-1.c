a0d17866a4be3d7488ce9366ce4bbd4fa3013e24
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-04-17 02:38:50 +0000

    PR target/26961 	* fold-const.c (fold_ternary): When converting "A ? B : C" into either 	"A op B" or "A op C", we may need to convert A to the type of B and C.
    	* gcc.dg/fold-cond-1.c: New test case.
    	* gcc.dg/pr26961-1.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@113001 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/fold-cond-1.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-original" } */
+
+_Bool test1(int a, int b)
+{
+  return a ? b : 0;
+}
+
+_Bool test2(int c, int d)
+{
+  return c ? d : 1;
+}
+
+_Bool test3(int e, int f)
+{
+  return e ? 0 : f;
+}
+
+_Bool test4(int g, int h)
+{
+  return g ? 1 : h;
+}
+
+/* { dg-final { scan-tree-dump-times "a != 0 \&\& b != 0" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "c == 0 \\|\\| d != 0" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "e == 0 \&\& f != 0" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "\\(g \\| h\\) != 0" 1 "original" } } */
+/* { dg-final { cleanup-tree-dump "original" } } */
