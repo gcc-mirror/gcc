5bcc70565243f488a73b90fc3907f919600ef8ef
Volker Reichelt <reichelt@igpm.rwth-aachen.de>
2006-04-19 22:07:25 +0000

    re PR c++/26558 (segfault on syntax error)
    	PR c++/26558
    	* parser.c (cp_parser_class_name): Check for invalid typenames.
    	Rearrange code.
    
    	* g++.dg/parse/template19.C: New test.
    
    From-SVN: r113097

---
+++ gcc/testsuite/g++.dg/parse/template19.C
@@ -0,0 +1,11 @@
+// PR c++/26558
+// Origin: Jan Gorski <slimak@yk74.internetdsl.tpnet.pl>
+// { dg-do compile }
+
+template<int> struct A
+{
+  template<int> void foo()
+  {
+    foo<0>::; // { dg-error "before" }
+  }
+};
