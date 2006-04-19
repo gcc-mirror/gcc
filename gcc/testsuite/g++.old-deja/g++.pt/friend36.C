0014c2479a0e42f96dba222fe391800fa8c57660
Volker Reichelt <reichelt@igpm.rwth-aachen.de>
2006-04-19 18:07:19 +0000

    re PR c++/26739 (ICE in g++.old-deja/g++.pt/friend36.C)
    	PR c++/26739
    	* pt.c (tsubst_friend_function): Return early if
    	pushdecl_namespace_level fails.
    
    	* g++.old-deja/g++.pt/friend36.C: Adjust error markers.
    
    From-SVN: r113090

--- gcc/testsuite/g++.old-deja/g++.pt/friend36.C
+++ gcc/testsuite/g++.old-deja/g++.pt/friend36.C
@@ -1,12 +1,12 @@
 // { dg-do assemble  }
 
 template <class T>
-void f(T) {} // { dg-error "" } previously defined here
+void f(T) {} // { dg-error "previously" }
 
 template <class U>
 struct S {
   template <class T>
-  friend void f(T) {} // { dg-error "" } redeclaration
+  friend void f(T) {} // { dg-error "redefinition" }
 };
 
 S<int> si;
