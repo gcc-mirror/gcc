e5ab04e30263327b9299ac5d83e1b8687cda302b
dje <dje@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-04-26 17:57:03 +0000

    PR middle-end/27282         * gcc.c-torture/compile/pr27282.c: New test
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@113278 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr27282.c
@@ -0,0 +1,17 @@
+/* This test used to ICE on PowerPC at -O due to combine GEN_INT bug.  */
+typedef struct _ColRowInfo ColRowInfo;
+typedef struct { }
+GnmSheetRange;
+struct _ColRowInfo
+{
+  float size_pts;
+  unsigned margin_a:3;
+  unsigned margin_b:3;
+  unsigned visible:1;
+};
+colrow_equal (ColRowInfo const *a, ColRowInfo const *b)
+{
+  return a->size_pts == b->size_pts && a->margin_a == b->margin_a
+    && a->visible == b->visible;
+}
+
