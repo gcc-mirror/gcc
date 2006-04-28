2cdb4b8e946408c5672d4b6a3ef9b0d34389198c
amodra <amodra@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-04-28 03:36:15 +0000

    PR middle-end/27095 	* gcc.dg/pr27095.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@113340 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr27095.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+
+extern void *memset (void *, int, __SIZE_TYPE__);
+extern __SIZE_TYPE__ strlen (const char *);
+
+int
+main (int argc, char **argv)
+{
+  char x[8] = "abc";
+  memset (x, argc, strlen (x));
+  return 0;
+}
+/* { dg-final { scan-assembler-not "(?n)strlen\(.*\n\)+.*strlen" } } */
