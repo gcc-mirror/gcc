74ab8c2daec20e8f29d7a0f5852b4fbfb175b243
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2006-04-29 00:02:10 +0000

    re PR fortran/27304 (gfortran: Warn/abort when format in write does not fit passed arguments)
    2006-04-28  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/27304
    	* gfortran.dg/fmt_exhaust.f90: New test.
    
    From-SVN: r113364

---
+++ gcc/testsuite/gfortran.dg/fmt_exhaust.f90
@@ -0,0 +1,11 @@
+! { dg-do run }
+! PR27304 Test running out of data descriptors with data remaining.
+! Derived from case in PR.  Submitted by Jerry DeLisle <jvdelisle@gcc.gnu.org>.
+      program test
+      implicit none
+      integer :: n
+      n = 1
+      write(10,"(i7,(' abcd'))", err=10) n, n
+      call abort()
+ 10   close(10, status="delete")
+      end program test
