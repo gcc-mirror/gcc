b1482d1512f82f946b910712568a6955346ab96e
Volker Reichelt <reichelt@igpm.rwth-aachen.de>
2006-04-29 21:39:54 +0000

    re PR c++/11471 (ICE on illegal use of typedef'd member class)
    	PR c++/11471
    	PR c++/27102
    	* g++.dg/template/crash48.C: New test.
    
    From-SVN: r113382

---
+++ gcc/testsuite/g++.dg/template/crash48.C
@@ -0,0 +1,10 @@
+// PR c++/11471
+// Origin:  <bagnara@cs.unipr.it>
+// { dg-do compile }
+
+template<typename T> struct A
+{
+  typedef typename T::X X;
+};
+
+template<typename T> A<T>::X::X() {} // { dg-error "no type|invalid use" }
