c921ee27cefded5c57f0c5a1a96bb452a50cc8dd
Mark Mitchell <mmitchel@gcc.gnu.org>
2006-05-01 01:20:38 +0000

    Fix duplicated test
    From-SVN: r113406

--- gcc/testsuite/g++.dg/template/defarg8.C
+++ gcc/testsuite/g++.dg/template/defarg8.C
@@ -17,22 +17,3 @@ template<int> struct C
 };
 
 C<0> c;
-// PR c++/27094
-// { dg-options "--param ggc-min-expand=0 --param ggc-min-heapsize=0" }
-
-struct A
-{
-  ~A();
-};
-
-struct B : A
-{
-  B();
-};
-
-template<int> struct C
-{
-  C(const B& = B());
-};
-
-C<0> c;
