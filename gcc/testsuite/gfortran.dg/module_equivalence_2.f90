613e2ac8d3dc830f03c00b165ffcf85b96717240
Paul Thomas <pault@gcc.gnu.org>
2006-05-02 14:13:17 +0000

    re PR fortran/27269 (Segfault with EQUIVALENCEs in modules together with ONLY clauses)
    2006-05-02 Paul Thomas <pault@gcc.gnu.org>
    
            PR fortran/27269
            * module.c: Add static flag in_load_equiv.
            (mio_expr_ref): Return if no symtree and in_load_equiv.
            (load_equiv): If any of the equivalence members have no symtree, free
            the equivalence and the associated expressions.
    
            PR fortran/27324
            * trans-common.c (gfc_trans_common): Invert the order of calls to
            finish equivalences and gfc_commit_symbols.
    
            PR fortran/27269
            PR fortran/27324
            * gfortran.dg/module_equivalence_2.f90: New test.
    
    From-SVN: r113465

---
+++ gcc/testsuite/gfortran.dg/module_equivalence_2.f90
@@ -0,0 +1,24 @@
+! { dg-do run }
+! Tests the fix for PR27269 and PR27xxx.
+! The former caused a segfault in trying to process
+! module b, with an unused equivalence in a. The latter
+! produced an assembler error due to multiple declarations
+! for a module equivalence, when one of the variables was
+! initialized, as M in module a.
+!
+module a
+  integer, parameter :: dp = selected_real_kind (10)
+  real(dp) :: reM, M = 1.77d0
+  equivalence (M, reM)
+end module a
+
+module b
+  use a, only : dp
+end module b
+
+  use a
+  use b
+  if (reM .ne. 1.77d0) call abort ()
+  reM = 0.57d1
+  if (M .ne. 0.57d1) call abort ()
+end
