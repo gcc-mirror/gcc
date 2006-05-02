7f32f9e562b5e9168c41109f83e6694a75b66186
Kazu Hirata <kazu@codesourcery.com>
2006-05-02 15:13:02 +0000

    re PR target/27387 (Thumb thunk is not PIC)
    gcc/
    	PR target/27387
    	* arm.c (arm_output_mi_thunk): Use pc-relative addressing when
    	-mthumb -fPIC are used.
    
    testsuite/
    	PR target/27387
    	* gcc.target/arm/arm.exp: New.
    	* gcc.target/arm/pr27387.C: Likewise.
    
    From-SVN: r113470

---
+++ gcc/testsuite/gcc.target/arm/pr27387.C
@@ -0,0 +1,26 @@
+/* PR target/90000
+   We used to generate a non-PIC thunk on thumb even with -fPIC.
+   Make sure that won't happen anymore.  */
+
+/* { dg-do compile } */
+/* { dg-require-effective-target arm32 } */
+/* { dg-options "-mthumb -fPIC" } */
+
+struct A {
+  virtual void f ();
+};
+
+struct B {
+  virtual void g ();
+};
+
+struct C : public A, public B {
+  virtual void g();
+};
+
+void
+C::g()
+{
+}
+
+/* { dg-final { scan-assembler "LTHUNKPC" } } */
