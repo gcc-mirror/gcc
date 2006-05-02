b931db5f4dd15ca9a107c224bee750bfb977991a
Steve Ellcey <sje@cup.hp.com>
2006-05-02 22:16:13 +0000

    re PR testsuite/27032 (gcc.c-torture/compile/pr23237.c fails)
    	PR testsuite/27032
    	* gcc.c-torture/compile/pr23237.c: Move dg-do line.
    
    From-SVN: r113484

--- gcc/testsuite/gcc.c-torture/compile/pr23237.c
+++ gcc/testsuite/gcc.c-torture/compile/pr23237.c
@@ -1,8 +1,8 @@
-/* { dg-require-effective-target named_sections } */
-
 /* Don't assemble, as this section syntax may not be valid on all platforms
    (e.g., Darwin).  */
 /* { dg-do compile } */
+
+/* { dg-require-effective-target named_sections } */
 
 static __attribute__ ((__section__ (".init.data"))) char *message;
 static __attribute__ ((__section__ (".init.data"))) int (*actions[])(void) = {};
