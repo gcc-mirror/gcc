013a7f55166caf46d73fc46c9a2baf6bdac3be64
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-05-04 06:34:06 +0000

    PR middle-end/27388 	* gimplify.c (omp_is_private): If var is shared in some outer context, 	return false instead of true.  Stop searching on parallel context 	boundary.
    	* gcc.dg/gomp/pr27388-1.c: New test.
    	* gcc.dg/gomp/pr27388-2.c: New test.
    	* gcc.dg/gomp/pr27388-3.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@113514 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/gomp/pr27388-2.c
@@ -0,0 +1,35 @@
+/* PR middle-end/27388 */
+/* { dg-do compile } */
+/* { dg-options "-fopenmp -fdump-tree-omplower" } */
+
+extern void baz (int);
+
+void
+foo (void)
+{
+  int i;
+#pragma omp parallel for shared (i)
+  for (i = 0; i < 2; i++)
+    baz (i);
+}
+
+void
+bar (void)
+{
+  int j = 0;
+#pragma omp parallel shared (j)
+  {
+    j++;
+#pragma omp for
+    for (j = 0; j < 2; j++)
+      baz (j);
+  }
+}
+
+/* { dg-final { scan-tree-dump-times "shared\\\(i\\\)\[^\\n\]*private\\\(i\\\)" 0 "omplower" } } */
+/* { dg-final { scan-tree-dump-times "private\\\(i\\\)\[^\\n\]*shared\\\(i\\\)" 0 "omplower" } } */
+/* { dg-final { scan-tree-dump-times "omp for\[^\\n\]*private\\\(i\\\)" 1 "omplower" } } */
+/* { dg-final { scan-tree-dump-times "shared\\\(j\\\)\[^\\n\]*private\\\(j\\\)" 0 "omplower" } } */
+/* { dg-final { scan-tree-dump-times "private\\\(j\\\)\[^\\n\]*shared\\\(j\\\)" 0 "omplower" } } */
+/* { dg-final { scan-tree-dump-times "omp for\[^\\n\]*private\\\(j\\\)" 1 "omplower" } } */
+/* { dg-final { cleanup-tree-dump "omplower" } } */
---
+++ gcc/testsuite/gcc.dg/gomp/pr27388-3.c
@@ -0,0 +1,23 @@
+/* PR middle-end/27388 */
+/* { dg-do compile } */
+/* { dg-options "-fopenmp -fdump-tree-omplower" } */
+
+extern void bar (int);
+
+void
+foo (void)
+{
+  int i = 0, j = 0;
+#pragma omp parallel firstprivate (i) private (j)
+  {
+#pragma omp for
+    for (i = 0; i < 2; i++)
+      bar (i);
+#pragma omp for
+    for (j = 0; j < 2; j++)
+      bar (j);
+  }
+}
+
+/* { dg-final { scan-tree-dump-times "omp for\[^\\n\]*private" 0 "omplower" } } */
+/* { dg-final { cleanup-tree-dump "omplower" } } */
