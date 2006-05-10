fa96442627f29d7b558e2bf8cd595337ae19b87f
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-05-10 18:37:54 +0000

    2006-05-10  Paolo Carlini  <pcarlini@suse.de> 	    Peter Doerfler  <gcc@pdoerfler.com>
    	* include/tr1/hashtable (hashtable_iterator<>::hashtable_iterator(),
    	hashtable_const_iterator<>::hashtable_const_iterator(),
    	node_iterator<>::node_iterator(),
    	node_const_iterator<>::node_const_iterator()): Add.
    	(node_iterator<>::node_iterator(hash_node<>*),
    	node_const_iterator<>::node_const_iterator(hash_node<>*)): Tweak,
    	remove default.
    	* testsuite/tr1/6_containers/unordered/hashtable/
    	iterators_default_constructor.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@113681 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/tr1/6_containers/unordered/hashtable/iterators_default_constructor.cc
@@ -0,0 +1,32 @@
+// { dg-do compile }
+
+// Copyright (C) 2006 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 6.3 Unordered associative containers
+
+#include <tr1/unordered_set>
+
+void
+test01()
+{
+  std::tr1::unordered_set<int>::iterator                it;
+  std::tr1::unordered_set<int>::const_iterator         cit;
+  std::tr1::unordered_set<int>::local_iterator         lit;
+  std::tr1::unordered_set<int>::const_local_iterator  clit;
+}
