7ebcc52c5a640d18127d21d0d359bb318b452f7d
Volker Reichelt <reichelt@igpm.rwth-aachen.de>
2006-05-11 15:07:45 +0000

    re PR c++/27384 (ICE with invalid array size)
    	PR middle-end/27384
    	* fold-const.c (size_binop): Move sanity check for arguments to
    	the beginning of the function.
    
    	* g++.dg/other/fold1.C: New test.
    
    From-SVN: r113701

--- gcc/fold-const.c
+++ gcc/fold-const.c
@@ -1732,6 +1732,9 @@ size_binop (enum tree_code code, tree arg0, tree arg1)
 {
   tree type = TREE_TYPE (arg0);
 
+  if (arg0 == error_mark_node || arg1 == error_mark_node)
+    return error_mark_node;
+
   gcc_assert (TREE_CODE (type) == INTEGER_TYPE && TYPE_IS_SIZETYPE (type)
 	      && type == TREE_TYPE (arg1));
 
@@ -1750,9 +1753,6 @@ size_binop (enum tree_code code, tree arg0, tree arg1)
       /* Handle general case of two integer constants.  */
       return int_const_binop (code, arg0, arg1, 0);
     }
-
-  if (arg0 == error_mark_node || arg1 == error_mark_node)
-    return error_mark_node;
 
   return fold_build2 (code, type, arg0, arg1);
 }
