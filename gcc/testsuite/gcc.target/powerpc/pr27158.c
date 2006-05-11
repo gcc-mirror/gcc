8e9c3f03c04578a91f6c1bf36e44c58423180441
Roger Sayle <roger@eyesopen.com>
2006-05-11 14:17:19 +0000

    re PR target/27158 (ICE in extract_insn with -maltivec)
    	PR target/27158
    	* reload.c (find_reloads_toplev): Only return the simplified SUBREG
    	of a reg_equiv_constant if the result is a legitimate constant.
    
    	* gcc.target/powerpc/pr27158.c: New test case.
    
    From-SVN: r113695

---
+++ gcc/testsuite/gcc.target/powerpc/pr27158.c
@@ -0,0 +1,22 @@
+/* { dg-do compile { target powerpc*-*-* } } */
+/* { dg-xfail-if "" { "powerpc-*-eabispe*" "powerpc-ibm-aix*" } { "*" } { "" } } */
+/* { dg-options "-O2 -maltivec" } */
+#define REGLIST                                                              \
+         "77",  "78",  "79",  "80",  "81",  "82",  "83",  "84",  "85",  "86",\
+         "87",  "88",  "89",  "90",  "91",  "92",  "93",  "94",  "95",  "96",\
+         "97",  "98",  "99", "100", "101", "102", "103", "104", "105", "106",\
+        "107", "108"
+
+typedef __attribute__ ((vector_size (16))) float v4sf;
+
+void
+foo (int H)
+{
+  volatile v4sf tmp;
+  while (H-- > 0)
+    {
+      asm ("" : : : REGLIST);
+      tmp = (v4sf) __builtin_altivec_vspltisw (1);
+    }
+}
+
