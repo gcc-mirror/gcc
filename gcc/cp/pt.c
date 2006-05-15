cd2ba456af4afec9fb3f45a4af67fddaf15c43d9
reichelt <reichelt@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-05-15 09:34:59 +0000

    PR c++/27582 	* pt.c (any_dependent_template_arguments_p): Return early on invalid 	argument list.
    	* g++.dg/template/dependent-args1.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@113784 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -12760,6 +12760,8 @@ any_dependent_template_arguments_p (tree args)
 
   if (!args)
     return false;
+  if (args == error_mark_node)
+    return true;
 
   for (i = 0; i < TMPL_ARGS_DEPTH (args); ++i)
     {
---
+++ gcc/testsuite/g++.dg/template/dependent-args1.C
@@ -0,0 +1,11 @@
+// PR c++/27582
+// { dg-do compile }
+
+struct A
+{
+  template<int> void foo();
+};
+
+template<int N, void (A::*)() = &A::foo<N> > struct B {};
+
+B<int> b; // { dg-error "type/value mismatch|expected a constant|invalid type" }
