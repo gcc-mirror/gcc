13ec6585bbfa024ff4666366e9fcbb1bfc2cdec5
Volker Reichelt <reichelt@igpm.rwth-aachen.de>
2006-05-15 09:38:11 +0000

    re PR c++/27582 (ICE with invalid template parameter)
    	PR c++/27582
    	* pt.c (any_dependent_template_arguments_p): Return early on invalid
    	argument list.
    
    	* g++.dg/template/dependent-args1.C: New test.
    
    From-SVN: r113785

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -12660,6 +12660,8 @@ any_dependent_template_arguments_p (tree args)
 
   if (!args)
     return false;
+  if (args == error_mark_node)
+    return true;
 
   for (i = 0; i < TMPL_ARGS_DEPTH (args); ++i)
     {
