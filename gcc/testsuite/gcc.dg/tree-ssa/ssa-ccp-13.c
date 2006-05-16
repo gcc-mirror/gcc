faaf14368b72d741b4ea5bcb6b8cb172fb847c43
Richard Guenther <rguenther@suse.de>
2006-05-16 15:34:12 +0000

    re PR tree-optimization/22303 (CCP does not handle STRING_CSTs)
    2006-05-16  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/22303
    	* tree-ssa-ccp.c (fold_const_aggregate_ref): Handle reads
    	from STRING_CSTs.
    	(evaluate_stmt): Fall back to fold_const_aggregate_ref, if
    	ccp_fold did not simplify the statement.
    
    	* gcc.dg/tree-ssa/ssa-ccp-13.c: New testcase.
    
    From-SVN: r113826

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-ccp-13.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+static const char f[3] = "?";
+
+int foo()
+{
+  int i = 0;
+  return f[i] != '?';
+}
+
+/* { dg-final { scan-tree-dump "return 0;" "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
+
