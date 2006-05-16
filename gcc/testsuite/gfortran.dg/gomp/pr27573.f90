b570947c8ad996f00aaa621a9e3dee997baa2e2e
Jakub Jelinek <jakub@redhat.com>
2006-05-16 12:16:36 +0200

    re PR middle-end/27573 (ICE with -fopenmp  -fprofile-generate)
    	PR middle-end/27573
    	* omp-low.c (expand_omp_parallel): Don't assert
    	.OMP_DATA_I = &.OMP_DATA_O is the first statement in the block,
    	instead search for it.
    
    	* gcc.dg/gomp/pr27573.c: New test.
    	* gfortran.dg/gomp/pr27573.f90: New test.
    
    From-SVN: r113823

---
+++ gcc/testsuite/gfortran.dg/gomp/pr27573.f90
@@ -0,0 +1,15 @@
+! PR middle-end/27573
+! { dg-do compile }
+! { dg-options "-O2 -fopenmp -fprofile-generate" }
+
+program pr27573
+  integer i,j
+  j = 8
+  !$omp parallel
+    print *, "foo"
+    do i = 1, j - 1
+    end do
+  !$omp end parallel
+end
+
+! { dg-final { cleanup-coverage-files } }
