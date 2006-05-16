ba1274abe17dcb83e613f7a9f84813c56158e1f8
H.J. Lu <hongjiu.lu@intel.com>
2006-05-16 17:06:05 +0000

    re PR target/26885 (-m64 -m32 no longer creates 32-bit object)
    gcc/
    
    2006-05-16  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR driver/26885
    	* Makefile.in (GCC_OBJS): New.
    	(OBJS-common): Add opts-common.o.
    	(xgcc$(exeext)): Replace gcc.o with $(GCC_OBJS).
    	(cpp$(exeext)): Likewise.
    	(gcc.o): Also depend on opts.h.
    	(opts-common.o): New.
    
    	* common.opt (gcoff): Add Negative(gdwarf-2).
    	(gdwarf-2): Add Negative(gstabs).
    	(gstabs): Add Negative(gstabs+).
    	(gstabs+): Add Negative(gvms).
    	(gvms): Add Negative(gxcoff).
    	(gxcoff): Add Negative(gxcoff+).
    	(gxcoff+): Add Negative(gcoff).
    	* config/i386/i386.opt (m32): Add Negative(m64).
    	(m64): Add Negative(m32).
    
    	* doc/options.texi: Document the Negative option.
    
    	* gcc.c: Include "opts.h".
    	(main): Call prune_options after expandargv.
    
    	* optc-gen.awk: Generate common declarations for all flag
    	variables in options.c. Output the neg_index field.
    
    	* opts.c (find_opt): Moved to ...
    	* opts-common.c: Here. New file.
    
    	* opts.h (cl_option): Add a neg_index field.
    	(find_opt): New.
    	(prune_options): Likewise.
    
    gcc/cp/
    
    2006-05-16  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR driver/26885
    	* Make-lang.in (GXX_OBJS): Replace gcc.o with $(GCC_OBJS).
    
    gcc/fortran/
    
    2006-05-16  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR driver/26885
    	* Make-lang.in (GFORTRAN_D_OBJS): Replace gcc.o with
    	$(GCC_OBJS).
    
    gcc/java/
    
    2006-05-16  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR driver/26885
    	* Make-lang.in ($(GCJ)$(exeext)): Replace gcc.o with
    	$(GCC_OBJS).
    
    gcc/treelang/
    
    2006-05-16  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR driver/26885
    	* Make-lang.in (gtreelang$(exeext)): Replace gcc.o with
    	$(GCC_OBJS).
    
    From-SVN: r113828

--- gcc/gcc.c
+++ gcc/gcc.c
@@ -86,6 +86,7 @@ compilation is specified by a string called a "spec".  */
 #include "prefix.h"
 #include "gcc.h"
 #include "flags.h"
+#include "opts.h"
 
 /* By default there is no special suffix for target executables.  */
 /* FIXME: when autoconf is fixed, remove the host check - dj */
@@ -6031,6 +6032,8 @@ main (int argc, const char **argv)
   programname = p;
 
   xmalloc_set_program_name (programname);
+
+  prune_options (&argc, &argv);
 
 #ifdef GCC_DRIVER_HOST_INITIALIZATION
   /* Perform host dependent initialization when needed.  */
