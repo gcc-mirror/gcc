124c02016c90c9794e7e786c22afaf224e867e26
stuart <stuart@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-05-18 00:34:44 +0000

    * gcc/config/i386/i386.opt (-mstackrealign): New flag.  * 	gcc/config/i386/i386.c (force_align_arg_pointer): New attribute. 	(ix86_handle_cconv_attribute): Emit error when 	force_align_arg_pointer attribute collides with too many regparms. 	(ix86_function_regparm): Limit regparms when used with 	force_align_arg_pointer attribute.  (ix86_internal_arg_pointer): 	Support stack-realigning prologue in non-main functions.  Emit 	warning for nested functions under -mstackrealign, emit error for 	nested functions with force_align_arg_pointer attribute. 	* gcc/doc/extend.texi (force_align_arg_pointer): Document it. 	* gcc/doc/invoke.texi (-mstackrealign): Document it. 	* testsuite/gcc.target/i386/20060512-1.c: New. 	* testsuite/gcc.target/i386/20060512-2.c: New. 	* testsuite/gcc.target/i386/20060512-3.c: New. 	* testsuite/gcc.target/i386/20060512-4.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@113880 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/20060512-4.c
@@ -0,0 +1,12 @@
+/* { dg-do compile { target i?86-*-* } } */
+/* { dg-options "-mstackrealign" } */
+int
+outer_function (int x, int y)
+{
+  int __attribute__ ((__noinline__))
+  nested_function (int x, int y)
+    { /* { dg-error "-mstackrealign ignored for nested functions" } */
+      return (x + y);
+    }
+  return (3 + nested_function (x, y));
+}
