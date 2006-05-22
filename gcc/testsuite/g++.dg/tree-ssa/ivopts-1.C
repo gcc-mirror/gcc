06c51425356eac50f0576089e665963c949350f7
Richard Guenther <rguenther@suse.de>
2006-05-22 09:20:46 +0000

    re PR testsuite/27707 (g++.dg/tree-ssa/ivopts-1.C fails)
    2006-05-22  Richard Guenther  <rguenther@suse.de>
    
    	PR testsuite/27707
    	* g++.dg/tree-ssa/ivopts-1.C: XFAIL for hppa*-*-*.
    
    From-SVN: r113972

--- gcc/testsuite/g++.dg/tree-ssa/ivopts-1.C
+++ gcc/testsuite/g++.dg/tree-ssa/ivopts-1.C
@@ -13,6 +13,6 @@ void bar(void)
 }
 
 /* { dg-final { scan-tree-dump-not "-&x" "ivopts" } } */
-/* { dg-final { scan-tree-dump-not "offset: -4B" "ivopts" { xfail i?86-*-* x86_64-*-* } } } */
-/* { dg-final { scan-tree-dump-not "&x\\\[5\\\]" "ivopts" { xfail i?86-*-* x86_64-*-* } } } */
+/* { dg-final { scan-tree-dump-not "offset: -4B" "ivopts" { xfail i?86-*-* x86_64-*-* hppa*-*-* } } } */
+/* { dg-final { scan-tree-dump-not "&x\\\[5\\\]" "ivopts" { xfail i?86-*-* x86_64-*-* hppa*-*-* } } } */
 /* { dg-final { cleanup-tree-dump "ivopts" } } */
