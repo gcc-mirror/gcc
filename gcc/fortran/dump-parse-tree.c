9c88743aba8e4ca37125b5dcb1417eeaf336896e
Francois-Xavier Coudert <coudert@clipper.ens.fr>
2006-05-27 11:06:01 +0200

    Backport PRs 26551, 27320, 27552
    	Backports from mainline
    
    	PR fortran/27552
    	* dump-parse-tree.c (gfc_show_expr): Deal with Hollerith constants.
    	* data.c (create_character_intializer): Set from_H flag if character is
    	initialized by Hollerith constant.
    
    	PR fortran/27320
    	* dump-parse-tree.c (gfc_show_code_node): Try harder to find the
    	called procedure name.
    
    	PR fortran/26551
    	* resolve.c (resolve_call, resolve_function): Issue an error
    	if a function or subroutine call is recursive but the function or
    	subroutine wasn't declared as such.
    	* gfortran.dg/recursive_check_1.f: New test.
    	* gfortran.dg/recursive_check_2.f90: New test.
    
    Co-Authored-By: Feng Wang <fengwang@nudt.edu.cn>
    
    From-SVN: r114152

--- gcc/fortran/dump-parse-tree.c
+++ gcc/fortran/dump-parse-tree.c
@@ -348,6 +348,16 @@ gfc_show_expr (gfc_expr * p)
       break;
 
     case EXPR_CONSTANT:
+      if (p->from_H || p->ts.type == BT_HOLLERITH)
+	{
+	  gfc_status ("%dH", p->value.character.length);
+	  c = p->value.character.string;
+	  for (i = 0; i < p->value.character.length; i++, c++)
+	    {
+	      gfc_status_char (*c);
+	    }
+	  break;
+	}
       switch (p->ts.type)
 	{
 	case BT_INTEGER:
@@ -862,7 +872,13 @@ gfc_show_code_node (int level, gfc_code * c)
       break;
 
     case EXEC_CALL:
-      gfc_status ("CALL %s ", c->resolved_sym->name);
+      if (c->resolved_sym)
+	gfc_status ("CALL %s ", c->resolved_sym->name);
+      else if (c->symtree)
+	gfc_status ("CALL %s ", c->symtree->name);
+      else
+	gfc_status ("CALL ?? ");
+
       gfc_show_actual_arglist (c->ext.actual);
       break;
 
