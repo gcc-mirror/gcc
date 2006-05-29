1753331b5bb8a14b709916450c93c98c2e2a007b
Roger Sayle <roger@eyesopen.com>
2006-05-29 16:22:05 +0000

    re PR tree-optimization/24964 (Does not optimise abs(x)**2 to x**2)
    	PR tree-optimization/24964
    	* simplify-rtx.c (simplify_binary_operation_1): Add function comment.
    	<MULT>: Minor clean-up.  Don't convert x*-1.0 into -x if we honor
    	signaling NaNs.  Optimize -x*-x as x*x for all float modes, and
    	abs(x)*abs(x) as x*x for scalar floating point modes.
    
    	* gcc.target/i386/387-10.c: New test case.
    
    From-SVN: r114206

---
+++ gcc/testsuite/gcc.target/i386/387-10.c
@@ -0,0 +1,20 @@
+/* PR tree-optimization/24964 */
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -mfpmath=387" } */
+
+double fabs(double x);
+
+double test1(double x)
+{
+  double t = fabs(x);
+  return t*t;
+}
+
+double test2(double x)
+{
+  double t = -x;
+  return t*t;
+}
+
+/* { dg-final { scan-assembler-not "fchs" } } */
+/* { dg-final { scan-assembler-not "fabs" } } */
