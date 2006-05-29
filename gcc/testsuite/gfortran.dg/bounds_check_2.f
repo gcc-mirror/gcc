81ad3bc785c8a6366481340a21b5305e0bad6939
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-05-29 19:21:27 +0000

    PR fortran/19777
    	* trans-array.c (gfc_conv_array_ref): Perform out-of-bounds
    	checking for assumed-size arrrays for all but the last dimension.
    
    	* gfortran.dg/bounds_check_2.f: Add new check for multidimensional
    	arrays.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@114210 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/bounds_check_2.f
+++ gcc/testsuite/gfortran.dg/bounds_check_2.f
@@ -5,9 +5,12 @@
       integer          npts
       parameter        (npts=10)
       double precision v(npts)
+      double precision w(npts,npts,npts)
       external         init1
+      external         init2
 
       call init1 (npts, v)
+      call init2 (npts, w)
       end
 
       subroutine init1 (npts, v)
@@ -20,4 +23,17 @@
       do 10 i = 1, npts
          v(i) = 0
  10   continue
+      end
+
+      subroutine init2 (npts, w)
+      implicit none
+      integer          npts
+      double precision w(npts,npts,*)
+
+      integer          i
+
+      do 20 i = 1, npts
+         w(i,1,1) = 0
+         w(1,npts,i) = 0
+ 20   continue
       end
