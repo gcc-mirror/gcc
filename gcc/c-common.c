08e17da382974466445c68ae7bb636384370eeed
Volker Reichelt <reichelt@igpm.rwth-aachen.de>
2006-06-04 23:47:15 +0000

    re PR c++/27601 (ICE (in fold_offsetof_1, at c-common.c:5998) on strange offsetof)
    	PR c++/27601
    	* c-common.c (fold_offsetof_1): Handle static members.
    
    	* g++.dg/ext/offsetof1.C: New test.
    
    From-SVN: r114370

--- gcc/c-common.c
+++ gcc/c-common.c
@@ -5959,6 +5959,10 @@ fold_offsetof_1 (tree expr)
     case ERROR_MARK:
       return expr;
 
+    case VAR_DECL:
+      error ("cannot apply %<offsetof%> to static data member %qD", expr);
+      return error_mark_node;
+
     case INDIRECT_REF:
       return size_zero_node;
 
