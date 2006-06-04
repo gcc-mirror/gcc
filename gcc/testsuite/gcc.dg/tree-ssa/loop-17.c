f9ea6ec6c64b5a62a2f57967c0131345c3392348
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-06-04 12:59:40 +0000

    2006-06-04  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/27039
    	* fold-const.c (fold_comparison): Handle pointer comparison
    	again for all comparison codes.  Compare offsets in signed
    	size type.
    	(fold_binary): Move code from here.
    
    	* gcc.dg/tree-ssa/loop-17.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@114357 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/loop-17.c
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-sccp-details" } */
+
+/* To determine the number of iterations in this loop we need to fold
+   p_4 + 4B > p_4 + 8B to false.  This transformation has caused
+   troubles in the past due to overflow issues.  */
+
+int foo (int *p)
+{
+  int i = 0, *x;
+
+  for (x = p; x < p + 2; x++)
+    i++;
+
+  return i;
+}
+
+/* { dg-final { scan-tree-dump "set_nb_iterations_in_loop = 2" "sccp" } } */
+/* { dg-final { cleanup-tree-dump "sccp" } } */
