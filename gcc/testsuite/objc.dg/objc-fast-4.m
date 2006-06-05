b6396fc36e7ef56567009bf1821a3c91d528f410
mrs <mrs@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-06-05 19:20:49 +0000

    * objc.dg/objc-fast-4.m: Skip for ppc64.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@114404 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/objc.dg/objc-fast-4.m
+++ gcc/testsuite/objc.dg/objc-fast-4.m
@@ -1,10 +1,13 @@
-// The code should call objc_msgSend directly, not through a pointer.
+/* The code should call objc_msgSend directly, not through a pointer.  */
 /* { dg-do compile { target powerpc*-*-darwin* } } */
 /* { dg-options "-O0 -fnext-runtime" } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "-m64" } { "" } } */
+/* Radar 4015820 */
+
 #include <objc/Object.h>
 
 void foo(void) {
   Object *o;
   [o++ free];
 }
-/* { dg-final { scan-assembler-not "L_objc_msgSend\\\$non_lazy_ptr" } } */
+/* { dg-final { scan-assembler-not "objc_msgSend" } } */
