b0765c8c35d7871ae9fb67f2959e3d019793ad3d
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-06-05 07:33:18 +0000

    2006-06-05  Paolo Carlini  <pcarlini@suse.de>
    	* include/tr1/random.tcc (Max::value()): Cast 1 to Tp(1) and
    	adjust shift count to w; rename as Max_w.
    	(struct Mod_w): New.
    	(mersenne_twister<>::seed(Gen&, false_type): Use the latter.
    	(mersenne_twister<>::operator()()): Fix ~0ul to ~_UInt().
    	* testsuite/tr1/5_numerical_facilies/random/mersenne_twister/
    	cons/default.cc: Fix ~0ul to 2^32-1.
    	* testsuite/tr1/5_numerical_facilies/random/mersenne_twister/
    	cons/gen1.cc: Likewise.
    
    2006-06-05  Stephen M. Webb  <stephen.webb@bregmasoft.com>
    
    	* include/tr1/random: New.
    	* include/tr1/random.tcc: Likewise.
    	* include/Makefile.am: Add.
    	* testsuite/tr1/5_numerical_facilies/random/ranlux3.cc: New.
    	* testsuite/tr1/5_numerical_facilies/random/ranlux4.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/mt19937.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/variate_generator/
    	requirements/typedefs.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/subtract_with_carry/
    	cons/seed1.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/subtract_with_carry/
    	cons/seed2.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/subtract_with_carry/
    	cons/default.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/subtract_with_carry/
    	cons/gen1.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/subtract_with_carry/
    	requirements/typedefs.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/subtract_with_carry/
    	operators/equal.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/subtract_with_carry/
    	operators/not_equal.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/subtract_with_carry/
    	operators/serialize.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/uniform_real/
    	requirements/typedefs.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/discard_block/
    	requirements/requirements.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/linear_congruential/
    	cons/seed1.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/linear_congruential/
    	cons/seed2.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/linear_congruential/
    	cons/default.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/linear_congruential/
    	cons/gen1.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/linear_congruential/
    	requirements/non_uint_neg.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/linear_congruential/
    	requirements/typedefs.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/linear_congruential/
    	operators/equal.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/linear_congruential/
    	operators/not_equal.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/linear_congruential/
    	operators/serialize.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/xor_combine/
    	requirements/typedefs.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/minstd_rand.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/minstd_rand0.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/uniform_int/
    	cons/range.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/uniform_int/
    	cons/default.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/uniform_int/
    	cons/range_neg.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/uniform_int/
    	requirements/typedefs.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/mersenne_twister/
    	cons/seed1.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/mersenne_twister/
    	cons/seed2.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/mersenne_twister/
    	cons/default.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/mersenne_twister/
    	cons/gen1.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilies/random/mersenne_twister/
    	requirements/typedefs.cc: Likewise.
    	* include/Makefile.in: Regenerate.
    	* testsuite/tr1/headers.cc: Update.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@114384 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/tr1/5_numerical_facilies/random/linear_congruential/operators/serialize.cc
@@ -0,0 +1,50 @@
+// 2006-06-04  Stephen M. Webb <stephen.webb@bregmasoft.com>
+//
+// Copyright (C) 2006 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 5.1.4.1 Class template linear_congruential
+// 5.1 [3] Table 16
+
+#include <sstream>
+#include <tr1/random>
+#include <testsuite_hooks.h>
+
+void
+test01() 
+{
+  bool test __attribute__((unused)) = true;
+ 
+  std::stringstream str;
+  std::tr1::minstd_rand0 a;
+  std::tr1::minstd_rand0 b;
+  
+  a(); // advance
+  str << a;
+  
+  VERIFY( a != b );
+  
+  str >> b;
+  VERIFY( a == b );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/tr1/5_numerical_facilies/random/variate_generator/requirements/typedefs.cc
@@ -0,0 +1,42 @@
+// { dg-do compile }
+//
+// 2006-06-04  Stephen M. Webb <stephen.webb@bregmasoft.com>
+//
+// Copyright (C) 2006 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 5.1.3 class template variate_generator
+
+#include <tr1/random>
+
+void
+test01() 
+{ 
+  using namespace std::tr1;
+
+  typedef variate_generator
+    <
+    linear_congruential<unsigned long, 16807 , 0 , 2147483647>,
+    uniform_int<int>
+    > test_type;
+
+  typedef test_type::engine_type       engine_type;
+  typedef test_type::engine_value_type engine_value_type;
+  typedef test_type::distribution_type distribution_type;
+  typedef test_type::result_type       result_type;
+}
