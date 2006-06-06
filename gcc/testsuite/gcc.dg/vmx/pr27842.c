19155a13b9927963528e4dd44c3911680633871b
uweigand <uweigand@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-06-06 17:01:27 +0000

    PR target/27842 	* config/rs6000/altivec.md (UNSPEC_VSLW): Remove. 	("altivec_vspltisw_v4sf", "altivec_vslw_v4sf"): Remove. 	("mulv4sf3", "absv4sf3", "negv4sf3"): Adapt users to use 	V4SImode temporaries and operations instead.
    	PR target/27842
    	* gcc.dg/vmx/pr27842.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@114438 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vmx/pr27842.c
@@ -0,0 +1,28 @@
+/* { dg-do run } */
+
+#include <altivec.h>
+
+extern void abort (void);
+extern int memcmp (const void *, const void *, __SIZE_TYPE__);
+
+void test (vector float *p, int n)
+{
+  int i;
+  for (i = 0; i < n; i++)
+    p[i] = vec_abs (p[i]);
+}
+
+int
+main (void)
+{
+  vector float p = (vector float){ 0.5, 0.5, 0.5, 0.5 };
+  vector float q = p;
+
+  test (&p, 1);
+
+  if (memcmp (&p, &q, sizeof (p)))
+    abort ();
+
+  return 0;
+}
+
