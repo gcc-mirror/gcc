209fd0a02ebea7a7a0f553e5a86153a571900ef2
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-06-07 01:50:09 +0000

    2006-06-06  Paolo Carlini  <pcarlini@suse.de>
    	* include/tr1/random (mersenne_twister<>::operator==,
    	operator!=, operator<<, operator>>): Implement.
    	* testsuite/tr1/5_numerical_facilities/random/mersenne_twister/
    	operators/equal.cc: New.	
    	* testsuite/tr1/5_numerical_facilities/random/mersenne_twister/
    	operators/not_equal.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilities/random/mersenne_twister/
    	operators/serialize.cc: Likewise.
    
    	* include/tr1/random (subtract_with_carry<>::operator==): Fix,
    	compare the whole state; use std::equal.
    
    	* include/tr1/random: Minor formatting and style changes.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@114459 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/tr1/5_numerical_facilities/random/mersenne_twister/operators/serialize.cc
@@ -0,0 +1,54 @@
+// 2006-06-06  Paolo Carlini  <pcarlini@suse.de>
+//
+// Copyright (C) 2006 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 5.1.4.2 class template mersenne_twister [tr.rand.eng.mers]
+// 5.1.1 Table 16
+
+#include <sstream>
+#include <tr1/random>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+  using std::tr1::mersenne_twister;
+
+  std::stringstream str;
+  mersenne_twister<
+    unsigned long, 32, 624, 397, 31,
+    0x9908b0dful, 11, 7,
+    0x9d2c5680ul, 15,
+    0xefc60000ul, 18> u, v;
+  
+  u(); // advance
+  str << u;
+  
+  VERIFY( u != v );
+  
+  str >> v;
+  VERIFY( u == v );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
