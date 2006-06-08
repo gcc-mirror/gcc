792f183828f1c38c2b580e1e6cee3e5768f60666
mkuvyrkov <mkuvyrkov@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-06-08 08:51:13 +0000

    2006-06-08  Maxim Kuvyrkov  <mkuvyrkov@ispras.ru>
    	PR target/27863
    	* haifa-sched.c (unlink_other_notes, unlink_line_notes): Update basic
    	block boundaries.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@114484 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/haifa-sched.c
+++ gcc/haifa-sched.c
@@ -1243,11 +1243,21 @@ unlink_other_notes (rtx insn, rtx tail)
   while (insn != tail && NOTE_NOT_BB_P (insn))
     {
       rtx next = NEXT_INSN (insn);
+      basic_block bb = BLOCK_FOR_INSN (insn);
+
       /* Delete the note from its current position.  */
       if (prev)
 	NEXT_INSN (prev) = next;
       if (next)
 	PREV_INSN (next) = prev;
+
+      /* Basic block can begin with either LABEL or
+         NOTE_INSN_BASIC_BLOCK.  */
+      gcc_assert (BB_HEAD (bb) != insn);
+
+      /* Check if we are removing last insn in the BB.  */
+      if (BB_END (bb) == insn)
+        BB_END (bb) = prev;
 
       /* See sched_analyze to see how these are handled.  */
       if (NOTE_LINE_NUMBER (insn) != NOTE_INSN_EH_REGION_BEG
@@ -1279,11 +1289,21 @@ unlink_line_notes (rtx insn, rtx tail)
 
       if (write_symbols != NO_DEBUG && NOTE_LINE_NUMBER (insn) > 0)
 	{
+          basic_block bb = BLOCK_FOR_INSN (insn);
+
 	  /* Delete the note from its current position.  */
 	  if (prev)
 	    NEXT_INSN (prev) = next;
 	  if (next)
 	    PREV_INSN (next) = prev;
+
+          /* Basic block can begin with either LABEL or
+             NOTE_INSN_BASIC_BLOCK.  */
+          gcc_assert (BB_HEAD (bb) != insn);
+
+          /* Check if we are removing last insn in the BB.  */
+          if (BB_END (bb) == insn)
+            BB_END (bb) = prev;
 
 	  /* Record line-number notes so they can be reused.  */
 	  LINE_NOTE (insn) = insn;
