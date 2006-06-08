136778e9b41c1d1d095f9e2e74b7255c7ce4f415
Zdenek Dvorak <dvorakz@suse.cz>
2006-06-08 10:17:05 +0200

    re PR rtl-optimization/26449 (ICE in loop invariant motion)
    	PR rtl-optimization/26449
    	* loop-invariant.c (move_invariant_reg): Fail if force_operand fails.
    
    	* gcc.dg/pr26449.c: New test.
    
    From-SVN: r114481

---
+++ gcc/testsuite/gcc.dg/pr26449.c
@@ -0,0 +1,15 @@
+/* { dg-do compile { target i?86-*-* } } */
+/* { dg-options "-O1 -ftree-vectorize -march=pentium4 -std=c99" } */
+
+void matmul_i4 (int bbase_yn, int xcount)
+{
+  int x;
+  int * restrict dest_y;
+  const int * abase_n;
+
+  for (x = 0;  x < xcount;  x++)
+    {
+      dest_y[x] += abase_n[x] * bbase_yn; 
+    }
+}
+
