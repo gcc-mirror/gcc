11f7dd15f1893b04b07965f7cc8a6bfa9459ac9b
Joern Rennecke <amylaar@gcc.gnu.org>
2006-06-15 17:36:21 +0100

    _Pragma6.c: Fix comment.
    	* gcc.dg/cpp/_Pragma6.c: Fix comment.
    	Test is not supported on sh*-unknown-elf nor sh*-superh-elf.
    	* g++.dg/cpp/_Pragma1.C: Likewise.
    
    From-SVN: r114680

--- gcc/testsuite/gcc.dg/cpp/_Pragma6.c
+++ gcc/testsuite/gcc.dg/cpp/_Pragma6.c
@@ -1,7 +1,8 @@
 /* PR c/27747 */
 /* This is supposed to succeed only if
-   the target doesn't define HANDLE_PRAGMA_PACK_WITH_EXPANSION.  */
-/* { dg-do compile { target { ! *-*-solaris2* } } } */
+   the target defines HANDLE_PRAGMA_PACK_PUSH_POP
+   and doesn't define HANDLE_PRAGMA_PACK_WITH_EXPANSION.  */
+/* { dg-do compile { target { ! { *-*-solaris2* sh*-[us]*-elf } } } } */
 
 #define push bar
 #define foo _Pragma ("pack(push)")
