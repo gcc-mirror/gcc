e11b210a9c1b68c361a9f9f172a4d26e0d37ed87
Paolo Carlini <pcarlini@suse.de>
2006-06-16 22:13:13 +0000

    locale_facets.tcc (time_get<>::_M_extract_via_format): Ignore the value of the __err argument.
    2006-06-17  Paolo Carlini  <pcarlini@suse.de>
    
    	* include/bits/locale_facets.tcc (time_get<>::_M_extract_via_format):
    	Ignore the value of the __err argument.
    	(time_get<>::do_get_weekday): Likewise.
    	(time_get<>::do_get_monthname): Likewise.
    	* testsuite/22_locale/time_get/get_year/wchar_t/5.cc: New.
    	* testsuite/22_locale/time_get/get_year/char/5.cc: Likewise.
    	* testsuite/22_locale/time_get/get_monthname/wchar_t/5.cc: Likewise.
    	* testsuite/22_locale/time_get/get_monthname/char/5.cc: Likewise.
    	* testsuite/22_locale/time_get/get_weekday/wchar_t/5.cc: Likewise.
    	* testsuite/22_locale/time_get/get_weekday/char/5.cc: Likewise.
    	* testsuite/22_locale/time_get/get_date/wchar_t/5.cc: Likewise.
    	* testsuite/22_locale/time_get/get_date/char/5.cc: Likewise.
    	* testsuite/22_locale/time_get/get_time/wchar_t/5.cc: Likewise.
    	* testsuite/22_locale/time_get/get_time/char/5.cc: Likewise.
    
    From-SVN: r114728

---
+++ libstdc++-v3/testsuite/22_locale/time_get/get_date/char/5.cc
@@ -0,0 +1,71 @@
+// 2006-06-16  Paolo Carlini  <pcarlini@suse.de>
+
+// Copyright (C) 2006 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 22.2.5.1.1 time_get members
+
+#include <locale>
+#include <sstream>
+#include <testsuite_hooks.h>
+
+// Check that the err argument is ignored by get_date.
+void test01()
+{
+  using namespace std;
+  bool test __attribute__((unused)) = true;
+
+  typedef string::const_iterator iter_type;
+  typedef time_get<char, iter_type> time_get_type;
+  const ios_base::iostate goodbit = ios_base::goodbit;
+  const ios_base::iostate eofbit = ios_base::eofbit;  
+  const ios_base::iostate failbit = ios_base::failbit;
+  ios_base::iostate err = goodbit;
+  const locale loc_c = locale::classic();
+
+  // Create "C" time objects
+  const tm time_sanity = __gnu_test::test_tm(0, 0, 12, 26, 5, 97, 2, 0, 0);
+  tm tm0 = __gnu_test::test_tm(0, 0, 0, 0, 0, 0, 0, 0, 0);
+  tm tm1 = __gnu_test::test_tm(0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+  istringstream iss; 
+  iss.imbue(locale(loc_c, new time_get_type));
+
+  const time_get_type& tg = use_facet<time_get_type>(iss.getloc());
+
+  const string str0 = "1";
+  iter_type end0 = tg.get_date(str0.begin(), str0.end(), iss, err, &tm0);
+  VERIFY( err == (failbit | eofbit) );
+  VERIFY( tm0.tm_year == 0 );
+  VERIFY( tm0.tm_mon == 0 );
+  VERIFY( tm0.tm_mday == 0 );
+
+  const string str1 = "06/26/97 ";
+  iter_type end1 = tg.get_date(str1.begin(), str1.end(), iss, err, &tm1);
+  VERIFY( err == (failbit | eofbit) );
+  VERIFY( tm1.tm_year == time_sanity.tm_year );
+  VERIFY( tm1.tm_mon == time_sanity.tm_mon );
+  VERIFY( tm1.tm_mday == time_sanity.tm_mday );
+  VERIFY( *end1 == ' ' );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
