b6cf30ce1bfc93fcd3e3e0a6b3b89a92726e17b9
jiez <jiez@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-06-18 10:09:59 +0000

    * config/bfin/bfin.c (MAX_LOOP_LENGTH): Define to be 2042 instead 	of 4096.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@114755 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/bfin/bfin.c
+++ gcc/config/bfin/bfin.c
@@ -2739,7 +2739,7 @@ bfin_hardware_loop (void)
 #define MAX_LOOP_DEPTH 2
 
 /* Maximum size of a loop.  */
-#define MAX_LOOP_LENGTH 4096
+#define MAX_LOOP_LENGTH 2042
 
 /* We need to keep a vector of loops */
 typedef struct loop_info *loop_info;
