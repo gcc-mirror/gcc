94bf38b394f369bf88ff6b3c5016d1ba11c9ed7f
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-06-19 20:10:02 +0000

    2006-06-19  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/27090
    	* g++.dg/tree-ssa/pr27090.C: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@114786 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/tree-ssa/pr27090.C
@@ -0,0 +1,21 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-optimized" } */
+
+template <class T>
+struct Bar
+{
+        int get() { return static_cast<T*>(this)->get2(); }
+};
+struct Foo : public Bar<Foo>
+{
+        int get2() { return x; }
+        int x;
+};
+
+int foo(Foo& f)
+{
+        return f.get();
+}
+
+/* { dg-final { scan-tree-dump "return f->x;" "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
