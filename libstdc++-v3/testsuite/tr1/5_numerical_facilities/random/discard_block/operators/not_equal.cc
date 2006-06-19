3c618f8732814407a5c98146fa24761ba7dddd78
Paolo Carlini <pcarlini@suse.de>
2006-06-19 16:03:07 +0000

    random (xor_combine<>::operator<<): Fix typo.
    2006-06-19  Paolo Carlini  <pcarlini@suse.de>
    
    	* include/tr1/random (xor_combine<>::operator<<): Fix typo.
    	
    	* include/tr1/random (bernoulli_distribution::min(),
    	bernoulli_distribution::max(), geometric_distribution<>::min(),
    	geometric_distribution<>::max()): Remove.
    
    	* include/tr1/random (geometric_distribution<>::operator()):
    	Simplify formula.
    
    	* include/tr1/random: Minor cosmetic changes.
    
    	* testsuite/tr1/5_numerical_facilities/random/discard_block/
    	operators/equal.cc: New.
    	* testsuite/tr1/5_numerical_facilities/random/discard_block/
    	operators/not_equal.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilities/random/discard_block/
    	operators/serialize.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilities/random/xor_combine/
    	operators/equal.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilities/random/xor_combine/
    	operators/not_equal.cc: Likewise.
    	* testsuite/tr1/5_numerical_facilities/random/xor_combine/
    	operators/serialize.cc: Likewise.
    
    From-SVN: r114777

---
+++ libstdc++-v3/testsuite/tr1/5_numerical_facilities/random/discard_block/operators/not_equal.cc
@@ -0,0 +1,52 @@
+// 2006-06-19  Paolo Carlini  <pcarlini@suse.de>
+//
+// Copyright (C) 2006 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 5.1.4.5 class template discard_block [tr.rand.eng.disc]
+// 5.1.1 Table 16
+
+#include <tr1/random>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace std::tr1;
+
+  discard_block
+    <
+    subtract_with_carry<long, (1 << 24), 10, 24>,
+    389, 24
+    > u(1);
+
+  discard_block
+    <
+    subtract_with_carry<long, (1 << 24), 10, 24>,
+    389, 24
+    > v(2);
+
+  VERIFY( u != v );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/tr1/5_numerical_facilities/random/xor_combine/operators/serialize.cc
@@ -0,0 +1,56 @@
+// 2006-06-19  Paolo Carlini  <pcarlini@suse.de>
+//
+// Copyright (C) 2006 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 5.1.4.6 class template xor_combine [tr.rand.eng.xor]
+// 5.1.1 Table 16
+
+#include <sstream>
+#include <tr1/random>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+  using std::tr1::xor_combine;
+  using std::tr1::minstd_rand;
+  using std::tr1::mt19937;
+
+  std::stringstream str;
+  xor_combine
+    <
+    minstd_rand, 1,
+    mt19937, 2
+    > u, v;
+ 
+  u(); // advance
+  str << u;
+  
+  VERIFY( u != v );
+  
+  str >> v;
+  VERIFY( u == v );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
