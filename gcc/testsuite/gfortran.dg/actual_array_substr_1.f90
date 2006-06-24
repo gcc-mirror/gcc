0298d329a815077c1759301fd311c32b2dbf9e01
Paul Thomas <pault@gcc.gnu.org>
2006-06-24 14:25:24 +0000

    re PR fortran/28118 (ICE calling subroutine defined via explicit interface)
    2006-06-24  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/28118
    	* trans-array.c (gfc_conv_expr_descriptor): When building temp,
    	use the substring reference to calculate the length if the
    	expression does not have a charlen.
    
    2006-06-24  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/28118
    	* gfortran.dg/actual_array_substr_1.f90: New test.
    
    From-SVN: r114965

---
+++ gcc/testsuite/gfortran.dg/actual_array_substr_1.f90
@@ -0,0 +1,22 @@
+! { dg-do run }
+! Test fix of PR28118, in which a substring reference to an
+! actual argument with an array reference would cause a segfault.
+!
+! Contributed by Paul Thomas  <pault@gcc.gnu.org>
+!
+program gfcbug33
+  character(12) :: a(2)
+  a(1) = "abcdefghijkl"
+  a(2) = "mnopqrstuvwx"
+  call foo ((a(2:1:-1)(6:)))
+  call bar ((a(:)(7:11)))
+contains
+  subroutine foo (chr)
+    character(7) :: chr(:)
+    if (chr(1)//chr(2) .ne. "rstuvwxfghijkl") call abort ()
+  end subroutine foo
+  subroutine bar (chr)
+    character(*) :: chr(:)
+    if (trim(chr(1))//trim(chr(2)) .ne. "ghijkstuvw") call abort ()
+  end subroutine bar
+end program gfcbug33
