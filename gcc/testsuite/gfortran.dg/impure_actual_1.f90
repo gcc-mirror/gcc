d68bd5a8f29c31ca172979e5a37368aff6685d26
Paul Thomas <pault@gcc.gnu.org>
2006-06-25 15:11:02 +0000

    re PR fortran/25056 (non-PURE function should not be a valid argument)
    2006-06-25  Paul Thomas  <pault@gcc.gnu.org>
     
    	PR fortran/25056
    	* interface.c (compare_actual_formal): Signal an error if the formal
    	argument is a pure procedure and the actual is not pure.
    
    	PR fortran/27554
    	* resolve.c (resolve_actual_arglist): If the type of procedure
    	passed as an actual argument is not already declared, see if it is
    	an intrinsic.
    
    	PR fortran/25073
    	* resolve.c (resolve_select): Use bits 1 and 2 of a new int to
    	keep track of  the appearance of constant logical case expressions.
    	Signal an error is either value appears more than once.
    
    	PR fortran/20874
    	* resolve.c (resolve_fl_procedure): Signal an error if an elemental
    	function is not scalar valued.
    
    	PR fortran/20867
    	* match.c (recursive_stmt_fcn): Perform implicit typing of variables.
    
    	PR fortran/22038
    	* match.c (match_forall_iterator): Mark new variables as
    	FL_UNKNOWN if the match fails.
    
    	PR fortran/28119
    	* match.c (gfc_match_forall): Remove extraneous call to
    	gfc_match_eos.
    
    	PR fortran/25072
    	* resolve.c (resolve_code, resolve_function): Rework
    	forall_flag scheme so that it is set and has a value of
    	2, when the code->expr (ie. the forall mask) is resolved.
    	This is used to change "block" to "mask" in the non-PURE
    	error message.
    
    
    2006-06-25  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/20867
    	* gfortran.dg/stfunc_3.f90: New test.
    
    	PR fortran/25056
    	* gfortran.dg/impure_actual_1.f90: New test.
    
    	PR fortran/20874
    	* gfortran.dg/elemental_result_1.f90: New test.
    
    	PR fortran/25073
    	* gfortran.dg/select_7.f90: New test.
    
    	PR fortran/27554
    	* intrinsic_actual_1.f: New test.
    
    	PR fortran/22038
    	PR fortran/28119
    	* gfortran.dg/forall_4.f90: New test.
    
    	PR fortran/25072
    	* gfortran.dg/forall_5.f90: New test.
    
    From-SVN: r114987

---
+++ gcc/testsuite/gfortran.dg/impure_actual_1.f90
@@ -0,0 +1,25 @@
+! { dg-do compile }
+! Tests the fix for PR25056 in which a non-PURE procedure could be
+! passed as the actual argument to a PURE procedure.
+!
+! Contributed by Joost VandeVondele  <jv244@cam.ac.uk>
+!
+MODULE M1
+CONTAINS
+ FUNCTION L()
+  L=1
+ END FUNCTION L
+ PURE FUNCTION J(K)
+   INTERFACE
+     PURE FUNCTION K()
+     END FUNCTION K
+   END INTERFACE
+   J=K()
+ END FUNCTION J
+END MODULE M1
+USE M1
+ write(6,*) J(L) ! { dg-error "Expected a PURE procedure for argument" }
+END
+
+! { dg-final { cleanup-modules "M1" } }
+
