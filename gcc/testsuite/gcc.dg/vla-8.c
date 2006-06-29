4ada8cdcbe09ce1f239760684e3b27b96fc3ae6d
Mike Stump <mrs@apple.com>
2006-06-29 17:38:10 +0000

    * gcc.dg/vla-8.c: Add additional testcases.
    From-SVN: r115074

--- gcc/testsuite/gcc.dg/vla-8.c
+++ gcc/testsuite/gcc.dg/vla-8.c
@@ -4,3 +4,27 @@
 
 int a;
 struct s { void (*f)(int (*)[a]); };
+
+static int i;
+static int new_i() { i++; return i; }
+static int bar1(int a[new_i()][new_i()]);
+
+void foo(int n) {
+  extern void bar(int i[n][n]);			/* Since this isn't a VM type ensure we can have linkage.  */
+  extern int bar1(int a[new_i()][new_i()]);	/* Since this isn't a VM type ensure we can have linkage.  */
+}
+
+void foo1(int n) {
+  goto A;
+  void bar(int i[n][n]);			/* Not a VM type, as VM arguments don't matter. */
+  int bar1(int a[new_i()][new_i()]);		/* Not a VM type, as VM arguments don't matter. */
+ A:
+  ;
+}
+
+void foo2(int n) {
+  goto A;		/* { dg-error "jump into scope of identifier with variably modified type" } */
+  int (*(*bar2)(void))[n];
+ A:
+  ;
+}
