036d27596ac877cd814bb92eeec1d24b40cccdc4
Jason Merrill <jason@redhat.com>
2006-06-30 15:45:54 -0400

    re PR c++/18698 (Error message using "using" for code not using "using" ;-))
            PR c++/18698
            * decl2.c (grokfield): Only try to treat the decl as an access
            declaration if the scope is a class.
    
    From-SVN: r115102

---
+++ gcc/testsuite/g++.dg/parse/access10.C
@@ -0,0 +1,13 @@
+// PR c++/18698
+// The compiler was giving an error message for invalid syntax
+// that irrelevantly talked about using-declarations.
+
+template<int> struct A
+{
+    ::A~();			// { dg-bogus "using-declaration" }
+};
+
+// Instead of the bogus error we get 3 separate errors.
+// { dg-error "no type" "" { target *-*-* } 5 }
+// { dg-error "::" "" { target *-*-* } 5 }
+// { dg-error "~" "" { target *-*-* } 5 }
