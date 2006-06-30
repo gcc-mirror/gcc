dbffbef55c2d8ac812f077706f3ac6c7b749bfc2
mrs <mrs@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-06-30 20:10:51 +0000

    * g++.dg/ext/visibility/fvisibility-override2.C: Fix for systems 	that don't support internal visibility.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@115104 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ext/visibility/fvisibility-override2.C
+++ gcc/testsuite/g++.dg/ext/visibility/fvisibility-override2.C
@@ -1,6 +1,6 @@
 /* Test that -fvisibility does not override class member specific settings. */
 /* { dg-do compile } */
-/* { dg-require-visibility "" } */
+/* { dg-require-visibility "internal" } */
 /* { dg-options "-fvisibility=hidden" } */
 /* { dg-final { scan-not-hidden "Foo.methodEv" } } */
 
