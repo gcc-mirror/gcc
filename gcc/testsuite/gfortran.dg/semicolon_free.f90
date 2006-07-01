1b82aeb95f94a4471ba2e7688e9c681062cc4e41
tobi <tobi@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-07-01 15:03:30 +0000

    2006-07-01  Tobias Schlueter  <tobias.schlueter@physik.uni-muenchen.de>
    fortran/
    PR fortran/19259
    * parse.c (next_free): Error out on line starting with semicolon.
    (next_fixed): Fix formatting.  Error out on line starting with
    semicolon.
    testsuite/
    PR fortran/19259
    * gfortran.dg/semicolon_fixed.c: New.
    * gfortran.dg/semicolon_free.c: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@115115 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/semicolon_free.f90
@@ -0,0 +1,9 @@
+! { dg-do compile }
+! PR 19259 Semicolon cannot start a line
+x=1; y=1;
+x=2;;
+x=3;
+ ; ! { dg-error "Semicolon at" }
+;; ! { dg-error "Semicolon at" }
+111 ; ! { dg-error "Semicolon at" }
+end
