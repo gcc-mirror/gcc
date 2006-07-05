dc8f5205374552519f0e4a89c115f3a17c23931b
Richard Guenther <rguenther@suse.de>
2006-07-05 16:24:57 +0000

    re PR target/28158 (ICE on complex operation with -O1 -msse)
    2006-07-05  Richard Guenther  <rguenther@suse.de>
    
    	PR target/28158
    	* config/i386/i386.md (*negdf2_1): Restrict pattern to
    	TARGET_SSE2 targets if using SSE math.
    	(*absdf2_1): Likewise.
    
    	* gfortran.dg/pr28158.f90: New testcase.
    
    From-SVN: r115203

---
+++ gcc/testsuite/gfortran.dg/pr28158.f90
@@ -0,0 +1,7 @@
+! { dg-do compile { target i?86-*-* x86_64-*-* } }
+! { dg-require-effective-target ilp32 }
+! { dg-options "-O -msse -mfpmath=sse" }
+    subroutine yhalf(z)
+    complex cdexpj,z
+    z=cdexpj((0.d0,1.d0)*z)
+    end
