09b868558b4beffb64f04223ffb981d92052b53e
Richard Guenther <rguenther@suse.de>
2006-07-05 10:54:17 +0000

    re PR middle-end/27084 (Does not propagate memory load base through useless type conversion)
    2006-07-05  Richard Guenther  <rguenther@suse.de>
    	Andrew Pinski  <pinskia@gcc.gnu.org>
    
    	PR c++/27084
    	* cp-objcp-common.c (cxx_types_compatible_p): Ignore
    	top level qualifiers for pointer type comparisons.
    
    	* g++.dg/tree-ssa/copyprop-1.C: New testcase.
    
    Co-Authored-By: Andrew Pinski <pinskia@gcc.gnu.org>
    
    From-SVN: r115200

---
+++ gcc/testsuite/g++.dg/tree-ssa/copyprop-1.C
@@ -0,0 +1,29 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-dce2" } */
+
+/* Verify that we can eliminate the useless conversions to/from
+   const qualified pointer types
+     this_2 = o_1;
+     D.20003_4 = this_2->data_m;
+     this_5 = D.20003_4;
+     D.20005_6 = this_5->value;
+   copyprop should propagate o_1 and D.20003_4 to the loads of data_m
+   and value.  dce removes all traces of this.  */
+
+struct Data {
+  int get() const { return value; }
+  int value;
+};
+
+struct Object {
+  int operator[](int i) const { return data_m->get(); }
+  Data *data_m;
+};
+
+int foo(Object&o)
+{
+  return o[0];
+}
+
+/* { dg-final { scan-tree-dump-not ".* = \[^>;\]*;" "dce2" } } */
+/* { dg-final { cleanup-tree-dump "dce2" } } */
