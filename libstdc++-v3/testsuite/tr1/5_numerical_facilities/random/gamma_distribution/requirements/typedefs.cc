33251a2dc31fe5157741af51027ea0a145465dfc
Paolo Carlini <pcarlini@suse.de>
2006-07-06 23:30:14 +0000

    random (class gamma_distribution<>): Add.
    2006-07-06  Paolo Carlini  <pcarlini@suse.de>
    
    	* include/tr1/random (class gamma_distribution<>): Add.
    	* include/tr1/random.tcc (gamma_distribution<>::operator(),
    	operator<<(std::basic_ostream<>&, const gamma_distribution<>&)):
    	Define.
    	* testsuite/tr1/5_numerical_facilities/random/gamma_distribution/
    	requirements/typedefs.cc: New.
    
    From-SVN: r115236

---
+++ libstdc++-v3/testsuite/tr1/5_numerical_facilities/random/gamma_distribution/requirements/typedefs.cc
@@ -0,0 +1,37 @@
+// { dg-do compile }
+//
+// 2006-07-06  Paolo Carlini  <pcarlini@suse.de>
+//
+// Copyright (C) 2006 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 5.1.7.9 Class template gamma_distribution [tr.rand.dist.gamma]
+// 5.1.1 [7] Table 17
+
+#include <tr1/random>
+
+void
+test01() 
+{ 
+  using namespace std::tr1;
+
+  typedef gamma_distribution<double> test_type;
+
+  typedef test_type::input_type  input_type;
+  typedef test_type::result_type result_type;
+}
