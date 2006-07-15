9c9309815325c3da45ec43a0949f6619788e80ad
Mike Stump <mrs@gcc.gnu.org>
2006-07-15 17:03:44 +0000

    revert: re PR c/28280 (bogus "statement with no effect" warning with VLA and typeof)
    Revert:
            PR c/28280
            * c-parser.c (c_parser_typeof_specifier): Don't use
            c_finish_expr_stmt, open code desired semantics instead.
    
    From-SVN: r115479

--- gcc/testsuite/gcc.dg/vla-9.c
+++
@@ -1,9 +0,0 @@
-/* { dg-do compile  } */
-/* { dg-options "-std=c99 -pedantic-errors -W -Wall" } */
-/* PR c/28280 */
-
-void f(__SIZE_TYPE__ d)
-{
-  typedef int t[d];
-  t *g = (__typeof (g)) d;
-}
