ed4439b12944ccd426f51eb63f9fc234a7517e2a
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-07-17 15:31:12 +0000

    gcc/ 	PR middle-end/28403 	* optabs.c (expand_doubleword_shift): Wrap the call to 	do_compare_rtx_and_jump with NO_DEFER_POP and OK_DEFER_POP.
    gcc/testsuite/
    	PR middle-end/28403
    	* gcc.c-torture/execute/pr28403.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@115525 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/pr28403.c
@@ -0,0 +1,23 @@
+typedef unsigned long long ull;
+int global;
+
+int __attribute__((noinline))
+foo (int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8)
+{
+  global = x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8;
+}
+
+ull __attribute__((noinline))
+bar (ull x)
+{
+  foo (1, 2, 1, 3, 1, 4, 1, 5);
+  return x >> global;
+}
+
+int
+main (void)
+{
+  if (bar (0x123456789abcdefULL) != (0x123456789abcdefULL >> 18))
+    abort ();
+  exit (0);
+}
