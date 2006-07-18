548a6c6d0eaf08b916db237479647fa01d7e91a7
Diego Novillo <dnovillo@redhat.com>
2006-07-18 17:27:57 +0000

    re PR tree-optimization/28410 (Wrong aliasing with global var grouping during call clobbering)
    	PR 28410
    	* tree-ssa-operands.c (access_can_touch_variable): Update
    	comment.
    	Return true if ALIAS is .GLOBAL_VAR.
    
    testsuite/ChangeLog
    
    	PR 28410
    	* gcc.dg/tree-ssa/pr28410.c: New test.
    
    From-SVN: r115564

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr28410.c
@@ -0,0 +1,26 @@
+/* { dg-do run } */
+/* { dg-options "-O2 --param global-var-threshold=1" } */
+
+extern void abort(void);
+struct Bar { int p; };
+struct Foo { struct Bar *p; };
+struct Bar p0 = { 0 };
+struct Bar p1 = { 1 };
+void bar(struct Foo *f)
+{
+  f->p = &p0;
+}
+int foo(struct Foo *f)
+{
+  f->p->p = 1;
+  bar(f);
+  return f->p->p;
+}
+int main()
+{
+  struct Foo f;
+  f.p = &p1;
+  if (foo(&f) != 0)
+    abort ();
+  return 0;
+}
