04ad23843e0c04d788fb826f729742e84dc488e4
mark <mark@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-07-21 19:13:22 +0000

    * java/io/OutputStreamWriter.java (flush): Create work if null.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@115647 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/java/io/OutputStreamWriter.java
+++ libjava/java/io/OutputStreamWriter.java
@@ -203,6 +203,8 @@ public class OutputStreamWriter extends Writer
 
 	// Always write -- if we are close()ing then we want to make
 	// sure the converter is flushed.
+	if (work == null)
+	  work = new char[100];
 	writeChars(work, 0, wcount);
 	wcount = 0;
 
