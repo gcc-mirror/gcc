b85623b5c78ed8a187ae4d35f3723b27d84386e6
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-07-21 21:00:20 +0000

    * decl2.c (determine_visibility): Don't propagate visibility from         type to decl.         (constrain_class_visibility): Don't warn in system headers.         Don't warn about pointer fields.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@115651 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ext/visibility/warn2.C
+++ gcc/testsuite/g++.dg/ext/visibility/warn2.C
@@ -1,6 +1,5 @@
 // Complain when a class is specified with greater visibility than one of
-// its members' types or bases, and when a declaration has greater
-// visibility than its type.
+// its members' types or bases.
 
 // { dg-require-visibility "" }
 
@@ -13,7 +12,5 @@ struct B
 {				// { dg-warning "visibility" }
   N::A a;
 };
-
-N::A f () { }			// { dg-warning "visibility" }
 
 struct C: public N::A { };	// { dg-warning "visibility" }
