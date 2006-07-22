0ae57e4c099985f028848d23f9f2365c4a38fc5a
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2006-07-22 03:18:33 +0000

    re PR fortran/28339 (gfortran misses a record from a format statement)
    2006-07-21  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
            PR libgfortran/28339
            * gfortran.dg/arrayio_8.f90: New test.
    
    From-SVN: r115671

---
+++ gcc/testsuite/gfortran.dg/arrayio_8.f90
@@ -0,0 +1,15 @@
+! { dg-do run }
+! PR28339, This test checks that internal unit array I/O handles a full record
+! and and advances to th enext record properly.  Test case derived from PR
+! Submitted by Jerry DeLisle  <jvdelisle@gcc.gnu.org>
+      program main
+      integer          i
+      character*8      rec(3)
+      rec = ""
+      write (rec,fmt=99999)
+      if (rec(1).ne.'12345678') call abort()
+      if (rec(2).ne.'record2') call abort()
+      if (rec(3).ne.'record3') call abort()
+99999 format ('12345678',/'record2',/'record3')
+      end
+
