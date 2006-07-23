17933c7b9262c36514be09ac7261cf3fdb45ebe6
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-07-23 21:13:07 +0000

    * gcc.dg/fold-cond-1.c: Increase test case portability by checking 	that "g ? 1 : h" doesn't match, instead of checking for "(g | h) != 0" 	which may be transformed to "g || h" on some platforms.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@115691 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/fold-cond-1.c
+++ gcc/testsuite/gcc.dg/fold-cond-1.c
@@ -24,5 +24,6 @@ _Bool test4(int g, int h)
 /* { dg-final { scan-tree-dump-times "a != 0 \&\& b != 0" 1 "original" } } */
 /* { dg-final { scan-tree-dump-times "c == 0 \\|\\| d != 0" 1 "original" } } */
 /* { dg-final { scan-tree-dump-times "e == 0 \&\& f != 0" 1 "original" } } */
-/* { dg-final { scan-tree-dump-times "\\(g \\| h\\) != 0" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "g == 0 \\? h != 0 : 1" 0 "original" } } */
+/* { dg-final { scan-tree-dump-times "g != 0 \\? 1 : h != 0" 0 "original" } } */
 /* { dg-final { cleanup-tree-dump "original" } } */
