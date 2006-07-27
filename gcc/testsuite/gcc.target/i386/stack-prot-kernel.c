fa6adcab9d8c2ff6be87e30fb663ede9a664b9ff
Arjan van de Ven <arjan@linux.intel.com>
2006-07-27 23:12:08 +0000

    2006-07-27  Arjan van de Ven <arjan@linux.intel.com>
    	* config/i386/i386.md (stack_tls_protect_set_di) Use %gs rather than
    	%fs for -mcmodel=kernel.
    	(stack_tls_protect_test_di): Likewise.
    
    	* testsuite/gcc.target/i386/stack-prot-kernel.c: New test.
    
    From-SVN: r115782

---
+++ gcc/testsuite/gcc.target/i386/stack-prot-kernel.c
@@ -0,0 +1,12 @@
+/* { dg-do compile { target lp64 } } */
+/* { dg-options "-O2 -fstack-protector-all -mcmodel=kernel" } */
+
+void test1 (int x)
+{
+  char p[40];
+  int i;
+  for (i=0; i<40; i++)
+	p[i] = x;
+}
+
+/* { dg-final { scan-assembler-not "%fs" } } */
