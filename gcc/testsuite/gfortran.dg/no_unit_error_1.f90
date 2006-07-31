8b956d3d381610258beb367331e476698459fecf
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2006-07-31 00:09:16 +0000

    re PR fortran/28335 (flush() / write() statement on closed units - error?)
    2006-07-30  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/28335
    	* gfortran.dg/no_unit_error_1.f90: Delete test.
    	* gfortran.dg/no_unit_error_2.f90: Delete test.
    
    From-SVN: r115828

--- gcc/testsuite/gfortran.dg/no_unit_error_1.f90
+++
@@ -1,7 +0,0 @@
-! { dg-do run }
-! { dg-shouldfail "UNIT is not open before CLOSE" }
-! PR28335 Check for error on no unit.
-  close(88) ! { dg-output "Can't find specified UNIT in CLOSE" }
-  end
-
-
