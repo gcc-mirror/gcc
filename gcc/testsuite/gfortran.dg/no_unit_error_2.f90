8c21c758524ea1939da6597242dd08f104096d1a
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-07-31 00:09:16 +0000

    2006-07-30  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR libgfortran/28335
    	* gfortran.dg/no_unit_error_1.f90: Delete test.
    	* gfortran.dg/no_unit_error_2.f90: Delete test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@115828 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/no_unit_error_2.f90
+++
@@ -1,7 +0,0 @@
-! { dg-do run }
-! { dg-shouldfail "UNIT is not open before FLUSH" }
-! PR28335 Check for error on no unit.
-  flush(88) ! { dg-output "Can't find specified UNIT in FLUSH" }
-  end
-
-
