302c179abca50016c86f0a3f163c4b205f9f2ea5
reichelt <reichelt@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-07-31 22:52:20 +0000

    PR c++/6634 	* decl.c (grokdeclarator): Check whether "long" or "short" was 	specified for non-integral types.
    	* g++.dg/parse/long1.C: Add more tests.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@115843 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/parse/long1.C
+++ gcc/testsuite/g++.dg/parse/long1.C
@@ -5,3 +5,7 @@
 long long double x; // { dg-error "long long" }
 long double y;
 long float z;       // { dg-error "long" }
+
+typedef short void    SV; // { dg-error "short" }
+typedef long struct A LA; // { dg-error "long" }
+typedef short char    SC; // { dg-error "short" }
