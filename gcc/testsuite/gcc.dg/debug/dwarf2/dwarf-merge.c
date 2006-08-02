4533b23c92ae6ca4224167b6260105f05e165530
drow <drow@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-08-02 13:31:56 +0000

    gcc/ 	PR debug/28063 	* dwarf2out.c (prune_unused_types_prune): Move call to 	prune_unused_types_update_strings to cover the parent DIE also. gcc/testuite/ 	PR debug/28063 	* gcc.dg/debug/dwarf2/dwarf-merge.c: New test. 	* lib/target-supports.exp (get_compiler_messages): Add WANT_OUTPUT. 	Optionally return assembly text.  Update callers. 	(check_no_compiler_messages): Update verbose messages. 	(check_no_messages_and_pattern): New. 	(check_effective_target_string_merging): New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@115874 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/debug/dwarf2/dwarf-merge.c
@@ -0,0 +1,10 @@
+/* Verify that mergeable strings are used in the CU DIE.  */
+/* { dg-do compile } */
+/* { dg-require-effective-target string_merging } */
+/* { dg-options "-O2 -gdwarf-2 -dA" } */
+/* { dg-final { scan-assembler "DW_AT_producer: \"GNU C" } } */
+/* { dg-final { scan-assembler-not "GNU C\[^\\n\\r\]*DW_AT_producer" } } */
+
+void func (void)
+{
+}
