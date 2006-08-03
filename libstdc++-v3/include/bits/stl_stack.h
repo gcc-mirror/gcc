7e5dc35e34d0ec5da16ff386799dbad9e94c1c94
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-08-03 21:11:13 +0000

    2006-08-03  Paolo Carlini  <pcarlini@suse.de>
    	* include/bits/stl_queue.h: Trivial formatting fixes.
    	* include/bits/stl_stack.h: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@115911 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/stl_stack.h
+++ libstdc++-v3/include/bits/stl_stack.h
@@ -1,6 +1,7 @@
 // Stack implementation -*- C++ -*-
 
-// Copyright (C) 2001, 2002, 2004, 2005 Free Software Foundation, Inc.
+// Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006
+// Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -73,11 +74,11 @@ _GLIBCXX_BEGIN_NAMESPACE(std)
 
   template<typename _Tp, typename _Seq>
     inline bool
-    operator==(const stack<_Tp,_Seq>& __x, const stack<_Tp,_Seq>& __y);
+    operator==(const stack<_Tp, _Seq>& __x, const stack<_Tp, _Seq>& __y);
 
   template<typename _Tp, typename _Seq>
     inline bool
-    operator<(const stack<_Tp,_Seq>& __x, const stack<_Tp,_Seq>& __y);
+    operator<(const stack<_Tp, _Seq>& __x, const stack<_Tp, _Seq>& __y);
 
   /**
    *  @brief  A standard container giving FILO behavior.
@@ -141,7 +142,7 @@ _GLIBCXX_BEGIN_NAMESPACE(std)
        */
       explicit
       stack(const _Sequence& __c = _Sequence())
-      : c(__c) {}
+      : c(__c) { }
 
       /**
        *  Returns true if the %stack is empty.
