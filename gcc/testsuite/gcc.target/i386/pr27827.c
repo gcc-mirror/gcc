bab1de0adbbf48be61d056c7fc62a719798b4781
Paolo Bonzini <bonzini@gnu.org>
2006-08-06 12:06:31 +0000

    re PR debug/26827 ("GNAT BUG DETECTED" on compile GPS 1.3.1/gtkada)
    2006-08-06  Paolo Bonzini  <bonzini@gnu.org>
    
    	PR target/26827
    	* config/i386/i386.md: Add peephole2 to avoid "fld %st"
    	instructions.
    
    2006-08-06  Paolo Bonzini  <bonzini@gnu.org>
    
    	PR target/26827
    	* gcc.target/i386/pr27827.c: New testcase.
    
    From-SVN: r115969

---
+++ gcc/testsuite/gcc.target/i386/pr27827.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+
+double a, b;
+double f(double c)
+{
+  double x = a * b;
+  return x + c * a;
+}
+
+/* { dg-final { scan-assembler-not "fld\[ \t\]*%st" } } */
+/* { dg-final { scan-assembler "fmul\[ \t\]*%st" } } */
