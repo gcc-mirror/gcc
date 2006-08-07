ef497588b589798798f06313f80a526d09806d09
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-08-07 08:13:22 +0000

    * g++.dg/other/unused1.C: Skip on Solaris 2.5.1 and 2.6 too.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@115993 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/other/unused1.C
+++ gcc/testsuite/g++.dg/other/unused1.C
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-g" } */
-/* { dg-skip-if "" { { hppa*-*-hpux* } && { ! hppa*64*-*-* } } { "*" } { "" } } */
+/* { dg-skip-if "" { { hppa*-*-hpux* *-*-solaris2.[56]* } && { ! hppa*64*-*-* } } { "*" } { "" } } */
 
 /* Make sure we didn't eliminate casted types because we thought they were
    unused.  */
