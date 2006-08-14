c5183e0facfcb63d5200e5aaa894cf95f500d201
Richard Guenther <rguenther@suse.de>
2006-08-14 15:05:37 +0000

    re PR testsuite/28703 (FAIL: gcc.c-torture/execute/pr28651.c execution)
    2006-08-14  Richard Guenther  <rguenther@suse.de>
     
    	PR testsuite/28703
    	* gcc.c-torture/execute/pr28651.c: Do not use argc
    	to avoid optimization, instead forbid inlining.
    
    From-SVN: r116129

--- gcc/testsuite/gcc.c-torture/execute/pr28651.c
+++ gcc/testsuite/gcc.c-torture/execute/pr28651.c
@@ -1,5 +1,5 @@
 extern void abort (void);
-int
+int __attribute__((noinline))
 foo (unsigned int u)
 {
   return (int)(u + 4) < (int)u;
@@ -8,14 +8,7 @@ foo (unsigned int u)
 int
 main (int argc, char *argv[])
 {
-  unsigned int u;
-
-  /* Run with no arguments so u will be MAX_INT and the optimizers
-     won't know its value.  */
-  if (argc > 1)
-    u = 1;
-  else
-    u = 0x7fffffff;
+  unsigned int u = 0x7fffffff;
 
   if (foo (u) == 0)
     abort();
