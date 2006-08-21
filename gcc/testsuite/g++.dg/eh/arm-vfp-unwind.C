41c5a4371c35f12b67c0dc5a7d2b807b93a9e6bd
shinwell <shinwell@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-08-21 10:10:58 +0000

    gcc/ 	* config/arm/pr-support.c (__gnu_unwind_execute): Insert " + 1" in 	necessary places to pass the correct "number of registers" values 	to _Unwind_VRS_Pop.
    	gcc/testsuite/
     	* g++.dg/eh/arm-vfp-unwind.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@116291 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/eh/arm-vfp-unwind.C
@@ -0,0 +1,40 @@
+/* { dg-require-effective-target arm32 } */
+/* { dg-do run } */
+
+/* Test to catch off-by-one errors in arm/pr-support.c.  */
+
+#if defined (__VFP_FP__) && !defined (__SOFTFP__)
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+static void donkey ()
+{
+  asm volatile ("fcpyd d9, %P0" : : "w" (1.2345) : "d9");
+  throw 1;
+}
+
+int main()
+{
+  try
+    {
+      donkey ();
+    }
+  catch (int foo)
+    {
+      return 0;
+    }
+  return 1;
+}
+
+#else
+
+int main()
+{
+  return 0;
+}
+
+#endif
+
