aa666e00632d6f6433ad549eb061e9c568708edd
Andrew Pinski <pinskia@gcc.gnu.org>
2006-08-25 00:13:48 -0700

    re PR tree-optimization/28807 (wrong code with may_alias and structs)
    2006-08-24  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR tree-opt/28807
            * tree-ssa-operands.c (access_can_touch_variable): Don't say
            the access through a base which has an alias set of 0 cannot
            touch the variable.
    
    2006-08-24  Andrew Pinski  <pinskia@physics.uc.edu>
    
            PR tree-opt/28807
            * gcc.c-torture/execute/mayalias-2.c: New test.
            * gcc.dg/tree-ssa/alias-13.c: New test.
    
    From-SVN: r116393

---
+++ gcc/testsuite/gcc.dg/tree-ssa/alias-13.c
@@ -0,0 +1,33 @@
+/* { dg-do link } */
+/* { dg-options "-O2" } */
+
+
+struct a
+{
+  char a1;
+};
+
+int *aa;
+
+void g(int *a)
+{
+  aa = a;
+  *a = 2;
+}
+
+int t(int i, struct a *b)
+{
+  g(&i);
+  b->a1 = 1;
+  i = 2;
+  if (b->a1 != 1)
+    link_failure ();
+}
+int main(void)
+{
+  struct a b;
+  t(1, &b);
+  return 0;
+}
+
+
