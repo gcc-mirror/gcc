a73dee2badc46ba434883b42e130659e2001041a
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-08-25 22:01:28 +0000

    2006-08-26  Richard Guenther  <rguenther@suse.de>
    	PR middle-end/28814
    	* fold-const.c (fold_binary): Fold temporary to correct
    	type before constructing new comparison.
    
    	* gcc.dg/torture/pr28814.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@116439 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/pr28814.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */
+
+struct w49
+{
+  union
+  {
+  }
+  value;
+};
+f9887 (struct w49 a23040)
+{
+  unsigned long r9887;
+  if (((struct structure_type24753 *) (r9887 - 1)) == ((void *) 0))
+    {
+      backtrace ("stalin.sc", 7222, 248274);
+    }
+}
