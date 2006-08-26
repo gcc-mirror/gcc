1f2d863f329824fff66fa67794915087ee7b3a4a
Joseph Myers <joseph@codesourcery.com>
2006-08-26 01:22:23 +0100

    re PR c/28299 (ICE with -Wmissing-prototypes)
    	PR c/28299
    	* c-decl.c (start_function): Don't try to process prototype
    	information from old declaration that isn't a function.
    
    testsuite:
    	* gcc.dg/funcdef-var-1.c, gcc.dg/funcdef-var-2.c: New tests.
    	* gcc.dg/pr20368-1.c, gcc.dg/pr20368-2.c, gcc.dg/pr20368-3.c:
    	Expect extra diagnostics.
    
    From-SVN: r116459

---
+++ gcc/testsuite/gcc.dg/funcdef-var-2.c
@@ -0,0 +1,11 @@
+/* Test ICE on defining function with a name previously declared as a
+   nonfunction.  Bug 28299 from Bernhard Fischer <aldot@gcc.gnu.org>.
+   Reduced testcase from Volker Reichelt <reichelt@gcc.gnu.org>.  */
+
+/* { dg-do compile } */
+/* { dg-options "-Wmissing-prototypes" } */
+
+int foo;
+/* { dg-error "previous declaration" "previous declaration" { target *-*-* } 8 } */
+void foo () {} /* { dg-error "redeclared as different kind of symbol" } */
+/* { dg-warning "no previous prototype" "no previous prototype" { target *-*-* } 10 } */
