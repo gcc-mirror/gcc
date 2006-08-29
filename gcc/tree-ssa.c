4f652d47e74bba4e82d0e165b502beb38861eedc
amylaar <amylaar@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-08-29 15:52:54 +0000

    gcc:
    2006-08-29  Nathan Sidwell  <nathan@codesourcery.com>
    	    J"orn Rennecke  <joern.rennecke@st.com>
    
    	PR tree-optimization/17506
    	* tree-ssa.c (warn_uninit): If warning about a location outside of
    	the current function, note where the variable was declared.
    
    testsuite:
    
    2006-08-29  Volker Reichelt  <reichelt@igpm.rwth-aachen.de>
    	    Kazu Hirata  <kazu@codesourcery.com>
    
    	PR tree-optimization/17506
    	* gcc.dg/pr17506.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@116564 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa.c
+++ gcc/tree-ssa.c
@@ -1154,7 +1154,7 @@ warn_uninit (tree t, const char *gmsgid, void *data)
   tree var = SSA_NAME_VAR (t);
   tree def = SSA_NAME_DEF_STMT (t);
   tree context = (tree) data;
-  location_t * locus;
+  location_t *locus, *fun_locus;
 
   /* Default uses (indicated by an empty definition statement),
      are uninitialized.  */
@@ -1178,6 +1178,12 @@ warn_uninit (tree t, const char *gmsgid, void *data)
 	   ? EXPR_LOCUS (context)
 	   : &DECL_SOURCE_LOCATION (var));
   warning (0, gmsgid, locus, var);
+  fun_locus = &DECL_SOURCE_LOCATION (cfun->decl);
+  if (locus->file != fun_locus->file
+      || locus->line < fun_locus->line
+      || locus->line > cfun->function_end_locus.line)
+    inform ("%J%qD was declared here", var, var);
+
   TREE_NO_WARNING (var) = 1;
 }
    
