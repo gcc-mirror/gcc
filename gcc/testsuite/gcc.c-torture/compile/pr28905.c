fd2730c0a6adf8791147dd9718d653ab8ec7c044
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-09-05 08:36:39 +0000

    2006-09-05  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/28905
    	* tree-vrp.c (fix_equivalence_set): Manually implement
    	!value_ranges_intersect_p to also handle symbolic ranges.
    
    	* gcc.c-torture/compile/pr28905.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@116696 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr28905.c
@@ -0,0 +1,11 @@
+/* We used to ICE here because after VRP we ended up with
+   non-compatible ranges in a value-range equivalences set.  */
+void code_comment (int size)
+{
+  int i;
+  for (i = 0; i < size; i++)
+    if (i)
+      if (i < 0)
+        if (i < 0)
+          return;
+}
