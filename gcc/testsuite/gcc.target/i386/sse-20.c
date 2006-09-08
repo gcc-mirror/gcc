d0655f33aa551fddb74b0d8c591b0f58468b8527
Jason Merrill <jason@redhat.com>
2006-09-07 20:28:30 -0400

    re PR target/13685 (Building simple test application with -march=pentium3 -Os gives SIGSEGV (unaligned sse instruction))
            PR target/13685
            * config/i386/i386.c (override_options): Use 128-bit
            stack boundary if -msse.
    
    From-SVN: r116775

---
+++ gcc/testsuite/gcc.target/i386/sse-20.c
@@ -0,0 +1,26 @@
+/* PR target/13685 */
+/* { dg-options "-Os -msse" } */
+
+typedef float __m128 __attribute__ ((vector_size (16)));
+typedef int __m64 __attribute__ ((vector_size (8)));
+
+int puts (const char *s);
+void foo (__m128 *, __m64 *, int);
+
+int main (void)
+{
+  foo (0, 0, 0);
+  return 0;
+}
+
+void foo (__m128 *dst, __m64 *src, int n)
+{
+  __m128 xmm0 = { 0 };
+  while (n > 64)
+    {
+      puts ("");
+      xmm0 = __builtin_ia32_cvtpi2ps (xmm0, *src);
+      *dst = xmm0;
+      n --;
+    }
+}
