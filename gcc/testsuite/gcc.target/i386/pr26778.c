8ee7afab92dccaae1249e41cf0a4b00b61bbd395
bonzini <bonzini@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-09-09 12:43:32 +0000

    gcc: 2006-09-09  Paolo Bonzini  <bonzini@gnu.org> 	    Dale Johannesen  <dalej@apple.com>
    	PR target/26778
    	* regclass.c (struct reg_pref): Update documentation.
    	(regclass): Set prefclass to NO_REGS if memory is the best option.
    	(record_reg_classes): Cope with a prefclass set to NO_REGS.
    
    gcc/testsuite:
    2006-09-09  Eric Christopher  <echristo@apple.com>
    
    	PR target/26778
    	* gcc.target/i386/pr26778.c: New testcase.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@116798 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr26778.c
@@ -0,0 +1,20 @@
+/* { dg-do compile { target ilp32 } } */
+/* { dg-options "-O2 -march=pentium3" } */
+
+typedef union {
+  long long l;
+  double d;
+} db_number;
+
+double test(double x[3]) {
+  double th = x[1] + x[2];
+  if (x[2] != th - x[1]) {
+    db_number thdb;
+    thdb.d = th;
+    thdb.l++;
+    th = thdb.d;
+  }
+  return x[0] + th;
+}
+
+/* { dg-final { scan-assembler-not "mov.ps" } } */
