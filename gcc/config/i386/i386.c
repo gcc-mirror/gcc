44e13f85b36735a3411df4b277cac0851b50c085
H.J. Lu <hongjiu.lu@intel.com>
2006-09-12 02:54:42 +0000

    re PR target/13685 (Building simple test application with -march=pentium3 -Os gives SIGSEGV (unaligned sse instruction))
    gcc/
    
    2006-09-11  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/13685
    	PR target/27537
    	PR target/28621
    	* config/i386/i386.c (override_options): Always default to 16
    	byte stack boundary.
    
    gcc/testsuite/
    
    2006-09-11  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/13685
    	* gcc.target/i386/pr13685.c: New test.
    
    From-SVN: r116870

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -1502,12 +1502,10 @@ override_options (void)
     }
 
   /* Validate -mpreferred-stack-boundary= value, or provide default.
-     The default of 128 bits is for Pentium III's SSE __m128, but we
-     don't want additional code to keep the stack aligned when
-     optimizing for code size.  */
-  ix86_preferred_stack_boundary = (optimize_size
-				   ? TARGET_64BIT ? 128 : 32
-				   : 128);
+     The default of 128 bits is for Pentium III's SSE __m128, We can't
+     change it because of optimize_size.  Otherwise, we can't mix object
+     files compiled with -Os and -On.  */
+  ix86_preferred_stack_boundary = 128;
   if (ix86_preferred_stack_boundary_string)
     {
       i = atoi (ix86_preferred_stack_boundary_string);
