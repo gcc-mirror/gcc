90f30d6236d4e5cd6d1f08c064a43778d8927d7f
Jack Howarth <howarth@bromo.med.uc.edu>
2006-09-12 00:42:27 +0000

    re PR testsuite/28950 (regex wrong for testing on darwin in gcc/testsuite/gcc.target/powerpc/ppc-and-1.c)
    2006-09-11  Jack Howarth  <howarth@bromo.med.uc.edu>
    
            PR testsuite/28950
            * gcc.target/powerpc/ppc-and-1.c: Fix regex.
    
    From-SVN: r116868

--- gcc/testsuite/gcc.target/powerpc/ppc-and-1.c
+++ gcc/testsuite/gcc.target/powerpc/ppc-and-1.c
@@ -2,8 +2,8 @@
 /* { dg-require-effective-target lp64 } */
 /* { dg-options "-O2" } */
 
-/* { dg-final { scan-assembler "rlwinm \[0-9\]+,\[0-9\]+,0,0,30"  } } */
-/* { dg-final { scan-assembler "rlwinm \[0-9\]+,\[0-9\]+,0,29,30"  } } */
+/* { dg-final { scan-assembler "rlwinm r?\[0-9\]+,r?\[0-9\]+,0,0,30"  } } */
+/* { dg-final { scan-assembler "rlwinm r?\[0-9\]+,r?\[0-9\]+,0,29,30"  } } */
 /* { dg-final { scan-assembler-not "rldicr" } } */
 
 /* Origin:Pete Steinmetz <steinmtz@us.ibm.com> */
