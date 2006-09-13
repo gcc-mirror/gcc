92028edb8d31a2d62bc12cab608bf438beaa4e4d
Eric Botcazou <ebotcazou@libertysurf.fr>
2006-09-13 23:24:04 +0200

    re PR other/23541 (All error messages produce segfault)
    	PR other/23541
    	PR other/26507
    	Backport from gettext repository:
    
    	2003-09-04  Bruno Haible  <bruno@clisp.org>
    	* dgettext.c: Include <locale.h> after gettextP.h, not before. This
    	ensures that libintl_dcgettext is correctly declared on Solaris.
    	(Needed because Solaris <locale.h> includes libintl.h.)
    	* dngettext.c: Likewise, for the libintl_dcngettext declaration.
    
    From-SVN: r116933

--- intl/dngettext.c
+++ intl/dngettext.c
@@ -1,5 +1,5 @@
 /* Implementation of the dngettext(3) function.
-   Copyright (C) 1995-1997, 2000, 2001, 2002 Free Software Foundation, Inc.
+   Copyright (C) 1995-1997, 2000-2003 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify it
    under the terms of the GNU Library General Public License as published
@@ -20,9 +20,10 @@
 # include <config.h>
 #endif
 
+#include "gettextP.h"
+
 #include <locale.h>
 
-#include "gettextP.h"
 #ifdef _LIBC
 # include <libintl.h>
 #else
