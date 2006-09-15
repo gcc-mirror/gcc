61943a216576f8e1f7de347d2ef56883e2b746d8
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2006-09-15 13:32:12 +0000

    re PR fortran/29053 (Consecutive STREAM I/O file positions mixed up)
    2006-09-15  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/29053
    	* gfortran.dg/streamio_9.f90: New test.
    	* gfortran.dg/streamio_10.f90: New test.
    
    From-SVN: r116971

---
+++ gcc/testsuite/gfortran.dg/streamio_9.f90
@@ -0,0 +1,31 @@
+! { dg-do run }
+! PR29053 Stream IO test 9.
+! Contributed by Jerry DeLisle <jvdelisle@gcc.gnu.org>.
+! Test case derived from that given in PR by Steve Kargl.
+program pr29053
+   implicit none
+   real dt, t, u, a(10), b(10)
+   integer i, place
+   dt = 1.e-6
+   a = real( (/ (i, i=1, 10) /) )
+   b = a
+   open(unit=11, file='a.dat', access='stream')
+   open(unit=12, file='b.dat', access='stream')
+   do i = 1, 10
+      t = i * dt
+      write(11) t
+      write(12) a
+   end do
+   rewind(11)
+   rewind(12)
+   do i = 1, 10
+      t = i * dt
+      read(12) a
+      if (any(a.ne.b)) call abort()
+      read(11) u
+      if (u.ne.t) call abort()
+   end do
+   close(11, status="delete")
+   close(12, status="delete")
+end program pr29053
+
