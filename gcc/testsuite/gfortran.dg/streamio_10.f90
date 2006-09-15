c4c12403f9be24d9e21a8503f6e6a4c9f1735766
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-09-15 13:32:12 +0000

    2006-09-15  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR libgfortran/29053
    	* gfortran.dg/streamio_9.f90: New test.
    	* gfortran.dg/streamio_10.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@116971 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/streamio_10.f90
@@ -0,0 +1,37 @@
+! { dg-do run }
+! PR25093 Stream IO test 10
+! Contributed by Jerry DeLisle <jvdelisle@gcc.gnu.org>.
+! Test case derived from that given in PR by Steve Kargl.
+program stream_io_10
+  implicit none
+  integer :: a(4), b(4)
+  integer(kind=8) :: thepos
+  a = (/ 1, 2, 3, 4 /)
+  b = a
+  open(10, file="teststream", access="stream")
+  write(10) a
+  inquire(10, pos=thepos)
+  if (thepos.ne.17) call abort()
+
+  read(10, pos=1)
+  inquire(10, pos=thepos)
+  if (thepos.ne.1) call abort()
+
+  write(10, pos=15)
+  inquire(10, pos=thepos)
+  if (thepos.ne.15) call abort()
+
+  read(10, pos=3)
+  inquire(10, pos=thepos)
+  if (thepos.ne.3) call abort()
+
+  write(10, pos=1)
+  inquire(10, pos=thepos)
+  if (thepos.ne.1) call abort()
+
+  a = 0
+  read(10) a
+  if (any(a /= b)) call abort()
+
+  close(10, status="delete")
+end program stream_io_10
