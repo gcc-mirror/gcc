41d9adc7fac0487106d15207d2707fa5b53c5bbd
Eric Botcazou <ebotcazou@adacore.com>
2006-09-15 18:32:24 +0000

    re PR ada/15802 (ICE at expr.c:6764 (placeholder mechanism))
    	PR ada/15802
    	* decl.c (same_discriminant_p): New static function.
    	(gnat_to_gnu_entity) <E_Record_Type>: When there is a parent
    	subtype and we have discriminants, fix up the COMPONENT_REFs
    	for the discriminants to make them reference the corresponding
    	fields of the parent subtype after it has been built.
    
    From-SVN: r116981

---
+++ gcc/testsuite/gnat.dg/specs/double_record_extension1.ads
@@ -0,0 +1,11 @@
+package double_record_extension1 is
+
+   type T1(n: natural) is tagged record
+      s1: string (1..n);
+   end record;
+   type T2(j,k: natural) is new T1(j) with record
+      s2: string (1..k);
+   end record;
+   type T3 is new T2 (10, 10) with null record;
+
+end double_record_extension1;
---
+++ gcc/testsuite/gnat.dg/specs/double_record_extension2.ads
@@ -0,0 +1,15 @@
+package double_record_extension2 is
+
+  type Base_Message_Type (Num_Bytes : Positive) is tagged record
+     Data_Block : String (1..Num_Bytes);
+  end record;
+
+  type Extended_Message_Type (Num_Bytes1 : Positive; Num_Bytes2 : Positive) is new Base_Message_Type (Num_Bytes1) with record
+     A: String (1..Num_Bytes2);
+  end record;
+
+  type Final_Message_Type is new Extended_Message_Type with record
+     B : Integer;
+  end record;
+
+end double_record_extension2;
