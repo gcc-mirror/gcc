3c40b0525ea4b4fd83041c2b9d0ef78801a3dabe
Jack Howarth <howarth@bromo.med.uc.edu>
2006-09-16 08:55:54 +0000

    re PR testsuite/29055 (gcc.target/powerpc/darwin-bool-1.c fails on powerpc-apple-darwin8 at -m64)
    2006-09-16  Jack Howarth  <howarth@bromo.med.uc.edu>
    
            PR testsuite/29055
            * gcc.target/powerpc/darwin-bool-1.c: Skip -m64
    
    From-SVN: r116993

--- gcc/testsuite/gcc.target/powerpc/darwin-bool-1.c
+++ gcc/testsuite/gcc.target/powerpc/darwin-bool-1.c
@@ -1,6 +1,7 @@
 /* Check that sizeof(bool) is 4 if we don't use special options. */
 /* Matt Austern  <austern@apple.com> */
 /* { dg-do run { target powerpc*-*-darwin* } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "-m64" } { "" } } */
 
 int dummy1[sizeof(_Bool) - 3];
 int dummy2[5 - sizeof(_Bool)];
