a04d76ec03da25b2d6808e3838f0c0079c3109fe
Uros Bizjak <uros@kss-loka.si>
2006-09-18 12:14:53 +0200

    re PR target/28946 (assembler shifts set the flag ZF, no need to re-test to zero)
    	PR target/28946
    	* config/i386/i386.md ("*ashldi3_cconly_rex64", "*ashlsi3_cconly",
    	"*ashlhi3_cconly", "*ashlqi3_cconly", "*ashrdi3_one_bit_cconly_rex64",
    	"*ashrdi3_cconly_rex64", "*ashrsi3_one_bit_cconly", "*ashrsi3_cconly",
    	"*ashrhi3_one_bit_cconly", "*ashrhi3_cconly",
    	"*ashrqi3_one_bit_cconly", "*ashrqi3_cconly",
    	"*lshrdi3_cconly_one_bit_rex64", "*lshrdi3_cconly_rex64",
    	"*lshrsi3_one_bit_cconly", "*lshrsi3_cconly",
    	"*lshrhi3_one_bit_cconly", "*lshrhi3_cconly",
    	"*lshrqi2_one_bit_cconly", "*lshrqi2_cconly": New patterns to
    	implement only CC setting effects of shift instructions.
    
    testsuite/ChangeLog:
    
    	PR target/28946
    	* gcc.target/i386/pr28946.c: New test.
    
    From-SVN: r117022

---
+++ gcc/testsuite/gcc.target/i386/pr28946.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-options "-Os" } */
+/* { dg-final { scan-assembler-not "test" } } */
+
+int fct1 (void);
+int fct2 (void);
+
+int
+fct (unsigned nb)
+{
+  if ((nb >> 5) != 0)
+    return fct1 ();
+  else
+    return fct2 ();
+}
