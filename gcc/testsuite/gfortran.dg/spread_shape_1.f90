d974cfdeaa13bf40b4d118ffcde15f760d961f44
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-09-18 06:24:54 +0000

    2006-09-18  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/29060
    	* iresolve.c (resolve_spread): Build shape for result if the
    	source shape is available and dim and ncopies are constants.
    
    	PR fortran/28817
    	PR fortran/21918
    	* trans-decl.c (generate_local_decl): Change from 'warning' to
    	'gfc_warning' to have line numbers correctly reported.
    
    2006-09-18  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/29060
    	* gfortran.dg/spread_shape_1.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@117014 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/spread_shape_1.f90
@@ -0,0 +1,20 @@
+! { dg-do compile }
+! Tests the fix for PR29060 in which the shape of the result
+! of SPREAD was not available to the scalarizer.
+!
+! Contributed by Paul Thomas  <pault@gcc.gnu.org>
+  real,dimension(:, :),pointer :: ptr
+  real,dimension(2, 2),parameter :: u = &
+	reshape((/0.25, 0.5, 0.75, 1.00/),(/2,2/))
+  allocate (ptr(2,2))
+
+! Original PR
+  ptr(:, :) = u + spread ((/1.0, 2.0/), 2, size(u, 2))
+  if (any (ptr .ne. &
+        reshape ((/1.25, 2.50, 1.75, 3.00/), (/2, 2/)))) call abort ()
+
+! Check that the fix works correctly with the source shape after ncopies
+  ptr(:, :) = u + spread ((/2.0, 3.0/), 1, size (u, 1))
+  if (any (ptr .ne. &
+        reshape ((/2.25, 2.50, 3.75, 4.00/), (/2,2/)))) call abort ()
+end
