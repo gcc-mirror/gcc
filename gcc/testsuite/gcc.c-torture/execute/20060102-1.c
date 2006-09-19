33bc61c87f48f7b5cf8853431e0fcfd5d24ad3cd
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-09-19 17:44:12 +0000

    gcc/testsuite/ 	* gcc.c-torture/execute/pr28289.c (one): New variable. 	(main): Use it instead of argc. 	* gcc.c-torture/execute/20060102-1.c (one): New variable. 	(main): Use it instead of argc.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@117060 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/20060102-1.c
+++ gcc/testsuite/gcc.c-torture/execute/20060102-1.c
@@ -5,11 +5,12 @@ int f(int x)
   return (x >> 31) ? -1 : 1;
 }
 
-int main (int argc)
+volatile int one = 1;
+int main (void)
 {
   /* Test that the function above returns different values for
      different signs.  */
-  if (f(argc) == f(-argc))
+  if (f(one) == f(-one))
     abort ();
   return 0;
 }
