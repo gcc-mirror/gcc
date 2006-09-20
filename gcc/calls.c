dee3c9ddae3ad52ea17f2624c95159bc1728d3bc
jconner <jconner@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-09-20 18:57:46 +0000

    2006-09-20  Josh Conner  <jconner@apple.com>
    	PR middle-end/25505
    	* calls.c (expand_call): Allow reuse of structure return stack
    	temp.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@117091 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/calls.c
+++ gcc/calls.c
@@ -1985,7 +1985,7 @@ expand_call (tree exp, rtx target, int ignore)
 	    /* For variable-sized objects, we must be called with a target
 	       specified.  If we were to allocate space on the stack here,
 	       we would have no way of knowing when to free it.  */
-	    rtx d = assign_temp (TREE_TYPE (exp), 1, 1, 1);
+	    rtx d = assign_temp (TREE_TYPE (exp), 0, 1, 1);
 
 	    mark_temp_addr_taken (d);
 	    structure_value_addr = XEXP (d, 0);
