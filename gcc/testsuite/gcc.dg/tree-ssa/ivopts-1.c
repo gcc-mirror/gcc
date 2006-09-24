af17634547724868eb034351d63f2e29cce4d7b3
Zdenek Dvorak <dvorakz@suse.cz>
2006-09-24 21:11:16 +0200

    ivopts-1.c: New test.
    	* gcc.dg/tree-ssa/ivopts-1.c: New test.
    	* gcc.dg/tree-ssa/ivopts-2.c: New test.
    
    Co-Authored-By: Adam Nemet <anemet@caviumnetworks.com>
    
    From-SVN: r117183

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ivopts-1.c
@@ -0,0 +1,26 @@
+/* { dg-do compile } */
+
+/* Not all platforms support TImode integers.  */
+#if defined(__LP64__) && !defined(__hppa__)
+typedef int TItype __attribute__ ((mode (TI)));
+#else
+typedef long TItype;
+#endif
+
+TItype last_data_offset ;
+int store;
+char *data;
+
+f ()
+{
+
+  TItype data_offset = last_data_offset;
+  char *p;
+
+  for (p = data; *p; p++)
+    {
+      data_offset++;
+      g (data_offset);
+      store = data_offset + 1;
+    }
+}
---
+++ gcc/testsuite/gcc.dg/tree-ssa/ivopts-2.c
@@ -0,0 +1,24 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-ivopts-details" } */
+
+long last_data_offset;
+int store;
+char *data;
+  
+f ()
+{
+  
+  long data_offset = last_data_offset;
+  char *p;
+  
+  for (p = data; *p; p++)
+    {
+      data_offset++;
+      g (data_offset);
+      store = data_offset + 1;
+    }
+}
+
+/* We should only have two IVs.  */
+/* { dg-final { scan-tree-dump-not "\\n  candidates .*,.*,.*\\n\[^\\n\]*\\nFinal cost" "ivopts" } }  */
+/* { dg-final { cleanup-tree-dump "ivopts" } }  */
