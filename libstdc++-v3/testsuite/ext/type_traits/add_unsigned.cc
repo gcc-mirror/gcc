2bf8195036dccfae8347c92c759e9294ff3f83ad
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-09-29 13:38:58 +0000

    2006-09-29  Benjamin Kosnik  <bkoz@redhat.com>             Howard Hinnant  <hhinnant@apple.com>             Paolo Carlini  <pcarlini@suse.de>
    	* include/ext/type_traits.h (__remove_unsigned): Fix up for signed
    	char, bool, wchar_t, and floating point types.
    	(__add_unsigned): Same.	
    	* testsuite/ext/type_traits: New.
    	* testsuite/ext/type_traits.cc: Move...
    	* testsuite/ext/type_traits/numeric_traits.cc: ...here.	
    	* testsuite/ext/type_traits/add_unsigned_floating_neg.cc: New.
    	* testsuite/ext/type_traits/add_unsigned_integer_neg.cc: New.
    	* testsuite/ext/type_traits/remove_unsigned_floating_neg.cc: New.
    	* testsuite/ext/type_traits/remove_unsigned_integer_neg.cc: New.
    	* testsuite/ext/type_traits/add_unsigned.cc: New.
    	* testsuite/ext/type_traits/remove_unsigned.cc: New.
    	
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@117303 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/ext/type_traits/add_unsigned.cc
@@ -0,0 +1,49 @@
+// -*- C++ -*-
+
+// Copyright (C) 2006 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#include <ext/type_traits.h>
+#include <tr1/type_traits>
+#include <testsuite_hooks.h>
+
+template<typename T>
+  void
+  check_add_unsigned()
+  {
+    bool test __attribute__((unused)) = true;
+    typedef typename __gnu_cxx::__add_unsigned<T>::__type unsigned_type;
+    VERIFY( std::tr1::is_unsigned<unsigned_type>::value );
+  }
+
+int main()
+{
+  check_add_unsigned<char>();
+  check_add_unsigned<unsigned char>();
+  check_add_unsigned<signed char>();
+  check_add_unsigned<int>();
+  check_add_unsigned<unsigned int>();
+  check_add_unsigned<signed int>();
+  check_add_unsigned<long>();
+  check_add_unsigned<unsigned long>();
+  check_add_unsigned<signed long>();
+  check_add_unsigned<long long>();
+  check_add_unsigned<unsigned long long>();
+  check_add_unsigned<signed long long>();
+  return 0;
+}
