b7bf91917adec5526a5ffc2328a6402494d9e8ee
Steven G. Kargl <kargl@gcc.gnu.org>
2006-10-03 19:18:09 +0000

    nearest_1.f90: Add -O0 because -ffloat-store is insufficient to inhibit some test failures at -O3.
    2006-10-03  Steven G. Kargl  <kargl@gcc.gnu.org>
    
            * gfortran.dg/nearest_1.f90: Add -O0 because -ffloat-store is
            insufficient to inhibit some test failures at -O3.  Do testing
            on integers.
    
    From-SVN: r117412

--- gcc/testsuite/gfortran.dg/nearest_1.f90
+++ gcc/testsuite/gfortran.dg/nearest_1.f90
@@ -1,15 +1,18 @@
 ! { dg-do run }
-! { dg-options "-ffloat-store" }
+! { dg-options "-O0 -ffloat-store" }
 ! PR fortran/27021
 ! Original code submitted by Dominique d'Humieres
 ! Converted to Dejagnu for the testsuite by Steven G. Kargl
 program chop
+  integer ix, iy
   real o, t, td, tu, x, y
   o = 1.
   t = tiny(o)
   td = nearest(t,-1.0)
   x = td/2.0
   y = nearest(tiny(o),-1.0)/2.0
-  if (abs(x - y) /= 0.) call abort
+  ix = transfer(x,ix)
+  iy = transfer(y,iy)
+  if (ix /= iy) call abort
 end program chop
 
