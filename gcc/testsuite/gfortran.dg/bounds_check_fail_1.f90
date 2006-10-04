1047bab7432374044b6da3b47ebb310bc7f0d1a1
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-10-04 15:44:32 +0000

    * gfortran.dg/bounds_check_fail_1.f90: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@117431 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/bounds_check_fail_1.f90
@@ -0,0 +1,7 @@
+! { dg-do run }
+! { dg-options "-fbounds-check" }
+! { dg-shouldfail "foo" }
+  integer x(1)
+  x(2) = x(1) ! { dg-warning "out of bounds" }
+  end
+! { dg-output "out of bounds for array 'x', upper bound of dimension 1 exceeded.*at line 5" }
