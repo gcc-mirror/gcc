59a4f683da0813869d01e2a0a5f64c5c9b674149
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-10-04 07:57:34 +0000

    2006-10-04  Benjamin Kosnik  <bkoz@redhat.com> 	    Mike Stump  <mrs@apple.com>
    	* testsuite/27_io/ios_base/storage/2.cc: Fail on Darwin 8.[0-4].* only.
    	* testsuite/23_containers/vector/resize/1.cc: Same.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@117428 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/27_io/ios_base/storage/2.cc
+++ libstdc++-v3/testsuite/27_io/ios_base/storage/2.cc
@@ -25,9 +25,10 @@
 // XXX -lmalloc seems to work.
 // See http://gcc.gnu.org/ml/gcc/2002-05/msg01012.html
 // { dg-options "-lmalloc" { target mips*-*-irix6* } }
-// This fails on Darwin 8 because malloc doesn't return NULL even
-// if an allocation fails (filed as Radar 3884894).
-// { dg-do run { xfail *-*-darwin8* } }
+
+// This fails on some versions of Darwin 8 because malloc doesn't return
+// NULL even if an allocation fails (filed as Radar 3884894).
+// { dg-do run { xfail *-*-darwin8.[0-4].* } }
 
 #include <sstream>
 #include <iostream>
