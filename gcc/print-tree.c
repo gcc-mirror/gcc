85a76dc8e8f28a4d60cdd4890567a80948ee45ae
amylaar <amylaar@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-10-06 15:59:47 +0000

    * print-tree.c (print_node_brief, print_node): Print sign of Inf.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@117506 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/print-tree.c
+++ gcc/print-tree.c
@@ -138,7 +138,7 @@ print_node_brief (FILE *file, const char *prefix, tree node, int indent)
 
       d = TREE_REAL_CST (node);
       if (REAL_VALUE_ISINF (d))
-	fprintf (file, " Inf");
+	fprintf (file,  REAL_VALUE_NEGATIVE (d) ? " -Inf" : " Inf");
       else if (REAL_VALUE_ISNAN (d))
 	fprintf (file, " Nan");
       else
@@ -709,7 +709,7 @@ print_node (FILE *file, const char *prefix, tree node, int indent)
 
 	    d = TREE_REAL_CST (node);
 	    if (REAL_VALUE_ISINF (d))
-	      fprintf (file, " Inf");
+	      fprintf (file,  REAL_VALUE_NEGATIVE (d) ? " -Inf" : " Inf");
 	    else if (REAL_VALUE_ISNAN (d))
 	      fprintf (file, " Nan");
 	    else
