3ac571201309b925c13eebe0a140b20f93dbf4b1
Jakub Jelinek <jakub@redhat.com>
2006-10-06 18:57:27 +0200

    re PR tree-optimization/29330 (-O -ftree-loop-linear   -->  virtual memory exhausted)
    	PR tree-optimization/29330
    	* tree-data-ref.c (free_data_ref): Use DR_FREE_ACCESS_FNS macro.
    	(initialize_data_dependence_relation): Clear DDR_LOOP_NEST pointer
    	on newly allocated ddrs.
    	(find_loop_nest_1, find_loop_nest): Change LOOP_NEST to a pointer
    	to VEC (loop_p, heap) pointer.
    	(compute_data_dependences_for_loop): Adjust caller.
    	(free_dependence_relations): Free DDR_LOOP_NEST.
    
    	* tree-loop-linear.c (linear_transform_loops): Don't forget to
    	free DEPENDENCE_RELATIONS and DATAREFS.
    
    	* gcc.dg/pr29330.c: New test.
    
    From-SVN: r117509

---
+++ gcc/testsuite/gcc.dg/pr29330.c
@@ -0,0 +1,15 @@
+/* PR tree-optimization/29330 */
+/* { dg-do compile } */
+/* { dg-options "-O -ftree-loop-linear" } */
+
+int buf[2][2][2][2];
+
+void
+f (void)
+{
+  for (int a = 0; a < 2; ++a)
+    for (int b = 0; b < 2; ++b)
+      for (int c = 0; c < 2; ++c)
+	for (int d = 0; d < 2; ++d)
+	  buf[a][b][c][d] = 0;
+}
