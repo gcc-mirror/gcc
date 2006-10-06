2521f35f9132bb59f321a72ac3096f3c4091236d
mkuvyrkov <mkuvyrkov@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-10-06 21:45:13 +0000

    2006-10-06  Maxim Kuvyrkov  <mkuvyrkov@ispras.ru>
    	PR rtl-optimization/29128
    	* sched-int.h (IS_SPECULATION_BRANCHY_CHECK_P): New macro.
    	* sched-ebb.c (advance_target_bb): Use it to fix condition to
    	allow interblock movement of speculation checks.
    
    	* gcc.c-torture/compile/pr29128.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@117515 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr29128.c
@@ -0,0 +1,28 @@
+typedef unsigned long Eterm;
+process_main (void)
+{
+  register Eterm x0;
+  register Eterm *reg = ((void *) 0);
+  register Eterm *I = ((void *) 0);
+  static void *opcodes[] = {
+      &&lb_allocate_heap_zero_III,
+      &&lb_allocate_init_tIy, &&lb_allocate_zero_tt
+  };
+lb_allocate_heap_III:{
+    Eterm *next;
+    goto *(next);
+  }
+lb_allocate_heap_zero_III:{
+  }
+lb_allocate_init_tIy:{
+  }
+lb_allocate_zero_tt:{
+    Eterm *next;
+    {
+      Eterm *tmp_ptr = ((Eterm *) (((x0)) - 0x1));
+      (*(Eterm *) (((unsigned char *) reg) + (I[(0) + 1]))) = ((tmp_ptr)[0]);
+      x0 = ((tmp_ptr)[1]);
+    }
+    goto *(next);
+  }
+}
