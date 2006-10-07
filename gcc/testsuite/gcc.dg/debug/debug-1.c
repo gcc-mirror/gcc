3154b25027bd8af483eadd8558d05721a276cb69
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-10-07 09:22:20 +0000

    gcc/testsuite/ 	* gcc.dg/debug/debug-1.c: Use -fno-if-conversion MIPS targets. 	* gcc.dg/debug/debug-2.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@117530 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/debug/debug-1.c
+++ gcc/testsuite/gcc.dg/debug/debug-1.c
@@ -1,6 +1,24 @@
 /* Verify that the scheduler does not discard the lexical block.  */
 /* { dg-do compile } */
 /* { dg-options "-dA" } */
+/* On MIPS targets that support conditional moves, the optimal
+   implementation of this function is:
+
+	l[wd] tmp,p
+	li $2,2
+	jr $31
+	movz $2,$0,tmp
+
+   After if-conversion, we have a conditional move into a pseudo P
+   followed a copy of P into the return register ($2).  P is associated
+   with xyzzy, so if-conversion is behaving as expected, and has not lost
+   the variable association.  The destination of the second instruction
+   is associated with the function return value.  Combine then combines
+   these two instructions, removing the last use of P and xyzzy.
+
+   Everything is behaving as expected in this scenario, so we avoid
+   using conditional moves for this test.  */
+/* { dg-options "-dA -fno-if-conversion" { target mips*-*-* } } */
 /* { dg-final { scan-assembler "xyzzy" } } */
 
 long p;
--- gcc/testsuite/gcc.dg/debug/debug-2.c
+++ gcc/testsuite/gcc.dg/debug/debug-2.c
@@ -1,6 +1,8 @@
 /* Verify that the scheduler does not discard the lexical block.  */
 /* { dg-do compile } */
 /* { dg-options "-dA" } */
+/* See the comment in debug-1.c.  */
+/* { dg-options "-dA -fno-if-conversion" { target mips*-*-* } } */
 /* { dg-final { scan-assembler "xyzzy" } } */
 
 long p;
