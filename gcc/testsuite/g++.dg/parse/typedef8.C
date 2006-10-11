7a1af5508c8bc036ef409b3e0dd6634bf3d8a546
Lee Millward <lmillward@gcc.gnu.org>
2006-10-11 19:43:13 +0000

    add missing test for "typedef auto"
    From-SVN: r117642

--- gcc/testsuite/g++.dg/parse/typedef8.C
+++ gcc/testsuite/g++.dg/parse/typedef8.C
@@ -4,3 +4,8 @@ typedef static int a;   // { dg-error "conflicting" }
 typedef register int b; // { dg-error "conflicting" }
 typedef extern int c;   // { dg-error "conflicting" }
 static typedef int a;   // { dg-error "conflicting" }
+
+int foo()
+{
+  typedef auto int bar; // { dg-error "conflicting" }
+}
