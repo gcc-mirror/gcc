59810d826e26617f8cc7ec5ec53056ab7a26237b
Steve Ellcey <sje@cup.hp.com>
2006-10-12 16:52:33 +0000

    re PR testsuite/29093 (gcc.dg/debug/dwarf2/dwarf-file1.c fails on targets that have .loc)
    	PR testsuite/29093
    	* gcc.dg/debug/dwarf2/dwarf-file1.c: Check for ".file".
    
    From-SVN: r117667

--- gcc/testsuite/gcc.dg/debug/dwarf2/dwarf-file1.c
+++ gcc/testsuite/gcc.dg/debug/dwarf2/dwarf-file1.c
@@ -1,5 +1,9 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -gdwarf-2 -dA" } */
-/* { dg-final { scan-assembler "File Entry:" } } */
-/* Verify that a file entry is output for this file.  */
+/* { dg-final { scan-assembler "File Entry:|.file" } } */
+/* Verify that a file entry is output for this file.  Only systems that
+   do not define HAVE_AS_DWARF2_DEBUG_LINE will put out "File Entry:",
+   but systems that do define HAVE_AS_DWARF2_DEBUG_LINE will put out a
+   ".file" so we check for either.  */
+
 int bar = 3;
