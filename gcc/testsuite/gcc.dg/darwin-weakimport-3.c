1d0a2f45c7165e5ae998d22bfb005c136668855c
echristo <echristo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-10-13 21:37:12 +0000

    2006-10-13  Eric Christopher  <echristo@apple.com>
            * gcc.dg/darwin-weakimport-1.c: Use
            -fno-asynchronous-unwind-tables.
            * gcc.dg/darwin-weakimport-3.c: Ditto.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@117711 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/darwin-weakimport-3.c
+++ gcc/testsuite/gcc.dg/darwin-weakimport-3.c
@@ -1,4 +1,5 @@
 /* { dg-do compile { target *-*-darwin* } } */
+/* { dg-options "-fno-asynchronous-unwind-tables" } */
 /* { dg-require-weak "" } */
 
 /* { dg-final { scan-assembler-not "coalesced" } } */
