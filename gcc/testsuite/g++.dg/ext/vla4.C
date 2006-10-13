5b8345ac6a35a659498893c7a2458ed17c826f64
Mark Mitchell <mark@codesourcery.com>
2006-10-13 08:34:14 +0000

    re PR c++/29318 (ICE: type_info of pointer to VLA)
    	PR c++/29318
    	* rtti.c (get_tinfo_decl): Refuse to create type info objects for
    	variably modified types.
    	PR c++/29318
    	* g++.dg/ext/vla4.C: New test.
    
    From-SVN: r117686

---
+++ gcc/testsuite/g++.dg/ext/vla4.C
@@ -0,0 +1,21 @@
+// PR c++/29318
+// { dg-options "" }
+
+#include <typeinfo>
+
+void f(int i) {
+  try {
+    int a[i];
+    throw &a; // { dg-error "variable size" }
+  } catch (int (&)[i]) { // { dg-error "variable size" }
+  }
+}
+
+int main()
+{
+  int i = 5;
+  int va[i];
+  const std::type_info& info(typeid(&va)); // { dg-error "variable size" }
+
+  return 0;
+}
