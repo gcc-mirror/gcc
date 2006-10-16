b4c74ba24343fb49967075548248964138920c30
Mark Mitchell <mark@codesourcery.com>
2006-10-16 23:06:35 +0000

    re PR c++/29435 (seg fault with sizeof and templates)
    	PR c++/29435
    	* typeck.c (cxx_sizeof_or_alignof_type): Complete non-dependent
    	types when their sizes are required.  Refine test for VLAs.
    	PR c++/29435
    	* g++.dg/template/sizeof11.C: New test.
    
    From-SVN: r117799

---
+++ gcc/testsuite/g++.dg/template/sizeof11.C
@@ -0,0 +1,14 @@
+// PR c++/29435
+
+template < class T > struct A {};
+template < int> void g()
+{
+  sizeof (A < int>);
+}
+
+template < class T > struct B;
+template < int> void f()
+{
+  sizeof (B<int>); // { dg-error "incomplete" }
+}
+
