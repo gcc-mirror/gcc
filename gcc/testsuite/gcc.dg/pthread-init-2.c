5c425b702434cbee34cf8afef65b27ed62483c01
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2006-10-17 01:26:28 +0000

    Backport:
    	2006-10-14  Eric Botcazou  <ebotcazou@libertysurf.fr>
    
    	* gcc.dg/pthread-init-2.c (dg-options): Define _POSIX_C_SOURCE=199506L
    	on Solaris 2.5.1 too.
    
    	2006-10-07  John David Anglin  <dave.anglin@nrc-cnrc.gc.ca>
    
    	PR target/29300
    	* gcc.dg/pthread-init-2.c (dg-options): Define _POSIX_C_SOURCE=199506L
    	on hppa*-*-hpux*.
    
    	2006-10-03  Uros Bizjak  <uros@kss-loka.si>
    
    	* gcc-dg/pthread-init-common.h: Define _GNU_SOURCE. Add tests for
    	PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP,
    	PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP,
    	PTHREAD_ADAPTIVE_MUTEX_INITIALIZER_NP and
    	PTHREAD_RWLOCK_WRITER_NONRECURSIVE_INITIALIZER_NP mutex initializers.
    
    	2006-09-29  Kaveh R. Ghazi  <ghazi@caip.rutgers.edu>
    
    	* gcc.dg/pthread-init-1.c, pthread-init-2.c,
    	pthread-init-common.h: New.
    
    From-SVN: r117811

---
+++ gcc/testsuite/gcc.dg/pthread-init-2.c
@@ -0,0 +1,12 @@
+/* Various Solaris versions have been known to have problems with the
+ * pthread initialization macros when __STDC__ is defined.
+ *
+ * Origin: Kaveh Ghazi (ghazi@caip.rutgers.edu) 9/27/2006.
+ */
+
+/* { dg-do compile } */
+/* { dg-options "-Wextra -Wall -ansi" } */
+/* { dg-options "-Wextra -Wall -ansi -D_POSIX_C_SOURCE=199506L" { target { hppa*-*-hpux* *-*-solaris2.5.1 } } } */
+
+#include "pthread-init-common.h"
+
