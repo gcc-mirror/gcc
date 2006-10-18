02712c161ff3e6a2b7bd51645bd7fd084d1691c4
Tobias Burnus <burnus@net-b.de>
2006-10-18 19:19:23 +0200

    primary.c: Revert 'significand'-to-'significant' comment change.
    2006-10-16  Tobias Burnus  <burnus@net-b.de>
    
            * primary.c: Revert 'significand'-to-'significant' comment change.
            * invoke.texi (Warning Options): Minor cleanup for
              -Wimplicit-interface.
    
    From-SVN: r117858

--- gcc/fortran/primary.c
+++ gcc/fortran/primary.c
@@ -463,7 +463,7 @@ match_real_constant (gfc_expr ** result, int signflag)
       c = gfc_next_char ();
     }
 
-  /* Scan significant.  */
+  /* Scan significand.  */
   for (;; c = gfc_next_char (), count++)
     {
       if (c == '.')
