91ceb6b76458690abf3303e716d1acd5506efec8
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-10-21 13:21:06 +0000

    2006-10-21  Richard Guenther  <rguenther@suse.de>
    	PR middle-end/26898
    	* fold-const.c (fold_comparison): Fold signed comparisons
    	of the form X +- C1 CMP Y +- C2.
    
    	* gcc.dg/torture/pr26898-1.c: New testcase.
    	* gcc.dg/torture/pr26898-2.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@117931 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/pr26898-1.c
@@ -0,0 +1,14 @@
+/* { dg-do link } */
+
+#include <limits.h>
+
+extern void link_error(void);
+int main()
+{
+  int i0, i1;
+  if (!(i0 + 1 < i1 + 1 == i0 < i1))
+    link_error ();
+  if (!(i0 + INT_MIN < i1 - INT_MAX == i0 < i1 - -1))
+    link_error ();
+  return 0;
+}
