9c384dcabc464328ebe0e4da1da978416882e344
dberlin <dberlin@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-10-22 01:25:17 +0000

    Commit forgotten file
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@117947 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/pr26421.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr26421.c
@@ -16,5 +16,5 @@ int foo(void)
   return a.i;
 }
 
-/* { dg-final { scan-tree-dump-times "V_MAY_DEF" 1 "alias1" } } */
+/* { dg-final { scan-tree-dump-times "V_MAY_DEF" 2 "alias1" } } */
 /* { dg-final { cleanup-tree-dump "alias1" } } */
