6c5ee36db1d256621abf95ec65e7c4f6aee046f2
Andreas Krebbel <krebbel1@de.ibm.com>
2006-10-26 11:03:03 +0000

    s390.c (s390_init_frame_layout): Check positive frame_size.
    2006-10-26  Andreas Krebbel  <krebbel1@de.ibm.com>
    
    	* config/s390/s390.c (s390_init_frame_layout): Check positive
    	frame_size.
    
    2006-10-26  Andreas Krebbel  <krebbel1@de.ibm.com>
    
    	* gcc.dg/20061026.c: New testcase.
    
    From-SVN: r118066

---
+++ gcc/testsuite/gcc.dg/20061026.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-O1" } */
+
+/* This testcase failed on s390.  The frame size for function f will be
+   exactly 32768 bytes.  The back end has to recognize that this is to
+   large for a 16bit constant and therefore should reserve the literal
+   pool base pointer.  */
+
+int f () {
+  char a[32608];
+
+  g (a);
+}
