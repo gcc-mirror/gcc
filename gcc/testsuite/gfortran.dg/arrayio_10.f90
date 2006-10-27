d5e2eeeec7f36afdb5537f519e8588a51c0c2066
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2006-10-27 21:42:40 +0000

    re PR fortran/29563 (Internal read loses data.)
    2006-10-27  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/29563
    	* gfortran.dg/arrayio_9.f90: New test.
    	* gfortran.dg/arrayio_19.f90: New test.
    
    From-SVN: r118088

---
+++ gcc/testsuite/gfortran.dg/arrayio_10.f90
@@ -0,0 +1,12 @@
+! { dg-do run }
+! PR29563 Internal read loses data.
+! Test case submitted by Jerry DeLisle  <jvdelisle@gcc.gnu.org>
+! Without patch, values get muddled.
+program pr29563
+  character(len=4), dimension(3)::arraydata = (/'1123',' 456','789 '/)
+  real(kind=8), dimension(3) :: tmp
+  read(arraydata,*,iostat=iostat)tmp
+  if (tmp(1).ne.1123.0) call abort()
+  if (tmp(2).ne.456.0) call abort()
+  if (tmp(3).ne.789.0) call abort()
+end program pr29563
