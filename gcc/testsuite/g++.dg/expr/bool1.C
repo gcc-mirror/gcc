1a358480f30b961e7c0e4c164ad47f67dc5f1db6
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-10-28 23:01:59 +0000

    2006-10-28  Andrew Pinski  <andrew_pinski@playstation.sony.com>
            PR C++/29295
            * typeck.c (build_unary_op): Use same_type_p when comparing to
            boolean type.
    
    2006-10-28  Andrew Pinski  <andrew_pinski@playstation.sony.com>
    
            PR C++/29295
            * g++.dg/expr/bool1.C: New test.
            * g++.dg/expr/bool2.C: New test.
    
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@118118 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/expr/bool1.C
@@ -0,0 +1,21 @@
+// { dg-do run }
+// PR C++/29295
+// make sure that a typedef for a bool will have the
+//  the same results as a bool itself.
+
+extern "C" void abort();
+typedef bool my_bool;
+int main()
+{ 
+  my_bool b = false;
+  int i;
+
+  b++;
+  b++;
+  i = b;
+  if (i != 1)
+    abort ();
+  return 0;
+}
+
+
