fc7509584dfde56f2859f021f821eb804faf2384
Eric Botcazou <ebotcazou@gcc.gnu.org>
2006-10-31 23:29:37 +0000

    20061031-1.c: New test.
            * gcc.c-torture/execute/20061031-1.c: New test.
    
    From-SVN: r118345

---
+++ gcc/testsuite/gcc.c-torture/execute/20061031-1.c
@@ -0,0 +1,28 @@
+/* PR rtl-optimization/29631 */
+/* Origin: Falk Hueffner <falk@debian.org> */
+
+const signed char nunmap[] = { 17, -1, 1 };
+
+__attribute__((noinline))
+void ff(int i) {
+    asm volatile("");
+}
+
+__attribute__((noinline))
+void f(short delta)
+{
+    short p0 = 2, s;
+    for (s = 0; s < 2; s++)
+    {
+        p0 += delta;
+        ff(s);
+        if (nunmap[p0] == 17)
+            asm volatile("# nop");
+    }
+}
+
+int main(void)
+{
+    f(-1);
+    return 0;
+}
