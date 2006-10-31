e5c18c3c298de538f6a9acec3120b63d830dc307
Steven Bosscher <steven@gcc.gnu.org>
2006-10-31 05:31:53 +0000

    * opts.c (decode_options): Disable CSE skip blocks.
    From-SVN: r118219

--- gcc/opts.c
+++ gcc/opts.c
@@ -475,7 +475,6 @@ decode_options (unsigned int argc, const char **argv)
       flag_crossjumping = 1;
       flag_optimize_sibling_calls = 1;
       flag_cse_follow_jumps = 1;
-      flag_cse_skip_blocks = 1;
       flag_gcse = 1;
       flag_expensive_optimizations = 1;
       flag_ipa_type_escape = 1;
