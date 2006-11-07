a7f02c2d27203d8e3581bfdc19f86f863ebf0d80
Steve Ellcey <sje@cup.hp.com>
2006-11-07 19:14:27 +0000

    re PR other/25028 (TImode-to-floating conversions broken)
    	PR other/25028
    	* config/ia64/t-hpux (LIB1ASMFUNCS):  Filter out _fixtfdi,
    	_fixunstfdi, and  _floatditf
    	* gcc.dg/torture/fp-int-convert-float128-timode.c:  Do not XFAIL
    	on HP-UX.
    
    From-SVN: r118565

--- gcc/testsuite/gcc.dg/torture/fp-int-convert-float128-timode.c
+++ gcc/testsuite/gcc.dg/torture/fp-int-convert-float128-timode.c
@@ -2,7 +2,7 @@
 /* Origin: Joseph Myers <joseph@codesourcery.com> */
 /* { dg-do run { target { ia64-*-* || { { i?86-*-* x86_64-*-*} && lp64 } } } } */
 /* { dg-xfail-if "" { i?86-*-* x86_64-*-* } { "*" } { "" } } */
-/* { dg-xfail-if "" { ia64-*-* && lp64 } { "*" } { "" } } */
+/* { dg-xfail-if "" { ia64-*-linux* } { "*" } { "" } } */
 /* { dg-options "" } */
 
 #include "fp-int-convert.h"
