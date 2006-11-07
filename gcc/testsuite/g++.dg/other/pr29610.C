fc78ca67443f6d98fc975d95f34baa8e11bfa391
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-11-07 17:29:34 +0000

    2006-11-07  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/29610
    	* tree-cfgcleanup.c (cleanup_control_flow): Honor return value
    	of tree_purge_dead_eh_edges as it may free dominators.
    
    	* g++.dg/other/pr29610.C: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@118555 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/other/pr29610.C
@@ -0,0 +1,38 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -funswitch-loops" } */
+
+struct __normal_iterator 
+{
+  typedef int*const *_Iterator;
+  int*const * _M_current;
+  __normal_iterator(const _Iterator& __i) : _M_current(__i){}
+  const _Iterator& base() const {}
+};
+struct string { ~string(){} };
+struct vector 
+{
+  int** _M_finish;
+  __normal_iterator end() const   { return __normal_iterator (_M_finish); }
+  int size() const   { return end().base() - end().base(); }
+};
+class Painter
+{
+  int redraw_window(void);
+  typedef int (Painter::* SliceWindowFunc)(void);
+  int for_each(vector&, SliceWindowFunc);
+  void tcl_command(void);
+};
+inline int Painter::for_each(vector &layout, SliceWindowFunc func)
+{
+    for (unsigned int window = 0; window < layout.size();++window)
+        (this->*func)();
+}
+int t;
+int Painter::redraw_window(void) {t = 1;}
+string t2(int);
+vector *g(const string&);
+void Painter::tcl_command(void)
+{
+     for_each(*g(t2(2)), &Painter::redraw_window);
+}
+
