e7dd508724834c41fc0501faa6fa7d9585dd45ab
Erik Edelmann <eedelman@gcc.gnu.org>
2006-11-08 21:19:01 +0000

    re PR fortran/29630 ("Unclassifiable statement" with vector subscripts in initialization)
    2006-11-08  Erik Edelmann  <eedelman@gcc.gnu.org>
    
            PR fortran/29630
            PR fortran/29679
            * expr.c (find_array_section): Support vector subscripts.  Don't
            add sizes for dimen_type == DIMEN_ELEMENT to the shape array.
    
    testsuite/
    2006-11-08  Erik Edelmann  <eedelman@gcc.gnu.org>
    
            PR fortran/29630
            PR fortran/29679
            * gfortran.dg/initialization_2.f90: Test PRs 29630 and 29679 too.
    
    From-SVN: r118599

--- gcc/testsuite/gfortran.dg/initialization_2.f90
+++ gcc/testsuite/gfortran.dg/initialization_2.f90
@@ -1,7 +1,22 @@
-! { dg-do compile }
-! PR 29393: Ranks of PARAMETER-lhs in initializations
-    integer, parameter ::   A(-3:7,2)=0 
-    integer, parameter, dimension(3) :: V = (/ 2, 4, 6 /)
-    integer, parameter, dimension(3) :: B = A(V,1) 
-    integer, parameter, dimension(3) :: C = A(0:2,1) 
-end
+! {dg-do run }
+! Vector subscripts, ranks and shapes of initialization expressions (PRs 29393,
+! 29630 and 29679)
+program test
+
+    implicit none
+    integer :: i, j
+    integer, parameter :: a(4,4,4) = reshape([ (i,i=1,64) ], [4,4,4])
+    integer, parameter :: v(4) = [4, 1, 3, 2]
+
+    integer :: b1(3,3) = a(1:3, 2, 2:4)
+    integer :: b2(1,3) = a(2:2, 4, [1,4,3])
+    integer :: b2b(3) = a([1,4,3], 2, 4)
+    integer :: b3(4) = a(1, v, 3)
+    integer :: b4(3,3) = a(v([2,4,3]), 2, [2,3,4])
+
+    if (any(b1 /= reshape([21,22,23, 37,38,39, 53,54,55], [3,3]))) call abort()
+    if (any(b2 /= reshape([14, 62, 46], [1,3]))) call abort()
+    if (any(b2b /= [53, 56, 55])) call abort()
+    if (any(b3 /= [45, 33, 41, 37])) call abort()
+    if (any(b4 /= reshape([21,22,23, 37,38,39, 53,54,55], [3,3]))) call abort()
+end program test
