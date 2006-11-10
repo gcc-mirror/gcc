6f720ad03a19ecabdfa8bbcdb1594d3f52d12160
Serge Belyshev <belyshev@depni.sinp.msu.ru>
2006-11-10 13:44:40 +0000

    re PR middle-end/29726 (invalid folding of ((X >> C1) & C2) != 0 or "M-x is undefined" in emacs)
    gcc:
    2006-11-09  Serge Belyshev  <belyshev@depni.sinp.msu.ru>
    
    	PR middle-end/29726
    	* fold-const.c (fold_binary) <EQ_EXPR>: Fix typo in variable name.
    
    gcc/testsuite:
    2006-11-09  Serge Belyshev  <belyshev@depni.sinp.msu.ru>
    
    	PR middle-end/29726
    	* gcc.dg/fold-eqandshift-1.c (test5): Uncomment function.
    	(test6): Ditto.
            (dg-final): Add scan-tree-dump-times patterns for test5 and test6.
            * gcc.dg/fold-eqandshift-3.c: New test case.
    
    From-SVN: r118653

--- gcc/testsuite/gcc.dg/fold-eqandshift-1.c
+++ gcc/testsuite/gcc.dg/fold-eqandshift-1.c
@@ -25,7 +25,6 @@ int test4(int d)
   return (d >> 3) & 4;
 }
 
-#if 0
 void test5(int e)
 {
   if ((e >> 31) & 64)
@@ -37,10 +36,11 @@ void test6(unsigned int f)
   if ((f >> 31) & 64)
     foo();
 }
-#endif
 
 /* { dg-final { scan-tree-dump-times "\\(a \& 8\\) != 0" 1 "original" } } */
 /* { dg-final { scan-tree-dump-times "\\(b \& 32\\) != 0" 1 "original" } } */
 /* { dg-final { scan-tree-dump-times "c >> 3 \& 1" 1 "original" } } */
 /* { dg-final { scan-tree-dump-times "d >> 3 \& 4" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "e < 0" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "if \\(0\\)" 1 "original" } } */
 /* { dg-final { cleanup-tree-dump "original" } } */
