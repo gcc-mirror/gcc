015e23f4005b00afaf87c90ef67db48b364fe70e
Roger Sayle <roger@eyesopen.com>
2006-11-12 18:41:31 +0000

    re PR tree-optimization/13827 ((a & b) != (c & b) should be transformed to ((a^c) & b) !=0)
    	PR tree-optimization/13827
    	* fold-const.c (fold_binary) <EQ_EXPR, NE_EXPR>: Fold (X&C) op (Y&C)
    	as ((X^Y)&C) op 0.
    
    	* gcc.dg/fold-eqand-1.c: New test case.
    
    From-SVN: r118727

---
+++ gcc/testsuite/gcc.dg/fold-eqand-1.c
@@ -0,0 +1,17 @@
+/* PR tree-optimization/13827 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-original" } */
+
+unsigned foo (unsigned a, unsigned b)
+{
+  return (a & 0xff00) != (b & 0xff00);
+}
+
+unsigned bar (unsigned c, unsigned d)
+{
+  return (c & 0xff00) == (d & 0xff00);
+}
+
+/* { dg-final { scan-tree-dump-times "a \\^ b" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "c \\^ d" 1 "original" } } */
+/* { dg-final { cleanup-tree-dump "original" } } */
