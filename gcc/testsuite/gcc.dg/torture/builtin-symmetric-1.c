5c5b21550c5b1b9631b0d14569cc47f6d16dfc7b
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2006-11-12 23:51:36 +0000

    builtins.c (fold_builtin_cosh): New.
    	* builtins.c (fold_builtin_cosh): New.
    	(fold_builtin_1): Use it.
    	* fold-const.c (negate_mathfn_p): Add llround, lround, round,
    	trunc to the list of "odd" functions.  Also add llrint, lrint,
    	rint and nearbyint when flag_rounding_math is false.
    
    testsuite:
    	* gcc.dg/torture/builtin-symmetric-1.c: Add more cases.
    
    From-SVN: r118733

--- gcc/testsuite/gcc.dg/torture/builtin-symmetric-1.c
+++ gcc/testsuite/gcc.dg/torture/builtin-symmetric-1.c
@@ -21,6 +21,12 @@ extern void link_error(int);
     link_error(__LINE__); \
   } while (0)
 
+/* Test that FUNC(-VAR) == FUNC(VAR), where VAR has an int type.  */
+#define TESTIT_EVEN_I(FUNC,VAR) do { \
+  if (__builtin_##FUNC(-VAR) != __builtin_##FUNC(VAR)) \
+    link_error(__LINE__); \
+  } while (0)
+
 /* Test that -FUNC(ARG) == FUNC(-ARG).  */
 #define TESTIT_ODD(FUNC) do { \
   if (-__builtin_##FUNC##f(-xf) != __builtin_##FUNC##f(xf)) \
@@ -31,24 +37,40 @@ extern void link_error(int);
     link_error(__LINE__); \
   } while (0)
 
-void foo (float xf, double x, long double xl)
+void foo (float xf, double x, long double xl,
+	  int i, long l, long long ll, __INTMAX_TYPE__ im)
 {
   TESTIT_EVEN(cos);
+  TESTIT_EVEN(cosh);
+  TESTIT_EVEN(fabs);
+
+  TESTIT_EVEN_I(abs, i);
+  TESTIT_EVEN_I(imaxabs, im);
+  TESTIT_EVEN_I(labs, l);
+  TESTIT_EVEN_I(llabs, ll);
 
   TESTIT_ODD(asin);
   TESTIT_ODD(asinh);
   TESTIT_ODD(atan);
   TESTIT_ODD(atanh);
   TESTIT_ODD(cbrt);
+  TESTIT_ODD(erf);
+  TESTIT_ODD(llrint);
+  TESTIT_ODD(llround);
+  TESTIT_ODD(lrint);
+  TESTIT_ODD(lround);
+  TESTIT_ODD(nearbyint);
+  TESTIT_ODD(rint);
+  TESTIT_ODD(round);
   TESTIT_ODD(sin);
   TESTIT_ODD(sinh);
   TESTIT_ODD(tan);
   TESTIT_ODD(tanh);
-  TESTIT_ODD(erf);
+  TESTIT_ODD(trunc);
 }
 
 int main()
 {
-  foo (1,1,1);
+  foo (1,1,1,1,1,1,1);
   return 0;
 }
