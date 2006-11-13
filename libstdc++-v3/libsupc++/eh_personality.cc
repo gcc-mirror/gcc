51f3ea16752100ed8d78423d00035973fbd74988
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-11-13 13:28:28 +0000

    * libsupc++/eh_globals.cc (__cxxabiv1::__cxa_get_globals): 	Initialize propagatingExceptions if __ARM_EABI_UNWINDER__. 	* libsupc++/eh_personality.cc (empty_exception_spec): Define 	separately in __ARM_EABI_UNWINDER__ case.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@118757 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/libsupc++/eh_personality.cc
+++ libstdc++-v3/libsupc++/eh_personality.cc
@@ -192,6 +192,17 @@ restore_caught_exception(struct _Unwind_Exception* ue_header,
     }								\
   while (0)
 
+// Return true if the filter spec is empty, ie throw().
+
+static bool
+empty_exception_spec (lsda_header_info *info, _Unwind_Sword filter_value)
+{
+  const _Unwind_Word* e = ((const _Unwind_Word*) info->TType)
+			  - filter_value - 1;
+
+  return *e == 0;
+}
+
 #else
 typedef const std::type_info _throw_typet;
 
@@ -312,8 +323,6 @@ restore_caught_exception(struct _Unwind_Exception* ue_header,
 
 #define CONTINUE_UNWINDING return _URC_CONTINUE_UNWIND
 
-#endif // !__ARM_EABI_UNWINDER__
-
 // Return true if the filter spec is empty, ie throw().
 
 static bool
@@ -325,6 +334,8 @@ empty_exception_spec (lsda_header_info *info, _Unwind_Sword filter_value)
   e = read_uleb128 (e, &tmp);
   return tmp == 0;
 }
+
+#endif // !__ARM_EABI_UNWINDER__
 
 namespace __cxxabiv1
 {
