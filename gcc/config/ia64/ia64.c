0d94484ee9cdc14678630508ef13d95df3e93622
Steve Ellcey <sje@cup.hp.com>
2006-11-15 23:02:04 +0000

    ia64.c (ia64_hpux_init_libfuncs): Use HP-UX millicode routines for integer division.
    	* config/ia64/ia64.c (ia64_hpux_init_libfuncs):  Use HP-UX millicode
    	routines for integer division.
    
    From-SVN: r118870

--- gcc/config/ia64/ia64.c
+++ gcc/config/ia64/ia64.c
@@ -9272,6 +9272,22 @@ ia64_hpux_init_libfuncs (void)
 {
   ia64_init_libfuncs ();
 
+  /* The HP SI millicode division and mod functions expect DI arguments.
+     By turning them off completely we avoid using both libgcc and the
+     non-standard millicode routines and use the HP DI millicode routines
+     instead.  */
+
+  set_optab_libfunc (sdiv_optab, SImode, 0);
+  set_optab_libfunc (udiv_optab, SImode, 0);
+  set_optab_libfunc (smod_optab, SImode, 0);
+  set_optab_libfunc (umod_optab, SImode, 0);
+
+  set_optab_libfunc (sdiv_optab, DImode, "__milli_divI");
+  set_optab_libfunc (udiv_optab, DImode, "__milli_divU");
+  set_optab_libfunc (smod_optab, DImode, "__milli_remI");
+  set_optab_libfunc (umod_optab, DImode, "__milli_remU");
+
+  /* HP-UX libc has TF min/max/abs routines in it.  */
   set_optab_libfunc (smin_optab, TFmode, "_U_Qfmin");
   set_optab_libfunc (smax_optab, TFmode, "_U_Qfmax");
   set_optab_libfunc (abs_optab, TFmode, "_U_Qfabs");
