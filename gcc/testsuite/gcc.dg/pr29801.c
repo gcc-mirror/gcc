aecfc21dc36c6705efdfee01ab12edbe03f96ee2
rakdver <rakdver@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-11-17 10:29:07 +0000

    PR tree-optimization/29801 	* tree-ssa-ccp.c (get_symbol_constant_value): New function. 	(get_default_value): Use get_symbol_constant_value. 	(set_lattice_value): ICE when the value of the constant is 	changed. 	(visit_assignment): Ignore VDEFs of read-only variables.
    	* gcc.dg/pr29801.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@118926 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr29801.c
@@ -0,0 +1,24 @@
+/* We used to crash in ccp here, because the initial constant value of 2
+   was changed to 5.  */
+
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+static const int a = 2;
+
+int test (int param)
+{
+  int *p = (int *) &a;
+
+  if (param)
+    *p = 5;
+
+  return a;
+}
+
+/* Check that we return the correct (unchanged) value.  */
+
+/* { dg-final { scan-tree-dump-times "return 2" 1 "optimized" } } */
+/* { dg-final { scan-tree-dump-times "return 5" 0 "optimized" } } */
+
+/* { dg-final { cleanup-tree-dump "optimized" } } */
