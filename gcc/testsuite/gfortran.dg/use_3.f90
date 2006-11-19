39f87c0333117fa64894cb1f7dd0e69307a9cf0d
Francois-Xavier Coudert <coudert@clipper.ens.fr>
2006-11-19 22:18:26 +0100

    module.c (gfc_use_module): Uncomment the ISO_FORTRAN_ENV code.
    	* module.c (gfc_use_module): Uncomment the ISO_FORTRAN_ENV code.
    	Check that intrinsic and non-intrinsic modules don't conflict.
    	(use_iso_fortran_env_module): New function.
    	(create_int_parameter): New function.
    	* trans-types.c (gfc_init_kinds): Choose values for
    	gfc_numeric_storage_size and gfc_character_storage_size.
    	(gfc_numeric_storage_size, gfc_character_storage_size): New variables.
    	* resolve.c (resolve_symbol): Do no check intrinsic modules
    	against the list of intrinsic symbols.
    	* iso-fortran-env.def: New file.
    	* gfortran.h (gfc_numeric_storage_size,
    	gfc_character_storage_size): Add prototypes.
    
    	* gfortran.dg/use_3.f90: Remove error message.
    	* gfortran.dg/iso_fortran_env_1.f90: New test.
    	* gfortran.dg/iso_fortran_env_2.f90: New test.
    	* gfortran.dg/iso_fortran_env_3.f90: New test.
    	* gfortran.dg/iso_fortran_env_4.f90: New test.
    
    From-SVN: r118998

--- gcc/testsuite/gfortran.dg/use_3.f90
+++ gcc/testsuite/gfortran.dg/use_3.f90
@@ -7,6 +7,6 @@ end module foo
   use, intrinsic iso_fortran_env ! { dg-error "\"::\" was expected after module nature" }
   use, non_intrinsic iso_fortran_env ! { dg-error "\"::\" was expected after module nature" }
   use, nonintrinsic :: iso_fortran_env ! { dg-error "shall be either INTRINSIC or NON_INTRINSIC" }
-  use, intrinsic :: iso_fortran_env ! { dg-error "Can't find an intrinsic module named" }
+  use, intrinsic :: iso_fortran_env
 end
 ! { dg-final { cleanup-modules "foo" } }
