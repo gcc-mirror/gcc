f1ac90336344b19d7a4193bdc7361614f1188ac0
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-11-20 21:29:32 +0000

    fortran/ 2006-11-17  Tobias Burnus  <burnus@net-b.de>
        PR fortran/27546
        * primary.c (gfc_match_rvalue): Added IMPORT support.
    
    testsuite/
    2006-11-17  Tobias Burnus  <burnus@net-b.de>
    
        PR fortran/27546
        * gfortran.dg/import.f90: Extended test.
        * gfortran.dg/import2.f90: Extended test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@119028 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/import2.f90
+++ gcc/testsuite/gfortran.dg/import2.f90
@@ -15,18 +15,36 @@ subroutine test(x)
 end subroutine test
 
 
-subroutine bar(x)
+subroutine bar(x,y)
   type myType
     sequence
     integer :: i
   end type myType
   type(myType) :: x
+  integer(8) :: y
+  if(y /= 8) call abort()
   if(x%i /= 2) call abort()
   x%i = 5
+  y   = 42
 end subroutine bar
 
+module testmod
+  implicit none
+  integer, parameter :: kind = 8
+  type modType
+    real :: rv
+  end type modType
+  interface
+    subroutine other(x,y)
+      import ! { dg-error "Fortran 2003: IMPORT statement" }
+      type(modType) :: y ! { dg-error "not been declared within the interface" }
+      real(kind)    :: x ! { dg-error "has not been declared" }
+    end subroutine
+  end interface
+end module testmod
 
 program foo
+  integer, parameter :: dp = 8
   type myType
     sequence
     integer :: i
@@ -36,9 +54,10 @@ program foo
     integer :: i
   end type myType3
   interface
-    subroutine bar(x)
+    subroutine bar(x,y)
       import ! { dg-error "Fortran 2003: IMPORT statement" }
       type(myType) :: x ! { dg-error "not been declared within the interface" }
+      integer(dp)  :: y ! { dg-error "has not been declared" }
     end subroutine bar
     subroutine test(x)
       import :: myType3 ! { dg-error "Fortran 2003: IMPORT statement" }
@@ -49,10 +68,13 @@ program foo
 
   type(myType) :: y
   type(myType3) :: z
+  integer(dp) :: i8
   y%i = 2
-  call bar(y) ! { dg-error "Type/rank mismatch in argument" }
-  if(y%i /= 5) call abort()
+  i8 = 8
+  call bar(y,i8) ! { dg-error "Type/rank mismatch in argument" }
+  if(y%i /= 5 .or. i8/= 42) call abort()
   z%i = 7
   call test(z) ! { dg-error "Type/rank mismatch in argument" }
   if(z%i /= 1) call abort()
 end program foo
+! { dg-final { cleanup-modules "testmod" } }
