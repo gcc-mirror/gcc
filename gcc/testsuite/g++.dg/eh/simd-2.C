dedb3bd1607759362ae4cd0601745ad128004ded
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-11-24 14:06:26 +0000

    * g++.dg/eh/simd-2.C: Use -O -w in general for PowerPC. 	* gcc.dg/debug/dwarf2/const-2.c: Use powerpc_altivec_ok.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@119152 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/eh/simd-2.C
+++ gcc/testsuite/g++.dg/eh/simd-2.C
@@ -2,6 +2,7 @@
 // Contributed by Aldy Hernandez (aldy@quesejoda.com).
 // { dg-options "-O" }
 // { dg-options "-O -w" { target { { i?86-*-* x86_64-*-* } && ilp32 } } }
+// { dg-options "-O -w" { target powerpc*-*-* } }
 // { dg-options "-O -w -maltivec" { target { powerpc*-*-linux* && powerpc_altivec_ok } } }
 // { dg-xfail-if "" { "powerpc-*-eabispe*" "powerpc-ibm-aix*" } { "*" } { "" } }
 // { dg-do run }
