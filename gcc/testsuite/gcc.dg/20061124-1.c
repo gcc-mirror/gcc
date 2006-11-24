8bb501bb101f74c9c02e949edad65f07615e27d0
Andreas Krebbel <krebbel1@de.ibm.com>
2006-11-24 13:30:59 +0000

    s390.c (s390_emit_compare_and_swap): New function.
    2006-11-24  Andreas Krebbel  <krebbel1@de.ibm.com>
    
    	* config/s390.c (s390_emit_compare_and_swap): New function.
    	(s390_expand_cs_hqi, s390_expand_atomic): Call 
    	s390_emit_compare_and_swap.
    
    2006-11-24  Andreas Krebbel  <krebbel1@de.ibm.com>
    
    	* gcc.dg/20061124-1.c: New testcase.
    
    From-SVN: r119151

---
+++ gcc/testsuite/gcc.dg/20061124-1.c
@@ -0,0 +1,19 @@
+/* { dg-do run } */
+/* { dg-require-effective-target sync_char_short } */
+
+/* This testcase failed on s390 because no compare instruction for
+   the check of FLAG was emitted.  */
+
+unsigned short int count = 0;
+int flag = 1;
+
+extern void abort (void);
+
+int
+main ()
+{
+  __sync_add_and_fetch (&count, -1);
+
+  if (!flag)
+    abort ();
+}
