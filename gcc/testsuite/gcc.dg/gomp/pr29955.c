5c386a95ee20b9bb8f705ed1f8394ca7b4bafa7d
Jakub Jelinek <jakub@redhat.com>
2006-11-24 22:28:38 +0100

    re PR c/29955 (ICE with -fopenmp -fexceptions)
    	PR c/29955
    	* c-tree.h (c_maybe_initialize_eh): New prototype.
    	* c-decl.c (finish_decl): Move EH initialization...
    	(c_maybe_initialize_eh): ... here.  New function.
    	* c-parser.c (c_parser_omp_construct): Call c_maybe_initialize_eh
    	if not #pragma omp atomic.
    
    	* gcc.dg/gomp/pr29955.c: New test.
    
    From-SVN: r119168

---
+++ gcc/testsuite/gcc.dg/gomp/pr29955.c
@@ -0,0 +1,14 @@
+/* PR c/29955 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fopenmp -fexceptions" } */
+
+extern void bar (int);
+
+void
+foo (int n)
+{
+  int i;
+#pragma omp parallel for schedule(dynamic)
+  for (i = 0; i < n; i++)
+    bar (0);
+}
