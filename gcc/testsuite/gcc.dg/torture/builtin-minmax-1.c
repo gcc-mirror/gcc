a8e3bad42b9636e20442b52a3924c914dc34dd82
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2006-11-26 14:35:54 +0000

    builtins.c (fold_builtin_fmin_fmax): Handle NaN arguments.
    	* builtins.c (fold_builtin_fmin_fmax): Handle NaN arguments.
    
    testsuite:
    	* gcc.dg/torture/builtin-minmax-1.c: Test NaN in fmin/fmax.
    	Don't ever inline the testcase.
    
    From-SVN: r119224

--- gcc/testsuite/gcc.dg/torture/builtin-minmax-1.c
+++ gcc/testsuite/gcc.dg/torture/builtin-minmax-1.c
@@ -74,7 +74,25 @@ extern int pure(int) __attribute__ ((__pure__));
     link_error(__LINE__); \
   } while (0)
 
-void foo (float xf, double x, long double xl,
+/* Test that FUNC(NaN,x) == x.  We cast to (long) so "!=" folds.  Set
+   parameter SIGNAL to `s' for testing signaling NaN.  */
+#define TEST_NAN(FUNC,SIGNAL) do { \
+  if ((long)FUNC##f(__builtin_nan##SIGNAL##f(""),xf) != (long)xf) \
+    link_error(__LINE__); \
+  if ((long)FUNC##f(xf,__builtin_nan##SIGNAL##f("")) != (long)xf) \
+    link_error(__LINE__); \
+  if ((long)FUNC(__builtin_nan##SIGNAL(""),x) != (long)x) \
+    link_error(__LINE__); \
+  if ((long)FUNC(x,__builtin_nan##SIGNAL("")) != (long)x) \
+    link_error(__LINE__); \
+  if ((long)FUNC##l(__builtin_nan##SIGNAL##l(""),xl) != (long)xl) \
+    link_error(__LINE__); \
+  if ((long)FUNC##l(xl,__builtin_nan##SIGNAL##l("")) != (long)xl) \
+    link_error(__LINE__); \
+  } while (0)
+
+void __attribute__ ((__noinline__))
+     foo (float xf, double x, long double xl,
 	  float yf, double y, long double yl,
 	  int i, int j)
 {
@@ -91,6 +109,11 @@ void foo (float xf, double x, long double xl,
   
   TEST_NONNEG(fmin);
   TEST_NONNEG(fmax);
+
+  TEST_NAN(fmin,);
+  TEST_NAN(fmax,);
+  TEST_NAN(fmin,s);
+  TEST_NAN(fmax,s);
 }
 
 int main()
