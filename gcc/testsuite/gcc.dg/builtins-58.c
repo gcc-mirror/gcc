1c4709b4ce5104e3194d811f455d3a314b9d9162
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-11-27 11:38:42 +0000

    2006-11-27  Richard Guenther  <rguenther@suse.de>
    	PR middle-end/25620
    	* builtins.c (expand_builtin_pow): Optimize non integer valued
    	constant exponents using sqrt or cbrt if possible.  Always fall back
    	to expanding via optabs.
    
    	* gcc.target/i386/pow-1.c: New testcase.
    	* gcc.dg/builtins-58.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@119248 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/builtins-58.c
@@ -0,0 +1,33 @@
+/* { dg-do compile } */
+/* { dg-options "-O -ffast-math -std=c99" } */
+
+#include "builtins-config.h"
+
+#ifdef HAVE_C99_RUNTIME
+double test1 (double x)
+{
+  return __builtin_pow (x, 1./3.);
+}
+
+double test2 (double x)
+{
+  return __builtin_pow (x, 4./3.);
+}
+
+double test3a (double x)
+{
+  return __builtin_pow (x, 5./3.);
+}
+
+double test3b (double x)
+{
+  return __builtin_pow (x, -5./3.);
+}
+
+double test4 (double x)
+{
+  return __builtin_pow (x, 7./3.);
+}
+#endif
+
+/* { dg-final { scan-assembler-not "pow" } } */
