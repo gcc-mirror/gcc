91543a506f4bb2483fa6ee0983b228c530f7f1da
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-11-29 07:23:11 +0000

    2006-11-28  Andrew Pinski  <pinskia@gmail.com>
            PR tree-opt/29984
            * gcc.dg/tree-ssa/reassoc-12.c: New test.
    
    2006-11-28  Andrew Pinski  <pinskia@gmail.com>
    
            PR tree-opt/29984
            * tree-ssa-reassoc.c (eliminate_duplicate_pair): Handle MAX_EXPR
            and MIN_EXRP.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@119303 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/reassoc-12.c
@@ -0,0 +1,11 @@
+/* { dg-do compile } */ 
+/* { dg-options "-O2 -fdump-tree-reassoc1-details" } */
+int f(int a, int b)
+{
+  /* MAX_EXPR <a, a> should cause it to be equivalent to a.  */
+  int c = a>=b?a:b;
+  int d = c>=a?c:a;
+  return d;
+}
+/* { dg-final { scan-tree-dump-times "Equivalence:" 1 "reassoc1"} } */
+/* { dg-final { cleanup-tree-dump "reassoc1" } } */
