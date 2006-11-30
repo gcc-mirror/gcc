e9ec06a1ff8a21b76ca42d2312c53632490bf644
mueller <mueller@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-11-30 23:08:27 +0000

    2006-12-01  Dirk Mueller  <dmueller@suse.de>
            PR c++/18313
            * decl.c (grokdeclarator): Warn for type qualifiers on return
            type for non-dependent types.
            * pt.c (tsubst_function_type): Warn for type qualifiers on
            return type for dependent types.
    
            * g++.dg/warn/Wreturn-type-4.C: New testcase.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@119382 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/warn/Wreturn-type-4.C
@@ -0,0 +1,43 @@
+/* PR c++/18313 */
+/* { dg-do compile } */
+/* { dg-options "-Wreturn-type" } */
+
+volatile void bar(); /* { dg-warning "type qualifiers ignored" } */
+
+struct A
+{
+    const int bla(); /* { dg-warning "type qualifiers ignored" } */
+    static const A getA(); /* { dg-bogus "type qualifiers" } */
+};
+
+template<typename T> const T getfoo(const T def) /* { dg-bogus "type qualifiers ignored" } */
+{ return def; } 
+
+template<typename T> class Pair
+{
+    public:
+        T getLeft() const { return T(); }   /* { dg-warning "type qualifiers ignored" } */
+        const T getRight() const { return T(); } /* { dg-bogus "type qualifiers ignored" } */
+};
+
+template <typename T> struct S {
+    const int f();                     /* { dg-warning "type qualifiers ignored" } */
+    const T g();                       /* { dg-bogus "type qualifiers ignored" } */
+    T h();
+};
+
+int* testtemplate()
+{
+    int i;
+
+    Pair<const int> a;
+
+    a.getLeft();
+    a.getRight();
+
+    S<bool> b;
+    b.h();              /* { dg-bogus "type qualifiers ignored" } */
+    b.g();              /* { dg-bogus "type qualifiers ignored" } */
+
+    return getfoo<int*>(&i);
+}
