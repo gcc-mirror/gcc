c3a88be88a471325a3684af578c348bca2034fd3
Ryan Mansfield <rmansfield@qnx.com>
2006-12-01 15:55:11 +0000

    re PR c++/29066 (ptrmemfunc_vbit_in_delta is broken)
    2006-12-01  Ryan Mansfield  <rmansfield@qnx.com>
    
            PR c++/29066
            * typeck.c (build_binary_op):  Fix pointer to member function
            comparison for ptrmemfunc_vbit_in_delta targets.
    
    2006-12-01  Ryan Mansfield  <rmansfield@qnx.com>
    
            PR c++/29066
            * g++.dg/expr/pr29066.c: New.
    
    From-SVN: r119403

---
+++ gcc/testsuite/g++.dg/expr/pr29066.C
@@ -0,0 +1,42 @@
+// PR c++/29066
+// Test pointer to member function comparison
+// { dg-do run }
+
+extern "C" void abort (void);
+
+struct X
+{
+  virtual void a(void)=0;
+};
+
+struct Z : public X
+{
+  void a(void) {};
+};
+
+
+void f(X *obj)
+{
+  void (X::*xp)(void) = 0;
+  void (X::*xp2)(void) = 0;
+
+  xp = &X::a;
+
+  if (xp == xp2)
+    {
+      abort(); 
+    } 
+
+  if (xp == 0)
+    {
+      abort();
+    }
+}
+
+int main(int argc, char* argv[])
+{
+  Z myobj;
+
+  f(&myobj);
+  return 0;
+}
