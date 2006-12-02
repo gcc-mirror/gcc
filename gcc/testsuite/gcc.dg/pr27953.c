5f65db9f8c8e8353f5c765223be6b9d45c9a4373
Lee Millward <lmillward@gcc.gnu.org>
2006-12-02 16:54:35 +0000

    fix testcase from previous commit
    From-SVN: r119448

--- gcc/testsuite/gcc.dg/pr27953.c
+++ gcc/testsuite/gcc.dg/pr27953.c
@@ -1,4 +1,4 @@
 /* PR c/27953 */
 
 void foo(struct A a) {}  /* { dg-error "parameter list|definition|incomplete type" } */
-void foo() {}            /* { dg-error "redefinition" }
+void foo() {}            /* { dg-error "redefinition" } */
