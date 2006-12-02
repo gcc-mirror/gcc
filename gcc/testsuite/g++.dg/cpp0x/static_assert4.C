dff2f6650bae91d28851542d0aee478f8fc5d1ca
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-12-02 17:01:04 +0000

    2006-12-02  Andrew Pinski  <andrew_pinski@playstation.sony.com>
            PR C++/30033
            * decl.c (cp_tree_node_structure): Handle STATIC_ASSERT.
    
    2006-12-02  Andrew Pinski  <andrew_pinski@playstation.sony.com>
    
            PR C++/30033
            *  g++.dg/cpp0x/static_assert4.C: New testcase.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@119449 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/static_assert4.C
@@ -0,0 +1,15 @@
+// { dg-options "-std=c++0x --param ggc-min-heapsize=0 --param ggc-min-expand=0 " }
+// PR C++/30033
+// Make sure that the static assert does not crash the GC.
+
+template <class T>
+struct default_delete
+{
+  void
+  operator() (T * ptr) const
+  {
+    static_assert (sizeof (T) > 0, "Can't delete pointer to incomplete type");
+  }
+};
+
+
