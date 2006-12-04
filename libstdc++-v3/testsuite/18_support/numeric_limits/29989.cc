aced8a0ba91742cfd16ba0d529c29b99d0fb60af
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-12-04 21:11:52 +0000

    2006-12-04  Paolo Carlini  <pcarlini@suse.de>
    	PR libstdc++/29989 (again)
    	* include/bits/c++config: #undef min and max.
    	* testsuite/18_support/numeric_limits/29989.cc: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@119510 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/18_support/numeric_limits/29989.cc
@@ -0,0 +1,23 @@
+// { dg-do compile }
+
+// Copyright (C) 2006 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// libstdc++/29989
+#define max(a,b) quirk
+#include <limits>
