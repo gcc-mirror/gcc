6085f995750b319c98bebe219512c6ded2ef6c78
Mark Mitchell <mark@codesourcery.com>
2006-12-06 18:26:44 +0000

    re PR c++/29728 (ICE on invalid initializer in template function)
    	PR c++/29728
    	* decl.c (check_array_designated_initializer): New function.
    	(maybe_deduce_size_from_array_init): Use it.
    	(reshape_init_array): Likewise.
    
    	PR c++/29728
    	* g++.dg/template/crash62.C: New test.
    
    From-SVN: r119588

--- gcc/testsuite/g++.dg/ext/complit3.C
+++ gcc/testsuite/g++.dg/ext/complit3.C
@@ -2,7 +2,7 @@
 
 int Compound_Literals_0()
 {
-  static int y[] = (int []) {1, 2, 3}; // { dg-error "compound literal" }
-  static int z[] = (int [3]) {1}; // { dg-error "compound literal" }
+  static int y[] = (int []) {1, 2, 3}; // { dg-error "compound literal|size" }
+  static int z[] = (int [3]) {1}; // { dg-error "compound literal|size" }
   return y[0]+z[0]; 
 }
