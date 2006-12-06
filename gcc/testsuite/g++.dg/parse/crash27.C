95f804646d79ed3df1162f8ce7e0b0ebaaddf3f1
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-12-06 22:54:51 +0000

    PR c++/29730 	* parser.c (cp_parser_init_declarator): Reject initialization of 	functions. 	PR c++/29730 	* g++.dg/template/crash64.C: New test. 	* g++.dg/parse/crash27.C: Adjust error markers.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@119601 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/parse/crash27.C
+++ gcc/testsuite/g++.dg/parse/crash27.C
@@ -3,6 +3,3 @@
 void Dispatcher()
 	 (__builtin_offsetof (ArgsType, largeMsgLen))
 	/* { dg-error "function " "function" { target *-*-* } 4 } */
-	/* { dg-error "expected type" "expected 1" { target *-*-* } 4 } */
-	/* { dg-error "expected `,' before" "expected 2" { target *-*-* } 4 } */
-	/* { dg-error "expected `\\\)" "expected 3" { target *-*-* } 4 } */
