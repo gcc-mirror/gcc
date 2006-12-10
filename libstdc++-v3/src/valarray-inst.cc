c63bccc38c13dcae2253df963150c11a931bf3ba
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-12-10 16:47:27 +0000

    2006-12-10  Paolo Carlini  <pcarlini@suse.de>
    	* src/valarray-inst.cc (__gslice_to_index): Optimize performance.
    	* testsuite/performance/26_numerics/valarray_gslice_to_index.cc: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@119707 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/src/valarray-inst.cc
+++ libstdc++-v3/src/valarray-inst.cc
@@ -1,6 +1,7 @@
 // Explicit instantiation file.
 
-// Copyright (C) 2001, 2004, 2005 Free Software Foundation, Inc.
+// Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006
+// Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -69,40 +70,34 @@ _GLIBCXX_BEGIN_NAMESPACE(std)
                     const valarray<size_t>& __s, valarray<size_t>& __i)
   {
     // There are as much as dimensions as there are strides.
-    size_t __n = __l.size();
+    const size_t __n = __l.size();
 
-    // Get a buffer to hold current multi-index as we go through
-    // the gslice for the purpose of computing its linear-image.
-    size_t* const __t = static_cast<size_t*>
-      (__builtin_alloca(__n * sizeof (size_t)));
-    __valarray_fill(__t, __n, size_t(0));
+    // Holds current multi-index as we go through the gslice for the
+    // purpose of computing its linear-image.
+    valarray<size_t> __t(__l);
 
     // Note that this should match the product of all numbers appearing
     // in __l which describes the multidimensional sizes of the
-    // the generalized slice.
+    // generalized slice.
     const size_t __z = __i.size();
-    
+
     for (size_t __j = 0; __j < __z; ++__j)
       {
-        // Compute the linear-index image of (t_0, ... t_{n-1}).
-        // Normaly, we should use inner_product<>(), but we do it the
-        // the hard way here to avoid link-time can of worms.
-        size_t __a = __o;
-        for (size_t __k = 0; __k < __n; ++__k)
-          __a += __s[__k] * __t[__k];
+	// Compute the linear-index image of (t_0, ... t_{n-1}).
+	__i[__j] = __o;
 
-        __i[__j] = __a;
+	--__t[__n - 1];
+	__o += __s[__n - 1];
 
         // Process the next multi-index.  The loop ought to be
-        // backward since we're making a lexicagraphical visit.
-        ++__t[__n - 1];
-        for (size_t __k2 = __n - 1; __k2; --__k2)
+        // backward since we're making a lexicographical visit.
+        for (size_t __k2 = __n - 1; __k2 && !__t[__k2]; --__k2)
           {
-            if (__t[__k2] >= __l[__k2])
-              {
-                __t[__k2] = 0;
-                ++__t[__k2 - 1];
-              }
+	    __o -= __s[__k2] * __l[__k2];
+	    __t[__k2] = __l[__k2];
+
+	    --__t[__k2 - 1];
+	    __o += __s[__k2 - 1];
           }
       }
   }
