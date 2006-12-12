da32da74c73fc648fafe98916938543756f4edd5
Olivier Hainque <hainque@adacore.com>
2006-12-12 09:47:45 +0000

    mkfixinc.sh: Add "*-*-vxworks*" to the list of targets for which a no-op fixer is appropriate.
    2006-12-12  Olivier Hainque  <hainque@adacore.com>
    
    	* fixincludes/mkfixinc.sh: Add "*-*-vxworks*" to the list of
    	targets for which a no-op fixer is appropriate.
    
    From-SVN: r119769

--- fixincludes/mkfixinc.sh
+++ fixincludes/mkfixinc.sh
@@ -23,6 +23,7 @@ case $machine in
     i?86-*-mingw32* | \
     i?86-*-uwin* | \
     i?86-*-interix* | \
+    *-*-vxworks* | \
     powerpc-*-eabiaix* | \
     powerpc-*-eabisim* | \
     powerpc-*-eabi*    | \
