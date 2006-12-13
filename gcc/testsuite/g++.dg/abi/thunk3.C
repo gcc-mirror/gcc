4e37c006fb3948327ff04b9139202b5a7ae75ca9
jconner <jconner@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-12-13 02:16:19 +0000

    2006-12-12  Josh Conner  <jconner@apple.com>
    	* g++.dg/abi/thunk3.C: Use .weak_definition on darwin.
    	* g++.dg/abi/thunk4.C: Likewise.
    	* g++.dg/abi/rtti3.C: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@119815 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/abi/thunk3.C
+++ gcc/testsuite/g++.dg/abi/thunk3.C
@@ -1,5 +1,6 @@
 // { dg-require-weak "" }
-// { dg-final { scan-assembler-not ".weak\[\t \]_?_ZThn._N7Derived3FooEv" } }
+// { dg-final { scan-assembler-not ".weak\[\t \]_?_ZThn._N7Derived3FooEv" { target { ! { *-*-darwin* } } } } }
+// { dg-final { scan-assembler-not ".weak_definition\[\t \]_?_ZThn._N7Derived3FooEv" { target { *-*-darwin* } } } }
 
 struct Base 
 {
