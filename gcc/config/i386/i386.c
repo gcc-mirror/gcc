0a9ae7b110c0ea3fdc257500aa4058ce50e88318
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-12-13 18:07:47 +0000

    * config/i386/i386.h (FIRST_PSEUDO_REGISTER): Define to 53. 	(FIXED_REGISTERS, CALL_USED_REGISTERS): Remove dir register. 	(REG_ALLOC_ORDER): Remove one element due to removal of dir register. 	(FRAME_POINTER_REGNUM): Update register number to 20. 	(REG_CLASS_CONTENTS): Update contents due to removed dir register. 	(HI_REGISTER_NAMES): Remove "dirflag".
    	* config/i386/i386.c (regclass_map): Remove dirflag entry.
    	(dbx_register_map, dbx64_register_map, svr4_dbx_register_map):
    	Remove "dir" entry.
    	(ix86_md_asm_clobbers): Remove "dirflag" default asm clobber.
    
    	* config/i386/i386.md (R10_REG, R11_REG): Renumber.
    	(sse_prologue_save, *sse_prologue_save_insn): Renumber
    	hardcoded SSE register numbers.
    
    	* config/i386/mmx.md (mmx_emms, mmx_femms): Renumber
    	hardcoded MMX register numbers.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@119839 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -1052,8 +1052,8 @@ enum reg_class const regclass_map[FIRST_PSEUDO_REGISTER] =
   FLOAT_REGS, FLOAT_REGS, FLOAT_REGS, FLOAT_REGS,
   /* arg pointer */
   NON_Q_REGS,
-  /* flags, fpsr, fpcr, dirflag, frame */
-  NO_REGS, NO_REGS, NO_REGS, NO_REGS, NON_Q_REGS,
+  /* flags, fpsr, fpcr, frame */
+  NO_REGS, NO_REGS, NO_REGS, NON_Q_REGS,
   SSE_REGS, SSE_REGS, SSE_REGS, SSE_REGS, SSE_REGS, SSE_REGS,
   SSE_REGS, SSE_REGS,
   MMX_REGS, MMX_REGS, MMX_REGS, MMX_REGS, MMX_REGS, MMX_REGS,
@@ -1070,7 +1070,7 @@ int const dbx_register_map[FIRST_PSEUDO_REGISTER] =
 {
   0, 2, 1, 3, 6, 7, 4, 5,		/* general regs */
   12, 13, 14, 15, 16, 17, 18, 19,	/* fp regs */
-  -1, -1, -1, -1, -1, -1,		/* arg, flags, fpsr, fpcr, dir, frame */
+  -1, -1, -1, -1, -1,			/* arg, flags, fpsr, fpcr, frame */
   21, 22, 23, 24, 25, 26, 27, 28,	/* SSE */
   29, 30, 31, 32, 33, 34, 35, 36,       /* MMX */
   -1, -1, -1, -1, -1, -1, -1, -1,	/* extended integer registers */
@@ -1093,7 +1093,7 @@ int const dbx64_register_map[FIRST_PSEUDO_REGISTER] =
 {
   0, 1, 2, 3, 4, 5, 6, 7,		/* general regs */
   33, 34, 35, 36, 37, 38, 39, 40,	/* fp regs */
-  -1, -1, -1, -1, -1, -1,		/* arg, flags, fpsr, fpcr, dir, frame */
+  -1, -1, -1, -1, -1,			/* arg, flags, fpsr, fpcr, frame */
   17, 18, 19, 20, 21, 22, 23, 24,	/* SSE */
   41, 42, 43, 44, 45, 46, 47, 48,       /* MMX */
   8,9,10,11,12,13,14,15,		/* extended integer registers */
@@ -1158,7 +1158,7 @@ int const svr4_dbx_register_map[FIRST_PSEUDO_REGISTER] =
 {
   0, 2, 1, 3, 6, 7, 5, 4,		/* general regs */
   11, 12, 13, 14, 15, 16, 17, 18,	/* fp regs */
-  -1, 9, -1, -1, -1, -1,		/* arg, flags, fpsr, fpcr, dir, frame */
+  -1, 9, -1, -1, -1,			/* arg, flags, fpsr, fpcr, frame */
   21, 22, 23, 24, 25, 26, 27, 28,	/* SSE registers */
   29, 30, 31, 32, 33, 34, 35, 36,	/* MMX registers */
   -1, -1, -1, -1, -1, -1, -1, -1,	/* extended integer registers */
@@ -19926,8 +19926,6 @@ ix86_md_asm_clobbers (tree outputs ATTRIBUTE_UNUSED,
   clobbers = tree_cons (NULL_TREE, build_string (5, "flags"),
 			clobbers);
   clobbers = tree_cons (NULL_TREE, build_string (4, "fpsr"),
-			clobbers);
-  clobbers = tree_cons (NULL_TREE, build_string (7, "dirflag"),
 			clobbers);
   return clobbers;
 }
