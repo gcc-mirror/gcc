28f4586bf8676b0c6a44d239e366432d5e1fbe1b
Richard Guenther <rguenther@suse.de>
2006-12-14 13:14:23 +0000

    re PR middle-end/30197 (cexp ( __complex__ ( 0, x ) ) can be folded to cexpi (x))
    2006-12-14  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/30197
    	* builtins.c (fold_builtin_cexp): New function to fold cexp
    	to cexpi and exp parts.
    	* tree-ssa-pre.c (try_combine_conversion): Also handle
    	REALPART_EXPR and IMAGPART_EXPR.
    
    	* gcc.c-torture/execute/complex-1.c: Fix function name.
    	* gcc.dg/builtins-61.c: New testcase.
    
    From-SVN: r119860

---
+++ gcc/testsuite/gcc.dg/builtins-61.c
@@ -0,0 +1,31 @@
+/* { dg-do compile } */
+/* { dg-options "-O -ffast-math -fdump-tree-optimized" } */
+
+double test1 (double x)
+{
+  return __real __builtin_cexp(x * (__extension__ 1.0iF));
+}
+
+double test2(double x)
+{
+  return __imag __builtin_cexp((__extension__ 1.0iF) * x);
+}
+
+double test3(double x)
+{
+  _Complex c = __builtin_cexp(x * (__extension__ 1.0iF));
+  return __imag c + __real c;
+}
+
+double test4(double x, double y)
+{
+  _Complex c = __builtin_cexp(x);
+  x = __builtin_exp (x);
+  return x - __real c;
+}
+
+/* { dg-final { scan-tree-dump "cexpi" "optimized" } } */
+/* { dg-final { scan-tree-dump "sin" "optimized" } } */
+/* { dg-final { scan-tree-dump "cos" "optimized" } } */
+/* { dg-final { scan-tree-dump "return 0.0" "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
