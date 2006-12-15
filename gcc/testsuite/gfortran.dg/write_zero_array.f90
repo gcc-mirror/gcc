7a932987012990a94bf54b025d6af6512e093156
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2006-12-15 19:58:10 +0000

    re PR fortran/30145 (Fortran 90: write statement fails to ignore zero-sized array...)
    2006-12-15  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libfortran/30145
    	* gfortran.dg/write_zero_array: New test.
    
    From-SVN: r119943

---
+++ gcc/testsuite/gfortran.dg/write_zero_array.f90
@@ -0,0 +1,17 @@
+! { dg-do run }
+! PR30145 write statement fails to ignore zero-sized array
+! Test case from PR, submitted by Jerry DeLisle  <jvdelisle@gcc.gnu.org>
+program zeros
+ implicit none
+ character(20) :: msg = ""
+ integer :: itemp(10) = 0
+ integer :: ics
+ !This was OK
+ write(msg,*) 'itemp(6:0) = ',itemp(6:0),'a'
+ if (msg /= " itemp(6:0) = a") call abort()
+ !This did not work before patch, segfaulted
+ ics=6
+ write(msg,*) 'itemp(ics:0) = ',itemp(ics:0),'a'
+ if (msg /= " itemp(ics:0) = a") call abort()
+end program zeros
+
