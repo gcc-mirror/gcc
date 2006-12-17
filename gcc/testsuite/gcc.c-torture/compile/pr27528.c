111780f449efcd16b5f48015963382f39d2606e1
Eric Botcazou <ebotcazou@libertysurf.fr>
2006-12-17 20:58:54 +0100

    pr27528.c: Use empty templates.
    	* gcc.c-torture/compile/pr27528.c: Use empty templates.
    
    From-SVN: r119992

--- gcc/testsuite/gcc.c-torture/compile/pr27528.c
+++ gcc/testsuite/gcc.c-torture/compile/pr27528.c
@@ -6,13 +6,13 @@ int x[2] = { 1, 2 };
 static inline void __attribute__((__always_inline__))
 insn1 (int x)
 {
-  asm volatile ("# %0 %1" :: "n" (x), "i" (x));
+  asm volatile ("" :: "n" (x), "i" (x));
 }
 
 static inline void __attribute__((__always_inline__))
 insn2 (const void *x)
 {
-  asm volatile ("# %0 %1" :: "s" (x), "i" (x));
+  asm volatile ("" :: "s" (x), "i" (x));
 }
 #endif
 
@@ -29,10 +29,10 @@ foo (void)
   insn2 (&x[1]);
   insn2 ("string");
 #endif
-  asm volatile ("# %0 %1" :: "s" (x), "i" (x));
+  asm volatile ("" :: "s" (x), "i" (x));
   /* At the time of writing, &x[1] is decomposed before reaching expand
      when compiling with -O0.  */
-  asm volatile ("# %0 %1" :: "s" ("string"), "i" ("string"));
-  asm volatile ("# %0 %1" :: "s" (__FILE__), "i" (__FILE__));
-  asm volatile ("# %0 %1" :: "s" (__FUNCTION__), "i" (__FUNCTION__));
+  asm volatile ("" :: "s" ("string"), "i" ("string"));
+  asm volatile ("" :: "s" (__FILE__), "i" (__FILE__));
+  asm volatile ("" :: "s" (__FUNCTION__), "i" (__FUNCTION__));
 }
