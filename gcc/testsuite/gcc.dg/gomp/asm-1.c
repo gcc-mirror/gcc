58d12b177863d5fc83b7b1e77d3b36cb4cff3ac5
Jakub Jelinek <jakub@redhat.com>
2006-12-21 14:18:52 +0100

    re PR middle-end/30262 (ICE with nested fn accessed var in asm "m" constraint)
    	PR middle-end/30262
    	PR middle-end/30263
    	* tree-nested.c (walk_asm_expr): New function.
    	(walk_stmts): Use it for ASM_EXPR.
    
    	* gcc.c-torture/execute/20061220-1.c: New test.
    	* gcc.dg/gomp/asm-1.c: New test.
    
    From-SVN: r120107

---
+++ gcc/testsuite/gcc.dg/gomp/asm-1.c
@@ -0,0 +1,19 @@
+/* PR middle-end/30263 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fopenmp" } */
+
+void
+foo (void)
+{
+  int s0, s1 = 5, s2 = 6;
+  int p0, p1, p2;
+  int f0 = 4, f1 = 5, f2 = 6;
+#pragma omp parallel shared (s0, s1, s2) private (p0, p1, p2) \
+	    firstprivate (f0, f1, f2)
+  {
+    asm ("" : "=m" (p0) : "m" (p1), "mr" (p2));
+    if (omp_get_thread_num () == 0)
+      asm ("" : "=m" (s0) : "m" (s1), "mr" (s2));
+    asm ("" : "=m" (f0) : "m" (f1), "mr" (f2));
+  }
+}
