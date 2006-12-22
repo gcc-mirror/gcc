2872c0668dd6017f52ac3eb7c84e7a9822b18085
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-12-22 20:49:00 +0000

    2006-12-22  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/25818
    	* trans-array.c (gfc_trans_g77_array): If the variable is
    	optional or not always present, make the statement conditional
    	on presence of the argument.
    	* gfortran.h : Add symbol_attribute not_always_present.
    	* resolve.c (check_argument_lists): New function to check if
    	arguments are not present in all entries.
    
    	PR fortran/30084
    	* module.c (mio_component_ref): Move treatment of unique name
    	variables, during output, to fix_mio_expr.
    	(fix_mio_expr): New function that fixes defective expressions
    	before they are written to the module file.
    	(mio_expr): Call the new function.
    	(resolve_entries): Call check_argument_lists.
    
    2006-12-22  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/25818
    	* gfortran.dg/entry_array_specs_2.f: New test.
    
    	PR fortran/30084
    	* gfortran.dg/nested_modules_6.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@120155 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/nested_modules_6.f90
@@ -0,0 +1,35 @@
+! { dg-do compile }
+! Test the patch for PR30084 in which the reference to SIZE
+! in function diag caused a segfault in module.c.
+!
+! Contributed by Troban Trumsko <trumsko@yahoo.com>
+! and reduced by Steve Kargl <kargl@gcc.gnu.org>
+!
+module tao_random_numbers
+  integer, dimension(10) :: s_buffer
+  integer :: s_last = size (s_buffer)
+end module tao_random_numbers
+
+module linalg
+  contains
+  function diag (a) result (d)
+    real, dimension(:,:), intent(in) :: a
+    real, dimension(min(size(a,dim=1),size(a,dim=2))) :: d
+    integer :: i
+    do i = 1, min(size(a, dim = 1), size(a, dim = 2))
+       d(i) = a(i,i)
+    end do
+  end function diag
+end module linalg
+
+module vamp_rest
+  use tao_random_numbers
+  use linalg
+end module vamp_rest
+
+  use vamp_rest
+  real :: x(2, 2) = reshape ([1.,2.,3.,4.], [2,2]) ! { dg-warning "nonstandard" } 
+  print *, s_last
+  print *, diag (x)
+end
+! { dg-final { cleanup-modules "tao_random_numbers linalg vamp_rest" } }
