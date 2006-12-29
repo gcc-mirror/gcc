9d30f270db7f21cd6a501dc94b28a76831444e87
Jakub Jelinek <jakub@redhat.com>
2006-12-29 09:15:08 +0100

    re PR preprocessor/29612 (gcc --save-temps does not give "multi-character character constant" error)
    	PR preprocessor/29612
    	* directives.c (do_linemarker): Set pfile->buffer->sysp always, not
    	only when new_sysp is non-zero.
    
    	* gcc.dg/cpp/pr29612-1.c: New test.
    	* gcc.dg/cpp/pr29612-2.c: New test.
    
    From-SVN: r120257

---
+++ gcc/testsuite/gcc.dg/cpp/pr29612-2.c
@@ -0,0 +1,18 @@
+/* PR preprocessor/29612 */
+/* { dg-do preprocess } */
+/* { dg-options "-Wtraditional -fno-show-column" } */
+
+# 6 "pr29612-2.c"
+
+#if 1U /* { dg-warning "traditional C rejects" "numeric constant suffix" } */
+#endif
+
+# 1 "foo.h" 1 3
+
+#if 1U
+#endif /* No warning in system header.  */
+
+# 16 "pr29612-2.c" 2
+
+#if 1U /* { dg-warning "traditional C rejects" "numeric constant suffix" } */
+#endif
