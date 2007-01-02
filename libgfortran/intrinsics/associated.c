cf7b154f7d195e844115ec85aa9502a3df49bcd1
Tobias Burnus <burnus@net-b.de>
2007-01-02 13:48:14 +0100

    re PR fortran/30238 (associated(null-pointer, null-pointer) returns .true.)
    fortran/
    2007-01-02  Tobias Burnus  <burnus@net-b.de>
    
           PR fortran/30238
           * trans-intrinsic.c: Check for associated(NULL,NULL).
    
    libgfortran/
    2007-01-02  Tobias Burnus  <burnus@net-b.de>
    
           * intrinsics/associated.c: Check for associated(NULL,NULL).
    
    testsuite/
    2007-01-02  Tobias Burnus  <burnus@net-b.de>
    
           PR fortran/30238
           * gfortran.dg/associated_1.f90: Add test for associated(NULL,NULL).
           * gfortran.dg/associated_2.f90: Add test for associated(NULL,NULL).
    
    From-SVN: r120338

--- libgfortran/intrinsics/associated.c
+++ libgfortran/intrinsics/associated.c
@@ -39,6 +39,8 @@ associated (const gfc_array_void *pointer, const gfc_array_void *target)
 {
   int n, rank;
 
+  if (GFC_DESCRIPTOR_DATA (pointer) == NULL)
+    return 0;
   if (GFC_DESCRIPTOR_DATA (pointer) != GFC_DESCRIPTOR_DATA (target))
     return 0;
   if (GFC_DESCRIPTOR_DTYPE (pointer) != GFC_DESCRIPTOR_DTYPE (target))
