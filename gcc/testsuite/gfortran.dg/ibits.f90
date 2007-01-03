131ffbe16bb6b2b7b8bb424fe13f37c64d86afc8
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-01-03 03:45:50 +0000

    2007-01-03  Steven G. Kargl  <kargls@comcast.net>
    	* gfortran.dg/ibits.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@120380 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/ibits.f90
@@ -0,0 +1,15 @@
+! { dg-do run }
+! Test that the mask is properly converted to the kind type of j in ibits.
+program ibits_test
+  implicit none
+  integer(8), parameter :: n = z'00000000FFFFFFFF'
+  integer(8) i,j,k,m
+  j = 1
+  do i=1,70
+     j = ishft(j,1) + 1
+     k = ibits(j, 0, 32)
+     m = iand(j,n)
+     if (k /= m) call abort
+  end do
+end program ibits_test
+
