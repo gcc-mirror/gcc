f5f67da7d67cf4e696a9e6584b3effe20babaa05
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-01-03 08:04:11 +0000

    PR middle-end/30286 	* gcc.dg/pr30286.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@120387 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr30286.c
@@ -0,0 +1,37 @@
+/* PR middle-end/30286 */
+/* { dg-do run } */
+/* { dg-options "-O2 -ftrapv" } */
+
+extern void abort (void);
+struct S { struct S *s; };
+struct T { struct S *t[25]; };
+
+void
+__attribute__((noinline))
+foo (int i, struct T *x, struct S *y)
+{
+  int j;
+  for (j = 14; j > i; j--)
+    x->t[j] = y->s;
+}
+
+int
+main (void)
+{
+  struct S s;
+  struct T t;
+  int i;
+
+  s.s = &s;
+  __builtin_memset (&t, 0, sizeof (t));
+  foo (6, &t, &s);
+  for (i = 0; i < 25; i++)
+    if (t.t[i] != ((i > 6 && i <= 14) ? &s : (struct S *) 0))
+      abort ();
+  __builtin_memset (&t, 0, sizeof (t));
+  foo (-1, &t, &s);
+  for (i = 0; i < 25; i++)
+    if (t.t[i] != ((i >= 0 && i <= 14) ? &s : (struct S *) 0))
+      abort ();
+  return 0;
+}
