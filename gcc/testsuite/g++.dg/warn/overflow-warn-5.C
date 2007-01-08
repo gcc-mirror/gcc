08dfbb2b6a4622eb1217ad3bba98b7d40305b87d
manu <manu@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-01-08 21:04:26 +0000

    2007-01-08  Manuel Lopez-Ibanez  <manu@gcc.gnu.org>
    testsuite/
    	* g++.dg/warn/overflow-warn-1.C: Fix testcase.
    	* g++.dg/warn/overflow-warn-3.C: Likewise.
    	* g++.dg/warn/overflow-warn-4.C: Likewise.
    	* g++.dg/warn/overflow-warn-5.C: Likewise.
    	* g++.dg/warn/overflow-warn-6.C: Likewise.
    	* g++.dg/warn/Woverflow-1.C: Likewise.
    	* g++.dg/warn/Woverflow-2.C: Likewise.
    	* g++.dg/warn/Woverflow-3.C: Likewise.
    	* g++.dg/warn/multiple-overflow-warn-2.C: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@120588 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/warn/overflow-warn-5.C
+++ gcc/testsuite/g++.dg/warn/overflow-warn-5.C
@@ -5,17 +5,3 @@
 unsigned char rx_async(unsigned char p) {
     return p & 512; /* { dg-warning "overflow in implicit constant conversion" } */
 }
-/* PR c/27273 */
-/* { dg-do compile } */
-/* { dg-options "-Woverflow" } */
-
-unsigned char rx_async(unsigned char p) {
-    return p & 512; /* { dg-warning "overflow in implicit constant conversion" } */
-}
-/* PR c/27273 */
-/* { dg-do compile } */
-/* { dg-options "-Woverflow" } */
-
-unsigned char rx_async(unsigned char p) {
-    return p & 512; /* { dg-warning "overflow in implicit constant conversion" } */
-}
