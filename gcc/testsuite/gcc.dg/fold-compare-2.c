9112c6d30065fba63fa5d2aec9bf0a5ad1a6af91
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-01-10 21:07:38 +0000

    ./: 	* fold-const.c (fold_comparison): Fold comparisons like (x * 	1000 < 0) to (x < 0). testsuite/: 	* gcc.dg/fold-compare-2.c: New test case for fold_comparison.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@120649 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/fold-compare-2.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-vrp" } */
+
+extern void abort (void);
+
+int a;
+
+int
+main(void)
+{
+  if (a * 1000 < 0)
+    abort ();
+  if (a * -43 > 0)
+    abort ();
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "Removing basic block" 1 "vrp1" } } */
