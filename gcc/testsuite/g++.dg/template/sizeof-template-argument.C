6f9f76e336e9ce7a1c070351e4d21e6b84c0e82c
Simon Martin <simartin@users.sourceforge.net>
2007-01-11 19:46:03 +0000

    re PR c++/29573 (ICE after parse error in template argument)
    2007-01-11  Simon Martin  <simartin@users.sourceforge.net>
    
    	PR c++/29573
    	* tree.c (cp_tree_equal): Properly handle MODOP_EXPR trees.
    
    From-SVN: r120683

---
+++ gcc/testsuite/g++.dg/template/sizeof-template-argument.C
@@ -0,0 +1,15 @@
+/* This used to ICE (PR c++/29573) */
+/* { dg-do "compile" } */
+
+template<int> struct A {};
+
+template<typename> struct B : A <sizeof(=)> {}; /* { dg-error "parse error in template argument list" } */
+
+template<typename> struct C : A <sizeof(=)> {}; /* { dg-error "parse error in template argument list" } */
+
+int a;
+
+template<typename> struct D : A <sizeof(a=1)> {}; /* This used to ICE as well. */
+
+template<typename> struct E : A <sizeof(a=1)> {}; /* This used to ICE as well. */
+
