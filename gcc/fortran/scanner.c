4d499824ede45b788cd35a4d9cd6c560fce7f11d
Brooks Moses <brooks.moses@codesourcery.com>
2007-01-11 09:07:36 +0000

    simplify.c: Update copyright to 2007.
    * simplify.c: Update copyright to 2007.
    * scanner.c: Same.
    
    From-SVN: r120668

--- gcc/fortran/scanner.c
+++ gcc/fortran/scanner.c
@@ -1,5 +1,5 @@
 /* Character scanner.
-   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007
    Free Software Foundation, Inc.
    Contributed by Andy Vaught
 
