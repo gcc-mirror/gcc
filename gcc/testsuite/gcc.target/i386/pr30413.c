37fc8424ac5a0896dc43a0d506d434b60c55452d
Uros Bizjak <ubizjak@gmail.com>
2007-01-14 12:14:20 +0100

    re PR target/30413 (%z produces ICE for char operands)
            PR target/30413
            * config/i386/i386.c (print_operand) ['z']: Output 'b' for
            operands of size 1.
    
    testsuite/ChangeLog:
    
            PR target/30413
            * gcc.target/i386/pr30413.c: New test.
    
    From-SVN: r120769

---
+++ gcc/testsuite/gcc.target/i386/pr30413.c
@@ -0,0 +1,18 @@
+/* { dg-do run { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2" } */
+
+extern void abort (void);
+
+int test() {
+  char a, b = -1;
+  asm volatile ("mov%z0 %1, %0" : "=q"(a) : "m"(b));
+  return a;
+}
+
+int main()
+{
+  if (test() != -1)
+    abort();
+
+  return 0;
+}
