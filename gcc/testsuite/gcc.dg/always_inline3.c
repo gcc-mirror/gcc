4f69811862d880a8d2340500dbf7a7aead2d71f3
Jan Hubicka <jh@suse.cz>
2007-01-14 19:23:31 +0100

    always_inline3.c: Update for pre-inline tail recursion elimination.
    	* gcc.dg/always_inline3.c: Update for pre-inline tail recursion
    	elimination.
    
    From-SVN: r120777

--- gcc/testsuite/gcc.dg/always_inline3.c
+++ gcc/testsuite/gcc.dg/always_inline3.c
@@ -1,10 +1,11 @@
 /* { dg-do compile } */
 /* { dg-options "-Winline -O2" } */
-void do_something_evil (void);
+int do_something_evil (void);
 inline __attribute__ ((always_inline)) void
 q2(void)
 { 				/* { dg-error "recursive" "" } */
-  do_something_evil ();
+  if (do_something_evil ())
+    return;
   q2(); 			/* { dg-error "called from here" "" } */
-  q2(); 			/* { dg-error "called from here" "" } */
+  q2(); /* With -O2 we don't warn here, it is eliminated by tail recursion.  */
 }
