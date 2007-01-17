9dd8004e9108e08d45b25781bc6d44a624f690b8
Paul Thomas <pault@gcc.gnu.org>
2007-01-17 17:33:35 +0000

    re PR fortran/30476 ([Regression 4.2, 4.3] Via other module imported generic interface rejected)
    2007-01-17  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/30476
    	* module.c (load_generic_interfaces): Make the marking of the
    	symbol as ambiguous conditional on the module names being
    	different.
    	(write_generic): Ensure that the generic interface has a
    	non-NULL module field.
    
    2007-01-17  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/30476
    	* gfortran.dg/generic_12.f90: New test.
    
    From-SVN: r120860

---
+++ gcc/testsuite/gfortran.dg/generic_12.f90
@@ -0,0 +1,32 @@
+! { dg-do compile }
+! Test the fix for PR30476 in which the generic interface hello
+! was found incorrectly to be ambiguous.
+!
+!Contributed by Tobias Burnus <burnus@gcc.gnu.org>
+!
+SUBROUTINE hello_x(dum)
+   IMPLICIT NONE
+   INTEGER :: dum
+   WRITE(0,*) "Hello world: ", dum
+END SUBROUTINE hello_x
+
+MODULE interfaces
+IMPLICIT NONE
+INTERFACE hello
+   SUBROUTINE hello_x(dum)
+      IMPLICIT NONE
+      INTEGER :: dum
+   END SUBROUTINE hello_x
+END INTERFACE
+END MODULE interfaces
+
+MODULE global_module
+  USE interfaces
+END MODULE global_module
+
+PROGRAM main
+  USE global_module
+  IMPLICIT NONE
+  CALL hello(10)
+END PROGRAM main
+! { dg-final { cleanup-modules "interfaces global_module" } }
