47b41fa34abeff893fb1358c8154e681e68fdddd
Nathan Sidwell <nathan@codesourcery.com>
2007-01-18 19:29:48 +0000

    200x-xx-xx Nathan Sidwell <nathan@codesourcery.com>
    gcc/
    200x-xx-xx  Nathan Sidwell  <nathan@codesourcery.com>
    
    	* config.gcc (m68k-*-aout*, m68k-*-coff*, m68020-*-elf*, m68k-*-elf*)
    	(m68k-*-uclinuxoldabi, m68k-*-uclinux*, m68k-*-rtems*): Add t-floatlib
    	to $tmake_file.
    	* config/m68k/t-floatlib: New file, extracting common code from...
    	* config/m68k/t-m68kbare, config/m68k/t-m68kelf,
    	* config/m68k/t-uclinux: Here.
    	* config/m68k/fpgnulib.c: Do not compile extendeed precision
    	routines on ColdFire targets.
    
    From-SVN: r120917

--- gcc/config/m68k/fpgnulib.c
+++ gcc/config/m68k/fpgnulib.c
@@ -361,6 +361,10 @@ __fixsfsi (float a1)
 
 #else /* EXTFLOAT */
 
+/* We do not need these routines for coldfire, as it has no extended
+   float format. */
+#if !defined (__mcoldfire__)
+
 /* Primitive extended precision floating point support.
 
    We assume all numbers are normalized, don't do any rounding, etc.  */
@@ -569,4 +573,5 @@ __gexf2 (long double x1, long double x2)
   return __cmpdf2 ((double) x1, (double) x2);
 }
 
+#endif /* !__mcoldfire__ */
 #endif /* EXTFLOAT */
