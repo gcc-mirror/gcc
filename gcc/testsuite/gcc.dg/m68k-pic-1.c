7dd6ff6d8cec359d8eab76708d34d939a9eb3641
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-01-18 19:33:01 +0000

    gcc/ 	* config/m68k/m68k.md (cmpsi): Remove outdated flag_pic handling.
    gcc/testsuite/
    200x-xx-xx  Nathan Sidwell  <nathan@codesourcery.com>
    
    	* gcc.dg/m68k-pic-1.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@120919 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/m68k-pic-1.c
@@ -0,0 +1,18 @@
+/* { dg-do compile { target m68k-*-* } }  */
+/* { dg-options "-O2 -fpic" }  */
+
+extern void Foo (void *);
+
+char *ary[] = {"a", "b", "c", "d", "e"};
+
+void Bar (void)
+{
+  int cnt = 0;
+
+  for (cnt = 0; cnt < 4;  ++cnt)
+    {
+      char *ptr = ary[cnt];
+
+      Foo (&ptr);
+    }
+}
