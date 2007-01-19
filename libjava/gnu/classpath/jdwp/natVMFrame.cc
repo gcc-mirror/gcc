9949d6957412d24e00da411bba894544efe68d1c
kseitz <kseitz@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-01-19 17:46:57 +0000

    * gnu/classpath/jdwp/natVMFrame.cc: Mark unused parameters with         MAYBE_UNUSED.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@120972 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/gnu/classpath/jdwp/natVMFrame.cc
+++ libjava/gnu/classpath/jdwp/natVMFrame.cc
@@ -1,6 +1,6 @@
 // natFrame.cc -- native support for VMFrame.java
 
-/* Copyright (C) 2006 Free Software Foundation
+/* Copyright (C) 2006, 2007 Free Software Foundation
 
    This file is part of libgcj.
 
@@ -9,18 +9,20 @@ Libgcj License. Please consult the file "LIBGCJ_LICENSE" for
 details. */
 
 #include <gcj/cni.h>
+#include <jvm.h>
 
 #include <gnu/classpath/jdwp/VMFrame.h>
 
 using namespace java::lang;
 
 Object*
-gnu::classpath::jdwp::VMFrame::getValue (jint slot)
+gnu::classpath::jdwp::VMFrame::getValue (MAYBE_UNUSED jint slot)
 {
   return 0;
 }
 
 void
-gnu::classpath::jdwp::VMFrame::setValue (jint slot, Object* value)
+gnu::classpath::jdwp::VMFrame::setValue (MAYBE_UNUSED jint slot,
+					 MAYBE_UNUSED Object* value)
 {
 }
