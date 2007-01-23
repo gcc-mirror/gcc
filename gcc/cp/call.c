fabdc1d15e571f29ef8a47a134ab04b0cabcd321
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-01-23 18:17:40 +0000

    * call.c (null_ptr_cst_p): Replace use of TREE_CONSTANT_OVERFLOW with 	TREE_OVERFLOW. 	* typeck.c (ignore_overflows): Remove the remaining uses of 	TREE_CONSTANT_OVERFLOW.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@121084 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/call.c
+++ gcc/cp/call.c
@@ -1,6 +1,7 @@
 /* Functions related to invoking methods and overloaded functions.
    Copyright (C) 1987, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006 Free Software Foundation, Inc.
+   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007
+   Free Software Foundation, Inc.
    Contributed by Michael Tiemann (tiemann@cygnus.com) and
    modified by Brendan Kehoe (brendan@cygnus.com).
 
@@ -432,7 +433,7 @@ null_ptr_cst_p (tree t)
   if (CP_INTEGRAL_TYPE_P (TREE_TYPE (t)) && integer_zerop (t))
     {
       STRIP_NOPS (t);
-      if (!TREE_CONSTANT_OVERFLOW (t))
+      if (!TREE_OVERFLOW (t))
 	return true;
     }
   return false;
