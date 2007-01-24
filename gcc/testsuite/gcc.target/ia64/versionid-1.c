4e586241948213d8f1539a064dfaa910a081e71e
sje <sje@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-01-24 19:47:31 +0000

    * gcc.target/ia64/versionid-1.c: New test. 	* gcc.target/ia64/versionid-2.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@121130 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/ia64/versionid-1.c
@@ -0,0 +1,10 @@
+/* { dg-do compile { target ia64-*-hpux* } } */
+
+extern int foo () __attribute__((version_id ("20040821")));
+
+int bar(int i)
+{
+	return (foo() + 1);
+}
+
+/* { dg-final { scan-assembler "alias.*foo.*foo{20040821}" } } */
---
+++ gcc/testsuite/gcc.target/ia64/versionid-2.c
@@ -0,0 +1,10 @@
+/* { dg-do compile { target ia64-*-hpux* } } */
+
+extern int foo () __attribute__((version_id ("20040821")));
+
+int foo(int i)
+{
+	return (1);
+}
+
+/* { dg-final { scan-assembler "alias.*foo.*foo{20040821}" } } */
