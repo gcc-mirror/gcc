c3eaf28fd1b4875d0d7aa8bb6f6a470bd013fb87
kseitz <kseitz@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-01-25 18:04:01 +0000

    * jvmti.cc (_Jv_JVMTI_GetMethodName): New function.         (_Jv_JVMTI_Interface): Define GetMethodName.         * testsuite/libjava.jvmti/getmethodname.java: New file.         * testsuite/libjava.jvmti/natgetmethodname.cc: New file.         * testsuite/libjava.jvmti/getmethodname.out: New file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@121179 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/testsuite/libjava.jvmti/getmethodname.java
@@ -0,0 +1,10 @@
+public class getmethodname
+{
+  public static native void do_getmethodname_tests ();
+
+  public static void main (String[] args)
+  {
+    System.out.println ("JVMTI GetMethodName tests");
+    do_getmethodname_tests ();
+  }
+}
