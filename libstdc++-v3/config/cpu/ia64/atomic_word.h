aa76348cb0868f7c538abc297e5f79107a1e952f
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-01-26 18:00:42 +0000

    2007-01-26  Paolo Carlini  <pcarlini@suse.de>
    	PR libstdc++/30586
    	* config/cpu/ia64/atomic_word.h: Just include <bits/cxxabi_tweaks.h>.
    	* testsuite/abi/30586.cc: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@121209 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/config/cpu/ia64/atomic_word.h
+++ libstdc++-v3/config/cpu/ia64/atomic_word.h
@@ -1,6 +1,6 @@
 // Low-level type for atomic operations -*- C++ -*-
 
-// Copyright (C) 2004 Free Software Foundation, Inc.
+// Copyright (C) 2004, 2005, 2006, 2007 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -30,7 +30,7 @@
 #ifndef _GLIBCXX_ATOMIC_WORD_H
 #define _GLIBCXX_ATOMIC_WORD_H	1
 
-#include <cxxabi.h>
+#include <bits/cxxabi_tweaks.h>
 
 typedef int _Atomic_word;
 
