2e6a83a7924e9079240b7a60206c39806c46b32d
Steven G. Kargl <kargl@gcc.gnu.org>
2007-01-26 23:36:11 +0000

    Steven G.
    2007-01-26  Steven Bosscher  <stevenb.gcc@gmail.com>
    	        Steven G. Kargl  <kargl@gcc.gnu,org>
    
    	* testsuite/gfortran.dg/backslash_3.f: New test.
    
    	* fortran/io.c (next_char): Deal with backslash escaped characters.
    	Issue warnings in non -std=gnu cases.
    	* fortran/primary.c (next_string_char): Issue warnings in non
    
    From-SVN: r121218

---
+++ gcc/testsuite/gfortran.dg/backslash_3.f
@@ -0,0 +1,25 @@
+C { dg-do run }
+C PR fortran/30278
+      program a
+      character(len=1), parameter  :: c1 = char(8), c2 = char(92)
+      character(len=35) str1, str2
+      character(len=37) :: str4, str3
+
+      open(10, status='scratch')
+      write(10, 100)
+      rewind(10)
+      read(10,'(A34)') str1
+      str2 = 'Does ' // c1 // 'ackslash result in ' // c1 // 'ackslash'
+      if (str1 .ne. str2) call abort
+
+      rewind(10)
+      write (10, 200)
+      rewind(10)
+      read(10,'(A37)') str3
+      str4 = 'Does ' //c2// 'backslash result in ' //c2// 'backslash'
+      if (str3 .ne. str4) call abort
+
+      stop
+ 100  format ('Does \backslash result in \backslash')
+ 200  format ('Does \\backslash result in \\backslash')
+      end
