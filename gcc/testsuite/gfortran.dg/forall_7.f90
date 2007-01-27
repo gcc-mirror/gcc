1a1fd7149459b0380c85f91e0d3378703b66b104
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-01-27 19:02:25 +0000

    * trans-stmt.c (forall_info): Replace the next_nest and outer 	fields that previously implemented a doubly-linked list with a 	single prev_nest field (singly-linked list). 	(gfc_trans_nested_forall_loop): The nested_forall_info argument 	now denotes the innermost FORALL in the loop nest. 	(compute_overall_iter_number): Use prev_nest instead of next_nest. 	(gfc_trans_forall_1): Link/cons the new "info" to the head of the 	nested_forall_info linked list.  Free the current "info" when done.
    	* gfortran.dg/forall_7.f90: New test case.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@121236 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/forall_7.f90
@@ -0,0 +1,16 @@
+! { dg-do run }
+  integer :: a(10,10)
+  integer :: tot
+  a(:,:) = 0
+  forall (i = 1:10)
+    forall (j = 1:10)
+      a(i,j) = 1
+    end forall
+    forall (k = 1:10)
+      a(i,k) = a(i,k) + 1
+    end forall
+  end forall
+  tot = sum(a(:,:))
+! print *, tot
+  if (tot .ne. 200) call abort ()
+end
