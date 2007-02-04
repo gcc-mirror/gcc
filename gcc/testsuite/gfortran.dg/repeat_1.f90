bfe7d4b13a2ddf4f562c4e0a3d1ff1a2b7118910
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-02-04 21:33:10 +0000

    PR fortran/30611
    	* trans-intrinsic.c (gfc_conv_intrinsic_repeat): Evaluate
    	arguments only once. Generate check that NCOPIES argument is not
    	negative.
    
    	* intrinsics/string_intrinsics.c (string_repeat): Don't check
    	if ncopies is negative.
    
    	* gcc/testsuite/gfortran.dg/repeat_1.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@121581 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/repeat_1.f90
@@ -0,0 +1,20 @@
+! { dg-do run }
+! { dg-shouldfail "negative NCOPIES argument to REPEAT intrinsic" }
+  character(len=80) :: str
+  integer :: i
+  i = -1
+  write(str,"(a)") repeat ("a", f())
+  if (trim(str) /= "aaaa") call abort
+  write(str,"(a)") repeat ("a", i)
+
+contains
+
+  integer function f()
+    integer :: x = 5
+    save x
+
+    x = x - 1
+    f = x
+  end function f
+end
+! { dg-output "Fortran runtime error: Argument NCOPIES of REPEAT intrinsic is negative .* line 6)"
