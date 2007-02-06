6b1ea908fe70fd706dfa2ae9b5765ccdc3774fe0
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-02-06 18:15:46 +0000

    * gcc.target/i386/cvt-1.c: Don't expect xmm registers.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@121659 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/cvt-1.c
+++ gcc/testsuite/gcc.target/i386/cvt-1.c
@@ -1,7 +1,7 @@
 /* { dg-do compile { target i?86-*-* x86_64-*-* } } */
 /* { dg-options "-O2 -march=k8 -mfpmath=sse" } */
-/* { dg-final { scan-assembler "cvttsd2si\[^\\n\]*xmm" } } */
-/* { dg-final { scan-assembler "cvttss2si\[^\\n\]*xmm" } } */
+/* { dg-final { scan-assembler "cvttsd2si" } } */
+/* { dg-final { scan-assembler "cvttss2si" } } */
 int a,a1;
 double b;
 float b1;
