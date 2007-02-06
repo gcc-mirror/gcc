03d3e95382e636baa43e1ad0b635c427615cc16a
Ira Rosen <irar@il.ibm.com>
2007-02-06 08:41:01 +0000

    tree-vect-patterns.c (vect_recog_widen_mult_pattern): Check that vectype is not NULL.
    	* tree-vect-patterns.c (vect_recog_widen_mult_pattern): Check that 
    	vectype is not NULL.
    	(vect_pattern_recog_1): Likewise.
    
    From-SVN: r121640

---
+++ gcc/testsuite/gcc.dg/vect/fast-math-vect-pow-2.c
@@ -0,0 +1,33 @@
+/* { dg-do compile } */
+
+typedef double d_type;
+struct
+{
+  d_type x;
+  d_type y;
+} S[100];
+
+#define N 16
+d_type foo (d_type t);
+
+d_type
+main1 ()
+{
+  int i;
+  d_type t;
+
+  for (i = 0; i < N; i++)
+    {
+      t = (d_type) i / (d_type) 10;
+      S[5].x = t * t;
+    }
+  return S[5].x;
+}
+
+int
+main (void)
+{
+  d_type tmp = main1 ();
+}
+/* { dg-final { cleanup-tree-dump "vect" } } */
+
