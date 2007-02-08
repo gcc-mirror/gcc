e35a0e6476c15c34df002f698dbfbbcc44338238
Roger Sayle <roger@eyesopen.com>
2007-02-08 16:41:18 +0000

    trans-stmt.c (gfc_trans_forall_1): Optimize the cases where the mask expression is a compile-time constant...
    	* trans-stmt.c (gfc_trans_forall_1): Optimize the cases where the
    	mask expression is a compile-time constant (".true." or ".false.").
    
    	* gfortran.dg/forall_8.f90: New test case.
    	* gfortran.dg/forall_9.f90: Likewise.
    
    From-SVN: r121714

---
+++ gcc/testsuite/gfortran.dg/forall_8.f90
@@ -0,0 +1,9 @@
+! { dg-do compile }
+! { dg-options "-O2 -fdump-tree-original" }
+  integer a(100)
+  forall (i=1:100,.true.)
+      a(i) = 0
+  end forall
+  end
+! { dg-final { scan-tree-dump-times "temp" 0 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
