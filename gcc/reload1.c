81ef6b6a7f23048d306120c73f49ab2a78052aa6
kkojima <kkojima@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-02-10 06:47:49 +0000

    PR rtl-optimization/29599 	* reload1.c (eliminate_regs_in_insn): Take the destination 	mode into account when computing the offset.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@121792 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/reload1.c
+++ gcc/reload1.c
@@ -1,7 +1,7 @@
 /* Reload pseudo regs into hard regs for insns that require hard regs.
    Copyright (C) 1987, 1988, 1989, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006 Free Software Foundation,
-   Inc.
+   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007
+   Free Software Foundation, Inc.
 
 This file is part of GCC.
 
@@ -3100,6 +3100,7 @@ eliminate_regs_in_insn (rtx insn, int replace)
 	  {
 	    rtx to_rtx = ep->to_rtx;
 	    offset += ep->offset;
+	    offset = trunc_int_for_mode (offset, GET_MODE (reg));
 
 	    if (GET_CODE (XEXP (plus_cst_src, 0)) == SUBREG)
 	      to_rtx = gen_lowpart (GET_MODE (XEXP (plus_cst_src, 0)),
