647e9f063f0f3df989bf6b81e5416009fa73822a
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-02-11 19:48:07 +0000

    * config/i386/i386.c (TARGET_VECTORIZE_BUILTIN_CONVERSION): Define.         (ix86_builtin_conversion): New function.
    testsuite/ChangeLog:
    
            * gcc.dg/vect/vect-intfloat-conversion-1.c: Loop gets vectorized
            on i?86 and x86_64.
            * gcc.dg/vect/vect-intfloat-conversion-2.c: Ditto.
            * gcc.dg/vect/vect-floatint-conversion-1.c: New.
            * gcc.dg/vect/vect-93.c: Another loop gets vectorized
            on i?86 and x86_64.
            * gcc.dg/vect/vect-113.c: Ditto.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@121820 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/vect-intfloat-conversion-2.c
@@ -0,0 +1,40 @@
+/* { dg-require-effective-target vect_int } */
+
+#include <stdarg.h>
+#include "tree-vect.h"
+
+#define N 32
+
+int main1 ()
+{
+  int i;
+  int int_arr[N] = {0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45};
+  float float_arr[N];
+  char char_arr[N];
+
+  for (i = 0; i < N; i++){
+    float_arr[i] = (float) int_arr[i];
+    char_arr[i] = 0;
+  }
+
+  /* check results:  */
+  for (i = 0; i < N; i++)
+    {
+      if (float_arr[i] != (float) int_arr[i]) 
+        abort (); 
+      if (char_arr[i] != 0)
+	abort ();
+    }   
+
+  return 0;
+}
+
+int main (void)
+{ 
+  check_vect ();
+  
+  return main1 ();
+}
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { target powerpc*-*-* i?86-*-* x86_64-*-* } } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
