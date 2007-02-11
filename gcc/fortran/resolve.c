b5d67022d0ec98fc7dd2384941da184bef64fd35
Bernhard Fischer <aldot@gcc.gnu.org>
2007-02-11 01:25:44 +0100

    backport: re PR fortran/24783 ([4.1 and 4.2 only] Implicit none in module overwrite explicit in procedure)
    2007-02-10  Bernhard Fischer  <aldot@gcc.gnu.org>
    
    	Backport from trunk
    	PR fortran/24783
    	* resolve.c (resolve_variable): Get the implicit type from the
    	symbols namespace rather than the default namespace. Fix whitespace.
    	(resolve_formal_arglist, resolve_equivalence): Fix typo.
    
    From-SVN: r121812

--- gcc/fortran/resolve.c
+++ gcc/fortran/resolve.c
@@ -227,7 +227,7 @@ resolve_formal_arglist (gfc_symbol * proc)
                 {
                   gfc_error
                     ("Character-valued argument '%s' of statement function at "
-                     "%L must has constant length",
+                     "%L must have constant length",
                      sym->name, &sym->declared_at);
                   continue;
                 }
@@ -2889,7 +2889,7 @@ resolve_variable (gfc_expr * e)
   else
     {
       /* Must be a simple variable reference.  */
-      if (gfc_set_default_type (sym, 1, NULL) == FAILURE)
+      if (gfc_set_default_type (sym, 1, sym->ns) == FAILURE)
 	return FAILURE;
       e->ts = sym->ts;
     }
@@ -5711,11 +5711,9 @@ resolve_symbol (gfc_symbol * sym)
     case FL_PARAMETER:
       if (resolve_fl_parameter (sym) == FAILURE)
 	return;
-
       break;
 
     default:
-
       break;
     }
 
@@ -6383,7 +6381,7 @@ resolve_equivalence (gfc_equiv *eq)
 	{
 	  if (value_name != NULL)
 	    {
-	      gfc_error ("Initialized objects '%s' and '%s'  cannot both "
+	      gfc_error ("Initialized objects '%s' and '%s' cannot both "
 			 "be in the EQUIVALENCE statement at %L",
 			 value_name, sym->name, &e->where);
 	      continue;
