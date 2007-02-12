d8d25baea642cfd4e4bcb4d4b9e3f2b12921ffac
Roger Sayle <roger@eyesopen.com>
2007-02-12 18:41:08 +0000

    i386.md (*bswapdi2_rex): Renamed from bswapdi2.
    	* config/i386/i386.md (*bswapdi2_rex): Renamed from bswapdi2.
    	(bswapdi2): New define_expand to implement 32-bit implementation.
    
    	* gcc.target/i386/builtin-bswap-3.c: New test case.
    
    From-SVN: r121851

---
+++ gcc/testsuite/gcc.target/i386/builtin-bswap-3.c
@@ -0,0 +1,9 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -march=nocona" } */
+/* { dg-final { scan-assembler-not "bswapdi2" } } */
+
+long long foo (long long x)
+{
+  return __builtin_bswap64 (x);
+}
+
