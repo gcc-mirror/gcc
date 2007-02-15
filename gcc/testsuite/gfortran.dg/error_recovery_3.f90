c615861bd19a0f4161f003831a84b6d3fb63d22f
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-02-15 06:51:42 +0000

    2007-02-15  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR fortran/30779
    	* gfortran.dg/error_recovery_3.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@121983 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/error_recovery_3.f90
@@ -0,0 +1,9 @@
+! { dg-do compile }
+! PR30779 incomplete file triggers ICE.
+! Note: This file is deliberately cut short to verify a graceful exit. Before
+! the patch this gave ICE.
+MODULE M1
+ INTEGER :: I
+END MODULE M1
+
+USE M1,                    ONLY: I,&! { dg-error "Missing" }
