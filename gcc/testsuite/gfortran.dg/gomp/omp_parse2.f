7efeea5e7c608b4dd93de915e1f4faf4c58a6009
Jakub Jelinek <jakub@gcc.gnu.org>
2007-02-21 09:36:18 +0100

    [multiple changes]
    2007-02-21  Bernhard Fischer  <aldot@gcc.gnu.org>
    
    	* parse.c (next_free): Gooble spaces after OpenMP sentinel.
    
    2007-02-21  Jakub Jelinek  <jakub@redhat.com>
    
    	* gfortran.dg/gomp/omp_parse1.f90: New test.
    	* gfortran.dg/gomp/omp_parse2.f: New test.
    
    From-SVN: r122190

---
+++ gcc/testsuite/gfortran.dg/gomp/omp_parse2.f
@@ -0,0 +1,14 @@
+c { dg-do compile }
+c { dg-options "-fopenmp -fdump-tree-omplower" }
+!$omp  parallel
+      call bar
+c$omp	end parallel
+C$omp 	 	p
+*$omp+arallel
+      call bar
+!$omp e
+!$omp+ndparallel
+      end
+
+! { dg-final { scan-tree-dump-times "pragma omp parallel" 2 "omplower" } }
+! { dg-final { cleanup-tree-dump "omplower" } }
