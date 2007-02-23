a643a79077174c3fbc44560d1155d38d6d1d9ed2
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2007-02-23 18:20:01 +0000

    re PR fortran/30681 ("obsolescent" vs. "obsolete")
    2007-02-23  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR fortran/30681
    	* gfortran.dg/pr17229.f: Change "Obsolete" to "Obsolescent".
    
    From-SVN: r122269

--- gcc/testsuite/gfortran.dg/pr17229.f
+++ gcc/testsuite/gfortran.dg/pr17229.f
@@ -5,18 +5,18 @@
 
       l = .false.
       i = -1
-      if (l) if (i) 999,999,999 ! { dg-warning "Obsolete: arithmetic IF statement" }
+      if (l) if (i) 999,999,999 ! { dg-warning "Obsolescent: arithmetic IF statement" }
 
       l = .true.
-      if (l) if (i) 10,999,999 ! { dg-warning "Obsolete: arithmetic IF statement" }
+      if (l) if (i) 10,999,999 ! { dg-warning "Obsolescent: arithmetic IF statement" }
       go to 999
 
    10 i = 0
-      if (l) if (i) 999,20,999 ! { dg-warning "Obsolete: arithmetic IF statement" }
+      if (l) if (i) 999,20,999 ! { dg-warning "Obsolescent: arithmetic IF statement" }
       go to 999
 
    20 i = 1
-      if (l) if (i) 999,999,30 ! { dg-warning "Obsolete: arithmetic IF statement" }
+      if (l) if (i) 999,999,30 ! { dg-warning "Obsolescent: arithmetic IF statement" }
       go to 999
 
   999 call abort
