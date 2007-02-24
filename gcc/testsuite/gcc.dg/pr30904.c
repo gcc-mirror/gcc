5804b39ebca0eb1585104b1616513a80afc3a85c
bonzini <bonzini@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-02-24 16:07:41 +0000

    2007-02-23  Paolo Bonzini  <bonzini@gnu.org>
            PR tree-optimization/30904
    	* gcc.dg/pr30904.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@122290 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr30904.c
@@ -0,0 +1,18 @@
+/* { dg-do link } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+extern int t;
+extern void link_error(void);
+int main (void)
+{
+      struct { unsigned int a : 7; } s;
+      s.a = t;
+      if (s.a >> 8)
+          link_error ();
+      if (s.a >> 9)
+          link_error ();
+}
+
+
+/* { dg-final { scan-tree-dump-times "link_error" 0 "optimized" { xfail *-*-* } } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
