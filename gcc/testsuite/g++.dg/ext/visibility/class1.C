f6bc8640768a5c4c207d47e3f4e137b3e323d3c5
Geoffrey Keating <geoffk@apple.com>
2007-02-27 00:10:22 +0000

    class1.C: Port to Darwin.
    	* g++.dg/ext/visibility/class1.C: Port to Darwin.
    	* g++.dg/ext/visibility/noPLT.C: Likewise.
    
    From-SVN: r122348

--- gcc/testsuite/g++.dg/ext/visibility/class1.C
+++ gcc/testsuite/g++.dg/ext/visibility/class1.C
@@ -2,9 +2,9 @@
 // Init should not be hidden, so calling it should use the PLT.
 
 // { dg-options "-fpic" }
-// { dg-do compile { target i?86-*-* x86_64-*-* } }
+// { dg-do compile { target i?86-*-* x86_64-*-* *-*-darwin* } }
 // { dg-require-visibility "" }
-// { dg-final { scan-assembler "InitEv@PLT" } }
+// { dg-final { scan-assembler "InitEv@PLT|indirect_symbol.*InitEv" } }
 
 #pragma GCC visibility push(hidden)
 struct __attribute__ ((visibility ("default"))) nsINIParser
