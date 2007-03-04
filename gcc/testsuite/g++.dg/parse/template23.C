785359fcbccbf466886b7943e2373ed77c57bcd6
simartin <simartin@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-04 19:50:54 +0000

    2007-03-04  Simon Martin  <simartin@users.sourceforge.net>
    	PR c++/30895
    	* tree.c (cp_tree_equal): Properly handle COMPLEX_CST trees.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@122532 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/template23.C
@@ -0,0 +1,10 @@
+/* PR c++/30895 This used to ICE.  */
+/* { dg-do "compile" } */
+
+template<int> struct A {};
+
+template<typename T> struct B
+{
+  A<T(0i)> a1; /* { dg-error "imaginary constants are a GCC extension" } */
+  A<T(0i)> a2; /* { dg-error "imaginary constants are a GCC extension" } */
+};
