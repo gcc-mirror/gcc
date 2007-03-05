9d2868c93fb0a0474615a9710ecf19483294b498
Brooks Moses <brooks.moses@codesourcery.com>
2007-03-05 08:41:11 +0000

    simplify.c (gfc_simplify_ibclr): Fix POS comparison.
    * simplify.c (gfc_simplify_ibclr): Fix POS comparison.
    (gfc_simplify_ibset): Same.
    
    From-SVN: r122542

--- gcc/fortran/simplify.c
+++ gcc/fortran/simplify.c
@@ -1295,7 +1295,7 @@ gfc_simplify_ibclr (gfc_expr * x, gfc_expr * y)
 
   k = gfc_validate_kind (x->ts.type, x->ts.kind, false);
 
-  if (pos > gfc_integer_kinds[k].bit_size)
+  if (pos >= gfc_integer_kinds[k].bit_size)
     {
       gfc_error ("Second argument of IBCLR exceeds bit size at %L",
 		 &y->where);
@@ -1401,7 +1401,7 @@ gfc_simplify_ibset (gfc_expr * x, gfc_expr * y)
 
   k = gfc_validate_kind (x->ts.type, x->ts.kind, false);
 
-  if (pos > gfc_integer_kinds[k].bit_size)
+  if (pos >= gfc_integer_kinds[k].bit_size)
     {
       gfc_error ("Second argument of IBSET exceeds bit size at %L",
 		 &y->where);
