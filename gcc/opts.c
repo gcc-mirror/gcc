699fd48374a0163f69cde978c71996055a1ce4e1
Manuel López-Ibáñez <manu@gcc.gnu.org>
2007-03-06 21:04:35 +0000

    invoke.texi (Warning Options): Replace -Werror- with -Werror=.
    2007-03-06  Manuel Lopez-Ibanez  <manu@gcc.gnu.org>
    
    	* doc/invoke.texi (Warning Options): Replace -Werror- with
    	-Werror=.
    	* opts.c (common_handle_option): Likewise.
    
    From-SVN: r122638

--- gcc/opts.c
+++ gcc/opts.c
@@ -688,7 +688,7 @@ common_handle_option (size_t scode, const char *arg, int value,
 	option_index = find_opt (new_option, lang_mask);
 	if (option_index == N_OPTS)
 	  {
-	    error ("-Werror-%s: No option -%s", arg, new_option);
+	    error ("-Werror=%s: No option -%s", arg, new_option);
 	  }
 	else
 	  {
