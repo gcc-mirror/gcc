53f4720239843a49a58cfaafa805c4dcd53746d4
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-08 09:09:38 +0000

    2007-03-08 Paul Thomas <pault@gcc.gnu.org>
    	PR fortran/31011
    	* expr.c (find_array_section): Correct arithmetic for section
    	size.
    
    2007-03-08 Paul Thomas <pault@gcc.gnu.org>
    
    	PR fortran/31011
    	* gfortran.dg/parameter_array_section_2.f90: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@122689 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/parameter_array_section_2.f90
@@ -0,0 +1,14 @@
+! { dg-do run }
+! { dg-options "-O" }
+! Test the fix for PR31011 in which the length of the array sections
+! with stride other than unity were incorrectly calculated.
+!
+! Contributed by <terry@chem.gu.se>
+!
+program PotentialMatrix
+ implicit none
+ real(kind=8),dimension(2),parameter::v2=(/1,2/)
+ real(kind=8),dimension(4),parameter::v4=(/1,2,3,4/)
+ if (any (v2*v4(1:3:2) .ne. (/1,6/))) call abort ()
+ if (any (v2*v4(3:1:-2) .ne. (/3,2/))) call abort ()
+end
