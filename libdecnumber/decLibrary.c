f002566861e881f3faa7997dd3292aca53436925
Ben Elliston <bje@au.ibm.com>
2007-03-08 22:22:12 +0000

    decContext.c, [...]: Add libgcc-style license exception clause.
    	* decContext.c, decContext.h, decDPD.h, decimal128.c,
    	decimal128.h, decimal32.c, decimal32.h, decimal64.c, decimal64.h,
    	decLibrary.c, decNumber.c, decNumber.h, decNumberLocal.h,
    	decRound.c, decRound.h, decUtility.c, decUtility.h: Add
    	libgcc-style license exception clause.
    
    From-SVN: r122721

--- libdecnumber/decLibrary.c
+++ libdecnumber/decLibrary.c
@@ -8,6 +8,15 @@
    the Free Software Foundation; either version 2, or (at your option)
    any later version.
 
+   In addition to the permissions in the GNU General Public License,
+   the Free Software Foundation gives you unlimited permission to link
+   the compiled version of this file into combinations with other
+   programs, and to distribute those combinations without any
+   restriction coming from the use of this file.  (The General Public
+   License restrictions do apply in other respects; for example, they
+   cover modification of the file, and distribution when not linked
+   into a combine executable.)
+
    GCC is distributed in the hope that it will be useful, but WITHOUT
    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
