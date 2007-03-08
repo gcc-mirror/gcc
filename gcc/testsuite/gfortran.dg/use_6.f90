56bedf420fd15955cfa194ead87f2beaedac9509
Tobias Burnus <burnus@net-b.de>
2007-03-08 14:11:54 +0100

    [multiple changes]
    2007-07-08  Tobias Burnus  <burnus@net-b.de>
    
           * module.c (gfc_match_use): Support renaming of operators
           in USE statements.
           * gfortran.texi (Fortran 2003 Status): Document support of
           renaming of operators.
    
    2007-03-08  Tobias Burnus  <burnus@net-b.de>
    
           * gfortran.dg/use_5.f90: New test.
           * gfortran.dg/use_6.f90: Ditto.
           * gfortran.dg/use_7.f90: Ditto.
    
    From-SVN: r122699

---
+++ gcc/testsuite/gfortran.dg/use_6.f90
@@ -0,0 +1,45 @@
+! { dg-do "compile" }
+! { dg-options "-std=f95" }
+! Renaming of operators
+module z
+  interface operator(.addfive.)
+    module procedure sub2
+  end interface
+contains
+function sub2(x)
+  integer :: sub
+  integer,intent(in) :: x
+  sub2 = x + 5
+end function sub2
+end module z
+
+module y
+  interface operator(.addfive.)
+    module procedure sub
+  end interface
+contains
+function sub(x)
+  integer :: sub
+  integer,intent(in) :: x
+  sub = x + 15
+end function sub
+end module y
+
+module x
+  interface operator(.addfive.)
+    module procedure sub
+  end interface
+contains
+function sub(x)
+  integer :: sub
+  integer,intent(in) :: x
+  sub = x + 25
+end function sub
+end module x
+
+use x, only : operator(.bar.) => operator(.addfive.) ! { dg-error "Fortran 2003: Renaming operators in USE statements" }
+use y, operator(.my.) => operator(.addfive.) ! { dg-error "Fortran 2003: Renaming operators in USE statements" }
+use z
+end
+
+! { dg-final { cleanup-tree-dump "x y z" } }
