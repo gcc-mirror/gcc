5dbcb7c447db35a345494679ba02531d5deecd2f
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-09 23:09:10 +0000

    * fold-const.c (fold_comparison): Remove compile-time evaluation of 	complex constant equality/inequality comparisons for here. 	(fold_binary) <EQ_EXPR>: Simplify complex comparisons that are 	known at compile-time or can be simplified to a scalar comparison. 	(fold_relational_const): Move compile-time evaluation of complex 	constant equality/inequality comparisons to here.
    	* gcc.dg/fold-eqcmplx-1.c: New test case.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@122767 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/fold-eqcmplx-1.c
@@ -0,0 +1,10 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-original" } */
+
+int foo(float x, float y)
+{
+  return (_Complex float)x == (_Complex float)y;
+}
+
+/* { dg-final { scan-tree-dump-times "x == y" 1 "original" } } */
+/* { dg-final { cleanup-tree-dump "original" } } */
