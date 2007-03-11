57a93644a8e121c8884d27530dd4eb709f20569c
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-11 16:17:32 +0000

    2007-03-11  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/30883
    	* parse.c (parse_interface): Use the default types from the
    	formal namespace if a function or its result do not have a type
    	after parsing the specification statements.
    
    2007-03-11  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/30883
    	* gfortran.dg/interface_11.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@122822 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/interface_11.f90
@@ -0,0 +1,29 @@
+! { dg-do compile }
+! Tests the fix for PR30883 in which interface functions and
+! their results did not get an implicit type.
+!
+! Contributed by Joost VandeVondele <jv244@cam.ac.uk>
+!
+MODULE M1
+  IMPLICIT NONE
+CONTAINS
+  SUBROUTINE S1(F1, F2, G1, G2)
+    INTERFACE
+      FUNCTION F1(i, a)
+      END FUNCTION F1
+      FUNCTION F2(i, a)
+        implicit complex (a-z)
+      END FUNCTION F2
+    END INTERFACE
+    INTERFACE
+      FUNCTION g1(i, a) result(z)
+      END FUNCTION g1
+      FUNCTION g2(i, a) result(z)
+        implicit complex (a-z)
+      END FUNCTION g2
+    END INTERFACE
+  END SUBROUTINE S1
+END MODULE
+
+END
+! { dg-final { cleanup-modules "m1" } }
