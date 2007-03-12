4c2454f257be870ed51e5515c6e5643ea9dc79e4
spark <spark@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-12 22:37:51 +0000

    2007-03-12  Seongbae Park <seongbae.park@gmail.com>
    	* gcc.dg/c90-vla-1.c: Reflect the change of the error message.
    	* gcc.dg/c99-vla-1.c: Likewise.
    	* g++.dg/ext/vla1.C: Likewise
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@122871 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/c99-vla-1.c
+++ gcc/testsuite/gcc.dg/c99-vla-1.c
@@ -5,7 +5,7 @@
 void
 tdef (int n)
 {
-  typedef int A[n];	/* { dg-bogus "forbids variable-size array" } */
+  typedef int A[n];	/* { dg-bogus "forbids variable length array" } */
   A a;
   A *p;
   p = &a;
