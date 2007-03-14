5d5d27dc617ee581cf8e9143ca4985101528c919
Jakub Jelinek <jakub@redhat.com>
2007-03-14 13:20:47 +0100

    module.c (mio_typespec): Don't look at ts->cl if not BT_CHARACTER.
    	* module.c (mio_typespec): Don't look at ts->cl if not BT_CHARACTER.
    
    	* gfortran.dg/module_implicit_conversion.f90: New test.
    
    From-SVN: r122917

---
+++ gcc/testsuite/gfortran.dg/module_implicit_conversion.f90
@@ -0,0 +1,9 @@
+! { dg-do compile }
+! { dg-options "-std=gnu" }
+
+module module_implicit_conversion
+  ! double complex :: s = (1.0D0, 0D0) 
+  double complex :: s = (1.0, 0D0)
+end module module_implicit_conversion
+
+! { dg-final { cleanup-modules "module_implicit_conversion" } }
