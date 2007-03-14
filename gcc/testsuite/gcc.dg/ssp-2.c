054d18868e89cefe8b8b87e5b28f9b6a6cc33b15
echristo <echristo@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-14 21:11:20 +0000

    2007-03-14  Eric Christopher  <echristo@apple.com>
    	* gcc.dg/ssp-1.c: New.
            * gcc.dg/ssp-2.c: Ditto.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@122932 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/ssp-2.c
@@ -0,0 +1,26 @@
+/* { dg-do run { target native } } */
+/* { dg-options "-fstack-protector" } */
+#include <stdlib.h>
+
+void
+__stack_chk_fail (void)
+{
+  exit (0); /* pass */
+}
+
+void
+overflow()
+{
+  int i = 0;
+  char foo[30];
+
+  /* Overflow buffer.  */
+  for (i = 0; i < 50; i++)
+      foo[i] = 42;
+}
+
+int main (void)
+{
+  overflow ();
+  return 1; /* fail */
+}
