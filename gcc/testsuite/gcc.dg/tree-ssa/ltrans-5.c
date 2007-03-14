c4d8f83b551c953d7cbbe85cf24bab3595defaaa
Sebastian Pop <sebastian.pop@inria.fr>
2007-03-15 00:23:24 +0100

    tree-loop-linear.c (gather_interchange_stats): For multidimensional arrays...
    	* tree-loop-linear.c (gather_interchange_stats): For multidimensional
    	arrays, multiply the access strides by the size of the sub-array.
    	* testsuite/gcc.dg/tree-ssa/ltrans-5.c: New.
    
    From-SVN: r122935

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ltrans-5.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */ 
+/* { dg-options "-O2 -ftree-loop-linear -fdump-tree-ltrans-all" } */
+
+int foo ()
+{
+  int A[100][1111];
+  int i, j;
+
+  for( i = 0; i < 1111; i++)
+    for( j = 0; j < 100; j++)
+      A[j][i] = 5 * A[j][i];
+
+  return A[10][10];
+}
+
+/* { dg-final { scan-tree-dump-times "transformed loop" 1 "ltrans"} } */ 
+/* { dg-final { cleanup-tree-dump "ltrans" } } */
