d33d6db009db55c792b1fbaa343220b079c251d7
Eric Christopher <echristo@gcc.gnu.org>
2007-03-14 21:11:20 +0000

    ssp-1.c: New.
    2007-03-14  Eric Christopher  <echristo@apple.com>
    
    	* gcc.dg/ssp-1.c: New.
            * gcc.dg/ssp-2.c: Ditto.
    
    From-SVN: r122932

---
+++ gcc/testsuite/gcc.dg/ssp-1.c
@@ -0,0 +1,21 @@
+/* { dg-do run { target native } } */
+/* { dg-options "-fstack-protector" } */
+#include <stdlib.h>
+
+static void
+__stack_chk_fail (void)
+{
+  exit (0); /* pass */
+}
+
+int main ()
+{
+  int i;
+  char foo[255];
+
+  // smash stack
+  for (i = 0; i <= 400; i++)
+    foo[i] = 42;
+
+  return 1; /* fail */
+}
