ec5586d6ef4f06ea15c77d67dc8eeb5b4cb0fde1
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-15 08:53:42 +0000

    gcc/testsuite/ 	* gcc.dg/cpp/assert4.c: Treat VxWorks as a unix target.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@122948 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cpp/assert4.c
+++ gcc/testsuite/gcc.dg/cpp/assert4.c
@@ -78,7 +78,8 @@
 # error
 #endif
 
-#if ( defined __unix__ && !defined __CYGWIN__ ) || defined _AIX
+#if ( defined __unix__ && !defined __CYGWIN__ ) || defined _AIX \
+    || defined __vxworks
 # if !#system(unix)
 #  error
 # endif
