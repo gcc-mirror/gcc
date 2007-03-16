15ec875c733b473953eae3637f13665dffb0589b
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-16 10:11:14 +0000

    2007-03-16  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/31146
    	* tree-ssa-forwprop.c (forward_propagate_addr_expr_1): Restructure
    	to allow recursion of forward_propagate_addr_expr.
    	(forward_propagate_addr_into_variable_array_index): Likewise.
    	(forward_propagate_addr_expr): Likewise.
    	(tree_ssa_forward_propagate_single_use_vars): Likewise.
    	(forward_propagate_addr_expr_1): Recurse on simple copies
    	instead of propagating into them.  Do so for useless conversions
    	as well.
    	(forward_propagate_addr_expr): Clean up unused statements after
    	recursion.
    
    	* g++.dg/tree-ssa/pr31146.C: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@122985 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/tree-ssa/pr31146.C
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-forwprop1" } */
+
+/* We should be able to optimize this to i[j] = 1 during
+   early optimizations.  */
+
+int i[5];
+void foo (int j)
+{
+  void *p = &i[j];
+  int *q = (int *)p;
+  *q = 1;
+}
+
+/* { dg-final { scan-tree-dump "i\\\[j.*\\\] = 1;" "forwprop1" } } */
+/* { dg-final { cleanup-tree-dump "forwprop1" } } */
