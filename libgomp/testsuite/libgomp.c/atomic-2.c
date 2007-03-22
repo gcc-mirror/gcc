a17d193ed9741b648785e456c52ea08557d40c6a
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-22 07:00:08 +0000

    * testsuite/config/default.exp: New file. 	* testsuite/lib/libgomp.exp: New file. 	* testsuite/lib/libgomp.dg (load_gcc_lib, libgomp_init, 	libgomp_target_compile, libgomp_option_help, libgomp_option_proc, 	load_lib *, load_gcc_lib *): Move to libgomp.exp. 	(libgomp_load): Remove. 	* testsuite/lib/libgomp.exp (libgomp_init): Compute 	always_ld_library_path, not ld_library_path.  Set additional_flags 	to -march=i486 for ilp32 x86_64-*-* and i386-*-* targets. 	(target_compile): Do not call libgomp_init.  Append lang_library_path 	and lang_link_flags to options. 	* testsuite/libgomp.c/c.exp: Set DEFAULT_FLAGS to -O2.  Set 	ld_library_path from always_ld_library_path.  Set LD_LIBRARY_PATH 	here. 	* testsuite/libgomp.c++/c++.exp: Set ld_library_path from 	always_ld_library_path.  Set LD_LIBRARY_PATH here. 	* testsuite/libgomp.fortran/fortran.exp: Ditto. 	* testsuite/libgomp.c/atomic-1.c: Set dg-options to 	"-O2 -march=pentium" for ilp32 x86 targets. Simplify check for 	CX8 flag. 	* testsuite/libgomp.c/atomic-2.c: Set dg-options to "-O2 -mcx16" for 	lp64 x86 targets. Do not check for SSE3 bit. Do not define bit_SSE3. 	* testsuite/libgomp.c/pr29947-1.c: Remove default dg-options. 	* testsuite/libgomp.c/pr29947-1.c: Ditto. 	* testsuite/libgomp.c/atomic-10.c: Ditto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@123125 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgomp/testsuite/libgomp.c/atomic-2.c
+++ libgomp/testsuite/libgomp.c/atomic-2.c
@@ -1,7 +1,5 @@
 /* { dg-do run } */
-/* { dg-options "-O2 -fopenmp" } */
-/* { dg-options "-O2 -fopenmp -march=nocona" { target i?86-*-* x86_64-*-* } } */
-/* { dg-options "-O2 -fopenmp" { target ilp32 } } */
+/* { dg-options "-O2 -mcx16" { target { { i?86-*-* x86_64-*-* } && lp64 } } } */
 
 double d = 1.5;
 long double ld = 3;
@@ -22,12 +20,11 @@ int
 main (void)
 {
 #ifdef __x86_64__
-# define bit_SSE3 (1 << 0)
 # define bit_CX16 (1 << 13)
   unsigned int ax, bx, cx, dx;
   __asm__ ("cpuid" : "=a" (ax), "=b" (bx), "=c" (cx), "=d" (dx)
            : "0" (1) : "cc");
-  if ((cx & (bit_SSE3 | bit_CX16)) != (bit_SSE3 | bit_CX16))
+  if (!(cx & bit_CX16))
     return 0;
 #endif
   test ();
