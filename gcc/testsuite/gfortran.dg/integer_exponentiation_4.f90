3c2e80433d69dc6df77a1e916fe35d75a470528f
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2007-03-23 07:00:56 +0000

    re PR fortran/30834 (ICE with kind=8 exponentiaton)
    	PR fortran/30834
    
    	* arith.c (complex_pow): Rewrite to handle large power.
    	(gfc_arith_power): Handle large power in the real and integer
    	cases.
    
    	* gfortran.dg/integer_exponentiation_3.F90: New	test.
    	* gfortran.dg/integer_exponentiation_4.f90: New test.
    	* gfortran.dg/integer_exponentiation_5.F90: New test.
    
    From-SVN: r123154

---
+++ gcc/testsuite/gfortran.dg/integer_exponentiation_4.f90
@@ -0,0 +1,44 @@
+! { dg-do compile }
+! { dg-options "" }
+program test
+  implicit none
+
+!!!!!! INTEGER BASE !!!!!!
+  print *, 0**0
+  print *, 0**1
+  print *, 0**(-1) ! { dg-error "Division by zero" }
+  print *, 0**(huge(0))
+  print *, 0**(-huge(0)-1) ! { dg-error "Division by zero" }
+  print *, 0**(2_8**32)
+  print *, 0**(-(2_8**32)) ! { dg-error "Division by zero" }
+
+  print *, 1**huge(0)
+  print *, 1**(-huge(0)-1)
+  print *, 1**huge(0_8)
+  print *, 1**(-huge(0_8)-1_8)
+  print *, (-1)**huge(0)
+  print *, (-1)**(-huge(0)-1)
+  print *, (-1)**huge(0_8)
+  print *, (-1)**(-huge(0_8)-1_8)
+
+  print *, 2**huge(0) ! { dg-error "Arithmetic overflow" }
+  print *, 2**huge(0_8) ! { dg-error "Arithmetic overflow" }
+  print *, (-2)**huge(0) ! { dg-error "Arithmetic overflow" }
+  print *, (-2)**huge(0_8) ! { dg-error "Arithmetic overflow" }
+
+  print *, 2**(-huge(0)-1)
+  print *, 2**(-huge(0_8)-1_8)
+  print *, (-2)**(-huge(0)-1)
+  print *, (-2)**(-huge(0_8)-1_8)
+
+!!!!!! REAL BASE !!!!!!
+  print *, 0.0**(-1) ! { dg-error "Arithmetic overflow" }
+  print *, 0.0**(-huge(0)-1) ! { dg-error "Arithmetic overflow" }
+  print *, 2.0**huge(0) ! { dg-error "Arithmetic overflow" }
+  print *, nearest(1.0,-1.0)**(-huge(0)) ! { dg-error "Arithmetic overflow" }
+
+!!!!!! COMPLEX BASE !!!!!!
+  print *, (2.0,-4.3)**huge(0) ! { dg-error "Arithmetic NaN" }
+  print *, (2.0,-4.3)**(-huge(0)) ! { dg-error "Arithmetic NaN" }
+
+end program test
---
+++ gcc/testsuite/gfortran.dg/integer_exponentiation_5.F90
@@ -0,0 +1,78 @@
+! { dg-do run }
+! { dg-options "-fno-range-check" }
+module mod_check
+  implicit none
+
+  interface check
+    module procedure check_i8
+    module procedure check_i4
+    module procedure check_r8
+    module procedure check_r4
+    module procedure check_c8
+    module procedure check_c4
+  end interface check
+
+contains
+
+  subroutine check_i8 (a, b)
+    integer(kind=8), intent(in) :: a, b
+    if (a /= b) call abort()
+  end subroutine check_i8
+
+  subroutine check_i4 (a, b)
+    integer(kind=4), intent(in) :: a, b
+    if (a /= b) call abort()
+  end subroutine check_i4
+
+  subroutine check_r8 (a, b)
+    real(kind=8), intent(in) :: a, b
+    if (a /= b) call abort()
+  end subroutine check_r8
+
+  subroutine check_r4 (a, b)
+    real(kind=4), intent(in) :: a, b
+    if (a /= b) call abort()
+  end subroutine check_r4
+
+  subroutine check_c8 (a, b)
+    complex(kind=8), intent(in) :: a, b
+    if (a /= b) call abort()
+  end subroutine check_c8
+
+  subroutine check_c4 (a, b)
+    complex(kind=4), intent(in) :: a, b
+    if (a /= b) call abort()
+  end subroutine check_c4
+
+end module mod_check
+
+program test
+  use mod_check
+  implicit none
+
+  integer(kind=4) :: i4
+  integer(kind=8) :: i8
+  real(kind=4) :: r4
+  real(kind=8) :: r8
+  complex(kind=4) :: c4
+  complex(kind=8) :: c8
+
+#define TEST(base,exp,var) var = base; call check((var)**(exp),(base)**(exp))
+
+!!!!! INTEGER BASE !!!!!
+  TEST(3,23,i4)
+  TEST(-3,23,i4)
+  TEST(3_8,43_8,i8)
+  TEST(-3_8,43_8,i8)
+
+  TEST(17_8,int(huge(0),kind=8)+1,i8)
+
+!!!!! REAL BASE !!!!!
+  TEST(0.0,-1,r4)
+  TEST(0.0,-huge(0)-1,r4)
+  TEST(2.0,huge(0),r4)
+  TEST(nearest(1.0,-1.0),-huge(0),r4)
+
+end program test
+
+! { dg-final { cleanup-modules "mod_check" } }
