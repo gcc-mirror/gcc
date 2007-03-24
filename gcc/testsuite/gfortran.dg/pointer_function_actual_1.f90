6a661315c2e9581727c969320d335da82dbd4829
Paul Thomas <pault@gcc.gnu.org>
2007-03-24 12:30:58 +0000

    re PR fortran/31215 (ICE on valid code with gfortran)
    2007-03-24  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/31215
    	* trans-expr.c (gfc_apply_interface_mapping_to_expr): Return
    	int result that is non-zero if the expression is the function
    	result.  Only the characteristics of the result expression
    	can be used in a procedure interface, so simplify LEN in situ
    	using its character length.
    
    	PR fortran/31219
    	PR fortran/31200
    	* trans-expr.c (gfc_conv_function_call): Do not use
    	gfc_conv_expr_reference for actual pointer function with formal
    	target because a temporary is created that does not transfer
    	the reference correctly.  Do not indirect formal pointer
    	functions since it is the function reference that is needed.
    
    2007-03-24  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/31219
    	* gfortran.dg/pointer_function_actual_1.f90: New test.
    
    	PR fortran/31200
    	* gfortran.dg/pointer_function_actual_2.f90: New test.
    
    	PR fortran/31215
    	* gfortran.dg/result_in_spec_1.f90: New test.
    
    From-SVN: r123183

---
+++ gcc/testsuite/gfortran.dg/pointer_function_actual_1.f90
@@ -0,0 +1,51 @@
+! { dg-do run }
+! Tests the fix for PR31219, in which an ICE would result because
+! the reference to the pointer function f would be indirected, as
+! if it were the result that is being passed.
+!
+! COntributed by Joost VandeVondele <jv244@cam.ac.uk>
+!
+FUNCTION F() RESULT(RES)
+ INTEGER, POINTER :: RES
+ ALLOCATE(RES)
+ RES=2
+END FUNCTION F
+
+SUBROUTINE S1(f,*,*)
+ INTERFACE
+  FUNCTION F() RESULT(RES)
+   INTEGER, POINTER :: RES
+  END FUNCTION F
+ END INTERFACE
+ RETURN F()
+END SUBROUTINE
+
+PROGRAM TEST
+   INTERFACE
+    FUNCTION F() RESULT(RES)
+     INTEGER, POINTER :: RES
+    END FUNCTION F
+   END INTERFACE
+
+
+   INTERFACE
+    SUBROUTINE S1(f,*,*)
+      INTERFACE
+       FUNCTION F() RESULT(RES)
+        INTEGER, POINTER :: RES
+       END FUNCTION F
+      END INTERFACE
+     END SUBROUTINE
+   END INTERFACE
+
+   CALL S1(F,*1,*2)
+
+   1 CONTINUE
+   CALL ABORT()
+
+   GOTO 3
+   2 CONTINUE
+
+   3 CONTINUE
+END
+
---
+++ gcc/testsuite/gfortran.dg/pointer_function_actual_2.f90
@@ -0,0 +1,22 @@
+! { dg-do run }
+! Tests the fix for PR31200, in which the target x would
+! not be associated with p
+!
+! COntributed by Joost VandeVondele <jv244@cam.ac.uk>
+!
+  REAL,TARGET :: x
+  CALL s3(f(x))
+CONTAINS
+  FUNCTION f(a)
+    REAL,POINTER :: f
+    REAL,TARGET :: a
+    f => a
+  END FUNCTION
+  SUBROUTINE s3(targ)
+    REAL,TARGET :: targ
+    REAL,POINTER :: p
+    p => targ
+    IF (.NOT. ASSOCIATED(p,x)) CALL ABORT()
+  END SUBROUTINE
+END
+
