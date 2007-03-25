5773afc5b5b659bc20842e72627b492023d65cb5
Dorit Nuzman <dorit@il.ibm.com>
2007-03-25 11:08:29 +0000

    re PR middle-end/30784 (ICE on loop vectorization (-O1 -march=athlon-xp -ftree-vectorize))
            PR tree-optimization/30784
            * fold-const.c (fold_ternary): Handle CONSTRUCTOR in case
            BIT_FIELD_REF.
    
    From-SVN: r123197

---
+++ gcc/testsuite/gcc.dg/vect/pr30784.c
@@ -0,0 +1,30 @@
+/* { dg-require-effective-target vect_int } */
+
+#include <stdarg.h>
+#include "tree-vect.h"
+
+long stack_vars_sorted[32];
+
+int
+main1 (long n)
+{
+  long si;
+
+  for (si = 0; si < n; ++si)
+    stack_vars_sorted[si] = si;
+}
+
+int main ()
+{
+  long si;
+
+  check_vect ();
+  main1 (32);
+
+  for (si = 0; si < 32; ++si)
+    if (stack_vars_sorted[si] != si)
+      abort ();
+
+  return 0;
+}
+/* { dg-final { cleanup-tree-dump "vect" } } */
