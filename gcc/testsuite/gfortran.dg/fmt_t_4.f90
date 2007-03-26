659a1792a2553402713a4d249c2d4eb8a4435d84
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-26 03:41:29 +0000

    2007-03-25  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR libgfortran/31199
    	* gfortran.dg/fmt_t_4.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@123207 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/fmt_t_4.f90
@@ -0,0 +1,16 @@
+! { dg-do run }
+! PR31199, test case from PR report.
+       program write_write
+       character(len=20) :: a,b,c
+       write (10,"(a,t1,a,a)") "xxxxxxxxx", "abc", "def"
+       write (10,"(a,t1,a)",advance='no') "xxxxxxxxx", "abc"
+       write (10,"(a)") "def"
+       write (10,"(a)") "abcdefxxx"
+       rewind(10)
+       read(10,*) a
+       read(10,*) b
+       read(10,*) c
+       if (a.ne.b) call abort()
+       IF (b.ne.c) call abort()
+       end
+
