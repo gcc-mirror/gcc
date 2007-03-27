45ccdeb8c85d1cf7b438b9667be950e12ac23926
mrs <mrs@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-27 01:04:30 +0000

    * config/rs6000/darwin.h (DARWIN_MINVERSION_SPEC): Add 	objective-c-header, objective-c++-header and objc++-cpp-output 	support. 	* config/i386/darwin.h (DARWIN_MINVERSION_SPEC): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@123252 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/darwin.h
+++ gcc/config/i386/darwin.h
@@ -87,7 +87,9 @@ Boston, MA 02110-1301, USA.  */
 #define DARWIN_MINVERSION_SPEC				\
  "%{!m64|fgnu-runtime:10.4;				\
     ,objective-c|,objc-cpp-output:10.5;			\
+    ,objective-c-header:10.5;				\
     ,objective-c++|,objective-c++-cpp-output:10.5;	\
+    ,objective-c++-header|,objc++-cpp-output:10.5;	\
     :10.4}"
 
 #undef SUBTARGET_EXTRA_SPECS
