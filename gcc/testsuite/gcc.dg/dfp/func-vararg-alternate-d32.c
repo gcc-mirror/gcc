9f363d2ac246d19590a65900ae9747df48719384
Janis Johnson <janis187@us.ibm.com>
2007-03-27 23:38:05 +0000

    func-vararg-alternate.h: New file.
    	* gcc.dg/dfp/func-vararg-alternate.h: New file.
    	* gcc.dg/dfp/func-vararg-alternate-d32.c: New test.
    	* gcc.dg/dfp/func-vararg-alternate-d64.c: New test.
    	* gcc.dg/dfp/func-vararg-alternate-d128.c: New test.
    
    From-SVN: r123282

---
+++ gcc/testsuite/gcc.dg/dfp/func-vararg-alternate-d32.c
@@ -0,0 +1,20 @@
+/* { dg-options "-std=gnu99" } */
+
+/* Simple test of vararg passing for problematic types with and without
+   double values passed between them.  */
+
+#define DTYPE _Decimal32
+#define ONE 1.0df
+#define THREE 3.0df
+#define SEVEN 7.0df
+#define ELEVEN 11.0df
+#define INTS 1
+
+#include "func-vararg-alternate.h"
+
+int
+main ()
+{
+  doit ();
+  return 0;
+}
