74aab153b68fc0104587de60b95f628c9d6c4f4e
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-28 20:11:20 +0000

    * tree-dfa.c (get_ref_base_and_extent): Do not expect positive 	offsets for BIT_FIELD_REF and COMPONENT_REF.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@123311 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gnat.dg/thin_pointer.ads
@@ -0,0 +1,22 @@
+with System;
+
+package Thin_Pointer is
+
+   type Stream is array (Integer range <>) of Character;
+
+   type Stream_Ptr is access Stream;
+   for Stream_Ptr'Size use Standard'Address_Size;
+
+   type Buf is record
+      A : System.Address;
+   end record;
+
+   type Buf_Wrapper is record
+      B : Buf;
+   end record;
+
+   type Buf_Ptr is access Buf_Wrapper;
+
+   procedure Set_Buffer (AD : Buf_Ptr; Buffer : Stream_ptr);
+
+end Thin_Pointer;
