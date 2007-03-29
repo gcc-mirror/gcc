a5cb63da389310aedb799916a1c6107af50cb879
brobecke <brobecke@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-29 20:55:57 +0000

    * filenames.h (FILENAME_CMP): Adjust define to call filename_cmp         regardless of the type of file system.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@123343 138bc75d-0d04-0410-961f-82ee72b054a4

--- include/filenames.h
+++ include/filenames.h
@@ -5,7 +5,7 @@
    use forward- and back-slash in path names interchangeably, and
    some of them have case-insensitive file names.
 
-   Copyright 2000, 2001 Free Software Foundation, Inc.
+   Copyright 2000, 2001, 2007 Free Software Foundation, Inc.
 
 This file is part of BFD, the Binary File Descriptor library.
 
@@ -38,14 +38,15 @@ Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA. 
    want to know whether to prepend the current working directory to
    a file name, which should not be done with a name like d:foo.  */
 #define IS_ABSOLUTE_PATH(f)	(IS_DIR_SEPARATOR((f)[0]) || (((f)[0]) && ((f)[1] == ':')))
-#define FILENAME_CMP(s1, s2)	strcasecmp(s1, s2)
 
 #else  /* not DOSish */
 
 #define IS_DIR_SEPARATOR(c)	((c) == '/')
 #define IS_ABSOLUTE_PATH(f)	(IS_DIR_SEPARATOR((f)[0]))
-#define FILENAME_CMP(s1, s2)	strcmp(s1, s2)
 
 #endif /* not DOSish */
+
+extern int filename_cmp (const char *s1, const char *s2);
+#define FILENAME_CMP(s1, s2)	filename_cmp(s1, s2)
 
 #endif /* FILENAMES_H */
