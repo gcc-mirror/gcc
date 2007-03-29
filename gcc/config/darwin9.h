d9dbda201bedc78cb887b867bc7cf964651e8608
Mike Stump <mrs@gcc.gnu.org>
2007-03-29 18:36:43 +0000

    * config/darwin9.h (ASM_OUTPUT_ALIGNED_COMMON): Fix typo.
    From-SVN: r123339

--- gcc/config/darwin9.h
+++ gcc/config/darwin9.h
@@ -24,10 +24,10 @@
 #undef  ASM_OUTPUT_ALIGNED_COMMON
 #define ASM_OUTPUT_ALIGNED_COMMON(FILE, NAME, SIZE, ALIGN)		\
   do {									\
-    unsigned HOST_WIDE_INT _new_size = SIZE;				\
+    unsigned HOST_WIDE_INT _new_size = (SIZE);				\
     fprintf ((FILE), ".comm ");						\
     assemble_name ((FILE), (NAME));					\
     if (_new_size == 0) _new_size = 1;					\
     fprintf ((FILE), ","HOST_WIDE_INT_PRINT_UNSIGNED",%u\n",		\
-	     (SIZE), floor_log2 ((ALIGN) / BITS_PER_UNIT));		\
+	     _new_size, floor_log2 ((ALIGN) / BITS_PER_UNIT));		\
   } while (0)
