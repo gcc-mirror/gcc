ccfa609a4ed2efc76539be2d9a31bd9c581f8500
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-04-01 15:29:05 +0000

    2007-04-01  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR libgfortran/31366
    	* gfortran.dg/direct_io_6.f90: New test.
    	
    2007-04-01  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/31207
    	* gfortran.dg/advance_5.f90: New test.
    	* gfortran.dg/advance_6.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@123402 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/advance_4.f90
@@ -0,0 +1,10 @@
+! { dg-do run }
+! PR31207 Last record truncated for read after short write
+program main
+  character(10) :: answer
+  write (12,'(A,T2,A)',advance="no") 'XXXXXX','ABCD'
+  close (12)
+  read (12, '(6A)') answer
+  close (12, status="delete")
+  if (answer /= "XABCDX") call abort()
+end program main
