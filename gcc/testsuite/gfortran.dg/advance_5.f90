201c811d49e228c5e88f1e794f05d145b50e9128
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2007-04-01 15:29:05 +0000

    re PR fortran/31366 ([4.2 only] Last record truncated for read after short write, direct access file)
    2007-04-01  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/31366
    	* gfortran.dg/direct_io_6.f90: New test.
    	
    2007-04-01  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/31207
    	* gfortran.dg/advance_5.f90: New test.
    	* gfortran.dg/advance_6.f90: New test.
    
    From-SVN: r123402

---
+++ gcc/testsuite/gfortran.dg/advance_5.f90
@@ -0,0 +1,17 @@
+! { dg-do run }
+! PR31207 Last record truncated for read after short write.
+character(len=20) :: b
+! write something no advance
+open(10,file="fort.10",position="rewind")
+write(10, '(a,t1,a)',advance='no') 'xxxxxx', 'abc'
+close(10)
+! append some data
+open(10,file="fort.10",position="append")
+write(10, '(a)') 'def'
+close(10)
+! check what is in the first record
+open(10,file="fort.10",position="rewind")
+read(10,'(a)') b
+close(10, status="delete")
+if (b.ne."abcxxx") call abort()
+end
