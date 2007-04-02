f77e0a1a5c3712c21354e798dff78e8754cd2275
schwab <schwab@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-04-02 11:21:23 +0000

    * filename_cmp.c: Include "config.h".
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@123425 138bc75d-0d04-0410-961f-82ee72b054a4

--- libiberty/filename_cmp.c
+++ libiberty/filename_cmp.c
@@ -16,6 +16,10 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #ifdef HAVE_STRING_H
 #include <string.h>
 #endif
