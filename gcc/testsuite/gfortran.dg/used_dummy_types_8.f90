145bdc2cbc1237d3f3ab25f45d18f1fce7ecb2b5
Paul Thomas <pault@gcc.gnu.org>
2007-04-07 20:25:43 +0000

    re PR fortran/30880 (Derived types with default value -- function with ENTRY: rejected at compile time)
    2007-04-07  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/30880
    	* resolve.c (resolve_fl_variable): Set flag to 2 for automatic
    	arrays.  Make condition for automatic array error explicit.
    	If a dummy, no error on an INTENT(OUT) derived type.
    
    2007-04-07  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/30880
    	* gfortran.dg/used_dummy_types_8.f90: New test.
    
    From-SVN: r123645

---
+++ gcc/testsuite/gfortran.dg/used_dummy_types_8.f90
@@ -0,0 +1,35 @@
+! { dg-do compile }
+! Tests the fix for PR30880, in which the variable d1
+! in module m1 would cause an error in the main program
+! because it has an initializer and is a dummy.  This
+! came about because the function with multiple entries
+! assigns the initializer earlier than for other cases.
+!
+! Contributed by Joost VandeVondele <jv244@cam.ac.uk> 
+!
+MODULE M1
+  TYPE T1
+  INTEGER :: i=7
+  END TYPE T1
+CONTAINS
+  FUNCTION F1(d1) RESULT(res)
+    INTEGER :: res
+    TYPE(T1), INTENT(OUT) :: d1
+    TYPE(T1), INTENT(INOUT) :: d2
+    res=d1%i
+    d1%i=0
+    RETURN
+  ENTRY E1(d2) RESULT(res)
+    res=d2%i
+    d2%i=0
+  END FUNCTION F1
+END MODULE M1
+
+  USE M1
+  TYPE(T1) :: D1
+  D1=T1(3)
+  write(6,*) F1(D1)
+  D1=T1(3)
+  write(6,*) E1(D1)
+END
+! { dg-final { cleanup-modules "m1" } }
