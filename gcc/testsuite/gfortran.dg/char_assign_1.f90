9b5b47d35f9af0d313b66599214181db8c99265c
tobi <tobi@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-04-12 18:30:06 +0000

    File missing from previous commit
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@123762 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/char_assign_1.f90
@@ -0,0 +1,23 @@
+! { dg-do run }
+! { dg-options "-Wcharacter-truncation" }
+! Tests the fix for PR31266: references to CHARACTER
+! components lead to the wrong length being assigned to substring
+! expressions.
+type data
+   character(len=5) :: c
+end type data
+type(data), dimension(5), target :: y
+character(len=2), dimension(5) :: p
+character(len=3), dimension(5) :: q
+
+y(:)%c = "abcdef" ! { dg-warning "in assignment \\(5/6\\)" }
+p(1) = y(1)%c(3:)
+if (p(1).ne."cd") call abort()
+
+p(1) = y(1)%c  ! { dg-warning "in assignment \\(2/5\\)" }
+if (p(1).ne."ab") call abort()
+
+q = "xyz"
+p = q ! { dg-warning "CHARACTER expression will be truncated in assignment \\(2/3\\)" }
+if (any (p.ne.q(:)(1:2))) call abort()
+end
