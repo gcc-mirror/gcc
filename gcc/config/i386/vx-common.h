f4de8ba686f8a4c7cc2d7ff9fa53315e826560c4
Richard Sandiford <richard@codesourcery.com>
2007-04-12 12:47:05 +0000

    config.gcc (*-*-vxworks*): Don't add to tm_files in this stanza.
    gcc/
    	* config.gcc (*-*-vxworks*): Don't add to tm_files in this stanza.
    	(arm-wrs-vxworks, mips-wrs-vxworks, powerpc-wrs-vxworks)
    	(powerpc-wrs-vxworksae): Use ${tm_file}.
    	(i[4567]86-wrs-vxworks, i[4567]86-wrs-vxworksae): Add svr4.h
    	after elfos.h.  Remove i386/sysv4.h and add i386/vx-common.h.
    	* config/i386/vx-common.h: New file.
    
    From-SVN: r123744

---
+++ gcc/config/i386/vx-common.h
@@ -0,0 +1,30 @@
+/* IA32 VxWorks and VxWorks AE target definitions.
+   Copyright (C) 2007 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING.  If not, write to
+the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.  */
+
+#define ASM_OUTPUT_ALIGNED_BSS(FILE, DECL, NAME, SIZE, ALIGN) \
+  asm_output_aligned_bss (FILE, DECL, NAME, SIZE, ALIGN)
+
+/* The svr4 ABI for the i386 says that records and unions are returned
+   in memory.  */
+
+#undef RETURN_IN_MEMORY
+#define RETURN_IN_MEMORY(TYPE) \
+  (TYPE_MODE (TYPE) == BLKmode \
+   || (VECTOR_MODE_P (TYPE_MODE (TYPE)) && int_size_in_bytes (TYPE) == 8))
