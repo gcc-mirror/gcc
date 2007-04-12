e14bee04a2b3177bacc5a1973cf025e2d46e2926
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-04-12 08:46:30 +0000

    2007-04-12  Tobias Burnus  <burnus@net-b.de>
    	PR fortran/31472
    	* decl.c (match_attr_spec): Allow PRIVATE/PUBLIC
    	attribute in type definitions.
    	(gfc_match_private): Allow PRIVATE statement only
    	in specification part of modules.
    	(gfc_match_public): Ditto for PUBLIC.
    	(gfc_match_derived_decl): Allow PRIVATE/PUBLIC attribute only in
    	specificification part of modules.
    
    2007-04-12  Tobias Burnus  <burnus@net-b.de>
    
    	PR fortran/31472
    	* gfortran.dg/access_spec_1.f90: New test.
    	* gfortran.dg/access_spec_2.f90: New test.
    	* gfortran.dg/non_module_public.f90: Match new error message.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@123735 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/non_module_public.f90
+++ gcc/testsuite/gfortran.dg/non_module_public.f90
@@ -1,5 +1,5 @@
 ! { dg-do compile }
 ! PR20837 - A symbol may not be declared PUBLIC or PRIVATE outside a module.
 ! Contributed by Joost VandeVondele <jv244@cam.ac.uk>
-integer, parameter, public :: i=1 ! { dg-error "allowed outside of a MODULE" }
+integer, parameter, public :: i=1 ! { dg-error "outside of the specification part of a module" }
 END
