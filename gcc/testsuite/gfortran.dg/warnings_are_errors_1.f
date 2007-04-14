db2f00f5edad92f245b7fbe9cc389021b0fbba42
aldot <aldot@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-04-14 18:33:11 +0000

    2007-04-14  Bernhard Fischer
    	PR fortran/21061
    	* gfortran.dg/warnings_are_errors_1.f90: New testcase.
    	* gfortran.dg/warnings_are_errors_1.f: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@123820 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/warnings_are_errors_1.f
@@ -0,0 +1,24 @@
+! { dg-do compile }
+! { dg-options " -Werror" }
+! PR fortran/21061
+! gfortran ignores -Werror
+! fixed-form tests
+       program warnings_are_errors_1
+       implicit none
+       integer(kind=1) :: i
+       real :: r1, r2(3)
+! gfc_warning_now:
+0      ! { dg-warning "Zero is not a valid statement label" }
+!
+34 5   i=0 
+! gfc_notify_std(GFC_STD_F95_DEL):
+       do r1 = 1.0, 2 ! { dg-warning "Obsolete: REAL DO loop iterator" }
+         i = i+1
+       end do
+       call foo j bar
+! gfc_warning:
+       r2(4) = 0 ! { dg-warning "is out of bounds" }
+       
+       goto 3 45
+       end
+! { dg-final { output-exists-not } }
---
+++ gcc/testsuite/gfortran.dg/warnings_are_errors_1.f90
@@ -0,0 +1,26 @@
+! { dg-do compile }
+! { dg-options "-Werror -Wunused" }
+! PR fortran/21061
+! gfortran ignores -Werror
+! free-form tests
+
+! gfc_notify_std:
+       function char_ (ch) ! { dg-warning "is obsolescent in fortran 95" }
+       character(*) :: char_, ch
+        char_ = ch
+       end function char_
+
+! warning(0,...):
+!      function wrong_warn (i) ! { -warning "Function does not return a value" }
+!      integer i
+!      end function wrong_warn
+
+       implicit none
+! gfc_warning:
+1234  complex :: cplx ! { dg-warning "defined but cannot be used" }
+      cplx = 20.
+
+! gfc_warning_now:
+ 1 ! { dg-warning "Ignoring statement label in empty statement" }
+       end
+! { dg-final { output-exists-not } }
