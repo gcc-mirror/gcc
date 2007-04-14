92dcf50a789fac602b78200ddba9a1f4b99dfab1
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-04-14 17:09:41 +0000

    PR fortran/31561 	* gfortran.dg/vect/vect-4.f90: xfail on vect_no_align targets.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@123817 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/vect/vect-4.f90
+++ gcc/testsuite/gfortran.dg/vect/vect-4.f90
@@ -10,7 +10,7 @@ Y = Y + A * X
 END
 
 ! { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } } 
-! { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 1 "vect" } }
-! { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 1 "vect" } }
+! { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 1 "vect" { xfail vect_no_align } } }
+! { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 1 "vect" { xfail vect_no_align } } }
 ! { dg-final { scan-tree-dump-times "accesses have the same alignment." 1 "vect" } }
 ! { dg-final { cleanup-tree-dump "vect" } }
