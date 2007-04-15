0dd13847494549108e669e45e74aced4d4435be8
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-04-15 00:59:15 +0000

    2007-04-14  Andrew Pinski  <andrew_pinski@playstation.sony.com>
            PR c/31520
            * c-del.c (finish_decl): Grab the type of the decl after the call
            to store_init_value.
    2007-04-14  Andrew Pinski  <andrew_pinski@playstation.sony.com>
    
            PR C/31520
            * testsuite/gcc.dg/gnu89-init-4.c: New testcase.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@123835 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-decl.c
+++ gcc/c-decl.c
@@ -3383,7 +3383,7 @@ c_maybe_initialize_eh (void)
 void
 finish_decl (tree decl, tree init, tree asmspec_tree)
 {
-  tree type = TREE_TYPE (decl);
+  tree type;
   int was_incomplete = (DECL_SIZE (decl) == 0);
   const char *asmspec = 0;
 
@@ -3409,6 +3409,8 @@ finish_decl (tree decl, tree init, tree asmspec_tree)
 			    || TREE_CODE (decl) == FUNCTION_DECL
 			    || TREE_CODE (decl) == FIELD_DECL))
     objc_check_decl (decl);
+
+  type = TREE_TYPE (decl);
 
   /* Deduce size of array from initialization, if not already known.  */
   if (TREE_CODE (type) == ARRAY_TYPE
