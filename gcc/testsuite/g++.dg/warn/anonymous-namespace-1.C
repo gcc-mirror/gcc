8fff206014983b3f02f42b442d4b7c94942fc1de
Seongbae Park <seongbae.park@gmail.com>
2007-04-16 16:49:02 +0000

    re PR c++/29365 (Unnecessary anonymous namespace warnings)
    gcc/ChangeLog:
    
    2007-04-16  Seongbae Park <seongbae.park@gmail.com>
    
    	PR c++/29365
    
    	* cp/decl2.c (constrain_class_visibility):
    	Do not warn about the use of anonymous namespace in the main input file.
    
    gcc/testsuite/ChangeLog:
    
    2007-04-16  Seongbae Park <seongbae.park@gmail.com>
    
    	PR c++/29365
    	Testcase for c++ anonymous namespace warning
    
    	* g++.dg/warn/anonymous-namespace-1.C: New test
    	* g++.dg/warn/anonymous-namespace-1.h: New test
    
    From-SVN: r123879

---
+++ gcc/testsuite/g++.dg/warn/anonymous-namespace-1.C
@@ -0,0 +1,17 @@
+// Test for the warning of exposing types from an anonymous namespace
+// { dg-do compile }
+
+#include "anonymous-namespace-1.h"
+
+namespace {
+ class good { };
+}
+
+class foo::bar : public good { }; 
+class foobar1
+{
+  good g;
+};
+
+class foobar : public bad { }; // { dg-warning "uses the anonymous namespace" }
+class foobar2 { bad b; }; // { dg-warning "uses the anonymous namespace" }
