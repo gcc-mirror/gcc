5f4a9d838ac518b3c88b6642b99aa46169f59904
Simon Martin <simartin@users.sourceforge.net>
2007-04-17 19:41:12 +0000

    re PR c++/31517 (ICE on invalid initialization of static member of a template class)
    2007-04-17  Simon Martin  <simartin@users.sourceforge.net>
    
    	PR c++/31517
    	* pt.c (value_dependent_expression_p): Handle MODOP_EXPRs.
    
    From-SVN: r123925

---
+++ gcc/testsuite/g++.dg/template/init7.C
@@ -0,0 +1,9 @@
+/* PR c++/31517. This used to ICE.  */
+/* { dg-do "compile" } */
+
+template<typename> struct A
+{
+  static const int i=0;
+};
+
+template<typename T> const int A<T>::i = 0=0; /* { dg-error "duplicate initialization" } */
