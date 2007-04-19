759a2de39acb5bc66b70f343d84e933ea04dfb9a
Eric Botcazou <ebotcazou@libertysurf.fr>
2007-04-19 13:19:39 +0200

    re PR rtl-optimization/29841 (ICE with scheduling and __builtin_trap)
    	PR rtl-optimization/29841
    	* cfgbuild.c (control_flow_insn_p): Return TRUE for unconditional
    	trap instructions.
    	* sched-deps.c (sched_analyze_insn): Prevent all non-jump instructions
    	that may cause control flow transfer from being moved.
    
    From-SVN: r123971

---
+++ gcc/testsuite/gcc.dg/invalid-call-1.c
@@ -0,0 +1,17 @@
+/* PR rtl-optimization/29841 */
+/* Testcase by Khem Raj <raj.khem@gmail.com> */
+
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+/* { dg-options "-O2 -mtune=i586" { target i?86-*-* } } */
+
+typedef void (*fp)(void);
+extern char* bar(void* a1, int a2);
+extern char* mar(int n);
+char* cptr;
+
+void foo()
+{
+  cptr = mar(6);
+  ((char *(*)(void *,int (*)(void *,unsigned char **),char**))((fp)bar))(0,0,(void*)(0)); /* { dg-warning "" "non-compatible type" } */
+}
