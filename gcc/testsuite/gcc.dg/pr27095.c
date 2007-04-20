e7cb6415e55d4d8a69449a687a09584b3a7b6488
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-04-20 17:38:10 +0000

    * gcc.dg/pr28796-2.c: Add -mieee for alpha.         * gcc.dg/pr27095.c: Cope with alpha GOT load.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@123998 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr27095.c
+++ gcc/testsuite/gcc.dg/pr27095.c
@@ -11,10 +11,12 @@ main (int argc, char **argv)
   memset (x, argc, strlen (x));
   return 0;
 }
-/* { dg-final { scan-assembler-not "(?n)strlen\(.*\n\)+.*strlen" { target { ! { powerpc*-*-darwin* hppa*-*-hpux* ia64-*-hpux* } } } } } */
+/* { dg-final { scan-assembler-not "(?n)strlen\(.*\n\)+.*strlen" { target { ! { powerpc*-*-darwin* hppa*-*-hpux* ia64-*-hpux* alpha*-*-* } } } } } */
 /* hppa*-*-hpux* has an IMPORT statement for strlen (plus the branch). */
 /* *-*-darwin* has something similar. */
 /* { dg-final { scan-assembler-not "(?n)strlen\(.*\n\)+.*strlen\(.*\n\)+.*strlen" { target hppa*-*-hpux* } } } */
 /* { dg-final { scan-assembler-not "(?n)bl L_strlen\(.*\n\)+.*bl L_strlen" { target powerpc*-*-darwin* } } } */
 /* ia64-*-hpux* has a global statement, a type statement, and the branch. */
 /* { dg-final { scan-assembler-not "(?n)strlen\(.*\n\)+.*strlen\(.*\n\)+.*strlen\(.*\n\)+.*strlen" { target ia64-*-hpux* } } } */
+/* alpha-*-* has a GOT load and the call.  */
+/* { dg-final { scan-assembler-not "(?n)jsr .*,strlen\(.*\n\)+.*jsr .*,strlen" { target alpha*-*-* } } } */
