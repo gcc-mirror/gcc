c807acf9c4a7e9fe7cb96e7f5d3112def5c4b2cf
Nick Clifton <nickc@redhat.com>
2007-04-22 21:11:59 +0000

    * gcc.dg/20020312-2.c: Add support for the FRV.
    From-SVN: r124046

--- gcc/testsuite/gcc.dg/20020312-2.c
+++ gcc/testsuite/gcc.dg/20020312-2.c
@@ -74,6 +74,12 @@ extern void abort (void);
 /* No pic register.  */
 #elif defined(__SPU__)
 #  define PIC_REG  "126"
+#elif defined __frv__
+# ifdef __FRV_FDPIC__
+#  define PIC_REG "gr15"
+# else
+#  define PIC_REG "gr17"
+#endif
 #else
 # error "Modify the test for your target."
 #endif
