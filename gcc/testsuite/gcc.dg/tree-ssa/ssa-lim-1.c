e0a607311cc1cb15070da6cd2c2e10f8eeae867b
Richard Guenther <rguenther@suse.de>
2007-04-22 11:26:49 +0000

    re PR tree-optimization/29789 (Missed invariant out of the loop with conditionals and shifts)
    2007-04-22  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/29789
    	* tree-ssa-loop-im.c (stmt_cost): Adjust cost of shifts.
    	(rewrite_reciprocal): New helper split out from
    	determine_invariantness_stmt.
    	(rewrite_bittest): Likewise.
    	(determine_invariantness_stmt): Rewrite (A >> B) & 1 to
    	A & (1 << B) if (1 << B) is loop invariant but (A >> B)
    	is not.
    
    	* gcc.dg/tree-ssa/ssa-lim-1.c: New testcase.
    	* gcc.dg/tree-ssa/ssa-lim-2.c: Likewise.
    
    From-SVN: r124042

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-lim-1.c
@@ -0,0 +1,22 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-lim" } */
+
+/* This is a variant that does cause fold to place a cast to
+   int before testing bit 1.  */
+
+void
+quantum_toffoli (int control1, int control2, int target,
+		 unsigned long *state, int size)
+{
+  int i;
+
+  for(i=0; i<size; i++)
+    {
+       if (state[i] & ((unsigned long) 1 << control1))
+         if (state[i] & ((unsigned long) 1 << control2))
+           state[i] ^= ((unsigned long) 1 << target);
+    }
+}
+
+/* { dg-final { scan-tree-dump-times "shifttmp" 6 "lim" } } */
+/* { dg-final { cleanup-tree-dump "lim" } } */
