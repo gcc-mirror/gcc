676b23ba25b9f21507847fe1f7a90266aaa2e81f
Olga Golovanevsky <olga@gcc.gnu.org>
2007-04-24 15:58:44 +0000

    typo fix in str_empty.C
    From-SVN: r124110

--- gcc/testsuite/g++.dg/other/str_empty.C
+++ gcc/testsuite/g++.dg/other/str_empty.C
@@ -1,5 +1,5 @@
 // PR c++/31617
-// Segfault in integer_rezop
+// Segfault in integer_zerop
 // Origin: Andrew Pinski  <andrew_pinski@playstation.sony.com>
 // { dg-do compile }
 // { dg-options "-O2" }
