4df28528f69a4400fed72d69792fc043d1ebd826
Ian Lance Taylor <iant@google.com>
2007-04-24 20:44:45 +0000

    re PR tree-optimization/31602 (Overflow warning causes GDB -Werror build failure)
    ./:	PR tree-optimization/31602
    	* tree-ssa-loop-ch.c (copy_loop_headers): Set TREE_NO_WARNING for
    	conditionals in the copied loop header.
    	* tree-cfg.c (fold_cond_expr_cond): Don't issue undefined overflow
    	warnings if TREE_NO_WARNING is set.
    	* doc/invoke.texi (Warning Options): Clarify that
    	-Wstrict-overflow does not warn about loops.
    testsuite/:
    	PR tree-optimization/31602
    	* gcc.dg/Wstrict-overflow-11.c: We no longer issue a warning.
    
    From-SVN: r124120

--- gcc/testsuite/gcc.dg/Wstrict-overflow-11.c
+++ gcc/testsuite/gcc.dg/Wstrict-overflow-11.c
@@ -3,14 +3,16 @@
 
 /* Based on strict-overflow-5.c.  */
 
-/* We can only unroll when using strict overflow semantics.  */
+/* We can only unroll when using strict overflow semantics.  But we
+   don't issue a warning for relying on undefined overflow in
+   loops.  */
 
 int foo (int i)
 {
   int index;
   int r=0;
  
-  for (index = i; index <= i+4; index+=2) /* { dg-warning "assuming signed overflow does not occur" "correct warning" } */
+  for (index = i; index <= i+4; index+=2)
     r++;
  
   return r;
