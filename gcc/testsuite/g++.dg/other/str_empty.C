9905fb5e5fede0e4d865b3ba42b9ecb35e40c0c4
Olga Golovanevsky <olga@gcc.gnu.org>
2007-04-24 15:49:09 +0000

    new file str_empty.C
    From-SVN: r124108

---
+++ gcc/testsuite/g++.dg/other/str_empty.C
@@ -0,0 +1,16 @@
+// PR c++/31617
+// Segfault in integer_rezop
+// Origin: Andrew Pinski  <andrew_pinski@playstation.sony.com>
+// { dg-do compile }
+// { dg-options "-O2" }
+
+struct polynomial {
+  ~polynomial ();
+};
+
+void spline_rep1 ()
+{
+  new polynomial[0];
+}
+
+
