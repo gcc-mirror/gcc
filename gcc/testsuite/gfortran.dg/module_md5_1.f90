d98a483229d68800ec865a954830c902774bd3b9
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-04-24 21:37:37 +0000

    PR fortran/31587
    	* lib/gcc-dg.exp (scan-module): New function.
    	* gfortran.dg/module_md5_1.f90: New test.
    
    	* module.c (write_char): Add character to the MD5 buffer.
    	(read_md5_from_module_file): New function.
    	(gfc_dump_module): Compute MD5 for new module file. Call
    	read_md5_from_module_file. Only overwrite old module file
    	if the new MD5 is different.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@124126 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/module_md5_1.f90
@@ -0,0 +1,14 @@
+! Check that we can write a module file, that it has a correct MD5 sum,
+! and that we can read it back.
+!
+! { dg-do compile }
+module foo
+  integer(kind=4), parameter :: pi = 3_4
+end module foo
+
+program test
+  use foo
+  print *, pi
+end program test
+! { dg-final { scan-module "foo" "MD5:18a257e13c90e3872b7b9400c2fc6e4b" } }
+! { dg-final { cleanup-modules "foo" } }
