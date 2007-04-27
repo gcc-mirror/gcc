5f47119618c89ef4c8dd30ca36bfc8a8c2400958
Andrew Pinski <andrew_pinski@playstation.sony.com>
2007-04-27 01:37:42 +0000

    re PR c++/30016 (internal compiler error: in convert_move, at expr.c:362)
    2007-04-26  Andrew Pinski  <andrew_pinski@playstation.sony.com>
    
            PR C++/30016
            * typeck.c (build_reinterpret_cast_1): Only allow conversion to
            integeral types from vectors types.
    
    2007-04-26  Andrew Pinski  <andrew_pinski@playstation.sony.com>
    
            PR C++/30016
            * g++.dg/ext/vector6.C: New test.
    
    From-SVN: r124210

--- gcc/cp/typeck.c
+++ gcc/cp/typeck.c
@@ -5094,7 +5094,7 @@ build_reinterpret_cast_1 (tree type, tree expr, bool c_cast_p,
     }
   else if (TREE_CODE (type) == VECTOR_TYPE)
     return fold_if_not_in_template (convert_to_vector (type, expr));
-  else if (TREE_CODE (intype) == VECTOR_TYPE)
+  else if (TREE_CODE (intype) == VECTOR_TYPE && INTEGRAL_TYPE_P (type))
     return fold_if_not_in_template (convert_to_integer (type, expr));
   else
     {
