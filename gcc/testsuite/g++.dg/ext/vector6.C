4a13c3cd6deb4ab8052b3a4de1f643682a46f4a2
Andrew Pinski <andrew_pinski@playstation.sony.com>
2007-04-27 01:37:02 +0000

    re PR c++/30016 (internal compiler error: in convert_move, at expr.c:362)
    2007-04-26  Andrew Pinski  <andrew_pinski@playstation.sony.com>
    
            PR C++/30016
            * typeck.c (build_reinterpret_cast_1): Only allow conversion to
            integeral types from vectors types.
    
    2007-04-26  Andrew Pinski  <andrew_pinski@playstation.sony.com>
    
            PR C++/30016
            * g++.dg/ext/vector6.C: New test.
    
    From-SVN: r124209

---
+++ gcc/testsuite/g++.dg/ext/vector6.C
@@ -0,0 +1,12 @@
+// { dg-options "" }
+// { dg-do compile }
+// C++/30016, we were allowing conversion between vector types
+// and union types which is invalid.
+
+typedef float __v_4F __attribute__ ((vector_size (16)));
+typedef union {__v_4F v; float a[4];} __v4F;
+void f(void)
+{
+      __v_4F b;
+      (reinterpret_cast<__v4F>(b).a)[1] = 1; // { dg-error "" }
+}
