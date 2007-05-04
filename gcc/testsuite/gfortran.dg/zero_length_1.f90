70aa27669b01c03118b7e18596aafbb0b00f1cf1
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2007-05-04 15:20:17 +0000

    re PR libfortran/31210 (I/O of string with (non-constant) zero length)
    	PR libfortran/31210
    	* gfortran.dg/zero_length_1.f90: New test.
    
    From-SVN: r124429

---
+++ gcc/testsuite/gfortran.dg/zero_length_1.f90
@@ -0,0 +1,18 @@
+! { dg-do run }
+! PR libfortran/31210
+program test
+  implicit none
+  integer :: l = 0
+  character(len=20) :: s
+  
+  write(s,'(A,I1)') foo(), 0
+  if (trim(s) /= "0") call abort
+
+contains
+
+  function foo()
+    character(len=l) :: foo
+    foo = "XXXX"
+  end function
+
+end program test
