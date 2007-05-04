de38521a76eda53936caccdc410fee503fee2ce9
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-05-04 11:10:06 +0000

    PR fortran/31251 	* simplify.c (gfc_simplify_len): Only simplify integer lengths.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@124415 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/simplify.c
+++ gcc/fortran/simplify.c
@@ -2136,14 +2136,15 @@ gfc_simplify_len (gfc_expr *e)
     }
 
   if (e->ts.cl != NULL && e->ts.cl->length != NULL
-      && e->ts.cl->length->expr_type == EXPR_CONSTANT)
+      && e->ts.cl->length->expr_type == EXPR_CONSTANT
+      && e->ts.cl->length->ts.type == BT_INTEGER)
     {
       result = gfc_constant_result (BT_INTEGER, gfc_default_integer_kind,
 				    &e->where);
       mpz_set (result->value.integer, e->ts.cl->length->value.integer);
       return range_check (result, "LEN");
     }
-  
+
   return NULL;
 }
 
