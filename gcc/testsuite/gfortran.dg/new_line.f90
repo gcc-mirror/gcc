22aef73f17b09c1805aee5bb4a64f1e012bec49d
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2007-05-06 22:56:52 +0000

    re PR fortran/31764 (NEW_LINE with array argument)
    	PR fortran/31764
    
    	* simplify.c (gfc_simplify_new_line): NEW_LINE can be simplified
    	even for non constant arguments.
    
    	* gfortran.dg/new_line.f90: Add new checks.
    
    From-SVN: r124482

--- gcc/testsuite/gfortran.dg/new_line.f90
+++ gcc/testsuite/gfortran.dg/new_line.f90
@@ -2,6 +2,19 @@
 ! Checks Fortran 2003's new_line intrinsic function
 ! PR fortran/28585
 program new_line_check
-    implicit none
-    if(achar(10) /= new_line('a')) call abort()
+  implicit none
+  character(len=10) :: a1
+  character(len=10) :: a2(2)
+  character(len=10), parameter :: a3 = "1234567890"
+  character(len=10), parameter :: a4(2) = "1234567890"
+  character(len=10), parameter :: a5(2) = repeat("1234567890",2)
+
+  if(achar(10) /= new_line('a')) call abort
+
+  if (iachar(new_line(a1)) /= 10) call abort
+  if (iachar(new_line(a2)) /= 10) call abort
+  if (iachar(new_line(a3)) /= 10) call abort
+  if (iachar(new_line(a4)) /= 10) call abort
+  if (iachar(new_line(a5)) /= 10) call abort
+
 end program new_line_check
