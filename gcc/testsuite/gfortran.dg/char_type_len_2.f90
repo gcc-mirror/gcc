16f8ffc831e99cf28ed7fe2aa9f96ef8dda30678
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2007-05-06 04:10:53 +0000

    re PR fortran/31251 (Non-integer character length leads to segfault)
    2007-05-05  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR fortran/31251
    	* decl.c (match_char_spec): Add check for invalid character lengths.
    
    2007-05-05  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR fortran/31251
    	* gfortran.dg/char_type_len_2.f90: New test.
    
    From-SVN: r124469

---
+++ gcc/testsuite/gfortran.dg/char_type_len_2.f90
@@ -0,0 +1,8 @@
+! { dg-do compile }
+! PR31251 Non-integer character length leads to segfault
+! Submitted by Jerry DeLisle  <jvdelisle@gcc.gnu.org>
+  character(len=2.3) :: s ! { dg-error "must be of INTEGER type" }
+  character(kind=1,len=4.3) : t ! { dg-error "must be of INTEGER type" }
+  character(len=,,7.2,kind=1) : u ! { dg-error "Syntax error in CHARACTER declaration" }
+  character(len=7,kind=2) : v ! ! { dg-error "Kind 2 is not a CHARACTER kind" }
+  end
