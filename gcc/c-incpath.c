b715888442912dde83d607e9a1fb9eceb0db4d49
froydnj <froydnj@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-05-07 17:41:35 +0000

    PR 22133 	* c-incpath.c (add_path): Strip trailing path separators.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@124504 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-incpath.c
+++ gcc/c-incpath.c
@@ -389,13 +389,18 @@ add_path (char *path, int chain, int cxx_aware, bool user_supplied_p)
   cpp_dir *p;
 
 #if defined (HAVE_DOS_BASED_FILE_SYSTEM)
-  /* Convert all backslashes to slashes.  The native CRT stat()
-     function does not recognize a directory that ends in a backslash
-     (unless it is a drive root dir, such "c:\").  Forward slashes,
-     trailing or otherwise, cause no problems for stat().  */
-  char* c;
-  for (c = path; *c; c++)
-    if (*c == '\\') *c = '/';
+  /* Remove unnecessary trailing slashes.  On some versions of MS
+     Windows, trailing  _forward_ slashes cause no problems for stat().
+     On newer versions, stat() does not recognise a directory that ends
+     in a '\\' or '/', unless it is a drive root dir, such as "c:/",
+     where it is obligatory.  */
+  int pathlen = strlen (path);
+  char* end = path + pathlen - 1;
+  /* Preserve the lead '/' or lead "c:/".  */
+  char* start = path + (pathlen > 2 && path[1] == ':' ? 3 : 1);
+  
+  for (; end > start && IS_DIR_SEPARATOR (*end); end--)
+    *end = 0;
 #endif
 
   p = XNEW (cpp_dir);
