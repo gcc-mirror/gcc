841cc7daf3e285036b0468ce9eb28695a8080e98
Mike Stump <mrs@apple.com>
2007-05-08 01:31:24 +0000

    invoke.texi (Warning Options): Document that -Wempty-body also checks for and while statements in C++.
    	* doc/invoke.texi (Warning Options): Document that -Wempty-body
    	also checks for and while statements in C++.
    
    cp:
    	* parser.c (check_empty_body): Add.
    	(cp_parser_iteration_statement): Add call to check_empty_body.
    
    testsuite:
    	* g++.old-deja/g++.mike/empty.C: Add.
    
    From-SVN: r124534

---
+++ gcc/testsuite/g++.old-deja/g++.mike/empty.C
@@ -0,0 +1,25 @@
+// { dg-options "-W" }
+
+#define NOPE
+
+void foo() {
+  while (1); /* { dg-error "suggest a space before " } */
+    {
+    }
+  for (;;); /* { dg-error "suggest a space before " } */
+    {
+    }
+  while (1)
+    ;
+  for (;;)
+    ;
+  while (1) ;
+  for (;;) ;
+  /* These two work when using mapped locations */
+  while (1) NOPE; /* { dg-bogus "suggest a space before " "suggest" { xfail *-*-* } } */
+  for (;;) NOPE; /* { dg-bogus "suggest a space before " "suggest" { xfail *-*-* } } */
+  while (1)
+    NOPE;
+  for (;;)
+    NOPE;
+}
