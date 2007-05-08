9f8d875582965bc4fcf3c170ec0356f4c452c1fc
Chao-ying Fu <fu@mips.com>
2007-05-08 22:53:25 +0000

    * gcc.target/mips/msub-1.c, gcc.target/mips/msub-2.c,
    * gcc.target/mips/msub-3.c, gcc.target/mips/msub-4.c,
    * gcc.target/mips/msubu-1.c, gcc.target/mips/msubu-2.c,
    * gcc.target/mips/msubu-3.c, gcc.target/mips/msubu-4.c: New tests.
    
    From-SVN: r124559

---
+++ gcc/testsuite/gcc.target/mips/msub-3.c
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-mips-options "-O2 -mips32 -mgp32" } */
+/* { dg-final { scan-assembler-times "\tmsub\t" 2 } } */
+
+long long
+f1 (int x, int y, long long z)
+{
+  return z - (long long) y * x;
+}
+
+long long
+f2 (int x, int y, long long z)
+{
+  long long t = (long long) x * y;
+  int temp = 5;
+  if (temp == 5)
+    z -= t;
+  return z;
+}
