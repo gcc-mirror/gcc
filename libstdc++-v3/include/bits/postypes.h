ee60de3614cc99dcfd33e1123f00b35a4862096d
Paolo Carlini <paolo@gcc.gnu.org>
2007-05-14 10:52:05 +0000

    postypes.h (class fpos<>): Remove redundant forward declaration.
    2007-05-14  Paolo Carlini  <pcarlini@suse.de>
    
    	* include/bits/postypes.h (class fpos<>): Remove redundant forward
    	declaration.
    
    From-SVN: r124709

--- libstdc++-v3/include/bits/postypes.h
+++ libstdc++-v3/include/bits/postypes.h
@@ -1,6 +1,7 @@
 // Position types -*- C++ -*-
 
-// Copyright (C) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+// Copyright (C) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
+// 2006, 2007
 // Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
@@ -76,9 +77,6 @@ _GLIBCXX_BEGIN_NAMESPACE(std)
 
   /// Integral type for I/O operation counts and buffer sizes.
   typedef ptrdiff_t	streamsize; // Signed integral type
-
-  template<typename _StateT>
-    class fpos;
 
   /**
    *  @brief  Class representing stream positions.
