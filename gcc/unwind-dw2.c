c3dd0e8588136d0b1ce121dcebf82701189af760
geoffk <geoffk@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-05-18 22:50:18 +0000

    * unwind-dw2.c (uw_identify_context): Use the CFA, not the IP.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@124837 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/unwind-dw2.c
+++ gcc/unwind-dw2.c
@@ -1526,7 +1526,7 @@ uw_install_context_1 (struct _Unwind_Context *current,
 static inline _Unwind_Ptr
 uw_identify_context (struct _Unwind_Context *context)
 {
-  return _Unwind_GetIP (context);
+  return _Unwind_GetCFA (context);
 }
 
 
