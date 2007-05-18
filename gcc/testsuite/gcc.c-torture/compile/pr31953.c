b60b471172167ddbfe0ef0e6d2691af28aae9196
Ian Lance Taylor <iant@google.com>
2007-05-18 05:37:27 +0000

    re PR tree-optimization/31953 (ICE in set_value_range, at tree-vrp.c:305)
    ./:	PR tree-optimization/31953
    	* tree-vrp.c (set_value_range_to_value): Add equiv parameter.
    	Change all callers.
    	(set_value_range_to_null): Call set_value_range_to_value.
    	(extract_range_from_comparison): Likewise.
    testsuite/:
    	PR tree-optimization/31953
    	* gcc.c-torture/compile/pr31953.c: New test.
    
    From-SVN: r124823

---
+++ gcc/testsuite/gcc.c-torture/compile/pr31953.c
@@ -0,0 +1,14 @@
+struct WView
+{
+  int hexedit_mode:1;
+};
+toggle_hexedit_mode (struct WView *view)
+{
+  if (view->hexedit_mode)
+    {
+    }
+  else
+    {
+      view->hexedit_mode = !view->hexedit_mode;
+    }
+}
