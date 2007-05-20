488520a7cf03b3117d4f35a39cffb5dc9ad99cef
dfranke <dfranke@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-05-20 10:22:15 +0000

    gcc/fortran: 2007-05-20  Daniel Franke  <franke.daniel@gmail.com>
            PR fortran/32001
            * check.c (check_rest): Improved argument conformance check and
            fixed error message generation.
    
    
    gcc/testsuite:
    2007-05-20  Daniel Franke  <franke.daniel@gmail.org>
    
    	PR fortran/32001
    	* gfortran.dg/min_max_conformance.f90: Added more testcases.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@124869 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/min_max_conformance.f90
+++ gcc/testsuite/gfortran.dg/min_max_conformance.f90
@@ -1,9 +1,10 @@
 ! { dg-compile }
+! { dg-options "-fmax-errors=0" }
 ! PR 31919:  Tests for different ranks in min/max were missing.
 program pr31919
-  integer :: i4a(2, 2), i4b(2), i4c(4)
-  real(4) :: r4a(2, 2), r4b(2), r4c(4)
-  real(8) :: r8a(2, 2), r8b(2), r8c(4)
+  integer :: i4, i4a(2, 2), i4b(2), i4c(4)
+  real(4) :: r4, r4a(2, 2), r4b(2), r4c(4)
+  real(8) :: r8, r8a(2, 2), r8b(2), r8c(4)
 
   i4a = max(i4a, i4b)            ! { dg-error "Incompatible ranks" }
   i4a = max0(i4a, i4b)           ! { dg-error "Incompatible ranks" }
@@ -32,4 +33,19 @@ program pr31919
   r4a = min1(r4b, r4c)           ! { dg-error "different shape for arguments" }
   r4a = amin1(r4b, r4c)          ! { dg-error "different shape for arguments" }
   r8a = dmin1(r8b, r8c)          ! { dg-error "different shape for arguments" }
+
+  ! checking needs to be position independent
+  i4a = min(i4, i4a, i4, i4b)    ! { dg-error "Incompatible ranks" }
+  r4a = min(r4, r4a, r4, r4b)    ! { dg-error "Incompatible ranks" }
+  r8a = min(r8, r8a, r8, r8b)    ! { dg-error "Incompatible ranks" }
+  i4a = min(i4, i4b, i4, i4c)    ! { dg-error "different shape for arguments" }
+  r4a = min(r4, r4b, r4, r4c)    ! { dg-error "different shape for arguments" }
+  r8a = min(r8, r8b, r8, r8c)    ! { dg-error "different shape for arguments" }
+
+  i4a = max(i4, i4a, i4, i4b)    ! { dg-error "Incompatible ranks" }
+  r4a = max(r4, r4a, r4, r4b)    ! { dg-error "Incompatible ranks" }
+  r8a = max(r8, r8a, r8, r8b)    ! { dg-error "Incompatible ranks" }
+  i4a = max(i4, i4b, i4, i4c)    ! { dg-error "different shape for arguments" }
+  r4a = max(r4, r4b, r4, r4c)    ! { dg-error "different shape for arguments" }
+  r8a = max(r8, r8b, r8, r8c)    ! { dg-error "different shape for arguments" }
 end program
