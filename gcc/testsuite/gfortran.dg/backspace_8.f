40174b6f840af9df69b4d256b7a5626dcc2e8008
Thomas Koenig <tkoenig@gcc.gnu.org>
2007-05-20 21:07:14 +0000

    re PR fortran/31618 ([4.2, 4.1 only] backspace intrinsic is not working on an unformatted file)
    2007-05-20  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/31618
    	Backport from trunk.
    	* io/transfer.c (read_block_direct):  Instead of calling us_read,
    	set dtp->u.p.current_unit->current_record = 0 so that pre_position
    	will read the record marker.
    	(data_transfer_init):  For different error conditions, call
    	generate_error, then return.
    
    2007-05-20  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/31618
    	Backport from trunk.
    	* gfortran.dg/backspace_8.f:  New test case.
    
    From-SVN: r124878

---
+++ gcc/testsuite/gfortran.dg/backspace_8.f
@@ -0,0 +1,18 @@
+C { dg-do run }
+C PR libfortran/31618 - backspace after an error didn't work.
+      program main
+      character*78 msg
+      open (21, file="backspace_7.dat", form="unformatted")
+      write (21) 42, 43
+      write (21) 4711, 4712
+      write (21) -1, -4
+      rewind (21)
+      read (21) i,j
+      read (21,err=100,end=100) i,j,k
+      call abort
+ 100  continue
+      backspace 21
+      read (21) i,j
+      if (i .ne. 4711 .or. j .ne. 4712) call abort
+      close (21,status="delete")
+      end
