32ac0e0467b3e0e4b3c857b8956ce5d7a57bcaab
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-05-20 09:54:23 +0000

    PR target/31585         * gcc.target/i386/sse-vect-types.c: Do not redefine "static"         and "__inline".  Use "-msse" instead of "-msse2".
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@124868 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/sse-vect-types.c
+++ gcc/testsuite/gcc.target/i386/sse-vect-types.c
@@ -1,13 +1,5 @@
 /* { dg-do compile { target i?86-*-* x86_64-*-* } } */
-/* { dg-options "-O0 -msse2" } */
-
-/* Test the intrinsics without optimization.  All of them are
-   defined as inline functions in mmintrin.h that reference the proper
-   builtin functions.  Defining away "static" and "__inline" results in
-   all of them being compiled as proper functions.  */
-
-#define static
-#define __inline
+/* { dg-options "-O0 -msse" } */
 
 #include <xmmintrin.h>
 
