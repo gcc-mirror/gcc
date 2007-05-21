60c4429c7e517425d5e43c688b720516a5338648
Paolo Bonzini <bonzini@gnu.org>
2007-05-21 22:48:38 +0000

    c-cppbuiltin.c (c_cpp_builtins): Define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_1...
    2007-05-21  Paolo Bonzini  <bonzini@gnu.org>
                Paolo Carlini  <pcarlini@suse.de>
    	    Uros Bizjak  <ubizjak@gmail.com>
    
    	* c-cppbuiltin.c (c_cpp_builtins): Define
    	__GCC_HAVE_SYNC_COMPARE_AND_SWAP_1, __GCC_HAVE_SYNC_COMPARE_AND_SWAP_2,
    	__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4, __GCC_HAVE_SYNC_COMPARE_AND_SWAP_8,
    	__GCC_HAVE_SYNC_COMPARE_AND_SWAP_16, if appropriate.
    	* doc/cpp.texi ([Standard Predefined Macros]): Document.
    
    2007-05-21  Paolo Bonzini  <bonzini@gnu.org>
                Paolo Carlini  <pcarlini@suse.de>
    	    Uros Bizjak  <ubizjak@gmail.com>
    
    	* gcc.dg/gcc-have-sync-compare-and-swap.c,
    	gcc.target/i386/gcc-have-sync-compare-and-swap-1.c,
    	gcc.target/i386/gcc-have-sync-compare-and-swap-2.c,
    	gcc.target/i386/gcc-have-sync-compare-and-swap-3.c,
    	gcc.target/i386/gcc-have-sync-compare-and-swap-4.c,
    	New testcases.
    
    Co-Authored-By: Paolo Carlini <pcarlini@suse.de>
    Co-Authored-By: Uros Bizjak <ubizjak@gmail.com>
    
    From-SVN: r124926

---
+++ gcc/testsuite/gcc.dg/gcc-have-sync-compare-and-swap.c
@@ -0,0 +1,56 @@
+/* { dg-do link } */
+
+void f1()
+{
+#ifdef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_1
+  typedef int __attribute__  ((__mode__ (__QI__))) qi_int_type;
+  qi_int_type qi_int;
+  __sync_bool_compare_and_swap (&qi_int, (qi_int_type)0, (qi_int_type)1);
+#endif
+}
+
+void f2()
+{
+#ifdef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_2
+  typedef int __attribute__ ((__mode__ (__HI__))) hi_int_type;
+  hi_int_type hi_int;
+  __sync_bool_compare_and_swap (&hi_int, (hi_int_type)0, (hi_int_type)1);
+#endif
+}
+
+void f4()
+{
+#ifdef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_4
+  typedef int __attribute__ ((__mode__ (__SI__))) si_int_type;
+  si_int_type si_int;
+  __sync_bool_compare_and_swap (&si_int, (si_int_type)0, (si_int_type)1);
+#endif
+}
+
+void f8()
+{
+#ifdef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_8
+  typedef int __attribute__ ((__mode__ (__DI__))) di_int_type;
+  di_int_type di_int;
+  __sync_bool_compare_and_swap (&di_int, (di_int_type)0, (di_int_type)1);
+#endif
+}
+
+void f16()
+{
+#ifdef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_16
+  typedef int __attribute__ ((__mode__ (__TI__))) ti_int_type;
+  ti_int_type ti_int;
+  __sync_bool_compare_and_swap (&ti_int, (ti_int_type)0, (ti_int_type)1);
+#endif
+}
+
+int main()
+{
+  f1();
+  f2();
+  f4();
+  f8();
+  f16();  
+  return 0;
+}
