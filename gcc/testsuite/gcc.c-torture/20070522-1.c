18701a2488ed04092c0891223856c9215527c089
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-05-22 23:05:40 +0000

    2007-05-22  Andrew Pinski  <andrew_pinski@playstation.sony.com>
            PR middle-end/31606
            * gcc.c-torture/20070522-1.c: New testcase.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@124968 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/20070522-1.c
@@ -0,0 +1,11 @@
+/* This was PR 31606, we were trying to use TImode to expand the
+   struct copy in SRA but TImode is not support on ia32. */
+
+struct Collision {
+   int edge;
+   int triangle;
+  float _M_instance[3] __attribute__((__aligned__));
+};
+void  get_collisions(struct Collision a) {
+  struct Collision b = a;
+}
