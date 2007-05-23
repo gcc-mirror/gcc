f177efd39d119ec06d25a60db5f78b08c5936496
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2007-05-23 03:14:35 +0000

    re PR fortran/31051 ([4.2 Only] gfortran bug with x and t format descriptors.)
    2007-05-22  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/31051
    	* gfortran.dg/fmt_t_3.f90: New.
    
    From-SVN: r124978

---
+++ gcc/testsuite/gfortran.dg/fmt_t_3.f90
@@ -0,0 +1,15 @@
+! { dg-do run }
+! PR31051 bug with x and t format descriptors.
+! Test case prepared by Jerry DeLisle  <jvdelisle@gcc.gnu.org> from PR.
+program t
+   integer, parameter :: n = 9
+   character(len=40) :: fmt
+   character(len=2), dimension(n) :: y
+   open(unit=10, status="scratch")
+   y = 'a '
+   fmt = '(a,1x,(t7, 3a))'
+   write(10, fmt) 'xxxx', (y(i), i = 1,n)
+   rewind(10)
+   read(10, '(a)') fmt
+   if (fmt.ne."xxxx  a a a") call abort()
+end program t
