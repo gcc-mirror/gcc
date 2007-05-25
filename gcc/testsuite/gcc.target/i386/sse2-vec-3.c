fc6c9f75effaf7d1e793a5f5a0fa937668208972
H.J. Lu <hongjiu.lu@intel.com>
2007-05-25 14:33:27 +0000

    i386.c (__builtin_ia32_vec_ext_v2df): Mark it with MASK_SSE2.
    gcc/
    
    2007-05-25  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* config/i386/i386.c (__builtin_ia32_vec_ext_v2df): Mark it
    	with MASK_SSE2.
    	(__builtin_ia32_vec_ext_v2di): Likewise.
    	(__builtin_ia32_vec_ext_v4si): Likewise.
    	(__builtin_ia32_vec_ext_v8hi): Likewise.
    	(__builtin_ia32_vec_ext_v16qi): Likewise.
    	(__builtin_ia32_vec_set_v8hi): Likewise.
    
    gcc/testsuite/
    
    2007-05-25  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* gcc.target/i386/sse2-check.h: New.
    	* gcc.target/i386/sse2-vec-1.c: Likewise.
    	* gcc.target/i386/sse2-vec-2.c: Likewise.
    	* gcc.target/i386/sse2-vec-3.c: Likewise.
    	* gcc.target/i386/sse2-vec-4.c: Likewise.
    	* gcc.target/i386/sse2-vec-5.c: Likewise.
    	* gcc.target/i386/sse2-vec-6.c: Likewise.
    
    From-SVN: r125063

---
+++ gcc/testsuite/gcc.target/i386/sse2-vec-3.c
@@ -0,0 +1,37 @@
+/* { dg-do run { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -msse2" } */
+
+#include "sse2-check.h"
+
+#include <emmintrin.h>
+
+static void
+sse2_test (void)
+{
+  union
+    {
+      __m128i x;
+      char c[16];
+      short s[8];
+      int i[4];
+      long long ll[2];
+    } val1;
+  int res[4];
+  int masks[4];
+  int i;
+
+  for (i = 0; i < 16; i++)
+    val1.c[i] = i;
+
+  res[0] = __builtin_ia32_vec_ext_v4si ((__v4si)val1.x, 0);
+  res[1] = __builtin_ia32_vec_ext_v4si ((__v4si)val1.x, 1);
+  res[2] = __builtin_ia32_vec_ext_v4si ((__v4si)val1.x, 2);
+  res[3] = __builtin_ia32_vec_ext_v4si ((__v4si)val1.x, 3);
+
+  for (i = 0; i < 4; i++)
+    masks[i] = i;
+
+  for (i = 0; i < 4; i++)
+    if (res[i] != val1.i [masks[i]])
+      abort ();
+}
