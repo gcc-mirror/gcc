36032710357e5af5f3743c46c29c457e2b04ed05
Paul Thomas <pault@gcc.gnu.org>
2007-05-25 06:34:10 +0000

    re PR fortran/32047 (ICE (segfault) for pure function without argument)
    2007-05-25  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/32047
    	* trans-expr.c (gfc_apply_interface_mapping_to_expr): Change
    	order in logic under EXPR_FUNCTION to handle functions with
    	no arguments.
    
    2007-05-25  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/32047
    	* gfortran.dg/result_in_spec_2.f90: New test.
    
    From-SVN: r125057

---
+++ gcc/testsuite/gfortran.dg/result_in_spec_2.f90
@@ -0,0 +1,31 @@
+! { dg-do run }
+! Tests the fix for PR32047, in which the null agument
+! function for the character length would cause an ICE.
+!
+! Contributed by Tobias Burnus <burnus@gcc.gnu.org >
+!
+module test1
+  implicit none
+contains
+  character(f()) function test2() result(r)
+    interface
+      pure function f()
+        integer f
+      end function f
+    end interface
+    r = '123'
+  end function test2
+end module test1
+
+pure function f()
+  integer :: f
+  f = 3
+end function f
+
+program test
+  use test1
+  implicit none
+  if(len (test2()) /= 3) call abort ()
+  if(test2() /= '123') call abort ()
+end program test
+! { dg-final { cleanup-modules "test1" } }
