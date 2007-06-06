0cb8d27cd3ab2935184bd54c917c980a60f11565
Vasiliy Fofanov <fofanov@adacore.com>
2007-06-06 12:15:55 +0200

    2007-04-20  Vasiliy Fofanov  <fofanov@adacore.com>
    	* s-osprim-vms.ads, s-osprim-vms.adb
    	(Initialize) New procedure. Noop on VMS, added for interface
    	commonality.
    
    From-SVN: r125369

--- gcc/ada/s-osprim-vms.ads
+++ gcc/ada/s-osprim-vms.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                  S p e c                                 --
 --                                                                          --
---          Copyright (C) 1998-2005 Free Software Foundation, Inc.          --
+--          Copyright (C) 1998-2007, Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNARL is free software; you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -27,12 +27,12 @@
 -- covered by the  GNU Public License.                                      --
 --                                                                          --
 -- GNARL was developed by the GNARL team at Florida State University.       --
--- Extensive contributions were provided by Ada Core Technologies Inc.      --
+-- Extensive contributions were provided by Ada Core Technologies, Inc.     --
 --                                                                          --
 ------------------------------------------------------------------------------
 
 --  This package provides low level primitives used to implement clock and
---  delays in non tasking applications on Alpha/VMS
+--  delays in non tasking applications on Alpha/VMS.
 
 --  The choice of the real clock/delay implementation (depending on whether
 --  tasking is involved or not) is done via soft links (see s-soflin.ads)
@@ -52,14 +52,19 @@ package System.OS_Primitives is
    Max_Sensible_Delay : constant Duration :=
                           Duration'Min (183 * 24 * 60 * 60.0,
                                         Duration'Last);
-   --  Max of half a year delay, needed to prevent exceptions for large
-   --  delay values. It seems unlikely that any test will notice this
-   --  restriction, except in the case of applications setting the clock at
-   --  at run time (see s-tastim.adb). Also note that a larger value might
-   --  cause problems (e.g overflow, or more likely OS limitation in the
-   --  primitives used). In the case where half a year is too long (which
-   --  occurs in high integrity mode with 32-bit words, and possibly on
-   --  some specific ports of GNAT), Duration'Last is used instead.
+   --  Max of half a year delay, needed to prevent exceptions for large delay
+   --  values. It seems unlikely that any test will notice this restriction,
+   --  except in the case of applications setting the clock at run time (see
+   --  s-tastim.adb). Also note that a larger value might cause problems (e.g
+   --  overflow, or more likely OS limitation in the primitives used). In the
+   --  case where half a year is too long (which occurs in high integrity mode
+   --  with 32-bit words, and possibly on some specific ports of GNAT),
+   --  Duration'Last is used instead.
+
+   procedure Initialize;
+   --  Initialize global settings related to this package. This procedure
+   --  should be called before any other subprograms in this package. Note
+   --  that this procedure can be called several times.
 
    function OS_Clock return OS_Time;
    --  Returns "absolute" time, represented as an offset
@@ -67,33 +72,33 @@ package System.OS_Primitives is
 
    function Clock return Duration;
    pragma Inline (Clock);
-   --  Returns "absolute" time, represented as an offset
-   --  relative to "the Epoch", which is Jan 1, 1970 on unixes.
-   --  This implementation is affected by system's clock changes.
+   --  Returns "absolute" time, represented as an offset relative to "the
+   --  Epoch", which is Jan 1, 1970 00:00:00 UTC on UNIX systems. This
+   --  implementation is affected by system's clock changes.
 
    function Monotonic_Clock return Duration;
    pragma Inline (Monotonic_Clock);
-   --  Returns "absolute" time, represented as an offset
-   --  relative to "the Epoch", which is Jan 1, 1970.
-   --  This clock implementation is immune to the system's clock changes.
+   --  Returns "absolute" time, represented as an offset relative to "the Unix
+   --  Epoch", which is Jan 1, 1970 00:00:00 UTC. This clock implementation is
+   --  immune to the system's clock changes.
 
    Relative          : constant := 0;
    Absolute_Calendar : constant := 1;
    Absolute_RT       : constant := 2;
-   --  Values for Mode call below. Note that the compiler (exp_ch9.adb)
-   --  relies on these values. So any change here must be reflected in
-   --  corresponding changes in the compiler.
+   --  Values for Mode call below. Note that the compiler (exp_ch9.adb) relies
+   --  on these values. So any change here must be reflected in corresponding
+   --  changes in the compiler.
 
    procedure Timed_Delay (Time : Duration; Mode : Integer);
-   --  Implements the semantics of the delay statement when no tasking is
-   --  used in the application.
+   --  Implements the semantics of the delay statement when no tasking is used
+   --  in the application.
    --
    --    Mode is one of the three values above
    --
    --    Time is a relative or absolute duration value, depending on Mode.
    --
-   --  Note that currently Ada.Real_Time always uses the tasking run time, so
-   --  this procedure should never be called with Mode set to Absolute_RT.
+   --  Note that currently Ada.Real_Time always uses the tasking run time,
+   --  so this procedure should never be called with Mode set to Absolute_RT.
    --  This may change in future or bare board implementations.
 
    function To_Duration (T : OS_Time; Mode : Integer) return Duration;
