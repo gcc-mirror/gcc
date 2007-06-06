60fce41336693ee3cfb0d722fb05d4dc7039516c
Manuel López-Ibáñez <manu@gcc.gnu.org>
2007-06-06 20:49:09 +0000

    re PR testsuite/25241 ([C++] DejaGNU does not distinguish between errors and warnings)
    2007-06-06  Manuel Lopez-Ibanez  <manu@gcc.gnu.org>
    
    	PR testsuite/25241
    
    testsuite/
    	* gcc.dg/simd-5.c: Mark output as a note.
    	* gcc.dg/simd-6.c: Mark output as a note. Fix incorrect use of
    	dg-message.
    	* gcc.dg/simd-1.c: Likewise.
    	* gcc.dg/always_inline.c: Mark output as a sorry.
    	* gcc.dg/always_inline2.c: Likewise.
    	* gcc.dg/always_inline3.c: Likewise.
    
    From-SVN: r125505

--- gcc/testsuite/gcc.dg/always_inline2.c
+++ gcc/testsuite/gcc.dg/always_inline2.c
@@ -1,8 +1,8 @@
 /* { dg-do compile } */
 /* { dg-options "-Winline -O2" } */
-inline __attribute__ ((always_inline)) void t(void); /* { dg-message "body not available" "" } */
+inline __attribute__ ((always_inline)) void t(void); /* { dg-message "sorry\[^\n\]*body not available" "" } */
 void
 q(void)
 {
-  t(); 				/* { dg-message "called from here" "" } */
+  t(); 				/* { dg-message "sorry\[^\n\]*called from here" "" } */
 }
