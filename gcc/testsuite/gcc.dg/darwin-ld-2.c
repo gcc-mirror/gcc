20967296dc0517322a6058ec55491de9707c0c34
Geoffrey Keating <geoffk@apple.com>
2007-06-07 19:04:02 +0000

    attr-weakref-1-darwin.c: Run on all Darwin platforms.
    	* gcc.dg/attr-weakref-1-darwin.c: Run on all Darwin platforms.
    	* gcc.dg/20050826-1.c: Run on all Darwin platforms.
    	* gcc.dg/darwin-ld-2.c: Run on all Darwin platforms.
    	* gcc.dg/darwin-ld-4.c: Run on all Darwin platforms.
    	* gcc.dg/cleanup-8.c: Run on i386-darwin9 and later.
    	* gcc.dg/cleanup-9.c: Run on i386-darwin9 and later.
    	* gcc.dg/cleanup-10.c: Run on i386-darwin9 and later.
    	* gcc.dg/cleanup-11.c: Run on i386-darwin9 and later.
    
    From-SVN: r125538

--- gcc/testsuite/gcc.dg/darwin-ld-2.c
+++ gcc/testsuite/gcc.dg/darwin-ld-2.c
@@ -2,7 +2,7 @@
 /* Developed by Devang Patel <dpatel@apple.com>.  */
 
 /* { dg-options "-bind_at_load" } */
-/* { dg-do link { target powerpc-*-darwin* } } */
+/* { dg-do link { target *-*-darwin* } } */
 
 int main()
 {
--- gcc/testsuite/gcc.dg/darwin-ld-4.c
+++ gcc/testsuite/gcc.dg/darwin-ld-4.c
@@ -2,7 +2,7 @@
 /* Developed by Devang Patel <dpatel@apple.com>.  */
 
 /* { dg-options "-bundle" } */
-/* { dg-do link { target powerpc-*-darwin* } } */
+/* { dg-do link { target *-*-darwin* } } */
 
 int main()
 {
