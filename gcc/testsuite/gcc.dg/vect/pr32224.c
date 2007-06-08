0d2665809fd15fe7c5ee73a7adee1ae64a002ffd
Dorit Nuzman <dorit@il.ibm.com>
2007-06-08 08:57:54 +0000

    re PR tree-optimization/32224 (ICE in vect_analyze_operations, at tree-vect-analyze.c:374)
            PR tree-optimization/32224
            * tree-vect-analyze.c (vect_determine_vectorization_factor): Fail
            vectorization upon a non GIMPLE_MODIFY_STMT.
    
    From-SVN: r125566

---
+++ gcc/testsuite/gcc.dg/vect/pr32224.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */
+typedef unsigned long int *mp_ptr;
+typedef const unsigned long int *mp_srcptr;
+gmpz_export (void *data)
+{
+  mp_srcptr zp;
+  int count, i;
+  mp_ptr __dst = ((mp_ptr) data);
+  mp_srcptr __src = (zp);
+
+  for (i = 0; i < count; i++)
+  {
+    __asm__ ("bswap %q0": "=r" (*__dst):"0" (*(__src)));
+    __src++;
+  }
+}
+/* { dg-final { cleanup-tree-dump "vect" } } */
