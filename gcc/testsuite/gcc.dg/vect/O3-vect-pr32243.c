6fc069dd301be72d2c8e3804fb53a817486f2aed
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-06-08 09:06:46 +0000

    PR tree-optimization/32243 	* tree-vect-transform.c (vectorizable_type_promotion): Move check 	for ncopies after ratio check between nunits_out and nunits_in. 	(vectorizable_type_demotion): Remove single-use variable "scalar_type".
    testsuite/ChangeLog:
    	
    	PR tree-optimization/32243
    	* gcc.dg/vect/vect.exp: Add support for -O3 tests.  Reset default
    	flags for -Os tests.
    	* gcc.dg/vect/03-vect-pr32243.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@125567 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/O3-vect-pr32243.c
@@ -0,0 +1,27 @@
+/* { dg-do compile } */
+
+typedef struct __GLcontextRec GLcontext;
+
+struct gl_renderbuffer
+{
+  struct gl_renderbuffer *Wrapped;
+  void (*PutValues) (GLcontext * ctx, struct gl_renderbuffer * rb,
+		     int count, const int x[], const int y[],
+		     const void *values, const char *mask);
+};
+
+void
+put_mono_values_s8 (GLcontext * ctx, struct gl_renderbuffer *s8rb,
+		    int count, const int x[], const int y[],
+		    const void *value, const char *mask)
+{
+  struct gl_renderbuffer *dsrb = s8rb->Wrapped;
+  int temp[4096], i;
+  const char val = *((char *) value);
+  for (i = 0; i < count; i++)
+    if (!mask || mask[i])
+      temp[i] = (temp[i] & 0xffffff) | val;
+  dsrb->PutValues (ctx, dsrb, count, x, y, temp, mask);
+}
+
+/* { dg-final { cleanup-tree-dump "vect" } } */
