69c553ef2aa52adbaeab79751c1d421cc61f5216
Uros Bizjak <ubizjak@gmail.com>
2007-06-11 12:13:00 +0200

    re PR target/32280 (_mm_srli_si128, heinous code for some shifts)
    	PR target/32280
    	* config/i386/sse.md ("sse2_ashlti", "sse2_lshrti3"): Move ...
    	* config/i386/i386.md ("sse2_ashlti", "sse2_lshrti3"): ... to here.
    
    testsuite/ChangeLog:
    	
    	PR target/32280
    	* gcc.target/i386/pr32280.c: New test.
    
    From-SVN: r125615

---
+++ gcc/testsuite/gcc.target/i386/pr32280.c
@@ -0,0 +1,17 @@
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -msse2" } */
+
+typedef long long __m128i __attribute__ ((__vector_size__ (16)));
+
+__m128i foo1(__m128i __a)
+{
+ return (__m128i)__builtin_ia32_pslldqi128 (__a, 8);
+}
+
+__m128i foo2(__m128i __a)
+{
+ return (__m128i)__builtin_ia32_psrldqi128 (__a, 8);
+}
+
+/* { dg-final { scan-assembler "psrldq" } } */
+/* { dg-final { scan-assembler "pslldq" } } */
