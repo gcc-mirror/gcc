8375344417d39f567bf83e2c1b3a5d941bc1c111
ghazi <ghazi@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-06-12 16:28:01 +0000

    * fold-const.c (fold_binary): Guard (X-X) -> 0 transformation 	with !HONOR_NANS and !HONOR_INFINITIES. 	* simplify-rtx.c (simplify_binary_operation_1): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@125652 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/simplify-rtx.c
+++ gcc/simplify-rtx.c
@@ -1772,10 +1772,14 @@ simplify_binary_operation_1 (enum rtx_code code, enum machine_mode mode,
     case MINUS:
       /* We can't assume x-x is 0 even with non-IEEE floating point,
 	 but since it is zero except in very strange circumstances, we
-	 will treat it as zero with -funsafe-math-optimizations.  */
+	 will treat it as zero with -funsafe-math-optimizations and
+	 -ffinite-math-only.  */
       if (rtx_equal_p (trueop0, trueop1)
 	  && ! side_effects_p (op0)
-	  && (! FLOAT_MODE_P (mode) || flag_unsafe_math_optimizations))
+	  && (! FLOAT_MODE_P (mode)
+	      || (flag_unsafe_math_optimizations
+		  && !HONOR_NANS (mode)
+		  && !HONOR_INFINITIES (mode))))
 	return CONST0_RTX (mode);
 
       /* Change subtraction from zero into negation.  (0 - x) is the
