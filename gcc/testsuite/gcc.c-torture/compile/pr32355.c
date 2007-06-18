2195f002b7923f0c0bfb156c144849dafc3668ac
zadeck <zadeck@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-06-18 16:47:05 +0000

    2007-06-18  Kenneth Zadeck <zadeck@naturalbridge.com>
    	PR middle-end/32355
    	* gcse (rest_of_handle_gcse): Add call to df_finish_pass after
    	cse_main.
    	* df-problems.c (df_note_bb_compute): Fix dumping info.
    	
    2007-06-18  Kenneth Zadeck <zadeck@naturalbridge.com>
    
    	* gcc.c-torture/compile/pr32355.c: New testcase.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@125812 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr32355.c
@@ -0,0 +1,33 @@
+/* { dg-options "-O3" } */
+
+typedef struct
+{
+}
+__sigset_t;
+typedef struct
+{
+    char coredump;
+}
+EMode;
+extern EMode Mode;
+struct sigaction
+{
+  __sigset_t sa_mask;
+  int sa_flags;
+};
+doSignalsSetup (void)
+{
+  static const int signals[] = {
+    1, 2 , 3, 4, 6, 8, 11, 13, 14, 15, 10, 12, 17, 7
+  };
+  unsigned int i, sig;
+  struct sigaction sa;
+  for (i = 0; i < sizeof (signals) / sizeof (int); i++)
+    {
+      sig = signals[i];
+      if (Mode.coredump && (sig == 4 || sig == 8))
+        continue;
+      sa.sa_flags = (sig == 17);
+      sigemptyset (&sa.sa_mask);
+    }
+}
