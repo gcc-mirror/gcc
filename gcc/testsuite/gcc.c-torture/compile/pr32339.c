af4c82ed94434ca16d03b575be97c813c371b3a9
Seongbae Park <spark@gcc.gnu.org>
2007-06-18 20:02:33 +0000

    re PR rtl-optimization/32339 (ICE in insert_save, at caller-save.c:726)
    gcc/ChangeLog:
    
    2007-06-18  Seongbae Park  <seongbae.park@gmail.com>
    
    	PR rtl-optimization/32339
    	* df-scan.c (df_uses_record): Don't modify flags but just add to
    	it for df_ref_record.
    
    gcc/testsuite/ChangeLog:
    
    2007-06-18  Martin Michlmayr <tbm@cyrius.com>
    
    	PR rtl-optimization/32339
    	* gcc.c-torture/compile/pr32339.c: New test.
    
    From-SVN: r125825

---
+++ gcc/testsuite/gcc.c-torture/compile/pr32339.c
@@ -0,0 +1,17 @@
+/* We used to ICE in insert_save at caller-save.c,
+   due to missing REG_DEAD for register use in post-decrement on ia64. */
+struct city_dialog
+{
+  struct city *pcity;
+  char change_list_names[200][200];
+  int change_list_ids[200];
+};
+change_callback (void)
+{
+  struct city_dialog *pdialog;
+  int n;
+  int i;
+  get_city_dialog_production_full (pdialog->change_list_names[n],
+    pdialog->pcity);
+  pdialog->change_list_ids[n++] = i;
+}
