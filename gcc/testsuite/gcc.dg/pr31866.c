a849aec42c06c8c42867417b3156b915d4fa3348
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-06-21 12:20:42 +0000

    PR tree-optimization/31866 	* tree-ssa-coalesce.c (create_outofssa_var_map): Do nothing 	if ASM_EXPR's input is not a SSA_NAME.
    	* gcc.dg/pr31866.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@125919 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr31866.c
@@ -0,0 +1,28 @@
+/* PR tree-optimization/31866 */
+/* { dg-do compile { target alpha-*-* cris-*-* i?86-*-* mmix-*-* powerpc*-*-* rs6000-*-* x86_64-*-* } } */
+/* { dg-options "-O2" } */
+
+#if defined (__alpha__)
+# define REG "$1"
+#elif defined (__CRIS__)
+# define REG "r10"
+#elif defined (__i386__)
+# define REG "%eax"
+#elif defined (__MMIX__)
+# define REG "$8"
+#elif defined (__powerpc__) || defined (__PPC__) || defined (__ppc__) \
+	|| defined (__POWERPC__) || defined (PPC) || defined (_IBMR2)
+# define REG "6"
+#elif defined (__x86_64__)
+# define REG "rax"
+#endif
+
+long int
+foo (void)
+{
+  unsigned long int s;
+  long int a = (long int) 0;
+  register long int r asm (REG) = a;
+  asm ("" : "=r" (s) : "0" (r));
+  return s;
+}
