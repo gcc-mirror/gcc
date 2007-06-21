437fd8c09ad87e30302098cc76e638d8734f8208
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-06-21 17:06:05 +0000

    PR tree-optimization/19590 	* tree-vrp.c (adjust_range_with_scev): Set the range when the result 	of scev is a constant. 	* gcc/testsuite/gcc.dg/tree-ssa/pr19590.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@125925 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr19590.c
@@ -0,0 +1,37 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-ivopts" } */
+
+void vnum_test8(int *data) 
+{ 
+  int i; 
+  int stop = data[3]; 
+  int m = data[4]; 
+  int n = m; 
+  for (i=1; i<stop; i++) { 
+    int k = data[2]; 
+    data[k] = 2; 
+    data[0] = m - n; 
+    k = data[1]; 
+    m = m + k; 
+    n = n + k; 
+  } 
+} 
+
+/* Using the SCEV analysis, this loop should be transformed to:
+
+   | void vnum_result8(int *data) 
+   |{ 
+   |  int i; 
+   |  int stop = data[3]; 
+   |  for (i=1; i<stop; i++) { 
+   |    int k = data[2]; 
+   |    data[k] = 2; 
+   |    data[0] = 0; 
+   |  } 
+   |}
+
+*/
+
+/* { dg-final { scan-tree-dump-times "= 0;" 1 "ivopts"} } */
+/* { dg-final { scan-tree-dump-times "= 2;" 1 "ivopts"} } */
+/* { dg-final { cleanup-tree-dump "ivopts" } }  */
