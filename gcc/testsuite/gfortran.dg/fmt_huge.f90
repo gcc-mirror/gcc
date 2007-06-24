6a41ab4e2f1fe8559c5fc58fa8b71add52d527d5
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-06-24 18:58:36 +0000

    2007-06-24  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR fortran/32446
    	* gfortran.dg/fmt_huge.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@125986 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/fmt_huge.f90
@@ -0,0 +1,6 @@
+! { dg-do run }
+! PR32446 printing big numbers in F0.1 format.
+! This segfaulted before the patch.
+      open (10, status="scratch")
+      write (10,'(F0.1)') huge(1.0)
+      END
