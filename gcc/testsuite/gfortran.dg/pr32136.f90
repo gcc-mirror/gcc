03f9dbf0b3c5b4150becb3cb5469e4c546f7d918
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-06-24 19:24:02 +0000

    2007-06-24  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/32136
    	* gfortran.dg/pr32136.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@125987 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr32136.f90
@@ -0,0 +1,11 @@
+! { dg-do run }
+! { dg-options "-std=gnu" }
+! Tests PR32136, which went away!
+!
+! Contributed by Tobias Burnus <burnus@gcc.gnu.org>
+!
+real(kind(0d0)), parameter :: r(1) = &
+    transfer(transfer(sqrt(2d0), (/ .true. /) ), (/ 0d0 /), 1)
+    if (r(1) .ne. sqrt(2d0)) call abort ()
+end
+
