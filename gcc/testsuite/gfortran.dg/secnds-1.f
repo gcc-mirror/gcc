b0026c23df2fe611cf4e4d232caa0d9e56e2466f
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-06-25 02:48:14 +0000

    2007-06-24  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR fortran/32456
    	* gfortran.dg/secnds-1.f: Revise test to reduce random errors.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@125996 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/secnds-1.f
+++ gcc/testsuite/gfortran.dg/secnds-1.f
@@ -6,18 +6,25 @@ C Contributed by Paul Thomas  <pault@gcc.gnu.org>
 C
       character*20 dum1, dum2, dum3
       real t1, t1a, t2, t2a
-      real*8 dat1, dat2
-      integer i, j, values(8)
+      real*4 dat1, dat2
+      integer i, j, values(8), k
       t1 = secnds (0.0)
       call date_and_time (dum1, dum2, dum3, values)
       t1a = secnds (0.0)
-      dat1 = 0.001*real (values(8)) + real (values(7)) +
-     &        60.0*real (values(6)) + 3600.0* real (values(5))
-      if (((dat1 - t1) < 0.) .or. ((dat1 - t1) > (t1a - t1))) call abort ()
+      dat1 = 0.001 * real(values(8)) + real(values(7)) +
+     &        60.0 * real(values(6)) + 3600.0 * real(values(5))
+      ! handle midnight shift
+      if ((t1a - t1) < -12.0*3600.0 ) t1 = t1 - 24.0*3600.0
+      if ((t1a - dat1) < -12.0*3600.0 ) dat1 = dat1 - 24.0*3600.0
+      if ((dat1 < nearest(t1, -1.)) .or. (dat1  > nearest(t1a, 1.)))
+     &    call abort ()
       t2a = secnds (t1a)
       call date_and_time (dum1, dum2, dum3, values)
       t2 = secnds (t1)
-      dat2 = 0.001*real (values(8)) + real (values(7)) +
-     &        60.0*real (values(6)) + 3600.0* real (values(5))
-      if (((dat2 - dat1) < t2a) .or. ((dat2 - dat1) > t2)) call abort ()
+      dat2 = 0.001 * real(values(8)) + real(values(7)) +
+     &        60.0 * real(values(6)) + 3600.0 * real(values(5))
+      ! handle midnight shift
+      if ((dat2 - dat1) < -12.0*3600.0 ) dat1 = dat1 - 24.0*3600.0
+      if (((dat2 - dat1) < t2a - 0.008) .or.
+     &    ((dat2 - dat1) > t2 + 0.008)) call abort ()
       end
