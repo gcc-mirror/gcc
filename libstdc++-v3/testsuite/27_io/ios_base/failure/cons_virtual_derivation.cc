db016a4e82aa4d342e09771144668a17799d569c
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-06-26 15:58:45 +0000

    2007-06-26  Benjamin Kosnik  <bkoz@redhat.com>
    	* testsuite/util/testsuite_api.h: New.
    	* testsuite/27_io/ios_base/failure: Add.
    	* testsuite/27_io/ios_base/failure/cons_virtual_derivation.cc: New.
    	* testsuite/27_io/ios_base/failure/what-1.cc: Same.
    	* testsuite/27_io/ios_base/failure/what-2.cc: Same.
    	* testsuite/27_io/ios_base/failure/what-big.cc: Same.
    	* testsuite/27_io/ios_base/failure/what-3.cc: Same.
    	* testsuite/19_diagnostics/logic_error/
    	cons_virtual_derivation.cc: Same.
    	* testsuite/19_diagnostics/runtime_error/
    	cons_virtual_derivation.cc: Same.
    	* testsuite/18_support/bad_alloc/cons_virtual_derivation.cc: Same.
    	* testsuite/18_support/bad_cast/cons_virtual_derivation.cc: Same.
    	* testsuite/18_support/bad_exception/cons_virtual_derivation.cc: Same.
    	* testsuite/18_support/bad_typeid/cons_virtual_derivation.cc: Same.
    	* testsuite/ext/concurrence_lock_error: New.
    	* testsuite/ext/concurrence_lock_error/
    	cons_virtual_derivation.cc: Same.
    	* testsuite/ext/forced_exception_error: New.
    	* testsuite/ext/forced_exception_error/
    	cons_virtual_derivation.cc: Same.
    	* testsuite/ext/concurrence_unlock_error: New.
    	* testsuite/ext/concurrence_unlock_error/
    	cons_virtual_derivation.cc: Same.
    	* testsuite/20_util/function_objects/bad_function_call: New.
    	* testsuite/20_util/function_objects/bad_function_call/
    	cons_virtual_derivation.cc: Same.
    	
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126032 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/27_io/ios_base/failure/cons_virtual_derivation.cc
@@ -0,0 +1,30 @@
+// { dg-do run { xfail *-*-* } }
+// 2007-05-29 Benjamin Kosnik  <bkoz@redhat.com>
+
+// Copyright (C) 2007 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#include <ios>
+#include <testsuite_api.h>
+
+int main()
+{
+  typedef std::ios_base::failure test_type;
+  __gnu_test::diamond_derivation<test_type, false>::test();
+  return 0;
+}
