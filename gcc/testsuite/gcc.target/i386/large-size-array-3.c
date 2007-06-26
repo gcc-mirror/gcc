58129721cbb1bca22eba452a9fd1fe971b914dc8
hmchang <hmchang@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-06-26 17:28:13 +0000

    * gcc.target/i386/large-size-array-3.c: Remove the larger size of 	the scan-assembler constant.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126035 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/large-size-array-3.c
+++ gcc/testsuite/gcc.target/i386/large-size-array-3.c
@@ -1,4 +1,4 @@
 /* { dg-do compile { target { { i?86-*-* x86_64-*-* } && lp64 } } } */
 /* { dg-options "-mcmodel=medium" } */
-/* { dg-final { scan-assembler "8589934592|8589934588" } } */
+/* { dg-final { scan-assembler "8589934588" } } */
 int bigarray[2147483647];
