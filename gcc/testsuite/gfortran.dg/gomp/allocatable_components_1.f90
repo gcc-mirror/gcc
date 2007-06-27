24189c587c23578bcbe2e7395fef5afb75ee5184
Daniel Franke <franke.daniel@gmail.com>
2007-06-27 16:02:31 -0400

    re PR fortran/32467 (structure containing allocatable array is accepted in COPYIN clause)
    gcc/fortran:
    2007-06-24  Daniel Franke  <franke.daniel@gmail.com>
    
    	PR fortran/32467
    	* openmp.c (resolve_omp_clauses): Emit error on allocatable components 
    	in COPYIN, COPYPRIVATE, FIRSTPRIVATE and LASTPRIVATE clauses.
    
    gcc/testsuite:
    2007-06-24  Daniel Franke  <franke.daniel@gmail.com>
    
            PR fortran/32467
            * gfortran.dg/gomp/allocatable_components_1.f90: New test.
    
    From-SVN: r126063

---
+++ gcc/testsuite/gfortran.dg/gomp/allocatable_components_1.f90
@@ -0,0 +1,59 @@
+! { dg-do compile }
+!
+! PR fortran/32467
+! Derived types with allocatable components
+!
+
+MODULE test_allocatable_components
+  type :: t
+    integer, allocatable :: a(:)
+  end type
+
+CONTAINS
+  SUBROUTINE test_copyin()
+    TYPE(t), SAVE :: a
+
+    !$omp threadprivate(a)
+    !$omp parallel copyin(a)        ! { dg-error "has ALLOCATABLE components" }
+      ! do something
+    !$omp end parallel
+  END SUBROUTINE
+
+  SUBROUTINE test_copyprivate()
+    TYPE(t) :: a
+
+    !$omp single                    ! { dg-error "has ALLOCATABLE components" }
+      ! do something
+    !$omp end single copyprivate (a)
+  END SUBROUTINE
+
+  SUBROUTINE test_firstprivate
+    TYPE(t) :: a
+
+    !$omp parallel firstprivate(a)  ! { dg-error "has ALLOCATABLE components" }
+      ! do something
+    !$omp end parallel
+  END SUBROUTINE
+
+  SUBROUTINE test_lastprivate
+    TYPE(t) :: a
+    INTEGER :: i
+
+    !$omp parallel do lastprivate(a)  ! { dg-error "has ALLOCATABLE components" }
+      DO i = 1, 1
+      END DO
+    !$omp end parallel do
+  END SUBROUTINE
+
+  SUBROUTINE test_reduction
+    TYPE(t) :: a(10)
+    INTEGER :: i
+
+    !$omp parallel do reduction(+: a)   ! { dg-error "must be of intrinsic type" }
+    DO i = 1, SIZE(a)
+    END DO
+    !$omp end parallel do
+  END SUBROUTINE
+END MODULE
+
+! { dg-final { cleanup-modules "test_allocatable_components" } }
