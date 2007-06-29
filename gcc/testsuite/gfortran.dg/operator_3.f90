622af87fc843e25c9631a43aaa1b30cc647b2825
Daniel Franke <franke.daniel@gmail.com>
2007-06-29 15:05:58 -0400

    re PR fortran/31580 (Better error message for not-found operator)
    gcc/fortran:
    2007-06-29  Daniel Franke  <franke.daniel@gmail.com>
    
            PR fortran/31580
            * resolve.c (resolve_operator): Added check whether a user 
    	defined operator is available.
    
    gcc/testsuite:
    2007-06-29  Daniel Franke  <franke.daniel@gmail.com>
    
    	PR fortran/31580
    	* gfortran.dg/operator_3.f90: New test.
    
    From-SVN: r126117

---
+++ gcc/testsuite/gfortran.dg/operator_3.f90
@@ -0,0 +1,9 @@
+! { dg-compile }
+! PR fortran/31580
+!
+! Testcase contributed by Tobias Burnus <burnus AT gcc DOT gnu DOT org>
+!
+PROGRAM test
+  real :: a,b
+  if(a .nonex. b) stop       ! { dg-error "Unknown operator" }
+end program
