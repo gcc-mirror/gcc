eec33acaa617a5aff35eca6bdd365fcc1a73444d
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-06-29 20:13:41 +0000

    PR middle-end/32372 	* cse.c (cse_insn): Avoid invalid sharing in between register note and 	the insn pattern.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126122 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr32372.c
@@ -0,0 +1,21 @@
+typedef struct AVCodecContext
+{
+  int flags;
+  void *priv_data;
+  char codec_name[32];
+}
+AVCodecContext;
+typedef struct ScanTable
+{
+  int obmc;
+  int umvplus;
+  int h263_aic;
+}
+MpegEncContext;
+MPV_encode_init (AVCodecContext *avctx)
+{
+  MpegEncContext *s = avctx->priv_data;
+  s->umvplus = (avctx->flags & 0x02000000) ? 1 : 0;
+  s->h263_aic = (avctx->flags & 0x01000000) ? 1 : 0;
+  s->h263_aic = s->obmc || s->umvplus;
+}
