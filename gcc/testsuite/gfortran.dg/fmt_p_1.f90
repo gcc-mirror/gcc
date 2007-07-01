e6918fe98eb11f59bb446347f952ac5f604dc442
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-01 15:49:37 +0000

    2007-06-29  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR libgfortran/32554
    	* gfortran.dg/fmt_p_1.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126174 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/fmt_p_1.f90
@@ -0,0 +1,13 @@
+! { dg-do run }
+! PR32554 Bug in P formatting
+! Test case from the bug reporter
+program gfcbug66
+  real(8) :: x = 1.0e-100_8
+  character(50) :: outstr
+  write (outstr,'(1X,2E12.3)')    x, 2 * x
+  if (outstr.ne."    0.100E-99   0.200E-99") call abort
+  ! Before patch 2 * x was put out wrong
+  write (outstr,'(1X,1P,2E12.3)') x, 2 * x
+  if (outstr.ne."    1.000-100   2.000-100") call abort
+end program gfcbug66
+
