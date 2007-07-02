269ef14dfd9a5235f3569ba68d5efbb985349cb8
Douglas Gregor <doug.gregor@gmail.com>
2007-07-02 13:11:13 +0000

    typedefs_neg.cc: Tweak line numbers.
    2007-07-02  Douglas Gregor  <doug.gregor@gmail.com>
    
            * testsuite/20_util/make_signed/requirements/typedefs_neg.cc:
            Tweak line numbers.
            * testsuite/20_util/make_unsigned/requirements/typedefs_neg.cc:
            Ditto.
            * testsuite/20_util/make_unsigned/requirements/typedefs-1.cc:
            Don't try to create an unsigned wchar_t.
            * testsuite/20_util/make_unsigned/requirements/typedefs-2.cc:
            Don't try to create an unsigned wchar_t.
            * testsuite/util/testsuite_hooks.h: Remove a stray semicolon.
    
    From-SVN: r126203

--- libstdc++-v3/testsuite/20_util/make_unsigned/requirements/typedefs_neg.cc
+++ libstdc++-v3/testsuite/20_util/make_unsigned/requirements/typedefs_neg.cc
@@ -49,8 +49,8 @@ void test01()
 // { dg-error "instantiated from here" "" { target *-*-* } 41 }
 // { dg-error "instantiated from here" "" { target *-*-* } 43 }
 
-// { dg-error "invalid use of incomplete type" "" { target *-*-* } 418 }
-// { dg-error "declaration of" "" { target *-*-* } 384 }
+// { dg-error "invalid use of incomplete type" "" { target *-*-* } 414 }
+// { dg-error "declaration of" "" { target *-*-* } 380 }
 
 // { dg-excess-errors "At global scope" }
 // { dg-excess-errors "In instantiation of" }
