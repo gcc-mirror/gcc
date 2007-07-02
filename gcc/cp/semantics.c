d1d4c97f60327e5e9a851198c5e87282d15a5e31
Jakub Jelinek <jakub@redhat.com>
2007-07-02 15:01:37 +0200

    re PR c++/31748 (bad diagnostic for invalid private clause)
    	PR c++/31748
    	* semantics.c (finish_omp_clauses): Use %qD instead of %qE for
    	DECL_P in not a variable and appears more than once error messages.
    
    	* g++.dg/gomp/pr31748.C: New test.
    
    From-SVN: r126202

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -3344,14 +3344,17 @@ finish_omp_clauses (tree clauses)
 	    {
 	      if (processing_template_decl)
 		break;
-	      error ("%qE is not a variable in clause %qs", t, name);
+	      if (DECL_P (t))
+		error ("%qD is not a variable in clause %qs", t, name);
+	      else
+		error ("%qE is not a variable in clause %qs", t, name);
 	      remove = true;
 	    }
 	  else if (bitmap_bit_p (&generic_head, DECL_UID (t))
 		   || bitmap_bit_p (&firstprivate_head, DECL_UID (t))
 		   || bitmap_bit_p (&lastprivate_head, DECL_UID (t)))
 	    {
-	      error ("%qE appears more than once in data clauses", t);
+	      error ("%qD appears more than once in data clauses", t);
 	      remove = true;
 	    }
 	  else
