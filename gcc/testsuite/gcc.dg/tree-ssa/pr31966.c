58bc8309d5b60a741be3f24372cbf9fdbff79d8a
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-02 14:26:11 +0000

    PR tree-optimization/31966 	PR tree-optimization/32533 	* tree-if-conv.c (add_to_dst_predicate_list): Use "edge", not 	"basic_block" description as its third argument.  Update function 	calls to get destination bb from "edge" argument.  Save "cond" into 	aux field of the edge.  Update prototype for changed arguments. 	(find_phi_replacement_condition): Operate on incoming edges, not 	on predecessor blocks.  If there is a condition saved in the 	incoming edge aux field, AND it with incoming bb predicate. 	Return source bb of the first edge. 	(clean_predicate_lists): Clean aux field of outgoing node edges. 	(tree_if_conversion): Do not initialize cond variable. Move 	variable declaration into the loop. 	(replace_phi_with_cond_gimple_modify_stmt): Remove unneded 	initializations of new_stmt, arg0 and arg1 variables.
    testsuite/ChangeLog:
    
    	PR tree-optimization/31966
    	PR tree-optimization/32533
    	* gcc.dg/tree-ssa/pr31966.c: New runtime test.
    	* gfortran.dg/pr32533.f90: Ditto.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126206 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr31966.c
@@ -0,0 +1,50 @@
+/* Contributed by Jack Lloyd  <lloyd@randombit.net> */
+
+/* { dg-options "-O2 -ftree-vectorize" } */
+/* { dg-options "-O2 -ftree-vectorize -march=nocona" { target { i?86-*-* x86_64-*-* } } } */
+
+typedef unsigned long long word;
+
+const unsigned int MP_WORD_BITS = 64;
+const word MP_WORD_MASK = ~((word)0);
+const word MP_WORD_TOP_BIT = (word)1 << (8*sizeof(word) - 1);
+
+extern void abort (void);
+
+word do_div(word n1, word n0, word d)
+{
+  word high = n1 % d, quotient = 0;
+  unsigned int j;
+
+  for(j = 0; j != MP_WORD_BITS; ++j)
+    {
+      word high_top_bit = (high & MP_WORD_TOP_BIT);
+
+      high <<= 1;
+      high |= (n0 >> (MP_WORD_BITS-1-j)) & 1;
+      quotient <<= 1;
+
+      if(high_top_bit || high >= d)
+	{
+	  high -= d;
+	  quotient |= 1;
+	}
+    }
+
+  return quotient;
+}
+
+int main()
+{
+  word result;
+
+  result = do_div(0x0000000000200000ll,
+		  0x0000000000000000ll,
+		  0x86E53497CE000000ll);
+
+  
+  if (result != 0x3CBA83)
+    abort ();
+
+  return 0;
+}
