bb97a5795d64fbecc9924026addbe56e4e3d70b9
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-03 06:35:05 +0000

    PR middle-end/32176 	* gcc.dg/pr32176.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126245 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr32176.c
@@ -0,0 +1,28 @@
+/* Contributed by Francois-Xavier Coudert  <fxcoudert@gcc.gnu.org>  */
+
+/* { dg-do compile } */
+/* { dg-options "-O2 -fprefetch-loop-arrays" } */
+/* { dg-options "-O2 -fprefetch-loop-arrays -march=i686 -msse" { target { { i?86-*-* x86_64-*-* } && ilp32 } } } */
+
+void foo (void)
+{
+  int i, m;
+  float xa[21];
+
+  m = 0;
+  while (1)
+    {
+      i = 0;
+      while (1)
+	{
+	  if (xa[(long int)i] == xa[(long int)(i+m)])
+	    _gfortran_abort ();
+	  if (i == 10)
+	    break;
+	  i++;
+	}
+      if (m == 10)
+	break;
+      m++;
+    }
+}
