0ef89dfdc208f14d10e19a0d9e3aad04f879bc0a
krebbel <krebbel@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-06 10:47:31 +0000

    2007-07-06  Andreas Krebbel  <krebbel1@de.ibm.com>
    	* libgcc2.h (word_type): Type definition removed.
    	(cmp_return_type, shift_count_type): Type definitions added.
    	(__lshrdi3, __ashldi3, __ashrdi3): word_type of second parameter
    	replaced with shift_count_type.
    	(__cmpdi2, __ucmpdi2): word_type of return type replaced with
    	cmp_return_type.
    	* libgcc2.c (__udivmoddi4, __moddi3): Type of local variable c
    	changed from word_type to Wtype.
    	(__lshrdi3, __ashldi3, __ashrdi3): word_type of second parameter
    	replaced with shift_count_type.
    	(__cmpdi2, __ucmpdi2): word_type of return type replaced with
    	cmp_return_type.
    	* c-common.c (handle_mode_attribute): Handling for libgcc_cmp_return and
    	libgcc_shift_count attribute added.
    	* target-def.h (TARGET_LIBGCC_CMP_RETURN_MODE,
    	TARGET_LIBGCC_SHIFT_COUNT_MODE): New target hooks defined.
    	(TARGET_INITIALIZER): New target hooks added.
    	* targhooks.c (default_libgcc_cmp_return_mode,
    	default_libgcc_shift_count_mode): Default implementations for the new
    	target hooks added.
    	* targhooks.h (default_libgcc_cmp_return_mode,
    	default_libgcc_shift_count_mode): Function prototypes added.
    	* target.h (struct gcc_target): Fields for the new target hooks added.
    	* optabs.c (expand_binop): Use shift_count_mode when expanding shift
    	as library call.
    	(prepare_cmp_insn): Use cmp_return_mode when expanding comparison as
    	library call.
    
    	* doc/tm.texi (TARGET_LIBGCC_CMP_RETURN_MODE,
    	TARGET_LIBGCC_SHIFT_COUNT_MODE): Documentation added.
    
    	* config/s390/s390.c (s390_libgcc_cmp_return_mode,
    	s390_libgcc_shift_count_mode): Functions added.
    	(TARGET_LIBGCC_CMP_RETURN_MODE,	TARGET_LIBGCC_SHIFT_COUNT_MODE): Target
    	hooks defined.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126410 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/targhooks.h
+++ gcc/targhooks.h
@@ -31,6 +31,8 @@ extern rtx default_builtin_setjmp_frame_value (void);
 extern bool default_pretend_outgoing_varargs_named (CUMULATIVE_ARGS *);
 
 extern enum machine_mode default_eh_return_filter_mode (void);
+extern enum machine_mode default_libgcc_cmp_return_mode (void);
+extern enum machine_mode default_libgcc_shift_count_mode (void);
 extern unsigned HOST_WIDE_INT default_shift_truncation_mask
   (enum machine_mode);
 extern unsigned int default_min_divisions_for_recip_mul (enum machine_mode);
