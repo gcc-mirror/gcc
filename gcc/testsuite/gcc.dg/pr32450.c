56d17681225aa3b26dd0ca0958fe9c4a329b07f4
Uros Bizjak <ubizjak@gmail.com>
2007-07-06 10:53:15 +0200

    re PR rtl-optimization/32450 (-pg causes miscompilation)
    	PR rtl_optimization/32450
    	* function.c (thread_prologue_and_epilogue_insns): Emit blockage insn
    	to ensure that instructions are not moved into the prologue when
    	profiling is on.  Remove unused prologue_end variable.
    	(expand_function_end): Emit blockage insn instead of ASM_INPUT rtx
    	as a scheduling barrier.
    
    testsuite/ChangeLog:
    
    	PR rtl_optimization/32450
    	* gcc.dg/pr32450.c: New runtime test.
    
    From-SVN: r126403

---
+++ gcc/testsuite/gcc.dg/pr32450.c
@@ -0,0 +1,34 @@
+/* Contributed by Joost VandeVondele  <jv244@cam.ac.uk> */
+
+/* { dg-do run } */
+/* { dg-require-profiling "-pg" } */
+/* { dg-options "-O2 -pg" } */
+/* { dg-options "-O2 -pg -mtune=core2" { target { i?86-*-* x86_64-*-* } } } */
+/* { dg-options "-O2 -pg -static" { target hppa*-*-hpux* } } */
+
+extern void abort (void);
+
+int stack_pointer;
+
+void
+__attribute__((noinline))
+mystop ()
+{
+  abort ();
+}
+
+void
+__attribute__((noinline))
+add ()
+{
+  if (stack_pointer + 1 > 10)
+    mystop ();
+
+  stack_pointer = stack_pointer + 1;
+}
+
+int main ()
+{
+  add ();
+  return stack_pointer - 1;
+}
