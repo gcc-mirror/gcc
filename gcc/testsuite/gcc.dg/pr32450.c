ab6393bb55e4fd6fd52623453b0881c4bfb58b4d
Uros Bizjak <ubizjak@gmail.com>
2007-07-06 11:49:52 +0200

    re PR rtl-optimization/32450 (-pg causes miscompilation)
            PR rtl-optimization/32450
            * function.c (thread_prologue_and_epilogue_insns): Emit blockage insn
            to ensure that instructions are not moved into the prologue when
            profiling is on.
    
    testsuite/ChangeLog:
    
            PR rtl-optimization/32450
            * gcc.dg/pr32450.c: New runtime test.
    
    From-SVN: r126407

---
+++ gcc/testsuite/gcc.dg/pr32450.c
@@ -0,0 +1,33 @@
+/* Contributed by Joost VandeVondele  <jv244@cam.ac.uk> */
+
+/* { dg-do run } */
+/* { dg-require-profiling "-pg" } */
+/* { dg-options "-O2 -pg" } */
+/* { dg-options "-O2 -pg -static" { target hppa*-*-hpux* } } */
+
+extern void abort (void);
+
+int stack_pointer;
+
+void
+__attribute__((noinline))
+mystop ()
+{
+  abort ();
+}
+
+void
+__attribute__((noinline))
+add ()
+{
+  if (stack_pointer + 1 > 10)
+    mystop ();
+
+  stack_pointer = stack_pointer + 1;
+}
+
+int main ()
+{
+  add ();
+  return stack_pointer - 1;
+}
