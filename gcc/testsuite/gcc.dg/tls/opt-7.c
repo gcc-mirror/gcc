8cea2d67aba7bf4d4551667c39647b478214fba1
Nathan Froyd <froydnj@codesourcery.com>
2007-07-06 20:45:29 +0000

    20001012-1.c: Run on all fpic-capable targets.
    	* gcc.dg/20001012-1.c: Run on all fpic-capable targets.
    	* gcc.dg/20001012-2.c: Likewise.
    	* gcc.dg/tls/opt-7.c: Require an fpic-capable target.
    	* gcc.dg/20000222-1.c: Add -fPIC on all fpic-capable targets.
    
    From-SVN: r126426

--- gcc/testsuite/gcc.dg/tls/opt-7.c
+++ gcc/testsuite/gcc.dg/tls/opt-7.c
@@ -1,6 +1,7 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -fPIC" } */
 /* { dg-require-effective-target tls } */
+/* { dg-require-effective-target fpic } */
 
 static __thread void *baz [4] __attribute__((tls_model ("initial-exec")));
 void foo (void)
