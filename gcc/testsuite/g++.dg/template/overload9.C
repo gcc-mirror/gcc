9788fee1c2448852af5d37eac5ed4f88e1f43352
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-07 07:31:54 +0000

    PR c++/32232 	* pt.c (resolve_overloaded_unification): Robustify.  Return a 	bool, not an int. 	(type_unification_real): Adjust accordingly. 	PR c++/32232 	* g++.dg/template/overload9.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126435 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/overload9.C
@@ -0,0 +1,18 @@
+// PR c++/32232
+
+template <typename T> struct A;
+template <typename T> struct B {};
+template <typename T> A<T>& operator<<(A<T>&, const B<T>&);
+
+template <typename T>
+struct A 
+{
+  A<T>& operator<<(A<T>& (*)(A<T>&)); // { dg-error "candidate" }
+};
+
+template <typename T> A<T>& foo(A<T>&);
+extern A<char> c;
+
+int main () {
+  c << (1, foo); // { dg-error "no match" }
+}
