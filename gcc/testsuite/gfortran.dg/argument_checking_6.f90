a77da7d9212b8fcc35a77548b26605fc838c63ed
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-08 20:57:07 +0000

    2007-07-08  Tobias Burnus  <burnus@net-b.de>
    	PR fortran/32669
    	* interface.c (get_expr_storage_size): Properly obtain lower bound.
    	(compare_actual_formal): Add space before parenthesis.
    
    2007-07-08  Tobias Burnus  <burnus@net-b.de>
    
    	PR fortran/32669
    	* gfortran.dg/argument_checking_6.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126467 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/argument_checking_6.f90
@@ -0,0 +1,24 @@
+! { dg-do compile }
+! PR fortran/32669
+!
+! Contributed by Janus Weil <jaydub66@gmail.com>
+!
+program tfe
+implicit none
+
+real,dimension(-1:1) ::  w
+real,dimension(1:4) ::  x
+real,dimension(0:3) ::  y
+real,dimension(-1:2) ::  z
+
+call sub(x(:))
+call sub(y(:))
+call sub(z(:))
+call sub(w(:)) ! { dg-error "too few elements" }
+
+contains
+  subroutine sub(a)
+    implicit none
+    real,dimension(1:4) :: a
+  end subroutine sub
+end program tfe
