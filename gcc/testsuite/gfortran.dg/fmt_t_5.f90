57728874d2a68f3918a2e44c65c6ae100e34eaf6
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2007-07-09 00:17:42 +0000

    re PR fortran/32678 ([4.2, 4.1]GFortan works incorrectly when writing with FORMAT Tx)
    2007-07-08  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR fortran/32678
    	* gfortran.dg/fmt_t_5.f90: New test.
    
    From-SVN: r126478

---
+++ gcc/testsuite/gfortran.dg/fmt_t_5.f90
@@ -0,0 +1,18 @@
+! { dg-do run }
+! PR32678 GFortan works incorrectly when writing with FORMAT Tx
+! Before patch, NULLs were inserted in output.
+! Test case from reporter enhanced to detect this problem.
+      character(25) :: output
+      character(1)  :: c
+      output = ""
+      open (unit=10, file="pr32678testfile", status="replace")
+      write (10,10) '12','a','b'
+      close (10, status="keep")
+      open (unit=10, file="pr32678testfile", access="stream")
+      read(10, pos=1) output(1:21)
+      if (output(1:21).ne."ab                  x") call abort
+      read(10) c
+      if (c.ne.achar(10)) call abort
+      close (10, status="delete")
+ 10   format (a2,t1,a1,t2,a1,t20,' x')
+      end
