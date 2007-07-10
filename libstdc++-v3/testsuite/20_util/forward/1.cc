d7f61ae2e8f8c6f740585be6e16067343be1ae87
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-10 19:20:51 +0000

    2007-07-10  Paolo Carlini  <pcarlini@suse.de>
    	* include/std/utility (identity, move, forward): Add.
    	* testsuite/20_util/move/requirements/explicit_instantiation.cc: New.
    	* testsuite/20_util/move/1.cc: New.
    	* testsuite/20_util/identity/value.cc: New.
    	* testsuite/20_util/identity/requirements/typedefs.cc: New.
    	* testsuite/20_util/identity/requirements/explicit_instantiation.cc:
    	New.
    	* testsuite/20_util/forward/requirements/explicit_instantiation.cc:
    	New.
    	* testsuite/20_util/forward/1.cc: New.
    	* testsuite/20_util/forward/1_neg.cc: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126522 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/20_util/forward/1.cc
@@ -0,0 +1,44 @@
+// { dg-do compile }
+// { dg-options "-std=gnu++0x" }
+
+// 2007-07-10  Paolo Carlini  <pcarlini@suse.de>
+//
+// Copyright (C) 2007 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#include <memory>
+#include <utility>
+
+template<class T, class A1, class A2>
+  std::shared_ptr<T>
+  factory(A1&& a1, A2&& a2)
+  {
+    return std::shared_ptr<T>(new T(std::forward<A1>(a1),
+				    std::forward<A2>(a2)));
+  }
+
+struct A
+{
+  A(int&, const double&);
+};
+
+void g()
+{
+  int i = 2;
+  std::shared_ptr<A> sp1 = factory<A>(i, 1.414);
+}
