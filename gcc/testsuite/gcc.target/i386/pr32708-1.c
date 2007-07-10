911efec9c202fcfbbaa1d77dabcf7dc6b07674a7
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-10 19:26:58 +0000

    PR target/32708         * config/i386/sse.md (vec_concatv2di): Disable for TARGET_64BIT.         (*vec_concatv2di_rex): New insn pattern.
    testsuite/ChangeLog:
    
            PR target/32708
            * gcc.target/i386/pr32708-1.c: New test.
            * gcc.target/i386/pr32708-2.c: Ditto.
            * gcc.target/i386/pr32708-3.c: Ditto.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126523 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr32708-1.c
@@ -0,0 +1,18 @@
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -msse2" } */
+
+typedef long long __v2di __attribute__ ((__vector_size__ (16)));
+typedef long long __m128i __attribute__ ((__vector_size__ (16)));
+
+static __inline __m128i __attribute__((__always_inline__))
+_mm_set_epi64x (long long __q1, long long __q0)
+{
+  return __extension__ (__m128i)(__v2di){ __q0, __q1 };
+}
+
+__m128i long2vector(long long __i)
+{
+  return _mm_set_epi64x (0, __i);
+}
+
+/* { dg-final { scan-assembler-not "movq2dq" } } */
