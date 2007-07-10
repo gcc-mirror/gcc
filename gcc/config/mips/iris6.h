c4b420229430383d7e625373dd76371299ce55d8
Rainer Orth <ro@TechFak.Uni-Bielefeld.DE>
2007-07-10 18:03:28 +0000

    re PR target/32538 (All libgomp tests fail to link on IRIX 6: copysignl undefined)
    	PR target/32538
    	* config/mips/iris6.h (LIBGCC_SPEC): Add libm.
    
    From-SVN: r126521

--- gcc/config/mips/iris6.h
+++ gcc/config/mips/iris6.h
@@ -1,6 +1,6 @@
 /* Definitions of target machine for GNU compiler.  IRIX version 6.
    Copyright (C) 1994, 1995, 1996, 1997, 1998, 2000, 2001, 2002, 2003, 2004,
-   2005, 2006
+   2005, 2006, 2007
    Free Software Foundation, Inc.
 
 This file is part of GCC.
@@ -96,10 +96,11 @@ Boston, MA 02110-1301, USA.  */
      " %{pthread:-lpthread} %{p:libprof1.a%s}%{pg:libprof1.a%s} -lc " \
      SUBTARGET_WARN_UNUSED_SPEC "}"
 
-/* Avoid getting two warnings for libgcc.a everytime we link.  */
+/* Avoid getting two warnings for libgcc.a everytime we link.  libgcc.a
+   contains references to copysignl, so link with libm to resolve them.  */
 #undef LIBGCC_SPEC
 #define LIBGCC_SPEC \
-  SUBTARGET_DONT_WARN_UNUSED_SPEC " -lgcc " SUBTARGET_WARN_UNUSED_SPEC
+  SUBTARGET_DONT_WARN_UNUSED_SPEC " -lgcc -lm " SUBTARGET_WARN_UNUSED_SPEC
 
 #undef ENDFILE_SPEC
 #define ENDFILE_SPEC \
