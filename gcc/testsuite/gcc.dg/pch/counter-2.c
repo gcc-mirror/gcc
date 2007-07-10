f38306151a70731214d84491ef72b1757f6de4c2
Manuel López-Ibáñez <manu@gcc.gnu.org>
2007-07-10 09:17:01 +0000

    re PR testsuite/25241 ([C++] DejaGNU does not distinguish between errors and warnings)
    2007-07-10  Manuel Lopez-Ibanez <manu@gcc.gnu.org>
    
    	PR testsuite/25241
    	* gcc.dg/pch/counter-2.c: Match every message with its appropriate
    	directive.
    
    From-SVN: r126511

--- gcc/testsuite/gcc.dg/pch/counter-2.c
+++ gcc/testsuite/gcc.dg/pch/counter-2.c
@@ -7,7 +7,9 @@
 #error __COUNTER__ != 0
 #endif
 
-#include "counter-2.h"/* { dg-error "`__COUNTER__' is invalid|No such file|they were invalid" } */
+#include "counter-2.h" /* { dg-warning "not used because `__COUNTER__' is invalid" } */
+/* { dg-error "counter-2.h: No such file or directory" "" { target *-*-* } 10 } */
+/* { dg-error "one or more PCH files were found, but they were invalid" "" { target *-*-* } 10 } */
 
 int main(void) 
 {
