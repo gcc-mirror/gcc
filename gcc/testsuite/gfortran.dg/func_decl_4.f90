f8faa85e29fc2c8f4e3941134ff03704334e427d
Daniel Franke <dfranke@gcc.gnu.org>
2007-07-12 18:15:11 -0400

    re PR fortran/31639 ([4.1/4.2/4.3] ICE in gfc_conv_constant, at fortran/trans-const.c:348 with len)
    gcc/fortran:
    2007-07-12  Daniel Franke  <franke.daniel@gmail.com>
    
    	PR fortran/31639
    	* decl.c (gfc_match_suffix): Removed surplus general error that hides
    	a more specific message.
    	* resolve.c (resolve_fl_variable): Reject illegal initializiers only
    	if not already done.
    	(resolve_fl_procedure): Added check for initializers of functions.
    
    gcc/testsuite:
    2007-07-12  Daniel Franke  <franke.daniel@gmail.com>
    
    	PR fortran/31639
    	* gfortran.dg/func_decl_4.f90: New test.
    
    From-SVN: r126605

---
+++ gcc/testsuite/gfortran.dg/func_decl_4.f90
@@ -0,0 +1,17 @@
+! { dg-do compile }
+! { dg-options "-c" }
+!
+! Functions shall not have an initializer.
+!
+
+function f1()                      ! { dg-error "cannot have an initializer" }
+  integer :: f1 = 42
+end function
+
+function f2() RESULT (r)           ! { dg-error "cannot have an initializer" }
+  integer :: r = 42
+end function
+
+function f3() RESULT (f3)          ! { dg-error "must be different than function name" }
+  integer :: f3 = 42
+end function                       ! { dg-excess-errors "" }
