e1c8221962aa8dfba5b2462449bccfe10c2d561e
Jakub Jelinek <jakub@redhat.com>
2007-07-12 14:16:54 +0200

    re PR fortran/32550 (openmp: COPYPRIVATE of pointer variables fails)
    	PR fortran/32550
    	* trans.h (GFC_POINTER_TYPE_P): Define.
    	* trans-types.c (gfc_sym_type): Set it for types on attr->sym.pointer.
    	* trans-openmp.c (gfc_omp_privatize_by_reference): Return false
    	if GFC_POINTER_TYPE_P is set on the type.
    
    	* testsuite/libgomp.fortran/pr32550.f90: New test.
    	* testsuite/libgomp.fortran/crayptr2.f90: New test.
    
    From-SVN: r126583

---
+++ libgomp/testsuite/libgomp.fortran/pr32550.f90
@@ -0,0 +1,20 @@
+! PR fortran/32550
+! { dg-do run }
+
+      integer, pointer, save :: ptr
+      integer, target :: targ
+      integer :: e
+!$omp threadprivate(ptr)
+      e = 0
+      targ = 42
+!$omp parallel shared(targ)
+!$omp single
+      ptr => targ
+!$omp end single copyprivate(ptr)
+      if (ptr.ne.42) then
+!$omp atomic
+	e = e + 1
+      end if
+!$omp end parallel
+      if (e.ne.0) call abort
+      end
