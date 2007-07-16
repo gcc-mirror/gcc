5273aaa9059a832ebe21bc60a4efb2b5acd61066
Ulrich Weigand <uweigand@gcc.gnu.org>
2007-07-16 13:30:46 +0000

    Fix broken commit -- add files that were missed.
    From-SVN: r126682

---
+++ gcc/testsuite/gcc.target/spu/dfcmgt.c
@@ -0,0 +1,10 @@
+/* { dg-do compile } */
+/* { dg-options "-march=celledp -O1" } */
+/* { dg-final { scan-assembler "dfcmgt" } } */
+
+int foo(double x, double y)
+{
+  if (__builtin_fabs(x) > __builtin_fabs(y))
+    return 0;
+}
+
