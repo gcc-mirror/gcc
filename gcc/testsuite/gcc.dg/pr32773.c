c473c074663dee54c89889fa79b1545a7c9e30c7
rakdver <rakdver@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-17 03:56:40 +0000

    PR rtl-optimization/32773 	* cfglayout.c (force_one_exit_fallthru): New function. 	(cfg_layout_finalize): Use it.
    	* gcc.dg/pr32773.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126700 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr32773.c
@@ -0,0 +1,11 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fprofile-use" } */
+/* { dg-options "-O -m4 -fprofile-use" { target sh-*-* } } */
+
+void foo (int *p)
+{
+  if (p)
+    *p = 0;
+} /* { dg-message "note: \[^\n\]*execution counts estimated" } */
+
+/* { dg-final { cleanup-coverage-files } } */
