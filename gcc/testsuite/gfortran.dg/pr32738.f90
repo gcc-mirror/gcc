532a6e9842208aeb6eafcfce72023d1c40586605
Daniel Franke <franke.daniel@gmail.com>
2007-07-19 17:11:38 -0400

    re PR fortran/32738 (Error: Function 'tree_size' at (1) has no IMPLICIT type)
    2007-07-19  Daniel Franke  <franke.daniel@gmail.com>
    
    	PR fortran/32738
    	* gfortran.dg/pr32768.f90: New test.
    
    From-SVN: r126790

---
+++ gcc/testsuite/gfortran.dg/pr32738.f90
@@ -0,0 +1,44 @@
+! { dg-do compile }
+! PR fortran/32738 
+!
+! A regression that mysteriously appeared and disappeared again.
+! Added to the testsuite "just in case".
+!
+! Contributed by Michael Richmond <michael DOT a DOT richmond AT nasa DT gov>
+!
+
+module cluster_definition
+  implicit none
+  integer, parameter, public:: cluster_size = 1000
+end module cluster_definition
+module cluster_tree
+  use cluster_definition, only: ct_cluster_size => cluster_size
+  implicit none
+  private
+  private:: ct_initialize, ct_dealloc, ct_tree_size
+  public:: initialize, dealloc, tree_size
+  interface initialize
+     module procedure ct_initialize
+  end interface
+  interface dealloc
+     module procedure ct_dealloc
+  end interface
+  interface tree_size
+     module procedure ct_tree_size
+  end interface
+contains
+  subroutine ct_initialize()
+  end subroutine ct_initialize
+  subroutine ct_dealloc()
+  end subroutine ct_dealloc
+  function ct_tree_size(t) result(s)
+    integer :: t
+    integer :: s
+    s = 0
+  end function ct_tree_size
+end module cluster_tree
+program example
+  use cluster_tree
+  implicit none
+     print *, tree_size(1)
+end program example
