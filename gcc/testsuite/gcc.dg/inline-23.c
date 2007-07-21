6957dd4ff85f0aa3883c7880513331ae9440a515
rask <rask@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-21 09:30:24 +0000

    * gcc.dg/inline-23.c: Use pointer sized type for cast from pointer.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126809 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/inline-23.c
+++ gcc/testsuite/gcc.dg/inline-23.c
@@ -2,16 +2,17 @@
 /* { dg-options "-std=gnu89" } */
 /* Make sure we can inline a varargs function whose variable arguments
    are not used.  See PR32493.  */
+#include <stddef.h>
 static inline __attribute__((always_inline)) void __check_printsym_format(const
 char *fmt, ...)
 {
 }
 static inline __attribute__((always_inline)) void print_symbol(const char *fmt,
-unsigned long addr)
+ptrdiff_t addr)
 {
  __check_printsym_format(fmt, "");
 }
 void do_initcalls(void **call)
 {
-   print_symbol(": %s()", (unsigned long) *call);
+   print_symbol(": %s()", (ptrdiff_t) *call);
 }
