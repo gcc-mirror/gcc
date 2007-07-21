bef2a00be661744f229a58738a1852b136491534
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2007-07-21 21:57:21 +0000

    re PR target/32641 (C99 fpclassify, isinf, isfinite, isnormal may raise FP exceptions)
    fixincludes:
    	PR target/32641
    
    	* inclhack.def (solaris_math_4, solaris_math_5, solaris_math_6,
    	solaris_math_7): Constify and make FP exception-safe.
    	* tests/base/iso/math_c99.h: Update.
    
    	* fixincl.x: Regenerate.
    
    gcc/testsuite:
    	* gcc.dg/c99-math-double-1.c: Mark test variables as volatile.
    	Test negative numbers also.
    	* gcc.dg/c99-math-float-1.c: Likewise.
    	* gcc.dg/c99-math-long-double-1.c: Likewise.
    	* gcc.dg/c99-math.h: Check for FP exceptions.  Update for negative
    	test inputs.
    
    From-SVN: r126815

--- gcc/testsuite/gcc.dg/c99-math-long-double-1.c
+++ gcc/testsuite/gcc.dg/c99-math-long-double-1.c
@@ -6,14 +6,17 @@
 
 int main(void)
 {
-  long double nan = NAN;
-  long double inf = INFINITY;
-  long double huge = HUGE_VALL;
-  long double norm = __LDBL_MIN__;
-  long double sub = __LDBL_MIN__ / 2;
-  long double zero = 0.0l;
+  volatile long double nan = NAN;
+  volatile long double inf = INFINITY;
+  volatile long double huge = HUGE_VALL;
+  volatile long double norm1 = __LDBL_MIN__;
+  volatile long double norm2 = 1;
+  volatile long double norm3 = __LDBL_MAX__;
+  volatile long double sub = __LDBL_MIN__ / 2;
+  volatile long double zero = 0.0l;
 
-  C99_MATH_TESTS (nan, inf, huge, norm, sub, zero)
+  C99_MATH_TESTS (nan, inf, huge, norm1, norm2, norm3, sub, zero, /*neg=*/0)
+  C99_MATH_TESTS (-nan, -inf, -huge, -norm1, -norm2, -norm3, -sub, -zero, /*neg=*/1)
 
   return 0;
 }
--- gcc/testsuite/gcc.dg/c99-math.h
+++ gcc/testsuite/gcc.dg/c99-math.h
@@ -1,9 +1,14 @@
 #include <math.h>
+#include <fenv.h>
 
 extern void abort(void);
 
-#define C99_MATH_TESTS(nan, inf, huge, norm, sub, zero)	\
+#define C99_MATH_TESTS(nan, inf, huge, norm1, norm2, norm3, sub, zero, neg) \
 {							\
+  if (feclearexcept (FE_ALL_EXCEPT) != 0)		\
+    abort();						\
+							\
+							\
   if (fpclassify (nan) != FP_NAN)			\
     abort ();						\
 							\
@@ -13,7 +18,13 @@ extern void abort(void);
   if (fpclassify (huge) != FP_INFINITE)			\
     abort ();						\
 							\
-  if (fpclassify (norm) != FP_NORMAL)			\
+  if (fpclassify (norm1) != FP_NORMAL)			\
+    abort ();						\
+							\
+  if (fpclassify (norm2) != FP_NORMAL)			\
+    abort ();						\
+							\
+  if (fpclassify (norm3) != FP_NORMAL)			\
     abort ();						\
 							\
   if (fpclassify (sub) != FP_SUBNORMAL)			\
@@ -32,7 +43,13 @@ extern void abort(void);
   if (isnan (huge))					\
     abort ();						\
 							\
-  if (isnan (norm))					\
+  if (isnan (norm1))					\
+    abort ();						\
+							\
+  if (isnan (norm2))					\
+    abort ();						\
+							\
+  if (isnan (norm3))					\
     abort ();						\
 							\
   if (isnan (sub))					\
@@ -51,7 +68,13 @@ extern void abort(void);
   if (!isinf (huge))					\
     abort ();						\
 							\
-  if (isinf (norm))					\
+  if (isinf (norm1))					\
+    abort ();						\
+							\
+  if (isinf (norm2))					\
+    abort ();						\
+							\
+  if (isinf (norm3))					\
     abort ();						\
 							\
   if (isinf (sub))					\
@@ -70,7 +93,13 @@ extern void abort(void);
   if (isfinite (huge))					\
     abort ();						\
 							\
-  if (!isfinite (norm))					\
+  if (!isfinite (norm1))				\
+    abort ();						\
+							\
+  if (!isfinite (norm2))				\
+    abort ();						\
+							\
+  if (!isfinite (norm3))				\
     abort ();						\
 							\
   if (!isfinite (sub))					\
@@ -89,7 +118,13 @@ extern void abort(void);
   if (isnormal (huge))					\
     abort ();						\
 							\
-  if (!isnormal (norm))					\
+  if (!isnormal (norm1))				\
+    abort ();						\
+							\
+  if (!isnormal (norm2))				\
+    abort ();						\
+							\
+  if (!isnormal (norm3))				\
     abort ();						\
 							\
   if (isnormal (sub))					\
@@ -99,28 +134,103 @@ extern void abort(void);
     abort ();						\
 							\
 							\
-  if (signbit (norm))					\
+  if (!!signbit (nan) != neg)				\
     abort ();						\
 							\
-  if (!signbit (-(norm)))				\
+  if (!!signbit (inf) != neg)				\
+    abort ();						\
+							\
+  if (!!signbit (huge) != neg)				\
+    abort ();						\
+							\
+  if (!!signbit (norm1) != neg)				\
+    abort ();						\
+							\
+  if (!!signbit (norm2) != neg)				\
+    abort ();						\
+							\
+  if (!!signbit (norm3) != neg)				\
+    abort ();						\
+							\
+  if (!!signbit (sub) != neg)				\
+    abort ();						\
+							\
+  if (!!signbit (zero) != neg)				\
     abort ();						\
 							\
 							\
-  if (!isgreater ((inf), (norm)))			\
+  if (neg)						\
+  {							\
+    if (!isless ((inf), (norm1)))			\
+      abort ();						\
+							\
+    if (!isless ((inf), (norm2)))			\
+      abort ();						\
+							\
+    if (!isless ((inf), (norm3)))			\
+      abort ();						\
+							\
+    if (!islessequal ((inf), (huge)))			\
+      abort ();						\
+							\
+    if (!isgreater ((norm1), (inf)))			\
+      abort ();						\
+							\
+    if (!isgreater ((norm2), (inf)))			\
+      abort ();						\
+							\
+    if (!isgreater ((norm3), (inf)))			\
+      abort ();						\
+							\
+    if (!isgreaterequal ((huge), (inf)))		\
+      abort ();						\
+  }							\
+  else							\
+  {							\
+    if (!isgreater ((inf), (norm1)))			\
+      abort ();						\
+							\
+    if (!isgreater ((inf), (norm2)))			\
+      abort ();						\
+							\
+    if (!isgreater ((inf), (norm3)))			\
+      abort ();						\
+							\
+    if (!isgreaterequal ((inf), (huge)))		\
+      abort ();						\
+							\
+    if (!isless ((norm1), (inf)))			\
+      abort ();						\
+							\
+    if (!isless ((norm2), (inf)))			\
+      abort ();						\
+							\
+    if (!isless ((norm3), (inf)))			\
+      abort ();						\
+							\
+    if (!islessequal ((huge), (inf)))			\
+      abort ();						\
+  }							\
+							\
+  if (!islessgreater ((inf), (norm1)))			\
     abort ();						\
 							\
-  if (!isgreaterequal ((inf), (huge)))			\
+  if (!islessgreater ((inf), (norm2)))			\
     abort ();						\
 							\
-  if (!isless ((norm), (inf)))				\
+  if (!islessgreater ((inf), (norm3)))			\
     abort ();						\
 							\
-  if (!islessequal ((huge), (inf)))			\
+  if (!isunordered ((nan), (norm1)))			\
     abort ();						\
 							\
-  if (!islessgreater ((inf), (norm)))			\
+  if (!isunordered ((nan), (norm2)))			\
     abort ();						\
 							\
-  if (!isunordered ((nan), (norm)))			\
+  if (!isunordered ((nan), (norm3)))			\
     abort ();						\
+							\
+							\
+  if (fetestexcept (FE_ALL_EXCEPT) != 0)		\
+    abort();						\
 }
