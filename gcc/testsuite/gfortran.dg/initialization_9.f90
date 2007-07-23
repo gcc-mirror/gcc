01c4a808e7c39f268527280725795d3ff9e8f7b8
dfranke <dfranke@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-23 20:35:03 +0000

    gcc/fortran: 2007-07-23  Daniel Franke  <franke.daniel@gmail.com>
    	PR fortran/25104
    	PR fortran/31639
    	* expr.c (check_transformational): Reject valid transformational
    	intrinsics to avoid ICE.
    	(check_inquiry): Report error for assumed character lengths for
    	all supported standards.
    	(check_init_expr): Whitespace fix.
    
    gcc/testsuite:
    2007-07-23  Daniel Franke  <franke.daniel@gmail.com>
    
    	PR fortran/31639
    	* gfortran.dg/initialization_9.f90: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126858 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/initialization_9.f90
@@ -0,0 +1,11 @@
+! { dg-do compile }
+!
+! PR fortran/31639
+! Contributed by Martin Michlmayr <tbm AT cyrius DOT com>
+
+   integer function xstrcmp(s1)
+     character*(*), intent(in) :: s1
+     integer :: n1 = len(s1)            ! { dg-error "assumed character length variable" }
+     n1 = 1
+     return
+   end function xstrcmp
