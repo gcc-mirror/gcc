d61e7fe912734c891cde7b39f34f91b301a73d10
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-23 12:49:45 +0000

    gcc/ 	* config/mips/mips.c (override_options): Use mips_costs to derive 	the default branch cost. 	* config/mips/mips.h (BRANCH_COST): Use mips_branch_cost rather 	than mips_costs. 	* config/mips/mips.opt (mbranch-cost=): New option. 	* doc/invoke.texi (-mbrach-cost): Document new MIPS option.
    gcc/testsuite/
    	* gcc.target/mips/branch-cost-1.c: New test.
    	* gcc.target/mips/branch-cost-2.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126846 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/mips/branch-cost-1.c
@@ -0,0 +1,8 @@
+/* { dg-mips-options "-mbranch-cost=1 -mips64 -O2" } */
+int
+foo (int x, int y, int z, int k)
+{
+  return x == k ? x + y : z - x;
+}
+/* { dg-final { scan-assembler-not "\t(movz|movn)\t" } } */
+/* { dg-final { scan-assembler "\t(bne|beq)\t" } } */
