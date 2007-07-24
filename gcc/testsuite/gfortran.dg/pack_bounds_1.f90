18fe404fbc9a86d21bcb6d0f02cf2a1d4448d705
Thomas Koenig <tkoenig@gcc.gnu.org>
2007-07-24 05:52:44 +0000

    re PR fortran/30814 (non-conforming array sizes in PACK should raise an error)
    2007-07-24  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/30814
    	* trans-decl.c (generate_function_code):  Add argument
    	for flag_bounds_check to the array for set_options.
    	* invoke.texi:  Mention that some checks require
    	-fbounds-check to be set during compilation of the
    	main program.
    
    2007-07-24  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/30814
    	* libgfortran.h:  Add bounds_check to compile_options_t.
    	* runtime/compile_options.c (set_options):  Add handling
    	of compile_options.bounds_check.
    	* intrinsics/pack_generic.c (pack_internal):  Also determine
    	the number of elements if compile_options.bounds_check is
    	true.  Raise runtime error if a different array shape is
    	detected.
    
    2007-07-24  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/30814
    	* gfortran.dg/pack_bounds_1.f90:  New test case.
    
    From-SVN: r126866

---
+++ gcc/testsuite/gfortran.dg/pack_bounds_1.f90
@@ -0,0 +1,10 @@
+! { dg-do run }
+! { dg-options "-fbounds-check" }
+! { dg-shouldfail "Incorrect extent in return value of  PACK intrinsic" }
+! PR 30814 - a bounds error with pack was not caught.
+program main
+  integer :: a(2,2), b(5)
+  a = reshape((/ 1, -1, 1, -1 /), shape(a))
+  b = pack(a, a /= 0)
+end program main
+! { dg-output "Fortran runtime error: Incorrect extent in return value of PACK intrinsic" }
