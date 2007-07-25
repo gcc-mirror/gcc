17f9cc79931f60c4f363dc5b58cc4f40d59ea981
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-25 01:17:11 +0000

    2007-07-24  H.J. Lu  <hongjiu.lu@intel.com>
    	*  gcc.dg/dfp/fe-convert-2.c: Unconditionally enable testing
    	for converting from 1.0e-96 and 0.00048828125 to _Decimal32.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126898 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/dfp/fe-convert-2.c
+++ gcc/testsuite/gcc.dg/dfp/fe-convert-2.c
@@ -14,11 +14,10 @@ CONVERT (101, d, d32, 1.0e97, FE_OVERFLOW|FE_INEXACT)
 CONVERT (102, d, d32, -1.0e96, FE_INEXACT)
 CONVERT (103, d, d32, -1.0e97, FE_OVERFLOW|FE_INEXACT) 
 
-#ifdef __DECIMAL_BID_FORMAT__
-/* These only result in fp exceptions with BID. DPD doesn't work.  */
+/* FIXME: These only result in fp exceptions when libbid is used.
+   libdecnumber doesn't work correctly.  */
 CONVERT (104, d, d32, 1.0e-96, FE_UNDERFLOW|FE_INEXACT)
 CONVERT (105, d, d32, 0.00048828125, FE_INEXACT)  /* exact power of 2 */
-#endif
 
 int
 main ()
@@ -27,10 +26,8 @@ main ()
   convert_101 ();
   convert_102 ();
   convert_103 ();
-#ifdef __DECIMAL_BID_FORMAT__
   convert_104 ();
   convert_105 ();
-#endif
 
   if (failcnt != 0)
     abort ();
