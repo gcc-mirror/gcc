80447f96b88bd2fef934670b8165fe115684913f
rask <rask@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-25 10:54:52 +0000

    * gcc.dg/torture/pr30313.c (struct S): Make sure the bit-field is 	  exactly as wide as an int.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126906 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/torture/pr30313.c
+++ gcc/testsuite/gcc.dg/torture/pr30313.c
@@ -4,7 +4,15 @@ static inline void bar(){}
 
 struct S
 {
+#if __INT_MAX__ == 32767
+  signed int i: 16;
+#elif __INT_MAX__ == 2147483647
   signed int i: 32;
+#elif __INT_MAX__ == 9223372036854775807
+  signed int i: 64;
+#else
+#error Please add support for your target here
+#endif
 };
 
 int main()
