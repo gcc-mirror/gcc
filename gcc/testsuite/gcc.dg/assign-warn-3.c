5153cd4b9ce4dfd34528a9f826655230a9f91c2a
Nathan Froyd <froydnj@codesourcery.com>
2007-07-26 12:36:39 +0000

    assign-warn-3.c (f0): Declare as inline.
    	PR/19232
    	* gcc.dg/assign-warn-3.c (f0): Declare as inline.
    	(f1): Likewise.
    
    From-SVN: r126955

--- gcc/testsuite/gcc.dg/assign-warn-3.c
+++ gcc/testsuite/gcc.dg/assign-warn-3.c
@@ -5,9 +5,9 @@
 /* { dg-options "-O3 -std=c99 -pedantic-errors" } */
 
 /* This is valid to execute, so maybe shouldn't warn at all.  */
-void f0(x) signed char *x; { }
+inline void f0(x) signed char *x; { }
 void g0(unsigned char *x) { f0(x); } /* { dg-bogus "warning: pointer targets in passing argument 1 of 'f0' differ in signedness" } */
 
 /* This is undefined on execution but still must compile.  */
-void f1(x) int *x; { }
+inline void f1(x) int *x; { }
 void g1(unsigned int *x) { f1(x); } /* { dg-bogus "warning: pointer targets in passing argument 1 of 'f1' differ in signedness" } */
