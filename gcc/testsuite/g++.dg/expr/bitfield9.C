83144bd60ce827e50425094f47f39aed75324cd6
Mark Mitchell <mmitchel@gcc.gnu.org>
2007-07-27 17:13:29 +0000

    re PR c++/32346 (long long bitfield passed to int argument incorrectly)
    	PR c++/32346
    	* call.c (convert_for_arg_passing): Only widen bitfields to their
    	declared types if necessary.
    	PR c++/32346
    	* g++.dg/expr/bitfield9.C: New test.
    
    From-SVN: r126986

---
+++ gcc/testsuite/g++.dg/expr/bitfield9.C
@@ -0,0 +1,25 @@
+// PR c++/32346
+// { dg-do run }
+
+extern "C" void abort();
+
+struct S {
+  long long i : 32;
+};
+
+void f(int i, int j) {
+  if (i != 0xabcdef01)
+    abort();
+  if (j != 0)
+    abort();
+}
+
+void g(S s) {
+  f(s.i, 0);
+}
+
+int main() {
+  S s;
+  s.i = 0xabcdef01;
+  g(s);
+}
