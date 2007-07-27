08e5dca92fecfab3d843cf5550ade83bb8425e43
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-27 16:33:50 +0000

    2007-07-27  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR fortran/32760
    	* gfortran.dg/private_type_7.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@126982 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/private_type_7.f90
@@ -0,0 +1,34 @@
+! { dg-do compile }
+! PR32760 Error defining subroutine named PRINT
+! Test case derived from original PR.
+
+module gfcbug68
+  implicit none
+  private :: write
+
+contains
+
+  function foo (i)
+    integer, intent(in)  :: i
+    integer foo
+
+    write (*,*) i
+    call write(i)
+    foo = i
+  end function foo
+
+  subroutine write (m)
+    integer, intent(in) :: m
+    print *, m*m*m
+  end subroutine write
+
+end module gfcbug68
+
+program testit
+  use gfcbug68
+  integer :: i = 27
+  integer :: k
+  
+  k = foo(i)
+  print *, "in the main:", k
+end program testit
