269a565538d4e84b188bbefb712b21ba6636fef8
rask <rask@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-27 23:03:01 +0000

    PR testsuite/32471 	* gcc.dg/torture/pr30364-1.c (f)(main): Use INT_MAX instead of 	  assuming it is 0x7ffffffff. 	* gcc.dg/torture/pr30364-2.c (f)(main): Likewise. 	* gcc.dg/torture/pr30364-3.c (f)(main): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@127005 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/torture/pr30364-2.c
+++ gcc/testsuite/gcc.dg/torture/pr30364-2.c
@@ -1,19 +1,20 @@
 /* { dg-do run } */
 
+#include <limits.h>
 extern void abort (void);
 
 int f(unsigned int a, unsigned int b)
 {
-  if (a > 0x7FFFFFF0) return 0;
-  if (b > 0x7FFFFFF0) return 0;
+  if (a > INT_MAX - 15) return 0;
+  if (b > INT_MAX - 15) return 0;
 
   int c = (a - 20) + (b - 20);
-  return c > 0x7FFFFFF0;
+  return c > INT_MAX - 15;
 }
 
 int main()
 {
-  if (f (0x7FFFFFF0, 41) != 1)
+  if (f (INT_MAX - 15, 41) != 1)
     abort ();
   return 0;
 }
