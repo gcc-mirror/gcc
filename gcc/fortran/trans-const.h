d234d788594330c1fdd82980d512b32fd0485b64
Nick Clifton <nickc@redhat.com>
2007-08-01 16:29:36 +0000

    arith.c: Change copyright header to refer to version 3 of the GNU General Public...
    * arith.c: Change copyright header to refer to version 3 of the GNU General 
      Public License and to point readers at the COPYING3 file and the FSF's 
      license web page.
    * openmp.c, interface.c, intrinsic.c, trans-array.c, trans-expr.c, symbol.c, 
      iso-fortran-env.def, intrinsic.h, decl.c, trans-array.h, matchexp.c, 
      dump-parse-tree.c, trans-common.c, array.c, Make-lang.in, trans-openmp.c, 
      gfortran.h, error.c, iso-c-binding.def, lang.opt, data.c, trans-const.c, 
      trans-stmt.c, expr.c, trans-const.h, trans-stmt.h, module.c, trans.c, 
      scanner.c, trans-types.c, trans.h, gfortranspec.c, trans-types.h, 
      lang-specs.h, io.c, bbt.c, resolve.c, f95-lang.c, st.c, iresolve.c, match.c,
      trans-decl.c, trans-io.c, target-memory.c, match.h, target-memory.h, parse.c,
      arith.h, check.c, dependency.c, parse.h, types.def, convert.c, dependency.h,
      primary.c, trans-intrinsic.c, options.c, misc.c, simplify.c: Likewise.
    
    From-SVN: r127129

--- gcc/fortran/trans-const.h
+++ gcc/fortran/trans-const.h
@@ -1,13 +1,13 @@
 /* Header for code constant translation functions
-   Copyright (C) 2002, 2003, 2004, 2005, 2006 Free Software Foundation,
-   Inc.
+   Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007
+   Free Software Foundation, Inc.
    Contributed by Paul Brook
 
 This file is part of GCC.
 
 GCC is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free
-Software Foundation; either version 2, or (at your option) any later
+Software Foundation; either version 3, or (at your option) any later
 version.
 
 GCC is distributed in the hope that it will be useful, but WITHOUT ANY
@@ -16,9 +16,8 @@ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 for more details.
 
 You should have received a copy of the GNU General Public License
-along with GCC; see the file COPYING.  If not, write to the Free
-Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
-02110-1301, USA.  */
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
 
 /* Converts between INT_CST and GMP integer representations.  */
 tree gfc_conv_mpz_to_tree (mpz_t, int);
