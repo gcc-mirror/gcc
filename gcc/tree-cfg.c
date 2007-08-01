fd859561282621052d1b18849d4ba1e006201ca3
rakdver <rakdver@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-08-01 11:41:25 +0000

    * tree-cfg.c (tree_merge_blocks): Preserve loop exit phi nodes only 	in loop closed ssa.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@127119 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-cfg.c
+++ gcc/tree-cfg.c
@@ -1274,9 +1274,10 @@ tree_merge_blocks (basic_block a, basic_block b)
       tree copy;
       bool may_replace_uses = may_propagate_copy (def, use);
 
-      /* In case we have loops to care about, do not propagate arguments of
-	 loop closed ssa phi nodes.  */
+      /* In case we maintain loop closed ssa form, do not propagate arguments
+	 of loop exit phi nodes.  */
       if (current_loops
+	  && (current_loops->state & LOOP_CLOSED_SSA)
 	  && is_gimple_reg (def)
 	  && TREE_CODE (use) == SSA_NAME
 	  && a->loop_father != b->loop_father)
