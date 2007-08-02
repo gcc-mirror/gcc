e77f031de8d0c47391627633b0646558840dd4d9
Nick Clifton <nickc@redhat.com>
2007-08-02 10:36:13 +0000

    typeck.c: Change copyright header to refer to version 3 of the GNU General Public...
    * typeck.c: Change copyright header to refer to version 3 of the GNU General 
      Public License and to point readers at the COPYING3 file and the FSF's license
      web page.
    * optimize.c, lang-specs.h, init.c, class.c, repo.c, decl.c, config-lang.in, 
      cp-tree.def, call.c, decl.h, ptree.c, Make-lang.in, method.c, rtti.c, 
      cp-objcp-common.c, g++spec.c, cp-objcp-common.h, except.c, error.c, 
      operators.def, cvt.c, tree.c, mangle.c, cp-tree.h, dump.c, search.c, 
      friend.c, expr.c, cp-gimplify.c, cxx-pretty-print.c, cp-lang.c, typeck2.c, 
      pt.c, cxx-pretty-print.h, semantics.c, name-lookup.c, lex.c, decl2.c,
      name-lookup.h, parser.c: Likewise.
    
    From-SVN: r127154

--- gcc/cp/method.c
+++ gcc/cp/method.c
@@ -1,14 +1,15 @@
 /* Handle the hair of processing (but not expanding) inline functions.
    Also manage function and variable name overloading.
    Copyright (C) 1987, 1989, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
+   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2007
+   Free Software Foundation, Inc.
    Contributed by Michael Tiemann (tiemann@cygnus.com)
 
 This file is part of GCC.
 
 GCC is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
+the Free Software Foundation; either version 3, or (at your option)
 any later version.
 
 GCC is distributed in the hope that it will be useful,
@@ -17,9 +18,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with GCC; see the file COPYING.  If not, write to
-the Free Software Foundation, 51 Franklin Street, Fifth Floor,
-Boston, MA 02110-1301, USA.  */
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
 
 
 /* Handle method declarations.  */
