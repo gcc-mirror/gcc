2ee0c1fb57b65ecef76c0fb14c9d05c4fbec2980
Paolo Carlini <pcarlini@suse.de>
2007-08-02 17:39:51 +0000

    DR 660, [Ready] in Toronto.
    2007-08-02  Paolo Carlini  <pcarlini@suse.de>
    
    	DR 660, [Ready] in Toronto.
    	* include/bits/stl_function.h (bit_and, bit_or, bit_xor): Add.
    	* testsuite/20_util/function_objects/dr660.cc: New.
    	* docs/html/ext/howto.html: Add an entry for DR 660, update.
    
    	* docs/html/ext/lwg-closed.html, docs/html/ext/lwg-active.html,
    	docs/html/ext/lwg-defects.html: Import Revision 49.
    
    From-SVN: r127166

---
+++ libstdc++-v3/testsuite/20_util/function_objects/dr660.cc
@@ -0,0 +1,42 @@
+// 2007-08-02  Paolo Carlini  <pcarlini@suse.de>
+
+// Copyright (C) 2007 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#include <functional>
+#include <testsuite_hooks.cc>
+
+// DR 660. Missing Bitwise Operations.
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  for (int i1 = 0; i1 < 1000; ++i1)
+    for (int i2 = 0; i2 < 1000; ++i2)
+      {
+	VERIFY( std::bit_and<int>()(i1, i2) == (i1 & i2) );
+	VERIFY( std::bit_or<int>()(i1, i2) == (i1 | i2) );
+	VERIFY( std::bit_xor<int>()(i1, i2) == (i1 ^ i2) );
+      }
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
