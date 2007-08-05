9dfbac5b94278cac26c43986553827e0fe4adc8e
Volker Reichelt <v.reichelt@netcologne.de>
2007-08-05 20:09:32 +0000

    revert: parser.c (cp_parser_member_declaration): Pedwarn about stray semicolons after member declarations.
    cp:
    	Revert:
    	2007-03-26  Dirk Mueller  <dmueller@suse.de>
    
    	* parser.c (cp_parser_member_declaration): Pedwarn
    	about stray semicolons after member declarations.
    
    testsuite:
    	* g++.dg/warn/pedantic2.C: Fix error marker.  Add more tests.
    
    	Revert:
    	2007-03-22  Dirk Mueller  <dmueller@suse.de>
    
    	* g++.old-deja/g++.brendan/warnings2.C: Don't use -pedantic.
    
    From-SVN: r127226

--- gcc/testsuite/g++.dg/warn/pedantic2.C
+++ gcc/testsuite/g++.dg/warn/pedantic2.C
@@ -2,5 +2,9 @@
 
 class foo
 {
-  foo() {};      // { dg-error "extra" }
+  foo() {};
+  void bar() {};
+
+  foo(int) {};;  // { dg-error "extra" }
+  void bar(int) {};;  // { dg-error "extra" }
 };
