94ea794e3a92fa2f4d829f898e3fd98bea4bb872
rask <rask@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-08-07 10:09:59 +0000

    * gcc.c-torture/execute/simd-4.c (__ev_convert_s64)(main): Use 	  int32_t for 32-bit integer. Use int64_t for 64-bit integer.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@127267 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/simd-4.c
+++ gcc/testsuite/gcc.c-torture/execute/simd-4.c
@@ -1,17 +1,18 @@
-typedef int __attribute__((vector_size(8))) v2si;
-long long s64;
+#include <stdint.h>
+typedef int32_t __attribute__((vector_size(8))) v2si;
+int64_t s64;
 
-static inline long long
+static inline int64_t
 __ev_convert_s64 (v2si a)
 {
-  return (long long) a;
+  return (int64_t) a;
 }
 
 int main()
 {
-  union { long long ll; int i[2]; } endianness_test;
+  union { int64_t ll; int32_t i[2]; } endianness_test;
   endianness_test.ll = 1;
-  int little_endian = endianness_test.i[0];
+  int32_t little_endian = endianness_test.i[0];
   s64 = __ev_convert_s64 ((v2si){1,0xffffffff});
   if (s64 != (little_endian ? 0xffffffff00000001LL : 0x1ffffffffLL))
     abort ();
