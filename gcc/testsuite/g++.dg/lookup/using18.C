f8be65bb3cad41ca6c58a6631e8976df2e8789b5
aaw <aaw@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-08-10 18:12:12 +0000

    gcc/cp/ 	* name-lookup.c (do_nonmember_using_decl): Print an error for ambiguous 	type lookups. 	(ambiguous_decl): Construct tree of ambiguous types.  Remove extaneous 	function parameter. 	(unqualified_namespace_lookup): Fix ambiguous_decl call. 	(lookup_using_namespace): Fix ambiguous_decl call. 	(qualified_lookup_using_namespace): Fix ambiguous_decl call.
    	gcc/testsuite/
    	* g++.dg/lookup/using18.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@127347 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/lookup/using18.C
@@ -0,0 +1,20 @@
+// Copyright (C) 2007 Free Software Foundation
+// Contributed by Ollie Wild <aaw@google.com>
+// { dg-do compile }
+
+namespace N1 {
+  void f ();
+  struct f; // { dg-error "" "candidate" }
+}
+
+namespace N2 {
+  void f (int);
+  struct f; // { dg-error "" "candidate" }
+}
+
+namespace M {
+  using namespace N1;
+  using namespace N2;
+}
+
+using M::f; // { dg-error "ambiguous" }
