654b60732e6a604471862085f0037fd05df86887
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2007-08-12 20:19:59 +0000

    re PR fortran/31629 (option to make module entities PRIVATE by default)
    	PR fortran/31629
    
    	* lang.opt (-fmodule-private): New option.
    	* gfortran.h (gfc_option_t): Add flag_module_private member.
    	* invoke.texi (-fmodule-private): Document the new option.
    	* module.c (gfc_check_access): Allow the -fmodule-private option
    	to modify the default behaviour.
    	* options.c (gfc_init_options): Initialize flag_module_private.
    	(gfc_handle_option): Handle -fmodule-private.
    
    	* gfortran.dg/module_private_1.f90: New test.
    
    From-SVN: r127381

---
+++ gcc/testsuite/gfortran.dg/module_private_1.f90
@@ -0,0 +1,20 @@
+! { dg-do compile }
+! { dg-options "-fmodule-private" }
+module bar
+  implicit none
+  public :: i
+  integer :: i
+end module bar
+
+module foo
+  implicit none
+  integer :: j
+end module foo
+
+program main
+  use bar, only : i
+  use foo, only : j ! { dg-error "not found in module" }
+  i = 1
+  j = 1
+  print *, i, j
+end program main
