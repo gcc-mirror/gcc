0f92de1c3b1d7eaa16b897f298f06fe436387070
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-08-14 09:01:52 +0000

    (Output_Non_Modifed_In_Out_Warnings, Warn_On_Unassigned_Out_Parameter): 	New functions.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@127472 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/sem_warn.ads
+++ gcc/ada/sem_warn.ads
@@ -105,6 +105,13 @@ package Sem_Warn is
    -- Output Routines --
    ---------------------
 
+   procedure Output_Non_Modifed_In_Out_Warnings;
+   --  Warnings about IN OUT parameters that could be IN are collected till
+   --  the end of the compilation process (see body of this routine for a
+   --  discussion of why this is done). This procedure outputs the warnings.
+   --  Note: this should be called before Output_Unreferenced_Messages, since
+   --  if we have an IN OUT warning, that's the one we want to see!
+
    procedure Output_Obsolescent_Entity_Warnings (N : Node_Id; E : Entity_Id);
    --  N is a reference to obsolescent entity E, for which appropriate warning
    --  messages are to be generated (caller has already checked that warnings
@@ -159,6 +166,14 @@ package Sem_Warn is
    --  the index is of the form of a literal or Name'Length [- literal], then
    --  a warning is generated that the subscripting operation is possibly
    --  incorrectly assuming a lower bound of 1.
+
+   procedure Warn_On_Unassigned_Out_Parameter
+     (Return_Node : Node_Id;
+      Scope_Id    : Entity_Id);
+   --  Called when processing a return statement given by Return_Node. Scope_Id
+   --  is the Entity_Id for the procedure in which the return statement lives.
+   --  A check is made for the case of a procedure with out parameters that
+   --  have not yet been assigned, and appropriate warnings are given.
 
    procedure Warn_On_Useless_Assignment
      (Ent : Entity_Id;
