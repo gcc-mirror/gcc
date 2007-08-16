a86a1d8bc148960f6534efd6f4920551ada443b6
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-08-16 09:05:17 +0000

    /cp 2007-08-16  Paolo Carlini  <pcarlini@suse.de>
    	PR c++/31132
    	* pt.c (tsubst_friend_function): When check_classfn
    	returns error_mark_node likewise return it.
    
    /testsuite
    2007-08-16  Paolo Carlini  <pcarlini@suse.de>
    
    	PR c++/31132
    	* g++.dg/template/crash69.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@127535 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/crash69.C
@@ -0,0 +1,12 @@
+// PR c++/31132
+
+template<typename T> class A
+{
+  static int i; // { dg-error "is private" }
+  friend int T::foo(); // { dg-error "does not match" }
+};
+
+struct B
+{
+  void foo() { A<B>::i; } // { dg-error "within|candidate" }
+};
