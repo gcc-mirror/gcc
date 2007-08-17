e3c15b04f6622c310294816d89fc4664323fb0b3
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-08-17 05:24:24 +0000

    PR middle-end/32970 gcc/ 	* tree.c (reconstruct_complex_type): For a pointer to a vector, 	use build_qualified_type to retain qualifiers of the base type. testsuite/ 	* g++.dg/ext/altivec-14.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@127578 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/ext/altivec-14.C
@@ -0,0 +1,13 @@
+/* { dg-do compile { target powerpc*-*-* } } */
+/* { dg-require-effective-target powerpc_altivec_ok } */
+/* { dg-options "-maltivec" } */
+
+void f (__attribute__((altivec (vector__))) signed int * a,
+	__attribute__((altivec (vector__))) signed int * const b);
+
+int
+foo (void)
+{
+  __attribute__((altivec (vector__))) signed int a[1], b[1];
+  f (a, b);
+}
