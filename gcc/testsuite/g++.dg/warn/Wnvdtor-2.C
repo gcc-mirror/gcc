a37e11205961890d4a52cca73615df79976171b2
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-08-20 15:08:24 +0000

    PR c++/7302         * cp/class.c (finish_struct_1): Warn when a class has virtual         functions and accessible non-virtual destructor.
            * doc/invoke.texi (-Wnon-virtual-dtor): Update documentation.
    
            * g++.dg/warn/Wnvdtor-2.C: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@127649 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/warn/Wnvdtor-2.C
@@ -0,0 +1,54 @@
+// PR c++/7302
+// { dg-do compile }
+// { dg-options "-Wnon-virtual-dtor" }
+
+// Warn when a class has virtual functions and accessible non-virtual
+// destructor, in which case it would be possible but unsafe to delete
+// an instance of a derived class through a pointer to the base class.
+
+struct A
+{ // { dg-bogus "non-virtual destructor" }
+protected:
+  ~A();
+public:
+  virtual void f() = 0;
+};
+
+struct B
+{ // { dg-bogus "non-virtual destructor" }
+private:
+  ~B();
+public:
+  virtual void f() = 0;
+};
+
+struct C
+{ // { dg-warning "non-virtual destructor" }
+  virtual void f() = 0;
+};
+
+struct D
+{ // { dg-warning "non-virtual destructor" }
+  ~D();
+  virtual void f() = 0;
+};
+
+struct E;
+
+struct F
+{ // { dg-warning "non-virtual destructor" }
+protected:
+  friend class E;
+  ~F();
+public:
+  virtual void f() = 0;
+};
+
+struct G
+{ // { dg-warning "non-virtual destructor" }
+private:
+  friend class E;
+  ~G();
+public:
+  virtual void f() = 0;
+};
