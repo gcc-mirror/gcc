3a950715b55e140de3adf4a0d797b69505c0fabc
bonzini <bonzini@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-08-23 12:15:20 +0000

    gcc: 2007-08-23  Paolo Bonzini  <bonzini@gnu.org>
    	* config/i386/sse.md (*sse_and<mode>3, *sse_ior<mode>3,
    	*sse_nand<mode>3, *sse_xor<mode>3): New.
    
    gcc/testsuite:
    2007-08-23  Paolo Bonzini  <bonzini@gnu.org>
    
    	* gcc.target/i386/xorps-sse.c: New.
    	* gcc.target/i386/xorps-sse2.c: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@127735 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/xorps-sse2.c
@@ -0,0 +1,15 @@
+/* Test that we generate xorps when the result is used in FP math.  */
+/* { dg-do compile } */
+/* { dg-options "-O -msse2 -mno-sse3" } */
+/* { dg-final { scan-assembler "xorps\[ \t\]" { xfail *-*-* } } } */
+/* { dg-final { scan-assembler-not "pxor" { xfail *-*-* } } } */
+
+#define vector __attribute__ ((vector_size (16)))
+
+vector float i(vector float f, vector float h)
+{
+  vector int g = { 0x80000000, 0, 0x80000000, 0 };
+  vector int f_int = (vector int) f;
+  return ((vector float) (f_int ^ g)) + h;
+}
+
