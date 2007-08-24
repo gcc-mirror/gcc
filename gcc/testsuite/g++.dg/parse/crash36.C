affb3cb2002b1e238f0ae6188742c543934ea270
Jakub Jelinek <jakub@redhat.com>
2007-08-24 08:54:49 +0200

    re PR c++/32567 (ICE on invalid use of parameter pack)
    	PR c++/32567
    	* typeck.c (build_unary_op) <case PREINCREMENT_EXPR>: Return
    	error_mark_node right away if build_expr_type_conversion
    	returned it.
    
    	* g++.dg/parse/crash36.C: New test.
    
    From-SVN: r127760

---
+++ gcc/testsuite/g++.dg/parse/crash36.C
@@ -0,0 +1,11 @@
+// PR c++/32567
+// { dg-do compile }
+// { dg-options "-std=c++98" }
+
+template <typename... T> struct A	// { dg-error "does not include variadic templates" }
+{
+  static T &t;				// { dg-error "not expanded with|T" }
+  static const int i = sizeof (++t);	// { dg-error "invalid use of template type parameter" }
+};
+
+int x[A <int>::i];	// { dg-error "is not an integral constant-expression" }
