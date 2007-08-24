86251bd92dc389b727d6dd84882019ddc4dc8cd0
jiez <jiez@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-08-24 17:22:51 +0000

    * config/bfin/bfin.h (TARGET_CPU_CPP_BUILTINS): Define 	__NO_BUILTIN if -fno-builtin.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@127782 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/bfin/bfin.h
+++ gcc/config/bfin/bfin.h
@@ -76,6 +76,8 @@ extern int target_flags;
 	builtin_define ("__BFIN_FDPIC__");	\
       if (TARGET_ID_SHARED_LIBRARY)		\
 	builtin_define ("__ID_SHARED_LIB__");	\
+      if (flag_no_builtin)			\
+	builtin_define ("__NO_BUILTIN");	\
     }                                           \
   while (0)
 #endif
