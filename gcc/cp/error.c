77379503c5a90de7dfddffdb43c6a77fb7f66648
Jakub Jelinek <jakub@redhat.com>
2007-08-24 19:12:41 +0200

    re PR c++/31941 ("confused by earlier errors" message without earlier error message)
    	PR c++/31941
    	* error.c (resolve_virtual_fun_from_obj_type_ref): Handle
    	TARGET_VTABLE_USES_DESCRIPTORS targets properly.
    
    	* g++.dg/parse/crash37.C: New test.
    
    From-SVN: r127778

--- gcc/cp/error.c
+++ gcc/cp/error.c
@@ -1305,10 +1305,14 @@ static tree
 resolve_virtual_fun_from_obj_type_ref (tree ref)
 {
   tree obj_type = TREE_TYPE (OBJ_TYPE_REF_OBJECT (ref));
-  int index = tree_low_cst (OBJ_TYPE_REF_TOKEN (ref), 1);
+  HOST_WIDE_INT index = tree_low_cst (OBJ_TYPE_REF_TOKEN (ref), 1);
   tree fun = BINFO_VIRTUALS (TYPE_BINFO (TREE_TYPE (obj_type)));
-    while (index--)
+  while (index)
+    {
       fun = TREE_CHAIN (fun);
+      index -= (TARGET_VTABLE_USES_DESCRIPTORS
+		? TARGET_VTABLE_USES_DESCRIPTORS : 1);
+    }
 
   return BV_FN (fun);
 }
