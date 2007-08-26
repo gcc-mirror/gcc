95b6682397a1e993c33a375881aaffd998495d68
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-08-26 18:11:42 +0000

    2007-08-26  Tobias Burnus  <burnus@net-b.de>
    	PR fortran/32980
    	* intrinsic.h (gfc_simplify_gamma,gfc_simplify_lgamma,
    	gfc_resolve_gamma,gfc_resolve_lgamma): New function declations.
    	* mathbuiltins.def: Define GAMMA and LGAMMA.
    	* intrinsic.c (add_functions): Add GAMMA, DGAMMA, LGAMMA, ALGAMA
    	and DLGAMA.
    	* simplify.c (gfc_simplify_gamma,gfc_simplify_lgamma): New functions.
    	* iresolve.c (gfc_resolve_gamma,gfc_resolve_lgamma): New functions.
    	* intrinsic.texi: Add documentation for GAMMA and LGAMMA.
    
    2007-08-26  Tobias Burnus  <burnus@net-b.de>
    
    	PR fortran/32980
    	* gfortran.dg/gamma_1.f90: New.
    	* gfortran.dg/gamma_2.f90: New.
    	* gfortran.dg/gamma_3.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@127809 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/gamma_2.f90
@@ -0,0 +1,36 @@
+! { dg-do compile }
+! { dg-options "-std=f2003 -Wall" }
+!
+! Test the vendor intrinsic (d)gamma, lgamma and algama/dlgama
+! gamma is also part of the Fortran 2008 draft; lgamma is called
+! log_gamma in the Fortran 2008 draft.
+!
+! PR fortran/32980
+!
+subroutine foo()
+intrinsic :: gamma
+intrinsic :: dgamma
+intrinsic :: lgamma
+intrinsic :: algama
+intrinsic :: dlgama
+
+integer, parameter :: sp = kind(1.0)
+integer, parameter :: dp = kind(1.0d0)
+integer, parameter :: qp = selected_real_kind(p=15,r=900)
+
+real(sp) :: rsp = 1.0_sp
+real(dp) :: rdp = 1.0_dp
+real(dp) :: rqp = 1.0_qp
+
+rsp = gamma(rsp)  ! FIXME:  "is not included in the selected standard"
+rdp = gamma(rdp)  ! FIXME:  "is not included in the selected standard"
+rqp = gamma(rqp)  ! FIXME:  "is not included in the selected standard"
+rdp = dgamma(rdp) ! { dg-error "is not included in the selected standard" }
+
+rsp = lgamma(rsp) ! FIXME:  "is not included in the selected standard"
+rdp = lgamma(rdp) ! FIXME:  "is not included in the selected standard"
+rqp = lgamma(rqp) ! FIXME:  "is not included in the selected standard"
+rsp = algama(rsp) ! { dg-error "is not included in the selected standard" }
+rdp = dlgama(rdp) ! { dg-error "is not included in the selected standard" }
+end subroutine foo
+end
