c5b9117ecabde0a935e826afff0728b04c1d47c9
Tobias Burnus <burnus@net-b.de>
2007-08-26 19:59:34 +0200

    re PR fortran/33188 (Fortran 2003: Type declarations without componends)
    2007-08-26  Tobias Burnus  <burnus@net-b.de>
    
    	PR fortran/33188
    	* parse.c (parse_derived): Support empty derived type
    	definitions for Fortran 2003.
    
    2007-08-26  Tobias Burnus  <burnus@net-b.de>
    
    	PR fortran/33188
    	* gfortran.dg/used_types_18.f90: New.
    
    From-SVN: r127808

---
+++ gcc/testsuite/gfortran.dg/used_types_18.f90
@@ -0,0 +1,12 @@
+! { dg do-compile }
+! { dg-options "-std=f2003" }
+!
+! Fortran 2003 allowes TYPE without components
+! The error message for -std=f95 is tested in
+! gfortran.dg/access_spec_2.f90
+!
+! PR fortran/33188
+!
+type t
+end type
+end
