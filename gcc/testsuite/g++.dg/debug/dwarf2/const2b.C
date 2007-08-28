a7d0852d3f7d3e0a596f53efb2d75cf522ffb43f
Jakub Jelinek <jakub@redhat.com>
2007-08-28 11:44:57 +0200

    re PR debug/32914 (ICE in rtl_for_decl_init with -g option)
    	PR debug/32914
    	* dwarf2out.c (rtl_for_decl_init): If vector decl has CONSTRUCTOR
    	initializer, use build_vector_from_ctor if possible to create
    	VECTOR_CST out of it.  If vector initializer is not VECTOR_CST
    	even after this, return NULL.
    
    	* d++.dg/debug/const3.C: New test.
    	* d++.dg/debug/const4.C: New test.
    	* d++.dg/debug/dwarf2/const1.C: New test.
    	* d++.dg/debug/dwarf2/const2.C: New test.
    	* d++.dg/debug/dwarf2/const2b.C: New test.
    
    From-SVN: r127854

---
+++ gcc/testsuite/g++.dg/debug/dwarf2/const2b.C
@@ -0,0 +1,6 @@
+/* { dg-do compile { target i386*-*-* } } */
+/* { dg-options "-O -gdwarf-2 -dA -msse" } */
+/* { dg-final { scan-assembler "DW_AT_const_value" } } */
+
+typedef float FloatVect __attribute__((__vector_size__(16)));
+const FloatVect Foo = { 250000000.0, 0.0, 0.0, 0.0 };
