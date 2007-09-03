9b31b8f41d71084c959906d96097069d23cfb333
hp <hp@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-09-03 13:41:43 +0000

    * gcc.target/cris/builtin_bswap_v3.c: New testcase. 	* gcc.target/cris/builtin_bswap_v8.c: New testcase.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@128044 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/cris/builtin_bswap_v3.c
@@ -0,0 +1,12 @@
+/* Check that we don't use the swap insn for bswap by checking assembler 
+   output.  The swap instruction was added in v8.  */
+/* { dg-do compile } */
+/* { dg-skip-if "" { "cris-*-elf" } { "-march*" } { "" } } */
+/* { dg-options "-O2 -march=v3" } */
+/* { dg-final { scan-assembler-not "\[ \t\]swapwb\[ \t\]" } } */
+
+int
+f (int a)
+{
+	return __builtin_bswap32(a);
+}
---
+++ gcc/testsuite/gcc.target/cris/builtin_bswap_v8.c
@@ -0,0 +1,12 @@
+/* Check that we use the swap insn for bswap by checking assembler
+   output.  The swap instruction was added in v8.  */
+/* { dg-do compile } */
+/* { dg-skip-if "" { "cris-*-elf" } { "-march*" } { "" } } */
+/* { dg-options "-O2 -march=v8" } */
+/* { dg-final { scan-assembler "\[ \t\]swapwb\[ \t\]" } } */
+
+int
+f (int a)
+{
+	return __builtin_bswap32(a);
+}
