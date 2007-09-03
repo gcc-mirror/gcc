53c194ab7edcc9a1a2511b259f411f157391b4c6
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-09-03 15:19:17 +0000

    2007-09-03  H.J. Lu  <hongjiu.lu@intel.com>
    	* gcc.dg/vect/no-tree-scev-cprop-vect-iv-1.c: Restored.  Moved
    	to ...
    	* gcc.dg/vect/no-scevccp-vect-iv-1.c: New test.
    	* gcc.dg/vect/no-tree-scev-cprop-vect-iv-2.c: Restored.  Moved
    	to ...
    	* gcc.dg/vect/no-scevccp-vect-iv-2.c: New test.
    	* gcc.dg/vect/no-tree-scev-cprop-vect-iv-3.c: Restored.  Moved
    	to ...
    	* gcc.dg/vect/no-scevccp-vect-iv-3.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@128045 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/no-scevccp-vect-iv-3.c
@@ -0,0 +1,27 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target vect_int } */
+
+#include <stdarg.h>
+#include "tree-vect.h"
+
+#define N 26
+ 
+unsigned int main1 ()
+{  
+  unsigned short i;
+  unsigned int intsum = 0;
+
+  /* vectorization of reduction with induction, and widenning sum: 
+     sum shorts into int. 
+     Need -fno-tree-scev-cprop or else the loop is eliminated.  */
+  for (i = 0; i < N; i++)
+    {
+      intsum += i;
+    } 
+
+  return intsum;
+}
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { target vect_widen_sum_hi_to_si } } } */
+/* { dg-final { scan-tree-dump-times "vect_recog_widen_sum_pattern: detected" 1 "vect" { target vect_widen_sum_hi_to_si } } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
