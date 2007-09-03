f568cd7be88adee8585a0ae4f741a9937210a8a2
Jesper Nilsson <jesper.nilsson@axis.com>
2007-09-03 00:45:11 +0000

    builtin_clz_v0.c: New testcase.
    	* gcc.target/cris/builtin_clz_v0.c: New testcase.
    	* gcc.target/cris/builtin_clz_v3.c: New testcase.
    
    From-SVN: r128033

---
+++ gcc/testsuite/gcc.target/cris/builtin_clz_v0.c
@@ -0,0 +1,12 @@
+/* Check that we don't use the lz insn for clz by checking assembler output.
+   The lz insn was implemented in CRIS v3 (ETRAX 4).  */
+/* { dg-do compile } */
+/* { dg-skip-if "" { "cris-*-elf" } { "-march*" } { "" } } */
+/* { dg-options "-O2 -march=v0" } */
+/* { dg-final { scan-assembler-not "\[ \t\]lz\[ \t\]" } } */
+
+int
+f (int a)
+{
+	return __builtin_clz(a);
+}
