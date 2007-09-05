c22e07a3ff98bd2a8d4947118bde62397c52c4fb
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-09-05 01:36:09 +0000

    2007-09-04  Andrew Pinski  <andrew_pinski@playstation.sony.com>
            * config.gcc (powerpc*-*-*): Install
            spu2vmx.h, vec_types.h, and si2vmx.h headers.
            * config/rs6000/spu2vmx.h: New header.
            * config/rs6000/si2vmx.h: New header.
            * config/rs6000/vec_types.h: New header.
    2007-09-04  Andrew Pinski  <andrew_pinski@playstation.sony.com>
    
            * g++.dg/other/spu2vmx-1.C: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@128118 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/other/spu2vmx-1.C
@@ -0,0 +1,15 @@
+/* { dg-do compile { target powerpc*-*-* } } */
+/* { dg-require-effective-target powerpc_altivec_ok } */
+/* { dg-options "-maltivec" } */
+
+#include <altivec.h>
+#include <spu2vmx.h>
+
+vec_uint4 f(vec_uint4 a, vec_uint4 b)
+{
+  return spu_add(a, b);
+}
+vec_float4 f(vec_float4 a, vec_float4 b)
+{
+  return spu_add(a, b);
+}
