f9612ead203999c4cb0e461ac31d5b6dbd1cd65a
jiez <jiez@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-09-06 00:09:05 +0000

    * config.gcc (tm_file): Add linux.h for bfin*-uclinux*. 	(tm_defines): Define UCLIBC_DEFAULT to 1. 	(extra_options): Add linux.opt. 	* config/bfin/linux.h (CPLUSPLUS_CPP_SPEC): Don't define. 	(CRT_CALL_STATIC_FUNCTION): Likewise. 	(NO_IMPLICIT_EXTERN_C): Likewise. 	(TARGET_OS_CPP_BUILTINS): Define as LINUX_TARGET_OS_CPP_BUILTINS. 	* config/bfin/elf.h (OBJECT_FORMAT_ELF): Don't define. 	* config/bfin/uclinux.h (CPLUSPLUS_CPP_SPEC): Don't define. 	(ENDFILE_SPEC): Don't define. 	(LIB_SPEC): Likewise. 	(CRT_CALL_STATIC_FUNCTION): Likewise. 	(NO_IMPLICIT_EXTERN_C): Likewise. 	(LINUX_TARGET_OS_CPP_BUILTINS): Likewise. 	(TARGET_OS_CPP_BUILTINS): Define as LINUX_TARGET_OS_CPP_BUILTINS.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@128163 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/bfin/uclinux.h
+++ gcc/config/bfin/uclinux.h
@@ -1,48 +1,8 @@
-/* The GNU C++ standard library requires that these macros be defined.  */
-#undef CPLUSPLUS_CPP_SPEC
-#define CPLUSPLUS_CPP_SPEC "-D_GNU_SOURCE %(cpp)"
-
 #undef  STARTFILE_SPEC
 #define STARTFILE_SPEC \
   "%{!shared: crt1%O%s} crti%O%s crtbegin%O%s crtlibid%O%s"
 
-#undef  ENDFILE_SPEC
-#define ENDFILE_SPEC \
-  "crtend%O%s crtn%O%s"
-
-#undef  LIB_SPEC
-#define LIB_SPEC "%{pthread:-lpthread} -lc"
-
-#ifdef __BFIN_FDPIC__
-#define CRT_CALL_STATIC_FUNCTION(SECTION_OP, FUNC)	\
-asm (SECTION_OP); \
-asm ("P3 = [SP + 20];\n\tcall " USER_LABEL_PREFIX #FUNC ";"); \
-asm (TEXT_SECTION_ASM_OP);
-#endif
-
-#define NO_IMPLICIT_EXTERN_C
-
-#define LINUX_TARGET_OS_CPP_BUILTINS()				\
-    do {							\
-	builtin_define ("__gnu_linux__");			\
-	builtin_define_std ("linux");				\
-	builtin_define_std ("unix");				\
-	builtin_assert ("system=linux");			\
-	builtin_assert ("system=unix");				\
-	builtin_assert ("system=posix");			\
-    } while (0)
-
-#define TARGET_OS_CPP_BUILTINS()		\
-  do						\
-    {						\
-	LINUX_TARGET_OS_CPP_BUILTINS();		\
-	if (flag_pic)				\
-	  {					\
-	    builtin_define ("__PIC__");		\
-	    builtin_define ("__pic__");		\
-	  }					\
-    }						\
-  while (0)
+#define TARGET_OS_CPP_BUILTINS() LINUX_TARGET_OS_CPP_BUILTINS()
 
 #define MD_UNWIND_SUPPORT "config/bfin/linux-unwind.h"
 
