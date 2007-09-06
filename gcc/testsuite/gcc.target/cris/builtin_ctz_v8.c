221818503d867c0f7cd84ed989a60fc830e40707
Jesper Nilsson <jesper.nilsson@axis.com>
2007-09-06 00:17:15 +0000

    builtin_ctz_v3.c: New testcase.
    	* gcc.target/cris/builtin_ctz_v3.c: New testcase.
    	* gcc.target/cris/builtin_ctz_v8.c: New testcase.
    
    From-SVN: r128168

---
+++ gcc/testsuite/gcc.target/cris/builtin_ctz_v8.c
@@ -0,0 +1,12 @@
+/* Check that we use the swap insn for ctz by checking assembler output.
+   The swap instruction was implemented in v8.  */
+/* { dg-do compile } */
+/* { dg-skip-if "" { "cris-*-elf" } { "-march*" } { "" } } */
+/* { dg-options "-O2 -march=v8" } */
+/* { dg-final { scan-assembler "\[ \t\]swapwbr\[ \t\]" } } */
+
+int
+f (int a)
+{
+	return __builtin_ctz(a);
+}
