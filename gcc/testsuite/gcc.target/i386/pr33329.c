e9ddec4ef1c716a7891b45d04734cf92f3563cc5
Uros Bizjak <ubizjak@gmail.com>
2007-09-08 13:33:08 +0200

    re PR target/33329 (ICE in expand_simple_binop, at optabs.c:1294)
            PR target/33329
            PR target/26449
            * config/i386/sse.md (mulv4si3): Do not expand sse2 sequence.
            (*sse2_mulv4si3): New define_insn_and_split pattern. Split insn in
            split1 pass.
            (mulv16qi3): Implement as define_insn_and_split pattern instead of
            define_expand, to split insn in split1 pass.
            (mulv2di3): Ditto.
    
    testsuite/ChangeLog:
    
            PR target/33329
            PR target/26449
            * gcc.target/i386/pr33329.c: New file.
    
    From-SVN: r128269

---
+++ gcc/testsuite/gcc.target/i386/pr33329.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -ftree-vectorize -msse2" } */
+
+extern void g (int *);
+
+void f (void)
+{
+  int tabs[8], tabcount;
+
+  for (tabcount = 1; tabcount <= 8; tabcount += 7)
+    {
+      int i;
+      for (i = 0; i < 8; i++)
+	tabs[i] = i * 2;
+      g (tabs);
+    }
+}
+
