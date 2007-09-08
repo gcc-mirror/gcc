13f676a55e53243de2815f58959cfa4a68f9c190
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-09-08 08:21:05 +0000

    Add support file for controlled2.adb
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@128264 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gnat.dg/controlled1.ads
@@ -0,0 +1,13 @@
+
+with Ada.Finalization; use Ada.Finalization;
+package controlled1 is
+   type Test is new Controlled with null record;
+   procedure Add_Test (T : access Test'Class);
+   
+   type Test_Case1 is new Test with null record;
+   type Test_Suite is new Test with null record;
+   
+   type Test_Case is new Test_Case1 with record
+      Link_Under_Test : Natural;
+   end record;
+end;
