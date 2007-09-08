1b0af592d3709c7d2ffe46c384b42c7dbdda1072
rakdver <rakdver@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-09-08 13:18:49 +0000

    PR tree-optimization/32283 	* tree-ssa-loop-ivopts.c (may_eliminate_iv): Use 	estimated_loop_iterations. 	(determine_use_iv_cost_condition): Decrease cost of expressions 	used in iv elimination.
    	* gcc.dg/tree-ssa/loop-31.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@128272 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/loop-31.c
@@ -0,0 +1,19 @@
+/* PR 32283 */
+
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-optimized" } */
+
+short a[(2048)];
+short foo (int len, int v)
+{
+  int i;
+  for (i = 0; i < len; i++) {
+      a[i] = v;
+  }
+  return a[0];
+}
+
+/* When we do not have addressing mode including multiplication,
+   the memory access should be strength-reduced.  */
+/* { dg-final { scan-tree-dump-times " \\+ 2" 1 "optimized" { target arm-*-* ia64-*-* } } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
