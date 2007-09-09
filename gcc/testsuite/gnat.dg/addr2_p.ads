7a8faae861e39ac771e5387288b5315d45b78070
Eric Botcazou <ebotcazou@adacore.com>
2007-09-09 11:22:30 +0000

    addr2_p.ads: Bump size of array to 9.
            * gnat.dg/addr2_p.ads: Bump size of array to 9.
    
    From-SVN: r128295

--- gcc/testsuite/gnat.dg/addr2_p.ads
+++ gcc/testsuite/gnat.dg/addr2_p.ads
@@ -1,10 +1,10 @@
 
 package addr2_p is
    
-   type Block is array (1 .. 4) of Integer;
+   type Block is array (1 .. 9) of Integer;
    
    procedure Process (Blk : Block);
    
-   B1 : constant Block := Block'((1,2,3,4));
-   B2 : constant Block := (1,2,3,4);
+   B1 : constant Block := Block'((1,2,3,4,5, others => 0));
+   B2 : constant Block := (1,2,3,4,5, others => 0);
 end;
