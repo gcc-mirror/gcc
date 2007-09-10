5bbb5716aa8e1f9512bfe1f98a35ac7ef508bcbb
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-09-10 09:10:00 +0000

    PR target/33369 	* gcc/config/i386/sse.md (ashr<mode>3): Change op2 mode to SImode. 	Use 'N' operand constraint for op2. 	(lshr<mode>3): Ditto. 	(ashl<mode>3): Ditto. 	(vec_shl_<mode>): Use const_0_to_255_mul_8_operand predicate for op2. 	(vec_shr_<mode>): Use const_0_to_255_mul_8_operand predicate for op2.
    	* gcc/config/i386/i386.c (ix86_expand_builtin) [IX86_BUILTIN_PSLL?128,
    	IX86_BUILTIN_PSRA*?128, IX86_BUILTIN_PSRL?128]: Convert op1 to SImode.
    
    testsuite/ChangeLog:
    	
    	PR target/33369
    	* gcc.dg/vect/pr33369.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@128328 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/pr33369.c
@@ -0,0 +1,21 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target vect_shift } */
+
+typedef struct tagPOINT
+{
+  int x;
+  int y;
+} POINT;
+
+void
+f (POINT * ptBuf)
+{
+  int i;
+  for (i = 0; i < 4; i++)
+    {
+      ptBuf[i].x = ((ptBuf[i].x) << 4);
+      ptBuf[i].y = ((ptBuf[i].y) << 4);
+    }
+}
+
+/* { dg-final { cleanup-tree-dump "vect" } } */
