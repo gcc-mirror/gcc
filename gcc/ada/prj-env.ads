10583694809011467d2b4578a5e263608d7e4dc3
Vincent Celier <celier@adacore.com>
2007-09-10 14:48:44 +0200

    prj-env.ads, [...] (Create_Mapping_File (Language)): Remove parameter Runtime_Project.
    2007-09-10  Vincent Celier  <celier@adacore.com>
    
    	* prj-env.ads, prj-env.adb (Create_Mapping_File (Language)): Remove
    	parameter Runtime_Project.
    
    From-SVN: r128336

--- gcc/ada/prj-env.ads
+++ gcc/ada/prj-env.ads
@@ -10,14 +10,13 @@
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
--- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
 -- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
 -- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
 -- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
 -- for  more details.  You should have  received  a copy of the GNU General --
--- Public License  distributed with GNAT;  see file COPYING.  If not, write --
--- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
--- Boston, MA 02110-1301, USA.                                              --
+-- Public License  distributed with GNAT; see file COPYING3.  If not, go to --
+-- http://www.gnu.org/licenses for a complete copy of the license.          --
 --                                                                          --
 -- GNAT was originally developed  by the GNAT team at  New York University. --
 -- Extensive contributions were provided by Ada Core Technologies Inc.      --
@@ -47,7 +46,6 @@ package Prj.Env is
    procedure Create_Mapping_File
      (Project  : Project_Id;
       Language : Name_Id;
-      Runtime  : Project_Id;
       In_Tree  : Project_Tree_Ref;
       Name     : out Path_Name_Type);
    --  Create a temporary mapping file for project Project. For each source or
