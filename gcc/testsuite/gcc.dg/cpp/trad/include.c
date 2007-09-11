95c50ad31b1856aa921ef3272fee684878dfea20
hp <hp@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-09-11 21:15:24 +0000

    * gcc.dg/cpp/trad/include.c: Don't run for newlib targets.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@128394 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cpp/trad/include.c
+++ gcc/testsuite/gcc.dg/cpp/trad/include.c
@@ -3,8 +3,9 @@
 /* Test that macros are not expanded in the <> quotes of #inlcude.  */
 
 /* vxWorksCommon.h uses the "#" operator to construct the name of an
-   include file, thus making the file incompatible with -traditional-cpp.  */
-/* { dg-do preprocess { target { ! vxworks_kernel } } } */
+   include file, thus making the file incompatible with -traditional-cpp.
+   Newlib uses ## when including stdlib.h as of 2007-09-07.  */
+/* { dg-do preprocess { target { { ! vxworks_kernel } && { ! newlib } } } } */
 
 #define __STDC__ 1		/* Stop complaints about non-ISO compilers.  */
 #define stdlib 1
