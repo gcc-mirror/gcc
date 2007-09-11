7e4aeb3297fc305dc887732c5d03360a7677fba5
Jason Merrill <jason@redhat.com>
2007-09-11 11:07:59 -0400

    re PR middle-end/27945 (Packed struct of variable length has wrong size)
            PR middle-end/27945
            * stor-layout.c (layout_decl): Do pack variable size fields.
    
    From-SVN: r128380

---
+++ gcc/testsuite/gcc.dg/packed-vla.c
@@ -0,0 +1,30 @@
+/* { dg-do run } */
+/* { dg-options "" } */
+
+extern int printf (const char *, ...);
+extern void abort ();
+
+int func(int levels) 
+{
+  struct bar {
+    unsigned char	a;
+    int			b[levels];
+  } __attribute__ ((__packed__)) bar;
+
+  struct foo {
+    unsigned char	a;
+    int			b[4];
+  } __attribute__ ((__packed__)) foo;
+
+  printf("foo %d\n", sizeof(foo));
+  printf("bar %d\n", sizeof(bar));
+
+  if (sizeof (foo) != sizeof (bar))
+    abort ();
+}
+
+int main()
+{
+  func(4);
+  return 0;
+}
