e31fc6a53150ed4b22314913c5768dae0895263c
Jonathan Wakely <jwakely.gcc@gmail.com>
2007-09-14 20:17:29 +0000

    boost_shared_ptr.h: (__weak_ptr::lock()): Add missing template argument.
    2007-09-14  Jonathan Wakely  <jwakely.gcc@gmail.com>
    
    	* include/tr1_impl/boost_shared_ptr.h: (__weak_ptr::lock()): Add
    	missing template argument.
    	* testsuite/tr1/2_general_utilities/shared_ptr/requirements/
    	explicit_instantiation/2.cc: New.
    	* testsuite/tr1/2_general_utilities/weak_ptr/requirements/
    	explicit_instantiation/2.cc: New.
    
    From-SVN: r128503

--- libstdc++-v3/include/tr1_impl/boost_shared_ptr.h
+++ libstdc++-v3/include/tr1_impl/boost_shared_ptr.h
@@ -856,7 +856,7 @@ _GLIBCXX_BEGIN_NAMESPACE_TR1
 	    // Q: How can we get here?
 	    // A: Another thread may have invalidated r after the
 	    //    use_count test above.
-	    return __shared_ptr<element_type>();
+	    return __shared_ptr<element_type, _Lp>();
 	  }
 	
 #else
