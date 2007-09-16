2d34ae57a5f7b8d02a1b98565a996f2eb8ef3829
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-09-16 09:45:36 +0000

    gcc/ 	* config/mips/mips.md (SHORT): Fix long line. 	(SUBDI): New mode iterator.  Extend the shift-and-truncate insns 	to QImode and HImoe.
    gcc/testsuite/
    	* gcc.target/mips/truncate-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@128525 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/mips/truncate-1.c
@@ -0,0 +1,20 @@
+/* { dg-mips-options "-O -mgp64" } */
+
+#define TEST(ID, TYPE, SHIFT)				\
+  int __attribute__((nomips16))				\
+  f##ID (unsigned long long y)				\
+  {							\
+    return (TYPE) ((TYPE) (y >> SHIFT) + 1);		\
+  }
+
+TEST (1, int, 32)
+TEST (2, short, 32)
+TEST (3, char, 32)
+TEST (4, int, 33)
+TEST (5, short, 33)
+TEST (6, char, 33)
+TEST (7, int, 61)
+TEST (8, short, 61)
+TEST (9, char, 61)
+
+/* { dg-final { scan-assembler-not "\tsll\t\[^\n\]*,0" } } */
