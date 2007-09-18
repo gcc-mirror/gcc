d1bc0659b7e57c1422888f0d3e48dc9fcae66015
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-09-18 21:25:40 +0000

    PR fortran/31119
    	* trans-array.c (gfc_conv_ss_startstride): Only perform bounds
    	checking for optional args when they are present.
    
    	* gfortran.dg/bounds_check_9.f90: New test.
    	* gfortran.dg/bounds_check_fail_2.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@128587 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/bounds_check_fail_2.f90
@@ -0,0 +1,39 @@
+! { dg-do run }
+! { dg-options "-fbounds-check" }
+! { dg-shouldfail "foo" }
+!
+! PR 31119
+module sub_mod
+contains
+  elemental subroutine set_optional(i,idef,iopt)
+    integer, intent(out)          :: i
+    integer, intent(in)           :: idef
+    integer, intent(in), optional :: iopt
+    if (present(iopt)) then
+      i = iopt
+    else
+      i = idef
+    end if
+  end subroutine set_optional
+
+  subroutine sub(ivec)
+    integer          , intent(in), optional :: ivec(:)
+    integer                                 :: ivec_(2)
+    call set_optional(ivec_,(/1,2/))
+    if (any (ivec_ /= (/1,2/))) call abort
+    call set_optional(ivec_,(/1,2/),ivec)
+    if (present (ivec)) then
+      if (any (ivec_ /= ivec)) call abort
+    else
+      if (any (ivec_ /= (/1,2/))) call abort
+    end if
+  end subroutine sub
+end module sub_mod
+
+program main
+  use sub_mod, only: sub
+  call sub()
+  call sub((/4,5/))
+  call sub((/4/))
+end program main
+! { dg-output "Fortran runtime error: Array bound mismatch" }
