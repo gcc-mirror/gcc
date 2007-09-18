e74392f0a8f7e617180f5bc034ff4c12b5fa8405
Paolo Carlini <pcarlini@suse.de>
2007-09-18 14:35:42 +0000

    re PR c++/33464 (Broken diagnostic: 'trait_expr' not supported by dump_expr)
    /cp
    2007-09-18  Paolo Carlini  <pcarlini@suse.de>
    
    	PR c++/33464
    	* cxx-pretty-print.c (pp_cxx_trait_expression): Add.
    	(pp_cxx_primary_expression): Use it.
    	* cxx-pretty-print.h (pp_cxx_trait_expression): Declare.
    	* error.c (dump_expr): Use it.
    
    /testsuite
    2007-09-18  Paolo Carlini  <pcarlini@suse.de>
    
    	PR c++/33464
    	* g++.dg/ext/is_class_error.C: Rename to is_class_error1.C.
    	* g++.dg/ext/is_class_error2.C: New.
    
    From-SVN: r128578

---
+++ gcc/testsuite/g++.dg/ext/is_class_error2.C
@@ -0,0 +1,22 @@
+// PR c++/33464
+
+template<int> void foo()
+{
+  __has_nothrow_assign(int)(); // { dg-error "'__has_nothrow_assign\\(int\\)' cannot be used" }
+  __has_trivial_assign(int)(); // { dg-error "'__has_trivial_assign\\(int\\)' cannot be used" }
+  __has_nothrow_constructor(int)(); // { dg-error "'__has_nothrow_constructor\\(int\\)' cannot be used" }
+  __has_trivial_constructor(int)(); // { dg-error "'__has_trivial_constructor\\(int\\)' cannot be used" } 
+  __has_nothrow_copy(int)(); // { dg-error "'__has_nothrow_copy\\(int\\)' cannot be used" }
+  __has_trivial_copy(int)(); // { dg-error "'__has_trivial_copy\\(int\\)' cannot be used" }
+  __has_trivial_destructor(int)(); // { dg-error "'__has_trivial_destructor\\(int\\)' cannot be used" }
+  __has_virtual_destructor(int)(); // { dg-error "'__has_virtual_destructor\\(int\\)' cannot be used" }
+  __is_abstract(int)(); // { dg-error "'__is_abstract\\(int\\)' cannot be used" }
+  __is_base_of(int, float)(); // { dg-error "'__is_base_of\\(int, float\\)' cannot be used" }
+  __is_class(int)(); // { dg-error "'__is_class\\(int\\)' cannot be used" }
+  __is_convertible_to(int, float)(); // { dg-error "unimplemented" }
+  __is_empty(int)(); // { dg-error "'__is_empty\\(int\\)' cannot be used" }
+  __is_enum(int)(); // { dg-error "'__is_enum\\(int\\)' cannot be used" }
+  __is_pod(int)(); // { dg-error "'__is_pod\\(int\\)' cannot be used" }
+  __is_polymorphic(int)(); // { dg-error "'__is_polymorphic\\(int\\)' cannot be used" }
+  __is_union(int)(); // { dg-error "'__is_union\\(int\\)' cannot be used" }
+}
