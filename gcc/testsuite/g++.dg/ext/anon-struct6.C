24f58e749753a97db167a73a59dfe1d32f103686
Paolo Carlini <pcarlini@suse.de>
2007-09-20 23:05:38 +0000

    re PR c++/33460 (ICE with static member in anonymous union)
    cp/
    2007-09-20  Paolo Carlini  <pcarlini@suse.de>
    
    	PR c++/33460
    	* semantics.c (finish_id_expression): Use consistently
    	context_for_name_lookup.
    	* decl.c (fixup_anonymous_aggr): Fix error message for
    	anonymous struct (vs union).
    
    testsuite/
    2007-09-20  Paolo Carlini  <pcarlini@suse.de>
    
    	PR c++/33460
    	* g++.dg/ext/anon-struct6.C: New.
    
    From-SVN: r128637

---
+++ gcc/testsuite/g++.dg/ext/anon-struct6.C
@@ -0,0 +1,10 @@
+// PR c++/33460
+
+struct A
+{
+  struct
+  {  // { dg-error "anonymous struct cannot have function members" }
+    struct { static int i; }; // { dg-error "prohibits anonymous structs|non-static data members" } 
+    void foo() { i; }
+  }; // { dg-error "prohibits anonymous structs" }
+};
