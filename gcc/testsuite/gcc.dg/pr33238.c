786025ea79bffb3746e1964e561dae17f37dc8af
Jakub Jelinek <jakub@redhat.com>
2007-09-20 23:19:36 +0200

    re PR c/33238 (ICE on statement expression using variable-sized structure in tree_low_cst, at tree.c:4502)
    	PR c/33238
    	PR c/27301
    	* gimplify.c (gimplify_vla_decl): New function.
    	(gimplify_decl_expr): Move VLA decl handling to gimplify_vla_decl.
    	Call it.
    	(gimplify_target_expr): Handle variable length TARGET_EXPRs.
    
    	* gcc.c-torture/execute/20070919-1.c: New test.
    	* gcc.dg/pr33238.c: New test.
    	* gcc.dg/pr27301.c: New test.
    
    From-SVN: r128629

---
+++ gcc/testsuite/gcc.dg/pr33238.c
@@ -0,0 +1,12 @@
+/* PR c/33238 */
+/* { dg-do compile } */
+/* { dg-options "-std=gnu89" } */
+
+void
+reverse (void *x, int y, int z)
+{
+  struct { char w[z]; } *p = x, a;
+  int i, j;
+  for (i = y - 1, j = 0; j < y / 2; i--, j++)
+    ({ a = p[i]; p[i] = p[j]; p[j] = a; });
+}
