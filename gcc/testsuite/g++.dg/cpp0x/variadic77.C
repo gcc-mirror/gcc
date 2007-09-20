4745e4eb75aed535cc05f3393dd1209ffd8bac09
Jakub Jelinek <jakub@redhat.com>
2007-09-20 23:21:03 +0200

    re PR c++/33496 (ICE with sizeof for invalid argument pack)
    	PR c++/33496
    	* pt.c (tsubst_copy) <case SIZEOF_EXPR>: Handle error_mark_node
    	returned from tsubst_pack_expansion.
    	(tsubst_copy_and_build) <case SIZEOF_EXPR>: Likewise.
    	(tsubst_copy_and_build) <case CONSTRUCTOR>: Likewise.
    
    	* g++.dg/cpp0x/variadic76.C: New test.
    	* g++.dg/cpp0x/variadic77.C: New test.
    	* g++.dg/cpp0x/variadic78.C: New test.
    
    From-SVN: r128630

---
+++ gcc/testsuite/g++.dg/cpp0x/variadic77.C
@@ -0,0 +1,22 @@
+// PR c++/33496
+// { dg-do compile }
+// { dg-options "-std=gnu++0x" }
+
+template<int M, int N> struct pair
+{
+  int i, j;
+  pair () : i (M), j (N) {}
+};
+
+template<int... M> struct S
+{
+  template<int... N> static int foo ()
+  {
+    return sizeof... (pair<M, N>);	// { dg-error "mismatched argument pack lengths" }
+  }
+};
+
+int bar ()
+{
+  return S<0, 1, 2>::foo<0, 1> ();
+}
