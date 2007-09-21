9ff5922be2bb05efbed339b56d514bb85c564203
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-09-21 11:17:13 +0000

    PR fortran/33439 	* gfortran.dg/gomp/pr33439.f90: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@128649 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/gomp/pr33439.f90
@@ -0,0 +1,38 @@
+! PR fortran/33439
+! { dg-do compile }
+! { dg-options "-fopenmp" }
+
+subroutine pr33439_1
+  integer :: s, i
+  s = 4
+!$omp parallel default(none)	! { dg-error "enclosing parallel" }
+  call somethingelse
+!$omp do schedule(static, s)	! { dg-error "not specified in enclosing parallel" }
+  do i = 1, 8
+    call something
+  end do
+!$omp end do
+!$omp end parallel
+end subroutine pr33439_1
+
+subroutine pr33439_2
+  integer :: s, i
+  s = 4
+!$omp parallel default(none)	! { dg-error "enclosing parallel" }
+!$omp do schedule(static, s)	! { dg-error "not specified in enclosing parallel" }
+  do i = 1, 8
+    call something
+  end do
+!$omp end do
+!$omp end parallel
+end subroutine pr33439_2
+
+subroutine pr33439_3
+  integer :: s, i
+  s = 4
+!$omp parallel do default(none) schedule(static, s) ! { dg-error "enclosing parallel" }
+  do i = 1, 8
+    call something
+  end do
+!$omp end parallel do
+end subroutine pr33439_3
