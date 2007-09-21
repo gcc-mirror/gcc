a227604ce8dfd3335bb3e6c939aa9e1035abbe35
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-09-21 10:21:29 +0000

    2007-09-21  Tobias Burnus  <burnus@net-b.de>
            PR fortran/33037
            * simplify.c (gfc_simplify_transfer): Warn if source size
            is smaller than result size.
    
    2007-09-21  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/33037
            * gfortran.dg/transfer_check_1.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@128646 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/simplify.c
+++ gcc/fortran/simplify.c
@@ -4059,6 +4059,11 @@ gfc_simplify_transfer (gfc_expr *source, gfc_expr *mold, gfc_expr *size)
       result_size = result_elt_size;
     }
 
+  if (source_size < result_size)
+    gfc_warning("Intrinsic TRANSFER at %L has partly undefined result: "
+		"source size %ld < result size %ld", &source->where,
+		(long) source_size, (long) result_size);
+
   /* Allocate the buffer to store the binary version of the source.  */
   buffer_size = MAX (source_size, result_size);
   buffer = (unsigned char*)alloca (buffer_size);
---
+++ gcc/testsuite/gfortran.dg/transfer_check_1.f90
@@ -0,0 +1,8 @@
+! { dg-do compile }
+! PR fortran/33037
+!
+print *, transfer('x', 0, 20) ! { dg-warning "has partly undefined result" }
+print *, transfer(1_1, 0) ! { dg-warning "has partly undefined result" }
+print *, transfer([1_2,2_2], 0)
+print *, transfer([1_2,2_2], 0_8) ! { dg-warning "has partly undefined result" }
+end
