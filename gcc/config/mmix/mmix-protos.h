60e90d090baeb13169246bc81bdc81f41af17aaf
Hans-Peter Nilsson <hp@bitrange.com>
2007-09-25 02:18:37 +0000

    mmix.h (FUNCTION_INCOMING_ARG_REGNO_P): Don't define bogus target macro.
    	* config/mmix/mmix.h (FUNCTION_INCOMING_ARG_REGNO_P): Don't define
    	bogus target macro.
    	(INCOMING_REGNO, OUTGOING_REGNO): Define.
    	* config/mmix/mmix.c (mmix_opposite_regno): New function.
    	* config/mmix/mmix-protos.h (mmix_opposite_regno): Prototype.
    
    From-SVN: r128744

--- gcc/config/mmix/mmix-protos.h
+++ gcc/config/mmix/mmix-protos.h
@@ -49,6 +49,7 @@ extern void mmix_asm_output_align (FILE *, int);
 extern int mmix_shiftable_wyde_value (unsigned HOST_WIDEST_INT);
 extern void mmix_output_register_setting (FILE *, int, HOST_WIDEST_INT, int);
 extern void mmix_conditional_register_usage (void);
+extern int mmix_opposite_regno (int, int);
 extern int mmix_local_regno (int);
 extern int mmix_dbx_register_number (int);
 extern int mmix_use_simple_return (void);
