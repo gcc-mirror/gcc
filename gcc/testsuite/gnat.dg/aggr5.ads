daa5546e21d7cdd88b70af40875406dc7dad6864
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-09-26 10:41:08 +0000

    Update test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@128778 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gnat.dg/aggr5.ads
+++
@@ -1,7 +0,0 @@
-        
-package aggr5 is
-   type Event is limited interface;
-   type Event_Access is access all Event'Class;
-   type Q_Action_Event is limited interface and Event;
-   function Build (X : integer) return Event_Access;
-end aggr5;
