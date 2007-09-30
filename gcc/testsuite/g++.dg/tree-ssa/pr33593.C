a45db20b7574c6a50e5b2c3bca189b21973a7b10
Diego Novillo <dnovillo@google.com>
2007-09-30 12:00:36 -0400

    re PR tree-optimization/33593 (tree-outof-ssa moves sources of non-call exceptions past sequence points)
    	PR 33593
    	* tree-ssa-ter.c (is_replaceable_p): Return false if STMT may
    	throw an exception.
    
    
    testsuite/ChangeLog
    
    	PR 33593
    	* g++.dg/tree-ssa/pr33593.C: New test.
    
    From-SVN: r128893

---
+++ gcc/testsuite/g++.dg/tree-ssa/pr33593.C
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fnon-call-exceptions -fdump-tree-optimized" } */
+
+#include <stdio.h>
+
+void foo (int) { printf ("Bar\n"); }
+
+int
+main (void)
+{
+  int a = 1 / 0;	// { dg-warning "division by zero" }
+  printf ("Foo\n");
+  foo (a);
+}
+
+// The expression 1 / 0 should not be propagated into the call to foo() if it
+// may trap.
+// { dg-final { scan-tree-dump-times "foo \\(1 \\/ 0\\)" 0 "optimized" } }
+// { dg-final { cleanup-tree-dump "optimized" } }
