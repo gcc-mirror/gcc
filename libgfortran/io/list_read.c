af2778d32e8d916747229999a1a359a3af764018
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2007-10-03 00:33:09 +0000

    re PR libfortran/33253 (namelist: reading back a string with apostrophe)
    2007-10-02  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libfortran/33253
    	* io/list_read.c (read_character): Use DELIM_APOSTROPHE and DELIM_QUOTE
            and quote value in check of first character in string.
    
    From-SVN: r128973

--- libgfortran/io/list_read.c
+++ libgfortran/io/list_read.c
@@ -893,7 +893,10 @@ read_character (st_parameter_dt *dtp, int length __attribute__ ((unused)))
       goto get_string;
 
     default:
-      if (dtp->u.p.namelist_mode)
+      if (dtp->u.p.namelist_mode
+	  && (dtp->u.p.current_unit->flags.delim == DELIM_APOSTROPHE
+	      || dtp->u.p.current_unit->flags.delim == DELIM_QUOTE
+	      || quote == ' '))
 	{
 	  unget_char (dtp,c);
 	  return;
