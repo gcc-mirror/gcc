995acea9959111f272fce63fbf54734bf25ca547
matz <matz@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-04 13:35:06 +0000

    PR rtl-optimization/33653         * gcc.dg/pr33653.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129007 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr33653.c
@@ -0,0 +1,10 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-rtl-shorten" } */
+
+void f (volatile char *p)
+{
+  char c = p[0];
+}
+
+/* { dg-final { scan-rtl-dump "mem/v" "shorten" } } */
+/* { dg-final { cleanup-rtl-dump "shorten" } } */
