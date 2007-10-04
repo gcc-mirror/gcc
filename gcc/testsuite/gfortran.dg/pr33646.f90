d17cd69f66c1c0cb10074efa10252deea1ad260e
H.J. Lu <hongjiu.lu@intel.com>
2007-10-04 15:39:11 +0000

    re PR fortran/33646 (Gcc 4.3 revision 128954 failed tonto in SPEC CPU 2006)
    2007-10-04  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR fortran/33646
    	* gfortran.dg/pr33646.f90: New file.
    
    From-SVN: r129015

---
+++ gcc/testsuite/gfortran.dg/pr33646.f90
@@ -0,0 +1,59 @@
+! { dg-do compile }
+! PR fortran/33646
+!
+!
+
+module BAR_MODULE
+   implicit none
+   private
+   public    create_
+   interface create_
+      module procedure create
+   end interface
+   type system_type
+       integer(kind=kind(1)) :: max_memory_used
+   end type
+
+contains
+
+   subroutine create(self)
+    type(system_type) :: self
+      pointer :: self
+      allocate(self)
+   end subroutine
+
+end
+
+module FOO_MODULE
+   use BAR_MODULE
+   implicit none
+   private
+   public    create_
+   interface create_
+      module procedure create
+   end interface
+
+   public    create_copy_
+   interface create_copy_
+      module procedure create_copy
+   end interface
+contains
+
+   subroutine create(self)
+    character(*) :: self
+      pointer :: self
+      nullify(self)
+      allocate(self)
+
+      self = " "
+   end subroutine
+
+   subroutine create_copy(self,s)
+    character(*) :: self
+      pointer :: self
+      character(*) :: s
+      call create_(self)
+   end subroutine
+end
+
+! { dg-final { cleanup-modules "BAR_MODULE FOO_MODULE" } }
