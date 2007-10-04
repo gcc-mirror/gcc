37b22c82838f375dc1302a1ee1220fd5a81a3a1a
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2007-10-04 16:02:42 +0000

    re PR libfortran/33253 (namelist: reading back a string with apostrophe)
    2007-10-04  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libfortran/33253
    	* gfortran.dg/namelist_39.f90: Revise to use long names.
    
    From-SVN: r129018

--- gcc/testsuite/gfortran.dg/namelist_39.f90
+++ gcc/testsuite/gfortran.dg/namelist_39.f90
@@ -1,25 +1,29 @@
 ! { dg-do run }
 ! PR33421 and PR33253 Weird quotation of namelist output of character arrays
-! Test case from Toon Moene, adapted by Jerry DeLisle  <jvdelisle@gcc.gnu.org>
+! Test case from Toon Moone, adapted by Jerry DeLisle  <jvdelisle@gcc.gnu.org>
+
+! Long names used to test line_buffer feature is working.
 
 program test
 implicit none
-character(len=45) :: b(3)
-namelist /nam/ b
-b = 'x'
-open(99, status="scratch")
+character(len=45) :: b01234567890123456789012345678901234567890123456789012345678901(3)
+namelist /nam/ b01234567890123456789012345678901234567890123456789012345678901
+b01234567890123456789012345678901234567890123456789012345678901 = 'x'
+open(99)
 write(99,'(4(a,/),a)') "&NAM", &
-      " b(1)=' AAP NOOT MIES WIM ZUS JET',", &
-      " b(2)='SURF.PRESSURE',", &
-      " b(3)='APEKOOL',", &
+      " b01234567890123456789012345678901234567890123456789012345678901(1)=' AAP NOOT MIES WIM ZUS JET',", &
+      " b01234567890123456789012345678901234567890123456789012345678901(2)='SURF.PRESSURE',", &
+      " b01234567890123456789012345678901234567890123456789012345678901(3)='APEKOOL',", &
       " /"
 rewind(99)
 read(99,nml=nam)
 close(99)
 
-if (b(1).ne." AAP NOOT MIES WIM ZUS JET                   ") call abort
-if (b(2).ne."SURF.PRESSURE                                ") call abort
-if (b(3).ne."APEKOOL                                      ") call abort
-
+if (b01234567890123456789012345678901234567890123456789012345678901(1).ne.&
+    " AAP NOOT MIES WIM ZUS JET                   ") call abort
+if (b01234567890123456789012345678901234567890123456789012345678901(2).ne.&
+    "SURF.PRESSURE                                ") call abort
+if (b01234567890123456789012345678901234567890123456789012345678901(3).ne.&
+    "APEKOOL                                      ") call abort
 end program test
 
