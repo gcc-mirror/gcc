981db194768f6990aef28c6ea50b2f37012dd92d
bergner <bergner@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-05 17:55:18 +0000

    * ra-conflict.c: Include "sparseset.h". 	(conflicts): Change to HOST_WIDEST_FAST_INT. 	(allocnos_live): Redefine variable as a sparseset. 	(SET_ALLOCNO_LIVE, CLEAR_ALLOCNO_LIVE, GET_ALLOCNO_LIVE): Delete macros. 	(allocno_row_words): Removed global variable. 	(partial_bitnum, max_bitnum, adjacency_pool, adjacency): New variables. 	(CONFLICT_BITNUM, CONFLICT_BITNUM_FAST): New defines. 	(conflict_p, set_conflict_p, set_conflicts_p): New functions. 	(record_one_conflict_between_regnos): Cache allocno values and reuse. 	Use set_conflict_p. 	(record_one_conflict): Update uses of allocnos_live to use 	the sparseset routines.  Use set_conflicts_p. 	(mark_reg_store): Likewise. 	(set_reg_in_live): Likewise. 	(global_conflicts): Update uses of allocnos_live. 	Use the new adjacency list to visit an allocno's neighbors 	rather than iterating over all possible allocnos. 	Call set_conflicts_p to setup conflicts rather than adding 	them manually. 	* global.c: Comments updated. 	(CONFLICTP): Delete define. 	(regno_compare): New function.  Add prototype. 	(global_alloc): Sort the allocno to regno mapping according to 	which basic blocks the regnos are referenced in.  Modify the 	conflict bit matrix to a compressed triangular bitmatrix. 	Only allocate the conflict bit matrix and adjacency lists if 	we are actually going to allocate something. 	(expand_preferences): Use conflict_p.  Update uses of allocnos_live. 	(prune_preferences): Use the FOR_EACH_CONFLICT macro to visit an 	allocno's neighbors rather than iterating over all possible allocnos. 	(mirror_conflicts): Removed function. 	(dump_conflicts): Iterate over regnos rather than allocnos so 	that all dump output will be sorted by regno number. 	Use the FOR_EACH_CONFLICT macro. 	* ra.h: Comments updated. 	(conflicts): Update prototype to HOST_WIDEST_FAST_INT. 	(partial_bitnum, max_bitnum, adjacency, adjacency_pool): Add prototypes. 	(ADJACENCY_VEC_LENGTH, FOR_EACH_CONFLICT): New defines. 	(adjacency_list_d, adjacency_iterator_d): New types. 	(add_neighbor, adjacency_iter_init, adjacency_iter_done, 	adjacency_iter_next, regno_basic_block): New static inline functions. 	(EXECUTE_IF_SET_IN_ALLOCNO_SET): Removed define. 	(conflict_p): Add function prototype. 	* sparseset.h, sparseset.c: New files. 	* Makefile.in (OBJS-common): Add sparseset.o. 	(sparseset.o): New rule.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129037 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/sparseset.h
@@ -0,0 +1,162 @@
+/* SparseSet implementation.
+   Copyright (C) 2007 Free Software Foundation, Inc.
+   Contributed by Peter Bergner <bergner@vnet.ibm.com>
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef GCC_SPARSESET_H
+#define GCC_SPARSESET_H
+
+#include "system.h"
+#include <assert.h>
+
+#define SPARSESET_ELT_BITS ((unsigned) HOST_BITS_PER_WIDEST_FAST_INT)
+#define SPARSESET_ELT_TYPE unsigned int
+
+/* Data Structure used for the SparseSet representation.  */
+
+typedef struct sparseset_def
+{
+  SPARSESET_ELT_TYPE *dense;	/* Dense array.  */
+  SPARSESET_ELT_TYPE *sparse; 	/* Sparse array.  */
+  SPARSESET_ELT_TYPE members;	/* Number of elements.  */
+  SPARSESET_ELT_TYPE size;	/* Maximum number of elements.  */
+  SPARSESET_ELT_TYPE iter;	/* Iterator index.  */
+  unsigned char iter_inc;	/* Iteration increment amount.  */
+  bool iterating;
+  SPARSESET_ELT_TYPE elms[2];   /* Combined dense and sparse arrays.  */
+} *sparseset;
+
+#define sparseset_free(MAP)  free(MAP)
+extern sparseset sparseset_alloc (SPARSESET_ELT_TYPE n_elms);
+extern void sparseset_clear_bit (sparseset, SPARSESET_ELT_TYPE);
+extern void sparseset_copy (sparseset, sparseset);
+extern void sparseset_and (sparseset, sparseset, sparseset);
+extern void sparseset_and_compl (sparseset, sparseset, sparseset);
+extern void sparseset_ior (sparseset, sparseset, sparseset);
+extern bool sparseset_equal_p (sparseset, sparseset);
+
+/* Operation: S = {}
+   Clear the set of all elements.  */
+
+static inline void
+sparseset_clear (sparseset s)
+{
+  s->members = 0;
+  s->iterating = false;
+}
+
+/* Return the number of elements currently in the set.  */
+
+static inline SPARSESET_ELT_TYPE
+sparseset_cardinality (sparseset s)
+{
+  return s->members;
+}
+
+/* Return the maximum number of elements this set can hold.  */
+
+static inline SPARSESET_ELT_TYPE
+sparseset_size (sparseset s)
+{
+  return s->size;
+}
+
+/* Return true if e is a member of the set S, otherwise return false.  */
+
+static inline bool
+sparseset_bit_p (sparseset s, SPARSESET_ELT_TYPE e)
+{
+  SPARSESET_ELT_TYPE idx;
+
+  gcc_assert (e < s->size);
+
+  idx = s->sparse[e];
+
+  return idx < s->members && s->dense[idx] == e;
+}
+
+/* Low level insertion routine not meant for use outside of sparseset.[ch].
+   Assumes E is valid and not already a member of the set S.  */
+
+static inline void
+sparseset_insert_bit (sparseset s, SPARSESET_ELT_TYPE e, SPARSESET_ELT_TYPE idx)
+{
+  s->sparse[e] = idx;
+  s->dense[idx] = e;
+}
+
+/* Operation: S = S + {e}
+   Insert E into the set S, if it isn't already a member.  */
+
+static inline void
+sparseset_set_bit (sparseset s, SPARSESET_ELT_TYPE e)
+{
+  if (!sparseset_bit_p (s, e))
+    sparseset_insert_bit (s, e, s->members++);
+}
+
+/* Return and remove an arbitrary element from the set S.  */
+
+static inline SPARSESET_ELT_TYPE
+sparseset_pop (sparseset s)
+{
+  SPARSESET_ELT_TYPE mem = s->members;
+
+  gcc_assert (mem != 0);
+
+  s->members = mem - 1;
+  return s->dense[mem];
+}
+
+static inline void
+sparseset_iter_init (sparseset s)
+{
+  s->iter = 0;
+  s->iter_inc = 1;
+  s->iterating = true;
+}
+
+static inline bool
+sparseset_iter_p (sparseset s)
+{
+  if (s->iterating && s->iter < s->members)
+    return true;
+  else
+    return s->iterating = false;
+}
+
+static inline SPARSESET_ELT_TYPE
+sparseset_iter_elm (sparseset s)
+{
+  return s->dense[s->iter];
+}
+
+static inline void
+sparseset_iter_next (sparseset s)
+{
+  s->iter += s->iter_inc;
+  s->iter_inc = 1;
+}
+
+#define EXECUTE_IF_SET_IN_SPARSESET(SPARSESET, ITER)			\
+  for (sparseset_iter_init (SPARSESET);					\
+       sparseset_iter_p (SPARSESET)					\
+       && (((ITER) = sparseset_iter_elm (SPARSESET)) || 1);		\
+       sparseset_iter_next (SPARSESET))
+
+#endif /* GCC_SPARSESET_H */
