1a309dfb7abe475f67bd43a0bca04e6e892f1a86
Michael Matz <matz@suse.de>
2007-10-05 17:56:52 +0000

    re PR middle-end/33667 (RTL sharing with divmod)
    gcc/
     PR middle-end/33667
     * lower-subreg.c (decompose_multiword_subregs): Use
     validate_unshare_change().
    
    testsuite/
     * gcc.dg/pr33667.c: New testcase.
    
    From-SVN: r129038

---
+++ gcc/testsuite/gcc.dg/pr33667.c
@@ -0,0 +1,21 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+
+typedef unsigned int size_t;
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned long long int uint64_t;
+struct magic {
+  uint8_t mask_op;
+  union {
+   uint64_t _mask;
+  } _u;
+  union VALUETYPE {
+   uint16_t h;
+  } value;
+};
+void cvt_16(union VALUETYPE *p, const struct magic *m)
+{
+  if (m->_u._mask)
+    p->h %= (uint16_t) m->_u._mask;
+}
