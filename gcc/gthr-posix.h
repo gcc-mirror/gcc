c0aa46dbe493ad58cb52582b27453ddee38c400c
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-05 05:35:46 +0000

    2007-09-13  Doug Kwan  <dougkwan@google.com>
            * gcc/gthr-posix.h (__gthread_cond_broadcast, __gthread_cond_wait,
            __gthread_cond_wait_recursive): Add to extend interface for POSIX
            conditional variables. (__GTHREAD_HAS_COND): Macro defined to signify
            support of conditional variables.
            * gcc/gthr-posix95.h (__gthread_cond_broadcast, __gthread_cond_wait,
            __gthread_cond_wait_recursive): Add to extend interface for POSIX
            conditional variables. (__GTHREAD_HAS_COND): Macro defined to signify
            support of conditional variables.
            * gcc/gthr-single.h (__gthread_cond_broadcast, __gthread_cond_wait,
            __gthread_cond_wait_recursive): Add to extend interface for POSIX
            conditional variables.
            * gcc/gthr.h: Update comments to document new interface.
            * libstdc++-v3/include/ext/concurrent.h (class __mutex,
            class __recursive_mutex): Add new method gthread_mutex to access
            inner gthread mutex.
            [__GTHREAD_HAS_COND] (class __concurrence_broadcast_error,
            class __concurrence_wait_error, class __cond): Add.
            * guard.cc (recursive_push, recursive_pop): Delete.
            (init_in_progress_flag, set_init_in_progress_flag): Add to
            replace recursive_push and recursive_pop.
            (throw_recursive_init_exception): Add.
            (acquire, __cxa_guard_acquire, __cxa_guard_abort and
            __cxa_guard_release): [__GTHREAD_HAS_COND] Use a conditional
            for synchronization of static variable initialization.
            The global mutex is only held briefly when guards are
            accessed. [!__GTHREAD_HAS_COND] Fall back to the old code,
            which deadlocks.
            * testsuite/thread/guard.cc: Add new test. It deadlocks with the
            old locking code in libstdc++-v3/libsup++/guard.cc.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129030 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/gthr-posix.h
+++ gcc/gthr-posix.h
@@ -47,6 +47,11 @@ typedef pthread_key_t __gthread_key_t;
 typedef pthread_once_t __gthread_once_t;
 typedef pthread_mutex_t __gthread_mutex_t;
 typedef pthread_mutex_t __gthread_recursive_mutex_t;
+typedef pthread_cond_t __gthread_cond_t;
+
+/* POSIX like conditional variables are supported.  Please look at comments
+   in gthr.h for details. */
+#define __GTHREAD_HAS_COND	1	
 
 #define __GTHREAD_MUTEX_INIT PTHREAD_MUTEX_INITIALIZER
 #define __GTHREAD_ONCE_INIT PTHREAD_ONCE_INIT
@@ -57,6 +62,7 @@ typedef pthread_mutex_t __gthread_recursive_mutex_t;
 #else
 #define __GTHREAD_RECURSIVE_MUTEX_INIT_FUNCTION __gthread_recursive_mutex_init_function
 #endif
+#define __GTHREAD_COND_INIT PTHREAD_COND_INITIALIZER
 
 #if SUPPORTS_WEAK && GTHREAD_USE_WEAK
 # ifndef __gthrw_pragma
@@ -88,6 +94,8 @@ __gthrw3(pthread_mutex_lock)
 __gthrw3(pthread_mutex_trylock)
 __gthrw3(pthread_mutex_unlock)
 __gthrw3(pthread_mutex_init)
+__gthrw3(pthread_cond_broadcast)
+__gthrw3(pthread_cond_wait)
 #else
 __gthrw(pthread_once)
 __gthrw(pthread_getspecific)
@@ -98,6 +106,8 @@ __gthrw(pthread_mutex_lock)
 __gthrw(pthread_mutex_trylock)
 __gthrw(pthread_mutex_unlock)
 __gthrw(pthread_mutex_init)
+__gthrw(pthread_cond_broadcast)
+__gthrw(pthread_cond_wait)
 #endif
 
 __gthrw(pthread_key_create)
@@ -110,20 +120,16 @@ __gthrw(pthread_mutexattr_destroy)
 #if defined(_LIBOBJC) || defined(_LIBOBJC_WEAK)
 /* Objective-C.  */
 #if defined(__osf__) && defined(_PTHREAD_USE_MANGLED_NAMES_)
-__gthrw3(pthread_cond_broadcast)
 __gthrw3(pthread_cond_destroy)
 __gthrw3(pthread_cond_init)
 __gthrw3(pthread_cond_signal)
-__gthrw3(pthread_cond_wait)
 __gthrw3(pthread_exit)
 __gthrw3(pthread_mutex_destroy)
 __gthrw3(pthread_self)
 #else
-__gthrw(pthread_cond_broadcast)
 __gthrw(pthread_cond_destroy)
 __gthrw(pthread_cond_init)
 __gthrw(pthread_cond_signal)
-__gthrw(pthread_cond_wait)
 __gthrw(pthread_exit)
 __gthrw(pthread_mutex_destroy)
 __gthrw(pthread_self)
@@ -735,6 +741,25 @@ static inline int
 __gthread_recursive_mutex_unlock (__gthread_recursive_mutex_t *mutex)
 {
   return __gthread_mutex_unlock (mutex);
+}
+
+static inline int
+__gthread_cond_broadcast (__gthread_cond_t *cond)
+{
+  return __gthrw_(pthread_cond_broadcast) (cond);
+}
+
+static inline int
+__gthread_cond_wait (__gthread_cond_t *cond, __gthread_mutex_t *mutex)
+{
+  return __gthrw_(pthread_cond_wait) (cond, mutex);
+}
+
+static inline int
+__gthread_cond_wait_recursive (__gthread_cond_t *cond,
+			       __gthread_recursive_mutex_t *mutex)
+{
+  return __gthread_cond_wait (cond, mutex);
 }
 
 #endif /* _LIBOBJC */
