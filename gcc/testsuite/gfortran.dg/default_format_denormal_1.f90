991125935677cb9bbc339f7d5ed8ec19e579f592
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-06 21:22:39 +0000

    * gfortran.dg/default_format_denormal_2.f90: New test. 	* gfortran.dg/default_format_2.inc: New test. 	* gfortran.dg/default_format_denormal_1.f90: New test. 	* gfortran.dg/default_format_1.inc: New test. 	* gfortran.dg/default_format_1.f90: Don't test for denormalized 	numbers. 	* gfortran.dg/default_format_2.f90: Don't test for denormalized 	numbers.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129057 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/default_format_denormal_1.f90
@@ -0,0 +1,21 @@
+! { dg-do run { xfail *-apple-darwin* } }
+! Test XFAILed on these platforms because the system's printf() lacks
+! proper support for denormals.
+!
+! This tests that the default formats for formatted I/O of reals are
+! wide enough and have enough precision, by checking that values can
+! be written and read back.
+!
+include "default_format_1.inc"
+
+program main
+  use test_default_format
+
+  if (test (tiny(0.0_4), -1) /= 0) call abort
+  if (test (-tiny(0.0_4), 1) /= 0) call abort
+
+  if (test (tiny(0.0_8), -1) /= 0) call abort
+  if (test (-tiny(0.0_8), 1) /= 0) call abort
+end program main
+!
+! { dg-final { cleanup-modules "test_default_format" } }
