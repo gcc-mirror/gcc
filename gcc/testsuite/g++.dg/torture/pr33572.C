90834304e999d7d758dc74849b78b31939478bf8
Alexandre Oliva <aoliva@redhat.com>
2007-10-08 23:57:20 +0000

    re PR tree-optimization/33572 (wrong code with -O)
    PR tree-optimization/33572
    * g++.dg/torture/pr33572.C: Replace with complete test.
    
    From-SVN: r129144

--- gcc/testsuite/g++.dg/torture/pr33572.C
+++ gcc/testsuite/g++.dg/torture/pr33572.C
@@ -1,36 +1,16 @@
 // { dg-do run }
-namespace __gnu_cxx {
-	template<bool> struct __pool {
-		void _M_reclaim_block(char* p, unsigned long bytes);
-	};
-}
+#include <vector>
+#include <memory>
 
-struct vector {
-	~vector() { deallocate(0); }
-	void deallocate(int* p) {
-		if (p) {
-			static __gnu_cxx::__pool<true> pool;
-			pool._M_reclaim_block((char*)0, 0);
-		}
-	}
-};
+struct Foo { virtual void f() {} };
 
-struct Foo { virtual void f() { } };
-
-struct auto_ptr {
-	Foo* ptr;
-	auto_ptr() : ptr(0) { }
-	~auto_ptr() { delete ptr; }
-	Foo* release() { Foo* tmp = ptr; ptr = 0; return tmp; }
-	void reset(Foo* p) { ptr = p; }
-};
-
-int main(int argc, char**) {
-	auto_ptr foo;
+int main(int argc, char**)
+{
+	std::auto_ptr<Foo> foo;
 	if (argc) {
 		foo.reset(new Foo());
 	} else {
-		vector v;
+		std::vector<int> v;
 	}
 	Foo* p = foo.release();
 	p->f();
