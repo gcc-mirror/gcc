0e6c9eaa5c0c9bf00972834f3a47e5146bd47dee
Johannes Singler <singler@ira.uka.de>
2007-10-08 21:14:45 +0000

    base.h: Added plus and multiplies functor for differently typed objects.
    2007-10-08  Johannes Singler  <singler@ira.uka.de>
    
    	* include/parallel/base.h: Added plus and multiplies functor
           for differently typed objects.
    	* include/parallel/numeric: Use it.
    	* include/parallel/for_each_selectors.h: Allowed different types.
    	* include/parallel/partial_sum.h: Fixed return value.
    	* testsuite/26_numerics/accumulate/1.cc: Tests for accumulate.
    	* testsuite/26_numerics/inner_product/1.cc: Tests for inner_product.
    
    From-SVN: r129140

---
+++ libstdc++-v3/testsuite/26_numerics/inner_product/1.cc
@@ -0,0 +1,56 @@
+// Copyright (C) 2001, 2004 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 26.4.2 [lib.inner_product]
+
+#include <numeric>
+#include <testsuite_hooks.h>
+
+int A1[] = {1, 2, 3, 4,  5,  6,  7,  8,  9, 10};
+int A2[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+const int NA = sizeof(A1) / sizeof(int);
+
+bool B1[] = {false, true, true, false, true, false, true, true, false, true};
+bool B2[] = {true, false, true, true, false, true, false, true, true, false};
+const int NB = sizeof(B1) / sizeof(bool);
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  int res = std::inner_product(A1, A1 + NA, A2, 31);
+  VERIFY( res == 983 );
+}
+
+void
+test02()
+{
+  bool test __attribute__((unused)) = true;
+
+  int res = std::inner_product(B1, B1 + NB, B2, 100);
+  VERIFY( res == 102 );
+}
+
+int
+main()
+{
+    test01();
+    test02();
+    return 0;
+}
