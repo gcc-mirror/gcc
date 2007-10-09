05c42b11e64489819f65e0daec4679f311ec3632
Richard Sandiford <rsandifo@nildram.co.uk>
2007-10-09 11:05:33 +0000

    re PR tree-optimization/33615 (Hoisting of potentially-throwing values for -fnon-call-exceptions)
    gcc/
    	PR tree-optimization/33615
    	* tree-ssa-pre.c (compute_avail): Don't call make_values_for_stmt
    	if the statement might throw.  Fix formatting.
    
    gcc/testsuite/
    	PR tree-optimization/33615
    	* g++.dg/tree-ssa/pr33615-2.C: New test.
    
    From-SVN: r129164

---
+++ gcc/testsuite/g++.dg/tree-ssa/pr33615-2.C
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fnon-call-exceptions -fdump-tree-pre-details -w" } */
+
+extern volatile int y;
+
+double
+foo (double a, int x)
+{
+  while (x--)
+    {
+      y++;
+      a += 1.0 / 0.0;
+    }
+  return a;
+}
+
+// The expression 1.0 / 0.0 should not be treated as a loop invariant
+// if it may throw an exception.
+// { dg-final { scan-tree-dump-times "Replaced 1\\\.0e\\\+0 / 0\\\.0" 0 "pre" } }
+// { dg-final { cleanup-tree-dump "pre" } }
