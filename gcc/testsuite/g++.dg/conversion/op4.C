649873d293ba29ed719f10aad428371fd07502dc
simartin <simartin@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-12 18:43:33 +0000

    gcc/cp/
    2007-10-12  Simon Martin  <simartin@users.sourceforge.net>
    
    	PR c++/26698
    	* call.c (build_user_type_conversion_1): Do not consider conversion
    	functions to convert a (possibly cv-qualified) object to the (possibly
    	cv-qualified) same object type (or a reference to it), to a (possibly
    	cv-qualified) base class of that type (or a reference to it).
    
    gcc/testsuite/
    
    2007-10-12  Simon Martin  <simartin@users.sourceforge.net>
    
    	PR c++/26698
    	* g++.dg/conversion/op4.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129274 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/conversion/op4.C
@@ -0,0 +1,19 @@
+/* PR c++/26698 */
+/* { dg-do "compile" } */
+
+struct X {
+  int x;
+  X (int i = 0) : x (i) {}
+  operator X& (void) const {
+    return *(new X);
+  }
+};
+
+void add_one (X & ref) { /* { dg-error "in passing argument" } */
+  ++ ref.x;
+}
+
+void foo() {
+  X const a (2);
+  add_one(a); /* { dg-error "invalid initialization of reference of type" } */
+}
