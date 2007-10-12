3c285765879c471cb7ca2364e5a5c165e380aa41
Nathan Froyd <froydnj@codesourcery.com>
2007-10-12 16:12:45 +0000

    re PR rtl-optimization/11001 (global register %edi versus string builtins)
    gcc/
    	PR 11001
    	* config/i386/i386.md (strmov): Check for esi and edi usage.
    	* config/i386/i386.c (decide_alg): Check whether we can use a
    	rep prefix and adjust algorithm choice accordingly.
    	(ix86_expand_strlen): Check for eax, ecx, and edi usage.
    
    gcc/testsuite/
    	PR 11001
    	* gcc.target/i386/pr11001-strlen-1.c: New testcase.
    	* gcc.target/i386/pr11001-strlen-2.c: New testcase.
    	* gcc.target/i386/pr11001-strlen-3.c: New testcase.
    	* gcc.target/i386/pr11001-memset-1.c: New testcase.
    	* gcc.target/i386/pr11001-memset-2.c: New testcase.
    	* gcc.target/i386/pr11001-memset-3.c: New testcase.
    	* gcc.target/i386/pr11001-memcpy-1.c: New testcase.
    	* gcc.target/i386/pr11001-memcpy-2.c: New testcase.
    	* gcc.target/i386/pr11001-memcpy-3.c: New testcase.
    
    From-SVN: r129265

---
+++ gcc/testsuite/gcc.target/i386/pr11001-memcpy-3.c
@@ -0,0 +1,23 @@
+/* Ensure that we don't use 'rep movX' in the presence of register globals.  */
+/* { dg-do compile } */
+/* { dg-options "-Os -m32" } */
+
+extern void *memcpy (void *, const void *, __SIZE_TYPE__);
+
+register int regvar asm("%ecx"); /* { dg-warning "call-clobbered register" } */
+
+int foo[10];
+int bar[10];
+
+char baz[15];
+char quux[15];
+
+void
+do_copy ()
+{
+  memcpy (foo, bar, sizeof foo);
+  memcpy (baz, quux, sizeof baz);
+}
+
+/* { dg-final { scan-assembler-not "rep movsl" } } */
+/* { dg-final { scan-assembler-not "rep movsb" } } */
---
+++ gcc/testsuite/gcc.target/i386/pr11001-memset-2.c
@@ -0,0 +1,23 @@
+/* Ensure that we don't use 'rep stoX' in the presence of register globals.  */
+/* { dg-do compile } */
+/* { dg-options "-Os -m32" } */
+
+extern void *memset (void *, int, __SIZE_TYPE__);
+
+register int regvar asm("%ecx"); /* { dg-warning "call-clobbered register" } */
+
+int foo[10];
+int bar[10];
+
+char baz[15];
+char quux[15];
+
+void
+do_copy ()
+{
+  memset (foo, 0, sizeof foo);
+  memset (baz, 0, sizeof baz);
+}
+
+/* { dg-final { scan-assembler-not "rep stosl" } } */
+/* { dg-final { scan-assembler-not "rep stosb" } } */
