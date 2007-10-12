920901ac7686621f11f91e8606f8e55c1fafce69
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-12 16:51:53 +0000

    2007-10-12  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/33542
    	* resolve.c (resolve_actual_arglist): If the actual argument is
    	ambiguous, then there is an error.
    
    2007-10-12  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/33542
    	* gfortran.dg/ambiguous_specific_1.f90: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129268 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/ambiguous_specific_1.f90
@@ -0,0 +1,38 @@
+! { dg-do compile }
+! Checks the fix for PR33542, in which the ambiguity in the specific
+! interfaces of foo was missed.
+!
+! Contributed by Tobias Burnus <burnus@gcc.gnu.org>
+!
+MODULE M1
+   INTERFACE FOO
+     MODULE PROCEDURE FOO
+   END INTERFACE
+CONTAINS
+   SUBROUTINE FOO(I)
+     INTEGER, INTENT(IN) :: I
+     WRITE(*,*) 'INTEGER'
+   END SUBROUTINE FOO
+END MODULE M1
+
+MODULE M2
+   INTERFACE FOO
+     MODULE PROCEDURE FOO
+   END INTERFACE
+CONTAINS
+   SUBROUTINE FOO(R)
+     REAL, INTENT(IN) :: R
+     WRITE(*,*) 'REAL'
+   END SUBROUTINE FOO
+END MODULE M2
+
+PROGRAM P
+   USE M1
+   USE M2
+   implicit none
+   external bar
+   CALL FOO(10)
+   CALL FOO(10.)
+   call bar (foo)  ! { dg-error "is ambiguous" }
+END PROGRAM P
+! { dg-final { cleanup-modules "m1 m2" } }
