2b270c42f8567a4ffa46fa62e1c52feab372a189
dje <dje@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-13 15:03:07 +0000

    * gcc.target/powerpc/parity-1.c: POWER5 feature, not POWER6.         * gcc.target/powerpc/popcount-1.c: Same.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129284 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/parity-1.c
+++ gcc/testsuite/gcc.target/powerpc/parity-1.c
@@ -1,5 +1,5 @@
 /* { dg-do compile { target { ilp32 } } } */
-/* { dg-options "-O2 -mcpu=power6" } */
+/* { dg-options "-O2 -mcpu=power5" } */
 /* { dg-final { scan-assembler "popcntb" } } */
 /* { dg-final { scan-assembler-not "mullw" } } */
 
