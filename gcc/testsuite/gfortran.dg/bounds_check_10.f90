08ddab21215fba57be1ebdb495b9374953a6b679
Tobias Schlüter <tobi@gcc.gnu.org>
2007-10-13 23:43:49 +0200

    re PR fortran/33254 (Diagnose different string lengths in array constructors at run time)
    2007-10-13  Tobias Schlueter  <tobi@gcc.gnu.org>
        Paul Thomas  <pault@gcc.gnu.org>
    
    PR fortran/33254
    PR fortran/33727
    fortran/
    * trans-array.c (get_array_ctor_var_strlen): Check upper bound for
    constness instead of lower bound.
    (get_array_ctor_strlen): Add bounds-checking code.
    testsuite/
    * bounds_check_10.f90: New.
    
    Co-Authored-By: Paul Thomas <pault@gcc.gnu.org>
    
    From-SVN: r129286

---
+++ gcc/testsuite/gfortran.dg/bounds_check_10.f90
@@ -0,0 +1,15 @@
+! { dg-do run }
+! { dg-options "-fbounds-check" }
+! { dg-shouldfail "Different CHARACTER lengths" }
+! PR fortran/33254: No bounds checking for array constructors
+program array_char
+implicit none
+character (len=2) :: x, y
+character (len=2) :: z(3)
+x = "a "
+y = "cd"
+z = [y(1:1), x(1:len(trim(x)))]  ! should work
+z = [trim(x), trim(y), "aaaa"] ! [ "a", "cd", "aaaa" ] should catch first error
+end program array_char
+
+! { dg-output "Different CHARACTER lengths .1/2. in array constructor" }
