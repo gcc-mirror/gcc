f8ccbb9fd21247a54ff83e7460ff9939bc2f1f5a
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-15 14:03:52 +0000

    2007-10-15  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR libfortran/33055
    	* gfortran.dg/inquire_11.f90: New test.
    	* gfortan.dg/negative_unit_int8.f: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129346 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/inquire_11.f90
@@ -0,0 +1,10 @@
+! { dg-do compile }
+! PR 33055 Runtime error in INQUIRE unit existance with -fdefault-integer-8
+! Test case from PR33217 prepared by Jerry DeLisle <jvdelisle@gcc.gnu.org>
+MODULE print_it
+CONTAINS
+  SUBROUTINE i()
+    LOGICAL :: qexist
+    INQUIRE (UNIT=1, EXIST=qexist)
+  END SUBROUTINE i
+END MODULE print_it
