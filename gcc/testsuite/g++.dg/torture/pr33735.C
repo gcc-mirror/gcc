1432b19f40085f820a883101a40e878452b15e2a
Alexandre Oliva <aoliva@redhat.com>
2007-10-15 17:07:20 +0000

    re PR tree-optimization/33735 (verify_stmts failed: missing PHI def)
    gcc/ChangeLog:
    PR tree-optimization/33735
    PR tree-optimization/33572
    * tree-inline.c (update_ssa_across_abnormal_edges): Revert
    2007-10-09's change.
    * except.c (duplicate_eh_regions): Don't look for prev_try
    beyond ERT_ALLOWED_EXCEPTIONS with an empty list.
    gcc/testsuite/ChangeLog:
    PR tree-optimization/33735
    * g++.dg/torture/pr33735.C: New.
    
    From-SVN: r129356

---
+++ gcc/testsuite/g++.dg/torture/pr33735.C
@@ -0,0 +1,21 @@
+// { dg-do compile }
+#include <string>
+typedef struct _ts { } PyThreadState;
+PyThreadState * Py_NewInterpreter(void);
+void Py_EndInterpreter(PyThreadState *);
+class ApplicationError {
+public:
+    ApplicationError(std::string errormsg) : errormsg(errormsg)  { }
+    std::string errormsg;
+};
+void run()
+{
+    PyThreadState *py_state=__null;
+    try {
+        if (!(py_state=Py_NewInterpreter()))
+            throw ApplicationError("error");
+    }
+    catch(ApplicationError e) {
+        Py_EndInterpreter(py_state);
+    }
+}
