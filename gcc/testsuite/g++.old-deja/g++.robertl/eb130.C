947fb1b8173745821bc3f77669d1a1a8364d05e2
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-18 22:23:11 +0000

    2007-10-18  Benjamin Kosnik  <bkoz@redhat.com>
    	* g++.old-deja/g++.robertl/eb130.C: Adjust include paths.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129464 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.robertl/eb130.C
+++ gcc/testsuite/g++.old-deja/g++.robertl/eb130.C
@@ -1,11 +1,11 @@
 // { dg-do assemble  }
-// { dg-options "-Wall" }
+// { dg-options "-Wall -Wno-deprecated" }
 // Gives spurious warnings when compiled with -Wall
 // EGCS 06/08/98 i686-pc-linux-gnulibc1
 // Message-Id: <199806081358.PAA02505@achibm5.chemie.uni-karlsruhe.de>
 // From: Klaus-Georg Adams <Klaus-Georg.Adams@chemie.uni-karlsruhe.de>
 
 
-#include <ext/hash_set>
+#include <backward/hash_set>
 __gnu_cxx::hash_set<int> foo;
 
