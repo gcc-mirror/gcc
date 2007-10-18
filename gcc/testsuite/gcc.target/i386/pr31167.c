24fd3b2d737d795af1974882e2cd43141ce25099
Uros Bizjak <ubizjak@gmail.com>
2007-10-18 10:59:31 +0200

    pr31167.c: Add i?86-*-* to target selector.
    	* gcc.target/i386/pr31167.c: Add i?86-*-* to target selector.
    	Require lp64 effective target.
    
    From-SVN: r129432

--- gcc/testsuite/gcc.target/i386/pr31167.c
+++ gcc/testsuite/gcc.target/i386/pr31167.c
@@ -1,4 +1,5 @@
-/* { dg-do compile { target x86_64-*-* } } */
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-require-effective-target lp64 } */
 /* { dg-options "-O" } */
 
 typedef int int32_t;
