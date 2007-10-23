fbc08f2bc7bc1253b1e6fe0fae74098583288375
davem <davem@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-23 04:33:26 +0000

    gcc/:
    2007-10-22  Seongbae Park <seongbae.park@gmail.com>
    	David S. Miller  <davem@davemloft.net>
    
    	* df-scan.c (df_get_call_refs): Mark global registers as both a
    	DF_REF_REG_USE and a non-clobber DF_REF_REG_DEF.
    
    
    gcc/testsuite/:
    
    2007-10-22  David S. Miller  <davem@davemloft.net>
    
    	* gcc.dg/globalreg-1.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129572 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/globalreg-1.c
@@ -0,0 +1,54 @@
+/* { dg-do run { target sparc*-*-* } } */
+/* { dg-options "-std=gnu99 -Os" } */
+
+/* This is a massively distilled test case based upon
+   mm/memory.c:unmap_vmas() in the Linux kernel when compiled
+   on sparc64 for SMP which uses a global register as the
+   base of the per-cpu variable area.
+
+   Because of a bug in global register handling in the dataflow
+   code, the loop-invariant pass would move 'expression(regval)'
+   outside of the loop.  */
+
+extern void exit(int);
+extern void abort(void);
+
+register unsigned long regval __asm__("g6");
+
+extern void cond_resched(void);
+
+unsigned int var;
+
+static unsigned long expression(unsigned long v)
+{
+  unsigned long ret;
+
+  __asm__("" : "=r" (ret) : "0" (0));
+  return ret + v;
+}
+
+void func(unsigned long *pp)
+{
+  int i;
+
+  for (i = 0; i < 56; i++) {
+    cond_resched();
+    *pp = expression(regval);
+  }
+}
+
+void __attribute__((noinline)) cond_resched(void)
+{
+	regval++;
+}
+
+int main(void)
+{
+  unsigned long val;
+
+  regval = 100;
+  func(&val);
+  if (val != 156)
+    abort();
+  exit(0);
+}
