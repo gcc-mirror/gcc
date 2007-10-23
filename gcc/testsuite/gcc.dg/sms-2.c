d7794febf1e5ba030dcbd4e0cadc1ffa95986b21
revitale <revitale@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-23 07:53:42 +0000

    SMS: Add new testcase
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129575 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/sms-2.c
@@ -0,0 +1,34 @@
+/* The same test as 921011-1.c.  It can fails on gcc 4.1 due to a dfs
+   traversal of the loops after versioning.  */
+
+/* { dg-do compile } */
+/* { dg-options "-O2 -fmodulo-sched -dm" } */
+
+
+void
+fun (nb)
+     int nb;
+{
+  int th, h, em, nlwm, nlwS, nlw, sy;
+
+  while (nb--)
+    while (h--)
+      {
+	nlw = nlwm;
+	while (nlw)
+	  {
+	    if (nlwS == 1)
+	      {
+	      }
+	    else
+	      if (nlwS == 1)
+		{
+		}
+	    nlwS--; nlw--;
+	  }
+	if (em)
+	  nlwS--;
+	if (++sy == th)
+	  sy = 0;
+      }
+}
