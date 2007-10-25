0bf2cf8907a382fd2223ac0975516ab99283eaa8
Ira Rosen <irar@gcc.gnu.org>
2007-10-25 07:25:55 +0000

    re PR tree-optimization/33866 (ICE in vect_get_vec_def_for_stmt_copy, at tree-vect-transform.c:1937)
    	PR tree-optimization/33866
    	* tree-vect-transform.c (vectorizable_store): Check operands of all the
    	stmts in the group of strided accesses. Get def stmt type for each store
    	in the group and pass it to vect_get_vec_def_for_stmt_copy ().
    
    From-SVN: r129623

---
+++ gcc/testsuite/gcc.dg/vect/pr33866.c
@@ -0,0 +1,31 @@
+/* Testcase by Martin Michlmayr <tbm@cyrius.com> */
+/* { dg-do compile } */
+
+typedef struct
+{
+  long *coords;
+}
+fill_iter_info;
+
+extern H5Diterate (fill_iter_info *);
+
+void test_select_fill_hyper_simple (long *offset)
+{
+  long start[2];
+  int num_points;
+  long points[16][2];
+  fill_iter_info iter_info;
+  int i, j;
+  iter_info.coords = (long *) points;
+  for (i = 0, num_points = 0; j < (int) start[1]; j++, num_points++)
+  {
+    points[num_points][0] = i + start[0];
+    points[num_points][1] = j + start[1];
+  }
+  H5Diterate (&iter_info);
+}
+
+/* Needs interleaving support.  */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { target { vect_interleave } } } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
+
