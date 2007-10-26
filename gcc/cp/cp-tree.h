91a77d68e72ba6e7597884fc10afd07a9700bb5a
Jason Merrill <jason@redhat.com>
2007-10-26 15:54:10 -0400

    re PR c++/24791 (ICE on invalid instantiation of template's static member)
            PR c++/24791
            * pt.c (get_template_info): New fn.
            (template_class_depth): Use it.
            (push_template_decl_real): Check that the template args of the
            definition match the args of the previous declaration.
    
    From-SVN: r129660

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -4437,6 +4437,7 @@ extern bool uses_parameter_packs                (tree);
 extern bool template_parameter_pack_p           (const_tree);
 extern tree make_pack_expansion                 (tree);
 extern bool check_for_bare_parameter_packs      (tree);
+extern tree get_template_info			(tree);
 extern int template_class_depth			(tree);
 extern int is_specialization_of			(tree, tree);
 extern bool is_specialization_of_friend		(tree, tree);
