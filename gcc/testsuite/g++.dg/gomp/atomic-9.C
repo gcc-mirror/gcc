5db2503012293b57f55342177b75325aee66c420
razya <razya@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-29 11:09:09 +0000

    2007-09-23  Razya Ladelsky 	    Zdenek Dvorak
    	OMP_ATOMIC expand testsuite related changes.
    
    	* testsuite/gcc.dg/gomp/atomic-3.c: Search for string on
    	ompexp dump instead of gimple dump.
    	* testsuite/gcc.dg/gomp/atomic-9.c: Same.
    	* testsuite/gcc.dg/gomp/atomic-10.c: Same.
    	* testsuite/g++.dg/gomp/atomic-3.C: Same.
    	* testsuite/g++.dg/gomp/atomic-9.C: Same.
    	* testsuite/g++.dg/gomp/atomic-10.C: Same.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129717 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/gomp/atomic-9.C
+++ gcc/testsuite/g++.dg/gomp/atomic-9.C
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-fopenmp -fdump-tree-gimple" } */
+/* { dg-options "-fopenmp -fdump-tree-ompexp" } */
 
 volatile int *bar(void);
 
@@ -9,5 +9,5 @@ void f1(void)
     *bar() += 1;
 }
 
-/* { dg-final { scan-tree-dump-times "__sync_fetch_and_add" 1 "gimple" { target i?86-*-* x86_64-*-* ia64-*-* powerpc*-*-* alpha*-*-* } } } */
-/* { dg-final { cleanup-tree-dump "gimple" } } */
+/* { dg-final { scan-tree-dump-times "__sync_fetch_and_add" 1 "ompexp" { target i?86-*-* x86_64-*-* ia64-*-* powerpc*-*-* alpha*-*-* } } } */
+/* { dg-final { cleanup-tree-dump "ompexp" } } */
