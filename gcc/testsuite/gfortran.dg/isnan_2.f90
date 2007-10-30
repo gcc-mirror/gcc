e1332188a4c377db70d82b8e6a5df3961b1a4499
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2007-10-30 21:48:23 +0000

    re PR fortran/33596 (ICE with simplified ISNAN)
    	PR fortran/33596
    
    	* trans-intrinsic.c (gfc_conv_intrinsic_isnan): Strip NOP_EXPR
    	from the result of build_call_expr.
    
    	* gfortran.dg/isnan_2.f90: New test.
    
    From-SVN: r129782

---
+++ gcc/testsuite/gfortran.dg/isnan_2.f90
@@ -0,0 +1,17 @@
+! Test for the ISNAN intrinsic on constants
+!
+! { dg-do run }
+! { dg-options "-fno-range-check" }
+! { dg-options "-fno-range-check -pedantic-errors -mieee" { target sh*-*-* } }
+!
+  implicit none
+  character(len=1) :: s
+  write(s,'(L1)') isnan(0.)
+  if (s /= 'F') call abort
+
+  write(s,'(L1)') isnan(exp(huge(0.)))
+  if (s /= 'F') call abort
+
+  write(s,'(L1)') isnan(0./0.)
+  if (s /= 'T') call abort
+end
