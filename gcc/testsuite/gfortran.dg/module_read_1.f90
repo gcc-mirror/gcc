ed69e50620a42f9b5a5dc320e7ae3091a2f378cd
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-31 15:10:12 +0000

    2007-10-31  Tobias Burnus  <burnus@net-b.de>
            PR fortran/33941
            * modules.c (intrinsics): Use only alphabetic names for
            intrinsic operators.
    
    
    2007-10-31  Dominique d'Humieres  <dominiq@lps.ens.fr>
                Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/33941
            * gfortran.dg/module_read_1.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129801 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/module_read_1.f90
@@ -0,0 +1,29 @@
+! { dg-do run }
+! PR fortran/33941
+! The problem was that the intrinsic operators
+! were written to the module file as '/=' etc.
+! but this format was not understood on reading.
+!
+! Test case by Toby White, stripped down by
+! Dominique d'Humieres and Francois-Xavier Coudert
+
+module foo
+contains
+  function pop(n) result(item)
+    integer :: n
+    character(len=merge(1, 0, n > 0)) :: item
+  end function pop
+  function push(n) result(item)
+    integer :: n
+    character(len=merge(1, 0, n /= 0)) :: item
+  end function push
+end module foo
+
+program test
+  use foo
+  if(len(pop(0)) /= 0) call abort()
+  if(len(pop(1)) /= 1) call abort()
+  if(len(push(0)) /= 0) call abort()
+  if(len(push(1)) /= 1) call abort()
+end program
+! { dg-final { cleanup-modules "foo" } }
