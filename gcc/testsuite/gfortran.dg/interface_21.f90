e26011c9088dcacbd3e9001887eabe8e7e54b205
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-31 14:30:48 +0000

    2007-10-31  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR fortran/33162
    	* gfortran.dg/interface_19.f90: New.
    	* gfortran.dg/interface_20.f90: New.
    	* gfortran.dg/interface_21.f90: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129799 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/interface_21.f90
@@ -0,0 +1,22 @@
+! { dg-do compile }
+! PR33162 INTRINSIC functions as ACTUAL argument
+! Test case adapted from PR by Jerry DeLisle <jvdelisle@gcc.gnu.org>
+module m
+implicit none
+contains
+  subroutine sub(a)
+    interface
+      function a(x)
+        real :: a, x
+        intent(in) :: x
+      end function a
+    end interface
+    print *, a(4.0)
+  end subroutine sub
+end module m
+
+use m
+implicit none
+EXTERNAL foo  ! implicit interface is undefined
+call sub(foo) ! { dg-error "Type/rank mismatch in argument" }
+end
