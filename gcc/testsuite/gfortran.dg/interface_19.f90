69d10e15fb001b34aedfe14a7af7c9e1fafea6a0
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2007-10-31 14:30:48 +0000

    re PR fortran/33162 (INTRINSIC functions as ACTUAL argument)
    2007-10-31  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR fortran/33162
    	* gfortran.dg/interface_19.f90: New.
    	* gfortran.dg/interface_20.f90: New.
    	* gfortran.dg/interface_21.f90: New.
    
    From-SVN: r129799

---
+++ gcc/testsuite/gfortran.dg/interface_19.f90
@@ -0,0 +1,29 @@
+! { dg-do run }
+! PR33162 INTRINSIC functions as ACTUAL argument
+! Test case adapted from PR by Jerry DeLisle <jvdelisle@gcc.gnu.org>
+module m
+implicit none
+contains
+  subroutine sub(a)
+    optional :: a
+    character(25) :: temp
+    interface
+      function a(x)
+        real(kind=8):: a
+        real(kind=8):: x
+        intent(in) :: x
+      end function a
+    end interface
+    if(present(a)) then
+      write(temp,'(f16.10)')a(4.0d0)
+      if (trim(temp) /= '   -0.6536436209') call abort
+    endif
+  end subroutine sub
+end module m
+
+use m
+implicit none
+intrinsic dcos
+call sub()
+call sub(dcos)
+end
