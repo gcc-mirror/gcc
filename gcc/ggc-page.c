1bf8cfe3cbd7414b17b6f2bb3c7bb7099822d647
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-11-01 22:20:29 +0000

    2007-11-01  Richard Guenther  <rguenther@suse.de>
            * ggc-page.c (extra_order_size_tab): Add bitmap_head size.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129833 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ggc-page.c
+++ gcc/ggc-page.c
@@ -195,6 +195,7 @@ static const size_t extra_order_size_table[] = {
   sizeof (struct function),
   sizeof (struct basic_block_def),
   sizeof (bitmap_element),
+  sizeof (bitmap_head),
   /* PHI nodes with one to three arguments are already covered by the
      above sizes.  */
   sizeof (struct tree_phi_node) + sizeof (struct phi_arg_d) * 3,
