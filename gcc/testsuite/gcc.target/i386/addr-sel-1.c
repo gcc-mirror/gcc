9d85b4853be3eb11746748e20918a9b57480fbab
Eric Botcazou <ebotcazou@libertysurf.fr>
2007-11-03 08:53:01 +0100

    re PR rtl-optimization/28940 (address selection does not work correctly)
    	PR rtl-optimization/28940
    	* gcc.target/i386/addr-sel-1.c: New test.
    
    From-SVN: r129868

---
+++ gcc/testsuite/gcc.target/i386/addr-sel-1.c
@@ -0,0 +1,16 @@
+/* PR rtl-optimization/28940 */
+/* Origin: Lev Makhlis <lmakhlis@bmc.com> */
+
+/* { dg-do compile } */
+/* { dg-require-effective-target ilp32 } */
+/* { dg-options "-O2 -mtune=i686" } */
+
+char a[10], b[10];
+
+int f(int i)
+{
+  return a[i+1] + b[i+1];
+}
+
+/* { dg-final { scan-assembler "a\\+1" } } */
+/* { dg-final { scan-assembler "b\\+1" } } */
