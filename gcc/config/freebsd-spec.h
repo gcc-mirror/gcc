f612c0bfb7a349e6d4b30a6bc26edb22ff9d29ac
Gerald Pfeifer <gerald@gcc.gnu.org>
2007-11-04 20:09:45 +0000

    freebsd-spec.h (FBSD_TARGET_OS_CPP_BUILTINS): Fix broken else-if chain.
    	* config/freebsd-spec.h (FBSD_TARGET_OS_CPP_BUILTINS): Fix
    	broken else-if chain.
    
    	* config.gcc: Proactively add FreeBSD 10 and FreeBSD 11.
    	* config/freebsd-spec.h (FBSD_TARGET_OS_CPP_BUILTINS): Ditto.
    
    From-SVN: r129887

--- gcc/config/freebsd-spec.h
+++ gcc/config/freebsd-spec.h
@@ -51,11 +51,15 @@ Boston, MA 02110-1301, USA.  */
 #define FBSD_TARGET_OS_CPP_BUILTINS()					\
   do									\
     {									\
-	if (FBSD_MAJOR == 9)						\
-	  builtin_define ("__FreeBSD__=9");			       	\
+	if (FBSD_MAJOR == 11)						\
+	  builtin_define ("__FreeBSD__=11");				\
+	else if (FBSD_MAJOR == 10)					\
+	  builtin_define ("__FreeBSD__=10");				\
+	else if (FBSD_MAJOR == 9)					\
+	  builtin_define ("__FreeBSD__=9");				\
 	else if (FBSD_MAJOR == 8)					\
 	  builtin_define ("__FreeBSD__=8");			       	\
-	if (FBSD_MAJOR == 7)						\
+	else if (FBSD_MAJOR == 7)                                       \
 	  builtin_define ("__FreeBSD__=7");			       	\
 	else if (FBSD_MAJOR == 6)					\
 	  builtin_define ("__FreeBSD__=6");			       	\
