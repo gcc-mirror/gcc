eeb0ae2330c278624a09d903512153cef5cab34d
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-11-07 20:48:08 +0000

    PR rtl-optimization/33822 	* rtl.h (REG_OFFSET): Fix comment. 	* var-tracking.c (INT_MEM_OFFSET): New macro. 	(var_mem_set): Use it. 	(var_mem_delete_and_set): Likewise. 	(var_mem_delete): Likewise. 	(same_variable_part_p): Likewise. 	(vt_get_decl_and_offset): Likewise. 	(offset_valid_for_tracked_p): New predicate. 	(count_uses): Do not track locations with invalid offsets. 	(add_uses): Likewise. 	(add_stores): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@129970 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/out-of-bounds-1.c
@@ -0,0 +1,13 @@
+/* PR rtl-optimization/33822 */
+/* Origin: Andrew Pinski <pinskia@gcc.gnu.org> */
+
+/* { dg-do compile } */
+/* { dg-options "-O -g" } */
+/* { dg-options "-O -g -mstrict-align" { target powerpc*-*-* } } */
+
+void ProjectOverlay(const float localTextureAxis[2], char *lump)
+{
+   const void *d = &localTextureAxis;
+   int size = sizeof(float)*8 ;
+   __builtin_memcpy( &lump[ 0 ], d, size );  
+}
