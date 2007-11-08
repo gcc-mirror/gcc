17541d72a769ffafcfa9bd84efb74c08a075ff7b
Kenneth Zadeck <zadeck@naturalbridge.com>
2007-11-08 16:45:53 +0000

    [multiple changes]
    2007-11-07  Kenneth Zadeck <zadeck@naturalbridge.com>
    
    	PR middle-end/33826
    	* ipa-pure-const (static_execute): Added code to keep recursive
    	functions from being marked as pure or const.
    	* ipa-utils (searchc): Fixed comment.
    2007-11-08  Kenneth Zadeck <zadeck@naturalbridge.com>
    
    	PR middle-end/33826
    	* gcc.dg/pr33826.c: New.
    	* gcc.dg/tree-ssa/20030714-1.c: Removed two tests that depend on 
    	recursive functions being marked pure or const.
    
    From-SVN: r130006

--- gcc/testsuite/gcc.dg/tree-ssa/20030714-1.c
+++ gcc/testsuite/gcc.dg/tree-ssa/20030714-1.c
@@ -34,13 +34,6 @@ find_base_value (src)
 }
 
 
-/* There should be four IF conditionals.  */
-/* { dg-final { scan-tree-dump-times "if " 4 "dom3"} } */
-
 /* There should be no casts to short unsigned int.  */
 /* { dg-final { scan-tree-dump-times "\\(short unsigned int\\)" 0 "dom3"} } */
 
-/* There should be two loads of ->code.  */
-/* { dg-final { scan-tree-dump-times "->code" 2 "dom3"} } */
-
-/* { dg-final { cleanup-tree-dump "dom3" } } */
