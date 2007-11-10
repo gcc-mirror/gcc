69a837167a2ec4f5c8f7d92c1713561e69a1c6b2
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-11-10 07:40:37 +0000

    PR tree-optimization/33680 	* tree-data-ref.c (split_constant_offset) <case ADDR_EXPR>: Punt 	if the added cast involves variable length types.
    	* gcc.c-torture/compile/20071108-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@130067 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/20071108-1.c
@@ -0,0 +1,13 @@
+/* PR tree-optimization/33680 */
+
+int
+f (int dim, int *b, int *c)
+{
+  int newcentroid[3][dim];
+  int *a = newcentroid[2];
+  int i, dist = 0;
+  __builtin_memcpy (newcentroid, c, sizeof (newcentroid));
+  for (i = 0; i < dim; i++)
+    dist += (a[i] - b[i]) * (a[i] - b[i]);
+  return dist;
+}
