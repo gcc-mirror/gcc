cb8e078d3f0aa2a99eb9d14ba0f38354ffc181a6
Jakub Jelinek <jakub@redhat.com>
2007-11-15 20:02:54 +0100

    re PR middle-end/23848 (stack deallocation can be more efficient)
    	PR middle-end/23848
    	* tree-ssa-ccp.c (optimize_stack_restore): New function.
    	(execute_fold_all_builtins): Call optimize_stack_restore for
    	BUILT_IN_STACK_RESTORE.
    
    	* gcc.dg/tree-ssa/pr23848-1.c: New test.
    	* gcc.dg/tree-ssa/pr23848-2.c: New test.
    	* gcc.dg/tree-ssa/pr23848-3.c: New test.
    	* gcc.dg/tree-ssa/pr23848-4.c: New test.
    
    From-SVN: r130206

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr23848-4.c
@@ -0,0 +1,25 @@
+/* PR middle-end/23848 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+void bar (char *, char *, char *, char *, int);
+void foo (int size)
+{
+  char temp[size];
+  temp[size-1] = '\0';
+  {
+    char temp2[size];
+    {
+      char temp3[size];
+      {
+	char temp4[size];
+	bar (temp, temp2, temp3, temp4, size);
+      }
+    }
+    __asm __volatile ("" : : "r" (&temp[0]), "r" (&temp2[0]) : "memory");
+  }
+}
+
+/* { dg-final { scan-tree-dump-times "__builtin_stack_save" 1 "optimized"} } */
+/* { dg-final { scan-tree-dump-times "__builtin_stack_restore" 1 "optimized"} } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
