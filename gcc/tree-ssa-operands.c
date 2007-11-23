cb2d412c3e3fc246c1e52809ff13d587388f8b5f
Richard Guenther <rguenther@suse.de>
2007-11-23 09:49:29 +0000

    tree-ssa-copy.c (may_propagate_copy): Remove redundant checks.
    2007-11-23  Richard Guenther  <rguenther@suse.de>
    
    	* tree-ssa-copy.c (may_propagate_copy): Remove redundant
    	checks.
    	(merge_alias_info): Do verification only if checking is
    	enabled.  Merge flow-sensitive alias information in simple
    	cases.
    	* tree-ssa-operands.c (get_addr_dereference_operands): Also
    	complain about missing NMTs.
    
    	* gcc.dg/tree-ssa/alias-17.c: New testcase.
    
    From-SVN: r130370

--- gcc/tree-ssa-operands.c
+++ gcc/tree-ssa-operands.c
@@ -1643,16 +1643,18 @@ get_addr_dereference_operands (tree stmt, tree *addr, int flags, tree full_ref,
 	  /* If we are emitting debugging dumps, display a warning if
 	     PTR is an SSA_NAME with no flow-sensitive alias
 	     information.  That means that we may need to compute
-	     aliasing again.  */
+	     aliasing again or that a propagation pass forgot to
+	     update the alias information on the pointers.  */
 	  if (dump_file
 	      && TREE_CODE (ptr) == SSA_NAME
-	      && pi == NULL)
+	      && (pi == NULL
+		  || pi->name_mem_tag == NULL_TREE))
 	    {
 	      fprintf (dump_file,
 		  "NOTE: no flow-sensitive alias info for ");
 	      print_generic_expr (dump_file, ptr, dump_flags);
 	      fprintf (dump_file, " in ");
-	      print_generic_stmt (dump_file, stmt, dump_flags);
+	      print_generic_stmt (dump_file, stmt, 0);
 	    }
 
 	  if (TREE_CODE (ptr) == SSA_NAME)
