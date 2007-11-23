69609004bdc6df041c8a981b4d96dc09d23b9148
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-11-23 13:39:44 +0000

    PR c++/34198 	* c-common.c (conversion_warning): For INTEGER_TYPE to 	INTEGER_TYPE conversions call get_narrower on expr to avoid 	spurious warnings from binop shortening or when the implicit 	conversion can't change the value.
    	* gcc.dg/Wconversion-5.c: New test.
    	* g++.dg/Wconversion3.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@130377 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/Wconversion-5.c
@@ -0,0 +1,35 @@
+/* PR c++/34198 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -Wconversion" } */
+
+signed char sc;
+unsigned char uc;
+short int ss;
+unsigned short int us;
+int si;
+unsigned int ui;
+
+void test1 (void)
+{
+  int a = uc & 0xff;
+  int b = sc & 0x7f;
+  int c = 0xff & uc;
+  int d = 0x7f & sc;
+  int e = uc & sc;
+  unsigned char f = (int) uc;
+  signed char g = (int) sc;
+  unsigned char h = (unsigned int) (short int) uc;
+  signed char i = (int) (unsigned short int) sc;	/* { dg-warning "may alter its value" } */
+  unsigned char j = (unsigned int) (short int) us;	/* { dg-warning "may alter its value" } */
+  signed char k = (int) (unsigned short int) ss;	/* { dg-warning "may alter its value" } */
+}
+
+void test2 (void)
+{
+  signed char a = (unsigned char) sc;		/* { dg-warning "may change the sign" } */
+  unsigned char b = (signed char) uc;		/* { dg-warning "may change the sign" } */
+  signed char c = (int) (unsigned char) sc;	/* { dg-warning "may change the sign" } */
+  unsigned char d = (int) (signed char) uc;	/* { dg-warning "may change the sign" } */
+  int e = (unsigned int) si;			/* { dg-warning "may change the sign" } */
+  unsigned int f = (int) ui;			/* { dg-warning "may change the sign" } */
+}
