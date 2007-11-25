a78f714d74fb9ceef2d29fd4c1fac390481eff5b
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-11-25 09:59:42 +0000

    2007-11-25  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/33499
    	* decl.c (get_proc_name): If ENTRY statement occurs before type
    	specification, set the symbol untyped and ensure that it is in
    	the procedure namespace.
    
    2007-11-25  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/33499
    	* gfortran.dg/entry_16.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@130403 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/entry_16.f90
@@ -0,0 +1,43 @@
+! { dg-do run }
+! Tests the fix for PR33499 in which the ENTRY cx_radc was not
+! getting its TYPE.
+!
+! Contributed by Michael Richmond <michael.a.richmond@nasa.gov>
+!
+MODULE complex
+  IMPLICIT NONE
+  PRIVATE
+  PUBLIC :: cx, OPERATOR(+), OPERATOR(.eq.)
+  TYPE cx
+    integer :: re
+    integer :: im
+  END TYPE cx
+  INTERFACE OPERATOR (+)
+    MODULE PROCEDURE cx_cadr, cx_radc
+  END INTERFACE
+  INTERFACE OPERATOR (.eq.)
+    MODULE PROCEDURE cx_eq
+  END INTERFACE
+  CONTAINS
+  FUNCTION cx_cadr(z, r)
+  ENTRY cx_radc(r, z)
+    TYPE (cx) :: cx_cadr, cx_radc
+    TYPE (cx), INTENT(IN) :: z
+    integer, INTENT(IN) :: r
+    cx_cadr%re = z%re + r
+    cx_cadr%im = z%im
+  END FUNCTION cx_cadr
+  FUNCTION cx_eq(u, v)
+    TYPE (cx), INTENT(IN) :: u, v
+    logical :: cx_eq
+    cx_eq = (u%re .eq. v%re) .and. (u%im .eq. v%im)
+  END FUNCTION cx_eq
+END MODULE complex
+
+  use complex
+  type(cx) :: a = cx (1, 2), c, d
+  logical :: f
+  integer :: b = 3
+  if (.not.((a + b) .eq. (b + a))) call abort ()
+  if (.not.((a + b) .eq. cx (4, 2))) call abort ()
+end
