25b6dd9cde22f34790a17ada051b7b89cbcf1123
Richard Guenther <rguenther@suse.de>
2007-11-27 15:00:37 +0000

    re PR middle-end/34241 (ICE in forward_propagate_into_cond)
    2007-11-27  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/34241
    	* tree-ssa-forwprop.c (forward_propagate_addr_expr): Make sure
    	to only delete real conversion chains.
    
    	* g++.dg/torture/pr34241.C: New testcase.
    
    From-SVN: r130468

---
+++ gcc/testsuite/g++.dg/torture/pr34241.C
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+
+inline void *operator  new (__SIZE_TYPE__, void *__p) throw ()
+{
+  return __p;
+}
+struct A
+{
+  A(int, double);
+  inline explicit A (int pattern, bool cs)
+  {
+    new (this) A (pattern, double(cs));
+  }
+};
+A test ()
+{
+  const A a (42, true);
+}
