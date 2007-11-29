256a5f7b6fd0ef672416ea0bdba679446f38475b
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-11-29 17:41:37 +0000

    2007-11-29  Tobias Burnus  <burnus@net-b.de>
            PR fortran/34248
            * trans-decl.c (generate_dependency_declarations): Check
            for NULL pointers before accessing the string length.
    
    2007-11-29  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/34248
            * gfortran.dg/result_in_spec_3.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@130517 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/result_in_spec_3.f90
@@ -0,0 +1,15 @@
+! { dg-do compile }
+!
+! PR fortran/34248
+!
+! There was an ICE for assumed-length functions
+! if RESULT(...) was used and no value assigned
+! to the result variable.
+!
+character(*) FUNCTION test() RESULT(ctab)
+  ctab = "Hello"
+END function test
+
+FUNCTION test2() RESULT(res)
+  character(*) :: res
+END function test2
