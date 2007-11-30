d5409153d10d746acf72a79dad1417d1bcae67ad
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-11-30 23:21:48 +0000

    2007-11-30  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR libfortran/34291
    	* gfortran.dg/namelist_41.f90: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@130549 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/namelist_41.f90
@@ -0,0 +1,22 @@
+! { dg-do run }
+! PR34291 Segfault on &end in namelist expanded read of character
+  implicit none
+  character(len=10), dimension(2)  :: var
+  namelist /inx/ var
+  var = "goodbye"
+  open(unit=11, status='scratch')
+  write (11, *) "&inx"
+  write (11, *) "var(1)='hello'" 
+  write (11, *) "&end"
+  rewind (11)
+  read(11,nml=inx)
+  if (var(1) /= 'hello' .and. var(2) /= 'goodbye') call abort
+  var = "goodbye"
+  rewind (11)
+  write (11, *) "$inx"
+  write (11, *) "var(1)='hello'" 
+  write (11, *) "$end"
+  rewind (11)
+  read(11,nml=inx)
+  if (var(1) /= 'hello' .and. var(2) /= 'goodbye') call abort
+end
