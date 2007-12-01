867f133ec412ca7592f16b804b34aac9b82703f2
Ollie Wild <aaw@google.com>
2007-12-01 08:56:55 +0000

    re PR c++/8171 (Cannot compare pointer to member function of base and derived class)
    	PR c++/8171
    
    	gcc/cp/
    	* typeck.c (build_binary_op): Add conversion of pointers to function
    	members appearing as operands to the equality operators.
    
    	gcc/testsuite/
    	* g++.dg/conversion/ptrmem9.C: New test.
    
    From-SVN: r130554

---
+++ gcc/testsuite/g++.dg/conversion/ptrmem9.C
@@ -0,0 +1,26 @@
+// Copyright (C) 2007 Free Software Foundation
+// Contributed by Ollie Wild <aaw@google.com>
+// { dg-do compile }
+
+// Test implicit conversion of pointers to member functions appearing as
+// operands of the equality operators.
+
+struct B { };
+
+struct BV { };
+
+struct D : B, virtual BV { };
+
+struct C { };
+
+void f ()
+{
+  void (D::*pd) () = 0;
+  void (B::*pb) () = 0;
+  void (BV::*pbv) () = 0;
+  void (C::*pc) () = 0;
+
+  pd == pb;
+  pd == pbv;  // { dg-error "" }
+  pd == pc;   // { dg-error "" }
+}
