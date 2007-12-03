d2dda7fed054ab18eaabd73adb0ca30b7bcb6605
Jakub Jelinek <jakub@redhat.com>
2007-12-04 00:06:55 +0100

    omp-low.c (lookup_decl_in_outer_ctx): Allow calling this with !ctx->is_nested.
    	* omp-low.c (lookup_decl_in_outer_ctx): Allow calling this
    	with !ctx->is_nested.
    	(maybe_lookup_decl_in_outer_ctx): Look up in outer contexts
    	even if !ctx->is_nested.
    	(lower_copyprivate_clauses, lower_send_clauses,
    	lower_send_shared_vars): Call lookup_decl_in_outer_ctx
    	unconditionally.
    
    	* testsuite/libgomp.c/private-1.c: New test.
    
    From-SVN: r130590

---
+++ libgomp/testsuite/libgomp.c/private-1.c
@@ -0,0 +1,54 @@
+extern void abort (void);
+
+int a = 18;
+
+void
+f1 (int i, int j, int k)
+{
+  int l = 6, m = 7, n = 8;
+#pragma omp parallel private(j, m) shared(k, n) firstprivate(i, l) \
+	    num_threads(1)
+  {
+    j = 6;
+    m = 5;
+    if (++a != 19 || ++i != 9 || j != 6 || ++l != 7 || m != 5 || ++n != 9)
+      #pragma omp atomic
+	k++;
+  }
+  if (a != 19 || i != 8 || j != 26 || k != 0 || l != 6 || m != 7 || n != 9)
+    abort ();
+}
+
+int v1 = 1, v2 = 2, v5 = 5;
+int err;
+
+void
+f2 (void)
+{
+  int v3 = 3;
+#pragma omp sections private (v1) firstprivate (v2)
+  {
+  #pragma omp section
+    {
+      int v4 = 4;
+      v1 = 7;
+      #pragma omp parallel num_threads(1) firstprivate(v1, v2, v3, v4)
+	{
+	  if (++v1 != 8 || ++v2 != 3 || ++v3 != 4 || ++v4 != 5 || ++v5 != 6)
+	    err = 1;
+	}
+      if (v1 != 7 || v2 != 2 || v3 != 3 || v4 != 4 || v5 != 6)
+	abort ();
+      if (err)
+	abort ();
+    }
+  }
+}
+
+int
+main (void)
+{
+  f1 (8, 26, 0);
+  f2 ();
+  return 0;
+}
