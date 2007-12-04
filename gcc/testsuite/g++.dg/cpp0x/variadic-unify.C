db60ff18e2cc35235d436c7e492d8564af44ea9b
Douglas Gregor <doug.gregor@gmail.com>
2007-12-04 19:27:14 +0000

    re PR c++/33091 ([c++0x] ICE using remove_reference on variadic param pack)
    2007-12-04  Douglas Gregor  <doug.gregor@gmail.com>
    
           PR c++/33091
           * pt.c (unify_pack_expansion): If we didn't deduce any actual
           bindings for the template parameter pack, don't try to keep the
           empty deduced arguments.
           (unify): If a parameter is a template-id whose template argument
           list contains a pack expansion that is not at the end, then we
           cannot unify against that template-id.
    
    2007-12-04  Douglas Gregor  <doug.gregor@gmail.com>
    
           PR c++/33091
           * g++.dg/cpp0x/variadic-unify.C: New.
    
    From-SVN: r130604

---
+++ gcc/testsuite/g++.dg/cpp0x/variadic-unify.C
@@ -0,0 +1,15 @@
+// { dg-options "-std=c++0x" }
+template<typename...> struct tuple { };
+
+template<typename... Args1, typename... Args2>
+void foo(tuple<Args1..., Args2...>, tuple<Args1...>, tuple<Args2...>);
+
+struct X{ };
+
+void bar()
+{
+  tuple<int, float> tif;
+  tuple<double, X> tdx;
+  tuple<int, float, double, X> tall;
+  foo(tall, tif, tdx);
+}
