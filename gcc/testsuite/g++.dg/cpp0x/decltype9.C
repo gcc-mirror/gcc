91929b4dee6788fdfbaee519272b8d027b3d47ba
Jakub Jelinek <jakub@redhat.com>
2007-12-05 11:45:21 +0100

    re PR c++/34271 (ICE in invalid initialization of static template member)
    	PR c++/34271
    	* semantics.c (finish_decltype_type): For SCOPE_REF issue an
    	error instead of assertion failure.
    	* parser.c (cp_parser_decltype): If closing paren is not found,
    	return error_mark_node.
    
    	* g++.dg/cpp0x/decltype9.C: New test.
    	* g++.dg/cpp0x/decltype10.C: New test.
    
    From-SVN: r130619

---
+++ gcc/testsuite/g++.dg/cpp0x/decltype9.C
@@ -0,0 +1,10 @@
+// PR c++/34271
+// { dg-do compile }
+// { dg-options "-std=c++0x" }
+
+template<int> struct A
+{
+  static int i;
+};
+
+template<int N> int A<N>::i(decltype (A::i));	// { dg-error "member function|must be an expression" }
