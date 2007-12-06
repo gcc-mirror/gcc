6af5d898a5aa667e20681fb5284394ad77c2dc68
Sebastian Pop <sebastian.pop@amd.com>
2007-12-06 19:32:21 +0000

    ltrans-7.f90: New.
    2007-12-06  Sebastian Pop  <sebastian.pop@amd.com>
    
    	* gfortran.dg/ltrans-7.f90: New.
    
    From-SVN: r130658

---
+++ gcc/testsuite/gfortran.dg/ltrans-7.f90
@@ -0,0 +1,30 @@
+! { dg-do compile }
+! { dg-options "-O2 -ftree-loop-linear -fdump-tree-ltrans-all" }
+
+Program FOO
+  IMPLICIT INTEGER	(I-N)
+  IMPLICIT REAL*8	(A-H, O-Z)
+  PARAMETER (N1=1335, N2=1335)
+  COMMON U(N1,N2), V(N1,N2), P(N1,N2)
+
+  PC = 0.0D0
+  UC = 0.0D0
+  VC = 0.0D0
+
+  do I = 1, M
+     do J = 1, M
+        PC = PC + abs(P(I,J))
+        UC = UC + abs(U(I,J))
+        VC = VC + abs(V(I,J))
+     end do
+     U(I,I) = U(I,I) * ( mod (I, 100) /100.)
+  end do
+
+  write(6,366) PC, UC, VC
+366  format(/, ' PC = ',E12.4,/,' UC = ',E12.4,/,' VC = ',E12.4,/)
+
+end Program FOO
+
+! Please do not XFAIL.
+! { dg-final { scan-tree-dump-times "transformed loop" 1 "ltrans"} }
+! { dg-final { cleanup-tree-dump "ltrans" } }
