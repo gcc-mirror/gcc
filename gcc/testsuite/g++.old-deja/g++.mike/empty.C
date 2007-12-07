8296db69ad314576f82c3b5628f9f503f57df161
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-12-07 20:12:10 +0000

    * g++.old-deja/g++.mike/empty.C: Remove 2 xfails.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@130694 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.mike/empty.C
+++ gcc/testsuite/g++.old-deja/g++.mike/empty.C
@@ -16,8 +16,8 @@ void foo() {
   while (1) ;
   for (;;) ;
   /* These two work when using mapped locations */
-  while (1) NOPE; /* { dg-bogus "suggest a space before " "suggest" { xfail *-*-* } } */
-  for (;;) NOPE; /* { dg-bogus "suggest a space before " "suggest" { xfail *-*-* } } */
+  while (1) NOPE; /* { dg-bogus "suggest a space before " "suggest" } */
+  for (;;) NOPE; /* { dg-bogus "suggest a space before " "suggest" } */
   while (1)
     NOPE;
   for (;;)
