be53c6d4abca9b31e58f7c90ffd5071896d76a48
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-12-10 22:49:43 +0000

    PR tree-optimization/34371 	* tree-vectorizer.h (struct _loop_vec_info): Add num_iters_unchanged 	field. 	(LOOP_VINFO_NITERS_UNCHANGED): Define to num_iters_unchanged field. 	* tree-vectorizer.c (set_prologue_iterations, 	slpeel_tree_peel_loop_to_edge): Call unshare_expr on 	LOOP_VINFO_NITERS_UNCHANGED before using it.
    	* gcc.c-torture/compile/20071207-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@130755 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/20071207-1.c
@@ -0,0 +1,14 @@
+/* PR tree-optimization/34371 */
+/* Testcase by Martin Michlmayr <tbm@cyrius.com> */
+
+void centerln (int width, int ch, char *s)
+{
+  unsigned int sidebar;
+  int i;
+  char linet1[1000];
+  char linet2[100];
+  sidebar = (width - __builtin_strlen (s)) / 2;
+  for (i = 0; i < sidebar; i++)
+    linet2[i] = ch;
+  __builtin_strcpy (linet1, linet2);
+}
