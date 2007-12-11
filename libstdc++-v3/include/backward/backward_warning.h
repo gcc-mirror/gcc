e623a9883fa3e456bf3f5564277c7bd78a3cdacd
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-12-11 21:48:16 +0000

    2007-12-11  Benjamin Kosnik  <bkoz@redhat.com>
    	PR libstdc++/34015
    	* include/backward/backward_warning.h: Adjust warning message.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@130778 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/backward/backward_warning.h
+++ libstdc++-v3/include/backward/backward_warning.h
@@ -29,26 +29,32 @@
 #define _BACKWARD_BACKWARD_WARNING_H 1
 
 #ifdef __DEPRECATED
-#warning This file includes at least one deprecated or antiquated header. \
-  Please consider use of an equivalent, non-deprecated interface for the  \
-  requested functionality. A list of valid replacements is as follows:	  \
-                                                                          \
-  Use:					Instead of:                       \
-  <sstream>, basic_stringbuf	   	<strstream>, strstreambuf	  \
-  <sstream>, basic_istringstream	<strstream>, istrstream		  \
-  <sstream>, basic_ostringstream	<strstream>, ostrstream		  \
-  <sstream>, basic_stringstream		<strstream>, strstream		  \
-  <unordered_set>, unordered_set       	<ext/hash_set>, hash_set	  \
-  <unordered_set>, unordered_multiset	<ext/hash_set>, hash_multiset	  \
-  <unordered_map>, unordered_map	<ext/hash_set>, hash_map	  \
-  <unordered_map>, unordered_multimap	<ext/hash_set>, hash_multimap	  \
-  <functional>, bind			<functional>, binder1st	  	  \
-  <functional>, bind			<functional>, binder2nd	  	  \
-  <functional>, bind			<functional>, bind1st	  	  \
-  <functional>, bind			<functional>, bind2nd	  	  \
-  <memory>, unique_ptr       		<memory>, auto_ptr	  	  \
-                                                                          \
-  To disable this warning use -Wno-deprecated.
+#warning \
+  This file includes at least one deprecated or antiquated header which \
+  may be removed without further notice at a future date. Please use a \
+  non-deprecated interface with equivalent functionality instead. For a \
+  listing of replacement headers and interfaces, consult the file \
+  backward_warning.h. To disable this warning use -Wno-deprecated.
+
+/*
+  A list of valid replacements is as follows:
+
+  Use:					Instead of:
+  <sstream>, basic_stringbuf	   	<strstream>, strstreambuf
+  <sstream>, basic_istringstream	<strstream>, istrstream
+  <sstream>, basic_ostringstream	<strstream>, ostrstream
+  <sstream>, basic_stringstream		<strstream>, strstream
+  <unordered_set>, unordered_set     	<ext/hash_set>, hash_set
+  <unordered_set>, unordered_multiset	<ext/hash_set>, hash_multiset
+  <unordered_map>, unordered_map	<ext/hash_set>, hash_map
+  <unordered_map>, unordered_multimap	<ext/hash_set>, hash_multimap
+  <functional>, bind			<functional>, binder1st
+  <functional>, bind			<functional>, binder2nd
+  <functional>, bind			<functional>, bind1st
+  <functional>, bind			<functional>, bind2nd
+  <memory>, unique_ptr       		<memory>, auto_ptr
+*/
+
 #endif
 
 #endif
