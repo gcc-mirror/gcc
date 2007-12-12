96ee3a4acd31befaabea989a2ceaa2225990519b
Tobias Burnus <burnus@net-b.de>
2007-12-12 19:54:26 +0100

    re PR fortran/34254 ("character(kind=c_char) function" fails if c_char is not host associated)
    2007-12-12  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/34254
            * decl.c (match_char_kind): Support use-associated/imported
            kind parameters.
            (gfc_match_kind_spec): Support als BT_CHARACTER, when
            re-scanning kind spec.
    
    2007-12-12  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/34254
            * gfortran.dg/function_kinds_3.f90: New.
    
    From-SVN: r130793

---
+++ gcc/testsuite/gfortran.dg/function_kinds_3.f90
@@ -0,0 +1,31 @@
+! { dg-do compile }
+!
+! PR fortran/34254
+!
+! The character-kind parameter was not accepted.
+!
+module m
+  integer, parameter :: char_t = kind('a')
+end module m
+
+character(1,char_t) function test1()
+  use m
+  test1 = 'A'
+end function test1
+
+character(len=1,kind=char_t) function test2()
+  use m
+  test2 = 'A'
+end function test2
+
+character(kind=char_t,len=1) function test3()
+  use m
+  test3 = 'A'
+end function test3
+
+character(1,kind=char_t) function test4()
+  use m
+  test4 = 'A'
+end function test4
+
+! { dg-final { cleanup-modules "m" } }
