a70770d2d5760af79dd42b8d36b6c10ea2d97053
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-12-13 21:49:09 +0000

    PR middle-end/33088 	* gimplify.c (gimplify_modify_expr_complex_part): Add note to comment. 	* tree-complex.c (init_dont_simulate_again): Return true if there are 	uninitialized loads generated by gimplify_modify_expr_complex_part. 	* tree-gimple.c (is_gimple_reg_type): Return false for complex types 	if not optimizing. 	* tree-ssa.c (ssa_undefined_value_p): New predicate extracted from... 	(warn_uninit): ...here.  Use ssa_undefined_value_p. 	* tree-ssa-pre.c (is_undefined_value): Delete. 	(phi_translate_1): Use ssa_undefined_value_p. 	(add_to_exp_gen): Likewise. 	(make_values_for_stmt): Likewise. 	* tree-flow.h (ssa_undefined_value_p): Declare.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@130917 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/complex-5.c
@@ -0,0 +1,55 @@
+/* PR middle-end/33088 */
+/* Origin: Joseph S. Myers <jsm28@gcc.gnu.org> */
+
+/* { dg-do run { target i?86-*-linux* x86_64-*-linux* } } */
+/* { dg-options "-std=c99 -O -ffloat-store -lm" } */
+
+#include <fenv.h>
+#include <stdlib.h>
+
+volatile int x[1024];
+
+void __attribute__((noinline))
+fill_stack (void)
+{
+  volatile int y[1024];
+  int i;
+  for (i = 0; i < 1024; i++)
+    y[i] = 0x7ff00000;
+  for (i = 0; i < 1024; i++)
+    x[i] = y[i];
+}
+
+volatile _Complex double vc;
+
+void __attribute__((noinline))
+use_complex (_Complex double c)
+{
+  vc = c;
+}
+
+double t0, t1, t2, t3;
+
+#define USE_COMPLEX(X, R, C) \
+  do { __real__ X = R; __imag__ X = C; use_complex (X); } while (0)
+
+void __attribute__((noinline))
+use_stack (void)
+{
+  _Complex double a, b, c, d;
+  USE_COMPLEX (a, t0, t1);
+  USE_COMPLEX (b, t1, t2);
+  USE_COMPLEX (c, t2, t3);
+  USE_COMPLEX (d, t3, t0);
+}
+
+int
+main (void)
+{
+  fill_stack ();
+  feclearexcept (FE_INVALID);
+  use_stack ();
+  if (fetestexcept (FE_INVALID))
+    abort ();
+  exit (0);
+}
