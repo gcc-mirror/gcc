77c8bed2a03447272cb01064921d613c31c51e7f
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-12-17 14:16:42 +0000

    PR c/34506 	* c-parser.c (c_parser_omp_all_clauses): Accept optional comma 	in between clauses.
    	* parser.c (cp_parser_omp_all_clauses): Accept optional comma
    	in between clauses.
    
    	* gcc.dg/gomp/clause-2.c: New test.
    	* g++.dg/gomp/clause-4.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131008 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/gomp/clause-2.c
@@ -0,0 +1,23 @@
+/* PR c/34506 */
+/* { dg-do compile } */
+
+#define p parallel
+
+void
+foo (int x)
+{
+#pragma omp p num_threads (4) if (1) private (x)
+    ;
+#pragma omp p num_threads(4)if(1)private(x)
+    ;
+#pragma omp p num_threads (4), if (1) , private (x)
+    ;
+#pragma omp p num_threads(4),if(1),private(x)
+    ;
+#pragma omp p, num_threads (4), if (1), private (x)	/* { dg-error "clause before" } */
+    ;
+#pragma omp p num_threads (4), if (1), private (x),	/* { dg-error "clause before" } */
+    ;
+#pragma omp p num_threads (4), , if (1), private (x)	/* { dg-error "clause before" } */
+    ;
+}
