ffeb38bce907f5d15bc9bb96b07bfdb5263f6051
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2007-12-17 00:51:25 +0000

    re PR libfortran/34427 (Revision 130708 breaks namelist input)
    2007-12-16  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR fortran/34427
    	* gfortran.dg/namelist_42.f90: Update.
    	* gfortran.dg/namelist_43.f90: New.
    
    From-SVN: r131004

--- gcc/testsuite/gfortran.dg/namelist_42.f90
+++ gcc/testsuite/gfortran.dg/namelist_42.f90
@@ -20,9 +20,22 @@
   write (10,*) " &nl foo = 5, 5, 5, nan, infinity, infinity "
   write (10,*)
   write (10,*) "      = 1, /"
-! Does not work
-  !write (10,*) " &nl foo = 5, 5, 5, nan, infinity, infinity"
-  !write (10,*) "      = 1, /"
+  rewind (10)
+  READ (10, NML = nl)
+  close (10)
+
+  if(infinity /= 1) call abort()
+  if(any(foo(1:3) /= [5.0, 5.0, 5.0]) .or. .not.isnan(foo(4)) &
+     .or. foo(5) <= huge(foo) .or. any(foo(6:11) /= -1.0)) &
+    call abort()
+! Works too:
+  foo = -1.0
+  infinity = -1
+
+  open (10, status="scratch")
+  rewind (10)
+  write (10,'(a)') "&nl foo = 5, 5, 5, nan, infinity, infinity"
+  write (10,'(a)') "=1,/"
   rewind (10)
   READ (10, NML = nl)
   CLOSE (10)
