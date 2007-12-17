cf3d20e8d8be52f465fbea58fc86743c1b257040
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-12-17 00:51:25 +0000

    2007-12-16  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR fortran/34427
    	* gfortran.dg/namelist_42.f90: Update.
    	* gfortran.dg/namelist_43.f90: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131004 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/namelist_43.f90
@@ -0,0 +1,46 @@
+! { dg-do run }
+! { dg-options "-mieee" { target sh*-*-* } }
+!
+! PR fortran/34427
+!
+! Check that namelists and the real values Inf, NaN, Infinity
+! properly coexist with interceding line ends and spaces.
+!
+PROGRAM TEST
+  IMPLICIT NONE
+  real , DIMENSION(10) ::foo 
+  integer :: infinity
+  integer :: numb
+  NAMELIST /nl/ foo
+  NAMELIST /nl/ infinity
+  foo = -1.0
+  infinity = -1
+
+  open (10, status="scratch")
+
+  write (10,'(a)') " &nl foo(1:6) = 5, 5, 5, nan, infinity"
+  write (10,'(a)')
+  write (10,'(a)')
+  write (10,'(a)')
+  write (10,'(a)')
+  write (10,'(a)') "infinity"
+  write (10,'(a)')
+  write (10,'(a)')
+  write (10,'(a)') "         "
+  write (10,'(a)')
+  write (10,'(a)')
+  write (10,'(a)')
+  write (10,'(a)')
+  write (10,'(a)')
+  write (10,'(a)')
+  write (10,'(a)')
+  write (10,'(a)')
+  write (10,'(a)') "=1/"
+  rewind (10)
+  READ (10, NML = nl)
+  CLOSE (10)
+  if(infinity /= 1) call abort
+  if(any(foo(1:3) /= [5.0, 5.0, 5.0]) .or. .not.isnan(foo(4)) &
+     .or. (foo(5) <= huge(foo)) .or. any(foo(6:10) /= -1.0)) &
+    call abort
+END PROGRAM TEST
