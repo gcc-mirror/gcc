eef75f5ec020e74ff2c9a90ffed32427abfba837
Richard Sandiford <rsandifo@gcc.gnu.org>
2007-12-18 07:40:17 +0000

    200x-xx-xx Kaz Kylheku <kaz@zeugmasystems.com>
    gcc/
    200x-xx-xx  Kaz Kylheku  <kaz@zeugmasystems.com>
    
    	PR rtl-optimization/34456
    	* resource.c (mark_set_resources): Use regs_invalidated_by_call
    	rather than call_used_regs and global_regs.
    
    gcc/testsuite/
    	PR rtl-optimization/34456
    	* gcc.c-torture/execute/pr34456.c: New test.
    
    From-SVN: r131033

---
+++ gcc/testsuite/gcc.c-torture/execute/pr34456.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+int __attribute__ ((noinline)) debug (void) { return 1; }
+int errors;
+
+struct s { int elt; int (*compare) (int); };
+
+static int
+compare (const void *x, const void *y)
+{
+  const struct s *s1 = x, *s2 = y;
+  int (*compare1) (int);
+  int elt2;
+
+  compare1 = s1->compare;
+  elt2 = s2->elt;
+  if (elt2 != 0 && debug () && compare1 (s1->elt) != 0)
+    errors++;
+  return compare1 (elt2);
+}
+
+int bad_compare (int x) { return -x; }
+struct s array[2] = { { 1, bad_compare }, { -1, bad_compare } };
+
+int
+main (void)
+{
+  qsort (array, 2, sizeof (struct s), compare);
+  return errors == 0;
+}
