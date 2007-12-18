4cf36211354241794588bab658e807ac550dc9de
Douglas Gregor <doug.gregor@gmail.com>
2007-12-18 21:19:41 +0000

    re PR c++/32565 (ICE with specialization of variadic template)
    2007-12-18  Douglas Gregor  <doug.gregor@gmail.com>
                Jakub Jelinek  <jakub@redhat.com>
    	
    	PR c++/32565
    	PR c++/33943
    	PR c++/33965
    	* pt.c (template_template_parm_bindings_ok_p): New; verifies
    	bindings of template template parameters after all template
    	arguments have been deduced.
    	(coerce_template_parms): Don't complain when COMPLAIN doesn't
    	include tf_error.
    	(fn_type_unification): Use template_template_parm_bindings_ok_p. 
    	(unify): Deal with variadic, bound template template parameters. 
    	(get_class_bindings): Use template_template_parm_bindings_ok_p. 	
    	
    2007-12-18  Douglas Gregor  <doug.gregor@gmail.com>
    	    Jakub Jelinek  <jakub@redhat.com>
    	
    	PR c++/32565
    	PR c++/33943
    	PR c++/33965
    	* g++.dg/cpp0x/variadic86.C: New.
    	* g++.dg/cpp0x/variadic87.C: New.
    	* g++.dg/cpp0x/variadic84.C: New.
    	* g++.dg/cpp0x/variadic85.C: New.
    	* g++.dg/template/ttp25.C: New.
    	
    	
    
    Co-Authored-By: Jakub Jelinek <jakub@redhat.com>
    
    From-SVN: r131041

---
+++ gcc/testsuite/g++.dg/template/ttp25.C
@@ -0,0 +1,26 @@
+// { dg-do compile }
+template<typename T, template<T> class C>
+void f1(T, C<5>);
+
+template<typename T, template<T> class C>
+void f2(C<5>, T);
+
+template<typename T, template<T> class C>
+void f3(C<5>, T);
+
+template<typename T> struct metafun { typedef T type; };
+
+template<> struct metafun<short> { typedef int type; };
+
+template<typename T, template<typename metafun<T>::type> class C>
+void f4(T, C<5>);
+
+template<int N> struct X {};
+void g() {
+  f1(5l, X<5>()); // { dg-error "no matching" }
+  f2(X<5>(), 5);
+  f3(X<5>(), 5l); // { dg-error "no matching" }
+  f4(5, X<5>());
+  f4(5l, X<5>()); // { dg-error "no matching" }
+  f4((short)5, X<5>());
+}
