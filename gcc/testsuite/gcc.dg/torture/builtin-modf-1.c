e6eb0797924766aa07a2afd6aa48b2a43704ef5a
Janis Johnson <janis187@us.ibm.com>
2007-12-18 22:50:56 +0000

    * gcc.dg/torture/builtin-modf-1.c: Remove -funsafe-math-optimizations.
    From-SVN: r131045

--- gcc/testsuite/gcc.dg/torture/builtin-modf-1.c
+++ gcc/testsuite/gcc.dg/torture/builtin-modf-1.c
@@ -7,9 +7,6 @@
 
 /* { dg-do link } */
 /* { dg-options "-fno-finite-math-only" { target sh*-*-* } } */
-/* In order to fold algebraic exprs below, targets with "composite"
-   floating point formats need -funsafe-math-optimizations.  */
-/* { dg-options "-funsafe-math-optimizations" { target mips*-*-irix6* powerpc*-*-* } } */
 
 extern void link_error(int);
 
