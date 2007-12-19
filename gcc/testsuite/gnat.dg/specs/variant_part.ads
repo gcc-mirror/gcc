5ce4352251de3720974920752afebf51a63e2ecd
Arnaud Charlet <charlet@gcc.gnu.org>
2007-12-19 17:27:18 +0100

    Update error message.
    From-SVN: r131087

--- gcc/testsuite/gnat.dg/specs/variant_part.ads
+++ gcc/testsuite/gnat.dg/specs/variant_part.ads
@@ -1,7 +1,7 @@
 -- { dg-do compile }
 package Variant_Part is
    type T1(b: boolean) is record
-     case (b) is              -- { dg-error "discriminant name expected" }
+     case (b) is    -- { dg-error "discriminant name may not be parenthesized" }
         when others => null;
      end case;
    end record;
