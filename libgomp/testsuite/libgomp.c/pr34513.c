355efab120dc300ab2d52080920aee3988d93ae4
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-12-19 12:58:32 +0000

    PR c++/34513 	* parser.c (cp_parser_omp_parallel): For non-combined parallel 	call cp_parser_statement rather than 	cp_parser_already_scoped_statement.
    	* testsuite/libgomp.c/pr34513.c: New test.
    	* testsuite/libgomp.c++/pr34513.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131059 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libgomp/testsuite/libgomp.c/pr34513.c
@@ -0,0 +1,33 @@
+/* PR c++/34513 */
+/* { dg-do run } */
+
+#include <omp.h>
+
+extern void abort ();
+
+static int errors = 0;
+static int thrs = 4;
+
+int
+main ()
+{
+  omp_set_dynamic (0);
+
+  #pragma omp parallel num_threads (thrs)
+  {
+    static int shrd = 0;
+
+    #pragma omp atomic
+      shrd += 1;
+
+    #pragma omp barrier
+
+    if (shrd != thrs)
+      #pragma omp atomic
+	errors += 1;
+  }
+
+  if (errors)
+    abort ();
+  return 0;
+}
