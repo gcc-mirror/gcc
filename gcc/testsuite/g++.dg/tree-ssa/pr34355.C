8a171a5948d2c67775cb88046c224eb663c55fdf
Zdenek Dvorak <ook@ucw.cz>
2007-12-19 16:01:19 +0100

    re PR tree-optimization/34355 (ICE: invariant not recomputed when ADDR_EXPR changed with -ftree-parallelize-loops)
    	PR tree-optimization/34355
    	* tree-parloops.c (take_address_of): Handle expresions
    	instead of just variables.
    	(eliminate_local_variables_1): Force whole invariant
    	address to ssa name.
    
    	* g++.dg/tree-ssa/pr34355.C: New test.
    
    From-SVN: r131063

---
+++ gcc/testsuite/g++.dg/tree-ssa/pr34355.C
@@ -0,0 +1,26 @@
+// { dg-do compile }
+// { dg-options "-O3 -ftree-parallelize-loops=4" }
+
+typedef double EXPRESS[5];
+
+extern int Terms;
+
+void Parse_Rel_Factor (EXPRESS Express, int *Terms)
+{
+  EXPRESS Local_Express = {5.0, 4.0, 3.0, 2.0, 1.0};
+  int Local_Terms = 5;
+
+  int i;
+
+  for (i = (*Terms); i < Local_Terms; i++)
+    Express[i] = 0.0;
+
+  Express[i] += Local_Express[i];
+}
+
+double Parse_Float ()
+{
+  EXPRESS Express = {1.0, 2.0, 3.0, 4.0, 5.0};
+
+  Parse_Rel_Factor (Express, &Terms);
+}
