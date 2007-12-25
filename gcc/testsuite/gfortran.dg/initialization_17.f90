13aebeb002a570fa6148d687d96701b2f14ac094
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-12-25 12:05:23 +0000

    2007-12-25  Tobias Burnus  <burnus@net-b.de>
            PR fortran/34514
            * decl.c (attr_decl1): Reject specifying the DIMENSION for
            already initialized variable.
            (do_parm): Reject PARAMETER for already initialized variable.
    
    2007-12-25  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/34514
            * gfortran.dg/initialization_17.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131169 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/initialization_17.f90
@@ -0,0 +1,10 @@
+! { dg-do compile }
+!
+! PR fortran/34514
+!
+! Initialization and typespec changes.
+!
+integer :: n = 5, m = 7
+parameter (n = 42) ! { dg-error "Initializing already initialized variable" }
+dimension :: m(3)  ! { dg-error "after its initialisation" } 
+end
