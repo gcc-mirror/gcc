5b7a9c2e7d8b68a0e6f71992d28bb565ea9b0d22
sam <sam@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-12-27 13:04:14 +0000

    gcc/ada/ 	PR ada/34553 	* adaint.c (__gnat_open_new_temp, __gnat_tmp_name): Use mkstemp() 	instead of mktemp() or tmpnam() on NetBSD.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131201 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/adaint.c
+++ gcc/ada/adaint.c
@@ -887,7 +887,8 @@ __gnat_open_new_temp (char *path, int fmode)
 
   strcpy (path, "GNAT-XXXXXX");
 
-#if (defined (__FreeBSD__) || defined (linux)) && !defined (__vxworks)
+#if (defined (__FreeBSD__) || defined (__NetBSD__) || defined (linux)) && \
+  !defined (__vxworks)
   return mkstemp (path);
 #elif defined (__Lynx__)
   mktemp (path);
@@ -979,7 +980,7 @@ __gnat_tmp_name (char *tmp_filename)
     free (pname);
   }
 
-#elif defined (linux) || defined (__FreeBSD__)
+#elif defined (linux) || defined (__FreeBSD__) || defined (__NetBSD__)
 #define MAX_SAFE_PATH 1000
   char *tmpdir = getenv ("TMPDIR");
 
