afb83f81dccd7e7ed2e65cf55a5b8a9105c98a09
dorit <dorit@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-12-27 19:14:17 +0000

    PR tree-optimization/34591         * tree-vect-trasnform.c (vect_estimate_min_profitable_iters): Skip         stmts (including reduction stmts) that are not live.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131206 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/pr34591.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+
+int av_resample(int filter_length, short *src, short *filter)
+{
+    int i;
+    int val=0;
+    for(i=0; i<filter_length; i++)
+      val += src[ i ] * filter[i];
+    return val;
+}
+
+/* { dg-final { cleanup-tree-dump "vect" } } */
