d5bf8d02e7b67de803855bed4366a15a026b380a
tkoenig <tkoenig@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-12-27 12:33:52 +0000

    2007-12-27  Thomas Koenig  <tkoenig@gcc.gnu.org>
    	PR libfortran/34594
    	* runtime/error.c: If there was a previous error, don't
    	mask it with another error mesage, EOF or EOR condition.
    
    2007-12-27  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR libfortran/34594
    	* gfortran.dg/direct_io_8.f90:  New test case.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131199 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgfortran/runtime/error.c
+++ libgfortran/runtime/error.c
@@ -410,6 +410,13 @@ translate_error (int code)
 void
 generate_error (st_parameter_common *cmp, int family, const char *message)
 {
+
+  /* If there was a previous error, don't mask it with another
+     error message, EOF or EOR condition.  */
+
+  if ((cmp->flags & IOPARM_LIBRETURN_MASK) == IOPARM_LIBRETURN_ERROR)
+    return;
+
   /* Set the error status.  */
   if ((cmp->flags & IOPARM_HAS_IOSTAT))
     *cmp->iostat = (family == LIBERROR_OS) ? errno : family;
