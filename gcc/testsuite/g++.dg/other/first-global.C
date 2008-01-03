8c7cc333be628a708bac4b59d1745c2c91bdcdc3
John David Anglin <dave.anglin@nrc-cnrc.gc.ca>
2008-01-03 02:33:12 +0000

    re PR middle-end/34562 (FAIL: g++.dg/other/first-global.C scan-assembler _GLOBAL__I_foobar)
    	PR middle-end/34562
    	* g++.dg/other/first-global.C: Also accept _GLOBAL__I_65535_0_foobar in
    	scan-assembler check.
    
    From-SVN: r131274

--- gcc/testsuite/g++.dg/other/first-global.C
+++ gcc/testsuite/g++.dg/other/first-global.C
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-fpie" { target *-*-darwin* } } */
-/* { dg-final { scan-assembler "_GLOBAL__I_foobar" } } */
+/* { dg-final { scan-assembler "_GLOBAL__I(_|_65535_0_)foobar" } } */
 
 struct foo { foo (); };
 foo foobar;
