5989df83be8402add1f7cad75ee98e7a8ac2dbda
Paul Thomas <pault@gcc.gnu.org>
2008-01-06 22:00:00 +0000

    re PR fortran/34545 (ICE when compiling Fortran 95 code)
    2008-01-06  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/34545
    	* module.c (load_needed): If the namespace has no proc_name
    	give it the module symbol.
    
    2008-01-06  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/34545
    	* gfortran.dg/use_12.f90: New test.
    
    From-SVN: r131364

---
+++ gcc/testsuite/gfortran.dg/use_12.f90
@@ -0,0 +1,25 @@
+! { dg-do compile }
+! Tests the fix of PR34545, in which the 'numclusters' that determines the size
+! of fnres was not properly associated.
+!
+! Reported by Jon D. Richards <jon_d_r@msn.com>
+!
+module m1
+  integer :: numclusters = 2
+end module m1
+
+module m2
+  contains
+    function get_nfirst( ) result(fnres)
+      use m1, only: numclusters
+      real :: fnres(numclusters)   ! change to REAL and it works!!  
+    end function get_nfirst
+end module m2
+
+program kmeans_driver
+   use m1
+   use m2
+   integer :: nfirst(3)
+   nfirst(1:numclusters) = get_nfirst( )
+end program kmeans_driver
+! { dg-final { cleanup-modules "m1 m2" } }
