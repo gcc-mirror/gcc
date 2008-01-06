02c8c8d3c8688a4b10ec5541268e436fa13a0753
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-01-06 19:36:44 +0000

    2008-01-06  Tobias Burnus  <burnus@net-b.de>
           PR fortran/34689
           * interface.c (compare_actual_formal): Fix intent(out) check for
           function result variables.
    
    2008-01-06  Tobias Burnus  <burnus@net-b.de>
    
           PR fortran/34689
           * gfortran.dg/intent_out_4.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131359 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/intent_out_4.f90
@@ -0,0 +1,18 @@
+! { dg-do compile }
+!
+! PR fortran/34689
+!
+! The following (cf. libgomp.fortran/appendix-a/a.33.3.f90)
+! was rejected because the intent check missed a FL_FUNCTION
+! for the result variable.
+!
+function test()
+  implicit none
+  integer :: test
+  interface
+    subroutine foo(a)
+      integer(inout) :: a
+    end subroutine foo
+  end interface
+  call foo(a)
+end function test
