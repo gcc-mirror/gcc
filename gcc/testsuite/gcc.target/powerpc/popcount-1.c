5252f8477ff691bf2b3625d99629fcb9c4b1e12b
Jack Howarth <howarth@bromo.med.uc.edu>
2008-01-07 21:02:24 +0000

    re PR testsuite/34575 (gcc.target/powerpc/parity-1.c and popcount-1.c scan-assembler popcntb on darwin9)
    2008-01-07  Jack Howarth  <howarth@bromo.med.uc.edu>
    
    	PR testsuite/34575
    	* gcc.target/powerpc/popcount-1.c: Skip on darwin.
    	* gcc.target/powerpc/parity-1.c: Likewise.
    
    From-SVN: r131383

--- gcc/testsuite/gcc.target/powerpc/popcount-1.c
+++ gcc/testsuite/gcc.target/powerpc/popcount-1.c
@@ -1,4 +1,5 @@
 /* { dg-do compile { target { ilp32 } } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
 /* { dg-options "-O2 -mcpu=power5" } */
 /* { dg-final { scan-assembler "popcntb" } } */
 /* { dg-final { scan-assembler-not "mullw" } } */
