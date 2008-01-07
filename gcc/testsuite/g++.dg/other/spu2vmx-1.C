6dd116bef6b4bf39ac8cce9e2e6e33c54d4a8a38
Janis Johnson <janis187@us.ibm.com>
2008-01-07 21:05:03 +0000

    target-supports.exp (check_effective_target_powerpc_spu): New.
    	* lib/target-supports.exp (check_effective_target_powerpc_spu): New.
    	* g++.dg/other/spu2vmx-1.C: Use it.
    
    From-SVN: r131385

--- gcc/testsuite/g++.dg/other/spu2vmx-1.C
+++ gcc/testsuite/g++.dg/other/spu2vmx-1.C
@@ -1,5 +1,5 @@
 /* { dg-do compile { target powerpc*-*-* } } */
-/* { dg-require-effective-target powerpc_altivec_ok } */
+/* { dg-require-effective-target powerpc_spu } */
 /* { dg-options "-maltivec" } */
 
 #include <altivec.h>
