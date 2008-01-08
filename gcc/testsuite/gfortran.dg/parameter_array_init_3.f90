0c6ce8b02716646c4ecf998a96c40621a5cba15e
Paul Thomas <pault@gcc.gnu.org>
2008-01-08 15:14:33 +0000

    re PR fortran/34476 (Parameters: Bogus out of bounds error in array constructor)
    2008-01-08  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/34476
    	* expr.c (find_array_element): Check that the array bounds are
    	constant before using them.  Use lower, as well as upper bound.
    	(check_restricted): Allow implied index variable.
    
    2008-01-08  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/34476
    	* gfortran.dg/parameter_array_init_3.f90: New test.
    
    From-SVN: r131396

---
+++ gcc/testsuite/gfortran.dg/parameter_array_init_3.f90
@@ -0,0 +1,16 @@
+! { dg-do compile }
+! Test the fix for PR34476 in which an 'out of bounds' error would be
+! generated for the array initializations AND the implicit index 'i'
+! would be rejected.
+!
+! Reported by Tobias Burnus <burnus@gcc.gnu.org> following a thread
+! on comp.lang.fortran (see PR)
+!
+module abuse_mod
+   implicit none
+   integer i
+   character(8), parameter :: HEX1 = '40490FDB'
+   integer(1), parameter :: MSKa1(len(HEX1)) =  [(1,i=1,len(HEX1))]
+   integer(1), parameter :: ARR1(len(HEX1)) = [( MSKa1(i), i=1,len(HEX1) )]
+end module abuse_mod
+! { dg-final { cleanup-modules "abuse_mod" } }
