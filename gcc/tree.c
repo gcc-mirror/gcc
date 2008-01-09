b2ee3594e198f3b6ef5b6f86f602338abde00dd5
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-01-09 12:14:01 +0000

    2008-01-09  Richard Guenther  <rguenther@suse.de>
    	PR middle-end/34679
    	* tree.c (host_integerp): Check for sizetype only if the
    	type is an integer type.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131425 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree.c
+++ gcc/tree.c
@@ -4946,7 +4946,8 @@ host_integerp (const_tree t, int pos)
 	      || (! pos && TREE_INT_CST_HIGH (t) == -1
 		  && (HOST_WIDE_INT) TREE_INT_CST_LOW (t) < 0
 		  && (!TYPE_UNSIGNED (TREE_TYPE (t))
-		      || TYPE_IS_SIZETYPE (TREE_TYPE (t))))
+		      || (TREE_CODE (TREE_TYPE (t)) == INTEGER_TYPE
+			  && TYPE_IS_SIZETYPE (TREE_TYPE (t)))))
 	      || (pos && TREE_INT_CST_HIGH (t) == 0)));
 }
 
