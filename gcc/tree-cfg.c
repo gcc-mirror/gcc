5c6e497432adc5aec4703f28f7b17e21ad1223a3
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-01-10 16:59:06 +0000

    2008-01-10  Richard Guenther  <rguenther@suse.de>
    	PR middle-end/34683
    	* tree-cfg.c (tree_merge_blocks): Do not go through the
    	full-blown folding and stmt updating path if we just deal
    	with virtual operands.
    	* tree-ssa-copy.c (may_propagate_copy): Do not short-cut
    	test for abnormal SSA_NAMEs.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131446 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-cfg.c
+++ gcc/tree-cfg.c
@@ -1322,7 +1322,21 @@ tree_merge_blocks (basic_block a, basic_block b)
 	}
       else
         {
-          replace_uses_by (def, use);
+	  /* If we deal with a PHI for virtual operands, we can simply
+	     propagate these without fussing with folding or updating
+	     the stmt.  */
+	  if (!is_gimple_reg (def))
+	    {
+	      imm_use_iterator iter;
+	      use_operand_p use_p;
+	      tree stmt;
+
+	      FOR_EACH_IMM_USE_STMT (stmt, iter, def)
+		FOR_EACH_IMM_USE_ON_STMT (use_p, iter)
+		  SET_USE (use_p, use);
+	    }
+	  else
+            replace_uses_by (def, use);
           remove_phi_node (phi, NULL, true);
         }
     }
