86415518ea940ef2715e0312b9b411b654f48615
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2008-01-12 08:47:27 +0000

    re PR fortran/34432 (integer(kind=init_expression) function  is rejected)
    2008-01-12  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR fortran/34432
    	* gfortran.dg/parens_7.f90: New test.
    
    From-SVN: r131489

---
+++ gcc/testsuite/gfortran.dg/parens_7.f90
@@ -0,0 +1,17 @@
+! { dg-do compile }
+! PR34432 integer(kind=init_expression) function  is rejected
+module m
+  integer, parameter :: int_t = 4
+end module m
+
+program test
+  print *, test4()
+contains
+
+integer(kind=(int_t)) function test4() ! This failed before patch
+  use m
+  test4 = 345
+end function test4
+
+ 
+end program test
