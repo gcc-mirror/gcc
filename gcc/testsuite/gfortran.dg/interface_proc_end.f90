1c343287f8006e61f14c9342d5bd02b34ba4d85c
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-01-13 21:29:49 +0000

    2008-01-13  Tobias Burnus  <burnus@net-b.de>
            PR fortran/34763
            * decl.c (contained_procedure): Only check directly preceeding
            * state.
    
    2008-01-13  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/34763
            * gfortran.dg/interface_proc_end.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131512 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/interface_proc_end.f90
@@ -0,0 +1,19 @@
+! { dg-do compile}
+!
+! PR fortran/34763
+! Before, gfortran did not allow for the "END" in
+! the interface, which is no module procedure.
+!
+! Test case contributed by Dick Hendrickson
+!
+      module n
+      contains
+      subroutine n_interface
+      INTERFACE
+            SUBROUTINE NGSXDY(TLS1,TLS2)
+            REAL  ::  TLS1,TLS2
+            END ! OK
+      END INTERFACE
+      end ! { dg-error "END SUBROUTINE statement" }
+      end module ! { dg-error "END SUBROUTINE statement" }
+! { dg-excess-errors "Unexpected end of file" }
