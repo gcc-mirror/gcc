cc747729046b84b49059ec4bb0abe7400fdcbbfc
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-01-17 12:42:20 +0000

    * gcc.c-torture/compile/20080114-1.c: Use empty asm statements.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131596 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/compile/20080114-1.c
+++ gcc/testsuite/gcc.c-torture/compile/20080114-1.c
@@ -6,9 +6,9 @@ int type;
 void stuck(int res)
 {
   if (type == 1) {
-    if (res == 0) asm volatile("nop");
+    if (res == 0) asm volatile("");
   }
   else if (type == 0) {
-    if (res == 0) asm volatile("nop" : : "i" (0));
+    if (res == 0) asm volatile("" : : "i" (0));
   }
 }
