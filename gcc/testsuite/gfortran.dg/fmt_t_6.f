cc28eddead08d4bc612f8cd398eb207d15d7da9d
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-01-18 22:22:21 +0000

    2007-01-18  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR target/34782
    	* gfortran.dg/fmt_t_6.f: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131641 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/fmt_t_6.f
@@ -0,0 +1,10 @@
+! { dg-do  run }
+! PR34782 tab format failure to display properly (regression vs. g77)
+      character a(6)
+      character(22)  :: output
+      data a / 'a', 'b', 'c', 'd', 'e', 'f' /
+      !write(*,'(a)') "123456789012345678901234567890"
+      write(output,'(T20,A3,  T1,A4,  T5,A2,  T7,A2,  T9,A4, T17,A2)')
+     1 'a', 'b', 'c', 'd', 'e', 'f' 
+      if (output .ne. "   b c d   e     f   a") call abort
+      end
