0fdb73dac58cf702c1da5405e8587ad99eed9558
John David Anglin <dave.anglin@nrc-cnrc.gc.ca>
2008-01-19 21:55:47 +0000

    ia64-2.C: Place "dg-do run" statement before "dg-require-weak" statement.
    	* g++.dg/eh/ia64-2.C: Place "dg-do run" statement before
    	"dg-require-weak" statement.
    	* g++.dg/eh/weak1.C: Likewise.
    
    From-SVN: r131658

--- gcc/testsuite/g++.dg/eh/ia64-2.C
+++ gcc/testsuite/g++.dg/eh/ia64-2.C
@@ -5,9 +5,9 @@
 // But br.call returns to the start of the next bundlem so during
 // unwinding the call was considered to be outside of the EH region
 // while it should have been inside.
+// { dg-do run }
 // { dg-require-weak "" }
 // { dg-options "-O2" }
-// { dg-do run }
 
 struct A {};
 struct B { virtual ~B(); };
