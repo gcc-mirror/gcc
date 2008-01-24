07b78b8f8912aae15109cd5dcc38bcd442a42f10
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2008-01-24 20:09:05 +0000

    backport: re PR tree-optimization/33826 (GCC generates wrong code for infinitely recursive functions)
    	Backport:
    	2008-01-10  Kaveh R. Ghazi  <ghazi@caip.rutgers.edu>
    
    	* gcc.dg/pr33826.c: Require nonpic.
    	
    	2007-11-07  Kenneth Zadeck <zadeck@naturalbridge.com>
    	
    	PR middle-end/33826
    	* ipa-pure-const (static_execute): Added code to keep recursive
    	functions from being marked as pure or const.
    	* ipa-utils (searchc): Fixed comment.
    	
    	2007-11-08  Kenneth Zadeck <zadeck@naturalbridge.com>
    	
    	PR middle-end/33826
    	* gcc.dg/pr33826.c: New.
    	* gcc.dg/tree-ssa/20030714-1.c: Removed two tests that depend on 
    	recursive functions being marked pure or const.
    
    From-SVN: r131805

---
+++ gcc/testsuite/gcc.dg/pr33826.c
@@ -0,0 +1,41 @@
+/* Regression test for PR middle-end/33826 */
+/* Verify that recursive functions cannot be pure or const.  */
+
+/* { dg-do compile } */
+/* { dg-require-effective-target nonpic } */
+/* { dg-options "-O1 -fdump-ipa-pure-const" } */
+
+int recurese1 (int i)
+{
+  return recurse1 (i+1);
+}
+
+int recurse2a (int i)
+{
+  return recurse2b (i+1);
+}
+
+int recurse2b (int i)
+{
+  return recurse2a (i+1);
+}
+
+int norecurse1a (int i)
+{
+  return norecurse1b (i+1);
+}
+
+int norecurse1b (int i)
+{
+  return i+1;
+}
+
+/* { dg-final { scan-ipa-dump "found to be const: norecurse1a" "pure-const" } } */
+/* { dg-final { scan-ipa-dump "found to be const: norecurse1b" "pure-const" } } */
+/* { dg-final { scan-ipa-dump-not "found to be pure: recurse1" "pure-const" } } */
+/* { dg-final { scan-ipa-dump-not "found to be pure: recurse2a" "pure-const" } } */
+/* { dg-final { scan-ipa-dump-not "found to be pure: recurse2b" "pure-const" } } */
+/* { dg-final { scan-ipa-dump-not "found to be const: recurse1" "pure-const" } } */
+/* { dg-final { scan-ipa-dump-not "found to be const: recurse2a" "pure-const" } } */
+/* { dg-final { scan-ipa-dump-not "found to be const: recurse2b" "pure-const" } } */
+/* { dg-final { cleanup-ipa-dump "pure-const" } } */
