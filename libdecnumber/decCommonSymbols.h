7fa4b30a1674de01bf12aef706415e876aa08e4f
Janis Johnson <janis187@us.ibm.com>
2008-01-25 18:47:12 +0000

    decCommonSymbols.h: Commit.
            * decCommonSymbols.h: Commit.
            * decDPDSybmols.h: Commit.
    
    From-SVN: r131831

---
+++ libdecnumber/decCommonSymbols.h
@@ -0,0 +1,9 @@
+#if !defined(DECCOMMONSYMBOLS)
+#define DECCOMMONSYMBOLS
+
+#ifdef IN_LIBGCC2
+#define DECCOMBFROM __decCOMBFROM
+#define DECCOMBMSD __decCOMBMSD
+#endif
+
+#endif
---
+++ libdecnumber/decDPDSymbols.h
@@ -0,0 +1,15 @@
+#if !defined(DECDPDSYMBOLS)
+#define DECDPDSYMBOLS
+
+#ifdef IN_LIBGCC2
+#define BCD2DPD __decBCD2DPD
+#define BIN2BCD8 __decBIN2BCD8
+#define BIN2CHAR __decBIN2CHAR
+#define BIN2DPD __decBIN2DPD
+#define DPD2BCD8 __decDPD2BCD8
+#define DPD2BIN __decDPD2BIN
+#define DPD2BINK __decDPD2BINK
+#define DPD2BINM __decDPD2BINM
+#endif
+
+#endif
