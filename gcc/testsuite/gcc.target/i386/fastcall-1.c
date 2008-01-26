26b142a4b9bd7124b0fb53157df226c049b3afb4
dannysmith <dannysmith@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-01-26 09:31:30 +0000

    PR target/34970
    gcc
    	* config/i386/cygming.h (ASM_OUTPUT_LABELREF): Define.
    testsuite
    	* gcc.target/i386/fastcall-1.c: Scan for correct label.
    	Use -std=gnu89 switch. 
     
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131859 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/fastcall-1.c
+++ gcc/testsuite/gcc.target/i386/fastcall-1.c
@@ -1,4 +1,5 @@
 /* { dg-do compile { target i?86-*-mingw32* i?86-*-cygwin* } } */
+/* { dg-options "-std=gnu89" } */
 
 void
 __attribute__ ((fastcall))
@@ -15,3 +16,6 @@ f3() { }
 void
 __attribute__ ((fastcall))
 f4(int x, int y, int z) { }
+
+/* Scan for global label with correct prefix and suffix.  */
+/* { dg-final { scan-assembler "\.globl\[ \t\]@f4@12" } } */
