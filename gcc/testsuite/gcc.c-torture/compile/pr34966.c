680f02e4b47faab19c6f90630a4be1b15122e6fb
Jakub Jelinek <jakub@redhat.com>
2008-01-26 18:02:44 +0100

    pr34966.c (atan): Only use asm on i?86/x86_64.
    	* gcc.c-torture/compile/pr34966.c (atan): Only use asm
    	on i?86/x86_64.
    
    From-SVN: r131867

--- gcc/testsuite/gcc.c-torture/compile/pr34966.c
+++ gcc/testsuite/gcc.c-torture/compile/pr34966.c
@@ -4,7 +4,11 @@ __inline double
 atan (double __x)
 {
   register double __result;
+#if defined(__i386__) || defined(__x86_64__)
   __asm __volatile__ ("" : "=t" (__result) : "0" (__x));
+#else
+  __result = __x;
+#endif
   return __result;
 }
 
