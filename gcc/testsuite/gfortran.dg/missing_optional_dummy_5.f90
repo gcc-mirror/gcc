2c80cb0ef7df9e250a0a7826a4414d0895c65027
Tobias Burnus <burnus@net-b.de>
2008-01-26 21:44:49 +0100

    re PR fortran/34848 (internal compiler error with optional argument of character type and array return type)
    2008-01-26  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/34848
            * trans-expr.c (gfc_conv_function_call): Don't call
            gfc_add_interface_mapping if the expression is NULL.
    
    2008-01-26  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/34848
            * gfortran.dg/missing_optional_dummy_5.f90: New.
    
    From-SVN: r131876

---
+++ gcc/testsuite/gfortran.dg/missing_optional_dummy_5.f90
@@ -0,0 +1,29 @@
+! { dg-do compile }
+! { dg-options "-fdump-tree-original" }
+!
+! PR fortran/34848
+! 
+! This was before giving an ICE; additionally
+! the "0" for the string size of the absent optional
+! argument was missing.
+!
+module krmod
+contains
+ subroutine doit()
+   implicit none
+    real :: doit1(2)
+    doit1 = tm_doit()
+   return
+ end subroutine doit
+ function tm_doit(genloc)
+   implicit none
+   character, optional  :: genloc
+   real :: tm_doit(2)
+   tm_doit = 42.0 
+ end function tm_doit
+end module krmod
+
+! { dg-final { scan-tree-dump " tm_doit \\(&parm.7, 0B, 0\\);" "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
+! { dg-final { cleanup-modules "pr22146" } }
+
