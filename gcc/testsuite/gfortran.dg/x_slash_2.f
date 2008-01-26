d2e60c9efbe40341a2199078a02c319cdbc811d4
tkoenig <tkoenig@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-01-26 15:22:59 +0000

    2008-01-26  Thomas Koenig  <tkoenig@gcc.gnu.org>
    	PR libfofortran/34887
    	* io/transfer.c (next_record_w): Always move to the farthest
    	position when completing the record (also when we are
    	processing a slash edit descriptor).
    
    2008-01-26  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR libfofortran/34887
    	* gfortran.dg/x_slash_2.f:  New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131864 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/x_slash_2.f
@@ -0,0 +1,11 @@
+! { dg-do run }
+! PR 34887 - reverse tabs followed by a slash used to confuse I/O.
+      program main
+      character(len=2) :: b, a
+      open(10,form="formatted")
+      write (10,'(3X, A, T1, A,/)') 'aa', 'bb'
+      rewind(10)
+      read (10,'(A2,1X,A2)') b,a
+      if (a /= 'aa' .or. b /= 'bb') call abort
+      close(10,status="delete")
+      end
