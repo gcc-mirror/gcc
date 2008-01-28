b27b79dadfd8deddd753a4b7dfc4dfa7f93943f5
aldot <aldot@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-01-28 20:54:34 +0000

    2008-01-28  Bernhard Fischer  <aldot@gcc.gnu.org>
    	* g++.old-deja/g++.pt/incomplete1.C: Fix typo in comment.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@131919 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.pt/incomplete1.C
+++ gcc/testsuite/g++.old-deja/g++.pt/incomplete1.C
@@ -3,7 +3,7 @@
 // Copyright (C) 2000 Free Software Foundation, Inc.
 // Contributed by Nathan Sidwell 28 Nov 2000 <nathan@codesourcery.com>
 
-// Inspired by by 756. We'd ICE when trying to define a member of an
+// Inspired by 756. We'd ICE when trying to define a member of an
 // incomplete template type.
 
 template<class X> struct ObjCount;
