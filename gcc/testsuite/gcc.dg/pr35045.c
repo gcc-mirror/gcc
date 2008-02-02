6994f254d5a429d1eecfb998f859e34582e2885a
Michael Matz <matz@suse.de>
2008-02-02 15:00:57 +0000

    re PR target/35045 (gcc-4.3 generates wrong code on i386 with -O3)
            PR target/35045
            * postreload-gcse.c (record_last_reg_set_info_regno): Renamed
            from record_last_reg_set_info.
            (record_last_reg_set_info): Take an RTX argument, iterate over all
            constituent hardregs.
            (record_last_set_info, record_opr_changes): Change calls to
            new signature or to record_last_reg_set_info_regno.
    
            * gcc.dg/pr35045.c: New test.
    
    From-SVN: r132071

---
+++ gcc/testsuite/gcc.dg/pr35045.c
@@ -0,0 +1,37 @@
+/* { dg-do run } */
+/* { dg-options "-O2 -fgcse-after-reload" } */
+
+extern void abort (void);
+
+__attribute__((noinline)) __complex__ float
+give_neg1 (void)
+{
+  __complex__ float res;
+  __real__ res = -1.0;
+  __imag__ res = 1.0;
+  return res;
+}
+
+__attribute__((noinline)) __complex__ float
+mycacoshf (__complex__ float x)
+{
+  __complex__ float res;
+  res = give_neg1 ();
+
+  /* We have to use the positive branch.  */
+  if (__real__ res < 0.0)
+    {
+      unsigned a,b,c,d,e,f;
+      res = -res; 
+      asm __volatile__ ("" : "=r" (a), "=r" (b), "=r" (c), "=r" (d), "=r" (e), "=r" (f));
+    }
+  return res;
+}
+
+int main()
+{
+  __complex__ float res = mycacoshf(1.0);
+  if (__imag__ res >= 0.0)
+    abort();
+  return 0;
+}
