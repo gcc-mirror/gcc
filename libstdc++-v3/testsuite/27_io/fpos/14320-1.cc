0cc24ff082435b48eca7ec991e18eb437d7094b7
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2008-02-14 21:41:20 +0000

    14320-1.cc: Check for "long long" and remove XFAIL.
    	* testsuite/27_io/fpos/14320-1.cc: Check for "long long" and
    	remove XFAIL.
    
    From-SVN: r132321

--- libstdc++-v3/testsuite/27_io/fpos/14320-1.cc
+++ libstdc++-v3/testsuite/27_io/fpos/14320-1.cc
@@ -20,7 +20,7 @@
 
 // 27.4.3 fpos
 
-// { dg-do run { xfail *-*-* } }
+// { dg-do run }
 
 #include <typeinfo>
 #include <limits>
@@ -36,6 +36,11 @@ void test01()
   typedef istreambuf_iterator<char>::difference_type Distance;
 
   bool found = false;
+  // The C++ standard didn't originally have "long long", however that
+  // type will be in the C++0x standard and testing for it allows
+  // ilp32 targets to pass this test when `Distance' is 64 bits.
+  if (typeid(Distance) == typeid(long long int))
+    found = true;
   if (typeid(Distance) == typeid(long int))
     found = true;
   if (typeid(Distance) == typeid(int))
