508515b0277f2adadc610acb2dd14f769ba992ff
Dominique d'Humieres <dominiq@lps.ens.fr>
2008-02-15 15:33:57 +0100

    re PR target/35119 (FAIL: g++.dg/template/spec35.C scan-assembler)
    	PR testsuite/35119
    	* testsuite/g++.dg/template/spec35.C
    	Change the regular expressions in dg-final to match the assembly
    	code generated by Darwin 9.
    
    From-SVN: r132344

--- gcc/testsuite/g++.dg/template/spec35.C
+++ gcc/testsuite/g++.dg/template/spec35.C
@@ -8,22 +8,22 @@
 template<class T>
 static void f1 (T) { }
 
-// { dg-final { scan-assembler-not ".glob(a|)l\[\t \]*_Z2f1IfEvT_" } }
+// { dg-final { scan-assembler-not ".glob(a|)l\[\t \]*_?_Z2f1IfEvT_" } }
 template<>
 void f1<float> (float) { }  // Expected to have static linkage
 
 template<class T>
 void f2 (T) { }
 
-// { dg-final { scan-assembler ".glob(a|)l\[\t \]*_Z2f2IfEvT_" } }
+// { dg-final { scan-assembler ".glob(a|)l\[\t \]*_?_Z2f2IfEvT_" } }
 template<>
 void f2<float> (float) { }  // Expected to have global linkage
 
 void instantiator ()
 {
-  // { dg-final { scan-assembler-not ".glob(a|)l\[\t \]*_Z2f1IiEvT_" } }
+  // { dg-final { scan-assembler-not ".glob(a|)l\[\t \]*_?_Z2f1IiEvT_" } }
   f1(0);  // Expected to have static linkage
 
-  // { dg-final { scan-assembler ".weak\[\t \]*_Z2f2IiEvT_" } }
+  // { dg-final { scan-assembler ".weak(_definition)?\[\t \]*_?_Z2f2IiEvT_" } }
   f2(0);  // Expected to have weak global linkage
 }
