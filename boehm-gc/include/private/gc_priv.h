6656fda37b7354784b395dffbdd4049c441b4e99
Andreas Tobler <a.tobler@schweiz.org>
2008-02-19 21:29:26 +0000

    dyn_load.c (GC_dyld_image_add): Remove ifdef clause and use the macro GC_GETSECTBYNAME instead.
    2007-02-19  Andreas Tobler  <a.tobler@schweiz.org>
    
    	* dyn_load.c (GC_dyld_image_add): Remove ifdef clause and use the macro
    	GC_GETSECTBYNAME instead.
    	* include/private/gc_priv.h: Define GC_GETSECTBYNAME according to the
    	architecture (Darwin).
    
    From-SVN: r132452

--- boehm-gc/include/private/gc_priv.h
+++ boehm-gc/include/private/gc_priv.h
@@ -472,16 +472,18 @@ extern GC_warn_proc GC_current_warn_proc;
 #      if defined(POWERPC)
 #              if CPP_WORDSZ == 32
 #                define GC_THREAD_STATE_T ppc_thread_state_t
-#		  define GC_MACH_THREAD_STATE PPC_THREAD_STATE
-#		  define GC_MACH_THREAD_STATE_COUNT PPC_THREAD_STATE_COUNT
+#		 define GC_MACH_THREAD_STATE PPC_THREAD_STATE
+#		 define GC_MACH_THREAD_STATE_COUNT PPC_THREAD_STATE_COUNT
 #                define GC_MACH_HEADER mach_header
 #                define GC_MACH_SECTION section
+#                define GC_GETSECTBYNAME getsectbynamefromheader
 #              else
 #                define GC_THREAD_STATE_T ppc_thread_state64_t
-#		  define GC_MACH_THREAD_STATE PPC_THREAD_STATE64
-#		  define GC_MACH_THREAD_STATE_COUNT PPC_THREAD_STATE64_COUNT
+#		 define GC_MACH_THREAD_STATE PPC_THREAD_STATE64
+#		 define GC_MACH_THREAD_STATE_COUNT PPC_THREAD_STATE64_COUNT
 #                define GC_MACH_HEADER mach_header_64
 #                define GC_MACH_SECTION section_64
+#                define GC_GETSECTBYNAME getsectbynamefromheader_64
 #              endif
 #      elif defined(I386) || defined(X86_64)
 #              if CPP_WORDSZ == 32
@@ -490,12 +492,14 @@ extern GC_warn_proc GC_current_warn_proc;
 #                define GC_MACH_THREAD_STATE_COUNT x86_THREAD_STATE32_COUNT
 #                define GC_MACH_HEADER mach_header
 #                define GC_MACH_SECTION section
+#                define GC_GETSECTBYNAME getsectbynamefromheader
 #              else
 #                define GC_THREAD_STATE_T x86_thread_state64_t
 #                define GC_MACH_THREAD_STATE x86_THREAD_STATE64
 #                define GC_MACH_THREAD_STATE_COUNT x86_THREAD_STATE64_COUNT
 #                define GC_MACH_HEADER mach_header_64
 #                define GC_MACH_SECTION section_64
+#                define GC_GETSECTBYNAME getsectbynamefromheader_64
 #              endif
 #      else
 #              error define GC_THREAD_STATE_T
