4e06f6f816e517ae582dda5083d7ff291b7e992b
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-02-19 01:21:03 +0000

    gcc/
    2008-02-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/35189
    	* config/i386/i386.c (OPTION_MASK_ISA_MMX_SET): New.
    	(OPTION_MASK_ISA_3DNOW_SET): Likewise.
    	(OPTION_MASK_ISA_SSE_SET): Likewise.
    	(OPTION_MASK_ISA_SSE2_SET): Likewise.
    	(OPTION_MASK_ISA_SSE3_SET): Likewise.
    	(OPTION_MASK_ISA_SSSE3_SET): Likewise.
    	(OPTION_MASK_ISA_SSE4_1_SET): Likewise.
    	(OPTION_MASK_ISA_SSE4_2_SET): Likewise.
    	(OPTION_MASK_ISA_SSE4_SET): Likewise.
    	(OPTION_MASK_ISA_SSE4A_SET): Likewise.
    	(OPTION_MASK_ISA_SSE5_SET): Likewise.
    	(OPTION_MASK_ISA_3DNOW_A_UNSET): Likewise.
    	(OPTION_MASK_ISA_MMX_UNSET): Updated.
    	(OPTION_MASK_ISA_3DNOW_UNSET): Updated.
    	(OPTION_MASK_ISA_SSE_UNSET): Likewise.
    	(OPTION_MASK_ISA_SSE3_UNSET): Likewise.
    	(OPTION_MASK_ISA_SSSE3_UNSET): Likewise.
    	(OPTION_MASK_ISA_SSE4_1_UNSET): Likewise.
    	(OPTION_MASK_ISA_SSE4_2_UNSET): Likewise.
    	(OPTION_MASK_ISA_SSE4A_UNSET): Likewise.
    	(OPTION_MASK_ISA_SSE5_UNSET): Likewise.
    	(OPTION_MASK_ISA_SSE4): Removed.
    	(ix86_handle_option): Turn on bits in ix86_isa_flags and
    	ix86_isa_flags_explicit with OPTION_MASK_ISA_XXX_SET for
    	-mXXX.
    	(override_options): Don't turn on implied SSE/MMX bits in
    	ix86_isa_flags.
    
    gcc/testsuite/
    
    2008-02-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/35189
    	* gcc.target/i386/isa-1.c: New.
    	* gcc.target/i386/isa-2.c: Likewise.
    	* gcc.target/i386/isa-3.c: Likewise.
    	* gcc.target/i386/isa-4.c: Likewise.
    	* gcc.target/i386/isa-5.c: Likewise.
    	* gcc.target/i386/isa-6.c: Likewise.
    	* gcc.target/i386/isa-7.c: Likewise.
    	* gcc.target/i386/isa-8.c: Likewise.
    	* gcc.target/i386/isa-9.c: Likewise.
    	* gcc.target/i386/isa-10.c: Likewise.
    	* gcc.target/i386/isa-11.c: Likewise.
    	* gcc.target/i386/isa-12.c: Likewise.
    	* gcc.target/i386/isa-13.c: Likewise.
    	* gcc.target/i386/isa-14.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@132403 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/isa-9.c
@@ -0,0 +1,34 @@
+/* { dg-do run } */
+/* { dg-options "-march=amdfam10 -mno-sse5" } */
+
+extern void abort (void);
+
+int
+main ()
+{
+#if !defined __SSE__
+  abort ();
+#endif
+#if !defined __SSE2__
+  abort ();
+#endif
+#if !defined __SSE3__
+  abort ();
+#endif
+#if defined __SSSE3__
+  abort ();
+#endif
+#if defined __SSE4_1__
+  abort ();
+#endif
+#if defined __SSE4_2__
+  abort ();
+#endif
+#if !defined __SSE4A__
+  abort ();
+#endif
+#if defined __SSE5__
+  abort ();
+#endif
+  return 0;
+}
