bb78d95d3ae698a6f73aedd072de352e641ba51f
Jason Merrill <jason@redhat.com>
2008-02-19 23:47:47 -0500

    re PR c++/34950 (ICE in svn boost math toolkit)
            PR c++/34950
            * pt.c (resolve_overloaded_unification): Set processing_template_decl
            while we look for possible bindings.
    
    From-SVN: r132470

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -10107,6 +10107,7 @@ resolve_overloaded_unification (tree tparms,
 	  if (TREE_CODE (fn) != TEMPLATE_DECL)
 	    continue;
 
+	  ++processing_template_decl;
 	  subargs = get_bindings (fn, DECL_TEMPLATE_RESULT (fn),
 				  expl_subargs, /*check_ret=*/false);
 	  if (subargs)
@@ -10115,6 +10116,7 @@ resolve_overloaded_unification (tree tparms,
 	      good += try_one_overload (tparms, targs, tempargs, parm,
 					elem, strict, sub_strict, addr_p);
 	    }
+	  --processing_template_decl;
 	}
     }
   else if (TREE_CODE (arg) != OVERLOAD
