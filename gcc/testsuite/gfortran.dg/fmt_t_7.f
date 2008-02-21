b194358018f737bca23084dd5d1a2834783a6f1a
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-02-21 02:33:17 +0000

    2008-02-20  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR libfortran/34974
    	* gfortran.dg/fmt_t_7.f: New test.
    
    	PR libfortran/35132
    	* gfortran.dg/streamio_15.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@132513 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/fmt_t_7.f
@@ -0,0 +1,16 @@
+! { dg-do run }
+! PR34974 null bytes when reverse-tabbing long records
+! Test case prpared by Jerry DeLisle <jvdelisle@gcc.gnu.org>
+       program test
+       character(1) :: a, b, c
+       write (10,'(t50000,a,t1,a)') 'b', 'a'
+       close (10)
+       open (10, access="stream")
+       read (10, pos=1) a
+       read (10, pos=50000) b
+       read (10, pos=25474) c
+       close (10, status="delete")
+       if (a /= "a") call abort
+       if (b /= "b") call abort
+       if (c /= " ") call abort
+       end
