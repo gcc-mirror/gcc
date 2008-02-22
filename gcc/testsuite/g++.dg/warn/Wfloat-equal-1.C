ae3115664af05ea853e101ea812148a12c5ed96b
Ralf Wildenhues <Ralf.Wildenhues@gmx.de>
2008-02-22 06:17:46 +0000

    re PR c/19999 (-Wfloat-equal does not warn for complex numbers)
    gcc/:
    PR c/19999
    * c-typeck.c (build_binary_op): Warn about floating point
    comparisons if FLOAT_TYPE_P, not only for REAL_TYPE.
    
    gcc/cp/:
    * typeck.c (build_binary_op): Warn about floating point
    comparisons if FLOAT_TYPE_P, not only for REAL_TYPE.
    
    gcc/testsuite/:
    * gcc.dg/Wfloat-equal-1.c: New.
    * g++.dg/warn/Wfloat-equal-1.C: New.
    
    From-SVN: r132540

---
+++ gcc/testsuite/g++.dg/warn/Wfloat-equal-1.C
@@ -0,0 +1,10 @@
+/* PR c/19999 */
+/* { dg-do compile } */
+/* { dg-options "-Wfloat-equal" } */
+
+double a, b;
+_Complex double c, d;
+int f(void) { return a == b; } /* { dg-warning "comparing floating point" } */
+int g(void) { return c == d; } /* { dg-warning "comparing floating point" } */
+int h(void) { return a != b; } /* { dg-warning "comparing floating point" } */
+int i(void) { return c != d; } /* { dg-warning "comparing floating point" } */
