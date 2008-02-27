f5c064ab04a306a571f36b1d59db2c0337433f3e
Samuel Tardieu <sam@rfc1149.net>
2008-02-27 11:53:55 +0000

    re PR ada/34799 ([Ada] Assertion fails when no component clause is given for generic record member)
        gcc/ada/
    	PR ada/34799
    	* sem_ch13.adb (Analyze_Record_Representation_Clause): Check
    	that underlying type is present.
    
        gcc/testsuite/
    	PR ada/34799
    	* gnat.dg/specs/pr34799.ads: New test.
    
    From-SVN: r132707

---
+++ gcc/testsuite/gnat.dg/specs/pr34799.ads
@@ -0,0 +1,19 @@
+-- { dg-do compile }
+-- { dg-options "-gnatwa" }
+
+package PR34799 is
+   generic
+      type Custom_T is private;
+   package Handler is
+      type Storage_T is record
+         A : Boolean;
+         B : Boolean;
+         C : Custom_T;
+      end record;
+
+      for Storage_T use record
+         A at 0 range 0..0;
+         B at 1 range 0..0;
+      end record;
+   end Handler;
+end PR34799;
