f4abe9e0e3f36d33d40aed76f7b5a40d84f3e5f8
Uros Bizjak <ubizjak@gmail.com>
2008-02-28 10:39:13 +0100

    pr34351.c: Compile for x86 targets only.
    	* gcc.dg/pr34351.c: Compile for x86 targets only.  Use %ebx register.
    
    From-SVN: r132738

--- gcc/testsuite/gcc.dg/pr34351.c
+++ gcc/testsuite/gcc.dg/pr34351.c
@@ -1,4 +1,4 @@
-/* { dg-do compile } */
+/* { dg-do compile  { target i?86-*-* x86_64-*-* } } */
 /* { dg-options "-Wall" } */
 
-register int * volatile x asm ("r13"); /* { dg-warning "optimization may eliminate reads and/or writes to register variables" } */
+register int * volatile x asm ("ebx"); /* { dg-warning "optimization may eliminate reads and/or writes to register variables" } */
