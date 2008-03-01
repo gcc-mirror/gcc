303fd117fc141a05e1218c20324115cc326e3375
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-03-01 12:19:04 +0000

    PR fortran/34770 	* gfortran.dg/pointer_1.f90: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@132801 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pointer_1.f90
@@ -0,0 +1,14 @@
+! Testcase for PR34770
+! { dg-do run }
+  implicit none
+  integer, target :: x(0:12)
+  integer, pointer :: z(:)
+  integer i
+  do i = 0,12
+    x(i) = i
+  enddo
+  z => x
+  do i = 0,12
+    if (x(i) /= i .or. z(i) /= i) call abort
+  enddo
+end
