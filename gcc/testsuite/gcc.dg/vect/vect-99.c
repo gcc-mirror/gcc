2bf3f7e019067b8d665a2a12ca943b91667b8a85
Uros Bizjak <uros@gcc.gnu.org>
2008-03-03 08:39:58 +0100

    vect-99.c: Include stdlib.h and tree-vect.h.
            * gcc.dg/vect/vect-99.c: Include stdlib.h and tree-vect.h.
            (main): Call check_vect and foo.  Check array values after
            the call to foo.
    
    From-SVN: r132833

--- gcc/testsuite/gcc.dg/vect/vect-99.c
+++ gcc/testsuite/gcc.dg/vect/vect-99.c
@@ -1,5 +1,8 @@
 /* { dg-require-effective-target vect_int } */
 
+#include <stdlib.h>
+#include "tree-vect.h"
+
 int ca[100];
 
 void foo (int n)
@@ -12,6 +15,16 @@ void foo (int n)
 
 int main (void)
 {
+  int i;
+
+  check_vect ();
+
+  foo(100);
+
+  for (i = 0; i < 100; ++i) {
+    if (ca[i] != 2)
+      abort();
+  }
   return 0;
 }
 
