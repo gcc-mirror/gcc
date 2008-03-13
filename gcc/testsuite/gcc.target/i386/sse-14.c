1359ef39755c2a648c44e410e974450d91a4bd99
Uros Bizjak <ubizjak@gmail.com>
2008-03-13 14:33:47 +0100

    re PR target/34000 (GCC pedwarns about use of static inline functions from system headers in extern inline functions)
    	PR target/34000
    	PR target/35553
    	* config/i386/xmmintrin.h: Change all static inline functions to
    	extern inline and add __gnu_inline__ attribute.
    	* config/i386/bmintrin.h: Ditto.
    	* config/i386/smmintrin.h: Ditto.
    	* config/i386/tmmintrin.h: Ditto.
    	* config/i386/mmintrin-common.h: Ditto.
    	* config/i386/ammintrin.h: Ditto.
    	* config/i386/emmintrin.h: Ditto.
    	* config/i386/pmmintrin.h: Ditto.
    	* config/i386/mmintrin.h: Ditto.
    	* config/i386/mm3dnow.h: Ditto.
    
    testsuite/ChangeLog:
    
    	PR target/34000
    	PR target/35553
    	* g++.dg/other/i386-3.C: New test.
    	* gcc.target/i386/sse-13.c: Redefine extern instead of static.
    	* gcc.target/i386/sse-14.c: Ditto.
    	* gcc.target/i386/mmx-1.c: Ditto.
    	* gcc.target/i386/mmx-2.c: Ditto.
    	* gcc.target/i386/3dnow-1.c: Ditto.
    	* gcc.target/i386/3dnow-2.c: Ditto.
    	* gcc.target/i386/3dnowA-1.c: Ditto.
    	* gcc.target/i386/3dnowA-2.c: Ditto.
    
    From-SVN: r133169

--- gcc/testsuite/gcc.target/i386/sse-14.c
+++ gcc/testsuite/gcc.target/i386/sse-14.c
@@ -3,10 +3,10 @@
 
 /* Test that the intrinsics compile without optimization.  All of them are
    defined as inline functions in {,x,e,p,t,s,a,b}mmintrin.h  and mm3dnow.h
-   that reference the proper builtin functions.  Defining away "static" and
+   that reference the proper builtin functions.  Defining away "extern" and
    "__inline" results in all of them being compiled as proper functions.  */
 
-#define static
+#define extern
 #define __inline
 
 #include <bmmintrin.h>
