1c8d19bb9d7cfb2efcb7b524c7fcc72c02def4d5
Richard Guenther <rguenther@suse.de>
2008-03-14 17:45:20 +0000

    re PR tree-optimization/13761 ([tree-ssa] component refs to the same struct should not alias)
    2008-03-14  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/13761
    	* gcc.dg/tree-ssa/ssa-lim-3.c: New testcase.
    
    From-SVN: r133224

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-lim-3.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-lim-details -fdump-tree-optimized" } */
+
+struct { int x; int y; } global;
+void foo(int n)
+{
+  int i;
+  for ( i=0; i<n; i++)
+    global.y += global.x*global.x;
+}
+
+/* { dg-final { scan-tree-dump "Executing store motion of global.y" "lim" } } */
+/* { dg-final { scan-tree-dump "Moving statement.*global.x.*out of loop 1" "lim" } } */
+/* { dg-final { cleanup-tree-dump "lim" } } */
