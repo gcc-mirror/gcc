2a3b172ea4236e3ffa0556b8bb4e81515b8dc5e2
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-03-14 03:13:17 +0000

    2008-03-13  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR fortran/35475
    	* gfortran.dg/derived_sub.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@133205 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/derived_sub.f90
@@ -0,0 +1,33 @@
+! { dg-do compile }
+! PR35475  gfortran fails to compile valid code with ICE error in fold-const.c
+! Test case from PR report added to avoid future regression
+module modone
+  type mytype
+    real :: myvar
+  end type
+end module
+
+module modtwo
+  interface
+    subroutine subone(mytype_cur)
+      use modone
+      type (mytype) mytype_cur
+    end subroutine
+  end interface
+
+contains
+
+  subroutine subtwo(mytype_cur)
+    use modone
+    type (mytype) mytype_cur,mytype_fin
+    mytype_fin=mytype_cur
+    return
+  end subroutine
+
+  subroutine subthree(mytype_cur)
+    use modone
+    type (mytype) mytype_cur
+    call subone(mytype_cur)
+  end subroutine 
+
+end module
