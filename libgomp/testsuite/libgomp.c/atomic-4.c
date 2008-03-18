8119fc93077ce835232c711273cd8f3b097f17f4
Jakub Jelinek <jakub@redhat.com>
2008-03-18 13:21:02 +0100

    re PR middle-end/35611 (FAIL: libgomp.c/omp-nested-1.c execution test)
    	PR middle-end/35611
    	* gimplify.c (gimplify_expr): Gimplify second operand of
    	OMP_ATOMIC_LOAD.
    
    	* testsuite/libgomp.c/atomic-4.c: New test.
    
    From-SVN: r133309

---
+++ libgomp/testsuite/libgomp.c/atomic-4.c
@@ -0,0 +1,18 @@
+/* PR middle-end/35611 */
+/* { dg-options "-O2" } */
+
+extern void abort (void);
+
+int
+main (void)
+{
+  long double d = .0L;
+  int i;
+  #pragma omp parallel for shared (d)
+    for (i = 0; i < 1000; i++)
+      #pragma omp atomic
+	d += 1.0L;
+  if (d != 1000.0L)
+    abort ();
+  return 0;
+}
