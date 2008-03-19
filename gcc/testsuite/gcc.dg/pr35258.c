566df4923ca201a0b2526fef6c90e6f67ad388a3
Andreas Krebbel <krebbel1@de.ibm.com>
2008-03-19 10:06:52 +0000

    cse.c (cse_insn): Avoid creation of overlapping MEMs.
    2008-03-19  Andreas Krebbel  <krebbel1@de.ibm.com>
    
    	* cse.c (cse_insn): Avoid creation of overlapping MEMs.
    	* alias.c (nonoverlapping_memrefs_p): Export for use in other modules.
    	* alias.h (nonoverlapping_memrefs_p): Likewise.
    
    2008-03-19  Andreas Krebbel  <krebbel1@de.ibm.com>
    
    	* gcc.dg/pr35258.c: New testcase.
    
    From-SVN: r133338

---
+++ gcc/testsuite/gcc.dg/pr35258.c
@@ -0,0 +1,28 @@
+/* { dg-do run } */
+/* { dg-options "-O1" } */
+
+extern void *memcpy (void *, const void *, __SIZE_TYPE__);
+extern int memcmp (const void *, const void *, __SIZE_TYPE__);
+extern void abort(void);
+
+char str[9] = "1234";
+
+void
+bar (void)
+{
+  unsigned int temp;
+  char *p = &str[2];
+
+  memcpy (&temp, &str[1], 4);
+  memcpy (p, &temp, 4);
+  str[1] = '.';
+}
+
+int main()
+{
+  bar();
+  if (memcmp (str, "1.234", 5) != 0)
+    abort ();
+
+  return 0;
+}
