94e9831662a5824b2cc403cba23829ae61616a29
Andrew Pinski <andrew_pinski@playstation.sony.com>
2008-03-21 22:16:49 +0000

    re PR target/27946 (double to long long and back to double stores to the stack)
    2008-03-21  Andrew Pinski  <andrew_pinski@playstation.sony.com>
    
            PR target/27947
            * config/rs6000/rs6000.md (floatdidf2): Discouraging fprs and
            encouraging but not allowing gprs for input;
            change the input constraint to !f#r.
            (fix_truncdfdi2): Discouraging fprs and encouraging but not allowing
            gprs for output;
            change the output constraint to !f#r.
    
    
    2008-03-21  Andrew Pinski  <andrew_pinski@playstation.sony.com>
    
            PR target/27947
            * gcc.target/powerpc/ppc64-double-1.c: New testcase.
    
    From-SVN: r133438

---
+++ gcc/testsuite/gcc.target/powerpc/ppc64-double-1.c
@@ -0,0 +1,11 @@
+// { dg-do compile }
+// { dg-options "-O2 -mpowerpc64" }
+// { dg-final { scan-assembler-not "stfd"  }  }
+
+// The register allocator should have allocated the temporary long long value in a floating point register.
+
+double
+d2ll2d (double d)
+{
+        return (double)(long long)d;
+}
