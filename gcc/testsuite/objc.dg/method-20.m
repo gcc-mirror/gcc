9c349a39eb5b2466b60a0be488a0c55bbf7915b2
Andrew Pinski <pinskia@gmail.com>
2008-03-25 20:19:58 -0700

    re PR objc/29197 (ICE after error with array type with undefined variable)
    2008-03-25  Andrew Pinski  <pinskia@gmail.com>
    
            PR objc/29197
            * objc-act.c (encode_type): Handle when type is error_mark_node.
            (objc_push_parm): Handle when the type of parm is error_mark_node.
    
    2008-03-25  Andrew Pinski  <pinskia@gmail.com>
    
            PR objc/29197
            * objc.dg/method-20.m: New testcase.
    
    From-SVN: r133540

---
+++ gcc/testsuite/objc.dg/method-20.m
@@ -0,0 +1,8 @@
+/* { dg-do compile }  */
+
+/* We used to crash after we found the type for int[m] was declared as invalid. */
+/* PR objc/29197 */
+
+@ implementation NGActiveSocket
++ (void) socketPair:(int[m]) _pair {} /* { dg-error "" } */
+@end
