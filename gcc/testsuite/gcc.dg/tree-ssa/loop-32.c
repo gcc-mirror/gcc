063a8bce957e73d3f560df94aaed5b240cef0db9
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-03-27 10:25:36 +0000

    2008-03-27  Zdenek Dvorak  <ook@ucw.cz>
    	* tree-affine.h (aff_combination_expand): Declare.
    	(get_inner_reference_aff): Likewise.
    	* tree-affine.c	(aff_combination_expand): Split out from
    	tree_to_aff_combination_expand.
    	(get_inner_reference_aff): New function.
    	* tree-parloops.c (loop_parallel_p): Free vectorizer info.
            * tree-ssa-loop-im.c: Include tree-affine.h and pointer-set.h.
            (struct lim_aux_data): sm_done field removed.
            (mem_ref_loc_p, mem_ref_locs_p): New types.
            (struct mem_ref): Added id, stored, accesses_in_loop,
            indep_loop, dep_loop, indep_ref, dep_ref fields.
            Removed is_stored, locs and next fields.
            (memory_accesses): New variable.
            (movement_possibility): Do not allow moving statements
            that store to memory.
            (outermost_indep_loop, simple_mem_ref_in_stmt, mem_ref_in_stmt):
            New functions.
            (determine_max_movement): For statements with memory references,
            find the outermost loop in that the reference is independent.
            (move_computations_stmt): Mark the virtual operands for
            renaming.
            (memref_free, mem_ref_alloc, mem_ref_locs_alloc, mark_ref_stored,
            gather_mem_refs_stmt, gather_mem_refs_in_loops, vtoe_hash, vtoe_eq,
            vtoe_free, record_vop_access, get_vop_accesses, get_vop_stores,
            add_vop_ref_mapping, create_vop_ref_mapping_loop,
            create_vop_ref_mapping, analyze_memory_references,
            cannot_overlap_p, mem_refs_may_alias_p, rewrite_mem_ref_loc,
            get_all_locs_in_loop, ref_always_accessed_p,
            refs_independent_p, record_indep_loop, ref_indep_loop_p_1,
            ref_indep_loop_p, can_sm_ref_p, find_refs_for_sm,
            store_motion_loop, store_motion): New functions.
            (struct vop_to_refs_elt): New type.
            (record_mem_ref_loc, free_mem_ref_locs, rewrite_mem_refs,
            memref_hash, memref_eq, hoist_memory_references): Rewritten.
            (schedule_sm): Replaced by...
            (execute_sm): ... this.
            (determine_lsm_ref, hoist_memory_references,
            loop_suitable_for_sm, gather_mem_refs_stmt, gather_mem_refs,
            find_more_ref_vops, free_mem_ref, free_mem_refs,
            determine_lsm_loop, determine_lsm): Removed.
            (tree_ssa_lim_finalize): Free data structures used by store
            motion.
            (tree_ssa_lim): Call analyze_memory_references.  Use
            store_motion instead of determine_lsm.
    
    	* gcc.dg/tree-ssa/loop-32.c: New testcase.
    	* gcc.dg/tree-ssa/loop-33.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@133637 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/loop-32.c
@@ -0,0 +1,46 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-lim-details" } */
+
+int x;
+int a[100];
+
+struct a
+{
+  int X;
+  int Y;
+};
+
+void bla(void);
+
+void test1(void)
+{
+  unsigned i;
+
+  /* We should perform store motion here.  */
+  for (x = 0; x < 100; x++)
+    a[x] = x;
+}
+
+void test2(void)
+{
+  unsigned i;
+
+  /* But not here.  */
+  for (x = 0; x < 100; x++)
+    bla ();
+}
+
+void test3(struct a *A)
+{
+  unsigned i;
+
+  /* But we should here (using base + offset analysis).  */
+  for (i = 0; i < 100; i++)
+    {
+      A[5].X += i;
+      A[5].Y += i;
+    }
+}
+
+/* { dg-final { scan-tree-dump-times "Executing store motion of" 3 "lim" } } */
+/* { dg-final { cleanup-tree-dump "lim" } } */
