626f4dd8f52c0e85892dc4bd9f7bb2a1c87e2f3c
Richard Guenther <rguenther@suse.de>
2008-03-28 13:44:41 +0000

    re PR rtl-optimization/19580 (missed load/store motion)
    2008-03-28  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/19580
    	* gcc.dg/tree-ssa/loop-34.c: New testcase.
    
    From-SVN: r133683

---
+++ gcc/testsuite/gcc.dg/tree-ssa/loop-34.c
@@ -0,0 +1,21 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-lim-details" } */
+
+int r[6];
+
+void f (int n)
+{
+  while (-- n)
+    {
+      r [0] += r [5];
+      r [1] += r [0];
+      r [2] += r [1];
+      r [3] += r [2];
+      r [4] += r [3];
+      r [5] += r [4];
+    }
+}
+
+
+/* { dg-final { scan-tree-dump-times "Executing store motion of r" 6 "lim" } } */
+/* { dg-final { cleanup-tree-dump "lim" } } */
