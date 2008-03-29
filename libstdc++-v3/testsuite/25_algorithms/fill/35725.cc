ceb7d6545c1c717988ab152b2293bf2dc562ba43
Paolo Carlini <pcarlini@suse.de>
2008-03-29 22:38:36 +0000

    re PR libstdc++/35725 (ambiguous std::fill with character array)
    2008-03-29  Paolo Carlini  <pcarlini@suse.de>
    
    	PR libstdc++/35725
    	* include/bits/stl_algobase.h (__fill_a(_ForwardIterator,
    	_ForwardIterator, _Tp), __fill_a(_Tp*, _Tp*, _Tp),
    	__fill_n_a(_OutputIterator, _Size, _Tp), __fill_n_a(_Tp*, _Size,
    	_Tp)): Fix signatures.
    	* testsuite/25_algorithms/fill/35725.cc: New.
    	* testsuite/25_algorithms/fill_n/35725.cc: Likewise.
    
    From-SVN: r133722

---
+++ libstdc++-v3/testsuite/25_algorithms/fill/35725.cc
@@ -0,0 +1,30 @@
+// { dg-do compile }
+
+// Copyright (C) 2008 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// libstdc++/35725
+void test01()
+{
+  std::vector<std::string> foo(20);  
+  std::fill(foo.begin(), foo.end(), "bar");
+}
