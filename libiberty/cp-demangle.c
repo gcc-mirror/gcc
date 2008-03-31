8935c4b3476e3b8c17f0c8bcd97811a470851a2a
Ian Lance Taylor <ian@gcc.gnu.org>
2008-03-31 17:44:22 +0000

    Update copyright date.
    From-SVN: r133762

--- libiberty/cp-demangle.c
+++ libiberty/cp-demangle.c
@@ -1,5 +1,6 @@
 /* Demangler for g++ V3 ABI.
-   Copyright (C) 2003, 2004, 2005, 2006, 2007 Free Software Foundation, Inc.
+   Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008
+   Free Software Foundation, Inc.
    Written by Ian Lance Taylor <ian@wasabisystems.com>.
 
    This file is part of the libiberty library, which is part of GCC.
