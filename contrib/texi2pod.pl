8b8cd72e08ecdd007617a077f97db5b59f653765
Joseph Myers <joseph@codesourcery.com>
2008-04-01 19:49:36 +0100

    texi2pod.pl: Handle @samp and @url inside verbatim blocks.
    contrib:
    	* texi2pod.pl: Handle @samp and @url inside verbatim blocks.
    	Handle @heading.  Handle enumerated lists with starting numbers
    	and extra headings.
    
    gcc:
    	* doc/include/gpl_v3.texi: Update for manpage generation.
    	* doc/gcc.texi, doc/gccint.texi: Include gpl_v3.texi instead of
    	gpl.texi.
    	* doc/sourcebuild.texi: Document gpl_v3.texi as well as gpl.texi.
    	* Makefile.in (TEXI_GCC_FILES, TEXI_GCCINT_FILES): Include
    	gpl_v3.texi instead of gpl.texi.
    	(gpl.pod): New.
    
    gcc/fortran:
    	* gfortran.texi: Include gpl_v3.texi instead of gpl.texi
    	* Make-lang.in (GFORTRAN_TEXI): Include gpl_v3.texi instead of
    	gpl.texi.
    
    gcc/java:
    	* gcj.texi: Include gpl_v3.texi instead of gpl.texi
    	* Make-lang.in (TEXI_JAVA_FILES): Include gpl_v3.texi instead of
    	gpl.texi.
    
    From-SVN: r133796

--- contrib/texi2pod.pl
+++ contrib/texi2pod.pl
@@ -218,9 +218,11 @@ while(<$inf>) {
     s/\@\}/&rbrace;/g;
     s/\@\@/&at;/g;
 
-    # Inside a verbatim block, handle @var specially.
+    # Inside a verbatim block, handle @var, @samp and @url specially.
     if ($shift ne "") {
 	s/\@var\{([^\}]*)\}/<$1>/g;
+	s/\@samp\{([^\}]*)\}/"$1"/g;
+	s/\@url\{([^\}]*)\}/<$1>/g;
     }
 
     # POD doesn't interpret E<> inside a verbatim block.
@@ -250,7 +252,7 @@ while(<$inf>) {
 	next;
     };
 
-    /^\@(?:section|unnumbered|unnumberedsec|center)\s+(.+)$/
+    /^\@(?:section|unnumbered|unnumberedsec|center|heading)\s+(.+)$/
 	and $_ = "\n=head2 $1\n";
     /^\@subsection\s+(.+)$/
 	and $_ = "\n=head3 $1\n";
@@ -321,8 +323,14 @@ while(<$inf>) {
     /^\@itemx?\s*(.+)?$/ and do {
 	if (defined $1) {
             if ($ic) {
-                # Entity escapes prevent munging by the <> processing below.
-                $_ = "\n=item $ic\&LT;$1\&GT;\n";
+		if ($endw eq "enumerate") {
+		    $_ = "\n=item $ic $1\n";
+		    $ic =~ s/(\d+)/$1 + 1/eg;
+		} else {
+		    # Entity escapes prevent munging by the <>
+		    # processing below.
+		    $_ = "\n=item $ic\&LT;$1\&GT;\n";
+		}
             } else {
                 $_ = "\n=item $1\n";
             }
