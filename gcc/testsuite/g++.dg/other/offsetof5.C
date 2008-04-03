f23ce5f0f2a57dab072970903959367a5f62f535
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-03 07:43:46 +0000

    PR c++/35741 	* semantics.c (finish_offsetof): Undo effect of convert_from_reference 	before calling fold_offsetof.
    	* g++.dg/other/offsetof5.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@133863 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/other/offsetof5.C
@@ -0,0 +1,22 @@
+// PR c++/35741
+// { dg-do compile }
+
+#include <stddef.h>
+
+struct A
+{
+  char c;
+  int &i;
+};
+
+int j = offsetof (A, i);		// { dg-warning "invalid access|offsetof" }
+
+template <typename T>
+struct S
+{
+  T h;
+  T &i;
+  static const int j = offsetof (S, i);	// { dg-warning "invalid access|offsetof" }
+};
+
+int k = S<int>::j;			// { dg-warning "instantiated from here" }
