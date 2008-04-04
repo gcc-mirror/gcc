8cff5a31da50573393aaebbdae5d3986750db290
janis <janis@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-04 21:02:55 +0000

    * gcc.dg/torture/builtin-modf-1.c: Use special options for 	powerpc*-*-linux*.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@133912 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/torture/builtin-modf-1.c
+++ gcc/testsuite/gcc.dg/torture/builtin-modf-1.c
@@ -7,7 +7,7 @@
 
 /* { dg-do link } */
 /* { dg-options "-fno-finite-math-only" { target sh*-*-* } } */
-/* { dg-options "-funsafe-math-optimizations -fsigned-zeros -fno-associative-math" { target powerpc-*-darwin* } } */
+/* { dg-options "-funsafe-math-optimizations -fsigned-zeros -fno-associative-math" { target powerpc-*-darwin* powerpc*-*-linux* } } */
 
 extern void link_error(int);
 
