27183bba6bc249ae4c73bb799c7df702e98a2467
Uros Bizjak <uros@gcc.gnu.org>
2008-04-06 08:40:47 +0200

    re PR target/12329 (x86: local function declared with attribute((regparm(3))) gets corrupted parent frame pointer)
            PR target/12329
            * config/i386/i386.c (ix86_function_regparm): Error if regparm(3)
            attribute is used for nested functions.
    
    testsuite/ChangeLog:
    
            PR target/12329
            * gcc.target/i386/pr12329.c: New test.
    
    From-SVN: r133954

---
+++ gcc/testsuite/gcc.target/i386/pr12329.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target ilp32 } */
+/* { dg-options "-O2" } */
+
+extern void abort (void);
+
+int test_nested (int i)
+{
+  int __attribute__ ((__noinline__, __regparm__(3))) foo(int j, int k, int l)
+  { /* { dg-error "nested functions are limited to 2 register parameters" } */
+    return i + j + k + l;
+  }
+
+  return foo (i, i+1, i+2);
+}
