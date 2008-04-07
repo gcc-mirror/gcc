9c7eac6e007705e1335e881589b342bb478454bf
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-07 22:10:41 +0000

    2008-04-07  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR fortran/25829 28655
    	* gfortran.dg/write_check2.f90: Update dg-error.
    	* gfortran.dg/io_constraints_1.f90: Udate dg-error.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@133991 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/write_check2.f90
+++ gcc/testsuite/gfortran.dg/write_check2.f90
@@ -4,7 +4,7 @@
   character(len=20) :: str
   write(13,'(a)',advance='yes')  'Hello:'
   write(13,'(a)',advance='no')   'Hello:'
-  write(13,'(a)',advance='y')    'Hello:' ! { dg-error "ADVANCE=specifier at \\(1\\) must have value = YES or NO." }
-  write(13,'(a)',advance='yet')  'Hello:' ! { dg-error "ADVANCE=specifier at \\(1\\) must have value = YES or NO." }
-  write(13,'(a)',advance='yess') 'Hello:' ! { dg-error "ADVANCE=specifier at \\(1\\) must have value = YES or NO." }
+  write(13,'(a)',advance='y')    'Hello:' ! { dg-error "ADVANCE= specifier at \\(1\\) must have value = YES or NO." }
+  write(13,'(a)',advance='yet')  'Hello:' ! { dg-error "ADVANCE= specifier at \\(1\\) must have value = YES or NO." }
+  write(13,'(a)',advance='yess') 'Hello:' ! { dg-error "ADVANCE= specifier at \\(1\\) must have value = YES or NO." }
   end
