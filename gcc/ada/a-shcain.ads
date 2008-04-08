1ed69f611a49303c68258905e067c1c5888b9248
Arnaud Charlet <charlet@gcc.gnu.org>
2008-04-08 08:44:51 +0200

    a-cgcaso.adb, [...]: (Swap, Sift): Avoid use of complex renaming.
    2008-04-08  Arnaud Charlet  <charlet@adacore.com>
    	    Matthew Heaney  <heaney@adacore.com>
    
    	* a-cgcaso.adb, a-convec.adb: (Swap, Sift): Avoid use of complex
    	renaming.
    
    	* a-cgaaso.ads, a-secain.ads, a-slcain.ads, a-shcain.ads,  
    	a-crdlli.ads, a-coormu.ads, a-ciormu.ads: modified header to conform
    	to convention for non-RM specs.
    	Add descriptive header, and documented each operation
    	document each operation
    
    From-SVN: r134009

--- gcc/ada/a-shcain.ads
+++ gcc/ada/a-shcain.ads
@@ -6,16 +6,34 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
--- This specification is derived from the Ada Reference Manual for use with --
--- GNAT.  In accordance with the copyright of that document, you can freely --
--- copy and modify this specification,  provided that if you redistribute a --
--- modified version,  any changes that you have made are clearly indicated. --
+--          Copyright (C) 2004-2008, Free Software Foundation, Inc.         --
 --                                                                          --
+-- GNAT is free software;  you can  redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License  distributed with GNAT;  see file COPYING.  If not, write --
+-- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
+-- Boston, MA 02110-1301, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+-- This unit was originally developed by Matthew J Heaney.                  --
 ------------------------------------------------------------------------------
 
 with Ada.Containers;
 
 function Ada.Strings.Hash_Case_Insensitive
   (Key : String) return Containers.Hash_Type;
-
 pragma Pure (Ada.Strings.Hash_Case_Insensitive);
+--  Computes a hash value for Key without regard for character case. This is
+--  useful as the generic actual Hash function when instantiating a hashed
+--  container package with type String as the key.
