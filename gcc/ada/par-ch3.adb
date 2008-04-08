c6a34c2d43cb56ecd405226b467ec13bf12ac0c5
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-08 06:54:13 +0000

    2008-04-08  Ed Schonberg  <schonberg@adacore.com>
    	* par-ch3.adb (P_Type_Declaration) Reject the keyword "synchronized"
    	in a type declaration, if this is not an interface declaration or
    	private type extension.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134045 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/par-ch3.adb
+++ gcc/ada/par-ch3.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---          Copyright (C) 1992-2007, Free Software Foundation, Inc.         --
+--          Copyright (C) 1992-2008, Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -767,6 +767,10 @@ package body Ch3 is
                   --  Interface
 
                   else
+                     if Token /= Tok_Interface then
+                        Error_Msg_SC ("NEW or INTERFACE expected");
+                     end if;
+
                      Typedef_Node :=
                        P_Interface_Type_Definition (Abstract_Present);
                      Abstract_Present := True;
