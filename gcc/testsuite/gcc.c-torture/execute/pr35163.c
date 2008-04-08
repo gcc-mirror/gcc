a5e34caf3c36920fd52c0163914d0b2275c4ca0d
Andy Hutchinson <hutchinsonandy@aim.com>
2008-04-08 22:17:52 +0000

    re PR testsuite/35005 (New testcase execute/20071211-1.c assumes 32 bit integers)
    	PR target/35005
    	* execute/20071221-1.c: Adapt test for 16 bit int targets.
    	* execute/pr35163.c: Ditto.
    
    From-SVN: r134114

--- gcc/testsuite/gcc.c-torture/execute/pr35163.c
+++ gcc/testsuite/gcc.c-torture/execute/pr35163.c
@@ -4,7 +4,11 @@ int main()
 {
   signed char a = -30;
   signed char b = -31;
+  #if(__SIZEOF_INT__ >= 4)
   if (a > (unsigned short)b)
+#else
+  if ((long) a > (unsigned short)b)
+#endif
     abort ();
   return 0;
 }
