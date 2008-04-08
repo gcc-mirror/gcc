f970b6bf3b38e38b8b3218ad6a22879e12e46b42
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-08 06:47:40 +0000

    2008-04-08  Robert Dewar  <dewar@adacore.com>
    	* a-strbou.ads, a-strbou.adb (From_String): New procedure (for use by
    	Stream_Convert)
    
    	* sem_ch13.ads (Rep_Item_Too_Late): Document that Stream_Convert sets
    	FOnly
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134019 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/a-strbou.adb
+++ gcc/ada/a-strbou.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---          Copyright (C) 1992-2005, Free Software Foundation, Inc.         --
+--          Copyright (C) 1992-2008, Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -59,6 +59,15 @@ package body Ada.Strings.Bounded is
       begin
          return Times (Left, Right, Max_Length);
       end "*";
+
+      -----------------
+      -- From_String --
+      -----------------
+
+      function From_String (Source : String) return Bounded_String is
+      begin
+         return To_Super_String (Source, Max_Length, Error);
+      end From_String;
 
       ---------------
       -- Replicate --
