e7416e456f5699c678dabc2da8a19caaec9a9d93
hutchinsonandy <hutchinsonandy@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-08 22:17:52 +0000

    PR target/35005 	* execute/20071221-1.c: Adapt test for 16 bit int targets. 	* execute/pr35163.c: Ditto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134114 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/20071211-1.c
+++ gcc/testsuite/gcc.c-torture/execute/20071211-1.c
@@ -3,7 +3,11 @@ extern void abort() __attribute__ ((noreturn));
 struct s
 {
   unsigned long long f1 : 40;
+#if(__SIZEOF_INT__ >= 4)
   unsigned int f2 : 24;
+#else
+  unsigned long int f2 : 24;
+#endif
 } sv;
 
 int main()
