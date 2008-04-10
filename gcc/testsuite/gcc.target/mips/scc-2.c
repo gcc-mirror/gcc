5fb79e4c29afffae11b32f374ee8b120264305e9
Adam Nemet <anemet@caviumnetworks.com>
2008-04-10 18:28:45 +0000

    mips.md (GPR2): New mode iterator.
    	* config/mips/mips.md (GPR2): New mode iterator.
    	(seq): Add comment.
    	(*seq_<mode>, *seq_<mode>_mips16, *sne_<mode>, *sgt<u>_<mode>,
    	*sgt<u>_<mode>_mips16, *sge<u>_<mode>, *slt<u>_<mode>,
    	*slt<u>_<mode>_mips16 *sle<u>_<mode>, *sle<u>_<mode>_mips16):
    	Rewrite these to take two modes, the mode of comparison and the
    	mode of the destination.
    	* config/mips/mips.c (mips_expand_scc): Instead of having
    	paradoxical subreg as destination, expand "narrowing" scc if mode
    	of comparison is SI and target is requested in DI mode.
    	(mips_emit_int_order_test): Update comment.  Make mode of
    	comparison match CMP0 rather than TARGET.  When creating inverse
    	target use mode of TARGET.
    
    testsuite/
    
    	* gcc.target/mips/scc-2.c: New test.
    	* gcc.target/mips/scc-3.c: New test.
    	* gcc.target/mips/scc-4.c: New test.
    
    From-SVN: r134167

---
+++ gcc/testsuite/gcc.target/mips/scc-2.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */
+/* { dg-mips-options "-O -mgp64" } */
+
+/* { dg-final { scan-assembler-not "and\t\|andi\t\|ext\t\|sll\t\|srl\t" } } */
+/* { dg-final { scan-assembler-times "slt\t\|sltu\t" 12 } } */
+
+
+#define TEST(N, LHS, REL, RHS) \
+  NOMIPS16 long long w##N (int a, int b) {return LHS REL RHS;} \
+  NOMIPS16 int n##N (long long a, long long b) {return LHS REL RHS;} \
+
+TEST (eq, a, ==, 0);
+TEST (ne, a, !=, 0);
+TEST (gt, a, >, b);
+TEST (ge, a, >=, 1);
+TEST (lt, a, <, b);
+TEST (le, a, <=, 11);
---
+++ gcc/testsuite/gcc.target/mips/scc-4.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-mips-options "-O -mabi=o64" } */
+
+/* { dg-final { scan-assembler "slt\t" } } */
+/* { dg-final { scan-assembler "sltu\t\|xor\t\|xori\t" } } */
+
+/* This test should work both in mips16 and non-mips16 mode.  */
+
+int
+f (long long a, long long b)
+{
+  return a > 5;
+}
