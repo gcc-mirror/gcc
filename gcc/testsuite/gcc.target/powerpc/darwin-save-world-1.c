415d43a4dbddc87d9143d802760cec1f4cc7b3af
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-13 03:17:01 +0000

    2008-04-12  Andrew Pinski  <pinskia@gmail.com>
            * config/rs6000/rs6000.c (compute_save_world_info): Set lr_save_p if
            we are going to "save the world".
    
    
    2008-04-12  Andrew Pinski  <pinskia@gmail.com>
    
            * gcc.target/powerpc/darwin-save-world-1.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134237 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/darwin-save-world-1.c
@@ -0,0 +1,18 @@
+/* { dg-do run { target powerpc*-*-* } } */
+/* { dg-require-effective-target powerpc_altivec_ok } */
+/* { dg-options "-maltivec" } */
+
+/* With altivec turned on, Darwin wants to save the world but we did not mark lr as being saved any more
+   as saving the lr is not needed for saving altivec registers.  */
+
+int main (void)
+{
+  __label__ l1;
+  void __attribute__((used)) q(void)
+  {
+    goto l1;
+  }
+
+  l1:;
+  return 0;
+}
