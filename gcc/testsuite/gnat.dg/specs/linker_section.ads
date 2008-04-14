2efcad9f79ca7e4708a3f029b88e83b15eaa0727
Robert Dewar <dewar@adacore.com>
2008-04-14 16:38:16 +0000

    sem_prag.adb (Analyze_Pragma, [...]): Extend error to every non-object and change error message.
    2008-04-14  Robert Dewar  <dewar@adacore.com>
    
        gcc/ada/
    	* sem_prag.adb (Analyze_Pragma, Linker_Section case): Extend error
    	to every non-object and change error message.
    
        gcc/testsuite/
    	* gnat.dg/specs/linker_section.ads: Check for more error conditions.
    
    From-SVN: r134280

--- gcc/testsuite/gnat.dg/specs/linker_section.ads
+++ gcc/testsuite/gnat.dg/specs/linker_section.ads
@@ -3,8 +3,11 @@ package Linker_Section is
    pragma Linker_Section (Entity  => Data1,
                           Section => ".eeprom");
    type EEPROM_String is new String;
-   pragma Linker_Section (Entity  => EEPROM_String, -- { dg-error "type" }
+   pragma Linker_Section (Entity  => EEPROM_String, -- { dg-error "objects" }
                           Section => ".eeprom");
    Data2 : constant EEPROM_String := "12345678901234567";
+   package Inner is end;
+   pragma Linker_Section (Entity  => Inner,        -- { dg-error "objects" }
+                          Section => ".eeprom");
 end Linker_Section;
 
