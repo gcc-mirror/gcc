23fed36d2cfd8a117ed648160779b4bc60b73d9e
sam <sam@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-14 09:39:39 +0000

    2008-04-14  Rolf Ebert  <rolf.ebert.gcc@gmx.de>
        gcc/ada/
    	PR ada/20822
    	* xgnatugn.adb (Put_Line): New procedure, ensuring Unix
    	line endings even on non-Unix platforms.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134257 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/xgnatugn.adb
+++ gcc/ada/xgnatugn.adb
@@ -97,6 +97,7 @@ with Ada.Strings.Fixed;          use Ada.Strings.Fixed;
 with Ada.Strings.Unbounded;      use Ada.Strings.Unbounded;
 with Ada.Strings.Maps;           use Ada.Strings.Maps;
 with Ada.Strings.Maps.Constants; use Ada.Strings.Maps.Constants;
+with Ada.Streams.Stream_IO;      use Ada.Streams.Stream_IO;
 with Ada.Text_IO;                use Ada.Text_IO;
 
 with GNAT.Spitbol;               use GNAT.Spitbol;
@@ -108,12 +109,14 @@ procedure Xgnatugn is
    --  Print usage information. Invoked if an invalid command line is
    --  encountered.
 
-   Output_File : File_Type;
+   subtype Sfile is Ada.Streams.Stream_IO.File_Type;
+
+   Output_File : Sfile;
    --  The preprocessed output is written to this file
 
    type Input_File is record
       Name : VString;
-      Data : File_Type;
+      Data : Ada.Text_IO.File_Type;
       Line : Natural := 0;
    end record;
    --  Records information on an input file. Name and Line are used
@@ -122,6 +125,10 @@ procedure Xgnatugn is
    function Get_Line (Input : access Input_File) return String;
    --  Returns a line from Input and performs the necessary
    --  line-oriented checks (length, character set, trailing spaces).
+
+   procedure Put_Line (F : Sfile; S : String);
+   procedure Put_Line (F : Sfile; S : VString);
+   --  Local version of Put_Line ensures Unix style line endings
 
    Number_Of_Warnings : Natural := 0;
    Number_Of_Errors   : Natural := 0;
@@ -351,6 +358,21 @@ procedure Xgnatugn is
          return Trim (Line, Right);
       end;
    end Get_Line;
+
+   --------------
+   -- Put_Line --
+   --------------
+
+   procedure Put_Line (F : Sfile; S : String) is
+   begin
+      String'Write (Stream (F), S);
+      Character'Write (Stream (F), ASCII.LF);
+   end Put_Line;
+
+   procedure Put_Line (F : Sfile; S : VString) is
+   begin
+      Put_Line (F, To_String (S));
+   end Put_Line;
 
    -----------
    -- Error --
@@ -1311,7 +1333,7 @@ begin
          Open (Source_File.Data, In_File, Argument (2));
 
       exception
-         when Name_Error =>
+         when Ada.Text_IO.Name_Error =>
             Valid_Command_Line := False;
       end;
    end if;
@@ -1324,7 +1346,7 @@ begin
          Open (Dictionary_File.Data, In_File, Argument (3));
 
       exception
-         when Name_Error =>
+         when Ada.Text_IO.Name_Error =>
             Valid_Command_Line := False;
       end;
    end if;
@@ -1349,7 +1371,7 @@ begin
          Create (Output_File, Out_File, S (Output_File_Name));
 
       exception
-         when Name_Error | Use_Error =>
+         when Ada.Text_IO.Name_Error | Ada.Text_IO.Use_Error =>
             Valid_Command_Line := False;
       end;
    end if;
