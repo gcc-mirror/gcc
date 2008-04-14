dcf461fa7febf4b70d0bc78b4517ef2e17a9f7b7
Samuel Tardieu <sam@rfc1149.net>
2008-04-14 16:41:58 +0000

    storage.ads: Fix expected error message.
        gcc/testsuite/
    	* gnat.dg/specs/storage.ads: Fix expected error message.
    
    From-SVN: r134281

--- gcc/testsuite/gnat.dg/specs/storage.ads
+++ gcc/testsuite/gnat.dg/specs/storage.ads
@@ -3,7 +3,7 @@ with System.Pool_Global;
 package Storage is
    x1: System.Pool_Global.Unbounded_No_Reclaim_Pool;
    type T1 is access integer;
-   for T1'Storage_Pool use (x1);  -- { dg-error "denote a variable" }
+   for T1'Storage_Pool use (x1);  -- { dg-error "must be a variable" }
    type T2 is access Integer;
    for T2'Storage_Pool use x1;
 end Storage;
