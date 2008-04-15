1f93c018f2b896c143db6b3659e6d5f42f7ec1fb
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-15 15:09:42 +0000

    PR c/35751 	* c-decl.c (finish_decl): If extern or static var has variable 	size, set TREE_TYPE (decl) to error_mark_node.
    	* decl.c (layout_var_decl): If extern or static var has variable
    	size, set TREE_TYPE (decl) to error_mark_node.
    
    	* gcc.dg/gomp/pr35751.c: New test.
    	* g++.dg/gomp/pr35751.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134317 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/gomp/pr35751.C
@@ -0,0 +1,34 @@
+// PR c/35751
+// { dg-do compile }
+// { dg-options "-fopenmp" }
+
+void
+foo (int i)
+{
+  extern int a[i];	// { dg-error "storage size of" }
+  static int b[i];	// { dg-error "storage size of" }
+
+#pragma omp parallel
+  {
+    a[0] = 0;
+    b[0] = 0;
+  }
+
+#pragma omp parallel shared (a, b)
+  {
+    a[0] = 0;
+    b[0] = 0;
+  }
+
+#pragma omp parallel private (a, b)
+  {
+    a[0] = 0;
+    b[0] = 0;
+  }
+
+#pragma omp parallel firstprivate (a, b)
+  {
+    a[0] = 0;
+    b[0] = 0;
+  }
+}
