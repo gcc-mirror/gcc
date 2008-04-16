a93bd95061eaa8cc068f925759de807a64233e51
Jakub Jelinek <jakub@redhat.com>
2008-04-16 18:10:04 +0200

    re PR c/35739 (ICE with _Decimal128 and va_list)
    	PR c/35739
    	* tree-nrv.c (tree_nrv): Don't optimize if result_type is GIMPLE
    	reg type.
    
    	* gcc.dg/dfp/pr35739.c: New test.
    
    From-SVN: r134354

---
+++ gcc/testsuite/gcc.dg/dfp/pr35739.c
@@ -0,0 +1,16 @@
+/* PR c/35739 */
+/* { dg-do compile { target *-*-linux* } } */
+/* { dg-options "-O -fpreprocessed -fmudflap" } */
+
+_Decimal128
+foo (int n, ...)
+{
+  int i;
+  _Decimal128 j = 0;
+  __builtin_va_list ap;
+  __builtin_va_start (ap, n);
+  for (i = 0; i < n; i++)
+    j += __builtin_va_arg (ap, _Decimal128);
+  __builtin_va_end (ap);
+  return j;
+}
