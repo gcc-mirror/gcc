a0dcce1ce47b067ed1fc2677232947e8cb11a491
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-16 18:39:08 +0000

    PR target/35944         * gfortran.dg/pr35944-1.f90: New test.         * gfortran.dg/pr35944-2.f90: Ditto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134359 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr35944-1.f90
@@ -0,0 +1,22 @@
+! { dg-do run }
+
+  implicit none
+  integer i
+  real rda1(10), rda(10), rval
+  double precision dda1(10), dda(10), dval
+
+  rda = (/ 1,2,3,4,5,6,7,8,9,10 /)
+  rDA1 = MOD (1.1*(rDA(1)-5.0), P=(rDA-2.5))
+  DO i = 1, 10
+    rVAL = MOD (1.1*(rDA(1)-5.0), P=(rDA(i)-2.5))
+    if (rval /= rda1(i)) call abort
+  enddo
+
+  dda = (/ 1,2,3,4,5,6,7,8,9,10 /)
+  dDA1 = MOD (1.1d0*(dDA(1)-5.0d0), P=(dDA-2.5d0))
+  DO i = 1, 10
+    dVAL = MOD (1.1d0*(dDA(1)-5.0d0), P=(dDA(i)-2.5d0))
+    if (dval /= dda1(i)) call abort
+  enddo
+
+end
