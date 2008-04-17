659ce413ebf46451f3b81dd2347e7705405dc5a6
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-17 09:09:31 +0000

    2008-04-17  Richard Guenther  <rguenther@suse.de>
    	* tree-vn.c (expressions_equal_p): Do not check type
    	equality or compatibility before calling operand_equal_p.
    	* fold-const.c (operand_equal_p): Check equivalence of
    	integer constants before bailing out due to signedness or
    	precision differences.
    	* tree-ssa-sccvn.c (copy_reference_ops_from_ref): Ignore
    	spurious differences in type qualification.  Ignore types
    	for COMPONENT_REFs at all.
    
    	* gcc.dg/tree-ssa/ssa-fre-17.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134384 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-fre-17.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-fre-details -fno-tree-sra" } */
+
+struct Bar {
+  int dom;
+};
+struct Foo {
+  struct Bar doms[3];
+};
+
+int foo(int i, int j, int k)
+{
+  struct Foo f;
+
+  f.doms[0].dom = i;
+  f.doms[1].dom = j;
+  f.doms[2].dom = k;
+  return f.doms[0LL].dom;
+}
+
+/* { dg-final { scan-tree-dump "Replaced f.doms\\\[0\\\].dom with i_" "fre" } } */
+/* { dg-final { cleanup-tree-dump "fre" } } */
+
