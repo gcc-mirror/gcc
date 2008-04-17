d60819f8688af24e3a0cf4264bc0b3e1a06a6255
Christian Bruel <christian.bruel@st.com>
2008-04-17 08:49:16 +0200

    fix SH long long compare
    From-SVN: r134380

---
+++ gcc/testsuite/gcc.dg/long-long-compare-1.c
@@ -0,0 +1,126 @@
+/* Problem only noticed on SH for -mcbranchdi DImode comparison with constants.
+ * Target dependant failure but test valid for alls.  */
+/* { dg-do run } */
+/* { dg-options "-O0" } */
+/* { dg-options "-O0 -mcbranchdi" { target sh4-*-* } } */
+
+extern void abort(void);
+extern void exit(int);
+
+int test2(long long n)
+{
+  if (n < 2)
+    return 1;
+  return 0;
+}
+
+int test1(long long n)
+{
+  if (n < 1)
+    return 1;
+  return 0;
+}
+
+int test0(long long n)
+{
+  if (n < 0)
+    return 1;
+  return 0;
+}
+
+int test1n(long long n)
+{
+  if (n < -1LL)
+    return 1;
+  return 0;
+}
+
+int test2n(long long n)
+{
+  if (n < -2LL)
+    return 1;
+  return 0;
+}
+
+int main()
+{
+  if (test2n (-1LL))
+    abort ();
+
+  if (test2n (-2LL))
+    abort ();
+
+  if (test2n (0LL))
+    abort ();
+
+  if (test2n (1LL))
+    abort ();
+
+  if (test2n (2LL))
+    abort ();
+ 
+  if (test1n (-1LL))
+    abort ();
+
+  if (!test1n (-2LL))
+    abort ();
+
+  if (test1n (0LL))
+    abort ();
+
+  if (test1n (1LL))
+    abort ();
+
+  if (test1n (2LL))
+    abort ();
+
+  if (!test0 (-1LL))
+    abort ();
+
+  if (!test0 (-2LL))
+    abort ();
+
+  if (test0 (0LL))
+    abort ();
+
+  if (test0 (1LL))
+    abort ();
+
+  if (test0 (2LL))
+    abort ();
+
+  if (!test2 (-1LL))
+    abort ();
+
+  if (!test2 (-2LL))
+    abort ();
+
+  if (!test2 (0LL))
+    abort ();
+
+  if (!test2 (1LL))
+    abort ();
+
+  if (test2 (2LL))
+    abort ();
+
+  if (!test1 (-1LL))
+    abort ();
+
+  if (!test1 (-2LL))
+    abort ();
+
+  if (!test1 (0LL))
+    abort ();
+
+  if (test1 (1LL))
+    abort ();
+
+  if (test1 (2LL))
+    abort ();
+
+  exit (0);
+}
+
+
+
