f7515b0b5a57a944184c3709f0fe416afaa4e37b
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-19 21:35:02 +0000

    PR libgcj/35979: 	* jni.cc (_Jv_JNI_NewStringUTF): Return NULL if bytes==NULL.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134471 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/jni.cc
+++ libjava/jni.cc
@@ -1,6 +1,6 @@
 // jni.cc - JNI implementation, including the jump table.
 
-/* Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007
+/* Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008
    Free Software Foundation
 
    This file is part of libgcj.
@@ -1332,6 +1332,9 @@ _Jv_JNI_NewStringUTF (JNIEnv *env, const char *bytes)
 {
   try
     {
+      // For compatibility with the JDK.
+      if (!bytes)
+	return NULL;
       jstring result = JvNewStringUTF (bytes);
       return (jstring) wrap_value (env, result);
     }
