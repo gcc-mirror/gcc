772236db27f9c3dcb7a28a0d26d6134b63dffe18
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-21 18:41:04 +0000

    PR target/26445         * g++.dg/other/i386-4.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134522 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/other/i386-4.C
@@ -0,0 +1,29 @@
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O1 -msse2" } */
+
+#include <xmmintrin.h>
+
+void ConvertFloatSSE (void *inBuff, void *outBuff, int len)
+{
+  unsigned char *inByteBuffer = reinterpret_cast < unsigned char *>(inBuff);
+  float *outFloatBuffer = reinterpret_cast < float *>(outBuff);
+
+  for (int i = 0; i < len / 4; i++)
+    {
+      __m128i register1 =
+	_mm_set_epi8 (*inByteBuffer, *(inByteBuffer + 1),
+		      *(inByteBuffer + 2), 0,
+		      *(inByteBuffer + 3), *(inByteBuffer + 4),
+		      *(inByteBuffer + 5), 0,
+		      *(inByteBuffer + 6), *(inByteBuffer + 7),
+		      *(inByteBuffer + 8), 0,
+		      *(inByteBuffer + 9), *(inByteBuffer + 10),
+		      *(inByteBuffer + 11), 0);
+      __m128i register2 = _mm_srai_epi32 (register1, 8);
+      __m128 register3 = _mm_cvtepi32_ps (register2);
+
+      _mm_store_ps (outFloatBuffer, register3);
+      outFloatBuffer += 4;
+      inByteBuffer += 12;
+    }
+}
