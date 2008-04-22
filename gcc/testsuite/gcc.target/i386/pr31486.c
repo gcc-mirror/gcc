44552520ee0cecc48b8d432f684c72c810aefde2
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-22 06:48:48 +0000

    PR target/31486 	* gcc.target/i386/pr31486.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134549 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr31486.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-msse -mno-sse2" } */
+
+typedef double __v2df __attribute__ ((vector_size (16)));
+
+__v2df b = { 1.1, 1.2 };
+
+extern __v2df a2 (__v2df a, __v2df b);
+
+void test2 ()
+{
+  b = a2 (b, b);
+}
