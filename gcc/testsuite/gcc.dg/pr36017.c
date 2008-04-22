aa79060d8e9d55eb0efd2db75c9a15923c36b945
Jakub Jelinek <jakub@redhat.com>
2008-04-23 00:36:27 +0200

    re PR rtl-optimization/36017 (Miscompilation of tail call sqrt)
    	PR rtl-optimization/36017
    	* builtins.c (expand_errno_check): Clear CALL_EXPR_TAILCALL before
    	expanding the library call.
    
    	* gcc.dg/pr36017.c: New test.
    
    From-SVN: r134570

---
+++ gcc/testsuite/gcc.dg/pr36017.c
@@ -0,0 +1,29 @@
+/* PR rtl-optimization/36017 */
+/* { dg-do run { target i?86-*-linux* x86_64-*-linux* } } */
+/* { dg-options "-O2 -lm" } */
+
+extern double sqrt (double);
+extern void abort (void);
+
+__attribute__((noinline)) double
+foo (double a)
+{
+  double b, c, d = 0.7;
+  if (a <= d)
+    b = sqrt (d * a);
+  else
+    {
+      c = (1.0 - d) * (1.0 - a);
+      b = c > 0 ? 1.0 - sqrt (c) : 1.0;
+    }
+  return b;
+}
+
+int
+main (void)
+{
+  double c = foo (0.5);
+  if (c > 0.5917)
+    abort ();
+  return 0;
+}
