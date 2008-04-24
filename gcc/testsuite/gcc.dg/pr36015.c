8563747760384c3fc7f9190a3516b25e2fbbdb2a
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-24 06:59:55 +0000

    PR target/36015 	* config/i386/i386.c (init_cumulative_args): Don't pass anything 	in registers for -m32 only if stdarg_p (fntype).
    	* gcc.dg/pr36015.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134621 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr36015.c
@@ -0,0 +1,22 @@
+/* PR target/36015 */
+/* { dg-do run } */
+/* { dg-options "-O0" } */
+/* { dg-options "-O0 -mregparm=3" { target { { i?86-*-* x86_64-*-* } && ilp32 } } } */
+
+static int test ();
+
+int
+main (void)
+{
+  test (0, 1, 2, 3, 4, 5, 6, 7);
+  return 0;
+}
+
+static int
+test (int a, int b, int c, int d, int e, int f, int g, int h)
+{
+  if (a != 0 || b != 1 || c != 2 || d != 3
+      || e != 4 || f != 5 || g != 6 || h != 7)
+    __builtin_abort ();
+  return 0;
+}
