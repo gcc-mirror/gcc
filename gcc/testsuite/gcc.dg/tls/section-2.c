e9447137e85999b81ee5b9c9a038d10ead418d53
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-04-30 08:32:56 +0000

    * gcc.dg/tls/section-2.c: Restrict to vxworks.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134817 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tls/section-2.c
+++ gcc/testsuite/gcc.dg/tls/section-2.c
@@ -1,6 +1,7 @@
 /* Verify that we get errors for trying to put TLS data in 
    sections which can't work.  */
 /* { dg-require-effective-target tls } */
+/* { dg-do compile { target *-*-vxworks } } */
 
 #define A(X)	__attribute__((section(X)))
 
