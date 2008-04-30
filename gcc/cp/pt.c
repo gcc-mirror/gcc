480831b8b266d0a8a4bda8083f95dd663abcf7ac
Jakub Jelinek <jakub@redhat.com>
2008-04-30 17:04:56 +0200

    re PR c++/35986 (ICE with ambiguous template functions)
    	PR c++/35986
    	* pt.c (more_specialized_fn): Stop the loop even if there are no
    	arguments before ellipsis.
    
    	* g++.dg/overload/template4.C: New test.
    
    From-SVN: r134824

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -13559,7 +13559,9 @@ more_specialized_fn (tree pat1, tree pat2, int len)
 
   processing_template_decl++;
 
-  while (len--)
+  while (len--
+	 /* Stop when an ellipsis is seen.  */
+	 && args1 != NULL_TREE && args2 != NULL_TREE)
     {
       tree arg1 = TREE_VALUE (args1);
       tree arg2 = TREE_VALUE (args2);
@@ -13722,10 +13724,6 @@ more_specialized_fn (tree pat1, tree pat2, int len)
 
       args1 = TREE_CHAIN (args1);
       args2 = TREE_CHAIN (args2);
-
-      /* Stop when an ellipsis is seen.  */
-      if (args1 == NULL_TREE || args2 == NULL_TREE)
-	break;
     }
 
   processing_template_decl--;
