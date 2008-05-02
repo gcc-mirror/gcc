37030e5ad53b5453d4fe24fffda5edc5db56ac08
bdavis <bdavis@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-05-02 04:05:12 +0000

    2008-05-01  Bud Davis  <bdavis9659@sbcglobal.net>
    	PR fortran/PR35940	
    	* gfortran.dg/index.f90: New test.
    
    2008-05-01  Bud Davis  <bdavis9659@sbcglobal.net>
    
    	PR35940/Fortran
    	* simplify.c (gfc_simplify_index): Check for direction argument 
    	being a constant.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134879 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/index.f90
@@ -0,0 +1,22 @@
+! { dg-do run }
+! pr35940
+      program FA1031
+      implicit none
+      integer I
+      INTEGER IDA1(10)
+      LOGICAL GDA1(10)
+      INTEGER RSLT(10)
+      DATA RSLT /4,1,4,1,4,1,4,1,4,1/
+      IDA1   = 0
+      gda1 = (/ (i/2*2 .ne. I, i=1,10) /)
+
+      IDA1 = INDEX ( 'DEFDEF' , 'DEF', GDA1 )    !fails
+      do I = 1, 10
+         if (IDA1(i).NE.RSLT(i)) call abort
+      end do
+      IDA1 = INDEX ( (/ ('DEFDEF',i=1,10) /) , 'DEF', GDA1 )    !works
+      do I = 1, 10
+         if (IDA1(i).NE.RSLT(i)) call abort
+      end do
+
+      END
