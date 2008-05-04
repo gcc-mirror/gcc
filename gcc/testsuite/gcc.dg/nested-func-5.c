0981f26054d9dcefb157312035237ca795de2a03
hutchinsonandy <hutchinsonandy@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-05-04 13:10:14 +0000

    2008-05-04   Andy Hutchinson   <hutchinsonandy@aim.com>
    	* gcc.dg/nested-func-5.c: Skip for target without trampolines.
    	* gcc.dg/torture/nested-fn-1.c: Ditto.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134931 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/nested-func-5.c
+++ gcc/testsuite/gcc.dg/nested-func-5.c
@@ -1,6 +1,7 @@
 /* { dg-do compile } */
 /* { dg-options "-fexceptions" } */
 /* PR28516: ICE generating ARM unwind directives for nested functions.  */
+/* { dg-require-effective-target trampolines } */
 
 void ex(int (*)(void));
 void foo(int i)
