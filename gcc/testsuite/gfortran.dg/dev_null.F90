33437ca004897edeb4c9ab3dabd585e33bb78326
Danny Smith <dannysmith@users.sourceforge.net>
2008-05-05 21:15:51 +0000

    * gfortran.dg/dev_null.f90. Remove and replace with...
    	* gfortran.dg/dev_null.F90: Use preprocessor define to
    	generalize the DEV_NULL name.
    
    From-SVN: r134955

--- gcc/testsuite/gfortran.dg/dev_null.F90
+++ gcc/testsuite/gfortran.dg/dev_null.F90
@@ -1,8 +1,13 @@
 ! { dg-do run }
 ! pr19478 read from /dev/null
 ! Thomas.Koenig@online.de
+#if defined  _WIN32
+#define DEV_NULL "nul"
+#else
+#define DEV_NULL "/dev/null"
+#endif
       character*20 foo
-      open(10,file="/dev/null")
+      open(10,file=DEV_NULL)
       write(10,'(A)') "Hello"
       rewind(10)
       read(10,'(A)',end=100) foo
