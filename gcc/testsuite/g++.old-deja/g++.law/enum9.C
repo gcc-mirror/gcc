cb1f2cedac0ecfb461cd06d57e4d831253580d0c
drow <drow@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-05-06 18:34:53 +0000

    * g++.old-deja/g++.jason/enum6.C, g++.old-deja/g++.law/enum9.C, 	g++.old-deja/g++.other/enum4.C, gfortran/enum_9.f90, 	gfortran.dg/enum_10.f90: Broaden dg-options pattern.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@134993 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.law/enum9.C
+++ gcc/testsuite/g++.old-deja/g++.law/enum9.C
@@ -1,5 +1,17 @@
 // { dg-do run  }
 // { dg-options "-fshort-enums" }
+
+// On ARM EABI targets this testcase will cause a warning to be emitted
+// whilst EABI attributes are being merged at link time unless
+// the --no-enum-size-warning option is passed to the linker.  Whilst the
+// enum-size attributes should only be emitted if there are values of
+// enum type that can escape the compilation unit, gcc cannot currently
+// detect this; if this facility is added then this linker option should
+// not be needed.  arm-*-linux*eabi should be a good approximation to
+// those platforms where the EABI supplement defines enum values to be
+// 32 bits wide.
+// { dg-options "-fshort-enums -Wl,--no-enum-size-warning" { target arm*-*-linux*eabi } }
+
 // GROUPS passed enums
   extern "C" int printf (const char *, ...);
 
