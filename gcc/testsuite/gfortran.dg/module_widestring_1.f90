3d65dd3f859feff1313e6ea3af9915bd254c5342
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-05-09 08:02:52 +0000

    PR fortran/36162
            * module.c (quote_string, unquote_string,
            mio_allocated_wide_string): New functions.
            (mio_expr): Call mio_allocated_wide_string where needed.
    
    	* gfortran.dg/module_widestring_1.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@135109 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/module_widestring_1.f90
@@ -0,0 +1,16 @@
+! { dg-do run }
+! { dg-options "-fbackslash" }
+!
+! Testcase from PR36162
+module m
+  character(*), parameter ::  a ='H\0z'
+end module m
+
+  use m
+  character(len=20) :: s
+  if (a /= 'H\0z') call abort
+  if (ichar(a(2:2)) /= 0) call abort
+  write (s,"(A)") a
+end
+
+! { dg-final { cleanup-modules "m" } }
