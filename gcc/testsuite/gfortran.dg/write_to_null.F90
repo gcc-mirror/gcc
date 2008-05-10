6e90dc6b64630fd7485eff318a24531a49654daf
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2008-05-10 19:37:45 +0000

    write_to_null.f90: Rename to write_to_null.F90.
    	* write_to_null.f90: Rename to write_to_null.F90.
    	* write_to_null.F90: On Windows, "nul" is the equivalent of the
    	Unix /dev/null.
    
    From-SVN: r135158

--- gcc/testsuite/gfortran.dg/write_to_null.F90
+++ gcc/testsuite/gfortran.dg/write_to_null.F90
@@ -1,8 +1,15 @@
 ! { dg-do run }
 ! pr18983
 ! could not write to /dev/null
+
+#if defined  _WIN32
+#define DEV_NULL "nul"
+#else
+#define DEV_NULL "/dev/null"
+#endif
+
        integer i
-       open(10,file="/dev/null")
+       open(10,file=DEV_NULL)
        do i = 1,100
          write(10,*) "Hello, world"
        end do
