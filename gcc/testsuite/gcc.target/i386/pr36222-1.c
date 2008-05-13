08e64088dac74db3f526e2d764096968635efc1a
Uros Bizjak <uros@gcc.gnu.org>
2008-05-13 23:33:40 +0200

    re PR target/36222 (x86 fails to optimize out __v4si -> __m128i move)
            PR target/36222
            * config/i386/i386.c (ix86_expand_vector_init_general): Rearrange op0
            and op1 expansion before vector concat to have less live pseudos.
    
    testsuite/ChangeLog:
    
            PR target/36222
            * gcc.target/i386/pr36222-1.c: New test.
    
    From-SVN: r135275

---
+++ gcc/testsuite/gcc.target/i386/pr36222-1.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -msse2" } */
+
+typedef long long __m128i __attribute__ ((__vector_size__ (16), __may_alias__));
+typedef int __v4si __attribute__ ((__vector_size__ (16)));
+
+__m128i _mm_set_epi32 (int __q3, int __q2, int __q1, int __q0)
+{
+  return (__m128i)(__v4si){ __q0, __q1, __q2, __q3 };
+}
+
+/* { dg-final { scan-assembler-not "movdqa" } } */
