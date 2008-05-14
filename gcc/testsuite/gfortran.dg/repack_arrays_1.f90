1c3339af10c27fafd9573c0a77c79144e23ebc51
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2008-05-14 21:44:36 +0000

    re PR fortran/36059 (-frepack-arrays: symbols w/ TARGET should not be repacked)
            PR fortran/36059
    
            * trans-decl.c (gfc_build_dummy_array_decl): Don't repack
            arrays that have the TARGET attribute.
    
            * gfortran.dg/repack_arrays_1.f90: New test.
    
    From-SVN: r135310

---
+++ gcc/testsuite/gfortran.dg/repack_arrays_1.f90
@@ -0,0 +1,24 @@
+! { dg-do run }
+! { dg-options "-frepack-arrays" }
+!
+! Check that arrays marked with TARGET attribute are not repacked.
+!
+program test2
+   use iso_c_binding
+   implicit none
+   real, target :: x(7)
+   type(c_ptr) cp1, cp2
+
+   x = 42
+   if (.not. c_associated(c_loc(x(3)),point(x(::2)))) call abort
+contains
+  function point(x)
+    use iso_c_binding
+    real, intent(in), target :: x(:)
+    type(c_ptr) point
+    real, pointer :: p
+
+    p => x(2)
+    point = c_loc(p)
+  end function point
+end program test2 
