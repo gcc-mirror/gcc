c5fcd67041670039f624444358456f0a29c40b50
Andreas Tobler <a.tobler@schweiz.org>
2008-05-14 21:49:41 +0000

    sparc.h (NO_PROFILE_COUNTERS): Define as 0.
    2008-05-14  Andreas Tobler  <a.tobler@schweiz.org>
    
    	* config/sparc/sparc.h (NO_PROFILE_COUNTERS): Define as 0.
    
    From-SVN: r135312

--- gcc/config/sparc/sparc.h
+++ gcc/config/sparc/sparc.h
@@ -2483,3 +2483,6 @@ extern int sparc_indent_opcode;
 
 /* The number of Pmode words for the setjmp buffer.  */
 #define JMP_BUF_SIZE 12
+
+/* We use gcc _mcount for profiling.  */
+#define NO_PROFILE_COUNTERS 0
