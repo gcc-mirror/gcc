a53e5ca39955ad2f5fc76cc1731d01b977128ea0
irar <irar@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-05-14 12:28:53 +0000

    PR tree-optimization/36098 	* tree-vect-analyze.c (vect_analyze_group_access): Set the gap 	value for the first load in the group in case of a gap. 	(vect_build_slp_tree): Check that there are no gaps in loads.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@135290 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/O3-pr36098.c
@@ -0,0 +1,22 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target vect_int } */
+
+typedef struct {
+  int iatom[3];
+  int blocknr;
+} t_sortblock;
+
+#define DIM 3
+
+void foo (int ncons, t_sortblock *sb, int *iatom)
+{
+ int i, m;
+
+ for(i=0; (i<ncons); i++,iatom+=3)
+   for(m=0; (m<DIM); m++)
+     iatom[m]=sb[i].iatom[m];
+}
+
+/* { dg-final { scan-tree-dump-times "vectorizing stmts using SLP" 1 "vect" { xfail *-*-* } } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
+
