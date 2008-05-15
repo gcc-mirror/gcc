8ded35f90c50bd3ff06e75f0bc90dc611360dcac
Richard Guenther <rguenther@suse.de>
2008-05-15 08:37:22 +0000

    re PR tree-optimization/36009 (PRE causes missed loop store motion, store sinking doesn't work)
    2008-05-15  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/36009
    	PR tree-optimization/36204
    	* tree-ssa-loop-im.c (tree-ssa-propagate.h): Include.
    	(determine_invariantness_stmt): Record the loop a store is
    	always executed in.
    	* Makefile.in (tree-ssa-loop-im.o): Add tree-ssa-propagate.h
    	dependency.
    
    	* gcc.dg/tree-ssa/ssa-lim-5.c: New testcase.
    	* gcc.dg/tree-ssa/ssa-lim-6.c: Likewise..
    
    From-SVN: r135330

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-lim-6.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-lim-details" } */
+
+double a[16][64], y[64], x[16];
+void foo(void)
+{
+  int i, j;
+  for (j = 0; j < 64; ++j)
+    for (i = 0; i < 16; ++i)
+      y[j] = y[j] + a[i][j] * x[i];
+}
+
+/* { dg-final { scan-tree-dump "Executing store motion of y" "lim" } } */
+/* { dg-final { cleanup-tree-dump "lim" } } */
