c127dd86af4a31150d186e0febf6992215184ce4
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-05-16 16:02:02 +0000

    2008-05-16  Sebastian Pop  <sebastian.pop@amd.com> 	    Jan Sjodin  <jan.sjodin@amd.com>
    	PR tree-optimization/36228
    	* tree-data-ref.c (initialize_data_dependence_relation): Fast dependence
    	test when the references are the same, call compute_self_dependence.
    	* tree-data-ref.h (struct data_dependence_relation): Add self_reference_p.
    	(DDR_SELF_REFERENCE): New.
    
    	* testsuite/gcc.dg/vect/pr36228.c: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@135426 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/pr36228.c
@@ -0,0 +1,31 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 -fdump-tree-vect-details" } */
+
+#define COLS         8
+#define ROWS         8
+
+int
+t_run_test(void);
+
+int
+t_run_test()
+{
+     int k_1,i_1, j_1;
+     static signed char f_1[ROWS][COLS] ;
+     static long F_1[ROWS][COLS] ;
+     long cosMatrixA[ROWS][COLS] ;
+
+     for( k_1 = 0 ; k_1 < COLS ; k_1++ )
+        {
+            for( i_1 = 0 ; i_1 < ROWS ; i_1++ )
+            {
+                for( j_1 = 0 ; j_1 < COLS ; j_1++ )
+                    F_1[i_1][j_1] += f_1[i_1][k_1] * cosMatrixA[k_1][j_1] ;
+            }
+        }
+
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "versioning for alias required" 0 "vect" } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
