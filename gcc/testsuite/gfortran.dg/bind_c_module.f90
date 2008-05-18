e7bff0d1d5a2ca472ab99eba22e006a34ad37c56
Tobias Burnus <burnus@gcc.gnu.org>
2008-05-18 13:10:11 +0200

    re PR fortran/36251 (PUBLIC and PRIVATE abuse)
    2008-05-18  Steven G. Kargl  <kargls@comcast.net>
    
            PR fortran/36251
            * symbol.c (check_conflict): Issue errors for abuse of PUBLIC,
            * PRIVATE,
            and BIND(C).
            * resolve.c (gfc_verify_binding_labels): Fix NULL pointer
            * dereference.
    
    2008-05-18  Steven G. Kargl  <kargls@comcast.net>
    
            PR fortran/36251
            gfortran.dg/public_private_module.f90: new test.
            gfortran.dg/bind_c_module.f90: new test.
    
    From-SVN: r135495

---
+++ gcc/testsuite/gfortran.dg/bind_c_module.f90
@@ -0,0 +1,22 @@
+! { dg-do compile }
+! See PR fortran/36251.
+module a
+  implicit none
+  integer :: i = 42
+end module a
+
+! Causes ICE
+module b
+  use iso_c_binding
+  use a
+  implicit none
+  bind(c) :: a  ! { dg-error "attribute applied to" }
+end module b
+
+! Causes ICE
+module d
+  use a
+  implicit none
+  bind(c) :: a  ! { dg-error "attribute applied to" }
+end module d
+! { dg-final { cleanup-modules "a" } }
