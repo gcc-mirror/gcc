0c3fc9363e9bd170ca08731dd88fa55092514a87
Janis Johnson <janis187@us.ibm.com>
2008-05-20 23:25:13 +0000

    try-catch-2.mm: Move dg-options after dg-xfail-if.
    	* obj-c++.dg/try-catch-2.mm: Move dg-options after dg-xfail-if.
    	* obj-c++.dg/encode-8.mm: Move dg-options after dg-do.
    	* obj-c++.dg/bitfield-4.mm: Ditto; use dg-prune-output instead of
    	dg-excess-errors for possible additional message.
    	* obj-c++.dg/layout-1.mm: Use dg-prune-output instead of
    	dg-excess-errors for possible additional message.
    
    From-SVN: r135686

--- gcc/testsuite/obj-c++.dg/bitfield-4.mm
+++ gcc/testsuite/obj-c++.dg/bitfield-4.mm
@@ -1,8 +1,8 @@
 /* Check if the @defs() construct preserves the correct
    layout of bitfields.  */
 /* Contributed by Ziemowit Laski <zlaski@apple.com>.  */
-/* { dg-options "-lobjc -Wpadded" } */
 /* { dg-do run } */
+/* { dg-options "-lobjc -Wpadded" } */
 
 #include <objc/Object.h>
 
@@ -49,7 +49,7 @@ int main(void)
   return 0;
 }
 
-/* { dg-excess-errors "In file included from" { target lp64 } } */
+/* { dg-prune-output "In file included from" }  Ignore this message.  */
 /* { dg-bogus "padding struct to align" "PR23610" { xfail lp64 } 1 } */
 
 /* { dg-bogus "padding struct size" "PR23610" { xfail lp64 } 28 } */
