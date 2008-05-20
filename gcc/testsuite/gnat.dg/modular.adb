37e0d910ccc48ebef02cd73da6d0fcd2456806b2
sam <sam@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-05-20 20:24:33 +0000

    gcc/testsuite/ 	PR ada/30740 	* gnat.dg/modular.adb: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@135675 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gnat.dg/modular.adb
@@ -0,0 +1,16 @@
+-- { dg-do run }
+with Ada.Text_IO;
+procedure Modular is
+   type T1 is mod 9;
+   package T1_IO is new Ada.Text_IO.Modular_IO(T1);
+   X: T1 := 8;
+   J1: constant := 5;
+begin
+   for J2 in 5..5 loop
+      pragma Assert(X*(2**J1) = X*(2**J2));
+      if X*(2**J1) /= X*(2**J2) then
+         Ada.Text_IO.Put_Line("Failed");
+         raise Program_Error;
+      end if;
+   end loop;
+end Modular;
