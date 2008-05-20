9857228c4ca42e03fb12177065a14b5dadaf7e00
Sebastian Pop <spop@gcc.gnu.org>
2008-05-20 19:17:12 +0000

    re PR tree-optimization/36181 (Simple for loop generates ICE with -ftree-parallelize-loops=2)
    2008-05-20  Sebastian Pop  <sebastian.pop@amd.com>
    	    Jan Sjodin  <jan.sjodin@amd.com>
    
    	PR tree-optimization/36181
    	* tree-parloops.c (loop_has_vector_phi_nodes): New.
    	(parallelize_loops): Don't parallelize when the loop has vector
    	phi nodes.
    
    	* gcc.dg/tree-ssa/pr36181.c: New.
    
    From-SVN: r135673

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr36181.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 -ftree-parallelize-loops=2" } */
+
+int foo ()
+{
+  int i, sum = 0, data[1024];
+
+  for(i = 0; i<1024; i++)
+    sum += data[i];
+
+  return sum;
+}
+
