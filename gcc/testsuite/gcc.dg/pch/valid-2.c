3d1ff4b48d1428940e58b607a14d3f690fafeff0
Janis Johnson <janis187@us.ibm.com>
2008-05-21 00:04:45 +0000

    re PR testsuite/20771 (Duplicate PCH test names)
    	PR testsuite/20771
    	* lib/dg-pch.exp: Move a flag in arguments to dg-test to differentiate
    	compile results for pch test lines in gcc.sum.
    	* gcc.dg/pch/counter-2.c: Add comments to dg-error directives to
    	make them unique in gcc.sum.
    	* gcc.dg/pch/valid-1.c: Ditto.
    	* gcc.dg/pch/valid-2.c: Ditto.
    	* gcc.dg/pch/valid-3.c: Ditto.
    	* gcc.dg/pch/warn-1.c: Same for dg-warning.
    
    From-SVN: r135696

--- gcc/testsuite/gcc.dg/pch/valid-2.c
+++ gcc/testsuite/gcc.dg/pch/valid-2.c
@@ -1,6 +1,6 @@
 /* { dg-options "-I. -Winvalid-pch -fexceptions" } */
 
 #include "valid-2.h" /* { dg-warning "settings for -fexceptions do not match" } */
-/* { dg-error "No such file" "" { target *-*-* } 3 } */
-/* { dg-error "they were invalid" "" { target *-*-* } 3 } */
+/* { dg-error "No such file" "no such file" { target *-*-* } 3 } */
+/* { dg-error "they were invalid" "invalid files" { target *-*-* } 3 } */
 int x;
