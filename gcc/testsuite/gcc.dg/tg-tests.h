3bf5906b317941790438b4503a2234cdc710dca3
Kaveh R. Ghazi <ghazi@caip.rutgers.edu>
2008-05-23 04:47:12 +0000

    builtin-types.def (BT_FN_INT_INT_INT_INT_INT_INT_VAR): New.
    	* builtin-types.def (BT_FN_INT_INT_INT_INT_INT_INT_VAR): New.
    	* builtins.c (fold_builtin_fpclassify): New.
    	(fold_builtin_varargs): Handle BUILT_IN_FPCLASSIFY.
    	* builtins.def (BUILT_IN_FPCLASSIFY): New.
    	* c-common.c (handle_type_generic_attribute): Adjust to accept
    	fixed arguments before an elipsis.
    	(check_builtin_function_arguments): Handle BUILT_IN_FPCLASSIFY.
    	* doc/extend.texi: Document __builtin_fpclassify.
    
    testsuite:
    	* gcc.dg/builtins-error.c: Test __builtin_fpclassify.  Also
    	add tests for all previous type-generic builtins.
    	* gcc.dg/pr28796-2.c: Add -DUNSAFE flag.
    	* gcc.dg/tg-tests.h: Test __builtin_fpclassify.
    
    From-SVN: r135789

--- gcc/testsuite/gcc.dg/tg-tests.h
+++ gcc/testsuite/gcc.dg/tg-tests.h
@@ -1,9 +1,17 @@
 /* Test various type-generic builtins by calling "main_tests()".  */
 
+#define FP_NAN 1
+#define FP_INF 2
+#define FP_NORMAL 3
+#define FP_SUBNORMAL 4
+#define FP_ZERO 5
+#define fpclassify(X) __builtin_fpclassify(FP_NAN, FP_INF, FP_NORMAL, FP_SUBNORMAL, FP_ZERO, (X))
+
 void __attribute__ ((__noinline__))
 foo_1 (float f, double d, long double ld,
        int res_unord, int res_isnan, int res_isinf,
-       int res_isinf_sign, int res_isfin, int res_isnorm)
+       int res_isinf_sign, int res_isfin, int res_isnorm,
+       int classification)
 {
   if (__builtin_isunordered (f, 0) != res_unord)
     __builtin_abort ();
@@ -71,16 +79,30 @@ foo_1 (float f, double d, long double ld,
     __builtin_abort ();
   if (__builtin_finitel (ld) != res_isfin)
     __builtin_abort ();
+
+  /* Subnormals can abruptly underflow to zero in unsafe math
+     mode, so bypass testing these numbers if necessary.  */
+#ifdef UNSAFE
+  if (classification != FP_SUBNORMAL)
+#endif
+    {
+      if (fpclassify(f) != classification)
+	__builtin_abort ();
+      if (fpclassify(d) != classification)
+	__builtin_abort ();
+      if (fpclassify(ld) != classification)
+	__builtin_abort ();
+    }
 }
 
 void __attribute__ ((__noinline__))
 foo (float f, double d, long double ld,
      int res_unord, int res_isnan, int res_isinf,
-     int res_isfin, int res_isnorm)
+     int res_isfin, int res_isnorm, int classification)
 {
-  foo_1 (f, d, ld, res_unord, res_isnan, res_isinf, res_isinf, res_isfin, res_isnorm);
+  foo_1 (f, d, ld, res_unord, res_isnan, res_isinf, res_isinf, res_isfin, res_isnorm, classification);
   /* Try all the values negated as well.  */
-  foo_1 (-f, -d, -ld, res_unord, res_isnan, res_isinf, -res_isinf, res_isfin, res_isnorm);
+  foo_1 (-f, -d, -ld, res_unord, res_isnan, res_isinf, -res_isinf, res_isfin, res_isnorm, classification);
 }
 
 int __attribute__ ((__noinline__))
@@ -92,35 +114,35 @@ main_tests (void)
   
   /* Test NaN.  */
   f = __builtin_nanf(""); d = __builtin_nan(""); ld = __builtin_nanl("");
-  foo(f, d, ld, /*unord=*/ 1, /*isnan=*/ 1, /*isinf=*/ 0, /*isfin=*/ 0, /*isnorm=*/ 0);
+  foo(f, d, ld, /*unord=*/ 1, /*isnan=*/ 1, /*isinf=*/ 0, /*isfin=*/ 0, /*isnorm=*/ 0, FP_NAN);
 
   /* Test infinity.  */
   f = __builtin_inff(); d = __builtin_inf(); ld = __builtin_infl();
-  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 1, /*isfin=*/ 0, /*isnorm=*/ 0);
+  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 1, /*isfin=*/ 0, /*isnorm=*/ 0, FP_INF);
 
   /* Test zero.  */
   f = 0; d = 0; ld = 0;
-  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 0, /*isfin=*/ 1, /*isnorm=*/ 0);
+  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 0, /*isfin=*/ 1, /*isnorm=*/ 0, FP_ZERO);
 
   /* Test one.  */
   f = 1; d = 1; ld = 1;
-  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 0, /*isfin=*/ 1, /*isnorm=*/ 1);
+  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 0, /*isfin=*/ 1, /*isnorm=*/ 1, FP_NORMAL);
 
   /* Test minimum values.  */
   f = __FLT_MIN__; d = __DBL_MIN__; ld = __LDBL_MIN__;
-  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 0, /*isfin=*/ 1, /*isnorm=*/ 1);
+  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 0, /*isfin=*/ 1, /*isnorm=*/ 1, FP_NORMAL);
 
   /* Test subnormal values.  */
   f = __FLT_MIN__/2; d = __DBL_MIN__/2; ld = __LDBL_MIN__/2;
-  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 0, /*isfin=*/ 1, /*isnorm=*/ 0);
+  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 0, /*isfin=*/ 1, /*isnorm=*/ 0, FP_SUBNORMAL);
 
   /* Test maximum values.  */
   f = __FLT_MAX__; d = __DBL_MAX__; ld = __LDBL_MAX__;
-  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 0, /*isfin=*/ 1, /*isnorm=*/ 1);
+  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 0, /*isfin=*/ 1, /*isnorm=*/ 1, FP_NORMAL);
 
   /* Test overflow values.  */
   f = __FLT_MAX__*2; d = __DBL_MAX__*2; ld = __LDBL_MAX__*2;
-  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 1, /*isfin=*/ 0, /*isnorm=*/ 0);
+  foo(f, d, ld, /*unord=*/ 0, /*isnan=*/ 0, /*isinf=*/ 1, /*isfin=*/ 0, /*isnorm=*/ 0, FP_INF);
 
   return 0;
 }
