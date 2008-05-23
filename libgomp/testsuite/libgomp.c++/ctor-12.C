7a0112e7a42a5a3891d93fa449348df6446fc8bb
Jakub Jelinek <jakub@redhat.com>
2008-05-23 13:52:44 +0200

    re PR c++/36308 (OpenMP privatized vars don't get dtors called if they are virtual)
    	PR c++/36308
    	* semantics.c (omp_clause_info_fndecl): New function.
    	(finish_omp_clauses): Use it.
    
    	* testsuite/libgomp.c++/ctor-11.C: New test.
    	* testsuite/libgomp.c++/ctor-12.C: New test.
    
    From-SVN: r135798

---
+++ libgomp/testsuite/libgomp.c++/ctor-12.C
@@ -0,0 +1,65 @@
+// PR c++/36308
+// { dg-do run }
+
+extern "C" void abort ();
+
+static int ctors, dtors, copyctors, n, m;
+
+struct A
+{
+  A ()
+  {
+    l = 0;
+    #pragma omp atomic
+      ctors++;
+  }
+  A (const A &x)
+  {
+    l = x.l;
+    #pragma omp atomic
+      copyctors++;
+  }
+  virtual A& operator= (const A &x)
+  {
+    l = x.l;
+    #pragma omp atomic
+      assignops++;
+    return *this;
+  }
+  virtual ~A ()
+  {
+    #pragma omp atomic
+      dtors++;
+  }
+  int l;
+  static int ctors, dtors, copyctors, assignops;
+};
+
+int A::ctors;
+int A::dtors;
+int A::copyctors;
+int A::assignops;
+
+int
+main ()
+{
+  A a;
+#pragma omp parallel private (a)
+  {
+    a.l = 6;
+    #pragma omp single copyprivate (a)
+    {
+      a.l = 3;
+    }
+    if (a.l != 3)
+      abort ();
+    #pragma omp atomic
+      n++;
+  }
+  if (A::ctors != n + 1
+      || A::copyctors != 0
+      || A::dtors != n
+      || A::assignops != n - 1)
+    abort ();
+  return 0;
+}
