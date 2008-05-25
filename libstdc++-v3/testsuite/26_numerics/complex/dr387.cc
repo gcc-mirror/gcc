23ed71c60c5a2783c092a00cd39133c2f057d4dc
Paolo Carlini <paolo.carlini@oracle.com>
2008-05-25 18:59:19 +0000

    complex (complex<>::real(_Tp), [...]): Add per DR 387.
    2008-05-25  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* include/std/complex (complex<>::real(_Tp), complex<>::imag(_Tp),
    	complex<float>::real(float), complex<float>::imag(float),
    	complex<double>::real(double), complex<double>::imag(double),
    	complex<long double>::real(long double),
    	complex<long double>::imag(long double)): Add per DR 387.
    	(complex<>::real(), complex<>::imag(), complex<float>::real(),
    	complex<float>::imag(), complex<double>::real(),
    	complex<double>::imag(), complex<long double>::real(),
    	complex<long double>::imag(long double)): Adjust in C++0x mode.
    	(real(complex<>&), imag(complex<>&), real(const complex<>&),
    	imag(const complex<>&)): Likewise.
    	* testsuite/26_numerics/complex/dr387.cc: New.
    	* doc/xml/manual/intro.xml: Add an entry for DR 387.
    
    From-SVN: r135878

---
+++ libstdc++-v3/testsuite/26_numerics/complex/dr387.cc
@@ -0,0 +1,52 @@
+// 2008-05-22  Paolo Carlini  <paolo.carlini@oracle.com>
+//
+// Copyright (C) 2008 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#include <complex>
+#include <testsuite_hooks.h>
+
+// DR 387. std::complex over-encapsulated.
+template<typename T>
+  void
+  do_test()
+  {
+    bool test __attribute__((unused)) = true;
+
+    const T r = 1.0;
+    const T i = -1.0;
+    const T v = 0.0;
+
+    std::complex<T> z1(r, i);
+    z1.real(v);
+    VERIFY( z1.real() == v );
+    VERIFY( z1.imag() == i );
+
+    std::complex<T> z2(r, i);
+    z2.imag(v);
+    VERIFY( z2.real() == r );
+    VERIFY( z2.imag() == v );
+  }
+
+int main()
+{
+  do_test<float>();
+  do_test<double>();
+  do_test<long double>();
+  return 0;
+}
