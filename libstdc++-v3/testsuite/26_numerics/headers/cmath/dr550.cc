774c3d8647cc7012937cfc9d2d6dacc85b6cf8e9
Paolo Carlini <paolo.carlini@oracle.com>
2008-05-26 19:18:24 +0000

    cmath (pow(float, int), [...]): Do not define in C++0x mode, per DR 550.
    2008-05-26  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* include/c_global/cmath (pow(float, int), pow(double, int),
    	pow(long double, int)): Do not define in C++0x mode, per DR 550.
    	* include/tr1_impl/cmath (pow): Do not bring in unconditionally
    	from namespace std.
    	* include/tr1/cmath (pow(double, double), pow(float, float),
    	pow(long double, long double), pow(_Tp, _Up)): Define.
    	* include/tr1/complex (pow): Do not bring in from namespace std.
    	(pow(const std::complex<_Tp>&, int), pow(const std::complex<_Tp>&,
    	const _Tp&), pow(const _Tp&, const std::complex<_Tp>&),
    	pow(const std::complex<_Tp>&, const std::complex<_Tp>&)): Define.
    	* include/tr1_impl/complex (pow(const std::complex<_Tp>&,
    	const _Up&), pow(const _Tp&, const std::complex<_Up>&),
    	pow(const std::complex<_Tp>&, const std::complex<_Up>&)): Always
    	define.
    	* doc/xml/manual/intro.xml: Add an entry for DR 550.
    	* testsuite/26_numerics/headers/cmath/dr550.cc: New.
    	* testsuite/tr1/8_c_compatibility/cmath/overloads.cc: Adjust.
    
    From-SVN: r135955

---
+++ libstdc++-v3/testsuite/26_numerics/headers/cmath/dr550.cc
@@ -0,0 +1,47 @@
+// { dg-options "-std=gnu++0x" }
+// 2008-05-26  Paolo Carlini  <paolo.carlini@oracle.com>
+//
+// Copyright (C) 2008 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#include <cmath>
+#include <testsuite_hooks.h>
+#include <testsuite_tr1.h>
+
+// DR 550. What should the return type of pow(float,int) be?
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  using __gnu_test::check_ret_type;
+
+  const int          i1 = 1;
+  const float        f1 = 1.0f;
+  const double       d1 = 1.0;
+  const long double ld1 = 1.0l;
+
+  check_ret_type<double>(std::pow(f1, i1));
+  VERIFY( std::pow(f1, i1) == std::pow(double(f1), double(i1)) );
+  check_ret_type<double>(std::pow(d1, i1));
+  check_ret_type<long double>(std::pow(ld1, i1));
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
