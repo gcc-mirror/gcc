dd383eebef22066c2eff724d9d0c964975b72d79
Arnaud Charlet <charlet@adacore.com>
2008-05-27 09:11:45 +0000

    * gnat.dg/sync1.ad[sb]: New test.
    From-SVN: r135989

---
+++ gcc/testsuite/gnat.dg/sync1.ads
@@ -0,0 +1,12 @@
+package sync1 is
+   type Chopstick_Type is synchronized interface;
+   
+   type Chopstick is new Chopstick_Type with private;
+private
+   protected type Chopstick is new Chopstick_Type with
+      entry Pick_Up;
+      procedure Put_Down;
+   private
+      Busy : Boolean := False;
+   end Chopstick;
+end sync1;
