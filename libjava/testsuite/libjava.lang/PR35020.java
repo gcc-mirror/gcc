71292a05408898d2e37ef5442083d1819ead741a
Andrew Haley <aph@redhat.com>
2008-05-28 15:12:47 +0000

    re PR libgcj/35020 (Class.getSimpleName() differs from Sun Java)
    2008-05-28  Andrew Haley  <aph@redhat.com>
    
    	* java/lang/Class.java (getSimpleName): Use getEnclosingClass().	
    	* testsuite/libjava.lang/PR35020.java: New cases.
    	* testsuite/libjava.lang/PR35020.out: New cases.
    
    From-SVN: r136103

--- libjava/testsuite/libjava.lang/PR35020.java
+++ libjava/testsuite/libjava.lang/PR35020.java
@@ -1,21 +1,30 @@
+class outer$inner
+{
+};
+
 public class PR35020
 {
-   class inner
-   {
-   }
-   public static void main(String[] args)
-   {
-      System.out.println(inner.class.getSimpleName());
-      System.out.println(PR35020.class.getSimpleName());
-      System.out.println(Class.class.getSimpleName());
-      System.out.println((new int[7]).getClass().getSimpleName());
-      System.out.println((new Object[1][1][1][1][1][1][1][1]).getClass().getSimpleName());
-      System.out.println((new java.security.PrivilegedAction() 
-	{
-	  public Object run() {
-	    return null;
-	  }
-	}).getClass().getSimpleName());
-   }
+  class PR35020$Inner
+  {
+  };
+  class inner
+  {
+  }
+  public static void main(String[] args)
+  {
+    System.out.println(inner.class.getSimpleName());
+    System.out.println(PR35020.class.getSimpleName());
+    System.out.println(Class.class.getSimpleName());
+    System.out.println((new int[7]).getClass().getSimpleName());
+    System.out.println((new Object[1][1][1][1][1][1][1][1]).getClass().getSimpleName());
+    System.out.println((new java.security.PrivilegedAction() 
+      {
+	public Object run() {
+	  return null;
+	}
+      }).getClass().getSimpleName());
+    System.out.println(PR35020$Inner.class.getSimpleName());
+    System.out.println(outer$inner.class.getSimpleName());
+    System.out.println(outer$inner.inner.class.getSimpleName());
+  }
 }
-
