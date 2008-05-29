32006f36c7bd83a77a68365a12355e69e8e96de7
dfranke <dfranke@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-05-29 18:21:35 +0000

    gcc: 2008-05-29  Daniel Franke  <franke.daniel@gmail.com>
            PR target/36348
            * config/darwin-f.c: New.
            * config/t-darwin: Added rule to build darwin-f.o.
            * config.gcc: Defined new variable, fortran_target_objs.
            (*-*-darwin*): Set fortran_target_objs.
            * Makefile.in: Defined new variable FORTRAN_TARGET_OBJS.
            * configure.ac: Substitute fortran_target_objs, set
            FORTRAN_TARGET_OBJS.
            * configure: Regenerated.
    
    gcc/fortran:
    2008-05-29  Daniel Franke  <franke.daniel@gmail.com>
    
            PR target/36348
            * Make-lang.in (F95_OBJS): Added dependency on FORTRAN_TARGET_OBJS.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@136178 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/darwin-f.c
@@ -0,0 +1,60 @@
+/* Darwin support needed only by Fortran frontends.
+   Copyright (C) 2008 Free Software Foundation, Inc.
+   Contributed by Daniel Franke.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+
+/* Provide stubs for the hooks defined by darwin.h
+     TARGET_EXTRA_PRE_INCLUDES, TARGET_EXTRA_INCLUDES
+
+   As both, gcc and gfortran link in incpath.o, we can not
+   conditionally undefine said hooks if fortran is build.
+   However, we can define do-nothing stubs of said hooks as
+   we are not interested in objc include files in Fortran.
+
+   The hooks original purpose (see also darwin-c.c):
+    * darwin_register_objc_includes
+      Register the GNU objective-C runtime include path if STDINC.
+
+    * darwin_register_frameworks
+      Register all the system framework paths if STDINC is true and setup
+      the missing_header callback for subframework searching if any
+      frameworks had been registered.  */
+
+
+#include "ansidecl.h"
+
+/* Prototypes for functions below to avoid a lengthy list of includes
+   to achieve the same.  */
+void darwin_register_objc_includes (const char *, const char *, int);
+void darwin_register_frameworks (const char *, const char *, int);
+
+
+void
+darwin_register_objc_includes (const char *sysroot ATTRIBUTE_UNUSED,
+			       const char *iprefix ATTRIBUTE_UNUSED,
+			       int stdinc ATTRIBUTE_UNUSED)
+{
+}
+
+void
+darwin_register_frameworks (const char *sysroot ATTRIBUTE_UNUSED,
+			    const char *iprefix ATTRIBUTE_UNUSED,
+			    int stdinc ATTRIBUTE_UNUSED)
+{
+}
