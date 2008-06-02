ffd6ff7271cb576aa0d49e8939b0c4b3b7e9a720
Andy Hutchinson <hutchinsonandy@aim.com>
2008-06-02 22:30:37 +0000

    pr19340.c: Disable for AVR as it has no scheduling.
    * gcc.dg/pr19340.c: Disable for AVR as it has no scheduling.
    * gcc.dg/section1.c: XFAIL AVR as bss section is used  differently.
    
    From-SVN: r136303

--- gcc/testsuite/gcc.dg/section1.c
+++ gcc/testsuite/gcc.dg/section1.c
@@ -1,5 +1,5 @@
 /* PR optimization/6871 */
 /* Constant variables belong in .rodata, not .bss.  */
-/* { dg-final { scan-assembler-not "\.bss" } } */
+/* { dg-final { scan-assembler-not "\.bss" { xfail avr-*-*} } } */
 
 const int i = 0;
