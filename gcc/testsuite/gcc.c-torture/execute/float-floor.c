0aae926db604eb8b4e5ff09046c571e79429a2d0
Andy Hutchinson <hutchinsonandy@aim.com>
2008-06-03 23:46:07 +0000

    float-floor.c: Adjust test for 4 byte doubles.
    PR/34880
    * gcc.c-torture/execute/float-floor.c: Adjust test for 4 byte doubles.
    
    From-SVN: r136344

--- gcc/testsuite/gcc.c-torture/execute/float-floor.c
+++ gcc/testsuite/gcc.c-torture/execute/float-floor.c
@@ -1,4 +1,10 @@
+
+#if(__SIZEOF_DOUBLE__==8)
 double d = 1024.0 - 1.0 / 32768.0;
+#else
+double d = 1024.0 - 1.0 / 16384.0;
+#endif
+
 extern double floor(double);
 extern float floorf(float);
 extern void abort();
