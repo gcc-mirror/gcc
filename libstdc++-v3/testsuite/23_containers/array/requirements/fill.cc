65ab6c26b0262edcb4a1c5e52d9c480973432b8f
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-06-13 07:35:22 +0000

    2008-06-13  Paolo Carlini  <paolo.carlini@oracle.com>
    	* include/tr1_impl/array (array<>::assign): Do not define in
    	C++0x mode, per DR 776...
    	(array<>::fill): ... define instead.
    	* doc/xml/manual/intro.xml: Add an entry for DR 776.
    	* testsuite/23_containers/array/requirements/fill.cc: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@136741 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/23_containers/array/requirements/fill.cc
@@ -0,0 +1,47 @@
+// { dg-options "-std=gnu++0x" }
+
+// 2008-06-13  Paolo Carlini  <paolo.carlini@oracle.com>
+//
+// Copyright (C) 2008 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#include <array>
+#include <testsuite_hooks.h>
+
+// DR 776. Undescribed assign function of std::array.
+void test01()
+{ 
+  bool test __attribute__((unused)) = true;
+
+  const size_t len = 3;
+  typedef std::array<int, len> array_type;
+
+  array_type a = { { 0, 1, 2 } };
+  const int value = 5;
+
+  a.fill(value);
+  VERIFY( a[0] == value );
+  VERIFY( a[1] == value );
+  VERIFY( a[2] == value );  
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
