b2f0b2a33f40ea27f6f1f130f0e20a3aa44eb02c
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-06-13 10:20:57 +0000

    PR middle-end/36520 	* builtins.c (get_memory_rtx): Test for the presence of DECL_SIZE_UNIT 	before evaluating it.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@136747 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/20080613-1.c
@@ -0,0 +1,40 @@
+/* PR middle-end/36520 */
+/* Testcase by Richard Guenther <rguenth@gcc.gnu.org> */
+
+typedef long unsigned int size_t;
+typedef unsigned short int sa_family_t;
+struct cmsghdr   {
+    size_t cmsg_len;
+    __extension__ unsigned char __cmsg_data [];
+};
+typedef unsigned int uint32_t;
+struct in6_addr   {
+    union       {
+        uint32_t u6_addr32[4];
+    } in6_u;
+};
+struct sockaddr_in   {
+    sa_family_t sin_family;
+};
+struct in6_pktinfo   {
+    struct in6_addr ipi6_addr;
+};
+typedef union {
+    struct sockaddr_in sin;
+} sockaddr_any;
+static sockaddr_any src_addr;
+
+inline struct cmsghdr * cmsg_put(struct cmsghdr *cm, int type, void *data, size_t len)
+{
+    memcpy(((cm)->__cmsg_data), data, len);
+}
+
+int hop_sendmsg(int fd) {
+    struct cmsghdr *cm;
+    if (src_addr.sin.sin_family) {
+        if (src_addr.sin.sin_family == 2) {
+            struct in6_pktinfo info;
+            cm = cmsg_put(cm, 50, &info, sizeof(info));
+        }
+    }
+}
