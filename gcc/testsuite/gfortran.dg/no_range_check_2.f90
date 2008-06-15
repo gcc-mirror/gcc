fb63575c3b199ef16880faf1af93478553f33adf
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-06-15 20:07:34 +0000

    2008-06-15  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR fortran/36515
    	* gfortran.dg/no_range_check_2.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@136823 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/no_range_check_2.f90
@@ -0,0 +1,11 @@
+! { dg-do run }
+! { dg-options "-fno-range-check" }
+! PR36515 Integer read a value overflow for an invalid integer.
+! This tests that -fno-range-check allows this legacy behavior at runtime.
+program int_range
+character(25) :: inputline = "-2147483648"
+integer*4 smallest
+read(inputline,100) smallest
+100 format(1i11)
+if (smallest.ne.-2147483648) call abort
+end
