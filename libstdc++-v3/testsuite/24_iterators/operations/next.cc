65b0413a1927d561d87fe6b0c2b440811014c835
Chris Fairles <chris.fairles@gmail.com>
2008-06-17 21:33:15 +0000

    stl_iterator_base_funcs.h (next, prev): Add in C++0x mode.
    2008-06-17  Chris Fairles  <chris.fairles@gmail.com>
    
    	* include/bits/stl_iterator_base_funcs.h (next, prev): Add in
    	C++0x mode.
    	* testsuite/24_iterators/operations/next.cc: New.
    	* testsuite/24_iterators/operations/prev.cc: Likewise.
    
    From-SVN: r136873

---
+++ libstdc++-v3/testsuite/24_iterators/operations/next.cc
@@ -0,0 +1,55 @@
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2008 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 24.3.4 Iterator operations [iterator.operations]
+
+#include <vector>
+#include <list>
+#include <iterator>
+#include <testsuite_hooks.h>
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  std::vector<int> c(3);
+  std::vector<int>::iterator i = c.begin(), j;
+
+  j = std::next(i, 3);
+  VERIFY( i == c.begin() );
+  VERIFY( j == c.end() );
+}
+
+void test02()
+{
+  bool test __attribute__((unused)) = true;
+  std::list<int> c(3);    
+  std::list<int>::iterator i = c.begin(), j;
+
+  j = std::next(i, 3);
+  VERIFY( i == c.begin() );
+  VERIFY( j == c.end() );
+}
+
+int main()
+{
+  test01();
+  test02();
+  return 0;
+}
