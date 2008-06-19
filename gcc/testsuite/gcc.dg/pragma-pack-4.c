c0043d0647d83519595064a05606d8150dcde4e3
hutchinsonandy <hutchinsonandy@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-06-19 22:03:05 +0000

    * gcc.dg/pragma-pack-4.c: Skip for AVR target.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@136969 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pragma-pack-4.c
+++ gcc/testsuite/gcc.dg/pragma-pack-4.c
@@ -1,5 +1,7 @@
 /* PR c/28286 */
 /* { dg-do compile } */
+/* { dg-skip-if "no Pragma pack" { "avr-*-*" } { "*" } { "" } } */
+
 
 #pragma pack(0.5)         /* { dg-warning "invalid constant" } */
 #pragma pack(push,0.5)    /* { dg-warning "invalid constant" } */
