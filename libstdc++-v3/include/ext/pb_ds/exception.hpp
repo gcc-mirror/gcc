5a473aae8162d101bf593cafe71333b3df326665
Paolo Carlini <paolo.carlini@oracle.com>
2008-06-24 12:04:24 +0000

    exception.hpp (__throw_container_error, [...]): Mark inline.
    2008-06-24  Paolo Carlini  <paolo.carlini@oracle.com>
    	    Chalathip Thumkanon  <chalathip@gmail.com>
    
    	* include/ext/pb_ds/exception.hpp (__throw_container_error,
    	__throw_insert_error, __throw_join_error, __throw_resize_error):
    	Mark inline.
    
    Co-Authored-By: Chalathip Thumkanon <chalathip@gmail.com>
    
    From-SVN: r137066

--- libstdc++-v3/include/ext/pb_ds/exception.hpp
+++ libstdc++-v3/include/ext/pb_ds/exception.hpp
@@ -1,6 +1,6 @@
 // -*- C++ -*-
 
-// Copyright (C) 2005, 2006, 2007 Free Software Foundation, Inc.
+// Copyright (C) 2005, 2006, 2007, 2008 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the terms
@@ -72,35 +72,35 @@ namespace __gnu_pbds
   struct resize_error : public container_error { };
 
 #if __EXCEPTIONS
-  void
+  inline void
   __throw_container_error(void)
   { throw container_error(); }
 
-  void
+  inline void
   __throw_insert_error(void)
   { throw insert_error(); }
 
-  void
+  inline void
   __throw_join_error(void)
   { throw join_error(); }
 
-  void
+  inline void
   __throw_resize_error(void)
   { throw resize_error(); }
 #else
-  void
+  inline void
   __throw_container_error(void)
   { std::abort(); }
 
-  void
+  inline void
   __throw_insert_error(void)
   { std::abort(); }
 
-  void
+  inline void
   __throw_join_error(void)
   { std::abort(); }
 
-  void
+  inline void
   __throw_resize_error(void)
   { std::abort(); }
 #endif
