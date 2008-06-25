d2864a71b9562625502b78ea533df09a5ed11c15
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-06-25 11:13:44 +0000

    2008-06-25  Richard Guenther  <rguenther@suse.de>
    	* tree-ssa-structalias.c (fieldoff_compare): Make sure to
    	not overflow the result type.
    
    	* gcc.c-torture/compile/20080625-1.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@137104 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/20080625-1.c
@@ -0,0 +1,16 @@
+struct peakbufStruct {
+    unsigned int lnum [5000];
+    int lscan [5000][4000];
+    double lmz [5000][4000];
+    double lint [5000][4000];
+    int PeaksInBuf;
+    unsigned char freelists [350000];
+    unsigned char freelistl [5000];
+    unsigned int LastFreeL;
+} peakbuf;
+void foo(int);
+void findmzROI(int i, int *p_scan)
+{
+    foo(peakbuf.PeaksInBuf);
+    __builtin_memmove(p_scan, peakbuf.lscan[i], peakbuf.lnum[i]*sizeof(int));
+}
