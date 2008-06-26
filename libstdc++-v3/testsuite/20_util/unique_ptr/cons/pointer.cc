b3754f1becf5cc8c20df587db515505247a6516e
Chris Fairles <chris.fairles@gmail.com>
2008-06-26 11:36:02 +0000

    pointer_array.cc: New.
    2008-06-26  Chris Fairles  <chris.fairles@gmail.com>
    
            * testsuite/20_util/unique_ptr/cons/pointer_array.cc: New.
            * testsuite/20_util/unique_ptr/cons/pointer.cc: Likewise.
            * testsuite/20_util/unique_ptr/cons/pointer_array_convertible.cc:
            Likewise.
            * testsuite/20_util/unique_ptr/assign/move_array.cc: Likewise.
            * testsuite/20_util/unique_ptr/assign/move.cc: Likewise.
            * testsuite/20_util/unique_ptr/specialized_algorithms/
            comparisons_array.cc: Likewise.
            * testsuite/20_util/unique_ptr/specialized_algorithms/comparisons.cc:
            Likewise
            * testsuite/20_util/unique_ptr/specialized_algorithms/swap.cc:
            Likewise.
    
    From-SVN: r137147

---
+++ libstdc++-v3/testsuite/20_util/unique_ptr/cons/pointer.cc
@@ -0,0 +1,118 @@
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2008 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 20.6.11 Template class unique_ptr [unique.ptr]
+
+#include <memory>
+#include <testsuite_hooks.h>
+
+struct A
+{
+  A() { ++ctor_count; }
+  virtual ~A() { ++dtor_count; }
+  static long ctor_count;
+  static long dtor_count;
+};
+long A::ctor_count = 0;
+long A::dtor_count = 0;
+
+struct B : A
+{
+  B() { ++ctor_count; }
+  virtual ~B() { ++dtor_count; }
+  static long ctor_count;
+  static long dtor_count;
+};
+long B::ctor_count = 0;
+long B::dtor_count = 0;
+
+
+struct reset_count_struct
+{
+  ~reset_count_struct()
+  {
+    A::ctor_count = 0;
+    A::dtor_count = 0;
+    B::ctor_count = 0;
+    B::dtor_count = 0;
+  }
+};
+
+// 20.6.11.2.1 unique_ptr constructors [unique.ptr.single.ctor]
+
+// Construction from pointer
+void
+test01()
+{
+  reset_count_struct __attribute__((unused)) reset;
+  bool test __attribute__((unused)) = true;
+
+  std::unique_ptr<A> A_default;
+  VERIFY( A_default.get() == 0 );
+  VERIFY( A::ctor_count == 0 );
+  VERIFY( A::dtor_count == 0 );
+  VERIFY( B::ctor_count == 0 );
+  VERIFY( B::dtor_count == 0 );
+
+  std::unique_ptr<A> A_from_A(new A);
+  VERIFY( A_from_A.get() != 0 );
+  VERIFY( A::ctor_count == 1 );
+  VERIFY( A::dtor_count == 0 );
+  VERIFY( B::ctor_count == 0 );
+  VERIFY( B::dtor_count == 0 );
+
+  std::unique_ptr<A> A_from_B(new B);
+  VERIFY( A_from_B.get() != 0 );
+  VERIFY( A::ctor_count == 2 );
+  VERIFY( A::dtor_count == 0 );
+  VERIFY( B::ctor_count == 1 );
+  VERIFY( B::dtor_count == 0 );
+}
+
+void
+test02()
+{
+  reset_count_struct __attribute__((unused)) reset;
+  bool test __attribute__((unused)) = true;
+
+  A * const A_default = 0;
+  std::unique_ptr<A> p1(A_default);
+  VERIFY( p1.get() == 0 );
+  VERIFY( A::ctor_count == 0 );
+  VERIFY( A::dtor_count == 0 );
+  VERIFY( B::ctor_count == 0 );
+  VERIFY( B::dtor_count == 0 );
+
+  A * const A_from_A = new A;
+  std::unique_ptr<A> p2(A_from_A);
+  VERIFY( p2.get() == A_from_A );
+  VERIFY( A::ctor_count == 1 );
+  VERIFY( A::dtor_count == 0 );
+  VERIFY( B::ctor_count == 0 );
+  VERIFY( B::dtor_count == 0 );
+}
+
+int
+main()
+{
+  test01();
+  test02();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/20_util/unique_ptr/cons/pointer_array_convertible.cc
@@ -0,0 +1,41 @@
+// { dg-do compile }
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2008 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#include <memory>
+
+struct A
+{
+};
+
+struct B : A
+{
+  virtual ~B() { }
+};
+
+// 20.4.5.1 unique_ptr constructors [unique.ptr.cons]
+
+// Construction from pointer of derived type
+void
+test01()
+{
+  std::unique_ptr<B[]> B_from_A(new A[3]); //{ dg-error "invalid conversion from" }
+}
+//{ dg-excess-errors "initialization" }
