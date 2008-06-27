7133f4fc27ee85d8eab2b827df227149432acca4
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-06-27 22:26:48 +0000

    PR c++/36364 	* repo.c (repo_emit_p): Put const static data members initialized 	by const expr into *.rpo file, just return 2 if IDENTIFIER_REPO_CHOSEN 	for it is 0.
    	* g++.dg/template/repo9.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@137205 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/repo9.C
@@ -0,0 +1,48 @@
+// PR c++/36364
+// { dg-options "-frepo" }
+// { dg-final { cleanup-repo-files } }
+// { dg-require-host-local "" }
+
+template <typename C> struct A
+{
+  static void assign (C &c1, const C &c2) { c1 = c2; }
+};
+
+template <typename C, typename T> struct B
+{
+  struct D
+  {
+    static const C terminal;
+    static unsigned long stor[];
+    static D &empty_rep ()
+    {
+      void *p = reinterpret_cast <void *>(&stor);
+      return *reinterpret_cast <D *>(p);
+    }
+    void test (unsigned long n)
+    {
+      T::assign (this->refdata ()[n], terminal);
+    }
+    C *refdata () throw ()
+    {
+      return reinterpret_cast <C *>(this + 1);
+    }
+  };
+  C *dataplus;
+  C *data () const { return dataplus; }
+  D *rep () const { return &((reinterpret_cast < D * >(data ()))[-1]); }
+  static D & empty_rep () { return D::empty_rep (); }
+  B () : dataplus (empty_rep ().refdata ()) { }
+  ~B () { }
+  void push_back (C c) { rep ()->test (10); }
+};
+
+template <typename C, typename T> const C B <C, T>::D::terminal = C ();
+template <typename C, typename T> unsigned long B <C, T>::D::stor[64];
+
+int
+main ()
+{
+  B <char, A <char> > s;
+  s.push_back ('a');
+}
