e2cefdd9d0a72ed372da289665aa04b1da46dd9c
Ira Rosen <irar@il.ibm.com>
2008-06-30 11:43:55 +0000

    re PR tree-optimization/36648 (segfault in global constructor with -O3)
    	PR tree-optimization/36648
    	* tree-vect-transform.c (vect_do_peeling_for_loop_bound): Divide
    	number of prolog iterations by step. Fix the comment.
    
    From-SVN: r137272

---
+++ gcc/testsuite/g++.dg/vect/pr36648.cc
@@ -0,0 +1,24 @@
+/* { dg-require-effective-target vect_float } */
+
+struct vector
+{
+  vector() : x(0), y(0), z(0) { }
+  float x,y,z;
+};
+
+struct Foo
+{
+  int dummy;
+  /* Misaligned access.  */
+  vector array_of_vectors[4];
+};
+
+Foo foo;
+
+int main() { }
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } }*/
+/* { dg-final { scan-tree-dump-times "vectorizing stmts using SLP" 1 "vect" } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
+
+
