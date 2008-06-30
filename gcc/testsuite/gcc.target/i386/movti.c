b527440e919b835f9a1429d2ee5faa2ce7db111f
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-06-30 21:24:15 +0000

    * config/i386/i386.md (*movti_rex64): Add "!" to "r" constraint 	of operand 0.
    testsuite/ChangeLog:
    
    	* gcc.target/i386/movti.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@137291 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/movti.c
@@ -0,0 +1,10 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target dfp } */
+/* { dg-options "-O -std=gnu99" } */
+
+_Decimal128 test (void)
+{
+  return 1234123412341234.123412341234dl;
+}
+
+/* { dg-final { scan-assembler-not "movabs" } } */
