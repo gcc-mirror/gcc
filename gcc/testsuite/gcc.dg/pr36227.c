d47a9a8e14391207442d93584394cd7d9fcd030e
Andy Hutchinson <hutchinsonandy@aim.com>
2008-07-02 22:24:18 +0000

    * gcc.dg/pr36227.c: Correct test for targets where long is not the same size as pointer.
    From-SVN: r137395

--- gcc/testsuite/gcc.dg/pr36227.c
+++ gcc/testsuite/gcc.dg/pr36227.c
@@ -1,12 +1,22 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -Wstrict-overflow=3" } */
+#if (__SIZEOF_LONG_LONG__ == __SIZEOF_POINTER__)
+typedef unsigned long long ptrcast;
+#elif (__SIZEOF_LONG__ == __SIZEOF_POINTER__)
+typedef unsigned long ptrcast;
+#elif (__SIZEOF_INT__ == __SIZEOF_POINTER__)
+typedef unsigned int ptrcast;
+#else
+#error Add target support here
+#endif
 
 volatile unsigned long *
 sat_add(volatile unsigned long *ptr, unsigned long i, volatile unsigned long *end)
 {
-  if ((unsigned long)ptr + i * sizeof(*ptr) > (unsigned long)ptr) /* { dg-bogus "pointer wraparound" } */
+  if ((ptrcast)ptr + i * sizeof(*ptr) > (ptrcast)ptr) /* { dg-bogus "pointer wraparound" } */
     return ptr + i;
   else
     return end;
 }
+
 
