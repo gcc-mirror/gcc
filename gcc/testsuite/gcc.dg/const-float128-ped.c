1c859206f532eebcb50770c6276e355e1ea3b946
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-07-02 18:54:04 +0000

    2008-07-02  H.J. Lu  <hongjiu.lu@intel.com>
    	* g++.dg/abi/mangle24.C: Remove -mmmx.
    	* gcc.dg/const-float80-ped.c: Likewise.
    	* gcc.dg/const-float80.c: Likewise.
    	* gcc.dg/torture/fp-int-convert-float80.c: Likewise.
    
    	* g++.dg/abi/mangle25.C: Enable x86.
    	* gcc.dg/const-float128-ped.c: Likewise.
    	* gcc.dg/const-float128.c: Likewise.
    	* gcc.dg/torture/fp-int-convert-float128.c: Likewise.
    	* gcc.target/i386/pr32191.c: Likewise.
    	* gcc.target/i386/pr32268.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@137377 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/const-float128-ped.c
+++ gcc/testsuite/gcc.dg/const-float128-ped.c
@@ -1,5 +1,5 @@
 /* Test 'q' suffix with -pedantic on __float128 type constants.  */
-/* { dg-do compile { target { ia64-*-* || { { i?86-*-* x86_64-*-* } && lp64 } } } } */
+/* { dg-do compile { target ia64-*-* i?86-*-* x86_64-*-* } } */
 /* { dg-options "-pedantic" } */
 
 __float128 a = 123.456789q; /* { dg-warning "non-standard suffix on floating constant" } */
