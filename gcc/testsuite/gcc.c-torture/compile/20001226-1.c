b12285bdfc30eeefc48555bc6aa4e571248ee14d
Ulrich Weigand <uweigand@de.ibm.com>
2008-07-02 15:56:31 +0000

    re PR target/36698 (gcc.c-torture/compile/20001226-1.c exceeds SPU local store size with -O0)
    	PR target/36698
    	* gcc.c-torture/compile/20001226-1.c: XFAIL -O0 case on SPU.
    
    	* gcc.dg/pr27095.c: Provide target-specific regexp for SPU.
    
    From-SVN: r137367

--- gcc/testsuite/gcc.c-torture/compile/20001226-1.c
+++ gcc/testsuite/gcc.c-torture/compile/20001226-1.c
@@ -4,6 +4,7 @@
 /* { dg-xfail-if "function larger than 64K" { m6811-*-* } { "*" } { "" } } */
 /* { dg-skip-if "too much code for avr" { "avr-*-*" } { "*" } { "" } } */
 /* { dg-xfail-if "jump beyond 128K not supported" { xtensa-*-* } { "-O0" } { "" } } */
+/* { dg-xfail-if "PR36698" { spu-*-* } { "-O0" } { "" } } */
 /* { dg-skip-if "" { m32c-*-* } { "*" } { "" } } */
 
 /* This testcase exposed two branch shortening bugs on powerpc.  */
