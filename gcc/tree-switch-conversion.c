a167a67620fe7124ed29891ba32e7af676831a3c
Martin Jambor <mjambor@suse.cz>
2008-07-02 11:59:34 +0200

    tree-switch-conversion.c: Included timevar.h
    2008-07-02  Martin Jambor  <mjambor@suse.cz>
    
    	* tree-switch-conversion.c: Included timevar.h
    	(pass_convert_switch): Added a timevar id (TV_TREE_SWITCH_CONVERSION).
    
    	* timevar.def: Added TV_TREE_SWITCH_CONVERSION.
    
    From-SVN: r137349

--- gcc/tree-switch-conversion.c
+++ gcc/tree-switch-conversion.c
@@ -868,7 +868,7 @@ struct gimple_opt_pass pass_convert_switch =
   NULL,					/* sub */
   NULL,					/* next */
   0,					/* static_pass_number */
-  0,					/* tv_id */
+  TV_TREE_SWITCH_CONVERSION,		/* tv_id */
   PROP_cfg | PROP_ssa,	                /* properties_required */
   0,					/* properties_provided */
   0,					/* properties_destroyed */
