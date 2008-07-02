369619d4c5c4971cbb0343e70c6febd1b50a586e
irar <irar@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-07-02 07:04:18 +0000

    * gcc.dg/vect/costmodel/spu/costmodel-vect-iv-9.c: Add noinline 	attribute to main1(). 	* gcc.dg/vect/costmodel/spu/costmodel-vect-33.c: Increase the loop 	bound in order to make it worth to vectorize the loop. 	* gcc.dg/vect/costmodel/spu/costmodel-vect-76b.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@137342 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/vect/costmodel/spu/costmodel-vect-76b.c
+++ gcc/testsuite/gcc.dg/vect/costmodel/spu/costmodel-vect-76b.c
@@ -3,20 +3,20 @@
 #include <stdarg.h>
 #include "../../tree-vect.h"
 
-#define N 8
+#define N 16
 #define OFF 4
 
 /* Check handling of accesses for which the "initial condition" -
    the expression that represents the first location accessed - is
    more involved than just an ssa_name.  */
 
-int ib[N+OFF] __attribute__ ((__aligned__(16))) = {0, 1, 3, 5, 7, 11, 13, 17, 0, 2, 6, 10};
+int ib[N+OFF] __attribute__ ((__aligned__(16))) = {0, 1, 3, 5, 7, 11, 13, 17, 0, 2, 6, 10, 0, 1, 3, 5, 7, 11, 13, 17};
 
 int main1 (int *pib)
 {
   int i;
   int ia[N+OFF];
-  int ic[N+OFF] = {0, 1, 3, 5, 7, 11, 13, 17, 0, 2, 6, 10};
+  int ic[N+OFF] = {0, 1, 3, 5, 7, 11, 13, 17, 0, 2, 6, 10, 0, 1, 3, 5, 7, 11, 13, 17};
 
   for (i = OFF; i < N; i++)
     {
