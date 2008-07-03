a826d79c44a6cafbc06093ce8067e7f27e6df18c
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-07-03 12:14:16 +0000

    gcc/
    2008-07-03  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* config/i386/i386.c (contains_aligned_value_p): Return true
    	for TCmode.
    	(ix86_data_alignment): Align TCmode to 128bits.
    	(ix86_local_alignment): Likewise.
    
    testsuite/
    
    2008-07-03  H.J. Lu  <hongjiu.lu@intel.com>
    	    Uros Bizjak  <ubizjak@gmail.com>
    
    	* gcc.target/i386/float128-1.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@137416 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/float128-1.c
@@ -0,0 +1,24 @@
+/* { dg-do run { target *-*-linux* } } */
+/* { dg-options "-O2 -msse2" } */
+
+#include "sse2-check.h"
+
+extern void abort (void);
+
+typedef _Complex float __attribute__((mode(TC))) _Complex128;
+
+_Complex128 __attribute__ ((noinline))
+foo (_Complex128 x, _Complex128 y)
+{
+  return x * y;
+}
+
+static void
+sse2_test (void)
+{
+  _Complex128 a = 1.3q + 3.4qi, b = 5.6q + 7.8qi, c;
+
+  c = foo (a, b);
+  if (__real__(c) == 0.0q || __imag__ (c) == 0.0q)
+    abort ();
+}
