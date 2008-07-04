4acedca122b595670e54d836ccc4b96801e8bb99
Chris Fairles <chris.fairles@gmail.com>
2008-07-04 18:45:09 +0000

    ratio: New, per N2661.
    2008-07-04  Chris Fairles  <chris.fairles@gmail.com>
    
    	* include/std/ratio: New, per N2661.
    	* include/Makefile.am: Update.
    	* include/Makefile.in: Regenerate.
    	* testsuite/20_util/ratio/cons/cons1.cc: New.
    	* testsuite/20_util/ratio/cons/cons_overflow.cc: Likewise.
    	* testsuite/20_util/ratio/operations/ops1.cc: Likewise.
    	* testsuite/20_util/ratio/operations/ops2.cc: Likewise.
    	* testsuite/20_util/ratio/operations/ops3.cc: Likewise.
    	* testsuite/20_util/ratio/operations/ops_overflow.cc: Likewise.
    	* testsuite/20_util/ratio/comparisons/comp1.cc: Likewise.
    
    From-SVN: r137482

---
+++ libstdc++-v3/testsuite/20_util/ratio/comparisons/comp1.cc
@@ -0,0 +1,87 @@
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2008 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this library; see the file COPYING.  If not, write to
+// the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+#include <ratio>
+#include <testsuite_hooks.h>
+
+#ifdef _GLIBCXX_USE_C99_STDINT_TR1
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+ 
+  VERIFY(( std::ratio_equal<std::ratio<2,6>, std::ratio<1,3>>::value == 1 ));
+  VERIFY(( std::ratio_equal<std::ratio<2,6>, std::ratio<1,4>>::value == 0 ));
+  
+  VERIFY(( std::ratio_not_equal<std::ratio<2,6>, 
+           std::ratio<1,3>>::value == 0 ));
+  VERIFY(( std::ratio_not_equal<std::ratio<2,6>, 
+           std::ratio<1,4>>::value == 1 ));
+}
+
+void
+test02()
+{
+  bool test __attribute__((unused)) = true;
+ 
+  VERIFY(( std::ratio_less<std::ratio<1,4>, std::ratio<1,3>>::value == 1 ));
+  VERIFY(( std::ratio_less<std::ratio<-1,3>, std::ratio<1,3>>::value == 1 ));
+  
+  VERIFY(( std::ratio_less<std::ratio<1,3>, std::ratio<1,4>>::value == 0 ));
+  VERIFY(( std::ratio_less<std::ratio<1,3>, std::ratio<-1,3>>::value == 0 ));
+      
+  VERIFY(( std::ratio_less_equal<std::ratio<-1,3>, 
+           std::ratio<-1,3>>::value == 1 ));
+  VERIFY(( std::ratio_less_equal<std::ratio<1,4>, 
+           std::ratio<1,3>>::value == 1 ));
+  
+  VERIFY(( std::ratio_less_equal<std::ratio<1,4>, 
+           std::ratio<-1,3>>::value == 0 ));
+  VERIFY(( std::ratio_less_equal<std::ratio<1,3>, 
+           std::ratio<-1,3>>::value == 0 ));
+  
+  VERIFY(( std::ratio_greater<std::ratio<1,3>, std::ratio<1,4>>::value == 1 ));
+  VERIFY(( std::ratio_greater<std::ratio<1,3>, std::ratio<-1,3>>::value == 1 ));
+  
+  VERIFY(( std::ratio_greater<std::ratio<1,4>, std::ratio<1,3>>::value == 0 ));
+  VERIFY(( std::ratio_greater<std::ratio<-1,3>, std::ratio<1,3>>::value == 0 ));
+
+  VERIFY(( std::ratio_greater_equal<std::ratio<1,3>, 
+           std::ratio<1,3>>::value == 1 ));
+  VERIFY(( std::ratio_greater_equal<std::ratio<1,3>, 
+           std::ratio<-1,3>>::value == 1 ));
+  
+  VERIFY(( std::ratio_greater_equal<std::ratio<-1,3>, 
+           std::ratio<1,3>>::value == 0 ));
+  VERIFY(( std::ratio_greater_equal<std::ratio<1,4>, 
+           std::ratio<1,3>>::value == 0 ));
+}
+
+#endif //_GLIBCXX_USE_C99_STDINT_TR1
+
+int main()
+{
+#ifdef _GLIBCXX_USE_C99_STDINT_TR1
+  test01();
+  test02();
+#endif
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/20_util/ratio/operations/ops_overflow.cc
@@ -0,0 +1,51 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-do compile }
+
+// 2008-07-03 Chris Fairles <chris.fairles@gmail.com>
+
+// Copyright (C) 2008 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this library; see the file COPYING.  If not, write to
+// the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+#include <ratio>
+
+#ifdef _GLIBCXX_USE_C99_STDINT_TR1
+
+void
+test01()
+{
+  std::ratio_add<std::ratio<INTMAX_MAX, 1>, std::ratio<1>>::type r1;
+}
+
+void
+test02()
+{  
+  std::ratio_multiply<std::ratio<-INTMAX_MAX, 2>, std::ratio<3, 2>>::type r1;
+  std::ratio_multiply<std::ratio<INTMAX_MAX>, std::ratio<INTMAX_MAX>>::type r2;
+}
+
+// { dg-error "instantiated from here" "" { target *-*-* } 31 }
+// { dg-error "instantiated from here" "" { target *-*-* } 37 }
+// { dg-error "instantiated from here" "" { target *-*-* } 38 }
+// { dg-error "overflow in addition" "" { target *-*-* } 127 }
+// { dg-error "overflow in multiplication" "" { target *-*-* } 95 }
+// { dg-error "overflow in multiplication" "" { target *-*-* } 97 }
+// { dg-error "overflow in multiplication" "" { target *-*-* } 99 }
+// { dg-excess-errors "In instantiation of" }
+// { dg-excess-errors "out of range" }
+
+#endif //_GLIBCXX_USE_C99_STDINT_TR1
