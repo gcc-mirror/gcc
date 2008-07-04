0e1f4c6b6f0b65285af7f34443c29c7074736d1c
Richard Guenther <rguenther@suse.de>
2008-07-04 09:34:36 +0000

    tree-ssa-structalias.c (lookup_vi_for_tree): Declare.
    2008-07-04  Richard Guenther  <rguenther@suse.de>
    
    	* tree-ssa-structalias.c (lookup_vi_for_tree): Declare.
    	(do_sd_constraint): Handle a dereference of ESCAPED and CALLUSED
    	properly to compute the reachability set if we do field-sensitive PTA.
    	* invoke.texi (max-fields-for-field-sensitive): Document default.
    	* opts.c (decode_options): Set max-fields-for-field-sensitive to
    	100 for optimize >= 2.
    
    	* gcc.dg/tree-ssa/pta-callused.c: New testcase.
    
    From-SVN: r137453

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pta-callused.c
@@ -0,0 +1,27 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 --param max-fields-for-field-sensitive=2 -fdump-tree-alias" } */
+
+struct Foo {
+  int *p, *q;
+};
+
+int foo (int ***x) __attribute__((pure));
+
+int bar (int b)
+{
+  int i;
+  struct Foo f;
+  int *p, **q;
+  p = &i;
+  f.p = &i;
+  f.q = f.p;
+  if (b)
+    q = &f.p;
+  else
+    q = &f.q;
+  return foo (&q);
+}
+
+/* { dg-final { scan-tree-dump "CALLUSED = { f f.q i q }" "alias" } } */
+/* { dg-final { cleanup-tree-dump "alias" } } */
+
