b622a383be65aaf3ed882495aa71a12fc18426d5
Ralf Wildenhues <rwild@gcc.gnu.org>
2008-07-08 18:11:43 +0000

    Actually add libjava/gnu/gcj/convert/make-trie.h.
    From-SVN: r137636

---
+++ libjava/gnu/gcj/convert/make-trie.h
@@ -0,0 +1,13 @@
+/* Copyright (C) 2008  Free Software Foundation
+
+   This file is part of libgcj.
+
+This software is copyrighted work licensed under the terms of the
+Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
+details.  */
+
+#include <stddef.h>
+
+extern void print_table (char *name, FILE *out);
+
+extern void enter (int key, int value);
