d089c11dea1eac79e1c6a285d30346e16a09f57a
Johannes Singler <singler@ira.uka.de>
2008-07-09 15:33:14 +0000

    random_shuffle.h: (sequential_random_shuffle) Copy data back.
    2008-07-09  Johannes Singler  <singler@ira.uka.de>
    
            * include/parallel/random_shuffle.h:
            (sequential_random_shuffle) Copy data back.
            * testsuite/25_algorithms/random_shuffle/1.cc:
            New. Generic random_shuffle functionality test.
    
    From-SVN: r137659

--- libstdc++-v3/include/parallel/random_shuffle.h
+++ libstdc++-v3/include/parallel/random_shuffle.h
@@ -491,6 +491,9 @@ template<typename RandomAccessIterator, typename RandomNumberGenerator>
                                       rng);
           }
 
+        // Copy elements back.
+        std::copy(target, target + n, begin);
+
         delete[] dist0;
         delete[] dist1;
         delete[] oracles;
