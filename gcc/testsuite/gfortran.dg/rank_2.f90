bdad068304fad30f7082d9e7e6076a1e2ee1c2cf
Tobias Burnus <burnus@net-b.de>
2008-07-17 07:54:42 +0200

    re PR fortran/36825 ([F08] Rank > 7 arrays  [will break library ABI]  libgfortran I/O+intrinsics:)
    2008-07-17  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/36825
            PR fortran/36824
            * array.c (gfc_match_array_spec): Fix array-rank check.
            * resolve.c (resolve_fl_derived): Fix constentness check
            for the array dimensions.
    
    2008-07-17  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/36825
            PR fortran/36824
            * gfortran.dg/rank_2.f90: Add additional array-rank test.
            * gfortran.dg/array_4.f90: New.
    
    From-SVN: r137910

--- gcc/testsuite/gfortran.dg/rank_2.f90
+++ gcc/testsuite/gfortran.dg/rank_2.f90
@@ -5,4 +5,8 @@
 ! Fortran   2008 allows 15 dimensions (including co-array ranks)
 !
 integer :: a(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15) ! { dg-error "more than 7 dimensions" }
+
+! PR fortran/36825:
+integer,parameter                        :: N=10
+complex,dimension(-N:N,-N:N,0:1,0:1,-N:N,-N:N,0:1,0:1)   :: P ! { dg-error "more than 7 dimensions" }
 end
