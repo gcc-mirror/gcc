5ae53a2559819d1628193d5a3d9257029ebe1fe7
H.J. Lu <hongjiu.lu@intel.com>
2008-07-18 15:42:59 +0000

    re PR middle-end/36858 (Incorrect alignment attribute on stack parameter)
    gcc/
    
    2008-07-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR middle-end/36858
    	* function.c (locate_and_pad_parm): Cap boundary earlier.
    
    testsuite/
    
    2008-07-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR middle-end/36858
    	* gcc.target/i386/vararg-1.c: New.
    
    From-SVN: r137954

---
+++ gcc/testsuite/gcc.target/i386/vararg-1.c
@@ -0,0 +1,32 @@
+/* PR middle-end/36858 */
+/* { dg-do run } */
+/* { dg-options "-w" { target { lp64 } } } */
+/* { dg-options "-w -msse2 -mpreferred-stack-boundary=2" { target { ilp32 } } } */
+
+#include "sse2-check.h"
+#include <stdarg.h>
+#include <emmintrin.h>
+
+int
+__attribute__((noinline))
+test (int a, ...)
+{
+  return a;
+}
+
+__m128 n1 = { -283.3, -23.3, 213.4, 1119.03 };
+
+int
+__attribute__((noinline))
+foo (void)
+{
+  return test (1, n1);
+}
+
+static void
+__attribute__((noinline))
+sse2_test (void)
+{
+  if (foo () != 1)
+    abort ();
+}
