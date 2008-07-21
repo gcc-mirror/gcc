279086c39066177adac8f9589683429e01c325a3
Paolo Carlini <paolo.carlini@oracle.com>
2008-07-21 16:18:38 +0000

    re PR c++/36871 (__has_nothrow_copy(T) false for T with a template ctor)
    /cp
    2008-07-21  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/36871
    	PR c++/36872
    	* semantics.c (classtype_has_nothrow_assign_or_copy_p): Only check
    	copy constructors and copy assignment operators proper.
    
    /testsuite
    2008-07-21  Paolo Carlini  <paolo.carlini@oracle.com>
    
            PR c++/36871
    	PR c++/36872
            * g++.dg/ext/has_nothrow_copy.C: Rename to...
            * g++.dg/ext/has_nothrow_copy-1.C: ... this.
            * g++.dg/ext/has_nothrow_copy-2.C: New.
            * g++.dg/ext/has_nothrow_copy-3.C: Likewise.
            * g++.dg/ext/has_nothrow_copy-4.C: Likewise.
            * g++.dg/ext/has_nothrow_copy-5.C: Likewise.
            * g++.dg/ext/has_nothrow_copy-6.C: Likewise.
            * g++.dg/ext/has_nothrow_copy-7.C: Likewise.
    
    From-SVN: r138034

---
+++ gcc/testsuite/g++.dg/ext/has_nothrow_copy-2.C
@@ -0,0 +1,12 @@
+// PR c++/36871
+// { dg-do "run" }
+#include <cassert>
+
+struct A { template <class T> A (T) throw (int); };
+struct B { B (B&) throw (); template <class T> B (T) throw (int); };
+
+int main ()
+{
+  assert (__has_nothrow_copy (A));
+  assert (__has_nothrow_copy (B));
+}
---
+++ gcc/testsuite/g++.dg/ext/has_nothrow_copy-5.C
@@ -0,0 +1,13 @@
+// PR c++/36872
+// { dg-do "run" }
+#include <cassert>
+
+struct S {
+    S (const S&) throw ();
+    S (int) throw (int);
+};
+
+int main ()
+{
+  assert (__has_nothrow_copy (S));
+}
