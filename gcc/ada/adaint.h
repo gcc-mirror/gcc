fdcfe366d3604729089b67c2ca80515761dcb8b0
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-08-04 12:50:41 +0000

    2008-08-04  Pascal Obry  <obry@adacore.com>
    	* adaint.h: Add missing prototype.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@138614 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/adaint.h
+++ gcc/ada/adaint.h
@@ -101,6 +101,7 @@ extern int    __gnat_is_absolute_path              (char *,int);
 extern int    __gnat_is_directory		   (char *);
 extern int    __gnat_is_writable_file		   (char *);
 extern int    __gnat_is_readable_file		   (char *name);
+extern int    __gnat_is_executable_file            (char *name);
 extern void   __gnat_set_readonly                  (char *name);
 extern void   __gnat_set_writable                  (char *name);
 extern void   __gnat_set_executable                (char *name);
