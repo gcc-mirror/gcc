9e071d06dfb8dbd1e2f76f26c918bce0a1e5fb9e
Ulrich Weigand <uweigand@de.ibm.com>
2008-08-05 13:53:20 +0000

    spu.h (CANNOT_CHANGE_MODE_CLASS): Allow (multi)word-sized SUBREG of multi-word hard register.
    	* config/spu/spu.h (CANNOT_CHANGE_MODE_CLASS): Allow (multi)word-sized
    	SUBREG of multi-word hard register.
    	* config/spu/spu.c (valid_subreg): Likewise.
    	(adjust_operand): Handle SUBREGs of multi-word hard registers.
    
    From-SVN: r138711

--- gcc/config/spu/spu.h
+++ gcc/config/spu/spu.h
@@ -263,6 +263,7 @@ enum reg_class {
    only true for SPU. */
 #define CANNOT_CHANGE_MODE_CLASS(FROM, TO, CLASS) \
         ((GET_MODE_SIZE (FROM) > 4 || GET_MODE_SIZE (TO) > 4) \
+	 && (GET_MODE_SIZE (FROM) < 16 || GET_MODE_SIZE (TO) < 16) \
 	 && GET_MODE_SIZE (FROM) != GET_MODE_SIZE (TO))
 
 #define REGISTER_TARGET_PRAGMAS() do {					\
