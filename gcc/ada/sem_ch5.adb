dab17737dfa9e0ecc64ba5bbec40c8f1aa76a71e
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-08-05 14:37:44 +0000

    2008-08-05  Ed Schonberg  <schonberg@adacore.com>
    	* sem_ch5.adb (Analyze_Assignment_Statement): Apply conversion to
    	right-hand side when it is an anonymous access_to_subprogram, to force
    	static accessibility check when needed.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@138723 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/sem_ch5.adb
+++ gcc/ada/sem_ch5.adb
@@ -581,16 +581,19 @@ package body Sem_Ch5 is
 
       --  Ada 2005 (AI-385): When the lhs type is an anonymous access type,
       --  apply an implicit conversion of the rhs to that type to force
-      --  appropriate static and run-time accessibility checks. This
-      --  applies as well to anonymous access-to-subprogram types that
-      --  are component subtypes.
+      --  appropriate static and run-time accessibility checks. This applies
+      --  as well to anonymous access-to-subprogram types that are component
+      --  subtypes or formal parameters.
 
       if Ada_Version >= Ada_05
         and then Is_Access_Type (T1)
-        and then Is_Local_Anonymous_Access (T1)
       then
-         Rewrite (Rhs, Convert_To (T1, Relocate_Node (Rhs)));
-         Analyze_And_Resolve (Rhs, T1);
+         if Is_Local_Anonymous_Access (T1)
+           or else Ekind (T2) = E_Anonymous_Access_Subprogram_Type
+         then
+            Rewrite (Rhs, Convert_To (T1, Relocate_Node (Rhs)));
+            Analyze_And_Resolve (Rhs, T1);
+         end if;
       end if;
 
       --  Ada 2005 (AI-231): Assignment to not null variable
