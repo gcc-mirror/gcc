6d65617886e363fdf518567364af5f5619adfaf4
Marc Gauthier <marc@tensilica.com>
2008-08-06 16:02:16 +0000

    config.gcc: Match more processor names for Xtensa.
    2008-08-06  Marc Gauthier  <marc@tensilica.com>
    	* config.gcc: Match more processor names for Xtensa.
    	* configure.ac: Likewise.
    	* doc/install.texi (Specific): Likewise.
    	* configure: Regenerate.
    testsuite/
    	* lib/target-supports.exp (check_profiling_available): Match more
    	processor names for Xtensa.
    	* g++.old-deja/g++.jason/thunk3.C: Likewise.
    	* gcc.dg/intmax_t-1.c: Likewise.
    	* gcc.dg/sibcall-3.c: Likewise.
    	* gcc.dg/sibcall-4.c: Likewise.
    	* gcc.c-torture/compile/20001226-1.c: Likewise.
    
    From-SVN: r138810

--- gcc/testsuite/gcc.c-torture/compile/20001226-1.c
+++ gcc/testsuite/gcc.c-torture/compile/20001226-1.c
@@ -3,7 +3,7 @@
 /* { dg-do assemble } */
 /* { dg-xfail-if "function larger than 64K" { m6811-*-* } { "*" } { "" } } */
 /* { dg-skip-if "too much code for avr" { "avr-*-*" } { "*" } { "" } } */
-/* { dg-xfail-if "jump beyond 128K not supported" { xtensa-*-* } { "-O0" } { "" } } */
+/* { dg-xfail-if "jump beyond 128K not supported" { xtensa*-*-* } { "-O0" } { "" } } */
 /* { dg-xfail-if "PR36698" { spu-*-* } { "-O0" } { "" } } */
 /* { dg-skip-if "" { m32c-*-* } { "*" } { "" } } */
 
