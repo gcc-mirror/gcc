270349caad48c7655a083229ec423e6ea39d1c3d
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-08-06 08:57:21 +0000

    2008-08-06  Thomas Quinot  <quinot@adacore.com>
    	* g-socket-dummy.adb, g-socket-dummy.ads, g-sothco-dummy.adb,
    	g-sothco-dummy.ads, g-socthi-dummy.adb, g-socthi-dummy.ads,
    	g-sttsne-dummy.ads: New files.
    
    	* gcc-interface/Makefile.in, Makefile.rtl: Use placeholder sources
    	with pragma Unimplemented_Unit for sockets packages on Nucleus.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@138781 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/ada/g-sothco-dummy.adb
@@ -0,0 +1,34 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                         GNAT COMPILER COMPONENTS                         --
+--                                                                          --
+--              G N A T . S O C K E T S . T H I N _ C O M M O N             --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--                       Copyright (C) 2008, AdaCore                        --
+--                                                                          --
+-- GNAT is free software;  you can  redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License  distributed with GNAT;  see file COPYING.  If not, write --
+-- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
+-- Boston, MA 02110-1301, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+-- GNAT was originally developed  by the GNAT team at  New York University. --
+-- Extensive contributions were provided by Ada Core Technologies Inc.      --
+--                                                                          --
+------------------------------------------------------------------------------
+
+pragma No_Body;
