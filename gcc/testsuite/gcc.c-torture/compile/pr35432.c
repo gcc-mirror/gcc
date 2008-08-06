a28d1b79f7aeaebe09045a716e111c403a5df750
Aldy Hernandez <aldyh@redhat.com>
2008-08-06 10:31:22 +0000

    re PR middle-end/35432 (ICE with zero-sized array)
            PR middle-end/35432
            * gimplify.c (gimplify_modify_expr): Do not optimize zero-sized types
            if want_value.
     testsuite/
            * gcc.c-torture/compile/pr35432.c: New file.
    
    From-SVN: r138794

---
+++ gcc/testsuite/gcc.c-torture/compile/pr35432.c
@@ -0,0 +1,22 @@
+/* PR middle-end/35432 */
+
+struct A
+{
+  char c[0];
+};
+
+void foo(struct A a)
+{
+  (a = a).c;
+}
+/* PR middle-end/35432 */
+
+struct A
+{
+  char c[0];
+};
+
+void foo(struct A a)
+{
+  (a = a).c;
+}
