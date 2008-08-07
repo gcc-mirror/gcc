9aa4d3c1cbd78511c868b2361eb033d685e1dc25
Richard Guenther <rguenther@suse.de>
2008-08-07 10:01:48 +0000

    re PR middle-end/37042 (Strict-aliasing warnings are printed for _mm_load_si128, even though __m128i is __attribute__((__may_alias__)).)
    2008-08-07  Richard Guenther  <rguenther@suse.de>
    
    	PR middle-end/37042
    	* tree-ssa-alias-warnings.c (nonstandard_alias_p): Ref-all
    	pointers can access anything.
    
    	* gcc.dg/Wstrict-aliasing-bogus-ref-all-2.c: New testcase.
    
    From-SVN: r138837

---
+++ gcc/testsuite/gcc.dg/Wstrict-aliasing-bogus-ref-all-2.c
@@ -0,0 +1,24 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -Wall" } */
+
+typedef long long __m128i __attribute__ ((__vector_size__ (16), __may_alias__));
+
+extern __inline __m128i __attribute__((__gnu_inline__, __always_inline__, __artificial__))
+_mm_load_si128 (__m128i const *__P)
+{
+  return *__P;
+}
+
+static const short __attribute__((__aligned__(16))) tbl[8] =
+{ 1, 2, 3, 4, 5, 6, 7, 8};
+
+
+__m128i get_vec(void)
+{
+  __m128i ret;
+
+  ret = _mm_load_si128((__m128i *)tbl); /* { dg-bogus "type-punning" } */
+
+  return ret;
+}
+
