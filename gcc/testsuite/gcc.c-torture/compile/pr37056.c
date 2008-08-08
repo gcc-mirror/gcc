b87477876194a77fd92bf1535bd9a690cd33067f
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-08-08 11:30:13 +0000

    2008-08-08  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/37056
    	* gimple.h (gimple_assign_rhs_class): New helper function.
    	* tree-ssa-loop-niter.c (get_val_for): Fix tuplification, handle
    	unary operations properly.
    
    	* gcc.c-torture/compile/pr37056.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@138865 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr37056.c
@@ -0,0 +1,28 @@
+extern void abort (void);
+
+static union {
+    char buf[12 * sizeof (long long)];
+} u;
+
+int main ()
+{
+  int off, len, i;
+  char *p, *q;
+
+  for (off = 0; off < (sizeof (long long)); off++)
+    for (len = 1; len < (10 * sizeof (long long)); len++)
+      {
+	for (i = 0; i < (12 * sizeof (long long)); i++)
+	  u.buf[i] = 'a';
+	p = (__extension__ (__builtin_constant_p ('\0') && ('\0') == '\0'
+			    ? ({void *__s = (u.buf + off); __s;})
+			    : __builtin_memset (u.buf + off, '\0', len)));
+	if (p != u.buf + off)
+	  abort ();
+	for (i = 0; i < off; i++, q++)
+	  if (*q != 'a')
+	    abort ();
+      }
+  return 0;
+}
+
