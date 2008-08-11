6f21ff3ed8e443469a52c3e97865c3548253764d
Jakub Jelinek <jakub@redhat.com>
2008-08-11 12:23:08 +0200

    re PR rtl-optimization/36998 (Ada bootstrap broken on i586-*-*)
    	PR rtl-optimization/36998
    	* dwarf2out.c (compute_barrier_args_size_1,
    	compute_barrier_args_size): Temporarily remove assertions.
    
    	* gcc.dg/pr36998.c: New test.
    
    From-SVN: r138951

---
+++ gcc/testsuite/gcc.dg/pr36998.c
@@ -0,0 +1,23 @@
+/* PR rtl-optimization/36998 */
+/* { dg-do compile } */
+/* { dg-options "-Os -fasynchronous-unwind-tables" } */
+/* { dg-options "-Os -mpreferred-stack-boundary=2 -fasynchronous-unwind-tables" { target { { i?86-*-* x86_64-*-* } && ilp32 } } } */
+
+void foo (const char *, ...) __attribute__ ((noreturn));
+int bar (const char *, ...);
+extern __SIZE_TYPE__ strlen (const char *);
+int baz (char *, char *, int, void *);
+
+void
+test (char *w, int x, char *y, char *z)
+{
+  char *p, b[32];
+  for (p = y; *p; p += strlen (p) + 1)
+    {
+      baz (w, p, x, z);
+      foo ("msg1 %s", b);
+    }
+  for (p = y; *p; p += strlen (p) + 1)
+    bar (" %s", p);
+  foo ("msg2 %s", b);
+}
