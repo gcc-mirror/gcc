bb8a5b522594d2caa31ac38c4faeb85087106024
Paolo Carlini <paolo@gcc.gnu.org>
2008-08-12 20:05:02 +0000

    decl5.C: Expand new expected errors.
    2008-08-12  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* g++.old-deja/g++.other/decl5.C: Expand new expected errors.
    
    From-SVN: r139035

--- gcc/testsuite/g++.old-deja/g++.other/decl5.C
+++ gcc/testsuite/g++.old-deja/g++.other/decl5.C
@@ -16,7 +16,7 @@ struct A {
   int m;
   };
   struct Z;
-  expand me;          // { dg-error "" } not name a type
+  expand me;          // { dg-error "error: 'expand' does not name a type" }
   void foo(struct A::e);
   void foo(struct A::z);  // { dg-warning "" } extra qualification
 };
@@ -71,7 +71,7 @@ namespace NMS
   };
 }
 
-NMS::D thing;         // { dg-error "" } not name a type
+NMS::D thing;         // { dg-error "error: 'D' in namespace 'NMS' does not name a type" }
 void NMS::fn()
 {
   i = 3;
