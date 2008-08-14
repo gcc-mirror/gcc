6d419a6ef357429e73332bbecdbe63101f8f4c38
Paolo Carlini <paolo.carlini@oracle.com>
2008-08-14 01:17:09 +0000

    Re-instate last patch...
    2008-08-13  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	Re-instate last patch, amended to use __exchange_and_add_dispatch
    	and __atomic_add_dispatch in eh_ptr.cc and eh_throw.cc.
    
    From-SVN: r139091

---
+++ libstdc++-v3/testsuite/18_support/exception_ptr/rethrow_exception.cc
@@ -0,0 +1,113 @@
+// { dg-options "-std=gnu++0x" }
+// 2008-05-25  Sebastian Redl  <sebastian.redl@getdesigned.at>
+
+// Copyright (C) 2008 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// rethrow_exception() and preservation of data
+
+#include <exception>
+#include <typeinfo>
+#include <cstring>
+#include <stdexcept>
+#include <testsuite_hooks.h>
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace std;
+
+  try {
+    rethrow_exception(copy_exception(0));
+  } catch(...) {
+  }
+}
+
+void test02()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace std;
+
+  try {
+    rethrow_exception(copy_exception(runtime_error("test")));
+  } catch(exception &e) {
+    VERIFY( typeid(e) == typeid(runtime_error) );
+    VERIFY( strcmp(e.what(), "test") == 0 );
+  }
+}
+
+void test03()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace std;
+
+  exception_ptr ep;
+  try {
+    throw 0;
+  } catch(...) {
+    ep = current_exception();
+  }
+  try {
+    rethrow_exception(ep);
+  } catch(...) {
+  }
+}
+
+void test04()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace std;
+
+  // Weave the exceptions in an attempt to confuse the machinery.
+  try {
+    throw 0;
+  } catch(...) {
+    exception_ptr ep1 = current_exception();
+    try {
+      throw 1;
+    } catch(...) {
+      exception_ptr ep2 = current_exception();
+      try {
+        rethrow_exception(ep1);
+      } catch(...) {
+        try {
+          rethrow_exception(ep2);
+        } catch(...) {
+          try {
+            rethrow_exception(ep1);
+          } catch(...) {
+          }
+          try {
+            rethrow_exception(ep2);
+          } catch(...) {
+          }
+        }
+      }
+    }
+  }
+}
+
+int main()
+{
+  test01();
+  test02();
+  test03();
+  test04();
+
+  return 0;
+}
