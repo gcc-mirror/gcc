e1046890e76463bf47a4876bbbd016008b5aee26
Manuel López-Ibáñez <manu@gcc.gnu.org>
2008-08-15 11:23:20 +0000

    pr30551-6.c: Skip for SPU.
    2008-08-15  Manuel Lopez-Ibanez  <manu@gcc.gnu.org>
    
    testsuite/
    	* gcc.dg/pr30551-6.c: Skip for SPU.
    	* gcc.dg/pr30551-3.c: Likewise.
    	* gcc.dg/pr30551.c: Likewise.
    	* g++.dg/warn/pr30551-2.C: Likewise.
    	* g++.dg/warn/pr30551.C: Likewise.
    
    From-SVN: r139126

--- gcc/testsuite/g++.dg/warn/pr30551-2.C
+++ gcc/testsuite/g++.dg/warn/pr30551-2.C
@@ -1,6 +1,6 @@
 // PR 30551 -Wmain is enabled by -pedantic/-pedantic-errors.
 // { dg-do compile }
 // { dg-options "-pedantic-errors" }
-
+// { dg-skip-if "-Wmain not enabled with -pedantic on SPU" { spu-*-* } } 
 int main(char a) {} /* { dg-error "error: first argument of .*main.* should be .int." } */
 /* { dg-error "error: .*main.* takes only zero or two arguments" "" { target *-*-* } 5 } */ 
--- gcc/testsuite/gcc.dg/pr30551-6.c
+++ gcc/testsuite/gcc.dg/pr30551-6.c
@@ -1,7 +1,7 @@
 /* PR 30551 -Wmain is enabled by -pedantic. */
 /* { dg-do compile } */
 /* { dg-options "-pedantic" } */
-
+/* { dg-skip-if "-Wmain not enabled with -pedantic on SPU" { spu-*-* } } */
 void main(char a) {} /* { dg-warning "first argument of .main. should be .int." } */
 /* { dg-warning ".main. takes only zero or two arguments" "" { target *-*-* } 5 } */ 
 /* { dg-warning "return type of .main. is not .int." "" { target *-*-* } 5 } */ 
