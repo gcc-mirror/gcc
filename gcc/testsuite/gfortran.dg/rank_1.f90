5ca16c548b7739e67ec4adc9a5ce233613eeb9d4
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-08-16 15:53:14 +0000

    2008-08-16  Tobias Burnus  <burnus@net-b.de>
            PR fortran/36825
            * libgfortran.h: Reduce GFC_MAX_DIMENSIONS to 7.
    
    2008-08-16  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/36825
            * gfortran.dg/rank_1.f90: Modify error message.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@139152 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/rank_1.f90
+++ gcc/testsuite/gfortran.dg/rank_1.f90
@@ -4,6 +4,7 @@
 ! Fortran < 2008 allows 7  dimensions
 ! Fortran   2008 allows 15 dimensions (including co-array ranks)
 !
-integer :: a(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
-integer :: b(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16) ! { dg-error "has more than 15 dimensions" }
+! FIXME: Rank patch was reverted because of PR 36825.
+integer :: a(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15) ! { dg-error "has more than 7 dimensions" }
+integer :: b(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16) ! { dg-error "has more than 7 dimensions" }
 end
