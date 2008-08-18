826174ed5baf39165e89813106f5df7bc36021c7
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-08-18 18:21:01 +0000

    gcc/ 	* rtlanal.c (subreg_offset_representable_p): Check HARD_REGNO_MODE_OK.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@139207 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/rtlanal.c
+++ gcc/rtlanal.c
@@ -3241,7 +3241,8 @@ subreg_offset_representable_p (unsigned int xregno, enum machine_mode xmode,
 {
   struct subreg_info info;
   subreg_get_info (xregno, xmode, offset, ymode, &info);
-  return info.representable_p;
+  return (info.representable_p
+	  && HARD_REGNO_MODE_OK (xregno + info.offset, ymode));
 }
 
 /* Return the final regno that a subreg expression refers to.  */
