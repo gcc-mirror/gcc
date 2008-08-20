c4ab2baad2c3e42f8afdeccd96b4bccdefbcb19e
Richard Guenther <rguenther@suse.de>
2008-08-20 08:28:17 +0000

    tree-vrp.c (found_in_subgraph): Remove.
    2008-08-20  Richard Guenther  <rguenther@suse.de>
    
    	* tree-vrp.c (found_in_subgraph): Remove.
    	(live): New global static.
    	(live_on_edge): New function.
    	(blocks_visited): Remove.
    	(register_edge_assert_for_2): Use live_on_edge.
    	(find_conditional_asserts): Remove code dealing with
    	found_in_subgraph.  Do not walk the CFG.
    	(find_switch_asserts): Likewise.
    	(find_assert_locations_1): Renamed from find_assert_locations.
    	Move finding assert locations for conditional and switch
    	statements first.  Update live bitmap.  Do not walk the CFG.
    	(find_assert_locations): New function.
    	(insert_range_assertions): Remove entry of CFG walk.
    	Adjust call to find_assert_locations.
    	* tree-ssa-pre.c (do_regular_insertion): Ignore critical edges
    	that only can appear because of fake exit edges but assert we
    	never try to insert on those.
    	(fini_pre): Do not remove fake exit edges here...
    	(execute_pre): ...but here, before committing edge inserts.
    
    	* gcc.dg/tree-ssa/pr20701.c: Scan vrp1 dump.
    	* gcc.dg/tree-ssa/ssa-dom-thread-1.c: Pass -fno-tree-vrp.
    	* gcc.dg/tree-ssa/ssa-pre-20.c: New testcase.
    
    From-SVN: r139263

--- gcc/testsuite/gcc.dg/tree-ssa/ssa-dom-thread-1.c
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-dom-thread-1.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */ 
-/* { dg-options "-O2 -fdump-tree-dom1-details" } */
+/* { dg-options "-O2 -fno-tree-vrp -fdump-tree-dom1-details" } */
 void t(void);
 void q(void);
 void q1(void);
