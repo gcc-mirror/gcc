7a4b9af6316315fef771961b3dc88aa8f7b3ba24
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-08-20 19:29:54 +0000

    2008-08-20  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR libstdc++/33979 (partial)
    	* include/bits/postypes.h (u16streampos, u32streampos): Add.
    	* include/bits/char_traits.h (char_traits<char16_t>,
    	char_traits<char32_t>): Add.
    	* include/bits/stringfwd.h (u16string, u32string): Add.
    	* include/ext/vstring_fwd.h: Add typedefs for char16_t/char32_t.
    	* testsuite/21_strings/char_traits/requirements/char32_t/typedefs.cc:
    	New.
    	* testsuite/21_strings/char_traits/requirements/
    	explicit_instantiation/short/1.cc: Likewise.
    	* testsuite/21_strings/char_traits/requirements/
    	explicit_instantiation/char32_t/1.cc: Likewise.
    	* testsuite/21_strings/char_traits/requirements/
    	explicit_instantiation/wchar_t/1.cc: Likewise.
    	* testsuite/21_strings/char_traits/requirements/
    	explicit_instantiation/char16_t/1.cc: Likewise.
    	* testsuite/21_strings/char_traits/requirements/
    	explicit_instantiation/char/1.cc: Likewise.
    	* testsuite/21_strings/char_traits/requirements/char16_t/typedefs.cc:
    	Likewise.
    	* testsuite/21_strings/basic_string/requirements/
    	explicit_instantiation/char32_t/1.cc: Likewise.
    	* testsuite/21_strings/basic_string/requirements/
    	explicit_instantiation/wchar_t/1.cc: Likewise.
    	* testsuite/21_strings/basic_string/requirements/
    	explicit_instantiation/char16_t/1.cc: Likewise.
    	* testsuite/21_strings/basic_string/requirements/
    	explicit_instantiation/char/1.cc: Likewise.
    	* testsuite/21_strings/headers/string/types_std_c++0x.cc: Likewise.
    	* testsuite/ext/vstring/requirements/explicit_instantiation/
    	char32_t/1.cc: Likewise.
    	* testsuite/ext/vstring/requirements/explicit_instantiation/
    	char16_t/1.cc: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@139339 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/char_traits.h
+++ libstdc++-v3/include/bits/char_traits.h
@@ -374,6 +374,195 @@ _GLIBCXX_BEGIN_NAMESPACE(std)
 
 _GLIBCXX_END_NAMESPACE
 
+#if (defined(__GXX_EXPERIMENTAL_CXX0X__) \
+     && defined(_GLIBCXX_USE_C99_STDINT_TR1))
+
+#include <cstdint>
+
+_GLIBCXX_BEGIN_NAMESPACE(std)
+
+  template<>
+    struct char_traits<char16_t>
+    {
+      typedef char16_t          char_type;
+      typedef uint_least16_t    int_type;
+      typedef streamoff         off_type;
+      typedef u16streampos      pos_type;
+      typedef mbstate_t         state_type;
+
+      static void
+      assign(char_type& __c1, const char_type& __c2)
+      { __c1 = __c2; }
+
+      static bool
+      eq(const char_type& __c1, const char_type& __c2)
+      { return __c1 == __c2; }
+
+      static bool
+      lt(const char_type& __c1, const char_type& __c2)
+      { return __c1 < __c2; }
+
+      static int
+      compare(const char_type* __s1, const char_type* __s2, size_t __n)
+      {
+	for (size_t __i = 0; __i < __n; ++__i)
+	  if (lt(__s1[__i], __s2[__i]))
+	    return -1;
+	  else if (lt(__s2[__i], __s1[__i]))
+	    return 1;
+	return 0;
+      }
+
+      static size_t
+      length(const char_type* __s)
+      {
+	size_t __i = 0;
+	while (!eq(__s[__i], char_type()))
+	  ++__i;
+	return __i;
+      }
+
+      static const char_type*
+      find(const char_type* __s, size_t __n, const char_type& __a)
+      {
+	for (size_t __i = 0; __i < __n; ++__i)
+	  if (eq(__s[__i], __a))
+	    return __s + __i;
+	return 0;
+      }
+
+      static char_type*
+      move(char_type* __s1, const char_type* __s2, size_t __n)
+      {
+	return (static_cast<char_type*>
+		(__builtin_memmove(__s1, __s2, __n * sizeof(char_type))));
+      }
+
+      static char_type*
+      copy(char_type* __s1, const char_type* __s2, size_t __n)
+      {
+	return (static_cast<char_type*>
+		(__builtin_memcpy(__s1, __s2, __n * sizeof(char_type))));
+      }
+
+      static char_type*
+      assign(char_type* __s, size_t __n, char_type __a)
+      { 
+	std::fill_n(__s, __n, __a);
+	return __s;
+      }
+
+      static char_type
+      to_char_type(const int_type& __c) { return char_type(__c); }
+
+      static int_type
+      to_int_type(const char_type& __c) { return int_type(__c); }
+
+      static bool
+      eq_int_type(const int_type& __c1, const int_type& __c2)
+      { return __c1 == __c2; }
+
+      static int_type
+      eof() { return static_cast<int_type>(-1); }
+
+      static int_type
+      not_eof(const int_type& __c)
+      { return eq_int_type(__c, eof()) ? 0 : __c; }
+    };
+
+  template<>
+    struct char_traits<char32_t>
+    {
+      typedef char32_t          char_type;
+      typedef uint_least32_t    int_type;
+      typedef streamoff         off_type;
+      typedef u32streampos      pos_type;
+      typedef mbstate_t         state_type;
+
+      static void
+      assign(char_type& __c1, const char_type& __c2)
+      { __c1 = __c2; }
+
+      static bool
+      eq(const char_type& __c1, const char_type& __c2)
+      { return __c1 == __c2; }
+
+      static bool
+      lt(const char_type& __c1, const char_type& __c2)
+      { return __c1 < __c2; }
+
+      static int
+      compare(const char_type* __s1, const char_type* __s2, size_t __n)
+      {
+	for (size_t __i = 0; __i < __n; ++__i)
+	  if (lt(__s1[__i], __s2[__i]))
+	    return -1;
+	  else if (lt(__s2[__i], __s1[__i]))
+	    return 1;
+	return 0;
+      }
+
+      static size_t
+      length(const char_type* __s)
+      {
+	size_t __i = 0;
+	while (!eq(__s[__i], char_type()))
+	  ++__i;
+	return __i;
+      }
+
+      static const char_type*
+      find(const char_type* __s, size_t __n, const char_type& __a)
+      {
+	for (size_t __i = 0; __i < __n; ++__i)
+	  if (eq(__s[__i], __a))
+	    return __s + __i;
+	return 0;
+      }
+
+      static char_type*
+      move(char_type* __s1, const char_type* __s2, size_t __n)
+      {
+	return (static_cast<char_type*>
+		(__builtin_memmove(__s1, __s2, __n * sizeof(char_type))));
+      }
+
+      static char_type*
+      copy(char_type* __s1, const char_type* __s2, size_t __n)
+      { 
+	return (static_cast<char_type*>
+		(__builtin_memcpy(__s1, __s2, __n * sizeof(char_type))));
+      }
+
+      static char_type*
+      assign(char_type* __s, size_t __n, char_type __a)
+      {
+	std::fill_n(__s, __n, __a);
+	return __s;
+      }
+
+      static char_type
+      to_char_type(const int_type& __c) { return char_type(__c); }
+
+      static int_type
+      to_int_type(const char_type& __c) { return int_type(__c); }
+
+      static bool
+      eq_int_type(const int_type& __c1, const int_type& __c2)
+      { return __c1 == __c2; }
+
+      static int_type
+      eof() { return static_cast<int_type>(-1); }
+
+      static int_type
+      not_eof(const int_type& __c)
+      { return eq_int_type(__c, eof()) ? 0 : __c; }
+    };
+
+_GLIBCXX_END_NAMESPACE
+
+#endif 
+
 #undef _CHAR_TRAITS_EOF
 
-#endif
+#endif // _CHAR_TRAITS_H
