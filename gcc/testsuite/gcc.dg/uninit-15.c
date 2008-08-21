96501113618dd86ec855db2931c1b5f9038c3e51
Richard Guenther <rguenther@suse.de>
2008-08-21 11:22:52 +0000

    re PR testsuite/37182 (Revision 139286 caused gcc.dg/pr17506.c and gcc.dg/uninit-15.c)
    2008-08-21  Richard Guenther  <rguenther@suse.de>
    
    	PR testsuite/37182
    	* gcc.dg/pr17506.c: Remove duplicate testcase.
    	* gcc.dg/uninit-15.c: Adjust to allow for both correct
    	behaviors, one xfailed.
    
    From-SVN: r139374

--- gcc/testsuite/gcc.dg/uninit-15.c
+++ gcc/testsuite/gcc.dg/uninit-15.c
@@ -1,17 +1,26 @@
+/* PR tree-optimization/17506
+   We issue an uninitialized variable warning at a wrong location at
+   line 11, which is very confusing.  Make sure we print out a note to
+   make it less confusing.  (xfailed alternative)
+   But it is of course ok if we warn in bar about uninitialized use
+   of j.  (not xfailed alternative)  */
 /* { dg-do compile } */
-/* { dg-options "-O -Wuninitialized" } */
+/* { dg-options "-O1 -Wuninitialized" } */
 
-inline int foo (int i)
+inline int
+foo (int i)
 {
-    if (i) return 1; /* { dg-warning "is used uninitialized" } */
-    return 0;
+  if (i) /* { dg-warning "used uninitialized in this function" "" { xfail *-*-* } } */
+    return 1;
+  return 0;
 }
 
-void baz();
+void baz (void);
 
-void bar()
+void
+bar (void)
 {
-    int j;           /* { dg-message "was declared here" } */
-    for (; foo(j); ++j)
-        baz();
+  int j; /* { dg-message "note: 'j' was declared here" "" { xfail *-*-* } } */
+  for (; foo (j); ++j)  /* { dg-warning "'j' is used uninitialized" } */
+    baz ();
 }
