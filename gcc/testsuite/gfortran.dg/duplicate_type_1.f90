fee3292b1460655f81a06c160de992638749373a
Daniel Kraft <d@domob.eu>
2008-08-22 22:36:12 +0200

    re PR fortran/30239 (duplicate data type assignment not detected)
    2008-08-22  Daniel Kraft  <d@domob.eu>
    
    	PR fortran/30239
    	* symbol.c (gfc_add_type): Warn on -Wsurprising if a function-result
    	type is re-declared but neither -pedantic nor -std=f* is given and so
    	this is no error.
    	* invoke.texi (-Wsurprising): Document this new behaviour.
    
    From-SVN: r139499

---
+++ gcc/testsuite/gfortran.dg/duplicate_type_1.f90
@@ -0,0 +1,23 @@
+! { dg-do compile }
+! { dg-options "-std=f95" }
+
+! PR fortran/30239
+! Check for errors when a symbol gets declared a type twice, even if it
+! is the same.
+
+INTEGER FUNCTION foo ()
+  IMPLICIT NONE
+  INTEGER :: foo ! { dg-error "basic type of" }
+  INTEGER :: foo ! { dg-error "basic type of" }
+  foo = 42
+END FUNCTION foo
+
+INTEGER FUNCTION bar () RESULT (x)
+  IMPLICIT NONE
+  INTEGER :: x ! { dg-error "basic type of" }
+
+  INTEGER :: y
+  INTEGER :: y ! { dg-error "basic type of" }
+
+  x = 42
+END FUNCTION bar
