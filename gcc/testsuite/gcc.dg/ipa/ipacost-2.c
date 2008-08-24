d98c3a35f99592c6cb020cdeffa153f7f94ae0da
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-08-24 20:11:22 +0000

    * gcc.dg/ipa/ipacost-1.c: New testcase. 	* gcc.dg/ipa/ipacost-2.c: New testcase. 	* gcc.dg/ipa/ipa-7.c: Update template.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@139544 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/ipa/ipacost-2.c
@@ -0,0 +1,60 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 -fipa-cp -fdump-ipa-cp -fno-early-inlining -fdump-tree-optimized"  } */
+
+int array[100];
+
+int t(int);
+
+static int 
+i_can_be_propagated_fully (int *a)
+{
+  int i;
+  for (i=0;i<50;i++)
+  {
+    t(a[i]);
+    t(a[i+1]);
+    t(a[i+2]);
+    t(a[i+3]);
+  }
+}
+static int 
+i_can_be_propagated_fully2 (int *a)
+{
+  i_can_be_propagated_fully (a);
+  i_can_be_propagated_fully (a);
+  i_can_be_propagated_fully (a);
+}
+static int 
+i_can_not_be_propagated_fully (int *a)
+{
+  int i;
+  for (i=0;i<50;i++)
+  {
+    t(a[i]);
+    t(a[i+1]);
+    t(a[i+2]);
+    t(a[i+3]);
+  }
+}
+int 
+i_can_not_be_propagated_fully2 (int *a)
+{
+  i_can_not_be_propagated_fully (a);
+  i_can_not_be_propagated_fully (a);
+  i_can_not_be_propagated_fully (a);
+}
+main()
+{
+  i_can_be_propagated_fully2 (array);
+  i_can_not_be_propagated_fully2 (array);
+}
+
+/* { dg-final { scan-ipa-dump-times "versioned function i_can_be_propagated_fully2" 1 "cp"  } } */
+/* { dg-final { scan-ipa-dump-times "versioned function i_can_be_propagated_fully " 1 "cp"  } } */
+/* { dg-final { scan-ipa-dump-times "versioned function i_can_not_be_propagated_fully2" 1 "cp"  } } */
+/* { dg-final { scan-ipa-dump-times "versioned function i_can_not_be_propagated_fully " 1 "cp"  } } */
+/* { dg-final { scan-tree-dump-not "i_can_be_propagated" "optimized"  } } */
+/* { dg-final { scan-tree-dump-not "i_can_be_propagated" "optimized"  } } */
+/* { dg-final { scan-tree-dump "symbol: array" "optimized"  } } */
+/* { dg-final { cleanup-ipa-dump "cp" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
