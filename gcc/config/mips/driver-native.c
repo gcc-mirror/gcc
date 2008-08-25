d97e6aca01c5d9e6b41fe5e2e4c551e9777162ea
Adam Nemet <anemet@caviumnetworks.com>
2008-08-25 00:57:01 +0000

    config.gcc (mips64*-*-linux*): Handle mips64octeon*-*-linux*.
    	* config.gcc (mips64*-*-linux*): Handle mips64octeon*-*-linux*.
    	* config/mips/mips.h (enum processor_type): Add PROCESSOR_OCTEON.
    	(TARGET_OCTEON): New macro.
    	(TARGET_CPU_CPP_BUILTINS): Define __OCTEON__ for Octeon.
    	(MIPS_ISA_LEVEL_SPEC, MIPS_ARCH_FLOAT_SPEC): Handle -march=octeon.
    	(ISA_HAS_POP): New macro.
    	* config/mips/driver-native.c (host_detect_local_cpu): Handle
    	Octeon.
    	* config/mips/mips.c (mips_cpu_info_table, mips_rtx_cost_data):
    	Handle Octeon.
    	* config/mips/mips.md (cpu): Add octeon.
    	(type): Add pop attribute value.
    	(popcount<mode>2): New pattern.
    	* doc/invoke.texi (-march=@var{arch}): Add octeon.
    testsuite/
    	* gcc.target/mips/octeon-pop-1.c: New test.
    
    From-SVN: r139554

--- gcc/config/mips/driver-native.c
+++ gcc/config/mips/driver-native.c
@@ -67,6 +67,8 @@ host_detect_local_cpu (int argc, const char **argv)
 	  cpu = "sb1";
 	else if (strstr (buf, "R5000") != NULL)
 	  cpu = "r5000";
+	else if (strstr (buf, "Octeon") != NULL)
+	  cpu = "octeon";
 	break;
       }
 
---
+++ gcc/testsuite/gcc.target/mips/octeon-pop-1.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-mips-options "-O -march=octeon -mgp64" } */
+/* { dg-final { scan-assembler "\tpop\t" } } */
+/* { dg-final { scan-assembler "\tdpop\t" } } */
+
+NOMIPS16 int
+f (long long a)
+{
+  return __builtin_popcountll (a);
+}
+
+NOMIPS16 int
+g (int a)
+{
+  return __builtin_popcount (a);
+}
