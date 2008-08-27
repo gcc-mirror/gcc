d4033c81042ca6e288013027c4662734694aaa0c
Manuel López-Ibáñez <manu@gcc.gnu.org>
2008-08-27 16:06:00 +0000

    re PR c++/17880 (-Wsequence-point doesn't warn inside if, while, do conditions, for beg/cond/end expressions etc.)
    2008-08-27  Manuel Lopez-Ibanez  <manu@gcc.gnu.org>
    
    	PR c++/17880
    cp/
    	* semantics.c (maybe_convert_cond): Call verify_sequence_points.
    	(finish_return_stmt): Likewise.
    	(finish_switch_condition): Likewise.
    testsuite/
    	* g++.dg/warn/sequence-pt-1.C: New.
    	* g++.dg/warn/sequence-pt-pr17880.C: New.
    
    From-SVN: r139625

---
+++ gcc/testsuite/g++.dg/warn/sequence-pt-pr17880.C
@@ -0,0 +1,54 @@
+// PR 17880
+// { dg-do compile }
+// { dg-options "-Wsequence-point" }
+
+int
+foo (int x)
+{
+  unsigned int a;
+  int b;
+
+  b = (a += 5) > a;  // { dg-warning "undefined" "sequence point warning" }
+  b = (a += 5) + a == 10;  // { dg-warning "undefined" "sequence point warning" }
+  b = (a -= 5) > a;  // { dg-warning "undefined" "sequence point warning" }
+  b = (a -= 5) + a == 10;  // { dg-warning "undefined" "sequence point warning" }
+  b = a-- > a;  // { dg-warning "undefined" "sequence point warning" }
+  b = a-- + a == 10;  // { dg-warning "undefined" "sequence point warning" }
+  b = ++a > a;  // { dg-warning "undefined" "sequence point warning" }
+  b = ++a + a == 10;  // { dg-warning "undefined" "sequence point warning" }
+
+  if ((a += 5) > a) return -1;  // { dg-warning "undefined" "sequence point warning" }
+  if ((a += 5) + a == 10) return -1;  // { dg-warning "undefined" "sequence point warning" }
+  if ((a -= 5) > a) return -1;  // { dg-warning "undefined" "sequence point warning" }
+  if ((a -= 5) + a == 10) return -1;  // { dg-warning "undefined" "sequence point warning" }
+  if (a-- > a) return -1;  // { dg-warning "undefined" "sequence point warning" }
+  if (a-- + a == 10) return -1;  // { dg-warning "undefined" "sequence point warning" }
+  if (++a > a) return -1;  // { dg-warning "undefined" "sequence point warning" }
+  if (++a + a == 10) return -1;  // { dg-warning "undefined" "sequence point warning" }
+  do {} while ((a += 5) > a);  // { dg-warning "undefined" "sequence point warning" }
+  while ((a += 5) > a);  // { dg-warning "undefined" "sequence point warning" }
+  for ((a += 5) > a;;);  // { dg-warning "undefined" "sequence point warning" }
+  for (b = (a += 5) > a;;);  // { dg-warning "undefined" "sequence point warning" }
+  for (; (a += 5) > a;);  // { dg-warning "undefined" "sequence point warning" }
+  for (;; b = (a += 5) > a);  // { dg-warning "undefined" "sequence point warning" }
+  for (;; a++ + a++);  // { dg-warning "undefined" "sequence point warning" }
+  if (a) a++ - a--;    // { dg-warning "undefined" "sequence point warning" }
+  ((a +=5) > a) ? a : b; // { dg-warning "undefined" "sequence point warning" }
+  return (a++ - a--); // { dg-warning "undefined" "sequence point warning" }
+}
+
+void bar (int i)
+{
+  int a = i++ - i++; // { dg-warning "undefined" "sequence point warning" }
+} 
+
+void baz (int i)
+{
+  switch (i++ + i++)  // { dg-warning "undefined" "sequence point warning" }
+    {
+    case 1:
+      i++ - i++;  // { dg-warning "undefined" "sequence point warning" }
+    case 2:
+      break;
+    }
+}
