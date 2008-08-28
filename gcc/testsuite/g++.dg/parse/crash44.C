ffe79825902c65abf908d8376329f4a85269af60
Paolo Carlini <paolo.carlini@oracle.com>
2008-08-28 23:37:41 +0000

    re PR c++/37260 (infinite loop in init)
    /cp
    2008-08-28  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/37260
    	* decl.c (reshape_init_r): Check init for error_mark_node.
    
    /testsuite
    2008-08-28  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/37260
    	* g++.dg/parse/crash44.C: New.
    
    From-SVN: r139740

---
+++ gcc/testsuite/g++.dg/parse/crash44.C
@@ -0,0 +1,15 @@
+// PR c++/37260
+// { dg-options "" }
+
+struct pthread_once_t { };
+struct test {
+  pthread_once_t once;
+};
+
+int main(void) {
+  struct test foo = {
+    once: PTHREAD_ONCE_INITIALIZER // { dg-error "error: 'PTHREAD_ONCE_INITIALIZER' was not declared in this scope" }
+  };
+
+  return 0;
+}
