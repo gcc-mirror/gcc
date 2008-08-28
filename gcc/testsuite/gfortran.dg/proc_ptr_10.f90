0e989e565be6895130a946abfe4324103a7ae239
janus <janus@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-08-28 15:10:50 +0000

    2008-08-28  Janus Weil  <janus@gcc.gnu.org>
    	PR fortran/37253
    	* module.c (ab_attribute,attr_bits,mio_symbol_attribute): Take care of
    	saving attr.procedure and attr.proc_ptr to the module file.
    
    
    2008-08-28  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/37253
    	* gfortran.dg/proc_ptr_10.f90: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@139713 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/proc_ptr_10.f90
@@ -0,0 +1,30 @@
+! { dg-do run }
+!
+! PR fortran/37253
+!
+! Contributed by Dominique d'Humieres <dominiq@lps.ens.fr>
+
+module myMod
+
+  CONTAINS
+
+  real function proc3( arg1 )
+     integer :: arg1
+     proc3 = arg1+7
+  end function proc3
+
+  subroutine proc4( arg1 )
+     procedure(real), pointer :: arg1
+     if (arg1(0)/=7) call abort()
+  end subroutine proc4
+
+end module myMod
+
+program myProg
+  use myMod
+  PROCEDURE (real), POINTER :: p => NULL()
+  p => proc3
+  call proc4( p )
+end program myProg
+ 
+! { dg-final { cleanup-modules "myMod" } }
