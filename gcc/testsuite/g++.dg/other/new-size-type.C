5bdc1946f07880739cdbca20105cebcba5795de0
Dodji Seketeli <dodji@redhat.com>
2008-08-28 14:49:48 +0000

    re PR c++/36741 (Bogus "large integer implicitly truncated" passing size_t constant to new)
    2008-08-28  Dodji Seketeli  <dodji@redhat.com>
    
    	PR c++/36741
    	* tree.c (int_fits_type_p): Don't forget unsigned integers
    	  of type sizetype which higher end word equals -1.
    
    From-SVN: r139712

---
+++ gcc/testsuite/g++.dg/other/new-size-type.C
@@ -0,0 +1,10 @@
+// Contributed by Dodji Seketeli <dodji@redhat.com>
+// Origin: PR c++/36741
+
+#include <stddef.h>
+const char*
+foo()
+{
+    return new char[~static_cast<size_t>(0)];// { dg-bogus "large" }
+}
+
