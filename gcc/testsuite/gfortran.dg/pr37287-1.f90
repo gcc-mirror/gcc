302e9fd31d51226850543a66d9112917de4c37d4
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-08-31 11:23:04 +0000

    PR debug/37287 	* dwarf2out.c (gen_namespace_die): For DECL_EXTERNAL modules don't 	add source coords.
    	* gfortran.dg/pr37287-1.f90: New test.
    	* gfortran.dg/pr37287-2.F90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@139826 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr37287-1.f90
@@ -0,0 +1,16 @@
+! PR debug/37287
+! { dg-do link }
+! { dg-options "-g -DPR37287_1" }
+! { dg-additional-sources pr37287-2.F90 }
+module pr37287_1
+  use iso_c_binding, only : c_ptr, c_associated, c_null_ptr
+  implicit none
+contains
+  subroutine set_null(ptr)
+      type(c_ptr), intent(out) :: ptr
+      ptr = c_null_ptr
+  end subroutine set_null
+end module pr37287_1
+end
+! { dg-final { cleanup-modules "pr37287_1" } }
+! { dg-final { cleanup-modules "pr37287_2" } }
