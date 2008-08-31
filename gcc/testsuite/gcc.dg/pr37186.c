c01b75e75b0b9abd188c2c9feb1ae30ef6ed56a9
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-08-31 15:09:38 +0000

    2008-08-31  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c/37271
    	* gcc.dg/pr37186.c: Add dg-message command for line 5.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@139833 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr37186.c
+++ gcc/testsuite/gcc.dg/pr37186.c
@@ -2,7 +2,7 @@
 /* { dg-do compile } */
 /* { dg-options "-Wall -Werror -Wno-error=pointer-sign" } */
 
-int foo(signed char *);
+int foo(signed char *); /* { dg-message "note: expected 'signed char \\*' but argument is of type 'unsigned char \\*'" } */
 int bar(unsigned char *p)
 {
   return foo(p); /* { dg-warning "pointer targets in passing argument 1 of 'foo' differ in signedness" } */
