f1fb2997dc59523723f0cb73ea02cecf4e913021
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-09-01 13:39:42 +0000

    2008-09-01  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/37305
    	* tree-ssa-ccp.c (ccp_fold): Do not set TREE_OVERFLOW on
    	the result of constant conversions.
    	(fold_gimple_assign): Likewise.
    
    	* gcc.c-torture/compile/pr37305.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@139864 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr37305.c
@@ -0,0 +1,19 @@
+typedef int int32_t;
+typedef unsigned int uint32_t;
+static inline int
+safe_add_s_s (int si1, int si2)
+{
+  if ((si1 > 0) && (si2 > 0) && (si1 > (si2)) || (si1 < 0) && (si2 < 0)
+      && (si1 < ((-__INT_MAX__ - 1) - si2)))
+    return si1;
+}
+
+uint32_t g_8;
+uint32_t
+func_24 (int32_t p_25)
+{
+  uint32_t l_30 = -1L;
+  if ((safe_mod_u_u (1, 1)) | (safe_add_s_s (g_8, l_30)))
+    return 1;
+}
+
