9b2ae3dd549022349e5f0578c4f61fa42c068ad7
schwab <schwab@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-09-01 12:35:24 +0000

    * config/ia64/ia64.c (TARGET_ADDRESS_COST): Update.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@139861 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/ia64/ia64.c
+++ gcc/config/ia64/ia64.c
@@ -408,7 +408,7 @@ static const struct attribute_spec ia64_attribute_table[] =
 #undef TARGET_RTX_COSTS
 #define TARGET_RTX_COSTS ia64_rtx_costs
 #undef TARGET_ADDRESS_COST
-#define TARGET_ADDRESS_COST hook_int_rtx_0
+#define TARGET_ADDRESS_COST hook_int_rtx_bool_0
 
 #undef TARGET_UNSPEC_MAY_TRAP_P
 #define TARGET_UNSPEC_MAY_TRAP_P ia64_unspec_may_trap_p
