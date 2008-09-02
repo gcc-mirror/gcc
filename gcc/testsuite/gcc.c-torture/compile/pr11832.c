24ce68eed968ce82a4b392b7b096b075ac77b9c7
John David Anglin <dave.anglin@nrc-cnrc.gc.ca>
2008-09-02 23:43:21 +0000

    pr33009.c: xfail on hppa*-*-*.
    	* gcc.c-torture/compile/pr33009.c: xfail on hppa*-*-*.
    	* gcc.c-torture/compile/pr11832.c: Likewise.
    
    From-SVN: r139919

--- gcc/testsuite/gcc.c-torture/compile/pr11832.c
+++ gcc/testsuite/gcc.c-torture/compile/pr11832.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
-/* Currently ICEs for MIPS, CRIS and PowerPC; see PR33642.  */
-/* { dg-xfail-if "PR33642" { mips*-*-* powerpc*-*-linux* cris-*-* crisv32-*-* } { "*" } { "" } } */
+/* Currently ICEs for HPPA, MIPS, CRIS and PowerPC; see PR33642.  */
+/* { dg-xfail-if "PR33642" { hppa*-*-* mips*-*-* powerpc*-*-linux* cris-*-* crisv32-*-* } { "*" } { "" } } */
 /* Currently ICEs for (x86 && ilp32 && pic).  */
 /* { dg-xfail-if "PR33642/36240" { { i?86-*-* x86_64-*-* } && { ilp32 && { ! nonpic } } } { "*" } { "" } } */
 /* { dg-prune-output ".*internal compiler error.*" }
