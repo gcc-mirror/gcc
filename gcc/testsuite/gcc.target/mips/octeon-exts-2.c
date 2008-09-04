c84241327480cec52608beaba811faf1f09e0b03
Adam Nemet <anemet@caviumnetworks.com>
2008-09-04 21:24:31 +0000

    mips.h (ISA_HAS_EXTS): New macro.
    	* config/mips/mips.h (ISA_HAS_EXTS): New macro.
    	* config/mips/mips.md (*ashr_trunc<mode>): Name the pattern
    	combining an arithmetic right shift by more than 31 and a
    	trunction.  Don't match for out-of-range shift amounts.  Set
    	attribute mode to <MODE>.
    	(*lshr32_trunc<mode>): Name the pattern combining a logical right
    	shift by 32 and and a truncation.  Set attribute mode to <MODE>.
    	(*<optab>_trunc<mode>_exts): New pattern for truncated right
    	shifts by less than 32.
    	(extv): Change predicate on first operand to accept registers.
    	Change predicate of the other operands from immediate_operand to
    	const_int_operand.  Expand exts when source is a register.
    	(extzv): Change predicate of the constant operands from
    	immediate_operand to const_int_operand.
    	(extzv<mode>): Change predicate of the constant operands from
    	immediate_operand to const_int_operand and no constraint. Also
    	remove mode.
    	(*extzv_trunc<mode>_exts): New pattern.
    
    testsuite/
    	* gcc.target/mips/truncate-2.c: New test.
    	* gcc.target/mips/octeon-exts-1.c: New test.
    	* gcc.target/mips/octeon-exts-2.c: New test.
    	* gcc.target/mips/octeon-exts-3.c: New test.
    	* gcc.target/mips/octeon-exts-4.c: New test.
    
    From-SVN: r140009

---
+++ gcc/testsuite/gcc.target/mips/octeon-exts-2.c
@@ -0,0 +1,37 @@
+/* { dg-do compile } */
+/* { dg-mips-options "-O -march=octeon" } */
+/* { dg-final { scan-assembler-times "\texts\t" 4 } } */
+
+struct bar
+{
+  unsigned long long a:1;
+  long long b:14;
+  unsigned long long c:48;
+  long long d:1;
+};
+
+NOMIPS16 int
+f1 (struct bar *s, int a)
+{
+  return (int) s->b + a;
+}
+
+NOMIPS16 char
+f2 (struct bar *s)
+{
+  return s->d + 1;
+}
+
+NOMIPS16 int
+f3 ()
+{
+  struct bar s;
+  asm ("" : "=r"(s));
+  return (int) s.b + 1;
+}
+
+NOMIPS16 long long
+f4 (struct bar *s)
+{
+  return s->d;
+}
---
+++ gcc/testsuite/gcc.target/mips/octeon-exts-4.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-mips-options "-O -march=octeon -mgp64" } */
+/* { dg-final { scan-assembler-not "\tsll\t\[^\n\]*,0" } } */
+/* { dg-final { scan-assembler-times "\texts\t" 6 } } */
+
+#define TEST(ID, TYPE, SHIFT)				\
+  int NOMIPS16						\
+  f##ID (long long y)					\
+  {							\
+    return (TYPE) ((TYPE) (y >> SHIFT) + 1);		\
+  }							\
+  int NOMIPS16						\
+  g##ID (unsigned long long y)				\
+  {							\
+    return (TYPE) ((TYPE) (y >> SHIFT) + 1);		\
+  }
+
+TEST (1, int, 10)
+TEST (2, short, 5)
+TEST (3, char, 31)
