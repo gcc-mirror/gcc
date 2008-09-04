640cbe76c769ea9737e671d32e51b37693848ebf
Jonathan Wakely <jwakely.gcc@gmail.com>
2008-09-04 22:33:10 +0000

    re PR libstdc++/36962 ([C++0x] Add constructors / assignment operators from unique_ptr to shared_ptr)
    	PR libstdc++/36962
    	* include/Makefile.am: Update headers.
    	* include/Makefile.in: Regenerate.
    	* include/std/memory: Update headers.
    	* include/tr1/memory: Likewise.
    	* include/tr1_impl/boost_shared_ptr.h: Remove, splitting into
    	separate implementations for std and tr1.
    	* include/bits/boost_sp_shared_count.h: Remove.
    	* include/tr1/boost_sp_shared_count.h: Remove.
    	* include/bits/shared_ptr.h: New. Combines boost_sp_shared_count.h
    	and boost_shared_ptr.h into one file.
    	(__shared_count,__shared_ptr,shared_ptr,swap): Add unique_ptr
    	and rvalue-reference support as per current WP.
    	* include/tr1/shared_ptr.h: New. Combines boost_sp_shared_count.h
    	and boost_shared_ptr.h into one file.
    	* testsuite/20_util/shared_ptr/cons/unique_ptr.cc: New.
    	* testsuite/20_util/shared_ptr/cons/unique_ptr_deleter.cc: New.
    	* testsuite/20_util/shared_ptr/cons/unique_ptr_deleter_ref.cc: New.
    	* testsuite/20_util/shared_ptr/cons/unique_ptr_neg.cc: New.
    	* testsuite/20_util/shared_ptr/assign/auto_ptr_rvalue.cc: New.
    	* testsuite/20_util/shared_ptr/assign/unique_ptr_lvalue_neg.cc: New.
    	* testsuite/20_util/shared_ptr/assign/unique_ptr_rvalue.cc: New.
    	* testsuite/20_util/shared_ptr/modifiers/swap_rvalue.cc: New.
    	* testsuite/20_util/shared_ptr/assign/auto_ptr_rvalue_neg.cc: Remove.
    
    From-SVN: r140012

---
+++ libstdc++-v3/testsuite/20_util/shared_ptr/cons/unique_ptr_deleter.cc
@@ -0,0 +1,60 @@
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2008 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 20.7.12.2 Template class shared_ptr [util.smartptr.shared]
+
+#include <memory>
+#include <testsuite_hooks.h>
+
+struct A { };
+
+struct D {
+    void operator()(A* p) const { delete p;  ++count; }
+    static int count;
+};
+
+int D::count = 0;
+
+// 20.7.12.2.1 shared_ptr constructors [util.smartptr.shared.const]
+
+// Construction from unique_ptr
+int
+test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  std::unique_ptr<A, D> up(new A, D());
+  {
+      std::shared_ptr<A> sp(std::move(up));
+      VERIFY( up.get() == 0 );
+      VERIFY( sp.get() != 0 );
+      VERIFY( sp.use_count() == 1 );
+  }
+  VERIFY( D::count == 1 );
+
+  return 0;
+}
+
+int
+main()
+{
+  test01();
+  return 0;
+}
