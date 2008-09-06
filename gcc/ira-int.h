2236fd042bc8ec18f4add6ab6a916a0135a0fcda
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-09-06 08:08:49 +0000

    gcc/ 	* ira-int.h (ira_zero_hard_reg_set, ira_one_hard_reg_set): Delete. 	* ira-color.c (setup_allocno_left_conflicts_num): Use 	hard_reg_set_equal_p instead of a comparison with 	ira_zero_hard_reg_set. 	* ira.c (setup_reg_subclasses): Likewise. 	(setup_cover_and_important_classes): Likewise. 	(setup_class_translate): Likewise. 	(setup_reg_class_intersect_union): Likewise. 	(ira_zero_hard_reg_set, ira_one_hard_reg_set): Delete. 	(ira_init_once): Don't initialize them.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@140054 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ira-int.h
+++ gcc/ira-int.h
@@ -702,10 +702,6 @@ ira_allocno_set_iter_next (ira_allocno_set_iterator *i)
 
 /* ira.c: */
 
-/* Hard regsets whose all bits are correspondingly zero or one.  */
-extern HARD_REG_SET ira_zero_hard_reg_set;
-extern HARD_REG_SET ira_one_hard_reg_set;
-
 /* Map: hard regs X modes -> set of hard registers for storing value
    of given mode starting with given hard register.  */
 extern HARD_REG_SET ira_reg_mode_hard_regset
