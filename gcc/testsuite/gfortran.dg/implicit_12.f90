10c17e8fd0cf9fe6471f4841472337364f54b1e1
Tobias Burnus <burnus@gcc.gnu.org>
2008-09-08 09:19:46 +0200

    re PR fortran/37400 (implicit character(len=*,kind=kind('A')) (Q) ... no longer gives the right answer.)
    2008-09-07  Tobias Burnus  <burnus@net.b.de>
    
            PR fortran/37400
            * symbol.c (gfc_set_default_type): Copy char len.
    
    2008-09-07  Tobias Burnus  <burnus@net.b.de>
    
            PR fortran/37400
            * gfortran.dg/implicit_12.f90: New test.
    
    From-SVN: r140100

---
+++ gcc/testsuite/gfortran.dg/implicit_12.f90
@@ -0,0 +1,23 @@
+! { dg-do run}
+!
+! PR fortran/37400
+!
+module mod 
+   implicit character(len=*,kind=kind('A')) (Q) 
+   parameter(Q1 = '12345678')     ! len=8
+   parameter(Q2 = 'abcdefghijkl') ! len=12
+   contains 
+      subroutine sub(Q3) 
+         if(len('#'//Q3//'#') /= 15) call abort()
+         if('#'//Q3//'#' /= '#ABCDEFGHIJKLM#') call abort()
+      end subroutine sub 
+end module mod 
+program startest 
+   use mod 
+   implicit none
+   if(len('#'//Q1//'#') /= 10) call abort()
+   if(len('#'//Q2//'#') /= 14) call abort()
+   if('#'//Q1//'#' /='#12345678#') call abort()
+   if('#'//Q2//'#' /='#abcdefghijkl#') call abort()
+   call sub('ABCDEFGHIJKLM') ! len=13
+end program startest
