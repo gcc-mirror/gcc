48347b9e6f15e9e16223a7e4ceea2a4d30596009
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-09-09 19:18:55 +0000

    PR target/37438 	* config/i386/i386.md (zero_extendqihi2_movzbl): Enable when optimizing 	for size, not speed.
    	* gcc.dg/pr37438.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@140167 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr37438.c
@@ -0,0 +1,21 @@
+/* PR target/37438 */
+/* { dg-do compile } */
+/* { dg-options "-Os" } */
+/* { dg-options "-Os -march=i486" { target { { i686-*-* x86_64-*-* } && ilp32 } } } */
+
+extern int bar (unsigned long long int);
+extern int baz (const char *, unsigned int, unsigned short);
+
+int
+foo (unsigned long long int x)
+{
+  return (x & 0xff) | ((unsigned int) (x >> 12) & ~0xff);
+}
+
+int
+test (const char *v, unsigned int w, unsigned long long int x)
+{
+  unsigned short k;
+  k = ((bar (x) & 0xff) << 8) | (foo (x) & 0xff);
+  return baz (v, w, k);
+}
