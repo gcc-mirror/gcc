018fe68f30a380821f44af06c36d24a0df8fc4b4
Aldy Hernandez <aldyh@redhat.com>
2008-09-09 14:48:15 +0000

    pr12329.c: Place error message on function name line.
            PR/37442
            * gcc.target/i386/pr12329.c: Place error message on function name
            line.
            * gcc.target/i386/sse-5.c: Same.
    
    From-SVN: r140155

--- gcc/testsuite/gcc.target/i386/pr12329.c
+++ gcc/testsuite/gcc.target/i386/pr12329.c
@@ -6,8 +6,8 @@ extern void abort (void);
 
 int test_nested (int i)
 {
-  int __attribute__ ((__noinline__, __regparm__(3))) foo(int j, int k, int l)
-  { /* { dg-error "nested functions are limited to 2 register parameters" } */
+  int __attribute__ ((__noinline__, __regparm__(3))) foo(int j, int k, int l) /* { dg-error "nested functions are limited to 2 register parameters" } */
+  {
     return i + j + k + l;
   }
 
