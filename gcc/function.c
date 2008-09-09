881cb5783559f78af90afca59ce84628f1b9b439
Jakub Jelinek <jakub@redhat.com>
2008-09-09 22:24:58 +0200

    re PR rtl-optimization/37408 (Invalid insn scheduling)
    	PR rtl-optimization/37408
    	* function.c (assign_parm_find_stack_rtl): Set correct MEM_SIZE
    	if parm is promoted.
    
    From-SVN: r140172

--- gcc/function.c
+++ gcc/function.c
@@ -2336,6 +2336,11 @@ assign_parm_find_stack_rtl (tree parm, struct assign_parm_data_one *data)
   stack_parm = gen_rtx_MEM (data->promoted_mode, stack_parm);
 
   set_mem_attributes (stack_parm, parm, 1);
+  /* set_mem_attributes could set MEM_SIZE to the passed mode's size,
+     while promoted mode's size is needed.  */
+  if (data->promoted_mode != BLKmode
+      && data->promoted_mode != DECL_MODE (parm))
+    set_mem_size (stack_parm, GEN_INT (GET_MODE_SIZE (data->promoted_mode)));
 
   boundary = data->locate.boundary;
   align = BITS_PER_UNIT;
