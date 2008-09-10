8199c8a865840ef3c426d6c37ade2199a0a86812
H.J. Lu <hjl@gcc.gnu.org>
2008-09-10 07:14:28 -0700

    re PR target/37434 (ICE in extract_insn, at recog.c:2027)
    gcc/
    
    2008-09-10  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/37434:
    	* config/i386/i386.c (ix86_expand_vector_init_interleave): Force
    	the even element into register.
    	(ix86_expand_vector_init_general): Don't use
    	ix86_expand_vector_init_interleave on V16QImode and V8HImode
    	if we can't move from GPR to SSE register directly.
    
    gcc/testsuite/
    
    2008-09-10  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/37434:
    	* gcc.target/i386/pr37434-1.c: New.
    	* gcc.target/i386/pr37434-2.c: Likewise.
    	* gcc.target/i386/pr37434-3.c: Likewise.
    	* gcc.target/i386/pr37434-4.c: Likewise.
    	* gcc.target/i386/sse2-set-v8hi-1a.c: Likewise.
    	* gcc.target/i386/sse2-set-v8hi-2a.c: Likewise.
    	* gcc.target/i386/sse4_1-set-v16qi-1a.c: Likewise.
    	* gcc.target/i386/sse4_1-set-v16qi-2a.c: Likewise.
    	* gcc.target/i386/sse4_1-set-v16qi-3a.c: Likewise.
    
    From-SVN: r140231

---
+++ gcc/testsuite/gcc.target/i386/pr37434-1.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -msse2" } */
+
+typedef short __v8hi __attribute__ ((__vector_size__ (16)));
+typedef long long __m128i __attribute__ ((__vector_size__ (16)));
+__m128i Set_AC4R_SETUP_I( const short *val ) {
+  short D2073 = *val;
+  short D2076 = *(val + 2);
+  short D2079 = *(val + 4);
+  __v8hi D2094 = {D2073, D2076, D2079, 0, D2073, D2076, D2079, 0};
+  return (__m128i)D2094;
+}
