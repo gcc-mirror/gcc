298ed4ca2295023e07a67569fa7db1c0544458ab
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-09-10 21:07:29 +0000

    2008-09-10  H.J. Lu  <hongjiu.lu@intel.com>
    	* expr.c (find_array_element): Reformat.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@140248 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/expr.c
+++ gcc/fortran/expr.c
@@ -1036,13 +1036,12 @@ find_array_element (gfc_constructor *cons, gfc_array_ref *ar,
 	}
         /* Check the bounds.  */
       if ((ar->as->upper[i]
-	     && ar->as->upper[i]->expr_type == EXPR_CONSTANT
-	     && mpz_cmp (e->value.integer,
-			 ar->as->upper[i]->value.integer) > 0)
-		||
-	  (ar->as->lower[i]->expr_type == EXPR_CONSTANT
-	     && mpz_cmp (e->value.integer,
-			 ar->as->lower[i]->value.integer) < 0))
+	   && ar->as->upper[i]->expr_type == EXPR_CONSTANT
+	   && mpz_cmp (e->value.integer,
+		       ar->as->upper[i]->value.integer) > 0)
+	  || (ar->as->lower[i]->expr_type == EXPR_CONSTANT
+	      && mpz_cmp (e->value.integer,
+			  ar->as->lower[i]->value.integer) < 0))
 	{
 	  gfc_error ("Index in dimension %d is out of bounds "
 		     "at %L", i + 1, &ar->c_where[i]);
@@ -1061,19 +1060,18 @@ find_array_element (gfc_constructor *cons, gfc_array_ref *ar,
       mpz_mul (span, span, tmp);
     }
 
-    for (nelemen = mpz_get_ui (offset); nelemen > 0; nelemen--)
-      {
-        if (cons)
-	  {
-	    if (cons->iterator)
-	      {
-	        cons = NULL;
-	      
-	        goto depart;
-	      }
-	    cons = cons->next;
-	  }
-      }
+  for (nelemen = mpz_get_ui (offset); nelemen > 0; nelemen--)
+    {
+      if (cons)
+	{
+	  if (cons->iterator)
+	    {
+	      cons = NULL;
+	      goto depart;
+	    }
+	  cons = cons->next;
+	}
+    }
 
 depart:
   mpz_clear (delta);
