a64057fc40ea5a2009811f018169839c8cbff43d
Andreas Schwab <schwab@suse.de>
2008-09-11 09:11:23 +0000

    xgot-1.c: Add -mcpu=5206 to select a ColdFire target.
    * gcc.target/m68k/xgot-1.c: Add -mcpu=5206 to select a ColdFire
    target.
    
    From-SVN: r140272

--- gcc/testsuite/gcc.target/m68k/xgot-1.c
+++ gcc/testsuite/gcc.target/m68k/xgot-1.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-fpic -mxgot" } */
+/* { dg-options "-fpic -mxgot -mcpu=5206" } */
 /* { dg-final { scan-assembler "foo@GOT,\%\[ad\]\[0-7\]" } } */
 
 extern int foo;
