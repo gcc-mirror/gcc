1a8c909ebd0bbd58ed5308bc66dc923ec5cfaafd
Eric Botcazou <ebotcazou@adacore.com>
2008-09-17 21:48:08 +0000

    varasm.c (initializer_constant_valid_p): Forbid view-conversions from aggregate to non-aggregate type if...
    	* varasm.c (initializer_constant_valid_p): Forbid view-conversions
    	from aggregate to non-aggregate type if the bit pattern is not fully
    	preserved afterwards.
    
    From-SVN: r140432

---
+++ gcc/testsuite/gnat.dg/specs/static_initializer3.ads
@@ -0,0 +1,27 @@
+with Unchecked_Conversion;
+
+package Static_Initializer3 is
+
+  type Byte is range 0 .. 16#FF#;
+  for Byte'Size use 8;
+
+  type Word is range 0 .. 16#FFFF# ;
+  for Word'Size use 16;
+
+  type R is record
+    b1 : Boolean;
+    b2 : Boolean;
+  end record;
+  for R use record
+    b1 at 0 range 0..3;
+    b2 at 0 range 4..7;
+  end record;
+  for R'Size use 8;
+
+  function Conv is new Unchecked_Conversion (R, Byte);
+
+  C1 : constant Byte := Conv ((true, false));
+
+  C2 : constant Word := Word(C1);
+
+end Static_Initializer3;
