7cc3311d5efc983c2a9b399b7fe80a4ddeb50f8a
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-09-18 20:21:03 +0000

    2008-09-18  Paul Thomas  <pault@gcc.gnu.org>
           PR fortran/35945
           * resolve.c (resolve_fl_variable_derived):  Remove derived type
           comparison for use associated derived types.  Host association
           of a derived type will not arise if there is a local derived type
           whose use name is the same.
    
           PR fortran/36700
           * match.c (gfc_match_call):  Use the existing symbol even if
           it is a function.
    
    2008-09-18  Paul Thomas  <pault@gcc.gnu.org>
    
           PR fortran/35945
           * gfortran.dg/host_assoc_types_2.f90: New test.
    
           PR fortran/36700
           * gfortran.dg/host_assoc_call_2.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@140474 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/host_assoc_call_2.f90
@@ -0,0 +1,18 @@
+! { dg-do compile }
+! Tests the fix for PR36700, in which the call to the function would
+! cause an ICE.
+!
+! Contributed by <terry@chem.gu.se>
+!
+module Diatoms
+  implicit none
+contains
+  function InitialDiatomicX () result(v4)    ! { dg-error "has a type" }
+    real(kind = 8), dimension(4) :: v4
+    v4 = 1
+  end function InitialDiatomicX
+  subroutine FindDiatomicPeriod
+    call InitialDiatomicX ()    ! { dg-error "which is not consistent with the CALL" }
+  end subroutine FindDiatomicPeriod
+end module Diatoms
+! { dg-final { cleanup-modules "Diatoms" } }
