bacfcc02fd330714fd67275ffb681a6071291e2d
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-09-23 18:57:18 +0000

    PR c++/37533 	* semantics.c (finish_omp_for): If processing_template_decl, just build 	MODIFY_EXPR for init instead of calling cp_build_modify_expr.
    	* g++.dg/gomp/pr37533.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@140613 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -4291,8 +4291,12 @@ finish_omp_for (location_t locus, tree declv, tree initv, tree condv,
 	}
 
       if (!processing_template_decl)
-	init = fold_build_cleanup_point_expr (TREE_TYPE (init), init);
-      init = cp_build_modify_expr (decl, NOP_EXPR, init, tf_warning_or_error);
+	{
+	  init = fold_build_cleanup_point_expr (TREE_TYPE (init), init);
+	  init = cp_build_modify_expr (decl, NOP_EXPR, init, tf_warning_or_error);
+	}
+      else
+	init = build2 (MODIFY_EXPR, void_type_node, decl, init);
       if (cond && TREE_SIDE_EFFECTS (cond) && COMPARISON_CLASS_P (cond))
 	{
 	  int n = TREE_SIDE_EFFECTS (TREE_OPERAND (cond, 1)) != 0;
---
+++ gcc/testsuite/g++.dg/gomp/pr37533.C
@@ -0,0 +1,50 @@
+// PR c++/37533
+// { dg-do compile }
+// { dg-options "-fopenmp" }
+
+template<int>
+void
+f1 ()
+{
+#pragma omp parallel for
+  for (int i = ""; i < 4; ++i)	// { dg-error "invalid conversion from" }
+    ;
+}
+
+template<int>
+void
+f2 ()
+{
+  int i;
+#pragma omp parallel for
+  for (i = ""; i < 4; ++i)	// { dg-error "invalid conversion from" }
+    ;
+}
+
+template<typename T>
+void
+f3 ()
+{
+#pragma omp parallel for
+  for (T i = ""; i < 4; ++i)	// { dg-error "invalid conversion from" }
+    ;
+}
+
+template<typename T>
+void
+f4 ()
+{
+  T i;
+#pragma omp parallel for
+  for (i = ""; i < 4; ++i)	// { dg-error "invalid conversion from" }
+    ;
+}
+
+void
+bar ()
+{
+  f1<0> ();			// { dg-message "instantiated from here" }
+  f2<1> ();			// { dg-message "instantiated from here" }
+  f3<int> ();			// { dg-message "instantiated from here" }
+  f4<int> ();			// { dg-message "instantiated from here" }
+}
