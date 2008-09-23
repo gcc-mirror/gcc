c3d5e955b3b3175deef8c213c12b50d859280b7c
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-09-23 18:14:24 +0000

    2008-09-23  Paolo Carlini  <paolo.carlini@oracle.com>
    	Implement DR 396 [Ready].
    	* include/std/bitset (bitset<>::bitset(const std::basic_string<>&,
    	size_t, size_t, _CharT, _CharT), bitset<>::bitset(const char*,
    	char, char), bitset<>::to_string(_CharT, _CharT),
    	bitset<>::_M_copy_from_string(const std::basic_string<>&, size_t,
    	size_t, _CharT, _CharT), bitset<>::_M_copy_to_string
    	(std::basic_string<>&, _CharT, _CharT), bitset<>::_M_copy_from_ptr(
    	const _CharT*, size_t, size_t, size_t, _CharT, _CharT)): Add.
    	(operator>>(std::basic_istream<>&, bitset<_Nb>&), operator<<(
    	std::basic_ostream<>&, const bitset<_Nb>&)): Adjust.
    	* include/debug/bitset (bitset<>::bitset(const std::basic_string<>&,
    	size_t, size_t, _CharT, _CharT), bitset<>::bitset(const char*,
    	char, char), bitset<>::to_string(_CharT, _CharT)): Add.
    	* doc/xml/manual/intro.xml: Add an entry for DR 396.
    	* testsuite/23_containers/bitset/cons/dr396.cc: Add.
    	* testsuite/23_containers/bitset/to_string/dr396.cc: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@140607 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/23_containers/bitset/to_string/dr396.cc
@@ -0,0 +1,65 @@
+// 2008-09-23  Paolo Carlini  <pcarlini@suse.de>
+
+// Copyright (C) 2008 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// 23.3.5.2 bitset members
+
+#include <bitset>
+#include <testsuite_hooks.h>
+
+// DR 396. what are characters zero and one.
+void test01()
+{
+  using namespace std;
+  bool test __attribute__((unused)) = true;
+
+  bitset<5> b5;
+  string s0 = b5.to_string<char, char_traits<char>, allocator<char> >('a', 'b');
+  VERIFY( s0 == "aaaaa" );
+
+  string s1 = b5.to_string<char, char_traits<char>, allocator<char> >('b');
+  VERIFY( s1 == "bbbbb" );
+
+  b5.set(0);
+  string s2 = b5.to_string<char, char_traits<char> >('c', 'd');
+  VERIFY( s2 == "ccccd" );
+
+  string s3 = b5.to_string<char, char_traits<char> >('d');
+  VERIFY( s3 == "dddd1" );
+
+  b5.set(2);
+  string s4 = b5.to_string<char>('e', 'f');
+  VERIFY( s4 == "eefef" );
+
+  string s5 = b5.to_string<char>('f');
+  VERIFY( s5 == "ff1f1" );
+
+  b5.set(4);
+  string s6 = b5.to_string('g', 'h');
+  VERIFY( s6 == "hghgh" );
+
+  string s7 = b5.to_string('h');
+  VERIFY( s7 == "1h1h1" );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
