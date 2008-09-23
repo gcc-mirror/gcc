301b177e7a1e651264e8928544a8fcd4bc7fe894
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-09-23 08:52:53 +0000

    * gcc.dg/cpp/cmdlne-dU-16.c: Specify -std=c89 on Solaris. 	* gcc.dg/cpp/cmdlne-dU-17.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@140581 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cpp/cmdlne-dU-16.c
+++ gcc/testsuite/gcc.dg/cpp/cmdlne-dU-16.c
@@ -1,5 +1,6 @@
 /* { dg-do preprocess } */
 /* { dg-options "-P -dU" } */
+/* { dg-options "-P -dU -std=c89" { target *-*-solaris2.* } } */
 /* { dg-final { scan-file cmdlne-dU-16.i "^\n*#define __STDC__ 1\n*$" } } */
 #ifdef __STDC__
 #endif
--- gcc/testsuite/gcc.dg/cpp/cmdlne-dU-17.c
+++ gcc/testsuite/gcc.dg/cpp/cmdlne-dU-17.c
@@ -1,4 +1,5 @@
 /* { dg-do preprocess } */
 /* { dg-options "-P -dU" } */
+/* { dg-options "-P -dU -std=c89" { target *-*-solaris2.* } } */
 /* { dg-final { scan-file cmdlne-dU-17.i "^\n*1\n+#define __STDC__ 1\n*$" } } */
 __STDC__
