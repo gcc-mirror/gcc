061d8b33a13cdbf56976ad0d39a2242da319adef
Richard Henderson <rth@redhat.com>
2008-09-24 15:44:28 -0700

    dwarf2.h (DW_OP_GNU_encoded_addr): New.
            * dwarf2.h (DW_OP_GNU_encoded_addr): New.
            * unwind-dw2.c (execute_stack_op): Handle it.
    
    From-SVN: r140649

--- gcc/dwarf2.h
+++ gcc/dwarf2.h
@@ -543,6 +543,7 @@ enum dwarf_location_atom
     DW_OP_GNU_push_tls_address = 0xe0,
     /* The following is for marking variables that are uninitialized.  */
     DW_OP_GNU_uninit     = 0xf0,
+    DW_OP_GNU_encoded_addr = 0xf1,
     /* HP extensions.  */
     DW_OP_HP_unknown     = 0xe0, /* Ouch, the same as GNU_push_tls_address.  */
     DW_OP_HP_is_value    = 0xe1,
