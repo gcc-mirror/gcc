fefc36c103967113c86b5da66653a289fe5f7293
Simon Martin <simartin@users.sourceforge.net>
2008-09-30 19:56:13 +0000

    re PR c++/37555 (ICE with invalid typedef)
    gcc/cp/
    
    2008-09-30  Simon Martin  <simartin@users.sourceforge.net>
    
    	    PR c++/37555
    	    * decl.c (grokdeclarator): Set the type for typedefs to a
    	    nested-name-specifier to error_mark_node.
    
    gcc/testsuite/
    
    2008-09-30  Simon Martin  <simartin@users.sourceforge.net>
    
    	    PR c++/37555
    	    * g++.dg/parse/error32.C: New test.
    
    From-SVN: r140796

---
+++ gcc/testsuite/g++.dg/parse/error32.C
@@ -0,0 +1,12 @@
+/* PR c++/37555 */
+/* { dg-do "compile" } */
+
+struct A {};
+
+typedef void (A::T)(); /* { dg-error "typedef name may not be a nested-name-specifier" } */
+
+void foo()
+{
+  T t;
+  t; /* { dg-error "was not declared" } */
+}
