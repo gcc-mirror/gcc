25010f2aeec81fd773ddee5f7886c86964bfc1a6
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-10-11 17:57:20 +0000

    * config/i386/cpuid.h (__cpuid_count): New defines. 	* config/i386/driver-i386.c (struct cache_desc): New structure. 	(describe_cache): Use struct cache_desc to pass cache descriptions. 	(detect_l2_cache): Ditto. Rename from decode_l2_cache. 	(detect_caches_amd): Use struct cache_desc to describe caches. 	(decode_caches_intel): Use struct cache_desc to pass cache 	descriptions.  Update descriptions to match latest (rev -032, 	December 2007) CPUID documentation.  Do not check valid bit here. 	Check for Xeon MP value 0x49 problems. 	(detect_caches_cpuid2): New function, split from detect_caches_intel. 	Check valid bit before calling decode_caches_intel.  Detect number 	of times to repeat CPUID instruction. 	(detect_caches_cpuid4): New function. 	(detect_caches_intel): Depending on max_level, call 	detect_caches_cpuid2 or detect_caches_cpuid4.  Call detect_l2_cache 	only when other methods fail to provide valid L2 cache description.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@141064 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/cpuid.h
+++ gcc/config/i386/cpuid.h
@@ -74,6 +74,13 @@
 	   "xchg{l}\t{%%}ebx, %1\n\t"			\
 	   : "=a" (a), "=r" (b), "=c" (c), "=d" (d)	\
 	   : "0" (level))
+
+#define __cpuid_count(level, count, a, b, c, d)		\
+  __asm__ ("xchg{l}\t{%%}ebx, %1\n\t"			\
+	   "cpuid\n\t"					\
+	   "xchg{l}\t{%%}ebx, %1\n\t"			\
+	   : "=a" (a), "=r" (b), "=c" (c), "=d" (d)	\
+	   : "0" (level), "2" (count))
 #else
 /* Host GCCs older than 3.0 weren't supporting Intel asm syntax
    nor alternatives in i386 code.  */
@@ -83,12 +90,24 @@
 	   "xchgl\t%%ebx, %1\n\t"			\
 	   : "=a" (a), "=r" (b), "=c" (c), "=d" (d)	\
 	   : "0" (level))
+
+#define __cpuid_count(level, count, a, b, c, d)		\
+  __asm__ ("xchgl\t%%ebx, %1\n\t"			\
+	   "cpuid\n\t"					\
+	   "xchgl\t%%ebx, %1\n\t"			\
+	   : "=a" (a), "=r" (b), "=c" (c), "=d" (d)	\
+	   : "0" (level), "2" (count))
 #endif
 #else
 #define __cpuid(level, a, b, c, d)			\
   __asm__ ("cpuid\n\t"					\
 	   : "=a" (a), "=b" (b), "=c" (c), "=d" (d)	\
 	   : "0" (level))
+
+#define __cpuid_count(level, count, a, b, c, d)		\
+  __asm__ ("cpuid\n\t"					\
+	   : "=a" (a), "=b" (b), "=c" (c), "=d" (d)	\
+	   : "0" (level), "2" (count))
 #endif
 
 /* Return highest supported input value for cpuid instruction.  ext can
