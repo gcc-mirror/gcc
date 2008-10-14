9c88061824631d4fc318c57b72cfd1913007d50c
Douglas Gregor <doug.gregor@gmail.com>
2008-10-14 15:03:51 +0000

    re PR c++/37553 (ICE in build_c_cast)
    2008-10-14  Douglas Gregor  <doug.gregor@gmail.com>
    
           PR c++/37553
           * tree.c (build_type_attribute_qual_variant): Hash on the
           unqualified type, and don't overwrite an existing
           (type_hash_eq): Make the TYPE_NAME of the types significant, to
           allow distinguishing between wchar_t and its underlying type. This
           also means that we'll retain a little more typedef information.
    
    2008-10-14  Douglas Gregor  <doug.gregor@gmail.com>
    
           PR c++/37553
           * g++.dg/ext/alias-canon2.C: New.
    
    From-SVN: r141111

---
+++ gcc/testsuite/g++.dg/ext/alias-canon2.C
@@ -0,0 +1,36 @@
+// { dg-do compile }
+// PR c++/37553
+typedef unsigned int ui32;
+__extension__ typedef unsigned long long int ui64;
+
+typedef ui32 __attribute__ ((__may_alias__)) ui32a;
+typedef ui64 __attribute__ ((__may_alias__)) ui64a;
+
+union u_u32
+{
+  ui32a v;
+} __attribute__ ((__may_alias__));
+
+union u_u64
+{
+  ui64a v;
+  struct
+  {
+    union u_u32 lo32, hi32;
+  } u;
+} __attribute__ ((__may_alias__));
+
+void
+out_long (ui64 longVal)
+{
+  if ((*(union u_u64 *) &longVal).u.lo32.v < 0x10000000ul)
+    {
+      if ((ui32) ((*(union u_u64 *) &longVal).u.lo32.v) < 0x4000u)
+        {
+          /* do something useful */
+        }
+    }
+}
+
+void f(ui32 *) { }
+void f(ui32a *) { }
