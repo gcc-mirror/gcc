21c74256c36074cd3bab4895079977ab11672789
Thomas Koenig <tkoenig@gcc.gnu.org>
2008-10-15 16:27:58 +0000

    re PR libfortran/34670 (bounds checking for array intrinsics)
    2008-10-15  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR libfortran/34670
    	* intrinsics/reshape_generic.c:  Add bounds checking.
    	* m4/reshape.m4:  Likewise.
            * generated/reshape_c10.c: Regenerated.
            * generated/reshape_c16.c: Regenerated.
            * generated/reshape_c4.c: Regenerated.
            * generated/reshape_c8.c: Regenerated.
            * generated/reshape_i16.c: Regenerated.
            * generated/reshape_i4.c: Regenerated.
            * generated/reshape_i8.c: Regenerated.
            * generated/reshape_r10.c: Regenerated.
            * generated/reshape_r16.c: Regenerated.
            * generated/reshape_r4.c: Regenerated.
            * generated/reshape_r8.c: Regenerated.
            * generated/spread_r4.c: Regenerated.
    
    2008-10-15  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR libfortran/34670
    	* gfortran.dg/reshape_3.f90:  New test.
    	* gfortran.dg/reshape_4.f90:  New test.
    	* gfortran.dg/reshape_order_1.f90:  Use correct shape.
    	* gfortran.dg/reshape_order_2.f90:  Likewise.
    	* gfortran.dg/reshape_order_3.f90:  Likewise.
    	* gfortran.dg/reshape_order_4.f90:  Likewise.
    
    From-SVN: r141144

--- gcc/testsuite/gfortran.dg/reshape_order_1.f90
+++ gcc/testsuite/gfortran.dg/reshape_order_1.f90
@@ -4,7 +4,7 @@
 program main
   implicit none
   integer(kind=1), dimension(6) :: source1 = (/ 1, 2, 3, 4, 5, 6 /)
-  integer, dimension(2) :: shape1 = (/ 2, 5/)
+  integer, dimension(2) :: shape1 = (/ 2, 3/)
   integer(kind=1), dimension(2) :: pad1 = (/ 0, 0/)
   character(len=200) :: l1, l2
   integer :: i1, i2
