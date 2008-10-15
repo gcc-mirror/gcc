7dd3db6ea365e42e565bb546b2e2729ef4828638
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-10-15 16:17:45 +0000

    2008-10-15  Sebastian Pop  <sebastian.pop@amd.com>     	    Harsha Jagasia  <harsha.jagasia@amd.com>
    	PR tree-optimization/37828
    	* testsuite/gcc.dg/graphite/pr37828.c: New.
    	* graphite.c (graphite_trans_loop_block): Do not loop block
    	single nested loops.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@141142 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/graphite/pr37828.c
@@ -0,0 +1,25 @@
+/* { dg-options "-O2 -floop-block" } */
+
+typedef struct foo
+{
+  struct foo **Node;
+} foo;
+
+static int sort_and_split (foo **Root, foo **Finite, long first)
+{
+  foo *cd;
+  long i;
+  for (i = 0; i < first; i++)
+    cd->Node[i] = Finite[first+i];
+
+  sort_and_split(Root, Finite, first);
+  return (0);
+}
+
+
+void Build_foo(foo **Root, foo **Finite, foo **Infinite)
+{
+  long low, high;
+  while (sort_and_split(Root, Finite, low) == 0);
+}
+
