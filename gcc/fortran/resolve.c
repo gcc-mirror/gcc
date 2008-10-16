dd9315ded08da38108ff04216afb2ac333ab5380
Daniel Kraft <d@domob.eu>
2008-10-16 18:28:23 +0200

    resolve.c (resolve_elemental_actual): Handle calls to intrinsic subroutines correctly.
    2008-10-16  Daniel Kraft  <d@domob.eu>
    
    	* resolve.c (resolve_elemental_actual): Handle calls to intrinsic
    	subroutines correctly.
    
    2008-10-16  Daniel Kraft  <d@domob.eu>
    
    	* gfortran.dg/elemental_intrinsic_1.f03: New test.
    
    From-SVN: r141175

--- gcc/fortran/resolve.c
+++ gcc/fortran/resolve.c
@@ -1352,10 +1352,18 @@ resolve_elemental_actual (gfc_expr *expr, gfc_code *c)
       else
 	return SUCCESS;
     }
-  else if (c && c->ext.actual != NULL && c->symtree->n.sym->attr.elemental)
+  else if (c && c->ext.actual != NULL)
     {
       arg0 = c->ext.actual;
-      esym = c->symtree->n.sym;
+      
+      if (c->resolved_sym)
+	esym = c->resolved_sym;
+      else
+	esym = c->symtree->n.sym;
+      gcc_assert (esym);
+
+      if (!esym->attr.elemental)
+	return SUCCESS;
     }
   else
     return SUCCESS;
