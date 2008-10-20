11824985de2dd921a45fbfe5460df22d2dc6bea0
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-10-20 16:43:28 +0000

    2008-10-20  Paolo Carlini  <paolo.carlini@oracle.com>
    	* include/tr1_impl/hashtable_policy.h (_Hash_node<>::_Hash_node<>
    	(_Args&&...)): Add in C++0x mode.
    	* include/tr1_impl/hashtable (_Hashtable<>::_M_allocate_node,
    	_Hashtable<>::_M_deallocate_node): Use _M_get_Node_allocator in
    	C++0x mode.
    
    	* include/tr1_impl/hashtable (_Hashtable<>::max_size): Use
    	Node_allocator for improved accuracy.
    	* testsuite/tr1/6_containers/unordered_multimap/capacity/
    	29134-multimap.cc: Adjust.
    	* testsuite/tr1/6_containers/unordered_multimap/capacity/
    	29134-map.cc: Likewise.
    	* testsuite/tr1/6_containers/unordered_multimap/capacity/
    	29134-multiset.cc: Adjust.
    	* testsuite/tr1/6_containers/unordered_multimap/capacity/
    	29134-set.cc: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@141242 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/tr1/6_containers/unordered_map/capacity/29134-map.cc
+++ libstdc++-v3/testsuite/tr1/6_containers/unordered_map/capacity/29134-map.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2006 Free Software Foundation, Inc.
+// Copyright (C) 2006, 2007, 2008 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -28,7 +28,8 @@ void test01()
 
   std::tr1::unordered_map<int, int> um;
 
-  VERIFY( um.max_size() == um.get_allocator().max_size() );
+  VERIFY( (um.max_size() == std::allocator<std::tr1::__detail::_Hash_node<
+ 	   std::pair<const int, int>, false> >().max_size()));
 }
 
 int main()
