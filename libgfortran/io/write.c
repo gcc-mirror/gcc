b71eda4d769adc97a6acd0ec1a2b6995f77655bc
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-10-23 02:31:00 +0000

    2008-10-22  Jerry DeLisle  <jvdelisle@gcc.gnu.org
    	PR libfortran/37707
    	* io/list_read.c (read_character): Remove code to look ahead in namelist
    	reads to descriminate non-delimited strings from namelist objects.
    	* io/write.c (namelist_write): Delimit character strings with quote or
    	apostrophe, defaulting to quote.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@141317 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgfortran/io/write.c
+++ libgfortran/io/write.c
@@ -1442,20 +1442,8 @@ namelist_write (st_parameter_dt *dtp)
 
   /* Set the delimiter for namelist output.  */
   tmp_delim = dtp->u.p.current_unit->delim_status;
-  switch (tmp_delim)
-    {
-    case (DELIM_QUOTE):
-      dtp->u.p.nml_delim = '"';
-      break;
 
-    case (DELIM_APOSTROPHE):
-      dtp->u.p.nml_delim = '\'';
-      break;
-
-    default:
-      dtp->u.p.nml_delim = '\0';
-      break;
-    }
+  dtp->u.p.nml_delim = tmp_delim == DELIM_APOSTROPHE ? '\'' : '"';
 
   /* Temporarily disable namelist delimters.  */
   dtp->u.p.current_unit->delim_status = DELIM_NONE;
