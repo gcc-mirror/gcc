45bc572c86125d86ea782853fe6f1cbff94c63b4
Mikael Morin <mikael.morin@tele2.fr>
2008-10-30 21:45:09 +0100

    re PR fortran/37903 (wrong-code for complicated vector subscripts)
    2008-10-30  Mikael Morin  <mikael.morin@tele2.fr>
    
            PR fortran/37903
            * trans-array.c (gfc_trans_create_temp_array): If n is less
    	than the temporary dimension, assert that loop->from is
    	zero (reverts to earlier versions). If there is at least one
    	null loop->to[n], it is a callee allocated array so set the
    	size to NULL and break.
    	(gfc_trans_constant_array_constructor): Set the offset to zero.
    	(gfc_trans_array_constructor): Remove loop shifting around the
    	temporary creation.
    	(gfc_conv_loop_setup): Prefer zero-based descriptors if
    	possible.  Calculate the translation from loop variables to
    	array indices if an array constructor.
    
    2008-10-30  Mikael Morin  <mikael.morin@tele2.fr>
    
            PR fortran/37749
            * trans-array.c (gfc_trans_create_temp_array): If size is NULL
    	use the array bounds for loop->to.
    
    2008-10-30  Mikael Morin  <mikael.morin@tele2.fr>
    
            PR fortran/37903
            * gfortran.dg/vector_subscript_4.f90: New test.
    
    2008-10-30  Mikael Morin  <mikael.morin@tele2.fr>
    
            PR fortran/37749
            * gfortran.dg/vector_subscript__5.f90: New test.
    
    From-SVN: r141467

---
+++ gcc/testsuite/gfortran.dg/vector_subscript_4.f90
@@ -0,0 +1,13 @@
+! { dg-do compile }
+! { dg-options "-fdump-tree-original" }
+!
+! Test the fix for PR37903, in which the temporary for the vector index
+! got the wrong size.
+!
+! Contributed by Mikael Morin <mikael.morin@tele2.fr>
+!
+     integer :: i(-1:1) = 1, j(3) = 1, k(3)
+      k = j((/1,1,1/)+i)
+      end
+! { dg-final { scan-tree-dump-times "A\.3\\\[3\\\]" 1 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
