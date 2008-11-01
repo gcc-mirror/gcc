a3050002169c1f8298aa6c8126129184c096f515
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-11-01 17:00:49 +0000

    2008-11-01  Steven G. Kargl  <kargls@comcast.net>
    	PR fortran/19925
    	* gfortran.dg/initialization_20.f90: New test.
    	* gfortran.dg/initialization_21.f90: Ditto.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@141519 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/initialization_20.f90
@@ -0,0 +1,10 @@
+! { dg-do compile }
+! Test for PR19925
+!
+program pr19925
+  implicit none
+  integer j
+  integer, parameter :: n = 100000
+  integer, parameter :: i(n)=(/(j,j=1,n)/) ! { dg-error "number of elements" }
+  print *, i(5)
+end program pr19925
