38db79a685a184f994dff76c5303db535fa9bd9e
Jakub Jelinek <jakub@gcc.gnu.org>
2008-11-03 23:05:57 +0100

    re PR testsuite/37241 (FAIL: g++.dg/abi/key2.C)
    	PR testsuite/37241
    	* g++.dg/abi/key2.C: Adjust regexps to allow __ZTV1f and __ZTI1f
    	sharing one .section directive.
    
    From-SVN: r141565

--- gcc/testsuite/g++.dg/abi/key2.C
+++ gcc/testsuite/g++.dg/abi/key2.C
@@ -2,9 +2,9 @@
 // PR darwin/25908
 
 // { dg-do compile { target *-*-darwin* } }
-// { dg-final { scan-assembler ".globl __ZTV1f\\n	.weak_definition __ZTV1f\\n	.section __DATA,__const_coal,coalesced" } }
+// { dg-final { scan-assembler ".globl __ZTV1f\\n	.weak_definition __ZTV1f(\\n	.section __DATA,__const_coal,coalesced)?\\n	.align" } }
 // { dg-final { scan-assembler ".globl __ZTS1f\\n	.weak_definition __ZTS1f\\n	.section __TEXT,__const_coal,coalesced" } }
-// { dg-final { scan-assembler ".globl __ZTI1f\\n	.weak_definition __ZTI1f\\n	.section __DATA,__const_coal,coalesced" } }
+// { dg-final { scan-assembler ".globl __ZTI1f\\n	.weak_definition __ZTI1f(\\n	.section __DATA,__const_coal,coalesced)?\\n	.align" } }
 
 class f
 {
