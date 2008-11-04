a49be7fca6583df7644a75831d673ca5c5f93c5a
dannysmith <dannysmith@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-11-04 02:16:07 +0000

    2008-11-03  Mikael Pettersson  <mikpe@it.uu.se>
            PR target/37989
    	* config/i386/mingw32.h (REAL_LIBGCC_SPEC): Only add libgcc_s.a
    	or libgcc_eh.a to spec if ENABLE_SHARED_LIBGCC.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@141575 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/mingw32.h
+++ gcc/config/i386/mingw32.h
@@ -89,11 +89,15 @@ along with GCC; see the file COPYING3.  If not see
   %(shared_libgcc_undefs)"
 
 /* Include in the mingw32 libraries with libgcc */
+#ifdef ENABLE_SHARED_LIBGCC
+#define SHARED_LIBGCC_SPEC "%{shared-libgcc:-lgcc_s} %{!shared-libgcc:-lgcc_eh}"
+#else
+#define SHARED_LIBGCC_SPEC /*empty*/
+#endif
 #undef REAL_LIBGCC_SPEC
 #define REAL_LIBGCC_SPEC \
   "%{mthreads:-lmingwthrd} -lmingw32 \
-   %{shared-libgcc:-lgcc_s} \
-   %{!shared-libgcc:-lgcc_eh} \
+   "SHARED_LIBGCC_SPEC" \
    -lgcc \
    -lmoldname -lmingwex -lmsvcrt"
 
