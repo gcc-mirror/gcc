15ffa5d7ad95fd903101dac67b0930f8e915275c
ghazi <ghazi@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-11-07 09:00:14 +0000

    * g++.dg/ipa/iinline-1.C: Remove -c flag, add -fpie for PIC 	targets.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@141664 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ipa/iinline-1.C
+++ gcc/testsuite/g++.dg/ipa/iinline-1.C
@@ -1,7 +1,8 @@
 /* Verify that simple indirect calls are inlined even without early
    inlining..  */
 /* { dg-do compile } */
-/* { dg-options "-O3 -c -fdump-ipa-inline -fno-early-inlining"  } */
+/* { dg-options "-O3 -fdump-ipa-inline -fno-early-inlining"  } */
+/* { dg-options "-O3 -fdump-ipa-inline -fno-early-inlining -fpie" { target { ! nonpic } } } */
 
 extern void non_existent (const char *, int);
 
