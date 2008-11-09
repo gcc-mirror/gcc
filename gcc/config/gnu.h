40b2ba118fe99992dd7666049d748728bf3a3933
Thomas Schwinge <tschwinge@gnu.org>
2008-11-09 23:09:02 +0100

    gnu.h: Add copyright and licensing header.
    2008-11-09  Thomas Schwinge  <tschwinge@gnu.org>
    
    	* config/i386/gnu.h: Add copyright and licensing header.
    	* config/gnu.h: Likewise.
    
    From-SVN: r141721

--- gcc/config/gnu.h
+++ gcc/config/gnu.h
@@ -1,5 +1,25 @@
 /* Configuration common to all targets running the GNU system.  */
 
+/*
+Copyright (C) 1994, 1995, 1997, 1998, 1999, 2002, 2003, 2004, 2007, 2008 Free
+Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /* Provide GCC options for standard feature-test macros.  */
 #undef CPP_SPEC
 #define CPP_SPEC "%{posix:-D_POSIX_SOURCE} %{bsd:-D_BSD_SOURCE}"
