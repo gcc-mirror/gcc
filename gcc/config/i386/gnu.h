b6f5d245eab9b3fbfc25d30ee717145e139f4d44
tschwinge <tschwinge@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-11-09 22:09:02 +0000

    2008-11-09  Thomas Schwinge  <tschwinge@gnu.org>
    	* config/i386/gnu.h: Add copyright and licensing header.
    	* config/gnu.h: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@141721 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/gnu.h
+++ gcc/config/i386/gnu.h
@@ -1,5 +1,25 @@
 /* Configuration for an i386 running GNU with ELF as the target machine.  */
 
+/*
+Copyright (C) 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
+2005, 2007, 2008 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #undef TARGET_VERSION
 #define TARGET_VERSION fprintf (stderr, " (i386 GNU)");
 
