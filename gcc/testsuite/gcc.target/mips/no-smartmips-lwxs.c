da2c85251631d75408f1e4a5ef276234c431c33c
clm <clm@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-11-11 00:06:10 +0000

    * gcc.target/mips/no-smartmips-lwxs.c: New test. 	* gcc.target/mips/no-smartmips-ror-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@141759 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/mips/no-smartmips-lwxs.c
@@ -0,0 +1,8 @@
+/* { dg-do compile } */
+/* { dg-mips-options "-O -mno-smartmips" } */
+
+NOMIPS16 int scaled_indexed_word_load (int a[], int b)
+{
+  return a[b];
+}
+/* { dg-final { scan-assembler-not "\tlwxs\t" } } */
