23b49dbe8dfb1560c196edbe693e46fb0aa46698
Catherine Moore <clm@codesourcery.com>
2008-11-10 19:06:10 -0500

    no-smartmips-lwxs.c: New test.
    	* gcc.target/mips/no-smartmips-lwxs.c: New test.
    	* gcc.target/mips/no-smartmips-ror-1.c: New test.
    
    From-SVN: r141759

---
+++ gcc/testsuite/gcc.target/mips/no-smartmips-ror-1.c
@@ -0,0 +1,9 @@
+/* { dg-do compile } */
+/* { dg-mips-options "-O -mno-smartmips -march=mips32r2" } */
+
+NOMIPS16 int rotate_left (unsigned a, unsigned s)
+{
+  return (a << s) | (a >> (32 - s));
+}
+/* { dg-final { scan-assembler-not "\tror\t" } } */
+
