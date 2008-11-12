6af97bf4448bde676600a42fe1e3e292e2f9a55d
Tobias Burnus <burnus@net-b.de>
2008-11-12 19:38:08 +0100

    re PR fortran/38065 (private/public confusion with a contained function)
    2008-11-12  Tobias Burnus  <burnus@net-b.de>
    
           PR fortran/38065
           PR fortran/38094
           * gfortran.dg/private_type_4.f90: Add -std=f95 option.
    
    From-SVN: r141798

--- gcc/testsuite/gfortran.dg/private_type_4.f90
+++ gcc/testsuite/gfortran.dg/private_type_4.f90
@@ -1,5 +1,8 @@
 ! { dg-do compile }
+! { dg-options "-std=f95" }
 ! PR 25093: Check that a PUBLIC function can't be of PRIVATE type
+! in Fortran 95; in Fortran 2003 it is allowed (cf. PR fortran/38065)
+!
 module m1
 
     type :: t1
