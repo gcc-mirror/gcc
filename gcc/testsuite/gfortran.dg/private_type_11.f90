0d6872cba72c3fad11d88e31611fc5ff229f8288
Tobias Burnus <burnus@gcc.gnu.org>
2008-11-12 07:59:33 +0100

    re PR fortran/38065 (private/public confusion with a contained function)
    2008-11-12  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/38065
            * resolve.c (resolve_fntype): Fix private derived type checking.
    
    2008-11-12  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/38065
            * gfortran.dg/private_type_11.f90: New test.
            * gfortran.dg/private_type_12.f90: New test.
    
    From-SVN: r141780

---
+++ gcc/testsuite/gfortran.dg/private_type_11.f90
@@ -0,0 +1,23 @@
+! { dg-do compile }
+! { dg-options "-std=f2003" }
+! PR fortran/38065
+!
+! Reported by Norman S. Clerman
+! and reduced by Joost VandeVondele
+!
+MODULE M1
+  IMPLICIT NONE
+  PRIVATE
+  TYPE T1
+   INTEGER :: I1
+  END TYPE T1
+  PUBLIC :: S1,F2
+CONTAINS
+  SUBROUTINE S1
+  CONTAINS
+   TYPE(T1) FUNCTION F1()
+   END FUNCTION F1
+  END SUBROUTINE S1
+  TYPE(T1) FUNCTION F2()
+  END FUNCTION F2
+END MODULE M1
