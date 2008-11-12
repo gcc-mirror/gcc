83d8b61fc223d966edb58543df58d3428d683370
Jack Howarth <howarth@bromo.med.uc.edu>
2008-11-12 17:47:13 +0000

    re PR testsuite/37202 (FAIL: gcc.dg/visibility-1[4-9].c)
    2008-11-12  Jack Howarth  <howarth@bromo.med.uc.edu>
    
    	PR testsuite/37202
    	* gcc.dg/visibility-14.c:  Skip on *-*-darwin*.
    	* gcc.dg/visibility-15.c:  Same.
    	* gcc.dg/visibility-16.c:  Same.
    	* gcc.dg/visibility-17.c:  Same.
    	* gcc.dg/visibility-18.c:  Same.
    	* gcc.dg/visibility-19.c:  Same.
    
    From-SVN: r141794

--- gcc/testsuite/gcc.dg/visibility-15.c
+++ gcc/testsuite/gcc.dg/visibility-15.c
@@ -1,5 +1,6 @@
 /* Test that accessed external functions are marked. */
 /* { dg-do compile } */
+/* { dg-skip-if "" { *-*-darwin* } { "*" } { "" } } */
 /* { dg-require-visibility "" } */
 /* { dg-final { scan-hidden "foo" } } */
 
