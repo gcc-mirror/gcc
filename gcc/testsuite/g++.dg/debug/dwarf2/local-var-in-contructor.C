6b20f353f6c35385c5e473672ed373e4470df908
Dodji Seketeli <dodji@redhat.com>
2008-11-12 21:57:44 +0000

    re PR debug/27574 (MIssing debug info at -O0 for a local variable in a C++ constructor)
    gcc/ChangeLog:
    2008-11-12  Dodji Seketeli  <dodji@redhat.com>
    
    	PR debug/27574
    	* cgraph.h: New abstract_and_needed member to struct cgraph_node.
    	* cgraphunit.c (cgraph_analyze_functions): Flag abstract functions
    	- which clones are reachable - as "abstract and needed".
    	* cgraph.c (cgraph_release_function_body):  If a node is "abstract and needed",
    	do not release its DECL_INITIAL() content because that will be needed to emit
    	debug info.
    
    gcc/testsuite/ChangeLog:
    2008-11-12  Dodji Seketeli  <dodji@redhat.com>
    
    	PR debug/27574
    	* g++.dg/debug/dwarf2/local-var-in-contructor.C: New test.
    
    From-SVN: r141807

---
+++ gcc/testsuite/g++.dg/debug/dwarf2/local-var-in-contructor.C
@@ -0,0 +1,30 @@
+// Contributed by Dodji Seketeli <dodji@redhat.com>
+// Origin PR27574
+// { dg-do compile }
+// { dg-options "-O0 -g" }
+// { dg-final { scan-assembler "problem" } }
+
+void f (int *)
+{
+}
+
+class A
+{
+public:
+ A(int i);
+};
+
+A::A(int i)
+{
+ int *problem = new int(i);
+ f (problem);
+}
+
+int
+main (void)
+{
+  A a (0);
+
+  return 0;
+}
+
