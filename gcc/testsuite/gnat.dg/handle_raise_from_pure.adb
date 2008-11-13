02f226eecfc47cfad508dd0349f4bb44f84db9c6
hainque <hainque@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-11-13 14:43:23 +0000

    ada/         * gcc-interface/decl.c (gnat_to_gnu_entity) <case E_Function>:         Turn Ada Pure on subprograms back into GCC CONST when eh constructs         are explicit to the middle-end.  Tidy.
            testsuite/
            * gnat.dg/test_raise_from_pure.adb: Adjust to match revised intent.
            * gnat.dg/wrap_raise_from_pure.ad[bs]: Remove.
            * gnat.dg/handle_raise_from_pure.adb: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@141821 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gnat.dg/handle_raise_from_pure.adb
+++ gcc/testsuite/gnat.dg/handle_raise_from_pure.adb
@@ -1,8 +1,11 @@
 --  { dg-do run }
 --  { dg-options "-O2" }
+with Ada.Text_Io; use Ada.Text_IO;
 with Raise_From_Pure; use Raise_From_Pure;
-procedure test_raise_from_pure is
+procedure handle_raise_from_pure is
    K : Integer;
 begin
    K := Raise_CE_If_0 (0);
+exception
+   when others => Put_Line ("exception caught");
 end;
--- gcc/testsuite/gnat.dg/wrap_raise_from_pure.ads
+++
@@ -1,4 +0,0 @@
-
-package Wrap_Raise_From_Pure is
-   procedure Check;
-end;
