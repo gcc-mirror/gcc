ffe6ff66ca8f440d717b2a3a5012dcfb3f2edb4a
Richard Sandiford <rdsandiford@googlemail.com>
2008-11-13 22:49:37 +0000

    loongson-muldiv-1.c: New test.
    gcc/testsuite/
    	* gcc.target/mips/loongson-muldiv-1.c: New test.
    	* gcc.target/mips/loongson-muldiv-2.c: Likewise.
    
    From-SVN: r141836

---
+++ gcc/testsuite/gcc.target/mips/loongson-muldiv-1.c
@@ -0,0 +1,16 @@
+/* { dg-mips-options "-O2 -march=loongson2e" } */
+
+typedef int st;
+typedef unsigned int ut;
+
+NOMIPS16 st smul (st x, st y) { return x * y; }
+NOMIPS16 st sdiv (st x, st y) { return x / y + x % y; }
+
+NOMIPS16 ut umul (ut x, ut y) { return x * y; }
+NOMIPS16 ut udiv (ut x, ut y) { return x / y + x % y; }
+
+/* { dg-final { scan-assembler-times "\tmultu.g\t" 2 } } */
+/* { dg-final { scan-assembler-times "\tdivu.g\t" 1 } } */
+/* { dg-final { scan-assembler-times "\tmodu.g\t" 1 } } */
+/* { dg-final { scan-assembler-times "\tdiv.g\t" 1 } } */
+/* { dg-final { scan-assembler-times "\tmod.g\t" 1 } } */
