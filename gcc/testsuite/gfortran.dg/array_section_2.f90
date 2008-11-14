993ac38b0fd488754dad42d4202447cabc9d3156
Paul Thomas <pault@gcc.gnu.org>
2008-11-14 18:03:05 +0000

    re PR fortran/38033 (Bounds of  a pointer/allocatable array not stabilized)
    2008-10-14  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/38033
    	* trans-array.c (gfc_trans_create_temp_array): Stabilize the
    	'to' expression.
    	(gfc_conv_loop_setup): Use the end expression for the loop 'to'
    	if it is available.
    
    2008-10-14  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/38033
    	* gfortran.dg/array_section_2.f90: New test.
    
    From-SVN: r141861

---
+++ gcc/testsuite/gfortran.dg/array_section_2.f90
@@ -0,0 +1,16 @@
+! { dg-do compile }
+! { dg-options "-fdump-tree-original" }
+!
+! PR38033 - size(a) was not stabilized correctly and so the expression was
+! evaluated twice outside the loop and then within the scalarization loops.
+!
+! Contributed by Thomas Bruel  <tmbdev@gmail.com>
+!
+program test
+   integer, parameter :: n = 100
+   real, pointer :: a(:),temp(:)  ! pointer or allocatable have the same effect
+   allocate(a(n), temp(n))
+   temp(1:size(a)) = a
+end program
+! { dg-final { scan-tree-dump-times "size0" 1 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
