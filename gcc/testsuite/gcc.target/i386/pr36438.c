6684599e6480faaef84c605c5c35de5d4c0d794b
Uros Bizjak <ubizjak@gmail.com>
2008-11-14 08:59:33 +0100

    backport: re PR rtl-optimization/36438 (gcc ICE compiling code with mmx builtin)
    	Backport from mainline:
    	2008-06-06  Uros Bizjak <ubizjak@gmail.com>
    
    	PR rtl-optimization/36438
    	* cse.c (fold_rtx) [ASHIFT, LSHIFTRT, ASHIFTRT]: Break out early
    	for vector shifts with constant scalar shift operands.
    
    testsuite/ChangeLog:
    
    	Backport from mainline:
    	2008-06-06  Uros Bizjak <ubizjak@gmail.com>
    
    	PR rtl-optimization/36438
    	* gcc.target/i386/pr36438.c
    
    From-SVN: r141849

---
+++ gcc/testsuite/gcc.target/i386/pr36438.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -mmmx" } */
+
+#include <mmintrin.h>
+
+extern __m64 SetS16 (unsigned short, unsigned short,
+		     unsigned short, unsigned short);
+
+void
+foo (__m64 * dest)
+{
+  __m64 mask = SetS16 (0x00FF, 0xFF00, 0x0000, 0x00FF);
+
+  mask = _mm_slli_si64 (mask, 8);
+  mask = _mm_slli_si64 (mask, 8);
+
+  *dest = mask;
+
+  _mm_empty ();
+}
