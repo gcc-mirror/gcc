940bee2788264eccc88a231cb251b935505d4008
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-11-15 19:33:07 +0000

    2008-11-15  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR libfortran/37294
    	* gfortran.dg/namelist_57.f90: New test.
    	* gfortran.dg/namelist_print_1.f: Adjust test conditions.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@141893 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/namelist_57.f90
@@ -0,0 +1,12 @@
+! { dg-do run }
+! PR37294 Namelist I/O to array character internal units.
+! Test case from adapted from PR by Jerry DeLisle <jvdelisle@gcc.gnu.org>
+  character(30) :: line(3)
+  namelist /stuff/ n
+  n = 123
+  line = ""
+  write(line,nml=stuff)
+  if (line(1) .ne. "&STUFF") call abort
+  if (line(2) .ne. " N=        123,") call abort
+  if (line(3) .ne. " /") call abort
+  end 
