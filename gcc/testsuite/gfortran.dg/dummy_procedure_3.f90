4d45d495663618e45f0a0baa481b74ee7a87a597
Paul Thomas <pault@gcc.gnu.org>
2008-11-15 17:26:13 +0000

    re PR fortran/37926 (Program gives wrong output (connected to char len))
    2008-11-15  Paul Thomas  <pault@gcc.gnu.org>
    
            PR fortran/37926
            * trans-expr.c (gfc_add_interface_mapping): Transfer the formal
    	arglist and the always_explicit attribute if the dummy arg is a
    	procedure.
    
    2008-11-15  Paul Thomas  <pault@gcc.gnu.org>
    
            PR fortran/37926
            * gfortran.dg/dummy_procedure_3.f90: New test.
    
    From-SVN: r141890

---
+++ gcc/testsuite/gfortran.dg/dummy_procedure_3.f90
@@ -0,0 +1,40 @@
+! { dg-do run }
+! PR37926 - the interface did not transfer the formal
+! argument list for the call to 'asz' in the specification of 'p'.
+!
+! Contributed by Janus Weil  <janus@gcc.gnu.org>
+!
+module m
+contains
+  pure integer function mysize(a)
+    integer,intent(in) :: a(:)
+    mysize = size(a)
+  end function
+end module
+
+program prog
+  use m
+  implicit none
+  character(3) :: str
+  integer :: i(3) = (/1,2,3/)
+  str = p(i,mysize)
+  if (len(str) .ne. 3) call abort
+  if (str .ne. "BCD") call abort
+contains
+  function p(y,asz)
+    implicit none
+    integer :: y(:)
+    interface
+      pure integer function asz(c)
+        integer,intent(in) :: c(:)
+      end function
+    end interface
+    character(asz(y)) p
+    integer i
+    do i=1,asz(y)
+      p(i:i) = achar(iachar('A')+y(i))
+    end do
+  end function
+end
+! { dg-final { cleanup-modules "m" } }
+
