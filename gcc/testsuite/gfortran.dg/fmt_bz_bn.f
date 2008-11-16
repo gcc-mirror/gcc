dfc1a404f251f8f08d117f4af416d84ff1af59cb
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2008-11-16 16:18:36 +0000

    re PR libfortran/38097 (I/O with blanks in exponent fails; blank="NULL", BN edit descriptor)
    2008-11-16  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libfortran/38097
    	* gfortran.dg/fmt_bz_bn.f: New test.
    
    From-SVN: r141920

---
+++ gcc/testsuite/gfortran.dg/fmt_bz_bn.f
@@ -0,0 +1,27 @@
+c     { dg-do run }
+c     PR38097 I/O with blanks in exponent fails; BN edit descriptor
+c     Test case derived from reporter.
+      character(11)  :: a = ' 2.  3 e+ 3'
+      character(11)  :: b = ' 2.003 e+ 3'
+      character(11)  :: c = ' 2.002 e+1 '
+      real :: f
+
+      f = 0.0
+      read (a,'(BZ,E11.0)') f
+      if (f .ne. 2003.0) call abort
+      f = 0.0
+      read (a,'(BN,E11.0)') f
+      if (f .ne. 2300.0) call abort
+      f = 0.0
+      read (b,'(BN,E11.0)') f
+      if (f .ne. 2003.0) call abort
+      f = 0.0
+      read (c,'(E11.0)') f
+      if (f .ne. 2.0020) call abort
+      f = 0.0
+      read (c,'(BZ,E11.0)') f
+      if (f .ne. 2.002e10) call abort
+
+      end
+c     end of program
+
