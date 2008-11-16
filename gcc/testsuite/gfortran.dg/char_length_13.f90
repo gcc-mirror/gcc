9a7675b4165d3cc0ae707eca7ec482e2857023f9
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-11-16 14:19:38 +0000

    2008-11-16  Tobias Burnus  <burnus@net-b.de>
            PR fortran/38095
            * trans-expr.c (gfc_map_intrinsic_function): Fix pointer access.
    
    2008-11-16  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/38095
            * gfortran.dg/char_length_13.f90: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@141917 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/char_length_13.f90
@@ -0,0 +1,36 @@
+! { dg-do compile }
+!
+! PR fortran/38095
+!
+! Contributed by Vivek Rao
+!
+! Compiling the program below gave an ICE
+!
+module bar
+  implicit none
+contains
+elemental function trim_append(xx,yy) result(xy)
+  character (len=*), intent(in) :: xx,yy
+  character (len=len(xx) + len(yy)) :: xy
+  xy = trim(xx) // yy
+end function trim_append
+function same(xx) result(yy)
+  character (len=*), intent(in) :: xx(:)
+  character (len=len(xx))       :: yy(size(xx))
+  yy = [xx]
+end function same
+subroutine foo(labels)
+  character (len=*), intent(in) :: labels(:)
+  print*,"size(labels)=",size(labels)
+end subroutine foo
+subroutine xmain()
+  call foo(trim_append(["a"],same(["b"])))
+end subroutine xmain
+end module bar
+
+program main
+  use bar
+  call xmain()
+end program main
+
+! { dg-final { cleanup-modules "bar" } }
