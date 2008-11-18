411c3b5d2c54f061cffccc7e03a3ac5eedb27b38
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-11-18 22:00:12 +0000

    PR target/37362 	* config/mips/mips.md (move_doubleword_fpr<mode>): Check that "high" 	is a register or zero operand in the correct mode before generating 	mtch1 insn or a register operand in the correct mode before generating 	mfch1 insn. 	(mtch1<mode>): Correct operand 1 predicate to reg_or_0_operand.
    testsuite/ChangeLog:
    
    	PR target/37362
    	* gcc.target/mips/pr37362.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@141978 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/mips/pr37362.c
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-mips-options "-march=mips64r2 -mabi=n32" } */
+
+typedef float TFtype __attribute__((mode(TF)));
+
+TFtype
+__powitf (TFtype x, int m)
+{
+  unsigned int n = m < 0 ? -m : m;
+  TFtype y = n % 2 ? x : 1;
+  while (n >>= 1)
+    {
+      x = x * x;
+      if (n % 2)
+	y = y * x;
+    }
+  return m < 0 ? 1/y : y;
+}
+
