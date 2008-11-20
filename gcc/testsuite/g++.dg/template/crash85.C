8320114a44cb39824d8ba98c3799e4c5f028b750
Dodji Seketeli <dodji@redhat.com>
2008-11-20 00:00:39 +0000

    re PR c++/37142 (ICE: in dependent_type_p, at cp/pt.c:15585)
    gcc/testsuite/ChangeLog:
    2008-11-19  Dodji Seketeli  <dodji@redhat.com>
    
    	PR c++/37142
    	* g++.dg/template/crash85.C: New test.
    
    gcc/cp/ChangeLog
    2008-11-19  Dodji Seketeli  <dodji@redhat.com>
    
    	PR c++/37142
    	* pt.c (coerce_template_template_parm): Use the more robust
    	uses_template_parms instead of dependent_type_p.
    
    From-SVN: r142025

---
+++ gcc/testsuite/g++.dg/template/crash85.C
@@ -0,0 +1,15 @@
+// Contributed by Dodji Seketeli <dodji@redhat.com>
+// Origin: PR c++/37142
+// { dg-do compile }
+
+template<typename T, const T a, template <typename U, U u> class W> struct A {};
+
+template<typename T, const T t> struct B {};
+
+int
+main ()
+{
+  A<long, 0, B> a;
+  return 0;
+}
+
