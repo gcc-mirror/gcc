2c69d5275c7efc88be6791d2b3091a18082df7b9
Paul Thomas <pault@gcc.gnu.org>
2008-11-24 06:34:16 +0000

    re PR fortran/34820 (internal compiler error: in gfc_conv_descriptor_data_get, at fortran/trans-array.c:147)
    2008-11-24  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/34820
    	* trans-expr.c (gfc_conv_function_call): Remove all code to
    	deallocate intent out derived types with allocatable
    	components.
    	(gfc_trans_assignment_1): An assignment from a scalar to an
    	array of derived types with allocatable components, requires
    	a deep copy to each array element and deallocation of the
    	converted rhs expression afterwards.
    	* trans-array.c : Minor whitespace.
    	* trans-decl.c (init_intent_out_dt): Add code to deallocate
    	allocatable components of derived types with intent out.
    	(generate_local_decl): If these types are unused, set them
    	referenced anyway but allow the uninitialized warning.
    
    	PR fortran/34143
    	* trans-expr.c (gfc_trans_subcomponent_assign): If a conversion
    	expression has a null data pointer argument, nullify the
    	allocatable component.
    
    	PR fortran/32795
    	* trans-expr.c (gfc_trans_subcomponent_assign): Only nullify
    	the data pointer if the source is not a variable.
    
    2008-11-24  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/34820
    	* gfortran.dg/alloc_comp_constructor_6.f90 : New test.
    	* gfortran.dg/alloc_comp_basics_1.f90 : Reduce expected refs to
    	'builtin_free' from 24 to 18.
    
    	PR fortran/34143
    	* gfortran.dg/alloc_comp_constructor_5.f90 : New test.
    
    	PR fortran/32795
    	* gfortran.dg/alloc_comp_constructor_4.f90 : New test.
    
    From-SVN: r142148

---
+++ gcc/testsuite/gfortran.dg/alloc_comp_constructor_6.f90
@@ -0,0 +1,38 @@
+! { dg-do run }
+! { dg-options "-fdefault-integer-8 -O2" }
+! Tests the fix for PR34143, where the implicit type
+! conversion in the derived type constructor would fail,
+! when 'yy' was not allocated.  The testscase is an
+! extract from alloc_comp_constructor.f90.
+!
+! Reported by Thomas Koenig <tkoenig@gcc.gnu.org>
+!
+Program test_constructor
+    implicit none
+    type :: thytype
+        integer(4) :: a(2,2)
+    end type thytype
+    type :: mytype
+        integer(4), allocatable :: a(:, :)
+        type(thytype), allocatable :: q(:)
+    end type mytype
+    integer, allocatable :: yy(:,:)
+    type (thytype), allocatable :: bar(:)
+    call non_alloc
+    call alloc
+contains
+    subroutine non_alloc
+      type (mytype) :: x
+      x = mytype(yy, bar)
+      if (allocated (x%a) .or. allocated (x%q)) call abort
+    end subroutine non_alloc
+    subroutine alloc
+      type (mytype) :: x
+      allocate (yy(2,2))
+      allocate (bar(2))
+      yy = reshape ([10,20,30,40],[2,2])
+      bar = thytype (reshape ([1,2,3,4],[2,2]))
+      x = mytype(yy, bar)
+      if (.not.allocated (x%a) .or. .not.allocated (x%q)) call abort
+    end subroutine alloc
+end program test_constructor
