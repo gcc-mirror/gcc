8714fc766e4ee088b6853d11b09bebc386bdf333
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-11-24 06:34:16 +0000

    2008-11-24  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/34820
    	* trans-expr.c (gfc_conv_function_call): Remove all code to
    	deallocate intent out derived types with allocatable
    	components.
    	(gfc_trans_assignment_1): An assignment from a scalar to an
    	array of derived types with allocatable components, requires
    	a deep copy to each array element and deallocation of the
    	converted rhs expression afterwards.
    	* trans-array.c : Minor whitespace.
    	* trans-decl.c (init_intent_out_dt): Add code to deallocate
    	allocatable components of derived types with intent out.
    	(generate_local_decl): If these types are unused, set them
    	referenced anyway but allow the uninitialized warning.
    
    	PR fortran/34143
    	* trans-expr.c (gfc_trans_subcomponent_assign): If a conversion
    	expression has a null data pointer argument, nullify the
    	allocatable component.
    
    	PR fortran/32795
    	* trans-expr.c (gfc_trans_subcomponent_assign): Only nullify
    	the data pointer if the source is not a variable.
    
    2008-11-24  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/34820
    	* gfortran.dg/alloc_comp_constructor_6.f90 : New test.
    	* gfortran.dg/alloc_comp_basics_1.f90 : Reduce expected refs to
    	'builtin_free' from 24 to 18.
    
    	PR fortran/34143
    	* gfortran.dg/alloc_comp_constructor_5.f90 : New test.
    
    	PR fortran/32795
    	* gfortran.dg/alloc_comp_constructor_4.f90 : New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@142148 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/alloc_comp_auto_array_2.f90
@@ -0,0 +1,40 @@
+! { dg-do run }
+! Tests the fix for PR34820, in which the nullification of the
+! automatic array iregion occurred in the caller, rather than the
+! callee.  Since 'nproc' was not available, an ICE ensued. During
+! the bug fix, it was found that the scalar to array assignment
+! of derived types with allocatable components did not work and
+! the fix of this is tested too.
+!
+! Contributed by Toon Moene <toon@moene.indiv.nluug.nl>
+!
+module grid_io
+  type grid_index_region
+    integer, allocatable::lons(:)
+  end type grid_index_region
+contains
+  subroutine read_grid_header()
+    integer :: npiece = 1
+    type(grid_index_region),allocatable :: iregion(:)
+    allocate (iregion(npiece + 1))
+    call read_iregion(npiece,iregion)
+    if (size(iregion) .ne. npiece + 1) call abort
+    if (.not.allocated (iregion(npiece)%lons)) call abort
+    if (allocated (iregion(npiece+1)%lons)) call abort
+    if (any (iregion(npiece)%lons .ne. [(i, i = 1, npiece)])) call abort
+    deallocate (iregion)
+  end subroutine read_grid_header
+
+  subroutine read_iregion (nproc,iregion)
+    integer,intent(in)::nproc
+    type(grid_index_region), intent(OUT)::iregion(1:nproc)
+    integer :: iarg(nproc)
+    iarg = [(i, i = 1, nproc)]
+    iregion = grid_index_region (iarg) !
+  end subroutine read_iregion
+end module grid_io
+
+  use grid_io
+  call read_grid_header
+end
+! { dg-final { cleanup-tree-dump "grid_io" } }
---
+++ gcc/testsuite/gfortran.dg/alloc_comp_constructor_5.f90
@@ -0,0 +1,29 @@
+! { dg-do run }
+! { dg-options "-fdefault-integer-8" }
+! Tests the fix for PR34143, in which the implicit conversion of yy, with
+! fdefault-integer-8, would cause a segfault at runtime.
+!
+! Contributed by Thomas Koenig <tkoenig@gcc.gnu.org>
+!
+Program test_constructor
+    implicit none
+    type :: thytype
+        integer(4) :: a(2,2)
+    end type thytype
+    type :: mytype
+        integer(4), allocatable :: a(:, :)
+        type(thytype), allocatable :: q(:)
+    end type mytype
+    integer, allocatable :: yy(:,:)
+    type (thytype), allocatable :: bar(:)
+    type (mytype) :: x, y
+    x = mytype(yy, bar)
+    if (allocated (x%a) .or. allocated (x%q)) call abort
+    allocate (yy(2,2))
+    allocate (bar(2))
+    yy = reshape ([10,20,30,40],[2,2])
+    bar = thytype (reshape ([1,2,3,4],[2,2]))
+    ! Check that unallocated allocatables work
+    y = mytype(yy, bar)
+    if (.not.allocated (y%a) .or. .not.allocated (y%q)) call abort
+end program test_constructor
