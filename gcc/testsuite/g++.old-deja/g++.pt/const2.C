c63d77544c698d5ee05f194ae241db82b1769497
Jack Howarth <howarth@bromo.med.uc.edu>
2008-11-24 18:11:12 +0000

    re PR testsuite/38241 (g++.old-deja/g++.pt/const2.C fails on *-apple-darwin9)
    2008-11-24  Jack Howarth  <howarth@bromo.med.uc.edu>
    
    	PR testsuite/38241
    	* g++.old-deja/g++.pt/const2.C: Prune output for Darwin.
    
    From-SVN: r142164

--- gcc/testsuite/g++.old-deja/g++.pt/const2.C
+++ gcc/testsuite/g++.old-deja/g++.pt/const2.C
@@ -1,5 +1,6 @@
 // { dg-do link }
 // This test should get a linker error for the reference to A<int>::i.
+// { dg-prune-output "ld: symbol" }
 // { dg-message "i" "" { target *-*-* } 0 }
 
 template <class T> struct B { static const int i = 3; };
