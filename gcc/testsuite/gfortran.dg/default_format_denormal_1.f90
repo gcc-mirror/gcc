15c58b190c94455c0ea869366c6d0c1175d0381b
Janis Johnson <janis187@us.ibm.com>
2008-11-25 18:13:33 +0000

    * gfortran.dg/default_format_denormal_1.f90: Fix xfail for darwin.
    From-SVN: r142196

--- gcc/testsuite/gfortran.dg/default_format_denormal_1.f90
+++ gcc/testsuite/gfortran.dg/default_format_denormal_1.f90
@@ -1,4 +1,4 @@
-! { dg-do run { xfail *-*-darwin[89] *-*-freebsd* *-*-mingw* } }
+! { dg-do run { xfail *-*-darwin[89]* *-*-freebsd* *-*-mingw* } }
 ! Test XFAILed on these platforms because the system's printf() lacks
 ! proper support for denormals.
 !
