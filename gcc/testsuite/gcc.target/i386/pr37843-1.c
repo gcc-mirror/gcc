300300180d4dfc6c9d11e42c0e960d243e6eeb0c
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-11-28 16:30:56 +0000

    2008-11-28  H.J. Lu  <hongjiu.lu@intel.com>
    	PR middle-end/37843
    	* gcc.target/i386/pr37843-1.c: Make it nonpic targets only.
    	* gcc.target/i386/pr37843-2.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@142259 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/pr37843-1.c
+++ gcc/testsuite/gcc.target/i386/pr37843-1.c
@@ -1,5 +1,5 @@
 /* Test for stack alignment with sibcall optimization.  */
-/* { dg-do compile { target *-*-linux* } } */
+/* { dg-do compile { target nonpic } } */
 /* { dg-options "-O2 -mpreferred-stack-boundary=6 -mincoming-stack-boundary=5" } */
 /* { dg-final { scan-assembler "and\[lq\]?\[\\t \]*\\$-64,\[\\t \]*%\[re\]?sp" } } */
 /* { dg-final { scan-assembler "call\[\\t \]*foo" } } */
