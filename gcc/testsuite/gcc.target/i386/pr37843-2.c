fdbeaf4cbe4f67b39a4a0bc2e5451acd2b0dedaf
H.J. Lu <hongjiu.lu@intel.com>
2008-11-28 16:30:56 +0000

    re PR middle-end/37843 (unaligned stack in main due to tail call optimization)
    2008-11-28  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR middle-end/37843
    	* gcc.target/i386/pr37843-1.c: Make it nonpic targets only.
    	* gcc.target/i386/pr37843-2.c: Likewise.
    
    From-SVN: r142259

--- gcc/testsuite/gcc.target/i386/pr37843-2.c
+++ gcc/testsuite/gcc.target/i386/pr37843-2.c
@@ -1,5 +1,5 @@
 /* Test for stack alignment with sibcall optimization.  */
-/* { dg-do compile { target *-*-linux* } } */
+/* { dg-do compile { target nonpic } } */
 /* { dg-options "-O2 -mpreferred-stack-boundary=6 -mincoming-stack-boundary=6" } */
 /* { dg-final { scan-assembler-not "and\[lq\]?\[\\t \]*\\$-64,\[\\t \]*%\[re\]?sp" } } */
 /* { dg-final { scan-assembler-not "call\[\\t \]*foo" } } */
