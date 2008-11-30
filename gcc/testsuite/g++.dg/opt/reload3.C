0e51758578201c1506f3b3f673d111769b13959c
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-11-30 19:19:06 +0000

    PR target/38287 	* config/sparc/sparc.md (divsi3 expander): Remove constraints. 	(divsi3_sp32): Add new alternative with 'K' for operand #2. 	(cmp_sdiv_cc_set): Factor common string. 	(udivsi3_sp32): Add new alternative with 'K' for operand #2. 	Add TARGET_V9 case. 	(cmp_udiv_cc_set): Factor common string.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@142295 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/opt/reload3.C
@@ -0,0 +1,39 @@
+// PR target/38287
+// { dg-do run }
+// { dg-options "-O2 -mcpu=v8 -fPIC" { target { { sparc*-*-* } && { ilp32 && fpic } } } }
+
+#include <cstdlib>
+
+class QTime
+{
+public:
+    explicit QTime(int ms = 0) : ds(ms) {}
+    static QTime currentTime() { return QTime(); }
+    QTime addMSecs(int ms) const;
+    int msecs() const { return ds; }
+private:
+    unsigned ds;
+};
+
+static const unsigned MSECS_PER_DAY = 86400000;
+
+QTime QTime::addMSecs(int ms) const
+{
+    QTime t;
+    if ( ms < 0 ) {
+        // % not well-defined for -ve, but / is.
+        int negdays = (MSECS_PER_DAY-ms) / MSECS_PER_DAY;
+        t.ds = ((int)ds + ms + negdays*MSECS_PER_DAY)
+                % MSECS_PER_DAY;
+    } else {
+        t.ds = ((int)ds + ms) % MSECS_PER_DAY;
+    }
+    return t;
+}
+
+int main()
+{
+  if (QTime(1).addMSecs(1).msecs() != 2)
+    abort ();
+  return 0;
+}
