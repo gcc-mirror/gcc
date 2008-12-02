5c7bdd9966015e2fac8177f1c7a71b49ac690515
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-12-02 14:55:04 +0000

    2008-12-02  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/38359
    	* gcc.c-torture/compile/pr38359.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@142357 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr38359.c
@@ -0,0 +1,17 @@
+unsigned _ov_64_seek_lap (_Bool x1, _Bool x2, _Bool x3)
+{
+  unsigned ltmp_3978_7__PHI_TEMPORARY;
+  signed ltmp_4011_7;
+
+  if (!x1 || !x2)
+    while (1) ;
+
+  if (x3)
+    ltmp_3978_7__PHI_TEMPORARY = 0xffffff7e;
+  else
+    ltmp_3978_7__PHI_TEMPORARY = 1;
+
+  ltmp_4011_7 = -1;
+  return ltmp_4011_7 >> ltmp_3978_7__PHI_TEMPORARY;
+}
+
