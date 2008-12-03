cea1b9ebeef015414db84f1a3e6ab18e94476530
Tomas Bily <tomby@gcc.gnu.org>
2008-12-03 14:43:12 +0100

    pr38250.c: New file.
            * testsuite/gcc.dg/tree-ssa/pr38250.c: New file. 
    
    --This list and those below, will be ignored--
    
    A    gcc/testsuite/gcc.dg/tree-ssa/pr38250.c
    
    From-SVN: r142395

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr38250.c
@@ -0,0 +1,24 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -ftree-loop-distribution" } */
+
+typedef long unsigned int size_t;
+typedef struct   {
+          long dat[2];
+} gsl_complex_long_double;
+typedef struct {
+ size_t size;
+ size_t stride;
+ long *data;
+} gsl_vector_complex_long_double;
+
+void gsl_vector_complex_long_double_set_zero (gsl_vector_complex_long_double * v) 
+{
+ long * const data = v->data;
+ const size_t n = v->size;
+ const size_t stride = v->stride;
+ const gsl_complex_long_double zero = { { 0,0} } ;
+ size_t i;
+ for (i = 0; i < n; i++)     
+  *(gsl_complex_long_double *) (data + 2 * i * stride) = zero;
+}
+
