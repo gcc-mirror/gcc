025fb219bc5f8fbca16b62cf3754ced5c1889628
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-12-04 06:25:04 +0000

    2008-12-03  H.J. Lu  <hongjiu.lu@intel.com>
    	* g++.old-deja/g++.eh/badalloc1.C: Add the missing "}".
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@142428 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.eh/badalloc1.C
+++ gcc/testsuite/g++.old-deja/g++.eh/badalloc1.C
@@ -2,7 +2,7 @@
 // __cxa_allocate_exception's emergency buffer mutex will
 // itself call malloc(), and will fail if there is no more
 // memory available.
-// { dg-do run { xfail { { xstormy16-*-* *-*-darwin[3-7]* } || vxworks_rtp } }
+// { dg-do run { xfail { { xstormy16-*-* *-*-darwin[3-7]* } || vxworks_rtp } } }
 // Copyright (C) 2000, 2002, 2003 Free Software Foundation, Inc.
 // Contributed by Nathan Sidwell 6 June 2000 <nathan@codesourcery.com>
 
