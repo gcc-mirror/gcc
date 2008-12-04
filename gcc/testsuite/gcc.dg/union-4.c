3cfaee7a73b078a34156033bd974f3aab02cc0f2
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-12-04 11:44:41 +0000

    * cse.c (equiv_constant): Fix pasto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@142432 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/union-4.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-rtl-cse1" } */
+
+extern void abort(void);
+
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+
+typedef union {
+  uint32 i32;
+  uint64 i64;
+} u64;
+
+void foo(void)
+{
+  u64 data;
+  data.i64 = 1;
+  if (data.i32 != 1)
+    abort ();
+}
+
+/* { dg-final { scan-rtl-dump-not "abort" "cse1" { target i?86-*-* } } } */
+/* { dg-final { cleanup-rtl-dump "cse1" } } */
