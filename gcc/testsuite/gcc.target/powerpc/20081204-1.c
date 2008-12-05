c187a21f76390d3bfd0ca2235ec432c23147f957
Joseph Myers <joseph@codesourcery.com>
2008-12-05 16:09:12 +0000

    rs6000.md (move_from_CR_gt_bit): Enable for TARGET_HARD_FLOAT && !TARGET_FPRS, not TARGET_E500.
    	* config/rs6000/rs6000.md (move_from_CR_gt_bit): Enable for
    	TARGET_HARD_FLOAT && !TARGET_FPRS, not TARGET_E500.
    	* config/rs6000/spe.md (e500_cr_ior_compare): Likewise.
    
    testsuite:
    	* gcc.target/powerpc/20081204-1.c: New test.
    
    From-SVN: r142477

---
+++ gcc/testsuite/gcc.target/powerpc/20081204-1.c
@@ -0,0 +1,7 @@
+/* Test for ICE arising from inconsistent use of TARGET_E500 versus
+   TARGET_HARD_FLOAT && !TARGET_FPRS.  */
+/* { dg-do compile } */
+/* { dg-options "-mcpu=750 -mfloat-gprs=single" } */
+static int comp(const void *a,const void *b){
+  return (*(float *)a<*(float *)b)-(*(float *)a>*(float *)b);
+}
