2a64c730f197a8c6e6d029713c0ff0f4d803f86e
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-12-05 16:59:34 +0000

    PR middle-end/37248 	* fold-const.c (make_bit_field_ref): Change bitpos and bitsize 	arguments to HOST_WIDE_INT.  If type has different signedness 	than unsignedp or different precision from bitsize, create 	the right type for BIT_FIELD_REF and cast to type. 	(fold_truthop): Change first_bit and end_bit to HOST_WIDE_INT.
    	Revert:
    	2008-03-05  Richard Guenther  <rguenther@suse.de>
    	PR c++/35336
    	* fold-const.c (fold_truthop): Remove code generating
    	BIT_FIELD_REFs of structure bases.
    	(fold_binary): Likewise.
    	(make_bit_field_ref): Remove.
    	(optimize_bit_field_compare): Remove.
    	(all_ones_mask_p): Remove.
    
    	* gcc.target/i386/pr37248-1.c: New test.
    	* gcc.target/i386/pr37248-2.c: New test.
    	* gcc.target/i386/pr37248-3.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@142484 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr37248-1.c
@@ -0,0 +1,19 @@
+/* PR middle-end/37248 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+struct S
+{
+  unsigned char a : 1;
+  unsigned char b : 1;
+  unsigned char c : 1;
+} s;
+
+int
+foo (struct S x)
+{
+  return x.a && x.b && x.c;
+}
+
+/* { dg-final { scan-tree-dump "& 7\[^\n\t\]*== 7" "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
