e57e60cb26ce39d3c4707869a6375ba2e00e2ebc
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-12-06 04:17:31 +0000

    2008-12-05  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR fortran/38291
    	* gfortran.dg/streamio_16.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@142516 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/streamio_16.f90
@@ -0,0 +1,19 @@
+! { dg-do run }
+! PR38291 Rejects I/O with POS= if FMT=*
+character(15) :: sAccess
+character(1) :: instr
+integer :: mypos, i
+mypos = 0
+open(50, access="stream", form="formatted")
+write(50, *, pos=1) "Just something "
+do i=1,17
+  read( 50, *,pos=i)
+  inquire(50, access=sAccess, pos=mypos)
+  if (sAccess.ne."STREAM") call abort
+  if (mypos.ne.18) call abort
+end do
+read (50,*, end=10)
+call abort
+ 10 continue
+close(50,status="delete")
+end
