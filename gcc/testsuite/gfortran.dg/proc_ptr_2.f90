fb7ca5a762a94ab5c7f3d16831b3dc037dfa6619
Janus Weil <janus@gcc.gnu.org>
2008-12-06 13:15:49 +0100

    re PR fortran/38415 (procedure pointer assignment to abstract interface)
    2008-12-06  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/38415
    	* expr.c (gfc_check_pointer_assign): Added a check for abstract
    	interfaces in procedure pointer assignments, removed check involving
    	gfc_compare_interfaces until PR38290 is fixed completely.
    
    
    2008-12-06  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/38415
    	* gfortran.dg/proc_ptr_2.f90: Extended.
    	* gfortran.dg/proc_ptr_11.f90: Modified.
    
    From-SVN: r142520

--- gcc/testsuite/gfortran.dg/proc_ptr_2.f90
+++ gcc/testsuite/gfortran.dg/proc_ptr_2.f90
@@ -8,9 +8,17 @@ PROCEDURE(REAL), POINTER :: ptr
 PROCEDURE(REAL), SAVE    :: noptr    ! { dg-error "attribute conflicts with" }
 REAL :: x
 
+ abstract interface
+   subroutine bar(a)
+     integer :: a
+   end subroutine bar
+ end interface
+
 ptr => cos(4.0)        ! { dg-error "Invalid procedure pointer assignment" }
 ptr => x               ! { dg-error "Invalid procedure pointer assignment" }
 ptr => sin(x)          ! { dg-error "Invalid procedure pointer assignment" }
+
+ptr => bar             ! { dg-error "is invalid in procedure pointer assignment" }
 
 ALLOCATE(ptr)          ! { dg-error "must be ALLOCATABLE" }
 
