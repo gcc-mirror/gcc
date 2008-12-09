57510da6e9a4e4531a9c885b964eb91b28d12b10
aph <aph@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-12-09 10:42:41 +0000

    2008-12-09  Andrew Haley  <aph@redhat.com>
    	* fixed-value.c (do_fixed_add): Add comment.
    	* tree-ssa-loop-ivopts.c (iv_ca_cost): Likewise.
    	* builtins.c (fold_builtin_sqrt): Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@142589 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-loop-ivopts.c
+++ gcc/tree-ssa-loop-ivopts.c
@@ -4355,6 +4355,8 @@ iv_ca_add_use (struct ivopts_data *data, struct iv_ca *ivs,
 static comp_cost
 iv_ca_cost (struct iv_ca *ivs)
 {
+  /* This was a conditional expression but it triggered a bug in the
+     Solaris 8 compiler.  */
   if (ivs->bad_uses)
     return infinite_cost;
   else
