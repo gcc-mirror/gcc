7d2bb3573bb5b43bdc4e6c47d38e1fab1552eda8
dfranke <dfranke@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-12-09 19:25:55 +0000

    2008-12-09  Daniel Franke  <franke.daniel@gmail.com>
            PR fortran/36457
            * lang.opt: Added option idirafter.
            * cpp.h (gfc_cpp_add_include_path_after): New prototype.
    	* cpp.c (gfc_cpp_handle_option): Recognize and handle OPT_dirafter.
            (gfc_cpp_add_include_path_after): New, adds user-defined search path
            after any other paths.
            * invoke.texi (idirafter): New.
            (no-range-check): Fixed entry in option-index.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@142607 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/cpp.h
+++ gcc/fortran/cpp.h
@@ -41,6 +41,7 @@ gfc_try gfc_cpp_preprocess (const char *source_file);
 void gfc_cpp_done (void);
 
 void gfc_cpp_add_include_path (char *path, bool user_supplied);
+void gfc_cpp_add_include_path_after (char *path, bool user_supplied);
 
 void gfc_cpp_register_include_paths (void);
 
