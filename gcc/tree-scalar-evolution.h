771f882e18593bf51b526dc3cc75a41aaec2313c
Zdenek Dvorak <ook@ucw.cz>
2008-12-12 21:32:47 +0100

    re PR tree-optimization/32044 (final value replacement too aggressive for e.g. targets with no native div/mod insns)
    	PR tree-optimization/32044
    	* tree-scalar-evolution.h (expression_expensive_p): Declare.
    	* tree-scalar-evolution.c (expression_expensive_p): New function.
    	(scev_const_prop): Avoid introducing expensive expressions.
    	* tree-ssa-loop-ivopts.c (may_eliminate_iv): Ditto.
    
    	* gcc.dg/pr34027-1.c: Change outcome.
    	* gcc.dg/tree-ssa/pr32044.c: New test.
    
    From-SVN: r142719

--- gcc/tree-scalar-evolution.h
+++ gcc/tree-scalar-evolution.h
@@ -35,6 +35,7 @@ extern void gather_stats_on_scev_database (void);
 extern void scev_analysis (void);
 unsigned int scev_const_prop (void);
 
+bool expression_expensive_p (tree);
 extern bool simple_iv (struct loop *, gimple, tree, affine_iv *, bool);
 
 /* Returns the basic block preceding LOOP or ENTRY_BLOCK_PTR when the
