0c6ca7f80ad0d8922d775dd5f026b0303131850b
Sebastian Pop <sebastian.pop@amd.com>
2008-12-12 17:28:06 +0000

    re PR middle-end/38409 ([graphite] ICE : in canonicalize_loop_ivs, at tree-parloops.c:1384)
    2008-12-12  Sebastian Pop  <sebastian.pop@amd.com>
    
    	PR middle-end/38409
    	* gcc.dg/graphite/pr38409.c: New.
    	* graphite.c (nb_reductions_in_loop): Use simple_iv.
    
    From-SVN: r142716

---
+++ gcc/testsuite/gcc.dg/graphite/pr38409.c
@@ -0,0 +1,48 @@
+/* { dg-options "-O2 -floop-block" } */
+
+typedef struct test input ;
+struct test 
+{
+  int type ;
+  int symflag ;
+};
+Chv_copyEntriesToVector ( input *chv,double *dvec) 
+{
+  double *entries ;
+  int mm, nent;
+  int first, i, k , stride ;
+  if ( ((chv)->type == 1) ) 
+    {
+      for ( i = 0 ; i < 10 ; i++)
+        {
+	  dvec[2*mm] = entries[2*k] ;
+	  k += stride ;
+	  stride -= 2 ;
+        }
+    }
+  return(mm) ; 
+}
+/* { dg-options "-O2 -floop-block" } */
+
+typedef struct test input ;
+struct test 
+{
+  int type ;
+  int symflag ;
+};
+Chv_copyEntriesToVector ( input *chv,double *dvec) 
+{
+  double *entries ;
+  int mm, nent;
+  int first, i, k , stride ;
+  if ( ((chv)->type == 1) ) 
+    {
+      for ( i = 0 ; i < 10 ; i++)
+        {
+	  dvec[2*mm] = entries[2*k] ;
+	  k += stride ;
+	  stride -= 2 ;
+        }
+    }
+  return(mm) ; 
+}
