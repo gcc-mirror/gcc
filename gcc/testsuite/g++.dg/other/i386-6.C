e902fefa5a7d2935e8ac785ac191594878f211dc
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-12-14 19:51:03 +0000

    * g++.dg/other/i386-5.C: Include x86intrin.h. 	* g++.dg/other/i386-6.C: Ditto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@142752 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/other/i386-6.C
+++ gcc/testsuite/g++.dg/other/i386-6.C
@@ -1,11 +1,8 @@
-/* Test that {,x,e,p,t,s,w,g,a,b}mmintrin.h, mm3dnow.h and mm_malloc.h are
+/* Test that {,x,e,p,t,s,w,a,b,i}mmintrin.h, mm3dnow.h and mm_malloc.h are
    usable with -O -pedantic-errors.  */
 /* { dg-do compile { target i?86-*-* x86_64-*-* } } */
 /* { dg-options "-O -pedantic-errors -march=k8 -m3dnow -mavx -msse5 -maes -mpclmul" } */
 
-#include <wmmintrin.h>
-#include <bmmintrin.h>
-#include <immintrin.h>
-#include <mm3dnow.h>
+#include <x86intrin.h>
 
 int dummy;
