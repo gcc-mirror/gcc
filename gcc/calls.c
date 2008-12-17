0e456625276c293c62302c6c4d38d995e2b59880
Jan Hubicka <jh@suse.cz>
2008-12-17 13:03:04 +0100

    calls.c (expand_call): Do not sibcall if outgoing_reg_parm_stack_space does not match.
    	* calls.c (expand_call): Do not sibcall if
    	outgoing_reg_parm_stack_space does not match.
    
    Co-Authored-By: Kai Tietz <kai.tietz@onevision.com>
    
    From-SVN: r142792

--- gcc/calls.c
+++ gcc/calls.c
@@ -2298,6 +2298,10 @@ expand_call (tree exp, rtx target, int ignore)
 	 It does not seem worth the effort since few optimizable
 	 sibling calls will return a structure.  */
       || structure_value_addr != NULL_RTX
+      /* If outgoing reg parm stack space changes, we can not do sibcall.  */
+      || (OUTGOING_REG_PARM_STACK_SPACE (funtype)
+	  != OUTGOING_REG_PARM_STACK_SPACE (TREE_TYPE (current_function_decl)))
+      || (reg_parm_stack_space != REG_PARM_STACK_SPACE (fndecl))
       /* Check whether the target is able to optimize the call
 	 into a sibcall.  */
       || !targetm.function_ok_for_sibcall (fndecl, exp)
