ba7226a7e1bf9f55ebf8016c57c4d8ab8b1333a8
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-12-27 15:38:22 +0000

    PR middle-end/38633 	* tree-cfg.c (replace_block_vars_by_duplicates): Only call 	replace_by_duplicate_decl for VAR_DECLs or CONST_DECLs.
    	* gcc.dg/gomp/pr38633.c: New test.
    	* g++.dg/gomp/pr38633.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@142934 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/gomp/pr38633.c
@@ -0,0 +1,14 @@
+/* PR middle-end/38633 */
+/* { dg-do compile } */
+/* { dg-options "-fopenmp" } */
+
+void
+foo ()
+{
+#pragma omp parallel
+  {
+    struct A { int i; } j;
+    j.i = 6;
+    j.i++;
+  }
+}
