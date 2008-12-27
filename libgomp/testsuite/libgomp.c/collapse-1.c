29380f16cf65b96761f690399f5cc82e2976dcb3
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-12-27 15:20:28 +0000

    * testsuite/libgomp.c/collapse-1.c (main): Add private(k) clause.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@142932 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgomp/testsuite/libgomp.c/collapse-1.c
+++ libgomp/testsuite/libgomp.c/collapse-1.c
@@ -17,7 +17,7 @@ main (void)
 	  a[i][j][k] = i + j * 4 + k * 16;
   #pragma omp parallel
     {
-      #pragma omp for collapse(2) reduction(|:l)
+      #pragma omp for collapse(2) reduction(|:l) private(k)
 	for (i = 0; i < 2; i++)
 	  for (j = 0; j < 2; j++)
 	    for (k = 0; k < 2; k++)
