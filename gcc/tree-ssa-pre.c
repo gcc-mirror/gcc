0b355794dc5851d922085df25c96edf936687971
Mark Mitchell <mark@codesourcery.com>
2009-01-02 18:04:28 +0000

    re PR tree-optimization/33649 (cc1 segfaults when multiple tree opts disabled)
    	PR 33649
    	* tree-ssa-pre.c (compute_antic): Correct loop bounds.
    
    From-SVN: r143014

--- gcc/tree-ssa-pre.c
+++ gcc/tree-ssa-pre.c
@@ -2394,7 +2394,7 @@ compute_antic (void)
 	fprintf (dump_file, "Starting iteration %d\n", num_iterations);
       num_iterations++;
       changed = false;
-      for (i = 0; i < last_basic_block - NUM_FIXED_BLOCKS; i++)
+      for (i = 0; i < n_basic_blocks - NUM_FIXED_BLOCKS; i++)
 	{
 	  if (TEST_BIT (changed_blocks, postorder[i]))
 	    {
@@ -2425,7 +2425,7 @@ compute_antic (void)
 	    fprintf (dump_file, "Starting iteration %d\n", num_iterations);
 	  num_iterations++;
 	  changed = false;
-	  for (i = 0; i < last_basic_block - NUM_FIXED_BLOCKS; i++)
+	  for (i = 0; i < n_basic_blocks - NUM_FIXED_BLOCKS; i++)
 	    {
 	      if (TEST_BIT (changed_blocks, postorder[i]))
 		{
