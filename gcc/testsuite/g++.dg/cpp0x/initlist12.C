0ede37fed4a47c959ab369d5028609c0c1a16e00
Jason Merrill <jason@gcc.gnu.org>
2009-01-02 19:27:19 -0500

    fix tests
    From-SVN: r143026

--- gcc/testsuite/g++.dg/cpp0x/initlist12.C
+++ gcc/testsuite/g++.dg/cpp0x/initlist12.C
@@ -1,15 +1,16 @@
 // PR c++/38698
+// { dg-options "-std=c++0x" }
 
 struct A
 {
   int i;
 };
 
-A a({1,2});
+A a({1,2});			// { dg-error "too many initializers" }
 
 union U
 {
   int i,j;
 };
 
-U u({1,2});
+U u({1,2});			// { dg-error "too many initializers" }
