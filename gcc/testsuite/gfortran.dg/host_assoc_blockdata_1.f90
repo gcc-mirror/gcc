fc12e066b5d68d94e17d66075213a6c2a88db132
tkoenig <tkoenig@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-01-05 10:43:39 +0000

    2009-01-05  Thomas Koenig  <tkoenig@gcc.gnu.org>
    	PR fortran/38672
    	* trans-types.c (gfc_get_derived_type):  Check for the
    	presence of derived->ns->proc_name before
    	accessing derived->ns->proc_name->attr.flavor .
    	* resolve.c (resolve_symbol):  Likewise.
    
    2009-01-05  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/38672
    	* gfortran.dg/host_assoc_blockdata_1.f90:  New test.
    	* gfortran.dg/host_assoc_blockdata_2.f90:  New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@143074 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/host_assoc_blockdata_1.f90
@@ -0,0 +1,11 @@
+! { dg-do compile }
+! PR 38672 - this used to ICE.
+MODULE globals
+  TYPE :: type1
+     integer :: x
+  END TYPE type1
+  TYPE (type1) :: pdm_bps
+END module globals
+BLOCK DATA
+   use globals
+END BLOCK DATA
---
+++ gcc/testsuite/gfortran.dg/host_assoc_blockdata_2.f90
@@ -0,0 +1,17 @@
+! { dg-do compile }
+MODULE globals
+  TYPE :: type1
+     sequence
+     integer :: x
+  END TYPE type1
+  TYPE (type1) :: pdm_bps
+  common /co/ pdm_bps
+END module globals
+BLOCK DATA
+   use globals
+END BLOCK DATA
+
+program main
+  use globals
+  common /co/ pdm_bps ! { dg-error "already in a COMMON block" }
+end program main
