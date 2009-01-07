30af0edbf4637051e029063027052b843dbe8aac
Janis Johnson <janis187@us.ibm.com>
2009-01-07 18:31:27 +0000

    target-supports-dg.exp (current_compiler_flags): New.
    	* lib/target-supports-dg.exp (current_compiler_flags): New.
    	(check-flags): Use it; do not access dg-test local variables.
    	(dg-skip-if): Do not access dg-test local variables.
    	(dg-xfail-run-if): Ditto.
    	(dg-shouldfail): Ditto.
    	* gcc.test-framework/test-framework.exp
    	(check_effective_target_def_nocache): New.
    	* gcc.test-framework/test-framework.awk: Handle scan tests.
    	* gcc.test-framework/dg-do-run-xrif-nocache-exp-XF.c: New test.
    	* gcc.test-framework/dg-error-nocache-exp-P.c: New test.
    	* gcc.test-framework/dg-nocache-scanasm-exp-XF.c: New test.
    	* gcc.test-framework/dg-warning-nocache-exp-P.c: New test.
    	* gcc.test-framework/dg-nocache-sif-exp-P.c: New test.
    	* gcc.test-framework/dg-nocache-sif-exp-U.c: New test.
    	* gcc.test-framework/dg-nocache-xif-exp-P.c: New test.
    	* gcc.test-framework/dg-do-run-sft-nocache-exp-P.c: New test.
    	* gcc.test-framework/dg-nocache-xif-exp-XP.c: New test.
    
    From-SVN: r143165

---
+++ gcc/testsuite/gcc.test-framework/dg-nocache-sif-exp-P.c
@@ -0,0 +1,9 @@
+/* { dg-do compile } */
+/* { dg-options "-DNOT_THE_RIGHT_DEFINE" } */
+/* { dg-skip-if "comment" { def_nocache } { "*" } { "" } } */
+
+int
+main ()
+{
+  return 0;
+}
---
+++ gcc/testsuite/gcc.test-framework/dg-nocache-xif-exp-XP.c
@@ -0,0 +1,9 @@
+/* { dg-do compile } */
+/* { dg-options "-DDEFINED" } */
+/* { dg-xfail-if "comment" { def_nocache } { "*" } { "" } } */
+
+int
+main ()
+{
+  return 0;
+}
