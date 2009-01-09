8b791297bfc5e1dd776fab8ee7b772143838f03a
Paul Thomas <pault@gcc.gnu.org>
2009-01-09 23:47:55 +0000

    check.c: Update copyright year.
    2009-01-10  Paul Thomas  <pault@gcc.gnu.org>
    
    	* check.c : Update copyright year.
    	* dependency.c : Update copyright year.
    	* interface.c : ditto.
    	* intrinsic.c : ditto.
    	* intrinsic.h : ditto.
    	* module.c : ditto.
    	* simplify.c : ditto.
    	* symbol.c : ditto.
    	* trans-stmt.c : ditto.
    	* trans-types.c : ditto.
    
    From-SVN: r143236

--- gcc/fortran/interface.c
+++ gcc/fortran/interface.c
@@ -1,5 +1,5 @@
 /* Deal with interfaces.
-   Copyright (C) 2000, 2001, 2002, 2004, 2005, 2006, 2007, 2008
+   Copyright (C) 2000, 2001, 2002, 2004, 2005, 2006, 2007, 2008, 2009
    Free Software Foundation, Inc.
    Contributed by Andy Vaught
 
--- gcc/fortran/module.c
+++ gcc/fortran/module.c
@@ -1,6 +1,6 @@
 /* Handle modules, which amounts to loading and saving symbols and
    their attendant structures.
-   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008
+   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
    Free Software Foundation, Inc.
    Contributed by Andy Vaught
 
