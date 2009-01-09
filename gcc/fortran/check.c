e4a4d1984de74bb848f10d5c973136f903ecb908
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-01-09 23:47:55 +0000

    2009-01-10  Paul Thomas  <pault@gcc.gnu.org>
    	* check.c : Update copyright year.
    	* dependency.c : Update copyright year.
    	* interface.c : ditto.
    	* intrinsic.c : ditto.
    	* intrinsic.h : ditto.
    	* module.c : ditto.
    	* simplify.c : ditto.
    	* symbol.c : ditto.
    	* trans-stmt.c : ditto.
    	* trans-types.c : ditto.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@143236 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/check.c
+++ gcc/fortran/check.c
@@ -1,5 +1,5 @@
 /* Check functions
-   Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008
+   Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
    Free Software Foundation, Inc.
    Contributed by Andy Vaught & Katherine Holcomb
 
--- gcc/fortran/intrinsic.h
+++ gcc/fortran/intrinsic.h
@@ -1,6 +1,6 @@
 /* Header file for intrinsics check, resolve and simplify function
    prototypes.
-   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008
+   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
    Free Software Foundation, Inc.
    Contributed by Andy Vaught & Katherine Holcomb
 
