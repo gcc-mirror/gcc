f3056c5903878061b38250d1ac4944d08624d776
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-01-10 11:49:04 +0000

    PR c++/38648 	* typeck.c (cp_build_modify_expr): Check for NULL current_function_decl.
    	* g++.dg/expr/string-1.C: New test.
    	* g++.dg/expr/string-2.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@143245 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/expr/string-2.C
@@ -0,0 +1,10 @@
+// { dg-do compile }
+// This testcase used to seg fault (PR c++/38648)
+
+char a[1];
+
+int foo(a = "") // { dg-error "invalid array assignment" }
+{ // { dg-error "" }
+  return 0;
+}
+
