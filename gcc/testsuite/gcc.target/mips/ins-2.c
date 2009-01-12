0e510b3e4e3a6998f65b33b7f5b238abed2587d1
Adam Nemet <anemet@caviumnetworks.com>
2009-01-12 04:27:31 +0000

    expmed.c (store_bit_field_1): Properly truncate the paradoxical subreg of op0 to the original op0.
    	* expmed.c (store_bit_field_1): Properly truncate the paradoxical
    	subreg of op0 to the original op0.
    
    testsuite/
    	* gcc.target/mips/ins-2.c: New test.
    
    From-SVN: r143288

---
+++ gcc/testsuite/gcc.target/mips/ins-2.c
@@ -0,0 +1,27 @@
+/* { dg-do compile } */
+/* { dg-options "-O -meb isa_rev>=2 -mgp64" } */
+/* { dg-final { scan-assembler-times "\tins\t|\tdins\t" 1 } } */
+/* { dg-final { scan-assembler-times "\tsll\t|\tins\t" 1 } } */
+
+/* When inserting something into the top bit of a 32-bit structure,
+   we must make sure that the register remains properly sign-extended.
+   There are two ways of doing this:
+ 
+     - use purely 32-bit bit manipulations (a single INS, matched twice here).
+     - use a 64-bit bit manipulation (DINS), and sign-extend the result.  We
+     check for this extension using SLL.  */
+
+struct s
+{
+  int a:3;
+  int b:29;
+};
+
+NOMIPS16 void
+f (int a)
+{
+  struct s s;
+  asm volatile ("" : "=r"(s));
+  s.a = a;
+  asm volatile ("" :: "r"(s));
+}
