a7cbc5175600c96bab096dd100fc0ee918a34073
Jakub Jelinek <jakub@redhat.com>
2009-01-13 19:11:50 +0100

    re PR c++/38795 (ICE with reinterpret_cast and variadic templates)
    	PR c++/38795
    	* tree.c (cp_walk_subtrees): Handle REINTERPRET_CAST_EXPR,
    	STATIC_CAST_EXPR, CONST_CAST_EXPR and DYNAMIC_CAST_EXPR the same
    	as CAST_EXPR.
    
    	* g++.dg/cpp0x/pr38795.C: New test.
    
    From-SVN: r143351

---
+++ gcc/testsuite/g++.dg/cpp0x/pr38795.C
@@ -0,0 +1,13 @@
+// PR c++/38795
+// { dg-do compile }
+// { dg-options "-std=gnu++0x" }
+
+template<typename... T> int foo(int i)
+{
+  return *reinterpret_cast<T*>(i);	// { dg-error "not expanded with|T" }
+}
+
+void bar(int i)
+{
+  foo<int>(i);
+}
