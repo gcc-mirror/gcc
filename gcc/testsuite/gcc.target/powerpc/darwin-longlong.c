82fc74c7a62c273849e7152faff7ff8f44eb001e
Daniel Jacobowitz <dan@codesourcery.com>
2009-01-13 03:05:10 +0000

    target-supports.exp (check_effective_target_powerpc64): New.
    	* lib/target-supports.exp (check_effective_target_powerpc64): New.
    	* gcc.target/powerpc/darwin-longlong.c: Explicitly require 64-bit
    	instruction support.  Do not check for it at runtime.
    
    From-SVN: r143324

--- gcc/testsuite/gcc.target/powerpc/darwin-longlong.c
+++ gcc/testsuite/gcc.target/powerpc/darwin-longlong.c
@@ -1,16 +1,8 @@
 /* { dg-do run { target powerpc*-*-* } } */
-/* { dg-xfail-if "" { "powerpc-*-eabispe*" "powerpc-ibm-aix*" } { "*" } { "" } } */
+/* { dg-require-effective-target powerpc64 } */
 /* { dg-options "-mcpu=G5" } */
 
-#include <signal.h>
 #include <stdlib.h>
-
-void
-sig_ill_handler (int sig)
-{
-    exit(0);
-}
-
 
 int  msw(long long in)
 {
@@ -24,16 +16,6 @@ int  msw(long long in)
 
 int main()
 {
-
-  /* Exit on systems without 64bit instructions.  */
-  signal (SIGILL, sig_ill_handler);
-#ifdef __MACH__
-  asm volatile ("extsw r0,r0");
-#else
-  asm volatile ("extsw 0,0");
-#endif
-  signal (SIGILL, SIG_DFL);
-
   if (msw(1) != 0)
     abort();
   exit(0);
