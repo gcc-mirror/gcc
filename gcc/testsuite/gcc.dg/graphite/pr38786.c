8293dc882e5738603be2c66090a1e2c480166f41
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-01-14 15:29:39 +0000

    2009-01-13  Sebastian Pop  <sebastian.pop@amd.com>
    	* gcc.dg/graphite/pr38786.c: Fix commit problem.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@143373 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/graphite/pr38786.c
+++ gcc/testsuite/gcc.dg/graphite/pr38786.c
@@ -18,23 +18,3 @@ dummy_slice_too_big (int bits_slice)
       for (l = 0; l < 65; l++)
 	img->cofAC[i][j][k][l] = rdopt->cofAC[i][j][k][l];
 }
-/* { dg-options "-O2 -fgraphite-identity" } */
-
-typedef struct
-{
-  int ****cofAC;
-} ImageParameters;
-typedef struct
-{
-  int ****cofAC;
-} RD_DATA;
-extern RD_DATA *rdopt;
-extern ImageParameters *img;
-dummy_slice_too_big (int bits_slice)
-{
-  int i, j, k, l;
-  for (j = 0; j < 4; j++)
-    for (k = 0; k < 2; k++)
-      for (l = 0; l < 65; l++)
-	img->cofAC[i][j][k][l] = rdopt->cofAC[i][j][k][l];
-}
