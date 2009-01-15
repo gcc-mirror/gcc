f1253e7e69d20a489bb734ea66d0702e9eaa4b76
Steve Ellcey <sje@cup.hp.com>
2009-01-15 18:09:04 +0000

    re PR c++/38357 (ICE cc1plus (Segmentation fault))
    	PR c++/38357
    	* g++.dg/template/crash87.C: New test.
    
    From-SVN: r143403

---
+++ gcc/testsuite/g++.dg/template/crash87.C
@@ -0,0 +1,27 @@
+// Origin: PR c++/38357
+// { dg-do compile }
+
+class BUG
+{
+public:
+ bool name() { return true; }
+};
+
+template <bool T>
+struct BUG1_5
+{
+
+};
+
+template <bool name>
+class BUG2 : BUG
+{
+public:
+ typedef BUG1_5<name> ptr; // { dg-error "could not convert template argument" }
+};
+
+int main()
+{
+ BUG2<false> b;
+ return 0;
+}
