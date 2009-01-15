0efc44420bbbcdabc7b0606102f474ae3cea4cf1
Dodji Seketeli <dodji@redhat.com>
2009-01-15 13:01:36 +0000

    re PR c++/38636 (ICE with broken ctor declaration)
    gcc/cp/ChangeLog:
    2009-01-13  Dodji Seketeli  <dodji@redhat.com>
    
    	PR c++/38636
    	* name-lookup.c (pushtag): Don't create members to types that are not
    	being created.
    
    gcc/testsuite/ChangeLog:
    2009-01-13  Dodji Seketeli  <dodji@redhat.com>
    
    	PR c++/38636
    	* g++.dg/parse/crash50.C: New test.
    
    From-SVN: r143392

---
+++ gcc/testsuite/g++.dg/parse/crash50.C
@@ -0,0 +1,10 @@
+// Contributed by Dodji Seketeli <dodji@redhat.com>
+// Origin PR c++/38636
+// { dg-do compile }
+
+struct A; // { dg-error "forward declaration of 'struct A'" }
+
+A::A(
+
+struct B; // { dg-error "expected '\\)' before ';' token|invalid use of incomplete type 'struct A'" }
+
