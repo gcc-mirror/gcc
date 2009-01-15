e939805b7bf63bec46a4b2de6f272d3f77238448
H.J. Lu <hongjiu.lu@intel.com>
2009-01-15 15:44:41 +0000

    re PR middle-end/37843 (unaligned stack in main due to tail call optimization)
    gcc/
    
    2009-01-15  H.J. Lu  <hongjiu.lu@intel.com>
    	    Joey Ye  <joey.ye@intel.com>
    
    	PR middle-end/37843
    	* cfgexpand.c (expand_stack_alignment): Don't update stack
    	boundary nor check incoming stack boundary here.
    	(gimple_expand_cfg): Update stack boundary and check incoming
    	stack boundary here.
    
    gcc/testsuite/
    
    2009-01-15  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR middle-end/37843
    	* gcc.target/i386/pr37843-3.c: Replace _Decimal128 with __m128.
    
    Co-Authored-By: Joey Ye <joey.ye@intel.com>
    
    From-SVN: r143400

--- gcc/testsuite/gcc.target/i386/pr37843-3.c
+++ gcc/testsuite/gcc.target/i386/pr37843-3.c
@@ -1,13 +1,15 @@
 /* Test for stack alignment with sibcall optimization.  */
-/* { dg-do compile { target { *-*-linux* && { ilp32 && nonpic } } } } */
-/* { dg-options "-O2 -std=gnu99 -mpreferred-stack-boundary=4 -mincoming-stack-boundary=2" } */
+/* { dg-do compile { target { ilp32 && nonpic } } } */
+/* { dg-options "-O2 -msse2 -mpreferred-stack-boundary=4 -mstackrealign" } */
 /* { dg-final { scan-assembler-not "andl\[\\t \]*\\$-16,\[\\t \]*%\[re\]?sp" } } */
 /* { dg-final { scan-assembler-not "call\[\\t \]*foo" } } */
 /* { dg-final { scan-assembler "jmp\[\\t \]*foo" } } */
 
-extern int foo (_Decimal128);
+#include <emmintrin.h>
 
-int bar (_Decimal128 x)
+extern int foo (__m128, __m128, __m128, __m128);
+
+int bar (__m128 x1, __m128 x2, __m128 x3, __m128 x4)
 {
-    return foo(x);
+    return foo (x1, x2, x3, x4);
 }
