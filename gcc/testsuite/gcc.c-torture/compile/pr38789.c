99e2edfde9d158ec7c3a893b62fea400517d7784
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-01-16 15:01:24 +0000

    PR tree-optimization/38789 	* tree-ssa-threadedge.c 	(record_temporary_equivalences_from_stmts_at_dest): Ignore calls to 	__builtin_constant_p.
    	* gcc.c-torture/compile/pr38789.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@143435 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr38789.c
@@ -0,0 +1,18 @@
+/* PR tree-optimization/38789 */
+
+void
+baz (int v)
+{
+  unsigned a = (v == 1) ? 1 : 2;
+
+  if (__builtin_constant_p (a))
+    asm volatile ("# constant %0" :: "i" (a));
+  else
+    asm volatile ("# register %0" :: "r" (a));
+
+  a = 6;
+  if (__builtin_constant_p (a))
+    asm volatile ("# constant %0" :: "i" (a));
+  else
+    asm volatile ("# register %0" :: "r" (a));
+}
