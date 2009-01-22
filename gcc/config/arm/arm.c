442dc742e44ddfdcd11c81be6ac562fbba62da4b
Mark Shinwell <shinwell@codesourcery.com>
2009-01-22 11:16:45 +0000

    arm.c (all_architectures): Add iWMMXt2 entry.
    2009-01-22  Mark Shinwell  <shinwell@codesourcery.com>
                Joseph Myers  <joseph@codesourcery.com>
    
    	* config/arm/arm.c (all_architectures): Add iWMMXt2 entry.
    	* config/arm/arm-cores.def: New ARM_CORE entry for iWMMXt2.
    	* config/arm/arm-tune.md: Regenerate.
    	* doc/invoke.texi (ARM Options): Document -mcpu=iwmmxt2 and
    	-march=iwmmxt2.
    
    Co-Authored-By: Joseph Myers <joseph@codesourcery.com>
    
    From-SVN: r143565

--- gcc/config/arm/arm.c
+++ gcc/config/arm/arm.c
@@ -665,6 +665,7 @@ static const struct processors all_architectures[] =
   {"armv7-m", cortexm3,	  "7M",	 FL_CO_PROC |		  FL_FOR_ARCH7M, NULL},
   {"ep9312",  ep9312,     "4T",  FL_LDSCHED | FL_CIRRUS | FL_FOR_ARCH4, NULL},
   {"iwmmxt",  iwmmxt,     "5TE", FL_LDSCHED | FL_STRONG | FL_FOR_ARCH5TE | FL_XSCALE | FL_IWMMXT , NULL},
+  {"iwmmxt2", iwmmxt2,     "5TE", FL_LDSCHED | FL_STRONG | FL_FOR_ARCH5TE | FL_XSCALE | FL_IWMMXT , NULL},
   {NULL, arm_none, NULL, 0 , NULL}
 };
 
