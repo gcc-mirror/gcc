9a7965a1811295d9ec134c5e3cd9d8f5774376af
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-01-27 10:18:54 +0000

    PR middle-end/38969 	* calls.c (initialize_argument_information): Do not wrap complex 	arguments in SAVE_EXPR.
    testsuite/ChangeLog:
    
    	PR middle-end/38969
    	* gcc.c-torture/execute/pr38969.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@143699 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/pr38969.c
@@ -0,0 +1,25 @@
+__complex__ float
+__attribute__ ((noinline)) foo (__complex__ float x)
+{
+  return x;
+}
+
+__complex__ float
+__attribute__ ((noinline)) bar (__complex__ float x)
+{
+  return foo (x);
+}
+
+int main()
+{
+  __complex__ float a, b;
+  __real__ a = 9;
+  __imag__ a = 42;
+
+  b = bar (a);
+
+  if (a != b)
+    abort ();
+
+  return 0;
+}
