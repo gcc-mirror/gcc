bee6fa6d32b8a231cae2af0cf700456d827a507e
Adam Nemet <anemet@caviumnetworks.com>
2009-01-28 02:55:20 +0000

    c.opt (Wpacked-bitfield-compat): Change init value to -1.
    	* c.opt (Wpacked-bitfield-compat): Change init value to -1.
    	* c-opts.c (c_common_post_options): If -W*packed-bitfield-compat
    	was not supplied then set warn_packed_bitfield_compat to the
    	default value of 1.
    	* stor-layout.c (place_field): Check warn_packed_bitfield_compat
    	against 1.
    
    From-SVN: r143718

--- gcc/c-opts.c
+++ gcc/c-opts.c
@@ -1085,6 +1085,11 @@ c_common_post_options (const char **pfilename)
   if (warn_sign_conversion == -1)
     warn_sign_conversion =  (c_dialect_cxx ()) ? 0 : warn_conversion;
 
+  /* -Wpacked-bitfield-compat is on by default for the C languages.  The
+     warning is issued in stor-layout.c which is not part of the front-end so
+     we need to selectively turn it on here.  */
+  if (warn_packed_bitfield_compat == -1)
+    warn_packed_bitfield_compat = 1;
 
   /* Special format checking options don't work without -Wformat; warn if
      they are used.  */
