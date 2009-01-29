88ff55103eb0c07b466826f31cf5248a5df50e81
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-01-29 17:02:52 +0000

    2009-01-29  Paolo Carlini  <paolo.carlini@oracle.com>
    	* typeck.c (invalid_nonstatic_memfn_p): Use
    	DECL_NONSTATIC_MEMBER_FUNCTION_P.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@143761 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/typeck.c
+++ gcc/cp/typeck.c
@@ -1,6 +1,6 @@
 /* Build expressions with type checking for C++ compiler.
    Copyright (C) 1987, 1988, 1989, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008
+   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
    Free Software Foundation, Inc.
    Hacked by Michael Tiemann (tiemann@cygnus.com)
 
@@ -1494,7 +1494,7 @@ cxx_sizeof_or_alignof_expr (tree e, enum tree_code op, bool complain)
 bool
 invalid_nonstatic_memfn_p (const_tree expr, tsubst_flags_t complain)
 {
-  if (TREE_CODE (TREE_TYPE (expr)) == METHOD_TYPE)
+  if (DECL_NONSTATIC_MEMBER_FUNCTION_P (expr))
     {
       if (complain & tf_error)
         error ("invalid use of non-static member function");
