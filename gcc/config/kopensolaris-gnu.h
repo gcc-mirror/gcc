416ac4d45b958aca5851655c91c70ba6742e9651
Robert Millan <rmh@aybabtu.com>
2009-01-29 18:48:41 +0000

    configure.ac: Recognize GNU/kOpenSolaris (*-*-kopensolaris*-gnu).
    2009-01-29  Robert Millan  <rmh@aybabtu.com>
    
    	* configure.ac: Recognize GNU/kOpenSolaris (*-*-kopensolaris*-gnu).
    	* configure: Regenerate.
    
    gcc/
    2009-01-29  Robert Millan  <rmh@aybabtu.com>
    
    	* gcc/config.gcc: Recognize GNU/kOpenSolaris (*-*-kopensolaris*-gnu).
    	* gcc/config/i386/kopensolaris-gnu.h: New file.  Undefine
    	`MD_UNWIND_SUPPORT'.
    	* gcc/config/kopensolaris-gnu.h: New file (based on kfreebsd-gnu.h).
    
    From-SVN: r143768

---
+++ gcc/config/kopensolaris-gnu.h
@@ -0,0 +1,37 @@
+/* Definitions for kOpenSolaris-based GNU systems with ELF format
+   Copyright (C) 2004, 2006, 2007, 2009
+   Free Software Foundation, Inc.
+   Contributed by Robert Millan.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#undef LINUX_TARGET_OS_CPP_BUILTINS    
+#define LINUX_TARGET_OS_CPP_BUILTINS()		\
+  do						\
+    {						\
+	builtin_define ("__OpenSolaris_kernel__");	\
+	builtin_define ("__GLIBC__");		\
+	builtin_define_std ("unix");		\
+	builtin_assert ("system=unix");		\
+	builtin_assert ("system=posix");	\
+    }						\
+  while (0)
+
+#ifdef GLIBC_DYNAMIC_LINKER
+#undef GLIBC_DYNAMIC_LINKER
+#define GLIBC_DYNAMIC_LINKER "/lib/ld.so.1"
+#endif
