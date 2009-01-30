709cdea68ac169ec69d645e21fc6cdf262f79eb8
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-01-30 20:46:32 +0000

    PR target/39013 	* c-decl.c (pop_scope): Set DECL_EXTERNAL for functions declared 	inline but never defined.
    	* gcc.target/i386/pr39013-1.c: New test.
    	* gcc.target/i386/pr39013-2.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@143803 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr39013-1.c
@@ -0,0 +1,15 @@
+/* PR target/39013 */
+/* { dg-do compile { target *-*-linux* } } */
+/* { dg-options "-O2 -fpie -std=gnu89" } */
+
+inline int foo (void);
+extern inline int bar (void);
+
+int
+main (void)
+{
+  return foo () + bar ();
+}
+
+/* { dg-final { scan-assembler "foo@PLT" } } */
+/* { dg-final { scan-assembler "bar@PLT" } } */
