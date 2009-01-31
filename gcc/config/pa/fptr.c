5d15a648eff97beced79eea772198f009c42092c
John David Anglin <dave.anglin@nrc-cnrc.gc.ca>
2009-01-31 20:07:56 +0000

    fptr.c: Revert license to GPL 2.
    	* config/pa/fptr.c: Revert license to GPL 2.
    	* config/pa/milli64.S: Likewise.
    
    From-SVN: r143833

--- gcc/config/pa/fptr.c
+++ gcc/config/pa/fptr.c
@@ -6,7 +6,7 @@ This file is part of GCC.
 
 GCC is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free
-Software Foundation; either version 3, or (at your option) any later
+Software Foundation; either version 2, or (at your option) any later
 version.
 
 In addition to the permissions in the GNU General Public License, the
@@ -24,8 +24,9 @@ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 for more details.
 
 You should have received a copy of the GNU General Public License
-along with GCC; see the file COPYING3.  If not see
-<http://www.gnu.org/licenses/>.  */
+along with GCC; see the file COPYING.  If not, write to the Free
+Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.  */
 
 /* WARNING: The code is this function depends on internal and undocumented
    details of the GNU linker and dynamic loader as implemented for parisc
