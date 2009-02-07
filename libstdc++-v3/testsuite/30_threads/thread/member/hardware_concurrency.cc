d7afcd2b9ba4534fce7ec4d6d31a508af312b928
Benjamin Kosnik <bkoz@redhat.com>
2009-02-07 21:56:55 +0000

    thread (thread::id): Move definition inside thread.
    2009-02-06  Benjamin Kosnik  <bkoz@redhat.com>
    
    	* include/std/thread (thread::id): Move definition inside thread.
    	Use native_handle_type. Remove this_thread::get_id friend.
    	Change __thread_data_ptr to __shared_base_ptr.
    	(thread::id::id(native_handle_type): Make public. Still explicit.
    	Use native_handle_type. Change _M_thread_id to _M_thread.
    	(thread::__thread_data_base): Rename to _Impl_base. Use id, change
    	_M_thread_handle to _M_id.
    	(thread::__thread_data): Rename to _Impl.
    	Fixup for renames.
    	(thread::_M_make_thread_data): Return derived type.
    	(thread::hardware_concurrency): Add definition for default case.
    	(thread::get_id): Now can define inline.
    	(thread): Change _M_thread_data to _M_data.
    	(this_thread::get_id): Now can define inline.
    	* src/thread.cc (__thread_proxy): Rename to
    	execute_native_thread_routine.
    	Fixup for other renames.
    	* testsuite/30_threads/thread/cons/assign_neg.cc: New.
    	* testsuite/30_threads/thread/cons/copy_neg.cc: New.
    	* testsuite/30_threads/thread/algorithm: Move to..
    	* testsuite/30_threads/thread/swap: ...this.
    	* testsuite/30_threads/thread/member/hardware_concurrency.cc: Add.
    	* testsuite/30_threads/thread/id/operators.cc: New.
    
    From-SVN: r144007

---
+++ libstdc++-v3/testsuite/30_threads/thread/member/hardware_concurrency.cc
@@ -0,0 +1,37 @@
+// { dg-do run { target *-*-freebsd* *-*-netbsd* *-*-linux* *-*-solaris* *-*-cygwin *-*-darwin* alpha*-*-osf* mips-sgi-irix6* } }
+// { dg-options " -std=gnu++0x -pthread" { target *-*-freebsd* *-*-netbsd* *-*-linux* alpha*-*-osf* mips-sgi-irix6* } }
+// { dg-options " -std=gnu++0x -pthreads" { target *-*-solaris* } }
+// { dg-options " -std=gnu++0x " { target *-*-cygwin *-*-darwin* } }
+// { dg-require-cstdint "" }
+// { dg-require-gthreads "" }
+
+// Copyright (C) 2009 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#include <thread>
+#include <testsuite_hooks.h>
+
+int main()
+{
+  bool test __attribute__((unused)) = true;
+
+  // Current implementation punts on this.
+  VERIFY( std::thread::hardware_concurrency() == 0 );
+
+  return 0;
+}
