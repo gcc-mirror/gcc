73c5e2aae9a37b711ea6040f36147fbe28798da5
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-02-11 11:43:24 +0000

    PR target/39118 	* config/i386/i386.md (UNSPEC_MEMORY_BLOCKAGE): New constant. 	(memory_blockage): New expander. 	(*memory_blockage): New insn pattern. 	* config/i386/i386.c (ix86_expand_prologue): Use memory_blockage 	instead of general blockage at the end of function prologue when 	frame pointer is used to access red zone area.  Do not emit blockage 	when profiling, it is emitted in generic code. 	(ix86_expand_epilogue): Emit memory_blockage at the beginning of 	function epilogue when frame pointer is used to access red zone area.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@144100 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -7997,7 +7997,6 @@ ix86_expand_prologue (void)
 {
   rtx insn;
   bool pic_reg_used;
-  bool emit_blockage = false;
   struct ix86_frame frame;
   HOST_WIDE_INT allocate;
 
@@ -8209,14 +8208,12 @@ ix86_expand_prologue (void)
         insn = emit_insn (gen_set_got (pic_offset_table_rtx));
     }
 
-  /* Prevent function calls from being scheduled before the call to mcount.
-     In the pic_reg_used case, make sure that the got load isn't deleted.  */
-  if (crtl->profile)
-    {
-      if (pic_reg_used)
-	emit_insn (gen_prologue_use (pic_offset_table_rtx));
-      emit_blockage = true;
-    }
+  /* In the pic_reg_used case, make sure that the got load isn't deleted
+     when mcount needs it.  Blockage to avoid call movement across mcount
+     call is emitted in generic code after the NOTE_INSN_PROLOGUE_END
+     note.  */
+  if (crtl->profile && pic_reg_used)
+    emit_insn (gen_prologue_use (pic_offset_table_rtx));
 
   if (crtl->drap_reg && !crtl->stack_realign_needed)
     {
@@ -8235,10 +8232,7 @@ ix86_expand_prologue (void)
      prologue, and moving instructions that access redzone area via frame
      pointer inside push sequence violates this assumption.  */
   if (frame_pointer_needed && frame.red_zone_size)
-    emit_blockage = true;
-
-  if (emit_blockage)
-    emit_insn (gen_blockage ()); 
+    emit_insn (gen_memory_blockage ());
 
   /* Emit cld instruction if stringops are used in the function.  */
   if (TARGET_CLD && ix86_current_function_needs_cld)
@@ -8325,6 +8319,11 @@ ix86_expand_epilogue (int style)
 	      || stack_realign_fp);
 
   ix86_compute_frame_layout (&frame);
+
+  /* See the comment about red zone and frame
+     pointer usage in ix86_expand_prologue.  */
+  if (frame_pointer_needed && frame.red_zone_size)
+    emit_insn (gen_memory_blockage ()); 
 
   /* Calculate start of saved registers relative to ebp.  Special care
      must be taken for the normal return case of a function using
