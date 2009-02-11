bab64f23e9aa03c3f309c7de92004c544667a5d6
Paolo Bonzini <bonzini@gnu.org>
2009-02-11 08:56:41 +0000

    i386.md: Add two new peephole2 to avoid mov followed by arithmetic with memory operands.
    gcc:
    2009-02-06  Paolo Bonzini  <bonzini@gnu.org>
    
    	* config/i386/i386.md: Add two new peephole2 to avoid mov followed
    	by arithmetic with memory operands.
    	* config/i386/predicates.md (commutative_operator): New.
    
    gcc/testsuite:
    2009-02-06  Paolo Bonzini  <bonzini@gnu.org>
    
    	* gcc.target/i386/pr38824.c: New testcase.
    
    From-SVN: r144098

---
+++ gcc/testsuite/gcc.target/i386/pr38824.c
@@ -0,0 +1,21 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -msse" } */
+
+typedef float v4sf __attribute__ ((__vector_size__ (16)));
+
+void bench_1(float * out, float * in, float f, unsigned int n)
+{
+    n /= 4;
+    v4sf scalar = { f, f, f, f };
+    do
+    {
+        v4sf arg = *(v4sf *)in;
+        v4sf result = arg + scalar;
+        *(v4sf *) out = result;
+        in += 4;
+        out += 4;
+    }
+    while (--n);
+}
+
+/* { dg-final { scan-assembler-not "addps\[^\\n\]*%\[er\]" } } */
