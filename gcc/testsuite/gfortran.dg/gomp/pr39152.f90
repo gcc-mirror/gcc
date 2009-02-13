bc0a240fbdc240374977d7d95e8885bffe0e9712
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-02-13 14:37:11 +0000

    gcc/
    2009-02-13  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/39152
    	* config/i386/i386.md: Restrict the new peephole2 to move
    	between the general purpose registers.
    
    gcc/testsuite/
    
    2009-02-13  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/39152
    	* gfortran.dg/gomp/pr39152.f90: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@144158 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/gomp/pr39152.f90
@@ -0,0 +1,32 @@
+! { dg-do compile { target { { i?86-*-* x86_64-*-* } && ilp32 } } }
+! { dg-options "-march=i486 -fopenmp -mavx -O3 -funroll-all-loops" } 
+
+  call test_workshare
+
+contains
+  subroutine test_workshare
+    integer :: i, j, k, l, m
+    double precision, dimension (64) :: d, e
+    integer, dimension (10) :: f, g
+    integer, dimension (16, 16) :: a, b, c
+    integer, dimension (16) :: n
+!$omp parallel num_threads (4) private (j, k)
+!$omp barrier
+!$omp workshare
+    where (g .lt. 0)
+      f = 100
+    elsewhere
+      where (g .gt. 6) f = f + sum (g)
+      f = 300 + f
+    end where
+!$omp end workshare nowait
+!$omp workshare
+    forall (j = 1:16, k = 1:16) b (k, j) = a (j, k)
+    forall (j = 2:16, n (17 - j) / 4 * 4 .ne. n (17 - j))
+      n (j) = n (j - 1) * n (j)
+    end forall
+!$omp endworkshare
+!$omp end parallel
+
+  end subroutine test_workshare
+end
