9329b3cefeab0a42c017e58ba0c8c5e4f4f3491e
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-02-13 19:14:07 +0000

    PR c++/39070         * semantics.c (finish_call_expr): Change koenig_p parm to int.         If -1, don't set KOENIG_LOOKUP_P but do keep hidden candidates.         * cp-tree.h: Adjust prototype.         * pt.c (tsubst_copy_and_build) [CALL_EXPR]: Pass -1.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@144161 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/decltype16.C
@@ -0,0 +1,10 @@
+// PR c++/39070
+// { dg-options "-std=c++0x" }
+
+template<typename X> struct junk {
+   template<typename Z> static Z y();
+   template<typename Y> static int  test(...);
+   template<typename Y> static char test(decltype(y<Y>())*);
+   static int const value=sizeof(test<X>(0));
+};
+typedef char type[junk<int>::value==sizeof(char) ? 1 : -1];
