8731846f45984a7e2ee2e242e8c1c34dc4dee7bb
janis <janis@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-02-18 22:19:26 +0000

    2009-02-18  Jack Howarth <howarth@bromo.med.uc.edu>
    	PR testsuite/38166
    	* g++.dg/ext/visibility/class1.C: Revert revision 122348
    	and skip on Darwin.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@144274 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ext/visibility/class1.C
+++ gcc/testsuite/g++.dg/ext/visibility/class1.C
@@ -2,9 +2,10 @@
 // Init should not be hidden, so calling it should use the PLT.
 
 // { dg-options "-fpic" }
-// { dg-do compile { target i?86-*-* x86_64-*-* *-*-darwin* } }
+// { dg-do compile { target i?86-*-* x86_64-*-* } }
+// { dg-skip-if "" { *-*-darwin* } { "*" } { "" } }
 // { dg-require-visibility "" }
-// { dg-final { scan-assembler "InitEv@PLT|indirect_symbol.*InitEv" } }
+// { dg-final { scan-assembler "InitEv@PLT" } }
 
 #pragma GCC visibility push(hidden)
 struct __attribute__ ((visibility ("default"))) nsINIParser
