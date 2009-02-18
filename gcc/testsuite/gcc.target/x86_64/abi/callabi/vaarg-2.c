d812e36534bfc4b91638a8eecc427522a558be0b
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-02-18 17:06:33 +0000

    2009-02-18  H.J. Lu  <hongjiu.lu@intel.com>
    	* gcc.dg/callabi/callabi.exp: Moved to ...
    	* gcc.target/x86_64/abi/callabi/callabi.exp: Here.  Only run
    	in 64bit mode.
    
    	* gcc.target/x86_64/abi/callabi/func-1.c: Don't check target.
    	* gcc.target/x86_64/abi/callabi/func-indirect.c: Likewise.
    	* gcc.target/x86_64/abi/callabi/func-indirect.c: Likewise.
    	* gcc.target/x86_64/abi/callabi/pr38891.c: Likewise.
    	* gcc.target/x86_64/abi/callabi/vaarg-1.c: Likewise.
    	* gcc.target/x86_64/abi/callabi/vaarg-2.c: Likewise.
    	* gcc.target/x86_64/abi/callabi/vaarg-3.c: Likewise.
    
    	* gcc.dg/callabi/callabi.h: Moved to ...
    	* gcc.target/x86_64/abi/callabi/callabi.h: Here.
    
    	* gcc.dg/callabi/func-1.c: Moved to ...
    	* gcc.target/x86_64/abi/callabi/func-1.c: Here.
    
    	* gcc.dg/callabi/func-indirect.c: Moved to ...
    	* gcc.target/x86_64/abi/callabi/func-indirect.c: Here.
    
    	* gcc.dg/callabi/pr38891.c: Moved to ...
    	* gcc.target/x86_64/abi/callabi/pr38891.c: Here.
    
    	* gcc.dg/callabi/vaarg-1.c: Moved to ...
    	* gcc.target/x86_64/abi/callabi/vaarg-1.c: Here.
    
    	* gcc.dg/callabi/vaarg-2.c: Moved to ...
    	* gcc.target/x86_64/abi/callabi/vaarg-2.c: Here.
    
    	* gcc.dg/callabi/vaarg-3.c: Moved to ...
    	* gcc.target/x86_64/abi/callabi/vaarg-3.c: Here.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@144264 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/x86_64/abi/callabi/vaarg-2.c
+++ gcc/testsuite/gcc.target/x86_64/abi/callabi/vaarg-2.c
@@ -1,7 +1,7 @@
 /* Test for cross x86_64<->w64 abi va_list calls.
 */
 /* Origin: Kai Tietz <kai.tietz@onevision.com> */
-/* { dg-do run { target { x86_64-*-* } } } */
+/* { dg-do run } */
 /* { dg-options "-std=gnu99" } */
 #include "callabi.h"
 
@@ -10,32 +10,32 @@ extern void abort (void);
 #define SZ_ARGS	1ll,2ll,3ll,4ll,5ll,6ll,7ll,0ll
 
 static
-int fct1 (CROSS_VA_LIST argp, ...)
+int CALLABI_CROSS fct1 (va_list argp, ...)
 {
   long long p1,p2;
   int ret = 1;
-  va_list argp_2;
+  CROSS_VA_LIST argp_2;
+  CROSS_VA_START (argp_2,argp);
 
-    __va_start (argp_2,argp);
   do {
-    p1 = __va_arg (argp_2, long long);
-    p2 = CROSS_VA_ARG (argp, long long);
+    p1 = CROSS_VA_ARG (argp_2, long long);
+    p2 = __va_arg (argp, long long);
     if (p1 != p2)
       ret = 0;
   } while (ret && p1 != 0);
-  __va_end (argp_2);
+  CROSS_VA_END (argp_2);
   return ret;
 }
 
 static
-int CALLABI_CROSS fct2 (int dummy, ...)
+int fct2 (int dummy, ...)
 {
-  CROSS_VA_LIST argp;
+  va_list argp;
   int ret = dummy;
 
-  CROSS_VA_START (argp, dummy);
+  __va_start (argp, dummy);
   ret += fct1 (argp, SZ_ARGS);
-  CROSS_VA_END (argp);
+  __va_end (argp);
   return ret;
 }
 
