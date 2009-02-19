778492dcab72f6296abf139ae793dd222b476a94
Uros Bizjak <ubizjak@gmail.com>
2009-02-19 13:44:40 +0100

    re PR target/39228 (387 optimised __builtin_isinf() gives incorrect result)
    	PR target/39228
    	* config/i386/i386.md (isinfxf2): Split from isinf<mode>2.
    	(UNSPEC_FXAM_MEM): New unspec.
    	(fxam<mode>2_i387_with_temp): New insn and split pattern.
    	(isinf<mode>2): Use MODEF mode iterator.  Force operand[1] through
    	memory using fxam<mode>2_i387_with_temp to remove excess precision.
    
    testsuite/ChangeLog:
    
    	PR target/39228
    	* gcc.c-torture/execute/pr39228.c: New test.
    
    From-SVN: r144295

---
+++ gcc/testsuite/gcc.c-torture/execute/pr39228.c
@@ -0,0 +1,36 @@
+extern void abort (void);
+
+static int __attribute__((always_inline)) testf (float b)
+{
+  float c = 1.01f * b;
+
+  return __builtin_isinff (c);
+}
+
+static int __attribute__((always_inline)) test (double b)
+{
+  double c = 1.01 * b;
+
+  return __builtin_isinf (c);
+}
+
+static int __attribute__((always_inline)) testl (long double b)
+{
+  long double c = 1.01L * b;
+
+  return __builtin_isinfl (c);
+}
+
+int main()
+{
+  if (testf (__FLT_MAX__) < 1)
+    abort ();
+
+  if (test (__DBL_MAX__) < 1)
+    abort ();
+
+  if (testl (__LDBL_MAX__) < 1)
+    abort ();
+
+  return 0;
+}
