2d9d874040c91b62611daff710053146dc4dfd76
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-02-19 21:15:12 +0000

    PR target/39175 	* c-common.c (c_determine_visibility): If visibility changed and 	DECL_RTL has been already set, call make_decl_rtl to update symbol 	flags.
    	* decl2.c (determine_visibility): If visibility changed and
    	DECL_RTL has been already set, call make_decl_rtl to update symbol
    	flags.
    
    	* gcc.dg/visibility-20.c: New test.
    	* g++.dg/ext/visibility/visibility-11.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@144305 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/visibility-20.c
@@ -0,0 +1,18 @@
+/* PR target/39175 */
+/* { dg-do compile } */
+/* { dg-require-visibility "" } */
+/* { dg-options "-O2 -fvisibility=hidden -fpic" { target fpic } } */
+
+__attribute__((noinline)) int
+foo (int x)
+{
+  return x;
+}
+
+int foo (int x);
+
+int
+bar (int x)
+{
+  return foo (x);
+}
