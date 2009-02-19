07dd13870dfa40944eb4478f1b8bfce021556080
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-02-19 01:58:15 +0000

    gcc/cp
    2009-02-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR c++/39219
    	* parser.c (cp_parser_enum_specifier): Apply all attributes.
    
    gcc/testsuite/
    
    2009-02-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR c++/39219
    	* g++.dg/parse/attr3.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@144284 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/attr3.C
@@ -0,0 +1,14 @@
+// PR c++/39219
+
+enum __attribute__ ((deprecated)) E { e };
+struct S { enum __attribute__ ((deprecated)) F { f = e }; };
+
+int main () {
+    E x;	// { dg-warning "'E' is deprecated" "" }
+    x = e;
+
+    S::F y;	// { dg-warning "'F' is deprecated" "" }
+    y = S::f;
+
+    return x + y;
+}
