4c64d3ddec2b86cd3b0ecef3c377affbb87cc370
H.J. Lu <hjl@gcc.gnu.org>
2009-02-21 07:55:53 -0800

    re PR target/39256 (__m256 isn't returned in ymm0 in 32bit)
    gcc/
    
    2008-02-21  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/39256
    	* config/i386/i386.c (type_natural_mode): Remove an extra
    	space in the warning message.
    	(function_value_32): Handle 32-byte vector modes.
    	(return_in_memory_32): Likewise.
    
    gcc/testsuite/
    
    2008-02-21  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/39256
    	* gcc.target/i386/abi-2.c: New.
    
    From-SVN: r144355

---
+++ gcc/testsuite/gcc.target/i386/abi-2.c
@@ -0,0 +1,8 @@
+/* Make certain that we pass __m256i in the correct register for AVX.  */
+/* { dg-do compile } */
+/* { dg-options "-O1 -mavx" } */
+
+typedef long long __m256i __attribute__ ((__vector_size__ (32)));
+__m256i foo (void) { return (__m256i){ 1, 2, 3, 4 }; }
+
+/* { dg-final { scan-assembler-times "ymm0" 1 } } */
