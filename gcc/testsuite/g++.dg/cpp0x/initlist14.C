5f0fc83311a043deb76a57ce6689c4579354ae48
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-02-23 20:28:50 +0000

    * pt.c (unify): Call maybe_adjust_types_for_deduction when         deducing from an initializer list.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@144392 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/initlist14.C
@@ -0,0 +1,19 @@
+// Bug: We weren't doing the normal replacement of array with pointer
+// for deduction in the context of a call because of the initializer list.
+// { dg-options "-std=c++0x" }
+
+#include <initializer_list>
+
+struct string
+{
+  string (const char *);
+};
+
+template <class T>
+struct vector
+{
+  template <class U>
+  vector (std::initializer_list<U>);
+};
+
+vector<string> v = { "a", "b", "c" };
