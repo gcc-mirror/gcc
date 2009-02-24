4fd01733e5748ffe4538a25f46606595d5693611
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-02-24 05:29:04 +0000

    2009-02-23  H.J. Lu  <hongjiu.lu@intel.com>
    	* g++.dg/init/static-init1.C: Replace int with __PTRDIFF_TYPE__.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@144402 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/init/static-init1.C
+++ gcc/testsuite/g++.dg/init/static-init1.C
@@ -2,4 +2,4 @@
 // Make sure we don't think we can initialize a at compile time.
 
 char c;
-short a[] = { (short)((int)&c + (int)&c) };
+short a[] = { (short)((__PTRDIFF_TYPE__)&c + (__PTRDIFF_TYPE__)&c) };
