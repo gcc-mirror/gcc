3d280f42d3e26d22afe91b4816d89e9618b52639
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-03-01 11:05:00 +0000

    * tree-inline.c (tree_function_versioning): Output debug info.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@144516 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/debug/dwarf2/ipa-cp1.c
@@ -0,0 +1,30 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -gdwarf-2 -dA" } */
+void q(int p);
+static void
+t(int constant_propagated_par)
+{
+  int local_var = constant_propagated_par + 1;
+  q(local_var);
+  q(local_var);
+  q(local_var);
+  q(local_var);
+  q(local_var);
+  q(local_var);
+  q(local_var);
+  q(local_var);
+  q(local_var);
+  q(local_var);
+  q(local_var);
+}
+main()
+{
+  t(5);
+  t(5);
+  t(5);
+  t(5);
+  t(5);
+  t(5);
+  t(5);
+}
+/* { dg-final { scan-assembler "local_var" } } */
