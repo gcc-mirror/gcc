1cf5abb382f9dab7203ae6a8d9f2f4421190d1ff
Jan Hubicka <jh@suse.cz>
2009-03-01 12:02:30 +0100

    re PR debug/39267 (gdb testsuite regressions)
    	PR debug/39267
    	* tree-inline.c (setup_one_parameter): Do not copy propagate
    	arguments when not optimizing.
    	PR debug/39267
    	* gcc.dg/debug/dwarf2/inline1.c: New testcase.
    
    From-SVN: r144515

---
+++ gcc/testsuite/gcc.dg/debug/dwarf2/inline1.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-O0 -gdwarf-2 -dA" } */
+void e(int);
+__attribute__ ((always_inline)) inline int
+t(int function_parameter)
+{
+  e(function_parameter);
+}
+
+void test (int function_parameter2)
+{
+  t(function_parameter2);
+}
+
+/* Verify that we get both function_parameter and function_parameter2 declared
+   in test.  Overall we should have 3 variables with location defined (also
+   function_parameter in offline copy of t.  */
+/* { dg-final { scan-assembler-times " DW_AT_location" 3 } } */
