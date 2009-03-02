6dfb529ce2820d176b3f4c637a23544998377e39
Richard Guenther <rguenther@suse.de>
2009-03-02 11:52:15 +0000

    re PR middle-end/39318 (internal compiler error: verify_stmts failed)
    2009-03-02  Richard Guenther  <rguenther@suse.de>
                Ira Rosen  <irar@il.ibm.com>
    
    	PR tree-optimization/39318
    	* tree-vect-transform.c (vectorizable_call): Transfer the EH region
    	information to the vectorized statement.
    
    
    Co-Authored-By: Ira Rosen <irar@il.ibm.com>
    
    From-SVN: r144541

---
+++ gcc/testsuite/gfortran.dg/vect/pr39318.f90
@@ -0,0 +1,21 @@
+! { dg-do compile } 
+! { dg-options "-c -fopenmp -fexceptions -O2 -ftree-vectorize" } 
+
+      subroutine adw_trajsp (F_u,i0,in,j0,jn)
+      implicit none
+      real F_u(*)
+      integer i0,in,j0,jn
+      integer n,i,j
+      real*8 xsin(i0:in,j0:jn)
+!$omp parallel do private(xsin)
+         do j=j0,jn
+         do i=i0,in
+            xsin(i,j) = sqrt(F_u(n))
+         end do
+         end do
+!$omp end parallel do
+      return
+      end
+
+! { dg-final { cleanup-tree-dump "vect" } }
+
