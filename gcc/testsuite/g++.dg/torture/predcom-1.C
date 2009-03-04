630c80e6355a0637cfb2250840e1131a9cd4e283
Nathan Sidwell <nathan@codesourcery.com>
2009-03-04 23:26:51 +0000

    predcom-1.C: New test.
    2009-03-04  Nathan Sidwell  <nathan@codesourcery.com>
    
    	* g++.dg/torture/predcom-1.C: New test.
    
    From-SVN: r144629

---
+++ gcc/testsuite/g++.dg/torture/predcom-1.C
@@ -0,0 +1,29 @@
+/* Test for ICE in predictive commoning with empty loop header block
+   on arm-none-linux-gnueabi.  */
+
+struct Foo
+{
+  double *ptr;
+  
+  Foo (double *ptr_)
+    : ptr (ptr_)
+  {
+  }
+  
+  Foo PostInc ()
+  {
+    return Foo (ptr++);
+  }
+};
+
+bool Baz (Foo first, double *last)
+{
+  Foo prev (first.ptr);
+  
+  first.ptr++;
+
+  while (first.ptr != last)
+    if (*first.PostInc ().ptr < *prev.PostInc ().ptr)
+      return false;
+}
+
