131d0a9e82cd5655dfb1491c8ef2c2031371a65f
irar <irar@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-03-10 17:29:21 +0000

    PR tree-optimization/39422 	* gcc.dg/vect/costmodel/spu/costmodel-vect-76b.c: Move 	constant array with static initialization to global memory. 	* gcc.dg/vect/costmodel/spu/costmodel-vect-76c.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@144754 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/vect/costmodel/spu/costmodel-vect-76b.c
+++ gcc/testsuite/gcc.dg/vect/costmodel/spu/costmodel-vect-76b.c
@@ -11,12 +11,12 @@
    more involved than just an ssa_name.  */
 
 int ib[N+OFF] __attribute__ ((__aligned__(16))) = {0, 1, 3, 5, 7, 11, 13, 17, 0, 2, 6, 10, 0, 1, 3, 5, 7, 11, 13, 17};
+int ic[N+OFF] = {0, 1, 3, 5, 7, 11, 13, 17, 0, 2, 6, 10, 0, 1, 3, 5, 7, 11, 13, 17};
 
 int main1 (int *pib)
 {
   int i;
   int ia[N+OFF];
-  int ic[N+OFF] = {0, 1, 3, 5, 7, 11, 13, 17, 0, 2, 6, 10, 0, 1, 3, 5, 7, 11, 13, 17};
 
   for (i = OFF; i < N; i++)
     {
