1f477a18c46bb6c3901cb019cd1bfdd8242dc2aa
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-03-12 20:45:39 +0000

    PR target/39431 	* config/i386/predicates.md (cmpxchg8b_pic_memory_operand): New 	predicate. 	* config/i386/sync.md (sync_compare_and_swap<mode>, 	sync_compare_and_swap_cc<mode>): For DImode with -m32 -fpic check 	if operands[1] is cmpxchg8b_pic_memory_operand, if not force address 	into a register. 	(sync_double_compare_and_swapdi_pic, 	sync_double_compare_and_swap_ccdi_pic): Require operand 1 to be 	cmpxchg8b_pic_memory_operand instead of just memory_operand.
    	* gcc.target/i386/pr39431.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@144825 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr39431.c
@@ -0,0 +1,15 @@
+/* PR target/39431 */
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+/* { dg-options "-O2 -march=i686 -fpic" { target { ilp32 && fpic } } } */
+
+extern void bar (char *, int);
+
+int
+foo (long long *p, long long oldv, long long *q, int n)
+{
+  char buf[n];
+  bar (buf, n);
+  p[256 + n] = __sync_val_compare_and_swap (p + n, oldv, oldv + 6);
+  return __sync_bool_compare_and_swap (q + n, oldv, oldv + 8);
+}
