1039b93746e15cfb48b3a6e863d3a005331e5d1e
H.J. Lu <hongjiu.lu@intel.com>
2009-03-13 13:25:50 +0000

    backport: re PR testsuite/39451 (Invalid x86 constraint in gcc.dg/asm-b.c)
    2009-03-13  H.J. Lu  <hongjiu.lu@intel.com>
    
    	Backport from mainline:
    	2009-03-13  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR testsuite/39451
    	* gcc.dg/asm-b.c: Use "Q" constraint on %b1/%h1 and "R"
    	constraint on (%2).
    
    From-SVN: r144835

--- gcc/testsuite/gcc.dg/asm-b.c
+++ gcc/testsuite/gcc.dg/asm-b.c
@@ -24,8 +24,8 @@ bar (void)
 #if defined (__powerpc__) || defined (__PPC__) || defined (__ppc__) || defined (_POWER) || defined (__ppc64__)
   __asm __volatile ("sthbrx %1,0,%2" : "=m" (*z) : "r" (y), "r" (z));
 #elif defined __i386__ || defined __x86_64__
-  __asm __volatile ("movb %b1,1(%2); movb %h1,(%2)" : "=m" (*z) : "r" (y), "r"
-(z));
+  __asm __volatile ("movb %b1,1(%2)\n\tmovb %h1,(%2)"
+		    : "=m" (*z) : "Q" (y), "R" (z));
 #endif
   return (x & 1) == 0;
 }
