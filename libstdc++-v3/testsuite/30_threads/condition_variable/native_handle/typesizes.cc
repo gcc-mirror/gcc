8f6678383c00182d95441e71c8ced4d0dbe02f41
Benjamin Kosnik <bkoz@redhat.com>
2009-03-13 05:38:16 +0000

    typesizes.cc: Include tweaks.
    2009-03-12  Benjamin Kosnik  <bkoz@redhat.com>
    
    	* testsuite/30_threads/condition_variable_any/native_handle/
    	typesizes.cc: Include tweaks.
    	* testsuite/30_threads/mutex/native_handle/typesizes.cc: Same.
    	* testsuite/30_threads/timed_mutex/native_handle/typesizes.cc: Same.
    	* testsuite/30_threads/thread/native_handle/typesizes.cc: Same.
    	* testsuite/30_threads/recursive_mutex/native_handle/typesizes.cc: Same.
    	* testsuite/30_threads/condition_variable/native_handle/
    	typesizes.cc: Same.
    
    From-SVN: r144830

--- libstdc++-v3/testsuite/30_threads/condition_variable/native_handle/typesizes.cc
+++ libstdc++-v3/testsuite/30_threads/condition_variable/native_handle/typesizes.cc
@@ -23,8 +23,8 @@
 // Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
 // USA.
 
-#include <thread/all.h>
 #include <condition_variable>
+#include <thread/all.h>
 
 int main()
 {
