7846db3467fdd4154a3fb49c25cd8bf360bf1b78
aesok <aesok@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-03-15 13:09:44 +0000

    PR target/34299 	* config/avr/avr.c (avr_handle_fndecl_attribute): Move code for 	generate a warning if the function name does not begin with 	"__vector" and the function has either the 'signal' or 'interrupt' 	attribute, from here to ... 	(avr_declare_function_name): ...here. New function. 	* config/avr/avr.h (ASM_DECLARE_FUNCTION_NAME): Redefine. 	* config/avr/avr-protos.h (avr_declare_function_name): Declare.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@144870 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/avr/avr-protos.h
+++ gcc/config/avr/avr-protos.h
@@ -1,6 +1,6 @@
 /* Prototypes for exported functions defined in avr.c
    
-   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2006, 2007, 2008
+   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2006, 2007, 2008, 2009
    Free Software Foundation, Inc.
    Contributed by Denis Chertykov (denisc@overta.ru)
 
@@ -32,6 +32,7 @@ extern enum reg_class avr_regno_reg_class (int r);
 extern enum reg_class avr_reg_class_from_letter (int c);
 extern int frame_pointer_required_p (void);
 extern void asm_globalize_label (FILE *file, const char *name);
+extern void avr_asm_declare_function_name (FILE *, const char *, tree);
 extern void order_regs_for_local_alloc (void);
 extern int initial_elimination_offset (int from, int to);
 extern int avr_simple_epilogue (void);
