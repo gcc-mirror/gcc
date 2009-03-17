a3f596ba3de6975109c5d3d5900a1693cfebb96d
Jakub Jelinek <jakub@redhat.com>
2009-03-17 18:49:28 +0100

    re PR debug/39471 (DW_TAG_imported_module should be used (not DW_TAG_imported_declaration))
    	PR debug/39471
    	* dwarf2out.c (dwarf2out_imported_module_or_decl_1): Emit
    	DW_TAG_imported_module even if decl is IMPORTED_DECL with
    	NAMESPACE_DECL in its DECL_INITIAL.
    
    	* cp-gimplify.c (cp_gimplify_expr): Don't set DECL_NAME
    	on IMPORTED_DECL.
    
    	* g++.dg/debug/dwarf2/imported-module-2.C: Expect
    	DW_TAG_imported_module, not just any DW_TAG_imported prefixed tag.
    	* g++.dg/debug/dwarf2/imported-module-3.C: Likewise.
    	* g++.dg/debug/dwarf2/imported-module-4.C: Likewise.
    
    From-SVN: r144911

--- gcc/cp/cp-gimplify.c
+++ gcc/cp/cp-gimplify.c
@@ -593,8 +593,6 @@ cp_gimplify_expr (tree *expr_p, gimple_seq *pre_p, gimple_seq *post_p)
 
 	  IMPORTED_DECL_ASSOCIATED_DECL (using_directive)
 	    = TREE_OPERAND (*expr_p, 0);
-	  DECL_NAME (using_directive)
-	    = DECL_NAME (TREE_OPERAND (*expr_p, 0));
 	  TREE_CHAIN (using_directive) = BLOCK_VARS (block);
 	  BLOCK_VARS (block) = using_directive;
 	}
