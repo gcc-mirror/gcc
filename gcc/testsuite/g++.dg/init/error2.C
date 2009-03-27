a5be0bc246f6244d732a78168ca4a60318010200
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-03-27 13:11:29 +0000

    gcc/cp/
    2009-03-27  Simon Martin  <simartin@users.sourceforge.net>
    
    	PR c++/29727
    	* decl.c (check_array_designated_initializer): Handle error_mark_node.
    
    gcc/testsuite/
    
    2009-03-27  Simon Martin  <simartin@users.sourceforge.net>
    
    	PR c++/29727
    	* g++.dg/init/error2.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@145104 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/init/error2.C
@@ -0,0 +1,15 @@
+/* PR c++/29727 */
+/* { dg-do "compile" } */
+
+template<int> struct A
+{
+  static int a[1];
+};
+template<int N> int A<N>::a[1] = { X:0 }; /* { dg-error "does not allow designated|was not declared|designated initializer for an array" } */
+
+void foo()
+{
+  A<0>::a;
+}
+
+
