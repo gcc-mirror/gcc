38150ede8ad6888fdd345bee3712de01636a1e3e
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-03-27 21:36:53 +0000

    * tree-inline.c: Include gt-tree-inline.h. 	(clone_fn_id_num): New variable. 	(clone_function_name): New function. 	(tree_function_versioning): Use it. 	* Makefile.in (GTFILES): Add tree-inline.c.
    	* gcc.dg/ipa/ipacost-1.c: Adjust match pattern for change
    	in clone naming.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@145126 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/ipa/ipacost-1.c
+++ gcc/testsuite/gcc.dg/ipa/ipacost-1.c
@@ -53,6 +53,7 @@ main()
 /* { dg-final { scan-ipa-dump-times "versioned function i_can_be_propagated_fully " 1 "cp"  } } */
 /* { dg-final { scan-ipa-dump-not "versioned function i_can_not_be_propagated_fully2" "cp"  } } */
 /* { dg-final { scan-ipa-dump-not "versioned function i_can_not_be_propagated_fully " "cp"  } } */
-/* { dg-final { scan-tree-dump-not "i_can_be_propagated" "optimized"  } } */
+/* { dg-final { scan-tree-dump-not "i_can_be_propagated_fully " "optimized"  } } */
+/* { dg-final { scan-tree-dump-not "i_can_be_propagated_fully2 " "optimized"  } } */
 /* { dg-final { cleanup-ipa-dump "cp" } } */
 /* { dg-final { cleanup-tree-dump "optimized" } } */
