dbea18313dfdbd99ed6b78f2b2e943f2abe45c65
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-03-27 22:01:15 +0000

    * include/backward/hashtable.h (_S_num_primes): Change to 29. 	(__stl_prime_list): Add 5 at the start of the list.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@145132 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/backward/hashtable.h
+++ libstdc++-v3/include/backward/hashtable.h
@@ -210,16 +210,16 @@ _GLIBCXX_BEGIN_NAMESPACE(__gnu_cxx)
     };
 
   // Note: assumes long is at least 32 bits.
-  enum { _S_num_primes = 28 };
+  enum { _S_num_primes = 29 };
 
   static const unsigned long __stl_prime_list[_S_num_primes] =
     {
-      53ul,         97ul,         193ul,       389ul,       769ul,
-      1543ul,       3079ul,       6151ul,      12289ul,     24593ul,
-      49157ul,      98317ul,      196613ul,    393241ul,    786433ul,
-      1572869ul,    3145739ul,    6291469ul,   12582917ul,  25165843ul,
-      50331653ul,   100663319ul,  201326611ul, 402653189ul, 805306457ul,
-      1610612741ul, 3221225473ul, 4294967291ul
+      5ul,          53ul,         97ul,         193ul,       389ul,
+      769ul,        1543ul,       3079ul,       6151ul,      12289ul,
+      24593ul,      49157ul,      98317ul,      196613ul,    393241ul,
+      786433ul,     1572869ul,    3145739ul,    6291469ul,   12582917ul,
+      25165843ul,   50331653ul,   100663319ul,  201326611ul, 402653189ul,
+      805306457ul,  1610612741ul, 3221225473ul, 4294967291ul
     };
 
   inline unsigned long
