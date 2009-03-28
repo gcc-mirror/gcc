08cf8f5852573752a41d3db8a15c675ddbb34f92
Richard Guenther <rguenther@suse.de>
2009-03-28 14:15:49 +0000

    Warray-bounds.c: Do not use redundant stores.
    2009-03-28  Richard Guenther  <rguenther@suse.de>
    
    	* gcc.dg/Warray-bounds.c: Do not use redundant stores.
    	* g++.dg/warn/Warray-bounds.C: Likewise.
    
    From-SVN: r145189

--- gcc/testsuite/gcc.dg/Warray-bounds.c
+++ gcc/testsuite/gcc.dg/Warray-bounds.c
@@ -26,10 +26,10 @@ int* f(void) {
     a[ 9] = 0;
     a[10] = 0;             /* { dg-warning "array subscript" } */
     a[11] = 0;             /* { dg-warning "array subscript" } */
-    a[2 * n() - 11] = 0;    /* { dg-warning "array subscript" } */
-    a[2 * n() - 10] = 0;
-    a[2 * n() -  1] = 0;
-    a[2 * n() -  0] = 0;    /* { dg-warning "array subscript" } */
+    a[2 * n() - 11] = 1;    /* { dg-warning "array subscript" } */
+    a[2 * n() - 10] = 1;
+    a[2 * n() -  1] = 1;
+    a[2 * n() -  0] = 1;    /* { dg-warning "array subscript" } */
 
     b[-1] = 0;             /* { dg-warning "array subscript" } */
     b[ 0] = 0;
@@ -37,10 +37,10 @@ int* f(void) {
     b[ 9] = 0;
     b[10] = 0;             /* { dg-warning "array subscript" } */
     b[11] = 0;             /* { dg-warning "array subscript" } */
-    b[2 * n() - 11] = 0;    /* { dg-warning "array subscript" } */
-    b[2 * n() - 10] = 0;
-    b[2 * n() -  1] = 0;
-    b[2 * n() -  0] = 0;    /* { dg-warning "array subscript" } */
+    b[2 * n() - 11] = 1;    /* { dg-warning "array subscript" } */
+    b[2 * n() - 10] = 1;
+    b[2 * n() -  1] = 1;
+    b[2 * n() -  0] = 1;    /* { dg-warning "array subscript" } */
 
     c.c[-1] = 0;           /* { dg-warning "array subscript" } */
     c.c[ 0] = 0;
@@ -48,10 +48,10 @@ int* f(void) {
     c.c[ 9] = 0;
     c.c[10] = 0;           /* { dg-warning "array subscript" } */
     c.c[11] = 0;           /* { dg-warning "array subscript" } */
-    c.c[2 * n() - 11] = 0;  /* { dg-warning "array subscript" } */
-    c.c[2 * n() - 10] = 0;
-    c.c[2 * n() -  1] = 0;
-    c.c[2 * n() -  0] = 0;  /* { dg-warning "array subscript" } */
+    c.c[2 * n() - 11] = 1;  /* { dg-warning "array subscript" } */
+    c.c[2 * n() - 10] = 1;
+    c.c[2 * n() -  1] = 1;
+    c.c[2 * n() -  0] = 1;  /* { dg-warning "array subscript" } */
 
     g(&a[8]);
     g(&a[9]);
