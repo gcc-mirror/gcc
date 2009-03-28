d360ad90ffe01aa874398d1c65048db9fe9234cf
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-03-28 10:05:24 +0000

    2009-03-28  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/38968
    	* tree-vect-analyze.c (vect_compute_data_ref_alignment):
    	Use FLOOR_MOD_EXPR to compute misalignment.
    
    	* gfortran.dg/vect/fast-math-pr38968.f90: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@145171 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/vect/fast-math-pr38968.f90
@@ -0,0 +1,21 @@
+program mymatmul
+  implicit none
+  integer, parameter :: kp = 4
+  integer, parameter :: n = 2000
+  real(kp), dimension(n,n) :: rr, ri
+  complex(kp), dimension(n,n) :: a,b,c
+  real :: t1, t2
+  integer :: i, j, k
+
+  do j = 1, n
+     do k = 1, n
+        do i = 1, n
+           c(i,j) = c(i,j) + a(i,k) * b(k,j)
+        end do
+     end do
+  end do
+
+end program mymatmul
+
+! { dg-final { scan-tree-dump "vectorized 1 loops" "vect" } }
+! { dg-final { cleanup-tree-dump "vect" } }
