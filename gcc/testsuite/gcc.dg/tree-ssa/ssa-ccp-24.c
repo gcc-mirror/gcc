e8114fbacc9e1f4702e754288d39913d4c8f3f35
Richard Guenther <rguenther@suse.de>
2009-03-28 12:52:13 +0000

    re PR tree-optimization/38180 (CCP does not propagate through constant initializers)
    2009-03-28  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/38180
    	* tree-ssa-ccp.c (get_default_value): Simplify.
    	(likely_value): Likewise.
    	(surely_varying_stmt_p): Properly handle VOP case.
    	(ccp_initialize): Likewise.
    	(ccp_fold): Handle propagating through *&.
    	(fold_const_aggregate_ref): Also handle decls.
    
    	* gcc.dg/tree-ssa/ssa-ccp-24.c: New testcase.
    
    From-SVN: r145184

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-ccp-24.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-ccp1" } */
+
+static const int x;
+int foo()
+{
+  const int *p = &x;
+  int y = *p;
+  return y + 1;
+}
+
+static const int x2[3] = { 1, 0, 2 };
+int bar()
+{
+  int i = 1;
+  const int *p = &x2[i];
+  int y = *p;
+  return y + 1;
+}
+
+/* { dg-final { scan-tree-dump-times "return 1;" 2 "ccp1" } } */
+/* { dg-final { cleanup-tree-dump "ccp1" } } */
+
