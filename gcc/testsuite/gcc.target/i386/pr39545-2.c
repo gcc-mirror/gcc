eeae74a1a98590abc9eda18b198ff92f473f2e36
H.J. Lu <hongjiu.lu@intel.com>
2009-03-29 15:45:10 +0000

    re PR target/39545 (Structures with flexible array member passed/returned incorrectly)
    gcc/
    
    2009-03-29  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/39545
    	* config/i386/i386.c (classify_argument): Ignore flexible array
    	member in struct and warn ABI change.
    
    gcc/testsuite/
    
    2009-03-29  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/39545
    	* gcc.c-torture/compile/pr16566-2.c: Add -Wno-psabi for x86-64.
    
    	* gcc.target/i386/pr39545-1.c: New.
    	* gcc.target/i386/pr39545-2.c: Likewise.
    
    	* gcc.target/x86_64/abi/test_passing_structs.c (flex1_struct): New.
    	(flex2_struct): Likewise.
    	(check_struct_passing7): Likewise.
    	(check_struct_passing8): Likewise.
    	(f1s): Likewise.
    	(f2s): Likewise.
    	(main): Call check_struct_passing7 and check_struct_passing8.
    
    From-SVN: r145237

---
+++ gcc/testsuite/gcc.target/i386/pr39545-2.c
@@ -0,0 +1,18 @@
+/* PR target/39545 */
+/* { dg-do compile } */
+/* { dg-require-effective-target lp64 } */
+/* { dg-options "-O2" } */
+
+struct flex
+{
+  int i;
+  int flex [];
+};
+
+struct flex
+foo (int x)
+{ /* { dg-message "note: The ABI of passing struct with a flexible array member has changed in GCC 4.4" } */
+  struct flex s;
+  s.i = x;
+  return s;
+}
