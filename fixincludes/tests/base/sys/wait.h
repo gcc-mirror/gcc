615ce5fd7ac9df44ecc9820f58383569c880fb73
Joseph Myers <joseph@codesourcery.com>
2009-03-29 19:32:48 +0100

    math.h, [...]: Update.
    	* tests/base/math.h, tests/base/pthread.h, tests/base/sys/wait.h:
    	Update.
    
    From-SVN: r145257

--- fixincludes/tests/base/sys/wait.h
+++ fixincludes/tests/base/sys/wait.h
@@ -9,14 +9,6 @@
 
 
 
-#if defined( AIX_SYSWAIT_CHECK )
-/* bos325, */
-struct rusage;
-extern pid_t wait3();
-	/* pid_t wait3(int *, int, struct rusage *); */
-#endif  /* AIX_SYSWAIT_CHECK */
-
-
 #if defined( AIX_SYSWAIT_2_CHECK )
 #define WSTOPSIG(__x)    (int)(WIFSTOPPED(__x) ? (int)((((unsigned int)__x) >> 8) & 0xff) : -1)
 #endif  /* AIX_SYSWAIT_2_CHECK */
