eddd291e260549659b0a04f6ae73093fb4d3d467
H.J. Lu <hongjiu.lu@intel.com>
2009-04-01 15:56:29 +0000

    re PR c++/35240 (ICE with new in template parameter)
    2009-04-01  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR c++/35240
    	* g++.dg/template/pr35240.C: New.
    
    From-SVN: r145408

---
+++ gcc/testsuite/g++.dg/template/pr35240.C
@@ -0,0 +1,12 @@
+// PR c++/35240
+// { dg-do compile }
+
+
+template<int> struct A {};
+
+template<int N> A<sizeof(new int[N][N])> foo();
+
+void bar()
+{
+  foo<1>();	// { dg-message "unimplemented" }
+}
