0930c6941290ddc1a387a7850379d43035f34d22
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-01 13:32:22 +0000

    PR c/37772 	* c-parser.c (c_parser_asm_statement): Skip until close paren and 	return if c_parser_asm_string_literal returned NULL.
    	* gcc.dg/pr37772.c: New test.
    	* g++.dg/ext/asm11.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@145401 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/ext/asm11.C
@@ -0,0 +1,20 @@
+// PR c/37772
+// { dg-do compile }
+// { dg-options "" }
+
+void
+foo ()
+{
+  int i;
+  asm ();		  // { dg-error "expected string-literal before" }
+  asm (1);		  // { dg-error "expected string-literal before" }
+  asm (int);		  // { dg-error "expected string-literal before" }
+  asm (: "=r" (i));	  // { dg-error "expected string-literal before" }
+  asm (1 : "=r" (i));	  // { dg-error "expected string-literal before" }
+  asm (int : "=r" (i));	  // { dg-error "expected string-literal before" }
+  asm (: : "r" (i));	  // { dg-error "expected string-literal before" }
+  asm (1 : : "r" (i));	  // { dg-error "expected string-literal before" }
+  asm (int : : "r" (i));  // { dg-error "expected string-literal before" }
+  asm (: : : "memory");	  // { dg-error "expected string-literal before" }
+  asm (1 : : : "memory"); // { dg-error "expected string-literal before" }
+}
