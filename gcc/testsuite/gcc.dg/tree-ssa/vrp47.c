74f874cfc305e2541a4024e03c95c62d73e5f374
Janis Johnson <janis187@us.ibm.com>
2009-04-02 18:03:00 +0000

    * gcc.dg/tree-ssa-vrp47.c: XFAIL two dump scans for powerpc.
    From-SVN: r145464

--- gcc/testsuite/gcc.dg/tree-ssa/vrp47.c
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp47.c
@@ -30,10 +30,12 @@ int f(int x)
 
 /* Test that x and y are never compared to 0 -- they're always known to be
    0 or 1.  */
-/* { dg-final { scan-tree-dump-times "\[xy\]\[^ \]* !=" 0 "vrp1" } } */
+/* xfail: PR middle-end/38219  */
+/* { dg-final { scan-tree-dump-times "\[xy\]\[^ \]* !=" 0 "vrp1" { xfail powerpc*-*-* }} } */
 
 /* This one needs more copy propagation that only happens in dom1.  */
-/* { dg-final { scan-tree-dump-times "x\[^ \]* & y" 1 "dom1" } } */
+/* xfail: PR middle-end/38219  */
+/* { dg-final { scan-tree-dump-times "x\[^ \]* & y" 1 "dom1" { xfail powerpc*-*-* } } } */
 /* { dg-final { scan-tree-dump-times "x\[^ \]* & y" 1 "vrp1" { xfail *-*-* } } } */
 
 /* These two are fully simplified by VRP.  */
