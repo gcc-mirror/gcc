34f42a49e5809f486e9f019bfaf067952da404cd
Jan Hubicka <jh@suse.cz>
2009-04-02 16:08:01 +0200

    passes.c (init_optimization_passes): Remove two copies of ehcleanup pass.
    	* passes.c (init_optimization_passes): Remove two copies of ehcleanup
    	pass.
    
    From-SVN: r145451

--- gcc/passes.c
+++ gcc/passes.c
@@ -592,7 +592,6 @@ init_optimization_passes (void)
       /* Initial scalar cleanups before alias computation.
 	 They ensure memory accesses are not indirect wherever possible.  */
       NEXT_PASS (pass_strip_predict_hints);
-      NEXT_PASS (pass_cleanup_eh);
       NEXT_PASS (pass_update_address_taken);
       NEXT_PASS (pass_rename_ssa_copies);
       NEXT_PASS (pass_complete_unrolli);
@@ -690,7 +689,6 @@ init_optimization_passes (void)
       NEXT_PASS (pass_phi_only_cprop);
       NEXT_PASS (pass_cd_dce);
       NEXT_PASS (pass_tracer);
-      NEXT_PASS (pass_cleanup_eh);
 
       /* FIXME: If DCE is not run before checking for uninitialized uses,
 	 we may get false warnings (e.g., testsuite/gcc.dg/uninit-5.c).
