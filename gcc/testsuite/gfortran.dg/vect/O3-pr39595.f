2f0fa28e1f7c64c4e0f352dfd97b02de06c6e94b
Ira Rosen <irar@il.ibm.com>
2009-04-02 12:39:28 +0000

    re PR tree-optimization/39595 (ICE in vectorizable_store at tree-vect-transform.c:5361)
    	PR tree-optimization/39595
    	* tree-vect-slp.c (vect_build_slp_tree): Check that the size of 
    	interleaved loads group is not greater than the SLP group size.
    
    From-SVN: r145445

---
+++ gcc/testsuite/gfortran.dg/vect/O3-pr39595.f
@@ -0,0 +1,17 @@
+! { dg-do compile }
+      subroutine foo(a,c,i,m)
+      real a(4,*),b(3,64),c(3,200),d(64)
+      integer*8 i,j,k,l,m
+      do j=1,m,64
+        do k=1,m-j+1
+          d(k)=a(4,j-1+k)
+          do l=1,3
+            b(l,k)=c(l,i)+a(l,j-1+k)
+          end do
+        end do
+        call bar(b,d,i)
+      end do
+      end
+
+! { dg-final { cleanup-tree-dump "vect" } }
+
