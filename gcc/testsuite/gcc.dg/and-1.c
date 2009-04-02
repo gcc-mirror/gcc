5faf9fc92ab35ff8673e460143da2050f07d63ad
Janis Johnson <janis187@us.ibm.com>
2009-04-02 18:00:28 +0000

    and-1.c: Restrict the targets for which the test is run and XFAIL for powerpc and spu.
    	* gcc.dg/and-1.c: Restrict the targets for which the test is run
    	and XFAIL for powerpc and spu.
    
    From-SVN: r145462

--- gcc/testsuite/gcc.dg/and-1.c
+++ gcc/testsuite/gcc.dg/and-1.c
@@ -1,8 +1,9 @@
-/* { dg-do compile } */
+/* { dg-do compile { target powerpc*-*-* spu-*-* } } */
 /* { dg-options "-O2" } */
-/* { dg-final { scan-assembler "and" { target powerpc*-*-* spu-*-* } } } */
+/* { dg-final { scan-assembler "and" } } */
 /* There should be no nand for this testcase (for either PPC or SPU). */
-/* { dg-final { scan-assembler-not "nand" { target powerpc*-*-* spu-*-* } } } */
+/* xfail: PR tree-optimization/33512  */
+/* { dg-final { scan-assembler-not "nand" { xfail *-*-* } } } */
 
 int f(int y)
 {
