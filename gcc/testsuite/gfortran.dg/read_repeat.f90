511395eec6bb850257fcd92839e05042095856bf
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2009-04-04 19:04:53 +0000

    re PR fortran/39528 (repeated entries are not read when using list-directed input)
    2009-04-04  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libfortran/39528
    	* gfortran.dg/read_repeat.f90: New test.
    
    2009-04-04  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libfortran/39528
    	Backport from trunk.
    	* io/list_read.c (list_formatted_read_scalar): Move check for read
    	completion to just after the check for a repeated value.
    
    From-SVN: r145548

---
+++ gcc/testsuite/gfortran.dg/read_repeat.f90
@@ -0,0 +1,24 @@
+! { dg-do run }
+! PR39528 repeated entries not read when using list-directed input.
+! Test case derived from reporters example.
+program rread
+  implicit none
+  integer :: iarr(1:7), ia, ib, i
+
+  iarr = 0
+  
+  write(10,*) " 2*1  3*2 /"
+  write(10,*) " 12"
+  write(10,*) " 13"
+  rewind(10)
+
+  read(10,*) (iarr(i), i=1,7)
+  read(10,*) ia, ib
+
+  if (any(iarr(1:2).ne.1)) call abort
+  if (any(iarr(3:5).ne.2)) call abort
+  if (any(iarr(6:7).ne.0)) call abort
+  if (ia .ne. 12 .or. ib .ne. 13) call abort
+  
+  close(10, status="delete")
+end program rread
--- libgfortran/io/list_read.c
+++ libgfortran/io/list_read.c
@@ -1712,15 +1712,15 @@ list_formatted_read_scalar (st_parameter_dt *dtp, bt type, void *p, int kind,
     }
   else
     {
-      if (dtp->u.p.input_complete)
-	goto cleanup;
-
       if (dtp->u.p.repeat_count > 0)
 	{
 	  if (check_type (dtp, type, kind))
 	    return;
 	  goto set_value;
 	}
+
+      if (dtp->u.p.input_complete)
+	goto cleanup;
 
       if (dtp->u.p.at_eol)
 	finish_separator (dtp);
