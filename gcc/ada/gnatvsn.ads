f08c713323cbf550720ba2e3504c06c42d60017e
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-07 13:37:49 +0000

    * gnatvsn.ads: Bump version number.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@145671 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/gnatvsn.ads
+++ gcc/ada/gnatvsn.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---          Copyright (C) 1992-2008 Free Software Foundation, Inc.          --
+--          Copyright (C) 1992-2009 Free Software Foundation, Inc.          --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -79,7 +79,7 @@ package Gnatvsn is
    --  value should never be decreased in the future, but it would be
    --  OK to increase it if absolutely necessary.
 
-   Library_Version : constant String := "4.4";
+   Library_Version : constant String := "4.5";
    --  Library version. This value must be updated whenever any change to the
    --  compiler affects the library formats in such a way as to obsolete
    --  previously compiled library modules.
@@ -90,7 +90,7 @@ package Gnatvsn is
    Verbose_Library_Version : constant String := "GNAT Lib v" & Library_Version;
    --  Version string stored in e.g. ALI files.
 
-   Current_Year : constant String := "2008";
+   Current_Year : constant String := "2009";
    --  Used in printing copyright messages
 
 end Gnatvsn;
