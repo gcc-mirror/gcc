045ac3671593f6cb1a087a22cc9b8bff2b357198
Daniel Franke <franke.daniel@gmail.com>
2009-04-10 10:12:01 -0400

    re PR fortran/38709 (ICE on zero-sized array in initialization expression)
    gcc/fortran/:
    2009-04-10  Daniel Franke  <franke.daniel@gmail.com>
    
            PR fortran/38709
            * expr.c (find_array_section): Leave early on zero-sized arrays.
    
    
    gcc/testsuite/:
    2009-04-10  Daniel Franke  <franke.daniel@gmail.com>
    
            PR fortran/38709
            * gfortran.dg/zero_sized_6.f90: New.
    
    From-SVN: r145909

---
+++ gcc/testsuite/gfortran.dg/zero_sized_6.f90
@@ -0,0 +1,6 @@
+! { dg-do "compile" }
+! PR38709 - ICE-on-invalid on zero-sized array in init-expr.
+
+  INTEGER, PARAMETER :: a(1) = (/ 1 /)
+  INTEGER, PARAMETER :: i = a(shape(1))   ! { dg-error "Incompatible ranks" }
+END
