f5cd2fee76ba57ade94f30429a14288c440b92cc
dfranke <dfranke@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-11 09:33:11 +0000

    gcc/fortran/: 2009-04-11  Daniel Franke  <frake.daniel@gmail.com>
            * resolve.c (resolve_global_procedure): Enable whole-file checking for
            procedures that are declared later in the file.
    
    
    gcc/testsuite:
    2009-04-11  Daniel Franke  <franke.daniel@gmail.com>
    
            * gfortran.dg/whole_file_5.f90: New.
            * gfortran.dg/whole_file_6.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@145953 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/whole_file_6.f90
@@ -0,0 +1,18 @@
+! { dg-do "compile" }
+! { dg-options "-O3 -fwhole-file -fdump-tree-optimized" }
+!
+! Check that inlining of functions declared AFTER usage works.
+! If yes, then the dump does not contain a call to F().
+!
+
+PROGRAM main
+  INTEGER :: a(3)
+  a = f()
+  print *, a
+END PROGRAM
+
+INTEGER FUNCTION f()
+  f = 42.0
+END FUNCTION
+
+! { dg-final { scan-tree-dump-times "= f\(\)" 0 "optimized" } }
