4d7221309b3d13258a824b27533563cd012a1b6f
Dave Korn <dave.korn.cygwin@gmail.com>
2009-04-11 05:31:17 +0000

    cygwin-stdint.h (INT_LEAST32_TYPE): Update to match changes in Cygwin 1.7 (UINT_LEAST32_TYPE...
    	* config/i386/cygwin-stdint.h (INT_LEAST32_TYPE):  Update to
    	match changes in Cygwin 1.7
    	(UINT_LEAST32_TYPE, INT_FAST16_TYPE, INT_FAST32_TYPE,
    	UINT_FAST16_TYPE, UINT_FAST32_TYPE):  Likewise.
    
    From-SVN: r145949

--- gcc/config/i386/cygwin-stdint.h
+++ gcc/config/i386/cygwin-stdint.h
@@ -19,32 +19,43 @@ along with GCC; see the file COPYING3.  If not see
 
 #define SIG_ATOMIC_TYPE "int"
 
+/* Exact-width integer types */
+
 #define INT8_TYPE "signed char"
 #define INT16_TYPE "short int"
 #define INT32_TYPE "int"
 #define INT64_TYPE "long long int"
+
 #define UINT8_TYPE "unsigned char"
 #define UINT16_TYPE "short unsigned int"
 #define UINT32_TYPE "unsigned int"
 #define UINT64_TYPE "long long unsigned int"
 
+/* Minimum-width integer types */
+
 #define INT_LEAST8_TYPE "signed char"
 #define INT_LEAST16_TYPE "short int"
-#define INT_LEAST32_TYPE "long int"
+#define INT_LEAST32_TYPE "int"
 #define INT_LEAST64_TYPE "long long int"
+
 #define UINT_LEAST8_TYPE "unsigned char"
 #define UINT_LEAST16_TYPE "short unsigned int"
-#define UINT_LEAST32_TYPE "long unsigned int"
+#define UINT_LEAST32_TYPE "unsigned int"
 #define UINT_LEAST64_TYPE "long long unsigned int"
 
+/* Fastest minimum-width integer types */
+
 #define INT_FAST8_TYPE "signed char"
-#define INT_FAST16_TYPE "long int"
-#define INT_FAST32_TYPE "long int"
+#define INT_FAST16_TYPE "int"
+#define INT_FAST32_TYPE "int"
 #define INT_FAST64_TYPE "long long int"
+
 #define UINT_FAST8_TYPE "unsigned char"
-#define UINT_FAST16_TYPE "long unsigned int"
-#define UINT_FAST32_TYPE "long unsigned int"
+#define UINT_FAST16_TYPE "unsigned int"
+#define UINT_FAST32_TYPE "unsigned int"
 #define UINT_FAST64_TYPE "long long unsigned int"
+
+/* Integer types capable of holding object pointers */
 
 #define INTPTR_TYPE "long int"
 #define UINTPTR_TYPE "long unsigned int"
