724518ff7b8d00e2cad390ccfa46ebbbcde93024
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-13 10:37:17 +0000

    2009-04-13  Ozkan Sezer  <sezeroz@gmail.com>
            PR target/39062
            * ssp.c: Also include malloc.h for alloca(). MinGW needs it.
            * configure.ac: Also check for malloc.h.
            * configure: Regenerated.
            * config.h.in: Regenerated.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@146000 138bc75d-0d04-0410-961f-82ee72b054a4

--- libssp/ssp.c
+++ libssp/ssp.c
@@ -36,6 +36,9 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 #ifdef HAVE_ALLOCA_H
 # include <alloca.h>
 #endif
+#ifdef HAVE_MALLOC_H
+# include <malloc.h>
+#endif
 #ifdef HAVE_STRING_H
 # include <string.h>
 #endif
