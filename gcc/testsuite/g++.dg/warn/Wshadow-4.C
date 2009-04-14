f9b20c620aac52350d4de6956878f791df10ba38
Jason Merrill <jason@redhat.com>
2009-04-14 13:04:04 -0400

    re PR c++/39763 (-Wshadow reports shadowed declarations for parameters of unnamed temp objects)
    	PR c++/39763
    	* name-lookup.c (pushdecl_maybe_friend): Avoid all warnings
    	about shadowing by tentative parms.
    
    From-SVN: r146053

--- gcc/testsuite/g++.dg/warn/Wshadow-4.C
+++ gcc/testsuite/g++.dg/warn/Wshadow-4.C
@@ -18,3 +18,15 @@ int foo(int infoo)		// { dg-warning "shadowed declaration" }
   };
   return outfoo;
 }
+
+// PR c++/39763
+int foo2(void)
+{
+    int infoo = 0;		// { dg-warning "shadowed declaration" }
+    int outfoo( INetURLObject( infoo ).GetMainURL()); // { dg-bogus "shadows" }
+    struct A
+    {
+      void f(int infoo) { }	// { dg-warning "shadows a previous local" }
+    };
+    return outfoo;
+}
