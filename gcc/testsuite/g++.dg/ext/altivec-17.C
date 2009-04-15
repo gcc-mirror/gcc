bf8f98953b25cef452694bff49e4bb8d0a5b7007
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-15 09:34:37 +0000

    ./ 	* config/rs6000/rs6000.c (rs6000_init_builtins): Set TYPE_NAME of 	our distinct integral and vector types. testsuite/ 	* g++.dg/ext/altivec-17.C: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@146092 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/ext/altivec-17.C
@@ -0,0 +1,16 @@
+// { dg-do compile { target powerpc*-*-* } }
+// { dg-require-effective-target powerpc_altivec_ok }
+// { dg-options "-maltivec" }
+
+// Make sure that bool vectors have distinct names to int vectors
+
+#define vector__ __attribute__((altivec (vector__)))
+#define bool__ __attribute__((altivec(bool__)))
+
+typedef vector__ unsigned int simd_type;
+typedef vector__ bool__ int bool_simd_type;
+
+void Foo (bool_simd_type const &a)
+{
+  simd_type const &v = a; // { dg-error "'const unsigned int __vector__&' from expression of type 'const __bool int __vector__'" }
+}
