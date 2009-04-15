c5eada552f7e85d9ff2954e5d7aa6b2f9ac34271
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-15 09:04:56 +0000

    * gnat.dg/sync1.ads: Fix error now flagged by gnat. 	* gnat.dg/specs/limited1.ads: Ditto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@146088 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gnat.dg/specs/limited1.ads
+++ gcc/testsuite/gnat.dg/specs/limited1.ads
@@ -5,8 +5,6 @@ package limited1 is
    
    type Buffer is limited interface;
    type Synchronous_Buffer_Type is synchronized interface and Buffer;
-   type Client_Buffer_Type is new Synchronous_Buffer_Type with private;
 
 private
-   type Client_Buffer_Type is new Synchronous_Buffer_Type with null record;
 end limited1;
