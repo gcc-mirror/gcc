8e2c1630a2c8a361b0dbba271b3b3c06e73ae008
Richard Guenther <rguenther@suse.de>
2009-04-16 09:39:20 +0000

    re PR tree-optimization/39764 (ICE in set_lattice_value, at tree-ssa-ccp.c:468 with -ffinite-math-only)
    2009-04-15  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/39764
    	* tree-ssa-ccp.c (get_value): Canonicalize value with
    	canonicalize_float_value.
    
    	* g++.dg/torture/pr39764.C: New testcase.
    
    From-SVN: r146157

--- gcc/tree-ssa-ccp.c
+++ gcc/tree-ssa-ccp.c
@@ -227,6 +227,8 @@ typedef enum
    doing the store).  */
 static prop_value_t *const_val;
 
+static void canonicalize_float_value (prop_value_t *);
+
 /* Dump constant propagation value VAL to file OUTF prefixed by PREFIX.  */
 
 static void
@@ -385,6 +387,8 @@ get_value (tree var)
   val = &const_val[SSA_NAME_VERSION (var)];
   if (val->lattice_val == UNINITIALIZED)
     *val = get_default_value (var);
+
+  canonicalize_float_value (val);
 
   return val;
 }
