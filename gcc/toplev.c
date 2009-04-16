2dfeb300f788760c436633d99e144ab97192d63c
espindola <espindola@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-16 14:31:45 +0000

    2009-04-16  Rafael Avila de Espindola  <espindola@google.com>
    	* common.opt (fhelp): Add Var(help_flag).
    	* gcc-plugin.h (plugin_info): Add help.
    	* plugin.c (plugin_name_args): Add help.
    	(register_plugin_info): Set plugin->help.
    	(print_help_one_plugin): New.
    	(print_plugins_help): New.
    	* plugin.h (print_plugins_help): New.
    	* toplev.c (toplev_main): Call print_plugins_help if needed.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@146195 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/toplev.c
+++ gcc/toplev.c
@@ -2279,6 +2279,9 @@ toplev_main (unsigned int argc, const char **argv)
   if (version_flag)
     print_version (stderr, "");
 
+  if (help_flag)
+    print_plugins_help (stderr, "");
+
   /* Exit early if we can (e.g. -help).  */
   if (!exit_after_options)
     do_compile ();
