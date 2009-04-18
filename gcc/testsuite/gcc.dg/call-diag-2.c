3ce6296587139c291f7f5c31dbdc30e9c2e1829f
Joseph Myers <joseph@codesourcery.com>
2009-04-18 21:02:47 +0100

    re PR c/35210 (gcc incorrectly allows calling function returning "const void")
    	PR c/35210
    	* c-typeck.c (build_function_call): Check for calling a function
    	with qualified void return types.  Call require_complete_type when
    	generating a trap.
    
    testsuite:
    	* gcc.dg/call-diag-2.c: New test.
    
    From-SVN: r146324

---
+++ gcc/testsuite/gcc.dg/call-diag-2.c
@@ -0,0 +1,17 @@
+/* Test diagnostics for calling function returning qualified void or
+   other incomplete type other than void.  PR 35210.  */
+/* { dg-do compile } */
+/* { dg-options "-pedantic-errors" } */
+
+const void f_cv (void);
+struct s f_s (void);
+void f_v (void);
+
+void g1 (void) { f_cv (); } /* { dg-error "qualified void" } */
+void g2 (void) { f_s (); } /* { dg-error "invalid use of undefined type" } */
+void g3 (void) { ((const void (*) (void)) f_v) (); } /* { dg-error "qualified void" } */
+/* { dg-warning "function called through a non-compatible type" "cast" { target *-*-* } 12 } */
+/* { dg-message "will abort" "abort" { target *-*-* } 12 } */
+void g4 (void) { ((struct s (*) (void)) f_v) (), (void) 0; } /* { dg-error "invalid use of undefined type" } */
+/* { dg-warning "function called through a non-compatible type" "cast" { target *-*-* } 15 } */
+/* { dg-message "will abort" "abort" { target *-*-* } 15 } */
