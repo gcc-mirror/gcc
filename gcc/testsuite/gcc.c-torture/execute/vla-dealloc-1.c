20845d35828c2d166550453b14b6e52c5f3b2826
Joseph Myers <joseph@codesourcery.com>
2009-04-19 21:19:54 +0100

    re PR c/19771 (VLA deallocation)
    	PR c/19771
    	* c-semantics.c (pop_stmt_list): Propagate
    	STATEMENT_LIST_HAS_LABEL to parent statement list.
    
    testsuite:
    	* gcc.c-torture/execute/vla-dealloc-1.c: New test.
    
    From-SVN: r146358

---
+++ gcc/testsuite/gcc.c-torture/execute/vla-dealloc-1.c
@@ -0,0 +1,22 @@
+/* VLAs should be deallocated on a jump to before their definition,
+   including a jump to a label in an inner scope.  PR 19771.  */
+
+void *volatile p;
+
+int
+main (void)
+{
+  int n = 0;
+  if (0)
+    {
+    lab:;
+    }
+  int x[n % 1000 + 1];
+  x[0] = 1;
+  x[n % 1000] = 2;
+  p = x;
+  n++;
+  if (n < 1000000)
+    goto lab;
+  return 0;
+}
