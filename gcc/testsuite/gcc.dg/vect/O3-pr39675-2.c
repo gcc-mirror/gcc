1065dd4eac10c9017f2a6b1fd011f3032cc4a595
irar <irar@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-20 11:26:18 +0000

    PR tree-optimization/39675 	* tree-vect-loop.c (vect_transform_loop): Remove currently redundant 	check of the return code of vect_schedule_slp. Check that stmt_vec_info 	still exists for the statement, before checking its vectorization type.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@146399 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/O3-pr39675-2.c
@@ -0,0 +1,32 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target vect_int } */
+
+#define N 128
+
+int out[N*4], out2[N], in[N*4];
+
+void
+foo ()
+{
+  int i, a0, a1, a2, a3;
+
+  for (i = 0; i < N; i++) 
+    { 
+      a0 = in[i*4];
+      a1 = in[i*4 + 1];
+      a2 = in[i*4 + 2];
+      a3 = in[i*4 + 3];
+
+      out[i*4] = a0;
+      out[i*4 + 1] = a1;
+      out[i*4 + 2] = a2;
+      out[i*4 + 3] = a3;
+
+      out2[i] = a0;
+    }
+}
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect"  { target vect_strided_wide } } } */
+/* { dg-final { scan-tree-dump-times "vectorizing stmts using SLP" 1 "vect" { target vect_strided_wide } } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
+  
