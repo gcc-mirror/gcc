37041295f5ded2ee556e2b98f28402b6f35d1459
Andrew Pinski <andrew_pinski@playstation.sony.com>
2009-04-20 21:59:38 +0000

    framework-2.c: Fix up for non existent includes being fatal errors now.
    2009-04-20  Andrew Pinski <andrew_pinski@playstation.sony.com>
    
            * gcc.dg/framework-2.c: Fix up for non existent includes
            being fatal errors now.
    
    From-SVN: r146458

--- gcc/testsuite/gcc.dg/framework-2.c
+++ gcc/testsuite/gcc.dg/framework-2.c
@@ -1,4 +1,6 @@
 /* { dg-do compile { target *-*-darwin* } } */
 /* { dg-options "-F$srcdir/gcc.dg" } */
 
-#include <Foundation/Foundation.h> /* { dg-error "Foundation/Foundation.h: No such file" } */
+#include <Foundation/Foundation.h> /* { dg-error "Foundation/Foundation.h" } */
+/* { dg-message "terminated" "" { target *-*-* } 0 } */
+
