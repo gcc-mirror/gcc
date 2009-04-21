9258414e0e4a23e4916c4f2196345632cb00b6d9
manu <manu@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-21 19:49:23 +0000

    2009-04-21  Manuel Lopez-Ibanez  <manu@gcc.gnu.org>
    	PR c++/35711
    cp/
    	* typeck.c (check_for_casting_away_constness): We diagnose casting
    	away any qualifiers not just constness.
    	(casts_away_constness): Mention that it handles more than just
    	constness.
    testsuite/
    	* g++.dg/warn/pr35711.C: New.
    	* g++.dg/conversion/ptrmem2.C: Update.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@146537 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/warn/pr35711.C
@@ -0,0 +1,8 @@
+// PR 35711
+// { dg-do compile }
+// { dg-options "-Wcast-qual" }
+
+int* foo (volatile int *p)
+{
+  return (int*)p; // { dg-warning "cast from type 'volatile int\\*' to type 'int\\*' casts away qualifiers" }
+}
