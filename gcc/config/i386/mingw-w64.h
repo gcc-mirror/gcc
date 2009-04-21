72f432d4a40b886b354968094faca3220e51854f
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-21 16:41:14 +0000

    2009-04-21  Kai Tietz  <kai.tietz@onevision.com>
            * config.gcc: Add additional configuration for
            i686-w64-mingw* and x86_64-w64-mingw* triplet.
            * config/i386/mingw-w64.h: New mingw-w64 specific header.
            (CPP_SPEC): Redefine for allowing -municode option.
            (STARTFILE_SPEC): Likewise.
            * config/i386/t-mingw-w64: New.
            * config/i386/mingw-w64.opt: New.
            (municode): Add new target option.
            * doc/invoke.texi (municode): Add documentation for new option.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@146529 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/i386/mingw-w64.h
@@ -0,0 +1,34 @@
+/* Operating system specific defines to be used when targeting GCC for
+   hosting on Windows 32/64 via mingw-w64 runtime, using GNU tools and
+   the Windows API Library.
+   Copyright (C) 2009,
+   2009 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+/* Enable -municode feature.  */
+
+#undef CPP_SPEC
+#define CPP_SPEC "%{posix:-D_POSIX_SOURCE} %{mthreads:-D_MT} \
+  %{municode:-DUNICODE}"
+
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "%{shared|mdll:dllcrt2%O%s} \
+  %{!shared:%{!mdll:%{!municode:crt2%O%s}}} \
+  %{!shared:%{!mdll:%{municode:crt2u%O%s}}} \
+  %{pg:gcrt2%O%s} \
+  crtbegin.o%s"
