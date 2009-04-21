73c1bd312ef7dca1bcd3f409d9c84b38d4671a4a
Paolo Carlini <paolo.carlini@oracle.com>
2009-04-21 20:26:46 +0000

    re PR libstdc++/39802 (std::num_get fails to parse negative zero input correctly)
    2009-04-21  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR libstdc++/39802
    	* include/bits/locale_facets.tcc (num_get<>::_M_extract_int
    	(_InIter, _InIter, ios_base&, ios_base::iostate&, _ValueT&)):
    	Always accept negative values, for unsigned types too.
    	* testsuite/22_locale/num_get/get/char/39802.cc: New.
    	* testsuite/22_locale/num_get/get/wchar_t/39802.cc: Likewise.
    
    From-SVN: r146538

---
+++ libstdc++-v3/testsuite/22_locale/num_get/get/wchar_t/39802.cc
@@ -0,0 +1,77 @@
+// Copyright (C) 2009 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 22.2.2.1.1  num_get members
+
+#include <locale>
+#include <sstream>
+#include <limits>
+#include <testsuite_hooks.h>
+
+// libstdc++/39802
+void test01()
+{
+  using namespace std;
+  typedef istreambuf_iterator<wchar_t> iterator_type;
+  
+  bool test __attribute__((unused)) = true;
+
+  wstringstream ss;
+  const num_get<wchar_t>& ng = use_facet<num_get<wchar_t> >(ss.getloc()); 
+  ios_base::iostate err;
+  iterator_type end;
+  const wstring empty;
+
+  unsigned long ul0 = 1;
+  const unsigned long ul1 = numeric_limits<unsigned long>::max();
+
+  ss << L"-0";
+  err = ios_base::goodbit;
+  end = ng.get(ss.rdbuf(), 0, ss, err, ul0);
+  VERIFY( err == ios_base::eofbit );
+  VERIFY( ul0 == 0 );
+
+  ss.clear();
+  ss.str(empty);
+  ss << L"-1";
+  err = ios_base::goodbit;
+  end = ng.get(ss.rdbuf(), 0, ss, err, ul0);
+  VERIFY( err == ios_base::eofbit );
+  VERIFY( ul0 == ul1 );
+
+  ss.clear();
+  ss.str(empty);
+  ss << L'-' << ul1;
+  err = ios_base::goodbit;
+  end = ng.get(ss.rdbuf(), 0, ss, err, ul0);
+  VERIFY( err == ios_base::eofbit );
+  VERIFY( ul0 == 1 );
+
+  ss.clear();
+  ss.str(empty);
+  ss << L'-' << ul1 << L'0';
+  err = ios_base::goodbit;
+  end = ng.get(ss.rdbuf(), 0, ss, err, ul0);
+  VERIFY( err == (ios_base::eofbit | ios_base::failbit) );
+  VERIFY( ul0 == ul1 );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
