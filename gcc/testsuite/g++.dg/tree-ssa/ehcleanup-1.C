dbb1dd1eae7d8122e21bce4fe81a24f22fec475f
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-22 17:39:33 +0000

    g++.dg/tree-ssa/ehcleanup-1.C: New testcase.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@146603 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/tree-ssa/ehcleanup-1.C
@@ -0,0 +1,24 @@
+// { dg-options "-O2 -fdump-tree-ehcleanup1" }
+extern void can_throw ();
+class a
+{
+public:
+  ~a ()
+  {
+    if (0)
+      can_throw ();
+  }
+};
+void
+t (void)
+{
+  class a a;
+  can_throw ();
+}
+// We ought to remove implicit cleanup, since destructor is empty. 
+// { dg-final { scan-tree-dump-times "Empty EH handler" 1 "ehcleanup1" } }
+//
+// And as a result also contained control flow.
+// { dg-final { scan-tree-dump-times "Removing unreachable" 1 "ehcleanup1" } }
+//
+// { dg-final { cleanup-tree-dump "ehcleanup1" } }
