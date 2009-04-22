f5ad5c7fa6f9117bb0122f8e989fab750762d4b6
H.J. Lu <hongjiu.lu@intel.com>
2009-04-22 14:14:08 +0000

    backport: [multiple changes]
    2009-04-22  H.J. Lu  <hongjiu.lu@intel.com>
    
    	Backport from mainline:
    	2009-04-22  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/39845
    	* gcc.c-torture/compile/pr39845.c: New testcase.
    
    	2009-04-21  Richard Guenther  <rguenther@suse.de>
    
    	PR middle-end/39829
    	* gcc.dg/torture/pr39829.c: New testcase.
    
    From-SVN: r146579

---
+++ gcc/testsuite/gcc.dg/torture/pr39829.c
@@ -0,0 +1,27 @@
+/* { dg-do compile } */
+/* { dg-options "-w" } */
+
+void foo (void * DAG_temp117584)
+{
+  char  uA;
+  void* pA;
+  void* pB;
+  void* pC;
+  do {
+      int DAG_temp117585;
+      int DAG_temp117586;
+      void ** __indir_union1 = (void**)DAG_temp117584;
+      DAG_temp117585 = (long)*__indir_union1;
+      DAG_temp117586 = DAG_temp117585;
+      if ( DAG_temp117586 != (int)268435456 )
+	pA = (void*)&uA;
+      pB = (void*)&pA;
+      pC = pB;
+      union __block_indir0_u {  struct {  long val; }  __indir_struct; }
+      * __indir_union = (union __block_indir0_u*)pC;
+      f(__indir_union->__indir_struct.val);
+
+      DAG_temp117584 += 64;
+  } while (1);
+}
+
