30349c7400289ab4b00f07c73a2d630bb28f3b23
Paolo Bonzini <bonzini@gnu.org>
2009-04-24 10:29:18 +0000

    re PR middle-end/39867 (Wrong result of conditional operator exp < 2 ? 2U : (unsigned int) exp)
    2009-04-24  Paolo Bonzini  <bonzini@gnu.org>
    
    	PR middle-end/39867
    	* fold-const.c (fold_cond_expr_with_comparison): When folding
    	> and >= to MAX, make sure the MAX uses the same type as the
    	comparison operands.
    
    testsuite:
    2009-04-24  Paolo Bonzini  <bonzini@gnu.org>
    
    	PR middle-end/39867
    	* gcc.dg/pr39867.c: New.
    
    From-SVN: r146695

---
+++ gcc/testsuite/gcc.dg/pr39867.c
@@ -0,0 +1,12 @@
+/* { dg-do link } */
+/* { dg-options "-O2" } */
+
+int main (void)
+{
+  int exp = -1;
+  /* Wrong folding of the LHS to an unsigned MAX leads to 4294967295 != 2.  */
+  if ((exp < 2 ? 2U : (unsigned int) exp) != 2)
+    link_error ();
+  return 0;
+}
+
