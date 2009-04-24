cc285c23bb3bdcf559391b7a4179bb975f459aa7
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-24 10:33:02 +0000

    Minor reformatting.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@146697 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/s-osinte-darwin.ads
+++ gcc/ada/s-osinte-darwin.ads
@@ -7,7 +7,7 @@
 --                                  S p e c                                 --
 --                                                                          --
 --             Copyright (C) 1991-1994, Florida State University            --
---          Copyright (C) 1995-2008, Free Software Foundation, Inc.         --
+--          Copyright (C) 1995-2009, Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNARL is free software; you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
--- gcc/ada/sem_warn.adb
+++ gcc/ada/sem_warn.adb
@@ -212,10 +212,16 @@ package body Sem_Warn is
       end if;
 
       --  Check multiple code statements in a row
-      --  Note : the following code is now unreachable, because Asm statements
+
+      --  Note: the following code is now unreachable, because Asm statements
       --  are procedure calls whose actuals are concatenations, and as a result
       --  of a recent stack usage optimization each such call has its own
       --  block.
+
+      --  Are they always concatenations??? if so why not remove this code???
+
+      --  And indeed if we are really losing this warning, that's really bad
+      --  and we need to put it back ???
 
       if Is_List_Member (N)
         and then Present (Prev (N))
