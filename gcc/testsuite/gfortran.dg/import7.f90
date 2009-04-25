a15d1cafc1d164f1995f143feb46ea2ee4e85017
janus <janus@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-25 08:11:48 +0000

    2009-04-25  Janus Weil  <janus@gcc.gnu.org>
    	PR fortran/39688
    	* decl.c (gfc_match_import): Use 'sym->name' instead of 'name'.
    	They differ if the symbol has been use-renamed.
    
    
    2009-04-25  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/39688
    	* gfortran.dg/import7.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@146762 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/import7.f90
@@ -0,0 +1,58 @@
+! { dg-do compile }
+!
+! PR39688: IMPORT of derived type fails
+!
+! Contributed by Bob Corbett <robert.corbett@sun.com>
+
+       MODULE MOD
+         TYPE T1
+           SEQUENCE
+           TYPE(T2), POINTER :: P
+         END TYPE
+         TYPE T2
+           SEQUENCE
+           INTEGER I
+         END TYPE
+       END
+
+       PROGRAM MAIN
+         USE MOD, T3 => T1, T4 => T2
+         TYPE T1
+           SEQUENCE
+           TYPE(T2), POINTER :: P
+         END TYPE
+         INTERFACE SUBR
+           SUBROUTINE SUBR1(X)
+             IMPORT T3
+             TYPE(T3) X
+           END SUBROUTINE
+           SUBROUTINE SUBR2(X)
+             IMPORT T1
+             TYPE(T1) X
+           END SUBROUTINE
+         END INTERFACE
+         TYPE T2
+           SEQUENCE
+           REAL X
+         END TYPE
+       END
+
+       SUBROUTINE SUBR1(X)
+         USE MOD
+         TYPE(T1) X
+       END
+
+       SUBROUTINE SUBR2(X)
+         TYPE T1
+           SEQUENCE
+           TYPE(T2), POINTER :: P
+         END TYPE
+         TYPE T2
+           SEQUENCE
+           REAL X
+         END TYPE
+         TYPE(T1) X
+       END
+
+! { dg-final { cleanup-modules "mod" } }
+
