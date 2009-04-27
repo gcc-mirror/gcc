dd430c7818adb558fc350ed2026a30a4bd5c6c18
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-27 14:02:09 +0000

    gcc/
    2009-04-27  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/39903
    	* config/i386/i386.c (construct_container): Don't call
    	gen_reg_or_parallel with BLKmode on X86_64_SSE_CLASS,
    	X86_64_SSESF_CLASS and X86_64_SSEDF_CLASS.
    
    gcc/testsuite/
    
    2009-04-27  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/39903
    	* gcc.dg/torture/pr39903-1.c: New.
    	* gcc.dg/torture/pr39903-2.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@146842 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/pr39903-1.c
@@ -0,0 +1,24 @@
+/* PR target/39903 */
+/* { dg-do run } */
+/* { dg-options "-Wno-psabi" } */
+
+struct X {
+  double d;
+  double b[];
+};
+
+struct X __attribute__((noinline))
+foo (double d)
+{
+  struct X x;
+  x.d = d;
+  return x;
+}
+extern void abort (void);
+int main()
+{
+  struct X x = foo(3.0);
+  if (x.d != 3.0)
+    abort ();
+  return 0;
+}
