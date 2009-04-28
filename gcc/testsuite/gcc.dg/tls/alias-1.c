ea968683718d5d25e7419d0007a841f97f9134ee
jingyu <jingyu@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-04-28 20:39:11 +0000

    PR testsuite/39790 	* lib/target-supports.exp (check_effective_target_tls): Remove 	comment of caching. 	(check_effective_target_tls_native): Likewise. 	(check_effective_target_tls_runtime): Likewise. 	* gcc.dg/tls/alias-1.c (dg-require-effective-target): Change target 	tls to tls_runtime. 	* gcc.dg/tls/opt-2.c: Add dg-require-effective-target tls_runtime.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@146913 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tls/alias-1.c
+++ gcc/testsuite/gcc.dg/tls/alias-1.c
@@ -1,7 +1,7 @@
 /* { dg-do link } */
 /* { dg-require-alias "" } */
 /* { dg-require-visibility "" } */
-/* { dg-require-effective-target tls } */
+/* { dg-require-effective-target tls_runtime } */
 /* Test that encode_section_info handles the change from externally
    defined to locally defined (via hidden).   Extracted from glibc.  */
 
