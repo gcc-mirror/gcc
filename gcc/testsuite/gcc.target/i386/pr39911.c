b91322f2ba88548f5137cea907c85cd74b0b90a8
Uros Bizjak <uros@gcc.gnu.org>
2009-04-28 08:54:02 +0200

    re PR target/39911 (The 'z' suffix doesn't work with 16bit integer insn)
    	PR target/39911
    	* config/i386/i386.c (print_operand) ['Z']: Handle floating point
    	and integer modes for x87 operands.  Do not ICE for unsupported size,
    	generate error instead.  Generate error for unsupported operand types.
    	['z']: Do not handle HImode memory operands specially.  Warning
    	for floating-point operands.  Fallthru to 'Z' for unsupported operand
    	types.  Do not ICE for unsupported size, generate error instead.
    	(output_387_binary_op): Use %Z to output operands.
    	(output_fp_compare): Ditto.
    	(output_387_reg_move): Ditto.
    
    testsuite/ChangeLog:
    
    	PR target/39911
    	* gcc.target/i386/pr39911.c: New test.
    
    From-SVN: r146874

---
+++ gcc/testsuite/gcc.target/i386/pr39911.c
@@ -0,0 +1,57 @@
+/* { dg-do assemble } */
+/* { dg-options "-O2" } */
+
+void 
+bar1 () 
+{
+  char foo;
+  asm volatile ("mov%z0 %1, %0": "=m" (foo): "iq" (-23));
+  asm volatile ("add%z0 %1, %0": "+m" (foo): "iq" (23));
+  asm volatile ("mov%z0 %1, %0": "=q" (foo): "iq" (-23));
+  asm volatile ("add%z0 %1, %0": "+q" (foo): "iq" (23));
+}
+
+void
+bar2 () 
+{
+  short foo;
+  asm volatile ("mov%z0 %1, %0": "=m" (foo): "ir" (-23));
+  asm volatile ("add%z0 %1, %0": "+m" (foo): "ir" (23));
+  asm volatile ("mov%z0 %1, %0": "=r" (foo): "ir" (-23));
+  asm volatile ("add%z0 %1, %0": "+r" (foo): "ir" (23));
+
+  asm volatile ("pop%z0 %0": "=m" (foo));
+  asm volatile ("pop%z0 %0": "=r" (foo));
+}
+
+void
+bar3 () 
+{
+  int foo;
+  asm volatile ("mov%z0 %1, %0": "=m" (foo): "ir" (-23));
+  asm volatile ("add%z0 %1, %0": "+m" (foo): "ir" (23));
+  asm volatile ("mov%z0 %1, %0": "=r" (foo): "ir" (-23));
+  asm volatile ("add%z0 %1, %0": "+r" (foo): "ir" (23));
+
+  if (sizeof (void *) == sizeof (int))
+    {
+      asm volatile ("pop%z0 %0": "=m" (foo));
+      asm volatile ("pop%z0 %0": "=r" (foo));
+    }
+}
+
+void
+bar4 () 
+{
+  if (sizeof (void *) == sizeof (long long))
+    {
+      long long foo;
+      asm volatile ("mov%z0 %1, %0": "=m" (foo): "er" (-23));
+      asm volatile ("add%z0 %1, %0": "+m" (foo): "er" (23));
+      asm volatile ("mov%z0 %1, %0": "=r" (foo): "er" (-23));
+      asm volatile ("add%z0 %1, %0": "+r" (foo): "er" (23));
+
+      asm volatile ("pop%z0 %0": "=m" (foo));
+      asm volatile ("pop%z0 %0": "=r" (foo));
+    }
+}
