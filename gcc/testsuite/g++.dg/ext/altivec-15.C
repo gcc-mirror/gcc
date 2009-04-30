5ca83513a9d1aa8700730996abf0f55321353967
Janis Johnson <janis187@us.ibm.com>
2009-04-30 17:53:23 +0000

    re PR testsuite/39776 (FAIL: g++.dg/ext/altivec-15.C)
    	PR testsuite/39776
    	* g++.dg/ext/altivec-15.C: Remove dg-error for messages that are
    	no longer issued.
    
    From-SVN: r147011

--- gcc/testsuite/g++.dg/ext/altivec-15.C
+++ gcc/testsuite/g++.dg/ext/altivec-15.C
@@ -2,12 +2,15 @@
 /* { dg-require-effective-target powerpc_altivec_ok } */
 /* { dg-options "-maltivec" } */
 
+/* This test was added for an internal compiler error.  The number and
+   content of error messages is irrelevant.  */
+
 struct SubData
 {
     inline const Float Clamp(Float f, Float f0, Float f1) // { dg-error "" }
     }
     inline const void SinCos(Float angle, Float& sine, Float& cosine) // { dg-error "" }
     {
-        C0 = __builtin_vec_splat(_simdCosEstCoefficients, 0); // { dg-error "" }
-        C1 = __builtin_vec_splat(_simdCosEstCoefficients, 1); // { dg-error "" }
+        C0 = __builtin_vec_splat(_simdCosEstCoefficients, 0);
+        C1 = __builtin_vec_splat(_simdCosEstCoefficients, 1);
 
