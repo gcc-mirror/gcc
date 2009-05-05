a4d954b3eceaea177e0fee4831480d49efd9b471
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-05-05 06:37:05 +0000

    PR c++/40013 	* pt.c (tsubst): If magic NOP_EXPR with side-effects has no type, 	set it from its operand's type after tsubst_expr.
    	* g++.dg/ext/vla7.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@147119 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/ext/vla7.C
@@ -0,0 +1,30 @@
+// PR c++/40013
+// { dg-options "" }
+
+template <class T>
+struct A
+{
+  struct B
+  {
+    struct
+    {
+      int fn () { return 0; }
+    } b;
+  };
+  void test ();
+};
+
+template <class T>
+void
+A <T>::test ()
+{
+  B a;
+  int vla[a.b.fn ()];
+}
+
+int
+main ()
+{
+  A <char> a;
+  a.test ();
+}
