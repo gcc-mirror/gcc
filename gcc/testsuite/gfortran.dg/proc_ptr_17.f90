9664884ec9117af7a78ce3984ca5c8b87e31318f
janus <janus@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-05-05 20:41:00 +0000

    2009-05-05  Janus Weil  <janus@gcc.gnu.org>
    	PR fortran/39998
    	* expr.c (gfc_check_pointer_assign): Check for statement functions and
    	internal procedures in procedure pointer assignments.
    
    
    2009-05-05  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/39998
    	* gfortran.dg/proc_ptr_17.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@147133 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/proc_ptr_17.f90
@@ -0,0 +1,16 @@
+! { dg-do compile }
+! { dg-options "-std=f2003" }
+!
+! PR39998: Procedure Pointer Assignments: Statement Functions & Internal Functions.
+!
+! Contributed by Tobias Burnus <burnus@net-b.de>
+
+  procedure(), pointer :: p
+  f(x) = x**2
+  p => f  ! { dg-error "invalid in procedure pointer assignment" }
+  p => sub  ! { dg-error "invalid in procedure pointer assignment" }
+contains
+  subroutine sub
+  end subroutine sub
+end
+
