72211fd122a8d04d36b9ca79a8c8ea69b12dd9af
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-05-05 21:09:16 +0000

    PR middle-end/39666 	* gimplify.c (gimplify_switch_expr): If case labels cover the whole 	range of the type, but default label is missing, add it with one 	of the existing labels instead of adding a new label for it.
    	* gcc.dg/pr39666-1.c: New test.
    	* gcc.dg/pr39666-2.c: Likewise.
    	* g++.dg/warn/Wuninitialized-4.C: Likewise.
    	* g++.dg/warn/Wuninitialized-5.C: Likewise.
    	* gfortran.dg/pr39666-1.f90: Likewise.
    	* gfortran.dg/pr39666-2.f90: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@147136 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr39666-2.f90
@@ -0,0 +1,14 @@
+! PR middle-end/39666
+! { dg-do compile }
+! { dg-options "-O2 -Wuninitialized" }
+
+FUNCTION f(n)	! { dg-warning "may be used uninitialized" }
+  INTEGER, INTENT(in) :: n
+  REAL                :: f
+
+  SELECT CASE (n)
+    CASE (:-1); f = -1.0
+    CASE (0);   f =  0.0
+    CASE (2:);  f =  1.0
+  END SELECT
+END FUNCTION
