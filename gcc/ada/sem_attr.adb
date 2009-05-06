b92625409b2db508eba00753a4f257c1fcad7a73
Arnaud Charlet <charlet@gcc.gnu.org>
2009-05-06 12:16:45 +0200

    Minor reformatting.
    From-SVN: r147168

--- gcc/ada/sem_attr.adb
+++ gcc/ada/sem_attr.adb
@@ -1067,6 +1067,7 @@ package body Sem_Attr is
             --  If there is an implicit dereference, then we must freeze
             --  the designated type of the access type, since the type of
             --  the referenced array is this type (see AI95-00106).
+
             --  As done elsewhere, freezing must not happen when pre-analyzing
             --  a pre- or postcondition or a default value for an object or
             --  for a formal parameter.
