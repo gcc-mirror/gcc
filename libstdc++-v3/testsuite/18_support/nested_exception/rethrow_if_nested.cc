ec2060947a2fb5f43432e0a21eaf9e54005e60f4
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-05-12 21:08:00 +0000

    2009-05-12  Jonathan Wakely  <jwakely.gcc@gmail.com>
    	* libsupc++/exception: Include nested_exception.h in C++0x mode.
    	* libsupc++/nested_exception.h: New.
    	* libsupc++/Makefile.am: Add new header.
    	* libsupc++/Makefile.in: Regenerate.
    	* testsuite/18_support/nested_exception/rethrow_nested.cc: New.
    	* testsuite/18_support/nested_exception/throw_with_nested.cc: New.
    	* testsuite/18_support/nested_exception/cons.cc: New.
    	* testsuite/18_support/nested_exception/nested_ptr.cc: New.
    	* testsuite/18_support/nested_exception/rethrow_if_nested.cc: New.
    	* doc/xml/manual/status_cxx200x.xml: Adjust.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@147446 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/18_support/nested_exception/rethrow_if_nested.cc
@@ -0,0 +1,110 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-require-atomic-builtins "" }
+
+// Copyright (C) 2009 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <exception>
+#include <testsuite_hooks.h>
+
+struct derived : std::nested_exception { };
+
+struct base { virtual ~base() = default; };
+
+struct derived2 : base, std::nested_exception { };
+
+void test01() 
+{
+  bool test __attribute__((unused)) = false;
+
+  try
+  {
+    throw 42;
+  }
+  catch (...)
+  {
+    derived d;
+    try
+    {
+      std::rethrow_if_nested(d);
+    }
+    catch (const int& i)
+    {
+      test = true;
+      VERIFY( i == 42 );
+    }
+  }
+
+  VERIFY( test );
+}
+
+void test02() 
+{
+  bool test __attribute__((unused)) = false;
+
+  try
+  {
+    throw base();
+  }
+  catch (const base& b)
+  {
+    std::rethrow_if_nested(b);
+    test = true;
+  }
+
+  VERIFY( test );
+}
+
+void test03() 
+{
+  bool test __attribute__((unused)) = false;
+
+  try
+  {
+    throw 42;
+  }
+  catch (...)
+  {
+    try
+    {
+      throw derived2();
+    }
+    catch (const base& b)
+    {
+      try
+      {
+        std::rethrow_if_nested(b);
+      }
+      catch (const int& i)
+      {
+        VERIFY( i == 42 );
+        test = true;
+      }
+    }
+  }
+
+  VERIFY( test );
+}
+
+
+int main()
+{
+  test01();
+  test02();
+  test03();
+  return 0;
+}
