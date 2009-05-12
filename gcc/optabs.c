5e8d18263db963468b8ac2c15e352c91b7c9a3e6
Paolo Bonzini <bonzini@gnu.org>
2009-05-12 09:52:27 +0000

    revert: optabs.c (prepare_cmp_insn): Temporarily disable test that causes spurious differences between trunk and...
    2009-05-12  Paolo Bonzini  <bonzini@gnu.org>
    
    	Revert:
    
    	2009-05-12  Paolo Bonzini  <bonzini@gnu.org>
    
    	* optabs.c (prepare_cmp_insn): Temporarily disable test that
    	causes spurious differences between trunk and cond-optab branch.
    
    From-SVN: r147426

--- gcc/optabs.c
+++ gcc/optabs.c
@@ -4015,6 +4015,17 @@ prepare_cmp_insn (rtx x, rtx y, enum rtx_code comparison, rtx size,
   gcc_assert (methods == OPTAB_DIRECT || methods == OPTAB_WIDEN
 	      || methods == OPTAB_LIB_WIDEN);
 
+  /* If we are optimizing, force expensive constants into a register.  */
+  if (CONSTANT_P (x) && optimize
+      && (rtx_cost (x, COMPARE, optimize_insn_for_speed_p ())
+          > COSTS_N_INSNS (1)))
+    x = force_reg (mode, x);
+
+  if (CONSTANT_P (y) && optimize
+      && (rtx_cost (y, COMPARE, optimize_insn_for_speed_p ())
+          > COSTS_N_INSNS (1)))
+    y = force_reg (mode, y);
+
 #ifdef HAVE_cc0
   /* Make sure if we have a canonical comparison.  The RTL
      documentation states that canonical comparisons are required only
