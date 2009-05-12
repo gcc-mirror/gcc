2be517627af141485812051da28eb6bf510f87b4
Tobias Burnus <burnus@net-b.de>
2009-05-13 00:42:45 +0200

    re PR fortran/40110 (Bind(C): gfortran rejects the questionable but formally correct INTEGER(C_CHAR))
    2009-05-12  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/40110
            * decl.c (gfc_match_kind_spec): Turn C kind error into a
            * warning.
    
    2009-05-12  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/40110
            * gfortran.dg/bind_c_usage_18.f90: Change dg-error into
            dg-warning.
            * gfortran.dg/c_kind_tests_2.f03: Ditto.
            * gfortran.dg/interop_params.f03: Ditto.
    
    From-SVN: r147452

--- gcc/testsuite/gfortran.dg/bind_c_usage_18.f90
+++ gcc/testsuite/gfortran.dg/bind_c_usage_18.f90
@@ -7,7 +7,7 @@ subroutine foo(x,y,z,a) bind(c) ! { dg-warning "but may not be C interoperable" 
   use iso_c_binding
   implicit none
   integer(4) :: x
-  integer(c_float) :: y ! { dg-error "C kind type parameter is for type REAL" }
+  integer(c_float) :: y ! { dg-warning "C kind type parameter is for type REAL" }
   complex(c_float) :: z ! OK, c_float == c_float_complex
   real(c_float_complex) :: a ! OK, c_float == c_float_complex
 end subroutine foo
@@ -16,8 +16,8 @@ use iso_c_binding
 implicit none
 integer, parameter :: it = c_int
 integer, parameter :: dt = c_double
-complex(c_int), target    :: z1  ! { dg-error "C kind type parameter is for type INTEGER" }
-complex(it), target       :: z2  ! { dg-error "C kind type parameter is for type INTEGER" }
+complex(c_int), target    :: z1  ! { dg-warning "C kind type parameter is for type INTEGER" }
+complex(it), target       :: z2  ! { dg-warning "C kind type parameter is for type INTEGER" }
 complex(c_double), target :: z3  ! OK
 complex(dt), target       :: z4  ! OK
 type(c_ptr) :: ptr
--- gcc/testsuite/gfortran.dg/c_kind_tests_2.f03
+++ gcc/testsuite/gfortran.dg/c_kind_tests_2.f03
@@ -4,11 +4,11 @@ module c_kind_tests_2
 
   integer, parameter :: myF = c_float
   real(myF), bind(c) :: myCFloat
-  integer(myF), bind(c) :: myCInt       ! { dg-error "is for type REAL" }
-  integer(c_double), bind(c) :: myCInt2 ! { dg-error "is for type REAL" }
+  integer(myF), bind(c) :: myCInt       ! { dg-warning "is for type REAL" }
+  integer(c_double), bind(c) :: myCInt2 ! { dg-warning "is for type REAL" }
 
   integer, parameter :: myI = c_int
-  real(myI) :: myReal             ! { dg-error "is for type INTEGER" }
-  real(myI), bind(c) :: myCFloat2 ! { dg-error "is for type INTEGER" }
+  real(myI) :: myReal             ! { dg-warning "is for type INTEGER" }
+  real(myI), bind(c) :: myCFloat2 ! { dg-warning "is for type INTEGER" }
   real(4), bind(c) :: myFloat     ! { dg-warning "may not be a C interoperable" }
 end module c_kind_tests_2
