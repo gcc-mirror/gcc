4ed7f970f4b999074e4971b5f84286b3c0fd3464
Dave Korn <dave.korn.cygwin@gmail.com>
2009-05-14 00:59:33 +0000

    msformat-c.c (ms_printf_length_specs): Use enumeration values even in sentinel and empty entries.
    	* config/i386/msformat-c.c (ms_printf_length_specs):  Use enumeration
    	values even in sentinel and empty entries.
    	(ms_printf_flag_specs):  Likewise.
    	(ms_scanf_flag_specs):  Likewise.
    	(ms_strftime_flag_specs):  Likewise.
    	(ms_print_char_table):  Likewise.
    	(ms_scan_char_table):  Likewise.
    	(ms_time_char_table):  Likewise.
    
    From-SVN: r147513

--- gcc/config/i386/msformat-c.c
+++ gcc/config/i386/msformat-c.c
@@ -36,12 +36,12 @@ along with GCC; see the file COPYING3.  If not see
 
 static format_length_info ms_printf_length_specs[] =
 {
-  { "h", FMT_LEN_h, STD_C89, NULL, 0, 0 },
-  { "l", FMT_LEN_l, STD_C89, NULL, 0, 0 },
-  { "I32", FMT_LEN_l, STD_EXT, NULL, 0, 0 },
-  { "I64", FMT_LEN_ll, STD_EXT, NULL, 0, 0 },
-  { "I", FMT_LEN_L, STD_EXT, NULL, 0, 0 },
-  { NULL, 0, 0, NULL, 0, 0 }
+  { "h", FMT_LEN_h, STD_C89, NULL, FMT_LEN_none, STD_C89 },
+  { "l", FMT_LEN_l, STD_C89, NULL, FMT_LEN_none, STD_C89 },
+  { "I32", FMT_LEN_l, STD_EXT, NULL, FMT_LEN_none, STD_C89 },
+  { "I64", FMT_LEN_ll, STD_EXT, NULL, FMT_LEN_none, STD_C89 },
+  { "I", FMT_LEN_L, STD_EXT, NULL, FMT_LEN_none, STD_C89 },
+  { NULL, FMT_LEN_none, STD_C89, NULL, FMT_LEN_none, STD_C89 }
 };
 
 static const format_flag_spec ms_printf_flag_specs[] =
@@ -55,7 +55,7 @@ static const format_flag_spec ms_printf_flag_specs[] =
   { 'w',  0, 0, N_("field width"),     N_("field width in printf format"),     STD_C89 },
   { 'p',  0, 0, N_("precision"),       N_("precision in printf format"),       STD_C89 },
   { 'L',  0, 0, N_("length modifier"), N_("length modifier in printf format"), STD_C89 },
-  { 0, 0, 0, NULL, NULL, 0 }
+  { 0, 0, 0, NULL, NULL, STD_C89 }
 };
 
 static const format_flag_pair ms_printf_flag_pairs[] =
@@ -72,7 +72,7 @@ static const format_flag_spec ms_scanf_flag_specs[] =
   { 'w',  0, 0, N_("field width"),            N_("field width in scanf format"),              STD_C89 },
   { 'L',  0, 0, N_("length modifier"),        N_("length modifier in scanf format"),          STD_C89 },
   { '\'', 0, 0, N_("''' flag"),               N_("the ''' scanf flag"),                       STD_EXT },
-  { 0, 0, 0, NULL, NULL, 0 }
+  { 0, 0, 0, NULL, NULL, STD_C89 }
 };
 
 static const format_flag_pair ms_scanf_flag_pairs[] =
@@ -84,7 +84,7 @@ static const format_flag_pair ms_scanf_flag_pairs[] =
 static const format_flag_spec ms_strftime_flag_specs[] =
 {
   { '#', 0,   0, N_("'#' flag"),     N_("the '#' strftime flag"),          STD_EXT },
-  { 0, 0, 0, NULL, NULL, 0 }
+  { 0, 0, 0, NULL, NULL, STD_C89 }
 };
 
 static const format_flag_pair ms_strftime_flag_pairs[] =
@@ -107,7 +107,7 @@ static const format_char_info ms_print_char_table[] =
   /* X/Open conversion specifiers.  */
   { "C",   0, STD_EXT, { TEX_WI,  BADLEN,  T89_S,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN }, "-w",        "",   NULL },
   { "S",   1, STD_EXT, { TEX_W,   BADLEN,  T89_S,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN }, "-wp",       "R",  NULL },
-  { NULL,  0, 0, NOLENGTHS, NULL, NULL, NULL }
+  { NULL,  0, STD_C89, NOLENGTHS, NULL, NULL, NULL }
 };
 
 static const format_char_info ms_scan_char_table[] =
@@ -125,7 +125,7 @@ static const format_char_info ms_scan_char_table[] =
   /* X/Open conversion specifiers.  */
   { "C",     1, STD_EXT, { TEX_W,   BADLEN,  T89_S,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN }, "*w",   "W",   NULL },
   { "S",     1, STD_EXT, { TEX_W,   BADLEN,  T89_S,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN,  BADLEN }, "*aw",  "W",   NULL },
-  { NULL, 0, 0, NOLENGTHS, NULL, NULL, NULL }
+  { NULL, 0, STD_C89, NOLENGTHS, NULL, NULL, NULL }
 };
 
 static const format_char_info ms_time_char_table[] =
@@ -142,7 +142,7 @@ static const format_char_info ms_time_char_table[] =
   { "%",		0, STD_C89, NOLENGTHS, "",       "",   NULL },
   /* C99 conversion specifiers.  */
   { "z",		0, STD_C99, NOLENGTHS, "#",      "",  NULL },
-  { NULL,		0, 0, NOLENGTHS, NULL, NULL, NULL }
+  { NULL,		0, STD_C89, NOLENGTHS, NULL, NULL, NULL }
 };
 
 const format_kind_info mingw_format_attributes[3] =
