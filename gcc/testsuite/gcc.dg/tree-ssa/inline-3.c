a7b61d8cf74caca09cd58095e8904e5a038a84b9
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-05-15 17:11:00 +0000

    * doc/invoke.texi (max-early-inliner-iterations): New flag. 	* ipa-inline.c (enum inlining_mode): New INLINE_SIZE_NORECURSIVE. 	(try_inline): Fix return value. 	(cgraph_decide_inlining_incrementally): Honor new value. 	(cgraph_early_inlining): Handle indirect inlining. 	* params.def (PARAM_EARLY_INLINER_MAX_ITERATIONS): New.
    	* testsuite/gcc.dg/tree-ssa/inline-3.c: New testcase
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@147587 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/inline-3.c
@@ -0,0 +1,29 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-einline2" } */
+extern void inlined ();
+void inline_me_too (void);
+void inline_through_me (void (*ptr)(void));
+void
+inline_me (void)
+{
+  inlined();
+}
+
+void main(void)
+{
+  inline_through_me (inline_me);
+  inline_through_me (inline_me_too);
+}
+void
+inline_through_me (void (*ptr)(void))
+{
+  ptr();
+}
+
+void
+inline_me_too (void)
+{
+  inlined();
+}
+/* { dg-final { scan-tree-dump-times "Inlining inline_me " 1 "einline2"} } */
+/* { dg-final { scan-tree-dump-times "Inlining inline_me_too " 1 "einline2"} } */
