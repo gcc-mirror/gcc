e0a6661b792947c0f09d6bdb3b9ee98723b2993f
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2009-05-17 14:15:24 +0000

    iso-fortran-env.def: Define INT8, INT16, INT32, INT64, REAL32, REAL64 and REAL128.
    	* iso-fortran-env.def: Define INT8, INT16, INT32, INT64, REAL32,
    	REAL64 and REAL128.
    	* gfortran.h (gfc_get_int_kind_from_width_isofortranenv,
    	gfc_get_real_kind_from_width_isofortranenv): New prototypes.
    	* iso-c-binding.def: Update definitions for the INT*_T,
    	INT_LEAST*_T and INT_FAST*_T named parameters.
    	* trans-types.c (get_typenode_from_name, get_int_kind_from_name,
    	gfc_get_real_kind_from_width_isofortranenv): New functions.
    
    	* gfortran.dg/c_kind_int128_test1.f03: Also test C_INT_FAST128_T.
    	* gfortran.dg/c_kind_int128_test2.f03: Update comment.
    	* gfortran.dg/c_kind_params.f90: Also test int_fast*_t.
    	* gfortran.dg/c_kinds.c: Add int_fast*_t arguments.
    
    From-SVN: r147635

--- gcc/testsuite/gfortran.dg/c_kind_params.f90
+++ gcc/testsuite/gfortran.dg/c_kind_params.f90
@@ -5,16 +5,16 @@
 ! the -w option is needed to make f951 not report a warning for 
 ! the -std=c99 option that the C file needs.
 !
-! Note: int_fast*_t currently not supported, cf. PR 448.
 module c_kind_params
   use, intrinsic :: iso_c_binding
   implicit none
 
 contains
   subroutine param_test(my_short, my_int, my_long, my_long_long, &
-       my_int8_t, my_int_least8_t, my_int16_t, &
-       my_int_least16_t, my_int32_t, my_int_least32_t, &
-       my_int64_t, my_int_least64_t, &
+       my_int8_t, my_int_least8_t, my_int_fast8_t, &
+       my_int16_t, my_int_least16_t, my_int_fast16_t, &
+       my_int32_t, my_int_least32_t, my_int_fast32_t, &
+       my_int64_t, my_int_least64_t, my_int_fast64_t, &
        my_intmax_t, my_intptr_t, my_float, my_double, my_long_double, &
        my_char, my_bool) bind(c)
     integer(c_short), value :: my_short
@@ -23,16 +23,16 @@ contains
     integer(c_long_long), value :: my_long_long
     integer(c_int8_t), value :: my_int8_t
     integer(c_int_least8_t), value :: my_int_least8_t
-!   integer(c_int_fast8_t), value :: my_int_fast8_t
+    integer(c_int_fast8_t), value :: my_int_fast8_t
     integer(c_int16_t), value :: my_int16_t
     integer(c_int_least16_t), value :: my_int_least16_t
-!   integer(c_int_fast16_t), value :: my_int_fast16_t
+    integer(c_int_fast16_t), value :: my_int_fast16_t
     integer(c_int32_t), value :: my_int32_t
     integer(c_int_least32_t), value :: my_int_least32_t
-!   integer(c_int_fast32_t), value :: my_int_fast32_t
+    integer(c_int_fast32_t), value :: my_int_fast32_t
     integer(c_int64_t), value :: my_int64_t
     integer(c_int_least64_t), value :: my_int_least64_t
-!   integer(c_int_fast64_t), value :: my_int_fast64_t
+    integer(c_int_fast64_t), value :: my_int_fast64_t
     integer(c_intmax_t), value :: my_intmax_t
     integer(c_intptr_t), value :: my_intptr_t
     real(c_float), value :: my_float
@@ -48,19 +48,19 @@ contains
 
     if(my_int8_t      /= 1_c_int8_t)        call abort()
     if(my_int_least8_t  /= 2_c_int_least8_t ) call abort()
-    print *, 'c_int_fast8_t is:        ', c_int_fast8_t
+    if(my_int_fast8_t  /= 3_c_int_fast8_t ) call abort()
 
     if(my_int16_t     /= 1_c_int16_t)       call abort()
     if(my_int_least16_t /= 2_c_int_least16_t) call abort()
-    print *, 'c_int_fast16_t is:       ', c_int_fast16_t
+    if(my_int_fast16_t  /= 3_c_int_fast16_t ) call abort()
 
     if(my_int32_t     /= 1_c_int32_t)       call abort()
     if(my_int_least32_t /= 2_c_int_least32_t) call abort()
-    print *, 'c_int_fast32_t is:       ', c_int_fast32_t
+    if(my_int_fast32_t  /= 3_c_int_fast32_t ) call abort()
 
     if(my_int64_t     /= 1_c_int64_t)       call abort()
     if(my_int_least64_t /= 2_c_int_least64_t) call abort()
-    print *, 'c_int_fast64_t is:       ', c_int_fast64_t
+    if(my_int_fast64_t  /= 3_c_int_fast64_t ) call abort()
 
     if(my_intmax_t /= 1_c_intmax_t) call abort()
     if(my_intptr_t /= 0_c_intptr_t) call abort()
