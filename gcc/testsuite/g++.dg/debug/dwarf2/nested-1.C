a68a3b4e816d9d7fe276eab351b509c0643752c6
Dodji Seketeli <dodji@redhat.com>
2009-05-18 19:26:41 +0000

    re PR debug/40109 (Incorrect debug info nesting for typedef statements within namespaces)
    Fix for PR debug/40109
    
    gcc/ChangeLog:
    PR debug/40109
    * dwarf2out.c (gen_type_die_with_usage): Generate the DIE as a
    child of the containing namespace's DIE.
    
    gcc/testsuite/ChangeLog:
    PR debug/40109
    * g++.dg/debug/dwarf2/nested-1.C: New test.
    
    From-SVN: r147676

---
+++ gcc/testsuite/g++.dg/debug/dwarf2/nested-1.C
@@ -0,0 +1,29 @@
+// Contributed by Dodji Seketeli <dodji@redhat.com>
+// Origin PR debug/40109
+// { dg-do compile }
+// { dg-options  "-g -dA -O0" }
+
+namespace A
+{
+
+  class B
+  {
+  };
+  typedef A::B AB;
+};
+
+int
+main()
+{
+  A::AB ab;
+  return 0;
+}
+
+// { dg-final { scan-assembler "DW_TAG_typedef" } }
+//
+// What we want to do here is to be sure that the DIE of A::AB is generated
+// as a child of the DIE of the namespace A declaration.
+// So this test won't catch a regression on this fix yet. To write a proper
+// test for this fix, we would need a dwarf reader written in tcl,
+// or something along those lines.
+
