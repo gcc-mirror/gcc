fda31640d39b7bb9822414655b34064b468e58f5
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-05-19 21:17:00 +0000

    2009-05-19  H.J. Lu  <hongjiu.lu@intel.com>
    	PR c/40172
    	* gcc.dg/pr40172.c: Renamed to ...
    	* gcc.dg/pr40172-1.c: This.
    
    	* gcc.dg/pr40172-2.c: New.
    	* gcc.dg/pr40172-3.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@147719 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr40172-3.c
@@ -0,0 +1,17 @@
+/* PR middle-end/40172 */
+/* { dg-do compile */
+/* { dg-xfail-if "" { "*-*-*" } { "*" } { "" } } */
+/* { dg-options "-Wall -W -Werror -Wlogical-op" } */
+
+extern int xxx;
+
+#define XXX xxx
+
+int
+test (void)
+{
+  if (!XXX && xxx)
+    return 4;
+  else
+    return 0;
+}
