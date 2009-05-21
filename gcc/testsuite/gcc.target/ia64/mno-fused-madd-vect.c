265b3b8dbd88b4fbd169ddd5c1cac032b7aabc90
Steve Ellcey <sje@cup.hp.com>
2009-05-21 23:53:02 +0000

    re PR target/37846 (Option -mno-fused-madd should be supported on IA-64)
    	PR target/37846
    	* gcc.target/ia64/mfused-madd-vect.c: New test.
    	* gcc.target/ia64/mfused-madd.c: New test.
    	* gcc.target/ia64/mno-fused-madd-vect.c: New test.
    	* gcc.target/ia64/mno-fused-madd.c: New test.
    
    From-SVN: r147783

---
+++ gcc/testsuite/gcc.target/ia64/mno-fused-madd-vect.c
@@ -0,0 +1,31 @@
+/* { dg-do compile */
+/* { dg-options "-O2 -mno-fused-madd -ftree-vectorize" } */
+/* { dg-final { scan-assembler "fpmpy" } } */
+
+/* fpma and fpms will show in either way because there are no
+   specific vector add/sub instructions.  So we just check for fpmpy.  */
+
+#define N 16
+extern bar(float *, float *, float *, float *);
+void foo()
+{
+	int i;
+	float a[N], b[N], c[N], d[N];
+	bar(a,b,c,d);
+	for (i = 0; i < N; i++) {
+		a[i] = b[i] + c[i] * d[i];
+	}
+	bar(a,b,c,d);
+	for (i = 0; i < N; i++) {
+		a[i] = b[i] - c[i] * d[i];
+	}
+	bar(a,b,c,d);
+	for (i = 0; i < N; i++) {
+		a[i] = b[i] * c[i] + d[i];
+	}
+	bar(a,b,c,d);
+	for (i = 0; i < N; i++) {
+		a[i] = b[i] * c[i] - d[i];
+	}
+	bar(a,b,c,d);
+}
