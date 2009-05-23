947dd720e0422abb2d1f55742a3df1d0c4b5b316
denisc <denisc@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-05-23 07:16:07 +0000

    * config/avr/avr.c: Change my email address. 	* config/avr/avr.h: Likewise. 	* config/avr/avr.md: Likewise. 	* config/avr/avr-protos.h: Likewise. 	* config/avr/libgcc.S: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@147817 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/avr/avr.h
+++ gcc/config/avr/avr.h
@@ -2,7 +2,7 @@
    for ATMEL AVR at90s8515, ATmega103/103L, ATmega603/603L microcontrollers.
    Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 
    2008, 2009 Free Software Foundation, Inc.
-   Contributed by Denis Chertykov (denisc@overta.ru)
+   Contributed by Denis Chertykov (chertykov@gmail.com)
 
 This file is part of GCC.
 
