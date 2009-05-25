1eb4ddaff88afdbaa1a7d5ecf9c4fc61800078b2
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-05-25 03:07:00 +0000

    2009-05-24  Jerry DeLisle  <jvdelisle@gcc.gnu.org> 	    Dominique Dhumieres
    	PR fortran/35732
    	PR fortran/39872
    	* trans-array.c (gfc_conv_ss_startstride): Add one to index.
    	* gfortran.dg/bounds_check_fail_3.f90: New test.
    	* gfortran.dg/bounds_check_fail_4.f90: New test.
    	* gfortran.dg/bounds_check_14.f90: Update test.
    	* gfortran.dg/bound_4.f90: Update test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@147842 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/bounds_check_fail_4.f90
@@ -0,0 +1,12 @@
+! { dg-do run }
+! { dg-options "-fbounds-check" }
+! { dg-shouldfail "foo" }
+  integer x(10), m, n
+  x = (/ (i, i = 1, 10) /)
+  m = -3
+  n = -2
+  x(7:1:m) = x(1:3) + x(6:2:n) 
+  if (any(x /= (/ 5, 2, 3, 6, 5, 6, 7, 8, 9, 10 /))) call abort()
+  x(8:1:m) = x(1:3) + x(5:2:n) 
+  end
+! { dg-output "line 10 .* bound mismatch, .* dimension 1 .* array \'x\' \\\(2/3\\\)" }
