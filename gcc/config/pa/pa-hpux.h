8633c31574501e823ed7f28ed949c30e3b0b3430
Richard Sandiford <r.sandiford@uk.ibm.com>
2009-06-02 19:15:26 +0000

    pa-hpux.h (LINK_SPEC): Remove "%<fwhole-program".
    gcc/
    	* config/pa/pa-hpux.h (LINK_SPEC): Remove "%<fwhole-program".
    	* config/pa/pa-hpux10.h (LINK_SPEC): Likewise.
    	* config/pa/pa-hpux11.h (LINK_SPEC): Likewise.
    	* gcc.c (set_collect_gcc_options): Don't add -fwhole-program
    	to COLLECT_GCC_OPTIONS.
    
    From-SVN: r148099

--- gcc/config/pa/pa-hpux.h
+++ gcc/config/pa/pa-hpux.h
@@ -95,12 +95,10 @@ along with GCC; see the file COPYING3.  If not see
 #undef LINK_SPEC
 #if ((TARGET_DEFAULT | TARGET_CPU_DEFAULT) & MASK_PA_11)
 #define LINK_SPEC \
-  "%<fwhole-program\
-   %{!mpa-risc-1-0:%{!march=1.0:%{static:-L/lib/pa1.1 -L/usr/lib/pa1.1 }}}%{mlinker-opt:-O} %{!shared:-u main} %{static:-a archive} %{g*:-a archive} %{shared:-b}"
+  "%{!mpa-risc-1-0:%{!march=1.0:%{static:-L/lib/pa1.1 -L/usr/lib/pa1.1 }}}%{mlinker-opt:-O} %{!shared:-u main} %{static:-a archive} %{g*:-a archive} %{shared:-b}"
 #else
 #define LINK_SPEC \
-  "%<fwhole-program\
-   %{mlinker-opt:-O} %{!shared:-u main} %{static:-a archive} %{g*:-a archive} %{shared:-b}"
+  "%{mlinker-opt:-O} %{!shared:-u main} %{static:-a archive} %{g*:-a archive} %{shared:-b}"
 #endif
 
 /* hpux8 and later have C++ compatible include files, so do not
