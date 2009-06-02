fe8a77795ad17af6538796266edf9a04b54b2f34
Alexandre Oliva <aoliva@redhat.com>
2009-06-02 08:10:53 +0000

    cfgexpand.c (gimple_expand_cfg): Discard the source location only for builtins that are not overridden.
    * cfgexpand.c (gimple_expand_cfg): Discard the source location
    only for builtins that are not overridden.
    
    From-SVN: r148078

--- gcc/cfgexpand.c
+++ gcc/cfgexpand.c
@@ -2438,7 +2438,7 @@ gimple_expand_cfg (void)
   rtl_profile_for_bb (ENTRY_BLOCK_PTR);
 
   insn_locators_alloc ();
-  if (!DECL_BUILT_IN (current_function_decl))
+  if (!DECL_IS_BUILTIN (current_function_decl))
     {
       /* Eventually, all FEs should explicitly set function_start_locus.  */
       if (cfun->function_start_locus == UNKNOWN_LOCATION)
