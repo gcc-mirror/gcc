2771246a535b04258f26c8b768664dd271771db3
Alexander Strange <astrange@ithinksw.com>
2009-06-05 08:30:21 +0000

    re PR tree-optimization/36318 (SRA pessimizes struct copies without -Os)
    2009-06-05  Alexander Strange <astrange@ithinksw.com>
    
    	PR tree-optimization/36318
    	* gcc.dg/tree-ssa/sra-7.c: New test.
    
    From-SVN: r148203

---
+++ gcc/testsuite/gcc.dg/tree-ssa/sra-7.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-optimized" } */
+
+typedef struct { char f[4]; } __attribute__((aligned (4))) s;
+
+void a(s *s1, s *s2)
+{
+  *s1 = *s2;
+}
+
+/* Struct copies should not be split into members.  */
+/* { dg-final { scan-tree-dump "\\\*s1_.\\\(D\\\) = \\\*s2"  "optimized"} } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
