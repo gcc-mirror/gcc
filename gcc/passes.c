b50b1b69d53ee6f79903eb8b0dac95426208e6c7
revitale <revitale@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-06-10 18:19:49 +0000

    Reschedule predictive-commoning pass
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@148352 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/passes.c
+++ gcc/passes.c
@@ -648,7 +648,6 @@ init_optimization_passes (void)
 	  NEXT_PASS (pass_copy_prop);
 	  NEXT_PASS (pass_dce_loop);
 	  NEXT_PASS (pass_lim);
-	  NEXT_PASS (pass_predcom);
 	  NEXT_PASS (pass_tree_unswitch);
 	  NEXT_PASS (pass_scev_cprop);
 	  NEXT_PASS (pass_empty_loop);
@@ -665,6 +664,7 @@ init_optimization_passes (void)
 	      NEXT_PASS (pass_lower_vector_ssa);
 	      NEXT_PASS (pass_dce_loop);
 	    }
+          NEXT_PASS (pass_predcom);
 	  NEXT_PASS (pass_complete_unroll);
 	  NEXT_PASS (pass_slp_vectorize);
 	  NEXT_PASS (pass_parallelize_loops);
