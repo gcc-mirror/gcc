8ed0ce9982d3a3ae3317d5fa44b96b391f58c385
H.J. Lu <hjl@gcc.gnu.org>
2009-06-11 21:27:34 -0700

    i386.c (OPTION_MASK_ISA_CRC32_SET): New.
    gcc/
    
    2009-06-11  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* config/i386/i386.c (OPTION_MASK_ISA_CRC32_SET): New.
    	(OPTION_MASK_ISA_CRC32_UNSET): Likewise.
    	(ix86_handle_option): Handle OPT_mcrc32.
    	(ix86_target_string): Add -mcrc32.
    	(bdesc_args): Enable crc32 builtins with OPTION_MASK_ISA_CRC32.
    
    	* config/i386/i386.h (TARGET_CRC32): New.
    
    	* config/i386/i386.md (sse4_2_crc32<mode>): Also check
    	TARGET_CRC32.
    	(sse4_2_crc32di): Likewise.
    
    	* config/i386/i386.opt (mcrc32: New.
    
    	* doc/invoke.texi: Document -mcrc32.
    
    gcc/testsuite/
    
    2009-06-11  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* gcc.target/i386/crc32-1.c: New.
    	* gcc.target/i386/crc32-2.c: Likewise.
    
    From-SVN: r148410

---
+++ gcc/testsuite/gcc.target/i386/crc32-1.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -mcrc32" } */
+/* { dg-final { scan-assembler "crc32b\[^\\n\]*eax" } } */
+/* { dg-final { scan-assembler "crc32w\[^\\n\]*eax" } } */
+/* { dg-final { scan-assembler "crc32l\[^\\n\]*eax" } } */
+
+unsigned int
+crc32b (unsigned int x, unsigned char y)
+{
+  return __builtin_ia32_crc32qi (x, y);
+}
+
+unsigned int
+crc32w (unsigned int x, unsigned short y)
+{
+  return __builtin_ia32_crc32hi (x, y);
+}
+
+unsigned int
+crc32d (unsigned int x, unsigned int y)
+{
+  return __builtin_ia32_crc32si (x, y);
+}
