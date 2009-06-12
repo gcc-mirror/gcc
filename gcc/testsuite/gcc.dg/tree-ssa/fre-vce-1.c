380c5f612a168b4a271b392ac16e202b4088c52d
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-06-12 19:00:39 +0000

    2009-06-12  Andrew Pinski  <andrew_pinski@playstation.sony.com>
            PR tree-opt/38865
            * tree-ssa-sccvn.c (visit_reference_op_load): If vn_reference_lookup
            is returns NULL and OP is a VCE, look through the VCE.
    
    
    
    2009-06-12  Andrew Pinski  <andrew_pinski@playstation.sony.com>
    
            PR tree-opt/38865
            * gcc.dg/tree-ssa/fre-vce-1.c
    :
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@148437 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/fre-vce-1.c
@@ -0,0 +1,35 @@
+/* { dg-options "-O2 -fdump-tree-fre -w" } */
+/* { dg-do compile } */
+#define vector __attribute__((vector_size(sizeof(int)*4) ))
+struct s { vector int i; };
+vector float f(struct s *sv)
+{
+  sv->i = (vector int){1, 2, 3, 4};
+  return (vector float)sv->i;
+}
+
+
+vector float f1(struct s *sv, vector int a)
+{
+  sv->i = a;
+  return (vector float)sv->i;
+}
+
+struct s1 { int i; };
+
+void g(struct s1 *, float);
+void a1 (struct s1 sv)
+{
+  sv.i = 1;
+  g(&sv, *(float*)&sv.i);
+}
+
+
+void a2 (struct s1 sv, int i)
+{
+  sv.i = i;
+  g(&sv, *(float*)&sv.i);
+}
+
+/* { dg-final { scan-tree-dump-times "sv_\[0-9\]\\\(D\\\)->i" 2 "fre" } } */
+/* { dg-final { scan-tree-dump-times "sv.i" 2 "fre" } } */
