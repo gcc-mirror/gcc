5fd8300bd5fda7ade5791662c5909094b22850ab
Ian Lance Taylor <iant@google.com>
2009-06-16 16:46:31 +0000

    bitmap.c (bitmap_clear): Don't declare as inline.
    	* bitmap.c (bitmap_clear): Don't declare as inline.
    	* gimple.c (gimplify_assign): Likewise.
    	* tree-ssa-sccvn.c (vn_nary_op_compute_hash): Likewise.
    	* haifa-sched.c (insn_cost): Don't declare with HAIFA_INLINE.
    	(sched_scan_info): Remove duplicate definition.
    
    From-SVN: r148539

--- gcc/tree-ssa-sccvn.c
+++ gcc/tree-ssa-sccvn.c
@@ -1362,7 +1362,7 @@ vn_reference_insert_pieces (tree vuse, alias_set_type set, tree type,
 
 /* Compute and return the hash value for nary operation VBO1.  */
 
-inline hashval_t
+hashval_t
 vn_nary_op_compute_hash (const vn_nary_op_t vno1)
 {
   hashval_t hash = 0;
