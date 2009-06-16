c174c6502ea73db9ed09df9bde8558d257763f95
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-06-16 12:31:49 +0000

    2009-06-16  Richard Guenther  <rguenther@suse.de>
    	* tree-ssa-structalias.c (do_ds_constraint): Stores in global
    	variables add them to ESCAPED.
    	(find_func_aliases): Do not make all indirectly stored values
    	escaped.
    
    	* gcc.dg/tree-ssa/pta-escape-1.c: New testcase.
    	* gcc.dg/tree-ssa/pta-escape-2.c: Likewise.
    	* gcc.dg/tree-ssa/pta-escape-3.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-fre-27.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@148525 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pta-escape-1.c
@@ -0,0 +1,37 @@
+/* { dg-do run } */
+/* { dg-options "-O -fdump-tree-alias-details" } */
+
+int *i;
+void __attribute__((noinline))
+foo (void)
+{
+  *i = 1;
+}
+int __attribute__((noinline))
+bar(int local_p)
+{
+  int x = 0;
+  int *j;
+  int **p;
+  if (local_p)
+    p = &j;
+  else
+    p = &i;
+  *p = &x;  /* This makes x escape.  */
+  foo ();
+  return x;
+}
+extern void abort (void);
+int main()
+{
+  int k = 2;
+  i = &k;
+  if (bar (1) != 0 || k != 1)
+    abort ();
+  if (bar (0) != 1)
+    abort ();
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump "ESCAPED, points-to vars: { x }" "alias" } } */
+/* { dg-final { cleanup-tree-dump "alias" } } */
---
+++ gcc/testsuite/gcc.dg/tree-ssa/pta-escape-2.c
@@ -0,0 +1,38 @@
+/* { dg-do run } */
+/* { dg-options "-O -fdump-tree-alias-details" } */
+
+int *i;
+void __attribute__((noinline))
+foo (void)
+{
+  *i = 1;
+}
+int __attribute__((noinline))
+bar(int local_p, int **q)
+{
+  int x = 0;
+  int *j;
+  int **p;
+  if (local_p)
+    p = &j;
+  else
+    p = q;
+  *p = &x;  /* This makes x escape.  */
+  foo ();
+  return x;
+}
+extern void abort (void);
+int main()
+{
+  int k = 2;
+  int **q = &i;
+  i = &k;
+  if (bar (1, q) != 0 || k != 1)
+    abort ();
+  if (bar (0, q) != 1)
+    abort ();
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump "ESCAPED, points-to vars: { x }" "alias" } } */
+/* { dg-final { cleanup-tree-dump "alias" } } */
