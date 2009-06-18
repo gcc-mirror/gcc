2c2f70e1f266b6582398248096feccaceb2dd70c
Uros Bizjak <ubizjak@gmail.com>
2009-06-18 13:35:38 +0200

    convert.c (convert_to_integer): Convert (int)logb() into ilogb().
    	* convert.c (convert_to_integer): Convert (int)logb() into ilogb().
    
    testsuite/ChangeLog:
    
    	* gcc.dg/builtins-65.c: New test.
    
    From-SVN: r148653

---
+++ gcc/testsuite/gcc.dg/builtins-65.c
@@ -0,0 +1,39 @@
+/* { dg-do link } */
+/* { dg-options "-O2 -ffast-math" } */
+
+extern int ilogbf (float);
+extern float logbf (float);
+extern int ilogb (double);
+extern double logb (double);
+extern int ilogbl (long double);
+extern long double logbl (long double);
+
+extern void link_error(void);
+
+void testf(float x)
+{
+  if ((int) logbf (x) != ilogbf (x))
+    link_error ();
+}
+
+void test(double x)
+{
+  if ((int) logb (x) != ilogb (x))
+    link_error ();
+}
+
+void testl(long double x)
+{
+  if ((int) logbl (x) != ilogbl (x))
+    link_error ();
+}
+
+int main()
+{
+  testf (2.0f);
+  test (2.0);
+  testl (2.0l);
+
+  return 0;
+}
+
