074160ef5d621f140ce3372e63084471d5505b5c
H.J. Lu <hongjiu.lu@intel.com>
2009-06-18 21:23:29 +0000

    backport: re PR target/40470 (unable to find a register to spill in class ‘SSE_FIRST_REG’)
    gcc/
    
    2009-06-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	Backport from mainline:
    	2009-06-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/40470
    	* config/i386/i386.h (CLASS_LIKELY_SPILLED_P): Add SSE_FIRST_REG.
    
    gcc/testsuite/
    
    2009-06-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	Backport from mainline:
    	2009-06-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/40470
    	* gcc.dg/torture/pr40470-1.c: New.
    	* gcc.dg/torture/pr40470-2.c: Likewise.
    	* gcc.dg/torture/pr40470-3.c: Likewise.
    	* gcc.dg/torture/pr40470-4.c: Likewise.
    
    From-SVN: r148676

--- gcc/config/i386/i386.h
+++ gcc/config/i386/i386.h
@@ -1550,6 +1550,7 @@ enum reg_class
    || ((CLASS) == AD_REGS)						\
    || ((CLASS) == SIREG)						\
    || ((CLASS) == DIREG)						\
+   || ((CLASS) == SSE_FIRST_REG)					\
    || ((CLASS) == FP_TOP_REG)						\
    || ((CLASS) == FP_SECOND_REG))
 
