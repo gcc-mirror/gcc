2a457a9b4fa448489522d39390e857541a95abd5
H.J. Lu <hongjiu.lu@intel.com>
2009-06-18 21:12:12 +0000

    re PR target/40470 (unable to find a register to spill in class ‘SSE_FIRST_REG’)
    gcc/
    
    2009-06-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/40470
    	* config/i386/i386.h (CLASS_LIKELY_SPILLED_P): Add SSE_FIRST_REG.
    
    gcc/testsuite/
    
    2009-06-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/40470
    	* gcc.dg/torture/pr40470-1.c: New.
    	* gcc.dg/torture/pr40470-2.c: Likewise.
    	* gcc.dg/torture/pr40470-3.c: Likewise.
    	* gcc.dg/torture/pr40470-4.c: Likewise.
    
    From-SVN: r148673

---
+++ gcc/testsuite/gcc.dg/torture/pr40470-1.c
@@ -0,0 +1,22 @@
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-msse4" } */
+#include <nmmintrin.h>
+__m128i load (char *);
+char *
+foo (char *p1, char *p2,
+     int bmsk, __m128i mask1, __m128i mask2)
+{
+  int len = 0;
+  __m128i frag1, frag2;
+  int  cmp_s;
+  if( !p2[0]) return p1;
+  if( !p1[0] ) return NULL;
+  frag2 = load (p2); 
+  frag1 = load (p1);
+  frag2 = _mm_blendv_epi8(frag2, mask2, mask1);
+  frag1 = _mm_blendv_epi8(frag1, mask2, mask1);
+  cmp_s = _mm_cmpistrs(frag2, frag1, 0x0c);
+  if( cmp_s )
+    __asm("bsfl %[bmsk], %[len]" : [len] "=r" (len) : [bmsk] "r" (bmsk) );
+  return p1 + len;
+}
