cb7a89619d7ac8fcefaab6c20d09bb55b7dc5cee
Tobias Burnus <burnus@net-b.de>
2009-06-20 20:07:10 +0200

    re PR fortran/40452 (-fbounds-check: False positive due to ignoring storage association)
    2009-06-20  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/40452
            * trans-decl.c (add_argument_checking): Disable bounds check
            for allowed argument storage association.
    
    2009-06-20  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/40452
            * gfortran.dg/bounds_check_strlen_9.f90: New test.
    
    From-SVN: r148750

--- gcc/fortran/trans-decl.c
+++ gcc/fortran/trans-decl.c
@@ -3835,7 +3835,11 @@ add_argument_checking (stmtblock_t *block, gfc_symbol *sym)
 
 	/* For POINTER, ALLOCATABLE and assumed-shape dummy arguments, the
 	   string lengths must match exactly.  Otherwise, it is only required
-	   that the actual string length is *at least* the expected one.  */
+	   that the actual string length is *at least* the expected one.
+	   Sequence association allows for a mismatch of the string length
+	   if the actual argument is (part of) an array, but only if the
+	   dummy argument is an array. (See "Sequence association" in
+	   Section 12.4.1.4 for F95 and 12.4.1.5 for F2003.)  */
 	if (fsym->attr.pointer || fsym->attr.allocatable
 	    || (fsym->as && fsym->as->type == AS_ASSUMED_SHAPE))
 	  {
@@ -3843,6 +3847,8 @@ add_argument_checking (stmtblock_t *block, gfc_symbol *sym)
 	    message = _("Actual string length does not match the declared one"
 			" for dummy argument '%s' (%ld/%ld)");
 	  }
+	else if (fsym->as && fsym->as->rank != 0)
+	  continue;
 	else
 	  {
 	    comparison = LT_EXPR;
