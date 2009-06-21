93973253875f390195795adae9af63e4c55d3564
irar <irar@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-06-21 07:25:21 +0000

    PR testsuite/40475 	* gcc.dg/vect/vect-nest-cycle-1.c: Fail to vectorize on targets 	without misalignment support. 	* gcc.dg/vect/vect-nest-cycle-2.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@148758 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/vect/vect-nest-cycle-1.c
+++ gcc/testsuite/gcc.dg/vect/vect-nest-cycle-1.c
@@ -43,6 +43,6 @@ int main ()
   return 0;
 }
 
-/* { dg-final { scan-tree-dump-times "OUTER LOOP VECTORIZED" 1 "vect" } } */
+/* { dg-final { scan-tree-dump-times "OUTER LOOP VECTORIZED" 1 "vect" { xfail vect_no_align } } } */
 /* { dg-final { cleanup-tree-dump "vect" } } */
 
