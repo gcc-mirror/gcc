5eabd16b31b0ef13bc7f3a04fdd6dad04b62f1fe
revitale <revitale@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-06-21 11:36:03 +0000

    Fix 40359
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@148762 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/vect/vect-96.c
+++ gcc/testsuite/gcc.dg/vect/vect-96.c
@@ -45,5 +45,5 @@ int main (void)
 /* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } } */
 /* { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 1 "vect" { target { {! vect_no_align} && vector_alignment_reachable } } } } */
 /* { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 1 "vect" { xfail { { vect_no_align || vect_hw_misalign } || {! vector_alignment_reachable} } } } } */
-/* { dg-final { scan-tree-dump-times "Alignment of access forced using versioning." 1 "vect" { target { vect_no_align || { {! vector_alignment_reachable} && {!vect_hw_misalign} } } } } } */
+/* { dg-final { scan-tree-dump-times "Alignment of access forced using versioning." 1 "vect" { target { vect_no_align || { {! vector_alignment_reachable} && {! vect_hw_misalign} } } } } } */
 /* { dg-final { cleanup-tree-dump "vect" } } */
