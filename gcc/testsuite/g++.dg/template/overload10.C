c4bcc71fcb21e0543b6ae017bf9381edddfb6316
Jason Merrill <jason@redhat.com>
2009-06-24 12:25:52 -0400

    re PR c++/40342 (ambiguous overload not diagnosed)
    	PR c++/40342
    	* decl.c (decls_match): Check DECL_TI_TEMPLATE too.
    	* class.c (resolve_address_of_overloaded_function): Fix typo.
    
    From-SVN: r148910

---
+++ gcc/testsuite/g++.dg/template/overload10.C
@@ -0,0 +1,6 @@
+// PR c++40342
+
+template <typename T1, typename T2> int f(T1 *, const T2 *); // { dg-error "" }
+template <typename T1, typename T2> int f(const T1 *, T2 *); // { dg-error "" }
+
+int (*p)(const int *, const int *) = f; // { dg-error "ambiguous" }
