7e196f892ae1e8238a36a2adbc210c394c089d2d
Janus Weil <janus@gcc.gnu.org>
2009-06-24 12:59:56 +0200

    re PR fortran/40427 ([F03] Procedure Pointer Components with OPTIONAL arguments)
    2009-06-24  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/40427
    	* gfortran.h (gfc_component): New member 'formal_ns'.
    	(gfc_copy_formal_args_ppc,void gfc_ppc_use): New.
    	* interface.c (gfc_ppc_use): New function, analogous to
    	gfc_procedure_use, but for procedure pointer components.
    	* module.c (MOD_VERSION): Bump module version.
    	(mio_component): Treat formal arguments.
    	(mio_formal_arglist): Changed argument from gfc_symbol to
    	gfc_formal_arglist.
    	(mio_symbol): Changed argument of mio_formal_arglist.
    	* resolve.c (resolve_ppc_call,resolve_expr_ppc): Call gfc_ppc_use,
    	to check actual arguments and treat formal args correctly.
    	(resolve_fl_derived): Copy formal args of procedure pointer components
    	from their interface.
    	* symbol.c (gfc_copy_formal_args_ppc): New function, analogous to
    	gfc_copy_formal_args, but for procedure pointer components.
    
    
    2009-06-24  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/40427
    	* gfortran.dg/proc_ptr_comp_11.f90: New.
    
    From-SVN: r148906

--- gcc/fortran/resolve.c
+++ gcc/fortran/resolve.c
@@ -4847,9 +4847,7 @@ resolve_ppc_call (gfc_code* c)
 			      comp->formal == NULL) == FAILURE)
     return FAILURE;
 
-  /* TODO: Check actual arguments.
-     gfc_procedure_use (stree->n.sym, &c->expr1->value.compcall.actual,
-			&c->expr1->where);*/
+  gfc_ppc_use (comp, &c->expr1->value.compcall.actual, &c->expr1->where);
 
   return SUCCESS;
 }
@@ -4881,8 +4879,7 @@ resolve_expr_ppc (gfc_expr* e)
 			      comp->formal == NULL) == FAILURE)
     return FAILURE;
 
-  /* TODO: Check actual arguments.
-     gfc_procedure_use (stree->n.sym, &e->value.compcall.actual, &e->where);  */
+  gfc_ppc_use (comp, &e->value.compcall.actual, &e->where);
 
   return SUCCESS;
 }
@@ -9040,7 +9037,7 @@ resolve_fl_derived (gfc_symbol *sym)
 	      c->ts.interface = ifc;
 	      c->attr.function = ifc->attr.function;
 	      c->attr.subroutine = ifc->attr.subroutine;
-	      /* TODO: gfc_copy_formal_args (c, ifc);  */
+	      gfc_copy_formal_args_ppc (c, ifc);
 
 	      c->attr.allocatable = ifc->attr.allocatable;
 	      c->attr.pointer = ifc->attr.pointer;
@@ -9051,7 +9048,7 @@ resolve_fl_derived (gfc_symbol *sym)
 	      c->attr.always_explicit = ifc->attr.always_explicit;
 	      /* Copy array spec.  */
 	      c->as = gfc_copy_array_spec (ifc->as);
-	      /*if (c->as)
+	      /* TODO: if (c->as)
 		{
 		  int i;
 		  for (i = 0; i < c->as->rank; i++)
@@ -9066,7 +9063,7 @@ resolve_fl_derived (gfc_symbol *sym)
 		  c->ts.cl = gfc_get_charlen();
 	          c->ts.cl->resolved = ifc->ts.cl->resolved;
 		  c->ts.cl->length = gfc_copy_expr (ifc->ts.cl->length);
-		  /*gfc_expr_replace_symbols (c->ts.cl->length, c);*/
+		  /* TODO: gfc_expr_replace_symbols (c->ts.cl->length, c);*/
 		  /* Add charlen to namespace.  */
 		  /*if (c->formal_ns)
 		    {
---
+++ gcc/testsuite/gfortran.dg/proc_ptr_comp_11.f90
@@ -0,0 +1,41 @@
+! { dg-do run }
+!
+! PR 40427: Procedure Pointer Components with OPTIONAL arguments
+!
+! Original test case by John McFarland <john.mcfarland@swri.org>
+! Modified by Janus Weil <janus@gcc.gnu.org>
+
+PROGRAM prog
+
+ ABSTRACT INTERFACE
+ SUBROUTINE sub_template(i,j,o)
+   INTEGER, INTENT(in) :: i
+   INTEGER, INTENT(in), OPTIONAL :: j, o
+ END SUBROUTINE sub_template
+ END INTERFACE
+
+ TYPE container
+   PROCEDURE(sub_template), POINTER, NOPASS :: s
+ END TYPE container
+
+ PROCEDURE(sub_template), POINTER :: f
+ TYPE (container) :: c
+
+ c%s => sub
+ f => sub
+
+ CALL f(2,o=4)
+ CALL c%s(3,o=6)
+
+CONTAINS
+
+ SUBROUTINE sub(i,arg2,arg3)
+   INTEGER, INTENT(in) :: i
+   INTEGER, INTENT(in), OPTIONAL :: arg2, arg3
+   if (present(arg2)) call abort()
+   if (.not. present(arg3)) call abort()
+   if (2*i/=arg3) call abort()
+ END SUBROUTINE sub
+
+END PROGRAM prog
+
