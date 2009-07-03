b5a4c0720f2d5d2245ec7a2a72f157b4975a9b6e
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-03 14:11:14 +0000

    2009-07-03  Richard Guenther  <rguenther@suse.de>
    	PR middle-end/34163
    	* tree-chrec.c (chrec_convert_1): Fold (T2)(t +- x) to
    	(T2)t +- (T2)x if t +- x is known to not overflow and
    	the conversion widens the operation.
    	* Makefile.in (tree-chrec.o): Add $(FLAGS_H) dependency.
    
    	* gfortran.dg/pr34163.f90: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@149207 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr34163.f90
@@ -0,0 +1,16 @@
+! { dg-do compile }
+! { dg-options "-O2 -fpredictive-commoning -fdump-tree-pcom-details" }
+subroutine trisolve2(x,i1,i2,nxyz)
+integer :: nxyz
+real,dimension(nxyz):: au1
+real,allocatable,dimension(:) :: gi
+integer :: i1 , i2
+real,dimension(i2)::x
+integer :: i
+allocate(gi(nxyz))
+do i = i1+1 , i2
+   x(i) = gi(i)*(x(i)-au1(i-1)*x(i-1))
+enddo
+end subroutine trisolve2
+! { dg-final { scan-tree-dump "Executing predictive commoning" "pcom" } }
+! { dg-final { cleanup-tree-dump "pcom" } }
