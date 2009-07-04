9ee8efb930e81d5d60b35597d24745082e5acea3
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-04 11:33:17 +0000

    * gcc.dg/framework-2.c: Adjust testcase to pass.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@149226 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/framework-2.c
+++ gcc/testsuite/gcc.dg/framework-2.c
@@ -1,6 +1,6 @@
 /* { dg-do compile { target *-*-darwin* } } */
 /* { dg-options "-F$srcdir/gcc.dg" } */
 
-#include <Foundation/Foundation.h> /* { dg-error "Foundation/Foundation.h" } */
+#include <Foundation/Foundation.h>
 /* { dg-message "terminated" "" { target *-*-* } 0 } */
-
+/* { dg-message "Foundation/Foundation.h" "" { target *-*-* } 0 } */
