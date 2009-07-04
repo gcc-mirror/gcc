258bd5dcfde78204c66bd632f26b347260a2bca1
Jakub Jelinek <jakub@redhat.com>
2009-07-04 19:20:46 +0200

    trans-intrinsic.c (gfc_conv_intrinsic_minmaxloc): For integer maxloc initialize limit to -huge-1 rather than just -huge.
    	* trans-intrinsic.c (gfc_conv_intrinsic_minmaxloc): For integer
    	maxloc initialize limit to -huge-1 rather than just -huge.
    
    	* gfortran.dg/maxloc_1.f90: New test.
    
    From-SVN: r149236

---
+++ gcc/testsuite/gfortran.dg/maxloc_1.f90
@@ -0,0 +1,14 @@
+! { dg-do run }
+  integer :: a(3), n
+  a(1) = -huge(n)
+  a(2) = -huge(n)
+  a(3) = -huge(n)
+  a(1) = a(1) - 1
+  a(2) = a(2) - 1
+  a(3) = a(3) - 1
+  n = maxloc (a, dim = 1)
+  if (n .ne. 1) call abort
+  a(2) = -huge(n)
+  n = maxloc (a, dim = 1)
+  if (n .ne. 2) call abort
+end
