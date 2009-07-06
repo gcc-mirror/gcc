8207d787b58acae882de7cb3510e0e3d7392b10a
Benjamin Kosnik <bkoz@gcc.gnu.org>
2009-07-06 22:44:58 +0000

    3.cc: Add copyright.
    2009-07-06  Benjamin Kosnik  <bkoz@redhat.com>
    
    	*  testsuite/21_strings/c_strings/wchar_t/3.cc: Add copyright.
    	*  testsuite/21_strings/c_strings/char/3.cc: Same.
    
    From-SVN: r149300

--- libstdc++-v3/testsuite/21_strings/c_strings/wchar_t/3.cc
+++ libstdc++-v3/testsuite/21_strings/c_strings/wchar_t/3.cc
@@ -1,6 +1,23 @@
 // { dg-do compile { target correct_iso_cpp_string_wchar_protos } }
 // { dg-options "-O2" }
 
+// Copyright (C) 2009 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
 #include <cwchar>
 
 const wchar_t *cw1, *cw2;
@@ -23,8 +40,8 @@ test01 ()
 
   w1 = wmemchr (cw2, L'/', 3);		// { dg-error "invalid conversion" }
   w1 = wcschr (cw2, L'/');		// { dg-error "invalid conversion" }
-  w1 = wcspbrk (cw2, L"abc");		// { dg-error "invalid conversion" }
   w1 = wcsrchr (cw2, L'c');		// { dg-error "invalid conversion" }
+  w1 = wcspbrk (cw2, L"abc");		// { dg-error "invalid conversion" }
   w1 = wcsstr (cw2, L"abc");		// { dg-error "invalid conversion" }
 
   cw1 = wmemchr (cw2, L'/', 3);
