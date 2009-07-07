128278833475615a6c242cc79684c20a4039fd04
Jakub Jelinek <jakub@redhat.com>
2009-07-07 16:07:19 +0200

    re PR middle-end/40669 (ICE in gimple_assign_set_rhs1 from eliminate_tail_call)
    	PR middle-end/40669
    	* tree-tailcall.c (adjust_return_value_with_ops,
    	create_tailcall_accumulator): Set DECL_GIMPLE_REG_P on the temporary
    	if it has complex or vector type.
    
    	Backport from mainline:
    	2009-06-03  Richard Guenther  <rguenther@suse.de>
    
    	PR middle-end/40328
    	* fold-const.c (fold_convert): Fold the build COMPLEX_EXPR.
    
    	* gcc.dg/pr40669.c: New test.
    
    From-SVN: r149329

---
+++ gcc/testsuite/gcc.dg/pr40669.c
@@ -0,0 +1,26 @@
+/* PR middle-end/40669 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -ffast-math" } */
+
+double _Complex
+test (int d, int t, double *x, double *y, double *z, int n,
+      double _Complex (*fnp) (double))
+{
+  int m = n / 2;
+  double min = y[t], max = z[t], med = x[m * d + t];
+  double _Complex result = 0.0;
+
+  if (n == 0)
+    return 0.0;
+
+  if (min > med)
+    result += test (d, (t + 1) % d, x + (m + 1) * d, y, z, n - m - 1, fnp);
+  else if (max < med)
+    result += test (d, (t + 1) % d, x, y, z, m, fnp);
+  else
+    {
+      result += fnp (y[0] + x[m]);
+      result += test (d, (t + 1) % d, x + (m + 1) * d, y, z, n - m - 1, fnp);
+    }
+  return result;
+}
