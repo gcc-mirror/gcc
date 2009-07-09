cb23bb8848e64d200195d6aea59954c8c78eb34e
Dodji Seketeli <dodji@redhat.com>
2009-07-09 18:00:58 +0000

    re PR c++/40684 (ICE in tsubst)
    2009-07-09  Dodji Seketeli  <dodji@redhat.com>
    
    gcc/cp/ChangeLog:
    	PR c++/40684
    	* pt.c (type_unification_real): Use tsubst_template_arg instead
    	of tsubst to substitute default template arguments.
    
    gcc/testsuite/ChangeLog:
    	PR c++/40684
    	* gcc/testsuite/g++.dg/template/unify11.C: New test.
    
    From-SVN: r149425

---
+++ gcc/testsuite/g++.dg/template/unify11.C
@@ -0,0 +1,36 @@
+// Contributed by Dodji Seketeli <dodji@redhat.com>
+// Origin: PR c++/40684
+// { dg-options "-std=c++0x" }
+
+struct A
+{
+};
+
+template <typename S, typename T, typename U, typename S::v = &S::v::s>
+typename S::A
+foo (S c, T t, U u)
+{
+}
+
+struct B
+{
+  struct C
+  {
+    template <typename U>
+    C (U t)
+    {
+      A a;
+      A b = foo (this, a, t); // { dg-error "no matching function" }
+    }
+  } c;
+  B () : c (A ())
+  {
+  }
+};
+
+int
+main ()
+{
+  B f;
+}
+
