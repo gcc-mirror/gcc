116b07221768e5ab5e602b4b3e01c8685e63a451
Jakub Jelinek <jakub@redhat.com>
2009-07-09 22:16:56 +0200

    dwarf2.h (enum dwarf_location_atom): Add DW_OP_implicit_value and DW_OP_stack_value.
    2009-07-09  Jakub Jelinek  <jakub@redhat.com>
    
    	* dwarf2.h (enum dwarf_location_atom): Add DW_OP_implicit_value
    	and DW_OP_stack_value.
    
    From-SVN: r149438

--- include/dwarf2.h
+++ include/dwarf2.h
@@ -548,6 +548,10 @@ enum dwarf_location_atom
     DW_OP_call_frame_cfa = 0x9c,
     DW_OP_bit_piece = 0x9d,
 
+    /* DWARF 4 extensions.  */
+    DW_OP_implicit_value = 0x9e,
+    DW_OP_stack_value = 0x9f,
+
     DW_OP_lo_user = 0xe0,	/* Implementation-defined range start.  */
     DW_OP_hi_user = 0xff,	/* Implementation-defined range end.  */
 
