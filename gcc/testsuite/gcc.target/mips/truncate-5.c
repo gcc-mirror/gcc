85ece28d613b552bc84ca915ea018dde41c0ac02
nemet <nemet@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-09 05:43:56 +0000

    * simplify-rtx.c (simplify_binary_operation_1) <AND>: Transform (and         (truncate)) into (truncate (and)).
    testsuite/
    	* gcc.target/mips/truncate-5.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@149402 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/mips/truncate-5.c
@@ -0,0 +1,14 @@
+/* If we AND in DI mode (i.e. replace the order of TRUNCATE and AND) then we
+   can remove the TRUNCATE.  */
+/* { dg-options "-O -mgp64" } */
+/* { dg-final { scan-assembler-not "\tsll\t\[^\n\]*,0" } } */
+
+struct s
+{
+  unsigned a:5;
+};
+
+f (struct s *s, unsigned long long a)
+{
+  s->a = a & 0x3;
+}
