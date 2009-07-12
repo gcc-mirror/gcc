a8fe6bf41bf0280fc339f79aa0d2073363693fe4
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-12 19:19:03 +0000

    PR c++/40689 	* init.c (build_new_1): Handle initializer list as array initializer. 	(build_vec_init): Likewise. 	* typeck.c (cp_build_modify_expr): Likewise. 	* typeck2.c (process_init_constructor_array): Error rather than abort 	if too many initializers.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@149533 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/initlist20.C
@@ -0,0 +1,17 @@
+// PR c++/40689
+// { dg-options "-std=c++0x" }
+
+class X
+{
+ public:
+  X(): data {1,2,3,4,5} {}
+ private:
+  const short data[5];
+};
+
+int main()
+{
+  const float * pData = new const float[4] { 1.5, 2.5, 3.5, 4.5 };
+
+  return 0;
+}
