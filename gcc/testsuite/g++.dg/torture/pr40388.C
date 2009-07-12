7ca6f6d65a12c6c4a0ba9a2348367a4147abdf2b
Jason Merrill <jason@gcc.gnu.org>
2009-07-12 16:22:36 -0400

    fix double paste
    From-SVN: r149535

--- gcc/testsuite/g++.dg/torture/pr40388.C
+++ gcc/testsuite/g++.dg/torture/pr40388.C
@@ -19,24 +19,3 @@ void bar()
 {
   A a1, a2;
 }
-void foo();
-
-struct A
-{
-  ~A()
-  {
-    try
-    {
-      foo();
-      foo();
-    }
-    catch (...)
-    {
-    }
-  }
-};
-
-void bar()
-{
-  A a1, a2;
-}
