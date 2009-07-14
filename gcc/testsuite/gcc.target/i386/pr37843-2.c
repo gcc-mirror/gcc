28c0ad53b8cb96b3e9c39b2e997730ad5a2bd9af
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-14 15:43:04 +0000

    * gcc.target/i386/pr37843-1.c, gcc.target/i386/pr37843-2.c, 	gcc.target/i386/pr37843-3.c: Allow leading underscore on function 	name.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@149629 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/pr37843-2.c
+++ gcc/testsuite/gcc.target/i386/pr37843-2.c
@@ -2,8 +2,8 @@
 /* { dg-do compile { target nonpic } } */
 /* { dg-options "-O2 -mpreferred-stack-boundary=6 -mincoming-stack-boundary=6" } */
 /* { dg-final { scan-assembler-not "and\[lq\]?\[\\t \]*\\$-64,\[\\t \]*%\[re\]?sp" } } */
-/* { dg-final { scan-assembler-not "call\[\\t \]*foo" } } */
-/* { dg-final { scan-assembler "jmp\[\\t \]*foo" } } */
+/* { dg-final { scan-assembler-not "call\[\\t \]*_?foo" } } */
+/* { dg-final { scan-assembler "jmp\[\\t \]*_?foo" } } */
 
 extern int foo (void);
 
