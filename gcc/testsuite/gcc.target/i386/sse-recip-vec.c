9037ac3703805c6717aed3120d53779a2e5f236c
Uros Bizjak <ubizjak@gmail.com>
2009-07-15 09:01:54 +0200

    sse-recip-vec.c: Move arrays out of test function to enable vectorization.
    	* gcc.target/i386/sse-recip-vec.c: Move arrays out of test
    	function to enable vectorization.
    	* gcc.target/i386/sse2-lrint-vec.c: Ditto.
    	* gcc.target/i386/sse2-lrintf-vec.c: Ditto.
    
    From-SVN: r149659

--- gcc/testsuite/gcc.target/i386/sse-recip-vec.c
+++ gcc/testsuite/gcc.target/i386/sse-recip-vec.c
@@ -7,15 +7,15 @@ extern float sqrtf (float);
 
 #define N 8
 
+float a[N] = { 0.f, 18.f, 108.f, 324.f, 720.f, 1944.f, 3087.f, 5832.f };
+float b[N] = { 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f };
+float r[N];
+
+float rc[N] = { 0.f, 3.f, 6.f, 9.f, 12.f, 18.f, 21.f, 27.f };
+
 static void
 sse_test (void)
 {
-  float a[N] = { 0.f, 18.f, 108.f, 324.f, 720.f, 1944.f, 3087.f, 5832.f };
-  float b[N] = { 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f };
-  float r[N];
-
-  float rc[N] = { 0.f, 3.f, 6.f, 9.f, 12.f, 18.f, 21.f, 27.f };
-
   int i;
 
   for (i = 0; i < N; i++)
