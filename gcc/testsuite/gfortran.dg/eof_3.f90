250c0316773cddc80aa579c2064ea2cf115a8fce
Janne Blomqvist <jb@gcc.gnu.org>
2009-07-20 02:22:37 +0300

    re PR fortran/40714 (Fortran runtime error: Invalid argument)
    2009-07-19  Janne Blomqvist  <jb@gcc.gnu.org>
    	    Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libfortran/40714
    	* gfortran.dg/eof_3.f90: New test.
    
    Co-Authored-By: Jerry DeLisle <jvdelisle@gcc.gnu.org>
    
    From-SVN: r149796

---
+++ gcc/testsuite/gfortran.dg/eof_3.f90
@@ -0,0 +1,8 @@
+! { dg-do run }
+! PR40714 A read hitting EOF should leave the unit structure in a correct state
+program test
+open(unit=32,status="scratch",access="sequential",form="unformatted")
+read(32,end=100)
+100 continue
+write (32)
+end program test
