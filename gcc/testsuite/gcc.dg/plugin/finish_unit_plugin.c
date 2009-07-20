67a2b55a06644eea26249e710fa29677122fe789
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-20 19:28:05 +0000

    ./: 	* toplev.c: Invoke FINISH_UNIT callbacks before call to 	finalize(). testsuite/: 	* gcc.dg/plugin/finish_unit_plugin.c: New test. 	* gcc.dg/plugin/finish_unit-test-1.c: New test. 	* gcc.dg/plugin/plugin.exp: Added finish_unit_plugin.c test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@149833 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/plugin/finish_unit_plugin.c
@@ -0,0 +1,28 @@
+/* This plugin creates a fake function in the FINISH_UNIT callback, in
+ * other words right after compilation of the translation unit. 
+*/
+#include "gcc-plugin.h"
+#include "config.h"
+#include "system.h"
+#include "coretypes.h"
+#include "tm.h"
+#include "toplev.h"
+#include "basic-block.h"
+#include "gimple.h"
+#include "tree.h"
+#include "tree-pass.h"
+#include "intl.h"
+
+int plugin_is_GPL_compatible;
+
+static void finish_unit_callback (void *gcc_data, void *user_data)
+{
+  cgraph_build_static_cdtor ('I', NULL, DEFAULT_INIT_PRIORITY);
+}
+
+int plugin_init (struct plugin_name_args *plugin_info,
+                 struct plugin_gcc_version *version)
+{
+  register_callback ("finish_unit", PLUGIN_FINISH_UNIT, &finish_unit_callback, NULL);
+  return 0;
+}
