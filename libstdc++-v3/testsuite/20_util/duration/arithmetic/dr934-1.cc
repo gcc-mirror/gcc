165dbb3cbbe4d70c07107207facf984feb37f315
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-21 14:48:47 +0000

    2009-07-21  Paolo Carlini  <paolo.carlini@oracle.com>
    	* include/std/chrono (duration<>::operator%=,  operator%):
    	Add, per DR 934.
    	* testsuite/20_util/duration/arithmetic/dr934-1.cc: New.
    	* testsuite/20_util/duration/arithmetic/dr934-2.cc: Likewise.
    
    	* include/std/chrono (operator/): Simplify implementation.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@149856 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/20_util/duration/arithmetic/dr934-1.cc
@@ -0,0 +1,48 @@
+// { dg-do compile }
+// { dg-options "-std=gnu++0x" }
+// { dg-require-cstdint "" }
+
+// Copyright (C) 2009 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <chrono>
+
+class ClockTime
+{
+  typedef std::chrono::hours hours;
+  typedef std::chrono::minutes minutes;
+  typedef std::chrono::seconds seconds;
+
+public:
+  hours hours_;
+  minutes minutes_;
+  seconds seconds_;
+
+  template<typename Rep, typename Period>
+    explicit
+    ClockTime(const std::chrono::duration<Rep, Period>& d)
+    : hours_  (std::chrono::duration_cast<hours>  (d)),
+      minutes_(std::chrono::duration_cast<minutes>(d % hours(1))),
+      seconds_(std::chrono::duration_cast<seconds>(d % minutes(1))) { }
+};
+
+// DR 934.
+void test01()
+{
+  std::chrono::duration<int> d;
+  ClockTime ct(d);
+}
---
+++ libstdc++-v3/testsuite/20_util/duration/arithmetic/dr934-2.cc
@@ -0,0 +1,54 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-require-cstdint "" }
+
+// Copyright (C) 2009 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <chrono>
+#include <testsuite_hooks.h>
+
+// DR 934.
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace std::chrono;
+
+  const duration<int> d0(17);
+  duration<int> d3(d0);
+  d3 %= 5;
+  VERIFY( d3.count() == 2 );
+
+  const duration<int> d4(7);
+  duration<int> d5(d0);
+  d5 %= d4;
+  VERIFY( d5.count() == 3 );
+
+  const duration<int> d6 = d0 % 6;
+  VERIFY( d6.count() == 5 );
+
+  const duration<int> d7(11);
+  const duration<int> d8 = d0 % d7;
+  VERIFY( d8.count() == 6 );
+}
+
+int
+main()
+{
+  test01();
+  return 0;
+}
