567cd5189541199db944bb9a9c09b7522b872508
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-21 14:51:13 +0000

    PR tree-optimization/40813 	* tree-inline.c (copy_bb): Regimplify RHS after last stmt, not before 	it.
    	* g++.dg/opt/inline15.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@149857 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/opt/inline15.C
@@ -0,0 +1,42 @@
+// PR tree-optimization/40813
+// { dg-do compile }
+// { dg-options "-O -fcheck-new" }
+
+typedef __SIZE_TYPE__ size_t;
+typedef void *P;
+struct A;
+struct B
+{
+  void *b[5];
+  A *foo () { return (A *) & b[0]; }
+};
+struct A
+{
+  void *operator new (size_t x, B &y) { return y.foo (); }
+};
+struct C : public A
+{
+  virtual int bar () { }
+};
+struct D : public C
+{
+  static B baz (unsigned *x) { B b; new (b) D (x); return b; }
+  D (unsigned *x) { }
+};
+struct E
+{
+  B e;
+  B fn (unsigned *a) { return D::baz (a); }
+  E (P b, unsigned *a) : e (fn (a)) { }
+};
+
+static unsigned *
+fn2 ()
+{
+}
+
+void
+test (P x)
+{
+  E (x, fn2 ());
+}
