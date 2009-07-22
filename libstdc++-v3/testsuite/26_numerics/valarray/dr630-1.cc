513cdcc1fa080e956d2b9b1ee06cd43954bd29ba
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-22 12:19:58 +0000

    2009-07-22  Paolo Carlini  <paolo.carlini@oracle.com>
    	* include/std/valarray (valarray<>::operator=(const valarray<>&),
    	valarray<>::operator=(initializer_list<>)): Implement resolution
    	of DR 630, [Ready] in Frankfurt.
    	* testsuite/26_numerics/valarray/dr630-1.cc: New.
    	* testsuite/26_numerics/valarray/dr630-2.cc: Likewise.
    	* doc/xml/manual/intro.xml: Add an entry for DR 630.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@149929 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/26_numerics/valarray/dr630-1.cc
@@ -0,0 +1,60 @@
+// Copyright (C) 2009 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <valarray>
+#include <testsuite_hooks.h>
+
+// DR 630.
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace std;
+
+  valarray<int> v1;
+  const valarray<int> v2(-1, 10000);
+
+  v1 = v2;
+  VERIFY( v1.size() == v2.size() );
+  VERIFY( (v1 == v2).min() == true );
+
+  valarray<int> v3(0, 10000);
+  const valarray<int> v4;
+
+  v3 = v4;
+  VERIFY( v3.size() == v4.size() );
+  VERIFY( v3.size() == 0 );
+
+  valarray<int> v5(0, 100);
+  const valarray<int> v6(-1, 10000);
+
+  v5 = v6;
+  VERIFY( v5.size() == v6.size() );
+  VERIFY( (v5 == v6).min() == true );
+
+  valarray<int> v7(0, 10000);
+  const valarray<int> v8(-1, 100);
+
+  v7 = v8;
+  VERIFY( v7.size() == v8.size() );
+  VERIFY( (v7 == v8).min() == true );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/26_numerics/valarray/dr630-2.cc
@@ -0,0 +1,62 @@
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2009 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <valarray>
+#include <testsuite_hooks.h>
+
+// DR 630.
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace std;
+
+  valarray<int> v1;
+
+  v1 = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+  VERIFY( v1.size() == 10 );
+  VERIFY( v1.min() == -1 );
+  VERIFY( v1.max() == -1 );
+
+  valarray<int> v2(0, 10);
+
+  v2 = { -1, -1, -1, -1, -1 };
+  VERIFY( v2.size() == 5 );
+  VERIFY( v2.min() == -1 );
+  VERIFY( v2.max() == -1 );
+
+  valarray<int> v3(0, 5);
+
+  v3 = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+  VERIFY( v3.size() == 10 );
+  VERIFY( v3.min() == -1 );
+  VERIFY( v3.max() == -1 );
+
+  valarray<int> v4(0, 10);
+
+  v4 = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+  VERIFY( v4.size() == 10 );
+  VERIFY( v4.min() == -1 );
+  VERIFY( v4.max() == -1 );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
