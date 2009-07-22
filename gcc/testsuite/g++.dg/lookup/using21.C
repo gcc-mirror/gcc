660c48c497a8e4e98d0430fbe584092e45350968
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-22 08:43:19 +0000

    2009-07-22  Richard Guenther  <rguenther@suse.de>
    	PR c++/40799
    	* cp-gimplify.c (cp_gimplify_expr): Move handling of using
    	related exprs to ...
    	(cp_genericize_r): ... genericization stage.
    	(cp_genericize): Adjust.
    
    	* g++.dg/lookup/using21.C: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@149919 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/lookup/using21.C
@@ -0,0 +1,26 @@
+// PR c++/40799
+
+namespace Bar {
+  typedef int A;
+}
+class CollectionDeleteGuard {
+public:
+  CollectionDeleteGuard(int);
+};
+CollectionDeleteGuard::CollectionDeleteGuard(int)
+{
+  using Bar::A;
+}
+// PR c++/40799
+
+namespace Bar {
+  typedef int A;
+};
+class CollectionDeleteGuard {
+public:
+  CollectionDeleteGuard(int);
+};
+CollectionDeleteGuard::CollectionDeleteGuard(int)
+{
+  using Bar::A;
+}
