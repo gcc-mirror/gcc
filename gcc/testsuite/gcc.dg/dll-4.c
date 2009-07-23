aef96411dda49fce0294a54c5d396b73d4f11857
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-23 22:27:57 +0000

    * gcc.dg/dll-4.c: Allow foo1 and foo2 in either order in 	scan-assembler.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@150031 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/dll-4.c
+++ gcc/testsuite/gcc.dg/dll-4.c
@@ -11,5 +11,6 @@ int foo2 = 5;	/* { dg-warning "redeclared without dllimport" } */
 int f () { return foo1 + foo2; }
 
 /* FIXME: We should scan the output of nm for this case.  */
-/* { dg-final { scan-assembler "(foo2:.*\.comm\[ \t_\]*foo1)" } } */
+/* { dg-final { scan-assembler "(foo2:)" } } */
+/* { dg-final { scan-assembler "(\.comm\[ \t_\]*foo1)" } } */
 /* { dg-final { scan-assembler-not "(__imp_|_imp__)" } } */
