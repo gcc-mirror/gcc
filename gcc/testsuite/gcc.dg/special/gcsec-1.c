c8795808f87d264428a77fccd74ed95a482c7400
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-25 13:45:02 +0000

    * lib/target-supports.exp (check_effective_target_static): New 	procedure. 	* gcc.dg/special/gcsec-1.c (dg-options): Use -static only when 	supported. 	* g++.old-deja/g++.law/weak.C: Require static effective target.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@150080 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/special/gcsec-1.c
+++ gcc/testsuite/gcc.dg/special/gcsec-1.c
@@ -3,10 +3,8 @@
 /* { dg-do run { xfail rs6000-*-aix* powerpc*-*-aix* } } */
 /* { dg-require-gc-sections "" } */
 
-/* { dg-options "-ffunction-sections -fdata-sections -Wl,--gc-sections -static" } */
-/* { dg-options "-ffunction-sections -fdata-sections -Wl,--gc-sections -static" { target native } } */
-/* Solaris 10 does not support static linking; there is no libc.a.  */
-/* { dg-options "-ffunction-sections -fdata-sections -Wl,--gc-sections" { target *-*-netware* i?86-*-solaris2.1[0-9] } } */
+/* { dg-options "-ffunction-sections -fdata-sections -Wl,--gc-sections" } */
+/* { dg-options "-ffunction-sections -fdata-sections -Wl,--gc-sections -static" { target static } } */
 
 #include <stdlib.h>
 
