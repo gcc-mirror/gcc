504ed63a1a4f3f35a5fc774f547e4849f53dc2b4
Tobias Burnus <burnus@net-b.de>
2009-07-25 21:39:07 +0200

    re PR fortran/33197 (Fortran 2008: math functions)
    2009-07-25  Tobias Burnus  <burnus@net-b.de>
                Francois-Xavier Coudert  <fxcoudert@gcc.gnu.org>
    
            PR fortran/33197
            * intrinsic.c (add_functions): Support complex arguments for
            acos,acosh,asin,asinh,atan,atanh.
            * invoke.texi (ACOS,ACOSH,ASIN,ASINH,ATAN,ATANH): Support
            complex arguments.
            * simplify.c (gfc_simplify_acos,gfc_simplify_acosh,
            gfc_simplify_asin,gfc_simplify_asinh,gfc_simplify_atan,
            gfc_simplify_atanh,gfc_simplify_atan,gfc_simplify_asinh,
            gfc_simplify_acosh,gfc_simplify_atanh): Support
            complex arguments.
    
    2009-07-25  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/33197
            * intrinsics/c99_functions.c (cacosf,cacos,cacosl,casinf,
            casin,casind,catanf,catan,catanl,cacoshf,cacosh,cacoshl,
            casinhf,casinh,casinhf,catanhf,catanh,catanhl): New functions.
            * c99_protos.h: Add prototypes for those.
    
    2009-07-25  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/33197
            * gfortran.dg/complex_intrinsic_5.f90: New test.
            * gfortran.dg/complex_intrinsic_7.f90: New test.
    
    
    Co-Authored-By: Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
    
    From-SVN: r150087

---
+++ gcc/testsuite/gfortran.dg/complex_intrinsic_7.f90
@@ -0,0 +1,45 @@
+! { dg-do compile }
+! { dg-require-effective-target mpc }
+! { dg-options "-fdump-tree-original" }
+!
+! PR fortran/33197
+!
+! Fortran 2008 complex trigonometric functions: tan, cosh, sinh, tanh
+!
+! Compile-time simplificiations
+!
+implicit none
+real(4), parameter :: pi  = 2*acos(0.0_4)
+real(8), parameter :: pi8 = 2*acos(0.0_8)
+real(4), parameter :: eps  = 10*epsilon(0.0_4)
+real(8), parameter :: eps8 = 10*epsilon(0.0_8)
+complex(4), parameter :: z0_0  = cmplx(0.0_4, 0.0_4, kind=4)
+complex(4), parameter :: z1_1  = cmplx(1.0_4, 1.0_4, kind=4)
+complex(4), parameter :: zp_p  = cmplx(pi,    pi,    kind=4)
+complex(8), parameter :: z80_0 = cmplx(0.0_8, 0.0_8, kind=8)
+complex(8), parameter :: z81_1 = cmplx(1.0_8, 1.0_8, kind=8)
+complex(8), parameter :: z8p_p = cmplx(pi8,   pi8,   kind=8)
+
+if (abs(tan(z0_0)  - cmplx(0.0,0.0,4)) > eps) call abort()
+if (abs(tan(z1_1)  - cmplx(0.27175257,1.0839232,4)) > eps) call abort()
+if (abs(tan(z80_0) - cmplx(0.0_8,0.0_8,8)) > eps8) call abort()
+if (abs(tan(z81_1) - cmplx(0.27175258531951174_8,1.0839233273386946_8,8)) > eps8) call abort()
+
+if (abs(cosh(z0_0)  - cmplx(1.0,0.0,4)) > eps) call abort()
+if (abs(cosh(z1_1)  - cmplx(0.83372992,0.98889768,4)) > eps) call abort()
+if (abs(cosh(z80_0) - cmplx(1.0_8,0.0_8,8)) > eps8) call abort()
+if (abs(cosh(z81_1) - cmplx(0.83373002513114913_8,0.98889770576286506_8,8)) > eps8) call abort()
+
+if (abs(sinh(z0_0)  - cmplx(0.0,0.0,4)) > eps) call abort()
+if (abs(sinh(z1_1)  - cmplx(0.63496387,1.2984575,4)) > eps) call abort()
+if (abs(sinh(z80_0) - cmplx(0.0_8,0.0_8,8)) > eps8) call abort()
+if (abs(sinh(z81_1) - cmplx(0.63496391478473613_8,1.2984575814159773_8,8)) > eps8) call abort()
+
+if (abs(tanh(z0_0)  - cmplx(0.0,0.0,4)) > eps) call abort()
+if (abs(tanh(z1_1)  - cmplx(1.0839232,0.27175257,4)) > eps) call abort()
+if (abs(tanh(z80_0) - cmplx(0.0_8,0.0_8,8)) > eps8) call abort()
+if (abs(tanh(z81_1) - cmplx(1.0839233273386946_8,0.27175258531951174_8,8)) > eps8) call abort()
+
+end
+! { dg-final { scan-tree-dump-times "abort" 0 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
