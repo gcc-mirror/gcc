0bcc2f25f0f61476d973b0e46ea5268213707a6e
simartin <simartin@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-26 08:16:41 +0000

    gcc/cp/
    2009-07-26  Simon Martin  <simartin@users.sourceforge.net>
    
    	PR c++/40749
    	* decl.c (grokdeclarator): Do not set TREE_NO_WARNING for functions
    	with a qualified return type.
    
    gcc/testsuite/
    
    2007-07-26  Simon Martin  <simartin@users.sourceforge.net>
    
    	PR c++/40749
    	* g++.dg/warn/Wreturn-type-6.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@150097 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/warn/Wreturn-type-6.C
@@ -0,0 +1,13 @@
+/* PR c++/40749 */
+/* { dg-do "compile" } */
+/* { dg-options "-Wreturn-type" } */
+
+struct A {};
+const A a() {} /* { dg-warning "no return statement" } */
+const A& b() {} /* { dg-warning "no return statement" } */
+
+const int c() {} /* { dg-warning "no return statement" } */
+
+template<class T>
+const int foo(T t) {} /* { dg-warning "no return statement" } */
+int d = foo<int>(0), e = foo<int>(1);
