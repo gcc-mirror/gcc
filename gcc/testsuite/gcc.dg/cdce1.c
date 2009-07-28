4981977244c4ae6536d31b3b3616f09b26a74ef7
Jakub Jelinek <jakub@redhat.com>
2009-07-28 18:11:21 +0200

    re PR testsuite/40891 (Revision 150143 failed gcc.dg/cdce[12].c)
    	PR testsuite/40891
    	* gcc.dg/cdce1.c: Adjust note line number.
    	* gcc.dg/cdce2.c: Likewise.
    
    From-SVN: r150164

--- gcc/testsuite/gcc.dg/cdce1.c
+++ gcc/testsuite/gcc.dg/cdce1.c
@@ -2,7 +2,7 @@
 /* { dg-skip-if "doubles are floats" { "avr-*-*" "m32c-*-*" } { "*" } { "" } } */
 /* { dg-options "-O2 -fmath-errno -fdump-tree-cdce-details  -lm" } */
 /* { dg-options "-O2 -fmath-errno -fdump-tree-cdce-details" { target *-*-netware* } } */
-/* { dg-final { scan-tree-dump  "cdce1.c:16: note: function call is shrink-wrapped into error conditions\."  "cdce" } } */
+/* { dg-final { scan-tree-dump  "cdce1.c:17: note: function call is shrink-wrapped into error conditions\."  "cdce" } } */
 /* { dg-final { cleanup-tree-dump "cdce" } } */
 
 #include <stdlib.h>
