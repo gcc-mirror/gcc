032502c26ffec2ca9b3721df0ffd4808bd149e18
Kai Tietz <kai.tietz@onevision.com>
2009-07-28 16:52:21 +0000

    mingw-w64.h (LINK_SPEC): Add separating space between commands.
    2009-07-28  Kai Tietz  <kai.tietz@onevision.com>
    
    	* config/i386/mingw-w64.h (LINK_SPEC): Add
    	separating space between commands.
    
    From-SVN: r150170

--- gcc/config/i386/mingw-w64.h
+++ gcc/config/i386/mingw-w64.h
@@ -56,7 +56,7 @@ along with GCC; see the file COPYING3.  If not see
 #endif
 
 #undef LINK_SPEC
-#define LINK_SPEC SUB_LINK_SPEC "%{mwindows:--subsystem windows} \
+#define LINK_SPEC SUB_LINK_SPEC " %{mwindows:--subsystem windows} \
   %{mconsole:--subsystem console} \
   %{shared: %{mdll: %eshared and mdll are not compatible}} \
   %{shared: --shared} %{mdll:--dll} \
