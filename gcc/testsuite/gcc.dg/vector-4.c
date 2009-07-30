cb5c784dcfce285d74a3d6c22d4e398b240e49f3
John David Anglin <dave.anglin@nrc-cnrc.gc.ca>
2009-07-30 23:12:31 +0000

    vector-4.c: Add -fno-common option on hppa*-*-hpux* (32-bit).
    	* gcc.dg/vector-4.c: Add -fno-common option on hppa*-*-hpux* (32-bit).
    
    From-SVN: r150282

--- gcc/testsuite/gcc.dg/vector-4.c
+++ gcc/testsuite/gcc.dg/vector-4.c
@@ -1,4 +1,5 @@
 /* { dg-do compile } */
+/* { dg-options "-fno-common" { target { { hppa*-*-hpux* } && { ! hppa*64*-*-* } } } } */
 #define vector __attribute__((vector_size(4*sizeof(int)) ))
 
 vector int a, b, c;
