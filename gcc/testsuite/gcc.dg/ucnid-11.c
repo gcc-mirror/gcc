74136246bd972f376132651f85617155c319e79e
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-30 22:56:14 +0000

    * gcc.dg/ucnid-11.c: Skip on hppa*-*-hpux* (32-bit). 	* gcc.dg/ucnid-12.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@150281 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/ucnid-11.c
+++ gcc/testsuite/gcc.dg/ucnid-11.c
@@ -1,5 +1,6 @@
 /* { dg-do run } */
 /* { dg-xfail-if "" { powerpc-ibm-aix* *-*-solaris2.* } { "*" } { "" } } */
+/* { dg-skip-if "-fdata-sections not supported" { { hppa*-*-hpux* } && { ! hppa*64*-*-* } } { "*" } { "" } } */
 /* { dg-options "-std=c99 -fextended-identifiers -fdata-sections" } */
 
 #include "ucnid-3.c"
--- gcc/testsuite/gcc.dg/ucnid-12.c
+++ gcc/testsuite/gcc.dg/ucnid-12.c
@@ -1,5 +1,6 @@
 /* { dg-do run } */
 /* { dg-xfail-if "" { powerpc-ibm-aix* *-*-solaris2.* } { "*" } { "" } } */
+/* { dg-skip-if "-ffunction-sections not supported" { { hppa*-*-hpux* } && { ! hppa*64*-*-* } } { "*" } { "" } } */
 /* { dg-options "-std=c99 -fextended-identifiers -ffunction-sections" } */
 
 #include "ucnid-4.c"
