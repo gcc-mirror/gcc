657e81c74ffdd2917174e326f0facc996dfbfa1d
Adam Nemet <anemet@caviumnetworks.com>
2009-08-02 06:45:33 +0000

    ext-3.c: Add NOMIPS16.
    	* gcc.target/mips/ext-3.c: Add NOMIPS16.
    	* gcc.target/mips/ext-4.c: Likewise.
    	* gcc.target/mips/interrupt_handler.c: Likewise.
    	* gcc.target/mips/truncate-4.c: Likewise.
    	* gcc.target/mips/truncate-5.c: Likewise.
    
    From-SVN: r150345

--- gcc/testsuite/gcc.target/mips/interrupt_handler.c
+++ gcc/testsuite/gcc.target/mips/interrupt_handler.c
@@ -4,20 +4,20 @@
 
 void f () { }
 
-void __attribute__ ((interrupt)) v0 () { }
-void __attribute__ ((interrupt, use_shadow_register_set)) v1 () { }
-void __attribute__ ((interrupt, keep_interrupts_masked)) v2 () { }
-void __attribute__ ((interrupt, use_debug_exception_return)) v3 () { }
-void __attribute__ ((interrupt, use_shadow_register_set, keep_interrupts_masked)) v4 () { }
-void __attribute__ ((interrupt, use_shadow_register_set, use_debug_exception_return)) v5 () { }
-void __attribute__ ((interrupt, keep_interrupts_masked, use_debug_exception_return)) v6 () { }
-void __attribute__ ((interrupt, use_shadow_register_set, keep_interrupts_masked, use_debug_exception_return)) v7 () { }
+NOMIPS16 void __attribute__ ((interrupt)) v0 () { }
+NOMIPS16 void __attribute__ ((interrupt, use_shadow_register_set)) v1 () { }
+NOMIPS16 void __attribute__ ((interrupt, keep_interrupts_masked)) v2 () { }
+NOMIPS16 void __attribute__ ((interrupt, use_debug_exception_return)) v3 () { }
+NOMIPS16 void __attribute__ ((interrupt, use_shadow_register_set, keep_interrupts_masked)) v4 () { }
+NOMIPS16 void __attribute__ ((interrupt, use_shadow_register_set, use_debug_exception_return)) v5 () { }
+NOMIPS16 void __attribute__ ((interrupt, keep_interrupts_masked, use_debug_exception_return)) v6 () { }
+NOMIPS16 void __attribute__ ((interrupt, use_shadow_register_set, keep_interrupts_masked, use_debug_exception_return)) v7 () { }
 
-void __attribute__ ((interrupt)) w0 () { t(); }
-void __attribute__ ((interrupt, use_shadow_register_set)) w1 () { t(); }
-void __attribute__ ((interrupt, keep_interrupts_masked)) w2 () { t(); }
-void __attribute__ ((interrupt, use_debug_exception_return)) w3 () { t(); }
-void __attribute__ ((interrupt, use_shadow_register_set, keep_interrupts_masked)) w4 () { t(); }
-void __attribute__ ((interrupt, use_shadow_register_set, use_debug_exception_return)) w5 () { t(); }
-void __attribute__ ((interrupt, keep_interrupts_masked, use_debug_exception_return)) w6 () { t(); }
-void __attribute__ ((interrupt, use_shadow_register_set, keep_interrupts_masked, use_debug_exception_return)) w7 () { t(); }
+NOMIPS16 void __attribute__ ((interrupt)) w0 () { t(); }
+NOMIPS16 void __attribute__ ((interrupt, use_shadow_register_set)) w1 () { t(); }
+NOMIPS16 void __attribute__ ((interrupt, keep_interrupts_masked)) w2 () { t(); }
+NOMIPS16 void __attribute__ ((interrupt, use_debug_exception_return)) w3 () { t(); }
+NOMIPS16 void __attribute__ ((interrupt, use_shadow_register_set, keep_interrupts_masked)) w4 () { t(); }
+NOMIPS16 void __attribute__ ((interrupt, use_shadow_register_set, use_debug_exception_return)) w5 () { t(); }
+NOMIPS16 void __attribute__ ((interrupt, keep_interrupts_masked, use_debug_exception_return)) w6 () { t(); }
+NOMIPS16 void __attribute__ ((interrupt, use_shadow_register_set, keep_interrupts_masked, use_debug_exception_return)) w7 () { t(); }
