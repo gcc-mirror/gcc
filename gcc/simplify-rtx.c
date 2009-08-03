808deb18a4c9ee11f46d24926e464ae2c9cd5401
Janis Johnson <janis187@us.ibm.com>
2009-08-03 21:43:32 +0000

    re PR c/39902 (x * 1.0DF gets wrong value)
    	PR c/39902
    	* simplify-rtx.c (simplify_binary_operation_1): Disable
    	simplifications for decimal float operations.
    	* gcc.target/powerpc/pr39902-2.c: New test.
    
    From-SVN: r150384

--- gcc/simplify-rtx.c
+++ gcc/simplify-rtx.c
@@ -2009,6 +2009,7 @@ simplify_binary_operation_1 (enum rtx_code code, enum machine_mode mode,
       /* x*2 is x+x and x*(-1) is -x */
       if (GET_CODE (trueop1) == CONST_DOUBLE
 	  && SCALAR_FLOAT_MODE_P (GET_MODE (trueop1))
+	  && !DECIMAL_FLOAT_MODE_P (GET_MODE (trueop1))
 	  && GET_MODE (op0) == mode)
 	{
 	  REAL_VALUE_TYPE d;
