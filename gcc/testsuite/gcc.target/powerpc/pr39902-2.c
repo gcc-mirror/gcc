50cd60be11a52f6ee997d331f83c5b62ee59f448
Janis Johnson <janis187@us.ibm.com>
2009-08-03 21:38:53 +0000

    re PR c/39902 (x * 1.0DF gets wrong value)
    	PR c/39902
    	* simplify-rtx.c (simplify_binary_operation_1): Disable
    	simplifications for decimal float operations.
    
    	PR c/39902
    	* gcc.target/powerpc/pr39902-2.c: New test.
    
    From-SVN: r150383

---
+++ gcc/testsuite/gcc.target/powerpc/pr39902-2.c
@@ -0,0 +1,28 @@
+/* Check that simplification "x*(-1)" -> "-x" is not performed for decimal
+   float types.  */
+
+/* { dg-do compile { target { powerpc*-*-linux* && powerpc_fprs } } } */
+/* { dg-options "-std=gnu99 -O -mcpu=power6" } */
+/* { dg-final { scan-assembler-not "fneg" } } */
+
+extern _Decimal32 a32, b32;
+extern _Decimal64 a64, b64;
+extern _Decimal128 a128, b128;
+
+void
+foo32 (void)
+{
+  b32 = a32 * -1.0DF;
+}
+
+void
+foo64 (void)
+{
+  b64 = a64 * -1.0DD;
+}
+
+void
+foo128 (void)
+{
+  b128 = a128 * -1.0DL;
+}
