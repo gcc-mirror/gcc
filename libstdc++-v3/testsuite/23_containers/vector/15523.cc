6eb126395c70fa5164769f46cbe16c13125cf6ee
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-08-04 13:01:08 +0000

    2009-08-04  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR libstdc++/15523
    	* include/debug/safe_iterator.h (_Safe_iterator<>::
    	_Safe_iterator(const _Safe_iterator&), _Safe_iterator<>::
    	operator=(const _Safe_iterator&)): Implement resolution of DR 408,
    	do not error out when the source is a value-initialized iterator.
    	* testsuite/23_containers/vector/15523.cc: New.
    	* doc/xml/manual/intro.xml: Add an entry for DR 408.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@150455 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/23_containers/vector/15523.cc
@@ -0,0 +1,39 @@
+// Copyright (C) 2009 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-D_GLIBCXX_DEBUG" }
+
+#include <vector> 
+
+// libstdc++/15523
+void test01() 
+{
+  using namespace std;
+
+  vector<vector<int>::const_iterator> x(2); 
+
+  vector<int>::iterator i2, i3;
+  vector<int>::const_iterator ci1(i2);
+
+  i2 = i3;
+} 
+
+int main()
+{
+  test01();
+  return 0;
+}
