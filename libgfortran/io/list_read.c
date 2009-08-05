451a2c55e3ff087cf397feb010a450fd620d9e99
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2009-08-05 03:15:18 +0000

    re PR fortran/40853 (I/O: Namelist read error)
    2009-08-04  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libfortran/40853
    	* io/list_read.c (nml_get_obj_data): Do not set nl
    	pointer to first_nl if nl->next is NULL.
    
    From-SVN: r150476

--- libgfortran/io/list_read.c
+++ libgfortran/io/list_read.c
@@ -2772,7 +2772,7 @@ get_name:
 
   if (nl->type == GFC_DTYPE_DERIVED)
     nml_touch_nodes (nl);
-  if (component_flag && nl->var_rank > 0)
+  if (component_flag && nl->var_rank > 0 && nl->next)
     nl = first_nl;
 
   /* Make sure no extraneous qualifiers are there.  */
