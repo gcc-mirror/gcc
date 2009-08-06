6f3cfaa30d80b3843fa6fbbac41d359916897c3a
jamborm <jamborm@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-08-06 11:55:30 +0000

    2009-08-06  Martin Jambor  <mjambor@suse.cz>
    	PR middle-end/32964
    	* testsuite/gcc.dg/tree-ssa/pr32964.c: New test.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@150523 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr32964.c
@@ -0,0 +1,26 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-optimized" } */
+
+union A
+{
+ float a;
+};
+
+float t(float a)
+{
+  union A a1, a2, a3;
+  int i;
+
+  a1.a = a;
+  for(i = 0; i<100; i++)
+    {
+      a2 = a1;
+      a2.a += a;
+      a1 = a2;
+  }
+  a3 = a1;
+  return a3.a;
+}
+
+/* { dg-final { scan-tree-dump-times "union" 0 "optimized"} } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
