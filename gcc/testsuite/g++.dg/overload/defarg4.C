7553122861e1a5d694adbefbbbbfe2a479f91171
Dodji Seketeli <dodji@gcc.gnu.org>
2009-08-07 15:13:12 +0200

    re PR c++/39987 (Rejects default argument that is a template via access failure)
    2009-08-07  Dodji Seketeli  <dodji@redhat.com>
    
    	PR c++/39987
    	* gcc/testsuite/g++.dg/overload/defarg4.C: Oops, forgot to add
    	the testcase.
    
    From-SVN: r150568

---
+++ gcc/testsuite/g++.dg/overload/defarg4.C
@@ -0,0 +1,26 @@
+// Contributed by Dodji Seketeli <dodji@redhat.com>
+// Origin PR c++/39987
+// { dg-do "compile" }
+
+class foo
+{
+ template<typename U>
+ static bool func(const U& x)
+ {}
+public:
+ template<typename U>
+ unsigned int Find(const U& x, bool (*pFunc) (const U&) = func) const
+ {}
+};
+
+class bar {
+ bool Initialize();
+protected:
+ foo b;
+};
+
+bool bar::Initialize()
+{
+        b.Find(b);
+}
+
