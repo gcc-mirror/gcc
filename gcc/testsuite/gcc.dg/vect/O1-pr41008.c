a18d432721f3762fca7dc8b98c7b497adea649e2
irar <irar@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-08-09 12:13:19 +0000

    PR tree-optimization/41008 	* tree-vect-loop.c (vect_is_simple_reduction): Get operands 	from condition only in case it's a comparison. Adjust checks.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@150591 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/O1-pr41008.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+
+double heating[2][2];
+
+void foo (int, int);
+
+void map_do()
+{
+  int jsav, ksav, k, j;
+
+  for(k = 0; k < 2; k++)
+    for(j = 0; j < 2; j++)
+      if (heating[k][j] > 0.)
+        {
+          jsav = j;
+          ksav = k;
+        }
+
+  foo (jsav, ksav);
+}
+
+/* { dg-final { cleanup-tree-dump "vect" } } */
+
