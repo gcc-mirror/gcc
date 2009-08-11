cf046737dfbfc289c2c94e10b3d7dd9546d6431d
janus <janus@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-08-11 20:08:35 +0000

    2009-08-11  Janus Weil  <janus@gcc.gnu.org>
    	PR fortran/41022
    	* trans-expr.c (gfc_conv_procedure_call): Handle procedure pointer
    	components as actual arguments.
    
    2009-08-11  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/41022
    	* gfortran.dg/proc_ptr_comp_14.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@150665 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/proc_ptr_comp_14.f90
@@ -0,0 +1,40 @@
+! { dg-do run }
+!
+! PR 41022: [F03] procedure pointer components as actual arguments
+!
+! Contributed by Juergen Reuter <reuter@physik.uni-freiburg.de>
+
+program foo
+
+   type :: container_t
+      procedure(proc), nopass, pointer :: proc => null ()
+   end type container_t
+
+   type(container_t), target :: obj1
+   type(container_t) :: obj2
+
+   obj1%proc => proc
+   call transfer_proc_ptr (obj2, obj1)
+
+   if (obj2%proc()/=7) call abort()
+
+contains
+
+   subroutine transfer_proc_ptr (obj2, obj1)
+     type(container_t), intent(out) :: obj2
+     type(container_t), intent(in), target :: obj1
+     call assign_proc_ptr (obj2%proc, obj1)
+   end subroutine transfer_proc_ptr
+
+   subroutine assign_proc_ptr (ptr, obj1)
+     procedure(proc), pointer :: ptr
+     type(container_t), intent(in), target :: obj1
+     ptr => obj1%proc
+   end subroutine assign_proc_ptr
+
+   integer function proc ()
+      proc = 7
+   end function
+
+end program foo
+
