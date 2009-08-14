3f75cac2238688b5654cb70c046ae2c1711a0066
janus <janus@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-08-14 22:02:45 +0000

    2009-08-14  Janus Weil  <janus@gcc.gnu.org>
    	PR fortran/41070
    	* resolve.c (resolve_structure_cons): Make sure that ts.u.derived is
    	only used if type is BT_DERIVED.
    
    2009-08-14  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/41070
    	* gfortran.dg/structure_constructor_10.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@150781 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/structure_constructor_10.f90
@@ -0,0 +1,28 @@
+! { dg-do compile }
+!
+! PR 41070: [4.5 Regression] Error: Components of structure constructor '' at (1) are PRIVATE
+!
+! Contributed by Michael Richmond <michael.a.richmond@nasa.gov>
+
+MODULE cdf_aux_mod
+IMPLICIT NONE
+
+TYPE :: one_parameter
+  CHARACTER (8) :: name
+END TYPE one_parameter
+
+TYPE :: the_distribution
+  CHARACTER (8) :: name
+END TYPE the_distribution
+
+TYPE (the_distribution), PARAMETER :: the_beta = the_distribution('cdf_beta')
+END MODULE cdf_aux_mod
+
+SUBROUTINE cdf_beta()
+  USE cdf_aux_mod
+  IMPLICIT NONE
+  CALL check_complements(the_beta%name)
+END SUBROUTINE cdf_beta
+
+! { dg-final { cleanup-modules "cdf_aux_mod" } }
+
