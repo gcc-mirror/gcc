408cd90fbdd7fbd5a2f1838d9319f1d394b3de50
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-08-14 02:35:32 +0000

    2009-08-13  Jerry DeLisle <jvdelisle@gcc.gnu.org>
    	PR fortran/40955
    	* gfortran.dg/winapi.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@150733 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/winapi.f90
@@ -0,0 +1,23 @@
+! { dg-do run { target *-*-cygwin* *-*-mingw* } }
+! { dg-options "-lkernel32" }
+! Test case provided by Dennis Wassel.
+
+PROGRAM winapi
+
+  USE, INTRINSIC :: iso_c_binding
+  IMPLICIT NONE
+
+  INTERFACE
+     ! Specifically select the lstrlenA version for ASCII.
+     FUNCTION lstrlen(string) BIND(C, name = "lstrlenA")
+       USE, INTRINSIC :: iso_c_binding
+       IMPLICIT NONE
+       !GCC$ ATTRIBUTES STDCALL :: lstrlen
+       INTEGER (C_INT)          :: lstrlen
+       CHARACTER(KIND=C_CHAR), INTENT(in) :: string(*)
+     END FUNCTION lstrlen
+  END INTERFACE
+  
+  IF (lstrlen(C_CHAR_"winapi"//C_NULL_CHAR) /= 6) CALL abort()
+
+END PROGRAM winapi
