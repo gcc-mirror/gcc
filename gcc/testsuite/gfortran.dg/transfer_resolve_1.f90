76d18abecd34999180b45300226a1f25d3a7b35b
Paul Thomas <pault@gcc.gnu.org>
2009-08-16 21:01:50 +0000

    re PR fortran/40847 (segfault & bogus warning)
    2009-08-16  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/40847
    	* iresolve.c (gfc_resolve_transfer): Correct error in 'mold'
    	character length for case where length expresson is NULL.
    
    2009-08-16  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/40847
    	* gfortran.dg/transfer_resolve_1.f90 : New test.
    
    From-SVN: r150815

---
+++ gcc/testsuite/gfortran.dg/transfer_resolve_1.f90
@@ -0,0 +1,21 @@
+! { dg-do run }
+! PR40847 - an error in gfc_resolve_transfer caused the character length
+! of 'mold' to be set incorrectly.
+!
+! Contributed by Joost VandeVondele <jv244@cam.ac.uk>
+!
+program test_elemental
+
+if (any (transfer_size((/0.,0./),(/'a','b'/)) .ne. [4 ,4])) call abort
+
+contains
+
+   elemental function transfer_size (source, mold)
+     real, intent(in)         :: source
+     character(*), intent(in) :: mold
+     integer                  :: transfer_size
+     transfer_size = SIZE(TRANSFER(source, (/mold/)))
+     return
+   end function transfer_size
+
+end program test_elemental
