7c12760fa3f5601da164b1ef43a3cf4e11ad1dd5
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-08-17 19:21:38 +0000

    2009-08-17  Benjamin Kosnik  <bkoz@redhat.com>
    	* testsuite/30_threads/packaged_task/requirements/typedefs.cc: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@150857 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/30_threads/packaged_task/requirements/typedefs.cc
@@ -0,0 +1,28 @@
+// { dg-do compile }
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2009 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <future>
+
+void test01()
+{
+  // Check for required typedefs
+  typedef std::packaged_task<int()>	test_type;
+  typedef test_type::result_type	result_type;
+}
