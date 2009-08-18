b75755d741603b06742e27c255a52eaf85be61a3
janus <janus@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-08-18 14:23:35 +0000

    2009-08-18  Janus Weil  <janus@gcc.gnu.org> 	    Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/40870
    	* trans-types.c (gfc_get_ppc_type): Include formal args in backend_decl
    	using the interface symbol. Character types are returned by reference.
    	(gfc_get_derived_type): Prevent infinite recursion loop
    	if a PPC has a derived-type formal arg.
    
    2009-08-18  Janus Weil  <janus@gcc.gnu.org>
    	    Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/40870
    	* gfortran.dg/proc_ptr_comp_13.f90: Extended.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@150875 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/proc_ptr_comp_13.f90
+++ gcc/testsuite/gfortran.dg/proc_ptr_comp_13.f90
@@ -1,6 +1,7 @@
 ! { dg-do run }
 !
-! PR 40882: [F03] infinite recursion in gfc_get_derived_type with PPC returning derived type
+! PR 40882: [F03] infinite recursion in gfc_get_derived_type with PPC returning derived type.
+! At the same time, check that a formal argument does not cause infinite recursion (PR 40870).
 !
 ! Contributed by Janus Weil <janus@gcc.gnu.org>
 
@@ -9,6 +10,7 @@ implicit none
 type :: t
   integer :: data
   procedure(foo), pointer, nopass :: ppc
+  procedure(type(t)), pointer, nopass :: ppc2
 end type
 
 type(t) :: o,o2
@@ -16,7 +18,7 @@ type(t) :: o,o2
 o%data = 1
 o%ppc => foo
 
-o2 = o%ppc()
+o2 = o%ppc(o)
 
 if (o%data /= 1) call abort()
 if (o2%data /= 5) call abort()
@@ -25,9 +27,9 @@ if (associated(o2%ppc)) call abort()
 
 contains
 
-  function foo()
-    type(t) :: foo
-    foo%data = 5
+  function foo(arg)
+    type(t) :: foo, arg
+    foo%data = arg%data * 5
     foo%ppc => NULL()
   end function
 
