8c40d061eca6c784a919c666e363cac4116874cc
Uros Bizjak <uros@gcc.gnu.org>
2009-08-23 11:46:00 +0200

    re PR target/40718 (Invalid code produced with -foptimize-sibling-calls)
    	PR target/40718
    	* config/i386/i386.c (*call_pop_1): Disable for sibling calls.
    	(*sibcall_pop_1): New insn pattern.
    
    testsuite/ChangeLog:
    
    	PR target/40718
    	* gcc.target/i386/pr40718.c: New test.
    
    From-SVN: r151028

---
+++ gcc/testsuite/gcc.target/i386/pr40718.c
@@ -0,0 +1,36 @@
+/* { dg-do run } */
+/* { dg-require-effective-target ilp32 } */
+/* { dg-options "-O1 -foptimize-sibling-calls" } */
+
+void abort (void);
+
+struct S
+{
+  void (__attribute__((__stdcall__)) *f) (struct S *);
+  int i;
+};
+
+void __attribute__((__stdcall__))
+foo (struct S *s)
+{
+  s->i++;
+}
+
+void __attribute__((__stdcall__))
+bar (struct S *s)
+{
+  foo(s);
+  s->f(s);
+}
+
+int main (void)
+{
+  struct S s = { foo, 0 };
+
+  bar (&s);
+  if (s.i != 2)
+    abort ();
+
+  return 0;
+}
+
