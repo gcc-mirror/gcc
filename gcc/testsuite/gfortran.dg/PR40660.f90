033e7d21e5c989d3b0c3e6c42eb050f22f98d394
Aldy Hernandez <aldyh@gcc.gnu.org>
2009-08-25 00:27:52 +0000

    re PR debug/40660 (Weird break points with 4.5, works with 4.4)
    fortran/
            PR fortran/40660
            * trans-io.c (build_dt): Pass UNKNOWN_LOCATION to build_call_expr_loc.
            (transfer_array_desc): Same.
    
    From-SVN: r151069

---
+++ gcc/testsuite/gfortran.dg/PR40660.f90
@@ -0,0 +1,15 @@
+! { dg-do compile }
+! { dg-options "-fdump-tree-original-lineno" }
+!
+! PR fortran/40660
+
+PROGRAM test
+  INTEGER, DIMENSION(3) :: a1,a2
+  a1 = 1
+  PRINT*, a1
+  a2 = 2
+end program test
+
+! { dg-final { scan-tree-dump-times ": 3\] _gfortran" 0 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
+
