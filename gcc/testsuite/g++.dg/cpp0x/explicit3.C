1dad57e65d9539eccc652dbb42b5574db72bb3fc
Jason Merrill <jason@redhat.com>
2009-08-26 00:35:26 -0400

    call.c (build_builtin_candidate): Don't set LOOKUP_ONLYCONVERTING if we're contextually converting to bool.
    	* call.c (build_builtin_candidate): Don't set LOOKUP_ONLYCONVERTING
    	if we're contextually converting to bool.
    	(build_conditional_expr): Likewise.
    	* typeck.c (condition_conversion): Likewise.
    
    From-SVN: r151114

---
+++ gcc/testsuite/g++.dg/cpp0x/explicit3.C
@@ -0,0 +1,50 @@
+// Test for "contextually converted to bool"
+// { dg-options "-std=c++0x" }
+
+struct A
+{
+  explicit operator bool();
+};
+
+void f (bool);
+
+struct B
+{
+  bool b;
+};
+
+struct C
+{
+  operator int();
+};
+
+struct D
+{
+  operator int();
+};
+
+int main()
+{
+  A a; C c; D d;
+  // These contexts use an explicit bool conversion.
+  if (a) {}
+  for (; a; ) {}
+  do {} while (a);
+  while (a) {}
+  a ? 1 : 0;
+  a || true;
+  a && true;
+  !a;
+
+  a ? c : 1;
+  a ? c : d;
+
+  // These do not.
+  switch (a); 			// { dg-error "" }
+  bool b = a;			// { dg-error "" }
+  f(a);				// { dg-error "" }
+  B b2 = { a };			// { dg-error "" }
+  a + true;			// { dg-message "" }
+  b ? a : true;			// { dg-message "" }
+  a ? a : true;			// { dg-message "" }
+}
