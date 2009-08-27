8c5b0c2b1bd816d3629294eacf1b6e624988e1ac
dodji <dodji@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-08-27 07:05:24 +0000

    Fix for PR debug/41170
    gcc/ChangeLog:
    	PR debug/41770
    	* dwarf2out.c (get_context_die): Declare this static function.
    	(gen_type_die_with_usage): Make sure a DIE is a generated for
    	the context of a typedef.
    
    gcc/testsuite/ChangeLog:
    	PR debug/41770
    	* debug/dwarf2/namespace-1.C: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@151138 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/debug/dwarf2/namespace-1.C
@@ -0,0 +1,30 @@
+// Contributed by Dodji Seketeli <dodji@redhat.com>
+// Origin PR debug/41170
+// { dg-options "-g -dA" }
+//
+// We want to test that there is a DW_TAG_namespace DIE DW_AT_name is set
+// to "not_emitted". That namespace die has a child DW_TAG_typedef DIE
+// which DW_AT_name is the null terminated string "T".
+// { dg-final { scan-assembler-times "DIE +\\(.*?\\) DW_TAG_namespace" 1 } }
+// { dg-final { scan-assembler-times "DW_AT_name: \"not_emitted\"" 1 } }
+// { dg-final { scan-assembler-times "DIE +\\(.*?\\) DW_TAG_typedef" 1 } }
+// { dg-final { scan-assembler-times "\.ascii \"T.0\"\[\t \]+# DW_AT_name" 1 } }
+
+struct strukt
+{
+  int m;
+};
+
+namespace not_emitted
+{
+  typedef strukt T;
+}
+
+int
+main()
+{
+  not_emitted::T t;
+  t.m = 0;
+  return 0;
+}
+
