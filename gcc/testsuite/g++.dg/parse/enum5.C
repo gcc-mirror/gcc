e72cb4332ac94ca1067b554ab6a05b5c650e2fce
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-08-31 21:08:33 +0000

    PR c++/41127 	* parser.c (cp_parser_enum_specifier): Make sure the : is followed by a 	type-specifier-seq before we commit.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@151246 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/parse/enum5.C
@@ -0,0 +1,17 @@
+// PR c++/41127
+
+#define CHAR_BIT 8
+enum EE {ee};
+typedef unsigned int T;
+
+struct D {
+        T : sizeof(unsigned int) * CHAR_BIT; // OK
+        EE : sizeof(EE) * CHAR_BIT; // OK
+        enum EE : sizeof(EE) * CHAR_BIT; // not OK
+        enum EE xxxx : sizeof(EE) * CHAR_BIT; // OK
+        T x : sizeof(unsigned int) * CHAR_BIT; // OK
+        enum FF {ff} : sizeof(int) * CHAR_BIT; // OK
+} element;
+
+enum EE xx;
+EE yy;
