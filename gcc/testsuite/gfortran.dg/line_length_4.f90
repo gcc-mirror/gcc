2b458d3eb5119cbe9f3ed967d5c11193be59eb89
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-09-02 01:03:34 +0000

    2009-09-01  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	PR fortran/39229
    	* gfortran.dg/line_length_3.f: New test.
    	* gfortran.dg/line_length_4.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@151309 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/line_length_4.f90
@@ -0,0 +1,18 @@
+! { dg-do compile }
+! { dg-options "-Wline-truncation -ffree-line-length-80" }
+! PR39229 No warning of truncated lines if a continuation line follows 
+  implicit none
+  call foo([11, 22, 33, 44, 55, 66, 770, 9900, 1100, 1100, 120],11,'hello') !no warn
+
+  print *, min(35 &
+   &  ,                        25  ), " Explanation ! "                         warn
+  contains
+   subroutine foo(a,n,s)
+     integer :: a(*), n, i
+     character(len=*) :: s
+     do i = 1, n
+       print *, s, a(i)
+     end do
+   end subroutine foo
+  end
+! { dg-warning "Line truncated" " " { target *-*-* } 8 }
