a30d561cee63aad33852148e3b069e1dea1d1bb9
Ian Lance Taylor <iant@google.com>
2009-09-02 18:47:29 +0000

    macro.c (stringify_arg): Escape CPP_WCHAR tokens.
    libcpp/:
    	* macro.c (stringify_arg): Escape CPP_WCHAR tokens.
    gcc/testsuite/:
    	* gcc.dg/20090902-1.c: New test.
    
    From-SVN: r151347

--- libcpp/macro.c
+++ libcpp/macro.c
@@ -375,7 +375,7 @@ stringify_arg (cpp_reader *pfile, macro_arg *arg)
 	}
 
       escape_it = (token->type == CPP_STRING || token->type == CPP_CHAR
-		   || token->type == CPP_WSTRING || token->type == CPP_STRING
+		   || token->type == CPP_WSTRING || token->type == CPP_WCHAR
 		   || token->type == CPP_STRING32 || token->type == CPP_CHAR32
 		   || token->type == CPP_STRING16 || token->type == CPP_CHAR16);
 
