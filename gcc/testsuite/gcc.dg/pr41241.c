c189c8e3ff356ca9a1a1fd2c8c7d78e8aa6df3d7
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-09-06 19:31:55 +0000

    PR bootstrap/41241 	* combine-stack-adj.c (struct csa_memlist): Rename to... 	(struct csa_reflist): ... this.  Rename mem field to ref. 	(free_csa_memlist): Rename to... 	(free_csa_reflist): ... this. 	(record_one_stack_memref): Rename to... 	(record_one_stack_ref): ... this.  Handle also REG_P. 	(try_apply_stack_adjustment): Handle also REG_P. 	(struct record_stack_memrefs_data): Rename to... 	(struct record_stack_refs_data): ... this.  Rename memlist field to 	reflist. 	(record_stack_memrefs): Rename to... 	(record_stack_refs): ... this.  For DEBUG_INSNs keep traversing 	subexpressions instead of failing when a MEM contains SP references. 	For SP itself in DEBUG_INSNs queue it also onto reflist chain. 	(combine_stack_adjustments_for_block): Adjust for mem to ref renaming.
    	* gcc.dg/pr41241.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@151462 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr41241.c
@@ -0,0 +1,34 @@
+/* PR bootstrap/41241 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fcompare-debug -g" } */
+/* { dg-options "-O2 -fcompare-debug -g -march=i586 -mtune=i586 -fomit-frame-pointer" { target { { i?86-*-* x86_64-*-* } && ilp32 } } } */
+
+typedef struct { int t1; int t2; int t3; } *T;
+typedef struct { int s; } S;
+
+int fn1 (int);
+int fn2 (int, int, int);
+int fn3 (void);
+
+static S
+bar ()
+{
+  S s = { 0 };
+  return s;
+}
+
+void
+foo (T x)
+{
+  int a, b, c, d, e;
+  T f, g;
+  S h;
+  a = x->t2;
+  fn1 (x->t1);
+  h = bar (b);
+  c = fn1 (e);
+  d = fn3 ();
+  f->t3 &= g->t3 |= fn2 (0, b, x->t1);
+  fn2 (0, c, d);
+  fn2 (0, e, 0);
+}
