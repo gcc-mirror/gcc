3c45b96b27d5be00740b13ad80ae6c6be9c96dbc
Richard Guenther <rguenther@suse.de>
2009-09-06 16:48:41 +0000

    re PR middle-end/41261 (ice with -O2 -fprofile-arcs when compiling Linux kernel)
    2009-09-06  Richard Guenther  <rguenther@suse.de>
    
    	PR middle-end/41261
    	* tree-ssa-alias.c (refs_may_alias_p_1): Bail out for function decls.
    
    	* gcc.dg/torture/pr41261.c: New testcase.
    
    From-SVN: r151460

---
+++ gcc/testsuite/gcc.dg/torture/pr41261.c
@@ -0,0 +1,8 @@
+/* { dg-do compile } */
+/* { dg-options "-fprofile-arcs" } */
+
+extern void relocate_kernel();
+void machine_kexec(void *control_page)
+{ 
+  __builtin_memcpy(control_page, relocate_kernel, 2048);
+}
