475a53093ece891ea01afe994707164c509e8d7c
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-09-08 22:31:13 +0000

    * gcc.dg/profile-dir-1.c, gcc.dg/profile-dir-2.c, 	gcc.dg/profile-dir-3.c: Disable for remote-host testing.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@151537 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/profile-dir-3.c
+++ gcc/testsuite/gcc.dg/profile-dir-3.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-O -fprofile-generate -fprofile-dir=. -fdump-tree-tree_profile" } */
+/* { dg-require-host-local "" } */
 /* { dg-final { scan-tree-dump " ./profile-dir-3.gcda" "tree_profile" } } */
 
 int
