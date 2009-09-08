7bb8b5ca1874f79103dd9c8943b7387d8c4cd0a1
dodji <dodji@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-09-08 22:36:41 +0000

    Use -fno-merge-debug-string for some dwarf tests
    	Use -fno-merge-debug-string to comply with Darwin
    	* g++.dg/debug/dwarf2/namespace-1.C: Use -fno-merge-debug-string
    	and adjust.
    	* g++.dg/debug/dwarf2/template-func-params-6.C: Likewise.
    	* g++.dg/debug/dwarf2/template-func-params-7.C: Likewise.
    	* g++.dg/debug/dwarf2/template-params-6.C: Likewise.
    	* g++.dg/debug/dwarf2/pubnames-1.C: Likewise. Also, adjust to
    	take darwin specifics in account.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@151538 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/debug/dwarf2/namespace-1.C
+++ gcc/testsuite/g++.dg/debug/dwarf2/namespace-1.C
@@ -1,12 +1,12 @@
 // Contributed by Dodji Seketeli <dodji@redhat.com>
 // Origin PR debug/41170
-// { dg-options "-g -dA" }
+// { dg-options "-g -dA -fno-merge-debug-strings" }
 //
 // We want to test that there is a DW_TAG_namespace DIE DW_AT_name is set
 // to "not_emitted". That namespace die has a child DW_TAG_typedef DIE
 // which DW_AT_name is the null terminated string "T".
 // { dg-final { scan-assembler-times "DIE +\\(.*?\\) DW_TAG_namespace" 1 } }
-// { dg-final { scan-assembler-times "DW_AT_name: \"not_emitted\"" 1 } }
+// { dg-final { scan-assembler-times "\"not_emitted.0\".*?DW_AT_name" 1 } }
 // { dg-final { scan-assembler-times "DIE +\\(.*?\\) DW_TAG_typedef" 1 } }
 // { dg-final { scan-assembler-times "\.ascii \"T.0\"\[\t \]+.*?DW_AT_name" 1 } }
 
--- gcc/testsuite/g++.dg/debug/dwarf2/template-func-params-6.C
+++ gcc/testsuite/g++.dg/debug/dwarf2/template-func-params-6.C
@@ -1,8 +1,8 @@
 // Contributed by Dodji Seketeli <dodji@redhat.com>
 // Origin PR debug/30161
-// { dg-options "-g -dA" }
+// { dg-options "-g -dA -fno-merge-debug-strings" }
 // { dg-final { scan-assembler-times "DW_TAG_GNU_template_template_param" 2 } }
-// { dg-final { scan-assembler-times "DW_AT_GNU_template_name: \"vector\"" 1 } }
+// { dg-final { scan-assembler-times "\"vector.0\".*?DW_AT_GNU_template_name" 1 } }
 // { dg-final { scan-assembler-times ".ascii \"U.0\".*?DW_AT_name" 1 } }
 
 template <class T>
