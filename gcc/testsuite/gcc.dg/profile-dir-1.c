edf736cb4d256faf2f7f83e2fe09bd68e93880be
Joseph Myers <joseph@codesourcery.com>
2009-09-08 23:31:13 +0100

    profile-dir-1.c, [...]: Disable for remote-host testing.
    	* gcc.dg/profile-dir-1.c, gcc.dg/profile-dir-2.c,
    	gcc.dg/profile-dir-3.c: Disable for remote-host testing.
    
    From-SVN: r151537

--- gcc/testsuite/gcc.dg/profile-dir-1.c
+++ gcc/testsuite/gcc.dg/profile-dir-1.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-O -fprofile-generate=. -fdump-tree-tree_profile" } */
+/* { dg-require-host-local "" } */
 /* { dg-final { scan-tree-dump " ./profile-dir-1.gcda" "tree_profile" } } */
 
 int
