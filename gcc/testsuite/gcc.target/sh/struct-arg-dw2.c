b324d3bd8603451e45d202bd2cba67092449891b
kkojima <kkojima@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-09-16 01:12:16 +0000

    * config/sh/sh.c (output_stack_adjust): Add new argument frame_p. 	(sh_expand_prologue): Update calls to output_stack_adjust. 	(sh_expand_epilogue): Likewise.
    	* gcc.target/sh/struct-arg-dw2.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@151735 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/sh/struct-arg-dw2.c
@@ -0,0 +1,26 @@
+/* Verify that we don't generate fame related insn against stack adjustment
+   for the object sent partially in registers. */
+/* { dg-do compile { target "sh*-*-*" } } */
+/* { dg-options "-g" } */
+/* { dg-final { scan-assembler-not "\t.cfi_def_cfa_offset 16" } } */
+
+typedef struct
+{
+  unsigned short A1;
+  unsigned short A2;
+} A_t;
+
+typedef struct
+{
+  A_t C13[10];
+} C_t;
+
+void
+Store (C_t Par)
+{
+  unsigned char *ptr;
+  unsigned int test;
+
+  ptr = (unsigned char*) 0x12345678;
+  ptr++;
+}
