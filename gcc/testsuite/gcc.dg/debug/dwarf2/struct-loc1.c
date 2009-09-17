6e0982ac93b945a239af795e598e65073c4b4620
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-09-17 20:31:54 +0000

    * c-decl.c (finish_struct): Adjust DECL_SOURCE_LOCATION of 	TYPE_STUB_DECL.
    	* gcc.dg/debug/dwarf2/struct-loc1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@151816 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/debug/dwarf2/struct-loc1.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */
+/* { dg-options "-g -O0 -dA -fno-merge-debug-strings" } */
+
+struct foo;
+struct foo *obj;
+struct foo
+{
+  int x;
+};
+
+int
+main ()
+{
+  return 0;
+}
+
+/* { dg-final { scan-assembler "DW_TAG_structure_type\[^\\r\\n\]*\[\\r\\n\]+\[^\\r\\n\]*\"foo\[^\\r\\n\]*DW_AT_name(\[^\\r\\n\]*\[\\r\\n\]+\[^\\r\\n\]*DW_AT_)*\[^\\r\\n\]*\[\\r\\n\]+\[^\\r\\n\]*\[^0-9a-fA-FxX](0x)?6\[^0-9a-fA-FxX]\[^\\r\\n\]*DW_AT_decl_line" } } */
