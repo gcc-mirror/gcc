ed6d9a1b9bcd377da8fe8fadae73f316536f2160
Chris Demetriou <cgd@google.com>
2009-09-20 01:08:06 -0700

    re PR preprocessor/28435 (-MMD vs not found system header (included from a system header))
    [gcc/testsuite/ChangeLog]
    2009-09-20  Chris Demetriou  <cgd@google.com>
    
            PR preprocessor/28435:
            * gcc.dg/cpp/missing-header-MD.c: Make output checking match
            gcc.dg/cpp/missing-header-1.c, and add PR number to test comment.
            * gcc.dg/cpp/missing-header-MMD.c: Likewise.
            * gcc.dg/cpp/missing-sysheader-MD.c: Likewise.
            * gcc.dg/cpp/missing-sysheader-MMD.c: Likewise.
    
    From-SVN: r151894

--- gcc/testsuite/gcc.dg/cpp/missing-header-MD.c
+++ gcc/testsuite/gcc.dg/cpp/missing-header-MD.c
@@ -1,6 +1,10 @@
-/* Test that missing user headers are fatal errors with -MD.  */
+/* Test that missing user headers are fatal errors with -MD.  PR 28435.  */
 /* { dg-do compile } */
 /* { dg-options "-MD" } */
 
-#include "nonexistent.h" /* { dg-error "nonexistent.h" } */
+#include "nonexistent.h"
+/* { dg-message "nonexistent.h" "" { target *-*-* } 0 } */
 /* { dg-message "terminated" "" { target *-*-* } 0 } */
+
+/* This declaration should not receive any diagnostic.  */
+foo bar;
--- gcc/testsuite/gcc.dg/cpp/missing-header-MMD.c
+++ gcc/testsuite/gcc.dg/cpp/missing-header-MMD.c
@@ -1,6 +1,10 @@
-/* Test that missing user headers are fatal errors with -MMD.  */
+/* Test that missing user headers are fatal errors with -MMD.  PR 28435.  */
 /* { dg-do compile } */
 /* { dg-options "-MMD" } */
 
-#include "nonexistent.h" /* { dg-error "nonexistent.h" } */
+#include "nonexistent.h"
+/* { dg-message "nonexistent.h" "" { target *-*-* } 0 } */
 /* { dg-message "terminated" "" { target *-*-* } 0 } */
+
+/* This declaration should not receive any diagnostic.  */
+foo bar;
