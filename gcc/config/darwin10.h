c9c80434b62f701de3502d8b25cd9462f3374a56
Jack Howarth <howarth@bromo.med.uc.edu>
2009-09-22 02:28:19 +0000

    re PR middle-end/41260 (major regressions on *-apple-darwin10 at -m64 caused by r147995)
    PR middle-end/41260
    * gcc/config.gcc: Use darwin9.h and darwin10.h on darwin10 and later.
    * gcc/config/darwin10.h: Add file to pass -no_compact_unwind on
    LIB_SPEC for darwin10 and later since it always uses the unwinder in
    libSystem which is derived from the gcc 4.2.1 unwinder.
    
    From-SVN: r151960

---
+++ gcc/config/darwin10.h
@@ -0,0 +1,25 @@
+/* Target definitions for Darwin (Mac OS X) systems.
+   Copyright (C) 2006, 2007 Free Software Foundation, Inc.
+   Contributed by Jack Howarth <howarth@bromo.med.uc.edu>.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+/* Fix PR41260 by passing -no_compact_unwind on darwin10 and later until
+unwinder in libSystem is fixed to digest new epilog unwinding notes. */
+
+#undef LIB_SPEC
+#define LIB_SPEC "%{!static:-no_compact_unwind -lSystem}"
