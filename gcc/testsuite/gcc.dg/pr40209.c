827f08ae8674823228dd9e8426d6fcbd850d205f
nvachhar <nvachhar@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-09-22 16:48:19 +0000

    2009-09-22  Neil Vachharajani  <nvachhar@google.com>
            * testsuite/gcc.dg/pr40209.c: Add test case.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@152032 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr40209.c
@@ -0,0 +1,22 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fprofile-use" } */
+
+void process(const char *s);
+
+struct BaseHolder {
+  unsigned int base_;
+};
+
+void UInt2Str(struct BaseHolder *b, unsigned int x) {
+  static const char digit[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+  char buf[100];
+  int i = 100;
+  do {
+    buf[--i] = digit[x % b->base_];
+    x /= b->base_;
+  } while (x > 0);
+  process(buf);
+}
+
+/* Ignore a warning that is irrelevant to the purpose of this test.  */
+/* { dg-prune-output ".*\.gcda not found, execution counts estimated.*" } */
