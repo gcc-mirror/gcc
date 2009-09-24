c33ac441b202e1c6a19ba085c8c23946ff3e7a2e
H.J. Lu <hongjiu.lu@intel.com>
2009-09-24 16:49:13 +0000

    pr12329.c (dg-do): Changed to run.
    2009-09-24  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* gcc.target/i386/pr12329.c (dg-do): Changed to run.
    	(test_nested): Renamed to ...
    	(test_nested1): This.
    	(test_nested2): New.
    	(main): Likewise.
    
    From-SVN: r152126

--- gcc/testsuite/gcc.target/i386/pr12329.c
+++ gcc/testsuite/gcc.target/i386/pr12329.c
@@ -1,10 +1,10 @@
-/* { dg-do compile } */
+/* { dg-do run } */
 /* { dg-require-effective-target ilp32 } */
 /* { dg-options "-O2" } */
 
 extern void abort (void);
 
-int test_nested (int i)
+int test_nested1 (int i)
 {
   int __attribute__ ((__noinline__, __regparm__(3))) foo(int j, int k, int l)
   {
@@ -12,4 +12,36 @@ int test_nested (int i)
   }
 
   return foo (i, i+1, i+2);
+}
+
+int test_nested2 (int i)
+{
+  int x;
+
+  int __attribute__ ((__noinline__, __regparm__(3))) foo(int j, int k, int l)
+  {
+    return i + j + k + l;
+  }
+
+  x = foo (i+3, i+1, i+2);
+  if (x != (4*i + 6))
+    abort ();
+
+  return x;
+}
+
+int
+main ()
+{
+  int i = test_nested1 (3);
+
+  if (i != 15)
+    abort ();
+
+  i = test_nested2 (4);
+
+  if (i != 22)
+    abort ();
+
+  return 0;
 }
