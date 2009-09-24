01c3cf4d51cfe3c30cb6e04c895f1c1be0fcd837
Eric Botcazou <ebotcazou@adacore.com>
2009-09-24 11:52:05 +0000

    * gnat.dg/nested_proc.adb: New test.
    From-SVN: r152115

---
+++ gcc/testsuite/gnat.dg/nested_proc.adb
@@ -0,0 +1,33 @@
+-- { dg-do run }
+-- Test that a static link is correctly passed to a subprogram which is
+-- indirectly called through an aggregate.
+
+procedure Nested_Proc is
+
+  I : Integer := 0;
+
+  procedure P1 (X : Integer) is
+  begin
+    I := X;
+  end;
+
+  type Func_Ptr is access procedure (X : Integer);
+
+  type Arr is array (1..64) of Integer;
+
+  type Rec is record
+    F : Func_Ptr;
+    A : Arr;
+  end record;
+
+  procedure P2 (R : Rec) is
+  begin
+     R.F (1);
+  end;
+
+begin
+  P2 ((F => P1'Access, A => (others => 0)));
+  if I /= 1 then
+    raise Program_Error;
+  end if;
+end;
