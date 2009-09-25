014ea9ed20afdbcafd2219a181d1f0347c70d039
hainque <hainque@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-09-25 09:14:09 +0000

    Commit testsuite part of r151322, omitted by mistake then.
            * gnat.dg/array7.ads: Include Signed_Integer_Type_Definition.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@152164 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gnat.dg/array7.ads
+++ gcc/testsuite/gnat.dg/array7.ads
@@ -1,10 +1,20 @@
 package Array7 is
 
-   type Arr is array (Positive range <>) of Integer;
-   type Arr_Acc is access Arr;
+   package Range_Subtype is
+      type Arr is array (Positive range <>) of Integer;
+      type Arr_Acc is access Arr;
 
-   subtype My_Range is Integer range 1 .. 25;
+      subtype My_Range is Integer range 1 .. 25;
+      function Get_Arr (Nbr : My_Range) return Arr_Acc;
+   end;
 
-   function Get_Arr (Nbr : My_Range) return Arr_Acc;
+   package Range_Type is
+
+      type My_Range is range 1 .. 25;
+      type Arr is array (My_Range range <>) of Integer;
+      type Arr_Acc is access Arr;
+
+      function Get_Arr (Nbr : My_Range) return Arr_Acc;
+   end;
 
 end Array7;
