aa58f943d88dfcb1d0d7609b62079269e318cf1d
Jonathan Gray <jsg@openbsd.org>
2009-09-27 13:20:23 +0000

    config.gcc: Update OpenBSD targets.
    	* config.gcc: Update OpenBSD targets.
    	* config/openbsd-stdint.h: New file.
    	* config/openbsd-libpthread.h: New file.
    	* config/openbsd.h: Update and break out LIB_SPEC definition.
    	* config/alpha/openbsd.h: Overhaul to reflect ELF migration.
    	* config/i386/openbsdelf.h: Correct types.
    	* config/m68k/openbsd.h: Likewise.
    	* config/mips/openbsd.h: Likewise.
    	* config/vax/openbsd.h: Likewise.
    
    From-SVN: r152218

--- gcc/config/m68k/openbsd.h
+++ gcc/config/m68k/openbsd.h
@@ -42,16 +42,19 @@ along with GCC; see the file COPYING3.  If not see
 
 /* This must agree with <machine/ansi.h> */
 #undef SIZE_TYPE
-#define SIZE_TYPE "unsigned int"
+#define SIZE_TYPE "long unsigned int"
 
 #undef PTRDIFF_TYPE
-#define PTRDIFF_TYPE "int"
+#define PTRDIFF_TYPE "long int"
 
 #undef WCHAR_TYPE
 #define WCHAR_TYPE "int"
 
 #undef WCHAR_TYPE_SIZE
 #define WCHAR_TYPE_SIZE 32
+
+#undef WINT_TYPE
+#define WINT_TYPE "int"
 
 /* Storage layout.  */
 
