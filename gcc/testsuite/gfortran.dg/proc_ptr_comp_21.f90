c2ed3e6e487d9cf6b7823c4c828a613fd915dc9b
H.J. Lu <hongjiu.lu@intel.com>
2009-09-28 20:38:53 +0000

    Backport PRs 40642, 40758, 41212, 41242, 41248, 41275, 41295, 41347, 41470, 41476
    2009-09-28  H.J. Lu  <hongjiu.lu@intel.com>
    
    	Backport from mainline:
    	2009-09-26  Andreas Schwab  <schwab@linux-m68k.org>
    
    	PR c/41476
    	* gcc.dg/cond-constqual-1.c: New test.
    
    	2009-09-26  Michael Matz  <matz@suse.de>
    
    	PR lto/40758
    	PR middle-end/41470
    	* gcc.dg/pr41470.c: New test.
    
    	2009-09-23  Alexandre Oliva  <aoliva@redhat.com>
    
    	PR debug/41248
    	* gcc.dg/pr41248.c: New.
    
    	2009-09-22  Alexandre Oliva  <aoliva@redhat.com>
    
    	PR debug/41295
    	* gcc.dg/pr41295.c: New.
    
    	2009-09-20  Andrew Pinski  <pinskia@gcc.gnu.org>
    
    	PR middle-end/40642
    	* g++.dg/torture/pr40642.C: New testcase.
    
    	2009-09-17  Michael Matz  <matz@suse.de>
    
    	PR middle-end/41347
    	* gfortran.dg/pr41347.f90: New test.
    
    	2009-09-16  Michael Matz  <matz@suse.de>
    
    	PR fortran/41212
    	* gfortran.dg/pr41212.f90: New test.
    
    	2009-09-11  Michael Matz  <matz@suse.de>
    
    	PR middle-end/41275
    	* g++.dg/tree-ssa/pr41275.C: New test.
    
    	2009-09-11  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/41242
    	* gfortran.dg/proc_ptr_comp_21.f90: New.
    
    From-SVN: r152250

---
+++ gcc/testsuite/gfortran.dg/proc_ptr_comp_21.f90
@@ -0,0 +1,28 @@
+! { dg-do compile }
+!
+! PR 41242: [4.5 Regression] PPC call rejected (related to user-defined assignment?)
+!
+! Original test case by Juergen Reuter <reuter@physik.uni-freiburg.de>
+! Modified by Janus Weil <janus@gcc.gnu.org>
+
+  type :: nf_t
+     procedure(integer), nopass, pointer :: get_n_in
+  end type
+
+  interface assignment(=)
+     procedure op_assign
+  end interface
+
+  type(nf_t) :: prc_lib
+  prc_lib = "foobar"
+  print *, prc_lib%get_n_in()
+
+contains
+
+  elemental subroutine op_assign (str, ch)
+    type(nf_t), intent(out) :: str
+    character(len=*), intent(in) :: ch
+  end subroutine
+
+end
+
