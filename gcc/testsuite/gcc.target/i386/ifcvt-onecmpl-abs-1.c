650260479ac18d6e26af41f77c74b451ce426a46
Easwaran Raman <eraman@google.com>
2009-09-28 21:26:31 +0000

    ifcvt.c (noce_try_abs): Recognize pattern and call expand_one_cmpl_abs_nojump.
    ./:	* ifcvt.c (noce_try_abs): Recognize pattern and call
    	expand_one_cmpl_abs_nojump.
    	* optabs.c (expand_one_cmpl_abs_nojump): New function.
    	* optabs.h (expand_one_cmpl_abs_nojump): Declare.
    testsuite/:
    	* gcc.target/i386/ifcvt-onecmpl-abs-1.c: New file.
    	* gcc.c-torture/execute/ifcvt-onecmpl-abs-1.c: New file.
    
    From-SVN: r152253

---
+++ gcc/testsuite/gcc.target/i386/ifcvt-onecmpl-abs-1.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* This test checks for if-conversion of one's complement
+ * abs function.  */
+/* { dg-options "-O" } */
+/* { dg-final { scan-assembler "sar" } } */
+/* { dg-final { scan-assembler "xor" } } */
+
+/* Check code generation for one's complement version of abs */
+
+int onecmplabs(int x)
+{
+  if (x < 0)
+    x = ~x;
+  return x;
+}
