69df988270323c0daead0cba6c12448996e0e984
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-09-30 15:49:31 +0000

    remove utf8
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@152331 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp0x/lambda/lambda-deduce-neg.C
+++ gcc/testsuite/g++.dg/cpp0x/lambda/lambda-deduce-neg.C
@@ -3,7 +3,7 @@
 
 int main() {
   int i = 0;
-  int& r = [&] () { return i; } (); // { dg-error "" "invalid initialization of non-const reference of type ‘int&’ from a temporary of type ‘int’" }
+  int& r = [&] () { return i; } (); // { dg-error "" "invalid initialization of non-const reference of type .int&. from a temporary of type .int." }
 
   return 0;
 }
