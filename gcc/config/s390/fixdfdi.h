9332b7a302122b7cd63e49653e8e925fc5a5d92e
Andreas Krebbel <Andreas.Krebbel@de.ibm.com>
2009-09-30 10:41:26 +0000

    fixdfdi.h: Add Runtime Library Exception wording to the License text.
    2009-09-30  Andreas Krebbel  <Andreas.Krebbel@de.ibm.com>
    
    	* config/s390/fixdfdi.h: Add Runtime Library Exception wording to
    	the License text.
    
    From-SVN: r152325

--- gcc/config/s390/fixdfdi.h
+++ gcc/config/s390/fixdfdi.h
@@ -1,5 +1,6 @@
 /* Definitions of target machine for GNU compiler, for IBM S/390
-   Copyright (C) 1999, 2000, 2001, 2007, 2008 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2000, 2001, 2007, 2008 and 2009
+   Free Software Foundation, Inc.
    Contributed by Hartmut Penner (hpenner@de.ibm.com) and
                   Ulrich Weigand (uweigand@de.ibm.com).
 
@@ -15,8 +16,13 @@ WARRANTY; without even the implied warranty of MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 for more details.
 
-You should have received a copy of the GNU General Public License
-along with GCC; see the file COPYING3.  If not see
+Under Section 7 of GPL version 3, you are granted additional
+permissions described in the GCC Runtime Library Exception, version
+3.1, as published by the Free Software Foundation.
+
+You should have received a copy of the GNU General Public License and
+a copy of the GCC Runtime Library Exception along with this program;
+see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 <http://www.gnu.org/licenses/>.  */
 
 #ifdef L_fixunstfdi
