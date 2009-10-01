0a5a37d431341768dac49c74016dceded06c6140
Loren J. Rittle <ljrittle@acm.org>
2009-10-01 23:08:07 +0000

    * gcc.dg/20021014-1.c (*-*-freebsd*): Use dg-message.
    From-SVN: r152386

--- gcc/testsuite/gcc.dg/20021014-1.c
+++ gcc/testsuite/gcc.dg/20021014-1.c
@@ -3,7 +3,7 @@
 /* { dg-options "-O2 -p" } */
 /* { dg-options "-O2 -p -static" { target hppa*-*-hpux* } } */
 /* { dg-error "profiler" "No profiler support" { target xstormy16-*-* } 0 } */
-/* { dg-error "" "consider using `-pg' instead of `-p' with gprof(1)" { target *-*-freebsd* } 0 } */
+/* { dg-message "" "consider using `-pg' instead of `-p' with gprof(1)" { target *-*-freebsd* } 0 } */
 
 extern void abort (void);
 extern void exit (int);
