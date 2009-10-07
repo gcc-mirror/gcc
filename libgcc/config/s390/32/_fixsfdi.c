e38fe7388bfa19ec16f678479ec6d174ea77e255
krebbel <krebbel@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-10-07 08:26:47 +0000

    2009-10-07  Andreas Krebbel  <Andreas.Krebbel@de.ibm.com>
    	* config.gcc: Don't include the makefile fragments intended for
    	libgcc.
    	* config/s390/fixdfdi.h: File removed.
    	* config/s390/libgcc-glibc.ver: File removed.
    	* config/s390/s390.h: Remove the fixdfdi.h hack.
    	* config/s390/t-crtstuff: File moved to libgcc dir.
    	* config/s390/t-linux: Likewise.
    	* config/s390/t-tpf: libgcc specific parts removed.
    	* config/s390/t-linux64: Likewise.
    
    2009-10-07  Andreas Krebbel  <Andreas.Krebbel@de.ibm.com>
    
    	* config.host: Include the s390 makefile fragments.
    	* config/s390/32/_fixdfdi.c: New file.
    	* config/s390/32/_fixsfdi.c: New file.
    	* config/s390/32/_fixtfdi.c: New file.
    	* config/s390/32/_fixunsdfdi.c: New file.
    	* config/s390/32/_fixunssfdi.c: New file.
    	* config/s390/32/_fixunstfdi.c: New file.
    	* config/s390/32/t-floattodi: New file.
    	* config/s390/libgcc-glibc.ver: New file.
    	* config/s390/t-crtstuff: New file.
    	* config/s390/t-linux: New file.
    	* config/s390/t-tpf: New file.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@152515 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libgcc/config/s390/32/_fixsfdi.c
@@ -0,0 +1,93 @@
+/* Definitions of target machine for GNU compiler, for IBM S/390
+   Copyright (C) 1999, 2000, 2001, 2007, 2008 and 2009
+   Free Software Foundation, Inc.
+   Contributed by Hartmut Penner (hpenner@de.ibm.com) and
+                  Ulrich Weigand (uweigand@de.ibm.com).
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+Under Section 7 of GPL version 3, you are granted additional
+permissions described in the GCC Runtime Library Exception, version
+3.1, as published by the Free Software Foundation.
+
+You should have received a copy of the GNU General Public License and
+a copy of the GCC Runtime Library Exception along with this program;
+see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+<http://www.gnu.org/licenses/>.  */
+
+#define EXP(fp)         (((fp.l) >> 23) & 0xFF)
+#define EXCESS          126
+#define SIGNBIT         0x80000000
+#define SIGN(fp)        ((fp.l) & SIGNBIT)
+#define HIDDEN          (1 << 23)
+#define MANT(fp)        (((fp.l) & 0x7FFFFF) | HIDDEN)
+#define FRAC(fp)        ((fp.l) & 0x7FFFFF)
+
+typedef int DItype_x __attribute__ ((mode (DI)));
+typedef unsigned int UDItype_x __attribute__ ((mode (DI)));
+typedef int SItype_x __attribute__ ((mode (SI)));
+typedef unsigned int USItype_x __attribute__ ((mode (SI)));
+
+union float_long
+  {
+    float f;
+    USItype_x l;
+  };
+
+DItype_x __fixsfdi (float a1);
+
+/* convert double to int */
+DItype_x
+__fixsfdi (float a1)
+{
+    register union float_long fl1;
+    register int exp;
+    register DItype_x l;
+
+    fl1.f = a1;
+
+    /* +/- 0, denormalized */
+
+    if (!EXP (fl1))
+      return 0;
+
+    exp = EXP (fl1) - EXCESS - 24;
+
+    /* number < 1 */
+
+    if (exp < -24)
+      return 0;
+
+    /* NaN */
+
+    if ((EXP(fl1) == 0xff) && (FRAC(fl1) != 0)) /* NaN */
+      return 0x8000000000000000ULL;
+
+    /* Number big number & +/- inf */
+
+    if (exp >= 40) {
+	l = (long long)1<<63;
+	if (!SIGN(fl1))
+	    l--;
+	return l;
+    }
+
+    l = MANT(fl1);
+
+    if (exp > 0)
+      l <<= exp;
+    else
+      l >>= -exp;
+
+    return (SIGN (fl1) ? -l : l);
+}
---
+++ libgcc/config/s390/32/_fixtfdi.c
@@ -0,0 +1,102 @@
+/* Definitions of target machine for GNU compiler, for IBM S/390
+   Copyright (C) 1999, 2000, 2001, 2007, 2008 and 2009
+   Free Software Foundation, Inc.
+   Contributed by Hartmut Penner (hpenner@de.ibm.com) and
+                  Ulrich Weigand (uweigand@de.ibm.com).
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+Under Section 7 of GPL version 3, you are granted additional
+permissions described in the GCC Runtime Library Exception, version
+3.1, as published by the Free Software Foundation.
+
+You should have received a copy of the GNU General Public License and
+a copy of the GCC Runtime Library Exception along with this program;
+see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+<http://www.gnu.org/licenses/>.  */
+
+#define EXPD(fp)	   (((fp.l.i[0]) >> 16) & 0x7FFF)
+#define EXPONENT_BIAS	   16383
+#define MANTISSA_BITS      112
+#define PRECISION          (MANTISSA_BITS + 1)
+#define SIGNBIT		   0x80000000
+#define SIGND(fp)	   ((fp.l.i[0]) & SIGNBIT)
+#define MANTD_HIGH_LL(fp)  ((fp.ll[0] & HIGH_LL_FRAC_MASK) | HIGH_LL_UNIT_BIT)
+#define MANTD_LOW_LL(fp)   (fp.ll[1])
+#define FRACD_ZERO_P(fp)   (!fp.ll[1] && !(fp.ll[0] & HIGH_LL_FRAC_MASK))
+#define HIGH_LL_FRAC_BITS  48
+#define HIGH_LL_UNIT_BIT   ((UDItype_x)1 << HIGH_LL_FRAC_BITS)
+#define HIGH_LL_FRAC_MASK  (HIGH_LL_UNIT_BIT - 1)
+
+typedef int DItype_x __attribute__ ((mode (DI)));
+typedef unsigned int UDItype_x __attribute__ ((mode (DI)));
+typedef int SItype_x __attribute__ ((mode (SI)));
+typedef unsigned int USItype_x __attribute__ ((mode (SI)));
+
+union double_long {
+  long double d;
+  struct {
+      SItype_x i[4]; /* 32 bit parts: 0 upper ... 3 lowest */
+    } l;
+  UDItype_x ll[2];   /* 64 bit parts: 0 upper, 1 lower */
+};
+
+DItype_x __fixtfdi (long double a1);
+
+/* convert double to unsigned int */
+DItype_x
+__fixtfdi (long double a1)
+{
+    register union double_long dl1;
+    register int exp;
+    register UDItype_x l;
+
+    dl1.d = a1;
+
+    /* +/- 0, denormalized */
+    if (!EXPD (dl1))
+      return 0;
+
+    /* The exponent - considered the binary point at the right end of
+       the mantissa.  */
+    exp = EXPD (dl1) - EXPONENT_BIAS - MANTISSA_BITS;
+
+    /* number < 1: If the mantissa would need to be right-shifted more bits than
+       its size the result would be zero.  */
+    if (exp <= -PRECISION)
+      return 0;
+
+    /* NaN: All exponent bits set and a nonzero fraction.  */
+    if ((EXPD(dl1) == 0x7fff) && !FRACD_ZERO_P (dl1))
+      return 0x8000000000000000ULL;
+
+    /* One extra bit is needed for the unit bit which is appended by
+       MANTD_HIGH_LL on the left of the matissa.  */
+    exp += HIGH_LL_FRAC_BITS + 1;
+
+    /* If the result would still need a left shift it will be too large
+       to be represented.  Compared to the unsigned variant we have to
+       take care that there is still space for the sign bit to be
+       applied.  So we can only go on if there is a right-shift by one
+       or more.  */
+    if (exp >= 0)
+      {
+	l = 1ULL << 63; /* long long min */
+	return SIGND (dl1) ? l : l - 1;
+      }
+
+    l = MANTD_LOW_LL (dl1) >> (HIGH_LL_FRAC_BITS + 1)
+        | MANTD_HIGH_LL (dl1) << (64 - (HIGH_LL_FRAC_BITS + 1));
+
+    return SIGND (dl1) ? -(l >> -exp) : l >> -exp;
+}
