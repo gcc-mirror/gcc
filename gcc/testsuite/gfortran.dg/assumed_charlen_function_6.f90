6c19d9b5b20707a26e1f2d36db109ae6c5acda30
Daniel Kraft <d@domob.eu>
2009-10-07 20:13:28 +0200

    re PR fortran/41615 (Bogus error message: "internal function" should be "module procedure")
    2009-10-07  Daniel Kraft  <d@domob.eu>
    
    	PR fortran/41615
    	* resolve.c (resolve_contained_fntype): Clarify error message for
    	invalid assumed-length character result on module procedures.
    
    2009-10-07  Daniel Kraft  <d@domob.eu>
    
    	PR fortran/41615
    	* gfortran.dg/assumed_charlen_function_6.f90: New test.
    
    From-SVN: r152534

---
+++ gcc/testsuite/gfortran.dg/assumed_charlen_function_6.f90
@@ -0,0 +1,37 @@
+! { dg-do compile }
+
+! PR fortran/41615
+! Output nicer error message for invalid assumed-len character function result
+! depending on what kind of contained procedure it is.
+
+module funcs
+   implicit none
+contains
+      function assumed_len(x) ! { dg-error "module procedure" }
+         character(*) assumed_len
+         integer, intent(in) :: x
+      end function assumed_len
+end module funcs
+
+module mod2
+  implicit none
+contains
+  subroutine mysub ()
+    contains
+      function assumed_len(x) ! { dg-error "internal function" }
+         character(*) assumed_len
+         integer, intent(in) :: x
+      end function assumed_len
+  end subroutine
+end module mod2
+
+program main
+  implicit none
+contains
+      function assumed_len(x) ! { dg-error "internal function" }
+         character(*) assumed_len
+         integer, intent(in) :: x
+      end function assumed_len
+end program main
+
+! { dg-final { cleanup-modules "funcs mod2" } }
