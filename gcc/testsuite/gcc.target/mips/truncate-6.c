096125f1074c9bc1f0941bd35b97e8832bea85fb
nemet <nemet@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-10-08 17:54:24 +0000

    * combine.c (label_tick_ebb_start): Fix comment. 	(combine_instructions): Set label_tick and label_tick_ebb_start before 	calling setup_incoming_promotions.  Start them from 1.  Increment 	label_tick instead of deriving it from the BB index.  Rather than 	comparing ticks use the block from the previous iteration to decide 	whether to start a new EBB.  Remove empty lines before function.
    testsuite/
    	* gcc.target/mips/truncate-6.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@152567 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/mips/truncate-6.c
@@ -0,0 +1,12 @@
+/* setup_incoming_promotions should detect x to be already sign-extended due
+   to PROMOTE_MODE.  Thus the truncation should be removed by combine.  Based
+   on gcc.c-torture/execute/pr34070-2.c.  */
+/* { dg-options "-O -mgp64" } */
+/* { dg-final { scan-assembler-not "\tsll\t\[^\n\]*,0" } } */
+
+NOMIPS16 int f(unsigned int x, int n, int *p)
+{
+  if (p)
+    *p = 1;
+  return ((int)x) / (1 << n);
+}
