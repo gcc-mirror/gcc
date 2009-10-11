30da36fc7c2b4d578ba79a94a6f73f3d2fc3b6d2
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-10-11 12:53:24 +0000

    * config/i386/i386.md (*setcc_di_1): New insn_and_split pattern. 	(*setcc_si_1_and): Ditto. 	(*setcc_si_1_movzbl): Ditto. 	(*setcc_<mode>_2): Ditto. 	(*setcc_qi): Rename from *setcc_1. 	(*setcc_qi_slp): Rename from *setcc_2.
    	(*zero_extendqihi2_movzbw_and splitter): Use ix86_expand_clear.
    	(*zero_extendqisi2_movzbw_and splitter): Ditto.
    
    	* config/i386/i386.c (ix86_expand_clear): Remove reload_completed from
    	"if" condition, there is already assert with reload_completed present.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@152642 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -12637,7 +12637,7 @@ ix86_expand_clear (rtx dest)
   tmp = gen_rtx_SET (VOIDmode, dest, const0_rtx);
 
   /* This predicate should match that for movsi_xor and movdi_xor_rex64.  */
-  if (reload_completed && (!TARGET_USE_MOV0 || optimize_insn_for_speed_p ()))
+  if (!TARGET_USE_MOV0 || optimize_insn_for_speed_p ())
     {
       rtx clob = gen_rtx_CLOBBER (VOIDmode, gen_rtx_REG (CCmode, FLAGS_REG));
       tmp = gen_rtx_PARALLEL (VOIDmode, gen_rtvec (2, tmp, clob));
