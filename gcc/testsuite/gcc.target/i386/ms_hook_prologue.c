afd2c302c6f4377ed46d809744115e67d363d997
Stefan Dösinger <stefan@codeweavers.com>
2009-10-12 16:36:37 +0000

    i386.md (vswapmov): New.
    2009-10-12  Stefan Dösinger  <stefan@codeweavers.com>
    
    	* config/i386/i386.md (vswapmov): New.
    	* config/i386/i386.c (ix86_handle_fndecl_attribute): New.
    	(ix86_function_ms_hook_prologue): New.
    	(ix86_expand_prologue): Handle ms_hook_prologue attribute.
    	* configure.ac: Test for swap suffix support in as.
    	* configure: Rebuild.
    
    From-SVN: r152670

---
+++ gcc/testsuite/gcc.target/i386/ms_hook_prologue.c
@@ -0,0 +1,29 @@
+/* Test that the ms_hook_prologue attribute generates the correct code.  */
+
+/* { dg-do run } */
+/* { dg-require-effective-target ilp32 } */
+/* { dg-options "-O2 -fomit-frame-pointer" } */
+
+int __attribute__ ((__ms_hook_prologue__)) foo ()
+{
+  unsigned char *ptr = (unsigned char *) foo;
+
+  /* The NOP mov must not be optimized away by optimizations.
+     The push %ebp, mov %esp, %ebp must not be removed by
+     -fomit-frame-pointer */
+
+  /* movl.s %edi, %edi */
+  if(*ptr++ != 0x8b) return 1;
+  if(*ptr++ != 0xff) return 1;
+  /* push %ebp */
+  if(*ptr++ != 0x55) return 1;
+  /* movl.s %esp, %ebp */
+  if(*ptr++ != 0x8b) return 1;
+  if(*ptr++ != 0xec) return 1;
+  return 0;
+}
+
+int main ()
+{
+  return foo();
+}
