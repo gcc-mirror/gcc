b4722fd8dea9ece0db2c6ea8c1e6e55c2f082e27
matz <matz@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-10-12 16:31:37 +0000

    * gcc.dg/plugin/one_time_plugin.c: Update test to use the cfg pass.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@152669 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/plugin/one_time_plugin.c
+++ gcc/testsuite/gcc.dg/plugin/one_time_plugin.c
@@ -31,7 +31,7 @@ struct gimple_opt_pass one_pass =
 {
   {
   GIMPLE_PASS,
-  "useless",                           /* name */
+  "cfg",                           /* name */
   one_pass_gate,                         /* gate */
   one_pass_exec,       /* execute */
   NULL,                                 /* sub */
@@ -53,7 +53,7 @@ int plugin_init (struct plugin_name_args *plugin_info,
   struct register_pass_info p;
 
   p.pass = &one_pass.pass;
-  p.reference_pass_name = "useless";
+  p.reference_pass_name = "cfg";
   p.ref_pass_instance_number = 1;
   p.pos_op = PASS_POS_INSERT_AFTER;
 
