cd610ee673e18c94047f4ae4d7e60c96dae107cc
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-10-12 05:15:02 +0000

    gcc/ChangeLog: PR debug/41616 * tree-into-ssa.c (insert_phi_nodes_for): Build debug bind stmts on updates too. (maybe_register_def): Likewise.  Take stmt iterator. (rewrite_update_stmt): Take stmt iterator and pass it on. (rewrite_update_enter_block): Pass stmt iterator. gcc/testsuite/ChangeLog: PR debug/41616 * gcc.dg/guality/pr41616-1.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@152662 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/guality/pr41616-1.c
@@ -0,0 +1,20 @@
+/* { dg-do run { xfail *-*-* } } */
+/* { dg-options "-g -O2" } */
+
+#include "guality.h"
+
+inline int f(int *a)
+{
+  return *a;
+}
+
+int
+main(int argc, char *argv[])
+{
+  int b = -1;
+  GUALCHKVAL (b);
+  if (argc > 0)
+    b = -f(&b);
+  GUALCHKVAL (b);
+  return b;
+}
