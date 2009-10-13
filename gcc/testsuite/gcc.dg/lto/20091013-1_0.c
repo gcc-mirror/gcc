b02a92ce3e51af8fa68748816c0e7aec1ea86bc6
Richard Guenther <rguenther@suse.de>
2009-10-13 12:39:06 +0000

    re PR lto/41668 (ICE in  get_alias_set, at alias.c:698)
    2009-10-13  Richard Guenther  <rguenther@suse.de>
    
    	PR lto/41668
    	* gcc.dg/lto/20091006-2_0.c: New testcase.
    	* gcc.dg/lto/20091006-2_1.c: Likewise.
    	* gcc.dg/lto/20091006-2_2.c: Likewise.
    	* gcc.dg/lto/20091013-1_0.c: Likewise.
    	* gcc.dg/lto/20091013-1_1.c: Likewise.
    	* gcc.dg/lto/20091013-1_2.c: Likewise.
    
    From-SVN: r152704

---
+++ gcc/testsuite/gcc.dg/lto/20091013-1_0.c
@@ -0,0 +1,21 @@
+/* { dg-lto-do link } */
+/* { dg-lto-options {{-fPIC -shared -flto} {-fPIC -shared -O2 -flto}} } */
+
+void * HeapAlloc(void*,unsigned int,unsigned long);
+
+typedef struct tagGdiFont GdiFont;
+
+typedef struct tagDC {
+    int xunused;
+    GdiFont *gdiFont;
+    unsigned int font_code_page;
+} DC;
+
+DC *alloc_dc_ptr( void *funcs, unsigned short magic )
+{
+  DC *dc;
+  if (!(dc = HeapAlloc( 0, 0, sizeof(*dc) ))) return ((void *)0);
+  dc->gdiFont = 0;
+  return dc;
+}
+
