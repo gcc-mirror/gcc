e58c17e7b132e005fd7f556135b03b1f0ef95f30
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-10-23 12:18:42 +0000

    PR c/41673 	* alias.c (get_alias_set): Call langhook before returning 0 for 	types with structural equality. 	* c-common.c (c_common_get_alias_set): Use alias set of element 	type for arrays with structural comparison.
    testsuite:
    	* gcc.dg/Wstrict-aliasing-bogus-vla-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@153496 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/Wstrict-aliasing-bogus-vla-1.c
@@ -0,0 +1,10 @@
+/* PR 41673: bogus -Wstrict-aliasing warning from VLA dereference.  */
+/* { dg-do compile } */
+/* { dg-options "-std=gnu99 -O2 -Wall" } */
+
+int main(int argc, char *argv[])
+{
+    float x[argc];
+    float y[argc];
+    return 0 == __builtin_memcpy(y, x, argc * sizeof(*x));
+}
