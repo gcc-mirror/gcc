7814d4186a9b0b10e6e62133d5210cb2589de1de
matz <matz@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-10-26 13:00:36 +0000

    PR tree-optimization/41783 	* tree-ssa-alias.c (get_continuation_for_phi): Export, add a special 	case for simple diamonds 	* tree-ssa-alias.h (get_continuation_for_phi): Declare. 	* tree-ssa-pre.c (translate_vuse_through_block): Add same_valid 	argument, use alias oracle to skip some vdefs. 	(phi_translate_1): Change call to above, don't allocate new 	value ids if they can stay the same. 	(compute_avail): Allow vuse walking when looking up references.
    testsuite/
    	* gcc.dg/pr41783.c: New test.
    	* gcc.dg/tree-ssa/ssa-pre-23.c: Adjust.
    	* gcc.dg/tree-ssa/ssa-pre-24.c: Don't xfail anymore.
    	* gcc.dg/tree-ssa/ssa-pre-27.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@153551 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/ssa-pre-24.c
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-pre-24.c
@@ -9,12 +9,7 @@ void foo(int *p, double *x, int n)
 }
 
 /* We should remove the unnecessary insertion of a phi-node and
-   _not_ end up using the phi result for replacement *p.
-   The issue here is that when PHI-translating the virtual operands
-   we assign different value-numbers to the load.  Re-running VN
-   after insertion or trying to be clever and doing this on the
-   fly during PHI translation would solve this.  The next copyprop
-   fixes this anyway.  */
+   _not_ end up using the phi result for replacement *p.  */
 
-/* { dg-final { scan-tree-dump-not "= prephitmp" "pre" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-not "= prephitmp" "pre" } } */
 /* { dg-final { cleanup-tree-dump "pre" } } */
