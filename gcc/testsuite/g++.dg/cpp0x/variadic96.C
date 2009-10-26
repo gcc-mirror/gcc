f96d6fd02e10a4f266c5c459461562887cbf0c6a
Dodji Seketeli <dodji@redhat.com>
2009-10-26 18:31:22 +0000

    re PR c++/41785 ([C++0x] ICE on canonical types with variadic templates and CRTP)
    Fix PR c++/41785
    
    gcc/cp/ChangeLog:
    
    	PR c++/41785
    	* pt.c (template_args_equal): Handle comparison of
    	an ARGUMENT_PACK_SELECT node with the arguments node it selects into.
    	* cp-tree.def: Fix a typo in the description of TYPE_PACK_EXPANSION.
    
    gcc/testsuite/ChangeLog:
    	PR c++/41785
    	* gcc/testsuite/g++.dg/cpp0x/variadic96.C: New test.
    
    From-SVN: r153564

---
+++ gcc/testsuite/g++.dg/cpp0x/variadic96.C
@@ -0,0 +1,26 @@
+// Contributed by Dodji Seketeli <dodji@redhat.com>
+// Origin: PR c++/41785
+// { dg-options -std=c++0x }
+
+struct a {};
+
+template < typename T, typename ENCLOSING >
+struct base;
+
+template < typename... T >
+struct derived
+  : public base< T, derived< T... > >...
+{};
+
+template < typename... T>
+struct base< a, derived< T... > >
+{
+  typedef derived< T... >
+          Derived;
+};
+
+int main()
+{
+  derived< a > instance;
+}
+
