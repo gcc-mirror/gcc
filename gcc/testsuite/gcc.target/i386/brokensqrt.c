42a868cd5053099a2780f28969be964fabf9e8bc
matz <matz@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-10-29 16:41:10 +0000

    * tree-ssa-math-opts.c (execute_convert_to_rsqrt): Remove. 	(gate_convert_to_rsqrt): Ditto. 	(pass_convert_to_rsqrt): Ditto. 	* tree-pass.h (pass_convert_to_rsqrt): Don't declare. 	* passes.c (init_optimization_passes): Don't add pass_convert_to_rsqrt 	to pass list.
    	* config/i386/i386.c (ix86_emit_swdivsf): Change evaluation order.
    
    testsuite/
    	* gcc.target/i386/sse-recip.c: Use fabsf instead of != .
    	* gcc.target/i386/sse-recip-vec.c: Ditto.
    	* gcc.target/i386/brokensqrt.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@153713 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/brokensqrt.c
@@ -0,0 +1,29 @@
+/* { dg-do run } */
+/* { dg-options "-O2 -ffast-math -msse -mfpmath=sse -mrecip" } */
+#include "sse-check.h"
+
+extern float sqrtf (float);
+float __attribute__((noinline)) broken (float a, float b)
+{
+  return sqrtf (a / b);
+}
+
+extern void abort (void);
+extern void *memcpy (void *, const void *, __SIZE_TYPE__);
+static void
+sse_test (void)
+{
+  int i;
+  float x;
+  char buf[sizeof (float)];
+  x = broken (0.0f, 10000.0f);
+  /* A convoluted way to check for the correct result (zero) for all
+     floating point formats.
+     We can't use ==, !=, or range checks, or isinf/isnan/isunordered,
+     because all of these will not do the right thing under -ffast-math,
+     as they can assume that neither nan nor inf are returned.  */
+  memcpy (&buf, &x, sizeof (float));
+  for (i = 0; i < sizeof (float); i++)
+    if (buf[i] != 0)
+      abort ();
+}
