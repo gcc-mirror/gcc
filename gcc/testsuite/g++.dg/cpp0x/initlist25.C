14e7937304366acb860ca11cd97e57c62f4990f7
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-11-01 05:06:42 +0000

    PR c++/41754 	* call.c (compare_ics): Avoid bad union use when 	comparing two ck_lists.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@153788 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/initlist25.C
@@ -0,0 +1,17 @@
+// PR c++/41754
+// { dg-options -std=c++0x }
+// { dg-do run }
+
+#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        map<string, string> m;
+        m.insert({{"t", "t"}, {"y", "y"}});
+
+        return 0;
+}
