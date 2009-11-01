d99419eb0cd86223baeb9d06d528cfda2cbcd1b0
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-11-01 12:43:42 +0000

    2009-11-01  Tobias Burnus  <burnus@net-b.de>
            PR fortran/41850
            * trans-expr.c (gfc_conv_procedure_call): Deallocate intent-out
            variables only when present. Remove unneccessary present check.
    
    2009-11-01  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/41850
            * gfortran.dg/intent_out_6.f90: New testcase.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@153793 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/intent_out_6.f90
@@ -0,0 +1,39 @@
+! { dg-do run }
+!
+! PR fortran/41850
+!
+module test_module
+  implicit none
+contains
+  subroutine sub2(a)
+    implicit none
+    real,allocatable,intent(out),optional :: a(:)
+    if(present(a)) then
+      if(allocated(a)) call abort()
+      allocate(a(1))
+      a(1) = 5
+    end if
+  end subroutine sub2
+  subroutine sub1(a)
+    implicit none
+    real,allocatable,intent(out),optional :: a(:)
+!    print *,'in sub1'
+    call sub2(a)
+    if(present(a)) then
+      if(a(1) /= 5) call abort()
+    end if
+  end subroutine sub1
+end module test_module
+
+program test
+  use test_module
+  implicit none
+  real, allocatable :: x(:)
+  allocate(x(1))
+  call sub1()
+  x = 8
+  call sub1(x)
+  if(x(1) /= 5) call abort()
+end program
+
+! { dg-final { cleanup-modules "test_module" } }
