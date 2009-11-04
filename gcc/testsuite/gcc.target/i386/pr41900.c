139ffe66e5fe0f90782f3ae5fe18db0853ff583a
Uros Bizjak <uros@gcc.gnu.org>
2009-11-04 15:14:49 +0100

    re PR target/41900 (call *%esp shouldn't be generated because of CPU errata)
    	PR target/41900
    	* config/i386/i386.h (ix86_arch_indices) <X86_ARCH_CALL_ESP>: New.
    	(TARGET_CALL_ESP): New define.
    	* config/i386/i386.c (initial_ix86_tune_features): Initialize
    	X86_ARCH_CALL_ESP.
    	* config/i386/i386.md 
    	(*call_pop_1_esp, *call_1_esp, *call_value_pop_1_esp,
    	*call_value_1_esp): Rename from *call_pop_1, *call_1,
    	*call_value_pop_1 and *call_value_1.  Depend on TARGET_CALL_ESP.
    	(*call_pop_1, *call_1, *call_value_pop_1, *call_value_1):
    	New patterns, use "lsm" as operand 1 constraint.
    	* config/i386/predicates.md (call_insn_operand): Depend on 
    	index_register_operand for !TARGET_CALL_ESP to avoid %esp register.
    
    testsuite/ChangeLog:
    
    	PR target/41900
    	* gcc.target/i386/pr41900.c: New test.
    
    From-SVN: r153896

---
+++ gcc/testsuite/gcc.target/i386/pr41900.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target ilp32 } */
+/* { dg-options "-O2 -fomit-frame-pointer -mpreferred-stack-boundary=2" } */
+
+int main ()
+{
+  volatile unsigned code = 0xc3;
+
+  ((void (*)(void)) &code) ();
+  return 0;
+}
+
+/* { dg-final { scan-assembler-not "call\[ \\t\]+\\*%esp" } } */
