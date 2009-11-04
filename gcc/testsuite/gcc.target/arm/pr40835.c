446a1e961a07bf1f495bf06e7c08363f0b6fa3de
rearnsha <rearnsha@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-11-04 14:09:55 +0000

    2009-11-04  Richard Earnshaw  <rearnsha@arm.com>
    	PR target/40835
    	* arm.md (peephole2 patterns for move and compare): New.
    
    2009-11-04  Wei Guozhi  <carrot@google.com>
    
    	PR target/40835
    	* gcc.target/arm/pr40835: New testcase.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@153895 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/pr40835.c
@@ -0,0 +1,55 @@
+/* { dg-options "-mthumb -Os -march=armv5te" }  */
+/* { dg-final { scan-assembler-not "cmp" } } */
+
+int bar();
+void goo(int, int);
+
+void eq()
+{
+  int v = bar();
+  if (v == 0)
+    return;
+  goo(1, v);
+}
+
+void ge()
+{
+  int v = bar();
+  if (v >= 0)
+    return;
+  goo(1, v);
+}
+
+void gt()
+{
+  int v = bar();
+  if (v > 0)
+    return;
+  goo(1, v);
+}
+
+void lt()
+{
+  int v = bar();
+  if (v < 0)
+    return;
+  goo(1, v);
+}
+
+void le()
+{
+  int v = bar();
+  if (v <= 0)
+    return;
+  goo(1, v);
+}
+
+unsigned int foo();
+
+void leu()
+{
+  unsigned int v = foo();
+  if (v <= 0)
+    return;
+  goo(1, v);
+}
