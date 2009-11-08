1770a4b65afb87436af705fec62c9d382ef68e6f
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-11-08 03:47:44 +0000

    * tree.c (reconstruct_complex_type): Preserve attributes.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@154007 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree.c
+++ gcc/tree.c
@@ -9214,7 +9214,8 @@ reconstruct_complex_type (tree type, tree bottom)
   else
     return bottom;
 
-  return build_qualified_type (outer, TYPE_QUALS (type));
+  return build_type_attribute_qual_variant (outer, TYPE_ATTRIBUTES (type),
+					    TYPE_QUALS (type));
 }
 
 /* Returns a vector tree node given a mode (integer, vector, or BLKmode) and
