7b906350518fd0cb431a5e8c2a45bcb1449cdab2
Uros Bizjak <uros@gcc.gnu.org>
2009-11-08 19:10:10 +0100

    backport: re PR middle-end/41963 (177.mesa in SPEC CPU 2K is miscompiled)
    	Backport from mainline:
    	2009-11-06  Michael Matz  <matz@suse.de>
    
    	PR middle-end/41963
    	* tree-ssa-math-opts.c (execute_cse_reciprocals): Check all uses
    	of a potential reciprocal to really be reciprocals.
    
    testsuite/ChangeLog:
    
    	Backport from mainline:
    	2009-11-06  Michael Matz  <matz@suse.de>
    
    	PR middle-end/41963
    	* gcc.target/i386/pr41963.c: New test.
    
    From-SVN: r154011

---
+++ gcc/testsuite/gcc.target/i386/pr41963.c
@@ -0,0 +1,36 @@
+/* { dg-do run } */
+/* { dg-options "-O2 -ffast-math -mrecip" } */
+#include <math.h>
+
+extern float sqrtf(float);
+
+static __attribute__((noinline)) void f (float *dst, float *src)
+{
+  int i, j;
+  for (i = 0; i < 2; i++)
+    {
+      float len;
+      dst[0] = src[0];
+      dst[1] = src[1];
+      len = sqrtf (dst[0] * dst[0] + dst[1] * dst[1]);
+      if (len > 0.5f)
+	{
+	  len = 1.0f / len;
+	  dst[0] *= len;
+	  dst[1] *= len;
+	}
+    }
+}
+
+extern void abort (void);
+
+int main()
+{
+  float dst[2], src[2];
+  src[0] = 2.0f;
+  src[1] = 5.0f;
+  f (dst, src);
+  if (fabsf (dst[0] * dst[0] + dst[1] * dst[1] - 1.0f) > 0.01f)
+    abort ();
+  return 0;
+}
