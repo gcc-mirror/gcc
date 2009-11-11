4d9b926d2288abbcd1c331385187e988a5b8377d
janus <janus@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-11-11 22:37:31 +0000

    2009-11-11  Janus Weil  <janus@gcc.gnu.org>
    	PR fortran/41978
    	* resolve.c (resolve_ref): Take care of procedure pointer component
    	references.
    
    
    2009-11-11  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/41978
    	* gfortran.dg/proc_ptr_comp_22.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@154107 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/proc_ptr_comp_22.f90
@@ -0,0 +1,31 @@
+! { dg-do compile }
+!
+! PR 41978: [F03] ICE in gfc_conv_expr_descriptor for array PPC assignment
+!
+! Contributed by Daniel Kraft <domob@gcc.gnu.org>
+
+MODULE m
+  IMPLICIT NONE
+
+  TYPE t
+    PROCEDURE(myproc), POINTER, PASS :: myproc
+  END TYPE t
+
+CONTAINS
+
+  INTEGER FUNCTION myproc (me)
+    CLASS(t), INTENT(IN) :: me
+    myproc = 42
+  END FUNCTION myproc
+
+END MODULE m
+
+PROGRAM main
+  USE m
+  IMPLICIT NONE
+
+  TYPE(t) :: arr(2)
+  arr%myproc => myproc  ! { dg-error "must not have the POINTER attribute" }
+END PROGRAM main
+ 
+! { dg-final { cleanup-modules "m" } }
