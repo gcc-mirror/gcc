8ad7097c112322eb6048f6d465b540676b0d2d2e
Paolo Carlini <paolo.carlini@oracle.com>
2009-11-13 15:06:28 +0000

    algorithm: Include <utility>, per UK-300.
    2009-11-13  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* include/std/algorithm: Include <utility>, per UK-300.
    	* include/bits/stl_algobase.h: Don't do it here.
    
    From-SVN: r154152

--- libstdc++-v3/include/bits/stl_algobase.h
+++ libstdc++-v3/include/bits/stl_algobase.h
@@ -63,12 +63,13 @@
 #include <bits/cpp_type_traits.h>
 #include <ext/type_traits.h>
 #include <ext/numeric_traits.h>
+#include <bits/stl_pair.h>
 #include <bits/stl_iterator_base_types.h>
 #include <bits/stl_iterator_base_funcs.h>
 #include <bits/stl_iterator.h>
 #include <bits/concept_check.h>
 #include <debug/debug.h>
-#include <utility> // UK-300.
+#include <bits/move.h> // For std::swap and _GLIBCXX_MOVE
 
 _GLIBCXX_BEGIN_NAMESPACE(std)
 
