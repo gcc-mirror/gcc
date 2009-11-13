fc1e08468e6e828266648443e01c61e213aa33a8
Jason Merrill <jason@redhat.com>
2009-11-13 09:40:13 -0500

    re PR c++/11987 (Accepts-invalid with inherited nested type)
    	PR c++/11987
    	* parser.c (cp_parser_direct_declarator): Give helpful error about
    	trying to define member of a dependent typedef.
    	* pt.c (resolve_typename_type): Don't resolve a typedef typename.
    	* tree.c (typedef_variant_p): New.
    	* cp-tree.h: Declare it.
    
    From-SVN: r154149

---
+++ gcc/testsuite/g++.dg/parse/typename11.C
@@ -0,0 +1,16 @@
+// PR c++/11987
+
+template <int dim> struct X {
+  struct I { I(); };
+};
+
+template <int dim> struct Y : X<dim> {
+  typedef typename X<dim>::I I;
+};
+
+// note: I is nested type in X, not Y!
+template <int dim>
+Y<dim>::I::I () {}		// { dg-error "dependent typedef" }
+// { dg-error "no type|dependent type" "" { target *-*-* } 13 }
+
+template struct Y<1>;
