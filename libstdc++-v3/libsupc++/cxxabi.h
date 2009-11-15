4c264311181b8d2e8f5128098f5c2867909e997f
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-11-15 20:59:55 +0000

    2009-11-15  Steve Ward  <planet36@gmail.com>
    	* libsupc++/cxxabi.h (__cxa_demangle): Fix typo in comment.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@154191 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/libsupc++/cxxabi.h
+++ libstdc++-v3/libsupc++/cxxabi.h
@@ -164,7 +164,7 @@ namespace __cxxabiv1
    *
    *  @param __status @a *__status is set to one of the following values:
    *   0: The demangling operation succeeded.
-   *  -1: A memory allocation failiure occurred.
+   *  -1: A memory allocation failure occurred.
    *  -2: @a mangled_name is not a valid name under the C++ ABI mangling rules.
    *  -3: One of the arguments is invalid.
    *
