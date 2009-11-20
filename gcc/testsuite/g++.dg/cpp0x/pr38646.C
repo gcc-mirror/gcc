2282976b05c99c73fff7d23cb1af92b07113f078
Simon Martin <simartin@users.sourceforge.net>
2009-11-20 21:37:23 +0000

    re PR c++/38646 (ICE with invalid specialization of variadic template)
    gcc/cp/
    
    2009-11-20  Simon Martin  <simartin@users.sourceforge.net>
    
    	PR c++/38646
    	* pt.c (process_partial_specialization): Do not turn wrongly located
    	parameter pack arguments into error_mark_node.
    	Split too long lines into two.
    
    gcc/testsuite/
    
    2009-11-20  Simon Martin  <simartin@users.sourceforge.net>
    
    	PR c++/38646
    	* g++.dg/cpp0x/pr38646.C: New test.
    
    From-SVN: r154378

---
+++ gcc/testsuite/g++.dg/cpp0x/pr38646.C
@@ -0,0 +1,12 @@
+/* PR c++/38646 */
+/* { dg-do "compile" } */
+/* { dg-options "-std=c++0x" } */
+
+template<int...> struct A;
+
+template<int... N> struct A<N..., N...> /* { dg-error "must be at the end" } */
+{
+  template<typename> struct B;
+
+  template<typename T> struct B<T*> {};
+};
