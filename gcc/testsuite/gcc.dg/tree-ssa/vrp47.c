b5308c02b4a47313308859129f345336faaf588d
Andreas Krebbel <Andreas.Krebbel@de.ibm.com>
2009-11-20 12:58:48 +0000

    vrp47.c: Skip on S/390.
    2009-11-20  Andreas Krebbel  <Andreas.Krebbel@de.ibm.com>
    
    	* gcc.dg/tree-ssa/vrp47.c: Skip on S/390.
    
    From-SVN: r154363

--- gcc/testsuite/gcc.dg/tree-ssa/vrp47.c
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp47.c
@@ -1,6 +1,9 @@
 /* Skip on MIPS, where LOGICAL_OP_NON_SHORT_CIRCUIT inhibits the setcc
    optimizations that expose the VRP opportunity.  */
-/* { dg-do compile { target { ! mips*-*-* } } } */
+/* Skip on S/390.  Lower values in BRANCH_COST lead to two conditional
+   jumps when evaluating an && condition.  VRP is not able to optimize
+   this.  */
+/* { dg-do compile { target { ! mips*-*-* ! s390*-*-* } } } */
 /* { dg-options "-O2 -fdump-tree-vrp -fdump-tree-dom" } */
 
 int h(int x, int y)
