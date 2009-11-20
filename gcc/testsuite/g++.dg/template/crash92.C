de31360209ac0686c27228b1f4daeee7c669abfd
H.J. Lu <hongjiu.lu@intel.com>
2009-11-20 18:53:27 +0000

    pr41935.c: Removed.
    2009-11-20  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* c-c++-common/pr41935.c: Removed.
    	* g++.dg/template/crash92.C: Likewise.
    	* gfortran.dg/missing_optional_dummy_6.f90: Likewise.
    
    From-SVN: r154376

--- gcc/testsuite/g++.dg/template/crash92.C
+++
@@ -1,7 +0,0 @@
-// PR c++/42055
-
-template<typename T> void foo(T, T); // { dg-error "candidates|template" }
-
-template<typename T> void foo(T, int); // { dg-error "template" }
-
-template void foo(int, int); // { dg-error "ambiguous template specialization" }
