27bcaf0476caeaed36b3e739a4ea79dabebfca24
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2009-11-22 02:05:12 +0000

    re PR fortran/41807 (data statement with nested type constructors)
    2009-11-21  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR fortran/41807
    	* gfortran.dg/data_value_1.f90: Update test.
    	* gfortran.dg/array_constructor_32.f90: New test.
    
    From-SVN: r154419

--- gcc/testsuite/gfortran.dg/data_value_1.f90
+++ gcc/testsuite/gfortran.dg/data_value_1.f90
@@ -3,12 +3,14 @@
 ! is not a constant and so the DATA statement did not have
 ! a constant value expression.
 !
+! Modified dg-error for PR41807
+!
 ! Contributed by Philippe Marguinaud <philippe.marguinaud@meteo.fr>
 !
       TYPE POINT
         REAL :: X 
       ENDTYPE
       TYPE(POINT) :: P
-      DATA P / POINT(1.+X) / ! { dg-error "non-constant DATA value" }
+      DATA P / POINT(1.+X) / ! { dg-error "non-constant initialization" }
       print *, p
       END
