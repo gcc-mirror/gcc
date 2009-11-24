1270a51a44684c19fd9d9096252d371c107b878d
hutchinsonandy <hutchinsonandy@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-11-24 01:22:52 +0000

    * gcc.c-torture/execute/pr40404.c: Use long for bitfield on 16bit int targets.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@154487 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/pr40404.c
+++ gcc/testsuite/gcc.c-torture/execute/pr40404.c
@@ -1,7 +1,14 @@
 extern void abort (void);
+
+#if (__SIZEOF_INT__ <= 2)
+struct S {
+  unsigned long ui17 : 17;
+} s;
+#else
 struct S {
   unsigned int ui17 : 17;
 } s;
+#endif
 int main()
 {
   s.ui17 = 0x1ffff;
