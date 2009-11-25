9b196c9394288a3e892456bb3e51d84681de8ff1
Sebastian Pop <sebastian.pop@amd.com>
2009-11-25 05:11:38 +0000

    interchange-7.c: Un-XFAILed.
    2009-10-16  Sebastian Pop  <sebastian.pop@amd.com>
    
    	* gcc.dg/graphite/interchange-7.c: Un-XFAILed.
    	* gcc.dg/graphite/interchange-9.c: Un-XFAILed.
    
    From-SVN: r154597

--- gcc/testsuite/gcc.dg/graphite/interchange-9.c
+++ gcc/testsuite/gcc.dg/graphite/interchange-9.c
@@ -7,8 +7,9 @@ foo (int *x)
   for (j = 0;  j < 10000; ++j)
     for (i = 0;  i < 10000; ++i)
       sum += x[10000 * i + j];
+
   return sum;
 }
 
-/* { dg-final { scan-tree-dump-times "will be interchanged" 1 "graphite" { xfail *-*-* } } } */ 
+/* { dg-final { scan-tree-dump-times "will be interchanged" 1 "graphite" } } */
 /* { dg-final { cleanup-tree-dump "graphite" } } */
