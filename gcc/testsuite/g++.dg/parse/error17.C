85539630e3abdad70f84d87eb0a4458f44961e03
Paolo Carlini <paolo.carlini@oracle.com>
2009-11-27 10:44:49 +0000

    re PR c++/35112 (ICE and broken diagnostic with ambiguous class name)
    /cp
    2009-11-27  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/35112
    	* pt.c (print_overloaded_functions): New.
    	(print_candidates): Call the latter.
    	* parser.c (cp_parser_class_name): Do not duplicate the diagnostics
    	after the cp_parser_lookup_name call.
    
    /testsuite
    2009-11-27  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/35112
    	* g++.dg/parse/crash53.C: New.
    	* g++.dg/parse/error17.C: Adjust, error messages are not
    	duplicated anymore.
    	* g++.dg/template/dtor7.C: Remove xfail-ed dg-bogus directives.
    
    From-SVN: r154698

--- gcc/testsuite/g++.dg/parse/error17.C
+++ gcc/testsuite/g++.dg/parse/error17.C
@@ -2,10 +2,8 @@
 // PR c++/16965
 
 template <typename T> struct B { 
-  static int Bar(T); // { dg-error "14:candidates are: " "1" }
-  // { dg-error "14:with T = int" "2" { target *-*-* } 5 }
+  static int Bar(T); // { dg-error "14:candidates are: |with T = int" }
 }; 
 struct D : B<int>, B<char> {}; 
  
-int i2 = D::Bar(2); // { dg-error "13:reference to 'Bar' is ambiguous" }
-// { dg-error "10:reference to 'Bar' is ambiguous" "2" { target *-*-* } 10 }
+int i2 = D::Bar(2); // { dg-error "10:reference to 'Bar' is ambiguous" }
