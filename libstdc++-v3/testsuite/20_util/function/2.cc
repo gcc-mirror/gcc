d276e8d68c5f22b2f7198d251915be6360d60e9b
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-11-30 09:34:04 +0000

    2009-11-30  Paolo Carlini  <paolo.carlini@oracle.com>
    	* include/tr1_impl/functional: Remove file, copy its contents,
    	trivially adjusted...
    	* include/std/functional: ... here, and...
    	* include/tr1_impl/functional: ... here.
    	* include/Makefile.am: Adjust.
    	* include/Makefile.in: Regenerate.
    
    2009-11-30  Jonathan Wakely  <jwakely.gcc@gmail.com>
    
            * include/tr1_impl/functional (function): Add rvalue support and
            tweak doxygen markup.
            * testsuite/20_util/function/assign/move.cc: New.
            * testsuite/20_util/function/cons/move.cc: New.
            * testsuite/20_util/function/invoke/move_only.cc: New.
            * testsuite/20_util/function/cmp/cmp_neg.cc: New.
            * testsuite/20_util/function/1.cc: Copy from testsuite/tr1/.
            * testsuite/20_util/function/2.cc: Likewise.
            * testsuite/20_util/function/3.cc: Likewise.
            * testsuite/20_util/function/4.cc: Likewise.
            * testsuite/20_util/function/5.cc: Likewise.
            * testsuite/20_util/function/6.cc: Likewise.
            * testsuite/20_util/function/7.cc: Likewise.
            * testsuite/20_util/function/8.cc: Likewise.
            * testsuite/20_util/function/9.cc: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@154759 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/20_util/function/2.cc
@@ -0,0 +1,78 @@
+// { dg-options "-std=gnu++0x" }
+// 2005-01-15 Douglas Gregor <dgregor@cs.indiana.edu>
+//
+// Copyright (C) 2005, 2009 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 20.7.15 polymorphic function object wrapper
+#include <functional>
+#include <testsuite_hooks.h>
+#include <testsuite_tr1.h>
+
+using namespace __gnu_test;
+
+bool test __attribute__((unused)) = true;
+
+// Put function pointers into function<> wrappers
+void test02()
+{
+  using std::function;
+
+  function<int(float)> f1(truncate_float);
+  VERIFY( f1 );
+  VERIFY( !!f1 );
+  VERIFY( !(f1 == 0) );
+  VERIFY( !(0 == f1) );
+  VERIFY( f1 != 0 );
+  VERIFY( 0 != f1 );
+
+  // Copy-construction
+  function<int(float)> f2(f1);
+  VERIFY( f2 );
+
+  // Invocation
+  VERIFY( f1(3.1f) == 3 );
+  VERIFY( f2(3.1f) == 3 );
+
+  // Assignment to zero
+  f1 = 0;
+  VERIFY( !f1 );
+
+  // Swap
+  f1.swap(f2);
+  VERIFY( f1 );
+  VERIFY( !f2 );
+  VERIFY( f1(3.1f) == 3 );
+
+  // Assignment from a function pointer
+  f2 = truncate_float;
+  VERIFY( f2(3.1f) == 3 );
+
+  // target_type and target() functions
+  const function<int(float)>& f1c = f1;
+  VERIFY( typeid(int(*)(float)) == f1.target_type() );
+  VERIFY( f2.target<int(*)(float)>() != 0 );
+  VERIFY( *f2.target<int(*)(float)>() == &truncate_float );
+  VERIFY( f1c.target<int(*)(float)>() != 0 );
+  VERIFY( *f1c.target<int(*)(float)>() == &truncate_float );
+}
+
+int main()
+{
+  test02();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/20_util/function/5.cc
@@ -0,0 +1,107 @@
+// { dg-options "-std=gnu++0x" }
+// 2005-01-15 Douglas Gregor <dgregor@cs.indiana.edu>
+//
+// Copyright (C) 2005, 2009 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 20.7.15 polymorphic function object wrapper
+#include <functional>
+#include <testsuite_hooks.h>
+#include <testsuite_tr1.h>
+
+using namespace __gnu_test;
+
+bool test __attribute__((unused)) = true;
+
+// Put member pointers into function<> wrappers
+void test05()
+{
+  using std::function;
+
+  X x;
+  x.bar = 17;
+
+  function<int(X&)> frm(&X::bar);
+  VERIFY( frm );
+  VERIFY( frm(x) == 17 );
+  VERIFY( typeid(int X::*) == frm.target_type() );
+  VERIFY( *frm.target<int X::*>() == &X::bar );
+
+  function<int(X&)> fr(&X::foo);
+  VERIFY( fr );
+  VERIFY( fr(x) == 1 );
+  VERIFY( typeid(int (X::*)()) == fr.target_type() );
+  VERIFY( *fr.target<int (X::*)()>() == &X::foo );
+
+  function<int(const X&)> frc(&X::foo_c);
+  VERIFY( frc );
+  VERIFY( frc(x) == 2 );
+  VERIFY( typeid(int (X::*)() const) == frc.target_type() );
+  VERIFY( *frc.target<int (X::*)() const >() == &X::foo_c );
+
+  function<int(volatile X&)> frv(&X::foo_v);
+  VERIFY( frv );
+  VERIFY( frv(x) == 3 );
+  VERIFY( typeid(int (X::*)() volatile) == frv.target_type() );
+  VERIFY( *frv.target<int (X::*)() volatile >() == &X::foo_v );
+  VERIFY( frv.target<int (X::*)() const volatile>() == 0 );
+
+  function<int(const volatile X&)> frcv(&X::foo_cv);
+  VERIFY( frcv );
+  VERIFY( frcv(x) == 4 );
+  VERIFY( typeid(int (X::*)() const volatile) == frcv.target_type() );
+  VERIFY( *frcv.target<int (X::*)() const volatile >() == &X::foo_cv );
+  VERIFY( frcv.target<int (X::*)() const>() == 0 );
+
+  function<int(X*)> grm(&X::bar);
+  VERIFY( grm );
+  VERIFY( grm(&x) == 17 );
+  VERIFY( typeid(int X::*) == grm.target_type() );
+  VERIFY( *grm.target<int X::*>() == &X::bar );
+
+  function<int(X*)> gr(&X::foo);
+  VERIFY( gr );
+  VERIFY( gr(&x) == 1 );
+  VERIFY( typeid(int (X::*)()) == gr.target_type() );
+  VERIFY( *gr.target<int (X::*)()>() == &X::foo );
+
+  function<int(const X*)> grc(&X::foo_c);
+  VERIFY( grc );
+  VERIFY( grc(&x) == 2 );
+  VERIFY( typeid(int (X::*)() const) == grc.target_type() );
+  VERIFY( *grc.target<int (X::*)() const >() == &X::foo_c );
+
+  function<int(volatile X*)> grv(&X::foo_v);
+  VERIFY( grv );
+  VERIFY( grv(&x) == 3 );
+  VERIFY( typeid(int (X::*)() volatile) == grv.target_type() );
+  VERIFY( *grv.target<int (X::*)() volatile >() == &X::foo_v );
+  VERIFY( grv.target<int (X::*)() const volatile>() == 0 );
+
+  function<int(const volatile X*)> grcv(&X::foo_cv);
+  VERIFY( grcv );
+  VERIFY( grcv(&x) == 4 );
+  VERIFY( typeid(int (X::*)() const volatile) == grcv.target_type() );
+  VERIFY( *grcv.target<int (X::*)() const volatile >() == &X::foo_cv );
+  VERIFY( grcv.target<int (X::*)() const>() == 0 );
+}
+
+int main()
+{
+  test05();
+  return 0;
+}
