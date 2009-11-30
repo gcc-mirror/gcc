831b9ef8f0c8f6a068aa34bcccac81f7405f8d1e
Richard Guenther <rguenther@suse.de>
2009-11-30 10:36:54 +0000

    tree-dump.c (dump_option_value_in): Add TDF_NOUID.
    2009-11-30  Richard Guenther  <rguenther@suse.de>
    
    	* tree-dump.c (dump_option_value_in): Add TDF_NOUID.
    	* tree-pass.h (TDF_NOUID): Likewise.
    	* print-rtl.c: Include tree-pass.h.
    	(print_mem_expr): Pass dump_flags.
    	(print_rtx): Likewise.
    	* print-tree.c: Include tree-pass.h.
    	(print_node_brief): Handle TDF_NOUID.
    	(print_node): Likewise.
    	* tree-pretty-print.c (dump_decl_name): Likewise.
    	(dump_generic_node): Likewise.
    	* Makefile.in (print-rtl.o, print-tree.o): Add $(TREE_PASS_H)
    	dependency.
    
    From-SVN: r154775

--- gcc/tree-dump.c
+++ gcc/tree-dump.c
@@ -821,6 +821,7 @@ static const struct dump_option_value_info dump_options[] =
   {"memsyms", TDF_MEMSYMS},
   {"verbose", TDF_VERBOSE},
   {"eh", TDF_EH},
+  {"nouid", TDF_NOUID},
   {"all", ~(TDF_RAW | TDF_SLIM | TDF_LINENO | TDF_TREE | TDF_RTL | TDF_IPA
 	    | TDF_STMTADDR | TDF_GRAPH | TDF_DIAGNOSTIC | TDF_VERBOSE
 	    | TDF_RHS_ONLY)},
