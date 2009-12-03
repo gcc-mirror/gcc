7691a8456d46a5684c9d60bf3cbdcf7648658e4d
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-12-03 10:18:04 +0000

    2009-12-03  Paolo Carlini  <paolo.carlini@oracle.com>
    	* testsuite/20_util/bind/ref.cc: Add.
    	* testsuite/20_util/bind/all_bound.cc: Likewise.
    	* testsuite/20_util/bind/nested.cc: Likewise.
    	* testsuite/20_util/bind/placeholders.cc: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@154939 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/20_util/bind/all_bound.cc
@@ -0,0 +1,39 @@
+// { dg-options "-std=gnu++0x" }
+// Copyright (C) 2009 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 20.7.11 Function template bind
+
+#include <functional>
+#include <testsuite_hooks.h>
+
+// Operations on empty function<> objects
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  VERIFY( std::bind(std::plus<int>(), 3, 5)() == 8 );
+  VERIFY( std::bind(std::minus<int>(), 3, 5)() == -2 );
+  VERIFY( std::bind<int>(std::plus<int>(), 3, 5)() == 8 );
+  VERIFY( std::bind<int>(std::minus<int>(), 3, 5)() == -2 );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
