35b5bba18d2d02af170644bbfe76a1bb9925cf0c
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-12-04 03:09:43 +0000

    * g++.dg/abi/guard2.C: Run on *-*-linux* rather than x86-*-*
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@154966 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/abi/guard2.C
+++ gcc/testsuite/g++.dg/abi/guard2.C
@@ -1,6 +1,6 @@
 // PR c++/41611
 // Test that the guard gets its own COMDAT group.
-// { dg-final { scan-assembler "_ZGVZN1A1fEvE1i,comdat" { target i?86-*-* x86_64-*-* } } }
+// { dg-final { scan-assembler "_ZGVZN1A1fEvE1i,comdat" { target *-*-linux* } } }
 
 struct A {
   static int f()
