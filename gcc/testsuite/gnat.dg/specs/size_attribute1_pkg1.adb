321e10dd4ceaa5675213936b5d4be25c5b64c551
Eric Botcazou <ebotcazou@adacore.com>
2009-12-04 12:05:08 +0000

    trans.c (add_decl_expr): At toplevel, mark the TYPE_ADA_SIZE field of records and unions.
    	* gcc-interface/trans.c (add_decl_expr): At toplevel, mark the
    	TYPE_ADA_SIZE field of records and unions.
    
    	* gcc-interface/trans.c (Attribute_to_gnu) <Attr_Length>: Set the
    	source location of the node onto the comparison expression if it
    	is not cached.
    
    From-SVN: r154978

---
+++ gcc/testsuite/gnat.dg/specs/size_attribute1_pkg1.adb
@@ -0,0 +1,13 @@
+package body Size_Attribute1_Pkg1 is
+
+  type Rec is
+    record
+      F : T;
+    end record;
+
+  procedure Dummy is
+  begin
+    null;
+  end;
+
+end Size_Attribute1_Pkg1;
---
+++ gcc/testsuite/gnat.dg/specs/size_attribute1_pkg2.adb
@@ -0,0 +1,9 @@
+package body Size_Attribute1_Pkg2 is
+
+  procedure Proc is
+    I : Integer := T'Size;
+  begin
+    null;
+  end;
+
+end Size_Attribute1_Pkg2;
