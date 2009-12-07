7fccdfcfb9e6dd397d43f00551d28b09192b363d
Jakub Jelinek <jakub@redhat.com>
2009-12-07 23:23:01 +0100

    Fix ABM.
    2009-12-04  Jakub Jelinek  <jakub@redhat.com>
    
    	* config/i386/i386.c (bdesc_special_args): Move __builtin_clzs from
    	this array ...
    	(bdesc_args): ... here.
    	* config.gcc (i[34567]86-*-*): Include abmintrin.h.
    	(x86_64-*-*): Likewise.
    
    	* gcc.target/i386/sse-12.c: Add -mabm to dg-options, mention
    	abmintrin.h is also tested.
    	* gcc.target/i386/sse-13.c: Likewise.
    
    From-SVN: r155059

--- gcc/testsuite/gcc.target/i386/sse-12.c
+++ gcc/testsuite/gcc.target/i386/sse-12.c
@@ -1,7 +1,8 @@
-/* Test that {,x,e,p,t,s,w,a,b,i}mmintrin.h, xopintrin.h, mm3dnow.h and mm_malloc.h are
-   usable with -O -std=c89 -pedantic-errors.  */
+/* Test that {,x,e,p,t,s,w,a,b,i}mmintrin.h, xopintrin.h, mm3dnow.h,
+   abmintrin.h and mm_malloc.h are usable with -O -std=c89
+   -pedantic-errors.  */
 /* { dg-do compile } */
-/* { dg-options "-O -std=c89 -pedantic-errors -march=k8 -m3dnow -mavx -mfma4 -mxop -maes -mpclmul" } */
+/* { dg-options "-O -std=c89 -pedantic-errors -march=k8 -m3dnow -mavx -mfma4 -mxop -maes -mpclmul -mabm" } */
 
 #include <x86intrin.h>
 
