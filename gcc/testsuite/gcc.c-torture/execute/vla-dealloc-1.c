4f566bd60e0e2fbdd34da8514643fe723379f40e
Andy Hutchinson <hutchinsonandy@gcc.gnu.org>
2009-12-07 00:21:47 +0000

    * gcc.c-torture/execute/vla-dealloc-1.c: Use lower loop count for 16bit int targets.
    From-SVN: r155029

--- gcc/testsuite/gcc.c-torture/execute/vla-dealloc-1.c
+++ gcc/testsuite/gcc.c-torture/execute/vla-dealloc-1.c
@@ -1,6 +1,12 @@
 /* VLAs should be deallocated on a jump to before their definition,
    including a jump to a label in an inner scope.  PR 19771.  */
 
+#if (__SIZEOF_INT__ <= 2)
+#define LIMIT 10000
+#else
+#define LIMIT 1000000
+#endif
+
 void *volatile p;
 
 int
@@ -16,7 +22,7 @@ main (void)
   x[n % 1000] = 2;
   p = x;
   n++;
-  if (n < 1000000)
+  if (n < LIMIT)
     goto lab;
   return 0;
 }
