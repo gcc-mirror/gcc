d505015aa52a7113d13a185e2aa4c14e77c9a1fa
Sebastian Pop <spop@gcc.gnu.org>
2009-12-09 15:18:22 +0000

    re PR tree-optimization/42285 (ICE in Graphite's scan_tree_for_params for 416.gamess)
    Fix PR42285.
    
    2009-12-08  Sebastian Pop  <sebpop@gmail.com>
    
    	PR middle-end/42285
    	* graphite-scop-detection.c (graphite_can_represent_init): Also
    	handle more complex MULT_EXPRs containing parameters by recursion
    	on the structure.
    
    	* testsuite/gfortran.dg/graphite/pr42285.f90: New.
    
    From-SVN: r155108

---
+++ gcc/testsuite/gfortran.dg/graphite/pr42285.f90
@@ -0,0 +1,24 @@
+! { dg-options "-O2 -floop-interchange" }
+
+SUBROUTINE EFGRDM(NCF,NFRG,G,RTRMS,GM,IOPT,K1)
+  IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+  DIMENSION G(*),RTRMS(*),GM(*)
+
+  DUM = 0
+  DO I=1,NFRG
+     DO J=1,3
+        IF (IOPT.EQ.0) THEN
+           GM(K1)=G(K1)
+        END IF
+     END DO
+     DO J=1,3
+        JDX=NCF*9+IOPT*9*NFRG
+        DO M=1,3
+           DUM=DUM+RTRMS(JDX+M)
+        END DO
+        GM(K1)=DUM
+     END DO
+  END DO
+  RETURN
+END SUBROUTINE EFGRDM
+
