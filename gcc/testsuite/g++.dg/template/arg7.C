302e39e5a8a5a2449bdf345050409597e7ec0484
Jason Merrill <jason@redhat.com>
2009-12-11 00:26:51 -0500

    re PR c++/27425 (ICE with invalid template-template-parameter)
    	PR c++/27425
    	PR c++/34274
    	PR c++/42301
    	* pt.c (expand_template_argument_pack): Handle null arg gracefully.
    	(convert_template_argument): Use %T for type.
    
    From-SVN: r155149

---
+++ gcc/testsuite/g++.dg/template/arg7.C
@@ -0,0 +1,11 @@
+// PR c++/27425, 34274
+
+template<typename T> struct A
+{
+  template<template<T> class> struct B {}; // { dg-error "void|mismatch|expected" }
+  // { dg-bogus "not supported" "" { target *-*-* } 5 }
+  template<T> struct C;			   // { dg-error "void" }
+  B<C> b;
+};
+
+A<void> a;			// { dg-message "instantiated" }
