c2f2791fc5de3d41ccd3f41aa4d21018b171ede1
Ramana Radhakrishnan <ramana.radhakrishnan@arm.com>
2009-12-11 11:21:33 +0000

    Fix PR41574 on 4.4 branch.
    2009-12-11  Ramana Radhakrishnan  <ramana.radhakrishnan@arm.com>
    
    	2009-10-05  Doug Kwan  <dougkwan@google.com>
    
    	PR rtl-optimization/41574
    	* combine.c (distribute_and_simplify_rtx): Quit if RTX mode is
    	floating point and we are not doing unsafe math optimizations.
    
    
    2009-12-11  Ramana Radhakrishnan  <ramana.radhakrishnan@arm.com>
    
    	2009-10-08  Doug Kwan  <dougkwan@google.com>
    
    	PR rtl-optimization/41574
    	* gcc.dg/pr41574.c: New test.
    
    From-SVN: r155157

---
+++ gcc/testsuite/gcc.dg/pr41574.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -march=armv7-a -mfloat-abi=softfp -mfpu=neon -fno-unsafe-math-optimizations -fdump-rtl-combine" { target { arm*-*-* } } } */
+/* { dg-options "-O2 -fno-unsafe-math-optimizations -fdump-rtl-combine" { target { ! arm*-*-* } } } */
+
+
+static const double one=1.0;
+
+double
+f(double x)
+{
+  return x*(one+x);
+}
+
+/* { dg-final { scan-rtl-dump-not "\\(plus:DF \\(mult:DF" "combine" } } */
+/* { dg-final { cleanup-rtl-dump "combine*" } } */
