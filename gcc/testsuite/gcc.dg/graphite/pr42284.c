7cb792f51087a104ac286ca9483c25849e437733
Sebastian Pop <sebpop@gmail.com>
2009-12-14 16:51:43 +0000

    re PR tree-optimization/42284 (failing tree check in  graphite-sese-to-poly.c for 164.gzip)
    	PR middle-end/42284
    	* testsuite/gcc.dg/graphite/pr42284.c: New.
    
    From-SVN: r155219

---
+++ gcc/testsuite/gcc.dg/graphite/pr42284.c
@@ -0,0 +1,27 @@
+/* { dg-options "-O2 -fgraphite-identity" } */
+
+int
+huft_build (unsigned *b)
+{
+  int k;
+  for (k = 0; k <= 10; k++)
+    if (foo ());
+}
+int
+inflate_fixed ()
+{
+  int i;
+  unsigned l[288];
+  for (i = 0; i < 144; i++)
+    l[i] = 8;
+  for (; i < 256; i++)
+    l[i] = 9;
+  for (; i < 280; i++)
+    l[i] = 7;
+  for (; i < 288; i++)
+    l[i] = 8;
+  if ((i = huft_build (l)) != 0)
+    return i;
+  for (i = 0; i < 30; i++)
+    l[i] = 5;
+}
