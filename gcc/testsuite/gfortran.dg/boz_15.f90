3c0c50279beaa7857d301a864ff89e8a07bf09ed
Dominique d'Humieres <dominiq@lps.ens.fr>
2009-12-14 18:56:12 +0100

    boz_15.f90: Fix typos.
    	* gfortran.dg/boz_15.f90: Fix typos.
    
    From-SVN: r155225

--- gcc/testsuite/gfortran.dg/boz_15.f90
+++ gcc/testsuite/gfortran.dg/boz_15.f90
@@ -8,7 +8,7 @@
 ! which needs integer(16) support.
 !
 implicit none
-character(len=255) :: str
+character(len=256) :: str
 integer,parameter :: xp = selected_real_kind (precision (0.0d0)+1)
 real(xp)    :: r1,r2
 complex(xp) :: z1,z2
@@ -16,8 +16,8 @@ complex(xp) :: z1,z2
 r2 = 5.0_xp
 r1 = 2.0_xp
 ! Real B(OZ)
-write(str,'(b126)') r1
-read (str,'(b126)') r2
+write(str,'(b128)') r1
+read (str,'(b128)') r2
 if(r2 /= r1) call abort()
 ! Real (B)O(Z)
 r2 = 5.0_xp
@@ -33,8 +33,8 @@ if(r2 /= r1) call abort()
 z2 = cmplx(5.0_xp,7.0_xp)
 z1 = cmplx(2.0_xp,3.0_xp)
 ! Complex B(OZ)
-write(str,'(2b126)') z1
-read (str,'(2b126)') z2
+write(str,'(2b128)') z1
+read (str,'(2b128)') z2
 if(z2 /= z1) call abort()
 ! Complex (B)O(Z)
 z2 = cmplx(5.0_xp,7.0_xp)
