5e5482890def85e3e4f4a7c963dd807fc4a7de2e
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-12-14 20:14:49 +0000

    * g++.dg/other/i386-2.C: Add -mpopcnt -mabm -mlwp to dg-options. 	* g++.dg/other/i386-3.C: Ditto. 	* g++.dg/other/i386-5.C: Remove duplicated test. 	* g++.dg/other/i386-6.C: Ditto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155238 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/other/i386-6.C
+++
@@ -1,8 +0,0 @@
-/* Test that {,x,e,p,t,s,w,a,i}mmintrin.h, fma4intrin.h, xopintrin.h, mm3dnow.h and
-   mm_malloc.h are usable with -O -pedantic-errors.  */
-/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
-/* { dg-options "-O -pedantic-errors -march=k8 -m3dnow -mavx -msse4a -mfma4 -mxop -maes -mpclmul" } */
-
-#include <x86intrin.h>
-
-int dummy;
