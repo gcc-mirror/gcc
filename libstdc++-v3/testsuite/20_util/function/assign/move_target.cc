ec903a9c42e640af51c771cb3a844283acdc566b
Jonathan Wakely <jwakely.gcc@gmail.com>
2009-12-15 17:42:47 +0000

    functional (function::function): Move construct target.
    2009-12-15  Jonathan Wakely  <jwakely.gcc@gmail.com>
    
    	* include/std/functional (function::function): Move construct target.
    	(function::operator=): Use perfect forwarding for argument.
    	(function::operator()): Use new __throw_bad_function_call.
    	* include/bits/functexcept.h (__throw_bad_function_call): Declare.
    	* src/functexcept.cc (__throw_bad_function_call): Define.
    	* config/abi/pre/gnu.ver: Add new symbol.
    	* testsuite/20_util/function/cons/move_target.cc: New.
    	* testsuite/20_util/function/assign/move_target.cc: New.
    
    From-SVN: r155261

---
+++ libstdc++-v3/testsuite/20_util/function/assign/move_target.cc
@@ -0,0 +1,47 @@
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2009 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <functional>
+
+struct moveable
+{
+  moveable() = default;
+  ~moveable() = default;
+  // target object must be CopyConstructible,
+  // but should not be copied during this test
+  moveable(const moveable& c) { throw "copied"; }
+  moveable& operator=(const moveable&) = delete;
+  moveable(moveable&&) { }
+
+  void operator()() const { }
+};
+
+void test01()
+{
+  std::function<void ()> f;
+  f = moveable();
+  f();
+}
+
+int main()
+{
+  test01();
+
+  return 0;
+}
