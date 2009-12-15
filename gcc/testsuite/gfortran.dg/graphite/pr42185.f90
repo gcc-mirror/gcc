5daf19b17b2b5fff65f64a04d82f85d48251d047
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-12-15 15:17:46 +0000

    PR graphite/42185         * graphite-sese-to-poly.c (is_reduction_operation_p): Assert that         we are a GIMPLE_ASSIGN.  Do not calculate rhs code twice.         (follow_ssa_with_commutative_ops): Return NULL on non assignment.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155256 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/graphite/pr42185.f90
@@ -0,0 +1,27 @@
+! { dg-compile }
+! { dg-options "-fgraphite -O -ffast-math" }
+
+MODULE powell
+  INTEGER, PARAMETER :: dp=8
+CONTAINS
+  SUBROUTINE trsapp (n,npt,xopt,xpt,gq,hq,pq,delta,step,d,g,hd,hs,crvmin)
+    REAL(dp), DIMENSION(*), INTENT(INOUT)    :: step, d, g, hd, hs
+    LOGICAL                                  :: jump1, jump2
+    REAL(dp) :: alpha, angle, angtest, bstep, cf, cth, dd, delsq, dg, dhd, &
+      reduc, sg, sgk, shs, ss, sth, temp, tempa, tempb
+    DO i=1,n
+       dd=dd+d(i)**2
+    END DO
+    mainloop : DO
+       IF ( .NOT. jump2 ) THEN
+          IF ( .NOT. jump1 ) THEN
+             bstep=temp/(ds+SQRT(ds*ds+dd*temp))
+             IF (alpha < bstep) THEN
+                IF (ss < delsq) CYCLE mainloop
+             END IF
+             IF (gg <= 1.0e-4_dp*ggbeg) EXIT mainloop
+          END IF
+       END IF
+    END DO mainloop
+  END SUBROUTINE trsapp
+END MODULE powell
