fdd3340f47ab6153da62534d7b392e63078bfe78
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-12-17 22:07:13 +0000

    * dwarf2out.c (loc_descriptor): For SYMBOL_REFs and LABEL_REFs 	use DW_OP_addr+DW_OP_stack_value instead of DW_OP_implicit_value. 	(add_const_value_attribute): For CONST_STRING, SYMBOL_REFs and 	LABEL_REFs use DW_OP_addr+DW_OP_stack_value DW_AT_location instead of 	DW_AT_const_value.
    	* gcc.dg/debug/dwarf2/const-1.c: Don't expect DW_AT_const_value,
    	but instead DW_AT_location with DW_OP_addr+DW_OP_stack_value.  Add
    	-gno-strict-dwarf -fno-merge-debug-strings to dg-options.
    	* g++.dg/debug/dwarf2/const1.C: Likewise.
    	* g++.dg/debug/dwarf2/template-params-3.C: Likewise.
    	* g++.dg/debug/dwarf2/template-func-params-3.C: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155327 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/debug/dwarf2/const-1.c
+++ gcc/testsuite/gcc.dg/debug/dwarf2/const-1.c
@@ -1,7 +1,7 @@
 /* { dg-do compile } */
-/* { dg-options "-O -gdwarf-2 -dA" } */
+/* { dg-options "-O -gdwarf-2 -dA -gno-strict-dwarf -fno-merge-debug-strings" } */
 /* { dg-require-visibility "" } */
-/* { dg-final { scan-assembler "DW_AT_const_value" } } */
+/* { dg-final { scan-assembler "DW_AT_location\[^\\r\\n\]*\[\\r\\n\]*\[^\\r\\n\]*DW_OP_addr\[^\\r\\n\]*\[\\r\\n\]*\[^\\r\\n\]*fnx\[^\\r\\n\]*\[\\r\\n\]*\[^\\r\\n\]*DW_OP_stack_value" } } */
 
-extern void x() __attribute__((visibility("hidden")));
-static void (*f)() = x;
+extern void fnx() __attribute__((visibility("hidden")));
+static void (*f)() = fnx;
