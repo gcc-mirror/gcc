ae1e43b3acdaa2431b3b1c00785c21b4112f9dca
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-12-17 09:37:16 +0000

    2009-12-16  Benjamin Kosnik  <bkoz@redhat.com>
    	PR libstdc++/21772 part 2
    	* doc/xml/manual/test.xml: Add documentation about testing details.
    	* testsuite/util/exception/safety.h: New. Functor objects for
    	testing C++0x container classes.
    	* testsuite/util/testsuite_container_traits.h: Add traits.
    
    	* testsuite/23_containers/list/requirements/exception/
    	basic.cc: New.
    	generation_prohibited.cc: New.
    	propagation_consistent.cc: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155306 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/23_containers/list/requirements/exception/basic.cc
@@ -0,0 +1,40 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-require-cstdint "" }
+
+// 2009-11-30  Benjamin Kosnik  <benjamin@redhat.com>
+
+// Copyright (C) 2009 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <list>
+#include <exception/safety.h>
+
+void
+value()
+{
+  typedef __gnu_cxx::throw_value_limit value_type;
+  typedef __gnu_cxx::throw_allocator_limit<value_type> allocator_type;
+  typedef std::list<value_type, allocator_type> test_type;
+  __gnu_test::basic_safety<test_type> test;
+}
+
+// Container requirement testing, exceptional behavior
+int main()
+{
+  value();
+  return 0;
+}
