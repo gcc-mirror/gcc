4a83fadbd74dbaef5af67c8ac64c38f661553c9b
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-12-19 11:24:49 +0000

    2009-12-19  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/42108
    	* tree-ssa-sccvn.c (last_vuse_ptr): New variable.
    	(vn_reference_lookup_2): Update last seen VUSE.
    	(vn_reference_lookup_3): Avoid updating last seen VUSE after
    	translating.
    	(visit_reference_op_load): Use last seen VUSE from the first
    	lookup when entering into the table.
    
    	* gfortran.dg/pr42108.f90: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155360 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr42108.f90
@@ -0,0 +1,27 @@
+! { dg-do compile }
+! { dg-options "-O2 -fdump-tree-fre" }
+
+subroutine  eval(foo1,foo2,foo3,foo4,x,n,nnd)
+  implicit real*8 (a-h,o-z)
+  dimension foo3(n),foo4(n),x(nnd)
+  nw=0
+  foo3(1)=foo2*foo4(1)
+  do i=2,n
+    foo3(i)=foo2*foo4(i)
+    do  j=1,i-1
+      temp=0.0d0
+      jmini=j-i
+      do  k=i,nnd,n
+        temp=temp+(x(k)-x(k+jmini))**2
+      end do
+      temp = sqrt(temp+foo1)
+      foo3(i)=foo3(i)+temp*foo4(j)
+      foo3(j)=foo3(j)+temp*foo4(i)
+    end do
+  end do
+end subroutine eval
+
+! There should be only one load from n left
+
+! { dg-final { scan-tree-dump-times "\\*n_" 1 "fre" } }
+! { dg-final { cleanup-tree-dump "fre" } }
