5666bb32d1861b6622a2f98b5e26fa7c8e3a310f
Jason Merrill <jason@redhat.com>
2009-12-22 18:16:38 -0500

    re PR c++/42331 (ICE with invalid array initializer)
    	PR c++/42331
    	* typeck.c (cp_build_modify_expr): Fix thinko.
    
    From-SVN: r155410

---
+++ gcc/testsuite/g++.dg/cpp0x/initlist29.C
@@ -0,0 +1,14 @@
+// PR c++/42331
+// { dg-options "-std=c++0x" }
+
+class Mesh
+{
+public:
+  Mesh(const char*)
+  { typele={0}; }		// { dg-error "" }
+
+private:
+  int typele[7][2];
+};
+
+Mesh m(0);
