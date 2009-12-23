070ba483434fc103357a001baeb4f907170854c4
Sebastian Pop <sebastian.pop@amd.com>
2009-12-23 07:50:12 +0000

    Fix PR42334: correct the update of the LST on loop interchange and distribution.
    2009-12-15  Sebastian Pop  <sebastian.pop@amd.com>
    
            PR middle-end/42178
            PR middle-end/42334
            * graphite-interchange.c (lst_perfect_nestify): Reset to NULL the LSTs
            that are empty.
            (lst_do_interchange_1): Renamed lst_interchange_select_inner.
            (lst_try_interchange): Reimplemented.
            (lst_interchange_select_inner): Same.
            (lst_do_interchange): Renamed lst_interchange_select_outer.
            Reimplemented.
            (scop_do_interchange): Update use of lst_interchange_select_outer.
    
            * graphite-interchange.c (lst_try_interchange): Do not increment the
            the OUTER index when there is no AFTER kernel.  Do not increment the
            OUTER index for after processing the AFTER kernel.
            (lst_interchange_select_inner): Call lst_try_interchange only on loops.
            (lst_interchange_select_outer): Do not pass in a pointer to the OUTER
            index.  Do not pass to lst_interchange_select_inner the OUTER index.
            (scop_do_interchange): Update use of lst_interchange_select_outer.
    
            * graphite-interchange.c (lst_try_interchange): Do not modify OUTER
            index.  Call lst_interchange_select_inner only once.
            (lst_interchange_select_inner): Update use of lst_try_interchange.
            (lst_interchange_select_outer): Update.
    
            * testsuite/g++.dg/graphite/pr42130.C: Add -fgraphite-identity.
            * testsuite/gcc.dg/graphite/block-0.c: Un-XFAILed.
            * testsuite/gcc.dg/graphite/pr42211.c: New.
            * testsuite/gfortran.dg/graphite/pr42334.f90: New.
    
            * testsuite/gfortran.dg/graphite/graphite.exp
            (DEFAULT_FLAGS_GRAPHITE_IDENTITY): Remove -fdump-tree-graphite-all.
            * testsuite/gfortran.dg/graphite/interchange-1.f: Add comment.  Clean
            the graphite dump file.
            * testsuite/gfortran.dg/graphite/interchange-2.f: Same.
            * testsuite/gfortran.dg/graphite/pr42334-1.f: New.
    
    From-SVN: r155418

---
+++ gcc/testsuite/gfortran.dg/graphite/pr42334-1.f
@@ -0,0 +1,16 @@
+! { dg-options "-O2 -floop-interchange" }
+
+      subroutine linel(icmdl,stre,anisox)
+      real*8 stre(6),tkl(3,3),ekl(3,3),anisox(3,3,3,3)
+            do m1=1,3
+               do m2=1,m1
+                  do m3=1,3
+                     do m4=1,3
+                        tkl(m1,m2)=tkl(m1,m2)+
+     &                       anisox(m1,m2,m3,m4)*ekl(m3,m4)
+                     enddo
+                  enddo
+               enddo
+            enddo
+            stre(1)=tkl(1,1)
+      end
