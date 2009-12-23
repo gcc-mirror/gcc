d2a271ac6ff9029f8c2c014585e8b127c3fea0b5
Sebastian Pop <sebastian.pop@amd.com>
2009-12-23 07:50:32 +0000

    re PR debug/42186 ([graphite] internal compiler error: verify_ssa failed)
    Fix PR42186.
    
    2009-12-17  Sebastian Pop  <sebastian.pop@amd.com>
    
    	PR middle-end/42186
    	* testsuite/gfortran.dg/graphite/pr42186.f90: New.
    
    From-SVN: r155420

---
+++ gcc/testsuite/gfortran.dg/graphite/pr42186.f90
@@ -0,0 +1,14 @@
+! { dg-options "-fgraphite-identity -g -O3 -ffast-math" }
+MODULE erf_fn
+CONTAINS
+  SUBROUTINE CALERF(ARG,RESULT,JINT)
+    DIMENSION A(5),B(4),C(9),D(8),P(6),Q(5)
+    IF (Y <= THRESH) THEN
+       DO I = 1, 3
+          XNUM = (XNUM + A(I)) * YSQ
+          XDEN = (XDEN + B(I)) * YSQ
+       END DO
+       RESULT = X * (XNUM + A(4)) / (XDEN + B(4))
+    END IF
+  END SUBROUTINE CALERF
+END MODULE erf_fn
