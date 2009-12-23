e2bf200700a29fc74f9cc7dd4ae942c89f8dff24
Paolo Carlini <paolo.carlini@oracle.com>
2009-12-23 17:14:15 +0000

    stl_deque.h (copy(_Deque_iterator, _Deque_iterator, _Deque_iterator), [...]): Declare.
    2009-12-23  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* include/bits/stl_deque.h (copy(_Deque_iterator, _Deque_iterator,
    	_Deque_iterator), move(_Deque_iterator, _Deque_iterator,
    	_Deque_iterator)): Declare.
    	* include/bits/deque.tcc: Implement the latter.
    	* testsuite/performance/25_algorithms/copy_deque_iterators.cc: New.
    	* testsuite/25_algorithms/move/2.cc: Likewise.
    	* testsuite/25_algorithms/copy/5.cc: Likewise.
    	* testsuite/23_containers/deque/requirements/dr438/assign_neg.cc:
    	Adjust dg-error line number.
    	* testsuite/23_containers/deque/requirements/dr438/insert_neg.cc:
    	Likewise.
    	* testsuite/23_containers/deque/requirements/dr438/
    	constructor_1_neg.cc: Likewise.
    	* testsuite/23_containers/deque/requirements/dr438/
    	constructor_2_neg.cc: Likewise.
    
    From-SVN: r155432

--- libstdc++-v3/testsuite/23_containers/deque/requirements/dr438/assign_neg.cc
+++ libstdc++-v3/testsuite/23_containers/deque/requirements/dr438/assign_neg.cc
@@ -18,7 +18,7 @@
 // <http://www.gnu.org/licenses/>.
 
 // { dg-do compile }
-// { dg-error "no matching" "" { target *-*-* } 1502 }
+// { dg-error "no matching" "" { target *-*-* } 1534 }
 // { dg-excess-errors "" }
 
 #include <deque>
--- libstdc++-v3/testsuite/23_containers/deque/requirements/dr438/insert_neg.cc
+++ libstdc++-v3/testsuite/23_containers/deque/requirements/dr438/insert_neg.cc
@@ -18,7 +18,7 @@
 // <http://www.gnu.org/licenses/>.
 
 // { dg-do compile }
-// { dg-error "no matching" "" { target *-*-* } 1586 }
+// { dg-error "no matching" "" { target *-*-* } 1618 }
 // { dg-excess-errors "" }
 
 #include <deque>
