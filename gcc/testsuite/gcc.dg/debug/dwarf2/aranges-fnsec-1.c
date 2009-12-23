8fe443dc76ca493aff5d4dab38f67c1841460ecd
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-12-23 16:54:35 +0000

    PR debug/42454 	* dwarf2out.c (add_ranges_by_labels_to_AT_range_list): New function. 	(dwarf2out_finish): Call add_ranges_by_labels_to_AT_range_list.
    	* gcc.dg/debug/dwarf2/aranges-fnsec-1.c: Add check for .debug_ranges.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155429 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/debug/dwarf2/aranges-fnsec-1.c
+++ gcc/testsuite/gcc.dg/debug/dwarf2/aranges-fnsec-1.c
@@ -1,9 +1,11 @@
-/* Test that .debug_aranges does not have an entry for the text
-   section if nothing went in there.  */
+/* Test that .debug_aranges and .debug_ranges do not have an entry for the
+   text section if nothing went in there.  */
 /* Origin: Joseph Myers <joseph@codesourcery.com> */
 /* { dg-do compile } */
-/* { dg-options "-gdwarf-2 -ffunction-sections -w" } */
+/* { dg-options "-gdwarf-2 -ffunction-sections -w -dA" } */
 /* { dg-final { scan-assembler-not "\\.Letext0-\\.Ltext0" } } */
+/* { dg-final { scan-assembler-not "\\.Ltext0\[^\n\r\]*Offset 0x0" } } */
+/* { dg-final { scan-assembler "DW_AT_ranges" } } */
 
 int
 f (void)
