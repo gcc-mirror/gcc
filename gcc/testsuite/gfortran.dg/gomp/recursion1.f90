8b5f6dd83d7887c725337c83f302279946767212
Janus Weil <janus@gcc.gnu.org>
2009-12-29 20:29:54 +0100

    re PR fortran/42517 (-fcheck=recursion does not work with -fopenmp)
    gcc/fortran/
    2009-12-29  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/42517
    	* invoke.texi: Document the interference of
    	-fcheck=recursion and -fopenmp.
    	* trans-decl.c (gfc_generate_function_code): Disable -fcheck=recursion
    	when used with -fopenmp.
    
    gcc/testsuite/
    2009-12-29  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/42517
    	* gfortran.dg/gomp/recursion1.f90: New test.
    
    From-SVN: r155506

---
+++ gcc/testsuite/gfortran.dg/gomp/recursion1.f90
@@ -0,0 +1,27 @@
+! { dg-do run }
+! { dg-options "-fopenmp -fcheck=recursion" }
+!
+! PR 42517: Bogus runtime error with -fopenmp -fcheck=recursion
+!
+! Contributed by Janus Weil <janus@gcc.gnu.org>
+
+implicit none
+integer :: i,s
+
+s=0
+!$omp parallel do private(i) shared(s)
+do i=1,10
+  call sub(i)
+end do
+!$omp end parallel do
+if (s/=55) call abort()
+
+contains
+
+  subroutine sub (n)
+    integer :: n
+    s = s + n
+    print '(A,i3)',"loop =",n
+  end subroutine
+
+end
