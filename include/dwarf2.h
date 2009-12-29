df7b6aaae4bdbc5deb5219b3a2eece6eab8027bb
Joel Brobecker <brobecker@adacore.com>
2009-12-29 04:14:21 +0000

    * dwarf2.h (enum dwarf_attribute): Add DW_AT_GNAT_descriptive_type.
    From-SVN: r155501

--- include/dwarf2.h
+++ include/dwarf2.h
@@ -425,6 +425,10 @@ enum dwarf_attribute
     DW_AT_GNU_template_name = 0x2110,
     /* VMS extensions.  */
     DW_AT_VMS_rtnbeg_pd_address = 0x2201,
+    /* GNAT extensions.  */
+    /* GNAT descriptive type.
+       See http://gcc.gnu.org/wiki/DW_AT_GNAT_descriptive_type .  */
+    DW_AT_GNAT_descriptive_type	= 0x2302,
     /* UPC extension.  */
     DW_AT_upc_threads_scaled = 0x3210,
     /* PGI (STMicroelectronics) extensions.  */
