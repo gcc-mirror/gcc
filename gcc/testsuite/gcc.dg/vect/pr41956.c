e8332698dfb8e51e5fa800d37f50dd9386d90f47
irar <irar@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-12-30 12:53:18 +0000

    PR tree-optimization/41956 	* tree-vect-slp.c (vect_supported_load_permutation_p): Add check that 	the load indices differ.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155523 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/pr41956.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target vect_int } */
+
+void K (int *gpwgts, int *badminpwgt, int *badmaxpwgt)
+{
+  int i;
+  for (i = 0; i < 10; i += 2) {
+    badminpwgt[i] = badminpwgt[i+1] = gpwgts[i]+gpwgts[i];
+    badmaxpwgt[i] = badmaxpwgt[i+1] = gpwgts[i]+gpwgts[i];
+  }
+}
+
+/* { dg-final { cleanup-tree-dump "vect" } } */
