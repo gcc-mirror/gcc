117b43a74e7d24519cc07e73e997f9fb2ba11ef0
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-01 11:27:50 +0000

    2010-01-01  Paolo Carlini  <paolo.carlini@oracle.com>
    	Revert last change, reinstantiate basic_string and vstring tests.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155547 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/ext/vstring/requirements/exception/propagation_consistent.cc
@@ -0,0 +1,55 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-require-cstdint "" }
+
+// 2009-09-14  Benjamin Kosnik  <benjamin@redhat.com>
+
+// Copyright (C) 2009 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <ext/vstring.h>
+#include <exception/safety.h>
+
+void
+value()
+{
+#if 1
+  typedef char value_type;
+#else
+  // XXX
+  // __gnu_cxx::__rc_string_base<>::_Rep::_M_align’ with constructor
+  // not allowed in union
+  // typedef __gnu_cxx::throw_value_limit value_type;
+#endif
+  typedef __gnu_cxx::throw_allocator_limit<value_type> allocator_type;
+  typedef std::char_traits<value_type> traits_type;
+
+  typedef __gnu_cxx::__versa_string<value_type, traits_type,
+    allocator_type, __gnu_cxx::__rc_string_base> test_type_rc;
+
+  typedef __gnu_cxx::__versa_string<value_type, traits_type,
+    allocator_type, __gnu_cxx::__sso_string_base> test_type_sso;
+
+  __gnu_test::propagation_consistent<test_type_rc> test_rc;
+  __gnu_test::propagation_consistent<test_type_sso> test_sso;
+}
+
+// Container requirement testing, exceptional behavior
+int main()
+{
+  value();
+  return 0;
+}
