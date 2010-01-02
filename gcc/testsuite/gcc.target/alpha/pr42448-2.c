3e0c794aea6fd83d5b16dfed51d570ab338d60b7
Uros Bizjak <ubizjak@gmail.com>
2010-01-02 15:28:25 +0100

    re PR target/42448 (Wrong code with _Complex char in structure)
    	PR target/42448
    	* config/alpha/predicates.md (aligned_memory_operand): Return false
    	for CQImode.
    	(unaligned_memory_operand): Return true for CQImode.
    	* config/alpha/alpha.c (get_aligned_mem): Assert that location
    	doesn not cross aligned SImode word boundary.
    
    testsuite/ChangeLog:
    
    	PR target/42448
    	* gcc.target/alpha/pr42448-1.c: New test.
    	* gcc.target/alpha/pr42448-2.c: Ditto.
    
    From-SVN: r155567

---
+++ gcc/testsuite/gcc.target/alpha/pr42448-2.c
@@ -0,0 +1,27 @@
+/* { dg-do run } */
+/* { dg-options "-mcpu=21064 -O0" } */
+
+extern void abort (void);
+
+struct S2180
+{
+  char t;
+  _Complex char u[4];
+};
+
+struct S2180 s2180;
+
+int
+main (void)
+{
+  volatile struct S2180 x;
+
+  s2180.u[3] = 3 + 4i;
+
+  x.u[3] = s2180.u[3];
+
+  if (x.u[3] != s2180.u[3])
+    abort ();
+
+  return 0;
+}
