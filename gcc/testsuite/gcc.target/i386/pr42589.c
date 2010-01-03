ba1ee2281d44f1443bae38966276221abedd39d7
Richard Guenther <rguenther@suse.de>
2010-01-03 15:23:29 +0000

    re PR rtl-optimization/42589 (bswap optimization does not work for 32bit (but for 64bit) on 64bit registers)
    2010-01-03  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/42589
    	* tree-ssa-math-opts.c (execute_optimize_bswap): Allow
    	double-word expansion of bswap32.
    
    	* gcc.target/i386/pr42589.c: New testcase.
    
    From-SVN: r155588

---
+++ gcc/testsuite/gcc.target/i386/pr42589.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target ilp32 } */
+/* { dg-options "-O2 -march=i486" } */
+
+void
+foo (unsigned long long *p)
+{
+  unsigned long long tmp;
+  tmp = *p;
+  tmp = (tmp >> 32) | (tmp << 32);
+  tmp = (((tmp & 0xff00ff00ff00ff00ULL) >> 8)
+	 | ((tmp & 71777214294589695ULL) << 8));
+  *p = (((tmp & 0xffff0000ffff0000ULL) >> 16)
+	| ((tmp & 281470681808895ULL) << 16));
+}
+
+/* { dg-final { scan-assembler-times "bswap" 2 } } */
