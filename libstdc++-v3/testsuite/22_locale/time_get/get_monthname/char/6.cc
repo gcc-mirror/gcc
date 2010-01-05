54e1206558f182f5935cb32a36a8d2dc448acd40
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-05 11:44:43 +0000

    2010-01-05  Paolo Carlini  <paolo.carlini@oracle.com>
    	* testsuite/22_locale/time_get/get_monthname/char/6.cc: New.
    	* testsuite/22_locale/time_get/get_monthname/wchar_t/6.cc: Likewise.
    	* testsuite/22_locale/time_get/get_weekday/char/6.cc: Likewise.
    	* testsuite/22_locale/time_get/get_weekday/wchar_t/6.cc: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155644 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/22_locale/time_get/get_monthname/char/6.cc
@@ -0,0 +1,89 @@
+// 2010-01-05  Paolo Carlini  <paolo.carlini@oracle.com>
+
+// Copyright (C) 2010 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 22.2.5.1.1 time_get members
+
+#include <locale>
+#include <sstream>
+#include <cstring>
+#include <testsuite_hooks.h>
+
+void test01()
+{
+  using namespace std;
+  bool test __attribute__((unused)) = true;
+
+  typedef istreambuf_iterator<char> iterator_type;
+
+  // basic construction
+  locale loc_c = locale::classic();
+
+  // create an ostream-derived object, cache the time_get facet
+  iterator_type end;
+
+  istringstream iss;
+  iss.imbue(loc_c);
+  const time_get<char>& tim_get = use_facet<time_get<char> >(iss.getloc()); 
+
+  const ios_base::iostate good = ios_base::goodbit;
+  ios_base::iostate errorstate = good;
+
+  // iter_type 
+  // get_monthname(iter_type, iter_type, ios_base&, 
+  //               ios_base::iostate&, tm*) const
+
+  // sanity checks for "C" locale
+
+  const char* amname[12] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+			     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+  for (unsigned i = 0; i < 12; ++i)
+    {
+      iss.str(amname[i]);
+      iterator_type is_it01(iss);
+      tm time01;
+      memset(&time01, -1, sizeof(tm));
+      errorstate = good;
+      tim_get.get_monthname(is_it01, end, iss, errorstate, &time01);
+      VERIFY( time01.tm_mon == i );
+      VERIFY( errorstate == ios_base::eofbit );
+    }
+
+  const char* mname[12] = { "January", "February", "March", "April",
+			    "May", "June", "July", "August",
+			    "September", "October", "November", "December" };
+
+  for (unsigned i = 0; i < 12; ++i)
+    {
+      iss.str(mname[i]);
+      iterator_type is_it01(iss);
+      tm time01;
+      memset(&time01, -1, sizeof(tm));
+      errorstate = good;
+      tim_get.get_monthname(is_it01, end, iss, errorstate, &time01);
+      VERIFY( time01.tm_mon == i );
+      VERIFY( errorstate == ios_base::eofbit );
+    }
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
