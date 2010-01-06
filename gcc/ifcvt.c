786c3fbc1dabe8094277b73701241b218e483e81
Uros Bizjak <ubizjak@gmail.com>
2010-01-06 14:14:17 +0100

    * ifcvt.c (if_convert): Output slim multiple dumps with TDF_SLIM.
    From-SVN: r155673

--- gcc/ifcvt.c
+++ gcc/ifcvt.c
@@ -4165,7 +4165,12 @@ if_convert (void)
 
 #ifdef IFCVT_MULTIPLE_DUMPS
       if (dump_file && cond_exec_changed_p)
-	print_rtl_with_bb (dump_file, get_insns ());
+	{
+	  if (dump_flags & TDF_SLIM)
+	    print_rtl_slim_with_bb (dump_file, get_insns (), dump_flags);
+	  else
+	    print_rtl_with_bb (dump_file, get_insns ());
+	}
 #endif
     }
   while (cond_exec_changed_p);
