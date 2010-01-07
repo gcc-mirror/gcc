2e8e6a99ee156e8cd15b5e4b9a0eeaa49e2a970b
Paolo Carlini <paolo.carlini@oracle.com>
2010-01-07 00:22:51 +0000

    re PR libstdc++/26701 (std::time_get parses only 2 digits of year, in en_GB locale.)
    2010-01-06  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR libstdc++/26701
    	* include/bits/locale_facets_nonio.tcc (time_get<>::_M_extract_num):
    	Encode short two digits over four parsings to negative numbers.
    	(time_get<>::_M_extract_via_format): Adjust, accept both two digits
    	and four digits for both 'y' and 'Y'.
    	(time_get<>::do_get_year): Call time_get<>::_M_extract_num.
    	* doc/xml/manual/prerequisites.xml: Add en_GB.
    	* testsuite/lib/libstdc++.exp: Adjust
    	* testsuite/22_locale/time_get/get_date/char/26701.cc: New.
    	* testsuite/22_locale/time_get/get_date/wchar_t/26701.cc: Likewise.
    
    From-SVN: r155685

---
+++ libstdc++-v3/testsuite/22_locale/time_get/get_date/char/26701.cc
@@ -0,0 +1,64 @@
+// { dg-require-namedlocale "" }
+
+// 2010-01-06  Paolo Carlini  <paolo.carlini@oracle.com>
+
+// Copyright (C) 2010 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 22.2.5.1.1 time_get members
+
+#include <locale>
+#include <sstream>
+#include <testsuite_hooks.h>
+
+// libstdc++/26701
+void test01()
+{
+  using namespace std;
+  bool test __attribute__((unused)) = true;
+
+  typedef istreambuf_iterator<char> iterator_type;
+
+  locale loc_en = locale("en_GB");
+  
+  tm tm0 = __gnu_test::test_tm(0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+  iterator_type end;
+
+  istringstream iss; 
+  iss.imbue(loc_en);
+  const time_get<char>& tg = use_facet<time_get<char> >(iss.getloc());
+
+  const ios_base::iostate good = ios_base::goodbit;
+  ios_base::iostate errorstate = good;
+
+  iss.str("01/02/2003");
+  iterator_type is_it0(iss);
+  
+  errorstate = good;
+  tg.get_date(is_it0, end, iss, errorstate, &tm0);
+  VERIFY( errorstate == ios_base::eofbit );
+  VERIFY( tm0.tm_year + 1900 == 2003 );
+  VERIFY( tm0.tm_mon + 1 == 2 );
+  VERIFY( tm0.tm_mday == 1 );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
