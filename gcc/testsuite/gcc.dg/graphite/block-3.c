3f12432d0acdd0262f856efdabf8ce18fa27afb3
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-07 18:33:01 +0000

    Update loop-block testcases.
    2010-01-06  Sebastian Pop  <sebastian.pop@amd.com>
    
    	* graphite-blocking.c (scop_do_block): Print "SCoP will be loop
    	blocked" only when both the strip mine and the interchange have
    	been applied.
    
    	* testsuite/gcc.dg/graphite/block-2.c: Removed.
    	* testsuite/gcc.dg/graphite/block-3.c: Add scan pattern.
    	* testsuite/gcc.dg/graphite/block-4.c: Same.
    	* testsuite/gcc.dg/graphite/block-5.c: Same.
    	* testsuite/gcc.dg/graphite/block-6.c: Same.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155703 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/graphite/block-3.c
+++ gcc/testsuite/gcc.dg/graphite/block-3.c
@@ -9,17 +9,18 @@ void test (void)
 {
   int i, j, k;
 
-  /* These loops contain too few iterations for being strip-mined by 64.  */
+  /* These loops contain too few iterations to be blocked by 64.  */
   for (i = 0; i < 24; i++)
     for (j = 0; j < 24; j++)
       for (k = 0; k < 24; k++)
         A[i][j][k] = B[i][k] * C[k][j];
 
-  /* These loops should still be strip mined.  */
+  /* These loops should still be loop blocked.  */
   for (i = 0; i < M; i++)
     for (j = 0; j < M; j++)
       for (k = 0; k < M; k++)
         A[i][j][k] = B[i][k] * C[k][j];
 }
 
+/* { dg-final { scan-tree-dump-times "will be loop blocked" 1 "graphite" } } */
 /* { dg-final { cleanup-tree-dump "graphite" } } */
