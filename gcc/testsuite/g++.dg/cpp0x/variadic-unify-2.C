beb8be281281825b184e0e987dd239d1708fbb50
dodji <dodji@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-07 19:21:46 +0000

    Fix PR c++/40155
    gcc/cp/ChangeLog:
    	c++/40155
    	* pt.c (unify_pack_expansion): In non-deduced contexts, re-use template
    	arguments that were previously deduced.
    
    gcc/testsuite/ChangeLog:
    	c++/40155
    	* g++.dg/cpp0x/variadic-unify-2.C: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155705 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/variadic-unify-2.C
@@ -0,0 +1,14 @@
+// Contributed by Dodji Seketeli <dodji@redhat.com>
+// Origin: PR c++/40155
+// { dg-options "-std=c++0x" }
+// { dg-do compile }
+
+template <typename T> struct identity
+{  typedef T type;  };
+
+template <typename RT, typename... A>
+int forward_call(RT (*) (A...), typename identity<A>::type...);
+
+int g (double);
+
+int i = forward_call(&g, 0);
