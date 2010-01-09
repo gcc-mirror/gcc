7368569212f3e0ab9a02ad46b5721b760ea5971d
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-09 14:46:25 +0000

    Update copyright notices of my recent commits.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155766 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/vec.h
+++ gcc/vec.h
@@ -1,5 +1,6 @@
 /* Vector API for GNU compiler.
-   Copyright (C) 2004, 2005, 2007, 2008, 2009 Free Software Foundation, Inc.
+   Copyright (C) 2004, 2005, 2007, 2008, 2009, 2010
+   Free Software Foundation, Inc.
    Contributed by Nathan Sidwell <nathan@codesourcery.com>
 
 This file is part of GCC.
