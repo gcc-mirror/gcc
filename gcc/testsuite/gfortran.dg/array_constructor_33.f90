8defd2217a48b681719de1e3c847094d9af9363f
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-09 18:31:00 +0000

    2010-01-09 Jerry DeLisle <jvdelisle@gcc.gnu.org>
    	PR fortran/32489
    	* gfortran.dg/array_constructor_33.f90: New test.
    	gfortran.dg/array_function_5.f90: Add credit.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155772 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/array_constructor_33.f90
@@ -0,0 +1,10 @@
+! { dg-do compile }
+! PR20923 gfortran slow for large array constructors.
+! Test case prepared from PR by Jerry DeLisle <jvdelisle@gcc.gnu.org>
+program sel
+    implicit none
+    integer(kind=4),parameter      :: n=1000
+    integer(kind=4)                :: i,j
+    real(kind=4),dimension(n*n)    :: vect
+    vect(:) = (/ ((( (i+j+3)),i=1,n),j=1,n) /)
+end
