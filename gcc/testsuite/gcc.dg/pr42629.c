bb956f3ea05a1c9b59f21aa776da3f582c00d4e0
Alexandre Oliva <aoliva@redhat.com>
2010-01-09 14:41:17 +0000

    re PR debug/42629 ("-fcompare-debug failure (length)" with "-O1 -fsched-pressure -fschedule-insns")
    gcc/ChangeLog:
    PR debug/42629
    * haifa-sched.c (dying_use_p): Debug insns don't count.
    gcc/testsuite/ChangeLog:
    PR debug/42629
    * gcc.dg/pr42629.c: New.
    
    From-SVN: r155763

---
+++ gcc/testsuite/gcc.dg/pr42629.c
@@ -0,0 +1,27 @@
+/* This failed -fcompare-debug because register pressure computation
+   took debug insns into account.  */
+/* { dg-do compile } */
+/* { dg-options "-O1 -fsched-pressure -fschedule-insns -fcompare-debug" } */
+
+int lzo_adler32(int adler, char *buf)
+{
+  int s1 = adler;
+  int s2 = adler;
+  s1 += buf[0];
+  s2 += s1;
+  s1 += buf[1];
+  s2 += s1;
+  s1 += buf[2];
+  s2 += s1;
+  s1 += buf[3];
+  s2 += s1;
+  s1 += buf[4];
+  s2 += s1;
+  s1 += buf[5];
+  s2 += s1;
+  s1 += buf[6];
+  s2 += s1;
+  s1 += buf[7];
+  s2 += s1;
+  return (s2 << 16) + s1;
+}
