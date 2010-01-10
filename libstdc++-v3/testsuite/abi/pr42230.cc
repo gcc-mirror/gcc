9b2adcdbc12edf30eb4bedcff79f8f9050c7f29a
H.J. Lu <hjl@gcc.gnu.org>
2010-01-09 16:24:52 -0800

    Return dgs.alc on success
    libiberty/
    
    2010-01-09  Ian Lance Taylor  <iant@google.com>
    
    	PR other/42230
    	* cp-demangle.c (d_demangle): Return dgs.alc on success.
    
    libstdc++-v3/
    
    2010-01-09  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR other/42230
    	* testsuite/abi/pr42230.cc: New.
    
    From-SVN: r155785

---
+++ libstdc++-v3/testsuite/abi/pr42230.cc
@@ -0,0 +1,16 @@
+// { dg-do run }
+
+#include <cxxabi.h>
+#include <cassert>
+#include <cstddef>
+
+int main()
+{
+  std::size_t length = 0;
+  int cc;
+
+  char* ret = abi::__cxa_demangle("e", 0, &length, &cc);
+
+  assert( (cc < 0 && !ret) || (ret && length) );
+  return 0;
+}
