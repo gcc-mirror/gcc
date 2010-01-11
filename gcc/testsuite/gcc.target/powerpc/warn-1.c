3767f512426fb5ec2c3459a83e220c3343125fc3
Janis Johnson <janis187@us.ibm.com>
2010-01-11 20:29:40 +0000

    re PR target/42416 (ICE in rs6000_bulitin_vec_perm for 177.mesa with -mvsx)
    	PR target/42416
    	* config/rs6000/rs6000.c (rs6000_override_options): On targets
    	that support VSX, warn for -mno-altivec if vsx is not disabled,
    	and disable vsx.
    	* gcc.target/powerpc/warn-1.c: New test.
    	* gcc.target/powerpc/warn-2.c: New test.
    
    From-SVN: r155816

---
+++ gcc/testsuite/gcc.target/powerpc/warn-1.c
@@ -0,0 +1,18 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
+/* { dg-options "-O -mvsx -mno-altivec" } */
+
+/* { dg-warning "-mvsx and -mno-altivec are incompatible" "" { target *-*-* } 1 } */
+
+double
+foo (double *x, double *y)
+{
+  double z[2];
+  int i;
+
+  for (i = 0; i < 2; i++)
+    z[i] = x[i] + y[i];
+  return z[0] * z[1];
+}
+
+/* { dg-final { scan-assembler-not "xsadddp" } } */
---
+++ gcc/testsuite/gcc.target/powerpc/warn-2.c
@@ -0,0 +1,18 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
+/* { dg-options "-O -mcpu=power7 -mno-altivec" } */
+
+/* { dg-warning "-mno-altivec disables vsx" "" { target *-*-* } 1 } */
+
+double
+foo (double *x, double *y)
+{
+  double z[2];
+  int i;
+
+  for (i = 0; i < 2; i++)
+    z[i] = x[i] + y[i];
+  return z[0] * z[1];
+}
+
+/* { dg-final { scan-assembler-not "xsadddp" } } */
