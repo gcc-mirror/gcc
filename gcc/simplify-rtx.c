e5c43e8d7255195fac6e290f9673a3c82cb375fa
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-12 09:49:51 +0000

    Update Copyright years.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155833 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/simplify-rtx.c
+++ gcc/simplify-rtx.c
@@ -1,6 +1,6 @@
 /* RTL simplification functions for GNU compiler.
    Copyright (C) 1987, 1988, 1989, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008
+   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010
    Free Software Foundation, Inc.
 
 This file is part of GCC.
