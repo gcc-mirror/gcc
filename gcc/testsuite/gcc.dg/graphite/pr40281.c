66ea9105793aa2c23fc050d57eb8943b7a6a4b99
Sebastian Pop <sebastian.pop@amd.com>
2010-01-13 07:58:50 +0000

    re PR middle-end/40281 (-fprefetch-loop-arrays: ICE: in initialize_matrix_A, at tree-data-ref.c:1887)
    2010-01-13  Sebastian Pop  <sebastian.pop@amd.com>
    
    	PR middle-end/40281
    	* tree-scalar-evolution.c (instantiate_scev_1): Base and stride
    	evolutions should not variate in inner loops.
    
    	* gcc.dg/graphite/pr40281.c: New.
    
    From-SVN: r155851

---
+++ gcc/testsuite/gcc.dg/graphite/pr40281.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fprefetch-loop-arrays -w" } */
+/* { dg-options "-O -fprefetch-loop-arrays -march=i686 -msse -w" { target { { i?86-*-* x86_64-*-* } && ilp32 } } } */
+
+void foo(int);
+void bar(int n)
+{
+ int a[2], i, j = 0;
+
+ for (i = 0; i < 2; i += j+1)
+   for (j = 0; j < (n ? 1 : 2); ++j)
+     foo(a[i] + a[j]);
+}
