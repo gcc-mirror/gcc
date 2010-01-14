79cd32e08e7529844ec414e23c3f537bf280da5e
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-14 22:52:02 +0000

    Fix 42747, make -mvsx enable sqrt
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155923 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/pr42747.c
@@ -0,0 +1,8 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
+/* { dg-require-effective-target powerpc_vsx_ok } */
+/* { dg-options "-O2 -mcpu=power7 -ffast-math" } */
+
+double foo (double x) { return __builtin_sqrt (x); }
+
+/* { dg-final { scan-assembler "xssqrtdp" } } */
