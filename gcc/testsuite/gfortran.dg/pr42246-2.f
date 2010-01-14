6a6a03b472c230daf49b8cf3b2985351ab154264
abel <abel@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-14 11:22:20 +0000

    PR rtl-optimization/42246         * sel-sched-ir.h (get_all_loop_exits): Include exits from inner         loops.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@155900 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr42246-2.f
@@ -0,0 +1,21 @@
+C PR rtl-optimization/42246
+C { dg-do compile { target powerpc*-*-* ia64-*-* x86_64-*-* } }
+C { dg-options "-O2 -fselective-scheduling -fsel-sched-pipelining -fsel-sched-pipelining-outer-loops" }
+
+      subroutine distance(x,clo)
+      implicit real*8 (a-h,o-z)
+      dimension x(2,6),x1(2,6),clo(6)
+      do 60 i=1,2
+        do 20 j=1,6
+          x(i,j)=clo(j)
+   20   continue
+        do 40 iq=1,6
+          x1(i,iq)=0.0d0
+   40   continue
+        do 50 j=1,6
+          x(i,j)=x1(i,j)
+   50   continue
+   60 continue
+      return
+      end
+
