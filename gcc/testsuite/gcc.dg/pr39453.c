8ec4d0ada31aee6ee23b4ed7f234171f6639c0dd
Alexander Monakov <amonakov@ispras.ru>
2010-01-14 13:40:19 +0300

    re PR rtl-optimization/39453 (ICE : in init_seqno, at sel-sched.c:6433)
    2010-01-14  Alexander Monakov  <amonakov@ispras.ru>
    
    	PR rtl-optimization/39453
    	PR rtl-optimization/42246
    	* sel-sched-ir.c (considered_for_pipelining_p): Do not test
    	for pipelining_p.
    	(sel_add_loop_preheaders): Add preheader to last_added_blocks.
    
    	* gcc.dg/pr39453.c: New.
    	* gcc.dg/pr42246.c: New.
    
    From-SVN: r155892

---
+++ gcc/testsuite/gcc.dg/pr39453.c
@@ -0,0 +1,18 @@
+/* { dg-do compile { target powerpc*-*-* ia64-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -fselective-scheduling2 -fsel-sched-pipelining -fsel-sched-pipelining-outer-loops" } */
+
+int foo(int k, int n)
+{
+  int i;
+  for (i = 0; i < n; i += 8) {
+    int j;
+    for (j = 0; j < n; j += 8) {
+      while (k < n) {
+        k += 8;
+      }
+    }
+  }
+  return k;
+}
+
+
