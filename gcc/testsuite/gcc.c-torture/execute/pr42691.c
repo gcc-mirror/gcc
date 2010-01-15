49b6b0ccb5671ba560ea57ad7da6bbba680c5449
Jing Yu <jingyu@google.com>
2010-01-15 22:11:43 +0000

    backport: re PR rtl-optimization/42691 (problematic REG_EQUAL note added to SUBREG)
    Backport from mainline(4.5) to fix a regression bug.
    
    2010-01-15  Jing Yu  <jingyu@google.com>
    
    	PR rtl-optimization/42691
    	* combine.c (try_combine): Set changed_i3_dest to 1 when I2 and I3 set
    	a pseudo to a constant and are merged, and adjust comments.
    
    	2010-01-15  Jing Yu  <jingyu@google.com>
    
    	PR rtl-optimization/42691
    	* gcc.c-torture/execute/pr42691.c: New.
    
    From-SVN: r155949

---
+++ gcc/testsuite/gcc.c-torture/execute/pr42691.c
@@ -0,0 +1,41 @@
+extern void abort (void);
+
+union _D_rep
+{
+  unsigned short rep[4];
+  double val;
+};
+
+int add(double* key, double* table)
+{
+  unsigned i = 0;
+  double* deletedEntry = 0;
+  while (1) {
+    double* entry = table + i;
+
+    if (*entry == *key)
+      break;
+
+    union _D_rep _D_inf = {{ 0, 0, 0, 0x7ff0 }};
+    if (*entry != _D_inf.val)
+      abort ();
+
+    union _D_rep _D_inf2 = {{ 0, 0, 0, 0x7ff0 }};
+    if (!_D_inf2.val)
+      deletedEntry = entry;
+
+    i++;
+  }
+  if (deletedEntry)
+    *deletedEntry = 0.0;
+  return 0;
+}
+
+int main ()
+{
+  union _D_rep infinit = {{ 0, 0, 0, 0x7ff0 }};
+  double table[2] = { infinit.val, 23 };
+  double key = 23;
+  int ret = add (&key, table);
+  return ret;
+}
