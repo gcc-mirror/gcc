ebea03e03e7025644f2bb4c35236bef0afe543ae
Dodji Seketeli <dodji@redhat.com>
2010-01-18 21:18:49 +0000

    re PR c++/42634 (ICE with -g -O2 -std=c++0x   in copy_fn_p, at cp/decl.c:9973)
    Fix PR c++/42634
    
    gcc/cp/ChangeLog:
        PR c++/42634
        * error.c (dump_template_parms): Use innermost template
        arguments before calling count_non_default_template_args.
        (count_non_default_template_args): We are being called with
        template innermost arguments now. There is no need to ensure
        that again.
    
    gcc/testsuite/ChangeLog:
        PR c++/42634
        * g++.dg/template/error45.C: New test.
    
    From-SVN: r156022

---
+++ gcc/testsuite/g++.dg/template/error45.C
@@ -0,0 +1,34 @@
+// Origin PR c++/42634
+// { dg-options "-g -std=gnu++0x" }
+// { dg-do compile }
+
+template<typename T> T declval();
+
+template<typename T, typename... Args> struct is_constructible {
+    template<typename T1, typename... Args1> static decltype(T1(declval<Args1>()...), char()) test();
+    static const bool value = sizeof(test<T, Args...>()) == 1;
+};
+template<bool> struct enable_if {
+        typedef void type;
+};
+template<class T1, class T2> struct pair {
+    template<class U2,
+             class = typename enable_if<is_constructible<T2,U2&&>::value>::type
+             >
+    pair(const T1&, U2&&) { }
+};
+struct string {
+  string() : p(0) {}
+  char* p;
+};
+
+struct Foo {
+  string s;
+  int i;
+};
+
+void f()
+{
+  pair<int, Foo>(1, Foo());
+}
+
