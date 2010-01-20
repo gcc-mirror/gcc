0672b2688cf41e5ddb0b6a92dd60a990afbc567d
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-20 12:30:15 +0000

    2010-01-20  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/42717
    	* tree-ssa-dce.c (get_live_post_dom): Remove.
    	(forward_edge_to_pdom): Take an arbitrary edge to copy
    	degenerate PHI args from.
    	(remove_dead_stmt): Use the first post-dominator even if it
    	does not contain live statements as redirection destination.
    
    	* gcc.c-torture/compile/pr42717.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@156076 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr42717.c
@@ -0,0 +1,30 @@
+static signed char
+foo (signed char si1, unsigned char si2)
+{
+  return (si1 ^ si2) & (-si2 ^ si2) ? : si1 - si2;
+}
+
+struct S0
+{
+};
+
+unsigned char g_21;
+
+struct S0 g_34;
+
+void
+bar (unsigned char p_20)
+{
+  unsigned char *l_22 = &g_21;
+  unsigned char l_23 = 0;
+  struct S0 *l = &g_34;
+  goto lbl_42;
+  for (; l_23; l_23 = foo (l_23, 1))
+    {
+      for (p_20 = 0; 0; p_20 = foo (p_20, 1))
+	lbl_42:;
+      (l == &g_34) ? 0 : "";
+lbl_85:*l_22 = p_20;
+    }
+  goto lbl_85;
+}
