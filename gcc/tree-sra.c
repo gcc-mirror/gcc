65c0a517d9f0a26ead665a1abc1801f0e4ac75b2
Steve Ellcey <sje@cup.hp.com>
2010-01-22 22:22:51 +0000

    Makefile.in (tree-sra.o): Add $(EXPR_H) dependency.
    2010-01-22  Steve Ellcey  <sje@cup.hp.com>
    
    	* Makefile.in (tree-sra.o): Add $(EXPR_H) dependency.
    	* tree-sra.c: Add include of expr.h.
    
    From-SVN: r156181

--- gcc/tree-sra.c
+++ gcc/tree-sra.c
@@ -77,6 +77,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "alloc-pool.h"
 #include "tm.h"
 #include "tree.h"
+#include "expr.h"
 #include "gimple.h"
 #include "cgraph.h"
 #include "tree-flow.h"
