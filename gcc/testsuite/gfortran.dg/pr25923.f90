14c92e725d700f416c5b6909796d5d81f52d8880
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-24 16:59:51 +0000

    2010-01-24  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/41044
    	PR fortran/41167
    	* expr.c (remove_subobject_ref): If the constructor is NULL use
    	the expression as the source.
    	(simplify_const_ref): Change the type of expression if
    	there are component references.  Allow for substring to be at
    	the end of an arbitrarily long chain of references.  If an
    	element is found that is not in an EXPR_ARRAY, assume that this
    	is scalar initialization of array. Call remove_subobject_ref in
    	this case with NULL second argument.
    
    2010-01-24  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/41044
    	* gfortran.dg/parameter_array_ref_2.f90 : New test.
    
    	PR fortran/41167
    	* gfortran.dg/char_array_arg_1.f90 : New test.
    
    	* gfortran.dg/pr25923.f90 : Remove XFAIL.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@156197 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/pr25923.f90
+++ gcc/testsuite/gfortran.dg/pr25923.f90
@@ -10,7 +10,7 @@ implicit none
 
 contains
 
-  function baz(arg) result(res) ! { dg-warning "res.yr' may be" "" { xfail *-*-* } }
+  function baz(arg) result(res) ! { dg-warning "res.yr' may be" }
     type(bar), intent(in) :: arg
     type(bar) :: res
     logical, external:: some_func
