057dd91d0c1df52679f71644d95fea2ae9522f3e
Jakub Jelinek <jakub@redhat.com>
2010-01-26 10:47:45 +0100

    re PR fortran/42866 (ICE for REDUCTION with ALLOCATABLE array as variable on SECTIONS)
    	PR fortran/42866
    	* omp-low.c (expand_omp_sections): Only use single_pred if
    	l2_bb is single_pred_p.
    
    	* testsuite/libgomp.fortran/allocatable5.f90: New test.
    
    From-SVN: r156235

---
+++ libgomp/testsuite/libgomp.fortran/allocatable5.f90
@@ -0,0 +1,17 @@
+! PR fortran/42866
+! { dg-do run }
+
+program pr42866
+  integer, allocatable :: a(:)
+  allocate (a(16))
+  a = 0
+  !$omp parallel
+    !$omp sections reduction(+:a)
+      a = a + 1
+    !$omp section
+      a = a + 2
+    !$omp end sections
+  !$omp end parallel
+  if (any (a.ne.3)) call abort
+  deallocate (a)
+end
