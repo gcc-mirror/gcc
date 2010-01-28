d6bedeaf4822a04dc62809459d4a6634c4fe41d0
H.J. Lu <hongjiu.lu@intel.com>
2010-01-28 21:35:16 +0000

    Use SSE2 on gcc.target/i386/pr42881.c.
    2010-01-28  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* gcc.target/i386/pr42881.c: Use SSE2.
    
    From-SVN: r156333

--- gcc/testsuite/gcc.target/i386/pr42881.c
+++ gcc/testsuite/gcc.target/i386/pr42881.c
@@ -1,10 +1,9 @@
 /* PR target/42881 */
 /* { dg-do run } */
-/* { dg-options "-O0 -msse" } */
-#include "sse-check.h"
-#include <xmmintrin.h>
+/* { dg-options "-O0 -msse2" } */
+#include "sse2-check.h"
 static void
-sse_test (void)
+sse2_test (void)
 {
   double a[2];
   __m128d x = _mm_set1_pd(3);
