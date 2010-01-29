93e520a2dbc3f73508916e9d41ddcf529cd3d1da
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2010-01-29 15:55:13 +0000

    inclhack.def (solaris_int_limits_3): New fix.
    	* inclhack.def (solaris_int_limits_3): New fix.
    	* fixincl.x: Regenerate.
    	* tests/base/sys/int_limits.h [SOLARIS_INT_LIMITS_3_CHECK]: New
    	test.
    
    	* inclhack.def (solaris_int_const, solaris_int_limits_1,
    	solaris_int_limits_2 ): Applies to Solaris 2 in general.
    	Remove select.
    	Add mach for *-*-solaris2*.
    	Change second c_fix_arg to select.
    	Remove #pragma ident from test_text.
    	* fixincl.x: Regenerate.
    	* tests/base/sys/int_const.h [SOLARIS_INT_CONST_CHECK]: Remove
    	#pragma trigger text.
    	* tests/base/sys/int_limits.h [SOLARIS_INT_LIMITS_1_CHECK,
    	SOLARIS_INT_LIMITS_2_CHECK]: Likewise.
    
    From-SVN: r156356

--- fixincludes/tests/base/sys/int_limits.h
+++ fixincludes/tests/base/sys/int_limits.h
@@ -10,15 +10,18 @@
 
 
 #if defined( SOLARIS_INT_LIMITS_1_CHECK )
-#pragma ident	"@(#)int_limits.h	1.9	04/09/28 SMI"
 #define	UINT8_MAX	(255)
 #define	UINT16_MAX	(65535)
 #endif  /* SOLARIS_INT_LIMITS_1_CHECK */
 
 
 #if defined( SOLARIS_INT_LIMITS_2_CHECK )
-#pragma ident	"@(#)int_limits.h	1.9	04/09/28 SMI"
 #define	INT_FAST16_MAX INT32_MAX
 #define	UINT_FAST16_MAX UINT32_MAX
 #define	INT_FAST16_MIN INT32_MIN
 #endif  /* SOLARIS_INT_LIMITS_2_CHECK */
+
+
+#if defined( SOLARIS_INT_LIMITS_3_CHECK )
+#define	SIZE_MAX	4294967295U
+#endif  /* SOLARIS_INT_LIMITS_3_CHECK */
