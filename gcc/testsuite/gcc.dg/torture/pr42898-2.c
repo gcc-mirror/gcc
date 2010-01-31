e023c7b0a0a101ea88a21cdd2d249a17adc8dcd1
Eric Botcazou <ebotcazou@adacore.com>
2010-01-31 21:08:15 +0000

    re PR middle-end/42898 (volatile structures and compound literal initializers)
    2010-01-31  Eric Botcazou  <ebotcazou@adacore.com>
    
    	PR middle-end/42898
    	Backport from mainline:
    	2009-04-23  Eric Botcazou  <ebotcazou@adacore.com>
    
    	* gimplify.c (gimplify_modify_expr_rhs) <VAR_DECL>: Do not do a direct
    	assignment from the constructor either if the target is volatile.
    
    From-SVN: r156416

---
+++ gcc/testsuite/gcc.dg/torture/pr42898-2.c
@@ -0,0 +1,26 @@
+/* { dg-do compile } */
+/* { dg-skip-if "" { *-*-* } { "-O0" } { "" } } */
+/* { dg-options "-fdump-tree-optimized" } */
+
+struct hardware {
+  int parm1:8;
+  int :4;
+  int parm2:4;
+  int parm3:15;
+  int parm4:1;
+};
+
+const struct hardware h = {
+  .parm1=42,
+  .parm2=13,
+  .parm3=11850,
+  .parm4=1,
+};
+
+void f1(volatile struct hardware *ptr)
+{
+  *ptr = h;
+}
+
+/* { dg-final { scan-tree-dump-times "\\*ptr" 1 "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
