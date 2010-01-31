f261acb287db48efa4c171561a91c5cb30f7d25e
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-31 20:00:54 +0000

    PR middle-end/42898 	* gcc.dg/torture/pr42898-2.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@156414 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/pr42898-2.c
@@ -0,0 +1,25 @@
+/* { dg-do compile } */
+/* { dg-options "-fdump-tree-optimized" } */
+
+struct hardware {
+  int parm1:8;
+  int :4;
+  int parm2:4;
+  int parm3:15;
+  int parm4:1;
+};
+
+const struct hardware h = {
+  .parm1=42,
+  .parm2=13,
+  .parm3=11850,
+  .parm4=1,
+};
+
+void f1(volatile struct hardware *ptr)
+{
+  *ptr = h;
+}
+
+/* { dg-final { scan-tree-dump-times "\\*ptr" 1 "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
