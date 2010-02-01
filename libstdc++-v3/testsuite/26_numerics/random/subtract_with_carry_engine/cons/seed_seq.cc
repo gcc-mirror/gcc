79a27af7a554de9ead3b5bdab56948f5d7b297a2
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-02-01 19:45:23 +0000

    2010-02-01  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR libstdc++/42408
    	* include/bits/random.h (linear_congruential_engine<>::
    	linear_congruential_engine(seed_seq&),
    	linear_congruential_engine<>::seed(seed_seq&),
    	mersenne_twister<>::mersenne_twister(seed_seq&),
    	mersenne_twister<>::seed(seed_seq&),
    	subtract_with_carry_engine<>::subtract_with_carry_engine(seed_seq&),
    	subtract_with_carry_engine<>::seed(seed_seq&),
    	discard_block_engine<>::discard_block_engine(seed_seq&),
    	discard_block_engine<>::seed(seed_seq&),
    	independent_bits_engine<>::independent_bits_engine(seed_seq&),
    	independent_bits_engine<>::seed(seed_seq&),
    	shuffle_order_engine<>::shuffle_order_engine(seed_seq&),
    	shuffle_order_engine<>::seed(seed_seq&)): Templatize.
    	* include/bits/random.tcc: Adjust.
    	* testsuite/26_numerics/random/subtract_with_carry_engine/cons/
    	seed_seq.cc: New.
    	* testsuite/26_numerics/random/mersenne_twister_engine/cons/
    	seed_seq.cc: Likewise.
    	* testsuite/26_numerics/random/linear_congruential_engine/
    	cons/seed_seq.cc: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@156430 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/26_numerics/random/subtract_with_carry_engine/cons/seed_seq.cc
@@ -0,0 +1,39 @@
+// { dg-options "-std=c++0x" }
+// { dg-require-cstdint "" }
+//
+// 2010-02-01  Paolo Carlini  <paolo.carlini@oracle.com>
+//
+// Copyright (C) 2010 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 26.4.3.3 Class template subtract_with_carry_engine [rand.eng.sub]
+// 26.4.2.2 Concept RandomNumberEngine [rand.concept.eng]
+
+#include <random>
+
+void
+test01()
+{
+  std::seed_seq seed;
+  std::subtract_with_carry_engine<unsigned long, 24, 10, 24> x(seed);
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
