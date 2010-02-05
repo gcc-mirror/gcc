5c95f07b6154507f16f349d69f0aef23266c81ac
Richard Guenther <rguenther@suse.de>
2010-02-05 15:28:31 +0000

    tree-ssa-ccp.c (get_symbol_constant_value): Strip all conversions.
    2010-02-05  Richard Guenther  <rguenther@suse.de>
    
    	* tree-ssa-ccp.c (get_symbol_constant_value): Strip all
    	conversions.
    	(fold_const_aggregate_ref): Likewise.
    	(ccp_fold_stmt): Substitute loads.
    	(maybe_fold_reference): Verify types before substituting.
    	Unshare properly.
    	(fold_gimple_assign): Unshare properly.
    	(fold_stmt_1): Insert conversion if necessary before replacing
    	the RHS.
    
    	* gcc.dg/tree-ssa/ssa-ccp-28.c: New testcase.
    
    From-SVN: r156519

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-ccp-28.c
@@ -0,0 +1,26 @@
+/* { dg-do run } */
+/* { dg-options "-O -fdump-tree-ccp1" } */
+
+extern void abort (void);
+
+static int g[1];
+
+static int * const p = &g[0];
+static int * const q = &g[0];
+
+int main(void)
+{
+  g[0] = 1;
+  *p = 0;
+  *p = *q;
+  if (g[0] != 0)
+    abort ();
+  return 0;
+}
+
+/* We should have replaced all loads from p and q with the constant
+   initial value.  */
+
+/* { dg-final { scan-tree-dump-times "= p;" 0 "ccp1" } } */
+/* { dg-final { scan-tree-dump-times "= q;" 0 "ccp1" } } */
+/* { dg-final { cleanup-tree-dump "ccp1" } } */
