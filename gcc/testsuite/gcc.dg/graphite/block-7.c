f21ef1e73742a23f9ee24b0f90d5a0111baf97c8
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-02-06 17:41:09 +0000

    Clarify comments and use build_pddr.
    2010-01-20  Sebastian Pop  <sebastian.pop@amd.com>
    
    	* graphite-dependences.c: Clarify comments.
    	(build_pddr): Add an extra parameter direction.
    	(graphite_legal_transform_dr): Use build_pddr.
    	(dot_original_deps_stmt_1): Update use of build_pddr.
    	(dot_transformed_deps_stmt_1): Same.
    	(dot_original_deps): Same.
    	(dot_transformed_deps): Same.
    
    	* testsuite/gcc.dg/graphite/interchange-12.c: New.
    	* gcc/testsuite/gcc.dg/graphite/block-7.c: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@156545 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/graphite/block-7.c
@@ -0,0 +1,19 @@
+#define N 1000
+
+float A[N][N], B[N][N], C[N][N];
+
+void matmult ()
+{
+  int i, j, k;
+
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++)
+      {
+        A[i][j] = 0;
+        for (k = 0; k < N; k++)
+          A[i][j] += B[i][k] * C[k][j];
+      }
+}
+
+/* { dg-final { scan-tree-dump-times "SCoP will be loop blocked" 1 "graphite" } } */
+/* { dg-final { cleanup-tree-dump "graphite" } } */
