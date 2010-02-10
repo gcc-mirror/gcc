5a80a58ba41e5ce149dbc3befd8054adfed2850c
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-02-10 09:21:12 +0000

    * builtins.c (set_builtin_user_assembler_name): Also handle 	ffs if int is smaller than word.
    	* gcc.dg/builtin-ffs-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@156646 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/builtins.c
+++ gcc/builtins.c
@@ -13594,6 +13594,14 @@ set_builtin_user_assembler_name (tree decl, const char *asmspec)
     case BUILT_IN_ABORT:
       abort_libfunc = set_user_assembler_libfunc ("abort", asmspec);
       break;
+    case BUILT_IN_FFS:
+      if (INT_TYPE_SIZE < BITS_PER_WORD)
+	{
+	  set_user_assembler_libfunc ("ffs", asmspec);
+	  set_optab_libfunc (ffs_optab, mode_for_size (INT_TYPE_SIZE,
+						       MODE_INT, 0), "ffs");
+	}
+      break;
     default:
       break;
     }
