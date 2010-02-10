1e1f8b7f57588db63a2b9922fe5a74e11646544e
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-02-10 21:48:25 +0000

    PR c++/43016 	* semantics.c (maybe_add_lambda_conv_op): Set DECL_INTERFACE_KNOWN.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@156671 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp0x/lambda/lambda-conv.C
+++ gcc/testsuite/g++.dg/cpp0x/lambda/lambda-conv.C
@@ -1,6 +1,16 @@
+// Test for conversion from stateless lambda to function pointer, which is
+// not yet part of the draft but hopefully will be after the March 2010
+// meeting.
+
 // { dg-options -std=c++0x }
+// { dg-final { scan-assembler "weak\[^\n\r\]*_?_ZZ1fvENUlvE_cvPFvvEEv" { target { ! { *-*-darwin* *-*-mingw* *-*-cygwin } } } } }
+
+inline void f()
+{
+  void (*pfn)() = []{};
+}
 
 int main()
 {
-  void (*pfn)() = []{};
+  f();
 }
