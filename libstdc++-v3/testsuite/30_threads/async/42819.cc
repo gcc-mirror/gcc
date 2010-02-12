5eddaef09c34b7a0bb1568633fa96759e4cffec3
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-02-12 22:31:15 +0000

    2010-02-12  Jonathan Wakely  <jwakely.gcc@gmail.com> 	    Paolo Carlini  <paolo.carlini@oracle.com>
    	PR libstdc++/42819, DR 1315
    	* include/std/future (async): Use std::result_of for the template
    	argument of the std::future return type; adjust everywhere.
    	* testsuite/30_threads/async/42819.cc: New.
    	* testsuite/30_threads/packaged_task/cons/assign_neg.cc: Adjust
    	dg-error line number.
    	* testsuite/30_threads/packaged_task/cons/copy_neg.cc: Likewise.
    	* testsuite/30_threads/future/cons/assign_neg.cc: Likewise.
    	* testsuite/30_threads/future/cons/copy_neg.cc: Likewise.
    	* testsuite/30_threads/promise/cons/assign_neg.cc: Likewise.
    	* testsuite/30_threads/promise/cons/assign_neg.cc: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@156742 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/30_threads/async/42819.cc
@@ -0,0 +1,59 @@
+// { dg-do run { target *-*-freebsd* *-*-netbsd* *-*-linux* *-*-solaris* *-*-cygwin *-*-darwin* alpha*-*-osf* mips-sgi-irix6* } }
+// { dg-options " -std=gnu++0x -pthread" { target *-*-freebsd* *-*-netbsd* *-*-linux* alpha*-*-osf* mips-sgi-irix6* } }
+// { dg-options " -std=gnu++0x -pthreads" { target *-*-solaris* } }
+// { dg-options " -std=gnu++0x " { target *-*-cygwin *-*-darwin* } }
+// { dg-require-cstdint "" }
+// { dg-require-gthreads "" }
+// { dg-require-atomic-builtins "" }
+
+// Copyright (C) 2010 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <future>
+#include <testsuite_hooks.h>
+
+int do_work1(int value) { return value; }
+int do_work2(int value) { return value * 2; }
+
+int work1(int value)
+{
+  auto handle = std::async([=] { return do_work2(value); });
+  int tmp = do_work1(value);
+  return tmp + handle.get();
+}
+
+int work2(int value)
+{
+  auto handle = std::async(do_work2, value);
+  int tmp = do_work1(value);
+  return tmp + handle.get();
+}
+
+// libstdc++/42819
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  VERIFY( work1(1) == 3 );
+  VERIFY( work2(2) == 6 );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
