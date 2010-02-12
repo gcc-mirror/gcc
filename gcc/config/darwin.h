93230de2243b997c93e9fc8f06ca40ff488d00be
davek <davek@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-02-12 17:35:18 +0000

    2010-02-12  Dave Korn  <dave.korn.cygwin@gmail.com> 	    Jack Howarth  <howarth@bromo.med.uc.edu> 	    Iain Sandoe <iain.sandoe@sandoe-acoustics.co.uk>
    	PR target/42982
    	Partial revert of unintended change in fix for PR41605.
    	* gcc/config/darwin.h: Fix typo.
    	* gcc/config/darwin9.h: Same.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@156736 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/darwin.h
+++ gcc/config/darwin.h
@@ -272,7 +272,7 @@ extern GTY(()) int darwin_ms_struct;
     %{A} %{e*} %{m} %{r} %{x} \
     %{o*}%{!o:-o a.out} \
     %{!A:%{!nostdlib:%{!nostartfiles:%S}}} \
-    %{L*} %(link_libgcc) %o %{fprofile-arcs|fprofile-generate|coverage:-lgcov} \
+    %{L*} %(link_libgcc) %o %{fprofile-arcs|fprofile-generate*|coverage:-lgcov} \
     %{fopenmp|ftree-parallelize-loops=*: \
       %{static|static-libgcc|static-libstdc++|static-libgfortran: libgomp.a%s; : -lgomp } } \
     %{!nostdlib:%{!nodefaultlibs: %(link_ssp) %G %L }} \
