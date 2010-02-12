c63e9a1846e9d6a27f816fe9802820af347e2697
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-02-12 19:46:29 +0000

    PR c++/43054 	* tree.c (cp_tree_equal): Correct CALL_EXPR logic, handle 	EXPR_PACK_EXPANSION.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@156737 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/variadic99.C
@@ -0,0 +1,22 @@
+// PR c++/43054
+// { dg-options "-std=c++0x" }
+
+template<typename R> struct future { };
+
+template<typename Fn, typename... Args>
+ auto
+ async(Fn&& fn, Args&&... args)
+ -> future<decltype(fn(args...))>;
+
+template<typename Fn, typename... Args>
+ auto
+ async(Fn&& fn, Args&&... args)
+ -> future<decltype(fn(args...))>;
+
+int work2(int value);
+
+void work(int value)
+{
+  async(work2, value);
+}
+
