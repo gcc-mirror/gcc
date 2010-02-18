4aced1a1e0a85d16a1b900a2cccaee5b22c3876f
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-02-18 18:52:12 +0000

    2010-02-18  Benjamin Kosnik  <bkoz@redhat.com>
    	* doc/xml/manual/using.xml: Update.
    	* doc/xml/manual/status_cxx1998.xml: Same.
    	* doc/xml/manual/status_cxx200x.xml: Add link.
    	* doc/html/*: Regenerate.
    
    	* testsuite/17_intro/freestanding.cc: Add.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@156871 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/17_intro/freestanding.cc
@@ -0,0 +1,50 @@
+// { dg-options "-x c -std=gnu++0x -lsupc++" }
+
+// Copyright (C) 2010 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <cstdarg>
+#include <cstddef>
+#include <cstdlib>
+
+#include <exception>
+#include <limits>
+#include <new>
+#include <typeinfo>
+
+#include <initializer_list>
+#include <type_traits>
+
+int main()
+{
+  std::exception e;
+
+  const char* str = typeid(e).name();
+  
+  typedef std::numeric_limits<long> limit_type;
+  limit_type limit_l;
+  int r = limit_type::radix;
+
+  const char* cp = new char;
+  delete cp;
+
+  bool b = std::is_integral<int>::value;
+
+  std::initializer_list<int> ilisti;
+
+  return 0;
+}
