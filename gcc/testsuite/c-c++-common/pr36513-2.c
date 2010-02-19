ea0567b978b89e58aada9b3a3660c3ab38f2d2fd
Manuel López-Ibáñez <manu@gcc.gnu.org>
2010-02-19 21:21:34 +0000

    re PR target/36513 (-Wlogical-op warns about strchr)
    2010-02-19  Manuel López-Ibáñez  <manu@gcc.gnu.org>
    
    	PR 36513
    testsuite/
    	* c-c++-common/pr36513.c: New testcase.
    	* c-c++-common/pr36513-2.c: New testcase.
    
    From-SVN: r156912

---
+++ gcc/testsuite/c-c++-common/pr36513-2.c
@@ -0,0 +1,13 @@
+/* PR 36513: -Wlogical-op warns about strchr */
+/* { dg-do compile } */
+/* { dg-options "-Wlogical-op" } */
+#ifdef __cplusplus
+#include <cstring>
+#else 
+#include <string.h>
+#endif
+int main2 ()
+{
+  char *s, t;
+  strchr (s, t);
+}
