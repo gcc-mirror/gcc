c7da0354531098a6827b42b00689e92ef61053cd
Richard Guenther <rguenther@suse.de>
2010-02-19 15:42:31 +0000

    re PR tree-optimization/42916 ("-fcompare-debug failure" with "-O1 -funroll-loops -ftree-vectorize")
    2010-02-19  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/42916
    	* tree-vect-slp.c (vect_slp_analyze_bb): Count only real
    	instructions.
    
    	* gcc.dg/pr42916.c: New testcase.
    
    From-SVN: r156898

---
+++ gcc/testsuite/gcc.dg/pr42916.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -funroll-loops -ftree-vectorize -fcompare-debug" } */
+
+int seed;
+
+static inline int hash(const char *str)
+{
+  int h = seed++;
+  int i = 12;
+  while (i--)
+    h += (h << 3) ^ *str++;
+  return h;
+}
+
+void f(const char *str, int *h)
+{
+  int i = 6;
+  while (i--)
+    *h++ = hash(str);
+}
