54564d01b93944df0c0b34daf82e41280e8e3435
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-02-21 13:06:07 +0000

    2010-02-21  Tobias Burnus  <burnus@net-b.de>
            PR fortran/35259
            * doc/invoke.texi (-fassociative-math): Document that this
            option is automatically enabled for Fortran.
    
    2010-02-21  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/35259
            * gfortran.h (gfc_option_t): New flag -fprotect-parens.
            * lang.opt: Ditto.
            * option.c (gfc_init_options,gfc_handle_option): Ditto.
            * trans-expr.c (gfc_conv_expr_op): Use the flag.
            * invoke.texi: Document new -fno-protect-parens flag.
    
    2010-02-21  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/35259
            * gfortran.dg/reassoc_5.f90: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@156937 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/reassoc_5.f90
@@ -0,0 +1,19 @@
+! { dg-do compile }
+! { dg-options "-O3 -ffast-math -fdump-tree-optimized -fno-protect-parens" }
+!
+! PR fortran/35259
+! Test for -fno-protect-parens
+!
+function test(b)
+  real a
+  a = (b + 5.) - 5.
+  test = a
+end
+
+! Test copied from reassoc_1.f90 which checked for -fprotect-parens (default),
+! and thus for the occurance of "5 - 5".
+!
+! We need an explicit +5 and -5, and an intermediate ((bla)) expression
+! (the reassoc barrier).  Make use of "." matching lineends.
+! { dg-final { scan-tree-dump-times "\\\+ 5.*\\\)\\\).* - 5" 0 "optimized" } }
+! { dg-final { cleanup-tree-dump "optimized" } }
