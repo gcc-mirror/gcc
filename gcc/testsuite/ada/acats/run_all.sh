4d1f6da3eb4d8b529796a2e6d1dbd3108eb0adbb
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-02-24 12:51:44 +0000

    PR ada/32547 	* lib/gnat-dg.exp (gnat_load): Redefine.
    	* ada/acats/run_acats: Run run_all.sh with $SHELL.
    	* ada/acats/run_all.sh: Downcase tasking not implemented message.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@157037 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/ada/acats/run_all.sh
+++ gcc/testsuite/ada/acats/run_all.sh
@@ -282,7 +282,7 @@ for chapter in $chapters; do
       cat ${i}.log >> $dir/acats.log
       egrep -e '(==== |\+\+\+\+ |\!\!\!\! )' ${i}.log > /dev/null 2>&1
       if [ $? -ne 0 ]; then
-         grep 'Tasking not implemented' ${i}.log > /dev/null 2>&1
+         grep 'tasking not implemented' ${i}.log > /dev/null 2>&1
 
          if [ $? -ne 0 ]; then
             display "FAIL:	$i"
