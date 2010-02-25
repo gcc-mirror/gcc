018afad39fc0b88d5bfd4842efa52a7923cff371
Ed Smith-Rowland <3dw4rd@verizon.net>
2010-02-25 17:13:04 +0000

    limits (__numeric_limits_base::max_digits10, [...]): Add.
    2010-02-25  Ed Smith-Rowland  <3dw4rd@verizon.net>
    
    	* include/std/limits (__numeric_limits_base::max_digits10,
    	numeric_limits<>::max_digits10, numeric_limits<>::lowest): Add.
    	* src/limits.cc: Define max_digits10.
    	* config/abi/pre/gnu.ver: Adjust.
    	* testsuite/18_support/numeric_limits/dr559.cc: Test max_digits10
    	and lowest too.
    	* testsuite/18_support/numeric_limits/lowest.cc: New.
    	* testsuite/18_support/numeric_limits/max_digits10.cc: New.
    
    From-SVN: r157067

---
+++ libstdc++-v3/testsuite/18_support/numeric_limits/lowest.cc
@@ -0,0 +1,82 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-add-options ieee }
+
+// 2010-02-25  Ed Smith-Rowland
+
+// Copyright (C) 2010 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 18.2.1.1 template class numeric_limits
+
+#include <limits>
+#include <type_traits>
+#include <testsuite_hooks.h>
+
+template<typename T>
+  void
+  do_test(std::true_type)
+  {
+    bool test __attribute__((unused)) = true;
+    T limits_min = std::numeric_limits<T>::min();
+    VERIFY( std::numeric_limits<T>::lowest() == limits_min );
+  }
+
+template<typename T>
+  void
+  do_test(std::false_type)
+  {
+    bool test __attribute__((unused)) = true;
+    T limits_max = std::numeric_limits<T>::max();
+    VERIFY( std::numeric_limits<T>::lowest() == -limits_max );
+  }
+
+template<typename Tp>
+  void
+  do_test()
+  { do_test<Tp>(typename std::is_integral<Tp>::type()); }
+
+void test01()
+{
+  do_test<char>();
+  do_test<signed char>();
+  do_test<unsigned char>();
+  do_test<wchar_t>();
+  do_test<char16_t>();
+  do_test<char32_t>();
+
+  do_test<short>();
+  do_test<unsigned short>();
+
+  do_test<int>();
+  do_test<unsigned int>();
+
+  do_test<long>();
+  do_test<unsigned long>();
+
+  do_test<long long>();
+  do_test<unsigned long long>();
+
+  do_test<float>();
+  do_test<double>();
+  do_test<long double>();
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/18_support/numeric_limits/max_digits10.cc
@@ -0,0 +1,67 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-add-options ieee }
+
+// 2010-02-25  Ed Smith-Rowland
+
+// Copyright (C) 2010 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 18.2.1.1 template class numeric_limits
+
+#include <limits>
+#include <cmath>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  VERIFY( std::numeric_limits<bool>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<char>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<signed char>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<unsigned char>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<wchar_t>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<short>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<unsigned short>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<int>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<unsigned int>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<long>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<unsigned long>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<long long>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<unsigned long long>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<char16_t>::max_digits10 == 0 );
+  VERIFY( std::numeric_limits<char32_t>::max_digits10 == 0 );
+
+  const int f_max_digits10 = (2 + std::numeric_limits<float>::digits
+			      * 643 / 2136);
+  VERIFY( std::numeric_limits<float>::max_digits10 == f_max_digits10 );
+
+  const int d_max_digits10 = (2 + std::numeric_limits<double>::digits
+			      * 643 / 2136);
+  VERIFY( std::numeric_limits<double>::max_digits10 == d_max_digits10 );
+
+  const int ld_max_digits10 = (2 + std::numeric_limits<long double>::digits
+			       * 643 / 2136);
+  VERIFY( std::numeric_limits<long double>::max_digits10 == ld_max_digits10 );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
