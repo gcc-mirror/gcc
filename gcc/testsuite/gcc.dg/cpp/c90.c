7e1542b921b63aea61cc239be5eeeb632631a5b9
Manuel López-Ibáñez <manu@gcc.gnu.org>
2010-02-26 17:09:29 +0000

    re PR c/20631 (Support -std=c90 as alias for -std=c89)
    2010-02-26  Manuel López-Ibáñez  <manu@gcc.gnu.org>
    
    	PR c/20631
    	* doc/cpp.texi: Use c90 instead of c89 and gnu90 instead of gnu89.
    	* doc/standards.texi: Likewise.
    	* doc/extend.texi: Likewise.
    	* doc/trouble.texi: Likewise.
    	* doc/cppopts.texi: Likewise. 
    	* doc/install.texi: Likewise.
    	* c.opt (std=c90,std=gnu90): New options.
    	* c-opts.c (c_common_handle_option): Handle them.
    testsuite/
    	* gcc.dg/cpp/c90-pedantic.c: New.
    	* gcc.dg/cpp/c90.c: New.
    	* gcc.dg/gnu90-const-expr-1.c: New.
    	* gcc.dg/Woverlength-strings-pedantic-c90.c: New.
    
    From-SVN: r157096

---
+++ gcc/testsuite/gcc.dg/cpp/c90.c
@@ -0,0 +1,10 @@
+/* Copyright (C) 2000 Free Software Foundation, Inc.  */
+
+/* { dg-do preprocess } */
+/* { dg-options "-std=c90" } */
+
+/* This file is for testing the preprocessor in -std=c90 mode.
+   Neil Booth, 2 Dec 2000.  */
+
+#if 1LL
+#endif
