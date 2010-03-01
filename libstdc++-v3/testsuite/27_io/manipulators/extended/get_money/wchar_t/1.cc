604b384dd4580785292d8706719d51e8e1f44295
Paolo Carlini <paolo.carlini@oracle.com>
2010-03-01 19:12:39 +0000

    iomanip (get_money, put_money): Add in C++0x mode; tidy.
    2010-03-01  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* include/std/iomanip (get_money, put_money): Add in C++0x mode; tidy.
    	* testsuite/27_io/manipulators/extended/get_money/char/1.cc: New.
    	* testsuite/27_io/manipulators/extended/get_money/wchar_t/1.cc:
    	Likewise.
    	* testsuite/27_io/manipulators/extended/put_money/char/1.cc: Likewise.
    	* testsuite/27_io/manipulators/extended/put_money/wchar_t/1.cc:
    	Likewise.
    
    From-SVN: r157153

---
+++ libstdc++-v3/testsuite/27_io/manipulators/extended/get_money/wchar_t/1.cc
@@ -0,0 +1,49 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-require-namedlocale "" }
+
+// 2010-03-01  Paolo Carlini  <paolo.carlini@oracle.com>
+
+// Copyright (C) 2010 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <sstream>
+#include <iomanip>
+#include <testsuite_hooks.h>
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  std::locale loc_de = std::locale("de_DE@euro");
+
+  std::wistringstream iss;
+  iss.imbue(loc_de);
+
+  iss.str(L"7200000000,00 ");
+
+  std::wstring str;
+  iss >> get_money(str);
+
+  VERIFY( str == L"720000000000" );
+  VERIFY( iss.eof() );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
