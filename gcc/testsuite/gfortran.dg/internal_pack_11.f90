34de9f8b3f212ffcb65ba1f4bb8f697c6ecf336a
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-03-02 11:58:02 +0000

    2010-03-02  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/43180
    	* trans-array.c (gfc_conv_array_parameter): A full array of
    	derived type need not be restricted to a symbol without an
    	array spec to use the call to gfc_conv_expr_descriptor.
    
    	PR fortran/43173
    	* trans-array.c (gfc_conv_array_parameter): Contiguous refs to
    	allocatable arrays do not need temporaries.
    
    2010-03-02  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/43180
    	* gfortran.dg/internal_pack_10.f90: New test.
    
    	PR fortran/43173
    	* gfortran.dg/internal_pack_11.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@157163 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/internal_pack_11.f90
@@ -0,0 +1,18 @@
+! { dg-do compile }
+! { dg-options "-fdump-tree-original" }
+!
+! Test the fix for PR43173, where unnecessary calls to internal_pack/unpack
+! were being produced below. These references are contiguous and so do not
+! need a temporary.
+!
+! Contributed Tobias Burnus <burnus@gcc.gnu.org>
+!
+  REAL, allocatable :: ot(:)
+  integer :: time_steps
+
+  call foo (ot) ! OK, no temporary
+  call foo (ot(0:5:1)) ! Was an unnecessary temporary
+  call foo (ot(0:time_steps)) ! Was an unnecessary temporary
+  end
+! { dg-final { scan-tree-dump-times "unpack" 0 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
