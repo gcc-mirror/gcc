a82ec6aa3c72f6445f0f3add370d9a25d32e61df
H.J. Lu <hongjiu.lu@intel.com>
2010-03-03 16:54:19 +0000

    Update gcc.dg/pr36997.c for vector size change.
    2010-03-03  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* gcc.dg/pr36997.c: Adjust error message.
    
    From-SVN: r157198

--- gcc/testsuite/gcc.dg/pr36997.c
+++ gcc/testsuite/gcc.dg/pr36997.c
@@ -5,5 +5,5 @@ typedef int __m64 __attribute__ ((__vector_size__ (8), __may_alias__));
 __m64 _mm_add_si64 (__m64 __m1, __m64 __m2)
 {
     return (__m64) __builtin_ia32_paddq ((long long)__m1, (long long)__m2); /* { dg-error "incompatible type" } */
-    /* { dg-message "note: expected 'long long int __vector__' but argument is of type 'long long int'" "" { target *-*-* } 7 } */
+    /* { dg-message "note: expected 'long long int __vector.1.' but argument is of type 'long long int'" "" { target *-*-* } 7 } */
 }
