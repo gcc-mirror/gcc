794a888886dc62516c4ed6403d4441a18b5de597
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-03-10 23:25:37 +0000

    * config/sparc/sol2-bi.h (CC1_SPEC): Default to -mcpu=v9 for -m32. 	* config/sparc/t-sol2-64 (MULTILIB_DIRNAMES): Use sparcv8plus. ada/ 	* gcc-interface/Makefile.in (SPARC/Solaris): Use sparcv8plus.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@157372 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sparc/sol2-bi.h
+++ gcc/config/sparc/sol2-bi.h
@@ -237,7 +237,7 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 %{mcypress:-mcpu=cypress} \
 %{msparclite:-mcpu=sparclite} %{mf930:-mcpu=f930} %{mf934:-mcpu=f934} \
 %{mv8:-mcpu=v8} %{msupersparc:-mcpu=supersparc} \
-%{m32:%{m64:%emay not use both -m32 and -m64}} \
+%{m64:%{m32:%emay not use both -m32 and -m64}} \
 %{m64:-mptr64 -mstack-bias -mno-v8plus \
   %{!mcpu*:%{!mcypress:%{!msparclite:%{!mf930:%{!mf934:%{!mv8*:%{!msupersparc:-mcpu=v9}}}}}}}} \
 "
@@ -249,7 +249,7 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 %{mv8:-mcpu=v8} %{msupersparc:-mcpu=supersparc} \
 %{m32:%{m64:%emay not use both -m32 and -m64}} \
 %{m32:-mptr32 -mno-stack-bias \
-  %{!mcpu*:%{!mcypress:%{!msparclite:%{!mf930:%{!mf934:%{!mv8*:%{!msupersparc:-mcpu=cypress}}}}}}}} \
+  %{!mcpu*:%{!mcypress:%{!msparclite:%{!mf930:%{!mf934:%{!mv8*:%{!msupersparc:-mcpu=v9}}}}}}}} \
 %{mv8plus:-m32 -mptr32 -mno-stack-bias \
   %{!mcpu*:%{!mcypress:%{!msparclite:%{!mf930:%{!mf934:%{!mv8:%{!msupersparc:-mcpu=v9}}}}}}}} \
 "
