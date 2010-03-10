abf4d60b541534d42af4e36f723273737164ea38
Tobias Burnus <burnus@net-b.de>
2010-03-10 19:56:46 +0100

    2010-03-10  Tobias Burnus  <burnus@net-b.de
            PR fortran/43303
            * symbol.c (get_iso_c_sym): Set sym->result.
    
    2010-03-10  Tobias Burnus  <burnus@net-b.de
    
            PR fortran/43303
            * gfortran.dg/c_assoc_3.f90: New test.
    
    From-SVN: r157364

--- gcc/fortran/symbol.c
+++ gcc/fortran/symbol.c
@@ -4502,6 +4502,8 @@ get_iso_c_sym (gfc_symbol *old_sym, char *new_name,
   new_symtree->n.sym->module = gfc_get_string (old_sym->module);
   new_symtree->n.sym->from_intmod = old_sym->from_intmod;
   new_symtree->n.sym->intmod_sym_id = old_sym->intmod_sym_id;
+  if (old_sym->attr.function)
+    new_symtree->n.sym->result = new_symtree->n.sym;
   /* Build the formal arg list.  */
   build_formal_args (new_symtree->n.sym, old_sym, add_optional_arg);
 
