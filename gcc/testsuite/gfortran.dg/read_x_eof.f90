e1061681b93e842d8317043e1accf3491a37d1f7
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2010-03-11 02:26:36 +0000

    re PR libfortran/43320 (200.sixtrack fails setup)
    2010-03-10  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libfortran/43320
    	PR libfortran/43265
    	* gfortran.dg/read_eof_6.f: New test
    	* gfortran.dg/read_x_eof.f90: Update test.
    	* gfortran.dg/read_x_past.f: Update test.
    
    From-SVN: r157378

---
+++ gcc/testsuite/gfortran.dg/read_x_eof.f90
@@ -0,0 +1,16 @@
+! { dg-do run }
+! PR43265 No EOF condition if reading with '(x)' from an empty file
+! Test case from the reporter.
+program pr43265
+implicit none
+integer::i
+open(23,status="scratch")
+write(23,'(a)') "Line 1"
+write(23,'(a)') "Line 2"
+write(23,'(a)') "Line 3"
+rewind(23)
+do i=1,10
+  read(23,'(1x)',end=12)
+enddo
+12 if (i.ne.4) call abort
+end
