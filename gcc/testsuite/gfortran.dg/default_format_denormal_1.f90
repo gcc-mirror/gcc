6ac0538d37b0a4d0574bff858693dd34b76e9934
Kai Tietz <kai.tietz@onevision.com>
2010-03-12 14:40:42 +0000

    default_format_denormal_1.f90: Don't assume fail for *-*-mingw* targets.
    2010-03-12  Kai Tietz  <kai.tietz@onevision.com>
    
            * gfortran.dg/default_format_denormal_1.f90: Don't assume
            fail for *-*-mingw* targets.
    
    From-SVN: r157406

--- gcc/testsuite/gfortran.dg/default_format_denormal_1.f90
+++ gcc/testsuite/gfortran.dg/default_format_denormal_1.f90
@@ -1,4 +1,4 @@
-! { dg-do run { xfail *-*-darwin[89]* *-*-freebsd* *-*-mingw* *-*-cygwin* spu-*-* } }
+! { dg-do run { xfail *-*-darwin[89]* *-*-freebsd* *-*-cygwin* spu-*-* } }
 ! Test XFAILed on these platforms because the system's printf() lacks
 ! proper support for denormals.
 !
