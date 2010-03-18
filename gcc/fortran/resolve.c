1821bcfc49276dee748251bdefbb2040dd718954
Paul Thomas <pault@gcc.gnu.org>
2010-03-18 21:23:35 +0000

    re PR fortran/43039 ([lto/-fwhole-file] fortran-dev - ICE in gfc_conv_component_ref dynamic_dispatch_5.f03)
    2010-03-18  Paul Thomas  <pault@gcc.gnu.org>
    
            PR fortran/43039
            * trans-expr.c (conv_parent_component_references): Ensure that
    	'dt' has a backend_decl.
    
            PR fortran/43043
            * trans-expr.c (gfc_conv_structure): Ensure that the derived
    	type has a backend_decl.
    
            PR fortran/43044
            * resolve.c (resolve_global_procedure): Check that the 'cl'
    	structure is not NULL.
    
    From-SVN: r157552

--- gcc/fortran/resolve.c
+++ gcc/fortran/resolve.c
@@ -1851,12 +1851,13 @@ resolve_global_procedure (gfc_symbol *sym, locus *where,
      
       /* Non-assumed length character functions.  */
       if (sym->attr.function && sym->ts.type == BT_CHARACTER
-	  && gsym->ns->proc_name->ts.u.cl->length != NULL)
+	    && gsym->ns->proc_name->ts.u.cl != NULL
+	    && gsym->ns->proc_name->ts.u.cl->length != NULL)
 	{
 	  gfc_charlen *cl = sym->ts.u.cl;
 
 	  if (!sym->attr.entry_master && sym->attr.if_source == IFSRC_UNKNOWN
-              && cl && cl->length && cl->length->expr_type != EXPR_CONSTANT)
+                && cl && cl->length && cl->length->expr_type != EXPR_CONSTANT)
 	    {
               gfc_error ("Nonconstant character-length function '%s' at %L "
 			 "must have an explicit interface", sym->name,
