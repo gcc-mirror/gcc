21b04412b046c5819813c4b7d3853277385b93aa
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2010-03-20 14:42:04 +0000

    re PR fortran/43409 (I/O: INQUIRE for SIZE does not work.)
    2010-03-20  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR fortran/43409
    	* gfortran.dg/inquire_size.f90: New test.
    
    From-SVN: r157594

---
+++ gcc/testsuite/gfortran.dg/inquire_size.f90
@@ -0,0 +1,28 @@
+! { dg-do run }
+! PR43409 I/O: INQUIRE for SIZE does not work.
+integer :: i
+character(30) :: aname = "noname"
+logical :: is_named
+
+open(25, file="testfile", status="replace", access="stream", form="unformatted")
+do i=1,100
+  write(25) i, "abcdefghijklmnopqrstuvwxyz"
+enddo
+flush(25)
+
+inquire(unit=25, named=is_named, name=aname, size=i)
+if (.not.is_named) call abort
+if (aname /= "testfile") call abort
+if (i /= 3000) call abort
+
+inquire(file="testfile", size=i)
+if (.not.is_named) call abort
+if (aname /= "testfile") call abort
+if (i /= 3000) call abort
+
+close(25, status="delete")
+inquire(file="testfile", size=i)
+if (i /= -1)  call abort
+end
+
+
