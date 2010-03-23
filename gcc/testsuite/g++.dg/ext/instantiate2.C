5b5cfcb3b8177ebe2bf56ef1501b64a054080515
Mike Stump <mikestump@comcast.net>
2010-03-23 20:02:57 +0000

    re PR target/33120 (Data not put in BSS section on Mac OS)
    	PR target/33120
    	* config/darwin.h (ASM_OUTPUT_ALIGNED_BSS): Add.
    	* config/darwin.c (darwin_output_aligned_bss): Add.
    	* config/darwin-protos.h: Add darwin_output_aligned_bss.
    
    testsuite:
    	* g++.dg/ext/instantiate2.C: Update for .zerofill as it doesn't
    	follow the usual conventions for symbol definitions.
    	* gcc.target/i386/darwin-zerofill.c: Add.
    
    From-SVN: r157677

--- gcc/testsuite/g++.dg/ext/instantiate2.C
+++ gcc/testsuite/g++.dg/ext/instantiate2.C
@@ -8,7 +8,8 @@ template <class T> struct A {
 template <class T> T A<T>::t = 0;
 static template struct A<int>;
 
-// { dg-final { scan-assembler "\n_?_ZN1AIiE1tE(:|\n|\t)" } }
+// { dg-final { scan-assembler "\n_?_ZN1AIiE1tE(:|\n|\t)" { target { ! *-apple-darwin* } } } }
+// { dg-final { scan-assembler ".zerofill __DATA, __bss2, __ZN1AIiE1tE" { target *-apple-darwin* } } }
 void test_int() { A<int>::t = 42; }
 
 // { dg-final { scan-assembler-not "\n_?_ZN1AIcE1tE(:|\n|\t)" } }
