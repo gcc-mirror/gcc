012cd3b6b52f81950a23953ef21e383d7bd29d06
Mike Stump <mikestump@comcast.net>
2010-03-24 03:04:16 +0000

    * g++.dg/warn/Wstrict-aliasing-float-ref-int-obj.C: Enhance portability.
    From-SVN: r157690

--- gcc/testsuite/g++.dg/warn/Wstrict-aliasing-float-ref-int-obj.C
+++ gcc/testsuite/g++.dg/warn/Wstrict-aliasing-float-ref-int-obj.C
@@ -4,9 +4,9 @@
 
 int foo() {
   int x;
-  float& q = reinterpret_cast<float&> (x);  /* { dg-message "initialized" "" { xfail *-*-* } } */
+  float& q = reinterpret_cast<float&> (x);  /* { dg-message "dereferencing type-punned" "" { target *-*-* } } */
   q = 1.0; /* { dg-warning "does break strict-aliasing" "" { xfail *-*-* } } */
   return x;
 }
 
-/* { dg-message "dereferencing type-punned" "" { target *-*-* } 7 } */
+/* { dg-message "initialized" "" { xfail *-*-* } 7 } */
