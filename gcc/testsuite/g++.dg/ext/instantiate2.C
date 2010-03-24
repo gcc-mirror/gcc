b98aa20c89ab5c5b237e5fad0294589a1c4a3cd0
Mike Stump <mrs@gcc.gnu.org>
2010-03-24 16:07:51 +0000

    re PR java/43504 (many libjava testsuite regressions on x86_64-apple-darwin10)
    	PR java/43504
    	Revert fix for PR target/33120.
    
    From-SVN: r157697

--- gcc/testsuite/g++.dg/ext/instantiate2.C
+++ gcc/testsuite/g++.dg/ext/instantiate2.C
@@ -8,8 +8,7 @@ template <class T> struct A {
 template <class T> T A<T>::t = 0;
 static template struct A<int>;
 
-// { dg-final { scan-assembler "\n_?_ZN1AIiE1tE(:|\n|\t)" { target { ! *-apple-darwin* } } } }
-// { dg-final { scan-assembler ".zerofill __DATA, __bss2, __ZN1AIiE1tE" { target *-apple-darwin* } } }
+// { dg-final { scan-assembler "\n_?_ZN1AIiE1tE(:|\n|\t)" } }
 void test_int() { A<int>::t = 42; }
 
 // { dg-final { scan-assembler-not "\n_?_ZN1AIcE1tE(:|\n|\t)" } }
