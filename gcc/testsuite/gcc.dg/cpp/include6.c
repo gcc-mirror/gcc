43d0678ae3af360c837b9ef6d82694da1e8040a4
Jason Merrill <jason@redhat.com>
2010-03-29 16:57:00 -0400

    * gcc.dg/cpp/include6.c: Change [] to ().
    From-SVN: r157805

--- gcc/testsuite/gcc.dg/cpp/include6.c
+++ gcc/testsuite/gcc.dg/cpp/include6.c
@@ -7,8 +7,8 @@
 #include u"stddef.h"		/* { dg-error "include expects" } */
 #include U"stddef.h"		/* { dg-error "include expects" } */
 #include u8"stddef.h"		/* { dg-error "include expects" } */
-#include R"[stddef.h]"		/* { dg-error "include expects" } */
-#include LR"[stddef.h]"		/* { dg-error "include expects" } */
-#include uR"[stddef.h]"		/* { dg-error "include expects" } */
-#include UR"[stddef.h]"		/* { dg-error "include expects" } */
-#include u8R"[stddef.h]"	/* { dg-error "include expects" } */
+#include R"(stddef.h)"		/* { dg-error "include expects" } */
+#include LR"(stddef.h)"		/* { dg-error "include expects" } */
+#include uR"(stddef.h)"		/* { dg-error "include expects" } */
+#include UR"(stddef.h)"		/* { dg-error "include expects" } */
+#include u8R"(stddef.h)"	/* { dg-error "include expects" } */
