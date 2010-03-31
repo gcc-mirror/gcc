3b9c9e45f9b6c898d6a9837e88679a772e15a3d9
Uros Bizjak <uros@gcc.gnu.org>
2010-03-31 20:09:04 +0200

    backport: re PR tree-optimization/43528 (ICE: in tree_low_cst, at tree.c:6198 with -mms-bitfields at x86_64-linux)
    	Backport from mainline:
    	2010-03-27  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR tree-optimization/43528
    	* stor-layout.c (place_field): Check that constant fits into
    	unsigned HWI when skipping calculation of MS bitfield layout.
    
    	2010-03-26  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/43524
    	* config/i386/i386.c (ix86_expand_prologue) [TARGET_STACK_PROBE]:
    	Remove invalid assert and wrong comment.
    
    testsuite/ChangeLog:
    
    	Backport from mainline:
    	2010-03-27  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR tree-optimization/43528
    	* gcc.target/i386/pr43528.c: New test.
    
    	2010-03-26  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/43524
    	* gcc.target/i386/pr43524.c: New test.
    
    From-SVN: r157880

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -8280,12 +8280,9 @@ ix86_expand_prologue (void)
 			       GEN_INT (-allocate), -1);
   else
     {
-      /* Only valid for Win32.  */
       rtx eax = gen_rtx_REG (Pmode, AX_REG);
       bool eax_live;
       rtx t;
-
-      gcc_assert (!TARGET_64BIT || cfun->machine->call_abi == MS_ABI);
 
       if (cfun->machine->call_abi == MS_ABI)
 	eax_live = false;
