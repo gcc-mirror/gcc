1359ee7afa8bb57cdaf6326609eb1b1d97668231
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-04-01 17:33:32 +0000

    * cgraph.c (cgraph_add_function_insertion_hook): Update comment.         (cgraph_remove_function_insertion_hook): Same.         (cgraph_call_function_insertion_hooks): Same.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@157920 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cgraph.c
+++ gcc/cgraph.c
@@ -273,7 +273,7 @@ cgraph_call_node_removal_hooks (struct cgraph_node *node)
   }
 }
 
-/* Register HOOK to be called with DATA on each removed node.  */
+/* Register HOOK to be called with DATA on each inserted node.  */
 struct cgraph_node_hook_list *
 cgraph_add_function_insertion_hook (cgraph_node_hook hook, void *data)
 {
@@ -290,7 +290,7 @@ cgraph_add_function_insertion_hook (cgraph_node_hook hook, void *data)
   return entry;
 }
 
-/* Remove ENTRY from the list of hooks called on removing nodes.  */
+/* Remove ENTRY from the list of hooks called on inserted nodes.  */
 void
 cgraph_remove_function_insertion_hook (struct cgraph_node_hook_list *entry)
 {
@@ -302,7 +302,7 @@ cgraph_remove_function_insertion_hook (struct cgraph_node_hook_list *entry)
   free (entry);
 }
 
-/* Call all node removal hooks.  */
+/* Call all node insertion hooks.  */
 void
 cgraph_call_function_insertion_hooks (struct cgraph_node *node)
 {
