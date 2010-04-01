dba9a1a567d3d19e8d86019264bf9d92184bd21a
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-04-01 12:43:04 +0000

    * g++.dg/cpp/_Pragma1.C: Skip on alpha*-dec-osf*. 	* g++.dg/eh/spbp.C: Likewise. 	* g++.dg/ext/label13.C (C::C): xfail dg-bogus on alpha*-dec-osf*. 	* g++.dg/other/pragma-ep-1.C: Properly define p, remove 	superfluous casts. 	* gcc.dg/mtune.c: Add dg-bogus "mcpu". 	* objc.dg/dwarf-1.m: Skip on  alpha*-dec-osf*. 	* objc.dg/dwarf-2.m: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@157905 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/mtune.c
+++ gcc/testsuite/gcc.dg/mtune.c
@@ -2,4 +2,5 @@
 /* { dg-options "-mtune=foo" } */
 /* { dg-error "mtune" "" { target *-*-* } 0 } */
 /* { dg-bogus "march" "" { target *-*-* } 0 } */
+/* { dg-bogus "mcpu" "" { target *-*-* } 0 } */
 int i;
