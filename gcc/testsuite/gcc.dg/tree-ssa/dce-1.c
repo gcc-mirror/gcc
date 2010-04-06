b1a0b3b4ff189d34f58e2111121b35fb8c279602
Jan Hubicka <jh@suse.cz>
2010-04-06 15:18:18 +0000

    re PR tree-optimization/42906 (Empty loop not removed)
    	PR tree-optimization/42906
    
    	* tree-ssa-dce.c (mark_control_dependent_edges_necessary): Add IGNORE_SELF
    	argument; set visited_control_parents for fully processed BBs.
    	(find_obviously_necessary_stmts): Update call of
    	mark_control_dependent_edges_necessary.
    	(propagate_necessity): Likewise; handle PHI edges more curefully.
    
    	* gcc.dg/tree-ssa/dce-1.c: New testcase.
    
    From-SVN: r158004

---
+++ gcc/testsuite/gcc.dg/tree-ssa/dce-1.c
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-cddce1" } */
+int foo (int b, int j)
+{
+  if (b)
+    {
+      int i;
+      for (i = 0; i<1000; ++i)
+        ;
+      j = 0;
+    }
+  return j;
+}
+/* Check that empty loop is eliminated in this case.  We should no longer have
+   the exit condition after the loop.  */
+/* { dg-final { scan-tree-dump-not "999" "cddce1"} } */
+/* { dg-final { scan-tree-dump-not "1000" "cddce1"} } */
+/* { dg-final { cleanup-tree-dump "cddce1" } } */
+
