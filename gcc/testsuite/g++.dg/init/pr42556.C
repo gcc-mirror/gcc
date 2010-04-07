6addabbbe815be8107207fcb6e2aa640e2e8c6ca
Jie Zhang <jie@codesourcery.com>
2010-04-07 10:14:45 +0000

    re PR c++/42556 (Unnecessary generation of a zero initializer for array with C++)
    	cp/
    	PR c++/42556
    	* typeck2.c (split_nonconstant_init_1): Drop empty CONSTRUCTOR
    	when all of its elements are non-constant and have been split out.
    
    	testsuite/
    	PR c++/42556
    	* g++.dg/init/pr42556.C: New test.
    
    From-SVN: r158047

---
+++ gcc/testsuite/g++.dg/init/pr42556.C
@@ -0,0 +1,10 @@
+// { dg-do compile }
+// { dg-options "-fdump-tree-gimple" }
+
+void foo (int a, int b, int c, int d)
+{
+  int v[4] = {a, b, c, d};
+}
+
+// { dg-final { scan-tree-dump-not "v = {}"  "gimple" } }
+// { dg-final { cleanup-tree-dump "gimple" } }
