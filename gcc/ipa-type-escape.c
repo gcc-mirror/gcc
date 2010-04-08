e87cb2f08b9169275f89c3c88793032b2fa81ac3
Jakub Jelinek <jakub@redhat.com>
2010-04-08 13:29:28 +0200

    backport: re PR tree-optimization/43607 (ICE in gimple_op, at gimple.h:1633 with -fipa-type-escape)
    	Backport from mainline:
    	2010-04-01  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/43607
    	* ipa-type-escape.c (check_call): Do not access non-existing
    	arguments.
    
    From-SVN: r158118

--- gcc/ipa-type-escape.c
+++ gcc/ipa-type-escape.c
@@ -1,6 +1,6 @@
 /* Type based alias analysis.
-   Copyright (C) 2004, 2005, 2006, 2007, 2008 Free Software Foundation,
-   Inc.
+   Copyright (C) 2004, 2005, 2006, 2007, 2008, 2010
+   Free Software Foundation, Inc.
    Contributed by Kenneth Zadeck <zadeck@naturalbridge.com>
 
 This file is part of GCC.
@@ -1343,7 +1343,8 @@ check_call (gimple call)
       if (TYPE_ARG_TYPES (TREE_TYPE (callee_t)))
 	{
 	  for (arg_type = TYPE_ARG_TYPES (TREE_TYPE (callee_t)), i = 0;
-	       arg_type && TREE_VALUE (arg_type) != void_type_node;
+	       arg_type && TREE_VALUE (arg_type) != void_type_node
+	       && i < gimple_call_num_args (call);
 	       arg_type = TREE_CHAIN (arg_type), i++)
 	    {
 	      tree operand = gimple_call_arg (call, i);
@@ -1365,7 +1366,7 @@ check_call (gimple call)
 	     have to do this; the front ends should always process
 	     the arg list from the TYPE_ARG_LIST. */
 	  for (arg_type = DECL_ARGUMENTS (callee_t), i = 0;
-	       arg_type;
+	       arg_type && i < gimple_call_num_args (call);
 	       arg_type = TREE_CHAIN (arg_type), i++)
 	    {
 	      tree operand = gimple_call_arg (call, i);
