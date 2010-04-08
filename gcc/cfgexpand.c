5be8931100edcfa6622ad4fe9083d402b77f1770
Jakub Jelinek <jakub@redhat.com>
2010-04-08 11:17:52 +0200

    re PR debug/43670 ("-fcompare-debug failure (length)" with -O -ftree-vrp)
    	PR debug/43670
    	* cfgexpand.c (expand_debug_expr): If for non-NULL offset
    	op0 is not a MEM, just return NULL instead of assertion
    	failure.
    	(discover_nonconstant_array_refs): Don't walk debug stmts.
    
    	* gcc.dg/pr43670.c: New test.
    
    From-SVN: r158109

--- gcc/cfgexpand.c
+++ gcc/cfgexpand.c
@@ -2502,7 +2502,8 @@ expand_debug_expr (tree exp)
 	  {
 	    enum machine_mode addrmode, offmode;
 
-	    gcc_assert (MEM_P (op0));
+	    if (!MEM_P (op0))
+	      return NULL;
 
 	    op0 = XEXP (op0, 0);
 	    addrmode = GET_MODE (op0);
@@ -3623,7 +3624,8 @@ discover_nonconstant_array_refs (void)
     for (gsi = gsi_start_bb (bb); !gsi_end_p (gsi); gsi_next (&gsi))
       {
 	gimple stmt = gsi_stmt (gsi);
-	walk_gimple_op (stmt, discover_nonconstant_array_refs_r, NULL);
+	if (!is_gimple_debug (stmt))
+	  walk_gimple_op (stmt, discover_nonconstant_array_refs_r, NULL);
       }
 }
 
