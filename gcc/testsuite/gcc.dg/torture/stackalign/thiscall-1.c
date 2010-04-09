5a84efd3f60af101e802bef5ecc817b315eecc71
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-04-09 08:52:54 +0000

    2010-04-09  Kai Tietz  <kai.tietz@onevision.com>
            * g++.dg/other/pr35504.C: Add check for thiscall.
            * g++.dg/torture/stackalign/eh-thiscall-1.C: New.
            * gcc.dg/torture/stackalign/thiscall-1.c: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@158153 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/stackalign/thiscall-1.c
@@ -0,0 +1,31 @@
+/* { dg-do run { target { { i?86-*-* x86_64-*-* } && ilp32 } } } */
+
+#include "check.h"
+
+#ifndef ALIGNMENT
+#define ALIGNMENT	64
+#endif
+
+typedef int aligned __attribute__((aligned(ALIGNMENT)));
+
+int global;
+
+__attribute__ ((thiscall))
+void
+foo (int j, int k, int m, int n, int o)
+{
+  aligned i;
+
+  if (check_int (&i,  __alignof__(i)) != i)
+    abort ();
+
+  if (i != 20 || j != 1 || k != 2 || m != 3 || n != 4 || o != 5)
+    abort ();
+}
+
+int
+main()
+{
+  foo (1, 2, 3, 4, 5);
+  return 0;
+}
