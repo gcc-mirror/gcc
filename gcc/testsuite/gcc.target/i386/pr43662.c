951fbc161f676ea0028cbc3d7547e999e9993ee0
Jie Zhang <jie@codesourcery.com>
2010-04-19 10:04:43 +0000

    re PR target/43662 (ICE in insert_save with ms_abi attribute)
    	PR target/43662
    	* reginfo.c (reinit_regs): Set caller_save_initialized_p
    	to false.
    
    	testsuite/
    	PR target/43662
    	* gcc.target/i386/pr43662.c: New test.
    
    From-SVN: r158510

---
+++ gcc/testsuite/gcc.target/i386/pr43662.c
@@ -0,0 +1,24 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target lp64 } */
+/* { dg-options "-O2" } */
+
+void __attribute__ ((ms_abi)) foo (void)
+{
+}
+
+typedef struct _IAVIStreamImpl
+{
+  int sInfo;
+  int has;
+} IAVIStreamImpl;
+
+extern int __attribute__ ((ms_abi)) aso (void *);
+extern int sre (void *);
+
+int AVIFILE_OpenCompressor (IAVIStreamImpl *This)
+{
+  if (This->has != 0)
+    aso (&This->has);
+  sre (&This->sInfo);
+  return 0;
+}
