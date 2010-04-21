2e1fdf999df26a0f513a2796ff2c27b2f34a4364
Jakub Jelinek <jakub@redhat.com>
2010-04-21 16:02:39 +0200

    re PR middle-end/43570 (OpenMP: Invalid read of size 1 (libgomp.fortran/vla6.f90))
    	PR middle-end/43570
    	* omp-low.c (scan_sharing_clauses): Don't scan_omp_op
    	OMP_CLAUSE_DECL for OMP_CLAUSE_COPYPRIVATE.
    	(lower_copyprivate_clauses): Use private var in outer
    	context instead of original var.  Make sure the types
    	are correct for VLAs.
    
    	* testsuite/libgomp.fortran/vla8.f90: New test.
    
    From-SVN: r158603

--- gcc/omp-low.c
+++ gcc/omp-low.c
@@ -1421,10 +1421,6 @@ scan_sharing_clauses (tree clauses, omp_context *ctx)
 	  break;
 
 	case OMP_CLAUSE_COPYPRIVATE:
-	  if (ctx->outer)
-	    scan_omp_op (&OMP_CLAUSE_DECL (c), ctx->outer);
-	  /* FALLTHRU */
-
 	case OMP_CLAUSE_COPYIN:
 	  decl = OMP_CLAUSE_DECL (c);
 	  by_ref = use_pointer_for_field (decl, NULL);
@@ -2671,7 +2667,7 @@ lower_copyprivate_clauses (tree clauses, gimple_seq *slist, gimple_seq *rlist,
 
   for (c = clauses; c ; c = OMP_CLAUSE_CHAIN (c))
     {
-      tree var, ref, x;
+      tree var, new_var, ref, x;
       bool by_ref;
 
       if (OMP_CLAUSE_CODE (c) != OMP_CLAUSE_COPYPRIVATE)
@@ -2681,17 +2677,27 @@ lower_copyprivate_clauses (tree clauses, gimple_seq *slist, gimple_seq *rlist,
       by_ref = use_pointer_for_field (var, NULL);
 
       ref = build_sender_ref (var, ctx);
-      x = lookup_decl_in_outer_ctx (var, ctx);
-      x = by_ref ? build_fold_addr_expr (x) : x;
+      x = new_var = lookup_decl_in_outer_ctx (var, ctx);
+      if (by_ref)
+	{
+	  x = build_fold_addr_expr (new_var);
+	  x = fold_convert (TREE_TYPE (ref), x);
+	}
       gimplify_assign (ref, x, slist);
 
-      ref = build_receiver_ref (var, by_ref, ctx);
+      ref = build_receiver_ref (var, false, ctx);
+      if (by_ref)
+	{
+	  ref = fold_convert (build_pointer_type (TREE_TYPE (new_var)), ref);
+	  ref = build_fold_indirect_ref (ref);
+	}
       if (is_reference (var))
 	{
+	  ref = fold_convert (TREE_TYPE (new_var), ref);
 	  ref = build_fold_indirect_ref (ref);
-	  var = build_fold_indirect_ref (var);
+	  new_var = build_fold_indirect_ref (new_var);
 	}
-      x = lang_hooks.decls.omp_clause_assign_op (c, var, ref);
+      x = lang_hooks.decls.omp_clause_assign_op (c, new_var, ref);
       gimplify_and_add (x, rlist);
     }
 }
