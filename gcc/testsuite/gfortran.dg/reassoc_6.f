50fa62c69940ddea294ac7656ec7c04ffa990221
amonakov <amonakov@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-04-22 12:44:30 +0000

    2010-04-22  Alexander Monakov  <amonakov@ispras.ru>
    	* tree-ssa-reassoc.c (eliminate_plus_minus_pair): Handle BIT_NOT_EXPR
    	to simplify a + ~a.
    
    	* gfortran.dg/reassoc_6.f: New testcase.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@158645 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/reassoc_6.f
@@ -0,0 +1,20 @@
+! { dg-do compile }
+! { dg-options "-O2 -fdump-tree-optimized" }
+
+        subroutine test(nb,nx,r2)
+        implicit none
+        integer nb,nx,i,l
+        real*8 r2(nb,nx)
+
+
+             do i=1,nx
+                do l=1,nb
+                   r2(l,i)=0.0d0
+                enddo
+             enddo
+
+        return
+        end
+! Verify that offset of the first element is simplified
+! { dg-final { scan-tree-dump-not "~" "optimized" } }
+! { dg-final { cleanup-tree-dump "optimized" } }
