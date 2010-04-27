78eaf7bf186397f1ea7ca581c1e76e543eaac9b6
Martin Jambor <mjambor@suse.cz>
2010-04-27 12:07:47 +0200

    re PR middle-end/43812 (compiling .cc file with -fwhole-program results in ICE, in ipcp_iterate_stage, at ipa-cp.c:760)
    2010-04-27  Martin Jambor  <mjambor@suse.cz>
    
    	PR middle-end/43812
    	* ipa.c (dissolve_same_comdat_group_list): New function.
    	(function_and_variable_visibility): Call
    	dissolve_same_comdat_group_list when comdat group contains external or
    	newly local nodes.
    	* cgraphunit.c (verify_cgraph_node): Verify that same_comdat_group
    	lists are circular and that they contain only DECL_ONE_ONLY nodes.
    
    	* testsuite/g++.dg/ipa/pr43812.C: New test.
    
    From-SVN: r158777

---
+++ gcc/testsuite/g++.dg/ipa/pr43812.C
@@ -0,0 +1,38 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fwhole-program -fipa-cp" } */
+
+typedef float scoord_t;
+typedef scoord_t sdist_t;
+typedef sdist_t dist_t;
+template<typename T> class TRay { };
+typedef TRay<dist_t> Ray;
+class BBox { };
+class RenderContext { };
+class RefCounted {
+public:
+    void deref () const {
+        if (--ref_count <= 0) {
+            delete this;
+        }
+    }
+    mutable int ref_count;
+};
+template<class T> class Ref {
+public:
+    ~Ref () {
+        if (obj) obj->deref ();
+    }
+    T *obj;
+};
+class Material : public RefCounted { };
+class Surface {
+public:
+    virtual ~Surface () { }
+    class IsecInfo   { };
+    virtual const IsecInfo *intersect (Ray &ray, RenderContext &context) const;
+    Ref<const Material> material;
+};
+class LocalSurface : public Surface {
+    virtual BBox bbox () const;
+};
+BBox LocalSurface::bbox () const { }
