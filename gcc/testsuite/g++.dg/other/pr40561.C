b8d26a68da98838776945fd86137f339e7ebf3bd
Richard Guenther <rguenther@suse.de>
2010-04-27 12:15:53 +0000

    backport: re PR tree-optimization/38564 (Revision 142662 caused ICE in insert_into_preds_of_block)
    2010-04-27  Richard Guenther  <rguenther@suse.de>
    
    	Backport from mainline
    	2008-12-29  Richard Guenther  <rguenther@suse.de>
    
    	PR middle-end/38564
    	* fold-const.c (fold_comparison): Use the correct result type.
    
    	* gcc.c-torture/compile/pr38564.c: New testcase.
    	* g++.dg/other/pr40561.C: Likewise.
    
    From-SVN: r158782

---
+++ gcc/testsuite/g++.dg/other/pr40561.C
@@ -0,0 +1,38 @@
+// { dg-do compile }
+
+#include <set>
+
+class SyAccess;
+class VamsBase 
+{
+	public:
+	virtual ~VamsBase(void);
+};
+
+class VamsFnct : public VamsBase 
+{	
+	public:
+	~VamsFnct(void);
+	std::set<SyAccess*> getNullDependencies(void) const
+	{	return std::set<SyAccess*>();
+	}
+};
+
+class VamsFnctSystem:public VamsFnct
+{	public:
+	VamsFnctSystem(
+		const bool _bPassDependencies);
+};
+
+template< std::set<SyAccess*> (VamsFnct::*__GET_DEP__)(void) const >
+class VamsSystemFunction:public VamsFnctSystem
+{	public:
+	VamsSystemFunction()
+	    :VamsFnctSystem(
+		__GET_DEP__ != &VamsFnct::getNullDependencies
+		)
+	{
+	}
+};
+
+VamsSystemFunction<&VamsFnct::getNullDependencies> s;
