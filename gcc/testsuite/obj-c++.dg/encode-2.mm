619dea2d5c764e83340099eea2f33296e148b4da
Iain Sandoe <iains@gcc.gnu.org>
2010-05-01 00:32:13 +0000

    [multiple changes]
    2010-04-30  Iain Sandoe  <iains@gcc.gnu.org>
    
    	PR objc++/32052
    	* objc-act.c (encode_aggregate_within): Encode structure tags
    	with template args for ObjC++.
    
    2010-04-30  Iain Sandoe <iains@gcc.gnu.org>
    
    	PR objc++/32052
    	* obj-c++.dg/encode-2.mm: Remove XFAIL. Add test for anonymous
    	structure and nested declarations.
    	* obj-c++.dg/encode-3.mm:  Remove XFAIL. Add test for anonymous
    	structure and nested declarations.  Reduce header clutter and
    	use _exit() rather than abort().
    	* objc.dg/encode-10.m: New.
    	* objc.dg/encode-11.m: New.
    
    From-SVN: r158958

--- gcc/testsuite/obj-c++.dg/encode-2.mm
+++ gcc/testsuite/obj-c++.dg/encode-2.mm
@@ -6,9 +6,21 @@ struct Vec {
  int z;
 };
 
+typedef struct {
+  Vec<double> dvec;
+  Vec<float> fvec;
+  float fscalar;
+  double dscalar;
+  Vec<char> chVec;
+  int iscalar;
+} anonymous;
+
 Vec<double> dd;
+
 const char *enc = @encode(Vec<float>);
 const char *enc2 = @encode(Vec<double>);
+const char *enc3 = @encode(anonymous);
 
-/* { dg-final { scan-assembler "{Vec<float>=ffi}" { xfail "*-*-*" } } } PR32052 */
-/* { dg-final { scan-assembler "{Vec<double>=ddi}" { xfail "*-*-*" } } } PR32052 */
+/* { dg-final { scan-assembler "{Vec<float>=ffi}" } }  */
+/* { dg-final { scan-assembler "{Vec<double>=ddi}" } }  */
+/* { dg-final { scan-assembler "{?={Vec<double>=ddi}{Vec<float>=ffi}fd{Vec<char>=cci}i}" } }  */
--- gcc/testsuite/obj-c++.dg/encode-3.mm
+++ gcc/testsuite/obj-c++.dg/encode-3.mm
@@ -1,18 +1,30 @@
-/* { dg-do run { xfail { "*-*-*" } } } PR32052 */
+/* { dg-do run } */
 
-#include <stdlib.h>
-#include <string.h>
+extern "C" {
+extern void _exit(int);
+extern int strcmp(const char *, const char *);
+}
 
 template <class T>
 struct Vec {
- T x, y;
- long z;
- long long zz;
+  T x, y;
+  long z;
+  long long zz;
 };
 
-Vec<double> dd;
+typedef struct {
+  float fscalar;
+  double dv[10];
+  int iscalar;
+  long z;
+  long long zz;
+  Vec<const char> cv;
+} anonymous;
+
+//Vec<double> dd;
 const char *enc = @encode(Vec<float>);
 const char *enc2 = @encode(Vec<double>);
+const char *enc3 = @encode(anonymous);
 
 #ifdef __LP64__
 #define L "q"
@@ -24,13 +36,16 @@ int main(void) {
   const char *encode = @encode(long);
 
   if (strcmp (encode, L))
-    abort();
+    _exit(-(__LINE__));
 
   if (strcmp (enc, "{Vec<float>=ff" L "q}"))
-    abort();
+    _exit(-(__LINE__));
 
   if (strcmp (enc2, "{Vec<double>=dd" L "q}"))
-    abort();
+    _exit(-(__LINE__));
+
+  if (strcmp (enc3, "{?=f[10d]i" L "q{Vec<const char>=rcrc" L "q}}"))
+    _exit(-(__LINE__));
 
   return 0;
 }
