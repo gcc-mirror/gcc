5e7b92b9ff393c7cc64a7ff7d3400ab86d5f587a
Tobias Burnus <burnus@net-b.de>
2010-05-06 00:11:14 +0200

    re PR fortran/43985 (ICE when assumed size cray-pointee passed as argument)
    2010-05-06  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/43985
            * trans-types.c (gfc_sym_type): Mark Cray pointees as
            GFC_POINTER_TYPE_P.
    
    2010-05-06  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/43985
            * gfortran.dg/gomp/crayptr5.f90: New test case.
    
    From-SVN: r159086

---
+++ gcc/testsuite/gfortran.dg/gomp/crayptr5.f90
@@ -0,0 +1,27 @@
+! { dg-do compile }
+! { dg-options "-fopenmp -fcray-pointer" }
+!
+! PR fortran/43985
+
+subroutine pete(A)
+  real(8) :: A
+  print *, 'pete got ',A
+  if (A /= 3.0) call abort()
+end subroutine pete
+
+       subroutine bob()
+         implicit none
+         real(8) peted
+         pointer (ipeted, peted(*))
+         integer(4) sz
+         ipeted = malloc(5*8)
+         peted(1:5) = [ 1.,2.,3.,4.,5.]
+         sz = 3
+!$omp parallel default(shared)
+         call pete(peted(sz))
+!$omp end parallel
+         return
+       end subroutine bob
+
+call bob()
+end
