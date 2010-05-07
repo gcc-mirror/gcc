2cd5965fa3cd3586a642e75c76a256f36f8a99d0
Jason Merrill <jason@redhat.com>
2010-05-07 09:53:46 -0400

    * g++.dg/cpp0x/nullptr20.C: Use sprintf.
    From-SVN: r159153

--- gcc/testsuite/g++.dg/cpp0x/nullptr20.C
+++ gcc/testsuite/g++.dg/cpp0x/nullptr20.C
@@ -11,7 +11,7 @@ int main()
   char buf1[64];
   char buf2[64];
 
-  std::snprintf(buf1, sizeof(buf1), "%p", (void*)0);
-  std::snprintf(buf2, sizeof(buf2), "%p", nullptr);
+  std::sprintf(buf1, "%p", (void*)0);
+  std::sprintf(buf2, "%p", nullptr);
   return std::strcmp(buf1, buf2) != 0;
 }
