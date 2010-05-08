901ad63f3070d0f561f205618cc830486af365fb
Eric Botcazou <ebotcazou@adacore.com>
2010-05-08 11:50:18 +0000

    exp_disp.adb (Make_Tags): Mark the imported view of dispatch tables.
    	* exp_disp.adb (Make_Tags): Mark the imported view of dispatch tables.
    	* gcc-interface/decl.c (gnat_to_gnu_entity) <E_Variable>: Make imported
    	constants really constant.
    	<E_Record_Subtype>: Strip the suffix for dispatch table entities.
    
    From-SVN: r159184

---
+++ gcc/testsuite/gnat.dg/lto5.adb
@@ -0,0 +1,9 @@
+-- { dg-do run }
+-- { dg-options "-flto" }
+
+with Lto5_Pkg;
+
+procedure Lto5 is
+begin
+   null;
+end;
---
+++ gcc/testsuite/gnat.dg/lto5_pkg.ads
@@ -0,0 +1,6 @@
+pragma Eliminate (p, d);
+
+package Lto5_Pkg is
+   type t is tagged null record;
+   procedure d (a : t);
+end;
