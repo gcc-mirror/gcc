3d7a712a1e3b2e67b5261e8e4b34f3b701f49a70
Richard Guenther <rguenther@suse.de>
2010-05-09 18:17:33 +0000

    re PR c/44024 (missed optimization)
    2010-05-09  Richard Guenther  <rguenther@suse.de>
    
    	PR middle-end/44024
    	* fold-const.c (tree_single_nonzero_warnv_p): Properly
    	handle &FUNCTION_DECL.
    
    	* gcc.dg/pr44024.c: New testcase.
    
    From-SVN: r159205

---
+++ gcc/testsuite/gcc.dg/pr44024.c
@@ -0,0 +1,14 @@
+/* { dg-do link } */
+/* { dg-options "-fdelete-null-pointer-checks -fdump-tree-original" } */
+
+void foo();
+
+int main()
+{
+  if (foo == (void *)0)
+    link_error ();
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-not "foo" "original" } } */
+/* { dg-final { cleanup-tree-dump "original" } } */
