6dd40464ebad4337c6007c858589cff6ec7750e1
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-05-11 15:05:36 +0000

    2010-05-11  Kai Tietz  <kai.tietz@onevision.com>
            * lto-coff.c (validate_file): Add x64-coff support.
            * lto-coff.h (IMAGE_FILE_MACHINE_ADM64): New.
            (COFF_KNOWN_MACHINES): Add IMAGE_FILE_MACHINE_ADM64.
            * lto-lang.c (lto_build_c_type_nodes): Add check for
            'long long unsigned int' for x64-windows.
            (lto_init): Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@159271 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/lto/lto-lang.c
+++ gcc/lto/lto-lang.c
@@ -1019,6 +1019,12 @@ lto_build_c_type_nodes (void)
       uintmax_type_node = long_unsigned_type_node;
       signed_size_type_node = long_integer_type_node;
     }
+  else if (strcmp (SIZE_TYPE, "long long unsigned int") == 0)
+    {
+      intmax_type_node = long_long_integer_type_node;
+      uintmax_type_node = long_long_unsigned_type_node;
+      signed_size_type_node = long_long_integer_type_node;
+    }
   else
     gcc_unreachable ();
 
@@ -1061,6 +1067,11 @@ lto_init (void)
     {
       set_sizetype (long_unsigned_type_node);
       size_type_node = long_unsigned_type_node;
+    }
+  else if (strcmp (SIZE_TYPE, "long long unsigned int") == 0)
+    {
+      set_sizetype (long_long_unsigned_type_node);
+      size_type_node = long_long_unsigned_type_node;
     }
   else
     gcc_unreachable ();
