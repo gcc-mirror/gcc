b0b48c1d97e7aa097e96d348fe7a4b4d7301eba8
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-05-12 12:11:00 +0000

    PR middle-end/44085 	* gimplify.c (enum omp_region_type): Add ORT_UNTIED_TASK, 	change value of ORT_TASK. 	(new_omp_context): Handle ORT_UNTIED_TASK like ORT_TASK. 	(omp_notice_threadprivate_variable): New function. 	(omp_notice_variable): Call it for threadprivate variables. 	If enclosing ctx is a task, print enclosing task rather than 	enclosing parallel.  Handle ORT_UNTIED_TASK like ORT_TASK. 	(gimplify_omp_task): Pass ORT_UNTIED_TASK instead of ORT_TASK 	if task has untied clause.
    	* gcc.dg/gomp/pr44085.c: New test.
    	* gfortran.dg/gomp/pr44085.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@159316 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/gomp/pr44085.c
@@ -0,0 +1,27 @@
+/* PR middle-end/44085 */
+/* { dg-do compile } */
+/* { dg-require-effective-target tls_native } */
+/* { dg-options "-fopenmp" } */
+
+int thr1, thr2;
+#pragma omp threadprivate (thr1, thr2)
+
+void
+foo (void)
+{
+#pragma omp task untied	/* { dg-error "enclosing task" } */
+  {
+    thr1++;		/* { dg-error "used in untied task" } */
+    thr2 |= 4;		/* { dg-error "used in untied task" } */
+  }
+}
+
+void
+bar (void)
+{
+#pragma omp task
+  {
+    thr1++;
+    thr2 |= 4;
+  }
+}
