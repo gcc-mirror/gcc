c673254729cfa36f819153d213d0097dfee34937
froydnj <froydnj@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-05-12 15:53:39 +0000

    * tree-mudflap.c (build_function_type_0, build_function_type_1, 	build_function_type_2, build_function_type_3): Remove. 	(mudflap_init): Use build_function_type_list.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@159328 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-mudflap.c
+++ gcc/tree-mudflap.c
@@ -332,24 +332,6 @@ mf_make_mf_cache_struct_type (tree field_type)
   return struct_type;
 }
 
-#define build_function_type_0(rtype)           				\
-  build_function_type (rtype, void_list_node)
-#define build_function_type_1(rtype, arg1)                		\
-  build_function_type (rtype, tree_cons (0, arg1, void_list_node))
-#define build_function_type_3(rtype, arg1, arg2, arg3)                  \
-  build_function_type (rtype,						\
-		       tree_cons (0, arg1, 				\
-				  tree_cons (0, arg2,  			\
-                                              tree_cons (0, arg3, 	\
-							 void_list_node))))
-#define build_function_type_4(rtype, arg1, arg2, arg3, arg4)            \
-  build_function_type (rtype, 						\
-		       tree_cons (0, arg1,				\
-				  tree_cons (0, arg2,   		\
-                                             tree_cons (0, arg3,	\
-							tree_cons (0, arg4, \
-                                                		   void_list_node)))))
-
 /* Initialize the global tree nodes that correspond to mf-runtime.h
    declarations.  */
 void
@@ -377,15 +359,15 @@ mudflap_init (void)
   mf_cache_structptr_type = build_pointer_type (mf_cache_struct_type);
   mf_cache_array_type = build_array_type (mf_cache_struct_type, 0);
   mf_check_register_fntype =
-    build_function_type_4 (void_type_node, ptr_type_node, size_type_node,
-                           integer_type_node, mf_const_string_type);
+    build_function_type_list (void_type_node, ptr_type_node, size_type_node,
+			      integer_type_node, mf_const_string_type, NULL_TREE);
   mf_unregister_fntype =
-    build_function_type_3 (void_type_node, ptr_type_node, size_type_node,
-                           integer_type_node);
+    build_function_type_list (void_type_node, ptr_type_node, size_type_node,
+			      integer_type_node, NULL_TREE);
   mf_init_fntype =
-    build_function_type_0 (void_type_node);
+    build_function_type_list (void_type_node, NULL_TREE);
   mf_set_options_fntype =
-    build_function_type_1 (integer_type_node, mf_const_string_type);
+    build_function_type_list (integer_type_node, mf_const_string_type, NULL_TREE);
 
   mf_cache_array_decl = mf_make_builtin (VAR_DECL, "__mf_lookup_cache",
                                          mf_cache_array_type);
@@ -409,10 +391,6 @@ mudflap_init (void)
   mf_set_options_fndecl = mf_make_builtin (FUNCTION_DECL, "__mf_set_options",
                                            mf_set_options_fntype);
 }
-#undef build_function_type_4
-#undef build_function_type_3
-#undef build_function_type_1
-#undef build_function_type_0
 
 
 /* ------------------------------------------------------------------------ */
