3e5482040c58423a2a6e5fdb321d630adcea200c
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-05-15 13:54:44 +0000

    Add -std=c99 to avx-cmpsd-1.c and avx-cmpss-1.c.
    2010-05-15  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* gcc.target/i386/avx-cmpsd-1.c: Add -std=c99.
    	* gcc.target/i386/avx-cmpss-1.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@159432 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/avx-cmpss-1.c
+++ gcc/testsuite/gcc.target/i386/avx-cmpss-1.c
@@ -1,6 +1,6 @@
 /* { dg-do run } */
 /* { dg-require-effective-target avx } */
-/* { dg-options "-O2 -mavx" } */
+/* { dg-options "-O2 -mavx -std=c99" } */
 
 #define CHECK_H "avx-check.h"
 #define TEST avx_test
