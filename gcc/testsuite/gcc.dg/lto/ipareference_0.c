84f79018b829de7417bf0818e487db4624b8096f
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-05-15 17:06:48 +0000

    * gcc.dg/lto/ipareference_1.c: Rename to ... 	* gcc.dg/lto/ipareference_0.c: ... this one. 	* gcc.dg/lto/ipareference_2.c: Rename to ... 	* gcc.dg/lto/ipareference_1.c: ... this one.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@159436 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/lto/ipareference_0.c
@@ -0,0 +1,26 @@
+/* { dg-lto-options {{ -O1 -fwhopr }} } */
+/* { dg-lto-do run } */
+
+/* Test that ipa-reference notice that get_val will not change since do_nothing does not
+   modify anything.  This needs streaming cross file boundary summaries.  */
+extern int get_val (void);
+extern int set_val (void);
+extern do_nothing (void);
+void abort (void);
+main()
+{
+  int a;
+  int b;
+  set_val ();
+  a = get_val ();
+  do_nothing();
+  b = get_val ();
+  if (a==b)
+    {
+      if (!__builtin_constant_p (a==b))
+	abort ();
+      return 0;
+    }
+  else
+    abort ();
+}
