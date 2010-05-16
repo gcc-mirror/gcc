cdc6637d7c78ec66bff42ef5a38c1f5a4cd2074d
Daniel Franke <franke.daniel@gmail.com>
2010-05-16 16:01:06 -0400

    re PR fortran/35779 (error pointer wrong in PARAMETER)
    gcc/fortran/:
    2010-05-16  Daniel Franke  <franke.daniel@gmail.com>
    
            PR fortran/35779
    	* array.c (match_array_list): Revert functional change of 2010-05-13.
    
    gcc/fortran/:
    2010-05-16  Daniel Franke  <franke.daniel@gmail.com>
    
            PR fortran/35779
    	* gfortran.dg/initialization_25.f90: Commented testcase.
    	* gfortran.dg/initialization_26.f90: New.
    
    From-SVN: r159465

--- gcc/testsuite/gfortran.dg/initialization_25.f90
+++ gcc/testsuite/gfortran.dg/initialization_25.f90
@@ -4,8 +4,9 @@
 ! Tescase contributed by
 ! Dick Hendrickson <dick DOT hendrickson AT gmail DOT com>
 !
+! Initial patch was reverted as it broke nested loops (see initialization_26.f90).
+!
 
-  INTEGER :: J1
-  INTEGER,PARAMETER :: I3(10) = (/(J1,J1=10,1,-1)/)
-  INTEGER,PARAMETER :: I2(10) = (/(J1,J1=its_bad,1,-1)/) ! { dg-error "does not reduce" }
+!   INTEGER :: J1
+!   INTEGER,PARAMETER :: I2(10) = (/(J1,J1=its_bad,1,-1)/) ! { dg - error "does not reduce" }
 END
