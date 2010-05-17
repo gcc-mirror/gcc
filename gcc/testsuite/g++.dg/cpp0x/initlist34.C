9d2b7551cded29cd7eaa87ed8ceb5c01f9e022aa
Jason Merrill <jason@redhat.com>
2010-05-17 15:53:55 -0400

    re PR c++/44157 ([C++0x] GCC wrongly takes a std::initializer_list argument as non-deduced context)
    	PR c++/44157
    	* call.c (build_over_call): Limit init-list deduction warning to
    	cases where the argument is actually an init-list.
    
    From-SVN: r159509

---
+++ gcc/testsuite/g++.dg/cpp0x/initlist34.C
@@ -0,0 +1,12 @@
+// PR c++/44157
+// { dg-options "-std=c++0x" }
+
+#include <initializer_list>
+
+template<typename T>
+void f(T) { }
+
+int main() {
+  std::initializer_list<int> a = { 0 };
+  f(a);
+}
