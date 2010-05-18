d3bd87c3760f14f27c84634420b10713acc95653
aesok <aesok@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-05-18 20:54:59 +0000

    * tree.h (build_int_cstu): Implement as static inline. 	* tree.c (build_int_cstu): Remove function. 	(double_int_to_tree, double_int_fits_to_tree_p): Handle size types as 	sign extended.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@159544 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree.h
+++ gcc/tree.h
@@ -4001,9 +4001,16 @@ tree_to_double_int (const_tree cst)
 extern tree double_int_to_tree (tree, double_int);
 extern bool double_int_fits_to_tree_p (const_tree, double_int);
 
+/* Create an INT_CST node with a CST value zero extended.  */
+
+static inline tree
+build_int_cstu (tree type, unsigned HOST_WIDE_INT cst)
+{
+  return double_int_to_tree (type, uhwi_to_double_int (cst));
+}
+
 extern tree build_int_cst (tree, HOST_WIDE_INT);
 extern tree build_int_cst_type (tree, HOST_WIDE_INT);
-extern tree build_int_cstu (tree, unsigned HOST_WIDE_INT);
 extern tree build_int_cst_wide (tree, unsigned HOST_WIDE_INT, HOST_WIDE_INT);
 extern tree build_int_cst_wide_type (tree,
 				     unsigned HOST_WIDE_INT, HOST_WIDE_INT);
