d3577e1b34eed00ae542039ee6633b3be54060e4
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-05-20 14:57:27 +0000

    PR middle-end/44197 	* varpool.c (varpool_remove_node): Handle in-varpool aliases. 	* gcc.c-torture/compile/pr44197.c: New file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@159629 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr44197.c
@@ -0,0 +1,22 @@
+typedef unsigned short int __uint16_t;
+enum
+{
+  _ISupper = (1 << (0)), _ISlower = (1 << (1)), _ISalpha =
+    (1 << (2)), _ISdigit = (1 << (3)), _ISxdigit = (1 << (4)), _ISspace =
+    (1 << (5)), _ISprint = (1 << (6)), _ISgraph = (1 << (7)), _ISblank =
+    (1 << (8)), _IScntrl = (1 << (9)), _ISpunct = (1 << (10)), _ISalnum =
+    (1 << (11))
+};
+typedef __uint16_t __ctype_mask_t;
+extern const __ctype_mask_t *__C_ctype_b;
+extern
+__typeof (__C_ctype_b)
+     __C_ctype_b __asm__ ("" "__GI___C_ctype_b")
+  __attribute__ ((visibility ("hidden")));
+     static const __ctype_mask_t __C_ctype_b_data[] = {
+     };
+
+const __ctype_mask_t *__C_ctype_b = __C_ctype_b_data + 128;
+extern
+__typeof (__C_ctype_b)
+     __EI___C_ctype_b __attribute__ ((alias ("" "__GI___C_ctype_b")));
