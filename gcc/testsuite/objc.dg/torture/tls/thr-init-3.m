8524ba70c36f0f18208947651092dc880cf7816f
Iain Sandoe <iains@gcc.gnu.org>
2010-05-25 09:10:52 +0000

    re PR objc/44140 (ObjC lto/whopr fails)
    	PR ObjC/44140
    	* objc.dg/torture/tls/thr-init-3.m: XFAIL lto/whopr for all.
    
    From-SVN: r159812

--- gcc/testsuite/objc.dg/torture/tls/thr-init-3.m
+++ gcc/testsuite/objc.dg/torture/tls/thr-init-3.m
@@ -1,5 +1,6 @@
 /* { dg-require-effective-target tls } */
 /* { dg-do run } */
+/* { dg-xfail-if "PR44140" { *-*-* } { "-flto" "-fwhopr" } { "" } } */
 /* { dg-xfail-run-if "Needs OBJC2 ABI" { *-*-darwin* && { lp64 && { ! objc2 } } } { "-fnext-runtime" } { "" } } */
 /* { dg-additional-sources "../../../objc-obj-c++-shared/Object1.m" } */
 
