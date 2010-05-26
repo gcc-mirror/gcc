a3fe427a0ab62ce823a70d0d069c2e4d36046700
steven <steven@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-05-26 15:23:00 +0000

    * config/spu/spu-c.c: Do not include function.h, rtl.h, expr.h, 	insn-config.h, insn-codes.h, recog.h, and optabs.h.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@159876 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/spu/spu-c.c
+++ gcc/config/spu/spu-c.c
@@ -22,15 +22,8 @@
 #include "tree.h"
 #include "c-tree.h"
 #include "c-pragma.h"
-#include "function.h"
-#include "rtl.h"
-#include "expr.h"
 #include "tm_p.h"
 #include "langhooks.h"
-#include "insn-config.h"
-#include "insn-codes.h"
-#include "recog.h"
-#include "optabs.h"
 
 
 /* Keep the vector keywords handy for fast comparisons.  */
