d0149355b45ed0d7973db36d37d35f34676deaca
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-05-27 20:40:48 +0000

    2010-05-27  Kai Tietz  <kai.tietz@onevision.com>
            PR bootstrap/44299
            * config/i386/winnt.c (IN_GCC_FRONTEND): Undefine.
            * config/i386/winnt-cxx.c (IN_GCC_FRONTEND): Likewise.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@159949 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/winnt-cxx.c
+++ gcc/config/i386/winnt-cxx.c
@@ -17,7 +17,7 @@ for more details.
 You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
-
+#undef IN_GCC_FRONTEND
 #include "config.h"
 #include "system.h"
 #include "coretypes.h"
