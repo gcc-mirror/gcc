accf9ad64aac122eee74d485245901daa276542a
Steven Bosscher <steven@gcc.gnu.org>
2010-05-28 23:47:05 +0000

    c-lex.c: Do not include c-tree.h.
    	* c-lex.c: Do not include c-tree.h.
    	* c-pretty-print.c: Likewise.
    	* c-opts.c: Likewise.
    	* c-gimplify.c: Likewise.
    	* c-common.c: Likewise.
    	* c-dump.c: Likewise.  Include c-common.h.
    
    From-SVN: r160008

--- gcc/c-lex.c
+++ gcc/c-lex.c
@@ -27,7 +27,6 @@ along with GCC; see the file COPYING3.  If not see
 #include "tree.h"
 #include "input.h"
 #include "output.h"
-#include "c-tree.h"
 #include "c-common.h"
 #include "flags.h"
 #include "timevar.h"
