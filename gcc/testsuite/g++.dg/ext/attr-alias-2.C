2afe938900925d5bb7ed607076b924c88d707dde
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-06-08 09:59:10 +0000

    cp/ 	* decl.c (record_key_method_defined): New, broken out of ... 	(finish_function): ... here.  Call it. 	(start_decl): Treat aliases as definitions.
    	testsuite/
    	* g++.dg/ext/attr-alias-1.C: New.
    	* g++.dg/ext/attr-alias-2.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@160431 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/ext/attr-alias-2.C
@@ -0,0 +1,37 @@
+/* { dg-do run }  */
+/* { dg-require-alias "" } */
+
+#include <typeinfo>
+
+struct Klass
+{
+  int implementation () const;
+  virtual int magic () const;
+};
+
+int Klass::implementation (void) const
+{
+  return 0;
+}
+
+int Klass::magic () const
+  __attribute__ ((alias ("_ZNK5Klass14implementationEv")));
+
+int __attribute__ ((noinline))
+  Foo (Klass const *ptr)
+{
+  if (ptr->magic () != 0)
+    return 1;
+
+  if (typeid (*ptr) != typeid (Klass))
+    return 2;
+
+  return 0;
+}
+
+int main ()
+{
+  Klass obj;
+  
+  return Foo (&obj);
+}
