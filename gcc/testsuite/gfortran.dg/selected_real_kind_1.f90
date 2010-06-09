70987f6299983c5a764c37827a95f71f831087f0
Daniel Franke <franke.daniel@gmail.com>
2010-06-09 17:36:33 -0400

    re PR fortran/44347 (SELECT_REAL_KIND: Wrongly accepts non-scalar arguments)
    gcc/fortran/:
    2010-06-09  Daniel Franke  <franke.daniel@gmail.com>
    
            PR fortran/44347
            * check.c (gfc_check_selected_real_kind): Verify that the
            actual arguments are scalar.
    
    gcc/testsuite/:
    2010-06-09  Daniel Franke  <franke.daniel@gmail.com>
    
            PR fortran/44347
            * gfortran.dg/selected_real_kind_1.f90: New.
    
    From-SVN: r160506

---
+++ gcc/testsuite/gfortran.dg/selected_real_kind_1.f90
@@ -0,0 +1,10 @@
+! { dg-do "compile" }
+!
+! PR fortran/44347 - arguments of SELECTED_REAL_KIND shall be scalar
+! Testcase contributed by Vittorio Zecca <zeccav AT gmail DOT com>
+!
+
+  dimension ip(1), ir(1)
+  i = selected_real_kind(ip, i)      ! { dg-error "must be a scalar" }
+  j = selected_real_kind(i, ir)      ! { dg-error "must be a scalar" }
+end
