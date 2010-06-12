fcfbd84e9415b04dfe1e8ea39bf85549c6f7ec57
Joseph Myers <joseph@codesourcery.com>
2010-06-12 01:13:23 +0100

    common.opt (Wstrict-aliasing=, [...]): Add RejectNegative.
    	* common.opt (Wstrict-aliasing=, Wstrict-overflow=, fabi-version=,
    	flto-compression-level=, ftree-parallelize-loops=): Add
    	RejectNegative.
    
    testsuite:
    	* gcc.dg/opts-1.c: New test.
    
    From-SVN: r160640

---
+++ gcc/testsuite/gcc.dg/opts-1.c
@@ -0,0 +1,9 @@
+/* Test negative forms of various options are rejected.  */
+/* { dg-do compile } */
+/* { dg-options "-Wno-strict-aliasing=1 -Wno-strict-overflow=1 -fno-abi-version=1 -fno-lto-compression-level=1 -fno-tree-parallelize-loops=1" } */
+
+/* { dg-error "-fno-abi-version" "-fno-abi-version" { target *-*-* } 0 } */
+/* { dg-error "-fno-lto-compression-level" "-fno-lto-compression-level" { target *-*-* } 0 } */
+/* { dg-error "-fno-tree-parallelize-loops" "-fno-tree-parallelize-loops" { target *-*-* } 0 } */
+/* { dg-warning "-Wno-strict-overflow" "-Wno-strict-overflow" { target *-*-* } 0 } */
+/* { dg-warning "-Wno-strict-aliasing" "-Wno-strict-aliasing" { target *-*-* } 0 } */
