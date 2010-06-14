8ab7d796d82889397ac6b74477be99fc5ee76831
Jakub Jelinek <jakub@redhat.com>
2010-06-14 17:19:04 +0200

    re PR tree-optimization/44508 (libgomp.fortran/vla1.f90 ICE on s390)
    	PR tree-optimization/44508
    	* tree-ssa-propagate.h (substitute_and_fold): Add DO_DCE
    	argument.
    	* tree-ssa-propagate.c (substitute_and_fold): If !DO_DCE,
    	don't eliminate trivially dead stmts.
    	* tree-vrp.c (vrp_finalize): Pass false as last argument
    	to substitute_and_fold.
    	* tree-ssa-copy.c (fini_copy_prop): Pass true as last argument
    	to substitute_and_fold.
    	* tree-ssa-ccp.c (ccp_finalize): Likewise.
    
    	* gcc.dg/tree-ssa/pr21086.c: Adjust.
    
    From-SVN: r160749

--- gcc/testsuite/gcc.dg/tree-ssa/pr21086.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr21086.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -fdump-tree-vrp1" } */
+/* { dg-options "-O2 -fdump-tree-vrp1 -fdump-tree-dce1" } */
 
 int
 foo (int *p)
@@ -16,8 +16,9 @@ foo (int *p)
 }
 
 /* Target with fno-delete-null-pointer-checks should not fold checks */
-/* { dg-final { scan-tree-dump-times "Folding predicate " 1 "vrp1" { target { ! keeps_null_pointer_checks } } } } */
+/* { dg-final { scan-tree-dump "Folding predicate " "vrp1" { target { ! keeps_null_pointer_checks } } } } */
 /* { dg-final { scan-tree-dump-times "Folding predicate " 0 "vrp1" { target {   keeps_null_pointer_checks } } } } */
-/* { dg-final { scan-tree-dump-not "b_. =" "vrp1" { target { ! avr-*-* } } } } */
-/* { dg-final { scan-tree-dump "b_. =" "vrp1" { target { avr-*-* } } } } */
+/* { dg-final { scan-tree-dump-not "b_. =" "dce1" { target { ! avr-*-* } } } } */
+/* { dg-final { scan-tree-dump "b_. =" "dce1" { target { avr-*-* } } } } */
 /* { dg-final { cleanup-tree-dump "vrp1" } } */
+/* { dg-final { cleanup-tree-dump "dce1" } } */
