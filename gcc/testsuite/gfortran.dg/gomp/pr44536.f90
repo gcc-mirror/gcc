79943d19dc27298a384fce25c604fe6191e0765f
Jakub Jelinek <jakub@redhat.com>
2010-06-15 14:15:26 +0200

    re PR fortran/44536 (OMP: missing error with default(none))
    	PR fortran/44536
    	* langhooks.h (struct lang_hooks_for_decls): Add omp_report_decl.
    	* langhooks-def.h (LANG_HOOKS_OMP_REPORT_DECL): Define.
    	(LANG_HOOKS_DECLS): Add it.
    	* gimplify.c (omp_notice_variable): Call
    	lang_hooks.decls.omp_report_decl.
    
    	* trans-openmp.c (gfc_omp_predetermined_sharing): Don't return
    	OMP_CLAUSE_DEFAULT_SHARED for artificial vars with
    	GFC_DECL_SAVED_DESCRIPTOR set.
    	(gfc_omp_report_decl): New function.
    	* trans.h (gfc_omp_report_decl): New prototype.
    	* f95-lang.c (LANG_HOOKS_OMP_REPORT_DECL): Redefine.
    
    	* gfortran.dg/gomp/pr44536.f90: New test.
    	* gfortran.dg/gomp/sharing-3.f90: Remove xfail.
    
    From-SVN: r160779

---
+++ gcc/testsuite/gfortran.dg/gomp/pr44536.f90
@@ -0,0 +1,10 @@
+! PR fortran/44536
+! { dg-do compile }
+! { dg-options "-fopenmp" }
+      subroutine foo (a, i, j)
+        integer, dimension(:) :: a
+        integer :: i, j
+!$omp parallel default(none) shared(i, j)	! { dg-error "enclosing parallel" }
+        j=a(i)					! { dg-error "not specified in" }
+!$omp end parallel
+      end subroutine
