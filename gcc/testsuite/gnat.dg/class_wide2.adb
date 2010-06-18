12f7dd1733fae0e1f7ae64937f7a3e43c12369b1
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-06-18 09:28:22 +0000

    * gnat.dg/class_wide2.adb: Remove bogus message, no longer generated.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@160968 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gnat.dg/class_wide2.adb
+++ gcc/testsuite/gnat.dg/class_wide2.adb
@@ -4,7 +4,7 @@ package body Class_Wide2 is
 
    procedure Initialize is
       Var_Acc : Class_Acc := new Grand_Child;
-      Var     : Grand_Child'Class := Grand_Child'Class (Var_Acc.all);  -- { dg-bogus "already constrained" "" { xfail *-*-* } }
+      Var     : Grand_Child'Class := Grand_Child'Class (Var_Acc.all);
 
    begin
       Var := Grand_Child'Class (Var_Acc.all);
