eeed09ef663f6c6f0c4f41f64d6029e6474263b0
Vincent Celier <celier@adacore.com>
2010-06-23 06:17:00 +0000

    a-reatim.adb: Call System.OS_Primitives.Initialize during elaboration
    2010-06-23  Vincent Celier  <celier@adacore.com>
    
    	* a-reatim.adb: Call System.OS_Primitives.Initialize during elaboration
    
    From-SVN: r161245

--- gcc/ada/a-reatim.adb
+++ gcc/ada/a-reatim.adb
@@ -7,7 +7,7 @@
 --                                 B o d y                                  --
 --                                                                          --
 --             Copyright (C) 1991-1994, Florida State University            --
---                     Copyright (C) 1995-2009, AdaCore                     --
+--                     Copyright (C) 1995-2010, AdaCore                     --
 --                                                                          --
 -- GNARL is free software; you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -31,6 +31,8 @@
 -- Extensive contributions were provided by Ada Core Technologies, Inc.     --
 --                                                                          --
 ------------------------------------------------------------------------------
+
+with System.OS_Primitives;
 
 package body Ada.Real_Time is
 
@@ -242,4 +244,6 @@ package body Ada.Real_Time is
       return Time_Span (D);
    end To_Time_Span;
 
+begin
+   System.OS_Primitives.Initialize;
 end Ada.Real_Time;
