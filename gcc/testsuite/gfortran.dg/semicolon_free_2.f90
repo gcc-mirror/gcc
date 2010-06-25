1b37751e20018b7c3fc58446fc80066f8698088a
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-06-25 19:01:04 +0000

    2010-06-25  Tobias Burnus  <burnus@net-b.de>
            * parse.c (next_free, next_fixed): Allow ";" as first character.
    
    2010-06-25  Tobias Burnus  <burnus@net-b.de>
    
            * gfortran.dg/semicolon_fixed.f: Update.
            * gfortran.dg/semicolon_fixed_2.f: New.
            * gfortran.dg/semicolon_free_2.f90: New.
            * gfortran.dg/semicolon_free.f90: Update.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@161405 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/semicolon_free_2.f90
+++ gcc/testsuite/gfortran.dg/semicolon_free_2.f90
@@ -1,10 +1,10 @@
 ! { dg-do compile }
-! { dg-options "-std=f2003" }
+! { dg-options "-std=f2008" }
 ! PR 19259 Semicolon cannot start a line
 x=1; y=1;
 x=2;;
 x=3;
- ; ! { dg-error "Semicolon at" }
-;; ! { dg-error "Semicolon at" }
+ ; ! OK
+;; ! OK
 111 ; ! { dg-error "Semicolon at" }
 end
