8b3057b30d78d0e30ec9f0db8806229bd8f28dfc
Jan Hubicka <jh@suse.cz>
2010-06-26 16:45:40 +0200

    ipa-split-2.c: New testcase.
    	* gcc.dg/tree-ssa/ipa-split-2.c: New testcase.
    	* ipa-split.c (consider_split): PHI in entry block is OK as long as all
    	edges comming from header are equivalent.
    	(visit_bb): Handle PHIs correctly.
    	* tree-inline.c (copy_phis_for_bb): Be able to copy
    	PHI from entry edge.
    	(copy_cfg_body): Produce edge from entry BB before copying
    	PHIs.
    
    From-SVN: r161433

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ipa-split-2.c
@@ -0,0 +1,41 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 -fdump-tree-fnsplit" } */
+int b;
+int c;
+int d;
+split_me(int a)
+{
+  int t = 0;
+  if (d>4)
+    return;
+  do
+   {
+  long_function (t);
+  long_function (t);
+  long_function (t);
+  long_function (t);
+  long_function (t);
+  long_function (t);
+  make_me_irregular:
+  long_function (t);
+  long_function (t);
+  long_function (t);
+  long_function (t);
+  long_function (t);
+   t=b;
+   }
+  while (t);
+  if (c)
+    goto make_me_irregular;
+}
+
+main()
+{
+  split_me (1);
+  split_me (2);
+  split_me (3);
+  split_me (4);
+  split_me (5);
+}
+/* { dg-final { scan-tree-dump-times "Splitting function" 1 "fnsplit"} } */
+/* { dg-final { cleanup-tree-dump "fnsplit" } } */
