6a9f3fe9c5fc1ae2c12dfac96ec6eb6f792dd3bd
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-06-29 19:37:46 +0000

    PR debug/44668 	* dwarf2out.c (add_accessibility_attribute): New function. 	(gen_subprogram_die, gen_variable_die, gen_field_die): Use it 	instead of adding DW_AT_accessibility manually. 	(gen_enumeration_type_die, gen_struct_or_union_type_die, 	gen_typedef_die): Use it.
    	* g++.dg/debug/dwarf2/accessibility1.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@161563 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/debug/dwarf2/accessibility1.C
@@ -0,0 +1,24 @@
+// PR debug/44668
+// { dg-do compile }
+// { dg-options "-g -dA" }
+
+struct C
+{
+private:
+  typedef int a;
+  a b;
+  enum g { g1, g2 } h;
+  struct D { int i; } i;
+protected:
+  typedef int c;
+  c d;
+public:
+  typedef int e;
+  e f;
+} c;
+
+// 3 private DW_TAG_member dies, 1 private DW_TAG_typedef,
+// 1 private DW_TAG_enumeration_type and 1 private DW_TAG_structure_type
+// { dg-final { scan-assembler-times "3\[^\\r\\n\]* DW_AT_accessibility" 6 } }
+// 1 private DW_TAG_member die, 1 private DW_TAG_typedef
+// { dg-final { scan-assembler-times "2\[^\\r\\n\]* DW_AT_accessibility" 2 } }
