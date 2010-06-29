ec878c4f27bd60c90783157b65c3632644c42839
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-06-29 14:02:23 +0000

    2010-06-29  Richard Guenther  <rguenther@suse.de>
    	* tree-dfa.c (dump_variable): Remove noalias_state dumping.
    	* tree-flow.h (enum noalias_state): Remove.
    	(struct var_ann_d): Remove noalias_state member.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@161535 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-flow.h
+++ gcc/tree-flow.h
@@ -147,29 +147,6 @@ enum need_phi_state {
 };
 
 
-/* The "no alias" attribute allows alias analysis to make more
-   aggressive assumptions when assigning alias sets, computing
-   points-to information and memory partitions.  These attributes
-   are the result of user annotations or flags (e.g.,
-   -fargument-noalias).  */
-enum noalias_state {
-    /* Default state.  No special assumptions can be made about this
-       symbol.  */
-    MAY_ALIAS = 0,
-
-    /* The symbol does not alias with other symbols that have a
-       NO_ALIAS* attribute.  */
-    NO_ALIAS,
-
-    /* The symbol does not alias with other symbols that have a
-       NO_ALIAS*, and it may not alias with global symbols.  */
-    NO_ALIAS_GLOBAL,
-
-    /* The symbol does not alias with any other symbols.  */
-    NO_ALIAS_ANYTHING
-};
-
-
 struct GTY(()) var_ann_d {
   /* Used when building base variable structures in a var_map.  */
   unsigned base_var_processed : 1;
@@ -186,11 +163,6 @@ struct GTY(()) var_ann_d {
   /* True for HEAP artificial variables.  These variables represent
      the memory area allocated by a call to malloc.  */
   unsigned is_heapvar : 1;
-
-  /* This field describes several "no alias" attributes that some
-     symbols are known to have.  See the enum's definition for more
-     information on each attribute.  */
-  ENUM_BITFIELD (noalias_state) noalias_state : 2;
 
   /* Used by var_map for the base index of ssa base variables.  */
   unsigned base_index;
