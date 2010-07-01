00ecc71bb2ddf0923f49fce52206ad467df19930
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-07-01 14:37:22 +0000

    * g++.dg/cpp0x/nullptr04.C: Use __INTPTR_TYPE__. 	* g++.dg/other/pr25632.C: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@161673 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp0x/nullptr04.C
+++ gcc/testsuite/g++.dg/cpp0x/nullptr04.C
@@ -3,17 +3,15 @@
 
 // Test cast to int
 
-#define unsigned
-__extension__ typedef __SIZE_TYPE__ ssize_t;
-#undef unsigned
+__extension__ typedef __INTPTR_TYPE__ intptr_t;
 
 const int n4 = static_cast<const int>(nullptr); // { dg-error "invalid static_cast " }
 const short int n5 = reinterpret_cast<short int>(nullptr); // { dg-error "loses precision" }
-const ssize_t n6 = reinterpret_cast<ssize_t>(nullptr);
-const ssize_t n7 = (ssize_t)nullptr;
+const intptr_t n6 = reinterpret_cast<intptr_t>(nullptr);
+const intptr_t n7 = (intptr_t)nullptr;
 
 decltype(nullptr) mynull = 0;
 const int n8 = static_cast<const int>(mynull); // { dg-error "invalid static_cast " }
 const short int n9 = reinterpret_cast<short int>(mynull); // { dg-error "loses precision" }
-const ssize_t n10 = reinterpret_cast<ssize_t>(mynull);
-const ssize_t n11 = (ssize_t)mynull;
+const intptr_t n10 = reinterpret_cast<intptr_t>(mynull);
+const intptr_t n11 = (intptr_t)mynull;
