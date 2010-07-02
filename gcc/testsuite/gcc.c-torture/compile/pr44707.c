1a4fc49d7fccd1048eec7c73153e5de4da0a5885
Ulrich Weigand <uweigand@de.ibm.com>
2010-07-02 11:48:30 +0000

    re PR target/44707 (operand requires impossible reload)
    ChangeLog:
    
    	PR target/44707
    	* config/rs6000/rs6000.c (rs6000_legitimize_reload_address): Recognize
    	(lo_sum (high ...) ...) patterns generated by earlier passes.
    
    testsuite/ChangeLog:
    
    	PR target/44707
    	* gcc.c-torture/compile/pr44707.c: New test.
    
    From-SVN: r161703

---
+++ gcc/testsuite/gcc.c-torture/compile/pr44707.c
@@ -0,0 +1,14 @@
+extern struct { int a, b, c, d; } v;
+extern int w;
+
+void
+foo (void)
+{
+  int e1 = v.a;
+  int e2 = w;
+  int e3 = v.b;
+  int e4 = v.c;
+  int e5 = v.d;
+  __asm__ volatile ("/* %0 %1 %2 %3 %4 */" : : "nro" (e1), "nro" (e2), "nro" (e3), "nro" (e4), "nro" (e5));
+}
+
