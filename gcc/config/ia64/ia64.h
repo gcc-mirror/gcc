f501ee2341402fbc44d163673e2403637f1e4a69
aesok <aesok@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-07-05 17:49:56 +0000

    * config/ia64/ia64.h (MEMORY_MOVE_COST): Remove macro. 	* config/ia64/t-ia64 (ia64.o): Depend on reload.h. 	* config/ia64/ia64.c Include reload.h. 	(ia64_memory_move_cost): New function. 	(TARGET_MEMORY_MOVE_COST): Define. 	(ia64_register_move_cost): Replace MEMORY_MOVE_COST with 	memory_move_cost.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@161845 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/ia64/ia64.h
+++ gcc/config/ia64/ia64.h
@@ -1,6 +1,6 @@
 /* Definitions of target machine GNU compiler.  IA-64 version.
    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
-   2009 Free Software Foundation, Inc.
+   2009, 2010 Free Software Foundation, Inc.
    Contributed by James E. Wilson <wilson@cygnus.com> and
    		  David Mosberger <davidm@hpl.hp.com>.
 
@@ -1303,12 +1303,6 @@ do {									\
 
 
 /* Describing Relative Costs of Operations */
-
-/* A C expression for the cost of moving data of mode M between a
-   register and memory.  */
-#define MEMORY_MOVE_COST(MODE,CLASS,IN) \
-  ((CLASS) == GENERAL_REGS || (CLASS) == FR_REGS || (CLASS) == FP_REGS \
-   || (CLASS) == GR_AND_FR_REGS ? 4 : 10)
 
 /* A C expression for the cost of a branch instruction.  A value of 1 is the
    default; other values are interpreted relative to that.  Used by the
