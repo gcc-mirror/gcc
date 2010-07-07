1d80f4016018789384b570fd4acf077f58c4abdc
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-07-07 13:33:04 +0000

    Retry rdrand if the carry flag isn't valid.
    gcc/
    
    2010-07-07  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/44844
    	* config/i386/i386.md (rdrand<mode>): Changed to expand to
    	retry if the carry flag isn't valid.
    	(rdrand<mode>_1): New.
    
    gcc/testsuite/
    
    2010-07-07  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/44844
    	* gcc.target/i386/rdrand-1.c: Scan "jnc".
    	* gcc.target/i386/rdrand-2.c: Likewise.
    	* gcc.target/i386/rdrand-3.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@161910 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/rdrand-3.c
+++ gcc/testsuite/gcc.target/i386/rdrand-3.c
@@ -2,6 +2,7 @@
 /* { dg-require-effective-target lp64 } */
 /* { dg-options "-O2 -mrdrnd " } */
 /* { dg-final { scan-assembler "rdrand\[ \t]+(%|)rax" } } */
+/* { dg-final { scan-assembler "jnc\[ \t]+" } } */
 
 #include <immintrin.h>
 
