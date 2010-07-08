5951297a1afe3039a9716f2cb6ee924a7a388f37
Eric Botcazou <ebotcazou@gcc.gnu.org>
2010-07-08 20:02:29 +0000

    re PR middle-end/44843 (All 32-bit fortran execution tests generate unaligned access)
    	PR middle-end/44843
    	* emit-rtl.c (set_mem_attributes_minus_bitpos): Do not rely on the
    	pointed-to type of the offset in a MEM_REF to compute the alignment.
    
    From-SVN: r161974

---
+++ gcc/testsuite/gcc.c-torture/execute/20100708-1.c
@@ -0,0 +1,29 @@
+/* PR middle-end/44843 */
+/* Verify that we don't use the alignment of struct S for inner accesses.  */
+
+struct S
+{
+  double for_alignment;
+  struct { int x, y, z; } a[16];
+};
+
+void f(struct S *s) __attribute__((noinline));
+
+void f(struct S *s)
+{
+  unsigned int i;
+
+  for (i = 0; i < 16; ++i)
+    {
+      s->a[i].x = 0;
+      s->a[i].y = 0;
+      s->a[i].z = 0;
+    }
+}
+
+int main (void)
+{
+  struct S s;
+  f (&s);
+  return 0;
+}
