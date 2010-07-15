f9716d2c5fe118e73f835f2b7af81377439283b0
davek <davek@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-07-15 01:45:51 +0000

    * config/i386/cygwin.h (LIBGCJ_SONAME): Update. 	* config/i386/mingw32.h (LIBGCJ_SONAME): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@162200 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/cygwin.h
+++ gcc/config/i386/cygwin.h
@@ -301,5 +301,5 @@ while (0)
 #define LIBGCC_SONAME "cyggcc_s" LIBGCC_EH_EXTN "-1.dll"
 
 /* We should find a way to not have to update this manually.  */
-#define LIBGCJ_SONAME "cyggcj" /*LIBGCC_EH_EXTN*/ "-11.dll"
+#define LIBGCJ_SONAME "cyggcj" /*LIBGCC_EH_EXTN*/ "-12.dll"
 
