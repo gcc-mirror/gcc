0603fe683c89ccf3c1663817c44f0d320a57ba5e
Jeffrey Yasskin <jyasskin@google.com>
2010-07-20 20:16:53 +0000

    re PR c++/44641 (Generated constructors and destructors get wrong debug location when a typedef uses a forward declaration of the type before the definition)
    PR c++/44641
            * gcc/cp/pt.c (instantiate_class_template): Propagate the
            template's location to its instance.
    
           	* gcc/testsuite/lib/scanasm.exp (dg-function-on-line): Test
            that a function is defined on the current line.
            * gcc/testsuite/g++.dg/debug/dwarf2/lineno-simple1.C: New.
            Line number sanity test.
            * gcc/testsuite/g++.dg/debug/dwarf2/pr44641.C: New.
    
    From-SVN: r162349

---
+++ gcc/testsuite/g++.dg/debug/dwarf2/pr44641.C
@@ -0,0 +1,41 @@
+// Origin: PR 44641
+// { dg-do compile }
+// { dg-options "-g -O0 -dA" }
+
+template <class A> struct MisplacedDbg;
+template<class T> struct MisplacedDbg<T*>;
+struct Full;
+template<> struct MisplacedDbg<Full>;
+
+struct Arg;
+typedef MisplacedDbg<Arg> Typedef1;
+typedef MisplacedDbg<Arg*> Typedef2;
+typedef MisplacedDbg<Full> Typedef3;
+
+template<typename T> struct Base  {
+  virtual ~Base() {
+  }
+};
+
+template <>
+struct MisplacedDbg<Full>  // { dg-function-on-line {_ZN12MisplacedDbgI4FullEC[12]Ev} }
+                           // { dg-function-on-line {_ZN12MisplacedDbgI4FullED0Ev} { target *-*-* } 21 }
+
+    : public Base<int> {
+};
+
+template <class T>
+struct MisplacedDbg<T*>  // { dg-function-on-line {_ZN12MisplacedDbgIP3ArgEC[12]Ev} }
+                         // { dg-function-on-line {_ZN12MisplacedDbgIP3ArgED0Ev} { target *-*-* } 28 }
+    : public Base<int> {
+};
+
+template <class A>
+struct MisplacedDbg  // { dg-function-on-line {_ZN12MisplacedDbgI3ArgEC[12]Ev} }
+                     // { dg-function-on-line {_ZN12MisplacedDbgI3ArgED0Ev} { target *-*-* } 34 }
+    : public Base<int> {
+};
+
+static MisplacedDbg<Arg> static_var1;
+static MisplacedDbg<Arg*> static_var2;
+static MisplacedDbg<Full> static_var3;
