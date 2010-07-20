f5d6836a24cd516bab504b829f30565f76da2fa5
Richard Guenther <rguenther@suse.de>
2010-07-20 12:05:59 +0000

    lto-symtab.c (lto_symtab_merge): Use gimple_types_compatible_p.
    2010-07-20  Richard Guenther  <rguenther@suse.de>
    
    	* lto-symtab.c (lto_symtab_merge): Use gimple_types_compatible_p.
    	(lto_symtab_merge_decls_2): Likewise.
    	* gimple.h (gimple_types_compatible_p): Declare.
    	* gimple.c (gimple_queue_type_fixup): Remove.
    	(gimple_fixup_complete_and_incomplete_subtype_p): Likewise.
    	(gimple_compatible_complete_and_incomplete_type_p): New
    	function.
    	(gimple_types_compatible_p): Adjust.
    	(gimple_register_type): Remove type fixup code.
    	(print_gimple_types_stats): Adjust.
    	(free_gimple_type_tables): Likewise.
    	* lto-streamer-in.c (input_gimple_stmt): Use gimple_types_compatible_p.
    	* tree-ssa.c (useless_type_conversion_p): Likewise.
    
    From-SVN: r162330

--- gcc/tree-ssa.c
+++ gcc/tree-ssa.c
@@ -1426,7 +1426,7 @@ useless_type_conversion_p (tree outer_type, tree inner_type)
      compared types.  */
   else if (AGGREGATE_TYPE_P (inner_type)
 	   && TREE_CODE (inner_type) == TREE_CODE (outer_type))
-    return false;
+    return gimple_types_compatible_p (outer_type, inner_type, false);
 
   return false;
 }
