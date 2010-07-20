898a19d2d4b78ba984aea7a20eed10633f38cb13
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-07-20 17:34:18 +0000

    PR c++/44967 	* pt.c (tsubst_copy_and_build): Handle partial substitution of 	CALL_EXPR.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@162343 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -12500,6 +12500,15 @@ tsubst_copy_and_build (tree t,
 	      }
 	  }
 
+	if (processing_template_decl
+	    && (type_dependent_expression_p (function)
+		|| any_type_dependent_arguments_p (call_args)))
+	  {
+	    ret = build_nt_call_vec (function, call_args);
+	    KOENIG_LOOKUP_P (ret) = koenig_p;
+	    goto call_out;
+	  }
+
 	/* We do not perform argument-dependent lookup if normal
 	   lookup finds a non-function, in accordance with the
 	   expected resolution of DR 218.  */
@@ -12512,15 +12521,14 @@ tsubst_copy_and_build (tree t,
 		|| TREE_CODE (function) == IDENTIFIER_NODE)
 	    /* Only do this when substitution turns a dependent call
 	       into a non-dependent call.  */
-	    && type_dependent_expression_p_push (t)
-	    && !any_type_dependent_arguments_p (call_args))
+	    && type_dependent_expression_p_push (t))
 	  function = perform_koenig_lookup (function, call_args);
 
 	if (TREE_CODE (function) == IDENTIFIER_NODE)
 	  {
 	    unqualified_name_lookup_error (function);
-	    release_tree_vector (call_args);
-	    return error_mark_node;
+	    ret = error_mark_node;
+	    goto call_out;
 	  }
 
 	/* Remember that there was a reference to this entity.  */
@@ -12551,6 +12559,7 @@ tsubst_copy_and_build (tree t,
 				  koenig_p,
 				  complain);
 
+      call_out:
 	release_tree_vector (call_args);
 
 	return ret;
