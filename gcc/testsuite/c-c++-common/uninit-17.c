426797b226b4934a571b8e0e0e7bd9f495fd45f9
Bernd Schmidt <bernds@codesourcery.com>
2010-07-21 12:36:44 +0000

    re PR middle-end/44738 (c-c++-common/uninit-17.c failed)
    	PR middle-end/44738
    	* tree-ssa.c (warn_uninit): Avoid emitting an unnecessary message.
    
    	PR middle-end/44738
    	* c-c++-common/uninit-17.c: Correct expected error.
    
    From-SVN: r162372

--- gcc/testsuite/c-c++-common/uninit-17.c
+++ gcc/testsuite/c-c++-common/uninit-17.c
@@ -9,9 +9,9 @@ static void bar(int a, int *ptr)
 {
   do
   {
-    int b; /* { dg-message "note: 'b' was declared here" } */
+    int b; /* { dg-warning "is used uninitialized" } */
     if (b < 40) {
-      ptr[0] = b; /* { dg-warning "may be used uninitialized" } */
+      ptr[0] = b;
     }
     b += 1;
     ptr++;
