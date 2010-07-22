87d822bba9594a3092ffda523ebd41a2080afa59
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-07-22 12:14:27 +0000

    2010-07-22  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/45017
    	* tree-ssa-sccvn.c (vn_reference_eq): Make sure we honor
    	TYPE_PRECISION of integral types in addition to size.
    
    	* gcc.c-torture/execute/pr45017.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@162411 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/pr45017.c
@@ -0,0 +1,22 @@
+int tester(char *bytes)
+{
+  union {
+      struct {
+	  unsigned int r1:4;
+	  unsigned int r2:4;
+      } fmt;
+      char value[1];
+  } ovl;
+
+  ovl.value[0] = bytes[0];
+  return ovl.fmt.r1;
+}
+extern void abort (void);
+int main()
+{
+  char buff = 0x2f;
+  if (tester(&buff) != 0x0f)
+    abort ();
+  return 0;
+}
+
