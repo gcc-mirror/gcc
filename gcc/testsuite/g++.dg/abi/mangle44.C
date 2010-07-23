96f3a5d239c71dbb50ffd5c0f731fc3b5a9d0c44
Jason Merrill <jason@redhat.com>
2010-07-23 09:54:01 -0400

    re PR c++/45008 (Template code not expanded properly)
    	PR c++/45008
    	* g++.dg/abi/mangle44.C: New.
    
    From-SVN: r162459

---
+++ gcc/testsuite/g++.dg/abi/mangle44.C
@@ -0,0 +1,19 @@
+// PR c++/45008
+
+template <typename T>
+struct A
+{
+  void fn1 () {
+   struct Nested {
+    static void fn2 () { }
+   };
+   Nested::fn2();
+  }
+};
+
+void fn3 () {
+  A<double> a;
+  a.fn1();
+}
+
+// { dg-final { scan-assembler-not "_ZZN1AIT_E3fn1EvEN6Nested3fn2Ev" } }
