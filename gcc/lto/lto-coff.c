fbbc5704999e6899a03fe7c30457feed07a5d271
Andi Kleen <ak@gcc.gnu.org>
2010-07-23 05:25:46 +0000

    lto-coff.c (hash_name, eq_name): Move.
    2010-07-10  Andi Kleen <ak@linux.intel.com>
    
    	* lto-coff.c (hash_name, eq_name): Move.
    	(lto_obj_build_section_table): Call lto_obj_create_section_hash_table.
    	* lto-elf.c: (hash_name, eq_name): Move.
    	(lto_obj_build_section_table): Call lto_obj_create_section_hash_table.
    	* lto-macho.c: (hash_name, eq_name): Move.
    	(lto_obj_build_section_table): Call lto_obj_create_section_hash_table.
    	* lto.c: (hash_name, eq_name): Move from lto-*.c
    	(lto_obj_create_section_hash_table): Add.
    	(free_with_string): Add.
    
    From-SVN: r162441

--- gcc/lto/lto-coff.c
+++ gcc/lto/lto-coff.c
@@ -134,29 +134,6 @@ lto_file_init (lto_file *file, const char *filename, off_t offset)
   file->offset = offset;
 }
 
-/* Returns a hash code for P.  */
-
-static hashval_t
-hash_name (const void *p)
-{
-  const struct lto_section_slot *ds = (const struct lto_section_slot *) p;
-  return (hashval_t) htab_hash_string (ds->name);
-}
-
-/* Returns nonzero if P1 and P2 are equal.  */
-
-static int
-eq_name (const void *p1, const void *p2)
-{
-  const struct lto_section_slot *s1 =
-    (const struct lto_section_slot *) p1;
-  const struct lto_section_slot *s2 =
-    (const struct lto_section_slot *) p2;
-
-  return strcmp (s1->name, s2->name) == 0;
-}
-
-
 /* Build a hash table whose key is the section names and whose data is
    the start and size of each section in the .o file.  */
 
@@ -169,7 +146,7 @@ lto_obj_build_section_table (lto_file *lto_file)
   ssize_t strtab_size;
   char *strtab;
 
-  section_hash_table = htab_create (37, hash_name, eq_name, free);
+  section_hash_table = lto_obj_create_section_hash_table ();
 
   /* Seek to start of string table.  */
   if (coff_file->strtab_offs != lseek (coff_file->fd,
