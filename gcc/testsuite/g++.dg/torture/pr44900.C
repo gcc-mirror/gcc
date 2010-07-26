114e33e1e6658de6e5a65e5d4bdc8623acb11527
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2010-07-26 16:23:01 +0000

    pr44900.C: Use dg-require-effective-target sse_runtime.
    	* g++.dg/torture/pr44900.C: Use dg-require-effective-target
    	sse_runtime.
    	* gcc.dg/attr-weak-hidden-1a.c: Use dg-require-weak,
    	dg-require-visibility.
    
    From-SVN: r162541

--- gcc/testsuite/g++.dg/torture/pr44900.C
+++ gcc/testsuite/g++.dg/torture/pr44900.C
@@ -1,6 +1,6 @@
 /* { dg-do run { target i?86-*-* x86_64-*-* } } */
 /* { dg-options "-msse" } */
-/* { dg-require-effective-target sse } */
+/* { dg-require-effective-target sse_runtime } */
 
 typedef float __m128 __attribute__ ((__vector_size__ (16), __may_alias__));
 typedef float __v4sf __attribute__ ((__vector_size__ (16)));
--- gcc/testsuite/gcc.dg/attr-weak-hidden-1a.c
+++ gcc/testsuite/gcc.dg/attr-weak-hidden-1a.c
@@ -1,3 +1,5 @@
+/* { dg-require-weak "" } */
+/* { dg-require-visibility "" } */
 void abort (void);
 int __attribute__((weak, visibility("hidden"))) foo (void) { return 1; }
 int
