f67a9881a86bcdeb67fa52c0a8eb45ce9d4134d4
Paolo Carlini <paolo@gcc.gnu.org>
2010-07-27 17:27:06 +0000

    [multiple changes]
    2010-07-27  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* include/ext/vstring_util.h: Include bits/range_access.h.
    	* testsuite/ext/vstring/range_access.cc: New test.
    
    2010-07-27  Ed Smith-Rowland  <3dw4rd@verizon.net>
    
    	* include/bits/range_access.h: New.
    	* include/Makefile.in: Add bits/range_access.h.
    	* include/Makefile.am: Regenerate.
    	* include/std/array: Include bits/range_access.h.
    	* include/std/deque: Ditto.
    	* include/std/forward_list: Ditto.
    	* include/std/iterator: Ditto.
    	* include/std/list: Ditto.
    	* include/std/map: Ditto.
    	* include/std/regex: Ditto.
    	* include/std/set: Ditto.
    	* include/std/string: Ditto.
    	* include/std/unordered_map: Ditto.
    	* include/std/unordered_set: Ditto.
    	* include/std/vector: Ditto.
    	* include/std/valarray: Add begin() and end().
    	* libsupc++/initializer_list: Ditto.
    	* include/tr1_impl/utility: Add begin() and end().
    	* include/std/tuple: Ditto.
    	* testsuite/24_iterators/headers/iterator/range_access.cc: New test.
    	* testsuite/24_iterators/range_access.cc: Ditto.
    	* testsuite/28_regex/range_access.cc: Ditto.
    	* testsuite/18_support/initializer_list/range_access.cc: Ditto.
    	* testsuite/21_strings/basic_string/range_access.cc: Ditto.
    	* testsuite/26_numerics/valarray/range_access.cc: Ditto.
    	* testsuite/23_containers/unordered_map/range_access.cc: Ditto.
    	* testsuite/23_containers/multimap/range_access.cc: Ditto.
    	* testsuite/23_containers/set/range_access.cc: Ditto.
    	* testsuite/23_containers/unordered_multimap/range_access.cc: Ditto.
    	* testsuite/23_containers/forward_list/range_access.cc: Ditto.
    	* testsuite/23_containers/unordered_set/range_access.cc: Ditto.
    	* testsuite/23_containers/vector/range_access.cc: Ditto.
    	* testsuite/23_containers/deque/range_access.cc: Ditto.
    	* testsuite/23_containers/multiset/range_access.cc: Ditto.
    	* testsuite/23_containers/list/range_access.cc: Ditto.
    	* testsuite/23_containers/unordered_multiset/range_access.cc: Ditto.
    	* testsuite/23_containers/map/range_access.cc: Ditto.
    	* testsuite/23_containers/array/range_access.cc: Ditto.
    	* testsuite/20_util/tuple/range_access.cc: Ditto.
    	* testsuite/20_util/pair/range_access.cc: Ditto.
    
    From-SVN: r162578

---
+++ libstdc++-v3/testsuite/ext/vstring/range_access.cc
@@ -0,0 +1,37 @@
+// { dg-do compile }
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2010 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without Pred the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 24.6.5, range access [iterator.range]
+
+#include <ext/vstring.h>
+
+void
+test01()
+{
+  __gnu_cxx::__vstring s("Hello, World!");
+  std::begin(s);
+  std::end(s);
+
+#ifdef _GLIBCXX_USE_WCHAR_T
+  __gnu_cxx::__wvstring ws(L"Hello, World!");
+  std::begin(ws);
+  std::end(ws);
+#endif
+}
