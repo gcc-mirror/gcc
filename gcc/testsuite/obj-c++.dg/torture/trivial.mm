5a8e93d832589035a38c8a7d20cc52284565f4a0
Iain Sandoe <iains@gcc.gnu.org>
2010-07-27 12:02:50 +0000

    re-enable tls and lto tests for ObjC/C++
    	PR ObjC/44140
    	* obj-c++.dg/torture/tls/thr-init-1.mm: Re-enable test.
    	* obj-c++.dg/torture/tls/thr-init-2.mm: Ditto.
    	* obj-c++.dg/torture/tls/thr-init-3.mm: Ditto.
    	* obj-c++.dg/torture/trivial.mm: Ditto.
    	* objc.dg/torture/tls/thr-init-2.m: Ditto.
    	* objc.dg/torture/tls/thr-init-3.m: Ditto.
    	* objc.dg/torture/tls/thr-init.m: Ditto.
    	* objc.dg/torture/trivial.m: Ditto.
    
    From-SVN: r162563

--- gcc/testsuite/obj-c++.dg/torture/trivial.mm
+++ gcc/testsuite/obj-c++.dg/torture/trivial.mm
@@ -1,6 +1,5 @@
 // { dg-do run }
 
-// { dg-skip-if "PR44140" { *-*-* } { "-flto" "-fwhopr" } { "" } }
 // { dg-xfail-run-if "OBJC2 runtime" { *-*-darwin* && { lp64 && { ! objc2 } } } { "-fnext-runtime"  } { "-fgnu-runtime" } }
 // { dg-additional-sources "../../objc-obj-c++-shared/Object1.mm" }
 
