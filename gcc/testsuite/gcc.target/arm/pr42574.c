e3baafb71a677c35245218a94fcb566a32b8b5f4
mkuvyrkov <mkuvyrkov@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-07-27 21:06:31 +0000

    PR rtl-optimization/40956 	PR target/42495 	PR middle-end/42574 	* gcc.target/arm/pr40956.c, gcc.target/arm/pr42495.c, 	* gcc.target/arm/pr42574.c: Add tests.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@162600 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/pr42574.c
@@ -0,0 +1,24 @@
+/* { dg-options "-mthumb -Os -fpic -march=armv5te" }  */
+/* { dg-require-effective-target arm_thumb1_ok } */
+/* { dg-require-effective-target fpic } */
+/* Make sure the address of glob.c is calculated only once and using
+   a logical shift for the offset (200<<1).  */
+/* { dg-final { scan-assembler-times "lsl" 1 } } */
+
+struct A {
+ char a[400];
+ float* c;
+};
+struct A glob;
+void func();
+void func1(float*);
+int func2(float*, int*);
+void func3(float*);
+
+void test(int *p) {
+ func1(glob.c);
+ if (func2(glob.c, p)) {
+   func();
+ }
+ func3(glob.c);
+}
