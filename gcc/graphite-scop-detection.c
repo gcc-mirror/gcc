259c0e4464e413838d067165bc12e3ceca23c112
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-08-11 20:29:10 +0000

    Add loop_exits_from_bb_p.
    2010-07-20  Vladimir Kargov  <kargov@gmail.com>
    	    Sebastian Pop  <sebastian.pop@amd.com>
    
    	* cfgloop.c (is_loop_exit): Renamed loop_exits_to_bb_p.
    	(loop_exits_from_bb_p): New.
    	* cfgloop.h (is_loop_exit): Renamed loop_exits_to_bb_p.
    	(loop_exits_from_bb_p): Declared.
    	* graphite-scop-detection.c (scopdet_basic_block_info): Call
    	loop_exits_to_bb_p.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@163153 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/graphite-scop-detection.c
+++ gcc/graphite-scop-detection.c
@@ -594,7 +594,7 @@ scopdet_basic_block_info (basic_block bb, loop_p outermost_loop,
 	  {
 	    /* Ignore loop exits.  They will be handled after the loop
 	       body.  */
-	    if (is_loop_exit (loop, e->dest))
+	    if (loop_exits_to_bb_p (loop, e->dest))
 	      {
 		result.exits = true;
 		continue;
