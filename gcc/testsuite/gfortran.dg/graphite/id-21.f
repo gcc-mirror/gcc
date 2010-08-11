1c2a7491be7c2f03af404ace3956ad2bc356afae
Sebastian Pop <sebastian.pop@amd.com>
2010-08-11 20:29:43 +0000

    Propagate constant values or parametric expressions outside the scop region.
    2010-07-22  Sebastian Pop  <sebastian.pop@amd.com>
    
    	* graphite-sese-to-poly.c (propagate_expr_outside_region): New.
    	(rewrite_close_phi_out_of_ssa): Propagate constant values or
    	parametric expressions outside the scop region.
    	(rewrite_cross_bb_scalar_deps): Same.
    	* sese.c (rename_uses): Use NULL_TREE instead of NULL for trees.
    
    	* gcc.dg/graphite/run-id-5.c: New.
    	* gcc.dg/graphite/run-id-6.c: New.
    	* gfortran.dg/graphite/id-21.f: New.
    
    From-SVN: r163157

---
+++ gcc/testsuite/gfortran.dg/graphite/id-21.f
@@ -0,0 +1,20 @@
+      MODULE LES3D_DATA
+      DOUBLE PRECISION,ALLOCATABLE,DIMENSION(:,:,:) ::
+     >             P, T, H
+      DOUBLE PRECISION,ALLOCATABLE,DIMENSION(:,:,:,:) ::
+     >             HF
+      DOUBLE PRECISION,ALLOCATABLE,DIMENSION(:,:,:,:,:) ::
+     >             Q
+      END MODULE LES3D_DATA
+      USE LES3D_DATA
+      DO K = 1, KMAX - 1
+         DO J = 1, JMAX - 1
+            DO I = 1, I2
+               T(I,J,K) = (EI - HF(I,J,K,1)) / HF(I,J,K,3)
+            ENDDO
+            P(1:I2,J,K) = Q(1:I2,J,K,1,M) * HF(1:I2,J,K,4) * T(1:I2,J,K)
+            IF(ISGSK .EQ. 1) H(1:I2,J,K) =
+     >                   (Q(1:I2,J,K,5,M) + P(1:I2,J,K))
+         END DO
+      ENDDO
+      END
