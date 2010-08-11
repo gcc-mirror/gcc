666f4bf028657b6b4d9f0e773d7ed1dff78a7e7f
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-08-11 10:04:43 +0000

    * opts.h (struct cl_option_handler_func): Make handler take 	cl_decoded_option structure as parameter, not individual elements. 	(struct cl_option_handlers): Make callbacks take cl_decoded_option 	structure as parameter, not individual elements. 	(handle_option): Take cl_decoded_option structure as parameter, 	not individual elements. 	(handle_generated_option): Declare. 	* opts-common.c (handle_option): Take cl_decoded_option structure 	as parameter, not individual elements.  Update calls to callback 	and handler functions. 	(handle_generated_option): New. 	(read_cmdline_option): Update calls to callback functions and 	handle_option. 	* opts.c (common_handle_option, complain_wrong_lang, 	unknown_option_callback, post_handling_callback, 	lang_handle_option, target_handle_option): Take cl_decoded_option 	structure as parameter, not individual elements. 	(lang_handle_option, target_handle_option, common_handle_option): 	Assert option has at most one argument. 	(enable_warning_as_error): Call handle_generated_option instead of 	handle_option.  Do not pass -Werror argument as argument of 	generated option.
    c-family:
    	* c-opts.c (c_common_handle_option): Call handle_generated_option
    	instead of handle_option.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@163095 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-family/c-opts.c
+++ gcc/c-family/c-opts.c
@@ -437,8 +437,8 @@ c_common_handle_option (size_t scode, const char *arg, int value,
     case OPT_Wall:
       warn_unused = value;
       set_Wformat (value);
-      handle_option (OPT_Wimplicit, NULL, value, c_family_lang_mask, kind,
-		     handlers);
+      handle_generated_option (OPT_Wimplicit, NULL, value,
+			       c_family_lang_mask, kind, handlers);
       warn_char_subscripts = value;
       warn_missing_braces = value;
       warn_parentheses = value;
@@ -539,11 +539,11 @@ c_common_handle_option (size_t scode, const char *arg, int value,
     case OPT_Wimplicit:
       gcc_assert (value == 0 || value == 1);
       if (warn_implicit_int == -1)
-	handle_option (OPT_Wimplicit_int, NULL, value,
-		       c_family_lang_mask, kind, handlers);
+	handle_generated_option (OPT_Wimplicit_int, NULL, value,
+				 c_family_lang_mask, kind, handlers);
       if (warn_implicit_function_declaration == -1)
-	handle_option (OPT_Wimplicit_function_declaration, NULL, value,
-		       c_family_lang_mask, kind, handlers);
+	handle_generated_option (OPT_Wimplicit_function_declaration, NULL,
+				 value, c_family_lang_mask, kind, handlers);
       break;
 
     case OPT_Wimport:
