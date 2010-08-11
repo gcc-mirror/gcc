87b28340fd2de93c135ec1529227ca77722be056
Sebastian Pop <sebastian.pop@amd.com>
2010-08-11 20:28:31 +0000

    Do not rewrite out of SSA scalar phi nodes that can be scev_analyzable_p.
    2010-07-15  Sebastian Pop  <sebastian.pop@amd.com>
    
    	* graphite-sese-to-poly.c (reduction_phi_p): Do not rewrite out of
    	SSA scalar phi nodes that can be scev_analyzable_p.
    
    	* gfortran.dg/graphite/id-20.f: Adjust testcase.
    
    From-SVN: r163148

--- gcc/testsuite/gfortran.dg/graphite/id-20.f
+++ gcc/testsuite/gfortran.dg/graphite/id-20.f
@@ -1,4 +1,10 @@
-      DO 32 MB=1,NVIRA
-     *                     PVIRA(MA,MB)*(EA(MA+NOA)+EA(MB+NOA))*PT5
-   32 CONTINUE
+! { dg-options "-O3 -ffast-math" }
+
+      DIMENSION FPQR(25,25,25)
+      INTEGER P,Q,R
+            DO 130 R=1,N4MAX,2
+               IF(P.GT.1) THEN
+                  FPQR(P,Q,R)= RM2*FPQR(P,Q,R-2)*REC(P+Q+R-2)
+               END IF
+  130       RM2= RM2+TWO
       END
