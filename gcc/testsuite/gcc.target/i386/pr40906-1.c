b2cd1042630f43290c82cebb7e99512754010a66
Uros Bizjak <ubizjak@gmail.com>
2010-08-12 20:20:44 +0200

    pr40906-1.c: Add -fno-asynchronous-unwind-tables to dg-options.
    	* gcc.target/i386/pr40906-1.c: Add -fno-asynchronous-unwind-tables
    	to dg-options.
    	* gcc.target/i386/pr40906-2.c: Ditto.
    	* gcc.target/i386/pr40906-3.c: Ditto.
    
    From-SVN: r163198

--- gcc/testsuite/gcc.target/i386/pr40906-1.c
+++ gcc/testsuite/gcc.target/i386/pr40906-1.c
@@ -1,6 +1,6 @@
 /* { dg-do run } */
 /* { dg-require-effective-target ilp32 } */
-/* { dg-options "-O2 -fomit-frame-pointer -mpush-args -mno-accumulate-outgoing-args" } */
+/* { dg-options "-O2 -fomit-frame-pointer -fno-asynchronous-unwind-tables -mpush-args -mno-accumulate-outgoing-args" } */
 
 void abort (void);
 
