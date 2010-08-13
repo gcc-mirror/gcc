cf677bb86756a4ab60f1db9592261cfa90265b17
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2010-08-13 05:14:18 +0000

    re PR fortran/42526 (bogus truncation warning for default-initialized character components)
    2010-08-12  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libfortran/42526
    	* gfortran.dg/char_component_initializer_1.f90: New test.
    
    From-SVN: r163220

---
+++ gcc/testsuite/gfortran.dg/char_component_initializer_2.f90
@@ -0,0 +1,16 @@
+! { dg-do run }
+! { dg-options "-Wall" }
+! Added -Wall option to make sure PR42526 does not show up again.
+program gfcbug62
+  implicit none
+  character(len=16) ::    tdefi(2) = (/'0z1jan0000','1hr       '/)
+  type t_ctl
+     character(len=16) :: tdefi(2) = (/'0z1jan0000','1hr       '/)
+  end type t_ctl
+
+  type(t_ctl) :: ctl
+  integer     :: i,k
+  i = 1
+  k = 1
+  if (tdefi(1) .ne. ctl%tdefi(1)) call abort ()
+end program gfcbug62
