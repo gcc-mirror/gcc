ba3721c1c20ccd158a3631af7518e6d1d2662514
Tobias Burnus <burnus@net-b.de>
2010-08-15 18:20:56 +0200

    re PR fortran/45211 (C interoperable error when compiling BIND(C) function in a module.)
    2010-08-15  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/45211
            * decl.c (verify_c_interop_param): Remove superfluous space (" ").
            (verify_c_interop): Handle unresolved DT with bind(C).
    
    2010-08-15  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/45211
            * gfortran.dg/bind_c_usage_21.f90: New.
            * gfortran.dg/bind_c_dts_3.f03: Update dg-error.
    
    From-SVN: r163264

---
+++ gcc/testsuite/gfortran.dg/bind_c_usage_21.f90
@@ -0,0 +1,25 @@
+! { dg-do compile }
+!
+! PR fortran/45211
+!
+! Contributed by Scot Breitenfeld
+!
+module m
+contains
+  FUNCTION liter_cb(link_info) bind(C)
+    USE ISO_C_BINDING
+    IMPLICIT NONE
+
+    INTEGER(c_int) liter_cb
+
+    TYPE, bind(C) :: info_t
+       INTEGER(c_int) :: type
+    END TYPE info_t
+
+    TYPE(info_t) :: link_info
+
+    liter_cb = 0
+  END FUNCTION liter_cb
+end module m
+
+! { dg-final { cleanup-modules "m" } }
