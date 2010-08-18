6485bed9fc7732dac2044806647ef0e63367d2cd
qiyao <qiyao@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-08-18 12:33:43 +0000

    gcc/         PR target/45094         * config/arm/arm.c (output_move_double): Fix typo generating         instructions ('ldr'->'str').
    gcc/testsuite/
    
            PR target/45094
            * gcc.target/arm/pr45094.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@163338 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/pr45094.c
@@ -0,0 +1,27 @@
+/* { dg-do run } */
+/* { dg-require-effective-target arm_neon_hw } */
+/* { dg-options "-O2 -mcpu=cortex-a8" } */
+/* { dg-add-options arm_neon } */
+
+#include <stdlib.h>
+
+long long buffer[32];
+
+void __attribute__((noinline)) f(long long *p, int n)
+{
+  while (--n >= 0)
+    {
+      *p = 1;
+      p += 32;
+    }
+}
+
+int main(void)
+{
+  f(buffer, 1);
+  
+  if (!buffer[0])
+    abort();
+
+  return 0;
+}
