65d376958ca90c8189a611e1c4c9e82e4616a586
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-08-20 19:23:52 +0000

    PR testsuite/45361 	* gcc.target/i386/volatile-2.c: Update scan strings to also 	include (%rip) for the memory reference on x86_64.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@163416 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/volatile-2.c
+++ gcc/testsuite/gcc.target/i386/volatile-2.c
@@ -10,8 +10,8 @@ extern int volatile obj_0;
 void test_0 (int data)
 {
   /* should not reread obj */
-  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_0" } } */
-  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_0," } } */
+  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_0(\\(%rip\\))?" } } */
+  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_0(\\(%rip\\))?," } } */
   obj_0 = data;
 }
 
@@ -19,8 +19,8 @@ extern int volatile obj_1;
 int test_1 (int data)
 {
   /* should not reread obj */
-  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_1" } } */
-  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_1," } } */
+  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_1(\\(%rip\\))?" } } */
+  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_1(\\(%rip\\))?," } } */
   return obj_1 = data;
 }
 
@@ -28,8 +28,8 @@ extern int volatile obj_2;
 int test_2 (void)
 {
   /* should not reread obj */
-  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_2" } } */
-  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_2," } } */
+  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_2(\\(%rip\\))?" } } */
+  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_2(\\(%rip\\))?," } } */
   return obj_2 = 0;
 }
 
@@ -40,8 +40,8 @@ extern int volatile obj_3;
 int test_3 (int data)
 {
   /* should not reread obj */
-  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_3" } } */
-  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_3," } } */
+  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_3(\\(%rip\\))?" } } */
+  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_3(\\(%rip\\))?," } } */
   return (obj_3 = data, 0);
 }
 
@@ -49,16 +49,16 @@ extern int volatile obj_4;
 int test_4 (void)
 {
   /* should not reread obj */
-  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_4" } } */
-  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_4," } } */
+  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_4(\\(%rip\\))?" } } */
+  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_4(\\(%rip\\))?," } } */
   return (obj_4 = 0, 0);
 }
 extern int volatile obj_5;
 int test_5 (void)
 {
   /* should reread obj */
-  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_5" } } */
-  /* { dg-final { scan-assembler "movl\[ \t\]obj_5," } } */
+  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_5(\\(%rip\\))?" } } */
+  /* { dg-final { scan-assembler "movl\[ \t\]obj_5(\\(%rip\\))?," } } */
   return (obj_5 = 0, obj_5);
 }
 
@@ -68,8 +68,8 @@ extern int volatile obj_6;
 void test_6 (int data, int cond)
 {
   /* should not reread obj */
-  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_6" } } */
-  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_6," } } */
+  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_6(\\(%rip\\))?" } } */
+  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_6(\\(%rip\\))?," } } */
   cond ? obj_6 = data : 0;
 }
 
@@ -77,8 +77,8 @@ extern int volatile obj_7;
 int test_7 (int data, int cond)
 {
   /* should not reread obj */
-  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_7" } } */
-  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_7," } } */
+  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_7(\\(%rip\\))?" } } */
+  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_7(\\(%rip\\))?," } } */
   return cond ? obj_7 = data : 0;
 }
 
@@ -86,7 +86,7 @@ extern int volatile obj_8;
 int test_8 (int cond)
 {
   /* should not reread obj */
-  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_8" } } */
-  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_8," } } */
+  /* { dg-final { scan-assembler "movl\[ \t\]\[^,\]+, obj_8(\\(%rip\\))?" } } */
+  /* { dg-final { scan-assembler-not "movl\[ \t\]obj_8(\\(%rip\\))?," } } */
   return cond ? obj_8 = 0 : 0;
 }
