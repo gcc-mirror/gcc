994fc9abdde826d0b843b48e8ef1627295c3ae25
Jakub Jelinek <jakub@redhat.com>
2010-08-20 20:49:46 +0200

    re PR middle-end/44974 (Function with attribute noreturn omits a call to another function with noreturn)
    	PR middle-end/44974
    	* builtins.c (expand_builtin): Don't optimize away
    	calls to DECL_LOOPING_CONST_OR_PURE_P builtins.
    
    	* gcc.dg/pr44974.c: New test.
    
    From-SVN: r163415

---
+++ gcc/testsuite/gcc.dg/pr44974.c
@@ -0,0 +1,23 @@
+/* PR middle-end/44974 */
+/* { dg-do compile } */
+/* { dg-options "-O -fno-optimize-sibling-calls" } */
+
+extern void foo (int status) __attribute__ ((__noreturn__));
+extern void bar (int status) __attribute__ ((__noreturn__));
+extern void _Exit (int status) __attribute__ ((__noreturn__));
+
+void
+foo (int status)
+{
+  _Exit (status);
+}
+
+void
+_Exit (int status)
+{
+  bar (status);
+}
+
+/* { dg-final { scan-assembler "call\[^\n\]*_Exit" { target i?86-*-* x86_64-*-* ia64-*-* sparc*-*-* } } } */
+/* { dg-final { scan-assembler "bl\[^\n\]*_Exit" { target powerpc*-*-* } } } */
+/* { dg-final { scan-assembler "brasl\[^\n\]*_Exit" { target { s390*-*-* && lp64 } } } } */
