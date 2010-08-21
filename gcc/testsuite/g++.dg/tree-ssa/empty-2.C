508e475706c3560a86b08446e1bb764773b93ed9
Jan Hubicka <jh@suse.cz>
2010-08-21 11:46:15 +0200

    re PR middle-end/45307 (Stores expanding to no RTL not removed by tree optimizers, Empty ctors/dtors not eliminated)
    	PR c++/45307
    	PR c++/17736
    	* cgraph.h (cgraph_only_called_directly_p,
    	cgraph_can_remove_if_no_direct_calls_and_refs_p): Handle
    	static cdtors.
    	* cgraphunit.c (cgraph_decide_is_function_needed): Static cdtors
    	are not needed.
    	(cgraph_finalize_function): Static cdtors are reachable.
    	(cgraph_mark_functions_to_output): Use cgraph_only_called_directly_p.
    
    	* gcc.dg/ipa/ctor-empty-1.c: Add testcase.
    	* g++.dg/tree-ssa/empty-2.C: Check that constructor got optimized out.
    
    From-SVN: r163439

--- gcc/testsuite/g++.dg/tree-ssa/empty-2.C
+++ gcc/testsuite/g++.dg/tree-ssa/empty-2.C
@@ -1,8 +1,11 @@
 // PR c++/45307
-// { dg-options -fdump-tree-gimple }
+// { dg-options "-fdump-tree-gimple -fdump-tree-optimized" }
 
 struct fallible_t { };
 const fallible_t fallible = fallible_t();
 
 // { dg-final { scan-tree-dump-not "fallible" "gimple" } }
+// Whole constructor should be optimized away.
+// { dg-final { scan-tree-dump-not "int" "optimized" } }
 // { dg-final { cleanup-tree-dump "gimple" } }
+// { dg-final { cleanup-tree-dump "optimized" } }
