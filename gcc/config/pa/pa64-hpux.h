698b676d43c5961b2f48f46e33e8444db51f46dd
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-08-22 16:23:38 +0000

    PR boehm-gc/34544 	* gthr-posix.h (__gthread_active_init): Delete. 	(__gthread_active_p): Do activity check here. 	Don't include errno.h on hppa-hpux.  Update comment. 	* gthr-posix95.h (__gthread_active_init): Delete. 	(__gthread_active_p): Do activity check here. 	Don't include errno.h on hppa-hpux.  Update comment. 	* config.gcc (hppa[12]*-*-hpux11*): Define extra_parts. 	* config/pa/pa64-hpux.h (LIB_SPEC): When -static is specified, only 	add -lpthread when -mt or -pthread is specified. 	* config/pa/pa-hpux11.h (LIB_SPEC): likewise. 	(LINK_GCC_C_SEQUENCE_SPEC): Define. 	* config/pa/t-pa-hpux11 (LIBGCCSTUB_OBJS): Define. 	(stublib.c, pthread_default_stacksize_np-stub.o, 	pthread_mutex_lock-stub.o, pthread_mutex_unlock-stub.o, 	$(T)libgcc_stub.a): Add methods. 	* config/pa/t-pa64 (LIBGCCSTUB_OBJS): Add pthread stubs. 	(stublib.c, pthread_default_stacksize_np-stub.o, 	pthread_mutex_lock-stub.o, pthread_mutex_unlock-stub.o): Add methods. 	* config/pa/stublib.c (pthread_default_stacksize_np, pthread_mutex_lock, 	pthread_mutex_unlock): New stubs.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@163461 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/pa/pa64-hpux.h
+++ gcc/config/pa/pa64-hpux.h
@@ -59,35 +59,35 @@ along with GCC; see the file COPYING3.  If not see
 #if ((TARGET_DEFAULT | TARGET_CPU_DEFAULT) & MASK_GNU_LD)
 #define LIB_SPEC \
   "%{!shared:\
-     %{!p:%{!pg:%{static|mt|pthread:%{fopenmp:%{static:-a shared} -lrt\
-		  %{static:-a archive}} -lpthread} -lc\
+     %{!p:%{!pg:%{fopenmp:%{static:-a shared} -lrt %{static:-a archive}}\
+	    %{mt|pthread:-lpthread} -lc\
 	    %{static:%{!nolibdld:-a shared -ldld -a archive -lc}}}}\
      %{p:%{!pg:%{static:%{!mhp-ld:-a shared}%{mhp-ld:-a archive_shared}}\
 	   -lprof %{static:-a archive}\
-	   %{static|mt|pthread:%{fopenmp:%{static:-a shared} -lrt\
-	     %{static:-a archive}} -lpthread} -lc\
+	   %{fopenmp:%{static:-a shared} -lrt %{static:-a archive}}\
+	   %{mt|pthread:-lpthread} -lc\
 	   %{static:%{!nolibdld:-a shared -ldld -a archive -lc}}}}\
      %{pg:%{static:%{!mhp-ld:-a shared}%{mhp-ld:-a archive_shared}}\
        -lgprof %{static:-a archive}\
-       %{static|mt|pthread:%{fopenmp:%{static:-a shared} -lrt\
-	 %{static:-a archive}} -lpthread} -lc\
+       %{fopenmp:%{static:-a shared} -lrt %{static:-a archive}}\
+       %{mt|pthread:-lpthread} -lc\
        %{static:%{!nolibdld:-a shared -ldld -a archive -lc}}}}\
    %{shared:%{mt|pthread:-lpthread}}"
 #else
 #define LIB_SPEC \
   "%{!shared:\
-     %{!p:%{!pg:%{static|mt|pthread:%{fopenmp:%{static:-a shared} -lrt\
-		  %{static:-a archive}} -lpthread} -lc\
+     %{!p:%{!pg:%{fopenmp:%{static:-a shared} -lrt %{static:-a archive}}\
+	    %{mt|pthread:-lpthread} -lc\
 	    %{static:%{!nolibdld:-a shared -ldld -a archive -lc}}}}\
      %{p:%{!pg:%{static:%{mgnu-ld:-a shared}%{!mgnu-ld:-a archive_shared}}\
 	   -lprof %{static:-a archive}\
-	   %{static|mt|pthread:%{fopenmp:%{static:-a shared} -lrt\
-	     %{static:-a archive}} -lpthread} -lc\
+	   %{fopenmp:%{static:-a shared} -lrt %{static:-a archive}}\
+	   %{mt|pthread:-lpthread} -lc\
 	   %{static:%{!nolibdld:-a shared -ldld -a archive -lc}}}}\
      %{pg:%{static:%{mgnu-ld:-a shared}%{!mgnu-ld:-a archive_shared}}\
        -lgprof %{static:-a archive}\
-       %{static|mt|pthread:%{fopenmp:%{static:-a shared} -lrt\
-	 %{static:-a archive}} -lpthread} -lc\
+       %{fopenmp:%{static:-a shared} -lrt %{static:-a archive}}\
+       %{mt|pthread:-lpthread} -lc\
        %{static:%{!nolibdld:-a shared -ldld -a archive -lc}}}}\
    %{shared:%{mt|pthread:-lpthread}}"
 #endif
