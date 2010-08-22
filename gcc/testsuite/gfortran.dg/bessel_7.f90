a323a41ec114506a1d159bb7493dac846243605b
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-08-22 08:34:14 +0000

    2010-08-22  Tobias Burnus  <burnus@net-b.de>             Dominique d'Humieres <dominiq@lps.ens.fr>
            PR fortran/45367
            PR fortran/36158
            * gfortran.dg/bessel_6.f90: Increase numeric tolerence.
            * gfortran.dg/bessel_7.f90: Increase numeric tolerence.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@163454 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/bessel_7.f90
+++ gcc/testsuite/gfortran.dg/bessel_7.f90
@@ -8,12 +8,12 @@
 implicit none
 real,parameter :: values(*) = [0.0, 0.5, 1.0, 0.9, 1.8,2.0,3.0,4.0,4.25,8.0,34.53, 475.78] 
 real,parameter :: myeps(size(values)) = epsilon(0.0) &
-                  * [2, 2, 2, 5, 5, 2, 12, 2, 4, 3, 30, 130 ]
+                  * [2, 3, 3, 5, 7, 2, 12, 4, 7, 3, 30, 168 ]
 ! The following is sufficient for me - the values above are a bit
 ! more tolerant
 !                  * [0, 0, 0, 3, 3, 0, 9, 0, 2, 1, 22, 130 ]
 integer,parameter :: nit(size(values)) =  &
-                 [100, 100, 100, 100, 100, 100, 10, 100, 100, 100, 10, 25 ]
+                 [100, 100, 100, 25, 15, 100, 10, 31, 7, 100, 7, 25 ]
 integer, parameter :: Nmax = 100
 real :: rec(0:Nmax), lib(0:Nmax)
 integer :: i
@@ -32,13 +32,13 @@ real X, myeps, myeps2
 rec = BESSEL_YN(0, Nmax, X)
 lib = [ (BESSEL_YN(i, X), i=0,Nmax) ]
 
-!print *, 'YN for X = ', X, ' -- Epsilon = ',epsilon(x)
+print *, 'YN for X = ', X, ' -- Epsilon = ',epsilon(x)
 do i = 0, Nmax
-!  print '(i2,2e17.9,e12.2,f14.10,2l3)', i, rec(i), lib(i), &
-!        rec(i)-lib(i), ((rec(i)-lib(i))/rec(i))/epsilon(x), &
-!        i > nit .or. rec(i) == lib(i) &
-!                .or. abs((rec(i)-lib(i))/rec(i)) < myeps2, &
-!        rec(i) == lib(i) .or. abs((rec(i)-lib(i))/rec(i)) < myeps
+  print '(i2,2e17.9,e12.2,f14.10,2l3)', i, rec(i), lib(i), &
+        rec(i)-lib(i), ((rec(i)-lib(i))/rec(i))/epsilon(x), &
+        i > nit .or. rec(i) == lib(i) &
+                .or. abs((rec(i)-lib(i))/rec(i)) < myeps2, &
+        rec(i) == lib(i) .or. abs((rec(i)-lib(i))/rec(i)) < myeps
 if (.not. (i > nit .or. rec(i) == lib(i) &
                    .or. abs((rec(i)-lib(i))/rec(i)) < myeps2)) &
   call abort ()
