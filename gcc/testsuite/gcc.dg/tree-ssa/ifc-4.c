bd544141e09187ded0b02cac4ee5ce56ca38622c
Sebastian Pop <sebastian.pop@amd.com>
2010-08-24 23:35:48 +0000

    Add flag -ftree-loop-if-convert-stores.
    This patch adds a flag that controls the replacement of the memory
    writes that are in predicated basic blocks with a full write:
    
    for (...)
      if (cond)
        A[i] = foo
    
    is replaced with:
    
    for (...)
      A[i] = cond ? foo : A[i]
    
    In order to do this, we have to call gimple_could_trap_p instead of
    gimple_assign_rhs_could_trap_p, as we have to also check that the LHS
    of assign stmts does not trap.
    
    	* common.opt (ftree-loop-if-convert-stores): New flag.
    	* doc/invoke.texi (ftree-loop-if-convert-stores): Documented.
    	* tree-if-conv.c (ifc_temp_var): Pass an extra parameter GSI.  Insert
    	the created statement before GSI.
    	(if_convertible_phi_p): Allow virtual phi nodes when
    	flag_loop_if_convert_stores is set.
    	(if_convertible_gimple_assign_stmt_p): Allow memory reads and writes
    	Do not handle types that do not match is_gimple_reg_type.
    	Remove loop and bb parameters.  Call gimple_could_trap_p instead of
    	when flag_loop_if_convert_stores is set, as LHS can contain
    	memory refs.
    	(if_convertible_stmt_p): Remove loop and bb parameters.  Update calls
    	to if_convertible_gimple_assign_stmt_p.
    	(if_convertible_loop_p): Update call to if_convertible_stmt_p.
    	(replace_phi_with_cond_gimple_assign_stmt): Renamed
    	predicate_scalar_phi.  Do not handle virtual phi nodes.
    	(ifconvert_phi_nodes): Renamed predicate_all_scalar_phis.
    	Call predicate_scalar_phi.
    	(insert_gimplified_predicates): Insert the gimplified predicate of a BB
    	just after the labels for flag_loop_if_convert_stores, otherwise
    	insert the predicate in the end of the BB.
    	(predicate_mem_writes): New.
    	(combine_blocks): Call predicate_all_scalar_phis.  When
    	flag_loop_if_convert_stores is set, call predicate_mem_writes.
    	(tree_if_conversion): Call mark_sym_for_renaming when
    	flag_loop_if_convert_stores is set.
    	(main_tree_if_conversion): Return TODO_update_ssa_only_virtuals when
    	flag_loop_if_convert_stores is set.
    
    	* gcc.dg/tree-ssa/ifc-4.c: New.
    	* gcc.dg/tree-ssa/ifc-7.c: New.
    
    From-SVN: r163530

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ifc-4.c
@@ -0,0 +1,53 @@
+/* { dg-do compile } */
+/* { dg-options "-c -O2 -ftree-vectorize -fdump-tree-ifcvt-stats" { target *-*-* } } */
+
+struct ht
+{
+  void * (*alloc_subobject) (int);
+};
+typedef struct cpp_reader cpp_reader;
+typedef struct cpp_token cpp_token;
+typedef struct cpp_macro cpp_macro;
+enum cpp_ttype
+{
+    CPP_PASTE,
+};
+struct cpp_token {
+  __extension__ enum cpp_ttype type : 8;
+} cpp_comment_table;
+struct cpp_macro {
+  union cpp_macro_u
+  {
+    cpp_token * tokens;
+  } exp;
+  unsigned int count;
+};
+struct cpp_reader
+{
+  struct ht *hash_table;
+};
+create_iso_definition (cpp_reader *pfile, cpp_macro *macro)
+{
+  unsigned int num_extra_tokens = 0;
+  {
+    cpp_token *tokns =
+      (cpp_token *) pfile->hash_table->alloc_subobject (sizeof (cpp_token)
+							* macro->count);
+    {
+      cpp_token *normal_dest = tokns;
+      cpp_token *extra_dest = tokns + macro->count - num_extra_tokens;
+      unsigned int i;
+      for (i = 0; i < macro->count; i++)
+	{
+	  if (macro->exp.tokens[i].type == CPP_PASTE)
+	    *extra_dest++ = macro->exp.tokens[i];
+	  else
+	    *normal_dest++ = macro->exp.tokens[i];
+	}
+    }
+  }
+}
+
+/* This cannot be if-converted because the stores are to aggregate types.  */
+/* { dg-final { scan-tree-dump-times "Applying if-conversion" 0 "ifcvt" } } */
+/* { dg-final { cleanup-tree-dump "ifcvt" } } */
