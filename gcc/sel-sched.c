9b6b8023dfd34e0e7784e8e2e000089e611c65d3
Andrey Belevantsev <abel@ispras.ru>
2010-08-24 13:10:40 +0400

    sel-sched.c: Add 2010 to copyright years.
            * sel-sched.c: Add 2010 to copyright years.
            * sel-sched-ir.c: Likewise.
            * sel-sched-ir.h: Likewise.
    
    From-SVN: r163515

--- gcc/sel-sched.c
+++ gcc/sel-sched.c
@@ -1,5 +1,5 @@
 /* Instruction scheduling pass.  Selective scheduler and pipeliner.
-   Copyright (C) 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
+   Copyright (C) 2006, 2007, 2008, 2009, 2010 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
