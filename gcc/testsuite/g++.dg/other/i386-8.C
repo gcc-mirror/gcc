932c9bffa0224eb16e061da1802f6f95fb23e7d3
Jakub Jelinek <jakub@redhat.com>
2010-08-25 19:49:26 +0200

    re PR rtl-optimization/45400 (XBMC AudioEngine Compilation C++ Internal Compiler Error /w Optimization)
    	PR rtl-optimization/45400
    	* combine.c (simplify_shift_const_1) <case SUBREG>: Only use
    	SUBREG_REG if both modes are of MODE_INT class.
    
    	* g++.dg/other/i386-8.C: New test.
    
    From-SVN: r163551

---
+++ gcc/testsuite/g++.dg/other/i386-8.C
@@ -0,0 +1,23 @@
+// PR rtl-optimization/45400
+// { dg-do compile { target i?86-*-* x86_64-*-* } }
+// { dg-options "-O2 -msse2" }
+// { dg-options "-O2 -msse2 -fpic" { target fpic } }
+// { dg-require-effective-target sse2 }
+
+#include <xmmintrin.h>
+
+static inline unsigned short
+bar (unsigned short x)
+{
+  return ((x << 8) | (x >> 8));
+}
+
+unsigned int
+foo (float *x, short *y)
+{
+  __m128 a = _mm_set_ps1 (32767.5f);
+  __m128 b = _mm_mul_ps (_mm_load_ps (x), a);
+  __m64 c = _mm_cvtps_pi16 (b);
+  __builtin_memcpy (y, &c, sizeof (short) * 4);
+  y[0] = bar (y[0]);
+}
