9407f6bcfba46e815e8b5b2ea9e1be9cb8c4df79
Richard Guenther <rguenther@suse.de>
2010-08-25 10:03:19 +0000

    re PR middle-end/45379 (~10% slowdown on test_fpu at revision 163278)
    2010-08-25  Richard Guenther  <rguenther@suse.de>
    
    	PR middle-end/45379
    	* emit-rtl.c (set_mem_attributes_minus_bitpos): Handle
    	TARGET_MEM_REF in alignment computation.
    
    From-SVN: r163540

--- gcc/emit-rtl.c
+++ gcc/emit-rtl.c
@@ -1615,6 +1615,11 @@ set_mem_attributes_minus_bitpos (rtx ref, tree t, int objectp,
 	align = MAX (align, TYPE_ALIGN (type));
     }
 
+  else if (TREE_CODE (t) == TARGET_MEM_REF)
+    /* ??? This isn't fully correct, we can't set the alignment from the
+       type in all cases.  */
+    align = MAX (align, TYPE_ALIGN (type));
+
   else if (TREE_CODE (t) == MISALIGNED_INDIRECT_REF)
     {
       if (integer_zerop (TREE_OPERAND (t, 1)))
