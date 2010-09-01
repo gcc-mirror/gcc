c961549127e25a146e277c2c4b2c82cf35174fca
Andi Kleen <ak@linux.intel.com>
2010-09-01 11:47:41 +0000

    bootstrap-lto.mk (STAGE2_CFLAGS, [...]): Change to -fwhopr=jobserver -fuse-linker-plugin -frandom-seed=1.
    2010-09-01  Andi Kleen  <ak@linux.intel.com>
    
            * bootstrap-lto.mk (STAGE2_CFLAGS, STAGE3_CFLAGS): Change
            to -fwhopr=jobserver -fuse-linker-plugin -frandom-seed=1.
    
    From-SVN: r163725

--- config/bootstrap-lto.mk
+++ config/bootstrap-lto.mk
@@ -1,8 +1,8 @@
 # This option enables LTO for stage2 and stage3.  It requires lto to
 # be enabled for stage1 with --enable-stage1-languages.
 
-STAGE2_CFLAGS += -flto
-STAGE3_CFLAGS += -flto
+STAGE2_CFLAGS += -fwhopr=jobserver -fuse-linker-plugin -frandom-seed=1
+STAGE3_CFLAGS += -fwhopr=jobserver -fuse-linker-plugin -frandom-seed=1
 
 # Ada fails to build with LTO, turn it off for now.
 BOOT_ADAFLAGS += -fno-lto
