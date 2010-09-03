79ee0029f41dabc810aabd2a600f8e8b32568ecb
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-09-03 09:58:21 +0000

    * passes.c (rest_of_decl_compilation): Do not add local vars into varpol. 	* varpool.c (varpool_get_node, varpool_node): Sanity check that only 	static or extern vars are in varpool. 	(varpool_finalize_decl): Sanity check that only static vars are finalized.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@163804 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/passes.c
+++ gcc/passes.c
@@ -219,7 +219,8 @@ rest_of_decl_compilation (tree decl,
   /* Let cgraph know about the existence of variables.  */
   if (in_lto_p && !at_end)
     ;
-  else if (TREE_CODE (decl) == VAR_DECL && !DECL_EXTERNAL (decl))
+  else if (TREE_CODE (decl) == VAR_DECL && !DECL_EXTERNAL (decl)
+	   && TREE_STATIC (decl))
     varpool_node (decl);
 }
 
