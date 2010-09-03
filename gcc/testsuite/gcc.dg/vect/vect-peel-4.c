e6bc4de90d944db687e20de1ac5fbebe32c9642d
Steve Ellcey <sje@cup.hp.com>
2010-09-03 20:26:30 +0000

    vect-peel-3.c: XFAIL if vect_no_align.
    2010-09-03  Steve Ellcey  <sje@cup.hp.com>
    
    	* gcc.dg/vect/vect-peel-3.c: XFAIL if vect_no_align.
    	* gcc.dg/vect/vect-peel-4.c: Ditto.
    
    From-SVN: r163842

--- gcc/testsuite/gcc.dg/vect/vect-peel-4.c
+++ gcc/testsuite/gcc.dg/vect/vect-peel-4.c
@@ -42,6 +42,6 @@ int main (void)
 }
 
 /* { dg-final { scan-tree-dump-times "vectorized 1 loops" 2 "vect" } } */
-/* { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 1 "vect" } } */
+/* { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 1 "vect"  { xfail vect_no_align } } } */
 /* { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 0 "vect" } } */
 /* { dg-final { cleanup-tree-dump "vect" } } */
