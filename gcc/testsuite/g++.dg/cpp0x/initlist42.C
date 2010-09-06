948e1ebc502a0517e6becc3dde642251d55456f4
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-09-06 04:49:33 +0000

    * call.c (implicit_conversion): Fix value-init of enums. 	(convert_like_real): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@163897 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/initlist42.C
@@ -0,0 +1,13 @@
+// { dg-options -std=c++0x }
+
+enum Unscoped { };
+enum class Scoped { };
+
+Unscoped bar(Unscoped x) { return x; }
+Scoped bar(Scoped x) { return x; }
+
+auto var1u = bar(Unscoped()); // OK
+auto var1s = bar(Scoped()); // OK
+
+auto var2u = bar(Unscoped{}); // #1 Error, but should work
+auto var2s = bar(Scoped{}); // #2 Error, but should work
