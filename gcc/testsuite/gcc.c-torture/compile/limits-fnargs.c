7bce7e63caa4bbc3b3ea9af6fce87229a8ddfade
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-09-08 17:50:41 +0000

    gcc: 	* doc/sourcebuild.texi (Effective-Target Keywords): Document 	run_expensive_tests.
    	gcc/testsuite:
    	* lib/target-supports.exp
    	(check_effective_target_run_expensive_tests): New proc.
    	* gcc.c-torture/compile/limits-fnargs.c: Use
    	dg-require-effective-target run_expensive_tests.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@164012 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/compile/limits-fnargs.c
+++ gcc/testsuite/gcc.c-torture/compile/limits-fnargs.c
@@ -1,4 +1,5 @@
 /* { dg-timeout-factor 4.0 } */
+/* { dg-require-effective-target run_expensive_tests } */
 
 #define PAR1 int, int, int, int, int, int, int, int, int, int
 #define PAR2 PAR1, PAR1, PAR1, PAR1, PAR1, PAR1, PAR1, PAR1, PAR1, PAR1
