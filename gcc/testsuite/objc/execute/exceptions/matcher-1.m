f9a69540b036691a4b17cb5f206a6d09ce600b97
nicola <nicola@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-09-08 21:03:51 +0000

    * objc/execute/exceptions/throw-nil.m: New test. 	* objc/execute/exceptions/handler-1.m: Updated to use the new 	objc_set_uncaught_exception_handler() function. 	* objc/execute/exceptions/matcher-1.m: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@164024 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/objc/execute/exceptions/matcher-1.m
@@ -0,0 +1,68 @@
+/* Test custom exception matchers  */
+/* Author: Nicola Pero */
+
+#ifdef __NEXT_RUNTIME__
+/* This test only runs for the GNU runtime.  TODO: It should work on
+   the NEXT runtime as well (needs testing).
+ */
+
+int main(void)
+{
+  return 0;
+}
+
+#else
+
+#include <objc/objc-api.h>
+#include <objc/objc-exception.h>
+#include <objc/Object.h>
+#include <stdlib.h>
+
+static unsigned int handlerExpected = 0;
+
+void
+my_exception_matcher(Class match_class, id exception)
+{
+  /* Always matches.  */
+  return 1;
+}
+
+@interface A : Object
+@end
+
+@implementation A
+@end
+
+@interface B : Object
+@end
+
+@implementation B
+@end
+
+int 
+main(int argc, char *argv[])
+{
+  objc_setExceptionMatcher (my_exception_matcher);
+
+  @try
+    {
+      @throw [A new];
+    }
+  @catch (B *exception)
+    {
+      /* Since we installed an exception matcher that always matches,
+	 the exception should be sent here even if it's of class A and
+	 this is looking for exceptions of class B.
+       */
+      return 0;
+    }
+  @catch (id exception)
+    {
+      abort ();
+    }
+
+  abort ();
+}
+
+
+#endif
