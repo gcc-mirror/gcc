616f12900dccc06a528586f8c7f969012d1e50a1
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2010-09-11 16:34:01 +0000

    * gfortran.dg/promotion.f90: Fix options.
    From-SVN: r164215

--- gcc/testsuite/gfortran.dg/promotion.f90
+++ gcc/testsuite/gfortran.dg/promotion.f90
@@ -1,6 +1,6 @@
 ! { dg-do run { target i?86-*-* x86_64-*-* } }
 ! { dg-require-effective-target ilp32 }
-! { dg-options "-fdefault-integer-8 -fdefault-real-8" }
+! { dg-options "-fdefault-integer-8 -fdefault-real-8 -fdefault-double-8" }
 program a
    logical l
    integer i
