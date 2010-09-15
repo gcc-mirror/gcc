441b624e3d4b2be01deaf8156bd5ea2b39849259
Jason Merrill <jason@redhat.com>
2010-09-15 19:55:43 -0400

    * decl2.c (grokbitfield): Diagnose non-integral width.
    From-SVN: r164321

---
+++ gcc/testsuite/g++.dg/cpp0x/scoped_enum2.C
@@ -0,0 +1,11 @@
+// { dg-options -std=c++0x }
+
+enum class E { e = 10 };
+enum E2 { e2 = 10 };
+
+struct C {
+  int arr[E::e];    // { dg-error "non-integral type" }
+  int arr2[E2::e2]; // OK
+  int i: E::e;	    // { dg-error "non-integral type" }
+  int i2: E2::e2;   // OK
+};
