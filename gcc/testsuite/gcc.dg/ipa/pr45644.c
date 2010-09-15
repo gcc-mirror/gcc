1faab08daf19e4769865936614a8a7bef1d72dd6
Martin Jambor <mjambor@suse.cz>
2010-09-15 17:59:27 +0200

    re PR middle-end/45644 (450.soplex in SPEC CPU 2006 is miscompiled)
    2010-09-15  Martin Jambor  <mjambor@suse.cz>
    
    	PR middle-end/45644
    	* tree-sra.c (create_access): Check for bit-fields directly.
    
    	* testsuite/gcc.dg/ipa/pr45644.c: New test.
    
    From-SVN: r164313

---
+++ gcc/testsuite/gcc.dg/ipa/pr45644.c
@@ -0,0 +1,35 @@
+/* Verify that we do not IPA-SRA bitfields.  */
+/* { dg-do run } */
+/* { dg-options "-O2"  } */
+
+extern void abort (void);
+
+struct S
+{
+  int j : 8;
+  int i : 24;
+  int l;
+};
+
+static int __attribute__((noinline)) foo (struct S *s)
+{
+  int z = s->i;
+  if (z != 777)
+    abort ();
+  return 0;
+}
+
+int __attribute__((noinline)) bar (struct S *s)
+{
+  return foo (s);
+}
+
+int main (int argc, char *argv[])
+{
+  struct S s;
+  s.j = 5;
+  s.i = 777;
+  s.l = -1;
+
+  return bar (&s);
+}
