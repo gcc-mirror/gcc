336592236075fc884e928f9d2948889a3ac21657
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-09-17 13:57:04 +0000

    2010-09-17  Richard Guenther  <rguenther@suse.de>
    	PR middle-end/45678
    	* gcc.dg/torture/pr45678-1.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@164369 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/pr45678-1.c
@@ -0,0 +1,16 @@
+/* { dg-do run } */
+
+typedef float V __attribute__ ((vector_size (16)));
+V g;
+float d[4] = { 4, 3, 2, 1 };
+
+int
+main ()
+{
+  V e;
+  __builtin_memcpy (&e, &d, sizeof (d));
+  V f = { 5, 15, 25, 35 };
+  e = e * f;
+  g = e;
+  return 0;
+}
