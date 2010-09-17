4c588abff3db7852724edf638a795b7a37f70f2b
Jan Hubicka <jh@suse.cz>
2010-09-17 13:27:25 +0200

    lto-cgraph.c (compute_ltrans_boundary): Use const_value_known.
    	* lto-cgraph.c (compute_ltrans_boundary): Use const_value_known.
    	* lto.c (lto_promote_cross_file_statics): Use const_value_known.
    
    From-SVN: r164366

--- gcc/lto/lto.c
+++ gcc/lto/lto.c
@@ -1008,7 +1008,7 @@ lto_promote_cross_file_statics (void)
 	 from this partition that are not in this partition.
 	 This needs to be done recursively.  */
       for (vnode = varpool_nodes; vnode; vnode = vnode->next)
-	if ((TREE_READONLY (vnode->decl) || DECL_IN_CONSTANT_POOL (vnode->decl))
+	if ((vnode->const_value_known || DECL_IN_CONSTANT_POOL (vnode->decl))
 	    && DECL_INITIAL (vnode->decl)
 	    && !varpool_node_in_set_p (vnode, vset)
 	    && referenced_from_this_partition_p (&vnode->ref_list, set, vset)
@@ -1048,7 +1048,7 @@ lto_promote_cross_file_statics (void)
 			   && !v->externally_visible && v->analyzed)
 		    {
 		      if (promote_var (v)
-			  && DECL_INITIAL (v->decl) && TREE_READONLY (v->decl)
+			  && DECL_INITIAL (v->decl) && v->const_value_known
 			  && !pointer_set_insert (inserted, vnode))
 			VEC_safe_push (varpool_node_ptr, heap,
 				       promoted_initializers, v);
