765c3e8f08dbd7208f348e5d318ed2dc8042275b
H.J. Lu <hongjiu.lu@intel.com>
2010-09-17 17:49:30 +0000

    Update stack alignment when increasing local variable alignment.
    gcc/
    
    2010-09-17  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR middle-end/45678
    	* cfgexpand.c (update_stack_alignment): New.
    	(get_decl_align_unit): Use it.
    	(expand_one_stack_var_at): Call update_stack_alignment.
    
    gcc/testsuite/
    
    2010-09-17  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR middle-end/45678
    	* gcc.dg/torture/pr45678-2.c: New.
    
    From-SVN: r164375

--- gcc/cfgexpand.c
+++ gcc/cfgexpand.c
@@ -205,19 +205,11 @@ static bool has_protected_decls;
    smaller than our cutoff threshold.  Used for -Wstack-protector.  */
 static bool has_short_buffer;
 
-/* Discover the byte alignment to use for DECL.  Ignore alignment
-   we can't do with expected alignment of the stack boundary.  */
+/* Update stack alignment requirement.  */
 
-static unsigned int
-get_decl_align_unit (tree decl)
+static void
+update_stack_alignment (unsigned int align)
 {
-  unsigned int align;
-
-  align = LOCAL_DECL_ALIGNMENT (decl);
-
-  if (align > MAX_SUPPORTED_STACK_ALIGNMENT)
-    align = MAX_SUPPORTED_STACK_ALIGNMENT;
-
   if (SUPPORTS_STACK_ALIGNMENT)
     {
       if (crtl->stack_alignment_estimated < align)
@@ -233,6 +225,22 @@ get_decl_align_unit (tree decl)
     crtl->stack_alignment_needed = align;
   if (crtl->max_used_stack_slot_alignment < align)
     crtl->max_used_stack_slot_alignment = align;
+}
+
+/* Discover the byte alignment to use for DECL.  Ignore alignment
+   we can't do with expected alignment of the stack boundary.  */
+
+static unsigned int
+get_decl_align_unit (tree decl)
+{
+  unsigned int align;
+
+  align = LOCAL_DECL_ALIGNMENT (decl);
+
+  if (align > MAX_SUPPORTED_STACK_ALIGNMENT)
+    align = MAX_SUPPORTED_STACK_ALIGNMENT;
+
+  update_stack_alignment (align);
 
   return align / BITS_PER_UNIT;
 }
@@ -735,6 +743,7 @@ expand_one_stack_var_at (tree decl, HOST_WIDE_INT offset)
       if (align == 0 || align > max_align)
 	align = max_align;
 
+      update_stack_alignment (align);
       DECL_ALIGN (decl) = align;
       DECL_USER_ALIGN (decl) = 0;
     }
