bef6486a5a2fdb1008e582d5ac048dd243e91142
Mikael Morin <mikael@gcc.gnu.org>
2010-09-23 11:11:23 +0000

    re PR fortran/45745 (ICE in gfc_conv_array_stride)
    2010-09-23  Mikael Morin  <mikael@gcc.gnu.org>
    
    	PR fortran/45745
    	PR fortran/45648
    	* trans-array.c (gfc_conv_expr_descriptor): Handle 
    	ss->type == GFC_SS_INTRINSIC (for {l,u}bound intrinsics) case. 
    
    2010-09-23  Mikael Morin  <mikael@gcc.gnu.org>
    
    	PR fortran/45745
    	PR fortran/45648
    	* gfortran.dg/vector_subscript_bound_1.f90: New.
    
    From-SVN: r164558

---
+++ gcc/testsuite/gfortran.dg/vector_subscript_bound_1.f90
@@ -0,0 +1,19 @@
+! { dg-do compile }
+!
+! PR fortran/45745
+! ICE with {L,U}BOUND intrinsic function as vector subscript on derived
+! type component. 
+!
+! Original test by Joost Van de Vondele <Joost.VandeVondele@pci.uzh.ch>
+
+MODULE pw_types
+  TYPE pw_type
+     REAL, DIMENSION ( : ), POINTER :: cr
+  END TYPE pw_type
+CONTAINS
+  SUBROUTINE pw_write(pw)
+    TYPE(pw_type), INTENT(in) :: pw
+    PRINT *, pw%cr(LBOUND(pw%cr))
+    PRINT *, pw%cr(UBOUND(pw%cr))
+  END SUBROUTINE pw_write
+END MODULE
