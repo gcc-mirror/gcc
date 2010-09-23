bd992fe5d55b910f8090731b362d1b7f6c72e917
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-09-23 11:00:27 +0000

    2010-09-23  Richard Guenther  <rguenther@suse.de>
    	PR middle-end/45750
    	* gimplify.c (gimplify_expr): Properly pass on GS_ERROR when
    	gimplifying MEM_REF.
    
    	* gcc.dg/pr45750.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@164555 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr45750.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-options "-O" } */
+
+struct S
+{
+  int i;
+};
+
+int invalid[] = 0; /* { dg-error "invalid initializer" } */
+
+void foo (void)
+{
+  if (((struct S *)undeclared)->i); /* { dg-error "undeclared" } */
+}
+
+/* { dg-message "reported only once" "" { target *-*-* } 13 } */
