030ef1fba5d03893d6cd2ae86395aadb76d788b7
Jakub Jelinek <jakub@redhat.com>
2010-09-23 12:57:37 +0200

    alloca-4.c: Remove -m32 from dg-options.
    	* gcc.dg/torture/stackalign/alloca-4.c: Remove -m32 from dg-options.
    	* gcc.dg/torture/stackalign/alloca-5.c: Likewise.  Fix up -DDEBUG
    	build.
    
    From-SVN: r164554

--- gcc/testsuite/gcc.dg/torture/stackalign/alloca-5.c
+++ gcc/testsuite/gcc.dg/torture/stackalign/alloca-5.c
@@ -1,6 +1,6 @@
 /* PR middle-end/45234 */
 /* { dg-do run { target { { i?86-*-* x86_64-*-* } && ilp32 } } } */
-/* { dg-options "-m32 -mincoming-stack-boundary=2 -mpreferred-stack-boundary=2" } */
+/* { dg-options "-mincoming-stack-boundary=2 -mpreferred-stack-boundary=2" } */
 
 #include "check.h"
 
@@ -22,7 +22,7 @@ main (void)
   if (__builtin_strncmp (p, "good", 5) != 0)
     {
 #ifdef DEBUG
-      p[size] = '\0';
+      p[5] = '\0';
       printf ("Failed: %s != good\n", p);
 #endif
      abort ();
