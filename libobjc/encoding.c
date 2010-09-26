7116b6ea5c1ed39cb2e52ceb192f583ad8e4f62b
Kai Tietz <kai.tietz@onevision.com>
2010-09-26 14:32:45 +0000

    sendmsg.c (get_imp): Remove inline.
    2010-09-26  Kai Tietz  <kai.tietz@onevision.com>
    
    	* sendmsg.c (get_imp):  Remove inline.
    	(objc_msg_lookup): Likewise.
    	(objc_get_uninstalled_dtable): Likewise.
    	* encoding.c (objc_skip_type_qualifiers): Likewise.
    	(objc_skip_offset): Likewise.
    	* archive.c (__objc_write_object): Likewise
    	(__objc_write_class):
    	(__objc_write_selector):
    	(objc_read_char):
    	(objc_read_unsigned_char):
    	(objc_read_short):
    	(objc_read_unsigned_short):
    	(objc_read_int):
    	(objc_read_long):
    	(__objc_read_nbyte_uint):
    	(objc_read_unsigned_int):
    	(objc_read_unsigned_long):
    	* objc/objc-decls.h (obc_EXPORT): Remove dllexport for DLL_EXPORT case.
    	(objc_EXPORT): Likewise.
    	* objc/message.h (objc-decls.h): Add include.
    	* objc/objc-api.h: Mark API by objc_EXPORT.
    	* libobjc.def (__objc_responds_to): Removed.
    
    From-SVN: r164632

--- libobjc/encoding.c
+++ libobjc/encoding.c
@@ -563,7 +563,7 @@ objc_promoted_size (const char *type)
   occurring in method prototype encodings.
 */
 
-inline const char *
+const char *
 objc_skip_type_qualifiers (const char *type)
 {
   while (*type == _C_CONST
@@ -701,7 +701,7 @@ objc_skip_typespec (const char *type)
   Skip an offset as part of a method encoding.  This is prepended by a
   '+' if the argument is passed in registers.
 */
-inline const char *
+const char *
 objc_skip_offset (const char *type)
 {
   if (*type == '+')
