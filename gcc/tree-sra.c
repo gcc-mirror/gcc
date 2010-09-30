69eb54ac94deb1bd955958a5d65c706a62be1abc
jamborm <jamborm@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-09-30 12:31:21 +0000

    2010-09-30  Martin Jambor  <mjambor@suse.cz>
    	* tree-sra.c (type_consists_of_records_p): Do not check for trailing
    	zero sized bit-fields.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@164750 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-sra.c
+++ gcc/tree-sra.c
@@ -815,14 +815,12 @@ create_access (tree expr, gimple stmt, bool write)
 
 /* Return true iff TYPE is a RECORD_TYPE with fields that are either of gimple
    register types or (recursively) records with only these two kinds of fields.
-   It also returns false if any of these records has a zero-size field as its
-   last field or has a bit-field.  */
+   It also returns false if any of these records contains a bit-field.  */
 
 static bool
 type_consists_of_records_p (tree type)
 {
   tree fld;
-  bool last_fld_has_zero_size = false;
 
   if (TREE_CODE (type) != RECORD_TYPE)
     return false;
@@ -838,12 +836,7 @@ type_consists_of_records_p (tree type)
 	if (!is_gimple_reg_type (ft)
 	    && !type_consists_of_records_p (ft))
 	  return false;
-
-	last_fld_has_zero_size = tree_low_cst (DECL_SIZE (fld), 1) == 0;
       }
-
-  if (last_fld_has_zero_size)
-    return false;
 
   return true;
 }
