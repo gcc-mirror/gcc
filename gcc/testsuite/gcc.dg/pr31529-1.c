2d3419d4e094e58c4b36099dc1d876ec34906fdc
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-09-30 12:22:33 +0000

    2010-09-30  Richard Guenther  <rguenther@suse.de>
    	PR testsuite/45702
    	* gcc.dg/debug/pr41893-1.c: Adjust.
    	* gcc.dg/pr30762-1.c: Likewise.
    	* gcc.dg/pr31529-1.c: Likewise.
    	* gcc.dg/pr34457-1.c: Likewise.
    	* gcc.dg/pr34668-1.c: Likewise.
    	* gcc.dg/pr43557-1.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@164749 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr31529-1.c
+++ gcc/testsuite/gcc.dg/pr31529-1.c
@@ -1,8 +1,10 @@
 /* { dg-do link } */
 /* { dg-require-effective-target lto } */
-/* { dg-options "-flto -r -nostdlib" } */
+/* { dg-options "-flto" } */
 /* { dg-additional-sources "pr31529-2.c" } */
 
 getline ()
 {
 }
+
+int main() { return 0; }
