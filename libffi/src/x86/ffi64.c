2c79b197dc7492a9e0b980cad9350586667461a3
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-10-01 07:31:49 +0000

    PR libffi/45677 	* src/x86/ffi64.c (ffi_prep_cif_machdep): Ensure cif->bytes is 	a multiple of 8. 	* testsuite/libffi.call/many2.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@164829 138bc75d-0d04-0410-961f-82ee72b054a4

--- libffi/src/x86/ffi64.c
+++ libffi/src/x86/ffi64.c
@@ -378,7 +378,7 @@ ffi_prep_cif_machdep (ffi_cif *cif)
 	  if (align < 8)
 	    align = 8;
 
-	  bytes = ALIGN(bytes, align);
+	  bytes = ALIGN (bytes, align);
 	  bytes += cif->arg_types[i]->size;
 	}
       else
@@ -390,7 +390,7 @@ ffi_prep_cif_machdep (ffi_cif *cif)
   if (ssecount)
     flags |= 1 << 11;
   cif->flags = flags;
-  cif->bytes = bytes;
+  cif->bytes = ALIGN (bytes, 8);
 
   return FFI_OK;
 }
