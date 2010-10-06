835e1464c1ebe3b70b1546928c2c51fb64f51ab6
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-10-06 17:17:16 +0000

    2010-10-06  Paolo Carlini  <paolo.carlini@oracle.com>
    	* include/std/functional (struct _Weak_result_type_impl): Add
    	specializazions for cv-qualified function types, for variadic
    	function types and pointers and references to function types,
    	and for pointers to member function types.
    	(struct _Reference_wrapper_base): Add specializations for
    	cv-qualified unary and binary function types.
    	* testsuite/20_util/reference_wrapper/result_type.cc: New.
    	* testsuite/20_util/reference_wrapper/typedefs-2.cc: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@165056 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/20_util/reference_wrapper/result_type.cc
@@ -0,0 +1,42 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-do compile }
+
+// 2010-10-06  Paolo Carlini  <paolo.carlini@oracle.com>
+
+// Copyright (C) 2010 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <functional>
+
+using namespace std;
+
+struct T;
+
+reference_wrapper<int(float, ...)>::result_type                       i01;
+reference_wrapper<int(float, ...) const>::result_type                 i02;
+reference_wrapper<int(float, ...) volatile>::result_type              i03;
+reference_wrapper<int(float, ...) const volatile>::result_type        i04;
+
+reference_wrapper<int(*)(float, ...)>::result_type                    i05;
+reference_wrapper<int(* const)(float, ...)>::result_type              i06;
+reference_wrapper<int(* volatile)(float, ...)>::result_type           i07;
+reference_wrapper<int(* const volatile)(float, ...)>::result_type     i08;
+
+reference_wrapper<int(T::*)(float, ...)>::result_type                 i09;
+reference_wrapper<int(T::*)(float, ...) const>::result_type           i10;
+reference_wrapper<int(T::*)(float, ...) volatile>::result_type        i11;
+reference_wrapper<int(T::*)(float, ...) const volatile>::result_type  i12;
