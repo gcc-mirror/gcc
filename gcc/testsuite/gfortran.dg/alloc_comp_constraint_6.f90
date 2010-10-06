6619e385fced24c8ed80fcdaf991c1cff7c12340
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2010-10-06 22:49:28 +0000

    re PR fortran/45889 (Regression with I/O of element of allocatable array in derived type)
    2010-10-06  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR fortran/45889
    	* gfortran.dg/alloc_comp_constraint_6.f90: New test.
    
    From-SVN: r165069

---
+++ gcc/testsuite/gfortran.dg/alloc_comp_constraint_6.f90
@@ -0,0 +1,21 @@
+! { dg-do compile )
+! PR45889 Regression with I/O of element of allocatable array in derived type 
+module cell
+  implicit none
+  private 
+  type, public:: unit_cell
+     integer                                             ::num_species
+     character(len=8), dimension(:),          allocatable::species_symbol
+  end type unit_cell
+  type(unit_cell),                            public, save::current_cell 
+  contains
+  subroutine cell_output
+    implicit none
+    integer::i
+    do i=1,current_cell%num_species
+       write(*,*)(current_cell%species_symbol(i))
+    end do
+    return
+  end subroutine cell_output
+end module cell
+! { dg-final { cleanup-modules "cell" } }
