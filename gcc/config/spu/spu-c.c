ea130120a157893d06b90b8d4cec06a5af3c90ff
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-10-06 20:10:43 +0000

    * config/spu/spu-c.c (spu_resolve_overloaded_builtin): Don't use 	define_builtin_std where it's unnecessary. 	* config/i386/nto.h (TARGET_OS_CPP_BUILTINS): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@165065 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/spu/spu-c.c
+++ gcc/config/spu/spu-c.c
@@ -188,19 +188,19 @@ spu_resolve_overloaded_builtin (location_t loc, tree fndecl, void *passed_args)
 void
 spu_cpu_cpp_builtins (struct cpp_reader *pfile)
 {
-  builtin_define_std ("__SPU__");
+  cpp_define (pfile, "__SPU__");
   cpp_assert (pfile, "cpu=spu");
   cpp_assert (pfile, "machine=spu");
   if (spu_arch == PROCESSOR_CELLEDP)
-    builtin_define_std ("__SPU_EDP__");
-  builtin_define_std ("__vector=__attribute__((__spu_vector__))");
+    cpp_define (pfile, "__SPU_EDP__");
+  cpp_define (pfile, "__vector=__attribute__((__spu_vector__))");
   switch (spu_ea_model)
     {
     case 32:
-      builtin_define_std ("__EA32__");
+      cpp_define (pfile, "__EA32__");
       break;
     case 64:
-      builtin_define_std ("__EA64__");
+      cpp_define (pfile, "__EA64__");
       break;
     default:
        gcc_unreachable ();
