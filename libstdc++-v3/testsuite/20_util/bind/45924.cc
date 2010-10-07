8aad4afa9f22489b8135e5b4d6f87ace3b78ee52
Jonathan Wakely <jwakely.gcc@gmail.com>
2010-10-07 00:39:23 +0000

    re PR libstdc++/45924 ([C++0x] bind<R>(x)(y, z) fails)
    2010-10-07  Jonathan Wakely  <jwakely.gcc@gmail.com>
    
    	PR libstdc++/45924
    	* include/std/functional (_Bind_result::operator()): Do not expand
    	template parameter pack in forward call.
    	* testsuite/20_util/bind/45924.cc: New.
    
    From-SVN: r165076

---
+++ libstdc++-v3/testsuite/20_util/bind/45924.cc
@@ -0,0 +1,45 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-do compile }
+
+// Copyright (C) 2010 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <functional>
+
+// PR libstdc++/45924
+
+struct f
+{
+  int operator()(int, int) const { return 0; }
+};
+
+void test01()
+{
+  int i = 0;
+  using namespace std::placeholders;
+  auto b = std::bind<int>(f(), _1, _2);
+  auto const bc(b);
+  b(i, i);
+  bc(i, i);
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
+
