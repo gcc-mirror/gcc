9adafb9fd1f50bbb699c58dda439b4293737c094
Nathan Froyd <froydnj@codesourcery.com>
2010-10-08 13:29:29 +0000

    sparc-protos.h (function_arg_advance, [...]): Delete.
    	* config/sparc/sparc-protos.h (function_arg_advance, function-arg):
    	Delete.
    	* config/sparc/sparc.h (FUNCTION_ARG_ADVANCE, FUNCTION_ARG): Delete.
    	(FUNCTION_INCOMING_ARG): Delete.
    	* config/sparc/sparc.c (scan_record_type): Const-ify tree argument.
    	(function_arg_slotno): Likewise.  Take bool arguments.
    	(sparc_arg_partial_bytes): Update call to it.
    	(function_arg): Rename to...
    	(sparc_function_arg_1): ...this.  Take bool arguments.
    	(sparc_function_arg, sparc_function_incoming_arg): New functions.
    	(function_arg_advance): Rename to...
    	(sparc_function_arg_advance): ...this.  Take bool argument.
    	(TARGET_FUNCTION_ARG_ADVANCE, TARGET_FUNCTION_ARG): Define.
    	(TARGET_FUNCTION_INCOMING_ARG): Define.
    
    From-SVN: r165183

--- gcc/config/sparc/sparc.h
+++ gcc/config/sparc/sparc.h
@@ -1444,35 +1444,6 @@ struct sparc_args {
 #define INIT_CUMULATIVE_ARGS(CUM, FNTYPE, LIBNAME, FNDECL, N_NAMED_ARGS) \
 init_cumulative_args (& (CUM), (FNTYPE), (LIBNAME), (FNDECL));
 
-/* Update the data in CUM to advance over an argument
-   of mode MODE and data type TYPE.
-   TYPE is null for libcalls where that information may not be available.  */
-
-#define FUNCTION_ARG_ADVANCE(CUM, MODE, TYPE, NAMED) \
-function_arg_advance (& (CUM), (MODE), (TYPE), (NAMED))
-
-/* Determine where to put an argument to a function.
-   Value is zero to push the argument on the stack,
-   or a hard register in which to store the argument.
-
-   MODE is the argument's machine mode.
-   TYPE is the data type of the argument (as a tree).
-    This is null for libcalls where that information may
-    not be available.
-   CUM is a variable of type CUMULATIVE_ARGS which gives info about
-    the preceding args and about the function being called.
-   NAMED is nonzero if this argument is a named parameter
-    (otherwise it is an extra parameter matching an ellipsis).  */
-
-#define FUNCTION_ARG(CUM, MODE, TYPE, NAMED) \
-function_arg (& (CUM), (MODE), (TYPE), (NAMED), 0)
-
-/* Define where a function finds its arguments.
-   This is different from FUNCTION_ARG because of register windows.  */
-
-#define FUNCTION_INCOMING_ARG(CUM, MODE, TYPE, NAMED) \
-function_arg (& (CUM), (MODE), (TYPE), (NAMED), 1)
-
 /* If defined, a C expression which determines whether, and in which direction,
    to pad out an argument with extra space.  The value should be of type
    `enum direction': either `upward' to pad above the argument,
