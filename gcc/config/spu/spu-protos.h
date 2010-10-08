ee9034d42b22394b81ca54766c252c1efd72913d
froydnj <froydnj@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-10-08 13:16:43 +0000

    * config/spu/spu-protos.h (spu_function_arg): Delete. 	* config/spu/spu.h (FUNCTION_ARG): Delete. 	(FUNCTION_ARG_ADVANCE): Move code to ... 	* config/spu/spu.c (spu_function_arg_advance): New function. 	(spu_function_arg): Make static.  Take a const_tree and a bool. 	(spu_setup_incoming_varargs): Call spu_function_arg_advance. 	(TARGET_FUNCTION_ARG, TARGET_FUNCTION_ARG_ADVANCE): Define.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@165179 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/spu/spu-protos.h
+++ gcc/config/spu/spu-protos.h
@@ -55,8 +55,6 @@ extern int spu_constant_address_p (rtx x);
 extern int spu_legitimate_constant_p (rtx x);
 extern int spu_initial_elimination_offset (int from, int to);
 extern rtx spu_function_value (const_tree type, const_tree func);
-extern rtx spu_function_arg (int cum, enum machine_mode mode, tree type,
-			     int named);
 extern void spu_setup_incoming_varargs (int *cum, enum machine_mode mode,
 					tree type, int *pretend_size,
 					int no_rtl);
