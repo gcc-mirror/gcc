678451e45df34d26de6b7aaf7209cc7ef28fb4bb
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-10-09 05:34:10 +0000

    Support "andhi/andsi/anddi" as a zero-extending move.
    gcc/
    
    2010-10-08  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/45913
    	* config/i386/i386.c (ix86_binary_operator_ok): Support
    	"andhi/andsi/anddi" as a zero-extending move.
    
    gcc/testsuite/
    
    2010-10-08  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/45913
    	* gcc.target/i386/pr45913.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@165215 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -14987,7 +14987,16 @@ ix86_binary_operator_ok (enum rtx_code code, enum machine_mode mode,
 
   /* Source 1 cannot be a non-matching memory.  */
   if (MEM_P (src1) && !rtx_equal_p (dst, src1))
-    return false;
+    {
+      /* Support "andhi/andsi/anddi" as a zero-extending move.  */
+      return (code == AND
+	      && (mode == HImode
+		  || mode == SImode
+		  || (TARGET_64BIT && mode == DImode))
+	      && CONST_INT_P (src2)
+	      && (INTVAL (src2) == 0xff
+		  || INTVAL (src2) == 0xffff));
+    }
 
   return true;
 }
