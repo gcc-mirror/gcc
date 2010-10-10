6ea7f14af8bb3c974c544c6c905a087452b79470
Kai Tietz <kai.tietz@onevision.com>
2010-10-10 21:25:09 +0200

    2010-10.10  Kai Tietz  <kai.tietz@onevision.com>
    	* g++.dg/ext/dllexport-MI1.C: Enable for x86_64 mingw
    	and adjust -export symbol scanning.
    	* g++.dg/ext/dllexport1.C: Likewise.
    	* g++.dg/ext/dllexport3.C: Likewise.
    	* gcc.dg/dll-3.c: Likewise.
    	* gcc.dg/dll-4.c: Likewise.
    	* gcc.dg/dll-5.c: Likewise.
    
    From-SVN: r165261

--- gcc/testsuite/gcc.dg/dll-3.c
+++ gcc/testsuite/gcc.dg/dll-3.c
@@ -2,7 +2,7 @@
 
 /* { dg-do compile { target arm*-*-pe* } } */
 /* { dg-do compile { target i?86-pc-cygwin } } */
-/* { dg-do compile { target i?86-pc-mingw* } } */
+/* { dg-do compile { target i?86-*-mingw* x86_64-*-mingw*} } */
 
 __declspec (dllimport) int foo1 ();
 __declspec (dllexport) int foo1 ();	/* { dg-warning "previous dllimport ignored" } */
@@ -13,6 +13,6 @@ __declspec (dllimport) int foo2 ();	/* { dg-warning "dllimport ignored" } */
 __declspec (dllexport) int foo1 () { return foo2 (); }
 __declspec (dllexport) int foo2 () { return foo1 (); }
 
-/* { dg-final { scan-assembler "\.section\[ \t\]*.drectve\n.*-export:foo2" } } */
-/* { dg-final { scan-assembler "-export:foo1" } } */
+/* { dg-final { scan-assembler "\.section\[ \t\]*.drectve\n.*-export:\[\\\\\"\]*foo2" } } */
+/* { dg-final { scan-assembler "-export:\[\\\\\"\]*foo1" } } */
 /* { dg-final { scan-assembler-not "(__imp_foo1|_imp__foo1|__imp_foo2|_imp__foo2)" } } */
