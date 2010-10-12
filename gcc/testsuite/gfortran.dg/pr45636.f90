f46842422035db1b85c4aaa550dc811110ac937d
Jakub Jelinek <jakub@redhat.com>
2010-10-13 00:01:04 +0200

    re PR fortran/45636 (Failed to fold simple Fortran string)
    	PR fortran/45636
    	* tree-ssa-forwprop.c: Include expr.h.
    	(constant_pointer_difference, simplify_builtin_call): New functions.
    	(tree_ssa_forward_propagate_single_use_vars): Call
    	simplify_builtin_call on builtin calls.
    
    	* gcc.c-torture/execute/pr45636.c: New test.
    	* gfortran.dg/pr45636.f90: New test.
    
    From-SVN: r165401

---
+++ gcc/testsuite/gfortran.dg/pr45636.f90
@@ -0,0 +1,14 @@
+! PR fortran/45636
+! { dg-do compile }
+! { dg-options "-O2 -fdump-tree-forwprop2" }
+! PR 45636 - make sure no memset is needed for a short right-hand side.
+program main
+  character(len=2), parameter :: x='a '
+  character(len=1), parameter :: y='b'
+  character(len=4) :: a, b
+  a = x
+  b = y
+  call sub(a, b)
+end program main
+! { dg-final { scan-tree-dump-times "memset" 0 "forwprop2" } }
+! { dg-final { cleanup-tree-dump "forwprop2" } }
