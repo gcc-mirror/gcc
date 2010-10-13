ab450cc6fe4fd6a88ad6675000672ac66646bae8
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-10-13 13:03:31 +0000

    2010-10-13  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/45970
    	* tree-ssa-alias.h (stmt_kills_ref_p): Declare.
    	* tree-ssa-alias.c (stmt_kills_ref_p_1): New function.
    	(stmt_kills_ref_p): Likewise.
    	* tree-ssa-dse.c (dse_optimize_stmt): Use it.
    
    	* gcc.dg/tree-ssa/ssa-dse-13.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@165422 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-alias.h
+++ gcc/tree-ssa-alias.h
@@ -105,6 +105,7 @@ extern bool ref_maybe_used_by_stmt_p (gimple, tree);
 extern bool stmt_may_clobber_ref_p (gimple, tree);
 extern bool stmt_may_clobber_ref_p_1 (gimple, ao_ref *);
 extern bool call_may_clobber_ref_p (gimple, tree);
+extern bool stmt_kills_ref_p (gimple, tree);
 extern tree get_continuation_for_phi (gimple, ao_ref *, bitmap *);
 extern void *walk_non_aliased_vuses (ao_ref *, tree,
 				     void *(*)(ao_ref *, tree, void *),
