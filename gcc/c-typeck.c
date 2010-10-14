8f09564f3f7de1ee149148471c8fd7feb58de28d
Joseph Myers <joseph@codesourcery.com>
2010-10-14 23:41:05 +0100

    re PR c/45969 (ICE in build_binary_op, at c-typeck.c:9833)
    	PR c/45969
    	* c-typeck.c (build_binary_op): Don't try to compute a semantic
    	type with excess precision for boolean operations.
    
    testsuite:
    	* gcc.c-torture/compile/pr45969-1.c: New test.
    
    From-SVN: r165485

--- gcc/c-typeck.c
+++ gcc/c-typeck.c
@@ -9091,6 +9091,10 @@ build_binary_op (location_t location, enum tree_code code,
      precision.  */
   bool may_need_excess_precision;
 
+  /* True means this is a boolean operation that converts both its
+     operands to truth-values.  */
+  bool boolean_op = false;
+
   if (location == UNKNOWN_LOCATION)
     location = input_location;
 
@@ -9318,6 +9322,7 @@ build_binary_op (location_t location, enum tree_code code,
 	  op0 = c_common_truthvalue_conversion (location, op0);
 	  op1 = c_common_truthvalue_conversion (location, op1);
 	  converted = 1;
+	  boolean_op = true;
 	}
       if (code == TRUTH_ANDIF_EXPR)
 	{
@@ -9828,7 +9833,8 @@ build_binary_op (location_t location, enum tree_code code,
   if (build_type == NULL_TREE)
     {
       build_type = result_type;
-      if (type0 != orig_type0 || type1 != orig_type1)
+      if ((type0 != orig_type0 || type1 != orig_type1)
+	  && !boolean_op)
 	{
 	  gcc_assert (may_need_excess_precision && common);
 	  semantic_result_type = c_common_type (orig_type0, orig_type1);
