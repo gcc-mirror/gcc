0d16f46289b7dba6d45325c83609f48b18749204
H.J. Lu <hongjiu.lu@intel.com>
2010-10-14 02:42:50 +0000

    Add more 256bit AVX testcases.
    2010-10-13  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* gcc.target/i386/recip-vec-sqrtf-avx.c: New.
    	* gcc.target/i386/recip-vec-divf-avx.c: Likewise.
    	* gcc.target/i386/vectorize4-avx.c: Likewise.
    
    From-SVN: r165449

---
+++ gcc/testsuite/gcc.target/i386/recip-vec-divf-avx.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -ffast-math -ftree-vectorize -mavx -mfpmath=sse -mrecip" } */
+
+float a[16];
+float b[16];
+float r[16];
+
+void t1(void)
+{
+ int i;
+
+ for (i = 0; i < 16; i++)
+   r[i] = a[i] / b[i];
+}
+
+/* { dg-final { scan-assembler "vrcpps" } } */
