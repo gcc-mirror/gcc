587e91d04ccaa2e2359e2362cf71e57a5172bcd9
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-10-14 02:42:50 +0000

    Add more 256bit AVX testcases.
    2010-10-13  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* gcc.target/i386/recip-vec-sqrtf-avx.c: New.
    	* gcc.target/i386/recip-vec-divf-avx.c: Likewise.
    	* gcc.target/i386/vectorize4-avx.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@165449 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/recip-vec-sqrtf-avx.c
@@ -0,0 +1,34 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -ffast-math -ftree-vectorize -mavx -mfpmath=sse -mrecip" } */
+
+float a[16];
+float b[16];
+float r[16];
+
+extern float sqrtf (float);
+
+void t1(void)
+{
+ int i;
+
+ for (i = 0; i < 16; i++)
+   r[i] = a[i] / sqrtf (b[i]);
+}
+
+void t2(void)
+{
+ int i;
+
+ for (i = 0; i < 16; i++)
+   r[i] = sqrtf (a[i] / b[i]);
+}
+
+void t3(void)
+{
+ int i;
+
+ for (i = 0; i < 16; i++)
+   r[i] = sqrtf (a[i]);
+}
+
+/* { dg-final { scan-assembler-times "vrsqrtps" 3 } } */
