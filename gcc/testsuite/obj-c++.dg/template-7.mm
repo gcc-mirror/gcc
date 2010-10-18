a1176db975a2a05d65e0277dd812d4e6844814b2
nicola <nicola@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-10-18 18:54:01 +0000

    In gcc/objc/: 2010-10-18  Nicola Pero  <nicola.pero@meta-innovation.com>
            Merge from 'apple/trunk' branch on FSF servers.
    
            2006-03-10  Fariborz Jahanian <fjahanian@apple.com>
    
            Radar 4407151
            * objc/objc-act.c (objc_is_class_name): template parameter is not
            an objective class name.
            (objc_generate_cxx_cdtors): Check for the null
            objc_implementation_context.
    
    In gcc/testsuite/:
    2010-10-18  Nicola Pero  <nicola.pero@meta-innovation.com>
    
            Merge from 'apple/trunk' branch on FSF servers.
    
            2006-01-17  Fariborz Jahanian <fjahanian@apple.com>
    
            Radar 4407151
            * obj-c++.dg/template-7.mm: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@165657 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/obj-c++.dg/template-7.mm
@@ -0,0 +1,21 @@
+// Test that objective-c++ does not confuse a template parameter named 'Object'
+// with an interface of the same name.
+// Author: Fariborz Jahanian <fjahanian@apple.com>
+// { dg-do compile }
+// { dg-options "" }
+typedef struct objc_class *Class;
+
+@interface Object
+{
+ Class isa;
+}
+@end
+
+template <class Object>
+struct pyobject_type
+{ 
+    static Object* checked_downcast(Object* x)
+    {
+        return x;
+    }
+};
