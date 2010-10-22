f71c8090985b6add42a9823b3d0c9039e99cb7b5
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-10-22 18:37:41 +0000

    PR c++/46103 	* init.c (build_vec_init): Handle memberwise move.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@165849 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/implicit10.C
@@ -0,0 +1,19 @@
+// PR c++/46103
+// { dg-options -std=c++0x }
+
+struct MoveOnly {
+  MoveOnly(const MoveOnly&) = delete;
+  MoveOnly(MoveOnly&&) { }
+  MoveOnly() = default;
+};
+
+struct A {
+  MoveOnly mo[1];
+  A() = default;
+  A(A&&) = default;
+};
+
+int main() {
+  A a;
+  A aa = static_cast<A&&>(a);
+}
