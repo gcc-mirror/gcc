76d8a30aaf0a266709be00a9e01148e60d803f1e
Ian Lance Taylor <iant@google.com>
2010-10-26 13:39:37 +0000

    re PR middle-end/45687 (possible wrong code bug)
    gcc/:
    	PR middle-end/45687
    	* ipa-prop.c (ipa_modify_call_arguments): Correct type of MEM_REF
    	offset.
    gcc/testsuite:
    	* gcc.c-torture/execute/20101025-1.c: New test.
    
    From-SVN: r165964

---
+++ gcc/testsuite/gcc.c-torture/execute/20101025-1.c
@@ -0,0 +1,30 @@
+static int g_7;
+static int *volatile g_6 = &g_7;
+int g_3;
+
+static int f1 (int *p_58)
+{
+    return *p_58;
+}
+
+void f2 (int i) __attribute__ ((noinline));
+void f2 (int i)
+{
+  g_3 = i;
+}
+
+int f3 (void) __attribute__ ((noinline));
+int f3 (void)
+{
+    *g_6 = 1;
+    f2 (f1 (&g_7));
+    return 0;
+}
+
+int main ()
+{
+  f3 ();
+  if (g_3 != 1)
+    abort ();
+  exit (0);
+}
