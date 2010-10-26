519b62acd8ceb95c762dc128bb2b2fac7805cdd6
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-10-26 13:56:42 +0000

    Properly align parameters on stack for x86.
    gcc/
    
    2010-10-26  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/44948
    	* config/i386/i386.c (ix86_old_function_arg_boundary): New.
    	(ix86_function_arg_boundary): Always align parameters on stack
    	in 64bit and align parameters with alignment >= 16byte on stack 
    	in 32bit.  Warn alignment change.
    
    gcc/testsuite/
    
    2010-10-26  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/44948
    	* g++.dg/abi/param2.C: Add -Wno-psabi for ilp32 x86.
    	* g++.dg/vect/pr33860a.cc: Likewise.
    	* gcc.c-torture/compile/20070522-1.c: Likewise.
    	* gcc.dg/compat/struct-align-1_x.c: Likewise.
    	* gcc.dg/compat/struct-align-1_y.c: Likewise.
    	* gcc.dg/compat/struct-align-2_x.c: Likewise.
    	* gcc.dg/compat/struct-align-2_y.c: Likewise.
    	* gcc.dg/pr44136.c: Likewise.
    
    	* gcc.c-torture/compile/pr33617.c: Add -Wno-psabi for x86.
    	* gcc.dg/compat/vector-1_x.c: Likewise.
    	* gcc.dg/compat/vector-1_y.c: Likewise.
    	* gcc.dg/compat/vector-2_x.c: Likewise.
    	* gcc.dg/compat/vector-2_y.c: Likewise.
    	* gcc.dg/pr43300.c: Likewise.
    
    	* gcc.dg/compat/vector-1a_x.c: Add -Wno-psabi.
    	* gcc.dg/compat/vector-1a_y.c: Likewise.
    	* gcc.dg/compat/vector-1b_x.c: Likewise.
    	* gcc.dg/compat/vector-1b_y.c: Likewise.
    	* gcc.dg/compat/vector-2a_x.c: Likewise.
    	* gcc.dg/compat/vector-2a_y.c: Likewise.
    	* gcc.dg/compat/vector-2b_x.c: Likewise.
    	* gcc.dg/compat/vector-2b_y.c: Likewise.
    	* gcc.target/i386/pr39162.c: Likewise.
    	* gcc.target/i386/pr40906-2.c: Likewise.
    	* gcc.target/i386/sse-5.c: Likewise.
    
    	* gcc.dg/pr35442.c: Prune ABI change warnings.
    
    	* gcc.c-torture/execute/pr38151.x: New.
    	* gcc.target/i386/pr44948-1a.c: Likewise.
    	* gcc.target/i386/pr44948-1b.c: Likewise.
    	* gcc.target/i386/pr44948-2a.c: Likewise.
    	* gcc.target/i386/pr44948-2b.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@165965 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/pr39162.c
+++ gcc/testsuite/gcc.target/i386/pr39162.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -msse2 -mno-avx" } */
+/* { dg-options "-O2 -Wno-psabi -msse2 -mno-avx" } */
 
 typedef long long __m256i __attribute__ ((__vector_size__ (32), __may_alias__));
 
