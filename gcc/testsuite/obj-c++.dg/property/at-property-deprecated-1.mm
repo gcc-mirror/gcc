cb92539e4e1429271eeeb6e1e709edf86c9ced68
nicola <nicola@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-01 21:12:12 +0000

    In gcc/objc/: 2010-11-01  Nicola Pero  <nicola.pero@meta-innovation.com>
            * objc-act.c (objc_maybe_build_component_ref): Warn about using
            deprecated properties.
            (objc_maybe_printable_name): Support PROPERTY_DECL.
            
    In gcc/testsuite/:
    2010-11-01  Nicola Pero  <nicola.pero@meta-innovation.com>
    
            * objc.dg/property/at-property-deprecated-1.m: New.
            * obj-c++.dg/property/at-property-deprecated-1.mm: New. 
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@166147 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/obj-c++.dg/property/at-property-deprecated-1.mm
@@ -0,0 +1,37 @@
+/* Contributed by Nicola Pero <nicola.pero@meta-innovation.com>, October 2010.  */
+/* { dg-do compile } */
+
+/* Test that properties can be deprecated.  */
+
+#include <stdlib.h>
+#include <objc/objc.h>
+#include <objc/runtime.h>
+
+@interface MyRootClass
+{
+  Class isa;
+  int a;
+}
+@property int a __attribute__((deprecated));
++ (id) initialize;
++ (id) alloc;
+- (id) init;
+@end
+
+@implementation MyRootClass
++ (id) initialize { return self; }
++ (id) alloc { return class_createInstance (self, 0); }
+- (id) init { return self; }
+@synthesize a;
+@end
+
+int main (void)
+{
+  MyRootClass *object = [[MyRootClass alloc] init];
+
+  object.a = 40;      /* { dg-warning ".a. is deprecated .declared at " } */
+  if (object.a != 40) /* { dg-warning ".a. is deprecated .declared at " } */
+    abort ();
+
+  return (0);
+}
