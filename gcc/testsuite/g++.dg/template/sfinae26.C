574e2ba2eec1097f3decd0ade984097e425a4d7f
Dodji Seketeli <dodji@redhat.com>
2010-11-02 12:58:48 +0000

    Fix PR c++/46170, c++/46162
    gcc/cp/ChangeLog:
    	PR c++/46170
    	PR c++/46162
    	* pt.c (check_valid_ptrmem_cst_expr): Add a complain parameter to
    	control diagnostic.
    	(convert_nontype_argument, convert_nontype_argument): Pass the
    	complain parameter down to check_valid_ptrmem_cst_expr.
    
    gcc/testsuite/ChangeLog:
    	PR c++/46170
    	PR c++/46162
    	* g++.dg/template/sfinae26.C: New test.
    	* g++.dg/template/sfinae27.C: Likewise.
    
    From-SVN: r166181

---
+++ gcc/testsuite/g++.dg/template/sfinae26.C
@@ -0,0 +1,34 @@
+// Origin: PR c++/46170
+// { dg-do "compile" }
+
+namespace util {
+  struct option_value {
+  };
+  template <class T> struct options_map_impl {
+    typedef T options_struct_type;
+    typedef bool (*opt_func)(const option_value&, options_struct_type&);
+    template <class V, V K>  static  bool  set_member_constant(const option_value&,
+							       options_struct_type&, V options_struct_type::*);
+    template <class V, V options_struct_type::*mem, V K>  static  bool 
+    set_member_constant(const option_value& opt, options_struct_type& t) {
+      return set_member_constant<V,K>(opt, t, mem);
+    }
+  };
+}
+struct cflat_options {
+  bool show_precharges;
+};
+typedef util::options_map_impl<cflat_options> options_map_impl_type;
+class register_options_modifier {
+  typedef options_map_impl_type::opt_func modifier_type;
+public:  register_options_modifier();
+  register_options_modifier(const char* Mode,    const modifier_type COM,   
+			    const char* h);
+};
+static const register_options_modifier
+cflat_opt_mod_show_precharges("precharges",
+			      &options_map_impl_type::set_member_constant<bool,
+									  &cflat_options::show_precharges, true>, "show precharge expressions"),
+  cflat_opt_mod_no_show_precharges("no-" "precharges",
+				   &options_map_impl_type::set_member_constant<bool,
+									       &cflat_options::show_precharges, false>, "hide precharge expressions");
