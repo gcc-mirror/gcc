900484dee595d91a71c7ae63734c199bd137afc6
Jason Merrill <jason@redhat.com>
2010-11-01 21:31:50 -0400

    initializer_list: Decorate with constexpr.
    	* libsupc++/initializer_list: Decorate with constexpr.
    
    Co-Authored-By: Benjamin Kosnik <bkoz@redhat.com>
    
    From-SVN: r166170

---
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-initlist2.C
@@ -0,0 +1,10 @@
+// { dg-options -std=c++0x }
+
+#include <initializer_list>
+
+constexpr auto list = { 1, 2, 3, 4 };
+
+#define SA(X) static_assert(X, #X)
+SA(list.size() == 4);
+SA(list.begin()[2] == 3);
+SA(list.end()[-1] == 4);
