b7fa98e27bb48a44ad2a29e471653a56bdbee831
Richard Guenther <rguenther@suse.de>
2010-11-03 15:55:34 +0000

    re PR bootstrap/44807 (bootstrap failure on i686 with BOOT_CFLAGS='-O3')
    2010-11-03  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/44807
    	* gcc.dg/torture/pr44807.c: New testcase.
    
    From-SVN: r166256

---
+++ gcc/testsuite/gcc.dg/torture/pr44807.c
@@ -0,0 +1,40 @@
+/* { dg-do compile } */
+
+struct jobstats
+{
+  int j_jobslots;
+};
+struct jobstats js;
+int *jobs;
+
+typedef int sh_job_map_func_t (int *, int, int, int);
+
+static void
+map_over_jobs (sh_job_map_func_t func)
+{
+  int i;
+  int set, oset;
+  if (js.j_jobslots)
+    return;
+  sigemptyset (&set);
+  sigaddset (set, 17);
+  sigemptyset (&oset);
+  sigprocmask (0, set, oset);
+  for (i = 0; js.j_jobslots; i++)
+    if (jobs[i])
+      func (jobs, 0, 0, 0);
+  sigprocmask (oset, ((void *) 0));
+}
+
+int
+print_job (int *job, int format, int state, int job_index)
+{
+  map_over_jobs (print_job);
+}
+
+void
+list_running_jobs (void)
+{
+  map_over_jobs (print_job);
+}
+
