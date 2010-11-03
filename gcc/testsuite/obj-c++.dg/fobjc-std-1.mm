1ef143b60593f2ea5cc4096ce679c6a35fa1b338
nicola <nicola@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-03 13:39:39 +0000

    In gcc/: 2010-11-03  Nicola Pero  <nicola.pero@meta-innovation.com>
            Implemented -fobjc-std=objc1 flag.
            * doc/invoke.texi (Option Summary): Document -fobjc-std=objc1
            option.
            * doc/standards.texi (Standards): Updated the section on
            Objective-C; mention support for Objective-C 2.0 features and the
            new -fstd-objc=objc1 flag to disable them.
    
    In gcc/c-family/:
    2010-11-03  Nicola Pero  <nicola.pero@meta-innovation.com>
    
            Implemented -fobjc-std=objc1 flag.
            * c.opt (fobjc-std=objc1): New option.
    
    In gcc/objc/:
    2010-11-03  Nicola Pero  <nicola.pero@meta-innovation.com>
    
            Implemented -fobjc-std=objc1 flag.
            * objc-act.c (objc_start_class_interface): If attributes are
            specified when flag_objc1_only is set, print an error.
            (objc_start_category_interface): Same change.
            (objc_start_protocol): Same change.
            (objc_add_method_declaration): Same change.
            (objc_start_method_definition): Same change.
            (objc_build_keyword_decl): Same change.
            (objc_set_method_opt): If flag_objc1_only is set, print an error.
            (objc_add_property_declaration): Same change.
            (objc_add_synthesize_declaration): Same change.
            (objc_add_dynamic_declaration): Same change.
            (objc_finish_foreach_loop): Same change.
            (objc_maybe_build_component_ref): If flag_objc1_only is set,
            return immediately.
    
    In gcc/testsuite/:
    2010-11-03  Nicola Pero  <nicola.pero@meta-innovation.com>
    
            Implemented -fobjc-std=objc1 flag.
            * objc.dg/fobjc-std-1.m: New.
            * obj-c++.dg/fobjc-std-1.mm: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@166246 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/obj-c++.dg/fobjc-std-1.mm
@@ -0,0 +1,55 @@
+/* Test warnings when using -fobjc-std=objc1.  */
+/* { dg-do compile } */
+/* { dg-options "-fobjc-std=objc1" } */
+
+#include <objc/objc.h>
+
+__attribute__ ((deprecated))
+@interface MyRootClass /* { dg-error "class attributes are not available in Objective.C 1.0" } */
+{
+  Class isa; /* { dg-error ".@package. is not available in Objective.C 1.0" } */
+@package
+  int a;
+  int b;
+}
++ (id) alloc __attribute__ ((deprecated)); /* { dg-error "not available in Objective.C 1.0" } */
+- (id) init;
+- (id) testMe: (id) __attribute__((unused)) argument; /* { dg-error "not available in Objective.C 1.0" } */
+@property (nonatomic) int a; /* { dg-error "not available in Objective.C 1.0" } */
+@property (nonatomic) int b; /* { dg-error "not available in Objective.C 1.0" } */
+@end
+
+@implementation MyRootClass
++ (id) alloc { return self; }
+- (id) init  { return self; }
+- (id) testMe: (id) __attribute__((unused)) argument { return self; } /* { dg-error "not available in Objective.C 1.0" } */
+@synthesize a; /* { dg-error "not available in Objective.C 1.0" } */
+@dynamic b; /* { dg-error "not available in Objective.C 1.0" } */
+@end
+
+__attribute__ ((deprecated))
+@protocol MyProtocol /* { dg-error "protocol attributes are not available in Objective.C 1.0" } */
+
+@required /* { dg-error "not available in Objective.C 1.0" } */
+- (id) variable __attribute__ ((deprecated)); /* { dg-error "not available in Objective.C 1.0" } */
+@optional /* { dg-error "not available in Objective.C 1.0" } */
+@end
+#if 0 /* fast enumeration is not implemented even in Objective-C 2.0 */
+@interface MyRootClass (NSFastEnumeration)
+- (unsigned long)countByEnumeratingWithState: (struct __objcFastEnumerationState *)state
+                                     objects:(id *)stackbuf 
+                                       count:(unsigned int)len;
+@end
+
+@class NSArray;
+
+int array_length (NSArray *array)
+{
+  int i = 0;
+
+  for (id object in array) /*  dg-error "not available in Objective.C 1.0"  */
+    i++;
+      
+  return i;
+}
+#endif
