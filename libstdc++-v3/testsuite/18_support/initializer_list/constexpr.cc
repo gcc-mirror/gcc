0e6ac87ebaf95910b2a816aaddc80ed9f0d00fb4
Benjamin Kosnik <bkoz@redhat.com>
2010-11-04 05:53:05 +0000

    tuple (tuple): Make default constructors constexpr.
    2010-11-03  Benjamin Kosnik  <bkoz@redhat.com>
    
    	* include/std/tuple (tuple): Make default constructors constexpr.
    	* testsuite/20_util/tuple/cons/constexpr.cc: Add.
    
    	* testsuite/18_support/initializer_list/requirements/
    	constexpr_functions.cc: Add.
    	* testsuite/18_support/initializer_list/constexpr.cc: Add.
    
    From-SVN: r166301

---
+++ libstdc++-v3/testsuite/18_support/initializer_list/constexpr.cc
@@ -0,0 +1,30 @@
+// { dg-do compile }
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2010 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <initializer_list>
+#include <testsuite_common_types.h>
+
+int main()
+{
+  __gnu_test::constexpr_default_constructible test1;
+  test1.operator()<std::initializer_list<long>>();
+
+  return 0;
+}
