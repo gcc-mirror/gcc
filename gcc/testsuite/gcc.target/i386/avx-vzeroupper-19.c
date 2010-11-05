d73ee4e74eb82f29e5949c13c6544b50d0f96d4c
H.J. Lu <hongjiu.lu@intel.com>
2010-11-05 12:53:39 +0000

    Check 256bit AVX register in move expanders.
    gcc/
    
    2010-11-05  H.J. Lu  <hongjiu.lu@intel.com>
    	    Uros Bizjak  <ubizjak@gmail.com>
    
    	* config/i386/i386.c (ix86_expand_move): Set use_avx256_p if
    	256bit AVX register is used.
    	(ix86_expand_vector_move_misalign): Likewise.
    	(ix86_expand_vector_move): Replace use_avx256_p with
    	VALID_AVX256_REG_MODE.
    
    gcc/testsuite/
    
    2010-11-05  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* gcc.target/i386/avx-vzeroupper-19.c: New.
    
    Co-Authored-By: Uros Bizjak <ubizjak@gmail.com>
    
    From-SVN: r166358

---
+++ gcc/testsuite/gcc.target/i386/avx-vzeroupper-19.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 -mavx -mtune=generic -dp" } */
+
+void feat_s3_cep_dcep (int cepsize_used, float **mfc, float **feat)
+{
+  float *f;
+  float *w, *_w;
+  int i;
+  __builtin_memcpy (feat[0], mfc[0], cepsize_used * sizeof(float));
+  f = feat[0] + cepsize_used;
+  w = mfc[2];
+  _w = mfc[-2];
+  for (i = 0; i < cepsize_used; i++)
+    f[i] = w[i] - _w[i];
+}
+
+/* { dg-final { scan-assembler-times "avx_vzeroupper" 1 } } */
