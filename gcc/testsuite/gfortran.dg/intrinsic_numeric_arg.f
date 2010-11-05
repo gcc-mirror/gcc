583ddb6423da96a5415fe63c6ecf7b9c451769fa
bdavis <bdavis@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-05 03:19:13 +0000

    2010-11-04  Bud Davis	<jimmied@smu.edu>
    	* gfortran.dg/intrinsic_numeric_arg.f: Add test to check 
    	error message.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@166347 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/intrinsic_numeric_arg.f
@@ -0,0 +1,9 @@
+! this test checks for a non-numeric argument to an
+! intrinsic function (of which ABS() is one of many).
+!      { dg-do compile }
+       LOGICAL Z
+       CHARACTER A
+       REAL R
+       R = ABS(Z) !  { dg-error " must be a numeric type" }
+       R = ABS(A) !  { dg-error " must be a numeric type" }
+       END
