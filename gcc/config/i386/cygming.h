aa1ff3cbb5bb2fa5eb13805cb5e519c65fe6d946
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-06 08:55:57 +0000

    * config/i386/cygming.h (DWARF_FRAME_REGISTERS): Redefine.  Use 33 	in 64-bit mode and 17 otherwise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@166397 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/cygming.h
+++ gcc/config/i386/cygming.h
@@ -84,6 +84,10 @@ along with GCC; see the file COPYING3.  If not see
   (TARGET_64BIT ? dbx64_register_map[(n)]		\
 		: svr4_dbx_register_map[(n)])
 
+/* The MS_ABI changes the set of call-used registers.  */
+#undef DWARF_FRAME_REGISTERS
+#define DWARF_FRAME_REGISTERS (TARGET_64BIT ? 33 : 17)
+
 #ifdef HAVE_GAS_PE_SECREL32_RELOC
 /* Use section relative relocations for debugging offsets.  Unlike
    other targets that fake this by putting the section VMA at 0, PE
