8873d8a12d48fbd8d9b7a9e053227a88a0866e4a
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-08 23:42:09 +0000

    2010-11-08  Jonathan Wakely  <jwakely.gcc@gmail.com>
    	* include/bits/unique_ptr.h: Move misplaced static_assert and use
    	tuple's constexpr constructor in constexpr constructors.
    	* testsuite/20_util/unique_ptr/cons/ptr_deleter.cc: New.
    	* testsuite/20_util/unique_ptr/cons/ptr_deleter_neg.cc: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@166460 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/20_util/unique_ptr/cons/ptr_deleter.cc
@@ -0,0 +1,68 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-do run }
+
+// Copyright (C) 2010 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 20.9.10 Template class unique_ptr [unique.ptr]
+
+#include <memory>
+#include <testsuite_hooks.h>
+
+static int count;
+
+void del(int* p) { ++count; delete p; }
+void vdel(int* p) { ++count; delete[] p; }
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+  count = 0;
+  {
+    std::unique_ptr<int, void(*)(int*)> p(nullptr, del);
+  }
+  VERIFY( count == 0 );
+  {
+    std::unique_ptr<int, void(*)(int*)> p(new int, del);
+  }
+  VERIFY( count == 1 );
+}
+
+void
+test02()
+{
+  bool test __attribute__((unused)) = true;
+  count = 0;
+  {
+    std::unique_ptr<int[], void(*)(int*)> p(nullptr, vdel);
+  }
+  VERIFY( count == 0 );
+  {
+    std::unique_ptr<int[], void(*)(int*)> p(new int[1], vdel);
+  }
+  VERIFY( count == 1 );
+}
+
+int
+main()
+{
+  test01();
+  test02();
+  return 0;
+}
+
