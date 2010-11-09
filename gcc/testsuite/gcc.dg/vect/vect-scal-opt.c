fc489e4a0749622dcaa653cb871d865dbeaf594e
Richard Guenther <rguenther@suse.de>
2010-11-09 13:45:45 +0000

    re PR tree-optimization/46312 (gcc.dg/vec-scal-opt2.c fails for ARM targets.)
    2010-11-09  Richard Guenther  <rguenther@suse.de>
    
    	PR testsuite/46312
    	* gcc.dg/vec-scal-opt.c: Move ...
    	* gcc.dg/vect/vect-scal-opt.c: ... here.
    	* gcc.dg/vec-scal-opt1.c: Move ...
    	* gcc.dg/vect/vect-scal-opt1.c: ... here.
    	* gcc.dg/vec-scal-opt2.c: Move ...
    	* gcc.dg/vect/vect-scal-opt2.c: ... here.
    
    From-SVN: r166489

--- gcc/testsuite/gcc.dg/vect/vect-scal-opt.c
+++ gcc/testsuite/gcc.dg/vect/vect-scal-opt.c
@@ -1,4 +1,6 @@
 /* { dg-do compile } */
+/* { dg-require-effective-target vect_shift } */
+/* { dg-require-effective-target vect_int } */
 /* { dg-options "-O -fdump-tree-veclower" } */
 
 #define vidx(type, vec, idx) (*((type *) &(vec) + idx))
@@ -8,13 +10,15 @@ __attribute__((vector_size((elcount)*sizeof(type)))) type
 short k;
 
 int main (int argc, char *argv[]) {
+   k = argc;
    vector(8, short) v0 = {argc,1,2,3,4,5,6,7};
+   vector(8, short) v2 = {k,   k,k,k,k,k,k,k};
    vector(8, short) r1;
 
-   r1 = v0 >> (vector(8, short)){2,2,2,2,2,2,2,2};
+   r1 = v0 >> v2;
 
    return vidx(short, r1, 0);
 }
 
-/* { dg-final { scan-tree-dump-times ">> 2" 1 "veclower" } } */
+/* { dg-final { scan-tree-dump-times ">> k.\[0-9_\]*" 1 "veclower" } } */
 /* { dg-final { cleanup-tree-dump "veclower" } } */
