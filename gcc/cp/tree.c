108ad6a33ff0e9aa0a990cfca9cd2e237030e27f
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-09 03:54:12 +0000

    PR c++/46335 	* tree.c (bot_manip): Check TREE_SIDE_EFFECTS as well.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@166470 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/tree.c
+++ gcc/cp/tree.c
@@ -1763,11 +1763,10 @@ bot_manip (tree* tp, int* walk_subtrees, void* data)
   splay_tree target_remap = ((splay_tree) data);
   tree t = *tp;
 
-  if (!TYPE_P (t) && TREE_CONSTANT (t))
+  if (!TYPE_P (t) && TREE_CONSTANT (t) && !TREE_SIDE_EFFECTS (t))
     {
       /* There can't be any TARGET_EXPRs or their slot variables below
-	 this point.  We used to check !TREE_SIDE_EFFECTS, but then we
-	 failed to copy an ADDR_EXPR of the slot VAR_DECL.  */
+	 this point.  */
       *walk_subtrees = 0;
       return NULL_TREE;
     }
