197a830e4355554df133c9bc54c75269cfb70ac7
Joern Rennecke <amylaar@spamcop.net>
2010-11-10 11:29:00 +0000

    re PR target/46407 (rx-elf --enable-werror-all build failure)
    	PR target/46407
    	* config/rx/rx.h (REGISTER_NAMES): Remove trailing semicolon.
    	* config/rx/rx.c (rx_promote_function_mode): Mark punsignedp
    	with ATTRIBUTE_UNUSED.
    	(valid_psw_flag): Constify parameter which.
    	(rx_memory_move_cost): Change type of parameter regclass to reg_class_t.
    
    From-SVN: r166529

--- gcc/config/rx/rx.h
+++ gcc/config/rx/rx.h
@@ -341,7 +341,7 @@ typedef unsigned int CUMULATIVE_ARGS;
   {								\
     "r0",  "r1",  "r2",   "r3",   "r4",   "r5",   "r6",   "r7",	\
       "r8",  "r9",  "r10",  "r11",  "r12",  "r13",  "r14",  "r15", "cc"	\
-  };
+  }
 
 #define ADDITIONAL_REGISTER_NAMES	\
 {					\
