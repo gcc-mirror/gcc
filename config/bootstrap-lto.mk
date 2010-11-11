dc09b1077d0acd283afffd5eb09dda34b9f6ba95
Jan Hubicka <jh@suse.cz>
2010-11-12 00:06:25 +0100

    * config/bootstrap-lto: Use -flto.
    From-SVN: r166630

--- config/bootstrap-lto.mk
+++ config/bootstrap-lto.mk
@@ -1,8 +1,8 @@
 # This option enables LTO for stage2 and stage3.  It requires lto to
 # be enabled for stage1 with --enable-stage1-languages.
 
-STAGE2_CFLAGS += -fwhopr=jobserver -fuse-linker-plugin -frandom-seed=1
-STAGE3_CFLAGS += -fwhopr=jobserver -fuse-linker-plugin -frandom-seed=1
+STAGE2_CFLAGS += -flto=jobserver -fuse-linker-plugin -frandom-seed=1
+STAGE3_CFLAGS += -flto=jobserver -fuse-linker-plugin -frandom-seed=1
 
 # Ada fails to build with LTO, turn it off for now.
 BOOT_ADAFLAGS += -fno-lto
