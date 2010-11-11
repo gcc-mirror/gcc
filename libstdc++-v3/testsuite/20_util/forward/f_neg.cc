ad269884765cfe6e5f4cf1f3abb50b4e74f79ac9
Paolo Carlini <paolo.carlini@oracle.com>
2010-11-11 13:10:49 +0000

    move.h (forward): Implement N3143, resolving US 90.
    2010-11-11  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* include/bits/move.h (forward): Implement N3143, resolving US 90.
    	* testsuite/20_util/forward/a.cc: New.
    	* testsuite/20_util/forward/b.cc: Likewise.
    	* testsuite/20_util/forward/c_neg.cc: Likewise.
    	* testsuite/20_util/forward/d.cc: Likewise.
    	* testsuite/20_util/forward/e.cc: Likewise.
    	* testsuite/20_util/forward/f_neg.cc: Likewise.
    
    From-SVN: r166599

---
+++ libstdc++-v3/testsuite/20_util/forward/f_neg.cc
@@ -0,0 +1,90 @@
+// { dg-do compile }
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2010 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-error "static assertion failed" "" { target *-*-* } 64 }
+
+#include <utility>
+
+template <class T>
+  struct C
+  {
+    T t_;
+
+    C() {}
+
+    explicit C(const T& t) : t_(t) { }
+
+    template <class U,
+              class = typename std::enable_if
+                    <
+                        std::is_convertible<U, T>::value
+                    >::type>
+      C(C<U>&& c) : t_(std::forward<T>(c.t_)) { }
+  };
+
+class B;
+
+class A
+{
+  int data_;
+
+  friend class B;
+public:
+  explicit
+  A(int data = 1)
+  : data_(data) { }
+
+  ~A() { data_ = -1; }
+
+  void test() const
+  {
+    __builtin_abort();
+  }
+};
+
+class B
+{
+  int data_;
+public:
+  explicit
+  B(int data = 1)
+  : data_(data) { }
+
+  B(const A& a) : data_(a.data_) { }
+
+  B(A&& a) : data_(a.data_) { a.data_ = 100; }
+
+  ~B() { data_ = -1; }
+
+  void test() const
+  {
+    __builtin_abort();
+  }
+};
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2009/n2951.html
+// Test F.
+int main()
+{
+  A a(3);
+  C<A> ca(a);
+  C<const B&> cb(std::move(ca));
+  cb.t_.test();
+}
