e5479368dcb5ed0cc30c2eeff6cd664e5457cb52
nicola <nicola@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-13 15:53:32 +0000

    In gcc/objc/: 2010-11-13  Nicola Pero  <nicola.pero@meta-innovation.com>
            * objc-act.c (objc_get_protocol_qualified_type): detect cases
            where we are asked to attach a protocol to something which is not
            an Objective-C object type, and produce an error.
    
    In gcc/testsuite/:
    2010-11-13  Nicola Pero  <nicola.pero@meta-innovation.com>
    
            * objc/compile/20060406-1.m: Fixed testcase not to try to qualify
            a pointer to an arbitrary C struct with an Objective-C protocol.
            Test various valid uses of typedef with Objective-C objects and
            protocols instead.
            * objc.dg/invalid-type-1.m: New.
            * obj-c++.dg/invalid-type-1.m: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@166709 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/obj-c++.dg/invalid-type-1.mm
@@ -0,0 +1,25 @@
+/* Contributed by Nicola Pero <nicola.pero@meta-innovation.com>, November 2010.  */
+/* { dg-do compile } */
+#include <objc/objc.h>
+
+typedef int Integer;
+
+@class MyClass;
+
+typedef MyClass AClass;
+
+@protocol MyProtocol
+- (void) method;
+@end
+
+Class <MyProtocol> class_object; /* This is fine.  */
+
+id <MyProtocol> object; /* This is fine.  */
+
+AClass <MyProtocol> *object1; /* This is fine.  */
+
+Integer <MyProtocol> *object2; /* { dg-error ".Integer. is not a template" } */
+/* { dg-error ".MyProtocol. was not declared in this scope" "" { target *-*-* } 21 } */
+
+Integer <NonExistingProtocol> *object3; /* { dg-error ".Integer. is not a template" } */
+/* { dg-error ".NonExistingProtocol. was not declared in this scope" "" { target *-*-* } 24 } */
