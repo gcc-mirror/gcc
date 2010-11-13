989b59db15278ce97f73b7c1fdb56959241cca01
Iain Sandoe <iains@gcc.gnu.org>
2010-11-13 17:15:17 +0000

    fsf-nsstring-format-1.m: Adjust format messages.
    gcc/testsuite:
    
    	* objc.dg/fsf-nsstring-format-1.m: Adjust format messages.
    	* obj-c++.dg/fsf-nsstring-format-1.mm: Likewise.
    
    From-SVN: r166713

--- gcc/testsuite/objc.dg/fsf-nsstring-format-1.m
+++ gcc/testsuite/objc.dg/fsf-nsstring-format-1.m
@@ -38,7 +38,7 @@ NSString *s10 (int dum, NSString *fmt1, ... ) __attribute__((format_arg(2))) ; /
 void foo (void)
 {
   s1 (@"format not checked %d %s", 3, 4);
-  printf("this one is checked %d %s", 3, 4, 5); /* { dg-warning "format .%s. expects type .char .., but argument 3 has type .int." } */
+  printf("this one is checked %d %s", 3, 4, 5); /* { dg-warning "format '%s' expects argument of type 'char .', but argument 3 has type 'int'" } */
 			/* { dg-warning "too many arguments for format" "" { target *-*-* } 41 } */
-  printf(s9 (1, "and so is this %d %d %s", 3, 4), 5, 6, 12); /* { dg-warning "format .%s. expects type .char .., but argument 4 has type .int." } */
+  printf(s9 (1, "and so is this %d %d %s", 3, 4), 5, 6, 12); /* { dg-warning "format '%s' expects argument of type 'char .', but argument 4 has type 'int'" } */
 }
