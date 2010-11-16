2b12962772d3b0e85d860609f32bfb28515d5ba9
Joern Rennecke <amylaar@spamcop.net>
2010-11-16 21:46:28 +0000

    re PR java/46386 (negative shift count in build_constants_constructor for 16 bit pointer targets)
    2010-11-09  Joern Rennecke  <amylaar@spamcop.net>
    	    Andrew Haley  <aph@redhat.com>
    
    	PR java/46386
    gcc/java:
    	* constants.c (build_constants_constructor): Avoid negative shift.
    gcc:
    	* config/pdp11/t-pdp11 (java/constants.o-warn): Remove.
    	* config/t-pnt16-warn (java/constants.o-warn): Likewise.
    
    Co-Authored-By: Andrew Haley <aph@redhat.com>
    
    From-SVN: r166826

--- gcc/java/constants.c
+++ gcc/java/constants.c
@@ -540,8 +540,8 @@ build_constants_constructor (void)
 	     FIXME: This is a kludge.  The field we're initializing is
 	     not a scalar but a union, and that's how we should
 	     represent it in the compiler.  We should fix this.  */
-	  if (BYTES_BIG_ENDIAN && POINTER_SIZE > 32)
-	    temp <<= POINTER_SIZE - 32;
+	  if (BYTES_BIG_ENDIAN)
+	    temp <<= ((POINTER_SIZE > 32) ? POINTER_SIZE - 32 : 0);
 
           CONSTRUCTOR_PREPEND_VALUE (t, get_tag_node (outgoing_cpool->tags[i]));
           CONSTRUCTOR_PREPEND_VALUE (d,
