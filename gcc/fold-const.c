180ed1b2bd57ce7f8d537db11242c18703121c78
Richard Henderson <rth@redhat.com>
2010-11-15 19:45:36 -0800

    fold-const.c (operand_equal_for_comparison_p): Handle FMA_EXPR...
    	* fold-const.c (operand_equal_for_comparison_p): Handle FMA_EXPR,
    	WIDEN_MULT_PLUS_EXPR, WIDEN_MULT_MINUS_EXPR, VEC_COND_EXPR,
    	DOT_PROD_EXPR.
    
    From-SVN: r166792

--- gcc/fold-const.c
+++ gcc/fold-const.c
@@ -2635,6 +2635,14 @@ operand_equal_p (const_tree arg0, const_tree arg1, unsigned int flags)
 	case TRUTH_ORIF_EXPR:
 	  return OP_SAME (0) && OP_SAME (1);
 
+	case FMA_EXPR:
+	case WIDEN_MULT_PLUS_EXPR:
+	case WIDEN_MULT_MINUS_EXPR:
+	  if (!OP_SAME (2))
+	    return 0;
+	  /* The multiplcation operands are commutative.  */
+	  /* FALLTHRU */
+
 	case TRUTH_AND_EXPR:
 	case TRUTH_OR_EXPR:
 	case TRUTH_XOR_EXPR:
@@ -2648,6 +2656,8 @@ operand_equal_p (const_tree arg0, const_tree arg1, unsigned int flags)
 				      TREE_OPERAND (arg1, 0), flags));
 
 	case COND_EXPR:
+	case VEC_COND_EXPR:
+	case DOT_PROD_EXPR:
 	  return OP_SAME (0) && OP_SAME (1) && OP_SAME (2);
 
 	default:
