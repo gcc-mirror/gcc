11ea05ac682b2a5dae4a928a96934db63844dc1c
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-18 16:41:53 +0000

    * gcc.target/i386/pr46470.c: Skip for 32-bit PIC.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@166910 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/pr46470.c
+++ gcc/testsuite/gcc.target/i386/pr46470.c
@@ -1,4 +1,9 @@
 /* { dg-do compile } */
+/* The pic register save adds unavoidable stack pointer references.  */
+/* { dg-skip-if "" { ilp32 && { ! nonpic } }  { "*" } { "" } } */
+/* These options are selected to ensure 1 word needs to be allocated
+   on the stack to maintain alignment for the call.  This should be
+   transformed to push+pop.  We also want to force unwind info updates.  */
 /* { dg-options "-Os -fomit-frame-pointer -fasynchronous-unwind-tables" } */
 /* { dg-options "-Os -fomit-frame-pointer -mpreferred-stack-boundary=3 -fasynchronous-unwind-tables" { target ilp32 } } */
 
