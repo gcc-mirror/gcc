993466f3d18992422941c1a57b53cff04acffce6
Eric Botcazou <ebotcazou@adacore.com>
2010-11-22 12:50:12 +0000

    re PR testsuite/46230 (several vectorization tests FAIL or XPASS on SPARC)
    	PR testsuite/46230
    	* gcc.dg/vect/pr46009.c: Add vect_int_mult target selector.
    
    From-SVN: r167029

--- gcc/testsuite/gcc.dg/vect/pr46009.c
+++ gcc/testsuite/gcc.dg/vect/pr46009.c
@@ -70,5 +70,5 @@ main (void)
   return 0;
 }
 
-/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 2 "vect"  } } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 2 "vect" { target vect_int_mult } } } */
 /* { dg-final { cleanup-tree-dump "vect" } } */
