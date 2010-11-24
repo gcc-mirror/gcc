0d40d51cf5d58f791e23b201b5100c8db4063ad3
Jakub Jelinek <jakub@redhat.com>
2010-11-24 17:56:44 +0100

    re PR rtl-optimization/46614 (gcc.dg/vect/vect-strided-u8-i8-gap4.c FAILs with -fno-rename-registers -fsched2-use-superblocks)
    	PR rtl-optimization/46614
    	* sched-deps.c (NON_FLUSH_JUMP_KIND, NON_FLUSH_JUMP_P): Define.
    	 (deps_analyze_insn): Mark JUMP_INSNs in
    	last_pending_memory_flush that weren't added through
    	flush_pending_lists with NON_FLUSH_JUMP_KIND.
    	(sched_analyze_2, sched_analyze_insn): Check NON_FLUSH_JUMP_P
    	on INSN_LIST instead of JUMP_P check on its operand.
    	* sched-rgn.c (concat_INSN_LIST): Copy over REG_NOTE_KIND.
    
    	* gcc.dg/pr46614.c: New test.
    
    From-SVN: r167121

---
+++ gcc/testsuite/gcc.dg/pr46614.c
@@ -0,0 +1,56 @@
+/* PR rtl-optimization/46614 */
+/* { dg-do run } */
+/* { dg-options "-O -fno-rename-registers -fsched2-use-superblocks -fschedule-insns2 -funroll-loops" } */
+
+extern void abort (void);
+
+struct S
+{
+  unsigned char a;
+  unsigned char b;
+  unsigned int c;
+  unsigned int e;
+  unsigned char f;
+  unsigned int g;
+};
+
+void bar (struct S *x)
+{
+  int i;
+  struct S *p = x;
+  struct S r[16];
+  unsigned j;
+  for (i = 0; i < 16; i++)
+    {
+      r[i].c = p->b + p->c;
+      j = p->c + p->f;
+      r[i].a = j + p->b;
+      r[i].f = p->f + p->e;
+      r[i].g = p->b + p->c;
+    }
+  for (i = 0; i < 16; i++)
+    {
+      if (r[i].c != x[i].b + x[i].c
+	  || r[i].a != x[i].c + x[i].f + x[i].b
+	  || r[i].f != x[i].f + x[i].e
+	  || r[i].g != x[i].b + x[i].c)
+	abort ();
+    }
+  for (i = 0; i < 16; i++)
+    {
+      r[i].b = p->c;
+      if (r[i].b != x[i].c)
+	abort ();
+    }
+}
+
+int
+main ()
+{
+  int i;
+  struct S x[16];
+  for (i = 0; i < 16; i++)
+    x[i].b = x[i].c = x[i].e = x[i].f = 5;
+  bar (x);
+  return 0;
+}
