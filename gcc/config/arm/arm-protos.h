2b471f50dad12326b5fd1774356f8ee5a246e3bc
ibolton <ibolton@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-26 13:21:32 +0000

    2010-11-26  Ian Bolton  <ian.bolton@arm.com>
    	* config/arm/arm.c (arm_option_override): enable loop array
    	prefetching at -O3 for suitable targets, and configure params.
    	* config/arm/arm-protos.h (struct tune_params): Add fields for
    	configuring loop array prefetching.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@167175 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/arm/arm-protos.h
+++ gcc/config/arm/arm-protos.h
@@ -218,6 +218,9 @@ struct tune_params
   bool (*rtx_costs) (rtx, RTX_CODE, RTX_CODE, int *, bool);
   bool (*sched_adjust_cost) (rtx, rtx, rtx, int *);
   int constant_limit;
+  int num_prefetch_slots;
+  int l1_cache_size;
+  int l1_cache_line_size;
 };
 
 extern const struct tune_params *current_tune;
