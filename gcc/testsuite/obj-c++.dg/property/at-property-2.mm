9d780cb2fbf609e85c00d037a570f53a3ea4773c
Nicola Pero <nicola.pero@meta-innovation.com>
2010-11-27 18:17:14 +0000

    In gcc/cp/: 2010-11-27 Nicola Pero <nicola.pero@meta-innovation.com>
    In gcc/cp/:
    2010-11-27  Nicola Pero  <nicola.pero@meta-innovation.com>
    
    	PR objc++/46222
    	* decl.c (grokdeclarator): Replaced an assert (for a case that can
    	never happen in C++, but could happen in ObjC++ for invalid code)
    	with a check that prints an error message and returns
    	error_mark_node.
    
    In gcc/testsuite/:
    2010-11-27  Nicola Pero  <nicola.pero@meta-innovation.com>
    
    	PR objc++/46222	
    	* obj-c++.dg/property/at-property-2.mm: Uncommented testcase.
    
    From-SVN: r167202

--- gcc/testsuite/obj-c++.dg/property/at-property-2.mm
+++ gcc/testsuite/obj-c++.dg/property/at-property-2.mm
@@ -8,7 +8,6 @@
 }
 @property int name __attribute__((deprecated));
 @property int table __attribute__((xxx));       /* { dg-warning ".xxx. attribute directive ignored" } */
-/* FIXME: the test below should not ICE.
-@property void function (void);                  { dg-error "can.t make .function. into a method" } */
+@property void function (void);                 /* { dg-error "declaration of function .function. in invalid context" } */
 @property typedef int j;                        /*  { dg-error "invalid type for property" } */
 @end
