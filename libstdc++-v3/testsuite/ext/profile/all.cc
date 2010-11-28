18af637ed97554b264b1b56fb55c1767938c92cf
Paolo Carlini <paolo@gcc.gnu.org>
2010-11-28 10:22:55 +0000

    re PR libstdc++/46683 (FAIL: ext/profile/all.cc)
    2010-11-28  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR libstdc++/46683
    	* testsuite/ext/profile/mh.cc: Move dg-require* directives to the end.
    	* testsuite/ext/profile/all.cc: Likewise.
    
    From-SVN: r167219

--- libstdc++-v3/testsuite/ext/profile/all.cc
+++ libstdc++-v3/testsuite/ext/profile/all.cc
@@ -1,11 +1,11 @@
-// { dg-require-profile-mode "" }
 // { dg-options "-std=gnu++0x -O0" }
 // { dg-options "-std=gnu++0x -O0 -D_GLIBCXX_PROFILE_NO_THREADS" { target { ! tls_native } } }
 // { dg-do compile }
+// { dg-require-profile-mode "" }
 
 // -*- C++ -*-
 
-// Copyright (C) 2006, 2007, 2009 Free Software Foundation, Inc.
+// Copyright (C) 2006, 2007, 2009, 2010 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
