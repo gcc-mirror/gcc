9960d752f0bd2b1e3f0c62d50deae42a1303c8ce
dodji <dodji@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-29 16:30:54 +0000

    Fix PR c++/45383
    Reverting the fix for PR c++/42260 fixes c++/45383.
    This reverts commit r155415.
    
    gcc/cp/
    	Reverted patch for PR c++/42260
    	* cp-tree.h (lookup_conversions): Reverted "Add new bool parameter to
    	declarationE."
    	* search.c (lookup_conversion): Reverted "Use new bool parameter in
    	definition".
    	* call.c (add_builtin_candidates): Reverted "Don't lookup template
              conversion"
    	(convert_class_to_reference, build_user_type_conversion_1,
    	 build_op_call): Reverted "Adjust".
    	* cvt.c (build_expr_type_conversion): Reverted "Likewise".
    
    gcc/testsuite/
    	Reverted patch for PR c++/42260
    	* conversion/cast2.C: Reverted New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@167248 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/search.c
+++ gcc/cp/search.c
@@ -2440,13 +2440,10 @@ lookup_conversions_r (tree binfo,
    functions in this node were selected.  This function is effectively
    performing a set of member lookups as lookup_fnfield does, but
    using the type being converted to as the unique key, rather than the
-   field name.
-   If LOOKUP_TEMPLATE_CONVS_P is TRUE, the returned TREE_LIST contains
-   the non-hidden user-defined template conversion functions too.  */
+   field name.  */
 
 tree
-lookup_conversions (tree type,
-		    bool lookup_template_convs_p)
+lookup_conversions (tree type)
 {
   tree convs, tpl_convs;
   tree list = NULL_TREE;
@@ -2472,9 +2469,6 @@ lookup_conversions (tree type,
 	  list = probe;
 	}
     }
-
-  if (lookup_template_convs_p == false)
-    tpl_convs = NULL_TREE;
 
   for (; tpl_convs; tpl_convs = TREE_CHAIN (tpl_convs))
     {
