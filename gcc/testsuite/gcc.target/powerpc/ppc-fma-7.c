e4a0bb38ebdb55418fb46741526bc06cc82553e2
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-30 20:32:07 +0000

    Add new FMA test case
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@167312 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/ppc-fma-7.c
@@ -0,0 +1,18 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
+/* { dg-require-effective-target powerpc_altivec_ok } */
+/* { dg-options "-O3 -ftree-vectorize -mcpu=power6 -ffast-math" } */
+/* { dg-final { scan-assembler-times "fmadd" 1 } } */
+/* { dg-final { scan-assembler-times "fmsub " 1 } } */
+/* { dg-final { scan-assembler-not "fmul" } } */
+/* { dg-final { scan-assembler-not "fadd " } } */
+
+/* Check whether the common FFT idiom (a*b)+c and (a*b)-c generates two fma
+   instructions, instead of a multiply, add, and subtract.  */
+
+void
+fft (double *result, double a, double b, double c)
+{
+  result[0] = (a*b) + c;
+  result[1] = (a*b) - c;
+}
