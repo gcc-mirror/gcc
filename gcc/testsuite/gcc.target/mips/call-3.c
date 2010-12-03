64a92633f0061e0244860c01b31a88a78eb1ae06
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2010-12-03 12:40:57 +0000

    gcc:
    	Backport from mainline:
    	2010-11-12  Rainer Orth  <ro@CeBiTec.Uni-Bielefeld.DE>
    
    	* config/mips/iris6.h [!IRIX_USING_GNU_LD]
    	(SUPPORTS_INIT_PRIORITY): Define.
    
    	gcc/testsuite:
    	Backport from mainline:
    	2010-11-12  Rainer Orth  <ro@CeBiTec.Uni-Bielefeld.DE>
    
    	* lib/prune.exp (prune_gcc_output): Ignore IRIX 6 linker multiline
    	warning.
    	* g++.dg/abi/packed1.C (struct INNER): Ignore warning on
    	mips-sgi-irix*.
    	* g++.dg/cpp/_Pragma1.C: Skip on mips-sgi-irix*.
    	* gcc.target/mips/call-3.c: Add dg-require-visibility.
    
    From-SVN: r167419

--- gcc/testsuite/gcc.target/mips/call-3.c
+++ gcc/testsuite/gcc.target/mips/call-3.c
@@ -1,5 +1,6 @@
 /* { dg-options "-O2 -mrelax-pic-calls -mno-shared" } */
 /* { dg-final { scan-assembler "\\.reloc\t1f,R_MIPS_JALR,g\n1:\tjalr\t" } } */
+/* { dg-require-visibility "" } */
 
 __attribute__ ((visibility ("hidden"))) void g ();
 
