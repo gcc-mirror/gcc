694481d86f1710a0977586b6abf6d11dd73469d5
Dave Korn <dave.korn.cygwin@gmail.com>
2010-12-06 01:13:36 +0000

    pr46674.c (LABEL3): New macro definition.
    	* gcc.dg/pr46674.c (LABEL3): New macro definition.
    	(LABEL2): Likewise.
    	(LABEL): Likewise.
    	(jelly): Account for user label prefix in asm name.
    
    From-SVN: r167483

--- gcc/testsuite/gcc.dg/pr46674.c
+++ gcc/testsuite/gcc.dg/pr46674.c
@@ -2,9 +2,13 @@
 /* { dg-require-alias "" } */
 /* { dg-options "-O2" } */
 
+#define LABEL3(pfx, x) # pfx x
+#define LABEL2(pfx, x) LABEL3(pfx, x)
+#define LABEL(x) LABEL2(__USER_LABEL_PREFIX__, x)
+
 int yum;
 void dessert (void) { ++yum; }
-extern void jelly (void) __asm__ ("jelly2") __attribute__ ((alias ("dessert"), weak));
+extern void jelly (void) __asm__ (LABEL ("jelly2")) __attribute__ ((alias ("dessert"), weak));
 extern void wobbly (void) __attribute__ ((alias ("jelly2"), weak));
 
 /* { dg-final { scan-assembler "wobbly" } } */
