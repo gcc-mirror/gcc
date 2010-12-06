e08ec256988cbae9dfc61a9d7a1d2a691d381853
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-12-06 23:06:48 +0000

    2010-12-06  Tobias Burnus  <burnus@net-b.de>
            PR fortran/46817
            * quadmath-imp.h: Add LGPL header.
            * quadmath.h: Add LGPL header.
            * quadmath_io.c: Add LGPL header.
            * quadmath_weak.h: Add LGPL header.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@167521 138bc75d-0d04-0410-961f-82ee72b054a4

--- libquadmath/quadmath-imp.h
+++ libquadmath/quadmath-imp.h
@@ -1,3 +1,23 @@
+/* GCC Quad-Precision Math Library
+   Copyright (C) 2010 Free Software Foundation, Inc.
+   Written by Francois-Xavier Coudert  <fxcoudert@gcc.gnu.org>
+
+This file is part of the libiberty library.
+Libiberty is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+Libiberty is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with libiberty; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+Boston, MA 02110-1301, USA.  */
+
 #ifndef QUADMATH_IMP_H
 #define QUADMATH_IMP_H
 
