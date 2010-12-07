1b6d012b5070fb155e99c169ef1f2a82861538ac
H.J. Lu <hongjiu.lu@intel.com>
2010-12-07 01:29:10 +0000

    Add testcases for PRs middle-end/41562 and middle-end/46185.
    2010-12-06  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR middle-end/41562
    	* g++.dg/graphite/pr41562.C: New.
    
    	PR middle-end/46185
    	* gcc.dg/graphite/pr46185.c: New.
    
    From-SVN: r167527

---
+++ gcc/testsuite/g++.dg/graphite/pr41562.C
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-O -ftree-loop-distribution -floop-block" } */
+
+typedef struct stSirenEncoder { } *SirenEncoder;
+
+int Siren7_EncodeFrame(SirenEncoder encoder, unsigned char *DataIn, unsigned char *DataOut) {
+   int number_of_regions;
+   static int absolute_region_power_index[28] = {0};
+   static int region_mlt_bit_counts[28] = {0};
+   int dwRes = 0;
+   int region;
+   if (dwRes != 0) return dwRes;
+   for(region = 0; region < number_of_regions; region++)
+   {
+     absolute_region_power_index[region] += 24;
+     region_mlt_bit_counts[region] = 0;
+   }
+}
