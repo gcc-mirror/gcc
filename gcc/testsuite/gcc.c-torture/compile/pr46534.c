370f7271f3cc81277c9a0c6d32ff63528d1a068f
Jakub Jelinek <jakub@redhat.com>
2010-12-07 16:11:46 +0100

    backport: re PR middle-end/46534 (ICE optimizing printf ("...>10MBstring\n") into puts)
    	Backport from mainline
    	2010-11-18  Jakub Jelinek  <jakub@redhat.com>
    
    	PR middle-end/46534
    	* builtins.c (fold_builtin_printf): Don't copy and modify string
    	before build_string_literal, instead modify what
    	build_string_literal returned.
    
    	* gcc.c-torture/compile/pr46534.c: New test.
    
    From-SVN: r167540

---
+++ gcc/testsuite/gcc.c-torture/compile/pr46534.c
@@ -0,0 +1,17 @@
+/* PR middle-end/46534 */
+
+extern int printf (const char *, ...);
+
+#define S1 "                    "
+#define S2 S1 S1 S1 S1 S1 S1 S1 S1 S1 S1
+#define S3 S2 S2 S2 S2 S2 S2 S2 S2 S2 S2
+#define S4 S3 S3 S3 S3 S3 S3 S3 S3 S3 S3
+#define S5 S4 S4 S4 S4 S4 S4 S4 S4 S4 S4
+#define S6 S5 S5 S5 S5 S5 S5 S5 S5 S5 S5
+#define S7 S6 S6 S6 S6 S6 S6 S6 S6 S6 S6
+
+void
+foo (void)
+{
+  printf (S7 "\n");
+}
