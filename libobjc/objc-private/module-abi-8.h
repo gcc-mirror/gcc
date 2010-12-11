375d1239a479316e3fc806708477b98b076673e8
Nicola Pero <nicola.pero@meta-innovation.com>
2010-12-11 14:03:38 +0000

    module-abi-8.h (struct objc_symtab): Updated description of sel_ref_cnt and refs.
    2010-12-11  Nicola Pero  <nicola.pero@meta-innovation.com>
    
    	* objc-private/module-abi-8.h (struct objc_symtab): Updated
    	description of sel_ref_cnt and refs.
    	* objc/deprecated/struct_objc_symtab.h (objc_symtab): Same change.
    
    From-SVN: r167710

--- libobjc/objc-private/module-abi-8.h
+++ libobjc/objc-private/module-abi-8.h
@@ -47,8 +47,11 @@ struct objc_static_instances
    categories  defined in the module.   */
 struct objc_symtab
 {
-  unsigned long sel_ref_cnt;  /* Unknown. */
-  SEL        refs;            /* Unknown. */
+  unsigned long sel_ref_cnt;  /* Unused (always set to 0). */
+  SEL      refs;              /* The table of selectors referenced in
+                                 this module.  This is terminated by a
+                                 selector with NULL sel_id and NULL
+                                 sel_types.  */
   unsigned short cls_def_cnt; /* Number of classes compiled (defined)
                                  in the module. */
   unsigned short cat_def_cnt; /* Number of categories compiled
--- libobjc/objc/deprecated/struct_objc_symtab.h
+++ libobjc/objc/deprecated/struct_objc_symtab.h
@@ -5,8 +5,11 @@
    A Symtab contains a variable length array of pointers to classes and
    categories  defined in the module.   */
 typedef struct objc_symtab {
-  unsigned long sel_ref_cnt;                     /* Unknown. */
-  SEL        refs;                              /* Unknown. */
+  unsigned long sel_ref_cnt;  /* Unused (always set to 0). */
+  SEL      refs;              /* The table of selectors referenced in
+                                 this module.  This is terminated by a
+                                 selector with NULL sel_id and NULL
+                                 sel_types.  */
   unsigned short cls_def_cnt;                   /* Number of classes compiled
                                                   (defined) in the module. */
   unsigned short cat_def_cnt;                   /* Number of categories 
