a6b29a1091df5785cfc53bc5fa573dfe67041ce6
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-12-11 01:23:43 +0000

    * gcc.dg/funcorder.c: Remove XFAIL for hppa*64*-*-*.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@167708 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/funcorder.c
+++ gcc/testsuite/gcc.dg/funcorder.c
@@ -1,10 +1,8 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -funit-at-a-time" } */
-/* { dg-final { if [ istarget hppa*-*-* ] { scan-assembler-not "link_error,%r" { xfail hppa*64*-*-* } } else { scan-assembler-not "link_error" } } } */
+/* { dg-final { scan-assembler-not "link_error" } } */
 /*  In unit-at-time the functions should be assembled in order
-    e q t main, so we realize that they are pure.  The test is
-    xfailed on hppa64 because variable r in q is sign extended
-    to 64-bits.  As a result, "if (t!=mem)" is not simplified.  */
+    e q t main, so we realize that they are pure.  */
  
 static int mem;
 static int e(void) __attribute__ ((noinline));
