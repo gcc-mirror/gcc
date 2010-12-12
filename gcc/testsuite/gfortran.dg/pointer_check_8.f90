f8d40ab50b12ffc93a3c5bae670ff7032a03ed62
janus <janus@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-12-12 21:14:13 +0000

    2010-12-12  Janus Weil  <janus@gcc.gnu.org>
    	PR fortran/46809
    	* resolve.c (resolve_select_type): Set the location of the first
    	argument when generating the EXTENDS_TYPE_OF call.
    
    2010-12-12  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/46809
    	* gfortran.dg/pointer_check_8.f90: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@167731 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pointer_check_8.f90
@@ -0,0 +1,21 @@
+! { dg-do compile}
+! { dg-options "-fcheck=pointer" }
+!
+! PR 46809: [OOP] ICE with -fcheck=pointer for CLASS IS
+!
+! Contributed by Salvatore Filippone <sfilippone@uniroma2.it>
+
+  type t
+  end type t
+
+contains
+
+  subroutine sub(a)
+    class(t) :: a
+    select type (a)
+      class is (t)
+        print *, 'Hi there'
+    end select
+  end subroutine
+
+end
