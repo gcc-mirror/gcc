3dd9c27287d3101232e056f2799a31816b937b2b
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-12-14 10:57:11 +0000

    * g++.dg/other/first-global.C: Update regexp to match new 	names of ctors.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@167791 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/other/first-global.C
+++ gcc/testsuite/g++.dg/other/first-global.C
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 /* { dg-add-options bind_pic_locally } */
-/* { dg-final { scan-assembler "_GLOBAL__I(_|_65535_0_)foobar" } } */
+/* { dg-final { scan-assembler "_GLOBAL__(I|sub_I)(_|_65535_0_)foobar" } } */
 
 struct foo { foo (); };
 foo foobar;
