ae294470ddc56c7af37c4246553840779e38aa0f
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-12-15 17:50:34 +0000

    PR debug/46815 	* cp-gimplify.c (cp_genericize): When changing RESULT_DECL 	into invisible reference, change also DECL_VALUE_EXPR of 	NRV optimized variable.
    	* g++.dg/guality/pr46815.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@167865 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/cp-gimplify.c
+++ gcc/cp/cp-gimplify.c
@@ -958,6 +958,23 @@ cp_genericize (tree fndecl)
       DECL_BY_REFERENCE (t) = 1;
       TREE_ADDRESSABLE (t) = 0;
       relayout_decl (t);
+      if (DECL_NAME (t))
+	{
+	  /* Adjust DECL_VALUE_EXPR of the original var.  */
+	  tree outer = outer_curly_brace_block (current_function_decl);
+	  tree var;
+
+	  if (outer)
+	    for (var = BLOCK_VARS (outer); var; var = DECL_CHAIN (var))
+	      if (DECL_NAME (t) == DECL_NAME (var)
+		  && DECL_HAS_VALUE_EXPR_P (var)
+		  && DECL_VALUE_EXPR (var) == t)
+		{
+		  tree val = convert_from_reference (t);
+		  SET_DECL_VALUE_EXPR (var, val);
+		  break;
+		}
+	}
     }
 
   /* If we're a clone, the body is already GIMPLE.  */
