05575a4695b01941551f84e718997f6803c82dbc
Sebastian Pop <sebastian.pop@amd.com>
2010-12-15 05:04:50 +0000

    Fix PR46845: handle scop parameters before scev analysis.
    2010-12-14  Sebastian Pop  <sebastian.pop@amd.com>
    
    	PR tree-optimization/46845
    	* sese.c (scalar_evolution_in_region): Handle scop parameters
    	before scev analysis.
    
    	* gcc.dg/graphite/id-pr46845.c: New.
    
    From-SVN: r167844

---
+++ gcc/testsuite/gcc.dg/graphite/id-pr46845.c
@@ -0,0 +1,17 @@
+/* { dg-options "-O2 -ffast-math -fgraphite-identity -w -Wno-psabi" { target { i?86-*-* x86_64-*-* } } } */
+
+typedef float V2SF __attribute__ ((vector_size (128)));
+
+V2SF
+foo (int x, V2SF a)
+{
+  V2SF b;
+  if (x & 42)
+    b = a;
+  else
+    b = a + (V2SF) {1.0f/0.0f - 1.0f/0.0f, 1.0f/0.0f - 1.0f/0.0f};
+  while (x--)
+    a += b;
+
+  return a;
+}
