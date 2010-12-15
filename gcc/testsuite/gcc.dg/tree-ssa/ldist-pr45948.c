db990c65db9f947154ac8126013e25315c265790
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-12-15 05:04:30 +0000

    Fix PR45948: add ssa defs from builtin partitions to the last partition.
    2010-12-14  Sebastian Pop  <sebastian.pop@amd.com>
    
    	PR tree-optimization/45948
    	* tree-loop-distribution.c (ssa_name_has_uses_outside_loop_p): New.
    	(stmt_has_scalar_dependences_outside_loop): New.
    	(stmt_generated_in_another_partition): New.
    	(add_scalar_computations_to_partition): New.
    	(rdg_build_partitions): Call add_scalar_computations_to_partition.
    
    	* gcc.dg/tree-ssa/ldist-pr45948.c: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@167842 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ldist-pr45948.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -ftree-loop-distribution -fdump-tree-ldist-details" } */
+
+extern void bar(int);
+
+void
+foo (int i, int n)
+{
+  int a[30];
+  int b[30];
+  for (; i < n; i++)
+    a[i] = b[i] = 0;
+
+  while (1)
+    if (b[0])
+      bar (a[i - 1]);
+}
+
+/* We should apply loop distribution and generate 2 memset (0).  */
+
+/* { dg-final { scan-tree-dump "distributed: split to 3" "ldist" } } */
+/* { dg-final { scan-tree-dump-times "__builtin_memset" 4 "ldist" } } */
+/* { dg-final { cleanup-tree-dump "ldist" } } */
