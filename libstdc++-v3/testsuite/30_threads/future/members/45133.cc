ea4a64c32fae1fa3e3e8e11049fe53964d9cb10c
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-12-15 02:07:07 +0000

    2010-12-15  Jonathan Wakely  <jwakely.gcc@gmail.com>
    	PR libstdc++/46951
    	* testsuite/30_threads/future/members/45133.cc: Fix dg-options.
    	* testsuite/30_threads/shared_future/members/45133.cc: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@167839 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/30_threads/future/members/45133.cc
+++ libstdc++-v3/testsuite/30_threads/future/members/45133.cc
@@ -1,4 +1,7 @@
-// { dg-options "-std=gnu++0x" }
+// { dg-do run { target *-*-freebsd* *-*-netbsd* *-*-linux* *-*-solaris* *-*-cygwin *-*-darwin* alpha*-*-osf* mips-sgi-irix6* } }
+// { dg-options " -std=gnu++0x -pthread" { target *-*-freebsd* *-*-netbsd* *-*-linux* alpha*-*-osf* mips-sgi-irix6* } }
+// { dg-options " -std=gnu++0x -pthreads" { target *-*-solaris* } }
+// { dg-options " -std=gnu++0x " { target *-*-cygwin *-*-darwin* } }
 // { dg-require-cstdint "" }
 // { dg-require-gthreads "" }
 // { dg-require-atomic-builtins "" }
