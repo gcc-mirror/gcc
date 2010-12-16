a4b56e0218ecfc45a452cdbe60f3d19f35a6c8b9
Ulrich Weigand <uweigand@de.ibm.com>
2010-12-16 18:12:23 +0000

    t-spu-elf (LIB2_SIDITI_CONV_FUNC): Define.
    	* config/spu/t-spu-elf (LIB2_SIDITI_CONV_FUNC): Define.
    	* config/spu/spu.h (MIN_UNITS_PER_WORD): Do not define.
    	(LIBGCC2_UNITS_PER_WORD): Define if not already defined.
    
    From-SVN: r167950

--- gcc/config/spu/spu.h
+++ gcc/config/spu/spu.h
@@ -65,9 +65,16 @@ extern GTY(()) int spu_tune;
 
 #define UNITS_PER_WORD (BITS_PER_WORD/BITS_PER_UNIT)
 
-/* We never actually change UNITS_PER_WORD, but defining this causes
-   libgcc to use some different sizes of types when compiling. */
-#define MIN_UNITS_PER_WORD 4
+/* When building libgcc, we need to assume 4 words per units even
+   though UNITS_PER_WORD is 16, because the SPU has basically a 32-bit
+   instruction set although register size is 128 bits.  In particular,
+   this causes libgcc to contain __divdi3 instead of __divti3 etc.
+   However, we allow this default to be re-defined on the command
+   line, so that we can use the LIB2_SIDITI_CONV_FUNCS mechanism
+   to get (in addition) TImode versions of some routines.  */
+#ifndef LIBGCC2_UNITS_PER_WORD
+#define LIBGCC2_UNITS_PER_WORD 4
+#endif
 
 #define POINTER_SIZE 32
 
