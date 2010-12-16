957a77a4d2798de115ea5cebe72570fcd008cf13
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-12-16 01:47:08 +0000

    PR lto/46976 	* gcc.dg/lto/pr46940_0.c: Fix commit.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@167896 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/lto/pr46940_0.c
+++ gcc/testsuite/gcc.dg/lto/pr46940_0.c
@@ -1,5 +1,6 @@
-* { dg-require-linker-plugin "" } */
-* { dg-extra-ld-options "-fuse-linker-plugin" } */
+/* { dg-require-linker-plugin "" } */
+/* { dg-extra-ld-options "-fuse-linker-plugin" } */
+#include <stdio.h>
 
 extern __attribute__((visibility("hidden"))) void _moz_foo (void);
 extern __typeof (_moz_foo) _moz_foo __asm__ ("" "INT__foo") __attribute__((__visibility__("hidden"))) ;
