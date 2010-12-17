4d5d65862a2a7d54abfee98d94aa2cb536b3d9ff
Bernd Schmidt <bernds@codesourcery.com>
2010-12-17 13:48:40 +0000

    arm.c (arm_select_cc_mode): Before calling arm_select_dominance_cc_mode for AND or IOR operations...
    gcc/
    	* config/arm/arm.c (arm_select_cc_mode): Before calling
    	arm_select_dominance_cc_mode for AND or IOR operations, ensure
    	that op is NE or EQ.
    
    gcc/testsuite/
    	* gcc.c-torture/compile/20101217-1.c: New test.
    
    From-SVN: r167981

---
+++ gcc/testsuite/gcc.c-torture/compile/20101217-1.c
@@ -0,0 +1,36 @@
+/* Testcase provided by HUAWEI.  */
+#include <stdio.h>
+int main()
+{
+        int cur_k;
+        int cur_j=0;
+        int cur_i=28;
+        unsigned char temp_data[8];
+        unsigned int Data_Size=20;
+
+        for (cur_k=0;cur_j<7;cur_j++,cur_i++) {
+                if (cur_j%2==0) {
+                        temp_data[cur_k++]=0;
+                }
+                if (cur_k==7) {
+                        for (;cur_k>0;cur_k--) {
+                                if (cur_k>2) {
+                                        if ((temp_data[7-cur_k]=='n' || temp_data[7-cur_k]=='N' ) && (temp_data[7-cur_k+1]=='a' || temp_data[7-cur_k+1]=='A' )) {
+                                                break;
+                                        }
+                                }
+                                if (cur_k==1) {
+                                        if (temp_data[7-cur_k]=='n' || temp_data[7-cur_k]=='N' ) {
+                                                break;
+                                        }
+                                }
+                        }
+                        if (cur_k==7) {
+                        } else {
+                                if (cur_k>0)
+                                        printf("dfjk");
+                        }
+                }
+        }
+return 0;
+}
