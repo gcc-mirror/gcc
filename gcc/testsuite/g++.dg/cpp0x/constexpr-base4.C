ebb526f950a667e2bef588325e9a5f7bfb80ed09
Jakub Jelinek <jakub@redhat.com>
2010-12-27 13:54:30 +0100

    re PR c++/46626 (simple use of virtual methods causes pure virtual method call in c++0x mode)
    	PR c++/46626
    	* semantics.c (build_data_member_initialization): For CLEANUP_STMT
    	recurse into CLEANUP_BODY.
    
    	* g++.dg/cpp0x/constexpr-base4.C: New test.
    
    From-SVN: r168271

---
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-base4.C
@@ -0,0 +1,28 @@
+// PR c++/46626
+// { dg-do run }
+// { dg-options "-std=c++0x" }
+
+struct A
+{
+  virtual void f () = 0;
+  virtual ~A () { }
+};
+
+struct B : A
+{
+  virtual void f () { }
+};
+
+static void
+foo (A *a)
+{
+  a->f ();
+}
+
+int
+main ()
+{
+  B b;
+  foo (&b);
+  return 0;
+}
