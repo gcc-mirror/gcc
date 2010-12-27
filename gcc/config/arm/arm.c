0258b55957795a3e1babb5e0aeeb2f12c4fdd64f
Yao Qi <yao@codesourcery.com>
2010-12-27 12:32:44 +0000

    backport: re PR debug/45447 (ICE with `-g -femit-struct-debug-baseonly')
    Backport from mainline:
    gcc/
            PR target/45447
            * config/arm/arm.c (arm_build_builtin_va_list): Assign
            va_list_name to TYPE_STUB_DECL (va_list_type).
    
    gcc/testsuite/
            PR target/45447
            * gcc.target/arm/pr45447.c: New test.
    
    From-SVN: r168269

--- gcc/config/arm/arm.c
+++ gcc/config/arm/arm.c
@@ -964,6 +964,7 @@ arm_build_builtin_va_list (void)
 			     va_list_type);
   DECL_ARTIFICIAL (va_list_name) = 1;
   TYPE_NAME (va_list_type) = va_list_name;
+  TYPE_STUB_DECL (va_list_type) = va_list_name;
   /* Create the __ap field.  */
   ap_field = build_decl (FIELD_DECL, 
 			 get_identifier ("__ap"),
