e8c2fb69aecfe3042ca2283c85a8f6af7f7a0f36
Sebastian Pop <sebastian.pop@amd.com>
2010-12-28 19:09:16 +0000

    Fix bootstrap without graphite.
    2010-12-28  Sebastian Pop  <sebastian.pop@amd.com>
    
    	* Makefile.in (graphite.o): Add dependence on DIAGNOSTIC_H.
    	* graphite.c: Include diagnostic.h.
    
    From-SVN: r168297

--- gcc/graphite.c
+++ gcc/graphite.c
@@ -35,6 +35,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "config.h"
 #include "system.h"
 #include "coretypes.h"
+#include "diagnostic.h"
 #include "tree-flow.h"
 #include "tree-dump.h"
 #include "cfgloop.h"
