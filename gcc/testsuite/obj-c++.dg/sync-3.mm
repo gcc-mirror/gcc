37153b1e0743ba79cc6d86b92e843995ab015755
Nicola Pero <nicola.pero@meta-innovation.com>
2010-12-29 21:52:00 +0000

    In gcc/objc/: 2010-12-29 Nicola Pero <nicola.pero@meta-innovation.com>
    In gcc/objc/:
    2010-12-29  Nicola Pero  <nicola.pero@meta-innovation.com>
    
    	PR objc/47118
    	* objc-act.c (objc_build_synchronized): Check the argument of
    	@synchronized and emit an appropriate error if it is not a valid
    	Objective-C object.  Deal gracefully with that case.  Updated
    	comments and variable names.
    
    In gcc/testsuite/:
    2010-12-29  Nicola Pero  <nicola.pero@meta-innovation.com>
    
    	PR objc/47118
    	* objc.dg/sync-3.m: New.
    	* obj-c++.dg/sync-3.mm: New.
    
    From-SVN: r168326

---
+++ gcc/testsuite/obj-c++.dg/sync-3.mm
@@ -0,0 +1,128 @@
+/* Contributed by Nicola Pero <nicola.pero@meta-innovation.com>, December 2010.  */
+/* { dg-options "-fobjc-exceptions" } */
+/* { dg-do compile } */
+
+/* Test that the compiler is checking the argument of @synchronized(),
+   and produce errors when invalid types are used.  */
+
+#include <objc/objc.h>
+
+@interface MyObject
+{
+  Class isa;
+}
+@end
+
+@implementation MyObject
+@end
+
+@protocol MyProtocol;
+
+typedef MyObject MyObjectTypedef;
+typedef MyObject *MyObjectPtrTypedef;
+typedef int intTypedef;
+
+typedef struct { float x; float y; } point, *point_ptr;
+
+int test (id object)
+{
+  int dummy = 0;
+
+  {
+    int x;
+    @synchronized (x) /* { dg-error ".@synchronized. argument is not an object" } */
+    { dummy++; }
+  }
+
+  {
+    intTypedef x;
+    @synchronized (x) /* { dg-error ".@synchronized. argument is not an object" } */
+    { dummy++; }
+  }
+
+  {
+    int *x;
+    @synchronized (x) /* { dg-error ".@synchronized. argument is not an object" } */
+    { dummy++; }
+  }
+
+  {
+    point x;
+    @synchronized (x) /* { dg-error ".@synchronized. argument is not an object" } */
+    { dummy++; }
+  }
+
+  {
+    point_ptr x;
+    @synchronized (x) /* { dg-error ".@synchronized. argument is not an object" } */
+    { dummy++; }
+  }
+
+  {
+    id x;
+    @synchronized (x) /* Ok */
+    { dummy++; }
+  }
+
+  {
+    id <MyProtocol> x;
+    @synchronized (x) /* Ok */
+    { dummy++; }
+  }
+
+  {
+    MyObject *x;
+    @synchronized (x) /* Ok */
+    { dummy++; }
+  }
+
+  {
+    MyObject <MyProtocol> *x;
+    @synchronized (x) /* Ok */
+    { dummy++; }
+  }
+
+  {
+    static MyObject *x;
+    @synchronized (x) /* Ok */
+    { dummy++; }
+  }
+
+  {
+    MyObjectTypedef *x;
+    @synchronized (x) /* Ok */
+    { dummy++; }
+  }
+
+  {
+    MyObjectTypedef <MyProtocol> *x;
+    @synchronized (x) /* Ok */
+    { dummy++; }
+  }
+
+  {
+    MyObjectPtrTypedef x;
+    @synchronized (x) /* Ok */
+    { dummy++; }
+  }
+
+  {
+    Class x;
+    @synchronized (x) /* Ok */
+    { dummy++; }
+  }
+
+  @synchronized (1) /* { dg-error ".@synchronized. argument is not an object" } */
+    { dummy++; }
+
+  @synchronized ("Test") /* { dg-error ".@synchronized. argument is not an object" } */
+    { dummy++; }
+
+  @synchronized () /* { dg-error "expected" } */
+    { dummy++; }
+
+  @synchronized (int) /* { dg-error "expected" } */
+    { dummy++; }
+
+  return dummy;
+}
