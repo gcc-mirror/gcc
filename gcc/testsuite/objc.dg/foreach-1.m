5d19c1500460004ffadc7272393e1622116627ba
Iain Sandoe <iains@gcc.gnu.org>
2011-01-08 20:32:32 +0000

    objc-act.c (objc_finish_foreach_loop): Mark collection expression as read.
    gcc/objc:
    
    	* objc-act.c (objc_finish_foreach_loop): Mark collection expression
    	as read.
    
    gcc/testsuite:
    
    	* objc.dg/foreach-1.m: Add "-Wall" to flags.
    
    From-SVN: r168602

--- gcc/testsuite/objc.dg/foreach-1.m
+++ gcc/testsuite/objc.dg/foreach-1.m
@@ -4,6 +4,7 @@
 /* { dg-do run } */
 /* { dg-xfail-run-if "Needs OBJC2 ABI" { *-*-darwin* && { lp64 && { ! objc2 } } } { "-fnext-runtime" } { "" } } */
 /* { dg-additional-sources "../objc-obj-c++-shared/Object1.m" } */
+/* { dg-options "-Wall" } */
 
 #import "../objc-obj-c++-shared/Object1.h"
 
