b52956be02218c3fda87f05f821e971879df83e5
Janus Weil <janus@gcc.gnu.org>
2011-01-09 11:35:50 +0100

    re PR fortran/46313 ([OOP] class container naming collisions)
    2011-01-09  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/46313
    	* class.c (get_unique_type_string): Make type name start with upper
    	case letter.
    
    
    2011-01-09  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/46313
    	* gfortran.dg/class_35.f90: New.
    
    From-SVN: r168610

--- gcc/fortran/class.c
+++ gcc/fortran/class.c
@@ -116,13 +116,16 @@ gfc_class_null_initializer (gfc_typespec *ts)
 
 static void
 get_unique_type_string (char *string, gfc_symbol *derived)
-{  
+{
+  char dt_name[GFC_MAX_SYMBOL_LEN+1];
+  sprintf (dt_name, "%s", derived->name);
+  dt_name[0] = TOUPPER (dt_name[0]);
   if (derived->module)
-    sprintf (string, "%s_%s", derived->module, derived->name);
+    sprintf (string, "%s_%s", derived->module, dt_name);
   else if (derived->ns->proc_name)
-    sprintf (string, "%s_%s", derived->ns->proc_name->name, derived->name);
+    sprintf (string, "%s_%s", derived->ns->proc_name->name, dt_name);
   else
-    sprintf (string, "_%s", derived->name);
+    sprintf (string, "_%s", dt_name);
 }
 
 
---
+++ gcc/testsuite/gfortran.dg/class_35.f90
@@ -0,0 +1,26 @@
+! { dg-do run }
+!
+! PR 46313: [OOP] class container naming collisions
+!
+! Contributed by Tobias Burnus <burnus@gcc.gnu.org>
+
+module one
+  type two_three
+  end type
+end module
+
+module one_two
+  type three
+  end type
+end module
+
+use one
+use one_two
+class(two_three), allocatable :: a1
+class(three), allocatable :: a2
+
+if (same_type_as(a1,a2)) call abort()
+
+end 
+
+! { dg-final { cleanup-modules "one one_two" } }
