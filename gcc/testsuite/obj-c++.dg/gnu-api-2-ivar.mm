20ad86886588d1e419725d20b2cf75129a6ef504
Iain Sandoe <iains@gcc.gnu.org>
2011-01-09 16:59:00 +0000

    gnu-api-2-class.mm: Skip for Darwin < 9.
    gcc/testsuite:
    
    	* obj-c++.dg/gnu-api-2-class.mm: Skip for Darwin < 9.
    	* obj-c++.dg/gnu-api-2-ivar.mm: Likewise.
    	* obj-c++.dg/gnu-api-2-method.mm: Likewise.
    	* obj-c++.dg/gnu-api-2-objc.mm: Likewise.
    	* obj-c++.dg/gnu-api-2-object.mm: Likewise.
    	* obj-c++.dg/gnu-api-2-property.mm: Likewise.
    	* obj-c++.dg/gnu-api-2-protocol.mm: Likewise.
    	* obj-c++.dg/gnu-api-2-sel.mm: Likewise.
    	* objc.dg/gnu-api-2-class.m: Likewise.
    	* objc.dg/gnu-api-2-ivar.m: Likewise.
    	* objc.dg/gnu-api-2-method.m: Likewise.
    	* objc.dg/gnu-api-2-objc.m: Likewise.
    	* objc.dg/gnu-api-2-object.m: Likewise.
    	* objc.dg/gnu-api-2-property.m: Likewise.
    	* objc.dg/gnu-api-2-protocol.m: Likewise.
    	* objc.dg/gnu-api-2-sel.m: Likewise.
    
    From-SVN: r168617

--- gcc/testsuite/obj-c++.dg/gnu-api-2-ivar.mm
+++ gcc/testsuite/obj-c++.dg/gnu-api-2-ivar.mm
@@ -3,6 +3,7 @@
   This is test 'ivar', covering all functions starting with 'ivar'.  */
 
 /* { dg-do run } */
+/* { dg-skip-if "No API#2 pre-Darwin9" { *-*-darwin[5-8]* } { "-fnext-runtime" } { "" } } */
 /* { dg-xfail-run-if "Needs OBJC2 ABI" { *-*-darwin* && { lp64 && { ! objc2 } } } { "-fnext-runtime" } { "" } } */
 
 /* To get the modern GNU Objective-C Runtime API, you include
