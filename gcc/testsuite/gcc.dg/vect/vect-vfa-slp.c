112578854e7453dc8f25858f2cee9ee1d9d5d935
Eric Botcazou <ebotcazou@adacore.com>
2011-01-10 12:51:00 +0000

    re PR testsuite/46230 (several vectorization tests FAIL or XPASS on SPARC)
    	PR testsuite/46230
    	* gcc.dg/vect/vect-vfa-slp.c: Remove XFAIL.
    	* gcc.dg/vect/slp-23.c: Remove XFAIL for SLP.
    	* gcc.dg/vect/slp-35.c: Remove XFAILs.
    	* gcc.dg/vect/no-tree-pre-slp-29.c: Likewise.
    
    From-SVN: r168629

--- gcc/testsuite/gcc.dg/vect/vect-vfa-slp.c
+++ gcc/testsuite/gcc.dg/vect/vect-vfa-slp.c
@@ -52,5 +52,5 @@ main (void)
   return 0;
 } 
 
-/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect"  { xfail vect_no_align } } } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } } */
 /* { dg-final { cleanup-tree-dump "vect" } } */
