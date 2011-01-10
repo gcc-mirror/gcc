06c9eb5136fe0e778cc3a643131eba2a3dfb77a8
Jan Hubicka <jh@suse.cz>
2011-01-10 22:54:33 +0100

    re PR lto/46083 (gcc.dg/initpri1.c FAILs with -flto/-fwhopr (attribute constructor/destructor doesn't work))
    	PR lto/46083
    	* lto-streamer-out.c (pack_ts_function_decl_value_fields): Store
    	DECL_FINI_PRIORITY.
    	* lto-streamer-in.c (unpack_ts_function_decl_value_fields):
    	Restore DECL_FINI_PRIORITY.
    	* gcc.dg/initpri3.c: New testcase.
    
    From-SVN: r168642

--- gcc/lto-streamer-out.c
+++ gcc/lto-streamer-out.c
@@ -496,6 +496,8 @@ pack_ts_function_decl_value_fields (struct bitpack_d *bp, tree expr)
   bp_pack_value (bp, DECL_DISREGARD_INLINE_LIMITS (expr), 1);
   bp_pack_value (bp, DECL_PURE_P (expr), 1);
   bp_pack_value (bp, DECL_LOOPING_CONST_OR_PURE_P (expr), 1);
+  if (DECL_STATIC_DESTRUCTOR (expr))
+    bp_pack_value (bp, DECL_FINI_PRIORITY (expr), HOST_BITS_PER_SHORT);
 }
 
 
