323c999de43a9df3cb58e3acc25c7163854f8991
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-01-10 23:43:54 +0000

    * config/bootstrap-lto.mk: -fuse-linker-plugin is default now; 	pass -fno-lto to STAGEprofile.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@168645 138bc75d-0d04-0410-961f-82ee72b054a4

--- config/bootstrap-lto.mk
+++ config/bootstrap-lto.mk
@@ -1,5 +1,6 @@
 # This option enables LTO for stage2 and stage3.  It requires lto to
 # be enabled for stage1 with --enable-stage1-languages.
 
-STAGE2_CFLAGS += -flto=jobserver -fuse-linker-plugin -frandom-seed=1
-STAGE3_CFLAGS += -flto=jobserver -fuse-linker-plugin -frandom-seed=1
+STAGE2_CFLAGS += -flto=jobserver -frandom-seed=1
+STAGE3_CFLAGS += -flto=jobserver -frandom-seed=1
+STAGEprofile_CFLAGS +=  -fno-lto
