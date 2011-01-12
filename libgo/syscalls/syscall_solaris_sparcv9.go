f2ee78b861b8daab2675e1a24798b1069f763dc0
Ian Lance Taylor <ian@gcc.gnu.org>
2011-01-12 06:34:08 +0000

    Preliminary framework for Solaris support.
    Partly from Rainer Orth.
    
    From-SVN: r168697

---
+++ libgo/syscalls/syscall_solaris_sparcv9.go
@@ -0,0 +1,7 @@
+// syscall_solaris_v9.go -- Solaris sparc9v specific support
+
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package syscall
