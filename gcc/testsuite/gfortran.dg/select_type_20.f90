29a63d67791eb9a7bea3c64425ff3b3494968812
Tobias Burnus <burnus@net-b.de>
2011-01-13 17:32:33 +0100

    re PR fortran/45848 ([OOP] ICE on invalid code in fortran/symbol.c:2410)
    2011-01-13  Tobias Burnus  <burnus@net-b.de>
                Mikael Morin  <mikael@gcc.gnu.org>
    
            PR fortran/45848
            PR fortran/47204
            * gfortran.h (gfc_code): Move union ext's case_list into
            the struct block.
            * dump-parse-tree.c (show_code_node): Adapt by prefixing
            * case_list
            by "block.".
            * frontend-passes.c (gfc_code_walker): Ditto.
            * match.c (gfc_match_goto, gfc_match_call, gfc_match_case,
            gfc_match_type_is, gfc_match_class_is): Ditto.
            * resolve.c (resolve_select, resolve_select_type): Ditto.
            * st.c (gfc_free_statement): Ditto.
            * trans-stmt.c (gfc_trans_integer_select,
            * gfc_trans_logical_select,
            gfc_trans_character_select): Ditto.
            * parse.c (resolve_all_program_units): For error recovery, avoid
            segfault is proc_name is NULL.
    
    2011-01-13  Tobias Burnus  <burnus@net-b.de>
                Mikael Morin  <mikael@gcc.gnu.org>
    
            PR fortran/45848
            PR fortran/47204
            * gfortran.dg/select_type_20.f90: New.
            * gfortran.dg/select_type_21.f90: New.
    
    
    Co-Authored-By: Mikael Morin <mikael@gcc.gnu.org>
    
    From-SVN: r168753

---
+++ gcc/testsuite/gfortran.dg/select_type_20.f90
@@ -0,0 +1,35 @@
+! { dg-do compile }
+! PR fortran/45848
+! PR fortran/47204
+!
+! Contributed by Harald Anlauf and Zdenek Sojka
+!
+module gfcbug111
+  implicit none
+
+  type, abstract :: inner_product_class
+  end type inner_product_class
+
+  type, extends(inner_product_class) :: trivial_inner_product_type
+  end type trivial_inner_product_type
+
+contains
+
+  function my_dot_v_v (this,a,b) ! { dg-error "has no IMPLICIT type" }
+    class(trivial_inner_product_type), intent(in) :: this
+    class(vector_class),               intent(in) :: a,b ! { dg-error "Derived type" }
+    real :: my_dot_v_v
+
+    select type (a)
+    class is (trivial_vector_type) ! { dg-error "Syntax error in CLASS IS" }
+       select type (b) ! { dg-error "Expected TYPE IS" }
+       class is (trivial_vector_type) ! { dg-error "Syntax error in CLASS IS" }
+       class default
+       end select
+    class default ! { dg-error "Unclassifiable statement" }
+    end select ! { dg-error "Expecting END FUNCTION" }
+  end function my_dot_v_v
+end module gfcbug111
+
+select type (a)
+! { dg-excess-errors "Unexpected end of file" }
