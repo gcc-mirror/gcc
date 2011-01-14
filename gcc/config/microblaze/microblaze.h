ebd5c429d312cabef0ba4867320d9c2605a968ac
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-01-14 18:46:24 +0000

    * config/microblaze/microblaze.h (CC1_SPEC): Remove %{save-temps: }. 	* config/mips/mips.h (CC1_SPEC): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@168803 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/microblaze/microblaze.h
+++ gcc/config/microblaze/microblaze.h
@@ -1,5 +1,5 @@
 /* Definitions of target machine for GNU compiler for Xilinx MicroBlaze.
-   Copyright 2009, 2010 Free Software Foundation, Inc.
+   Copyright 2009, 2010, 2011 Free Software Foundation, Inc.
 
    Contributed by Michael Eager <eager@eagercon.com>.
 
@@ -94,7 +94,6 @@ extern enum pipeline_type microblaze_pipe;
 #ifndef CC1_SPEC
 #define CC1_SPEC " \
 %{G*} %{gline:%{!g:%{!g0:%{!g1:%{!g2: -g1}}}}} \
-%{save-temps: } \
 %(subtarget_cc1_spec) \
 %{mxl-multiply-high:-mcpu=v6.00.a} \
 "
