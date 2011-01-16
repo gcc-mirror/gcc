e7b8ec290314a24d67f8256c75b34a6a627c5205
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-01-16 16:44:35 +0000

    PR fortran/46416 	* quadmath.h (cbrtq, finiteq, isnanq, signbitq, sqrtq): Remove 	const from prototype argument. 	(cimagq, conjq, cprojq, crealq, fdimq, fmaxq, fminq, ilogbq, 	llrintq, log2q, lrintq, nearbyintq, remquoq): New prototypes. 	(__quadmath_extern_inline): Define. 	(cimagq, conjq, crealq): New inlines. 	* Makefile.am (libquadmath_la_SOURCES): Add math/cimagq.c, 	math/conjq.c, math/cprojq.c, math/crealq.c, math/fdimq.c, 	math/fmaxq.c, math/fminq.c, math/ilogbq.c, math/llrintq.c, 	math/log2q.c, math/lrintq.c, math/nearbyintq.c and math/remquoq.c. 	* Makefile.in: Regenerated. 	* quadmath_weak.h (cimagq, conjq, cprojq, crealq, fdimq, fmaxq, 	fminq, ilogbq, llrintq, log2q, lrintq, nearbyintq, remquoq): Add. 	* quadmath-imp.h (__LITTLE_ENDIAN__): Don't define. 	(ieee854_float128): Use __BYTE_ORDER == __ORDER_BIG_ENDIAN__ 	tests instead of __BIG_ENDIAN__. 	* quadmath.map (QUADMATH_1.0): Add cimagq, conjq, cprojq, crealq, 	fdimq, fmaxq, fminq, ilogbq, llrintq, log2q, lrintq, nearbyintq 	and remquoq. 	* libquadmath.texi (cimagq, conjq, cprojq, crealq, fdimq, fmaxq, 	fminq, ilogbq, llrintq, log2q, lrintq, nearbyintq, remquoq): Add. 	* math/cprojq.c: New file. 	* math/ilogbq.c: New file. 	* math/fminq.c: New file. 	* math/llrintq.c: New file. 	* math/log2q.c: New file. 	* math/lrintq.c: New file. 	* math/crealq.c: New file. 	* math/nearbyintq.c: New file. 	* math/fmaxq.c: New file. 	* math/conjq.c: New file. 	* math/remquoq.c: New file. 	* math/cimagq.c: New file. 	* math/fdimq.c: New file.
    	* math/ldexpq.c: Include errno.h.  Set errno to ERANGE if needed.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@168854 138bc75d-0d04-0410-961f-82ee72b054a4

--- libquadmath/quadmath.h
+++ libquadmath/quadmath.h
@@ -36,7 +36,7 @@ extern __float128 asinhq (__float128);
 extern __float128 atanq (__float128);
 extern __float128 atanhq (__float128);
 extern __float128 atan2q (__float128, __float128);
-extern __float128 cbrtq (const __float128);
+extern __float128 cbrtq (__float128);
 extern __float128 ceilq (__float128);
 extern __float128 copysignq (__float128, __float128);
 extern __float128 coshq (__float128);
@@ -46,38 +46,47 @@ extern __float128 erfcq (__float128);
 extern __float128 expq (__float128);
 extern __float128 expm1q (__float128);
 extern __float128 fabsq (__float128);
-extern int finiteq (const __float128);
+extern __float128 fdimq (__float128, __float128);
+extern int finiteq (__float128);
 extern __float128 floorq (__float128);
 extern __float128 fmaq (__float128, __float128, __float128);
+extern __float128 fmaxq (__float128, __float128);
+extern __float128 fminq (__float128, __float128);
 extern __float128 fmodq (__float128, __float128);
 extern __float128 frexpq (__float128, int *);
 extern __float128 hypotq (__float128, __float128);
 extern int isinfq (__float128);
-extern int isnanq (const __float128);
+extern int ilogbq (__float128);
+extern int isnanq (__float128);
 extern __float128 j0q (__float128);
 extern __float128 j1q (__float128);
 extern __float128 jnq (int, __float128);
 extern __float128 ldexpq (__float128, int);
 extern __float128 lgammaq (__float128);
+extern long long int llrintq (__float128);
 extern long long int llroundq (__float128);
 extern __float128 logq (__float128);
 extern __float128 log10q (__float128);
+extern __float128 log2q (__float128);
 extern __float128 log1pq (__float128);
+extern long int lrintq (__float128);
 extern long int lroundq (__float128);
 extern __float128 modfq (__float128, __float128 *);
 extern __float128 nanq (const char *);
+extern __float128 nearbyintq (__float128);
 extern __float128 nextafterq (__float128, __float128);
 extern __float128 powq (__float128, __float128);
 extern __float128 remainderq (__float128, __float128);
+extern __float128 remquoq (__float128, __float128, int *);
 extern __float128 rintq (__float128);
 extern __float128 roundq (__float128);
 extern __float128 scalblnq (__float128, long int);
 extern __float128 scalbnq (__float128, int);
-extern int signbitq (const __float128);
+extern int signbitq (__float128);
 extern void sincosq (__float128, __float128 *, __float128 *);
 extern __float128 sinhq (__float128);
 extern __float128 sinq (__float128);
-extern __float128 sqrtq (const __float128);
+extern __float128 sqrtq (__float128);
 extern __float128 tanq (__float128);
 extern __float128 tanhq (__float128);
 extern __float128 tgammaq (__float128);
@@ -90,6 +99,8 @@ extern __float128 ynq (int, __float128);
 // Prototypes for complex functions
 extern __float128 cabsq (__complex128);
 extern __float128 cargq (__complex128);
+extern __float128 cimagq (__complex128);
+extern __float128 crealq (__complex128);
 extern __complex128 cacosq (__complex128);
 extern __complex128 cacoshq (__complex128);
 extern __complex128 casinq (__complex128);
@@ -102,7 +113,9 @@ extern __complex128 cexpq (__complex128);
 extern __complex128 cexpiq (__float128);
 extern __complex128 clogq (__complex128);
 extern __complex128 clog10q (__complex128);
+extern __complex128 conjq (__complex128);
 extern __complex128 cpowq (__complex128, __complex128);
+extern __complex128 cprojq (__complex128);
 extern __complex128 csinq (__complex128);
 extern __complex128 csinhq (__complex128);
 extern __complex128 csqrtq (__complex128);
@@ -145,5 +158,26 @@ extern void quadmath_dtoaq (char *, size_t, size_t, __float128);
 #define M_2_SQRTPIq	1.1283791670955125738961589031215452Q  /* 2/sqrt(pi) */
 #define M_SQRT2q	1.4142135623730950488016887242096981Q  /* sqrt(2) */
 #define M_SQRT1_2q	0.7071067811865475244008443621048490Q  /* 1/sqrt(2) */
+
+#define __quadmath_extern_inline \
+  extern inline __attribute__ ((__gnu_inline__))
+
+__quadmath_extern_inline __float128
+cimagq (__complex128 __z)
+{
+  return __imag__ __z;
+}
+
+__quadmath_extern_inline __float128
+crealq (__complex128 __z)
+{
+  return __real__ __z;
+}
+
+__quadmath_extern_inline __complex128
+conjq (__complex128 __z)
+{
+  return __extension__ ~__z;
+}
 
 #endif
