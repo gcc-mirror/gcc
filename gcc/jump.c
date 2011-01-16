bd4f6389ffe201a822542a68e81d569f58e1fc4c
Jakub Jelinek <jakub@redhat.com>
2011-01-16 21:14:37 +0100

    backport: re PR rtl-optimization/46865 (Using -save-temps (or ccache, distcc) produces different results with multiline macros containing asm code)
    	Backport from mainline
    	2010-12-10  Jakub Jelinek  <jakub@redhat.com>
    
    	PR rtl-optimization/46865
    	* rtl.c (rtx_equal_p_cb, rtx_equal_p): For last operand of
    	ASM_OPERANDS and ASM_INPUT if integers are different,
    	call locator_eq.
    	* jump.c (rtx_renumbered_equal_p): Likewise.
    
    	* gcc.target/i386/pr46865-1.c: New test.
    	* gcc.target/i386/pr46865-2.c: New test.
    
    From-SVN: r168862

--- gcc/jump.c
+++ gcc/jump.c
@@ -1728,7 +1728,13 @@ rtx_renumbered_equal_p (const_rtx x, const_rtx y)
 
 	case 'i':
 	  if (XINT (x, i) != XINT (y, i))
-	    return 0;
+	    {
+	      if (((code == ASM_OPERANDS && i == 6)
+		   || (code == ASM_INPUT && i == 1))
+		  && locator_eq (XINT (x, i), XINT (y, i)))
+		break;
+	      return 0;
+	    }
 	  break;
 
 	case 't':
---
+++ gcc/testsuite/gcc.target/i386/pr46865-2.c
@@ -0,0 +1,32 @@
+/* PR rtl-optimization/46865 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -save-temps" } */
+
+extern unsigned long f;
+
+#define m1(f)							\
+  if (f & 1)							\
+    asm volatile ("nop /* asmnop */\n");			\
+  else								\
+    asm volatile ("nop /* asmnop */\n");
+
+#define m2(f)							\
+  if (f & 1)							\
+    asm volatile ("nop /* asmnop */\n" : : "i" (6) : "cx");	\
+  else								\
+    asm volatile ("nop /* asmnop */\n" : : "i" (6) : "cx");
+
+void
+foo (void)
+{
+  m1 (f);
+}
+
+void
+bar (void)
+{
+  m2 (f);
+}
+
+/* { dg-final { scan-assembler-times "asmnop" 2 } } */
+/* { dg-final { cleanup-saved-temps } } */
