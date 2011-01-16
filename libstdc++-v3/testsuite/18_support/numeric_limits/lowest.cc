89a5f4865a2f96fe71e4df7e5782d65b09b107af
Paolo Carlini <paolo.carlini@oracle.com>
2011-01-16 20:55:30 +0000

    re PR libstdc++/47320 (FAIL: 18_support/numeric_limits/lowest.cc execution test)
    2011-01-16  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR libstdc++/47320
    	* testsuite/18_support/numeric_limits/lowest.cc:
    	Only test wchar_t when _GLIBCXX_USE_WCHAR_T is defined.
    
    From-SVN: r168873

--- libstdc++-v3/testsuite/18_support/numeric_limits/lowest.cc
+++ libstdc++-v3/testsuite/18_support/numeric_limits/lowest.cc
@@ -3,7 +3,7 @@
 
 // 2010-02-25  Ed Smith-Rowland
 
-// Copyright (C) 2010 Free Software Foundation
+// Copyright (C) 2010, 2011 Free Software Foundation
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -54,7 +54,9 @@ void test01()
   do_test<char>();
   do_test<signed char>();
   do_test<unsigned char>();
+#ifdef _GLIBCXX_USE_WCHAR_T
   do_test<wchar_t>();
+#endif
   do_test<char16_t>();
   do_test<char32_t>();
 
