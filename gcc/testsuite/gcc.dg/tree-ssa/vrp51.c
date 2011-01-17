da9d988f03ebf281e54b374fa643568f61294969
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-01-17 20:31:35 +0000

    gcc/testsuite/ 	* gcc.dg/tree-ssa/vrp51.c: Prefix each "bug." string with "vrp.". 	Update dg-final accordingly.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@168933 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/vrp51.c
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp51.c
@@ -10,10 +10,10 @@ v4 (unsigned a, unsigned b)
   if (b < 0x0110) return;
   /* constant true.  */
   if (!__builtin_constant_p ((a|b) >= 0x01000))
-    __asm__("bug.always.true");
+    __asm__("vrp.bug.always.true");
   /* VRP must not think that this is constant.  */
   if (__builtin_constant_p ((a|b) >= 0x10000))
-    __asm__("bug.not.always.true");
+    __asm__("vrp.bug.not.always.true");
 }
 
 void
@@ -23,13 +23,13 @@ u4 (unsigned n)
   if (n < 0x10101) return;
   /* always true.  */
   if (!__builtin_constant_p (n & 0x00100))
-    __asm__("bug.always.true");
+    __asm__("vrp.bug.always.true");
   /* VRP must not think that this is constant true.  */
   if (__builtin_constant_p (n & 0x00001))
-    __asm__("bug.not.always.true");
+    __asm__("vrp.bug.not.always.true");
   /* Out of range, always evaluates to constant false.  */
   if (!__builtin_constant_p (n & 0x01000))
-    __asm__("bug.always.false");
+    __asm__("vrp.bug.always.false");
 }
 
 void
@@ -38,7 +38,7 @@ u5 (unsigned n)
   struct s {unsigned exp:8;} x;
   x.exp = n;
   if (__builtin_constant_p(((n + 1) & 255) > 1))
-    __asm__("bug.not.always.true");
+    __asm__("vrp.bug.not.always.true");
 }
 
 void
@@ -49,10 +49,10 @@ v5 (int a, int b)
   if (b < 0x0110) return;
   /* constant true.  */
   if (!__builtin_constant_p ((a|b) >= 0x01000))
-    __asm__("bug.always.true");
+    __asm__("vrp.bug.always.true");
   /* VRP must not think that this is always true.  */
   if (__builtin_constant_p ((a|b) >= 0x10000))
-    __asm__("bug.not.always.true");
+    __asm__("vrp.bug.not.always.true");
 }
 
-/* { dg-final { scan-assembler-not "bug\." } } */
+/* { dg-final { scan-assembler-not "vrp\\\.bug\\\." } } */
