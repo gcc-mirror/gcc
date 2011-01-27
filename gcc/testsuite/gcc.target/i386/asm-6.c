467fd7909a24165420749615272a713ffefd2a4a
Eric Botcazou <ebotcazou@gcc.gnu.org>
2011-01-27 08:39:33 +0000

    re PR rtl-optimization/44174 (can't find a register in class 'CLOBBERED_REGS' while reloading 'asm')
    	PR rtl-optimization/44174
    	* gcc.target/i386/asm-6.c: New test.
    
    From-SVN: r169325

---
+++ gcc/testsuite/gcc.target/i386/asm-6.c
@@ -0,0 +1,16 @@
+/* PR rtl-optimization/44174 */
+/* Testcase by Jakub Jelinek <jakub@gcc.gnu.org> */
+
+/* { dg-do compile } */
+/* { dg-options "-O2 -fpic" { target fpic } } */
+
+int f0 (int, int, int, int, int);
+int f1 (void);
+
+void
+f2 (void)
+{
+  unsigned v1, v2, v3, v4;
+  __asm__ ("" : "=a" (v1), "=d" (v2), "=c" (v3), "=r" (v4));
+  f0 (f1 (), f1 (), f1 (), f1 (), (v4 >> 8) & 0xff);
+}
