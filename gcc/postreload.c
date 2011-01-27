fd8136415b500178f6555c7e4d0ddbc7cbe93f60
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-01-27 04:33:18 +0000

    Update copyright year
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@169322 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/postreload.c
+++ gcc/postreload.c
@@ -1,7 +1,7 @@
 /* Perform simple optimizations to clean up the result of reload.
    Copyright (C) 1987, 1988, 1989, 1992, 1993, 1994, 1995, 1996, 1997,
    1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
-   2010 Free Software Foundation, Inc.
+   2010, 2011 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
