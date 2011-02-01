30f276c1dc32c199f42be0efd4b79771768ce40d
Benjamin Kosnik <bkoz@redhat.com>
2011-02-01 01:31:42 +0000

    run_doxygen: Allow doxygen 1.7.0 again.
    2011-02-01  Benjamin Kosnik  <bkoz@redhat.com>
    
    	* scripts/run_doxygen: Allow doxygen 1.7.0 again.
    	* doc/doxygen/user.cfg.in (PDF_HYPERLINKS): Re-enable.
    	(COMPACT_LATEX): Enable.
    
    	* include/profile/impl/profiler_container_size.h: Adjust doxygen markup.
    	* include/profile/impl/profiler_hash_func.h: Same.
    	* include/bits/hashtable.h: Same.
    	* include/backward/auto_ptr.h: Same.
    	* include/backward/strstream: Same.
    	* include/backward/backward_warning.h: Same.
    	* include/backward/binders.h: Same.
    
    From-SVN: r169464

--- libstdc++-v3/include/bits/hashtable.h
+++ libstdc++-v3/include/bits/hashtable.h
@@ -24,8 +24,7 @@
 
 /** @file bits/hashtable.h
  *  This is an internal header file, included by other library headers.
- *  You should not attempt to use it directly.
- *  @headername{unordered_map, unordered_set}
+ *  Do not attempt to use it directly. @headername{unordered_map, unordered_set}
  */
 
 #ifndef _HASHTABLE_H
