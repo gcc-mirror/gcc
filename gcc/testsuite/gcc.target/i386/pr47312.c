d325c8f403481791b665eaf58c94f9d30d38ba56
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-02-03 10:53:19 +0000

    PR target/47312 	* expr.c (expand_expr_real_2) <case FMA_EXPR>: If target doesn't expand 	fma, expand FMA_EXPR as fma{,f,l} call.
    	* gcc.target/i386/pr47312.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@169786 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr47312.c
@@ -0,0 +1,23 @@
+/* PR target/47312 */
+/* { dg-do link } */
+/* { dg-require-effective-target lto } */
+/* { dg-require-effective-target xop } */
+/* { dg-options "-O -flto -mno-sse3 -mxop" } */
+
+extern double fma (double, double, double);
+extern float fmaf (float, float, float);
+extern long double fmal (long double, long double, long double);
+
+volatile float f;
+volatile double d;
+volatile long double ld;
+
+int
+main ()
+{
+  f = fmaf (f, f, f);
+  d = fma (d, d, d);
+  ld = fmal (ld, ld, ld);
+  asm volatile ("" : : "r" (&f), "r" (&d), "r" (&ld) : "memory");
+  return 0;
+}
