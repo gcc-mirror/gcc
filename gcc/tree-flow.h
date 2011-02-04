351b6ccf86d48ad98795df9defe0af7f35fdd66d
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-02-04 15:50:51 +0000

    PR inline-asm/23200 	* tree-ssa-ter.c (is_replaceable_p): Add TER argument.  Don't 	do bb, locus and block comparison and disallow loads if it is 	not set. 	(stmt_is_replaceable_p): New function. 	(process_replaceable, find_replaceable_in_bb): Adjust is_replaceable_p 	callers. 	* expr.c (expand_expr_real_1) <case SSA_NAME>: If 	get_gimple_for_ssa_name try for EXPAND_INITIALIZER harder to use 	SSA_NAME_DEF_STMT. 	* tree-flow.h (stmt_is_replaceable_p): New prototype.
    	* gcc.dg/pr23200.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@169831 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-flow.h
+++ gcc/tree-flow.h
@@ -1,5 +1,5 @@
 /* Data and Control Flow Analysis for Trees.
-   Copyright (C) 2001, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010
+   Copyright (C) 2001, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011
    Free Software Foundation, Inc.
    Contributed by Diego Novillo <dnovillo@redhat.com>
 
@@ -851,6 +851,9 @@ bool fixup_noreturn_call (gimple stmt);
 
 /* In ipa-pure-const.c  */
 void warn_function_noreturn (tree);
+
+/* In tree-ssa-ter.c  */
+bool stmt_is_replaceable_p (gimple);
 
 #include "tree-flow-inline.h"
 
