d2f64ff5003d3cde1a58440bede1357907177339
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2011-02-07 16:33:00 +0000

    backport: target-supports.exp (check_avx_hw_available): New procedure.
    	Backport from mainline:
    	2010-07-23  Uros Bizjak  <ubizjak@gmail.com>
    
    	* lib/target-supports.exp (check_avx_hw_available): New procedure.
    	(check_effective_target_avx_runtime): New procedure.
    
    	* gcc.dg/compat/vector-1b_main.c: Use avx_runtime effective target.
    	Remove cpuid.h include and __get_cpuid test.
    	* gcc.dg/compat/vector-2b_main.c: Ditto.
    
    	* gcc.target/i386/avx-check.h (main): Also check bit_OSXSAVE.
    
    From-SVN: r169887

--- gcc/testsuite/gcc.dg/compat/vector-1b_main.c
+++ gcc/testsuite/gcc.dg/compat/vector-1b_main.c
@@ -1,11 +1,9 @@
 /* { dg-skip-if "test AVX vector" { ! { i?86-*-* x86_64-*-* } } } */
-/* { dg-require-effective-target avx } */
+/* { dg-require-effective-target avx_runtime } */
 
 /* Test compatibility of vector types: layout between separately-compiled
    modules, parameter passing, and function return.  This test uses
    vectors of integer values.  */
-
-#include "cpuid.h"
 
 extern void vector_1_x (void);
 extern void exit (int);
@@ -14,14 +12,6 @@ int fails;
 int
 main ()
 {
-  unsigned int eax, ebx, ecx, edx;
-
-  if (!__get_cpuid (1, &eax, &ebx, &ecx, &edx))
-    return 0;
-
-  /* Run AVX vector test only if host has AVX support.  */
-  if (ecx & bit_AVX)
-    vector_1_x ();
-
+  vector_1_x ();
   exit (0);
 }
