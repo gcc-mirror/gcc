689ff841b4de850cb9f31fad4e9a217b682b593f
Iain Sandoe <iains@gcc.gnu.org>
2011-02-07 21:46:10 +0000

    re PR target/47558 (163267 breaks exception traceback in xplor-nih)
    2011-02-07  Iain Sandoe  <iains@gcc.gnu.org>
    
    	PR target/47558
    	* config/darwin10.h (LINK_GCC_C_SEQUENCE_SPEC): Put -lSystem first
    	on 10.6 and later to ensure that we always use the unwinder from
    	the system.  Only add -no_compact_unwind when tarteting darwin
    	10.6 or later.
    
    From-SVN: r169905

--- gcc/config/darwin10.h
+++ gcc/config/darwin10.h
@@ -1,5 +1,5 @@
 /* Target definitions for Darwin (Mac OS X) systems.
-   Copyright (C) 2009, 2010 Free Software Foundation, Inc.
+   Copyright (C) 2009, 2010, 2011 Free Software Foundation, Inc.
    Contributed by Jack Howarth <howarth@bromo.med.uc.edu>.
 
 This file is part of GCC.
@@ -18,8 +18,15 @@ You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
 
-/* Fix PR41260 by passing -no_compact_unwind on darwin10 and later until
-unwinder in libSystem is fixed to digest new epilog unwinding notes. */
-
 #undef LIB_SPEC
-#define LIB_SPEC "%{!static:-no_compact_unwind -lSystem}"
+#define LIB_SPEC "%{!static: -lSystem }"
+
+/* Fix PR41260 by passing -no_compact_unwind on darwin10 and later until
+   unwinder in libSystem is fixed to digest new epilog unwinding notes.
+
+   Fix PR47558 by linking against libSystem ahead of libgcc_ext. */
+#undef  LINK_GCC_C_SEQUENCE_SPEC
+#define LINK_GCC_C_SEQUENCE_SPEC \
+"%:version-compare(>= 10.6 mmacosx-version-min= -no_compact_unwind) \
+   %{!static:%{!static-libgcc: \
+      %:version-compare(>= 10.6 mmacosx-version-min= -lSystem) } } %G %L"
