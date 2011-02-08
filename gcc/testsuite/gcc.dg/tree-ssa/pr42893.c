9a7ea8bfbae6bef2d592f7f806e2f9c9e012fb47
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-02-08 18:54:12 +0000

    PR tree-optimization/42893 	* gcc.tree-ssa/pr42893.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@169933 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr42893.c
@@ -0,0 +1,30 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 -fdump-tree-optimized" } */
+
+struct frame_info;
+void tui_registers_changed_hook (void);
+extern struct frame_info *deprecated_selected_frame;
+int tui_refreshing_registers = 0;
+void
+tui_registers_changed_hook (void)
+{
+  struct frame_info *fi;
+  fi = deprecated_selected_frame;
+  if (fi)
+
+    {
+      if (tui_refreshing_registers == 0)
+
+	{
+	  tui_refreshing_registers = 1;
+	  tui_refreshing_registers = 0;
+	}
+    }
+  return;
+}
+
+/* No references to tui_refreshing_registers should remain.  */
+/* { dg-final { scan-tree-dump-not "tui_refreshing_registers" "optimized"} } */
+
+/* { dg-final { cleanup-tree-dump "optimized" } } */
+
