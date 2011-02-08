272d26c4e9c21945abb17c93a1e7e5a59c34b13a
uweigand <uweigand@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-02-08 00:10:51 +0000

    * g++.dg/abi/packed1.C: Expect warning on spu-*-* as well.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@169908 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/abi/packed1.C
+++ gcc/testsuite/g++.dg/abi/packed1.C
@@ -4,7 +4,7 @@
 
 extern "C" void abort ();
 
-struct INNER { // { dg-warning "inefficient.*vptr" "" { target alpha*-*-* ia64-*-* hppa*-*-* mips-sgi-irix* sparc*-*-* sh*-*-* } }
+struct INNER { // { dg-warning "inefficient.*vptr" "" { target alpha*-*-* ia64-*-* hppa*-*-* mips-sgi-irix* sparc*-*-* spu-*-* sh*-*-* } }
   virtual int foo() const { return 1; }
 } __attribute__ ((packed));
 
