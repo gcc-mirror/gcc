7ca6c614a97d44d32814518bc290aae4062b0763
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2011-02-10 17:27:25 +0000

    re PR target/46610 (gcc.target/mips/save-restore-?.c ICEs on IRIX 6.5: unrecognizable insn)
    	PR target/46610
    	* gcc.target/mips/save-restore-1.c: Skip on mips-sgi-irix6*.
    	* gcc.target/mips/save-restore-3.c: Likewise.
    	* gcc.target/mips/save-restore-4.c: Likewise.
    	* gcc.target/mips/save-restore-5.c: Likewise.
    
    From-SVN: r170014

--- gcc/testsuite/gcc.target/mips/save-restore-3.c
+++ gcc/testsuite/gcc.target/mips/save-restore-3.c
@@ -1,6 +1,7 @@
 /* Check that we can use the save instruction to save spilled arguments
    when the argument save area is out of range of a direct load or store.  */
 /* { dg-options "(-mips16) isa_rev>=1 -mabi=32 -O2" } */
+/* { dg-skip-if "PR target/46610" { mips-sgi-irix6* } } */
 
 void bar (int *);
 
