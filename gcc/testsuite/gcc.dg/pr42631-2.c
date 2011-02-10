06c969bd2687012065ba4555e947b6124358f8fe
Jie Zhang <jie@codesourcery.com>
2011-02-10 04:22:44 +0000

    re PR testsuite/47622 (FAIL: gcc.dg/pr42631.c scan-rtl-dump-not web "Web oldreg")
    	PR testsuite/47622
    	Revert
    	2011-02-05  Jie Zhang  <jie@codesourcery.com>
    	PR debug/42631
    	* web.c (entry_register): Don't clobber the number of the
    	first uninitialized reference in used[].
    
    	testsuite/
    	PR testsuite/47622
    	Revert
    	2011-02-05  Jie Zhang  <jie@codesourcery.com>
    	PR debug/42631
    	* gcc.dg/pr42631.c: Update test.
    	* gcc.dg/pr42631-2.c: New test.
    
    From-SVN: r169997

--- gcc/testsuite/gcc.dg/pr42631-2.c
+++
@@ -1,9 +0,0 @@
-/* { dg-do compile } */
-/* { dg-options "-O2 -funroll-loops -fdump-rtl-web" } */
-
-foo()
-{
-}
-
-/* { dg-final { scan-rtl-dump-not "Web oldreg" "web" } } */
-/* { dg-final { cleanup-rtl-dump "web" } } */
