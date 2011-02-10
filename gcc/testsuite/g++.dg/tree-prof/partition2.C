bacbe9e0799398ca8c04723809dfcc0ad09d98b7
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-02-10 17:14:15 +0000

    PR target/46610 	* gcc.target/mips/save-restore-1.c: Skip on mips-sgi-irix6*. 	* gcc.target/mips/save-restore-3.c: Likewise. 	* gcc.target/mips/save-restore-4.c: Likewise. 	* gcc.target/mips/save-restore-5.c: Likewise.
    	PR target/47683
    	* g++.dg/tree-prof/partition1.C: Skip on mips-sgi-irix*.
    	* g++.dg/tree-prof/partition2.C: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@170012 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/tree-prof/partition2.C
+++ gcc/testsuite/g++.dg/tree-prof/partition2.C
@@ -1,6 +1,7 @@
 // PR middle-end/45458
 // { dg-require-effective-target freorder }
 // { dg-options "-fnon-call-exceptions -freorder-blocks-and-partition" }
+// { dg-skip-if "PR target/47683" { mips-sgi-irix* } }
 
 int
 main ()
