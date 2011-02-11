84b44e0fe402a8cb253695e2d09b6a0051c52f19
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2011-02-11 11:09:21 +0000

    re PR target/47629 (gcc.target/i386/pr47312.c fails to link on Solaris 9)
    	PR target/47629
    	* gcc.target/i386/pr47312.c: Use dg-require-effective-target
    	c99_runtime, dg-add-options c99_runtime.
    	(main): Use __asm__.
    
    From-SVN: r170049

--- gcc/testsuite/gcc.target/i386/pr47312.c
+++ gcc/testsuite/gcc.target/i386/pr47312.c
@@ -2,7 +2,9 @@
 /* { dg-do link } */
 /* { dg-require-effective-target lto } */
 /* { dg-require-effective-target xop } */
+/* { dg-require-effective-target c99_runtime } */
 /* { dg-options "-O -flto -mno-sse3 -mxop" } */
+/* { dg-add-options c99_runtime } */
 
 extern double fma (double, double, double);
 extern float fmaf (float, float, float);
@@ -18,6 +20,6 @@ main ()
   f = fmaf (f, f, f);
   d = fma (d, d, d);
   ld = fmal (ld, ld, ld);
-  asm volatile ("" : : "r" (&f), "r" (&d), "r" (&ld) : "memory");
+  __asm__ volatile ("" : : "r" (&f), "r" (&d), "r" (&ld) : "memory");
   return 0;
 }
