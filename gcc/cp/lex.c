dd2e44f0f27961dd558b55d417dd5e780a9fc1b1
Iain Sandoe <iains@gcc.gnu.org>
2011-02-13 19:57:30 +0000

    Revert r170108, r170107, r170105, r170104, r170103, r170102, r170101, r170097.
    From-SVN: r170111

--- gcc/cp/lex.c
+++ gcc/cp/lex.c
@@ -33,7 +33,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "cpplib.h"
 #include "flags.h"
 #include "c-family/c-pragma.h"
-#include "objcp/plugin/lex.h"
+#include "c-family/c-objc.h"
 #include "output.h"
 #include "tm_p.h"
 #include "timevar.h"
@@ -449,7 +449,7 @@ unqualified_name_lookup_error (tree name)
     }
   else
     {
-      if (!PLUGIN_UNQUALIFIED_NAME_LOOKUP_ERROR (name))
+      if (!objc_diagnose_private_ivar (name))
 	{
 	  error ("%qD was not declared in this scope", name);
 	  suggest_alternatives_for (location_of (name), name);
