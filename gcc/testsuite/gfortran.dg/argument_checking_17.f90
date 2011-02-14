18b98502acb761c74a43e716e8f433783b191a8e
Tobias Burnus <burnus@gcc.gnu.org>
2011-02-14 15:05:52 +0100

    re PR fortran/47569 (gfortran does not detect that the parameters for passing a partial string to a subroutine are incorrect)
    2011-02-14  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/47569
            * interface.c (compare_parameter): Avoid ICE with
            character components.
    
    2011-02-14  Tobias Burnus  <burnus@net-b.de>
    
            * gfortran.dg/argument_checking_13.f90: Update dg-error.
            * gfortran.dg/argument_checking_17.f90: New.
    
    From-SVN: r170129

---
+++ gcc/testsuite/gfortran.dg/argument_checking_17.f90
@@ -0,0 +1,26 @@
+! { dg-do compile }
+!
+! PR fortran/47569
+!
+! Contributed by Jos de Kloe 
+!
+module teststr
+  implicit none
+  integer, parameter :: GRH_SIZE = 20, NMAX = 41624
+  type strtype
+    integer   :: size
+    character :: mdr(NMAX)
+  end type strtype
+contains
+  subroutine sub2(string,str_size)
+    integer,intent(in)    :: str_size
+    character,intent(out) :: string(str_size)
+    string(:) = 'a'
+  end subroutine sub2
+  subroutine sub1(a)
+    type(strtype),intent(inout) :: a
+    call sub2(a%mdr(GRH_SIZE+1),a%size-GRH_SIZE)
+  end subroutine sub1
+end module teststr
+
+! { dg-final { cleanup-modules "teststr" } }
