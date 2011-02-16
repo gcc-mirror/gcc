45cab1d72e5a1702c0abf9d91c07dec5e42f110b
Joseph Myers <joseph@codesourcery.com>
2011-02-16 22:23:01 +0000

    lm32.h (ASM_SPEC): Use %{muser-enabled} instead of %{muser-extend-enabled}.
    	* config/lm32/lm32.h (ASM_SPEC): Use %{muser-enabled} instead of
    	%{muser-extend-enabled}.
    
    From-SVN: r170224

--- gcc/config/lm32/lm32.h
+++ gcc/config/lm32/lm32.h
@@ -1,7 +1,7 @@
 /* Definitions of target machine for GNU compiler, Lattice Mico32 architecture.
    Contributed by Jon Beniston <jon@beniston.com>
 
-   Copyright (C) 2009, 2010 Free Software Foundation, Inc.
+   Copyright (C) 2009, 2010, 2011 Free Software Foundation, Inc.
 
    This file is part of GCC.
 
@@ -54,7 +54,7 @@
 %{mdivide-enabled} \
 %{mbarrel-shift-enabled} \
 %{msign-extend-enabled} \
-%{muser-extend-enabled} \
+%{muser-enabled} \
 "
 
 /* Let link script define all link options. 
