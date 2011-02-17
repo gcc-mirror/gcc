3032caa3aa2d2bd8aafed6e4be4738781770de9c
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-02-17 00:15:11 +0000

    * config/sh/embed-elf.h (LIBGCC_SPEC): Match -m4-340 instead of 	--m4-340.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@170230 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sh/embed-elf.h
+++ gcc/config/sh/embed-elf.h
@@ -1,6 +1,6 @@
 /* Definitions of target machine for GNU compiler for Renesas / SuperH SH 
    non-Linux embedded targets.
-   Copyright (C) 2002, 2003, 2007, 2010 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2003, 2007, 2010, 2011 Free Software Foundation, Inc.
    Contributed by J"orn Rennecke <joern.rennecke@superh.com>
 
 This file is part of GCC.
@@ -29,7 +29,7 @@ along with GCC; see the file COPYING3.  If not see
 #define LIBGCC_SPEC "%{!shared: \
   %{m4-100*:-lic_invalidate_array_4-100} \
   %{m4-200*:-lic_invalidate_array_4-200} \
-  %{m4-300*|-m4-340:-lic_invalidate_array_4a %{!Os: -lgcc-4-300}} \
+  %{m4-300*|m4-340:-lic_invalidate_array_4a %{!Os: -lgcc-4-300}} \
   %{m4a*:-lic_invalidate_array_4a}} \
   %{Os: -lgcc-Os-4-200} \
   -lgcc \
