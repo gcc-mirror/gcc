b3c1b8a1d6838854acf96be354339a62ff27599e
Michael Matz <matz@suse.de>
2011-02-18 19:52:16 +0000

    re PR fortran/45586 (ICE non-trivial conversion at assignment)
    	PR fortran/45586
    	* gfortran.h (struct gfc_component): Add norestrict_decl member.
    	* trans.h (struct lang_type): Add nonrestricted_type member.
    	* trans-expr.c (gfc_conv_component_ref): Search fields with correct
    	parent type.
    	* trans-types.c (mirror_fields, gfc_nonrestricted_type): New.
    	(gfc_sym_type): Use it.
    
    testsuite/
    	PR fortran/45586
    	* gfortran.dg/lto/pr45586_0.f90: New test.
    	* gfortran.dg/typebound_proc_20.f90: Ditto.
    	* gfortran.dg/typebound_proc_21.f90: Ditto.
    
    From-SVN: r170284

---
+++ gcc/testsuite/gfortran.dg/typebound_proc_21.f90
@@ -0,0 +1,27 @@
+! { dg-do compile }
+!
+! PR fortran/47455
+!
+module class_t
+    type :: tx
+        integer, dimension(:), allocatable :: i
+    end type tx
+    type :: t
+        type(tx), pointer :: x
+    contains
+        procedure :: calc
+        procedure :: find_x
+    end type t
+contains
+    subroutine calc(this)
+        class(t), target :: this
+        this%x = this%find_x()
+    end subroutine calc
+    function find_x(this)
+        class(t), intent(in) :: this
+        type(tx), pointer :: find_x
+        find_x => null()
+    end function find_x
+end module class_t
+
+! { dg-final { cleanup-modules "class_t" } }
