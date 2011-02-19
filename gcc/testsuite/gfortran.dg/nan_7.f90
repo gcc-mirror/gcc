f3c81b78c7d29865ef357b6477d6597140e505bc
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-02-19 15:31:15 +0000

    2011-02-19  Jerry DeLisle  <jvdelisle@gcc.gnu.org> 	    Tobias Burnus  <burnus@net-b.de>
    	* lib/target-supports.exp
    	(check_effective_target_fortran_large_real): New check for large reals.
    	* gfortran.dg/nan_7.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@170320 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/nan_7.f90
@@ -0,0 +1,15 @@
+! { dg-do run }
+! { dg-options "-fno-range-check" }
+! { dg-require-effective-target fortran_real_16 }
+! { dg-require-effective-target fortran_integer_16 }
+! PR47293 NAN not correctly read
+character(len=200) :: str
+real(16) :: r
+integer(16) :: k2
+integer(16), parameter :: quietnan = 170099645085600953110659059745250344960
+r = 1.0
+str = 'NAN' ; read(str,*) r
+k2 = transfer(r,k2)
+k2 = iand(k2, z'fff80000000000000000000000000000')
+if (k2.ne.quietnan) call abort
+end
