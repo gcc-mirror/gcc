e3cc08e9b5e8cfdfcf138e579b6c1d85f3b9b2df
Tobias Burnus <burnus@net-b.de>
2011-02-27 15:29:59 +0100

    backport: [multiple changes]
    2011-01-27  Tobias Burnus  <burnus@net-b.de>
    
            Backport from mainline
            2011-02-26  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/47886
            * openmp.c (gfc_resolve_omp_directive): Resolve if()
            condition of OpenMP's task.
    
    2011-01-27  Tobias Burnus  <burnus@net-b.de>
    
            Backport from mainline
            2011-02-27  Jakub Jelinek  <jakub@redhat.com>
    
            PR fortran/47886
            * testsuite/libgomp.fortran/task3.f90: New test.
    
    From-SVN: r170541

---
+++ libgomp/testsuite/libgomp.fortran/task3.f90
@@ -0,0 +1,27 @@
+! { dg-do run }
+! { dg-options "-fopenmp" }
+!
+! PR fortran/47886
+!
+! Test case contributed by Bill Long
+
+!  derived from OpenMP test OMP3f/F03_2_7_1d.F90
+program F03_2_7_1d
+   use omp_lib
+   implicit none
+   integer, parameter :: NT = 4
+   integer :: sum = 0
+
+   call omp_set_num_threads(NT); 
+
+   !$omp parallel
+   !$omp task if(omp_get_num_threads() > 0)
+   !$omp atomic
+      sum = sum + 1
+   !$omp end task
+   !$omp end parallel
+   if (sum /= NT) then
+      print *, "FAIL - sum == ", sum, " (expected ", NT, ")"
+      call abort
+   end if
+end program F03_2_7_1d
