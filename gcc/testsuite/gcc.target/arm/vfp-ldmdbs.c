d82e2f006b2921267ce00ddd84b609f20b3f70b9
ams <ams@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-02-28 11:43:25 +0000

    2011-02-28  Kazu Hirata  <kazu@codesourcery.com>
    	gcc/testsuite/
    	* gcc.target/arm/vfp-ldmdbd.c, gcc.target/arm/vfp-ldmdbs.c,
    	gcc.target/arm/vfp-ldmiad.c, gcc.target/arm/vfp-ldmias.c,
    	gcc.target/arm/vfp-stmdbd.c, gcc.target/arm/vfp-stmdbs.c,
    	gcc.target/arm/vfp-stmiad.c, gcc.target/arm/vfp-stmias.c: New.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@170559 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/vfp-ldmdbs.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target arm_vfp_ok } */
+/* { dg-options "-O2 -mfpu=vfp -mfloat-abi=softfp" } */
+
+extern void baz (float);
+
+void
+foo (float *p, float a, int n)
+{
+  do
+    bar (*--p + a);
+  while (n--);
+}
+
+/* { dg-final { scan-assembler "fldmdbs" } } */
