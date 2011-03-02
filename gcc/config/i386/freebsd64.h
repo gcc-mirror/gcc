46d8fabb202d1107d3179f4b9928152ea74a72d7
Joseph Myers <joseph@codesourcery.com>
2011-03-02 22:35:36 +0000

    freebsd64.h (CC1_SPEC): Define.
    	* config/i386/freebsd64.h (CC1_SPEC): Define.
    	* config/i386/linux64.h (CC1_SPEC): Define.
    	* config/i386/x86-64.h (CC1_SPEC): Don't define.
    
    From-SVN: r170625

--- gcc/config/i386/freebsd64.h
+++ gcc/config/i386/freebsd64.h
@@ -1,5 +1,5 @@
 /* Definitions for AMD x86-64 running FreeBSD with ELF format
-   Copyright (C) 2002, 2004, 2007, 2010 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2004, 2007, 2010, 2011 Free Software Foundation, Inc.
    Contributed by David O'Brien <obrien@FreeBSD.org>
 
 This file is part of GCC.
@@ -24,6 +24,9 @@ along with GCC; see the file COPYING3.  If not see
 
 #define SUBTARGET_EXTRA_SPECS \
   { "fbsd_dynamic_linker", FBSD_DYNAMIC_LINKER }
+
+#undef CC1_SPEC
+#define CC1_SPEC "%(cc1_cpu) %{profile:-p}"
 
 /* Provide a LINK_SPEC appropriate for the FreeBSD/x86-64 ELF target.
    This is a copy of LINK_SPEC from <i386/freebsd.h> tweaked for
