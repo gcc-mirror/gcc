8fddbce4ab902f855472c30dd1c8bb713854e3c4
Marc Glisse <marc.glisse@normalesup.org>
2011-03-02 14:57:57 +0000

    re PR libstdc++/47913 ([C++0x] improve ratio_add to overflow less often)
    2011-03-02  Marc Glisse  <marc.glisse@normalesup.org>
    
    	PR libstdc++/47913
    	* include/std/ratio (ratio_add): Avoid denominator overflow.
    	* testsuite/20_util/ratio/operations/47913.cc: New.
    
    From-SVN: r170616

---
+++ libstdc++-v3/testsuite/20_util/ratio/operations/47913.cc
@@ -0,0 +1,42 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-require-cstdint "" }
+
+// Copyright (C) 2011 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <ratio>
+#include <testsuite_hooks.h>
+
+// libstdc++/47913
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace std;
+
+  const intmax_t m = (intmax_t)1 << (4 * sizeof(intmax_t) - 1);
+  typedef ratio_add<ratio<1, (m - 1) * (m - 2)>,
+                    ratio<1, (m - 3) * (m - 2)> > ra_type;
+
+  VERIFY( ra_type::num == 2 );
+  VERIFY( ra_type::den == (m - 1) * (m - 3) );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
