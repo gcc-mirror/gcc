13f1e629cddb0d26cdde206d26ff7a303bed2a34
Jakub Jelinek <jakub@redhat.com>
2011-03-05 01:01:32 +0100

    * gcc.dg/torture/pr47968.c: Ignore warnings.
    From-SVN: r170691

--- gcc/testsuite/gcc.dg/torture/pr47968.c
+++ gcc/testsuite/gcc.dg/torture/pr47968.c
@@ -1,4 +1,5 @@
 /* { dg-do compile } */
+/* { dg-options "-w -Wno-psabi" } */
 
 typedef __attribute__ ((vector_size (16))) float float4;
 typedef __attribute__ ((vector_size (16))) double double2;
@@ -8,4 +9,3 @@ float foo (double2 d2)
   float4 f4 = (float4) d2;
   return *(float *) &f4;
 }
-
