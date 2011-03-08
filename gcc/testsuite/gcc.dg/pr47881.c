7a76e05f93e3fbf553729d92022cf842b01fec49
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-08 15:51:12 +0000

    PR debug/47881 	* ira.c (ira): Call df_analyze again if delete_trivially_dead_insns 	removed anything.
    	* gcc.dg/pr47881.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@170780 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr47881.c
@@ -0,0 +1,24 @@
+/* PR debug/47881 */
+/* { dg-do compile } */
+/* { dg-options "-O -fcompare-debug -fno-dce -funroll-loops -fno-web" } */
+
+extern int data[];
+
+int
+foo (int *t, int *f, int n)
+{
+  int i = 0, a, b, c, d;
+  while (data[*f] && n)
+    n--;
+  for (; i < n; i += 4)
+    {
+      a = data[*(f++) & 0x7f];
+      c = data[*(f++) & 0x7f];
+      c = data[*(f++) & 0x7f];
+      d = data[*(f++) & 0x7f];
+      if ((a & 0x80) || (b & 0x80) || (c & 0x80) || (d & 0x80))
+	return 1;
+      *(t++) = 16;
+    }
+  return 0;
+}
