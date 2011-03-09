32578a980af524adf13bf6f27093d1f1ec4439e6
jamborm <jamborm@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-09 16:59:55 +0000

    2011-03-09  Martin Jambor  <mjambor@suse.cz>
    	PR tree-optimization/47714
    	* cp/method.c (use_thunk): Clear addressable flag of thunk arguments.
    
    	* testsuite/g++.dg/torture/pr47714.C: New test.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@170822 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/method.c
+++ gcc/cp/method.c
@@ -372,6 +372,7 @@ use_thunk (tree thunk_fndecl, bool emit_p)
       DECL_CONTEXT (x) = thunk_fndecl;
       SET_DECL_RTL (x, NULL);
       DECL_HAS_VALUE_EXPR_P (x) = 0;
+      TREE_ADDRESSABLE (x) = 0;
       t = x;
     }
   a = nreverse (t);
