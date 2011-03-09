6c02db4e499a1d582ccd6b776df854d155d435e7
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-09 22:13:09 +0000

    Solaris specific syslog support.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@170837 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libgo/go/syslog/syslog_c.c
@@ -0,0 +1,19 @@
+/* syslog_c.c -- call syslog for Go.
+
+   Copyright 2011 The Go Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style
+   license that can be found in the LICENSE file.  */
+
+#include <syslog.h>
+
+/* We need to use a C function to call the syslog function, because we
+   can't represent a C varargs function in Go.  */
+
+void syslog_c(int, const char*)
+  asm ("libgo_syslog.syslog.syslog_c");
+
+void
+syslog_c (int priority, const char *msg)
+{
+  syslog (priority, "%s", msg);
+}
