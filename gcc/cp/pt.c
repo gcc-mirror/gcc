186dd96cfa5e25a11c917c650ff0e953ce9d6a5e
Jason Merrill <jason@redhat.com>
2011-03-09 12:57:48 -0500

    re PR c++/44629 (ICE in unify, at cp/pt.c:15155)
    	PR c++/44629
    	* pt.c (unify): An unresolved overload is a nondeduced context.
    
    From-SVN: r170825

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -14125,6 +14125,9 @@ unify (tree tparms, tree targs, tree parm, tree arg, int strict)
       return 1;
 
     default:
+      /* An unresolved overload is a nondeduced context.  */
+      if (type_unknown_p (parm))
+	return 0;
       gcc_assert (EXPR_P (parm));
 
       /* We must be looking at an expression.  This can happen with
---
+++ gcc/testsuite/g++.dg/template/nontype22.C
@@ -0,0 +1,11 @@
+// PR c++/44629
+// The proper mangling is unclear.
+
+template<typename T> int cmp1(T a, T b);
+template<typename T, int (*cmp)(T, T) = cmp1> struct A { };
+template <typename T> void f (A<T> &);
+void g()
+{
+  A<char> a;
+  f(a);				// { dg-bogus "" "" { xfail *-*-* } }
+}
