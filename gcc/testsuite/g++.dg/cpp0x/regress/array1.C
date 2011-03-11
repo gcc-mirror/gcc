e8d6ec55fd0e42332ef3014312c5b139c20d0fa8
Jason Merrill <jason@redhat.com>
2011-03-11 11:49:41 -0500

    re PR c++/47808 ([C++0x] internal compiler error: in tsubst_copy_and_build, at cp/pt.c:13326)
    	PR c++/47808
    	* decl.c (compute_array_index_type): Discard folding
    	if it didn't produce a constant.
    
    From-SVN: r170878

---
+++ gcc/testsuite/g++.dg/cpp0x/regress/array1.C
@@ -0,0 +1,16 @@
+// PR c++/47808
+// { dg-options -std=c++0x }
+
+template <typename T>
+inline T abs (T const & x) { return x; }
+
+template <typename T>
+void f (T)
+{
+  typedef int ai[(abs(0.1) > 0) ? 1 : -1];
+}
+
+int main()
+{
+  f(1);
+}
