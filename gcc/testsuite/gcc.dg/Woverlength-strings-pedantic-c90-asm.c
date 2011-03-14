87f9e23dafd42a3ad8df6feb6cc413e3aa6f110b
Tom Tromey <tromey@redhat.com>
2011-03-14 18:01:10 +0000

    c-parser.c (c_parser_asm_string_literal): Clear warn_overlength_strings.
    gcc
    	* c-parser.c (c_parser_asm_string_literal): Clear
    	warn_overlength_strings.
    gcc/testsuite
    	* gcc.dg/Woverlength-strings-pedantic-c90-asm.c: New file.
    	* gcc.dg/Woverlength-strings-pedantic-c89-asm.c: New file.
    	* gcc.dg/Woverlength-strings-pedantic-c99-asm.c: New file.
    
    From-SVN: r170948

---
+++ gcc/testsuite/gcc.dg/Woverlength-strings-pedantic-c90-asm.c
@@ -0,0 +1,48 @@
+/* -Woverlength-strings complains about string constants which are too long
+   for the C standard's "minimum maximum" limits.  It is off by default,
+   but implied by -pedantic.  */
+
+/* { dg-options "-std=c90 -pedantic" } */
+
+#define TEN "          "
+#define HUN TEN TEN TEN TEN TEN  TEN TEN TEN TEN TEN
+#define THO HUN HUN HUN HUN HUN  HUN HUN HUN HUN HUN
+
+/* C89's minimum-maximum is 509. */
+__asm__ (HUN HUN HUN HUN HUN TEN);
+
+/* C99's minimum-maximum is 4095.  */
+__asm__ (
+  THO THO THO THO     /* 4000 */
+  TEN TEN TEN TEN TEN /* 4050 */
+  TEN TEN TEN TEN     /* 4090 */
+  "123456");
+
+void
+f (void)
+{
+  /* C89's minimum-maximum is 509. */
+  __asm__ (HUN HUN HUN HUN HUN TEN);
+  __asm__ (HUN HUN HUN HUN HUN TEN : :);
+  __asm__ goto (HUN HUN HUN HUN HUN TEN : : : : label);
+
+  /* C99's minimum-maximum is 4095.  */
+  __asm__ (
+	   THO THO THO THO     /* 4000 */
+	   TEN TEN TEN TEN TEN /* 4050 */
+	   TEN TEN TEN TEN     /* 4090 */
+	   "123456");
+  __asm__ (
+	   THO THO THO THO     /* 4000 */
+	   TEN TEN TEN TEN TEN /* 4050 */
+	   TEN TEN TEN TEN     /* 4090 */
+	   "123456" : :);
+  __asm__ goto (
+		THO THO THO THO     /* 4000 */
+		TEN TEN TEN TEN TEN /* 4050 */
+		TEN TEN TEN TEN     /* 4090 */
+		"123456" : : : : label);
+
+ label: ;
+}
+
