2e64f8b8f6e40ba69829f030eb9bf7220ae39bb3
Jakub Jelinek <jakub@redhat.com>
2011-03-14 21:12:16 +0100

    re PR middle-end/38878 (gcc.dg/tree-ssa/foldaddr-1.c XFAILed)
    	PR middle-end/38878
    	* fold-const.c (fold_binary_loc) <case EQ_EXPR, NE_EXPR>: Add
    	STRIP_NOPS on arg0 and arg1.  When optimizing X +- C == X
    	and C - X == X also strip nops from +/-/p+ operand.
    	When optimizing -X == C, fold C to arg0's type.
    
    	* gcc.dg/tree-ssa/foldaddr-1.c: Remove xfail.
    
    From-SVN: r170957

--- gcc/testsuite/gcc.dg/tree-ssa/foldaddr-1.c
+++ gcc/testsuite/gcc.dg/tree-ssa/foldaddr-1.c
@@ -11,6 +11,6 @@ int foo(char *b)
 /* Folding should have determined that the two addresses were
    not identical and thus collapsed the function into a trivial
    "return 0".  */
-/* { dg-final { scan-tree-dump-times "return 0" 1 "original" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-times "return 0" 1 "original" } } */
 /* { dg-final { cleanup-tree-dump "original" } } */
 
