4e9eacabedc9d8f0c6faedd0355f151bc646921f
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-14 17:58:12 +0000

    gcc 	* c-parser.c (disable_extension_diagnostics): Save 	warn_overlength_strings. 	(restore_extension_diagnostics): Restore warn_overlength_strings. gcc/testsuite 	* gcc.dg/Woverlength-strings-pedantic-c89-ext.c: New file. 	* gcc.dg/Woverlength-strings-pedantic-c90-ext.c: New file. 	* gcc.dg/Woverlength-strings-pedantic-c99-ext.c: New file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@170947 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/Woverlength-strings-pedantic-c89-ext.c
@@ -0,0 +1,19 @@
+/* -Woverlength-strings complains about string constants which are too long
+   for the C standard's "minimum maximum" limits.  It is off by default,
+   but implied by -pedantic.  */
+
+/* { dg-options "-std=c89 -pedantic" } */
+
+#define TEN "xxxxxxxxxx"
+#define HUN TEN TEN TEN TEN TEN  TEN TEN TEN TEN TEN
+#define THO HUN HUN HUN HUN HUN  HUN HUN HUN HUN HUN
+
+/* C89's minimum-maximum is 509. */
+const char x510[] = __extension__ HUN HUN HUN HUN HUN TEN;
+
+/* C99's minimum-maximum is 4095.  */
+const char x4096[] = __extension__
+  THO THO THO THO     /* 4000 */
+  TEN TEN TEN TEN TEN /* 4050 */
+  TEN TEN TEN TEN     /* 4090 */
+  "123456";
