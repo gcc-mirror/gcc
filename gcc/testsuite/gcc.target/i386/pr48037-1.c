8a62fbb0da4121dcc499f4c1a84ecd32877b646d
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-15 12:22:12 +0000

    2011-03-15  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/48037
    	* tree-ssa.c (maybe_rewrite_mem_ref_base): Rewrite vector
    	selects into BIT_FIELD_REFs.
    	(non_rewritable_mem_ref_base): Check if a MEM_REF is a
    	vector select.
    
    	* gcc.target/i386/pr48037-1.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@170986 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr48037-1.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target lp64 } */
+/* { dg-options "-O -fno-math-errno" } */
+
+typedef double __m128d __attribute__((vector_size(16)));
+__m128d vsqrt1 (__m128d const x)
+{
+  double const* __restrict__ const y = (double const*)&x;
+  double const a = __builtin_sqrt(y[0]);
+  double const b = __builtin_sqrt(y[1]);
+  return (__m128d) { a, b };
+}
+
+/* Verify we do not spill x to the stack.  */
+/* { dg-final { scan-assembler-not "%rsp" } } */
