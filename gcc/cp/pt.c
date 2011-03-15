6871351bb0a13e548c66a27926076d20e06e6fd7
Jason Merrill <jason@redhat.com>
2011-03-15 14:31:12 -0400

    pt.c (value_dependent_expression_p): Don't check value_dependent_expression_p on the operand.
    	* pt.c (value_dependent_expression_p) [NOEXCEPT_EXPR]: Don't
    	check value_dependent_expression_p on the operand.
    
    From-SVN: r171013

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -18143,9 +18143,7 @@ value_dependent_expression_p (tree expression)
 
     case NOEXCEPT_EXPR:
       expression = TREE_OPERAND (expression, 0);
-      /* FIXME why check value-dependency?  */
-      return (type_dependent_expression_p (expression)
-	      || value_dependent_expression_p (expression));
+      return type_dependent_expression_p (expression);
 
     case SCOPE_REF:
       {
