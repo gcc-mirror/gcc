64a3d6470e5eb75c87719ba4593a356f67a51c98
Richard Guenther <rguenther@suse.de>
2011-03-16 13:53:09 +0000

    re PR middle-end/26134 (fold *(float*)(&complex_float_var) into REALPART_EXPR<complex_float_var>)
    2011-03-16  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/26134
    	* tree-ssa.c (maybe_rewrite_mem_ref_base): Handle rewriting
    	complex part accesses to REALPART_EXPR and IMAGPART_EXPR.
    	(non_rewritable_mem_ref_base): Handle complex type component
    	accesses, constrain offsets for vector and complex extracts
    	more properly.
    
    	* gcc.dg/tree-ssa/complex-6.c: New testcase.
    
    From-SVN: r171046

---
+++ gcc/testsuite/gcc.dg/tree-ssa/complex-6.c
@@ -0,0 +1,25 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-optimized" } */
+
+float
+quantum_real(float _Complex a)
+{
+  float *p = (float *) &a;
+  return p[0];
+}
+float
+quantum_imag(float _Complex a)
+{
+  float *p = (float *) &a;
+  return p[1];
+}
+float
+quantum_foo(float _Complex a)
+{
+  float *p = (float *) &a;
+  return p[2];
+}
+
+/* { dg-final { scan-tree-dump-times "REALPART_EXPR" 1 "optimized" } } */
+/* { dg-final { scan-tree-dump-times "IMAGPART_EXPR" 1 "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
