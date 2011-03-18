9351d214eff7f606d667c8e9c8974a3f95c18597
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-18 11:48:29 +0000

    2011-03-18  Paolo Carlini  <paolo.carlini@oracle.com>
    	* testsuite/util/testsuite_random.h: New.
    	* testsuite/lib/libstdc++.exp (check_v3_target_c99_math,
    	dg-require-c99_math): Add.
    	* testsuite/26_numerics/random/bernoulli_distribution/
    	operators/values.cc: New.
    	* testsuite/26_numerics/random/binomial_distribution/
    	operators/values.cc: Likewise.
    	* testsuite/26_numerics/random/geometric_distribution/
    	operators/values.cc: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@171133 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/26_numerics/random/geometric_distribution/operators/values.cc
@@ -0,0 +1,51 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-require-cstdint "" }
+//
+// Copyright (C) 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 26.5.8.2.3 Class template geometric_distribution [rand.dist.bern.geom]
+
+#include <random>
+#include <functional>
+#include <testsuite_random.h>
+
+void test01()
+{
+  using namespace __gnu_test;
+
+  std::mt19937 eng;
+
+  std::geometric_distribution<> gd1(0.5);
+  auto bgd1 = std::bind(gd1, eng);
+  testDiscreteDist(bgd1, [](int n) { return geometric_pdf(n, 0.5); } );
+
+  std::geometric_distribution<> gd2(0.75);
+  auto bgd2 = std::bind(gd2, eng);
+  testDiscreteDist(bgd2, [](int n) { return geometric_pdf(n, 0.75); } );
+
+  // libstdc++/48114
+  std::geometric_distribution<> gd3(0.25);
+  auto bgd3 = std::bind(gd3, eng);
+  testDiscreteDist(bgd3, [](int n) { return geometric_pdf(n, 0.25); } );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
