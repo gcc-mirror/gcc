0a362bb538848423d9f1089c8d14d70a779af12a
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-18 15:06:51 +0000

    PR c++/23372 	* gimplify.c (gimplify_arg): Strip redundant TARGET_EXPR.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@171146 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/opt/pr23372.C
@@ -0,0 +1,15 @@
+// PR c++/23372
+// { dg-options -fdump-tree-gimple }
+
+// There shouldn't be an assignment to a temporary in the GIMPLE,
+// as that represents a redundant copy.
+// { dg-final { scan-tree-dump-not "=" gimple } }
+
+struct A {
+  int a[1000];
+  //A(A const &);
+};
+void f(A);
+void g(A *a) { f(*a); }
+
+// { dg-final { cleanup-tree-dump gimple } }
