16e329fbae1208a4d576fb8e383f827200c2bb41
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2011-03-19 14:35:05 +0000

    re PR libfortran/47439 (Fun with scratch files on Windows  MKTEMP only allows for 26 files)
    	PR libfortran/47439
    	* gfortran.dg/scratch_1.f90: Adjust test.
    
    From-SVN: r171180

--- gcc/testsuite/gfortran.dg/scratch_1.f90
+++ gcc/testsuite/gfortran.dg/scratch_1.f90
@@ -1,7 +1,7 @@
 ! { dg-do run }
 ! Check that we can open more than 26 scratch files concurrently
   integer :: i
-  do i = 1, 3000
+  do i = 1, 30
     print *, i
     open(100+i,status="scratch")
   end do
