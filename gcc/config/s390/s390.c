2b03de53e95aedac38e937f96886f7ba709dc64c
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-21 09:25:47 +0000

    PR target/48213 	* config/s390/s390.c (s390_delegitimize_address): Don't call 	lowpart_subreg if orig_x has BLKmode.
    	* gcc.dg/pr48213.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@171211 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/s390/s390.c
+++ gcc/config/s390/s390.c
@@ -5062,6 +5062,8 @@ s390_delegitimize_address (rtx orig_x)
 
   if (GET_MODE (orig_x) != Pmode)
     {
+      if (GET_MODE (orig_x) == BLKmode)
+	return orig_x;
       y = lowpart_subreg (GET_MODE (orig_x), y, Pmode);
       if (y == NULL_RTX)
 	return orig_x;
