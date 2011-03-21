2757d5ecfc14883087e062b169e8355f8cc74b19
Thomas Koenig <tkoenig@gcc.gnu.org>
2011-03-21 07:14:42 +0000

    re PR fortran/22572 (Double occurrence of matmul intrinsic not optimised)
    2010-03-21  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/22572
    	* gfortran.h (gfc_option_t) : Add
    	flag_aggressive_function_elimination.
    	(gfc_dep_compare_functions):  Add prototype.
    	* lang.opt: Add faggressive-function-elimination.
    	* invoke.texi: Document -faggressive-function-elimination.
    	* frontend_passes (expr_array):  New static variable.
    	(expr_size):  Likewise.
    	(expr_count):  Likewise.
    	(current_code):  Likewise.
    	(current_ns):  Likewise.
    	(gfc_run_passes):  Allocate and free space for expressions.
    	(cfe_register_funcs):  New function.
    	(create_var):  New function.
    	(cfc_expr_0):  New function.
    	(cfe_code):  New function.
    	(optimize_namespace):  Invoke gfc_code_walker with cfe_code
    	and cfe_expr_0.
    	* dependency.c (gfc_dep_compare_functions):  New function.
    	(gfc_dep_compare_expr):  Use it.
    	* options.c (gfc_init_options):  Handle
    	flag_aggressive_function_elimination.
    	(gfc_handle_option):  Likewise.
    
    2010-03-21  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/22572
    	* gfortran.dg/function_optimize_1.f90:  New test.
    	* gfortran.dg/function_optimize_2.f90:  New test.
    
    From-SVN: r171207

---
+++ gcc/testsuite/gfortran.dg/function_optimize_2.f90
@@ -0,0 +1,47 @@
+! { dg-do compile }
+! { dg-options "-O -faggressive-function-elimination -fdump-tree-original" }
+program main
+  implicit none
+  real, dimension(2,2) :: a, b, c, d
+  real :: x, z
+  integer :: i
+  character(60) :: line
+  real, external :: ext_func
+  interface
+     elemental function element(x)
+       real, intent(in) :: x
+       real :: elem
+     end function element
+     pure function mypure(x)
+       real, intent(in) :: x
+       integer :: mypure
+     end function mypure
+     elemental impure function elem_impure(x)
+       real, intent(in) :: x
+       real :: elem_impure
+     end function elem_impure
+  end interface
+
+  data a /2., 3., 5., 7./
+  data b /11., 13., 17., 23./
+  write (unit=line, fmt='(4F7.2)') matmul(a,b) + matmul(a,b)
+  x = 1.2
+  z = sin(x) + cos(x) + sin(x) + cos(x)
+  print *,z
+  x = ext_func(a) + 23 + ext_func(a)
+  print *,d,x
+  z = element(x) + element(x)
+  print *,z
+  i = mypure(x) - mypure(x)
+  print *,i
+  z = elem_impure(x) - elem_impure(x)
+  print *,z
+end program main
+! { dg-final { scan-tree-dump-times "matmul_r4" 1 "original" } }
+! { dg-final { scan-tree-dump-times "__builtin_sinf" 1 "original" } }
+! { dg-final { scan-tree-dump-times "__builtin_cosf" 1 "original" } }
+! { dg-final { scan-tree-dump-times "ext_func" 1 "original" } }
+! { dg-final { scan-tree-dump-times "element" 1 "original" } }
+! { dg-final { scan-tree-dump-times "mypure" 1 "original" } }
+! { dg-final { scan-tree-dump-times "elem_impure" 1 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
