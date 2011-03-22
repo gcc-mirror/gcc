bbd399cf9190106e0f71566741f907bc33e00bb4
Joseph Myers <joseph@codesourcery.com>
2011-03-22 22:30:46 +0000

    bfin-opts.h: New.
    	* config/bfin/bfin-opts.h: New.
    	* config/bfin/bfin.c (bfin_lib_id_given, bfin_cpu_type,
    	bfin_si_revision, bfin_workarounds): Remove.
    	(bfin_cpus): Make static const.
    	(bfin_load_pic_reg): Check global_options_set.x_bfin_library_id
    	not bfin_lib_id_given.
    	(bfin_handle_option): Don't set bfin_lib_id_given.  Access
    	bfin_cpu_type, bfin_si_revision and bfin_workarounds via opts
    	pointer. Use error_at.  Don't assert that global structures are in
    	use.
    	* config/bfin/bfin.h: Include bfin-opts.h.
    	(enum bfin_cpu_type, bfin_cpu_t): Move to bfin-opts.h.
    	(bfin_cpu_type, bfin_si_revision, bfin_workarounds): Remove.
    	* bfin.opt (config/bfin/bfin-opts.h): New HeaderInclude entry.
    	(bfin_cpu_type, bfin_si_revision, bfin_workarounds): New Variable
    	entries.
    
    From-SVN: r171314

--- gcc/config/bfin/bfin.h
+++ gcc/config/bfin/bfin.h
@@ -22,53 +22,14 @@
 #ifndef _BFIN_CONFIG
 #define _BFIN_CONFIG
 
+#ifndef BFIN_OPTS_H
+#include "config/bfin/bfin-opts.h"
+#endif
+
 #define OBJECT_FORMAT_ELF
 
 #define BRT 1
 #define BRF 0
-
-/* CPU type.  */
-typedef enum bfin_cpu_type
-{
-  BFIN_CPU_UNKNOWN,
-  BFIN_CPU_BF512,
-  BFIN_CPU_BF514,
-  BFIN_CPU_BF516,
-  BFIN_CPU_BF518,
-  BFIN_CPU_BF522,
-  BFIN_CPU_BF523,
-  BFIN_CPU_BF524,
-  BFIN_CPU_BF525,
-  BFIN_CPU_BF526,
-  BFIN_CPU_BF527,
-  BFIN_CPU_BF531,
-  BFIN_CPU_BF532,
-  BFIN_CPU_BF533,
-  BFIN_CPU_BF534,
-  BFIN_CPU_BF536,
-  BFIN_CPU_BF537,
-  BFIN_CPU_BF538,
-  BFIN_CPU_BF539,
-  BFIN_CPU_BF542,
-  BFIN_CPU_BF542M,
-  BFIN_CPU_BF544,
-  BFIN_CPU_BF544M,
-  BFIN_CPU_BF547,
-  BFIN_CPU_BF547M,
-  BFIN_CPU_BF548,
-  BFIN_CPU_BF548M,
-  BFIN_CPU_BF549,
-  BFIN_CPU_BF549M,
-  BFIN_CPU_BF561
-} bfin_cpu_t;
-
-/* Value of -mcpu= */
-extern bfin_cpu_t bfin_cpu_type;
-
-/* Value of -msi-revision= */
-extern int bfin_si_revision;
-
-extern unsigned int bfin_workarounds;
 
 /* Print subsidiary information on the compiler version in use.  */
 #define TARGET_VERSION fprintf (stderr, " (BlackFin bfin)")
