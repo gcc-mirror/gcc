3ca4674345ced707dfbdd7858b28a8c09aebf202
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-22 15:05:23 +0000

    * config/abi/pre/gnu.ver (GLIBCXX_3.4.15): Export _ZNSsC2EOSs 	and _ZNSbIwSt11char_traitsIwESaIwEEC2EOS2_. 	* config/abi/post/solaris2.8/baseline_symbols.txt: Regenerated. 	* config/abi/post/solaris2.8/sparcv9/baseline_symbols.txt: Likewise. 	* config/abi/post/s390x-linux-gnu/baseline_symbols.txt: Likewise. 	* config/abi/post/x86_64-linux-gnu/baseline_symbols.txt: Likewise. 	* config/abi/post/x86_64-linux-gnu/32/baseline_symbols.txt: Likewise. 	* config/abi/post/powerpc-linux-gnu/baseline_symbols.txt: Likewise. 	* config/abi/post/powerpc64-linux-gnu/baseline_symbols.txt: Likewise. 	* config/abi/post/powerpc64-linux-gnu/32/baseline_symbols.txt: 	Likewise. 	* config/abi/post/solaris2.10/baseline_symbols.txt: Likewise. 	* config/abi/post/solaris2.10/amd64/baseline_symbols.txt: Likewise. 	* config/abi/post/solaris2.10/sparcv9/baseline_symbols.txt: Likewise. 	* config/abi/post/i486-linux-gnu/baseline_symbols.txt: Likewise. 	* config/abi/post/i386-linux-gnu/baseline_symbols.txt: Likewise. 	* config/abi/post/s390-linux-gnu/baseline_symbols.txt: Likewise. 	* testsuite/21_strings/basic_string/cons/char/moveable2.cc: New test. 	* testsuite/21_strings/basic_string/cons/wchar_t/moveable2.cc: New 	test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@171291 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/21_strings/basic_string/cons/wchar_t/moveable2.cc
@@ -0,0 +1,54 @@
+// { dg-options "-std=gnu++0x -fno-inline" }
+// { dg-require-string-conversions "" }
+
+// Copyright (C) 2010, 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// NOTE: This makes use of the fact that we know how moveable
+// is implemented on string (via swap). If the implementation changed
+// this test may begin to fail.
+
+#include <string>
+#include <utility>
+#include <testsuite_hooks.h>
+
+class twstring : public std::basic_string<wchar_t>
+{
+public:
+  twstring() : std::basic_string<wchar_t>() {}
+  twstring(twstring&& s) : std::basic_string<wchar_t>(std::move(s)) {}
+};
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  twstring a, b;
+  a.push_back(L'1');
+  b = std::move(a);
+  VERIFY( b.size() == 1 && b[0] == L'1' && a.size() == 0 );
+
+  twstring c(std::move(b));
+  VERIFY( c.size() == 1 && c[0] == L'1' );
+  VERIFY( b.size() == 0 );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
