1bb721dcde610f30cd72d019060122bdf6d2c348
Joseph Myers <joseph@codesourcery.com>
2011-03-22 22:42:51 +0000

    pa-hpux.opt (flag_pa_unix): New Variable entry.
    	* config/pa/pa-hpux.opt (flag_pa_unix): New Variable entry.
    	(munix=93): Use Var.
    	* config/pa/pa-hpux1010.opt (munix=95): Use Var.
    	* config/pa/pa-hpux1111.opt (munix=98): Use Var.
    	* config/pa/pa-opts.h: New.
    	* config/pa/pa.c (pa_cpu, flag_pa_unix): Remove.
    	(pa_handle_option): Don't assert that global structures are in
    	use.  Access target_flags via opts pointer.  Don't handle
    	OPT_mschedule_, OPT_mfixed_range_, OPT_munix_93, OPT_munix_95 or
    	OPT_munix_98 here.
    	(pa_option_override): Handle deferred OPT_mfixed_range_.
    
    From-SVN: r171324

---
+++ gcc/config/pa/pa-opts.h
@@ -0,0 +1,37 @@
+/* Definitions for option handling for HP PA.
+   Copyright (C) 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001,
+   2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011
+   Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef PA_OPTS_H
+#define PA_OPTS_H
+
+/* Which processor to schedule for.  */
+
+enum processor_type
+{
+  PROCESSOR_700,
+  PROCESSOR_7100,
+  PROCESSOR_7100LC,
+  PROCESSOR_7200,
+  PROCESSOR_7300,
+  PROCESSOR_8000
+};
+
+#endif
