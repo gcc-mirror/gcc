4a8d3d9161118cfa3e8188b7a84f34cf842e0787
Joseph Myers <joseph@codesourcery.com>
2011-03-22 22:48:11 +0000

    v850-opts.h: New.
    	* config/v850/v850-opts.h: New.
    	* config/v850/v850.c (small_memory): Replace with
    	small_memory_physical_max array.  Make that array static const.
    	(v850_handle_memory_option): Take integer value of argument.  Take
    	gcc_options pointer, option text and location.  Return void.
    	Update for changes to small memory structures.
    	(v850_handle_option): Access target_flags via opts pointer.  Don't
    	assert that global structures are in use.  Update calls to
    	v850_handle_memory_option.
    	(v850_encode_data_area): Update references to small memory
    	settings.
    	* config/v850/v850.h (struct small_memory_info, small_memory):
    	Remove.
    	(enum small_memory_type): Move to v850-opts.h.
    	* config/v850/v850.opt (config/v850/v850-opts.h): New
    	HeaderInclude entry.
    	(small_memory_max): New Variable entry.
    	(msda): Replace by pair of options msda= and msda-.  Use UInteger.
    	(mtda, mzda): Likewise.
    
    From-SVN: r171327

---
+++ gcc/config/v850/v850-opts.h
@@ -0,0 +1,34 @@
+/* Definitions for option handling for NEC V850 series.
+   Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
+   2007, 2008, 2009, 2010, 2011  Free Software Foundation, Inc.
+
+   This file is part of GCC.
+
+   GCC is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+
+   GCC is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GCC; see the file COPYING3.  If not see
+   <http://www.gnu.org/licenses/>.  */
+
+#ifndef V850_OPTS_H
+#define V850_OPTS_H
+
+enum small_memory_type {
+  /* tiny data area, using EP as base register */
+  SMALL_MEMORY_TDA = 0,
+  /* small data area using dp as base register */
+  SMALL_MEMORY_SDA,
+  /* zero data area using r0 as base register */
+  SMALL_MEMORY_ZDA,
+  SMALL_MEMORY_max
+};
+
+#endif
