190b2187b62863306378ea0940a6ddd2eb3629d5
Richard Guenther <rguenther@suse.de>
2011-03-24 12:44:58 +0000

    re PR middle-end/48269 (Incorrect fortify warning for a packed struct member)
    2011-03-24  Richard Guenther  <rguenther@suse.de>
    
    	PR middle-end/48269
    	* tree-object-size.c (addr_object_size): Do not double-account
    	for MEM_REF offsets.
    
    	* gcc.dg/builtin-object-size-10.c: New testcase.
    
    From-SVN: r171388

---
+++ gcc/testsuite/gcc.dg/builtin-object-size-10.c
@@ -0,0 +1,26 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-objsz-details" } */
+
+typedef struct {
+    char sentinel[4];
+    char data[0];
+} drone_packet;
+typedef struct {
+    char type_str[16];
+    char channel_hop;
+} drone_source_packet;
+drone_packet *
+foo(char *x)
+{
+  drone_packet *dpkt = __builtin_malloc(sizeof(drone_packet)
+					+ sizeof(drone_source_packet));
+  drone_source_packet *spkt = (drone_source_packet *) dpkt->data;
+  __builtin___snprintf_chk (spkt->type_str, 16,
+			    1, __builtin_object_size (spkt->type_str, 1),
+			    "%s", x);
+  return dpkt;
+}
+
+/* { dg-final { scan-tree-dump "maximum object size 21" "objsz" } } */
+/* { dg-final { scan-tree-dump "maximum subobject size 16" "objsz" } } */
+/* { dg-final { cleanup-tree-dump "objsz" } } */
