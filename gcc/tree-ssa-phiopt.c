2473bfb75ae39104bdf39593e67e51b3270fdd9e
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-24 14:39:36 +0000

    2011-03-24  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/48270
    	* tree-ssa-phiopt.c (cond_if_else_store_replacement): Do
    	not free datarefs before ddrs.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@171394 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-phiopt.c
+++ gcc/tree-ssa-phiopt.c
@@ -1494,8 +1494,6 @@ cond_if_else_store_replacement (basic_block then_bb, basic_block else_bb,
   else_ddrs = VEC_alloc (ddr_p, heap, 1);
   compute_all_dependences (then_datarefs, &then_ddrs, NULL, false);
   compute_all_dependences (else_datarefs, &else_ddrs, NULL, false);
-  free_data_refs (then_datarefs);
-  free_data_refs (else_datarefs);
   blocks[0] = then_bb;
   blocks[1] = else_bb;
   blocks[2] = join_bb;
@@ -1517,6 +1515,8 @@ cond_if_else_store_replacement (basic_block then_bb, basic_block else_bb,
         {
           free_dependence_relations (then_ddrs);
           free_dependence_relations (else_ddrs);
+	  free_data_refs (then_datarefs);
+	  free_data_refs (else_datarefs);
           VEC_free (gimple, heap, then_stores);
           VEC_free (gimple, heap, else_stores);
           return false;
@@ -1539,6 +1539,8 @@ cond_if_else_store_replacement (basic_block then_bb, basic_block else_bb,
         {
           free_dependence_relations (then_ddrs);
           free_dependence_relations (else_ddrs);
+	  free_data_refs (then_datarefs);
+	  free_data_refs (else_datarefs);
           VEC_free (gimple, heap, then_stores);
           VEC_free (gimple, heap, else_stores);
           return false;
@@ -1556,6 +1558,8 @@ cond_if_else_store_replacement (basic_block then_bb, basic_block else_bb,
 
   free_dependence_relations (then_ddrs);
   free_dependence_relations (else_ddrs);
+  free_data_refs (then_datarefs);
+  free_data_refs (else_datarefs);
   VEC_free (gimple, heap, then_stores);
   VEC_free (gimple, heap, else_stores);
 
