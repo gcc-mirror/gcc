5ade176d5eb914bf78919ac2f610599059fba340
Jason Merrill <jason@redhat.com>
2011-03-25 12:17:08 -0400

    Core 1135
    	Core 1135
    	* method.c (defaulted_late_check): Check for exception spec mismatch.
    	(defaultable_fn_check): Allow exception spec and virtual.
    	* class.c (check_for_override): A virtual dtor is non-trivial.
    
    From-SVN: r171462

---
+++ gcc/testsuite/g++.dg/cpp0x/defaulted23.C
@@ -0,0 +1,27 @@
+// Test for checking of exception specifications on defaulted fns
+// { dg-options -std=c++0x }
+
+struct A
+{
+  A() noexcept = default;
+};
+
+struct B
+{
+  B() throw (int) = default; // { dg-error "exception-specification that differs from the implicit declaration" }
+};
+
+struct C
+{
+  C() throw (int) { }
+};
+
+struct D: C
+{
+  D() throw (int) = default;
+};
+
+struct E
+{
+  E() = default;
+};
