291110570c24fb2c53a1502420141d24613069cb
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-30 10:42:20 +0000

    Fix a typo in FLOAT_SSE_REGS.
    2011-03-30  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/48349
    	* config/i386/i386.h (REG_CLASS_CONTENTS): Fix a typo in
    	FLOAT_SSE_REGS.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@171718 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.h
+++ gcc/config/i386/i386.h
@@ -1272,7 +1272,7 @@ enum reg_class
 { 0xe0000000,    0x1f },		/* MMX_REGS */			\
 { 0x1fe00100,0x1fe000 },		/* FP_TOP_SSE_REG */		\
 { 0x1fe00200,0x1fe000 },		/* FP_SECOND_SSE_REG */		\
-{ 0x1fe0ff00,0x3fe000 },		/* FLOAT_SSE_REGS */		\
+{ 0x1fe0ff00,0x1fe000 },		/* FLOAT_SSE_REGS */		\
    { 0x1ffff,  0x1fe0 },		/* FLOAT_INT_REGS */		\
 { 0x1fe100ff,0x1fffe0 },		/* INT_SSE_REGS */		\
 { 0x1fe1ffff,0x1fffe0 },		/* FLOAT_INT_SSE_REGS */	\
