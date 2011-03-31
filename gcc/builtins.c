5c9d7b338c51887cfdc0e0b6de187dff31b9b3c9
matz <matz@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-31 15:59:10 +0000

    * builtins.c (build_va_arg_indirect_ref): Use 	build_simple_mem_ref_loc.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@171789 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/builtins.c
+++ gcc/builtins.c
@@ -4748,7 +4748,7 @@ std_gimplify_va_arg_expr (tree valist, tree type, gimple_seq *pre_p,
 tree
 build_va_arg_indirect_ref (tree addr)
 {
-  addr = build_fold_indirect_ref_loc (EXPR_LOCATION (addr), addr);
+  addr = build_simple_mem_ref_loc (EXPR_LOCATION (addr), addr);
 
   if (flag_mudflap) /* Don't instrument va_arg INDIRECT_REF.  */
     mf_mark (addr);
