6813c50460325879b52ba004502c7c29ad53686a
Jeffrey Yasskin <jyasskin@google.com>
2011-04-01 01:07:46 +0000

    backport: conformance problems in libsupc++.
    Merge r171807 from trunk to the 4.6 release branch, to fix two C++
    conformance problems in libsupc++.
    
    Tested: bootstrap + make check-c++ on x86_64
    
    libstdc++-v3/ChangeLog:
    
    2011-03-31  Jeffrey Yasskin  <jyasskin@google.com>
    
            * libsupc++/exception_ptr.h: Forward-declare std::type_info.
            * libsupc++/nested_exception.h (__throw_with_nested): Remove a
    	redundant default argument from std::__throw_with_nested.
    
    From-SVN: r171815

--- libstdc++-v3/libsupc++/nested_exception.h
+++ libstdc++-v3/libsupc++/nested_exception.h
@@ -117,7 +117,7 @@ namespace std
   // with a type that has an accessible nested_exception base.
   template<typename _Ex>
     inline void
-    __throw_with_nested(_Ex&& __ex, const nested_exception* = 0)
+    __throw_with_nested(_Ex&& __ex, const nested_exception*)
     { throw __ex; }
 
   template<typename _Ex>
