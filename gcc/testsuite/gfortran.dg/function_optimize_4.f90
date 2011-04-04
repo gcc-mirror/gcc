f6e36c3d0986332cb0c68fe7049e05fcd25f63a0
tkoenig <tkoenig@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-04-04 20:22:21 +0000

    2011-04-04  Thomas Koenig  <tkoenig@gcc.gnu.org>
    	PR fortran/48412
    	* frontend-passes (cfe_expr_0):  Reverse the order of going
    	through the loops.
    
    2011-04-04  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/48412
    	* function_optimize_4.f90:  New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@171952 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/function_optimize_4.f90
@@ -0,0 +1,20 @@
+! { dg-do run }
+! { dg-options "-O" }
+! PR 48412 - function elimination got temporary varibles in the wrong order.
+! Test case contributed by Joost VandeVondele.
+
+INTEGER FUNCTION S1(m,ma,lx)
+INTEGER :: m,ma,lx
+
+IF (((m < 0).AND.(MODULO(ABS(ma-lx),2) == 1)).OR.&
+    ((m > 0).AND.(MODULO(ABS(ma-lx),2) == 0))) THEN
+   S1=1
+ELSE
+   S1=0
+ENDIF
+
+END FUNCTION
+
+INTEGER :: s1
+IF (S1(1,2,1).NE.0) CALL ABORT()
+END
