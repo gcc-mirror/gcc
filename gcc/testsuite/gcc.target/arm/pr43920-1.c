84b5eccce8a3f4f0b540b6ceda01a47358b1fd1a
ramana <ramana@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-04-06 09:41:07 +0000

    Fix commit for PR target/43920
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@172031 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/pr43920-1.c
+++ gcc/testsuite/gcc.target/arm/pr43920-1.c
@@ -19,24 +19,3 @@ f (int start, int end, int *start_)
 /* { dg-final { scan-assembler-times "\torr" 0 } } */
 /* { dg-final { scan-assembler-times "\tit\t" 0 } } */
 /* { dg-final { scan-assembler "\tbeq" } } */
-/* { dg-do compile } */
-/* { dg-options "-mthumb -Os" }  */
-/* { dg-require-effective-target arm_thumb2_ok } */
-
-int
-f (int start, int end, int *start_)
-{
-  if (start == -1 || end == -1)
-    return -1;
-
-  if (end - start)
-    return -1;
-
-  *start_ = start;
-
-  return 0;
-}
-
-/* { dg-final { scan-assembler-times "\torr" 0 } } */
-/* { dg-final { scan-assembler-times "\tit\t" 0 } } */
-/* { dg-final { scan-assembler "\tbeq" } } */
