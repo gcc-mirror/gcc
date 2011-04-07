407a163c2a8f7b4a240da393eb37a5c2ded591ed
Andrey Belevantsev <abel@ispras.ru>
2011-04-07 10:53:44 +0400

    backport: re PR rtl-optimization/45652 (gcc.dg/compat/scalar-by-value-3 FAILs with -O2 -fselective-scheduling2)
            Backport from mainline
            2010-11-22  Alexander Monakov  <amonakov@ispras.ru>
    
            PR rtl-optimization/45652
            * alias.c (get_reg_base_value): New.
            * rtl.h (get_reg_base_value): Add prototype.
            * sel-sched.c (init_regs_for_mode): Use it.  Don't use registers with
            non-null REG_BASE_VALUE for renaming.
    
    From-SVN: r172080

---
+++ gcc/testsuite/gcc.dg/pr45652.c
@@ -0,0 +1,39 @@
+/* { dg-do run { target powerpc*-*-* ia64-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -fselective-scheduling2" } */
+
+struct S {
+  double i[2];
+};
+
+void __attribute__ ((noinline)) checkcd (struct S x)
+{
+  if (x.i[0] != 7.0 || x.i[1] != 8.0)
+    __builtin_abort ();
+}
+
+void __attribute__ ((noinline)) testvacd (int n, ...)
+{
+  int i;
+  __builtin_va_list ap;
+  __builtin_va_start (ap, n);
+  for (i = 0; i < n; i++)
+    {
+      struct S t = __builtin_va_arg (ap, struct S);
+      checkcd (t);
+    }
+  __builtin_va_end (ap);
+}
+
+void
+testitcd (void)
+{
+  struct S x = { { 7.0, 8.0 } };
+  testvacd (2, x, x);
+}
+
+int
+main ()
+{
+  testitcd ();
+  return 0;
+}
