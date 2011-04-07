3dacfcad47a1e6dba0f2e4618f71f7d5310b0ede
jyasskin <jyasskin@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-04-07 04:11:12 +0000

    In C++0x mode, import iota and is_sorted from namespace std into namespace __gnu_cxx to avoid ambiguous call errors.  copy_n would be a good candidate for this too, except it was standardized with a different return type.
    Tested:
      `make -k check-c++` on x86_64-unknown-linux-gnu.
    
    libstdc++-v3/ChangeLog:
    
    2011-04-06  Jeffrey Yasskin  <jyasskin@google.com>
    
            * include/ext/algorithm (is_sorted): In C++0x mode import from
            namespace std.
            * include/ext/numeric (iota): In C++0x mode import from
            namespace std.
            * testsuite/ext/is_sorted/cxx0x.cc: New.
            * testsuite/ext/iota/cxx0x.cc: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@172073 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/ext/is_sorted/cxx0x.cc
@@ -0,0 +1,28 @@
+// { dg-do compile }
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <ext/algorithm>
+#include <vector>
+
+void foo()
+{
+  std::vector<int> v;
+  is_sorted(v.begin(), v.end());
+}
