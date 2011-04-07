0b0f7a12c93a2886aac5b4b0adb80c9e833922f1
Andrey Belevantsev <abel@ispras.ru>
2011-04-07 10:59:19 +0400

    backport: re PR target/43603 (gcc-4.4.3 ICE on ia64 with -O3)
            Backport from mainline
            2010-12-07  Andrey Belevantsev  <abel@ispras.ru>
            PR target/43603
            * haifa-sched.c (sched_create_recovery_edges): Update
            dominator info.
            * sel-sched-ir.c (maybe_tidy_empty_bb): Update dominator info
            after deleting an empty block.
            (tidy_control_flow): Also verify dominators.
            (sel_remove_bb): Update dominator info after removing a block.
            (sel_redirect_edge_and_branch_force): Assert that no unreachable
            blocks will be created. Update dominator info.
            (sel_redirect_edge_and_branch): Update dominator info when
            basic blocks do not become unreachable.
            (sel_remove_loop_preheader): Update dominator info.
    
            2010-10-14  Andrey Belevantsev  <abel@ispras.ru>
    
            * sel-sched-ir.c (maybe_tidy_empty_bb): Simplify comment.
            (tidy_control_flow): Tidy vertical space.
            (sel_remove_bb): New variable idx.  Use it to remember the basic
            block index before deleting the block.
            (sel_remove_empty_bb): Remove dead code, simplify and insert to ...
            (sel_merge_blocks): ... here.
            * sel-sched-ir.h (sel_remove_empty_bb): Remove prototype.
    
    From-SVN: r172084

---
+++ gcc/testsuite/g++.dg/opt/pr46640.C
@@ -0,0 +1,44 @@
+// { dg-do compile { target x86_64-*-* } }
+// { dg-options "-fschedule-insns2 -fsel-sched-pipelining -fselective-scheduling2 -fno-exceptions -O" }
+
+struct QBasicAtomicInt
+{
+  int i, j;
+  bool deref ()
+  {
+    asm volatile ("":"=m" (i), "=qm" (j));
+  }
+};
+
+struct Data
+{
+  QBasicAtomicInt ref;
+  void *data;
+};
+
+struct QByteArray
+{
+  Data * d;
+  ~QByteArray ()
+  {
+    d->ref.deref ();
+  }
+};
+
+int indexOf (unsigned);
+int stat (void *, int *);
+QByteArray encodeName ();
+
+bool makeDir (unsigned len)
+{
+  unsigned i = 0;
+  while (len)
+    {
+      int st;
+      int pos = indexOf (i);
+      QByteArray baseEncoded = encodeName ();
+      if (stat (baseEncoded.d->data, &st) && stat (baseEncoded.d, &st))
+        return false;
+      i = pos;
+    }
+}
