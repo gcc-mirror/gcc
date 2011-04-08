554f1948b9c4fdbf3920666dc5b2814fe821b627
Jakub Jelinek <jakub@redhat.com>
2011-04-08 13:38:19 +0200

    re PR tree-optimization/48377 (miscompilation at -O3)
    	PR tree-optimization/48377
    	* tree-vect-data-refs.c (vector_alignment_reachable_p): Set
    	is_packed to true even for types with smaller TYPE_ALIGN than
    	TYPE_SIZE.
    
    	* gcc.dg/vect/pr48377.c: New test.
    
    From-SVN: r172172

---
+++ gcc/testsuite/gcc.dg/vect/pr48377.c
@@ -0,0 +1,25 @@
+/* PR tree-optimization/48377 */
+/* { dg-do run } */
+
+typedef unsigned int U __attribute__((__aligned__ (1), __may_alias__));
+
+__attribute__((noinline, noclone)) unsigned int
+foo (const char *s, int len)
+{
+  const U *p = (const U *) s;
+  unsigned int f = len / sizeof (unsigned int), hash = len, i;
+
+  for (i = 0; i < f; ++i)
+    hash += *p++;
+  return hash;
+}
+
+char buf[64] __attribute__((aligned (32)));
+
+int
+main (void)
+{
+  return foo (buf + 1, 26) != 26;
+}
+
+/* { dg-final { cleanup-tree-dump "vect" } } */
