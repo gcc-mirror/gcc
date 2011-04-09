6c0ba6bd3990f0e329aa3acef1409ab764b449c5
Jason Merrill <jason@redhat.com>
2011-04-09 15:04:11 -0400

    re PR c++/48528 (FAIL: g++.dg/cpp0x/sfinae10.C (test for excess errors))
    	PR c++/48528
    	* g++.dg/cpp0x/sfinae10.C: Remove abstract class.
    
    From-SVN: r172230

--- gcc/testsuite/g++.dg/cpp0x/sfinae10.C
+++ gcc/testsuite/g++.dg/cpp0x/sfinae10.C
@@ -10,9 +10,7 @@ decltype(T(std::declval<Args>()...), char()) f(int);
 template<class, class...>
 char (&f(...))[2];
 
-struct A { virtual ~A() = 0; };
 struct B {};
 
-static_assert(sizeof(f<A, int, int>(0)) != 1, "Error"); // a
 static_assert(sizeof(f<B, void, int>(0)) != 1, "Error"); // b
 static_assert(sizeof(f<void, int, int>(0)) != 1, "Error"); // c
