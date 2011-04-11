8fb3e11aa5b6c3096a8b3e7d098927b2875ca9b5
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2011-04-11 18:14:23 +0000

    pr47917.c: Define _ISO_C_SOURCE=19990L for alpha*-dec-osf5*.
    	* gcc.dg/torture/pr47917.c: Define _ISO_C_SOURCE=19990L for
    	alpha*-dec-osf5*.
    
    From-SVN: r172270

--- gcc/testsuite/gcc.dg/torture/pr47917.c
+++ gcc/testsuite/gcc.dg/torture/pr47917.c
@@ -1,5 +1,6 @@
 /* { dg-do run } */
 /* { dg-options "-std=c99" } */
+/* { dg-options "-std=c99 -D_ISO_C_SOURCE=19990L" { target alpha*-dec-osf5* } } */
 /* { dg-options "-std=c99 -D_XOPEN_SOURCE=500" { target mips-sgi-irix6.5 *-*-solaris2.[89] } } */
 /* { dg-options "-std=gnu99" { target *-*-hpux* } } */
 /* { dg-xfail-if "no C99 snprintf function" { *-*-hpux10* } } */
