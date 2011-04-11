b24290fb5bfba313dc7143aa29a7b161fd501997
Jason Merrill <jason@redhat.com>
2011-04-11 18:00:06 -0400

    PR c++/48457, Core 1238
    	PR c++/48457, Core 1238
    	* call.c (reference_binding): Allow rvalue reference to bind to
    	function lvalue.
    	* tree.c (lvalue_kind): Functions are always lvalues.
    
    From-SVN: r172282

---
+++ gcc/testsuite/g++.dg/cpp0x/rv-func.C
@@ -0,0 +1,22 @@
+// PR c++/48457, Core 1238
+// { dg-options -std=c++0x }
+
+template<class T>
+T&& create();
+
+template<class T, class Arg>
+void test() {
+  T t(create<Arg>());
+  (void) t;
+}
+
+void f (void (&)());
+void f (void (&&)());
+
+int main() {
+  test<void(&)(), void()>();
+  test<void(&&)(), void()>();
+  // This call should choose the lvalue reference overload.
+  // { dg-final { scan-assembler-not "_Z1fOFvvE" } }
+  f(create<void()>());
+}
