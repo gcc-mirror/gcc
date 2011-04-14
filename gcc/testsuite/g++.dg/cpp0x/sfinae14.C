a66e8081b522edc78354e41ded044a597dc100d9
Jason Merrill <jason@redhat.com>
2011-04-14 11:00:07 -0400

    re PR c++/48557 ([C++0x][SFINAE] Hard errors with void as operand of binary built-in operators)
    	PR c++/48557
    	* typeck.c (cp_build_binary_op): Don't decay void operands.
    
    From-SVN: r172433

---
+++ gcc/testsuite/g++.dg/cpp0x/sfinae14.C
@@ -0,0 +1,27 @@
+// PR c++/48557
+// { dg-options -std=c++0x }
+
+template<class T>
+struct add_rval_ref
+{
+  typedef T&& type;
+};
+
+template<>
+struct add_rval_ref<void>
+{
+  typedef void type;
+};
+
+template<class T>
+typename add_rval_ref<T>::type create();
+
+template<class T, class U,
+  class = decltype(create<T>() + create<U>())
+>
+char f(int);
+
+template<class, class>
+char (&f(...))[2];
+
+static_assert(sizeof(f<void, int>(0)) != 1, "Error");  // (a)
