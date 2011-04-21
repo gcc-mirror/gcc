27b32feb19e33eb5af213979ebb8d8728e28c7da
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-04-21 09:39:57 +0000

    gcc/ 	* tree-vect-data-refs.c (vect_drs_dependent_in_basic_block): Use 	operand_equal_p to compare DR_BASE_ADDRESSes. 	(vect_check_interleaving): Likewise.
    gcc/testsuite/
    	* gcc.dg/vect/vect-119.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@172817 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/vect-119.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+
+#define OUTER 32
+#define INNER 40
+
+static unsigned int
+bar (const unsigned int x[INNER][2], unsigned int sum)
+{
+  int i;
+
+  for (i = 0; i < INNER; i++)
+    sum += x[i][0] * x[i][0] + x[i][1] * x[i][1];
+  return sum;
+}
+
+unsigned int foo (const unsigned int x[OUTER][INNER][2])
+{
+  int i;
+  unsigned int sum;
+
+  sum = 0.0f;
+  for (i = 0; i < OUTER; i++)
+    sum = bar (x[i], sum);
+  return sum;
+}
+
+/* { dg-final { scan-tree-dump-times "Detected interleaving of size 2" 1 "vect" } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
