6861b092d6b98557d55d134aa9ce900568c154be
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-04-23 10:26:38 +0000

    2011-04-23  Tobias Burnus  <burnus@net-b.de>
            PR fortran/18918
            * module.c (mio_array_spec): Set as->cotype on reading.
            * resolve.c (resolve_allocate_expr): Fix allocating coarray
            components.
    
    2011-04-23  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/18918
            * gfortran.dg/coarray_19.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@172897 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/coarray_19.f90
@@ -0,0 +1,27 @@
+! { dg-do compile }
+! { dg-options "-fcoarray=single" }
+!
+! PR fortran/18918
+!
+
+! Was failing before as the "x%a()[]" was
+! regarded as coindexed
+subroutine test2()
+  type t
+    integer, allocatable :: a(:)[:]
+  end type t
+  type(t), SAVE :: x
+  allocate(x%a(1)[*])
+end subroutine test2
+
+
+module m
+  integer, allocatable :: a(:)[:]
+end module m
+
+! Was failing as "a" was allocatable but
+! as->cotype was not AS_DEFERERED.
+use m
+end
+
+! { dg-final { cleanup-modules "m" } }
