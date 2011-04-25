861960977ab98474999e942ab31436fbc028a3ea
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-04-25 23:04:16 +0000

    2011-04-25  Paolo Carlini  <paolo.carlini@oracle.com>
    	* include/std/type_traits (struct underlying_type): Add.
    	* testsuite/20_util/underlying_type/requirements/typedefs-1.cc: New.
    	* testsuite/20_util/underlying_type/requirements/typedefs-2.cc:
    	Likewise.
    	* testsuite/20_util/underlying_type/requirements/
    	explicit_instantiation.cc: Likewise.
    	* testsuite/20_util/declval/requirements/1_neg.cc: Adjust dg-error
    	line number.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@172944 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/20_util/underlying_type/requirements/typedefs-2.cc
@@ -0,0 +1,44 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-do compile }
+//
+// Copyright (C) 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 
+// NB: This file is for testing type_traits with NO OTHER INCLUDES.
+
+#include <type_traits>
+
+void test01()
+{
+  using namespace std;
+
+  enum E1 : unsigned { };
+  enum E2 : char { };
+  enum class E3 { };
+  enum class E4 : unsigned char { c = 1 };
+  enum class E5 : int { a = -1, b = 1 };
+  enum class E6 : long { c = __LONG_MAX__ };  
+
+  static_assert(is_same<underlying_type<E1>::type, unsigned>::value, "Error");
+  static_assert(is_same<underlying_type<E2>::type, char>::value, "Error");
+  static_assert(is_same<underlying_type<E3>::type, int>::value, "Error");
+  static_assert(is_same<underlying_type<E4>::type,
+			unsigned char>::value, "Error");
+  static_assert(is_same<underlying_type<E5>::type, int>::value, "Error");
+  static_assert(is_same<underlying_type<E6>::type, long>::value, "Error");
+}
