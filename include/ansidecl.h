cdd0bb4bde4f6885045e65860de0320ccf6b23f6
jkratoch <jkratoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-04-25 18:05:37 +0000

    include/ 	* ansidecl.h (ENUM_BITFIELD): New, from gcc/system.h.
    contrib/
    	* paranoia.cc (ENUM_BITFIELD): Remove.
    
    gcc/
    	* system.h (ENUM_BITFIELD): Remove.
    
    libcpp/
    	* system.h (ENUM_BITFIELD): Remove.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@172933 138bc75d-0d04-0410-961f-82ee72b054a4

--- include/ansidecl.h
+++ include/ansidecl.h
@@ -416,6 +416,15 @@ So instead we use the macro below and test it against specific values.  */
 #define EXPORTED_CONST const
 #endif
 
+/* Be conservative and only use enum bitfields with GCC.
+   FIXME: provide a complete autoconf test for buggy enum bitfields.  */
+
+#if (GCC_VERSION > 2000)
+#define ENUM_BITFIELD(TYPE) __extension__ enum TYPE
+#else
+#define ENUM_BITFIELD(TYPE) unsigned int
+#endif
+
 #ifdef __cplusplus
 }
 #endif
