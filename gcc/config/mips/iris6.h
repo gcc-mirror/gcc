4f823da5adbf2c709d76481f493a0f4ba729da66
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-04-26 15:42:17 +0000

    * config/mips/iris6.h (LOCAL_LABEL_PREFIX): Don't test 	TARGET_NEWABI. 	(WINT_TYPE_SIZE): Use INT_TYPE_SIZE. 	(TARGET_OS_CPP_BUILTINS): Remove TARGET_IRIX6 guards.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@172976 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/mips/iris6.h
+++ gcc/config/mips/iris6.h
@@ -77,7 +77,7 @@ along with GCC; see the file COPYING3.  If not see
   while (0)
 
 #undef LOCAL_LABEL_PREFIX
-#define LOCAL_LABEL_PREFIX (TARGET_NEWABI ? "." : "$")
+#define LOCAL_LABEL_PREFIX "."
 
 #undef ASM_DECLARE_OBJECT_NAME
 #define ASM_DECLARE_OBJECT_NAME mips_declare_object_name
@@ -104,7 +104,7 @@ along with GCC; see the file COPYING3.  If not see
 #define WINT_TYPE (Pmode == DImode ? "int" : "long int")
 
 #undef WINT_TYPE_SIZE
-#define WINT_TYPE_SIZE 32
+#define WINT_TYPE_SIZE INT_TYPE_SIZE
 
 /* C99 stdint.h types.  */
 #define INT8_TYPE "signed char"
@@ -175,16 +175,15 @@ along with GCC; see the file COPYING3.  If not see
       /* IRIX 6.5.18 and above provide many ISO C99		\
 	 features protected by the __c99 macro.			\
 	 libstdc++ v3 needs them as well.  */			\
-      if (TARGET_IRIX6)						\
-	if (flag_isoc99 || c_dialect_cxx ())			\
-	  builtin_define ("__c99");				\
+      if (flag_isoc99 || c_dialect_cxx ())			\
+	builtin_define ("__c99");				\
 								\
       /* The GNU C++ standard library requires that		\
 	 __EXTENSIONS__ and _SGI_SOURCE be defined on at	\
 	 least IRIX 6.2 and probably all IRIX 6 prior to 6.5.	\
 	 We don't need this on IRIX 6.5 itself, but it		\
 	 shouldn't hurt other than the namespace pollution.  */	\
-      if (!flag_iso || (TARGET_IRIX6 && c_dialect_cxx ()))	\
+      if (!flag_iso || c_dialect_cxx ())			\
 	{							\
 	  builtin_define ("__EXTENSIONS__");			\
 	  builtin_define ("_SGI_SOURCE");			\
