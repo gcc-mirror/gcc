756d50f590a590a7b9cadabf92217f256e4b8256
Nick Clifton <nickc@redhat.com>
2011-04-27 14:55:28 +0000

    20020312-2.c: Add definition for RX.
    	* gcc.dg/20020312-2.c: Add definition for RX.
    
    	* lib/target-supports.exp (add_options_for_ieee): Use -mnofpu
    	option with RX targets.
    
    	* gcc.target/rx/pack.c: Replace C++ style // comments with C style
    	/* */ comments.
    
    From-SVN: r173033

--- gcc/testsuite/gcc.dg/20020312-2.c
+++ gcc/testsuite/gcc.dg/20020312-2.c
@@ -58,6 +58,8 @@ extern void abort (void);
 # else
 #  define PIC_REG  "30"
 # endif
+#elif defined(__RX__)
+/* No pic register.  */
 #elif defined(__s390__)
 # define PIC_REG  "12"
 #elif defined(__sparc__)
