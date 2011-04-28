5c1811fc4f9d392f3f11bea59700a03274603f5a
Jason Merrill <jason@redhat.com>
2011-04-28 11:02:17 -0400

    re PR c++/48046 (Expected diagnostic "reference to  'type' is ambiguous" not given for function-local static declaration)
    	PR c++/48046
    	* parser.c (cp_parser_diagnose_invalid_type_name): Commit
    	to tentative parse sooner.
    
    From-SVN: r173116

--- gcc/cp/parser.c
+++ gcc/cp/parser.c
@@ -2333,6 +2333,7 @@ cp_parser_diagnose_invalid_type_name (cp_parser *parser,
 				      location_t location)
 {
   tree decl, old_scope;
+  cp_parser_commit_to_tentative_parse (parser);
   /* Try to lookup the identifier.  */
   old_scope = parser->scope;
   parser->scope = scope;
@@ -2423,7 +2424,6 @@ cp_parser_diagnose_invalid_type_name (cp_parser *parser,
       else
 	gcc_unreachable ();
     }
-  cp_parser_commit_to_tentative_parse (parser);
 }
 
 /* Check for a common situation where a type-name should be present,
