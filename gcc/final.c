6feeb21620a8fb7ee200f62e174b1c38eebabb38
Jakub Jelinek <jakub@redhat.com>
2011-04-28 19:01:55 +0200

    re PR middle-end/48597 (x86-64 unwind register save location misses half-register write?)
    	PR middle-end/48597
    	* final.c (final_scan_insn): Call dwarf2out_frame_debug even for
    	inline asm.
    
    From-SVN: r173121

--- gcc/final.c
+++ gcc/final.c
@@ -2238,6 +2238,11 @@ final_scan_insn (rtx insn, FILE *file, int optimize_p ATTRIBUTE_UNUSED,
 	    location_t loc;
 	    expanded_location expanded;
 
+	    /* Make sure we flush any queued register saves in case this
+	       clobbers affected registers.  */
+	    if (dwarf2out_do_frame ())
+	      dwarf2out_frame_debug (insn, false);
+
 	    /* There's no telling what that did to the condition codes.  */
 	    CC_STATUS_INIT;
 
