6ecde111c12993f6aace9f151b30e1d9dbcfd8be
Richard Guenther <rguenther@suse.de>
2011-04-28 09:55:41 +0000

    re PR tree-optimization/40052 (missed optimizations on (extended) logical types: (x | 1) --> 1)
    2011-04-28  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/40052
    	PR tree-optimization/15347
    	* gcc.dg/tree-ssa/vrp57.c: New testcase.
    	* gcc.dg/pr15347.c: Likewise.
    
    From-SVN: r173064

---
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp57.c
@@ -0,0 +1,17 @@
+/* PR40052 */
+/* { dg-do compile } */
+/* { dg-options "-O -ftree-vrp -fdump-tree-optimized" } */
+
+int foo(_Bool b)
+{
+    return b | 1;
+}
+
+int bar(_Bool b)
+{
+    return b & -2;
+}
+
+/* { dg-final { scan-tree-dump "return 0;" "optimized" } } */
+/* { dg-final { scan-tree-dump "return 1;" "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
