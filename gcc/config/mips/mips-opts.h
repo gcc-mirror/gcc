498fed4e444684d2f24b849c472b8713bd6a7d29
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-05-03 18:51:49 +0000

    * config/mips/mips-opts.h: New. 	* config/mips/mips.c (enum mips_r10k_cache_barrier_setting): Move 	to mips-opts.h. 	(mips_abi, mips_code_readable, mips_r10k_cache_barriee): Remove. 	(mips_handle_option): Don't handle OPT_mabi_, OPT_mcode_readable_ 	or OPT_mr10k_cache_barrier_ here.  Access mips_cache_flush_func 	via opts pointer. 	* config/mips/mips.h (enum mips_code_readable_setting): Move to 	mips-opts.h. 	(mips_abi, mips_code_readable): Don't declare. 	* config/mips/mips.opt (config/mips/mips-opts.h): New 	HeaderInclude. 	(mabi=): Use Enum and Var. 	(mips_abi): New Enum and EnumValue entries. 	(mcode-readable=): Use Enum and Var. 	(mips_code_readable_setting): New Enum and EnumValue entries. 	(mr10k-cache-barrier=): Use Enum and Var. 	(mips_r10k_cache_barrier_setting): New Enum and EnumValue entries.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@173338 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/mips/mips-opts.h
@@ -0,0 +1,39 @@
+/* Definitions for option handling for MIPS.
+   Copyright (C) 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998
+   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2007, 2008, 2009, 2010, 2011
+   Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef MIPS_OPTS_H
+#define MIPS_OPTS_H
+
+/* Enumerates the setting of the -mcode-readable option.  */
+enum mips_code_readable_setting {
+  CODE_READABLE_NO,
+  CODE_READABLE_PCREL,
+  CODE_READABLE_YES
+};
+
+/* Enumerates the setting of the -mr10k-cache-barrier option.  */
+enum mips_r10k_cache_barrier_setting {
+  R10K_CACHE_BARRIER_NONE,
+  R10K_CACHE_BARRIER_STORE,
+  R10K_CACHE_BARRIER_LOAD_STORE
+};
+
+#endif
