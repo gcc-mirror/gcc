73452ce79a0be60509fde8f6f2949aa84b0b7766
Dodji Seketeli <dodji@redhat.com>
2011-05-06 08:34:53 +0000

    re PR c++/48838 (valid template code does not compile)
    Fix PR c++/48838
    
    gcc/cp
    
    	PR c++/48838
    	* cp-tree.h (non_static_member_function_p): Declare new function.
    	* tree.c (non_static_member_function_p): Define it.
    	* semantics.c (finish_call_expr): Use it.
    
    gcc/testsuite
    
    	PR c++/48838
    	* g++.dg/template/member9.C: New test case.
    
    From-SVN: r173473

--- gcc/cp/tree.c
+++ gcc/cp/tree.c
@@ -1480,6 +1480,22 @@ build_overload (tree decl, tree chain)
   return ovl_cons (decl, chain);
 }
 
+/* Return TRUE if FN is a non-static member function, FALSE otherwise.
+   This function looks into BASELINK and OVERLOAD nodes.  */
+
+bool
+non_static_member_function_p (tree fn)
+{
+  if (fn == NULL_TREE)
+    return false;
+
+  if (is_overloaded_fn (fn))
+    fn = get_first_fn (fn);
+
+  return (DECL_P (fn)
+	  && DECL_NONSTATIC_MEMBER_FUNCTION_P (fn));
+}
+
 
 #define PRINT_RING_SIZE 4
 
