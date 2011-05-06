9e33e3210a0bf2cc04f409423bc9210de5e31729
Jason Merrill <jason@redhat.com>
2011-05-06 17:57:49 -0400

    re PR c++/48909 ([C++0x] ICE in cxx_eval_conditional_expression, at cp/semantics.c:6213)
    	PR c++/48909
    	* semantics.c (cxx_eval_conditional_expression): Check
    	integer_zerop/onep instead.
    
    From-SVN: r173511

---
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-condition2.C
@@ -0,0 +1,18 @@
+// PR c++/48909
+// { dg-options -std=c++0x }
+
+#define SA(X) static_assert((X),#X)
+
+constexpr int const * is_sorted_until(int const * first, int const * last)
+{
+ return first == last || first + 1 == last ? last
+  : (*(first + 1) < *first) != false ? first + 1
+  : is_sorted_until(first + 1, last);
+}
+
+int main()
+{
+ static constexpr int array[2] = {0, 1};
+ constexpr int const * last = is_sorted_until(array, array + 2);
+ SA(last==array+2);
+}
