2e8d9212daaa8c6162ab872485dd03d28c96ea4c
Tobias Burnus <burnus@net-b.de>
2011-05-06 20:12:25 +0200

    re PR fortran/48858 (Incorrect error for same binding label on two generic interface specifics)
    2011-05-06  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/48858
            PR fortran/48820
            * lang.opt (std=f2008tr): New.
            * libgfortran.h (GFC_STD_F2008_TR): New macro constant.
            * decl.c (verify_c_interop_param): Allow OPTIONAL in BIND(C)
            procedures for -std=f2008tr/gnu/legacy.
    
    2011-05-06  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/48858
            PR fortran/48820
            * gfortran.dg/bind_c_usage_22.f90: New.
            * gfortran.dg/bind_c_usage_23.f90: New.
            * gfortran.dg/bind_c_usage_24.f90: New.
            * gfortran.dg/bind_c_usage_24_c.c: New.
    
    From-SVN: r173500

---
+++ gcc/testsuite/gfortran.dg/bind_c_usage_23.f90
@@ -0,0 +1,64 @@
+! { dg-do compile }
+! { dg-options "-std=f2008" }
+!
+! PR fortran/48858
+! PR fortran/48820
+!
+! OPTIONAL + BIND(C) is allowed since TR 29113
+!
+
+! VALID
+subroutine sub(z) bind(C)
+  use iso_c_binding
+  integer(c_int), value :: z
+end subroutine sub
+
+! VALID since TR29113
+subroutine sub2(z) bind(C) ! { dg-error "with OPTIONAL attribute in procedure" }
+  use iso_c_binding
+  integer(c_int), optional :: z
+end subroutine sub2
+
+! VALID since TR29113
+subroutine sub2a(z) bind(C) ! { dg-error "with OPTIONAL attribute in procedure" }
+  use iso_c_binding
+  integer(c_int) :: z
+  optional :: z
+end subroutine sub2a
+
+! VALID since TR29113
+subroutine sub2b(z) bind(C) ! { dg-error "with OPTIONAL attribute in procedure" }
+  use iso_c_binding
+  optional :: z
+  integer(c_int) :: z
+end subroutine sub2b
+
+! Invalid
+subroutine sub3(z) bind(C) ! { dg-error "cannot have both the OPTIONAL and the VALUE attribute" }
+  use iso_c_binding
+  integer(c_int), value, optional :: z
+end subroutine sub3
+
+! Invalid
+subroutine sub3a(z) bind(C) ! { dg-error "cannot have both the OPTIONAL and the VALUE attribute" }
+  use iso_c_binding
+  integer(c_int) :: z
+  optional :: z
+  value :: z
+end subroutine sub3a
+
+! Invalid
+subroutine sub3b(z) bind(C) ! { dg-error "cannot have both the OPTIONAL and the VALUE attribute" }
+  use iso_c_binding
+  optional :: z
+  value :: z
+  integer(c_int) :: z
+end subroutine sub3b
+
+! Invalid
+subroutine sub3c(z) bind(C) ! { dg-error "cannot have both the OPTIONAL and the VALUE attribute" }
+  use iso_c_binding
+  value :: z
+  integer(c_int) :: z
+  optional :: z
+end subroutine sub3c
