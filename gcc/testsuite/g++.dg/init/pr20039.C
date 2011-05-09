9420e113a12ecabc02aa996378690a63656a59d4
Fabien Chêne <fabien@gcc.gnu.org>
2011-05-09 22:56:29 +0200

    re PR c++/20039 (uninitialized const in `new' of `const struct')
    gcc/testsuite/ChangeLog:
    
    2011-05-09  Fabien Chene  <fabien@gcc.gnu.org>
    	PR c++/20039
    	* g++.dg/init/pr20039.C: New.
    
    From-SVN: r173592

---
+++ gcc/testsuite/g++.dg/init/pr20039.C
@@ -0,0 +1,19 @@
+// PR c++/20039
+// { dg-do compile }
+
+struct M
+{
+  M() : m(0) { }
+  int m;
+};
+
+struct X
+{
+  M m;
+};
+
+int mymain()
+{
+  const X *p = new const X[2]; // { dg-error "uninitialized const" }
+  return 0;
+}
