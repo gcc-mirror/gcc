78cdac2b18761c1a047dff2949a42e47891f6158
Jakub Jelinek <jakub@redhat.com>
2011-05-10 10:45:00 +0200

    backport: re PR tree-optimization/48837 (Wrong optimization of recursive function calls)
    	Backported from mainline
    	2011-05-07  Zdenek Dvorak  <ook@ucw.cz>
    
    	PR tree-optimization/48837
    	* tree-tailcall.c (tree_optimize_tail_calls_1): Do not mark tailcalls
    	when accumulator transformation is performed.
    
    	* gcc.dg/pr48837.c: New testcase.
    
    From-SVN: r173609

--- gcc/tree-tailcall.c
+++ gcc/tree-tailcall.c
@@ -1019,6 +1019,14 @@ tree_optimize_tail_calls_1 (bool opt_tailcalls)
 					     integer_one_node);
     }
 
+  if (a_acc || m_acc)
+    {
+      /* When the tail call elimination using accumulators is performed,
+	 statements adding the accumulated value are inserted at all exits.
+	 This turns all other tail calls to non-tail ones.  */
+      opt_tailcalls = false;
+    }
+
   for (; tailcalls; tailcalls = next)
     {
       next = tailcalls->next;
