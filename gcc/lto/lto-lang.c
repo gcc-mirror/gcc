8242dd04b79486c5890e599caa9c492eac38c2f8
Nathan Froyd <froydnj@codesourcery.com>
2011-05-10 16:56:19 +0000

    use build_function_type less in c-family and LTO
    use build_function_type less in c-family and LTO
    gcc/c-family/
    	* c-common.c (def_fn_type): Don't call build_function_type, call
    	build_function_type_array or build_varargs_function_type_array
    	instead.
    	(c_common_nodes_and_builtins): Likewise.
    
    gcc/lto/
    	* lto-lang.c (def_fn_type): Don't call build_function_type, call
    	build_function_type_array or build_varargs_function_type_array
    	instead.
    
    From-SVN: r173623

--- gcc/lto/lto-lang.c
+++ gcc/lto/lto-lang.c
@@ -433,7 +433,8 @@ handle_format_arg_attribute (tree * ARG_UNUSED (node), tree ARG_UNUSED (name),
 static void
 def_fn_type (builtin_type def, builtin_type ret, bool var, int n, ...)
 {
-  tree args = NULL, t;
+  tree t;
+  tree *args = XALLOCAVEC (tree, n);
   va_list list;
   int i;
 
@@ -444,18 +445,17 @@ def_fn_type (builtin_type def, builtin_type ret, bool var, int n, ...)
       t = builtin_types[a];
       if (t == error_mark_node)
 	goto egress;
-      args = tree_cons (NULL_TREE, t, args);
+      args[i] = t;
     }
   va_end (list);
-
-  args = nreverse (args);
-  if (!var)
-    args = chainon (args, void_list_node);
 
   t = builtin_types[ret];
   if (t == error_mark_node)
     goto egress;
-  t = build_function_type (t, args);
+  if (var)
+    t = build_varargs_function_type_array (t, n, args);
+  else
+    t = build_function_type_array (t, n, args);
 
  egress:
   builtin_types[def] = t;
