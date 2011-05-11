10c224a9b0780fe3ae5d83a588046887203f2285
Richard Guenther <rguenther@suse.de>
2011-05-11 10:52:57 +0000

    re PR middle-end/18041 (OR of two single-bit bitfields is inefficient)
    2011-05-11  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/18041
    	* tree-ssa-forwprop.c (simplify_bitwise_and): Rename to ...
    	(simplify_bitwise_binary): ... this.  Handle operand conversions
    	by applying them to the result instead.
    	(tree_ssa_forward_propagate_single_use_vars): Adjust.  CSE tree code.
    
    	* gcc.dg/tree-ssa/forwprop-13.c: New testcase.
    
    From-SVN: r173650

---
+++ gcc/testsuite/gcc.dg/tree-ssa/forwprop-13.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-optimized" } */
+
+struct B {
+    unsigned bit0 : 1;
+    unsigned bit1 : 1;
+};
+
+void
+foo (struct B *b)
+{
+  b->bit0 = b->bit0 | b->bit1;
+}
+
+/* { dg-final { scan-tree-dump-not "\\\(unsigned" "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
