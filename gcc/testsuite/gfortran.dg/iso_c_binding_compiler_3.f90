e0ac42061cbe30e46a9d958b1bccd4e8f530228b
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-05-12 17:40:29 +0000

    2011-05-12  Tobias Burnus  <burnus@net-b.de>
            PR fortran/48972
            * resolve.c (resolve_intrinsic): Don't resolve module
            intrinsics multiple times.
    
    2011-05-12  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/48972
            * gfortran.dg/iso_c_binding_compiler_3.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@173708 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/iso_c_binding_compiler_3.f90
@@ -0,0 +1,27 @@
+! { dg-do compile }
+! { dg-options "-Wall" }
+!
+! PR fortran/45823
+!
+! We used to warn about 
+!  "Type specified for intrinsic function" for this file
+!
+
+use iso_c_binding
+use iso_Fortran_env
+implicit none
+intrinsic sin
+real :: x = 3.4
+print *, sin(x), c_sizeof(c_int), compiler_options(), compiler_version()
+end
+
+
+module test_mod
+    use iso_fortran_env
+end module test_mod
+
+subroutine test
+use test_mod
+end subroutine test
+
+! { dg-final { cleanup-modules "test_mod" } }
