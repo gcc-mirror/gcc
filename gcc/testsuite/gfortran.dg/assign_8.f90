d61d771f3bc92b15a77aedb29e36ffe13d3ca47a
Tobias Burnus <burnus@net-b.de>
2011-05-13 22:59:07 +0200

    re PR fortran/48972 (OPEN with Unicode file name)
    2011-05-13  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/48972
            PR fortran/48991
            * gfortran.dg/assign_8.f90: Update dg-error.
    
    From-SVN: r173738

--- gcc/testsuite/gfortran.dg/assign_8.f90
+++ gcc/testsuite/gfortran.dg/assign_8.f90
@@ -1,4 +1,4 @@
 ! { dg-do compile }
 ! PR fortran/20883
-      write (*, a) b ! { dg-error "must be of type CHARACTER or INTEGER" }
+      write (*, a) b ! { dg-error "must be of type default-kind CHARACTER or of INTEGER" }
       end
