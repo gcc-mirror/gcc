9e412fd25c8e6064924460a3dc66b7c068355bfe
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-05-14 09:39:38 +0000

    2011-05-14  Tobias Burnus  <burnus@net-b.de>
            * doc/invoke.texi (-Ofast): Also enables -fstack-arrays.
    
    2011-05-14  Tobias Burnus  <burnus@net-b.de>
    
            * options.c (gfc_init_options, gfc_post_options): Enable
            -fstack-arrays by default if -Ofast is used.
            * invoke.texi (-fstack-arrays): Document this.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@173751 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/options.c
+++ gcc/fortran/options.c
@@ -125,7 +125,7 @@ gfc_init_options (unsigned int decoded_options_count,
 
   /* Default value of flag_max_stack_var_size is set in gfc_post_options.  */
   gfc_option.flag_max_stack_var_size = -2;
-  gfc_option.flag_stack_arrays = 0;
+  gfc_option.flag_stack_arrays = -1;
 
   gfc_option.flag_range_check = 1;
   gfc_option.flag_pack_derived = 0;
@@ -273,6 +273,9 @@ gfc_post_options (const char **pfilename)
 
   if (gfc_option.flag_protect_parens == -1)
     gfc_option.flag_protect_parens = !optimize_fast;
+
+  if (gfc_option.flag_stack_arrays == -1)
+    gfc_option.flag_stack_arrays = optimize_fast;
 
   /* By default, disable (re)allocation during assignment for -std=f95,
      and enable it for F2003/F2008/GNU/Legacy. */
