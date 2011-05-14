3b4c8f3325a4425a05b4d47b41bc96ce9d536c09
tkoenig <tkoenig@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-05-14 09:48:08 +0000

    2011-05-14  Thomas Koenig  <tkoenig@gcc.gnu.org>
    	PR fortran/22572
    	* frontend-passes.c (cfe_register_funcs):  Also register functions
    	for potential elimination if the rank is > 0, the shape is unknown
    	and reallocate on assignment is active.
    	(create_var):  For rank > 0 functions with unknown shape, create
    	an allocatable temporary.
    
    2011-05-14  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/22572
    	* function_optimize_7.f90:  New test case.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@173752 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/function_optimize_7.f90
@@ -0,0 +1,46 @@
+! { dg-do compile }
+! { dg-options "-O -fdump-tree-original -Warray-temporaries" }
+subroutine xx(n, m, a, b, c, d, x, z, i, s_in, s_out)
+  implicit none
+  integer, intent(in) :: n, m
+  real, intent(in), dimension(n,n) :: a, b, c
+  real, intent(out), dimension(n,n) :: d
+  real, intent(in), dimension(n,m) :: s_in
+  real, intent(out), dimension(m) :: s_out
+  integer, intent(out) :: i
+  real, intent(inout) :: x
+  real, intent(out) :: z
+  character(60) :: line
+  real, external :: ext_func
+  interface
+     elemental function element(x)
+       real, intent(in) :: x
+       real :: elem
+     end function element
+     pure function mypure(x)
+       real, intent(in) :: x
+       integer :: mypure
+     end function mypure
+     elemental impure function elem_impure(x)
+       real, intent(in) :: x
+       real :: elem_impure
+     end function elem_impure
+  end interface
+
+  d = matmul(a,b) + matmul(a,b)    ! { dg-warning "Creating array temporary" }
+  z = sin(x) + cos(x) + sin(x) + cos(x)
+  x = ext_func(a) + 23 + ext_func(a)
+  z = element(x) + element(x)
+  i = mypure(x) - mypure(x)
+  z = elem_impure(x) - elem_impure(x)
+  s_out = sum(s_in,1) + 3.14 / sum(s_in,1) ! { dg-warning "Creating array temporary" }
+end subroutine xx
+! { dg-final { scan-tree-dump-times "matmul_r4" 1 "original" } }
+! { dg-final { scan-tree-dump-times "__builtin_sinf" 1 "original" } }
+! { dg-final { scan-tree-dump-times "__builtin_cosf" 1 "original" } }
+! { dg-final { scan-tree-dump-times "ext_func" 2 "original" } }
+! { dg-final { scan-tree-dump-times "element" 1 "original" } }
+! { dg-final { scan-tree-dump-times "mypure" 1 "original" } }
+! { dg-final { scan-tree-dump-times "elem_impure" 2 "original" } }
+! { dg-final { scan-tree-dump-times "sum_r4" 1 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
