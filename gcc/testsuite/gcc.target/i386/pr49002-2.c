03f65847e5de0b4e98c083c980cffa48cdbfdf75
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-05-18 22:12:28 +0000

    Properly handle 256bit load cast.
    gcc/
    
    2011-05-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/49002
    	* config/i386/sse.md (avx_<ssemodesuffix><avxsizesuffix>_<ssemodesuffix>):
    	Properly handle load cast.
    
    gcc/testsuite/
    
    2011-05-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/49002
    	* gcc.target/i386/pr49002-1.c: New test.
    	* gcc.target/i386/pr49002-2.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@173880 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr49002-2.c
@@ -0,0 +1,14 @@
+/* PR target/49002 */
+/* { dg-do compile } */
+/* { dg-options "-O -mavx" } */
+
+#include <immintrin.h>
+
+void foo(const __m128d from, __m256d *to)
+{
+  *to = _mm256_castpd128_pd256(from);
+}
+
+/* Ensure we store ymm, not xmm.  */
+/* { dg-final { scan-assembler-not "vmovapd\[\t \]*%xmm\[0-9\]\+,\[^,\]*" } } */
+/* { dg-final { scan-assembler "vmovapd\[\t \]*%ymm\[0-9\]\+,\[^,\]*" } } */
