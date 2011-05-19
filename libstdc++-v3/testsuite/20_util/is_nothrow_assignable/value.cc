f263981ac8f33dce2cfd904986ee596de5ddcdb1
Paolo Carlini <paolo@gcc.gnu.org>
2011-05-19 10:30:20 +0000

    [multiple changes]
    2011-05-19  Daniel Krugler  <daniel.kruegler@googlemail.com>
    
    	* testsuite/util/testsuite_tr1.h: Add test classes.
    	* testsuite/20_util/is_nothrow_assignable/value.cc: Add.
    
    2011-05-19  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* include/std/type_traits (is_assignable, is_copy_assignable,
    	is_move_assignable, is_nothrow_assignable, is_nothrow_copy_assignable,
    	is_nothrow_move_assignable): Add; minor tweaks elsewhere.
    	(has_nothrow_copy_assign): Remove.
    	* testsuite/util/testsuite_tr1.h: Add test classes.
    	* testsuite/20_util/is_assignable/requirements/typedefs.cc: Add.
    	* testsuite/20_util/is_assignable/requirements/
    	explicit_instantiation.cc: Likewise.
    	* testsuite/20_util/is_nothrow_assignable/value.cc: Likewise.
    	* testsuite/20_util/is_nothrow_assignable/requirements/typedefs.cc:
    	Likewise.
    	* testsuite/20_util/is_nothrow_assignable/requirements/
    	explicit_instantiation.cc: Likewise.
    	* testsuite/20_util/is_move_assignable/value.cc: Likewise.
    	* testsuite/20_util/is_move_assignable/requirements/typedefs.cc:
    	Likewise.
    	* testsuite/20_util/is_move_assignable/requirements/
    	explicit_instantiation.cc: Likewise.
    	* testsuite/20_util/is_copy_assignable/value.cc: Likewise.
    	* testsuite/20_util/is_copy_assignable/requirements/typedefs.cc:
    	Likewise.
    	* testsuite/20_util/is_copy_assignable/requirements/
    	explicit_instantiation.cc: Likewise.
    	* testsuite/20_util/is_nothrow_move_assignable/value.cc: Likewise.
    	* testsuite/20_util/is_nothrow_move_assignable/requirements/
    	typedefs.cc: Likewise.
    	* testsuite/20_util/is_nothrow_move_assignable/requirements/
    	explicit_instantiation.cc: Likewise.
    	* testsuite/20_util/is_nothrow_copy_assignable/value.cc: Likewise.
    	* testsuite/20_util/is_nothrow_copy_assignable/requirements/
    	typedefs.cc: Likewise.
    	* testsuite/20_util/is_nothrow_copy_assignable/requirements/
    	explicit_instantiation.cc: Likewise.
    	* testsuite/20_util/make_signed/requirements/typedefs_neg.cc: Adjust
    	dg-error line numbers.
    	* testsuite/20_util/make_unsigned/requirements/typedefs_neg.cc:
    	Likewise.
    	* testsuite/20_util/declval/requirements/1_neg.cc: Likewise.
    
    From-SVN: r173899

---
+++ libstdc++-v3/testsuite/20_util/is_nothrow_assignable/value.cc
@@ -0,0 +1,58 @@
+// { dg-options "-std=gnu++0x" }
+// 2011-05-19  Paolo Carlini  <paolo.carlini@oracle.com>
+//
+// Copyright (C) 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <type_traits>
+#include <testsuite_hooks.h>
+#include <testsuite_tr1.h>
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  using std::is_nothrow_assignable;
+  using namespace __gnu_test;
+
+  // Positive tests.
+  VERIFY( (test_relationship<is_nothrow_assignable, int&, int>(true)) );
+  VERIFY( (test_relationship<is_nothrow_assignable, int&, const int>(true)) );
+
+  VERIFY( (test_relationship<is_nothrow_assignable,
+	   NoexceptCopyAssignClass&, const NoexceptCopyAssignClass&>(true)) );
+  VERIFY( (test_relationship<is_nothrow_assignable,
+	   NoexceptMoveAssignClass&, NoexceptMoveAssignClass&&>(true)) );
+  VERIFY( (test_relationship<is_nothrow_assignable,
+	   NoexceptCopyAssignClass&, NoexceptCopyAssignClass&&>(true)) );
+
+  // Negative tests.
+  VERIFY( (test_relationship<is_nothrow_assignable, int, int>(false)) );
+  VERIFY( (test_relationship<is_nothrow_assignable, int, const int>(false)) );
+
+  VERIFY( (test_relationship<is_nothrow_assignable,
+	   ExceptCopyAssignClass&, const ExceptCopyAssignClass&>(false)) );
+  VERIFY( (test_relationship<is_nothrow_assignable,
+	   ExceptMoveAssignClass&, ExceptMoveAssignClass&&>(false)) );
+  VERIFY( (test_relationship<is_nothrow_assignable,
+	   NoexceptMoveAssignClass&, const NoexceptMoveAssignClass&>(false)) );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/20_util/is_nothrow_move_assignable/value.cc
@@ -0,0 +1,75 @@
+// { dg-options "-std=gnu++0x" }
+// 2011-05-19  Paolo Carlini  <paolo.carlini@oracle.com>
+//
+// Copyright (C) 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <type_traits>
+#include <testsuite_hooks.h>
+#include <testsuite_tr1.h>
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  using std::is_nothrow_move_assignable;
+  using namespace __gnu_test;
+
+  // Positive tests.
+  VERIFY( (test_property<is_nothrow_move_assignable, int>(true)) );
+  VERIFY( (test_property<is_nothrow_move_assignable, float>(true)) );
+  VERIFY( (test_property<is_nothrow_move_assignable, EnumType>(true)) );
+  VERIFY( (test_property<is_nothrow_move_assignable, int*>(true)) );
+  VERIFY( (test_property<is_nothrow_move_assignable, int(*)(int)>(true)) );
+  VERIFY( (test_property<is_nothrow_move_assignable,
+	   int (ClassType::*)>(true)) );
+  VERIFY( (test_property<is_nothrow_move_assignable,
+	   int (ClassType::*) (int)>(true)) );
+
+  VERIFY( (test_property<is_nothrow_move_assignable,
+	   NoexceptMoveAssignClass>(true)) );
+  VERIFY( (test_property<is_nothrow_move_assignable,
+	   NoexceptCopyAssignClass>(true)) );
+
+  // Negative tests.
+  VERIFY( (test_property<is_nothrow_move_assignable, void>(false)) );
+  VERIFY( (test_property<is_nothrow_move_assignable, int[2]>(false)) );
+  VERIFY( (test_property<is_nothrow_move_assignable, float[][3]>(false)) );
+  VERIFY( (test_property<is_nothrow_move_assignable,
+	   EnumType[2][3][4]>(false)) );
+  VERIFY( (test_property<is_nothrow_move_assignable, int*[3]>(false)) );
+  VERIFY( (test_property<is_nothrow_move_assignable,
+	   int(*[][2])(int)>(false)) );
+  VERIFY( (test_property<is_nothrow_move_assignable,
+	   int (ClassType::*[2][3])>(false)) );
+  VERIFY( (test_property<is_nothrow_move_assignable, 
+	   int (ClassType::*[][2][3]) (int)>(false)) );
+
+  VERIFY( (test_property<is_nothrow_move_assignable,
+	   ExceptMoveAssignClass>(false)) );
+  VERIFY( (test_property<is_nothrow_move_assignable,
+	   ExceptCopyAssignClass>(false)) );
+  VERIFY( (test_property<is_nothrow_move_assignable,
+	   DeletedMoveAssignClass>(false)) );
+  VERIFY( (test_property<is_nothrow_move_assignable,
+	   DeletedCopyAssignClass>(false)) );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
