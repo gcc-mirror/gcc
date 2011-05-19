664e12c1265280786f83d8fd1ec1ebbf50c153c0
Paolo Carlini <paolo.carlini@oracle.com>
2011-05-19 20:48:39 +0000

    tuple (tuple_element<__i, [...]): Add.
    2011-05-19  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* include/std/tuple (tuple_element<__i, const _Tp>,
    	tuple_element<__i, volatile _Tp>, tuple_element<__i,
    	const volatile _Tp>, tuple_size<const _Tp>, tuple_size<volatile _Tp>,
    	tuple_size<const volatile _Tp>): Add.
    	* include/std/utility (tuple_size<std::pair<_Tp1, _Tp2>>): Tweak.
    	* include/std/array (tuple_size<array<_Tp, _Nm>>): Likewise.
    	* testsuite/20_util/tuple/cv_tuple_size.cc: New.
    	* testsuite/20_util/tuple/cv_tuple_element.cc: Likewise.
    	* testsuite/20_util/weak_ptr/comparison/cmp_neg.cc: Tweak dg-warning
    	line number.
    
    From-SVN: r173919

---
+++ libstdc++-v3/testsuite/20_util/tuple/cv_tuple_size.cc
@@ -0,0 +1,45 @@
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// Tuple
+
+#include <tuple>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace std;
+
+  VERIFY( tuple_size<const tuple<> >::value == 0 );
+  VERIFY( tuple_size<volatile tuple<int> >::value == 1 );
+  VERIFY( tuple_size<const volatile tuple<void> >::value == 1 );
+
+  typedef tuple<int, const int&, void> test_tuple1;
+  VERIFY( tuple_size<const test_tuple1>::value == 3 );
+  VERIFY( tuple_size<const volatile test_tuple1>::value == 3 );
+  VERIFY( tuple_size<volatile tuple<tuple<void> > >::value == 1 );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
