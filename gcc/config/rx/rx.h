52716f706445f9793b221109a9aacdbfad7974e3
Nick Clifton <nickc@redhat.com>
2011-05-20 11:21:13 +0000

    * config/rx/rx.h (HAVE_PRE_DECREMENT): Fix typo in macro name.
    From-SVN: r173940

--- gcc/config/rx/rx.h
+++ gcc/config/rx/rx.h
@@ -168,7 +168,7 @@ extern enum rx_cpu_types  rx_cpu_type;
 
 #define HANDLE_PRAGMA_PACK_PUSH_POP	1
 
-#define HAVE_PRE_DECCREMENT		1
+#define HAVE_PRE_DECREMENT		1
 #define HAVE_POST_INCREMENT		1
 
 #define MOVE_RATIO(SPEED) 		((SPEED) ? 4 : 2)
