72fc0f640a5c7718ae28e09e1db825c2c61faef4
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-05-21 22:01:29 +0000

    PR c++/48780 	* cvt.c (type_promotes_to): Don't promote scoped enums.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@174005 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/enum13.C
@@ -0,0 +1,20 @@
+// PR c++/48780
+// { dg-options "-std=c++0x -fabi-version=5 -Wabi" }
+
+typedef __builtin_va_list __gnuc_va_list;
+typedef __gnuc_va_list va_list;
+
+enum struct A : short { X };
+
+void foo(int x, ...) {
+   va_list vl;
+   __builtin_va_start(vl, x);
+   enum A t = __builtin_va_arg(vl, enum A); // { dg-warning "promote" }
+   __builtin_va_end(vl);
+}
+
+int main() {
+   foo(0, A::X);		// { dg-warning "will not promote" }
+}
+
+// { dg-prune-output "note" }
