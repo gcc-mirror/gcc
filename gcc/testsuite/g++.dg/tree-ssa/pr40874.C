e6d944b22615da54b20b4f53a9dad5cad4e0eeb4
Richard Guenther <rguenther@suse.de>
2011-05-23 14:36:28 +0000

    re PR tree-optimization/40874 (Function object abstraction penalty with inline functions.)
    2011-05-23  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/40874
    	* g++.dg/tree-ssa/pr40874.C: New testcase.
    
    From-SVN: r174068

---
+++ gcc/testsuite/g++.dg/tree-ssa/pr40874.C
@@ -0,0 +1,25 @@
+// { dg-do compile }
+// { dg-options "-O -fdump-tree-optimized" }
+
+struct pf
+{
+  inline pf(int(*x)(int)) : x(x) {}
+
+  inline int operator()(int a) const
+    {
+      return x(a);
+    }
+
+  int (*x)(int);
+};
+
+inline int g(int x) { return x/x - 1; }
+
+int main(int argc, char* argv[])
+{
+  pf f(g);
+  return f(3);
+}
+
+// { dg-final { scan-tree-dump "return 0" "optimized" } }
+// { dg-final { cleanup-tree-dump "optimized" } }
