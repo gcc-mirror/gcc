44a3efe1a275bf4f5e4596928bb2f1d940ec2e3e
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-05-23 15:32:19 +0000

    PR c++/47544 	* pt.c (instantiate_decl): Handle =default.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@174071 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/defaulted27.C
@@ -0,0 +1,19 @@
+// PR c++/47544
+// { dg-options -std=c++0x }
+// { dg-final { scan-assembler "_ZN1sIiEC2Ev" } }
+// { dg-final { scan-assembler-not "_ZN1sIiED2Ev" } }
+
+template <typename T>
+struct s {
+  s();
+  ~s() = default;
+};
+
+extern template struct s<int>;
+
+template <typename T>
+s<T>::s() = default;
+
+template struct s<int>;
+
+s<int> a;
