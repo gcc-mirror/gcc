4702f095fa6a0e4a76f0fff61b1693fde6db9eaa
Paolo Carlini <paolo@gcc.gnu.org>
2011-05-23 00:05:24 +0000

    [multiple changes]
    2011-05-22  Jonathan Wakely  <jwakely.gcc@gmail.com>
    
    	* testsuite/20_util/bind/cv_quals_2.cc: New.
    
    2011-05-22  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR libstdc++/49058
    	* include/std/functional (_Bind<_Functor(_Bound_args...)>::
    	operator()(_Args&&...)): Don't cv qualify _Functor directly
    	in the default template argument, SFINAE doesn't apply when
    	the functor has no arguments.
    	* testsuite/20_util/bind/49058_1.cc: New.
    	* testsuite/20_util/bind/49058_2.cc: Likewise.
    
    From-SVN: r174048

---
+++ libstdc++-v3/testsuite/20_util/bind/cv_quals_2.cc
@@ -0,0 +1,49 @@
+// Copyright (C) 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++0x" }
+
+#include <functional>
+#include <testsuite_hooks.h>
+
+struct X
+{
+  int operator()() const { return 0; }
+  int operator()() volatile { return 1; }
+  int operator()() const volatile { return 2; }
+  void operator()() { };
+};
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  const auto b0 = std::bind(X());
+  VERIFY( b0() == 0 );
+
+  volatile auto b1 = std::bind(X());
+  VERIFY( b1() == 1 );
+
+  const volatile auto b2 = std::bind(X());
+  VERIFY( b2() == 2 );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
