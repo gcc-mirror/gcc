c0156a980026b9fb9e01e232769220d6cc46652a
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-05-25 14:35:00 +0000

    PR c++/45080 	* pt.c (instantiate_class_template_1): Call maybe_add_lambda_conv_op. 	* semantics.c (lambda_function): Check COMPLETE_OR_OPEN_TYPE_P.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@174203 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -8566,6 +8566,9 @@ instantiate_class_template_1 (tree type)
 	}
     }
 
+  if (CLASSTYPE_LAMBDA_EXPR (type))
+    maybe_add_lambda_conv_op (type);
+
   /* Set the file and line number information to whatever is given for
      the class itself.  This puts error messages involving generated
      implicit functions at a predictable point, and the same point
