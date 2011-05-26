66c674b69d2bcd5fdf8dfd7b3f591f31e9a1e56b
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-05-26 18:19:36 +0000

    2011-05-26  Paul Thomas  <pault@gcc.gnu.org> 	    Thomas Koenig  <tkoenig@gcc.gnu.org>
    	PR fortran/48955
    	* trans-expr.c (gfc_trans_assignment_1): GFC_REVERSE_NOT_SET
    	changed to GFC_ENABLE_REVERSE.
    	* trans-array.c (gfc_init_loopinfo): GFC_CANNOT_REVERSE changed
    	to GFC_INHIBIT_REVERSE.
    	* gfortran.h : Enum gfc_reverse is now GFC_ENABLE_REVERSE,
    	GFC_FORWARD_SET, GFC_REVERSE_SET and GFC_INHIBIT_REVERSE.
    	* dependency.c (gfc_dep_resolver): Change names for elements of
    	gfc_reverse as necessary. Change the logic so that forward
    	dependences are remembered as well as backward ones. When both
    	have appeared, force a temporary.
    
    2011-05-26  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/48955
    	* gfortran.dg/dependency_40.f90 : New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@174302 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/dependency_40.f90
@@ -0,0 +1,29 @@
+! { dg-do run }
+! PR 48955 - missing array temporary when there was both a forward
+! and a backward dependency.
+! Test case slightly modified from the original one by Kacper Kowalik.
+program ala
+   implicit none
+
+   integer, parameter  :: n = 6
+   real, dimension(n), parameter :: result = [1.,10.,30.,90.,270., 243.];
+   real, dimension(n) :: v0, v1
+   character(len=80) :: line1, line2
+
+   v0 = [1.0, 3.0, 9.0, 27.0, 81.0, 243.0]
+   v1 = v0
+
+   v1(2:n-1) = v1(1:n-2) + v1(3:n)
+   if (any(v1 /= result)) call abort
+   v1 = v0
+   v1(2:n-1) = v0(1:n-2) + v0(3:n)
+   if (any(v1 /= result)) call abort
+
+   v1 = v0
+   v1(2:n-1) = v1(3:n) + v1(1:n-2)
+   if (any(v1 /= result)) call abort
+   v1 = v0
+   v1(2:n-1) = v0(3:n) + v0(1:n-2)
+   if (any(v1 /= result)) call abort
+
+end program ala
