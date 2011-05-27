a4aa9d6b4ccfec311bb0a7b47848c17be5f7b7af
Jakub Jelinek <jakub@redhat.com>
2011-05-27 21:23:46 +0200

    re PR c++/49165 (ICE on for-loop/throw combination)
    	PR c++/49165
    	* c-common.c (c_common_truthvalue_conversion) <case COND_EXPR>: For
    	C++ don't call c_common_truthvalue_conversion on void type arms.
    
    	* g++.dg/eh/cond6.C: New test.
    
    From-SVN: r174351

--- gcc/c-family/c-common.c
+++ gcc/c-family/c-common.c
@@ -3915,14 +3915,15 @@ c_common_truthvalue_conversion (location_t location, tree expr)
       /* Distribute the conversion into the arms of a COND_EXPR.  */
       if (c_dialect_cxx ())
 	{
+	  tree op1 = TREE_OPERAND (expr, 1);
+	  tree op2 = TREE_OPERAND (expr, 2);
+	  /* In C++ one of the arms might have void type if it is throw.  */
+	  if (!VOID_TYPE_P (TREE_TYPE (op1)))
+	    op1 = c_common_truthvalue_conversion (location, op1);
+	  if (!VOID_TYPE_P (TREE_TYPE (op2)))
+	    op2 = c_common_truthvalue_conversion (location, op2);
 	  expr = fold_build3_loc (location, COND_EXPR, truthvalue_type_node,
-			      TREE_OPERAND (expr, 0),
-			      c_common_truthvalue_conversion (location,
-							      TREE_OPERAND (expr,
-									    1)),
-			      c_common_truthvalue_conversion (location,
-							      TREE_OPERAND (expr,
-									    2)));
+				  TREE_OPERAND (expr, 0), op1, op2);
 	  goto ret;
 	}
       else
