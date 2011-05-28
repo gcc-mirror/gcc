60ef2e56028cf9131e61716135bc289a1e193941
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-05-28 00:53:49 +0000

    PR c++/47049 	* semantics.c (maybe_add_lambda_conv_op): Fix COMDAT sharing. 	* decl.c (start_preparsed_function): Don't call comdat_linkage for 	a template.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@174375 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/decl.c
+++ gcc/cp/decl.c
@@ -12672,6 +12672,7 @@ start_preparsed_function (tree decl1, tree attrs, int flags)
 	DECL_EXTERNAL (decl1) = 0;
 
       if (ctx != NULL_TREE && DECL_DECLARED_INLINE_P (ctx)
+	  && !processing_template_decl
 	  && TREE_PUBLIC (ctx))
 	/* This is a function in a local class in an extern inline
 	   function.  */
