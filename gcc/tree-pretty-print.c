9ea3d82629ae93d478ce394734886e7118975254
davidxl <davidxl@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-05-30 04:42:05 +0000

    Revert wrong checkin
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@174424 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-pretty-print.c
+++ gcc/tree-pretty-print.c
@@ -3013,25 +3013,3 @@ pp_base_tree_identifier (pretty_printer *pp, tree id)
     pp_append_text (pp, IDENTIFIER_POINTER (id),
 		    IDENTIFIER_POINTER (id) + IDENTIFIER_LENGTH (id));
 }
-
-#if 0
-void
-pass_dump_function_header (FILE *dump_file, tree fdecl, struct function *fun)
-{
-  const char *dname, *aname;
-  struct cgraph_node *node = cgraph_get_node (fdecl);
-  dname = lang_hooks.decl_printable_name (fdecl, 2);
-  aname = (IDENTIFIER_POINTER
-	   (DECL_ASSEMBLER_NAME (fdecl)));
-  fprintf (dump_file, "\n;; Function %s (%s)[fundef_no:%d][uid=%d]",
-           dname, aname, fun->funcdef_no, node->uid);
-  fprintf (dump_file, "%s\n\n",
-           node->frequency == NODE_FREQUENCY_HOT
-           ? " (hot)"
-           : node->frequency == NODE_FREQUENCY_UNLIKELY_EXECUTED
-           ? " (unlikely executed)"
-           : node->frequency == NODE_FREQUENCY_EXECUTED_ONCE
-           ? " (executed once)"
-           : "");
-}
-#endif
