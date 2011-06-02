f7a6fca499c181a012baa05673db826cbfe7cc61
langton <langton@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-06-02 21:41:12 +0000

    * trans-decl.c (gfc_trans_deferred_vars): Treat assumed-size Cray         pointees as AS_EXPLICIT.
        * gfortran.dg/PR49268.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@174584 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/PR49268.f90
@@ -0,0 +1,51 @@
+! { dg-do run }
+! { dg-options "-fcray-pointer" }
+
+! Test the fix for a runtime error 
+! Contributed by Mike Kumbera <kumbera1@llnl.gov>
+
+        program bob
+        implicit none
+        integer*8 ipfoo
+        integer n,m,i,j
+        real*8 foo
+        
+        common /ipdata/ ipfoo
+        common /ipsize/ n,m
+        POINTER ( ipfoo, foo(3,7) )
+
+        n=3
+        m=7
+
+        ipfoo=malloc(8*n*m)
+        do i=1,n
+            do j=1,m
+                foo(i,j)=1.d0
+            end do
+        end do
+        call use_foo()
+        end  program bob
+
+
+        subroutine use_foo()
+        implicit none
+        integer n,m,i,j
+        integer*8 ipfoo
+        common /ipdata/ ipfoo
+        common /ipsize/ n,m
+        real*8 foo,boo
+
+        !fails if * is the last dimension
+        POINTER ( ipfoo, foo(n,*) )
+
+        !works if the last dimension is specified
+        !POINTER ( ipfoo, foo(n,m) )
+        boo=0.d0
+        do i=1,n
+            do j=1,m
+               boo=foo(i,j)+1.0
+               if (abs (boo - 2.0) .gt. 1e-6) call abort
+            end do
+        end do
+
+        end subroutine use_foo
