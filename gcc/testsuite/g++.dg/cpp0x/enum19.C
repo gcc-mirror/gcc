3c395ecf7d41384016acbcc18d4847c72685013c
Jason Merrill <jason@redhat.com>
2011-06-07 11:09:29 -0400

    re PR c++/48780 ([C++0x] scoped enumerations and va_arg (default argument promotions))
    	PR c++/48780
    	* typeck.c (perform_integral_promotions): Don't promote scoped enums.
    	* call.c (convert_arg_to_ellipsis): Promote them here in old ABI.
    
    From-SVN: r174751

---
+++ gcc/testsuite/g++.dg/cpp0x/enum19.C
@@ -0,0 +1,12 @@
+// We shouldn't give an ABI warning about promotion in switch.
+// { dg-options "-std=c++0x -fabi-version=5 -Wabi" }
+
+enum class Foo { X };
+void test(Foo val)
+{
+    switch(val)
+    {
+    case Foo::X:
+        break;
+    }
+};
