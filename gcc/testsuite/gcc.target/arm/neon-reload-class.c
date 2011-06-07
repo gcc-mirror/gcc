a02a9ea59728a0323dcf5889b6632d2b18aa80e6
amonakov <amonakov@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-06-07 14:46:42 +0000

    2011-06-07  Sergey Grechanik  <mouseentity@ispras.ru>
    	* config/arm/arm.c (coproc_secondary_reload_class): Return NO_REGS for
    	constant vectors.
    
    testsuite:
    	* gcc.target/arm/neon-reload-class.c: New test.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@174749 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/neon-reload-class.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target arm_neon_ok } */
+/* { dg-options "-O2 -ftree-vectorize" }  */
+/* { dg-add-options arm_neon } */
+
+
+void
+_op_blend_p_caa_dp(unsigned *s, unsigned* e, unsigned *d, unsigned c) {
+  while (d < e) {
+    *d = ( (((((*s) >> 8) & 0x00ff00ff) * (c)) & 0xff00ff00) + (((((*s) & 0x00ff00ff) * (c)) >> 8) & 0x00ff00ff) );
+    d++;
+    s++;
+  }
+}
+
+/* These constants should be emitted as immediates rather than loaded from memory.  */
+
+/* { dg-final { scan-assembler-not "(\\.d?word|mov(w|t))" } } */
