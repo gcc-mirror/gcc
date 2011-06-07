901a9d428e1f22c1e70415ae2d992a2247a7a714
davidxl <davidxl@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-06-07 19:13:09 +0000

    Handle assembler name in -fdisable/enable options
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@174762 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/inline_2.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized -fdisable-tree-einline=0:100 -fdisable-ipa-inline" } */
+int g;
+__attribute__((always_inline)) void bar (void)
+{
+  g++;
+}
+
+int foo (void)
+{
+  bar ();
+  return g;
+}
+
+int foo2 (void)
+{
+  bar();
+  return g + 1;
+}
+
+/* { dg-final { scan-tree-dump-times "bar" 5 "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
+/* { dg-excess-errors "extra notes" } */
