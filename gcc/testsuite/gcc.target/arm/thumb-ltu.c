264404492618e038a4686e6fd50d18802b2f9fa0
Janis Johnson <janisjo@codesourcery.com>
2011-06-10 18:23:11 +0000

    20090811-1.c: Skip for incompatible options, do not override other options.
    	* gcc/testsuite/gcc.target/arm/20090811-1.c: Skip for incompatible
    	options, do not override other options.
    	* gcc/testsuite/gcc.target/arm/combine-cmp-shift.c: Skip for
    	incompatible options.
    	* gcc/testsuite/gcc.target/arm/pr45094.c: Likewise.
    	* gcc/testsuite/gcc.target/arm/scd42-1.c: Likewise.
    	* gcc/testsuite/gcc.target/arm/scd42-3.c: Likewise.
    	* gcc/testsuite/gcc.target/arm/thumb-ltu.c: Likewise.
    
    From-SVN: r174920

--- gcc/testsuite/gcc.target/arm/thumb-ltu.c
+++ gcc/testsuite/gcc.target/arm/thumb-ltu.c
@@ -1,4 +1,5 @@
 /* { dg-do compile } */
+/* { dg-skip-if "incompatible options" { arm*-*-* } { "-march=*" } { "-march=armv6" "-march=armv6j" "-march=armv6z" } } */
 /* { dg-options "-mcpu=arm1136jf-s -mthumb -O2" } */
 
 void f(unsigned a, unsigned b, unsigned c, unsigned d)
