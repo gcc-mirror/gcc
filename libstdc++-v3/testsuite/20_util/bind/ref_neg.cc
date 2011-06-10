32293e2bd60afe1e629c5b563e00314b3201495c
Jason Merrill <jason@redhat.com>
2011-06-10 10:20:28 -0400

    * testsuite/20_util/bind/ref_neg.cc: Remove wrong test lines.
    From-SVN: r174907

--- libstdc++-v3/testsuite/20_util/bind/ref_neg.cc
+++ libstdc++-v3/testsuite/20_util/bind/ref_neg.cc
@@ -29,10 +29,8 @@ int inc(int& i) { return ++i; }
 void test01()
 {
   const int dummy = 0;
-  std::bind(&inc, _1)(0);               // { dg-error "" }
-  std::bind(&inc, std::ref(dummy))();  // { dg-error "" }
-  std::bind(&inc, dummy)();
-  std::bind(&inc, 0)();
+  std::bind(&inc, _1)(0);               // { dg-error  "no match" }
+  std::bind(&inc, std::ref(dummy))();	// { dg-error  "no match" }
 }
 
 struct Inc
@@ -46,8 +44,8 @@ struct Inc
 void test02()
 {
   const int dummy = 0;
-  std::bind(Inc(), _1)(dummy);                  // { dg-error "" }
-  std::bind(&Inc::f, Inc(), std::ref(dummy))(); // { dg-error "" }
+  std::bind(Inc(), _1)(dummy);                  // { dg-error  "no match" }
+  std::bind(&Inc::f, Inc(), std::ref(dummy))(); // { dg-error  "no match" }
 }
 
 int main()
