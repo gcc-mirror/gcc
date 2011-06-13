4f21f0daa3b108e1e38be13234e4f6119aa1c33b
Thomas Koenig <tkoenig@gcc.gnu.org>
2011-06-13 08:36:47 +0000

    frontend-passes.c (remove_trim): New function.
    2011-06-13  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	* frontend-passes.c (remove_trim):  New function.
    	(optimize_assignment):  Use it.
    	(optimize_comparison):  Likewise.  Return correct status
    	for previous change.
    
    2011-06-13  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	* gfortran.dg/trim_optimize_8.f90:  New test case.
    
    From-SVN: r174983

---
+++ gcc/testsuite/gfortran.dg/trim_optimize_8.f90
@@ -0,0 +1,14 @@
+! { dg-do compile }
+! { dg-options "-O -fdump-tree-original" }
+! Check that trailing trims are also removed from assignment of
+! expressions involving concatenations of strings .
+program main
+  character(2) :: a,b
+  character(8) :: d
+  a = 'a '
+  b = 'b '
+  if (trim(a // trim(b)) /= 'a b ') call abort
+  if (trim (trim(a) // trim(b)) /= 'ab ') call abort
+end
+! { dg-final { scan-tree-dump-times "string_len_trim" 1 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
