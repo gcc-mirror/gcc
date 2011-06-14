97bf331110778169510a07a3ba8a42778c2826ce
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-06-14 18:15:43 +0000

    PR c++/49290 	* semantics.c (cxx_fold_indirect_ref): Local, more permissive copy 	of fold_indirect_ref_1. 	(cxx_eval_indirect_ref): Use it.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@175041 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-array-ptr7.C
@@ -0,0 +1,20 @@
+// PR c++/49290
+// { dg-options -std=c++0x }
+
+typedef unsigned T;
+struct S
+{
+  constexpr T foo (void);
+  unsigned s1[16];
+};
+
+constexpr T
+S::foo ()
+{
+  return *(T *) (s1 + 10);
+}
+
+constexpr S s = { 0,1,2,3,4,5,6,7,8,9,10 };
+
+#define SA(X) static_assert ((X), #X)
+SA(s.foo() == 10);
