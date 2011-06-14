62fa2b631309af82e77fcb266f8547439fb34fd2
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-06-14 15:05:26 +0000

    2011-06-14  Tom de Vries  <tom@codesourcery.com>
    	PR target/45098
    	* gcc.target/arm/ivopts-3.c: New test.
    	* gcc.target/arm/ivopts-4.c: New test.
    	* gcc.target/arm/ivopts-5.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@175025 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/ivopts-3.c
@@ -0,0 +1,26 @@
+/* { dg-do assemble } */
+/* { dg-options "-Os -mthumb -fdump-tree-ivopts -save-temps" } */
+
+extern unsigned int foo2 (short*) __attribute__((pure));
+
+unsigned int
+tr3 (short array[], unsigned int n)
+{
+  int sum = 0;
+  unsigned int x;
+  x = 0;
+  while (1)
+    {
+      sum += foo2 (&array[x]);
+      if (!(x < n))
+        break;
+      x++;
+    }
+  return sum;
+}
+
+/* { dg-final { scan-tree-dump-times "PHI <ivtmp" 1 "ivopts"} } */
+/* { dg-final { scan-tree-dump-times "PHI <x" 0 "ivopts"} } */
+/* { dg-final { scan-tree-dump-times ", x" 0 "ivopts"} } */
+/* { dg-final { object-size text <= 30 { target arm_thumb2_ok } } } */
+/* { dg-final { cleanup-tree-dump "ivopts" } } */
