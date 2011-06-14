84f32ce3b24cd508e9a4b3b4d716ab7fc293fba1
Jason Merrill <jason@redhat.com>
2011-06-14 14:16:06 -0400

    re PR c++/49117 (4.5 -> 4.6: user-unfriendly change in "invalid conversion" error message)
    	PR c++/49117
    	* call.c (perform_implicit_conversion_flags): Print source type as
    	well as expression.
    
    From-SVN: r175044

--- gcc/testsuite/g++.dg/other/error23.C
+++ gcc/testsuite/g++.dg/other/error23.C
@@ -2,4 +2,4 @@
 // { dg-do compile }
 
 int v __attribute ((vector_size (8)));
-bool b = !(v - v);	// { dg-error "could not convert .\\(__vector.2. int\\)\\{0, 0\\}. to .bool.|in argument to unary" }
+bool b = !(v - v);	// { dg-error "could not convert .\\(__vector.2. int\\)\\{0, 0\\}. from .__vector.2. int. to .bool.|in argument to unary" }
