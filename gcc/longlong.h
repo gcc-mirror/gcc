ed7d4a795263d3d2c0c5022308bcced0659d2417
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-06-18 15:35:10 +0000

    Use long long builtins of zero counting for x86-64.
    2011-06-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* longlong.h (count_leading_zeros): Use long long builtin for
    	x86-64.
    	(count_trailing_zeros): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@175183 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/longlong.h
+++ gcc/longlong.h
@@ -1,6 +1,6 @@
 /* longlong.h -- definitions for mixed size 32/64 bit arithmetic.
    Copyright (C) 1991, 1992, 1994, 1995, 1996, 1997, 1998, 1999, 2000,
-   2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010
+   2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011
    Free Software Foundation, Inc.
 
    This file is part of the GNU C Library.
@@ -436,8 +436,8 @@ UDItype __umulsidi3 (USItype, USItype);
 	   : "0" ((UDItype) (n0)),					\
 	     "1" ((UDItype) (n1)),					\
 	     "rm" ((UDItype) (dv)))
-#define count_leading_zeros(count, x)	((count) = __builtin_clzl (x))
-#define count_trailing_zeros(count, x)	((count) = __builtin_ctzl (x))
+#define count_leading_zeros(count, x)	((count) = __builtin_clzll (x))
+#define count_trailing_zeros(count, x)	((count) = __builtin_ctzll (x))
 #define UMUL_TIME 40
 #define UDIV_TIME 40
 #endif /* x86_64 */
