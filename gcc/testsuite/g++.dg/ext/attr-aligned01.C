344bf2e1a23979f149b9489155dfe2ee74e38273
Jason Merrill <jason@redhat.com>
2011-06-20 22:24:09 -0400

    re PR c++/48138 (__attribute__((aligned)) should give an error when applied to a typedef or template parameter, at least in C++0x mode.)
    	PR c++/48138
    	* pt.c (canonicalize_type_argument): New.
    	(convert_template_argument, unify): Use it.
    
    From-SVN: r175236

--- gcc/testsuite/g++.dg/ext/attr-aligned01.C
+++ gcc/testsuite/g++.dg/ext/attr-aligned01.C
@@ -1,20 +1,25 @@
 // PR c++/48138
-// { dg-options -std=c++0x }
 
 #define ALIGNED(x) __attribute__((aligned(x)))
-#define SA(X) static_assert ((X),#X)
+#define SA(X) int ar[(X)?1:-1];
 
 template<typename T>
 void type_alignment(const T&) {
   struct { char c; T t; } s;
-  SA((char*)&s.t - (char*)&s.c == 8);
+  SA((char*)&s.t - (char*)&s.c == 1);
 }
+
+template <class T> struct A { char c; T t; };
 
 int main() {
   typedef char unaligned[15];
   typedef char aligned[15] ALIGNED(8);
 
+  A<aligned> a;			// { dg-warning "ignoring attributes" }
+
+  SA((char*)&a.t - (char*)&a.c == 1);
+
   aligned z;
-  type_alignment(z);
-  type_alignment<unaligned ALIGNED(8)>(z);
+  type_alignment(z);		// { dg-warning "ignoring attributes" "" { xfail *-*-* } }
+  type_alignment<unaligned ALIGNED(8)>(z); // { dg-warning "ignoring attributes" "" { xfail *-*-* } }
 }
