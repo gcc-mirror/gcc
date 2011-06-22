5e260adbb1f177a25a39691d3e4510d6bc3b898a
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-06-22 04:18:02 +0000

    PR c++/49172 	* decl.c (cp_finish_decl): Adjust init_const_expr_p for refs. 	(grokdeclarator): constexpr doesn't apply const for refs. 	* parser.c (cp_parser_initializer_clause): Don't call 	maybe_constant_value here. 	* call.c (initialize_reference): Handle constexpr.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@175284 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp0x/constexpr-ice2.C
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-ice2.C
@@ -1,3 +1,3 @@
 // { dg-options -std=c++0x }
 int x;
-constexpr int& rx = x; // { dg-error "int&" }
+constexpr int& rx = x;
---
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-ref2.C
@@ -0,0 +1,17 @@
+// Negative reference variable tests.
+// { dg-options -std=c++0x }
+
+extern int *p;
+constexpr int& ri = *p;		// { dg-error "p" }
+
+extern constexpr int &er;	// { dg-error "not a definition" }
+constexpr int& ri2 = er;	// { dg-error "er" }
+
+void f(int j)
+{
+  constexpr int i = 42;
+  constexpr int const& ri = i;	// { dg-error "" }
+
+  constexpr int& rj = j;	// { dg-error "" }
+}
+
