1da9434b4ba81839aa43dfb53344a6dbfb0e314f
Uros Bizjak <uros@gcc.gnu.org>
2011-06-22 13:20:07 +0200

    enable-execute-stack-empty.c (__enable_execute_stack): Add prototype.
    	* enable-execute-stack-empty.c (__enable_execute_stack): Add prototype.
    
    From-SVN: r175292

--- libgcc/enable-execute-stack-empty.c
+++ libgcc/enable-execute-stack-empty.c
@@ -1,6 +1,9 @@
 /* Dummy implementation of __enable_execute_stack.  */
 
+extern void __enable_execute_stack (void *);
+
 /* Attempt to turn on execute permission for the stack.  */
+
 void
 __enable_execute_stack (void *addr __attribute__((__unused__)))
 {
