607ce62330b6f831b672ed41e0d108dbf04a377d
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-06-23 18:52:58 +0000

    * tree-vect-data-refs.c (vect_compute_data_ref_alignment): Use 	get_pointer_alignment to see if base isn't sufficiently aligned.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@175348 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-vect-data-refs.c
+++ gcc/tree-vect-data-refs.c
@@ -859,7 +859,9 @@ vect_compute_data_ref_alignment (struct data_reference *dr)
       || (TREE_CODE (base_addr) == SSA_NAME
 	  && tree_int_cst_compare (ssize_int (TYPE_ALIGN_UNIT (TREE_TYPE (
 						      TREE_TYPE (base_addr)))),
-				   alignment) >= 0))
+				   alignment) >= 0)
+      || (get_pointer_alignment (base_addr, TYPE_ALIGN (vectype))
+	  >= TYPE_ALIGN (vectype)))
     base_aligned = true;
   else
     base_aligned = false;
