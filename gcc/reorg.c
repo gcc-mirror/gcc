629c3a3359e542258a3814a984bd0f505c648cf7
Eric Botcazou <ebotcazou@adacore.com>
2011-06-27 13:00:16 +0000

    reorg.c (fill_simple_delay_slots): Use stop_search_p to stop the forward scan as well.
    	* reorg.c (fill_simple_delay_slots): Use stop_search_p to stop the
    	forward scan as well.
    
    From-SVN: r175525

--- gcc/reorg.c
+++ gcc/reorg.c
@@ -2152,7 +2152,7 @@ fill_simple_delay_slots (int non_jumps_p)
 	      /* This must be an INSN or CALL_INSN.  */
 	      pat = PATTERN (trial);
 
-	      /* USE and CLOBBER at this level was just for flow; ignore it.  */
+	      /* Stand-alone USE and CLOBBER are just for flow.  */
 	      if (GET_CODE (pat) == USE || GET_CODE (pat) == CLOBBER)
 		continue;
 
@@ -2271,15 +2271,12 @@ fill_simple_delay_slots (int non_jumps_p)
 	    }
 
 	  if (target == 0)
-	    for (trial = next_nonnote_insn (insn); trial; trial = next_trial)
+	    for (trial = next_nonnote_insn (insn); !stop_search_p (trial, 1);
+		 trial = next_trial)
 	      {
 		next_trial = next_nonnote_insn (trial);
 
-		if (LABEL_P (trial)
-		    || BARRIER_P (trial))
-		  break;
-
-		/* We must have an INSN, JUMP_INSN, or CALL_INSN.  */
+		/* This must be an INSN or CALL_INSN.  */
 		pat = PATTERN (trial);
 
 		/* Stand-alone USE and CLOBBER are just for flow.  */
@@ -2293,7 +2290,7 @@ fill_simple_delay_slots (int non_jumps_p)
 		else
 		  trial_delay = trial;
 
-		/* Stop our search when seeing an unconditional jump.  */
+		/* Stop our search when seeing a jump.  */
 		if (JUMP_P (trial_delay))
 		  break;
 
