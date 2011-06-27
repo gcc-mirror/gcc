ada07b0df6bde3c730749bfc7b456e5760b14efa
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-06-27 16:45:49 +0000

    PR debug/49544 	* cselib.c (promote_debug_loc): If cselib_preserve_constants 	and l has two DEBUG_INSN owned locs instead of just one, adjust 	the second location's setting_insn too.
    	* gcc.dg/pr49544.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@175540 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr49544.c
@@ -0,0 +1,19 @@
+/* PR debug/49544 */
+/* { dg-do compile } */
+/* { dg-options "-g -O2" } */
+
+int baz (int, int, void *);
+
+static inline __attribute__ ((always_inline)) long
+foo (int x, int y, void *z)
+{
+  if (y < 0)
+    return baz (x, y, z);
+  return 0;
+}
+
+long
+bar (long x, long y, long z)
+{
+  return foo (x, y, (void *) z);
+}
