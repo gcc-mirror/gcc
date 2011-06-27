2f1dfc760986d93e193c5e9f24842ffd44b1ae39
Jason Merrill <jason@redhat.com>
2011-06-27 16:16:02 -0400

    re PR c++/49440 (Invalid dynamic_cast for unnamed namespace)
    	PR c++/49440
    	* class.c (set_linkage_according_to_type): Hand off to
    	determine_visibility.
    
    From-SVN: r175558

--- gcc/cp/class.c
+++ gcc/cp/class.c
@@ -677,21 +677,10 @@ get_vtable_name (tree type)
    the abstract.  */
 
 void
-set_linkage_according_to_type (tree type, tree decl)
+set_linkage_according_to_type (tree type ATTRIBUTE_UNUSED, tree decl)
 {
-  /* If TYPE involves a local class in a function with internal
-     linkage, then DECL should have internal linkage too.  Other local
-     classes have no linkage -- but if their containing functions
-     have external linkage, it makes sense for DECL to have external
-     linkage too.  That will allow template definitions to be merged,
-     for example.  */
-  if (no_linkage_check (type, /*relaxed_p=*/true))
-    {
-      TREE_PUBLIC (decl) = 0;
-      DECL_INTERFACE_KNOWN (decl) = 1;
-    }
-  else
-    TREE_PUBLIC (decl) = 1;
+  TREE_PUBLIC (decl) = 1;
+  determine_visibility (decl);
 }
 
 /* Create a VAR_DECL for a primary or secondary vtable for CLASS_TYPE.
