6dd61d84943131714866b22a82362d92316d5e40
Janis Johnson <janisjo@codesourcery.com>
2011-06-28 01:37:28 +0000

    pr45701-1.c: Don't specify -march; remove dg-prune-output.
    	* gcc.target/arm/pr45701-1.c: Don't specify -march; remove
    	dg-prune-output.
    	* gcc.target/arm/pr45701-2.c: Ditto.
    
    From-SVN: r175573

--- gcc/testsuite/gcc.target/arm/pr45701-1.c
+++ gcc/testsuite/gcc.target/arm/pr45701-1.c
@@ -1,7 +1,6 @@
 /* { dg-do compile } */
 /* { dg-skip-if "" { ! { arm_thumb1_ok || arm_thumb2_ok } } } */
-/* { dg-options "-march=armv7-a -mthumb -Os" }  */
-/* { dg-prune-output "switch .* conflicts with" } */
+/* { dg-options "-mthumb -Os" }  */
 /* { dg-final { scan-assembler "push\t\{r3" } } */
 /* { dg-final { scan-assembler-not "r8" } } */
 
