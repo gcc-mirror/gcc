f0e01a929585cc00887a299d32f3b63e59c7ed7e
Eric Botcazou <ebotcazou@gcc.gnu.org>
2011-06-30 19:35:45 +0000

    re PR tree-optimization/49572 (gcc.dg/tree-ssa/20030709-2.c scan-tree-dump-times cddce2 ".rtmem" 0)
    	PR tree-optimization/49572
    	* tree-ssa-dom.c (initialize_hash_element) <GIMPLE_SINGLE_RHS>: Use the
    	type of the RHS instead of that of the LHS for the expression type.
    
    From-SVN: r175722

--- gcc/tree-ssa-dom.c
+++ gcc/tree-ssa-dom.c
@@ -208,13 +208,11 @@ initialize_hash_element (gimple stmt, tree lhs,
     {
       enum tree_code subcode = gimple_assign_rhs_code (stmt);
 
-      expr->type = NULL_TREE;
-
       switch (get_gimple_rhs_class (subcode))
         {
         case GIMPLE_SINGLE_RHS:
 	  expr->kind = EXPR_SINGLE;
-	  expr->type = TREE_TYPE (gimple_assign_lhs (stmt));
+	  expr->type = TREE_TYPE (gimple_assign_rhs1 (stmt));
 	  expr->ops.single.rhs = gimple_assign_rhs1 (stmt);
 	  break;
         case GIMPLE_UNARY_RHS:
