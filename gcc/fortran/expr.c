49860194e29e3cb2d571e9f9a413b1858967953e
Janus Weil <janus@gcc.gnu.org>
2011-07-02 13:08:41 +0200

    re PR fortran/49562 ([OOP] assigning value to type-bound function)
    2011-07-02  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/49562
    	* expr.c (gfc_check_vardef_context): Handle type-bound procedures.
    
    
    2011-07-02  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/49562
    	* gfortran.dg/typebound_proc_23.f90: New.
    
    From-SVN: r175779

--- gcc/fortran/expr.c
+++ gcc/fortran/expr.c
@@ -4394,8 +4394,8 @@ gfc_check_vardef_context (gfc_expr* e, bool pointer, bool alloc_obj,
       sym = e->value.function.esym ? e->value.function.esym : e->symtree->n.sym;
     }
 
-  if (!pointer && e->expr_type == EXPR_FUNCTION
-      && sym->result->attr.pointer)
+  attr = gfc_expr_attr (e);
+  if (!pointer && e->expr_type == EXPR_FUNCTION && attr.pointer)
     {
       if (!(gfc_option.allow_std & GFC_STD_F2008))
 	{
@@ -4432,7 +4432,6 @@ gfc_check_vardef_context (gfc_expr* e, bool pointer, bool alloc_obj,
 
   /* Find out whether the expr is a pointer; this also means following
      component references to the last one.  */
-  attr = gfc_expr_attr (e);
   is_pointer = (attr.pointer || attr.proc_pointer);
   if (pointer && !is_pointer)
     {
