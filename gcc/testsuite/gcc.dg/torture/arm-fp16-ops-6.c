88515b8e5c61d22ae0340cc7517e7dae62c98764
Janis Johnson <janisjo@codesourcery.com>
2011-07-05 21:53:06 +0000

    target-supports.exp (add_options_for_arm_fp16): Renamed from add_options_for_arm_neon_fp16.
    	* lib/target-supports.exp (add_options_for_arm_fp16): Renamed
    	from add_options_for_arm_neon_fp16.
    	(check_effective_target_arm_fp16_ok_nocache): Renamed from
    	check_effective_target_arm_neon_fp16_ok_nocache.
    	Check -mfpu and -mfloat-abi options from current multilib.
    	Do not require neon support.
    	(check_effective_target_arm_fp16_ok): Renamed from
    	check_effecitve_target_arm_neon_fp16_ok.
    	* g++.dg/ext/arm-fp16/arm-fp16-ops-5.C: Use new names for
    	arm_neon_fp16_ok and arm_fp16.
    	* g++.dg/ext/arm-fp16/arm-fp16-ops-6.C: Likewise.
    	* gcc.dg/torture/arm-fp16-ops-5.c: Likewise.
    	* gcc.dg/torture/arm-fp16-ops-6.c: Likewise.
    	* gcc.target/arm/fp16-compile-vcvt.c: Likewise.
    
    From-SVN: r175890

--- gcc/testsuite/gcc.dg/torture/arm-fp16-ops-6.c
+++ gcc/testsuite/gcc.dg/torture/arm-fp16-ops-6.c
@@ -1,8 +1,8 @@
 /* Test various operators on __fp16 and mixed __fp16/float operands.  */
 /* { dg-do compile { target arm*-*-* } } */
-/* { dg-require-effective-target arm_neon_fp16_ok } */
+/* { dg-require-effective-target arm_fp16_ok } */
 /* { dg-options "-mfp16-format=ieee -ffast-math" } */
-/* { dg-add-options arm_neon_fp16 } */
+/* { dg-add-options arm_fp16 } */
 
 #include "arm-fp16-ops.h"
 
