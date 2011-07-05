478f1d377b79662e53331cba591b8ebfeabf1eb1
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-07-05 16:36:59 +0000

    * g++.dg/rtti/anon-ns1.C: Allow for null-termination.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@175867 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/rtti/anon-ns1.C
+++ gcc/testsuite/g++.dg/rtti/anon-ns1.C
@@ -2,7 +2,7 @@
 // The typeinfo name for A should start with * so we compare
 // it by address rather than contents.
 
-// { dg-final { scan-assembler "\"\*N\[^\"\]+1AE\"" } }
+// { dg-final { scan-assembler "\"\*N\[^\"\]+1AE" } }
 
 namespace
 {
