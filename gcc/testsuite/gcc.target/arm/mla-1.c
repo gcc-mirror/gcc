7122998a4a736deabb561a78a008a8101f5e7c24
Janis Johnson <janisjo@codesourcery.com>
2011-07-07 15:07:57 +0000

    target-supports.exp (check_effective_target_arm_thumb1): New.
    	* lib/target-supports.exp (check_effective_target_arm_thumb1): New.
    	(check_effective_target_arm_thumb2): Clarify comment, add valid code.
    	*  gcc.target/arm/mla-1.c: Skip for arm_thumb1, don't specify -march,
    	add tab to scan target.
    
    From-SVN: r175982

--- gcc/testsuite/gcc.target/arm/mla-1.c
+++ gcc/testsuite/gcc.target/arm/mla-1.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -march=armv5te" } */
-/* { dg-prune-output "switch .* conflicts with" } */
+/* { dg-skip-if "" { arm_thumb1 } { "*" } { "" } } */
+/* { dg-options "-O2" } */
 
 
 int
@@ -19,4 +19,4 @@ foo (int *p, int *q)
   return accum;
 }
 
-/* { dg-final { scan-assembler "mla" } } */
+/* { dg-final { scan-assembler "mla\\t" } } */
