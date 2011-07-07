54a679614b849c3ff67b5b6f6abc1418478e8ccb
Eric Botcazou <ebotcazou@gcc.gnu.org>
2011-07-07 20:41:10 +0000

    re PR target/49660 (64-bit gcc doesn't enable -mv8plus with -m32 on Solaris/SPARC)
    	PR target/49660
    	* config/sparc/sol2.h [TARGET_64BIT_DEFAULT] (TARGET_DEFAULT): Add
    	MASK_V8PLUS, remove commented out flag and reorder.
    
    	Backport from mainline
    	2011-06-28  Rainer Orth  <ro@CeBiTec.Uni-Bielefeld.DE>
    
    	* config/sparc/sol2-64.h (TARGET_DEFAULT): Remove.
    	(TARGET_64BIT_DEFAULT): Define.
    	* config.gcc (sparc*-*-solaris2*): Move sparc/sol2-64.h to front
    	of tm_file.
    	* config/sparc/sol2.h [TARGET_64BIT_DEFAULT] (TARGET_DEFAULT): Define.
    
    From-SVN: r176009

--- gcc/config/sparc/sol2.h
+++ gcc/config/sparc/sol2.h
@@ -153,11 +153,18 @@ along with GCC; see the file COPYING3.  If not see
 #undef SUN_INTEGER_MULTIPLY_64
 #define SUN_INTEGER_MULTIPLY_64 1
 
-/* Solaris allows 64 bit out and global registers in 32 bit mode.
-   sparc_override_options will disable V8+ if not generating V9 code.  */
+/* Solaris allows 64-bit out and global registers to be used in 32-bit mode.
+   sparc_override_options will disable V8+ if either not generating V9 code
+   or generating 64-bit code.  */
 #undef TARGET_DEFAULT
-#define TARGET_DEFAULT (MASK_V8PLUS + MASK_APP_REGS + MASK_FPU \
-			+ MASK_LONG_DOUBLE_128)
+#ifdef TARGET_64BIT_DEFAULT
+#define TARGET_DEFAULT \
+  (MASK_V9 + MASK_64BIT + MASK_PTR64 + MASK_STACK_BIAS + \
+   MASK_V8PLUS + MASK_APP_REGS + MASK_FPU + MASK_LONG_DOUBLE_128)
+#else
+#define TARGET_DEFAULT \
+  (MASK_V8PLUS + MASK_APP_REGS + MASK_FPU + MASK_LONG_DOUBLE_128)
+#endif
 
 /* Solaris-specific #pragmas are implemented on top of attributes.  Hook in
    the bits from config/sol2.c.  */
