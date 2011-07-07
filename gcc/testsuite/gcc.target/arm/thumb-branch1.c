e49e84133e34978718575b7ae2e6c976588c217c
janis <janis@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-07-07 17:50:13 +0000

    * gcc.target/arm/pr40657-2.c: Remove -march option and unneeded 	dg-prune-output. 	* gcc.target/arm/pr42235.c: Likewise. 	* gcc.target/arm/pr42495.c: Likewise. 	* gcc.target/arm/pr42574.c: Likewise. 	* gcc.target/arm/thumb-branch1.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@175997 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/thumb-branch1.c
+++ gcc/testsuite/gcc.target/arm/thumb-branch1.c
@@ -1,7 +1,6 @@
 /* { dg-do compile } */
 /* { dg-skip-if "" { ! { arm_thumb1_ok || arm_thumb2_ok } } } */
-/* { dg-options "-Os -mthumb -march=armv5te" } */
-/* { dg-prune-output "switch .* conflicts with" } */
+/* { dg-options "-Os -mthumb" } */
 
 int returnbool(int a, int b)
 {
