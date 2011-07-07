427c2230c00b4bc8b9f26ae229d2aee6fe822529
Eric Botcazou <ebotcazou@gcc.gnu.org>
2011-07-07 20:43:43 +0000

    re PR target/49660 (64-bit gcc doesn't enable -mv8plus with -m32 on Solaris/SPARC)
    	PR target/49660
    	* config/sparc/sol2.h [TARGET_64BIT_DEFAULT] (TARGET_DEFAULT): Add
    	MASK_V8PLUS, remove commented out flag and reorder.
    
    	Backport from mainline
    	2011-06-28  Rainer Orth  <ro@CeBiTec.Uni-Bielefeld.DE>
    
    	* config/sparc/sol2-64.h (TARGET_DEFAULT): Remove.
    	(TARGET_64BIT_DEFAULT): Define.
    	* config.gcc (sparc*-*-solaris2*): Move sparc/sol2-64.h to front
    	of tm_file.
    	* config/sparc/sol2.h [TARGET_64BIT_DEFAULT] (TARGET_DEFAULT): Define.
    
    From-SVN: r176010

--- gcc/config/sparc/sol2-64.h
+++ gcc/config/sparc/sol2-64.h
@@ -1,7 +1,7 @@
 /* Definitions of target machine for GCC, for bi-arch SPARC
    running Solaris 2, defaulting to 64-bit code generation.
 
-   Copyright (C) 1999, 2010 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2010, 2011 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
@@ -19,7 +19,4 @@ You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
 
-#undef TARGET_DEFAULT
-#define TARGET_DEFAULT \
-  (MASK_V9 + MASK_PTR64 + MASK_64BIT /* + MASK_HARD_QUAD */ + \
-   MASK_STACK_BIAS + MASK_APP_REGS + MASK_FPU + MASK_LONG_DOUBLE_128)
+#define TARGET_64BIT_DEFAULT 1
