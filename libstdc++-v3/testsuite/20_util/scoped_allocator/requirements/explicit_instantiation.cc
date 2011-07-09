677aad9c92d346e6cfea943445c2d04a4a4c8cea
Jonathan Wakely <jwakely.gcc@gmail.com>
2011-07-09 13:45:56 +0000

    Makefile.am: Add new header.
    2011-07-09  Jonathan Wakely  <jwakely.gcc@gmail.com>
    
    	* include/Makefile.am: Add new header.
    	* include/Makefile.in: Regenerate.
    	* include/std/scoped_allocator: New.
    	* doc/xml/manual/status_cxx200x.xml: Update.
    	* testsuite/20_util/scoped_allocator/1.cc: New.
    	* testsuite/20_util/scoped_allocator/propagation.cc: New.
    	* testsuite/20_util/scoped_allocator/requirements/typedefs.cc: New.
    	* testsuite/20_util/scoped_allocator/requirements/
    	explicit_instantiation.cc: New.
    
    From-SVN: r176079

---
+++ libstdc++-v3/testsuite/20_util/scoped_allocator/requirements/explicit_instantiation.cc
@@ -0,0 +1,47 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-do compile }
+
+// Copyright (C) 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// NB: This file is for testing memory with NO OTHER INCLUDES.
+
+#include <scoped_allocator>
+#include <memory>
+
+typedef short test_type;
+
+template<typename T>
+  struct minimal_allocator
+  {
+    typedef T value_type;
+    minimal_allocator();
+    template <typename U>
+      minimal_allocator(const minimal_allocator<U>&);
+    T* allocate(unsigned long);
+    void deallocate(T*, unsigned long);
+  };
+
+namespace std
+{
+  template struct scoped_allocator_adaptor<std::allocator<test_type>>;
+
+  template struct scoped_allocator_adaptor<minimal_allocator<test_type>>;
+
+  template struct scoped_allocator_adaptor<std::allocator<test_type>,
+                                           minimal_allocator<test_type>>;
+}
