17b0f68c782ac7863ecb0a75aae8846c14030e22
Mikael Morin <mikael.morin@sfr.fr>
2011-07-18 22:35:18 +0200

    re PR fortran/49648 (ICE(segfault) with MATMUL and function-result actual argument)
    2011-07-18  Mikael Morin  <mikael.morin@sfr.fr>
    
    	PR fortran/49648
    	* gfortran.dg/result_in_spec_4.f90: New test.
    
    From-SVN: r176422

---
+++ gcc/testsuite/gfortran.dg/result_in_spec_4.f90
@@ -0,0 +1,27 @@
+! { dg-do compile }
+!
+! PR fortran/49648
+! ICE for calls to a use-associated function returning an array whose spec
+! depends on a function call.
+
+! Contributed by Tobias Burnus <burnus@net-b.de>
+
+module m2
+  COMPLEX, SAVE, ALLOCATABLE :: P(:)
+contains
+  FUNCTION getPhaseMatrix() RESULT(PM)
+    COMPLEX:: PM(SIZE(P),3)
+    PM=0.0
+  END FUNCTION
+end module m2
+
+module m
+  use m2
+contains
+   SUBROUTINE gf_generateEmbPot()
+      COMPLEX :: sigma2(3,3)
+      sigma2 = MATMUL(getPhaseMatrix(), sigma2)
+   END SUBROUTINE
+end module m
+
+! { dg-final { cleanup-modules "m m2" } }
