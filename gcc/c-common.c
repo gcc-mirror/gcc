26d5e2ee02934a35c8a145942246c33e78ed07b0
Jakub Jelinek <jakub@redhat.com>
2011-07-19 21:31:01 +0200

    backport: re PR c++/49165 (ICE on for-loop/throw combination)
    	Backport from mainline
    	2011-05-27  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/49165
    	* c-common.c (c_common_truthvalue_conversion) <case COND_EXPR>: For
    	C++ don't call c_common_truthvalue_conversion on void type arms.
    
    	* g++.dg/eh/cond6.C: New test.
    
    From-SVN: r176484

--- gcc/c-common.c
+++ gcc/c-common.c
@@ -3430,13 +3430,18 @@ c_common_truthvalue_conversion (location_t location, tree expr)
 					       TREE_OPERAND (expr, 0));
 
     case COND_EXPR:
-      /* Distribute the conversion into the arms of a COND_EXPR.  */
-      return fold_build3 (COND_EXPR, truthvalue_type_node,
-		TREE_OPERAND (expr, 0),
-		c_common_truthvalue_conversion (location,
-						TREE_OPERAND (expr, 1)),
-		c_common_truthvalue_conversion (location,
-						TREE_OPERAND (expr, 2)));
+      {
+	tree op1 = TREE_OPERAND (expr, 1);
+	tree op2 = TREE_OPERAND (expr, 2);
+        /* In C++ one of the arms might have void type if it is throw.  */
+	if (!VOID_TYPE_P (TREE_TYPE (op1)))
+	  op1 = c_common_truthvalue_conversion (location, op1);
+	if (!VOID_TYPE_P (TREE_TYPE (op2)))
+	  op2 = c_common_truthvalue_conversion (location, op2);
+	/* Distribute the conversion into the arms of a COND_EXPR.  */
+	return fold_build3 (COND_EXPR, truthvalue_type_node,
+			    TREE_OPERAND (expr, 0), op1, op2);
+      }
 
     CASE_CONVERT:
       /* Don't cancel the effect of a CONVERT_EXPR from a REFERENCE_TYPE,
