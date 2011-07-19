efb858488aaf9cb893327842659e0e0d28b71137
Jakub Jelinek <jakub@redhat.com>
2011-07-19 14:54:34 +0200

    backport: re PR c++/49165 (ICE on for-loop/throw combination)
    	Backport from mainline
    	2011-05-26  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/49165
    	* gimplify.c (shortcut_cond_r): Don't special case
    	COND_EXPRs if they have void type on one of their arms.
    
    	* g++.dg/eh/cond5.C: New test.
    
    From-SVN: r176452

---
+++ gcc/testsuite/g++.dg/eh/cond5.C
@@ -0,0 +1,43 @@
+// PR c++/49165
+// { dg-do run }
+
+extern "C" void abort ();
+
+int
+foo (bool x, int y)
+{
+  if (y < 10 && (x ? true : throw 1))
+    y++;
+  if (y > 20 || (x ? true : throw 2))
+    y++;
+  return y;
+}
+
+int
+main ()
+{
+  if (foo (true, 0) != 2
+      || foo (true, 10) != 11
+      || foo (false, 30) != 31)
+    abort ();
+  try
+    {
+      foo (false, 0);
+      abort ();
+    }
+  catch (int i)
+    {
+      if (i != 1)
+	abort ();
+    }
+  try
+    {
+      foo (false, 10);
+      abort ();
+    }
+  catch (int i)
+    {
+      if (i != 2)
+	abort ();
+    }
+}
