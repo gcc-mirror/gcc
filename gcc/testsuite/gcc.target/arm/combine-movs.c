d05de055898079433d229a98b4259d584dfec6d1
cltang <cltang@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-07-20 06:21:36 +0000

    2011-07-20  Chung-Lin Tang  <cltang@codesourcery.com>
    	* config/arm/arm.c (arm_canonicalize_comparison): Add case to
    	canonicalize left operand from ZERO_EXTEND to AND.
    
    	testsuite/
    	* gcc.target/arm/combine-movs.c: New.
    	* gcc.target/arm/unsigned-extend-2.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@176495 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/combine-movs.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-skip-if "" { arm_thumb1 } } */
+/* { dg-options "-O" }  */
+
+void foo (unsigned long r[], unsigned int d)
+{
+  int i, n = d / 32;
+  for (i = 0; i < n; ++i)
+    r[i] = 0;
+}
+
+/* { dg-final { scan-assembler "movs\tr\[0-9\]" } } */
