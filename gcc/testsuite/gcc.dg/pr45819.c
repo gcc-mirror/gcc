27315aa68fbb97cc8cbb71da2ca54b9e8052b057
Richard Guenther <rguenther@suse.de>
2011-07-22 11:55:30 +0000

    re PR middle-end/45819 (unexpected unaligned access to volatile int)
    2011-07-22  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/45819
    	* tree-ssa-forwprop.c (forward_propagate_addr_expr_1): Properly
    	preserve volatile and notrap flags.
    
    	* gcc.dg/pr45819.c: New testcase.
    
    From-SVN: r176623

---
+++ gcc/testsuite/gcc.dg/pr45819.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+struct ehci_regs {
+    char x;
+    unsigned int port_status[0];
+} __attribute__ ((packed));
+
+struct ehci_hcd {
+    struct ehci_regs *regs;
+};
+
+int ehci_hub_control (struct ehci_hcd *ehci, int wIndex)
+{
+  unsigned int *status_reg = &ehci->regs->port_status[wIndex];
+  return *(volatile unsigned int *)status_reg;
+}
+
+/* { dg-final { scan-tree-dump "={v}" "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
