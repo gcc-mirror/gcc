52832da6c32a571262cdbfdbfdeb235b8578e41a
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2011-07-22 16:02:25 +0000

    re PR debug/47393 (g++.dg/debug/dwarf2/icf.C FAILs on IRIX)
    	PR debug/47393
    	* g++.dg/debug/dwarf2/icf.C: XFAIL some scan-assembler on
    	mips-sgi-irix*.
    
    From-SVN: r176633

--- gcc/testsuite/g++.dg/debug/dwarf2/icf.C
+++ gcc/testsuite/g++.dg/debug/dwarf2/icf.C
@@ -40,11 +40,11 @@ test2(A* a)
 
 // Verify that we get .debug_dcall and .debug_vcall tables generated
 // and that we see entries for both virtual calls. 
-// { dg-final { scan-assembler "\\.section.*\.debug_dcall" } }
+// { dg-final { scan-assembler "\\.section.*\.debug_dcall" { xfail mips-sgi-irix* } } }
 // { dg-final { scan-assembler "\\.section.*\.debug_vcall" } }
-// { dg-final { scan-assembler "New caller" } }
-// { dg-final { scan-assembler "Caller DIE offset" } }
+// { dg-final { scan-assembler "New caller" { xfail mips-sgi-irix* } } }
+// { dg-final { scan-assembler "Caller DIE offset" { xfail mips-sgi-irix* } } }
 // { dg-final { scan-assembler "Point of call" } }
-// { dg-final { scan-assembler "Callee DIE offset" } }
+// { dg-final { scan-assembler "Callee DIE offset" { xfail mips-sgi-irix* } } }
 // { dg-final { scan-assembler "0\[ \t\]+.*Vtable slot" } }
 // { dg-final { scan-assembler "0x1\[ \t\]+.*Vtable slot" } }
