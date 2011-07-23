f8795576746c817acdc9e77ee13184f17744d05b
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-07-23 03:17:11 +0000

    2011-07-22  Benjamin Kosnik  <bkoz@redhat.com> 	    Daniel Krugler  <daniel.kruegler@googlemail.com>
    	* include/bits/move.h (move, forward): Mark constexpr.
    	* include/bits/stl_pair.h (pair): Mark move ctors constexpr.
    	* testsuite/20_util/pair/make_pair/constexpr.cc: New.
    	* testsuite/20_util/pair/cons/constexpr.cc: Add tests.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@176672 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/move.h
+++ libstdc++-v3/include/bits/move.h
@@ -58,12 +58,12 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
   
   /// forward (as per N3143)
   template<typename _Tp>
-    inline _Tp&&
+    inline constexpr _Tp&&
     forward(typename std::remove_reference<_Tp>::type& __t) noexcept
     { return static_cast<_Tp&&>(__t); }
 
   template<typename _Tp>
-    inline _Tp&&
+    inline constexpr _Tp&&
     forward(typename std::remove_reference<_Tp>::type&& __t) noexcept
     {
       static_assert(!std::is_lvalue_reference<_Tp>::value, "template argument"
@@ -78,7 +78,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
    *  @return Same, moved.
   */
   template<typename _Tp>
-    inline typename std::remove_reference<_Tp>::type&&
+    inline constexpr typename std::remove_reference<_Tp>::type&&
     move(_Tp&& __t) noexcept
     { return static_cast<typename std::remove_reference<_Tp>::type&&>(__t); }
 
---
+++ libstdc++-v3/testsuite/20_util/pair/make_pair/constexpr.cc
@@ -0,0 +1,42 @@
+// { dg-do compile }
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+
+// NOTE: This makes use of the fact that we know how moveable
+// is implemented on pair, and also vector. If the implementation 
+// changes this test may begin to fail.
+
+#include <memory>
+#include <testsuite_hooks.h>
+
+void
+test1()
+{
+  bool test __attribute__((unused)) = true;
+  typedef std::pair<int, float> pair_type;
+  constexpr pair_type p1 = std::make_pair(22, 22.222);
+}
+
+int 
+main() 
+{
+  test1();
+  return 0;
+}
