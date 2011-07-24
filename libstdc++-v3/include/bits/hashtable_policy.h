5d25efc0fd72fef10dbc606561fade95f2c99a88
fdumont <fdumont@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-07-24 19:20:26 +0000

    2011-07-24  François Dumont  <francois.cppdevs@free.fr>
            * include/bits/hashtable_policy.h (_Prime_rehash_policy): Use
            __builtin_floor rather than __builtin_ceil to compute next resize
            value.
            * testsuite/23_containers/unordered_set/hash_policy/load_factor.cc:
            New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@176717 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/hashtable_policy.h
+++ libstdc++-v3/include/bits/hashtable_policy.h
@@ -427,10 +427,10 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
   _Prime_rehash_policy::
   _M_next_bkt(std::size_t __n) const
   {
-    const unsigned long* __p = std::lower_bound(__prime_list, __prime_list
+    const unsigned long __p = *std::lower_bound(__prime_list, __prime_list
 						+ _S_n_primes, __n);
     _M_next_resize =
-      static_cast<std::size_t>(__builtin_ceil(*__p * _M_max_load_factor));
+      static_cast<std::size_t>(__builtin_floor(__p * _M_max_load_factor));
     return *__p;
   }
 
@@ -441,10 +441,10 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
   _M_bkt_for_elements(std::size_t __n) const
   {
     const float __min_bkts = __n / _M_max_load_factor;
-    const unsigned long* __p = std::lower_bound(__prime_list, __prime_list
+    const unsigned long __p = *std::lower_bound(__prime_list, __prime_list
 						+ _S_n_primes, __min_bkts);
     _M_next_resize =
-      static_cast<std::size_t>(__builtin_ceil(*__p * _M_max_load_factor));
+      static_cast<std::size_t>(__builtin_floor(__p * _M_max_load_factor));
     return *__p;
   }
 
@@ -469,17 +469,17 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	if (__min_bkts > __n_bkt)
 	  {
 	    __min_bkts = std::max(__min_bkts, _M_growth_factor * __n_bkt);
-	    const unsigned long* __p =
-	      std::lower_bound(__prime_list, __prime_list + _S_n_primes,
-			       __min_bkts);
+	    const unsigned long __p =
+	      *std::lower_bound(__prime_list, __prime_list + _S_n_primes,
+				__min_bkts);
 	    _M_next_resize = static_cast<std::size_t>
-	      (__builtin_ceil(*__p * _M_max_load_factor));
+	      (__builtin_floor(__p * _M_max_load_factor));
 	    return std::make_pair(true, *__p);
 	  }
 	else
 	  {
 	    _M_next_resize = static_cast<std::size_t>
-	      (__builtin_ceil(__n_bkt * _M_max_load_factor));
+	      (__builtin_floor(__n_bkt * _M_max_load_factor));
 	    return std::make_pair(false, 0);
 	  }
       }
