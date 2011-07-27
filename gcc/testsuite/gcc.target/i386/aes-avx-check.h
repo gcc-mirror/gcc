c7447c8278c8ef10ab99630497871370df3bdd4c
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-07-27 15:39:58 +0000

    * gcc.target/i386/avx-os-support.h: New. 	* gcc.target/i386/avx-check.h: Include avx-os-support.h 	(main): Check avx_os_support before the test is run. 	* gcc.target/i386/aes-avx-check.h: Ditto. 	* gcc.target/i386/pclmul-avx-check.h: Ditto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@176829 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/aes-avx-check.h
+++ gcc/testsuite/gcc.target/i386/aes-avx-check.h
@@ -3,6 +3,7 @@
 #endif
 #include <stdlib.h>
 #include "cpuid.h"
+#include "avx-os-support.h"
 
 static void aes_avx_test (void);
 
@@ -22,8 +23,9 @@ main ()
     return 0;
 
   /* Run AES + AVX test only if host has AES + AVX support.  */
-  if ((ecx & (bit_AVX | bit_OSXSAVE | bit_AES))
-      == (bit_AVX | bit_OSXSAVE | bit_AES))
+  if (((ecx & (bit_AVX | bit_OSXSAVE | bit_AES))
+       == (bit_AVX | bit_OSXSAVE | bit_AES))
+      && avx_os_support ())
     {
       do_test ();
 #ifdef DEBUG
