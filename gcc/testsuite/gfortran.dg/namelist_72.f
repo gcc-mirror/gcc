e8a78f6123e8d64c7993a5d9cac5b829bc185a1d
Tobias Burnus <burnus@net-b.de>
2011-07-27 23:35:08 +0200

    backport: re PR libfortran/49791 (Formatted namelist reads fails with: Cannot match namelist object)
    2011-07-27  Tobias Burnus  <burnus@net-b.de>
    
            Backport from mainline
            2011-07-23  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/49791
            * io/list_read.c (nml_parse_qualifier): Remove check to
            enabled extended read for another case.
    
    2011-07-27  Tobias Burnus  <burnus@net-b.de>
    
            Backported from mainline
            2011-07-23  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/49791
            * gfortran.dg/namelist_72.f: New.
    
    From-SVN: r176850

---
+++ gcc/testsuite/gfortran.dg/namelist_72.f
@@ -0,0 +1,33 @@
+! { dg-do run }
+!
+! PR fortran/49791
+!
+! Contributed by Elliott Sales de Andrade
+!
+      program namelist_test
+
+      dimension xpos(5000), ypos(5000)
+      namelist /geometry/ xpos, ypos
+
+      xpos = -huge(xpos)
+      ypos = -huge(ypos)
+
+      open(unit=4,file='geometry.in')
+      write(4,'(a)') '$geometry'
+      write(4,'(a)') ' xpos(1)= 0.00, 0.10, 0.20, 0.30, 0.40,'
+      write(4,'(a)') ' ypos(1)= 0.50, 0.60, 0.70, 0.80, 0.90,'
+      write(4,'(a)') '$end'
+
+      close(4)
+
+      open (unit=4,file='geometry.in',status='old',form='formatted')
+      read (4,geometry)
+      close(4, status='delete')
+
+      !print *, 'xpos', xpos(1:10), 'ypos', ypos(1:10)
+
+      if (any (xpos(1:5) /= [0.00, 0.10, 0.20, 0.30, 0.40]))call abort()
+      if (any (ypos(1:5) /= [0.50, 0.60, 0.70, 0.80, 0.90]))call abort()
+      if (any (xpos(6:) /= -huge(xpos))) call abort ()
+      if (any (ypos(6:) /= -huge(ypos))) call abort ()
+      end
