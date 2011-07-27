5019a14eee123f629c24747178e6ab3c22a5dbc9
Uros Bizjak <ubizjak@gmail.com>
2011-07-27 17:39:58 +0200

    avx-os-support.h: New.
    	* gcc.target/i386/avx-os-support.h: New.
    	* gcc.target/i386/avx-check.h: Include avx-os-support.h
    	(main): Check avx_os_support before the test is run.
    	* gcc.target/i386/aes-avx-check.h: Ditto.
    	* gcc.target/i386/pclmul-avx-check.h: Ditto.
    
    From-SVN: r176829

--- gcc/testsuite/gcc.target/i386/pclmul-avx-check.h
+++ gcc/testsuite/gcc.target/i386/pclmul-avx-check.h
@@ -3,6 +3,7 @@
 #endif
 #include <stdlib.h>
 #include "cpuid.h"
+#include "avx-os-support.h"
 
 static void pclmul_avx_test (void);
 
@@ -22,8 +23,9 @@ main ()
     return 0;
 
   /* Run PCLMUL + AVX test only if host has PCLMUL + AVX support.  */
-  if ((ecx & (bit_AVX | bit_OSXSAVE | bit_PCLMUL))
-      == (bit_AVX | bit_OSXSAVE | bit_PCLMUL))
+  if (((ecx & (bit_AVX | bit_OSXSAVE | bit_PCLMUL))
+       == (bit_AVX | bit_OSXSAVE | bit_PCLMUL))
+      && avx_os_support ())
     {
       do_test ();
 #ifdef DEBUG
