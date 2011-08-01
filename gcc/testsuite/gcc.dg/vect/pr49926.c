67f2d54f56cf4e978cf8e2d78330da7948f99591
Ira Rosen <ira.rosen@linaro.org>
2011-08-01 16:58:20 +0000

    re PR tree-optimization/49926 (ice in process_use, at tree-vect-stmts.c:405)
            PR tree-optimization/49926
            * tree-vect-loop.c (vect_is_slp_reduction): Check that a
            statement in a chain doesn't have uses both inside and
            outside the loop.
    
    From-SVN: r177063

---
+++ gcc/testsuite/gcc.dg/vect/pr49926.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target vect_int } */
+
+int a, b, c[10];
+
+void
+foo (unsigned int x, int y, int z, int *w)
+{
+  do
+    {
+      *w = z;
+      y = x;
+      if (y)
+        for (b = -4; b; b++)
+          {
+            z = y &= a &= 1;
+            y &= c[b + 4];
+          }
+    }
+  while (1);
+}
+
+/* { dg-final { cleanup-tree-dump "vect" } } */
