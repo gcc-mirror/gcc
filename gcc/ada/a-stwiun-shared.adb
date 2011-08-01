69b6b231df3bd6836ed8734b1a52b975aa1eb7c8
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-08-01 09:22:08 +0000

    2011-08-01  Pascal Obry  <obry@adacore.com>
    	* a-stzunb-shared.adb, a-strunb-shared.adb, a-stwiun-shared.adb:
    	Fix Replace_Slice when High is above current string size.
    	(Replace_Slice): Fix DL computation when High is above current
    	string length.
    
    2011-08-01  Gary Dismukes  <dismukes@adacore.com>
    
    	* gnat_rm.texi: Add documentation for pragma Static_Elaboration_Desired.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@177004 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/a-stwiun-shared.adb
+++ gcc/ada/a-stwiun-shared.adb
@@ -1359,7 +1359,9 @@ package body Ada.Strings.Wide_Unbounded is
       --  Do replace operation when removed slice is not empty
 
       if High >= Low then
-         DL := By'Length + SR.Last + Low - High - 1;
+         DL := By'Length + SR.Last + Low - Integer'Min (High, SR.Last) - 1;
+         --  This is the number of characters remaining in the string after
+         --  replacing the slice.
 
          --  Result is empty string, reuse empty shared string
 
@@ -1406,7 +1408,9 @@ package body Ada.Strings.Wide_Unbounded is
       --  Do replace operation only when replaced slice is not empty
 
       if High >= Low then
-         DL := By'Length + SR.Last + Low - High - 1;
+         DL := By'Length + SR.Last + Low - Integer'Min (High, SR.Last) - 1;
+         --  This is the number of characters remaining in the string after
+         --  replacing the slice.
 
          --  Result is empty string, reuse empty shared string
 
