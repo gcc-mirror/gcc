c7c2e8316cc7cc99d8dfc9aa166e7af1b0b14516
Benjamin Kosnik <bkoz@redhat.com>
2011-08-04 02:26:52 +0000

    filebuf_sputn_unbuf.cc: Include unistd.h for unlink.
    2011-08-03  Benjamin Kosnik  <bkoz@redhat.com>
    
    	* testsuite/performance/27_io/filebuf_sputn_unbuf.cc: Include
    	unistd.h for unlink.
    	* testsuite/performance/27_io/ofstream_insert_int.cc: Same.
    	* testsuite/performance/27_io/ifstream_extract_int.cc: Same.
    	* testsuite/performance/27_io/ifstream_getline-2.cc: Same.
    	* testsuite/performance/27_io/fstream_seek_write.cc: Same.
    	* testsuite/performance/27_io/ifstream_extract_chars.cc: Same.
    	* testsuite/performance/27_io/ofstream_insert_float.cc: Same.
    	* testsuite/performance/27_io/ifstream_extract_float.cc: Same.
    	* testsuite/performance/27_io/filebuf_sputc.cc: Same.
    
    From-SVN: r177307

--- libstdc++-v3/testsuite/performance/27_io/fstream_seek_write.cc
+++ libstdc++-v3/testsuite/performance/27_io/fstream_seek_write.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2003, 2009 Free Software Foundation, Inc.
+// Copyright (C) 2003, 2009, 2011 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -15,7 +15,7 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-
+#include <unistd.h>
 #include <fstream>
 #include <testsuite_performance.h>
 
