7538b7118fb89cd003bcbdf7918449def721db86
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-08-05 22:09:56 +0000

    Add gcc.target/i386/pr47364-2.c and gcc.dg/torture/pr47364-1.c.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@177488 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr47364-2.c
@@ -0,0 +1,11 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+
+extern __SIZE_TYPE__ strlen (const char *);
+void foo (char *, const char *);
+int bar (const char *prefix)
+{
+    char buff[256];
+    foo (buff, prefix);
+    return strlen(buff);
+}
