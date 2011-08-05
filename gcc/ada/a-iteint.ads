844982aeed5addccc1630f4d407dc1e100c40fe2
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-08-05 14:59:54 +0000

    2011-08-05  Thomas Quinot  <quinot@adacore.com>
    	* g-expect.adb: Minor reformatting.
    
    2011-08-05  Bob Duff  <duff@adacore.com>
    
    	* a-fihema.adb: Comment out OS_Lib.
    
    2011-08-05  Matthew Heaney  <heaney@adacore.com>
    
    	* Makefile.rtl, impunit.adb: Added a-c[oi]mutr.ad[sb]
    	(unbounded multiway tree containers) and a-iteint.ads.
    	* a-comutr.ads, a-comutr.adb:
    	This is the new Ada 2012 unit for unbounded multiway tree containers
    	* a-cimutr.ads, a-cimutr.adb
    	This is the new Ada 2012 unit for indefinite multiway tree containers
    	* a-iteint.ads: New file.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@177449 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/ada/a-iteint.ads
@@ -0,0 +1,46 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                         GNAT LIBRARY COMPONENTS                          --
+--                                                                          --
+--               A D A . I T E R A T O R . I N T E R F A C E S              --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--          Copyright (C) 2004-2011, Free Software Foundation, Inc.         --
+--                                                                          --
+-- This specification is derived from the Ada Reference Manual for use with --
+-- GNAT. The copyright notice above, and the license provisions that follow --
+-- apply solely to the  contents of the part following the private keyword. --
+--                                                                          --
+-- GNAT is free software;  you can  redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.                                     --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+generic
+   type Cursor is private;
+   No_Element : Cursor;
+   pragma Unreferenced (No_Element);
+package Ada.Iterator_Interfaces is
+   type Forward_Iterator is limited interface;
+   function First (Object : Forward_Iterator) return Cursor is abstract;
+   function Next (Object : Forward_Iterator; Position : Cursor) return Cursor
+     is abstract;
+   type Reversible_Iterator is limited interface and Forward_Iterator;
+   function Last (Object : Reversible_Iterator) return Cursor is abstract;
+   function Previous (Object : Reversible_Iterator; Position : Cursor)
+     return Cursor is abstract;
+end Ada.Iterator_Interfaces;
