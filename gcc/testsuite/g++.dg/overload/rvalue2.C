f6878ed0b08f9e3031e0b3a697dcaa5cf24dd331
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-08-05 19:12:24 +0000

    PR c++/49812 	* typeck.c (cp_build_unary_op) [POSTINCREMENT_EXPR]: Strip cv-quals.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@177479 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/overload/rvalue2.C
@@ -0,0 +1,11 @@
+// PR c++/49812
+// The call should choose the second f because i++ is an int rvalue.
+
+template <class T> void f(const volatile T& t) { t.i; }
+template <class T> void f(const T&);
+
+int main()
+{
+  volatile int i = 0;
+  f(i++);
+}
