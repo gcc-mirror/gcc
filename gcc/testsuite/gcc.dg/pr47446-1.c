a3e682428096d59622193120047c3a6b7d373d2c
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-08-06 14:36:52 +0000

    Add testcases for PRs 47449/47446.
    2011-08-06  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR middle-end/47449
    	* gcc.target/i386/pr47449.c: New.
    
    	PR target/47446
    	* gcc.dg/pr47446-1.c: New.
    	* gcc.dg/pr47446-2.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@177512 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr47446-1.c
@@ -0,0 +1,40 @@
+/* { dg-do assemble } */
+/* { dg-options "-O2" } */
+
+extern void abort (void);
+enum
+{
+  __GCONV_OK = 0,
+  __GCONV_NOCONV,
+  __GCONV_NODB,
+  __GCONV_NOMEM,
+  __GCONV_EMPTY_INPUT,
+  __GCONV_FULL_OUTPUT,
+  __GCONV_ILLEGAL_INPUT,
+  __GCONV_INCOMPLETE_INPUT,
+  __GCONV_ILLEGAL_DESCRIPTOR,
+  __GCONV_INTERNAL_ERROR
+};
+int
+foo (int result)
+{
+  int irreversible = 0;
+  switch (result)
+    {
+    case __GCONV_ILLEGAL_INPUT:
+      irreversible = -1L;
+      break;
+    case __GCONV_FULL_OUTPUT:
+      irreversible = -2L;
+      break;
+    case __GCONV_INCOMPLETE_INPUT:
+      irreversible = -3L;
+      break;
+    case __GCONV_EMPTY_INPUT:
+    case __GCONV_OK:
+      break;
+    default:
+      abort ();
+    }
+  return irreversible;
+}
