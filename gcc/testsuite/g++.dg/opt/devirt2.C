9f0c0418d9233be0ef9e8d46177f15824f62508b
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-08-06 21:37:22 +0000

    PR testsuite/48727 	* g++.dg/opt/devirt2.C: Use specific pattern for alpha*-*-*.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@177537 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/opt/devirt2.C
+++ gcc/testsuite/g++.dg/opt/devirt2.C
@@ -1,10 +1,13 @@
 // { dg-do compile }
 // { dg-options "-O2" }
+// { dg-final { scan-assembler-times "xyzzy" 2 { target { ! { alpha*-*-* hppa*-*-* ia64*-*-hpux* sparc*-*-* } } } } }
 // The IA64 and HPPA compilers generate external declarations in addition
 // to the call so those scans need to be more specific.
-// { dg-final { scan-assembler-times "xyzzy" 2 { target { ! { hppa*-*-* ia64*-*-hpux* sparc*-*-* } } } } }
 // { dg-final { scan-assembler-times "br\[^\n\]*xyzzy" 2 { target ia64*-*-hpux* } } }
 // { dg-final { scan-assembler-times "xyzzy\[^\n\]*,%r" 2 { target hppa*-*-* } } }
+// If assembler supports explicit relocation directives, the alpha compiler generates
+// literal/lituse_jsr pairs, so the scans need to be more specific.
+// { dg-final { scan-assembler-times "jsr\[^\n\]*xyzzy" 2 { target alpha*-*-* } } }
 // Unless the assembler supports -relax, the 32-bit SPARC compiler generates
 // sethi/jmp instead of just call, so the scans need to be more specific.
 // With subexpressions, Tcl regexp -inline -all returns both the complete
