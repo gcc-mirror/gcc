b569be8ca62060f697f5a90a7933d880f334ace6
Jason Merrill <jason@redhat.com>
2011-08-06 00:34:45 -0400

    re PR c++/48993 ([C++0x] segmentation fault when compiling this program with constexpr)
    	PR c++/48993
    	* semantics.c (potential_constant_expression_1) [CALL_EXPR]: Sorry
    	on 'this' in a constructor.
    
    From-SVN: r177499

--- gcc/testsuite/g++.dg/cpp0x/constexpr-48089.C
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-48089.C
@@ -22,3 +22,29 @@ struct R {
 };
 
 constexpr R r;			// { dg-bogus "" "" { xfail *-*-* } }
+
+// Ill-formed (no diagnostic required)
+struct T {
+  int i;
+  constexpr int f() { return i; }
+  constexpr T(): i(0) { }
+  constexpr T(const T& t) : i(f()) { } // { dg-message "" }
+};
+
+constexpr T t1;
+// Ill-formed (diagnostic required)
+constexpr T t2(t1);		// { dg-error "" }
+
+// Well-formed
+struct U {
+  int i, j;
+  constexpr int f(int _i) { return _i; }
+  constexpr int g() { return i; }
+  constexpr U(): i(0), j(0) { }
+  constexpr U(const U& t) : i(f(t.i)),j(0) { } // { dg-bogus "" "" { xfail *-*-* } }
+  constexpr U(int _i) : i(_i),j(g()) { } // { dg-bogus "" "" { xfail *-*-* } }
+};
+
+constexpr U u1;
+constexpr U u2(u1);		// { dg-bogus "" "" { xfail *-*-* } }
+constexpr U u3(1);		// { dg-bogus "" "" { xfail *-*-* } }
