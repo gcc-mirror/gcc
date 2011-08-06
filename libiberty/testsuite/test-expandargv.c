b551ffd8d6033cbdaef400f4d85138561c0a6a58
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-08-06 17:38:04 +0000

    * testsuite/test-expandargv.c (writeout_test): Check result of fwrite.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@177529 138bc75d-0d04-0410-961f-82ee72b054a4

--- libiberty/testsuite/test-expandargv.c
+++ libiberty/testsuite/test-expandargv.c
@@ -189,7 +189,7 @@ writeout_test (int test, const char * test_data)
 {
   char filename[256];
   FILE *fd;
-  size_t len;
+  size_t len, sys_fwrite;
   char * parse;
 
   /* Unique filename per test */
@@ -208,7 +208,10 @@ writeout_test (int test, const char * test_data)
   /* Run all possible replaces */
   run_replaces (parse);
 
-  fwrite (parse, len, sizeof (char), fd);
+  sys_fwrite = fwrite (parse, sizeof (char), len, fd);
+  if (sys_fwrite != len)
+    fatal_error (__LINE__, "Failed to write to test file.", errno);
+
   free (parse);
   fclose (fd);
 }
