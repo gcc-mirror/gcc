76fd9e61bb17a03d63f8a9a4dfcb15f1d8b7a064
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-08-08 12:08:31 +0000

    gcc: 	* config/sparc/driver-sparc.c: New file. 	* config/sparc/x-sparc: New file. 	* config.host: Use driver-sparc.o, sparc/x-sparc on 	sparc*-*-solaris2*. 	* config/sparc/sparc.opt (native): New value for enum 	processor_type. 	* config/sparc/sparc-opts.h (PROCESSOR_NATIVE): Declare. 	* config/sparc/sparc.c (sparc_option_override): Abort if 	PROCESSOR_NATIVE gets here. 	* config/sparc/sol2.h [__sparc__] (host_detect_local_cpu): Declare. 	(EXTRA_SPEC_FUNCTIONS, MCPU_MTUNE_NATIVE_SPECS, 	DRIVER_SELF_SPECS): Define. 	* doc/invoke.texi (SPARC Options, -mcpu): Document native. 	(SPARC Options, -mtune): Likewise. 	* configure.ac (EXTRA_GCC_LIBS): Check for libkstat. 	Substitute result. 	* configure: Regenerate. 	* Makefile.in (EXTRA_GCC_LIBS): Set. 	(xgcc$(exeext)): Add $(EXTRA_GCC_LIBS). 	(cpp$(exeext)): Likewise.
    	gcc/cp:
    	* Make-lang.in (g++$(exeext)): Add $(EXTRA_GCC_LIBS).
    
    	gcc/fortran:
    	* Make-lang.in (gfortran$(exeext)): Add $(EXTRA_GCC_LIBS).
    
    	gcc/go:
    	* Make-lang.in (gccgo$(exeext)): Add $(EXTRA_GCC_LIBS).
    
    	gcc/java:
    	* Make-lang.in ($(XGCJ)$(exeext)): Add $(EXTRA_GCC_LIBS).
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@177559 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sparc/sol2.h
+++ gcc/config/sparc/sol2.h
@@ -157,6 +157,22 @@ along with GCC; see the file COPYING3.  If not see
 %{!m32:%{!m64:%(cpp_arch_default)}} \
 "
 
+/* -mcpu=native handling only makes sense with compiler running on
+   a SPARC chip.  */
+#if defined(__sparc__)
+extern const char *host_detect_local_cpu (int argc, const char **argv);
+# define EXTRA_SPEC_FUNCTIONS						\
+  { "local_cpu_detect", host_detect_local_cpu },
+
+# define MCPU_MTUNE_NATIVE_SPECS					\
+   " %{mcpu=native:%<mcpu=native %:local_cpu_detect(cpu)}"		\
+   " %{mtune=native:%<mtune=native %:local_cpu_detect(tune)}"
+#else
+# define MCPU_MTUNE_NATIVE_SPECS ""
+#endif
+
+#define DRIVER_SELF_SPECS MCPU_MTUNE_NATIVE_SPECS
+
 #undef	CC1_SPEC
 #if DEFAULT_ARCH32_P
 #define CC1_SPEC "\
