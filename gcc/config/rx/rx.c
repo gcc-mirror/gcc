7f87e0730cde32054f1f8955b2e259e232bda249
Nick Clifton <nickc@gcc.gnu.org>
2011-08-09 11:16:45 +0000

    rx.c (rx_expand_builtin): Fix typo checking the setpsw builtin.
    	* config/rx/rx.c (rx_expand_builtin): Fix typo checking the setpsw
    	builtin.
    
    From-SVN: r177587

--- gcc/config/rx/rx.c
+++ gcc/config/rx/rx.c
@@ -2158,10 +2158,10 @@ rx_expand_builtin (tree exp,
       if (! valid_psw_flag (op, "clrpsw"))
 	return NULL_RTX;
       return rx_expand_void_builtin_1_arg (op, gen_clrpsw, false);
+    case RX_BUILTIN_SETPSW:  
       if (! valid_psw_flag (op, "setpsw"))
 	return NULL_RTX;
       return rx_expand_void_builtin_1_arg (op, gen_setpsw, false);
-    case RX_BUILTIN_SETPSW:  
     case RX_BUILTIN_INT:     return rx_expand_void_builtin_1_arg
 	(op, gen_int, false);
     case RX_BUILTIN_MACHI:   return rx_expand_builtin_mac (exp, gen_machi);
