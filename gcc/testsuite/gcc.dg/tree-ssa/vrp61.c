4001900fb3dd67c055a9350244fd4a644c6d7c7a
Richard Guenther <rguenther@suse.de>
2011-08-09 14:03:10 +0000

    tree-vrp.c (zero_nonzero_bits_from_vr): Also return precise information for ranges with only negative values.
    2011-08-09  Richard Guenther  <rguenther@suse.de>
    
    	* tree-vrp.c (zero_nonzero_bits_from_vr): Also return precise
    	information for ranges with only negative values.
    	(extract_range_from_binary_expr_1): Adjust BIT_IOR_EXPR and
    	BIT_AND_EXPR handling to handle ranges with negative values.
    
    	* gcc.dg/tree-ssa/vrp57.c: Disable CCP.
    	* gcc.dg/tree-ssa/vrp60.c: New testcase.
    	* gcc.dg/tree-ssa/vrp61.c: Likewise.
    	* gcc.dg/tree-ssa/vrp62.c: Likewise.
    
    From-SVN: r177597

---
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp61.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-vrp1" } */
+
+int f (int x, int y)
+{
+  if (x > -1024 && x < 0 && y > -1024 && y < 0)
+    {
+      x = x ^ y;
+      if (x < 0 || x > 1023)
+	return 1234;
+    }
+  return x;
+}
+
+/* { dg-final { scan-tree-dump-not "1234" "vrp1" } } */
+/* { dg-final { cleanup-tree-dump "vrp1" } } */
