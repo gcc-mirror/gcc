e4ae859de0fa4e0da931697be6853a178eb75c8e
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-08-11 15:57:38 +0000

    2011-08-11  Romain Geissler  <romain.geissler@gmail.com>             Brian Hackett  <bhackett1024@gmail.com>
    gcc/ChangeLog:
    
          * plugin.def: Add event for finish_decl.
          * plugin.c (register_callback, invoke_plugin_callbacks): Same.
          * c-decl.c (finish_decl): Invoke callbacks on above event.
          * doc/plugins.texi: Document above event.
    
    gcc/cp/ChangeLog:
    
          * decl.c (cp_finish_decl): Invoke callbacks on finish_decl event.
    
    gcc/testsuite/ChangeLog:
    
          * g++.dg/plugin/decl_plugin.c: New.
          * g++.dg/plugin/decl-plugin-test.C: New.
          * g++.dg/plugin/plugin.exp: Add above testcase.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@177674 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/plugin/decl_plugin.c
@@ -0,0 +1,51 @@
+/* A plugin example that shows which declarations are caught by FINISH_DECL */
+
+#include "gcc-plugin.h"
+#include <stdlib.h>
+#include "config.h"
+#include "system.h"
+#include "coretypes.h"
+#include "tree.h"
+#include "tree-pass.h"
+#include "intl.h"
+
+int plugin_is_GPL_compatible;
+
+/* Callback function to invoke after GCC finishes a declaration. */
+
+void plugin_finish_decl (void *event_data, void *data)
+{
+  tree decl = (tree) event_data;
+
+  const char *kind = NULL;
+  switch (TREE_CODE(decl)) {
+  case FUNCTION_DECL:
+    kind = "Function"; break;
+  case PARM_DECL:
+    kind = "Parameter"; break;
+  case VAR_DECL:
+    if (DECL_FILE_SCOPE_P(decl))
+      kind = "Global";
+    else
+      kind = "Local";
+    break;
+  case FIELD_DECL:
+    kind = "Field"; break;
+  default:
+    kind = "Unknown";
+  }
+
+  warning (0, G_("Decl %s %s"),
+           kind, IDENTIFIER_POINTER (DECL_NAME (decl)));
+}
+
+int
+plugin_init (struct plugin_name_args *plugin_info,
+             struct plugin_gcc_version *version)
+{
+  const char *plugin_name = plugin_info->base_name;
+
+  register_callback (plugin_name, PLUGIN_FINISH_DECL,
+                     plugin_finish_decl, NULL);
+  return 0;
+}
