7d7d492267e2f1b8053bfd232c98a0f9fbde5982
aesok <aesok@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-08-16 17:04:13 +0000

    * config/m32c/m32c.h (PREFERRED_RELOAD_CLASS, 	PREFERRED_OUTPUT_RELOAD_CLASS): Remove macro. 	* config/m32c/m32c-protos.h (m32c_preferred_reload_class, 	m32c_preferred_output_reload_class): Remove. 	* config/m32c/m32c.c (m32c_preferred_reload_class): Make static. 	Change rclass argument and return types to reg_class_t. Use 	reg_class_subset_p instead of class_sizes. 	(m32c_preferred_output_reload_class): Make static. Change rclass 	argument and return types to reg_class_t. 	(TARGET_PREFERRED_RELOAD_CLASS, 	TARGET_PREFERRED_OUTPUT_RELOAD_CLASS): Define.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@177790 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/m32c/m32c-protos.h
+++ gcc/config/m32c/m32c-protos.h
@@ -66,8 +66,6 @@ int  m32c_limit_reload_class (enum machine_mode, int);
 int  m32c_modes_tieable_p (enum machine_mode, enum machine_mode);
 bool m32c_mov_ok (rtx *, enum machine_mode);
 char * m32c_output_compare (rtx, rtx *);
-int  m32c_preferred_output_reload_class (rtx, int);
-int  m32c_preferred_reload_class (rtx, int);
 int  m32c_prepare_move (rtx *, enum machine_mode);
 int  m32c_prepare_shift (rtx *, int, int);
 int  m32c_reg_ok_for_base_p (rtx, int);
