1e138510faf8ba23eab43a70fbe96ef3f9dd798e
Richard Sandiford <richard.sandiford@linaro.org>
2011-08-17 11:13:20 +0000

    re PR target/50090 (ARM EABI symbols in libgcc.a have default visibility)
    libgcc/
    	PR target/50090
    	* config/arm/bpabi-lib.h (RENAME_LIBRARY): Use a C-level alias
    	instead of an assembly one.
    
    From-SVN: r177826

--- libgcc/config/arm/bpabi-lib.h
+++ libgcc/config/arm/bpabi-lib.h
@@ -28,9 +28,8 @@
 
 /* Make __aeabi_AEABI_NAME an alias for __GCC_NAME.  */
 #define RENAME_LIBRARY(GCC_NAME, AEABI_NAME)		\
-  __asm__ (".globl\t__aeabi_" #AEABI_NAME "\n"		\
-	   RENAME_LIBRARY_SET "\t__aeabi_" #AEABI_NAME 	\
-	     ", __" #GCC_NAME "\n");
+  typeof (__##GCC_NAME) __aeabi_##AEABI_NAME \
+    __attribute__((alias ("__" #GCC_NAME)));
 
 /* Give some libgcc functions an additional __aeabi name.  */
 #ifdef L_muldi3
