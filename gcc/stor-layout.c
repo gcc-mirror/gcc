f44157f84be4cc82697697136464ed7711595aa5
Jakub Jelinek <jakub@redhat.com>
2011-08-18 16:29:10 +0200

    re PR target/50009 (Segmentation fault in tree_nop_conversion)
    	PR target/50009
    	* stor-layout.c (update_alignment_for_field): Don't ICE on
    	packed flexible array members if ms_bitfield_layout_p.
    
    	* gcc.c-torture/compile/pr50009.c: New test.
    
    From-SVN: r177860

--- gcc/stor-layout.c
+++ gcc/stor-layout.c
@@ -1,7 +1,7 @@
 /* C-compiler utilities for types and variables storage layout
    Copyright (C) 1987, 1988, 1992, 1993, 1994, 1995, 1996, 1996, 1998,
-   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010
-   Free Software Foundation, Inc.
+   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+   2011 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
@@ -935,7 +935,8 @@ update_alignment_for_field (record_layout_info rli, tree field,
 	 applies if there was an immediately prior, nonzero-size
 	 bitfield.  (That's the way it is, experimentally.) */
       if ((!is_bitfield && !DECL_PACKED (field))
-	  || (!integer_zerop (DECL_SIZE (field))
+	  || ((DECL_SIZE (field) == NULL_TREE
+	       || !integer_zerop (DECL_SIZE (field)))
 	      ? !DECL_PACKED (field)
 	      : (rli->prev_field
 		 && DECL_BIT_FIELD_TYPE (rli->prev_field)
