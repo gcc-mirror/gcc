a2f3eae24de6e78302ea2b10c3f57635ca954ec2
Tobias Burnus <burnus@net-b.de>
2011-08-18 16:23:41 +0200

    re PR fortran/50109 (Formatted namelist read with multiple "!" fails with: Cannot match namelist object name !)
    2011-08-18  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/50109
            * io/list_read.c (eat_separator): Fix skipping over "!" lines.
    
    2011-08-18  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/50109
            * gfortran.dg/namelist_73.f90: New.
    
    From-SVN: r177858

---
+++ gcc/testsuite/gfortran.dg/namelist_73.f90
@@ -0,0 +1,28 @@
+! { dg-do run }
+!
+! PR fortran/50109
+!
+! Contributed by Jim Hanson
+!
+      program namelist_test
+
+      integer nfp
+      namelist /indata/ nfp
+
+      nfp = 99
+      open(unit=4, status='scratch')
+      write(4,'(a)') '$indata'
+      write(4,'(a)') 'NFP = 5,'
+      write(4,'(a)') "!  "
+      write(4,'(a)') "! "
+      write(4,'(a)') "!  "
+      write(4,'(a)') '/'
+
+      rewind(4)
+      read (4,nml=indata)
+      close(4)
+
+!      write(*,*) nfp
+      if (nfp /= 5) call abort()
+
+      end
