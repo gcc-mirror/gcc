ab37ef4cd06dae81792cea537d919adb81ca0e9c
Richard Guenther <rguenther@suse.de>
2011-08-19 12:30:36 +0000

    pr50067-1.c: New testcase.
    2011-08-19  Richard Guenther  <rguenther@suse.de>
    
    	* gcc.dg/torture/pr50067-1.c: New testcase.
    	* gcc.dg/torture/pr50067-2.c: Likewise.
    
    From-SVN: r177896

---
+++ gcc/testsuite/gcc.dg/torture/pr50067-2.c
@@ -0,0 +1,20 @@
+/* { dg-do run } */
+
+/* Make sure data-dependence analysis does not compute a bogus
+  distance vector for the different sized accesses.  */
+
+extern int memcmp(const void *, const void *, __SIZE_TYPE__);
+extern void abort (void);
+short a[32] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
+short b[32] = { 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, };
+int main()
+{
+  int i;
+  for (i = 0; i < 32; ++i)
+    {
+      a[i] = (*((char(*)[32])&a[0]))[i+8];
+    }
+  if (memcmp (&a, &b, sizeof (a)) != 0)
+    abort ();
+  return 0;
+}
