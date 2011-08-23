ba05e6332574e2e2ad32e00fe60b70c9d99636e7
Uros Bizjak <uros@gcc.gnu.org>
2011-08-23 21:49:57 +0200

    sfp-machine.h (ASM_INVALID): New define.
    	* config/i386/64/sfp-machine.h (ASM_INVALID): New define.
    	(ASM_DIVZERO): Ditto.
    	(FP_HANLDE_EXCEPTIONS): Use ASM_INVALID and ASM_DIVZERO.
    
    From-SVN: r178009

--- libgcc/config/i386/64/sfp-machine.h
+++ libgcc/config/i386/64/sfp-machine.h
@@ -79,17 +79,25 @@ struct fenv
   unsigned short int __unused5;
 };
 
+#ifdef __AVX__
+ #define ASM_INVALID "vdivss %0, %0, %0"
+ #define ASM_DIVZERO "vdivss %1, %0, %0"
+#else
+ #define ASM_INVALID "divss %0, %0"
+ #define ASM_DIVZERO "divss %1, %0"
+#endif
+
 #define FP_HANDLE_EXCEPTIONS						\
   do {									\
     if (_fex & FP_EX_INVALID)						\
       {									\
 	float f = 0.0;							\
-	__asm__ __volatile__ ("divss %0, %0 " : : "x" (f));		\
+	__asm__ __volatile__ (ASM_INVALID : : "x" (f));			\
       }									\
     if (_fex & FP_EX_DIVZERO)						\
       {									\
 	float f = 1.0, g = 0.0;						\
-	__asm__ __volatile__ ("divss %1, %0" : : "x" (f), "x" (g));	\
+	__asm__ __volatile__ (ASM_DIVZERO : : "x" (f), "x" (g));	\
       }									\
     if (_fex & FP_EX_OVERFLOW)						\
       {									\
