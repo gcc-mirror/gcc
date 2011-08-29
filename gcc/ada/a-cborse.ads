704880481a22c59657091bfb5b998efd989c9a5f
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-08-29 14:21:27 +0000

    2011-08-29  Matthew Heaney  <heaney@adacore.com>
    	* a-cbhama.ads, a-cbhase.ads (Move): Clear Source following assignment
    	to Target.
    
    2011-08-29  Matthew Heaney  <heaney@adacore.com>
    
    	* a-cborma.ads, a-cborse.ads (Cursor): Default-initialize all
    	components of record type.
    	* a-cborma.adb, a-cborse.adb (Move): Clear Source following assignmentw
    	to Target.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@178243 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/a-cborse.ads
+++ gcc/ada/a-cborse.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---          Copyright (C) 2004-2010, Free Software Foundation, Inc.         --
+--          Copyright (C) 2004-2011, Free Software Foundation, Inc.         --
 --                                                                          --
 -- This specification is derived from the Ada Reference Manual for use with --
 -- GNAT. The copyright notice above, and the license provisions that follow --
@@ -257,7 +257,7 @@ private
 
    type Cursor is record
       Container : Set_Access;
-      Node      : Count_Type;
+      Node      : Count_Type := 0;
    end record;
 
    use Tree_Types;
