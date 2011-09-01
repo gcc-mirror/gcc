f76574fd81e17231469dbb8552ff3070ec5e7b8a
Mikael Morin <mikael.morin@sfr.fr>
2011-09-01 17:52:39 +0200

    re PR fortran/50050 (Internal compiler error free_expr0 at expr.c:3709 via gfc_done_2)
    2011-09-01  Mikael Morin  <mikael.morin@sfr.fr>
    
    	PR fortran/50050
    	* resolve.c (gfc_expr_to_initialize): Don't copy rank.
    	Free copied shape.  Recalculate shape and rank.
    
    2011-09-01  Mikael Morin  <mikael.morin@sfr.fr>
    
    	PR fortran/50050
    	* gfortran.dg/alloc_comp_initializer_3.f90: New test.
    	* gfortran.dg/pointer_comp_init.f90: New test.
    
    From-SVN: r178424

---
+++ gcc/testsuite/gfortran.dg/pointer_comp_init_1.f90
@@ -0,0 +1,30 @@
+! { dg-do compile }
+!
+! PR fortran/50050
+! ICE whilst trying to access NULL shape.
+
+! Reduced from the FoX library http://www1.gly.bris.ac.uk/~walker/FoX/
+! Contributed by Andrew Benson <abenson@its.caltech.edu>
+
+module m_common_attrs
+  implicit none
+
+  type dict_item
+  end type dict_item
+
+  type dict_item_ptr
+     type(dict_item), pointer :: d => null()
+  end type dict_item_ptr
+
+contains
+
+  subroutine add_item_to_dict()
+    type(dict_item_ptr), pointer :: tempList(:)
+    integer :: n
+
+    allocate(tempList(0:n+1)) 
+  end subroutine add_item_to_dict
+
+end module m_common_attrs
+
+! { dg-final { cleanup-modules "m_common_attrs" } }
