42ea8b27905a16bcc455a9d45b24701a7aeacc06
Richard Sandiford <richard.sandiford@linaro.org>
2011-09-02 13:32:10 +0000

    re PR target/49987 (gcc.c-torture/compile/pr34856.c fails on powerpc-darwin9 from r176228)
    gcc/
    	PR target/49987
    	* config/rs6000/rs6000.c (paired_expand_vector_init): Check for
    	valid CONST_VECTOR operands.
    	(rs6000_expand_vector_init): Likewise.
    
    From-SVN: r178474

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -4503,7 +4503,9 @@ paired_expand_vector_init (rtx target, rtx vals)
   for (i = 0; i < n_elts; ++i)
     {
       x = XVECEXP (vals, 0, i);
-      if (!CONSTANT_P (x))
+      if (!(CONST_INT_P (x)
+	    || GET_CODE (x) == CONST_DOUBLE
+	    || GET_CODE (x) == CONST_FIXED))
 	++n_var;
     }
   if (n_var == 0)
@@ -4655,7 +4657,9 @@ rs6000_expand_vector_init (rtx target, rtx vals)
   for (i = 0; i < n_elts; ++i)
     {
       x = XVECEXP (vals, 0, i);
-      if (!CONSTANT_P (x))
+      if (!(CONST_INT_P (x)
+	    || GET_CODE (x) == CONST_DOUBLE
+	    || GET_CODE (x) == CONST_FIXED))
 	++n_var, one_var = i;
       else if (x != CONST0_RTX (inner_mode))
 	all_const_zero = false;
