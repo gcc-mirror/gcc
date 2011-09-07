aad0eef6aaf3de8c9fbd5edb3b5fd31161b52dda
Georg-Johann Lay <avr@gjlay.de>
2011-09-07 16:55:08 +0000

    section1.c: Don't xfail for avr.
    	* gcc.dg/section1.c: Don't xfail for avr.
    
    From-SVN: r178649

--- gcc/testsuite/gcc.dg/section1.c
+++ gcc/testsuite/gcc.dg/section1.c
@@ -1,5 +1,5 @@
 /* PR optimization/6871 */
 /* Constant variables belong in .rodata, not .bss.  */
-/* { dg-final { scan-assembler-not "\.bss" { xfail avr-*-*} } } */
+/* { dg-final { scan-assembler-not "\.bss" } } */
 
 const int i = 0;
