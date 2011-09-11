ce7c2504688895a0056dc9567a75c81ad0cc7403
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-09-11 18:56:40 +0000

    * gcc-interface/decl.c (maybe_pad_type): Do not try to change the form 	of an addressable type. 	* gcc-interface/trans.c (gnat_gimplify_expr) <VIEW_CONVERT_EXPR>: New. 	Deal with those cases for which creating a temporary is mandatory.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@178764 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gnat.dg/atomic5.ads
@@ -0,0 +1,25 @@
+-- { dg-do compile }
+
+with Unchecked_Conversion;
+
+package Atomic5 is
+
+  type Byte is mod 2 ** 8;
+  for Byte'Size use 8;
+
+  type Unsigned_32 is mod 2 ** 32;
+  for Unsigned_32'Size use 32;
+
+  type R is record
+    A,B,C,D : Byte;
+  end record;
+  for R'Alignment use 4;
+  pragma Atomic (R);
+
+  function Conv is new Unchecked_Conversion (R, Unsigned_32);
+
+  procedure Proc1;
+
+  procedure Proc2;
+
+end Atomic5;
