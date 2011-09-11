819a653eee396dc4db95ddc3c87805acab08c2de
Eric Botcazou <ebotcazou@adacore.com>
2011-09-11 18:56:40 +0000

    decl.c (maybe_pad_type): Do not try to change the form of an addressable type.
    	* gcc-interface/decl.c (maybe_pad_type): Do not try to change the form
    	of an addressable type.
    	* gcc-interface/trans.c (gnat_gimplify_expr) <VIEW_CONVERT_EXPR>: New.
    	Deal with those cases for which creating a temporary is mandatory.
    
    From-SVN: r178764

---
+++ gcc/testsuite/gnat.dg/atomic5.adb
@@ -0,0 +1,20 @@
+package body Atomic5 is
+
+  function Create return R is
+  begin
+    return (A => 0, B => 1, C => 2, D => 4);
+  end;
+
+  procedure Proc1 is
+    I : Unsigned_32;
+  begin
+    I := Conv(Create);
+  end;
+
+  procedure Proc2 is
+    I : Unsigned_32;
+  begin
+    I := Conv(R'(A => 0, B => 1, C => 2, D => 4));
+  end;
+
+end Atomic5;
