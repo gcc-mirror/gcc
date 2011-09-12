9451462a926a6a47d7e1069b58b691fdf4433ad3
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-09-12 19:25:59 +0000

    gcc/ 2011-09-11  Bernd Schmidt  <bernds@codesourcery.com> 	    Richard Sandiford  <rdsandiford@googlemail.com>
    	* config/mips/mips.c (mips_epilogue): New structure.
    	(mips16e_save_restore_reg): Queue REG_CFA_RESTORE notes when
    	restoring registers.
    	(mips_epilogue_emit_cfa_restores): New function.
    	(mips_epilogue_set_cfa): Likewise.
    	(mips_restore_reg): Queue REG_CFA_RESTORE notes.  When restoring
    	the current CFA register from the stack, redefine the CFA in terms
    	of the stack pointer.
    	(mips_expand_epilogue): Set up mips_epilogue.  Attach CFA information
    	to the epilogue instructions.
    
    gcc/testsuite/
    	* gcc.target/mips/mips.exp (mips_option_groups): Add debug options.
    	* gcc.target/mips/interrupt_handler-2.c: New test.
    	* gcc.target/mips/interrupt_handler-3.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@178794 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/mips/interrupt_handler-2.c
@@ -0,0 +1,14 @@
+/* Make sure that we emit .cfa_restore notes for LO and HI.  */
+/* { dg-options "-mips32r2 -msoft-float -O -g" } */
+/* { dg-final { scan-assembler "\t\\\.cfi_restore 64\n" } } */
+/* { dg-final { scan-assembler "\t\\\.cfi_restore 65\n" } } */
+/* { dg-final { scan-assembler-not "\\\.cfi_def_cfa( |\t)" } } */
+/* { dg-final { scan-assembler-not "\\\.cfi_def_cfa_register( |\t)" } } */
+
+extern void f (void);
+
+NOMIPS16 void __attribute__ ((interrupt, use_shadow_register_set))
+v1 (void)
+{
+  f ();
+}
