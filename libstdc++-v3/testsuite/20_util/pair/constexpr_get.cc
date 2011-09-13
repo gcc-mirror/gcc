9480716c80167f17226d9bd9d70883c6bf7bda5f
Daniel Krugler <daniel.kruegler@googlemail.com>
2011-09-13 00:06:25 +0000

    tuple (_Head_base<>::_M_head, [...]): Change to static constexpr functions; adjust everywhere.
    2011-09-12  Daniel Krugler  <daniel.kruegler@googlemail.com>
    	    Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* include/std/tuple (_Head_base<>::_M_head, _Head_base<>::_M_tail,
    	_Tuple_impl<>::_M_head, _Tuple_impl<>::_M_tail): Change to static
    	constexpr functions; adjust everywhere.
    	(__get_helper, get): Declare constexpr all three overloads.
    	(tuple_cat): Declare constexpr; use late return type to improve
    	error messages.
    	* include/std/utility (__pair_get<>::__get, __pair_get<>::__move_get,
    	__pair_get<>::__const_get, get): Declare all constexpr.
    	* include/std/array (get): Likewise.
    	* testsuite/20_util/tuple/creation_functions/constexpr.cc: Re-enable
    	tuple_cat test.
    	* testsuite/23_containers/array/constexpr_get.cc: New.
    	* testsuite/20_util/tuple/element_access/constexpr_get.cc: Likewise.
    	* testsuite/20_util/pair/constexpr_get.cc: Likewise.
    	* testsuite/20_util/weak_ptr/comparison/cmp_neg.cc: Adjust dg-error
    	line number.
    
    Co-Authored-By: Paolo Carlini <paolo.carlini@oracle.com>
    
    From-SVN: r178799

---
+++ libstdc++-v3/testsuite/20_util/pair/constexpr_get.cc
@@ -0,0 +1,28 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-do compile }
+
+// Copyright (C) 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <utility>
+
+std::pair<int, int> pi;
+const std::pair<int, int> cpi;
+
+constexpr const int& cri = std::get<0>(cpi);
+constexpr int&  ri = std::get<0>(pi);
+constexpr int&& rri = std::get<0>(std::move(pi));
---
+++ libstdc++-v3/testsuite/23_containers/array/constexpr_get.cc
@@ -0,0 +1,30 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-do compile }
+
+// Copyright (C) 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// Tuple
+
+#include <array>
+
+std::array<int, 5> ai;
+const std::array<int, 5> cai(ai);
+
+constexpr const int& cri = std::get<0>(cai);
+constexpr int&  ri = std::get<0>(ai);
+constexpr int&& rri = std::get<0>(std::move(ai));
