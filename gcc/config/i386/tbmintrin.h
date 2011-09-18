e0cdd8d4c204592661f161e9be8009355488c46f
H.J. Lu <hongjiu.lu@intel.com>
2011-09-18 15:39:18 +0000

    Remove tmp from bmiintrin.h and tbmintrin.h.
    2011-09-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* config/i386/bmiintrin.h: Remove tmp.
    	* config/i386/tbmintrin.h: Likewise.
    
    From-SVN: r178943

--- gcc/config/i386/tbmintrin.h
+++ gcc/config/i386/tbmintrin.h
@@ -47,64 +47,55 @@ __bextri_u32 (unsigned int __X, const unsigned int __I)
 extern __inline unsigned int __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blcfill_u32 (unsigned int __X)
 {
-	unsigned int tmp = (__X) & ((__X) + 1);
-	return tmp;
+  return __X & (__X + 1);
 }
 
 extern __inline unsigned int __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blci_u32 (unsigned int __X)
 {
-	unsigned int tmp = (__X) | (~((__X) + 1));
-	return tmp;
+  return __X | ~(__X + 1);
 }
 
 extern __inline unsigned int __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blcic_u32 (unsigned int __X)
 {
-	unsigned int tmp = (~(__X)) & ((__X) + 1);
-	return tmp;
+  return ~__X & (__X + 1);
 }
 
 extern __inline unsigned int __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blcmsk_u32 (unsigned int __X)
 {
-	unsigned int tmp = (__X) ^ ((__X) + 1);
-	return tmp;
+  return __X ^ (__X + 1);
 }
 
 extern __inline unsigned int __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blcs_u32 (unsigned int __X)
 {
-	unsigned int tmp = (__X) | ((__X) + 1);
-	return tmp;
+  return __X | (__X + 1);
 }
 
 extern __inline unsigned int __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blsfill_u32 (unsigned int __X)
 {
-	unsigned int tmp = (__X) | ((__X) - 1);
-	return tmp;
+  return __X | (__X - 1);
 }
 
 extern __inline unsigned int __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blsic_u32 (unsigned int __X)
 {
-	unsigned int tmp = (~(__X)) | ((__X) - 1);
-	return tmp;
+  return ~__X | (__X - 1);
 }
 
 extern __inline unsigned int __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __t1mskc_u32 (unsigned int __X)
 {
-	unsigned int tmp = (~(__X)) | ((__X) + 1);
-	return tmp;
+  return ~__X | (__X + 1);
 }
 
 extern __inline unsigned int __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __tzmsk_u32 (unsigned int __X)
 {
-	unsigned int tmp = (~(__X)) & ((__X) - 1);
-	return tmp;
+  return ~__X & (__X - 1);
 }
 
 
@@ -114,78 +105,68 @@ __tzmsk_u32 (unsigned int __X)
 extern __inline unsigned long long __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __bextri_u64 (unsigned long long __X, const unsigned int __I)
 {
-	return __builtin_ia32_bextri_u64 (__X, __I);
+  return __builtin_ia32_bextri_u64 (__X, __I);
 }
 #else
-#define __bextri_u64(X, I)                                                       \
-        ((unsigned long long)__builtin_ia32_bextri_u64 ((unsigned long long)(X), \
-	                                                (unsigned long long)(I)))
+#define __bextri_u64(X, I)						   \
+  ((unsigned long long)__builtin_ia32_bextri_u64 ((unsigned long long)(X), \
+						  (unsigned long long)(I)))
 #endif /*__OPTIMIZE__ */
 
 extern __inline unsigned long long __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blcfill_u64 (unsigned long long __X)
 {
-	unsigned long long tmp = (__X) & ((__X) + 1);
-	return tmp;
+  return __X & (__X + 1);
 }
 
 extern __inline unsigned long long __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blci_u64 (unsigned long long __X)
 {
-	unsigned long long tmp = (__X) | (~((__X) + 1));
-	return tmp;
+  return __X | ~(__X + 1);
 }
 
 extern __inline unsigned long long __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blcic_u64 (unsigned long long __X)
 {
-	unsigned long long tmp = (~(__X)) & ((__X) + 1);
-	return tmp;
+  return ~__X & (__X + 1);
 }
 
 extern __inline unsigned long long __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blcmsk_u64 (unsigned long long __X)
 {
-	unsigned long long tmp = (__X) ^ ((__X) + 1);
-	return tmp;
+  return __X ^ (__X + 1);
 }
 
 extern __inline unsigned long long __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blcs_u64 (unsigned long long __X)
 {
-	unsigned long long tmp = (__X) | ((__X) + 1);
-	return tmp;
+  return __X | (__X + 1);
 }
 
 extern __inline unsigned long long __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blsfill_u64 (unsigned long long __X)
 {
-	unsigned long long tmp = (__X) | ((__X) - 1);
-	return tmp;
+  return __X | (__X - 1);
 }
 
 extern __inline unsigned long long __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __blsic_u64 (unsigned long long __X)
 {
-	unsigned long long tmp = (~(__X)) | ((__X) - 1);
-	return tmp;
+  return ~__X | (__X - 1);
 }
 
 extern __inline unsigned long long __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __t1mskc_u64 (unsigned long long __X)
 {
-	unsigned long long tmp = (~(__X)) | ((__X) + 1);
-	return tmp;
+  return ~__X | (__X + 1);
 }
 
 extern __inline unsigned long long __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 __tzmsk_u64 (unsigned long long __X)
 {
-	unsigned long long tmp = (~(__X)) & ((__X) - 1);
-	return tmp;
+  return ~__X & (__X - 1);
 }
 
 
 #endif /* __x86_64__  */
 #endif /* _TBMINTRIN_H_INCLUDED */
-
