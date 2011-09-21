0c51472758bef54b179181f083cb2cb4fa937ca3
Tom de Vries <tom@codesourcery.com>
2011-09-21 07:41:22 +0000

    final.c (final): Handle if JUMP_LABEL is not LABEL_P.
    2011-09-21  Tom de Vries  <tom@codesourcery.com>
    
    	* final.c (final): Handle if JUMP_LABEL is not LABEL_P.
    
    From-SVN: r179034

--- gcc/final.c
+++ gcc/final.c
@@ -1739,7 +1739,7 @@ final (rtx first, FILE *file, int optimize_p)
       if (optimize_p && JUMP_P (insn))
 	{
 	  rtx lab = JUMP_LABEL (insn);
-	  if (lab && LABEL_NUSES (lab) == 1)
+	  if (lab && LABEL_P (lab) && LABEL_NUSES (lab) == 1)
 	    {
 	      LABEL_REFS (lab) = insn;
 	    }
