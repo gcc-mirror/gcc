377fc3e81d6e928293a00a5d9d4eb3ca9def8449
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-09-27 00:02:54 +0000

    2011-09-26  Benjamin Kosnik  <bkoz@redhat.com             Jonathan Wakely  <jwakely.gcc@gmail.com>
    	PR libstdc++/48698
    	* acinclude.m4 (GLIBCXX_ENABLE_SYMVERS): Set libtool_VERSION here.
    	* configure.ac: Move AC_SUBST of libtool_VERSION past call to
    	GLIBCXX_ENABLE_SYMVERS.
    	* configure: Regenerate.
    	* include/bits/c++config: Use __7 as versioned namespace name.
    	* config/abi/pre/gnu-versioned-namespace.ver: Change mangling as
    	per above.
    	* include/c_global/cwchar: Adjust nested namespaces.
    	* testsuite/20_util/bind/48698.cc: Add test case.
    	* testsuite/ext/profile/mutex_extensions_neg.cc: Change line number.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@179221 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/20_util/bind/48698.cc
+++ libstdc++-v3/testsuite/20_util/bind/48698.cc
@@ -1,28 +1,31 @@
-// { dg-options "-D_GLIBCXX_DEBUG -D_GLIBCXX_PROFILE" }
 // { dg-do compile }
+// { dg-options "-std=gnu++0x" }
 
-// -*- C++ -*-
-
-// Otherwise we may get *multiple* errors.
-#undef _GLIBCXX_PARALLEL
-
-// Copyright (C) 2006, 2007, 2009, 2010, 2011 Free Software Foundation, Inc.
+// Copyright (C) 2011 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the
 // Free Software Foundation; either version 3, or (at your option)
 // any later version.
-//
+
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-//
+
 // You should have received a copy of the GNU General Public License along
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-#include <vector>
+// 20.7.11 Function template bind
 
-// { dg-error "multiple inlined namespaces" "" { target *-*-* } 256 }
+#include <functional>
+
+int f(int i);
+
+// Don't want placeholders to interfere with a possibly-versioned namespace.
+int g()
+{
+  std::bind(f, std::placeholders::_6);
+}
