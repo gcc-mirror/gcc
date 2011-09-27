4ef69dfc4a826dfec807ac74114748595936806a
Ira Rosen <ira.rosen@linaro.org>
2011-09-27 12:26:34 +0000

    tree-vect-stmts.c (vectorizable_type_demotion): Handle basic block vectorization.
            * tree-vect-stmts.c (vectorizable_type_demotion): Handle basic
            block vectorization.
            (vectorizable_type_promotion): Likewise.
            (vect_analyze_stmt): Call vectorizable_type_demotion and
            vectorizable_type_promotion for basic blocks.
            (supportable_widening_operation): Don't assume loop
            vectorization.
            * tree-vect-slp.c (vect_build_slp_tree): Allow multiple types
            for basic blocks.  Update vectorization factor for basic block
            vectorization.
            (vect_analyze_slp_instance): Allow multiple types for basic
            block vectorization.  Recheck unrolling factor after
            construction of SLP instance.
    
    From-SVN: r179267

---
+++ gcc/testsuite/gcc.dg/vect/bb-slp-27.c
@@ -0,0 +1,49 @@
+/* { dg-require-effective-target vect_int } */
+
+#include <stdarg.h>
+#include "tree-vect.h"
+
+#define A 3
+#define N 16
+
+short src[N], dst[N];
+
+void foo (int a)
+{
+  dst[0] += a*src[0];
+  dst[1] += a*src[1];
+  dst[2] += a*src[2];
+  dst[3] += a*src[3];
+  dst[4] += a*src[4];
+  dst[5] += a*src[5];
+  dst[6] += a*src[6];
+  dst[7] += a*src[7];
+}
+
+
+int main (void)
+{
+  int i;
+
+  check_vect ();
+
+  for (i = 0; i < N; i++)
+    {
+      dst[i] = 0;
+      src[i] = i;
+    }
+
+  foo (A);
+
+  for (i = 0; i < 8; i++)
+    {
+      if (dst[i] != A * i)
+        abort ();
+    }
+
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "basic block vectorized using SLP" 1 "slp" { target { vect_int_mult && { vect_unpack && vect_pack_trunc } } } } } */
+/* { dg-final { cleanup-tree-dump "slp" } } */
+
