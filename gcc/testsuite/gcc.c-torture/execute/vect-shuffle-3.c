38134a2a03db157c81f8f6dd6af1a9df36912497
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-10-03 17:08:43 +0000

    Fix broken commit wrt vect-shuffle-N.c tests.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@179468 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/vect-shuffle-3.c
+++ gcc/testsuite/gcc.c-torture/execute/vect-shuffle-3.c
@@ -24,43 +24,7 @@ int main (int argc, char *argv[]) {
     vector (8, short) v2;
 
     vector (8, short) mask = {0,0,1,2,3,4,5,6};
-    
-    v2 = f (v0,  mask);
-    shufcompare (short, 8, v2, v0, mask);
 
-    v2 = f (v0, v1);
-    shufcompare (short, 8, v2, v0, v1);
-
-    return 0;
-}
-
-#define vector(elcount, type)  \
-__attribute__((vector_size((elcount)*sizeof(type)))) type
-
-#define vidx(type, vec, idx) (*(((type *) &(vec)) + idx))
-
-#define shufcompare(type, count, vres, v0, mask) \
-do { \
-    int __i; \
-    for (__i = 0; __i < count; __i++) { \
-        if (vidx(type, vres, __i) != vidx(type, v0, vidx(type, mask, __i))) \
-            __builtin_abort (); \
-    } \
-} while (0)
-
-vector (8, short) __attribute__ ((noinline))
-f (vector (8, short) x, vector (8, short) mask) {
-    return __builtin_shuffle (x, mask);
-}
-
-
-int main (int argc, char *argv[]) {
-    vector (8, short) v0 = {argc, 1,2,3,4,5,6,7};
-    vector (8, short) v1 = {argc, 1,argc,3,4,5,argc,7};
-    vector (8, short) v2;
-
-    vector (8, short) mask = {0,0,1,2,3,4,5,6};
-    
     v2 = f (v0,  mask);
     shufcompare (short, 8, v2, v0, mask);
 
