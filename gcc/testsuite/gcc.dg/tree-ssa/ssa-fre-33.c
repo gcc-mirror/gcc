87b533973970ef09f94167d428fbc63331045d96
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-10-05 14:35:15 +0000

    2011-10-05  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/38885
    	* tree-ssa-sccvn.c (vn_reference_lookup_3): Handle partial reads
    	from constants.
    
    	* gcc.dg/tree-ssa/ssa-fre-33.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@179556 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-fre-33.c
@@ -0,0 +1,21 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-fre1-details" } */
+
+#define vector __attribute__((vector_size(16) ))
+
+struct {
+    float i;
+    vector float global_res;
+} s;
+float x;
+int main(int argc)
+{
+  vector float res = (vector float){0.0f,0.0f,0.0f,1.0f};
+  res += (vector float){1.0f,2.0f,3.0f,4.0f};
+  s.global_res = res;
+  x = *((float*)&s.global_res + 1);
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump "Replaced BIT_FIELD_REF.*with 2" "fre1" } } */
+/* { dg-final { cleanup-tree-dump "fre1" } } */
