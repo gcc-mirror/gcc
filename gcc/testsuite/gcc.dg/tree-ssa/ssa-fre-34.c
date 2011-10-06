a3bb56f01d7d44f2114770debac8e172b1872a07
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-10-06 08:41:44 +0000

    2011-10-06  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/38884
    	* tree-ssa-sccvn.c (vn_reference_lookup_3): Handle partial
    	reads from aggregate SSA names.
    
    	* gcc.dg/tree-ssa/ssa-fre-34.c: New testcase.
    	* gcc.dg/tree-ssa/ssa-fre-35.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@179593 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-fre-34.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-fre1-details" } */
+
+#define vector __attribute__((vector_size(16) ))
+
+struct {
+    float i;
+    vector float global_res;
+} s;
+float foo(float f)
+{
+  vector float res = (vector float){0.0f,f,0.0f,1.0f};
+  s.global_res = res;
+  return *((float*)&s.global_res + 1);
+}
+
+/* { dg-final { scan-tree-dump "Replaced BIT_FIELD_REF.*with f" "fre1" } } */
+/* { dg-final { cleanup-tree-dump "fre1" } } */
