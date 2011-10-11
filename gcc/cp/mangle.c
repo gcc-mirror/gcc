a91885eaa2acf819c78e21d05e7cd23efd623466
Janis Johnson <janisjo@codesourcery.com>
2011-10-11 17:02:42 +0000

    re PR c++/44473 (iterators already defined for std::vector when using std::decimal)
    gcc/
    	PR c++/44473
    	* mangle.c (write_type): Handle CV qualifiers for decimal classes.
    
    gcc/testsuite/
    	PR c++/44473
    	* g++.dg/dfp/44473-1.C: New test.
    	* g++.dg/dfp/44473-2.C: New test.
    	* g++.dg/dfp/mangle-1.C: New test.
    	* g++.dg/dfp/mangle-2.C: New test.
    	* g++.dg/dfp/mangle-3.C: New test.
    	* g++.dg/dfp/mangle-4.C: New test.
    	* g++.dg/dfp/mangle-5.C: New test.
    
    From-SVN: r179809

--- gcc/cp/mangle.c
+++ gcc/cp/mangle.c
@@ -1790,11 +1790,6 @@ write_type (tree type)
   if (find_substitution (type))
     return;
 
-  /* According to the C++ ABI, some library classes are passed the
-     same as the scalar type of their single member and use the same
-     mangling.  */
-  if (TREE_CODE (type) == RECORD_TYPE && TYPE_TRANSPARENT_AGGR (type))
-    type = TREE_TYPE (first_field (type));
 
   if (write_CV_qualifiers_for_type (type) > 0)
     /* If TYPE was CV-qualified, we just wrote the qualifiers; now
@@ -1813,6 +1808,12 @@ write_type (tree type)
 
       /* See through any typedefs.  */
       type = TYPE_MAIN_VARIANT (type);
+
+      /* According to the C++ ABI, some library classes are passed the
+	 same as the scalar type of their single member and use the same
+	 mangling.  */
+      if (TREE_CODE (type) == RECORD_TYPE && TYPE_TRANSPARENT_AGGR (type))
+	type = TREE_TYPE (first_field (type));
 
       if (TYPE_PTRMEM_P (type))
 	write_pointer_to_member_type (type);
