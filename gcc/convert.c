ad1d5893845eb245a84976d894e539913e0a26df
Joseph Myers <joseph@codesourcery.com>
2011-10-12 12:57:36 +0100

    re PR c/50565 (initializer element is not computable at load time)
    	PR c/50565
    	* convert.c (convert_to_integer): Do not narrow operands of
    	pointer subtraction.
    
    testsuite:
    	* gcc.c-torture/compile/pr50565-1.c,
    	gcc.c-torture/compile/pr50565-2.c: New tests.
    
    From-SVN: r179846

--- gcc/convert.c
+++ gcc/convert.c
@@ -727,6 +727,15 @@ convert_to_integer (tree type, tree expr)
 	    tree arg0 = get_unwidened (TREE_OPERAND (expr, 0), type);
 	    tree arg1 = get_unwidened (TREE_OPERAND (expr, 1), type);
 
+	    /* Do not try to narrow operands of pointer subtraction;
+	       that will interfere with other folding.  */
+	    if (ex_form == MINUS_EXPR
+		&& CONVERT_EXPR_P (arg0)
+		&& CONVERT_EXPR_P (arg1)
+		&& POINTER_TYPE_P (TREE_TYPE (TREE_OPERAND (arg0, 0)))
+		&& POINTER_TYPE_P (TREE_TYPE (TREE_OPERAND (arg1, 0))))
+	      break;
+
 	    if (outprec >= BITS_PER_WORD
 		|| TRULY_NOOP_TRUNCATION (outprec, inprec)
 		|| inprec > TYPE_PRECISION (TREE_TYPE (arg0))
