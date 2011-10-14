e46924ca3e93aa6ecfff11a86f3cb546ffe018bf
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-10-14 14:39:32 +0000

    gcc/testsuite/ 2011-10-14  David Alan Gilbert  <david.gilbert@linaro.org>
    	* gcc.dg/di-longlong64-sync-1.c: New test.
    	* gcc.dg/di-sync-multithread.c: New test.
    	* gcc.target/arm/di-longlong64-sync-withhelpers.c: New test.
    	* gcc.target/arm/di-longlong64-sync-withldrexd.c: New test.
    	* lib/target-supports.exp: (arm_arch_*_ok): Series of effective-target
    	tests for v5, v6, v6k, and v7-a, and add-options helpers.
    	(check_effective_target_arm_arm_ok): New helper.
    	(check_effective_target_sync_longlong): New helper.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@179983 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/di-longlong64-sync-withhelpers.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target arm_arch_v5_ok } */
+/* { dg-options "-std=gnu99" } */
+/* { dg-add-options arm_arch_v5 } */
+/* { dg-message "note: '__sync_fetch_and_nand' changed semantics in GCC 4.4" "" { target *-*-* } 0 } */
+/* { dg-message "note: '__sync_nand_and_fetch' changed semantics in GCC 4.4" "" { target *-*-* } 0 } */
+/* { dg-message "file included" "In file included" { target *-*-* } 0 } */
+
+#include "../../gcc.dg/di-longlong64-sync-1.c"
+
+/* On an old ARM we have no ldrexd or strexd so we have to use helpers.  */
+/* { dg-final { scan-assembler-not "ldrexd" } } */
+/* { dg-final { scan-assembler-not "strexd" } } */
+/* { dg-final { scan-assembler "__sync_" } } */
