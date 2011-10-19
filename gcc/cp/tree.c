a675085e12b2419649db9ef2e766c24fcd0da4c1
Jason Merrill <jason@redhat.com>
2011-10-19 18:21:52 -0400

    re PR c++/50793 (G++ doesn't value-initialize all members of non-trivial type in default argument)
    	PR c++/50793
    	* tree.c (bot_manip): Propagate AGGR_INIT_ZERO_FIRST.
    
    From-SVN: r180225

--- gcc/cp/tree.c
+++ gcc/cp/tree.c
@@ -1840,7 +1840,11 @@ bot_manip (tree* tp, int* walk_subtrees, void* data)
       tree u;
 
       if (TREE_CODE (TREE_OPERAND (t, 1)) == AGGR_INIT_EXPR)
-	u = build_cplus_new (TREE_TYPE (t), TREE_OPERAND (t, 1));
+	{
+	  u = build_cplus_new (TREE_TYPE (t), TREE_OPERAND (t, 1));
+	  if (AGGR_INIT_ZERO_FIRST (TREE_OPERAND (t, 1)))
+	    AGGR_INIT_ZERO_FIRST (TREE_OPERAND (u, 1)) = true;
+	}
       else
 	u = build_target_expr_with_type (TREE_OPERAND (t, 1), TREE_TYPE (t));
 
