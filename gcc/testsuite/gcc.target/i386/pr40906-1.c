08810c7497e49fdb42b4009d185e87a9f6c85ffd
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-10-20 13:04:34 +0000

    * gcc.target/i386/pr40906-1.c, gcc.target/i386/pr40906-2.c, 	gcc.target/i386/pr46226.c: Do not use 	-mno-accumulate-outgoing-args.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@180252 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/pr40906-1.c
+++ gcc/testsuite/gcc.target/i386/pr40906-1.c
@@ -1,6 +1,7 @@
 /* { dg-do run } */
 /* { dg-require-effective-target ia32 } */
 /* { dg-options "-O2 -fomit-frame-pointer -fno-asynchronous-unwind-tables -mpush-args -mno-accumulate-outgoing-args" } */
+/* { dg-options "-O2 -fomit-frame-pointer -fno-asynchronous-unwind-tables -mpush-args" { target *-*-mingw* *-*-cygwin* } } */
 
 void abort (void);
 
