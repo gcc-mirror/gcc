b8fda5b0f3c3243b22d81bd839b099a29de5f784
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-10-24 18:04:37 +0000

    * gcc.target/i386/sse-5.c (dg-options): Add -mno-sse. 	Remove -march=i386. 	(dg-skip-if): Remove. 	* gcc.target/i386/funcspec-1.c: Ditto. 	* gcc.target/i386/funcspec-3.c (dg-options): Add -mno-sse3.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@180397 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/funcspec-1.c
+++ gcc/testsuite/gcc.target/i386/funcspec-1.c
@@ -3,10 +3,7 @@
    for a function that doesn't use attribute((option)).  */
 /* { dg-do compile } */
 /* { dg-require-effective-target ia32 } */
-/* { dg-skip-if "" { i?86-*-* x86_64-*-* } { "-march=*" } { "-march=i386" } } */
-/* { dg-options "-O3 -ftree-vectorize -march=i386" } */
-/* { dg-final { scan-assembler "addps\[ \t\]" } } */
-/* { dg-final { scan-assembler "fsubs\[ \t\]" } } */
+/* { dg-options "-O3 -ftree-vectorize -mno-sse" } */
 
 #ifndef SIZE
 #define SIZE 1024
@@ -33,3 +30,6 @@ i387_subnums (void)
   for (; i < SIZE; ++i)
     a[i] = b[i] - c[i];
 }
+
+/* { dg-final { scan-assembler "addps\[ \t\]" } } */
+/* { dg-final { scan-assembler "fsubs\[ \t\]" } } */
