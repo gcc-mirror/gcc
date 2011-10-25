6deac0c22c6b398bb1ef11378055fab03f98c555
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-10-25 13:48:10 +0000

    /cp 2011-10-25  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/50858
    	* typeck.c (composite_pointer_type_r): Check return value of
    	composite_pointer_type_r for error_mark_node.
    
    /testsuite
    2011-10-25  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/50858
    	* g++.dg/overload/operator5.C: New.
    
    /cp
    2011-10-25  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/50861
    	* pt.c (tsubst_copy_and_build): Check return value of
    	tsubst_copy_and_build for error_mark_node.
    
    /testsuite
    2011-10-25  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/50861
    	* g++.dg/template/crash108.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@180430 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/overload/operator5.C
@@ -0,0 +1,16 @@
+// PR c++/50858
+
+class String {
+   typedef struct ImplicitConversionFromWTFStringToBoolDisallowedA* 
+   (String::*UnspecifiedBoolTypeA);
+   typedef struct ImplicitConversionFromWTFStringToBoolDisallowedB* 
+   (String::*UnspecifiedBoolTypeB);
+   operator UnspecifiedBoolTypeA() const;
+   operator UnspecifiedBoolTypeB() const;
+};
+inline bool equalIgnoringCase(const String& a, const String& b) { }
+inline bool equalPossiblyIgnoringCase(const String& a, 
+                                      const String& b,
+                                      bool ignoreCase) {
+  return ignoreCase ? equalIgnoringCase(a, b) : (a == b); } // { dg-error "ambiguous" }
+// { dg-message "note" { target *-*-* } 15 }
