732a0ad314ba1603b9fd07199dd83518ed54ad4d
Jakub Jelinek <jakub@redhat.com>
2011-10-30 13:16:01 +0100

    tree-vectorizer.h (NUM_PATTERNS): Bump to 9.
    	* tree-vectorizer.h (NUM_PATTERNS): Bump to 9.
    	* tree-vect-patterns.c (vect_recog_vector_vector_shift_pattern): New
    	function.
    	(vect_vect_recog_func_ptrs): Add it.
    
    From-SVN: r180677

--- gcc/tree-vectorizer.h
+++ gcc/tree-vectorizer.h
@@ -902,7 +902,7 @@ extern void vect_slp_transform_bb (basic_block);
    Additional pattern recognition functions can (and will) be added
    in the future.  */
 typedef gimple (* vect_recog_func_ptr) (VEC (gimple, heap) **, tree *, tree *);
-#define NUM_PATTERNS 8
+#define NUM_PATTERNS 9
 void vect_pattern_recog (loop_vec_info);
 
 /* In tree-vectorizer.c.  */
