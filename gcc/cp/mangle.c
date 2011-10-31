596c1381757b7bbc4f9f4741026b301e16412c0c
Diego Novillo <dnovillo@google.com>
2011-10-31 10:04:19 -0400

    mangle.c (get_mangled_id): Factor from ...
    	* mangle.c (get_mangled_id): Factor from ...
    	(mangle_decl): ... here.
    	Call get_mangled_id.
    
    From-SVN: r180698

--- gcc/cp/mangle.c
+++ gcc/cp/mangle.c
@@ -3177,13 +3177,21 @@ mangle_decl_string (const tree decl)
   return result;
 }
 
+/* Return an identifier for the external mangled name of DECL.  */
+
+static tree
+get_mangled_id (tree decl)
+{
+  tree id = mangle_decl_string (decl);
+  return targetm.mangle_decl_assembler_name (decl, id);
+}
+
 /* Create an identifier for the external mangled name of DECL.  */
 
 void
 mangle_decl (const tree decl)
 {
-  tree id = mangle_decl_string (decl);
-  id = targetm.mangle_decl_assembler_name (decl, id);
+  tree id = get_mangled_id (decl);
   SET_DECL_ASSEMBLER_NAME (decl, id);
 
   if (G.need_abi_warning)
