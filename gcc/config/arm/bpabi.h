f4496f0fecea4060d1a6e036d80b092aa564f7f3
Andrew Stubbs <ams@codesourcery.com>
2011-11-01 21:57:39 +0000

    bpabi.h (BE8_LINK_SPEC): Recognize generic-armv7 tuning.
    2011-11-01  Andrew Stubbs  <ams@codesourcery.com>
    
    	* config/arm/bpabi.h (BE8_LINK_SPEC): Recognize generic-armv7 tuning.
    
    From-SVN: r180747

--- gcc/config/arm/bpabi.h
+++ gcc/config/arm/bpabi.h
@@ -58,6 +58,7 @@
 #define BE8_LINK_SPEC \
   " %{mbig-endian:%{march=armv7-a|mcpu=cortex-a5	\
    |mcpu=cortex-a8|mcpu=cortex-a9|mcpu=cortex-a15	\
+   |mcpu=generic-armv7-a				\
    |march=armv7-m|mcpu=cortex-m3			\
    |march=armv7e-m|mcpu=cortex-m4			\
    |march=armv6-m|mcpu=cortex-m0			\
