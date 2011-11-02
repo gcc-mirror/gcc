8fbf5688fb7314c59ccd2774fd16f8d91ef56dd4
Jason Merrill <jason@redhat.com>
2011-11-02 16:16:56 -0400

    parser.c (cp_parser_decl_specifier_seq): Change "C++0x" to "C++11" in warnings.
    	* parser.c (cp_parser_decl_specifier_seq): Change "C++0x" to
    	"C++11" in warnings.
    	(cp_lexer_get_preprocessor_token): Likewise.
    	(cp_parser_binary_expression): Likewise.
    
    From-SVN: r180795

--- gcc/testsuite/g++.dg/cpp0x/auto1.C
+++ gcc/testsuite/g++.dg/cpp0x/auto1.C
@@ -1,8 +1,8 @@
-// { dg-options "-std=c++98 -Wc++0x-compat" }
+// { dg-options "-std=c++98 -Wc++11-compat" }
 
-// Test warning for use of auto in C++98 mode with C++0x
+// Test warning for use of auto in C++98 mode with C++11
 // compatibility warnings
 void f()
 {
-  auto int x = 5; // { dg-warning "will change meaning" }
+  auto int x = 5; // { dg-warning "changes meaning" }
 }
