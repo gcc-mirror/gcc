e08cc018ac1ed9e201d064c52d0a378b7014e636
Jason Merrill <jason@redhat.com>
2011-11-02 17:24:56 -0400

    re PR c++/50930 ([C++0x] Valid brace-or-equal-initializer of non-static data member rejected)
    	PR c++/50930
    	* init.c (build_aggr_init): Don't set LOOKUP_ONLYCONVERTING
    	if the initializer has TARGET_EXPR_DIRECT_INIT_P.
    	(expand_default_init): An initializer with TARGET_EXPR_DIRECT_INIT_P
    	or TARGET_EXPR_LIST_INIT_P doesn't need more processing.
    	* tree.c (bot_manip): Propagate TARGET_EXPR_IMPLICIT_P,
    	TARGET_EXPR_LIST_INIT_P, TARGET_EXPR_DIRECT_INIT_P.
    	* call.c (convert_like_real): Set TARGET_EXPR_DIRECT_INIT_P
    	as appropriate on list-value-initialization.
    
    From-SVN: r180802

---
+++ gcc/testsuite/g++.dg/cpp0x/nsdmi-list2.C
@@ -0,0 +1,32 @@
+// PR c++/50930
+// { dg-options -std=c++0x }
+
+struct nmc {
+ nmc() = default;
+ nmc(nmc&&) = delete; // line 3
+};
+
+struct A { // line 6
+ nmc n{};
+ nmc n2 = {};
+} a; // line 8
+
+// ------
+
+struct lock_t {
+  int lock[4];
+};
+
+struct pthread_mutex_t {
+  volatile lock_t __spinlock;
+};
+
+struct mutex {
+  pthread_mutex_t m = { };
+  mutex() = default;
+};
+
+int main()
+{
+  mutex mx;
+}
