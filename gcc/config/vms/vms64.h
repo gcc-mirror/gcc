ba89fcb67207e6d364910572ab98ba165f5ad7dd
Tristan Gingold <gingold@adacore.com>
2011-11-07 09:03:20 +0000

    vms.h (TARGET_OBJECT_SUFFIX, [...]): Move to config/vms/vms.h
    2011-11-07  Tristan Gingold  <gingold@adacore.com>
    
    	* config/alpha/vms.h (TARGET_OBJECT_SUFFIX,
    	TARGET_EXECUTABLE_SUFFIX, TARGET_OS_CPP_BUILTINS,
    	TARGET_ABI_OPEN_VMS, LONG_TYPE_SIZE, ADA_LONG_TYPE_SIZE,
    	POINTER_SIZE, POINTERS_EXTEND_UNSIGNED): Move to config/vms/vms.h
    	(SUBTARGET_OS_CPP_BUILTINS): Define.
    	(TARGET_DEFAULT): Tune according to POINTER_SIZE.
    	(MASK_RETURN_ADDR): Define in 64 bit mode.
    	* config/ia64/vms.h: Likewise.
    	* config/vms/vms.h: New file.
    	* config/vms/vms64.h: New file.
    	* config/alpha/vms64.h: Removed.
    	* config/ia64/vms64.h: Removed.
    	* config.gcc (alpha64-dec-*vms*, alpha*-dec-*vms*, ia64-hp-*vms*):
    	Adjust for above change.
    
    From-SVN: r181078

--- gcc/config/vms/vms64.h
+++ gcc/config/vms/vms64.h
@@ -18,33 +18,11 @@ You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
 
-#undef TARGET_OS_CPP_BUILTINS
-#define TARGET_OS_CPP_BUILTINS()                \
-    do {                                        \
-        builtin_define_std ("vms");             \
-        builtin_define_std ("VMS");             \
-        builtin_define ("__ALPHA");             \
-        builtin_assert ("system=vms");          \
-        builtin_define ("__IEEE_FLOAT");        \
-        builtin_define ("__LONG_POINTERS=1");   \
-    } while (0)
-
-#undef SUBTARGET_SWITCHES
-#define SUBTARGET_SWITCHES \
-  { "malloc64",    MASK_MALLOC64,     "Malloc data into P2 space" },
-
-#undef TARGET_DEFAULT
-#define TARGET_DEFAULT (MASK_FPREGS | MASK_GAS | MASK_MALLOC64)
-
 #undef LONG_TYPE_SIZE
 #define LONG_TYPE_SIZE 64
 
 #undef POINTER_SIZE
 #define POINTER_SIZE 64
-
-/* Eventhough pointers are 64bits, only 32bit ever remain significant in code
-   addresses.  */
-#define MASK_RETURN_ADDR (GEN_INT (0xffffffff))
 
 /* Defaults to "long int" */
 #undef SIZE_TYPE
