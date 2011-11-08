b8bc0ff76b12959e1aefafda407c03604605728c
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2011-11-08 23:15:11 +0000

    re PR fortran/50409 (SIGSEGV in gfc_simplify_expr)
    	PR fortran/50409
    	* expr.c (gfc_simplify_expr): Substrings can't have negative
    	length.
    	* gcc/testsuite/gfortran.dg/string_5.f90: Improve testcase.
    
    From-SVN: r181181

--- gcc/testsuite/gfortran.dg/string_5.f90
+++ gcc/testsuite/gfortran.dg/string_5.f90
@@ -1,7 +1,14 @@
 ! { dg-do compile }
-! PR fortran/48876 - this used to segfault.
-! Test case contributed by mhp77 (a) gmx.at.
 program test
+
+  ! PR fortran/48876 - this used to segfault.
+  ! Test case contributed by mhp77 (a) gmx.at.
   character ::  string =  "string"( : -1 )
+
+  ! PR fortran/50409
+  character v(3)
+  v = (/ ('123'(i:1), i = 3, 1, -1) /) 
+  print *, v
+
 end program test
 
