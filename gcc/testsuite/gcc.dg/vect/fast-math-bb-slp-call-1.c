b57a47d0966219cd1647e3439f77cd48d6151bb2
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-11-08 12:09:57 +0000

    * tree-vect-stmts.c (vectorizable_call): Add SLP_NODE argument. 	Handle vectorization of SLP calls. 	(vect_analyze_stmt): Adjust caller, add call to it for SLP too. 	(vect_transform_stmt): Adjust vectorizable_call caller, remove 	assertion. 	* tree-vect-slp.c (vect_get_and_check_slp_defs): For calls start 	with op_idx 3. 	(vect_build_slp_tree): Allow CALL_EXPR.
    	* lib/target-supports.exp (check_effective_target_vect_call_sqrtf,
    	check_effective_target_vect_call_copysignf,
    	check_effective_target_vect_call_lrint): New procedures.
    	* gcc.dg/vect/vect.exp: Run fast-math-bb-slp* tests using
    	$VECT_SLP_CFLAGS with -ffast-math.
    	* gcc.dg/vect/fast-math-vect-call-1.c: New test.
    	* gcc.dg/vect/fast-math-vect-call-2.c: New test.
    	* gcc.dg/vect/fast-math-bb-slp-call-1.c: New test.
    	* gcc.dg/vect/fast-math-bb-slp-call-2.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@181157 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/fast-math-bb-slp-call-1.c
@@ -0,0 +1,49 @@
+#include "tree-vect.h"
+
+extern float copysignf (float, float);
+extern float sqrtf (float);
+extern float fabsf (float);
+extern void abort (void);
+float a[64], b[64], c[64], d[64];
+
+__attribute__((noinline, noclone)) void
+f1 (void)
+{
+  a[0] = copysignf (b[0], c[0]) + 1.0f + sqrtf (d[0]);
+  a[1] = copysignf (b[1], c[1]) + 2.0f + sqrtf (d[1]);
+  a[2] = copysignf (b[2], c[2]) + 3.0f + sqrtf (d[2]);
+  a[3] = copysignf (b[3], c[3]) + 4.0f + sqrtf (d[3]);
+  a[4] = copysignf (b[4], c[4]) + 5.0f + sqrtf (d[4]);
+  a[5] = copysignf (b[5], c[5]) + 6.0f + sqrtf (d[5]);
+  a[6] = copysignf (b[6], c[6]) + 7.0f + sqrtf (d[6]);
+  a[7] = copysignf (b[7], c[7]) + 8.0f + sqrtf (d[7]);
+}
+
+__attribute__((noinline, noclone)) int
+main1 ()
+{
+  int i;
+
+  for (i = 0; i < 8; i++)
+    {
+      asm ("");
+      b[i] = (i & 1) ? -4 * i : 4 * i;
+      c[i] = (i & 2) ? -8 * i : 8 * i;
+      d[i] = i * i;
+    }
+  f1 ();
+  for (i = 0; i < 8; i++)
+    if (fabsf (((i & 2) ? -4 * i : 4 * i) + 1 + i + i - a[i]) >= 0.0001f)
+      abort ();
+  return 0;
+}
+
+int
+main ()
+{
+  check_vect ();
+  return main1 ();
+}
+
+/* { dg-final { scan-tree-dump-times "basic block vectorized using SLP" 1 "slp" { target { vect_call_copysignf && vect_call_sqrtf } } } } */
+/* { dg-final { cleanup-tree-dump "slp" } } */
---
+++ gcc/testsuite/gcc.dg/vect/fast-math-bb-slp-call-2.c
@@ -0,0 +1,65 @@
+#include "tree-vect.h"
+
+extern long int lrint (double);
+extern void abort (void);
+long int a[64];
+double b[64];
+
+__attribute__((noinline, noclone)) void
+f1 (void)
+{
+  a[0] = lrint (b[0]) + 1;
+  a[1] = lrint (b[1]) + 2;
+  a[2] = lrint (b[2]) + 3;
+  a[3] = lrint (b[3]) + 4;
+  a[4] = lrint (b[4]) + 5;
+  a[5] = lrint (b[5]) + 6;
+  a[6] = lrint (b[6]) + 7;
+  a[7] = lrint (b[7]) + 8;
+}
+
+__attribute__((noinline, noclone)) void
+f2 (void)
+{
+  a[0] = lrint (b[0]);
+  a[1] = lrint (b[1]);
+  a[2] = lrint (b[2]);
+  a[3] = lrint (b[3]);
+  a[4] = lrint (b[4]);
+  a[5] = lrint (b[5]);
+  a[6] = lrint (b[6]);
+  a[7] = lrint (b[7]);
+}
+
+__attribute__((noinline, noclone)) int
+main1 ()
+{
+  int i;
+
+  for (i = 0; i < 8; i++)
+    {
+      asm ("");
+      b[i] = ((i & 1) ? -4 * i : 4 * i) + 0.25;
+    }
+  f1 ();
+  for (i = 0; i < 8; i++)
+    if (a[i] != ((i & 1) ? -4 * i : 4 * i) + 1 + i)
+      abort ();
+    else
+      a[i] = 131.25;
+  f2 ();
+  for (i = 0; i < 8; i++)
+    if (a[i] != ((i & 1) ? -4 * i : 4 * i))
+      abort ();
+  return 0;
+}
+
+int
+main ()
+{
+  check_vect ();
+  return main1 ();
+}
+
+/* { dg-final { scan-tree-dump-times "basic block vectorized using SLP" 2 "slp" { target vect_call_lrint } } } */
+/* { dg-final { cleanup-tree-dump "slp" } } */
