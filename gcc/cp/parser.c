57a74da57e54dfdf5bdf24f6c9bff287aec74e1a
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-11-09 22:25:12 +0000

    PR c++/51046 	* parser.c (cp_parser_range_for): check_for_bare_parameter_packs.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@181239 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/parser.c
+++ gcc/cp/parser.c
@@ -9292,6 +9292,8 @@ cp_parser_range_for (cp_parser *parser, tree scope, tree init, tree range_decl)
      at instantiation. If not, it is done just ahead. */
   if (processing_template_decl)
     {
+      if (check_for_bare_parameter_packs (range_expr))
+	range_expr = error_mark_node;
       stmt = begin_range_for_stmt (scope, init);
       finish_range_for_decl (stmt, range_decl, range_expr);
       if (!type_dependent_expression_p (range_expr)
