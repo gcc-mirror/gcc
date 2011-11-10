49fe93f41068a64691cb450cd5b9490dc81880ae
Jason Merrill <jason@redhat.com>
2011-11-10 15:28:16 -0500

    PR c++/51079, DR 495
    	PR c++/51079, DR 495
    	* call.c (joust): Check the second conversion sequence
    	before checking templates.
    
    From-SVN: r181270

---
+++ gcc/testsuite/g++.dg/template/conv12.C
@@ -0,0 +1,25 @@
+// PR c++/51079
+
+#if __cplusplus > 199711L
+struct C1
+{
+  template <class T>
+  operator T() = delete;	// { dg-message "declared here" "" { target c++11 } }
+  operator bool() { return false; }
+} c1;
+
+int ic1 = c1;			// { dg-error "deleted" "" { target c++11 } }
+int ac1 = c1 + c1;		// { dg-error "deleted" "" { target c++11 } }
+#endif
+
+struct C2
+{
+private:
+  template <class T>
+  operator T();			// { dg-error "private" }
+public:
+  operator bool() { return false; }
+} c2;
+
+int ic2 = c2;			// { dg-error "" }
+int ac2 = c2 + c2;		// { dg-error "" }
