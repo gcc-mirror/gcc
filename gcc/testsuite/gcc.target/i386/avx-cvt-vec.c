e35525e9b44270dd312522094fe82246c572f034
Uros Bizjak <ubizjak@gmail.com>
2011-11-15 20:40:04 +0100

    sse2-cvt-vec.c: New test.
    	* gcc.target/i386/sse2-cvt-vec.c: New test.
    	* gcc.target/i386/avx-cvt-vec.c: Ditto.
    	* gcc.target/i386/avx-cvt-2-vec.c: Ditto.
    
    From-SVN: r181390

---
+++ gcc/testsuite/gcc.target/i386/avx-cvt-vec.c
@@ -0,0 +1,9 @@
+/* { dg-do run } */
+/* { dg-options "-O2 -ffast-math -ftree-vectorize -mavx" } */
+/* { dg-require-effective-target avx } */
+/* { dg-skip-if "no M_PI" { vxworks_kernel } } */
+
+#define CHECK_H "avx-check.h"
+#define TEST avx_test
+
+#include "sse2-cvt-vec.c"
