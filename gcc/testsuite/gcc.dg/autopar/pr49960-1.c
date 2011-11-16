c417d87da338158a7c343e30474d9490c0a8c612
razya <razya@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-11-16 14:58:04 +0000

    PR tree-optimization/49960
    	* gcc.dg/autopar/pr49960.c: New test.
    	* gcc.dg/autopar/pr49960-1.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@181409 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/autopar/pr49960-1.c
@@ -0,0 +1,36 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -ftree-parallelize-loops=4 -fdump-tree-parloops-details -fdump-tree-optimized" } */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() 
+{
+  unsigned int x, y, idx, H = 1024, W = 1024;
+  
+  int * tmps = (int *)malloc(H*W*sizeof(int));
+  
+  /* This loop gets parallelized even though output dependences exist 
+     between writes to 'tmps' that prevent parallelization. 
+     For example: tmps[1] = 1, ..., tmps[1] = 17.  */
+  
+  for(x = 1; x < H; x++) 
+    {
+      for(y = 1; y < W; y++) 
+	{
+	  idx = x*W+y;
+	  tmps[idx % 4096] = idx;	  
+	}
+    }
+  
+  for(x = 1; x < 8; x++)
+    printf("tmps[%d]=%d\n", x, tmps[x]);
+  
+  return 0;
+}
+/* Check that no loop gets parallelized.  */
+
+/* { dg-final { scan-tree-dump-times "SUCCESS: may be parallelized" 0 "parloops" } } */
+/* { dg-final { scan-tree-dump-times "loopfn" 0 "optimized" } } */
+/* { dg-final { cleanup-tree-dump "parloops" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
