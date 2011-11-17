d846e425c90211f07c13797880a4a0cd94879440
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2011-11-17 11:34:33 +0000

    Provide all dummy functions in alloc_cpp.cc, eh_cpp.cc
    	* alloc_cpp.cc [__osf__] (_ZnwX, _ZdlPv, _ZnaX, _ZdaPv,
    	_ZnwXRKSt9nothrow_t, _ZdlPvRKSt9nothrow_t, _ZdaPvRKSt9nothrow_t):
    	Dummy functions.
    	* eh_cpp.cc [__osf__] (__cxa_allocate_exception, __cxa_throw,
    	__cxa_begin_catch, __cxa_end_catch, __cxa_tm_cleanup): Likewise.
    
    From-SVN: r181442

--- libitm/alloc_cpp.cc
+++ libitm/alloc_cpp.cc
@@ -61,11 +61,15 @@ extern void *_ZnaXRKSt9nothrow_t (size_t, c_nothrow_p) __attribute__((weak));
 extern void _ZdaPvRKSt9nothrow_t (void *, c_nothrow_p) __attribute__((weak));
 
 #ifdef __osf__ /* Really: !HAVE_WEAKDEF  */
-void *
-_ZnaXRKSt9nothrow_t (size_t, c_nothrow_p)
-{
-  return NULL;
-}
+void *_ZnwX (size_t) { return NULL; }
+void _ZdlPv (void *) { return; }
+void *_ZnaX (size_t) { return NULL; }
+void _ZdaPv (void *) { return; }
+
+void *_ZnwXRKSt9nothrow_t (size_t, c_nothrow_p) { return NULL; }
+void _ZdlPvRKSt9nothrow_t (void *, c_nothrow_p) { return; }
+void *_ZnaXRKSt9nothrow_t (size_t, c_nothrow_p) { return NULL; }
+void _ZdaPvRKSt9nothrow_t (void *, c_nothrow_p) { return; }
 #endif /* __osf__ */
 
 /* Wrap the delete nothrow symbols for usage with a single argument.
