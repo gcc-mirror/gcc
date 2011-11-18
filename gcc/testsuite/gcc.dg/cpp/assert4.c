708b09dce53662f3b92e7187ff0945002793366b
Joseph Myers <joseph@codesourcery.com>
2011-11-18 22:28:06 +0000

    * gcc.dg/cpp/assert4.c: Test __linux__, not __gnu_linux__.
    From-SVN: r181497

--- gcc/testsuite/gcc.dg/cpp/assert4.c
+++ gcc/testsuite/gcc.dg/cpp/assert4.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2003, 2006, 2008 Free Software Foundation, Inc.
+/* Copyright (C) 2003, 2006, 2008, 2009, 2011 Free Software Foundation, Inc.
    Test builtin preprocessor assertions.
    By Kaveh Ghazi <ghazi@caip.rutgers.edu>.  */
 
@@ -7,7 +7,7 @@
 
 /* Check for #system assertions.  */
 
-#if defined __gnu_linux__
+#if defined __linux__
 # if !#system(linux) || !#system(unix) || !#system(posix)
 #  error
 # endif
