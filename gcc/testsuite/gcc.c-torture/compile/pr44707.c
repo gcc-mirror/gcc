4a0051e4db55a7de184bc51f9d2a03b352d16420
Ulrich Weigand <uweigand@de.ibm.com>
2011-11-18 18:17:05 +0000

    pr44707.c: Do not assume assembler supports /* ...
    	* gcc.c-torture/compile/pr44707.c: Do not assume assembler
    	supports /* ... */ comments.
    
    From-SVN: r181489

--- gcc/testsuite/gcc.c-torture/compile/pr44707.c
+++ gcc/testsuite/gcc.c-torture/compile/pr44707.c
@@ -9,6 +9,6 @@ foo (void)
   int e3 = v.b;
   int e4 = v.c;
   int e5 = v.d;
-  __asm__ volatile ("/* %0 %1 %2 %3 %4 */" : : "nro" (e1), "nro" (e2), "nro" (e3), "nro" (e4), "nro" (e5));
+  __asm__ volatile ("" : : "nro" (e1), "nro" (e2), "nro" (e3), "nro" (e4), "nro" (e5));
 }
 
