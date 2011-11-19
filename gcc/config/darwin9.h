40353fb3aa64e07ac3dd27fc2306b5c5010b91d2
Iain Sandoe <iains@gcc.gnu.org>
2011-11-19 12:21:08 +0000

    darwin.h (ASM_DEBUG_SPEC): New.
    gcc:
    
    	* config/darwin.h (ASM_DEBUG_SPEC): New.
    	* config/darwin9.h (ASM_DEBUG_SPEC): New.
    
    From-SVN: r181507

--- gcc/config/darwin9.h
+++ gcc/config/darwin9.h
@@ -41,6 +41,12 @@ along with GCC; see the file COPYING3.  If not see
      %{mdynamic-no-pic: %n'-mdynamic-no-pic' overrides '-pie', '-fpie' or '-fPIE'; \
       :-pie}}"
 
+/* Only ask as for debug data if the debug style is stabs (since as doesn't
+   yet generate dwarf.)  */
+
+#undef  ASM_DEBUG_SPEC
+#define ASM_DEBUG_SPEC  "%{g*:%{!g0:%{gstabs:--gstabs}}}"
+
 #undef  ASM_OUTPUT_ALIGNED_COMMON
 #define ASM_OUTPUT_ALIGNED_COMMON(FILE, NAME, SIZE, ALIGN)		\
   do {									\
