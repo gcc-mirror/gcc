cf54716442d585e0f3988470a9596e006adec6ed
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2011-11-21 14:28:49 +0000

    Adapt c-c++-common/tm/malloc.c for Solaris headers with C++
    	* c-c++-common/tm/malloc.c: Scan tree dumps for std::malloc if
    	*-*-solaris2* && c++.
    
    From-SVN: r181579

--- gcc/testsuite/c-c++-common/tm/malloc.c
+++ gcc/testsuite/c-c++-common/tm/malloc.c
@@ -17,7 +17,9 @@ void foobar(void)
     z = (char *)malloc (666);
 }
 
-/* { dg-final { scan-tree-dump-times " malloc .666" 1 "tmmark" } } */
+/* { dg-final { scan-tree-dump-times " malloc .666" 1 "tmmark" { target { ! { *-*-solaris2* && c++ } } } } } */
+/* Solaris 2 headers are C++-aware and declare std::malloc.  */
+/* { dg-final { scan-tree-dump-times " std::malloc .666" 1 "tmmark" { target { *-*-solaris2* && c++ } } } } */
 /* { dg-final { scan-tree-dump-times "__builtin__ITM_malloc" 1 "tmmark" } } */
 /* { dg-final { scan-tree-dump-times "__builtin__ITM_calloc" 1 "tmmark" } } */
 /* { dg-final { scan-tree-dump-times "__builtin__ITM_free" 2 "tmmark" } } */
