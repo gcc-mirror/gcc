b06c89b674a97ae989872ee46c47e3bc9e81f8cc
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-11-21 19:27:30 +0000

    PR c++/50958 gcc/cp/ 	* parser.c (lookup_literal_operator): New. 	(cp_parser_userdef_char_literal): Use it. 	(cp_parser_userdef_numeric_literal): Use it. 	(cp_parser_userdef_string_literal): Use lookup_name. libcpp/ 	* expr.c (cpp_userdef_char_remove_type): Fix typo.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@181595 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/udlit-raw-length.C
@@ -0,0 +1,27 @@
+// { dg-options "-std=c++0x" }
+// PR c++/50958
+
+typedef decltype(sizeof(0)) size_type;
+
+constexpr size_type
+cstrlen_impl(const char* s, size_type i)
+{
+  return s[i] ? cstrlen_impl(s, i + 1) : i;
+}
+
+constexpr size_type
+cstrlen(const char* s)
+{
+  return s ? cstrlen_impl(s, 0) : throw 0;
+}
+
+constexpr size_type
+operator "" _lenraw(const char* digits)
+{
+  return cstrlen(digits);
+}
+
+static_assert(123_lenraw == 3, "Ouch");
+static_assert(1_lenraw == 1, "Ouch");
+static_assert(012_lenraw == 3, "Ouch");
+static_assert(0_lenraw == 1, "Ouch");
