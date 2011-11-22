bcd29ab77bf374a644f309d1225817ca0d7f2aef
iains <iains@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-11-22 08:50:40 +0000

    gcc/testsuite:
    	* gcc.misc-tests/gcov-16.c: Adjust scan-assembler for Darwin.
    	* gcc.misc-tests/gcov-17.c: Likewise.
    	* g++.dg/gcov/gcov-8.C: Likewise.
    	* g++.dg/gcov/gcov-9.C: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@181610 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/gcov/gcov-8.C
+++ gcc/testsuite/g++.dg/gcov/gcov-8.C
@@ -10,4 +10,5 @@ void X::Fink ()
 {
 }
 
-/* { dg-final { scan-assembler "\\.hidden\t__gcov___ZN1X4FinkEv" } } */
+/* { dg-final { scan-assembler "\\.hidden\t__gcov___ZN1X4FinkEv" { target { ! *-*-darwin*  } } } } */
+/* { dg-final { scan-assembler "\\.private_extern ___gcov___ZN1X4FinkEv" { target *-*-darwin* } } } */
