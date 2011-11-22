0c082b203756d2f9fc166019190a12c1bc50157c
Iain Sandoe <iains@gcc.gnu.org>
2011-11-22 08:50:40 +0000

    gcov-16.c: Adjust scan-assembler for Darwin.
    gcc/testsuite:
    
    	* gcc.misc-tests/gcov-16.c: Adjust scan-assembler for Darwin.
    	* gcc.misc-tests/gcov-17.c: Likewise.
    	* g++.dg/gcov/gcov-8.C: Likewise.
    	* g++.dg/gcov/gcov-9.C: Likewise.
    
    From-SVN: r181610

--- gcc/testsuite/g++.dg/gcov/gcov-9.C
+++ gcc/testsuite/g++.dg/gcov/gcov-9.C
@@ -10,6 +10,8 @@ extern "C" void (*Foo ()) ()
   return Boo;
 }
 
-/* { dg-final { scan-assembler "\\.hidden\t__gcov___Z3Boov" } } */
+/* { dg-final { scan-assembler "\\.hidden\t__gcov___Z3Boov" { target { ! *-*-darwin*  } } } } */
+/* { dg-final { scan-assembler "\\.private_extern ___gcov___Z3Boov" { target *-*-darwin* } } } */
 /* { dg-final { scan-assembler "__gcov__Foo:" } } */
-/* { dg-final { scan-assembler-not "\\.hidden\t__gcov__Foo" } } */
+/* { dg-final { scan-assembler-not "\\.hidden\t__gcov__Foo" { target { ! *-*-darwin*  } } } } */
+/* { dg-final { scan-assembler-not "\\.private_extern ___gcov__Foo" { target *-*-darwin* } } } */
--- gcc/testsuite/gcc.misc-tests/gcov-17.c
+++ gcc/testsuite/gcc.misc-tests/gcov-17.c
@@ -8,4 +8,5 @@ void __attribute__ ((visibility ("hidden"), weak)) Foo ()
 {
 }
 
-/* { dg-final { scan-assembler "\\.hidden\t__gcov__Foo" } } */
+/* { dg-final { scan-assembler "\\.hidden\t__gcov__Foo" { target { ! *-*-darwin*  } } } } */
+/* { dg-final { scan-assembler "\\.private_extern ___gcov__Foo" { target *-*-darwin* } } } */
