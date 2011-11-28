7f785f51cfb35d2a2c0cc7abb3b330eb206184e7
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-11-28 13:24:23 +0000

    2011-11-28  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR libstdc++/51288
    	* include/std/iomanip (get_money, put_money): Use sentry.
    	* testsuite/27_io/manipulators/extended/get_money/char/51288.cc: New.
    	* testsuite/27_io/manipulators/extended/get_money/wchar_t/51288.cc:
    	Likewise.
    	* testsuite/27_io/manipulators/extended/put_money/char/51288.cc:
    	Likewise.
    	* testsuite/27_io/manipulators/extended/put_money/wchar_t/51288.cc:
    	Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@181775 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/27_io/manipulators/extended/put_money/wchar_t/51288.cc
@@ -0,0 +1,49 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-require-namedlocale "en_US.UTF-8" }
+
+// Copyright (C) 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <sstream>
+#include <iomanip>
+#include <testsuite_hooks.h>
+
+// libstdc++/51288
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  std::locale loc_us = std::locale("en_US.UTF-8");
+
+  std::wostringstream oss;
+  oss.imbue(loc_us);
+
+  const std::wstring str(L"123");
+
+  oss.setstate(std::ios_base::failbit);
+
+  oss << std::put_money(str);
+
+  VERIFY( oss.str().empty() );
+  VERIFY( oss.fail() );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
