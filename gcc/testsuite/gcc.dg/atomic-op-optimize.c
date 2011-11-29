004ca39da1a62c5ab5c17ea584010a001bdc40f7
amacleod <amacleod@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-11-29 19:42:44 +0000

    2011-11-29  Andrew MacLeod  <amacleod@redhat.com>
    	PR target/50123
    	* optabs.c (maybe_optimize_fetch_op): New.  Look for more optimal
    	instructions for a FECTH_OP or OP_FECTH sequence.
    	(expand_atomic_fetch_op): Call maybe_optimize_fetch_op.
    	* testsuite/gcc.dg/atomic-op-optimize.c: New.  Test for optimizations.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@181815 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/atomic-op-optimize.c
@@ -0,0 +1,20 @@
+/* Both these atomic operations should be optimized to an exchange operation.
+   Test that it at happens on x86 by making sure there are 2 xchg's and no
+   compare_exchange loop.  */
+
+/* { dg-require-effective-target sync_int_long } */
+/* { dg-do compile { target { i?86-*-* x86_64-*-* } } } */
+/* { dg-final { scan-assembler-times "cmpxchg" 0 } } */
+/* { dg-final { scan-assembler-times "xchg" 2 } } */
+
+int x;
+
+int f()
+{
+  return __atomic_fetch_and (&x, 0, __ATOMIC_RELAXED);
+}
+
+int g()
+{
+  return __atomic_fetch_or (&x, -1, __ATOMIC_RELAXED);
+}
