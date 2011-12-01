a95888bb35e78b1c256a840d2adaf56bf8a82a1d
Paolo Carlini <paolo.carlini@oracle.com>
2011-12-01 09:34:43 +0000

    re PR c++/51367 (Broken diagnostic: 'pointer_type' not supported by dump_expr)
    /cp
    2011-12-01  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/51367
    	* pt.c (unify_inconsistency): Use either %qT or %qE depending on
    	whether parm is a type or non-type parameter.
    
    /cp
    2011-12-01  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/51367
    	* g++.dg/template/error47.C: New.
    
    From-SVN: r181876

---
+++ gcc/testsuite/g++.dg/template/error47.C
@@ -0,0 +1,9 @@
+// PR c++/51367
+
+template<typename T> void foo(T, T); // { dg-message "template" }
+
+void bar(void* p)
+{
+  foo(0, p); // { dg-error "no matching" }
+}
+// { dg-message "candidate|parameter 'T' ('int' and 'void*')" { target *-*-* } 7 }
