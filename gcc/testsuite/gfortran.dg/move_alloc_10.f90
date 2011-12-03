fde50fe6afbf70fc1dfeb1f0c69e404c894bfd2c
Tobias Burnus <burnus@net-b.de>
2011-12-03 12:03:30 +0100

    check.c (gfc_check_move_alloc): Allow nonpolymorphic FROM with polymorphic TO.
    2011-12-03  Tobias Burnus  <burnus@net-b.de>
    
            * check.c (gfc_check_move_alloc): Allow nonpolymorphic
            FROM with polymorphic TO.
            * trans-intrinsic.c (conv_intrinsic_move_alloc): Handle
            nonpolymorphic FROM with polymorphic TO.
    
    2011-12-03  Tobias Burnus  <burnus@net-b.de>
    
            * gfortran.dg/select_type_23.f03: Revert Rev. 181801,
            i.e. remove the dg-error line.
            * gfortran.dg/move_alloc_5.f90: Ditto and change back
            to dg-do run.
            * gfortran.dg/move_alloc_9.f90: New.
            * gfortran.dg/move_alloc_10.f90: New
    
    From-SVN: r181966

---
+++ gcc/testsuite/gfortran.dg/move_alloc_10.f90
@@ -0,0 +1,79 @@
+! { dg-do run }
+!
+! Test move_alloc for polymorphic scalars
+!
+! The following checks that a move_alloc from
+! a TYPE to a CLASS works
+!
+module myalloc
+  implicit none
+
+  type :: base_type
+     integer :: i  =2
+  end type base_type
+
+  type, extends(base_type) :: extended_type
+     integer :: j = 77
+  end type extended_type
+contains
+  subroutine myallocate (a)
+    class(base_type), allocatable, intent(inout) :: a
+    type(extended_type), allocatable :: tmp
+
+   allocate (tmp)
+
+   if (tmp%i /= 2 .or. tmp%j /= 77) call abort()
+   tmp%i = 5
+   tmp%j = 88
+
+   select type(a)
+     type is(base_type)
+       if (a%i /= -44) call abort()
+       a%i = -99
+     class default
+       call abort ()
+   end select
+
+   call move_alloc (from=tmp, to=a)
+
+   select type(a)
+     type is(extended_type)
+       if (a%i /= 5) call abort()
+       if (a%j /= 88) call abort()
+       a%i = 123
+       a%j = 9498
+     class default
+       call abort ()
+   end select
+
+   if (allocated (tmp)) call abort()
+  end subroutine myallocate
+end module myalloc
+
+program main
+  use myalloc
+  implicit none
+  class(base_type), allocatable :: a
+
+  allocate (a)
+
+  select type(a)
+    type is(base_type)
+      if (a%i /= 2) call abort()
+      a%i = -44
+    class default
+      call abort ()
+  end select
+
+  call myallocate (a)
+
+  select type(a)
+    type is(extended_type)
+      if (a%i /= 123) call abort()
+      if (a%j /= 9498) call abort()
+    class default
+      call abort ()
+  end select
+end program main
+
+! { dg-final { cleanup-modules "myalloc" } }
--- gcc/testsuite/gfortran.dg/select_type_23.f03
+++ gcc/testsuite/gfortran.dg/select_type_23.f03
@@ -3,10 +3,6 @@
 ! PR 48699: [OOP] MOVE_ALLOC inside SELECT TYPE
 !
 ! Contributed by Salvatore Filippone <sfilippone@uniroma2.it>
-!
-! Note that per Fortran 2008, 8.1.9.2, "within the block following
-! a TYPE IS type guard statement, the associating entity (16.5.5) is not polymorphic"
-!
 
 program testmv2
 
@@ -20,7 +16,7 @@ program testmv2
 
   select type(sm2) 
   type is (bar)
-    call move_alloc(sm2,sm) ! { dg-error "must be either both polymorphic or both nonpolymorphic" }
+    call move_alloc(sm2,sm)
   end select
 
 end program testmv2
