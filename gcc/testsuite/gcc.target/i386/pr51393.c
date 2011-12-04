067c17b293998a31c1b57e624a99b8108d697f22
Uros Bizjak <uros@gcc.gnu.org>
2011-12-04 14:23:48 +0100

    re PR target/51393 (Wrong parameter type for _mm256_insert_epi64 in avxintrin.h)
    	PR target/51393
    	* config/i386/avxintrin.h (_mm256_insert_epi64): Declare second
    	parameter as long long.
    
    testsuite/ChangeLog:
    
    	PR target/51393
    	* gcc.target/i386/pr51393.c: New test.
    
    From-SVN: r181988

---
+++ gcc/testsuite/gcc.target/i386/pr51393.c
@@ -0,0 +1,22 @@
+/* { dg-do run } */
+/* { dg-require-effective-target avx } */
+/* { dg-require-effective-target lp64 } */
+/* { dg-options "-O -mavx" } */
+
+#include "avx-check.h"
+#include <immintrin.h>
+
+static void
+__attribute__((noinline))
+avx_test (void)
+{
+  long long in = 0x800000000ll;
+  long long out;
+
+  __m256i zero = _mm256_setzero_si256();
+  __m256i tmp  = _mm256_insert_epi64 (zero, in, 0);
+  out = _mm256_extract_epi64(tmp, 0);
+
+  if (in != out)
+    abort ();
+}
