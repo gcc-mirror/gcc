671b487dc35fcde6a5f611e0714b419953c4c3ed
Thomas Koenig <tkoenig@gcc.gnu.org>
2011-12-05 20:11:44 +0000

    re PR fortran/51338 (seg fault in gfc_dep_compare_expr with -O2)
    2011-12-05  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/51338
    	* dependency.c (are_identical_variables):  Handle case where
    	end fields of substring references are NULL.
    
    2011-12-05  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/51338
    	* gfortran.dg/assumed_charlen_substring_1.f90:  New test.
    
    From-SVN: r182024

---
+++ gcc/testsuite/gfortran.dg/assumed_charlen_substring_1.f90
@@ -0,0 +1,10 @@
+! { dg-do compile }
+! { dg-options "-O -fdump-tree-original" }
+! PR 51338 - this used to ICE.
+! Original test case by Bud Davis.
+subroutine foo(a,b)
+  character(len=*) :: a
+  if (a(1:) /= a(1:)) call do_not_use
+end subroutine foo
+! { dg-final { scan-tree-dump-times "do_not_use" 0 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
