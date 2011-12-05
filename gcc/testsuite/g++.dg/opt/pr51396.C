5ed3d3b86cc6b24082dfe76a20b3ce81db57f6f5
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-12-05 20:56:14 +0000

    PR tree-optimization/51396 	* tree-ssa-math-opts.c (convert_mult_to_fma): Don't optimize 	if MUL_RESULT has zero uses.
    	* g++.dg/opt/pr51396.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182028 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/opt/pr51396.C
@@ -0,0 +1,24 @@
+// PR tree-optimization/51396
+// { dg-do compile }
+// { dg-options "-O2 -fnon-call-exceptions -mfma" }
+// { dg-options "-O2 -fnon-call-exceptions -mfma" { target i?86-*-* x86_64-*-* } }
+
+double baz (double) throw ();
+
+struct C
+{
+  C (double d = 0.0) : c (d) {}
+  double c;
+};
+
+static inline void
+foo (double x, const C &y)
+{
+  x ? (y.c * baz (x)) : (C (), y);
+}
+
+void
+bar (double x, C y)
+{
+  foo (x, y);
+}
