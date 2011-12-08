e11c67d07882cd920df5357ef8612e9c3cae5fea
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-12-08 09:44:57 +0000

    * include/bits/atomic_base.h (__calculate_memory_order): Rename to... 	(__cmpexch_failure_order): This, and rewrite as constexpr function. 	(compare_exchange_strong, compare_exchange_weak): Use it. 	* include/std/atomic (compare_exchange_strong, compare_exchange_weak): 	Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182105 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/atomic_base.h
+++ libstdc++-v3/include/bits/atomic_base.h
@@ -59,14 +59,12 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       memory_order_seq_cst
     } memory_order;
 
-  inline memory_order
-  __calculate_memory_order(memory_order __m) noexcept
+  // Drop release ordering as per [atomics.types.operations.req]/21
+  constexpr memory_order
+  __cmpexch_failure_order(memory_order __m) noexcept
   {
-    const bool __cond1 = __m == memory_order_release;
-    const bool __cond2 = __m == memory_order_acq_rel;
-    memory_order __mo1(__cond1 ? memory_order_relaxed : __m);
-    memory_order __mo2(__cond2 ? memory_order_acquire : __mo1);
-    return __mo2;
+    return __m == memory_order_acq_rel ? memory_order_acquire
+      : __m == memory_order_release ? memory_order_relaxed : __m;
   }
 
   inline void
@@ -505,7 +503,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 			    memory_order __m = memory_order_seq_cst) noexcept
       {
 	return compare_exchange_weak(__i1, __i2, __m,
-				     __calculate_memory_order(__m));
+				     __cmpexch_failure_order(__m));
       }
 
       bool
@@ -513,7 +511,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 		   memory_order __m = memory_order_seq_cst) volatile noexcept
       {
 	return compare_exchange_weak(__i1, __i2, __m,
-				     __calculate_memory_order(__m));
+				     __cmpexch_failure_order(__m));
       }
 
       bool
@@ -544,7 +542,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 			      memory_order __m = memory_order_seq_cst) noexcept
       {
 	return compare_exchange_strong(__i1, __i2, __m,
-				       __calculate_memory_order(__m));
+				       __cmpexch_failure_order(__m));
       }
 
       bool
@@ -552,7 +550,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 		 memory_order __m = memory_order_seq_cst) volatile noexcept
       {
 	return compare_exchange_strong(__i1, __i2, __m,
-				       __calculate_memory_order(__m));
+				       __cmpexch_failure_order(__m));
       }
 
       __int_type
