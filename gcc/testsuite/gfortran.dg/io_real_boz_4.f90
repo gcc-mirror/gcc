14dcdf69d57b85cc5926162da7699f4846bb3faf
Tobias Burnus <burnus@net-b.de>
2011-12-08 19:54:27 +0100

    re PR libfortran/51407 (Use of BOZ edit descriptors to edit real and complex data and F2008)
    2011-12-08  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/51407
            * io/transfer.c (require_numeric_type): New function.
            (formatted_transfer_scalar_read, formatted_transfer_scalar_write):
            Use it, allow BOZ edit descriptors with F2008.
    
    2011-12-08  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/51407
            * gfortran.dg/io_real_boz_3.f90: New.
            * gfortran.dg/io_real_boz_4.f90: New.
            * gfortran.dg/io_real_boz_5.f90: New.
    
    From-SVN: r182132

---
+++ gcc/testsuite/gfortran.dg/io_real_boz_4.f90
@@ -0,0 +1,15 @@
+! { dg-do  run }
+! { dg-options "-std=f2003" }
+!
+! PR fortran/51407
+!
+! Valid in F2008, but in F95/F2003:
+! { dg-output "Expected INTEGER for item 1 in formatted transfer, got REAL" }
+! { dg-shouldfail "Only F2003: BOZ edit with REAL" }
+!
+       real(kind=16) :: x
+       character(len=32) :: str1
+       x = 1.0_16 + 2.0_16**(-105)
+       write (str1,'(z32)') x
+       write (str1,'(z0)') x
+       end
