de44e2b0d694d2d0f596cdb2cd88624106650ed5
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-12-10 20:42:55 +0000

    Let GET_MODE_2XWIDER_MODE find a double-width vector mode.
            * genmodes.c (struct mode_data): Remove wider_2x member.
            (blank_mode): Adjust initializer.
            (calc_wider_mode): Use XALLOCAVEC.
            (emit_move_wider): Select double-width same-element vectors for
            2xwider vectors.
            * machmode.h (GET_MODE_2XWIDER_MODE): Update documentation.
            * config/i386/i386.c (doublesize_vector_mode): Remove.
            (expand_vselect_vconcat): Use GET_MODE_2XWIDER_MODE instead.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182191 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/genmodes.c
+++ gcc/genmodes.c
@@ -63,7 +63,6 @@ struct mode_data
 
   struct mode_data *component;	/* mode of components */
   struct mode_data *wider;	/* next wider mode */
-  struct mode_data *wider_2x;	/* 2x wider mode */
 
   struct mode_data *contained;  /* Pointer to list of modes that have
 				   this mode as a component.  */
@@ -83,7 +82,7 @@ static struct mode_data *void_mode;
 static const struct mode_data blank_mode = {
   0, "<unknown>", MAX_MODE_CLASS,
   -1U, -1U, -1U, -1U,
-  0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0,
   "<unknown>", 0, 0, 0, 0
 };
 
@@ -790,7 +789,7 @@ calc_wider_mode (void)
 
   /* Allocate max_n_modes + 1 entries to leave room for the extra null
      pointer assigned after the qsort call below.  */
-  sortbuf = (struct mode_data **) alloca ((max_n_modes + 1) * sizeof (struct mode_data *));
+  sortbuf = XALLOCAVEC (struct mode_data *, max_n_modes + 1);
 
   for (c = 0; c < MAX_MODE_CLASS; c++)
     {
@@ -804,7 +803,6 @@ calc_wider_mode (void)
 	  for (prev = 0, m = modes[c]; m; m = next)
 	    {
 	      m->wider = void_mode;
-	      m->wider_2x = void_mode;
 
 	      /* this is nreverse */
 	      next = m->next;
@@ -826,7 +824,6 @@ calc_wider_mode (void)
 	  sortbuf[i] = 0;
 	  for (j = 0; j < i; j++)
 	    sortbuf[j]->next = sortbuf[j]->wider = sortbuf[j + 1];
-
 
 	  modes[c] = sortbuf[0];
 	}
@@ -1059,6 +1056,21 @@ emit_mode_wider (void)
 	  else
 	    {
 	      if (m2->precision != (unsigned int) -1)
+		continue;
+	    }
+
+	  /* For vectors we want twice the number of components,
+	     with the same element type.  */
+	  if (m->cl == MODE_VECTOR_INT
+	      || m->cl == MODE_VECTOR_FLOAT
+	      || m->cl == MODE_VECTOR_FRACT
+	      || m->cl == MODE_VECTOR_UFRACT
+	      || m->cl == MODE_VECTOR_ACCUM
+	      || m->cl == MODE_VECTOR_UACCUM)
+	    {
+	      if (m2->ncomponents != 2 * m->ncomponents)
+		continue;
+	      if (m->component != m2->component)
 		continue;
 	    }
 
