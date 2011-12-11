5948668b79dab3441bf3b3186916424ce95dd938
tkoenig <tkoenig@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-12-11 17:43:22 +0000

    2011-12-11  Thomas Koenig  <tkoenig@gcc.gnu.org>
    	PR fortran/50690
    	* frontend-passes.c (in_omp_workshare):  New variable.
    	(cfe_expr_0):  Don't eliminiate common function if it would put
    	the variable immediately into a WORKSHARE construct.
    	(optimize_namespace):  Set in_omp_workshare.
    	(gfc_code_walker):  Keep track of OMP PARALLEL and OMP WORKSHARE
    	constructs.
    
    2011-12-11  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/50690
    	* gfortran.dg/gomp/workshare2.f90:  New test.
    	* gfortran.dg/gomp/workshare3.f90:  New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182208 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/gomp/workshare2.f90
@@ -0,0 +1,53 @@
+! { dg-do compile }
+! { dg-options "-fopenmp -ffrontend-optimize -fdump-tree-original" }
+! PR 50690 - this used to ICE because workshare could not handle
+! BLOCKs.
+! To test for correct execution, run this program (but don't forget
+! to unset the stack limit).
+program foo
+  implicit none
+  integer, parameter :: n = 10000000
+  real, parameter :: eps = 3e-7
+  integer :: i,j
+  real :: A(n), B(5), C(n)
+  real :: tmp
+  B(1) = 3.344
+  tmp = B(1)
+  do i=1,10
+     call random_number(a)
+     c = a
+     !$omp parallel default(shared)
+     !$omp workshare
+     A(:) = A(:)*cos(B(1))+A(:)*cos(B(1))
+     !$omp end workshare nowait
+     !$omp end parallel ! sync is implied here
+  end do
+
+  c = c*tmp + c*tmp
+
+  do j=1,n
+     if (abs(a(j)-c(j)) > eps) then
+        print *,1,j,a(j), c(j)
+        call abort
+     end if
+  end do
+
+  do i=1,10
+     call random_number(a)
+     c = a
+     !$omp parallel workshare default(shared)
+     A(:) = A(:)*cos(B(1))+A(:)*cos(B(1))
+     !$omp end parallel workshare
+  end do
+
+  c = c*tmp + c*tmp
+  do j=1,n
+     if (abs(a(j)-c(j)) > eps) then
+        print *,2,j,a(j), c(j)
+        call abort
+     end if
+  end do
+
+end program foo
+! { dg-final { scan-tree-dump-times "__var" 0 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
