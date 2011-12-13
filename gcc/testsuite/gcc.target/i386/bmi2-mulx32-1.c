9170437b040010ea0d5cad5a8d73f0307117fd13
Uros Bizjak <uros@gcc.gnu.org>
2011-12-13 16:00:09 +0100

    re PR testsuite/51524 ([BMI2] New regression on 182266 vs 182257)
    	PR testsuite/51524
    	* gcc.target/i386/bmi2-mulx32-1.c (gen_mulx): Add attribute regparm(2).
    	* gcc.target/i386/bmi2-mulx32-2.c (calc_mulx_u32): Ditto.
    
    From-SVN: r182287

--- gcc/testsuite/gcc.target/i386/bmi2-mulx32-1.c
+++ gcc/testsuite/gcc.target/i386/bmi2-mulx32-1.c
@@ -15,7 +15,7 @@ calc_mul_u32 (unsigned volatile a, unsigned b)
   return res;
 }
 
-__attribute__((noinline))
+__attribute__((noinline, regparm (2)))
 unsigned long long
 gen_mulx (unsigned a, unsigned b)
 {
