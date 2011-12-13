cde465d0c76dfa9a394a232ffb9d311ed57e76ed
Andreas Krebbel <Andreas.Krebbel@de.ibm.com>
2011-12-13 20:19:34 +0000

    regmove.c (fixup_match_2): Only access call_used_regs with hard regs.
    2011-12-13  Andreas Krebbel  <Andreas.Krebbel@de.ibm.com>
    
    	* regmove.c (fixup_match_2): Only access call_used_regs with hard
    	regs.
    
    From-SVN: r182305

--- gcc/regmove.c
+++ gcc/regmove.c
@@ -860,7 +860,7 @@ fixup_match_2 (rtx insn, rtx dst, rtx src, rtx offset)
 	  if (REG_N_CALLS_CROSSED (REGNO (src)) == 0)
 	    break;
 
-	  if (call_used_regs [REGNO (dst)]
+	  if ((HARD_REGISTER_P (dst) && call_used_regs [REGNO (dst)])
 	      || find_reg_fusage (p, CLOBBER, dst))
 	    break;
 	}
