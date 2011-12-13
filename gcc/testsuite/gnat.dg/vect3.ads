ba3125d7800b1b7f968ebdfa24e4b8ec262f6b1c
Eric Botcazou <ebotcazou@adacore.com>
2011-12-13 11:26:14 +0000

    vect1.ad[sb]: Add more tests.
    	* gnat.dg/vect1.ad[sb]: Add more tests.
    	* gnat.dg/vect2.ad[sb]: Likewise.
    	* gnat.dg/vect3.ad[sb]: Likewise.
    	* gnat.dg/vect4.ad[sb]: Likewise.
    	* gnat.dg/vect5.ad[sb]: Likewise.
    	* gnat.dg/vect6.ad[sb]: Likewise.
    
    From-SVN: r182276

--- gcc/testsuite/gnat.dg/vect3.ads
+++ gcc/testsuite/gnat.dg/vect3.ads
@@ -8,6 +8,7 @@ package Vect3 is
    for Varray'Alignment use 16;
 
    function "+" (X, Y : Varray) return Varray;
+   procedure Add (X, Y : Varray; R : out Varray);
    procedure Add (X, Y : not null access Varray; R : not null access Varray);
 
 
@@ -17,6 +18,7 @@ package Vect3 is
    for Sarray'Alignment use 16;
 
    function "+" (X, Y : Sarray) return Sarray;
+   procedure Add (X, Y : Sarray; R : out Sarray);
    procedure Add (X, Y : not null access Sarray; R : not null access Sarray);
 
 
@@ -24,6 +26,7 @@ package Vect3 is
    for Darray1'Alignment use 16;
 
    function "+" (X, Y : Darray1) return Darray1;
+   procedure Add (X, Y : Darray1; R : out Darray1);
    procedure Add (X, Y : not null access Darray1; R : not null access Darray1);
 
 
@@ -31,6 +34,7 @@ package Vect3 is
    for Darray2'Alignment use 16;
 
    function "+" (X, Y : Darray2) return Darray2;
+   procedure Add (X, Y : Darray2; R : out Darray2);
    procedure Add (X, Y : not null access Darray2; R : not null access Darray2);
 
 
@@ -38,6 +42,7 @@ package Vect3 is
    for Darray3'Alignment use 16;
 
    function "+" (X, Y : Darray3) return Darray3;
+   procedure Add (X, Y : Darray3; R : out Darray3);
    procedure Add (X, Y : not null access Darray3; R : not null access Darray3);
 
 end Vect3;
