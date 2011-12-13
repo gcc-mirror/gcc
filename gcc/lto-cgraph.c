949e5786db04eee7fe3feec227d76e6c54d56c3a
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-12-13 21:59:39 +0000

    * lto-streamer-out.c (write_symbol): Use proper 64-bit host type. 	* lto-cgraph.c (input_cgraph_opt_section): Use 'int' for offsets. 	* lto-streamer-in.c (lto_read_body): Likewise. 	(lto_input_toplevel_asms): Likewise. 	* lto-section-in.c (lto_create_simple_input_block): Likewise. 	* ipa-inline-analysis.c (inline_read_section): Likewise. 	* ipa-prop.c (ipa_prop_read_section): Likewise.
    	* df.h (DF_NOTE): Fix typo in comment.
    lto/
    	* lto.h (lto_parse_hex): Delete.
    	* lto.c (lto_read_decls): Use 'int' for offsets.
    	(lto_parse_hex): Make static and return proper 64-bit host type.
    	(lto_resolution_read): Use proper 64-bit host type.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182310 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/lto-cgraph.c
+++ gcc/lto-cgraph.c
@@ -1689,9 +1689,9 @@ input_cgraph_opt_section (struct lto_file_decl_data *file_data,
 {
   const struct lto_function_header *header =
     (const struct lto_function_header *) data;
-  const int32_t cfg_offset = sizeof (struct lto_function_header);
-  const int32_t main_offset = cfg_offset + header->cfg_size;
-  const int32_t string_offset = main_offset + header->main_size;
+  const int cfg_offset = sizeof (struct lto_function_header);
+  const int main_offset = cfg_offset + header->cfg_size;
+  const int string_offset = main_offset + header->main_size;
   struct data_in *data_in;
   struct lto_input_block ib_main;
   unsigned int i;
--- gcc/lto-section-in.c
+++ gcc/lto-section-in.c
@@ -224,7 +224,7 @@ lto_create_simple_input_block (struct lto_file_decl_data *file_data,
     = (const struct lto_simple_header *) data;
 
   struct lto_input_block* ib_main;
-  int32_t main_offset = sizeof (struct lto_simple_header);
+  int main_offset = sizeof (struct lto_simple_header);
 
   if (!data)
     return NULL;
