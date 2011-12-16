1b14bc389b0afb30293836bc15951772480d26c2
gingold <gingold@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-12-16 15:25:30 +0000

    2011-12-16  Tristan Gingold  <gingold@adacore.com>
    	* config/vms/vms.c (VMS_CRTL_GLOBAL): Define.
    	(vms_patch_builtins): Handle.
    	* config/vms/vms-crtlmap.map: Add an entry for environ.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182405 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/vms/vms.c
+++ gcc/config/vms/vms.c
@@ -45,6 +45,9 @@ along with GCC; see the file COPYING3.  If not see
 /* Prepend x before the name for printf like functions.  */
 #define VMS_CRTL_PRNTF	(1 << 4)
 
+/* Prepend ga_ for global data.  */
+#define VMS_CRTL_GLOBAL (1 << 5)
+
 struct vms_crtl_name
 {
   /* The standard C name.  */
@@ -121,6 +124,12 @@ vms_patch_builtins (void)
         {
           memcpy (res + rlen, "__bsd44__", 9);
           rlen += 9;
+        }
+
+      if (n->flags & VMS_CRTL_GLOBAL)
+        {
+          memcpy (res + rlen, "ga_", 3);
+          rlen += 3;
         }
 
       if (n->flags & VMS_CRTL_FLOAT)
