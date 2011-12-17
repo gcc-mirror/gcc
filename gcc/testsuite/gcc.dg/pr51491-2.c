a23864731ccfa6b6675c4726b4eb8e5dc4153af7
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-12-17 11:39:49 +0000

    2011-12-17  Tom de Vries  <tom@codesourcery.com>
    	PR tree-optimization/51491
    	* gcc.dg/pr51491.c: New test.
    	* gcc.dg/pr51491-2.c: Same.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182433 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr51491-2.c
@@ -0,0 +1,34 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-ccp1" } */
+
+int g (int *);
+
+int
+f (int n)
+{
+  int tt = 0;
+  int t = 4;
+  {
+    int a[t
+          + (tt != 0 ? 6 : 0)
+         ];
+    tt = g (a);
+    {
+      int b[n];
+      tt += g (b);
+      if (n > 20)
+	tt += 148 * g (b);
+      tt += b[0];
+    }
+    tt += a[0];
+  }
+  {
+    int a[4];
+    tt += g (a);
+    tt += a[0];
+  }
+  return tt;
+}
+
+/* { dg-final { scan-tree-dump-times "CLOBBER" 2 "ccp1"} } */
+/* { dg-final { cleanup-treee-dump "ccp1" } } */
