74051031aaae0833759d30e3519bcea03f41f2e0
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-12-19 05:57:52 +0000

    PR c++/51489 	* semantics.c (cxx_eval_outermost_constant_expr): Check for 	conversion from pointer to integer here. 	(cxx_eval_constant_expression) [NOP_EXPR]: Not here.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182470 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-ptrsub.C
@@ -0,0 +1,14 @@
+// PR c++/51489
+// DR 1313
+// { dg-options "-std=c++0x" }
+
+struct array
+{
+  constexpr array() :x(0) {}
+  constexpr int const* begin() { return &x; }
+  int x;
+};
+constexpr array aa;
+constexpr auto b = aa.begin();
+static_assert(b-b == 0, "compiles just fine");
+static_assert(aa.begin()-aa.begin() == 0, "compiler thinks it's not a constant expression");
