da3507d0a40849173957fda7937209b204ef6814
Richard Sandiford <rdsandiford@googlemail.com>
2011-12-19 21:42:12 +0000

    ext-2.c: Add NOMIPS16.
    gcc/testsuite/
    	* gcc.target/mips/ext-2.c: Add NOMIPS16.
    
    From-SVN: r182501

--- gcc/testsuite/gcc.target/mips/ext-2.c
+++ gcc/testsuite/gcc.target/mips/ext-2.c
@@ -7,7 +7,7 @@
 /* { dg-final { scan-assembler-not "and" } } */
 /* { dg-final { scan-assembler-not "srl" } } */
 
-void
+NOMIPS16 void
 f (unsigned char x, unsigned char *r)
 {
   *r = 0x50 | (x >> 4);
