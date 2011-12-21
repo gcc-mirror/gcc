b200cc3fccb049b5335c3e9310b4ab7cf4b0075c
Richard Guenther <rguenther@suse.de>
2011-12-21 09:22:58 +0000

    re PR lto/41159 ([LTO] ICE in insert_value_copy_on_edge, at tree-outof-ssa.c:225)
    2011-12-21  Richard Guenther  <rguenther@suse.de>
    
    	PR lto/41159
    	* tree-outof-ssa.c (insert_value_copy_on_edge): Use the
    	mode of the pseudo as destination mode.  Only assert that
    	is equal to the promoted mode of the decl if it is a REG.
    
    From-SVN: r182570

--- gcc/tree-outof-ssa.c
+++ gcc/tree-outof-ssa.c
@@ -237,9 +237,10 @@ insert_value_copy_on_edge (edge e, int dest, tree src, source_location locus)
 
   var = SSA_NAME_VAR (partition_to_var (SA.map, dest));
   src_mode = TYPE_MODE (TREE_TYPE (src));
-  dest_mode = promote_decl_mode (var, &unsignedp);
+  dest_mode = GET_MODE (SA.partition_to_pseudo[dest]);
   gcc_assert (src_mode == TYPE_MODE (TREE_TYPE (var)));
-  gcc_assert (dest_mode == GET_MODE (SA.partition_to_pseudo[dest]));
+  gcc_assert (!REG_P (SA.partition_to_pseudo[dest])
+	      || dest_mode == promote_decl_mode (var, &unsignedp));
 
   if (src_mode != dest_mode)
     {
