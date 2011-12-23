5bf4dcf26d92bf18a53032378517e0365ba58385
Dmitry Plotnikov <dplotnikov@ispras.ru>
2011-12-23 13:53:17 +0000

    neon.md (float<mode><V_cvtto>2): New.
    2011-12-23  Dmitry Plotnikov  <dplotnikov@ispras.ru>
    
    	* config/arm/neon.md (float<mode><V_cvtto>2): New.
    	(floatuns<mode><V_cvtto>2): New.
    	(fix_trunc<mode><V_cvtto>2): New.
    	(fix_truncuns<mode><V_cvtto>2): New.
    	* config/arm/iterators.md (V_cvtto): New iterator.
    
    gcc/testsuite/:
    	* gcc.target/arm/vect-vcvt.c: New test.
    	* gcc.target/arm/vect-vcvtq.c: New test.
    	
    	* gcc.dg/vect/fast-math-pr35982.c: Added vect_strided2 alternative
    	in final check.
    	
    	* lib/target-supports.exp (check_effective_target_vect_intfloat_cvt):
    	True for ARM NEON.
    	(check_effective_target_vect_uintfloat_cvt): Likewise.
    	(check_effective_target_vect_floatuint_cvt): Likewise.
    	(check_effective_target_vect_floatint_cvt): Likewise.
    
    From-SVN: r182657

---
+++ gcc/testsuite/gcc.target/arm/neon/vect-vcvtq.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target arm_neon_ok } */
+/* { dg-options "-O2 -ftree-vectorize -fdump-tree-vect-details" } */
+/* { dg-add-options arm_neon } */
+
+#define N 32
+
+int ib[N] = {0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45};
+float fa[N];
+int ia[N];
+
+int convert()
+{
+  int i;
+
+  /* int -> float */
+  for (i = 0; i < N; i++)
+    fa[i] = (float) ib[i];
+
+  /* float -> int */
+  for (i = 0; i < N; i++)
+    ia[i] = (int) fa[i];
+
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "vectorized 2 loops" 1 "vect" } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
