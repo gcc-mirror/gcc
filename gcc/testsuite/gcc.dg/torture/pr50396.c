a2b6106f8a25a6ed55ca8ac8c557b69540fcf97a
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-12-23 09:10:18 +0000

    2011-12-23  Richard Guenther  <rguenther@suse.de>
    	PR rtl-optimization/50396
    	* simplify-rtx.c (simplify_binary_operation_1): Properly
    	guard code that only works for integers.
    
    	* gcc.dg/torture/pr50396.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182653 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/pr50396.c
@@ -0,0 +1,21 @@
+/* { dg-do run } */
+
+extern void abort (void);
+typedef float vf128 __attribute__((vector_size(16)));
+typedef float vf64 __attribute__((vector_size(8)));
+int main()
+{
+#if !__FINITE_MATH_ONLY__
+#if __FLT_HAS_QUIET_NAN__
+  vf128 v = (vf128){ 0.f, 0.f, 0.f, 0.f };
+  vf64 u = (vf64){ 0.f, 0.f };
+  v = v / (vf128){ 0.f, 0.f, 0.f, 0.f };
+  if (v[0] == v[0])
+    abort ();
+  u = u / (vf64){ 0.f, 0.f };
+  if (u[0] == u[0])
+    abort ();
+#endif
+#endif
+  return 0;
+}
