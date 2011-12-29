d8e5eb5711e236c0304becf5bd0ee28dfc3f67d5
Thomas Koenig <tkoenig@gcc.gnu.org>
2011-12-29 09:20:01 +0000

    dependency.c (gfc_dep_compare_functions): Document new behavior for REALs and complex.
    2011-12-29  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	* dependency.c (gfc_dep_compare_functions):  Document
    	new behavior for REALs and complex.  Add comment to cases
    	where only INTEGERs are handled.  Compare REAL and COMPLEX
    	constants, returning 0 and -2 only.  Add assert to make
    	sure that only integer constants are compared.
    
    2011-12-29  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	* gfortran.dg/function_optimize_9.f90:  New test.
    
    From-SVN: r182720

---
+++ gcc/testsuite/gfortran.dg/function_optimize_9.f90
@@ -0,0 +1,23 @@
+! { dg-do compile }
+! { dg-options "-O -fdump-tree-original" }
+program main
+  integer, parameter :: n=100
+  real, parameter :: pi=4*atan(1.)
+  real, parameter :: tmax=20.
+  real, parameter :: dt = tmax/(2*pi)/real(n)
+  real, parameter :: t0 = dt/30.
+  integer :: i
+  interface
+     pure function purefunc(x)
+       real :: purefunc
+       real, intent(in) :: x
+     end function purefunc
+  end interface
+  real :: a(n)
+  do i=1,n
+     a(i) = purefunc(dt*i + t0) * 3. + 2 * purefunc(t0 + i*dt) 
+  end do
+  print *,a
+end program main
+! { dg-final { scan-tree-dump-times "purefunc" 1 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
