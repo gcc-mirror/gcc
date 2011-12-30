f32fd5aaecb0673c9cdaf5a4f8fe6bbe1e9049a7
Paolo Carlini <paolo.carlini@oracle.com>
2011-12-30 11:40:37 +0000

    re PR libstdc++/51711 (regex.h contains incorrect code)
    2011-12-30  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR libstdc++/51711
    	* include/bits/regex.h (regex_replace): Fix thinko.
    	* testsuite/28_regex/algorithms/regex_replace/char/51711.cc: New.
    	* testsuite/28_regex/algorithms/regex_replace/wchar_t/51711.cc:
    	Likewise.
    
    From-SVN: r182740

---
+++ libstdc++-v3/testsuite/28_regex/algorithms/regex_replace/wchar_t/51711.cc
@@ -0,0 +1,31 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-do compile }
+
+// Copyright (C) 2011 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+//
+
+#include <regex>
+#include <string>
+
+// libstdc++/51711
+void test01()
+{
+  std::wstring toProcess(L"Bug\r\n");
+  std::wstring result __attribute__((unused))
+    = std::regex_replace(toProcess, std::wregex(L"\\r"), std::wstring(L"\\r"));
+}
