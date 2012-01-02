4274534954373f2368191fde5db823b160bc7074
Dodji Seketeli <dodji@redhat.com>
2012-01-02 17:08:45 +0000

    PR debug/49951 - jumpy stepping at end of scope in C++
    gcc/cp/
    
    	PR debug/49951
    	* decl.c (cxx_maybe_build_cleanup): Don't set location of the call
    	to the destructor.
    
    gcc/testsuite/
    
    	PR debug/49951
    	* g++.dg/gcov/gcov-2.C: Adjust.
    
    From-SVN: r182807

--- gcc/cp/decl.c
+++ gcc/cp/decl.c
@@ -13363,8 +13363,17 @@ cxx_maybe_build_cleanup (tree decl)
 	cleanup = call;
     }
 
+  /* build_delete sets the location of the destructor call to the
+     current location, even though the destructor is going to be
+     called later, at the end of the current scope.  This can lead to
+     a "jumpy" behaviour for users of debuggers when they step around
+     the end of the block.  So let's unset the location of the
+     destructor call instead.  */
+  if (cleanup != NULL && EXPR_P (cleanup))
+    SET_EXPR_LOCATION (cleanup, UNKNOWN_LOCATION);
   return cleanup;
 }
+
 
 /* When a stmt has been parsed, this function is called.  */
 
