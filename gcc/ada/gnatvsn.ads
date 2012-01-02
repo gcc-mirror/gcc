6a521d169d02f1ebb86373b58bf1ce122aa1440b
Eric Botcazou <ebotcazou@adacore.com>
2012-01-02 10:30:54 +0000

    * gnatvsn.ads (Current_Year): Bump to 2012.
    From-SVN: r182783

--- gcc/ada/gnatvsn.ads
+++ gcc/ada/gnatvsn.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---          Copyright (C) 1992-2011, Free Software Foundation, Inc.         --
+--          Copyright (C) 1992-2012, Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -92,7 +92,7 @@ package Gnatvsn is
    Verbose_Library_Version : constant String := "GNAT Lib v" & Library_Version;
    --  Version string stored in e.g. ALI files
 
-   Current_Year : constant String := "2011";
+   Current_Year : constant String := "2012";
    --  Used in printing copyright messages
 
 end Gnatvsn;
