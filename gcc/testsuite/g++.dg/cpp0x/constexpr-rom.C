1888d4758fd52f022e2dec9ee32e493d77439cd5
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-01-02 11:03:43 +0000

    gcc/testsuite/ 	* g++.dg/cpp0x/constexpr-rom.C: Look for .rdata rather than rodata 	for MIPS.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182791 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp0x/constexpr-rom.C
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-rom.C
@@ -1,6 +1,7 @@
 // PR c++/49673: check that test_data goes into .rodata
 // { dg-options -std=c++0x }
-// { dg-final { scan-assembler "rodata" { target { *-*-linux-gnu || *-*-elf } } } }
+// { dg-final { scan-assembler "\\.rdata" { target mips*-*-* } } }
+// { dg-final { scan-assembler "rodata" { target { { *-*-linux-gnu || *-*-elf } && { ! mips*-*-* } } } } }
 
 struct Data
 {
