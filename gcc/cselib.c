a4f436ffbb6a31d9aa56e4bebb7ccd9f7cf3bfe5
Jakub Jelinek <jakub@redhat.com>
2012-01-02 23:17:02 +0100

    re PR bootstrap/51725 (segfault in stage 3 when compiling gcc/opts.c for sparc64-linux)
    	PR bootstrap/51725
    	* cselib.c (add_mem_for_addr): Call canonical_cselib_val
    	on mem_elt first.
    
    From-SVN: r182819

--- gcc/cselib.c
+++ gcc/cselib.c
@@ -1,7 +1,7 @@
 /* Common subexpression elimination library for GNU compiler.
    Copyright (C) 1987, 1988, 1989, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011
-   Free Software Foundation, Inc.
+   1999, 2000, 2001, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011,
+   2012 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
@@ -1263,6 +1263,8 @@ static void
 add_mem_for_addr (cselib_val *addr_elt, cselib_val *mem_elt, rtx x)
 {
   struct elt_loc_list *l;
+
+  mem_elt = canonical_cselib_val (mem_elt);
 
   /* Avoid duplicates.  */
   for (l = mem_elt->locs; l; l = l->next)
