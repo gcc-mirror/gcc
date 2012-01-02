5f595f167555e127f24d3defedf431b06209bdfc
Richard Sandiford <rdsandiford@googlemail.com>
2012-01-02 11:33:35 +0000

    re PR target/51729 (dspr2-MULT.c and dspr2-MULTU.c fail for MIPS)
    gcc/testsuite/
    	PR target/51729
    	* gcc.target/mips/dspr2-MULT.c: Remove -ffixed-hi -ffixed-lo.
    	XFAIL.
    	* gcc.target/mips/dspr2-MULTU.c: Likewise.
    
    From-SVN: r182793

--- gcc/testsuite/gcc.target/mips/dspr2-MULTU.c
+++ gcc/testsuite/gcc.target/mips/dspr2-MULTU.c
@@ -1,11 +1,12 @@
 /* Test MIPS32 DSP REV 2 MULTU instruction.  Tune for a CPU that has
    pipelined multu.  */
 /* { dg-do compile } */
-/* { dg-options "-mgp32 -mdspr2 -O2 -ffixed-hi -ffixed-lo -mtune=74kc" } */
+/* { dg-options "-mgp32 -mdspr2 -O2 -mtune=74kc" } */
 
+/* See PR target/51729 for the reason behind the XFAILs.  */
 /* { dg-final { scan-assembler "\tmultu\t" } } */
-/* { dg-final { scan-assembler "ac1" } } */
-/* { dg-final { scan-assembler "ac2" } } */
+/* { dg-final { scan-assembler "ac1" { xfail *-*-* } } } */
+/* { dg-final { scan-assembler "ac2" { xfail *-*-* } } } */
 
 typedef unsigned long long a64;
 
