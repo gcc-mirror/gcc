008e4576df7c0f17c75aaf53406c3bbceb8b5626
Richard Guenther <rguenther@suse.de>
2012-01-03 13:19:28 +0000

    backport: re PR tree-optimization/50162 (Wrong vectorization)
    2012-01-03  Richard Guenther  <rguenther@suse.de>
     
    	Backport from mainline
    	2011-08-23  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/50162
    	* tree-vect-stmts.c (vectorizable_call): Fix argument lookup.
    
    From-SVN: r182843

--- gcc/tree-vect-stmts.c
+++ gcc/tree-vect-stmts.c
@@ -1416,7 +1416,7 @@ vectorizable_call (gimple stmt, gimple_stmt_iterator *gsi, gimple *vec_stmt)
 		}
 	      else
 		{
-		  vec_oprnd1 = gimple_call_arg (new_stmt, 2*i);
+		  vec_oprnd1 = gimple_call_arg (new_stmt, 2*i + 1);
 		  vec_oprnd0
 		    = vect_get_vec_def_for_stmt_copy (dt[i], vec_oprnd1);
 		  vec_oprnd1
