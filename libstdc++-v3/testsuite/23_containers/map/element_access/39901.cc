91b6856f80f2b4e0f539f3a8fd0a7c631896aecc
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-01-03 20:25:16 +0000

    /gcc/cp 2012-01-03  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/51738
    	* parser.c (cp_parser_postfix_open_square_expression): Handle
    	postfix-expression [ braced-init-list ].
    
    /gcc/testsuite
    2012-01-03  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/51738
    	* g++.dg/cpp0x/initlist-postfix-open-square.C: New.
    
    /libstdc++-v3
    2012-01-03  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/51738
    	* testsuite/23_containers/map/element_access/39901.cc: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182856 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/23_containers/map/element_access/39901.cc
@@ -0,0 +1,42 @@
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2012 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <map>
+#include <testsuite_hooks.h>
+
+// c++/39901
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  std::map<std::pair<int, int>, int> the_map;
+
+  the_map[{0, 1}] = 5;
+  VERIFY( (the_map.size() == 1) );
+  VERIFY( (the_map[{0, 1}] == 5) );
+
+  VERIFY( (the_map[{0, 0}] == 0) );
+  VERIFY( (the_map.size() == 2) );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
