958ea92a938a58c1d43c5499a3503ffd7d713154
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-01-03 12:07:18 +0000

    2012-01-03  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/51070
    	* tree-loop-distribution.c (stmt_has_scalar_dependences_outside_loop):
    	Properly handle calls.
    
    	* gcc.dg/torture/pr51070-2.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182839 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/pr51070-2.c
@@ -0,0 +1,35 @@
+/* { dg-do compile } */
+/* { dg-options "-fno-inline" } */
+
+int
+func_4 (int si1, int si2)
+{
+  return si1;
+}
+
+int
+func_14 (int left, int right)
+{
+  return 1;
+}
+
+int
+func_37 (int left, int right)
+{
+  return left;
+}
+
+int g_92[1024];
+int g_95[1024];
+int g_224;
+int g_352[1024];
+int
+func_9 ()
+{
+  for (; g_224; g_224 += 1)
+    {
+      g_95[0] = func_4 (func_37 (g_92[g_224], 0), 0);
+      g_92[g_224] = 0, g_352[g_224] = func_14 (0, 0);
+    }
+  return 0;
+}
