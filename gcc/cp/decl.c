73a98f369b8c571f1f158e0e0889209af5294cdd
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-01-03 09:33:33 +0000

    /cp 2012-01-03  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/15867
    	* decl.c (duplicate_decls): With -Wredundant-decls don't warn for
    	declaration followed by specialization.
    
    /testsuite
    2012-01-03  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/15867
    	* g++.dg/warn/Wredundant-decls-spec.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182833 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/decl.c
+++ gcc/cp/decl.c
@@ -1698,7 +1698,10 @@ duplicate_decls (tree newdecl, tree olddecl, bool newdecl_is_friend)
 	  /* Don't warn about extern decl followed by definition.  */
 	  && !(DECL_EXTERNAL (olddecl) && ! DECL_EXTERNAL (newdecl))
 	  /* Don't warn about friends, let add_friend take care of it.  */
-	  && ! (newdecl_is_friend || DECL_FRIEND_P (olddecl)))
+	  && ! (newdecl_is_friend || DECL_FRIEND_P (olddecl))
+	  /* Don't warn about declaration followed by specialization.  */
+	  && (! DECL_TEMPLATE_SPECIALIZATION (newdecl)
+	      || DECL_TEMPLATE_SPECIALIZATION (olddecl)))
 	{
 	  warning (OPT_Wredundant_decls, "redundant redeclaration of %qD in same scope", newdecl);
 	  warning (OPT_Wredundant_decls, "previous declaration of %q+D", olddecl);
