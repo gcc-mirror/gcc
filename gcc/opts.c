7ba6572377d8cc69471fc77f37c1880e808c449c
Aldy Hernandez <aldyh@redhat.com>
2012-01-04 14:53:30 +0000

    re PR middle-end/51212 (ICE: verify_flow_info failed: BB 3 can not throw but has an EH edge with -fgnu-tm -fnon-call-exceptions and transaction_callable)
            PR middle-end/51212
            * opts.c (finish_options): Sorry out when using transactional
            memory and non-call exceptions.
            * doc/invoke.texi (C Dialect Options): Document it.
    
    From-SVN: r182877

--- gcc/opts.c
+++ gcc/opts.c
@@ -666,6 +666,9 @@ finish_options (struct gcc_options *opts, struct gcc_options *opts_set,
   if (opts->x_flag_tm && opts->x_flag_non_call_exceptions)
     sorry ("transactional memory is not supported with non-call exceptions");
 
+  if (opts->x_flag_tm && opts->x_flag_non_call_exceptions)
+    sorry ("transactional memory is not supported with non-call exceptions");
+
   /* -Wmissing-noreturn is alias for -Wsuggest-attribute=noreturn.  */
   if (opts->x_warn_missing_noreturn)
     opts->x_warn_suggest_attribute_noreturn = true;
