637533b5d9c3428f489246d9fbde6b17d36be7b0
tkoenig <tkoenig@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-01-04 11:51:37 +0000

    2012-01-04  Thomas Koenig  <tkoenig@gcc.gnu.org>
    	PR fortran/49693
    	* trans-common.c (create_common): Update copyright years.  Mark
    	variables as used to avoid warnings about unused variables in
    	common blocks.
    
    2012-01-04  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/49693
    	* gfortran.dg/common_17.f90:  New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@182869 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/common_17.f90
@@ -0,0 +1,11 @@
+! { dg-do compile }
+! { dg-options "-Wall" }
+! PR fortran/49693 - this used to cause a spurious warning for the
+! variable in the common block.
+! Test case by Stephan Kramer.
+module foo
+  implicit none
+  integer:: a, b
+  common a
+end module foo
+! { dg-final { cleanup-modules "foo" } }
