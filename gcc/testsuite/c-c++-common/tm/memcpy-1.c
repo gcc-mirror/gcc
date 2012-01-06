4ee2f8308a5ff5205738cda31d3dd4e2b1427148
Patrick Marlier <patrick.marlier@gmail.com>
2012-01-06 21:58:44 +0000

    re PR testsuite/51655 (FAIL: c-c++-common/tm/memcpy-1.c (test for excess errors))
    PR testsuite/51655
            * c-c++-common/tm/memcpy-1.c: Declare memcpy instead of
            including <string.h>.
    
    From-SVN: r182972

--- gcc/testsuite/c-c++-common/tm/memcpy-1.c
+++ gcc/testsuite/c-c++-common/tm/memcpy-1.c
@@ -1,6 +1,10 @@
 /* { dg-do compile } */
 /* { dg-options "-fgnu-tm" } */
-#include <string.h>
+typedef __SIZE_TYPE__ size_t;
+#ifdef __cplusplus
+extern "C"
+#endif
+void *memcpy (void *__restrict, const void *__restrict, size_t);
 
 __attribute__((transaction_safe))
 void *wmemcpy(void *dest, const void *src, size_t n)
