e9078ebb19f0b5513786d0c43aa177f3916a11b2
Tobias Burnus <burnus@net-b.de>
2012-01-09 14:11:05 +0100

    re PR fortran/51578 (Import of same symbol via different modules and renaming)
    2012-01-09  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/51578
            * gfortran.h (gfc_use_list):
            * match.h (gfc_use_module): Rename to ...
            (gfc_use_modules): ... this.
            * module.c (use_locus, specified_nonint, specified_int): Remove
            global variable.
            (module_name): Change type to const char*, used with gfc_get_string.
            (module_list): New global variable.
            (free_rename): Free argument not global var.
            (gfc_match_use): Save match to module_list.
            (load_generic_interfaces, read_module): Don't free symtree.
            (write_dt_extensions, gfc_dump_module): Fix module-name I/O due to the
            type change of module_name.
            (write_symbol0, write_generic): Optimize due to the type change.
            (import_iso_c_binding_module, use_iso_fortran_env_module): Use
            locus of rename->where.
            (gfc_use_module): Take module_list as argument.
            (gfc_use_modules): New function.
            (gfc_module_init_2, gfc_module_done_2): Init module_list, rename_list.
            * parse.c (last_was_use_stmt): New global variable.
            (use_modules): New function.
            (decode_specification_statement, decode_statement): Move USE match up
            and call use_modules.
            (next_free, next_fixed): Call use_modules.
            (accept_statement): Don't call gfc_module_use.
    
    2012-01-09  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/51578
            * gfortran.dg/use_17.f90: New.
    
    From-SVN: r183010

---
+++ gcc/testsuite/gfortran.dg/use_17.f90
@@ -0,0 +1,39 @@
+! { dg-do compile }
+!
+! PR fortran/51578
+!
+! Contributed by Billy Backer
+!
+! Check that indict importing of the symbol "axx" works
+! even if renaming prevent the direct import.
+!
+module mod1
+integer :: axx=2
+end module mod1
+
+module mod2
+use mod1
+end module mod2
+
+subroutine sub1
+use mod1, oxx=>axx
+use mod2
+implicit none
+print*,axx ! Valid - was working before
+end subroutine sub1
+
+subroutine sub2
+use mod2
+use mod1, oxx=>axx
+implicit none
+print*,axx ! Valid - was failing before
+end subroutine sub2
+
+subroutine test1
+  use :: iso_c_binding
+  use, intrinsic :: iso_c_binding, only: c_double_orig => c_double
+  integer :: c_double
+  integer, parameter :: p1 = c_int, p2 = c_double_orig
+end subroutine test1
+
+! { dg-final { cleanup-modules "mod1 mod2" } }
