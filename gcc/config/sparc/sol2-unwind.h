a5f5d83a3f03056f336e53cee64a9b9363282159
Eric Botcazou <ebotcazou@adacore.com>
2012-01-09 18:22:35 +0000

    sol2-unwind.h (sparc64_is_sighandler): Check that the purported sigacthandler address isn't null before dereferencing it.
    	* config/sparc/sol2-unwind.h (sparc64_is_sighandler): Check that the
    	purported sigacthandler address isn't null before dereferencing it.
    	(sparc_is_sighandler): Likewise.
    
    From-SVN: r183022

--- gcc/config/sparc/sol2-unwind.h
+++ gcc/config/sparc/sol2-unwind.h
@@ -123,10 +123,10 @@ sparc64_fallback_frame_state (struct _Unwind_Context *context,
 	  /* This matches the call_user_handler pattern for Solaris 10.
 	     There are 2 cases so we look for the return address of the
 	     caller's caller frame in order to do more pattern matching.  */
-	  unsigned int sah_pattern
-	    = *(unsigned int *)(*(unsigned long *)(this_cfa + 176 + 15*8) - 4);
+	  unsigned long sah_address
+	    = *(unsigned long *)(this_cfa + 176 + 15*8);
 
-          if (sah_pattern == 0x92100019)
+          if (sah_address && *(unsigned int *)(sah_address - 4) == 0x92100019)
 	    /* This is the same setup as for Solaris 9, see below.  */
 	    regs_off = 176 + 176 + 176 + 304;
 	  else
@@ -371,10 +371,10 @@ sparc_fallback_frame_state (struct _Unwind_Context *context,
 	  /* This matches the call_user_handler pattern for Solaris 10.
 	     There are 2 cases so we look for the return address of the
 	     caller's caller frame in order to do more pattern matching.  */
-	  unsigned int sah_pattern
-	    = *(unsigned int *)(*(unsigned int *)(this_cfa + 96 + 15*4) - 4);
+	  unsigned int sah_address
+	    = *(unsigned int *)(this_cfa + 96 + 15*4);
 
-          if (sah_pattern == 0x92100019)
+          if (sah_address && *(unsigned int *)(sah_address - 4) == 0x92100019)
 	    /* This is the same setup as for Solaris 9, see below.  */
 	    regs_off = 96 + 96 + 96 + 160;
 	  else
