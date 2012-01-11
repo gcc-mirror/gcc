45152a7bc5439728859243f827c16cd945a8f24e
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-01-11 22:59:12 +0000

    PR bootstrap/51796 	* combine.c (distribute_notes): If i3 is a noreturn call, 	allow old_size to be equal to args_size and make sure the 	noreturn call gets REG_ARGS_SIZE note. 	* expr.c (fixup_args_size_notes): Put REG_ARGS_SIZE notes 	on noreturn calls even when the delta is 0.
    	* gcc.dg/pr51796.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@183111 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr51796.c
@@ -0,0 +1,15 @@
+/* PR bootstrap/51796 */
+/* { dg-do compile } */
+/* { dg-options "-Os -fno-omit-frame-pointer -fno-tree-dominator-opts -fno-tree-fre -fno-tree-pre" } */
+
+typedef void (*entry_func) (void) __attribute__ ((noreturn));
+extern entry_func entry_addr;
+static void bsd_boot_entry (void)
+{
+  stop ();
+}   
+void bsd_boot (void)
+{
+  entry_addr = (entry_func) bsd_boot_entry;
+  (*entry_addr) ();
+}
