fc97053254977c2709b8eca3caed6558e2ade7e2
Jason Merrill <jason@redhat.com>
2012-01-13 12:50:58 -0500

    re PR c++/51813 (-fvisibility=hidden causes std::codecvt members to be undefined)
    	PR c++/51813
    	* decl2.c (constrain_visibility): Clear DECL_VISIBILITY_SPECIFIED
    	when reducing the visibility.
    
    From-SVN: r183156

---
+++ gcc/testsuite/g++.dg/ext/visibility/template9.C
@@ -0,0 +1,14 @@
+// PR c++/51813
+// { dg-options -fvisibility=hidden }
+// { dg-final { scan-assembler-not "hidden\\s+_ZN1N1fI1AEEvT" } }
+
+struct A { };
+namespace N __attribute((visibility("default"))) {
+  template <class T> void f(T) { }
+  extern template void f(A);
+}
+
+int main()
+{
+  N::f(A());
+}
