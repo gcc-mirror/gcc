ad76fe700e7812977c64513fd552a701571b3e3f
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-01-13 23:35:44 +0000

    libgo: Only build iopl and ioperm on 386, amd64, alpha GNU/Linux.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@183171 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libgo/go/syscall/libcall_linux_amd64.go
@@ -0,0 +1,13 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// GNU/Linux library calls amd64 specific.
+
+package syscall
+
+//sys	Ioperm(from int, num int, on int) (err error)
+//ioperm(from _C_long, num _C_long, on int) int
+
+//sys	Iopl(level int) (err error)
+//iopl(level int) int
