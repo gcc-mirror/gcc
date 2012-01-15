41a8dcc8cb2ca69369c7616fbcaa63d728bf1d57
Uros Bizjak <uros@gcc.gnu.org>
2012-01-15 21:38:32 +0100

    re PR rtl-optimization/51821 (64bit > 32bit conversion produces incorrect results with optimizations)
    	PR rtl-optimization/51821
    	* recog.c (peep2_find_free_register): Determine clobbered registers
    	from insn pattern.
    
    testsuite/ChangeLog:
    
    	PR rtl-optimization/51821
    	* gcc.dg/pr51821.c: New test.
    
    From-SVN: r183200

--- gcc/recog.c
+++ gcc/recog.c
@@ -2933,6 +2933,7 @@ peep2_find_free_register (int from, int to, const char *class_str,
   static int search_ofs;
   enum reg_class cl;
   HARD_REG_SET live;
+  df_ref *def_rec;
   int i;
 
   gcc_assert (from < MAX_INSNS_PER_PEEP2 + 1);
@@ -2950,13 +2951,15 @@ peep2_find_free_register (int from, int to, const char *class_str,
 
   while (from != to)
     {
-      HARD_REG_SET this_live;
+      gcc_assert (peep2_insn_data[from].insn != NULL_RTX);
+
+      /* Don't use registers set or clobbered by the insn.  */
+      for (def_rec = DF_INSN_DEFS (peep2_insn_data[from].insn);
+	   *def_rec; def_rec++)
+	SET_HARD_REG_BIT (live, DF_REF_REGNO (*def_rec));
 
       if (++from >= MAX_INSNS_PER_PEEP2 + 1)
 	from = 0;
-      gcc_assert (peep2_insn_data[from].insn != NULL_RTX);
-      REG_SET_TO_HARD_REG_SET (this_live, peep2_insn_data[from].live_before);
-      IOR_HARD_REG_SET (live, this_live);
     }
 
   cl = (class_str[0] == 'r' ? GENERAL_REGS
