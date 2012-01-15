75e237b72e095709c4fb2cdaf0f9440c8a59c239
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-01-15 18:03:46 +0000

    PR rtl-optimization/51821 	* recog.c (peep2_find_free_register): Determine clobbered registers 	from insn pattern.
    testsuite/ChangeLog:
    
    	PR rtl-optimization/51821
    	* gcc.dg/pr51821.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@183194 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/recog.c
+++ gcc/recog.c
@@ -3038,6 +3038,7 @@ peep2_find_free_register (int from, int to, const char *class_str,
   static int search_ofs;
   enum reg_class cl;
   HARD_REG_SET live;
+  df_ref *def_rec;
   int i;
 
   gcc_assert (from < MAX_INSNS_PER_PEEP2 + 1);
@@ -3051,12 +3052,14 @@ peep2_find_free_register (int from, int to, const char *class_str,
 
   while (from != to)
     {
-      HARD_REG_SET this_live;
+      gcc_assert (peep2_insn_data[from].insn != NULL_RTX);
+
+      /* Don't use registers set or clobbered by the insn.  */
+      for (def_rec = DF_INSN_DEFS (peep2_insn_data[from].insn);
+	   *def_rec; def_rec++)
+	SET_HARD_REG_BIT (live, DF_REF_REGNO (*def_rec));
 
       from = peep2_buf_position (from + 1);
-      gcc_assert (peep2_insn_data[from].insn != NULL_RTX);
-      REG_SET_TO_HARD_REG_SET (this_live, peep2_insn_data[from].live_before);
-      IOR_HARD_REG_SET (live, this_live);
     }
 
   cl = (class_str[0] == 'r' ? GENERAL_REGS
