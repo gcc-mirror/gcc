55e83c66c7cbe6ac0ef526d7c70462eacde9511d
Paolo Carlini <paolo.carlini@oracle.com>
2012-01-18 20:27:23 +0000

    re PR c++/51225 ([c++0x] [4.7 Regression] ICE with invalid template parameter)
    /cp
    2012-01-18  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/51225
    	* typeck2.c (store_init_value): Within a template guard
    	cxx_constant_value with require_potential_constant_expression.
    	* pt.c (convert_nontype_argument): Likewise.
    
    /testsuite
    2012-01-18  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/51225
    	* g++.dg/cpp0x/pr51225.C: New.
    
    From-SVN: r183286

---
+++ gcc/testsuite/g++.dg/cpp0x/pr51225.C
@@ -0,0 +1,14 @@
+// PR c++/51225
+// { dg-options "-std=c++0x" }
+
+template<int> struct A {};
+
+template<typename> void foo()
+{
+  A<int(x)> a; // { dg-error "not declared|invalid type" }
+}
+
+template<typename> struct bar
+{
+  static constexpr A<1> b = A<1>(x); // { dg-error "not declared" }
+};
