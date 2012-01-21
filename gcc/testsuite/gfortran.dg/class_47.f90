7e1cbc7e91d19031d071b51d1a0f4c8722917436
Tobias Burnus <burnus@net-b.de>
2012-01-21 17:11:47 +0100

    re PR fortran/51913 ([OOP] bug when submitting a class pointer to a subroutine)
    2012-01-21  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/51913
            * interface.c (compare_parameter): Fix CLASS comparison.
    
    2012-01-21  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/51913
            * gfortran.dg/class_47.f90: New.
    
    From-SVN: r183369

---
+++ gcc/testsuite/gfortran.dg/class_47.f90
@@ -0,0 +1,40 @@
+! { dg-do compile }
+!
+! PR fortran/51913
+!
+! Contributed by Alexander Tismer
+!
+MODULE m_sparseMatrix
+
+  implicit none
+  
+  type :: sparseMatrix_t
+
+  end type sparseMatrix_t
+END MODULE m_sparseMatrix
+
+!===============================================================================
+module m_subroutine
+!  USE m_sparseMatrix !< when uncommenting this line program works fine
+
+  implicit none
+
+  contains
+  subroutine test(matrix)
+    use m_sparseMatrix
+    class(sparseMatrix_t), pointer :: matrix
+  end subroutine
+end module
+
+!===============================================================================
+PROGRAM main
+  use m_subroutine
+  USE m_sparseMatrix
+  implicit none
+
+  CLASS(sparseMatrix_t), pointer :: sparseMatrix
+  
+  call test(sparseMatrix)
+END PROGRAM
+
+! { dg-final { cleanup-modules "m_sparsematrix m_subroutine" } }
