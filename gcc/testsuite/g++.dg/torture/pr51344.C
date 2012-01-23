5965b617aa6ad71890de872bdbaa6fd0d7aab216
Jason Merrill <jason@redhat.com>
2012-01-23 10:30:48 -0500

    re PR target/51934 (FAIL: g++.dg/torture/pr51344.C  -O0  (test for excess errors)  due to "cdecl attribute ignored" warning)
    	PR target/51934
    	* g++.dg/torture/pr51344.C: Limit to x86.
    
    From-SVN: r183436

--- gcc/testsuite/g++.dg/torture/pr51344.C
+++ gcc/testsuite/g++.dg/torture/pr51344.C
@@ -1,10 +1,10 @@
-/* { dg-do compile } */
+/* { dg-do compile { target { i?86-*-* && ilp32 } } } */
 class A;
 
 template <class T>
 class B
 {
-  friend __attribute__((noreturn)) A& operator >>(A& a, B& b)
+  friend __attribute__((cdecl)) A& operator >>(A& a, B& b)
   {
     return a;
   }
