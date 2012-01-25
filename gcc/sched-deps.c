21ae439c983250880d3549e89015eea76c7bdbc4
Jakub Jelinek <jakub@redhat.com>
2012-01-25 21:22:53 +0100

    re PR middle-end/51986 (uninitialized variable warning regression prevents bootstrap)
    	PR middle-end/51986
    	* sched-deps.c (sched_get_condition_with_rev_uncached): Don't test
    	for pat == 0.
    
    From-SVN: r183536

--- gcc/sched-deps.c
+++ gcc/sched-deps.c
@@ -2,7 +2,7 @@
    instructions.
    Copyright (C) 1992, 1993, 1994, 1995, 1996, 1997, 1998,
    1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
-   2011
+   2011, 2012
    Free Software Foundation, Inc.
    Contributed by Michael Tiemann (tiemann@cygnus.com) Enhanced by,
    and currently maintained by, Jim Wilson (wilson@cygnus.com)
@@ -520,9 +520,6 @@ sched_get_condition_with_rev_uncached (const_rtx insn, bool *rev)
 {
   rtx pat = PATTERN (insn);
   rtx src;
-
-  if (pat == 0)
-    return 0;
 
   if (rev)
     *rev = false;
