2887732181ebf03ccc8d04521207b38437183cd9
Ian Lance Taylor <ian@gcc.gnu.org>
2012-01-26 05:43:42 +0000

    libgo/mksysinfo: Always define IPV6_TCLASS.
    From-SVN: r183552

--- libgo/mksysinfo.sh
+++ libgo/mksysinfo.sh
@@ -192,7 +192,7 @@ grep '^const _SHUT_' gen-sysinfo.go |
   sed -e 's/^\(const \)_\(SHUT[^= ]*\)\(.*\)$/\1\2 = _\2/' >> ${OUT}
 
 # The net package requires some const definitions.
-for m in IPV6_V6ONLY IPPROTO_IPV6 IPV6_JOIN_GROUP IPV6_LEAVE_GROUP; do
+for m in IPV6_V6ONLY IPPROTO_IPV6 IPV6_JOIN_GROUP IPV6_LEAVE_GROUP IPV6_TCLASS; do
   if ! grep "^const $m " ${OUT} >/dev/null 2>&1; then
     echo "const $m = 0" >> ${OUT}
   fi
