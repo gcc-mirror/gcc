10c7477699e2d8906204293e15c64b34d1e62231
Tom de Vries <tom@codesourcery.com>
2012-01-27 10:38:34 +0000

    re PR tree-optimization/51990 (ICE in copy_reference_ops_from_ref)
    2012-01-27  Tom de Vries  <tom@codesourcery.com>
    
    	PR tree-optimization/51990
    	* gcc.dg/pr51990.c: New test.
    	* gcc.dg/pr51990-2.c: Same.
    
    From-SVN: r183615

---
+++ gcc/testsuite/gcc.dg/pr51990.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+
+int
+zzz (char *s1, char *s2, int len, int *q)
+{
+  int z = 5;
+  unsigned int i, b;
+  struct s { char a[z]; };
+  struct s x;
+
+  extern int foo (int, ...) __attribute__((pure));
+
+  for (i = 0; i < len; i++)
+    s1[i] = s2[i];
+
+  b = z & 0x3;
+
+  len += (b == 0 ? 0 : 1) + z;
+
+  *q = len;
+  return foo (z, x, x);
+}
