ec9b7e8abfd6d91cbc9d6a088a0b5a863252df07
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-01-27 20:26:03 +0000

    PR libstdc++/51798 	* config/cpu/generic/atomicity_builtins/atomicity.h 	(__exchange_and_add, __atomic_add): Use __atomic_fetch_add 	with __ATOMIC_ACQ_REL semantics instead of __sync_fetch_and_add. 	* include/ext/atomicity.h (__exchange_and_add, __atomic_add): 	Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@183644 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/config/cpu/generic/atomicity_builtins/atomicity.h
+++ libstdc++-v3/config/cpu/generic/atomicity_builtins/atomicity.h
@@ -1,7 +1,7 @@
 // Low-level functions for atomic operations: version for CPUs providing
 // atomic builtins -*- C++ -*-
 
-// Copyright (C) 2006, 2009, 2010 Free Software Foundation, Inc.
+// Copyright (C) 2006, 2009, 2010, 2012 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -33,12 +33,12 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
   _Atomic_word 
   __attribute__ ((__unused__))
   __exchange_and_add(volatile _Atomic_word* __mem, int __val) throw ()
-  { return __sync_fetch_and_add(__mem, __val); }
+  { return __atomic_fetch_add(__mem, __val, __ATOMIC_ACQ_REL); }
 
   void
   __attribute__ ((__unused__))
   __atomic_add(volatile _Atomic_word* __mem, int __val) throw ()
-  { __sync_fetch_and_add(__mem, __val); }
+  { __atomic_fetch_add(__mem, __val, __ATOMIC_ACQ_REL); }
 
 _GLIBCXX_END_NAMESPACE_VERSION
 } // namespace
