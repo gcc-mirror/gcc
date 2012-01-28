1da74dbaf4fb2eb84f0a2c96db4c92cca6a285d9
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-01-28 15:45:36 +0000

    * gcc.dg/torture/pr50444.c: Fix dg directives.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@183667 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/torture/pr50444.c
+++ gcc/testsuite/gcc.dg/torture/pr50444.c
@@ -1,6 +1,6 @@
-/* { dg-require-effective-target sse2_runtime } */
-/* { dg-do run } */
+/* { dg-do run { target i?86-*-* x86_64-*-* } } */
 /* { dg-options "-msse2" } */
+/* { dg-require-effective-target sse2_runtime } */
 
 typedef long long __m128i __attribute__ ((__vector_size__ (16),
 __may_alias__));
