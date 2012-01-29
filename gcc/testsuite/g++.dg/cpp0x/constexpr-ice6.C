efff885da8d3afae3f871ae2412cfe44d6b44568
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-01-29 21:41:54 +0000

    /cp 2012-01-29  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/51327
    	* class.c (explain_non_literal_class): Correctly handle implicitly
    	deleted constructors.
    
    /testsuite
    2012-01-29  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/51327
    	* g++.dg/cpp0x/constexpr-ice6.C: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@183684 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-ice6.C
@@ -0,0 +1,11 @@
+// PR c++/51327
+// { dg-options -std=c++0x }
+
+struct A
+{
+  A(int);
+};
+
+struct B : A {};                   // { dg-error "no matching" }
+
+constexpr int foo(B) { return 0; } // { dg-error "invalid type" }
