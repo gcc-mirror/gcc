3a542b98f864ff8d4148babe585f85f83f812f74
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-01-30 13:48:37 +0000

    PR tree-optimization/52046 	* tree-vect-patterns.c (check_bool_pattern): Give up if 	a comparison could throw.
    	* gcc.dg/pr52046.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@183724 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr52046.c
@@ -0,0 +1,14 @@
+/* PR tree-optimization/52046 */
+/* { dg-do compile } */
+/* { dg-options "-O3 -fexceptions -fnon-call-exceptions" } */
+
+extern float a[], b[], c[], d[];
+extern int k[];
+
+void
+foo (void)
+{
+  int i;
+  for (i = 0; i < 1024; ++i)
+    k[i] = (a[i] < b[i]) | (c[i] < d[i]);
+}
