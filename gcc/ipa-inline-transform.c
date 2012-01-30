f8698b375afc829d817ba3fc3ab38929479a7222
Richard Guenther <rguenther@suse.de>
2012-01-30 13:39:12 +0000

    re PR middle-end/52045 (ICE: in execute_todo, at passes.c:1748 with -O3 -ftracer -fno-tree-ccp -fno-tree-copy-prop -fno-tree-dce and stpcpy_chk())
    2012-01-30  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/52045
    	* ipa-inline-transform.c (inline_transform): Call execute_fixup_cfg
    	before computing final todo.
    
    From-SVN: r183721

--- gcc/ipa-inline-transform.c
+++ gcc/ipa-inline-transform.c
@@ -369,11 +369,13 @@ inline_transform (struct cgraph_node *node)
     todo = optimize_inline_calls (current_function_decl);
   timevar_pop (TV_INTEGRATION);
 
+  cfun->always_inline_functions_inlined = true;
+  cfun->after_inlining = true;
+  todo |= execute_fixup_cfg ();
+
   if (!(todo & TODO_update_ssa_any))
     /* Redirecting edges might lead to a need for vops to be recomputed.  */
     todo |= TODO_update_ssa_only_virtuals;
 
-  cfun->always_inline_functions_inlined = true;
-  cfun->after_inlining = true;
-  return todo | execute_fixup_cfg ();
+  return todo;
 }
