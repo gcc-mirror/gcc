c2453253e725d72661385827a0d1e67b31f31c6a
Georg-Johann Lay <avr@gjlay.de>
2012-02-01 13:21:21 +0000

    avr.c: Resolve all AS1 and AS2 macros.
    	* config/avr/avr.c: Resolve all AS1 and AS2 macros.
    	* config/avr/avr.h (AS1, AS2, AS2C, AS3): Remove.
    	(OUT_AS1, OUT_AS2): Remove.
    
    From-SVN: r183800

--- gcc/config/avr/avr.h
+++ gcc/config/avr/avr.h
@@ -615,22 +615,6 @@ mmcu=*:-mmcu=%*}"
 #define TEST_HARD_REG_CLASS(CLASS, REGNO) \
   TEST_HARD_REG_BIT (reg_class_contents[ (int) (CLASS)], REGNO)
 
-/* Note that the other files fail to use these
-   in some of the places where they should.  */
-
-#if defined(__STDC__) || defined(ALMOST_STDC)
-#define AS2(a,b,c) #a " " #b "," #c
-#define AS2C(b,c) " " #b "," #c
-#define AS3(a,b,c,d) #a " " #b "," #c "," #d
-#define AS1(a,b) #a " " #b
-#else
-#define AS1(a,b) "a	b"
-#define AS2(a,b,c) "a	b,c"
-#define AS2C(b,c) " b,c"
-#define AS3(a,b,c,d) "a	b,c,d"
-#endif
-#define OUT_AS1(a,b) output_asm_insn (AS1(a,b), operands)
-#define OUT_AS2(a,b,c) output_asm_insn (AS2(a,b,c), operands)
 #define CR_TAB "\n\t"
 
 #define DWARF2_ADDR_SIZE 4
