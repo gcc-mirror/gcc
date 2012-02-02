d0da7abbba2eaaf0d26a9a7915adbde950c6dace
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-02-02 21:01:34 +0000

    2012-02-02  Tobias Burnus  <burnus@net-b.de>
            PR fortran/52093
            * simplify.c (gfc_simplify_size): Handle INTRINSIC_PARENTHESES.
    
    2012-02-02  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/52093
            * gfortran.dg/shape_7.f90: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@183848 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/shape_7.f90
@@ -0,0 +1,32 @@
+! { dg-do compile }
+! { dg-options "-fdump-tree-original" }
+!
+! PR fortran/52093
+!
+! Contributed by Mohammad Rahmani
+!
+
+Program Main
+ Implicit None
+ Integer:: X(2,2)
+ Integer:: X2(7:11,8:9)
+
+ if (size((X)) /= 4) call abort ()
+ if (any (Shape((X))  /= [2,2])) call abort ()
+ if (any (lbound((X)) /= [1,1])) call abort ()
+ if (any (ubound((X)) /= [2,2])) call abort ()
+
+ if (size(X2) /= 10) call abort ()
+ if (any (Shape(X2)  /= [5,2])) call abort ()
+ if (any (lbound(X2) /= [7,8]))  call abort ()
+ if (any (ubound(X2) /= [11,9])) call abort ()
+
+ if (size((X2)) /= 10) call abort ()
+ if (any (Shape((X2))  /= [5,2])) call abort ()
+ if (any (lbound((X2)) /= [1,1])) call abort ()
+ if (any (ubound((X2)) /= [5,2])) call abort ()
+End Program Main
+
+! { dg-final { scan-tree-dump-times "abort" 0 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
+
