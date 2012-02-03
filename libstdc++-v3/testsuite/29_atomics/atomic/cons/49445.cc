0f1fce69459a0572968cf7081c14bdc55785788c
Benjamin Kosnik <bkoz@redhat.com>
2012-02-03 21:10:00 +0000

    re PR libstdc++/49445 ([C++0x] Undefined reference to std::atomic<float> "operator float")
    2012-02-03  Benjamin Kosnik  <bkoz@redhat.com>
    
    	PR libstdc++/49445
    
    From-SVN: r183878

---
+++ libstdc++-v3/testsuite/29_atomics/atomic/cons/49445.cc
@@ -0,0 +1,42 @@
+// { dg-options "-std=gnu++0x" }
+
+// Copyright (C) 2012 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <atomic>
+
+enum class tacos : int
+{
+  cancun = 4,
+  el_loco = 5,
+  sabor = 6,
+  papalote = 9,
+  licious = 44,
+  jarritos = 55
+};
+
+// should minimally compile and link
+int main()
+{
+  std::atomic<float> af(0.0f);
+  float non_af = af;
+  
+  std::atomic<tacos> ae(tacos::sabor);
+  tacos non_ae = ae;
+
+  return 0;
+}
