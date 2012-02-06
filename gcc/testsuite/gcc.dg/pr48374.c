0ac4c2d04003768ece641baff3250856dc689179
Andrey Belevantsev <abel@ispras.ru>
2012-02-06 12:31:52 +0400

    pr48374.c: Actually add the test I forgot in the 2012-01-25 commit.
    2012-02-06  Andrey Belevantsev  <abel@ispras.ru>
    
          * gcc.dg/pr48374.c: Actually add the test I forgot
          in the 2012-01-25 commit.
    
    From-SVN: r183928

---
+++ gcc/testsuite/gcc.dg/pr48374.c
@@ -0,0 +1,17 @@
+/* { dg-do compile { target powerpc*-*-* ia64-*-* x86_64-*-* } } */
+/* { dg-options "-O -fschedule-insns2 -fsel-sched-pipelining -fsel-sched-pipelining-outer-loops -fselective-scheduling2 --param max-sched-extend-regions-iters=2"  } */
+
+void foo (int y)
+{
+  switch (y)
+    {
+    case 3:
+    case 5:
+    case 7:
+    case 11:
+      break;
+    default:
+      __builtin_unreachable ();
+    }
+}
+
