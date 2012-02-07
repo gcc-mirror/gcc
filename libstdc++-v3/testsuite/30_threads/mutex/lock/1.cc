a569dfd7a5cc54a8bd678f2440097e7d5f91513d
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-02-07 22:26:16 +0000

    * testsuite/30_threads/call_once/39909.cc: Remove duplicate target 	selector. 	* testsuite/30_threads/call_once/49668.cc: Likewise. 	* testsuite/30_threads/call_once/call_once1.cc: Likewise. 	* testsuite/30_threads/lock_guard/cons/1.cc: Likewise. 	* testsuite/30_threads/mutex/cons/1.cc: Likewise. 	* testsuite/30_threads/mutex/dest/destructor_locked.cc: Likewise. 	* testsuite/30_threads/mutex/lock/1.cc: Likewise. 	* testsuite/30_threads/mutex/native_handle/1.cc: Likewise. 	* testsuite/30_threads/mutex/native_handle/typesizes.cc: Likewise. 	* testsuite/30_threads/mutex/try_lock/1.cc: Likewise. 	* testsuite/30_threads/mutex/try_lock/2.cc: Likewise. 	* testsuite/30_threads/mutex/unlock/1.cc: Likewise. 	* testsuite/30_threads/unique_lock/cons/1.cc: Likewise. 	* testsuite/30_threads/unique_lock/cons/2.cc: Likewise. 	* testsuite/30_threads/unique_lock/cons/3.cc: Likewise. 	* testsuite/30_threads/unique_lock/cons/4.cc: Likewise. 	* testsuite/30_threads/unique_lock/cons/5.cc: Likewise. 	* testsuite/30_threads/unique_lock/cons/6.cc: Likewise. 	* testsuite/30_threads/unique_lock/locking/1.cc: Likewise. 	* testsuite/30_threads/unique_lock/locking/2.cc: Likewise. 	* testsuite/30_threads/unique_lock/locking/3.cc: Likewise. 	* testsuite/30_threads/unique_lock/locking/4.cc: Likewise. 	* testsuite/30_threads/unique_lock/modifiers/1.cc: Likewise. 	* testsuite/30_threads/unique_lock/modifiers/2.cc: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@183984 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/30_threads/mutex/lock/1.cc
+++ libstdc++-v3/testsuite/30_threads/mutex/lock/1.cc
@@ -1,5 +1,5 @@
-// { dg-do run { target *-*-freebsd* *-*-netbsd* *-*-linux* *-*-solaris* *-*-cygwin *-*-darwin* alpha*-*-osf* mips-sgi-irix6* powerpc-ibm-aix* powerpc-ibm-aix* } }
-// { dg-options " -std=gnu++0x -pthread" { target *-*-freebsd* *-*-netbsd* *-*-linux* alpha*-*-osf* mips-sgi-irix6* powerpc-ibm-aix* powerpc-ibm-aix* } }
+// { dg-do run { target *-*-freebsd* *-*-netbsd* *-*-linux* *-*-solaris* *-*-cygwin *-*-darwin* alpha*-*-osf* mips-sgi-irix6* powerpc-ibm-aix* } }
+// { dg-options " -std=gnu++0x -pthread" { target *-*-freebsd* *-*-netbsd* *-*-linux* alpha*-*-osf* mips-sgi-irix6* powerpc-ibm-aix* } }
 // { dg-options " -std=gnu++0x -pthreads" { target *-*-solaris* } }
 // { dg-options " -std=gnu++0x " { target *-*-cygwin *-*-darwin* } }
 // { dg-require-cstdint "" }
