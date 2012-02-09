16e0be9b8e67554b4c43b7bbd57978fb6e9f4611
Peter Bergner <bergner@vnet.ibm.com>
2012-02-09 08:46:02 -0600

    re PR middle-end/52140 (Libdfp testcase encounters ICE when compiled with soft-dfp (no -mcpu).)
    gcc/
    	PR middle-end/52140
    	* dojump.c (do_compare_rtx_and_jump): Use SCALAR_FLOAT_MODE_P.
    
    gcc/testsuite/
    	PR middle-end/52140
    	* gcc.dg/dfp/pr52140.c: New test.
    
    From-SVN: r184045

--- gcc/dojump.c
+++ gcc/dojump.c
@@ -1049,7 +1049,7 @@ do_compare_rtx_and_jump (rtx op0, rtx op1, enum rtx_code code, int unsignedp,
     }
   else
     {
-      if (GET_MODE_CLASS (mode) == MODE_FLOAT
+      if (SCALAR_FLOAT_MODE_P (mode)
 	  && ! can_compare_p (code, mode, ccp_jump)
 	  && can_compare_p (swap_condition (code), mode, ccp_jump))
 	{
@@ -1060,7 +1060,7 @@ do_compare_rtx_and_jump (rtx op0, rtx op1, enum rtx_code code, int unsignedp,
 	  op1 = tmp;
 	}
 
-      else if (GET_MODE_CLASS (mode) == MODE_FLOAT
+      else if (SCALAR_FLOAT_MODE_P (mode)
 	       && ! can_compare_p (code, mode, ccp_jump)
 
 	       /* Never split ORDERED and UNORDERED.  These must be implemented.  */
