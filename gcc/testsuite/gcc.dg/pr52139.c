c8b7e7bb8d2778c2bcfafd714e1604fa1aee9b99
Jakub Jelinek <jakub@redhat.com>
2012-02-09 22:39:20 +0100

    backport: re PR rtl-optimization/52139 (ICE: in remove_insn, at emit-rtl.c:3960 with -O -fPIC -fno-tree-dominator-opts -fno-tree-fre)
    	Backported from mainline
    	2012-02-08  Jakub Jelinek  <jakub@redhat.com>
    
    	PR rtl-optimization/52139
    	* cfgrtl.c (cfg_layout_merge_blocks): If BB_END
    	is a BARRIER after emit_insn_after_noloc, move BB_END
    	to the last non-BARRIER insn before it.
    
    	* gcc.dg/pr52139.c: New test.
    
    From-SVN: r184074

---
+++ gcc/testsuite/gcc.dg/pr52139.c
@@ -0,0 +1,49 @@
+/* PR rtl-optimization/52139 */
+/* { dg-do compile } */
+/* { dg-options "-O -fno-tree-dominator-opts -fno-tree-fre" } */
+/* { dg-options "-O -fno-tree-dominator-opts -fno-tree-fre -fpic" { target fpic } } */
+
+void *p;
+
+void
+foo (int a)
+{
+  switch (a)
+    {
+    case 0:
+    a0:
+    case 1:
+    a1:
+      p = &&a1;
+    case 2:
+    a2:
+      p = &&a2;
+    case 3:
+    a3:
+      p = &&a3;
+    case 4:
+    a4:
+      p = &&a4;
+    case 5:
+    a5:
+      p = &&a5;
+    case 6:
+    a6:
+      p = &&a6;
+    case 7:
+    a7:
+      p = &&a7;
+    case 8:
+    a8:
+      p = &&a8;
+    case 9:
+    a9:
+      p = &&a9;
+    case 10:
+    a10:
+      p = &&a10;
+    default:
+      p = &&a0;
+    }
+  goto *p;
+}
