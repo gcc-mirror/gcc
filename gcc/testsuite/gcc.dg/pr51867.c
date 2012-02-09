15ed19ff324f45494d471b65c321804f1858cd26
Bin Cheng <bin.cheng@arm.com>
2012-02-09 09:37:37 +0000

    re PR middle-end/51867 (GCC generates inconsistent code for same sources calling builtin calls, like sqrtf)
    	PR target/51867
    	* builtins.c (expand_builtin): Don't check DECL_ASSEMBLER_NAME_SET_P.
    
    	PR target/51867
    	* testsuite/c-c++-common/dfp/signbit-2.c: Change '-O0' to '-O1'.
    	* testsuite/gcc.dg/pr51867.c: New test.
    
    From-SVN: r184037

---
+++ gcc/testsuite/gcc.dg/pr51867.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-options "-O0 -fno-math-errno -fdump-rtl-expand" } */
+#include <math.h>
+
+float a(float x)
+{
+    return sqrtf(x);
+}
+float b(float x)
+{
+    return sqrtf(x);
+}
+/* Here the calls to sqrtf should be expanded into CALL_INSNs, rather than
+   fpu sqrtf rtl patterns.  */
+/* { dg-final { scan-rtl-dump-times "call_insn" 2 "expand" } } */
+/* { dg-final { cleanup-rtl-dump "expand" } } */
