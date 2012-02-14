6e3f8a30262f988bd062a6662c0b0c61bd9e884a
Jakub Jelinek <jakub@redhat.com>
2012-02-15 00:31:42 +0100

    re PR bootstrap/51969 (trunk gcc unable to build gcc 4.6)
    	PR bootstrap/51969
    	Backported from mainline
    	2011-11-08  Michael Matz  <matz@suse.de>
    
    	* gengtype.c (write_field_root): Avoid out-of-scope access of newv.
    
    From-SVN: r184239

--- gcc/gengtype.c
+++ gcc/gengtype.c
@@ -3594,14 +3594,13 @@ write_field_root (outf_p f, pair_p v, type_p type, const char *name,
 		  int has_length, struct fileloc *line, const char *if_marked,
 		  bool emit_pch, type_p field_type, const char *field_name)
 {
+  struct pair newv;
   /* If the field reference is relative to V, rather than to some
      subcomponent of V, we can mark any subarrays with a single stride.
      We're effectively treating the field as a global variable in its
      own right.  */
   if (v && type == v->type)
     {
-      struct pair newv;
-
       newv = *v;
       newv.type = field_type;
       newv.name = ACONCAT ((v->name, ".", field_name, NULL));
