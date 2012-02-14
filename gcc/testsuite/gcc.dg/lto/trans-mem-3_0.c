4a737c625c652bd552ea2da6885021776296d96c
hp <hp@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-02-14 22:06:56 +0000

    * lib/target-supports.exp (check_effective_target_fgnu_tm): New 	proc. 	* gfortran.dg/trans-mem-skel.f90: Gate test on effective_target 	fgnu_tm. 	* gcc.dg/lto/trans-mem-1_0.c, gcc.dg/lto/trans-mem-2_0.c, 	gcc.dg/lto/trans-mem-3_0.c, gcc.dg/lto/trans-mem-4_0.c: Ditto. 	* gcc.dg/tm/tm.exp: Gate the whole of gcc.dg/tm on 	effective_target fgnu_tm. 	* g++.dg/tm/tm.exp: Ditto for g++.dg/tm.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@184237 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/lto/trans-mem-3_0.c
+++ gcc/testsuite/gcc.dg/lto/trans-mem-3_0.c
@@ -1,6 +1,7 @@
 /* { dg-lto-options {{-flto}} } */
 /* { dg-lto-do link } */
 /* { dg-require-effective-target stdint_types } */
+/* { dg-require-effective-target fgnu_tm } */
 
 /* Test that we can build one object file with -fgnu-tm
    (trans-mem-3_1.c), but do the final link of all objects without
--- gcc/testsuite/gcc.dg/lto/trans-mem-4_0.c
+++ gcc/testsuite/gcc.dg/lto/trans-mem-4_0.c
@@ -1,6 +1,7 @@
 /* { dg-lto-options {{-flto -fgnu-tm}} } */
 /* { dg-lto-do link } */
 /* { dg-require-effective-target stdint_types } */
+/* { dg-require-effective-target fgnu_tm } */
 
 extern void foo() __attribute__((transaction_safe));
 
