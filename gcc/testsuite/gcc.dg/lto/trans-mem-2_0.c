5cd3514cddd76e0a33208f85c3a623c7fb53470c
Hans-Peter Nilsson <hp@axis.com>
2012-02-14 22:06:56 +0000

    target-supports.exp (check_effective_target_fgnu_tm): New proc.
    	* lib/target-supports.exp (check_effective_target_fgnu_tm): New
    	proc.
    	* gfortran.dg/trans-mem-skel.f90: Gate test on effective_target
    	fgnu_tm. 
    	* gcc.dg/lto/trans-mem-1_0.c, gcc.dg/lto/trans-mem-2_0.c,
    	gcc.dg/lto/trans-mem-3_0.c, gcc.dg/lto/trans-mem-4_0.c: Ditto.
    	* gcc.dg/tm/tm.exp: Gate the whole of gcc.dg/tm on
    	effective_target fgnu_tm.
    	* g++.dg/tm/tm.exp: Ditto for g++.dg/tm.
    
    From-SVN: r184237

--- gcc/testsuite/gcc.dg/lto/trans-mem-2_0.c
+++ gcc/testsuite/gcc.dg/lto/trans-mem-2_0.c
@@ -1,6 +1,7 @@
 /* { dg-lto-options {{-flto -fgnu-tm}} } */
 /* { dg-lto-do link } */
 /* { dg-require-effective-target stdint_types } */
+/* { dg-require-effective-target fgnu_tm } */
 
 #include "trans-mem.h"
 
--- gcc/testsuite/gfortran.dg/trans-mem-skel.f90
+++ gcc/testsuite/gfortran.dg/trans-mem-skel.f90
@@ -1,5 +1,6 @@
 ! { dg-do compile }
 ! { dg-options "-fgnu-tm" }
+! { dg-require-effective-target fgnu_tm }
 program foo
       real x
 end program foo
