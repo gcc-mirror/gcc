43156aa32f62a10adaef58313f6414708e7e5bcc
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-02-20 23:43:31 +0000

    PR middle-end/52141         * trans-mem.c (ipa_tm_scan_irr_block): Error out on GIMPLE_ASM's         in a transaction safe function.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@184417 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tm/pr52141.c
@@ -0,0 +1,24 @@
+/* { dg-do compile } */
+/* { dg-options "-fgnu-tm -O0 -w" } */
+
+__attribute__((always_inline))
+static void asmfunc(void)
+{
+  __asm__ (""); /* { dg-error "asm not allowed in .transaction_safe" } */
+}
+
+__attribute__((transaction_safe))
+static void f(void)
+{
+  asmfunc();
+}
+
+int main()
+{
+  __transaction_atomic {
+    f();
+  }
+  return 0;
+}
+
+/* { dg-message "inlined from \'f\'" "" { target *-*-* } 0 } */
