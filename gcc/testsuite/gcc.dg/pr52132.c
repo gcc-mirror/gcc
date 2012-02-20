ba641b7edab68004b645d5d80be6979c149fd0ab
Georg-Johann Lay <avr@gjlay.de>
2012-02-20 14:06:00 +0000

    pr52132.c: Fix FAIL on 16-bit int platforms.
    	* gcc.dg/pr52132.c: Fix FAIL on 16-bit int platforms.
    
    From-SVN: r184393

--- gcc/testsuite/gcc.dg/pr52132.c
+++ gcc/testsuite/gcc.dg/pr52132.c
@@ -2,6 +2,11 @@
 /* { dg-do compile } */
 /* { dg-options "-std=c99 -O2 -g" } */
 
+#if  (__SIZEOF_INT__ < __SIZEOF_FLOAT__) \
+  && (__SIZEOF_LONG__ == __SIZEOF_FLOAT__)
+#define int long
+#endif
+
 int l;
 void bar (void);
 
@@ -10,7 +15,11 @@ foo (int *x, float y)
 {
   float b;
   union { float f; int i; } u = { .f = y };
+#if  (__SIZEOF_INT__ < __SIZEOF_FLOAT__)
+  u.i += 127L << 23;
+#else
   u.i += 127 << 23;
+#endif
   u.f = ((-1.0f / 3) * u.f + 2) * u.f - 2.0f / 3;
   b = 0.5 * (u.f + l);
   if (b >= *x)
