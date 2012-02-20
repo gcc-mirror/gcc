7b575958744045cf3c24b337c8312ee5f1344530
Kai Tietz <ktietz@redhat.com>
2012-02-20 22:28:36 +0100

    re PR libstdc++/52300 (Gthreads functions linking error at dynamic linking with libstdc++ when using MinGW.)
            PR libstdc++/52300
            * gthr.h (GTHREAD_USE_WEAK): Define as zero for mingw.
    
    From-SVN: r184408

--- gcc/gthr.h
+++ gcc/gthr.h
@@ -155,6 +155,14 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 /* Include GTHREAD_FILE if one is defined.  */
 #elif defined(HAVE_GTHR_DEFAULT)
 #if SUPPORTS_WEAK
+/* The pe-coff weak support isn't fully compatible to ELF's weak.
+   For static libraries it might would work, but as we need to deal
+   with shared versions too, we disable it for mingw-targets.  */
+#ifdef __MINGW32__
+#undef GTHREAD_USE_WEAK
+#define GTHREAD_USE_WEAK 0
+#endif
+
 #ifndef GTHREAD_USE_WEAK
 #define GTHREAD_USE_WEAK 1
 #endif
