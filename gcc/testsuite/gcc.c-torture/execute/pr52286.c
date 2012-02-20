b0408f13d4b3170e5dc50149f042f21d1527767d
Georg-Johann Lay <avr@gjlay.de>
2012-02-20 14:15:02 +0000

    pr52286.c: Fix FAIL on 16-bit int platforms.
    	* gcc.c-torture/execute/pr52286.c: Fix FAIL on 16-bit int platforms.
    
    From-SVN: r184394

--- gcc/testsuite/gcc.c-torture/execute/pr52286.c
+++ gcc/testsuite/gcc.c-torture/execute/pr52286.c
@@ -5,9 +5,15 @@ extern void abort (void);
 int
 main ()
 {
+#if __SIZEOF_INT__ > 2
   int a, b;
   asm ("" : "=r" (a) : "0" (0));
   b = (~a | 1) & -2038094497;
+#else
+  long a, b;
+  asm ("" : "=r" (a) : "0" (0));
+  b = (~a | 1) & -2038094497L;
+#endif
   if (b >= 0)
     abort ();
   return 0;
