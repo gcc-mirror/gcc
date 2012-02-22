88e773b2bdd67c51a8ab88d7ac1d700954403ed9
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-02-22 19:38:30 +0000

    * g++.dg/opt/devirt2.C: Add rule for *-*-mingw* target.         * g++.dg/opt/pr48549.C (long): Replace by long by __PTRDIFF_TYPE__         derived pdiff_t type.         * g++.dg/torture/pr49720.C: Likewise
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@184490 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/opt/devirt2.C
+++ gcc/testsuite/g++.dg/opt/devirt2.C
@@ -5,7 +5,9 @@
 // { dg-additional-options "-mshort-calls" {target epiphany-*-*} }
 // Using -mno-abicalls avoids a R_MIPS_JALR .reloc.
 // { dg-additional-options "-mno-abicalls" { target mips*-*-* } }
-// { dg-final { scan-assembler-times "xyzzy" 2 { target { ! { alpha*-*-* hppa*-*-* ia64*-*-hpux* sparc*-*-* } } } } }
+// { dg-final { scan-assembler-times "xyzzy" 2 { target { ! { alpha*-*-* hppa*-*-* ia64*-*-hpux* sparc*-*-* *-*-mingw* } } } } }
+// For *-*-mingw* there is additionally one .def match
+// { dg-final { scan-assembler-times "xyzzy" 3 { target *-*-mingw* } } }
 // The IA64 and HPPA compilers generate external declarations in addition
 // to the call so those scans need to be more specific.
 // { dg-final { scan-assembler-times "br\[^\n\]*xyzzy" 2 { target ia64*-*-hpux* } } }
