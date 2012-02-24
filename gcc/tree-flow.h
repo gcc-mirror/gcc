4021ad55900b053d9060ae74cfaa504c117f89b7
Bernhard Reutner-Fischer <aldot@gcc.gnu.org>
2012-02-24 10:40:27 +0100

    tree-phinodes.c (make_phi_node): Mark static.
    2012-02-23  Bernhard Reutner-Fischer  <aldot@gcc.gnu.org>
    
    	* tree-phinodes.c (make_phi_node): Mark static.
    	* tree-flow.h (make_phi_node): Remove extern decl.
    	* doc/gimple.texi (make_phi_node): Remove documentation.
    
    From-SVN: r184545

--- gcc/tree-flow.h
+++ gcc/tree-flow.h
@@ -504,7 +504,6 @@ extern void find_referenced_vars_in (gimple);
 /* In tree-phinodes.c  */
 extern void reserve_phi_args_for_new_edge (basic_block);
 extern void add_phi_node_to_bb (gimple phi, basic_block bb);
-extern gimple make_phi_node (tree var, int len);
 extern gimple create_phi_node (tree, basic_block);
 extern void add_phi_arg (gimple, tree, edge, source_location);
 extern void remove_phi_args (edge);
