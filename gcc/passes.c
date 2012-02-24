e9d79eef3ca6f45d535cff1b6579e4d33182eb56
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-02-24 11:38:39 +0000

    2012-02-24  Richard Guenther  <rguenther@suse.de>
    	PR middle-end/52361
    	* passes.c (execute_function_todo): When verifying SSA form
    	verify gimple form first.
    	* tree-ssa.c (verify_ssa): Do not verify gimple form here.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@184549 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/passes.c
+++ gcc/passes.c
@@ -1724,11 +1724,14 @@ execute_function_todo (void *data)
 #if defined ENABLE_CHECKING
   if (flags & TODO_verify_ssa
       || (current_loops && loops_state_satisfies_p (LOOP_CLOSED_SSA)))
-    verify_ssa (true);
+    {
+      verify_gimple_in_cfg (cfun);
+      verify_ssa (true);
+    }
+  else if (flags & TODO_verify_stmts)
+    verify_gimple_in_cfg (cfun);
   if (flags & TODO_verify_flow)
     verify_flow_info ();
-  if (flags & TODO_verify_stmts)
-    verify_gimple_in_cfg (cfun);
   if (current_loops && loops_state_satisfies_p (LOOP_CLOSED_SSA))
     verify_loop_closed_ssa (false);
   if (flags & TODO_verify_rtl_sharing)
