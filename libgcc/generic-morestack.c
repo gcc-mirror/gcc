e9e053ebfdc29e3d51d3e3dbe19e8c91c1f96e23
Ian Lance Taylor <ian@gcc.gnu.org>
2012-02-28 17:43:43 +0000

    generic-morestack.c (__splitstack_releasecontext): Correct call to __morestack_release_segments.
    	* generic-morestack.c (__splitstack_releasecontext): Correct call
    	to __morestack_release_segments.
    
    From-SVN: r184634

--- libgcc/generic-morestack.c
+++ libgcc/generic-morestack.c
@@ -1104,7 +1104,9 @@ __splitstack_resetcontext (void *context[10], size_t *size)
 void
 __splitstack_releasecontext (void *context[10])
 {
-  __morestack_release_segments (context[MORESTACK_SEGMENTS], 1);
+  __morestack_release_segments (((struct stack_segment **)
+				 &context[MORESTACK_SEGMENTS]),
+				1);
 }
 
 /* Like __splitstack_block_signals, but operating on CONTEXT, rather
