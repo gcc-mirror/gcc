3a5a825aaecd124c57d3b08ba349631ac8e89123
Richard Guenther <rguenther@suse.de>
2012-02-28 09:15:49 +0000

    re PR tree-optimization/52402 (IPA-SRA creates aligned loads from unaligned memory)
    2012-02-28  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/52402
    	* ipa-prop.c (ipa_modify_call_arguments): Properly use
    	mis-aligned types when creating the accesses at the call site.
    
    	* gcc.dg/torture/pr52402.c: New testcase.
    
    From-SVN: r184619

---
+++ gcc/testsuite/gcc.dg/torture/pr52402.c
@@ -0,0 +1,28 @@
+/* { dg-do run } */
+
+typedef int v4si __attribute__((vector_size(16)));
+struct T { v4si i[2]; int j; } __attribute__((packed));
+
+static v4si __attribute__((noinline))
+foo (struct T t)
+{
+  return t.i[0];
+}
+
+static struct T *__attribute__((noinline))
+init ()
+{
+  char *p = __builtin_malloc (sizeof (struct T) + 1);
+  p++;
+  __builtin_memset (p, 1, sizeof (struct T));
+  return (struct T *)p;
+}
+
+int main()
+{
+  struct T *p;
+  p = init ();
+  if (foo (*p)[0] != 0x01010101)
+    __builtin_abort ();
+  return 0;
+}
