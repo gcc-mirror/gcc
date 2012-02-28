ed67de044190502dc34993e0d43a5f6b83f85147
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-02-28 20:53:35 +0000

    * config/i386/sfp-machine.h (_FP_STRUCT_LAYOUT): Define it         for mingw-targets as attribute gcc_struct.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@184641 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgcc/config/i386/sfp-machine.h
+++ libgcc/config/i386/sfp-machine.h
@@ -1,3 +1,8 @@
+#ifdef __MINGW32__
+  /* Make sure we are using gnu-style bitfield handling.  */
+#define _FP_STRUCT_LAYOUT  __attribute__ ((gcc_struct))
+#endif
+
 #ifdef __x86_64__
 #include "config/i386/64/sfp-machine.h"
 #else
