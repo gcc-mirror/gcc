90f56810247b62e5ad1c435dbe83a91c7974c275
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-03-06 09:54:06 +0000

    2012-03-06  Richard Guenther  <rguenther@suse.de>
    	PR lto/52097
    	* lto.c (uniquify_nodes): Merge TYPE_FIELDS of variant types.
    
    	* gcc.dg/lto/pr52097_0.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@184981 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/lto/pr52097_0.c
@@ -0,0 +1,20 @@
+/* { dg-lto-do link } */
+/* { dg-lto-options { { -O -flto -fexceptions -fnon-call-exceptions --param allow-store-data-races=0 } } } */
+
+typedef struct { unsigned int e0 : 16; } s1;
+typedef struct { unsigned int e0 : 16; } s2;
+typedef struct { s1 i1; s2 i2; } io;
+
+static io *i;
+
+void f1 (void)
+{
+  s1 x0;
+  i->i1 = x0;
+}
+
+int main ()
+{
+  f1 ();
+  return 0;
+}
