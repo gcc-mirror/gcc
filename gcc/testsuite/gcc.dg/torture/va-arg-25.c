26cac03428026f1ae21620032ee66a61c1b0d841
pthaugen <pthaugen@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-03-06 21:54:51 +0000

    * gcc.dg/torture/va-arg-25.c: Fix typo for powerpc64-*-* check.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@185019 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/torture/va-arg-25.c
+++ gcc/testsuite/gcc.dg/torture/va-arg-25.c
@@ -4,7 +4,7 @@
 /* { dg-options "-msse" { target { i?86-*-* x86_64-*-* } } } */
 /* { dg-require-effective-target sse_runtime { target { i?86-*-* x86_64-*-* } } } */
 /* { dg-options "-mabi=altivec -maltivec" { target { powerpc-*-* powerpc64-*-* } } } */
-/* { dg-require-effective-target vmx_hw { target { powerpc-*-* powerpc64--*-* } } } */
+/* { dg-require-effective-target vmx_hw { target { powerpc-*-* powerpc64-*-* } } } */
 
 #include <stdarg.h>
 #include <stdlib.h>
