11a439a1a161cb65507d13de3579a19b8b2c2477
gingold <gingold@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-03-09 13:03:58 +0000

    2012-03-09  Tristan Gingold  <gingold@adacore.com>
    	* ginclude/stddef.h: Do not define __size_t on VMS.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@185137 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ginclude/stddef.h
+++ gcc/ginclude/stddef.h
@@ -203,6 +203,8 @@ typedef __PTRDIFF_TYPE__ ptrdiff_t;
 #if (defined (__FreeBSD__) && (__FreeBSD__ >= 5)) \
   || defined(__FreeBSD_kernel__)
 /* __size_t is a typedef on FreeBSD 5, must not trash it. */
+#elif defined (VMS)
+/* __size_t is also a typedef on VMS.  */
 #else
 #define __size_t
 #endif
