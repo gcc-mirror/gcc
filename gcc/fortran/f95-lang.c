c8cd357b8d2f3963a71f37e4f73c3bea0d206ea2
steven <steven@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-03-09 22:52:33 +0000

    * Make-lang.in (convert.o): Depend on convert.h. 	* convert.c: Header and comment cleanups. 	(gfc_thruthvalue_conversion): Rename static function 	to truthvalue_conversion.  Do not use 'internal_error' from here, 	use 'gcc_unreachable' instead. 	(convert): Do not use 'error' for conversions to void, use 	'gcc_unreachable' instead.  Likewise for conversions to non-scalar 	types.  Do not hanlde ENUMERAL_TYPE, the front end never creates them. 	Clean up #if 0 code.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@185160 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/f95-lang.c
+++ gcc/fortran/f95-lang.c
@@ -1,5 +1,6 @@
 /* gfortran backend interface
-   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2010
+   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
+   2010, 2012
    Free Software Foundation, Inc.
    Contributed by Paul Brook.
 
