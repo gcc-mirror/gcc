43332529edd116f18441629f484c36a645ccb763
Martin Jambor <mjambor@suse.cz>
2012-03-13 15:29:42 +0100

    expr.c (expand_assignment): Handle misaligned scalar writes to memory through top-level MEM_REFs by calling...
    2012-03-13  Martin Jambor  <mjambor@suse.cz>
    
    	* expr.c (expand_assignment): Handle misaligned scalar writes to
    	memory through top-level MEM_REFs by calling store_bit_field.
    
    	* testsuite/gcc.dg/misaligned-expand-2.c: New test.
    
    From-SVN: r185336

---
+++ gcc/testsuite/gcc.dg/misaligned-expand-2.c
@@ -0,0 +1,42 @@
+/* Test that expand can generate correct stores to misaligned data even on
+   strict alignment platforms.  */
+
+/* { dg-do run } */
+/* { dg-options "-O0" } */
+
+extern void abort ();
+
+typedef unsigned int myint __attribute__((aligned(1)));
+
+void
+foo (myint *p, unsigned int i)
+{
+  *p = i;
+}
+
+#define cst 0xdeadbeef
+#define NUM 8
+
+struct blah
+{
+  char c;
+  myint i[NUM];
+};
+
+struct blah g;
+
+#define cst 0xdeadbeef
+
+int
+main (int argc, char **argv)
+{
+  int k;
+
+  for (k = 0; k < NUM; k++)
+    {
+      foo (&g.i[k], cst);
+      if (g.i[k] != cst)
+	abort ();
+    }
+  return 0;
+}
