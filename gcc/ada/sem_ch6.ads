3b8208fd6c1f4b25a42a853467b7ba164c74acf4
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-03-15 08:46:38 +0000

    2012-03-15  Robert Dewar  <dewar@adacore.com>
    	* sem_ch6.ads: Minor comment updates.
    	* sem_ch12.adb: Minor comment clarification.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@185412 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/sem_ch6.ads
+++ gcc/ada/sem_ch6.ads
@@ -29,6 +29,7 @@ package Sem_Ch6 is
    type Conformance_Type is
      (Type_Conformant, Mode_Conformant, Subtype_Conformant, Fully_Conformant);
    --  pragma Ordered (Conformance_Type);
+   --  Why is above line commented out ???
    --  Conformance type used in conformance checks between specs and bodies,
    --  and for overriding. The literals match the RM definitions of the
    --  corresponding terms. This is an ordered type, since each conformance
@@ -149,7 +150,10 @@ package Sem_Ch6 is
    --  on the appropriate component of the New_Id construct if not.
    --  Skip_Controlling_Formals is True when checking the conformance of
    --  a subprogram that implements an interface operation. In that case,
-   --  only the non-controlling formals can (and must) be examined.
+   --  only the non-controlling formals can (and must) be examined. The
+   --  argument Get_Inst is set to True when this is a check against a
+   --  formal access-to-subprogram type, indicating that mapping of types
+   --  is needed.
 
    procedure Check_Type_Conformant
      (New_Id  : Entity_Id;
@@ -167,8 +171,10 @@ package Sem_Ch6 is
       Get_Inst : Boolean := False) return Boolean;
    --  Check that the types of two formal parameters are conforming. In most
    --  cases this is just a name comparison, but within an instance it involves
-   --  generic actual types, and in the presence of anonymous access types it
-   --  must examine the designated types.
+   --  generic actual types, and in the presence of anonymous access types
+   --  it must examine the designated types. The argument Get_Inst is set to
+   --  True when this is a check against a formal access-to-subprogram type,
+   --  indicating that mapping of types is needed.
 
    procedure Create_Extra_Formals (E : Entity_Id);
    --  For each parameter of a subprogram or entry that requires an additional
