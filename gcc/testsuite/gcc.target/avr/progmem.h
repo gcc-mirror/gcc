73f46872b4d140254e9d167138e2e8e92be989c3
Georg-Johann Lay <avr@gjlay.de>
2012-03-20 17:38:29 +0000

    progmem.h (pgm_read_char): Define depending on __AVR_HAVE_LPMX__
    	* gcc.target/avr/progmem.h (pgm_read_char): Define depending on
    	__AVR_HAVE_LPMX__
    
    From-SVN: r185583

--- gcc/testsuite/gcc.target/avr/progmem.h
+++ gcc/testsuite/gcc.target/avr/progmem.h
@@ -5,6 +5,7 @@
             static const char __c[] PROGMEM = (s);          \
             &__c[0];}))
 
+#ifdef __AVR_HAVE_LPMX__
 #define pgm_read_char(addr)                                 \
     (__extension__({                                        \
             unsigned int __addr16 = (unsigned int)(addr);   \
@@ -12,3 +13,13 @@
             __asm__ ("lpm %0, %a1"                          \
                      : "=r" (__result) : "z" (__addr16));   \
             __result; }))
+#else
+#define pgm_read_char(addr)                                 \
+    (__extension__({                                        \
+            unsigned int __addr16 = (unsigned int)(addr);   \
+            char __result;                                  \
+            __asm__ ("lpm" "\n\t"                           \
+                     "mov %0, r0"                           \
+                     : "=r" (__result) : "z" (__addr16));   \
+            __result; }))
+#endif
