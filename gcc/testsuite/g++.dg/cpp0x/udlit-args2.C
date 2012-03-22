d6b0165ded320c9d758b7246da299c3c425bdcb9
Jakub Jelinek <jakub@redhat.com>
2012-03-22 11:10:24 +0100

    backport: re PR c++/52521 ([C++11] user defined literals and order of declaration)
    	Backported from mainline
    	2012-03-14  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/52521
    	* parser.c (lookup_literal_operator): Return fn only if
    	processed all arguments from args vector and argtypes is
    	void_list_node.
    
    	* g++.dg/cpp0x/udlit-args2.C: New test.
    
    From-SVN: r185683

---
+++ gcc/testsuite/g++.dg/cpp0x/udlit-args2.C
@@ -0,0 +1,15 @@
+// PR c++/52521
+// { dg-do compile }
+// { dg-options -std=c++11 }
+
+#include <cstddef>
+
+int operator "" _a (const char *);
+int operator "" _a (const char *, std::size_t);
+int a = 123_a;
+int a2 = "abc"_a;
+
+int operator "" _b (const char *, std::size_t);
+int operator "" _b (const char *);
+int b = 123_b;
+int b2 = "abc"_b;
