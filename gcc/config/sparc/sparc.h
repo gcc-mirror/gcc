4b552df4f1f764cd4007db49bcab3cca7ca6d7ea
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-03-24 18:47:38 +0000

    PR target/52610 	* config/sparc/sparc.h (ASM_CPU_SPEC): Pass -Av8 if -mcpu=leon.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@185765 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sparc/sparc.h
+++ gcc/config/sparc/sparc.h
@@ -329,6 +329,7 @@ extern enum cmodel sparc_cmodel;
 %{mcpu=sparclite86x:-Asparclite} \
 %{mcpu=f930:-Asparclite} %{mcpu=f934:-Asparclite} \
 %{mcpu=v8:-Av8} \
+%{mcpu=leon:-Av8} \
 %{mv8plus:-Av8plus} \
 %{mcpu=v9:-Av9} \
 %{mcpu=ultrasparc:%{!mv8plus:-Av9a}} \
