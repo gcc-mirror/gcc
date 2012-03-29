fe379f13a488d213aa594e6132e0f1f2186a876f
Uros Bizjak <ubizjak@gmail.com>
2012-03-29 21:59:36 +0200

    backport: re PR target/52698 (-maddress-mode=long doesn't work)
    	Backported from mainline
    	2012-03-27  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/52698
    	* config/i386/i386-protos.h (ix86_legitimize_reload_address):
    	New prototype.
    	* config/i386/i386.h (LEGITIMIZE_RELOAD_ADDRESS): New define.
    	* config/i386/i386.c: Include reload.h.
    	(ix86_legitimize_reload_address): New function.
    
    From-SVN: r185975

--- gcc/config/i386/i386-protos.h
+++ gcc/config/i386/i386-protos.h
@@ -57,7 +57,8 @@ extern bool legitimate_constant_p (rtx);
 extern bool constant_address_p (rtx);
 extern bool legitimate_pic_operand_p (rtx);
 extern int legitimate_pic_address_disp_p (rtx);
-
+extern bool ix86_legitimize_reload_address (rtx, enum machine_mode,
+					    int, int, int);
 extern void print_reg (rtx, int, FILE*);
 extern void print_operand (FILE*, rtx, int);
 extern void print_operand_address (FILE*, rtx);
