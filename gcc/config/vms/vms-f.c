c531f06a2a0fa1e00bbba2edfa2deff4843f6036
Tristan Gingold <gingold@adacore.com>
2012-03-30 13:54:23 +0000

    vms-f.c: New file.
    2012-03-30  Tristan Gingold  <gingold@adacore.com>
    
    	* config/vms/vms-f.c: New file.
    	* config/vms/t-vms (vms-f.o): New rule.
    	* config.gcc (*-*-*vms*): Define fortran_target_objs.
    
    From-SVN: r186013

---
+++ gcc/config/vms/vms-f.c
@@ -0,0 +1,31 @@
+/* VMS support needed only by Fortran frontends.
+   Copyright (C) 2012 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#include "config.h"
+#include "system.h"
+#include "coretypes.h"
+#include "tm.h"
+
+void
+vms_c_register_includes (const char *sysroot ATTRIBUTE_UNUSED,
+                         const char *iprefix ATTRIBUTE_UNUSED,
+			 int stdinc ATTRIBUTE_UNUSED)
+{
+  /* No-op for fortran.  */
+}
