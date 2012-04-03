794db76007e39d3193d199c5d0cc23796526650d
Richard Guenther <rguenther@suse.de>
2012-04-03 13:05:58 +0000

    backport: re PR middle-end/52580 (171.swim performance drop on x86 – vectorization doesn’t happen anymore)
    2012-04-03  Richard Guenther  <rguenther@suse.de>
    
    	Backport from mainline
    	2012-03-15  Richard Guenther  <rguenther@suse.de>
    
    	PR middle-end/52580
    	* tree-data-ref.c (subscript_dependence_tester_1): Check
    	all dimensions for non-conflicting access functions.
    
    	* gfortran.dg/vect/pr52580.f: New testcase.
    
    From-SVN: r186107

---
+++ gcc/testsuite/gfortran.dg/vect/pr52580.f
@@ -0,0 +1,33 @@
+! { dg-do compile }
+! { dg-require-effective-target vect_double }
+      SUBROUTINE CALC2
+      IMPLICIT REAL*8	(A-H, O-Z)
+      PARAMETER (N1=1335, N2=1335)
+
+      COMMON  U(N1,N2), V(N1,N2), P(N1,N2),
+     *        UNEW(N1,N2), VNEW(N1,N2),
+     1        PNEW(N1,N2), UOLD(N1,N2),
+     *        VOLD(N1,N2), POLD(N1,N2),
+     2        CU(N1,N2), CV(N1,N2),
+     *        Z(N1,N2), H(N1,N2), PSI(N1,N2)
+      COMMON /CONS/ DT,TDT,DX,DY,A,ALPHA,ITMAX,MPRINT,M,N,MP1,
+     1              NP1,EL,PI,TPI,DI,DJ,PCF
+      TDTS8 = TDT/8.D0
+      TDTSDX = TDT/DX
+      TDTSDY = TDT/DY
+
+      DO 200 J=1,N
+      DO 200 I=1,M
+      UNEW(I+1,J) = UOLD(I+1,J)+
+     1    TDTS8*(Z(I+1,J+1)+Z(I+1,J))*(CV(I+1,J+1)+CV(I,J+1)+CV(I,J)
+     2       +CV(I+1,J))-TDTSDX*(H(I+1,J)-H(I,J))
+      VNEW(I,J+1) = VOLD(I,J+1)-TDTS8*(Z(I+1,J+1)+Z(I,J+1))
+     1       *(CU(I+1,J+1)+CU(I,J+1)+CU(I,J)+CU(I+1,J))
+     2       -TDTSDY*(H(I,J+1)-H(I,J))
+      PNEW(I,J) = POLD(I,J)-TDTSDX*(CU(I+1,J)-CU(I,J))
+     1       -TDTSDY*(CV(I,J+1)-CV(I,J))
+  200 CONTINUE
+      RETURN
+      END
+! { dg-final { scan-tree-dump-times "LOOP VECTORIZED" 1 "vect" } }
+! { dg-final { cleanup-tree-dump "vect" } }
