8a1da634352052e0094ae44c31360e3976239542
Jakub Jelinek <jakub@redhat.com>
2012-04-03 10:49:14 +0200

    re PR middle-end/52835 (-O3 wrongly optimizes loop __builtin_memcpy away)
    	PR tree-optimization/52835
    	* tree-data-ref.c (build_rdg): Return NULL if
    	compute_data_dependences_for_loop failed.
    
    	* gfortran.dg/pr52835.f90: New test.
    
    From-SVN: r186101

---
+++ gcc/testsuite/gfortran.dg/pr52835.f90
@@ -0,0 +1,16 @@
+! PR tree-optimization/52835
+! { dg-do compile }
+! { dg-options "-O3 -fdump-tree-optimized" }
+
+subroutine foo (x, y, z, n)
+  integer :: n, i
+  real :: x(n), y(n), z(n)
+  do i = 1, n
+    z(i) = 0.0
+    y(i) = 0.0
+    call bar (y(i), z(i), x(i))
+  end do
+end subroutine
+
+! { dg-final { scan-tree-dump "bar " "optimized" } }
+! { dg-final { cleanup-tree-dump "optimized" } }
