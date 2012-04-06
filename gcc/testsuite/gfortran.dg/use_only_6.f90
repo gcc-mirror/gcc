631cfe30838acc834c38a7ce89339e144f49000f
Thomas Koenig <tkoenig@gcc.gnu.org>
2012-04-06 18:31:11 +0000

    re PR fortran/52668 (Incorrect unused warning for USE associating variable in common block)
    2012-04-06  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/52668
    	* module.c:  Only mark symbols as use_only if they have been
    	imported via an only list.
    
    2012-04-06  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/52668
    	* gfortran.dg/use_only_6.f90: New test.
    
    From-SVN: r186199

---
+++ gcc/testsuite/gfortran.dg/use_only_6.f90
@@ -0,0 +1,13 @@
+! { dg-do compile }
+! PR 52668 - there used to be a bogus warning about not using b.
+! Original test case by Arnaud Desitter.
+module mm
+  integer :: a, b
+  common /mm1/ a, b
+end module mm
+
+subroutine aa()
+  use mm, only: a
+  implicit none
+  a = 1
+end subroutine aa
