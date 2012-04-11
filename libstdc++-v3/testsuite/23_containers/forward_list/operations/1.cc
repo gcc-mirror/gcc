54e644dbd2c41837842fe7c752f6f606aafa5a36
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-04-11 17:31:57 +0000

    2012-04-11  Paolo Carlini  <paolo.carlini@oracle.com>
    	* include/bits/forward_list.h (splice_after(const_iterator,
    	forward_list&), splice_after(const_iterator, forward_list&,
    	consst_iterator), splice_after(const_iterator, forward_list&,
    	const_iterator, const_iterator), merge(forward_list&),
    	merge(forward_list&, _Comp)): Add per C++11 as published (and
    	LWG 1310).
    	* include/debug/forward_list: Adjust.
    
    	* include/bits/forward_list.h (splice_after(const_iterator,
    	forward_list&&, const_iterator)): Only declare.
    	(_M_transfer_after): Remove.
    	(_M_splice_after(const_iterator, forward_list&&)): Change signature.
    	(splice_after(const_iterator, forward_list&&, const_iterator,
    	const_iterator)): Use the latter.
    	* include/bits/forward_list.tcc (splice_after(const_iterator,
    	forward_list&&, const_iterator)): Define here.
    	(_M_splice_after): Define, use throughout.
    
    	* include/bits/forward_list.h (insert_after(const_iterator,
    	std::initializer_list<_Tp>)): Forward to insert_after(const_iterator,
    	_InputIterator, _InputIterator).
    	* include/bits/forward_list.tcc: Remove definition.
    
    	* testsuite/23_containers/forward_list/modifiers/6.cc: New.
    	* testsuite/23_containers/forward_list/operations/1.cc: Adjust.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@186338 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/23_containers/forward_list/operations/1.cc
+++ libstdc++-v3/testsuite/23_containers/forward_list/operations/1.cc
@@ -1,6 +1,6 @@
 // { dg-options "-std=gnu++0x" }
 
-// Copyright (C) 2008, 2009 Free Software Foundation, Inc.
+// Copyright (C) 2008, 2009, 2012 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -68,7 +68,7 @@ test02()
 
   VERIFY(*befy == 10.0);
   ++befy;
-  VERIFY(*befy == 15.0);
+  VERIFY(*befy == 14.0);
 }
 
 // This test verifies the following:
