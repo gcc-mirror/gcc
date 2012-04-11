a00cc15e3d1142b14dc26805c90f16e7dba0d6ad
Jonathan Wakely <jwakely.gcc@gmail.com>
2012-04-11 22:54:53 +0000

    re PR c++/52924 (Using an std::function object as deleter of shared_ptr in C++0x mode does not compile)
    	PR libstdc++/52924
    	* include/bits/shared_ptr_base.h (_Sp_counted_deleter): Add
    	user-defined destructor.
    	(_Sp_counted_inplace): Likewise.
    	* testsuite/20_util/shared_ptr/cons/52924.cc: New.
    	* testsuite/20_util/shared_ptr/cons/43820_neg.cc: Adjust dg-error
    	line numbers.
    
    From-SVN: r186363

--- libstdc++-v3/testsuite/20_util/shared_ptr/cons/52924.cc
+++ libstdc++-v3/testsuite/20_util/shared_ptr/cons/52924.cc
@@ -1,7 +1,7 @@
 // { dg-options "-std=gnu++0x" }
 // { dg-do compile }
 
-// Copyright (C) 2010, 2011, 2012 Free Software Foundation
+// Copyright (C) 2012 Free Software Foundation
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -18,23 +18,27 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// 20.9.11.2 Template class shared_ptr [util.smartptr.shared]
-
 #include <memory>
 
-// incomplete type
-struct X;
+// libstdc++/52924
 
-// get an auto_ptr rvalue
-std::auto_ptr<X>&& ap();
+struct A { } a;
 
-void test01()
+struct D {
+  ~D() noexcept(false) { }
+  void operator()(A*) { }
+} d;
+
+auto sp = std::shared_ptr<A>(&a, d);
+
+template<typename T>
+struct Alloc : std::allocator<T>
 {
-  X* px = 0;
-  std::shared_ptr<X> p1(px);   // { dg-error "here" }
-  // { dg-error "incomplete" "" { target *-*-* } 775 }
+  Alloc() = default;
+  ~Alloc() noexcept(false) { }
+  template<typename U> Alloc(const Alloc<U>&) { }
+};
 
-  std::shared_ptr<X> p9(ap());  // { dg-error "here" }
-  // { dg-error "incomplete" "" { target *-*-* } 869 }
+Alloc<A> al;
 
-}
+auto as = std::allocate_shared<A>(al);
