da97611d45bc266f3f6b0fc502bb2e7a0cee2529
Oleg Endo <olegendo@gcc.gnu.org>
2012-04-11 23:01:12 +0000

    re PR target/50751 (SH Target: Displacement addressing does not work for QImode and HImode)
    	PR target/50751
    	* gcc/target/sh/pr50751-4.c: New.
    	* gcc/target/sh/pr50751-5.c: New.
    	* gcc/target/sh/pr50751-6.c: New.
    	* gcc/target/sh/pr50751-7.c: New.
    
    From-SVN: r186365

---
+++ gcc/testsuite/gcc.target/sh/pr50751-5.c
@@ -0,0 +1,27 @@
+/* Check that the mov.w displacement addressing insn is generated and the 
+   base address is adjusted only once.  On SH2A this test is skipped because
+   there is a 4 byte mov.w insn that can handle larger displacements.  Thus
+   on SH2A the base address will not be adjusted in this case.  */
+/* { dg-do compile { target "sh*-*-*" } } */
+/* { dg-options "-O1" } */
+/* { dg-skip-if "" { "sh*-*-*" } { "-m5*" "-m2a*" } { "" } } */
+/* { dg-final { scan-assembler-times "add" 2 } } */
+
+void
+testfunc_00 (const short* ap, short* bp)
+{
+  bp[0] = ap[15];
+  bp[2] = ap[5];
+  bp[9] = ap[7];
+  bp[0] = ap[25];
+}
+
+void
+testfunc_01 (volatile const short* ap, volatile short* bp)
+{
+  bp[0] = ap[15];
+  bp[2] = ap[5];
+  bp[9] = ap[7];
+  bp[0] = ap[25];
+}
+
