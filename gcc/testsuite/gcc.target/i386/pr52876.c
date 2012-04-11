e623c80adcc35bf0f332fb0b4601a7ab6c78431d
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-04-11 19:31:45 +0000

    Check for incompatible pointer sign extension
    gcc/
    
    	PR rtl-optimization/52876
    	* emit-rtl.c (set_reg_attrs_from_value): Handle arbitrary value.
    	Don't call mark_reg_pointer for incompatible pointer sign
    	extension.
    
    	* reginfo.c (reg_scan_mark_refs): Call set_reg_attrs_from_value 
    	directly.
    
    gcc/testsuite
    
    	PR rtl-optimization/52876
    	* gcc.target/i386/pr52876.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@186351 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr52876.c
@@ -0,0 +1,25 @@
+/* { dg-do run { target { x32 } } } */
+/* { dg-options "-O2 -mx32 -maddress-mode=long" } */
+
+extern void abort (void);
+
+long long li;
+
+long long 
+__attribute__ ((noinline))
+testfunc (void* addr)
+{
+  li = (long long)(int)addr;
+  li &= 0xffffffff;
+  return li;
+}
+
+int main (void)
+{
+  volatile long long rv_test;
+  rv_test = testfunc((void*)0x87651234);
+  if (rv_test != 0x87651234ULL)
+    abort ();
+
+  return 0;
+}
