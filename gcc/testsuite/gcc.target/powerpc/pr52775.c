a3807e4f1fa93945558d8727423e7c36633db748
Michael Meissner <meissner@linux.vnet.ibm.com>
2012-04-12 21:00:07 +0000

    backport: re PR target/52775 (Change default for using FCFID instruction)
    [gcc]
    2012-04-12  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2012-04-12  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	PR target/52775
    	* config/rs6000/rs6000.h (TARGET_FCFID): Add TARGET_PPC_GPOPT to
    	the list of options to enable the FCFID instruction.
    	(TARGET_EXTRA_BUILTINS): Adjust comment.
    
    [gcc/testsuite]
    012-04-12  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2012-04-12  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	PR target/52775
    	* gcc.target/powerpc/pr52775.c: New file.
    
    From-SVN: r186392

---
+++ gcc/testsuite/gcc.target/powerpc/pr52775.c
@@ -0,0 +1,16 @@
+/* { dg-do compile { target { powerpc*-*-* && ilp32 } } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
+/* { dg-options "-O1 -mcpu=power4" } */
+/* { dg-final { scan-assembler-times "fcfid" 2 } } */
+
+double
+int_to_double (int *p)
+{
+  return (double)*p;
+}
+
+double
+long_long_to_double (long long *p)
+{
+  return (double)*p;
+}
