6bf40f52a26688085a5db081ea87f5862a7fd88d
Paolo Carlini <paolo@gcc.gnu.org>
2012-04-12 12:18:23 +0000

    re PR libstdc++/52942 (using std::ref with a std::unordered_map fails to compile)
    2012-04-12  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR libstdc++/52942
    	* include/bits/stl_function.h (_Identity, _Select1st, _Select2nd):
    	In C++11 mode do not derive from std::unary_function.
    	* include/ext/functional (identity, select1st, select2nd): Adjust.
    	* testsuite/23_containers/unordered_map/requirements/52942.cc: New.
    	* testsuite/23_containers/unordered_set/requirements/52942.cc: Likewise.
    
    From-SVN: r186376

---
+++ libstdc++-v3/testsuite/23_containers/unordered_map/requirements/52942.cc
@@ -0,0 +1,37 @@
+// { dg-do compile }
+// { dg-options "-std=gnu++11" }
+
+// Copyright (C) 2012 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without Pred the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <unordered_map>
+#include <functional>
+
+struct TFoo {};
+
+struct TFoo_hash
+{
+  std::size_t operator()(const TFoo &) const { return 0; }
+};
+
+void f1(std::unordered_map<TFoo, int, TFoo_hash> &) {}
+
+void f2()
+{
+  std::unordered_map<TFoo, int, TFoo_hash> map1;
+  std::bind(f1, std::ref(map1));
+}
