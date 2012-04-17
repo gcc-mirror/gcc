1730021a778cc07d1c7033c19bd5fd5eaae0eaca
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-04-17 16:41:40 +0000

    2012-04-17  Benjamin Kosnik  <bkoz@redhat.com>
    	* testsuite/20_util/specialized_algorithms/uninitialized_copy/
    	808590.cc: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@186539 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/20_util/specialized_algorithms/uninitialized_copy/808590.cc
@@ -0,0 +1,48 @@
+// Copyright (C) 2012 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without Pred the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <vector>
+#include <stdexcept>
+
+// 4.4.x only
+struct c 
+{
+  void *m;
+
+  c(void* o = 0) : m(o) {}
+  c(const c &r) : m(r.m) {}
+
+  template<class T>
+    explicit c(T &o) : m((void*)0xdeadfbeef) { }
+};
+
+int main() 
+{
+  std::vector<c> cbs;
+  const c cb((void*)0xcafebabe);
+
+  for (int fd = 62; fd < 67; ++fd) 
+    {
+      cbs.resize(fd + 1);
+      cbs[fd] = cb;
+    }
+
+  for (int fd = 62; fd< 67; ++fd)
+    if (cb.m != cbs[fd].m)
+      throw std::runtime_error("wrong");
+  return 0;
+}
