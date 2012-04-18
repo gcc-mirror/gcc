e24c256de02eecbde481f48e19ed3779f4020739
jye2 <jye2@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-04-18 09:04:09 +0000

    2012-04-18  Joey Ye  <joey.ye@arm.com>
            * gcc.target/arm/thumb1-imm.c: Skip it in non-thumb1 target.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@186563 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/thumb1-imm.c
+++ gcc/testsuite/gcc.target/arm/thumb1-imm.c
@@ -1,5 +1,7 @@
 /* Check for thumb1 imm [255-510] moves.  */
 /* { dg-require-effective-target arm_thumb1_ok } */
+/* { dg-options "-Os" } */
+/* { dg-skip-if "" { ! { arm_thumb1 } } } */
 
 int f()
 {
