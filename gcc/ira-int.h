d0a854af24839f665a04eaeb3f828c79587568cc
Richard Guenther <rguenther@suse.de>
2012-04-19 12:44:07 +0000

    ira-int.h (ira_allocno_object_iter_cond): Avoid out-of-bound array access.
    2012-04-19  Richard Guenther  <rguenther@suse.de>
    
    	* ira-int.h (ira_allocno_object_iter_cond): Avoid out-of-bound
    	array access.
    
    From-SVN: r186590

--- gcc/ira-int.h
+++ gcc/ira-int.h
@@ -1138,8 +1138,13 @@ static inline bool
 ira_allocno_object_iter_cond (ira_allocno_object_iterator *i, ira_allocno_t a,
 			      ira_object_t *o)
 {
-  *o = ALLOCNO_OBJECT (a, i->n);
-  return i->n++ < ALLOCNO_NUM_OBJECTS (a);
+  int n = i->n++;
+  if (n < ALLOCNO_NUM_OBJECTS (a))
+    {
+      *o = ALLOCNO_OBJECT (a, n);
+      return true;
+    }
+  return false;
 }
 
 /* Loop over all objects associated with allocno A.  In each
