9aa411205ba1898dfbd4fb4d5a3a5886907b28dc
Thomas Schwinge <thomas@codesourcery.com>
2012-04-20 10:08:13 +0200

    struct siginfo vs. siginfo_t
    	Backport from trunk (but apply to gcc/):
    
    gcc/
    	2012-04-20  Thomas Schwinge  <thomas@codesourcery.com>
    
    	* config/alpha/linux-unwind.h (alpha_fallback_frame_state): Use
    	siginfo_t instead of struct siginfo.
    	* config/bfin/linux-unwind.h (bfin_fallback_frame_state): Likewise.
    	* config/i386/linux-unwind.h (x86_fallback_frame_state): Likewise.
    	* config/ia64/linux-unwind.h (ia64_fallback_frame_state)
    	(ia64_handle_unwabi): Likewise.
    	* config/mips/linux-unwind.h (mips_fallback_frame_state): Likewise.
    	* config/pa/linux-unwind.h (pa32_fallback_frame_state): Likewise.
    	* config/sh/linux-unwind.h (shmedia_fallback_frame_state)
    	(sh_fallback_frame_state): Likewise.
    	* config/xtensa/linux-unwind.h (xtensa_fallback_frame_state): Likewise.
    
    From-SVN: r186612

--- gcc/config/bfin/linux-unwind.h
+++ gcc/config/bfin/linux-unwind.h
@@ -1,5 +1,5 @@
 /* DWARF2 EH unwinding support for Blackfin.
-   Copyright (C) 2007, 2009 Free Software Foundation, Inc.
+   Copyright (C) 2007, 2009, 2012 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
@@ -48,10 +48,10 @@ bfin_fallback_frame_state (struct _Unwind_Context *context,
     {
       struct rt_sigframe {
 	int sig;
-	struct siginfo *pinfo;
+	siginfo_t *pinfo;
 	void *puc;
 	char retcode[8];
-	struct siginfo info;
+	siginfo_t info;
 	struct ucontext uc;
       } *rt_ = context->cfa;
 
