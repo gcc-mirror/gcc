0d4665cf922a06daa10b49f3a10231763b4fb66b
Thomas Schwinge <thomas@codesourcery.com>
2012-04-20 10:14:00 +0200

    struct siginfo vs. siginfo_t
    	Backport from trunk (but apply to gcc/):
    
    	2012-04-20  Thomas Schwinge  <thomas@codesourcery.com>
    
    gcc/
    	* config/alpha/linux-unwind.h (alpha_fallback_frame_state): Use
    	siginfo_t instead of struct siginfo.
    	* config/bfin/linux-unwind.h (bfin_fallback_frame_state): Likewise.
    	* config/i386/linux-unwind.h (x86_fallback_frame_state): Likewise.
    	* config/ia64/linux-unwind.h (ia64_fallback_frame_state)
    	(ia64_handle_unwabi): Likewise.
    	* config/mips/linux-unwind.h (mips_fallback_frame_state): Likewise.
    	* config/pa/linux-unwind.h (pa32_fallback_frame_state): Likewise.
    	* config/sh/linux-unwind.h (shmedia_fallback_frame_state)
    	(sh_fallback_frame_state): Likewise.
    	* config/xtensa/linux-unwind.h (xtensa_fallback_frame_state): Likewise.
    
    From-SVN: r186613

--- gcc/config/i386/linux-unwind.h
+++ gcc/config/i386/linux-unwind.h
@@ -1,5 +1,5 @@
 /* DWARF2 EH unwinding support for AMD x86-64 and x86.
-   Copyright (C) 2004, 2005, 2006, 2009 Free Software Foundation, Inc.
+   Copyright (C) 2004, 2005, 2006, 2009, 2012 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
@@ -133,9 +133,9 @@ x86_fallback_frame_state (struct _Unwind_Context *context,
     {
       struct rt_sigframe {
 	int sig;
-	struct siginfo *pinfo;
+	siginfo_t *pinfo;
 	void *puc;
-	struct siginfo info;
+	siginfo_t info;
 	struct ucontext uc;
       } *rt_ = context->cfa;
       /* The void * cast is necessary to avoid an aliasing warning.
--- gcc/config/mips/linux-unwind.h
+++ gcc/config/mips/linux-unwind.h
@@ -1,5 +1,6 @@
 /* DWARF2 EH unwinding support for MIPS Linux.
-   Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
+   Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2012 Free Software
+   Foundation, Inc.
 
 This file is part of GCC.
 
@@ -75,7 +76,7 @@ mips_fallback_frame_state (struct _Unwind_Context *context,
       struct rt_sigframe {
 	u_int32_t ass[4];  /* Argument save space for o32.  */
 	u_int32_t trampoline[2];
-	struct siginfo info;
+	siginfo_t info;
 	_sig_ucontext_t uc;
       } *rt_ = context->cfa;
       sc = &rt_->uc.uc_mcontext;
