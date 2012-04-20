c522573e048c698f1fdcf2442e3a88648e9006bf
Michael Matz <matz@suse.de>
2012-04-20 14:40:09 +0000

    error.c (pedwarn_cxx98): Move va_end call after user of the va_list.
    	* error.c (pedwarn_cxx98): Move va_end call after user
    	of the va_list.
    
    From-SVN: r186626

--- gcc/cp/error.c
+++ gcc/cp/error.c
@@ -3352,13 +3352,15 @@ pedwarn_cxx98 (location_t location, int opt, const char *gmsgid, ...)
 {
   diagnostic_info diagnostic;
   va_list ap;
+  bool ret;
 
   va_start (ap, gmsgid);
   diagnostic_set_info (&diagnostic, gmsgid, &ap, location,
 		       (cxx_dialect == cxx98) ? DK_PEDWARN : DK_WARNING);
   diagnostic.option_index = opt;
+  ret = report_diagnostic (&diagnostic);
   va_end (ap);
-  return report_diagnostic (&diagnostic);
+  return ret;
 }
 
 /* Issue a diagnostic that NAME cannot be found in SCOPE.  DECL is what
