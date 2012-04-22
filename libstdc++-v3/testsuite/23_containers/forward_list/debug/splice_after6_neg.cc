7c688aded43731835cfd13566fe8240c346cc20d
Jonathan Wakely <jwakely.gcc@gmail.com>
2012-04-22 13:27:16 +0000

    forward_list (forward_list::splice_after): Check allocators are equal.
    	* include/debug/forward_list (forward_list::splice_after): Check
    	allocators are equal.
    	* src/c++11/debug.cc: Fix spelling.
    	* testsuite/23_containers/forward_list/debug/splice_after5_neg.cc:
    	New.
    	* testsuite/23_containers/forward_list/debug/splice_after6_neg.cc:
    	Likewise.
    	* testsuite/23_containers/forward_list/debug/splice_after7_neg.cc:
    	Likewise.
    
    From-SVN: r186669

---
+++ libstdc++-v3/testsuite/23_containers/forward_list/debug/splice_after6_neg.cc
@@ -0,0 +1,41 @@
+// { dg-options "-std=gnu++0x" }
+// { dg-require-debug-mode "" }
+// { dg-do run { xfail *-*-* } }
+
+// Copyright (C) 2012 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without Pred the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <forward_list>
+#include <testsuite_allocator.h>
+
+void
+test01()
+{
+  typedef __gnu_test::uneq_allocator<int> alloc_type;
+
+  std::forward_list<int, alloc_type> fl1({1, 2, 3}, alloc_type(1));
+  std::forward_list<int, alloc_type> fl2({1, 2, 3}, alloc_type(2));
+
+  fl1.splice_after(fl1.before_begin(), fl2, fl2.begin());
+}
+
+int
+main()
+{
+  test01();
+  return 0;
+}
