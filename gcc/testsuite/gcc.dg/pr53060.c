54b9f838faa635b6ae638cbc05195b8b181d4cc3
Richard Biener <rguenth@gcc.gnu.org>
2012-04-23 10:20:05 +0000

    re PR c/53060 (Typo in build_binary_op for scalar-vector ops)
    2012-04-23  Richard Guenther  <rguenther@suse.de>
    
    	PR c/53060
    	* c-typeck.c (build_binary_op): Fix typo.
    
    	* gcc.dg/pr53060.c: New testcase.
    
    From-SVN: r186696

---
+++ gcc/testsuite/gcc.dg/pr53060.c
@@ -0,0 +1,24 @@
+/* { dg-do run } */
+
+extern void abort (void);
+
+int                     f(void) { return 2; }
+unsigned int            g(void) { return 5; }
+unsigned int            h = 1;
+
+typedef unsigned int vec __attribute__((vector_size(16)));
+
+vec i = { 1, 2, 3, 4};
+
+vec fv1(void) { return i + (h ? f() : g()); }
+vec fv2(void) { return (h ? f() : g()) + i; }
+
+int main()
+{
+  vec i, j;
+  j = fv1();
+  if (j[0] != 3) abort();
+  i = fv2();
+  if (i[0] != 3) abort();
+  return 0;
+}
