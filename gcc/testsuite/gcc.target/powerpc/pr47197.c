24d5b351b1bb9d2fd66202cbf76857c404e06e85
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2012-04-24 15:51:58 +0000

    re PR target/47197 (ICE in gimplify_expr, at gimplify.c:7153 on AltiVec code (vec_dst))
    gcc:
    
    2012-04-24  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	PR target/47197
    	* config/rs6000/rs6000-c.c (fully_fold_convert): New function.
    	(altivec_build_resolved_builtin): Call fully_fold_convert.
    
    gcc/testsuite:
    
    2012-04-24  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	PR target/47197
    	* gcc.target/powerpc/pr47197.c: New test.
    
    From-SVN: r186771

---
+++ gcc/testsuite/gcc.target/powerpc/pr47197.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-options "-maltivec" } */
+
+/* Compile-only test to ensure that expressions can be passed to
+   Altivec builtins without error.  */
+
+#include <altivec.h>
+
+void func(unsigned char *buf, unsigned len)
+{
+        vec_dst(buf, (len >= 256 ? 0 : len) | 512, 2);
+}
