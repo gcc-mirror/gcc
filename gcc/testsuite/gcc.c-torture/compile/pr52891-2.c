49c567f51e11bf7b939026323eb8db841089d04d
gjl <gjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-04-24 17:18:54 +0000

    PR testsuite/52641 	PR tree-optimizations/52891 	* gcc.c-torture/compile/pr52891-2.c: Fix test for 16-bit int.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@186776 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/compile/pr52891-2.c
+++ gcc/testsuite/gcc.c-torture/compile/pr52891-2.c
@@ -1,6 +1,10 @@
 /* PR tree-optimizations/52891 */
 
+#if __SIZEOF_INT__ > 2
 struct __attribute__((packed)) S { unsigned s : 22; };
+#else
+struct __attribute__((packed)) S { unsigned s : 12; };
+#endif
 struct __attribute__((packed)) T { struct S t; } c;
 int a, b, d;
 
