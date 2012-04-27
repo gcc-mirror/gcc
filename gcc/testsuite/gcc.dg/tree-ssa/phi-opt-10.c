678919fdc043aa1bae6cf8ca68402bd9d54396ac
bonzini <bonzini@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-04-27 12:20:01 +0000

    2012-04-27  Paolo Bonzini  <bonzini@gnu.org>
    	* tree-ssa-phiopt.c (conditional_replacement): Replace PHIs
    	whose arguments are -1 and 0, by negating the result of the
    	conditional.
    
    testsuite:
    2012-04-27  Paolo Bonzini  <bonzini@gnu.org>
    
    	* gcc.c-torture/execute/20120427-2.c: New testcase.
    	* gcc.dg/tree-ssa/phi-opt-10.c: New testcase.
    	* gcc.dg/tree-ssa/ssa-pre-28.c: Bypass new optimization.
    	* gcc.dg/tree-ssa/ssa-ifcombine-7.c: Look into ifcombine dump.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@186905 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/phi-opt-10.c
@@ -0,0 +1,11 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-optimized" } */
+
+int nem1_phi (unsigned long a) { return a ? -1 : 0; }
+int eqm1_phi (unsigned long a) { return a ? 0 : -1; }
+
+int spaceship1 (long a) { return a > 0 ? 1 : a < 0 ? -1 : 0; }
+int spaceship2 (long a) { return a > 0 ? 1 : a == 0 ? 0 : -1; }
+
+/* { dg-final { scan-tree-dump-times " = -D" 4 "optimized"} } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
