394cb8e0f7f554f687f4572b7fff208ff08c3350
rearnsha <rearnsha@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-04-30 13:41:49 +0000

    2012-04-24  Ian Bolton  <ian.bolton at arm.com>             Sameera Deshpande  <sameera.deshpande at arm.com>             Greta Yorsh  <greta.yorsh at arm.com>
            * config/arm/arm-protos.h (ldm_stm_operation_p): New parameters.
            * config/arm/arm.c (ldm_stm_operation_p): New parameters.
            * config/arm/predicates.md (load_multiple_operation): Add arguments.
            (store_multiple_operation): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@186981 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/arm/arm-protos.h
+++ gcc/config/arm/arm-protos.h
@@ -62,7 +62,8 @@ extern bool arm_legitimize_reload_address (rtx *, enum machine_mode, int, int,
 extern rtx thumb_legitimize_reload_address (rtx *, enum machine_mode, int, int,
 					    int);
 extern int thumb1_legitimate_address_p (enum machine_mode, rtx, int);
-extern bool ldm_stm_operation_p (rtx, bool);
+extern bool ldm_stm_operation_p (rtx, bool, enum machine_mode mode,
+                                 bool, bool);
 extern int arm_const_double_rtx (rtx);
 extern int neg_const_double_rtx_ok_for_fpa (rtx);
 extern int vfp3_const_double_rtx (rtx);
