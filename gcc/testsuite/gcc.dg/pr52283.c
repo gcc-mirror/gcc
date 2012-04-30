b944e97a922d85b404cba6c4a4f2fbcf8e7701b8
Greta Yorsh <greta.yorsh@arm.com>
2012-04-30 14:56:51 +0100

    Fix failure in gcc.dg/pr52283.c by adding the missing dg-warning and dg-options.
    From-SVN: r186985

--- gcc/testsuite/gcc.dg/pr52283.c
+++ gcc/testsuite/gcc.dg/pr52283.c
@@ -1,6 +1,7 @@
 /* Test for case labels not integer constant expressions but folding
    to integer constants (used in Linux kernel).  */
 /* { dg-do compile } */
+/* { dg-options "-pedantic" } */
 
 extern unsigned int u;
 
@@ -9,7 +10,7 @@ b (int c)
 {
   switch (c)
     {
-    case (int) (2  | ((4 < 8) ? 8 : u)):
+    case (int) (2  | ((4 < 8) ? 8 : u)): /* { dg-warning "case label is not an integer constant expression" } */
       ;
     }
 }
