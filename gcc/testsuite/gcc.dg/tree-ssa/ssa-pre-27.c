1ef2db757f69a5e31d5bd68768b71f6d12262ab5
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-05-03 13:07:31 +0000

    2012-05-03  Richard Guenther  <rguenther@suse.de>
    	* tree-ssa-pre.c (valid_in_sets): Remove checking of trapping
    	operations.
    	(prune_clobbered_mems): Do it here.  Do not uselessly sort
    	expressions.
    	(compute_avail): Do not add possibly trapping operations to
    	EXP_GEN if they might not be executed in the block.
    
    	* gcc.dg/tree-ssa/ssa-pre-27.c: Remove XFAIL.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@187096 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/ssa-pre-27.c
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-pre-27.c
@@ -17,13 +17,12 @@ int foo2 (int i, int j, int b)
   int res = 0;
   if (b)
     res = i/j;
-  /* But we fail so here because of the possibly not returning
-     call in the same basic-block.  */
+  /* And here, the possibly not returning call in the same basic-block
+     comes after the trapping i/j.  */
   res += i/j;
   bar ();
   return res;
 }
 
-/* { dg-final { scan-tree-dump-times "# prephitmp" 1 "pre" } } */
-/* { dg-final { scan-tree-dump-times "# prephitmp" 2 "pre" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-times "# prephitmp" 2 "pre" } } */
 /* { dg-final { cleanup-tree-dump "pre" } } */
