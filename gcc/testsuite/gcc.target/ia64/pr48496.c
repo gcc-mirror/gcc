a2f2d218dd337a6508d504e477a3ae5371069d59
Eric Botcazou <ebotcazou@adacore.com>
2012-05-04 11:01:34 +0000

    re PR target/48496 ('asm' operand requires impossible reload)
    	PR target/48496
    	* recog.c (constrain_operands): If extra constraints are present, also
    	accept pseudo-registers with equivalent memory locations during reload.
    
    From-SVN: r187150

---
+++ gcc/testsuite/gcc.target/ia64/pr48496.c
@@ -0,0 +1,24 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+
+typedef unsigned int UINT64 __attribute__((__mode__(__DI__)));
+
+typedef struct
+{
+  UINT64 x[2] __attribute__((aligned(16)));
+} fpreg;
+
+struct ia64_args
+{
+  fpreg fp_regs[8];
+  UINT64 gp_regs[8];
+};
+
+ffi_call(long i, long gpcount, long fpcount, void **avalue)
+{
+  struct ia64_args *stack;
+  stack = __builtin_alloca (64);
+  asm ("stf.spill %0 = %1%P0" : "=m" (*&stack->fp_regs[fpcount++])
+                              : "f"(*(double *)avalue[i]));
+  stack->gp_regs[gpcount++] = *(UINT64 *)avalue[i];
+}
