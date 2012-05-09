99b113f9789e6fd7aa0f7fe1cc7cccc22d7d2fc1
Dehao Chen <dehao@google.com>
2012-05-09 07:10:45 +0000

    predict-1.c: Remove the replicated text in this test.
    gcc/testsuite
    
    	* gcc.dg/predict-1.c: Remove the replicated text in this test.
    	* gcc.dg/predict-2.c: Likewise.
    	* gcc.dg/predict-3.c: Likewise.
    	* gcc.dg/predict-4.c: Likewise.
    	* gcc.dg/predict-5.c: Likewise.
    	* gcc.dg/predict-6.c: Likewise.
    
    From-SVN: r187315

--- gcc/testsuite/gcc.dg/predict-2.c
+++ gcc/testsuite/gcc.dg/predict-2.c
@@ -25,30 +25,3 @@ void foo (int base, int bound)
 
 /* { dg-final { scan-tree-dump-not "loop iv compare heuristics" "profile_estimate"} } */
 /* { dg-final { cleanup-tree-dump "profile_estimate" } } */
-/* { dg-do compile } */
-/* { dg-options "-O2 -fdump-tree-profile_estimate" } */
-
-extern int global;
-
-int bar(int);
-
-void foo (int base, int bound)
-{
-  int i, ret = 0;
-  for (i = base; i < bound; i++)
-    {
-      if (i > bound * bound)
-	global += bar (i);
-      if (i > bound + 10)
-	global += bar (i);
-      if (i <= bound + 10)
-	global += bar (i);
-      if (i > base + 10)
-	global += bar (i);
-      if (i < base - 10)
-	global += bar (i);
-    }
-}
-
-/* { dg-final { scan-tree-dump-not "loop iv compare heuristics" "profile_estimate"} } */
-/* { dg-final { cleanup-tree-dump "profile_estimate" } } */
