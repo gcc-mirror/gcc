88fe37530ce368e67bbb0a3a7a9641bfcdd27606
tkoenig <tkoenig@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-05-11 18:50:14 +0000

    2012-05-11  Thomas Koenig  <tkoenig@gcc.gnu.org>
    	PR fortran/52537
    	* gfortran.dg/string_compare_4.f90:  Change option
    	to -fdump-tree-original.  Add test case for kind=4.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@187413 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/string_compare_4.f90
+++ gcc/testsuite/gfortran.dg/string_compare_4.f90
@@ -1,12 +1,15 @@
 ! { dg-do compile }
-! { dg-options "-ffrontend-optimize -fdump-fortran-original" }
+! { dg-options "-ffrontend-optimize -fdump-tree-original" }
 ! PR fortran/52537 - optimize comparisons with empty strings
 program main
   implicit none
   character(len=10) :: a
   character(len=30) :: line
+  character(len=4,kind=4) :: c4
   line = 'x'
   read (unit=line,fmt='(A)') a
+  c4 = 4_'foo'
+  if (c4 == 4_' ') print *,"foobar"
   if (trim(a) == '') print *,"empty"
   call foo(a)
   if (trim(a) == '    ') print *,"empty"
@@ -16,5 +19,5 @@ contains
     if (b /= '   ') print *,"full"
   end subroutine foo
 end program main
-! { dg-final { scan-tree-dump-times "_gfortran_string_len_trim" 3 "original" } }
+! { dg-final { scan-tree-dump-times "_gfortran_string_len_trim" 4 "original" } }
 ! { dg-final { cleanup-tree-dump "original" } }
