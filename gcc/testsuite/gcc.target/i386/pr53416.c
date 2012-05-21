8203380acdc9b8636b70c576ed417294a53e4feb
H.J. Lu <hongjiu.lu@intel.com>
2012-05-21 13:07:11 +0000

    Use unspec_volatile on rdrand<mode>_1
    gcc/
    
    	Backport from mainline
    	PR target/53416
    	* config/i386/i386.md (UNSPEC_RDRAND): Renamed to ...
    	(UNSPECV_RDRAND): This.
    	(rdrand<mode>_1): Updated.
    
    
    gcc/testsuite/
    
    	Backport from mainline
    	PR target/53416
    	* gcc.target/i386/pr53416.c: New file.
    
    From-SVN: r187713

---
+++ gcc/testsuite/gcc.target/i386/pr53416.c
@@ -0,0 +1,17 @@
+/* PR target/53416 */
+/* { dg-options "-O2 -mrdrnd" } */
+
+int test (void)
+{
+  unsigned int number = 0;
+  int result0, result1, result2, result3;
+
+  result0 = __builtin_ia32_rdrand32_step (&number);
+  result1 = __builtin_ia32_rdrand32_step (&number);
+  result2 = __builtin_ia32_rdrand32_step (&number);
+  result3 = __builtin_ia32_rdrand32_step (&number);
+
+  return result0 + result1 +result2 + result3;
+}
+
+/* { dg-final { scan-assembler-times "rdrand" 4 } } */
