6d199d3c08ec25787b76154a5d324d63561bccde
Paolo Carlini <paolo.carlini@oracle.com>
2012-05-25 23:40:10 +0000

    re PR c++/32054 (Storage classes on anonymous unions in classes)
    /cp
    2012-05-25  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/32054
    	* parser.c (cp_parser_member_declaration): A storage class is not
    	allowed in a declaration of an anonymous aggregate in a class scope.
    
    /testsuite
    2012-05-25  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/32054
    	* g++.dg/other/anon-union3.C: New.
    
    From-SVN: r187902

---
+++ gcc/testsuite/g++.dg/other/anon-union3.C
@@ -0,0 +1,25 @@
+// PR c++/32054
+
+class C
+{
+  auto union      // { dg-error "storage class" "" { target c++98 } }
+    {
+      int a;
+    };            // { dg-error "multiple|specified" "" { target c++11 } }
+  register union  // { dg-error "storage class" }
+    {
+      int b;
+    };
+  static union    // { dg-error "storage class" }
+    {
+      int c;
+    };
+  extern union    // { dg-error "storage class" }
+    {
+      int d;
+    };
+  mutable union   // { dg-error "storage class" }
+    {
+      int e;
+    };
+};
