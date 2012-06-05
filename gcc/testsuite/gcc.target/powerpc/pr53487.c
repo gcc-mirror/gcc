d7685183c11486a86721e03c8b3b64fcfd44f0cc
Michael Meissner <meissner@linux.vnet.ibm.com>
2012-06-05 19:40:34 +0000

    re PR target/53487 (Unrecognizable insn for conditional move)
    [gcc]
    2012-06-04  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	PR target/53487
    	* config/rs6000/rs6000.c (rs6000_generate_compare): If we are
    	doing an unsigned compare, make sure the second argument is not a
    	negative constant.
    	(rs6000_emit_cmove): Don't allow floating point comparisons when
    	generating ISEL moves.
    
    [gcc/testsuite]
    2012-06-04  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	* gcc.target/powerpc/pr53487.c: New test.
    
    From-SVN: r188248

---
+++ gcc/testsuite/gcc.target/powerpc/pr53487.c
@@ -0,0 +1,27 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
+/* { dg-require-effective-target powerpc_vsx_ok } */
+/* { dg-options "-O3 -mcpu=power7 -misel -ffast-math" } */
+
+struct phylo_s {
+  int left;
+};
+
+int Cluster(float **dmx, int N, struct phylo_s *tree)
+{
+  float **mx;
+  int *coord;
+  int i;
+  int Np;
+  int row, col;
+  float min;
+  for (col = 0; col < N; Np--)
+    {
+      for (row = 0; row < Np; row++)
+	for (col = row+1; col < Np; col++)
+	  if (mx[row][col] < min)
+	    i = row;
+      tree[Np-2].left = coord[i];
+    }
+  Free2DArray((void **) mx, N);
+}
