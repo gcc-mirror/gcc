519b3112f6923134094e8fcb51bdc88bebd95d1e
janis <janis@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-06-12 20:27:13 +0000

    * gcc.target/arm/di-longlong64-sync-withhelpers.c: Add comments 	to checks for multiple messages reported for one line of source code. 	* gcc.target/arm/di-longlong64-sync-withldrexd.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@188480 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/di-longlong64-sync-withhelpers.c
+++ gcc/testsuite/gcc.target/arm/di-longlong64-sync-withhelpers.c
@@ -2,8 +2,8 @@
 /* { dg-require-effective-target arm_arch_v5_ok } */
 /* { dg-options "-std=gnu99" } */
 /* { dg-add-options arm_arch_v5 } */
-/* { dg-message "note: '__sync_fetch_and_nand' changed semantics in GCC 4.4" "" { target *-*-* } 0 } */
-/* { dg-message "note: '__sync_nand_and_fetch' changed semantics in GCC 4.4" "" { target *-*-* } 0 } */
+/* { dg-message "note: '__sync_fetch_and_nand' changed semantics in GCC 4.4" "fetch_and_nand" { target *-*-* } 0 } */
+/* { dg-message "note: '__sync_nand_and_fetch' changed semantics in GCC 4.4" "nand_and_fetch" { target *-*-* } 0 } */
 /* { dg-message "file included" "In file included" { target *-*-* } 0 } */
 
 #include "../../gcc.dg/di-longlong64-sync-1.c"
