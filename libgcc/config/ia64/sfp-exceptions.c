1927db1f0e2e948b8f4a791476bee33b2186261f
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-06-13 15:56:55 +0000

    * config/ia64/sfp-machine.h (__sfp_handle_exceptions): New 	function declaration. 	(FP_HANDLE_EXCEPTIONS): Use __sfp_handle_exceptions. 	(FP_RND_MASK): New. 	* config/ia64/sfp-exceptions.c: New. 	* config/ia64/t-softfp (LIB2ADD): Add sfp-exceptions.c.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@188520 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libgcc/config/ia64/sfp-exceptions.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2012 Free Software Foundation, Inc.
+ *
+ * This file is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3, or (at your option) any
+ * later version.
+ * 
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * Under Section 7 of GPL version 3, you are granted additional
+ * permissions described in the GCC Runtime Library Exception, version
+ * 3.1, as published by the Free Software Foundation.
+ *
+ * You should have received a copy of the GNU General Public License and
+ * a copy of the GCC Runtime Library Exception along with this program;
+ * see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include "sfp-machine.h"
+
+void
+__sfp_handle_exceptions (int _fex)
+{
+  double d;
+
+  if (_fex & FP_EX_INVALID)
+    {
+      asm volatile ("frcpa.s0 %0, p1 = f0, f0" : "=f" (d) : : "p1");
+    }
+  if (_fex & FP_EX_DIVZERO)
+    {
+      asm volatile ("frcpa.s0 %0, p1 = f1, f0" : "=f" (d) : : "p1");
+    }
+  if (_fex & FP_EX_OVERFLOW)
+    {
+      d = __DBL_MAX__;
+      asm volatile ("fadd.d.s0 %0 = %0, %0" : "+f" (d));
+    }
+  if (_fex & FP_EX_UNDERFLOW)
+    {
+      d = __DBL_MIN__;
+      asm volatile ("fnma.d.s0 %0 = %0, %0, f0" : "+f" (d));
+    }
+  if (_fex & FP_EX_INEXACT)
+    {
+      d = __DBL_MAX__;
+      asm volatile ("fsub.d.s0 %0 = %0, f1" : "+f" (d));
+    }
+}
