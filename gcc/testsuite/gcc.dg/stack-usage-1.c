825bc4ece77a71d1b8e83bcc3c09db612036ed86
Christian Bruel <christian.bruel@st.com>
2012-06-14 10:43:20 +0200

    re PR target/53621 ([SH] Frame pointers not generated with -fno-omit-frame-pointer on GCC 4.7.0)
    PR target/53621
    * config/sh/sh.c (sh_option_override): Don't force
    flag_omit_frame_pointer and maccumulate_outgoing_args.
    * config/sh/sh.opt (maccumulate-outgoing-args): Init as Var.
    
    * gcc.dg/stack-usage-1.c: Force -fomit-frame-pointer on SH.
    
    From-SVN: r188599

--- gcc/testsuite/gcc.dg/stack-usage-1.c
+++ gcc/testsuite/gcc.dg/stack-usage-1.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-fstack-usage" } */
+/* { dg-options "-fstack-usage -fomit-frame-pointer" { target { sh-*-* } } } */
 
 /* This is aimed at testing basic support for -fstack-usage in the back-ends.
    See the SPARC back-end for example (grep flag_stack_usage_info in sparc.c).
