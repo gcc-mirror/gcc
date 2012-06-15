6da8be895c345e90cec9a70fdeb38111fc48a880
Michael Matz <matz@suse.de>
2012-06-15 14:40:38 +0000

    gimplify.c (gimplify_modify_expr): Fold generated statements.
    	* gimplify.c (gimplify_modify_expr): Fold generated statements.
    	* gimple-fold.c (can_refer_decl_in_current_unit_p): Check flag_ltrans.
    
    testsuite/
    	* gcc.dg/debug/dwarf2/inline3.c: Adjust.
    	* gcc.dg/tree-ssa/foldstring-1.c: Adjust.
    
    From-SVN: r188664

--- gcc/testsuite/gcc.dg/debug/dwarf2/inline3.c
+++ gcc/testsuite/gcc.dg/debug/dwarf2/inline3.c
@@ -1,7 +1,7 @@
 /* Verify that only one DW_AT_const_value is emitted for baz,
    not for baz abstract DIE and again inside of
    DW_TAG_inlined_subroutine.  */
-/* { dg-options "-O2 -g -dA" } */
+/* { dg-options "-O2 -g -dA -fmerge-all-constants" } */
 /* { dg-do compile } */
 /* { dg-final { scan-assembler-times " DW_AT_const_value" 1 } } */
 
@@ -11,6 +11,9 @@ static inline long
 foo (void)
 {
   const struct A baz = { .i = 2, .j = 21 };
+  /* We must make sure that baz isn't optimized away before inlining,
+     otherwise its initializer is also lost.  */
+  const struct A *p = &baz;
   asm volatile ("" : : : "memory");
   return baz.i * baz.j;
 }
