3eb2d93455837a86a7df735907ac19c2c2e78a0b
Uros Bizjak <uros@gcc.gnu.org>
2012-06-18 00:14:03 +0200

    * gcc.target/i386/pr33329.c: .
    From-SVN: r188717

--- gcc/testsuite/gcc.target/i386/pr33329.c
+++ gcc/testsuite/gcc.target/i386/pr33329.c
@@ -7,7 +7,7 @@ void f (void)
 {
   int tabs[1024], tabcount;
 
-  for (tabcount = 1; tabcount <= 1024; tabcount += 1023)
+  for (tabcount = 1; tabcount <= 8; tabcount += 7)
     {
       int i;
       for (i = 0; i < 1024; i++)
