a60f54b9364d256b710c66790c294507d7074792
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-06-17 22:09:40 +0000

    * gcc.target/i386/pr33329.c: Fix tabcount increase.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@188716 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/pr33329.c
+++ gcc/testsuite/gcc.target/i386/pr33329.c
@@ -7,7 +7,7 @@ void f (void)
 {
   int tabs[1024], tabcount;
 
-  for (tabcount = 1; tabcount <= 1024; tabcount += 7)
+  for (tabcount = 1; tabcount <= 1024; tabcount += 1023)
     {
       int i;
       for (i = 0; i < 1024; i++)
