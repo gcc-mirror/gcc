fe26539668cdc56eb8ce1550f83909a811523d15
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-06-18 09:32:51 +0000

    gcc/ 	PR middle-end/53698 	* expr.c (expand_expr_addr_expr_1): Convert to tmode before 	performing an addition.
    gcc/testsuite/
    	PR middle-end/53698
    	* gcc.target/i386/pr53698.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@188725 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr53698.c
@@ -0,0 +1,16 @@
+/* { dg-do compile { target { ! { ia32 } } } } */
+/* { dg-options "-O -mx32 -maddress-mode=long -fno-tree-dominator-opts" } */
+
+extern char foo[];
+
+void
+test2 (void)
+{
+  int s;
+  for (s = 0;; ++s)
+    {
+      if (foo[s] != s)
+	__builtin_abort ();
+      foo[s] = s;
+    }
+}
