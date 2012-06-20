a75f501709fc1562a96064688ca925d48562f131
Richard Guenther <rguenther@suse.de>
2012-06-20 12:00:20 +0000

    re PR tree-optimization/30318 (VRP does not create ANTI_RANGEs on overflow)
    2012-06-20  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/30318
    	* tree-vrp.c (range_int_cst_p): Do not reject overflowed
    	constants here.
    	(range_int_cst_singleton_p): But explicitely here.
    	(zero_nonzero_bits_from_vr): And here.
    	(extract_range_from_binary_expr_1): Re-implement PLUS_EXPR
    	to cover all cases we can perform arbitrary precision
    	arithmetic with double-ints.
    	(intersect_ranges): Handle adjacent anti-ranges.
    
    	* gcc.dg/tree-ssa/vrp69.c: New testcase.
    
    From-SVN: r188827

---
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp69.c
@@ -0,0 +1,38 @@
+/* { dg-do link } */
+/* { dg-options "-O2 -fdump-tree-vrp1" } */
+
+#include "vrp.h"
+
+void test1(int i, int j)
+{
+  RANGE(i, 1, 5);
+  RANGE(j, 7, 10);
+  CHECK_RANGE(i + j, 8, 15);
+}
+
+#define UINT_MAX 2*(unsigned)__INT_MAX__ + 1
+void test2(unsigned int i)
+{
+  RANGE(i, UINT_MAX - 0x4, UINT_MAX - 0x1);
+  CHECK_ANTI_RANGE(i + 0x2, 0x1, UINT_MAX - 0x3);
+}
+void test3(unsigned int i)
+{
+  RANGE(i, UINT_MAX - 0x4, UINT_MAX - 0x1);
+  CHECK_RANGE(i + 0x5, 0x0, 0x3);
+}
+void test4(unsigned int i)
+{
+  RANGE(i, 2, 4);
+  CHECK_ANTI_RANGE(i - 4, 1, UINT_MAX - 2);
+}
+void test5(unsigned int i)
+{
+  RANGE(i, 2, 4);
+  CHECK_RANGE(i - 8, UINT_MAX - 5, UINT_MAX - 3);
+}
+
+int main() {}
+
+/* { dg-final { scan-tree-dump-times "link_error" 0 "vrp1" } } */
+/* { dg-final { cleanup-tree-dump "vrp1" } } */
