e196f4b796bd08c891f245f425434709328541b9
Richard Guenther <rguenther@suse.de>
2012-06-22 08:07:47 +0000

    re PR gcov-profile/53744 (gcov version oscillates between 407* and 407p on branches)
    2012-06-22  Richard Guenther  <rguenther@suse.de>
    
    	PR gcov-profile/53744
    	* gcov-iov.c (main): Treat "" and "prerelease" the same.
    
    From-SVN: r188876

--- gcc/gcov-iov.c
+++ gcc/gcov-iov.c
@@ -48,8 +48,14 @@ main (int argc, char **argv)
   if (*ptr == '.')
     minor = strtoul (ptr + 1, 0, 10);
 
+  /* For releases the development phase is an empty string, for
+     prerelease versions on a release branch it is "prerelease".
+     Consider both equal as patch-level releases do not change
+     the GCOV version either.
+     On the trunk the development phase is "experimental".  */
   phase = argv[2][0];
-  if (phase == '\0')
+  if (phase == '\0'
+      || strcmp (argv[2], "prerelease") == 0)
     phase = '*';
 
   v[0] = (major < 10 ? '0' : 'A' - 10) + major;
