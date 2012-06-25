85d917484f771cd78ae2089f5d901d3c86f7ff6e
Jakub Jelinek <jakub@redhat.com>
2012-06-25 16:52:59 +0200

    re PR target/53759 (gcc -mavx emits vshufps for __builtin_ia32_loadlps)
    	PR target/53759
    	* config/i386/sse.md (sse_loadlps): Use x m x constraints instead
    	of x x x in the vmovlps load alternative.
    
    	* gcc.target/i386/pr53759.c: New test.
    
    From-SVN: r188937

---
+++ gcc/testsuite/gcc.target/i386/pr53759.c
@@ -0,0 +1,17 @@
+/* PR target/53759 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -mavx" } */
+/* { dg-require-effective-target avx } */
+
+#include <xmmintrin.h>
+
+void
+foo (__m128 *x, __m64 *y)
+{
+  __m128 a = _mm_setzero_ps ();
+  __m128 b = _mm_loadl_pi (a, y);
+  *x = _mm_add_ps (b, b);
+}
+
+/* { dg-final { scan-assembler "vmovlps\[ \\t\]" } } */
+/* { dg-final { scan-assembler-not "vshufps\[ \\t\]" } } */
