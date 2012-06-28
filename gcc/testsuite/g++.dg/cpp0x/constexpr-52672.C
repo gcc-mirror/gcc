0ff7968cc08d4307e10256b44748bbcd79191497
Kai Tietz <ktietz@redhat.com>
2012-06-28 22:29:20 +0200

    constexpr-52672.C (ul_ptr): Use SIZE_TYPE instead of hard-coded 'unsigned long'.
            * g++.dg/cpp0x/constexpr-52672.C (ul_ptr): Use SIZE_TYPE instead of
            hard-coded 'unsigned long'.
    
    From-SVN: r189056

--- gcc/testsuite/g++.dg/cpp0x/constexpr-52672.C
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-52672.C
@@ -2,7 +2,7 @@
 // { dg-do compile }
 // { dg-options "-std=c++11" }
 
-typedef unsigned long * ul_ptr;
+__extension__ typedef __SIZE_TYPE__ * ul_ptr;
 constexpr unsigned long a = *((ul_ptr)0x0); // { dg-error "" }
 constexpr unsigned long b = *((ul_ptr)(*((ul_ptr)0x0))); // { dg-error "" }
 constexpr unsigned long c = *((ul_ptr)*((ul_ptr)(*((ul_ptr)0x0)))); // { dg-error "" }
