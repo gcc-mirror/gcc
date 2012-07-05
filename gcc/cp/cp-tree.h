49bb4bbe90dff8b1cc71e08693ed24e00b0f648e
Jason Merrill <jason@redhat.com>
2012-07-05 15:39:01 -0400

    re PR c++/50852 (ICE: internal compiler error: in tsubst, at cp/pt.c:11030)
    	PR c++/50852
    	PR c++/53039
    	* tree.c (strip_typedefs_expr): New.
    	* cp-tree.h: Declare it.
    	* pt.c (convert_template_argument, unify): Use it.
    	* parser.c (cp_parser_template_declaration_after_export): Don't call
    	fixup_template_parms.
    
    From-SVN: r189298

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -5692,6 +5692,7 @@ extern bool type_has_nontrivial_copy_init	(const_tree);
 extern bool class_tmpl_impl_spec_p		(const_tree);
 extern int zero_init_p				(const_tree);
 extern tree strip_typedefs			(tree);
+extern tree strip_typedefs_expr			(tree);
 extern tree copy_binfo				(tree, tree, tree,
 						 tree *, int);
 extern int member_p				(const_tree);
