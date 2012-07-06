16b1765e55d0aef99d64d0fab2e297a5b985f265
Kai Tietz <ktietz@redhat.com>
2012-07-06 20:56:09 +0200

    backport: re PR bootstrap/52947 (bootstrap fails due to wrong include search path composition)
            Backport from mainline.
            PR bootstrap/52947
            * config/i386/mingw32.h (NATIVE_SYSTEM_HEADER_DIR): Define it always
            as "/mingw/include".
    
    From-SVN: r189339

--- gcc/config/i386/mingw32.h
+++ gcc/config/i386/mingw32.h
@@ -150,6 +150,11 @@ along with GCC; see the file COPYING3.  If not see
 #define STANDARD_STARTFILE_PREFIX_2 ""
 #endif
 
+/* For native mingw-version we need to take care that NATIVE_SYSTEM_HEADER_DIR
+   macro contains POSIX-style path.  See bug 52947.  */
+#undef NATIVE_SYSTEM_HEADER_DIR
+#define NATIVE_SYSTEM_HEADER_DIR "/mingw/include"
+
 /* Output STRING, a string representing a filename, to FILE.
    We canonicalize it to be in Unix format (backslashes are replaced
    forward slashes.  */
