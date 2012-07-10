a57df396bd4b901a12316397d21fb928f27eee84
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-07-10 00:03:23 +0000

    DR 1402 	PR c++/53733 	* cp-tree.h (FNDECL_SUPPRESS_IMPLICIT_DECL): New. 	(struct lang_decl_fn): Add suppress_implicit_decl field. 	* method.c (implicitly_declare_fn): Check it. 	(process_subob_fn): Add no_implicit_p parm. 	(walk_field_subobs, synthesized_method_walk): Likewise. 	(maybe_explain_implicit_delete): Adjust. 	(explain_implicit_non_constexpr): Adjust.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@189396 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp0x/defaulted21.C
+++ gcc/testsuite/g++.dg/cpp0x/defaulted21.C
@@ -3,7 +3,8 @@
 
 struct U {
   U();
-  U(U const&);
+private:
+  U(U const&);			// { dg-error "private" }
 };
 
 struct X {
@@ -13,7 +14,7 @@ struct X {
 };
 
 X::X(X&&)=default;		// { dg-message "implicitly deleted" }
-// { dg-error "does not have a move constructor" "" { target *-*-* } 15 }
+// { dg-prune-output "within this context" }
 
 X f() {
   return X();
