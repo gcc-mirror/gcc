ca675b2e5e8560c243e88fe54c7d2e5fa3c288d6
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-07-13 08:15:10 +0000

    2012-07-13  Richard Guenther  <rguenther@suse.de>
    	* gimple.c (gimple_types_compatible_p_1): Remove redundant
    	type attribute comparisons.
    	(gimple_canonical_types_compatible_p): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@189453 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/gimple.c
+++ gcc/gimple.c
@@ -3422,10 +3422,6 @@ gimple_types_compatible_p_1 (tree t1, tree t2, type_pair_t p,
 	goto different_types;
     }
 
-  /* If their attributes are not the same they can't be the same type.  */
-  if (!attribute_list_equal (TYPE_ATTRIBUTES (t1), TYPE_ATTRIBUTES (t2)))
-    goto different_types;
-
   /* Do type-specific comparisons.  */
   switch (TREE_CODE (t1))
     {
@@ -4449,10 +4445,6 @@ gimple_canonical_types_compatible_p (tree t1, tree t2)
       return true;
     }
 
-  /* If their attributes are not the same they can't be the same type.  */
-  if (!attribute_list_equal (TYPE_ATTRIBUTES (t1), TYPE_ATTRIBUTES (t2)))
-    return false;
-
   /* Do type-specific comparisons.  */
   switch (TREE_CODE (t1))
     {
@@ -4499,13 +4491,6 @@ gimple_canonical_types_compatible_p (tree t1, tree t2)
 	}
 
     case METHOD_TYPE:
-      /* Method types should belong to the same class.  */
-      if (!gimple_canonical_types_compatible_p
-	     (TYPE_METHOD_BASETYPE (t1), TYPE_METHOD_BASETYPE (t2)))
-	return false;
-
-      /* Fallthru  */
-
     case FUNCTION_TYPE:
       /* Function types are the same if the return type and arguments types
 	 are the same.  */
