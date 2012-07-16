1498e4f7a91a83ba9b6040a86381e5a35da495e3
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-07-16 17:16:29 +0000

    2012-07-16  Andrew Pinski  <apinski@cavium.com>
            * gcc.c-torture/execute/bswap-1.c: New testcase.
            * gcc.c-torture/compile/unalign-1.c: New testcase.
            * gcc.c-torture/compile/20120524-1.c: New testcase.
            * gcc.c-torture/compile/20101216-1.c: New testcase.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@189543 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/20120524-1.c
@@ -0,0 +1,4 @@
+  char CJPAT_Packet[1508] = {};
+void build_packet(int port, char *packet) {
+            memcpy(packet, CJPAT_Packet, sizeof(CJPAT_Packet)); 
+}
---
+++ gcc/testsuite/gcc.c-torture/compile/unalign-1.c
@@ -0,0 +1,15 @@
+typedef struct __attribute__ ((__packed__))
+{
+    char valueField[2];
+} ptp_tlv_t;
+typedef struct __attribute__ ((__packed__))
+{
+    char stepsRemoved;
+    ptp_tlv_t tlv[1];
+} ptp_message_announce_t;
+int ptplib_send_announce(int sequenceId, int i)
+{
+    ptp_message_announce_t tx_packet;
+    ((long long *)tx_packet.tlv[0].valueField)[sequenceId] = i;
+    f(&tx_packet);
+}
