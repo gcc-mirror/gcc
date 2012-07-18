13f1f50857edf803798ee6ab9f42a2ec79f56c57
Oleg Endo <olegendo@gcc.gnu.org>
2012-07-18 07:49:50 +0000

    re PR target/33135 ([SH] -ffinite-math-only should not be on by default)
    	PR target/33135
    	* config/sh/sh.opt (mieee): Use Var instead of Mask.  Correct
    	description.
    	* config/sh/sh.c (sh_option_override): Do not change 
    	flag_finite_math_only.  Set TARGET_IEEE to complement of
    	flag_finite_math_only.
    	* doc/invoke.texi (SH options): Add mno-ieee.  Correct description
    	of mieee and mno-ieee behavior.
    
    	PR target/33135
    	* gcc.target/sh/pr33135-1.c: New.
    	* gcc.target/sh/pr33135-2.c: New.
    	* gcc.target/sh/pr33135-3.c: New.
    	* gcc.target/sh/pr33135-4.c: New.
    
    From-SVN: r189602

--- gcc/config/sh/sh.c
+++ gcc/config/sh/sh.c
@@ -599,11 +599,6 @@ sh_option_override (void)
   SUBTARGET_OVERRIDE_OPTIONS;
   if (optimize > 1 && !optimize_size)
     target_flags |= MASK_SAVE_ALL_TARGET_REGS;
-  if (flag_finite_math_only == 2)
-    flag_finite_math_only
-      = !flag_signaling_nans && TARGET_SH2E && ! TARGET_IEEE;
-  if (TARGET_SH2E && !flag_finite_math_only)
-    target_flags |= MASK_IEEE;
   sh_cpu = PROCESSOR_SH1;
   assembler_dialect = 0;
   if (TARGET_SH2)
@@ -888,6 +883,11 @@ sh_option_override (void)
   TARGET_FSRRA = TARGET_FSRRA
 		 && flag_unsafe_math_optimizations
 		 && flag_finite_math_only;
+
+  /* If the -mieee option was not explicitly set by the user, turn it on
+     unless -ffinite-math-only was specified.  See also PR 33135.  */
+  if (! global_options_set.x_TARGET_IEEE)
+    TARGET_IEEE = ! flag_finite_math_only;
 
   if (sh_fixed_range_str)
     sh_fix_range (sh_fixed_range_str);
---
+++ gcc/testsuite/gcc.target/sh/pr33135-3.c
@@ -0,0 +1,32 @@
+/* Check that fcmp/eq and fcmp/gt instructions are generated when specifying
+   -ffinite-math-only and -mieee.  */
+/* { dg-do compile { target "sh*-*-*" } } */
+/* { dg-options "-O1 -ffinite-math-only -mieee" } */
+/* { dg-skip-if "" { "sh*-*-*" } { "-m1" "-m2" "-m3" "-m4al" "*nofpu" "-m4-340*" "-m4-400*" "-m4-500*" "-m5*" } { "" } }  */
+/* { dg-final { scan-assembler-times "fcmp/eq" 4 } } */
+/* { dg-final { scan-assembler-times "fcmp/gt" 4 } } */
+
+int
+test_00 (float a, float b)
+{
+  return a <= b;
+}
+
+int
+test_01 (float a, float b)
+{
+  return a >= b;
+}
+
+int
+test_02 (double a, double b)
+{
+  return a <= b;
+}
+
+int
+test_03 (double a, double b)
+{
+  return a >= b;
+}
+
