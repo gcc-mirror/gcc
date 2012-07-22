8a0678fa7e10bc211cef5bd3d71f9d5cf95c55fa
Hans-Peter Nilsson <hp@axis.com>
2012-07-22 15:45:07 +0000

    Handle recent changes in default atomics for cris*-*-linux*.
    	* gcc.target/cris/torture/sync-mis-xchg-i-1ml.c,
    	gcc.target/cris/torture/sync-mis-xchg-i-2ml.c,
    	gcc.target/cris/torture/sync-mis-xchg-i-3ml.c,
    	gcc.target/cris/torture/sync-mis-xchg-s-1ml.c,
    	gcc.target/cris/torture/sync-mis-op-i-1ml.c,
    	gcc.target/cris/torture/sync-mis-op-i-2ml.c,
    	gcc.target/cris/torture/sync-mis-op-i-3ml.c,
    	gcc.target/cris/torture/sync-mis-op-s-1ml.c: New tests.
    	* gcc.target/cris/torture/sync-mis-op-i-2a.c: Make sure
    	-mno-unaligned-atomic-may-use-library is in effect for
    	cris*-*-linux*.
    	* gcc.target/cris/torture/sync-mis-xchg-i-1.c,
    	gcc.target/cris/torture/sync-mis-xchg-i-2.c,
    	gcc.target/cris/torture/sync-mis-xchg-i-3.c,
    	gcc.target/cris/torture/sync-mis-xchg-i-2a.c,
    	gcc.target/cris/torture/sync-mis-xchg-s-1.c,
    	gcc.target/cris/torture/sync-mis-op-i-1.c,
    	gcc.target/cris/torture/sync-mis-op-i-2.c,
    	gcc.target/cris/torture/sync-mis-op-i-1a.c,
    	gcc.target/cris/torture/sync-mis-op-i-3.c,
    	gcc.target/cris/torture/sync-mis-op-i-3a.c,
    	gcc.target/cris/torture/sync-mis-op-s-1a.c,
    	gcc.target/cris/torture/sync-mis-xchg-i-1a.c,
    	gcc.target/cris/torture/sync-mis-xchg-i-3a.c,
    	gcc.target/cris/torture/sync-mis-xchg-s-1a.c: Similar.
    	* gcc.target/cris/torture/sync-mis-op-s-1.c: Ditto.
    	(main): Remove local variable x.
    	[mis_ok]: Check that atomics don't fail.
    
    From-SVN: r189757

--- gcc/testsuite/gcc.target/cris/torture/sync-mis-op-i-1ml.c
+++ gcc/testsuite/gcc.target/cris/torture/sync-mis-op-i-1ml.c
@@ -1,4 +1,4 @@
 /* { dg-do run { target *-*-linux* } } */
 /* { dg-additional-sources "../sync-1.c" } */
-/* { dg-options "-Dop -Dtype=int -Dmisalignment=2 -mno-unaligned-atomic-may-use-library" } */
+/* { dg-options "-Dop -Dtype=int -Dmis_ok" } */
 #include "sync-mis-op-s-1.c"
--- gcc/testsuite/gcc.target/cris/torture/sync-mis-op-i-3a.c
+++ gcc/testsuite/gcc.target/cris/torture/sync-mis-op-i-3a.c
@@ -2,4 +2,5 @@
 /* { dg-additional-sources "../sync-1.c" } */
 /* { dg-options "-Dop -Dtype=int -Dmisalignment=3 -DTRAP_USING_ABORT -mno-trap-using-break8" } */
 /* { dg-additional-options "-mtrap-unaligned-atomic" { target cris-*-elf } } */
+/* { dg-additional-options "-mno-unaligned-atomic-may-use-library" { target cris*-*-linux* } } */
 #include "sync-mis-op-s-1.c"
