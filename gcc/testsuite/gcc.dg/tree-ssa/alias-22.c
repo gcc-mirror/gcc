40287ad370cbfadd18e6e4e93375c79596d2776d
Andrew Pinski <apinski@cavium.com>
2012-07-22 05:32:16 +0000

    alias-21.c: New testcase.
    2012-07-21  Andrew Pinski  <apinski@cavium.com>
    
    	* gcc.dg/tree-ssa/alias-21.c: New testcase.
    	* gcc.dg/tree-ssa/alias-22.c: New testcase.
    
    From-SVN: r189752

---
+++ gcc/testsuite/gcc.dg/tree-ssa/alias-22.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+
+int f(int *r)
+{
+  r[1] = 0;
+  r[2] = 1;
+  return r[1];
+}
+
+
+/* { dg-final { scan-tree-dump-times "return 0;" 1 "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
+
