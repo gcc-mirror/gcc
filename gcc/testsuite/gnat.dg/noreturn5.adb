929384bb9b9b2f197f9bf6e143b5256d69e80584
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-07-25 19:57:51 +0000

    * gimple-low.c (lower_try_catch): New function. 	(lower_stmt) <GIMPLE_TRY>: Use it to lower GIMPLE_TRY_CATCH. 	<GIMPLE_CATCH>: Delete. 	<GIMPLE_EH_FILTER>: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@189865 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gnat.dg/noreturn5.adb
@@ -0,0 +1,30 @@
+with Ada.Characters.Handling; use Ada.Characters.Handling;
+with GNAT.OS_Lib;             use GNAT.OS_Lib;
+with Text_IO;                 use Text_IO;
+
+package body Noreturn5 is
+
+   procedure Proc (Arg_Line : Wide_String; Keep_Going : Boolean) is
+   begin
+      Put (To_String (Arg_Line));
+
+      if Keep_Going then
+         raise Constraint_Error;
+      else
+         OS_Exit (1);
+      end if;
+
+   exception
+      when Constraint_Error =>
+         raise;
+
+      when others =>
+         if Keep_Going then
+            raise Constraint_Error;
+         else
+            OS_Exit (1);
+         end if;
+
+   end;
+
+end Noreturn5;
