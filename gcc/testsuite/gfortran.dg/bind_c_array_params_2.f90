be2b24629d5ee23e5907f7451254c999a9300159
Uros Bizjak <uros@gcc.gnu.org>
2012-07-27 13:48:19 +0200

    bind_c_array_params_2.f90: Add "-mno-explicit-relocs" for alpha*-*-* targets.
    	* gfortran.dg/bind_c_array_params_2.f90: Add "-mno-explicit-relocs"
    	for alpha*-*-* targets.  Cleanup original tree dump.
    
    From-SVN: r189909

--- gcc/testsuite/gfortran.dg/bind_c_array_params_2.f90
+++ gcc/testsuite/gfortran.dg/bind_c_array_params_2.f90
@@ -1,5 +1,6 @@
 ! { dg-do compile }
 ! { dg-options "-std=f2008ts -fdump-tree-original" }
+! { dg-additional-options "-mno-explicit-relocs" { target alpha*-*-* } }
 !
 ! Check that assumed-shape variables are correctly passed to BIND(C)
 ! as defined in TS 29913
@@ -14,6 +15,6 @@ integer :: aa(4,4)
 call test(aa)
 end
 
-! { dg-final { scan-tree-dump-times "test \\\(&parm\\." 1 "original" } }
 ! { dg-final { scan-assembler-times "myBindC" 1 } }
-
+! { dg-final { scan-tree-dump-times "test \\\(&parm\\." 1 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
