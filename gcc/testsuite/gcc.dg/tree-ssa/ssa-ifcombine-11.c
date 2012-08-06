6e6b952be0853566db2f50ca5c14305d841353df
glisse <glisse@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-08-06 16:38:48 +0000

    2012-08-06 Marc Glisse <marc.glisse@inria.fr>
    gcc/
    	PR tree-optimization/51938
    	PR tree-optimization/52005
    	* tree-ssa-ifcombine.c (ifcombine_ifandif): New parameters for
    	inverted conditions.
    	(ifcombine_iforif): Remove, merge code into ifcombine_ifandif.
    	(tree_ssa_ifcombine_bb): Update calls to the above. Detect !a&&b
    	and !a||b patterns.
    
    gcc/testsuite/
    	PR tree-optimization/51938
    	PR tree-optimization/52005
    	* gcc.dg/tree-ssa/ssa-ifcombine-8.c: New testcase.
    	* gcc.dg/tree-ssa/ssa-ifcombine-9.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-ifcombine-10.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-ifcombine-11.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@190184 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-ifcombine-11.c
@@ -0,0 +1,21 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-optimized" } */
+
+/* Testcase for PR31657.  */
+int g(void);
+int f(int x, int a, int b)
+{
+  int t = 0;
+  int c = 1 << a;
+  if (!(x & 1))
+    t = 0;
+  else
+    if (x & (1 << 2))
+      t = g();
+    else
+      t = 0;
+  return t;
+}
+
+/* { dg-final { scan-tree-dump "& 5" "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-ifcombine-8.c
@@ -0,0 +1,25 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fno-trapping-math -fdump-tree-ifcombine" } */
+
+double test1 (double i, double j)
+{
+  if (i >= j)
+    if (i <= j)
+      goto plif;
+    else
+      goto plouf;
+  else
+    goto plif;
+
+plif:
+  return 0;
+plouf:
+  return -1;
+}
+
+/* The above should be optimized to a i > j test by ifcombine.
+   The transformation would also be legal with -ftrapping-math.
+   Instead we get u<=, which is acceptable with -fno-trapping-math.  */
+
+/* { dg-final { scan-tree-dump " u<= " "ifcombine" } } */
+/* { dg-final { cleanup-tree-dump "ifcombine" } } */
