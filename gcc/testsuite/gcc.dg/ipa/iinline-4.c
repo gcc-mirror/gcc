0d4911880dc3b06519afdf1ebd28b17430505a16
jamborm <jamborm@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-08-09 16:05:46 +0000

    2012-08-09  Martin Jambor  <mjambor@suse.cz>
    	* cgraph.h (cgraph_indirect_call_info): Field anc_offse renamd to
    	offset, updated all users.  New field agg_contents.
    	* ipa-prop.h (jump_func_type): Removed IPA_JF_CONST_MEMBER_PTR.
    	(ipa_pass_through_data): New field agg_preserved.
    	(ipa_ancestor_jf_data): Likewise.
    	(ipa_member_ptr_cst): Removed.
    	(ipa_agg_jf_item): New type.
    	(ipa_agg_jump_function): Likewise.
    	(ipa_jump_func): New field agg.  Removed field member_cst.
    	(ipa_get_jf_pass_through_agg_preserved): New function.
    	(ipa_get_jf_ancestor_agg_preserved): Likewise.
    	(ipa_get_jf_member_ptr_pfn): Removed.
    	(ipa_find_agg_cst_for_param): Declare.
    	(ipa_load_from_parm_agg): Likewise.
    	* ipa-prop.c (param_analysis_info): Fields modified and
    	visited_statements rename to parm_modified and parm_visited_statements
    	respectively, added fields ref_modified, ref_visited_statements,
    	pt_modified and pt_visited_statements.
    	(ipa_print_node_jump_functions_for_edge): Do not dump const member
    	functions.  Dump agg_preserved flags and aggregate jump functions.
    	(ipa_set_jf_simple_pass_through): Set also agg_preserved.
    	(ipa_set_ancestor_jf): Likewise.
    	(ipa_set_jf_arith_pass_through): Clear agg_preserved.
    	(ipa_set_jf_member_ptr_cst): Removed.
    	(is_parm_modified_before_stmt): Logic reversed, renamed to
    	parm_preserved_before_stmt_p.  Cache visited bitmap only for
    	naked DECL parameters.  All callers updated.
    	(load_from_unmodified_param): Allow NULL parms_ainfo.
    	(parm_ref_data_preserved_p): New function.
    	(parm_ref_data_pass_through_p): Likewise.
    	(ipa_load_from_parm_agg_1): Likewise.
    	(ipa_load_from_parm_agg): Likewise.
    	(compute_complex_assign_jump_func): Check if aggregate contents are
    	preserved.
    	(compute_complex_ancestor_jump_func): Likewise.
    	(compute_scalar_jump_functions): Removed.
    	(type_like_member_ptr_p): Also check field position are known and
    	sane.
    	(compute_pass_through_member_ptrs): Removed.
    	(determine_cst_member_ptr): Likewise.
    	(ipa_known_agg_contents_list): New type.
    	(determine_known_aggregate_parts): New function.
    	(compute_cst_member_ptr_arguments): Removed.
    	(ipa_compute_jump_functions_for_edge): Compute all kinds of jump
    	functions (scalar, aggregate and member pointer).
    	(ipa_get_member_ptr_load_param): Incorporate into
    	ipa_get_stmt_member_ptr_load_param, also pass back an offset.
    	(ipa_note_param_call): Clear agg_contents.
    	(ipa_analyze_indirect_call_uses): Also look for simple pointers loaded
    	from aggregates.  In such cases, store offset of the called field.
    	(ipa_analyze_node): Initialize new fields of param_analysis_info.
    	(update_jump_functions_after_inlining): Handle aggregate contents.
    	(ipa_find_agg_cst_for_param): New function.
    	(try_make_edge_direct_simple_call): Handle called aggregate values.
    	(update_indirect_edges_after_inlining): Make sure aggregate preserving
    	jump functions comply with type compatibility requirements.
    	(ipa_edge_duplication_hook): Copy also aggregate jump functions.
    	(ipa_write_jump_function): Stream agg_preserved flags and aggregate
    	jump functions.  Do not stream member pointer constant jump functions.
    	(ipa_read_jump_function): Likewise.
    	(ipa_write_indirect_edge_info): Stream new cgraph_indirect_call_info
    	fields.
    	(ipa_read_indirect_edge_info): Likewise.
    
    	* testsuite/gcc.dg/ipa/iinline-4.c: New test.
    	* testsuite/gcc.dg/ipa/iinline-5.c: Likewise.
    	* testsuite/gcc.dg/ipa/iinline-6.c: Likewise.
    	* testsuite/gcc.dg/ipa/iinline-7.c: Likewise.
    	* testsuite/gcc.dg/lto/20120723_0.c: Likewise.
    	* testsuite/gcc.dg/lto/20120723_1.c: Likewise.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@190260 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/ipa/iinline-4.c
@@ -0,0 +1,221 @@
+/* Verify that simple indirect calls are inlined even without early
+   inlining..  */
+/* { dg-do compile } */
+/* { dg-options "-O3 -fdump-ipa-inline -fno-early-inlining"  } */
+
+struct S
+{
+  int i;
+  void (*f)(struct S *);
+  unsigned u;
+};
+
+struct U
+{
+  struct U *next;
+  struct S s;
+  short a[8];
+};
+
+extern void non_existent(struct S *p, int);
+
+/* ----- 1 ----- */
+
+static void hooray1 (struct S *p)
+{
+  non_existent (p, 1);
+}
+
+static void hiphip1 (struct S *p)
+{
+  p->f (p);
+}
+
+int test1 (void)
+{
+  struct S s;
+  s.i = 1234;
+  s.f = hooray1;
+  s.u = 1001;
+  hiphip1 (&s);
+  return 0;
+}
+
+/* ----- 2 ----- */
+
+struct S *gp;
+
+static void hooray2 (struct S *p)
+{
+  non_existent (p, 2);
+}
+
+static void hip2 (struct S *p)
+{
+  p->f (p);
+}
+
+static void hiphip2 (struct S *p)
+{
+  hip2 (p);
+}
+
+int test2 (void)
+{
+  struct S *p = gp;
+  p->i = 2341;
+  p->f = hooray2;
+  p->u = 1002;
+  hiphip2 (p);
+  return 0;
+}
+
+/* ----- 3 ----- */
+
+static void hooray3 (struct S *p)
+{
+  non_existent (p, 2);
+}
+
+static void hip3 (struct S *p)
+{
+  p->f (p);
+}
+
+static __attribute__ ((flatten)) void hiphip3 (struct S *p)
+{
+  hip3 (p);
+}
+
+int test3 (void)
+{
+  struct S *p = gp;
+  p->i = 2341;
+  p->f = hooray3;
+  p->u = 1003;
+  hiphip3 (p);
+  return 0;
+}
+
+/* ----- 4 ----- */
+
+static void hooray4 (struct S *p)
+{
+  non_existent (p, 3);
+}
+
+static void hiphip4 (struct S s)
+{
+  s.f (&s);
+}
+
+int test4(void)
+{
+  struct S s;
+  s.i = 3412;
+  s.f = hooray4;
+  s.u = 1004;
+  hiphip4 (s);
+  return 0;
+}
+
+/* ----- 5 ----- */
+
+struct U *gu;
+
+static void hooray5 (struct S *p)
+{
+  non_existent (p, 5);
+}
+
+static void hip5 (struct S *p)
+{
+  p->f (p);
+}
+
+static void hiphip5 (struct U *u)
+{
+  hip5 (&u->s);
+}
+
+int test5 (void)
+{
+  struct U *u = gu;
+  u->next = u;
+  u->s.i = 9876;
+  u->s.f = hooray5;
+  u->s.u = 1005;
+  hiphip5 (u);
+  return 0;
+}
+
+/* ----- 6 ----- */
+
+static void hooray6 (struct S *p)
+{
+  non_existent (p, 6);
+}
+
+static void hip6 (struct S *p)
+{
+  p->f (p);
+}
+
+static __attribute__ ((flatten)) void hiphip6 (struct U *u)
+{
+  hip6 (&u->s);
+}
+
+int test6 (void)
+{
+  struct U *u = gu;
+  u->next = u;
+  u->s.i = 9876;
+  u->s.f = hooray6;
+  u->s.u = 1006;
+  hiphip6 (u);
+  return 0;
+}
+
+/* ----- 7 ----- */
+
+struct S **gdp;
+
+
+static void hooray7 (struct S *p)
+{
+  non_existent (p, 7);
+}
+
+static void hip7 (struct S *p)
+{
+  p->f (p);
+  gdp = &p;
+}
+
+static void hiphip7 (struct S *p)
+{
+  hip7 (p);
+  gdp = &p;
+}
+
+int test7 (void)
+{
+  struct S *p = gp;
+  p->i = 7341;
+  p->f = hooray7;
+  p->u = 1007;
+  hiphip7 (p);
+  return 0;
+}
+
+
+
+/* { dg-final { scan-ipa-dump "hooray1\[^\\n\]*inline copy in test1"  "inline"  } } */
+/* { dg-final { scan-ipa-dump "hooray2\[^\\n\]*inline copy in test2"  "inline"  } } */
+/* { dg-final { scan-ipa-dump "hooray3\[^\\n\]*inline copy in test3"  "inline"  } } */
+/* { dg-final { scan-ipa-dump "hooray4\[^\\n\]*inline copy in test4"  "inline"  } } */
+/* { dg-final { scan-ipa-dump "hooray5\[^\\n\]*inline copy in test5"  "inline"  } } */
+/* { dg-final { scan-ipa-dump "hooray6\[^\\n\]*inline copy in test6"  "inline"  } } */
+/* { dg-final { scan-ipa-dump "hooray7\[^\\n\]*inline copy in test7"  "inline"  } } */
+/* { dg-final { cleanup-ipa-dump "inline" } } */
---
+++ gcc/testsuite/gcc.dg/ipa/iinline-6.c
@@ -0,0 +1,72 @@
+/* Verify that simple indirect calls are inlined even without early
+   inlining..  */
+/* { dg-do run } */
+/* { dg-options "-O3 -fdump-ipa-inline -fno-early-inlining"  } */
+
+extern void abort (void);
+
+struct S
+{
+  int i;
+  void (*f)(struct S *);
+  int j,k,l;
+};
+
+struct Z
+{
+  unsigned u;
+  void (*f)(struct Z *, int);
+  struct Z *next;
+};
+
+static struct S *gs;
+static int gr = 111;
+char gc[1024];
+
+static __attribute__ ((noinline, noclone)) struct S *
+get_s (void)
+{
+  return (struct S *) &gc;
+}
+
+static void wrong_target (struct S *s)
+{
+  abort ();
+}
+
+static void good_target (struct S *s)
+{
+  gr = 0;
+}
+
+static void g1 (struct S *s)
+{
+  s->f (s);
+}
+
+static void f2 (struct Z *z)
+{
+  gs->f = good_target;
+  g1 ((struct S *) z);
+}
+
+static inline __attribute__ ((flatten)) void f1 (struct S *s)
+{
+  f2 ((struct Z *) s);
+}
+
+int main (int argc, char **argv)
+{
+  struct S *s = get_s();
+  s->i = 5678;
+  s->f = wrong_target;
+  s->j = 1234;
+  gs = s;
+  f1 (s);
+
+  return gr;
+}
+
+
+/* { dg-final { scan-ipa-dump-not "wrong_target\[^\\n\]*inline copy in" "inline"  } } */
+/* { dg-final { cleanup-ipa-dump "inline" } } */
