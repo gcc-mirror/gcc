80ca3f810c0520a978341c44c0673490c4f127f1
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-08-10 20:21:23 +0000

    * gcc.dg/torture/vector-shuffle1.c (f): Pass vectors indirectly 	to avoid warnings. 	(main): Adjust caller.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@190302 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/torture/vector-shuffle1.c
+++ gcc/testsuite/gcc.dg/torture/vector-shuffle1.c
@@ -5,15 +5,16 @@ extern void abort (void);
 
 typedef int v2si __attribute__((vector_size(2*sizeof(int))));
 
-v2si f(v2si x)
+void f(v2si *x)
 {
   /* This requires canonicalization of the mask to { 1, 0 }.  */
-  return __builtin_shuffle(x,x, (v2si) { 5, 0 });
+  *x = __builtin_shuffle(*x, *x, (v2si) { 5, 0 });
 }
 
 int main()
 {
-  v2si y = f((v2si) { 1, 2 });
+  v2si y = { 1, 2 };
+  f(&y);
   if (y[0] != 2 || y[1] != 1)
     abort ();
   return 0;
