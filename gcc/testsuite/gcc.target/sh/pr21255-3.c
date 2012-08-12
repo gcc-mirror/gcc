80a442d7b9b8dd3a2fcbd15f977c21640e5bcf2f
olegendo <olegendo@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-08-12 14:22:07 +0000

    * gcc.target/sh/pr50749-sf-postinc-1.c: Skip for -m3.  Don't skip for 	-m2e or -m2a. 	* gcc.target/sh/pr50749-sf-postinc-3.c: Likewise. 	* gcc.target/sh/pr53511-1.c: Likewise. 	* gcc.target/sh/pr50749-sf-predec-1.c: Likewise. 	* gcc.target/sh/pr50749-sf-predec-3.c: Likewise. 	* gcc.target/sh/pr53512-1.c: Skip for -m3*. 	* gcc.target/sh/pr53512-3.c: Likewise. 	* gcc.target/sh/pr21255-3.c: Skip for single precision FPU targets.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@190327 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/sh/pr21255-3.c
+++ gcc/testsuite/gcc.target/sh/pr21255-3.c
@@ -1,5 +1,6 @@
 /* { dg-do compile { target "sh*-*-*" } } */
 /* { dg-options "-O2 -fomit-frame-pointer" } */
+/* { dg-skip-if "" { "sh*-*-*" } { "-m2e" "-m3e" "*single-only" } { "" } }  */
 /* { dg-final { scan-assembler "mov #?0,r.*; mov #?20,r" } } */
 /* { dg-final { scan-assembler "mov #?1077149696,r.*; mov #?0,r" } } */
 double
--- gcc/testsuite/gcc.target/sh/pr53512-3.c
+++ gcc/testsuite/gcc.target/sh/pr53512-3.c
@@ -2,7 +2,7 @@
   -funsafe-math-optimizations and -ffinite-math-only.  */
 /* { dg-do compile { target "sh*-*-*" } } */
 /* { dg-options "-O1 -mfsrra -funsafe-math-optimizations -ffinite-math-only" } */
-/* { dg-skip-if "" { "sh*-*-*" } { "-m1" "-m2*" "-m4al" "*nofpu" "-m4-340*" "-m4-400*" "-m4-500*" "-m5*" } { "" } }  */
+/* { dg-skip-if "" { "sh*-*-*" } { "-m1" "-m2*" "-m3*" "-m4al" "*nofpu" "-m4-340*" "-m4-400*" "-m4-500*" "-m5*" } { "" } }  */
 /* { dg-final { scan-assembler "fsrra" } } */
 
 #include <math.h>
