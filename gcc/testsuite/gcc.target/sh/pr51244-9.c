eb11dba2b47beb01a873eb234ed98e30049e0bbf
Oleg Endo <olegendo@gcc.gnu.org>
2012-08-12 22:47:15 +0000

    re PR target/51244 ([SH] Inefficient conditional branch and code around T bit)
    	PR target/51244
    	* config/sh/sh.md: Add splits for inverted compare and branch
    	opportunities.
    	(*cmpeqsi_t): New insn.
    	(cmpgtsi_t, cmpgesi_t): Swap r and N alternatives.
    	(cmpgeusi_t): Use satisfies_constraint_Z.  Emit sett insn in
    	replacement insn list and not in the preparation statements.
    	(clrt, sett): Add mt_group attribute.
    
    	PR target/51244
    	* gcc.target/sh/pr51244-7.c: New.
    	* gcc.target/sh/pr51244-8.c: New.
    	* gcc.target/sh/pr51244-9.c: New.
    	* gcc.target/sh/pr51244-10.c: New.
    
    From-SVN: r190331

---
+++ gcc/testsuite/gcc.target/sh/pr51244-9.c
@@ -0,0 +1,35 @@
+/* Check that compare-branch is inverted properly.
+   Example:
+	mov.w	.L566,r2	->	mov.w	.L566,r2
+	add	r11,r2			add	r11,r2
+	mov.l	@(12,r2),r7		mov.l	@(8,r2),r5
+	mov.l	@(8,r2),r5		mov.l	@(12,r2),r2
+	mov	#0,r2			tst	r2,r2
+	cmp/hi	r2,r7			bt	.L534
+	bf	.L534
+*/
+/* { dg-do compile { target "sh*-*-*" } } */
+/* { dg-options "-O2" } */
+/* { dg-skip-if "" { "sh*-*-*" } { "-m5*" } { "" } } */
+/* { dg-final { scan-assembler-not "mov\t#0" } } */
+static inline unsigned int
+test_03_00 (unsigned int x)
+{
+  /* Return unassigned value on purpose.  */
+  unsigned int res;
+  return res;
+}
+
+struct S
+{
+  unsigned int a;
+  unsigned int b;
+};
+
+int test_03 (struct S* i)
+{
+ if ((i->a != 2 && i->a != 3) || i->a > test_03_00 (i->b))
+   return -5;
+
+ return -55;
+}
