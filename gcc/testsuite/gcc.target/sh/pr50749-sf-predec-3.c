0885e9500dbe5ed34225004b0b4a051889f58ad0
Oleg Endo <olegendo@gcc.gnu.org>
2012-08-12 14:22:07 +0000

    pr50749-sf-postinc-1.c: Skip for -m3.
    	* gcc.target/sh/pr50749-sf-postinc-1.c: Skip for -m3.  Don't skip for
    	-m2e or -m2a.
    	* gcc.target/sh/pr50749-sf-postinc-3.c: Likewise.
    	* gcc.target/sh/pr53511-1.c: Likewise.
    	* gcc.target/sh/pr50749-sf-predec-1.c: Likewise.
    	* gcc.target/sh/pr50749-sf-predec-3.c: Likewise.
    	* gcc.target/sh/pr53512-1.c: Skip for -m3*.
    	* gcc.target/sh/pr53512-3.c: Likewise.
    	* gcc.target/sh/pr21255-3.c: Skip for single precision FPU targets.
    
    From-SVN: r190327

--- gcc/testsuite/gcc.target/sh/pr50749-sf-predec-3.c
+++ gcc/testsuite/gcc.target/sh/pr50749-sf-predec-3.c
@@ -2,7 +2,7 @@
    inside a loop.  */
 /* { dg-do compile { target "sh*-*-*" } } */
 /* { dg-options "-O2" } */
-/* { dg-skip-if "" { "sh*-*-*" } { "-m1" "-m2*" "-m4al" "*nofpu" "-m4-340*" "-m4-400*" "-m4-500*" "-m5*" } { "" } }  */
+/* { dg-skip-if "" { "sh*-*-*" } { "-m1" "-m2" "-m3" "-m4al" "*nofpu" "-m4-340*" "-m4-400*" "-m4-500*" "-m5*" } { "" } }  */
 /* { dg-final { scan-assembler-times "fmov.s\tfr\[0-9]\+,@-r\[0-9]\+" 1 } } */
 
 float*
