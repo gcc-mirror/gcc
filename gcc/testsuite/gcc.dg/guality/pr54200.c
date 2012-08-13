a15039218ed24acb3c64c457d2d90d5942030f41
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-08-13 09:29:28 +0000

    2012-08-13  Richard Guenther  <rguenther@suse.de>
    	PR tree-optimization/54200
    	* tree-ssa-copyrename.c (rename_ssa_copies): Do not add
    	PHI results to another partition if not all PHI arguments
    	have the same partition.
    
    	* gcc.dg/guality/pr54200.c: New testcase.
    	* gcc.dg/tree-ssa/slsr-8.c: Adjust.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@190339 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/guality/pr54200.c
@@ -0,0 +1,28 @@
+/* PR tree-optimization/54200 */
+/* { dg-do run } */
+/* { dg-options "-g -fno-var-tracking-assignments" } */
+
+int o __attribute__((used));
+
+void bar (void) { o = 2; }
+
+int __attribute__((noinline,noclone))
+foo (int z, int x, int b)
+{
+  if (x == 1)
+    {
+      bar ();
+      return z;
+    }
+  else
+    {
+      int a = (x + z) + b;
+      return a; /* { dg-final { gdb-test 20 "z" "3" } } */
+    }
+}
+
+int main ()
+{
+  foo (3, 2, 1);
+  return 0;
+}
