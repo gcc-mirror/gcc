a66c9777da9b8fe0739899f59dfe7bb8443ffdd4
steven <steven@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-08-15 08:47:13 +0000

    * timevar.def (TV_VARPOOL, TV_WHOPR_WPA_LTRANS_EXEC, TV_LIFE, 	TV_LIFE_UPDATE, TV_DF_UREC, TV_INLINE_HEURISTICS, 	TV_TREE_LINEAR_TRANSFORM, TV_TREE_LOOP_INIT, TV_TREE_LOOP_FINI, 	TV_VPT, TV_LOCAL_ALLOC, TV_GLOBAL_ALLOC, TV_SEQABSTR): Remove. 	(TV_IPA_INLINING, TV_FLATTEN_INLINING, TV_EARLY_INLINING, 	TV_INLINE_PARAMETERS, TV_LOOP_INIT, TV_LOOP_FINI): New. 	* timevar.c (timevar_print): Make printing width of timevar names 	more flexible, but enforce maximum length. 	* ipa-inline.c (pass_early_inline): Use TV_EARLY_INLINING. 	(pass_ipa_inline): Use TV_IPA_INLINING. 	* ipa-inline-analysis.c (pass_inline_parameters): Use 	TV_INLINE_HEURISTICS. 	* tree-ssa-loop.c (pass_tree_loop_init): No timevar for wrapper pass. 	(pass_tree_loop_done): Likewise. 	* final.c (pass_shorten_branches): Use TV_SHORTEN_BRANCH. 	* loop-init.c (loop_optimizer_init): Push/pop TV_LOOP_INIT. 	(loop_optimizer_finalize): Push/pop TV_LOOP_FINI.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@190409 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ipa-inline-analysis.c
+++ gcc/ipa-inline-analysis.c
@@ -2274,7 +2274,7 @@ struct gimple_opt_pass pass_inline_parameters =
   NULL,					/* sub */
   NULL,					/* next */
   0,					/* static_pass_number */
-  TV_INLINE_HEURISTICS,			/* tv_id */
+  TV_INLINE_PARAMETERS,			/* tv_id */
   0,	                                /* properties_required */
   0,					/* properties_provided */
   0,					/* properties_destroyed */
