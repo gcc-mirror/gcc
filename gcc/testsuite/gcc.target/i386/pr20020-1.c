e327d1a362b29d605f15678072700ffb39fc4917
H.J. Lu <hongjiu.lu@intel.com>
2012-08-17 19:59:46 +0000

    Define MAX_FIXED_MODE_SIZE/TARGET_MEMBER_TYPE_FORCES_BLK for i386
    gcc/
    
    	PR target/20020
    	* config/i386/i386.c (ix86_member_type_forces_blk): New function.
    	(TARGET_MEMBER_TYPE_FORCES_BLK): New macro.
    
    	* config/i386/i386.h (MAX_FIXED_MODE_SIZE): New macro.
    
    gcc/testsuite/
    
    	PR target/20020
    	* gcc.target/i386/pr20020-1.c: New test.
    	* gcc.target/i386/pr20020-2.c: Likewise.
    	* gcc.target/i386/pr20020-3.c: Likewise.
    
    Co-Authored-By: Gary Funck <gary@intrepid.com>
    
    From-SVN: r190492

---
+++ gcc/testsuite/gcc.target/i386/pr20020-1.c
@@ -0,0 +1,26 @@
+/* Check that 128-bit struct's are represented as TImode values.  */
+/* { dg-do compile { target int128 } } */
+/* { dg-options "-O2 -fdump-rtl-expand" } */
+
+struct shared_ptr_struct
+{
+  unsigned long long phase:48;
+  unsigned short thread:16;
+  union
+    {
+      void *addr;
+      unsigned long long pad;
+    };
+};
+typedef struct shared_ptr_struct sptr_t;
+
+sptr_t S;
+
+sptr_t
+sptr_result (void)
+{
+  return S;
+}
+/* { dg-final { scan-rtl-dump "\\\(set \\\(reg:TI \[0-9\]* \\\[ <retval> \\\]\\\)" "expand" } } */
+/* { dg-final { scan-rtl-dump "\\\(set \\\(reg/i:TI 0 ax\\\)" "expand" } } */
+/* { dg-final { cleanup-rtl-dump "expand" } } */
