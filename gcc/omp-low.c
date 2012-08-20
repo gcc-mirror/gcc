d88513eaf77e9dd8bcc5a662518d18bd350e383c
Patrick Marlier <patrick.marlier@gmail.com>
2012-08-20 16:22:17 +0000

    re PR middle-end/53992 (Combining -fopenmp and -fgnu-tm results in segfault or misbehaving binaries)
    gcc/
    2012-08-20  Patrick Marlier  <patrick.marlier@gmail.com>
    
    	PR middle-end/53992
    	* omp-low.c (lower_omp_1): Handle GIMPLE_TRANSACTION.
    
    testsuite/
    2012-08-20  Patrick Marlier  <patrick.marlier@gmail.com>
    
    	PR middle-end/53992
    	* gcc.dg/gomp/pr53992.c: New test.
    
    From-SVN: r190536

--- gcc/omp-low.c
+++ gcc/omp-low.c
@@ -6827,6 +6827,9 @@ lower_omp_1 (gimple_stmt_iterator *gsi_p, omp_context *ctx)
       lower_omp (gimple_try_eval_ptr (stmt), ctx);
       lower_omp (gimple_try_cleanup_ptr (stmt), ctx);
       break;
+    case GIMPLE_TRANSACTION:
+      lower_omp (gimple_transaction_body_ptr (stmt), ctx);
+      break;
     case GIMPLE_BIND:
       lower_omp (gimple_bind_body_ptr (stmt), ctx);
       break;
