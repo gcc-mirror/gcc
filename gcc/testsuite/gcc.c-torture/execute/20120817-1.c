7ab6a8280170732268c190fffb7f522c73f7b800
Richard Earnshaw <rearnsha@arm.com>
2012-08-20 12:49:47 +0000

    re PR tree-optimization/54295 (Widening multiply-accumulate operation uses wrong value extension)
    	PR tree-ssa/54295
    	* tree-ssa-math-opts.c (widening_mult_conversion_strippable_p):
    	New function.
    	(is_widening_mult_rhs_p): Use it.
    
    	* gcc.c-torture/execute/20120817-1.c: New test.
    
    From-SVN: r190533

---
+++ gcc/testsuite/gcc.c-torture/execute/20120817-1.c
@@ -0,0 +1,14 @@
+typedef unsigned long long u64;
+unsigned long foo = 0;
+u64 f() __attribute__((noinline));
+
+u64 f() {
+  return ((u64)40) + ((u64) 24) * (int)(foo - 1);
+}
+
+int main ()
+{
+  if (f () != 16)
+    abort ();
+  exit (0);
+}
