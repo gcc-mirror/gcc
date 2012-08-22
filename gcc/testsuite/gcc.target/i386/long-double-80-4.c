39166195486e44e854054476d7a7c3532e8b1a41
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-08-22 19:46:28 +0000

    Add -mlong-double-64/-mlong-double-80 to i386
    gcc/
    
    	* doc/invoke.texi: Document -mlong-double-64/-mlong-double-80.
    
    	* config/i386/i386.c (flag_opts): Add -mlong-double-64.
    	(TARGET_HAS_BIONIC): Default long double to 64-bit for Bionic.
    
    	* config/i386/i386.h (LONG_DOUBLE_TYPE_SIZE): Use 64 if
    	TARGET_LONG_DOUBLE_64 is true.
    	(LIBGCC2_LONG_DOUBLE_TYPE_SIZE): New macro.
    	(WIDEST_HARDWARE_FP_SIZE): Defined to 80.
    
    	* config/i386/i386.opt (mlong-double-80): New option.
    	(mlong-double-64): Likewise.
    
    	* config/i386/i386-c.c (ix86_target_macros): Define
    	__LONG_DOUBLE_64__ for TARGET_LONG_DOUBLE_64.
    
    gcc/testsuite/
    
    	* gcc.target/i386/long-double-64-1.c: New file.
    	* gcc.target/i386/long-double-64-2.c: Likewise.
    	* gcc.target/i386/long-double-64-3.c: Likewise.
    	* gcc.target/i386/long-double-64-4.c: Likewise.
    	* gcc.target/i386/long-double-80-1.c: Likewise.
    	* gcc.target/i386/long-double-80-2.c: Likewise.
    	* gcc.target/i386/long-double-80-3.c: Likewise.
    	* gcc.target/i386/long-double-80-4.c: Likewise.
    	* gcc.target/i386/long-double-80-5.c: Likewise.
    	* gcc.target/i386/long-double-80-6.c: Likewise.
    	* gcc.target/i386/long-double-80-7.c: Likewise.
    
    libgcc/
    
    	* config/i386/t-linux (HOST_LIBGCC2_CFLAGS): New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@190599 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/long-double-80-4.c
@@ -0,0 +1,10 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -mlong-double-64 -mlong-double-80" } */
+
+long double
+foo (long double x)
+{
+  return x * x;
+}
+
+/* { dg-final { scan-assembler "fldt" } } */
---
+++ gcc/testsuite/gcc.target/i386/long-double-80-5.c
@@ -0,0 +1,10 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -mlong-double-64" } */
+
+__float80
+foo (__float80 x)
+{
+  return x * x;
+}
+
+/* { dg-final { scan-assembler "fldt" } } */
