7b22fcd7944967504f93e3be2b1f6ecb39578788
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-08-24 13:21:55 +0000

    Allow arg_pointer_rtx/frame_pointer_rtx for based_loc_descr
    gcc/
    
    	PR debug/52857
    	* dwarf2out.c (mem_loc_descriptor): Allow arg_pointer_rtx and
    	frame_pointer_rtx for based_loc_descr.
    
    gcc/testsuite/
    
    	PR debug/52857
    	* gcc.target/i386/pr52857-1.c: New.
    	* gcc.target/i386/pr52857-2.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@190647 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr52857-1.c
@@ -0,0 +1,10 @@
+/* { dg-do compile { target { ! { ia32 } } } } */
+/* { dg-options "-g -O -mx32 -maddress-mode=long" } */
+
+extern void get_BID128 (int *);
+void 
+__bid128_div (void)
+{
+  int res;
+  get_BID128 (&res);
+}
