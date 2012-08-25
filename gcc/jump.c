5b1ab22977cb65badf814651e511d11bba4098a6
Eric Botcazou <ebotcazou@adacore.com>
2012-08-25 23:22:21 +0000

    re PR rtl-optimization/54088 (ICE at dwarf2out.c:20632 with -O1 -g)
    	PR rtl-optimization/54088
    	* jump.c (delete_related_insns): Robustify latest change.
    
    From-SVN: r190675

--- gcc/jump.c
+++ gcc/jump.c
@@ -1260,9 +1260,9 @@ delete_related_insns (rtx insn)
 	  && GET_CODE (PATTERN (insn)) == SEQUENCE
 	  && CALL_P (XVECEXP (PATTERN (insn), 0, 0))))
     {
-      rtx p = insn;
+      rtx p;
 
-      for (p = NEXT_INSN (p);
+      for (p = next && INSN_DELETED_P (next) ? NEXT_INSN (next) : next;
 	   p && NOTE_P (p);
 	   p = NEXT_INSN (p))
 	if (NOTE_KIND (p) == NOTE_INSN_CALL_ARG_LOCATION)
