dcfbfb22047b41bdb97de0196c40515e3eeb0bcc
drepper <drepper@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-08-29 18:43:08 +0000

    Add simd_fast_mersenne_twister_engine class. 	* include/ext/random: New file. 	* include/ext/random.tcc: New file. 	* include/Makefile.am (ext_headers): Add random and random.tcc. 	* testsuite/26_numerics/random/simd_fast_mersenne_twister_engine/ 	operators/inequal.cc: New file. 	* testsuite/26_numerics/random/simd_fast_mersenne_twister_engine/ 	operators/equal.cc: New file. 	* testsuite/26_numerics/random/simd_fast_mersenne_twister_engine/ 	operators/serialize.cc: New file. 	* testsuite/26_numerics/random/simd_fast_mersenne_twister_engine/ 	cons/seed2.cc: New file. 	* testsuite/26_numerics/random/simd_fast_mersenne_twister_engine/ 	cons/default.cc: New file. 	* testsuite/26_numerics/random/simd_fast_mersenne_twister_engine/ 	cons/seed_seq.cc: New file. 	* testsuite/26_numerics/random/simd_fast_mersenne_twister_engine/ 	cons/copy.cc: New file. 	* testsuite/26_numerics/random/simd_fast_mersenne_twister_engine/ 	cons/seed1.cc: New file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@190783 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/26_numerics/random/simd_fast_mersenne_twister_engine/cons/seed_seq.cc
@@ -0,0 +1,43 @@
+// { dg-options "-std=c++0x" }
+// { dg-require-cstdint "" }
+//
+// 2010-02-01  Paolo Carlini  <paolo.carlini@oracle.com>
+// 2012-08-28  Ulrich Drepper  <drepper@gmail.com>, adapted for SFMT
+//
+// Copyright (C) 2010, 2012 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <ext/random>
+
+void
+test01()
+{
+  std::seed_seq seed;
+  __gnu_cxx::simd_fast_mersenne_twister_engine<
+    uint32_t, 607, 2,
+    15, 3, 13, 3,
+    0xfdff37ffU, 0xef7f3f7dU,
+    0xff777b7dU, 0x7ff7fb2fU,
+    0x00000001U, 0x00000000U,
+    0x00000000U, 0x5986f054U> x(seed);
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/26_numerics/random/simd_fast_mersenne_twister_engine/operators/inequal.cc
@@ -0,0 +1,52 @@
+// { dg-options "-std=c++0x" }
+// { dg-require-cstdint "" }
+//
+// 2010-03-16  Paolo Carlini  <paolo.carlini@oracle.com>
+// 2012-08-28  Ulrich Drepper  <drepper@gmail.com>, adapted for SFMT
+//
+// Copyright (C) 2010, 2012 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 26.5.3.2 Class template mersenne_twister_engine [rand.eng.mers]
+
+#include <ext/random>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  __gnu_cxx::sfmt19937 u, v;
+
+  VERIFY( !(u != v) );
+
+  u.discard(100);
+  v.discard(100);
+
+  VERIFY( !(u != v) );
+
+  v.discard(1);
+
+  VERIFY( u != v );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
