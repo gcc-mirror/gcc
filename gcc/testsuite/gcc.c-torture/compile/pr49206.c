51ad44fd6ba163c3945e4d20b2d8d63524d6a1ac
Uros Bizjak <ubizjak@gmail.com>
2012-09-02 10:49:31 +0200

    re PR target/49206 (RA failure in spill_failure, at reload1.c:2113)
    	PR target/49206
    	* gcc.c-torture/compile/pr49206.c: New test.
    
    From-SVN: r190857

---
+++ gcc/testsuite/gcc.c-torture/compile/pr49206.c
@@ -0,0 +1,26 @@
+struct S1
+{
+  short f0;
+};
+extern volatile struct S1 g_5;
+extern int g_120, i;
+extern short g_339;
+
+int
+func_72 (int x, int y, struct S1 z)
+{
+  for (z.f0 = -3; z.f0 > 16; z.f0 += 1)
+    foo ();
+  return g_120;
+}
+
+int
+main ()
+{
+  while (g_339 % 0x200003)
+    continue;
+  if (i)
+    func_72 (0, 0, g_5);
+  foo (&g_339);
+  return 0;
+}
