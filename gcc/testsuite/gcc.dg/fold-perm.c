fe1dba6dac26261f1f8b72ca185aa156d13f94d3
glisse <glisse@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-09-03 17:31:38 +0000

    2012-09-03  Marc Glisse  <marc.glisse@inria.fr>
    gcc/
    	* fold-const.c (fold_ternary_loc): Constant-propagate after
    	removing dead operands.
    
    
    gcc/testsuite/
    	* gcc.dg/fold-perm.c: Improve test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@190901 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/fold-perm.c
+++ gcc/testsuite/gcc.dg/fold-perm.c
@@ -3,11 +3,12 @@
 
 typedef int veci __attribute__ ((vector_size (4 * sizeof (int))));
 
-void fun (veci *f, veci *g, veci *h)
+void fun (veci *f, veci *g, veci *h, veci *i)
 {
   veci m = { 7, 7, 4, 6 };
   veci n = { 0, 1, 2, 3 };
   veci p = { 1, 1, 7, 6 };
+  *i = __builtin_shuffle (*i,  p, m);
   *h = __builtin_shuffle (*h, *h, p);
   *g = __builtin_shuffle (*f, *g, m);
   *f = __builtin_shuffle (*f, *g, n);
