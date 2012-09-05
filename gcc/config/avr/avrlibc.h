89a501f406dffbe96fafbd9d5ccbd28ffd7381cf
Georg-Johann Lay <avr@gjlay.de>
2012-09-05 12:19:47 +0000

    backport: re PR target/54461 ([avr] add configure option for better AVR-Libc integration)
    	Backport from 2012-09-05 mainline r190697.
    
    	PR target/54461
    	* configure.ac (noconfigdirs,target=avr-*-*): Add target-newlib,
    	target-libgloss if configured --with-avrlibc.
    	* configure: Regenerate.
    
    libgcc/
    	Backport from 2012-09-05 mainline r190697.
    
    	PR target/54461
    	* config.host (tmake_file,host=avr-*-*): Add avr/t-avrlibc if
    	configured --with-avrlibc.
    	* Makefile.in (FPBIT_FUNCS): filter-out LIB2FUNCS_EXCLUDE.
    	(DPBIT_FUNCS): Ditto.
    	(TPBIT_FUNCS): Ditto.
    	* config/avr/t-avrlibc: New file.
    
    gcc/
    	Backport from 2012-09-05 mainline r190697.
    
    	PR target/54461
    	* config.gcc (tm_file,target=avr-*-*): Add avr/avrlibc.h if
    	configured --with-avrlibc.
    	(tm_defines,target=avr-*-*): Add WITH_AVRLIBC if configured
    	--with-avrlibc.
    	* config/avr/avrlibc.h: New file.
    	* config/avr/avr-c.c: Build-in define __WITH_AVRLIBC__ if
    	configured --with-avrlibc.
    	* doc/invoke.texi (AVR Built-in Macros): Document __WITH_AVRLIBC__
    
    From-SVN: r190973

---
+++ gcc/config/avr/avrlibc.h
@@ -0,0 +1,31 @@
+/* Definitions of target machine for the GNU compiler collection
+   for Atmel AVR micro controller.
+   Copyright (C) 2012
+   Free Software Foundation, Inc.
+   Contributed by Georg-Johann Lay (avr@gjlay.de)
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+/* AVR-Libc implements functions from libgcc.a in libm.a, see PR54461.  */
+
+#undef  LIBGCC_SPEC
+#define LIBGCC_SPEC                                                     \
+  "%{!mmcu=at90s1*:%{!mmcu=attiny11:%{!mmcu=attiny12:%{!mmcu=attiny15:%{!mmcu=attiny28: -lgcc -lm }}}}}"
+
+#undef  LINK_GCC_C_SEQUENCE_SPEC
+#define LINK_GCC_C_SEQUENCE_SPEC \
+  "--start-group %G %L --end-group"
