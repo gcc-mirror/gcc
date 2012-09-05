fd28e216af180749a1b733a72218754ecc1b25d6
gjl <gjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-09-05 08:47:50 +0000

    PR target/54461 	* configure.ac (noconfigdirs,target=avr-*-*): Add target-newlib, 	target-libgloss if not configured --with-avrlibc=no. 	* configure: Regenerate.
    libgcc/
    	PR target/54461
    	* config.host (tmake_file,host=avr-*-*): Add avr/t-avrlibc if
    	not configured --with-avrlibc=no.
    	* config/avr/t-avrlibc: New file.
    	* Makefile.in (FPBIT_FUNCS): filter-out LIB2FUNCS_EXCLUDE.
    	(DPBIT_FUNCS): Ditto.
    	(TPBIT_FUNCS): Ditto.
    
    gcc/
    	PR target/54461
    	* config.gcc (tm_file,target=avr-*-*): Add avr/avrlibc.h if
    	not configured --with-avrlibc=no.
    	(tm_defines,target=avr-*-*): Add WITH_AVRLIBC if not configured
    	--with-avrlibc=no.
    	* config/avr/avrlibc.h: New file.
    	* config/avr/avr-c.c: Build-in define __WITH_AVRLIBC__ if
    	not configured --with-avrlibc=no.
    	* doc/invoke.texi (AVR Built-in Macros): Document __WITH_AVRLIBC__
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@190967 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/avr/avr-c.c
+++ gcc/config/avr/avr-c.c
@@ -148,6 +148,10 @@ avr_cpu_cpp_builtins (struct cpp_reader *pfile)
   cpp_define_formatted (pfile, "__AVR_SFR_OFFSET__=0x%x",
                         avr_current_arch->sfr_offset);
     
+#ifdef WITH_AVRLIBC
+  cpp_define (pfile, "__WITH_AVRLIBC__");
+#endif /* WITH_AVRLIBC */
+      
   /* Define builtin macros so that the user can easily query if or if not
      non-generic address spaces (and which) are supported.
      This is only supported for C.  For C++, a language extension is needed
