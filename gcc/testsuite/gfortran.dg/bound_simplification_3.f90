ca5c44227dd24ded1dbe654ba461b19ec2b87f34
Mikael Morin <mikael@gcc.gnu.org>
2012-09-08 09:22:54 +0000

    re PR fortran/54208 (compilation error for ubound construct in PARAMETER statements)
    fortran/
    	PR fortran/54208
    	* simplify.c (simplify_bound_dim): Resolve array spec before
    	proceeding with simplification.
    
    testsuite/
    	PR fortran/54208
    	* gfortran.dg/bound_simplification_3.f90: New test.
    
    From-SVN: r191090

---
+++ gcc/testsuite/gfortran.dg/bound_simplification_3.f90
@@ -0,0 +1,23 @@
+! { dg-do compile }
+! { dg-options "-fdump-tree-original" }
+!
+! PR fortran/54208
+! The I and J definitions used to raise an error because ARR's array spec
+! was resolved to late for the LBOUND and UBOUND calls to be simplified to
+! a constant.
+!
+! Contributed by Carlos A. Cruz <carlos.a.cruz@nasa.gov>
+
+program testit
+  integer, parameter :: n=2
+  integer, dimension(1-min(n,2)/2:n) :: arr
+  integer, parameter :: i=lbound(arr,1)
+  integer, parameter :: j=ubound(arr,1)
+  ! write(6,*) i, j
+  if (i /= 0) call abort
+  if (j /= 2) call abort
+end program testit
+
+! { dg-final { scan-tree-dump-times "bound" 0 "original" } }
+! { dg-final { scan-tree-dump-times "abort" 0 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
