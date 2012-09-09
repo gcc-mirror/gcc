84dbe9a415a440131bafce979ca9696e07e5dfdb
Jonathan Wakely <jwakely.gcc@gmail.com>
2012-09-09 18:40:46 +0000

    re PR libstdc++/54388 (std::array.at() const results in undefined behaviour)
    	PR libstdc++/54388
    	* include/std/array (array::at() const): Ensure lvalue result.
    	* testsuite/23_containers/array/element_access/54388.cc: New.
    
    From-SVN: r191117

---
+++ libstdc++-v3/testsuite/23_containers/array/element_access/54388.cc
@@ -0,0 +1,42 @@
+// { dg-options "-std=gnu++0x" }
+//
+// Copyright (C) 2012 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <array>
+#include <testsuite_hooks.h>
+
+struct A
+{
+  bool valid = true;
+  ~A() { valid = false; }
+};
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  const std::array<A, 1> a;
+  const A& aa = a.at(0);
+  VERIFY(aa.valid);
+}
+
+int main()
+{
+  test01();
+}
