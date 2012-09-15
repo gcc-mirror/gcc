11d3f544c02f2845f1a5230f3a6fe500b7960d42
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-09-15 15:42:01 +0000

    2012-09-15  Tobias Burnus  <burnus@net-b.de>
            * match.c (lock_unlock_statement, sync_statement): Fix potential
            double freeing.
            (sync_statement): Remove unreachable code.
            * simplify.c (gfc_simplify_bessel_n2): Avoid double freeing.
            (gfc_simplify_repeat): Remove bogus code.
            * target-memory.h (gfc_target_encode_expr): Update prototype.
            * target-memory.c (gfc_target_encode_expr, encode_array,
            encode_derived): Return unsigned HOST_WIDE_INT.
            (gfc_target_interpret_expr): Add assert.
            (gfc_merge_initializers): Fix "== 0" check for mpz_t.
            * symbol.c (gfc_get_typebound_proc): Add assert.
            (gfc_merge_initializers): Remove unreachable check.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@191343 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/target-memory.c
+++ gcc/fortran/target-memory.c
@@ -140,7 +140,7 @@ gfc_target_expr_size (gfc_expr *e)
    return the number of bytes of the buffer that have been
    used.  */
 
-static int
+static unsigned HOST_WIDE_INT
 encode_array (gfc_expr *expr, unsigned char *buffer, size_t buffer_size)
 {
   mpz_t array_size;
@@ -217,13 +217,14 @@ gfc_encode_character (int kind, int length, const gfc_char_t *string,
 }
 
 
-static int
+static unsigned HOST_WIDE_INT
 encode_derived (gfc_expr *source, unsigned char *buffer, size_t buffer_size)
 {
   gfc_constructor *c;
   gfc_component *cmp;
   int ptr;
   tree type;
+  HOST_WIDE_INT size;
 
   type = gfc_typenode_for_spec (&source->ts);
 
@@ -239,19 +240,24 @@ encode_derived (gfc_expr *source, unsigned char *buffer, size_t buffer_size)
 	    + TREE_INT_CST_LOW(DECL_FIELD_BIT_OFFSET(cmp->backend_decl))/8;
 
       if (c->expr->expr_type == EXPR_NULL)
- 	memset (&buffer[ptr], 0,
-		int_size_in_bytes (TREE_TYPE (cmp->backend_decl)));
+	{
+	  size = int_size_in_bytes (TREE_TYPE (cmp->backend_decl));
+	  gcc_assert (size >= 0);
+	  memset (&buffer[ptr], 0, size);
+	}
       else
 	gfc_target_encode_expr (c->expr, &buffer[ptr],
 				buffer_size - ptr);
     }
 
-  return int_size_in_bytes (type);
+  size = int_size_in_bytes (type);
+  gcc_assert (size >= 0);
+  return size;
 }
 
 
 /* Write a constant expression in binary form to a buffer.  */
-int
+unsigned HOST_WIDE_INT
 gfc_target_encode_expr (gfc_expr *source, unsigned char *buffer,
 			size_t buffer_size)
 {
@@ -567,6 +573,7 @@ gfc_target_interpret_expr (unsigned char *buffer, size_t buffer_size,
     case BT_DERIVED:
       result->representation.length = 
         gfc_interpret_derived (buffer, buffer_size, result);
+      gcc_assert (result->representation.length >= 0);
       break;
 
     default:
@@ -678,7 +685,7 @@ gfc_merge_initializers (gfc_typespec ts, gfc_expr *e, unsigned char *data,
 	{
 	  size_t elt_size = gfc_target_expr_size (c->expr);
 
-	  if (c->offset)
+	  if (mpz_cmp_si (c->offset, 0) != 0)
 	    len = elt_size * (size_t)mpz_get_si (c->offset);
 
 	  len = len + gfc_merge_initializers (ts, c->expr, &data[len],
