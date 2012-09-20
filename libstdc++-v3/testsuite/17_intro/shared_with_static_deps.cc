ac6d1200be5d5f6c49a8b6c4ae447bf82c449c25
Benjamin Kosnik <bkoz@redhat.com>
2012-09-20 02:10:22 +0000

    re PR libstdc++/28811 (--with-pic vs static libraries and libstdc++)
    2012-09-18  Benjamin Kosnik  <bkoz@redhat.com>
    
            PR libstdc++/28811
            PR libstdc++/54482
            * configure.ac (glibcxx_lt_pic_flag,
              glibcxx_compiler_pic_flag,
              glibcxx_compiler_shared_flag): New. Use them.
            (lt_prog_compiler_pic_CXX): Set via glibcxx_*_flag(s) above.
            (pic_mode): Set to default.
            (PIC_CXXFLAGS): Remove.
            * Makefile.am (PICFLAG, PICFLAG_FOR_TARGET): Remove. Comment.
            * libsupc++/Makefile.am: Use glibcxx_ld_pic_flag and
              glibcxx_compiler_shared_flag. Comment.
            * src/c++11/Makefile.am: Same.
            * src/c++98/Makefile.am: Same.
            * src/Makefile.am: Use glibcxx_compiler_pic_flag.
    
            * Makefile.in: Regenerated.
            * aclocal.m4: Same.
            * configure: Same.
            * doc/Makefile.in: Same.
            * include/Makefile.in: Same.
            * libsupc++/Makefile.in: Same.
            * po/Makefile.in: Same.
            * python/Makefile.in: Same.
            * src/Makefile.in: Same.
            * src/c++11/Makefile.in: Same.
            * src/c++98/Makefile.in: Same.
            * testsuite/Makefile.in: Same.
    
            * src/c++11/compatibility-atomic-c++0x.cc: Use
              _GLIBCXX_SHARED instead of PIC to designate shared-only
              code blocks.
            * src/c++11/compatibility-c++0x.cc: Same.
            * src/c++11/compatibility-thread-c++0x.cc: Same.
            * src/c++98/compatibility-list-2.cc: Same.
            * src/c++98/compatibility.cc: : Same.
    
            * testsuite/17_intro/shared_with_static_deps.cc: New.
    
            * doc/xml/manual/build_hacking.xml: Separate configure from
            make/build issues, add build details.
    
    From-SVN: r191509

---
+++ libstdc++-v3/testsuite/17_intro/shared_with_static_deps.cc
@@ -0,0 +1,33 @@
+// { dg-do link }
+// { dg-require-static-libstdcxx }
+// { dg-require-sharedlib "" }
+// { dg-require-effective-target fpic }
+// { dg-options "-shared -fPIC -static-libgcc -static-libstdc++" }
+
+// Copyright (C) 2012 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>
+
+// libstdc++/28811 --with-pic vs. static linking
+#include <iostream>
+#include <locale>
+
+int main()
+{
+  std::locale c __attribute__((unused)) = std::locale::classic();
+  std::cout << "i am old-skool\n";
+  return 0;
+}
