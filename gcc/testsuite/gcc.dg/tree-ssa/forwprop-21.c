9606465f8cb5e4b79c8467d6b5d410abcb4eca62
Marc Glisse <marc.glisse@inria.fr>
2012-09-20 11:37:05 +0200

    forwprop-19.c: Check in forwprop1.
    2012-09-20  Marc Glisse  <marc.glisse@inria.fr>
    
    	* gcc.dg/tree-ssa/forwprop-19.c: Check in forwprop1.
    	* gcc.dg/tree-ssa/forwprop-20.c: Check in forwprop1.
    	* gcc.dg/tree-ssa/forwprop-21.c: Check in copyprop1.
    	* gcc.dg/tree-ssa/forwprop-22.c: Check in copyprop1.
    
    From-SVN: r191560

--- gcc/testsuite/gcc.dg/tree-ssa/forwprop-21.c
+++ gcc/testsuite/gcc.dg/tree-ssa/forwprop-21.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O -fdump-tree-optimized" } */
+/* { dg-options "-O -fdump-tree-copyprop1" } */
 typedef int v4si __attribute__ ((vector_size (4 * sizeof(int))));
 
 int
@@ -9,5 +9,8 @@ test (v4si *x, v4si *y)
   v4si z = __builtin_shuffle (*x, *y, m);
   return z[2];
 }
-/* { dg-final { scan-tree-dump-not "VEC_PERM_EXPR" "optimized" } } */
-/* { dg-final { cleanup-tree-dump "optimized" } } */
+
+/* Optimization in forwprop1, cleanup in copyprop1.  */
+
+/* { dg-final { scan-tree-dump-not "VEC_PERM_EXPR" "copyprop1" } } */
+/* { dg-final { cleanup-tree-dump "copyprop1" } } */
