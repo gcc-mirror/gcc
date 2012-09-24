58bf5219edd00f3fa401e2fe8a8048820a7ac96a
glisse <glisse@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-09-24 12:43:43 +0000

    2012-09-24  Marc Glisse  <marc.glisse@inria.fr>
    gcc/
    	* tree-ssa-forwprop.c: Include tree-ssa-propagate.h.
    	(simplify_bitfield_ref): Handle constructors.
    	* Makefile.in (tree-ssa-forwprop.o): Depend on tree-ssa-propagate.h.
    
    gcc/testsuite/
    	* gcc.dg/tree-ssa/forwprop-23.c: New testcase.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@191665 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/forwprop-23.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-forwprop1" } */
+
+typedef long vec __attribute__ ((vector_size (2 * sizeof (long))));
+
+long f (long d, long e)
+{
+  vec x = { d, e };
+  vec m = { 1, 0 };
+  return __builtin_shuffle (x, m) [1];
+}
+
+
+/* { dg-final { scan-tree-dump-not "BIT_FIELD_REF" "forwprop1" } } */
+/* { dg-final { cleanup-tree-dump "forwprop1" } } */
