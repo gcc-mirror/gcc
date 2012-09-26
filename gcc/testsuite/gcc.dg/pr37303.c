56518289cee3393cf126af8d3aa6e692fa531cd0
sje <sje@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-09-26 20:33:28 +0000

    2012-09-26  Steve Ellcey  <sellcey@mips.com>
    	PR c/37303
    	* gcc.dg/pr37303.c: Check for rdata or rodata.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@191772 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr37303.c
+++ gcc/testsuite/gcc.dg/pr37303.c
@@ -1,5 +1,5 @@
 /* { dg-do compile { target *-*-elf* *-*-linux-gnu* } } */
 /* { dg-options "-std=c99" }
-/* { dg-final { scan-assembler "rodata" } } */
+/* { dg-final { scan-assembler "rdata|rodata" } } */
 
 struct S { const int *x; } s = { (const int[]){1, 2, 3} };
