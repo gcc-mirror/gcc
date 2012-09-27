54730221d58496de906e9fca8e8f741a8cca581e
Jakub Jelinek <jakub@redhat.com>
2012-09-27 12:53:42 +0200

    re PR target/54703 (_mm_sub_pd is incorrectly substituted with vandnps)
    	PR target/54703
    	* simplify-rtx.c (simplify_binary_operation_1): Perform
    	(x - (x & y)) -> (x & ~y) optimization only for integral
    	modes.
    
    	* gcc.target/i386/pr54703.c: New test.
    
    From-SVN: r191802

--- gcc/simplify-rtx.c
+++ gcc/simplify-rtx.c
@@ -1,7 +1,7 @@
 /* RTL simplification functions for GNU compiler.
    Copyright (C) 1987, 1988, 1989, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
    1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
-   2011  Free Software Foundation, Inc.
+   2011, 2012  Free Software Foundation, Inc.
 
 This file is part of GCC.
 
@@ -2239,7 +2239,7 @@ simplify_binary_operation_1 (enum rtx_code code, enum machine_mode mode,
 				    neg_const_int (mode, op1));
 
       /* (x - (x & y)) -> (x & ~y) */
-      if (GET_CODE (op1) == AND)
+      if (INTEGRAL_MODE_P (mode) && GET_CODE (op1) == AND)
 	{
 	  if (rtx_equal_p (op0, XEXP (op1, 0)))
 	    {
