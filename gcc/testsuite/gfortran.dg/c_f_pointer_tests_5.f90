abd3b740fb74c33bca0c71d02ecb65f7ae2ee000
janus <janus@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-09-30 16:36:02 +0000

    2012-09-30  Janus Weil  <janus@gcc.gnu.org>
    	PR fortran/54667
    	* intrinsic.texi (C_F_POINTER): Fix description.
    	* resolve.c (gfc_iso_c_sub_interface): Add a check for FPTR argument
    	of C_F_POINTER. Modify two error messages. Cleanup.
    
    2012-09-30  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/54667
    	* gfortran.dg/c_funloc_tests_6.f90: Modified error message.
    	* gfortran.dg/c_f_pointer_shape_test.f90: Ditto.
    	* gfortran.dg/c_f_pointer_tests_5.f90: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@191870 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/c_f_pointer_tests_5.f90
@@ -0,0 +1,13 @@
+! { dg-do compile }
+!
+! PR 54667: [OOP] gimplification failure with c_f_pointer
+!
+! Contributed by Andrew Benson <abensonca@gmail.com>
+
+use, intrinsic :: ISO_C_Binding
+type :: nc
+end type
+type(c_ptr) :: cSelf
+class(nc), pointer :: self
+call c_f_pointer(cSelf, self)  ! { dg-error "must not be polymorphic" }
+end
