0a59e5832e6febfd497966c0b0edfe948f34ff7a
Janus Weil <janus@gcc.gnu.org>
2012-10-02 23:02:16 +0200

    re PR fortran/54778 ([OOP] an ICE on invalid OO code)
    2012-10-02  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/54778
    	* interface.c (matching_typebound_op): Check for 'class_ok' attribute.
    
    2012-10-02  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/54778
    	* gfortran.dg/class_53.f90: New.
    
    From-SVN: r192005

---
+++ gcc/testsuite/gfortran.dg/class_53.f90
@@ -0,0 +1,18 @@
+! { dg-do compile }
+!
+! PR 54778: [OOP] an ICE on invalid OO code
+!
+! Contributed by Sylwester Arabas <slayoo@staszic.waw.pl>
+
+implicit none
+
+type :: arr_t
+  real :: at
+end type
+
+type(arr_t) :: this
+class(arr_t) :: elem   ! { dg-error "must be dummy, allocatable or pointer" }
+
+elem = this   ! { dg-error "Variable must not be polymorphic in intrinsic assignment" }
+
+end
