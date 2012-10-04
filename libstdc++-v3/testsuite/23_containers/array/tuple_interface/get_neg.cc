90f9c94e02d8b8ce71d6f5793b25a3b3849fc133
Paolo Carlini <paolo.carlini@oracle.com>
2012-10-04 00:02:29 +0000

    re PR libstdc++/53248 (std::array<T,0> doesn't work when T is not default-constructible)
    2012-10-03  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR libstdc++/53248
    	* include/std/array (__array_traits<>): Add.
    	(array<>): Allow for zero-size arrays of non default-constructible
    	elements.
    	* testsuite/23_containers/array/requirements/
    	non_default_constructible.cc: New.
    	* testsuite/23_containers/array/requirements/zero_sized_arrays.cc:
    	Adjust.
    	* testsuite/23_containers/array/tuple_interface/get_neg.cc: Adjust
    	dg-error line numbers.
    	* testsuite/23_containers/array/tuple_interface/tuple_element_neg.cc:
    	Likewise.
    
    From-SVN: r192056

--- libstdc++-v3/testsuite/23_containers/array/tuple_interface/get_neg.cc
+++ libstdc++-v3/testsuite/23_containers/array/tuple_interface/get_neg.cc
@@ -27,6 +27,6 @@ int n1 = std::get<1>(a);
 int n2 = std::get<1>(std::move(a));
 int n3 = std::get<1>(ca);
 
-// { dg-error "static assertion failed" "" { target *-*-* } 275 }
-// { dg-error "static assertion failed" "" { target *-*-* } 283 }
-// { dg-error "static assertion failed" "" { target *-*-* } 267 }
+// { dg-error "static assertion failed" "" { target *-*-* } 288 }
+// { dg-error "static assertion failed" "" { target *-*-* } 296 }
+// { dg-error "static assertion failed" "" { target *-*-* } 304 }
