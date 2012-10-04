db7f5754860be307762eb44ad2a175d568646121
Eric Botcazou <ebotcazou@adacore.com>
2012-10-04 17:01:53 +0000

    * gcc.dg/lto/20120723_0.c: Skip on SPARC 32-bit.
    From-SVN: r192089

--- gcc/testsuite/gcc.dg/lto/20120723_0.c
+++ gcc/testsuite/gcc.dg/lto/20120723_0.c
@@ -1,7 +1,9 @@
 /* Make sure that by reference and by value aggregate jump functions do not get
-   mixed up.  */
+   mixed up.
+   ??? This testcase is invalid C and can only pass on specific platforms.  */
 /* { dg-lto-do run } */
-/* { dg-lto-options {{-O3 -fno-early-inlining -flto}} } */
+/* { dg-skip-if "" { { sparc*-*-* } && ilp32 } { "*" } { "" } } */
+/* { dg-lto-options { {-O3 -fno-early-inlining -flto}} } */
 
 extern void abort (void);
 
