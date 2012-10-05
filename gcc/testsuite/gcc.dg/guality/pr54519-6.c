eee0ea7cd086b41148cf831169438366f271034b
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-10-05 19:24:38 +0000

    PR debug/54519 	* ipa-split.c (split_function): Add debug args and 	debug source and normal stmts for args_to_skip which are 	gimple regs. 	* tree-inline.c (copy_debug_stmt): When inlining, adjust 	source debug bind stmts to debug binds of corresponding 	DEBUG_EXPR_DECL.
    	* gcc.dg/guality/pr54519-1.c: New test.
    	* gcc.dg/guality/pr54519-2.c: New test.
    	* gcc.dg/guality/pr54519-3.c: New test.
    	* gcc.dg/guality/pr54519-4.c: New test.
    	* gcc.dg/guality/pr54519-5.c: New test.
    	* gcc.dg/guality/pr54519-6.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@192139 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/guality/pr54519-6.c
@@ -0,0 +1,27 @@
+/* PR debug/54519 */
+/* { dg-do run } */
+/* { dg-options "-g" } */
+
+#include "../nop.h"
+
+static inline void
+f1 (int x, int y)
+{
+  asm volatile (NOP);	/* { dg-final { gdb-test 11 "x" "2" } } */
+  asm volatile (NOP);	/* { dg-final { gdb-test 11 "y" "0" } } */
+}
+
+static inline void
+f2 (int z)
+{
+  f1 (z, 0);
+  f1 (z, 1);
+}
+
+int
+main ()
+{
+  f2 (2);
+  f2 (3);
+  return 0;
+}
