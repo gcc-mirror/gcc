5b5d3d76253622379bd7e60fe948c4b5b0988125
Paolo Carlini <paolo.carlini@oracle.com>
2012-10-09 16:09:32 +0000

    re PR c++/53763 (Missing error check on decltype when used within variadic template argument list)
    2012-10-09  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/53763
    	* g++.dg/cpp0x/decltype43.C: New.
    
    From-SVN: r192263

---
+++ gcc/testsuite/g++.dg/cpp0x/decltype43.C
@@ -0,0 +1,27 @@
+// PR c++/53763
+// { dg-do compile { target c++11 } }
+
+template<typename TYPE>
+struct A
+{
+  static int a(TYPE value)
+  {
+    return value;
+  }
+};
+
+template<typename... ARGS>
+struct B
+{
+  static int b(ARGS...)
+  {
+    return 0;
+  }
+};
+
+int main()
+{
+  int x = B<decltype(A<int>::a(1))>::b(A<int>::a(1));
+  int y = B<decltype(A     ::a(2))>::b(A<int>::a(2)); // { dg-error "template argument" }
+  return x + y;
+}
