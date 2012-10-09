883554eb10ec811c13458b4153fe24da738f8e23
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-10-09 11:46:25 +0000

    gcc/ 2012-10-08  Tobias Burnus  <burnus@net-b.de>
    	* lto-cgraph.c (input_node_opt_summary): Remove unused code.
    	* lto-opts.c (append_to_collect_gcc_options): Fix condition.
    	* lto-symtab.c (lto_cgraph_replace_node): Don't xstrdup string
    	which is passed to fprintf.
    
    gcc/lto/
    2012-10-08  Tobias Burnus  <burnus@net-b.de>
    
    	* lto-lang.c (lto_register_builtin_type): Avoid useless
    	decl creation.
    	* lto-object.c (lto_obj_file_open, lto_obj_file_open): Free memory.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@192250 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/lto/lto-lang.c
+++ gcc/lto/lto-lang.c
@@ -1079,10 +1079,13 @@ lto_register_builtin_type (tree type, const char *name)
 {
   tree decl;
 
-  decl = build_decl (UNKNOWN_LOCATION, TYPE_DECL, get_identifier (name), type);
-  DECL_ARTIFICIAL (decl) = 1;
   if (!TYPE_NAME (type))
-    TYPE_NAME (type) = decl;
+    {
+      decl = build_decl (UNKNOWN_LOCATION, TYPE_DECL,
+			 get_identifier (name), type);
+      DECL_ARTIFICIAL (decl) = 1;
+      TYPE_NAME (type) = decl;
+    }
 
   registered_builtin_types = tree_cons (0, type, registered_builtin_types);
 }
