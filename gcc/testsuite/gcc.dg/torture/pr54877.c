2de45fb6ee035764a6c48bc87086be9fcab7aa53
Jakub Jelinek <jakub@redhat.com>
2012-10-10 19:47:05 +0200

    re PR tree-optimization/54877 (ICE: tree check: expected ssa_name, have real_cst in copy_ssa_name_fn, at tree-ssanames.c:335)
    	PR tree-optimization/54877
    	* tree-vect-loop.c (vect_is_simple_reduction_1): For MINUS_EXPR
    	use make_ssa_name instead of copy_ssa_name.
    
    	* gcc.dg/torture/pr54877.c: New test.
    
    From-SVN: r192322

---
+++ gcc/testsuite/gcc.dg/torture/pr54877.c
@@ -0,0 +1,23 @@
+/* PR tree-optimization/54877 */
+/* { dg-do run } */
+/* { dg-options "-ffast-math" } */
+
+extern void abort (void);
+
+int
+foo (void)
+{
+  double d;
+  int i;
+  for (i = 0, d = 0; i < 64; i++)
+    d--;
+  return (int) d;
+}
+
+int
+main ()
+{
+  if (foo () != -64)
+    abort ();
+  return 0;
+}
