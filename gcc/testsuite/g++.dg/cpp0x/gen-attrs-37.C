21d505d8c0867cd1efafd31923b44328abe257ae
dodji <dodji@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-10-10 10:24:50 +0000

    Update g++.dg/cpp0x/gen-attrs-{8,36,37}.C as c++11 attributes to types are ignored
    The current implementation of C++11 attributes forbids them from being
    applied to a type unless the type is being declared.  I forgot to
    adjust g++.dg/cpp0x/gen-attrs-{8,36,37}.C that was being run only on
    ia32.
    
    Fixed thus, tested on i386-unknown-linux-gnu and
    x86_64-unknown-linux-gnu against trunk.
    
    gcc/testsuite/
    
    	* g++.dg/cpp0x/gen-attrs-8.C: Update the test to reflect the fact
    	that c++11 attributes to types are ignored for now.
    	* g++.dg/cpp0x/gen-attrs-36.C: Likewise.
    	* g++.dg/cpp0x/gen-attrs-37.C: Likewise
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@192300 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp0x/gen-attrs-37.C
+++ gcc/testsuite/g++.dg/cpp0x/gen-attrs-37.C
@@ -2,12 +2,14 @@
 // { dg-options "-std=c++11 -pedantic" }
 // { dg-do compile { target { { i?86-*-* x86_64-*-* } && ia32 } } }
 
+// c++11 attributes that apply to types are ignored for now
+
 struct S {
   int x;
   S(const S &s) {}
 };
 
-S getS() [[gnu::__stdcall__]];
+S getS() [[gnu::__stdcall__]];  // { dg-warning "ignored" }
 
 void test()
 {
--- gcc/testsuite/g++.dg/cpp0x/gen-attrs-8.C
+++ gcc/testsuite/g++.dg/cpp0x/gen-attrs-8.C
@@ -2,5 +2,5 @@
 // { dg-do compile { target { { i?86-*-* x86_64-*-* } && ia32 } } }
 
 extern int * ([[gnu::stdcall]] *fooPtr)( void); // { dg-error "expected" }
-int * [[gnu::stdcall]] myFn01( void) { return 0; }// { dg-warning "attribute only applies to function types" }
+int * [[gnu::stdcall]] myFn01( void) { return 0; }// { dg-warning "ignored" }
 
