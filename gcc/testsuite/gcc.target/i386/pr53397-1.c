ce6eb27d7d8d3867682f72d26df8fabd07537fa9
vekumar <vekumar@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-10-10 15:31:27 +0000

    Move tests written for pr53397 from gcc.dg to gcc.target/i386
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@192318 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/pr53397-1.c
+++ gcc/testsuite/gcc.target/i386/pr53397-1.c
@@ -1,7 +1,7 @@
 /* Prefetching when the step is loop invariant.  */
-/* { dg-do compile { target { i?86-*-* x86_64-*-* } } } */
+/* { dg-do compile } */
 /* { dg-require-effective-target sse2 } */
-/* { dg-options "-O3 -fprefetch-loop-arrays -fdump-tree-aprefetch-details --param min-insn-to-prefetch-ratio=3 --param simultaneous-prefetches=10 -fdump-tree-aprefetch-details" } */
+/* { dg-options "-O3 -msse2 -fprefetch-loop-arrays -fdump-tree-aprefetch-details --param min-insn-to-prefetch-ratio=3 --param simultaneous-prefetches=10 -fdump-tree-aprefetch-details" } */
 
 
 double data[16384];
--- gcc/testsuite/gcc.target/i386/pr53397-2.c
+++ gcc/testsuite/gcc.target/i386/pr53397-2.c
@@ -1,7 +1,7 @@
 /* Not prefetching when the step is loop variant.  */
-/* { dg-do compile { target { i?86-*-* x86_64-*-* } } } */
+/* { dg-do compile } */
 /* { dg-require-effective-target sse2 } */
-/* { dg-options "-O3 -fprefetch-loop-arrays -fdump-tree-aprefetch-details --param min-insn-to-prefetch-ratio=3 --param simultaneous-prefetches=10 -fdump-tree-aprefetch-details" } */
+/* { dg-options "-O3 -msse2 -fprefetch-loop-arrays -fdump-tree-aprefetch-details --param min-insn-to-prefetch-ratio=3 --param simultaneous-prefetches=10 -fdump-tree-aprefetch-details" } */
 
 double data[16384];
 void donot_prefetch_when_non_constant_step_is_variant(int step, int n)
