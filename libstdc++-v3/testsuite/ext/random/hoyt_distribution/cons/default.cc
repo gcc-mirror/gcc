ad7e9ce5491aa49fb0b8f9a764ea08130aa1a915
emsr <emsr@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-10-12 15:15:21 +0000

    dd the Hoyt and the arcsine distributions as extensions.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@192403 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/ext/random/hoyt_distribution/cons/default.cc
@@ -0,0 +1,44 @@
+// { dg-options "-std=c++11" }
+// { dg-require-cstdint "" }
+//
+// 2012-10-12  Edward M. Smith-Rowland <3dw4rd@verizon.net>
+//
+// Copyright (C) 2012 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <ext/random>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  __gnu_cxx::hoyt_distribution<> u;
+  VERIFY( u.q() == 0.5 );
+  VERIFY( u.omega() == 1.0 );
+  VERIFY( u.min() == 0.0 );
+  typedef __gnu_cxx::hoyt_distribution<>::result_type result_type;
+  VERIFY( u.max() == std::numeric_limits<result_type>::max() );
+}
+
+int
+main()
+{
+  test01();
+  return 0;
+}
