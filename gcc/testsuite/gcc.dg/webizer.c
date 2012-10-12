eca62c0670bcaaa76a35646afb86a9fc1d359d96
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-10-12 20:36:09 +0000

    * web.c (web_main): Do not set DF_RD_PRUNE_DEAD_DEFS flag.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@192413 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/webizer.c
@@ -0,0 +1,35 @@
+/* { dg-do run } */
+/* { dg-options "-O3 -funroll-loops" } */
+typedef struct rowbox {
+    int startx ;
+    int endx ;
+    int endx1 ;
+    int startx2 ;
+    int ypos ;
+    int desiredL ;
+} ROWBOX ;
+ROWBOX rowArray1[2] ;
+ROWBOX *rowArray = rowArray1;
+
+int numRows = 2;
+
+int row = 1;
+int block = 0;
+double ckt_size_factor ;
+
+__attribute__ ((noinline))
+configure2()
+{
+  block = 0 ;
+  for( row = 1 ; row <= numRows ; row++ ) {
+      block++ ;
+    if( rowArray[row].endx1 > 0 ) {
+      block++ ;
+    }
+  }
+}
+
+main()
+{
+  configure2();
+}
