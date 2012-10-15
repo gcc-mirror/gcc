860f5e77b7244a305026eb257d575a4856edfbe4
Uros Bizjak <ubizjak@gmail.com>
2012-10-15 20:42:03 +0200

    sse.md (UNSPEC_MOVU): Remove.
    	* config/i386/sse.md (UNSPEC_MOVU): Remove.
    	(UNSPEC_LOADU): New.
    	(UNSPEC_STOREU): Ditto.
    	(<sse>_movu<ssemodesuffix><avxsizesuffix>): Split to ...
    	(<sse>_loadu<ssemodesuffix><avxsizesuffix>): ... this and ...
    	(<sse>_storeu<ssemodesuffix><avxsizesuffix>) ... this.
    	(<sse2>_movdqu<avxsizesuffix>): Split to ...
    	(<sse2>_loaddqu<avxsizesuffix>): ... this and ...
    	(<sse2>_storedqu<avxsizesuffix>): ... this.
    	(*sse4_2_pcmpestr_unaligned): Update.
    	(*sse4_2_pcmpistr_unaligned): Ditto.
    
    	* config/i386/i386.c (ix86_avx256_split_vector_move_misalign): Use
    	gen_avx_load{dqu,ups,upd}256 to load from unaligned memory and
    	gen_avx_store{dqu,ups,upd}256 to store to unaligned memory.
    	(ix86_expand_vector_move_misalign): Use gen_sse_loadups or
    	gen_sse2_load{dqu,upd} to load from unaligned memory and
    	gen_sse_loadups or gen_sse2_store{dqu,upd}256 to store to
    	unaligned memory.
    	(struct builtin_description bdesc_spec) <IX86_BUILTIN_LOADUPS>:
    	Use CODE_FOR_sse_loadups.
    	<IX86_BUILTIN_LOADUPD>: Use CODE_FOR_sse2_loadupd.
    	<IX86_BUILTIN_LOADDQU>: Use CODE_FOR_sse2_loaddqu.
    	<IX86_BUILTIN_STOREUPS>: Use CODE_FOR_sse_storeups.
    	<IX86_BUILTIN_STOREUPD>: Use CODE_FOR_sse2_storeupd.
    	<IX86_BUILTIN_STOREDQU>: Use CODE_FOR_sse2_storedqu.
    	<IX86_BUILTIN_LOADUPS256>: Use CODE_FOR_avx_loadups256.
    	<IX86_BUILTIN_LOADUPD256>: Use CODE_FOR_avx_loadupd256.
    	<IX86_BUILTIN_LOADDQU256>: Use CODE_FOR_avx_loaddqu256.
    	<IX86_BUILTIN_STOREUPS256>: Use CODE_FOR_avx_storeups256.
    	<IX86_BUILTIN_STOREUPD256>: Use CODE_FOR_avx_storeupd256.
    	<IX86_BUILTIN_STOREDQU256>: Use CODE_FOR_avx_storedqu256.
    
    testsuite/ChangeLog:
    
    	* gcc.target/i386/avx256-unaligned-load-1.c: Update asm scan patterns.
    	* gcc.target/i386/avx256-unaligned-load-2.c: Ditto.
    	* gcc.target/i386/avx256-unaligned-load-3.c: Ditto.
    	* gcc.target/i386/avx256-unaligned-load-4.c: Ditto.
    	* gcc.target/i386/avx256-unaligned-store-1.c: Ditto.
    	* gcc.target/i386/avx256-unaligned-store-2.c: Ditto.
    	* gcc.target/i386/avx256-unaligned-store-3.c: Ditto.
    	* gcc.target/i386/avx256-unaligned-store-4.c: Ditto.
    
    From-SVN: r192468

--- gcc/testsuite/gcc.target/i386/avx256-unaligned-store-1.c
+++ gcc/testsuite/gcc.target/i386/avx256-unaligned-store-1.c
@@ -17,6 +17,6 @@ avx_test (void)
     d[i] = c[i] * 20.0;
 }
 
-/* { dg-final { scan-assembler-not "avx_movups256/2" } } */
+/* { dg-final { scan-assembler-not "avx_storeups256" } } */
 /* { dg-final { scan-assembler "vmovups.*\\*movv4sf_internal/3" } } */
 /* { dg-final { scan-assembler "vextractf128" } } */
