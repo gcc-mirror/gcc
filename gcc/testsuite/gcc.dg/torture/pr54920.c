f843144b28ada08524d795e248cab60d56a0a25d
Richard Guenther <rguenther@suse.de>
2012-10-15 11:22:49 +0000

    re PR tree-optimization/54920 (segfault in tree-ssa-pre.c during Firefox build)
    2012-10-15  Richard Guenther  <rguenther@suse.de>
    
    	PR tree-optimization/54920
    	* tree-ssa-pre.c (create_expression_by_pieces): Properly
    	allocate temporary storage for all NARY elements.
    
    	* gcc.dg/torture/pr54920.c: New testcase.
    
    From-SVN: r192454

---
+++ gcc/testsuite/gcc.dg/torture/pr54920.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+
+typedef short __v8hi __attribute__ ((__vector_size__ (16)));
+typedef long long __m128i __attribute__ ((__vector_size__ (16)));
+int a;
+__m128i b;
+
+void
+fn1 ()
+{
+  while (1)
+    b = (__m128i) (__v8hi) { a, 0, 0, 0, 0, 0 };
+}
