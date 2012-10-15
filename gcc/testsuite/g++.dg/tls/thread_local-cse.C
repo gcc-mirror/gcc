95bf35bd2566026f8337185dff6633358b3bbb7f
Richard Sandiford <rdsandiford@googlemail.com>
2012-10-15 19:58:03 +0000

    thread_local-cse.C: Move dg-do line.
    gcc/testsuite/
    	* g++.dg/tls/thread_local-cse.C: Move dg-do line.
    	* g++.dg/tls/thread_local-wrap4.C: Require fpic.
    
    From-SVN: r192473

--- gcc/testsuite/g++.dg/tls/thread_local-cse.C
+++ gcc/testsuite/g++.dg/tls/thread_local-cse.C
@@ -1,11 +1,11 @@
 // Test for CSE of the wrapper function: we should only call it once
 // for the two references to ir.
+// { dg-do run }
 // { dg-options "-std=c++11 -O -fno-inline -save-temps" }
 // { dg-require-effective-target tls_runtime }
 // { dg-require-alias }
 // { dg-final { scan-assembler-times "call *_ZTW2ir" 1 { xfail *-*-* } } }
 // { dg-final cleanup-saved-temps }
-// { dg-do run }
 
 // XFAILed until the back end supports a way to mark a function as cseable
 // though not pure.
