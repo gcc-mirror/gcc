84cac7eff72290d1b2a57c75df602a02ffd2f56e
Eric Botcazou <ebotcazou@adacore.com>
2012-10-21 20:29:17 +0000

    atomic1.ads: XFAIL on MIPS.
    	* gnat.dg/specs/atomic1.ads: XFAIL on MIPS.
    	* gnat.dg/specs/addr1.ads: Likewise.
    
    From-SVN: r192658

--- gcc/testsuite/gnat.dg/specs/addr1.ads
+++ gcc/testsuite/gnat.dg/specs/addr1.ads
@@ -18,18 +18,18 @@ package Addr1 is
   A: Arr (1 .. 4);
 
   Obj1: Rec1;
-  for Obj1'Address use A'Address; -- { dg-bogus "alignment" }
+  for Obj1'Address use A'Address; -- { dg-bogus "(alignment|erroneous)" }
 
   Obj2: Rec2;
-  for Obj2'Address use A'Address; -- { dg-bogus "alignment" }
+  for Obj2'Address use A'Address; -- { dg-bogus "(alignment|erroneous)" "" { xfail mips*-*-* } }
 
   Obj3: Rec1;
-  for Obj3'Address use A(1)'Address; -- { dg-bogus "alignment" }
+  for Obj3'Address use A(1)'Address; -- { dg-bogus "(alignment|erroneous)" }
 
   Obj4: Rec1;
   for Obj4'Address use A(2)'Address; -- { dg-warning "(alignment|erroneous)" }
 
   Obj5: Rec1;
-  for Obj5'Address use A(3)'Address; -- { dg-bogus "alignment" }
+  for Obj5'Address use A(3)'Address; -- { dg-bogus "(alignment|erroneous)" }
 
 end Addr1;
