165a9c4345e2cbb19938dde828b97d8e176dc540
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-10-21 20:29:17 +0000

    * gnat.dg/specs/atomic1.ads: XFAIL on MIPS. 	* gnat.dg/specs/addr1.ads: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@192658 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gnat.dg/specs/atomic1.ads
+++ gcc/testsuite/gnat.dg/specs/atomic1.ads
@@ -6,11 +6,11 @@ package Atomic1 is
   type UA is access all Arr;
 
   U : UA;
-  pragma Atomic (U);  -- { dg-error "atomic access" }
+  pragma Atomic (U);  -- { dg-error "atomic access" "" { xfail mips*-*-* } }
 
   type R is record
     U : UA;
-    pragma Atomic (U);  -- { dg-error "atomic access" }
+    pragma Atomic (U);  -- { dg-error "atomic access" "" { xfail mips*-*-* } }
   end record;
 
 end Atomic1;
