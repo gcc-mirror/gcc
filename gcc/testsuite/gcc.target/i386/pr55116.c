2adaa795e40c898f70aa58a4487587490304ddaa
H.J. Lu <hongjiu.lu@intel.com>
2012-10-29 23:09:03 +0000

    Add a testcase for PR middle-end/55116
    	PR middle-end/55116
    	* gcc.target/i386/pr55116.c: New file.
    
    From-SVN: r192967

---
+++ gcc/testsuite/gcc.target/i386/pr55116.c
@@ -0,0 +1,11 @@
+/* { dg-do compile { target { ! { ia32 } } } } */
+/* { dg-options "-O2 -mx32 -maddress-mode=long" } */
+
+int glob_int_arr[100];
+int glob_int = 4;
+
+void
+expr_global (void)
+{
+  __builtin_prefetch (glob_int_arr + glob_int, 0, 0);
+}
