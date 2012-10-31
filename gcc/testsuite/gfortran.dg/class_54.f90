755634e60252de3270554fd06c222da98128c3ac
Janus Weil <janus@gcc.gnu.org>
2012-10-31 22:55:50 +0100

    re PR fortran/53718 ([OOP] gfortran generates asm label twice in the same output file)
    2012-10-31  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/53718
    	* trans.h (GFC_DECL_PUSH_TOPLEVEL): Removed.
    	* trans-decl.c (gfc_get_symbol_decl,gfc_generate_function_code): Remove
    	GFC_DECL_PUSH_TOPLEVEL.
    	(build_function_decl): Do not push __copy procedure to toplevel.
    
    2012-10-31  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/53718
    	* gfortran.dg/class_54.f90: New.
    
    From-SVN: r193048

---
+++ gcc/testsuite/gfortran.dg/class_54.f90
@@ -0,0 +1,22 @@
+! { dg-do compile }
+!
+! PR 53718: [4.7/4.8 regression] [OOP] gfortran generates asm label twice in the same output file
+!
+! Contributed by Adrian Prantl <adrian@llnl.gov>
+
+module m
+  type t
+  end type
+end module
+
+subroutine sub1
+  use m
+  class(t), pointer :: a1
+end subroutine
+
+subroutine sub2
+  use m
+  class(t), pointer :: a2
+end subroutine
+
+! { dg-final { cleanup-modules "m" } }
