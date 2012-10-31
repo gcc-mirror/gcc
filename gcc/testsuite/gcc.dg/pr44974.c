2a381a57f3061e171923a14083ac37d957c9c98d
Jan Hubicka <jh@suse.cz>
2012-11-01 00:10:22 +0100

    * gcc.dg/pr44974.c: Add noinline.
    From-SVN: r193050

--- gcc/testsuite/gcc.dg/pr44974.c
+++ gcc/testsuite/gcc.dg/pr44974.c
@@ -2,9 +2,9 @@
 /* { dg-do compile } */
 /* { dg-options "-O -fno-optimize-sibling-calls" } */
 
-extern void foo (int status) __attribute__ ((__noreturn__));
-extern void bar (int status) __attribute__ ((__noreturn__));
-extern void _Exit (int status) __attribute__ ((__noreturn__));
+extern void foo (int status) __attribute__ ((__noreturn__,__noinline__));
+extern void bar (int status) __attribute__ ((__noreturn__,__noinline__));
+extern void _Exit (int status) __attribute__ ((__noreturn__,__noinline__));
 
 void
 foo (int status)
