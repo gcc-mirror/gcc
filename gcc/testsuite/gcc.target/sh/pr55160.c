c291b2adc6f25d897928c79f431d987eb74e4bc5
Oleg Endo <olegendo@gcc.gnu.org>
2012-11-01 21:28:49 +0000

    re PR target/55160 (Counterproductive loop induction variable optimization)
    	PR target/55160
    	* gcc.target/sh/pr55160.c: New.
    
    From-SVN: r193071

---
+++ gcc/testsuite/gcc.target/sh/pr55160.c
@@ -0,0 +1,25 @@
+/* Check that the decrement-and-test instruction is generated.  */
+/* { dg-do compile { target "sh*-*-*" } } */
+/* { dg-options "-O1" } */
+/* { dg-skip-if "" { "sh*-*-*" } { "-m5*"} { "" } }  */
+/* { dg-final { scan-assembler-times "dt\tr" 2 } } */
+
+int
+test_00 (int* x, int c)
+{
+  int s = 0;
+  int i;
+  for (i = 0; i < c; ++i)
+    s += x[i];
+  return s;
+}
+
+int
+test_01 (int* x, int c)
+{
+  int s = 0;
+  int i;
+  for (i = 0; i < c; ++i)
+    s += *--x;
+  return s;
+}
