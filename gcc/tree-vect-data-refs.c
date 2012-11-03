6aaedacf296e05119dacbc8de51328fba51ada74
Peter Bergner <bergner@vnet.ibm.com>
2012-11-03 09:02:13 -0500

    backport: re PR middle-end/53708 (Many failures of the objc tests with -O3 -fnext-runtime and -m32)
    	Backport from mainline
    	2012-10-31  Jakub Jelinek  <jakub@redhat.com>
    
    	PR tree-optimization/53708
    	* tree-vect-data-refs.c (vect_can_force_dr_alignment_p): Preserve
    	user-supplied alignment when used with an explicit section name.
    
    From-SVN: r193121

--- gcc/tree-vect-data-refs.c
+++ gcc/tree-vect-data-refs.c
@@ -4574,6 +4574,13 @@ vect_can_force_dr_alignment_p (const_tree decl, unsigned int alignment)
   if (TREE_ASM_WRITTEN (decl))
     return false;
 
+  /* Do not override explicit alignment set by the user when an explicit
+     section name is also used.  This is a common idiom used by many
+     software projects.  */
+  if (DECL_SECTION_NAME (decl) != NULL_TREE
+      && !DECL_HAS_IMPLICIT_SECTION_NAME_P (decl))
+    return false;
+
   if (TREE_STATIC (decl))
     return (alignment <= MAX_OFILE_ALIGNMENT);
   else
