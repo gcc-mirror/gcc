e40bc135766ed0e5b371229a8486cf77b8c38d79
Uros Bizjak <uros@gcc.gnu.org>
2012-11-05 11:28:16 +0100

    * gcc.dg/tree-ssa/cunroll-9.c: Fix scan file name.
    From-SVN: r193154

--- gcc/testsuite/gcc.dg/tree-ssa/cunroll-9.c
+++ gcc/testsuite/gcc.dg/tree-ssa/cunroll-9.c
@@ -18,5 +18,5 @@ t (int n)
     }
   return sum;
 }
-/* { dg-final { scan-tree-dump-times "Removed pointless exit:" 1 "cunroli" } } */
-/* { dg-final { cleanup-tree-dump "cunroli" } } */
+/* { dg-final { scan-tree-dump-times "Removed pointless exit:" 1 "cunrolli" } } */
+/* { dg-final { cleanup-tree-dump "cunrolli" } } */
