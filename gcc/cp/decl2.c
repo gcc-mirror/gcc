740ee7c578567dc07b9d81b3cd737dbc603d5cc1
Jakub Jelinek <jakub@redhat.com>
2012-11-05 16:07:14 +0100

    backport: re PR c++/54988 (fpmath=sse target pragma causes inlining failure because of target specific option mismatch)
    	Backported from mainline
    	2012-10-23  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/54988
    	* decl2.c (cplus_decl_attributes): Don't return early
    	if attributes is NULL.
    
    	* c-c++-common/pr54988.c: New test.
    
    From-SVN: r193165

--- gcc/cp/decl2.c
+++ gcc/cp/decl2.c
@@ -1302,8 +1302,7 @@ void
 cplus_decl_attributes (tree *decl, tree attributes, int flags)
 {
   if (*decl == NULL_TREE || *decl == void_type_node
-      || *decl == error_mark_node
-      || attributes == NULL_TREE)
+      || *decl == error_mark_node)
     return;
 
   if (processing_template_decl)
@@ -1312,8 +1311,6 @@ cplus_decl_attributes (tree *decl, tree attributes, int flags)
 	return;
 
       save_template_attributes (&attributes, decl);
-      if (attributes == NULL_TREE)
-	return;
     }
 
   cp_check_const_attributes (attributes);
