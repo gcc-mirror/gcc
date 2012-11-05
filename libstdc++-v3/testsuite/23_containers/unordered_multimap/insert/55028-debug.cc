6a2dcb1ff09cc7cdb473d4f864413da155ef480c
bkoz <bkoz@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-11-05 21:01:08 +0000

    2012-11-05  Benjamin Kosnik  <bkoz@redhat.com>             Oleg Smolsky  <oleg@smolsky.net>
    	PR libstdc++/55028
    	*  config/abi/pre/gnu-versioned-namespace.ver: Add symbols.
    	* testsuite/23_containers/unordered_multimap/insert/55028-debug.cc: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@193185 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/23_containers/unordered_multimap/insert/55028-debug.cc
@@ -0,0 +1,40 @@
+// { dg-options "-std=gnu++0x -D_GLIBCXX_DEBUG" }
+//
+// Copyright (C) 2012 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// libstdc++/55028
+#include <string>
+#include <unordered_map>
+#include <testsuite_hooks.h>
+
+struct MyType
+{ };
+
+void test()
+{
+  // using MyMap = std::multimap<std::string, MyType *>; // works
+  using MyMap = std::unordered_multimap<std::string, MyType*>; // fails to link
+  MyMap m;
+  m.insert(std::make_pair(std::string("blah"), new MyType));
+}
+
+int main()
+{
+  test();
+  return 0;
+}
