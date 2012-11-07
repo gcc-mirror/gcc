e9f958c283c24e3b93808850ee1f29d80765ba34
Jack Howarth <howarth@bromo.med.uc.edu>
2012-11-07 23:12:27 +0000

    Use -Wl,-undefined,dynamic_lookup on darwin
    	* testsuite/libgomp.c++/pr24455.C: Use
    	-Wl,-undefined,dynamic_lookup on darwin.
    
    From-SVN: r193318

--- libgomp/testsuite/libgomp.c++/pr24455.C
+++ libgomp/testsuite/libgomp.c++/pr24455.C
@@ -2,6 +2,7 @@
 // { dg-additional-sources pr24455-1.C }
 // { dg-require-effective-target tls_runtime }
 // { dg-options "-Wl,-G" { target powerpc-ibm-aix* } }
+// { dg-options "-Wl,-undefined,dynamic_lookup" { target *-*-darwin* } }
 
 extern "C" void abort (void);
 
