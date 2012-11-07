d720a22b6e192786811365dc76824c6242e5f16d
Jonathan Wakely <jwakely.gcc@gmail.com>
2012-11-07 09:17:42 +0000

    stl_vector.h (vector(size_type)): Add missing allocator parameter.
    	* include/bits/stl_vector.h (vector(size_type)): Add missing allocator
    	parameter.
    	* include/bits/stl_bvector.h: Likewise.
    	* include/debug/vector (vector(size_type)): Likewise.
    	* include/profile/vector (vector(size_type)): Likewise. Pass allocator
    	to base constructor.
    	* testsuite/23_containers/vector/requirements/dr438/assign_neg.cc:
    	Adjust dg-error line numbers.
    	* testsuite/23_containers/vector/requirements/dr438/
    	constructor_1_neg.cc: Likewise.
    	* testsuite/23_containers/vector/requirements/dr438/
    	constructor_2_neg.cc: Likewise.
    	* testsuite/23_containers/vector/requirements/dr438/insert_neg.cc:
    	Likewise.
    
    From-SVN: r193284

--- libstdc++-v3/testsuite/23_containers/vector/requirements/dr438/insert_neg.cc
+++ libstdc++-v3/testsuite/23_containers/vector/requirements/dr438/insert_neg.cc
@@ -18,7 +18,7 @@
 // <http://www.gnu.org/licenses/>.
 
 // { dg-do compile }
-// { dg-error "no matching" "" { target *-*-* } 1277 }
+// { dg-error "no matching" "" { target *-*-* } 1278 }
 
 #include <vector>
 
