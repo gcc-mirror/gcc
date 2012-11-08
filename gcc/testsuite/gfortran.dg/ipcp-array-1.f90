85bab85fbbfceb6928ea43a3d01d33b181589e17
jamborm <jamborm@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-11-08 16:41:06 +0000

    2012-11-08  Martin Jambor  <mjambor@suse.cz>
    	* ipa-prop.c (determine_known_aggregate_parts): Skip writes to
    	different declarations when tracking writes to a declaration.
    
    	* gfortran.dg/ipcp-array-1.f90: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@193330 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/ipcp-array-1.f90
@@ -0,0 +1,19 @@
+! { dg-do compile }
+! { dg-options "-O2 -fdump-ipa-cp-details -fno-inline -fdump-tree-optimized" }
+
+subroutine bar (a, b, n)
+  integer :: a(n), b(n)
+  call foo (a, b)
+contains
+subroutine foo (a, b)
+  integer :: a(:), b(:)
+  a = b
+end subroutine
+end
+
+! { dg-final { scan-ipa-dump "Creating a specialized node of foo" "cp" } }
+! { dg-final { scan-ipa-dump-times "Aggregate replacements\[^=\]*=\[^=\]*=\[^=\]*=\[^=\]*=\[^=\]*=\[^=\]*=\[^=\]*=\[^=\]*=" 2 "cp" } }
+! { dg-final { cleanup-ipa-dump "cp" } }
+! { dg-final { scan-tree-dump-not "stride;" "optimized" } }
+! { dg-final { scan-tree-dump-not "lbound;" "optimized" } }
+! { dg-final { cleanup-tree-dump "optimized" } }
