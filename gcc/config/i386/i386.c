d529eb7eb8aece9ddcbf2ca477d92153d139f711
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-11-09 09:10:04 +0000

    * config/i386/i386.c (release_scratch_register_on_entry): Also adjust 	sp_offset manually.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@193352 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -9462,6 +9462,7 @@ release_scratch_register_on_entry (struct scratch_reg *sr)
 {
   if (sr->saved)
     {
+      struct machine_function *m = cfun->machine;
       rtx x, insn = emit_insn (gen_pop (sr->reg));
 
       /* The RTX_FRAME_RELATED_P mechanism doesn't know about pop.  */
@@ -9469,6 +9470,7 @@ release_scratch_register_on_entry (struct scratch_reg *sr)
       x = gen_rtx_PLUS (Pmode, stack_pointer_rtx, GEN_INT (UNITS_PER_WORD));
       x = gen_rtx_SET (VOIDmode, stack_pointer_rtx, x);
       add_reg_note (insn, REG_FRAME_RELATED_EXPR, x);
+      m->fs.sp_offset -= UNITS_PER_WORD;
     }
 }
 
