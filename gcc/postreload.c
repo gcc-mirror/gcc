9c5813f5422e0040e16e766400ff95b0f2aea716
steven <steven@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-11-11 20:29:11 +0000

    gcc/ 	PR middle-end/55263 	* postreload.c (rest_of_handle_postreload): With non-call exceptions, 	if edges are purged call cleanup_cfg to remove unreachable blocks.
    testsuite/
    	PR middle-end/55263
    	* g++.dg/pr55263.C: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@193412 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/postreload.c
+++ gcc/postreload.c
@@ -2289,8 +2289,9 @@ rest_of_handle_postreload (void)
   reload_cse_regs (get_insns ());
   /* Reload_cse_regs can eliminate potentially-trapping MEMs.
      Remove any EH edges associated with them.  */
-  if (cfun->can_throw_non_call_exceptions)
-    purge_all_dead_edges ();
+  if (cfun->can_throw_non_call_exceptions
+      && purge_all_dead_edges ())
+    cleanup_cfg (0);
 
   return 0;
 }
