c25639b1a3aaf288a1fb5b6b6921defa7e5dfa4a
Jonathan Wakely <jwakely.gcc@gmail.com>
2012-11-15 01:38:17 +0000

    re PR libstdc++/53841 ([C++11] condition_variable::wait_until() fails with high resolution clocks)
    	PR libstdc++/53841
    	* include/std/condition_variable (condition_variable::wait_until):
    	Handle clocks with higher resolution than __clock_t.
    	(condition_variable::__wait_until_impl): Remove unnecessary _Clock
    	parameter.
    	* testsuite/30_threads/condition_variable/members/53841.cc: New.
    
    From-SVN: r193523

---
+++ libstdc++-v3/testsuite/30_threads/condition_variable/members/53841.cc
@@ -0,0 +1,50 @@
+// { dg-do compile }
+// { dg-options " -std=gnu++0x -pthread" { target *-*-freebsd* *-*-netbsd* *-*-linux* powerpc-ibm-aix* } }
+// { dg-options " -std=gnu++0x -pthreads" { target *-*-solaris* } }
+// { dg-options " -std=gnu++0x " { target *-*-cygwin *-*-darwin* } }
+// { dg-require-cstdint "" }
+// { dg-require-gthreads "" }
+
+// Copyright (C) 2012 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// PR libstdc++/53841
+
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+namespace ch = std::chrono;
+
+struct FPClock : ch::system_clock
+{
+    typedef double rep;
+    typedef std::ratio<1> period;
+    typedef ch::duration<rep, period> duration;
+    typedef ch::time_point<FPClock> time_point;
+
+    static time_point now()
+    { return time_point(duration(system_clock::now().time_since_epoch())); }
+};
+
+void f()
+{
+    std::mutex mx;
+    std::unique_lock<std::mutex> l(mx);
+    std::condition_variable cv;
+    cv.wait_until(l, FPClock::now());
+}
