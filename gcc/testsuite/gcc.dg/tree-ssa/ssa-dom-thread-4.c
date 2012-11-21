7224cf54b3af2b931fb83af65f9cfab5c1df814a
amker <amker@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-11-21 03:44:21 +0000

    * config/arm/arm-cores.def (cortex-m1, cortex-m0) 	(cortex-m0plus): Use v6m. 	* config/arm/arm-protos.h (tune_params): Add 	logical_op_non_short_circuit. 	* config/arm/arm.c (arm_slowmul_tune, arm_fastmul_tune) 	(arm_strongarm_tune, arm_xscale_tune, arm_9e_tune, arm_v6t2_tune) 	(arm_cortex_tune, arm_cortex_a15_tune, arm_cortex_a5_tune) 	(arm_cortex_a9_tune, arm_fa726te_tune): Set 	logical_op_non_short_circuit field. 	(arm_v6m_tune): New tune_params struct. 	* config/arm/arm.h (LOGICAL_OP_NON_SHORT_CIRCUIT): Define.
    	* gcc.dg/tree-ssa/ssa-dom-thread-4.c: Skip on ARM Cortex-M0.
    	* gcc.dg/tree-ssa/vrp47.c: Ditto.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@193687 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/ssa-dom-thread-4.c
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-dom-thread-4.c
@@ -59,7 +59,9 @@ bitmap_ior_and_compl (bitmap dst, const_bitmap a, const_bitmap b,
    code we missed the edge when the first conditional is false
    (b_elt is zero, which means the second conditional is always
    zero.  */
-/* { dg-final { scan-tree-dump-times "Threaded" 3 "dom1" { target { ! mips*-*-* } } } } */
+/* ARM Cortex-M0 defined LOGICAL_OP_NON_SHORT_CIRCUIT to false,
+   so skip below test.  */
+/* { dg-final { scan-tree-dump-times "Threaded" 3 "dom1" { target { ! { mips*-*-* || { arm_cortex_m && arm_thumb1 } } } } } } */
 /* MIPS defines LOGICAL_OP_NON_SHORT_CIRCUIT to 0, so we split var1 || var2
    into two conditions, rather than use (var1 != 0) | (var2 != 0).  */
 /* { dg-final { scan-tree-dump-times "Threaded" 4 "dom1" { target mips*-*-* } } } */
