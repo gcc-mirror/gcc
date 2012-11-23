77bc5132136626017244cc4324c84e3210c8f4fc
Jakub Jelinek <jakub@redhat.com>
2012-11-23 10:02:28 +0100

    re PR sanitizer/55435 ([asan] implement an attribute to disable asan instrumentation for a particular function)
    	PR sanitizer/55435
    	* c-common.c (handle_no_address_safety_analysis_attribute): New
    	function.
    	(c_common_attribute_table): Add no_address_safety_analysis.
    
    	* asan.c (gate_asan): Don't instrument functions with
    	no_address_safety_analysis attribute.
    	(gate_asan_O0): Use !optimize && gate_asan ().
    	* doc/extend.texi (no_address_safety_analysis): Document new
    	function attribute.
    
    	* c-c++-common/asan/attrib-1.c: New test.
    
    From-SVN: r193748

--- gcc/c-family/c-common.c
+++ gcc/c-family/c-common.c
@@ -309,6 +309,8 @@ static tree handle_common_attribute (tree *, tree, tree, int, bool *);
 static tree handle_noreturn_attribute (tree *, tree, tree, int, bool *);
 static tree handle_hot_attribute (tree *, tree, tree, int, bool *);
 static tree handle_cold_attribute (tree *, tree, tree, int, bool *);
+static tree handle_no_address_safety_analysis_attribute (tree *, tree, tree,
+							 int, bool *);
 static tree handle_noinline_attribute (tree *, tree, tree, int, bool *);
 static tree handle_noclone_attribute (tree *, tree, tree, int, bool *);
 static tree handle_leaf_attribute (tree *, tree, tree, int, bool *);
@@ -711,6 +713,10 @@ const struct attribute_spec c_common_attribute_table[] =
 			      handle_cold_attribute, false },
   { "hot",                    0, 0, true,  false, false,
 			      handle_hot_attribute, false },
+  { "no_address_safety_analysis",
+			      0, 0, true, false, false,
+			      handle_no_address_safety_analysis_attribute,
+			      false },
   { "warning",		      1, 1, true,  false, false,
 			      handle_error_attribute, false },
   { "error",		      1, 1, true,  false, false,
@@ -6474,6 +6480,22 @@ handle_cold_attribute (tree *node, tree name, tree ARG_UNUSED (args),
 	 lookup_attribute.  */
     }
   else
+    {
+      warning (OPT_Wattributes, "%qE attribute ignored", name);
+      *no_add_attrs = true;
+    }
+
+  return NULL_TREE;
+}
+
+/* Handle a "no_address_safety_analysis" attribute; arguments as in
+   struct attribute_spec.handler.  */
+
+static tree
+handle_no_address_safety_analysis_attribute (tree *node, tree name, tree, int,
+					     bool *no_add_attrs)
+{
+  if (TREE_CODE (*node) != FUNCTION_DECL)
     {
       warning (OPT_Wattributes, "%qE attribute ignored", name);
       *no_add_attrs = true;
