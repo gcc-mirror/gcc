45d5889a070819486a619ca2a2664c186537cb40
Tobias Burnus <burnus@net-b.de>
2012-11-29 09:43:48 +0100

    re PR fortran/52161 (Internal compiler errors with -fcheck=bounds in coarray tests)
    2012-11-28  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/52161
            * trans-stmt.c (gfc_trans_sync): Fix bound checking
            for -fcoarray=lib.
    
    2012-11-28  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/52161
            * coarray/sync_3.f90: Extend test.
    
    From-SVN: r193924

--- gcc/fortran/trans-stmt.c
+++ gcc/fortran/trans-stmt.c
@@ -784,12 +784,12 @@ gfc_trans_sync (gfc_code *code, gfc_exec_op type)
       else
 	{
 	  tree cond2;
-	  cond = fold_build2_loc (input_location, GE_EXPR, boolean_type_node,
+	  cond = fold_build2_loc (input_location, GT_EXPR, boolean_type_node,
 				  images, gfort_gvar_caf_num_images);
 	  cond2 = fold_build2_loc (input_location, LT_EXPR, boolean_type_node,
 				   images,
 				   build_int_cst (TREE_TYPE (images), 1));
-	  cond = fold_build2_loc (input_location, TRUTH_AND_EXPR,
+	  cond = fold_build2_loc (input_location, TRUTH_OR_EXPR,
 				  boolean_type_node, cond, cond2);
 	}
       gfc_trans_runtime_check (true, false, cond, &se.pre,
