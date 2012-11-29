bc7d0e9006266cf3e0963e7311cf3ba9db56e3d5
Jason Merrill <jason@redhat.com>
2012-11-29 15:12:58 -0500

    re PR c++/53137 (g++ segfault)
    	PR c++/53137
    	* pt.c (tsubst_expr) [DECL_EXPR]: Set LAMBDA_EXPR_THIS_CAPTURE here.
    	(tsubst_copy_and_build) [LAMBDA_EXPR]: And clear it here.
    	(instantiate_class_template_1): Not here.
    
    From-SVN: r193954

---
+++ gcc/testsuite/g++.dg/cpp0x/lambda/lambda-this6.C
@@ -0,0 +1,32 @@
+// PR c++/53137
+// { dg-options -std=c++11 }
+
+template <typename STORE>
+void getParent(STORE& tStore)
+{
+}
+
+struct  Store
+{
+    template <typename CheckParentFunc>
+    void updateChildCommon(CheckParentFunc c)
+    {
+        c();
+    }
+
+    template <typename T>
+    int& getStore();
+
+    template <typename T>
+    void updateChild(const T& obj)
+    {
+        updateChildCommon([this] () { getParent(getStore<T>()); });
+    }
+
+    void update(int obj);
+};
+
+void Store::update(int obj)
+{
+    updateChild(obj);
+}
