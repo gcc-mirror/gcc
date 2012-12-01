4d382327d5f1ec35b8f5c58d0c3a924ae09bda22
Alessandro Fanfarillo <alessandro.fanfarillo@gmail.com>
2012-12-01 08:00:22 +0000

    re PR fortran/46897 ([OOP] type-bound defined ASSIGNMENT(=) not used for derived type component in intrinsic assign)
    2012-12-01   Alessandro Fanfarillo <alessandro.fanfarillo@gmail.com>
                 Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/46897
    	* gfortran.h : Add bit field 'defined_assign_comp' to
    	symbol_attribute structure.
    	Add primitive for gfc_add_full_array_ref.
    	* expr.c (gfc_add_full_array_ref): New function.
    	(gfc_lval_expr_from_sym): Call new function.
    	* resolve.c (add_comp_ref): New function.
    	(build_assignment): New function.
    	(get_temp_from_expr): New function
    	(add_code_to_chain): New function
    	(generate_component_assignments): New function that calls all
    	the above new functions.
    	(resolve_code): Call generate_component_assignments.
    	(check_defined_assignments): New function.
    	(resolve_fl_derived0): Call check_defined_assignments.
    	(gfc_resolve): Reset component_assignment_level in case it is
    	left in a bad state by errors.
    
    
    	* resolve.c (is_sym_host_assoc, resolve_procedure_interface,
    	resolve_contained_fntype, resolve_procedure_expression,
    	resolve_elemental_actual, resolve_global_procedure,
    	is_scalar_expr_ptr, gfc_iso_c_func_interface, resolve_function,
    	set_name_and_label, gfc_iso_c_sub_interface,
    	resolve_specific_s0, resolve_operator, compare_bound_mpz_t,
    	gfc_resolve_character_operator, resolve_typebound_function,
    	gfc_resolve_expr, forall_index, remove_last_array_ref,
    	conformable_arrays, resolve_allocate_expr,
    	resolve_allocate_deallocate, resolve_select_type,
    	resolve_transfer, resolve_where,
    	gfc_resolve_where_code_in_forall, gfc_resolve_forall_body,
    	gfc_count_forall_iterators, resolve_values,
    	resolve_bind_c_comms, resolve_bind_c_derived_types,
    	gfc_verify_binding_labels, apply_default_init,
    	build_default_init_expr, apply_default_init_local,
    	resolve_fl_var_and_proc, resolve_fl_procedure,
    	gfc_resolve_finalizers, check_generic_tbp_ambiguity,
    	resolve_typebound_intrinsic_op, resolve_typebound_procedure,
    	resolve_typebound_procedures, ensure_not_abstract,
    	resolve_fl_derived0, resolve_fl_parameter, resolve_symbol,
    	resolve_equivalence_derived): Remove trailing white space.
    	* gfortran.h : Remove trailing white space.
    
    2012-12-01   Alessandro Fanfarillo <alessandro.fanfarillo@gmail.com>
                 Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/46897
    	* gfortran.dg/defined_assignment_1.f90: New test.
    	* gfortran.dg/defined_assignment_2.f90: New test.
    	* gfortran.dg/defined_assignment_3.f90: New test.
    	* gfortran.dg/defined_assignment_4.f90: New test.
    	* gfortran.dg/defined_assignment_5.f90: New test.
    
    
    Co-Authored-By: Paul Thomas <pault@gcc.gnu.org>
    
    From-SVN: r194016

---
+++ gcc/testsuite/gfortran.dg/defined_assignment_5.f90
@@ -0,0 +1,76 @@
+! { dg-do run }
+! Further test of typebound defined assignment
+!
+module m0
+  implicit none
+  type component
+    integer :: i = 0
+  contains
+    procedure :: assign0
+    generic :: assignment(=)=>assign0
+  end type
+  type parent
+    type(component) :: foo(2)
+  end type
+  type, extends(parent) :: child
+    integer :: j
+  end type
+contains
+  elemental subroutine assign0(lhs,rhs)
+    class(component), intent(INout) :: lhs
+    class(component), intent(in) :: rhs
+    lhs%i = 20
+  end subroutine
+end module
+
+module m1
+  implicit none
+  type component1
+    integer :: i = 0
+  contains
+    procedure :: assign1
+    generic :: assignment(=)=>assign1
+  end type
+  type parent1
+    type(component1) :: foo
+  end type
+  type, extends(parent1) :: child1
+    integer :: j = 7
+  end type
+contains
+  elemental subroutine assign1(lhs,rhs)
+    class(component1), intent(out) :: lhs
+    class(component1), intent(in) :: rhs
+    lhs%i = 30
+  end subroutine
+end module
+
+
+program main
+  use m0
+  use m1
+  implicit none
+  type(child) :: infant(2)
+  type(parent) :: dad, mum
+  type(child1) :: orphan(5)
+  type(child1), allocatable :: annie(:)
+  integer :: i, j, k
+
+  dad = parent ([component (3), component (4)])
+  mum = parent ([component (5), component (6)])
+  infant = [child(dad, 999), child(mum, 9999)]  ! { dg-warning "multiple part array references" }
+
+! Check that array sections are OK
+  i = 3
+  j = 4
+  orphan(i:j) = child1(component1(777), 1)
+  if (any (orphan%parent1%foo%i .ne. [0,0,30,30,0])) call abort
+  if (any (orphan%j .ne. [7,7,1,1,7])) call abort
+
+! Check that allocatable lhs's work OK.
+  annie = [(child1(component1(k), 2*k), k = 1,3)]
+  if (any (annie%parent1%foo%i .ne. [30,30,30])) call abort
+  if (any (annie%j .ne. [2,4,6])) call abort
+end
+
+
