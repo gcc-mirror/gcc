2ce8ac4bb25f28242da128a8d60df3ae90dab820
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-12-01 19:43:04 +0000

    PR ada/52110 	* s-osinte-hpux-dce.ads: Declare pthread_rwlockattr_t and 	pthread_rwlock_t subtypes.  Delete duplicate declaration of clockid_t. 	* s-taspri-hpux-dce.ads: Change "pragma Atomic (Thread)" to comment.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@194024 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/s-osinte-hpux-dce.ads
+++ gcc/ada/s-osinte-hpux-dce.ads
@@ -244,6 +244,14 @@ package System.OS_Interface is
    type pthread_condattr_t  is limited private;
    type pthread_key_t       is private;
 
+   --  Read/Write lock not supported on HPUX. To add support both types
+   --  pthread_rwlock_t and pthread_rwlockattr_t must properly be defined
+   --  with the associated routines pthread_rwlock_[init/destroy] and
+   --  pthread_rwlock_[rdlock/wrlock/unlock].
+
+   subtype pthread_rwlock_t     is pthread_mutex_t;
+   subtype pthread_rwlockattr_t is pthread_mutexattr_t;
+
    -----------
    -- Stack --
    -----------
@@ -444,7 +452,6 @@ private
    end record;
    pragma Convention (C, timespec);
 
-   type clockid_t is new int;
    CLOCK_REALTIME : constant clockid_t := 1;
 
    type cma_t_address is new System.Address;
