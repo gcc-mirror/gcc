0ed3d24e395cb42ef1db6a73c60f69cca51238cc
Aldy Hernandez <aldyh@redhat.com>
2012-12-03 16:11:21 +0000

    re PR middle-end/55401 (uninstrumented path in TM clones are still instrumented)
    	PR middle-end/55401
    	* trans-mem.c (get_tm_region_blocks): Exclude uninstrumented
    	blocks from vector if requested.
    	(collect_bb2reg): Pass new argument to
    	get_tm_region_blocks.
    	(get_bb_regions_instrumented): Add INCLUDE_UNINSTRUMENTED_P
    	argument, and pass it to expand_regions.
    	(execute_tm_mark): Pass new argument to
    	get_bb_regions_instrumented.
    	(execute_tm_edges): Same.
    
    From-SVN: r194099

---
+++ gcc/testsuite/gcc.dg/tm/pr55401.c
@@ -0,0 +1,22 @@
+/* { dg-do compile } */
+/* { dg-options "-fgnu-tm -O0 -fdump-tree-optimized" } */
+
+int george;
+int ringo;
+
+__attribute__((transaction_callable))
+void foo()
+{
+  ringo=666;
+  __transaction_atomic {
+      george=999;
+  }
+}
+
+/* There should only be 2 instrumented writes to GEORGE: one in FOO,
+   and one in the transactional clone to FOO.  There should NOT be
+   more than one instrumented write to GEORGE in the clone of
+   FOO.  */
+/* { dg-final { scan-tree-dump-times "ITM_WU\[0-9\] \\(&george," 2 "optimized" } } */
+
+/* { dg-final { cleanup-tree-dump "optimized" } } */
