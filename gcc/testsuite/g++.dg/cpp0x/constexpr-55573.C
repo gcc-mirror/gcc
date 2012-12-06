02e4455f8abc65d3bed025ea768cbd1c6cd24ad6
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-12-06 18:54:25 +0000

    PR c++/55573 	* semantics.c (adjust_temp_type): Handle VECTOR_CST.
    	* g++.dg/cpp0x/constexpr-55573.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@194262 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-55573.C
@@ -0,0 +1,22 @@
+// PR c++/55573
+// { dg-do compile }
+// { dg-options "-std=gnu++11" }
+
+template <typename T, int N>
+struct ExtVecTraits {
+  typedef T __attribute__((vector_size (N * sizeof (T)))) type;
+};
+
+template <typename T>
+using Vec4 = typename ExtVecTraits<T,4>::type;
+
+template <typename T>
+struct Rot3
+{
+  typedef Vec4<T> Vec;
+  Vec axis[3];
+  constexpr Rot3 (Vec4<T> ix, Vec4<T> iy, Vec4<T> iz) : axis {ix, iy, iz} {}
+};
+
+typedef Vec4<float> Vec;
+Rot3<float> r2 ((Vec) {0, 1, 0, 0}, (Vec){0, 0, 1, 0}, (Vec){1, 0, 0, 0});
