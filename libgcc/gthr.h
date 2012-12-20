5aa12dbf6c3613e26445926b511afd336fc79f7b
Jonathan Wakely <jwakely.gcc@gmail.com>
2012-12-20 22:19:57 +0000

    gthr.h (__gthread_cond_timedwait_recursive): Do not require.
    	* gthr.h (__gthread_cond_timedwait_recursive): Do not require.
    	* gthr-posix.h (__gthread_cond_timedwait_recursive): Remove.
    
    From-SVN: r194652

--- libgcc/gthr.h
+++ libgcc/gthr.h
@@ -129,9 +129,6 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
      int __gthread_cond_timedwait (__gthread_cond_t *cond,
                                    __gthread_mutex_t *mutex,
                                    const __gthread_time_t *abs_timeout);
-     int __gthread_cond_timedwait_recursive (__gthread_cond_t *cond,
-                                             __gthread_recursive_mutex_t *mutex,
-                                             const __gthread_time_t *abs_time)
 
 */
 
