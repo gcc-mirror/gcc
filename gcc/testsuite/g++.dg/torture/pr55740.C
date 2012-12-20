63f2ff0f23d633aad769997ec617ce2177c311ec
Richard Biener <rguenther@suse.de>
2012-12-20 12:45:48 +0000

    re PR rtl-optimization/55740 (ICE in verify_loop_structure, at cfgloop.c:1582, error: loop 2's header does not belong directly to it)
    2012-12-20  Richard Biener  <rguenther@suse.de>
    
    	PR middle-end/55740
    	* cfghooks.c (merge_blocks): Properly handle merging of
    	two loop headers.
    
    	* g++.dg/torture/pr55740.C: New testcase.
    
    From-SVN: r194633

---
+++ gcc/testsuite/g++.dg/torture/pr55740.C
@@ -0,0 +1,19 @@
+// { dg-do compile }
+
+static bool st_IsPathDelimiter( char c ) { return c == '/'; }
+bool IsValidPath( char const * filename )
+{
+  if ( !filename || filename[0] == 0 )     
+    return false;
+  char const * run = filename;
+  while ( run && *run )       
+    {
+      if ( run[0] == '.' )   
+	if ( run[1] != '.' || ( !st_IsPathDelimiter( run[2] ) && run[2] != 0 ) )   
+	  return false;   
+      while ( *run && !st_IsPathDelimiter( *run ) )
+	++run;
+      if ( *run ) 
+	++run;
+    }
+}
