bcbfcebe6bb072af17f03a02c90f2017db4165d1
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-12-21 21:38:11 +0000

    * rtlanal.c (volatile_insn_p): Delete commented out code. 	(side_effects_p): Likewise. 	(may_trap_p_1) <UNSPEC_VOLATILE>: Return 1 again. 	* target.def (unspec_may_trap_p): Adjust comment. 	* targhooks.c (default_unspec_may_trap_p): Don't handle UNSPEC_VOLATILE. 	* config/ia64/ia64.c (ia64_unspec_may_trap_p): Adjust to above change.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@194681 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/targhooks.c
+++ gcc/targhooks.c
@@ -102,10 +102,8 @@ default_unspec_may_trap_p (const_rtx x, unsigned flags)
 {
   int i;
 
-  if (GET_CODE (x) == UNSPEC_VOLATILE
-      /* Any floating arithmetic may trap.  */
-      || (SCALAR_FLOAT_MODE_P (GET_MODE (x))
-	  && flag_trapping_math))
+  /* Any floating arithmetic may trap.  */
+  if ((SCALAR_FLOAT_MODE_P (GET_MODE (x)) && flag_trapping_math))
     return 1;
 
   for (i = 0; i < XVECLEN (x, 0); ++i)
