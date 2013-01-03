90bb7d7ab2ccd6b7c9c311867e319057b402a075
Richard Biener <rguenther@suse.de>
2013-01-03 12:34:34 +0000

    re PR tree-optimization/55857 (ICE: verify_ssa failed)
    2013-01-03  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/55857
    	* tree-vect-stmts.c (vectorizable_load): Do not setup
    	re-alignment for invariant loads.
    
    	* gcc.dg/vect/pr55857-1.c: New testcase.
    	* gcc.dg/vect/pr55857-2.c: Likewise.
    
    From-SVN: r194850

---
+++ gcc/testsuite/gcc.dg/vect/pr55857-1.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */
+
+void 
+foo (int *data, unsigned len, const int qlp_coeff[], int lp, int residual[])
+{
+  int i;
+  int sum;
+  for(i = 0; i < (int)len; i++)
+    {     
+      sum = 0;   
+      sum += qlp_coeff[1] * data[i-2];   
+      sum += qlp_coeff[0] * data[i-1];   
+      residual[i] = data[i] - (sum >> lp);
+    }    
+}
+
+/* { dg-final { cleanup-tree-dump "vect" } } */
