5b42fd01389e9054ba20f7ef574aaeab1beaf677
Jakub Jelinek <jakub@redhat.com>
2013-01-08 09:14:04 +0100

    re PR sanitizer/55844 (-fsanitize=address -Os -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer -m64 doesn't work)
    	PR sanitizer/55844
    	* c-c++-common/asan/null-deref-1.c: Add -fno-shrink-wrap to
    	dg-options.
    
    From-SVN: r195005

--- gcc/testsuite/c-c++-common/asan/null-deref-1.c
+++ gcc/testsuite/c-c++-common/asan/null-deref-1.c
@@ -1,5 +1,5 @@
 /* { dg-do run } */
-/* { dg-options "-fno-omit-frame-pointer" } */
+/* { dg-options "-fno-omit-frame-pointer -fno-shrink-wrap" } */
 /* { dg-additional-options "-mno-omit-leaf-frame-pointer" { target { i?86-*-* x86_64-*-* } } } */
 /* { dg-shouldfail "asan" } */
 
