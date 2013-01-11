a1583ea9123cbdbc693976feab678086fbfb82c4
schwab <schwab@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-01-11 17:08:51 +0000

    * gcc.c-torture/compile/pr55921.c: Don't use matching constraints.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@195112 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/compile/pr55921.c
+++ gcc/testsuite/gcc.c-torture/compile/pr55921.c
@@ -14,8 +14,8 @@ foo (ucf *in, ucf *out, _Complex float r)
   _Complex float cf;
 
   ucf1.ll = in[i].ll;
-  __asm ("" : "=r" (cf) : "0" (ucf1.ll));
+  __asm ("" : "=r" (cf) : "r" (ucf1.ll));
   cf *= r;
-  __asm ("" : "=r" (ucf1.ll) : "0" (cf));
+  __asm ("" : "=r" (ucf1.ll) : "r" (cf));
   out[i].ll = ucf1.ll;
 }
