8129609c6ed6298bd1e4e3bbd2be9e6ae049a0a4
Richard Sandiford <rdsandiford@googlemail.com>
2013-01-14 18:18:49 +0000

    Update copyright years in libgomp.
    From-SVN: r195166

--- libgomp/config/linux/proc.c
+++ libgomp/config/linux/proc.c
@@ -1,5 +1,4 @@
-/* Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010
-   Free Software Foundation, Inc.
+/* Copyright (C) 2005-2013 Free Software Foundation, Inc.
    Contributed by Jakub Jelinek <jakub@redhat.com>.
 
    This file is part of the GNU OpenMP Library (libgomp).
--- libgomp/libgomp_f.h.in
+++ libgomp/libgomp_f.h.in
@@ -1,4 +1,4 @@
-/* Copyright (C) 2005, 2008, 2009 Free Software Foundation, Inc.
+/* Copyright (C) 2005-2013 Free Software Foundation, Inc.
    Contributed by Jakub Jelinek <jakub@redhat.com>.
 
    This file is part of the GNU OpenMP Library (libgomp).
