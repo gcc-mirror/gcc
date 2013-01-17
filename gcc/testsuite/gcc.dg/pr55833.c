c1f9696864b1b8ab20a6798751eec48986a2e98a
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-01-17 19:19:37 +0000

    Fix PR55833.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@195280 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr55833.c
@@ -0,0 +1,28 @@
+/* PR rtl-optimization/55833 */
+/* { dg-do compile } */
+/* { dg-options "-O3" } */
+
+int a, b, c;
+
+void foo()
+{
+    unsigned d, l, *p, k = 1;
+
+    if(bar())
+    {
+label:
+      	if((a = a <= 0))
+        {
+            if(c)
+                d = b;
+
+            if (b || d ? l : k ? : 0)
+                a = d = 0;
+
+            goto label;
+       	}
+    }
+
+    while(*p++)
+        goto label;
+}
