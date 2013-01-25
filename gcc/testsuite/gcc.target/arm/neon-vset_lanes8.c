92777c626aeb006e03a593e221fb37850447eed6
Janis Johnson <janisjo@codesourcery.com>
2013-01-25 00:22:02 +0000

    backport: neon-vset_lanes8.c: Fix for big-endian support.
    	Backport from mainline
    	2012-09-11  Christophe Lyon  <christophe.lyon@linaro.org>
    
            * gcc.target/arm/neon-vset_lanes8.c: Fix for big-endian support.
    
    From-SVN: r195452

--- gcc/testsuite/gcc.target/arm/neon-vset_lanes8.c
+++ gcc/testsuite/gcc.target/arm/neon-vset_lanes8.c
@@ -9,11 +9,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-int8x8_t x = { 1, 2, 3, 4, 5, 6, 7, 8 };
-int8x8_t y = { 1, 2, 3, 16, 5, 6, 7, 8 };
+int8_t x_init[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+int8_t y_init[8] = { 1, 2, 3, 16, 5, 6, 7, 8 };
 
 int main (void)
 {
+  int8x8_t x = vld1_s8 (x_init);
+  int8x8_t y = vld1_s8 (y_init);
+
   x = vset_lane_s8 (16, x, 3);
   if (memcmp (&x, &y, sizeof (x)) != 0)
     abort();
