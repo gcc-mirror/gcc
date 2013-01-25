2027f85fdf5042e09b16fb9a2aad110ae188ad8e
Janis Johnson <janisjo@codesourcery.com>
2013-01-25 00:23:05 +0000

    Backport from mainline 	2012-09-26  Janis Johnson  <janisjo@codesourcery.com>
    	* gcc.target/arm/combine-movs.c
    
    From-SVN: r195453

--- gcc/testsuite/gcc.target/arm/combine-movs.c
+++ gcc/testsuite/gcc.target/arm/combine-movs.c
@@ -9,4 +9,5 @@ void foo (unsigned long r[], unsigned int d)
     r[i] = 0;
 }
 
-/* { dg-final { scan-assembler "movs\tr\[0-9\]" } } */
+/* { dg-final { scan-assembler "lsrs\tr\[0-9\]" { target arm_thumb2 } } } */
+/* { dg-final { scan-assembler "movs\tr\[0-9\]" { target { ! arm_thumb2 } } } } */
