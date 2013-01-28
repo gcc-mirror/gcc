5a579c3b65d5571b3d10d2b7e22d4c1915cc6b1c
Leif Ekblad <leif@rdos.net>
2013-01-28 20:42:55 +0000

    config.gcc (i[34567]86-*-rdos*, [...]): New targets.
    	* config.gcc (i[34567]86-*-rdos*, x86_64-*-rdos*): New targets.
    	* config/i386/i386.h (TARGET_RDOS): New macro.
    	(DEFAULT_LARGE_SECTION_THRESHOLD): New macro.
    	* config/i386/i386.c (ix86_option_override_internal): For 64bit
    	TARGET_RDOS, set ix86_cmodel to CM_MEDIUM_PIC and flag_pic to 1.
    	* config/i386/i386.opt (mlarge-data-threshold): Initialize to
    	DEFAULT_LARGE_SECTION_THRESHOLD.
    	* config/i386/i386.md (R14_REG, R15_REG): New constants.
    	* config/i386/rdos.h: New file.
    	* config/i386/rdos64.h: New file.
    
    From-SVN: r195516

---
+++ gcc/config/i386/rdos64.h
@@ -0,0 +1,24 @@
+/* Definitions for RDOS on x86_64.
+   Copyright (C) 2013 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#undef REAL_PIC_OFFSET_TABLE_REGNUM
+#define REAL_PIC_OFFSET_TABLE_REGNUM  R15_REG
+
+#undef DEFAULT_LARGE_SECTION_THRESHOLD
+#define DEFAULT_LARGE_SECTION_THRESHOLD 16
