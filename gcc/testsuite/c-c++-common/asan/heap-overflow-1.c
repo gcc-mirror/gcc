e63f158158a16d77dd34492534e13c1a5ca27943
Jakub Jelinek <jakub@redhat.com>
2013-01-28 15:28:16 +0100

    re PR testsuite/56053 (FAIL: c-c++-common/asan/(global|stack)-overflow-1.c)
    	PR testsuite/56053
    	* c-c++-common/asan/heap-overflow-1.c: Don't include stdlib.h and
    	string.h.  Provide memset, malloc and free prototypes, adjust line
    	numbers in dg-output.
    	* c-c++-common/asan/stack-overflow-1.c: Don't include string.h.
    	Provide memset prototype and adjust line numbers in dg-output.
    	* c-c++-common/asan/global-overflow-1.c: Likewise.
    
    From-SVN: r195505

--- gcc/testsuite/c-c++-common/asan/heap-overflow-1.c
+++ gcc/testsuite/c-c++-common/asan/heap-overflow-1.c
@@ -2,8 +2,18 @@
 /* { dg-options "-fno-builtin-malloc -fno-builtin-free -fno-builtin-memset" } */
 /* { dg-shouldfail "asan" } */
 
-#include <stdlib.h>
-#include <string.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void *memset (void *, int, __SIZE_TYPE__);
+void *malloc (__SIZE_TYPE__);
+void free (void *);
+
+#ifdef __cplusplus
+}
+#endif
+
 volatile int ten = 10;
 int main(int argc, char **argv) {
   char *x = (char*)malloc(10);
@@ -14,8 +24,8 @@ int main(int argc, char **argv) {
 }
 
 /* { dg-output "READ of size 1 at 0x\[0-9a-f\]+ thread T0.*(\n|\r\n|\r)" } */
-/* { dg-output "    #0 0x\[0-9a-f\]+ (in _*main (\[^\n\r]*heap-overflow-1.c:11|\[^\n\r]*:0)|\[(\]).*(\n|\r\n|\r)" } */
+/* { dg-output "    #0 0x\[0-9a-f\]+ (in _*main (\[^\n\r]*heap-overflow-1.c:21|\[^\n\r]*:0)|\[(\]).*(\n|\r\n|\r)" } */
 /* { dg-output "0x\[0-9a-f\]+ is located 0 bytes to the right of 10-byte region\[^\n\r]*(\n|\r\n|\r)" } */
 /* { dg-output "allocated by thread T0 here:\[^\n\r]*(\n|\r\n|\r)" } */
 /* { dg-output "    #0 0x\[0-9a-f\]+ (in _*(interceptor_|)malloc|\[(\])\[^\n\r]*(\n|\r\n|\r)" } */
-/* { dg-output "    #1 0x\[0-9a-f\]+ (in _*main (\[^\n\r]*heap-overflow-1.c:9|\[^\n\r]*:0)|\[(\])\[^\n\r]*(\n|\r\n|\r)" } */
+/* { dg-output "    #1 0x\[0-9a-f\]+ (in _*main (\[^\n\r]*heap-overflow-1.c:19|\[^\n\r]*:0)|\[(\])\[^\n\r]*(\n|\r\n|\r)" } */
