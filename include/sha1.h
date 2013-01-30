d98540bbcb7ccf4c40c6df0ce1cef2c5fbb37ade
Kai Tietz <ktietz@redhat.com>
2013-01-30 17:58:10 +0100

    re PR other/54620 (sha1.c has incorrect math if sizeof(size_t) is 8)
    	PR other/54620
    	PR target/39064
    	* md5.h (md5_uintptr, md5_uint32): Define as uintptr_t/uint32_t if
    	stdint.h and sys/types.h headers are present.
    	* sha1.h (sha1_uintptr, sha1_uint32): Likewise.
    
    From-SVN: r195581

--- include/sha1.h
+++ include/sha1.h
@@ -39,6 +39,11 @@
 # include <sys/types.h>
 typedef u_int32_t sha1_uint32;
 typedef uintptr_t sha1_uintptr;
+#elif defined (HAVE_SYS_TYPES_H) && defined (HAVE_STDINT_H)
+#include <stdint.h>
+#include <sys/types.h>
+typedef uint32_t sha1_uint32;
+typedef uintptr_t sha1_uintptr;
 #else
 #  define INT_MAX_32_BITS 2147483647
 
