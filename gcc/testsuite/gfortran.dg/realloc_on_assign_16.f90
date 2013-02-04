16e247566db1df18a63965f8b3da7345459c6296
Paul Thomas <pault@gcc.gnu.org>
2013-02-04 22:33:15 +0000

    re PR fortran/56008 ([F03] wrong code with lhs-realloc on assignment with derived types having allocatable components)
    2013-02-04  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/56008
    	PR fortran/47517
    	* trans-array.c (gfc_alloc_allocatable_for_assignment): Save
    	the lhs descriptor before it is modified for reallocation. Use
    	it to deallocate allocatable components in the reallocation
    	block.  Nullify allocatable components for newly (re)allocated
    	arrays.
    
    2013-02-04  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/56008
    	* gfortran.dg/realloc_on _assign_16.f90 : New test.
    
    	PR fortran/47517
    	* gfortran.dg/realloc_on _assign_17.f90 : New test.
    
    From-SVN: r195741

---
+++ gcc/testsuite/gfortran.dg/realloc_on_assign_16.f90
@@ -0,0 +1,28 @@
+! { dg-do run }
+! Test the fix for PR56008
+!
+! Contributed by Stefan Mauerberger  <stefan.mauerberger@gmail.com>
+!
+PROGRAM main
+    !USE MPI
+
+    TYPE :: test_typ
+        REAL, ALLOCATABLE :: a(:)
+    END TYPE
+
+    TYPE(test_typ) :: xx, yy
+    TYPE(test_typ), ALLOCATABLE :: conc(:)
+
+    !CALL MPI_INIT(i)
+
+    xx = test_typ( [1.0,2.0] )
+    yy = test_typ( [4.0,4.9] )
+
+    conc = [ xx, yy ]
+
+    if (any (int (10.0*conc(1)%a) .ne. [10,20])) call abort
+    if (any (int (10.0*conc(2)%a) .ne. [40,49])) call abort
+
+    !CALL MPI_FINALIZE(i)
+
+END PROGRAM main
