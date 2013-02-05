b9b931d92908ce569db6c1dd2cb95253f1daa7af
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-02-05 09:13:48 +0000

    PR tree-optimization/55789 	* g++.dg/tree-ssa/inline-1.C: Update max-inliner-iterations. 	* g++.dg/tree-ssa/inline-2.C: Update max-inliner-iterations. 	* g++.dg/tree-ssa/inline-3.C: Update max-inliner-iterations. 	* g++.dg/ipa/inline-1.C: New testcase. 	* g++.dg/ipa/inline-2.C: New testcase. 	* g++.dg/ipa/inline-3.C: New testcase. 	* params.def (PARAM_EARLY_INLINER_MAX_ITERATIONS): Drop to 1.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@195751 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ipa/inline-1.C
+++ gcc/testsuite/g++.dg/ipa/inline-1.C
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -fdump-tree-einline --param max-early-inliner-iterations=3" } */
+/* { dg-options "-O2 -fdump-ipa-inline --param max-early-inliner-iterations=1" } */
 /* { dg-add-options bind_pic_locally } */
 
 namespace std {
@@ -7,7 +7,7 @@ namespace std {
 }
 
 template <class T, class E> void
-foreach (T b, T e, E ptr)
+foreach (T b, T e, void (*ptr)(E))
 {
   for (; b != e; b++)
     ptr(*b);
@@ -31,6 +31,6 @@ int main(int argc, char **argv)
   foreach (argv, argv + argc, inline_me_too);
 }
 
-/* { dg-final { scan-tree-dump-times "Inlining void inline_me\\(" 1 "einline"} } */
-/* { dg-final { scan-tree-dump-times "Inlining void inline_me_too\\(" 1 "einline"} } */
+/* { dg-final { scan-tree-dump-times "Considering void inline_me\\(" 1 "inline"} } */
+/* { dg-final { scan-tree-dump-times "Considering void inline_me_too\\(" 1 "inline"} } */
 /* { dg-final { cleanup-tree-dump "einline" } } */
