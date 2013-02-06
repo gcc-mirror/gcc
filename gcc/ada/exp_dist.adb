57cd943be451fa68fb17c3c2456fb46b29b39af4
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-02-06 10:05:12 +0000

    2013-02-06  Robert Dewar  <dewar@adacore.com>
    	* exp_prag.adb, sem_ch3.adb, exp_attr.adb, sem_prag.adb, sem_ch6.adb,
    	exp_intr.adb, exp_dist.adb, sem_ch13.adb: Internal clean up for
    	N_Pragma nodes.
    
    2013-02-06  Robert Dewar  <dewar@adacore.com>
    
    	* gnat_rm.texi: Minor text updates for pragma Warning.
    
    2013-02-06  Geert Bosch  <bosch@adacore.com>
    
    	* s-multip.adb (Number_Of_CPUs): Short-circuit in case of
    	CPU'Last = 1.
    
    2013-02-06  Vincent Celier  <celier@adacore.com>
    
    	* clean.adb (Delete): On VMS use host notation to delete all files.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@195788 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/exp_dist.adb
+++ gcc/ada/exp_dist.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---          Copyright (C) 1992-2012, Free Software Foundation, Inc.         --
+--          Copyright (C) 1992-2013, Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -2176,7 +2176,7 @@ package body Exp_Dist is
 
                Append_To (Decls,
                  Make_Pragma (Loc,
-                   Chars => Name_Import,
+                   Chars                        => Name_Import,
                    Pragma_Argument_Associations => New_List (
                      Make_Pragma_Argument_Association (Loc,
                        Chars      => Name_Convention,
