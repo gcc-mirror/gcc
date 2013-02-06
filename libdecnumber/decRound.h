8e322cb4b95da8005a57670b0e69148fda10f6f7
Richard Sandiford <rdsandiford@googlemail.com>
2013-02-06 22:09:24 +0000

    Update copyright in libdecnumber.
    From-SVN: r195821

--- libdecnumber/decRound.h
+++ libdecnumber/decRound.h
@@ -1,6 +1,6 @@
 /* Internal testing support for rounding for decimal float.
 
-   Copyright (C) 2006, 2009 Free Software Foundation, Inc.
+   Copyright (C) 2006-2013 Free Software Foundation, Inc.
 
    This file is part of GCC.
 
