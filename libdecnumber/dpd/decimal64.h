53fbfc5ac870b570d6950bd5b9763d2bd7ffca0e
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-02-06 22:09:24 +0000

    Update copyright in libdecnumber.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@195821 138bc75d-0d04-0410-961f-82ee72b054a4

--- libdecnumber/dpd/decimal64.h
+++ libdecnumber/dpd/decimal64.h
@@ -1,5 +1,5 @@
 /* Decimal 64-bit format module header for the decNumber C Library.
-   Copyright (C) 2005, 2007, 2009 Free Software Foundation, Inc.
+   Copyright (C) 2005-2013 Free Software Foundation, Inc.
    Contributed by IBM Corporation.  Author Mike Cowlishaw.
 
    This file is part of GCC.
