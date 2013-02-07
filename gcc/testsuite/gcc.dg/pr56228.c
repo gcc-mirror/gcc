e3936f47f63c128d6a667596111865f35869392f
Jakub Jelinek <jakub@redhat.com>
2013-02-07 09:04:58 +0100

    re PR target/56228 (Indirect call fails to assemble)
    	PR target/56228
    	* config/rs6000/rs6000.md (ptrm): New mode attr.
    	(call_indirect_aix<ptrsize>, call_indirect_aix<ptrsize>_nor11,
    	call_value_indirect_aix<pttrsize>,
    	call_value_indirect_aix<pttrsize>_nor11): Use <ptrm> instead of
    	m in constraints.
    
    	* gcc.dg/pr56228.c: New test.
    
    From-SVN: r195840

---
+++ gcc/testsuite/gcc.dg/pr56228.c
@@ -0,0 +1,16 @@
+/* PR target/56228 */
+/* { dg-do assemble } */
+/* { dg-options "-O2" } */
+
+short a[14] = { 1, 2 };
+short b[15] = { 3, 4 };
+
+int
+foo ()
+{
+  void (*fna) (void) = (void (*) (void)) a;
+  void (*fnb) (void) = (void (*) (void)) b;
+  fna ();
+  fnb ();
+  return a[1] == b[1];
+}
