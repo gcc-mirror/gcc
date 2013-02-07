54353978a4ba9b8c4d7c5b721b2daaca1b1c8975
Paolo Carlini <paolo@gcc.gnu.org>
2013-02-07 15:20:58 +0000

    pr56193.cc: Tweak.
    2013-02-07  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* testsuite/27_io/basic_ios/pr56193.cc: Tweak.
    
    From-SVN: r195852

--- libstdc++-v3/testsuite/27_io/basic_ios/pr56193.cc
+++ libstdc++-v3/testsuite/27_io/basic_ios/pr56193.cc
@@ -3,7 +3,7 @@
 
 // 2013-02-06  Edward Smith-Rowland <3dw4rd@verizon.net>
 //
-// Copyright (C) 2007-2013 Free Software Foundation, Inc.
+// Copyright (C) 2013 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -23,11 +23,9 @@
 #include <iostream>
 
 // PR libstdc++/56193
-
-int
+void
 test01()
 {
   std::cout << std::cout; // { dg-error "cannot bind" }
 }
-
 // { dg-error "initializing argument" "" { target *-*-* } 602 }
