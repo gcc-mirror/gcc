001f259468248e3baf3f258af703b4a1bff5c0ba
Uros Bizjak <ubizjak@gmail.com>
2013-02-11 18:29:53 +0100

    backport: re PR rtl-optimization/56275 (ICE in simplify_subreg, at simplify-rtx.c:5261 with vector code.)
    	Backport from mainline
    	2013-02-11  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR rtl-optimization/56275
    	* simplify-rtx.c (avoid_constant_pool_reference): Check that
    	offset is non-negative and less than cmode size before
    	calling simplify_subreg.
    
    testsuite/ChangeLog
    
    	Backport from mainline
    	2013-02-11  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR rtl-optimization/56275
    	* gcc.dg/pr56275.c: New test.
    
    From-SVN: r195947

---
+++ gcc/testsuite/gcc.dg/pr56275.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+/* { dg-options "-O2 -mno-sse" { target { i?86-*-* x86_64-*-* } } } */
+
+typedef long long v2tw __attribute__ ((vector_size (2 * sizeof (long long))));
+
+void tiger_block_v2 (long long in1, v2tw *res)
+{
+  v2tw i1 = { in1, in1 };
+
+  *res = i1 << 1;
+}
