fdb1fa9ebe52bb178f41a27ea5b55b5c17d1aa3c
Janus Weil <janus@gcc.gnu.org>
2013-02-12 13:15:26 +0100

    re PR fortran/46952 ([OOP] Spurious "recursive call" error with type bound procedure)
    2013-02-12  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/46952
    	* resolve.c (resolve_call): Do not check deferred procedures for
    	recursiveness.
    
    
    2013-02-12  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/46952
    	* gfortran.dg/typebound_deferred_1.f90: New.
    
    From-SVN: r195975

---
+++ gcc/testsuite/gfortran.dg/typebound_deferred_1.f90
@@ -0,0 +1,23 @@
+! { dg-do compile }
+!
+! PR 46952: [OOP] Spurious "recursive call" error with type bound procedure
+!
+! Contributed by Ian Harvey <ian_harvey@bigpond.com>
+
+module m
+
+  type, abstract :: t
+  contains
+    procedure(inter), pass, deferred :: foo
+  end type
+
+contains
+
+  subroutine inter(this)
+    class(t) :: this
+    call this%foo()
+  end subroutine inter
+
+end module m 
+
+! { dg-final { cleanup-modules "m" } }
