f2309b6a8d23291ecf53e5beb40c06a90e6b27e6
tmsriram <tmsriram@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-02-13 21:51:27 +0000

    2013-02-13  Sriraman Tallam  <tmsriram@google.com>
    	* g++.dg/ext/mv12-aux.C: Delete, move to mv12-aux.cc.
    	* g++.dg/ext/mv12-aux.cc: New file.
    	* g++.dg/ext/mv12.h: Remove directives. Fix comment.
    	* g++.dg/ext/mv12.C: Fix file name.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@196033 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ext/mv12.h
+++ gcc/testsuite/g++.dg/ext/mv12.h
@@ -1,6 +1,4 @@
-// Header file used by mv12.C and mv12-aux.C.
-// { dg-do compile { target i?86-*-* x86_64-*-* } }
-// { dg-options "" }
+// Header file used by mv12.C and mv12-aux.cc.
 
 int foo () __attribute__ ((target ("default")));
 int foo () __attribute__ ((target ("sse4.2")));
