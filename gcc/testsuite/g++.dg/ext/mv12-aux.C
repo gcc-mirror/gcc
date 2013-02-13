c35fb07ed26e3e1695dabfe4897a7af1728ae69d
tmsriram <tmsriram@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-02-13 18:55:02 +0000

    Make sure mv12-aux.C has same directives as mv12.C. mv12-aux.C should not compile for non-x86 targets.
    2013-02-13  Sriraman Tallam  <tmsriram@google.com>
    
    	* g++.dg/ext/mv12-aux.C: Add directives to match mv12.C.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@196026 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ext/mv12-aux.C
+++ gcc/testsuite/g++.dg/ext/mv12-aux.C
@@ -1,6 +1,9 @@
 // Test case to check if multiversioning works as expected when the versions
 // are defined in different files. Auxiliary file for mv12.C.
-// { dg-do compile }
+
+// { dg-do compile { target i?86-*-* x86_64-*-* } }
+// { dg-require-ifunc "" }
+// { dg-options "-O2" }
 
 #include "mv12.h"
 
