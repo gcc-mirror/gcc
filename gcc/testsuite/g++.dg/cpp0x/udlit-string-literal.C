911b08c636a42ef9995ce850b4337620e85b14c8
emsr <emsr@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-02-14 02:55:42 +0000

    gcc/libcpp/
    2013-02-13  Ed Smith-Rowland  <3dw4rd@verizon.net>
    
    	PR c++/55582
    	* libcpp/lex.c (lex_raw_string): Allow string literal with suffix
    	beginning with 's' to be parsed as a C++11 user-defined literal.
    
    
    gcc/testsuite/
    
    2013-02-13  Ed Smith-Rowland  <3dw4rd@verizon.net>
    
    	PR c++/55582
    	* g++.dg/cpp0x/udlit-string-literal.h: New.
    	* g++.dg/cpp0x/udlit-string-literal.C: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@196041 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/udlit-string-literal.C
@@ -0,0 +1,13 @@
+// { dg-options "-std=c++11" }
+// { dg-require-effective-target stdint_types }
+// PR c++/55582
+
+#include "udlit-string-literal.h"
+
+using namespace my_string_literals;
+
+decltype("Hello, World!"s) s;
+decltype(u8"Hello, World!"s) s8;
+decltype(L"Hello, World!"s) ws;
+decltype(u"Hello, World!"s) s16;
+decltype(U"Hello, World!"s) s32;
