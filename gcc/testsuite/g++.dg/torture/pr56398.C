267499fa590aa35d0e7ad86ee74abf1387c25f63
Marek Polacek <polacek@redhat.com>
2013-02-21 10:21:19 +0000

    New test for PR56398.
    From-SVN: r196199

---
+++ gcc/testsuite/g++.dg/torture/pr56398.C
@@ -0,0 +1,22 @@
+// { dg-do compile }
+// { dg-options "-g" }
+
+namespace
+{
+#0 "/usr/include/c/4.8/bits/postypes.h" 3
+}
+
+vtkpow (int b)
+{
+  int a1;
+  int b1;
+  int c;
+  while (b1)
+    {
+      while (b)
+    b1 = 0;
+      b1 = b1 - 1;
+      c = c * a1;
+    }
+  return c;
+}
