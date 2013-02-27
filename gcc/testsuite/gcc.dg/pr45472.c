436a956a80a5b083348ce9c336b59ae04f883f2e
Andrey Belevantsev <abel@ispras.ru>
2013-02-27 12:56:08 +0400

    re PR middle-end/45472 ([Middle-end volatile semantics] ICE: in move_op_ascend, at sel-sched.c:6124 with -fselective-scheduling2)
            PR middle-end/45472
    
    	gcc/
            * sel-sched-ir.c (merge_expr): Also change vinsn of merged expr
            when the may_trap_p bit of the exprs being merged differs.
    
            Reorder tests for speculativeness in the logical and operator.
    
    	testsuite/
    	* gcc.dg/45472.c: New test.
    
    From-SVN: r196308

---
+++ gcc/testsuite/gcc.dg/pr45472.c
@@ -0,0 +1,21 @@
+/* { dg-do compile { target powerpc*-*-* ia64-*-* x86_64-*-* } } */
+/* { dg-options "-O -fschedule-insns2 -fselective-scheduling2" } */
+
+struct S
+{
+  volatile long vl;
+  int i;
+};
+struct S s1, s2;
+
+void
+foo (int j, int c)
+{
+  int i;
+  for (i = 0; i <= j; i++)
+    {
+      if (c)
+	s2.vl += s1.vl;
+      s1 = s2;
+    }
+}
