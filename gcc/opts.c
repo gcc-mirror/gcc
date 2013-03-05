a5d1569a6dd3126faf40b9aee31555438918fba4
Jakub Jelinek <jakub@redhat.com>
2013-03-05 07:01:13 +0100

    re PR bootstrap/56509 (gnattools build failure)
    	PR bootstrap/56509
    	* opts.c (opts_obstack, opts_concat): Moved to...
    	* opts-common.c (opts_obstack, opts_concat): ... here.
    
    From-SVN: r196449

--- gcc/opts.c
+++ gcc/opts.c
@@ -268,40 +268,6 @@ add_comma_separated_to_vector (void **pvec, const char *arg)
   *pvec = v;
 }
 
-/* Like libiberty concat, but allocate using opts_obstack.  */
-
-char *
-opts_concat (const char *first, ...)
-{
-  char *newstr, *end;
-  size_t length = 0;
-  const char *arg;
-  va_list ap;
-
-  /* First compute the size of the result and get sufficient memory.  */
-  va_start (ap, first);
-  for (arg = first; arg; arg = va_arg (ap, const char *))
-    length += strlen (arg);
-  newstr = XOBNEWVEC (&opts_obstack, char, length + 1);
-  va_end (ap);
-
-  /* Now copy the individual pieces to the result string. */
-  va_start (ap, first);
-  for (arg = first, end = newstr; arg; arg = va_arg (ap, const char *))
-    {
-      length = strlen (arg);
-      memcpy (end, arg, length);
-      end += length;
-    }
-  *end = '\0';
-  va_end (ap);
-  return newstr;
-}
-
-/* Obstack for option strings.  */
-
-struct obstack opts_obstack;
-
 /* Initialize OPTS and OPTS_SET before using them in parsing options.  */
 
 void
