04414cc68753b7d8c26aeea6ea1c7f9d1068c774
Kai Tietz <ktietz@redhat.com>
2013-03-06 19:29:46 +0100

    20090914-2_0.c: Skip for mingw and cygwin targets.
    	* gcc.dg/lto/20090914-2_0.c: Skip for mingw and cygwin
    	targets.
    	* gcc.dg/lto/20091013-1_1.c: Set x64-mingw as xfail.
    	* gcc.dg/lto/20091013-1_2.c: Likewise.
    	* gcc.dg/pr31490.c: Adjust for LLP64 targets.
    
    From-SVN: r196508

--- gcc/testsuite/gcc.dg/lto/20091013-1_1.c
+++ gcc/testsuite/gcc.dg/lto/20091013-1_1.c
@@ -1,4 +1,4 @@
-/* { dg-xfail-if "cast to pointer of different size" { "avr-*-*" } { "*" } { "" } } */
+/* { dg-xfail-if "cast to pointer of different size" { "avr-*-*" x86_64-*-mingw* } { "*" } { "" } } */
 typedef struct HDC__ { int unused; } *HDC;
 typedef struct HFONT__ { int unused; } *HFONT;
 
