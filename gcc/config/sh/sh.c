9fe603c35ae9aeebef1fb77bf4df7d9832904d79
olegendo <olegendo@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-03-06 00:41:25 +0000

    PR target/56529 	* config/sh/sh.c (sh_option_override): Check for TARGET_DYNSHIFT 	instead of TARGET_SH2 for call-table case.  Do not set sh_div_strategy 	to SH_DIV_CALL_TABLE for TARGET_SH2. 	* config.gcc (sh_multilibs): Add m2 and m2a to sh*-*-linux* multilib 	list. 	* doc/invoke.texi (SH options): Document mdiv= call-div1, call-fp, 	call-table options.
    	PR target/56529
    	* config/sh/lib1funcs.S (udivsi3_i4i, sdivsi3_i4i): Add __SH2A__ to
    	inclusion list.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@196484 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sh/sh.c
+++ gcc/config/sh/sh.c
@@ -820,7 +820,7 @@ sh_option_override (void)
 		   || (TARGET_HARD_SH4 && TARGET_SH2E)
 		   || (TARGET_SHCOMPACT && TARGET_FPU_ANY)))
 	sh_div_strategy = SH_DIV_CALL_FP;
-      else if (! strcmp (sh_div_str, "call-table") && TARGET_SH2)
+      else if (! strcmp (sh_div_str, "call-table") && TARGET_DYNSHIFT)
 	sh_div_strategy = SH_DIV_CALL_TABLE;
       else
 	/* Pick one that makes most sense for the target in general.
@@ -840,8 +840,6 @@ sh_option_override (void)
 	  sh_div_strategy = SH_DIV_CALL_FP;
 	/* SH1 .. SH3 cores often go into small-footprint systems, so
 	   default to the smallest implementation available.  */
-	else if (TARGET_SH2)	/* ??? EXPERIMENTAL */
-	  sh_div_strategy = SH_DIV_CALL_TABLE;
 	else
 	  sh_div_strategy = SH_DIV_CALL_DIV1;
     }
