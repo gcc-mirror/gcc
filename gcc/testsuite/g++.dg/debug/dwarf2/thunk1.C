3687a208b7bfca87fc95ab9e26fa63d0edf474b5
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-03-06 16:14:53 +0000

    thunk1.C bikeshedding
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@196496 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/debug/dwarf2/thunk1.C
+++ gcc/testsuite/g++.dg/debug/dwarf2/thunk1.C
@@ -1,10 +1,9 @@
 // Test that we don't add the x86 PC thunk to .debug_ranges
 // { dg-do compile { target { { i?86-*-* x86_64-*-* } && ia32 } } }
+// { dg-require-effective-target fpic }
+// { dg-skip-if "darwin doesn't use the thunk for PIC" { *-*-darwin* } }
 // { dg-options "-g -fpic -fno-dwarf2-cfi-asm" }
 // { dg-final { scan-assembler-times "LFB3" 5 } }
-
-// Darwin doesn't use the thunk for PIC.
-// { dg-skip-if "no pic thunk" { *-*-darwin* } }
 
 template <class T> void f(T t) { }
 
