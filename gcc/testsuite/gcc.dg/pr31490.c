c1a5a2f975ac82969e38d4e3ca7a7c9f67dbc275
schwab <schwab@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-03-07 10:29:45 +0000

    * gcc.dg/pr31490.c: Fix last change.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@196519 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr31490.c
+++ gcc/testsuite/gcc.dg/pr31490.c
@@ -7,4 +7,4 @@ __extension__ typedef __UINTPTR_TYPE__ uintptr_t;
 
 int cpu (void *attr) {}
 const uintptr_t x __attribute__((section("foo"))) =  (uintptr_t)&cpu;
-const uintptr g __attribute__((section("foo"))) = 0;
+const uintptr_t g __attribute__((section("foo"))) = 0;
