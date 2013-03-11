6d09a025f3d915e7ac1743083ee4a50836926937
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-03-11 00:56:45 +0000

    * gcc.dg/pr44194-1.c: Skip compilation on hppa*64*-*-*.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@196589 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr44194-1.c
+++ gcc/testsuite/gcc.dg/pr44194-1.c
@@ -1,4 +1,4 @@
-/* { dg-do compile { target { { { { { i?86-*-* x86_64-*-* } && x32 } || lp64 } && { ! s390*-*-* } } &&  { ! alpha*-*-* } } } } */
+/* { dg-do compile { target { { { { { { i?86-*-* x86_64-*-* } && x32 } || lp64 } && { ! s390*-*-* } } && { ! hppa*64*-*-* } } && { ! alpha*-*-* } } } } */
 /* { dg-options "-O2 -fdump-rtl-dse1 -fdump-rtl-final" } */
 
 /* Restrict to 64-bit targets since 32-bit targets usually return small
