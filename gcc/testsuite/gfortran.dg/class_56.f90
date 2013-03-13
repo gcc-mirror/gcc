4c597d6afc0e9c01f646359842d3b0aa6d6afc81
Paul Thomas <pault@gcc.gnu.org>
2013-03-13 05:32:07 +0000

    re PR fortran/56575 (An invalid OO code causes ICE)
    2013-03-13  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/56575
    	* expr.c (gfc_default_initializer): Check that a class declared
    	type has any components.
    	* resolve.c (resolve_fl_derived0): On failing the test for C437
    	set the type to BT_UNKNOWN to prevent repeat error messages.
    
    2013-03-13  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/56575
    	* gfortran.dg/class_56.f90: New test.
    
    From-SVN: r196627

---
+++ gcc/testsuite/gfortran.dg/class_56.f90
@@ -0,0 +1,21 @@
+! { dg-do compile }
+! Test fix for PR56575.
+!
+! Contributed by A Kasahara  <latlon90180+gcc_bugzilla@gmail.com>
+!
+module lib_container
+  implicit none
+
+  type:: Object
+  end type Object
+
+  type:: Container
+    class(Object):: v ! { dg-error "must be allocatable or pointer" }
+  end type Container
+
+contains
+
+  subroutine proc(self)
+    class(Container), intent(inout):: self
+  end subroutine proc
+end module lib_container
