ad6fdc197621e1f48d72df91f021c187949ba6c2
Paolo Carlini <paolo.carlini@oracle.com>
2013-03-18 10:15:56 +0000

    PR libstdc++/55977 (partial, std::vector and std::deque bits)
    2013-03-18  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR libstdc++/55977 (partial, std::vector and std::deque bits)
    	* include/bits/stl_vector.h (_M_range_initialize(_InputIterator,
    	_InputIterator, std::input_iterator_tag)): Use emplace_back.
    	* include/bits/deque.tcc (_M_range_initialize(_InputIterator,
    	_InputIterator, std::input_iterator_tag)): Likewise.
    	* testsuite/23_containers/vector/cons/55977.cc: New.
    	* testsuite/23_containers/deque/cons/55977.cc: Likewise.
    	* testsuite/23_containers/vector/requirements/dr438/assign_neg.cc:
    	Adjust dg-error line number.
    	* testsuite/23_containers/vector/requirements/dr438/insert_neg.cc:
    	Likewise.
    
    From-SVN: r196774

--- libstdc++-v3/testsuite/23_containers/vector/requirements/dr438/assign_neg.cc
+++ libstdc++-v3/testsuite/23_containers/vector/requirements/dr438/assign_neg.cc
@@ -18,7 +18,7 @@
 // <http://www.gnu.org/licenses/>.
 
 // { dg-do compile }
-// { dg-error "no matching" "" { target *-*-* } 1236 }
+// { dg-error "no matching" "" { target *-*-* } 1240 }
 
 #include <vector>
 
