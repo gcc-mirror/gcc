b80793f266ddba8adcecad376bafedc154f31774
Jakub Jelinek <jakub@redhat.com>
2013-03-20 09:40:08 +0100

    re PR tree-optimization/56635 (internal compiler error: in find_lattice_value, at tree-complex.c:15)
    	PR tree-optimization/56635
    	* tree-ssa-phiopt.c (cond_if_else_store_replacement_1): Give up
    	if lhs of then_assign and else_assign don't have compatible types.
    
    	* g++.dg/torture/pr56635.C: New test.
    
    From-SVN: r196808

--- gcc/tree-ssa-phiopt.c
+++ gcc/tree-ssa-phiopt.c
@@ -1528,7 +1528,7 @@ cond_if_else_store_replacement_1 (basic_block then_bb, basic_block else_bb,
 				  basic_block join_bb, gimple then_assign,
 				  gimple else_assign)
 {
-  tree lhs_base, lhs, then_rhs, else_rhs, name;
+  tree lhs_base, lhs, else_lhs, then_rhs, else_rhs, name;
   source_location then_locus, else_locus;
   gimple_stmt_iterator gsi;
   gimple newphi, new_stmt;
@@ -1544,8 +1544,10 @@ cond_if_else_store_replacement_1 (basic_block then_bb, basic_block else_bb,
     return false;
 
   lhs = gimple_assign_lhs (then_assign);
+  else_lhs = gimple_assign_lhs (else_assign);
   if (!is_gimple_reg_type (TREE_TYPE (lhs))
-      || !operand_equal_p (lhs, gimple_assign_lhs (else_assign), 0))
+      || !operand_equal_p (lhs, else_lhs, 0)
+      || !types_compatible_p (TREE_TYPE (lhs), TREE_TYPE (else_lhs)))
     return false;
 
   lhs_base = get_base_address (lhs);
