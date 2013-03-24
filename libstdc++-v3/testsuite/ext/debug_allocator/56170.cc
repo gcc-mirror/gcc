95a83503a058f4ae36038ce80921c2cf8e54b908
Jonathan Wakely <jwakely.gcc@gmail.com>
2013-03-24 22:10:29 +0000

    re PR libstdc++/56170 (Extension debug_allocator seems non-compliant w.r.t. rebind)
    	PR libstdc++/56170
    	* include/ext/debug_allocator.h (debug_allocator): Add missing members
    	to meet allocator requirements.
    	* testsuite/ext/debug_allocator/56170.cc: New.
    
    From-SVN: r197023

---
+++ libstdc++-v3/testsuite/ext/debug_allocator/56170.cc
@@ -0,0 +1,27 @@
+// Copyright (C) 2013 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile }
+
+#include <list>
+#include <vector>
+#include <ext/debug_allocator.h>
+
+using __gnu_cxx::debug_allocator;
+
+template class std::list<int, debug_allocator<std::allocator<int> > >;
+template class std::vector<int, debug_allocator<std::allocator<int> > >;
