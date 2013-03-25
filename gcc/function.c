961299131ad5ad6a9b5fb8fa3b11dd1961cebfdb
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-03-25 11:24:12 +0000

    * function.c (emit_use_return_register_into_block): On cc0 targets, 	do not emit the sequence between cc0 setter and user.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@197036 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/function.c
+++ gcc/function.c
@@ -5598,12 +5598,17 @@ prepare_shrink_wrap (basic_block entry_block)
 static void
 emit_use_return_register_into_block (basic_block bb)
 {
-  rtx seq;
+  rtx seq, insn;
   start_sequence ();
   use_return_register ();
   seq = get_insns ();
   end_sequence ();
-  emit_insn_before (seq, BB_END (bb));
+  insn = BB_END (bb);
+#ifdef HAVE_cc0
+  if (reg_mentioned_p (cc0_rtx, PATTERN (insn)))
+    insn = prev_cc0_setter (insn);
+#endif
+  emit_insn_before (seq, insn);
 }
 
 
