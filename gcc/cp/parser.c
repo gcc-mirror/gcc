302589f0c0349b6b4068587d4440c75c74a480f7
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-03-28 10:00:05 +0000

    2013-03-28  Paolo Carlini  <paolo.carlini@oracle.com>
    	* call.c (joust): Don't call inform for a permerror returning false.
    	* parser.c (cp_parser_check_class_key): Likewise.
    	* pt.c (tsubst_copy_and_build): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@197190 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/parser.c
+++ gcc/cp/parser.c
@@ -23363,12 +23363,12 @@ cp_parser_check_class_key (enum tag_types class_key, tree type)
     return;
   if ((TREE_CODE (type) == UNION_TYPE) != (class_key == union_type))
     {
-      permerror (input_location, "%qs tag used in naming %q#T",
-		 class_key == union_type ? "union"
-		 : class_key == record_type ? "struct" : "class",
-		 type);
-      inform (DECL_SOURCE_LOCATION (TYPE_NAME (type)),
-	      "%q#T was previously declared here", type);
+      if (permerror (input_location, "%qs tag used in naming %q#T",
+		     class_key == union_type ? "union"
+		     : class_key == record_type ? "struct" : "class",
+		     type))
+	inform (DECL_SOURCE_LOCATION (TYPE_NAME (type)),
+		"%q#T was previously declared here", type);
     }
 }
 
