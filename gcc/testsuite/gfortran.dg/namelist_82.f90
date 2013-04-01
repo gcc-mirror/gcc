123d7a94342b49153722fe385b60dfd54a216aaa
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2013-04-01 20:59:34 +0000

    re PR fortran/56660 (Fails to read NAMELIST with certain form array syntax)
    2013-04-01  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR fortran/56660
    	* gfortran.dg/namelist_82.f90:  New test.
    
    From-SVN: r197322

---
+++ gcc/testsuite/gfortran.dg/namelist_82.f90
@@ -0,0 +1,33 @@
+! { dg-do run }
+! PR56660  Fails to read NAMELIST with certain form array syntax
+type ptracer
+   character(len = 2)  :: sname
+   logical             :: lini
+end type ptracer
+
+type(ptracer) , dimension(3) :: tracer
+namelist/naml1/  tracer
+
+tracer(:) = ptracer('XXX', .false.)
+
+open (99, file='nml.dat', status="replace")
+write(99,*) "&naml1"
+!write(99,*) "   tracer(2)   = 'bb' , .true."
+write(99,*) "   tracer(:)   = 'aa' , .true."
+write(99,*) "   tracer(2)   = 'bb' , .true."
+write(99,*) "/"
+rewind(99)
+
+read (99, nml=naml1)
+close (99, status="delete")
+
+if (tracer(1)%sname.ne.'aa') call abort()
+if (.not.tracer(1)%lini) call abort()
+if (tracer(2)%sname.ne.'bb') call abort()
+if (.not.tracer(2)%lini) call abort()
+if (tracer(3)%sname.ne.'XX') call abort()
+if (tracer(3)%lini) call abort()
+
+!write (*, nml=naml1)
+
+end
