7a80735b9353db7d9ad46a1880bfcd3e8efbc427
Wei Mi <wmi@google.com>
2013-04-02 16:53:49 +0000

    i386.c (ix86_rtx_costs): Set proper rtx cost for ashl<mode>3_mask...
    2013-04-02  Wei Mi  <wmi@google.com>
    
    	* config/i386/i386.c (ix86_rtx_costs): Set proper rtx cost for
    	ashl<mode>3_mask, *<shift_insn><mode>3_mask and
    	*<rotate_insn><mode>3_mask in i386.md.
    
    From-SVN: r197362

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -34174,6 +34174,13 @@ ix86_rtx_costs (rtx x, int code_i, int outer_code_i, int opno, int *total,
 	{
 	  if (CONST_INT_P (XEXP (x, 1)))
 	    *total = cost->shift_const;
+	  else if (GET_CODE (XEXP (x, 1)) == SUBREG
+		   && GET_CODE (XEXP (XEXP (x, 1), 0)) == AND)
+	    {
+	      /* Return the cost after shift-and truncation.  */
+	      *total = cost->shift_var;
+	      return true;
+	    }
 	  else
 	    *total = cost->shift_var;
 	}
