da694a7701bc6c2defe09cc590166de096dd26da
Marc Glisse <marc.glisse@inria.fr>
2013-04-03 11:44:28 +0200

    simplify-rtx.c (simplify_binary_operation_1): Handle VEC_MERGE.
    2013-04-03  Marc Glisse  <marc.glisse@inria.fr>
    
    gcc/
    	* simplify-rtx.c (simplify_binary_operation_1) <VEC_SELECT>:
    	Handle VEC_MERGE.
    	(simplify_ternary_operation) <VEC_MERGE>: Use unsigned HOST_WIDE_INT
    	for masks. Test for side effects. Handle nested VEC_MERGE. Handle
    	equal arguments.
    
    gcc/testsuite/
    	* gcc.target/i386/merge-1.c: New testcase.
    	* gcc.target/i386/avx2-vpblendd128-1.c: Make it non-trivial.
    
    From-SVN: r197394

---
+++ gcc/testsuite/gcc.target/i386/merge-1.c
@@ -0,0 +1,22 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -msse2" } */
+
+#include <x86intrin.h>
+
+void
+f (double *r, __m128d x, __m128d y, __m128d z)
+{
+  __m128d t=_mm_move_sd(x,y);
+  __m128d u=_mm_move_sd(t,z);
+  *r = u[0];
+}
+
+__m128d
+g(__m128d x, __m128d y, __m128d z)
+{
+  __m128d t=_mm_move_sd(x,y);
+  __m128d u=_mm_move_sd(t,z);
+  return u;
+}
+
+/* { dg-final { scan-assembler-times "movsd" 1 } } */
