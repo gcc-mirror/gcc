4b10ee0ba7fa4b623b7bb8bfe981cf26783cf2c5
Jakub Jelinek <jakub@redhat.com>
2013-04-03 19:57:32 +0200

    backport: re PR middle-end/54486 (Spurious printf format warning mentions nonexistent type 'sizetype')
    	Backported from mainline
    	2012-09-05  Jakub Jelinek  <jakub@redhat.com>
    
    	PR middle-end/54486
    	* builtins.c (fold_builtin_strspn, fold_builtin_strcspn): Use
    	build_int_cst with size_type_node instead of size_int.
    
    	* c-c++-common/pr54486.c: New test.
    
    From-SVN: r197444

--- gcc/builtins.c
+++ gcc/builtins.c
@@ -11575,7 +11575,7 @@ fold_builtin_strspn (location_t loc, tree s1, tree s2)
       if (p1 && p2)
 	{
 	  const size_t r = strspn (p1, p2);
-	  return size_int (r);
+	  return build_int_cst (size_type_node, r);
 	}
 
       /* If either argument is "", return NULL_TREE.  */
@@ -11620,7 +11620,7 @@ fold_builtin_strcspn (location_t loc, tree s1, tree s2)
       if (p1 && p2)
 	{
 	  const size_t r = strcspn (p1, p2);
-	  return size_int (r);
+	  return build_int_cst (size_type_node, r);
 	}
 
       /* If the first argument is "", return NULL_TREE.  */
