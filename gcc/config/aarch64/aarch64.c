9d821fa520370411c092b3fa4a6b4eabcedc034e
Kyrylo Tkachov <kyrylo.tkachov@arm.com>
2013-04-03 16:38:24 +0000

    re PR target/56809 (Revision 197266 causes trunk ICE for arm-none-eabi targets)
    2013-04-03  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    
    	PR target/56809
    	* config/aarch64/aarch64.c (is_jump_table): Use next_active_insn
    	instead of next_real_insn.
    
    From-SVN: r197434

--- gcc/config/aarch64/aarch64.c
+++ gcc/config/aarch64/aarch64.c
@@ -4070,7 +4070,7 @@ aarch64_output_casesi (rtx *operands)
 {
   char buf[100];
   char label[100];
-  rtx diff_vec = PATTERN (next_real_insn (operands[2]));
+  rtx diff_vec = PATTERN (next_active_insn (operands[2]));
   int index;
   static const char *const patterns[4][2] =
   {
