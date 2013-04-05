5c84774acb5ce694f5910ade4b850937520bc207
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-04-05 13:06:49 +0000

    * semantics.c (finish_omp_for): Disallow class iterators for 	OMP_SIMD and OMP_FOR_SIMD loops.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-4_0-branch@197515 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -5090,6 +5090,13 @@ finish_omp_for (location_t locus, enum tree_code code, tree declv, tree initv,
 
       if (CLASS_TYPE_P (TREE_TYPE (decl)))
 	{
+	  if (code == OMP_SIMD || code == OMP_FOR_SIMD)
+	    {
+	      error_at (elocus, "%<#pragma omp%s simd%> used with class "
+				"iteration variable %qE",
+			code == OMP_FOR_SIMD ? " for" : "", decl);
+	      return NULL;
+	    }
 	  if (handle_omp_for_class_iterator (i, locus, declv, initv, condv,
 					     incrv, &body, &pre_body, clauses))
 	    return NULL;
