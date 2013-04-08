6c8cbfd899bbf9b3fe9b3f09a0539017ebff3d5f
Andreas Krebbel <Andreas.Krebbel@de.ibm.com>
2013-04-08 07:55:57 +0000

    s390.c (s390_expand_insv): Only accept insertions within mode size.
    2013-04-08  Andreas Krebbel  <Andreas.Krebbel@de.ibm.com>
    
    	* config/s390/s390.c (s390_expand_insv): Only accept insertions
    	within mode size.
    
    From-SVN: r197567

--- gcc/config/s390/s390.c
+++ gcc/config/s390/s390.c
@@ -4647,6 +4647,9 @@ s390_expand_insv (rtx dest, rtx op1, rtx op2, rtx src)
   int smode_bsize, mode_bsize;
   rtx op, clobber;
 
+  if (bitsize + bitpos > GET_MODE_SIZE (mode))
+    return false;
+
   /* Generate INSERT IMMEDIATE (IILL et al).  */
   /* (set (ze (reg)) (const_int)).  */
   if (TARGET_ZARCH
