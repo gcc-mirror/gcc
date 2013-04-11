d5102150735b567fcce528095fe9ed956a12d341
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-04-11 15:21:24 +0000

    * gcc.dg/tree-ssa/vrp87.c: Do not run test on various targets.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@197808 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/vrp87.c
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp87.c
@@ -1,4 +1,5 @@
-/* { dg-do compile } */
+/* { dg-do compile { target { ! "m68k*-*-* mmix*-*-* mep*-*-* bfin*-*-* v850*-*-* picochip*-*-* moxie*-*-* cris*-*-* m32c*-*-* fr30*-*-* mcore*-*-*"} } } */
+
 /* { dg-options "-O2 -fdump-tree-vrp2-details -fdump-tree-cddce2-details" } */
 
 struct bitmap_head_def;
