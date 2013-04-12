06f9b387e517d6f54b6c9b35d4f972bc1542855c
Jakub Jelinek <jakub@redhat.com>
2013-04-12 10:18:59 +0200

    re PR tree-optimization/56918 (incorrect auto-vectorization of array initialization)
    	PR tree-optimization/56918
    	PR tree-optimization/56920
    	* fold-const.c (int_const_binop_1): Use op1.mul_with_sign (op2, ...)
    	instead of op1 - op2.  Pass 2 * TYPE_PRECISION (type) as second
    	argument to rshift method.  For 2 * HOST_BITS_PER_WIDE_INT precision
    	use wide_mul_with_sign method.
    
    	* gcc.dg/vect/pr56918.c: New test.
    	* gcc.dg/vect/pr56920.c: New test.
    
    From-SVN: r197846

--- gcc/fold-const.c
+++ gcc/fold-const.c
@@ -984,12 +984,22 @@ int_const_binop_1 (enum tree_code code, const_tree arg1, const_tree arg2,
       break;
 
     case MULT_HIGHPART_EXPR:
-      /* ??? Need quad precision, or an additional shift operand
-	 to the multiply primitive, to handle very large highparts.  */
       if (TYPE_PRECISION (type) > HOST_BITS_PER_WIDE_INT)
-	return NULL_TREE;
-      tmp = op1 - op2;
-      res = tmp.rshift (TYPE_PRECISION (type), TYPE_PRECISION (type), !uns);
+	{
+	  bool dummy_overflow;
+	  if (TYPE_PRECISION (type) != 2 * HOST_BITS_PER_WIDE_INT)
+	    return NULL_TREE;
+	  op1.wide_mul_with_sign (op2, uns, &res, &dummy_overflow);
+	}
+      else
+	{
+	  bool dummy_overflow;
+	  /* MULT_HIGHPART_EXPR can't ever oveflow, as the multiplication
+	     is performed in twice the precision of arguments.  */
+	  tmp = op1.mul_with_sign (op2, false, &dummy_overflow);
+	  res = tmp.rshift (TYPE_PRECISION (type),
+			    2 * TYPE_PRECISION (type), !uns);
+	}
       break;
 
     case TRUNC_DIV_EXPR:
