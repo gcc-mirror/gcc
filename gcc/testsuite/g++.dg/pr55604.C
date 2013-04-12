533c15bc0353f4778c022cafe6dfc3fb810fba3c
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-04-12 09:50:28 +0000

    2013-04-12  Richard Biener  <rguenther@suse.de>
    	* tree-pass.h (TODO_do_not_ggc_collect): New.
    	* passes.c (execute_one_ipa_transform_pass): Honor
    	TODO_do_not_ggc_collect.
    	(execute_one_pass): Likewise.
    
    	Revert
    	2013-04-10  Richard Biener  <rguenther@suse.de>
    
    	* passes.c (init_optimization_passes): Remove reload pass.
    	* ira.c (do_reload): Merge into ...
    	(ira): ... this.
    	(rest_of_handle_reload): Remove.
    	(pass_reload): Likewise.
    	* config/i386/i386.c (ix86_option_override): Refer to ira instead
    	of reload for vzeroupper pass placement.
    
    	* g++.dg/pr55604.C: Use -fdump-rtl-ira.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@197865 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/pr55604.C
+++ gcc/testsuite/g++.dg/pr55604.C
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O -fdump-rtl-ira" } */
+/* { dg-options "-O -fdump-rtl-reload" } */
 
 main ()
 {
@@ -8,4 +8,4 @@ main ()
   __builtin_printf ("%d %s\n", t, s);
 }
 
-/* { dg-final { cleanup-rtl-dump "ira" } } */
+/* { dg-final { cleanup-rtl-dump "reload" } } */
