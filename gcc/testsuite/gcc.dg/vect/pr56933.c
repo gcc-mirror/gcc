5185d248d5a554d394c80126646160abbf4f0bfa
Richard Biener <rguenther@suse.de>
2013-04-15 14:08:41 +0000

    re PR tree-optimization/56933 (Vectorizer missing read-write dependency for interleaved accesses)
    2013-04-15  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/56933
    	* tree-vectorizer.h (struct _stmt_vec_info): Remove read_write_dep
    	member.
    	(GROUP_READ_WRITE_DEPENDENCE): Remove.
    	(STMT_VINFO_GROUP_READ_WRITE_DEPENDENCE): Likewise.
    	* tree-vect-data-refs.c (vect_analyze_group_access): Move
    	dependence check ...
    	vect_analyze_data_ref_dependence (vect_analyze_data_ref_dependence):
    	... here.
    	* tree-vect-stmts.c (new_stmt_vec_info): Do not initialize
    	GROUP_READ_WRITE_DEPENDENCE.
    
    	* gcc.dg/vect/pr56933.c: New testcase.
    
    From-SVN: r197972

---
+++ gcc/testsuite/gcc.dg/vect/pr56933.c
@@ -0,0 +1,40 @@
+/* { dg-do run } */
+
+extern void abort (void);
+void __attribute__((noinline,noclone))
+foo (double *b, double *d, double *f)
+{
+  int i;
+  for (i = 0; i < 1024; i++)
+    {
+      d[2*i] = 2. * d[2*i];
+      d[2*i+1] = 4. * d[2*i+1];
+      b[i] = d[2*i] - 1.;
+      f[i] = d[2*i+1] + 2.;
+    }
+}
+int main()
+{
+  double b[1024], d[2*1024], f[1024];
+  int i;
+  for (i = 0; i < 2*1024; i++)
+    d[i] = 1.;
+  foo (b, d, f);
+  for (i = 0; i < 1024; i+= 2)
+    {
+      if (d[2*i] != 2.)
+	abort ();
+      if (d[2*i+1] != 4.)
+	abort ();
+    }
+  for (i = 0; i < 1024; i++)
+    {
+      if (b[i] != 1.)
+	abort ();
+      if (f[i] != 6.)
+	abort ();
+    }
+  return 0;
+}
+
+/* { dg-final { cleanup-tree-dump "vect" } } */
