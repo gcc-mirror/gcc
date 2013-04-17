3e7a28e891887c8dcd3968b8d46a5c32101ae1b4
jb <jb@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-04-17 10:19:40 +0000

    PR 40958 Compress module files with zlib.
    frontend ChangeLog:
    
    2013-04-17  Janne Blomqvist  <jb@gcc.gnu.org>
    
    	PR fortran/40958
    	* scanner.h: New file.
    	* Make-lang.in: Dependencies on scanner.h.
    	* scanner.c (gfc_directorylist): Move to scanner.h.
    	* module.c: Don't include md5.h, include scanner.h and zlib.h.
    	(MOD_VERSION): Add comment about backwards compatibility.
    	(module_fp): Change type to gzFile.
    	(ctx): Remove.
    	(gzopen_included_file_1): New function.
    	(gzopen_included_file): New function.
    	(gzopen_intrinsic_module): New function.
    	(write_char): Use gzputc.
    	(read_crc32_from_module_file): New function.
    	(read_md5_from_module_file): Remove.
    	(gfc_dump_module): Use gz* functions instead of stdio, check gzip
    	crc32 instead of md5.
    	(read_module_to_tmpbuf): Use gz* functions instead of stdio.
    	(gfc_use_module): Use gz* functions.
    
    testsuite ChangeLog:
    
    2013-04-17  Janne Blomqvist  <jb@gcc.gnu.org>
    
            PR fortran/40958
            * lib/gcc-dg.exp (scan-module): Uncompress module file before
            scanning.
            * gfortran.dg/module_md5_1.f90: Remove.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@198023 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/fortran/scanner.h
@@ -0,0 +1,32 @@
+/* Character scanner header.
+   Copyright (C) 2013 Free Software Foundation, Inc.
+   Contributed by Janne Blomqvist
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+
+/* Structure for holding module and include file search path.  */
+typedef struct gfc_directorylist
+{
+  char *path;
+  bool use_for_modules;
+  struct gfc_directorylist *next;
+}
+gfc_directorylist;
+
+/* List of include file search directories.  */
+extern gfc_directorylist *include_dirs, *intrinsic_modules_dirs;
