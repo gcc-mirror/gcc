bee52153273af07da6c5f997a5820e36c19bf7c5
jamborm <jamborm@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-04-19 12:00:27 +0000

    2013-04-19  Martin Jambor  <mjambor@suse.cz>
    	PR tree-optimization/56718
    	* ipa-cp.c (ipa_value_from_known_type_jfunc): Moved...
    	* ipa-prop.c (ipa_binfo_from_known_type_jfunc): ...here, renamed
    	and made public.  Adjusted all callers.
    	(ipa_intraprocedural_devirtualization): New function.
    	* ipa-prop.h (ipa_binfo_from_known_type_jfunc): Declare.
    	(ipa_intraprocedural_devirtualization): Likewise.
    	* Makefile.in (tree-ssa-pre.o): Add ipa-prop.h to dependencies.
    
    testsuite/
    	* g++.dg/ipa/imm-devirt-1.C: New test.
    	* g++.dg/ipa/imm-devirt-2.C: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@198088 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ipa-prop.h
+++ gcc/ipa-prop.h
@@ -507,6 +507,8 @@ tree ipa_get_indirect_edge_target (struct cgraph_edge *ie,
 				   vec<tree> ,
 				   vec<ipa_agg_jump_function_p> );
 struct cgraph_edge *ipa_make_edge_direct_to_target (struct cgraph_edge *, tree);
+tree ipa_binfo_from_known_type_jfunc (struct ipa_jump_func *);
+tree ipa_intraprocedural_devirtualization (gimple);
 
 /* Functions related to both.  */
 void ipa_analyze_node (struct cgraph_node *);
---
+++ gcc/testsuite/g++.dg/ipa/imm-devirt-2.C
@@ -0,0 +1,95 @@
+/* Verify that virtual calls are folded even early inlining puts them into one
+   function with the definition.  */
+/* { dg-do run } */
+/* { dg-options "-O2 -fdump-tree-fre1-details"  } */
+
+extern "C" void abort (void);
+
+class Distraction
+{
+public:
+  float f;
+  double d;
+  Distraction ()
+  {
+    f = 8.3;
+    d = 10.2;
+  }
+  virtual float bar (float z);
+};
+
+class A
+{
+public:
+  int data;
+  virtual int foo (int i);
+};
+
+class B : public A
+{
+public:
+  int data_2;
+  virtual int foo (int i);
+  virtual int baz (int i);
+};
+
+
+class C : public Distraction, public B
+{
+public:
+  __attribute__ ((noinline)) C();
+  virtual int foo (int i);
+};
+
+float __attribute__ ((noinline)) Distraction::bar (float z)
+{
+  f += z;
+  return f/2;
+}
+
+int __attribute__ ((noinline)) A::foo (int i)
+{
+  return i + 1;
+}
+
+int __attribute__ ((noinline)) B::foo (int i)
+{
+  return i + 2;
+}
+
+int __attribute__ ((noinline)) B::baz (int i)
+{
+  return i * 15;
+}
+
+int __attribute__ ((noinline)) C::foo (int i)
+{
+  return i + 3;
+}
+
+int __attribute__ ((noinline,noclone)) get_input(void)
+{
+  return 1;
+}
+
+static inline int middleman (class A *obj, int i)
+{
+  return obj->foo (i);
+}
+
+__attribute__ ((noinline)) C::C()
+{
+}
+
+int main (int argc, char *argv[])
+{
+  class C c;
+
+  if (middleman (&c, get_input ()) != 4)
+    abort ();
+
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump "Replacing call target" "fre1"  } } */
+/* { dg-final { cleanup-tree-dump "fre1" } } */
