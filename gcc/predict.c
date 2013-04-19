ff166d81d9a36cf86e4c5a50d686a35c58292051
Marek Polacek <polacek@redhat.com>
2013-04-19 08:18:29 +0000

    backport: re PR tree-optimization/48189 (ICE: SIGFPE (division by zero) in in predict_loops () at predict.c:991 with --param max-predicted-iterations=0)
    	Backport from mainline
    	2013-01-08  Steven Bosscher  <steven@gcc.gnu.org>
    		    Jakub Jelinek  <jakub@redhat.com>
    
    	PR tree-optimization/48189
    	* predict.c (predict_loops): If max is 0, don't call compare_tree_int.
    	If nitercst is 0, don't predict the exit edge.
    
    	* gcc.dg/pr48189.c: New test.
    
    From-SVN: r198085

--- gcc/predict.c
+++ gcc/predict.c
@@ -983,7 +983,8 @@ predict_loops (void)
 	  if (TREE_CODE (niter) == INTEGER_CST)
 	    {
 	      if (host_integerp (niter, 1)
-		  && compare_tree_int (niter, max-1) == -1)
+		  && max
+		  && compare_tree_int (niter, max - 1) == -1)
 		nitercst = tree_low_cst (niter, 1) + 1;
 	      else
 		nitercst = max;
@@ -1003,6 +1004,11 @@ predict_loops (void)
 	      predictor = PRED_LOOP_ITERATIONS_GUESSED;
 	    }
 	  else
+	    continue;
+
+	  /* If the prediction for number of iterations is zero, do not
+	     predict the exit edges.  */
+	  if (nitercst == 0)
 	    continue;
 
 	  probability = ((REG_BR_PROB_BASE + nitercst / 2) / nitercst);
