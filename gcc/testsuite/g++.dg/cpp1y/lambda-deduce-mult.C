d6fae9877330035590e0377511918983f9d8d07e
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-04-22 19:19:55 +0000

    N3648
    	* parser.c (cp_parser_lambda_introducer): Make lambda capture init
    	pedwarn unconditional except in C++1y mode.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@198156 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp1y/lambda-deduce-mult.C
+++ gcc/testsuite/g++.dg/cpp1y/lambda-deduce-mult.C
@@ -1,7 +1,7 @@
 // Testcase for an extension to allow return type deduction when the lambda
 // contains more than just a single return-statement.
 
-// { dg-options -std=c++0x }
+// { dg-options -std=c++1y }
 // { dg-do run }
 
 bool b;
---
+++ gcc/testsuite/g++.dg/cpp1y/lambda-init4.C
@@ -0,0 +1,14 @@
+// N3648: capture init example from paper
+// { dg-options "-std=c++1y" }
+// { dg-do run }
+
+int x = 4;
+auto y = [&r = x, x = x+1]()->int {
+  r += 2;
+  return x+2;
+}();  // Updates ::x to 6, and initializes y to 7.
+
+int main()
+{
+  if (x != 6 || y != 7) __builtin_abort();
+}
