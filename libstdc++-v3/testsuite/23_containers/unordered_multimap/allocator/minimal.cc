60eba405d23ddeb2d0a289d2b79bebdd89b7c01b
fdumont <fdumont@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-04-22 20:22:07 +0000

    2013-04-22  François Dumont  <fdumont@gcc.gnu.org>
    	* include/bits/hashtable_policy.h: Add C++11 allocator support.
    	* include/bits/hashtable.h: Likewise.
    	* include/bits/unordered_set.h: Likewise.
    	* include/bits/unordered_map.h: Likewise.
    	* include/debug/unordered_set: Likewise.
    	* include/debug/unordered_map: Likewise.
    	* include/std/unordered_set: Remove bits/algobase.h
    	include. Replace bits/alloc_traits.h by ext/alloc_traits.h.
    	* include/std/unordered_map: Likewise.
    	* include/ext/throw_allocator.h: Add checks on calls to allocator
    	construct/destroy.
    	(std::hash<__gnu_cxx::throw_value_limit>): Add conditional throw.
    	(std::hash<__gnu_cxx::throw_value_random>): Likewise.
    	* testsuite/util/regression/rand/priority_queue
    	/container_rand_regression_test.tcc: Adapt.
    	* testsuite/util/regression/rand/assoc
    	/container_rand_regression_test.tcc: Likewise.
    	* testsuite/util/testsuite_counter_type.h: Add count of destructors.
    	* testsuite/23_containers/unordered_set
    	/not_default_constructible_hash_neg.cc: Adjust dg-error line number.
    	* testsuite/23_containers/unordered_set/instantiation_neg.cc: Likewise.
    	* testsuite/23_containers/unordered_set/allocator/copy.cc: New.
    	* testsuite/23_containers/unordered_set/allocator/copy_assign.cc: New.
    	* testsuite/23_containers/unordered_set/allocator/minimal.cc: New.
    	* testsuite/23_containers/unordered_set/allocator/move_assign.cc: New.
    	* testsuite/23_containers/unordered_set/allocator/noexcept.cc: New.
    	* testsuite/23_containers/unordered_set/allocator/swap.cc: New.
    	* testsuite/23_containers/unordered_multiset/allocator/copy.cc: New.
    	* testsuite/23_containers/unordered_multiset/allocator/copy_assign.cc:
    	New.
    	* testsuite/23_containers/unordered_multiset/allocator/minimal.cc: New.
    	* testsuite/23_containers/unordered_multiset/allocator/move_assign.cc:
    	New.
    	* testsuite/23_containers/unordered_multiset/allocator/noexcept.cc: New.
    	* testsuite/23_containers/unordered_multiset/allocator/swap.cc: New.
    	* testsuite/23_containers/unordered_map/allocator/copy.cc: New.
    	* testsuite/23_containers/unordered_map/allocator/copy_assign.cc: New.
    	* testsuite/23_containers/unordered_map/allocator/minimal.cc: New.
    	* testsuite/23_containers/unordered_map/allocator/move_assign.cc: New.
    	* testsuite/23_containers/unordered_map/allocator/noexcept.cc:
    	New.
    	* testsuite/23_containers/unordered_map/allocator/swap.cc: New.
    	* testsuite/23_containers/unordered_multimap/allocator/copy.cc: New.
    	* testsuite/23_containers/unordered_multimap/allocator/copy_assign.cc:
    	New.
    	* testsuite/23_containers/unordered_multimap/allocator/minimal.cc: New.
    	* testsuite/23_containers/unordered_multimap/allocator/move_assign.cc:
    	New.
    	* testsuite/23_containers/unordered_multimap/allocator/noexcept.cc: New.
    	* testsuite/23_containers/unordered_multimap/allocator/swap.cc: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@198158 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/23_containers/unordered_multimap/allocator/minimal.cc
@@ -0,0 +1,64 @@
+// Copyright (C) 2013 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=c++11" }
+
+#include <unordered_map>
+#include <memory>
+#include <testsuite_hooks.h>
+#include <testsuite_allocator.h>
+
+struct T { int i; };
+
+struct hash
+{
+  std::size_t operator()(const T t) const noexcept
+  { return t.i; }
+};
+
+struct equal_to
+{
+  bool operator()(const T& lhs, const T& rhs) const noexcept
+  { return lhs.i == rhs.i; }
+};
+
+
+bool operator==(const T& l, const T& r) { return l.i == r.i; }
+bool operator<(const T& l, const T& r) { return l.i < r.i; }
+
+using __gnu_test::SimpleAllocator;
+
+template class std::unordered_multimap<T, T, hash, equal_to,
+				       SimpleAllocator<T>>;
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  typedef SimpleAllocator<T> alloc_type;
+  typedef std::allocator_traits<alloc_type> traits_type;
+  typedef std::unordered_multimap<T, T, hash, equal_to, alloc_type> test_type;
+  test_type v(alloc_type{});
+  v.emplace(std::piecewise_construct,
+	    std::make_tuple(T()), std::make_tuple(T()));
+  VERIFY( v.max_size() == traits_type::max_size(v.get_allocator()) );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/23_containers/unordered_multimap/allocator/move_assign.cc
@@ -0,0 +1,89 @@
+// Copyright (C) 2013 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=c++11" }
+
+#include <unordered_map>
+#include <testsuite_hooks.h>
+#include <testsuite_allocator.h>
+#include <testsuite_counter_type.h>
+
+using __gnu_test::propagating_allocator;
+using __gnu_test::counter_type;
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  typedef propagating_allocator<counter_type, false> alloc_type;
+  typedef __gnu_test::counter_type_hasher hash;
+  typedef std::unordered_multimap<counter_type, counter_type, hash,
+				  std::equal_to<counter_type>,
+				  alloc_type> test_type;
+
+  test_type v1(alloc_type(1));
+  v1.emplace(std::piecewise_construct,
+	     std::make_tuple(1), std::make_tuple(1));
+
+  test_type v2(alloc_type(2));
+  v2.emplace(std::piecewise_construct,
+	     std::make_tuple(2), std::make_tuple(2));
+
+  counter_type::reset();
+
+  v2 = std::move(v1);
+
+  VERIFY( 1 == v1.get_allocator().get_personality() );
+  VERIFY( 2 == v2.get_allocator().get_personality() );
+
+  // No move because key is const.
+  VERIFY( counter_type::move_assign_count == 0  );
+}
+
+void test02()
+{
+  bool test __attribute__((unused)) = true;
+  typedef propagating_allocator<counter_type, true> alloc_type;
+  typedef __gnu_test::counter_type_hasher hash;
+  typedef std::unordered_multimap<counter_type, counter_type, hash,
+				  std::equal_to<counter_type>,
+				  alloc_type> test_type;
+
+  test_type v1(alloc_type(1));
+  v1.emplace(std::piecewise_construct,
+	     std::make_tuple(1), std::make_tuple(1));
+
+  test_type v2(alloc_type(2));
+  v2.emplace(std::piecewise_construct,
+	     std::make_tuple(2), std::make_tuple(2));
+
+  counter_type::reset();
+
+  v2 = std::move(v1);
+
+  VERIFY(0 == v1.get_allocator().get_personality());
+  VERIFY(1 == v2.get_allocator().get_personality());
+
+  VERIFY( counter_type::move_assign_count == 0 );
+  VERIFY( counter_type::destructor_count == 2 );
+}
+
+int main()
+{
+  test01();
+  test02();
+  return 0;
+}
