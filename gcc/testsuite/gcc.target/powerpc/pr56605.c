bd3b883d7962973205700556cbd9dc9b10c05190
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2013-04-23 19:36:20 +0000

    backport: re PR rtl-optimization/56605 (Redundant branch introduced during loop2 phases)
    gcc:
    
    2013-04-23  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    	    Steven Bosscher <steven@gcc.gnu.org>
    
    	Backported from mainline.
    	PR rtl-optimization/56605
    	* loop-iv.c (implies_p): Handle equal RTXs and subregs.
    
    gcc/testsuite:
    
    2013-04-23  Bill Schmidt  wschmidt@linux.vnet.ibm.com>
    
    	Backported from mainline.
    	PR rtl-optimization/56605
    	* gcc.target/powerpc/pr56605.c: New.
    
    Co-Authored-By: Steven Bosscher <steven@gcc.gnu.org>
    
    From-SVN: r198207

---
+++ gcc/testsuite/gcc.target/powerpc/pr56605.c
@@ -0,0 +1,13 @@
+/* PR rtl-optimization/56605 */
+/* { dg-do compile { target { powerpc64-*-* && lp64 } } } */
+/* { dg-options "-O3 -mvsx -mcpu=power7 -fno-unroll-loops -fdump-rtl-loop2_doloop" } */
+
+void foo (short* __restrict sb, int* __restrict ia)
+{
+  int i;
+  for (i = 0; i < 4000; i++)
+    ia[i] = (int) sb[i];
+}
+
+/* { dg-final { scan-rtl-dump-times "\\\(compare:CC \\\(subreg:SI \\\(reg:DI" 1 "loop2_doloop" } } */
+/* { dg-final { cleanup-rtl-dump "loop2_doloop" } } */
