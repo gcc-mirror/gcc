1592b4df4599dc4b92db63426c472d7c445c5b75
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-04-25 10:42:01 +0000

    2013-04-25  Hristian Kirtchev  <kirtchev@adacore.com>
    	* exp_ch6.adb: Add with and use clause for Stringt.
    	(Expand_Contract_Cases): Moved from sem_ch6. Add formal parameters
    	Decls and Stmts along with comments on their usage.
    	* exp_ch6.ads (Expand_Contract_Cases): Moved from sem_ch6.
    	* sem_ch6.adb (Expand_Contract_Cases): Moved to exp_ch6.
    	(Process_Contract_Cases): Update the call to Expand_Contract_Cases.
    
    2013-04-25  Ed Schonberg  <schonberg@adacore.com>
    
    	* gnat_rm.texi: Minor editing, to clarify use of dimension aspects.
    	* sem_util.adb (Is_OK_Variable_For_Out_Formal): Reject an
    	aggregate for a packed type, which may be converted into an
    	unchecked conversion of an object.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@198292 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/exp_ch6.ads
+++ gcc/ada/exp_ch6.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---          Copyright (C) 1992-2012, Free Software Foundation, Inc.         --
+--          Copyright (C) 1992-2013, Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -70,6 +70,17 @@ package Exp_Ch6 is
    procedure Expand_Call (N : Node_Id);
    --  This procedure contains common processing for Expand_N_Function_Call,
    --  Expand_N_Procedure_Statement, and Expand_N_Entry_Call.
+
+   procedure Expand_Contract_Cases
+     (CCs     : Node_Id;
+      Subp_Id : Entity_Id;
+      Decls   : List_Id;
+      Stmts   : in out List_Id);
+   --  Given pragma Contract_Cases CCs, create the circuitry needed to evaluate
+   --  case guards and trigger consequence expressions. Subp_Id is the related
+   --  subprogram for which the pragma applies. Decls are the declarations of
+   --  Subp_Id's body. All generated code is added to list Stmts. If Stmts is
+   --  empty, a new list is created.
 
    procedure Freeze_Subprogram (N : Node_Id);
    --  generate the appropriate expansions related to Subprogram freeze
