dec257d3c1c9b0741b89012dd3db07cc985ad3e6
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-05-01 19:19:44 +0000

    /cp 2013-05-01  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/57132
    	* pt.c (tsubst_copy_and_build, MODOP_EXPR): Increase / decrease
    	c_inhibit_evaluation_warnings around build_x_modify_expr call.
    
    /testsuite
    2013-05-01  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/57132
    	* g++.dg/warn/Wdiv-by-zero-bogus-2.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@198504 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -13810,7 +13810,11 @@ tsubst_copy_and_build (tree t,
 
     case MODOP_EXPR:
       {
-	tree r = build_x_modify_expr
+	tree r;
+
+	++c_inhibit_evaluation_warnings;
+
+	r = build_x_modify_expr
 	  (EXPR_LOCATION (t),
 	   RECUR (TREE_OPERAND (t, 0)),
 	   TREE_CODE (TREE_OPERAND (t, 1)),
@@ -13824,6 +13828,9 @@ tsubst_copy_and_build (tree t,
 	   here.  */
 	if (TREE_NO_WARNING (t))
 	  TREE_NO_WARNING (r) = TREE_NO_WARNING (t);
+
+	--c_inhibit_evaluation_warnings;
+
 	RETURN (r);
       }
 
