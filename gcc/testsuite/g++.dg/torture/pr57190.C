587a19f1d7632728aea48f6b365cc38271b1954b
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-05-07 11:23:39 +0000

    2013-05-07  Richard Biener  <rguenther@suse.de>
    	PR middle-end/57190
    	* tree-eh.c (sink_clobbers): Properly propagate
    	SSA_NAME_OCCURS_IN_ABNORMAL_PHI.
    
    	* g++.dg/torture/pr57190.C: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@198675 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/torture/pr57190.C
@@ -0,0 +1,42 @@
+// { dg-do compile }
+
+namespace __gnu_cxx __attribute__ ((__visibility__ ("default"))) {
+    template<typename _Tp>     class new_allocator     {
+    };
+}
+namespace std {
+    template<typename>     class allocator;
+    template<class _CharT>     struct char_traits;
+    template<typename _CharT, typename _Traits = char_traits<_CharT>,            typename _Alloc = allocator<_CharT> >     class basic_string;
+    typedef basic_string<char> string;
+    template<typename _Tp>     class allocator: public __gnu_cxx::new_allocator<_Tp>     {
+    };
+    template<typename _CharT, typename _Traits, typename _Alloc>     class basic_string     {
+    public:
+	basic_string(const _CharT* __s, const _Alloc& __a = _Alloc());
+    };
+}
+class UIException {
+};
+class PasswordDialog {
+    void run() throw (UIException);
+};
+class MessageBox  {
+public:
+    MessageBox (std::string t) throw (UIException);
+    virtual int run() throw (UIException) ;
+};
+extern "C" {
+    struct __jmp_buf_tag   {
+    };
+    extern int __sigsetjmp (struct __jmp_buf_tag __env[1], int __savemask) throw ();
+    typedef struct __jmp_buf_tag sigjmp_buf[1];
+}
+sigjmp_buf password_dialog_sig_jmp_buf;
+void PasswordDialog::run() throw (UIException)
+{
+  __sigsetjmp (password_dialog_sig_jmp_buf, 1);
+  MessageBox* errmsg = __null;
+  errmsg = new MessageBox ("E R R O R");
+  errmsg->run();
+}
