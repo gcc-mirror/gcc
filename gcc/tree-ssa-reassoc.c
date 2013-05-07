9bf3047c94b9e546ced4912c29216f9a291a7646
Richard Biener <rguenth@gcc.gnu.org>
2013-05-07 14:43:17 +0000

    backport: re PR tree-optimization/57000 (ICE with -Ofast and -frounding-math)
    2013-05-07  Richard Biener  <rguenther@suse.de>
    
    	Backport from mainline
    	2013-04-19  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/57000
    	* tree-ssa-reassoc.c (pass_reassoc): Add TODO_update_ssa_only_virtuals.
    
    	* gcc.dg/tree-ssa/reassoc-27.c: New testcase.
    
    From-SVN: r198685

--- gcc/tree-ssa-reassoc.c
+++ gcc/tree-ssa-reassoc.c
@@ -4293,6 +4293,7 @@ struct gimple_opt_pass pass_reassoc =
   0,					/* properties_destroyed */
   0,					/* todo_flags_start */
   TODO_verify_ssa
+    | TODO_update_ssa_only_virtuals
     | TODO_verify_flow
     | TODO_ggc_collect			/* todo_flags_finish */
  }
