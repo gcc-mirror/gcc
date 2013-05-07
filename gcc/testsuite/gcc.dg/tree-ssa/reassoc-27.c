6cfae9666fb5b6dced069340c31ebb65bf877673
Richard Biener <rguenther@suse.de>
2013-05-07 08:11:46 +0000

    backport: [multiple changes]
    2013-05-07  Richard Biener  <rguenther@suse.de>
    
    	Backport from mainline
    	2013-05-06  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/57185
    	* tree-parloops.c (add_field_for_reduction): Handle anonymous
    	SSA names properly.
    
    	* gcc.dg/autopar/pr57185.c: New testcase.
    
    	2013-04-19  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/57000
    	* tree-ssa-reassoc.c (pass_reassoc): Add TODO_update_ssa_only_virtuals.
    
    	* gcc.dg/tree-ssa/reassoc-27.c: New testcase.
    
    From-SVN: r198665

---
+++ gcc/testsuite/gcc.dg/tree-ssa/reassoc-27.c
@@ -0,0 +1,11 @@
+/* { dg-do compile } */
+/* { dg-options "-O -ffast-math -frounding-math -fdump-tree-optimized" } */
+
+double baz (double foo, double bar)
+{
+  return foo * foo * foo * foo * bar * bar * bar * bar;
+}
+
+/* We should re-associate this as (foo * bar)**3.  */
+/* { dg-final { scan-tree-dump-times " \\\* " 3 "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
