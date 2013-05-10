3e2377d6d62b0c7aebb599db7715b27b9168f4b1
Alan Modra <amodra@gmail.com>
2013-05-10 11:24:06 +0930

    re PR target/55033 (PowerPC section type conflict error)
    	PR target/55033
    	* varasm.c (default_elf_select_section): Move !DECL_P check..
    	(get_named_section): ..to here before calling get_section_name.
    	Adjust assertion.
    	(default_section_type_flags): Add DECL_P check.
    	* config/i386/winnt.c (i386_pe_section_type_flags): Likewise.
    	* config/rs6000/rs6000.c (rs6000_xcoff_section_type_flags): Likewise.
    
    From-SVN: r198763

--- gcc/varasm.c
+++ gcc/varasm.c
@@ -402,12 +402,16 @@ get_named_section (tree decl, const char *name, int reloc)
 {
   unsigned int flags;
 
-  gcc_assert (!decl || DECL_P (decl));
   if (name == NULL)
-    name = TREE_STRING_POINTER (DECL_SECTION_NAME (decl));
+    {
+      gcc_assert (decl && DECL_P (decl) && DECL_SECTION_NAME (decl));
+      name = TREE_STRING_POINTER (DECL_SECTION_NAME (decl));
+    }
 
   flags = targetm.section_type_flags (decl, name, reloc);
 
+  if (decl && !DECL_P (decl))
+    decl = NULL_TREE;
   return get_section (name, flags, decl);
 }
 
@@ -5989,7 +5993,7 @@ default_section_type_flags (tree decl, const char *name, int reloc)
 	flags |= SECTION_RELRO;
     }
 
-  if (decl && DECL_ONE_ONLY (decl))
+  if (decl && DECL_P (decl) && DECL_ONE_ONLY (decl))
     flags |= SECTION_LINKONCE;
 
   if (decl && TREE_CODE (decl) == VAR_DECL && DECL_THREAD_LOCAL_P (decl))
@@ -6348,8 +6352,6 @@ default_elf_select_section (tree decl, int reloc,
       gcc_unreachable ();
     }
 
-  if (!DECL_P (decl))
-    decl = NULL_TREE;
   return get_named_section (decl, sname, reloc);
 }
 
