783faa4728c59b3ec494ebfc1a6a08597c085756
Jason Merrill <jason@redhat.com>
2013-05-13 15:32:35 -0400

    re PR c++/57041 (ICE in lookup_field_1, at cp/search.c:376 (with dot-prefixed structure initialisation))
    	PR c++/57041
    	* decl.c (reshape_init_class): Handle error_mark_node.
    
    From-SVN: r198844

---
+++ gcc/testsuite/g++.dg/ext/desig6.C
@@ -0,0 +1,18 @@
+// PR c++/57041
+// { dg-options "-std=gnu++11" }
+// { dg-prune-output "error:" }
+
+template<typename T>
+union u {
+  T a;
+  char b;
+};
+
+template<typename T>
+u<T> make_u(T t) {
+  return { .a = t };
+}
+
+int main() {
+  return make_u<int>(1).a;
+}
