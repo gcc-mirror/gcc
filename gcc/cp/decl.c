18dc5657de4d850295c3c8662520b256b00ce1af
Jason Merrill <jason@redhat.com>
2013-05-13 15:32:22 -0400

    re PR c++/57253 (GCC ignores ref-qualifiers of pseudo-function types in explicit specializations)
    	PR c++/57253
    	* decl.c (grokdeclarator): Apply ref-qualifier
    	in the TYPENAME case.
    
    From-SVN: r198842

--- gcc/cp/decl.c
+++ gcc/cp/decl.c
@@ -10284,7 +10284,7 @@ grokdeclarator (const cp_declarator *declarator,
 	      type = void_type_node;
 	    }
 	}
-      else if (memfn_quals)
+      else if (memfn_quals || rqual)
 	{
 	  if (ctype == NULL_TREE
 	      && TREE_CODE (type) == METHOD_TYPE)
