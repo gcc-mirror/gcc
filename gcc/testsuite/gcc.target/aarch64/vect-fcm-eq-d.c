fc21784dec6ab2642059b2a988af355a08cbba1a
James Greenhalgh <james.greenhalgh@arm.com>
2013-05-14 14:56:13 +0000

    [AArch64] Fix vcond where comparison and result have different types.
    gcc/
    
    	* config/aarch64/aarch64-simd.md
    	(aarch64_vcond_internal<mode>): Rename to...
    	(aarch64_vcond_internal<mode><mode>): ...This, for integer modes.
    	(aarch64_vcond_internal<VDQF_COND:mode><VDQF:mode>): ...This for
    	float modes. Clarify all iterator modes.
    	(vcond<mode><mode>): Use new name for vcond expanders.
    	(vcond<v_cmp_result><mode>): Likewise.
    	(vcondu<mode><mode>: Likewise.
    	* config/aarch64/iterators.md (VDQF_COND): New.
    
    gcc/testsuite/
    
    	* gcc.target/aarch64/vect-fcm.x: Add cases testing
    	FLOAT cmp FLOAT ? INT : INT.
    	 * gcc.target/aarch64/vect-fcm-eq-d.c: Define IMODE.
    	 * gcc.target/aarch64/vect-fcm-eq-f.c: Likewise.
    	 * gcc.target/aarch64/vect-fcm-ge-d.c: Likewise.
    	 * gcc.target/aarch64/vect-fcm-ge-f.c: Likewise.
    	 * gcc.target/aarch64/vect-fcm-gt-d.c: Likewise.
    	 * gcc.target/aarch64/vect-fcm-gt-f.c: Likewise.
    
    From-SVN: r198890

--- gcc/testsuite/gcc.target/aarch64/vect-fcm-eq-d.c
+++ gcc/testsuite/gcc.target/aarch64/vect-fcm-eq-d.c
@@ -2,12 +2,13 @@
 /* { dg-options "-O2 -ftree-vectorize -fdump-tree-vect-all -fno-unroll-loops --save-temps -fno-inline" } */
 
 #define FTYPE double
+#define ITYPE long
 #define OP ==
 #define INV_OP !=
 
 #include "vect-fcm.x"
 
-/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 4 "vect" } } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 8 "vect" } } */
 /* { dg-final { scan-assembler "fcmeq\\tv\[0-9\]+\.2d, v\[0-9\]+\.2d, v\[0-9\]+\.2d" } } */
 /* { dg-final { scan-assembler "fcmeq\\tv\[0-9\]+\.2d, v\[0-9\]+\.2d, 0" } } */
 /* { dg-final { cleanup-tree-dump "vect" } } */
