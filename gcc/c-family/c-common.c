8e71dad21a0bb28528d15a7c102642f75ec1bb24
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-05-14 20:50:43 +0000

    PR c++/57274 	* c-common.c (verify_tree): Don't recurse into SIZEOF_EXPR.
    	* c-c++-common/Wsequence-point-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@198903 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-family/c-common.c
+++ gcc/c-family/c-common.c
@@ -3032,6 +3032,7 @@ verify_tree (tree x, struct tlist **pbefore_sp, struct tlist **pno_sp,
   switch (code)
     {
     case CONSTRUCTOR:
+    case SIZEOF_EXPR:
       return;
 
     case COMPOUND_EXPR:
