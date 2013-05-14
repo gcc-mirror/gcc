2fcc3fb4dba7b617c41affff75d3ca84bdb3a68f
jgreenhalgh <jgreenhalgh@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-05-14 14:56:13 +0000

    [AArch64] Fix vcond where comparison and result have different types.
    gcc/
    
    	* config/aarch64/aarch64-simd.md
    	(aarch64_vcond_internal<mode>): Rename to...
    	(aarch64_vcond_internal<mode><mode>): ...This, for integer modes.
    	(aarch64_vcond_internal<VDQF_COND:mode><VDQF:mode>): ...This for
    	float modes. Clarify all iterator modes.
    	(vcond<mode><mode>): Use new name for vcond expanders.
    	(vcond<v_cmp_result><mode>): Likewise.
    	(vcondu<mode><mode>: Likewise.
    	* config/aarch64/iterators.md (VDQF_COND): New.
    
    gcc/testsuite/
    
    	* gcc.target/aarch64/vect-fcm.x: Add cases testing
    	FLOAT cmp FLOAT ? INT : INT.
    	 * gcc.target/aarch64/vect-fcm-eq-d.c: Define IMODE.
    	 * gcc.target/aarch64/vect-fcm-eq-f.c: Likewise.
    	 * gcc.target/aarch64/vect-fcm-ge-d.c: Likewise.
    	 * gcc.target/aarch64/vect-fcm-ge-f.c: Likewise.
    	 * gcc.target/aarch64/vect-fcm-gt-d.c: Likewise.
    	 * gcc.target/aarch64/vect-fcm-gt-f.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@198890 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/aarch64/iterators.md
+++ gcc/config/aarch64/iterators.md
@@ -83,6 +83,9 @@
 ;; Vector Float modes.
 (define_mode_iterator VDQF [V2SF V4SF V2DF])
 
+;; Modes suitable to use as the return type of a vcond expression.
+(define_mode_iterator VDQF_COND [V2SF V2SI V4SF V4SI V2DF V2DI])
+
 ;; All Float modes.
 (define_mode_iterator VALLF [V2SF V4SF V2DF SF DF])
 
--- gcc/testsuite/gcc.target/aarch64/vect-fcm-ge-f.c
+++ gcc/testsuite/gcc.target/aarch64/vect-fcm-ge-f.c
@@ -2,12 +2,13 @@
 /* { dg-options "-O2 -ftree-vectorize -fdump-tree-vect-all -fno-unroll-loops --save-temps -fno-inline" } */
 
 #define FTYPE float
+#define ITYPE int
 #define OP >=
 #define INV_OP <
 
 #include "vect-fcm.x"
 
-/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 4 "vect" } } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 8 "vect" } } */
 /* { dg-final { scan-assembler "fcmge\\tv\[0-9\]+\.\[24\]s, v\[0-9\]+\.\[24\]s, v\[0-9\]+\.\[24\]s" } } */
 /* { dg-final { scan-assembler "fcmge\\tv\[0-9\]+\.\[24\]s, v\[0-9\]+\.\[24\]s, 0" } } */
 /* { dg-final { scan-assembler "fcmlt\\tv\[0-9\]+\.\[24\]s, v\[0-9\]+\.\[24\]s, 0" } } */
