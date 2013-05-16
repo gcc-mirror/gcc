d99f53b2833efa65055cf44be047367c1c424c54
sje <sje@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-05-16 17:09:40 +0000

    2013-05-16  Steve Ellcey  <sellcey@imgtec.com>
    	* cfghooks.c (copy_bbs): Add update_dominance argument.
    	* cfghooks.h (copy_bbs): Update prototype.
    	* tree-cfg.c (gimple_duplicate_sese_region):
    	Add update_dominance argument.
    	* tree-flow.h (gimple_duplicate_sese_region): Update prototype.
    	* tree-ssa-loop-ch.c (copy_loop_headers): Update
    	gimple_duplicate_sese_region call.
    	* tree-vect-loop-manip.c (slpeel_tree_duplicate_loop_to_edge_cfg):
    	Update copy_bbs call.
    	* cfgloopmanip.c (duplicate_loop_to_header_edge): Ditto.
    	* trans-mem.c (ipa_uninstrument_transaction): Ditto.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@198980 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-flow.h
+++ gcc/tree-flow.h
@@ -395,7 +395,7 @@ extern void verify_gimple_in_cfg (struct function *);
 extern tree gimple_block_label (basic_block);
 extern void extract_true_false_edges_from_block (basic_block, edge *, edge *);
 extern bool gimple_duplicate_sese_region (edge, edge, basic_block *, unsigned,
-					basic_block *);
+					basic_block *, bool);
 extern bool gimple_duplicate_sese_tail (edge, edge, basic_block *, unsigned,
 				      basic_block *);
 extern void gather_blocks_in_sese_region (basic_block entry, basic_block exit,
