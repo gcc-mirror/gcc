a2e48e3caa2ca6871f174f873532128116a49be0
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-05-16 14:39:21 +0000

    Require section_anchors in gcc.dg/visibility-21.c
    	* gcc.dg/visibility-21.c: Require section_anchors.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@198974 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/visibility-21.c
+++ gcc/testsuite/gcc.dg/visibility-21.c
@@ -3,6 +3,7 @@
 /* { dg-options "-O2 -fsection-anchors" } */
 /* { dg-require-visibility "" } */
 /* { dg-require-weak "" } */
+/* { dg-require-effective-target section_anchors } */
 /* { dg-final { scan-assembler-not "ANCHOR" } } */
 
 int __attribute__((weak, visibility("hidden"))) weak_hidden[3];
