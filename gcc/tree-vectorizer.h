cb4792d2310b4138ccaad6bf9d1e557ddc6db2c9
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-05-16 16:24:19 +0000

    * tree-vectorizer.h (NUM_PATTERNS): Increment. 	* tree-vect-patterns.c (vect_vect_recog_func_ptrs): Add 	vect_recog_rotate_pattern. 	(vect_recog_rotate_pattern): New function.
    	* gcc.target/i386/rotate-3.c: New test.
    	* gcc.target/i386/rotate-3a.c: New test.
    	* gcc.target/i386/rotate-4.c: New test.
    	* gcc.target/i386/rotate-4a.c: New test.
    	* gcc.target/i386/rotate-5.c: New test.
    	* gcc.target/i386/rotate-5a.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@198978 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-vectorizer.h
+++ gcc/tree-vectorizer.h
@@ -1005,7 +1005,7 @@ extern void vect_slp_transform_bb (basic_block);
    Additional pattern recognition functions can (and will) be added
    in the future.  */
 typedef gimple (* vect_recog_func_ptr) (vec<gimple> *, tree *, tree *);
-#define NUM_PATTERNS 10
+#define NUM_PATTERNS 11
 void vect_pattern_recog (loop_vec_info, bb_vec_info);
 
 /* In tree-vectorizer.c.  */
