43bb4dd14d015211a19ccd8c1802cc7e1361d8b7
Marc Glisse <marc.glisse@inria.fr>
2013-05-16 12:26:51 +0200

    re PR middle-end/57286 (infinite recursion in fold-const.c:10037)
    2013-05-16  Marc Glisse  <marc.glisse@inria.fr>
    
    	PR middle-end/57286
    gcc/
    	* fold-const.c (fold_ternary_loc) <VEC_COND_EXPR>: Disable some
    	transformations to avoid an infinite loop.
    
    gcc/testsuite/
    	* gcc.dg/pr57286.c: New testcase.
    	* gcc.dg/vector-shift-2.c: Don't assume int has size 4.
    	* g++.dg/ext/vector22.C: Comment out transformations not
    	performed anymore.
    
    From-SVN: r198964

--- gcc/testsuite/g++.dg/ext/vector22.C
+++ gcc/testsuite/g++.dg/ext/vector22.C
@@ -3,13 +3,15 @@
 
 typedef unsigned vec __attribute__((vector_size(4*sizeof(int))));
 
+/* Disabled after PR57286
 void f(vec*a,vec*b){
   *a=(*a)?-1:(*b<10);
   *b=(*b)?(*a<10):0;
 }
+*/
 void g(vec*a,vec*b){
   *a=(*a)?(*a<*a):-1;
-  *b=(*b)?-1:(*b<*b);
+// *b=(*b)?-1:(*b<*b);
 }
 void h(vec*a){
   *a=(~*a==5);
