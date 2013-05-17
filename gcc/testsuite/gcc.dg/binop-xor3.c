0cf5320ed79975a1b2d61c9966605dfca9607edd
Marc Glisse <marc.glisse@inria.fr>
2013-05-17 17:31:02 +0200

    re PR testsuite/57313 (gcc.dg/binop-xor3.c scan-tree-dump-times)
    2013-05-17  Marc Glisse  <marc.glisse@inria.fr>
    
    	PR testsuite/57313
    	* gcc.dg/binop-xor3.c: Restrict to platforms known to work (x86).
    
    From-SVN: r199020

--- gcc/testsuite/gcc.dg/binop-xor3.c
+++ gcc/testsuite/gcc.dg/binop-xor3.c
@@ -7,5 +7,5 @@ foo (int a, int b)
   return ((a && !b) || (!a && b));
 }
 
-/* { dg-final { scan-tree-dump-times "\\\^" 1 "optimized" } } */
+/* { dg-final { scan-tree-dump-times "\\\^" 1 "optimized" { target i?86-*-* x86_64-*-* } } } */
 /* { dg-final { cleanup-tree-dump "optimized" } } */
