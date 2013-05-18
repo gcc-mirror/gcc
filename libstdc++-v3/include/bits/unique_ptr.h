ce5e50454e25423bede36e3f7c71ae44e4a0b570
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-05-18 15:07:02 +0000

    * include/bits/unique_ptr.h (make_unique): Define. 	* testsuite/20_util/unique_ptr/creation/single.cc: New. 	* testsuite/20_util/unique_ptr/creation/array.cc: New. 	* testsuite/20_util/unique_ptr/creation/array_neg.cc: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@199057 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/unique_ptr.h
+++ libstdc++-v3/include/bits/unique_ptr.h
@@ -607,6 +607,37 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       }
     };
 
+#if __cplusplus > 201103L
+  template<typename _Tp>
+    struct _MakeUniq
+    { typedef unique_ptr<_Tp> __single_object; };
+
+  template<typename _Tp>
+    struct _MakeUniq<_Tp[]>
+    { typedef unique_ptr<_Tp[]> __array; };
+
+  template<typename _Tp, size_t _Bound>
+    struct _MakeUniq<_Tp[_Bound]>
+    { struct __invalid_type { }; };
+
+  /// std::make_unique for single objects
+  template<typename _Tp, typename... _Args>
+    typename _MakeUniq<_Tp>::__single_object
+    make_unique(_Args&&... __args)
+    { return unique_ptr<_Tp>(new _Tp(std::forward<_Args>(__args)...)); }
+
+  /// std::make_unique for arrays of unknown bound
+  template<typename _Tp>
+    typename _MakeUniq<_Tp>::__array
+    make_unique(size_t __num)
+    { return unique_ptr<_Tp>(new typename remove_extent<_Tp>::type[__num]()); }
+
+  /// Disable std::make_unique for arrays of known bound
+  template<typename _Tp, typename... _Args>
+    typename _MakeUniq<_Tp>::__invalid_type
+    make_unique(_Args&&...) = delete;
+#endif
+
   // @} group pointer_abstractions
 
 _GLIBCXX_END_NAMESPACE_VERSION
---
+++ libstdc++-v3/testsuite/20_util/unique_ptr/creation/array.cc
@@ -0,0 +1,46 @@
+// { dg-options "-std=gnu++1y" }
+
+// Copyright (C) 2013 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 20.9.1.4 unique_ptr creation [unique.ptr.create]
+
+#include <memory>
+#include <testsuite_hooks.h>
+
+struct A
+{
+  A() : b(true) { }
+  A(int) : b(false) { }
+  bool b;
+};
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  std::unique_ptr<A[]> a = std::make_unique<A[]>(3);
+  VERIFY( a != nullptr );
+  VERIFY( a[0].b && a[1].b && a[2].b );
+}
+
+int
+main()
+{
+  test01();
+}
