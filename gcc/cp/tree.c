c6dc04853f88e6b09b8e08e4b0b839fbf7c18256
Jason Merrill <jason@redhat.com>
2013-05-20 13:01:01 -0400

    re PR c++/57325 (ICE in strip_typedefs, at cp/tree.c:1306)
    	PR c++/57325
    	* tree.c (build_cplus_array_type): Copy layout info if element
    	type is complete.
    
    From-SVN: r199112

--- gcc/cp/tree.c
+++ gcc/cp/tree.c
@@ -816,10 +816,12 @@ build_cplus_array_type (tree elt_type, tree index_type)
 
       if (TYPE_MAIN_VARIANT (t) != m)
 	{
-	  if (COMPLETE_TYPE_P (t) && !COMPLETE_TYPE_P (m))
+	  if (COMPLETE_TYPE_P (TREE_TYPE (t)) && !COMPLETE_TYPE_P (m))
 	    {
 	      /* m was built before the element type was complete, so we
-		 also need to copy the layout info from t.  */
+		 also need to copy the layout info from t.  We might
+	         end up doing this multiple times if t is an array of
+	         unknown bound.  */
 	      tree size = TYPE_SIZE (t);
 	      tree size_unit = TYPE_SIZE_UNIT (t);
 	      unsigned int align = TYPE_ALIGN (t);
