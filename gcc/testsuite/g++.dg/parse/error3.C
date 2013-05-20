65b7c3044e1e355984408d457ddad933af288fa6
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-05-20 17:13:47 +0000

    /cp 2013-05-20  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/12288
    	* parser.c (cp_parser_parameter_declaration): Check return value
    	of cp_parser_parse_and_diagnose_invalid_type_name.
    
    /testsuite
    2013-05-20  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/12288
    	* g++.dg/parse/error52.C: New.
    	* g++.dg/parse/error3.C: Adjust.
    	* g++.dg/parse/error36.C: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@199115 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/parse/error3.C
+++ gcc/testsuite/g++.dg/parse/error3.C
@@ -5,5 +5,4 @@ static void InstantiateConstraint(const float&, unsigned,
                                   void(*AddFunction)(const TYPE&,bool&,
                                                      char*, char*,
                                                      unsigned*));
-// { dg-error "64: ISO C\\+\\+ forbids declaration of 'parameter' with no type" "forbids" { target *-*-* }  { 5 } }
 // { dg-error "60: 'TYPE' does not name a type" "does not" { target *-*-* } { 5 } }
