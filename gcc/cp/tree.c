39bde5eae367fc1e57d277ea4496c485a3b9c09c
Jason Merrill <jason@redhat.com>
2013-05-23 16:51:22 -0400

    re PR c++/57388 ([C++11] ICE when function types with ref-qualifiers meet other function types)
    	PR c++/57388
    	* tree.c (build_ref_qualified_type): Clear
    	FUNCTION_RVALUE_QUALIFIED for lvalue ref-qualifier.
    
    From-SVN: r199269

--- gcc/cp/tree.c
+++ gcc/cp/tree.c
@@ -1769,8 +1769,10 @@ build_ref_qualified_type (tree type, cp_ref_qualifier rqual)
     {
     case REF_QUAL_RVALUE:
       FUNCTION_RVALUE_QUALIFIED (t) = 1;
-      /* Intentional fall through */
+      FUNCTION_REF_QUALIFIED (t) = 1;
+      break;
     case REF_QUAL_LVALUE:
+      FUNCTION_RVALUE_QUALIFIED (t) = 0;
       FUNCTION_REF_QUALIFIED (t) = 1;
       break;
     default:
