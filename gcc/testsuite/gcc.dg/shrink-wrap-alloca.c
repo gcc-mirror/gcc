3d6cc50a464cf3d003a97442a33f8a1b4c3ffd79
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2013-05-31 11:32:43 +0000

    Fix gcc.dg/shrink-wrap-alloca.c
    	* gcc.dg/shrink-wrap-alloca.c: Use __builtin_alloca.
    
    From-SVN: r199533

--- gcc/testsuite/gcc.dg/shrink-wrap-alloca.c
+++ gcc/testsuite/gcc.dg/shrink-wrap-alloca.c
@@ -1,13 +1,11 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -g" } */
 
-extern int * alloca (int);
-
 int *p;
 
 void
 test (int a)
 {
   if (a > 0)
-    p = alloca (4);
+    p = __builtin_alloca (4);
 }
