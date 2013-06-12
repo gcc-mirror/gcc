5d96d49002516991029e83c4ff38cdca3d88b941
Tobias Burnus <burnus@net-b.de>
2013-06-12 15:26:49 +0200

    finalize_10.f90: Update scan-tree-dump.
    2013-06-12  Tobias Burnus  <burnus@net-b.de>
    
            * gfortran.dg/finalize_10.f90: Update scan-tree-dump.
    
    
    Co-Authored-By: Dominique d'Humieres <dominiq@lps.ens.fr>
    
    From-SVN: r200006

--- gcc/testsuite/gfortran.dg/finalize_10.f90
+++ gcc/testsuite/gfortran.dg/finalize_10.f90
@@ -26,7 +26,7 @@ end subroutine foo
 
 ! Finalize CLASS + set default init
 ! { dg-final { scan-tree-dump-times "y->_vptr->_final \\(&desc.\[0-9\]+, y->_vptr->_size, 0\\);" 1 "original" } }
-! { dg-final { scan-tree-dump       "__builtin_memcpy \\(\\(void .\\) y->_data, \\(void .\\) y->_vptr->_def_init, \\(unsigned (long|int)\\) y->_vptr->_size\\);" "original" } }
+! { dg-final { scan-tree-dump       "__builtin_memcpy \\(\\(void .\\) y->_data, \\(void .\\) y->_vptr->_def_init, \\((unsigned long|unsigned int|character\\(kind=4\\))\\) y->_vptr->_size\\);" "original" } }
 ! { dg-final { scan-tree-dump-times "x->_vptr->_final \\(&x->_data, x->_vptr->_size, 0\\);" 1 "original" } }
 ! { dg-final { scan-tree-dump-times "x->_vptr->_copy \\(x->_vptr->_def_init, &x->_data\\);" 1 "original" } }
 
