e3f9e0acec78644ef45ec984b062c856fc53a9e0
Wei Mi <wmi@google.com>
2013-06-19 21:57:42 +0000

    re PR rtl-optimization/57518 (Redundant insn generated in LRA)
    2013-06-19  Wei Mi  <wmi@google.com>
    
    	PR rtl-optimization/57518
    	* ira.c (set_paradoxical_subreg): Set pdx_subregs[regno]
    	if regno is used in paradoxical subreg.
    	(update_equiv_regs): Check pdx_subregs[regno] before
    	set a reg to be equivalent with a mem.
    
    From-SVN: r200224

---
+++ gcc/testsuite/gcc.dg/pr57518.c
@@ -0,0 +1,15 @@
+/* PR rtl-optimization/57130 */
+
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-rtl-ira" } */
+/* { dg-final { scan-rtl-dump-not "REG_EQUIV.*mem.*\"ip\"" "ira" } } */
+
+char ip[10];
+int total;
+
+void foo() {
+  int t;
+
+  t = ip[2];
+  total = t & 0x3;
+}
