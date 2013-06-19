e997eed019c52743990b0889b4b26061179fcd1f
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-06-19 08:49:59 +0000

    /cp 2013-06-19  Manuel Lopez-Ibanez  <manu@gcc.gnu.org>
    	PR c++/57638
    	* pt.c (unify, [TEMPLATE_PARM_INDEX]): Pass to unify_type_mismatch
    	TREE_TYPE (arg), not arg itself.
    
    /testsuite
    2013-06-19  Manuel Lopez-Ibanez  <manu@gcc.gnu.org>
    
    	PR c++/57638
    	* g++.dg/template/error53.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@200192 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/error53.C
@@ -0,0 +1,13 @@
+// PR c++/57638
+
+template<int x>
+struct S {};
+
+template<long long i>
+void g(S<i>);
+
+void f()
+{
+  S<1000> t;
+  g(t);         // { dg-error "no matching" }
+}  // { dg-message "mismatched types 'long long int' and 'int'" "" { target *-*-* } 12 }
