16bc66ec4914124c4bf89513f7624df7b6fa3317
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-06-19 14:06:53 +0000

    * tree-ssa-forwprop.c (simplify_bitwise_binary_boolean): New function. 	(simplify_bitwise_binary): Use it to simpify certain binary ops on 	booleans.
    	* gcc.dg/tree-ssa/forwprop-28.c: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@200201 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/forwprop-28.c
@@ -0,0 +1,76 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-forwprop1" } */
+
+extern char *frob (void);
+extern _Bool testit (void);
+
+test (int code)
+{
+  char *temp = frob ();
+  int rotate = (code == 22);
+  if (temp == 0 && !rotate)
+    oof ();
+}
+
+test_2 (int code)
+{
+  char *temp = frob ();
+  int rotate = (code == 22);
+  if (!rotate && temp == 0)
+    oof ();
+}
+
+
+test_3 (int code)
+{
+  char *temp = frob ();
+  int rotate = (code == 22);
+  if (!rotate || temp == 0)
+    oof ();
+}
+
+
+test_4 (int code)
+{
+  char *temp = frob ();
+  int rotate = (code == 22);
+  if (temp == 0 || !rotate)
+    oof ();
+}
+
+
+test_5 (int code)
+{
+  _Bool temp = testit ();
+  _Bool rotate = (code == 22);
+  if (temp == 0 && !rotate)
+    oof ();
+}
+
+test_6 (int code)
+{
+  _Bool temp = testit ();
+  _Bool rotate = (code == 22);
+  if (!rotate && temp == 0)
+    oof ();
+}
+
+
+test_7 (int code)
+{
+  _Bool temp = testit ();
+  _Bool rotate = (code == 22);
+  if (!rotate || temp == 0)
+    oof ();
+}
+
+
+test_8 (int code)
+{
+  _Bool temp = testit ();
+  _Bool rotate = (code == 22);
+  if (temp == 0 || !rotate)
+    oof ();
+}
+
+/* { dg-final { scan-tree-dump-times "Replaced" 8 "forwprop1"} } */
