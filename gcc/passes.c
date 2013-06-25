8a845901e416539127df54145b12e21f478a073e
Richard Biener <rguenther@suse.de>
2013-06-25 08:21:31 +0000

    re PR middle-end/56977 (gcc -Og incorrectly warns about 'constant zero length parameter')
    2013-06-25  Richard Biener  <rguenther@suse.de>
    
    	PR middle-end/56977
    	* passes.c (init_optimization_passes): Move pass_fold_builtins
    	and pass_dce earlier with -Og.
    
    	* gcc.dg/pr56977.c: New testcase.
    
    From-SVN: r200391

--- gcc/passes.c
+++ gcc/passes.c
@@ -1536,15 +1536,15 @@ init_optimization_passes (void)
       /* Perform simple scalar cleanup which is constant/copy propagation.  */
       NEXT_PASS (pass_ccp);
       NEXT_PASS (pass_object_sizes);
+      /* Fold remaining builtins.  */
+      NEXT_PASS (pass_fold_builtins);
       /* Copy propagation also copy-propagates constants, this is necessary
-         to forward object-size results properly.  */
+         to forward object-size and builtin folding results properly.  */
       NEXT_PASS (pass_copy_prop);
+      NEXT_PASS (pass_dce);
       NEXT_PASS (pass_asan);
       NEXT_PASS (pass_tsan);
       NEXT_PASS (pass_rename_ssa_copies);
-      NEXT_PASS (pass_dce);
-      /* Fold remaining builtins.  */
-      NEXT_PASS (pass_fold_builtins);
       /* ???  We do want some kind of loop invariant motion, but we possibly
          need to adjust LIM to be more friendly towards preserving accurate
 	 debug information here.  */
