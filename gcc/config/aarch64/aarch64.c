9c023bf0bdfdcf26de6013f2a3692ac32511aa3e
Yufeng Zhang <yufeng.zhang@arm.com>
2013-06-27 12:30:11 +0000

    aarch64.c (aarch64_add_offset): Change to pass 'mode' to aarch64_plus_immediate and gen_rtx_PLUS.
    gcc/
    
    	* config/aarch64/aarch64.c (aarch64_add_offset): Change to pass
    	'mode' to aarch64_plus_immediate and gen_rtx_PLUS.
    
    From-SVN: r200466

--- gcc/config/aarch64/aarch64.c
+++ gcc/config/aarch64/aarch64.c
@@ -804,7 +804,7 @@ aarch64_force_temporary (rtx x, rtx value)
 static rtx
 aarch64_add_offset (enum machine_mode mode, rtx temp, rtx reg, HOST_WIDE_INT offset)
 {
-  if (!aarch64_plus_immediate (GEN_INT (offset), DImode))
+  if (!aarch64_plus_immediate (GEN_INT (offset), mode))
     {
       rtx high;
       /* Load the full offset into a register.  This
@@ -812,7 +812,7 @@ aarch64_add_offset (enum machine_mode mode, rtx temp, rtx reg, HOST_WIDE_INT off
       high = GEN_INT (offset);
       offset = 0;
       high = aarch64_force_temporary (temp, high);
-      reg = aarch64_force_temporary (temp, gen_rtx_PLUS (Pmode, high, reg));
+      reg = aarch64_force_temporary (temp, gen_rtx_PLUS (mode, high, reg));
     }
   return plus_constant (mode, reg, offset);
 }
