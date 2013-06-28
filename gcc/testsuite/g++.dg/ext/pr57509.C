ee429bdfe0a07195e96d5343bf534fc2e1753613
Marc Glisse <marc.glisse@inria.fr>
2013-06-28 12:31:39 +0200

    re PR c++/57509 (Segmentation fault when using __builtin_shuffle in templated class.)
    2013-06-28  Marc Glisse  <marc.glisse@inria.fr>
    
    	PR c++/57509
    	* g++.dg/ext/pr57509.C: Pass vectors by reference to avoid warnings.
    
    From-SVN: r200511

--- gcc/testsuite/g++.dg/ext/pr57509.C
+++ gcc/testsuite/g++.dg/ext/pr57509.C
@@ -4,8 +4,8 @@
 template <bool> struct enable_if {};
 template <> struct enable_if<true> {typedef void type;};
 template <class T> void f (T& v) { v = __builtin_shuffle (v, v); }
-template <class T> void g (T) {}
-template <class T> auto g (T x) -> typename enable_if<sizeof(__builtin_shuffle(x,x))!=2>::type {}
+template <class T> void g (T const&) {}
+template <class T> auto g (T const& x) -> typename enable_if<sizeof(__builtin_shuffle(x,x))!=2>::type {}
 typedef int v4i __attribute__((vector_size(4*sizeof(int))));
 typedef float v4f __attribute__((vector_size(4*sizeof(float))));
 int main(){
