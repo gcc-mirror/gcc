17a819cb0d6becdfe32f6b26164fb14b77d96369
Yufeng Zhang <yufeng.zhang@arm.com>
2013-07-23 12:12:53 +0000

    [AArch64, ILP32] 1/6 Initial support - configury changes
    gcc/
    	* config.gcc (aarch64*-*-*): Support --with-abi.
    	(aarch64*-*-elf): Support --with-multilib-list.
    	(aarch64*-*-linux*): Likewise.
    	(supported_defaults): Add abi to aarch64*-*-*.
    	* configure.ac: Mention AArch64 for --with-multilib-list.
    	* configure: Re-generated.
    	* config/aarch64/biarchilp32.h: New file.
    	* config/aarch64/biarchlp64.h: New file.
    	* config/aarch64/aarch64-elf.h (ENDIAN_SPEC): New define.
    	(ABI_SPEC): Ditto.
    	(MULTILIB_DEFAULTS): Ditto.
    	(DRIVER_SELF_SPECS): Ditto.
    	(ASM_SPEC): Update to also substitute -mabi.
    	* config/aarch64/aarch64-elf-raw.h (LINK_SPEC): Add linker script
    	file whose name depends on -mabi= and -mbig-endian.
    	* config/aarch64/aarch64.h (LONG_TYPE_SIZE): Change to depend on
    	TARGET_ILP32.
    	(POINTER_SIZE): New define.
    	(POINTERS_EXTEND_UNSIGNED): Ditto.
    	(enum aarch64_abi_type): New enumeration tag.
    	(AARCH64_ABI_LP64, AARCH64_ABI_ILP32): New enumerators.
    	(AARCH64_ABI_DEFAULT): Define to AARCH64_ABI_LP64 if undefined.
    	(TARGET_ILP32): New define.
    	* config/aarch64/aarch64.opt (mabi): New.
    	(aarch64_abi): New.
    	(ilp32, lp64): New values for -mabi.
    	* config/aarch64/t-aarch64 (comma): New define.
    	(MULTILIB_OPTIONS): Ditto.
    	(MULTILIB_DIRNAMES): Ditto.
    	* config/aarch64/t-aarch64-linux (MULTIARCH_DIRNAME): New define.
    	* doc/invoke.texi: Document -mabi for AArch64.
    
    From-SVN: r201164

--- gcc/config/aarch64/aarch64-elf-raw.h
+++ gcc/config/aarch64/aarch64-elf-raw.h
@@ -26,7 +26,8 @@
 #define ENDFILE_SPEC " crtend%O%s crtn%O%s"
 
 #ifndef LINK_SPEC
-#define LINK_SPEC "%{mbig-endian:-EB} %{mlittle-endian:-EL} -X"
+#define LINK_SPEC "%{mbig-endian:-EB} %{mlittle-endian:-EL} -X \
+  -maarch64elf%{mabi=ilp32*:32}%{mbig-endian:b}"
 #endif
 
 #endif /* GCC_AARCH64_ELF_RAW_H */
--- gcc/config/aarch64/aarch64-elf.h
+++ gcc/config/aarch64/aarch64-elf.h
@@ -111,12 +111,36 @@
 
 #define GLOBAL_ASM_OP "\t.global\t"
 
+#ifdef TARGET_BIG_ENDIAN_DEFAULT
+#define ENDIAN_SPEC "-mbig-endian"
+#else
+#define ENDIAN_SPEC "-mlittle-endian"
+#endif
+
+#if TARGET_DATA_MODEL == 1
+#define ABI_SPEC  "-mabi=lp64"
+#define MULTILIB_DEFAULTS { "mabi=lp64" }
+#elif TARGET_DATA_MODEL == 2
+#define ABI_SPEC  "-mabi=ilp32"
+#define MULTILIB_DEFAULTS { "mabi=ilp32" }
+#else
+#error "Unknown or undefined TARGET_DATA_MODEL!"
+#endif
+
+/* Force the default endianness and ABI flags onto the command line
+   in order to make the other specs easier to write.  */
+#undef DRIVER_SELF_SPECS
+#define DRIVER_SELF_SPECS \
+  " %{!mbig-endian:%{!mlittle-endian:" ENDIAN_SPEC "}}" \
+  " %{!mabi=*:" ABI_SPEC "}"
+
 #ifndef ASM_SPEC
 #define ASM_SPEC "\
 %{mbig-endian:-EB} \
 %{mlittle-endian:-EL} \
 %{mcpu=*:-mcpu=%*} \
-%{march=*:-march=%*}"
+%{march=*:-march=%*} \
+%{mabi=*:-mabi=%*}"
 #endif
 
 #undef TYPE_OPERAND_FMT
