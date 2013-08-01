46549c206e2970b76405f038399e7b2f42e0cc70
Maxim Kuvyrkov <maxim@kugelworks.com>
2013-08-01 21:09:10 +0000

    Fix licenses on several libgcc files.
    	* config/aarch64/sfp-machine.h, config/aarch64/sync-cache.c,
    	* config/i386/cpuinfo.c, config/ia64/unwind-ia64.h,
    	* config/mips/vr4120-div.S: Fix license from GPL-3.0+ to
    	GPL-3.0-with-GCC-exception.
    
    From-SVN: r201417

--- libgcc/config/ia64/unwind-ia64.h
+++ libgcc/config/ia64/unwind-ia64.h
@@ -2,21 +2,26 @@
    Contributed by Andrew MacLeod  <amacleod@cygnus.com>
                   Andrew Haley  <aph@cygnus.com>
 
-   This file is part of GCC.
+This file is part of GCC.
 
-   GCC is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3, or (at your option)
-   any later version.
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
 
-   GCC is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with GCC; see the file COPYING3.  If not see
-   <http://www.gnu.org/licenses/>.  */
+Under Section 7 of GPL version 3, you are granted additional
+permissions described in the GCC Runtime Library Exception, version
+3.1, as published by the Free Software Foundation.
+
+You should have received a copy of the GNU General Public License and
+a copy of the GCC Runtime Library Exception along with this program;
+see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+<http://www.gnu.org/licenses/>.  */
 
 #ifdef __VMS__
 /* On VMS, long is a 32 bit type.  */
