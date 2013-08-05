42f9a786da7981833d1b44455c4ef61085431af6
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-08-05 22:03:52 +0000

    /c-family 2013-08-05  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/58080
    	* c-common.c (pointer_int_sum): Add bool parameter.
    	* c-common.h (pointer_int_sum): Adjust declaration.
    
    /cp
    2013-08-05  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/58080
    	* typeck.c (cp_pointer_int_sum): Add tsubst_flags_t parameter.
    	(cp_build_binary_op): Adjust.
    
    /testsuite
    2013-08-05  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/58080
    	* g++.dg/cpp0x/pr58080.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@201512 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-family/c-common.h
+++ gcc/c-family/c-common.h
@@ -790,7 +790,8 @@ extern tree shorten_binary_op (tree result_type, tree op0, tree op1, bool bitwis
    and, if so, perhaps change them both back to their original type.  */
 extern tree shorten_compare (tree *, tree *, tree *, enum tree_code *);
 
-extern tree pointer_int_sum (location_t, enum tree_code, tree, tree);
+extern tree pointer_int_sum (location_t, enum tree_code, tree, tree,
+			     bool = true);
 
 /* Add qualifiers to a type, in the fashion for C.  */
 extern tree c_build_qualified_type (tree, int);
