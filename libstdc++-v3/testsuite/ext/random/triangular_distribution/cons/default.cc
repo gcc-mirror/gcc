172a54c6da1fabef891b1c3c80203b053926d13a
Paolo Carlini <paolo@gcc.gnu.org>
2013-08-07 12:23:15 +0000

    re PR libstdc++/58098 (wrong return value of normal_distribution::min())
    2013-08-07  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR libstdc++/58098
    	* include/bits/random.h (cauchy_distribution<>::min,
    	extreme_value_distribution<>::min, normal_distribution<>::min,
    	student_t_distribution<>::min): Fix.
    	* include/ext/random (normal_mv_distribution<>::min): Likewise.
    	* testsuite/26_numerics/random/cauchy_distribution/cons/parms.cc:
    	Adjust.
    	testsuite/26_numerics/random/cauchy_distribution/cons/default.cc:
    	Likewise.
    	* testsuite/26_numerics/random/extreme_value_distribution/cons/
    	parms.cc: Likewise.
    	testsuite/26_numerics/random/extreme_value_distribution/cons/
    	default.cc: Likewise.
    	* testsuite/26_numerics/random/normal_distribution/cons/parms.cc:
    	Likewise.
    	* testsuite/26_numerics/random/normal_distribution/cons/default.cc:
    	Likewise.
    	* testsuite/26_numerics/random/student_t_distribution/cons/parms.cc:
    	Likewise.
    	* testsuite/26_numerics/random/student_t_distribution/cons/default.cc:
    	Likewise.
    	* testsuite/ext/random/normal_mv_distribution/cons/parms.cc: Likewise.
    	* testsuite/ext/random/normal_mv_distribution/cons/default.cc: Likewise.
    
    	* testsuite/26_numerics/random/exponential_distribution/cons/parms.cc:
    	Minor tweak.
    	* testsuite/26_numerics/random/exponential_distribution/cons/default.cc:
    	Likewise.
    
    	* testsuite/ext/von_mises_distribution/*: Move to...
    	* testsuite/ext/random/von_mises_distribution/*: ... here.
    	* testsuite/ext/triangular_distribution/*: Move to...
    	* testsuite/ext/random/triangular_distribution/*: ... here.
    
    From-SVN: r201564

---
+++ libstdc++-v3/testsuite/ext/random/triangular_distribution/cons/default.cc
@@ -0,0 +1,44 @@
+// { dg-options "-std=c++0x" }
+// { dg-require-cstdint "" }
+//
+// 2008-11-24  Edward M. Smith-Rowland <3dw4rd@verizon.net>
+// 2013-03-02  Ulrich Drepper <drepper@gmail.com>
+//
+// Copyright (C) 2013 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <ext/random>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  __gnu_cxx::triangular_distribution<> u;
+  VERIFY( u.a() == 0.0 );
+  VERIFY( u.b() == 0.5 );
+  VERIFY( u.c() == 1.0 );
+  VERIFY( u.min() == 0.0 );
+  VERIFY( u.max() == 1.0 );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
