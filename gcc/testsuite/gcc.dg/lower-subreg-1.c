4ab104d3d00f7a9d66af5a153ccbd31ed11d825e
yufeng <yufeng@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-08-09 20:48:00 +0000

    gcc/testsuite/
    	* gcc.dg/lower-subreg-1.c: Skip aarch64*-*-*.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@201636 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/lower-subreg-1.c
+++ gcc/testsuite/gcc.dg/lower-subreg-1.c
@@ -1,4 +1,4 @@
-/* { dg-do compile { target { ! { mips64 || { arm*-*-* ia64-*-* sparc*-*-* spu-*-* tilegx-*-* } } } } } */
+/* { dg-do compile { target { ! { mips64 || { aarch64*-*-* arm*-*-* ia64-*-* sparc*-*-* spu-*-* tilegx-*-* } } } } } */
 /* { dg-options "-O -fdump-rtl-subreg1" } */
 /* { dg-skip-if "" { { i?86-*-* x86_64-*-* } && x32 } { "*" } { "" } } */
 /* { dg-require-effective-target ilp32 } */
