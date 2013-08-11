fb5803b8e3e75f4c4f07cdff9e2278c75241cb33
Janus Weil <janus@gcc.gnu.org>
2013-08-11 16:08:12 +0200

    backport: re PR fortran/58058 (Memory leak with transfer function)
    2013-08-11  Janus Weil  <janus@gcc.gnu.org>
    
    	Backport from trunk:
    	2013-08-09  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/58058
    	* trans-intrinsic.c (gfc_conv_intrinsic_transfer): Free the temporary
    	string, if necessary.
    
    2013-08-11  Janus Weil  <janus@gcc.gnu.org>
    
    	Backport from trunk:
    	2013-08-09  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/58058
    	* gfortran.dg/transfer_intrinsic_6.f90: New.
    
    From-SVN: r201653

---
+++ gcc/testsuite/gfortran.dg/transfer_intrinsic_6.f90
@@ -0,0 +1,20 @@
+! { dg-do compile }
+! { dg-options "-fdump-tree-original" }
+!
+! PR 58058: [4.7/4.8/4.9 Regression] Memory leak with transfer function
+!
+! Contributed by Thomas Jourdan <thomas.jourdan@orange.fr>
+
+  implicit none
+
+  integer, dimension(3) :: t1
+  character(len=64) :: str
+  
+  t1 = (/1,2,3/)
+
+  str = transfer(t1,str)
+
+end
+
+! { dg-final { scan-tree-dump-times "__builtin_free" 1 "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
