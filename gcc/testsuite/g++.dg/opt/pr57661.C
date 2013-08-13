da51794e1607d6bb4c8660498fb4eea61d3e6bad
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-08-13 17:06:13 +0000

    PR tree-optimization/57661 	* tree-inline.h (struct copy_body_data): Add blocks_to_copy field. 	* tree-inline.c (tree_function_versioning): Initialize it. 	(remap_gimple_stmt): Return GIMPLE_NOP for MEM_REF lhs clobber stmts 	if id->blocks_to_copy and MEM_REF's SSA_NAME is defined in a block 	that is not being copied.
    	* g++.dg/opt/pr57661.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@201698 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/opt/pr57661.C
@@ -0,0 +1,76 @@
+// PR tree-optimization/57661
+// { dg-do compile }
+// { dg-options "-O2 -fno-tree-forwprop -std=gnu++11" }
+
+template <typename>
+struct A
+{
+  ~A () {}
+};
+template <typename _Tp>
+using B = A <_Tp>;
+template <typename _Tp>
+class C : B <_Tp> {};
+namespace N { enum D { d }; }
+template <class>
+struct E
+{
+  ~E ();
+};
+template <class, class V>
+struct F : V {};
+template <class U, class V>
+struct G : F <U, V>
+{
+  N::D g1;
+  void g2 ();
+  void g3 ();
+  void g4 () { g3 (); }
+  static void g5 (G *__t) { __t->g4 (); }
+};
+template <class U, class V>
+struct H : G <U, V>
+{
+  E <U> *h1;
+  bool h2;
+  ~H () throw ()
+  {
+    this->g2 ();
+    if (h2)
+      delete h1;
+  }
+};
+template <class U, class V>
+struct I : H <U, V>, E <U>
+{
+  G <U, V> *i;
+  ~I () throw ()
+  {
+    i->g4 ();
+  }
+};
+struct J
+{
+  typedef C <char> j1;
+  typedef G <char, C <char>> j2;
+  J ();
+  j2 *j3;
+};
+struct K : J
+{
+  typedef G <char, C <char>> j2;
+  K () { j2::g5 (this->j3); }
+};
+template <class U, class V>
+void G <U, V>::g3 ()
+{
+  switch (g1)
+    {
+    case N::d:
+      {
+	I <U, V> *q = (I <U, V> *) this;
+	q->I <U, V>::~I ();
+      }
+    }
+}
+K r;
