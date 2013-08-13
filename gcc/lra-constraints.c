1f2e0c3a93f0948b302689dac8507b10f6617f68
Vladimir Makarov <vmakarov@gcc.gnu.org>
2013-08-13 16:40:33 +0000

    backport: re PR rtl-optimization/57459 (LRA inheritance bug)
    2013-08-13  Vladimir Makarov  <vmakarov@redhat.com>
    
    	Backport from mainline
    	2013-06-06  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR rtl-optimization/57459
    	* lra-constraints.c (update_ebb_live_info): Fix typo for operand
    	type when setting live regs.
    
    2013-08-13  Vladimir Makarov  <vmakarov@redhat.com>
    
    	Backport from mainline
    	2013-06-06  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR rtl-optimization/57459
    	* gcc.target/i386/pr57459.c: New test.
    
    From-SVN: r201695

--- gcc/lra-constraints.c
+++ gcc/lra-constraints.c
@@ -4396,7 +4396,7 @@ update_ebb_live_info (rtx head, rtx tail)
 	  bitmap_clear_bit (&live_regs, reg->regno);
       /* Mark each used value as live.  */
       for (reg = curr_id->regs; reg != NULL; reg = reg->next)
-	if (reg->type == OP_IN
+	if (reg->type != OP_OUT
 	    && bitmap_bit_p (&check_only_regs, reg->regno))
 	  bitmap_set_bit (&live_regs, reg->regno);
       /* It is quite important to remove dead move insns because it
