6ff0a28698bca7be7fea57e5fb037fdfbdddf151
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-08-13 13:23:55 +0000

    * gcc.dg/pr57980.c: Use vector of two elements, not just one.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@201686 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr57980.c
+++ gcc/testsuite/gcc.dg/pr57980.c
@@ -1,8 +1,8 @@
 /* PR tree-optimization/57980 */
 /* { dg-do compile } */
-/* { dg-options "-O -foptimize-sibling-calls" } */
+/* { dg-options "-O -foptimize-sibling-calls -w" } */
 
-typedef int V __attribute__ ((vector_size (sizeof (int))));
+typedef int V __attribute__ ((vector_size (2 * sizeof (int))));
 extern V f (void);
 
 V
