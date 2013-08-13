7013586ef1b8063f3a1d36748135cdc90b172f2f
macro <macro@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-08-13 21:39:26 +0000

    * gcc.target/mips/nan-legacy.c: Accept 4294967295 as an 	alternative to -1. 	* gcc.target/mips/nans-legacy.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@201711 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/mips/nan-legacy.c
+++ gcc/testsuite/gcc.target/mips/nan-legacy.c
@@ -4,4 +4,4 @@
 double d = __builtin_nan ("");
 
 /* { dg-final { scan-assembler "\t\\.nan\tlegacy\n" } } */
-/* { dg-final { scan-assembler "\t\\.word\t2146959359\n\t\\.word\t-1\n" } } */
+/* { dg-final { scan-assembler "\t\\.word\t2146959359\n\t\\.word\t(?:-1|4294967295)\n" } } */
--- gcc/testsuite/gcc.target/mips/nans-legacy.c
+++ gcc/testsuite/gcc.target/mips/nans-legacy.c
@@ -4,4 +4,4 @@
 double ds = __builtin_nans ("");
 
 /* { dg-final { scan-assembler "\t\\.nan\tlegacy\n" } } */
-/* { dg-final { scan-assembler "\t\\.word\t2147483647\n\t\\.word\t-1\n" } } */
+/* { dg-final { scan-assembler "\t\\.word\t2147483647\n\t\\.word\t(?:-1|4294967295)\n" } } */
