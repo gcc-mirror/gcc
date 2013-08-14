7608ecd75ffe0076296773311a134cfed17dc29e
janis <janis@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-08-14 16:52:40 +0000

    * gcc.target/arm/atomic-comp-swap-release-acquire.c: Move dg-do 	to be the first test directive. 	* gcc.target/arm/atomic-op-acq_rel.c: Likewise. 	* gcc.target/arm/atomic-op-acquire.c: Likewise. 	* gcc.target/arm/atomic-op-char.c: Likewise. 	* gcc.target/arm/atomic-op-consume.c: Likewise. 	* gcc.target/arm/atomic-op-int.c: Likewise. 	* gcc.target/arm/atomic-op-relaxed.c: Likewise. 	* gcc.target/arm/atomic-op-release.c: Likewise. 	* gcc.target/arm/atomic-op-seq_cst.c: Likewise. 	* gcc.target/arm/atomic-op-short.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@201730 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/atomic-op-seq_cst.c
+++ gcc/testsuite/gcc.target/arm/atomic-op-seq_cst.c
@@ -1,5 +1,5 @@
-/* { dg-require-effective-target arm_arch_v8a_ok } */
 /* { dg-do compile } */
+/* { dg-require-effective-target arm_arch_v8a_ok } */
 /* { dg-options "-O2" } */
 /* { dg-add-options arm_arch_v8a } */
 
