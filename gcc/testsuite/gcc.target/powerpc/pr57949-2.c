f2e80d2c4d7743c0f18b28b1aba90159a0ccfb3b
wschmidt <wschmidt@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-08-14 20:38:33 +0000

    gcc:
    2013-08-14  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	PR target/57949
    	* doc/invoke.texi: Add documentation of mcompat-align-parm
    	option.
    	* config/rs6000/rs6000.opt: Add mcompat-align-parm option.
    	* config/rs6000/rs6000.c (rs6000_function_arg_boundary): For AIX
    	and Linux, correct BLKmode alignment when 128-bit alignment is
    	required and compatibility flag is not set.
    	(rs6000_gimplify_va_arg): For AIX and Linux, honor specified
    	alignment for zero-size arguments when compatibility flag is not
    	set.
    
    gcc/testsuite:
    
    2013-08-14  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	PR target/57949
    	* gcc.target/powerpc/pr57949-1.c: New.
    	* gcc.target/powerpc/pr57949-2.c: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@201750 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/pr57949-2.c
@@ -0,0 +1,19 @@
+/* { dg-do compile { target { powerpc64*-*-* && lp64 } } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
+/* { dg-options "-O2 -mcpu=power7 -mcompat-align-parm" } */
+
+/* Verify that vs is not 16-byte aligned with -mcompat-align-parm.  */
+
+typedef float v4sf __attribute__ ((vector_size (16)));
+struct s { long m; v4sf v; };
+long n;
+v4sf ve;
+
+void pr57949 (long d1, long d2, long d3, long d4, long d5, long d6,
+	      long d7, long d8, long d9, struct s vs) {
+  n = vs.m;
+  ve = vs.v;
+}
+
+/* { dg-final { scan-assembler "ld .\*,136\\(1\\)" } } */
+/* { dg-final { scan-assembler "ld .\*,120\\(1\\)" } } */
