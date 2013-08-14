e839e2a9311ecfa2cfd8e9bc0b6eeba65da48418
Andrey Belevantsev <abel@ispras.ru>
2013-08-14 18:31:26 +0400

    re PR rtl-optimization/57662 (ICE: SIGSEGV in code_motion_process_successors with -fschedule-insns2 -fselective-scheduling2)
    PR rtl-optimization/57662
    
    gcc/
    	* sel-sched.c (code_motion_process_successors): When the current insn
    	is removed after the recursive traversal, break from the loop.
    	Add comments and debug printouts.
    
    testsuite/
    
    	* gcc.dg/pr57662.c: New test.
    
    From-SVN: r201728

---
+++ gcc/testsuite/gcc.dg/pr57662.c
@@ -0,0 +1,47 @@
+/* { dg-do compile { target powerpc*-*-* ia64-*-* x86_64-*-* } } */
+/* { dg-options " -O -fno-guess-branch-probability -fpeel-loops -freorder-blocks-and-partition -fschedule-insns2 -fsel-sched-pipelining -fselective-scheduling2 -ftree-pre" } */
+
+struct intC
+{
+  short x;
+  short y;
+};
+
+void Get(void);
+
+int size_x;
+
+struct
+{
+  int *depot_table;
+  struct intC *ti;
+  int size;
+} dummy;
+
+static inline int
+GetRotatedTileFromOffset (int *a, struct intC tidc)
+{
+  if (!*a)
+    Get ();
+  switch (*a)
+    {
+    case 0:
+      return (tidc.y << size_x) + tidc.x;
+    case 1:
+      return tidc.y + (dummy.size - tidc.x) * size_x;
+    case 2:
+      return tidc.x + (dummy.size - tidc.y) * size_x;
+    case 3:
+      return (dummy.size - tidc.x);
+    }
+  return 0;
+}
+
+int
+GetHangarNum (int *a, int i)
+{
+  while (dummy.size)
+    if (GetRotatedTileFromOffset (a, dummy.ti[i]))
+      return *dummy.depot_table;
+  return 0;
+}
