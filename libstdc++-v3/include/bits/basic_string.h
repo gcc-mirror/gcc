7e66313066525b0ce38e140e6d9c815e19d119bf
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-08-15 00:15:12 +0000

    2013-08-14  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR libstdc++/58163
    	* include/bits/basic_string.h (basic_string<>::operator[]): Fix
    	_GLIBCXX_DEBUG_PEDASSERT check vs C++11.
    	* include/ext/vstring.h: Likewise.
    	* testsuite/21_strings/basic_string/element_access/char/58163.cc:
    	New.
    	* testsuite/21_strings/basic_string/element_access/wchar_t/58163.cc:
    	Likewise.
    	* testsuite/ext/vstring/element_access/char/58163.cc: Likewise.
    	* testsuite/ext/vstring/element_access/wchar_t/58163.cc: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@201755 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/basic_string.h
+++ libstdc++-v3/include/bits/basic_string.h
@@ -842,10 +842,11 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       reference
       operator[](size_type __pos)
       {
-        // allow pos == size() as v3 extension:
+        // Allow pos == size() both in C++98 mode, as v3 extension,
+	// and in C++11 mode.
 	_GLIBCXX_DEBUG_ASSERT(__pos <= size());
-        // but be strict in pedantic mode:
-	_GLIBCXX_DEBUG_PEDASSERT(__pos < size());
+        // In pedantic mode be strict in C++98 mode.
+	_GLIBCXX_DEBUG_PEDASSERT(__cplusplus >= 201103L || __pos < size());
 	_M_leak();
 	return _M_data()[__pos];
       }
