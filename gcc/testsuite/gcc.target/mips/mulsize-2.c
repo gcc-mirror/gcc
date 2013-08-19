a43ff590b05822f3225b112ee88a8d6187f1cb8d
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-08-19 17:35:57 +0000

    gcc/testsuite/ 	* gcc.target/mips/mulsize-1.c: Check for SLL as well as SUBU. 	* gcc.target/mips/mulsize-2.c: Check for ADDU rather than SUBU. 	Check for SLL too.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@201848 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/mips/mulsize-2.c
+++ gcc/testsuite/gcc.target/mips/mulsize-2.c
@@ -1,5 +1,6 @@
 /* { dg-final { scan-assembler "\t.globl\tf9" } } */
-/* { dg-final { scan-assembler "\tsubu\t" } } */
+/* { dg-final { scan-assembler "\tsll\t" } } */
+/* { dg-final { scan-assembler "\taddu\t" } } */
 /* { dg-final { scan-assembler-not "\tli\t" } } */
 /* { dg-final { scan-assembler-not "\tmul\t" } } */
 int
