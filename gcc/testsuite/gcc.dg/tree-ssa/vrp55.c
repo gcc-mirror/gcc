40e5d37ce103ea1e9f6b77fae365179c5ab1720f
amylaar <amylaar@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-08-19 15:13:33 +0000

    * gcc.dg/tree-ssa/vrp55.c: Use keeps_null_pointer_checks to determine         correct test response.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@201843 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/vrp55.c
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp55.c
@@ -9,6 +9,7 @@ fu (char *p, int x)
     arf ();
 }
 
-/* { dg-final { scan-tree-dump-times "Threaded jump" 1 "vrp1" } } */
+/* { dg-final { scan-tree-dump-times "Threaded jump" 1 "vrp1" { target { ! keeps_null_pointer_checks } } } } */
+/* { dg-final { scan-tree-dump-times "Threaded jump" 0 "vrp1" { target {   keeps_null_pointer_checks } } } } */
 /* { dg-final { cleanup-tree-dump "vrp1" } } */
 
