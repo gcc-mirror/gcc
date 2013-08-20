4874cc89392ff850a32065860b67c189c37810d9
tejohnson <tejohnson@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-08-20 13:29:53 +0000

    2013-08-20  Teresa Johnson  <tejohnson@google.com>
    	PR rtl-optimizations/57451
    	* final.c (reemit_insn_block_notes): Prevent lexical blocks
    	from crossing split section boundaries.
    
    	* testsuite/g++.dg/tree-prof/pr57451.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@201883 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/tree-prof/pr57451.C
@@ -0,0 +1,27 @@
+// { dg-do run }
+// { dg-require-effective-target freorder }
+// { dg-options "-O2 -freorder-blocks-and-partition -g" }
+
+extern "C" void abort (void);
+struct MyException {};
+struct Data {
+    int nr;
+    Data() : nr(66) {}
+};
+Data __attribute__((noinline,noclone)) getData(int i)
+{
+  if (i) throw MyException();
+  Data data;
+  data.nr = i;
+  return data;
+}
+int main(int, char **)
+{
+  Data data;
+  try {
+      data = getData(1);
+  } catch (MyException& e) {
+      if (data.nr != 66)
+	abort ();
+  }
+}
