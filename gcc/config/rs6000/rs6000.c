4c476c8cffbde63022bc6154033cd9137c0a94ec
Oleg Endo <olegendo@gcc.gnu.org>
2013-08-29 18:37:46 +0000

    backport: re PR other/12081 (Gcc can't be compiled with -mregparm=3)
    	Backport from mainline
    	2013-08-05  Oleg Endo  <olegendo@gcc.gnu.org>
    
    	PR other/12081
    	* recog.h (rtx (*insn_gen_fn) (rtx, ...)): Replace typedef with	new
    	class insn_gen_fn.
    	* expr.c (move_by_pieces_1, store_by_pieces_2): Replace argument
    	rtx (*) (rtx, ...) with insn_gen_fn.
    	* genoutput.c (output_insn_data): Cast gen_? function pointers to
    	insn_gen_fn::stored_funcptr.  Add initializer braces.
    
    	Backport from mainline
    	2013-08-07  Oleg Endo  <olegendo@gcc.gnu.org>
    
    	PR other/12081
    	* config/rs6000/rs6000.c (gen_2arg_fn_t): Remove typedef.
    	(rs6000_emit_swdiv_high_precision, rs6000_emit_swdiv_low_precision,
    	rs6000_emit_swrsqrt): Don't cast result of GEN_FCN to gen_2arg_fn_t.
    
    From-SVN: r202083

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -284,9 +284,6 @@ static struct
   { "rsqrtd",	 (RECIP_DF_RSQRT | RECIP_V2DF_RSQRT) },
 };
 
-/* 2 argument gen function typedef.  */
-typedef rtx (*gen_2arg_fn_t) (rtx, rtx, rtx);
-
 /* Pointer to function (in rs6000-c.c) that can define or undefine target
    macros that have changed.  Languages that don't support the preprocessor
    don't link in rs6000-c.c, so we can't call it directly.  */
@@ -26657,7 +26654,7 @@ rs6000_emit_swdiv_high_precision (rtx dst, rtx n, rtx d)
   enum machine_mode mode = GET_MODE (dst);
   rtx x0, e0, e1, y1, u0, v0;
   enum insn_code code = optab_handler (smul_optab, mode);
-  gen_2arg_fn_t gen_mul = (gen_2arg_fn_t) GEN_FCN (code);
+  insn_gen_fn gen_mul = GEN_FCN (code);
   rtx one = rs6000_load_constant_and_splat (mode, dconst1);
 
   gcc_assert (code != CODE_FOR_nothing);
@@ -26695,7 +26692,7 @@ rs6000_emit_swdiv_low_precision (rtx dst, rtx n, rtx d)
   enum machine_mode mode = GET_MODE (dst);
   rtx x0, e0, e1, e2, y1, y2, y3, u0, v0, one;
   enum insn_code code = optab_handler (smul_optab, mode);
-  gen_2arg_fn_t gen_mul = (gen_2arg_fn_t) GEN_FCN (code);
+  insn_gen_fn gen_mul = GEN_FCN (code);
 
   gcc_assert (code != CODE_FOR_nothing);
 
@@ -26766,7 +26763,7 @@ rs6000_emit_swrsqrt (rtx dst, rtx src)
   int i;
   rtx halfthree;
   enum insn_code code = optab_handler (smul_optab, mode);
-  gen_2arg_fn_t gen_mul = (gen_2arg_fn_t) GEN_FCN (code);
+  insn_gen_fn gen_mul = GEN_FCN (code);
 
   gcc_assert (code != CODE_FOR_nothing);
 
