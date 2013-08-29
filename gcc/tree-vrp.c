c2ff4a23ee8e8d0a7090d0a31b691d26e36a6ea6
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-08-29 07:45:59 +0000

    2013-08-29  Richard Biener  <rguenther@suse.de>
    	PR tree-optimization/57685
    	* tree-vrp.c (register_edge_assert_for_1): Recurse only for
    	single-use operands to avoid exponential complexity.
    
    	* gcc.dg/torture/pr57685.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@202068 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-vrp.c
+++ gcc/tree-vrp.c
@@ -5410,10 +5410,14 @@ register_edge_assert_for_1 (tree op, enum tree_code code,
 	       && gimple_assign_rhs_code (op_def) == BIT_IOR_EXPR))
     {
       /* Recurse on each operand.  */
-      retval |= register_edge_assert_for_1 (gimple_assign_rhs1 (op_def),
-					    code, e, bsi);
-      retval |= register_edge_assert_for_1 (gimple_assign_rhs2 (op_def),
-					    code, e, bsi);
+      tree op0 = gimple_assign_rhs1 (op_def);
+      tree op1 = gimple_assign_rhs2 (op_def);
+      if (TREE_CODE (op0) == SSA_NAME
+	  && has_single_use (op0))
+	retval |= register_edge_assert_for_1 (op0, code, e, bsi);
+      if (TREE_CODE (op1) == SSA_NAME
+	  && has_single_use (op1))
+	retval |= register_edge_assert_for_1 (op1, code, e, bsi);
     }
   else if (gimple_assign_rhs_code (op_def) == BIT_NOT_EXPR
 	   && TYPE_PRECISION (TREE_TYPE (gimple_assign_lhs (op_def))) == 1)
