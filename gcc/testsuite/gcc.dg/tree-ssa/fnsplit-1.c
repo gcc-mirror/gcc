8745ccbf183af1eba2d430f43519f4602bfb1ce8
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-09-02 20:17:10 +0000

    * ipa-split.c (execute_split_functions): Split externally visible 	functions called once. 	* gcc.dg/tree-ssa/fnsplit-1.c: New testcase.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@202185 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/fnsplit-1.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-fnsplit" } */
+#include <stdio.h>
+int a[1000];
+
+void
+t(int a)
+{
+  if (a)
+    printf ("I Am Completely Operational,"),
+    printf ("And All My Circuits Are Functioning Perfectly\n");
+}
+int
+main(void)
+{
+  int i;
+  for (i = 0; i < 1000; i++)
+    t(a[i]);
+  return 0;
+}
+/* { dg-final { scan-tree-dump-times "Splitting function at:" 1 "fnsplit"} } */
+
+/* { dg-final { cleanup-tree-dump "fnsplit" } } */
