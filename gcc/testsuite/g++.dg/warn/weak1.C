2ca21174033431ba453a11807d1cc70d02dbfe77
dje <dje@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-09-04 21:21:39 +0000

    * g++.dg/warn/weak1.C: Skip on AIX.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@202263 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/warn/weak1.C
+++ gcc/testsuite/g++.dg/warn/weak1.C
@@ -2,6 +2,7 @@
 // { dg-require-weak "" }
 // The PA HP-UX dynamic loader doesn't support unsatisfied weak symbols.
 // { dg-skip-if "No unsat" { hppa*-*-hpux* } { "*" } { "" } }
+// { dg-skip-if "No weak unsat" { *-*-aix* } { "*" } { "" } }
 // The darwin loader does, but they do need to exist at link time.
 // { dg-skip-if "No link unsat" { *-*-darwin* } { "*" } { "" } }
 // For kernel modules and static RTPs, the loader treats undefined weak
