71191083b767f94d0bfeeaafd5201c8f67769459
Teresa Johnson <tejohnson@google.com>
2013-09-04 14:31:20 +0000

    dumpfile.c (dump_finish): Don't close stderr/stdout.
    2013-09-04  Teresa Johnson  <tejohnson@google.com>
    
    	* dumpfile.c (dump_finish): Don't close stderr/stdout.
    
    	* testsuite/gcc.dg/unroll_1.c: Test dumping to stderr.
    
    From-SVN: r202260

--- gcc/testsuite/gcc.dg/unroll_1.c
+++ gcc/testsuite/gcc.dg/unroll_1.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -fdump-rtl-loop2_unroll -fno-peel-loops -fdisable-tree-cunroll -fdisable-tree-cunrolli -fenable-rtl-loop2_unroll" } */
+/* { dg-options "-O2 -fdump-rtl-loop2_unroll=stderr -fno-peel-loops -fdisable-tree-cunroll -fdisable-tree-cunrolli -fenable-rtl-loop2_unroll" } */
 
 unsigned a[100], b[100];
 inline void bar()
@@ -11,7 +11,7 @@ int foo(void)
 {
   int i;
   bar();
-  for (i = 0; i < 2; i++)
+  for (i = 0; i < 2; i++) /* { dg-message "note: loop turned into non-loop; it never loops" } */
   {
      a[i]= b[i] + 1;
   }
@@ -21,12 +21,10 @@ int foo(void)
 int foo2(void)
 {
   int i;
-  for (i = 0; i < 2; i++)
+  for (i = 0; i < 2; i++) /* { dg-message "note: loop turned into non-loop; it never loops" } */
   {
      a[i]= b[i] + 1;
   }
   return 1;
 }
-
-/* { dg-final { scan-rtl-dump-times "loop turned into non-loop; it never loops" 2 "loop2_unroll" } } */
-/* { dg-final { cleanup-rtl-dump "loop2_unroll" } } */
+/* { dg-prune-output ".*" } */
