ec538a41e23c38bcc7b4393cecbd3ec6bd4ad544
ktkachov <ktkachov@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-09-09 13:32:50 +0000

    [gcc/] 2013-09-09  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    	* config/aarch64/aarch64.c (aarch64_select_cc_mode): Return CC_SWP for
    	comparison with negated operand.
    	* config/aarch64/aarch64.md (compare_neg<mode>): Match canonical RTL form.
    
    [gcc/testsuite/]	
    2013-09-09  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
    
    	* gcc.target/aarch64/cmn-neg.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@202400 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/aarch64/cmn-neg.c
@@ -0,0 +1,33 @@
+/* { dg-do run } */
+/* { dg-options "-O2 --save-temps" } */
+
+extern void abort (void);
+
+void __attribute__ ((noinline))
+foo_s32 (int a, int b)
+{
+  if (a < -b)
+    abort ();
+}
+/* { dg-final { scan-assembler "cmn\tw\[0-9\]" } } */
+
+void __attribute__ ((noinline))
+foo_s64 (long long a, long long b)
+{
+  if (a < -b)
+    abort ();
+}
+/* { dg-final { scan-assembler "cmn\tx\[0-9\]" } } */
+
+
+int
+main (void)
+{
+  int a = 30;
+  int b = 42;
+  foo_s32 (a, b);
+  foo_s64 (a, b);
+  return 0;
+}
+
+/* { dg-final { cleanup-saved-temps } } */
