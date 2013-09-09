8bed0f723ce65f7836e95cedfbf83b12e8a556f0
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-09-09 13:39:47 +0000

    /cp 2013-09-09  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/58362
    	* error.c (location_of): Don't handle PARM_DECLs specially.
    
    /testsuite
    2013-09-09  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/58362
    	* g++.dg/warn/Wunused-parm-5.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@202402 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/warn/Wunused-parm-5.C
@@ -0,0 +1,14 @@
+// PR c++/58362
+// { dg-options "-Wunused-parameter" }
+
+void f1 (long s) { }  // { dg-warning "15:unused parameter 's'" }
+
+void f2 (long s, int u) { }  // { dg-warning "15:unused parameter 's'" }
+// { dg-warning "22:unused parameter 'u'" "" { target *-*-* } 6 }
+
+void f3 (long s);
+void f3 (long s) { }  // { dg-warning "15:unused parameter 's'" }
+
+void f4 (long s, int u);
+void f4 (long s, int u) { }  // { dg-warning "15:unused parameter 's'" }
+// { dg-warning "22:unused parameter 'u'" "" { target *-*-* } 13 }
