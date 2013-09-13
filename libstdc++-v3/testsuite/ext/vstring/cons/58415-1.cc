4f03875418e8d9df12f819a0baeaece1cc7c13e7
Paolo Carlini <paolo.carlini@oracle.com>
2013-09-13 18:32:42 +0000

    re PR libstdc++/58415 (__sso_string_base move constructor does not null terminate local data when moving from empty string)
    2013-09-13  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR libstdc++/58415
    	* include/ext/sso_string_base.h (__sso_string_base<>::
    	__sso_string_base(__sso_string_base&&)): Fix thinkos about
    	_M_length vs _M_set_length.
    	* testsuite/ext/vstring/cons/58415-1.cc: New.
    	* testsuite/ext/vstring/cons/58415-2.cc: Likewise.
    
    From-SVN: r202574

---
+++ libstdc++-v3/testsuite/ext/vstring/cons/58415-1.cc
@@ -0,0 +1,41 @@
+// { dg-options "-std=gnu++11" }
+// { dg-require-string-conversions "" }
+
+// Copyright (C) 2013 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <ext/vstring.h>
+#include <testsuite_hooks.h>
+
+typedef __gnu_cxx::__versa_string<char> string;
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  string s1("string");
+  string s2("");
+  std::swap(s1, s2);
+
+  VERIFY( s1.c_str()[0] == '\0' );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
