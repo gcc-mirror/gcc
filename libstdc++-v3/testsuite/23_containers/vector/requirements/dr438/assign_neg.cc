076b3f1a6b4ff2cac6a830a3c48fb9e131dc78bb
glisse <glisse@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-09-17 12:23:54 +0000

    2013-09-17  Marc Glisse  <marc.glisse@inria.fr>
    	PR libstdc++/58338
    	* include/bits/stl_vector.h (vector::vector(),
    	vector::vector(const allocator_type&)): Merge.
    	(_Vector_impl::_Vector_impl(_Tp_alloc_type const&),
    	_Vector_impl::_Vector_impl(_Tp_alloc_type&&),
    	_Vector_impl::_M_swap_data,
    	_Vector_base::_Vector_base(const allocator_type&),
    	_Vector_base::_Vector_base(allocator_type&&),
    	_Vector_base::_Vector_base(_Vector_base&&), _Vector_base::~_Vector_base,
    	vector::vector(const allocator_type&), vector::operator[],
    	vector::operator[] const, vector::front, vector::front const,
    	vector::back, vector::back const, vector::pop_back,
    	vector::_M_erase_at_end): Mark as noexcept.
    	* include/debug/vector (vector::vector(const _Allocator&),
    	vector::operator[], vector::operator[] const, vector::front,
    	vector::front const, vector::back, vector::back const, vector::pop_back,
    	_M_requires_reallocation, _M_update_guaranteed_capacity,
    	_M_invalidate_after_nth): Mark as noexcept.
    	* include/profile/vector (vector::vector(const _Allocator&),
    	vector::operator[], vector::operator[] const, vector::front,
    	vector::front const, vector::back, vector::back const): Mark as
    	noexcept.
    	(vector::vector(vector&&, const _Allocator&)): Remove wrong noexcept.
    	* testsuite/23_containers/vector/requirements/dr438/assign_neg.cc:
    	Adjust line number.
    	* testsuite/23_containers/vector/requirements/dr438/
    	constructor_1_neg.cc: Likewise.
    	* testsuite/23_containers/vector/requirements/dr438/
    	constructor_2_neg.cc: Likewise.
    	* testsuite/23_containers/vector/requirements/dr438/insert_neg.cc:
    	Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@202650 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/23_containers/vector/requirements/dr438/assign_neg.cc
+++ libstdc++-v3/testsuite/23_containers/vector/requirements/dr438/assign_neg.cc
@@ -18,7 +18,7 @@
 // <http://www.gnu.org/licenses/>.
 
 // { dg-do compile }
-// { dg-error "no matching" "" { target *-*-* } 1308 }
+// { dg-error "no matching" "" { target *-*-* } 1302 }
 
 #include <vector>
 
--- libstdc++-v3/testsuite/23_containers/vector/requirements/dr438/insert_neg.cc
+++ libstdc++-v3/testsuite/23_containers/vector/requirements/dr438/insert_neg.cc
@@ -18,7 +18,7 @@
 // <http://www.gnu.org/licenses/>.
 
 // { dg-do compile }
-// { dg-error "no matching" "" { target *-*-* } 1349 }
+// { dg-error "no matching" "" { target *-*-* } 1343 }
 
 #include <vector>
 
