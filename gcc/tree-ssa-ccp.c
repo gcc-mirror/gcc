42eed6839d18a608c6250d03f21da4006f209f8e
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-09-23 16:07:19 +0000

    * tree-ssa-ccp.c (insert_clobber_before_stack_restore): Recurse on copy 	assignment statements.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@202833 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-ccp.c
+++ gcc/tree-ssa-ccp.c
@@ -1728,6 +1728,9 @@ insert_clobber_before_stack_restore (tree saved_val, tree var,
 	insert_clobber_before_stack_restore (gimple_phi_result (stmt), var,
 					     visited);
       }
+    else if (gimple_assign_ssa_name_copy_p (stmt))
+      insert_clobber_before_stack_restore (gimple_assign_lhs (stmt), var,
+					   visited);
     else
       gcc_assert (is_gimple_debug (stmt));
 }
