7017a7a9f8cc2161c9379e3eaf94ce889315e132
Tobias Burnus <burnus@net-b.de>
2013-09-25 21:56:20 +0200

    re PR fortran/58436 ([OOP] ICE (segfault) in generate_finalization_wrapper for CLASS(*))
    2013-09-25  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/58436
            * class.c (generate_finalization_wrapper): Handle CLASS(*).
    
    2013-09-25  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/58436
            * gfortran.dg/finalize_21.f90: New.
    
    From-SVN: r202923

---
+++ gcc/testsuite/gfortran.dg/finalize_21.f90
@@ -0,0 +1,12 @@
+! { dg-do compile }
+! { dg-options "-fdump-tree-original" }
+!
+! PR fortran/58436
+!
+! The following was ICEing and lacking _final=0
+!
+class(*), allocatable :: var
+end
+
+! { dg-final { scan-tree-dump "static struct __vtype__STAR __vtab__STAR = {._hash=0, ._size=., ._extends=0B, ._def_init=0B, ._copy=0B, ._final=0B};" "original" } }
+! { dg-final { cleanup-tree-dump "original" } }
