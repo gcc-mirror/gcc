3c468b4cb9068b7f81f8055f21f1abf3a915065e
Paulo Matos <pmatos@broadcom.com>
2013-09-27 14:54:43 +0000

    re PR middle-end/58463 (ICE with -fdump-tree-all-all in vector indexed access)
    	PR middle-end/58463
            * gcc.dg/pr58463.c: New test.
    
    From-SVN: r202976

---
+++ gcc/testsuite/gcc.dg/pr58463.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-options "-fdump-tree-ealias-details -O2" } */
+
+typedef struct
+{
+  int data16;
+}
+list_data;
+void
+fn1 (list_data * p1)
+{
+    p1->data16 = p1->data16 & 1 & p1->data16 >> 1;
+}
+
+/* { dg-final { cleanup-tree-dump "ealias" } } */
