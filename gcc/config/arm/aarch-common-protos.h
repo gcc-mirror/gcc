5bea0c6c8c3deb0b52aec49434484b35e75293f2
Kyrylo Tkachov <kyrylo.tkachov@arm.com>
2013-10-03 13:58:42 +0000

    aarch-common-protos.h (struct alu_cost_table): New.
    [gcc/]
    2013-10-03  Kyrylo Tkachov  <kyrylo.tkachov@arm.com>
                Richard Earnshaw  <richard.earnshaw@arm.com>
    
    	* config/arm/aarch-common-protos.h (struct alu_cost_table): New.
    	(struct mult_cost_table): Likewise.
    	(struct mem_cost_table): Likewise.
    	(struct fp_cost_table): Likewise.
    	(struct vector_cost_table): Likewise.
    	(cpu_cost_table): Likewise.
    	* config/arm/arm.opt (mold-rts-costs): New option.
    	(mnew-generic-costs): Likewise.
    	* config/arm/arm.c (generic_extra_costs): New table.
    	(cortexa15_extra_costs): Likewise.
    	(arm_slowmul_tune): Use NULL as new costs.
    	(arm_fastmul_tune): Likewise.
    	(arm_strongarm_tune): Likewise.
    	(arm_xscale_tune): Likewise.
    	(arm_9e_tune): Likewise.
    	(arm_v6t2_tune): Likewise.
    	(arm_cortex_a5_tune): Likewise.
    	(arm_cortex_a9_tune): Likewise.
    	(arm_v6m_tune): Likewise.
    	(arm_fa726te_tune): Likewise.
    	(arm_cortex_a15_tune): Use cortex15_extra_costs.
    	(arm_cortex_tune): Use generict_extra_costs.
    	(shifter_op_p): New function.
    	(arm_unspec_cost): Likewise.
    	(LIBCALL_COST): Define.
    	(arm_new_rtx_costs): New function.
    	(arm_rtx_costs): Use arm_new_rtx_costs when core-specific
    	table is available. Use old costs otherwise unless mnew-generic-costs
    	is specified.
    	* config/arm/arm-protos.h (tune_params): Add insn_extra_cost field.
    	(cpu_cost_table): Declare.
    
    Co-Authored-By: Richard Earnshaw <rearnsha@arm.com>
    
    From-SVN: r203160

--- gcc/config/arm/aarch-common-protos.h
+++ gcc/config/arm/aarch-common-protos.h
@@ -1,5 +1,4 @@
-/* Function prototypes for instruction scheduling dependeoncy routines,
-   defined in aarch-common.c
+/* Functions and structures shared between arm and aarch64.
 
    Copyright (C) 1991-2013 Free Software Foundation, Inc.
    Contributed by ARM Ltd.
@@ -32,5 +31,103 @@ extern int arm_no_early_alu_shift_dep (rtx, rtx);
 extern int arm_no_early_alu_shift_value_dep (rtx, rtx);
 extern int arm_no_early_mul_dep (rtx, rtx);
 extern int arm_no_early_store_addr_dep (rtx, rtx);
+
+/* RTX cost table definitions.  These are used when tuning for speed rather
+   than for size and should reflect the _additional_ cost over the cost
+   of the fastest instruction in the machine, which is COSTS_N_INSNS (1).
+   Therefore it's okay for some costs to be 0.
+   Costs may not have a negative value.  */
+struct alu_cost_table
+{
+  const int arith;		/* ADD/SUB.  */
+  const int logical;		/* AND/ORR/EOR/BIC, etc.  */
+  const int shift;		/* Simple shift.  */
+  const int shift_reg;		/* Simple shift by reg.  */
+  const int arith_shift;	/* Additional when arith also shifts...  */
+  const int arith_shift_reg;	/* ... and when the shift is by a reg.  */
+  const int log_shift;		/* Additional when logic also shifts...  */
+  const int log_shift_reg;	/* ... and when the shift is by a reg.  */
+  const int extnd;		/* Zero/sign extension.  */
+  const int extnd_arith;	/* Extend and arith.  */
+  const int bfi;		/* Bit-field insert.  */
+  const int bfx;		/* Bit-field extraction.  */
+  const int clz;		/* Count Leading Zeros.  */
+  const int non_exec;		/* Extra cost when not executing insn.  */
+  const bool non_exec_costs_exec; /* True if non-execution must add the exec
+				     cost.  */
+};
+
+struct mult_cost_table
+{
+  const int simple;
+  const int flag_setting;	/* Additional cost if multiply sets flags. */
+  const int extend;
+  const int add;
+  const int extend_add;
+  const int idiv;
+};
+
+/* Calculations of LDM costs are complex.  We assume an initial cost
+   (ldm_1st) which will load the number of registers mentioned in
+   ldm_regs_per_insn_1st registers; then each additional
+   ldm_regs_per_insn_subsequent registers cost one more insn.
+   Similarly for STM operations.
+   Therefore the ldm_regs_per_insn_1st/stm_regs_per_insn_1st and
+   ldm_regs_per_insn_subsequent/stm_regs_per_insn_subsequent fields indicate
+   the number of registers loaded/stored and are expressed by a simple integer
+   and not by a COSTS_N_INSNS (N) expression.
+   */
+struct mem_cost_table
+{
+  const int load;
+  const int load_sign_extend;	/* Additional to load cost.  */
+  const int ldrd;		/* Cost of LDRD.  */
+  const int ldm_1st;
+  const int ldm_regs_per_insn_1st;
+  const int ldm_regs_per_insn_subsequent;
+  const int loadf;		/* SFmode.  */
+  const int loadd;		/* DFmode.  */
+  const int load_unaligned;	/* Extra for unaligned loads.  */
+  const int store;
+  const int strd;
+  const int stm_1st;
+  const int stm_regs_per_insn_1st;
+  const int stm_regs_per_insn_subsequent;
+  const int storef;		/* SFmode.  */
+  const int stored;		/* DFmode.  */
+  const int store_unaligned;	/* Extra for unaligned stores.  */
+};
+
+struct fp_cost_table
+{
+  const int div;
+  const int mult;
+  const int mult_addsub;	/* Non-fused.  */
+  const int fma;		/* Fused.  */
+  const int addsub;
+  const int fpconst;		/* Immediate.  */
+  const int neg;		/* NEG and ABS.  */
+  const int compare;
+  const int widen;		/* Widen to this size.  */
+  const int narrow;		/* Narrow from this size.  */
+  const int toint;
+  const int fromint;
+  const int roundint;		/* V8 round to integral, remains FP format.  */
+};
+
+struct vector_cost_table
+{
+  const int alu;
+};
+
+struct cpu_cost_table
+{
+  const struct alu_cost_table alu;
+  const struct mult_cost_table mult[2]; /* SImode and DImode.  */
+  const struct mem_cost_table ldst;
+  const struct fp_cost_table fp[2]; /* SFmode and DFmode.  */
+  const struct vector_cost_table vect;
+};
+
 
 #endif /* GCC_AARCH_COMMON_PROTOS_H */
