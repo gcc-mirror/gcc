2284b03444d6845ce6402db37f4ec43f6c755f77
Marc Glisse <marc.glisse@inria.fr>
2013-10-03 18:13:54 +0200

    re PR c++/19476 (Missed null checking elimination with new)
    2013-10-03  Marc Glisse  <marc.glisse@inria.fr>
    
    	PR c++/19476
    gcc/c-family/
    	* c.opt (fcheck-new): Move to common.opt.
    
    gcc/
    	* common.opt (fcheck-new): Moved from c.opt. Make it 'Common'.
    	* calls.c (alloca_call_p): Use get_callee_fndecl.
    	* fold-const.c (tree_expr_nonzero_warnv_p): Handle operator new.
    	* tree-vrp.c (gimple_stmt_nonzero_warnv_p, stmt_interesting_for_vrp):
    	Likewise.
    	(vrp_visit_stmt): Remove duplicated code.
    
    gcc/testsuite/
    	* g++.dg/tree-ssa/pr19476-1.C: New file.
    	* g++.dg/tree-ssa/pr19476-2.C: Likewise.
    	* g++.dg/tree-ssa/pr19476-3.C: Likewise.
    	* g++.dg/tree-ssa/pr19476-4.C: Likewise.
    
    From-SVN: r203163

---
+++ gcc/testsuite/g++.dg/tree-ssa/pr19476-3.C
@@ -0,0 +1,11 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 -fcheck-new -fdump-tree-optimized" } */
+
+#include <new>
+
+int g(){
+  return 42 + (0 == new int);
+}
+
+/* { dg-final { scan-tree-dump-not "return 42" "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
