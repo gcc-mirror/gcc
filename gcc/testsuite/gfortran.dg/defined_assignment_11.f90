d61d15b677a281e98cbf26b56a66ba8991a31d39
Tobias Burnus <burnus@net-b.de>
2013-10-04 18:25:55 +0200

    backport: re PR fortran/57697 ([OOP] Segfault with defined assignment for components during intrinsic assignment)
    2013-10-04  Tobias Burnus  <burnus@net-b.de>
    
            Backport from mainline
            2013-09-25  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/57697
            PR fortran/58469
            * resolve.c (generate_component_assignments): Avoid double free
            at runtime and freeing a still-being used expr.
    
    
    2013-10-04  Tobias Burnus  <burnus@net-b.de>
    
            Backport from mainline
            2013-09-25  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/57697
            PR fortran/58469
            * gfortran.dg/defined_assignment_8.f90: New.
            * gfortran.dg/defined_assignment_9.f90: New.
    
    From-SVN: r203207

---
+++ gcc/testsuite/gfortran.dg/defined_assignment_11.f90
@@ -0,0 +1,43 @@
+! { dg-do run }
+!
+! PR fortran/57697
+!
+! Further test of typebound defined assignment
+!
+module m0
+  implicit none
+  type :: component
+    integer :: i = 42
+    integer, allocatable :: b
+  contains
+    procedure :: assign0
+    generic :: assignment(=) => assign0
+  end type
+  type, extends(component) :: comp2
+    real :: aa
+  end type comp2
+  type parent
+    type(component) :: foo
+    real :: cc
+  end type
+  type p2
+    type(parent) :: x
+  end type p2
+contains
+  elemental subroutine assign0(lhs,rhs)
+    class(component), intent(INout) :: lhs
+    class(component), intent(in) :: rhs
+    lhs%i = 20
+  end subroutine
+end module
+
+program main
+  use m0
+  implicit none
+  type(p2), allocatable :: left
+  type(p2) :: right
+!  print *, right%x%foo%i
+  left = right
+!  print *, left%x%foo%i
+  if (left%x%foo%i /= 20) call abort()
+end
