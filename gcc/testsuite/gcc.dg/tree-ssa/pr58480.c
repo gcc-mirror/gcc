0e6a0e48eb5ead024d12453cb811bdf0ef5c3377
Marc Glisse <marc.glisse@inria.fr>
2013-10-08 12:39:49 +0200

    re PR tree-optimization/58480 (Use attribute((nonnull)) to optimize callers)
    2013-10-08  Marc Glisse  <marc.glisse@inria.fr>
    
    	PR tree-optimization/58480
    gcc/
    	* tree-vrp.c (infer_nonnull_range): New function.
    	(infer_value_range): Call infer_nonnull_range.
    
    gcc/testsuite/
    	* gcc.dg/tree-ssa/pr58480.c: New file.
    
    From-SVN: r203271

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr58480.c
@@ -0,0 +1,19 @@
+/* { dg-do compile { target { ! keeps_null_pointer_checks } } } */
+/* { dg-options "-O2 -fdump-tree-vrp1" } */
+
+extern void eliminate (void);
+extern void* f1 (void *a, void *b) __attribute__((nonnull));
+extern void* f2 (void *a, void *b) __attribute__((nonnull(2)));
+void g1 (void*p, void*q){
+  f1 (q, p);
+  if (p == 0)
+    eliminate ();
+}
+void g2 (void*p, void*q){
+  f2 (q, p);
+  if (p == 0)
+    eliminate ();
+}
+
+/* { dg-final { scan-tree-dump-times "Folding predicate\[^\\n\]*to 0" 2 "vrp1" } } */
+/* { dg-final { cleanup-tree-dump "vrp1" } } */
