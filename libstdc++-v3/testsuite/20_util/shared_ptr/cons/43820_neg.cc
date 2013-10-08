61bf02e007ceaaa81f92a9e10372303816955ba5
Jonathan Wakely <jwakely.gcc@gmail.com>
2013-10-08 12:33:37 +0000

    re PR libstdc++/58659 (Construction of shared_ptr from unique_ptr mismatches new/delete and std::allocator for __shared_ptr_count)
    	PR libstdc++/58659
    	* include/bits/shared_ptr_base.h (__shared_count::__shared_count(P,D)):
    	Delegate to constructor taking allocator.
    	(__shared_count::_S_create_from_up): Inline into ...
    	(__shared_count::__shared_count(unique_ptr<Y,D>&&): Here. Use
    	std::conditional instead of constrained overloads. Allocate memory
    	using the allocator type that will be used for deallocation.
    	* testsuite/20_util/shared_ptr/cons/58659.cc: New.
    	* testsuite/20_util/shared_ptr/cons/43820_neg.cc: Adjust.
    
    From-SVN: r203274

--- libstdc++-v3/testsuite/20_util/shared_ptr/cons/43820_neg.cc
+++ libstdc++-v3/testsuite/20_util/shared_ptr/cons/43820_neg.cc
@@ -32,7 +32,7 @@ void test01()
 {
   X* px = 0;
   std::shared_ptr<X> p1(px);   // { dg-error "here" }
-  // { dg-error "incomplete" "" { target *-*-* } 807 }
+  // { dg-error "incomplete" "" { target *-*-* } 778 }
 
   std::shared_ptr<X> p9(ap());  // { dg-error "here" }
   // { dg-error "incomplete" "" { target *-*-* } 307 }
