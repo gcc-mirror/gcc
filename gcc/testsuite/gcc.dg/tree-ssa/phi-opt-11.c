f32420fbacaca6d047155499cbc6e343d9564495
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-10-09 18:57:14 +0000

    * tree-ssa-phiopts.c (rhs_is_fed_for_value_replacement): New function. 	(operand_equal_for_value_replacement): New function, extracted from 	value_replacement and enhanced to catch more cases. 	(value_replacement): Use operand_equal_for_value_replacement.
    	* gcc.dg/tree-ssa/phi-opt-11.c: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@203327 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/phi-opt-11.c
@@ -0,0 +1,25 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-optimized" } */
+
+int f(int a, int b, int c)
+{
+  if (a == 0 && b > c)
+   return 0;
+ return a;
+}
+
+int g(int a, int b, int c)
+{
+  if (a == 42 && b > c)
+   return 42;
+ return a;
+}
+
+int h(int a, int b, int c, int d)
+{
+  if (a == d && b > c)
+   return d;
+ return a;
+}
+/* { dg-final { scan-tree-dump-times "if" 0 "optimized"} } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
