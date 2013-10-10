8b093247c45d4a859e1fef4e3394125c78bb9a18
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-10-10 10:37:53 +0000

    2013-10-10  Robert Dewar  <dewar@adacore.com>
    	* sem_ch3.adb: Minor reformatting.
    
    2013-10-10  Robert Dewar  <dewar@adacore.com>
    
    	* sinput-c.adb (Load_File): Ensure Source_Align alignment.
    	* sinput-d.adb (Create_Debug_Source): Ensure Source_Align alignment.
    	* sinput-l.adb (Create_Instantiation_Source): Ensure Source_Align
    	alignment.
    	(Load_File): Ditto.
    	* sinput.ads, sinput.adb (Get_Source_File_Index): New optimized (single
    	line) version.
    	* types.ads (Source_Align): New definition.
    	(Source_Buffer): Document new alignment requirement.
    
    2013-10-10  Robert Dewar  <dewar@adacore.com>
    
    	* sem_prag.adb (Analyze_Pragma, case Linker_Section): Allow
    	this for types.
    
    2013-10-10  Robert Dewar  <dewar@adacore.com>
    
    	* gnat_rm.texi: Minor adjustment to doc for To_Address attribute.
    
    2013-10-10  Vadim Godunko  <godunko@adacore.com>
    
    	* s-stopoo.ads (Root_Storage_Pool): Add pragma
    	Preelaborable_Initialization.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@203343 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/types.ads
+++ gcc/ada/types.ads
@@ -183,11 +183,17 @@ package Types is
    No_Column_Number : constant Column_Number := 0;
    --  Special value used to indicate no column number
 
+   Source_Align : constant := 2 ** 12;
+   --  Alignment requirement for source buffers (by keeping source buffers
+   --  aligned, we can optimize the implementation of Get_Source_File_Index.
+   --  See this routine in Sinput for details.
+
    subtype Source_Buffer is Text_Buffer;
    --  Type used to store text of a source file. The buffer for the main
    --  source (the source specified on the command line) has a lower bound
    --  starting at zero. Subsequent subsidiary sources have lower bounds
-   --  which are one greater than the previous upper bound.
+   --  which are one greater than the previous upper bound, rounded up to
+   --  a multiple of Source_Align.
 
    subtype Big_Source_Buffer is Text_Buffer (0 .. Text_Ptr'Last);
    --  This is a virtual type used as the designated type of the access type
