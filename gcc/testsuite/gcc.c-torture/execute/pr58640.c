ef3cfba23d0f9c518cc71ff053994e77e80f1d1a
Jeff Law <law@redhat.com>
2013-10-11 14:31:25 -0600

    re PR tree-optimization/58640 (wrong code (segfaults) at -O3 on x86_64-linux-gnu)
    	PR tree-optimization/58640
    	* tree-ssa-threadupdate.c (mark_threaded_blocks): Truncate jump threading
    	paths that cross over two loop entry points.
    
    	* gcc.c-torture/execute/pr58640.c: New test.
    
    From-SVN: r203463

---
+++ gcc/testsuite/gcc.c-torture/execute/pr58640.c
@@ -0,0 +1,32 @@
+int a, b, c, d = 1, e;
+
+static signed char
+foo ()
+{
+  int f, g = a;
+
+  for (f = 1; f < 3; f++)
+    for (; b < 1; b++)
+      {
+        if (d)
+          for (c = 0; c < 4; c++)
+            for (f = 0; f < 3; f++)
+              {
+                for (e = 0; e < 1; e++)
+                  a = g;
+                if (f)
+                  break;
+              }
+        else if (f)
+          continue;
+        return 0;
+      }
+  return 0;
+}
+
+int
+main ()
+{
+  foo ();
+  exit (0);
+}
