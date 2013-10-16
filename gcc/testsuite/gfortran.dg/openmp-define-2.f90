23f4e3a5fbec5b852ef78ddb2f07f344c259ae63
tschwinge <tschwinge@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-10-16 10:53:36 +0000

    gcc/testsuite/ 	* c-c++-common/cpp/openmp-define-1.c: Move 	dg-require-effective-target fopenmp after dg-do directive. 	* c-c++-common/cpp/openmp-define-2.c: Likewise. 	* gfortran.dg/openmp-define-1.f90: Likewise. 	* gfortran.dg/openmp-define-2.f90: Likewise. 	* gfortran.dg/openmp-define-3.f90: Likewise.
    Completion of r203588, fix-up for r203417.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@203697 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/openmp-define-2.f90
+++ gcc/testsuite/gfortran.dg/openmp-define-2.f90
@@ -1,6 +1,6 @@
-! { dg-require-effective-target fopenmp }
 ! { dg-options "-cpp -fno-openmp" }
 ! { dg-do preprocess }
+! { dg-require-effective-target fopenmp }
 
 #ifdef _OPENMP
 # error _OPENMP defined
