1abe98f5e1399913d42eeea5fba17ca162deb48c
mrs <mrs@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-10-16 18:25:31 +0000

    * .dir-locals.el: Add.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@203715 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ .dir-locals.el
@@ -0,0 +1,24 @@
+;; Emacs settings.
+;; Copyright (C) 2012-2013 Free Software Foundation, Inc.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(
+ (tcl-mode . ((tcl-indent-level . 4)
+	      (tcl-continued-indent-level . 4)
+	      (indent-tabs-mode . t)))
+ (nil . ((bug-reference-url-format . "http://gcc.gnu.org/PR%s")))
+ (c-mode . ((c-file-style . "GNU")
+	    (indent-tabs-mode . t)))
+)
