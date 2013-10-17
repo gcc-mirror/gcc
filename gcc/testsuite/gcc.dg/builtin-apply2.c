92fd70fbd1c8e2cde05da1c2c27ff973d35242ca
Charles Baylis <charles.baylis@linaro.org>
2013-10-17 22:57:21 +0200

    builtin-apply2.c: Skip test on arm hardfloat ABI targets.
    2013-10-17  Charles Bayis  <charles.baylis@linaro.org>
    
    	* gcc.dg/builtin-apply2.c: Skip test on arm hardfloat ABI targets.
    	* gcc.dg/tls/pr42894.c: Remove dg-options for arm*-*-* targets.
    	* gcc.target/arm/thumb-ltu.c: Remove dg-skip-if and require
    	effective target arm_thumb1_ok.
    	* lib/target-supports.exp
    	(check_effective_target_arm_fp16_ok_nocache): Don't force
    	-mfloat-abi=soft when building for hardfloat target.
    
    From-SVN: r203799

--- gcc/testsuite/gcc.dg/builtin-apply2.c
+++ gcc/testsuite/gcc.dg/builtin-apply2.c
@@ -1,6 +1,6 @@
 /* { dg-do run } */
 /* { dg-skip-if "Variadic funcs have all args on stack. Normal funcs have args in registers." { "aarch64*-*-* avr-*-* " } { "*" } { "" } } */
-/* { dg-skip-if "Variadic funcs use Base AAPCS.  Normal funcs use VFP variant." { "arm*-*-*" } { "-mfloat-abi=hard" } { "" } } */
+/* { dg-skip-if "Variadic funcs use Base AAPCS.  Normal funcs use VFP variant." { arm*-*-* && arm_hf_eabi } { "*" } { "" } } */
 
 /* PR target/12503 */
 /* Origin: <pierre.nguyen-tuong@asim.lip6.fr> */
--- gcc/testsuite/gcc.dg/tls/pr42894.c
+++ gcc/testsuite/gcc.dg/tls/pr42894.c
@@ -1,6 +1,5 @@
 /* PR target/42894 */
 /* { dg-do compile } */
-/* { dg-options "-march=armv5te -mthumb" { target arm*-*-* } } */
 /* { dg-require-effective-target tls } */
 
 extern __thread int t;
