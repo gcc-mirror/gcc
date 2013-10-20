f5b9d5e877cff9d344645362dc57980a6c747e2c
Chris Jefferson <chris@bubblescope.net>
2013-10-20 09:07:36 +0000

    re PR libstdc++/58800 (std::nth_element segfaults on valid input)
    2013-10-20  Chris Jefferson  <chris@bubblescope.net>
    	    Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR libstdc++/58800
    	* include/bits/stl_algo.h (__unguarded_partition_pivot): Change
    	__last - 2 to __last - 1.
    	* testsuite/25_algorithms/nth_element/58800.cc: New
    
    Co-Authored-By: Paolo Carlini <paolo.carlini@oracle.com>
    
    From-SVN: r203872

--- libstdc++-v3/include/bits/stl_algo.h
+++ libstdc++-v3/include/bits/stl_algo.h
@@ -1917,7 +1917,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 				_RandomAccessIterator __last, _Compare __comp)
     {
       _RandomAccessIterator __mid = __first + (__last - __first) / 2;
-      std::__move_median_to_first(__first, __first + 1, __mid, (__last - 2),
+      std::__move_median_to_first(__first, __first + 1, __mid, __last - 1,
 				  __comp);
       return std::__unguarded_partition(__first + 1, __last, __first, __comp);
     }
