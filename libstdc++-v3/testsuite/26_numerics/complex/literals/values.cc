ae5543e66213b694f07474cc98fba9d4c5b0defc
Ed Smith-Rowland <3dw4rd@verizon.net>
2013-10-23 00:09:19 +0000

    Implement N3779 - User-defined Literals for std::complex, part 2 of UDL for Standard Library Types
    2013-09-27  Ed Smith-Rowland  <3dw4rd@verizon.net>
    
    	Implement N3779 - User-defined Literals for std::complex,
    	part 2 of UDL for Standard Library Types
    	* include/std/complex: Add complex literal operators.
    	* testsuite/26_numerics/complex/literals/types.cc: New.
    	* testsuite/26_numerics/complex/literals/values.cc: New.
    
    From-SVN: r203940

---
+++ libstdc++-v3/testsuite/26_numerics/complex/literals/values.cc
@@ -0,0 +1,48 @@
+// { dg-options "-std=gnu++1y" }
+// { dg-do run }
+
+// Copyright (C) 2013 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <complex>
+#include <testsuite_hooks.h>
+
+void
+test02()
+{
+  using namespace std::literals::complex_literals;
+
+  std::complex<float> j1 = 1.0if;
+  std::complex<float> k1 = 1if;
+  std::complex<double> j2 = 2.0i;
+  std::complex<double> k2 = 2i;
+  std::complex<long double> j4 = 4.0il;
+  std::complex<long double> k4 = 4il;
+
+  VERIFY( j1 == std::complex<float>(0.0F, 1.0F) );
+  VERIFY( k1 == std::complex<float>(0.0F, 1.0F) );
+  VERIFY( j2 == std::complex<double>(0.0, 2.0) );
+  VERIFY( k2 == std::complex<double>(0.0, 2.0) );
+  VERIFY( j4 == std::complex<long double>(0.0L, 4.0L) );
+  VERIFY( k4 == std::complex<long double>(0.0L, 4.0L) );
+}
+
+int
+main()
+{
+  test02();
+}
