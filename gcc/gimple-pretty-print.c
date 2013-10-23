d09c0e9bfb662a3edb7beb1771bfc2f7983886b8
Jakub Jelinek <jakub@redhat.com>
2013-10-23 08:32:23 +0200

    gimple-pretty-print.c (dump_ssaname_info): Always print "# " before the info, not after it.
    	* gimple-pretty-print.c (dump_ssaname_info): Always print "# " before
    	the info, not after it.
    	(gump_gimple_phi): Add COMMENT argument, if true, print "# " after
    	dump_ssaname_info call.
    	(pp_gimple_stmt_1): Adjust caller.
    	(dump_phi_nodes): Likewise.  Don't print "# " here.
    
    From-SVN: r203947

--- gcc/gimple-pretty-print.c
+++ gcc/gimple-pretty-print.c
@@ -1714,7 +1714,7 @@ dump_ssaname_info (pretty_printer *buffer, tree node, int spc)
     {
       unsigned int align, misalign;
       struct ptr_info_def *pi = SSA_NAME_PTR_INFO (node);
-      pp_string (buffer, "PT = ");
+      pp_string (buffer, "# PT = ");
       pp_points_to_solution (buffer, &pi->pt);
       newline_and_indent (buffer, spc);
       if (get_ptr_info_alignment (pi, &align, &misalign))
@@ -1722,7 +1722,6 @@ dump_ssaname_info (pretty_printer *buffer, tree node, int spc)
 	  pp_printf (buffer, "# ALIGN = %u, MISALIGN = %u", align, misalign);
 	  newline_and_indent (buffer, spc);
 	}
-      pp_string (buffer, "# ");
     }
 
   if (!POINTER_TYPE_P (TREE_TYPE (node))
@@ -1732,7 +1731,7 @@ dump_ssaname_info (pretty_printer *buffer, tree node, int spc)
       value_range_type range_type = get_range_info (node, &min, &max);
 
       if (range_type == VR_VARYING)
-	pp_printf (buffer, "# RANGE  VR_VARYING");
+	pp_printf (buffer, "# RANGE VR_VARYING");
       else if (range_type == VR_RANGE || range_type == VR_ANTI_RANGE)
 	{
 	  pp_printf (buffer, "# RANGE ");
@@ -1749,16 +1748,20 @@ dump_ssaname_info (pretty_printer *buffer, tree node, int spc)
 
 /* Dump a PHI node PHI.  BUFFER, SPC and FLAGS are as in pp_gimple_stmt_1.
    The caller is responsible for calling pp_flush on BUFFER to finalize
-   pretty printer.  */
+   pretty printer.  If COMMENT is true, print this after #.  */
 
 static void
-dump_gimple_phi (pretty_printer *buffer, gimple phi, int spc, int flags)
+dump_gimple_phi (pretty_printer *buffer, gimple phi, int spc, bool comment,
+		 int flags)
 {
   size_t i;
   tree lhs = gimple_phi_result (phi);
 
   if (flags & TDF_ALIAS)
     dump_ssaname_info (buffer, lhs, spc);
+
+  if (comment)
+    pp_string (buffer, "# ");
 
   if (flags & TDF_RAW)
     dump_gimple_fmt (buffer, spc, flags, "%G <%T, ", phi,
@@ -2095,7 +2098,7 @@ pp_gimple_stmt_1 (pretty_printer *buffer, gimple gs, int spc, int flags)
       break;
 
     case GIMPLE_PHI:
-      dump_gimple_phi (buffer, gs, spc, flags);
+      dump_gimple_phi (buffer, gs, spc, false, flags);
       break;
 
     case GIMPLE_OMP_PARALLEL:
@@ -2271,8 +2274,7 @@ dump_phi_nodes (pretty_printer *buffer, basic_block bb, int indent, int flags)
       if (!virtual_operand_p (gimple_phi_result (phi)) || (flags & TDF_VOPS))
         {
           INDENT (indent);
-          pp_string (buffer, "# ");
-          dump_gimple_phi (buffer, phi, indent, flags);
+	  dump_gimple_phi (buffer, phi, indent, true, flags);
           pp_newline (buffer);
         }
     }
