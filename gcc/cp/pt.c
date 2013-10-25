42681840e394597dfdd824248e4207d349b51187
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-10-25 16:26:10 +0000

    /cp 2013-10-25  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/58878
    	* pt.c (check_template_shadow): Don't skip declarations in inline
    	member templates.
    
    /testsuite
    2013-10-25  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/58878
    	* g++.dg/template/pr58878.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@204071 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -3511,7 +3511,8 @@ check_template_shadow (tree decl)
      name inside a class.  We check TPFI to avoid duplicate errors for
      inline member templates.  */
   if (decl == olddecl
-      || TEMPLATE_PARMS_FOR_INLINE (current_template_parms))
+      || (DECL_TEMPLATE_PARM_P (decl)
+	  && TEMPLATE_PARMS_FOR_INLINE (current_template_parms)))
     return true;
 
   error ("declaration of %q+#D", decl);
