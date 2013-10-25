3ff4317f1cfddf2e015db24a673e3ea5bf188920
François Dumont <fdumont@gcc.gnu.org>
2013-10-25 19:25:06 +0000

    formatter.h (__check_singular): Add const on iterator reference.
    2013-10-25  François Dumont  <fdumont@gcc.gnu.org>
    
    	* include/debug/formatter.h (__check_singular): Add const on
    	iterator reference.
    	* include/debug/functions.h (__check_singular): Likewise.
    	(__check_singular(const _Safe_iterator<_Ite, _Seq>&)): Delete.
    	(__check_dereferenceable(const _Ite&)): Add const on iterator
    	reference.
    	(__check_dereferenceable(const _Safe_local_iterator<>&)): New.
    	* include/debug/safe_iterator.h (__check_singular_aux): Review
    	comment.
    	* testsuite/23_containers/vector/debug/debug_functions.cc: New.
    	* testsuite/23_containers/unordered_set/debug/debug_functions.cc:
    	New.
    
    From-SVN: r204075

---
+++ libstdc++-v3/testsuite/23_containers/unordered_set/debug/debug_functions.cc
@@ -0,0 +1,92 @@
+// Copyright (C) 2013 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+//
+// { dg-options "-std=gnu++11" }
+// { dg-require-debug-mode "" }
+
+#include <unordered_set>
+#include <testsuite_hooks.h>
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace __gnu_debug;
+
+  std::unordered_set<int> u = { 0, 1, 2 };
+  VERIFY( __check_dereferenceable(u.begin()) );
+  auto it = u.begin();
+  VERIFY( __check_dereferenceable(it) );
+
+  VERIFY( __check_dereferenceable(u.cbegin()) );
+  auto cit = u.begin();
+  VERIFY( __check_dereferenceable(cit) );
+
+  VERIFY( !__check_dereferenceable(u.end()) );
+  it = u.end();
+  VERIFY( !__check_dereferenceable(it) );
+
+  auto bucket = u.bucket(0);
+  VERIFY( __check_dereferenceable(u.begin(bucket)) );
+  auto lit = u.begin(bucket);
+  VERIFY( __check_dereferenceable(lit) );
+
+  VERIFY( !__check_dereferenceable(u.end(bucket)) );
+}
+
+void test02()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace __gnu_debug;
+
+  std::unordered_set<int> u = { 0, 1, 2 };
+
+  VERIFY( !__check_singular(u.end()) );
+  auto it = u.end();
+  VERIFY( !__check_singular(it) );
+
+  VERIFY( !__check_singular(u.begin()) );
+  it = u.begin();
+  VERIFY( !__check_singular(it) );
+
+  u.clear();
+
+  VERIFY( it._M_singular() );
+  VERIFY( __check_singular(it) );
+
+  it = u.end();
+  VERIFY( !it._M_singular() );
+  VERIFY( !__check_singular(it) );
+
+  u = { 0, 1, 2 };
+
+  auto bucket = u.bucket(0);
+  VERIFY( !__check_singular(u.begin(bucket)) );
+  auto lit = u.begin(bucket);
+  VERIFY( !__check_singular(lit) );
+
+  VERIFY( !__check_singular(u.end(bucket)) );
+
+  u.clear();
+  VERIFY( __check_singular(lit) );
+}
+
+int main()
+{
+  test01();
+  test02();
+  return 0;
+}
