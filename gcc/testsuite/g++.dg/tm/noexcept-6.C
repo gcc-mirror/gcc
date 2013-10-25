08dea2d4edd6fee3beaa6097e9c8c0b7f57485c4
Tom de Vries <tom@codesourcery.com>
2013-10-25 13:49:48 +0000

    re PR c++/58282 (g++.dg/tm/noexcept-1.C -fno-exceptions SIGSEGV in gimple_build_eh_must_not_throw)
    2013-10-25  Tom de Vries  <tom@codesourcery.com>
    
    	PR c++/58282
    	* except.c (build_must_not_throw_expr): Handle
    	flag_exceptions.
    
    	* g++.dg/tm/noexcept-6.C: New test.
    
    From-SVN: r204067

---
+++ gcc/testsuite/g++.dg/tm/noexcept-6.C
@@ -0,0 +1,23 @@
+// { dg-do compile }
+// { dg-options "-fno-exceptions -fgnu-tm -O -std=c++0x -fdump-tree-tmlower" }
+
+struct TrueFalse
+{
+  static constexpr bool v() { return true; }
+};
+
+int global;
+
+template<typename T> int foo()
+{
+  return __transaction_atomic noexcept(T::v()) (global + 1);
+}
+
+int f1()
+{
+  return foo<TrueFalse>();
+}
+
+/* { dg-final { scan-tree-dump-times "eh_must_not_throw" 0 "tmlower" } } */
+/* { dg-final { scan-tree-dump-times "__transaction_atomic" 1 "tmlower" } } */
+/* { dg-final { cleanup-tree-dump "tmlower" } } */
