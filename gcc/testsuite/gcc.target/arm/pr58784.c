a26891f4f59a90038cab4067a06553d362c46de7
Vladimir Makarov <vmakarov@redhat.com>
2013-10-30 15:04:39 +0000

    re PR target/58784 ([ARM] LRA legitimate address issue with misalign neon_store)
    2013-10-30  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR target/58784
    	* lra.c (check_rtl): Remove address check before LRA work.
    
    2013-10-30  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR target/58784
    	* gcc.target/arm/pr58784.c: New.
    
    From-SVN: r204215

---
+++ gcc/testsuite/gcc.target/arm/pr58784.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-march=armv7-a -mfloat-abi=hard -mfpu=neon -marm -O2" } */
+
+typedef struct __attribute__ ((__packed__))
+{
+    char valueField[2];
+} ptp_tlv_t;
+typedef struct __attribute__ ((__packed__))
+{
+    char stepsRemoved;
+    ptp_tlv_t tlv[1];
+} ptp_message_announce_t;
+int ptplib_send_announce(int sequenceId, int i)
+{
+    ptp_message_announce_t tx_packet;
+    ((long long *)tx_packet.tlv[0].valueField)[sequenceId] = i;
+    f(&tx_packet);
+}
