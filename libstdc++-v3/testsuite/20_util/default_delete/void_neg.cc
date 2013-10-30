8d52ed5c815d2dd68df04b621e1d339e54b938f9
Jonathan Wakely <jwakely.gcc@gmail.com>
2013-10-30 19:48:52 +0000

    shared_ptr (__shared_ptr): Assert non-void pointer.
    	* include/bits/shared_ptr (__shared_ptr): Assert non-void pointer.
    	* include/bits/shared_ptr (default_delete): Likewise.
    	* testsuite/20_util/shared_ptr/cons/58839.cc: Do not use
    	default_delete<void>.
    	* testsuite/20_util/shared_ptr/cons/void_neg.cc: New.
    	* testsuite/20_util/default_delete/void_neg.cc: New.
    	* testsuite/20_util/shared_ptr/cons/43820_neg.cc: Adjust line numbers.
    	* testsuite/20_util/unique_ptr/assign/48635_neg.cc: Likewise.
    
    From-SVN: r204225

--- libstdc++-v3/testsuite/20_util/default_delete/void_neg.cc
+++ libstdc++-v3/testsuite/20_util/default_delete/void_neg.cc
@@ -1,7 +1,7 @@
-// { dg-options "-std=gnu++0x" }
+// { dg-options "-std=gnu++11" }
 // { dg-do compile }
 
-// Copyright (C) 2010-2013 Free Software Foundation, Inc.
+// Copyright (C) 2013 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -18,22 +18,13 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// 20.9.11.2 Template class shared_ptr [util.smartptr.shared]
+// 20.8.1.1 Default deleters [util.ptr.dltr]
 
 #include <memory>
 
-// incomplete type
-struct X;
-
-// get an auto_ptr rvalue
-std::auto_ptr<X>&& ap();
-
 void test01()
 {
-  X* px = 0;
-  std::shared_ptr<X> p1(px);   // { dg-error "here" }
-  // { dg-error "incomplete" "" { target *-*-* } 779 }
-
-  std::shared_ptr<X> p9(ap());  // { dg-error "here" }
-  // { dg-error "incomplete" "" { target *-*-* } 307 }
+  std::default_delete<void> d;
+  d(nullptr);   // { dg-error "here" }
+  // { dg-error "incomplete" "" { target *-*-* } 72 }
 }
