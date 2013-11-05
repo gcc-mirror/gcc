32297e69237c121ab693e69d352ed0ce83246c3d
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-11-05 18:03:03 +0000

    PR c++/58868 	* decl.c (check_initializer): Don't use build_vec_init for arrays 	of trivial type.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@204406 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/decl.c
+++ gcc/cp/decl.c
@@ -5683,6 +5683,7 @@ check_initializer (tree decl, tree init, int flags, vec<tree, va_gc> **cleanups)
 	  && !(init && BRACE_ENCLOSED_INITIALIZER_P (init)
 	       && CP_AGGREGATE_TYPE_P (type)
 	       && (CLASS_TYPE_P (type)
+		   || !TYPE_NEEDS_CONSTRUCTING (type)
 		   || type_has_extended_temps (type))))
 	{
 	  init_code = build_aggr_init_full_exprs (decl, init, flags);
