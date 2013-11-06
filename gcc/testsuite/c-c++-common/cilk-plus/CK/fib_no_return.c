28d31e407447df98a843559b204bc01c40577555
Balaji V. Iyer <balaji.v.iyer@intel.com>
2013-11-06 14:55:46 +0000

    fib.c: Reduced the iteration from 40 to 30.
    2013-11-05  Balaji V. Iyer  <balaji.v.iyer@intel.com>
    
            * c-c++-common/cilk-plus/CK/fib.c: Reduced the iteration from
            40 to 30.  Replaced iteration variable with a #define.  Instead of
            returning non-zero value for error, called __builtin_abort ().  Fixed
            a bug of calling fib_serial in serial case instead of fib.
            * c-c++-common/cilk-plus/CK/fib_init_expr_xy.c: Likewise.
            * c-c++-common/cilk-plus/CK/fib_no_return.c: Likewise.
            * c-c++-common/cilk-plus/CK/fib_no_sync.c: Likewise.
            * gcc.dg/cilk-plus/cilk-plus.exp: Removed duplicate/un-necessary
            compiler flag testing.
    
    From-SVN: r204457

--- gcc/testsuite/c-c++-common/cilk-plus/CK/fib_no_return.c
+++ gcc/testsuite/c-c++-common/cilk-plus/CK/fib_no_return.c
@@ -6,35 +6,43 @@
 #include <stdio.h>
 #endif
 
+#define FIB_ITERATION 30
+
 void fib        (int *, int);
 int fib_serial (int);
 
 int main(void)
 {
   int ii = 0, error = 0;
-  int fib_result[41], fib_serial_result[41];
+  int fib_result[FIB_ITERATION+1], fib_serial_result[FIB_ITERATION+1];
 
 #if HAVE_IO
-  for (ii = 0; ii <= 40; ii++)
+  for (ii = 0; ii <= FIB_ITERATION; ii++)
     {
       int result = 0;
       fib (&result, ii); 
       printf("fib (%2d) = %10d\n", ii, result);
     }
 #else
-  for (ii = 0; ii <= 40; ii++)
+  for (ii = 0; ii <= FIB_ITERATION; ii++)
     {
       fib (&fib_result[ii], ii);
-      fib_serial_result[ii] = fib_serial (ii);
     }
-
-  for (ii = 0; ii <= 40; ii++)
+  
+  fib_serial_result[0] = 0;
+  fib_serial_result[1] = 1;
+  
+  for (ii = 2; ii <= FIB_ITERATION; ii++)
+    fib_serial_result[ii] = fib_serial_result[ii-1] + fib_serial_result[ii-2];
+  
+  for (ii = 0; ii <= FIB_ITERATION; ii++)
     {
       if (fib_result[ii] != fib_serial_result[ii])
-	error = 1;
+	__builtin_abort ();
     }
 #endif
-  return error;
+  
+  return 0;
 }
 
 int fib_serial (int n)
@@ -44,8 +52,8 @@ int fib_serial (int n)
     return n;
   else
     {
-      fib (&x, n-1);
-      fib (&y, n-2);
+      x = fib_serial (n-1);
+      y = fib_serial (n-2);
       return (x+y);
     }
 }
