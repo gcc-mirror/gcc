82f63633f221cef68dc1c33f27a0770d792c28df
Wei Mi <wmi@google.com>
2013-11-06 02:46:17 +0000

    re PR regression/58985 (gcc.dg/pr57518.c scan-rtl-dump-not ira REG_EQUIV...)
    2013-11-05  Wei Mi  <wmi@google.com>
    
            PR regression/58985
            * testsuite/gcc.dg/pr57518.c: Add subreg in regexp pattern.
    
    From-SVN: r204438

--- gcc/testsuite/gcc.dg/pr57518.c
+++ gcc/testsuite/gcc.dg/pr57518.c
@@ -1,8 +1,8 @@
-/* PR rtl-optimization/57130 */
+/* PR rtl-optimization/57518 */
 
 /* { dg-do compile } */
 /* { dg-options "-O2 -fdump-rtl-ira" } */
-/* { dg-final { scan-rtl-dump-not "REG_EQUIV\[^\n\]*mem\[^\n\]*\"ip\"" "ira" } } */
+/* { dg-final { scan-rtl-dump-not "REG_EQUIV\[^\n\]*mem\[^\n\]*\"ip\".*subreg" "ira" } } */
 
 char ip[10];
 int total;
