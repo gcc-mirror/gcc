2dcf5fe1d268373e40d22e5fc4c656c97d181714
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-11-07 09:58:05 +0000

    Use Pmode with stack_pointer_rtx
    gcc/
    
    	PR target/59034
    	* config/i386/i386.md (push peepholer/splitter): Use Pmode
    	with stack_pointer_rtx.
    
    gcc/testsuite/
    
    	PR target/59034
    	* gcc.target/i386/pr59034-1.c: New test.
    	* gcc.target/i386/pr59034-2.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@204501 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr59034-1.c
@@ -0,0 +1,10 @@
+/* { dg-do compile { target { ! { ia32 } } } } */
+/* { dg-require-effective-target maybe_x32 } */
+/* { dg-options "-O -mx32 -mtune=corei7 -maddress-mode=short" } */
+
+extern int foo(int, ...);
+int bar(void) {
+  long double l = 1.2345E6;
+  foo(0, l);
+  return 0;
+}
