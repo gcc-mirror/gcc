39b2468cf60b8351346882214da7cbb3d7db7fd1
Joseph Myers <jsm28@gcc.gnu.org>
2013-11-08 22:15:09 +0000

    [multiple changes]
    2013-11-08  Andrew MacLeod  <amacleod@redhat.com>
    	    Joseph Myers  <joseph@codesourcery.com>
    
    	* ginclude/stdatomic.h: New file.
    	* Makefile.in (USER_H): Add stdatomic.h.
    
    testsuite:
    2013-11-08  Joseph Myers  <joseph@codesourcery.com>
    
    	* gcc.dg/atomic/stdatomic-compare-exchange-1.c,
    	gcc.dg/atomic/stdatomic-compare-exchange-2.c,
    	gcc.dg/atomic/stdatomic-compare-exchange-3.c,
    	gcc.dg/atomic/stdatomic-compare-exchange-4.c,
    	gcc.dg/atomic/stdatomic-exchange-1.c,
    	gcc.dg/atomic/stdatomic-exchange-2.c,
    	gcc.dg/atomic/stdatomic-exchange-3.c,
    	gcc.dg/atomic/stdatomic-exchange-4.c,
    	gcc.dg/atomic/stdatomic-fence.c, gcc.dg/atomic/stdatomic-flag.c,
    	gcc.dg/atomic/stdatomic-generic.c,
    	gcc.dg/atomic/stdatomic-kill-dep.c,
    	gcc.dg/atomic/stdatomic-load-1.c,
    	gcc.dg/atomic/stdatomic-load-2.c,
    	gcc.dg/atomic/stdatomic-load-3.c,
    	gcc.dg/atomic/stdatomic-load-4.c,
    	gcc.dg/atomic/stdatomic-lockfree.c,
    	gcc.dg/atomic/stdatomic-op-1.c, gcc.dg/atomic/stdatomic-op-2.c,
    	gcc.dg/atomic/stdatomic-op-3.c, gcc.dg/atomic/stdatomic-op-4.c,
    	gcc.dg/atomic/stdatomic-store-1.c,
    	gcc.dg/atomic/stdatomic-store-2.c,
    	gcc.dg/atomic/stdatomic-store-3.c,
    	gcc.dg/atomic/stdatomic-store-4.c, gcc.dg/c11-stdatomic-1.c: New
    	tests.
    
    From-SVN: r204603

---
+++ gcc/testsuite/gcc.dg/atomic/stdatomic-flag.c
@@ -0,0 +1,38 @@
+/* Test atomic_flag routines for existence and execution.  */
+/* { dg-do run } */
+/* { dg-options "-std=c11 -pedantic-errors" } */
+
+#include <stdatomic.h>
+
+extern void abort (void);
+atomic_flag a = ATOMIC_FLAG_INIT;
+
+int
+main ()
+{
+  int b;
+
+  if (!atomic_is_lock_free (&a))
+    abort ();
+
+  if (atomic_flag_test_and_set (&a))
+    abort ();
+  atomic_flag_clear_explicit (&a, memory_order_relaxed);
+  if (atomic_flag_test_and_set (&a))
+    abort ();
+  atomic_flag_clear (&a);
+
+  b = atomic_flag_test_and_set_explicit (&a, memory_order_seq_cst);
+  if (!atomic_flag_test_and_set (&a) || b != 0)
+    abort ();
+
+  b = atomic_flag_test_and_set_explicit (&a, memory_order_acq_rel);
+  if (!atomic_flag_test_and_set (&a) || b != 1)
+    abort ();
+
+  atomic_flag_clear_explicit (&a, memory_order_seq_cst);
+  if (atomic_flag_test_and_set (&a))
+    abort ();
+
+  return 0;
+}
