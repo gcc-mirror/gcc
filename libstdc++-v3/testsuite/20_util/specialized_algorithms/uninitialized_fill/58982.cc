5275f3e54604ae1e7143cd32d2cd45eae53b5d19
Jonathan Wakely <jwakely.gcc@gmail.com>
2013-11-09 12:38:00 +0000

    re PR libstdc++/58982 (std::vector<std::atomic<int>> vai(10); does not compile anymore)
    	PR libstdc++/58982
    	* include/bits/stl_algobase.h (__copy_move::__copy_m): Use assertion
    	to prevent using memmove() on non-assignable types.
    	(__copy_move_backward::__copy_move_b): Likewise.
    	* include/bits/stl_uninitialized.h (uninitialized_copy
    	uninitialized_copy_n, uninitialized_fill, uninitialized_fill_n,
    	__uninitialized_default, __uninitialized_default_n): Check for
    	assignable as well as trivial.
    	* testsuite/20_util/specialized_algorithms/uninitialized_copy/
    	58982.cc: New.
    	* testsuite/20_util/specialized_algorithms/uninitialized_copy_n/
    	58982.cc: New.
    	* testsuite/20_util/specialized_algorithms/uninitialized_fill/
    	58982.cc: New.
    	* testsuite/20_util/specialized_algorithms/uninitialized_fill_n/
    	58982.cc: New.
    	* testsuite/25_algorithms/copy/58982.cc: New.
    	* testsuite/25_algorithms/copy_n/58982.cc: New.
    
    From-SVN: r204615

---
+++ libstdc++-v3/testsuite/20_util/specialized_algorithms/uninitialized_fill/58982.cc
@@ -0,0 +1,41 @@
+// Copyright (C) 2013 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 20.7.12 specialized algorithms
+
+// { dg-options "-std=gnu++11" }
+// { dg-do compile }
+
+#include <memory>
+
+// libstdc++/58982
+
+// trivial class that is not assignable
+struct T
+{
+  T() = default;
+  ~T() = default;
+
+  T& operator=(const T&) = delete;
+};
+
+void
+test01(T* first, T* last)
+{
+  T t;
+  std::uninitialized_fill(first, last, t);
+}
---
+++ libstdc++-v3/testsuite/25_algorithms/copy/58982.cc
@@ -0,0 +1,42 @@
+// Copyright (C) 2013 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 25.3.1 copy
+
+// { dg-options "-std=gnu++11" }
+// { dg-do compile }
+
+#include <algorithm>
+
+// libstdc++/58982
+
+// trivial class that is not assignable
+struct T
+{
+  T() = default;
+  ~T() = default;
+
+  T& operator=(const T&) = delete;
+};
+
+void
+test01(T* result)
+{
+  T t[1];
+  std::copy(t, t+1, result); // { dg-error "here" }
+}
+// { dg-prune-output "not assignable" }
