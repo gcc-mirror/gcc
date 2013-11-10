024d645a0db64ac79bdb6cda0339a3d4d26e288b
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-11-10 11:47:18 +0000

    2013-11-10  Kai Tietz  <ktietz@redhat.com>
    	* config/i386/cygming-crtbegin.c (__gcc_register_frame):
    	Increment load-count on use of LIBGCC_SONAME DLL.
    	(hmod_libgcc): New static variable to hold handle of
    	LIBGCC_SONAME DLL.
    	(__gcc_deregister_frame): Decrement load-count of
    	LIBGCC_SONAME DLL.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@204635 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgcc/config/i386/cygming-crtbegin.c
+++ libgcc/config/i386/cygming-crtbegin.c
@@ -91,6 +91,9 @@ static EH_FRAME_SECTION_CONST char __EH_FRAME_BEGIN__[]
   = { };
 
 static struct object obj;
+
+/* Handle of libgcc's DLL reference.  */
+HANDLE hmod_libgcc;
 #endif
 
 #if TARGET_USE_JCR_SECTION
@@ -115,9 +118,14 @@ __gcc_register_frame (void)
 
   void (*register_frame_fn) (const void *, struct object *);
   HANDLE h = GetModuleHandle (LIBGCC_SONAME);
+
   if (h)
-    register_frame_fn = (void (*) (const void *, struct object *))
-			GetProcAddress (h, "__register_frame_info");
+    {
+      /* Increasing the load-count of LIBGCC_SONAME DLL.  */
+      hmod_libgcc = LoadLibrary (LIBGCC_SONAME);
+      register_frame_fn = (void (*) (const void *, struct object *))
+			  GetProcAddress (h, "__register_frame_info");
+    }
   else 
     register_frame_fn = __register_frame_info;
   if (register_frame_fn)
@@ -154,5 +162,7 @@ __gcc_deregister_frame (void)
     deregister_frame_fn = __deregister_frame_info;
   if (deregister_frame_fn)
      deregister_frame_fn (__EH_FRAME_BEGIN__);
+  if (hmod_libgcc)
+    FreeLibrary (hmod_libgcc);
 #endif
 }
