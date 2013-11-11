a86feb4c3acf0323659d9c003171ec3c933241a4
amylaar <amylaar@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-11-11 18:46:33 +0000

    * gcc.dg/tree-ssa/forwprop-28.c: Adjust for ARC         LOGICAL_OP_NON_SHORT_CIRCUIT definition.         * gcc.dg/tree-ssa/ssa-dom-thread-4.c: Likewise.         * gcc.dg/tree-ssa/ssa-ifcombine-ccmp-1.c: Likewise.         * gcc.dg/tree-ssa/ssa-ifcombine-ccmp-4.c: Likewise.         * gcc.dg/tree-ssa/ssa-ifcombine-ccmp-5.c: Likewise.         * gcc.dg/tree-ssa/ssa-ifcombine-ccmp-6.c: Likewise.         * gcc.dg/tree-ssa/vrp47.c: Likewise.         * gcc.dg/tree-ssa/vrp87.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@204681 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/ssa-dom-thread-4.c
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-dom-thread-4.c
@@ -61,7 +61,7 @@ bitmap_ior_and_compl (bitmap dst, const_bitmap a, const_bitmap b,
    zero.  */
 /* ARM Cortex-M0 defined LOGICAL_OP_NON_SHORT_CIRCUIT to false,
    so skip below test.  */
-/* { dg-final { scan-tree-dump-times "Threaded" 3 "dom1" { target { ! { { mips*-*-* avr-*-* } || { arm_cortex_m && arm_thumb1 } } } } } } */
+/* { dg-final { scan-tree-dump-times "Threaded" 3 "dom1" { target { ! { { mips*-*-* avr-*-* arc*-*-* } || { arm_cortex_m && arm_thumb1 } } } } } } */
 /* MIPS defines LOGICAL_OP_NON_SHORT_CIRCUIT to 0, so we split both
    "a_elt || b_elt" and "b_elt && kill_elt" into two conditions each,
    rather than using "(var1 != 0) op (var2 != 0)".  Also, as on other targets,
@@ -81,8 +81,9 @@ bitmap_ior_and_compl (bitmap dst, const_bitmap a, const_bitmap b,
       -> "kill_elt->indx == b_elt->indx" in the second condition,
 	 skipping the known-true "b_elt && kill_elt" in the second
 	 condition.  */
+/* Likewise for arc.  */
 /* For avr, BRANCH_COST is by default 0, so the default
    LOGICAL_OP_NON_SHORT_CIRCUIT definition also computes as 0.  */
-/* { dg-final { scan-tree-dump-times "Threaded" 6 "dom1" { target mips*-*-* avr-*-* } } } */
+/* { dg-final { scan-tree-dump-times "Threaded" 6 "dom1" { target mips*-*-* avr-*-* arc*-*-* } } } */
 /* { dg-final { cleanup-tree-dump "dom1" } } */
 
