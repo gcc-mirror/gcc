4c163af5e77c2035147a1e60bc84e58b75971e3f
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-11-11 16:55:29 +0000

    * tree.h (CONSTRUCTOR_NO_CLEARING): Define. 	* tree-core.h (CONSTRUCTOR_NO_CLEARING): Document it. 	* tree.def (CONSTRUCTOR): Likewise. 	* doc/generic.texi (CONSTRUCTOR): Likewise.  Update description. 	* gimplify.c (gimplify_init_constructor): Do not clear the object when 	the constructor is incomplete and CONSTRUCTOR_NO_CLEARING is set. ada/ 	* gcc-interface/utils2.c (gnat_build_constructor): Also set the flag 	CONSTRUCTOR_NO_CLEARING on the constructor.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@204677 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-core.h
+++ gcc/tree-core.h
@@ -831,6 +831,9 @@ struct GTY(()) tree_base {
            VAR_DECL, FUNCTION_DECL
            IDENTIFIER_NODE
 
+       CONSTRUCTOR_NO_CLEARING in
+           CONSTRUCTOR
+
        ASM_VOLATILE_P in
            ASM_EXPR
 
