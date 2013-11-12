c9a584aa7ab99230c50c659b209195e6b52ccde0
Adam Butcher <adam@jessamine.co.uk>
2013-11-12 20:17:54 +0000

    Add some generic lambda test cases.
    gcc/testsuite/g++.dg/cpp1y/
    	* lambda-generic.C: New test case.
    	* lambda-generic-cfun.C: New test case.
    	* lambda-generic-dep.C: New test case.
    	* lambda-generic-udt.C: New test case.
    	* lambda-generic-variadic.C: New test case.
    	* lambda-generic-x.C: New test case.
    	* lambda-generic-xcfun.C: New test case.
    	* lambda-generic-xudt.C: New test case.
    	* lambda-generic-mixed.C: New test case.
    
    From-SVN: r204716

---
+++ gcc/testsuite/g++.dg/cpp1y/lambda-generic-cfun.C
@@ -0,0 +1,25 @@
+// Generic lambda conversion to function ptr test from N3690 5.1.2.6
+// { dg-options "-std=c++1y" }
+
+void f1(int (*)(int)) { }
+void f2(char (*)(int)) { }
+void g(int (*)(int)) { } // #1
+void g(char (*)(char)) { } // #2
+void h(int (*)(int)) { } // #3
+void h(char (*)(int)) { } // #4
+
+int main()
+{
+  auto glambda = [](auto a) { return a; };
+  int (*fp)(int) = glambda;
+  f1(glambda); // OK
+  f2(glambda); // { dg-error "invalid user-defined conversion" }
+  g(glambda); // { dg-error "ambiguous" }
+  h(glambda); // OK: calls #3 since it is convertible from ID
+  int& (*fpi)(int*) = [](auto* a) -> auto& { return *a; }; // OK
+
+  auto GL = [](auto a) { return a; };
+  int (*GL_int)(int) = GL; // OK: through conversion function template
+  GL_int(3);
+}
+
