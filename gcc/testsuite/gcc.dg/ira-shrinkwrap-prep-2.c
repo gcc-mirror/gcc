732dad8f32152ed7cbb2d4cb8ba0fc21f608fe70
Martin Jambor <mjambor@suse.cz>
2013-11-12 13:53:53 +0100

    re PR rtl-optimization/10474 (shrink wrapping for functions)
    2013-11-12  Martin Jambor  <mjambor@suse.cz>
    
    	PR rtl-optimization/10474
    	* ira.c (interesting_dest_for_shprep): New function.
    	(split_live_ranges_for_shrink_wrap): Likewise.
    	(find_moveable_pseudos): Move calculation of dominance info,
    	df_analysios and the final anlyses to...
    	(ira): ...here, call split_live_ranges_for_shrink_wrap.
    
    testsuite/
    	* gcc.dg/pr10474.c: New testcase.
    	* gcc.dg/ira-shrinkwrap-prep-1.c: Likewise.
    	* gcc.dg/ira-shrinkwrap-prep-2.c: Likewise.
    
    From-SVN: r204698

---
+++ gcc/testsuite/gcc.dg/ira-shrinkwrap-prep-2.c
@@ -0,0 +1,36 @@
+/* { dg-do compile { target x86_64-*-* } } */
+/* { dg-options "-O3 -fdump-rtl-ira -fdump-rtl-pro_and_epilogue"  } */
+
+int __attribute__((noinline, noclone))
+foo (int a)
+{
+  return a + 5;
+}
+
+static int g;
+
+int __attribute__((noinline, noclone))
+bar (int a)
+{
+  int r;
+
+  if (a)
+    {
+      r = a;
+      while (r < 500)
+	if (r % 2)
+	  r = foo (r);
+	else
+	  r = foo (r+1);
+      g = r + a;
+    }
+  else
+    r = g+1;
+  return r;
+}
+
+/* { dg-final { scan-rtl-dump "Will split live ranges of parameters" "ira"  } } */
+/* { dg-final { scan-rtl-dump "Split live-range of register" "ira"  } } */
+/* { dg-final { scan-rtl-dump "Performing shrink-wrapping" "pro_and_epilogue"  } } */
+/* { dg-final { cleanup-rtl-dump "ira" } } */
+/* { dg-final { cleanup-rtl-dump "pro_and_epilogue" } } */
