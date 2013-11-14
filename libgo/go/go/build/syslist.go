9572918137e0b374c71c69c0c38b2adfacfbda1f
Ian Lance Taylor <ian@gcc.gnu.org>
2013-11-14 18:07:31 +0000

    libgo/go/go/build: use syslist.go from the gc stdlib.
    If cmd/go is rebuilt using -compiler gccgo the version of go/build that is linked into that cmd/go will not function properly as the list of file suffixes know as operating systems or architectures is incorrect.
    
    From-SVN: r204794

---
+++ libgo/go/go/build/syslist.go
@@ -0,0 +1,8 @@
+// Copyright 2011 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package build
+
+const goosList = "darwin dragonfly freebsd linux netbsd openbsd plan9 windows solaris "
+const goarchList = "386 amd64 arm "
