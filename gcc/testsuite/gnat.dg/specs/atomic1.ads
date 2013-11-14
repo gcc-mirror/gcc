65bc15c121f76475f9c42fe803bd94cb432f8974
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-11-14 22:39:21 +0000

    Revert the last change
    	* gnat.dg/specs/addr1.ads: Revert the last change.
    	* gnat.dg/specs/atomic1.ads: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@204829 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gnat.dg/specs/atomic1.ads
+++ gcc/testsuite/gnat.dg/specs/atomic1.ads
@@ -6,11 +6,11 @@ package Atomic1 is
   type UA is access all Arr;
 
   U : UA;
-  pragma Atomic (U);  -- { dg-error "atomic access" "" { xfail mips*-*-* { { i?86-*-* x86_64-*-* } && x32 } } }
+  pragma Atomic (U);  -- { dg-error "atomic access" "" { xfail mips*-*-* } }
 
   type R is record
     U : UA;
-    pragma Atomic (U);  -- { dg-error "atomic access" "" { xfail mips*-*-* { { i?86-*-* x86_64-*-* } && x32 } } }
+    pragma Atomic (U);  -- { dg-error "atomic access" "" { xfail mips*-*-* } }
   end record;
 
 end Atomic1;
