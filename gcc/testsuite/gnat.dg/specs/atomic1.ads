e4603a754f63751aa9f6d7e14539ecdbb2f4549a
H.J. Lu <hongjiu.lu@intel.com>
2013-11-14 12:40:15 +0000

    XFAIL addr1.ads and atomic1.ads on x32
    	* gnat.dg/specs/addr1.ads: XFAIL on x32.
    	* gnat.dg/specs/atomic1.ads: Likewise.
    
    From-SVN: r204789

--- gcc/testsuite/gnat.dg/specs/atomic1.ads
+++ gcc/testsuite/gnat.dg/specs/atomic1.ads
@@ -6,11 +6,11 @@ package Atomic1 is
   type UA is access all Arr;
 
   U : UA;
-  pragma Atomic (U);  -- { dg-error "atomic access" "" { xfail mips*-*-* } }
+  pragma Atomic (U);  -- { dg-error "atomic access" "" { xfail mips*-*-* { { i?86-*-* x86_64-*-* } && x32 } } }
 
   type R is record
     U : UA;
-    pragma Atomic (U);  -- { dg-error "atomic access" "" { xfail mips*-*-* } }
+    pragma Atomic (U);  -- { dg-error "atomic access" "" { xfail mips*-*-* { { i?86-*-* x86_64-*-* } && x32 } } }
   end record;
 
 end Atomic1;
