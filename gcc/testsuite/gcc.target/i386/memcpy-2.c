0874db6e96b721f9545c6b3976eb77601b09533d
Jan Hubicka <jh@suse.cz>
2013-11-19 02:30:51 +0100

    re PR middle-end/59175 (gcc.target/i386/memcpy-2.c fails with -m32)
    	PR middle-end/59175
    	* gcc.target/i386/memcpy-2.c: Fix template;
    	add +1 so the testcase passes at 32bit.
    
    Co-Authored-By: Uros Bizjak <ubizjak@gmail.com>
    
    From-SVN: r204996

--- gcc/testsuite/gcc.target/i386/memcpy-2.c
+++ gcc/testsuite/gcc.target/i386/memcpy-2.c
@@ -1,11 +1,11 @@
 /* { dg-do compile } */
 /* { dg-options "-O2" } */
-/* Memcpy should be inlined because block size is known.  */
-/* { dg-final { scan-assembler-not "memcpy" } } */
 void *a;
 void *b;
 t(unsigned int c)
 {
   if (c<10)
-    memcpy (a,b,c);
+    __builtin_memcpy (a,b,c+1);
 }
+/* Memcpy should be inlined because block size is known.  */
+/* { dg-final { scan-assembler-not "(jmp|call)\[\\t \]*memcpy" } } */
