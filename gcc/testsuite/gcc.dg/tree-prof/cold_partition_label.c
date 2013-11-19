2ae367c1e7081c2ec9a47267551d197002e79b23
Sriraman Tallam <tmsriram@google.com>
2013-11-19 22:12:21 +0000

    Emit a label for the split cold function part.
    Emit a label for the split cold function part.  Label name is formed by
    suffixing the original function name with "cold".
    
    Patch tested for bootstrap on all default languages on x86_64 and
    regression testsuite checked for parity with RUNTESTFLAGS -m32 and m64.
    
    From-SVN: r205057

---
+++ gcc/testsuite/gcc.dg/tree-prof/cold_partition_label.c
@@ -0,0 +1,36 @@
+/* Test case to check if function foo gets split and the cold function
+   gets a label.  */
+/* { dg-require-effective-target freorder } */
+/* { dg-options "-O2 -freorder-blocks-and-partition --save-temps" } */
+
+#define SIZE 10000
+
+const char *sarr[SIZE];
+const char *buf_hot;
+const char *buf_cold;
+
+__attribute__((noinline))
+void 
+foo (int path)
+{
+  int i;
+  if (path)
+    {
+      for (i = 0; i < SIZE; i++)
+	sarr[i] = buf_hot;
+    }
+  else
+    {
+      for (i = 0; i < SIZE; i++)
+	sarr[i] = buf_cold;
+    }
+}
+
+int
+main (int argc, char *argv[])
+{
+  buf_hot =  "hello";
+  buf_cold = "world";
+  foo (argc);
+  return 0;
+}
