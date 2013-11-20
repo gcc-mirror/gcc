0c509e888c5f91f307b5c28725c2825136ae978c
Ilya Enkovich <ilya.enkovich@intel.com>
2013-11-20 12:28:29 +0000

    dbxout.c (dbxout_type): Ignore POINTER_BOUNDS_TYPE.
            * dbxout.c (dbxout_type): Ignore POINTER_BOUNDS_TYPE.
            * dwarf2out.c (gen_subprogram_die): Ignore bound args.
            (gen_type_die_with_usage): Skip pointer bounds.
            (dwarf2out_global_decl): Likewise.
    
    From-SVN: r205109

--- gcc/dwarf2out.c
+++ gcc/dwarf2out.c
@@ -18288,7 +18288,7 @@ gen_subprogram_die (tree decl, dw_die_ref context_die)
 	    gen_formal_parameter_pack_die (generic_decl_parm,
 					   parm, subr_die,
 					   &parm);
-	  else if (parm)
+	  else if (parm && !POINTER_BOUNDS_P (parm))
 	    {
 	      dw_die_ref parm_die = gen_decl_die (parm, NULL, subr_die);
 
@@ -18300,6 +18300,8 @@ gen_subprogram_die (tree decl, dw_die_ref context_die)
 
 	      parm = DECL_CHAIN (parm);
 	    }
+	  else if (parm)
+	    parm = DECL_CHAIN (parm);
 
 	  if (generic_decl_parm)
 	    generic_decl_parm = DECL_CHAIN (generic_decl_parm);
@@ -19798,6 +19800,7 @@ gen_type_die_with_usage (tree type, dw_die_ref context_die,
     case FIXED_POINT_TYPE:
     case COMPLEX_TYPE:
     case BOOLEAN_TYPE:
+    case POINTER_BOUNDS_TYPE:
       /* No DIEs needed for fundamental types.  */
       break;
 
@@ -20445,7 +20448,8 @@ dwarf2out_global_decl (tree decl)
      declarations, file-scope (extern) function declarations (which
      had no corresponding body) and file-scope tagged type declarations
      and definitions which have not yet been forced out.  */
-  if (TREE_CODE (decl) != FUNCTION_DECL || !DECL_INITIAL (decl))
+  if ((TREE_CODE (decl) != FUNCTION_DECL || !DECL_INITIAL (decl))
+      && !POINTER_BOUNDS_P (decl))
     dwarf2out_decl (decl);
 }
 
