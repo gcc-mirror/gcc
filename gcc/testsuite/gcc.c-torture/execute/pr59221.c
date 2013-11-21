1a1e103fb402a6b48689a739366b1caba03ded75
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-11-21 19:45:16 +0000

    PR tree-optimization/59221 	* tree-ssa-threadedge.c (thread_across_edge): Properly manage 	temporary equivalences when threading through joiner blocks.
    	PR tree-optimization/59221
    	* gcc.c-torture/execute/pr59221.c: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@205229 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/pr59221.c
@@ -0,0 +1,19 @@
+
+
+int a = 1, b, d;
+short e;
+
+int
+main ()
+{
+  for (; b; b++)
+    ;
+  short f = a;
+  int g = 15;
+  e = f ? f : 1 << g;
+  int h = e;
+  d = h == 83647 ? 0 : h;
+  if (d != 1)
+    __builtin_abort ();
+  return 0;
+}
