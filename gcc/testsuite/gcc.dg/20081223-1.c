79a8482c13b5892bdcd0f56bcae1a1b461a65997
Jan Hubicka <jh@suse.cz>
2013-11-22 21:48:07 +0100

    20081223-1.c: Add -ffat-lto-objects.
    	* gcc.dg/20081223-1.c: Add -ffat-lto-objects.
    	* gcc.dg/vect/vect.exp: Add -ffat-lto-objects.
    
    From-SVN: r205288

--- gcc/testsuite/gcc.dg/20081223-1.c
+++ gcc/testsuite/gcc.dg/20081223-1.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-flto" { target lto } }  */
+/* { dg-options "-flto -ffat-lto-objects" { target lto } }  */
 
 typedef struct foo_ foo_t;
 foo_t bar;  /* { dg-error "storage size of 'bar' isn't known" }  */
