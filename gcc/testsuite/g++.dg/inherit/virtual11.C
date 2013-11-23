b941b4ce7a346ea74208989d8c48637d28eee815
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-11-23 21:01:46 +0000

    PR c++/59031 	* call.c (build_new_method_call_1): Comnpare function context 	with BASELINK_BINFO type rather than instance type before 	marking the call with LOOKUP_NONVIRTUAL.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@205317 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/inherit/virtual11.C
@@ -0,0 +1,17 @@
+// PR c++/59031
+// { dg-do compile }
+// { dg-options "-fdump-tree-gimple " }
+class B {
+ public:
+  virtual int add (int a, int b) {return a+ b;}
+};
+
+class D : public B {
+};
+
+int foo (int a, int b) {
+  D d;
+  return d.add(a, b);
+}
+// { dg-final { scan-tree-dump-not "OBJ_TYPE_REF" "gimple" } }
+// { dg-final { cleanup-tree-dump "gimple" } }
