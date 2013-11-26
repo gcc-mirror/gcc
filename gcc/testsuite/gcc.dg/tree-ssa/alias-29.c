e8e9e1cda43a2b56618172896acb62103cd6bab8
Richard Biener <rguenther@suse.de>
2013-11-26 09:04:44 +0000

    re PR tree-optimization/59287 (points-to analysis confused by union accesses)
    2013-11-26  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/59287
    	* tree-ssa-structalias.c (get_constraint_for_component_ref):
    	Remove no longer necessary special-casing of union accesses.
    
    	* gcc.dg/tree-ssa/alias-29.c: New testcase.
    
    From-SVN: r205380

---
+++ gcc/testsuite/gcc.dg/tree-ssa/alias-29.c
@@ -0,0 +1,27 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-optimized" } */
+
+union X {
+    int i;
+    void *p;
+};
+void bar (int);
+
+int * __attribute__((noinline,noclone))
+baz (int *p) { return p; }
+
+void foo (union X *x)
+{
+  struct Y { int i; } ystruct = {};
+  ystruct.i = * baz (&ystruct.i);
+  bar (x->i);
+}
+
+/* DSE and then DCE should be able to remove all uses of ystruct.
+   Formerly the union access for the parameter to bar let 'anything'
+   escape which made the call to bar possibly use ystruct and thus
+   prevent the store to ystruct.i from being eliminated.  The call to
+   baz makes sure that ystruct has its address taken.  */
+
+/* { dg-final { scan-tree-dump-not "ystruct" "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
