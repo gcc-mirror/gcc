73fd9cbeb30ff9bf789277073e945c1eb3198af8
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-11-27 10:00:48 +0000

    Handle vector increment/decrement in build_unary_op
    2013-11-27  Tom de Vries  <tom@codesourcery.com>
    	    Marc Glisse  <marc.glisse@inria.fr>
    
    	PR c++/59032
    	* c-typeck.c (build_unary_op): Allow vector increment and decrement.
    
    	* typeck.c (cp_build_unary_op): Allow vector increment and decrement.
    
    	* c-c++-common/pr59032.c: New testcase.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@205439 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/typeck.c
+++ gcc/cp/typeck.c
@@ -5748,7 +5748,9 @@ cp_build_unary_op (enum tree_code code, tree xarg, int noconvert,
 	    inc = cxx_sizeof_nowarn (TREE_TYPE (argtype));
 	  }
 	else
-	  inc = integer_one_node;
+	  inc = VECTOR_TYPE_P (argtype)
+	    ? build_one_cst (argtype)
+	    : integer_one_node;
 
 	inc = cp_convert (argtype, inc, complain);
 
