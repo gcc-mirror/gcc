14a981b922e03f200af99b1db09f599842d8eb7d
Marek Polacek <polacek@redhat.com>
2013-11-27 08:02:48 +0000

    ubsan.c (ubsan_type_descriptor): If varpool_get_node returns NULL for a decl, recreate that decl.
    2013-11-27  Marek Polacek  <polacek@redhat.com>
    
    	* ubsan.c (ubsan_type_descriptor): If varpool_get_node returns NULL
    	for a decl, recreate that decl.  Save into the hash table VAR_DECLs
    	rather than ADDR_EXPRs.
    testsuite/
    	* c-c++-common/ubsan/undefined-1.c: New test.
    
    From-SVN: r205433

---
+++ gcc/testsuite/c-c++-common/ubsan/undefined-1.c
@@ -0,0 +1,26 @@
+/* { dg-do run } */
+/* { dg-options "-fsanitize=undefined" } */
+/* { dg-skip-if "" { *-*-* } { "-flto" } { "" } } */
+
+int
+foo (int x, int y)
+{
+  const int z = 2;
+  if (z & 1)
+    return x << y;
+  return 0;
+}
+
+int
+bar (int x, int y)
+{
+  return x + y;
+}
+
+int
+main (void)
+{
+  foo (3, 2);
+  bar (12, 42);
+  return 0;
+}
