e934916c8647d1e343eaacf114fdbff22dfa07a4
Tom de Vries <tom@codesourcery.com>
2013-11-27 10:00:30 +0000

    Don't create out-of-bounds BIT_FIELD_REF.
    2013-11-27  Tom de Vries  <tom@codesourcery.com>
    	    Marc Glisse  <marc.glisse@inria.fr>
    
    	PR middle-end/59037
    	* semantics.c (cxx_fold_indirect_ref): Don't create out-of-bounds
    	BIT_FIELD_REF.
    
    	* fold-const.c (fold_indirect_ref_1): Don't create out-of-bounds
    	BIT_FIELD_REF.
    	* gimple-fold.c (gimple_fold_indirect_ref): Same.
    	* tree-cfg.c (verify_expr): Give error if BIT_FIELD_REF is
    	out-of-bounds.
    
    	* c-c++-common/pr59037.c: New testcase.
    
    Co-Authored-By: Marc Glisse <marc.glisse@inria.fr>
    
    From-SVN: r205438

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -9122,7 +9122,7 @@ cxx_fold_indirect_ref (location_t loc, tree type, tree op0, bool *empty_base)
 	      unsigned HOST_WIDE_INT indexi = offset * BITS_PER_UNIT;
 	      tree index = bitsize_int (indexi);
 
-	      if (offset/part_widthi <= TYPE_VECTOR_SUBPARTS (op00type))
+	      if (offset / part_widthi < TYPE_VECTOR_SUBPARTS (op00type))
 		return fold_build3_loc (loc,
 					BIT_FIELD_REF, type, op00,
 					part_width, index);
