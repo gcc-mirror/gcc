ff082cab8150ceabc83e2321824c5a4f372df396
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2013-11-28 12:27:58 +0000

    Add -lrt on Solaris
    	gcc/cp:
    	* g++spec.c (TIMELIB): Define.
    	(WITHLIBC, SKIPOPT): Adjust values.
    	(lang_specific_driver): Add TIME_LIBRARY if not passed explicitly.
    
    	gcc:
    	* config/sol2.h (TIME_LIBRARY): Define.
    
    From-SVN: r205483

--- gcc/cp/g++spec.c
+++ gcc/cp/g++spec.c
@@ -28,16 +28,22 @@ along with GCC; see the file COPYING3.  If not see
 #define LANGSPEC	(1<<1)
 /* This bit is set if they did `-lm' or `-lmath'.  */
 #define MATHLIB		(1<<2)
+/* This bit is set if they did `-lrt' or equivalent.  */
+#define TIMELIB		(1<<3)
 /* This bit is set if they did `-lc'.  */
-#define WITHLIBC	(1<<3)
+#define WITHLIBC	(1<<4)
 /* Skip this option.  */
-#define SKIPOPT		(1<<4)
+#define SKIPOPT		(1<<5)
 
 #ifndef MATH_LIBRARY
 #define MATH_LIBRARY "m"
 #endif
 #ifndef MATH_LIBRARY_PROFILE
 #define MATH_LIBRARY_PROFILE MATH_LIBRARY
+#endif
+
+#ifndef TIME_LIBRARY
+#define TIME_LIBRARY ""
 #endif
 
 #ifndef LIBSTDCXX
@@ -83,15 +89,21 @@ lang_specific_driver (struct cl_decoded_option **in_decoded_options,
   /* "-lm" or "-lmath" if it appears on the command line.  */
   const struct cl_decoded_option *saw_math = NULL;
 
+  /* "-lrt" or eqivalent if it appears on the command line.  */
+  const struct cl_decoded_option *saw_time = NULL;
+
   /* "-lc" if it appears on the command line.  */
   const struct cl_decoded_option *saw_libc = NULL;
 
   /* An array used to flag each argument that needs a bit set for
-     LANGSPEC, MATHLIB, or WITHLIBC.  */
+     LANGSPEC, MATHLIB, TIMELIB, or WITHLIBC.  */
   int *args;
 
   /* By default, we throw on the math library if we have one.  */
   int need_math = (MATH_LIBRARY[0] != '\0');
+
+  /* By default, we throw on the time library if we have one.  */
+  int need_time = (TIME_LIBRARY[0] != '\0');
 
   /* True if we saw -static.  */
   int static_link = 0;
@@ -135,6 +147,11 @@ lang_specific_driver (struct cl_decoded_option **in_decoded_options,
 	    {
 	      args[i] |= MATHLIB;
 	      need_math = 0;
+	    }
+	  else if (strcmp (arg, TIME_LIBRARY) == 0)
+	    {
+	      args[i] |= TIMELIB;
+	      need_time = 0;
 	    }
 	  else if (strcmp (arg, "c") == 0)
 	    args[i] |= WITHLIBC;
@@ -268,6 +285,12 @@ lang_specific_driver (struct cl_decoded_option **in_decoded_options,
 	  saw_math = &decoded_options[i];
 	}
 
+      if (!saw_time && (args[i] & TIMELIB) && library > 0)
+	{
+	  --j;
+	  saw_time = &decoded_options[i];
+	}
+
       if (!saw_libc && (args[i] & WITHLIBC) && library > 0)
 	{
 	  --j;
@@ -349,6 +372,15 @@ lang_specific_driver (struct cl_decoded_option **in_decoded_options,
       generate_option (OPT_l,
 		       saw_profile_flag ? MATH_LIBRARY_PROFILE : MATH_LIBRARY,
 		       1, CL_DRIVER, &new_decoded_options[j]);
+      added_libraries++;
+      j++;
+    }
+  if (saw_time)
+    new_decoded_options[j++] = *saw_time;
+  else if (library > 0 && need_time)
+    {
+      generate_option (OPT_l, TIME_LIBRARY, 1, CL_DRIVER,
+		       &new_decoded_options[j]);
       added_libraries++;
       j++;
     }
