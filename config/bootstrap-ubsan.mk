3163a5cab5300b83bc261da596a2301431505909
Marek Polacek <polacek@redhat.com>
2013-11-29 20:13:45 +0000

    bootstrap-ubsan.mk (POSTSTAGE1_LDFLAGS): Remove -lpthread -ldl.
    	* bootstrap-ubsan.mk (POSTSTAGE1_LDFLAGS): Remove -lpthread -ldl.
    	Add -B$$r/prev-$(TARGET_SUBDIR)/libsanitizer/.
    
    From-SVN: r205542

--- config/bootstrap-ubsan.mk
+++ config/bootstrap-ubsan.mk
@@ -2,6 +2,7 @@
 
 STAGE2_CFLAGS += -fsanitize=undefined
 STAGE3_CFLAGS += -fsanitize=undefined
-POSTSTAGE1_LDFLAGS += -fsanitize=undefined -static-libubsan -lpthread -ldl \
+POSTSTAGE1_LDFLAGS += -fsanitize=undefined -static-libubsan \
+		      -B$$r/prev-$(TARGET_SUBDIR)/libsanitizer/ \
 		      -B$$r/prev-$(TARGET_SUBDIR)/libsanitizer/ubsan/ \
 		      -B$$r/prev-$(TARGET_SUBDIR)/libsanitizer/ubsan/.libs
