b66eaab22ad0d61ac81ecef00ecdf3804c1d27ae
Eric Botcazou <ebotcazou@adacore.com>
2013-11-29 16:19:36 +0000

    re PR ada/54040 ([x32] Incorrect timeval and timespec)
    	PR ada/54040
    	PR ada/59346
    	* s-osinte-hpux.ads (timespec): Change type of tv_nsec field to time_t.
    	* s-osinte-kfreebsd-gnu.ads (timespec): Likewise.
    	* s-osinte-solaris-posix.ads (timespec): Likewise.
    
    From-SVN: r205532

--- gcc/ada/s-osinte-kfreebsd-gnu.ads
+++ gcc/ada/s-osinte-kfreebsd-gnu.ads
@@ -6,8 +6,8 @@
 --                                                                          --
 --                                  S p e c                                 --
 --                                                                          --
---        Copyright (C) 1991-1994, Florida State University                 --
---        Copyright (C) 1995-2005,2008,2012 Free Software Foundation, Inc.  --
+--               Copyright (C) 1991-1994, Florida State University          --
+--            Copyright (C) 1995-2013, Free Software Foundation, Inc.       --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -492,7 +492,7 @@ private
 
    type timespec is record
       tv_sec  : time_t;
-      tv_nsec : long;
+      tv_nsec : time_t;
    end record;
    pragma Convention (C, timespec);
 
