ebabd272702b9ee6b27c517790745b86d45beac4
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-12-01 01:40:16 +0000

    libgo: Avoid some cases of getting callers recursively.
    Avoids hanging inside older versions of glibc that do not
    support recurive calls to dl_iterate_phdr.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@205561 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgo/runtime/runtime.h
+++ libgo/runtime/runtime.h
@@ -776,6 +776,7 @@ extern struct backtrace_state *__go_get_backtrace_state(void);
 extern _Bool __go_file_line(uintptr, String*, String*, intgo *);
 extern byte* runtime_progname();
 extern void runtime_main(void*);
+extern uint32 runtime_in_callers;
 
 int32 getproccount(void);
 
