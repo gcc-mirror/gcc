1dfe980d31bae4dd2cce9cb7615de6248beb33cb
jasonwucj <jasonwucj@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-12-03 10:58:05 +0000

    Fix some testcases for nds32 target and provide new nds32 target specific tests.
    gcc/testsuite/
    	* gcc.dg/20020312-2.c: Add __nds32__ case.
    	* gcc.dg/builtin-apply2.c: Skip for nds32*-*-*.
    	* gcc.dg/sibcall-3.c: Expected fail for nds32*-*-*.
    	* gcc.dg/sibcall-4.c: Expected fail for nds32*-*-*.
    	* gcc.dg/stack-usage-1.c (SIZE): Define case for __nds32__.
    	* gcc.dg/torture/pr37868.c: Skip for nds32*-*-*.
    	* gcc.dg/torture/stackalign/builtin-apply-2.c: Skip for nds32*-*-*.
    	* gcc.dg/tree-ssa/20040204-1.c: Expected fail for nds32*-*-*.
    	* gcc.dg/tree-ssa/pr42585.c: Skip for nds32*-*-*.
    	* gcc.dg/tree-ssa/sra-12.c: Skip for nds32*-*-*.
    	* gcc.target/nds32: New nds32 specific directory and testcases.
    	* lib/target-supports.exp (check_profiling_available): Check for
    	nds32*-*-elf.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@205625 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/builtin-apply2.c
+++ gcc/testsuite/gcc.dg/builtin-apply2.c
@@ -1,5 +1,5 @@
 /* { dg-do run } */
-/* { dg-skip-if "Variadic funcs have all args on stack. Normal funcs have args in registers." { "aarch64*-*-* avr-*-* " } { "*" } { "" } } */
+/* { dg-skip-if "Variadic funcs have all args on stack. Normal funcs have args in registers." { "aarch64*-*-* avr-*-* nds32*-*-*" } { "*" } { "" } } */
 /* { dg-skip-if "Variadic funcs use Base AAPCS.  Normal funcs use VFP variant." { arm*-*-* && arm_hf_eabi } { "*" } { "" } } */
 
 /* PR target/12503 */
