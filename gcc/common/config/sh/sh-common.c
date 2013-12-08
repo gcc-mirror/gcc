7337ddf4bfe0544121c8735624f90b6bc6bc33c3
Oleg Endo <olegendo@gcc.gnu.org>
2013-12-08 22:15:59 +0000

    re PR target/52898 (SH Target: Inefficient DImode comparisons)
    	PR target/52898
    	PR target/51697
    	* common/config/sh/sh-common.c (sh_option_optimization_table): Remove
    	OPT_mcbranchdi entry.
    	* config/sh/sh.opt (mcbranchdi, mcmpeqdi): Mark as undocumented and
    	emit a warning.
    	* config/sh/sh.c (sh_option_override): Initialize TARGET_CBRANCHDI4
    	and TARGET_CMPEQDI_T variables.
    	* doc/invoke.texi (SH options): Undocument -mcbranchdi and -mcmpeqdi.
    
    	PR target/52898
    	PR target/51697
    	* gcc.target/sh/pr51697.c: New.
    
    From-SVN: r205794

--- gcc/common/config/sh/sh-common.c
+++ gcc/common/config/sh/sh-common.c
@@ -34,7 +34,6 @@ static const struct default_options sh_option_optimization_table[] =
     { OPT_LEVELS_1_PLUS_SPEED_ONLY, OPT_mdiv_, "inv:minlat", 1 },
     { OPT_LEVELS_SIZE, OPT_mdiv_, SH_DIV_STR_FOR_SIZE, 1 },
     { OPT_LEVELS_0_ONLY, OPT_mdiv_, "", 1 },
-    { OPT_LEVELS_SIZE, OPT_mcbranchdi, NULL, 0 },
     /* We can't meaningfully test TARGET_SHMEDIA here, because -m
        options haven't been parsed yet, hence we'd read only the
        default.  sh_target_reg_class will return NO_REGS if this is
