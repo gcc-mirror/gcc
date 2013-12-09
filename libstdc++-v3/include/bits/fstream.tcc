03fd2f60926c3730ff269a7aa9b05985ccf54273
Paolo Carlini <paolo.carlini@oracle.com>
2013-12-09 23:02:18 +0000

    re PR libstdc++/59427 (Opening with ios::in | ios::app does not allow appending)
    2013-12-09  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR libstdc++/59427
    	* include/bits/fstream.tcc (basic_filebuf<>::overflow,
    	basic_filebuf<>::xsputn): Per lwg/596, ios_base::app implies
    	ios_base:out.
    	* include/std/fstream (basic_filebuf<>::_M_set_buffer): Likewise.
    	* testsuite/27_io/basic_filebuf/sputc/char/59427.cc: New.
    	* testsuite/27_io/basic_filebuf/sputn/char/59427.cc: Likewise.
    
    From-SVN: r205837

--- libstdc++-v3/include/bits/fstream.tcc
+++ libstdc++-v3/include/bits/fstream.tcc
@@ -423,7 +423,8 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
     {
       int_type __ret = traits_type::eof();
       const bool __testeof = traits_type::eq_int_type(__c, __ret);
-      const bool __testout = _M_mode & ios_base::out;
+      const bool __testout = (_M_mode & ios_base::out
+			      || _M_mode & ios_base::app);
       if (__testout)
 	{
           if (_M_reading)
@@ -640,7 +641,8 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       // Optimization in the always_noconv() case, to be generalized in the
       // future: when __n is sufficiently large we write directly instead of
       // using the buffer.
-      const bool __testout = _M_mode & ios_base::out;
+      const bool __testout = (_M_mode & ios_base::out
+			      || _M_mode & ios_base::app);
       if (__check_facet(_M_codecvt).always_noconv()
  	   && __testout && !_M_reading)
 	{
---
+++ libstdc++-v3/testsuite/27_io/basic_filebuf/sputc/char/59427.cc
@@ -0,0 +1,39 @@
+// Copyright (C) 2013 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-require-fileio "" }
+
+#include <fstream>
+#include <testsuite_hooks.h>
+
+// libstdc++/59427
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  using namespace std;
+
+  filebuf a_f;
+  VERIFY( a_f.open("tmp_59427_sputc", ios_base::in | ios_base::app) );
+  VERIFY( 'a' == a_f.sputc('a')  );
+  VERIFY( a_f.close() );
+}
+
+int main()
+{
+  test01();
+  return 0;
+}
