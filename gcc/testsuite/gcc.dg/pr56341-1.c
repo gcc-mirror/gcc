f5d4f18c5332e5235b53f4b9496ef4e498e92f1f
Sandra Loosemore <sandra@codesourcery.com>
2013-12-11 11:50:05 -0500

    re PR middle-end/23623 (volatile keyword changes bitfield access size from 32bit to 8bit)
    2013-12-11  Sandra Loosemore  <sandra@codesourcery.com>
    
            PR middle-end/23623
            PR middle-end/48784
            PR middle-end/56341
            PR middle-end/56997
    
            gcc/
            * expmed.c (strict_volatile_bitfield_p): New function.
            (store_bit_field_1): Don't special-case strict volatile
            bitfields here.
            (store_bit_field): Handle strict volatile bitfields here instead.
            (store_fixed_bit_field): Don't special-case strict volatile
            bitfields here.
            (extract_bit_field_1): Don't special-case strict volatile
            bitfields here.
            (extract_bit_field): Handle strict volatile bitfields here instead.
            (extract_fixed_bit_field): Don't special-case strict volatile
            bitfields here.  Simplify surrounding code to resemble that in
            store_fixed_bit_field.
            * doc/invoke.texi (Code Gen Options): Update
            -fstrict-volatile-bitfields description.
    
            gcc/testsuite/
            * gcc.dg/pr23623.c: New test.
            * gcc.dg/pr48784-1.c: New test.
            * gcc.dg/pr48784-2.c: New test.
            * gcc.dg/pr56341-1.c: New test.
            * gcc.dg/pr56341-2.c: New test.
            * gcc.dg/pr56997-1.c: New test.
            * gcc.dg/pr56997-2.c: New test.
            * gcc.dg/pr56997-3.c: New test.
    
    From-SVN: r205896

---
+++ gcc/testsuite/gcc.dg/pr56341-1.c
@@ -0,0 +1,40 @@
+/* { dg-do run } */
+/* { dg-options "-fstrict-volatile-bitfields" } */
+
+extern void abort (void);
+
+struct test0
+{
+  unsigned char b1[2];
+} __attribute__((packed, aligned(2)));
+
+struct test1
+{
+  volatile unsigned long a1;
+  unsigned char b1[4];
+} __attribute__((packed, aligned(2)));
+
+struct test2
+{
+  struct test0 t0;
+  struct test1 t1;
+  struct test0 t2;
+} __attribute__((packed, aligned(2)));
+
+struct test2 xx;
+struct test2 *x1 = &xx;
+
+#define MAGIC 0x12345678
+
+void test0 (struct test2* x1)
+{
+  x1->t1.a1 = MAGIC;
+}
+
+int main()
+{
+  test0 (x1);
+  if (xx.t1.a1 != MAGIC)
+    abort ();
+  return 0;
+}
