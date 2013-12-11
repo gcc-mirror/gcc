2ce064c344f9e104a82049495733ca3205fa720a
Bernd Schmidt <bernds@codesourcery.com>
2013-12-11 12:40:08 +0000

    Add a missing tree code to a switch.
    	* c-common.c (c_fully_fold_internal): Handle ADDR_SPACE_CONVERT_EXPR.
    
    From-SVN: r205890

--- gcc/c-family/c-common.c
+++ gcc/c-family/c-common.c
@@ -1323,6 +1323,7 @@ c_fully_fold_internal (tree expr, bool in_init, bool *maybe_const_operands,
     case FIX_TRUNC_EXPR:
     case FLOAT_EXPR:
     CASE_CONVERT:
+    case ADDR_SPACE_CONVERT_EXPR:
     case VIEW_CONVERT_EXPR:
     case NON_LVALUE_EXPR:
     case NEGATE_EXPR:
