1b67c32f615d2edbe01337e2dffd915ad762b89e
chefmax <chefmax@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-12-12 11:44:23 +0000

    2013-12-12  Max Ostapenko  <m.ostapenko@partner.samsung.com>
    	* c-c++-common/tsan/free_race2.c: New file.
    	* c-c++-common/tsan/race_on_barrier2.c: Likewise.
    	* c-c++-common/tsan/race_on_mutex.c: Likewise.
    	* c-c++-common/tsan/race_on_mutex2.c: Likewise.
    	* c-c++-common/tsan/simple_race.c: Likewise.
    	* c-c++-common/tsan/simple_stack.c: Likewise.
    	* g++.dg/tsan/aligned_vs_unaligned_race.C: Likewise. Test applies only on x86_64-linux targets. 
    	* g++.dg/tsan/atomic_free.C: Likewise.
    	* g++.dg/tsan/atomic_free2.C: Likewise.
    	* g++.dg/tsan/benign_race.C: Likewise.
    	* g++.dg/tsan/cond_race.C: Likewise.
    	* g++.dg/tsan/default_options.C: Likewise.
    	* g++.dg/tsan/fd_close_norace.C: Likewise.
    	* g++.dg/tsan/fd_close_norace2.C: Likewise.
    	* g++-dg/tsan/tsan.exp: Modified to run additional C++ tests.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@205925 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/tsan/cond_race.C
@@ -0,0 +1,37 @@
+/* { dg-do run } */
+/* { dg-shouldfail "tsan" } */
+/* { dg-output "ThreadSanitizer: data race.*" } */
+/* { dg-output "pthread_cond_signal.*" } */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+struct Ctx {
+  pthread_mutex_t m;
+  pthread_cond_t c;
+  bool done;
+};
+
+void *thr(void *p) {
+  Ctx *c = (Ctx*)p;
+  pthread_mutex_lock(&c->m);
+  c->done = true;
+  pthread_mutex_unlock(&c->m);
+  pthread_cond_signal(&c->c);
+  return 0;
+}
+
+int main() {
+  Ctx *c = new Ctx();
+  pthread_mutex_init(&c->m, 0);
+  pthread_cond_init(&c->c, 0);
+  pthread_t th;
+  pthread_create(&th, 0, thr, c);
+  pthread_mutex_lock(&c->m);
+  while (!c->done)
+    pthread_cond_wait(&c->c, &c->m);
+  pthread_mutex_unlock(&c->m);
+  delete c;
+  pthread_join(th, 0);
+}
---
+++ gcc/testsuite/g++.dg/tsan/default_options.C
@@ -0,0 +1,34 @@
+/* { dg-do run } */
+/* { dg-shouldfail "tsan" } */
+
+#include <pthread.h>
+#include <stdio.h>
+
+extern "C" const char *__tsan_default_options() {
+  return "report_bugs=0";
+}
+
+int Global;
+
+void *Thread1(void *x) {
+  Global = 42;
+  return NULL;
+}
+
+void *Thread2(void *x) {
+  Global = 43;
+  return NULL;
+}
+
+int main() {
+  pthread_t t[2];
+  pthread_create(&t[0], NULL, Thread1, NULL);
+  pthread_create(&t[1], NULL, Thread2, NULL);
+  pthread_join(t[0], NULL);
+  pthread_join(t[1], NULL);
+  fprintf(stderr, "DONE\n");
+  return 0;
+}
+
+/* { dg-prune-output "WARNING: ThreadSanitizer: data race.*(\n|\r\n|\r)" } */
+/* { dg-output "DONE" } */
