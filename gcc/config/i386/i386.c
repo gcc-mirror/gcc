0cea58f2e5cf0774e56207ea760b052903229547
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-12-14 17:01:49 +0000

    Restore flag_pic in ix86_function_specific_restore
    gcc/
    
    	PR target/59492
    	* config/i386/i386.c (ix86_function_specific_restore): Don't
    	change -fPIC.
    
    2013-12-14   H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR target/59492
    	* g++.dg/other/pr59492.C: New file.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@205989 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -4337,6 +4337,9 @@ ix86_function_specific_restore (struct gcc_options *opts,
   unsigned int ix86_arch_mask;
   int i;
 
+  /* We don't change -fPIC.  */
+  opts->x_flag_pic = flag_pic;
+
   ix86_arch = (enum processor_type) ptr->arch;
   ix86_schedule = (enum attr_cpu) ptr->schedule;
   ix86_tune = (enum processor_type) ptr->tune;
