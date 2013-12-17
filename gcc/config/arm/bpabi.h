3eb65e5effb40f0260794cb78c74699cc14f7e99
jgreenhalgh <jgreenhalgh@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-12-17 12:27:38 +0000

    [ARM 3/5 big.LITTLE] Add support for -mcpu=cortex-a15.cortex-a7
    2013-12-17  James Greenhalgh  <james.greenhalgh@arm.com>
    
    	* config/arm/arm-cores.def (cortex-a15.cortex-a7): New.
    	* doc/invoke.texi: Document -mcpu=cortex-a15.cortex-a7.
    	* config/arm/arm-tables.opt: Regenerate.
    	* config/arm/arm-tune.md: Regenerate.
    	* config/arm/bpabi.h
    	(BE8_LINK_SPEC): Handle -mcpu=cortex-a5.cortex-a7.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@206047 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/arm/bpabi.h
+++ gcc/config/arm/bpabi.h
@@ -60,6 +60,7 @@
    |mcpu=cortex-a7                                      \
    |mcpu=cortex-a8|mcpu=cortex-a9|mcpu=cortex-a15       \
    |mcpu=cortex-a12					\
+   |mcpu=cortex-a15.cortex-a7				\
    |mcpu=marvell-pj4					\
    |mcpu=cortex-a53					\
    |mcpu=generic-armv7-a                                \
@@ -74,6 +75,7 @@
    |mcpu=cortex-a7                                      \
    |mcpu=cortex-a8|mcpu=cortex-a9|mcpu=cortex-a15       \
    |mcpu=cortex-a12					\
+   |mcpu=cortex-a15.cortex-a7				\
    |mcpu=cortex-a53					\
    |mcpu=marvell-pj4					\
    |mcpu=generic-armv7-a                                \
