f8c4f1f87f9b9e6876a42e196f1b47a4feff6e26
nickc <nickc@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-12-18 15:29:47 +0000

    * gcc.dg/pr32912-2.c: Fix for 16-bit targets.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@206089 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr32912-2.c
+++ gcc/testsuite/gcc.dg/pr32912-2.c
@@ -1,14 +1,24 @@
 /* { dg-do run } */
 /* { dg-options "-O2 -w" } */
-/* { dg-skip-if "TImode not supported" { "avr-*-*" } { "*" } { "" } } */
 
 extern void abort (void);
 
 #if(__SIZEOF_INT__ >= 4)
-typedef int __m128i __attribute__ ((__vector_size__ (16)));
+# define TYPE      int
+# define TYPED(a)  a
+
+#elif(__SIZEOF_INT__ > 2)
+# define TYPE      long
+# define TYPED(a)  a##L
+
 #else
-typedef long __m128i __attribute__ ((__vector_size__ (16)));
+# define TYPE      long long
+# define TYPED(a)  a##LL
 #endif
+
+
+typedef TYPE __m128i __attribute__ ((__vector_size__ (16)));
+
 __m128i
 foo (void)
 {
@@ -26,11 +36,7 @@ bar (void)
 int
 main (void)
 {
-#if(__SIZEOF_INT__ >= 4)
-  union { __m128i v; int i[sizeof (__m128i) / sizeof (int)]; } u, v;
-#else
-  union { __m128i v; long i[sizeof (__m128i) / sizeof (long)]; } u, v;
-#endif
+  union { __m128i v; TYPE i[sizeof (__m128i) / sizeof (TYPE)]; } u, v;
   int i;
 
   u.v = foo ();
@@ -39,9 +45,10 @@ main (void)
     {
       if (u.i[i] != ~v.i[i])
 	abort ();
+
       if (i < 3)
 	{
-	  if (u.i[i] != (0x11111111 << i))
+	  if (u.i[i] != (TYPED (0x11111111) << i))
 	    abort ();
 	}
       else if (u.i[i])
