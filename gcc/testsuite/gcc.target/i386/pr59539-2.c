90be6e465c82f68e872ed9f5ea2388f709fee359
Jakub Jelinek <jakub@redhat.com>
2013-12-18 17:50:06 +0100

    re PR target/59539 (Missed optimisation: VEX-prefixed operations don't need aligned data)
    	PR target/59539
    	* config/i386/sse.md
    	(<sse>_loadu<ssemodesuffix><avxsizesuffix><mask_name>,
    	<sse2_avx_avx512f>_loaddqu<mode><mask_name>): New expanders,
    	prefix existing define_insn names with *.
    
    	* gcc.target/i386/pr59539-1.c: New test.
    	* gcc.target/i386/pr59539-2.c: New test.
    
    From-SVN: r206090

---
+++ gcc/testsuite/gcc.target/i386/pr59539-2.c
@@ -0,0 +1,16 @@
+/* PR target/59539 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -mavx2" } */
+
+#include <immintrin.h>
+
+int
+foo (void *p1, void *p2)
+{
+  __m256i d1 = _mm256_loadu_si256 ((__m256i *) p1);
+  __m256i d2 = _mm256_loadu_si256 ((__m256i *) p2);
+  __m256i result = _mm256_cmpeq_epi16 (d1, d2);
+  return _mm256_movemask_epi8 (result);
+}
+
+/* { dg-final { scan-assembler-times "vmovdqu" 1 } } */
