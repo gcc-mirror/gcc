266d276c4eb2c06d3593ddf1d828776be91f0eea
Bingfeng Mei <bmei@broadcom.com>
2013-12-23 17:22:30 +0000

    vect-neg-store-1.c: New test.
    2013-12-23  Bingfeng Mei  <bmei@broadcom.com>
    
    	* gcc.dg/vect/vect-neg-store-1.c: New test.
    	* gcc.dg/vect/vect-neg-store-2.c: Ditto.
    
    From-SVN: r206181

---
+++ gcc/testsuite/gcc.dg/vect/vect-neg-store-2.c
@@ -0,0 +1,39 @@
+/* { dg-require-effective-target vect_int } */
+#include <stdlib.h>
+
+__attribute__((noinline, noclone))
+void test1(short x[128], short D)
+{
+    int i;
+    for (i=127; i>=0; i--) {
+	x[i] = D;
+    }
+}
+
+int main (void)
+{
+  short x[128 + 32];
+  int i;
+  
+  for (i = 0; i < 16; i ++)
+    {
+      asm ("");
+      x[i] = x[i + 144] = 5678;
+    }
+   
+  test1 (x + 16, 1234);
+  
+  for (i = 0; i < 128; i++)
+   if (x[i + 16] != 1234)
+     abort ();
+  
+  for (i = 0; i < 16; i++)
+    if (x[i] != 5678
+       || x[i + 144] != 5678)
+      abort ();
+         
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
