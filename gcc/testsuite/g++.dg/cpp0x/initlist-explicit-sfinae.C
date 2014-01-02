cdfa9d87ec6fa7c2b9c1f3681345c53d2fc34e78
glisse <glisse@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-01-02 22:30:43 +0000

    2014-01-02  Marc Glisse  <marc.glisse@inria.fr>
    gcc/cp/
    	* call.c (convert_like_real): Check complain.
    gcc/testsuite/
    	* g++.dg/cpp0x/initlist-explicit-sfinae.C: New file.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@206302 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/initlist-explicit-sfinae.C
@@ -0,0 +1,47 @@
+// { dg-do compile }
+// { dg-options -std=c++11 }
+template<typename _Tp>
+_Tp&& declval() noexcept;
+
+template<bool b>
+struct bt {
+    static constexpr bool value = b;
+};
+
+template <typename To_, typename... From_>
+class my_is_convertible_many {
+  private:
+    template <typename To>
+      struct indirector {
+	indirector(To);
+      };
+
+    template <typename To, typename... From>
+      struct tag {};
+
+    template <typename To, typename... From>
+      static auto test(tag<To, From...>)
+      -> decltype(indirector<To>({declval<From>()...}), bt<true>());
+    static auto test(...)
+      -> bt<false>;
+
+  public:
+    static constexpr bool value = decltype(test(tag<To_, From_...>()))::value;
+};
+
+struct A {};
+struct B {};
+struct C {};
+
+struct Test {
+  Test(A, A);
+  //Test(B, B);
+  explicit Test(C, C);
+}; 
+
+int main() {    
+  static_assert(my_is_convertible_many<Test, A, A>::value,""); // true, correct
+  static_assert(!my_is_convertible_many<Test, B, B>::value,""); // false, correct
+  static_assert(!my_is_convertible_many<Test, C, C>::value,""); // error
+  return 0;
+}
