48afbd2c007441bf94a4347c1a9dfa4f927a92dc
mrs <mrs@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-01-02 04:01:46 +0000

    (cp_tree_equal): Use tree_int_cst_equal.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/wide-int@206278 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/tree.c
+++ gcc/cp/tree.c
@@ -2606,7 +2606,7 @@ cp_tree_equal (tree t1, tree t2)
   switch (code1)
     {
     case INTEGER_CST:
-      return wi::to_widest (t1) == wi::to_widest (t2);
+      return tree_int_cst_equal (t1, t2);
 
     case REAL_CST:
       return REAL_VALUES_EQUAL (TREE_REAL_CST (t1), TREE_REAL_CST (t2));
