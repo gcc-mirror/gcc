18d73c7f597782a3957efcfd4adedda2e63fb2ae
Richard Sandiford <rdsandiford@googlemail.com>
2014-01-02 22:25:54 +0000

    Update copyright years in libgomp/
    From-SVN: r206297

--- libgomp/testsuite/libgomp.c/affinity-1.c
+++ libgomp/testsuite/libgomp.c/affinity-1.c
@@ -1,5 +1,5 @@
 /* Affinity tests.
-   Copyright (C) 2013 Free Software Foundation, Inc.
+   Copyright (C) 2013-2014 Free Software Foundation, Inc.
 
    GCC is free software; you can redistribute it and/or modify it under
    the terms of the GNU General Public License as published by the Free
