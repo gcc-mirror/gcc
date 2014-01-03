6c4de23c638fe836eff0abacc284ff76f95ffa89
Joseph Myers <joseph@codesourcery.com>
2014-01-03 15:54:24 +0000

    ibm-ldouble.c (__gcc_qdiv): Scale up arguments in case of small numerator and finite nonzero result.
    libgcc:
    	* config/rs6000/ibm-ldouble.c (__gcc_qdiv): Scale up arguments in
    	case of small numerator and finite nonzero result.
    
    gcc/testsuite:
    	* gcc.target/powerpc/rs6000-ldouble-3.c: New test.
    
    From-SVN: r206321

--- libgcc/config/rs6000/ibm-ldouble.c
+++ libgcc/config/rs6000/ibm-ldouble.c
@@ -189,7 +189,16 @@ __gcc_qdiv (double a, double b, double c, double d)
       || nonfinite (t))
     return t;
 
-  /* Finite nonzero result requires corrections to the highest order term.  */
+  /* Finite nonzero result requires corrections to the highest order
+     term.  These corrections require the low part of c * t to be
+     exactly represented in double.  */
+  if (fabs (a) <= 0x1p-969)
+    {
+      a *= 0x1p106;
+      b *= 0x1p106;
+      c *= 0x1p106;
+      d *= 0x1p106;
+    }
 
   s = c * t;                    /* (s,sigma) = c*t exactly.  */
   w = -(-b + d * t);	/* Written to get fnmsub for speed, but not
