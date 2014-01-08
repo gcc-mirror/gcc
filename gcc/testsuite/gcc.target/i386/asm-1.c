4862a7687d50854f57d17b79f858ae2b1e524e8d
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-01-08 19:45:54 +0000

    * gcc.target/i386/asm-1.c (dg-options): Remove -m32. 	* gcc.target/i386/incoming-5.c (dg-options): Ditto. 	* gcc.target/i386/pr55433.c (dg-options): Ditto. 	* gcc.target/i386/pr57848.c (dg-options): Ditto. 	* gcc.target/i386/pr59099.c (dg-options): Ditto. 	Require fpic effective target. 	* gcc.target/i386/pr56246.c (dg-do): Compile for fpic target only.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@206441 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/asm-1.c
+++ gcc/testsuite/gcc.target/i386/asm-1.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target ia32 } */
-/* { dg-options "-m32" } */
+/* { dg-options "" } */
 
 register unsigned int EAX asm ("r14"); /* { dg-error "register name" } */
 
--- gcc/testsuite/gcc.target/i386/pr57848.c
+++ gcc/testsuite/gcc.target/i386/pr57848.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O1 -m32" } */
+/* { dg-options "-O1" } */
 
 extern unsigned int __builtin_ia32_crc32si (unsigned int, unsigned int);
 #pragma GCC target("sse4.2")
