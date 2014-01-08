cb8210e4a452c903ca770c8cd13672ae16af10f1
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-01-08 22:33:12 +0000

    PR middle-end/59471 	* gcc.dg/pr59471.c (foo): Avoid vector type arguments or return 	type, use pointers to vector type instead.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@206448 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr59471.c
+++ gcc/testsuite/gcc.dg/pr59471.c
@@ -9,8 +9,8 @@ __attribute__ ((__vector_size__ (16)));
 typedef unsigned int uint32x4_t
 __attribute__ ((__vector_size__ (16)));
 
-uint8x4_t
-foo (uint16x8_t x)
+void
+foo (uint16x8_t *x, uint8x4_t *y)
 {
-  return (uint8x4_t) ((uint32x4_t) x)[0];
+  *y = (uint8x4_t) ((uint32x4_t) (*x))[0];
 }
