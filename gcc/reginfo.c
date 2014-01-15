e52beba9024b5499c5a75929423764b9aa81ffb8
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-01-15 21:41:03 +0000

    PR debug/54694
    Diagnose frame_pointer_required vs fixed hfp
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@206647 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/reginfo.c
+++ gcc/reginfo.c
@@ -86,7 +86,7 @@ static const char initial_call_really_used_regs[] = CALL_REALLY_USED_REGISTERS;
 char global_regs[FIRST_PSEUDO_REGISTER];
 
 /* Declaration for the global register. */
-static tree GTY(()) global_regs_decl[FIRST_PSEUDO_REGISTER];
+tree global_regs_decl[FIRST_PSEUDO_REGISTER];
 
 /* Same information as REGS_INVALIDATED_BY_CALL but in regset form to be used
    in dataflow more conveniently.  */
