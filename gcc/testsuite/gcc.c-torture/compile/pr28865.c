4edd2c141bd6e4aaf40320154b7ec6ee890257d3
nickc <nickc@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-01-16 12:17:48 +0000

    PR middle-end/28865
    	* varasm.c (output_constant): Return the number of bytes actually
    	emitted.
    	(output_constructor_array_range): Update the field size with the
    	number of bytes emitted by output_constant.
    	(output_constructor_regular_field): Likewise.  Also do not
     	complain if the total number of bytes emitted is now greater
    	than the expected fieldpos.
    	* output.h (output_constant): Update prototype and descriptive
    	comment.
    
    	* gcc.c-torture/compile/pr28865.c: New.
    	* gcc.c-torture/execute/pr28865.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@206661 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr28865.c
@@ -0,0 +1,16 @@
+struct var_len
+{
+  int field1;
+  const char field2[];
+};
+
+/* Note - strictly speaking this array declaration is illegal
+   since each element has a variable length.  GCC allows it
+   (for the moment) because it is used in existing code, such
+   as glibc.  */
+static const struct var_len var_array[] = 
+{
+  { 1, "Long exposure noise reduction" },
+  { 2, "Shutter/AE lock buttons" },
+  { 3, "Mirror lockup" }
+};
