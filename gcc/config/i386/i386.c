a611d7cb08d5b44fd0f2f8118f8083263a6a06df
Jakub Jelinek <jakub@redhat.com>
2014-01-16 20:24:07 +0100

    re PR target/59839 (i686 AVX2 gather intrinsic not compiling with -O0 on mingw w64)
    	PR target/59839
    	* config/i386/i386.c (ix86_expand_builtin): If target doesn't
    	satisfy operand 0 predicate for gathers, use a new pseudo as
    	subtarget.
    
    	* gcc.target/i386/pr59839.c: New test.
    
    From-SVN: r206682

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -35511,7 +35511,9 @@ addcarryx:
       mode4 = insn_data[icode].operand[5].mode;
 
       if (target == NULL_RTX
-	  || GET_MODE (target) != insn_data[icode].operand[0].mode)
+	  || GET_MODE (target) != insn_data[icode].operand[0].mode
+	  || !insn_data[icode].operand[0].predicate (target,
+						     GET_MODE (target)))
 	subtarget = gen_reg_rtx (insn_data[icode].operand[0].mode);
       else
 	subtarget = target;
