d9dcda6f487b2f088cb09bfff535307b249402c1
Jonathan Wakely <jwakely@redhat.com>
2014-01-22 23:50:58 +0000

    PR libstdc++/58764 (again)
    	PR libstdc++/58764 (again)
    	* include/bits/stl_list.h (list): Make default constructor's exception
    	specification conditional.
    	* include/bits/stl_vector.h (vector): Likewise.
    	* testsuite/util/testsuite_allocator.h (SimpleAllocator): Add noexcept
    	to default constructor.
    	* testsuite/23_containers/list/requirements/dr438/assign_neg.cc:
    	Adjust dg-error line number.
    	* testsuite/23_containers/list/requirements/dr438/constructor_1_neg.cc:
    	Likewise.
    	* testsuite/23_containers/list/requirements/dr438/constructor_2_neg.cc:
    	Likewise.
    	* testsuite/23_containers/list/requirements/dr438/insert_neg.cc:
    	Likewise.
    	* testsuite/23_containers/vector/requirements/dr438/assign_neg.cc:
    	Likewise.
    	* testsuite/23_containers/vector/requirements/dr438/
    	constructor_1_neg.cc: Likewise.
    	* testsuite/23_containers/vector/requirements/dr438/
    	constructor_2_neg.cc: Likewise.
    	* testsuite/23_containers/vector/requirements/dr438/insert_neg.cc:
    	Likewise.
    
    From-SVN: r206946

--- libstdc++-v3/testsuite/23_containers/list/requirements/dr438/constructor_2_neg.cc
+++ libstdc++-v3/testsuite/23_containers/list/requirements/dr438/constructor_2_neg.cc
@@ -18,7 +18,7 @@
 // <http://www.gnu.org/licenses/>.
 
 // { dg-do compile }
-// { dg-error "no matching" "" { target *-*-* } 1605 }
+// { dg-error "no matching" "" { target *-*-* } 1608 }
 
 #include <list>
 #include <utility>
--- libstdc++-v3/testsuite/23_containers/vector/requirements/dr438/assign_neg.cc
+++ libstdc++-v3/testsuite/23_containers/vector/requirements/dr438/assign_neg.cc
@@ -18,7 +18,7 @@
 // <http://www.gnu.org/licenses/>.
 
 // { dg-do compile }
-// { dg-error "no matching" "" { target *-*-* } 1312 }
+// { dg-error "no matching" "" { target *-*-* } 1316 }
 
 #include <vector>
 
