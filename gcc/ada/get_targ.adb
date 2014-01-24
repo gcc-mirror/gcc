e9185b9db30b857c3906b4000cac94f6ab85f292
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-01-24 15:18:46 +0000

    2014-01-24  Robert Dewar  <dewar@adacore.com>
    	* back_end.adb: Remove Short_Enums handling (handled in
    	Ttypes/Get_Targ now) Minor added comments.
    	* freeze.adb: Change name Short_Enums_On_Target to
    	Target_Short_Enums.
    	* get_targ.ads, get_targ.adb (Get_Short_Enums): New function.
    	* opt.ads: Minor comment updates.
    	* sem_ch13.adb: Change name Short_Enums_On_Target to
    	Target_Short_Enums.
    	* set_targ.adb: Set Short_Enums from gcc back end.
    	* set_targ.ads (Short_Enums): New variable.
    	* targparm.ads, targparm.adb: Remove Short_Enums entries (handled in
    	Ttypes/Get_Targ now).
    	* ttypes.ads (Target_Short_Enums): New constant boolean switch
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@207045 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/get_targ.adb
+++ gcc/ada/get_targ.adb
@@ -23,6 +23,8 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
+--  Version for use with gcc
+
 package body Get_Targ is
 
    --  Functions returning individual runtime. For the standard (GCC) back
@@ -231,6 +233,17 @@ package body Get_Targ is
    begin
       return C_Get_Bits_BE;
    end Get_Bits_BE;
+
+   ---------------------
+   -- Get_Short_Enums --
+   ---------------------
+
+   function Get_Short_Enums return Int is
+      flag_short_enums : Int;
+      pragma Import (C, flag_short_enums);
+   begin
+      return flag_short_enums;
+   end Get_Short_Enums;
 
    --------------------------
    -- Get_Strict_Alignment --
