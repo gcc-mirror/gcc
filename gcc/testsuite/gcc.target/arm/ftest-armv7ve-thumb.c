671274003b62bc3dc3a352c53f6a3ea21794366c
ktkachov <ktkachov@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-01-29 13:46:39 +0000

    [ARM] Add -march=armv7ve
    gcc/
    2014-01-29  Renlin Li  <Renlin.Li@arm.com>
    
    	* config/arm/arm-arches.def (ARM_ARCH): Add armv7ve arch.
    	* config/arm/arm.c (FL_FOR_ARCH7VE): New.
    	(arm_file_start): Generate correct asm header for armv7ve.
    	* config/arm/bpabi.h: Add multilib support for armv7ve.
    	* config/arm/driver-arm.c: Change the architectures of cortex-a7
    	and cortex-a15 to armv7ve.
    	* config/arm/t-aprofile: Add multilib support for armv7ve.
    	* doc/invoke.texi: Document -march=armv7ve.
    
    gcc/testsuite/
    2014-01-29  Renlin Li  <Renlin.Li@arm.com>
    
    	* gcc.target/arm/ftest-armv7ve-arm.c: New.
    	* gcc.target/arm/ftest-armv7ve-thumb.c: New.
    	* lib/target-supports.exp: New armfunc, armflag and armdef for armv7ve.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@207237 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/ftest-armv7ve-thumb.c
@@ -0,0 +1,40 @@
+/* { dg-do compile } */
+/* { dg-skip-if "avoid conflicting multilib options" { *-*-* } { "-march=*" } { "-march=armv7ve" } } */
+/* { dg-skip-if "avoid conflicting multilib options" { *-*-* } { "-marm" } { "" } } */
+/* { dg-options "-mthumb" } */
+/* { dg-add-options arm_arch_v7ve } */
+
+#define NEED_ARM_ARCH
+#define VALUE_ARM_ARCH 7
+
+#define NEED_ARM_ARCH_ISA_ARM
+#define VALUE_ARM_ARCH_ISA_ARM 1
+
+#define NEED_ARM_ARCH_ISA_THUMB
+#define VALUE_ARM_ARCH_ISA_THUMB 2
+
+#define NEED_ARM_ARCH_PROFILE
+#define VALUE_ARM_ARCH_PROFILE 'A'
+
+#define NEED_ARM_FEATURE_UNALIGNED
+#define VALUE_ARM_FEATURE_UNALIGNED 1
+
+#define NEED_ARM_FEATURE_LDREX
+#define VALUE_ARM_FEATURE_LDREX 15
+
+#define NEED_ARM_FEATURE_CLZ
+#define VALUE_ARM_FEATURE_CLZ 1
+
+#define NEED_ARM_FEATURE_DSP
+#define VALUE_ARM_FEATURE_DSP 1
+
+#define NEED_ARM_FEATURE_SIMD32
+#define VALUE_ARM_FEATURE_SIMD32 1
+
+#define NEED_ARM_FEATURE_QBIT
+#define VALUE_ARM_FEATURE_QBIT 1
+
+#define NEED_ARM_FEATURE_SAT
+#define VALUE_ARM_FEATURE_SAT 1
+
+#include "ftest-support.h"
