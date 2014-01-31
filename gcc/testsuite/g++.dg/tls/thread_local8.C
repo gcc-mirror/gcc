b81ddff272899847934d237d65aa183c3f66da8d
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-01-31 16:25:55 +0000

    PR c++/55800 	* decl2.c (get_tls_init_fn): Copy DECL_EXTERNAL from the variable.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@207360 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/tls/thread_local8.C
@@ -0,0 +1,11 @@
+// PR c++/55800
+// { dg-options "-std=c++11" }
+// { dg-require-alias "" }
+// { dg-final { scan-assembler "_ZTH12foo_instance" { target tls_native } } }
+
+struct foo
+{
+  foo();
+};
+
+thread_local foo foo_instance;
