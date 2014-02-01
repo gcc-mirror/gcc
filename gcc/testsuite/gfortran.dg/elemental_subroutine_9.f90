da78a067694a5bee1394a4bb2e7272214cb419bb
Paul Thomas <pault@gcc.gnu.org>
2014-02-01 18:50:41 +0000

    re PR fortran/59906 (error: size of variable '<anonymous>' is too large)
    2014-02-01  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/59906
    	* trans-stmt.c (gfc_add_loop_ss_code): In the case of character
    	SS_REFERENCE, use gfc_conv_string_parameter to ensure that a
    	pointer to the string is stored.
    	* trans-expr.c (gfc_conv_expr_reference): Likewise, use
    	gfc_conv_string_parameter to ensure that a pointer to is passed
    	to the elemental function.
    
    2014-02-01  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/59906
    	* gfortran.dg/elemental_subroutine_9.f90 : New test
    
    From-SVN: r207389

---
+++ gcc/testsuite/gfortran.dg/elemental_subroutine_9.f90
@@ -0,0 +1,39 @@
+! { dg-do run }
+!
+! PR fortran/59906
+!
+! Contributed by H Anlauf  <anlauf@gmx.de>
+!
+! Failed generate character scalar for scalarized loop for elemantal call.
+!
+program x
+  implicit none
+  call y('bbb')
+contains
+
+  subroutine y(str)
+    character(len=*), intent(in) :: str
+    character(len=len_trim(str)) :: str_aux
+    character(len=3) :: str3 = 'abc'
+
+    str_aux = str
+
+    ! Compiled but did not give correct result
+    if (any (str_cmp((/'aaa','bbb'/), str) .neqv. [.FALSE.,.TRUE.])) call abort
+
+    ! Did not compile
+    if (any (str_cmp((/'bbb', 'aaa'/), str_aux) .neqv. [.TRUE.,.FALSE.])) call abort
+
+    ! Verify patch
+    if (any (str_cmp((/'bbb', 'aaa'/), str3) .neqv. [.FALSE.,.FALSE.])) call abort
+    if (any (str_cmp((/'bbb', 'aaa'/), 'aaa') .neqv. [.FALSE.,.TRUE.])) call abort
+
+  end subroutine y
+
+  elemental logical function str_cmp(str1, str2)
+    character(len=*), intent(in) :: str1
+    character(len=*), intent(in) :: str2
+    str_cmp = (str1 == str2)
+  end function str_cmp
+
+end program x
