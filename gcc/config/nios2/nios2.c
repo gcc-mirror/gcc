1fa97c893417d40a68a59b8c4069cf6cef10799c
sandra <sandra@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-02-02 22:12:17 +0000

    2014-02-02  Sandra Loosemore  <sandra@codesourcery.com>
    	gcc/
    	* config/nios2/nios2.md (load_got_register): Initialize GOT
    	pointer from _gp_got instead of _GLOBAL_OFFSET_TABLE_.
    	* config/nios2/nios2.c (nios2_function_profiler): Likewise.
    
    	libgcc/
    	* config/nios2/crti.S (_init): Initialize GOT pointer from
    	_gp_got instead of _GLOBAL_OFFSET_TABLE_.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@207409 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/nios2/nios2.c
+++ gcc/config/nios2/nios2.c
@@ -667,8 +667,8 @@ nios2_function_profiler (FILE *file, int labelno ATTRIBUTE_UNUSED)
   if (flag_pic)
     {
       fprintf (file, "\tnextpc\tr2\n");
-      fprintf (file, "\t1: movhi\tr3, %%hiadj(_GLOBAL_OFFSET_TABLE_ - 1b)\n");
-      fprintf (file, "\taddi\tr3, r3, %%lo(_GLOBAL_OFFSET_TABLE_ - 1b)\n");
+      fprintf (file, "\t1: movhi\tr3, %%hiadj(_gp_got - 1b)\n");
+      fprintf (file, "\taddi\tr3, r3, %%lo(_gp_got - 1b)\n");
       fprintf (file, "\tadd\tr2, r2, r3\n");
       fprintf (file, "\tldw\tr2, %%call(_mcount)(r2)\n");
       fprintf (file, "\tcallr\tr2\n");
