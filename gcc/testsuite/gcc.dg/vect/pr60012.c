c86c2e73b039f42bf861036e41738ecf23c0d29c
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-02-05 15:14:56 +0000

    2014-02-05  Richard Biener  <rguenther@suse.de>
    	PR testsuite/60076
    	* gcc.dg/vect/pr60012.c: Require vect_extract_even_odd and
    	avoid using unsigned long long.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@207509 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/vect/pr60012.c
+++ gcc/testsuite/gcc.dg/vect/pr60012.c
@@ -8,14 +8,14 @@ typedef struct
 } complex16_t;
 
 void
-libvector_AccSquareNorm_ref (unsigned long long  *acc,
+libvector_AccSquareNorm_ref (unsigned int *acc,
 			     const complex16_t *x, unsigned len)
 {
   unsigned i;
   for (i = 0; i < len; i++)
-    acc[i] += ((unsigned long long)((int)x[i].real * x[i].real))
-	+ ((unsigned long long)((int)x[i].imag * x[i].imag));
+    acc[i] += ((unsigned int)((int)x[i].real * x[i].real))
+	+ ((unsigned int)((int)x[i].imag * x[i].imag));
 }
 
-/* { dg-final { scan-tree-dump "LOOP VECTORIZED" "vect" } } */
+/* { dg-final { scan-tree-dump "LOOP VECTORIZED" "vect" { target { vect_extract_even_odd } } } } */
 /* { dg-final { cleanup-tree-dump "vect" } } */
