0a756a3ffc875e13842708c8affcce48c9644dc7
Marek Polacek <polacek@redhat.com>
2014-02-06 13:57:37 +0000

    re PR c/60087 (Incorrect column number for -Wsign-compare)
    	PR c/60087
    c-family/
    	* c-common.c (warn_for_sign_compare): Call warning_at with location
    	instead of warning.
    testsuite/
    	* gcc.dg/pr60087.c: New test.
    
    From-SVN: r207554

---
+++ gcc/testsuite/gcc.dg/pr60087.c
@@ -0,0 +1,14 @@
+/* PR c/60087 */
+/* { dg-do compile } */
+/* { dg-options "-Wsign-compare" } */
+
+void
+foo (unsigned int ui, int i)
+{
+  const unsigned char uc = 0;
+  _Bool b;
+  b = 0 != ~uc; /* { dg-warning "9:promoted ~unsigned is always non-zero" } */
+  b = 2 != ~uc; /* { dg-warning "9:comparison of promoted ~unsigned with constant" } */
+  b = uc == ~uc; /* { dg-warning "10:comparison of promoted ~unsigned with unsigned" } */
+  b = i == ui; /* { dg-warning "9:comparison between signed and unsigned integer expressions" } */
+}
