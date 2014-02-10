2607ef8a7a92f75fe4d87ffd8a9ec9b367f9b1ec
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2014-02-10 16:44:05 +0000

    Don't xfail gcc.dg/binop-xor1.c
    	* gcc.dg/binop-xor1.c: Don't xfail scan-tree-dump-times.
    
    From-SVN: r207663

--- gcc/testsuite/gcc.dg/binop-xor1.c
+++ gcc/testsuite/gcc.dg/binop-xor1.c
@@ -7,5 +7,5 @@ foo (int a, int b, int c)
   return ((a && !b && c) || (!a && b && c));
 }
 
-/* { dg-final { scan-tree-dump-times "\\\^" 1 "optimized" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-times "\\\^" 1 "optimized" } } */
 /* { dg-final { cleanup-tree-dump "optimized" } } */
