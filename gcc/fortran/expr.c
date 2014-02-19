5f5600278868efaad8d865ef2aa9b59640629688
Tobias Burnus <burnus@net-b.de>
2014-02-20 00:32:46 +0100

    re PR fortran/49397 ([F03] ICE with proc pointer assignment)
    2014-02-19  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/49397
            * expr.c (gfc_check_pointer_assign): Add check for
            F2008Cor2, C729.
            * trans-decl.c (gfc_get_symbol_decl): Correctly generate
            external decl in a corner case.
    
    2014-02-19  Tobias Burnus  <burnus@net-b.de>
    
            PR fortran/49397
            * gfortran.dg/proc_ptr_45.f90: New.
            * gfortran.dg/proc_ptr_46.f90: New.
    
    From-SVN: r207927

--- gcc/fortran/expr.c
+++ gcc/fortran/expr.c
@@ -3555,6 +3555,16 @@ gfc_check_pointer_assign (gfc_expr *lvalue, gfc_expr *rvalue)
 	  return FAILURE;
 	}
 
+      /* Check F2008Cor2, C729.  */
+      if (!s2->attr.intrinsic && s2->attr.if_source == IFSRC_UNKNOWN
+	  && !s2->attr.external && !s2->attr.subroutine && !s2->attr.function)
+	{
+	  gfc_error ("Procedure pointer target '%s' at %L must be either an "
+		     "intrinsic, host or use associated, referenced or have "
+		     "the EXTERNAL attribute", s2->name, &rvalue->where);
+	  return FAILURE;
+	}
+
       return SUCCESS;
     }
 
