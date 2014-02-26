91bb5cd9a60c4ecd438e57980b405d6abd076972
Tim Shen <timshen91@gmail.com>
2014-02-26 19:45:15 +0000

    regex.tcc (match_results<>::format, [...]): Update __out after calling std::copy.
    2014-02-26  Tim Shen  <timshen91@gmail.com>
    
    	* include/bits/regex.tcc (match_results<>::format,
    	regex_replace<>): Update __out after calling std::copy.
    	* testsuite/28_regex/algorithms/regex_replace/char/dr2213.cc:
    	Add testcase.
    	* testsuite/28_regex/match_results/format.cc: Likewise.
    
    From-SVN: r208179

--- libstdc++-v3/testsuite/28_regex/algorithms/regex_replace/char/dr2213.cc
+++ libstdc++-v3/testsuite/28_regex/algorithms/regex_replace/char/dr2213.cc
@@ -1,9 +1,7 @@
 // { dg-options "-std=gnu++11" }
 
 //
-// 2013-09-24  Tim Shen <timshen91@gmail.com>
-//
-// Copyright (C) 2013-2014 Free Software Foundation, Inc.
+// Copyright (C) 2014 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -20,44 +18,26 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// 28.10.5 formatting
-// Tests ECMAScript format()
+// 28.11.4 regex_replace
+// Tests ECMAScript regex_replace's _Out_iter return value.
 
 #include <regex>
 #include <testsuite_hooks.h>
-#include <testsuite_regex.h>
 
 using namespace std;
-using namespace __gnu_test;
 
 void
 test01()
 {
   bool test __attribute__((unused)) = true;
 
-  cmatch m;
-  VERIFY(regex_search_debug("*** this is a string !!!", m,
-			    regex("(\\w+) (\\w+) (\\w+) (\\w+)")));
-  VERIFY(m.format("$&|$`|$3|$4|$2|$1|$'$$$")
-	 == "this is a string|*** |a|string|is|this| !!!$$");
-  VERIFY(m.format("&|\\3|\\4|\\2|\\1|\\",
-		  regex_constants::format_sed)
-	 == "this is a string|a|string|is|this|\\");
-}
-
-void
-test02()
-{
-  bool test __attribute__((unused)) = true;
-
+  char buff[4096] = {0};
   regex re("asdf");
   cmatch m;
-  regex_match("asdf", m, re);
-  string fmt = "|&|\\0|";
-  char buff[4096] = {0};
+  string s = "asdf";
   string res = "|asdf|asdf|";
-  VERIFY(m.format(buff, fmt.data(), fmt.data() + fmt.size(),
-		  regex_constants::format_sed) == buff + res.size());
+  VERIFY(regex_replace(buff, s.data(), s.data() + s.size(), re, "|&|\\0|",
+		       regex_constants::format_sed) == buff + res.size());
   VERIFY(res == buff);
 }
 
@@ -65,6 +45,5 @@ int
 main()
 {
   test01();
-  test02();
   return 0;
 }
