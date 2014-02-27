7a76df7f955128bb5c70d67bf428b8996ad48d42
Jeff Law <law@redhat.com>
2014-02-27 12:28:40 -0700

    re PR rtl-optimization/52714 (ICE in fixup_reorder_chain, at cfglayout.c:880)
    	PR rtl-optimization/52714
    	* combine.c (try_combine): When splitting an unrecognized PARALLEL
    	into two independent simple sets, if I3 is a jump, ensure the
    	pattern we place into I3 is a (set (pc) ...)
    
    	PR rtl-optimization/52714
    	* gcc.c-torture/compile/pr52714.c: New test.
    
    From-SVN: r208204

---
+++ gcc/testsuite/gcc.c-torture/compile/pr52714.c
@@ -0,0 +1,25 @@
+
+int __re_compile_fastmap(unsigned char *p)
+{
+    unsigned char **stack;
+    unsigned size;
+    unsigned avail;
+
+    stack = __builtin_alloca(5 * sizeof(unsigned char*));
+    if (stack == 0)
+	return -2;
+    size = 5;
+    avail = 0;
+
+    for (;;) {
+	switch (*p++) {
+	case 0:
+	    if (avail == size)
+		return -2;
+	    stack[avail++] = p;
+	}
+    }
+
+    return 0;
+}
+
