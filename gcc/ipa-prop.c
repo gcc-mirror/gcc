e40323639ede479a58d6575ea770d664e5c6134c
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-03-02 20:51:48 +0000

    PR ipa/60306
    	Revert:
    	2013-12-14   Jan Hubicka  <jh@suse.cz>
            PR middle-end/58477
            * ipa-prop.c (stmt_may_be_vtbl_ptr_store): Skip clobbers.
    
    	* testsuite/g++.dg/ipa/devirt-29.C: New testcase
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@208261 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ipa-prop.c
+++ gcc/ipa-prop.c
@@ -573,8 +573,7 @@ stmt_may_be_vtbl_ptr_store (gimple stmt)
 {
   if (is_gimple_call (stmt))
     return false;
-  else if (gimple_clobber_p (stmt))
-    return false;
+  /* TODO: Skip clobbers, doing so triggers problem in PR60306.  */
   else if (is_gimple_assign (stmt))
     {
       tree lhs = gimple_assign_lhs (stmt);
