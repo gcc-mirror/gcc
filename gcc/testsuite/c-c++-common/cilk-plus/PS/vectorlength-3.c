b3bdf019b761cc75d967e1330f3e90cd4c94e116
Jakub Jelinek <jakub@redhat.com>
2014-03-03 08:27:46 +0100

    c-pragma.c (c_pp_lookup_pragma): Handle PRAGMA_CILK_SIMD.
    	* c-pragma.c (c_pp_lookup_pragma): Handle PRAGMA_CILK_SIMD.
    	(init_pragma): Call cpp_register_deferred_pragma for PRAGMA_CILK_SIMD
    	even when flag_preprocess_only.
    
    	* c-c++-common/cilk-plus/PS/vectorlength-2.c: New test.
    	* c-c++-common/cilk-plus/PS/vectorlength-3.c: New test.
    
    From-SVN: r208268

---
+++ gcc/testsuite/c-c++-common/cilk-plus/PS/vectorlength-3.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fcilkplus -save-temps" } */
+
+#define vl(n) vectorlength(2*n)
+void
+foo (int *a, int *b, int *c)
+{
+  int i;
+#pragma simd vl(4)
+  for (i = 0; i < 64; i++)
+    a[i] = b[i] * c[i];
+}
+
+/* { dg-final { cleanup-saved-temps } } */
