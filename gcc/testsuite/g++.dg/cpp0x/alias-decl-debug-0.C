d4dffd5821291d604ef282a3701eda139f38b82e
schwab <schwab@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-03-09 07:52:48 +0000

    * g++.dg/cpp0x/alias-decl-debug-0.C: Move dg-skip after dg-do.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@208441 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp0x/alias-decl-debug-0.C
+++ gcc/testsuite/g++.dg/cpp0x/alias-decl-debug-0.C
@@ -1,6 +1,6 @@
 // Origin: PR c++/51032
-// { dg-skip-if "No stabs" { aarch64*-*-* mmix-*-* *-*-aix* alpha*-*-* hppa*64*-*-* ia64-*-* *-*-vxworks* nios2-*-* } { "*" } { "" } }
 // { dg-do compile { target c++11 } }
+// { dg-skip-if "No stabs" { aarch64*-*-* mmix-*-* *-*-aix* alpha*-*-* hppa*64*-*-* ia64-*-* *-*-vxworks* nios2-*-* } { "*" } { "" } }
 // { dg-options "-gstabs+" }
 
 template <class C>
