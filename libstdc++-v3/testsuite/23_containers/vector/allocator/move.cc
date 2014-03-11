a64ddaccf608af27809d7ddbfc1a0d9ae47beb38
Jonathan Wakely <jwakely@redhat.com>
2014-03-11 19:23:04 +0000

    backport: re PR libstdc++/59738 (r206444 caused FAIL: 23_containers/vector/52591.cc)
    	Backport from mainline.
    
    	2014-01-09  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/59738
    	* include/bits/stl_vector.h (vector<>::_M_move_assign): Restore
    	support for non-Movable types.
    
    	2014-01-08  François Dumont  <fdumont@gcc.gnu.org>
    
    	* include/bits/stl_vector.h (std::vector<>::_M_move_assign): Pass
    	*this allocator instance when building temporary vector instance
    	so that *this allocator does not get moved.
    	* testsuite/23_containers/vector/allocator/move.cc (test01): Add
    	check on a vector iterator.
    	* testsuite/23_containers/vector/allocator/move_assign.cc
    	(test02): Likewise.
    	(test03): New, test with a non-propagating allocator.
    
    	2013-11-15  Jonathan Wakely  <jwakely.gcc@gmail.com>
    
    	* testsuite/23_containers/vector/allocator/move.cc: New
    
    From-SVN: r208494

--- libstdc++-v3/testsuite/23_containers/vector/allocator/move.cc
+++ libstdc++-v3/testsuite/23_containers/vector/allocator/move.cc
@@ -1,80 +1,59 @@
-// Copyright (C) 2011-2013 Free Software Foundation, Inc.
+// Copyright (C) 2013 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the
 // Free Software Foundation; either version 3, or (at your option)
 // any later version.
- 
+
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
- 
+
 // You should have received a copy of the GNU General Public License along
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-options "-std=gnu++0x" }
+// { dg-options "-std=gnu++11" }
 
 #include <vector>
 #include <testsuite_hooks.h>
 #include <testsuite_allocator.h>
- 
+
 struct T { int i; };
 
-using __gnu_test::propagating_allocator;
+using __gnu_test::uneq_allocator;
 
 void test01()
 {
   bool test __attribute__((unused)) = true;
-  typedef propagating_allocator<T, false> alloc_type;
+  typedef uneq_allocator<T> alloc_type;
   typedef std::vector<T, alloc_type> test_type;
   test_type v1(alloc_type(1));
-  v1.push_back(T());
-  test_type v2(alloc_type(2));
-  v2.push_back(T());
-  v2 = std::move(v1);
+  v1 = { T() };
+  auto it = v1.begin();
+  test_type v2(std::move(v1));
   VERIFY(1 == v1.get_allocator().get_personality());
-  VERIFY(2 == v2.get_allocator().get_personality());
+  VERIFY(1 == v2.get_allocator().get_personality());
+  VERIFY( it == v2.begin() );
 }
 
 void test02()
 {
   bool test __attribute__((unused)) = true;
-  typedef propagating_allocator<T, true> alloc_type;
+  typedef uneq_allocator<T> alloc_type;
   typedef std::vector<T, alloc_type> test_type;
   test_type v1(alloc_type(1));
-  v1.push_back(T());
-  auto it = v1.begin();
-  test_type v2(alloc_type(2));
-  v2.push_back(T());
-  v2 = std::move(v1);
-  VERIFY( it == v2.begin() );
-  VERIFY(0 == v1.get_allocator().get_personality());
-  VERIFY(1 == v2.get_allocator().get_personality());
-}
-
-void test03()
-{
-  bool test __attribute__((unused)) = true;
-  typedef propagating_allocator<T, false> alloc_type;
-  typedef std::vector<T, alloc_type> test_type;
-  test_type v1(alloc_type(1));
-  v1.push_back(T());
-  auto it = v1.begin();
-  test_type v2(alloc_type(1));
-  v2.push_back(T());
-  v2 = std::move(v1);
-  VERIFY( it == v2.begin() );
+  v1 = { T() };
+  test_type v2(std::move(v1), alloc_type(2));
   VERIFY(1 == v1.get_allocator().get_personality());
-  VERIFY(1 == v2.get_allocator().get_personality());
+  VERIFY(2 == v2.get_allocator().get_personality());
 }
 
 int main()
 {
   test01();
   test02();
-  test03();
   return 0;
 }
