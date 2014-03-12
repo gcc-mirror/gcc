5a0d3f9f2a1f7286ee0343a64410df161aee56fe
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-03-12 09:15:08 +0000

    PR middle-end/60482 	* tree-vrp.c (register_edge_assert_for_1): Don't add assert 	if there are multiple uses, but op doesn't live on E edge. 	* tree-cfg.c (assert_unreachable_fallthru_edge_p): Also ignore 	clobber stmts before __builtin_unreachable.
    	* gcc.dg/vect/pr60482.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@208506 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/pr60482.c
@@ -0,0 +1,20 @@
+/* PR middle-end/60482 */
+/* { dg-do compile } */
+/* { dg-additional-options "-Ofast" } */
+/* { dg-require-effective-target vect_int } */
+
+double
+foo (double *x, int n)
+{
+  double p = 0.0;
+  int i;
+  x = __builtin_assume_aligned (x, 128);
+  if (n % 128)
+    __builtin_unreachable ();
+  for (i = 0; i < n; i++)
+    p += x[i];
+  return p;
+}
+
+/* { dg-final { scan-tree-dump-not "epilog loop required" "vect" } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
