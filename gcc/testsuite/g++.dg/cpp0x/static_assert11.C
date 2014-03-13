4e839e5647ab97b9133402834033ee4a77630a54
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-03-13 09:51:52 +0000

    /cp 2014-03-13  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/60254
    	* semantics.c (finish_static_assert): Call cxx_constant_value only
    	if require_potential_rvalue_constant_expression returns true.
    
    /testsuite
    2014-03-13  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/60254
    	* g++.dg/cpp0x/static_assert10.C: New.
    	* g++.dg/cpp0x/static_assert11.C: Likewise.
    	* g++.dg/cpp0x/static_assert3.C: Adjust.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@208538 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/static_assert11.C
@@ -0,0 +1,10 @@
+// PR c++/60254
+// { dg-do compile { target c++11 } }
+
+struct A
+{
+  template<typename T> bool foo(T)
+  {
+    static_assert(foo(0), "Error"); // { dg-error "non-constant condition|constant expression" }
+  }
+};
