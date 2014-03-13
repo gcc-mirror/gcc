9299bde01aed78cfea5a779eae1f79bac9c3ac00
Paolo Carlini <paolo.carlini@oracle.com>
2014-03-13 09:51:52 +0000

    re PR c++/60254 ([c++11] ICE with non-const expression in static_assert)
    /cp
    2014-03-13  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/60254
    	* semantics.c (finish_static_assert): Call cxx_constant_value only
    	if require_potential_rvalue_constant_expression returns true.
    
    /testsuite
    2014-03-13  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/60254
    	* g++.dg/cpp0x/static_assert10.C: New.
    	* g++.dg/cpp0x/static_assert11.C: Likewise.
    	* g++.dg/cpp0x/static_assert3.C: Adjust.
    
    From-SVN: r208538

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -6860,7 +6860,8 @@ finish_static_assert (tree condition, tree message, location_t location,
       else if (condition && condition != error_mark_node)
 	{
 	  error ("non-constant condition for static assertion");
-	  cxx_constant_value (condition);
+	  if (require_potential_rvalue_constant_expression (condition))
+	    cxx_constant_value (condition);
 	}
       input_location = saved_loc;
     }
