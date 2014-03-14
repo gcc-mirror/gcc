27ea311dce42a5df5ec4772cdb0e2b365b891016
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-03-14 19:06:54 +0000

    PR c++/58678 	* search.c (dfs_get_pure_virtuals): Treat the destructor of an 	abstract class as pure.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@208573 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/ipa/devirt-30.C
@@ -0,0 +1,25 @@
+// PR c++/58678
+// { dg-options "-O3 -fdump-ipa-devirt" }
+
+// We shouldn't speculatively devirtualize to ~B because B is an abstract
+// class; any actual object passed to f will be of some derived class which
+// has its own destructor.
+
+struct A
+{
+  virtual void f() = 0;
+  virtual ~A();
+};
+
+struct B : A
+{
+  virtual ~B() {}
+};
+
+void f(B* b)
+{
+  delete b;
+}
+
+// { dg-final { scan-ipa-dump-not "Speculatively devirtualizing" "devirt" } }
+// { dg-final { cleanup-ipa-dump "devirt" } }
