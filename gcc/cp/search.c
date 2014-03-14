7e343703fe22b55b9e6a446a0c0fdef989207c9d
Jason Merrill <jason@redhat.com>
2014-03-14 15:06:54 -0400

    re PR c++/58678 (pykde4-4.11.2 link error (devirtualization too trigger happy))
    	PR c++/58678
    	* search.c (dfs_get_pure_virtuals): Treat the destructor of an
    	abstract class as pure.
    
    From-SVN: r208573

--- gcc/cp/search.c
+++ gcc/cp/search.c
@@ -2096,6 +2096,22 @@ dfs_get_pure_virtuals (tree binfo, void *data)
 	if (DECL_PURE_VIRTUAL_P (BV_FN (virtuals)))
 	  vec_safe_push (CLASSTYPE_PURE_VIRTUALS (type), BV_FN (virtuals));
     }
+  /* Treat a virtual destructor in an abstract class as pure even if it
+     isn't declared as pure; there is no way it would be called through the
+     vtable except during construction, which causes undefined behavior.  */
+  if (binfo == TYPE_BINFO (type)
+      && CLASSTYPE_PURE_VIRTUALS (type)
+      && TYPE_HAS_NONTRIVIAL_DESTRUCTOR (type))
+    {
+      tree dtor = CLASSTYPE_DESTRUCTORS (type);
+      if (DECL_VIRTUAL_P (dtor) && !DECL_PURE_VIRTUAL_P (dtor))
+	{
+	  tree clone;
+	  DECL_PURE_VIRTUAL_P (dtor) = true;
+	  FOR_EACH_CLONE (clone, dtor)
+	    DECL_PURE_VIRTUAL_P (clone) = true;
+	}
+    }
 
   return NULL_TREE;
 }
