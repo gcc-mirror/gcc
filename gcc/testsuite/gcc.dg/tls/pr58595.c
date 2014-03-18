1e649cf8d2ca8aff8f7de0ab4a9155c4dcac0efe
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2014-03-18 10:18:07 +0000

    Fix gcc.dg/tls/pr58595.c on Solaris 9
    	* gcc.dg/tls/pr58595.c: Add tls options.
    
    From-SVN: r208635

--- gcc/testsuite/gcc.dg/tls/pr58595.c
+++ gcc/testsuite/gcc.dg/tls/pr58595.c
@@ -2,6 +2,7 @@
 /* { dg-do run } */
 /* { dg-options "-O2" } */
 /* { dg-additional-options "-fpic" { target fpic } } */
+/* { dg-add-options tls } */
 /* { dg-require-effective-target tls } */
 /* { dg-require-effective-target sync_int_long } */
 
