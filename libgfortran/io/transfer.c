9b798767627fdf43ec8ed6be7ac73490bb67d04d
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-03-21 22:19:44 +0000

    2014-03-21  Jerry DeLisle  <jvdelisle@gcc.gnu>
    	PR libfortran/60148
    	* io/transfer.c (data_transfer_init): If std= was specified, set
    	delim status to DELIM_NONE of no other was specified.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@208759 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgfortran/io/transfer.c
+++ libgfortran/io/transfer.c
@@ -2674,7 +2674,8 @@ data_transfer_init (st_parameter_dt *dtp, int read_flag)
   if (dtp->u.p.current_unit->delim_status == DELIM_UNSPECIFIED)
     {
       if (ionml && dtp->u.p.current_unit->flags.delim == DELIM_UNSPECIFIED)
-	dtp->u.p.current_unit->delim_status = DELIM_QUOTE;
+	dtp->u.p.current_unit->delim_status =
+	  compile_options.allow_std & GFC_STD_GNU ? DELIM_QUOTE : DELIM_NONE;
       else
 	dtp->u.p.current_unit->delim_status = dtp->u.p.current_unit->flags.delim;
     }
