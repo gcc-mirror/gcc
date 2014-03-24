79127ad5470e355503fec0857b0f96ea94dc9fd1
krebbel <krebbel@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-03-24 17:38:09 +0000

    2014-03-24  Andreas Krebbel  <Andreas.Krebbel@de.ibm.com>
    	PR rtl-optimization/60501
    	* optabs.def (addptr3_optab): New optab.
    	* optabs.c (gen_addptr3_insn, have_addptr3_insn): New function.
    	* doc/md.texi ("addptrm3"): Document new RTL standard expander.
    	* expr.h (gen_addptr3_insn, have_addptr3_insn): Add prototypes.
    
    	* lra.c (emit_add3_insn): Use the addptr pattern if available.
    
    	* config/s390/s390.md ("addptrdi3", "addptrsi3"): New expanders.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@208796 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/optabs.c
+++ gcc/optabs.c
@@ -4755,6 +4755,43 @@ have_add2_insn (rtx x, rtx y)
   return 1;
 }
 
+/* Generate and return an insn body to add Y to X.  */
+
+rtx
+gen_addptr3_insn (rtx x, rtx y, rtx z)
+{
+  enum insn_code icode = optab_handler (addptr3_optab, GET_MODE (x));
+
+  gcc_assert (insn_operand_matches (icode, 0, x));
+  gcc_assert (insn_operand_matches (icode, 1, y));
+  gcc_assert (insn_operand_matches (icode, 2, z));
+
+  return GEN_FCN (icode) (x, y, z);
+}
+
+/* Return true if the target implements an addptr pattern and X, Y,
+   and Z are valid for the pattern predicates.  */
+
+int
+have_addptr3_insn (rtx x, rtx y, rtx z)
+{
+  enum insn_code icode;
+
+  gcc_assert (GET_MODE (x) != VOIDmode);
+
+  icode = optab_handler (addptr3_optab, GET_MODE (x));
+
+  if (icode == CODE_FOR_nothing)
+    return 0;
+
+  if (!insn_operand_matches (icode, 0, x)
+      || !insn_operand_matches (icode, 1, y)
+      || !insn_operand_matches (icode, 2, z))
+    return 0;
+
+  return 1;
+}
+
 /* Generate and return an insn body to subtract Y from X.  */
 
 rtx
