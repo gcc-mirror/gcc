bebb2c46af14699655970537d0338479bcd0fdfc
fabien <fabien@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-03-26 21:33:28 +0000

    2014-03-26  Fabien Chene  <fabien@gcc.gnu.org>         PR c++/52369         * cp/method.c (walk_field_subobs): improve the diagnostic 	locations for both REFERENCE_TYPEs and non-static const members. 	* cp/init.c (diagnose_uninitialized_cst_or_ref_member): use %q#D 	instead of %qD to be consistent with the c++11 diagnostic.
    2014-03-26  Fabien Chene  <fabien@gcc.gnu.org>
    
            PR c++/52369
            * g++.dg/init/const10.C: New.
    	* g++.dg/init/const11.C: New.
    	* g++.dg/init/pr25811.C: Adjust.
    	* g++.dg/init/pr29043.C: Likewise.
    	* g++.dg/init/pr43719.C: Likewise.
    	* g++.dg/init/pr44086.C: Likewise.
    	* g++.dg/init/ctor8.C: Likewise.
    	* g++.dg/init/uninitialized1.C: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@208854 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/init/ctor8.C
+++ gcc/testsuite/g++.dg/init/ctor8.C
@@ -1,6 +1,6 @@
 // PR c++/29039
 
-typedef struct S {	    // { dg-error "reference" "" { target c++11 } }
+typedef struct S { // { dg-error "reference" "" { target c++11 } }
   int &r; 
 }; // { dg-warning "'typedef' was ignored" }
 
