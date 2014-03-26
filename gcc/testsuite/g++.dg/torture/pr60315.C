097681e48293a57ae6df99eabcbc7bdd95d519e0
schwab <schwab@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-03-26 08:31:30 +0000

    * g++.dg/torture/pr60315.C: Remove duplication.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@208839 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/torture/pr60315.C
+++ gcc/testsuite/g++.dg/torture/pr60315.C
@@ -14,19 +14,3 @@ extern Base* b;
 int main() {
     return (static_cast<Derived*>(b)->*(&Derived::f))();
 }
-// { dg-do compile }
-struct Base {
-    virtual int f() = 0;
-};
-
-struct Derived : public Base {
-    virtual int f() final override {
-        return 42;
-    }
-};
-
-extern Base* b;
-
-int main() {
-    return (static_cast<Derived*>(b)->*(&Derived::f))();
-}
