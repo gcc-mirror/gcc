743de8ed93b48297eb34a8c00e663f1425e874dd
mshawcroft <mshawcroft@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-03-27 10:13:50 +0000

    [PR 60580, AArch64] Fix __attribute__ ((optimize("no-omit-frame-pointer")))
    The implementation of -m[no-]omit-leaf-frame-pointer and
    -f[no-]omit-frame-pointer in the AArch64 target does not behave
    correctly in the presence of __attribute__ optimize.
    
    This patch adjusts the implementation to work in a similar fashion to
    the same functionality in the i386 target.
    
    The problem occurs because the current implementation uses a global
    'faked_omit_frame_pointer' to retain the original value of
    flag_omit_frame_pointer.  The global does not form part of the
    optimization save state.
    
    This solution removes the global and instead tracks required behaviour
    using only flag_omit_frame_pointer and flag_omit_leaf_frame_pointer.
    These two form part of the optimziation save state and target save
    state respectively.
    
    The additional complication for AArch64 is that the PCS requires that
    given -fno-omit-frame-pointer -momit-leave-frame-pointer, a leaf
    function that kills LR must create a frame record.  This is readily
    handled in aarch64_frame_pointer_required().  I've dropped logic in
    aarch64_can_eliminate() that attempts to detect this scenario since it
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@208862 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/aarch64/test_fp_attribute_1.c
@@ -0,0 +1,26 @@
+/* { dg-do compile } */
+/* { dg-options "-O0 -fno-omit-frame-pointer -fno-inline --save-temps" } */
+
+void
+leaf (void)
+{
+  int a = 0;
+}
+
+__attribute__ ((optimize("omit-frame-pointer")))
+void
+non_leaf_1 (void)
+{
+  leaf ();
+}
+
+__attribute__ ((optimize("omit-frame-pointer")))
+void
+non_leaf_2 (void)
+{
+  leaf ();
+}
+
+/* { dg-final { scan-assembler-times "str\tx30, \\\[sp\\\]" 2 } } */
+
+/* { dg-final { cleanup-saved-temps } } */
