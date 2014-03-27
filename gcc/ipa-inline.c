0bc23e2b01de030683d65e27a539ec169b25e2c6
dehao <dehao@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-03-27 01:01:07 +0000

    * ipa-inline.c (early_inliner): Update node's inline info.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@208861 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ipa-inline.c
+++ gcc/ipa-inline.c
@@ -2318,6 +2318,8 @@ early_inliner (void)
 		      edge->call_stmt, edge->callee->decl, false))
 		edge->call_stmt_cannot_inline_p = true;
 	    }
+	  if (iterations < PARAM_VALUE (PARAM_EARLY_INLINER_MAX_ITERATIONS) - 1)
+	    inline_update_overall_summary (node);
 	  timevar_pop (TV_INTEGRATION);
 	  iterations++;
 	  inlined = false;
