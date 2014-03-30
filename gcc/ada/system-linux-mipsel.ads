368348abbb3c4abff8f8e5bb356f326468fda8e9
Eric Botcazou <ebotcazou@adacore.com>
2014-03-30 15:47:43 +0000

    re PR ada/60703 (System.Address not preelaborable on MIPS)
    	PR ada/60703
    	* system-linux-alpha.ads: Adjust for Ada 2005.
    	* system-linux-mips.ads: Likewise.
    	* system-linux-mips64el.ads: Likewise.
    	* system-linux-mipsel.ads: Likewise.
    	* system-linux-s390.ads: Likewise.
    	* system-linux-s390x.ads: Likewise.
    	* system-linux-sparc.ads: Likewise.
    	* system-linux-sparcv9.ads: Likewise.
    	* system-rtems.ads: Likewise.
    	* system-vxworks-arm.ads: Likewise.
    
    From-SVN: r208945

--- gcc/ada/system-linux-mipsel.ads
+++ gcc/ada/system-linux-mipsel.ads
@@ -7,7 +7,7 @@
 --                                 S p e c                                  --
 --                        (GNU-Linux/MIPSEL Version)                        --
 --                                                                          --
---          Copyright (C) 1992-2012, Free Software Foundation, Inc.         --
+--          Copyright (C) 1992-2014, Free Software Foundation, Inc.         --
 --                                                                          --
 -- This specification is derived from the Ada Reference Manual for use with --
 -- GNAT. The copyright notice above, and the license provisions that follow --
@@ -62,6 +62,7 @@ package System is
    --  Storage-related Declarations
 
    type Address is private;
+   pragma Preelaborable_Initialization (Address);
    Null_Address : constant Address;
 
    Storage_Unit : constant := 8;
