75d850f3681a4f907b43983936db7f28a2ec230d
Patrick Palka <patrick@parcs.ath.cx>
2014-03-31 20:48:33 -0400

    re PR c++/44859 (missed warning: returning reference to temporary)
    	PR c++/44859
    	* typeck.c (maybe_warn_about_returning_address_of_local): Unwrap
    	COMPONENT_REFs and ARRAY_REFs sooner.
    
    From-SVN: r208970

---
+++ gcc/testsuite/g++.dg/warn/Wreturn-local-addr-2.C
@@ -0,0 +1,11 @@
+// PR c++/44859
+
+struct Base2 { int m_foo; };
+struct Derived2 : public Base2 {};
+
+const Base2& f8() { return Derived2(); } // { dg-warning "reference to temporary" }
+
+struct foo { };
+struct bar { foo base; };
+
+const foo& f9() { return bar().base; } // { dg-warning "reference to temporary" }
