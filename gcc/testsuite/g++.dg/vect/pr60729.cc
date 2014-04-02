cc9e213ae6409b9828be1af4331ff6e8890659b2
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-04-02 08:26:57 +0000

    2014-04-02  Richard Biener  <rguenther@suse.de>
    	PR middle-end/60729
    	* optabs.c (expand_abs_nojump): Honor flag_trapv only for
    	MODE_INTs.  Properly use negv_optab.
    	(expand_abs): Likewise.
    
    	* g++.dg/vect/pr60729.cc: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@209012 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/vect/pr60729.cc
@@ -0,0 +1,10 @@
+// { dg-do compile }
+// { dg-additional-options "-ftrapv" }
+
+void doSomething(int dim, double *Y, double *A) 
+{
+  for (int k=0; k<dim; k++) 
+    Y[k] += __builtin_fabs (A[k]);
+}
+
+// { dg-final { cleanup-tree-dump "vect" } }
