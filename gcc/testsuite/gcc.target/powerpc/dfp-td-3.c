a36dee05d086533602c86b965d0e808eb8dac940
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2014-04-04 14:27:06 +0000

    backport: builtins.def (BUILT_IN_FABSD32): New DFP ABS builtin.
    [gcc]
    
    2014-04-04  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2013-08-19  Peter Bergner  <bergner@vnet.ibm.com>
    		    Jakub Jelinek  <jakub@redhat.com>
    
    	* builtins.def (BUILT_IN_FABSD32): New DFP ABS builtin.
    	(BUILT_IN_FABSD64): Likewise.
    	(BUILT_IN_FABSD128): Likewise.
    	* builtins.c (expand_builtin): Add support for
    	new DFP ABS builtins.
    	(fold_builtin_1): Likewise.
    	* config/rs6000/dfp.md
    	(*abstd2_fpr): Handle non-overlapping destination
    	and source operands.
    	(*nabstd2_fpr): Likewise.
    
    [gcc/testsuite]
    
    2014-04-04  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2013-08-19  Peter Bergner  <bergner@vnet.ibm.com>
    
    	* gcc.target/powerpc/dfp-dd-2.c: New test.
    	* gcc.target/powerpc/dfp-td-2.c: Likewise.
    	* gcc.target/powerpc/dfp-td-3.c: Likewise.
    
    From-SVN: r209103

---
+++ gcc/testsuite/gcc.target/powerpc/dfp-td-3.c
@@ -0,0 +1,29 @@
+/* Test generation of DFP instructions for POWER6.  */
+/* { dg-do compile { target { powerpc*-*-linux* && powerpc_fprs } } } */
+/* { dg-options "-std=gnu99 -O1 -mcpu=power6" } */
+
+/* { dg-final { scan-assembler-times "fneg" 1 } } */
+/* { dg-final { scan-assembler-times "fabs" 1 } } */
+/* { dg-final { scan-assembler-times "fnabs" 1 } } */
+/* { dg-final { scan-assembler-times "fmr" 3 } } */
+
+/* These tests verify we generate fneg, fabs and fnabs and
+   associated fmr's since these are not done in place.  */
+
+_Decimal128
+func1 (_Decimal128 a, _Decimal128 b)
+{
+  return -b;
+}
+
+_Decimal128
+func2 (_Decimal128 a, _Decimal128 b)
+{
+  return __builtin_fabsd128 (b);
+}
+
+_Decimal128
+func3 (_Decimal128 a, _Decimal128 b)
+{
+  return - __builtin_fabsd128 (b);
+}
