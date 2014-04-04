55d22215a71974b9c2cc43f97f869866a06fae41
krebbel <krebbel@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-04-04 21:58:34 +0000

    2014-04-04  Andreas Krebbel  <Andreas.Krebbel@de.ibm.com>
    	* gcc.dg/builtin-bswap-6.c: Adjust return value to disable GCC
    	optimization.
    	* gcc.dg/builtin-bswap-7.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@209132 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/builtin-bswap-7.c
+++ gcc/testsuite/gcc.dg/builtin-bswap-7.c
@@ -3,6 +3,10 @@
 /* { dg-require-effective-target lp64 } */
 /* { dg-options "-O -fdump-rtl-combine" } */
 
+/* The test intentionally returns 1/2 instead of the obvious 0/1 to
+   prevent GCC from calculating the return value with arithmetic
+   instead of a comparison.  */
+
 #include <stdint.h>
 
 #define BS(X) __builtin_bswap64(X)
@@ -11,28 +15,28 @@ int foo1 (uint64_t a)
 {
   if (BS (a) == 0xA00000000)
     return 1;
-  return 0;
+  return 2;
 }
 
 int foo2 (uint64_t a)
 {
   if (BS (a) != 0xA00000000)
     return 1;
-  return 0;
+  return 2;
 }
 
 int foo3 (uint64_t a, uint64_t b)
 {
   if (BS (a) == BS (b))
     return 1;
-  return 0;
+  return 2;
 }
 
 int foo4 (uint64_t a, uint64_t b)
 {
   if (BS (a) != BS (b))
     return 1;
-  return 0;
+  return 2;
 }
 
 /* { dg-final { scan-rtl-dump-not "bswapdi" "combine" } } */
