738d6b6d83785a60b16ce864f6e0d8eb21991750
Andreas Schwab <schwab@gcc.gnu.org>
2014-04-06 17:55:12 +0000

    * gcc.c-torture/compile/pr60655-1.c: Use __SIZE_TYPE__ for size_t.
    From-SVN: r209167

--- gcc/testsuite/gcc.c-torture/compile/pr60655-1.c
+++ gcc/testsuite/gcc.c-torture/compile/pr60655-1.c
@@ -3,7 +3,7 @@
 typedef unsigned char unit;
 typedef unit *unitptr;
 extern short global_precision;
-typedef unsigned int size_t;
+typedef __SIZE_TYPE__ size_t;
 extern void *memcpy (void *dest, const void *src, size_t n);
 
 short mp_compare(const unit* r1, const unit* r2)
