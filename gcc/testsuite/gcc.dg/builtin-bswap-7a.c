da6ff45a3b8098c401b718214e4d5c9abdcde401
krebbel <krebbel@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-04-08 14:18:46 +0000

    2014-04-08  Andreas Krebbel  <Andreas.Krebbel@de.ibm.com>
    	PR rtl-optimization/60776
    	* gcc.dg/builtin-bswap-6.c: Use -mbranch-cost=0 for s390.
    	* gcc.dg/builtin-bswap-7.c: Likewise.
    	* gcc.dg/builtin-bswap-6a.c: New testcase.
    	* gcc.dg/builtin-bswap-7a.c: New testcase.
    
    	Revert
    	2014-04-04  Andreas Krebbel  <Andreas.Krebbel@de.ibm.com>
    
    	* gcc.dg/builtin-bswap-6.c: Adjust return value to disable GCC
    	optimization.
    	* gcc.dg/builtin-bswap-7.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@209221 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/builtin-bswap-7a.c
+++ gcc/testsuite/gcc.dg/builtin-bswap-7a.c
@@ -1,11 +1,12 @@
 /* { dg-do compile { target arm*-*-* alpha*-*-* ia64*-*-* x86_64-*-* s390x-*-* powerpc*-*-* rs6000-*-* } } */
 /* { dg-require-effective-target stdint_types } */
 /* { dg-require-effective-target lp64 } */
-/* { dg-options "-O -fdump-rtl-combine" } */
+/* { dg-options "-O2 -fdump-rtl-combine" } */
 
-/* The branch cost setting prevents the return value from being
-   calculated with arithmetic instead of doing a compare.  */
-/* { dg-additional-options "-mbranch-cost=0" { target s390x-*-* } } */
+/* The test is similiar to builtin-bswap-7.c but returns 1/2 instead
+   of 0/1 to prevent GCC from calculating the return value with
+   arithmetic instead of a comparison.  This requires the optimization
+   level to be bumped up to -O2 at least for x86_64.  */
 
 #include <stdint.h>
 
@@ -15,28 +16,28 @@ int foo1 (uint64_t a)
 {
   if (BS (a) == 0xA00000000)
     return 1;
-  return 0;
+  return 2;
 }
 
 int foo2 (uint64_t a)
 {
   if (BS (a) != 0xA00000000)
     return 1;
-  return 0;
+  return 2;
 }
 
 int foo3 (uint64_t a, uint64_t b)
 {
   if (BS (a) == BS (b))
     return 1;
-  return 0;
+  return 2;
 }
 
 int foo4 (uint64_t a, uint64_t b)
 {
   if (BS (a) != BS (b))
     return 1;
-  return 0;
+  return 2;
 }
 
 /* { dg-final { scan-rtl-dump-not "bswapdi" "combine" } } */
