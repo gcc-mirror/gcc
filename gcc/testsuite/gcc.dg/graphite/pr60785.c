9038ae48b816983065d8705f38b35144fdc4ddd7
Richard Biener <rguenther@suse.de>
2014-04-08 10:59:40 +0000

    re PR tree-optimization/60785 (ICE in gsi_for_stmt w/ -O2 -ftree-loop-linear)
    2014-04-08  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/60785
    	* graphite-sese-to-poly.c (rewrite_phi_out_of_ssa): Treat
    	default defs properly.
    
    	* gcc.dg/graphite/pr60785.c: New testcase.
    
    From-SVN: r209214

---
+++ gcc/testsuite/gcc.dg/graphite/pr60785.c
@@ -0,0 +1,27 @@
+/* { dg-options "-O2 -floop-interchange" } */
+
+static int
+aqc(void)
+{
+  return 1;
+}
+
+void
+gkd(void)
+{
+  int wu0;
+  static int b1y;
+  static int gw2;
+  static int *ydw = &gw2;
+  static int **m3l = &ydw;
+  **m3l = 0;
+  for (b1y = 0; b1y < 1; ++b1y)
+    {
+      int *cpj = &gw2;
+      if (*ydw |= aqc())
+	{
+	  *cpj = 0;
+	  *ydw = wu0;
+	}
+    }
+}
