5d1a5a53783dbfe3e1b9697f1dad4c685cf503c7
Cong Hou <congh@google.com>
2014-04-09 07:37:14 -0400

    re PR testsuite/60773 (FAIL: gcc.dg/vect/pr60656.c -flto -ffat-lto-objects  scan-tree-dump-times vect "vectorized 1 loops" 1)
    2014-04-09  Cong Hou  <congh@google.com>
    
    	PR testsuite/60773
    	* doc/sourcebuild.texi (vect_widen_mult_si_to_di_pattern): Add
    	documentation.
    
    	* lib/target-supports.exp:
    	(check_effective_target_vect_widen_si_to_di_pattern): New.
    	* gcc.dg/vect/pr60656.c: Require vect_long effective target.
    	Use scan-tree-dump-times for vect_widen_mult_si_to_di_pattern
    	targets only.
    	(foo): Fix up formatting.
    	(main): Call check_vect.
    
    From-SVN: r209241

--- gcc/testsuite/gcc.dg/vect/pr60656.c
+++ gcc/testsuite/gcc.dg/vect/pr60656.c
@@ -1,4 +1,5 @@
 /* { dg-require-effective-target vect_int } */
+/* { dg-require-effective-target vect_long } */
 
 #include "tree-vect.h"
 
@@ -12,7 +13,7 @@ foo ()
   for(i = 0; i < 4; ++i)
     {
       long P = v[i];
-      s += P*P*P;
+      s += P * P * P;
     }
   return s;
 }
@@ -27,7 +28,7 @@ bar ()
   for(i = 0; i < 4; ++i)
     {
       long P = v[i];
-      s += P*P*P;
+      s += P * P * P;
       __asm__ volatile ("");
     }
   return s;
@@ -35,11 +36,12 @@ bar ()
 
 int main()
 {
+  check_vect ();
+
   if (foo () != bar ())
     abort ();
   return 0;
 }
 
-/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { target vect_widen_mult_si_to_di_pattern } } } */
 /* { dg-final { cleanup-tree-dump "vect" } } */
-
