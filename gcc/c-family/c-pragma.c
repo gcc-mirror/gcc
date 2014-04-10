e6fbd3c42e4bfed02d24249bdd5ee304f07797ae
Jakub Jelinek <jakub@redhat.com>
2014-04-10 09:47:55 +0200

    backport: re PR middle-end/36282 (Spurious warning "asm declaration ignored due to conflict with previous rename")
    	Backport from mainline
    	2014-03-13  Jakub Jelinek  <jakub@redhat.com>
    
    	PR middle-end/36282
    	* c-pragma.c (apply_pragma_weak): Only look at
    	TREE_SYMBOL_REFERENCED (DECL_ASSEMBLER_NAME (decl)) if
    	DECL_ASSEMBLER_NAME_SET_P (decl).
    	(maybe_apply_pending_pragma_weaks): Exit early if
    	vec_safe_is_empty (pending_weaks) rather than only when
    	!pending_weaks.
    	(maybe_apply_pragma_weak): Likewise.  If !DECL_ASSEMBLER_NAME_SET_P,
    	set assembler name back to NULL afterwards.
    
    	* c-c++-common/pr36282-1.c: New test.
    	* c-c++-common/pr36282-2.c: New test.
    	* c-c++-common/pr36282-3.c: New test.
    	* c-c++-common/pr36282-4.c: New test.
    
    From-SVN: r209263

--- gcc/c-family/c-pragma.c
+++ gcc/c-family/c-pragma.c
@@ -259,6 +259,7 @@ apply_pragma_weak (tree decl, tree value)
 
   if (SUPPORTS_WEAK && DECL_EXTERNAL (decl) && TREE_USED (decl)
       && !DECL_WEAK (decl) /* Don't complain about a redundant #pragma.  */
+      && DECL_ASSEMBLER_NAME_SET_P (decl)
       && TREE_SYMBOL_REFERENCED (DECL_ASSEMBLER_NAME (decl)))
     warning (OPT_Wpragmas, "applying #pragma weak %q+D after first use "
 	     "results in unspecified behavior", decl);
@@ -276,7 +277,7 @@ maybe_apply_pragma_weak (tree decl)
   /* Avoid asking for DECL_ASSEMBLER_NAME when it's not needed.  */
 
   /* No weak symbols pending, take the short-cut.  */
-  if (!pending_weaks)
+  if (vec_safe_is_empty (pending_weaks))
     return;
   /* If it's not visible outside this file, it doesn't matter whether
      it's weak.  */
@@ -288,7 +289,13 @@ maybe_apply_pragma_weak (tree decl)
   if (TREE_CODE (decl) != FUNCTION_DECL && TREE_CODE (decl) != VAR_DECL)
     return;
 
-  id = DECL_ASSEMBLER_NAME (decl);
+  if (DECL_ASSEMBLER_NAME_SET_P (decl))
+    id = DECL_ASSEMBLER_NAME (decl);
+  else
+    {
+      id = DECL_ASSEMBLER_NAME (decl);
+      SET_DECL_ASSEMBLER_NAME (decl, NULL_TREE);
+    }
 
   FOR_EACH_VEC_ELT (*pending_weaks, i, pe)
     if (id == pe->name)
@@ -309,7 +316,7 @@ maybe_apply_pending_pragma_weaks (void)
   pending_weak *pe;
   symtab_node target;
 
-  if (!pending_weaks)
+  if (vec_safe_is_empty (pending_weaks))
     return;
 
   FOR_EACH_VEC_ELT (*pending_weaks, i, pe)
