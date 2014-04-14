b82b06487ef2331db4610d74a2d2816b8bb0ddd2
Jason Merrill <jason@redhat.com>
2014-04-14 00:25:55 -0400

    revert: gcc-dg.exp (dg-build-dso): New.
    	Revert:
    	* lib/gcc-dg.exp (dg-build-dso): New.
    	(gcc-dg-test-1): Handle dg-do-what "dso".
    
    From-SVN: r209353

--- gcc/testsuite/g++.dg/dso/dlclose1-dso.cc
+++
@@ -1,9 +0,0 @@
-// { dg-options "-fno-gnu-unique" }
-
-// A static variable in an inline function uses STB_GNU_UNIQUE normally.
-inline int foo() { static int i; return ++i; }
-
-extern "C" int fn()
-{
-  return foo();
-}
