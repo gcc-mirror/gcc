3bc315478bc34c8123c0bc192d2bb31844b879fa
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2014-04-14 12:56:54 +0000

    Skip gcc.dg/lto/pr55113 on Solaris 10+/x86
    	* gcc.dg/lto/pr55113_0.c: Skip on i?86-*-solaris2.1[0-1]*.
    
    From-SVN: r209368

--- gcc/testsuite/gcc.dg/lto/pr55113_0.c
+++ gcc/testsuite/gcc.dg/lto/pr55113_0.c
@@ -2,6 +2,7 @@
 /* { dg-lto-do link } */
 /* { dg-lto-options { { -flto -fshort-double -O0 } } }*/
 /* { dg-skip-if "PR60410" { x86_64-*-* || { i?86-*-* && lp64 } } } */
+/* { dg-skip-if "PR60410" { i?86-*-solaris2.1[0-9]* } } */
 
 int 
 main(void)
