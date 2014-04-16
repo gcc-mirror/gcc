6da7682f86de3d64d68f5956afcf1f120586dd4a
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-04-16 20:17:46 +0000

    2014-04-16  Patrick Palka  <patrick@parcs.ath.cx>
    	PR c++/60765
    	* decl2.c (cplus_decl_attributes): Handle
    	pointer-to-member-function declarations.
    
    2014-04-16  Patrick Palka  <patrick@parcs.ath.cx>
    
    	PR c++/60764
    	* call.c (build_user_type_coversion): Use build_dummy_object
    	to create the placeholder object for a constructor method call.
    	(build_special_member_call): Likewise.
    	(build_over_call): Check for the placeholder object with
    	is_dummy_object.
    	(build_new_method_call_1): Likewise.  Don't attempt to resolve
    	a dummy object for a constructor method call.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@209447 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/decl2.c
+++ gcc/cp/decl2.c
@@ -1427,7 +1427,15 @@ cplus_decl_attributes (tree *decl, tree attributes, int flags)
   if (TREE_CODE (*decl) == TEMPLATE_DECL)
     decl = &DECL_TEMPLATE_RESULT (*decl);
 
-  decl_attributes (decl, attributes, flags);
+  if (TREE_TYPE (*decl) && TYPE_PTRMEMFUNC_P (TREE_TYPE (*decl)))
+    {
+      attributes
+	= decl_attributes (decl, attributes, flags | ATTR_FLAG_FUNCTION_NEXT);
+      decl_attributes (&TYPE_PTRMEMFUNC_FN_TYPE (TREE_TYPE (*decl)),
+		       attributes, flags);
+    }
+  else
+    decl_attributes (decl, attributes, flags);
 
   if (TREE_CODE (*decl) == TYPE_DECL)
     SET_IDENTIFIER_TYPE_VALUE (DECL_NAME (*decl), TREE_TYPE (*decl));
