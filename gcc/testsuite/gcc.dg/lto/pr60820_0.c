6c5f20b680f656069b0fb41a1912fae1d06818dc
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-04-17 02:32:26 +0000

    PR lto/60820 	* gcc.dg/lto/pr60820_0.c: New testcase. 	* gcc.dg/lto/pr60820_1.c: New testcase.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@209460 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/lto/pr60820_0.c
@@ -0,0 +1,13 @@
+/* { dg-lto-do link } */
+/* { dg-lto-options {{-flto -r -nostdlib -O2}} } */
+#include <stdio.h>
+struct in6_addr {int bah;};
+extern const struct in6_addr in6addr_any;
+static const struct in6_addr local_in6addr_any = {1};
+#pragma weak in6addr_any = local_in6addr_any
+
+__attribute__ ((used))
+void foo2()
+{
+  fprintf (stderr, "v1: %p, v2: %p\n", &local_in6addr_any, &in6addr_any);
+}
