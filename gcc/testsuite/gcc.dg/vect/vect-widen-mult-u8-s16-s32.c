d367387cf1ba680dd02786332cf71ffa70b8d9a6
Cong Hou <congh@google.com>
2014-04-18 15:46:49 -0400

    tree-vect-patterns.c (vect_recog_widen_mult_pattern): Enhance the widen-mult pattern by handling two operands with different sizes.
    2014-04-18  Cong Hou  <congh@google.com>
    
            * tree-vect-patterns.c (vect_recog_widen_mult_pattern): Enhance
            the widen-mult pattern by handling two operands with different
            sizes.
            * tree-vect-stmts.c (vectorizable_conversion): Allow multi-steps
            conversions after widening mult operation.
            (supportable_widening_operation): Likewise.
    
    2014-04-18  Cong Hou  <congh@google.com>
    
            * gcc.dg/vect/vect-widen-mult-u8-s16-s32.c: New test.
            * gcc.dg/vect/vect-widen-mult-u8-u32.c: New test.
    
    From-SVN: r209524

---
+++ gcc/testsuite/gcc.dg/vect/vect-widen-mult-u8-s16-s32.c
@@ -0,0 +1,48 @@
+/* { dg-require-effective-target vect_int } */
+
+#include <stdarg.h>
+#include "tree-vect.h"
+
+#define N 64
+
+unsigned char X[N] __attribute__ ((__aligned__(__BIGGEST_ALIGNMENT__)));
+short Y[N] __attribute__ ((__aligned__(__BIGGEST_ALIGNMENT__)));
+int result[N];
+
+/* unsigned char * short -> int widening-mult.  */
+__attribute__ ((noinline)) int
+foo (int len) {
+  int i;
+
+  for (i=0; i<len; i++) {
+    result[i] = X[i] * Y[i];
+  }
+}
+
+int main (void)
+{
+  int i;
+
+  check_vect ();
+
+  for (i=0; i<N; i++) {
+    X[i] = i;
+    Y[i] = 64-i;
+    __asm__ volatile ("");
+  }
+
+  foo (N);
+
+  for (i=0; i<N; i++) {
+    if (result[i] != X[i] * Y[i])
+      abort ();
+  }
+
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { target { vect_widen_mult_hi_to_si || vect_unpack } } } } */
+/* { dg-final { scan-tree-dump-times "vect_recog_widen_mult_pattern: detected" 1 "vect" { target vect_widen_mult_hi_to_si_pattern } } } */
+/* { dg-final { scan-tree-dump-times "pattern recognized" 1 "vect" { target vect_widen_mult_hi_to_si_pattern } } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
+
