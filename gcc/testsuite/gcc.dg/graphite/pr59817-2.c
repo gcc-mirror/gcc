72d576cb50e045aa4efa3e6d414b0140dd486dde
Richard Biener <rguenther@suse.de>
2014-04-22 13:22:32 +0000

    backport: re PR tree-optimization/59817 (ICE in extract_affine_chrec with -O2 -ftree-loop-linear)
    2014-04-22  Richard Biener  <rguenther@suse.de>
    
    	Backport from mainline
    	2014-04-14  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/59817
    	PR tree-optimization/60453
    	* graphite-scop-detection.c (graphite_can_represent_scev): Complete
    	recursion to catch all CHRECs in the scalar evolution and restrict
    	the predicate for the remains appropriately.
    
    	* gfortran.dg/graphite/pr59817.f: New testcase.
    	* gcc.dg/graphite/pr59817-1.c: Likewise.
    	* gcc.dg/graphite/pr59817-2.c: Likewise.
    
    From-SVN: r209628

---
+++ gcc/testsuite/gcc.dg/graphite/pr59817-2.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -floop-interchange" } */
+
+void
+xl(void)
+{
+  static int j3;
+  for (j3 = 0; j3 < 1; ++j3) {
+      static int f2;
+      static int w7;
+      short int b5;
+      int ok;
+      f2 = (b5 += ok) ? (w7 = 0): (w7 ? 0 : (f2 = ok));
+  }
+}
