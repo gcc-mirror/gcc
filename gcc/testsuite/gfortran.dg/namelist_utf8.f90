446fb7326709d34894f7852d3be04d14e1b5b157
jvdelisle <jvdelisle@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-04-26 21:56:48 +0000

    2014-04-26  Jerry DeLisle  <jvdelisle@gcc.gnu>
    	PR libfortran/52539
    	* gfortran.dg/namelist_utf8.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@209829 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/namelist_utf8.f90
@@ -0,0 +1,30 @@
+! { dg-do run }
+! PR52539 UTF-8 support for namelist read and write
+
+character(len=10, kind=4) :: str, str2
+character(len=25, kind=4) :: str3
+
+namelist /nml/ str
+
+str = 4_'a'//char (int (z'4F60'),4)     &
+      //char (int (z'597D'), 4)//4_'b'
+
+open(99, encoding='utf-8',form='formatted')
+write(99, '(3a)') '&nml str = "', str, '" /'
+write(99, '(a)') str
+rewind(99)
+
+str = 4_'XXXX'
+str2 = 4_'YYYY'
+read(99,nml=nml)
+read(99, *) str2
+if (str2 /= str) call abort
+rewind(99)
+
+read(99,'(A)') str3
+if (str3 /= 4_'&nml str = "' // str // 4_'" /') call abort
+read(99,'(A)') str3
+if (str3 /= str) call abort
+
+close(99, status='delete')
+end
