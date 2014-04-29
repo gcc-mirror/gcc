9f1b5eff47847ef8b682697450a80e31df14a42c
alalaw01 <alalaw01@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-04-29 13:08:46 +0000

    Add execution + assembler tests of the AArch64 ZIP Intrinsics.
    	* gcc.target/aarch64/simd/simd.exp: New file.
    	* gcc.target/aarch64/simd/vzipf32_1.c: New file.
    	* gcc.target/aarch64/simd/vzipf32.x: New file.
    	* gcc.target/aarch64/simd/vzipp16_1.c: New file.
    	* gcc.target/aarch64/simd/vzipp16.x: New file.
    	* gcc.target/aarch64/simd/vzipp8_1.c: New file.
    	* gcc.target/aarch64/simd/vzipp8.x: New file.
    	* gcc.target/aarch64/simd/vzipqf32_1.c: New file.
    	* gcc.target/aarch64/simd/vzipqf32.x: New file.
    	* gcc.target/aarch64/simd/vzipqp16_1.c: New file.
    	* gcc.target/aarch64/simd/vzipqp16.x: New file.
    	* gcc.target/aarch64/simd/vzipqp8_1.c: New file.
    	* gcc.target/aarch64/simd/vzipqp8.x: New file.
    	* gcc.target/aarch64/simd/vzipqs16_1.c: New file.
    	* gcc.target/aarch64/simd/vzipqs16.x: New file.
    	* gcc.target/aarch64/simd/vzipqs32_1.c: New file.
    	* gcc.target/aarch64/simd/vzipqs32.x: New file.
    	* gcc.target/aarch64/simd/vzipqs8_1.c: New file.
    	* gcc.target/aarch64/simd/vzipqs8.x: New file.
    	* gcc.target/aarch64/simd/vzipqu16_1.c: New file.
    	* gcc.target/aarch64/simd/vzipqu16.x: New file.
    	* gcc.target/aarch64/simd/vzipqu32_1.c: New file.
    	* gcc.target/aarch64/simd/vzipqu32.x: New file.
    	* gcc.target/aarch64/simd/vzipqu8_1.c: New file.
    	* gcc.target/aarch64/simd/vzipqu8.x: New file.
    	* gcc.target/aarch64/simd/vzips16_1.c: New file.
    	* gcc.target/aarch64/simd/vzips16.x: New file.
    	* gcc.target/aarch64/simd/vzips32_1.c: New file.
    	* gcc.target/aarch64/simd/vzips32.x: New file.
    	* gcc.target/aarch64/simd/vzips8_1.c: New file.
    	* gcc.target/aarch64/simd/vzips8.x: New file.
    	* gcc.target/aarch64/simd/vzipu16_1.c: New file.
    	* gcc.target/aarch64/simd/vzipu16.x: New file.
    	* gcc.target/aarch64/simd/vzipu32_1.c: New file.
    	* gcc.target/aarch64/simd/vzipu32.x: New file.
    	* gcc.target/aarch64/simd/vzipu8_1.c: New file.
    	* gcc.target/aarch64/simd/vzipu8.x: New file.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@209893 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/aarch64/simd/vzipqu8_1.c
@@ -0,0 +1,11 @@
+/* Test the `vzipq_u8' AArch64 SIMD intrinsic.  */
+
+/* { dg-do run } */
+/* { dg-options "-save-temps -fno-inline" } */
+
+#include <arm_neon.h>
+#include "vzipqu8.x"
+
+/* { dg-final { scan-assembler-times "zip1\[ \t\]+v\[0-9\]+\.16b, ?v\[0-9\]+\.16b, ?v\[0-9\]+\.16b!?\(?:\[ \t\]+@\[a-zA-Z0-9 \]+\)?\n" 1 } } */
+/* { dg-final { scan-assembler-times "zip2\[ \t\]+v\[0-9\]+\.16b, ?v\[0-9\]+\.16b, ?v\[0-9\]+\.16b!?\(?:\[ \t\]+@\[a-zA-Z0-9 \]+\)?\n" 1 } } */
+/* { dg-final { cleanup-saved-temps } } */
