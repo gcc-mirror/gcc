bf324702b514a2d3c5ea534deb1019cb78399aaf
alalaw01 <alalaw01@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-04-29 19:14:43 +0000

    Add execution tests of ARM ZIP Intrinsics.
    	* gcc.target/arm/simd/simd.exp: New file.
    	* gcc.target/arm/simd/vzipqf32_1.c: New file.
    	* gcc.target/arm/simd/vzipqp16_1.c: New file.
    	* gcc.target/arm/simd/vzipqp8_1.c: New file.
    	* gcc.target/arm/simd/vzipqs16_1.c: New file.
    	* gcc.target/arm/simd/vzipqs32_1.c: New file.
    	* gcc.target/arm/simd/vzipqs8_1.c: New file.
    	* gcc.target/arm/simd/vzipqu16_1.c: New file.
    	* gcc.target/arm/simd/vzipqu32_1.c: New file.
    	* gcc.target/arm/simd/vzipqu8_1.c: New file.
    	* gcc.target/arm/simd/vzipf32_1.c: New file.
    	* gcc.target/arm/simd/vzipp16_1.c: New file.
    	* gcc.target/arm/simd/vzipp8_1.c: New file.
    	* gcc.target/arm/simd/vzips16_1.c: New file.
    	* gcc.target/arm/simd/vzips32_1.c: New file.
    	* gcc.target/arm/simd/vzips8_1.c: New file.
    	* gcc.target/arm/simd/vzipu16_1.c: New file.
    	* gcc.target/arm/simd/vzipu32_1.c: New file.
    	* gcc.target/arm/simd/vzipu8_1.c: New file.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@209908 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/simd/vzipqp8_1.c
@@ -0,0 +1,12 @@
+/* Test the `vzipQp8' ARM Neon intrinsic.  */
+
+/* { dg-do run } */
+/* { dg-require-effective-target arm_neon_ok } */
+/* { dg-options "-save-temps -O1 -fno-inline" } */
+/* { dg-add-options arm_neon } */
+
+#include "arm_neon.h"
+#include "../../aarch64/simd/vzipqp8.x"
+
+/* { dg-final { scan-assembler-times "vzip\.8\[ \t\]+\[qQ\]\[0-9\]+, ?\[qQ\]\[0-9\]+!?\(?:\[ \t\]+@\[a-zA-Z0-9 \]+\)?\n" 1 } } */
+/* { dg-final { cleanup-saved-temps } } */
