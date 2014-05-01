0b6de244c41ce155a58d2f80a82f650f1cfbede7
kyukhin <kyukhin@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-01 07:23:06 +0000

    gcc/ 	* tree-if-conv.c (is_cond_scalar_reduction): New function. 	(convert_scalar_cond_reduction): Likewise. 	(predicate_scalar_phi): Add recognition and transformation 	of simple conditioanl reduction to be vectorizable.
    gcc/testsuite/
    	* gcc.dg/cond-reduc-1.c: New test.
    	* gcc.dg/cond-reduc-2.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@209972 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/cond-reduc-2.c
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target vect_int } */
+
+#define N 512
+int a[N], b[N];
+void foo(int k)
+{
+  int i, res = 0;
+  for (i=0; i<N; i++)
+  {
+    if (b[i] != 0)
+      res += b[i];
+  }
+  a[k] = sum;
+}
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
+
