19fc9faaaf98b3c45ede31da2e3cb970ef7b372a
Marek Polacek <polacek@redhat.com>
2014-05-02 18:09:42 +0000

    c-typeck.c (c_finish_return): Separate warning_at calls.
    c/
    	* c-typeck.c (c_finish_return): Separate warning_at calls.
    cp/
    	* typeck.c (maybe_warn_about_returning_address_of_local): Separate
    	warning_at calls.
    
    From-SVN: r210012

--- gcc/cp/typeck.c
+++ gcc/cp/typeck.c
@@ -8309,10 +8309,12 @@ maybe_warn_about_returning_address_of_local (tree retval)
       if (TREE_CODE (valtype) == REFERENCE_TYPE)
 	warning (OPT_Wreturn_local_addr, "reference to local variable %q+D returned",
 		 whats_returned);
+      else if (TREE_CODE (whats_returned) == LABEL_DECL)
+	warning (OPT_Wreturn_local_addr, "address of label %q+D returned",
+		 whats_returned);
       else
-	warning (OPT_Wreturn_local_addr, "address of %s %q+D returned",
-		 TREE_CODE (whats_returned) == LABEL_DECL
-		 ? "label" : "local variable", whats_returned);
+	warning (OPT_Wreturn_local_addr, "address of local variable %q+D "
+		 "returned", whats_returned);
       return;
     }
 }
