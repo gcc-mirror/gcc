2c23db6dd33ea21edc8970449e420eea4eaa3616
Evgeny Stupachenko <evstupac@gmail.com>
2014-05-07 12:10:22 +0000

    re PR tree-optimization/52252 (An opportunity for x86 gcc vectorizer (gain up to 3 times))
    gcc/
    	* tree-vect-data-refs.c (vect_grouped_load_supported): New
    	check for loads group of length 3.
    	(vect_permute_load_chain): New permutations for loads group of
    	length 3.
    	* tree-vect-stmts.c (vect_model_load_cost): Change cost
    	of vec_perm_shuffle for the new permutations.
    
    gcc/testsuite/
    	PR tree-optimization/52252
    	* gcc.dg/vect/pr52252-ld.c: Test on loads group of size 3.
    
    From-SVN: r210155

---
+++ gcc/testsuite/gcc.dg/vect/pr52252-ld.c
@@ -0,0 +1,30 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -g -ftree-vectorize -mssse3 -fdump-tree-vect-details" { target { i?86-*-* x86_64-*-* } } } */
+
+#define byte unsigned char
+
+void
+matrix_mul (byte *in, byte *out, int size)
+{
+  int i;
+  for (i = 0; i < size; i++)
+    {
+      byte in0 = in[0];
+      byte in1 = in[1];
+      byte in2 = in[2];
+      byte out0, out1, out2, out3;
+      out0 = in0 + in1;
+      out1 = in0 + in2;
+      out2 = in1 + in2;
+      out3 = in0 + in1 + in2;
+      out[0] = out0;
+      out[1] = out1;
+      out[2] = out2;
+      out[3] = out3;
+      in += 3;
+      out += 4;
+    }
+}
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
