9c13483b14af847e3f3d1753519db25b4ae77fb6
Richard Biener <rguenther@suse.de>
2014-05-07 08:04:15 +0000

    re PR tree-optimization/57864 (ICE in bitmap_set_replace_value, at tree-ssa-pre.c:862)
    2014-05-07  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/57864
    	* gcc.dg/torture/pr57864.c: New testcase.
    
    From-SVN: r210142

---
+++ gcc/testsuite/gcc.dg/torture/pr57864.c
@@ -0,0 +1,37 @@
+/* { dg-do compile } */
+
+union U {
+    double val;
+    union U *ptr;
+};
+
+union U *d;
+double a;
+int b;
+int c;
+
+static void fn1(union U *p1, int p2, _Bool p3)
+{
+    union U *e;
+
+    if (p2 == 0)
+	a = ((union U*)((unsigned long)p1 & ~1))->val;
+
+    if (b) {
+	e = p1;
+    } else if (c) {
+	e = ((union U*)((unsigned long)p1 & ~1))->ptr;
+	d = e;
+    } else {
+	e = 0;
+	d = ((union U*)0)->ptr;
+    }
+
+    fn1 (e, 0, 0);
+    fn1 (0, 0, p3);
+}
+
+void fn2 (void)
+{
+  fn1 (0, 0, 0);
+}
