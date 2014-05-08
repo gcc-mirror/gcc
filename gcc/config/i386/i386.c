6b50e5dc340d8a62eec1e78c2de7735a84a476a4
wmi <wmi@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-08 16:44:52 +0000

    gcc/ 2014-05-08  Wei Mi  <wmi@google.com>
    	PR target/58066
    	* config/i386/i386.c (ix86_compute_frame_layout):
    	Update preferred_stack_boundary for call, expanded from
    	tls descriptor.
    	* config/i386/i386.md:
    	(*tls_global_dynamic_32_gnu): Update RTX to depend on
    	SP register.
    	(*tls_local_dynamic_base_32_gnu): Ditto.
    	(*tls_local_dynamic_32_once): Ditto.
    	(tls_global_dynamic_64_<mode>): Set
    	ix86_tls_descriptor_calls_expanded_in_cfun.
    	(tls_local_dynamic_base_64_<mode>): Ditto.
    	(tls_global_dynamic_32): Set
    	ix86_tls_descriptor_calls_expanded_in_cfun. Update RTX
    	to depend on SP register.
    	(tls_local_dynamic_base_32): Ditto.
    
    gcc/testsuite/
    2014-05-08  Wei Mi  <wmi@google.com>
    
    	PR target/58066
    	* gcc.target/i386/pr58066.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@210222 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -9486,20 +9486,30 @@ ix86_compute_frame_layout (struct ix86_frame *frame)
   frame->nregs = ix86_nsaved_regs ();
   frame->nsseregs = ix86_nsaved_sseregs ();
 
-  stack_alignment_needed = crtl->stack_alignment_needed / BITS_PER_UNIT;
-  preferred_alignment = crtl->preferred_stack_boundary / BITS_PER_UNIT;
-
   /* 64-bit MS ABI seem to require stack alignment to be always 16 except for
      function prologues and leaf.  */
-  if ((TARGET_64BIT_MS_ABI && preferred_alignment < 16)
+  if ((TARGET_64BIT_MS_ABI && crtl->preferred_stack_boundary < 128)
       && (!crtl->is_leaf || cfun->calls_alloca != 0
           || ix86_current_function_calls_tls_descriptor))
     {
-      preferred_alignment = 16;
-      stack_alignment_needed = 16;
       crtl->preferred_stack_boundary = 128;
       crtl->stack_alignment_needed = 128;
     }
+  /* preferred_stack_boundary is never updated for call
+     expanded from tls descriptor. Update it here. We don't update it in
+     expand stage because according to the comments before
+     ix86_current_function_calls_tls_descriptor, tls calls may be optimized
+     away.  */
+  else if (ix86_current_function_calls_tls_descriptor
+	   && crtl->preferred_stack_boundary < PREFERRED_STACK_BOUNDARY)
+    {
+      crtl->preferred_stack_boundary = PREFERRED_STACK_BOUNDARY;
+      if (crtl->stack_alignment_needed < PREFERRED_STACK_BOUNDARY)
+	crtl->stack_alignment_needed = PREFERRED_STACK_BOUNDARY;
+    }
+
+  stack_alignment_needed = crtl->stack_alignment_needed / BITS_PER_UNIT;
+  preferred_alignment = crtl->preferred_stack_boundary / BITS_PER_UNIT;
 
   gcc_assert (!size || stack_alignment_needed);
   gcc_assert (preferred_alignment >= STACK_BOUNDARY / BITS_PER_UNIT);
