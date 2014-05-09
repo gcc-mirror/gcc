a8218d79f8ad3b886adfefa24587192569f23f0f
Jonathan Wakely <jwakely@redhat.com>
2014-05-09 20:39:14 +0100

    gnu.ver (GLIBCXX_3.4.20): Correct regex_error export.
    	* config/abi/pre/gnu.ver (GLIBCXX_3.4.20): Correct regex_error export.
    	(GLIBCXX_3.4.21): Export base object constructor for regex_error.
    	* acinclude.m4 (libtool_VERSION): Bump.
    	* configure: Regenerate.
    	* testsuite/util/testsuite_abi.cc: Add GLIBCXX_3.4.21 version.
    	* testsuite/28_regex/regex_error/base.cc: New.
    
    From-SVN: r210290

---
+++ libstdc++-v3/testsuite/28_regex/regex_error/base.cc
@@ -0,0 +1,32 @@
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 28.6 [re.badexp]
+
+// { dg-options "-std=c++11" }
+
+#include <regex>
+
+struct re : std::regex_error
+{
+  re() : regex_error(std::regex_constants::error_stack) { }
+};
+
+int main()
+{
+  re r __attribute__((unused));
+}
