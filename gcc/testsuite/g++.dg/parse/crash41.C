15608833c968ed21eea73c208c2ee7d62d08d693
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-09 18:15:57 +0000

    PR c++/32019 	* call.c (build_conditional_expr_1): Improve ambiguity diagnostic.
    	PR c++/54348
    	* call.c (build_conditional_expr_1): If overload resolution finds
    	no match, just say "different types".
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@210282 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/parse/crash41.C
+++ gcc/testsuite/g++.dg/parse/crash41.C
@@ -5,4 +5,4 @@ struct A
   A(int)(); // { dg-error "declared" }
 };
 
-template<int> void foo(bool b, A a) { b ? a : 0; } // { dg-error "no match" }
+template<int> void foo(bool b, A a) { b ? a : 0; } // { dg-error "?:" }
