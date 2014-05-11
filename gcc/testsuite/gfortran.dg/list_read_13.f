ea2198c5e18174ff48b25b3f99b558454b217bb5
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2014-05-11 03:53:00 +0000

    backport: re PR libfortran/61049 (NIST test FM906 fails)
    2014-05-10  Jerry DeLisle  <jvdelisle@gcc.gnu>
    
    	Backport from trunk.
    	PR libfortran/61049
    	* gfortran.dg/list_read_13.f: New test.
    
    From-SVN: r210307

---
+++ gcc/testsuite/gfortran.dg/list_read_13.f
@@ -0,0 +1,13 @@
+c { dg-do run }
+c PR61049, reduced test case by  Dominique d'Humieres
+      character(len=30) :: buff = ", (2.0, 3.0),,6.0D0, 2*,"
+      DOUBLE PRECISION AVD, BVD, CVD, DVCORR 
+      COMPLEX AVC, BVC, CVC, ZVCORR
+      
+      read(buff, *, err=10)  AVD, AVC, BVC, BVD, CVC, CVD
+      goto 20
+ 10   call abort
+ 20   continue       
+      end
+    
+  
