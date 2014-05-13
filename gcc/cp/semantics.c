02b19aed916c16b507b9cdd189368585e5c9aaee
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-13 17:54:00 +0000

    PR c++/61151 	* semantics.c (is_this_parameter): Allow capture proxies too.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@210394 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -8158,8 +8158,10 @@ maybe_initialize_constexpr_call_table (void)
 bool
 is_this_parameter (tree t)
 {
-  return (TREE_CODE (t) == PARM_DECL
-	  && DECL_NAME (t) == this_identifier);
+  if (!DECL_P (t) || DECL_NAME (t) != this_identifier)
+    return false;
+  gcc_assert (TREE_CODE (t) == PARM_DECL || is_capture_proxy (t));
+  return true;
 }
 
 /* We have an expression tree T that represents a call, either CALL_EXPR
