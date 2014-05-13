b82f988e8d21c709426d691329cf025e978d94ed
Jonathan Wakely <jwakely@redhat.com>
2014-05-13 18:22:08 +0100

    re PR libstdc++/60497 (unique_ptr<T> tries to complete its type T even though it's not required to be a complete type)
    	PR libstdc++/60497
    	* include/debug/array (get): Qualify call to other get overload.
    	* include/profile/array (get): Likewise.
    	* include/std/array (get): Likewise.
    	* include/std/functional (_Mu, _Bind, _Bind_result): Qualify std::get.
    	* include/std/mutex (unique_lock, call_once): Use __addressof.
    	(__unlock_impl): Remove unused template.
    	(__try_to_lock): Declare inline.
    	(__try_lock_impl::__do_try_lock): Qualify function calls.
    	(lock): Avoid narrowing conversion.
    	* testsuite/20_util/bind/60497.cc: New.
    	* testsuite/23_containers/array/element_access/60497.cc: New.
    	* testsuite/30_threads/call_once/60497.cc: New.
    	* testsuite/30_threads/unique_lock/cons/60497.cc: New.
    
    From-SVN: r210388

---
+++ libstdc++-v3/testsuite/30_threads/unique_lock/cons/60497.cc
@@ -0,0 +1,49 @@
+// { dg-do compile }
+// { dg-options " -std=gnu++11 " }
+
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// libstdc++/60497
+
+#include <mutex>
+#include <memory>
+
+struct A;
+template<typename  T> struct B { T t; };
+
+template<typename Dummy>
+struct Lockable
+{
+  void lock();
+  void unlock();
+  bool try_lock();
+};
+
+using test_type = Lockable<std::unique_ptr<B<A>>>;
+
+void test01()
+{
+  test_type l;
+  std::unique_lock<test_type> ul(l);
+}
+
+void test02()
+{
+  test_type l1, l2, l3;
+  std::lock(l1, l2, l3);
+}
