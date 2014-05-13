b49122a5bf322806a59668d1166c5a4bf4d353ed
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-13 17:22:08 +0000

    PR libstdc++/60497 	* include/debug/array (get): Qualify call to other get overload. 	* include/profile/array (get): Likewise. 	* include/std/array (get): Likewise. 	* include/std/functional (_Mu, _Bind, _Bind_result): Qualify std::get. 	* include/std/mutex (unique_lock, call_once): Use __addressof. 	(__unlock_impl): Remove unused template. 	(__try_to_lock): Declare inline. 	(__try_lock_impl::__do_try_lock): Qualify function calls. 	(lock): Avoid narrowing conversion. 	* testsuite/20_util/bind/60497.cc: New. 	* testsuite/23_containers/array/element_access/60497.cc: New. 	* testsuite/30_threads/call_once/60497.cc: New. 	* testsuite/30_threads/unique_lock/cons/60497.cc: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@210388 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/20_util/bind/60497.cc
@@ -0,0 +1,40 @@
+// { dg-options "-std=gnu++11" }
+// { dg-do compile }
+
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// libstdc++/60497
+
+#include <functional>
+#include <memory>
+
+struct A;
+template<typename T> struct B { T t; };
+
+using UP = std::unique_ptr<B<A>>;
+
+bool f(UP&, UP&) { return true; }
+
+bool g(UP& p)
+{
+  auto binder = std::bind(f, std::ref(p), std::placeholders::_1);
+  bool b1 = binder(std::ref(p));
+  auto binderbinder = std::bind(binder, std::placeholders::_1);
+  bool b2 = binderbinder(std::ref(p));
+  return b1 && b2;
+}
