8fa5aaf3c066b3cbfe547b61089fb07a1a43c49f
Nathan Sidwell <nathan@codesourcery.com>
2014-05-13 13:35:20 +0000

    loongson-simd.c: Disable micromips too.
    2014-05-13  Nathan Sidwell  <nathan@codesourcery.com>
    	    Sandra Loosemore  <sandra@codesourcery.com>
    
    	gcc/testsuite/
    	* gcc.target/mips/loongson-simd.c: Disable micromips too.
    
    Co-Authored-By: Sandra Loosemore <sandra@codesourcery.com>
    
    From-SVN: r210374

--- gcc/testsuite/gcc.target/mips/loongson-simd.c
+++ gcc/testsuite/gcc.target/mips/loongson-simd.c
@@ -19,10 +19,10 @@ along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
 
 /* { dg-do run } */
-/* loongson.h does not handle or check for MIPS16ness.  There doesn't
-   seem any good reason for it to, given that the Loongson processors
-   do not support MIPS16.  */
-/* { dg-options "isa=loongson -mhard-float -mno-mips16 -flax-vector-conversions" } */
+/* loongson.h does not handle or check for MIPS16ness or
+   microMIPSness.  There doesn't seem any good reason for it to, given
+   that the Loongson processors do not support either.  */
+/* { dg-options "isa=loongson -mhard-float -mno-micromips -mno-mips16 -flax-vector-conversions" } */
 
 #include "loongson.h"
 #include <stdio.h>
