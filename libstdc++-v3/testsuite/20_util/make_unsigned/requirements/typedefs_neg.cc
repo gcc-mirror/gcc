f2b58c906cf64f3e082bda9808b9f049bb71d5b4
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-15 12:31:05 +0000

    PR libstdc++/60326 	* include/std/type_traits (__make_unsigned, __make_signed): Define 	specializations for wchar_t, char16_t and char32_t. 	* testsuite/20_util/make_signed/requirements/typedefs-4.cc: New. 	* testsuite/20_util/make_unsigned/requirements/typedefs-1.cc: Correct 	test for make_unsigned<volatile wchar_t>. 	* testsuite/20_util/make_unsigned/requirements/typedefs-2.cc: 	Likewise. 	* testsuite/20_util/declval/requirements/1_neg.cc: Adjust dg-error 	line number. 	* testsuite/20_util/make_signed/requirements/typedefs_neg.cc: 	Likewise. 	* testsuite/20_util/make_unsigned/requirements/typedefs_neg.cc: 	Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@210473 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/20_util/make_unsigned/requirements/typedefs_neg.cc
+++ libstdc++-v3/testsuite/20_util/make_unsigned/requirements/typedefs_neg.cc
@@ -48,5 +48,5 @@ void test01()
 // { dg-error "required from here" "" { target *-*-* } 40 }
 // { dg-error "required from here" "" { target *-*-* } 42 }
 
-// { dg-error "invalid use of incomplete type" "" { target *-*-* } 1632 }
-// { dg-error "declaration of" "" { target *-*-* } 1596 }
+// { dg-error "invalid use of incomplete type" "" { target *-*-* } 1647 }
+// { dg-error "declaration of" "" { target *-*-* } 1611 }
