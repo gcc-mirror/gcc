a2e6c10cbd7ff6f51052d5a550b4bf999189aa31
Zhenqiang Chen <zhenqiang.chen@linaro.org>
2014-05-15 06:54:48 +0000

    regcprop.h: New file.
    2014-05-15  Zhenqiang Chen  <zhenqiang.chen@linaro.org>
    
    	* regcprop.h: New file.
    	* regcprop.c (skip_debug_insn_p): New decl.
    	(replace_oldest_value_reg): Check skip_debug_insn_p.
    	(copyprop_hardreg_forward_bb_without_debug_insn.): New function.
    	* shrink-wrap.c: include regcprop.h
    	(prepare_shrink_wrap):
    	Call copyprop_hardreg_forward_bb_without_debug_insn.
    
    testsuite/ChangeLog:
    2014-05-15  Zhenqiang Chen  <zhenqiang.chen@linaro.org>
    
    	* shrink-wrap-loop.c: New test case.
    
    From-SVN: r210458

---
+++ gcc/testsuite/gcc.dg/shrink-wrap-loop.c
@@ -0,0 +1,20 @@
+/* { dg-do compile { target { { x86_64-*-* } || { arm_thumb2 } } } } */
+/* { dg-options "-O2 -fdump-rtl-pro_and_epilogue"  } */
+
+int foo (int *p1, int *p2);
+
+int
+test (int *p1, int *p2)
+{
+  int *p;
+
+  for (p = p2; p != 0; p++)
+    {
+      if (!foo (p, p1))
+        return 0;
+    }
+
+  return 1;
+}
+/* { dg-final { scan-rtl-dump "Performing shrink-wrapping" "pro_and_epilogue"  } } */
+/* { dg-final { cleanup-rtl-dump "pro_and_epilogue" } } */
