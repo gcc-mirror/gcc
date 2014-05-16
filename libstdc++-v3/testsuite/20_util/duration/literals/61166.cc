a2f4bd2ae1b6f66290315e145be759e4a8abd7ac
Ed Smith-Rowland <3dw4rd@verizon.net>
2014-05-16 09:30:57 +0000

    re PR libstdc++/61166 (overflow when parse number in std::duration operator"")
    2014-05-15  Ed Smith-Rowland  <3dw4rd@verizon.net>
    	    Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/61166
    	* include/bits/parse_numbers.h: Use integral_constant to remove
    	duplication and simplify.
    	* testsuite/20_util/duration/literals/61166.cc: New.
    
    Co-Authored-By: Jonathan Wakely <jwakely@redhat.com>
    
    From-SVN: r210511

---
+++ libstdc++-v3/testsuite/20_util/duration/literals/61166.cc
@@ -0,0 +1,39 @@
+// { dg-do run }
+// { dg-options "-std=gnu++1y" }
+
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// libstdc++/61166
+
+#include <chrono>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  using namespace std::literals::chrono_literals;
+
+  // std::numeric_limits<unsigned>::max() == 4294967295
+  VERIFY( (429496729510h).count() == 429496729510 );
+}
+
+int
+main()
+{
+  test01();
+}
