1e5911a98f7733c3263a6c9dee5ab3ff06dbf57c
vmakarov <vmakarov@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-16 17:37:17 +0000

    2014-05-16  Vladimir Makarov  <vmakarov@redhat.com>
    	PR rtl-optimization/60969
    	* ira-costs.c (record_reg_classes): Allow only memory for pseudo.
    	Calculate costs for this case.
    
    2014-05-16  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR rtl-optimization/60969
    	* g++.dg/pr60969.C: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@210520 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/pr60969.C
@@ -0,0 +1,30 @@
+/* { dg-do compile { target i?86-*-* } } */
+/* { dg-options "-O2 -ftree-vectorize -march=pentium4" } */
+
+struct A
+{
+  float f, g, h, k;
+  A () {}
+  A (float v0, float x, float y) : f(v0), g(x), h(y), k(0.0f) {}
+  A bar (A &a, float t) { return A (f + a.f * t, g + a.g * t, h + a.h * t); }
+};
+
+A
+baz (A &x, A &y, float t)
+{
+  return x.bar (y, t);
+}
+
+A *
+foo (A &s, A &t, A &u, A &v, int y, int z)
+{
+  A *x = new A[y * z];
+  for (int i = 0; i < 7; i++)
+    {
+      A s = baz (s, u, i / (float) z);
+      A t = baz (t, v, i / (float) z);
+      for (int j = 0; j < 7; j++)
+        x[i * y + j] = baz (s, t, j / (float) y);
+    }
+  return x;
+}
