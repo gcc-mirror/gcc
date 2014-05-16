a764d66099b476db7d59829da1b86fd4a701523f
Richard Biener <rguenther@suse.de>
2014-05-16 08:05:50 +0000

    tree-ssa-sccvn.c: Include tree-cfg.h and domwalk.h.
    2014-05-16  Richard Biener  <rguenther@suse.de>
    
    	* tree-ssa-sccvn.c: Include tree-cfg.h and domwalk.h.
    	(set_ssa_val_to): Handle unexpected sets to VN_TOP.
    	(visit_phi): Ignore edges marked as not executable.
    	(class cond_dom_walker): New.
    	(cond_dom_walker::before_dom_children): Value-number
    	control statements and mark successor edges as not
    	executable if possible.
    	(run_scc_vn): First walk all control statements in
    	dominator order, marking edges as not executable.
    	* tree-inline.c (copy_edges_for_bb): Be not confused
    	about random edge flags.
    
    	* gcc.dg/tree-ssa/ssa-fre-39.c: New testcase.
    	* gcc.dg/tree-ssa/ssa-fre-40.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-pre-8.c: One more elimination.
    	* gcc.dg/tree-ssa/struct-aliasing-2.c: Scan cddce1 dump.
    
    From-SVN: r210492

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-fre-39.c
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-fre1" } */
+
+int foo (int i)
+{
+  int k = i + 1;
+  int j = i + 1;
+  if (k != j)
+    k = k + 1;
+  if (k != j)
+    k = k + 1;
+  k = k - i;
+  return k;
+}
+
+/* We should be able to value-number the final assignment to k to 1.  */
+
+/* { dg-final { scan-tree-dump "k_. = 1;" "fre1" } } */
+/* { dg-final { cleanup-tree-dump "fre1" } } */
