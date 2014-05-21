5402533b93a68dbfef2a5e91efb42dacd1725aa5
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-21 17:23:07 +0000

    PR c++/61133 	* lambda.c (build_capture_proxy, add_capture): Treat normal 	captures and init-captures identically.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@210720 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp1y/lambda-init6.C
+++ gcc/testsuite/g++.dg/cpp1y/lambda-init6.C
@@ -1,12 +1,10 @@
-// Test that simple captures are not named in the closure type, but
-// initialized captures are named.
+// Test that captures are not named in the closure type.
 // { dg-do compile { target c++1y } }
 
 int main()
 {
   int i;
   auto lam = [i,j=42]{};
-  lam.j;
-  lam.j.foo;			// { dg-error "::j" }
+  lam.j;                        // { dg-error "no member" }
   lam.i;			// { dg-error "no member" }
 }
