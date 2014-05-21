2bf8460e28d3ce27681a7cd011e25b88b614953a
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-21 15:09:37 +0000

    PR libstdc++/61269 	* include/std/type_traits: Move include outside namespace std. 	* testsuite/20_util/declval/requirements/1_neg.cc: Adjust dg-error. 	* testsuite/20_util/make_signed/requirements/typedefs_neg.cc: Likewise. 	* testsuite/20_util/make_unsigned/requirements/typedefs_neg.cc: 	Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@210715 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/20_util/declval/requirements/1_neg.cc
+++ libstdc++-v3/testsuite/20_util/declval/requirements/1_neg.cc
@@ -19,7 +19,7 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-error "static assertion failed" "" { target *-*-* } 2033 }
+// { dg-error "static assertion failed" "" { target *-*-* } 2036 }
 
 #include <utility>
 
