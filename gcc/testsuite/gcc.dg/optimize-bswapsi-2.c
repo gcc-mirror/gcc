73984f8494f31ba93aaebf84a50dc77fd4017c11
Thomas Preud'homme <thomas.preudhomme@arm.com>
2014-05-23 03:33:28 +0000

    re PR tree-optimization/54733 (Missing opportunity to optimize endian independent load/store)
    2014-05-23  Thomas Preud'homme  <thomas.preudhomme@arm.com>
    
    	PR tree-optimization/54733
    gcc/
    	* tree-ssa-math-opts.c (nop_stats): New "bswap_stats" structure.
    	(CMPNOP): Define.
    	(find_bswap_or_nop_load): New.
    	(find_bswap_1): Renamed to ...
    	(find_bswap_or_nop_1): This. Also add support for memory source.
    	(find_bswap): Renamed to ...
    	(find_bswap_or_nop): This. Also add support for memory source and
    	detection of bitwise operations equivalent to load in host endianness.
    	(execute_optimize_bswap): Likewise. Also move its leading comment back
    	in place and split statement transformation into ...
    	(bswap_replace): This.
    
    gcc/testsuite
    	* gcc.dg/optimize-bswapdi-3.c: New test to check extension of bswap
    	optimization to support memory sources and bitwise operations
    	equivalent to load in host endianness.
    	* gcc.dg/optimize-bswaphi-1.c: Likewise.
    	* gcc.dg/optimize-bswapsi-2.c: Likewise.
    	* gcc.c-torture/execute/bswap-2.c: Likewise.
    
    From-SVN: r210843

---
+++ gcc/testsuite/gcc.dg/optimize-bswapsi-2.c
@@ -0,0 +1,49 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target bswap32 } */
+/* { dg-require-effective-target stdint_types } */
+/* { dg-options "-O2 -fdump-tree-bswap" } */
+/* { dg-options "-O2 -fdump-tree-bswap -march=z900" { target s390-*-* } } */
+
+#include <stdint.h>
+
+extern unsigned char data[4];
+
+struct uint32_st {
+  unsigned char u0, u1, u2, u3;
+};
+
+uint32_t read_le32_1 (void)
+{
+  return data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+}
+
+uint32_t read_le32_2 (struct uint32_st data)
+{
+  return data.u0 | (data.u1 << 8) | (data.u2 << 16) | (data.u3 << 24);
+}
+
+uint32_t read_le32_3 (unsigned char *data)
+{
+  return *data | (*(data + 1) << 8) | (*(data + 2) << 16)
+	 | (*(data + 3) << 24);
+}
+
+uint32_t read_be32_1 (void)
+{
+  return data[3] | (data[2] << 8) | (data[1] << 16) | (data[0] << 24);
+}
+
+uint32_t read_be32_2 (struct uint32_st data)
+{
+  return data.u3 | (data.u2 << 8) | (data.u1 << 16) | (data.u0 << 24);
+}
+
+uint32_t read_be32_3 (unsigned char *data)
+{
+  return *(data + 3) | (*(data + 2) << 8) | (*(data + 1) << 16)
+	 | (*data << 24);
+}
+
+/* { dg-final { scan-tree-dump-times "32 bit load in host endianness found at" 3 "bswap" } } */
+/* { dg-final { scan-tree-dump-times "32 bit bswap implementation found at" 3 "bswap" { xfail alpha*-*-* arm*-*-* } } } */
+/* { dg-final { cleanup-tree-dump "bswap" } } */
