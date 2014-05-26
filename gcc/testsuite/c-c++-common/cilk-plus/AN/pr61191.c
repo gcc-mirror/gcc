d754a1996dcee819033f27d6b3a32a63718c0ca2
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-26 18:44:18 +0000

    * c-c++-common/cilk-plus/AN/pr61191.c: Fix dg-error directives.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@210944 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/c-c++-common/cilk-plus/AN/pr61191.c
+++ gcc/testsuite/c-c++-common/cilk-plus/AN/pr61191.c
@@ -4,7 +4,7 @@
 
 double f(double * A, double * B)
 {
-  return __sec_reduce_add((B[0:500])(;
-/* { dg-error "expected expression before ';' token" "" {target *-*-*} 7 } */
-/* { dg-error "called object" "" {target *-*-*} 7 } */
-} /* { dg-error "expected" } */
+  return __sec_reduce_add((B[0:500])(; /* { dg-error "called object" "" { target c } } */
+/* { dg-error "expected expression before ';' token" "" { target c } 7 } */
+/* { dg-error "expected primary-expression before ';' token" "" { target c++ } 7 } */
+} /* { dg-error "expected" "" { target c } } */
