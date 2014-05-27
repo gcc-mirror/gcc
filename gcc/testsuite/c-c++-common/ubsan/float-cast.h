3a05a70c9631864b0c87824cf983ec0665eea501
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-27 11:41:45 +0000

    PR testsuite/61319 	* c-c++-common/ubsan/float-cast.h: Conditionally define LLONG_MAX, 	LLONG_MIN, and ULLONG_MAX.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@210970 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/c-c++-common/ubsan/float-cast.h
+++ gcc/testsuite/c-c++-common/ubsan/float-cast.h
@@ -1,5 +1,16 @@
 /* Various macros for -fsanitize=float-cast-overflow testing.  */
 
+/* E.g. on CentOS 5 these aren't defined in limits.h.  */
+#ifndef LLONG_MAX
+# define LLONG_MAX __LONG_LONG_MAX__
+#endif
+#ifndef LLONG_MIN
+# define LLONG_MIN (-__LONG_LONG_MAX__ - 1LL)
+#endif
+#ifndef ULLONG_MAX
+# define ULLONG_MAX (__LONG_LONG_MAX__ * 2ULL + 1ULL)
+#endif
+
 #define INT128_MAX (__int128) (((unsigned __int128) 1 << ((__SIZEOF_INT128__ * __CHAR_BIT__) - 1)) - 1)
 #define INT128_MIN (-INT128_MAX - 1)
 #define UINT128_MAX ((2 * (unsigned __int128) INT128_MAX) + 1)
