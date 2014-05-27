aceaaf255fc8715b1f91063da5f43d6706d20d53
Georg-Johann Lay <avr@gjlay.de>
2014-05-27 09:05:12 +0000

    backport: re PR libgcc/61152 (Missing GCC Runtime Library Exception in some files that are included in libgcc)
    gcc/
    	Backport from 2014-05-12 mainline r210322.
    	Backport from 2014-05-27 mainline r210959.
    	PR libgcc/61152
    	* config/arm/arm.h (License): Add GCC Runtime Library Exception.
    	* config/arm/arm-cores.def (License): Same.
    	* config/arm/arm-opts.h (License): Same.
    	* config/arm/aout.h (License): Same.
    	* config/arm/bpabi.h (License): Same.
    	* config/arm/elf.h (License): Same.
    	* config/arm/linux-elf.h (License): Same.
    	* config/arm/linux-gas.h (License): Same.
    	* config/arm/netbsd-elf.h (License): Same.
    	* config/arm/uclinux-eabi.h (License): Same.
    	* config/arm/uclinux-elf.h (License): Same.
    	* config/arm/vxworks.h (License): Same.
    libgcc/
    	Backport from 2014-05-27 mainline r210322.
    	PR libgcc/61152
    	* config/arm/bpabi-lib.h (License): Add GCC Runtime Library Exception.
    
    From-SVN: r210960

--- gcc/config/arm/elf.h
+++ gcc/config/arm/elf.h
@@ -16,6 +16,10 @@
    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
    License for more details.
 
+   Under Section 7 of GPL version 3, you are granted additional
+   permissions described in the GCC Runtime Library Exception, version
+   3.1, as published by the Free Software Foundation.
+
    You should have received a copy of the GNU General Public License
    along with GCC; see the file COPYING3.  If not see
    <http://www.gnu.org/licenses/>.  */
