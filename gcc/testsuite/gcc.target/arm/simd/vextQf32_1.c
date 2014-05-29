edf73b94602eeba827b25f1b69b3ec7c1c4f5e11
alalaw01 <alalaw01@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-29 17:24:34 +0000

    Add execution tests of ARM EXT intrinsics
    	gcc.target/arm/simd/vextQf32_1.c: New file.
    	gcc.target/arm/simd/vextQp16_1.c: New file.
    	gcc.target/arm/simd/vextQp8_1.c: New file.
    	gcc.target/arm/simd/vextQs16_1.c: New file.
    	gcc.target/arm/simd/vextQs32_1.c: New file.
    	gcc.target/arm/simd/vextQs64_1.c: New file.
    	gcc.target/arm/simd/vextQs8_1.c: New file.
    	gcc.target/arm/simd/vextQu16_1.c: New file.
    	gcc.target/arm/simd/vextQu32_1.c: New file.
    	gcc.target/arm/simd/vextQu64_1.c: New file.
    	gcc.target/arm/simd/vextQu8_1.c: New file.
    	gcc.target/arm/simd/vextQp64_1.c: New file.
    	gcc.target/arm/simd/vextf32_1.c: New file.
    	gcc.target/arm/simd/vextp16_1.c: New file.
    	gcc.target/arm/simd/vextp8_1.c: New file.
    	gcc.target/arm/simd/vexts16_1.c: New file.
    	gcc.target/arm/simd/vexts32_1.c: New file.
    	gcc.target/arm/simd/vexts64_1.c: New file.
    	gcc.target/arm/simd/vexts8_1.c: New file.
    	gcc.target/arm/simd/vextu16_1.c: New file.
    	gcc.target/arm/simd/vextu32_1.c: New file.
    	gcc.target/arm/simd/vextu64_1.c: New file.
    	gcc.target/arm/simd/vextu8_1.c: New file.
    	gcc.target/arm/simd/vextp64_1.c: New file.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@211059 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/simd/vextQf32_1.c
@@ -0,0 +1,12 @@
+/* Test the `vextQf32' ARM Neon intrinsic.  */
+
+/* { dg-do run } */
+/* { dg-require-effective-target arm_neon_ok } */
+/* { dg-options "-save-temps -O3 -fno-inline" } */
+/* { dg-add-options arm_neon } */
+
+#include "arm_neon.h"
+#include "../../aarch64/simd/extq_f32.x"
+
+/* { dg-final { scan-assembler-times "vext\.32\[ \t\]+\[qQ\]\[0-9\]+, \[qQ\]\[0-9\]+, \[qQ\]\[0-9\]+, #\[0-9\]+!?\(?:\[ \t\]+@\[a-zA-Z0-9 \]+\)?\n" 3 } } */
+/* { dg-final { cleanup-saved-temps } } */
