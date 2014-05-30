365884e24d9eea08c1d3737fef87c23d26950c72
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-30 18:36:24 +0000

    * g++.dg/asan/asan_test.C: Add -std=c++11 and 	-DSANITIZER_USE_DEJAGNU_GTEST=1 to dg-options, remove 	-DASAN_USE_DEJAGNU_GTEST=1. 	* g++.dg/asan/asan_mem_test.cc: Updated from upstream 	r209283. 	* g++.dg/asan/asan_oob_test.cc: Likewise. 	* g++.dg/asan/sanitizer_test_utils.h: Likewise. 	* g++.dg/asan/asan_str_test.cc: Likewise. 	* g++.dg/asan/asan_test_utils.h: Likewise. 	* g++.dg/asan/sanitizer_test_config.h: Likewise. 	* g++.dg/asan/asan_test.cc: Likewise.  Allow size 12 	for long double. 	* g++.dg/asan/sanitizer_pthread_wrappers.h: New file. 	Imported from upstream r209283. 	* g++.dg/asan/asan_test_config.h: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@211090 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/asan/asan_test.C
+++ gcc/testsuite/g++.dg/asan/asan_test.C
@@ -2,7 +2,7 @@
 // { dg-skip-if "" { *-*-* } { "*" } { "-O2" } }
 // { dg-skip-if "" { *-*-* } { "-flto" } { "" } }
 // { dg-additional-sources "asan_globals_test-wrapper.cc" }
-// { dg-options "-fsanitize=address -fno-builtin -Wall -Wno-format -Werror -g -DASAN_UAR=0 -DASAN_HAS_EXCEPTIONS=1 -DASAN_HAS_BLACKLIST=0 -DASAN_USE_DEJAGNU_GTEST=1 -lasan -lpthread -ldl" }
+// { dg-options "-std=c++11 -fsanitize=address -fno-builtin -Wall -Wno-format -Werror -g -DASAN_UAR=0 -DASAN_HAS_EXCEPTIONS=1 -DASAN_HAS_BLACKLIST=0 -DSANITIZER_USE_DEJAGNU_GTEST=1 -lasan -lpthread -ldl" }
 // { dg-additional-options "-DASAN_NEEDS_SEGV=1" { target { ! arm*-*-* } } }
 // { dg-additional-options "-DASAN_LOW_MEMORY=1 -DASAN_NEEDS_SEGV=0" { target arm*-*-* } }
 // { dg-additional-options "-DASAN_AVOID_EXPENSIVE_TESTS=1" { target { ! run_expensive_tests } } }
