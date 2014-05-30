99947ba93c995d0d3ad912616a3650193b60c702
alalaw01 <alalaw01@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-30 09:49:49 +0000

    Add execution tests of ARM REV intrinsics.
    	gcc.target/arm/simd/vrev16p8_1.c: New file.
    	gcc.target/arm/simd/vrev16qp8_1.c: New file.
    	gcc.target/arm/simd/vrev16qs8_1.c: New file.
    	gcc.target/arm/simd/vrev16qu8_1.c: New file.
    	gcc.target/arm/simd/vrev16s8_1.c: New file.
    	gcc.target/arm/simd/vrev16u8_1.c: New file.
    	gcc.target/arm/simd/vrev32p16_1.c: New file.
    	gcc.target/arm/simd/vrev32p8_1.c: New file.
    	gcc.target/arm/simd/vrev32qp16_1.c: New file.
    	gcc.target/arm/simd/vrev32qp8_1.c: New file.
    	gcc.target/arm/simd/vrev32qs16_1.c: New file.
    	gcc.target/arm/simd/vrev32qs8_1.c: New file.
    	gcc.target/arm/simd/vrev32qu16_1.c: New file.
    	gcc.target/arm/simd/vrev32qu8_1.c: New file.
    	gcc.target/arm/simd/vrev32s16_1.c: New file.
    	gcc.target/arm/simd/vrev32s8_1.c: New file.
    	gcc.target/arm/simd/vrev32u16_1.c: New file.
    	gcc.target/arm/simd/vrev32u8_1.c: New file.
    	gcc.target/arm/simd/vrev64f32_1.c: New file.
    	gcc.target/arm/simd/vrev64p16_1.c: New file.
    	gcc.target/arm/simd/vrev64p8_1.c: New file.
    	gcc.target/arm/simd/vrev64qf32_1.c: New file.
    	gcc.target/arm/simd/vrev64qp16_1.c: New file.
    	gcc.target/arm/simd/vrev64qp8_1.c: New file.
    	gcc.target/arm/simd/vrev64qs16_1.c: New file.
    	gcc.target/arm/simd/vrev64qs32_1.c: New file.
    	gcc.target/arm/simd/vrev64qs8_1.c: New file.
    	gcc.target/arm/simd/vrev64qu16_1.c: New file.
    	gcc.target/arm/simd/vrev64qu32_1.c: New file.
    	gcc.target/arm/simd/vrev64qu8_1.c: New file.
    	gcc.target/arm/simd/vrev64s16_1.c: New file.
    	gcc.target/arm/simd/vrev64s32_1.c: New file.
    	gcc.target/arm/simd/vrev64s8_1.c: New file.
    	gcc.target/arm/simd/vrev64u16_1.c: New file.
    	gcc.target/arm/simd/vrev64u32_1.c: New file.
    	gcc.target/arm/simd/vrev64u8_1.c: New file.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@211075 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/simd/vrev64u16_1.c
@@ -0,0 +1,12 @@
+/* Test the `vrev64u16' ARM Neon intrinsic.  */
+
+/* { dg-do run } */
+/* { dg-require-effective-target arm_neon_ok } */
+/* { dg-options "-save-temps -fno-inline" } */
+/* { dg-add-options arm_neon } */
+
+#include "arm_neon.h"
+#include "../../aarch64/simd/vrev64u16.x"
+
+/* { dg-final { scan-assembler "vrev64\.16\[ \t\]+\[dD\]\[0-9\]+, \[dD\]\[0-9\]+!?\(\[ \t\]+@\[a-zA-Z0-9 \]+\)?\n" } } */
+/* { dg-final { cleanup-saved-temps } } */
