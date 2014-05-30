3d1eef549eed9f54335411840d580743980afb1e
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-30 18:00:11 +0000

    PR target/60104 	* config/i386/i386.c (x86_output_mi_thunk): Add memory case 	for sibling-tail-calls. 	* config/i386/i386.md (sibcall_insn_operand): Add memory-constrain 	to its use. 	* config/i386/predicates.md (sibcall_memory_operand): New predicate. 	(sibcall_insn_operand): Add check for sibcall_memory_operand.
    	PR target/60104
    	* gcc.target/i386/sibcall-1.c: New test.
    	* gcc.target/i386/sibcall-2.c: New test.
    	* gcc.target/i386/sibcall-3.c: New test.
    	* gcc.target/i386/sibcall-4.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@211089 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/sibcall-4.c
@@ -0,0 +1,15 @@
+/* Testcase for PR target/46219.  */
+/* { dg-do compile { xfail { *-*-* } } */
+/* { dg-require-effective-target ia32 } */
+/* { dg-options "-O2" } */
+
+typedef void (*dispatch_t)(long offset);
+
+dispatch_t dispatch[256];
+
+void male_indirect_jump (long offset)
+{
+  dispatch[offset](offset);
+}
+
+/* { dg-final { scan-assembler-not "jmp[ \t]*.%eax" } } */
