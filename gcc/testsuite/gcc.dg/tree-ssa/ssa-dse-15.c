0ea48022b83f9f960724dab8fd75a4dc9d6e2269
Richard Biener <rguenther@suse.de>
2014-06-04 11:56:34 +0000

    re PR tree-optimization/60098 (DSE fails to DSE errno settings)
    2014-06-04  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/60098
    	* tree-ssa-dse.c (dse_possible_dead_store_p): Walk until
    	we hit a kill.
    	(dse_optimize_stmt): Simplify, now that we found a kill
    	earlier.
    
    	* gcc.dg/tree-ssa/ssa-dse-15.c: New testcase.
    
    From-SVN: r211224

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-dse-15.c
@@ -0,0 +1,24 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-dse1-details" } */
+
+void *foo (int *p)
+{
+  void *q;
+  /* We should be able to DSE this store (p may point to errno).  */
+  *p = 0;
+  q = __builtin_malloc (4);
+  *p = 0;
+  return q;
+}
+
+int j;
+void bar (int *i)
+{
+  /* This store is dead as well.  */
+  j = 1;
+  *i = 0;
+  j = 2;
+}
+
+/* { dg-final { scan-tree-dump-times "Deleted dead store" 2 "dse1" } } */
+/* { dg-final { cleanup-tree-dump "dse1" } } */
