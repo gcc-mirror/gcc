dc077a1b451ca07d71a39d619b63e9bea510b70a
Igor Zamyatin <igor.zamyatin@intel.com>
2014-06-04 10:12:50 +0000

    re PR c/58942 (cilkplus internal compiler error: tree check __sec_reduce_max_ind)
    gcc/c/
    	PR c/58942
    	* c-array-notation.c (fix_builtin_array_notation_fn): Handle the case
    	with a pointer.
    
    gcc/cp/
    	PR c/58942
    	* c-c++-common/cilk-plus/AN/pr58942.c: Check for correct handling of
    	the case with a pointer.
    
    gcc/testsuite/
    	PR c/58942
    	* c-c++-common/cilk-plus/AN/pr58942.c: Check for correct handling of
    	the case with a pointer.
    
    From-SVN: r211221

--- gcc/c/c-array-notation.c
+++ gcc/c/c-array-notation.c
@@ -310,7 +310,9 @@ fix_builtin_array_notation_fn (tree an_builtin_fn, tree *new_var)
       || an_type == BUILT_IN_CILKPLUS_SEC_REDUCE_MIN_IND)
     array_ind_value = build_decl (location, VAR_DECL, NULL_TREE, 
 				  TREE_TYPE (func_parm));
-  array_op0 = (*array_operand)[0];			      
+  array_op0 = (*array_operand)[0];
+  if (TREE_CODE (array_op0) == INDIRECT_REF)
+    array_op0 = TREE_OPERAND (array_op0, 0);
   switch (an_type)
     {
     case BUILT_IN_CILKPLUS_SEC_REDUCE_ADD:
