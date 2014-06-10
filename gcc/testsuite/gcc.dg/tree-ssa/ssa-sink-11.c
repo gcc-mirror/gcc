5625721fbbf403e891fd6988bdf62fa0ad24c6be
Richard Biener <rguenther@suse.de>
2014-06-10 10:29:44 +0000

    re PR tree-optimization/57186 (implement load sinking in loops)
    2014-06-10  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/57186
    	PR tree-optimization/59299
    	* gcc.dg/tree-ssa/ssa-sink-11.c: New testcase.
    	* gcc.dg/tree-ssa/ssa-sink-12.c: Likewise.
    
    From-SVN: r211404

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-sink-11.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+#define SIZE 64
+
+int foo (int v[], int a)
+{
+  int r, i;
+
+  for (i = 0; i < SIZE; i++)
+    r = v[i] + a;
+
+  return r;
+}
+
+/* { dg-final { scan-tree-dump "MEM\\\[.* \\+ 252B\\\]" "optimized"} } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-sink-12.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+#define SIZE 64
+
+int foo (int v1[], int v2[])
+{
+  int r, i, j;
+
+  for (j = 0; j < SIZE; j++)
+    for (i = 0; i < SIZE; i++)
+      r = v1[j] + v2[i];
+
+  return r;
+}
+
+/* { dg-final { scan-tree-dump "MEM\\\[.* \\+ 252B\\\]" "optimized"} } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
