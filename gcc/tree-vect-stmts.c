d53391a8b5cea9bf1debd833cf5e98443c41aa6e
kyukhin <kyukhin@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-06-11 08:37:53 +0000

    gcc/ 	* tree-vect-data-refs.c (vect_grouped_store_supported): New 	check for stores group of length 3. 	(vect_permute_store_chain): New permutations for stores group of 	length 3. 	* tree-vect-stmts.c (vect_model_store_cost): Change cost 	of vec_perm_shuffle for the new permutations.
    gcc/testsuite/
    	PR tree-optimization/52252
    	* gcc.dg/vect/pr52252-st.c: Test on stores group of size 3.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@211439 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-vect-stmts.c
+++ gcc/tree-vect-stmts.c
@@ -975,9 +975,9 @@ vect_model_store_cost (stmt_vec_info stmt_info, int ncopies,
      include the cost of the permutes.  */
   if (!store_lanes_p && group_size > 1)
     {
-      /* Uses a high and low interleave operation for each needed permute.  */
-      
-      int nstmts = ncopies * exact_log2 (group_size) * group_size;
+      /* Uses a high and low interleave or shuffle operations for each
+	 needed permute.  */
+      int nstmts = ncopies * ceil_log2 (group_size) * group_size;
       inside_cost = record_stmt_cost (body_cost_vec, nstmts, vec_perm,
 				      stmt_info, 0, vect_body);
 
