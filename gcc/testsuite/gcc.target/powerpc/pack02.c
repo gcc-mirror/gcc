8241efd129cd9cfa6d474973e22aa073bfe05163
Peter Bergner <bergner@vnet.ibm.com>
2014-06-13 13:42:53 -0500

    re PR testsuite/61415 (PowerPC test gcc.target/powerpc/tfmode_off.c fails if -mlong-double-64)
    gcc/
    	PR target/61415
    	* config/rs6000/rs6000-builtin.def (BU_MISC_1): Delete.
    	(BU_MISC_2): Rename to ...
    	(BU_LDBL128_2): ... this.
    	* config/rs6000/rs6000.h (RS6000_BTM_LDBL128): New define.
    	(RS6000_BTM_COMMON): Add RS6000_BTM_LDBL128.
    	* config/rs6000/rs6000.c (rs6000_builtin_mask_calculate): Handle
    	RS6000_BTM_LDBL128.
    	(rs6000_invalid_builtin): Add long double 128-bit builtin support.
    	(rs6000_builtin_mask_names): Add RS6000_BTM_LDBL128.
    	* config/rs6000/rs6000.md (unpacktf_0): Remove define)expand.
    	(unpacktf_1): Likewise.
    	* doc/extend.texi (__builtin_longdouble_dw0): Remove documentation.
    	(__builtin_longdouble_dw1): Likewise.
    	* doc/sourcebuild.texi (longdouble128): Document.
    
    gcc/testsuite/
    	PR target/61415
    	* lib/target-supports.exp (check_effective_target_longdouble128): New.
    	* gcc.target/powerpc/pack02.c: Use it.
    	* gcc.target/powerpc/tfmode_off.c: Likewise.
    
    From-SVN: r211653

--- gcc/testsuite/gcc.target/powerpc/pack02.c
+++ gcc/testsuite/gcc.target/powerpc/pack02.c
@@ -2,6 +2,7 @@
 /* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
 /* { dg-skip-if "" { powerpc*-*-*spe* } { "*" } { "" } } */
 /* { dg-require-effective-target powerpc_fprs } */
+/* { dg-require-effective-target longdouble128 } */
 /* { dg-options "-O2 -mhard-float" } */
 
 #include <stddef.h>
