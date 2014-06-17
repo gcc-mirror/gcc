debdc2a66f3ab8b9632ee2c99c69d260fc621667
Siva Chandra Reddy <sivachandra@google.com>
2014-06-17 23:10:16 +0000

    re PR debug/57519 (DW_TAG_imported_declaration put in wrong class (base class instead of derived class))
            PR debug/57519
            * class.c (handle_using_decl): Pass the correct scope to
            cp_emit_debug_info_for_using.
    
    From-SVN: r211755

---
+++ gcc/testsuite/g++.dg/debug/dwarf2/imported-decl-2.C
@@ -0,0 +1,32 @@
+// { dg-do compile }
+// { dg-options "-gdwarf-2 -dA -O0 -fno-merge-debug-strings" }
+
+class AAAA
+{
+ public:
+  int method (void);
+  int a;
+};
+
+int
+AAAA::method (void)
+{
+  return a;
+}
+
+class BBBB : public AAAA
+{
+ public:
+  using AAAA::method;
+
+  int method (int b);
+};
+
+int
+BBBB::method (int b)
+{
+  return a + b;
+}
+
+// { dg-final { scan-assembler-not "ascii \"BBBB\\\\0\".*ascii \"AAAA\\\\0\".*DW_TAG_imported_declaration" } }
+// { dg-final { scan-assembler-times "ascii \"AAAA\\\\0\".*ascii \"BBBB\\\\0\".*DIE .0x\[0-9a-f\]*. DW_TAG_imported_declaration" 1 } }
