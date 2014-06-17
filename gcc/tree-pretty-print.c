90d0d8d3dbf02ec3efa933645033789231dbece7
Xinliang David Li <davidxl@google.com>
2014-06-17 20:39:55 +0000

    Fix dot dump bug
    From-SVN: r211746

--- gcc/tree-pretty-print.c
+++ gcc/tree-pretty-print.c
@@ -3465,6 +3465,7 @@ dump_function_header (FILE *dump_file, tree fdecl, int flags)
     fprintf (dump_file, ", decl_uid=%d", DECL_UID (fdecl));
   if (node)
     {
+      fprintf (dump_file, ", cgraph_uid=%d", node->uid);
       fprintf (dump_file, ", symbol_order=%d)%s\n\n", node->order,
                node->frequency == NODE_FREQUENCY_HOT
                ? " (hot)"
