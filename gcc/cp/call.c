27942fe90c8b0c5866390177e3e9c3e1480cc152
Jason Merrill <jason@redhat.com>
2014-06-19 05:34:57 -0400

    re PR c++/59296 ([c++11] ref-qualified member function is ambiguous)
    	PR c++/59296
    	* call.c (add_function_candidate): Set LOOKUP_NO_RVAL_BIND
    	|LOOKUP_NO_TEMP_BIND for ref-qualifier handling.
    
    From-SVN: r211820

--- gcc/cp/call.c
+++ gcc/cp/call.c
@@ -1994,6 +1994,9 @@ add_function_candidate (struct z_candidate **candidates,
 		     object parameter has reference type.  */
 		  bool rv = FUNCTION_RVALUE_QUALIFIED (TREE_TYPE (fn));
 		  parmtype = cp_build_reference_type (parmtype, rv);
+		  /* Don't bind an rvalue to a const lvalue ref-qualifier.  */
+		  if (!rv)
+		    lflags |= LOOKUP_NO_RVAL_BIND|LOOKUP_NO_TEMP_BIND;
 		}
 	      else
 		{
