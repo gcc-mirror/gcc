1d805a56b6a5c2252baccd257e00a719a22ad143
Vidya Praveen <vidyapraveen@arm.com>
2014-06-26 11:35:47 +0100

    inline-22.c: Add bind_pic_locally.
    	gcc/testsuite/
    	* gcc.dg/inline-22.c: Add bind_pic_locally.
    	* gcc.dg/inline_4.c: Ditto.
    	* gcc.dg/fail_always_inline.c: Ditto.
    	* g++.dg/ipa/devirt-25.C: Ditto.
    
    From-SVN: r212024

--- gcc/testsuite/gcc.dg/fail_always_inline.c
+++ gcc/testsuite/gcc.dg/fail_always_inline.c
@@ -1,4 +1,5 @@
 /* { dg-do compile } */
+/* { dg-add-options bind_pic_locally } */
 
 extern __attribute__ ((always_inline)) void
  bar() { } /* { dg-warning "function might not be inlinable" } */
--- gcc/testsuite/gcc.dg/inline-22.c
+++ gcc/testsuite/gcc.dg/inline-22.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-funit-at-a-time -Wno-attributes" } */
+/* { dg-add-options bind_pic_locally } */
 /* Verify we can inline without a complete prototype and with promoted
    arguments.  See also PR32492.  */
 __attribute__((always_inline)) void f1() {}
