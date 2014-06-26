680a5a7c025e3a124a81266c173db490948ba3bc
Martin Jambor <mjambor@suse.cz>
2014-06-26 16:54:59 +0200

    2014-06-26  Martin Jambor  <mjambor@suse.cz>
    	* params.def (PARAM_ALLOW_LOAD_DATA_RACES)
    	(PARAM_ALLOW_PACKED_LOAD_DATA_RACES)
    	(PARAM_ALLOW_PACKED_STORE_DATA_RACES): Removed.
    	(PARAM_ALLOW_STORE_DATA_RACES): Set default to zero.
    	* opts.c (default_options_optimization): Set
    	PARAM_ALLOW_STORE_DATA_RACES to one at -Ofast.
    	* doc/invoke.texi (allow-load-data-races)
    	(allow-packed-load-data-races, allow-packed-store-data-races):
    	Removed.
    	(allow-store-data-races): Document the new default.
    
    testsuite/
    	* g++.dg/simulate-thread/bitfields-2.C: Remove allow-load-data-races
    	parameter.
    	* g++.dg/simulate-thread/bitfields.C: Likewise.
    	* gcc.dg/simulate-thread/strict-align-global.c: Remove
    	allow-packed-store-data-races parameter.
    	* gcc.dg/simulate-thread/subfields.c: Likewise.
    	* gcc.dg/tree-ssa/20050314-1.c: Set parameter allow-store-data-races
    	to one.
    
    From-SVN: r212034

--- gcc/testsuite/g++.dg/simulate-thread/bitfields-2.C
+++ gcc/testsuite/g++.dg/simulate-thread/bitfields-2.C
@@ -1,5 +1,5 @@
 /* { dg-do link } */
-/* { dg-options "--param allow-load-data-races=0 --param allow-store-data-races=0" } */
+/* { dg-options "--param allow-store-data-races=0" } */
 /* { dg-final { simulate-thread } } */
 
 /* Test that setting <var.a> does not touch either <var.b> or <var.c>.
