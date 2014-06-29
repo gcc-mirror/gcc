b82fe35410080429da8544b4c8b62e7038883f2c
Andrew Pinski <apinski@cavium.com>
2014-06-29 04:46:07 +0000

    lto.c (lto_read_decls): Fix comment in comment.
    2014-06-28  Andrew Pinski  <apinski@cavium.com>
    
    	* lto.c (lto_read_decls): Fix comment in comment.
    
    From-SVN: r212119

--- gcc/lto/lto.c
+++ gcc/lto/lto.c
@@ -1970,7 +1970,7 @@ lto_read_decls (struct lto_file_decl_data *decl_data, const void *data,
 			    len, scc_entry_len, scc_hash))
 	    continue;
 
-	  /* /* Do remaining fixup tasks for prevailing nodes.  */
+	  /* Do remaining fixup tasks for prevailing nodes.  */
 	  bool seen_type = false;
 	  for (unsigned i = 0; i < len; ++i)
 	    {
