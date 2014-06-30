e234921f5107f0e0bbbb5763bbc591f55309b765
zqchen <zqchen@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-06-30 03:36:47 +0000

    ChangeLog: 2014-06-30  Zhenqiang Chen  <zhenqiang.chen@linaro.org>
    	* loop-invariant.c (get_inv_cost): Handle register class.
    	(gain_for_invariant): Check the register pressure of the inv
    	and its overlapped register class, other than all.
    
    testsuite/ChangeLog:
    2014-06-30  Zhenqiang Chen  <zhenqiang.chen@linaro.org>
    
    	* ira-loop-pressure.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@212135 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/ira-loop-pressure.c
@@ -0,0 +1,32 @@
+/* Loop2_invariants pass should distinguish register pressures of different
+   register classes.  In this case, register pressue of INT is high.  But
+   we can still move the FP invariant out of the loop.  */
+
+/* { dg-do compile { target { x86_64-*-* && lp64 } } } */
+/* { dg-options "-O2 -fira-loop-pressure -fdump-rtl-loop2_invariant " } */
+
+float tt;
+extern void foo2 (int *, int *, int *, int *, int *, int *);
+extern int foo3 (int, int, int, int, int, int);
+int foo (int a, int b, int c, int d)
+{
+   int i = a;
+   int t1, t2, t3, t4, t5, t6;
+   t1 = t2 = t3 = t4 = t5 = t6 = 0;
+
+   for (; i > 0; i += c)
+    {
+      tt += 123456.0;
+      if (d > t1 + t2)
+	tt = 3.0;
+      foo2 (&t1, &t2, &t3, &t4, &t5, &t6);
+      t1 += t3 + t4 + a + b;
+      t2 -= t5 - t6 - c - d;
+    }
+
+ return foo3 (t1, t2, t3, t4, t5, t6);
+}
+
+/* { dg-final { scan-rtl-dump "Decided to move invariant" "loop2_invariant"  } } */
+/* { dg-final { cleanup-rtl-dump "loop2_invariant" } } */
+
