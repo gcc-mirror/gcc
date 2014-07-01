39f9091275d3f30ab02adbccb8ad2a3d36560803
Kyrylo Tkachov <kyrylo.tkachov@arm.com>
2014-07-01 12:32:35 +0000

    [AArch64] Implement some vca*_f[32,64] intrinsics.
    	* config/aarch64/arm_neon.h (vcage_f64): New intrinsic.
    	(vcagt_f64): Likewise.
    	(vcale_f64): Likewise.
    	(vcaled_f64): Likewise.
    	(vcales_f32): Likewise.
    	(vcalt_f64): Likewise.
    	(vcaltd_f64): Likewise.
    	(vcalts_f32): Likewise.
    
    	* gcc.target/aarch64/simd/vcage_f64.c: New test.
    	* gcc.target/aarch64/simd/vcagt_f64.c: Likewise.
    	* gcc.target/aarch64/simd/vcale_f64.c: Likewise.
    	* gcc.target/aarch64/simd/vcaled_f64.c: Likewise.
    	* gcc.target/aarch64/simd/vcales_f32.c: Likewise.
    	* gcc.target/aarch64/simd/vcalt_f64.c: Likewise.
    	* gcc.target/aarch64/simd/vcaltd_f64.c: Likewise.
    	* gcc.target/aarch64/simd/vcalts_f32.c: Likewise.
    
    From-SVN: r212196

---
+++ gcc/testsuite/gcc.target/aarch64/simd/vcales_f32.c
@@ -0,0 +1,36 @@
+/* Test the vcales_f32 AArch64 SIMD intrinsic.  */
+
+/* { dg-do run } */
+/* { dg-options "-save-temps -O3" } */
+
+#include "arm_neon.h"
+
+#define SIZE 6
+
+extern void abort (void);
+
+volatile float32_t in[SIZE] = { -10.4, -3.14, 0.0, 1.5, 5.3, 532.3 };
+
+int
+main (void)
+{
+  uint32_t expected;
+  uint32_t actual;
+
+  int i, j;
+
+  for (i = 0; i < SIZE; ++i)
+   for (j = 0; j < SIZE; ++j)
+     {
+        expected = __builtin_fabs (in[i]) <= __builtin_fabs (in[j]) ? -1 : 0;
+        actual = vcales_f32 (in[i], in[j]);
+
+        if (actual != expected)
+          abort ();
+     }
+
+  return 0;
+}
+
+/* { dg-final { scan-assembler "facge\[ \t\]+\[sS\]\[0-9\]+, ?\[sS\]\[0-9\]+, ?\[sS\]\[0-9\]+\n" } } */
+/* { dg-final { cleanup-saved-temps } } */
