f69fbf74ba65a70a887d353107986e512f0e7c4c
zqchen <zqchen@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-07-03 05:24:12 +0000

    ChangeLog: 2014-07-03  Zhenqiang Chen  <zhenqiang.chen@linaro.org>
    	* loop-invariant.c (struct invariant): Add a new member: eqno;
    	(find_identical_invariants): Update eqno;
    	(create_new_invariant): Init eqno;
    	(get_inv_cost): Compute comp_cost with eqno;
    
    
    testsuite/ChangeLog:
    2014-07-03  Zhenqiang Chen  <zhenqiang.chen@linaro.org>
    
    	* gcc.target/arm/identical-invariants.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@212256 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/identical-invariants.c
@@ -0,0 +1,29 @@
+/* { dg-do compile { target { arm_thumb2_ok } } } */
+/* { dg-options "-O2 -fdump-rtl-loop2_invariant " } */
+
+int t1, t2, t3, t4, t5, t6, t7, t8, t9, t10;
+extern void foo2 (int *, int *, int *, int *, int *, int *);
+extern int foo3 (int, int, int, int, int, int);
+int foo (int a, int b, int c, int d)
+{
+   int i = a;
+
+   for (; i > 0; i += b)
+    {
+      if (a > 0x1234567)
+	foo2 (&t1, &t2, &t3, &t4, &t5, &t6);
+      foo2 (&t1, &t2, &t3, &t4, &t5, &t6);
+      if (b > 0x1234567)
+	foo2 (&t7, &t2, &t8, &t4, &t5, &t6);
+      foo2 (&t1, &t2, &t3, &t4, &t5, &t6);
+      if (c > 0x1234567)
+	foo2 (&t1, &t9, &t10, &t4, &t5, &t6);
+      t2 = t5 - d;
+    }
+
+ return foo3 (t1, t2, t3, t4, t5, t6);
+}
+
+/* { dg-final { scan-rtl-dump "Decided to move invariant 0" "loop2_invariant" } } */
+/* { dg-final { cleanup-rtl-dump "loop2_invariant" } } */
+
