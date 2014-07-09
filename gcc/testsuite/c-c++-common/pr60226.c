677f36a639850b44bf9645f75aacc5990e9aa1cb
Mike Stump <mrs@gcc.gnu.org>
2014-07-09 01:02:31 +0000

    Fix pr60226.c on darwin.
    From-SVN: r212381

--- gcc/testsuite/c-c++-common/pr60226.c
+++ gcc/testsuite/c-c++-common/pr60226.c
@@ -4,7 +4,7 @@
 
 typedef int __attribute__ ((aligned (1 << 28))) int28;
 int28 foo[4] = {}; /* { dg-error "alignment of array elements is greater than element size" } */
-typedef int __attribute__ ((aligned (1 << 29))) int29; /* { dg-error "requested alignment is too large" } */
+typedef int __attribute__ ((aligned (1 << 29))) int29; /* { dg-error "requested alignment is too large|maximum object file alignment" } */
 
 void
 f (void)
