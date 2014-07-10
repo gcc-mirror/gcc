b4ccfa5f713dc5ecfd05b2429084bc463c857b52
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-07-10 14:02:26 +0000

    Add addressing=absolute and isa_rev=0 to mips fuse-caller-save tests
    2014-07-10  Tom de Vries  <tom@codesourcery.com>
    
    	* gcc.target/mips/fuse-caller-save.c: Add addressing=absolute to
    	dg-options.
    	* gcc.target/mips/fuse-caller-save-micromips.c: Same.
    	* gcc.target/mips/fuse-caller-save-mips16.c: Same.  Add isa_rev=0 to
    	dg-options.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@212432 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/mips/fuse-caller-save-mips16.c
+++ gcc/testsuite/gcc.target/mips/fuse-caller-save-mips16.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-fuse-caller-save (-mips16)" } */
+/* { dg-options "-fuse-caller-save (-mips16) addressing=absolute isa_rev=0" } */
 /* At -O0 and -O1, the register allocator behaves more conservatively, and
    the fuse-caller-save optimization doesnt' trigger.  */
 /* { dg-skip-if "" { *-*-* }  { "-O0" "-O1" } } */
