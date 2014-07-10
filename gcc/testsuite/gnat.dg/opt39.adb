d764963b54885cdf3604e54717b1a137492d7415
Eric Botcazou <ebotcazou@adacore.com>
2014-07-10 10:38:30 +0000

    re PR middle-end/53590 (compiler fails to generate SIMD instruction for FP division)
    	PR middle-end/53590
    	* function.c (allocate_struct_function): Revert r188667 change.
    
    	* gimple-low.c (lower_builtin_setjmp): Use properly-typed constant.
    
    From-SVN: r212424

---
+++ gcc/testsuite/gnat.dg/opt39.adb
@@ -0,0 +1,31 @@
+-- { dg-do compile }
+-- { dg-options "-O2 -fno-inline -fdump-tree-optimized" }
+
+procedure Opt39 (I : Integer) is
+
+  type Rec is record
+    I1 : Integer;
+    I2 : Integer;
+    I3 : Integer;
+    I4 : Integer;
+    I5 : Integer;
+  end record;
+
+  procedure Set (A : access Rec; I : Integer) is
+    Tmp : Rec := A.all;
+  begin
+    Tmp.I1 := I;
+    A.all := Tmp;
+  end;
+
+  R : aliased Rec;
+
+begin
+  Set (R'Access, I);
+  if R.I1 /= I then
+    raise Program_Error;
+  end if;
+end;
+
+-- { dg-final { scan-tree-dump-times "MEM" 1 "optimized" } }
+-- { dg-final { cleanup-tree-dump "optimized" } }
