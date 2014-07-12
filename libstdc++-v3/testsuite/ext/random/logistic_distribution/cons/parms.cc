8daac7749f44e79c918162dd27be7981a72c1f87
Edward Smith-Rowland <3dw4rd@verizon.net>
2014-07-12 01:32:20 +0000

    Add the logistic_distribution as an extension.
    2014-07-11  Edward Smith-Rowland  <3dw4rd@verizon.net>
    
    	Add the logistic_distribution as an extension.
    	* include/ext/random: Add the logistic_distribution.
    	* include/ext/random.tcc: Add the logistic_distribution.
    	* testsuite/ext/random/logistic_distribution/cons/parms.cc: New.
    	* testsuite/ext/random/logistic_distribution/cons/default.cc: New.
    	* testsuite/ext/random/logistic_distribution/requirements/typedefs.cc:
    	New.
    	* testsuite/ext/random/logistic_distribution/operators/inequal.cc: New.
    	* testsuite/ext/random/logistic_distribution/operators/equal.cc: New.
    	* testsuite/ext/random/logistic_distribution/operators/serialize.cc:
    	New.
    
    From-SVN: r212476

---
+++ libstdc++-v3/testsuite/ext/random/logistic_distribution/cons/parms.cc
@@ -0,0 +1,48 @@
+// { dg-options "-std=c++11" }
+// { dg-require-cstdint "" }
+//
+// 2014-07-11  Edward M. Smith-Rowland  <3dw4rd@verizon.net>
+//
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 26.4.8.3.* Class template logistic_distribution [rand.dist.ext.logistic]
+// 26.4.2.4 Concept RandomNumberDistribution [rand.concept.dist]
+
+#include <ext/random>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  bool test [[gnu::unused]] = true;
+
+  __gnu_cxx::logistic_distribution<> u(1.5, 3.0);
+  VERIFY( u.a() == 1.5 );
+  VERIFY( u.b() == 3.0 );
+
+  typedef __gnu_cxx::logistic_distribution<>::result_type result_type;
+  VERIFY( u.min() == -std::numeric_limits<result_type>::max() );
+  VERIFY( u.max() == std::numeric_limits<result_type>::max() );
+}
+
+int
+main()
+{
+  test01();
+  return 0;
+}
