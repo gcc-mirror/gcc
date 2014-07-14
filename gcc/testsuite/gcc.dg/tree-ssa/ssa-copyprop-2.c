236aff7251d50001f8526b04e5c39004490ec3cd
Richard Biener <rguenther@suse.de>
2014-07-14 13:52:38 +0000

    re PR c/61779 (gcc -Og fails with impossible constraint on legal C code)
    2014-07-14  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/61779
    	* tree-ssa-copy.c (copy_prop_visit_cond_stmt): Always try
    	simplifying a condition.
    
    	* gcc.dg/tree-ssa/ssa-copyprop-2.c: New testcase.
    
    From-SVN: r212521

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-copyprop-2.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-Og -fdump-tree-optimized" } */
+
+extern long long __sdt_unsp;
+void
+f(void)
+{
+  for (;;)
+    __asm__ ("%0" :: "i" (((!__extension__ (__builtin_constant_p ((((unsigned long long) (__typeof (__builtin_choose_expr (((__builtin_classify_type (0) + 3) & -4) == 4, (0), 0U))) __sdt_unsp) ) == 0) )) ? 1 : -1) ));
+}
+
+/* { dg-final { scan-tree-dump-not "PHI" "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
