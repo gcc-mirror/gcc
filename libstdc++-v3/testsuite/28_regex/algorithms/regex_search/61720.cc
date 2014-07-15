4228f9328c9a7bd0cb78e167cecb5a3fb774fb9c
Tim Shen <timshen@google.com>
2014-07-15 04:28:51 +0000

    re PR libstdc++/61720 (std::regex_search matches incorrectly)
    	PR libstdc++/61720
    	* include/bits/regex_executor.tcc (_Executor<>::_M_main_dispatch):
    	Clear match queue for next use.
    	* testsuite/28_regex/algorithms/regex_search/61720.cc: New file.
    
    From-SVN: r212539

---
+++ libstdc++-v3/testsuite/28_regex/algorithms/regex_search/61720.cc
@@ -0,0 +1,48 @@
+// { dg-options "-std=gnu++11" }
+
+//
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <regex>
+#include <testsuite_hooks.h>
+#include <testsuite_regex.h>
+
+using namespace __gnu_test;
+using namespace std;
+
+// libstdc++/61720
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  string s = R"("test\")";
+  VERIFY(!regex_search_debug(s, regex(R"("([^"]|\\")*[^\\]")")));
+  VERIFY(!regex_match_debug(s, regex(R"("([^"]|\\")*[^\\]")")));
+  VERIFY(!regex_search_debug(s, regex(R"("([^"]|\\")*[^\\]")",
+					 regex_constants::extended)));
+  VERIFY(!regex_match_debug(s, regex(R"("([^"]|\\")*[^\\]")",
+					regex_constants::extended)));
+}
+
+int
+main()
+{
+  test01();
+  return 0;
+}
