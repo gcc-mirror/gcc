8f3f392caa32f21ad2fa9aa95798a02b0e46db4f
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-07-15 05:36:35 +0000

    2014-06-15  Tobias Burnus  <burnus@net-b.de>
    gcc/fortran/
            * symbol.c (check_conflict): Add codimension conflict with
            pointer; fix cray-pointee check.
    
    gcc/testsuite/
            * gfortran.dg/coarray_34.f90: New.
            * gfortran.dg/coarray_6.f90: Update test case.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@212540 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/coarray_6.f90
+++ gcc/testsuite/gfortran.dg/coarray_6.f90
@@ -32,7 +32,7 @@ end module m
 
 subroutine bar()
   integer, parameter :: a[*] = 4 ! { dg-error "PARAMETER attribute conflicts with CODIMENSION" }
-  integer, pointer :: b[:] ! { dg-error "is not ALLOCATABLE, SAVE nor a dummy" }
+  integer :: b[*] ! { dg-error "is not ALLOCATABLE, SAVE nor a dummy" }
 end subroutine bar
 
 subroutine vol()
