9025d8f05b74e0991d3be842fd7ee3d7cb32b2d2
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-07-19 19:30:11 +0000

    * toplev.c (output_stack_usage): Adjust the location of the warning.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@212850 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/stack-usage-2.c
+++ gcc/testsuite/gcc.dg/stack-usage-2.c
@@ -1,33 +1,32 @@
 /* { dg-do compile } */
 /* { dg-options "-Wstack-usage=512" } */
 
-int foo1 (void)
+int foo1 (void)  /* { dg-bogus "stack usage" } */
 {
   char arr[16];
   arr[0] = 1;
   return 0;
-} /* { dg-bogus "stack usage" } */
+}
 
-int foo2 (void)
+int foo2 (void)  /* { dg-warning "stack usage is \[0-9\]* bytes" } */
 {
   char arr[1024];
   arr[0] = 1;
   return 0;
-} /* { dg-warning "stack usage is \[0-9\]* bytes" } */
+}
 
-int foo3 (void)
+int foo3 (void) /* { dg-warning "stack usage might be \[0-9\]* bytes" } */
 {
   char arr[1024] __attribute__((aligned (512)));
   arr[0] = 1;
   /* Force dynamic realignment of argument pointer.  */
   __builtin_apply ((void (*)()) foo2, 0, 0);
   return 0;
+}
 
-} /* { dg-warning "stack usage might be \[0-9\]* bytes" } */
-
-int foo4 (int n)
+int foo4 (int n) /* { dg-warning "stack usage might be unbounded" } */
 {
   char arr[n];
   arr[0] = 1;
   return 0;
-} /* { dg-warning "stack usage might be unbounded" } */
+}
