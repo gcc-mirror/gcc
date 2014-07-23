8ebb113c9a1838039a8971255e73cd83bcb1d5e1
ktkachov <ktkachov@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-07-23 09:24:58 +0000

    [ARM] Enable arm target in ira-shrinkwrap-prep* testcases.
    [gcc/]
    
    	* config/arm/arm.c (arm_get_frame_offsets): If both r3 and other
    	callee-saved registers are available for padding purpose
    	and r3 is not mandatory, then prefer use those callee-saved
    	instead of r3.
    
    [gcc/testsuite]
    
    	* gcc.dg/ira-shrinkwrap-prep-1.c (target): Add arm_nothumb.
    	* gcc.dg/ira-shrinkwrap-prep-2.c (target): Likewise.
    	* gcc.dg/pr10474.c (target): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@212927 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr10474.c
+++ gcc/testsuite/gcc.dg/pr10474.c
@@ -1,4 +1,4 @@
-/* { dg-do compile { target { { x86_64-*-* && lp64 } || { powerpc*-*-* && lp64 } } } } */
+/* { dg-do compile { target { { x86_64-*-* && lp64 } || { { powerpc*-*-* && lp64 } || arm_nothumb } } } } */
 /* { dg-options "-O3 -fdump-rtl-pro_and_epilogue"  } */
 
 void f(int *i)
