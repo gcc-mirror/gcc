6260d7fcc927d3372c91b483b79b1588870bf4a3
Alan Modra <amodra@gmail.com>
2014-07-27 22:49:21 +0930

    re PR libobjc/61920 (libobjc has undefined symbols on powerpc*-linux-gnu)
    2014-07-27  Alan Modra  <amodra@gmail.com>
                Matthias Klose  <doko@ubuntu.com>
    
            PR libobjc/61920
    
            * encoding.c: Define rs6000_special_adjust_field_align_p.
    
    Co-Authored-By: Matthias Klose <doko@ubuntu.com>
    
    From-SVN: r213100

--- libobjc/encoding.c
+++ libobjc/encoding.c
@@ -192,6 +192,7 @@ _darwin_rs6000_special_round_type_align (const char *struc, int comp, int spec)
    ? MAX (MAX (COMPUTED, SPECIFIED), 64)				\
    : MAX (COMPUTED, SPECIFIED));})
 
+#define rs6000_special_adjust_field_align_p(FIELD, COMPUTED) 0
 
 /* Skip a variable name, enclosed in quotes (").  */
 static inline
