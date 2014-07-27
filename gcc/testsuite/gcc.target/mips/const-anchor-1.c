f1fa08e32787c1e275ff0bad5036f32c7725dde1
Richard Sandiford <rdsandiford@googlemail.com>
2014-07-27 09:50:56 +0000

    re PR rtl-optimization/61926 (const-anchor optimisation is sensitive to ordering)
    gcc/testsuite/
    	PR rtl-optimization/61926
    	* gcc.target/mips/const-anchor-1.c, gcc.target/mips/const-anchor-2.c:
    	Reverse argument order.
    	* gcc.target/mips/const-anchor-3.c, gcc.target/mips/const-anchor-4.c:
    	New XFAILed tests that match the original order.
    
    From-SVN: r213093

--- gcc/testsuite/gcc.target/mips/const-anchor-1.c
+++ gcc/testsuite/gcc.target/mips/const-anchor-1.c
@@ -2,9 +2,9 @@
    (0x1234000) used to build another constant.  */
 /* { dg-skip-if "code quality test" { *-*-* } { "-O0" } { "" } } */
 /* { dg-final { scan-assembler-not "0x12330000|305332224" } } */
-/* { dg-final { scan-assembler "\td?addiu\t\\\$5,\\\$\[0-9\]*,-1" } } */
+/* { dg-final { scan-assembler "\td?addiu\t\\\$4,\\\$\[0-9\]*,-1" } } */
 
 NOMIPS16 void f ()
 {
-  g (0x12340001, 0x1233ffff);
+  g (0x1233ffff, 0x12340001);
 }
