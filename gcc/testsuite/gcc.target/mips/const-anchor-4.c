2e9b5471f5d0c8b06bd8194889f0e50de06f1543
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-07-27 09:50:56 +0000

    gcc/testsuite/ 	PR rtl-optimization/61926 	* gcc.target/mips/const-anchor-1.c, gcc.target/mips/const-anchor-2.c: 	Reverse argument order. 	* gcc.target/mips/const-anchor-3.c, gcc.target/mips/const-anchor-4.c: 	New XFAILed tests that match the original order.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@213093 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/mips/const-anchor-4.c
@@ -0,0 +1,10 @@
+/* Derive a constant (0x30001) from another constant.  */
+/* { dg-skip-if "code quality test" { *-*-* } { "-O0" } { "" } } */
+/* See PR61926 for the XFAILs.  */
+/* { dg-final { scan-assembler-not "0x300000|196608" { xfail *-*-* } } } */
+/* { dg-final { scan-assembler "\td?addiu\t\\\$5,\\\$\[0-9\]*,32763" { xfail *-*-* }  } } */
+
+NOMIPS16 void f ()
+{
+  g (0x28006, 0x30001);
+}
