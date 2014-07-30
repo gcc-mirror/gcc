5b9802c45c2c0cfe93d261518e5ca466197cbf8f
Paolo Carlini <paolo.carlini@oracle.com>
2014-07-30 20:06:29 +0000

    re PR c++/57397 (Off-by-one error in diagnostic when calling variadic function template with too few arguments)
    /cp
    2014-07-30  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/57397
    	* pt.c (unify_arity): Add boolean parameter.
    	(unify_too_few_arguments): Likewise.
    	(type_unification_real): Diagnose correctly insufficient
    	arguments in the presence of trailing variadic parameters;
    	deducing multiple trailing packs as empty is fine.
    
    /testsuite
    2014-07-30  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/57397
    	* g++.dg/cpp0x/vt-57397-1.C: New.
    	* g++.dg/cpp0x/vt-57397-2.C: Likewise.
    
    From-SVN: r213310

---
+++ gcc/testsuite/g++.dg/cpp0x/vt-57397-2.C
@@ -0,0 +1,24 @@
+// PR c++/57397
+// { dg-do compile { target c++11 } }
+
+template<class T1, class... Tn, class... Tm>
+void foo(T1, Tn..., Tm...);
+
+template<class T1, class T2, class... Tn, class... Tm>
+void bar(T1, T2, Tn..., Tm...);
+
+int main()
+{
+  foo();   // { dg-error "no matching" }
+  // { dg-message "candidate expects at least 1 argument, 0 provided" "" { target *-*-* } 12 }
+  foo(1);
+  foo(1, 2);
+  foo(1, 2, 3);
+  bar();   // { dg-error "no matching" }
+  // { dg-message "candidate expects at least 2 arguments, 0 provided" "" { target *-*-* } 17 }
+  bar(1);  // { dg-error "no matching" }
+  // { dg-message "candidate expects at least 2 arguments, 1 provided" "" { target *-*-* } 19 }
+  bar(1, 2);
+  bar(1, 2, 3);
+  bar(1, 2, 3, 4);
+}
