0d66ba6ced1a26807dc6537ac5f7ac4ffac8efec
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-07-30 14:12:37 +0000

    2014-07-30  Bob Duff  <duff@adacore.com>
    	* g-exctra.adb, g-exctra.ads, s-exctra.adb, s-exctra.ads, Makefile.rtl,
    	g-trasym.adb, g-trasym.ads, s-trasym.adb, s-trasym.ads: Move
    	GNAT.Traceback.Symbolic and GNAT.Exception_Traces into the System
    	hierarchy (System.Traceback.Symbolic and System.Exception_Traces), so
    	we can call them from the runtimes. Leave renamings in place under GNAT.
    
    2014-07-30  Yannick Moy  <moy@adacore.com>
    
    	* inline.adb (Check_And_Build_Body_To_Inline): Include code for
    	inlining in GNATprove mode.
    
    2014-07-30  Ed Schonberg  <schonberg@adacore.com>
    
    	* a-cohase.adb, a-cohase.ads (Generic_Keys): Add a
    	Reference_Control_Type to generic package, to keep additional
    	information for Reference_Types that manipulate keys. Add Adjust and
    	Finalize procedures for this type.
    	(Delete_Node): New procedure called when finalizing a
    	Reference_Control_Type, to remove a node whose element has been
    	improperly updated through a Reference.
    	(Insert): Detect tampering.
    	(Reference_Preserving_Key): Build proper Reference_Control_Type,
    	and update Busy and Lock bits to detect tampering.
    
    2014-07-30  Bob Duff  <duff@adacore.com>
    
    	* exp_intr.ads: Minor comment fix.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@213276 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/s-trasym.adb
+++ gcc/ada/s-trasym.adb
@@ -2,9 +2,9 @@
 --                                                                          --
 --                         GNAT RUN-TIME COMPONENTS                         --
 --                                                                          --
---             G N A T . T R A C E B A C K . S Y M B O L I C                --
+--           S Y S T E M . T R A C E B A C K . S Y M B O L I C              --
 --                                                                          --
---                                 S p e c                                  --
+--                                 B o d y                                  --
 --                                                                          --
 --                     Copyright (C) 1999-2014, AdaCore                     --
 --                                                                          --
@@ -29,9 +29,53 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
---  Run-time symbolic traceback support
+--  This is the default implementation for platforms where the full capability
+--  is not supported. It returns tracebacks as lists of LF separated strings of
+--  the form "0x..." corresponding to the addresses.
 
---  See file s-trasym.ads for full documentation of the interface
+with Ada.Exceptions.Traceback; use Ada.Exceptions.Traceback;
+with System.Address_Image;
 
-with System.Traceback.Symbolic;
-package GNAT.Traceback.Symbolic renames System.Traceback.Symbolic;
+package body System.Traceback.Symbolic is
+
+   ------------------------
+   -- Symbolic_Traceback --
+   ------------------------
+
+   function Symbolic_Traceback
+     (Traceback : System.Traceback_Entries.Tracebacks_Array) return String
+   is
+   begin
+      if Traceback'Length = 0 then
+         return "";
+
+      else
+         declare
+            Img : String := System.Address_Image (Traceback (Traceback'First));
+
+            Result : String (1 .. (Img'Length + 3) * Traceback'Length);
+            Last   : Natural := 0;
+
+         begin
+            for J in Traceback'Range loop
+               Img := System.Address_Image (Traceback (J));
+               Result (Last + 1 .. Last + 2) := "0x";
+               Last := Last + 2;
+               Result (Last + 1 .. Last + Img'Length) := Img;
+               Last := Last + Img'Length + 1;
+               Result (Last) := ASCII.LF;
+            end loop;
+
+            return Result (1 .. Last);
+         end;
+      end if;
+   end Symbolic_Traceback;
+
+   function Symbolic_Traceback
+     (E : Ada.Exceptions.Exception_Occurrence) return String
+   is
+   begin
+      return Symbolic_Traceback (Ada.Exceptions.Traceback.Tracebacks (E));
+   end Symbolic_Traceback;
+
+end System.Traceback.Symbolic;
