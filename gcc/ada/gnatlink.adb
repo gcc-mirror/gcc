e95ad7ea78884d4df12f60a8d3592989b895664b
Robert Dewar <dewar@adacore.com>
2014-07-31 09:46:06 +0000

    gnat_ugn.texi: Document new switch -gnato0.
    2014-07-31  Robert Dewar  <dewar@adacore.com>
    
    	* gnat_ugn.texi: Document new switch -gnato0.
    	* sem_ch3.ads: Minor reformatting.
    	* gnatlink.adb (Process_Args): Compile bind file with -gnato0
    	(we do not want overflow checks when incrementing elaboration
    	counters).
    	* einfo.ads: Minor reformatting.
    
    From-SVN: r213328

--- gcc/ada/gnatlink.adb
+++ gcc/ada/gnatlink.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---          Copyright (C) 1996-2013, Free Software Foundation, Inc.         --
+--          Copyright (C) 1996-2014, Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -675,6 +675,12 @@ procedure Gnatlink is
 
       Binder_Options.Increment_Last;
       Binder_Options.Table (Binder_Options.Last) := new String'("-gnatws");
+
+      --  Also suppress overflow checks (needed now that we have them on by
+      --  default, we want checks off when incrementing elaboration counters).
+
+      Binder_Options.Increment_Last;
+      Binder_Options.Table (Binder_Options.Last) := new String'("-gnato0");
 
       --  If we did not get an ali file at all, and we had at least one
       --  linker option, then assume that was the intended ali file after
