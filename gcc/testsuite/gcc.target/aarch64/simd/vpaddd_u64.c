9052a1e3319eb868d107f78be20086883b4d05b5
James Greenhalgh <james.greenhalgh@arm.com>
2014-07-31 16:45:38 +0000

    [AArch64] arm_neon.h - add vpaddd_f64, vpaddd_s64, vpaddd_u64 intrinsics
    gcc/
    
    	* config/aarch64/arm_neon.h (vpadd_<suf><8,16,32,64>): Move to
    	correct alphabetical position.
    	(vpaddd_f64): Rewrite using builtins.
    	(vpaddd_s64): Move to correct alphabetical position.
    	(vpaddd_u64): New.
    
    gcc/testsuite/
    
    	* gcc.target/aarch64/scalar_intrinsics.c (test_vpaddd_f64): New.
    	(test_vpaddd_s64): Likewise.
    	(test_vpaddd_s64): Likewise.
    	* gcc.target/aarch64/simd/vpaddd_f64: New.
    	* gcc.target/aarch64/simd/vpaddd_s64: New.
    	* gcc.target/aarch64/simd/vpaddd_u64: New.
    
    From-SVN: r213382

---
+++ gcc/testsuite/gcc.target/aarch64/simd/vpaddd_u64.c
@@ -0,0 +1,27 @@
+/* Test the vpaddd_u64 AArch64 SIMD intrinsic.  */
+
+/* { dg-do run } */
+/* { dg-options "-save-temps -O3" } */
+
+#include "arm_neon.h"
+
+#define SIZE 6
+
+extern void abort (void);
+
+uint64_t in[SIZE] = { 4ul, 4ul, 2ul, 2ul, 1ul, 1ul };
+
+int
+main (void)
+{
+  int i;
+
+  for (i = 0; i < SIZE / 2; ++i)
+    if (vpaddd_u64 (vld1q_u64 (in + 2 * i)) != 2 * in[2 * i])
+      abort ();
+
+  return 0;
+}
+
+/* { dg-final { scan-assembler "addp\[ \t\]+\[dD\]\[0-9\]+, v\[0-9\].2d+\n" } } */
+/* { dg-final { cleanup-saved-temps } } */
