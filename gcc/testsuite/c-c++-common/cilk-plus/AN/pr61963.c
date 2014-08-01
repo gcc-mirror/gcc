ffebf50f2f1654dea65d934fd9ad27408ea65434
Igor Zamyatin <igor.zamyatin@intel.com>
2014-08-01 17:20:02 +0000

    re PR other/61963 (CilkPlus Array Notation ICE in build_array_notation_ref on malformed function arguments.)
    	PR other/61963
    
    gcc/cp/
    	* parser.c (cp_parser_array_notation): Added check for array_type.
    
    gcc/testsuite/
    	* c-c++-common/cilk-plus/AN/pr61963.c: New test.
    
    From-SVN: r213494

---
+++ gcc/testsuite/c-c++-common/cilk-plus/AN/pr61963.c
@@ -0,0 +1,9 @@
+/* PR other/61963 */
+/* { dg-do compile } */
+/* { dg-options "-fcilkplus" } */
+
+void f (int * int *a) /* { dg-error "expected" } */
+{
+    a[0:64] = 0; /* { dg-error "was not declared" "" { target c++ } 7 } */
+    a[0:64] = 0;
+}
