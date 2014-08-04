99f9094652286d9c79d07aaba619692f75ba2f7b
Jonathan Wakely <jwakely@redhat.com>
2014-08-04 19:50:14 +0100

    backport: re PR libstdc++/61374 (string_view::operator string() is buggy)
    Backported from mainline
    2014-06-01  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/61374
    	* include/experimental/string_view (operator basic_string): Correct
    	order of arguments.
    	(to_string): Replace with member function.
    	Add inline specifiers. Remove unused header. Remove _S_empty_rep and
    	allow _M_str to be null.
    	* testsuite/experimental/string_view/cons/char/1.cc: Adjust to new
    	default constructor semantics.
    	* testsuite/experimental/string_view/cons/wchar_t/1.cc: Likewise.
    	* testsuite/experimental/string_view/operations/copy/char/1.cc: Fix
    	copyright dates. Remove unused header.
    	* testsuite/experimental/string_view/operations/copy/wchar_t/1.cc:
    	Likewise.
    	* testsuite/experimental/string_view/operations/data/char/1.cc:
    	Fix copyright dates. Adjust to new default constructor semantics.
    	* testsuite/experimental/string_view/operations/data/wchar_t/1.cc:
    	Likewise.
    	* testsuite/experimental/string_view/operations/to_string/1.cc: New.
    
    From-SVN: r213601

--- libstdc++-v3/testsuite/experimental/string_view/operations/copy/wchar_t/1.cc
+++ libstdc++-v3/testsuite/experimental/string_view/operations/copy/wchar_t/1.cc
@@ -1,6 +1,6 @@
 // { dg-options "-std=gnu++1y" }
 
-// Copyright (C) 2013 Free Software Foundation, Inc.
+// Copyright (C) 2013-2014 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -20,7 +20,6 @@
 // basic_string_view::copy
 
 #include <experimental/string_view>
-#include <stdexcept>
 #include <testsuite_hooks.h>
 
 bool
