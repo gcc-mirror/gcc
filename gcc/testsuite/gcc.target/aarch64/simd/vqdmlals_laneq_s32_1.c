d2937a2e3f9d5e3e69f7ef71da685f6583ebf924
Kyrylo Tkachov <kyrylo.tkachov@arm.com>
2014-08-05 10:43:41 +0000

    [AArch64] Implement some saturating math NEON intrinsics.
    	* config/aarch64/aarch64-simd.md (aarch64_sqdmulh_laneq<mode>):
    	Use VSDQ_HSI mode iterator.
    	(aarch64_sqrdmulh_laneq<mode>): Likewise.
    	(aarch64_sq<r>dmulh_laneq<mode>_internal): New define_insn.
    	* config/aarch64/aarch64-simd-builtins.def (sqdmulh_laneq):
    	Use BUILTIN_VDQHS macro.
    	(sqrdmulh_laneq): Likewise.
    	* config/aarch64/arm_neon.h (vqdmlalh_laneq_s16): New intrinsic.
    	(vqdmlals_laneq_s32): Likewise.
    	(vqdmlslh_laneq_s16): Likewise.
    	(vqdmlsls_laneq_s32): Likewise.
    	(vqdmulhh_laneq_s16): Likewise.
    	(vqdmulhs_laneq_s32): Likewise.
    	(vqrdmulhh_laneq_s16): Likewise.
    	(vqrdmulhs_laneq_s32): Likewise.
    
    	* gcc.target/aarch64/simd/vqdmlalh_laneq_s16_1.c: New test.
    	* gcc.target/aarch64/simd/vqdmlals_laneq_s32_1.c: Likewise.
    	* gcc.target/aarch64/simd/vqdmlslh_laneq_s16_1.c: Likewise.
    	* gcc.target/aarch64/simd/vqdmlsls_laneq_s32_1.c: Likewise.
    	* gcc.target/aarch64/simd/vqdmulhh_laneq_s16_1.c: Likewise.
    	* gcc.target/aarch64/simd/vqdmulhs_laneq_s32_1.c: Likewise.
    	* gcc.target/aarch64/simd/vqrdmulhh_laneq_s16_1.c: Likewise.
    	* gcc.target/aarch64/simd/vqrdmulhs_laneq_s32_1.c: Likewise.
    
    From-SVN: r213635

---
+++ gcc/testsuite/gcc.target/aarch64/simd/vqdmlals_laneq_s32_1.c
@@ -0,0 +1,35 @@
+/* Test the vqdmlals_laneq_s32 AArch64 SIMD intrinsic.  */
+
+/* { dg-do run } */
+/* { dg-options "-save-temps -O3 -fno-inline" } */
+
+#include "arm_neon.h"
+
+extern void abort (void);
+
+int
+main (void)
+{
+  int64_t arg1;
+  int32_t arg2;
+  int32x4_t arg3;
+  int64_t actual;
+  int64_t expected;
+
+  arg1 = -9223182289494545592LL;
+  arg2 = 32768;
+  arg3 = vcombine_s32 (vcreate_s32 (0xffff7fff8000ffffULL),
+                       vcreate_s32 (0x80000000ffff0000ULL));
+
+  actual = vqdmlals_laneq_s32 (arg1, arg2, arg3, 3);
+  expected = -9223323026982900920LL;
+
+  if (expected != actual)
+    abort ();
+
+  return 0;
+}
+
+
+/* { dg-final { scan-assembler-times "sqdmlal\[ \t\]+\[dD\]\[0-9\]+, ?\[sS\]\[0-9\]+, ?\[vV\]\[0-9\]+\.\[sS\]\\\[3\\\]\n" 1 } } */
+/* { dg-final { cleanup-saved-temps } } */
---
+++ gcc/testsuite/gcc.target/aarch64/simd/vqdmulhh_laneq_s16_1.c
@@ -0,0 +1,33 @@
+/* Test the vqdmulhh_laneq_s16 AArch64 SIMD intrinsic.  */
+
+/* { dg-do run } */
+/* { dg-options "-save-temps -O3 -fno-inline" } */
+
+#include "arm_neon.h"
+
+extern void abort (void);
+
+int
+main (void)
+{
+  int16_t arg1;
+  int16x8_t arg2;
+  int16_t actual;
+  int16_t expected;
+
+  arg1 = 268;
+  arg2 = vcombine_s16 (vcreate_s16 (0xffffffff00000000ULL),
+                       vcreate_s16 (0x0000800018410000ULL));
+
+  actual = vqdmulhh_laneq_s16 (arg1, arg2, 7);
+  expected = 0;
+
+  if (expected != actual)
+    abort ();
+
+  return 0;
+}
+
+
+/* { dg-final { scan-assembler-times "sqdmulh\[ \t\]+\[hH\]\[0-9\]+, ?\[hH\]\[0-9\]+, ?\[vV\]\[0-9\]+\.\[hH\]\\\[7\\\]\n" 1 } } */
+/* { dg-final { cleanup-saved-temps } } */
