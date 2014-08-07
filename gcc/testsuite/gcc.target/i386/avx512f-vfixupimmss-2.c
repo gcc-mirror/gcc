bcc93a18377db427ffcd28fd8ab5f82e047928c6
kyukhin <kyukhin@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-08-07 10:57:39 +0000

    gcc/testsuite 	* gcc.target/i386/avx512f-vfixupimmpd-2.c: Include float.h instead of 	values.h, change MAXDOUBLE for DBL_MAX. 	* gcc.target/i386/avx512f-vfixupimmsd-2.c: Ditto. 	* gcc.target/i386/avx512f-vfixupimmps-2.c: Include float.h instead of 	values.h, change MAXFLOAT for FLT_MAX. 	* gcc.target/i386/avx512f-vfixupimmss-2.c: Ditto. 	* gcc.target/i386/avx512f-vpermi2d-2.c: Do not include values.h. 	* gcc.target/i386/avx512f-vpermi2pd-2.c: Ditto. 	* gcc.target/i386/avx512f-vpermi2ps-2.c: Ditto. 	* gcc.target/i386/avx512f-vpermi2q-2.c: Ditto. 	* gcc.target/i386/avx512f-vpermt2d-2.c: Ditto. 	* gcc.target/i386/avx512f-vpermt2pd-2.c: Ditto. 	* gcc.target/i386/avx512f-vpermt2ps-2.c: Ditto. 	* gcc.target/i386/avx512f-vpermt2q-2.c: Ditto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@213706 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/avx512f-vfixupimmss-2.c
+++ gcc/testsuite/gcc.target/i386/avx512f-vfixupimmss-2.c
@@ -6,7 +6,7 @@
 #include "avx512f-check.h"
 #include "avx512f-helper.h"
 #include <math.h>
-#include <values.h>
+#include <float.h>
 #include "avx512f-mask-type.h"
 
 void
@@ -57,10 +57,10 @@ compute_fixupimmps (float *r, float src, int tbl)
       *r = M_PI_2;
       break;
     case 14:
-      *r = MAXFLOAT;
+      *r = FLT_MAX;
       break;
     case 15:
-      *r = -MAXFLOAT;
+      *r = -FLT_MAX;
       break;
     default:
       abort ();
