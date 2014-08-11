5f732aeb8ebbe04e9912faa497093dd91e8dc898
Richard Biener <rguenther@suse.de>
2014-08-11 10:55:10 +0000

    re PR tree-optimization/62070 (ICE: verify_ssa failed)
    2014-08-11  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/62070
    	* tree-ssa-loop-manip.c (gimple_duplicate_loop_to_header_edge):
    	Remove SSA checking.
    
    	* gcc.dg/pr62070.c: New testcase.
    
    From-SVN: r213810

---
+++ gcc/testsuite/gcc.dg/pr62070.c
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 -fno-tree-vectorize" } */
+
+int in[8][4];
+int out[4];
+
+void
+foo (void)
+{
+  int sum = 1;
+  int i, j, k;
+  for (k = 0; k < 4; k++)
+    {
+      for (j = 0; j < 4; j++)
+	for (i = 0; i < 4; i++)
+	  sum *= in[i + k][j];
+      out[k] = sum;
+    }
+}
