e2e4599a36c2ee11ff907e09806b67f01e6e1c36
Janis Johnson <janisjo@codesourcery.com>
2014-08-11 15:29:59 +0000

    frame-pointer-1.c: Skip if Thumb is not supported.
    	* gcc.target/arm/frame-pointer-1.c: Skip if Thumb is not supported.
    	* gcc.target/arm/pr56184.C: Likewise.
    	* gcc.target/arm/pr59896.c: Likewise.
    	* gcc.target/arm/stack-red-zone.c: Likewise.
    	* gcc.target/arm/thumb-find-work-register.c: Likewise.
    
    From-SVN: r213819

--- gcc/testsuite/gcc.target/arm/stack-red-zone.c
+++ gcc/testsuite/gcc.target/arm/stack-red-zone.c
@@ -1,4 +1,5 @@
 /* No stack red zone.  PR38644.  */
+/* { dg-skip-if "incompatible options" { ! { arm_thumb1_ok || arm_thumb2_ok } } { "*" } { "" } } */
 /* { dg-options "-mthumb -O2" } */
 /* { dg-final { scan-assembler "ldrb\[^\n\]*\\n\[\t \]*add\[\t \]*sp" } } */
 
