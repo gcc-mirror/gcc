29a122fc278e5fa813827948720a3d485dab236f
asutton <asutton@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-08-13 14:16:48 +0000

    2014-08-13  Andrew Sutton  <andrew.n.sutton@gmail.com>
            * pt.c (lookup_template_variable): Make dependent variable templates
            have unknown type.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@213910 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp1y/var-templ6.C
@@ -0,0 +1,12 @@
+// { dg-options "-std=c++1y" }
+
+template<typename T>
+  constexpr bool Class = __is_class(T);
+
+template<typename T>
+  constexpr bool Test = Class<T>;
+
+struct S { };
+
+static_assert(!Test<int>, "");
+static_assert(Test<S>, "");
