8d6ef28b4a38df62aaf562b33086713335577910
kugan <kugan@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-08-18 06:28:35 +0000

    gcc/testsuite 2014-08-18  Kugan Vivekanandarajah  <kuganv@linaro.org>
    	PR tree-optimization/52904
    	* gcc.dg/pr52904.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@214084 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr52904.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-options "-Wstrict-overflow -O2" } */
+extern int foo (int);
+
+int
+wait_reading_process_output (void)
+{
+  int nfds = 0;
+  int channel;
+
+  for (channel = 0; channel < 1024; ++channel)
+    {
+      if (foo (channel))
+	nfds++;
+    }
+
+  if (nfds < 0) /* { dg-bogus "assuming signed overflow does not occur" } */
+    return 1;
+  return 0;
+}
