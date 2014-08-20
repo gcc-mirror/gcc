321bff11a3a1f0e3ecc925304b05f25bb72c9c94
Joost VandeVondele <Joost.VandeVondele@mat.ethz.ch>
2014-08-20 09:08:57 +0200

    options.c (gfc_init_options_struct): assert that the frontend sets flag_errno_math and flag_associative_math.
    gcc/fortran/ChangeLog:
    
    2014-08-20  Joost VandeVondele  <Joost.VandeVondele@mat.ethz.ch>
    
    	* options.c (gfc_init_options_struct): assert that the frontend sets
            flag_errno_math and flag_associative_math.
    
    gcc/testsuite/ChangeLog:
    
    2014-08-20  Joost VandeVondele  <Joost.VandeVondele@mat.ethz.ch>
    
    	* gfortran.dg/errnocheck_1.f90: New test.
    	* gfortran.dg/associative_1.f90: New test.
    
    From-SVN: r214211

---
+++ gcc/testsuite/gfortran.dg/associative_1.f90
@@ -0,0 +1,10 @@
+! { dg-do compile }
+! { dg-options "-O1 -fno-signed-zeros -fno-trapping-math -fdump-tree-optimized" }
+! Fortran defaults to associative by default,
+! with -fno-signed-zeros -fno-trapping-math this should optimize away all additions
+SUBROUTINE S1(a)
+ REAL :: a
+ a=1+a-1
+END SUBROUTINE S1
+! { dg-final { scan-tree-dump-times " \\\+ " 0 "optimized" } }
+! { dg-final { cleanup-tree-dump "optimized" } }
