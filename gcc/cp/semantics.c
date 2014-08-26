c5db0a060bb980784f052d621df616863a0803e6
Jason Merrill <jason@redhat.com>
2014-08-26 16:05:13 -0400

    re PR middle-end/58624 (gcc internal compiler error: Segmentaion fault in insert_to_assembler_name_hash)
    	PR c++/58624
    	* pt.c (tsubst_copy_and_build) [VAR_DECL]: Use TLS wrapper.
    	* semantics.c (finish_id_expression): Don't call TLS wrapper in a
    	template.
    
    From-SVN: r214546

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -3490,6 +3490,7 @@ finish_id_expression (tree id_expression,
       tree wrap;
       if (VAR_P (decl)
 	  && !cp_unevaluated_operand
+	  && !processing_template_decl
 	  && DECL_THREAD_LOCAL_P (decl)
 	  && (wrap = get_tls_wrapper_fn (decl)))
 	{
