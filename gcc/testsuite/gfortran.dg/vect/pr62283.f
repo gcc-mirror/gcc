b1aef01e16b01f3b07a14c5c7cac92d98553517f
Richard Biener <rguenther@suse.de>
2014-08-28 13:13:45 +0000

    re PR fortran/62283 (basic-block vectorization fails)
    2014-08-28  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/62283
    	* tree-vect-data-refs.c (vect_enhance_data_refs_alignment):
    	Do not peel loops for alignment where the vector loop likely
    	doesn't run at least VF times.
    
    	* gfortran.dg/vect/pr62283.f: New testcase.
    	* gcc.dg/tree-ssa/cunroll-5.c: Adjust.
    	* gcc.dg/vect/costmodel/i386/costmodel-vect-31.c: Likewise.
    	* gcc.dg/vect/costmodel/i386/costmodel-vect-33.c: Likewise.
    	* gcc.dg/vect/costmodel/x86_64/costmodel-vect-31.c: Likewise.
    	* gcc.dg/vect/costmodel/x86_64/costmodel-vect-33.c: Likewise.
    	* gcc.dg/vect/vect-33.c: Likewise.
    
    From-SVN: r214678

---
+++ gcc/testsuite/gfortran.dg/vect/pr62283.f
@@ -0,0 +1,17 @@
+C { dg-do compile }
+C { dg-additional-options "-fvect-cost-model=dynamic" }
+      subroutine test2(x,y)
+      real x(4),y(4)
+      beta=3.141593
+      do i=1,4
+        y(i)=y(i)+beta*x(i)
+      end do
+      end
+
+      subroutine test3(x,y)
+      real x(4),y(4)
+      beta=3.141593
+      y=y+beta*x
+      end
+C { dg-final { scan-tree-dump-times "vectorized 1 loops" 2 "vect" { target { vect_hw_misalign } } } }
+C { dg-final { cleanup-tree-dump "vect" } }
