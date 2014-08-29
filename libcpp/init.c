b753b37bd6a43a9d1cc2433f57a091c213380147
Manuel López-Ibáñez <manu@gcc.gnu.org>
2014-08-29 15:28:45 +0000

    macro.c (warn_of_redefinition): Suppress warnings for builtins that lack the NODE_WARN flag...
    libcpp/ChangeLog:
    
    2014-08-29  Manuel López-Ibáñez  <manu@gcc.gnu.org>
    
    	* macro.c (warn_of_redefinition): Suppress warnings for builtins
    	that lack the NODE_WARN flag, unless Wbuiltin-macro-redefined.
    	(_cpp_create_definition): Use Wbuiltin-macro-redefined for
    	builtins that lack the NODE_WARN flag.
    	* directives.c (do_undef): Likewise.
    	* init.c (cpp_init_special_builtins): Do not change flags
    	depending on Wbuiltin-macro-redefined.
    
    
    gcc/c-family/ChangeLog:
    
    2014-08-29  Manuel López-Ibáñez  <manu@gcc.gnu.org>
    
    	* c.opt (Wbuiltin-macro-redefined): Use CPP, Var and Init.
    	* c-opts.c (c_common_handle_option): Do not handle here.
    
    From-SVN: r214730

--- libcpp/init.c
+++ libcpp/init.c
@@ -470,8 +470,7 @@ cpp_init_special_builtins (cpp_reader *pfile)
       cpp_hashnode *hp = cpp_lookup (pfile, b->name, b->len);
       hp->type = NT_MACRO;
       hp->flags |= NODE_BUILTIN;
-      if (b->always_warn_if_redefined
-          || CPP_OPTION (pfile, warn_builtin_macro_redefined))
+      if (b->always_warn_if_redefined)
 	hp->flags |= NODE_WARN;
       hp->value.builtin = (enum cpp_builtin_type) b->value;
     }
