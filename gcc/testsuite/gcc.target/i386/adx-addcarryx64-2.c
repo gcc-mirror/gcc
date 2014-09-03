9c6fb3ba59e1692160666e6ef21e654e6366af01
Ilya Tocar <ilya.tocar@intel.com>
2014-09-03 09:24:34 +0000

    adxintrin.h (_subborrow_u32): New.
    gcc/
    2014-09-02  Ilya Tocar  <ilya.tocar@intel.com>
    
            * config/i386/adxintrin.h (_subborrow_u32): New.
            (_addcarry_u32): Ditto.
            (_subborrow_u64): Ditto.
            (_addcarry_u64): Ditto.
            * config/i386/i386.c (ix86_builtins): Add IX86_BUILTIN_SBB32,
            IX86_BUILTIN_SBB64.
            (ix86_init_mmx_sse_builtins): Handle __builtin_ia32_sbb_u32,
            __builtin_ia32_sbb_u64
    
    
    testsuite/
    2014-09-02  Ilya Tocar  <ilya.tocar@intel.com>
    
            * gcc.target/i386/adx-addcarryx32-1.c: Test addcarry, subborrow.
            * gcc.target/i386/adx-addcarryx32-2.c: Ditto.
            * gcc.target/i386/adx-addcarryx32-3.c: Ditto.
            * gcc.target/i386/adx-addcarryx64-1.c: Ditto.
            * gcc.target/i386/adx-addcarryx64-2.c: Ditto.
            * gcc.target/i386/adx-addcarryx64-3.c: Ditto.
    
    From-SVN: r214860

--- gcc/testsuite/gcc.target/i386/adx-addcarryx64-2.c
+++ gcc/testsuite/gcc.target/i386/adx-addcarryx64-2.c
@@ -24,4 +24,31 @@ adx_test (void)
 
   if (x != sum_ref)
     abort ();
+
+  c = 0;
+  x = y = 0xFFFFFFFFFFFFFFFFLL;
+  sum_ref = 0xFFFFFFFFFFFFFFFELL;
+
+  /* X = 0xFFFFFFFFFFFFFFFF, Y = 0xFFFFFFFFFFFFFFFF, C = 0.  */
+  c = _addcarry_u64 (c, x, y, &x);
+  /* X = 0xFFFFFFFFFFFFFFFE, Y = 0xFFFFFFFFFFFFFFFF, C = 1.  */
+  c = _addcarry_u64 (c, x, y, &x);
+  /* X = 0xFFFFFFFFFFFFFFFE, Y = 0xFFFFFFFFFFFFFFFF, C = 1.  */
+
+  if (x != sum_ref)
+    abort ();
+
+  c = 0;
+  x = 1LL;
+  y = 0LL;
+  sum_ref = 0x0LL;
+
+  /* X = 0x0000000000000001, Y = 0x0000000000000000, C = 0.  */
+  c = _subborrow_u64 (c, x, y, &x);
+  /* X = 0xFFFFFFFFFFFFFFFF, Y = 0x0000000000000000, C = 1.  */
+  c = _subborrow_u64 (c, x, y, &x);
+  /* X = 0x0000000000000000, Y = 0x0000000000000000, C = 1.  */
+
+  if (x != sum_ref)
+    abort ();
 }
