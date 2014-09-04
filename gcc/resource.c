2eb8c2618d4931b7681e05f322e1a00cf44e835b
tbsaunde <tbsaunde@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-09-04 21:11:23 +0000

    Change the types of arguments and return values for several functions from rtx to rtx_insn *
    gcc/ChangeLog:
    
    2014-09-04  Trevor Saunders  <tsaunders@mozilla.com>
    
    	* emit-rtl.c (get_first_nonnote_insn): Return rtx_insn * instead of
    	rtx.
    	(get_last_nonnote_insn): Likewise.
    	(next_nonnote_insn_bb): Take rtx_insn * instead of rtx.
    	* resource.c (find_basic_block): Likewise.
    	* rtl.h: Adjust.
    	* rtlanal.c (keep_with_call_p): Take const rtx_insn * instead of
    	const_rtx.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@214922 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/resource.c
+++ gcc/resource.c
@@ -78,7 +78,7 @@ static HARD_REG_SET current_live_regs;
 static HARD_REG_SET pending_dead_regs;
 
 static void update_live_status (rtx, const_rtx, void *);
-static int find_basic_block (rtx, int);
+static int find_basic_block (rtx_insn *, int);
 static rtx_insn *next_insn_no_annul (rtx_insn *);
 static rtx_insn *find_dead_or_set_registers (rtx_insn *, struct resources*,
 					     rtx *, int, struct resources,
@@ -132,7 +132,7 @@ update_live_status (rtx dest, const_rtx x, void *data ATTRIBUTE_UNUSED)
    correct.  */
 
 static int
-find_basic_block (rtx insn, int search_limit)
+find_basic_block (rtx_insn *insn, int search_limit)
 {
   /* Scan backwards to the previous BARRIER.  Then see if we can find a
      label that starts a basic block.  Return the basic block number.  */
