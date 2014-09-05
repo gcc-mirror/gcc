ad4211596510c04caeee5404d7b066871114b2bb
Bin Cheng <bin.cheng@arm.com>
2014-09-05 03:45:57 +0000

    re PR target/55701 (Inline some instances of memset for ARM)
    	PR target/55701
    	* config/arm/arm.md (setmem): New pattern.
    	* config/arm/arm-protos.h (struct tune_params): New fields.
    	(arm_gen_setmem): New prototype.
    	* config/arm/arm.c (arm_slowmul_tune): Initialize new fields.
    	(arm_fastmul_tune, arm_strongarm_tune, arm_xscale_tune): Ditto.
    	(arm_9e_tune, arm_v6t2_tune, arm_cortex_tune): Ditto.
    	(arm_cortex_a8_tune, arm_cortex_a7_tune): Ditto.
    	(arm_cortex_a15_tune, arm_cortex_a53_tune): Ditto.
    	(arm_cortex_a57_tune, arm_cortex_a5_tune): Ditto.
    	(arm_cortex_a9_tune, arm_cortex_a12_tune): Ditto.
    	(arm_v7m_tune, arm_v6m_tune, arm_fa726te_tune): Ditto.
    	(arm_const_inline_cost): New function.
    	(arm_block_set_max_insns): New function.
    	(arm_block_set_non_vect_profit_p): New function.
    	(arm_block_set_vect_profit_p): New function.
    	(arm_block_set_unaligned_vect): New function.
    	(arm_block_set_aligned_vect): New function.
    	(arm_block_set_unaligned_non_vect): New function.
    	(arm_block_set_aligned_non_vect): New function.
    	(arm_block_set_vect, arm_gen_setmem): New functions.
    
    	testsuite
    	* gcc.target/arm/memset-inline-1.c: New test.
    	* gcc.target/arm/memset-inline-2.c: New test.
    	* gcc.target/arm/memset-inline-3.c: New test.
    	* gcc.target/arm/memset-inline-4.c: New test.
    	* gcc.target/arm/memset-inline-5.c: New test.
    	* gcc.target/arm/memset-inline-6.c: New test.
    	* gcc.target/arm/memset-inline-7.c: New test.
    	* gcc.target/arm/memset-inline-8.c: New test.
    	* gcc.target/arm/memset-inline-9.c: New test.
    	* gcc.target/arm/memset-inline-10.c: New test.
    
    From-SVN: r214937

---
+++ gcc/testsuite/gcc.target/arm/memset-inline-10.c
@@ -0,0 +1,95 @@
+/* { dg-do compile } */
+/* { dg-options "-march=armv7-a -mfloat-abi=hard -mfpu=neon -O2" } */
+
+#define BUF 100
+long a[BUF];
+
+typedef unsigned int size_t;
+typedef unsigned int wchar_t;
+void *memset (void *s, int c, size_t n);
+struct printf_info
+{
+  int prec;
+  int width;
+  wchar_t spec;
+  unsigned int is_long_double:1;
+  unsigned int is_short:1;
+  unsigned int is_long:1;
+  unsigned int alt:1;
+  unsigned int space:1;
+  unsigned int left:1;
+  unsigned int showsign:1;
+  unsigned int group:1;
+  unsigned int extra:1;
+  unsigned int is_char:1;
+  unsigned int wide:1;
+  unsigned int i18n:1;
+  unsigned int __pad:4;
+  unsigned short int user;
+  wchar_t pad;
+};
+
+void bar (int *alt, int *space, int *left, int *showsign,
+	  int *group,
+	  int *is_long_double,
+	  int *is_short,
+	  int *is_long,
+	  int *width,
+	  int *prec,
+	  int *use_outdigits,
+	  unsigned int *pad,
+	  wchar_t *spec);
+void __printf_fp (char *s, struct printf_info *pinfo);
+int foo(char *s)
+{
+  int alt = 0;
+  int space = 0;
+  int left = 0;
+  int showsign = 0;
+  int group = 0;
+  int is_long_double = 0;
+  int is_short = 0;
+  int is_long = 0;
+  int width = 0;
+  int prec = -1;
+  int use_outdigits = 0;
+  unsigned int pad = L' ';
+  wchar_t spec;
+
+  bar (&alt, &space, &left, &showsign, &group, &is_long_double,
+       &is_short, &is_long, &width, &prec, &use_outdigits, &pad, &spec);
+
+  a[1] = a[0] + a[2] + a[3] + a[4] + a[5] + a[6];
+  a[2] = a[1] + a[3] + a[5] + a[5] + a[6] + a[7];
+  a[3] = a[2] + a[5] + a[7] + a[6] + a[7] + a[8];
+  a[4] = a[3] + a[7] + a[11] + a[7] + a[8] + a[9];
+  a[5] = a[5] + a[11] + a[13] + a[8] + a[9] + a[10];
+  a[6] = a[7] + a[13] + a[17] + a[9] + a[10] + a[11];
+  a[7] = a[11] + a[17] + a[19] + a[10] + a[11] + a[12];
+  a[8] = a[17] + a[19] + a[23] + a[29] + a[31] + a[37];
+
+  {
+    struct printf_info info;
+    memset (&info, 0, sizeof (struct printf_info));
+    info.prec = prec;
+    info.width = width;
+    info.spec = spec;
+    info.is_long_double = is_long_double;
+    info.is_short = is_short;
+    info.is_long = is_long;
+    info.alt = alt;
+    info.space = space;
+    info.left = left;
+    info.showsign = showsign;
+    info.group = group;
+    info.pad = pad;
+    info.extra = 0;
+    info.i18n = use_outdigits;
+    info.wide = sizeof (wchar_t) != 1;
+
+    __printf_fp (s, &info);
+  }
+
+  return 0;
+}
+
---
+++ gcc/testsuite/gcc.target/arm/memset-inline-4.c
@@ -0,0 +1,68 @@
+/* { dg-do run } */
+/* { dg-skip-if "Don't inline memset using neon instructions on cortex-a9" { *-*-* } { "-mcpu=cortex-a9" } { "" } } */
+/* { dg-skip-if "Don't inline memset using neon instructions on cortex-a9" { *-*-* } { "-mtune=cortex-a9" } { "" } } */
+/* { dg-options "-save-temps -O2 -fno-inline" } */
+/* { dg-add-options "arm_neon" } */
+
+#include <string.h>
+#include <stdlib.h>
+
+#define LEN (100)
+int a[LEN];
+int b[LEN];
+int c[LEN];
+void
+foo1 (void)
+{
+    memset (a, -1, 8);
+    return;
+}
+
+void
+foo2 (void)
+{
+  memset (b, 1, 12);
+  return;
+}
+
+void
+foo3 (void)
+{
+  memset (c, 1, 13);
+  return;
+}
+
+void
+check (signed char *arr, int idx, int len, int v)
+{
+  int i;
+  for (i = 0; i < idx; i++)
+    if (arr[i] != v)
+      abort ();
+
+  for (i = idx; i < len; i++)
+    if (arr[i] != 0)
+      abort ();
+}
+
+int
+main(void)
+{
+  int i;
+
+  foo1 ();
+  check ((signed char *)a, 8, sizeof (a), -1);
+
+  foo2 ();
+  check ((signed char *)b, 12, sizeof (b), 1);
+
+  foo3 ();
+  check ((signed char *)c, 13, sizeof (c), 1);
+
+  return 0;
+}
+
+/* { dg-final { scan-assembler-not "bl?\[ \t\]+memset" { target { ! arm_thumb1_ok } } } } */
+/* { dg-final { scan-assembler-times "vst1\.8" 1 { target { arm_little_endian && arm_neon } } } } */
+/* { dg-final { scan-assembler "vstr" { target { arm_little_endian && arm_neon } } } } */
+/* { dg-final { cleanup-saved-temps } } */
