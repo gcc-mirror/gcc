8a06de59b051bb003174fa8638a8e0386f4574d1
Jakub Jelinek <jakub@redhat.com>
2014-09-08 22:07:29 +0200

    re PR tree-optimization/60196 (Incorrect compilation with -fwrapv and -ftree-vectorize)
    	PR tree-optimization/60196
    	PR tree-optimization/63189
    	Backported from mainline
    	2013-09-17  Cong Hou  <congh@google.com>
    
    	* tree-vect-patterns.c (vect_recog_dot_prod_pattern): Fix a bug
    	when checking the dot production pattern. The type of rhs operand
    	of multiply is now checked correctly.
    
    	* gcc.dg/vect/pr63189.c: New test.
    	* gcc.dg/vect/pr60196-1.c: New test.
    	* gcc.dg/vect/pr60196-2.c: New test.
    
    	Backported from mainline
    	2013-09-17  Cong Hou  <congh@google.com>
    
    	* gcc.dg/vect/vect-reduc-dot-s16c.c: Add a test case with dot product 
    	on two arrays with short and int types. This should not be recognized
    	as a dot product pattern.
    
    From-SVN: r215024

---
+++ gcc/testsuite/gcc.dg/vect/pr63189.c
@@ -0,0 +1,26 @@
+/* PR tree-optimization/63189 */
+/* { dg-do run } */
+
+#include "tree-vect.h"
+
+short int d[16] = { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+__attribute__((noinline, noclone)) void
+foo (void)
+{
+  int j, s = 0;
+  for (j = 0; j < 8; j++)
+    s += d[j] * j;
+  if (s != 7)
+    abort ();
+}
+
+int
+main ()
+{
+  check_vect ();
+  foo ();
+  return 0;
+}
+
+/* { dg-final { cleanup-tree-dump "vect" } } */
---
+++ gcc/testsuite/gcc.dg/vect/vect-reduc-dot-s16c.c
@@ -0,0 +1,73 @@
+/* { dg-require-effective-target vect_int } */
+
+#include <stdarg.h>
+#include "tree-vect.h"
+
+#define N 64
+#define DOT 43680
+
+signed short X[N] __attribute__ ((__aligned__(__BIGGEST_ALIGNMENT__)));
+signed int   Y[N] __attribute__ ((__aligned__(__BIGGEST_ALIGNMENT__)));
+
+/* (short, int)->int->int dot product.
+   Not detected as a dot-product pattern.  */
+
+__attribute__ ((noinline)) int
+foo (int len)
+{
+  int i;
+  int result = 0;
+
+  for (i = 0; i < len; i++)
+    {
+      result += (X[i] * Y[i]);
+    }
+  return result;
+}
+
+
+/* (int, short)->int->int dot product.
+   Not detected as a dot-product pattern.  */
+
+__attribute__ ((noinline)) int
+bar (int len)
+{
+  int i;
+  int result = 0;
+
+  for (i = 0; i < len; i++)
+    {
+      result += (Y[i] * X[i]);
+    }
+  return result;
+}
+
+int
+main (void)
+{
+  int i;
+  int dot;
+
+  check_vect ();
+
+  for (i = 0; i < N; i++)
+    {
+      X[i] = i;
+      Y[i] = N - i;
+      __asm__ volatile ("");
+    }
+
+  dot = foo (N);
+  if (dot != DOT)
+    abort ();
+
+  dot = bar (N);
+  if (dot != DOT)
+    abort ();
+
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 2 "vect" { target vect_unpack } } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
+
