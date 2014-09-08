023e4f4c32ec2763dbd3cfb2d2622abbcc1f20b4
Jakub Jelinek <jakub@redhat.com>
2014-09-08 22:18:02 +0200

    re PR tree-optimization/60196 (Incorrect compilation with -fwrapv and -ftree-vectorize)
    	PR tree-optimization/60196
    	PR tree-optimization/63189
    	* gcc.dg/vect/pr63189.c: New test.
    	* gcc.dg/vect/pr60196-1.c: New test.
    	* gcc.dg/vect/pr60196-2.c: New test.
    
    From-SVN: r215026

---
+++ gcc/testsuite/gcc.dg/vect/pr60196-1.c
@@ -0,0 +1,34 @@
+/* PR tree-optimization/63189 */
+/* { dg-additional-options "-fwrapv" } */
+/* { dg-do run } */
+
+#include "tree-vect.h"
+
+__attribute__((noinline, noclone)) static int
+bar (const short *a, int len)
+{
+  int x;
+  int x1 = 0;
+
+  for (x = 0; x < len; x++)
+    x1 += x * a[x];
+  return x1;
+}
+
+__attribute__((noinline, noclone)) void
+foo (void)
+{
+  short stuff[9] = {1, 1, 1, 1, 1, 1, 1, 1, 1 };
+  if (bar (stuff, 9) != 36)
+    abort ();
+}
+
+int
+main ()
+{
+  check_vect ();
+  foo ();
+  return 0;
+}
+
+/* { dg-final { cleanup-tree-dump "vect" } } */
