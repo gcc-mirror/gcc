a86507b29ec2520c70b12c84f4e6c1098f1a3fc0
Andi Kleen <ak@linux.intel.com>
2014-09-16 03:22:53 +0000

    Add some more test cases for fentry and pg
    Test fentry and no_instrument_function overriding.
    
    gcc/testsuite/:
    
    2014-09-15  Andi Kleen  <ak@linux.intel.com>
    
    	* gcc.dg/pg-override.c: New test.
    	* gcc.dg/pg.c: New test.
    	* gcc.target/i386/fentry-override.c: New test.
    	* gcc.target/i386/fentry.c: New test.
    
    From-SVN: r215285

---
+++ gcc/testsuite/gcc.target/i386/fentry.c
@@ -0,0 +1,18 @@
+/* Test -mfentry */
+/* { dg-do compile } */
+/* { dg-options "-fprofile -mfentry" } */
+/* { dg-final { scan-assembler "__fentry__" } } */
+/* Origin: Andi Kleen */
+extern void foobar(const char *);
+
+void func(void)
+{
+  foobar ("Hello world\n");
+}
+
+void func2(void)
+{
+  int i;
+  for (i = 0; i < 10; i++)
+    foobar ("Hello world");
+}
