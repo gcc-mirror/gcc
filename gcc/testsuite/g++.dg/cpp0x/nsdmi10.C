c9aa0007f8147be06783e9f7365d2b5241dee7cb
Paolo Carlini <paolo.carlini@oracle.com>
2014-09-19 09:34:14 +0000

    re PR c++/58328 ([C++11] bogus: error: constructor required before non-static data member for)
    2014-09-19  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/58328
    	* g++.dg/cpp0x/nsdmi10.C: New.
    
    From-SVN: r215386

---
+++ gcc/testsuite/g++.dg/cpp0x/nsdmi10.C
@@ -0,0 +1,18 @@
+// PR c++/58328
+// { dg-do compile { target c++11 } }
+
+struct A1 {
+  struct B1 {
+    int y1 = 1;
+  };
+
+  A1(const B1& opts = B1()) {}  // { dg-error "constructor" }
+};
+
+struct A2 {
+  struct B2 {
+    int x2, y2 = 1;
+  };
+
+  A2(const B2& opts = B2()) {}  // { dg-error "constructor" }
+};
