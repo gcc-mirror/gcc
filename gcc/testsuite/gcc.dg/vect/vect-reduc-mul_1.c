c50c91a9a33d9ade766a509c1fd3d6f9b66d19e7
Alan Lawrence <alan.lawrence@arm.com>
2014-09-22 16:33:05 +0000

    [Testsuite] Add tests of reductions using whole-vector-shifts (multiplication)
    	* lib/target-supports.exp (check_effective_target_whole_vector_shift):
    	New.
    
    	* gcc.dg/vect/vect-reduc-mul_1.c: New test.
    	* gcc.dg/vect/vect-reduc-mul_2.c: New test.
    
    From-SVN: r215473

---
+++ gcc/testsuite/gcc.dg/vect/vect-reduc-mul_1.c
@@ -0,0 +1,36 @@
+/* { dg-require-effective-target vect_int_mult } */
+/* { dg-require-effective-target whole_vector_shift } */
+
+/* Write a reduction loop to be reduced using vector shifts.  */
+
+extern void abort(void);
+
+unsigned char in[16];
+
+int
+main (unsigned char argc, char **argv)
+{
+  unsigned char i = 0;
+  unsigned char sum = 1;
+
+  for (i = 0; i < 16; i++)
+    in[i] = i + i + 1;
+
+  /* Prevent constant propagation of the entire loop below.  */
+  asm volatile ("" : : : "memory");
+
+  for (i = 0; i < 16; i++)
+    sum *= in[i];
+
+  if (sum != 33)
+    {
+      __builtin_printf("Failed %d\n", sum);
+      abort();
+    }
+
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump "Reduce using vector shifts" "vect" } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
+
