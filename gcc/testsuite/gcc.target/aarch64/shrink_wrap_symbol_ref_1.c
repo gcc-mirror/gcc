2b63a3acfa4f49c2117b1c03287be7b5df20ebf8
Jiong Wang <jiong.wang@arm.com>
2014-09-24 18:30:34 +0000

    Improve prepare_shrink_wrap to sink more instructions
      gcc/
        * shrink-wrap.c (move_insn_for_shrink_wrap): Add further check when
        !REG_P (src) to release more instruction sink opportunities.
    
      gcc/testsuite/
        * gcc.target/aarch64/shrink_wrap_symbol_ref_1.c: New testcase.
    
    From-SVN: r215563

---
+++ gcc/testsuite/gcc.target/aarch64/shrink_wrap_symbol_ref_1.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-rtl-pro_and_epilogue" } */
+
+extern char *asm_out_file;
+extern void default_elf_asm_output_ascii (char *, const char *, int);
+
+void
+assemble_string (const char *p, int size)
+{
+  int pos = 0;
+  int maximum = 2000;
+
+  while (pos < size)
+    {
+      int thissize = size - pos;
+
+      if (thissize > maximum)
+	thissize = maximum;
+
+      default_elf_asm_output_ascii (asm_out_file, p, thissize);;
+
+      pos += thissize;
+      p += thissize;
+    }
+}
+
+/* { dg-final { scan-rtl-dump "Performing shrink-wrapping" "pro_and_epilogue"  } } */
+/* { dg-final { cleanup-rtl-dump "pro_and_epilogue" } } */
