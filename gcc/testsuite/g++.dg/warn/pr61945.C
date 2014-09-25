6597738a3a5bb21cab76d013a3c6c3c0bb0b3d34
Marek Polacek <polacek@redhat.com>
2014-09-25 12:56:57 +0000

    re PR c++/61945 (tree check fail with -Woverloaded-virtual)
    	PR c++/61945
    	* class.c (warn_hidden): Check for FUNCTION_DECL.
    
    	* g++.dg/warn/pr61945.C: New test.
    
    From-SVN: r215597

---
+++ gcc/testsuite/g++.dg/warn/pr61945.C
@@ -0,0 +1,11 @@
+// PR c++/61945
+// { dg-do compile }
+// { dg-options "-Woverloaded-virtual" }
+
+class A {
+  virtual int foo ();	// { dg-warning "was hidden" }
+};
+class B : A {
+  template <typename>
+  void foo ();		// { dg-warning "by .B::foo\\(\\)." }
+};
