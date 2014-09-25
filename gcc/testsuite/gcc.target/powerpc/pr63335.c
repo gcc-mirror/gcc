43f02425eeb0ef6783b043ad1cf8f2161e3a2af4
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2014-09-25 15:15:06 +0000

    re PR target/63335 (GCC:failures for vector double on calls to bif vec_[all|any]_[nge|nle])
    [gcc]
    
    2014-09-25  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	PR target/63335
    	* config/rs6000/rs6000-c.c (altivec_build_resolved_builtin):
    	Exclude VSX_BUILTIN_XVCMPGEDP_P from special handling.
    
    [gcc/testsuite]
    
    2014-09-25  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	PR target/63335
    	* gcc.target/powerpc/pr63335.c: New test.
    
    From-SVN: r215604

---
+++ gcc/testsuite/gcc.target/powerpc/pr63335.c
@@ -0,0 +1,30 @@
+/* { dg-do run { target { powerpc64*-*-* } } } */
+/* { dg-require-effective-target powerpc_vsx_ok } */
+/* { dg-options "-mvsx" } */
+
+#include <altivec.h>
+
+void abort (void);
+
+vector double vec = (vector double) {99.0, 99.0};
+
+int main() {
+
+  int actual = vec_all_nge(vec, vec);
+  if ( actual != 0)
+    abort();
+
+  actual = vec_all_nle(vec, vec);
+  if ( actual != 0)
+    abort();
+
+  actual = vec_any_nge(vec, vec);
+  if ( actual != 0)
+    abort();
+
+  actual = vec_any_nle(vec, vec);
+  if ( actual != 0)
+    abort();
+
+  return 0;
+}
