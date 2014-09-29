4ec54a71a71f3a69be14600cea60e39e65701b40
James Clarke <jrtc27@jrtc27.com>
2014-09-29 18:40:57 +0000

    re PR target/61407 (Build errors on latest OS X 10.10 Yosemite with Xcode 6 on GCC 4.8.3)
    	PR target/61407
    
    	* config/darwin-c.c (version_as_macro): Added extra 0 for OS X 10.10
    	and above.
    	* config/darwin-driver.c (darwin_find_version_from_kernel): Removed
    	kernel version check to avoid incrementing it after every major OS X
    	release.
    	(darwin_default_min_version): Avoid static memory buffer.
    
    	* gcc.dg/darwin-minversion-1.c: Fixed formatting
    	* gcc.dg/darwin-minversion-2.c: Fixed formatting
    	* gcc.dg/darwin-minversion-3.c: Fixed formatting
    	* gcc.dg/darwin-minversion-4.c: Added test for OS X 10.10
    
    Co-Authored-By: Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
    
    From-SVN: r215690

--- gcc/testsuite/gcc.dg/darwin-minversion-3.c
+++ gcc/testsuite/gcc.dg/darwin-minversion-3.c
@@ -2,7 +2,8 @@
 /* { dg-options "-mmacosx-version-min=10.4.10" } */
 /* { dg-do compile { target *-*-darwin* } } */
 
-int main(void)
+int
+main ()
 {
 #if __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ != 1040
   fail me;
