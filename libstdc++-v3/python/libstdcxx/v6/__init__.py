b073d15fe57dc6cb8b8ec9537afd69616877db85
Siva Chandra Reddy <sivachandra@google.com>
2014-09-30 15:33:27 +0000

    hook.in: Only import libstdcxx.v6.
    2014-09-30  Siva Chandra Reddy  <sivachandra@google.com>
    
    	* python/hook.in: Only import libstdcxx.v6.
    	* python/libstdcxx/v6/__init__.py: Load printers and xmethods.
    
    From-SVN: r215726

--- libstdc++-v3/python/libstdcxx/v6/__init__.py
+++ libstdc++-v3/python/libstdcxx/v6/__init__.py
@@ -1,1 +1,32 @@
+# Copyright (C) 2014 Free Software Foundation, Inc.
 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gdb
+
+# Load the pretty-printers.
+from printers import register_libstdcxx_printers
+register_libstdcxx_printers(gdb.current_objfile())
+
+# Load the xmethods if GDB supports them.
+def gdb_has_xmethods():
+    try:
+        import gdb.xmethod
+        return True
+    except ImportError:
+        return False
+
+if gdb_has_xmethods():
+    from xmethods import register_libstdcxx_xmethods
+    register_libstdcxx_xmethods(gdb.current_objfile())
