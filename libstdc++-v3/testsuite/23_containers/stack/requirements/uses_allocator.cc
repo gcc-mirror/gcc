52ff6cec1cee5602d88b385b32013027c2df9887
Jonathan Wakely <jwakely@redhat.com>
2014-10-01 13:34:16 +0100

    stl_queue.h: Include missing header.
    	* include/bits/stl_queue.h: Include missing header.
    	* include/bits/stl_stack.h: Likewise.
    	* testsuite/23_containers/priority_queue/requirements/
    	uses_allocator.cc: New.
    	* testsuite/23_containers/queue/requirements/uses_allocator.cc: New.
    	* testsuite/23_containers/stack/requirements/uses_allocator.cc: New.
    
    From-SVN: r215756

---
+++ libstdc++-v3/testsuite/23_containers/stack/requirements/uses_allocator.cc
@@ -0,0 +1,29 @@
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++11" }
+// { dg-do compile }
+
+#include <stack>
+
+template<typename A>
+  using uses_allocator = std::uses_allocator<std::stack<int>, A>;
+
+static_assert( uses_allocator<std::allocator<int>>::value, "valid allocator" );
+
+struct X { };
+static_assert( !uses_allocator<X>::value, "invalid allocator" );
