a0101705ab9da1f5336ddaf9ba8ed56a3ede7c18
Jakub Jelinek <jakub@redhat.com>
2014-10-01 16:46:32 +0200

    re PR c++/63306 (ICE: Segmentation fault in analyze_functions())
    	PR c++/63306
    	Backported from mainline
    	2014-08-01  James Greenhalgh  <james.greenhalgh@arm.com>
    
    	PR regression/61510
    	* cgraphunit.c (analyze_functions): Use get_create rather than get
    	for decls which are clones of abstract functions.
    
    	* g++.dg/ipa/pr63306.C: New test.
    
    From-SVN: r215767

--- gcc/cgraphunit.c
+++ gcc/cgraphunit.c
@@ -1038,7 +1038,7 @@ analyze_functions (void)
 	      if (DECL_ABSTRACT_ORIGIN (decl))
 		{
 		  struct cgraph_node *origin_node
-	    	  = cgraph_get_node (DECL_ABSTRACT_ORIGIN (decl));
+		    = cgraph_get_create_node (DECL_ABSTRACT_ORIGIN (decl));
 		  origin_node->used_as_abstract_origin = true;
 		}
 	    }
