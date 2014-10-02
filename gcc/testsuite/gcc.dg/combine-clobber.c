5827c03c811c0dc329be032b542dcb37ac8aa816
Segher Boessenkool <segher@gcc.gnu.org>
2014-10-02 15:40:53 +0200

    Add file I forgot to add before.
    From-SVN: r215802

---
+++ gcc/testsuite/gcc.dg/combine-clobber.c
@@ -0,0 +1,22 @@
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -fdump-rtl-combine-all" } */
+
+/* This testcase checks if combine tries to combine sequences where the last
+   insn has a clobber of a reg, and a previous insn sets that reg.
+
+   In this case, we have three insns
+
+   (set flags (compare a b))
+   (set tmp (eq flags 0))
+   (parallel [(set dst (neg tmp))
+	      (clobber flags)])
+
+   Previously, combine would not try the three-insn combination because of
+   the set and clobber of flags.  Now it does.  Test that.  */
+
+
+int f(int a, int b) { return -(a == b); }
+
+/* This regexp works for reg parameters as well as mem parameters.  */
+/* { dg-final { scan-rtl-dump {neg:SI[^:]*eq:SI[^:]*:SI} "combine" } } */
+/* { dg-final { cleanup-rtl-dump "combine" } } */
