bb42836b34dca74aab99035ac236fa6e81b1d4c1
Manuel López-Ibáñez <manu@gcc.gnu.org>
2014-10-03 21:22:10 +0000

    tree-pretty-print.c (dump_location): Make it extern.
    gcc/ChangeLog:
    
    2014-10-03  Manuel López-Ibáñez  <manu@gcc.gnu.org>
    
    	* tree-pretty-print.c (dump_location): Make it extern. Dump also
    	the column.
    	* tree-pretty-print.h (dump_location): Declare.
    	* gimple-pretty-print.c (dump_gimple_phi): Use dump_location.
    	(pp_gimple_stmt_1): Likewise.
    	(dump_implicit_edges): Likewise.
    	* gimplify.c (gimplify_call_expr): Use LOCATION_FILE and
    	LOCATION_LINE.
    
    gcc/testsuite/ChangeLog:
    
    2014-10-03  Manuel López-Ibáñez  <manu@gcc.gnu.org>
    
    	* gcc.dg/tm/debug-1.c: Update regex.
    	* c-c++-common/raw-string-18.c: Update regex.
    	* c-c++-common/raw-string-19.c: Update regex.
    
    From-SVN: r215879

--- gcc/testsuite/gcc.dg/tm/debug-1.c
+++ gcc/testsuite/gcc.dg/tm/debug-1.c
@@ -19,8 +19,8 @@ int main() {
 	return 0;
 }
 
-/* { dg-final { scan-tree-dump-times ": 13:.*b = 9898" 1 "tmmark" } } */
-/* { dg-final { scan-tree-dump-times ": 14:.*_ITM_beginTransaction" 1 "tmmark" } } */
-/* { dg-final { scan-tree-dump-times ": 15:.*ITM_WU. \\(&z" 1 "tmmark" } } */
-/* { dg-final { scan-tree-dump-times ": 16:.*ITM_WU. \\(&a" 1 "tmmark" } } */
+/* { dg-final { scan-tree-dump-times ":13:.*b = 9898" 1 "tmmark" } } */
+/* { dg-final { scan-tree-dump-times ":14:.*_ITM_beginTransaction" 1 "tmmark" } } */
+/* { dg-final { scan-tree-dump-times ":15:.*ITM_WU. \\(&z" 1 "tmmark" } } */
+/* { dg-final { scan-tree-dump-times ":16:.*ITM_WU. \\(&a" 1 "tmmark" } } */
 /* { dg-final { cleanup-tree-dump "tmmark" } } */
