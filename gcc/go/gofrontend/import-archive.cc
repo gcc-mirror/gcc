8a5b2f56c4c9d80c8ac2c31c82304681f4e9da80
Ian Lance Taylor <ian@gcc.gnu.org>
2014-10-03 18:27:43 +0000

    compiler: Recognize 64-bit symbol tables in archives.
    By Dominik Vogt.
    
    From-SVN: r215869

--- gcc/go/gofrontend/import-archive.cc
+++ gcc/go/gofrontend/import-archive.cc
@@ -295,6 +295,15 @@ Archive_file::interpret_header(const Archive_header* hdr, off_t off,
       // This is the symbol table.
       pname->clear();
     }
+  else if (hdr->ar_name[1] == 'S' && hdr->ar_name[2] == 'Y'
+	   && hdr->ar_name[3] == 'M' && hdr->ar_name[4] == '6'
+	   && hdr->ar_name[5] == '4' && hdr->ar_name[6] == '/'
+	   && hdr->ar_name[7] == ' '
+	  )
+    {
+      // 64-bit symbol table.
+      pname->clear();
+    }
   else if (hdr->ar_name[1] == '/')
     {
       // This is the extended name table.
