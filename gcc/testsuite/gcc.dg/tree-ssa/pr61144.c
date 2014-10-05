ca7d22ca9a1dc73707e387f5a1d72f1a39cbd40c
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-10-05 04:56:14 +0000

    PR ipa/61144 	* gcc.dg/tree-ssa/pr61144.c: New testcase.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@215897 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr61144.c
@@ -0,0 +1,7 @@
+/* { dg-do compile } */
+/* { dg-require-weak "" } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+static int dummy = 0;
+extern int foo __attribute__((__weak__, __alias__("dummy")));
+int bar() { if (foo) return 1; return 0; }
+/* { dg-final { scan-tree-dump-not "return 0" "optimized"} } */
