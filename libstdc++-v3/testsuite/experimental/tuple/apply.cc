5720186d18883ddbbfcd422dd7e8638e19fd7bf3
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-10-06 12:26:45 +0000

    * include/Makefile.am: Add new header. 	* include/Makefile.in: Regenerate. 	* include/experimental/tuple: New 	* doc/xml/manual/status_cxx2014.xml: Update. 	* doc/html/manual/status.html: Regenerate. 	* testsuite/experimental/feat-lib-fund.cc: Test for new header. 	* testsuite/experimental/tuple/apply.cc: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@215930 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/experimental/tuple/apply.cc
@@ -0,0 +1,49 @@
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++14" }
+
+#include <experimental/tuple>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  auto t = std::make_tuple(1, '2', 3.0);
+  std::experimental::apply( [&](int& i, char& c, double& d) {
+      VERIFY(&i == &std::get<int>(t));
+      VERIFY(&c == &std::get<char>(t));
+      VERIFY(&d == &std::get<double>(t));
+    }, t);
+}
+
+constexpr int func(int i, int j) { return i + j; }
+
+void
+test02()
+{
+  constexpr auto t = std::make_tuple(1, 2);
+  constexpr int i = std::experimental::apply(func, t);
+  VERIFY( i == 3 );
+}
+
+int
+main()
+{
+  test01();
+  test02();
+}
