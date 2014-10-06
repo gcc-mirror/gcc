058e5c2dcf572cf3237f84d95c44a16d9507d887
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-10-06 07:34:24 +0000

    Commit forgotten hunk.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@215915 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/libgfortran.h
+++ gcc/fortran/libgfortran.h
@@ -22,6 +22,8 @@ along with GCC; see the file COPYING3.  If not see
    Note that no features were obsoleted nor deleted in F2003.
    Please remember to keep those definitions in sync with
    gfortran.texi.  */
+/* For now, use F2015 = GFC_STD_GNU.  */
+#define GFC_STD_F2015	        (1<<5)	/* PLACEHOLDER for Fortran 2015.  */
 #define GFC_STD_F2008_TS	(1<<9)	/* POST-F2008 technical reports.  */
 #define GFC_STD_F2008_OBS	(1<<8)	/* Obsolescent in F2008.  */
 #define GFC_STD_F2008		(1<<7)	/* New in F2008.  */
