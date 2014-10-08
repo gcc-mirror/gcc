bc9deadabb359840d304cdc6618bf1292ee030dd
Mark Wielaard <mjw@redhat.com>
2014-10-08 10:27:01 +0000

    testsuite deleted-member-function.C: Remove comment marker prefix.
    testsuite/ChangeLog
    
    	* g++.dg/debug/dwarf2/deleted-member-function.C: Remove comment marker
    	prefix from scan-assembler-times.
    
    From-SVN: r215997

--- gcc/testsuite/g++.dg/debug/dwarf2/deleted-member-function.C
+++ gcc/testsuite/g++.dg/debug/dwarf2/deleted-member-function.C
@@ -1,6 +1,6 @@
 // { dg-do compile }
 // { dg-options "-O -std=c++11 -g -dA" }
-// { dg-final { scan-assembler-times "# DW_AT_GNU_deleted" 2 } }
+// { dg-final { scan-assembler-times " DW_AT_GNU_deleted" 2 } }
 
 struct Foo
 {
