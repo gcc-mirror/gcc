285dc591ab7b39e09a796003fe7cddf5f0283ea0
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-10-09 08:37:26 +0000

    2014-10-09  Ville Voutilainen  <ville.voutilainen@gmail.com>
    	PR libstdc++/60132
        	* include/std/type_traits (is_trivially_copyable,
        	is_trivially_constructible, is_trivially_default_constructible,
        	is_trivially_copy_constructible, is_trivially_move_constructible,
        	is_trivially_assignable, is_trivially_copy_assignable,
        	is_trivially_move_assignable): New.
        	* testsuite/20_util/is_trivially_assignable/requirements/
        	typedefs.cc: Likewise.
        	* testsuite/20_util/is_trivially_assignable/requirements/
        	explicit_instantiation.cc: Likewise.
        	* testsuite/20_util/is_trivially_assignable/value.cc: Likewise.
    	* testsuite/20_util/is_trivially_constructible/requirements/
    	typedefs.cc: Likewise.
    	* testsuite/20_util/is_trivially_constructible/requirements/
    	explicit_instantiation.cc: Likewise.
    	* testsuite/20_util/is_trivially_constructible/value.cc: Likewise.
    	* testsuite/20_util/is_trivially_copyable/requirements/
    	typedefs.cc: Likewise.
    	* testsuite/20_util/is_trivially_copyable/requirements/
    	explicit_instantiation.cc: Likewise.
    	* testsuite/20_util/is_trivially_copyable/value.cc: Likewise.
    	* testsuite/20_util/is_trivially_copy_assignable/requirements/
    	typedefs.cc: Likewise.
    	* testsuite/20_util/is_trivially_copy_assignable/requirements/
    	explicit_instantiation.cc: Likewise.
    	* testsuite/20_util/is_trivially_copy_assignable/value.cc: Likewise.
    	* testsuite/20_util/is_trivially_copy_constructible/requirements/
    	typedefs.cc: Likewise.
    	* testsuite/20_util/is_trivially_copy_constructible/requirements/
    	explicit_instantiation.cc: Likewise.
        	* testsuite/20_util/is_trivially_copy_constructible/value.cc: Likewise.
        	* testsuite/20_util/is_trivially_default_constructible/requirements/
        	typedefs.cc: Likewise.
        	* testsuite/20_util/is_trivially_default_constructible/requirements/
        	explicit_instantiation.cc: Likewise.
        	* testsuite/20_util/is_trivially_default_constructible/
    	value.cc: Likewise.
        	* testsuite/20_util/is_trivially_move_assignable/requirements/
        	typedefs.cc: Likewise.
        	* testsuite/20_util/is_trivially_move_assignable/requirements/
        	explicit_instantiation.cc: Likewise.
        	* testsuite/20_util/is_trivially_move_assignable/value.cc: Likewise.
        	* testsuite/20_util/is_trivially_move_constructible/requirements/
        	typedefs.cc: Likewise.
        	* testsuite/20_util/is_trivially_move_constructible/requirements/
        	explicit_instantiation.cc: Likewise.
        	* testsuite/20_util/is_trivially_move_constructible/value.cc:
    	Likewise.
    	* testsuite/20_util/declval/requirements/1_neg.cc: Adjust dg-error
    	line number.
    	* testsuite/20_util/make_signed/requirements/typedefs_neg.cc:
    	Likewise.
    	* testsuite/20_util/make_unsigned/requirements/typedefs_neg.cc:
    	Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@216032 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/20_util/is_trivially_copy_constructible/requirements/explicit_instantiation.cc
+++ libstdc++-v3/testsuite/20_util/is_trivially_copy_constructible/requirements/explicit_instantiation.cc
@@ -1,29 +1,31 @@
-// { dg-options "-std=gnu++0x" }
+// { dg-options "-std=gnu++11" }
 // { dg-do compile }
-// 2009-11-12  Paolo Carlini  <paolo.carlini@oracle.com>
 //
-// Copyright (C) 2009-2014 Free Software Foundation, Inc.
+// 2014-10-09  Ville Voutilainen  <ville.voutilainen@gmail.com>
+//
+// Copyright (C) 2014 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the
 // Free Software Foundation; either version 3, or (at your option)
 // any later version.
-//
+
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-//
+
 // You should have received a copy of the GNU General Public License along
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-error "static assertion failed" "" { target *-*-* } 2142 }
+// NB: This file is for testing type_traits with NO OTHER INCLUDES.
 
-#include <utility>
+#include <type_traits>
 
-void test01()
+namespace std
 {
-  std::declval<int>();		// { dg-error "required from here" }
+  typedef short test_type;
+  template struct is_trivially_copy_constructible<test_type>;
 }
--- libstdc++-v3/testsuite/20_util/make_unsigned/requirements/typedefs_neg.cc
+++ libstdc++-v3/testsuite/20_util/make_unsigned/requirements/typedefs_neg.cc
@@ -48,5 +48,5 @@ void test01()
 // { dg-error "required from here" "" { target *-*-* } 40 }
 // { dg-error "required from here" "" { target *-*-* } 42 }
 
-// { dg-error "invalid use of incomplete type" "" { target *-*-* } 1667 }
-// { dg-error "declaration of" "" { target *-*-* } 1631 }
+// { dg-error "invalid use of incomplete type" "" { target *-*-* } 1710 }
+// { dg-error "declaration of" "" { target *-*-* } 1674 }
