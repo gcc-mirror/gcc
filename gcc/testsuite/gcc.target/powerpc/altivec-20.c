06e637fec40a34ee186f3347aca9fa8b7a1f10e6
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2014-10-13 02:35:03 +0000

    backport: rs6000-c.c (altivec_resolve_overloaded_builtin): Issue a warning message when vec_lvsl or vec_lvsr is used with a little endian...
    [gcc]
    
    2014-10-12  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	Backport from mainline r215880
    	2014-10-03  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* config/rs6000/rs6000-c.c (altivec_resolve_overloaded_builtin):
    	Issue a warning message when vec_lvsl or vec_lvsr is used with a
    	little endian target.
    
    	Backport from mainline r215882
    	2014-10-03  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* altivec.md (altivec_lvsl): New define_expand.
    	(altivec_lvsl_direct): Rename define_insn from altivec_lvsl.
    	(altivec_lvsr): New define_expand.
    	(altivec_lvsr_direct): Rename define_insn from altivec_lvsr.
    	* rs6000.c (rs6000_expand_builtin): Change to use
    	altivec_lvs[lr]_direct; remove commented-out code.
    
    [gcc/testsuite]
    
    2014-10-12  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	Backport from mainline r215880
    	2014-10-03  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* g++.dg/ext/altivec-2.C: Compile with -Wno-deprecated to avoid
    	failing with the new warning message.
    	* gcc.dg/vmx/3c-01a.c: Likewise.
    	* gcc.dg/vmx/ops-long-1.c: Likewise.
    	* gcc.dg/vmx/ops.c: Likewise.
    	* gcc.target/powerpc/altivec-20.c: Likewise.
    	* gcc.target/powerpc/altivec-6.c: Likewise.
    	* gcc.target/powerpc/altivec-vec-merge.c: Likewise.
    	* gcc.target/powerpc/vsx-builtin-8.c: Likewise.
    	* gcc.target/powerpc/warn-lvsl-lvsr.c: New test.
    
    	Backport from mainline r215882
    	2014-10-03  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* gcc.target/powerpc/lvsl-lvsr.c: New test.
    
    	Backport from mainline r216017
    	2014-10-08  Pat Haugen  <pthaugen@us.ibm.com>
    
    	* gcc.dg/vmx/3c-01a.c: Add default options from vmx.exp.
    	* gcc.dg/vmx/ops.c: Likewise.
    	* gcc.dg/vmx/ops-long-1.c: Likewise.
    
    From-SVN: r216135

--- gcc/testsuite/gcc.target/powerpc/altivec-20.c
+++ gcc/testsuite/gcc.target/powerpc/altivec-20.c
@@ -1,5 +1,5 @@
 /* { dg-do compile { target powerpc_altivec_ok } } */
-/* { dg-options "-maltivec -mcpu=G5 -O2" } */
+/* { dg-options "-maltivec -mcpu=G5 -O2 -Wno-deprecated" } */
 
 #include <altivec.h>
 
--- gcc/testsuite/gcc.target/powerpc/altivec-vec-merge.c
+++ gcc/testsuite/gcc.target/powerpc/altivec-vec-merge.c
@@ -1,7 +1,7 @@
 /* { dg-do run { target { powerpc*-*-* && vmx_hw } } } */
 /* { dg-do compile { target { powerpc*-*-* && { ! vmx_hw } } } } */
 /* { dg-require-effective-target powerpc_altivec_ok } */
-/* { dg-options "-maltivec -O2" } */
+/* { dg-options "-maltivec -O2 -Wno-deprecated" } */
 
 #include <altivec.h>
 
