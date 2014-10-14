adcdd84fdd8a53053f779469850451b24a58b8f7
olegendo <olegendo@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-10-14 00:50:18 +0000

    gcc/ 	PR target/63260 	* config/sh/sh.md (negsf2, negsf2_i, negdf2, negdf2_i, abssf2, 	abssf2_i, absdf2, absdf2_i): Remove fp_mode attribute.  Remove use 	of FPSCR. 	(negsf2_i): Rename to *negsf2_i. 	(abssf2_i): Rename to *abssf2_i. 	(negdf2_i): Rename to *negdf2_i. 	(absdf2_i): Rename to *absdf2_i.
    gcc/testsuite/
    	PR target/63260
    	* gcc.target/sh/pr63260.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@216173 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/sh/pr63260.c
@@ -0,0 +1,30 @@
+/* Check that there is no FPSCR precision mode change generated for fneg and
+   fabs instructions.  */
+/* { dg-do compile }  */
+/* { dg-options "-O1" } */
+/* { dg-skip-if "" { "sh*-*-*" } { "-m5*"} { "" } }  */
+/* { dg-final { scan-assembler-not "fpscr|fpchg" } } */
+
+float
+test_0 (float x)
+{
+  return -x;
+}
+
+double
+test_1 (double x)
+{
+  return -x;
+}
+
+float
+test_2 (float x)
+{
+  return __builtin_fabs (x);
+}
+
+double
+test_3 (double x)
+{
+  return __builtin_abs (x);
+}
