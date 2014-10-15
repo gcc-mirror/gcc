815d67c2ffc40783b2f898c8d7b145a4b56c20b8
Jason Merrill <jason@redhat.com>
2014-10-15 12:46:23 -0400

    re PR c++/56710 (Using reserved double underscore variable name in a lambda causes internal compiler error)
    	PR c++/56710
    	* semantics.c (finish_member_declaration): Don't push closure
    	members.
    
    From-SVN: r216275

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -2735,8 +2735,10 @@ finish_member_declaration (tree decl)
 					      /*friend_p=*/0);
 	}
     }
-  /* Enter the DECL into the scope of the class.  */
-  else if (pushdecl_class_level (decl))
+  /* Enter the DECL into the scope of the class, if the class
+     isn't a closure (whose fields are supposed to be unnamed).  */
+  else if (CLASSTYPE_LAMBDA_EXPR (current_class_type)
+	   || pushdecl_class_level (decl))
     {
       if (TREE_CODE (decl) == USING_DECL)
 	{
