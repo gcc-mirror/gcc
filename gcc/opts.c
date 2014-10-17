3a1880ee4ccdec00ad149122ebd2481f29169304
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-10-17 09:37:35 +0000

    Temporary fix for PR63566.
    	* ipa-icf.c (sem_function::merge): Local flags are set to false
    	to enforce equal calling convention to be used.
    	* opts.c (common_handle_option): Indentation fix.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@216386 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/opts.c
+++ gcc/opts.c
@@ -1982,8 +1982,8 @@ common_handle_option (struct gcc_options *opts,
       break;
 
     case OPT_fipa_icf:
-	opts->x_flag_ipa_icf_functions = value;
-	opts->x_flag_ipa_icf_variables = value;
+      opts->x_flag_ipa_icf_functions = value;
+      opts->x_flag_ipa_icf_variables = value;
       break;
 
     default:
