4435bb9232cf3a3ab015f7bc0f8dbe562bd8e0c8
Marek Polacek <polacek@redhat.com>
2014-10-19 16:47:35 +0000

    re PR c/63567 (Linux kernel build error due to non-static initializers)
    	PR c/63567
    	* c-typeck.c (output_init_element): Allow initializing objects with
    	static storage duration with compound literals even in C99 and add
    	pedwarn for it.
    
    	* gcc.dg/pr63567-3.c: New test.
    	* gcc.dg/pr63567-4.c: New test.
    
    From-SVN: r216440

--- gcc/c/c-typeck.c
+++ gcc/c/c-typeck.c
@@ -8251,11 +8251,14 @@ output_init_element (location_t loc, tree value, tree origtype,
     value = array_to_pointer_conversion (input_location, value);
 
   if (TREE_CODE (value) == COMPOUND_LITERAL_EXPR
-      && require_constant_value && !flag_isoc99 && pending)
+      && require_constant_value && pending)
     {
       /* As an extension, allow initializing objects with static storage
 	 duration with compound literals (which are then treated just as
 	 the brace enclosed list they contain).  */
+      if (flag_isoc99)
+	pedwarn_init (loc, OPT_Wpedantic, "initializer element is not "
+		      "constant");
       tree decl = COMPOUND_LITERAL_EXPR_DECL (value);
       value = DECL_INITIAL (decl);
     }
