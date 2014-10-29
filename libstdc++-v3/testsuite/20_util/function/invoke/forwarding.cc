1ceb3fa7741ad071994a33267270f8d705766d84
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-10-29 18:38:31 +0000

    Use perfect forwarding in std::function invokers.
    	* include/std/functional: (_Function_base::_Function_base()): Use
    	nullptr instead of literal zero.
    	(function::operator=(nullptr_t)): Likewise.
    	(_Function_handler::_M_invoke): Use perfect forwarding for _ArgTypes.
    	(function::_Invoker_type): Likewise.
    	* testsuite/20_util/function/invoke/forwarding.cc: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@216849 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/20_util/function/invoke/forwarding.cc
@@ -0,0 +1,56 @@
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++11" }
+
+#include <functional>
+#include <testsuite_hooks.h>
+
+struct Counted
+{
+  Counted() : count(0) { }
+  Counted(const Counted& c) : count(c.count + 1) { }
+  int count;
+};
+
+int func(Counted c) { return c.count; }
+
+std::function<int(Counted)> f = func;
+
+void
+test01()
+{
+  Counted c;
+  int n = f(c);
+  // 1 copy invoking function::operator() and 1 copy invoking func
+  VERIFY( n == 2 );
+}
+
+void
+test02()
+{
+  int n = f(Counted{});
+  // copy elided when invoking function::operator(), 1 copy invoking func
+  VERIFY( n == 1 );
+}
+
+int
+main()
+{
+  test01();
+  test02();
+}
