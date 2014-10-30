a3fec48aa8f2a0899daf9a16379c07f8d444ac51
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-10-30 17:22:12 +0000

    * c-objc-common.c (c_tree_printer) <case 'T'>: For a typedef name, 	print the stripped version as well, if they're not the same.
    	* gcc.dg/diag-aka-1.c: New test.
    	* gcc.dg/pr13804-1.c: Adjust dg-error.
    	* gcc.dg/redecl-14.c: Likewise.
    	* gcc.dg/pr56980.c: Adjust dg-message.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@216941 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr56980.c
+++ gcc/testsuite/gcc.dg/pr56980.c
@@ -5,12 +5,12 @@ typedef struct A { int i; } B;
 typedef union U { int i; } V;
 typedef enum E { G } F;
 
-void foo_s (struct A); /* { dg-message "expected .struct A. but argument is of type .B \\*." } */
-void foo_u (union U); /* { dg-message "expected .union U. but argument is of type .V \\*." } */
-void foo_e (enum E); /* { dg-message "expected .enum E. but argument is of type .F \\*." } */
-void foo_sp (B *); /* { dg-message "expected .B \\*. but argument is of type .struct B \\*." } */
-void foo_up (V *); /* { dg-message "expected .V \\*. but argument is of type .union V \\*." } */
-void foo_ep (F *); /* { dg-message "expected .F \\*. but argument is of type .enum F \\*." } */
+void foo_s (struct A); /* { dg-message "expected .struct A. but argument is of type .B \\* {aka struct A \\*}." } */
+void foo_u (union U); /* { dg-message "expected .union U. but argument is of type .V \\* {aka union U \\*}." } */
+void foo_e (enum E); /* { dg-message "expected .enum E. but argument is of type .F \\* {aka enum E \\*}." } */
+void foo_sp (B *); /* { dg-message "expected .B \\* {aka struct A \\*}. but argument is of type .struct B \\*." } */
+void foo_up (V *); /* { dg-message "expected .V \\* {aka union U \\*}. but argument is of type .union V \\*." } */
+void foo_ep (F *); /* { dg-message "expected .F \\* {aka enum E \\*}. but argument is of type .enum F \\*." } */
 
 void 
 bar (B *b, V *v, F *f)
