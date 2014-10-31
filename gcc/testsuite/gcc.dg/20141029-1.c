221be654b63cb001192b7cae6c8c0075ac1b91ff
DJ Delorie <dj@redhat.com>
2014-10-31 17:03:50 -0400

    expmed.c (strict_volatile_bitfield_p): Fix off-by-one error.
    * expmed.c (strict_volatile_bitfield_p): Fix off-by-one error.
    * gcc.dg/20141029-1.c: New.
    
    From-SVN: r216989

---
+++ gcc/testsuite/gcc.dg/20141029-1.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+/* { dg-options "-fstrict-volatile-bitfields -fdump-rtl-final" } */
+
+#define PERIPH (*(volatile struct system_periph *)0x81234)
+
+struct system_periph {
+  union {
+    unsigned short WORD;
+    struct {
+      unsigned short a:1;
+      unsigned short b:1;
+      unsigned short  :5;
+      unsigned short c:1;
+      unsigned short  :8;
+    } BIT;
+  } ALL;
+};
+
+void
+foo()
+{
+  while (1)
+    {
+      PERIPH.ALL.BIT.a = 1;
+    }
+}
+/* { dg-final { scan-rtl-dump-times "mem/v(/.)*:HI" 4 "final" } } */
+/* { dg-final { cleanup-rtl-dump "final" } } */
