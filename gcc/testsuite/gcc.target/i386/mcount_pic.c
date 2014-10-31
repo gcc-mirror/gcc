9a2db19920ccf87adac7ec25807ca0608d9d022e
Evgeny Stupachenko <evstupac@gmail.com>
2014-10-31 13:30:06 +0000

    re PR target/63534 (Bootstrap failure on x86_64/i686-linux)
    gcc/
    
    	PR target/63534
    	* config/i386/i386.c (ix86_init_pic_reg): Emit SET_GOT to
    	REAL_PIC_OFFSET_TABLE_REGNUM for mcount profiling.
    	(ix86_save_reg): Save REAL_PIC_OFFSET_TABLE_REGNUM when profiling
    	using mcount in 32bit PIC mode.
    	(ix86_elim_entry_set_got): New.
    	(ix86_expand_prologue): For the mcount profiling emit new SET_GOT
    	in PROLOGUE, delete initial if possible.
    
    gcc/testsuite/
    
    	PR target/63534
    	* gcc.target/i386/mcount_pic.c: New.
    
    From-SVN: r216975

---
+++ gcc/testsuite/gcc.target/i386/mcount_pic.c
@@ -0,0 +1,15 @@
+/* PR target/63534 */
+/* Check correct mcount generation.  */
+/* { dg-do run } */
+/* { dg-require-effective-target fpic } */
+/* { dg-require-effective-target ia32 } */
+/* { dg-options "-O2 -fpic -p -save-temps" } */
+
+int main ()
+{
+  return 0;
+}
+
+/* { dg-final { scan-assembler "mcount" } } */
+/* { dg-final { scan-assembler "get_pc_thunk" } } */
+/* { dg-final { cleanup-saved-temps } } */
