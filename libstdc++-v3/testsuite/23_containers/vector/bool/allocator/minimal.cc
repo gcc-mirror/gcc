ccd615e3fdf2d2b80577d27e884884e9f758e8ea
Jonathan Wakely <jwakely@redhat.com>
2014-10-31 20:49:40 +0000

    Make std::vector<bool> meet C++11 allocator requirements.
    	* include/bits/stl_bvector.h (_Bvector_base): Use allocator_traits.
    	(_Bvector_base::_Bvector_impl): Use allocator's pointer type.
    	(_Bvector_base::_M_end_addr()): Convert to raw pointer.
    	(vector<bool>): Use allocator_traits and _M_end_addr. Add allocator
    	extended constructors.
    	* include/bits/vector.tcc (vector<bool>): Use allocator_traits and
    	_M_end_addr.
    	* testsuite/23_containers/vector/bool/allocator/copy.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/minimal.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/noexcept.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/copy_assign.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/move.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/swap.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/ext_ptr.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/move_assign.cc: New.
    
    From-SVN: r216988

---
+++ libstdc++-v3/testsuite/23_containers/vector/bool/allocator/minimal.cc
@@ -0,0 +1,38 @@
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++11" }
+// { dg-do compile }
+
+#include <vector>
+#include <memory>
+#include <testsuite_allocator.h>
+
+using T = bool;
+
+using __gnu_test::SimpleAllocator;
+
+template class std::vector<T, SimpleAllocator<T>>;
+
+void test01()
+{
+  typedef SimpleAllocator<T> alloc_type;
+  typedef std::allocator_traits<alloc_type> traits_type;
+  typedef std::vector<T, alloc_type> test_type;
+  test_type v(alloc_type{});
+  v.push_back(T());
+}
