081db185ef94b45c72106e3725fe64de56b402d2
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-10-31 20:49:40 +0000

    Make std::vector<bool> meet C++11 allocator requirements.
    	* include/bits/stl_bvector.h (_Bvector_base): Use allocator_traits.
    	(_Bvector_base::_Bvector_impl): Use allocator's pointer type.
    	(_Bvector_base::_M_end_addr()): Convert to raw pointer.
    	(vector<bool>): Use allocator_traits and _M_end_addr. Add allocator
    	extended constructors.
    	* include/bits/vector.tcc (vector<bool>): Use allocator_traits and
    	_M_end_addr.
    	* testsuite/23_containers/vector/bool/allocator/copy.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/minimal.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/noexcept.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/copy_assign.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/move.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/swap.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/ext_ptr.cc: New.
    	* testsuite/23_containers/vector/bool/allocator/move_assign.cc: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@216988 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/23_containers/vector/bool/allocator/move.cc
@@ -0,0 +1,59 @@
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++11" }
+
+#include <vector>
+#include <testsuite_hooks.h>
+#include <testsuite_allocator.h>
+
+using T = bool;
+
+using __gnu_test::uneq_allocator;
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  typedef uneq_allocator<T> alloc_type;
+  typedef std::vector<T, alloc_type> test_type;
+  test_type v1(alloc_type(1));
+  v1 = { T() };
+  auto it = v1.begin();
+  test_type v2(std::move(v1));
+  VERIFY(1 == v1.get_allocator().get_personality());
+  VERIFY(1 == v2.get_allocator().get_personality());
+  VERIFY( it == v2.begin() );
+}
+
+void test02()
+{
+  bool test __attribute__((unused)) = true;
+  typedef uneq_allocator<T> alloc_type;
+  typedef std::vector<T, alloc_type> test_type;
+  test_type v1(alloc_type(1));
+  v1 = { T() };
+  test_type v2(std::move(v1), alloc_type(2));
+  VERIFY(1 == v1.get_allocator().get_personality());
+  VERIFY(2 == v2.get_allocator().get_personality());
+}
+
+int main()
+{
+  test01();
+  test02();
+  return 0;
+}
