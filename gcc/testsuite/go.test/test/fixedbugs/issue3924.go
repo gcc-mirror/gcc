b80ff967a3e63a916a33f690204e06e869ac8eea
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-04 17:49:01 +0000

    compiler: Logical operators should yield untyped bool for untyped operands.
    Fixes issue 6671 for gccgo.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@217094 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/go.test/test/fixedbugs/issue3924.go
+++ gcc/testsuite/go.test/test/fixedbugs/issue3924.go
@@ -1,4 +1,4 @@
-// errorcheck
+// compile
 
 // Copyright 2012 The Go Authors.  All rights reserved.
 // Use of this source code is governed by a BSD-style
@@ -9,5 +9,5 @@ package foo
 type mybool bool
 
 var x, y = 1, 2
-var _ mybool = x < y && x < y // ERROR "cannot use"
-var _ mybool = x < y || x < y // ERROR "cannot use"
+var _ mybool = x < y && x < y
+var _ mybool = x < y || x < y
