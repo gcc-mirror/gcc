3ff9b352df898807b0c8608a74cfb89b1626d38a
Hale Wang <hale.wang@arm.com>
2014-11-06 05:38:45 +0000

    arm.c: Add cortex-m7 tune.
    2014-10-11  Hale Wang  <Hale.Wang@arm.com>
    
    	* config/arm/arm.c: Add cortex-m7 tune.
    	* config/arm/arm-cores.def: Use cortex-m7 tune.
    
    From-SVN: r217173

--- gcc/config/arm/arm.c
+++ gcc/config/arm/arm.c
@@ -2022,6 +2022,27 @@ const struct tune_params arm_v7m_tune =
   8						/* Maximum insns to inline memset.  */
 };
 
+/* Cortex-M7 tuning.  */
+
+const struct tune_params arm_cortex_m7_tune =
+{
+  arm_9e_rtx_costs,
+  &v7m_extra_costs,
+  NULL,						/* Sched adj cost.  */
+  0,						/* Constant limit.  */
+  0,						/* Max cond insns.  */
+  ARM_PREFETCH_NOT_BENEFICIAL,
+  true,						/* Prefer constant pool.  */
+  arm_cortex_m_branch_cost,
+  false,					/* Prefer LDRD/STRD.  */
+  {true, true},					/* Prefer non short circuit.  */
+  &arm_default_vec_cost,                        /* Vectorizer costs.  */
+  false,                                        /* Prefer Neon for 64-bits bitops.  */
+  false, false,                                 /* Prefer 32-bit encodings.  */
+  false,					/* Prefer Neon for stringops.  */
+  8						/* Maximum insns to inline memset.  */
+};
+
 /* The arm_v6m_tune is duplicated from arm_cortex_tune, rather than
    arm_v6t2_tune. It is used for cortex-m0, cortex-m1 and cortex-m0plus.  */
 const struct tune_params arm_v6m_tune =
