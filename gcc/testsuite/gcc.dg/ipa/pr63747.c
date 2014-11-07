b90dc9e916b8bd8e36c35bf1127a4ea81e04eb6a
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-07 12:35:43 +0000

    PR ipa/63747 	* gcc.dg/ipa/pr63747.c: New test. 	* ipa-icf-gimple.c (func_checker::compare_gimple_switch): 	Missing checking for CASE_LOW and CASE_HIGH added.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@217219 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/ipa/pr63747.c
@@ -0,0 +1,40 @@
+/* { dg-options "-O2 -fdump-ipa-icf" } */
+/* { dg-do run } */
+
+static int __attribute__((noinline))
+foo(int i)
+{
+  switch (i)
+  {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+      return 0;
+    default:
+      return 1;
+  }
+}
+
+static int __attribute__((noinline))
+bar(int i)
+{
+  switch (i)
+  {
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+      return 0;
+    default:
+      return 1;
+  }
+}
+
+int main()
+{
+  return foo(0) + bar(4);
+}
+
+/* { dg-final { scan-ipa-dump "Equal symbols: 0" "icf"  } } */
+/* { dg-final { cleanup-ipa-dump "icf" } } */
