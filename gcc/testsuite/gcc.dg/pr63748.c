c7488fba456957f70edd5a6bb0464fe336f694c5
Patrick Palka <ppalka@gcc.gnu.org>
2014-11-10 20:43:40 +0000

    re PR tree-optimization/63748 (wrong may be used uninitialized warning (abnormal edges))
    2014-11-10  Patrick Palka  <ppalka@gcc.gnu.org>
    
    gcc/
    	PR middle-end/63748
    	* tree-ssa-propagate.c (may_propagate_copy): Allow propagating
    	SSA copies whose source and destination names both occur in
    	abnormal PHIs.
    
    gcc/testsuite/
    	PR middle-end/63748
    	* gcc.dg/pr63748.c: New testcase.
    
    From-SVN: r217317

---
+++ gcc/testsuite/gcc.dg/pr63748.c
@@ -0,0 +1,36 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -Wall" } */
+
+#include <setjmp.h>
+
+jmp_buf *alloc_jmp_buf ();
+int foo (void *);
+
+int
+test (int op, int noside)
+{
+  void *argvec = 0;
+
+  if (op)
+    {
+      jmp_buf *buf = alloc_jmp_buf (); /* { dg-bogus "uninitialized" } */
+      setjmp (*buf);
+
+      if (noside)
+        goto nosideret;
+
+    do_call_it:
+
+      if (noside)
+        goto nosideret;
+
+      return foo (argvec);
+    }
+
+  argvec = __builtin_alloca (1);
+  goto do_call_it;
+
+nosideret:
+  return 1;
+}
+
