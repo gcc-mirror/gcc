24c4f18d7028e31f9bf1e434c59b13f389d8e7e4
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-10 22:44:07 +0000

    Fix std::deque move construction with non-equal allocators.
    	* include/bits/stl_deque.h (_Deque_base::_Deque_base(_Deque_base&&)):
    	Dispatch according to whether allocators are always equal.
    	(_Deque_base::_M_move_impl()): Implement move-from state.
    	* testsuite/23_containers/deque/requirements/dr438/assign_neg.cc: Fix
    	dg-error line number.
    	* testsuite/23_containers/deque/requirements/dr438/
    	constructor_1_neg.cc: Likewise.
    	* testsuite/23_containers/deque/requirements/dr438/
    	constructor_2_neg.cc: Likewise.
    	* testsuite/23_containers/deque/requirements/dr438/insert_neg.cc:
    	Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@217322 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/23_containers/deque/requirements/dr438/insert_neg.cc
+++ libstdc++-v3/testsuite/23_containers/deque/requirements/dr438/insert_neg.cc
@@ -18,7 +18,7 @@
 // <http://www.gnu.org/licenses/>.
 
 // { dg-do compile }
-// { dg-error "no matching" "" { target *-*-* } 1943 }
+// { dg-error "no matching" "" { target *-*-* } 1965 }
 
 #include <deque>
 
