097a1c0bcf82a656ccc50121c800cb1b9ce16d5f
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-10 23:29:59 +0000

    2014-11-11  Uros Bizjak  <ubizjak@gmail.com>
    	Revert:
    	2014-10-31  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/63620
    	* config/i386/i386-protos.h (ix86_use_pseudo_pic_reg): Declare.
    	* config/i386/i386.c (ix86_use_pseudo_pic_reg): Export.
    	* config/i386/i386.md (*pushtf): Allow only CONST_DOUBLEs that won't
    	be reloaded through memory.
    	(*pushxf): Ditto.
    	(*pushdf): Ditto.
    
    testsuite/ChangeLog:
    
    2014-11-11  Uros Bizjak  <ubizjak@gmail.com>
    	    Ilya Enkovich  <ilya.enkovich@intel.com>
    
    	PR target/63620
    	* gcc.target/i386/pr63620.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@217325 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr63620.c
@@ -0,0 +1,20 @@
+/* PR target/63620 */
+/* { dg-do compile } */
+/* { dg-require-effective-target fpic } */
+/* { dg-require-effective-target ia32 } */
+/* { dg-options "-O2 -fpic -mfpmath=sse -msse" } */
+
+static const __float128 cf = 0.1E+30Q;
+
+typedef __float128 (*func)(__float128 x);
+
+__float128
+test (__float128 x, int p, func f)
+{
+  x = f (x);
+  if (p)
+    x = f (cf);
+  return x;
+}
+
+/* { dg-final { scan-assembler "get_pc_thunk" } } */
