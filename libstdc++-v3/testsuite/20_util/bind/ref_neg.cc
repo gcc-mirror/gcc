847e9cf81e5101f327e4cd5ac629df93b0afb581
Jonathan Wakely <jwakely@redhat.com>
2014-11-11 23:57:35 +0000

    Define __void_t and SFINAE-friendly iterator_traits.
    	* include/std/type_traits (__void_t): Define new alias template.
    	(_GLIBCXX_HAS_NESTED_TYPE): Redefine using __void_t.
    	* include/std/functional (_Maybe_get_result_type): Likewise.
    	* include/bits/stl_iterator_base_types.h (__iterator_traits): Likewise.
    	* include/bits/uses_allocator.h (__uses_allocator_helper): Likewise.
    	* testsuite/20_util/bind/ref_neg.cc: Adjust dg-error.
    	* testsuite/20_util/reference_wrapper/typedefs-3.cc: Adjust to changes
    	in _GLIBCXX_HAS_NESTED_TYPE.
    
    From-SVN: r217395

--- libstdc++-v3/testsuite/20_util/bind/ref_neg.cc
+++ libstdc++-v3/testsuite/20_util/bind/ref_neg.cc
@@ -30,10 +30,10 @@ void test01()
 {
   const int dummy = 0;
   std::bind(&inc, _1)(0);               // { dg-error  "no match" }
-  // { dg-error "rvalue|const" "" { target *-*-* } 1126 }
-  // { dg-error "rvalue|const" "" { target *-*-* } 1140 }
-  // { dg-error "rvalue|const" "" { target *-*-* } 1154 }
-  // { dg-error "rvalue|const" "" { target *-*-* } 1168 }
+  // { dg-error "rvalue|const" "" { target *-*-* } 1125 }
+  // { dg-error "rvalue|const" "" { target *-*-* } 1139 }
+  // { dg-error "rvalue|const" "" { target *-*-* } 1153 }
+  // { dg-error "rvalue|const" "" { target *-*-* } 1167 }
   std::bind(&inc, std::ref(dummy))();	// { dg-error  "no match" }
 }
 
