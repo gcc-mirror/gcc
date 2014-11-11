c8824f2ccb4bc1687a86c87a090c3589bce2053e
James Greenhalgh <james.greenhalgh@arm.com>
2014-11-11 17:37:35 +0000

    [Patch AArch64] Fix up BSL expander for floating point types
    gcc/
    
    	* config/aarch64/aarch64-simd.md
    	(aarch64_simd_bsl<mode>_internal): Remove float cases, canonicalize.
    	(aarch64_simd_bsl<mode>): Add gen_lowpart expressions where we
    	are punning between float vectors and integer vectors.
    
    gcc/testsuite/
    
    	* gcc.target/aarch64/vbslq_f64_1.c: New.
    	* gcc.target/aarch64/vbslq_f64_2.c: Likewise.
    	* gcc.target/aarch64/vbslq_u64_1.c: Likewise.
    	* gcc.target/aarch64/vbslq_u64_2.c: Likewise.
    
    From-SVN: r217362

---
+++ gcc/testsuite/gcc.target/aarch64/vbslq_f64_1.c
@@ -0,0 +1,21 @@
+/* Test vbslq_f64 can be folded.  */
+/* { dg-do assemble } */
+/* { dg-options "--save-temps -O3" } */
+
+#include <arm_neon.h>
+
+/* Folds to ret.  */
+
+float32x4_t
+fold_me (float32x4_t a, float32x4_t b)
+{
+  uint32x4_t mask = {-1, -1, -1, -1};
+  return vbslq_f32 (mask, a, b);
+}
+
+/* { dg-final { scan-assembler-not "bsl\\tv" } } */
+/* { dg-final { scan-assembler-not "bit\\tv" } } */
+/* { dg-final { scan-assembler-not "bif\\tv" } } */
+
+/* { dg-final { cleanup-saved-temps } } */
+
