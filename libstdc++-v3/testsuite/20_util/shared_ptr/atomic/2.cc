ff02021b3f6a1ac9318485f3e917e235358489c2
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-12 23:55:11 +0000

    std::shared_ptr atomic operations
    	PR libstdc++/57250
    	* config/abi/pre/gnu.ver: Export new symbols.
    	* include/Makefile.am: Add new header.
    	* include/Makefile.in: Regenerate.
    	* include/bits/shared_ptr_atomic.h: Define atomic access functions.
    	* include/std/memory: Include new header.
    	* src/c++11/shared_ptr.cc (_Sp_locker): Define and use mutex pool.
    	* testsuite/20_util/shared_ptr/atomic/1.cc: New.
    	* testsuite/20_util/shared_ptr/atomic/2.cc: New.
    	* testsuite/20_util/shared_ptr/atomic/3.cc: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@217452 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/20_util/shared_ptr/atomic/2.cc
@@ -0,0 +1,40 @@
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++11" }
+// { dg-require-gthreads "" }
+
+#include <memory>
+
+int main()
+{
+  using test_type = std::__shared_ptr<int, std::_S_single>;
+  bool test __attribute__ ((unused)) = false;
+  constexpr auto mo = std::memory_order_seq_cst;
+  const test_type p;
+  test = std::atomic_is_lock_free(&p);
+  test_type p2 = std::atomic_load(&p);
+  test_type p3 = std::atomic_load_explicit(&p, mo);
+  std::atomic_store(&p2, p);
+  std::atomic_store_explicit(&p2, p, mo);
+  test_type p4 = std::atomic_exchange(&p2, p);
+  p4 = std::atomic_exchange_explicit(&p2, p, mo);
+  test = std::atomic_compare_exchange_weak(&p2, &p3, p);
+  test = std::atomic_compare_exchange_strong(&p2, &p3, p);
+  test = std::atomic_compare_exchange_weak_explicit(&p2, &p3, p, mo, mo);
+  test = std::atomic_compare_exchange_strong_explicit(&p2, &p3, p, mo, mo);
+}
---
+++ libstdc++-v3/testsuite/20_util/shared_ptr/atomic/3.cc
@@ -0,0 +1,53 @@
+// { dg-do run { target *-*-freebsd* *-*-dragonfly* *-*-netbsd* *-*-linux* *-*-gnu* *-*-solaris* *-*-cygwin *-*-darwin* powerpc-ibm-aix* } }
+// { dg-options " -std=gnu++11 -pthread" { target *-*-freebsd* *-*-dragonfly* *-*-netbsd* *-*-linux* *-*-gnu* powerpc-ibm-aix* } }
+// { dg-options " -std=gnu++11 -pthreads" { target *-*-solaris* } }
+// { dg-options " -std=gnu++11 " { target *-*-cygwin *-*-darwin* } }
+// { dg-require-cstdint "" }
+// { dg-require-gthreads "" }
+
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <thread>
+#include <memory>
+#include <testsuite_hooks.h>
+
+struct leaddock
+{
+  ~leaddock();
+};
+
+std::shared_ptr<leaddock> global;
+
+leaddock::~leaddock()
+{
+  // If this destructor is called "inside" an atomic operation on global it
+  // will deadlock, so this checks that the atomic_store is done atomically.
+  auto copy = std::atomic_load(&global);
+  VERIFY( !copy );
+}
+
+void f()
+{
+  std::atomic_store(&global, std::make_shared<leaddock>());
+  std::atomic_store(&global, {});
+}
+
+int main()
+{
+  std::thread{ f }.join();
+}
