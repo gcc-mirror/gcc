d300d4c4ab09dfe15de988a153a06f41ca204ff6
H.J. Lu <hongjiu.lu@intel.com>
2014-11-12 02:05:06 +0000

    Fix dump scanning in g++.dg/ipa/devirt-42.C
    	* g++.dg/ipa/devirt-42.C: Fix dump scanning for "return 2".
    
    From-SVN: r217402

--- gcc/testsuite/g++.dg/ipa/devirt-42.C
+++ gcc/testsuite/g++.dg/ipa/devirt-42.C
@@ -31,7 +31,7 @@ main()
 /* { dg-final { scan-ipa-dump-times "Discovered a virtual call to a known target" 2 "inline"  } } */
 
 /* Verify that speculation is optimized by late optimizers.  */
-/* { dg-final { scan-tree-dump-times "return 2" 2 "optimized"  } } */
+/* { dg-final { scan-tree-dump-times "return 2" 3 "optimized"  } } */
 /* { dg-final { scan-tree-dump-not "OBJ_TYPE_REF" "optimized"  } } */
 
 /* { dg-final { cleanup-ipa-dump "inline" } } */
