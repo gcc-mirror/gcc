3cd383fd6d32752db39734c5fd084e0ca7c08aed
Alexander Ivchenko <alexander.ivchenko@intel.com>
2014-11-13 09:08:18 +0000

    target-supports.exp (error_h): New check.
    gcc/testsuite/
    	* lib/target-supports.exp (error_h): New check.
    	(libc_has_complex_functions): Ditto.
    	(tgmath_h): Ditto.
    	* gcc.dg/builtins-59.c: Add libc_has_complex_functions check.
    	* gcc.dg/builtins-61.c: Likewise.
    	* gcc.dg/builtins-67.c: Disable test for Bionic.
    	* gcc.dg/strlenopt-14g.c: Likewise.
    	* gcc.dg/strlenopt-14gf.c: Likewise.
    	* gcc.dg/c99-tgmath-1.c: Add tgmath_h check.
    	* gcc.dg/c99-tgmath-2.c: Likewise.
    	* gcc.dg/c99-tgmath-3.c: Likewise.
    	* gcc.dg/c99-tgmath-4.c: Likewise.
    	* gcc.dg/dfp/convert-dfp-round-thread.c: Add error_h check.
    
    From-SVN: r217467

--- gcc/testsuite/gcc.dg/strlenopt-14g.c
+++ gcc/testsuite/gcc.dg/strlenopt-14g.c
@@ -1,6 +1,8 @@
 /* This test needs runtime that provides stpcpy and mempcpy functions.  */
 /* { dg-do run { target *-*-linux* *-*-gnu* } } */
 /* { dg-options "-O2 -fdump-tree-strlen" } */
+/* Bionic targets don't have mempcpy */
+/* { dg-require-effective-target non_bionic } */
 
 #define USE_GNU
 #include "strlenopt.h"
