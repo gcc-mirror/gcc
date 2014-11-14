809ccc6ca489412356cbebf2134adf1475e8b5f4
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-14 07:30:27 +0000

    * configure.ac (--with-diagnostics-color): New configure 	option, default to --with-diagnostics-color=auto. 	* toplev.c (process_options): Use DIAGNOSTICS_COLOR_DEFAULT 	to determine -fdiagnostics-color= option default. 	* doc/invoke.texi (-fdiagnostics-color=): Document new 	default. 	* configure: Regenerated. 	* config.in: Regenerated.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@217540 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/toplev.c
+++ gcc/toplev.c
@@ -1267,12 +1267,28 @@ process_options (void)
 
   maximum_field_alignment = initial_max_fld_align * BITS_PER_UNIT;
 
-  /* Default to -fdiagnostics-color=auto if GCC_COLORS is in the environment,
-     otherwise default to -fdiagnostics-color=never.  */
-  if (!global_options_set.x_flag_diagnostics_show_color
-      && getenv ("GCC_COLORS"))
-    pp_show_color (global_dc->printer)
-      = colorize_init (DIAGNOSTICS_COLOR_AUTO);
+  /* If DIAGNOSTICS_COLOR_DEFAULT is -1, default to -fdiagnostics-color=auto
+     if GCC_COLORS is in the environment, otherwise default to
+     -fdiagnostics-color=never, for other values default to that
+     -fdiagnostics-color={never,auto,always}.  */
+  if (!global_options_set.x_flag_diagnostics_show_color)
+    switch ((int) DIAGNOSTICS_COLOR_DEFAULT)
+      {
+      case -1:
+	if (!getenv ("GCC_COLORS"))
+	  break;
+	/* FALLTHRU */
+      case DIAGNOSTICS_COLOR_AUTO:
+	pp_show_color (global_dc->printer)
+	  = colorize_init (DIAGNOSTICS_COLOR_AUTO);
+	break;
+      case DIAGNOSTICS_COLOR_YES:
+	pp_show_color (global_dc->printer)
+	  = colorize_init (DIAGNOSTICS_COLOR_YES);
+	break;
+      default:
+	break;
+      }
 
   /* Allow the front end to perform consistency checks and do further
      initialization based on the command line options.  This hook also
