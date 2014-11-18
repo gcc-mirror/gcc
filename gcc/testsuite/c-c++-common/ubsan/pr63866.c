530273ed65ede88c762ccb88782453d0c178da08
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-18 14:55:44 +0000

    PR sanitizer/63866 	* asan.c (asan_global_struct): Create a TYPE_DECL for "__asan_global", 	put it into TYPE_NAME and TYPE_STUB_DECL. 	* ubsan.c (ubsan_type_descriptor_type): New variable. 	Function renamed to ... 	(ubsan_get_type_descriptor_type): ... this.  Cache 	return value in ubsan_type_descriptor_type variable. 	Create a TYPE_DECL for "__ubsan_type_descriptor", put it into 	TYPE_NAME and TYPE_STUB_DECL. 	(ubsan_get_source_location_type): Create a TYPE_DECL for 	"__ubsan_source_location", put it into TYPE_NAME and TYPE_STUB_DECL. 	(ubsan_type_descriptor, ubsan_create_data): Call 	ubsan_get_type_descriptor_type instead of ubsan_type_descriptor_type. 	Create a TYPE_DECL for name, put it into TYPE_NAME and TYPE_STUB_DECL.
    	* c-c++-common/ubsan/pr63866.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@217718 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/c-c++-common/ubsan/pr63866.c
@@ -0,0 +1,11 @@
+/* PR sanitizer/63866 */
+/* { dg-do compile } */
+/* { dg-options "-fsanitize=undefined -fdump-ipa-cgraph" } */
+
+int
+foo (int x, int y)
+{
+  return x + y;
+}
+
+/* { dg-final { cleanup-ipa-dump "cgraph" } } */
