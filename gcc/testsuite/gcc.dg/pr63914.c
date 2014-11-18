bee1d478578db03924bf2505092db9ee967190da
Richard Biener <rguenther@suse.de>
2014-11-18 13:03:46 +0000

    re PR tree-optimization/63914 (ICE in set_lattice_value, at tree-ssa-ccp.c:517)
    2014-11-18  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/63914
    	* tree-ssa-ccp.c (canonicalize_value): Remove float value
    	canonicalization.
    	(valid_lattice_transition): Allow (partial) transition
    	from NaN to non-NaN if !HONOR_NANS.
    	(set_lattice_value): Check for valid lattice transitions
    	only when checking is enabled.
    
    	* gcc.dg/pr63914.c: New testcase.
    
    From-SVN: r217712

---
+++ gcc/testsuite/gcc.dg/pr63914.c
@@ -0,0 +1,45 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -ffast-math" } */
+
+typedef float __m128 __attribute__ ((__vector_size__ (16)));
+__m128 a, d, e;
+int b;
+struct dt_interpolation c;
+__m128
+fn1 (float p1)
+{
+  return (__attribute__ ((__vector_size__ (4 * sizeof 0))) float){ p1 };
+}
+__m128
+fn2 (float p1)
+{
+  return fn1 (p1);
+}
+struct dt_interpolation
+{
+  int width;
+};
+void
+fn3 (struct dt_interpolation *p1, int *p2)
+{
+  int i = 0, n = 0;
+  while (i < 2 * p1->width)
+    n = i++;
+  *p2 = n;
+}
+void
+fn4 ()
+{
+  __m128 f;
+  fn3 (&c, &b);
+  __m128 g = fn2 (1.f / b);
+  e = (__m128){};
+  __m128 h = e;
+  for (int i = 0; i < 2 * c.width; i++)
+    {
+      for (; c.width;)
+	f = a;
+      h = f;
+    }
+  d = h * g;
+}
