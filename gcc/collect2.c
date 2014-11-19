bb7acc769ae43436698e5a22e1a6c778377d578a
tocarip <tocarip@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-19 11:48:48 +0000

    gcc/ 2014-11-19  Ilya Tocar  <ilya.tocar@intel.com>
    	* collect2.c (main): Don't call fatal_error before
    	diagnostic_initialize.
    	* lto-wrapper.c (main): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@217765 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/collect2.c
+++ gcc/collect2.c
@@ -955,15 +955,15 @@ main (int argc, char **argv)
   signal (SIGCHLD, SIG_DFL);
 #endif
 
-  if (atexit (collect_atexit) != 0)
-    fatal_error ("atexit failed");
-
   /* Unlock the stdio streams.  */
   unlock_std_streams ();
 
   gcc_init_libintl ();
 
   diagnostic_initialize (global_dc, 0);
+
+  if (atexit (collect_atexit) != 0)
+    fatal_error ("atexit failed");
 
   /* Do not invoke xcalloc before this point, since locale needs to be
      set first, in case a diagnostic is issued.  */
