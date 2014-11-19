668bfa53cfb345bf60c5812f37b838a8a3f73c7a
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-19 20:38:59 +0000

    PR target/63947 	* config/i386/i386.c (put_condition_code) <case LTU, case GEU>: 	Output "b" and "nb" suffix for FP mode.
    testsuite/ChangeLog:
    
    	PR target/63947
    	* gcc.target/i386/pr63947.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@217810 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -14953,7 +14953,7 @@ put_condition_code (enum rtx_code code, machine_mode mode, bool reverse,
       if (mode == CCmode)
 	suffix = "b";
       else if (mode == CCCmode)
-	suffix = "c";
+	suffix = fp ? "b" : "c";
       else
 	gcc_unreachable ();
       break;
@@ -14976,9 +14976,9 @@ put_condition_code (enum rtx_code code, machine_mode mode, bool reverse,
       break;
     case GEU:
       if (mode == CCmode)
-	suffix = fp ? "nb" : "ae";
+	suffix = "nb";
       else if (mode == CCCmode)
-	suffix = "nc";
+	suffix = fp ? "nb" : "nc";
       else
 	gcc_unreachable ();
       break;
