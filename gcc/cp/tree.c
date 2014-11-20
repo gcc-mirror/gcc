75bda2e8134c013bc3d02b78e5b8b34022961985
Ville Voutilainen <ville.voutilainen@gmail.com>
2014-11-20 23:51:04 +0200

    re PR c++/63959 (G++ misreports volatile int as trivially copyable)
    	PR c++/63959
    	* tree.c (trivially_copyable_p): Check for CP_TYPE_VOLATILE_P.
    
    From-SVN: r217893

--- gcc/cp/tree.c
+++ gcc/cp/tree.c
@@ -3206,7 +3206,7 @@ trivially_copyable_p (const_tree t)
 	    && !TYPE_HAS_COMPLEX_MOVE_ASSIGN (t)
 	    && TYPE_HAS_TRIVIAL_DESTRUCTOR (t));
   else
-    return scalarish_type_p (t);
+    return !CP_TYPE_VOLATILE_P (t) && scalarish_type_p (t);
 }
 
 /* Returns 1 iff type T is a trivial type, as defined in [basic.types] and
