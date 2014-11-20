0cbddb3134dbe71ed7ba7dfd24725e69d2d8f72a
ppalka <ppalka@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-20 22:25:35 +0000

    Disable an unsafe VRP transformation when -fno-strict-overflow is set
    gcc/
    	* tree-vrp.c (test_for_singularity): New parameter
    	strict_overflow_p.  Set *strict_overflow_p to true if signed
    	overflow must be undefined for the return value to satisfy the
    	conditional.
    	(simplify_cond_using_ranges): Don't perform the simplification
    	if it violates overflow rules.
    
    gcc/testsuite/
    	* gcc.dg/no-strict-overflow-8.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@217895 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/no-strict-overflow-8.c
@@ -0,0 +1,25 @@
+/* { dg-do compile } */
+/* { dg-options "-fno-strict-overflow -O2 -fdump-tree-optimized" } */
+
+/* We cannot fold i > 0 because p->a - p->b can be larger than INT_MAX
+   and thus i can wrap.  Dual of Wstrict-overflow-18.c  */
+
+struct c { unsigned int a; unsigned int b; };
+extern void bar (struct c *);
+int
+foo (struct c *p)
+{
+  int i;
+  int sum = 0;
+
+  for (i = 0; i < p->a - p->b; ++i)
+    {
+      if (i > 0)
+	sum += 2;
+      bar (p);
+    }
+  return sum;
+}
+
+/* { dg-final { scan-tree-dump "i_.* > 0" "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
