b7d634fcc388d6e5ed6d85b640f8dc964f763db8
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-21 13:27:57 +0000

    PR debug/63239 	* g++.dg/debug/dwarf2/deleted-member-function.C: Pass 	-gno-strict-dwarf on darwin.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@217918 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/debug/dwarf2/deleted-member-function.C
+++ gcc/testsuite/g++.dg/debug/dwarf2/deleted-member-function.C
@@ -1,6 +1,7 @@
 // { dg-do compile }
 // { dg-options "-O -std=c++11 -g -dA" }
 // { dg-final { scan-assembler-times " DW_AT_GNU_deleted" 2 } }
+// { dg-additional-options "-gno-strict-dwarf" { target { *-*-darwin* } } }
 
 struct Foo
 {
