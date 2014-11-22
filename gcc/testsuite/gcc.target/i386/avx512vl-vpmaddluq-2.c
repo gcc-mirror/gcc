c1280a5c6870537252fd275a63b5518cedfa0002
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-22 08:19:33 +0000

    * gcc.target/i386/avx512vl-vpermb-2.c: Require avx512vbmi. 	* gcc.target/i386/avx512vl-vpermi2b-2.c: Ditto. 	* gcc.target/i386/avx512vl-vpermt2b-2.c: Ditto. 	* gcc.target/i386/avx512vl-vpmaddhuq-2.c: Require avx512ifma. 	* gcc.target/i386/avx512vl-vpmaddluq-2.c: Ditto. 	* gcc.target/i386/avx512vl-vpmultishiftqb-2.c: Ditto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@217960 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/avx512vl-vpmaddluq-2.c
+++ gcc/testsuite/gcc.target/i386/avx512vl-vpmaddluq-2.c
@@ -1,6 +1,7 @@
 /* { dg-do run } */
 /* { dg-options "-O2 -mavx512ifma -mavx512vl -DAVX512VL" } */
 /* { dg-require-effective-target avx512vl } */
+/* { dg-require-effective-target avx512ifma } */
 
 #define AVX512F_LEN 256
 #define AVX512F_LEN_HALF 128
