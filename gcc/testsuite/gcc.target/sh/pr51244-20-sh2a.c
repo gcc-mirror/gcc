10f916167ccf8451fb717689eef61d3895c1f2a2
Oleg Endo <olegendo@gcc.gnu.org>
2014-11-22 15:50:10 +0000

    re PR target/63783 ([SH] Miscompilation of boolean negation on SH4 using -O2)
    gcc/
    	PR target/63783
    	PR target/51244
    	* config/sh/sh_treg_combine.cc (sh_treg_combine::make_not_reg_insn):
    	Do not emit bitwise not insn.  Emit logical not insn sequence instead.
    	Adjust related comments throughout the file.
    
    gcc/testsuite/
    	PR target/63783
    	PR target/51244
    	* gcc.target/sh/torture/pr63783-1.c: New.
    	* gcc.target/sh/torture/pr63783-2.c: New.
    	* gcc.target/sh/pr51244-20.c: Adjust.
    	* gcc.target/sh/pr51244-20-sh2a.c: Adjust.
    
    From-SVN: r217969

--- gcc/testsuite/gcc.target/sh/pr51244-20-sh2a.c
+++ gcc/testsuite/gcc.target/sh/pr51244-20-sh2a.c
@@ -3,12 +3,12 @@
 /* { dg-do compile }  */
 /* { dg-options "-O2" } */
 /* { dg-skip-if "" { "sh*-*-*" } { "*" } { "-m2a*" } } */
-/* { dg-final { scan-assembler-times "tst" 5 } } */
-/* { dg-final { scan-assembler-times "movt" 0 } } */
+/* { dg-final { scan-assembler-times "tst" 6 } } */
+/* { dg-final { scan-assembler-times "movt" 1 } } */
 /* { dg-final { scan-assembler-times "nott" 1 } } */
 /* { dg-final { scan-assembler-times "cmp/eq" 2 } } */
 /* { dg-final { scan-assembler-times "cmp/hi" 4 } } */
 /* { dg-final { scan-assembler-times "cmp/gt" 3 } } */
-/* { dg-final { scan-assembler-times "not\t" 1 } } */
+/* { dg-final { scan-assembler-not "not\t" } } */
 
 #include "pr51244-20.c"
---
+++ gcc/testsuite/gcc.target/sh/torture/pr63783-2.c
@@ -0,0 +1,29 @@
+/* { dg-do run }  */
+/* { dg-additional-options "-std=c99" }  */
+
+#include <assert.h>
+
+long long decision_result;
+long long val;
+int truecount = 0;
+
+static void __attribute__((noinline))
+buggy (int flag)
+{
+  int condition;
+  if(flag == 0)
+    condition = val != 0;
+  else
+    condition = !decision_result;
+  if (condition)
+     truecount++;
+}
+
+int
+main (void)
+{
+  decision_result = 1;
+  buggy(1);
+  assert (truecount == 0);
+  return 0;
+}
