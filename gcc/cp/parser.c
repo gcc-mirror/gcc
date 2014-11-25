18adeccd008553a7634794c9b8a8209619742902
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-25 13:48:49 +0000

    /cp 2014-11-25  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/63786
    	* parser.c (cp_parser_label_for_labeled_statement): Check the case
    	with check_for_bare_parameter_packs.
    
    /testsuite
    2014-11-25  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/63786
    	* g++.dg/cpp0x/variadic163.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@218043 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/parser.c
+++ gcc/cp/parser.c
@@ -9820,14 +9820,17 @@ cp_parser_label_for_labeled_statement (cp_parser* parser, tree attributes)
 	cp_lexer_consume_token (parser->lexer);
 	/* Parse the constant-expression.  */
 	expr = cp_parser_constant_expression (parser);
+	if (check_for_bare_parameter_packs (expr))
+	  expr = error_mark_node;
 
 	ellipsis = cp_lexer_peek_token (parser->lexer);
 	if (ellipsis->type == CPP_ELLIPSIS)
 	  {
 	    /* Consume the `...' token.  */
 	    cp_lexer_consume_token (parser->lexer);
-	    expr_hi =
-	      cp_parser_constant_expression (parser);
+	    expr_hi = cp_parser_constant_expression (parser);
+	    if (check_for_bare_parameter_packs (expr_hi))
+	      expr_hi = error_mark_node;
 
 	    /* We don't need to emit warnings here, as the common code
 	       will do this for us.  */
