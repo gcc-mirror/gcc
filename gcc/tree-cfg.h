1a2a336065391b74c634e35854f382b24cdeaf35
Tom de Vries <tom@codesourcery.com>
2014-11-25 17:10:00 +0000

    Add verify_sese
    2014-11-25  Tom de Vries  <tom@codesourcery.com>
    
    	* tree-cfg.c (verify_sese): New function.
    	(move_sese_region_to_fn): Call verify_sese.
    	* tree-cfg.h (verify_sese): Declare.
    
    From-SVN: r218051

--- gcc/tree-cfg.h
+++ gcc/tree-cfg.h
@@ -73,6 +73,7 @@ extern bool gimple_duplicate_sese_tail (edge, edge, basic_block *, unsigned,
 				      basic_block *);
 extern void gather_blocks_in_sese_region (basic_block entry, basic_block exit,
 					  vec<basic_block> *bbs_p);
+extern void verify_sese (basic_block, basic_block, vec<basic_block> *);
 extern basic_block move_sese_region_to_fn (struct function *, basic_block,
 				           basic_block, tree);
 extern void dump_function_to_file (tree, FILE *, int);
