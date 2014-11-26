90acc3c9005d639e85a1fe7a24027f7936b8deed
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-26 16:06:53 +0000

    2014-11-26  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/59114
    	* g++.dg/cpp0x/auto43.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@218090 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/auto43.C
@@ -0,0 +1,12 @@
+// PR c++/59114
+// { dg-do compile { target c++11 } }
+
+template<int> struct A
+{
+  template<typename T> operator T();
+};
+
+void foo()
+{
+  A<0>().operator auto();  // { dg-error "invalid use of .auto" }
+}
