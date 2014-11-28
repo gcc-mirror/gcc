14b62468d4e322c874548fff364bcc6a533b1555
ktkachov <ktkachov@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-28 12:17:40 +0000

    [ARM] Add -mcpu=cortex-a17.cortex-a7
    	* config/arm/arm-cores.def (cortex-a17.cortex-a7): New entry.
    	* config/arm/arm-tables.opt: Regenerate.
    	* config/arm/arm-tune.md: Regenerate.
    	* config/arm/bpabi.h (BE8_LINK_SPEC): Add mcpu=cortex-a17.cortex-a7.
    	* config/arm/t-aprofile: Add cortex-a17.cortex-a7 entry to
    	MULTILIB_MATCHES.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@218146 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/arm/bpabi.h
+++ gcc/config/arm/bpabi.h
@@ -66,6 +66,7 @@
    |mcpu=cortex-a8|mcpu=cortex-a9|mcpu=cortex-a15       \
    |mcpu=cortex-a12|mcpu=cortex-a17			\
    |mcpu=cortex-a15.cortex-a7				\
+   |mcpu=cortex-a17.cortex-a7				\
    |mcpu=marvell-pj4					\
    |mcpu=cortex-a53					\
    |mcpu=cortex-a57					\
@@ -87,6 +88,7 @@
    |mcpu=cortex-a8|mcpu=cortex-a9|mcpu=cortex-a15       \
    |mcpu=cortex-a12|mcpu=cortex-a17			\
    |mcpu=cortex-a15.cortex-a7				\
+   |mcpu=cortex-a17.cortex-a7				\
    |mcpu=cortex-a53					\
    |mcpu=cortex-a57					\
    |mcpu=cortex-a57.cortex-a53				\
