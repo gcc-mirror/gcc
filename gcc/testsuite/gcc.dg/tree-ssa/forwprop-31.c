7e015fcefe33eded9a565e7e2ad3da11952249ae
Richard Biener <rguenther@suse.de>
2014-11-28 08:57:43 +0000

    re PR tree-optimization/64084 (match-and-simplify prefers complex matches)
    2014-11-28  Richard Biener  <rguenther@suse.de>
    
    	PR middle-end/64084
    	* genmatch.c (dt_node::gen_kids_1): New function, split out
    	from dt_node::gen_kids.
    	(decision_tree::cmp_node): DT_TRUE are generally not equal.
    	(decision_tree::find_node): Treat DT_TRUE as barrier for
    	node CSE on the same level.
    	(dt_node::append_node): Do not keep DT_TRUE last.
    	(dt_node::gen_kids): Emit code after each DT_TRUE node seen.
    
    	* gcc.dg/tree-ssa/ssa-ccp-34.c: New testcase.
    	* gcc.dg/tree-ssa/forwprop-31.c: Likewise.
    
    From-SVN: r218141

---
+++ gcc/testsuite/gcc.dg/tree-ssa/forwprop-31.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fno-tree-ccp -fdump-tree-forwprop1" } */
+
+int foo (int x)
+{
+  int y = 0;
+  int z = x + 1;
+  int w = z + y; /* becomes z */
+  return w - z; /* becomes 0 */
+}
+
+/* The original y = 0 stmt is also retained.  */
+/* { dg-final { scan-tree-dump-times "= 0;" 2 "forwprop1" } } */
+/* { dg-final { scan-tree-dump-times "-" 0 "forwprop1" } } */
+/* { dg-final { scan-tree-dump-times "\\+" 1 "forwprop1" } } */
+/* { dg-final { cleanup-tree-dump "forwprop1" } } */
