c306cfafefeb6e7b224cff163222c02f8def25f1
Richard Biener <rguenther@suse.de>
2014-12-01 13:13:28 +0000

    re PR tree-optimization/15346 ([tree-ssa] combine two successive divisions)
    2014-12-01  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/15346
    	* Makefile.in (gimple-match.o-warn): Remove -Wno-unused-parameter,
    	add -Wno-unused-but-set-variable.
    	* match.pd: Combine two successive divisions.
    
    	* gcc.dg/tree-ssa/forwprop-32.c: New testcase.
    
    From-SVN: r218211

---
+++ gcc/testsuite/gcc.dg/tree-ssa/forwprop-32.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-forwprop1 -fdump-tree-ccp1" } */
+
+int foo (int x)
+{
+  int tem = x / 3;
+  return tem / 5;
+}
+int bar (int x)
+{
+  int tem = x / 3;
+  return tem / (__INT_MAX__ / 2);
+}
+
+/* { dg-final { scan-tree-dump "x_.\\(D\\) / 15" "forwprop1" } } */
+/* { dg-final { scan-tree-dump "return 0;" "ccp1" } } */
+/* { dg-final { cleanup-tree-dump "forwprop1" } } */
+/* { dg-final { cleanup-tree-dump "ccp1" } } */
