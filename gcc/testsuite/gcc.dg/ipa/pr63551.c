7e4928177912d603df174e3ce58ef290c1cc6322
Martin Jambor <mjambor@suse.cz>
2014-12-01 13:52:58 +0100

    re PR tree-optimization/63551 (wrong code (segfaults) at -Os on x86_64-linux-gnu)
    2014-12-01  Martin Jambor  <mjambor@suse.cz>
    
    	PR ipa/63551
    	* ipa-inline-analysis.c (evaluate_conditions_for_known_args): Convert
    	value of the argument to the type of the value in the condition.
    
    testsuite/
    	* gcc.dg/ipa/pr63551.c: New test.
    	* gcc.dg/ipa/pr64041.c: Likewise.
    
    From-SVN: r218208

---
+++ gcc/testsuite/gcc.dg/ipa/pr63551.c
@@ -0,0 +1,33 @@
+/* { dg-do run } */
+/* { dg-options "-Os" } */
+
+union U
+{
+  unsigned int f0;
+  int f1;
+};
+
+int a, d;
+
+void
+fn1 (union U p)
+{
+  if (p.f1 <= 0)
+    if (a)
+      d = 0;
+}
+
+void
+fn2 ()
+{
+  d = 0;
+  union U b = { 4294967286 };
+  fn1 (b);
+}
+
+int
+main ()
+{
+  fn2 ();
+  return 0;
+}
