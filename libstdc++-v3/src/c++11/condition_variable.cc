2761faca6e7f580cec532de505e90ca534b3d264
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-12-02 01:51:25 +0000

    Define *_at_thread_exit() functions.
    	* config/abi/pre/gnu.ver: Add new exports.
    	* include/std/condition_variable (notify_all_at_thread_exit): Declare.
    	(__at_thread_exit_elt): New base class.
    	* include/std/future: Add comments documenting the implementation.
    	(__future_base::_State_baseV2::_State_baseV2()): Use brace-or-equal
    	initializers and define constructor as defaulted.
    	(__future_base::_State_baseV2::_M_ready): Replace member function
    	with member variable.
    	(__future_base::_State_baseV2::_M_set_result): Set _M_ready.
    	(__future_base::_State_baseV2::_M_set_delayed_result): Define.
    	(__future_base::_State_baseV2::_M_break_promise): Set _M_ready.
    	(__future_base::_State_baseV2::_Make_ready): New helper class.
    	(__future_base::_Deferred_state::_M_has_deferred): Remove requirement
    	for caller to own mutex.
    	(__future_base::_Async_state_impl::~_Async_state_impl): Call join
    	directly.
    	(__future_base::_Task_state_base::_M_run): Take arguments by
    	reference.
    	(__future_base::_Task_state_base::_M_run_delayed): Declare new pure
    	virtual function.
    	(__future_base::_Task_state::_M_run_delayed): Define override.
    	(promise::set_value_at_thread_exit): Define.
    	(promise::set_exception_at_thread_exit): Define.
    	(packaged_task::make_ready_at_thread_exit): Define.
    	* src/c++11/condition_variable.cc (notify_all_at_thread_exit): Define.
    	* src/c++11/future.cc
    	(__future_base::_State_baseV2::_Make_ready::_M_set): Define.
    	* testsuite/30_threads/condition_variable/members/3.cc: New.
    	* testsuite/30_threads/packaged_task/members/at_thread_exit.cc: New.
    	* testsuite/30_threads/promise/members/at_thread_exit.cc: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@218255 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/src/c++11/condition_variable.cc
+++ libstdc++-v3/src/c++11/condition_variable.cc
@@ -77,6 +77,80 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       __throw_system_error(__e);
   }
 
+  extern void
+  __at_thread_exit(__at_thread_exit_elt*);
+
+  namespace
+  {
+    __gthread_key_t key;
+
+    void run(void* p)
+    {
+      auto elt = (__at_thread_exit_elt*)p;
+      while (elt)
+	{
+	  auto next = elt->_M_next;
+	  elt->_M_cb(elt);
+	  elt = next;
+	}
+    }
+
+    void run()
+    {
+      auto elt = (__at_thread_exit_elt*)__gthread_getspecific(key);
+      __gthread_setspecific(key, nullptr);
+      run(elt);
+    }
+
+    struct notifier final : __at_thread_exit_elt
+    {
+      notifier(condition_variable& cv, unique_lock<mutex>& l)
+      : cv(&cv), mx(l.release())
+      {
+	_M_cb = &notifier::run;
+	__at_thread_exit(this);
+      }
+
+      ~notifier()
+      {
+	mx->unlock();
+	cv->notify_all();
+      }
+
+      condition_variable* cv;
+      mutex* mx;
+
+      static void run(void* p) { delete static_cast<notifier*>(p); }
+    };
+
+
+    void key_init() {
+      struct key_s {
+	key_s() { __gthread_key_create (&key, run); }
+	~key_s() { __gthread_key_delete (key); }
+      };
+      static key_s ks;
+      // Also make sure the callbacks are run by std::exit.
+      std::atexit (run);
+    }
+  }
+
+  void
+  __at_thread_exit(__at_thread_exit_elt* elt)
+  {
+    static __gthread_once_t once = __GTHREAD_ONCE_INIT;
+    __gthread_once (&once, key_init);
+
+    elt->_M_next = (__at_thread_exit_elt*)__gthread_getspecific(key);
+    __gthread_setspecific(key, elt);
+  }
+
+  void
+  notify_all_at_thread_exit(condition_variable& cv, unique_lock<mutex> l)
+  {
+    (void) new notifier{cv, l};
+  }
+
 _GLIBCXX_END_NAMESPACE_VERSION
 } // namespace
 
---
+++ libstdc++-v3/testsuite/30_threads/packaged_task/members/at_thread_exit.cc
@@ -0,0 +1,61 @@
+// { dg-do run { target *-*-freebsd* *-*-dragonfly* *-*-netbsd* *-*-linux* *-*-gnu* *-*-solaris* *-*-cygwin *-*-darwin* powerpc-ibm-aix* } }
+// { dg-options " -std=gnu++11 -pthread" { target *-*-freebsd* *-*-dragonfly* *-*-netbsd* *-*-linux* *-*-gnu* powerpc-ibm-aix* } }
+// { dg-options " -std=gnu++11 -pthreads" { target *-*-solaris* } }
+// { dg-options " -std=gnu++11 " { target *-*-cygwin *-*-darwin* } }
+// { dg-require-cstdint "" }
+// { dg-require-gthreads "" }
+// { dg-require-atomic-builtins "" }
+
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+
+#include <future>
+#include <testsuite_hooks.h>
+
+bool executed = false;
+
+int execute(int i) { executed = true; return i + 1; }
+
+std::future<int> f1;
+
+bool ready(std::future<int>& f)
+{
+  return f.wait_for(std::chrono::milliseconds(1)) == std::future_status::ready;
+}
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  std::packaged_task<int(int)> p1(execute);
+  f1 = p1.get_future();
+
+  p1.make_ready_at_thread_exit(1);
+
+  VERIFY( executed );
+  VERIFY( p1.valid() );
+  VERIFY( !ready(f1) );
+}
+
+int main()
+{
+  std::thread t{test01};
+  t.join();
+  VERIFY( ready(f1) );
+  VERIFY( f1.get() == 2 );
+}
