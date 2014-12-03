d31008d7a0d53b431f176aad8dda5498de823122
François Dumont <fdumont@gcc.gnu.org>
2014-12-03 19:47:00 +0000

    re PR libstdc++/13631 (Problems in messages)
    2014-12-03  François Dumont  <fdumont@gcc.gnu.org>
    
    	PR libstdc++/13631
    	* include/bits/codecvt.h (codecvt<char, char, mbstate_t>): friend class
    	std::messages<char>.
    	(codecvt<wchar_t, char, mbstate_t>): friend class
    	std::messages<wchar_t>.
    	* config/locale/gnu/messages_member.h
    	(messages<char>::do_open): Specialized.
    	(messages<char>::do_close): Likewise.
    	(messages<wchar_t>::do_open): Likewise.
    	(messages<wchar_t>::do_close): Likewise.
    	* config/locale/gnu/messages_member.cc:
    	(messages<char>::do_open): Implement. Use bind_textdomain_codeset based
    	on codecvt<char, char, mbstate_t>._M_c_locale_codecvt code set. Use
    	internal cache to keep opened domain name with locale information.
    	(messages<wchar_t>::do_open): Likewise with
    	codecvt<wchar_t, char, mbstate_t>.
    	(messages<char>::do_close): Implement. Clean cache information.
    	(messages<wchar_t>::do_close): Likewise.
    	(get_glibc_msg): New. Use dgettext rather than gettext using cached
    	domain name associated to catalog id.
    	(messages<char>::do_get): Use latter.
    	(messages<wchar_t>::do_get): Likewise and use also cached locale
    	codecvt<wchar_t, char, mbstate_t> facet to convert wchar_t default
    	value to char and the result back to wchar_t.
    	* testsuite/22_locale/messages/13631.cc: New.
    	* testsuite/22_locale/messages/members/char/2.cc: Use also fr_FR locale
    	for charset conversion to get the expected accented character.
    
    From-SVN: r218329

--- libstdc++-v3/config/locale/gnu/messages_members.h
+++ libstdc++-v3/config/locale/gnu/messages_members.h
@@ -41,53 +41,53 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 
   // Non-virtual member functions.
   template<typename _CharT>
-     messages<_CharT>::messages(size_t __refs)
-     : facet(__refs), _M_c_locale_messages(_S_get_c_locale()), 
-       _M_name_messages(_S_get_c_name())
-     { }
+    messages<_CharT>::messages(size_t __refs)
+    : facet(__refs), _M_c_locale_messages(_S_get_c_locale()),
+      _M_name_messages(_S_get_c_name())
+    { }
 
   template<typename _CharT>
-     messages<_CharT>::messages(__c_locale __cloc, const char* __s, 
-				size_t __refs) 
-     : facet(__refs), _M_c_locale_messages(0), _M_name_messages(0)
-     {
-       if (__builtin_strcmp(__s, _S_get_c_name()) != 0)
-	 {
-	   const size_t __len = __builtin_strlen(__s) + 1;
-	   char* __tmp = new char[__len];
-	   __builtin_memcpy(__tmp, __s, __len);
-	   _M_name_messages = __tmp;
-	 }
-       else
-	 _M_name_messages = _S_get_c_name();
+    messages<_CharT>::messages(__c_locale __cloc, const char* __s,
+			       size_t __refs)
+    : facet(__refs), _M_c_locale_messages(0), _M_name_messages(0)
+    {
+      if (__builtin_strcmp(__s, _S_get_c_name()) != 0)
+	{
+	  const size_t __len = __builtin_strlen(__s) + 1;
+	  char* __tmp = new char[__len];
+	  __builtin_memcpy(__tmp, __s, __len);
+	  _M_name_messages = __tmp;
+	}
+      else
+	_M_name_messages = _S_get_c_name();
 
-       // Last to avoid leaking memory if new throws.
-       _M_c_locale_messages = _S_clone_c_locale(__cloc);
-     }
+      // Last to avoid leaking memory if new throws.
+      _M_c_locale_messages = _S_clone_c_locale(__cloc);
+    }
 
   template<typename _CharT>
-    typename messages<_CharT>::catalog 
-    messages<_CharT>::open(const basic_string<char>& __s, const locale& __loc, 
+    typename messages<_CharT>::catalog
+    messages<_CharT>::open(const basic_string<char>& __s, const locale& __loc,
 			   const char* __dir) const
-    { 
+    {
       bindtextdomain(__s.c_str(), __dir);
-      return this->do_open(__s, __loc); 
+      return this->do_open(__s, __loc);
     }
 
   // Virtual member functions.
   template<typename _CharT>
     messages<_CharT>::~messages()
-    { 
+    {
       if (_M_name_messages != _S_get_c_name())
 	delete [] _M_name_messages;
-      _S_destroy_c_locale(_M_c_locale_messages); 
+      _S_destroy_c_locale(_M_c_locale_messages);
     }
 
   template<typename _CharT>
-    typename messages<_CharT>::catalog 
-    messages<_CharT>::do_open(const basic_string<char>& __s, 
+    typename messages<_CharT>::catalog
+    messages<_CharT>::do_open(const basic_string<char>& __s,
 			      const locale&) const
-    { 
+    {
       // No error checking is done, assume the catalog exists and can
       // be used.
       textdomain(__s.c_str());
@@ -95,36 +95,57 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
     }
 
   template<typename _CharT>
-    void    
-    messages<_CharT>::do_close(catalog) const 
+    void
+    messages<_CharT>::do_close(catalog) const
     { }
 
-   // messages_byname
-   template<typename _CharT>
-     messages_byname<_CharT>::messages_byname(const char* __s, size_t __refs)
-     : messages<_CharT>(__refs) 
-     { 
-       if (this->_M_name_messages != locale::facet::_S_get_c_name())
-	 {
-	   delete [] this->_M_name_messages;
-	   if (__builtin_strcmp(__s, locale::facet::_S_get_c_name()) != 0)
-	     {
-	       const size_t __len = __builtin_strlen(__s) + 1;
-	       char* __tmp = new char[__len];
-	       __builtin_memcpy(__tmp, __s, __len);
-	       this->_M_name_messages = __tmp;
-	     }
-	   else
-	     this->_M_name_messages = locale::facet::_S_get_c_name();
-	 }
+  // messages_byname
+  template<typename _CharT>
+    messages_byname<_CharT>::messages_byname(const char* __s, size_t __refs)
+    : messages<_CharT>(__refs)
+    {
+      if (this->_M_name_messages != locale::facet::_S_get_c_name())
+	{
+	  delete [] this->_M_name_messages;
+	  if (__builtin_strcmp(__s, locale::facet::_S_get_c_name()) != 0)
+	    {
+	      const size_t __len = __builtin_strlen(__s) + 1;
+	      char* __tmp = new char[__len];
+	      __builtin_memcpy(__tmp, __s, __len);
+	      this->_M_name_messages = __tmp;
+	    }
+	  else
+	    this->_M_name_messages = locale::facet::_S_get_c_name();
+	}
 
-       if (__builtin_strcmp(__s, "C") != 0
-	   && __builtin_strcmp(__s, "POSIX") != 0)
-	 {
-	   this->_S_destroy_c_locale(this->_M_c_locale_messages);
-	   this->_S_create_c_locale(this->_M_c_locale_messages, __s); 
-	 }
-     }
+      if (__builtin_strcmp(__s, "C") != 0
+	  && __builtin_strcmp(__s, "POSIX") != 0)
+	{
+	  this->_S_destroy_c_locale(this->_M_c_locale_messages);
+	  this->_S_create_c_locale(this->_M_c_locale_messages, __s);
+	}
+    }
+
+   //Specializations.
+  template<>
+    typename messages<char>::catalog
+    messages<char>::do_open(const basic_string<char>&,
+			    const locale&) const;
+
+  template<>
+    void
+    messages<char>::do_close(catalog) const;
+
+#ifdef _GLIBCXX_USE_WCHAR_T
+  template<>
+    typename messages<wchar_t>::catalog
+    messages<wchar_t>::do_open(const basic_string<char>&,
+			       const locale&) const;
+
+  template<>
+    void
+    messages<wchar_t>::do_close(catalog) const;
+#endif
 
 _GLIBCXX_END_NAMESPACE_VERSION
 } // namespace
