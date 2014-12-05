1002108694d0367262cbf70d1743627a5e71fbfa
H.J. Lu <hongjiu.lu@intel.com>
2014-12-05 11:52:44 +0000

    Stop only if there aren't any usable algorithms
    When searching for an usable algorithm with -minline-all-stringops,
    decide_alg stops when it sees libcall even if there is a usable
    algorithm.  It goes into an infinite loop.  This patch changes
    decide_alg to stop searching only if there aren't any usable algorithms.
    Testd on Linux/x86-64.
    
    gcc/
    
    	Backport from mainline
    	PR target/64108
    	* config/i386/i386.c (decide_alg): Stop only if there aren't
    	any usable algorithms.
    
    gcc/testsuite/
    
    	Backport from mainline
    	PR target/64108
    	* gcc.target/i386/memset-strategy-2.c: New test.
    
    From-SVN: r218419

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -23892,7 +23892,8 @@ decide_alg (HOST_WIDE_INT count, HOST_WIDE_INT expected_size,
 		      *noalign = alg_noalign;
 		      return alg;
 		    }
-		  break;
+		  else if (!any_alg_usable_p)
+		    break;
 		}
 	      else if (alg_usable_p (candidate, memset))
 		{
