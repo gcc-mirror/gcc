04430e3b35374412603b267ba57847b69cf9e8a0
tbsaunde <tbsaunde@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-12-08 00:35:33 +0000

    don't ICE when section attribute is used on things in comdats
    gcc/
    
    	PR ipa/63621
    	* symtab.c (symtab_node::verify): Check for section attribute before
    	asserting something isn't in a section and a comdat group.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@218476 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/ipa/pr63621.C
@@ -0,0 +1,29 @@
+// { dg-do compile }
+ class A
+{
+	public:
+		int __attribute__((section("a"))) f1(bool);
+		int f2(void *);
+		int f3(bool);
+};
+
+inline int A::f1(bool b)
+{
+	static  int c;
+	if (c)
+		;
+	return 0;
+}
+
+inline int A::f3(bool b)
+{
+	static __attribute__((section(""))) int c;
+	if (c)
+		;
+	return 0;
+}
+
+int A::f2(void *c)
+{
+	return f1(c) + f3(c);
+}
