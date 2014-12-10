3ec5dd98f08168f9230127008432286b8dc4f035
Jakub Jelinek <jakub@redhat.com>
2014-12-10 20:59:57 +0100

    re PR target/64252 (expand_vec_perm_pblendv caused miscompilation)
    	PR target/64252
    	* config/i386/i386.c (expand_vec_perm_pblendv): If not testing_p,
    	set dcopy.target to a new pseudo.
    
    	* gcc.dg/vect/pr64252.c: New test.
    	* gcc.dg/pr64252.c: New test.
    	* gcc.target/i386/avx2-pr64252.c: New test.
    
    Co-Authored-By: Evgeny Stupachenko <evstupac@gmail.com>
    
    From-SVN: r218602

---
+++ gcc/testsuite/gcc.dg/pr64252.c
@@ -0,0 +1,30 @@
+/* PR target/64252 */
+/* { dg-do run } */
+/* { dg-options "-O2" } */
+
+typedef unsigned int V __attribute__((vector_size (32)));
+
+__attribute__((noinline, noclone)) void
+foo (V *a, V *b, V *c, V *d, V *e)
+{
+  V t = __builtin_shuffle (*a, *b, *c);
+  V v = __builtin_shuffle (t, (V) { ~0U, ~0U, ~0U, ~0U, ~0U, ~0U, ~0U, ~0U }, (V) { 0, 1, 8, 3, 4, 5, 9, 7 });
+  v = v + *d;
+  *e = v;
+}
+
+int
+main ()
+{
+  V a, b, c, d, e;
+  int i;
+  a = (V) { 1, 2, 3, 4, 5, 6, 7, 8 };
+  b = (V) { 9, 10, 11, 12, 13, 14, 15, 16 };
+  c = (V) { 1, 3, 5, 7, 9, 11, 13, 15 };
+  d = (V) { 0, 0, 0, 0, 0, 0, 0, 0 };
+  foo (&a, &b, &c, &d, &e);
+  for (i = 0; i < 8; i++)
+    if (e[i] != ((i == 2 || i == 6) ? ~0U : 2 + 2 * i))
+      __builtin_abort ();
+  return 0;
+}
