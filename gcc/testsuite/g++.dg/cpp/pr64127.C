73ae830e9f5167c36e092da3122875b40eb06404
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-12-10 19:05:43 +0000

    2014-12-10  Paolo Carlini  <paolo.carlini@oracle.com>
    	* g++.dg/cpp/pr64127.C: Fix.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@218599 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp/pr64127.C
+++ gcc/testsuite/g++.dg/cpp/pr64127.C
@@ -1,9 +1,4 @@
 /* { dg-do compile { target c++98_only } } */
 
-template <0> int __copy_streambufs_eof; // { dg-error "" }
-// { dg-error "numeric constant" "" { target *-*-* } 3 }
-// { dg-warning "variable templates" "" { target *-*-* } 3 }
-__copy_streambufs_eof < // { dg-error "" }
-// { dg-error "parse error" "" { target *-*-* } 6 }
-// { dg-error "not name a type" "" { target *-*-* } 6 }
-
+template <0> int __copy_streambufs_eof; // { dg-error "expected identifier|numeric constant|variable templates" }
+__copy_streambufs_eof < // { dg-error "template argument|parse error|not name a type" }
