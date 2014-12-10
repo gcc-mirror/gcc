6789ccfa469492579bfb23dedd20218153cdcd04
Jonathan Wakely <jwakely@redhat.com>
2014-12-10 01:37:37 +0000

    Implement LWG DR 2329 and DR 2332.
    	* include/bits/regex.h (regex_match, regex_search): LWG DR 2329,
    	add deleted overloads for rvalue strings.
    	(regex_iterator, regex_token_iterator): LWG DR 2332, add deleted
    	constructors for rvalue regexes.
    	* testsuite/28_regex/algorithms/regex_match/dr2329_neg.cc: New.
    	* testsuite/28_regex/algorithms/regex_search/dr2329_neg.cc: New.
    	* testsuite/28_regex/iterators/regex_iterator/ctors/char/dr2332_neg.cc:
    	New.
    	* testsuite/28_regex/iterators/regex_token_iterator/ctors/char/
    	dr2332_neg.cc: New.
    
    From-SVN: r218555

---
+++ libstdc++-v3/testsuite/28_regex/iterators/regex_token_iterator/ctors/char/dr2332_neg.cc
@@ -0,0 +1,39 @@
+// Copyright (C) 2014 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++11" }
+// { dg-do compile }
+
+#include <regex>
+
+void
+test01()
+{
+  using iter_type = std::regex_token_iterator<const char*>;
+  const char* s = "";
+
+  iter_type(s, s, std::regex{});	// { dg-error "deleted" }
+
+  std::vector<int> v;
+  iter_type(s, s, std::regex{}, v);	// { dg-error "deleted" }
+
+  std::initializer_list<int> il = {1};
+  iter_type(s, s, std::regex{}, il);	// { dg-error "deleted" }
+
+  const int i[2] = { };
+  iter_type(s, s, std::regex{}, i);	// { dg-error "deleted" }
+}
