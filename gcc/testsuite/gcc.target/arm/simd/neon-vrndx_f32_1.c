26a4d424e19c57db7055f134eff87f26fa61cae5
Kyrylo Tkachov <kyrylo.tkachov@arm.com>
2014-12-11 13:34:22 +0000

    [ARM] Fix names of some rounding intrinsics, impement vrndx_f32 and vrndxq_f32
    	* config/arm/arm_neon.h (vrndqn_f32): Rename to...
    	(vrndnq_f32): ... this.
    	(vrndqa_f32): Rename to...
    	(vrndaq_f32): ... this.
    	(vrndqp_f32): Rename to...
    	(vrndpq_f32): ... this.
    	(vrndqm_f32): Rename to...
    	(vrndmq_f32): ... this.
    	(vrndx_f32): New intrinsic.
    	(vrndxq_f32): Likewise.
    
    	* config/arm/arm_neon.h (vrndqn_f32): Rename to...
    	(vrndnq_f32): ... this.
    	(vrndqa_f32): Rename to...
    	(vrndaq_f32): ... this.
    	(vrndqp_f32): Rename to...
    	(vrndpq_f32): ... this.
    	(vrndqm_f32): Rename to...
    	(vrndmq_f32): ... this.
    	(vrndx_f32): New intrinsic.
    	(vrndxq_f32): Likewise.
    
    From-SVN: r218622

---
+++ gcc/testsuite/gcc.target/arm/simd/neon-vrndx_f32_1.c
@@ -0,0 +1,17 @@
+/* Test the `vrndx_f32' ARM Neon intrinsic.  */
+
+/* { dg-do assemble } */
+/* { dg-require-effective-target arm_v8_neon_ok } */
+/* { dg-options "-save-temps -O2" } */
+/* { dg-add-options arm_v8_neon } */
+
+#include "arm_neon.h"
+
+float32x2_t
+test_vrndx_f32 (float32x2_t in)
+{
+  return vrndx_f32 (in);
+}
+
+/* { dg-final { scan-assembler "vrintx\.f32\[ 	\]+\[dD\]\[0-9\]+, \[dD\]\[0-9\]+\n" } } */
+/* { dg-final { cleanup-saved-temps } } */
