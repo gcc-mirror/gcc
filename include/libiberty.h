01ca36af914385acfc864adfae6923768581888a
Uros Bizjak <ubizjak@gmail.com>
2014-12-11 09:15:37 +0100

    xvasprintf.c: New file.
    	* xvasprintf.c: New file.
    	* vprintf-support.h: Likewise.
    	* vprintf-support.c: Likewise.
    	* Makefile.in (CFILES): Add vprintf-support.c, xvasprintf.c.
    	(REQUIRED_OFILES): Add vprintf-support.$(objext), xvasprintf.$(objext).
    	(vprintf-support.$(objext), xvasprintf.$(objext)): New targets.
    	(vasprintf.$(objext)): Depend on $(srcdir)/vprintf-support.h.
    	* functions.texi: Regenerate.
    	* vasprintf.c (int_vasprintf): Use libiberty_vprintf_buffer_size.
    
    include/ChangeLog:
    
    	* libiberty.h (xvasprintf): Declare.
    
    libcpp/ChangeLog:
    
    	* directives.c (cpp_define_formatted): Use xvasprintf.
    
    
    Co-Authored-By: Ben Elliston <bje@au.ibm.com>
    Co-Authored-By: Manuel López-Ibáñez <manu@gcc.gnu.org>
    
    From-SVN: r218618

--- include/libiberty.h
+++ include/libiberty.h
@@ -636,6 +636,11 @@ extern int asprintf (char **, const char *, ...) ATTRIBUTE_PRINTF_2;
 extern int vasprintf (char **, const char *, va_list) ATTRIBUTE_PRINTF(2,0);
 #endif
 
+/* Like vasprintf but allocates memory without fail. This works like
+   xmalloc.  */
+
+extern char *xvasprintf (const char *, va_list) ATTRIBUTE_MALLOC ATTRIBUTE_PRINTF(1,0);
+
 #if defined(HAVE_DECL_SNPRINTF) && !HAVE_DECL_SNPRINTF
 /* Like sprintf but prints at most N characters.  */
 extern int snprintf (char *, size_t, const char *, ...) ATTRIBUTE_PRINTF_3;
