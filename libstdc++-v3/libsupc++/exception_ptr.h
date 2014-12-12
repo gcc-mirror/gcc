146d81545912f621237df1947d60f2cd8ca0993c
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-12-12 15:10:08 +0000

    PR libstdc++/64241 	* libsupc++/exception_ptr.h: Return empty object when exceptions are 	disabled. 	* testsuite/18_support/exception_ptr/64241.cc: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@218675 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/libsupc++/exception_ptr.h
+++ libstdc++-v3/libsupc++/exception_ptr.h
@@ -168,16 +168,18 @@ namespace std
     exception_ptr 
     make_exception_ptr(_Ex __ex) _GLIBCXX_USE_NOEXCEPT
     {
-      __try
-	{
 #ifdef __EXCEPTIONS
+      try
+	{
 	  throw __ex;
-#endif
 	}
-      __catch(...)
+      catch(...)
 	{
 	  return current_exception();
 	}
+#else
+      return exception_ptr();
+#endif
     }
 
   // _GLIBCXX_RESOLVE_LIB_DEFECTS
