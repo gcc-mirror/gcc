9a58d6dea6fe2a79dae6c9d34e387cef769e8aaa
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-12-17 09:29:12 +0000

    PR tree-optimization/64322 	* tree-vrp.c (extract_range_from_binary_expr_1): Attempt to derive 	range for RSHIFT_EXPR even if vr0 range is not VR_RANGE or is symbolic.
    	* gcc.dg/tree-ssa/vrp95.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@218812 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp95.c
@@ -0,0 +1,50 @@
+/* PR tree-optimization/64322 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-vrp1" } */
+
+extern void link_error ();
+extern void required_check ();
+
+long long int
+foo (long long int x)
+{
+  x >>= sizeof (long long int) * __CHAR_BIT__ - 1;
+  if (x != 0 && x != -1)
+    link_error ();
+  return x;
+}
+
+unsigned long long int
+bar (unsigned long long int x)
+{
+  x >>= sizeof (long long int) * __CHAR_BIT__ - 1;
+  if (x != 0 && x != 1)
+    link_error ();
+  return x;
+}
+
+long long int
+baz (long long int x)
+{
+  x = (x >> sizeof (long long int) * __CHAR_BIT__ - 1) << 1;
+  x = x / 0x100000000LL;
+  if (x != 0)
+    link_error ();
+  return x;
+}
+
+unsigned long long int
+range (unsigned long long int x, int y)
+{
+  y &= 3;
+  x >>= sizeof (long long int) * __CHAR_BIT__ - 1 - y;
+  if (x > 15)
+    link_error ();
+  if (x == 15)
+    required_check ();
+  return x;
+}
+
+/* { dg-final { scan-tree-dump-not "link_error" "vrp1" } } */
+/* { dg-final { scan-tree-dump "required_check" "vrp1" } } */
+/* { dg-final { cleanup-tree-dump "vrp1" } } */
