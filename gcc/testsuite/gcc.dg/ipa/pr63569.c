e922baf33edbc4fe64062fc34a36b2bd4b5444a0
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-12-19 12:40:50 +0000

    Fix for PR ipa/63569.
    	PR ipa/63569
    	* gcc.dg/ipa/pr63569.c: New test.
    	PR ipa/63569
    	* ipa-icf-gimple.c (func_checker::compare_operand): Add missing
    	comparison for volatile flag.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@218949 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/ipa/pr63569.c
@@ -0,0 +1,32 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-ipa-icf-details"  } */
+
+static int f(int t, int *a) __attribute__((noinline));
+
+static int g(int t, volatile int *a) __attribute__((noinline));
+static int g(int t, volatile int *a)
+{
+  int i;
+  int tt = 0;
+  for(i=0;i<t;i++)
+    tt += *a;
+  return tt;
+}
+static int f(int t, int *a)
+{
+  int i;
+  int tt = 0;
+  for(i=0;i<t;i++)
+    tt += *a;
+  return tt;
+}
+
+
+int h(int t, int *a)
+{
+  return f(t, a) + g(t, a);
+}
+
+/* { dg-final { scan-ipa-dump "different operand volatility" "icf"  } } */
+/* { dg-final { scan-ipa-dump "Equal symbols: 0" "icf"  } } */
+/* { dg-final { cleanup-ipa-dump "icf" } } */
