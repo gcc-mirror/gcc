d427aa2e04fbd6e82166364f619aa1119eeda5ca
Kai Tietz <ktietz@redhat.com>
2014-12-19 17:47:59 +0100

    re PR c++/61198 (Crash when selecting specializations through aliases.)
    	PR c++/61198
    	* g++.dg/template/using30.C: New file.
    
    From-SVN: r218956

---
+++ gcc/testsuite/g++.dg/template/using30.C
@@ -0,0 +1,24 @@
+// { dg-do compile }
+// PR/61198
+
+template<int herp, typename derp_t>
+struct broken
+{
+	template<typename target_t>
+	using rebind = broken<herp, target_t>;
+};
+
+template<typename derp_t>
+struct broken<2, derp_t>
+{
+	template<typename target_t>
+	using rebind = broken<2, target_t>;
+};
+
+int main(int argc, char **argv)
+{		
+	broken<2, float>::rebind<double> u;
+
+	return 0;
+}
+
