369695b99d6eb0382c32ad442a55a5d3e656069a
Andrew Stubbs <ams@codesourcery.com>
2014-12-24 14:27:06 +0000

    Fix undefined-loop-2.c test case.
    2014-12-24  Andrew Stubbs  <ams@codesourcery.com>
    
    	PR testsuite/64032
    	* gcc.dg/undefined-loop-2.c: Don't allow GCC to optimize away the
    	loop exits too early.
    
    From-SVN: r219059

--- gcc/testsuite/gcc.dg/undefined-loop-2.c
+++ gcc/testsuite/gcc.dg/undefined-loop-2.c
@@ -15,8 +15,11 @@ foo (int p)
        (p
         ? array1[i]  /* { dg-message "note: possible undefined statement is here" } */
         : array2[i]) /* { dg-message "note: possible undefined statement is here" } */
-       && i < 5      /* { dg-warning "loop exit may only be reached after undefined behavior" } */
        && i < 100;   /* { dg-warning "loop exit may only be reached after undefined behavior" } */
        i++)
-    doSomething(array1[i]);
+    {
+      if (i >= 5)    /* { dg-warning "loop exit may only be reached after undefined behavior" } */
+	break;
+      doSomething(array1[i]);
+    }
 }
