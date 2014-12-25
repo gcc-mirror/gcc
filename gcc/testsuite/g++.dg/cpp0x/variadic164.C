7ad852d0e6cf4a54deb94e2d5fd474047f143e85
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-12-25 07:27:54 +0000

    PR c++/63522 	* parser.c (cp_parser_type_parameter): Call 	check_for_bare_parameter_packs on default argument.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@219066 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/variadic164.C
@@ -0,0 +1,12 @@
+// PR c++/63522
+// { dg-do compile { target c++11 } }
+
+template <typename...> struct tuple;
+template <typename...> void slice();
+template <int Index, typename...> using slice_result = decltype(slice<Index>);
+template <typename Tuple, typename... Tuples, int... ElementIndices,
+          typename =
+              typename tuple<slice_result<ElementIndices, Tuples...>,
+                             slice_result<ElementIndices, Tuples...>...>::type> // { dg-error "parameter pack" }
+void zip_with(Tuple...);
+decltype(zip_with(0)) d;	// { dg-error "no match" }
