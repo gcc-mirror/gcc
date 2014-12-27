eb68b105f13ab489607afbec33d00c634c183670
H.J. Lu <hongjiu.lu@intel.com>
2014-12-27 16:26:55 +0000

    Issue an error for ms_abi attribute with x32
    There is no counter part of x32 in MS ABI.   Issue an error when ms_abi
    attribute is used with x32.
    
    gcc/
    
    	PR target/64409
    	* config/i386/i386.c (ix86_function_type_abi): Issue an error
    	when ms_abi attribute is used with x32.
    
    gcc/testsuite/
    
    	PR target/64409
    	* gcc.target/i386/pr64409.c: New test.
    
    From-SVN: r219082

---
+++ gcc/testsuite/gcc.target/i386/pr64409.c
@@ -0,0 +1,6 @@
+/* { dg-do compile { target { ! { ia32 } } } } */
+/* { dg-require-effective-target maybe_x32 } */
+/* { dg-options "-O0 -mx32" } */
+
+int a;
+int* __attribute__ ((ms_abi)) fn1 () { return &a; } /* { dg-error "X32 does not support ms_abi attribute" } */
