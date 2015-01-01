d57044f22a371f74e26c7ee83b3b9149d2435747
mrs <mrs@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-01-01 01:44:11 +0000

    2014-12-31  James Bowman  <james.bowman@ftdichip.com>
    	* gcc.c-torture/execute/pr20621-1.c: Pass if stack < 64K.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@219134 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/execute/pr20621-1.c
+++ gcc/testsuite/gcc.c-torture/execute/pr20621-1.c
@@ -3,4 +3,8 @@
 struct big { int i[sizeof (int) >= 4 && sizeof (void *) >= 4 ? 0x4000 : 4]; };
 struct big gb;
 int foo (struct big b, int x) { return b.i[x]; }
+#if defined(STACK_SIZE) && STACK_SIZE <= 0x10000
+int main (void) { return 0; }
+#else
 int main (void) { return foo (gb, 0) + foo (gb, 1); }
+#endif
