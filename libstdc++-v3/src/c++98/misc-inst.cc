e4dd5b691d56f11c225e1e1847be3800854634b6
Bernd Edlinger <bernd.edlinger@hotmail.de>
2015-01-02 21:12:54 +0000

    re PR c++/64422 (basic_string::erase is unresloved)
    2015-01-02  Bernd Edlinger  <bernd.edlinger@hotmail.de>
    
            PR libstdc++/64422
            * src/c++98/misc-inst.cc (string::erase): Add missing overloads.
            (string::insert): Likewise.
            (string::replace): Likewise.
            (wstring::erase): Likewise.
            (wstring::insert): Likewise.
            (wstring::replace): Likewise.
            * testsuite/21_strings/basic_string/modifiers/64422.cc: New testcase.
    
    From-SVN: r219149

--- libstdc++-v3/src/c++98/misc-inst.cc
+++ libstdc++-v3/src/c++98/misc-inst.cc
@@ -68,20 +68,36 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
   // C++98 members that are not instantiated by src/c++11/string-inst.cc
   // because they changed in C++11 to take const_iterator parameters.
   template string::iterator string::erase(iterator);
+  template string::iterator string::erase(iterator, iterator);
+  template void string::insert(iterator, size_type, char);
   template void string::insert(iterator, iterator, iterator);
   template string::iterator string::insert(iterator, char);
+  template string& string::replace(iterator, iterator, const string&);
+  template string&
+  string::replace(iterator, iterator, const char*, size_type);
+  template string& string::replace(iterator, iterator, const char*);
   template string& string::replace(iterator, iterator, size_type, char);
   template string& string::replace(iterator, iterator, char*, char*);
+  template string&
+  string::replace(iterator, iterator, const char*, const char*);
   template string& string::replace(iterator, iterator, iterator, iterator);
   template string&
   string::replace(iterator, iterator, const_iterator, const_iterator);
 
 #ifdef _GLIBCXX_USE_WCHAR_T
   template wstring::iterator wstring::erase(iterator);
+  template wstring::iterator wstring::erase(iterator, iterator);
+  template void wstring::insert(iterator, size_type, wchar_t);
   template void wstring::insert(iterator, iterator, iterator);
   template wstring::iterator wstring::insert(iterator, wchar_t);
+  template wstring& wstring::replace(iterator, iterator, const wstring&);
+  template wstring&
+  wstring::replace(iterator, iterator, const wchar_t*, size_type);
+  template wstring& wstring::replace(iterator, iterator, const wchar_t*);
   template wstring& wstring::replace(iterator, iterator, size_type, wchar_t);
   template wstring& wstring::replace(iterator, iterator, wchar_t*, wchar_t*);
+  template wstring&
+  wstring::replace(iterator, iterator, const wchar_t*, const wchar_t*);
   template wstring& wstring::replace(iterator, iterator, iterator, iterator);
   template wstring&
   wstring::replace(iterator, iterator, const_iterator, const_iterator);
