d353bf189d2bbaf4059f402ee4d2a5ea074c349f
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-01-05 12:33:28 +0000

    Update copyright years.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@219188 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/dce.c
+++ gcc/dce.c
@@ -1,5 +1,5 @@
 /* RTL dead code elimination.
-   Copyright (C) 2005-2014 Free Software Foundation, Inc.
+   Copyright (C) 2005-2015 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
--- libgomp/splay-tree.h
+++ libgomp/splay-tree.h
@@ -1,6 +1,5 @@
 /* A splay-tree datatype.
-   Copyright 1998-2014
-   Free Software Foundation, Inc.
+   Copyright (C) 1998-2015 Free Software Foundation, Inc.
    Contributed by Mark Mitchell (mark@markmitchell.com).
 
    This file is part of the GNU OpenMP Library (libgomp).
