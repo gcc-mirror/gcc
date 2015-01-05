def3bf8c16bab6d5d3e3aa32c63ca025e59af3af
clm <clm@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-01-05 23:42:27 +0000

    2015-01-05  Radovan Obradovic  <radovan.obradovic@imgtec.com>
    	PR rtl-optimization/64287
    
    	gcc/
    	* toplev.c (HAVE_epilogue, HAVE_prologue): Provide default.
    	(process_options): Disable flag_ipa_ra if profiling.
    
    	gcc/testsuite/
    	* gcc.dg/aru-2.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@219208 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/aru-2.c
@@ -0,0 +1,26 @@
+/* { dg-do run } */
+/* { dg-options "-O2 -pg" } */
+
+static int __attribute__((noinline))
+bar (int x)
+{
+  return x + 3;
+}
+
+int __attribute__((noinline))
+foo (int y0, int y1, int y2, int y3, int y4) {
+  int r = 0;
+  r += bar (r + y4);
+  r += bar (r + y3);
+  r += bar (r + y2);
+  r += bar (r + y1);
+  r += bar (r + y0);
+  return r;
+}
+
+int
+main (void)
+{
+  int z = foo (0, 1, 2, 3, 4);
+  return !(z == 191);
+}
