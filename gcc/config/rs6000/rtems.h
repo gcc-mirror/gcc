1fbd41b727d3edc83f632cb25eca4727cfda0153
Sebastian Huber <sebastian.huber@embedded-brains.de>
2015-01-09 14:19:47 +0000

    RTEMS: Add e6500 multilibs for PowerPC
    Use 32-bit instructions only since currently there is no demand for a
    larger address space.  Provide one multilib with FPU and AltiVec support
    and one without.
    
    gcc/ChangeLog
    
    	* config/rs6000/rtems.h (CPP_OS_RTEMS_SPEC): Define __PPC_CPU_E6500__
    	for -mcpu=e6500.
    	* config/rs6000/t-rtems: Add e6500 multilibs.
    
    From-SVN: r219391

--- gcc/config/rs6000/rtems.h
+++ gcc/config/rs6000/rtems.h
@@ -52,7 +52,8 @@
 %{mcpu=750:  %{!Dppc*: %{!Dmpc*: -Dmpc750}  } } \
 %{mcpu=821:  %{!Dppc*: %{!Dmpc*: -Dmpc821}  } } \
 %{mcpu=860:  %{!Dppc*: %{!Dmpc*: -Dmpc860}  } } \
-%{mcpu=8540: %{!Dppc*: %{!Dmpc*: -Dppc8540}  } }" 
+%{mcpu=8540: %{!Dppc*: %{!Dmpc*: -Dppc8540}  } } \
+%{mcpu=e6500: -D__PPC_CPU_E6500__}"
 
 #undef  SUBSUBTARGET_EXTRA_SPECS
 #define SUBSUBTARGET_EXTRA_SPECS \
