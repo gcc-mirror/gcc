6c0fcb81dad4c8f2f9c687a40627db0442a475f4
Marc Glisse <marc.glisse@inria.fr>
2015-01-12 18:08:04 +0100

    Avoid undefined behavior in gcc.target/i386/pr64291-1.c
    	PR testsuite/64427
    	* gcc.target/i386/pr64291-1.c: Limit to lp64 tarrget.  Avoid
    	undefined behavior.
    	* gcc.target/i386/pr64291-2.c: Updated.
    
    From-SVN: r219479

--- gcc/testsuite/gcc.target/i386/pr64291-2.c
+++ gcc/testsuite/gcc.target/i386/pr64291-2.c
@@ -1,4 +1,14 @@
 /* { dg-do compile } */
-extern void abort (void);
+#include <assert.h>
 void f(void*p,...){}
-void g(void*p,long a,long b){if (a!=8) abort();}
+void g(void*p,long a,long b){assert(a==8);}
+typedef struct
+{
+  int _mp_size;
+  unsigned long *_mp_d;
+} __mpz_struct;
+typedef __mpz_struct mpz_t[1];
+void h(mpz_t x) {
+  x->_mp_d=0;
+  x->_mp_size=0;
+}
