7fc39e218995d7edf16eb28928d76e44df731e9c
Jan-Benedict Glaw <jbglaw@lug-owl.de>
2015-01-14 21:55:35 +0000

    Pull libiberty.h copyright update from Binutils
    2015-12-14  Jan-Benedict Glaw  <jbglaw@lug-owl.de>
    
    	* libiberty.h: Merge Copyright year update from Binutils.
    
    From-SVN: r219617

--- include/libiberty.h
+++ include/libiberty.h
@@ -1,7 +1,6 @@
 /* Function declarations for libiberty.
 
-   Copyright 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
-   2006, 2007, 2008, 2009, 2010, 2011, 2013 Free Software Foundation, Inc.
+   Copyright (C) 1997-2015 Free Software Foundation, Inc.
    
    Note - certain prototypes declared in this header file are for
    functions whoes implementation copyright does not belong to the
