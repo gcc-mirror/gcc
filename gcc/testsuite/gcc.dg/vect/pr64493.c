46b622cc2466685c60ef40256e49ad1fd8844cf7
Richard Biener <rguenther@suse.de>
2015-01-14 08:32:18 +0000

    re PR tree-optimization/64493 (ICE at -O3 on x86_64-linux-gnu)
    2015-01-14  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/64493
    	PR tree-optimization/64495
    	* tree-vect-loop.c (vect_finalize_reduction): For double-reductions
    	assign the proper vectorized PHI to the inner loop exit PHIs.
    
    	* gcc.dg/vect/pr64493.c: New testcase.
    	* gcc.dg/vect/pr64495.c: Likewise.
    
    From-SVN: r219579

---
+++ gcc/testsuite/gcc.dg/vect/pr64493.c
@@ -0,0 +1,31 @@
+/* { dg-do run } */
+
+#include "tree-vect.h"
+
+int a, b, c, d, e, f, g, h;
+
+int
+main ()
+{
+  check_vect ();
+
+  for (; a; a--)
+    for (d = 1; d <= 0; d++)
+      for (; d;)
+	if (h)
+	  {
+	    if (!g) __builtin_abort ();
+	    if (!0) __builtin_abort ();
+	  }
+
+  for (f = 4; f; f--)
+    {
+      for (b = 0; b < 2; b++)
+	c |= 1;
+      e |= c;
+    }
+
+  return 0;
+}
+
+/* { dg-final { cleanup-tree-dump "vect" } } */
