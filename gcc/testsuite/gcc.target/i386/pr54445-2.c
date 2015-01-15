645c25f6425651f56d6d6cd09005d32756cb53be
H.J. Lu <hongjiu.lu@intel.com>
2015-01-15 13:25:30 +0000

    Adjust scan string for PIE
    The order of address in the first operand is different due to different
    output orders of PLUS between output_addr_const and output_pic_addr_const.
    This patch adjusts scan string for PIE in gcc.target/i386/pr54445-2.c.
    
    	* gcc.target/i386/pr54445-2.c: Adjust scan string for PIE.
    
    From-SVN: r219654

--- gcc/testsuite/gcc.target/i386/pr54445-2.c
+++ gcc/testsuite/gcc.target/i386/pr54445-2.c
@@ -8,4 +8,4 @@ tls_array_lookup_with_negative_constant(long long int position) {
   return tls_array[position - 1];
 }
 
-/* { dg-final { scan-assembler "mov(b|zbl)\[ \t\](%fs:)?tls_array@tpoff-1\\(%" } } */
+/* { dg-final { scan-assembler "mov(b|zbl)\[ \t\](%fs:)?(-1\\+)?tls_array@tpoff(-1)?\\(%" } } */
