4f803217a06121d78e6640b46e6aa6bcc3606e9b
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-01-16 23:38:48 +0000

    * include/bits/locale_conv.h (wstring_convert, wbuffer_convert): New. 	* include/std/locale: Include new header. 	* include/Makefile.am: Add it. 	* include/Makefile.in: Regenerate. 	* testsuite/22_locale/conversions/buffer/requirements/typedefs.cc: New. 	* testsuite/22_locale/conversions/string/1.cc: New. 	* testsuite/22_locale/conversions/string/2.cc: New. 	* testsuite/22_locale/conversions/string/requirements/typedefs.cc: New. 	* testsuite/22_locale/conversions/string/requirements/typedefs-2.cc: 	New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@219780 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/22_locale/conversions/string/requirements/typedefs.cc
@@ -0,0 +1,34 @@
+// { dg-do compile }
+// { dg-options "-std=gnu++11" }
+
+// Copyright (C) 2012 Free Software Foundation
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 22.3.3.2.2  String conversions
+
+#include <locale>
+
+void test01()
+{
+  // Check for required typedefs
+  typedef std::codecvt<wchar_t, char, mbstate_t> codecvt_type;
+  typedef std::wstring_convert<codecvt_type> test_type;
+  typedef test_type::byte_string byte_string;
+  typedef test_type::wide_string wide_string;
+  typedef test_type::state_type state_type;
+  typedef test_type::int_type int_type;
+}
