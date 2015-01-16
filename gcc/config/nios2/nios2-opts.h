56314783c33b58a03f7eee9d48b4a2628ee6e1f0
Sandra Loosemore <sandra@codesourcery.com>
2015-01-15 19:43:24 -0500

    re PR target/59710 (Nios2: Missing gprel optimization)
    2015-01-15  Sandra Loosemore  <sandra@codesourcery.com>
    
    	PR target/59710
    
    	gcc/
    	* doc/invoke.texi (Option Summary): Document new Nios II
    	-mgpopt= syntax.
    	(Nios II Options): Likewise.
    	* config/nios2/nios2.opt: Add -mgpopt= option support.
    	Modify existing -mgpopt and -mno-gpopt options to be aliases.
    	* config/nios2/nios2-opts.h (enum nios2_gpopt_type): New.
    	* config/nios2/nios2.c (nios2_option_override): Adjust
    	-mgpopt defaulting.
    	(nios2_in_small_data_p): Return true for explicit small data
    	sections even with -G0.
    	(nios2_symbol_ref_in_small_data_p): Adjust to handle new -mgpopt=
    	option choices.
    
    	gcc/testsuite/
    	* gcc.target/nios2/gpopt-all.c: New test case.
    	* gcc.target/nios2/gpopt-local.c: New test case.
    	* gcc.target/nios2/gpopt-global.c: New test case.
    	* gcc.target/nios2/gpopt-data.c: New test case.
    	* gcc.target/nios2/gpopt-none.c: New test case.
    
    From-SVN: r219700

--- gcc/config/nios2/nios2-opts.h
+++ gcc/config/nios2/nios2-opts.h
@@ -20,6 +20,18 @@ along with GCC; see the file COPYING3.  If not see
 #ifndef NIOS2_OPTS_H
 #define NIOS2_OPTS_H
 
+/* Enumerate the possible -mgpopt choices.  */
+enum nios2_gpopt_type
+{
+  gpopt_unspecified = -1,
+  gpopt_none,
+  gpopt_local,
+  gpopt_global,
+  gpopt_data,
+  gpopt_all
+};
+
+
 /* Enumeration of all FPU insn codes.  */
 #define N2FPU_ALL_CODES							\
   N2FPU_CODE(fadds) N2FPU_CODE(fsubs) N2FPU_CODE(fmuls) N2FPU_CODE(fdivs) \
---
+++ gcc/testsuite/gcc.target/nios2/gpopt-none.c
@@ -0,0 +1,38 @@
+/* { dg-do compile } */
+/* { dg-options "-O -mgpopt=none" } */
+
+extern int a __attribute__ ((section (".sdata")));
+static volatile int b __attribute__ ((section (".sdata"))) = 1;
+extern int c __attribute__ ((section (".data")));
+static volatile int d __attribute__ ((section (".data"))) = 2;
+
+extern int e;
+static volatile int f = 3;
+
+volatile int g __attribute__ ((weak)) = 4;
+
+extern int h[100];
+static int i[100];
+static int j[100] __attribute__ ((section (".sdata")));
+
+typedef int (*ftype) (int);
+extern int foo (int);
+
+extern int bar (int, int*, int*, int*, ftype);
+
+int baz (void)
+{
+  return bar (a + b + c + d + e + f + g, h, i, j, foo);
+}
+
+/* { dg-final { scan-assembler-not "%gprel\\(a\\)" } } */
+/* { dg-final { scan-assembler-not "%gprel\\(b\\)" } } */
+/* { dg-final { scan-assembler-not "%gprel\\(c\\)" } } */
+/* { dg-final { scan-assembler-not "%gprel\\(d\\)" } } */
+/* { dg-final { scan-assembler-not "%gprel\\(e\\)" } } */
+/* { dg-final { scan-assembler-not "%gprel\\(f\\)" } } */
+/* { dg-final { scan-assembler-not "%gprel\\(g\\)" } } */
+/* { dg-final { scan-assembler-not "%gprel\\(h\\)" } } */
+/* { dg-final { scan-assembler-not "%gprel\\(i\\)" } } */
+/* { dg-final { scan-assembler-not "%gprel\\(j\\)" } } */
+/* { dg-final { scan-assembler-not "%gprel\\(foo\\)" } } */
