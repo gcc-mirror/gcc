9377495d6a9c2c22036441c6bc7de0cf7728b4a9
Markus Trippelsdorf <markus@trippelsdorf.de>
2015-01-16 11:12:52 +0000

    g++.dg/ipa/pr64612.C: New test.
    2015-01-16  Markus Trippelsdorf  <markus@trippelsdorf.de>
    
    	PR ipa/64163
    	PR ipa/64612
    	* g++.dg/ipa/pr64612.C: New test.
    
    From-SVN: r219721

---
+++ gcc/testsuite/g++.dg/ipa/pr64612.C
@@ -0,0 +1,66 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 -std=c++11" } */
+/* { dg-final { scan-assembler "_ZN5QListI7QStringED1Ev" } } */
+
+class A
+{
+public:
+  bool deref ();
+};
+class QString;
+struct B
+{
+  A ref;
+};
+template <typename> class QList
+{
+  B d;
+public:
+  ~QList ();
+  class const_iterator
+  {
+  };
+  const_iterator constBegin ();
+  void clear ();
+  void dealloc ();
+};
+template <typename T> QList<T>::~QList ()
+{
+  if (d.ref.deref ())
+    dealloc ();
+}
+template <typename T>
+void
+QList<T>::clear ()
+{
+  QList ();
+}
+class A1 : public QList<QString>
+{
+};
+class B1
+{
+public:
+  B1 (A1);
+};
+struct F
+{
+  void addMatch (const QString &&);
+  A1 m_matchingMimeTypes;
+};
+class G
+{
+  A1 matchingGlobs (const QString &) const;
+};
+void
+F::addMatch (const QString &&)
+{
+  m_matchingMimeTypes.clear ();
+}
+A1
+G::matchingGlobs (const QString &) const
+{
+  A1 a;
+  for (B1 b (a);;)
+    ;
+}
