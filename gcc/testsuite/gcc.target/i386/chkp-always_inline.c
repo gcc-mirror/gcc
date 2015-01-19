c11d86b47facb2f787a7cfde0755454c73b8b90e
Ilya Enkovich <ilya.enkovich@intel.com>
2015-01-19 10:26:52 +0000

    ipa-chkp.c (chkp_produce_thunks): Add early param to split thunks production into two passes.
    gcc/
    
    	* ipa-chkp.c (chkp_produce_thunks): Add early param
    	to split thunks production into two passes.  Keep
    	'always_inline' function bodies after the first pass.
    	(pass_data_ipa_chkp_early_produce_thunks): New.
    	(pass_ipa_chkp_early_produce_thunks): New.
    	(pass_ipa_chkp_produce_thunks::execute): Adjust to new
    	chkp_produce_thunks signature.
    	(make_pass_ipa_chkp_early_produce_thunks): New.
    	* passes.def (pass_ipa_chkp_early_produce_thunks): New.
    	(pass_ipa_chkp_produce_thunks): Move after local optimizations.
    	* tree-pass.h (make_pass_ipa_chkp_early_produce_thunks): New.
    
    gcc/testsuite/
    
    	* gcc.target/i386/chkp-always_inline.c: New.
    
    From-SVN: r219834

---
+++ gcc/testsuite/gcc.target/i386/chkp-always_inline.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target mpx } */
+/* { dg-options "-fcheck-pointer-bounds -mmpx -O2 -Wno-attributes" } */
+
+__attribute__((always_inline)) int f1 (int *p)
+{
+  return *p;
+}
+
+__attribute__((bnd_legacy)) int f2 (int *p)
+{
+  return f1 (p);
+}
