0c84d521a9fe232a30c7fe638a4af21bc7c10c91
Oleg Endo <olegendo@gcc.gnu.org>
2015-01-19 23:25:03 +0000

    re PR target/64652 ([SH] ICE when using -mdiv=call-fp)
    gcc/testsuite/
    	PR target/64652
    	* gcc.target/sh/torture/pr64652.c (test): Rename to test_0.
    	(test_1): New.
    
    From-SVN: r219870

--- gcc/testsuite/gcc.target/sh/torture/pr64652.c
+++ gcc/testsuite/gcc.target/sh/torture/pr64652.c
@@ -4,7 +4,13 @@
 /* { dg-skip-if "" { "sh*-*-*" } { "-m5*" } { "" } }  */
 
 int
-test (int a, int b, int c, int d)
+test_0 (int a, int b, int c, int d)
+{
+  return (a / b) + c + d;
+}
+
+unsigned int
+test_1 (unsigned int a, unsigned int b, unsigned int c, unsigned int d)
 {
   return (a / b) + c + d;
 }
