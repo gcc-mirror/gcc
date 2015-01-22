8dc239d39d0cb6d1d615ba4302c60a2516b980de
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-01-22 21:24:28 +0000

    PR target/52076 	* config/m68k/m68k.md (xorsi3_internal): Twiddle constraints to 	improve code density for small immediate to memory case. 	(insv): Better handle bitfield assignments when the field is 	being set to all ones. 	* config/m68k/predicates.md (reg_or_pow2_m1_operand): New 	operand predicate.
    	PR target/52076
    	* gcc.target/m68k/pr52076-1.c: New test.
    	* gcc.target/m68k/pr52076-2.c: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@220015 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/m68k/pr52076-1.c
@@ -0,0 +1,20 @@
+/* { dg-do assemble } /*
+/* { dg-options "-Os -fomit-frame-pointer -m68040" } */
+/* { dg-final { object-size text <= 72 } } */
+
+struct kobject {
+        unsigned int b7:1;
+        unsigned int :6;
+        unsigned int b0:1;
+        unsigned char x;
+        unsigned int f;
+};
+
+void ior(struct kobject *kobj) { kobj->f |= 4; }
+void ior_m(struct kobject *kobj) { kobj->f |= -4; }
+
+void xor(struct kobject *kobj) { kobj->f ^= 4; }
+void xor_m(struct kobject *kobj) { kobj->f ^= -4; }
+
+void and(struct kobject *kobj) { kobj->f &= 4; }
+void and_m(struct kobject *kobj) { kobj->f &= -4; }
