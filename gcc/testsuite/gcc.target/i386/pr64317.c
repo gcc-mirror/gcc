5c8bae594cdf2d3e215f95bc723921001d84f312
Vladimir Makarov <vmakarov@redhat.com>
2015-01-23 20:15:56 +0000

    re PR rtl-optimization/64317 (Ineffective allocation of PIC base register)
    2015-01-23  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR target/64317
    	* lra-lives.c (make_hard_regno_born): Add parameter.  Don't make
    	REAL_PIC_OFFSET_TABLE_REGNUM conflicting with pic offset pseudo.
    	(mark_regno_live, process_bb_lives): Pass new paramater value to
    	make_hard_regno_born.
    
    2015-01-23  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR target/64317
    	* gcc.target/i386/pr64317.c: New test.
    
    From-SVN: r220060

---
+++ gcc/testsuite/gcc.target/i386/pr64317.c
@@ -0,0 +1,24 @@
+/* { dg-do compile { target { ia32 } } } */
+/* { dg-options "-O2 -fPIE -pie" } */
+/* { dg-final { scan-assembler "addl\[ \\t\]+\[$\]_GLOBAL_OFFSET_TABLE_, %ebx" } } */
+/* { dg-final { scan-assembler "movl\[ \\t\]+c@GOT\[(\]%ebx\[)\]" } } */
+/* { dg-final { scan-assembler-not "movl\[ \\t\]+\[0-9]+\[(\]%esp\[)\], %ebx" } } */
+long c;
+
+int bar();
+
+int foo (unsigned int iters)
+{
+  unsigned int i;
+  
+  int res = 0;
+  static long t1;
+  
+  for (i = 0; i < iters; i++)
+    {
+      res = bar();
+      t1 = c + res;
+    }
+  
+  return t1 + res;
+}
