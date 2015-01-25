949e2a9f430a5baa543cfd1a81f59340fff3ef53
jb <jb@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-01-25 23:04:50 +0000

    PR 64770 Make testcase work properly under DejaGNU.
    2015-01-26  Janne Blomqvist  <jb@gcc.gnu.org>
    
    	PR libfortran/64770
    	* gfortran.dg/open_new_segv.f90: Add dg- stuff to make test work
    	correctly, clean up afterwards.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@220098 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/open_new_segv.f90
+++ gcc/testsuite/gfortran.dg/open_new_segv.f90
@@ -1,4 +1,5 @@
 ! { dg-do run }
+! { dg-shouldfail "File already exists" }
 ! PR 64770 SIGSEGV when trying to open an existing file with status="new"
 program pr64770
   implicit none
@@ -8,3 +9,6 @@ program pr64770
   open(99, file="pr64770test.dat", access="stream", form="unformatted", &
        status="new")
 end program pr64770
+! { dg-output "At line 10 of file.*" }
+! { dg-output "Fortran runtime error: File .pr64770test.dat. already exists" }
+! { dg-final { remote_file build delete "pr64770test.dat" } }
