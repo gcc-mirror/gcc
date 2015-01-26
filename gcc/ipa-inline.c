4abdcfcbb7e7374120b6a4ded88cb7b08e08b1c4
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-01-26 21:33:11 +0000

    PR ipa/64730 	* ipa-inline.c (inline_small_functions): Print "unknown" even 	if edge->call_stmt is non-NULL, but has builtins or unknown 	location.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@220139 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ipa-inline.c
+++ gcc/ipa-inline.c
@@ -1822,6 +1822,9 @@ inline_small_functions (void)
 		   " Estimated badness is %f, frequency %.2f.\n",
 		   edge->caller->name (), edge->caller->order,
 		   edge->call_stmt
+		   && (LOCATION_LOCUS (gimple_location ((const_gimple)
+							edge->call_stmt))
+		       > BUILTINS_LOCATION)
 		   ? gimple_filename ((const_gimple) edge->call_stmt)
 		   : "unknown",
 		   edge->call_stmt
