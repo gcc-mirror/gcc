e81fb91492ee4cba1bc5a31a45e74012a83e3d48
clyon <clyon@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-01-26 15:47:05 +0000

    [ARM/AArch64][testsuite] Add vpadd, vpmax and vpmin tests.
    2015-01-26  Christophe Lyon  <christophe.lyon@linaro.org>
    
    	* gcc.target/aarch64/advsimd-intrinsics/vpXXX.inc: New file.
    	* gcc.target/aarch64/advsimd-intrinsics/vpadd.c: New file.
    	* gcc.target/aarch64/advsimd-intrinsics/vpmax.c: New file.
    	* gcc.target/aarch64/advsimd-intrinsics/vpmin.c: New file.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@220119 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/aarch64/advsimd-intrinsics/vpmin.c
@@ -0,0 +1,20 @@
+#include <arm_neon.h>
+#include "arm-neon-ref.h"
+#include "compute-ref-data.h"
+
+
+#define INSN_NAME vpmin
+#define TEST_MSG "VPMIN"
+
+/* Expected results.  */
+VECT_VAR_DECL(expected,int,8,8) [] = { 0xf0, 0xf2, 0xf4, 0xf6,
+				       0xf0, 0xf2, 0xf4, 0xf6 };
+VECT_VAR_DECL(expected,int,16,4) [] = { 0xfff0, 0xfff2, 0xfff0, 0xfff2 };
+VECT_VAR_DECL(expected,int,32,2) [] = { 0xfffffff0, 0xfffffff0 };
+VECT_VAR_DECL(expected,uint,8,8) [] = { 0xf0, 0xf2, 0xf4, 0xf6,
+					0xf0, 0xf2, 0xf4, 0xf6 };
+VECT_VAR_DECL(expected,uint,16,4) [] = { 0xfff0, 0xfff2, 0xfff0, 0xfff2 };
+VECT_VAR_DECL(expected,uint,32,2) [] = { 0xfffffff0, 0xfffffff0 };
+VECT_VAR_DECL(expected,hfloat,32,2) [] = { 0xc1800000, 0xc1800000 };
+
+#include "vpXXX.inc"
