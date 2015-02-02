c58bfeadfa5d5a2d883bff78ec7db8cc77dc5463
David Malcolm <dmalcolm@redhat.com>
2015-02-02 16:11:15 +0000

    PR jit/64810: fix for arm_option_override
    gcc/ChangeLog:
    	PR jit/64810
    	* config/arm/arm.c (arm_option_override): Set
    	arm_selected_arch/cpu/tune to NULL on entry.
    
    From-SVN: r220351

--- gcc/config/arm/arm.c
+++ gcc/config/arm/arm.c
@@ -2627,6 +2627,10 @@ arm_gimplify_va_arg_expr (tree valist, tree type, gimple_seq *pre_p,
 static void
 arm_option_override (void)
 {
+  arm_selected_arch = NULL;
+  arm_selected_cpu = NULL;
+  arm_selected_tune = NULL;
+
   if (global_options_set.x_arm_arch_option)
     arm_selected_arch = &all_architectures[arm_arch_option];
 
