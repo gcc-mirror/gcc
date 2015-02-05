f13d3432b42029472050c5a87248fab701a44669
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-02-05 10:37:05 +0000

    PR rtl-optimization/64905 	* gcc.target/i386/pr64905.c: Require nonpic target. 	(dg-options): Add -fomit-frame-pointer. 	(main): Remove.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@220441 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/pr64905.c
+++ gcc/testsuite/gcc.target/i386/pr64905.c
@@ -1,22 +1,13 @@
-/* { dg-do compile { target { ! ia32 } } } */
-/* { dg-options "-Os -ffixed-rax -ffixed-rbx -ffixed-rcx -ffixed-rdx -ffixed-rdi -ffixed-rsi -ffixed-r8 -ffixed-r9 -ffixed-r10 -ffixed-r11 -ffixed-r12 -ffixed-r13 -ffixed-r14 -ffixed-r15" } */
+/* { dg-do compile { target { { ! ia32 } && nonpic } } } */
+/* { dg-options "-Os -fomit-frame-pointer -ffixed-rax -ffixed-rbx -ffixed-rcx -ffixed-rdx -ffixed-rdi -ffixed-rsi -ffixed-r8 -ffixed-r9 -ffixed-r10 -ffixed-r11 -ffixed-r12 -ffixed-r13 -ffixed-r14 -ffixed-r15" } */
 /* { dg-final { scan-assembler-not "movl\[ \t\]0\\(%.*\\), %.*" } } */
 
 typedef unsigned short uint16_t;
 uint16_t a_global;
 
-void __attribute__ ((noinline))
+void
 function (uint16_t **a_p)
 {
   // unaligned access by address in %rbp: mov    0x0(%rbp),%ebp
   a_global = **a_p;
-}
-
-int main(int argc, char **argv)
-{
-  uint16_t array [4] = { 1, 2, 3, 4 };
-  uint16_t *array_elem_p = &array [3];
-
-  function (&array_elem_p);
-  return 0;
 }
