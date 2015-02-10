696814edb80cfae8840213cf3e3100d0265944bd
Richard Biener <rguenther@suse.de>
2015-02-10 11:48:47 +0000

    re PR tree-optimization/64909 (Missed vectorization with bdver1)
    2015-02-10  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/64909
    	* tree-vect-loop.c (vect_estimate_min_profitable_iters): Properly
    	pass a scalar-stmt count estimate to the cost model.
    	* tree-vect-data-refs.c (vect_peeling_hash_get_lowest_cost): Likewise.
    
    	* gcc.dg/vect/costmodel/x86_64/costmodel-pr64909.c: New testcase.
    
    From-SVN: r220580

---
+++ gcc/testsuite/gcc.dg/vect/costmodel/x86_64/costmodel-pr64909.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target vect_int } */
+/* { dg-additional-options "-mtune=bdver1" } */
+
+unsigned short a[32];
+unsigned int b[32];
+void t()
+{
+  int i;
+  for (i=0;i<12;i++)
+    b[i]=a[i];
+}
+
+/* { dg-final { scan-tree-dump "vectorized 1 loops in function" "vect" } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
