675f18129007fbd8230f84b786c5a4f6d9d0e155
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-02-10 00:38:46 +0000

    PR ipa/64978 	* ipa-cp.c (gather_caller_stats): Skip thunks. 	(propagate_constants_topo): Skip aliases.
    	* g++.dg/torture/pr64978.C: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@220557 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/torture/pr64978.C
@@ -0,0 +1,30 @@
+/* { dg-do compile } */
+class A {
+public:
+  A(int, int);
+};
+class B {
+public:
+  void m_fn1(bool, const int *, int &);
+  unsigned m_fn2();
+};
+namespace {
+class C {
+  B &VTables;
+  void m_fn3(A, unsigned, const int *, int &);
+
+public:
+  int VFTableBuilder_VisitedBases;
+  B VFTableBuilder_VTables;
+  C() : VTables(VFTableBuilder_VTables) {
+    m_fn3(A(0, 0), 0, 0, VFTableBuilder_VisitedBases);
+  }
+};
+}
+int a;
+void C::m_fn3(A, unsigned, const int *, int &) {
+  for (;;)
+    1 ? VTables.m_fn2() : 0;
+}
+void B::m_fn1(bool, const int *, int &) { C(); }
+unsigned B::m_fn2() { m_fn1(0, 0, a); }
