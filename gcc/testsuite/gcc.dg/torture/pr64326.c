b2f2a1c99a71af82ae7af9fa2a1f5fd80b1ee226
Markus Trippelsdorf <trippels@gcc.gnu.org>
2015-02-10 15:10:55 +0000

    Fix formating of gcc.dg/torture/pr64326.c
    From-SVN: r220586

--- gcc/testsuite/gcc.dg/torture/pr64326.c
+++ gcc/testsuite/gcc.dg/torture/pr64326.c
@@ -6,25 +6,24 @@ char g;
 int
 fn1 ()
 {
-	  return d && c ? 0 : 1;
+  return d && c ? 0 : 1;
 }
 
 int
 fn2 ()
 {
-	  int h;
-	    for (;;)
-		        for (; e;)
-				      {
-					              int i, j;
-						              h = a ? 1 : b;
-							              if (h || fn1 () ^ g - 682)
-									                {
-												            for (i = 0; i < 5; i++)
-														                  for (j = 0; j < 2; j++)
-																	                  f[i][j] = 0;
-													                return 0;
-															          }
-								            }
+  int h;
+  for (;;)
+    for (; e;)
+      {
+	int i, j;
+	h = a ? 1 : b;
+	if (h || fn1 () ^ g - 682)
+	  {
+	    for (i = 0; i < 5; i++)
+	      for (j = 0; j < 2; j++)
+		f[i][j] = 0;
+	    return 0;
+	  }
+      }
 }
-
