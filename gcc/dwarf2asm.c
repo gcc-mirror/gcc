2cea03980164357c506c276b3687b463de00ad0c
Jakub Jelinek <jakub@redhat.com>
2015-02-12 22:19:47 +0100

    dwarf2asm.c (dw2_asm_output_vms_delta): Only define if ASM_OUTPUT_DWARF_VMS_DELTA is defined.
    	* dwarf2asm.c (dw2_asm_output_vms_delta): Only define
    	if ASM_OUTPUT_DWARF_VMS_DELTA is defined.
    	* dwarf2out.c (output_die): Use dw2_asm_output_vms_delta
    	only if ASM_OUTPUT_DWARF_VMS_DELTA is defined.
    
    From-SVN: r220660

--- gcc/dwarf2asm.c
+++ gcc/dwarf2asm.c
@@ -159,6 +159,7 @@ dw2_asm_output_delta (int size, const char *lab1, const char *lab2,
   va_end (ap);
 }
 
+#ifdef ASM_OUTPUT_DWARF_VMS_DELTA
 /* Output the difference between two symbols in instruction units
    in a given size.  */
 
@@ -171,11 +172,6 @@ dw2_asm_output_vms_delta (int size ATTRIBUTE_UNUSED,
 
   va_start (ap, comment);
 
-#ifndef ASM_OUTPUT_DWARF_VMS_DELTA
-  /* VMS Delta is only special on ia64-vms, but this function also gets
-     called on alpha-vms so it has to do something sane.  */
-  dw2_asm_output_delta (size, lab1, lab2, comment);
-#else
   ASM_OUTPUT_DWARF_VMS_DELTA (asm_out_file, size, lab1, lab2);
   if (flag_debug_asm && comment)
     {
@@ -183,10 +179,10 @@ dw2_asm_output_vms_delta (int size ATTRIBUTE_UNUSED,
       vfprintf (asm_out_file, comment, ap);
     }
   fputc ('\n', asm_out_file);
-#endif
 
   va_end (ap);
 }
+#endif
 
 /* Output a section-relative reference to a LABEL, which was placed in
    BASE.  In general this can only be done for debugging symbols.
