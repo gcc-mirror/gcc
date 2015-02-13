785f21af82139f512eb12f3318899c9f967409e6
Jason Merrill <jason@redhat.com>
2015-02-13 00:26:37 -0500

    re PR c++/64956 (__GXX_ABI_VERSION needs a proper definition for the 5.x releases)
    	PR c++/64956
    	* c-opts.c (c_common_post_options): Change flag_abi_version from 0
    	to the current highest version.
    	* c-cppbuiltin.c (c_cpp_builtins): Assert that it isn't 0.
    
    From-SVN: r220675

--- gcc/c-family/c-opts.c
+++ gcc/c-family/c-opts.c
@@ -886,6 +886,11 @@ c_common_post_options (const char **pfilename)
 	warn_abi = false;
     }
 
+  /* Change flag_abi_version to be the actual current ABI level for the
+     benefit of c_cpp_builtins.  */
+  if (flag_abi_version == 0)
+    flag_abi_version = 8;
+
   if (cxx_dialect >= cxx11)
     {
       /* If we're allowing C++0x constructs, don't warn about C++98
