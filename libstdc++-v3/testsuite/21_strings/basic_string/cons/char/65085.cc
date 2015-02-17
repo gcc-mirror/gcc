dfc7d6656222f778eeda7ac62fb9ee95334c8057
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-02-17 12:44:26 +0000

    PR libstdc++/65085 	* include/bits/basic_string.h (basic_string(basic_string&&)): Ensure 	empty string gets null-terminated. 	* testsuite/21_strings/basic_string/cons/char/65085.cc: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@220758 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/21_strings/basic_string/cons/char/65085.cc
@@ -0,0 +1,40 @@
+// Copyright (C) 2015 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++11" }
+
+#include <string>
+#include <new>
+#include <cstring>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  std::string s;
+  char buf[sizeof(std::string)];
+  std::memset(buf, 0xff, sizeof(buf));
+  std::string* p = ::new(buf) std::string(std::move(s));
+  VERIFY((*p)[0] == '\0');
+  p->~basic_string();
+}
+
+int
+main()
+{
+  test01();
+}
