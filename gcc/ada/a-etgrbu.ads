9a521e17fa8e0dcaeb60fcb4b202e3e8627c8047
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-02-20 13:55:31 +0000

    Code clean ups.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@220858 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/a-etgrbu.ads
+++ gcc/ada/a-etgrbu.ads
@@ -6,10 +6,30 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
+--            Copyright (C) 2015, Free Software Foundation, Inc.            --
+--                                                                          --
 -- This specification is derived from the Ada Reference Manual for use with --
--- GNAT.  In accordance with the copyright of that document, you can freely --
--- copy and modify this specification,  provided that if you redistribute a --
--- modified version,  any changes that you have made are clearly indicated. --
+-- GNAT. The copyright notice above, and the license provisions that follow --
+-- apply solely to the  contents of the part following the private keyword. --
+--                                                                          --
+-- GNAT is free software;  you can  redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.                                     --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+-- GNAT was originally developed  by the GNAT team at  New York University. --
+-- Extensive contributions were provided by Ada Core Technologies Inc.      --
 --                                                                          --
 ------------------------------------------------------------------------------
 
@@ -22,13 +42,14 @@
 --  an appropriate body provided.
 
 with System;
+with System.Multiprocessors;
 
 package Ada.Execution_Time.Group_Budgets is
-   pragma Preelaborate;
-
    pragma Unimplemented_Unit;
 
-   type Group_Budget is tagged limited private;
+   type Group_Budget
+     (CPU : System.Multiprocessors.CPU := System.Multiprocessors.CPU'First)
+   is tagged limited private;
 
    type Group_Budget_Handler is access
       protected procedure (GB : in out Group_Budget);
@@ -83,5 +104,7 @@ package Ada.Execution_Time.Group_Budgets is
    Group_Budget_Error : exception;
 
 private
-   type Group_Budget is tagged limited null record;
+   type Group_Budget
+     (CPU : System.Multiprocessors.CPU := System.Multiprocessors.CPU'First)
+   is tagged limited null record;
 end Ada.Execution_Time.Group_Budgets;
