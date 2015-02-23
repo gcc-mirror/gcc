0054cf60721b3cc44239015aed4913e0c1c83144
Andreas Krebbel <Andreas.Krebbel@de.ibm.com>
2015-02-23 08:13:51 +0000

    backport: hotpatch-1.c: Remove --save-temps option.
    Backport from mainline
    2015-02-23  Andreas Krebbel  <Andreas.Krebbel@de.ibm.com>
    
    	* gcc.target/s390/hotpatch-1.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-10.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-11.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-12.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-13.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-14.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-15.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-16.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-17.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-18.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-19.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-2.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-20.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-3.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-4.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-5.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-6.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-7.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-8.c: Remove --save-temps option.
    	* gcc.target/s390/hotpatch-9.c: Remove --save-temps option.
    	* gcc.target/s390/htm-nofloat-1.c: Cleanup --save-temps files.
    
    From-SVN: r220908

--- gcc/testsuite/gcc.target/s390/hotpatch-2.c
+++ gcc/testsuite/gcc.target/s390/hotpatch-2.c
@@ -1,7 +1,7 @@
 /* Functional tests for the function hotpatching feature.  */
 
 /* { dg-do compile } */
-/* { dg-options "-O3 -mzarch -mhotpatch=0,1 --save-temps" } */
+/* { dg-options "-O3 -mzarch -mhotpatch=0,1" } */
 
 #include <stdio.h>
 
