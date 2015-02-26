531f265a4dc6aae54c4a4d12226a98b5dfde5005
Tom de Vries <tom@codesourcery.com>
2015-02-26 14:25:30 +0000

    Add missing cleanup in gcc.target/i386/mcount_pic.c
    2015-02-26  Tom de Vries  <tom@codesourcery.com>
    
    	* gcc.target/i386/mcount_pic.c: Add missing cleanup-profile-file.
    
    From-SVN: r221012

--- gcc/testsuite/gcc.target/i386/mcount_pic.c
+++ gcc/testsuite/gcc.target/i386/mcount_pic.c
@@ -13,3 +13,4 @@ int main ()
 /* { dg-final { scan-assembler "mcount" } } */
 /* { dg-final { scan-assembler "get_pc_thunk" { xfail { *-*-solaris* && { ! gld } } } } } */
 /* { dg-final { cleanup-saved-temps } } */
+/* { dg-final { cleanup-profile-file } } */
