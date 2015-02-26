b9903eb32d7d396dce0f3898ea186d93577a2158
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-02-26 13:56:39 +0000

    PR tree-optimization/65048 	* tree-ssa-threadupdate.c (valid_jump_thread_path): New. 	(thread_through_all_blocks): Call valid_jump_thread_path. 	Remove invalid FSM jump-thread paths.
    	PR tree-optimization/65048
    	* gcc.dg/tree-ssa/ssa-dom-thread-9.c: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@221007 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-dom-thread-9.c
@@ -0,0 +1,50 @@
+/* PR 65048 */
+/* { dg-do compile } */
+/* { dg-options "-O3" } */
+
+int a, b, c, d;
+void fn (void);
+
+int
+foo (x)
+{
+  switch (x)
+    {
+    case 'A':
+      return 'T';
+    case 'U':
+      return 'A';
+    }
+}
+
+void
+bar (int x, int y)
+{
+  switch (c)
+    {
+    case 'U':
+      switch (x)
+	{
+	default:
+	  fn ();
+	case 'G':
+	  switch (y)
+	    {
+	    case 'A':
+	      d = 7;
+	    }
+	}
+    }
+}
+
+void
+baz (void)
+{
+  while (1)
+    {
+      a = foo ();
+      b = foo ();
+      bar (a, b);
+    }
+}
+
