892927b7762271038bf47dcaf0f4e39dbb7fdf24
Vladimir Makarov <vmakarov@redhat.com>
2015-02-27 14:15:02 +0000

    re PR target/65032 (ICE in reload_combine_note_use, at postreload.c:1556 on i686-linux-gnu)
    2015-02-27  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR target/65032
    	* lra-remat.c (update_scratch_ops): New.
    	(do_remat): Call it.
    	* lra.c (lra_register_new_scratch_op): New. Take code from ...
    	(remove_scratches): ... here.
    	* lra-int.h (lra_register_new_scratch_op): New prototype.
    
    2015-02-27  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR target/65032
    	* g++.dg/pr65032.C: New.
    
    From-SVN: r221062

--- gcc/lra-int.h
+++ gcc/lra-int.h
@@ -321,6 +321,7 @@ extern void lra_create_copy (int, int, int);
 extern lra_copy_t lra_get_copy (int);
 extern bool lra_former_scratch_p (int);
 extern bool lra_former_scratch_operand_p (rtx_insn *, int);
+extern void lra_register_new_scratch_op (rtx_insn *, int);
 
 extern int lra_new_regno_start;
 extern int lra_constraint_new_regno_start;
