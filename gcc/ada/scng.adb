c03b765d67c460c0766116630b4cb31752028baa
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-03-02 13:49:31 +0000

    2015-03-02  Robert Dewar  <dewar@adacore.com>
    	* scng.adb (Scan): Ignore illegal character in relaxed
    	semantics mode.
    
    2015-03-02  Ed Schonberg  <schonberg@adacore.com>
    
    	* sem_ch4.adb (Analyze_Set_Membership); Retain Overloaded flag
    	on left operand, so it can be properly resolved with type of
    	alternatives of right operand.
    	* sem_res.adb (Resolve_Set_Membership): Handle properly an
    	overloaded left-hand side when the alternatives on the right
    	hand side are literals of some universal type.	Use first
    	non-overloaded alternative to find expected type.
    
    2015-03-02  Ed Schonberg  <schonberg@adacore.com>
    
    	* exp_ch7.adb (Make_Set_Finalize_Address_Call): Use underlying
    	type to retrieve designated type, because the purported access
    	type may be a partial (private) view, when it is declared in
    	the private part of a nested package, and finalization actions
    	are generated when completing compilation of enclosing unit.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@221116 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/scng.adb
+++ gcc/ada/scng.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---          Copyright (C) 1992-2014, Free Software Foundation, Inc.         --
+--          Copyright (C) 1992-2015, Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -1757,10 +1757,15 @@ package body Scng is
                   then
                      Scan_Ptr := Scan_Ptr + 1;
 
-                  --  Otherwise we have an illegal comment character
+                  --  Otherwise we have an illegal comment character, ignore
+                  --  this error in relaxed semantics mode.
 
                   else
-                     Error_Illegal_Character;
+                     if Relaxed_RM_Semantics then
+                        Scan_Ptr := Scan_Ptr + 1;
+                     else
+                        Error_Illegal_Character;
+                     end if;
                   end if;
                end loop;
 
