401140215ac6d6699e9abc29eda56d11fce0b2a2
gjl <gjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-03-03 11:25:04 +0000

    gcc/ 	PR target/64331 	* config/avr/avr.c (context.h, tree-pass.h): Include them. 	(avr_pass_data_recompute_notes): New static variable. 	(avr_pass_recompute_notes): New class. 	(avr_register_passes): New static function. 	(avr_option_override): Call it. gcc/testsuite/ 	PR target/64331 	* gcc.target/avr/torture/pr64331.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@221143 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/avr/torture/pr64331.c
@@ -0,0 +1,37 @@
+/* { dg-do run } */
+
+typedef struct
+{
+  unsigned a, b;
+} T2;
+
+
+__attribute__((__noinline__, __noclone__))
+void foo2 (T2 *t, int x)
+{
+  if (x != t->a)
+    {
+      t->a = x;
+  
+      if (x && x == t->b)
+	t->a = 20;
+    }
+}
+
+
+T2 t;
+
+int main (void)
+{
+  t.a = 1;
+  t.b = 1234;
+
+  foo2 (&t, 1234);
+
+  if (t.a != 20)
+    __builtin_abort();
+
+  __builtin_exit (0);
+
+  return 0;
+}
