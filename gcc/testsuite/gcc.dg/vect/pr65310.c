b162e1e749b8c7cb567e5f38bbc461d77c9f8439
Richard Biener <rguenther@suse.de>
2015-03-05 09:14:01 +0000

    re PR tree-optimization/65310 (vectorizer uses wrong alignment)
    2015-03-05  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/65310
    	* tree-vect-data-refs.c (vect_compute_data_ref_alignment):
    	Properly preserve alignment of the base of the access.
    
    	* gcc.dg/vect/pr65310.c: New testcase.
    
    From-SVN: r221206

---
+++ gcc/testsuite/gcc.dg/vect/pr65310.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+
+struct a 
+{
+  int a[100];
+};
+typedef struct a b __attribute__ ((aligned (32)));
+typedef struct a c __attribute__ ((aligned (8)));
+
+int t(b *a)
+{
+  int i;
+  c *ptr = a;
+  for (i=0;i<100;i++)
+    ptr->a[i]++;
+}
+
+/* The memory access is through a pointer of type c which means
+   *ptr is not aligned.  */
+
+/* { dg-final { scan-tree-dump "can't force alignment" "vect" } } */
+/* { dg-final { scan-tree-dump-not "misalign = 0" "vect" } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
