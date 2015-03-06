2c7f8ba5553336092ad3ac0a3a610ee9fedaf3b1
James Greenhalgh <james.greenhalgh@arm.com>
2015-03-06 16:03:38 +0000

    [ARM testsuite obvious] Fixup atomic-comp-swap-release-acquire.c to not use ICF
    gcc/
    
    	* gcc.target/arm/atomic-comp-swap-release-acquire.c: Add
    	-fno-ipa-icf to dg-options.
    
    From-SVN: r221243

--- gcc/testsuite/gcc.target/arm/atomic-comp-swap-release-acquire.c
+++ gcc/testsuite/gcc.target/arm/atomic-comp-swap-release-acquire.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target arm_arch_v8a_ok } */
-/* { dg-options "-O2" } */
+/* { dg-options "-O2 -fno-ipa-icf" } */
 /* { dg-add-options arm_arch_v8a } */
 
 #include "../aarch64/atomic-comp-swap-release-acquire.x"
