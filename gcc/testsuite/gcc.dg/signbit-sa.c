11403457ec79ef0f9bba9f8fe99d6efecd34e606
John David Anglin <danglin@gcc.gnu.org>
2015-03-08 22:45:32 +0000

    signbit-sa.c: Add dg-require-effective-target c99_runtime
    	* gcc.dg/signbit-sa.c: Add dg-require-effective-target c99_runtime
    
    From-SVN: r221266

--- gcc/testsuite/gcc.dg/signbit-sa.c
+++ gcc/testsuite/gcc.dg/signbit-sa.c
@@ -1,5 +1,6 @@
 /* Some versions of Solaris <math.h> give strict-aliasing warnings for
    signbit.  */
+/* { dg-require-effective-target c99_runtime } */
 /* { dg-options "-std=c99 -O2 -Wstrict-aliasing" } */
 
 #include <math.h>
