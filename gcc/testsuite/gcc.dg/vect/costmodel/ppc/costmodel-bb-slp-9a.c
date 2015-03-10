76b9caed0a095ebca5b0af4ed9906ffad4e94cb1
Martin Sebor <msebor@redhat.com>
2015-03-10 21:06:41 +0000

    re PR testsuite/63175 (FAIL: gcc.dg/vect/costmodel/ppc/costmodel-bb-slp-9a.c scan-tree-dump-times slp2" basic block vectorized using SLP" 1)
    2015-03-10  Martin Sebor  <msebor@redhat.com>
    
    	PR testsuite/63175
    	* gcc.dg/vect/costmodel/ppc/costmodel-bb-slp-9a.c (main1): Move
    	checking of results into main to prevent it from getting optimized
    	away.
    	* gcc.dg/vect/costmodel/ppc/costmodel-bb-slp-9a-pr63175.c: New test.
    
    From-SVN: r221336

--- gcc/testsuite/gcc.dg/vect/costmodel/ppc/costmodel-bb-slp-9a.c
+++ gcc/testsuite/gcc.dg/vect/costmodel/ppc/costmodel-bb-slp-9a.c
@@ -1,6 +1,5 @@
 /* { dg-require-effective-target vect_int } */
 
-#include <stdarg.h>
 #include "../../tree-vect.h"
 
 #define N 16 
@@ -9,18 +8,25 @@ unsigned int out[N];
 unsigned int in[N] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
 __attribute__ ((noinline)) int
-main1 (unsigned int x, unsigned int y)
+main1 (void)
 {
-  int i;
   unsigned int *pin = &in[1];
   unsigned int *pout = &out[0];
-  unsigned int a0, a1, a2, a3;
 
   /* Misaligned load.  */
   *pout++ = *pin++;
   *pout++ = *pin++;
   *pout++ = *pin++;
   *pout++ = *pin++;
+
+  return 0;
+}
+
+int main (void)
+{
+  check_vect ();
+
+  main1 ();
 
   /* Check results.  */
   if (out[0] != in[1]
@@ -32,15 +38,6 @@ main1 (unsigned int x, unsigned int y)
   return 0;
 }
 
-int main (void)
-{
-  check_vect ();
-
-  main1 (2, 3);
-
-  return 0;
-}
-
-/* { dg-final { scan-tree-dump-times "basic block vectorized using SLP" 1 "slp"  { xfail  vect_no_align } } } */
+/* { dg-final { scan-tree-dump-times "basic block vectorized" 1 "slp"  { xfail  vect_no_align } } } */
 /* { dg-final { cleanup-tree-dump "slp" } } */
   
