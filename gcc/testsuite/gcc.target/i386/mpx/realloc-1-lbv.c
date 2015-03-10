3e0baf2a6a86aa44369df1984563905dcb776b37
ienkovich <ienkovich@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-03-10 09:48:51 +0000

    * lib/mpx-dg.exp (mpx_link_flags): Set path to wrappers library. 	* gcc.target/i386/mpx/calloc-1-lbv.c: New. 	* gcc.target/i386/mpx/calloc-1-ubv.c: New. 	* gcc.target/i386/mpx/calloc-1-nov.c: New. 	* gcc.target/i386/mpx/malloc-1-lbv.c: New. 	* gcc.target/i386/mpx/malloc-1-nov.c: New. 	* gcc.target/i386/mpx/malloc-1-ubv.c: New. 	* gcc.target/i386/mpx/realloc-1-lbv.c: New. 	* gcc.target/i386/mpx/realloc-1-nov.c: New. 	* gcc.target/i386/mpx/realloc-1-ubv.c: New. 	* gcc.target/i386/mpx/realloc-2-lbv.c: New. 	* gcc.target/i386/mpx/realloc-2-nov.c: New. 	* gcc.target/i386/mpx/realloc-2-ubv.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@221315 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/mpx/realloc-1-lbv.c
@@ -0,0 +1,27 @@
+/* { dg-do run } */
+/* { dg-shouldfail "bounds violation" } */
+/* { dg-options "-fcheck-pointer-bounds -mmpx" } */
+
+
+#define SHOULDFAIL
+
+#include "mpx-check.h"
+
+int rd (int *p, int i)
+{
+  int res = p[i];
+  printf ("%d\n", res);
+  return res;
+}
+
+int mpx_test (int argc, const char **argv)
+{
+  int *buf = (int *)malloc (100 * sizeof(int));
+  buf = (int *)realloc (buf, 200 * sizeof(int));
+
+  rd (buf, -1);
+
+  free (buf);
+
+  return 0;
+}
