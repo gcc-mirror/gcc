48188959fd8a9bb1d7da833466f314210693bc11
Paul Thomas <pault@gcc.gnu.org>
2015-03-10 19:39:05 +0000

    re PR fortran/65024 ([OOP] unlimited polymorphic pointer structure not built when it should be)
    2015-03-10  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/65024
    	* trans-expr.c (gfc_conv_component_ref): If the component
    	backend declaration is missing and the derived type symbol is
    	available in the reference, call gfc_build_derived_type.
    
    2015-03-10  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/65024
    	* gfortran.dg/unlimited_polymorphic_23.f90: New test
    
    From-SVN: r221334

---
+++ gcc/testsuite/gfortran.dg/unlimited_polymorphic_23.f90
@@ -0,0 +1,35 @@
+! {dg-do run }
+!
+! Test the fix for PR65024, in which the structure for the 'info'
+! component of type 'T' was not being converted into TREE_SSA and
+! so caused an ICE in trans-expr.c:gfc_conv_component_ref.
+!
+! Reported by  <matt@gneilson.plus.com>
+!
+MODULE X
+  TYPE T
+    CLASS(*), pointer :: info
+  END TYPE
+END MODULE
+
+PROGRAM P
+  call bug
+CONTAINS
+  SUBROUTINE BUG
+    USE X
+    CLASS(T), pointer :: e
+    integer, target :: i = 42
+    allocate(e)
+    e%info => NULL ()      ! used to ICE
+    if (.not.associated(e%info)) e%info => i      ! used to ICE
+    select type (z => e%info)
+      type is (integer)
+        if (z .ne.i) call abort
+    end select
+  END SUBROUTINE
+
+  SUBROUTINE NEXT
+    USE X
+    CLASS (T), pointer :: e
+  END SUBROUTINE
+END
