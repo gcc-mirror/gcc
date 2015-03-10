5a434178855e784d2667d68f0acf3c01bf48e847
James Greenhalgh <james.greenhalgh@arm.com>
2015-03-10 09:29:04 +0000

    [Patch testsuite] Further reduce the alignment in gcc.dg/vect/pr65310.c
    gcc/testsuite/
    
    	* gcc.dg/vect/pr65310.c (c): Reduce alignment to 4-bytes.
    
    From-SVN: r221310

--- gcc/testsuite/gcc.dg/vect/pr65310.c
+++ gcc/testsuite/gcc.dg/vect/pr65310.c
@@ -5,7 +5,7 @@ struct a
   int a[100];
 };
 typedef struct a b __attribute__ ((aligned (32)));
-typedef struct a c __attribute__ ((aligned (8)));
+typedef struct a c __attribute__ ((aligned (4)));
 
 int t(b *a)
 {
