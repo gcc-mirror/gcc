6cf6683f55b5e1b772d4bfc504c512390846470c
Uros Bizjak <uros@gcc.gnu.org>
2015-03-10 08:47:43 +0100

    ipa-icf-36.c (dg-final): Cleanup icf ipa dump.
    	* gcc.dg/ipa/ipa-icf-36.c (dg-final): Cleanup icf ipa dump.
    	* gcc.dg/ipa/ipa-icf-37.c (dg-final): Ditto.
    
    From-SVN: r221304

--- gcc/testsuite/gcc.dg/ipa/ipa-icf-36.c
+++ gcc/testsuite/gcc.dg/ipa/ipa-icf-36.c
@@ -34,3 +34,4 @@ int t(int tt)
 /* { dg-final { scan-ipa-dump "Semantic equality hit:f->e" "icf"  } } */
 /* { dg-final { scan-ipa-dump "Semantic equality hit:h->g" "icf"  } } */
 /* { dg-final { scan-ipa-dump "Semantic equality hit:k->i" "icf"  } } */
+/* { dg-final { cleanup-ipa-dump "icf" } } */
