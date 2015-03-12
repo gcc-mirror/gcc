f4252e723d153fa103a07e86408b33bb11b117c2
krebbel <krebbel@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-03-12 11:13:40 +0000

    S/390: Hotpatching fixes.
    2015-03-02  Dominik Vogt  <vogt@linux.vnet.ibm.com>
    
    	    * config/s390/s390.c (s390_reorg): Move code to output nops after label
    	    to s390_reorg ().
    	    (s390_asm_output_function_label): Likewise.
    	    * config/s390/s390.c (s390_asm_output_function_label):
    	    Fix function label alignment with -mhtopatch.
    	    * config/s390/s390.md ("unspecv"): New values UNSPECV_NOP_2_BYTE,
    	    UNSPECV_NOP_4_BYTE and UNSPECV_NOP_6_BYTE
    	    ("nop_2_byte"): New define_insn.
    	    ("nop_4_byte"): Likewise.
    	    ("nop_6_byte"): Likewise.
    	    * doc/extend.texi (hotpatch): hotpatch attribute doc fixes.
    	    * doc/invoke.texi (-mhotpatch): -mhotpatch doc fixes.
    
    2015-03-02  Dominik Vogt  <vogt@linux.vnet.ibm.com>
    
    	    * gcc.target/s390/hotpatch-21.c: New test for hotpatch alignment.
    	    * gcc.target/s390/hotpatch-22.c: Likewise.
    	    * gcc.target/s390/hotpatch-23.c: Likewise.
    	    * gcc.target/s390/hotpatch-24.c: Likewise.
    	    * gcc.target/s390/hotpatch-2.c: Also check hotpatch alignment.
    	    * gcc.target/s390/hotpatch-1.c: Update expected output.
    	    * gcc.target/s390/hotpatch-2.c: Likewise.
    	    * gcc.target/s390/hotpatch-3.c: Likewise.
    	    * gcc.target/s390/hotpatch-4.c: Likewise.
    	    * gcc.target/s390/hotpatch-5.c: Likewise.
    	    * gcc.target/s390/hotpatch-6.c: Likewise.
    	    * gcc.target/s390/hotpatch-7.c: Likewise.
    	    * gcc.target/s390/hotpatch-8.c: Likewise.
    	    * gcc.target/s390/hotpatch-9.c: Likewise.
    	    * gcc.target/s390/hotpatch-10.c: Likewise.
    	    * gcc.target/s390/hotpatch-11.c: Likewise.
    	    * gcc.target/s390/hotpatch-12.c: Likewise.
    	    * gcc.target/s390/hotpatch-13.c: Likewise.
    	    * gcc.target/s390/hotpatch-14.c: Likewise.
    	    * gcc.target/s390/hotpatch-15.c: Likewise.
    	    * gcc.target/s390/hotpatch-16.c: Likewise.
    	    * gcc.target/s390/hotpatch-17.c: Likewise.
    	    * gcc.target/s390/hotpatch-18.c: Likewise.
    	    * gcc.target/s390/hotpatch-19.c: Likewise.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@221381 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/s390/hotpatch-11.c
+++ gcc/testsuite/gcc.target/s390/hotpatch-11.c
@@ -13,4 +13,4 @@ void hp1(void)
 /* Check number of occurences of certain instructions.  */
 /* { dg-final { scan-assembler-times "nopr\t%r7" 1 } } */
 /* { dg-final { scan-assembler-not "nop\t0" } } */
-/* { dg-final { scan-assembler-not "brcl\t\t0,0" } } */
+/* { dg-final { scan-assembler-not "brcl\t0, 0" } } */
