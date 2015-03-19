1443772f3685beada7aae669875933cb3e8edcde
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-03-19 19:12:43 +0000

    * c-decl.c (c_decl_attributes): Also add "omp declare target" 	attribute for DECL_EXTERNAL VAR_DECLs.
    	* decl2.c (cplus_decl_attributes): Also add "omp declare target"
    	attribute for DECL_EXTERNAL VAR_DECLs.
    
    	* testsuite/libgomp.c/target-10.c: New test.
    	* testsuite/libgomp.c++/target-4.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@221520 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c/c-decl.c
+++ gcc/c/c-decl.c
@@ -4407,7 +4407,8 @@ c_decl_attributes (tree *node, tree attributes, int flags)
 {
   /* Add implicit "omp declare target" attribute if requested.  */
   if (current_omp_declare_target_attribute
-      && ((TREE_CODE (*node) == VAR_DECL && TREE_STATIC (*node))
+      && ((TREE_CODE (*node) == VAR_DECL
+	   && (TREE_STATIC (*node) || DECL_EXTERNAL (*node)))
 	  || TREE_CODE (*node) == FUNCTION_DECL))
     {
       if (TREE_CODE (*node) == VAR_DECL
