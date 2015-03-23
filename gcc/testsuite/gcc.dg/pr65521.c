f45be077ec1a40e5b3046cd19bd32e8e1ebaca73
Jakub Jelinek <jakub@redhat.com>
2015-03-23 16:17:20 +0100

    re PR ipa/65521 (nondeterministic -fcompare-debug failures)
    	PR ipa/65521
    	* ipa-icf.c (sem_item::update_hash_by_addr_refs): Hash
    	ultimate_alias_target ()->order ints instead of
    	ultimate_alias_target () pointers.
    
    	* gcc.dg/pr65521.c: New test.
    
    From-SVN: r221596

---
+++ gcc/testsuite/gcc.dg/pr65521.c
@@ -0,0 +1,39 @@
+/* PR ipa/65521 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fcompare-debug" } */
+
+struct S { int s; };
+int f6 (void *, unsigned long);
+int f7 (int, int *, unsigned long);
+int f8 (void);
+int f9 (void (*) (void));
+
+int
+f1 (void *p)
+{
+  return f6 (p, 256UL);
+}
+
+int
+f2 (void *p)
+{
+  return f6 (p, 256UL);
+}
+
+int
+f3 (struct S *x)
+{
+  return f7 (f8 (), &x->s, 16UL);
+}
+
+int
+f4 (struct S *x)
+{
+  return f7 (f8 (), &x->s, 16UL);
+}
+
+void
+f5 (void)
+{
+  f9 (f5);
+}
