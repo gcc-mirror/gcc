79613a00e36c4d6b8c1760e71070b0308b11675d
Alan Modra <amodra@gmail.com>
2015-03-26 19:03:53 +1030

    re PR regression/63150 (FAIL: gcc.target/powerpc/pr53199.c scan-assembler-times *)
    	PR target/63150
    	Backport from trunk 211857 and 221445.
    	2014-06-20  Maciej W. Rozycki  <macro@codesourcery.com>
    	* config/rs6000/rs6000.md: Append `DONE' to preparation
    	statements of `bswap' pattern splitters.
    
    	2015-03-16  Alan Modra  <amodra@gmail.com>
    	* config/rs6000/rs6000.md (bswapdi2): Remove one scratch reg.
    	Modify Z->r bswapdi splitter to use dest in place of scratch.
    	In r->Z and Z->r bswapdi splitter rename word_high, word_low
    	to word1, word2 and rearrange logic to suit.
    	(bswapdi2_64bit): Remove early clobber on Z->r alternative.
    	(bswapdi2_ldbrx): Likewise.  Remove '??' on r->r.
    	(bswapdi2_32bit): Remove early clobber on Z->r alternative.
    	Add one '?' on r->r.  Modify Z->r splitter to avoid need for
    	early clobber.
    
    From-SVN: r221689

--- gcc/testsuite/gcc.target/powerpc/pr53199.c
+++ gcc/testsuite/gcc.target/powerpc/pr53199.c
@@ -1,7 +1,7 @@
 /* { dg-do compile { target { powerpc*-*-* } } } */
 /* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
 /* { dg-options "-O2 -mcpu=power6 -mavoid-indexed-addresses" } */
-/* { dg-final { scan-assembler-times "lwbrx" 6 } } */
+/* { dg-final { scan-assembler-times "lwbrx" 12 } } */
 /* { dg-final { scan-assembler-times "stwbrx" 6 } } */
 
 /* PR 51399: bswap gets an error if -mavoid-indexed-addresses was used in
@@ -25,6 +25,24 @@ load64_reverse_3 (long long *p, int i)
   return __builtin_bswap64 (p[i]);
 }
 
+long long
+load64_reverse_4 (long long dummy __attribute__ ((unused)), long long *p)
+{
+  return __builtin_bswap64 (*p);
+}
+
+long long
+load64_reverse_5 (long long dummy __attribute__ ((unused)), long long *p)
+{
+  return __builtin_bswap64 (p[1]);
+}
+
+long long
+load64_reverse_6 (long long dummy __attribute__ ((unused)), long long *p, int i)
+{
+  return __builtin_bswap64 (p[i]);
+}
+
 void
 store64_reverse_1 (long long *p, long long x)
 {
@@ -44,7 +62,13 @@ store64_reverse_3 (long long *p, long long x, int i)
 }
 
 long long
-reg_reverse (long long x)
+reg_reverse_1 (long long x)
+{
+  return __builtin_bswap64 (x);
+}
+
+long long
+reg_reverse_2 (long long dummy __attribute__ ((unused)), long long x)
 {
   return __builtin_bswap64 (x);
 }
