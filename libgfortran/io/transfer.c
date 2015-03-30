6ed18a807948944a68d4532185582c61e4d8706a
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2015-03-30 16:51:37 +0000

    re PR libfortran/59513 (Fortran runtime error: Sequential READ or WRITE not allowed after EOF marker, possibly use REWIND or BACKSPACE)
    2015-03-30 Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/59513
    	* io/transfer.c (data_transfer_init): Do not error for
    	-std=legacy.
    
    From-SVN: r221772

--- libgfortran/io/transfer.c
+++ libgfortran/io/transfer.c
@@ -2533,15 +2533,15 @@ data_transfer_init (st_parameter_dt *dtp, int read_flag)
 	  return;
 	}
 
-      if (dtp->u.p.current_unit->endfile == AFTER_ENDFILE)
-      	{
+      if (compile_options.warn_std &&
+          dtp->u.p.current_unit->endfile == AFTER_ENDFILE)      	{
 	  generate_error (&dtp->common, LIBERROR_OPTION_CONFLICT,
 			"Sequential READ or WRITE not allowed after "
 			"EOF marker, possibly use REWIND or BACKSPACE");
 	  return;
 	}
-
     }
+
   /* Process the ADVANCE option.  */
 
   dtp->u.p.advance_status
