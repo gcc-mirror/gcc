055da6a8dfc48dae017dd17bdabaeabf57bec3d7
Ian Lance Taylor <ian@gcc.gnu.org>
2015-04-11 00:50:26 +0000

    compiler: discard carriage returns in raw string literals
    Fixes golang/go#10407.
    
    From-SVN: r222001

--- gcc/go/gofrontend/lex.cc
+++ gcc/go/gofrontend/lex.cc
@@ -1440,7 +1440,10 @@ Lex::gather_raw_string()
 	  bool issued_error;
 	  this->lineoff_ = p - this->linebuf_;
 	  p = this->advance_one_utf8_char(p, &c, &issued_error);
-	  Lex::append_char(c, true, &value, loc);
+	  // "Carriage return characters ('\r') inside raw string literals
+	  // are discarded from the raw string value."
+	  if (c != '\r')
+	      Lex::append_char(c, true, &value, loc);
 	}
       this->lineoff_ = p - this->linebuf_;
       if (!this->require_line())
