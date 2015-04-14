035762594004b36e416fde0245d2c1c412605389
Hans-Peter Nilsson <hp@axis.com>
2015-04-14 00:37:57 +0000

    Fix regression for libstdc++-v3/testsuite/29_atomics/atomic/62259.cc as...
    Fix regression for libstdc++-v3/testsuite/29_atomics/atomic/62259.cc
    as reported at <http://gcc.gnu.org/ml/gcc-patches/2015-04/msg00543.html>.
    
    	* testsuite/29_atomics/atomic/62259.cc: Assert atomic
    	alignment is larger-equal, not equal, to default alignment.
    
    From-SVN: r222072

--- libstdc++-v3/testsuite/29_atomics/atomic/62259.cc
+++ libstdc++-v3/testsuite/29_atomics/atomic/62259.cc
@@ -33,7 +33,7 @@ struct twoints {
   int32_t b;
 };
 
-static_assert( alignof(std::atomic<twoints>) == alignof(int64_t),
+static_assert( alignof(std::atomic<twoints>) >= alignof(int64_t),
                "std::atomic not suitably aligned" );
 
 // libstdc++/65147
@@ -44,7 +44,7 @@ struct power_of_two_obj {
 
 std::atomic<power_of_two_obj> obj1;
 
-static_assert( alignof(obj1) == alignof(int64_t),
+static_assert( alignof(obj1) >= alignof(int64_t),
                "std::atomic not suitably aligned" );
 
 struct container_struct {
@@ -54,5 +54,5 @@ struct container_struct {
 
 container_struct obj2;
 
-static_assert( alignof(obj2.ao) == alignof(int64_t),
+static_assert( alignof(obj2.ao) >= alignof(int64_t),
                "std::atomic not suitably aligned" );
