366ee94b445fd959c7947e45d2cb9a2a4a0b4509
Jakub Jelinek <jakub@redhat.com>
2015-04-15 13:47:44 +0200

    re PR ipa/65765 (Compiling Firefox with GCC 5 leads to broken javascript engine on x86-64)
    	PR ipa/65765
    	* ipa-icf-gimple.c (func_checker::compare_bb): For GIMPLE_NOP
    	and GIMPLE_PREDICT use break instead of return true. For
    	GIMPLE_EH_DISPATCH, compare dispatch region.
    
    	* g++.dg/ipa/pr65765.C: New test.
    
    From-SVN: r222123

--- gcc/ipa-icf-gimple.c
+++ gcc/ipa-icf-gimple.c
@@ -706,7 +706,11 @@ func_checker::compare_bb (sem_bb *bb1, sem_bb *bb2)
 	    return return_different_stmts (s1, s2, "GIMPLE_SWITCH");
 	  break;
 	case GIMPLE_DEBUG:
+	  break;
 	case GIMPLE_EH_DISPATCH:
+	  if (gimple_eh_dispatch_region (as_a <geh_dispatch *> (s1))
+	      != gimple_eh_dispatch_region (as_a <geh_dispatch *> (s2)))
+	    return return_different_stmts (s1, s2, "GIMPLE_EH_DISPATCH");
 	  break;
 	case GIMPLE_RESX:
 	  if (!compare_gimple_resx (as_a <gresx *> (s1),
@@ -734,7 +738,7 @@ func_checker::compare_bb (sem_bb *bb1, sem_bb *bb2)
 	  break;
 	case GIMPLE_PREDICT:
 	case GIMPLE_NOP:
-	  return true;
+	  break;
 	default:
 	  return return_false_with_msg ("Unknown GIMPLE code reached");
 	}
