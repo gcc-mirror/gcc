fce667e3f95a05a75c759754b6786d187f71bd43
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-04-17 17:59:24 +0000

    * config/i386/i386.h (LEGITIMIZE_RELOAD_ADDRESS): Remove. 	* config/i386/i386.c (ix86_legitimize_reload_address): Ditto. 	* config/i386/i386-protos.h (ix86_legitimize_reload_address): Ditto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@222193 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386-protos.h
+++ gcc/config/i386/i386-protos.h
@@ -67,8 +67,6 @@ extern bool ix86_expand_set_or_movmem (rtx, rtx, rtx, rtx, rtx, rtx,
 extern bool constant_address_p (rtx);
 extern bool legitimate_pic_operand_p (rtx);
 extern bool legitimate_pic_address_disp_p (rtx);
-extern bool ix86_legitimize_reload_address (rtx, machine_mode,
-					    int, int, int);
 extern void print_reg (rtx, int, FILE*);
 extern void ix86_print_operand (FILE *, rtx, int);
 
