c6f044f7664a1659618e0585c703036f472a1caa
Jason Merrill <jason@redhat.com>
2015-04-24 15:11:45 -0400

    re PR c++/50800 (Internal compiler error in finish_member_declarations, possibly related to may_alias attribute)
    	PR c++/50800
    gcc/
    	* tree.c (build_reference_type_for_mode): Don't pass can_alias_all
    	down when building TYPE_CANONICAL.
    	(build_pointer_type_for_mode): Likewise.
    gcc/cp/
    	* typeck.c (structural_comptypes): Don't check TYPE_REF_CAN_ALIAS_ALL.
    
    From-SVN: r222419

--- gcc/cp/typeck.c
+++ gcc/cp/typeck.c
@@ -1310,7 +1310,6 @@ structural_comptypes (tree t1, tree t2, int strict)
 
     case POINTER_TYPE:
       if (TYPE_MODE (t1) != TYPE_MODE (t2)
-	  || TYPE_REF_CAN_ALIAS_ALL (t1) != TYPE_REF_CAN_ALIAS_ALL (t2)
 	  || !same_type_p (TREE_TYPE (t1), TREE_TYPE (t2)))
 	return false;
       break;
