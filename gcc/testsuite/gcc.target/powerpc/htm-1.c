cb2c281a719e850b489478400c28b6c3c2294a34
bergner <bergner@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-04-27 14:52:50 +0000

    gcc/ 	PR target/64579 	* config/rs6000/htm.md: Remove all define_expands. 	(UNSPECV_HTM_TABORTDC, UNSPECV_HTM_TABORTDCI, UNSPECV_HTM_TABORTWC, 	UNSPECV_HTM_TABORTWCI): Remove. 	(UNSPECV_HTM_TABORTXC, UNSPECV_HTM_TABORTXCI, UNSPECV_HTM_TTEST): New. 	(tabort_internal, tbegin_internal, tcheck_internal, tend_internal, 	trechkpt_internal, treclaim_internal, tsr_internal): Rename from this... 	(tabort, tbegin, tcheck, tend, trechkpt, treclaim, tsr): ...to this. 	(tabortdc_internal, tabortdci_internal, tabortwc_internal, 	tabortwci_internal): Remove define_insns. 	(tabort<wd>c, tabort<wd>ci): New define_insns. 	(tabort): Use gpc_reg_operand. 	(tcheck): Remove operand. 	(htm_mfspr_<mode>, htm_mtspr_<mode>): Use GPR mode macro. 	* config/rs6000/htmxlintrin.h (__TM_end): Use _HTM_TRANSACTIONAL as 	expected value. 	* config/rs6000/rs6000-builtin.def (BU_HTM_SPR0): Remove. 	(BU_HTM_SPR1): Rename to BU_HTM_V1.  Remove use of RS6000_BTC_SPR. 	(tabort, tabortdc, tabortdci, tabortwc, tabortwci, tbegin, 	tcheck, tend, tendall, trechkpt, treclaim, tresume, tsuspend, 	tsr, ttest): Pass in the RS6000_BTC_CR attribute. 	(get_tfhar, set_tfhar, get_tfiar, set_tfiar, get_texasr, set_texasr, 	get_texasru, set_texasru): Pass in the RS6000_BTC_SPR attribute. 	(tcheck): Remove builtin argument. 	* config/rs6000/rs6000.c (rs6000_htm_spr_icode): Use TARGET_POWERPC64 	not TARGET_64BIT. 	(htm_expand_builtin): Fix usage of expandedp.  Disallow usage of the 	tabortdc and tabortdci builtins when not in 64-bit mode. 	Modify code to handle the loss of the HTM define_expands. 	Emit code to copy the CR register to TARGET. 	(htm_init_builtins): Modify code to handle the loss of the HTM 	define_expands. 	* config/rs6000/rs6000.h (RS6000_BTC_32BIT): Delete. 	(RS6000_BTC_64BIT): Likewise. 	(RS6000_BTC_CR): New macro. 	* doc/extend.texi: Update documentation for htm builtins.
    gcc/testsuite/
    
    	PR target/64579
    	* gcc.target/powerpc/htm-1.c: New test.
    	* gcc.target/powerpc/htm-builtin-1.c (__builtin_tabortdc): Only test
    	on 64-bit compiles.
    	(__builtin_tabortdci): Likewise.
    	(__builtin_tcheck): Remove operand.
    	* lib/target-supports.exp (check_htm_hw_available): New function.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@222467 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/htm-1.c
@@ -0,0 +1,52 @@
+/* { dg-do run { target { powerpc*-*-* && htm_hw } } } */
+/* { dg-require-effective-target powerpc_htm_ok } */
+/* { dg-options "-mhtm" } */
+
+/* Program to test PowerPC HTM instructions.  */
+
+#include <stdlib.h>
+#include <htmintrin.h>
+
+int
+main (void)
+{
+  long i;
+  unsigned long mask = 0;
+
+repeat:
+  if (__builtin_tbegin (0))
+    {
+      mask++;
+    }
+  else
+    abort();
+
+  if (mask == 1)
+    {
+      __builtin_tsuspend ();
+
+      if (_HTM_STATE (__builtin_tcheck ()) != _HTM_SUSPENDED)
+	abort ();
+
+      __builtin_tresume ();
+
+      if (_HTM_STATE (__builtin_tcheck ()) != _HTM_TRANSACTIONAL)
+	abort ();
+    }
+  else
+    mask++;
+
+  if (_HTM_STATE (__builtin_tendall ()) != _HTM_TRANSACTIONAL)
+    abort ();
+
+  if (mask == 1)
+    goto repeat;
+
+  if (_HTM_STATE (__builtin_tendall ()) != _HTM_NONTRANSACTIONAL)
+    abort ();
+
+  if (mask != 3)
+    abort ();
+
+  return 0;
+}
