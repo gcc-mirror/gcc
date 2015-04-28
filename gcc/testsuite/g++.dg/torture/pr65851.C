85ebc1938f831130e98e8b0865cb172487254818
Richard Biener <rguenther@suse.de>
2015-04-28 07:26:41 +0000

    re PR c++/65851 (ice in set_lattice_value at tree-ssa-cc p.c:535)
    2015-04-28  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/65851
    	* tree-ssa-ccp.c (set_lattice_value): Perform a meet when
    	changing CONSTANT to CONSTANT non-copy.  Get new_val by reference.
    	(ccp_lattice_meet): Remove stray argument.  Use operand_equal_p
    	rather than simple_cst_equal as the latter doesn't handle COMPLEX_CST.
    	(ccp_visit_phi_node): Adjust.
    	(evaluate_stmt): For simplifications to SSA names return its
    	lattice value if that isn't VARYING.  Return immediately when
    	simplified to a constant.
    	(visit_assignment): Adjust.
    	(ccp_visit_stmt): Likewise.
    
    	* g++.dg/torture/pr65851.C: New testcase.
    
    From-SVN: r222510

---
+++ gcc/testsuite/g++.dg/torture/pr65851.C
@@ -0,0 +1,37 @@
+// { dg-do compile }
+class A {
+  virtual unsigned long write(const char *, unsigned long);
+};
+char *a;
+int b;
+bool c;
+char e[16];
+class B {
+public:
+  void push_range(const char *);
+};
+class C : A {
+  B m_string;
+
+public:
+  unsigned long write(const char *p1, unsigned long p2) {
+    m_string.push_range(p1 + p2);
+  }
+};
+char *write_signed_decimal_backward(bool) {
+  char *d = 0;
+  if (b) {
+    if (c)
+      --a;
+    d = a;
+  }
+  return d;
+}
+
+template <typename TextOutputStreamType>
+void ostream_write(TextOutputStreamType &p1) {
+  char *f = write_signed_decimal_backward(false);
+  p1.write(f, e - f);
+}
+
+void operator<<(C p1, int) { ostream_write(p1); }
