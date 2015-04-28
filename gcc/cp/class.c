8c522687a0dbe28caaab2fa3ef40d1826b8e4d03
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-04-28 14:43:48 +0000

    PR c++/65734 gcc/ 	* stor-layout.c (layout_type): Layout the TYPE_MAIN_VARIANT. 	(finalize_type_size): Respect TYPE_USER_ALIGN. 	(layout_type) [ARRAY_TYPE]: Likewise. gcc/cp/ 	* class.c (fixup_attribute_variants): Respect TYPE_USER_ALIGN.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@222529 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/class.c
+++ gcc/cp/class.c
@@ -1989,14 +1989,23 @@ fixup_attribute_variants (tree t)
   if (!t)
     return;
 
+  tree attrs = TYPE_ATTRIBUTES (t);
+  unsigned align = TYPE_ALIGN (t);
+  bool user_align = TYPE_USER_ALIGN (t);
+
   for (variants = TYPE_NEXT_VARIANT (t);
        variants;
        variants = TYPE_NEXT_VARIANT (variants))
     {
       /* These are the two fields that check_qualified_type looks at and
 	 are affected by attributes.  */
-      TYPE_ATTRIBUTES (variants) = TYPE_ATTRIBUTES (t);
-      TYPE_ALIGN (variants) = TYPE_ALIGN (t);
+      TYPE_ATTRIBUTES (variants) = attrs;
+      unsigned valign = align;
+      if (TYPE_USER_ALIGN (variants))
+	valign = MAX (valign, TYPE_ALIGN (variants));
+      else
+	TYPE_USER_ALIGN (variants) = user_align;
+      TYPE_ALIGN (variants) = valign;
     }
 }
 
