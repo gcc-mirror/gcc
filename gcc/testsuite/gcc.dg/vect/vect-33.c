2539d153a6a28566f6288245b2a4f1e96c79fff7
wschmidt <wschmidt@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-04-28 15:44:24 +0000

    2015-04-28  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    	* gcc.dg/vect/vect-33.c: Remove spurious line.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@222533 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/vect/vect-33.c
+++ gcc/testsuite/gcc.dg/vect/vect-33.c
@@ -36,8 +36,6 @@ int main (void)
   return main1 ();
 } 
 
-/* vect_hw_misalign && { ! vect64 } */
-
 /* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect"  } } */
 /* { dg-final { scan-tree-dump "Vectorizing an unaligned access" "vect" { target { { { ! powerpc*-*-* } && vect_hw_misalign } && { { ! vect64 } || vect_multiple_sizes } } } } }  */
 /* { dg-final { scan-tree-dump "Alignment of access forced using peeling" "vect" { target { vector_alignment_reachable && { vect64 && {! vect_multiple_sizes} } } } } } */
