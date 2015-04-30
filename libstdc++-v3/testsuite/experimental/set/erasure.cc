c0b12dad9334c280ea30445a94f673b145ef88cc
Edward Smith-Rowland <3dw4rd@verizon.net>
2015-04-30 14:43:19 +0000

    Add fundamentals TR container erasure.
    2015-04-30  Edward Smith-Rowland  <3dw4rd@verizon.net>
    
    	Add fundamentals TR container erasure.
    	* include/Makefile.am: Add new headers.
    	* include/Makefile.in: Add new headers.
    	* include/experimental/array: New.
    	* include/experimental/deque: New.
    	* include/experimental/erase_if.tcc: New.
    	* include/experimental/forward_list: New.
    	* include/experimental/list: New.
    	* include/experimental/map: New.
    	* include/experimental/set: New.
    	* include/experimental/string: New.
    	* include/experimental/unordered_map: New.
    	* include/experimental/unordered_set: New.
    	* include/experimental/vector: New.
    	* testsuite/experimental/deque/erasure.cc: New.
    	* testsuite/experimental/forward_list/erasure.cc: New.
    	* testsuite/experimental/list/erasure.cc: New.
    	* testsuite/experimental/map/erasure.cc: New.
    	* testsuite/experimental/set/erasure.cc: New.
    	* testsuite/experimental/string/erasure.cc: New.
    	* testsuite/experimental/unordered_map/erasure.cc: New.
    	* testsuite/experimental/unordered_set/erasure.cc: New.
    	* testsuite/experimental/vector/erasure.cc: New.
    
    From-SVN: r222630

---
+++ libstdc++-v3/testsuite/experimental/set/erasure.cc
@@ -0,0 +1,55 @@
+// { dg-options "-std=gnu++14" }
+// { dg-do run }
+
+// Copyright (C) 2015 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a moved_to of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <experimental/set>
+#include <testsuite_hooks.h>
+
+auto is_odd = [](const int i) { return i % 2 != 0; };
+
+void
+test01()
+{
+  bool test [[gnu::unused]] = true;
+
+  std::set<int> s{ 10, 11, 12, 14, 15, 17, 18, 19 };
+  std::experimental::erase_if(s, is_odd);
+  std::set<int> t{ 10, 12, 14, 18 };
+  VERIFY( s == t );
+}
+
+void
+test02()
+{
+  bool test [[gnu::unused]] = true;
+
+  std::multiset<int> ms{ 20, 21, 22, 22, 23, 23, 24, 25 };
+  std::experimental::erase_if(ms, is_odd);
+  std::multiset<int> t{ 20, 22, 22, 24 };
+  VERIFY( ms == t );
+}
+
+int
+main()
+{
+  test01();
+  test02();
+
+  return 0;
+}
