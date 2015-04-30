1f94878be8c21c18d4b221d384af79cbeafb8b99
emsr <emsr@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-04-30 14:43:19 +0000

    2015-04-30  Edward Smith-Rowland  <3dw4rd@verizon.net>
    	Add fundamentals TR container erasure.
    	* include/Makefile.am: Add new headers.
    	* include/Makefile.in: Add new headers.
    	* include/experimental/array: New.
    	* include/experimental/deque: New.
    	* include/experimental/erase_if.tcc: New.
    	* include/experimental/forward_list: New.
    	* include/experimental/list: New.
    	* include/experimental/map: New.
    	* include/experimental/set: New.
    	* include/experimental/string: New.
    	* include/experimental/unordered_map: New.
    	* include/experimental/unordered_set: New.
    	* include/experimental/vector: New.
    	* testsuite/experimental/deque/erasure.cc: New.
    	* testsuite/experimental/forward_list/erasure.cc: New.
    	* testsuite/experimental/list/erasure.cc: New.
    	* testsuite/experimental/map/erasure.cc: New.
    	* testsuite/experimental/set/erasure.cc: New.
    	* testsuite/experimental/string/erasure.cc: New.
    	* testsuite/experimental/unordered_map/erasure.cc: New.
    	* testsuite/experimental/unordered_set/erasure.cc: New.
    	* testsuite/experimental/vector/erasure.cc: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@222630 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/experimental/vector/erasure.cc
@@ -0,0 +1,57 @@
+// { dg-options "-std=gnu++14" }
+// { dg-do run }
+
+// Copyright (C) 2015 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a moved_to of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <experimental/vector>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  bool test [[gnu::unused]] = true;
+
+  auto is_odd = [](const int i) { return i % 2 != 0; };
+
+  std::vector<int> v{ 10, 11, 12, 14, 15, 17, 18, 19 };
+  std::experimental::erase_if(v, is_odd);
+  std::vector<int> t{ 10, 12, 14, 18 };
+  VERIFY( v == t );
+}
+
+void
+test02()
+{
+  bool test [[gnu::unused]] = true;
+
+  std::vector<int> v{ 0, 11, 0, 0, 22, 33, 0, 0, 44, 0 };
+  std::experimental::erase(v, 0);
+  std::vector<int> t{ 11, 22, 33, 44 };
+  VERIFY( v == t );
+  std::experimental::erase(v, 55);
+  VERIFY( v == t );
+}
+
+int
+main()
+{
+  test01();
+  test02();
+
+  return 0;
+}
