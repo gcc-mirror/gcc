c0cf1b24f0a4716f5329106edb2a0ae9e4beea17
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-05-01 18:25:12 +0000

    * match.pd: New simplification patterns. 	(x + (x & 1))  -> ((x + 1) & ~1) 	(x & ~(x & y)) -> ((x & ~y)) 	(x | ~(x | y)) -> ((x | ~y))
    	* gcc.dg/20150120-1.c: New test.
    	* gcc.dg/20150120-2.c: New test.
    	* gcc.dg/20150120-3.c: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@222697 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/20150120-2.c
@@ -0,0 +1,32 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-original" } */
+
+/* x & ~(x & y) -> x & ~y */
+int fn1 (int x, int y)
+{
+	return x & ~(x & y);
+}
+int fn2 (int x, int y)
+{
+	return ~(x & y) & x;
+}
+int fn3 (int x, int y)
+{
+	return x & ~(y & x);
+}
+int fn4 (int x, int y)
+{
+	return ~(y & x) & x;
+}
+int fn5 (int z)
+{
+	return z & ~(z & 3);
+}
+int fn6 (int z)
+{
+	return ~(z & 3) & z;
+}
+
+
+/* { dg-final { scan-tree-dump-times "~y & x" 4 "original" } } */
+/* { dg-final { scan-tree-dump-times "z & -4" 2 "original" } } */
