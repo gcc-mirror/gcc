2600b9b2bb71baa5de3539d778a50040d631c16a
tkoenig <tkoenig@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-05-03 18:09:57 +0000

    2015-05-03  Thomas Koenig  <tkoenig@gcc.gnu.org>
    	PR fortran/37131
    	* gfortran.dg/bound_9.f90:  Add pointer assignment.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@222751 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/bound_9.f90
+++ gcc/testsuite/gfortran.dg/bound_9.f90
@@ -54,6 +54,7 @@ program main
   call foo(empty, a(2:0), n, m)
   if (n .ne. 1 .or. m .ne. 1) call abort
   allocate (x(0))
+  y => a(3:2)
   call bar (x, y, n, m)
   if (n .ne. 1 .or. m .ne. 1) call abort
 
