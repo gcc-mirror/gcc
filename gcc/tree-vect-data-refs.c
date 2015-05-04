78a8b26c40f8be8eaca5aaa2680e213e0bd7d05a
Richard Biener <rguenther@suse.de>
2015-05-04 14:24:49 +0000

    re PR middle-end/65965 (Straight-line memcpy/memset not vectorized when equivalent loop is)
    2015-05-04  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/65965
    	* tree-vect-data-refs.c (vect_analyze_data_ref_accesses): Split
    	store groups at gaps.
    
    	* gcc.dg/vect/bb-slp-33.c: New testcase.
    
    From-SVN: r222765

--- gcc/tree-vect-data-refs.c
+++ gcc/tree-vect-data-refs.c
@@ -2602,6 +2602,15 @@ vect_analyze_data_ref_accesses (loop_vec_info loop_vinfo, bb_vec_info bb_vinfo)
 	  if ((init_b - init_a) % type_size_a != 0)
 	    break;
 
+	  /* If we have a store, the accesses are adjacent.  This splits
+	     groups into chunks we support (we don't support vectorization
+	     of stores with gaps).  */
+	  if (!DR_IS_READ (dra)
+	      && (init_b - (HOST_WIDE_INT) TREE_INT_CST_LOW
+					     (DR_INIT (datarefs_copy[i-1]))
+		  != type_size_a))
+	    break;
+
 	  /* The step (if not zero) is greater than the difference between
 	     data-refs' inits.  This splits groups into suitable sizes.  */
 	  HOST_WIDE_INT step = tree_to_shwi (DR_STEP (dra));
