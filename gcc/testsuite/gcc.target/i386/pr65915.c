3cd66da2f3ff0667e5f13447c7a1fe41a6bff23e
tocarip <tocarip@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-05-05 15:43:13 +0000

    PR target/65915 * config/i386/i386.md (vector convert to float spltiter): Check for xmm16+, when splitting scalar float conversion. * config/i386/sse.md (sse2_cvtsi2sd): Support EVEX version.
    testsuite/ChangeLog:
    
    PR target/65915
    * gcc.target/i386/pr65915.c: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@222811 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr65915.c
@@ -0,0 +1,6 @@
+/* { dg-do run } */
+/* { dg-options "-O2 -mavx512f -fpic -mcmodel=medium" } */
+/* { dg-require-effective-target avx512f } */
+/* { dg-require-effective-target lp64 } */
+
+#include "avx512f-vrndscalepd-2.c"
