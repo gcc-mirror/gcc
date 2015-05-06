4e3bf2ef494264f881619fede409d4b030494778
Christian Bruel <christian.bruel@st.com>
2015-05-06 13:19:56 +0200

    re PR target/66015 (align directives not propagated after __attribute__ ((__optimize__ ("O2"))))
    2015-05-06  Christian Bruel  <christian.bruel@st.com>
    
    	PR target/66015
    	* config/aarch64/aarch64.c (aarch64_override_options): Move align_loops,
    	align_jumps, align_functions into aarch64_override_options_after_change.
    
    From-SVN: r222848

--- gcc/config/aarch64/aarch64.c
+++ gcc/config/aarch64/aarch64.c
@@ -6872,18 +6872,6 @@ aarch64_override_options (void)
 #endif
     }
 
-  /* If not opzimizing for size, set the default
-     alignment to what the target wants */
-  if (!optimize_size)
-    {
-      if (align_loops <= 0)
-	align_loops = aarch64_tune_params->loop_align;
-      if (align_jumps <= 0)
-	align_jumps = aarch64_tune_params->jump_align;
-      if (align_functions <= 0)
-	align_functions = aarch64_tune_params->function_align;
-    }
-
   aarch64_override_options_after_change ();
 }
 
@@ -6896,6 +6884,18 @@ aarch64_override_options_after_change (void)
     flag_omit_leaf_frame_pointer = false;
   else if (flag_omit_leaf_frame_pointer)
     flag_omit_frame_pointer = true;
+
+  /* If not optimizing for size, set the default
+     alignment to what the target wants */
+  if (!optimize_size)
+    {
+      if (align_loops <= 0)
+	align_loops = aarch64_tune_params->loop_align;
+      if (align_jumps <= 0)
+	align_jumps = aarch64_tune_params->jump_align;
+      if (align_functions <= 0)
+	align_functions = aarch64_tune_params->function_align;
+    }
 }
 
 static struct machine_function *
