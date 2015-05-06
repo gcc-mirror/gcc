ed723026f6db736b003b7d45a9ab77d8e96bdc04
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-05-06 02:07:40 +0000

    * decl.c (start_decl): Don't push the plain VAR_DECL for a 	variable template.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@222837 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/decl.c
+++ gcc/cp/decl.c
@@ -4825,8 +4825,11 @@ start_decl (const cp_declarator *declarator,
 
   was_public = TREE_PUBLIC (decl);
 
-  /* Enter this declaration into the symbol table.  */
-  decl = maybe_push_decl (decl);
+  /* Enter this declaration into the symbol table.  Don't push the plain
+     VAR_DECL for a variable template.  */
+  if (!template_parm_scope_p ()
+      || TREE_CODE (decl) != VAR_DECL)
+    decl = maybe_push_decl (decl);
 
   if (processing_template_decl)
     decl = push_template_decl (decl);
