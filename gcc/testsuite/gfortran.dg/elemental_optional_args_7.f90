4a8108f0ab7310371123340e0181ff4afd84789e
Mikael Morin <mikael@gcc.gnu.org>
2015-05-08 14:53:20 +0000

    trans-array.c (gfc_walk_elemental_function_args): Don't skip the advance to the next dummy argument when...
    gcc/fortran/
    	* trans-array.c (gfc_walk_elemental_function_args):
    	Don't skip the advance to the next dummy argument when skipping
    	absent optional args.
    gcc/testsuite/
    	* gfortran.dg/elemental_optional_args_7.f90: New.
    
    From-SVN: r222913

---
+++ gcc/testsuite/gfortran.dg/elemental_optional_args_7.f90
@@ -0,0 +1,37 @@
+! { dg-do run }
+!
+! The handling of scalar optional arguments passed to elemental procedure
+! did not keep actual arguments and dummy arguments synchronized while
+! walking them in gfc_walk_elemental_function_args, leading to a
+! null pointer dereference in the generated code.
+!
+  implicit none
+
+  integer, parameter :: n = 3
+
+  call do_test
+
+contains
+
+  elemental function five(nonopt1, opt1, nonopt2, opt2)
+    integer, intent(in), optional :: opt1, opt2
+    integer, intent(in) :: nonopt1, nonopt2
+    integer :: five
+
+    if (.not. present(opt1) .and. .not. present(opt2)) then
+      five = 5
+    else
+      five = -7
+    end if
+  end function five
+
+  subroutine do_test(opt)
+    integer, optional :: opt
+    integer :: i = -1, a(n) = (/ (i, i=1,n) /)
+    integer :: b(n)
+
+    b = five(a, nonopt2=i, opt2=opt)
+    if (any(b /= 5)) call abort
+  end subroutine do_test
+
+end
