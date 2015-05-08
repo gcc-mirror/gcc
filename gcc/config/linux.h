07ad804bfdbe0fa3406c5f2ff51d7c4c29a783c8
Szabolcs Nagy <szabolcs.nagy@arm.com>
2015-05-08 08:30:40 +0000

    [PATCH 0/13] Add musl support to GCC
    2015-05-08  Szabolcs Nagy  <szabolcs.nagy@arm.com>
    
    	* config/glibc-stdint.h (OPTION_MUSL): Define.
    	(INT_FAST16_TYPE, INT_FAST32_TYPE, UINT_FAST16_TYPE, UINT_FAST32_TYPE):
    	Change the definition based on OPTION_MUSL for 64 bit targets.
    	* config/linux.h (OPTION_MUSL): Redefine.
    	* config/alpha/linux.h (OPTION_MUSL): Redefine.
    	* config/rs6000/linux.h (OPTION_MUSL): Redefine.
    	* config/rs6000/linux64.h (OPTION_MUSL): Redefine.
    
    From-SVN: r222905

--- gcc/config/linux.h
+++ gcc/config/linux.h
@@ -32,11 +32,13 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 #define OPTION_GLIBC  (DEFAULT_LIBC == LIBC_GLIBC)
 #define OPTION_UCLIBC (DEFAULT_LIBC == LIBC_UCLIBC)
 #define OPTION_BIONIC (DEFAULT_LIBC == LIBC_BIONIC)
+#undef OPTION_MUSL
 #define OPTION_MUSL   (DEFAULT_LIBC == LIBC_MUSL)
 #else
 #define OPTION_GLIBC  (linux_libc == LIBC_GLIBC)
 #define OPTION_UCLIBC (linux_libc == LIBC_UCLIBC)
 #define OPTION_BIONIC (linux_libc == LIBC_BIONIC)
+#undef OPTION_MUSL
 #define OPTION_MUSL   (linux_libc == LIBC_MUSL)
 #endif
 
