263444332ae2c81e12aedce26bfef5773c5b7b94
Steve Ellcey <sellcey@mips.com>
2015-05-11 18:01:13 +0000

    * gcc.target/mips/branch-1.c: Pass argument to bar().
    From-SVN: r223010

--- gcc/testsuite/gcc.target/mips/branch-1.c
+++ gcc/testsuite/gcc.target/mips/branch-1.c
@@ -4,11 +4,11 @@
    on zero.  */
 /* { dg-options "forbid_cpu=octeon.*" } */
 
-void bar (void);
-NOMIPS16 void f1 (int x) { if (x & 4) bar (); }
-NOMIPS16 void f2 (int x) { if ((x >> 2) & 1) bar (); }
-NOMIPS16 void f3 (unsigned int x) { if (x & 0x10) bar (); }
-NOMIPS16 void f4 (unsigned int x) { if ((x >> 4) & 1) bar (); }
+void bar (int);
+NOMIPS16 void f1 (int x) { if (x & 4) bar (1); }
+NOMIPS16 void f2 (int x) { if ((x >> 2) & 1) bar (2); }
+NOMIPS16 void f3 (unsigned int x) { if (x & 0x10) bar (3); }
+NOMIPS16 void f4 (unsigned int x) { if ((x >> 4) & 1) bar (4); }
 /* { dg-final { scan-assembler "\tandi\t.*\tandi\t.*\tandi\t.*\tandi\t" } } */
 /* { dg-final { scan-assembler-not "\tsrl\t" } } */
 /* { dg-final { scan-assembler-not "\tsra\t" } } */
