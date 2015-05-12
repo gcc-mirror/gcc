07b456c3c24c639379724577e2b1d3c0b024f5f3
rearnsha <rearnsha@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-05-12 16:39:19 +0000

    * arm-protos.h (arm_sched_autopref): Delete. 	(tune_params): Re-organize, use enums for flag values. 	(FUSE_OPS): New macro. 	* arm.c (ARM_PREFETCH_NOT_BENEFICIAL): Update. 	(ARM_PREFETCH_BENEFICIAL): Likewise. 	(ARM_FUSE_NOTHING, ARM_FUSE_MOVW_MOVT): Delete. 	(arm_slowmul_tune, arm_fastmul_tune, arm_strongarm_tune) 	(arm_xscale_tune, arm_9e_tune, arm_marvell_pj4_tune) 	(arm_v6t2_tune, arm_cortex_tune, arm_cortex_a8_tune) 	(arm_cortex_a7_tune, arm_cortex_a15_tune, arm_cortex_a53_tune) 	(arm_cortex_a57_tune,  arm_xgene1_tune, arm_cortex_a5_tune) 	(arm_cortex_a9_tune, arm_cortex_a12_tune, arm_v7m_tune) 	(arm_cortex_m7_tune, arm_v6m_tune, arm_fa726te_tune): Use new 	format. 	(arm_option_override, thumb2_reorg, arm_print_tune_info) 	(aarch_macro_fusion_pair_p): Update uses of current_tune. 	* arm.h (LOGCIAL_OP_NON_SHORT_CIRCUIT): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@223090 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/arm/arm.h
+++ gcc/config/arm/arm.h
@@ -2057,10 +2057,11 @@ enum arm_auto_incmodes
   (current_tune->branch_cost (speed_p, predictable_p))
 
 /* False if short circuit operation is preferred.  */
-#define LOGICAL_OP_NON_SHORT_CIRCUIT				\
-  ((optimize_size)						\
-   ? (TARGET_THUMB ? false : true)				\
-   : (current_tune->logical_op_non_short_circuit[TARGET_ARM]))
+#define LOGICAL_OP_NON_SHORT_CIRCUIT					\
+  ((optimize_size)							\
+   ? (TARGET_THUMB ? false : true)					\
+   : TARGET_THUMB ? current_tune->logical_op_non_short_circuit_thumb	\
+   : current_tune->logical_op_non_short_circuit_arm)
 
 
 /* Position Independent Code.  */
