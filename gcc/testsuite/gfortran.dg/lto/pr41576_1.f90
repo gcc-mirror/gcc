71c87d1c0f5048166870801c418ddfc52e396a9f
Steven G. Kargl <kargl@gcc.gnu.org>
2015-05-18 18:59:48 +0000

    pr41521_0.f90: Move INTERFACE statement in program unit.
    2015-05-18  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	* gfortran.dg/lto/pr41521_0.f90: Move INTERFACE statement in program
    	unit.
    	* gfortran.dg/lto/pr41576_1.f90: Ditto.
    
    From-SVN: r223309

--- gcc/testsuite/gfortran.dg/lto/pr41576_1.f90
+++ gcc/testsuite/gfortran.dg/lto/pr41576_1.f90
@@ -1,12 +1,11 @@
 program test
   common /bar/ c, d
   integer(4) :: c, d
-  call foo
-  if (c/=1 .or. d/=2) call abort
-end program test
-
 interface
   subroutine foo()
   end subroutine
 end interface
+  call foo
+  if (c/=1 .or. d/=2) call abort
+end program test
 
