fd2ef1171407340dbb09935ab535debf40f56376
François Dumont <fdumont@gcc.gnu.org>
2015-05-20 19:02:22 +0000

    2015-05-20  François Dumont  <fdumont@gcc.gnu.org>
    	* include/bits/cpp_type_traits.h
    	(std::move_iterator): Delete declaration.
    	(std::__is_move_iterator<move_iterator>): Move partial specialization...
    	* include/bits/stl_iterator.h: ... here.
    	(std::__miter_base): Overloads for std::reverse_iterator and
    	std::move_iterator.
    	* include/bits/stl_algobase.h (std::__miter_base): Provide default
    	implementation.
    
    From-SVN: r223453

--- libstdc++-v3/include/bits/stl_algobase.h
+++ libstdc++-v3/include/bits/stl_algobase.h
@@ -277,17 +277,11 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
     __niter_base(_Iterator __it)
     { return __it; }
 
-  // If _Iterator is a move_iterator return its base otherwise return it
-  // untouched.  See copy, fill, ...
+  // Likewise for move_iterator.
   template<typename _Iterator>
-    struct _Miter_base
-    : _Iter_base<_Iterator, __is_move_iterator<_Iterator>::__value>
-    { };
-
-  template<typename _Iterator>
-    inline typename _Miter_base<_Iterator>::iterator_type
+    inline _Iterator
     __miter_base(_Iterator __it)
-    { return std::_Miter_base<_Iterator>::_S_base(__it); }
+    { return __it; }
 
   // All of these auxiliary structs serve two purposes.  (1) Replace
   // calls to copy with memmove whenever possible.  (Memmove, not memcpy,
