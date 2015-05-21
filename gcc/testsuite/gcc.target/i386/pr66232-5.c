f70d27e03b2fd9557fcd184f52ee9c82128cc647
H.J. Lu <hongjiu.lu@intel.com>
2015-05-21 21:58:57 +0000

    Allow indirect branch via GOT slot for x32
    X32 doesn't support indirect branch via 32-bit memory slot since
    indirect branch will load 64-bit address from 64-bit memory slot.
    Since x32 GOT slot is 64-bit, we should allow indirect branch via GOT
    slot for x32.
    
    gcc/
    
    	PR target/66232
    	* config/i386/constraints.md (Bg): New constraint for GOT memory
    	operand.
    	* config/i386/i386.md (*call_got_x32): New pattern.
    	(*call_value_got_x32): Likewise.
    	* config/i386/predicates.md (GOT_memory_operand): New predicate.
    
    gcc/testsuite/
    
    	PR target/66232
    	* gcc.target/i386/pr66232-1.c: New test.
    	* gcc.target/i386/pr66232-2.c: Likewise.
    	* gcc.target/i386/pr66232-3.c: Likewise.
    	* gcc.target/i386/pr66232-4.c: Likewise.
    	* gcc.target/i386/pr66232-5.c: Likewise.
    
    From-SVN: r223505

---
+++ gcc/testsuite/gcc.target/i386/pr66232-5.c
@@ -0,0 +1,16 @@
+/* { dg-do compile { target { ! { ia32 } } } } */
+/* { dg-require-effective-target maybe_x32 } */
+/* { dg-options "-O2 -fpic -mx32" } */
+
+extern void (*bar) (void);
+void
+foo (int n)
+{
+  int i;
+  for (i = 0; i < n; i++)
+    {
+      if (!bar)
+	continue;
+      (*bar) ();
+    }
+}
