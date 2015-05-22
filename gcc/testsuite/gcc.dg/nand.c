af563d4bb754b06f828d29aa74b5b663353cec97
Marc Glisse <marc.glisse@inria.fr>
2015-05-22 22:37:04 +0200

    match.pd ((x | y) & ~x -> y & ~x, (x & y) | ~x -> y | ~x): New simplifications.
    2015-05-22  Marc Glisse  <marc.glisse@inria.fr>
    
    gcc/
    	* match.pd ((x | y) & ~x -> y & ~x, (x & y) | ~x -> y | ~x): New
    	simplifications.
    gcc/testsuite/
    	* gcc.dg/nand.c: New testcase.
    
    From-SVN: r223587

---
+++ gcc/testsuite/gcc.dg/nand.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-original" } */
+
+unsigned f(unsigned x, unsigned y){
+  return (x | y) & ~x;
+}
+unsigned g(unsigned x, unsigned y){
+  return ~x & (y | x);
+}
+
+/* { dg-final { scan-tree-dump-times "return ~x & y;" 2 "original" } } */
+/* { dg-final { cleanup-tree-dump "original" } } */
