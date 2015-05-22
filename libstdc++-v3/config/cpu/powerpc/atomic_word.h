fd28834bc4b1c163e39ddc3521958d4dca44bc41
David Edelsohn <dje.gcc@gmail.com>
2015-05-22 12:43:02 +0000

    re PR target/66224 (PowerPC _GLIBCXX_READ_MEM_BARRIER too weak)
            PR target/66224
            * config/cpu/powerpc/atomic_word.h (_GLIBCXX_READ_MEM_BARRIER):
            Don't use isync. Use lwsync if available.
            * configure.host (atomic_word_dir) [aix[56789]*]: Delete to use
            powerpc cpu definition.
    
    From-SVN: r223554

--- libstdc++-v3/config/cpu/powerpc/atomic_word.h
+++ libstdc++-v3/config/cpu/powerpc/atomic_word.h
@@ -27,10 +27,11 @@
 
 typedef int _Atomic_word;
 
-#define _GLIBCXX_READ_MEM_BARRIER __asm __volatile ("isync":::"memory")
 #ifdef __NO_LWSYNC__
+#define _GLIBCXX_READ_MEM_BARRIER __asm __volatile ("sync":::"memory")
 #define _GLIBCXX_WRITE_MEM_BARRIER __asm __volatile ("sync":::"memory")
 #else
+#define _GLIBCXX_READ_MEM_BARRIER __asm __volatile ("lwsync":::"memory")
 #define _GLIBCXX_WRITE_MEM_BARRIER __asm __volatile ("lwsync":::"memory")
 #endif
 
