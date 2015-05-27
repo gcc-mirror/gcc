6fa6a8717af8f7704c04d4cbf284e46112ad8f81
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-05-27 21:13:25 +0000

    PR target/39726         * gcc.dg/target/m68k/pr39726-1.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@223781 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/m68k/pr39726.c
@@ -0,0 +1,65 @@
+/* { dg-do assemble } /*
+/* { dg-options "-O2 -fomit-frame-pointer" } */
+/* { dg-final { object-size text <= 228 } } */
+
+unsigned char v;
+
+int a0bs (unsigned char u, unsigned char w)
+{
+  if ((u - w) & 0x80)
+    v = 1;
+}
+
+int a1bs (unsigned char u, unsigned char w)
+{
+  if ((u + w) & 0x80)
+    v = 1;
+}
+
+int a0b (unsigned char u, unsigned char w)
+{
+  if ((u - w) & 0x22)
+    v = 1;
+}
+
+int a1b (unsigned char u, unsigned char w)
+{
+  if ((u + w) & 0x22)
+    v = 1;
+}
+
+int a0ws (unsigned short u, unsigned short w)
+{
+  if ((u - w) & 0x8000)
+    v = 1;
+}
+
+int a1ws (unsigned short u, unsigned short w)
+{
+  if ((u + w) & 0x8000)
+    v = 1;
+}
+
+int a0wbs (unsigned short u, unsigned short w)
+{
+  if ((u - w) & 0x80)
+    v = 1;
+}
+
+int a1wbs (unsigned short u, unsigned short w)
+{
+  if ((u + w) & 0x80)
+    v = 1;
+}
+
+int a0w (unsigned short u, unsigned short w)
+{
+  if ((u - w) & 0x8421)
+    v = 1;
+}
+
+int a1w (unsigned short u, unsigned short w)
+{
+  if ((u + w) & 0x8421)
+    v = 1;
+}
