8d813920caec71f9ecf0d78e52cd0a02d12e4612
Mike Frysinger <vapier@gentoo.org>
2015-05-28 14:05:44 +0000

    hppa-linux: add missing cpp specs
    Define CPP_SPEC for parisc linux targets so that -posix & -pthread work
    like on all other linux targets.
    
    Discovered via Gentoo bug https://bugs.gentoo.org/543114
    
    From-SVN: r223824

--- gcc/config/pa/pa-linux.h
+++ gcc/config/pa/pa-linux.h
@@ -28,7 +28,7 @@ along with GCC; see the file COPYING3.  If not see
   while (0)
 
 #undef CPP_SPEC
-#define CPP_SPEC "%{posix:-D_POSIX_SOURCE}"
+#define CPP_SPEC "%{posix:-D_POSIX_SOURCE} %{pthread:-D_REENTRANT}"
 
 #undef ASM_SPEC
 #define ASM_SPEC \
