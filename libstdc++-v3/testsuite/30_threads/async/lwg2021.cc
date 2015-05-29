4aec4c9e62a7bc1e067a6157854c74edba8bb439
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-05-29 13:44:52 +0000

    * include/std/future (__async_result_of): New alias template. 	(async): Use __async_result_of to pass decayed types to result_of. 	* testsuite/30_threads/async/lwg2021.cc: New. 	* doc/xml/manual/intro.xml: Document LWG 2021 status.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@223866 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/30_threads/async/lwg2021.cc
@@ -0,0 +1,42 @@
+// Copyright (C) 2015 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile { target *-*-freebsd* *-*-dragonfly* *-*-netbsd* *-*-linux* *-*-gnu* *-*-solaris* *-*-cygwin *-*-darwin* powerpc-ibm-aix* } }
+// { dg-options " -std=gnu++11 -pthread" { target *-*-freebsd* *-*-dragonfly* *-*-netbsd* *-*-linux* *-*-gnu* powerpc-ibm-aix* } }
+// { dg-options " -std=gnu++11 -pthreads" { target *-*-solaris* } }
+// { dg-options " -std=gnu++11 " { target *-*-cygwin *-*-darwin* } }
+// { dg-require-cstdint "" }
+// { dg-require-gthreads "" }
+// { dg-require-atomic-builtins "" }
+
+// LWG 2021. Further incorrect usages of result_of
+// Arguments to result_of should use decay.
+
+#include <future>
+
+struct A
+{
+  int operator()(int&&)&& { return 0; }
+  void operator()(int&)& { }
+};
+
+int main()
+{
+  A a;
+  int i = 0;
+  std::future<int> f = std::async(a, i);
+}
