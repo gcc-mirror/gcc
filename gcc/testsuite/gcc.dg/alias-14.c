fdc195e1e358d50f9bfcf9011ee285b612b2d4b5
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-05-30 00:32:04 +0000

    * alias.c (alias_set_entry_d): Add is_pointer and has_pointer. 	(alias_stats): Add num_universal. 	(alias_set_subset_of): Special case pointers; be ready for NULL 	children. 	(alias_sets_conflict_p): Special case pointers; be ready for NULL 	children. 	(init_alias_set_entry): Break out from ... 	(record_alias_subset): ... here; propagate new fields; 	allocate children only when really needed. 	(get_alias_set): Do less generous pointer globbing. 	(dump_alias_stats_in_alias_c): Update statistics. 	* gcc.dg/alias-8.c: Do not xfail. 	* gcc.dg/pr62167.c: Prevent FRE. 	* gcc.dg/alias-14.c: New testcase.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@223883 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/alias-14.c
@@ -0,0 +1,70 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+#include <stddef.h>
+void *a;
+int *b;
+struct c {void * a;} c;
+struct d {short * a;} d;
+
+int *ip= (int *)(size_t)2;
+int **ipp = &ip;
+
+int
+main()
+{
+  float **ptr;
+  void **uptr;
+  int* const* cipp = (int* const*)ipp;
+  /* as an extension we consider void * universal. Writes to it should alias.  */
+  asm ("":"=r"(ptr):"0"(&a));
+  a=NULL;
+  *ptr=(float*)(size_t)1;
+  if (!a)
+    __builtin_abort ();
+  a=NULL;
+  if (*ptr)
+    __builtin_abort ();
+
+  asm ("":"=r"(uptr):"0"(&b));
+  b=NULL;
+  *uptr=(void*)(size_t)1;
+  if (!b)
+    __builtin_abort ();
+  b=NULL;
+  if (*uptr)
+    __builtin_abort ();
+
+  /* Check that we disambiguate int * and char *.  */
+  asm ("":"=r"(ptr):"0"(&b));
+  b=NULL;
+  *ptr=(float*)(size_t)1;
+  if (b)
+    __builtin_abort ();
+
+  /* Again we should make void * in the structure conflict with any pointer.  */
+  asm ("":"=r"(ptr):"0"(&c));
+  c.a=NULL;
+  *ptr=(float*)(size_t)1;
+  if (!c.a)
+    __builtin_abort ();
+  c.a=NULL;
+  if (*ptr)
+    __builtin_abort ();
+
+  asm ("":"=r"(uptr):"0"(&d));
+  d.a=NULL;
+  *uptr=(void*)(size_t)1;
+  if (!d.a)
+    __builtin_abort ();
+  d.a=NULL;
+  if (*uptr)
+    __builtin_abort ();
+
+  if ((void *)*cipp != (void*)(size_t)2)
+    __builtin_abort ();
+  *ipp = NULL;
+  if (*cipp)
+    __builtin_abort ();
+
+  return 0;
+}
