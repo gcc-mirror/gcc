610ae31fb51689f4b85d076d8b05b2342ec9e91a
Dominik Vogt <vogt@linux.vnet.ibm.com>
2015-06-01 11:25:56 +0000

    PR 66215: S390: Fix placement of post-label NOPs with -mhotpatch
    gcc/ChangeLog:
    --------------
    2015-06-01  Dominik Vogt  <vogt@linux.vnet.ibm.com>
    
    	    Backport from mainline
    	    2015-05-29  Dominik Vogt  <vogt@linux.vnet.ibm.com>
    
    	    PR target/66215
    	    * config/s390/s390.c (s390_reorg): Fix placement of post-label NOPs
    	    with -mhotpatch=.
    
    gcc/testsuite/ChangeLog:
    ------------------------
    2015-06-01  Dominik Vogt  <vogt@linux.vnet.ibm.com>
    
    	    Backport from mainline
    	    2015-05-29  Dominik Vogt  <vogt@linux.vnet.ibm.com>
    
    	    PR target/66215
    	    * gcc.target/s390/hotpatch-1.c: Remove optimization options from
    	    dg-options.
    	    * gcc.target/s390/hotpatch-10.c: Likewise.
    	    * gcc.target/s390/hotpatch-11.c: Likewise.
    	    * gcc.target/s390/hotpatch-12.c: Likewise.
    	    * gcc.target/s390/hotpatch-17.c: Likewise.
    	    * gcc.target/s390/hotpatch-18.c: Likewise.
    	    * gcc.target/s390/hotpatch-20.c: Likewise.
    	    * gcc.target/s390/hotpatch-21.c: Likewise.
    	    * gcc.target/s390/hotpatch-22.c: Likewise.
    	    * gcc.target/s390/hotpatch-23.c: Likewise.
    	    * gcc.target/s390/hotpatch-24.c: Likewise.
    	    * gcc.target/s390/hotpatch-2.c: Likewise.  Adjust scan-assembler
    	    to check for the exact nops too.
    	    * gcc.target/s390/hotpatch-3.c: Likewise.
    	    * gcc.target/s390/hotpatch-4.c: Likewise.
    	    * gcc.target/s390/hotpatch-5.c: Likewise.
    	    * gcc.target/s390/hotpatch-6.c: Likewise.
    	    * gcc.target/s390/hotpatch-7.c: Likewise.
    	    * gcc.target/s390/hotpatch-8.c: Likewise.
    	    * gcc.target/s390/hotpatch-9.c: Likewise.
    	    * gcc.target/s390/hotpatch-14.c: Likewise.
    	    * gcc.target/s390/hotpatch-15.c: Likewise.
    	    * gcc.target/s390/hotpatch-16.c: Likewise.
    	    * gcc.target/s390/hotpatch-19.c: Likewise.
    	    * gcc.target/s390/hotpatch-25.c: Likewise.  Remove
    	    scan-assembler-times counting number of .align directives.
    	    * gcc.target/s390/hotpatch-13.c: Remove optimization options from
    	    dg-options.  Remove scan-assembler-times counting number of .align
    	    directives.
    	    * gcc.target/s390/hotpatch-26.c: New file.
    	    * gcc.target/s390/hotpatch-27.c: New file.
    	    * gcc.target/s390/hotpatch-28.c: New file.
    	    * gcc.target/s390/s390.exp: Run hotpatch-*.c tests as torture tests
    	    using -Os -O0 -O1 -O2 -O3 options.
    
    From-SVN: r223930

--- gcc/testsuite/gcc.target/s390/hotpatch-4.c
+++ gcc/testsuite/gcc.target/s390/hotpatch-4.c
@@ -1,7 +1,7 @@
 /* Functional tests for the function hotpatching feature.  */
 
 /* { dg-do compile } */
-/* { dg-options "-O3 -mzarch -mhotpatch=0,3" } */
+/* { dg-options "-mzarch -mhotpatch=0,3" } */
 
 #include <stdio.h>
 
@@ -12,7 +12,7 @@ void hp1(void)
 
 /* Check number of occurences of certain instructions.  */
 /* { dg-final { scan-assembler-not "pre-label NOPs" } } */
-/* { dg-final { scan-assembler "post-label.*(3 halfwords)" } } */
+/* { dg-final { scan-assembler "^\[^.\].*:\n.*post-label.*(3 halfwords).*\n\(\(.L.*:\n\)\|\(\[\[:space:\]\]*.cfi_.*\n\)\)*\[\[:space:\]\]*brcl\t0, 0" } } */
 /* { dg-final { scan-assembler-not "nopr\t%r7" } } */
 /* { dg-final { scan-assembler-not "nop\t0" } } */
 /* { dg-final { scan-assembler-times "brcl\t0, 0" 1 } } */
