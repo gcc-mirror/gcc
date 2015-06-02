b4cb2a41df2b4871e6c0596b2b6683788cda6e04
Steven G. Kargl <kargl@gcc.gnu.org>
2015-06-02 22:08:14 +0000

    simplify.c (gfc_simplify_reshape): Convert assert into returning NULL, which triggers an error condition.
    2015-06-02  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	* simplify.c (gfc_simplify_reshape): Convert assert into returning
    	NULL, which triggers an error condition.
    
    2015-06-02  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	* gfortran.dg/reshape_7.f90: New test.
    
    From-SVN: r224043

---
+++ gcc/testsuite/gfortran.dg/reshape_7.f90
@@ -0,0 +1,17 @@
+! { dg-do compile }
+! PR fortran/66380
+!
+subroutine p0
+   integer, parameter :: sh(2) = [2, 3]
+   integer, parameter :: &
+   & a(2,2) = reshape([1, 2, 3, 4], sh)   ! { dg-error "Different shape" }
+   if (a(1,1) /= 0) call abort
+end subroutine p0
+
+
+subroutine p1
+   integer, parameter :: sh(2) = [2, 1]
+   integer, parameter :: &
+   &  a(2,2) = reshape([1, 2, 3, 4], sh)  ! { dg-error "Different shape" }
+   if (a(1,1) /= 0) call abort
+end subroutine p1
