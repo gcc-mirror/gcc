b15dfdc80ad4a160231e5323979ccc59484ff5f0
Renlin Li <renlin.li@arm.com>
2015-06-02 11:14:16 +0000

    [PATCH][AARCH64]Use shl for vec_shr_<mode> rtx pattern.
    gcc/
    
    2015-06-02 Renlin Li <renlin.li@arm.com>
    
    	Backport from mainline
    	2015-04-30  Renlin Li  <renlin.li@arm.com>
    
    	* config/aarch64/aarch64-simd.md (vec_shr): Defined as an unspec.
    	* config/aarch64/iterators.md (unspec): Add UNSPEC_VEC_SHR.
    
    gcc/testsuite
    
    2015-06-02 Renlin Li <renlin.li@arm.com>
    
    	Backport from mainline
    	2015-04-30  Renlin Li  <renlin.li@arm.com>
    
    	* gcc.target/aarch64/vect-reduc-or_1.c: New.
    
    From-SVN: r224024

---
+++ gcc/testsuite/gcc.target/aarch64/vect-reduc-or_1.c
@@ -0,0 +1,34 @@
+/* { dg-do run } */
+/* { dg-options "-O2 -ftree-vectorize -fdump-tree-vect-all" } */
+/* Write a reduction loop to be reduced using whole vector right shift.  */
+
+extern void abort (void);
+
+unsigned char in[8] __attribute__((__aligned__(16)));
+
+int
+main (unsigned char argc, char **argv)
+{
+  unsigned char i = 0;
+  unsigned char sum = 1;
+
+  for (i = 0; i < 8; i++)
+    in[i] = (i + i + 1) & 0xfd;
+
+  /* Prevent constant propagation of the entire loop below.  */
+  asm volatile ("" : : : "memory");
+
+  for (i = 0; i < 8; i++)
+    sum |= in[i];
+
+  if (sum != 13)
+    {
+      __builtin_printf("Failed %d\n", sum);
+      abort ();
+    }
+
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump "Reduce using vector shifts" "vect" } } */
+/* { dg-final { cleanup-tree-dump "vect" } } */
