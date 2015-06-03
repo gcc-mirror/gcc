e004aa11b1d261b597aa3e062204af61762ba6a9
Richard Biener <rguenther@suse.de>
2015-06-03 13:10:13 +0000

    tree-vect-data-refs.c (vect_analyze_group_access): Properly compute GROUP_GAP for the first element.
    2015-06-03  Richard Biener  <rguenther@suse.de>
    
    	* tree-vect-data-refs.c (vect_analyze_group_access): Properly
    	compute GROUP_GAP for the first element.
    	* tree-vect-slp.c (vect_build_slp_tree_1): Remove restriction
    	on in-group gaps.
    
    	* gcc.dg/vect/bb-slp-36.c: New testcase.
    
    From-SVN: r224077

---
+++ gcc/testsuite/gcc.dg/vect/bb-slp-36.c
@@ -0,0 +1,35 @@
+/* { dg-require-effective-target vect_int } */
+
+#include "tree-vect.h"
+
+extern void abort (void);
+
+int a[8], b[8];
+
+void __attribute__((noinline,noclone))
+foo(void)
+{
+  a[0] = b[0];
+  a[1] = b[0];
+  a[2] = b[3];
+  a[3] = b[3];
+  a[4] = b[4];
+  a[5] = b[7];
+  a[6] = b[4];
+  a[7] = b[7];
+}
+
+int main()
+{
+  int i;
+  check_vect ();
+  for (i = 0; i < 8; ++i)
+    b[i] = i;
+  foo ();
+  if (a[0] != 0 || a[1] != 0 || a[2] != 3 || a[3] != 3
+      || a[4] != 4 || a[5] != 7 || a[6] != 4 || a[7] != 7)
+    abort ();
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump "basic block vectorized" "slp2" { target vect_perm } } } */
