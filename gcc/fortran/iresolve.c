8fa3561113f8432706fc5426408fe7b63ad17eca
tkoenig <tkoenig@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-06-04 18:55:29 +0000

    2015-06-04  Thomas Koenig  <tkoenig@netcologne.de>
    	PR fortran/58749
    	* iresolve.c (gfc_resolve_adjustl):  If string has a charlen,
    	copy it to the function.
    	(gfc_resolve_adjustr):  Likewise.
    
    2015-06-04  Thomas Koenig  <tkoenig@netcologne.de>
    
    	PR fortran/58749
    	* gfortran.dg/adjustl_1.f90:  New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@224137 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/iresolve.c
+++ gcc/fortran/iresolve.c
@@ -215,6 +215,9 @@ gfc_resolve_adjustl (gfc_expr *f, gfc_expr *string)
 {
   f->ts.type = BT_CHARACTER;
   f->ts.kind = string->ts.kind;
+  if (string->ts.u.cl)
+    f->ts.u.cl = gfc_new_charlen (gfc_current_ns, string->ts.u.cl);
+
   f->value.function.name = gfc_get_string ("__adjustl_s%d", f->ts.kind);
 }
 
@@ -224,6 +227,9 @@ gfc_resolve_adjustr (gfc_expr *f, gfc_expr *string)
 {
   f->ts.type = BT_CHARACTER;
   f->ts.kind = string->ts.kind;
+  if (string->ts.u.cl)
+    f->ts.u.cl = gfc_new_charlen (gfc_current_ns, string->ts.u.cl);
+
   f->value.function.name = gfc_get_string ("__adjustr_s%d", f->ts.kind);
 }
 
