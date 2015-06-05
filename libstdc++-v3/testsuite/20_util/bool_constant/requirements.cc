d51560f9afc5c8a826bcfa6fc90a96156b623559
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-06-05 17:17:53 +0000

    * doc/xml/manual/status_cxx2017.xml: Update status tables. 	* doc/html/manual/*: Regenerate. 	* include/std/type_traits (bool_constant): Define. 	* testsuite/20_util/bool_constant/requirements.cc: New. 	* testsuite/20_util/declval/requirements/1_neg.cc: Adjust dg-error. 	* testsuite/20_util/make_unsigned/requirements/typedefs_neg.cc: 	Likewise. 	* testsuite/20_util/make_signed/requirements/typedefs_neg.cc: 	Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@224160 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/20_util/bool_constant/requirements.cc
+++ libstdc++-v3/testsuite/20_util/bool_constant/requirements.cc
@@ -1,29 +1,26 @@
-// { dg-options "-std=gnu++11" }
-// { dg-do compile }
-// 2009-11-12  Paolo Carlini  <paolo.carlini@oracle.com>
-//
-// Copyright (C) 2009-2015 Free Software Foundation, Inc.
+// Copyright (C) 2015 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the
 // Free Software Foundation; either version 3, or (at your option)
 // any later version.
-//
+
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-//
+
 // You should have received a copy of the GNU General Public License along
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-error "static assertion failed" "" { target *-*-* } 2209 }
+// { dg-options "-std=gnu++17" }
+// { dg-do compile }
 
-#include <utility>
+#include <type_traits>
 
-void test01()
-{
-  std::declval<int>();		// { dg-error "required from here" }
-}
+static_assert( std::is_same<std::bool_constant<true>, std::true_type>::value,
+    "bool_constant<true> is true_type");
+static_assert( std::is_same<std::bool_constant<false>, std::false_type>::value,
+    "bool_constant<false> is false_type");
