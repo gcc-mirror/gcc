eac25264ebcfd82871dec732d818cbd1f4dc987c
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-06-08 10:15:34 +0000

    * objc-runtime-shared-support.c (build_module_descriptor): Set 	TREE_USED on UOBJC_MODULES_decl.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@224213 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/objc/objc-runtime-shared-support.c
+++ gcc/objc/objc-runtime-shared-support.c
@@ -519,6 +519,9 @@ build_module_descriptor (long vers, tree attr)
      is referenced by the runtime and, therefore, needed.  */
   DECL_PRESERVE_P (UOBJC_MODULES_decl) = 1;
 
+  /* Squash `defined but not used' warning.  */
+  TREE_USED (UOBJC_MODULES_decl) = 1;
+
   /* Allow the runtime to mark meta-data such that it can be assigned to target
      specific sections by the back-end.  */
   if (attr)
