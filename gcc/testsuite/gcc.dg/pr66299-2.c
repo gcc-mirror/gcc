f2e609c3e1863eb9a902cf45f5c5bbaf2e48e3cb
Marek Polacek <polacek@redhat.com>
2015-06-09 14:24:04 +0000

    re PR tree-optimization/66299 (more optimize opportunity)
    	PR tree-optimization/66299
    	* match.pd ((CST1 << A) == CST2 -> A == ctz (CST2) - ctz (CST1)
    	((CST1 << A) != CST2 -> A != ctz (CST2) - ctz (CST1)): New
    	patterns.
    
    	* gcc.dg/pr66299-1.c: New test.
    	* gcc.dg/pr66299-2.c: New test.
    	* gcc.dg/pr66299-3.c: New test.
    
    From-SVN: r224283

---
+++ gcc/testsuite/gcc.dg/pr66299-2.c
@@ -0,0 +1,33 @@
+/* PR tree-optimization/66299 */
+/* { dg-do run } */
+/* { dg-options "-fdump-tree-optimized -O" } */
+
+void
+test1 (int x, unsigned u)
+{
+  if ((1U << x) != 64
+      || (2 << x) != u
+      || (x << x) != 384
+      || (3 << x) == 9
+      || (x << 14) != 98304U
+      || (1 << x) == 14
+      || (3 << 2) != 12)
+    __builtin_abort ();
+}
+
+void
+test2 (int x)
+{
+  unsigned int t = ((unsigned int) 1U << x);
+  if (t != 2U)
+    __builtin_abort ();
+}
+
+int
+main (void)
+{
+  test1 (6, 128U);
+  test2 (1);
+}
+
+/* { dg-final { scan-tree-dump-not "<<" "optimized" } } */
