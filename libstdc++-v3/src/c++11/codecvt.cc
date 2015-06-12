af0b06c72817386e0ebdcc6c81500659aa1436de
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-06-12 10:26:05 +0000

    PR libstdc++/66464 	* src/c++11/codecvt.cc (codecvt_utf16_base<char32_t>::do_max_length): 	Return 4 not 3.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@224415 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/src/c++11/codecvt.cc
+++ libstdc++-v3/src/c++11/codecvt.cc
@@ -1124,7 +1124,7 @@ do_length(state_type&, const extern_type* __from,
 
 int
 __codecvt_utf16_base<char32_t>::do_max_length() const throw()
-{ return 3; }
+{ return 4; }
 
 #ifdef _GLIBCXX_USE_WCHAR_T
 // Define members of codecvt_utf16<wchar_t> base class implementation.
