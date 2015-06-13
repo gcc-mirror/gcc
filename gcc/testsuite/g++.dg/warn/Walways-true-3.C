db3d1ffcbb4c7b1193b23c17dc14d81c3f8dea0a
ppalka <ppalka@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-06-13 16:11:15 +0000

    Emit -Waddress warnings for comparing address of reference against NULL
    gcc/c-family/ChangeLog:
    
    	PR c++/65168
    	* c-common.c (c_common_truthvalue_conversion): Warn when
    	converting an address of a reference to a truth value.
    
    gcc/cp/ChangeLog:
    
    	PR c++/65168
    	* typeck.c (cp_build_binary_op): Warn when comparing an address
    	of a reference against NULL.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/65168
    	g++.dg/warn/Walways-true-3.C: New test.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@224455 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/warn/Walways-true-3.C
@@ -0,0 +1,46 @@
+// PR c++/65168
+// { dg-options "-Waddress" }
+
+void foo (void);
+
+int d;
+int &c = d;
+
+void
+bar (int &a)
+{
+  int &b = a;
+
+  if ((int *)&a) // { dg-warning "address of" }
+    foo ();
+
+  if (&b) // { dg-warning "address of" }
+    foo ();
+
+  if (!&c) // { dg-warning "address of" }
+    foo ();
+
+  if (!&(int &)(int &)a) // { dg-warning "address of" }
+    foo ();
+
+  if (&a == 0) // { dg-warning "never be NULL" }
+    foo ();
+
+  if (&b != 0) // { dg-warning "never be NULL" }
+    foo ();
+
+  if (0 == &(int &)(int &)c) // { dg-warning "never be NULL" }
+    foo ();
+
+  if (&a != (int *)0) // { dg-warning "never be NULL" }
+    foo ();
+}
+
+bool
+bar_1 (int &a)
+{
+  if (d == 5)
+    return &a; // { dg-warning "address of" }
+  else
+    return !&(int &)(int &)a; // { dg-warning "address of" }
+}
