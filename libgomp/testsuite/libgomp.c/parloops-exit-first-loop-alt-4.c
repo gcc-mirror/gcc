a6860be9c89b7025c938f7a53c209478d3d84ca9
Tom de Vries <tom@codesourcery.com>
2015-06-13 08:05:54 +0000

    Add gcc.dg/parloops-exit-first-loop-alt-4.c
    2015-06-13  Tom de Vries  <tom@codesourcery.com>
    
    	* gcc.dg/parloops-exit-first-loop-alt-4.c: New test.
    
    	* testsuite/libgomp.c/parloops-exit-first-loop-alt-4.c: New test.
    
    From-SVN: r224453

---
+++ libgomp/testsuite/libgomp.c/parloops-exit-first-loop-alt-4.c
@@ -0,0 +1,33 @@
+/* { dg-do run } */
+/* { dg-options "-O2 -ftree-parallelize-loops=2" } */
+
+/* Constant bound, reduction.  */
+
+#define N 4000
+
+unsigned int *a;
+
+unsigned int
+f (void)
+{
+  int i;
+  unsigned int sum = 1;
+
+  for (i = 0; i < N; ++i)
+    sum += a[i];
+
+  return sum;
+}
+
+int
+main (void)
+{
+  unsigned int res;
+  unsigned int array[N];
+  int i;
+  for (i = 0; i < N; ++i)
+    array[i] = i % 7;
+  a = &array[0];
+  res = f ();
+  return !(res == 11995);
+}
