ab313a8c426ebb85411fee580d696537c520cff0
Richard Biener <rguenther@suse.de>
2015-06-16 10:29:16 +0000

    tree-vect-stmts.c (vectorizable_load): Properly start loads with the first element if this is grouped loads.
    2015-06-16  Richard Biener  <rguenther@suse.de>
    
    	* tree-vect-stmts.c (vectorizable_load): Properly start loads
    	with the first element if this is grouped loads.
    
    	* gcc.dg/vect/slp-perm-11.c: New testcase.
    
    From-SVN: r224511

---
+++ gcc/testsuite/gcc.dg/vect/slp-perm-11.c
@@ -0,0 +1,35 @@
+/* { dg-require-effective-target vect_int } */
+
+#include "tree-vect.h"
+
+int a[64];
+int b[128];
+
+void __attribute__((noinline, noclone))
+foo (int s)
+{
+  int i;
+  for (i = 0; i < 32; ++i)
+    {
+      a[2*i] = b[i*s+1];
+      a[2*i+1] = b[i*s];
+    }
+}
+
+int main ()
+{
+  int i;
+  check_vect ();
+  for (i = 0; i < 128; ++i)
+    {
+      b[i] = i;
+      __asm__ volatile ("");
+    }
+  foo (4);
+  for (i = 0; i < 64; ++i)
+    if (a[i] != (4*(i/2) + (i & 1) ^ 1))
+      abort ();
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "vectorizing stmts using SLP" 1 "vect" { target vect_perm } } } */
