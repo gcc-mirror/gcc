855f036dcc09c3c0a7718bdcb0fd4125b57c1dce
Ilya Enkovich <enkovich.gnu@gmail.com>
2015-06-18 10:14:38 +0000

    re PR middle-end/66568 ([CHKP] internal compiler error: in expand_expr_addr_expr_1)
    gcc/
    
    	PR middle-end/66568
    	* cfgexpand.c (expand_return): Handle missing bounds.
    	(expand_gimple_stmt_1): Likewise.
    	* tree-chkp.c (chkp_expand_zero_bounds): New.
    	* tree-chkp.h (chkp_expand_zero_bounds): New.
    
    gcc/testsuite/
    
    	PR middle-end/66568
    	* gcc.target/i386/mpx/pr66568.c: New test.
    
    From-SVN: r224601

---
+++ gcc/testsuite/gcc.target/i386/mpx/pr66568.c
@@ -0,0 +1,10 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target fpic } */
+/* { dg-options "-O2 -fcheck-pointer-bounds -mmpx -O2 -fPIC" } */
+
+int a, b, c;
+void *set_test () {
+  if (b)
+    a ? exit (0) : exit (1);
+  b = c;
+}
