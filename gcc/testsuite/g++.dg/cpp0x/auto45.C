425ae6f18d229abada00788b079a75d913d2fa2a
ppalka <ppalka@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-06-24 12:10:57 +0000

    Revert fix for PR c++/30044
    gcc/cp/ChangeLog:
    
    	Revert:
    	2015-06-23  Patrick Palka  <ppalka@gcc.gnu.org>
    
    	PR c++/30044
    	* parser.c (cp_parser_template_parameter_list): Update
    	current_template_parms right after processing a paramater.
    	* pt.c (template_parms_to_args): Remove obsolete hack for
    	giving template template arguments the proper level.
    	(check_default_tmpl_args): Account for tested template
    	parameter_lists.
    	(splite_late_return_type): Remove obsolete hack for giving
    	template template arguments the proper level.
    
    gcc/testsuite/ChangeLog:
    
    	Revert:
    	2015-06-23  Patrick Palka  <ppalka@gcc.gnu.org>
    
    	PR c++/30044
    	* g++.dg/cpp0x/auto45.C: New test.
    	* g++.dg/template/pr30044.C: New test.
    	* g++.dg/template/crash83.C: Accept any error string.
    	* g++.dg/cpp0x/variadic18.C: Adjust to not shadow template
    	parameters.
    	* g++.dg/cpp0x/variadic18.C: Likewise
    	* g++.dg/template/canon-type-13.C: Likewise.
    	* g++.old-deja/g++.pt/ttp42.C: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@224896 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp0x/auto45.C
+++
@@ -1,5 +0,0 @@
-// Addendum to auto23.C, now with nested template parameter lists
-// { dg-do compile { target c++11 } }
-
-template<template <auto f()->int> class> struct A { };
-template<template <template <auto f()->int> class> class> struct B { };
