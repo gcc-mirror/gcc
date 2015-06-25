d6a818c59e002f76f237ad305bf4b73a415dcb3c
Richard Sandiford <richard.sandiford@arm.com>
2015-06-25 17:15:44 +0000

    tree-hash-traits.h: New file.
    gcc/
    	* tree-hash-traits.h: New file.
    	(tree_operand_hash): New class.
    	* sanopt.c: Include tree-hash-traits.h.
    	(sanopt_tree_map_traits): Use tree_operand_hash.
    	* tree-if-conv.c: Include tree-hash-traits.h.
    	(phi_args_hash_traits): Use tree_operand_hash.
    	* tree-ssa-uncprop.c: Include tree-hash-traits.h.
    	(val_ssa_equiv_hash_traits): Use tree_operand_hash.
    
    From-SVN: r224967

---
+++ gcc/tree-hash-traits.h
@@ -0,0 +1,42 @@
+/* Traits for hashing trees.
+   Copyright (C) 2014-2015 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef tree_hash_traits_h
+#define tree_hash_traits_h
+
+/* Hash for trees based on operand_equal_p.  */
+struct tree_operand_hash : ggc_ptr_hash <tree_node>
+{
+  static inline hashval_t hash (const_tree);
+  static inline bool equal_keys (const_tree, const_tree);
+};
+
+inline hashval_t
+tree_operand_hash::hash (const_tree t)
+{
+  return iterative_hash_expr (t, 0);
+}
+
+inline bool
+tree_operand_hash::equal_keys (const_tree t1, const_tree t2)
+{
+  return operand_equal_p (t1, t2, 0);
+}
+
+#endif
