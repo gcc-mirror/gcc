eee335102e36d60367366895729a48b1a24fb23c
Andrew MacLeod <amacleod@redhat.com>
2015-06-25 16:27:12 +0000

    alias.h (alias_set_type): Move typedef.
    2015-06-25  Andrew Macleod  <amacleod@redhat.com>
    
    	* alias.h (alias_set_type): Move typedef.
    	* coretypes.h (alias_set_type): Relocate typedef here.
    	* rtl.h: Don't include alias.h.
    	* jit/jit-common.h: Likewise
    
    From-SVN: r224948

--- gcc/alias.h
+++ gcc/alias.h
@@ -20,14 +20,6 @@ along with GCC; see the file COPYING3.  If not see
 #ifndef GCC_ALIAS_H
 #define GCC_ALIAS_H
 
-/* The type of an alias set.  Code currently assumes that variables of
-   this type can take the values 0 (the alias set which aliases
-   everything) and -1 (sometimes indicating that the alias set is
-   unknown, sometimes indicating a memory barrier) and -2 (indicating
-   that the alias set should be set to a unique value but has not been
-   set yet).  */
-typedef int alias_set_type;
-
 extern alias_set_type new_alias_set (void);
 extern alias_set_type get_alias_set (tree);
 extern alias_set_type get_deref_alias_set (tree);
