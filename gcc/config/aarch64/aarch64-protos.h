14677da9ae68b3f517bf1225340d4f39429f37e1
jgreenhalgh <jgreenhalgh@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-06-26 13:58:29 +0000

    [Patch AArch64 3/4] De-const-ify struct tune_params
    gcc/
    
    	* config/aarch64/aarch64-protos.h (tune_params): Remove
    	const from members.
    	(aarch64_tune_params): Remove const, change to no longer be
    	a pointer.
    	* config/aarch64/aarch64.c (aarch64_tune_params): Remove const,
    	change to no longer be a pointer, initialize to generic_tunings.
    	(aarch64_min_divisions_for_recip_mul): Change dereference of
    	aarch64_tune_params to member access.
    	(aarch64_reassociation_width): Likewise.
    	(aarch64_rtx_mult_cost): Likewise.
    	(aarch64_address_cost): Likewise.
    	(aarch64_branch_cost): Likewise.
    	(aarch64_rtx_costs): Likewise.
    	(aarch64_register_move_cost): Likewise.
    	(aarch64_memory_move_cost): Likewise.
    	(aarch64_sched_issue_rate): Likewise.
    	(aarch64_builtin_vectorization_cost): Likewise.
    	(aarch64_override_options): Take a copy of the selected tuning
    	struct in to aarch64_tune_params, rather than just setting
    	a pointer, change dereferences of aarch64_tune_params to member
    	accesses.
    	(aarch64_override_options_after_change): Change dereferences of
    	aarch64_tune_params to member access.
    	(aarch64_macro_fusion_p): Likewise.
    	(aarch_macro_fusion_pair_p): Likewise.
    	* config/aarch64/cortex-a57-fma-steering.c (gate): Likewise.
    
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@225016 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/aarch64/aarch64-protos.h
+++ gcc/config/aarch64/aarch64-protos.h
@@ -172,23 +172,23 @@ struct cpu_branch_cost
 
 struct tune_params
 {
-  const struct cpu_cost_table *const insn_extra_cost;
-  const struct cpu_addrcost_table *const addr_cost;
-  const struct cpu_regmove_cost *const regmove_cost;
-  const struct cpu_vector_cost *const vec_costs;
-  const struct cpu_branch_cost *const branch_costs;
-  const int memmov_cost;
-  const int issue_rate;
-  const unsigned int fusible_ops;
-  const int function_align;
-  const int jump_align;
-  const int loop_align;
-  const int int_reassoc_width;
-  const int fp_reassoc_width;
-  const int vec_reassoc_width;
-  const int min_div_recip_mul_sf;
-  const int min_div_recip_mul_df;
-  const unsigned int extra_tuning_flags;
+  const struct cpu_cost_table *insn_extra_cost;
+  const struct cpu_addrcost_table *addr_cost;
+  const struct cpu_regmove_cost *regmove_cost;
+  const struct cpu_vector_cost *vec_costs;
+  const struct cpu_branch_cost *branch_costs;
+  int memmov_cost;
+  int issue_rate;
+  unsigned int fusible_ops;
+  int function_align;
+  int jump_align;
+  int loop_align;
+  int int_reassoc_width;
+  int fp_reassoc_width;
+  int vec_reassoc_width;
+  int min_div_recip_mul_sf;
+  int min_div_recip_mul_df;
+  unsigned int extra_tuning_flags;
 };
 
 #define AARCH64_FUSION_PAIR(x, name, index) \
@@ -229,7 +229,7 @@ enum aarch64_extra_tuning_flags
 };
 #undef AARCH64_EXTRA_TUNING_OPTION
 
-extern const struct tune_params *aarch64_tune_params;
+extern struct tune_params aarch64_tune_params;
 
 HOST_WIDE_INT aarch64_initial_elimination_offset (unsigned, unsigned);
 int aarch64_get_condition_code (rtx);
