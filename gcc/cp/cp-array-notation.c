faf67f0c4508f805dbda667f2f984485d1625702
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-06-26 10:03:35 +0000

    * cp-array-notation.c (expand_sec_reduce_builtin): Use INDIRECT_REF_P. 	* cp-ubsan.c (cp_ubsan_check_member_access_r): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@224999 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/cp-array-notation.c
+++ gcc/cp/cp-array-notation.c
@@ -345,7 +345,7 @@ expand_sec_reduce_builtin (tree an_builtin_fn, tree *new_var)
     array_ind_value = get_temp_regvar (TREE_TYPE (func_parm), func_parm);
 
   array_op0 = (*array_operand)[0];
-  if (TREE_CODE (array_op0) == INDIRECT_REF)
+  if (INDIRECT_REF_P (array_op0))
     array_op0 = TREE_OPERAND (array_op0, 0);
   switch (an_type)
     {
