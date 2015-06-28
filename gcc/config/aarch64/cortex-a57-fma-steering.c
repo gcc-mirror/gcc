17369fbf51df42a766494aeee16b3c74a331ae4d
Chung-Lin Tang <cltang@codesourcery.com>
2015-06-28 20:46:01 +0000

    regrename.h (regrename_do_replace): Change to return bool.
    2015-06-28  Chung-Lin Tang <cltang@codesourcery.com>
    	    Sandra Loosemore <sandra@codesourcery.com>
    
    	gcc/
    	* regrename.h (regrename_do_replace): Change to return bool.
    	* regrename.c (rename_chains): Check return value of
    	regname_do_replace.
    	(regrename_do_replace): Re-validate the modified insns and
    	return bool status.
    	* config/aarch64/cortex-a57-fma-steering.c (rename_single_chain):
    	Update to match rename_chains changes.
    	* config/c6x/c6x.c (try_rename_operands): Assert that
    	regrename_do_replace returns true.
    
    Co-Authored-By: Sandra Loosemore <sandra@codesourcery.com>
    
    From-SVN: r225106

--- gcc/config/aarch64/cortex-a57-fma-steering.c
+++ gcc/config/aarch64/cortex-a57-fma-steering.c
@@ -289,11 +289,19 @@ rename_single_chain (du_head_p head, HARD_REG_SET *unavailable)
       return false;
     }
 
-  if (dump_file)
-    fprintf (dump_file, ", renamed as %s\n", reg_names[best_new_reg]);
-
-  regrename_do_replace (head, best_new_reg);
-  df_set_regs_ever_live (best_new_reg, true);
+  if (regrename_do_replace (head, best_new_reg))
+    {
+      if (dump_file)
+	fprintf (dump_file, ", renamed as %s\n", reg_names[best_new_reg]);
+      df_set_regs_ever_live (best_new_reg, true);
+    }
+  else
+    {
+      if (dump_file)
+	fprintf (dump_file, ", renaming as %s failed\n",
+		 reg_names[best_new_reg]);
+      return false;
+    }
   return true;
 }
 
