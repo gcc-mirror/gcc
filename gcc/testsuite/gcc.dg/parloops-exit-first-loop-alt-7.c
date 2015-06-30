6f6c75e2c9c4abdd87d4c4b15c3ace2f74906966
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-06-30 11:00:32 +0000

    Add parloops-exit-first-loop-alt-{5,6,7}.c
    2015-06-30  Tom de Vries  <tom@codesourcery.com>
    
    	* gcc.dg/parloops-exit-first-loop-alt-5.c: New test.
    	* gcc.dg/parloops-exit-first-loop-alt-6.c: New test.
    	* gcc.dg/parloops-exit-first-loop-alt-7.c: New test.
    	* gcc.dg/parloops-exit-first-loop-alt.c: Update comment.
    
    	* testsuite/libgomp.c/parloops-exit-first-loop-alt-5.c: New test.
    	* testsuite/libgomp.c/parloops-exit-first-loop-alt-6.c: New test.
    	* testsuite/libgomp.c/parloops-exit-first-loop-alt-7.c: New test.
    	* testsuite/libgomp.c/parloops-exit-first-loop-alt.c: Update comment.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@225172 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/parloops-exit-first-loop-alt-7.c
+++ gcc/testsuite/gcc.dg/parloops-exit-first-loop-alt-7.c
@@ -2,10 +2,10 @@
 /* { dg-require-effective-target pthread } */
 /* { dg-options "-O2 -ftree-parallelize-loops=2 -fdump-tree-parloops" } */
 
-/* Variable bound, vector addition, signed loop counter, unsigned bound.  */
+/* Variable bound, vector addition, signed loop counter, signed bound.  */
 
 void
-f (unsigned int n, unsigned int *__restrict__ a, unsigned int *__restrict__ b,
+f (int n, unsigned int *__restrict__ a, unsigned int *__restrict__ b,
    unsigned int *__restrict__ c)
 {
   int i;
--- libgomp/testsuite/libgomp.c/parloops-exit-first-loop-alt-7.c
+++ libgomp/testsuite/libgomp.c/parloops-exit-first-loop-alt-7.c
@@ -1,7 +1,7 @@
 /* { dg-do run } */
 /* { dg-additional-options "-ftree-parallelize-loops=2" } */
 
-/* Variable bound, vector addition, signed loop counter, unsigned bound.  */
+/* Variable bound, vector addition, signed loop counter, signed bound.  */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +13,7 @@ unsigned int b[N];
 unsigned int c[N];
 
 void __attribute__((noclone,noinline))
-f (unsigned int n, unsigned int *__restrict__ a, unsigned int *__restrict__ b,
+f (int n, unsigned int *__restrict__ a, unsigned int *__restrict__ b,
    unsigned int *__restrict__ c)
 {
   int i;
@@ -22,8 +22,8 @@ f (unsigned int n, unsigned int *__restrict__ a, unsigned int *__restrict__ b,
     c[i] = a[i] + b[i];
 }
 
-int
-main (void)
+static void __attribute__((noclone,noinline))
+init (void)
 {
   int i, j;
 
@@ -36,6 +36,14 @@ main (void)
 	b[k] = (k * 3) % 7;
 	c[k] = k * 2;
       }
+}
+
+int
+main (void)
+{
+  int i;
+
+  init ();
 
   f (N, a, b, c);
 
@@ -43,6 +51,22 @@ main (void)
     {
       unsigned int actual = c[i];
       unsigned int expected = i + ((i * 3) % 7);
+      if (actual != expected)
+	abort ();
+    }
+
+  /* Test low iteration count case.  */
+
+  init ();
+
+  f (10, a, b, c);
+
+  for (i = 0; i < N; i++)
+    {
+      unsigned int actual = c[i];
+      unsigned int expected = (i < 10
+			       ? i + ((i * 3) % 7)
+			       : i * 2);
       if (actual != expected)
 	abort ();
     }
