8a36714a1b9855a1a0d99fe162947258c7ecd674
Tom de Vries <tom@codesourcery.com>
2015-06-30 11:00:32 +0000

    Add parloops-exit-first-loop-alt-{5,6,7}.c
    2015-06-30  Tom de Vries  <tom@codesourcery.com>
    
    	* gcc.dg/parloops-exit-first-loop-alt-5.c: New test.
    	* gcc.dg/parloops-exit-first-loop-alt-6.c: New test.
    	* gcc.dg/parloops-exit-first-loop-alt-7.c: New test.
    	* gcc.dg/parloops-exit-first-loop-alt.c: Update comment.
    
    	* testsuite/libgomp.c/parloops-exit-first-loop-alt-5.c: New test.
    	* testsuite/libgomp.c/parloops-exit-first-loop-alt-6.c: New test.
    	* testsuite/libgomp.c/parloops-exit-first-loop-alt-7.c: New test.
    	* testsuite/libgomp.c/parloops-exit-first-loop-alt.c: Update comment.
    
    From-SVN: r225172

--- gcc/testsuite/gcc.dg/parloops-exit-first-loop-alt-6.c
+++ gcc/testsuite/gcc.dg/parloops-exit-first-loop-alt-6.c
@@ -2,13 +2,13 @@
 /* { dg-require-effective-target pthread } */
 /* { dg-options "-O2 -ftree-parallelize-loops=2 -fdump-tree-parloops" } */
 
-/* Variable bound, vector addition, signed loop counter, unsigned bound.  */
+/* Variable bound, vector addition, unsigned loop counter, signed bound.  */
 
 void
-f (unsigned int n, unsigned int *__restrict__ a, unsigned int *__restrict__ b,
+f (int n, unsigned int *__restrict__ a, unsigned int *__restrict__ b,
    unsigned int *__restrict__ c)
 {
-  int i;
+  unsigned int i;
 
   for (i = 0; i < n; ++i)
     c[i] = a[i] + b[i];
