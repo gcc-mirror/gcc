32e90dc6a0cda452b426b370326e315ac6f10f02
edlinger <edlinger@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-01 16:10:30 +0000

    gcc/ChangeLog: 2015-07-01  Bernd Edlinger  <bernd.edlinger@hotmail.de>
            PR rtl-optimization/61047
            * rtlanal.c (get_initial_register_offset): New function.
            (rtx_addr_can_trap_p_1): Check offsets of stack references.
    
    testsuite/ChangeLog:
    2015-07-01  Bernd Edlinger  <bernd.edlinger@hotmail.de>
    
            PR rtl-optimization/61047
            * gcc.c-torture/execute/20150611-1.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@225260 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/20150611-1.c
@@ -0,0 +1,18 @@
+int a, c, d;
+short b;
+
+int
+main ()
+{
+  int e[1];
+  for (; b < 2; b++)
+    {
+      a = 0;
+      if (b == 28378)
+        a = e[b];
+      if (!(d || b))
+        for (; c;)
+          ;
+    }
+  return 0;
+}
