fb1481e42bc15a45e8b0c68538d274e5f75fb03f
kargl <kargl@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-02 17:29:04 +0000

    2015-07-02  Steven G. Kargl  <kargl@gcc.gnu.org>
    	PR fortran/56520
    	* match.c (gfc_match_name): Special case unary minus and plus.
    
    2015-07-02  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	PR fortran/56520
    	* gfortran.dg/pr56520.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@225349 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr56520.f90
@@ -0,0 +1,13 @@
+! { dg-do compile }
+! PR fortran/56520
+!
+program misleading
+    implicit none
+    real a, c
+    a = 1.0
+    c = exp(+a) )    ! { dg-error "Unclassifiable statement" }
+    c = exp(-a) )    ! { dg-error "Unclassifiable statement" }
+    c = exp((a)) )   ! { dg-error "Unclassifiable statement" }
+    c = exp(a) )     ! { dg-error "Unclassifiable statement" }
+    c = exp(a)
+end program misleading
