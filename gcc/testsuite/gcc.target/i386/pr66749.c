385e3f0c2b92341efb1654e14968a9996f4e9720
H.J. Lu <hjl@gcc.gnu.org>
2015-07-06 08:17:44 -0700

    Add -march=iamcu to optimize for IA MCU
    IA MCU is based on Intel Pentium ISA without x87 and passing parameters
    in registers.  We want to optimize for IA MCU without changing existing
    Pentium codegen.  This patch adds PROCESSOR_IAMCU for -march=iamcu,
    which is based on -march=pentium with updated cost tables.
    
    gcc/
    
    	PR target/66749
    	* config/i386/i386.c (iamcu_cost): New.
    	(m_IAMCU): Likewise.
    	(initial_ix86_arch_features): Disable X86_ARCH_CMOV for m_IAMCU.
    	(processor_target_table): Add an entry for "iamcu".
    	(processor_alias_table): Likewise.
    	(ix86_issue_rate): Handle PROCESSOR_IAMCU.
    	(ix86_adjust_cost): Likewise.
    	(ia32_multipass_dfa_lookahead): Likewise.
    	* config/i386/i386.h (processor_type): Add PROCESSOR_IAMCU.
    	* config/i386/x86-tune.def: Updated for m_IAMCU.
    
    gcc/testsuite/
    
    	PR target/66749
    	* gcc.target/i386/pr66749.c: New test.
    
    From-SVN: r225460

---
+++ gcc/testsuite/gcc.target/i386/pr66749.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target ia32 } */
+/* { dg-require-effective-target nonpic } */
+/* { dg-options "-O2 -miamcu -mtune=iamcu" } */
+
+char a[10], b[10];
+
+int f(int i)
+{
+  return a[i+1] + b[i+1];
+}
+
+/* { dg-final { scan-assembler "a\\+1" } } */
+/* { dg-final { scan-assembler "b\\+1" } } */
