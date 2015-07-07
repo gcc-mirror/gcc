345fc96e88f20b8420c9a6d4947afd29f9e6ab68
abennett <abennett@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-07 16:07:51 +0000

    MIPS: Do not generate micromips code for the no-smartmips-lwxs.c testcase
    The LWXS instruction is part of the micromips ISA which means it is
    valid to generate it for the no-smartmips-lwxs.c testcase.
    
    testsuite/
    	* gcc.target/mips/no-smartmips-lwxs.c: Change NOMIPS16 to
    	NOCOMPRESSION.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@225519 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/mips/no-smartmips-lwxs.c
+++ gcc/testsuite/gcc.target/mips/no-smartmips-lwxs.c
@@ -1,7 +1,7 @@
 /* { dg-do compile } */
 /* { dg-options "-mno-smartmips" } */
 
-NOMIPS16 int scaled_indexed_word_load (int a[], int b)
+NOCOMPRESSION int scaled_indexed_word_load (int a[], int b)
 {
   return a[b];
 }
