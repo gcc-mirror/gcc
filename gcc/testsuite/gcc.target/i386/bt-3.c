649b69652491a08be058729bce7359082b80afbc
Uros Bizjak <uros@gcc.gnu.org>
2015-07-07 23:03:02 +0200

    i386.md (*jcc_bt<mode>): Only split before reload.
    	* config/i386/i386.md (*jcc_bt<mode>): Only split before reload.
    	Remove operand constraints.  Change operand 2 predicate to
    	nonmemory operand.  Limit const_int values to mode bitsize.  Only
    	allow const_int values less than 32 when optimizing for size.
    	(*jcc_bt<mode>_1, *jcc_bt<mode>_mask): Only split before reload.
    	Remove operand constraints.
    	(*bt<mode>): Use SImode for const_int values less than 32.
    	(regmode): Remove mode attribute.
    
    testsuite/ChangeLog:
    
    	* gcc.target/i386/bt-3.c: New test.
    	* gcc.target/i386/bt-4.c: Ditto.
    
    From-SVN: r225527

---
+++ gcc/testsuite/gcc.target/i386/bt-3.c
@@ -0,0 +1,14 @@
+/* { dg-do compile { target { ! { ia32 } } } } */
+/* { dg-options "-O2 -mtune=core2" } */
+
+extern void foo (void);
+
+int test (long long x)
+{
+  if (x & ( 0x01ULL << 60 ))
+    foo ();
+
+  return 0;
+}
+
+/* { dg-final { scan-assembler "btq\[ \t\]" } } */
