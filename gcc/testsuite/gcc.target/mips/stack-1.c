3d1e4f66cf99e6ca1f039cef62ef305918ff9e61
Andrew Bennett <andrew.bennett@imgtec.com>
2015-07-08 08:37:22 +0000

    MIPS: Do not build the stack-1.c testcase for micromips.
    testsuite/
    	* gcc.target/mips/stack-1.c: Do not build the testcase for micromips.
    
    From-SVN: r225536

--- gcc/testsuite/gcc.target/mips/stack-1.c
+++ gcc/testsuite/gcc.target/mips/stack-1.c
@@ -2,8 +2,8 @@
 /* { dg-final { scan-assembler "\tlw\t" } } */
 /* { dg-final { scan-assembler-not "\td?addiu\t(\\\$sp,)?\\\$sp,\[1-9\].*\tlw\t" } } */
 
-/* Avoid use of SAVE and RESTORE.  */
-NOMIPS16 int foo (int y)
+/* Avoid use of SAVE, RESTORE and JRADDIUSP.  */
+NOCOMPRESSION int foo (int y)
 {
   volatile int a = y;
   volatile int *volatile b = &a;
