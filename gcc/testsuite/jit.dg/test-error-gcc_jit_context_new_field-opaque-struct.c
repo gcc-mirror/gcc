e93a6f3001d1cda3f255a7c7c7ae6645934f0202
dmalcolm <dmalcolm@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-08 14:41:59 +0000

    PR jit/66783: improve error messages
    gcc/jit/ChangeLog:
    	PR jit/66783
    	* libgccjit.c (gcc_jit_context_new_field): Show name of field in
    	"unknown size" error message.
    	(gcc_jit_struct_set_fields): Show name of struct in error message.
    	(gcc_jit_context_new_global): Show name of global in
    	"unknown size" error message.
    	(gcc_jit_function_new_local): Likewise for local.
    
    gcc/testsuite/ChangeLog:
    	PR jit/66783
    	* jit.dg/test-error-gcc_jit_context_new_field-opaque-struct.c
    	(verify_code): Update expected error message.
    	* jit.dg/test-error-gcc_jit_context_new_global-opaque-struct.c
    	(verify_code): Likewise.
    	* jit.dg/test-error-gcc_jit_function_new_local-opaque-struct.c
    	(verify_code): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@225557 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/jit.dg/test-error-gcc_jit_context_new_field-opaque-struct.c
+++ gcc/testsuite/jit.dg/test-error-gcc_jit_context_new_field-opaque-struct.c
@@ -27,5 +27,5 @@ verify_code (gcc_jit_context *ctxt, gcc_jit_result *result)
   /* Verify that the correct error message was emitted.  */
   CHECK_STRING_VALUE (gcc_jit_context_get_first_error (ctxt),
 		      "gcc_jit_context_new_field:"
-		      " type has unknown size (type: struct opaque)");
+		      " unknown size for field \"f_opaque\" (type: struct opaque)");
 }
