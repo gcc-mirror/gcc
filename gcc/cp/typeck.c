e14647a72c8dbf9a87beee08f043348d73c69374
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-09 15:14:10 +0000

    2015-07-09  Paolo Carlini  <paolo.carlini@oracle.com>
    	* typeck.c (warn_args_num): Rename to error_args_num.
    	(convert_arguments): Adjust calls.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@225615 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/typeck.c
+++ gcc/cp/typeck.c
@@ -62,7 +62,7 @@ static void casts_away_constness_r (tree *, tree *, tsubst_flags_t);
 static bool casts_away_constness (tree, tree, tsubst_flags_t);
 static bool maybe_warn_about_returning_address_of_local (tree);
 static tree lookup_destructor (tree, tree, tree, tsubst_flags_t);
-static void warn_args_num (location_t, tree, bool);
+static void error_args_num (location_t, tree, bool);
 static int convert_arguments (tree, vec<tree, va_gc> **, tree, int,
                               tsubst_flags_t);
 
@@ -3559,10 +3559,10 @@ cp_build_function_call_vec (tree function, vec<tree, va_gc> **params,
 }
 
 /* Subroutine of convert_arguments.
-   Warn about wrong number of args are genereted. */
+   Print an error message about a wrong number of arguments.  */
 
 static void
-warn_args_num (location_t loc, tree fndecl, bool too_many_p)
+error_args_num (location_t loc, tree fndecl, bool too_many_p)
 {
   if (fndecl)
     {
@@ -3645,7 +3645,7 @@ convert_arguments (tree typelist, vec<tree, va_gc> **values, tree fndecl,
 	{
           if (complain & tf_error)
             {
-	      warn_args_num (input_location, fndecl, /*too_many_p=*/true);
+	      error_args_num (input_location, fndecl, /*too_many_p=*/true);
               return i;
             }
           else
@@ -3749,7 +3749,7 @@ convert_arguments (tree typelist, vec<tree, va_gc> **values, tree fndecl,
       else
 	{
           if (complain & tf_error)
-	    warn_args_num (input_location, fndecl, /*too_many_p=*/false);
+	    error_args_num (input_location, fndecl, /*too_many_p=*/false);
 	  return -1;
 	}
     }
