52758cdb74c413a81998b146b9787e9afaa588a0
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-09 09:51:09 +0000

    2015-07-09  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/65790
    	* g++.dg/cpp0x/vt-65790.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@225607 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/vt-65790.C
@@ -0,0 +1,35 @@
+// PR c++/65790
+// { dg-do compile { target c++11 } }
+
+extern "C" int printf(const char* ...);
+
+namespace std
+{
+  typedef decltype(sizeof(0)) size_t;
+
+  template<typename _Tp, _Tp... _Idx>
+    struct integer_sequence
+    {
+      typedef _Tp value_type;
+      static constexpr size_t size() { return sizeof...(_Idx); }
+    };
+
+  template<size_t... _Idx>
+    using index_sequence = integer_sequence<size_t, _Idx...>;
+}
+
+void g(std::size_t a, std::size_t b, std::size_t c)
+{
+  printf("%zu, %zu, %zu\n", a, b, c);
+}
+
+template <std::size_t... Seq>
+void f(std::index_sequence<Seq...>)
+{
+  g(Seq...);
+}
+
+int main()
+{
+  f(std::index_sequence<0, 1, 2>());
+}
