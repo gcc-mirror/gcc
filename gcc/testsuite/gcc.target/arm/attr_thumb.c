3311dd0052d76209b3fb298ee4517cd7adfb72ff
clyon <clyon@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-10 12:02:13 +0000

    2015-07-10  Christophe Lyon  <christophe.lyon@linaro.org>
    	* gcc.target/arm/attr_thumb.c: Skip if Thumb is not supported.
    	* gcc.target/arm/flip-thumb.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@225665 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/attr_thumb.c
+++ gcc/testsuite/gcc.target/arm/attr_thumb.c
@@ -1,5 +1,7 @@
 /* Check that attribute target thumb is recognized. */
 /* { dg-do compile } */
+/* Make sure the current multilib supports thumb.  */
+/* { dg-skip-if "" { ! { arm_thumb1_ok || arm_thumb2_ok } } } */
 /* { dg-options "-O2 -mno-restrict-it" } */
 /* { dg-final { scan-assembler-not "\\.arm"  } } */
 /* { dg-final { scan-assembler "\\.thumb_func" } } */
