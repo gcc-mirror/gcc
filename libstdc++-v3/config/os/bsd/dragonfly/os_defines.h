90f2e4f36c652070a374339181f971de23f18a3b
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-13 14:53:08 +0000

    2015-07-13  John Marino  <gnugcc@marino.st>
    	* config/os/bsd/dragonfly/os_defines.h (_GLIBCXX_USE_C99_CHECK,
    	_GLIBCXX_USE_C99_DYNAMIC, _GLIBCXX_USE_C99_LONG_LONG_CHECK,
    	_GLIBCXX_USE_C99_LONG_LONG_DYNAMIC): Define.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@225737 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/config/os/bsd/dragonfly/os_defines.h
+++ libstdc++-v3/config/os/bsd/dragonfly/os_defines.h
@@ -29,4 +29,9 @@
 // System-specific #define, typedefs, corrections, etc, go here.  This
 // file will come before all others.
 
+#define _GLIBCXX_USE_C99_CHECK 1
+#define _GLIBCXX_USE_C99_DYNAMIC (!(__ISO_C_VISIBLE >= 1999))
+#define _GLIBCXX_USE_C99_LONG_LONG_CHECK 1
+#define _GLIBCXX_USE_C99_LONG_LONG_DYNAMIC (_GLIBCXX_USE_C99_DYNAMIC || !defined __LONG_LONG_SUPPORTED)
+
 #endif
