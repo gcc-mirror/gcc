1c504cbdad2cfebaf61606a4582d1c2e9c617d5c
renlin <renlin@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-13 08:29:46 +0000

    [PATCH]Fix PR66556. Don't drop side-effect in simplify_const_relational_operation function.
    gcc/
    
    2015-07-13  Renlin Li  <renlin.li@arm.com>
    
    	PR rtl/66556
    	* simplify-rtx.c (simplify_const_relational_operation): Add
    	side_effects_p checks.
    
    gcc/testsuite/
    
    2015-07-13  Renlin Li  <renlin.li@arm.com>
    
    	PR rtl/66556
    	* gcc.c-torture/execute/pr66556.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@225729 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/pr66556.c
@@ -0,0 +1,52 @@
+/* { dg-do run } */
+
+extern void abort (void);
+
+struct {
+  unsigned f2;
+  unsigned f3 : 15;
+  unsigned f5 : 3;
+  short f6;
+} b = {0x7f8000, 6, 5, 0}, g = {8, 0, 5, 0};
+
+short d, l;
+int a, c, h = 8;
+volatile char e[237] = {4};
+short *f = &d;
+short i[5] = {3};
+char j;
+int *k = &c;
+
+int
+fn1 (unsigned p1) { return -p1; }
+
+void
+fn2 (char p1)
+{
+  a = p1;
+  e[0];
+}
+
+short
+fn3 ()
+{
+  *k = 4;
+  return *f;
+}
+
+int
+main ()
+{
+
+  unsigned m;
+  short *n = &i[4];
+
+  m = fn1 ((h && j) <= b.f5);
+  l = m > g.f3;
+  *n = 3;
+  fn2 (b.f2 >> 15);
+  if ((a & 0xff) != 0xff)
+    abort ();
+
+  return 0;
+}
