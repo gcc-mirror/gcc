474d4bbe1dae16f9f846d4ea0d166aa382186c2b
Mantas Mikaitis <mantas.mikaitis@arm.com>
2015-07-13 16:00:39 +0000

    [ARM][testsuite] Fix FAIL: gcc.target/arm/macro_defs0.c and macro_defs1.c when -marm forced
    On behalf of Mantas Mikaitis
    
    2015-07-13  Mantas Mikaitis  <mantas.mikaitis@arm.com>
    
    	* gcc.target/arm/macro_defs0.c: Add directive to skip
    	test if -marm is present.
    	* gcc.target/arm/macro_defs1.c: Likewise.
    
    From-SVN: r225742

--- gcc/testsuite/gcc.target/arm/macro_defs1.c
+++ gcc/testsuite/gcc.target/arm/macro_defs1.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
 /* { dg-skip-if "avoid conflicting multilib options" { *-*-* } { "-march=*" } { "-march=armv6-m" } } */
+/* { dg-skip-if "avoid conflicting multilib options" { *-*-* } { "-marm" } { "" } } */
 /* { dg-options "-march=armv6-m -mthumb" } */
 
 #ifdef __ARM_NEON_FP
