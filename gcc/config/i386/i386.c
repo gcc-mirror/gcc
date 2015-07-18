1504c6a206b6f31174da607c047058965ef39a2e
Uros Bizjak <ubizjak@gmail.com>
2015-07-18 13:36:32 +0200

    re PR target/66922 (wrong code for bit-field struct at -O1 and above on x86_64-linux-gnu)
    	PR target/66922
    	* config/i386/i386.c (ix86_expand_pinsr): Reject insertions
    	to misaligned positions.
    
    testsuite/ChangeLog:
    
    	PR target/66922
    	* gcc.target/i386/pr66922.c: New test.
    
    From-SVN: r225982

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -45381,6 +45381,10 @@ ix86_expand_pinsr (rtx *operands)
 	    return false;
 	  }
 
+	/* Reject insertions to misaligned positions.  */
+	if (pos & (size-1))
+	  return false;
+
 	rtx d = dst;
 	if (GET_MODE (dst) != dstmode)
 	  d = gen_reg_rtx (dstmode);
