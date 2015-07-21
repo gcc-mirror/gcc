a26dad6a5955ed8574efc8d149faca3963a48d46
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-21 18:33:35 +0000

    * configure.ac: Add check for new options in isl-0.15. 	* config.in, configure: Rebuilt. 	* graphite-blocking.c: Include <isl/constraint.h> 	* graphite-interchange.c,  graphite-poly.c: Likewise. 	* graphhite-scop-detection.c, graphite-sese-to-poly.c: Likewise. 	* graphite.c: Likewise. 	* graphite-isl-ast-to-gimple.c: Include <isl/constraint.h> and 	<isl/union_set.h>. 	* graphite-dependences.c: Include <isl/constraint.h>. 	(max_number_of_out_dimensions): Returns isl_stat. 	(extend_schedule_1): Likewise 	(extend_schedule): Corresponding changes. 	* graphite-optimize-isl.c: Include <isl/constraint.h> and 	<isl/union_set.h>. 	(getSingleMap): Change return type of isl_stat. 	(optimize_isl): Conditionally use 	isl_options_set_schedule_serialize_sccs. 	* graphite-poly.h (isl_stat, isl_stat_ok): Define fallbacks 	if not HAVE_ISL_OPTIONS_SET_SCHEDULE_SERIALIZE_SCCS.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@226050 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/graphite-optimize-isl.c
+++ gcc/graphite-optimize-isl.c
@@ -24,7 +24,9 @@ along with GCC; see the file COPYING3.  If not see
 /* Workaround for GMP 5.1.3 bug, see PR56019.  */
 #include <stddef.h>
 
+#include <isl/constraint.h>
 #include <isl/set.h>
+#include <isl/union_set.h>
 #include <isl/map.h>
 #include <isl/union_map.h>
 #include <isl/schedule.h>
@@ -507,13 +509,13 @@ getScheduleMap (isl_schedule *Schedule, isl_union_map **map_sepcl)
   return ScheduleMap;
 }
 
-static int
+static isl_stat
 getSingleMap (__isl_take isl_map *map, void *user)
 {
   isl_map **singleMap = (isl_map **) user;
   *singleMap = map;
 
-  return 0;
+  return isl_stat_ok;
 }
 
 static void
@@ -585,7 +587,11 @@ optimize_isl (scop_p scop)
 
   isl_options_set_schedule_max_constant_term (scop->ctx, CONSTANT_BOUND);
   isl_options_set_schedule_maximize_band_depth (scop->ctx, 1);
+#ifdef HAVE_ISL_OPTIONS_SET_SCHEDULE_SERIALIZE_SCCS
+  isl_options_set_schedule_serialize_sccs (scop->ctx, 1);
+#else
   isl_options_set_schedule_fuse (scop->ctx, ISL_SCHEDULE_FUSE_MIN);
+#endif
   isl_options_set_on_error (scop->ctx, ISL_ON_ERROR_CONTINUE);
 
 #ifdef HAVE_ISL_SCHED_CONSTRAINTS_COMPUTE_SCHEDULE
--- gcc/graphite-poly.c
+++ gcc/graphite-poly.c
@@ -25,6 +25,7 @@ along with GCC; see the file COPYING3.  If not see
 /* Workaround for GMP 5.1.3 bug, see PR56019.  */
 #include <stddef.h>
 
+#include <isl/constraint.h>
 #include <isl/set.h>
 #include <isl/map.h>
 #include <isl/union_map.h>
