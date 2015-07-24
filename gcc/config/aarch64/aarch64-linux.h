cbc4d45487ba0bccc3562bb96b65419720acc13c
Szabolcs Nagy <szabolcs.nagy@arm.com>
2015-07-24 14:27:55 +0000

    [AArch64] Fix LINUX_TARGET_LINK_SPEC to be consistent with ARM
    2015-07-24  Szabolcs Nagy  <szabolcs.nagy@arm.com>
    
    	PR target/65711
    	* config/aarch64/aarch64-linux.h (LINUX_TARGET_LINK_SPEC): Move
    	-dynamic-linker within %{!static %{!shared, and -rdynamic within
    	%{!static.
    
    From-SVN: r226158

--- gcc/config/aarch64/aarch64-linux.h
+++ gcc/config/aarch64/aarch64-linux.h
@@ -38,8 +38,9 @@
    %{static:-Bstatic}				\
    %{shared:-shared}				\
    %{symbolic:-Bsymbolic}			\
-   %{rdynamic:-export-dynamic}			\
-   -dynamic-linker " GNU_USER_DYNAMIC_LINKER "	\
+   %{!static:					\
+     %{rdynamic:-export-dynamic}		\
+     %{!shared:-dynamic-linker " GNU_USER_DYNAMIC_LINKER "}} \
    -X						\
    %{mbig-endian:-EB} %{mlittle-endian:-EL}     \
    -maarch64linux%{mabi=ilp32:32}%{mbig-endian:b}"
