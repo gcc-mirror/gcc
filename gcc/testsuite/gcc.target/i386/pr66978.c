f269b7508516848039987c6e93df0496299bdedf
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-24 12:08:00 +0000

    Add a testcase for PR bootstrap/66978
    	PR bootstrap/66978
    	* gcc.target/i386/pr66978.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@226150 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr66978.c
@@ -0,0 +1,15 @@
+/* { dg-do compile { target { ! { ia32 } } } } */
+/* { dg-require-effective-target maybe_x32 } */
+/* { dg-options "-O2 -mx32 -maddress-mode=short" } */
+
+extern int foo (int *);
+int
+bar (int *p)
+{
+  __attribute__ ((noinline, noclone))
+  int hack_digit (void)
+    {
+      return foo (p);
+    }
+  return hack_digit ();
+}
