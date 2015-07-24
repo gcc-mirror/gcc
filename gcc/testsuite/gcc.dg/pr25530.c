5f8d832e470c6faefcf12440e1eef1f6f9b4af28
Naveen H.S <Naveen.Hurugalawadi@caviumnetworks.com>
2015-07-24 04:50:41 +0000

    re PR middle-end/25530 ((unsigned / 2)*2 is not changed into unsigned &~1)
    PR middle-end/25530
    2015-07-24  Naveen H.S  <Naveen.Hurugalawadi@caviumnetworks.com>
    
    gcc/testsuite/ChangeLog:
    	* gcc.dg/pr25530.c: New test.
    
    gcc/ChangeLog:
    	* match.pd (mult (trunc_div @0 integer_pow2p@1) @1) : New simplifier.
    
    From-SVN: r226137

---
+++ gcc/testsuite/gcc.dg/pr25530.c
@@ -0,0 +1,11 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+/* { dg-require-effective-target int32 } */
+
+int
+f (unsigned t)
+{
+  return (t / 2) * 2;
+}
+
+/* { dg-final { scan-tree-dump "\& -2" "optimized" } } */
