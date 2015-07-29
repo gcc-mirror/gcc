64a69623069559cac702272c4a2c50d817c13d23
trippels <trippels@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-29 06:32:09 +0000

    Use fast unwinder for PowerPC
    2015-07-29  Markus Trippelsdorf  <markus@trippelsdorf.de>
    
    	PR sanitizer/63927
    	* sanitizer_common/sanitizer_stacktrace.h: Use fast unwinder
    	for PowerPC.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@226338 138bc75d-0d04-0410-961f-82ee72b054a4

--- libsanitizer/sanitizer_common/sanitizer_stacktrace.h
+++ libsanitizer/sanitizer_common/sanitizer_stacktrace.h
@@ -17,8 +17,7 @@ namespace __sanitizer {
 
 static const uptr kStackTraceMax = 256;
 
-#if SANITIZER_LINUX && (defined(__aarch64__) || defined(__powerpc__) || \
-                        defined(__powerpc64__) || defined(__sparc__) || \
+#if SANITIZER_LINUX && (defined(__aarch64__) || defined(__sparc__) || \
                         defined(__mips__))
 # define SANITIZER_CAN_FAST_UNWIND 0
 #elif SANITIZER_WINDOWS
