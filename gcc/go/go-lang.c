cb3306e5bf82c6a0f6de0c98dabdb15cd7e2529e
Andreas Schwab <schwab@linux-m68k.org>
2015-07-31 07:08:20 +0000

    go-lang.c (go_langhook_init_options_struct): Don't set x_flag_split_stack.
    * go-lang.c (go_langhook_init_options_struct): Don't set
    x_flag_split_stack.
    (go_langhook_post_options): Set it here instead.
    
    From-SVN: r226428

--- gcc/go/go-lang.c
+++ gcc/go/go-lang.c
@@ -155,10 +155,6 @@ go_langhook_init_options_struct (struct gcc_options *opts)
   opts->x_flag_errno_math = 0;
   opts->frontend_set_flag_errno_math = true;
 
-  /* We turn on stack splitting if we can.  */
-  if (targetm_common.supports_split_stack (false, opts))
-    opts->x_flag_split_stack = 1;
-
   /* Exceptions are used to handle recovering from panics.  */
   opts->x_flag_exceptions = 1;
   opts->x_flag_non_call_exceptions = 1;
@@ -291,6 +287,11 @@ go_langhook_post_options (const char **pfilename ATTRIBUTE_UNUSED)
   if (global_options.x_debug_info_level == DINFO_LEVEL_TERSE
       && global_options.x_write_symbols == NO_DEBUG)
     global_options.x_write_symbols = PREFERRED_DEBUGGING_TYPE;
+
+  /* We turn on stack splitting if we can.  */
+  if (!global_options_set.x_flag_split_stack
+      && targetm_common.supports_split_stack (false, &global_options))
+    global_options.x_flag_split_stack = 1;
 
   /* Returning false means that the backend should be used.  */
   return false;
