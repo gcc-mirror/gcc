6cb66519c3fec915e5377e2b71c87bf65564fd9a
nsz <nsz@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-08-03 11:12:00 +0000

    [ARM] PR target/66731 Fix vnmul insn with -frounding-math
    gcc:
    
    	PR target/66731
    	* config/arm/vfp.md (negmuldf3_vfp): Add new pattern.
    	(negmulsf3_vfp): Likewise.
    	(muldf3negdf_vfp): Disable for -frounding-math.
    	(mulsf3negsf_vfp): Likewise.
    	* config/arm/arm.c (arm_new_rtx_costs): Fix NEG cost for VNMUL,
    	fix MULT cost with -frounding-math.
    
    gcc/testsuite:
    
    	PR target/66731
    	* gcc.target/arm/vnmul-1.c: New.
    	* gcc.target/arm/vnmul-2.c: New.
    	* gcc.target/arm/vnmul-3.c: New.
    	* gcc.target/arm/vnmul-4.c: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@226496 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/vnmul-4.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target arm_vfp_ok } */
+/* { dg-skip-if "need fp instructions" { *-*-* } { "-mfloat-abi=soft" } { "" } } */
+/* { dg-options "-O2 -frounding-math -mfpu=vfp -mfloat-abi=hard" } */
+
+double
+foo_d (double a, double b)
+{
+  /* { dg-final { scan-assembler "vnmul\\.f64" } } */
+  return -(a * b);
+}
+
+float
+foo_s (float a, float b)
+{
+  /* { dg-final { scan-assembler "vnmul\\.f32" } } */
+  return -(a * b);
+}
