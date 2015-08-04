0efacb57a40412c029d4b33503f2bad15538072c
Kyrylo Tkachov <kyrylo.tkachov@arm.com>
2015-08-04 10:41:31 +0000

    [AArch64][12/14] Target attributes and target pragmas tests
    	* gcc.target/aarch64/pragma_cpp_predefs_1.c: New test.
    	* gcc.target/aarch64/target_attr_1.c: Likewise.
    	* gcc.target/aarch64/target_attr_2.c: Likewise.
    	* gcc.target/aarch64/target_attr_3.c: Likewise.
    	* gcc.target/aarch64/target_attr_4.c: Likewise.
    	* gcc.target/aarch64/target_attr_5.c: Likewise.
    	* gcc.target/aarch64/target_attr_6.c: Likewise.
    	* gcc.target/aarch64/target_attr_7.c: Likewise.
    	* gcc.target/aarch64/target_attr_8.c: Likewise.
    	* gcc.target/aarch64/target_attr_9.c: Likewise.
    	* gcc.target/aarch64/target_attr_10.c: Likewise.
    	* gcc.target/aarch64/target_attr_11.c: Likewise.
    	* gcc.target/aarch64/target_attr_12.c: Likewise.
    	* gcc.target/aarch64/target_attr_13.c: Likewise.
    	* gcc.target/aarch64/target_attr_14.c: Likewise.
    	* gcc.target/aarch64/target_attr_15.c: Likewise.
    
    From-SVN: r226565

---
+++ gcc/testsuite/gcc.target/aarch64/target_attr_12.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+
+/* Reject arguments to attributes that do not accept any.  */
+
+__attribute__ ((target ("general-regs-only=+crc")))
+int
+foo (int a)
+{
+  return a + 1;
+}
+
+/* { dg-error "does not accept an argument" "" { target *-*-* } 0 } */
+/* { dg-error "is invalid" "" { target *-*-* } 0 } */
---
+++ gcc/testsuite/gcc.target/aarch64/target_attr_13.c
@@ -0,0 +1,16 @@
+/* { dg-do assemble } */
+/* { dg-options "-O2 -march=armv8-a+crc+crypto" } */
+
+#include "arm_acle.h"
+
+/* Make sure that 'crypto' is not required to compile an intrinsic
+   from arm_acle.h in a non-crypto function.  Tests that arm_acle.h
+   properly clears the architectural features in its initial target
+   pragma.  */
+
+__attribute__ ((target ("+crc+nocrypto")))
+int
+foo (uint32_t a, uint8_t b)
+{
+  return __crc32b (a, b);
+}
