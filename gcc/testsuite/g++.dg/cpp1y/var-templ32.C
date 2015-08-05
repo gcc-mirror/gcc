9a69850dfd4332ed54a3b81fc62b7ae4802ee731
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-08-05 17:56:14 +0000

    PR c++/66260 	PR c++/66596 	PR c++/66649 	PR c++/66923 	* pt.c (lookup_template_variable): Use NULL_TREE for type. 	(instantiate_template_1): Also set DECL_TI_ARGS based on 	the immediate parent. 	(tsubst_copy_and_build) [TEMPLATE_ID_EXPR]: Handle variable templates. 	(finish_template_variable): Add complain parm. 	* cp-tree.h: Adjust.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@226642 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp1y/var-templ32.C
+++ gcc/testsuite/g++.dg/cpp1y/var-templ32.C
@@ -4,4 +4,4 @@ template<typename T>
 bool V1 = true;
 
 template<typename T>
-bool V1<int> = false; // { dg-error "primary template" }
+bool V1<int> = false; // { dg-error "primary template|not deducible" }
