dc062b4986328bfeec3ebd744a9c116e28828bf0
Jason Merrill <jason@redhat.com>
2015-08-05 13:56:14 -0400

    re PR c++/66260 ([C++14] Failure to compile variable template with recursively defined partial specialization)
    	PR c++/66260
    	PR c++/66596
    	PR c++/66649
    	PR c++/66923
    	* pt.c (lookup_template_variable): Use NULL_TREE for type.
    	(instantiate_template_1): Also set DECL_TI_ARGS based on
    	the immediate parent.
    	(tsubst_copy_and_build) [TEMPLATE_ID_EXPR]: Handle variable templates.
    	(finish_template_variable): Add complain parm.
    	* cp-tree.h: Adjust.
    
    From-SVN: r226642

--- gcc/testsuite/g++.dg/cpp1y/var-templ35.C
+++ gcc/testsuite/g++.dg/cpp1y/var-templ35.C
@@ -2,4 +2,4 @@
 // { dg-do compile { target c++14 } }
 
 template<typename T> int typeID{42};
-template<typename T> double typeID<double>{10.10}; // { dg-error "primary template|redeclaration" }
+template<typename T> double typeID<double>{10.10}; // { dg-error "primary template|redeclaration|not deducible" }
