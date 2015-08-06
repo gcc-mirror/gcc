c914ac45c7670c710f3ec17dbd5341e55ae2380f
Andreas Krebbel <krebbel@linux.vnet.ibm.com>
2015-08-06 10:21:41 +0000

    S/390: Clobber VRs in __builtin_tbegin.
    gcc/ChangeLog:
    	    * config/s390/s390.c (s390_expand_tbegin): Expand either
    	    tbegin_1_z13 or tbegin_1 depending on VX flag.
    	    * config/s390/s390.md ("tbegin_1_z13"): New expander.
    
    gcc/testsuite/ChangeLog:
    	    * gcc.target/s390/htm-builtins-z13-1.c: New test.
    
    From-SVN: r226672

---
+++ gcc/testsuite/gcc.target/s390/htm-builtins-z13-1.c
@@ -0,0 +1,34 @@
+/* Verify if VRs are saved and restored.  */
+
+/* { dg-do run } */
+/* { dg-require-effective-target vector } */
+/* { dg-options "-O3 -march=z13 -mzarch" } */
+
+typedef int __attribute__((vector_size(16))) v4si;
+
+v4si __attribute__((noinline))
+foo (v4si a)
+{
+  a += (v4si){ 1, 1, 1, 1 };
+  if (__builtin_tbegin (0) == 0)
+    {
+      a += (v4si){ 1, 1, 1, 1 };
+      __builtin_tabort (256);
+      __builtin_tend ();
+    }
+  else
+    a -= (v4si){ 1, 1, 1, 1 };
+
+  return a;
+}
+
+int
+main ()
+{
+  v4si a = (v4si){ 0, 0, 0, 0 };
+
+  a = foo (a);
+
+  if (a[0] != 0)
+    __builtin_abort ();
+}
