39c3145c6dcfa84a7f898490707e96a0d6eb22f4
Mikael Morin <mikael@gcc.gnu.org>
2015-08-07 09:41:31 +0000

    Fix latent elemental wrong code
    	PR fortran/66929
    gcc/fortran/
    	* trans-array.c (gfc_get_proc_ifc_for_expr): Use esym as procedure
    	symbol if available.
    gcc/testsuite/
    	PR fortran/66929
    	* gfortran.dg/generic_30.f90: New.
    	* gfortran.dg/generic_31.f90: New.
    
    From-SVN: r226717

---
+++ gcc/testsuite/gfortran.dg/generic_31.f90
@@ -0,0 +1,35 @@
+! { dg-do run }
+!
+! PR fortran/66929
+! Check that the specific FIRST symbol is used for the call to FOO,
+! so that the J argument is not assumed to be present
+
+module m
+  interface foo
+    module procedure first
+  end interface foo
+contains
+  elemental function bar(j) result(r)
+    integer, intent(in), optional :: j
+    integer :: r, s(2)
+    ! We used to have NULL dereference here, in case of a missing J argument
+    s = foo(j, [3, 7])
+    r = sum(s)
+  end function bar
+  elemental function first(i, j) result(r)
+    integer, intent(in), optional :: i
+    integer, intent(in) :: j
+    integer :: r
+    if (present(i)) then
+      r = i
+    else
+      r = -5
+    end if
+  end function first
+end module m
+program p
+  use m
+  integer :: i
+  i = bar()
+  if (i /= -10) call abort
+end program p
