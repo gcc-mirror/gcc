b5c93b09131b4781a1a36440704f89dd4c4ed1cc
kargl <kargl@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-08-10 21:45:01 +0000

    2015-08-10  Steven G. Kargl  <kargl@gcc.gnu.org>
    	PR libfortran/67140
    	* intrinsics/mvbits.c: Fix build for paltforms without c_int128_t.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@226769 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgfortran/intrinsics/mvbits.c
+++ libgfortran/intrinsics/mvbits.c
@@ -84,6 +84,7 @@ SUB_NAME (const TYPE *from, const int *frompos, const int *len, TYPE *to,
 #  undef TYPE
 #  undef UTYPE
 
+#if defined (HAVE_GFC_INTEGER_16)
 #  define TYPE GFC_INTEGER_16
 #  define UTYPE GFC_UINTEGER_16
 #  define SUB_NAME mvbits_i16
@@ -91,4 +92,5 @@ SUB_NAME (const TYPE *from, const int *frompos, const int *len, TYPE *to,
 #  undef SUB_NAME
 #  undef TYPE
 #  undef UTYPE
+#endif
 #endif
