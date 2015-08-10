0238e1e98acdae2b16fcda3d54c3e95a21c0750f
Steven G. Kargl <kargl@gcc.gnu.org>
2015-08-10 17:42:00 +0000

    [multiple changes]
    2015-08-10  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	PR libfortran/67140
    	* gfortran.map: Add _gfortran_mvbits_i16.
    	* intrinsics/mvbits.c: Generate mvbits for c_int128_t.
    
    2015-08-10  Francois-Xavier Coudert  <fxcoudert@gcc.gnu.org>
    
    	PR libfortran/67140
    	* gfortran.dg/pr67140.f90: New test.
    
    From-SVN: r226764

---
+++ gcc/testsuite/gfortran.dg/pr67140.f90
@@ -0,0 +1,12 @@
+! { dg-do run }
+!
+! Check that MVBITS is available for the largest integer kind (PR 67140)
+!
+program test
+  use iso_fortran_env
+  integer, parameter :: k = integer_kinds(size(integer_kinds))
+
+  integer(kind=k) :: i = 6
+  call mvbits(7_k,2,2,i,0)
+  if (i /= 5) call abort
+end
