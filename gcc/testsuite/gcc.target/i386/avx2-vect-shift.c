d2be285bf53e470cc9038ce1c59661428143b277
Uros Bizjak <uros@gcc.gnu.org>
2015-08-11 20:27:46 +0200

    * gcc.target/i386/avx2-vect-shift.c: Remove unneeded dg directives.
    From-SVN: r226787

--- gcc/testsuite/gcc.target/i386/avx2-vect-shift.c
+++ gcc/testsuite/gcc.target/i386/avx2-vect-shift.c
@@ -1,5 +1,4 @@
 /* { dg-do compile } */
-/* { dg-require-effective-target avx2 } */
 /* { dg-options "-mavx2 -O3 -fdump-tree-vect-details" } */
 
 #define N 32
@@ -13,4 +12,3 @@ void foo()
     a[i] = 1 << i;
 }
 /* { dg-final { scan-tree-dump "LOOP VECTORIZED" "vect" } } */
-/* { dg-final { cleanup-tree-dump "vect" } } */
