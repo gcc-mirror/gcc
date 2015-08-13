a65c36dc2b3a46ea1f9ce50a01247d5efd067f35
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-08-13 13:57:41 +0000

    * include/bits/alloc_traits.h: Add feature-test macro. 	* include/bits/allocator.h: Likewise. 	* include/bits/range_access.h: Likewise. 	* include/bits/stl_map.h: Likewise. 	* include/bits/unordered_map.h: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@226864 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/alloc_traits.h
+++ libstdc++-v3/include/bits/alloc_traits.h
@@ -36,6 +36,8 @@
 #include <bits/ptr_traits.h>
 #include <ext/numeric_traits.h>
 
+#define __cpp_lib_allocator_traits_is_always_equal 201411
+
 namespace std _GLIBCXX_VISIBILITY(default)
 {
 _GLIBCXX_BEGIN_NAMESPACE_VERSION
