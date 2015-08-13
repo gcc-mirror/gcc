0072c9ac909191c5709585e3882bbab8749c8b9c
Jonathan Wakely <jwakely@redhat.com>
2015-08-13 14:57:35 +0100

    2.cc: New.
    	* testsuite/30_threads/mutex/unlock/2.cc: New.
    	* testsuite/30_threads/recursive_mutex/unlock/2.cc: New.
    	* testsuite/30_threads/recursive_timed_mutex/unlock/2.cc: New.
    	* testsuite/30_threads/shared_mutex/unlock/1.cc: New.
    	* testsuite/30_threads/shared_timed_mutex/unlock/1.cc: New.
    	* testsuite/30_threads/timed_mutex/unlock/2.cc: New.
    
    From-SVN: r226863

---
+++ libstdc++-v3/testsuite/30_threads/shared_mutex/unlock/1.cc
@@ -0,0 +1,43 @@
+// { dg-do run { target *-*-freebsd* *-*-dragonfly* *-*-netbsd* *-*-linux* *-*-gnu* *-*-solaris* *-*-cygwin *-*-darwin* powerpc-ibm-aix* } }
+// { dg-options " -std=gnu++17 -pthread" { target *-*-freebsd* *-*-dragonfly* *-*-netbsd* *-*-linux* *-*-gnu* powerpc-ibm-aix* } }
+// { dg-options " -std=gnu++17 -pthreads" { target *-*-solaris* } }
+// { dg-options " -std=gnu++17 " { target *-*-cygwin *-*-darwin* } }
+// { dg-require-cstdint "" }
+// { dg-require-gthreads "" }
+
+// Copyright (C) 2015 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <shared_mutex>
+#include <thread>
+
+using mutex_type = std::shared_mutex;
+
+mutex_type m;
+
+void f()
+{
+  std::lock_guard<mutex_type> l(m);
+}
+
+int main()
+{
+  std::thread t1(f);
+  std::thread t2(f);
+  t1.join();
+  t2.join();
+}
