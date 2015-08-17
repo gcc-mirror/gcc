61fba55ca899493cdebb44b041cd8ac14510d04a
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-08-17 21:40:07 +0000

    /cp 2015-08-17  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/67216
    	* call.c (null_ptr_cst_p): In C++11 return 'false' for 'false'.
    
    /testsuite
    2015-08-17  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/67216
    	* g++.dg/cpp0x/nullptr34.C: New.
    	* g++.dg/warn/Wconversion2.C: Adjust.
    	* g++.dg/warn/Wnull-conversion-1.C: Likewise.
    	* g++.old-deja/g++.other/null3.C: Likewise.
    
    	* g++.dg/cpp0x/pr51313.C: Adjust.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@226956 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/warn/Wnull-conversion-1.C
+++ gcc/testsuite/g++.dg/warn/Wnull-conversion-1.C
@@ -6,10 +6,13 @@
 void func1(int* ptr);
 
 void func2() {
-  int* t = false;             // { dg-warning "converting 'false' to pointer" }
+  int* t = false;             // { dg-warning "converting 'false' to pointer" "" { target { ! c++11 } } }
+// { dg-error "cannot convert" "" { target c++11 } 9 }
   int* p;
-  p = false;                  // { dg-warning "converting 'false' to pointer" }
+  p = false;                  // { dg-warning "converting 'false' to pointer" "" { target { ! c++11 } } }
+// { dg-error "cannot convert" "" { target c++11 } 12 }
   int* r = sizeof(char) / 2;  // { dg-error "invalid conversion from" "" { target c++11 } }
-  func1(false);               // { dg-warning "converting 'false' to pointer" }
+  func1(false);               // { dg-warning "converting 'false' to pointer" "" { target { ! c++11 } } }
+// { dg-error "cannot convert" "" { target c++11 } 15 }
   int i = NULL;               // { dg-warning "converting to non-pointer" }
 }
--- gcc/testsuite/g++.old-deja/g++.other/null3.C
+++ gcc/testsuite/g++.old-deja/g++.other/null3.C
@@ -2,5 +2,6 @@
 
 void x()
 {
- int* p = 1==0;	// { dg-warning "converting 'false' to pointer" }
+ int* p = 1==0;	// { dg-warning "converting 'false' to pointer" "" { target { ! c++11 } } }
+// { dg-error "cannot convert" "" { target c++11 } 5 } 
 }
