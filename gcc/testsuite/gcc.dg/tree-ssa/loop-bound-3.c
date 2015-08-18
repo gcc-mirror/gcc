7b008bbc0c4050da71786bb51de743964807aa44
Bin Cheng <bin.cheng@arm.com>
2015-08-18 07:57:54 +0000

    tree-ssa-loop-niter.c (refine_value_range_using_guard): New.
    	* tree-ssa-loop-niter.c (refine_value_range_using_guard): New.
    	(determine_value_range): Call refine_value_range_using_guard for
    	each loop initial condition to improve value range.
    
    	gcc/testsuite/ChangeLog
    	* gcc.dg/tree-ssa/loop-bound-1.c: New test.
    	* gcc.dg/tree-ssa/loop-bound-3.c: New test.
    	* gcc.dg/tree-ssa/loop-bound-5.c: New test.
    
    From-SVN: r226966

---
+++ gcc/testsuite/gcc.dg/tree-ssa/loop-bound-3.c
@@ -0,0 +1,22 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-ivopts-details" } */
+
+int *a;
+
+int
+foo (unsigned char s, unsigned char l)
+{
+  unsigned char i;
+  int sum = 0;
+
+  for (i = s; i > l; i -= 1)
+    {
+      sum += a[i];
+    }
+
+  return sum;
+}
+
+/* Check loop niter bound information.  */
+/* { dg-final { scan-tree-dump "bounded by 254" "ivopts" } } */
+/* { dg-final { scan-tree-dump-not "bounded by 255" "ivopts" } } */
