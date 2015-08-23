1487cca00ca6f4e0fe60d72761737da162e96840
François-Xavier Coudert <fxcoudert@gcc.gnu.org>
2015-08-23 21:24:49 +0000

    re PR libfortran/62296 (EXECUTE_COMMAND_LINE not F2008 conforming)
    	PR libfortran/62296
    
    	* intrinsics/execute_command_line.c (EXEC_INVALIDCOMMAND): New
    	error code.
    	(cmdmsg_values): New error message.
    	(set_cmdstat): Rework runtime error.
    	(execute_command_line): Handle invalid command line error status.
    
    	* gfortran.dg/execute_command_line_2.f90: New test.
    
    From-SVN: r227105

---
+++ gcc/testsuite/gfortran.dg/execute_command_line_2.f90
@@ -0,0 +1,14 @@
+! { dg-do run }
+!
+! Check that EXECUTE_COMMAND_LINE handles invalid command lines appropriately
+!
+  integer :: s = 0, c = 0
+  character(len=255) :: msg = ""
+
+  ! This should fail, set CMDSTAT to nonzero value, and an error message
+  ! in CMDMSG.
+  call execute_command_line ("/nosuchfile", exitstat=s, cmdstat=c, cmdmsg=msg)
+  if (c == 0) call abort
+  if (len_trim(msg) == 0) call abort
+
+end
