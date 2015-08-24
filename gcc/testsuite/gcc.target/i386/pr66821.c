76eed9d920fedf44e7c3738acd5e434d3a00a972
H.J. Lu <hongjiu.lu@intel.com>
2015-08-24 15:31:02 +0000

    Add a testcase for PR target/66821
    	PR target/66821
    	* gcc.target/i386/pr66821.c: New test.
    
    From-SVN: r227133

---
+++ gcc/testsuite/gcc.target/i386/pr66821.c
@@ -0,0 +1,15 @@
+/* { dg-do compile { target ia32 } } */
+/* { dg-options "-O2 -fdump-tree-optimized -mtune=iamcu" } */
+
+void bar (void);
+
+void
+foo (int x)
+{
+  if (x != 2 && x != 3 && x != 10 && x != 11 && x != 17 && x != 18 && x != 23)
+    bar ();
+}
+
+/* Check if the tests have been folded into a bit test.  */
+/* { dg-final { scan-tree-dump "(8784908|0x0*860c0c)" "optimized" } } */
+/* { dg-final { scan-tree-dump "(<<|>>)" "optimized" } } */
