9fb5a14435f35f6671fecaff03474b4c146ca70d
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-08-24 18:43:02 +0000

    [gcc] 2015-08-24  Michael Meissner  <meissner@linux.vnet.ibm.com>
    	PR target/67211
    	* config/rs6000/rs6000-cpus.def (ISA_2_7_MASKS_SERVER): Set
    	-mefficient-unaligned-vsx on ISA 2.7.
    
    	* config/rs6000/rs6000.opt (-mefficient-unaligned-vsx): Convert
    	option to a masked option.
    
    	* config/rs6000/rs6000.c (rs6000_option_override_internal): Rework
    	logic for -mefficient-unaligned-vsx so that it is set via an arch
    	ISA option, instead of being set if -mtune=power8 is set. Move
    	-mefficient-unaligned-vsx and -mallow-movmisalign handling to be
    	near other default option handling.
    
    [gcc/testsuite]
    2015-08-24  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	PR target/67211
    	* g++.dg/pr67211.C: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@227144 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/pr67211.C
@@ -0,0 +1,50 @@
+/* { dg-do compile { target { powerpc*-*-* && lp64 } } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
+/* { dg-require-effective-target powerpc_p8vector_ok } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power7" } } */
+/* { dg-options "-mcpu=power7 -mtune=power8 -O3 -w" } */
+
+/* target/67211, compiler got a 'insn does not satisfy its constraints' error.  */
+
+template <typename _InputIterator, typename _ForwardIterator>
+void find_first_of(_InputIterator, _InputIterator, _ForwardIterator p3,
+                   _ForwardIterator p4) {
+  for (; p3 != p4; ++p3)
+    ;
+}
+
+template <typename, typename, typename> struct A {
+  int _S_buffer_size;
+  int *_M_cur;
+  int *_M_first;
+  int *_M_last;
+  int **_M_node;
+  void operator++() {
+    if (_M_cur == _M_last)
+      m_fn1(_M_node + 1);
+  }
+  void m_fn1(int **p1) {
+    _M_node = p1;
+    _M_first = *p1;
+    _M_last = _M_first + _S_buffer_size;
+  }
+};
+
+template <typename _Tp, typename _Ref, typename _Ptr>
+bool operator==(A<_Tp, _Ref, _Ptr>, A<_Tp, _Ref, _Ptr>);
+template <typename _Tp, typename _Ref, typename _Ptr>
+bool operator!=(A<_Tp, _Ref, _Ptr> p1, A<_Tp, _Ref, _Ptr> p2) {
+  return p1 == p2;
+}
+
+class B {
+public:
+  A<int, int, int> m_fn2();
+};
+struct {
+  B j;
+} a;
+void Linked() {
+  A<int, int, int> b, c, d;
+  find_first_of(d, c, b, a.j.m_fn2());
+}
