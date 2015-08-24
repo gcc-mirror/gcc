f7323638e41de5e3e10f8f80424b07095adf8831
fdumont <fdumont@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-08-24 20:21:57 +0000

    2015-08-24  François Dumont  <fdumont@gcc.gnu.org>
    	* include/debug/formatter.h (_Debug_msg_id::__msg_irreflexive_ordering):
    	New enum entry.
    	* include/debug/functions.h (_Irreflexive_checker): New.
    	(__is_irreflexive, __is_irreflexive_pred): New.
    	* include/debug/macros.h
    	(__glibcxx_check_irreflexive, __glibcxx_check_irreflexive_pred): New
    	macros.
    	(__glibcxx_check_irreflexive2, __glibcxx_check_irreflexive_pred2): New
    	macros limited to post-C++11 mode.
    	* include/debug/debug.h
    	(__glibcxx_requires_irreflexive, __glibcxx_requires_irreflexive_pred):
    	New macros, use latter.
    	(__glibcxx_requires_irreflexive2, __glibcxx_requires_irreflexive_pred2):
    	Likewise.
    	* include/bits/stl_algo.h
    	(partial_sort_copy): Add irreflexive debug check.
    	(partial_sort_copy): Likewise.
    	(lower_bound): Likewise.
    	(upper_bound): Likewise.
    	(equal_range): Likewise.
    	(binary_search): Likewise.
    	(inplace_merge): Likewise.
    	(includes): Likewise.
    	(next_permutation): Likewise.
    	(prev_permutation): Likewise.
    	(is_sorted_until): Likewise.
    	(minmax_element): Likewise.
    	(partial_sort): Likewise.
    	(nth_element): Likewise.
    	(sort): Likewise.
    	(merge): Likewise.
    	(stable_sort): Likewise.
    	(set_union): Likewise.
    	(set_intersection): Likewise.
    	(set_difference): Likewise.
    	(set_symmetric_difference): Likewise.
    	(min_element): Likewise.
    	(max_element): Likewise.
    	* include/bits/stl_algobase.h
    	(lower_bound): Likewise.
    	(lexicographical_compare): Likewise.
    	* include/bits/stl_heap.h
    	(push_heap): Likewise.
    	(pop_heap): Likewise.
    	(make_heap): Likewise.
    	(sort_heap): Likewise.
    	(is_heap_until): Likewise.
    	* testsuite/25_algorithms/lexicographical_compare/debug/
    	irreflexive_neg.cc: New.
    	* testsuite/25_algorithms/lower_bound/debug/irreflexive.cc: New.
    	* testsuite/25_algorithms/partial_sort_copy/debug/irreflexive_neg.cc:
    	New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@227147 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/debug/debug.h
+++ libstdc++-v3/include/debug/debug.h
@@ -77,41 +77,53 @@ namespace __gnu_debug
 # define __glibcxx_requires_string(_String)
 # define __glibcxx_requires_string_len(_String,_Len)
 # define __glibcxx_requires_subscript(_N)
+# define __glibcxx_requires_irreflexive(_First,_Last)
+# define __glibcxx_requires_irreflexive2(_First,_Last)
+# define __glibcxx_requires_irreflexive_pred(_First,_Last,_Pred)
+# define __glibcxx_requires_irreflexive_pred2(_First,_Last,_Pred)
 
 #else
 
 # include <debug/macros.h>
 
 # define __glibcxx_requires_cond(_Cond,_Msg) _GLIBCXX_DEBUG_VERIFY(_Cond,_Msg)
-# define __glibcxx_requires_valid_range(_First,_Last) \
-     __glibcxx_check_valid_range(_First,_Last)
-# define __glibcxx_requires_non_empty_range(_First,_Last) \
-     __glibcxx_check_non_empty_range(_First,_Last)
-# define __glibcxx_requires_sorted(_First,_Last) \
-     __glibcxx_check_sorted(_First,_Last)
-# define __glibcxx_requires_sorted_pred(_First,_Last,_Pred) \
-     __glibcxx_check_sorted_pred(_First,_Last,_Pred)
-# define __glibcxx_requires_sorted_set(_First1,_Last1,_First2) \
-     __glibcxx_check_sorted_set(_First1,_Last1,_First2)
+# define __glibcxx_requires_valid_range(_First,_Last)	\
+  __glibcxx_check_valid_range(_First,_Last)
+# define __glibcxx_requires_non_empty_range(_First,_Last)	\
+  __glibcxx_check_non_empty_range(_First,_Last)
+# define __glibcxx_requires_sorted(_First,_Last)	\
+  __glibcxx_check_sorted(_First,_Last)
+# define __glibcxx_requires_sorted_pred(_First,_Last,_Pred)	\
+  __glibcxx_check_sorted_pred(_First,_Last,_Pred)
+# define __glibcxx_requires_sorted_set(_First1,_Last1,_First2)	\
+  __glibcxx_check_sorted_set(_First1,_Last1,_First2)
 # define __glibcxx_requires_sorted_set_pred(_First1,_Last1,_First2,_Pred) \
-     __glibcxx_check_sorted_set_pred(_First1,_Last1,_First2,_Pred)
+  __glibcxx_check_sorted_set_pred(_First1,_Last1,_First2,_Pred)
 # define __glibcxx_requires_partitioned_lower(_First,_Last,_Value)	\
-     __glibcxx_check_partitioned_lower(_First,_Last,_Value)
+  __glibcxx_check_partitioned_lower(_First,_Last,_Value)
 # define __glibcxx_requires_partitioned_upper(_First,_Last,_Value)	\
-     __glibcxx_check_partitioned_upper(_First,_Last,_Value)
+  __glibcxx_check_partitioned_upper(_First,_Last,_Value)
 # define __glibcxx_requires_partitioned_lower_pred(_First,_Last,_Value,_Pred) \
-     __glibcxx_check_partitioned_lower_pred(_First,_Last,_Value,_Pred)
+  __glibcxx_check_partitioned_lower_pred(_First,_Last,_Value,_Pred)
 # define __glibcxx_requires_partitioned_upper_pred(_First,_Last,_Value,_Pred) \
-     __glibcxx_check_partitioned_upper_pred(_First,_Last,_Value,_Pred)
-# define __glibcxx_requires_heap(_First,_Last) \
-     __glibcxx_check_heap(_First,_Last)
-# define __glibcxx_requires_heap_pred(_First,_Last,_Pred) \
-     __glibcxx_check_heap_pred(_First,_Last,_Pred)
+  __glibcxx_check_partitioned_upper_pred(_First,_Last,_Value,_Pred)
+# define __glibcxx_requires_heap(_First,_Last)	\
+  __glibcxx_check_heap(_First,_Last)
+# define __glibcxx_requires_heap_pred(_First,_Last,_Pred)	\
+  __glibcxx_check_heap_pred(_First,_Last,_Pred)
 # define __glibcxx_requires_nonempty() __glibcxx_check_nonempty()
 # define __glibcxx_requires_string(_String) __glibcxx_check_string(_String)
 # define __glibcxx_requires_string_len(_String,_Len)	\
-     __glibcxx_check_string_len(_String,_Len)
+  __glibcxx_check_string_len(_String,_Len)
 # define __glibcxx_requires_subscript(_N) __glibcxx_check_subscript(_N)
+# define __glibcxx_requires_irreflexive(_First,_Last)	\
+  __glibcxx_check_irreflexive(_First,_Last)
+# define __glibcxx_requires_irreflexive2(_First,_Last)	\
+  __glibcxx_check_irreflexive2(_First,_Last)
+# define __glibcxx_requires_irreflexive_pred(_First,_Last,_Pred)	\
+  __glibcxx_check_irreflexive_pred(_First,_Last,_Pred)
+# define __glibcxx_requires_irreflexive_pred2(_First,_Last,_Pred)	\
+  __glibcxx_check_irreflexive_pred2(_First,_Last,_Pred)
 
 # include <debug/functions.h>
 
