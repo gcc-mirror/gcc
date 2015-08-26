8288a899fca9187cdbc75cfd2bd0dd0df2d7f391
jiwang <jiwang@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-08-26 13:16:19 +0000

    [AArch64][TLSIE][1/2] Rename test source file for reuse
    2015-08-26  Jiong Wang  <jiong.wang@arm.com>
    
    gcc/testsuite/
    	* gcc.target/aarch64/tlsle_1.x: Rename to tls_1.x
    	* gcc.target/aarch64/aarch64/tlsle12_1.c: Update source file name.
    	* gcc.target/aarch64/aarch64/tlsle24_1.c: Likewise.
    	* gcc.target/aarch64/aarch64/tlsle32_1.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@227216 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/aarch64/tlsle32_1.c
+++ gcc/testsuite/gcc.target/aarch64/tlsle32_1.c
@@ -3,7 +3,7 @@
 /* { dg-require-effective-target aarch64_tlsle32 } */
 /* { dg-options "-O2 -fpic -ftls-model=local-exec -mtls-size=32 --save-temps" } */
 
-#include "tlsle_1.x"
+#include "tls_1.x"
 
 /* { dg-final { scan-assembler-times "#:tprel_g1" 2 } } */
 /* { dg-final { scan-assembler-times "#:tprel_g0_nc" 2 } } */
