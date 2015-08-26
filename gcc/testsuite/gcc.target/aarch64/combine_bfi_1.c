7ebdc8814144cabb6224f5a50e1bddb357f15fe8
renlin <renlin@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-08-26 17:37:42 +0000

    [PATCH][AARCH64]Add backend aarch64_bfi pattern.
    2015-08-26  Renlin Li  <renlin.li@arm.com>
    
    gcc/
    	* config/aarch64/aarch64.md (aarch64_bfi): New pattern.
    
    gcc/testsuite/
    	* gcc.target/aarch64/combine_bfi_1.c: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@227226 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/aarch64/combine_bfi_1.c
@@ -0,0 +1,34 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-rtl-combine" } */
+
+int
+f1 (int x, int y)
+{
+  return (x & ~0x0ffff00) | ((y << 8) & 0x0ffff00);
+}
+
+int
+f2 (int x, int y)
+{
+  return (x & ~0x0ff000) | ((y & 0x0ff) << 12);
+}
+
+int
+f3 (int x, int y)
+{
+  return (x & ~0xffff) | (y & 0xffff);
+}
+
+int
+f4 (int x, int y)
+{
+  return (x & ~0xff) | (y & 0xff);
+}
+
+long
+f5 (long x, long y)
+{
+  return (x & ~0xffffffffull) | (y & 0xffffffff);
+}
+
+/* { dg-final { scan-rtl-dump-times "\\*aarch64_bfi" 5 "combine" } } */
