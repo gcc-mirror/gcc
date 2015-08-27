dfa37515566f34fac10204218d44f44413657080
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2015-08-27 14:26:08 +0000

    Update copyrights
    	libvtv:
    	Update copyrights.
    
    	contrib:
    	* update-copyright.py (GCCCmdLine): Add libvtv.
    
    From-SVN: r227262

--- libvtv/testsuite/libvtv.cc/virtfunc-test.cc
+++ libvtv/testsuite/libvtv.cc/virtfunc-test.cc
@@ -2,8 +2,7 @@
 
 /* This test script is part of GDB, the GNU debugger.
 
-   Copyright 1993, 1994, 1997, 1998, 1999, 2003, 2004,
-   Free Software Foundation, Inc.
+   Copyright (C) 1993-2015 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
