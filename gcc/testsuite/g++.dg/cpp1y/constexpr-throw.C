748c426a1b106499ced440f548743efdde564d4d
trippels <trippels@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-08-29 18:51:26 +0000

    Fix c++/67371 (issues with throw in constexpr)
    As PR67371 shows gcc currently rejects all throw statements in
    constant-expressions, even when they are never executed.
    
    	PR c++/67371
    	* constexpr.c (potential_constant_expression_1): Remove IF_STMT
    	case. Move label to COND_EXPR case. Remove checking of
    	SWITCH_STMT_BODY.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@227323 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp1y/constexpr-throw.C
@@ -0,0 +1,34 @@
+// { dg-do compile { target c++14 } }
+
+constexpr void f1() {
+  if (false)
+    throw;
+}
+
+constexpr void f2() {
+  if (true)
+    throw;
+} // { dg-error "not a constant-expression" }
+
+constexpr void f3() {
+  if (false)
+    ;
+  else
+    throw;
+}// { dg-error "not a constant-expression" }
+
+constexpr void f4() {
+  throw;
+}// { dg-error "not a constant-expression" }
+
+constexpr int fun(int n) {
+  switch (n) {
+  case 0:
+    return 1;
+  default:
+    throw; // { dg-error "not a constant-expression" }
+  }
+}
+
+static_assert(fun(0), "");
+static_assert(fun(1), ""); // { dg-error "non-constant" }
