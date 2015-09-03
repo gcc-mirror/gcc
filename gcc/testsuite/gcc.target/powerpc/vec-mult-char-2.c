010f20ee1f31f4b1944153e8d6c96cd6d4c76a98
Bill Schmidt <wschmidt@vnet.linux.ibm.com>
2015-09-03 18:08:42 +0000

    altivec.md (altivec_vperm_v8hiv16qi): New define_insn.
    [gcc]
    
    2015-09-03  Bill Schmidt  <wschmidt@vnet.linux.ibm.com>
    
    	* config/rs6000/altivec.md (altivec_vperm_v8hiv16qi): New
    	define_insn.
    	(mulv16qi3): New define_expand.
    
    [gcc/testsuite]
    
    2015-09-03  Bill Schmidt  <wschmidt@vnet.linux.ibm.com>
    
    	* gcc.target/powerpc/vec-mult-char-1.c: New test.
    	* gcc.target/powerpc/vec-mult-char-2.c: New test.
    	* lib/target-supports.exp (check_effective_target_vect_char_mult):
    	Return true for PowerPC targets that implement Altivec.
    
    From-SVN: r227464

---
+++ gcc/testsuite/gcc.target/powerpc/vec-mult-char-2.c
@@ -0,0 +1,21 @@
+/* { dg-do compile { target { powerpc*-*-* && vmx_hw } } } */
+/* { dg-require-effective-target powerpc_altivec_ok } */
+/* { dg-options "-maltivec" } */
+
+#include <altivec.h>
+
+vector unsigned char vmului(vector unsigned char v,
+			    vector unsigned char i)
+{
+	return v * i;
+}
+
+vector signed char vmulsi(vector signed char v,
+			  vector signed char i)
+{
+	return v * i;
+}
+
+/* { dg-final { scan-assembler-times "vmulesb" 2 } } */
+/* { dg-final { scan-assembler-times "vmulosb" 2 } } */
+/* { dg-final { scan-assembler-times "vperm" 2 } } */
