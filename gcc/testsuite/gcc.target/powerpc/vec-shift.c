5179d2a2137d80c429ecd01dd0aeab415ffd299e
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2015-09-03 13:52:17 +0000

    optabs.c (expand_binop): Don't create a broadcast vector with a source element wider than the inner mode.
    [gcc]
    
    2015-09-03  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* optabs.c (expand_binop): Don't create a broadcast vector with a
    	source element wider than the inner mode.
    
    [gcc/testsuite]
    
    2015-09-03  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* gcc.target/powerpc/vec-shift.c: New test.
    
    From-SVN: r227444

---
+++ gcc/testsuite/gcc.target/powerpc/vec-shift.c
@@ -0,0 +1,20 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-require-effective-target powerpc_altivec_ok } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power7" } } */
+/* { dg-options "-mcpu=power7 -O2" } */
+
+/* This used to ICE.  During gimplification, "i" is widened to an unsigned
+   int.  We used to fail at expand time as we tried to cram an SImode item
+   into a QImode memory slot.  This has been fixed to properly truncate the
+   shift amount when splatting it into a vector.  */
+
+typedef unsigned char v16ui __attribute__((vector_size(16)));
+
+v16ui vslb(v16ui v, unsigned char i)
+{
+	return v << i;
+}
+
+/* { dg-final { scan-assembler "vspltb" } } */
+/* { dg-final { scan-assembler "vslb" } } */
