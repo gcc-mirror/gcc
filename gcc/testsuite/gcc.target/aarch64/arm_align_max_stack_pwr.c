dbd31f0c56bfa6fe34819c61090fb8af2e7cb5da
renlin <renlin@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-09-03 17:16:33 +0000

    [PATCH][AARCH64]Make arm_align_max_stack_pwr.c and arm_align_max_pwr.c compile testcase, instead of execution.
    gcc/testsuite/
    
    2015-09-03  Renlin Li  <renlin.li@arm.com>
    
    	* gcc.target/aarch64/arm_align_max_pwr.c: Make it a compile test case,
    	  check the assembly.
    	* gcc.target/aarch64/arm_align_max_stack_pwr.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@227459 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/aarch64/arm_align_max_stack_pwr.c
+++ gcc/testsuite/gcc.target/aarch64/arm_align_max_stack_pwr.c
@@ -1,15 +1,20 @@
-/* { dg-do run } */
-
-#include <stdio.h>
-#include <assert.h>
+/* { dg-do compile } */
+/* { dg-options "-O1" } */
 
 #define align (1ul << __ARM_ALIGN_MAX_STACK_PWR)
+extern void foo (int *x);
+extern int bar (int x);
 
 int
-main ()
+dummy ()
 {
   int x __attribute__ ((aligned (align)));
+  int result;
 
-  assert ((((unsigned long)&x) & (align - 1)) == 0);
-  return 0;
+  foo (&x);
+  result = bar (x);
+
+  return result;
 }
+
+/* { dg-final { scan-assembler "and\tx\[0-9\]+, x\[0-9\]+, -65536" } } */
