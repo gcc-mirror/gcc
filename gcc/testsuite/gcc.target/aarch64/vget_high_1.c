7171dc86e4bb022c1f55a5a0af921ec3a6c9c17c
Alan Lawrence <alan.lawrence@arm.com>
2015-09-08 19:13:29 +0000

    [AArch64] vreinterpret(q?), vget_(low|high), vld1(q?)_dup
    gcc/:
    
    	* config/aarch64/arm_neon.h (vreinterpret_p8_f16, vreinterpret_p16_f16,
    	vreinterpret_f16_f64, vreinterpret_f16_s8, vreinterpret_f16_s16,
    	vreinterpret_f16_s32, vreinterpret_f16_s64, vreinterpret_f16_f32,
    	vreinterpret_f16_u8, vreinterpret_f16_u16, vreinterpret_f16_u32,
    	vreinterpret_f16_u64, vreinterpret_f16_p8, vreinterpret_f16_p16,
    	vreinterpretq_f16_f64, vreinterpretq_f16_s8, vreinterpretq_f16_s16,
    	vreinterpretq_f16_s32, vreinterpretq_f16_s64, vreinterpretq_f16_f32,
    	vreinterpretq_f16_u8, vreinterpretq_f16_u16, vreinterpretq_f16_u32,
    	vreinterpretq_f16_u64, vreinterpretq_f16_p8, vreinterpretq_f16_p16,
    	vreinterpret_f32_f16, vreinterpret_f64_f16, vreinterpret_s64_f16,
    	vreinterpret_u64_f16, vreinterpretq_u64_f16, vreinterpret_s8_f16,
    	vreinterpret_s16_f16, vreinterpret_s32_f16, vreinterpret_u8_f16,
    	vreinterpret_u16_f16, vreinterpret_u32_f16, vreinterpretq_p8_f16,
    	vreinterpretq_p16_f16, vreinterpretq_f32_f16, vreinterpretq_f64_f16,
    	vreinterpretq_s64_f16, vreinterpretq_s8_f16, vreinterpretq_s16_f16,
    	vreinterpretq_s32_f16, vreinterpretq_u8_f16, vreinterpretq_u16_f16,
    	vreinterpretq_u32_f16, vget_low_f16, vget_high_f16, vld1_dup_f16,
    	vld1q_dup_f16): New.
    
    gcc/testsuite/:
    
    	* gcc.target/aarch64/vget_high_1.c: Add float16x8->float16x4 case.
    	* gcc.target/aarch64/vget_low_1.c: Likewise.
    
    From-SVN: r227546

--- gcc/testsuite/gcc.target/aarch64/vget_high_1.c
+++ gcc/testsuite/gcc.target/aarch64/vget_high_1.c
@@ -14,6 +14,7 @@ VARIANT (int8_t, 8, int8x8_t, int8x16_t, s8)		\
 VARIANT (int16_t, 4, int16x4_t, int16x8_t, s16)		\
 VARIANT (int32_t, 2, int32x2_t, int32x4_t, s32)		\
 VARIANT (int64_t, 1, int64x1_t, int64x2_t, s64)		\
+VARIANT (float16_t, 4, float16x4_t, float16x8_t, f16)	\
 VARIANT (float32_t, 2, float32x2_t, float32x4_t, f32)	\
 VARIANT (float64_t, 1, float64x1_t, float64x2_t, f64)
 
@@ -51,6 +52,8 @@ main (int argc, char **argv)
   int16_t int16_t_data[8] = { -17, 19, 3, -999, 44048, 505, 9999, 1000};
   int32_t int32_t_data[4] = { 123456789, -987654321, -135792468, 975318642 };
   int64_t int64_t_data[2] = {0xfedcba9876543210LL, 0xdeadbabecafebeefLL };
+  float16_t float16_t_data[8] = { 1.25, 4.5, 7.875, 2.3125, 5.675, 8.875,
+      3.6875, 6.75};
   float32_t float32_t_data[4] = { 3.14159, 2.718, 1.414, 100.0 };
   float64_t float64_t_data[2] = { 1.01001000100001, 12345.6789 };
 
