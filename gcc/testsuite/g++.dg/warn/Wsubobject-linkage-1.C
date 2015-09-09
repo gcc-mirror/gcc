273aa49e2fe00de54cb1dbe67f1a6155fb47439b
Paolo Carlini <paolo.carlini@oracle.com>
2015-09-09 16:48:13 +0000

    re PR c++/53184 (Unnecessary anonymous namespace warnings)
    2015-09-09  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/53184
    	* doc/invoke.texi ([Wsubobject-linkage]): Document.
    
    /c-family
    2015-09-09  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/53184
    	* c.opt ([Wsubobject-linkage]): Add.
    
    /cp
    2015-09-09  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/53184
    	* decl2.c (constrain_class_visibility): Use Wsubobject-linkage.
    
    /testsuite
    2015-09-09  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/53184
    	* g++.dg/warn/Wsubobject-linkage-1.C: New.
    	* g++.dg/warn/Wsubobject-linkage-2.C: Likewise.
    	* g++.dg/warn/Wsubobject-linkage-3.C: Likewise.
    	* g++.dg/warn/Wsubobject-linkage-4.C: Likewise.
    
    From-SVN: r227593

---
+++ gcc/testsuite/g++.dg/warn/Wsubobject-linkage-1.C
@@ -0,0 +1,9 @@
+// PR c++/53184
+
+typedef volatile struct { } Foo;
+
+#line 6 "foo.C"
+struct Bar { Foo foo; };   // { dg-warning "no linkage" }
+// { dg-bogus "anonymous namespace" "" { target *-*-* } 6 }
+struct Bar2 : Foo { };     // { dg-warning "no linkage" }
+// { dg-bogus "anonymous namespace" "" { target *-*-* } 8 }
