8b661145105bf5d8d7be3f2e57b51c8b8d2c27b7
Segher Boessenkool <segher@gcc.gnu.org>
2015-09-10 19:25:35 +0200

    shrink-wrap: Header hygiene
    2015-09-10  Segher Boessenkool  <segher@kernel.crashing.org>
    
    	* shrink-wrap.c (requires_stack_frame_p): Make static.
    	(prepare_shrink_wrap): Likewise.
    	(dup_block_and_redirect): Likewise.
    	* shrink-wrap.h: Remove declarations of those functions.
    
    From-SVN: r227652

--- gcc/shrink-wrap.h
+++ gcc/shrink-wrap.h
@@ -23,11 +23,6 @@ along with GCC; see the file COPYING3.  If not see
 #include "function.h"
 
 /* In shrink-wrap.c.  */
-extern bool requires_stack_frame_p (rtx_insn *, HARD_REG_SET, HARD_REG_SET);
-extern void prepare_shrink_wrap (basic_block entry_block);
-extern void dup_block_and_redirect (basic_block bb, basic_block copy_bb,
-				    rtx_insn *before,
-				    bitmap_head *need_prologue);
 extern void try_shrink_wrapping (edge *entry_edge, edge orig_entry_edge,
 				 bitmap_head *bb_flags, rtx_insn *prologue_seq);
 extern edge get_unconverted_simple_return (edge, bitmap_head,
