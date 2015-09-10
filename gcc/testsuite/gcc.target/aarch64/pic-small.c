4d3f1d2125842947e16b0eeb0fa60911a8f9d639
jiwang <jiwang@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-09-10 09:15:14 +0000

    [AArch64] Skip tiny and large code model on gcc.target/aarch64/pic-small.c
    gcc/testsuite/
      * gcc.target/aarch64/pic-small.c (dg-skip-if): Skip tiny and large code model.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@227624 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/aarch64/pic-small.c
+++ gcc/testsuite/gcc.target/aarch64/pic-small.c
@@ -1,6 +1,7 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target aarch64_small_fpic } */
 /* { dg-options "-O2 -fpic -fno-inline --save-temps" } */
+/* { dg-skip-if "-fpic for AArch64 small code model" { aarch64*-*-* }  { "-mcmodel=tiny" "-mcmodel=large" } { "" } } */
 
 void abort ();
 int global_a;
