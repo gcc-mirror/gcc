5caff414f153f50aad00f1bc0d793776676e492b
Jonathan Wakely <jwakely@redhat.com>
2015-09-11 12:02:14 +0100

    Implement N4258 noexcept for std::basic_string.
    	PR libstdc++/58265
    	* doc/xml/manual/intro.xml: Document LWG 2063 and 2064 resolutions.
    	* doc/html/manual/bugs.html: Regenerate.
    	* include/bits/basic_string.h (basic_string): Implement N4258. Add
    	correct exception-specifications and propagate allocators correctly.
    	* include/bits/basic_string.tcc (basic_string::swap): Propagate
    	allocators correctly.
    	* include/debug/string (__gnu_debug::basic_string): Add correct
    	exceptions-specifications and allcoator-extended constructors.
    	* testsuite/21_strings/basic_string/allocator/char/copy.cc: New.
    	* testsuite/21_strings/basic_string/allocator/char/copy_assign.cc:
    	New.
    	* testsuite/21_strings/basic_string/allocator/char/minimal.cc: New.
    	* testsuite/21_strings/basic_string/allocator/char/move.cc: New.
    	* testsuite/21_strings/basic_string/allocator/char/move_assign.cc:
    	New.
    	* testsuite/21_strings/basic_string/allocator/char/noexcept.cc: New.
    	* testsuite/21_strings/basic_string/allocator/char/swap.cc: New.
    	* testsuite/21_strings/basic_string/allocator/wchar_t/copy.cc: New.
    	* testsuite/21_strings/basic_string/allocator/wchar_t/copy_assign.cc:
    	New.
    	* testsuite/21_strings/basic_string/allocator/wchar_t/minimal.cc: New.
    	* testsuite/21_strings/basic_string/allocator/wchar_t/move.cc: New.
    	* testsuite/21_strings/basic_string/allocator/wchar_t/move_assign.cc:
    	New.
    	* testsuite/21_strings/basic_string/allocator/wchar_t/noexcept.cc: New.
    	* testsuite/21_strings/basic_string/allocator/wchar_t/swap.cc: New.
    	* testsuite/util/testsuite_allocator.h (tracker_allocator): Define
    	defaulted assignment operators.
    
    From-SVN: r227681

---
+++ libstdc++-v3/testsuite/21_strings/basic_string/allocator/char/copy_assign.cc
@@ -0,0 +1,114 @@
+// Copyright (C) 2015 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+ 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+ 
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++11" }
+
+#include <string>
+#include <testsuite_hooks.h>
+#include <testsuite_allocator.h>
+ 
+#if _GLIBCXX_USE_CXX11_ABI
+using C = char;
+const C c = 'a';
+using traits = std::char_traits<C>;
+
+using __gnu_test::propagating_allocator;
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+  typedef propagating_allocator<C, false> alloc_type;
+  typedef std::basic_string<C, traits, alloc_type> test_type;
+  test_type v1(alloc_type(1));
+
+  v1.assign(1, c);
+  test_type v2(alloc_type(2));
+  v2.assign(1, c);
+  v2 = v1;
+  VERIFY(1 == v1.get_allocator().get_personality());
+  VERIFY(2 == v2.get_allocator().get_personality());
+
+  v1.assign(1, c);
+  test_type v3(alloc_type(3));
+  v3.assign(100, c);
+  v3 = v1;
+  VERIFY(1 == v1.get_allocator().get_personality());
+  VERIFY(3 == v3.get_allocator().get_personality());
+
+  v1.assign(100, c);
+  test_type v4(alloc_type(4));
+  v4.assign(1, c);
+  v4 = v1;
+  VERIFY(1 == v1.get_allocator().get_personality());
+  VERIFY(4 == v4.get_allocator().get_personality());
+
+  v1.assign(100, c);
+  test_type v5(alloc_type(5));
+  v5.assign(100, c);
+  v5 = v1;
+  VERIFY(1 == v1.get_allocator().get_personality());
+  VERIFY(5 == v5.get_allocator().get_personality());
+}
+
+void test02()
+{
+  bool test __attribute__((unused)) = true;
+  typedef propagating_allocator<C, true> alloc_type;
+  typedef std::basic_string<C, traits, alloc_type> test_type;
+  test_type v1(alloc_type(1));
+
+  v1.assign(1, c);
+  test_type v2(alloc_type(2));
+  v2.assign(1, c);
+  v2 = v1;
+  VERIFY(1 == v1.get_allocator().get_personality());
+  VERIFY(1 == v2.get_allocator().get_personality());
+
+  v1.assign(1, c);
+  test_type v3(alloc_type(3));
+  v3.assign(100, c);
+  v3 = v1;
+  VERIFY(1 == v1.get_allocator().get_personality());
+  VERIFY(1 == v3.get_allocator().get_personality());
+
+  v1.assign(100, c);
+  test_type v4(alloc_type(4));
+  v4.assign(1, c);
+  v4 = v1;
+  VERIFY(1 == v1.get_allocator().get_personality());
+  VERIFY(1 == v4.get_allocator().get_personality());
+
+  v1.assign(100, c);
+  test_type v5(alloc_type(5));
+  v5.assign(100, c);
+  v5 = v1;
+  VERIFY(1 == v1.get_allocator().get_personality());
+  VERIFY(1 == v5.get_allocator().get_personality());
+}
+
+int main()
+{
+  test01();
+  test02();
+  return 0;
+}
+#else
+int main()
+{
+  // COW strings don't support C++11 allocators
+}
+#endif
