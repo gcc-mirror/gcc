65440c482af0345046416641750df7897c33dc49
Jason Merrill <jason@redhat.com>
2015-09-14 15:54:27 -0400

    re PR c++/44282 (fastcall is not mangled at all)
    	PR c++/44282
    
    	* mangle.c (write_CV_qualifiers_for_type): Also warn about regparm
    	mangling with lower -fabi-version.
    
    From-SVN: r227761

---
+++ gcc/testsuite/g++.dg/abi/mangle-regparm1a.C
@@ -0,0 +1,21 @@
+// { dg-do run { target { { i?86-*-* x86_64-*-* } && ia32 } } }
+// { dg-options "-fabi-version=8 -Wabi -save-temps" }
+// { dg-final { scan-assembler "_Z18IndirectExternCallIPFviiEiEvT_T0_S3_" } }
+
+template <typename F, typename T>
+void IndirectExternCall(F f, T t1, T t2) { // { dg-warning "mangled name" }
+  typedef F (*WrapF)(F);
+  f (t1, t2);
+}
+
+__attribute__((regparm(3), stdcall))
+void regparm_func (int i, int j)
+{
+  if (i != 24 || j != 42)
+    __builtin_abort();
+}
+
+int main()
+{
+  IndirectExternCall (regparm_func, 24, 42);
+}
