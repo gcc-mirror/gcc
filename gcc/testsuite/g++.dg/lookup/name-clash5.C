15eefe57380a7c5fe749bf8241828c6ec39d0e21
Ville Voutilainen <ville.voutilainen@gmail.com>
2015-09-19 07:44:01 +0300

    Implement nested namespace definitions.
    /cp
    2015-09-18  Ville Voutilainen  <ville.voutilainen@gmail.com>
    
    	Implement nested namespace definitions.
    	* parser.c (cp_parser_namespace_definition): Grok nested namespace
    	definitions.
    
    /testsuite
    2015-09-18  Ville Voutilainen  <ville.voutilainen@gmail.com>
    
    	Implement nested namespace definitions.
    	* g++.dg/cpp1z/nested-namespace-def1.C: New.
    	* g++.dg/cpp1z/nested-namespace-def2.C: Likewise.
    	* g++.dg/cpp1z/nested-namespace-def3.C: Likewise.
    	* g++.dg/lookup/name-clash5.C: Adjust.
    	* g++.dg/lookup/name-clash6.C: Likewise.
    
    From-SVN: r227932

--- gcc/testsuite/g++.dg/lookup/name-clash5.C
+++ gcc/testsuite/g++.dg/lookup/name-clash5.C
@@ -6,8 +6,8 @@
 // "[Note: a namespace name or a class template name must be unique in its
 // declarative region (7.3.2, clause 14). ]"
 
-namespace N
-{ // { dg-message "previous declaration" }
+namespace N // { dg-message "previous declaration" }
+{
 }
 
 class N; // { dg-error "redeclared" }
