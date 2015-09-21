b0fc34b4df6c9a675057006e96517623bc0d4a9e
olegendo <olegendo@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-09-21 00:17:22 +0000

    gcc/ 	PR target/67126 	* config/sh/sh.md (*reg_lsb_t): Emit bld insn on SH2A. 	(*mov_t_msb_neg): Rewrite negc pattern.
    gcc/testsuite/
    	PR target/67126
    	* gcc.target/sh/pr51244-12.c: Adjust testcase.
    	* gcc.target/sh/pr54236-2.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@227957 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/sh/pr54236-2.c
+++ gcc/testsuite/gcc.target/sh/pr54236-2.c
@@ -5,7 +5,6 @@
 /* { dg-options "-O1" } */
 /* { dg-skip-if "" { "sh*-*-*" } { "-m5*"} { "" } } */
 /* { dg-final { scan-assembler-times "addc" 36 } } */
-/* { dg-final { scan-assembler-times "shlr" 22 } } */
 /* { dg-final { scan-assembler-times "shll" 14 } } */
 /* { dg-final { scan-assembler-times "add\tr" 12 } } */
 /* { dg-final { scan-assembler-not "movt" } } */
@@ -17,115 +16,118 @@
 
 /* { dg-final { scan-assembler-times "bclr\t#0" 1 { target { sh2a } } } } */
 
+/* { dg-final { scan-assembler-times "shlr" 22 { target { ! sh2a } } } } */
+/* { dg-final { scan-assembler-not "shlr" { target { sh2a } } } } */
+
 int
 test_000 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return a + (b & 1);
 }
 
 int
 test_001 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return a + b + (c & 1);
 }
 
 int
 test_002 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x add, 1x addc
+  // 1x shlr/bld, 1x add, 1x addc
   return a + b + c + (d & 1);
 }
 
 int
 test_003 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return (b & 1) + a;
 }
 
 int
 test_004 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return a + (c & 1) + b;
 }
 
 int
 test_005 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x add, 1x addc
+  // 1x shlr/bld, 1x add, 1x addc
   return a + b + (d & 1) + c;
 }
 
 int
 test_006 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return (c & 1) + a + b;
 }
 
 int
 test_007 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x add, 1x addc
+  // 1x shlr/bld, 1x add, 1x addc
   return a + (d & 1) + b + c;
 }
 
 int
 test_008 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x add, 1x addc
+  // 1x shlr/bld, 1x add, 1x addc
   return (d & 1) + a + b + c;
 }
 
 int
 test_009 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return a + b + (b & 1);
 }
 
 int
 test_010 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return a + (b & 1) + b;
 }
 
 int
 test_011 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return (b & 1) + a + b;
 }
 
 int
 test_012 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x add, 1x addc
+  // 1x shlr/bld, 1x add, 1x addc
   return a + b + d + (b & 1);
 }
 
 int
 test_013 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x add, 1x addc
+  // 1x shlr/bld, 1x add, 1x addc
   return a + d + (b & 1) + b;
 }
 
 int
 test_014 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x add, 1x addc
+  // 1x shlr/bld, 1x add, 1x addc
   return a + (b & 1) + d + b;
 }
 
 int
 test_015 (int a, int c, int b, int d)
 {
-  // 1x shlr, 1x add, 1x addc
+  // 1x shlr/bld, 1x add, 1x addc
   return (b & 1) + a + d + b;
 }
 
@@ -140,42 +142,42 @@ test_016 (int a, int b, int c, int d)
 int
 test_017 (int a, int b, int c, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return a + a + (a & 1);
 }
 
 int
 test_018 (int a, int b, int c, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return a + (a & 1) + a;
 }
 
 int
 test_019 (int a, int b, int c, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return (a & 1) + a + a;
 }
 
 int
 test_020 (int a, int b, int c, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return b + b + (a & 1);
 }
 
 int
 test_021 (int a, int b, int c, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return b + (a & 1) + b;
 }
 
 int
 test_022 (int a, int b, int c, int d)
 {
-  // 1x shlr, 1x addc
+  // 1x shlr/bld, 1x addc
   return (a & 1) + b + b;
 }
 
