df67b98cfa2f5943ddc469380abf4f3821f0f6af
tschwinge <tschwinge@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-09-23 14:46:55 +0000

    Refactor omp_reduction_init: omp_reduction_init_op
    	gcc/
    	* omp-low.h (omp_reduction_init_op): Declare.
    	* omp-low.c (omp_reduction_init_op): New, broken out of ...
    	(omp_reduction_init): ... here.  Call it.
    	* tree-parloops.c (initialize_reductions): Use
    	omp_reduction_init_op.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@228052 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-parloops.c
+++ gcc/tree-parloops.c
@@ -565,8 +565,8 @@ reduc_stmt_res (gimple *stmt)
 int
 initialize_reductions (reduction_info **slot, struct loop *loop)
 {
-  tree init, c;
-  tree bvar, type, arg;
+  tree init;
+  tree type, arg;
   edge e;
 
   struct reduction_info *const reduc = *slot;
@@ -577,16 +577,10 @@ initialize_reductions (reduction_info **slot, struct loop *loop)
   /* In the phi node at the header, replace the argument coming
      from the preheader with the reduction initialization value.  */
 
-  /* Create a new variable to initialize the reduction.  */
+  /* Initialize the reduction.  */
   type = TREE_TYPE (PHI_RESULT (reduc->reduc_phi));
-  bvar = create_tmp_var (type, "reduction");
-
-  c = build_omp_clause (gimple_location (reduc->reduc_stmt),
-			OMP_CLAUSE_REDUCTION);
-  OMP_CLAUSE_REDUCTION_CODE (c) = reduc->reduction_code;
-  OMP_CLAUSE_DECL (c) = SSA_NAME_VAR (reduc_stmt_res (reduc->reduc_stmt));
-
-  init = omp_reduction_init (c, TREE_TYPE (bvar));
+  init = omp_reduction_init_op (gimple_location (reduc->reduc_stmt),
+				reduc->reduction_code, type);
   reduc->init = init;
 
   /* Replace the argument representing the initialization value
