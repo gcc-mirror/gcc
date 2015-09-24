ba24ad380cf6ee626e1a523b5a3215072f3d056a
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-09-24 08:51:39 +0000

    Use CRTs provided by Solaris
    	gcc:
    	* configure.ac (gcc_cv_solaris_crts): New test.
    	* configure. Regenerate.
    	* config.in: Regenerate.
    	* config/sol2.h (STARTFILE_SPEC): Simplify, provide
    	HAVE_SOLARIS_CRTS variant.
    
    	libgcc:
    	* configure.ac (libgcc_cv_solaris_crts): New test.
    	* configure: Regenerate.
    	* config.in: Regenerate.
    	* config/sol2/crtp.c, config/sol2/crtpg.c: New files.
    	* config/gmon-sol2.c: Rename to ...
    	* config/sol2/gmon.c: ... this.
    	Include auto-target.h.
    	(internal_mcount): Wrap setup handling in !HAVE_SOLARIS_CRTS.
    	* config/t-sol2: Rename to ...
    	* config/sol2/t-sol2: ... this.
    	(gmon.o): Reflect renaming.
    	(crtp.o, crtpg.o): New rules.
    	* config.host (*-*-solaris2*): Reflect renaming.
    	Use system CRTs if present.
    	Remove default CRT case.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@228077 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libgcc/config/sol2/crtp.c
@@ -0,0 +1,33 @@
+/* Dummy _mcount when mixing profiling and non-profiling objects.
+   Copyright (C) 2015 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+Under Section 7 of GPL version 3, you are granted additional
+permissions described in the GCC Runtime Library Exception, version
+3.1, as published by the Free Software Foundation.
+
+You should have received a copy of the GNU General Public License and
+a copy of the GCC Runtime Library Exception along with this program;
+see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+<http://www.gnu.org/licenses/>.  */
+
+void _mcount (void);
+
+/* A dummy profiling support routine for non-profiling executables,
+   in case we link in some objects that have been compiled for profiling.  */
+
+void __attribute__((weak))
+_mcount (void)
+{
+}
