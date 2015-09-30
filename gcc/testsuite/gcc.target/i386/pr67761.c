96f127485924b881553ba4d66b5138d67ea11be8
ienkovich <ienkovich@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-09-30 13:08:01 +0000

    gcc/
    	* config/i386/i386.c (scalar_chain::analyze_register_chain): Ignore
    	debug insns.
    	(scalar_chain::convert_reg): Likewise.
    
    gcc/testsuite/
    
    	* gcc.target/i386/pr67761.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@228292 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr67761.c
@@ -0,0 +1,13 @@
+/* PR target/pr67761 */
+/* { dg-do compile { target { ia32 } } } */
+/* { dg-options "-O2 -march=slm -g" } */
+/* { dg-final { scan-assembler "paddq" } } */
+
+void
+test (long long *values, long long val, long long delta)
+{
+  unsigned i;
+
+  for (i = 0; i < 128; i++, val += delta)
+    values[i] = val;
+}
