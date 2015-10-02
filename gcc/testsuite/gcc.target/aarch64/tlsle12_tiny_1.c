320e616ce4636a8685ba8444a5b30e14d016e100
jiwang <jiwang@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-02 14:47:34 +0000

    [AArch64] Add more TLS local executable testcases
    2015-10-02  Jiong Wang  <jiong.wang@arm.com>
    
    	* gcc.target/aarch64/tlsle12_tiny_1.c: New testcase for tiny model.
    	* gcc.target/aarch64/tlsle24_tiny_1.c: Likewise.
    	* gcc.target/aarch64/tlsle_sizeadj_tiny_1.c: TLS size truncation test
    	for tiny model.
    	* gcc.target/aarch64/tlsle_sizeadj_small_1.c: TLS size truncation test
    	for small model.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@228395 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/aarch64/tlsle12_tiny_1.c
@@ -0,0 +1,8 @@
+/* { dg-do run } */
+/* { dg-require-effective-target tls_native } */
+/* { dg-options "-O2 -fpic -ftls-model=local-exec -mtls-size=12 -mcmodel=tiny --save-temps" } */
+/* { dg-skip-if "TLS 12bit size for tiny" { aarch64*-*-* }  { "-mcmodel=small" "-mcmodel=large" } { "" } } */
+
+#include "tls_1.x"
+
+/* { dg-final { scan-assembler-times "#:tprel_lo12" 2 } } */
