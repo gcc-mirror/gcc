4c9156537c50c4de823aa9d4d1c8f611abf01782
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-05 13:37:15 +0000

    Add a build_real_truncate helper function
    ...which simplifies the match.pd patterns I'm about to add.
    
    Bootstrapped & regression-tested on x86_64-linux-gnu.
    
    gcc/
    	* real.h (build_real_truncate): Declare.
    	* tree.c (build_real_truncate): New function.
    	(strip_float_extensions): Use it.
    	* builtins.c (fold_builtin_cabs, fold_builtin_sqrt, fold_builtin_cbrt)
    	(fold_builtin_hypot, fold_builtin_pow): Likewise.
    	* match.pd: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@228483 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/real.h
+++ gcc/real.h
@@ -440,6 +440,9 @@ bool real_can_shorten_arithmetic (machine_mode, machine_mode);
 /* In tree.c: wrap up a REAL_VALUE_TYPE in a tree node.  */
 extern tree build_real (tree, REAL_VALUE_TYPE);
 
+/* Likewise, but first truncate the value to the type.  */
+extern tree build_real_truncate (tree, REAL_VALUE_TYPE);
+
 /* Calculate R as X raised to the integer exponent N in mode MODE.  */
 extern bool real_powi (REAL_VALUE_TYPE *, machine_mode,
 		       const REAL_VALUE_TYPE *, HOST_WIDE_INT);
