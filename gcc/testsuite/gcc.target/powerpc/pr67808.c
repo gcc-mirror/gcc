2519f1f00ae538bc1021839af0b95144fefde92a
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-06 17:20:49 +0000

    [gcc] 2015-10-05  Michael Meissner  <meissner@linux.vnet.ibm.com> 	    Peter Bergner  <bergner@vnet.ibm.com>
    	PR target/67808
    	* config/rs6000/rs6000.md (extenddftf2): In the expander, only
    	allow registers, but provide insns for the combiner to create for
    	loads from memory. Separate VSX code from non-VSX code. For
    	non-VSX code, combine extenddftf2_fprs into extenddftf2 and rename
    	externaldftf2_internal to externaldftf2_fprs. Reorder constraints
    	so that registers come before memory operations. Drop support from
    	converting DFmode to TFmode, if the DFmode value is in a GPR
    	register.
    	(extenddftf2_fprs): Likewise.
    	(extenddftf2_internal): Likewise.
    	(extenddftf2_vsx): Likewise.
    	(extendsftf2): In the expander, only allow registers, but provide
    	insns for the combiner to create for stores and loads.
    
    [gcc/testsuite]
    2015-10-05  Michael Meissner  <meissner@linux.vnet.ibm.com>
    	    Peter Bergner <bergner@vnet.ibm.com>
    
    	PR target/67808
    	* gcc.target/powerpc/pr67808.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@228538 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/pr67808.c
@@ -0,0 +1,46 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
+/* { dg-require-effective-target powerpc_vsx_ok } */
+/* { dg-require-effective-target powerpc_vsx_ok } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power7" } } */
+/* { dg-options "-O1 -mvsx -mlra -mcpu=power7" } */
+
+/* PR 67808: LRA ICEs on simple double to long double conversion test case */
+
+void
+dfoo (long double *ldb1, double *db1)
+{
+  *ldb1 = *db1;
+}
+
+long double
+dfoo2 (double *db1)
+{
+  return *db1;
+}
+
+long double
+dfoo3 (double x)
+{
+  return x;
+}
+
+void
+ffoo (long double *ldb1, float *db1)
+{
+  *ldb1 = *db1;
+}
+
+long double
+ffoo2 (float *db1)
+{
+  return *db1;
+}
+
+long double
+ffoo3 (float x)
+{
+  return x;
+}
+
+/* { dg-final { scan-assembler "xxlxor" } } */
