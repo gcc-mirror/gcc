170f473b525d0af69dc4577186762a3519b952a4
Martin Jambor <mjambor@suse.cz>
2015-10-08 18:30:06 +0200

    Fix Solaris bootstrap
    2015-10-08  Martin Jambor  <mjambor@suse.cz>
    
    	* ipa-cp.c (meet_with_1): Make the argument of abs signed.  Remove
    	unnecessary MIN.
    
    From-SVN: r228607

--- gcc/ipa-cp.c
+++ gcc/ipa-cp.c
@@ -852,8 +852,8 @@ ipcp_alignment_lattice::meet_with_1 (unsigned new_align, unsigned new_misalign)
     }
   if (misalign != (new_misalign % align))
     {
-      int diff = abs (misalign - (new_misalign % align));
-      align = MIN (align, (unsigned) diff & -diff);
+      int diff = abs ((int) misalign - (int) (new_misalign % align));
+      align = (unsigned) diff & -diff;
       if (align)
 	misalign = misalign % align;
       else
