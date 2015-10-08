a361141865247626a73c0f2257a95bc7d4f274c9
ienkovich <ienkovich@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-08 13:14:09 +0000

    gcc/
    	* tree-ssa-loop-unswitch.c: Include "gimple-iterator.h" and
    	"cfghooks.h", add prototypes for introduced new functions.
    	(tree_ssa_unswitch_loops): Use from innermost loop iterator, move all
    	checks on ability of loop unswitching to tree_unswitch_single_loop;
    	invoke tree_unswitch_single_loop or tree_unswitch_outer_loop depending
    	on innermost loop check.
    	(tree_unswitch_single_loop): Add all required checks on ability of
    	loop unswitching under zero recursive level guard.
    	(tree_unswitch_outer_loop): New function.
    	(find_loop_guard): Likewise.
    	(empty_bb_without_guard_p): Likewise.
    	(used_outside_loop_p): Likewise.
    	(get_vop_from_header): Likewise.
    	(hoist_guard): Likewise.
    	(check_exit_phi): Likewise.
    
    gcc/testsuite/
    
    	* gcc.dg/loop-unswitch-2.c: New test.
    	* gcc.dg/loop-unswitch-3.c: Likewise.
    	* gcc.dg/loop-unswitch-4.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@228599 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/loop-unswitch-2.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -funswitch-loops -fdump-tree-unswitch-details" } */
+
+void foo (float **a, float **b, float *c, int n, int m, int l)
+{
+  int i,j,k;
+  float s;
+  for (i=0; i<l; i++)
+    for (j=0; j<n; j++)
+      for (k=0; k<m; k++)
+	c[i] += a[i][k] * b[k][j];
+}
+
+/* { dg-final { scan-tree-dump-times "guard hoisted" 2 "unswitch" } } */
+
---
+++ gcc/testsuite/gcc.dg/loop-unswitch-3.c
@@ -0,0 +1,26 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -funswitch-loops -fdump-tree-unswitch-details" } */
+
+#include <stdlib.h>
+#define N 32
+float *foo(int ustride, int size, float *src)
+{
+   float *buffer, *p;
+   int i, k;
+
+   if (!src)
+    return NULL;
+
+   buffer = (float *) malloc(N * size * sizeof(float));
+
+   if(buffer)
+      for(i=0, p=buffer; i<N; i++, src+=ustride)
+	for(k=0; k<size; k++)
+	  *p++ = src[k];
+
+   return buffer;
+}
+
+/* { dg-final { scan-tree-dump-times "guard hoisted" 1 "unswitch" } } */
+
+
