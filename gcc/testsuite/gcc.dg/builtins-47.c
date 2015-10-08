8b4af95f5318007e76569679c8ca2b72cfc4687d
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-08 16:54:37 +0000

    Move sqrt and cbrt simplifications to match.pd
    This patch moves the sqrt and cbrt simplification rules to match.pd.
    builtins.c now only does the constant folding.
    
    Bootstrapped & regression-tested on x86_64-linux-gnu.
    
    gcc/
    	* builtins.c (fold_builtin_sqrt, fold_builtin_cbrt): Delete.
    	(fold_builtin_1): Update accordingly.  Handle constant arguments here.
    	* match.pd: Add rules previously handled by fold_builtin_sqrt
    	and fold_builtin_cbrt.
    
    gcc/testsuite/
    	* gcc.dg/builtins-47.c: Test the optimized dump instead.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@228616 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/builtins-47.c
+++ gcc/testsuite/gcc.dg/builtins-47.c
@@ -1,5 +1,5 @@
 /* { dg-do run } */
-/* { dg-options "-O -ffast-math -fdump-tree-gimple" } */
+/* { dg-options "-O -ffast-math -fdump-tree-optimized" } */
 
 extern double sqrt (double);
 extern double pow (double, double);
@@ -15,5 +15,5 @@ int main ()
   return 0;
 }
 
-/* { dg-final { scan-tree-dump-times "sqrt" 0 "gimple" } } */
-/* { dg-final { scan-tree-dump-times "pow" 0 "gimple" } } */
+/* { dg-final { scan-tree-dump-times "sqrt" 0 "optimized" } } */
+/* { dg-final { scan-tree-dump-times "pow" 0 "optimized" } } */
