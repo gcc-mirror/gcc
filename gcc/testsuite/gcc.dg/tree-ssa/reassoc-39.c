5b03450c57ee2d9c050f8f67b3166e5c6b457e41
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-14 14:08:51 +0000

    * gcc.dg/tree-ssa/reassoc-39.c: Use -g.  Adjust dg-final. 	(f6): New. 	(f7): New. 	(f8): New. 	(f9): New. 	(f10): New. 	(f11): New. 	(f12): New. 	(f13): New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@228812 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/reassoc-39.c
+++ gcc/testsuite/gcc.dg/tree-ssa/reassoc-39.c
@@ -1,6 +1,9 @@
 /* PR tree-optimization/67815 */
 /* { dg-do compile } */
-/* { dg-options "-Ofast -fdump-tree-reassoc1-details" } */
+/* { dg-options "-Ofast -g -fdump-tree-reassoc1-details" } */
+
+extern float barf (float, float);
+extern double bar (double, double);
 
 float
 f0 (float x)
@@ -38,4 +41,67 @@ f5 (double x, double y, double z)
   return (x * (-1.0/12)) * z * __builtin_copysign (2.0, y);
 }
 
-/* { dg-final { scan-tree-dump-times "Optimizing copysign" 6 "reassoc1"} }*/
+float
+f6 (float x, float y)
+{
+  return 7.5f * y * __builtin_copysignf (2.0f, x);
+}
+
+float
+f7 (float x, float y)
+{
+  return -7.5f * y * __builtin_copysignf (2.0f, x);
+}
+
+float
+f8 (float x)
+{
+  float tmp1 = 7.5f;
+  float tmp2 = __builtin_copysignf (2.0f, x);
+  return tmp1 * tmp2;
+}
+
+double
+f9 (double x)
+{
+  double tmp1 = 7.5;
+  double tmp2 = __builtin_copysign (2.0, x);
+  return tmp1 * tmp2;
+}
+
+float
+f10 (float x)
+{
+  float tmp1 = 7.5f;
+  float tmp2 = __builtin_copysignf (2.0f, x);
+  float tmp3 = tmp2 * 24.0f;
+  return tmp1 * tmp2;
+}
+
+double
+f11 (double x)
+{
+  double tmp1 = 7.5;
+  double tmp2 = __builtin_copysign (2.0, x);
+  double tmp3 = tmp2 * 24.0;
+  return tmp1 * tmp2;
+}
+
+float
+f12 (float x)
+{
+  float tmp1 = 7.5f;
+  float tmp2 = __builtin_copysignf (2.0f, x);
+  /* Can't reassoc here.  */
+  return barf (tmp1 * tmp2, tmp2);
+}
+
+double
+f13 (double x)
+{
+  double tmp1 = 7.5;
+  double tmp2 = __builtin_copysign (2.0, x);
+  /* Can't reassoc here.  */
+  return bar (tmp1 * tmp2, tmp2);
+}
+/* { dg-final { scan-tree-dump-times "Optimizing copysign" 12 "reassoc1"} }*/
