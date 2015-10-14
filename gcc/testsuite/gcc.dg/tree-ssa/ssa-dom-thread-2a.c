bb1125df5370b6e29ddf9cb85b8c41a828796eee
Jeff Law <law@redhat.com>
2015-10-14 11:12:40 -0600

    [PATCH] Split ssa-dom-thread-2.c into separate files/tests
    	* gcc.dg/tree-ssa/ssa-dom-thread-2.c: Deleted.  The six functions
    	contained within have their own file/test now.
    	* gcc.dg/tree-ssa/ssa-dom-thread-2a.c: New test extracted from
    	ssa-dom-thread-2.c.  Tighten expected output slightly and comment
    	expectations a bit more.
    	* gcc.dg/tree-ssa/ssa-dom-thread-2b.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-dom-thread-2c.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-dom-thread-2d.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-dom-thread-2e.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-dom-thread-2f.c: Likewise.
    
    From-SVN: r228821

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-dom-thread-2a.c
@@ -0,0 +1,21 @@
+/* { dg-do compile } */ 
+/* { dg-options "-O2 -fdump-tree-vrp1-stats -fdump-tree-dom1-stats" } */
+
+void bla();
+
+/* In the following case, we should be able to thread edge through
+   the loop header.  */
+
+void thread_entry_through_header (void)
+{
+  int i;
+
+  for (i = 0; i < 170; i++)
+    bla ();
+}
+
+/* There's a single jump thread that should be handled by the VRP
+   jump threading pass.  */
+/* { dg-final { scan-tree-dump-times "Jumps threaded: 1" 1 "vrp1"} } */
+/* { dg-final { scan-tree-dump-times "Jumps threaded: 2" 0 "vrp1"} } */
+/* { dg-final { scan-tree-dump-not "Jumps threaded" "dom1"} } */
---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-dom-thread-2e.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */ 
+/* { dg-options "-O2 -fdump-tree-vrp1-stats -fdump-tree-dom1-stats" } */
+
+void foo();
+void bla();
+void bar();
+
+void dont_thread_3 (int nfirst)
+{
+  int i = 0;
+  int first = 0;
+
+  do
+    {
+      if (first)
+	foo ();
+      else
+	bar ();
+
+      first = nfirst;
+      bla ();
+    } while (i++ < 100);
+}
+
+/* Threading through the loop header is not safe here.  Peeling off
+   the first iteration then unswitching the loop would be safe.  */
+/* { dg-final { scan-tree-dump-not "Jumps threaded" "vrp1"} } */
+/* { dg-final { scan-tree-dump-not "Jumps threaded" "dom1"} } */
