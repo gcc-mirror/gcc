f66a9a76137456a12b3720add7b9b2c3bbcfff3f
Christian Bruel <christian.bruel@st.com>
2015-10-16 15:37:14 +0200

    re PR target/67745 ([ARM] wrong alignments when __attribute__ ((optimize,target,align) is used)
    2015-10-16  Christian Bruel  <christian.bruel@st.com>
    
    	PR target/67745
    	* config/arm/arm.h (FUNCTION_BOUNDARY): Move optimize_size condition to:
    	* config/arm/arm.c (arm_option_override_internal): Call
    	arm_override_options_after_change_1.
    	(arm_override_options_after_change): New function.
    	(arm_override_options_after_change_1): Likewise.
    	(TARGET_OVERRIDE_OPTIONS_AFTER_CHANGE): Define hook.
    
    From-SVN: r228904

---
+++ gcc/testsuite/gcc.target/arm/attr-align1.c
@@ -0,0 +1,27 @@
+/* PR target/67745
+   Verify alignment when both attribute optimize and target are used.  */
+/* { dg-do compile } */
+/* { dg-skip-if "" { ! { arm_thumb1_ok || arm_thumb2_ok } } } */
+
+void
+__attribute__ ((target ("arm")))
+bar()
+{
+}
+
+void
+__attribute__ ((target ("thumb")))
+__attribute__ ((optimize ("Os")))
+foo()
+{
+}
+
+void
+__attribute__ ((target ("thumb")))
+__attribute__ ((optimize ("O2")))
+rab()
+{
+}
+
+/* { dg-final { scan-assembler-times ".align\[ \t]2" 2 } } */
+/* { dg-final { scan-assembler ".align\[ \t]1" } } */
