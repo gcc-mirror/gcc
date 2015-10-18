2fe7f26c18c36933430add48d1139030c4a2f8d4
Iain Sandoe <iain@codesourcery.com>
2015-10-18 09:22:33 +0000

    fix Darwin bootstrap
    gcc/
    	* config/darwin-driver.h: Adjust includes to add diagnostic-core.
    
    From-SVN: r228939

--- gcc/config/darwin-driver.c
+++ gcc/config/darwin-driver.c
@@ -23,6 +23,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "coretypes.h"
 #include "tm.h"
 #include "opts.h"
+#include "diagnostic-core.h"
 
 #ifndef CROSS_DIRECTORY_STRUCTURE
 #include <sys/sysctl.h>
