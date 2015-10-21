64c7c74a82826ac5b68a5849d6f1e9135ce80a24
ienkovich <ienkovich@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-21 15:57:00 +0000

    gcc/
    	* omp-low.c (simd_clone_create): Set in_other_partition
    	for created clones.
    
    gcc/testsuite/
    
    	* gcc.dg/lto/simd-function_0.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@229127 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/omp-low.c
+++ gcc/omp-low.c
@@ -16318,6 +16318,8 @@ simd_clone_create (struct cgraph_node *old_node)
       DECL_STATIC_CONSTRUCTOR (new_decl) = 0;
       DECL_STATIC_DESTRUCTOR (new_decl) = 0;
       new_node = old_node->create_version_clone (new_decl, vNULL, NULL);
+      if (old_node->in_other_partition)
+	new_node->in_other_partition = 1;
       symtab->call_cgraph_insertion_hooks (new_node);
     }
   if (new_node == NULL)
