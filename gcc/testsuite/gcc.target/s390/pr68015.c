417eb4fd69d4088cb2b289447359f07409757d05
krebbel <krebbel@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-22 08:24:01 +0000

    S/390: PR68015 Fix ICE in s390_emit_compare
    gcc/ChangeLog:
    
    2015-10-22  Andreas Krebbel  <krebbel@linux.vnet.ibm.com>
    
    	PR target/68015
    	* config/s390/s390.md (mov<mode>cc): Emit compare only if we don't
    	already have a comparison result.
    
    gcc/testsuite/ChangeLog:
    
    2015-10-22  Andreas Krebbel  <krebbel@linux.vnet.ibm.com>
    
    	PR target/68015
    	* gcc.target/s390/pr68015.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@229163 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/s390/pr68015.c
@@ -0,0 +1,24 @@
+/* { dg-compile } */
+/* { dg-options "-O2 -march=z196" } */
+
+extern long useme (long, ...);
+
+void
+foo (void)
+{
+  long secs = useme (41);
+  long utc_secs = useme (42);
+  long h, m;
+
+  utc_secs = useme (42);
+  h = secs / 3600;
+  m = secs / 60;
+  if (utc_secs >= 86400)
+    {
+      m = 59;
+      h--;
+      if (h < 0)
+	h = 23;
+    }
+  useme (h, m);
+}
