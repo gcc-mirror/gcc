fb254810a90d7c24d36a77d1058d538c7e363a66
Uros Bizjak <uros@gcc.gnu.org>
2015-10-22 08:52:00 +0200

    backport: re PR target/68018 (ICE: in ix86_compute_frame_layout, at config/i386/i386.c:11308 with -mstackrealign)
    	Backport from mainline
    	2015-10-21  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/68018
    	* config/i386/i386.c (ix86_compute_frame_layout): Realign the stack
    	for 64-bit MS_ABI targets also when default incoming stack boundary
    	is overriden.
    
    testsuite/ChangeLog:
    
    	Backport from mainline
    	2015-10-21  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/68018
    	* gcc.target/i386/pr68018.c: New test.
    	Backport from mainline
    	2015-10-21  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/68018
    	* config/i386/i386.c (ix86_compute_frame_layout): Realign the stack
    	for 64-bit MS_ABI targets also when default incoming stack boundary
    	is overriden.
    
    testsuite/ChangeLog:
    
    	Backport from mainline
    	2015-10-21  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/68018
    	* gcc.target/i386/pr68018.c: New test.
    
    From-SVN: r229162

---
+++ gcc/testsuite/gcc.target/i386/pr68018.c
@@ -0,0 +1,10 @@
+/* { dg-do compile { target { *-*-linux* && { ! ia32 } } } } */
+/* { dg-options "-O -mabi=ms -mstackrealign" } */
+
+typedef float V __attribute__ ((vector_size (16)));
+
+int fn1 (V * x)
+{
+  V a = *x;
+  return a[0];
+}
