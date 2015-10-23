9f468f077255e4d99b2ba9cb6063fbb352530b15
Max Ostapenko <m.ostapenko@partner.samsung.com>
2015-10-23 10:22:37 +0300

    Fix ASan output pattern tests on Darwin.
    gcc/testsuite/
    
    	PR sanitizer/68042
    	* c-c++-common/asan/memcmp-1.c: Adjust test to pass on Darwin.
    	* c-c++-common/asan/sanity-check-pure-c-1.c: Likewise.
    
    From-SVN: r229212

--- gcc/testsuite/c-c++-common/asan/sanity-check-pure-c-1.c
+++ gcc/testsuite/c-c++-common/asan/sanity-check-pure-c-1.c
@@ -10,7 +10,7 @@ int main() {
 }
 
 /* { dg-output "heap-use-after-free.*(\n|\r\n|\r)" } */
-/* { dg-output "    #0 \[^\n\r]*(in _*(interceptor_|)free|\[(\])\[^\n\r]*(\n|\r\n|\r)" } */
+/* { dg-output "    #0 \[^\n\r]*(in _*(interceptor_|wrap_)free|\[(\])\[^\n\r]*(\n|\r\n|\r)" } */
 /* { dg-output "    #1 \[^\n\r]*(in _*main (\[^\n\r]*sanity-check-pure-c-1.c:8|\[^\n\r]*:0)|\[(\]).*(\n|\r\n|\r)" } */
-/* { dg-output "    #0 \[^\n\r]*(in _*(interceptor_|)malloc|\[(\])\[^\n\r]*(\n|\r\n|\r)" } */
+/* { dg-output "    #0 \[^\n\r]*(in _*(interceptor_|wrap_)malloc|\[(\])\[^\n\r]*(\n|\r\n|\r)" } */
 /* { dg-output "    #1 \[^\n\r]*(in _*main (\[^\n\r]*sanity-check-pure-c-1.c:7|\[^\n\r]*:0)|\[(\])\[^\n\r]*(\n|\r\n|\r)" } */
