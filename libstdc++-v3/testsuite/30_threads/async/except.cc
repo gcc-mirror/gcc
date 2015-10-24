bd20ab52c6641016756077843e2bcb9f15b3a434
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-24 21:36:50 +0000

    Return deferred future if thread cannot be run
    	* include/std/future (async): Use deferred function on exception.
    	* testsuite/30_threads/async/except.cc: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@229289 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/30_threads/async/except.cc
@@ -0,0 +1,70 @@
+// { dg-do run { target *-*-freebsd* *-*-dragonfly* *-*-netbsd* *-*-linux* *-*-gnu* *-*-solaris* *-*-cygwin *-*-rtems* *-*-darwin* powerpc-ibm-aix* } }
+// { dg-options " -std=gnu++11 -pthread" { target *-*-freebsd* *-*-dragonfly* *-*-netbsd* *-*-linux* *-*-gnu* powerpc-ibm-aix* } }
+// { dg-options " -std=gnu++11 -pthreads" { target *-*-solaris* } }
+// { dg-options " -std=gnu++11 " { target *-*-cygwin *-*-rtems* *-*-darwin* } }
+// { dg-require-cstdint "" }
+// { dg-require-gthreads "" }
+// { dg-require-atomic-builtins "" }
+
+// Copyright (C) 2010-2015 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+
+#include <future>
+#include <testsuite_hooks.h>
+
+struct Chucky
+{
+  Chucky() : copied(false) { }
+
+  Chucky(const Chucky& other) : copied(true)
+  {
+    if (other.copied)
+      return;
+    other.copied = true;
+    using namespace std;
+    // Throw on first DECAY_COPY to simulate inability to start a new thread.
+    throw system_error(make_error_code(errc::resource_unavailable_try_again));
+  }
+
+  void operator()() const { }
+
+  mutable bool copied;
+};
+
+void test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  using namespace std;
+
+  future<void> f = async(Chucky{});
+  VERIFY( f.wait_for(chrono::seconds(100)) == future_status::deferred );
+
+  bool caught = false;
+  try {
+    f = async(launch::async, Chucky{});
+  } catch (const system_error&) {
+    caught = true;
+  }
+  VERIFY( caught );
+}
+
+int main()
+{
+  test01();
+}
