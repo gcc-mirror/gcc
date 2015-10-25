fa788aea5387b066df50efa36810d7600063e7f1
John David Anglin <danglin@gcc.gnu.org>
2015-10-25 14:58:28 +0000

    Wno-frame-address.C: Skip on hppa*-*-*.
    	* g++.dg/Wno-frame-address.C: Skip on hppa*-*-*.
    
    From-SVN: r229298

--- gcc/testsuite/g++.dg/Wno-frame-address.C
+++ gcc/testsuite/g++.dg/Wno-frame-address.C
@@ -1,5 +1,5 @@
 // { dg-do compile }
-// { dg-skip-if "Cannot access arbitrary stack frames." { arm*-*-* } }
+// { dg-skip-if "Cannot access arbitrary stack frames." { arm*-*-* hppa*-*-* } }
 // { dg-options "-Werror" }
 
 // Verify that -Wframe-address is not enabled by default by enabling
