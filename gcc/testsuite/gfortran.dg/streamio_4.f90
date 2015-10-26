013a83cc0252d9cc8cce994000d7a44d8e40555b
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2015-10-26 10:22:15 +0000

    Fix race on temp file in gfortran streamio_*.f90 tests
    	* gfortran.dg/streamio_4.f90, gfortran.dg/streamio_5.f90,
    	* gfortran.dg/streamio_6.f90, gfortran.dg/streamio_7.f90,
    	* gfortran.dg/streamio_10.f90, gfortran.dg/streamio_12.f90: Add
    	suffix to the temporary filename to make it unique per testcase.
    
    From-SVN: r229315

--- gcc/testsuite/gfortran.dg/streamio_4.f90
+++ gcc/testsuite/gfortran.dg/streamio_4.f90
@@ -9,7 +9,7 @@ program streamtest
   integer        :: i,j,k
   integer, parameter :: lines = 5231
    
-  open(10, file="teststream", access="stream", form="formatted")
+  open(10, file="teststream_streamio_4", access="stream", form="formatted")
   
   do i=1,lines
     do j=0,9
@@ -19,7 +19,7 @@ program streamtest
   
   close(10)
   
-  open(10, file="teststream", access="stream",&
+  open(10, file="teststream_streamio_4", access="stream",&
   &form="formatted", position="append")
   do i=1,lines
     do j=0,9
