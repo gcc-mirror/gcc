09382f4e948e567fd47a518eeb5484d848898753
tschwinge <tschwinge@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-27 16:54:52 +0000

    [PR fortran/63865] OpenACC cache directive: match Fortran support with C/C++
    	gcc/fortran/
    	PR fortran/63865
    	* openmp.c (resolve_oacc_cache): Remove function.
    	(gfc_match_oacc_cache): Enable array sections.
    	(resolve_omp_clauses, gfc_resolve_oacc_directive): Change
    	accordingly.
    	* trans-openmp.c (gfc_trans_omp_clauses): Likewise.
    	gcc/testsuite/
    	PR fortran/63865
    	* gfortran.dg/goacc/coarray.f95: Expect the OpenACC cache
    	directive to work.
    	* gfortran.dg/goacc/loop-1.f95: Likewise.
    	* gfortran.dg/goacc/cache-1.f95: Likewise, and extend testing.
    	* gfortran.dg/goacc/cray.f95: Likewise.
    	* gfortran.dg/goacc/parameter.f95: Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@229448 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/goacc/cache-1.f95
+++ gcc/testsuite/gfortran.dg/goacc/cache-1.f95
@@ -3,10 +3,15 @@
 
 program test
   implicit none
-  integer :: i, d(10)
+  integer :: i, d(10), e(5,13)
 
   do concurrent (i=1:5)
     !$acc cache (d)
+    !$acc cache (d(1:3))
+    !$acc cache (d(i:i+2))
+
+    !$acc cache (e)
+    !$acc cache (e(1:3,2:4))
+    !$acc cache (e(i:i+2,i+1:i+3))
   enddo
 end
-! { dg-prune-output "unimplemented" }
