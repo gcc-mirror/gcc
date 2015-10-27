369635a259ab00ff46db8755a895545d86a56468
alalaw01 <alalaw01@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-27 12:36:55 +0000

    tree-scalar-evolution.c: Handle LSHIFT by constant
    gcc/:
    
    	tree-scalar-evolution.c (interpret_rhs_expr): Handle some LSHIFT_EXPRs
    	as equivalent MULT_EXPRs.
    
    gcc/testsuite/:
    
    	gcc.dg/vect/vect-strided-shift.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@229437 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/vect-strided-shift-1.c
@@ -0,0 +1,33 @@
+/* PR tree-optimization/65963.  */
+#include "tree-vect.h"
+
+#define N 512
+
+int in[2*N], out[N];
+
+__attribute__ ((noinline)) void
+loop (void)
+{
+  for (int i = 0; i < N; i++)
+    out[i] = in[i << 1] + 7;
+}
+
+int
+main (int argc, char **argv)
+{
+  check_vect ();
+  for (int i = 0; i < 2*N; i++)
+    {
+      in[i] = i;
+      __asm__ volatile ("" : : : "memory");
+    }
+  loop ();
+  __asm__ volatile ("" : : : "memory");
+  for (int i = 0; i < N; i++)
+    {
+      if (out[i] != i*2 + 7)
+	abort ();
+    }
+  return 0;
+}
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops in function" 1 "vect" { target { vect_strided2 } } } } */
