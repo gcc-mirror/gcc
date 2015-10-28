209f026e8413edd71da17dec1c28c26e9fc215ea
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-28 15:28:04 +0000

    2015-10-28  Richard Biener  <rguenther@suse.de>
    	* gimple-match-head.c (gimple_simplify): Allow VECTOR_CSTs
    	in simplifying VEC_COND_EXPR conditions.
    
    	* gcc.dg/tree-ssa/operand-equal-2.c: Adjust.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@229492 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/operand-equal-2.c
+++ gcc/testsuite/gcc.dg/tree-ssa/operand-equal-2.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -fdump-tree-forwprop1" } */
+/* { dg-options "-O -fdump-tree-fre1" } */
 
 typedef char __attribute__ ((vector_size (4))) v4qi;
 
@@ -9,4 +9,4 @@ void ret(char a)
   v4qi c={a,a,a,a},d={a,a,a,a};
   v = (c!=d);
 }
-/* { dg-final { scan-tree-dump "v = . 0, 0, 0, 0 ." "forwprop2"} } */
+/* { dg-final { scan-tree-dump "v = . 0, 0, 0, 0 ." "fre1"} } */
