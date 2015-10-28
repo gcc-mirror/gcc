254b22cc79c594756343f645cbcf7163e39cf5b4
Richard Biener <rguenther@suse.de>
2015-10-28 10:01:23 +0000

    re PR middle-end/68067 (Wrong constant folding)
    2015-10-28  Richard Biener  <rguenther@suse.de>
    
    	PR middle-end/68067
    	* fold-const.c (negate_expr_p): We cannot negate plus or minus
    	if overflow is not wrapping.  Likewise multiplication unless
    	one operand is constant and not power of two.
    	(fold_negate_expr): Adjust accordingly.
    
    	* gcc.dg/torture/pr68067-1.c: New testcase.
    	* gcc.dg/torture/pr68067-2.c: Likewise.
    
    From-SVN: r229479

---
+++ gcc/testsuite/gcc.dg/torture/pr68067-1.c
@@ -0,0 +1,12 @@
+/* { dg-do run } */
+
+int main()
+{
+  int a = -1;
+  static int b = -2147483647 - 1;
+  static int c = 0;
+  int t = a - (b - c);
+  if (t != 2147483647)
+    __builtin_abort();
+  return 0;
+}
