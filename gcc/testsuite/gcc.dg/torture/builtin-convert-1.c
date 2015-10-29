f5096a046ad4baffe70a46cabb08c74da6fb1653
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-29 18:32:19 +0000

    Require c99_runtime for builtin-convert-1.c
    ...which was failing on *-elf.  This test is making sure that we narrow
    maths calls, e.g. sinh -> sinhf, but we can only do that if we know that
    the target has a full c99 libm or if we have other proof that the function
    we want to generate is available.
    
    Tested on aarch64-none-elf (where it's skipped) and aarch64-linux-gnu
    (where it isn't).
    
    gcc/testsuite/
    	* gcc.dg/torture/builtin-convert-1.c: Require c99_runtime.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@229548 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/torture/builtin-convert-1.c
+++ gcc/testsuite/gcc.dg/torture/builtin-convert-1.c
@@ -8,6 +8,7 @@
 /* { dg-do link } */
 /* { dg-options "-ffast-math" } */
 /* { dg-add-options c99_runtime } */
+/* { dg-require-effective-target c99_runtime } */
 
 #include "../builtins-config.h"
 
