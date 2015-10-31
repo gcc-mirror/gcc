ce2ac1629f0aa497f2fc4577e30ce15d7f3fdee6
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-31 17:13:32 +0000

    * gnat.dg/discr45.adb: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@229630 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gnat.dg/discr45.adb
@@ -0,0 +1,44 @@
+-- { dg-do run }
+-- { dg-options "-O2 -gnatws" }
+
+procedure Discr45 is
+
+  function Ident_Int (I : Integer) return Integer is
+  begin
+    return I;
+  end;
+
+  procedure Proc (Signal : Boolean) is
+
+    subtype Index is Integer range 1..10;
+
+    type My_Arr is array (Index range <>) OF Integer;
+
+    type Rec (D3 : Integer := Ident_Int(1)) is record
+      case D3 is
+        when -5..10 => C1 : My_Arr(D3..Ident_Int(11));
+        when Others => C2 : Integer := Ident_Int(5);
+      end case;
+    end record;
+
+    X : Rec;
+
+    function Value return Rec;
+    pragma No_Inline (Value);
+
+    function Value return Rec is
+    begin
+      return X;
+    end;
+
+  begin
+    if X /= Value then
+      raise Constraint_Error;
+    elsif Signal then
+      raise Program_Error;
+    end if;
+  end;
+
+begin
+  Proc (True);
+end;
