dbcfd357daf1c1b3da938558725bddd5dd568a25
trippels <trippels@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-31 10:57:33 +0000

    ggc-common.c : Restore needed header for checking=release.
           *ggc-common.c : Restore needed header for checking=release.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@229623 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ggc-common.c
+++ gcc/ggc-common.c
@@ -26,6 +26,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "timevar.h"
 #include "diagnostic-core.h"
 #include "ggc-internal.h"
+#include "params.h"
 #include "hosthooks.h"
 #include "plugin.h"
 
