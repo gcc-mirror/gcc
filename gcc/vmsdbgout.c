1eddb04c7103a2f87118fc98111c1877b544a6a9
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-11-02 05:27:14 +0000

    [PATCH] Fix vms targets
    	* vmsdbgout.c: Revert unused header file reduction patch.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@229650 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/vmsdbgout.c
+++ gcc/vmsdbgout.c
@@ -22,6 +22,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "config.h"
 #include "system.h"
 #include "coretypes.h"
+#include "tm.h"
 
 #ifdef VMS_DEBUGGING_INFO
 #include "alias.h"
