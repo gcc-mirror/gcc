38101f91e6f6e27ccb8432c1965e6ccb1387c377
Mike Stump <mikestump@comcast.net>
2015-11-06 21:16:30 +0000

    * gcc.dg/pragma-diag-5.c: Make test cases unique.
    From-SVN: r229891

--- gcc/testsuite/gcc.dg/pragma-diag-5.c
+++ gcc/testsuite/gcc.dg/pragma-diag-5.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 #pragma GCC diagnostic /* { dg-warning "missing" "missing" } */
-/* { dg-warning "24:missing" "missing" { xfail *-*-* }  2 } */
+/* { dg-warning "24:missing" "wrong column" { xfail *-*-* }  2 } */
 
 #pragma GCC diagnostic warn /* { dg-warning "24:expected" } */
 
