3e4d188a5fe6df5994af7ee800614305afb7f8f7
Andre Vehreschild <vehre@gcc.gnu.org>
2015-11-08 18:37:42 +0100

    re PR fortran/68218 (ALLOCATE with size given by a module function)
    gcc/fortran/ChangeLog:
    
    2015-11-08  Andre Vehreschild  <vehre@gcc.gnu.org>
    
    	PR fortran/68218
    	* trans-array.c (gfc_array_init_size): Add gfc_evaluate_now() when
    	array spec in allocate is a function call.
    
    gcc/testsuite/ChangeLog:
    
    2015-11-08  Andre Vehreschild  <vehre@gcc.gnu.org>
    
    	PR fortran/68218
    	* gfortran.dg/allocate_with_arrayspec_1.f90: New test.
    
    From-SVN: r229956

---
+++ gcc/testsuite/gfortran.dg/allocate_with_arrayspec_1.f90
@@ -0,0 +1,29 @@
+! { dg-do run }
+! { dg-options "-fdump-tree-original" }
+
+MODULE mo_test
+
+  integer :: n = 0
+CONTAINS
+
+  FUNCTION nquery()
+    INTEGER :: nquery
+    WRITE (0,*) "hello!"
+    n = n + 1
+    nquery = n
+  END FUNCTION nquery
+
+END MODULE mo_test
+
+
+! ----------------------------------------------------------------------
+! MAIN PROGRAM
+! ----------------------------------------------------------------------
+PROGRAM example
+   USE mo_test
+   INTEGER, ALLOCATABLE :: query_buf(:)
+   ALLOCATE(query_buf(nquery()))
+   if (n /= 1 .or. size(query_buf) /= n) call abort()
+END PROGRAM example
+
+! { dg-final { scan-tree-dump-times "nquery" 5 "original" } }
