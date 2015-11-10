4b63f4a3dd0a01fa7d25b64f4f25ab3cfa657678
Jonathan Wakely <jwakely@redhat.com>
2015-11-10 18:08:50 +0000

    Fix return type of heterogeneous find for sets
    	PR libstdc++/68190
    	* include/bits/stl_multiset.h (multiset::find): Fix return types.
    	* include/bits/stl_set.h (set::find): Likewise.
    	* testsuite/23_containers/map/operations/2.cc: Test find return types.
    	* testsuite/23_containers/multimap/operations/2.cc: Likewise.
    	* testsuite/23_containers/multiset/operations/2.cc: Likewise.
    	* testsuite/23_containers/set/operations/2.cc: Likewise.
    
    From-SVN: r230118

--- libstdc++-v3/include/bits/stl_multiset.h
+++ libstdc++-v3/include/bits/stl_multiset.h
@@ -682,13 +682,15 @@ _GLIBCXX_BEGIN_NAMESPACE_CONTAINER
 #if __cplusplus > 201103L
       template<typename _Kt>
 	auto
-	find(const _Kt& __x) -> decltype(_M_t._M_find_tr(__x))
-	{ return _M_t._M_find_tr(__x); }
+	find(const _Kt& __x)
+	-> decltype(iterator{_M_t._M_find_tr(__x)})
+	{ return iterator{_M_t._M_find_tr(__x)}; }
 
       template<typename _Kt>
 	auto
-	find(const _Kt& __x) const -> decltype(_M_t._M_find_tr(__x))
-	{ return _M_t._M_find_tr(__x); }
+	find(const _Kt& __x) const
+	-> decltype(const_iterator{_M_t._M_find_tr(__x)})
+	{ return const_iterator{_M_t._M_find_tr(__x)}; }
 #endif
       //@}
 
