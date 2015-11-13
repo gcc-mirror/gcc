ec8b536ffd7056c0f82c309da713352e6e4deed5
Marek Polacek <polacek@redhat.com>
2015-11-13 14:05:59 +0000

    re PR c/68320 (internal compiler error: in declspecs_add_type)
    	PR c/68320
    	* c-parser.c (c_parser_for_statement): Treat unknown tokens as IDs.
    
    	* gcc.dg/pr68320.c: New test.
    
    From-SVN: r230322

---
+++ gcc/testsuite/gcc.dg/pr68320.c
@@ -0,0 +1,67 @@
+/* PR c/68320 */
+/* { dg-do compile } */
+/* { dg-options "" } */
+
+void
+fn1 (void)
+{
+  for (typedef int T;;) /* { dg-error "declaration of non-variable" } */
+    if (1)
+      ;
+  T x; /* { dg-error "unknown type name" } */
+}
+
+void
+fn2 (int i)
+{
+  for (typedef int T;;) /* { dg-error "declaration of non-variable" } */
+    if (1)
+      i = 5;
+  T x; /* { dg-error "unknown type name" } */
+}
+
+void
+fn3 (void)
+{
+  for (typedef int T;;) /* { dg-error "declaration of non-variable" } */
+    if (1)
+      {
+      }
+  T *x; /* { dg-error "unknown type name" } */
+}
+
+void
+fn4 (void)
+{
+  for (typedef int T;;) /* { dg-error "declaration of non-variable" } */
+    if (1)
+      ;
+  T, T; /* { dg-error "undeclared" } */
+}
+
+void
+fn5 (void)
+{
+  for (typedef int T;;) /* { dg-error "declaration of non-variable" } */
+    if (1)
+      ;
+  T = 10; /* { dg-error "undeclared" } */
+}
+
+void
+fn6 (void)
+{
+  for (typedef int T;;) /* { dg-error "declaration of non-variable" } */
+    if (1)
+      ;
+  T[0]; /* { dg-error "undeclared" } */
+}
+
+void
+fn7 (void)
+{
+  for (typedef int T;;) /* { dg-error "declaration of non-variable" } */
+    if (1)
+      ;
+  T (); /* { dg-warning "implicit declaration" } */
+}
