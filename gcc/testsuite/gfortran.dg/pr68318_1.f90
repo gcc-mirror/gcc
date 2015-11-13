c77c84dd79a726cde84d2dc7a46926f43f4401fa
kargl <kargl@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-11-13 00:14:32 +0000

    2015-11-12  Steven G. Kargl  <kargl@gcc.gnu.org>
    	PR fortran/68318
    	* decl.c (get_proc_name): Increment reference count for ENTRY.
    	While here, fix comment and use postfix ++ for consistency.
    
    2015-11-12  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	PR fortran/68318
    	* gfortran.dg/pr68318_1.f90: New test.
    	* gfortran.dg/pr68318_2.f90: Ditto.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@230278 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr68318_1.f90
@@ -0,0 +1,18 @@
+! { dg-do compile }
+! { dg-options "-O0"
+! PR fortran/68318
+! Original code submitted by Gerhard Steinmetz
+! <gerhard dot steinmetz dot fortran at t-online dot de>
+!
+module m
+   implicit none
+contains
+   subroutine s1
+   entry e        ! { dg-error "(2)" }
+   end
+   subroutine s2
+   entry e        ! { dg-error "is already defined" }
+   end
+end module
+! { dg-prune-output "Duplicate ENTRY attribute specified" }
+
