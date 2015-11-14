7b9f7d16b69fa74eb3c455e9b349b3f6e181659b
andreast <andreast@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-11-14 21:17:24 +0000

    2015-11-14  Andreas Tobler  <andreast@gcc.gnu.org>
        * acinclude.m4 (GLIBCXX_ENABLE_CLOCALE): Change locale implementation
        from darwin to DragonFly.
        * configure: Regenerate.
        * config/os/bsd/freebsd/ctype_configure_char.cc: Improve locale
        support, do it the same as DragonFly.
        * config/os/bsd/freebsd/os_defines.h: Add fine grained C99 defines.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@230383 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/config/os/bsd/freebsd/ctype_configure_char.cc
+++ libstdc++-v3/config/os/bsd/freebsd/ctype_configure_char.cc
@@ -1,6 +1,6 @@
 // Locale support -*- C++ -*-
 
-// Copyright (C) 2011-2015 Free Software Foundation, Inc.
+// Copyright (C) 2014-2015 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -37,32 +37,60 @@ namespace std _GLIBCXX_VISIBILITY(default)
 _GLIBCXX_BEGIN_NAMESPACE_VERSION
 
 // Information as gleaned from /usr/include/ctype.h
-  
+
   const ctype_base::mask*
   ctype<char>::classic_table() throw()
-  { return 0; }
+  { return NULL; }
 
-  ctype<char>::ctype(__c_locale, const mask* __table, bool __del, 
-		     size_t __refs) 
-  : facet(__refs), _M_del(__table != 0 && __del), 
-  _M_toupper(NULL), _M_tolower(NULL), 
-  _M_table(__table ? __table : classic_table()) 
-  { 
+  ctype<char>::ctype(__c_locale, const mask* __table, bool __del,
+		     size_t __refs)
+  : facet(__refs), _M_c_locale_ctype(_S_get_c_locale()),
+  _M_del(__table != 0 && __del), _M_widen_ok(0), _M_narrow_ok(0)
+  {
+    char* __old = setlocale(LC_CTYPE, NULL);
+    char* __sav = NULL;
+    if (strcmp(__old, "C"))
+      {
+	const size_t __len = strlen(__old) + 1;
+	__sav = new char[__len];
+	memcpy(__sav, __old, __len);
+	setlocale(LC_CTYPE, "C");
+      }
+    _M_toupper = NULL;
+    _M_tolower = NULL;
+    _M_table = __table ? __table : classic_table();
+    if (__sav)
+      {
+	setlocale(LC_CTYPE, __sav);
+	delete [] __sav;
+      }
     memset(_M_widen, 0, sizeof(_M_widen));
-    _M_widen_ok = 0;
     memset(_M_narrow, 0, sizeof(_M_narrow));
-    _M_narrow_ok = 0;
   }
 
-  ctype<char>::ctype(const mask* __table, bool __del, size_t __refs) 
-  : facet(__refs), _M_del(__table != 0 && __del), 
-  _M_toupper(NULL), _M_tolower(NULL), 
-  _M_table(__table ? __table : classic_table()) 
-  { 
+  ctype<char>::ctype(const mask* __table, bool __del, size_t __refs)
+  : facet(__refs), _M_c_locale_ctype(_S_get_c_locale()),
+  _M_del(__table != 0 && __del), _M_widen_ok(0), _M_narrow_ok(0)
+  {
+    char* __old = setlocale(LC_CTYPE, NULL);
+    char* __sav = NULL;
+    if (strcmp(__old, "C"))
+      {
+	const size_t __len = strlen(__old) + 1;
+	__sav = new char[__len];
+	memcpy(__sav, __old, __len);
+	setlocale(LC_CTYPE, "C");
+      }
+    _M_toupper = NULL;
+    _M_tolower = NULL;
+    _M_table = __table ? __table : classic_table();
+    if (__sav)
+      {
+	setlocale(LC_CTYPE, __sav);
+	delete [] __sav;
+      }
     memset(_M_widen, 0, sizeof(_M_widen));
-    _M_widen_ok = 0;
     memset(_M_narrow, 0, sizeof(_M_narrow));
-    _M_narrow_ok = 0;
   }
 
   char
@@ -84,7 +112,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
   ctype<char>::do_tolower(char __c) const
   { return ::tolower((int) __c); }
 
-  const char* 
+  const char*
   ctype<char>::do_tolower(char* __low, const char* __high) const
   {
     while (__low < __high)
