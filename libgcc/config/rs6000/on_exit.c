5bbad880c4d29bb441ecdbbdede312af2b1c7621
dje <dje@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-11-15 23:41:44 +0000

    * config/rs6000/on_exit.c: New file.         * config/rs6000/t-aix-cxa (LIB2ADDEH): Build on_exit.c.         * config/rs6000/libgcc-aix-cxa.ver (on_exit): Add symbol to exports.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@230398 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libgcc/config/rs6000/on_exit.c
@@ -0,0 +1,49 @@
+/* Copyright (C) 1999-2015 Free Software Foundation, Inc.
+
+   NOTE: This source is derived from an old version taken from the GNU C
+   Library (glibc).
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+Under Section 7 of GPL version 3, you are granted additional
+permissions described in the GCC Runtime Library Exception, version
+3.1, as published by the Free Software Foundation.
+
+You should have received a copy of the GNU General Public License and
+a copy of the GCC Runtime Library Exception along with this program;
+see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+<http://www.gnu.org/licenses/>.  */
+
+#include <stdlib.h>
+#include "exit.h"
+
+#define atomic_write_barrier() __asm__ ("eieio" ::: "memory")
+
+/* Register a function to be called by exit.  */
+int
+on_exit (void (*func) (int status, void *arg), void *arg)
+{
+  struct exit_function *new = __new_exitfn (&__exit_funcs);
+
+  if (new == NULL)
+    return -1;
+
+#ifdef PTR_MANGLE
+  PTR_MANGLE (func);
+#endif
+  new->func.on.fn = func;
+  new->func.on.arg = arg;
+  atomic_write_barrier ();
+  new->flavor = ef_on;
+  return 0;
+}
