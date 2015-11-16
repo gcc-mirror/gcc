5ce8d99a1b45692a14b4a8aaf5b481738bf95172
Tom de Vries <tom@codesourcery.com>
2015-11-16 12:40:24 +0000

    Remove first_pass_instance from pass_dominator
    2015-11-16  Tom de Vries  <tom@codesourcery.com>
    
    	* passes.def: Add arg to pass_dominator pass instantiation.
    	* tree-pass.h (first_pass_instance): Remove pass_dominator-related bit
    	of comment.
    	* tree-ssa-dom.c (pass_dominator::pass_dominator): Initialize
    	may_peel_loop_headers_p.
    	(pass_dominator::set_pass_param): New member function.  Set
    	may_peel_loop_headers_p.
    	(pass_dominator::may_peel_loop_headers_p): New private member.
    	(pass_dominator::execute): Use may_peel_loop_headers_p instead of
    	first_pass_instance.
    
    From-SVN: r230417

--- gcc/tree-pass.h
+++ gcc/tree-pass.h
@@ -631,11 +631,8 @@ extern bool function_called_by_processed_nodes_p (void);
 
 /* Set to true if the pass is called the first time during compilation of the
    current function.  Note that using this information in the optimization
-   passes is considered not to be clean, and it should be avoided if possible.
-   This flag is currently used to prevent loops from being peeled repeatedly
-   in jump threading; it will be removed once we preserve loop structures
-   throughout the compilation -- we will be able to mark the affected loops
-   directly in jump threading, and avoid peeling them next time.  */
+   passes is considered not to be clean, and it should be avoided if
+   possible.  */
 extern bool first_pass_instance;
 
 /* Declare for plugins.  */
