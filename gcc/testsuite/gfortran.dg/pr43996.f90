9de06cb01142ee62ad22e223756dce70c744c765
Steven G. Kargl <kargl@gcc.gnu.org>
2015-11-18 23:30:26 +0000

    * Revert previous commit due to preplexing regression.
    From-SVN: r230582

--- gcc/testsuite/gfortran.dg/pr43996.f90
+++
@@ -1,7 +0,0 @@
-! { dg-do compile }
-! PR fortran/43996
-!
-real, parameter :: a(720,360) = spread((/(j, j=1,720) /), dim=2, ncopies=360) ! { dg-error "number of elements" }
-real x
-x = a(720,360)
-end
