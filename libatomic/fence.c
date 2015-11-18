cd404a14a1515ecd35ab368fe02df305d15656aa
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-11-18 22:13:44 +0000

    Add out-of-line versions of some <stdatomic.h> functions (PR c/65083).
    PR c/65083 notes that some functions in <stdatomic.h> are normal
    functions, not generic functions, and so need to have out-of-line
    copies that can be called when macro expansion is suppressed (unlike
    the generic functions where DR#419 makes it undefined if you suppress
    a macro expansion).
    
    This patch adds such out-of-line definitions in libatomic for those
    six functions, at a new LIBATOMIC_1.2 symbol version, as trivial
    wrappers to the <stdatomic.h> macros, along with declarations of those
    functions in <stdatomic.h>.  Tests are added that are based on the
    corresponding tests for the macros, but with parentheses around the
    function names to force the out-of-line functions to be used.
    
    Bootstrapped with no regressions on x86_64-pc-linux-gnu.
    
    gcc:
    	* ginclude/stdatomic.h (atomic_thread_fence, atomic_signal_fence)
    	(atomic_flag_test_and_set, atomic_flag_test_and_set_explicit)
    	(atomic_flag_clear, atomic_flag_clear_explicit): Declare as
    	functions before defining as macros.
    
    gcc/testsuite:
    	* gcc.dg/atomic/stdatomic-fence-2.c,
    	gcc.dg/atomic/stdatomic-flag-2.c: New tests.
    
    libatomic:
    	* fence.c, flag.c: New files.
    	* Makefile.am (libatomic_la_SOURCES): Add fence.c and flag.c.
    	* Makefile.in: Regenerate.
    	* configure.ac (libtool_VERSION): Change to 3:0:2.
    	* configure: Regenerate.
    	* libatomic.map (LIBATOMIC_1.2): New symbol version.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@230578 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libatomic/fence.c
@@ -0,0 +1,45 @@
+/* Copyright (C) 2015 Free Software Foundation, Inc.
+
+   This file is part of the GNU Atomic Library (libatomic).
+
+   Libatomic is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   Libatomic is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+   more details.
+
+   Under Section 7 of GPL version 3, you are granted additional
+   permissions described in the GCC Runtime Library Exception, version
+   3.1, as published by the Free Software Foundation.
+
+   You should have received a copy of the GNU General Public License and
+   a copy of the GCC Runtime Library Exception along with this program;
+   see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include "libatomic_i.h"
+
+#include <stdatomic.h>
+
+/* Out-of-line versions of <stdatomic.h> fence functions.  */
+
+/* Fence as specified by ORDER.  */
+
+void
+(atomic_thread_fence) (memory_order order)
+{
+  atomic_thread_fence (order);
+}
+
+/* Fence as specified by ORDER but only establishing ordering between
+   a thread and a signal handler executed in that thread.  */
+
+void
+(atomic_signal_fence) (memory_order order)
+{
+  atomic_signal_fence (order);
+}
