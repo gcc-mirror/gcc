7d337486b69591f2a2e8ce26777f9847853ff742
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-11-19 16:18:39 +0000

    PR c++/67409 	* decl.c (identify_goto): Add LOC and DIAG_KIND arguments, call 	emit_diagnostic instead of permerror. 	(check_previous_goto_1): Adjust identify_goto callers, treat all 	cases but crossing initialization and entering scope of decl with 	non-trivial dtor as unconditional hard errors. 	(check_goto): Use identify_goto.  Treat all cases but crossing 	initialization and entering scope of decl with non-trivial dtor 	as unconditional hard errors.
    	* g++.dg/eh/goto3.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@230613 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/eh/goto3.C
@@ -0,0 +1,14 @@
+// PR c++/67409
+// { dg-options "-fpermissive" }
+
+void f()
+try
+  {
+    goto l2;       // { dg-message "from here" }
+  l1: ;	    // { dg-error "jump to label 'l1'" }
+  } catch (...)
+  {
+  l2: ;	    // { dg-error "jump to label 'l2'" }
+		   // { dg-message "enters catch block" "" { target *-*-*} 11 }
+    goto l1;       // { dg-message "from here|enters try block" }
+  }
