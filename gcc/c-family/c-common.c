2d3d3af78ed241a28055e9c55b56ac10cd26f163
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-11-19 04:26:48 +0000

    * c-common.c (shorten_compare): Don't -Wtype-limits if the 	non-constant operand comes from a macro.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@230589 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-family/c-common.c
+++ gcc/c-family/c-common.c
@@ -4650,7 +4650,9 @@ shorten_compare (location_t loc, tree *op0_ptr, tree *op1_ptr,
 	  type = c_common_unsigned_type (type);
 	}
 
-      if (TREE_CODE (primop0) != INTEGER_CST)
+      if (TREE_CODE (primop0) != INTEGER_CST
+	  /* Don't warn if it's from a macro.  */
+	  && !from_macro_expansion_at (EXPR_LOCATION (primop0)))
 	{
 	  if (val == truthvalue_false_node)
 	    warning_at (loc, OPT_Wtype_limits,
