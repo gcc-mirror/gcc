7d04e41de330866e17cde504acd7d5e5b63cdb56
Matthias Klose <doko@ubuntu.com>
2015-11-19 15:19:13 +0000

    configure.ac: Permit also ISL 0.15 with CLooG.
    2015-11-18  Matthias Klose  <doko@ubuntu.com>
    
            * configure.ac: Permit also ISL 0.15 with CLooG.
            * configure: Regenerate.
    
    gcc/
    2015-11-18  Matthias Klose  <doko@ubuntu.com>
    
            Backport from the gcc-5-branch
    
            Backport from mainline
            2015-07-21  Mike Frysinger  <vapier@gentoo.org>
                        Bernhard Reutner-Fischer  <aldot@gcc.gnu.org>
    
            * configure.ac: Add check for new options in isl-0.15.
            * config.in, configure: Rebuilt.
            * graphite-blocking.c: Include <isl/constraint.h>
            * graphite-interchange.c,  graphite-poly.c: Likewise.
            * graphhite-scop-detection.c, graphite-sese-to-poly.c: Likewise.
            * graphite.c, graphite-poly.c: Likewise.
            * graphite-dependences.c: Include <isl/constraint.h>.
            (max_number_of_out_dimensions): Returns isl_stat.
            (extend_schedule_1): Likewise
            (extend_schedule): Corresponding changes.
            * graphite-optimize-isl.c: Include <isl/constraint.h> and
            <isl/union_set.h>.
            (getSingleMap): Change return type of isl_stat.
            (optimize_isl): Conditionally use
            isl_options_set_schedule_serialize_sccs.
            * graphite-poly.h (isl_stat, isl_stat_ok): Define fallbacks
            if not HAVE_ISL_OPTIONS_SET_SCHEDULE_SERIALIZE_SCCS.
    
    From-SVN: r230610

--- gcc/graphite-poly.h
+++ gcc/graphite-poly.h
@@ -22,6 +22,11 @@ along with GCC; see the file COPYING3.  If not see
 #ifndef GCC_GRAPHITE_POLY_H
 #define GCC_GRAPHITE_POLY_H
 
+#ifndef HAVE_ISL_OPTIONS_SET_SCHEDULE_SERIALIZE_SCCS
+# define isl_stat int
+# define isl_stat_ok 0
+#endif
+
 typedef struct poly_dr *poly_dr_p;
 
 typedef struct poly_bb *poly_bb_p;
