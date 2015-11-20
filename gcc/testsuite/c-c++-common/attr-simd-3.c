cf0347ae4c15e3435225e3285bfcb6ed3bf044b1
Kirill Yukhin <kirill.yukhin@intel.com>
2015-11-20 05:42:41 +0000

    re PR c++/68158 (Function attributes parsed incorrectly in C++ (while -fcilkplus))
    gcc/testsuite/
    	* c-c++-common/attr-simd-3.c: Put xfail (PR68158) on dg-error.
    	* c-c++-common/attr-simd.c: Limit scan of dump to x86_64/i?86.
    
    From-SVN: r230641

--- gcc/testsuite/c-c++-common/attr-simd-3.c
+++ gcc/testsuite/c-c++-common/attr-simd-3.c
@@ -2,4 +2,4 @@
 /* { dg-options "-fcilkplus" } */
 /* { dg-prune-output "undeclared here \\(not in a function\\)|\[^\n\r\]* was not declared in this scope" } */
 
-void f () __attribute__((__simd__, __vector__)); /* { dg-error "in the same function marked as a Cilk Plus" } */
+void f () __attribute__((__simd__, __vector__)); /* { dg-error "in the same function marked as a Cilk Plus" "PR68158" { xfail c++ } } */
