39a9f2bea17b2d74c71ecac457c6771d7116390f
jiwang <jiwang@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-11-23 12:14:05 +0000

    [Patch] Drop constant overflow flag in adjust_range_with_scev when possible
    2015-11-23  Richard Biener  <rguenth@gcc.gnu.com>
                Jiong Wang  <jiong.wang@arm.com>
    
    gcc/
      PR tree-optimization/68317
      PR tree-optimization/68326
      * tree-vrp.c (adjust_range_with_scev): Call drop_tree_overflow if the
      final min and max are not infinity.
    
    gcc/testsuite/
      * gcc.dg/pr68317.c: New testcase.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@230754 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr68317.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+
+void bar (int);
+
+void
+foo ()
+{
+ int index = 0;
+
+ for (index; index <= 10; index--)
+   /* Result of the following multiply will overflow
+      when converted to signed int.  */
+   bar ((0xcafe + index) * 0xdead);
+}
