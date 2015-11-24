9211bbb2782fd934e697bcfd13f4547bfff8be37
Max Ostapenko <m.ostapenko@partner.samsung.com>
2015-11-24 14:24:46 +0200

    Move ptrace.h to appropriate place.
    libsanitizer/
    
    	* include/system/linux/asm/ptrace.h: Move to ...
    	* include/system/asm/ptrace.h: ... this.
    
    From-SVN: r230804

--- libsanitizer/include/system/asm/ptrace.h
+++ libsanitizer/include/system/asm/ptrace.h
@@ -1,4 +1,4 @@
-#include_next <linux/asm/ptrace.h>
+#include_next <asm/ptrace.h>
 /* ARM_VFPREGS_SIZE has been added in 3.0 */
 #if defined(__arm__) && !defined(ARM_VFPREGS_SIZE)
 /* The size of the user-visible VFP state as seen by PTRACE_GET/SETVFPREGS
