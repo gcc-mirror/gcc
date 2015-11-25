9c822ac867dd8f31e20861f97b0211efd3f511a5
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-11-25 11:28:49 +0000

    Improve verification of loop->latch in verify_loop_structure
    2015-11-25  Tom de Vries  <tom@codesourcery.com>
    
    	* cfgloop.c (find_single_latch): New function, factored out of ...
    	(flow_loops_find): ... here.
    	(verify_loop_structure): Improve verification of loop->latch.
    	* cfgloop.h (find_single_latch): Declare.
    	* omp-low.c (expand_omp_for_generic): Initialize latch of orig_loop.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@230866 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cfgloop.h
+++ gcc/cfgloop.h
@@ -270,6 +270,7 @@ bool mark_irreducible_loops (void);
 void release_recorded_exits (function *);
 void record_loop_exits (void);
 void rescan_loop_exit (edge, bool, bool);
+basic_block find_single_latch (struct loop*);
 
 /* Loop data structure manipulation/querying.  */
 extern void flow_loop_tree_node_add (struct loop *, struct loop *);
