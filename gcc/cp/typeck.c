e2225bd419ce30e32adc080be8cfe407d649fc95
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-11-26 09:52:48 +0000

    PR c++/68508 	* cp-tree.h (cp_ubsan_maybe_instrument_downcast): Add INTYPE argument. 	* cp-ubsan.c (cp_ubsan_maybe_instrument_downcast): Likewise.  Use 	it instead of or in addition to TREE_TYPE (op).  Use 	is_properly_derived_from, return NULL_TREE if TREE_TYPE (intype) and 	TREE_TYPE (type) are the same type minus qualifiers. 	* typeck.c (build_static_cast_1): Adjust callers.
    	* g++.dg/ubsan/pr68508.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@230928 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/typeck.c
+++ gcc/cp/typeck.c
@@ -6590,7 +6590,8 @@ build_static_cast_1 (tree type, tree expr, bool c_cast_p,
       if (flag_sanitize & SANITIZE_VPTR)
 	{
 	  tree ubsan_check
-	    = cp_ubsan_maybe_instrument_downcast (input_location, type, expr);
+	    = cp_ubsan_maybe_instrument_downcast (input_location, type,
+						  intype, expr);
 	  if (ubsan_check)
 	    expr = ubsan_check;
 	}
@@ -6737,7 +6738,8 @@ build_static_cast_1 (tree type, tree expr, bool c_cast_p,
       if (flag_sanitize & SANITIZE_VPTR)
 	{
 	  tree ubsan_check
-	    = cp_ubsan_maybe_instrument_downcast (input_location, type, expr);
+	    = cp_ubsan_maybe_instrument_downcast (input_location, type,
+						  intype, expr);
 	  if (ubsan_check)
 	    expr = ubsan_check;
 	}
