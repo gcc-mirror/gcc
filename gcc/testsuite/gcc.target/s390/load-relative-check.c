02718b323ba585f41a34e4202c9ca5ba1b3aa321
Andreas Krebbel <krebbel@linux.vnet.ibm.com>
2015-11-30 13:24:51 +0000

    S/390: Testsuite: Add asm scan patterns for -m31.
    gcc/testsuite/ChangeLog:
    
    2015-11-30  Andreas Krebbel  <krebbel@linux.vnet.ibm.com>
    
    	* gcc.target/s390/load-relative-check.c: Add scan patterns for
    	-m31.
    
    From-SVN: r231071

--- gcc/testsuite/gcc.target/s390/load-relative-check.c
+++ gcc/testsuite/gcc.target/s390/load-relative-check.c
@@ -2,10 +2,13 @@
 /* { dg-do compile { target { s390*-*-* } } } */
 /* { dg-options "-O2 -march=z10 -mzarch" } */
 
-/* { dg-final { scan-assembler "lgfrl\t%r.?,b.4" } } */
-/* { dg-final { scan-assembler "lgfrl\t%r.?,s.12" } } */
-/* { dg-final { scan-assembler "lgrl\t%r.?,s" } } */
-/* { dg-final { scan-assembler "larl\t%r.?,.L.?" } } */
+/* { dg-final { scan-assembler "lgfrl\t%r.?,b.4" { target { lp64 } } } } */
+/* { dg-final { scan-assembler "lgfrl\t%r.?,s.12" { target { lp64 } } } } */
+/* { dg-final { scan-assembler "lgrl\t%r.?,s" { target { lp64 } } } } */
+
+/* { dg-final { scan-assembler "lrl\t%r.?,b.4" { target { ! lp64 } } } } */
+/* { dg-final { scan-assembler "lrl\t%r.?,s.8" { target { ! lp64 } } } } */
+/* { dg-final { scan-assembler "lrl\t%r.?,s" { target { ! lp64 } } } } */
 
 int b[20];
 
