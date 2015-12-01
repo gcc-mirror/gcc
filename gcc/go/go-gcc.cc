3002293ac6a0719607f3d9946bf9e053e835842f
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-01 04:45:53 +0000

    PR go/68477 	* go-gcc.cc (Gcc_backend::string_constant_expression): Don't set 	TYPE_STRING_FLAG on a variant type.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@231096 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/go/go-gcc.cc
+++ gcc/go/go-gcc.cc
@@ -1279,7 +1279,6 @@ Gcc_backend::string_constant_expression(const std::string& val)
   tree const_char_type = build_qualified_type(unsigned_char_type_node,
 					      TYPE_QUAL_CONST);
   tree string_type = build_array_type(const_char_type, index_type);
-  string_type = build_variant_type_copy(string_type);
   TYPE_STRING_FLAG(string_type) = 1;
   tree string_val = build_string(val.length(), val.data());
   TREE_TYPE(string_val) = string_type;
