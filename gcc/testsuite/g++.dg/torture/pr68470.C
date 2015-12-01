ece95567dfbaff5b3e2fb8ca3552e0c086757cc7
Richard Biener <rguenther@suse.de>
2015-12-01 11:35:21 +0000

    re PR ipa/68470 (Internal Compiler Error observed by g++-4.9.2 and a few other versions (reported to Debian))
    2015-12-01  Richard Biener  <rguenther@suse.de>
    
    	PR ipa/68470
    	* ipa-split.c (split_function): Handle main part not returning.
    
    	* g++.dg/torture/pr68470.C: New testcase.
    
    From-SVN: r231108

---
+++ gcc/testsuite/g++.dg/torture/pr68470.C
@@ -0,0 +1,36 @@
+/* { dg-do compile } */
+
+void deallocate(void *);
+void *a;
+
+struct C {
+    virtual void m_fn1();
+};
+
+struct D {
+    C *m_fn2() {
+	if (a)
+	  __builtin_abort();
+    }
+};
+D getd();
+
+struct vec_int {
+    int _M_start;
+    ~vec_int() {
+	if (_M_start)
+	  deallocate(&_M_start);
+    }
+};
+vec_int *b;
+
+struct I {
+    virtual void m_fn3();
+};
+
+void I::m_fn3() {
+    if (a)
+      getd().m_fn2()->m_fn1();
+    b->~vec_int();
+}
+
