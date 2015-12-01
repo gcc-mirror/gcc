cdf66caf4fa836a4398f74faceb373d84e019bb7
Bin Cheng <bin.cheng@arm.com>
2015-12-01 05:22:16 +0000

    re PR tree-optimization/68529 (scev failed for while(i--))
    	PR tree-optimization/68529
    	* tree-ssa-loop-niter.c (number_of_iterations_ne): Add new param.
    	Compute no-overflow information for control iv.
    	(number_of_iterations_lt, number_of_iterations_le): Add new param.
    	(number_of_iterations_cond): Pass new argument to above functions.
    
    	PR tree-optimization/68529
    	* gcc.dg/tree-ssa/pr68529-1.c: New test.
    	* gcc.dg/tree-ssa/pr68529-2.c: New test.
    	* gcc.dg/tree-ssa/pr68529-3.c: New test.
    
    From-SVN: r231097

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr68529-3.c
@@ -0,0 +1,47 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -ftree-loop-distribution -ftree-loop-distribute-patterns -fdump-tree-ldist-details" } */
+
+void bar(char *s);
+int foo1(unsigned short l)
+{
+  char c[10000] = {};
+  unsigned short nchar = 9999;
+
+  while(nchar-- != l)
+    {
+      c[nchar] = 'A';
+    }
+
+  bar (c);
+  return 0;
+}
+
+int foo2()
+{
+  char c[100000] = {};
+  unsigned short nchar;
+
+  for (nchar = 0; nchar != 1000; --nchar)
+    {
+      c[nchar] = 'A';
+    }
+
+  bar (c);
+  return 0;
+}
+
+int foo3()
+{
+  char c[100000] = {};
+  unsigned short nchar;
+
+  for (nchar = 0; nchar != 1000; nchar += 3)
+    {
+      c[nchar] = 'A';
+    }
+
+  bar (c);
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "failed: evolution of offset is not affine" 3 "ldist" } } */
