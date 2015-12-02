3810ff24db44fde0629e0fc1e6449a219aef94fb
Richard Biener <rguenther@suse.de>
2015-12-02 09:47:43 +0000

    re PR tree-optimization/68625 (Segmentation fault in useless_type_conversion_p)
    2015-12-02  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/68625
    	* tree-cfgcleanup.c (cleanup_tree_cfg_bb): Do not call
    	cleanup_control_flow_bb.
    	(cleanup_tree_cfg_1): First perform cleanup_control_flow_bb
    	on all BBs, then cleanup_tree_cfg_bb and finally iterate
    	over the worklist doing both.
    
    	* gcc.dg/torture/pr68625.c: New testcase.
    
    From-SVN: r231162

---
+++ gcc/testsuite/gcc.dg/torture/pr68625.c
@@ -0,0 +1,51 @@
+/* { dg-do compile } */
+/* { dg-additional-options "-w" } */
+
+int **dp;
+int sg;
+
+void
+z9(void)
+{
+  int pz, oi, vz, yp, zi, hd, pw, gr, w9 = 0, j0 = -1, rb = &w9;
+  int *lr;
+  while (w9 < 1) {
+      lr++;
+      *lr = 1;
+      if (*lr < 1)
+	for (;;)
+	  if (pz && *lr) {
+ee:
+	      **dp = 0;
+	  }
+      pz = zi = vz;
+      if (j0 ^ (vz > 0))
+	continue;
+      **dp = 1;
+      while (**dp)
+	if (++oi) {
+	    int mq = dp;
+	    j0 = 1;
+	    while (pw < 1) {
+		if (++rb && mq)
+		  xq:
+		      hd = sg;
+		++pw;
+	    }
+	    sg = 0;
+	    while (!sg) {
+		goto ee;
+		while (++yp && gr++) {
+		    int i9, xa;
+		    while (++i9 && ++xa)
+		      fb:
+			  ;
+		}
+	    }
+	}
+  }
+  ++vz;
+  if (zi > hd)
+    goto xq;
+  goto fb;
+}
