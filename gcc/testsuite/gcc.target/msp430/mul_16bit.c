7ed86ba5787700903cbae053e141edb5c724cfe1
nickc <nickc@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-04 17:24:30 +0000

    gcc	* config.gcc (extra_gcc_objs): Define for MSP430.         * common/config/msp430/msp430-common.c (msp430_handle_option): 	Pass both -mmcu and -mcpu on to the back end if they are both 	defined. 	* config/msp430/msp430.c (hwmult_name): New function.         (msp430_option_override): If an unrecognised MCU name is 	detected only warn if the user has not provided suitable         -mhwmult and -mcpu options.  Use msp430_warn_mcu to control 	warning messages.  Generate warnings about conflicts between 	-mmcu and -mcpu and -mhwmult options. 	If neither -mcpu nor -mmcu have been specified but -mhwmult= 	f5series has the select the 430X isa. 	(msp430_no_hwmult): If -mmcu has not been specified and 	msp430_hwmult_type is AUTO then return true. 	* config/msp430/msp430.h (EXTRA_SPEC_FUNCTIONS): Define. 	(LIB_SPEC): Add hardware multiply library selection. 	* config/msp430/t-msp430: Delete hardware multiply multilibs. 	Add rule to build driver-msp430.o 	* config/msp430/driver-msp430.c: New file. 	* config/msp430/msp430.opt (warn-mcu): New option. 	* doc/invoke.texi: Update description of -mhwmult=auto.         Document -mwarn-mcu option.
    tests	* gcc.target/msp430/msp_abi_div_funcs.c: New test.
    	* gcc.target/msp430/mul_main.h: New test support file.
    	* gcc.target/msp430/mul_none.c: New test.
    	* gcc.target/msp430/mul_16bit.c: New test.
    	* gcc.target/msp430/mul_32bit.c: New test.
    	* gcc.target/msp430/mul_f5.c: New test.
    
    libgcc	* config/msp430/mpy.c (__mulhi3): Use a faster algorithm.
    	Allow for the second argument being negative.
    	* config.host (extra_parts): Define for MSP430.  Create separate
    	libraries for each of the hardware multiply formats.
    	* config/msp430/lib2hw_mul.S: Build only the multiply routines
    	that are needed.
    	* config/msp430/lib2mul.c: Likewise.
    	* config/msp430/t-msp430 (LIB2ADD): Remove lib2hw_mul.S.
    	Add rules to build hardware multiply libraries.
    	* config/msp430/lib2divSI.c: (__mspabi_divlu): Alias for
    	__mspabi_divul function.
    	(__mspabi_divllu): New stub function.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@231286 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/msp430/mul_16bit.c
@@ -0,0 +1,4 @@
+/* { dg-do run } */
+/* { dg-options "-mhwmult=16bit" } */
+
+#include "mul_main.h"
---
+++ gcc/testsuite/gcc.target/msp430/mul_f5.c
@@ -0,0 +1,4 @@
+/* { dg-do run } */
+/* { dg-options "-mhwmult=f5series" } */
+
+#include "mul_main.h"
