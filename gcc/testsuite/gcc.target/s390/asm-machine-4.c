7763d9729422ddc5dda9a3fe71a024c4ca7e0435
Dominik Vogt <vogt@linux.vnet.ibm.com>
2015-12-04 14:33:01 +0000

    [PATCH 2/2] S/390: Implement "target" attribute.
    gcc/ChangeLog
    
    	* config/s390/s390.c (s390_asm_file_start): Implement hook function to
    	emit .machine and .machinemode to the top of the assembler file.
    	(TARGET_ASM_FILE_START): Provide target hook.
    	(s390_asm_output_machine_for_arch): Protect with
    	HAVE_AS_MACHINE_MACHINEMODE instead of S390_USE_TARGET_ATTRIBUTE.
    
    gcc/testsuite/ChangeLog
    
    	* gcc.target/s390/asm-machine-1.c: Updated.
    	* gcc.target/s390/asm-machine-2.c: Updated.
    	* gcc.target/s390/asm-machine-3.c: Updated.
    	* gcc.target/s390/asm-machine-4.c: Updated.
    
    From-SVN: r231272

--- gcc/testsuite/gcc.target/s390/asm-machine-4.c
+++ gcc/testsuite/gcc.target/s390/asm-machine-4.c
@@ -3,6 +3,9 @@
 /* { dg-require-effective-target target_attribute } */
 /* { dg-options "-march=z13 -mzarch" } */
 
+/* { dg-final { scan-assembler-times "\t\.machine \"z13\"" 1 } } */
+/* { dg-final { scan-assembler-times "\t\.machinemode zarch" 2 } } */
+
 void foo(void) { }
 
 #pragma GCC target("arch=z196")
@@ -13,8 +16,8 @@ void bar(void) { }
 /* { dg-final { scan-assembler-times "\t\.machine pop" 1 } } */
 /* { dg-final { scan-assembler-times "\t\.machine \"z10\"" 1 } } */
 /* { dg-final { scan-assembler-times "\t\.machinemode push" 1 } } */
-/* { dg-final { scan-assembler-times "\t\.machinemode zarch" 1 } } */
+/* { dg-final { scan-assembler-times "\t\.machinemode zarch" 2 } } */
 /* { dg-final { scan-assembler-times "\t\.machinemode pop" 1 } } */
 
-/* { dg-final { scan-assembler-times "\t\.machine " 3 } } */
-/* { dg-final { scan-assembler-times "\t\.machinemode " 3 } } */
+/* { dg-final { scan-assembler-times "\t\.machine " 4 } } */
+/* { dg-final { scan-assembler-times "\t\.machinemode " 4 } } */
