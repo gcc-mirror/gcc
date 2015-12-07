17c6c56b906d99b45dc5eefa5d39435188235051
Ryan Burn <contact@rnburn.com>
2015-12-07 21:45:13 +0000

    re PR c++/68683 ([concepts] function satisfy_argument_deduction_constraint modifies a type tree node but leaves TYPE_CANONICAL unchanged)
    	PR c++/68683
    	* constraint.cc (satisfy_argument_deduction_constraint): Set
    	TYPE_CANONICAL to NULL_TREE if PLACEHOLDER_TYPE_CONSTRAINTS are
    	changed.
    
    From-SVN: r231385

---
+++ gcc/testsuite/g++.dg/concepts/pr68683.C
@@ -0,0 +1,24 @@
+// { dg-options "-std=c++1z" }
+
+template <typename, typename>
+struct is_same {
+  static constexpr bool value = true;
+};
+
+template <typename T, typename U>
+concept bool Same = is_same<T, U>::value;
+
+template <typename T>
+concept bool Integral = requires {
+  { T () } -> Same<typename T::value_type>;
+};
+
+struct A {
+  using value_type = bool;
+};
+
+int main () {
+  Integral<A>;
+  Integral<A>;
+  return 0;
+}
