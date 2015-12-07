7c010e27a9e9d975ff265264ae515f7dc9feebd6
Jakub Jelinek <jakub@redhat.com>
2015-12-07 23:44:19 +0100

    re PR c++/68760 (ICE in pp_string (pretty-print.c:909))
    	PR c++/68760
    	* error.c (dump_global_iord): Use DECL_SOURCE_FILE (t)
    	instead of LOCATION_FILE (input_location).
    
    	* g++.dg/tree-ssa/pr68760.C: New test.
    
    From-SVN: r231386

--- gcc/cp/error.c
+++ gcc/cp/error.c
@@ -953,7 +953,7 @@ dump_global_iord (cxx_pretty_printer *pp, tree t)
   else
     gcc_unreachable ();
 
-  pp_printf (pp, p, LOCATION_FILE (input_location));
+  pp_printf (pp, p, DECL_SOURCE_FILE (t));
 }
 
 static void
