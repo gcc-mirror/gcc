fc2e8e423f34f2163b0e3b49b4f37c77b5ead922
Kirill Yukhin <kirill.yukhin@intel.com>
2015-12-07 11:05:40 +0000

    re PR target/68633 ([i386, AVX-512] Spec2006/434.zeus miscompares when executed on KNL)
    PR target/68633
    
    gcc/
    	* config/i386/sse.md (define_insn "kunpckhi"): Fix operands order.
    	(define_insn "kunpcksi"): Ditto.
    	(define_insn "kunpckdi"): Ditto.
    gcc/testsuite
    	* gcc.target/i386/pr68633.c: New test.
    
    From-SVN: r231359

---
+++ gcc/testsuite/gcc.target/i386/pr68633.c
@@ -0,0 +1,24 @@
+/* { dg-do run } */
+/* { dg-options "-Ofast -mavx512f" } */
+/* { dg-require-effective-target avx512f } */
+
+#include <immintrin.h>
+
+#define AVX512F
+
+#include "avx512f-helper.h"
+
+void abort ();
+
+void
+TEST ()
+{
+  __mmask16 k1, k2, k3;
+
+  __asm__( "kmovw %1, %0" : "=k" (k1) : "r" (1) );
+  __asm__( "kmovw %1, %0" : "=k" (k2) : "r" (2) );
+
+  k3 = _mm512_kunpackb (k1, k2);
+  if (k3 != 0x201)
+    abort ();
+}
