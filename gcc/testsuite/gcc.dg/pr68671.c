c78b554f6b022ba35b4e27b2148183285739e3dc
Jakub Jelinek <jakub@redhat.com>
2015-12-07 09:58:24 +0100

    backport: re PR tree-optimization/68671 (gcc.dg/torture/pr66952.c FAILs with -fno-tree-dce)
    	Backported from mainline
    	2015-12-04  Jakub Jelinek  <jakub@redhat.com>
    
    	PR tree-optimization/68671
    	* tree-ssa-reassoc.c (maybe_optimize_range_tests): For basic
    	blocks starting with the successor of first bb we've modified
    	and ending with last_bb call reset_flow_sensitive_info_in_bb.
    
    	* gcc.dg/pr68671.c: New test.
    
    From-SVN: r231356

---
+++ gcc/testsuite/gcc.dg/pr68671.c
@@ -0,0 +1,23 @@
+/* PR tree-optimization/68671 */
+/* { dg-do run } */
+/* { dg-options " -O2 -fno-tree-dce" } */
+
+volatile int a = -1;
+volatile int b;
+
+static inline int
+fn1 (signed char p1, int p2)
+{
+  return (p1 < 0) || (p1 > (1 >> p2)) ? 0 : (p1 << 1);
+}
+
+int
+main ()
+{
+  signed char c = a;
+  b = fn1 (c, 1);
+  c = ((128 | c) < 0 ? 1 : 0);
+  if (c != 1)
+    __builtin_abort ();
+  return 0;
+}
