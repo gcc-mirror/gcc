3c6824f5fe3f41874644b99068008142ee342ee5
msebor <msebor@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-09 01:35:59 +0000

    Fix PR c++/68711 - [6 regression] SEGV on an invalid offsetof of a member of a virtual base.
    gcc/testsuite/ChangeLog:
    	* g++.dg/other/offsetof8.C: New test.
    
    gcc/cp/ChangeLog:
    	* typeck.c (build_class_member_access_expr): Strip NOPs before
            testing a potentially null operand for equality to zero.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@231437 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/other/offsetof8.C
@@ -0,0 +1,12 @@
+// PR c++/68711 - [5 regression] SEGV on an invalid offsetof of a member
+//                of a virtual base
+// { dg-do compile }
+
+struct A { int i; };
+
+struct B: virtual A { };
+
+int a[]  = {
+  !&((B*)0)->i,    // { dg-error "invalid access to non-static data member" }
+  __builtin_offsetof (B, i)   // { dg-error "invalid access to non-static" }
+};
