7c5f556750802b91d68cb7f5e548eb35a82eb224
jbeulich <jbeulich@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-11 15:53:24 +0000

    avoid alignment of static variables affecting stack's
    Function (or more narrow) scope static variables (as well as others not
    placed on the stack) should also not have any effect on the stack
    alignment. I noticed the issue first with Linux'es dynamic_pr_debug()
    construct using an 8-byte aligned sub-file-scope local variable.
    
    According to my checking bad behavior started with 4.6.x (4.5.3 was
    still okay), but generated code got quite a bit worse as of 4.9.0.
    
    gcc/
    2015-12-11  Jan Beulich  <jbeulich@suse.com>
    
    	* cfgexpand.c (expand_one_var): Exit early for static and
    	external variables when adjusting stack alignment related.
    
    gcc/testsuite/
    2015-12-11  Jan Beulich  <jbeulich@suse.com>
    
    	* gcc.c-torture/execute/stkalign.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@231569 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/execute/stkalign.c
@@ -0,0 +1,26 @@
+/* { dg-options "-fno-inline" } */
+
+#include <assert.h>
+
+#define ALIGNMENT 64
+
+unsigned test(unsigned n, unsigned p)
+{
+  static struct { char __attribute__((__aligned__(ALIGNMENT))) c; } s;
+  unsigned x;
+
+  assert(__alignof__(s) == ALIGNMENT);
+  asm ("" : "=g" (x), "+m" (s) : "0" (&x));
+
+  return n ? test(n - 1, x) : (x ^ p);
+}
+
+int main (int argc, char *argv[] __attribute__((unused)))
+{
+  unsigned int x = test(argc, 0);
+
+  x |= test(argc + 1, 0);
+  x |= test(argc + 2, 0);
+
+  return !(x & (ALIGNMENT - 1));
+}
