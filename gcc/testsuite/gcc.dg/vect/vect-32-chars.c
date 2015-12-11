f66a5fd5e6d07d074fb2b886615a81340d535a3d
ienkovich <ienkovich@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-11 09:43:52 +0000

    gcc/
    	* config/i386/i386.c (ix86_get_mask_mode): Use scalar
    	modes for 32 and 16 byte boolean vectors when possible.
    
    gcc/testsuite/
    
    	* gcc.dg/vect/vect-32-chars.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@231553 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/vect-32-chars.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-additional-options "-mavx512bw -mavx512vl" { target { i?86-*-* x86_64-*-* } } } */
+
+char a[32];
+char b[32];
+char c[32];
+
+void test()
+{
+  int i = 0;
+  for (i = 0; i < 32; i++)
+    if (b[i] > 0)
+      a[i] = c[i];
+}
+
+/* { dg-final { scan-tree-dump "vectorized 1 loops" "vect" { target { i?86-*-* x86_64-*-* } } } } */
