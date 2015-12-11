da0dcab1848db83728e0549c14bdacfc47321f5a
Dominik Vogt <vogt@linux.vnet.ibm.com>
2015-12-11 11:16:13 +0000

    S/390: Fix warnings in "*setmem_long..." patterns.
    gcc/ChangeLog
    
    	* config/s390/s390.c (s390_expand_setmem): Use new expanders.
    	* config/s390/s390.md ("*setmem_long")
    	("*setmem_long_and", "*setmem_long_31z"): Fix warnings.
    	("*setmem_long_and_31z"): New define_insn.
    	("setmem_long_<P:mode>"): New expanders.
    	* (<modesize>): New mode attribute
    
    gcc/testsuite/ChangeLog
    
    	* gcc.target/s390/md/setmem_long-1.c: New test.
    
    From-SVN: r231558

---
+++ gcc/testsuite/gcc.target/s390/md/setmem_long-1.c
@@ -0,0 +1,56 @@
+/* Machine description pattern tests.  */
+
+/* { dg-do run } */
+/* { dg-options "-mmvcle -dP -save-temps" } */
+
+__attribute__ ((noinline))
+void test(char *p, char c, int len)
+{
+  __builtin_memset(p, c, len);
+}
+
+__attribute__ ((noinline))
+void test2(char *p, int c, int len)
+{
+  __builtin_memset(p, (char)c, len);
+}
+
+/* Check that the right patterns are used.  */
+/* { dg-final { scan-assembler-times {c:9 .*{[*]setmem_long_?3?1?z?}} 1 } } */
+/* { dg-final { scan-assembler-times {c:15 .*{[*]setmem_long_and_?3?1?z?}} 1 { xfail *-*-* } } } */
+
+#define LEN 500
+char buf[LEN + 2];
+
+void init_buf(void)
+{
+  int i;
+
+  buf[0] = 0;
+  for (i = 1; i <= LEN; i++)
+    buf[i] = (0x10 + (i & 0x3f));
+  buf[LEN + 1] = 0x7f;
+}
+
+void validate_buf(char val)
+{
+  int i;
+
+  if (buf[0] != 0)
+    __builtin_abort();
+  for (i = 1; i <= LEN; i++)
+    if (buf[i] != val)
+      __builtin_abort();
+  if (buf[LEN + 1] != 0x7f)
+    __builtin_abort();
+}
+
+int main(void)
+{
+  init_buf();
+  test(buf + 1, 55, LEN);
+  validate_buf(55);
+  init_buf();
+  test(buf + 1, 66, LEN);
+  validate_buf(66);
+}
