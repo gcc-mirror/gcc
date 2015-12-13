378682db1f54eb74723308c9d308af26575c8cc5
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-13 12:51:02 +0000

    [PATCH] m68k testsuite TLC
    	* gcc.target/m68k/pr63347.c: Remove #include <stdlib> add -w to
    	command line options.
    	* gcc.target/m68k/20090709-1.c: Adjust expected output.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@231596 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/m68k/20090709-1.c
+++ gcc/testsuite/gcc.target/m68k/20090709-1.c
@@ -1,11 +1,11 @@
 /* { dg-do compile } */
 /* There should be 3 occurrences of .LC0 in the code:
    one for the definition of "0",
-   one for use in test1() and
    one for use in test2().
+   The occurrence in test1 is collapsed to an integer constant
    FIXME: At the moment m68k GCC does not optimize test1() to nop
    for some reason.  */
-/* { dg-final { scan-assembler-times ".LC0" 3 } } */
+/* { dg-final { scan-assembler-times ".LC0" 2 } } */
 
 void dummy(char *arg);
 
