7ca909c695cf94388d30d13e46d829ae0bcea84d
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-14 16:59:05 +0000

    PR c/68833 	* common.opt (Wmissing-noreturn): Add Warning option. 	* opts-common.c (control_warning_option): If opt is 	alias_target with alias_arg, set arg to it.
    	* c.opt (Wmissing-format-attribute, Wnormalized): Add Warning option.
    
    	* c-c++-common/pr68833-1.c: New test.
    	* c-c++-common/pr68833-2.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@231624 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/opts-common.c
+++ gcc/opts-common.c
@@ -1361,7 +1361,13 @@ control_warning_option (unsigned int opt_index, int kind, const char *arg,
 			diagnostic_context *dc)
 {
   if (cl_options[opt_index].alias_target != N_OPTS)
-    opt_index = cl_options[opt_index].alias_target;
+    {
+      gcc_assert (!cl_options[opt_index].cl_separate_alias
+		  && !cl_options[opt_index].cl_negative_alias);
+      if (cl_options[opt_index].alias_arg)
+	arg = cl_options[opt_index].alias_arg;
+      opt_index = cl_options[opt_index].alias_target;
+    }
   if (opt_index == OPT_SPECIAL_ignore)
     return;
   if (dc)
---
+++ gcc/testsuite/c-c++-common/pr68833-1.c
@@ -0,0 +1,22 @@
+/* PR c/68833 */
+/* { dg-do compile } */
+/* { dg-options "-Werror=larger-than-65536 -Werror=format -Werror=missing-noreturn" } */
+
+int a[131072];	/* { dg-error "size of 'a' is \[1-9]\[0-9]* bytes" } */
+int b[1024];	/* { dg-bogus "size of 'b' is \[1-9]\[0-9]* bytes" } */
+
+void
+f1 (const char *fmt)
+{
+  __builtin_printf ("%d\n", 1.2);	/* { dg-error "expects argument of type" } */
+  __builtin_printf (fmt, 1.2);		/* { dg-bogus "format not a string literal, argument types not checked" } */
+}
+
+extern void f2 (void);
+void
+f2 (void) /* { dg-error "candidate for attribute 'noreturn'" "detect noreturn candidate" } */
+{
+  __builtin_exit (0);
+}
+
+/* { dg-prune-output "treated as errors" } */
