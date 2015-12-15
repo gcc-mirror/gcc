3ce38f68f93279b193ecb91cdc3635db6f9c810f
Tim Shen <timshen@google.com>
2015-12-15 04:50:29 +0000

    re PR libstdc++/68863 (Regular expressions: Backreferences don't work in negative lookahead)
    	PR libstdc++/68863
    	* include/bits/regex_executor.tcc (_Executor::_M_lookahead):
    	Copy the captured content for lookahead, so that the backreferences
    	inside can refer to them.
    	* testsuite/28_regex/algorithms/regex_match/ecma/char/68863.cc:
    	New testcase.
    
    From-SVN: r231641

---
+++ libstdc++-v3/testsuite/28_regex/algorithms/regex_match/ecma/char/68863.cc
@@ -0,0 +1,43 @@
+// { dg-options "-std=gnu++11" }
+
+// Copyright (C) 2015 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 28.11.2 regex_match
+
+#include <regex>
+#include <testsuite_hooks.h>
+#include <testsuite_regex.h>
+
+using namespace __gnu_test;
+using namespace std;
+
+// libstdc++/68863
+void
+test01()
+{
+  bool test __attribute__((unused)) = true;
+
+  VERIFY(!std::regex_match("aa", std::regex("(.)(?!\\1).")));
+}
+
+int
+main()
+{
+  test01();
+  return 0;
+}
