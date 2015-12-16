36e24655ddb9e88ec3718e3e4e9dddc7dad1016c
Kirill Yukhin <kirill.yukhin@intel.com>
2015-12-16 13:43:56 +0000

    i386-c.c (ix86_target_macros_internal): Remove duplicate check (__CLZERO__).
    gcc/
    	* config/i386/i386-c.c (ix86_target_macros_internal): Remove
    	duplicate check (__CLZERO__).
    
    From-SVN: r231688

--- gcc/config/i386/i386-c.c
+++ gcc/config/i386/i386-c.c
@@ -439,8 +439,6 @@ ix86_target_macros_internal (HOST_WIDE_INT isa_flag,
     def_or_undef (parse_in, "__CLWB__");
   if (isa_flag & OPTION_MASK_ISA_MWAITX)
     def_or_undef (parse_in, "__MWAITX__");
-  if (isa_flag & OPTION_MASK_ISA_CLZERO)
-    def_or_undef (parse_in, "__CLZERO__");
   if (TARGET_IAMCU)
     {
       def_or_undef (parse_in, "__iamcu");
