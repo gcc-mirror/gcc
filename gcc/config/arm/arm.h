252e03b519637f57b482bf1da6c90357e1b05f61
Matthew Wahab <matthew.wahab@arm.com>
2015-12-16 11:45:25 +0000

    [ARM] Add support for ARMv8.1.
    	* config/arm/arm-arches.def: Add "armv8.1-a" and "armv8.1-a+crc".
    	* config/arm/arm-protos.h (FL2_ARCH8_1): New.
    	(FL2_FOR_ARCH8_1A): New.
    	* config/arm/arm-tables.opt: Regenerate.
    	* config/arm/arm.c (arm_arch8_1): New.
    	(arm_option_override): Set arm_arch8_1.
    	* config/arm/arm.h (TARGET_NEON_RDMA): New.
    	(arm_arch8_1): Declare.
    	* doc/invoke.texi (ARM Options, -march): Add "armv8.1-a" and
    	"armv8.1-a+crc".
    	(ARM Options, -mfpu): Fix a typo.
    
    From-SVN: r231678

--- gcc/config/arm/arm.h
+++ gcc/config/arm/arm.h
@@ -218,6 +218,9 @@ extern void (*arm_lang_output_object_attributes_hook)(void);
   (TARGET_32BIT && TARGET_HARD_FLOAT && TARGET_VFP			\
    && ARM_FPU_FSET_HAS (TARGET_FPU_FEATURES, FPU_FL_NEON))
 
+/* FPU supports ARMv8.1 Adv.SIMD extensions.  */
+#define TARGET_NEON_RDMA (TARGET_NEON && arm_arch8_1)
+
 /* Q-bit is present.  */
 #define TARGET_ARM_QBIT \
   (TARGET_32BIT && arm_arch5e && (arm_arch_notm || arm_arch7))
@@ -436,6 +439,9 @@ extern int arm_arch7em;
 
 /* Nonzero if this chip supports the ARM Architecture 8 extensions.  */
 extern int arm_arch8;
+
+/* Nonzero if this chip supports the ARM Architecture 8.1 extensions.  */
+extern int arm_arch8_1;
 
 /* Nonzero if this chip can benefit from load scheduling.  */
 extern int arm_ld_sched;
