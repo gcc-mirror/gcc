1408479363d5f34b306c75f60a6dfbdeb5be0544
Kyrylo Tkachov <kyrylo.tkachov@arm.com>
2015-12-16 10:01:48 +0000

    [ARM] PR target/68648: Fold NOT of CONST_INT in andsi_iorsi3_notsi splitter
    	PR target/68648
    	* config/arm/arm.md (*andsi_iorsi3_notsi): Try to simplify
    	the complement of operands[3] during splitting.
    
    	* gcc.c-torture/execute/pr68648.c: New test.
    
    From-SVN: r231675

---
+++ gcc/testsuite/gcc.c-torture/execute/pr68648.c
@@ -0,0 +1,20 @@
+int __attribute__ ((noinline))
+foo (void)
+{
+  return 123;
+}
+
+int __attribute__ ((noinline))
+bar (void)
+{
+  int c = 1;
+  c |= 4294967295 ^ (foo () | 4073709551608);
+  return c;
+}
+
+int
+main ()
+{
+  if (bar () != 0x83fd4005)
+    __builtin_abort ();
+}
