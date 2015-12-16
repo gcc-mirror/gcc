a10704e17a43ec70076c9c54f68f67dfca209c45
David Malcolm <dmalcolm@redhat.com>
2015-12-16 17:34:27 +0000

    C FE: fix range of primary-expression in c_parser_postfix_expression
    gcc/c/ChangeLog:
    	* c-parser.c (c_parser_postfix_expression): Use EXPR_LOC_OR_LOC
    	to preserve range information for the primary expression
    	in the call to c_parser_postfix_expression_after_primary.
    
    gcc/testsuite/ChangeLog:
    	* gcc.dg/cast-function-1.c (bar): Update column numbers.
    	* gcc.dg/diagnostic-range-bad-called-object.c: New test case.
    
    From-SVN: r231709

---
+++ gcc/testsuite/gcc.dg/diagnostic-range-bad-called-object.c
@@ -0,0 +1,24 @@
+/* { dg-options "-fdiagnostics-show-caret" } */
+
+/* Adapted from https://gcc.gnu.org/wiki/ClangDiagnosticsComparison */
+
+void call_of_non_function_ptr (char **argP, char **argQ)
+{
+  (argP - argQ)(); /* { dg-error "called object is not a function or function pointer" } */
+
+/* { dg-begin-multiline-output "" }
+   (argP - argQ)();
+   ~~~~~~^~~~~~~
+   { dg-end-multiline-output "" } */
+
+  argP();       /* { dg-error "called object 'argP' is not a function or function pointer" } */
+
+/* { dg-begin-multiline-output "" }
+   argP();
+   ^~~~
+   { dg-end-multiline-output "" }
+   { dg-begin-multiline-output "" }
+ void call_of_non_function_ptr (char **argP, char **argQ)
+                                       ^~~~
+   { dg-end-multiline-output "" } */
+}
