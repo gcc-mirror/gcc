9393bc31a2a33c7662fe9d11afb059da3b46c2a6
Michael Meissner <meissner@linux.vnet.ibm.com>
2015-12-17 01:01:24 +0000

    re PR target/68805 (ICE while var-tracking in simplify_binary_operation_1 with -g and -mvsx-timode)
    [gcc]
    2015-12-15  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	PR target/68805
    	* config/rs6000/rs6000.c (rs6000_gen_le_vsx_permute): Use ROTATE
    	instead of VEC_SELECT for TImode.
    
    	* config/rs6000/vsx.md (VSX_LE): Move TImode from VSX_LE to
    	VSX_LE_128, so that we use ROTATE to swap the 64-bit words instead
    	of using VEC_SELECT.
    	(VSX_LE_128): Likewise.
    	(define_peephole2): Add peephole to eliminate double xxpermdi when
    	copying TImode.
    
    [gcc/testsuite]
    2015-12-15  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	PR target/68805
    	* gcc.target/powerpc/pr68805.c: New test.
    
    From-SVN: r231732

---
+++ gcc/testsuite/gcc.target/powerpc/pr68805.c
@@ -0,0 +1,15 @@
+/* { dg-do compile { target powerpc64le-*-* } } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power8" } } */
+/* { dg-options "-O2 -mvsx-timode -mcpu=power8" } */
+
+typedef struct bar {
+  void *a;
+  void *b;
+} TYPE;
+
+void foo (TYPE *p, TYPE *q) { *p = *q; }
+
+/* { dg-final { scan-assembler     "lxvd2x"   } } */
+/* { dg-final { scan-assembler     "stxvd2x"  } } */
+/* { dg-final { scan-assembler-not "xxpermdi" } } */
+
