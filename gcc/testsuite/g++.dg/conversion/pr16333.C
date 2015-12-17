f85e1317f8ea933f5c615680353bd646f480f7d3
Patrick Palka <ppalka@gcc.gnu.org>
2015-12-17 04:01:47 +0000

    Fix some blockers of PR c++/24666 (arrays decay to pointers too early)
    gcc/cp/ChangeLog:
    
    	PR c++/16333
    	PR c++/41426
    	PR c++/59878
    	PR c++/66895
    	* typeck.c (convert_for_initialization): Don't perform an early
    	decaying conversion if converting to a class type.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/16333
    	PR c++/41426
    	PR c++/59878
    	PR c++/66895
    	* g++.dg/conversion/pr16333.C: New test.
    	* g++.dg/conversion/pr41426.C: New test.
    	* g++.dg/conversion/pr59878.C: New test.
    	* g++.dg/conversion/pr66895.C: New test.
    
    From-SVN: r231736

---
+++ gcc/testsuite/g++.dg/conversion/pr16333.C
@@ -0,0 +1,10 @@
+// PR c++/16333
+
+struct X {
+   X (const int (&)[3]);
+};
+
+int a[3];
+X foo1 () { return a; }
+const X &foo2 () { return a; } // { dg-warning "returning reference to temporary" }
+X &foo3 () { return a; } // { dg-error "invalid initialization" }
---
+++ gcc/testsuite/g++.dg/conversion/pr41426.C
@@ -0,0 +1,40 @@
+// PR c++/41426
+
+template <typename _T>
+struct A
+{
+   template <int _N>
+       A(_T (&V)[_N]);
+   A();
+};
+
+A<float> g1()
+{
+   float f[] = {1.1f, 2.3f};
+   return f;
+}
+
+const A<float> &g3()
+{
+   float f[] = {1.1f, 2.3f};
+   return f; // { dg-warning "returning reference to temporary" }
+}
+
+A<float> &g4()
+{
+   float f[] = {1.1f, 2.3f};
+   return f; // { dg-error "invalid initialization" }
+}
+
+struct B
+{
+   B (int (&v)[10]);
+   B();
+};
+
+B g2()
+{
+   int c[10];
+   return c;
+}
+
