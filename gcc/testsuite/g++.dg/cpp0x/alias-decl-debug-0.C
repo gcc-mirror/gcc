ffebb4e7e14436c210228c3a55b8bf481cd865fa
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-17 09:23:08 +0000

    Only support -gstabs on Mac OS X if assember supports it (PR target/67973)
    	gcc:
    	PR target/67973
    	* configure.ac (gcc_cv_as_stabs_directive): New test.
    	* configure: Regenerate.
    	* config.in: Regenerate.
    	* config/darwin.h (DBX_DEBUGGING_INFO): Wrap in
    	HAVE_AS_STABS_DIRECTIVE.
    	(PREFERRED_DEBUGGING_TYPE): Likewise.
    	* config/i386/darwin.h (PREFERRED_DEBUGGING_TYPE): Only include
    	DBX_DEBUG if HAVE_AS_STABS_DIRECTIVE.
    
    	* doc/sourcebuild.texi (Effective-Target Keywords, Environment
    	attributes): Document stabs.
    
    	gcc/testsuite:
    	PR target/67973
    	* lib/target-supports.exp (check_effective_target_stabs): New
    	proc.
    	* g++.dg/cpp0x/alias-decl-debug-0.C: Restrict to stabs targets.
    	* g++.dg/other/PR23205.C: Likewise.
    	* g++.dg/other/pr23205-2.C: Likewise.
    	* gcc.dg/20040813-1.c: Likewise.
    	* gcc.dg/darwin-20040809-2.c: Likewise.
    	* objc.dg/stabs-1.m: Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@231747 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp0x/alias-decl-debug-0.C
+++ gcc/testsuite/g++.dg/cpp0x/alias-decl-debug-0.C
@@ -1,6 +1,5 @@
 // Origin: PR c++/51032
-// { dg-do compile { target c++11 } }
-// { dg-skip-if "No stabs" { aarch64*-*-* mmix-*-* *-*-aix* alpha*-*-* hppa*64*-*-* ia64-*-* *-*-vxworks* nios2-*-* } { "*" } { "" } }
+// { dg-do compile { target { c++11 && stabs } } }
 // { dg-options "-gstabs+" }
 
 template <class C>
--- gcc/testsuite/gcc.dg/20040813-1.c
+++ gcc/testsuite/gcc.dg/20040813-1.c
@@ -1,8 +1,7 @@
 /* Test lang in N_SO stab.  */
 /* Contributed by Devang Patel  <dpatel@apple.com>  */
 
-/* { dg-do compile } */
-/* { dg-skip-if "No stabs" { aarch64*-*-* mmix-*-* *-*-aix* alpha*-*-* hppa*64*-*-* ia64-*-* tile*-*-* nios2-*-* *-*-vxworks* nvptx-*-* } { "*" } { "" } } */
+/* { dg-do compile { target stabs } } */
 /* { dg-options "-gstabs" } */
 
 int
