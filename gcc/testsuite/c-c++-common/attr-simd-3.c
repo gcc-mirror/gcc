04a60250629dcdf27f8566dfedb05eaf996e266e
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-18 14:27:45 +0000

    * c-c++-common/attr-simd-3.c: Requires pthreads.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@231816 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/c-c++-common/attr-simd-3.c
+++ gcc/testsuite/c-c++-common/attr-simd-3.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-fcilkplus" } */
 /* { dg-prune-output "undeclared here \\(not in a function\\)|\[^\n\r\]* was not declared in this scope" } */
+/* { dg-require-effective-target pthread } */
 
 void f () __attribute__((__simd__, __vector__)); /* { dg-error "in the same function marked as a Cilk Plus" "PR68158" } */
