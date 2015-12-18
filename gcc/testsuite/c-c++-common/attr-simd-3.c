1ffaae391f9c0df90f6f9d0874144124463a757e
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-18 21:14:58 +0000

    * lib/target-supports.exp (check_effective_target_cilkplus): Not nvptx. 	* c-c++-common/attr-simd-3.c: Require cilkplus. 	* gcc.dg/graphite/id-28.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@231838 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/c-c++-common/attr-simd-3.c
+++ gcc/testsuite/c-c++-common/attr-simd-3.c
@@ -1,6 +1,5 @@
-/* { dg-do compile } */
+/* { dg-do compile { target cilkplus } } */
 /* { dg-options "-fcilkplus" } */
 /* { dg-prune-output "undeclared here \\(not in a function\\)|\[^\n\r\]* was not declared in this scope" } */
-/* { dg-require-effective-target pthread } */
 
 void f () __attribute__((__simd__, __vector__)); /* { dg-error "in the same function marked as a Cilk Plus" "PR68158" } */
