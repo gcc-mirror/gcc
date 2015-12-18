5c846df71f33216fa8b5ccf21e88ee64b58b678a
Yuri Rumyantsev <ysrumyan@gmail.com>
2015-12-18 10:16:56 +0000

    re PR tree-optimization/68906 (ICE at -O3 on x86_64-linux-gnu: verify_ssa failed)
    gcc/
    
    2015-12-18  Yuri Rumyantsev  <ysrumyan@gmail.com>
    
    	PR tree-optimization/68906
    	* tree-ssa-loop-unswitch.c (tree_unswitch_outer_loop): Add check
    	that an exit block belongs to LOOP.
    
    gcc/testsuite/
    
    2015-12-18  Yuri Rumyantsev  <ysrumyan@gmail.com>
    
    	PR tree-optimization/68906
    	* gcc.dg/torture/pr68906.c: New test.
    
    From-SVN: r231812

--- gcc/tree-ssa-loop-unswitch.c
+++ gcc/tree-ssa-loop-unswitch.c
@@ -429,9 +429,9 @@ tree_unswitch_outer_loop (struct loop *loop)
   gcc_assert (loop->inner);
   if (loop->inner->next)
     return false;
-  /* Accept loops with single exit only.  */
+  /* Accept loops with single exit only which is not from inner loop.  */
   exit = single_exit (loop);
-  if (!exit)
+  if (!exit || exit->src->loop_father != loop)
     return false;
   /* Check that phi argument of exit edge is not defined inside loop.  */
   if (!check_exit_phi (loop))
