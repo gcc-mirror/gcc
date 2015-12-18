6ceddcd7b87911ddbb942923722af5a735dacedc
bernds <bernds@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-18 19:48:26 +0000

    Ignore -fdebug-prefix-map in producer string (by Daniel Kahn Gillmor)
    	* dwarf2out.c (gen_producer_string): Ignore -fdebug-prefix-map.
    
    testsuite/
    	* gcc.dg/debug/dwarf2/prod-options.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@231835 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/debug/dwarf2/prod-options.c
@@ -0,0 +1,12 @@
+/* Verify that the DW_AT_producer does not contain certain compiler options
+   such as -fdebug-prefix-map=; this is undesirable since path names make
+   the build not reproducible.  Other skipped options could be tested here
+   as well.  */
+/* { dg-do compile } */
+/* { dg-options "-O2 -gdwarf -dA -fdebug-prefix-map=a=b" } */
+/* { dg-final { scan-assembler "DW_AT_producer: \"GNU C" } } */
+/* { dg-final { scan-assembler-not "debug-prefix-map" } } */
+
+void func (void)
+{
+}
