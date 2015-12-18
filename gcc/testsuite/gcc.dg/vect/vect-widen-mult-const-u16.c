0bee0ef9b10dbf100454f792f395bd06bc5df3b0
Richard Biener <rguenther@suse.de>
2015-12-18 13:52:11 +0000

    re PR testsuite/68776 (gcc.dg/vect/vect-widen-mult-const-[su]16.c failures since r226675)
    2015-12-18  Richard Biener  <rguenther@suse.de>
    
    	PR testsuite/68776
    	* tree-vect-patterns.c (struct vect_recog_func): New.
    	(vect_vect_recog_func_ptrs): Make an array of function name pairs.
    	(vect_pattern_recog_1): Print the recognized pattern name.  Return
    	true if a pattern was recognized.
    	(vect_pattern_recog): Adjust.  Stop iterating over pattern recognition
    	functions on a stmt if a pattern was recognized.
    
    	* gcc.dg/vect/vect-widen-mult-const-s16.c: Adjust.
    	* gcc.dg/vect/vect-widen-mult-const-u16.c: Likewise.
    
    From-SVN: r231815

--- gcc/testsuite/gcc.dg/vect/vect-widen-mult-const-u16.c
+++ gcc/testsuite/gcc.dg/vect/vect-widen-mult-const-u16.c
@@ -73,4 +73,4 @@ int main (void)
 
 /* { dg-final { scan-tree-dump-times "vectorized 1 loops" 3 "vect" { target vect_widen_mult_hi_to_si } } } */
 /* { dg-final { scan-tree-dump-times "vect_recog_widen_mult_pattern: detected" 2 "vect" { target vect_widen_mult_hi_to_si_pattern } } } */
-/* { dg-final { scan-tree-dump-times "pattern recognized" 2 "vect" { target vect_widen_mult_hi_to_si_pattern } } } */
+/* { dg-final { scan-tree-dump-times "widen_mult pattern recognized" 2 "vect" { target vect_widen_mult_hi_to_si_pattern } } } */
