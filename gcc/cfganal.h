e4dbb0d449e778bc810d0d627a5aaefd0d7847b1
Jan Hubicka <hubicka@ucw.cz>
2015-12-20 06:50:29 +0100

    re PR tree-optimization/65337 (LTO bootstrap failure with Ada enabled)
    	PR middle-end/65337
    	* tree-ssa-dce.c (bb_postorder): New static var.
    	(forward_edge_to_pdom): Remove.
    	(remove_dead_stmt): Instead of redirecting edges only keep an edge
    	on a path to nearest live BB.
    	(eliminate_unnecessary_stmts): Free bb_postorder.
    	* cfganal.c (dfs_find_deadend): Add START_POINTES.
    	* cfganal.h (inverted_post_order_compute): Update prototype.
    
    From-SVN: r231856

--- gcc/cfganal.h
+++ gcc/cfganal.h
@@ -62,7 +62,7 @@ extern void add_noreturn_fake_exit_edges (void);
 extern void connect_infinite_loops_to_exit (void);
 extern int post_order_compute (int *, bool, bool);
 extern basic_block dfs_find_deadend (basic_block);
-extern int inverted_post_order_compute (int *);
+extern int inverted_post_order_compute (int *, sbitmap *start_points = 0);
 extern int pre_and_rev_post_order_compute_fn (struct function *,
 					      int *, int *, bool);
 extern int pre_and_rev_post_order_compute (int *, int *, bool);
