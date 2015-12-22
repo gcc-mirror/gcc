866ff3234e8ad6cfc8ea65c2770925534e69681e
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-22 09:52:13 +0000

    * gcc.dg/torture/pr68264.c: Tweak for Solaris.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@231898 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/torture/pr68264.c
+++ gcc/testsuite/gcc.dg/torture/pr68264.c
@@ -68,14 +68,24 @@ test (void)
   TEST (cosh (d), LARGE_ERANGE);
   TEST (sinh (d), LARGE_ERANGE);
   TEST (log (d), LARGE_NEG_EDOM);
-  TEST (log2 (d), LARGE_NEG_EDOM);
+#if defined (__sun__) && defined (__unix__)
+  /* Disabled due to a bug in Solaris libm.  */
+  if (0)
+#endif
+    TEST (log2 (d), LARGE_NEG_EDOM);
   TEST (log10 (d), LARGE_NEG_EDOM);
   /* Disabled due to glibc PR 6792, fixed in Apr 2015.  */
   if (0)
     TEST (log1p (d), LARGE_NEG_EDOM);
   TEST (exp (d), POWER_ERANGE);
-  TEST (exp2 (d), POWER_ERANGE);
-  TEST (expm1 (d), POWER_ERANGE);
+#if defined (__sun__) && defined (__unix__)
+  /* Disabled due to a bug in Solaris libm.  */
+  if (0)
+#endif
+    {
+      TEST (exp2 (d), POWER_ERANGE);
+      TEST (expm1 (d), POWER_ERANGE);
+    }
   TEST (sqrt (d), LARGE_NEG_EDOM);
   TEST (pow (100.0, d), POWER_ERANGE);
   TEST (pow (i, d), POWER_ERANGE);
