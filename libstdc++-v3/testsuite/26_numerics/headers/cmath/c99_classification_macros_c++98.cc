8babc12b224cfb1744799ad4d2020522b222e1fc
Jonathan Wakely <jwakely@redhat.com>
2016-01-08 14:59:36 +0000

    Use -std option in test for C99 macros in <cmath>
    	* testsuite/26_numerics/headers/cmath/
    	c99_classification_macros_c++.cc: Rename to ...
    	* testsuite/26_numerics/headers/cmath/
    	c99_classification_macros_c++98.cc: Here and add -std=gnu++98.
    	* testsuite/26_numerics/headers/cmath/
    	c99_classification_macros_c++0x.cc: Rename to ...
    	* testsuite/26_numerics/headers/cmath/
    	c99_classification_macros_c++11.cc: Here.
    
    From-SVN: r232165

--- libstdc++-v3/testsuite/26_numerics/headers/cmath/c99_classification_macros_c++98.cc
+++ libstdc++-v3/testsuite/26_numerics/headers/cmath/c99_classification_macros_c++98.cc
@@ -17,7 +17,7 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-
+// { dg-options "-std=gnu++98" }
 // { dg-do compile { xfail uclibc } }
 // { dg-excess-errors "" { target uclibc } }
 
