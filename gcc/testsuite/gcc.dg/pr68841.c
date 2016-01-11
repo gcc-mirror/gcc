ae8718b5b852a1e6852a9fe283848df4b0db6e3e
ktkachov <ktkachov@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-01-11 12:13:50 +0000

    [RTL-ifcvt] PR rtl-optimization/68841: Make sure one basic block doesn't clobber CC reg usage of the other
            PR rtl-optimization/68841
            * ifcvt.c (struct noce_if_info): Add orig_x field.
            (bbs_ok_for_cmove_arith): Add to_rename parameter.
            Don't record conflicts on to_rename if it's present.
            Allow memory destinations in sets.
            (noce_try_cmove_arith): Call bbs_ok_for_cmove_arith even on simple
            blocks, passing orig_x to the checks.
            (noce_process_if_block): Set if_info->orig_x appropriately.
    
            * gcc.dg/pr68841.c: New test.
            * gcc.c-torture/execute/pr68841.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@232223 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr68841.c
@@ -0,0 +1,34 @@
+/* { dg-do run } */
+/* { dg-options "-Og -fif-conversion -flive-range-shrinkage -fpeel-loops -frerun-cse-after-loop" } */
+
+static inline int
+foo (int *x, int y)
+{
+  int z = *x;
+  while (y > z)
+    z *= 2;
+  return z;
+}
+
+int
+main ()
+{
+  int i;
+  for (i = 1; i < 17; i++)
+    {
+      int j;
+      int k;
+      j = foo (&i, 7);
+      if (i >= 7)
+	k = i;
+      else if (i >= 4)
+	k = 8 + (i - 4) * 2;
+      else if (i == 3)
+	k = 12;
+      else
+	k = 8;
+      if (j != k)
+	__builtin_abort ();
+    }
+  return 0;
+}
