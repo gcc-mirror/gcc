da024748bed3deeb511d0946613e309ee525061f
andris <andris@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-01-12 04:57:10 +0000

    [PATCH 5/6] [DJGPP] Update DJGPP configuration related files
           * config/i386/djgpp.h (PREFERRED_DEBUGGING_TYPE): Define to DWARF2
           (TARGET_ASM_OUTPUT_IDENT): Define to default_asm_output_ident_directive
           (MD_EXEC_PREFIX): Remove.
           (MD_STARTFILE_PREFIX) Removee.
           (FILE_NAME_ABSOLUTE_P): Remove.
           (CPP_SPEC): Do not read macros from sys/version.h.
           (LINK_COMMAND_SPEC): Remove.
           (LOCAL_INCLUDE_DIR): Remove.
           (TARGET_ASM_NAMED_SECTION): Define to i386_djgpp_asm_named_section
           (TARGET_OS_CPP_BUILTINS): Add DJGPP (non ISO only), __DJGPP, __DJGPP__, unix.
           (POST_LINK_SPEC): Define to invoke stubify after linker
           (LIBSTDCXX): Remove define
           (DBX_REGISTER_NUMBER): Define to svr4_dbx_register_map.
           (DEFAULT_PCC_STRUCT_RETURN): Define to 1.
           (SUBTARGET_OVERRIDE_OPTIONS): Remove warning about -mbnu2210.
           (SUBTARGET_OVERRIDE_OPTIONS): Ignore -fPIC and generate message.
           (SUBTARGET_OVERRIDE_OPTIONS): Default to DWARF2 debugging info.
           (IX86_MAYBE_NO_LIBGCC_TFMODE): Remove.
           (i386_djgpp_asm_named_section): Add propotype of new procedure
    
           * config/i386/xm-djgpp.h (NATIVE_SYSTEM_HEADER_DIR): Define.
           (MD_EXEC_PREFIX): Define (moved from config/i386/djgpp.h).
           (STANDARD_STARTFILE_PREFIX_1): Define (moved from MD_STARTFILE_PREFIX in config/i386/djgpp.h).
           (STANDARD_STARTFILE_PREFIX_2): Define identical to STANDARD_STARTFILE_PREFIX_1.
           (LOCAL_INCLUDE_DIR): Define (moved from config/i386/djgpp.h).
           (GCC_DRIVER_HOST_INITIALIZATION): Fix reporting fatal installation errors.
           (MAX_OFILE_ALIGNMENT): Define to 128.
           (HAVE_FTW_H): Undefine as DJGPP do not have nftw, but have ftw.h.
    
           * config/i386/djgpp.c: New file. Add implementation of i386_djgpp_asm_named_section.
    
           * config/i386/djgpp.opt: Remove obsolete option -mbnu210.
    
           * config/i386/t-djgpp: New file. Add djgpp.o to EXTRA_OBJS.
           Add rule for building djgpp.o.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@232258 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/i386/djgpp.c
@@ -0,0 +1,47 @@
+/* Subroutines for DJGPP.
+   Copyright (C) 2013-2016 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#include "config.h"
+#include "system.h"
+#include "coretypes.h"
+#include "tm.h"
+#include "output.h"
+#include "lto-section-names.h"
+
+void
+i386_djgpp_asm_named_section(const char *name, unsigned int flags,
+			     tree)
+{
+  char flagchars[8], *f = flagchars;
+
+  if (flags & SECTION_WRITE)
+    *f++ = 'w';
+  if (flags & SECTION_CODE)
+    *f++ = 'x';
+
+  /* LTO sections need 1-byte alignment to avoid confusing the
+     zlib decompression algorithm with trailing zero pad bytes.  */
+  if (strncmp (name, LTO_SECTION_NAME_PREFIX,
+			strlen (LTO_SECTION_NAME_PREFIX)) == 0)
+    *f++ = '0';
+
+  *f++ = '\0';
+
+  fprintf (asm_out_file, "\t.section\t%s,\"%s\"\n", name, flagchars);
+}
