3368873db330e6f7bbd0e751af2d753f06d97f15
nickc <nickc@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-01-12 11:43:59 +0000

    PR target/68913 	* gcc.dg/lto/pr61886_0.c: Rename the external function called 	to fread so that it will be found in all target runtimes.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@232264 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/lto/pr61886_0.c
+++ gcc/testsuite/gcc.dg/lto/pr61886_0.c
@@ -4,12 +4,15 @@
 typedef __SIZE_TYPE__ size_t;
 typedef struct _IO_FILE FILE;
 
-extern size_t __fread_chk (void *__restrict __ptr, size_t __ptrlen, size_t __size, size_t __n, FILE *__restrict __stream) __asm__ ("" "__fread_chk")      __attribute__ ((__warn_unused_result__));
-extern size_t __fread_chk_warn (void *__restrict __ptr, size_t __ptrlen, size_t __size, size_t __n, FILE *__restrict __stream) __asm__ ("" "__fread_chk")      __attribute__ ((__warn_unused_result__)) __attribute__((__warning__ ("fread called with bigger size * nmemb than length " "of destination buffer")));
+#define STRING1(a) #a
+#define STRING2(a) STRING1(a)
+
+extern size_t fread (void *__restrict __ptr, size_t __ptrlen, size_t __size, size_t __n, FILE *__restrict __stream) __asm__ (STRING2(__USER_LABEL_PREFIX__) "fread")      __attribute__ ((__warn_unused_result__));
+extern size_t fread_warn (void *__restrict __ptr, size_t __ptrlen, size_t __size, size_t __n, FILE *__restrict __stream) __asm__ (STRING2(__USER_LABEL_PREFIX__) "fread")      __attribute__ ((__warn_unused_result__)) __attribute__((__warning__ ("fread called with bigger size * nmemb than length " "of destination buffer")));
 
 extern __inline __attribute__ ((__always_inline__)) __attribute__ ((__gnu_inline__)) __attribute__ ((__artificial__)) __attribute__ ((__warn_unused_result__))
 size_t
-fread (void *__restrict __ptr, size_t __size, size_t __n,
+local_fread (void *__restrict __ptr, size_t __size, size_t __n,
        FILE *__restrict __stream)
 {
   if (__builtin_object_size (__ptr, 0) != (size_t) -1)
@@ -17,9 +20,9 @@ fread (void *__restrict __ptr, size_t __size, size_t __n,
       if (!__builtin_constant_p (__size)
           || !__builtin_constant_p (__n)
           || (__size | __n) >= (((size_t) 1) << (8 * sizeof (size_t) / 2)))
-        return __fread_chk (__ptr, __builtin_object_size (__ptr, 0), __size, __n, __stream);
+        return fread (__ptr, __builtin_object_size (__ptr, 0), __size, __n, __stream);
       if (__size * __n > __builtin_object_size (__ptr, 0))
-        return __fread_chk_warn (__ptr, __builtin_object_size (__ptr, 0), __size, __n, __stream);
+        return fread_warn (__ptr, __builtin_object_size (__ptr, 0), __size, __n, __stream);
     }
 }
 
@@ -28,6 +31,6 @@ FILE *fp;
 int main ()
 {
   char file_contents[4096];
-  /* We shouldn't get this resolved to a call to __fread_chk_warn.  */
-  return fread (file_contents, 1, nmemb, fp);
+  /* We shouldn't get this resolved to a call to fread_warn.  */
+  return local_fread (file_contents, 1, nmemb, fp);
 }
