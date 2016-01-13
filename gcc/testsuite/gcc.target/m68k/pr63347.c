46e556f77c90a1d16d5adaa9e04c84097c19b5d9
Jeff Law <law@redhat.com>
2016-01-13 15:55:31 -0700

    re PR target/63347 (m68k misoptimisation with -fschedule-insns)
    	PR target/63347
    	* haifa-sched.c (prune_ready_list): If we have a SCHED_GROUP_P insn
    	that needs to be queued, just queue it for a single cycle.
    
    	PR target/63347
    	* gcc.target/m68k/pr63347.c: New test.
    
    From-SVN: r232354

---
+++ gcc/testsuite/gcc.target/m68k/pr63347.c
@@ -0,0 +1,46 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -mcpu=5208" } */
+
+#include <stdlib.h>
+
+void __attribute__ ((noinline))
+oof()
+{
+  asm volatile ("" ::: "memory");
+}
+int print_info(unsigned int *ip_addr)
+{
+    int invalid = 0;
+
+    if (ip_addr) {
+        unsigned int haddr = *ip_addr;
+        oof("stuff");
+        if (0x0 == haddr) {
+            invalid = 1;
+        }
+        oof("stuff2");
+    } else {
+        invalid = 1;
+    }
+
+    return invalid;
+}
+
+int main(int argc, char *argv[])
+{
+    unsigned int myaddr;
+    int ret;
+
+    myaddr = 0x0;
+    ret = print_info(&myaddr);
+    if (!ret)
+        abort ();
+
+    myaddr = 0x01020304;
+    ret = print_info(&myaddr);
+    if (ret)
+        abort ();
+    exit (0);
+}
+
+
