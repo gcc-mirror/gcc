ef6d1772682f6929016159bb23a5640a9676718a
Uros Bizjak <uros@gcc.gnu.org>
2016-01-13 17:50:17 +0100

    pr45685.c (dg-options): Add -mno-sse4.
    	* gcc.target/i386/pr45685.c (dg-options): Add -mno-sse4.
    	* gcc.target/i386/pr68432-1.c (dg-options): Ditto.
    	* gcc.target/i386/pr68432-2.c (dg-options): Ditto.
    	* gcc.target/i386/pr68432-3.c (dg-options): Ditto.
    
    From-SVN: r232329

--- gcc/testsuite/gcc.target/i386/pr68432-2.c
+++ gcc/testsuite/gcc.target/i386/pr68432-2.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-Os -fno-math-errno -fno-trapping-math -msse2 -mfpmath=sse" } */
+/* { dg-options "-Os -fno-math-errno -fno-trapping-math -msse2 -mno-sse4 -mfpmath=sse" } */
 
 float
 f1 (float f)
--- gcc/testsuite/gcc.target/i386/pr68432-3.c
+++ gcc/testsuite/gcc.target/i386/pr68432-3.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -fno-math-errno -fno-trapping-math -msse2 -mfpmath=sse" } */
+/* { dg-options "-O2 -fno-math-errno -fno-trapping-math -msse2 -mno-sse4 -mfpmath=sse" } */
 
 float __attribute__ ((cold))
 f1 (float f)
