bab0ad3a30b79e086b55bdb5337a2e5cab0cf437
Nick Clifton <nickc@redhat.com>
2016-01-14 12:36:31 +0000

    * lib/target-supports.exp 	(check_effective_target_arm_neon_ok_nocache): Add an option 	sequence that includes setting the ARM architecture to ARMv7-A. 	* gcc.target/arm/attr-neon.c: Use dg-add-options to add the 	command line options necessary to enable Neon support. 	* gcc.target/arm/neon-vlshr-imm-1.c: Likewise. 	* gcc.target/arm/neon-vshl-imm-1.c: Likewise. 	* gcc.target/arm/neon-vshr-imm-1.c: Likewise. 	* gcc.target/arm/pr69180.c: Likewise.
    From-SVN: r232362

--- gcc/testsuite/gcc.target/arm/neon-vlshr-imm-1.c
+++ gcc/testsuite/gcc.target/arm/neon-vlshr-imm-1.c
@@ -1,6 +1,7 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target arm_neon_ok } */
-/* { dg-options "-O2 -mfpu=neon -mfloat-abi=softfp -ftree-vectorize" } */
+/* { dg-options "-O2 -ftree-vectorize" } */
+/* { dg-add-options arm_neon } */
 /* { dg-final { scan-assembler "vshr\.u32.*#3" } } */
 
 /* Verify that VSHR immediate is used.  */
