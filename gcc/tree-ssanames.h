40c43acacc1765416278e6636c20c5e3a78a7384
Jeff Law <law@redhat.com>
2016-01-15 15:32:05 -0700

    re PR tree-optimization/69270 (DOM should exploit range information to create more equivalences)
    	PR tree-optimization/69270
    	* tree-ssanames.c (ssa_name_has_boolean_range): Moved here from
    	tree-ssa-dom.c.  Improve test for [0..1] ranve from VRP.
    	* tree-ssa-dom.c (ssa_name_has_boolean_range): Remove.
    	* tree-ssanames.h (ssa_name_has_boolean_range): Prototype.
    	* tree-ssa-uncprop.c (associate_equivalences_with_edges): Use
    	ssa_name_has_boolean_range and constant_boolean_node.
    
    	PR tree-optimization/69270
    	* gcc.dg/tree-ssa/pr69270-2.c: New test.
    	* gcc.dg/tree-ssa/pr69270-3.c: New test.
    
    From-SVN: r232453

--- gcc/tree-ssanames.h
+++ gcc/tree-ssanames.h
@@ -75,6 +75,7 @@ extern enum value_range_type get_range_info (const_tree, wide_int *,
 					     wide_int *);
 extern void set_nonzero_bits (tree, const wide_int_ref &);
 extern wide_int get_nonzero_bits (const_tree);
+extern bool ssa_name_has_boolean_range (tree);
 extern void init_ssanames (struct function *, int);
 extern void fini_ssanames (struct function *);
 extern void ssanames_print_statistics (void);
