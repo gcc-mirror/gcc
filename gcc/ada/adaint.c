4c0f272cfce3744f4420f86c07f44ed3cc5ecda1
Jakub Jelinek <jakub@redhat.com>
2016-01-15 21:15:44 +0100

    adaint.c (__gnat_locate_exec_on_path): Use const char * instead of char * for path_val to avoid warnings.
    	* adaint.c (__gnat_locate_exec_on_path): Use const char * instead
    	of char * for path_val to avoid warnings.
    
    From-SVN: r232448

--- gcc/ada/adaint.c
+++ gcc/ada/adaint.c
@@ -2791,7 +2791,7 @@ __gnat_locate_exec_on_path (char *exec_name)
   WS2SC (apath_val, wapath_val, EXPAND_BUFFER_SIZE);
 
 #else
-  char *path_val = getenv ("PATH");
+  const char *path_val = getenv ("PATH");
 
   /* If PATH is not defined, proceed with __gnat_locate_exec anyway, so we can
      find files that contain directory names.  */
