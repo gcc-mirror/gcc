88bed19634d9419cb95ca0062e6a03405cfbaf98
Alan Lawrence <alan.lawrence@arm.com>
2016-01-18 12:29:02 +0000

    Make SRA scalarize constant-pool loads
    PR target/63679
    
    gcc/ChangeLog:
        
    	* tree-sra.c (disqualified_constants, constant_decl_p): New.
    	(sra_initialize): Allocate disqualified_constants.
    	(sra_deinitialize): Free disqualified_constants.
    	(disqualify_candidate): Update disqualified_constants when appropriate.
    	(create_access): Scan for constant-pool entries as we go along.
    	(scalarizable_type_p): Add check against type_contains_placeholder_p.
    	(maybe_add_sra_candidate): Allow constant-pool entries.
    	(load_assign_lhs_subreplacements): Bind debug for constant pool vars.
    	(initialize_constant_pool_replacements): New.
    	(sra_modify_assign): Avoid mangling assignments created by previous,
    	and don't generate writes into constant pool.
    	(sra_modify_function_body): Call initialize_constant_pool_replacements.
        
    gcc/testsuite/:
    
    	* gcc.dg/tree-ssa/sra-17.c: New.
    	* gcc.dg/tree-ssa/sra-18.c: New.
    
    From-SVN: r232506

---
+++ gcc/testsuite/gcc.dg/tree-ssa/sra-17.c
@@ -0,0 +1,19 @@
+/* { dg-do run { target { aarch64*-*-* alpha*-*-* arm*-*-* hppa*-*-* powerpc*-*-* s390*-*-* } } } */
+/* { dg-options "-O2 -fdump-tree-esra --param sra-max-scalarization-size-Ospeed=32" } */
+
+extern void abort (void);
+
+int
+main (int argc, char **argv)
+{
+  long a[4] = { 7, 19, 11, 255 };
+  int tot = 0;
+  for (int i = 0; i < 4; i++)
+    tot = (tot*256) + a[i];
+  if (tot == 0x07130bff)
+    return 0;
+  abort ();
+}
+
+/* { dg-final { scan-tree-dump-times "Removing load: a = \\\*.LC0;" 1 "esra" } } */
+/* { dg-final { scan-tree-dump-times "SR.\[0-9_\]+ = \\\*.LC0\\\[" 4 "esra" } } */
---
+++ gcc/testsuite/gcc.dg/tree-ssa/sra-18.c
@@ -0,0 +1,28 @@
+/* { dg-do run { target { aarch64*-*-* alpha*-*-* arm*-*-* hppa*-*-* powerpc*-*-* s390*-*-* } } } */
+/* { dg-options "-O2 -fdump-tree-esra --param sra-max-scalarization-size-Ospeed=32" } */
+
+extern void abort (void);
+struct foo { long x; };
+
+struct bar { struct foo f[2]; };
+
+struct baz { struct bar b[2]; };
+
+int
+main (int argc, char **argv)
+{
+  struct baz a = { { { { { 4 }, { 5 } } }, { { { 6 }, { 7 } } }  } };
+  int tot = 0;
+  for (int i = 0; i < 2; i++)
+    for (int j = 0; j < 2; j++)
+      tot = (tot*256) + a.b[i].f[j].x;
+  if (tot == 0x04050607)
+    return 0;
+  abort ();
+}
+
+/* { dg-final { scan-tree-dump-times "Removing load: a = \\\*.LC0;" 1 "esra" } } */
+/* { dg-final { scan-tree-dump-times "SR.\[0-9_\]+ = \\\*.LC0\\.b\\\[0\\\]\\.f\\\[0\\\]\\.x" 1 "esra" } } */
+/* { dg-final { scan-tree-dump-times "SR.\[0-9_\]+ = \\\*.LC0\\.b\\\[0\\\]\\.f\\\[1\\\]\\.x" 1 "esra" } } */
+/* { dg-final { scan-tree-dump-times "SR.\[0-9_\]+ = \\\*.LC0\\.b\\\[1\\\]\\.f\\\[0\\\]\\.x" 1 "esra" } } */
+/* { dg-final { scan-tree-dump-times "SR.\[0-9_\]+ = \\\*.LC0\\.b\\\[1\\\]\\.f\\\[1\\\]\\.x" 1 "esra" } } */
