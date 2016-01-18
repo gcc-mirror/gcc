fb2073310b0008a8b7e2d8cf3892c1c626eb599d
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-01-18 12:53:03 +0000

    Add oacc kernels test in libgomp
    2016-01-18  Tom de Vries  <tom@codesourcery.com>
    
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-2.c: New test.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-3.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-and-seq-2.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-and-seq-3.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-and-seq-4.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-and-seq-5.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-and-seq-6.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-and-seq.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-collapse.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-data-2.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-data-enter-exit-2.c:
    	Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-data-enter-exit.c:
    	Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-data-update.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-data.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-g.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-mod-not-zero.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-n.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-nest.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop.c: Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-parallel-loop-data-enter-exit.c:
    	Same.
    	* testsuite/libgomp.oacc-c-c++-common/kernels-reduction.c: Same.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@232515 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libgomp/testsuite/libgomp.oacc-c-c++-common/kernels-loop-nest.c
@@ -0,0 +1,26 @@
+/* { dg-do run } */
+/* { dg-additional-options "-ftree-parallelize-loops=32" } */
+
+#include <stdlib.h>
+
+#define N 1000
+
+int
+main (void)
+{
+  int x[N][N];
+
+#pragma acc kernels copyout (x)
+  {
+    for (int ii = 0; ii < N; ii++)
+      for (int jj = 0; jj < N; jj++)
+	x[ii][jj] = ii + jj + 3;
+  }
+
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
+      if (x[i][j] != i + j + 3)
+	abort ();
+
+  return 0;
+}
