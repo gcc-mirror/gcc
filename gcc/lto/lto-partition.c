d03f956e067d67fc9c6e1d8530362133632bfba1
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-01-18 15:58:06 +0000

    PR lto/69003 	* lto-partition.c (rename_statics): Fix pasto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@232525 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/lto/lto-partition.c
+++ gcc/lto/lto-partition.c
@@ -1077,8 +1077,8 @@ rename_statics (lto_symtab_encoder_t encoder, symtab_node *node)
 		  IDENTIFIER_POINTER
 		    (DECL_ASSEMBLER_NAME (s->get_alias_target()->decl))))
 	&& ((s->real_symbol_p ()
-             && !DECL_EXTERNAL (node->decl)
-	     && !TREE_PUBLIC (node->decl))
+             && !DECL_EXTERNAL (s->decl)
+	     && !TREE_PUBLIC (s->decl))
  	    || may_need_named_section_p (encoder, s))
 	&& (!encoder
 	    || lto_symtab_encoder_lookup (encoder, s) != LCC_NOT_FOUND))
