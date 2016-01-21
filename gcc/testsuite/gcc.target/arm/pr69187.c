6d31cc758baf6756d0627da19b546625709176c6
Stefan Sørensen <stefan.sorensen@spectralink.com>
2016-01-21 10:52:46 +0100

    re PR target/69187 (ICE: Aborted when native compiling neon code with __builtin_neon_vmlals_lanev4hi)
    	PR target/69187
    	PR target/65624
    	* config/arm/arm-builtins.c (arm_expand_neon_builtin): Increase
    	args array size by one to avoid buffer overflow.
    
    	* gcc.target/arm/pr69187.c: New test.
    
    From-SVN: r232668

---
+++ gcc/testsuite/gcc.target/arm/pr69187.c
@@ -0,0 +1,19 @@
+/* PR target/69187 */
+/* { dg-do compile } */
+/* { dg-require-effective-target arm_neon } */
+/* { dg-options "-O0" }  */
+/* { dg-add-options arm_neon }  */
+
+#include <arm_neon.h>
+
+int32x4_t
+foo (void)
+{
+  int32x4_t vector_int32x4;
+  int16x4_t vector3_int16x4;
+  int16x4_t vector4_int16x4;
+  static int32_t buffer_int32x4[32];
+
+  vector_int32x4 = vld1q_s32(buffer_int32x4);
+  return vqdmlsl_lane_s16(vector_int32x4, vector3_int16x4, vector4_int16x4, 0);
+}
