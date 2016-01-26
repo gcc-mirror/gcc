43d3a2e4be0df40398b6a0e11f2b9155d36f07fe
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-01-26 13:01:44 +0000

    PR lto/69254 	* opts.h (parse_sanitizer_options): New prototype. 	* opts.c (sanitizer_opts): New array. 	(parse_sanitizer_options): New function. 	(common_handle_option): Use parse_sanitizer_options.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@232826 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/opts.h
+++ gcc/opts.h
@@ -372,6 +372,8 @@ extern void control_warning_option (unsigned int opt_index, int kind,
 extern char *write_langs (unsigned int mask);
 extern void print_ignored_options (void);
 extern void handle_common_deferred_options (void);
+unsigned int parse_sanitizer_options (const char *, location_t, int,
+				      unsigned int, int, bool);
 extern bool common_handle_option (struct gcc_options *opts,
 				  struct gcc_options *opts_set,
 				  const struct cl_decoded_option *decoded,
