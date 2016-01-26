0bccf11b4db8feb26c176a8754e03c896c929f70
H.J. Lu <hongjiu.lu@intel.com>
2016-01-26 14:33:12 +0000

    Remove -m32 from gcc.target/i386/pr68986-2.c
    	* gcc.target/i386/pr68986-2.c: Remove -m32.
    
    From-SVN: r232831

--- gcc/testsuite/gcc.target/i386/pr68986-2.c
+++ gcc/testsuite/gcc.target/i386/pr68986-2.c
@@ -1,7 +1,7 @@
 /* { dg-do compile { target ia32 } } */
 /* { dg-require-effective-target tls_native } */
 /* { dg-require-effective-target fpic } */
-/* { dg-options "-fPIC -mno-accumulate-outgoing-args -mpreferred-stack-boundary=2 -m32" } */
+/* { dg-options "-fPIC -mno-accumulate-outgoing-args -mpreferred-stack-boundary=2" } */
 
 extern __thread int msgdata;
 int
