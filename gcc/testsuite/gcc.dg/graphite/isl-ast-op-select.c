557c19428ab5a8c3108bc305e1c6f0c6807d61e8
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-01-26 22:02:11 +0000

    handle isl_ast_op_select
    2016-01-26  Abderrazek Zaafrani  <a.zaafrani@samsung.com>
                Sebastian Pop  <s.pop@samsung.com>
    
    	* graphite-isl-ast-to-gimple.c (ternary_op_to_tree): Handle
    	isl_ast_op_cond and isl_ast_op_select.
    	(gcc_expression_from_isl_expr_op): Same.
    
    	* gcc.dg/graphite/isl-ast-op-select.c: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@232851 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/graphite/isl-ast-op-select.c
@@ -0,0 +1,29 @@
+/* { dg-options "-O2 -floop-nest-optimize" } */
+
+static void kernel_gemm(int ni, int nj, int nk, double alpha, double beta, double C[1024][1024], double A[1024][1024], double B[1024][1024])
+{
+ int i, j, k;
+ for (i = 0; i < ni; i++)
+   for (j = 0; j < nj; j++)
+     {
+       C[i][j] *= beta;
+       for (k = 0; k < nk; ++k)
+         C[i][j] += alpha * A[i][k] * B[k][j];
+     }
+}
+
+void *polybench_alloc_data (int, int);
+
+int main(int argc, char** argv) {
+  int ni = 1024;
+  int nj = 1024;
+  int nk = 1024;
+  double alpha;
+  double beta;
+  double (*C)[1024][1024];
+  C = (double(*)[1024][1024])polybench_alloc_data ((1024) * (1024), sizeof(double));
+  double (*A)[1024][1024];
+  A = (double(*)[1024][1024])polybench_alloc_data ((1024) * (1024), sizeof(double));
+  double (*B)[1024][1024];
+  kernel_gemm (ni, nj, nk, alpha, beta, *C, *A, *B);
+}
