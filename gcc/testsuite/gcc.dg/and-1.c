616a997ae40bd7ee2859057723ee8b44b9304b68
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2016-01-27 17:11:50 +0000

    and-1.c: Remove nand test for powerpc*-*-*.
    2016-01-27  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* gcc.dg/and-1.c: Remove nand test for powerpc*-*-*.
    
    From-SVN: r232886

--- gcc/testsuite/gcc.dg/and-1.c
+++ gcc/testsuite/gcc.dg/and-1.c
@@ -1,8 +1,8 @@
 /* { dg-do compile } */
 /* { dg-options "-O2" } */
 /* { dg-final { scan-assembler "and" { target powerpc*-*-* spu-*-* } } } */
-/* There should be no nand for this testcase (for either PPC or SPU). */
-/* { dg-final { scan-assembler-not "nand" { target powerpc*-*-* spu-*-* } } } */
+/* There should be no nand for this testcase for SPU. */
+/* { dg-final { scan-assembler-not "nand" { target spu-*-* } } } */
 
 int f(int y)
 {
