5068be18dbe70f425a32c81190682cee24d7ce58
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-01-27 17:42:47 +0000

    gcc/: 	* common.opt (fkeep-gc-roots-live): New undocumented option. 	* tree-ssa-loop-ivopts.c (add_candidate_1): If 	-fkeep-gc-roots-live, skip pointers. 	(add_iv_candidate_for_biv): Handle add_candidate_1 returning 	NULL.
    gcc/testsuite/:
    	* gcc.dg/tree-ssa/ivopt_5.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@232888 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ivopt_5.c
@@ -0,0 +1,23 @@
+/* { dg-options "-O2 -fdump-tree-ivopts -fkeep-gc-roots-live" } */
+
+/* Only integer ivopts here when using -fkeep-gc-roots-live.   */
+
+void foo (char *pstart, int n)
+{
+  char *p;
+  char *pend = pstart + n;
+
+  for (p = pstart; p < pend; p++)
+    *p = 1;
+}
+
+void foo1 (char *pstart, int n)
+{
+  char *p;
+  char *pend = pstart + n;
+
+  for (p = pstart; p != pend; p++)
+    *p = 1;
+}
+
+/* { dg-final { scan-tree-dump-times "ivtmp.\[0-9_\]* = PHI <\[^0\]" 0 "ivopts"} } */
