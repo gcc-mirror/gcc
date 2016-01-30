343c05d39f7fcdf55178853884113715bc7bcc2b
Martin Sebor <msebor@redhat.com>
2016-01-30 17:30:32 +0000

    PR r++/68490 - error initializing a structure with a flexible array membe
    gcc/testsuite/ChangeLog:
    2016-01-30  Martin Sebor  <msebor@redhat.com>                                   
                                                                                    
            PR c++/68490                                                            
            * g++.dg/ext/flexary10.C: New test.
    
    From-SVN: r233010

---
+++ gcc/testsuite/g++.dg/ext/flexary10.C
@@ -0,0 +1,18 @@
+// PR c++/68490 - error initializing a structure with a flexible array member
+// { dg-do compile }
+// { dg-options "-Wpedantic" }
+
+struct A {
+  int n;
+  int a [];
+};
+
+struct A foo (void)
+{
+  // Verify the initializer below is accepted for compatibility with gcc
+  // (in C mode).
+  static struct A
+    a = { 2, { 1, 0 } };   // { dg-warning "initialization of a flexible array member" }
+
+  return a;
+}
