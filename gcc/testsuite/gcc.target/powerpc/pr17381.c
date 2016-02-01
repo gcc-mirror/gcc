aaf527dce118caf3411129e45bd2c82b19bbd4a5
msebor <msebor@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-02-01 19:38:53 +0000

    Remove duplicate copy of the test body inadvertently inserted by patch.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@233045 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/pr17381.c
+++ gcc/testsuite/gcc.target/powerpc/pr17381.c
@@ -9,14 +9,3 @@ float test1(float fParm)
   return fParm + 1.0f;
 }
 /* { dg-final { scan-assembler-times "fmr" 1 } } */
-/* PR target/17381 - Unnecessary register move for float extend */
-/* { dg-do compile } */
-/* { dg-options "-O2" } */
-
-double d;
-float test1(float fParm)
-{
-  d = fParm + 1.0;
-  return fParm + 1.0f;
-}
-/* { dg-final { scan-assembler-times "fmr" 1 } } */
