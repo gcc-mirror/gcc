415d564103942cc410a6439b44431505a7f09bb9
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-02-03 22:40:22 +0000

    PR c/69627 	* diagnostic-show-locus.c (layout::get_state_at_point): Don't read 	range->m_caret fields if range->m_show_caret_p is false.
    	* gcc.dg/pr69627.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@233114 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr69627.c
@@ -0,0 +1,27 @@
+/* PR c/69627 */
+/* { dg-do compile } */
+/* { dg-options "-fdiagnostics-show-caret" } */
+
+void
+foo ()
+{
+  float t[2] = { 1, 2 };
+  int const *s = 0;
+  t[1] / s;	/* { dg-error "invalid operands to binary /" } */
+/* { dg-begin-multiline-output "" }
+   t[1] / s;
+   ~~~~ ^
+   { dg-end-multiline-output "" } */
+}
+
+void
+bar ()
+{
+  float t[2] = { 1, 2 };
+  int const *s[2] = { 0, 0 };
+  t[1] / s[0];	/* { dg-error "invalid operands to binary /" } */
+/* { dg-begin-multiline-output "" }
+   t[1] / s[0];
+   ~~~~ ^ ~~~~
+   { dg-end-multiline-output "" } */
+}
