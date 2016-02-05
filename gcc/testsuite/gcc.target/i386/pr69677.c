f5d4b899a5df093cb92a89dcb29ec93fb3bfe8ce
H.J. Lu <hongjiu.lu@intel.com>
2016-02-05 16:24:06 +0000

    Add a testcase for PR target/69677
    	PR target/69677
    	* gcc.target/i386/pr69677.c: New test.
    
    From-SVN: r233180

---
+++ gcc/testsuite/gcc.target/i386/pr69677.c
@@ -0,0 +1,20 @@
+/* { dg-do compile { target { ia32 } } } */
+/* { dg-options "-O2 -mno-avx -march=corei7 -fdump-rtl-final" } */
+
+extern const unsigned int a[];
+extern const unsigned long long b[];
+
+int
+fn1 (unsigned int p1, unsigned long long p2)
+{
+  unsigned int p3;
+
+  p3 = a[p1];
+  if (p3 == 0 || p3 > 64)
+    return 0;
+
+  p2 &= b[p1];
+  return p2 == ((unsigned long long) 1 << (p3 - 1));
+}
+
+// { dg-final { scan-rtl-dump-not "S16 A64\[^\n\]*\\\*movv2di_internal" "final" } }
