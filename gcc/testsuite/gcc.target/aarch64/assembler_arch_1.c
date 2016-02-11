e1c1ecb0fe57884998e22526a7faa33e1f3528d5
Kyrylo Tkachov <kyrylo.tkachov@arm.com>
2016-02-11 13:27:28 +0000

    [AArch64] Only update assembler .arch directive when necessary
    	* config/aarch64/aarch64.c (aarch64_last_printed_arch_string):
    	New variable.
    	(aarch64_last_printed_tune_string): Likewise.
    	(aarch64_declare_function_name): Only output .arch assembler
    	directive if it will be different from the previously output
    	directive.  Same for .tune comment but only if -dA is set.
    	(aarch64_start_file): New function.
    	(TARGET_ASM_FILE_START): Define.
    
    	* gcc.target/aarch64/target_attr_15.c: Scan assembly for
    	.arch armv8-a\n.  Add -dA to dg-options.
    	* gcc.target/aarch64/assembler_arch_1.c: New test.
    	* gcc.target/aarch64/target_attr_7.c: Add -dA to dg-options.
    
    From-SVN: r233342

---
+++ gcc/testsuite/gcc.target/aarch64/assembler_arch_1.c
@@ -0,0 +1,20 @@
+/* { dg-do assemble } */
+/* { dg-options "-march=armv8-a" } */
+
+/* Make sure that the function header in assembly doesn't override
+   user asm arch_extension directives.  */
+
+__asm__ (".arch_extension lse");
+
+void
+foo (int i, int *v)
+{
+  register int w0 asm ("w0") = i;
+  register int *x1 asm ("x1") = v;
+
+  asm volatile (
+  "\tstset   %w[i], %[v]\n"
+  : [i] "+r" (w0), [v] "+Q" (v)
+  : "r" (x1)
+  : "x30");
+}
