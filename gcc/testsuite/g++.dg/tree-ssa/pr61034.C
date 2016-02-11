b86fba37e2f716806427a352020869637ccd93f6
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-02-11 08:15:44 +0000

    2016-02-11  Richard Biener  <rguenther@suse.de>
    	* g++.dg/tree-ssa/pr61034.C: Adjust.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@233317 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/tree-ssa/pr61034.C
+++ gcc/testsuite/g++.dg/tree-ssa/pr61034.C
@@ -49,6 +49,5 @@ bool f(I a, I b, I c, I d) {
 // a different initial CFG and thus the final outcome is different
 
 // { dg-final { scan-tree-dump-times "free" 10 "fre3" { target x86_64-*-* i?86-*-* } } }
-// { dg-final { scan-tree-dump-times "free" 3 "optimized" { target x86_64-*-* i?86-*-* } } }
 // { dg-final { scan-tree-dump-times "free" 14 "fre3" { target aarch64-*-* ia64-*-* arm-*-* hppa*-*-* sparc*-*-* powerpc*-*-* alpha*-*-* } } }
-// { dg-final { scan-tree-dump-times "free" 4 "optimized" { target aarch64-*-* ia64-*-* arm-*-* hppa*-*-* sparc*-*-* powerpc*-*-* alpha*-*-* } } }
+// { dg-final { scan-tree-dump-times "free" 0 "optimized" } }
