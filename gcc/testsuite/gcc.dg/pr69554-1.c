876217ae71cf0b34490f8f53bb2a12d99d8baa7a
David Malcolm <dmalcolm@redhat.com>
2016-02-12 19:18:03 +0000

    PR other/69554: avoid excessive source printing for widely-separated locations
    gcc/ChangeLog:
    	PR other/69554
    	* diagnostic-show-locus.c (struct line_span): New struct.
    	(layout::get_first_line): Delete.
    	(layout::get_last_line): Delete.
    	(layout::get_num_line_spans): New member function.
    	(layout::get_line_span): Likewise.
    	(layout::print_heading_for_line_span_index_p): Likewise.
    	(layout::get_expanded_location): Likewise.
    	(layout::calculate_line_spans): Likewise.
    	(layout::m_first_line): Delete.
    	(layout::m_last_line): Delete.
    	(layout::m_line_spans): New field.
    	(layout::layout): Update comment.  Replace m_first_line and
    	m_last_line with m_line_spans, replacing their initialization
    	with a call to calculate_line_spans.
    	(diagnostic_show_locus): When printing source lines and
    	annotations, rather than looping over a single span
    	of lines, instead loop over each line_span within
    	the layout, with an inner loop over the lines within them.
    	Call the context's start_span callback when changing line spans.
    	* diagnostic.c (diagnostic_initialize): Initialize start_span.
    	(diagnostic_build_prefix): Break out the building of the location
    	part of the string into...
    	(diagnostic_get_location_text): ...this new function, rewriting
    	it from nested ternary expressions to a sequence of "if"
    	statements.
    	(default_diagnostic_start_span_fn): New function.
    	* diagnostic.h (diagnostic_start_span_fn): New typedef.
    	(diagnostic_context::start_span): New field.
    	(default_diagnostic_start_span_fn): New prototype.
    
    gcc/fortran/ChangeLog:
    	PR other/69554
    	* error.c (gfc_diagnostic_start_span): New function.
    	(gfc_diagnostics_init): Initialize global_dc's start_span.
    
    gcc/testsuite/ChangeLog:
    	PR other/69554
    	* gcc.dg/pr69554-1.c: New test.
    	* gfortran.dg/pr69554-1.F90: New test.
    	* gfortran.dg/pr69554-2.F90: New test.
    	* lib/gcc-dg.exp (proc dg-locus): New function.
    	* lib/gfortran-dg.exp (proc gfortran-dg-test): Update comment to
    	distinguish between the caret-printing and non-caret-printing
    	cases.  If caret-printing has been explicitly enabled, bail out
    	without attempting to fix up the output.
    
    From-SVN: r233386

---
+++ gcc/testsuite/gcc.dg/pr69554-1.c
@@ -0,0 +1,152 @@
+/* { dg-options "-fdiagnostics-show-caret" } */
+
+/* Various versions of the same C error, with a variety of line spacing,
+   and of columns, to exercise the line-span handling in
+   diagnostic-show-locus.c (PR other/69554).  */
+
+/* All on one line.  */
+
+int test_1 (const char *p, const char *q)
+{
+  return (p + 1) + (q + 1); /* { dg-error "invalid operands" } */
+/* { dg-begin-multiline-output "" }
+   return (p + 1) + (q + 1);
+          ~~~~~~~ ^ ~~~~~~~
+   { dg-end-multiline-output "" } */
+}
+
+/* On separate lines, but without intervening lines.
+   This can be printed as a single span of lines.  */
+
+int test_2 (const char *p, const char *q)
+{
+  return (p + 1)
+           +  /* { dg-error "invalid operands" } */
+            (q + 1);
+/* { dg-begin-multiline-output "" }
+   return (p + 1)
+          ~~~~~~~
+            +
+            ^
+             (q + 1);
+             ~~~~~~~
+   { dg-end-multiline-output "" } */
+}
+
+/* On separate lines, with an intervening line between lines 1 and 2.
+   This is printed as 2 "spans" of lines, broken up by the intervening
+   line.  */
+
+int test_3 (const char *p, const char *q)
+{
+  return (p + 1) /* { dg-locus "10" } */
+
+           +  /* { dg-error "invalid operands" } */
+             (q + 1);
+/* { dg-locus "12" "" { target *-*-* } "44" } */
+/* { dg-begin-multiline-output "" }
+   return (p + 1)
+          ~~~~~~~
+   { dg-end-multiline-output "" } */
+/* { dg-begin-multiline-output "" }
+            +
+            ^
+              (q + 1);
+              ~~~~~~~
+   { dg-end-multiline-output "" } */
+}
+
+/* As above, but the intervening line is between lines 2 and 3,
+   so that the 2 spans are grouped the other way.  */
+
+int test_4 (const char *p, const char *q)
+{
+  return (p + 1)
+           +  /* { dg-error "invalid operands" } */
+
+             (q + 1); /* { dg-locus "14" } */
+/* { dg-begin-multiline-output "" }
+   return (p + 1)
+          ~~~~~~~
+            +
+            ^
+   { dg-end-multiline-output "" } */
+/* { dg-begin-multiline-output "" }
+              (q + 1);
+              ~~~~~~~
+   { dg-end-multiline-output "" } */
+}
+
+/* On separate lines, with intervening lines.
+   This is printed as 3 "spans" of lines, each span being an
+   individual line.  */
+
+int test_5 (const char *p, const char *q)
+{
+  return (p + 1) /* { dg-locus "10" } */
+
+           +  /* { dg-error "invalid operands" } */
+
+             (q + 1); /* { dg-locus "14" } */
+/* { dg-locus "12" "" { target *-*-* } "88" } */
+/* { dg-begin-multiline-output "" }
+   return (p + 1)
+          ~~~~~~~
+   { dg-end-multiline-output "" } */
+/* { dg-begin-multiline-output "" }
+            +
+            ^
+   { dg-end-multiline-output "" } */
+/* { dg-begin-multiline-output "" }
+              (q + 1);
+              ~~~~~~~
+   { dg-end-multiline-output "" } */
+}
+
+/* On separate lines, with numerous intervening lines.
+   This is printed as 3 "spans" of lines, each span being an
+   individual line.  */
+
+int test_6 (const char *p, const char *q)
+{
+  return (p + 1) /* { dg-locus "10" } */
+	  /* Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	     Maecenas nisl sapien, rutrum non euismod et, rutrum ac felis.
+	     Morbi nec nisi ipsum. Quisque pulvinar ante nec urna rhoncus,
+	     a cursus nisi commodo. Praesent euismod neque lectus, at
+	     dapibus ipsum gravida in. Pellentesque tempor massa eu viverra
+	     feugiat. Proin eleifend pulvinar urna, ut dapibus metus vehicula
+	     ac. Suspendisse rutrum finibus quam, ac dignissim diam blandit
+	     maximus. In blandit viverra pulvinar. Praesent vel tellus
+	     elementum, placerat lacus quis, ornare lectus. Donec ac
+	     eleifend nulla, sit amet condimentum risus. Vestibulum aliquam
+	     maximus ante non pellentesque. Praesent mollis ante in risus
+	     feugiat hendrerit. Praesent feugiat maximus urna nec blandit. */
+           +  /* { dg-error "invalid operands" } */
+	  /* Vestibulum ac nunc eget enim tempor tristique. Suspendisse
+	     potenti. Nam et sollicitudin enim. Morbi sed tincidunt lectus.
+	     Sed facilisis velit at ante maximus feugiat. Sed vestibulum mi
+	     id leo tempor, sed ullamcorper sapien efficitur. Vestibulum purus
+	     lacus, dignissim non magna at, tincidunt luctus nisl. Cum sociis
+	     natoque penatibus et magnis dis parturient montes, nascetur
+	     ridiculus mus. Donec elit elit, laoreet a dolor quis, eleifend
+	     dapibus metus. Proin lectus turpis, eleifend nec pharetra eu,
+	     fermentum in lacus. Morbi sit amet mauris orci. Nam sagittis,
+	     nibh vel fermentum dictum, purus ex hendrerit odio, feugiat
+	     fringilla sapien elit vitae nisl. Fusce mattis commodo risus
+	     nec convallis. */
+             (q + 1); /* { dg-locus "14" } */
+/* { dg-locus "12" "" { target *-*-* } "125" } */
+/* { dg-begin-multiline-output "" }
+   return (p + 1)
+          ~~~~~~~
+   { dg-end-multiline-output "" } */
+/* { dg-begin-multiline-output "" }
+            +
+            ^
+   { dg-end-multiline-output "" } */
+/* { dg-begin-multiline-output "" }
+              (q + 1);
+              ~~~~~~~
+   { dg-end-multiline-output "" } */
+}
---
+++ gcc/testsuite/gfortran.dg/pr69554-2.F90
@@ -0,0 +1,21 @@
+! { dg-do compile }
+! { dg-options "-fdiagnostics-show-caret" }
+! { dg-allow-blank-lines-in-output 1 }
+
+program main
+  goto 1000
+1000 continue ! first instance
+1000 continue ! second instance
+end
+
+#if 0
+! { dg-locus "4" "" { target *-*-* } "7" }
+! { dg-begin-multiline-output "" }
+
+ 1000 continue ! first instance
+    1
+ 1000 continue ! second instance
+    2
+Error: Duplicate statement label 1000 at (1) and (2)
+! { dg-end-multiline-output "" }
+#endif
