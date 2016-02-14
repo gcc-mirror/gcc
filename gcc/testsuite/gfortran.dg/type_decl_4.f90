b7c5658195b3e2c969159bb37ea804af840d41cc
Thomas Koenig <tkoenig@gcc.gnu.org>
2016-02-14 15:13:39 +0000

    re PR fortran/60526 (Accepts-invalid: Variable name same as type name)
    2016-02-14  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/60526
    	PR bootstrap/69816
    	* decl.c (build_sym):  Reverted previous patch.
    
    2016-02-14  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/60526
    	PR bootstrap/69816
    	* gfortran.dg/type_decl_4.f90:  Removed.
    
    From-SVN: r233411

--- gcc/testsuite/gfortran.dg/type_decl_4.f90
+++
@@ -1,7 +0,0 @@
-! { dg-do compile }
-program main
-  type Xx ! { dg-error "Symbol 'xx' at .1. also declared as a type at .2." }
-  end type Xx
-  real :: Xx  ! { dg-error "Symbol 'xx' at .1. also declared as a type at .2." }
-  
-end program main
