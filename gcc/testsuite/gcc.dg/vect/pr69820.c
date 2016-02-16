37fffac2dc0fdca46ea2ece1f663a1f9e43b470c
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-02-16 15:16:48 +0000

    PR tree-optimization/69820 	* tree-vect-patterns.c (type_conversion_p): Return false if 	*orig_type is unsigned single precision or boolean. 	(vect_recog_dot_prod_pattern, vect_recog_widen_mult_pattern): 	Formatting fix.
    	* gcc.dg/vect/pr69820.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@233457 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/pr69820.c
@@ -0,0 +1,35 @@
+/* PR tree-optimization/69820 */
+
+#include "tree-vect.h"
+
+unsigned int a[100];
+long long int b[100];
+unsigned short c[100];
+
+__attribute__((noinline, noclone)) void
+foo (void)
+{
+  int i;
+  for (i = 0; i < 100; ++i)
+    b[i] = a[i] * (c[i] * (_Bool) c[i]);
+}
+
+int
+main ()
+{
+  int i;
+  if (__SIZEOF_INT__ * __CHAR_BIT__ != 32)
+    return 0;
+  check_vect ();
+  for (i = 0; i < 100; ++i)
+    {
+      a[i] = 3489456818U;
+      b[i] = 0x1eadbeefbeefdeadLL;
+      c[i] = 38364;
+    }
+  foo ();
+  for (i = 0; i < 100; ++i)
+    if (b[i] != 0xed446af8U)
+      __builtin_abort ();
+  return 0;
+}
