00cd1633af4dab2c8281d43603734354a7944911
Tom de Vries <tom@codesourcery.com>
2016-02-16 13:49:12 +0000

    backport "Fix ICE for SIMD clones usage in LTO"
    2016-02-16  Tom de Vries  <tom@codesourcery.com>
    
    	backport from trunk:
    	2015-10-21  Ilya Enkovich  <enkovich.gnu@gmail.com>
    
    	* omp-low.c (simd_clone_create): Set in_other_partition
    	for created clones.
    
    	* gcc.dg/lto/simd-function_0.c: New test.
    
    From-SVN: r233449

---
+++ gcc/testsuite/gcc.dg/lto/simd-function_0.c
@@ -0,0 +1,34 @@
+/* { dg-lto-do link } */
+/* { dg-require-effective-target avx2 } */
+/* { dg-lto-options { { -fopenmp-simd -O3 -ffast-math -mavx2 -flto -flto-partition=max } } } */
+
+#define SIZE 4096
+float x[SIZE];
+
+
+#pragma omp declare simd
+float
+__attribute__ ((noinline))
+my_mul (float x, float y) {
+  return x * y;
+}
+
+__attribute__ ((noinline))
+int foo ()
+{
+  int i = 0;
+#pragma omp simd safelen (16)
+  for (i = 0; i < SIZE; i++)
+    x[i] = my_mul ((float)i, 9932.3323);
+  return (int)x[0];
+}
+
+int main ()
+{
+  int i = 0;
+  for (i = 0; i < SIZE; i++)
+    x[i] = my_mul ((float) i, 9932.3323);
+  foo ();
+  return (int)x[0];
+}
+
