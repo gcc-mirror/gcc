1bed93e4655e4d95eb69c0fb6286e178ef89acea
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2016-02-17 16:23:55 +0000

    altivec.md (*altivec_lvxl_<mode>_internal): Output correct instruction.
    [gcc]
    
    2016-02-17  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* config/rs6000/altivec.md (*altivec_lvxl_<mode>_internal): Output
    	correct instruction.
    
    [gcc/testsuite]
    
    2012-02-17  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* gcc.target/powerpc/vec-cg.c: New test.
    
    From-SVN: r233499

---
+++ gcc/testsuite/gcc.target/powerpc/vec-cg.c
@@ -0,0 +1,22 @@
+/* Test code generation of vector built-ins.  We don't have this for
+   most of ours today.  As new built-ins are added, please add to this
+   test case.  Update as necessary to add VSX, P8-vector, P9-vector,
+   etc. */
+
+/* { dg-do compile { target powerpc*-*-* } } */
+/* { dg-require-effective-target powerpc_altivec_ok } */
+/* { dg-options "-maltivec -O0" } */
+
+#include <altivec.h>
+
+static vector signed int i, *pi;
+static int int1;
+
+void
+b()
+{
+  i = __builtin_altivec_lvxl (int1, pi);
+  i = vec_lvxl (int1, pi);
+}
+
+/* { dg-final { scan-assembler-times "lvxl" 2 } } */
