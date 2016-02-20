2207c59959535b8b513126335e3317d59bd58ce2
Jason Merrill <jason@redhat.com>
2016-02-19 23:31:16 -0500

    re PR c++/69743 (function overload with variadic arguments - template instantiation depth exceeds maximum (gcc4, clang - no problem))
    	PR c++/69743
    	* call.c (remaining_arguments): No longer static.
    	* cp-tree.h: Declare it.
    	* pt.c (more_specialized_fn): Use it.
    
    From-SVN: r233579

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -5612,6 +5612,7 @@ extern tree make_temporary_var_for_ref_to_temp	(tree, tree);
 extern bool type_has_extended_temps		(tree);
 extern tree strip_top_quals			(tree);
 extern bool reference_related_p			(tree, tree);
+extern int remaining_arguments			(tree);
 extern tree perform_implicit_conversion		(tree, tree, tsubst_flags_t);
 extern tree perform_implicit_conversion_flags	(tree, tree, tsubst_flags_t, int);
 extern tree build_integral_nontype_arg_conv	(tree, tree, tsubst_flags_t);
