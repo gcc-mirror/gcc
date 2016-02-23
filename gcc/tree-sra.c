20c5e09e3f0d681a4f82acd01fd4c91816f7d21e
Martin Jambor <mjambor@suse.cz>
2016-02-23 11:55:47 +0100

    [pr 69666] No SRA default_def replacements for unscalarizable regions
    2016-02-23  Martin Jambor  <mjambor@suse.cz>
    
    	PR tree-optimization/69666
    	* tree-sra.c (sra_modify_assign): Do not attempt to create
    	default_def replacements for unscalarizable regions.
    
    testsuite/
    	* gcc.dg/tree-ssa/pr69666.c: New test.
    
    From-SVN: r233626

--- gcc/tree-sra.c
+++ gcc/tree-sra.c
@@ -3339,6 +3339,7 @@ sra_modify_assign (gimple *stmt, gimple_stmt_iterator *gsi)
     }
   else if (racc
 	   && !racc->grp_unscalarized_data
+	   && !racc->grp_unscalarizable_region
 	   && TREE_CODE (lhs) == SSA_NAME
 	   && !access_has_replacements_p (racc))
     {
