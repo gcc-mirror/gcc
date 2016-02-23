1cb1bfb61f115317542659bd5b65ac18aaca7e19
Martin Jambor <mjambor@suse.cz>
2016-02-23 13:54:44 +0100

    [pr 69666] No SRA default_def replacements for unscalarizable regions
    2016-02-23  Martin Jambor  <mjambor@suse.cz>
    
    	PR tree-optimization/69666
    	* tree-sra.c (sra_modify_assign): Do not attempt to create
    	default_def replacements for unscalarizable regions.
    
    testsuite/
    	* gcc.dg/tree-ssa/pr69666.c: New test.
    
    From-SVN: r233629

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr69666.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -w" } */
+
+int a, c, d;
+float b;
+void *memcpy();
+int fun1(int p1, unsigned char *p2) {
+  p2[p1] = b;
+  return a;
+}
+void fun2() {
+  unsigned char e[16];
+  fun1(16, e);
+  d = e[d];
+  memcpy(&c, e, sizeof(e));
+}
