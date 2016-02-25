9559fbacc6d7e9f07f96443baadf931758ab00f3
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2016-02-25 12:47:28 +0000

    Require init_priority support for g++.dg/ext/attr-constructor1.C
    	* g++.dg/ext/attr-constructor1.C: Require init_priority support.
    
    From-SVN: r233713

--- gcc/testsuite/g++.dg/ext/attr-constructor1.C
+++ gcc/testsuite/g++.dg/ext/attr-constructor1.C
@@ -1,5 +1,5 @@
 // PR c++/59281
-// { dg-do compile { target c++11 } }
+// { dg-do compile { target { c++11 && init_priority } } }
 
 enum class E : int { prio = 666 };
 void f (int) __attribute__((constructor(E::prio))); // { dg-error "integer" }
