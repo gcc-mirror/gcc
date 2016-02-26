d1f2bea53f8990253f655cfa22815046efb2ed87
Segher Boessenkool <segher@kernel.crashing.org>
2016-02-26 19:49:18 +0100

    powerpc: Handle DImode rotatert implemented with rlwinm (PR69946)
    Some DImode rotate-right-and-mask can be implemented best with a rlwinm
    instruction: those that could be a lshiftrt instead of a rotatert, while
    the mask is not right-aligned.  Why the rotate in the testcase is not
    optimised to a plain shift is another question, but we need to handle
    it here anyway.  We compute the shift amount for a 64-bit rotate.  This
    is 32 too high in this case; if we print using %h that is masked out (and
    this doesn't silently let through invalid instructions, everything is
    checked by rs6000_is_valid_shift_mask which is much more thorough).
    
    
    	PR target/69946
    	* config/rs6000/rs6000.c (rs6000_insn_for_shift_mask): Print rlwinm
    	shift amount using %h.  Add comment.
    
    gcc/testsuite/
    	* gcc.target/powerpc/pr69946.c: New file.
    
    From-SVN: r233755

---
+++ gcc/testsuite/gcc.target/powerpc/pr69946.c
@@ -0,0 +1,38 @@
+/* { dg-do compile { target { powerpc*-*-* && lp64 } } } */
+/* { dg-skip-if "" { powerpc_elfv2 } } */
+/* { dg-options "-O2" } */
+
+/* This generates a rotate:DI by 44, with mask 0xf00, which is implemented
+   using a rlwinm instruction.  We used to write 44 for the shift count
+   there; it should be 12.  */
+
+struct A
+{
+  int a : 4;
+  int : 2;
+  int b : 2;
+  int : 2;
+  int c : 2;
+  int d : 1;
+  int e;
+};
+struct B
+{
+  int a : 4;
+} *a;
+void bar (struct A);
+
+void
+foo (void)
+{
+  struct B b = a[0];
+  struct A c;
+  c.a = b.a;
+  c.b = 1;
+  c.c = 1;
+  c.d = 0;
+  bar (c);
+}
+
+/* { dg-final { scan-assembler-not {(?n)rlwinm.*,44,20,23} } } */
+/* { dg-final { scan-assembler-times {(?n)rlwinm.*,12,20,23} 1 } } */
