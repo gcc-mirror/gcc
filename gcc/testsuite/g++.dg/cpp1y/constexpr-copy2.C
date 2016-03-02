7f0e23e931064d8e8758e01d95bfb89b8fa32e6e
Jason Merrill <jason@redhat.com>
2016-03-01 21:32:38 -0500

    re PR c++/69995 ([C++14] Invalid result when evaluating constexpr function)
    	PR c++/69995
    
    	* constexpr.c (cxx_eval_call_expression): Unshare arg.
    	(cxx_eval_constant_expression) [DECL_EXPR]: Unshare init.
    	[TARGET_EXPR]: Unshare init.
    
    From-SVN: r233877

---
+++ gcc/testsuite/g++.dg/cpp1y/constexpr-copy2.C
@@ -0,0 +1,24 @@
+// PR c++/69995
+// { dg-do compile { target c++14 } }
+
+struct A
+{
+  int i;
+};
+
+constexpr int f(A a)
+{
+  ++a.i;
+  return a.i;
+}
+
+constexpr bool g()
+{
+  A a = { 42 };
+  if (f(a) != 43) return false;
+  if (a.i != 42) return false;
+  return true;
+}
+
+#define SA(X) static_assert((X),#X)
+SA(g());
