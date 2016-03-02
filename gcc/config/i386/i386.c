37d98220230bf243a5c3af3eebdb57b59c8d14c4
Richard Biener <rguenther@suse.de>
2016-03-02 08:55:42 +0000

    re PR target/67278 (ICE: verify_gimple failed on darwin 14.5 x86_64)
    2016-03-02  Richard Biener  <rguenther@suse.de>
    	Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/67278
    	* config/i386/i386.c (type_natural_mode): Handle XFmode vectors.
    
    	* gcc.dg/simd-8.c: New testcase.
    
    Co-Authored-By: Uros Bizjak <ubizjak@gmail.com>
    
    From-SVN: r233898

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -7794,6 +7794,10 @@ type_natural_mode (const_tree type, const CUMULATIVE_ARGS *cum,
 	{
 	  machine_mode innermode = TYPE_MODE (TREE_TYPE (type));
 
+	  /* There are no XFmode vector modes.  */
+	  if (innermode = XFmode)
+	    return mode;
+
 	  if (TREE_CODE (TREE_TYPE (type)) == REAL_TYPE)
 	    mode = MIN_MODE_VECTOR_FLOAT;
 	  else
