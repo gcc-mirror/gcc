23601f6747193588690fb026baee98866d219a42
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-03-02 07:57:17 +0000

    * gcc.target/i386/pr70007.c: Tweak.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@233894 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/pr70007.c
+++ gcc/testsuite/gcc.target/i386/pr70007.c
@@ -1,7 +1,9 @@
 /* PR rtl-optimization/70007 */
-/* { dg-do run } */
-/* { dg-options "-O -fgcse -mbmi2" } */
-/* { dg-require-effective-target bmi2 } */
+/* { dg-do run { target bmi2 } } */
+/* { dg-options "-O -fgcse -mbmi2 -Wno-psabi" } */
+/* { dg-require-effective-target int128 } */
+
+#include "bmi2-check.h"
 
 typedef unsigned short v32u16 __attribute__ ((vector_size (32)));
 typedef unsigned long long v32u64 __attribute__ ((vector_size (32)));
@@ -19,11 +21,10 @@ foo (v32u16 v32u16_0, v32u64 v32u64_0, v32u64 v32u64_1)
   return v32u64_1[3];
 }
 
-int
-main (void)
+static void
+bmi2_test ()
 {
-  u128 x = foo((v32u16){~0xba31, 0x47c6}, (v32u64){64}, (v32u64){0, 0x8b217e2514d23242, 0xac569b6dff9f82, 0x9d4cffe03c139c});
+  u128 x = foo((v32u16){(unsigned short) ~0xba31, 0x47c6}, (v32u64){64}, (v32u64){0, 0x8b217e2514d23242, 0xac569b6dff9f82, 0x9d4cffe03c139c});
   if (x != 0x3c74da5ca328d09)
     __builtin_abort();
-  return 0;
 }
