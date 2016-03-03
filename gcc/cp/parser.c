639d16778cadf1c2d743503090e406e8df96b84e
Jason Merrill <jason@redhat.com>
2016-03-03 13:43:01 -0500

    re PR c++/65061 (Issue with using declaration and member class template)
    	PR c++/65061
    	* parser.c (cp_parser_template_name): Call strip_using_decl.
    
    From-SVN: r233942

--- gcc/cp/parser.c
+++ gcc/cp/parser.c
@@ -13669,6 +13669,8 @@ cp_parser_template_name (cp_parser* parser,
 				/*ambiguous_decls=*/NULL,
 				token->location);
 
+  decl = strip_using_decl (decl);
+
   /* If DECL is a template, then the name was a template-name.  */
   if (TREE_CODE (decl) == TEMPLATE_DECL)
     ;
