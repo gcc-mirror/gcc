5dd12e436a086895d97036e63c5f9fd33fd226f5
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-03-03 09:12:53 +0000

    2016-03-03  Richard Biener  <rguenther@suse.de>
    	PR tree-optimization/55936
    	* tree-vrp.c (compare_name_with_value): Add use_equiv_p
    	parameter and guard unsafe equivalence use.
    	(vrp_evaluate_conditional_warnv_with_ops): Always use
    	safe equivalences but not via the quadratic compare_names
    	helper.
    
    	* gcc.dg/tree-ssa/vrp06.c: Remove XFAIL.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@233928 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/vrp06.c
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp06.c
@@ -30,4 +30,4 @@ foo (int i, int j, int a)
 
 /* { dg-final { scan-tree-dump-times "Folding predicate i_\[0-9\]+.*0 to 0" 1 "vrp1" } } */
 /* { dg-final { scan-tree-dump-times "Folding predicate j_\[0-9\]+.*0 to 1" 1 "vrp1" } } */
-/* { dg-final { scan-tree-dump-times "Folding predicate i_\[0-9]+.*j_\[0-9\]+.* to 0" 1 "vrp1" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-times "Folding predicate i_\[0-9]+.*j_\[0-9\]+.* to 0" 1 "vrp1" } } */
