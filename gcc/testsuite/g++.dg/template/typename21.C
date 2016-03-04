b54eff8bb1131e7bbd1e03633460be120f11ec99
Jason Merrill <jason@redhat.com>
2016-03-04 11:07:20 -0500

    re PR c++/70067 (internal compiler error: in strip_typedefs, at cp/tree.c:1466)
    	PR c++/70067
    
    	* tree.c (strip_typedefs): Handle TYPENAME_TYPE lookup finding the
    	same type.
    
    From-SVN: r233973

---
+++ gcc/testsuite/g++.dg/template/typename21.C
@@ -0,0 +1,11 @@
+// PR c++/70067
+// { dg-do compile { target c++98 } }
+
+template <class> struct A;
+template <class T> struct B { struct N { }; };
+template <class T> struct D: B<T> {
+  typedef typename D::N N;
+  A<N> *a;
+};
+
+D<int> d;
