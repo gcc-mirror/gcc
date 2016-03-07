2067a8f3c21aea9c049fcddb673aeee71911428d
Richard Biener <rguenther@suse.de>
2016-03-07 11:45:49 +0000

    re PR testsuite/70109 (FAIL: gcc.dg/vect/O3-pr36098.c scan-tree-dump-times vect "vectorizing stmts usin g SLP" 0)
    2016-03-07  Richard Biener  <rguenther@suse.de>
    
    	PR testsuite/70109
    	* gcc.dg/vect/O3-pr36098.c: New testcase.
    
    From-SVN: r234024

--- gcc/testsuite/gcc.dg/vect/O3-pr36098.c
+++ gcc/testsuite/gcc.dg/vect/O3-pr36098.c
@@ -17,4 +17,8 @@ void foo (int ncons, t_sortblock *sb, int *iatom)
      iatom[m]=sb[i].iatom[m];
 }
 
-/* { dg-final { scan-tree-dump-times "vectorizing stmts using SLP" 0 "vect" } } */
+/* The testcase was originally added for correctness reasons but now we
+   can vectorize it correctly if the target supports the permutations
+   required.  */
+
+/* { dg-final { scan-tree-dump-times "vectorizing stmts using SLP" 0 "vect" { target { ! vect_perm } } } } */
