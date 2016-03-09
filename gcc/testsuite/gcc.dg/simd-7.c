1bd49ced90474caa622367f92cdf110ffd9ec201
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-03-09 09:23:53 +0000

    PR middle-end/67278 	* gcc.dg/simd-7.c: Add -w -Wno-psabi to dg-options.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@234080 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/simd-7.c
+++ gcc/testsuite/gcc.dg/simd-7.c
@@ -1,4 +1,5 @@
 /* { dg-do compile } */
+/* { dg-options "-w -Wno-psabi" } */
 
 #if __SIZEOF_LONG_DOUBLE__ == 16 || __SIZEOF_LONG_DOUBLE__ == 8
 typedef long double a __attribute__((vector_size (16)));
