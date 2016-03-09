275274282fd79e98c400ed5bb28022cc1df19917
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-03-09 13:09:02 +0000

    Really don't xfail gcc.dg/vect/bb-slp-34.c on aarch64, arm
    	* gcc.dg/vect/bb-slp-34.c: Really don't xfail on aarch64-*-*,
    	arm-*-*.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@234081 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/vect/bb-slp-34.c
+++ gcc/testsuite/gcc.dg/vect/bb-slp-34.c
@@ -33,4 +33,4 @@ int main()
 }
 
 /* ??? XFAILed because we access "excess" elements with the permutation.  */
-/* { dg-final { scan-tree-dump "basic block vectorized" "slp2" { xfail { { vect_perm } && { ! { aarch64*-*-* arm*-*-* } } } } } } */
+/* { dg-final { scan-tree-dump "basic block vectorized" "slp2" { target vect_perm xfail { ! { aarch64*-*-* arm*-*-* } } } } } */
