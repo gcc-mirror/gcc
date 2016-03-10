ba52669fc5861ec7ae9d38ea007340fc6cef83ea
Alan Modra <amodra@gmail.com>
2016-03-10 21:17:13 +1030

    PR69195, Reload confused by invalid reg_equiv
    Optimizing indirect jumps to direct jumps, and deleting dead insns can
    lead to changes in register lifetimes, which in turn can result in bad
    reg_equiv info being passed to reload.  So do these tasks before
    calculating reg_equiv info.
    
    gcc/
    	PR rtl-optimization/69195
    	PR rtl-optimization/47992
    	* ira.c (recorded_label_ref): Delete.
    	(update_equiv_regs): Return void.
    	(indirect_jump_optimize): New function.
    	(ira): Call indirect_jump_optimize and delete_trivially_dead_insns
    	before regstat_compute_ri.  Don't rebuild_jump_labels here.
    	Delete update_regstat.
    gcc/testsuite/
    	* gcc.dg/pr69195.c: New.
    	* gcc.dg/pr69238.c: New.
    
    From-SVN: r234101

---
+++ gcc/testsuite/gcc.dg/pr69195.c
@@ -0,0 +1,27 @@
+/* { dg-do run } */
+/* { dg-options "-O3 -fno-dce -fno-forward-propagate" } */
+
+void __attribute__ ((noinline, noclone))
+foo (int *a, int n)
+{
+  int *lasta = a + n;
+  for (; a != lasta; a++)
+    {
+      *a *= 2;
+      a[1] = a[-1] + a[-2];
+    }
+}
+
+int
+main ()
+{
+  int a[16] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+  int r[16] = { 1, 2, 6, 6, 16, 24, 44, 80,
+		136, 248, 432, 768, 1360, 2400, 4256, 3760 };
+  unsigned i;
+  foo (&a[2], 13);
+  for (i = 0; i < 8; ++i)
+    if (a[i] != r[i])
+      __builtin_abort ();
+  return 0;
+}
