841166713da31fbd725b5f523fd9d321446522a8
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-03-11 22:32:11 +0000

    PR tree-optimization/70190 	* tree-ssa-threadbackward.c (fsm_find_control_statement_thread_paths): 	Handle cases where we can not extract the taken edge, even though we 	found a constant value.
    	PR tree-optimization/70190
    	* gcc.c-torture/compile/pr70190.c: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@234151 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-threadbackward.c
+++ gcc/tree-ssa-threadbackward.c
@@ -385,6 +385,16 @@ fsm_find_control_statement_thread_paths (tree name,
 
 	     We have to know the outgoing edge to figure this out.  */
 	  edge taken_edge = find_taken_edge ((*path)[0], arg);
+
+	  /* There are cases where we may not be able to extract the
+	     taken edge.  For example, a computed goto to an absolute
+	     address.  Handle those cases gracefully.  */
+	  if (taken_edge == NULL)
+	    {
+	      path->pop ();
+	      continue;
+	    }
+
 	  bool creates_irreducible_loop = false;
 	  if (threaded_through_latch
 	      && loop == taken_edge->dest->loop_father
