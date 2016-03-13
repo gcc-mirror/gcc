44e66a770938dabebfb572619c4c2f0f2492aec2
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2016-03-13 17:38:07 +0000

    re PR fortran/69043 (Trying to include a directory causes an infinite loop)
    2016-03-13  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	    Jim MacArthur  <jim.macarthur@codethink.co.uk>
    
    	PR fortran/69043
    	* scanner.c (load_file): Check that included file is regular.
    
    	PR fortran/69043
    	* gfortran.dg/include_9.f90: New test.
    
    Co-Authored-By: Jim MacArthur <jim.macarthur@codethink.co.uk>
    
    From-SVN: r234169

---
+++ gcc/testsuite/gfortran.dg/include_9.f90
@@ -0,0 +1,6 @@
+! { dg-do compile }
+! PR69043 Trying to include a directory causes an infinite loop
+      include '.'
+      program main
+      end program
+! { dg-error "is not a regular file" " " { target *-*-* } 3 }
