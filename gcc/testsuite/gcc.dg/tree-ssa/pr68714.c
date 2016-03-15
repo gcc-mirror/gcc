8be119c16e1ceb40ac5c319afacbad7283180cdf
Richard Henderson <rth@redhat.com>
2016-03-15 08:43:00 -0700

    Fix pr68714 test for targets not supporting V4SImode.
      * gcc.dg/tree-ssa/pr68714.c: Test during reassoc pass.
    
    From-SVN: r234220

--- gcc/testsuite/gcc.dg/tree-ssa/pr68714.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr68714.c
@@ -1,9 +1,9 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -fdump-tree-optimized" } */
+/* { dg-options "-O2 -fdump-tree-reassoc1" } */
 
 typedef int vec __attribute__((vector_size(16)));
 vec f(vec x,vec y){
   return x<y|x==y;
 }
 
-/* { dg-final { scan-tree-dump-times " <= " 1 "optimized" } } */
+/* { dg-final { scan-tree-dump-times " <= " 1 "reassoc1" } } */
