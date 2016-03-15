523f8924db2793abae1b981137b42da8b6c54878
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-03-15 23:08:45 +0000

    Fix compiling large files
      * line-map.c (new_linemap): Make alloc_size a size_t.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@234239 138bc75d-0d04-0410-961f-82ee72b054a4

--- libcpp/line-map.c
+++ libcpp/line-map.c
@@ -376,7 +376,7 @@ new_linemap (struct line_maps *set,
   if (LINEMAPS_USED (set, macro_map_p) == LINEMAPS_ALLOCATED (set, macro_map_p))
     {
       /* We ran out of allocated line maps. Let's allocate more.  */
-      unsigned alloc_size;
+      size_t alloc_size;
 
       /* Cast away extern "C" from the type of xrealloc.  */
       line_map_realloc reallocator = (set->reallocator
