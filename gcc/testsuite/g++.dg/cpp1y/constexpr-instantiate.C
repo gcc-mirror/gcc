8ced6ab75a3a0fcdf1a50ee4d8b0bb57e517ff7b
msebor <msebor@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-03-16 17:42:41 +0000

    g++.dg/cpp1y/constexpr-instantiate.C: Correct DejaGnu directives.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@234264 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp1y/constexpr-instantiate.C
+++ gcc/testsuite/g++.dg/cpp1y/constexpr-instantiate.C
@@ -1,7 +1,7 @@
 // PR c++/58281 - Problem with explicitly instantiated constexpr template
 //     functions 
 // { dg-do compile { target c++11 } }
-// { do-additional-options "-fdump-tree-optimized" }
+// { dg-options "-fdump-tree-optimized" }
 
 
 template <typename T>
@@ -17,4 +17,5 @@ bool g (int x) { return f (x); }
 template bool f<int>(int);
 
 // Verify that the defintions of both f() and g() are emitted.
-// { dg-final { scan-tree-dump "bool \[fg\](" "optimized" } }
+// { dg-final { scan-tree-dump-times "\nconstexpr bool f\\\(" 1 "optimized" } }
+// { dg-final { scan-tree-dump-times "\nbool g\\\(" 1 "optimized" } }
