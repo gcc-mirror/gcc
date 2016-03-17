c28550e1e866e85ba26995a84c3d2356863342dd
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-03-17 14:56:43 +0000

    Add goacc/kernels-alias-{3,4}.f95
    2016-03-17  Tom de Vries  <tom@codesourcery.com>
    
    	* gfortran.dg/goacc/kernels-alias-3.f95: New test.
    	* gfortran.dg/goacc/kernels-alias-4.f95: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@234292 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/goacc/kernels-alias-4.f95
@@ -0,0 +1,20 @@
+! { dg-additional-options "-O2" }
+! { dg-additional-options "-fdump-tree-ealias-all" }
+
+program main
+  implicit none
+  integer, parameter :: n = 2
+  integer, target, dimension (0:n-1) :: a
+  integer, pointer :: ptr(:)
+  ptr => a
+
+  !$acc kernels pcopyin (a, ptr(0:2))
+  a(0) = 0
+  ptr(0) = 1
+  !$acc end kernels
+
+end program main
+
+! Only the omp_data_i related loads should be annotated with cliques.
+! { dg-final { scan-tree-dump-times "clique 1 base 1" 3 "ealias" } }
+! { dg-final { scan-tree-dump-times "(?n)clique .* base .*" 3 "ealias" } }
