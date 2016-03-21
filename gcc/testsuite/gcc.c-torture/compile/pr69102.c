b628775ef9770174f5cd8b8475e57a4866dddb40
abel <abel@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-03-21 07:52:05 +0000

    gcc/         PR rtl-optimization/69102         * sched-deps.c (sched_analyze_insn): Do not set last_args_size field         when we have a readonly dependency context.
    testsuite/
    
            PR rtl-optimization/69102
            * gcc.c-torture/compile/pr69102.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@234359 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/pr69102.c
@@ -0,0 +1,21 @@
+/* { dg-options "-Og -fPIC -fschedule-insns2 -fselective-scheduling2 -fno-tree-fre --param=max-sched-extend-regions-iters=10" } */
+void bar (unsigned int);
+
+void
+foo (void)
+{
+  char buf[1] = { 3 };
+  const char *p = buf;
+  const char **q = &p;
+  unsigned int ch;
+  switch (**q)
+    {
+    case 1:  ch = 5; break;
+    case 2:  ch = 4; break;
+    case 3:  ch = 3; break;
+    case 4:  ch = 2; break;
+    case 5:  ch = 1; break;
+    default: ch = 0; break;
+    }
+  bar (ch);
+}
