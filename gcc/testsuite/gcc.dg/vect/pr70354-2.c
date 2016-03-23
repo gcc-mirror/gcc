0179520a83329a8daefcbb87c2609035694b928a
Jakub Jelinek <jakub@redhat.com>
2016-03-23 10:52:00 +0100

    re PR tree-optimization/70354 (Wrong code with -O3 -march=broadwell and -march=skylake-avx512.)
    	PR tree-optimization/70354
    	* tree-vect-patterns.c (vect_recog_vector_vector_shift_pattern): If
    	oprnd0 is wider than oprnd1 and there is a cast from the wider
    	type to oprnd1, mask it with the mask of the narrower type.
    
    	* gcc.dg/vect/pr70354-1.c: New test.
    	* gcc.dg/vect/pr70354-2.c: New test.
    	* gcc.target/i386/avx2-pr70354-1.c: New test.
    	* gcc.target/i386/avx2-pr70354-2.c: New test.
    
    From-SVN: r234417

---
+++ gcc/testsuite/gcc.dg/vect/pr70354-2.c
@@ -0,0 +1,37 @@
+/* PR tree-optimization/70354 */
+/* { dg-do run } */
+
+#ifndef main
+#include "tree-vect.h"
+#endif
+
+unsigned long long a[64], b[64];
+
+__attribute__((noinline, noclone)) void
+foo (void)
+{
+  int i;
+  for (i = 0; i < 64; i++)
+    a[i] <<= (b[i] - 0x1200000000ULL);
+}
+
+int
+main ()
+{
+  int i;
+#ifndef main
+  check_vect ();
+#endif
+  if (__CHAR_BIT__ != 8 || sizeof (long long int) != 8)
+    return 0;
+  for (i = 0; i < 64; i++)
+    {
+      a[i] = 0x1234ULL;
+      b[i] = 0x1200000000ULL + (i % 54);
+    }
+  foo ();
+  for (i = 0; i < 64; i++)
+    if (a[i] != (0x1234ULL << (i % 54)))
+      abort ();
+  return 0;
+}
