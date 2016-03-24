cb9b6b990346977c982d908ed31c860100279251
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-03-24 11:26:44 +0000

    PR target/70290 	* g++.dg/ext/pr70290.C: Add -Wno-psabi -w to dg-options.  Formatting.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@234450 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ext/pr70290.C
+++ gcc/testsuite/g++.dg/ext/pr70290.C
@@ -1,16 +1,18 @@
+/* PR target/70290 */
 /* { dg-do compile } */
+/* { dg-options "-Wno-psabi -w" } */
 /* { dg-additional-options "-mavx512vl" { target { i?86-*-* x86_64-*-* } } } */
 
 typedef int vec __attribute__((vector_size(32)));
 
 vec
-test1 (vec x,vec y)
+test1 (vec x, vec y)
 {
   return (x < y) ? 1 : 0;
 }
 
 vec
-test2 (vec x,vec y)
+test2 (vec x, vec y)
 {
   vec zero = { };
   vec one = zero + 1;
