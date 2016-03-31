97eb24c42a5d82617e576165867dd6678b6b1262
Jakub Jelinek <jakub@redhat.com>
2016-03-31 15:00:52 +0200

    re PR rtl-optimization/70460 (Miscompilation of glibc on i686-linux starting with r234101)
    	PR rtl-optimization/70460
    	* ira.c (indirect_jump_optimize): Don't substitute LABEL_REF
    	with operand from REG_LABEL_OPERAND, instead substitute
    	SET_SRC or REG_EQUAL note content if it is a LABEL_REF.
    	Don't do anything for REG_NON_LOCAL_GOTO jumps.
    
    	* gcc.c-torture/execute/pr70460.c: New test.
    
    From-SVN: r234614

---
+++ gcc/testsuite/gcc.c-torture/execute/pr70460.c
@@ -0,0 +1,29 @@
+/* PR rtl-optimization/70460 */
+
+int c;
+
+__attribute__((noinline, noclone)) void
+foo (int x)
+{
+  static int b[] = { &&lab1 - &&lab0, &&lab2 - &&lab0 };
+  void *a = &&lab0 + b[x];
+  goto *a;
+lab1:
+  c += 2;
+lab2:
+  c++;
+lab0:
+  ;
+}
+
+int
+main ()
+{
+  foo (0);
+  if (c != 3)
+    __builtin_abort ();
+  foo (1);
+  if (c != 4)
+    __builtin_abort ();
+  return 0;
+}
