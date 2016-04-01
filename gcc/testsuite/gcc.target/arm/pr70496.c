41d146597caea6d5cab6def07a8b127de7b2099c
Ramana Radhakrishnan <ramana.radhakrishnan@arm.com>
2016-04-01 14:58:53 +0000

    re PR target/70496 (inadvertent change to ASM_APP_OFF for .arm / .thumb directives.)
    Fix PR target/70496
    
    While doing the unified asm rewrite - I inadvertently changed the
    meaning of ASM_APP_OFF which causes failures when folks who know what
    they are doing switch between arm and thumb states within a
    function. The intent of the unified asm rewrite was not to affect any
    inline assembler code in that it would remain in divided syntax by
    default and switching back to unified asm in normal compiled code
    after the inline assembler block. Thanks to Jim Wilson for pointing it
    out on the linaro list.
    
    Fixed thusly and a test is added. Tested arm-none-eabi cross and
    applied.
    
    Ramana
    
    2016-04-01  Ramana Radhakrishnan  <ramana.radhakrishnan@arm.com>
    
    	PR target/70496
    	* config/arm/arm.h (ASM_APP_OFF): Handle TARGET_ARM
    	and TARGET_THUMB.
    
    2016-04-01  Ramana Radhakrishnan  <ramana.radhakrishnan@arm.com>
    
    	PR target/70496
    	* gcc.target/arm/pr70496.c: New test.
    
    From-SVN: r234675

---
+++ gcc/testsuite/gcc.target/arm/pr70496.c
@@ -0,0 +1,12 @@
+/* { dg-do assemble } */
+/* { dg-options "-mthumb -O2" } */
+/* { dg-require-effective-target arm_thumb2_ok } */
+
+int i;
+void
+main (void)
+{
+  __asm__ volatile (".arm");
+  i = 0;
+  __asm__ volatile ("\n cbz r0, 2f\n2:");
+}
