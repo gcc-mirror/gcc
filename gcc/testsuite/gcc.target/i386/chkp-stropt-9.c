4afdcfaa6f925047857feebc13d50a4583a75eb0
Ilya Enkovich <enkovich.gnu@gmail.com>
2016-04-01 10:40:51 +0000

    re PR target/69890 (FAIL: gcc.target/i386/chkp-* on x86_64-apple-darwin15)
    gcc/testsuite/
    
    	PR target/69890
    	* gcc.dg/strlenopt.h (memmove): New.
    	* gcc.target/i386/chkp-strlen-1.c: Include "../../gcc.dg/strlenopt.h"
    	instead of "string.h".
    	* gcc.target/i386/chkp-strlen-2.c: Likewise.
    	* gcc.target/i386/chkp-strlen-3.c: Likewise.
    	* gcc.target/i386/chkp-strlen-4.c: Likewise.
    	* gcc.target/i386/chkp-strlen-5.c: Likewise.
    	* gcc.target/i386/chkp-stropt-1.c: Likewise.
    	* gcc.target/i386/chkp-stropt-10.c: Likewise.
    	* gcc.target/i386/chkp-stropt-11.c: Likewise.
    	* gcc.target/i386/chkp-stropt-12.c: Likewise.
    	* gcc.target/i386/chkp-stropt-13.c: Likewise.
    	* gcc.target/i386/chkp-stropt-14.c: Likewise.
    	* gcc.target/i386/chkp-stropt-15.c: Likewise.
    	* gcc.target/i386/chkp-stropt-16.c: Likewise.
    	* gcc.target/i386/chkp-stropt-2.c: Likewise.
    	* gcc.target/i386/chkp-stropt-3.c: Likewise.
    	* gcc.target/i386/chkp-stropt-4.c: Likewise.
    	* gcc.target/i386/chkp-stropt-5.c: Likewise.
    	* gcc.target/i386/chkp-stropt-6.c: Likewise.
    	* gcc.target/i386/chkp-stropt-7.c: Likewise.
    	* gcc.target/i386/chkp-stropt-8.c: Likewise.
    	* gcc.target/i386/chkp-stropt-9.c: Likewise.
    
    From-SVN: r234666

--- gcc/testsuite/gcc.target/i386/chkp-stropt-9.c
+++ gcc/testsuite/gcc.target/i386/chkp-stropt-9.c
@@ -2,7 +2,7 @@
 /* { dg-options "-fcheck-pointer-bounds -mmpx -O2 -fdump-tree-chkpopt -fchkp-use-fast-string-functions" } */
 /* { dg-final { scan-tree-dump-not "memcpy_nobnd" "chkpopt" } } */
 
-#include "string.h"
+#include "../../gcc.dg/strlenopt.h"
 
 void test (void *buf1, void *buf2, size_t len)
 {
