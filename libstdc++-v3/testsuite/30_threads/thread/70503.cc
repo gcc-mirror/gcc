9236fc885b2dc17de36ece0a73dc4d22965fe76e
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-04-05 13:35:32 +0000

    Ensure std::thread helpers have internal linkage
    	PR libstdc++/70503
    	* src/c++11/thread.cc (execute_native_thread_routine,
    	execute_native_thread_routine_compat): Give internal linkage.
    	* testsuite/30_threads/thread/70503.cc: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@234746 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/30_threads/thread/70503.cc
@@ -0,0 +1,33 @@
+// Copyright (C) 2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do link }
+// { dg-options " -std=gnu++11 -static" { target *-*-*gnu* } }
+// { dg-require-cstdint "" }
+// { dg-require-gthreads "" }
+
+extern "C" {
+  void execute_native_thread_routine(void);
+  void execute_native_thread_routine_compat(void);
+}
+
+int main()
+{
+  execute_native_thread_routine(); // { dg-error "undefined reference" }
+  execute_native_thread_routine_compat(); // { dg-error "undefined reference" }
+}
+// { dg-prune-output "collect2: error: ld returned" }
