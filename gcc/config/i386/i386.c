3866f8dd853af142535496ad0da9a5f26087e817
Jakub Jelinek <jakub@redhat.com>
2016-04-06 14:47:19 +0200

    i386.c (ix86_simd_clone_compute_vecsize_and_simdlen): Add support for AVX512F clones...
    	* config/i386/i386.c (ix86_simd_clone_compute_vecsize_and_simdlen):
    	Add support for AVX512F clones, include them by default for
    	exported OpenMP declare simd functions.  For AVX2 allow simdlen 32
    	and use it if charasteric type is 8-bit, for AVX512F allow simdlen
    	up to 128.
    
    	* lib/target-supports.exp (check_effective_target_vect_simd_clones):
    	Check for avx512f effective targets instead of avx2.
    	* gcc.dg/gomp/declare-simd-1.c: Add scan-assembler-times directives
    	for AVX512F clones.
    	* gcc.dg/gomp/declare-simd-3.c: Likewise.
    	* g++.dg/gomp/declare-simd-1.C: Likewise.
    	* g++.dg/gomp/declare-simd-3.C: Likewise.
    	* g++.dg/gomp/declare-simd-4.C: Likewise.
    
    From-SVN: r234780

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -53761,7 +53761,7 @@ ix86_simd_clone_compute_vecsize_and_simdlen (struct cgraph_node *node,
 
   if (clonei->simdlen
       && (clonei->simdlen < 2
-	  || clonei->simdlen > 16
+	  || clonei->simdlen > 128
 	  || (clonei->simdlen & (clonei->simdlen - 1)) != 0))
     {
       warning_at (DECL_SOURCE_LOCATION (node->decl), 0,
@@ -53819,7 +53819,9 @@ ix86_simd_clone_compute_vecsize_and_simdlen (struct cgraph_node *node,
     {
       /* If the function isn't exported, we can pick up just one ISA
 	 for the clones.  */
-      if (TARGET_AVX2)
+      if (TARGET_AVX512F)
+	clonei->vecsize_mangle = 'e';
+      else if (TARGET_AVX2)
 	clonei->vecsize_mangle = 'd';
       else if (TARGET_AVX)
 	clonei->vecsize_mangle = 'c';
@@ -53829,8 +53831,8 @@ ix86_simd_clone_compute_vecsize_and_simdlen (struct cgraph_node *node,
     }
   else
     {
-      clonei->vecsize_mangle = "bcd"[num];
-      ret = 3;
+      clonei->vecsize_mangle = "bcde"[num];
+      ret = 4;
     }
   switch (clonei->vecsize_mangle)
     {
@@ -53846,6 +53848,10 @@ ix86_simd_clone_compute_vecsize_and_simdlen (struct cgraph_node *node,
       clonei->vecsize_int = 256;
       clonei->vecsize_float = 256;
       break;
+    case 'e':
+      clonei->vecsize_int = 512;
+      clonei->vecsize_float = 512;
+      break;
     }
   if (clonei->simdlen == 0)
     {
@@ -53854,9 +53860,24 @@ ix86_simd_clone_compute_vecsize_and_simdlen (struct cgraph_node *node,
       else
 	clonei->simdlen = clonei->vecsize_float;
       clonei->simdlen /= GET_MODE_BITSIZE (TYPE_MODE (base_type));
-      if (clonei->simdlen > 16)
-	clonei->simdlen = 16;
     }
+  else if (clonei->simdlen > 16)
+    switch (clonei->vecsize_int)
+      {
+      case 512:
+	/* For AVX512-F, support VLEN up to 128.  */
+	break;
+      case 256:
+	/* For AVX2, support VLEN up to 32.  */
+	if (clonei->simdlen <= 32)
+	  break;
+	/* FALLTHRU */
+      default:
+	/* Otherwise, support VLEN up to 16.  */
+	warning_at (DECL_SOURCE_LOCATION (node->decl), 0,
+		    "unsupported simdlen %d", clonei->simdlen);
+	return 0;
+      }
   return ret;
 }
 
@@ -53880,6 +53901,10 @@ ix86_simd_clone_adjust (struct cgraph_node *node)
     case 'd':
       if (!TARGET_AVX2)
 	str = "avx2";
+      break;
+    case 'e':
+      if (!TARGET_AVX512F)
+	str = "avx512f";
       break;
     default:
       gcc_unreachable ();
@@ -53918,6 +53943,10 @@ ix86_simd_clone_usable (struct cgraph_node *node)
       break;
     case 'd':
       if (!TARGET_AVX2)
+	return -1;
+      return 0;
+    case 'e':
+      if (!TARGET_AVX512F)
 	return -1;
       return 0;
     default:
