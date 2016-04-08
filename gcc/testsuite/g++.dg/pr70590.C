56cfb59670621ffe7761eb200814dcc27d99cf63
Patrick Palka <ppalka@gcc.gnu.org>
2016-04-08 20:17:10 +0000

    Fix PR c++/70590 (error: location references block not in block tree)
    gcc/cp/ChangeLog:
    
    	PR c++/70590
    	PR c++/70452
    	* constexpr.c (cxx_eval_outermost_expression): Call unshare_expr
    	on the result if it's not a CONSTRUCTOR.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/70590
    	PR c++/70452
    	* g++.dg/pr70590.C: New test.
    	* g++.dg/pr70590-2.C: New test.
    
    From-SVN: r234837

---
+++ gcc/testsuite/g++.dg/pr70590.C
@@ -0,0 +1,25 @@
+// PR c++/70590
+// { dg-do compile { target c++11 } }
+// { dg-options "-O2" }
+
+int a;
+
+constexpr int *
+foo ()
+{
+  return &a;
+}
+
+void blah (int *);
+
+int
+bar ()
+{
+  blah (foo ());
+}
+
+int
+baz ()
+{
+  blah (foo ());
+}
