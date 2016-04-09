5d61c198768776ac45330002778f596c21d482f1
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-04-09 17:15:15 +0000

    PR rtl-optimization/66669 	* gcc.dg/loop-8.c: Skip on hppa*-*-*.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@234861 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/loop-8.c
+++ gcc/testsuite/gcc.dg/loop-8.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-O1 -fdump-rtl-loop2_invariant" } */
-/* { dg-skip-if "unexpected IV" { "visium-*-*" } { "*" } { "" } } */
+/* { dg-skip-if "unexpected IV" { "hppa*-*-* visium-*-*" } { "*" } { "" } } */
 
 void
 f (int *a, int *b)
