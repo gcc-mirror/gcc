3cb15094b3fcd62e73bd0afcf1f27fd9ccaf1c57
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-04-09 11:23:51 +0000

    PR tree-optimization/70586 	* tree-ssa-ifcombine.c (bb_no_side_effects_p): Return false 	for any calls.
    	* gcc.c-torture/execute/pr70586.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@234849 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-ifcombine.c
+++ gcc/tree-ssa-ifcombine.c
@@ -125,7 +125,14 @@ bb_no_side_effects_p (basic_block bb)
       if (gimple_has_side_effects (stmt)
 	  || gimple_uses_undefined_value_p (stmt)
 	  || gimple_could_trap_p (stmt)
-	  || gimple_vuse (stmt))
+	  || gimple_vuse (stmt)
+	  /* const calls don't match any of the above, yet they could
+	     still have some side-effects - they could contain
+	     gimple_could_trap_p statements, like floating point
+	     exceptions or integer division by zero.  See PR70586.
+	     FIXME: perhaps gimple_has_side_effects or gimple_could_trap_p
+	     should handle this.  */
+	  || is_gimple_call (stmt))
 	return false;
     }
 
