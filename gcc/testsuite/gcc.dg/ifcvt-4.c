85115fb63266f4ae1979f2f5c4d79a7a2ff51a70
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2016-04-13 18:13:31 +0000

    Fix gcc.dg/ifcvt-4.c on 64-bit SPARC (PR rtl-optimization/68749)
    	PR rtl-optimization/68749
    	* gcc.dg/ifcvt-4.c: Use "word_mode" rather than "int" to limit the
    	effects of argument promotions.
    	Remove default args to dg-skip-if.
    
    From-SVN: r234955

--- gcc/testsuite/gcc.dg/ifcvt-4.c
+++ gcc/testsuite/gcc.dg/ifcvt-4.c
@@ -1,12 +1,14 @@
 /* { dg-options "-fdump-rtl-ce1 -O2 --param max-rtl-if-conversion-insns=3" } */
 /* { dg-additional-options "-misel" { target { powerpc*-*-* } } } */
-/* { dg-skip-if "Multiple set if-conversion not guaranteed on all subtargets" { "arm*-*-* hppa*64*-*-* visium-*-*" } {"*"} { "" } }  */
+/* { dg-skip-if "Multiple set if-conversion not guaranteed on all subtargets" { "arm*-*-* hppa*64*-*-* visium-*-*" } }  */
 
-int
-foo (int x, int y, int a)
+typedef int word __attribute__((mode(word)));
+
+word
+foo (word x, word y, word a)
 {
-  int i = x;
-  int j = y;
+  word i = x;
+  word j = y;
   /* Try to make taking the branch likely.  */
   __builtin_expect (x > y, 1);
   if (x > y)
