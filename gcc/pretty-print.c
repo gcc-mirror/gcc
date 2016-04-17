b3de2446458bf8c61a8e54d6bd424bafe7ab479e
Tom de Vries <tom@codesourcery.com>
2016-04-17 05:23:15 +0000

    Handle backslash in pp_write_text_as_dot_label_to_stream
    2016-04-17  Tom de Vries  <tom@codesourcery.com>
    
    	PR other/70433
    	* pretty-print.c (pp_write_text_as_dot_label_to_stream): Escape
    	backslash in label.
    
    From-SVN: r235077

--- gcc/pretty-print.c
+++ gcc/pretty-print.c
@@ -182,6 +182,12 @@ pp_write_text_as_dot_label_to_stream (pretty_printer *pp, bool for_record)
 
 	/* The following characters always have to be escaped
 	   for use in labels.  */
+	case '\\':
+	  /* There is a bug in some (f.i. 2.36.0) versions of graphiz
+	     ( http://www.graphviz.org/mantisbt/view.php?id=2524 ) related to
+	     backslash as last char in label.  Let's avoid triggering it.  */
+	  gcc_assert (*(p + 1) != '\0');
+	  /* Fall through.  */
 	case '"':
 	  escape_char = true;
 	  break;
