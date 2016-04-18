8dc1e574aeed3522ce95b83290a84933805199b0
Jonathan Wakely <jwakely@redhat.com>
2016-04-18 16:43:50 +0100

    Avoid -Wsign-compare warnings in std::to_string()
    	* include/ext/string_conversions.h (__stoa): Avoid -Wsign-compare
            warnings.
    
    From-SVN: r235151

--- libstdc++-v3/include/ext/string_conversions.h
+++ libstdc++-v3/include/ext/string_conversions.h
@@ -65,14 +65,24 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	int _M_errno;
       } const __save_errno;
 
+      struct _Range_chk {
+	  static bool
+	  _S_chk(_TRet, std::false_type) { return false; }
+
+	  static bool
+	  _S_chk(_TRet __val, std::true_type) // only called when _Ret is int
+	  {
+	    return __val < _TRet(__numeric_traits<int>::__min)
+	      || __val > _TRet(__numeric_traits<int>::__max);
+	  }
+      };
+
       const _TRet __tmp = __convf(__str, &__endptr, __base...);
 
       if (__endptr == __str)
 	std::__throw_invalid_argument(__name);
       else if (errno == ERANGE
-	       || (std::__are_same<_Ret, int>::__value
-		   && (__tmp < __numeric_traits<int>::__min
-		       || __tmp > __numeric_traits<int>::__max)))
+	  || _Range_chk::_S_chk(__tmp, std::is_same<_Ret, int>{}))
 	std::__throw_out_of_range(__name);
       else
 	__ret = __tmp;
