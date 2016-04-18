abb6e0a19db16b185d218256d34be6a8e7045689
Jonathan Wakely <jwakely@redhat.com>
2016-04-18 16:44:16 +0100

    Don't include internal header in libstdc++ tests
    	* testsuite/experimental/type_erased_allocator/1.cc: Don't include
    	internal header.
    	* testsuite/experimental/type_erased_allocator/1_neg.cc: Likewise.
    	* testsuite/experimental/type_erased_allocator/2.cc: Likewise.
    	* testsuite/experimental/type_erased_allocator/uses_allocator.cc:
    	Likewise. Add licence and change to compile-only test.
    
    From-SVN: r235153

--- libstdc++-v3/testsuite/experimental/type_erased_allocator/uses_allocator.cc
+++ libstdc++-v3/testsuite/experimental/type_erased_allocator/uses_allocator.cc
@@ -1,7 +1,26 @@
-#include <bits/uses_allocator.h>
+// Copyright (C) 2015-2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++14" }
+// { dg-do compile }
+
 #include <vector>
-#include <experimental/utility>
 #include <memory>
+#include <experimental/utility>
 
 using std::vector;
 using std::allocator;
@@ -11,12 +30,5 @@ struct A {
   using allocator_type = std::experimental::erased_type;
 };
 
-void test01() {
-    static_assert(uses_allocator<vector<int>, allocator<int>>());
-    static_assert(uses_allocator<A, allocator<A>>());
-}
-
-int main() {
-  test01();
-  return 0;
-}
+static_assert(uses_allocator<vector<int>, allocator<int>>(), "");
+static_assert(uses_allocator<A, allocator<A>>(), "");
