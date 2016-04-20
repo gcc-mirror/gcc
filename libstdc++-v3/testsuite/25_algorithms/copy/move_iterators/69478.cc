5477dcecbf1a5715b8166b5859556988574acb42
Jonathan Wakely <jwakely@redhat.com>
2016-04-20 15:30:39 +0100

    Fix assertions for move assignment of trivial types
    Backport from mainline
    2016-01-26  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/69478
    	* include/bits/stl_algobase.h (__copy_move<_IsMove, true,
    	random_access_iterator_tag>): Check is_move_assignable when moving.
    	(__copy_move_backwards<_IsMove, true, random_access_iterator_tag>):
    	Likewise.
    	* testsuite/25_algorithms/copy/move_iterators/69478.cc: New.
    	* testsuite/25_algorithms/copy_backward/move_iterators/69478.cc: New.
    	* testsuite/25_algorithms/move/69478.cc: New.
    	* testsuite/25_algorithms/move_backward/69478.cc: New.
    
    From-SVN: r235285

---
+++ libstdc++-v3/testsuite/25_algorithms/copy/move_iterators/69478.cc
@@ -0,0 +1,40 @@
+// { dg-options "-std=gnu++11" }
+// { dg-do compile }
+
+// Copyright (C) 2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// PR libstdc++/69478
+
+#include <algorithm>
+#include <iterator>
+
+void
+test01()
+{
+  // A move-only type that is also a trivial class.
+  struct trivial_rvalstruct
+  {
+    trivial_rvalstruct() = default;
+    trivial_rvalstruct(trivial_rvalstruct&&) = default;
+    trivial_rvalstruct& operator=(trivial_rvalstruct&&) = default;
+  };
+  static_assert(std::is_trivial<trivial_rvalstruct>::value, "");
+
+  trivial_rvalstruct a[1], b[1];
+  copy(std::make_move_iterator(a), std::make_move_iterator(a + 1), b);
+}
