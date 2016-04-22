49622efa89937fa310add53b1efb50273ec0d857
nsz <nsz@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-04-22 11:08:16 +0000

    [PATCH 1/2] (header usage fix) remove unused system header includes
    cherry picked from trunk r235361
    
    2016-04-22  Szabolcs Nagy  <szabolcs.nagy@arm.com>
    
    	* auto-profile.c: Remove <string.h> include.
    	* ipa-icf-gimple.c: Remove <list> include.
    	* diagnostic.c: Remove <new> include.
    	* genmatch.c: Likewise.
    	* pretty-print.c: Likewise.
    	* toplev.c: Likewise
    	* c/c-objc-common.c: Likewise.
    	* cp/error.c: Likewise.
    	* fortran/error.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@235363 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/error.c
+++ gcc/cp/error.c
@@ -31,8 +31,6 @@ along with GCC; see the file COPYING3.  If not see
 #include "ubsan.h"
 #include "internal-fn.h"
 
-#include <new>                    // For placement-new.
-
 #define pp_separate_with_comma(PP) pp_cxx_separate_with (PP, ',')
 #define pp_separate_with_semicolon(PP) pp_cxx_separate_with (PP, ';')
 
