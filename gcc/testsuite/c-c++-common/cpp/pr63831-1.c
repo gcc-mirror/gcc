ea79f1a60435385f4bed7844ccf00d6a99e90c17
Jason Merrill <jason@gcc.gnu.org>
2016-04-26 10:05:51 -0400

    * c-c++-common/cpp/pr63831-1.c: Don't test for C++ values here.
    From-SVN: r235445

--- gcc/testsuite/c-c++-common/cpp/pr63831-1.c
+++ gcc/testsuite/c-c++-common/cpp/pr63831-1.c
@@ -2,15 +2,15 @@
 /* { dg-do compile } */
 
 #ifdef __has_attribute
-typedef char T1[__has_attribute (__noreturn__) == 200809 ? 1 : -1];
+typedef char T1[__has_attribute (__noreturn__) ? 1 : -1];
 typedef char T2[__has_attribute (alloc_size) == 1 ? 1 : -1];
 typedef char T3[__has_attribute (non_existent_attribuuuute) == 0 ? 1 : -1];
 #endif
-#if __has_attribute (noreturn) == 200809
+#if __has_attribute (noreturn)
 typedef char T4;
 #endif
 #define d deprecated
-typedef char T5[__has_attribute (d) == 201309 ? 1 : -1];
+typedef char T5[__has_attribute (d) ? 1 : -1];
 T1 t1;
 T2 t2;
 T3 t3;
@@ -24,7 +24,7 @@ typedef char T8[__has_attribute (gnu::non_existent_attribuuuute) == 0 ? 1 : -1];
 typedef char T9;
 #endif
 #define d2 gnu::deprecated
-typedef char T10[__has_attribute (d) == 201309 ? 1 : -1];
+typedef char T10[__has_attribute (d) ? 1 : -1];
 T6 t6;
 T7 t7;
 T8 t8;
@@ -32,15 +32,15 @@ T9 t9;
 T10 t10;
 #endif
 #ifdef __has_cpp_attribute
-typedef char T11[__has_cpp_attribute (__noreturn__) == 200809 ? 1 : -1];
+typedef char T11[__has_cpp_attribute (__noreturn__) ? 1 : -1];
 typedef char T12[__has_cpp_attribute (alloc_size) == 1 ? 1 : -1];
 typedef char T13[__has_cpp_attribute (non_existent_attribuuuute) == 0 ? 1 : -1];
 #endif
-#if __has_cpp_attribute (noreturn) == 200809
+#if __has_cpp_attribute (noreturn)
 typedef char T14;
 #endif
 #define d deprecated
-typedef char T15[__has_cpp_attribute (d) == 201309 ? 1 : -1];
+typedef char T15[__has_cpp_attribute (d) ? 1 : -1];
 T11 t11;
 T12 t12;
 T13 t13;
@@ -54,7 +54,7 @@ typedef char T18[__has_cpp_attribute (gnu::non_existent_attribuuuute) == 0 ? 1 :
 typedef char T19;
 #endif
 #define d2 gnu::deprecated
-typedef char T20[__has_cpp_attribute (d) == 201309 ? 1 : -1];
+typedef char T20[__has_cpp_attribute (d) ? 1 : -1];
 T16 t16;
 T17 t17;
 T18 t18;
