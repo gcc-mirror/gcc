605a45569ca8e0506ac9a374f5710acbb8ba11cb
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-04-27 20:41:52 +0000

    PR c++/69024 	PR c++/68997 	* cilk.c (cilk_ignorable_spawn_rhs_op): Change to external linkage. 	(cilk_recognize_spawn): Renamed from recognize_spawn and change to 	external linkage. 	(cilk_detect_and_unwrap): Corresponding changes. 	(extract_free_variables): Don't extract free variables from 	AGGR_INIT_EXPR slot. 	* c-common.h (cilk_ignorable_spawn_rhs_op): Prototype. 	(cilk_recognize_spawn): Likewise.
    	PR c++/69024
    	PR c++/68997
    	* cp-gimplify.c (cp_gimplify_expr): Call cilk_cp_detect_spawn_and_unwrap
    	instead of cilk_detect_spawn_and_unwrap.
    	* cp-cilkplus.c (is_conversion_operator_function_decl_p): New.
    	(find_spawn): New.
    	(cilk_cp_detect_spawn_and_unwrap): New.
    	* lambda.c: Include cp-cilkplus.h.
    	* parser.c: Include cp-cilkplus.h.
    	* cp-tree.h (cpp_validate_cilk_plus_loop): Move prototype into...
    	* cp-cilkpus.h: New file.
    
    	PR c++/69024
    	PR c++/68997
    	* g++.dg/cilk-plus/CK/pr68001.cc: Fix to not depend on broken
    	diagnostic.
    	* g++.dg/cilk-plus/CK/pr69024.cc: New test.
    	* g++.dg/cilk-plus/CK/pr68997.cc: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@235534 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/cp/cp-cilkplus.h
@@ -0,0 +1,28 @@
+/* C++-specific tree lowering bits; see also c-gimplify.c and tree-gimple.c.
+
+   Copyright (C) 2002-2016 Free Software Foundation, Inc.
+   Contributed by Jason Merrill <jason@redhat.com>
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef GCC_CP_CILKPLUS_H
+#define GCC_CP_CILKPLUS_H
+
+extern bool cilk_cp_detect_spawn_and_unwrap (tree *);
+extern bool cpp_validate_cilk_plus_loop (tree);
+
+#endif /* ! GCC_CP_CILKPLUS_H */
