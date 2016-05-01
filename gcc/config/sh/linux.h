21b607581a6e09a84823bf04bf263e610f5d69d0
Yoshinori Sato <ysato@users.sourceforge.jp>
2016-05-01 12:59:39 +0900

    linux.h (SH_DIV_STRATEGY_DEFAULT, [...]): Remove.
    gcc/
    	* config/sh/linux.h (SH_DIV_STRATEGY_DEFAULT,
    	SH_DIV_STR_FOR_SIZE): Remove.
    	* config/sh/netbsd-elf.h (SH_DIV_STRATEGY_DEFAULT,
    	SH_DIV_STR_FOR_SIZE): Remove.
    
    From-SVN: r235688

--- gcc/config/sh/linux.h
+++ gcc/config/sh/linux.h
@@ -117,13 +117,6 @@ along with GCC; see the file COPYING3.  If not see
 #define DBX_REGISTER_NUMBER(REGNO) \
   (((REGNO) == 16) ? 16 : SH_DBX_REGISTER_NUMBER (REGNO))
 
-/* Since libgcc is compiled with -fpic for this target, we can't use
-   __sdivsi3_1 as the division strategy for -O0 and -Os.  */
-#undef SH_DIV_STRATEGY_DEFAULT
-#define SH_DIV_STRATEGY_DEFAULT SH_DIV_CALL2
-#undef SH_DIV_STR_FOR_SIZE
-#define SH_DIV_STR_FOR_SIZE "call2"
-
 /* Install the __sync libcalls.  */
 #undef TARGET_INIT_LIBFUNCS
 #define TARGET_INIT_LIBFUNCS  sh_init_sync_libfuncs
