f8b733224f3766dd202dd9700920b08abb52e53b
segher <segher@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-05-04 20:57:08 +0000

    shrink-wrap: Remove complicated simple_return manipulations
    Now that cfgcleanup knows how to optimize with return statements, the
    epilogue insertion code doesn't have to deal with it itself anymore.
    
    
    	* function.c (emit_use_return_register_into_block): Delete.
    	(gen_return_pattern): Delete.
    	(emit_return_into_block): Delete.
    	(active_insn_between): Delete.
    	(convert_jumps_to_returns): Delete.
    	(emit_return_for_exit): Delete.
    	(thread_prologue_and_epilogue_insns): Delete all code dealing with
    	simple_return for shrink-wrapped blocks.
    	* shrink-wrap.c (try_shrink_wrapping): Insert simple_return at the
    	end of blocks that need one.
    	(get_unconverted_simple_return): Delete.
    	(convert_to_simple_return): Delete.
    	* shrink-wrap.c (get_unconverted_simple_return): Delete declaration.
    	(convert_to_simple_return): Ditto.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@235905 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/shrink-wrap.h
+++ gcc/shrink-wrap.h
@@ -26,12 +26,6 @@ along with GCC; see the file COPYING3.  If not see
 extern bool requires_stack_frame_p (rtx_insn *, HARD_REG_SET, HARD_REG_SET);
 extern void try_shrink_wrapping (edge *entry_edge, bitmap_head *bb_flags,
 				 rtx_insn *prologue_seq);
-extern edge get_unconverted_simple_return (edge, bitmap_head,
-					   vec<edge> *, rtx_insn **);
-extern void convert_to_simple_return (edge entry_edge, edge orig_entry_edge,
-				      bitmap_head bb_flags,
-				      rtx_insn *returnjump,
-				      vec<edge> unconverted_simple_returns);
 #define SHRINK_WRAPPING_ENABLED \
   (flag_shrink_wrap && targetm.have_simple_return ())
 
