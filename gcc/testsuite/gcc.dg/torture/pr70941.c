faf393487150cd1682f256ab256b9850a5c715ad
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-05-06 15:23:56 +0000

    PR middle-end/70941 	* gcc.dg/torture/pr70941.c (abort): Remove prototype. 	(a, b, c, d): Change type from char to signed char. 	(main): Compare against (signed char) -1634678893 instead of 	hardcoded -109.  Use __builtin_abort instead of abort.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@235978 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/torture/pr70941.c
+++ gcc/testsuite/gcc.dg/torture/pr70941.c
@@ -1,14 +1,12 @@
 /* { dg-do run } */
 /* { dg-require-effective-target int32plus } */
 
-extern void abort (void);
-
-char a = 0, b = 0, c = 0, d = 0;
+signed char a = 0, b = 0, c = 0, d = 0;
 
 int main()
 {
   a = -(b - 405418259) - ((d && c) ^ 2040097152);
-  if (a != -109)
-    abort();
+  if (a != (signed char) -1634678893)
+    __builtin_abort ();
   return 0;
 }
