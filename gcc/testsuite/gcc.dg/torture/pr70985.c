171f6f05db326f704e48375045aa2a98ea77675c
Richard Biener <rguenther@suse.de>
2016-05-09 12:23:11 +0000

    re PR tree-optimization/70985 (ICE on valid code at -O3 on x86_64-linux-gnu: verify_gimple failed)
    2016-05-09  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/70985
    	* match.pd (BIT_FIELD_REF -> (type)): Disable on GIMPLE when
    	op0 isn't a gimple register.
    
    	* gcc.dg/torture/pr70985.c: New testcase.
    
    From-SVN: r236032

---
+++ gcc/testsuite/gcc.dg/torture/pr70985.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target int32plus } */
+
+struct
+{
+  int f0:24;
+} a, c, d;
+
+int b;
+
+int
+fn1 ()
+{
+  return 0;
+}
+
+void
+fn2 ()
+{
+  int e;
+  if (b) 
+    for (; e;)
+      {
+	d = c;
+	if (fn1 (b))
+	  b = a.f0;
+      }
+}
