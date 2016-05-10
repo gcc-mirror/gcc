9308a3f9daf49474720385ab7aac30ef6415a21f
Yuri Rumyantsev <ysrumyan@gmail.com>
2016-05-10 14:26:37 +0000

    backport: re PR debug/70935 (ICE: verify_ssa failed (error: definition in block 9 does not dominate use in block 12) w/ -O3 -g)
    gcc/
    
    2016-05-10  Yuri Rumyantsev  <ysrumyan@gmail.com>
    
    	Backport from mainline r235962.
    	2016-05-06  Yuri Rumyantsev  <ysrumyan@gmail.com>
    
    	PR debug/70935
    	* tree-ssa-loop-unswitch.c (find_loop_guard): Reject guard edge with
    	loop latch destination.
    
    gcc/testsuite/
    
    2016-05-10  Yuri Rumyantsev  <ysrumyan@gmail.com>
    
    	Backport from mainline r235962.
    	2016-05-06  Yuri Rumyantsev  <ysrumyan@gmail.com>
    
    	PR debug/70935
    	* gcc.dg/torture/pr70935.c: New test.
    
    From-SVN: r236081

---
+++ gcc/testsuite/gcc.dg/torture/pr70935.c
@@ -0,0 +1,39 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 -g" } */
+
+int d0, sj, v0, rp, zi;
+
+void
+zn(void)
+{
+  if (v0 != 0)
+    {
+      int *js, *r3;
+      int pm, gc;
+
+      for (gc = 0; gc < 1; ++gc)
+        {
+          sj = 1;
+          while (sj != 0)
+            ;
+        }
+      r3 = &pm;
+      *js = (long)&gc;
+ka:
+      for (d0 = 0; d0 < 2; ++d0)
+        {
+          d0 = zi;
+          if (zi)
+            for (pm = 2; pm != 0; --pm)
+              ;
+        }
+      while (*r3 != 0)
+        {
+          while (pm)
+            ;
+          ++r3;
+        }
+    }
+  rp = 0;
+  goto ka;
+}
