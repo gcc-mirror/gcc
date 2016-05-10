6072c02cbf7217695ebaec017ba65c399108fb8b
wschmidt <wschmidt@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-05-10 17:24:32 +0000

    [gcc]
    2016-05-10  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2016-05-10  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	PR target/70963
    	* config/rs6000/vsx.md (vsx_xvcvdpsxds_scale): Generate correct
    	code for a zero scale factor.
    	(vsx_xvcvdpuxds_scale): Likewise.
    
    [gcc/testsuite]
    
    2016-05-10  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2016-05-10  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	PR target/70963
    	* gcc.target/powerpc/pr70963.c: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@236097 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/pr70963.c
@@ -0,0 +1,43 @@
+/* { dg-do run { target { powerpc64*-*-* && vsx_hw } } } */
+/* { dg-require-effective-target powerpc_vsx_ok } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
+/* { dg-options "-maltivec" } */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <altivec.h>
+
+static int failed;
+static void test (void);
+
+static void check (int result, const char *name)
+{
+  if (!result)
+    {
+      failed++;
+      printf ("fail %s\n", name);
+    }
+}
+    
+int main (void)
+{
+  test ();
+  if (failed)
+    abort ();
+  return 0;
+}
+
+vector double x = { 81.0, 76.0 };
+vector long long y = { 81, 76 };
+
+static void test()
+{
+  vector long long a = vec_cts (x, 0);
+  vector double b = vec_ctf (a, 0);
+  vector long long c = __builtin_vsx_xvcvdpuxds_scale (x, 0);
+  vector double d = vec_ctf (c, 0);
+  check (vec_all_eq (a, y), "vec_cts");
+  check (vec_all_eq (b, x), "vec_ctf");
+  check (vec_all_eq (c, y), "xvcvdpuxds");
+  check (vec_all_eq (d, x), "vec_ctf unsigned");
+}
