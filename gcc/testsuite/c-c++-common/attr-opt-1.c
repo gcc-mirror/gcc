b68f1bd5aaf62653b90e752327187cb704bde94c
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-05-11 17:09:43 +0000

    * gcc.dg/attr-opt-1.c: Move to c-c++-common/. 	* gcc.dg/pr18079-2.c: Remove file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@236130 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/c-c++-common/attr-opt-1.c
+++ gcc/testsuite/c-c++-common/attr-opt-1.c
@@ -6,7 +6,7 @@ fn1 (double h, double l) /* { dg-message "previous definition" } */
 {
   return h + l;
 }
-double fn1 (double, double) __attribute__ ((optimize ("no-associative-math"))); /* { dg-warning "optimization attribute on .fn1. follows definition" } */
+double fn1 (double, double) __attribute__ ((optimize ("no-associative-math"))); /* { dg-warning "optimization attribute" } */
 
 __attribute__ ((optimize ("no-associative-math"))) double
 fn2 (double h, double l)
@@ -20,14 +20,14 @@ fn3 (double h, double l) /* { dg-message "previous definition" } */
 {
   return h + l;
 }
-double fn3 (double, double) __attribute__ ((optimize ("O2,no-associative-math"))); /* { dg-warning "optimization attribute on .fn3. follows definition" } */
+double fn3 (double, double) __attribute__ ((optimize ("O2,no-associative-math"))); /* { dg-warning "optimization attribute" } */
 
 __attribute__ ((optimize ("no-associative-math,O2"))) double
 fn4 (double h, double l) /* { dg-message "previous definition" } */
 {
   return h + l;
 }
-double fn4 (double, double) __attribute__ ((optimize ("O2,no-associative-math"))); /* { dg-warning "optimization attribute on .fn4. follows definition" } */
+double fn4 (double, double) __attribute__ ((optimize ("O2,no-associative-math"))); /* { dg-warning "optimization attribute" } */
 
 __attribute__ ((optimize ("no-reciprocal-math"), optimize ("no-associative-math"))) int
 fn5 (void)
