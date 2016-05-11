57215d5b85193c6b0a4dfdc6ac86f586b1e80a94
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-05-11 19:16:58 +0000

    * gcc.target/i386/sse-13.c: Add dg-add-options bind_pic_locally 	directive. 	* gcc.target/i386/pr66746.c: Ditto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@236140 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/mmx-2.c
+++ gcc/testsuite/gcc.target/i386/mmx-2.c
@@ -1,7 +1,7 @@
 /* { dg-do compile } */
 /* { dg-options "-O0 -Werror-implicit-function-declaration -mmmx" } */
-
 /* { dg-add-options bind_pic_locally } */
+
 /* Test that the intrinsics compile without optimization.  All of them are
    defined as inline functions in mmintrin.h that reference the proper
    builtin functions.  Defining away "extern" and "__inline" results in
