8ab3bdac33672a2ec681e8d57869830fdcaffd0e
Richard Biener <rguenther@suse.de>
2016-05-13 12:27:18 +0000

    re PR tree-optimization/42587 (bswap not recognized for memory)
    2016-05-13  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/42587
    	* tree-ssa-math-opts.c (perform_symbolic_merge): Handle BIT_FIELD_REF.
    	(find_bswap_or_nop_1): Likewise.
    	(bswap_replace): Likewise.
    
    	* gcc.dg/optimize-bswapsi-4.c: New testcase.
    
    From-SVN: r236204

---
+++ gcc/testsuite/gcc.dg/optimize-bswapsi-4.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target bswap32 } */
+/* { dg-options "-O2 -fdump-tree-bswap" } */
+/* { dg-additional-options "-march=z900" { target s390-*-* } } */
+
+typedef unsigned char u8;
+typedef unsigned int u32;
+union __anonunion
+{
+  u32 value;
+  u8 bytes[4];
+};
+
+u32
+acpi_ut_dword_byte_swap (u32 value)
+{
+  union __anonunion in;
+  in.value = value;
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+  return ((in.bytes[0] << 24) | (in.bytes[1] << 16)
+	  | (in.bytes[2] << 8) | in.bytes[3]);
+#else
+  return ((in.bytes[3] << 24) | (in.bytes[2] << 16)
+	  | (in.bytes[1] << 8) | in.bytes[0]);
+#endif
+}
+
+/* { dg-final { scan-tree-dump "32 bit bswap implementation found at" "bswap" } } */
