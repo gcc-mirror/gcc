785abfd3ff0eb7443747ffb83d136c1a62389ab6
Fritz Reese <fritzoreese@gmail.com>
2016-05-14 16:00:35 +0000

    dec_union_4.f90: Fix endian issue.
    2016-05-14  Fritz Reese  <fritzoreese@gmail.com>
    
    	* gfortran.dg/dec_union_4.f90: Fix endian issue.
    
    2016-05-14  Fritz Reese  <fritzoreese@gmail.com>
    
    	* gfortran.texi: Update example of DEC UNION extension.
    
    From-SVN: r236242

--- gcc/testsuite/gfortran.dg/dec_union_4.f90
+++ gcc/testsuite/gfortran.dg/dec_union_4.f90
@@ -14,29 +14,29 @@ end subroutine
 structure /s4/
   union ! U0                ! rax
     map
-      integer(8) rx
+      character(16) rx
     end map
     map
-      integer(4) rh         ! rah
+      character(8) rh         ! rah
       union ! U1
         map
-          integer(4) rl     ! ral
+          character(8) rl     ! ral
         end map
         map
-          integer(4) ex     ! eax
+          character(8) ex     ! eax
         end map
         map
-          integer(2) eh     ! eah
+          character(4) eh     ! eah
           union ! U2
             map
-              integer(2) el ! eal
+              character(4) el ! eal
             end map
             map
-              integer(2) x  ! ax
+              character(4) x  ! ax
             end map
             map
-              integer(1) h  ! ah
-              integer(1) l  ! al
+              character(2) h  ! ah
+              character(2) l  ! al
             end map
           end union
         end map
@@ -48,15 +48,16 @@ record /s4/ r4
 
 
 ! Nested unions
-r4.rx     =     z'7A7B7CCC7FFFFFFF'
-if ( r4.rx .ne. z'7A7B7CCC7FFFFFFF' ) call aborts ("rax")
-if ( r4.rh .ne.         z'7FFFFFFF' ) call aborts ("rah")
-if ( r4.rl .ne. z'7A7B7CCC'         ) call aborts ("ral")
-if ( r4.ex .ne. z'7A7B7CCC'         ) call aborts ("eax")
-if ( r4.eh .ne.     z'7CCC'         ) call aborts ("eah")
-if ( r4.el .ne. z'7A7B'             ) call aborts ("eal")
-if ( r4.x  .ne. z'7A7B'             ) call aborts ("ax")
-if ( r4.h  .ne.   z'7B'             ) call aborts ("ah")
-if ( r4.l  .ne. z'7A'               ) call aborts ("al")
+r4.rx     =     'AAAAAAAA.BBB.C.D'
+
+if ( r4.rx .ne. 'AAAAAAAA.BBB.C.D' ) call aborts ("rax")
+if ( r4.rh .ne. 'AAAAAAAA'         ) call aborts ("rah")
+if ( r4.rl .ne.         '.BBB.C.D' ) call aborts ("ral")
+if ( r4.ex .ne.         '.BBB.C.D' ) call aborts ("eax")
+if ( r4.eh .ne.         '.BBB'     ) call aborts ("eah")
+if ( r4.el .ne.             '.C.D' ) call aborts ("eal")
+if ( r4.x  .ne.             '.C.D' ) call aborts ("ax")
+if ( r4.h  .ne.             '.C'   ) call aborts ("ah")
+if ( r4.l  .ne.               '.D' ) call aborts ("al")
 
 end
