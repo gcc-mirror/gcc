6f1d347ceefca1bb684431a2deeaf6f8fa2a9069
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-05-16 09:59:25 +0000

    * tree.c (free_lang_data_in_decl): Also set target/optimization flags 	for thunks.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@236272 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree.c
+++ gcc/tree.c
@@ -5418,7 +5418,7 @@ free_lang_data_in_decl (tree decl)
 	      DECL_INITIAL (decl) = error_mark_node;
 	    }
 	}
-      if (gimple_has_body_p (decl))
+      if (gimple_has_body_p (decl) || (node && node->thunk.thunk_p))
 	{
 	  tree t;
 
