2a7d4c2049b1268470474294b0bac46d49b5325f
edlinger <edlinger@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-05-19 12:27:31 +0000

    2016-05-19  Bernd Edlinger  <bernd.edlinger@hotmail.de>
            * decl.c (finish_enum_value_list): Use the specified mode.
    
    testsuite:
    2016-05-19  Bernd Edlinger  <bernd.edlinger@hotmail.de>
    
            * c-c++-common/pr69669.c: Check the used mode.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@236458 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/c-c++-common/pr69669.c
+++ gcc/testsuite/c-c++-common/pr69669.c
@@ -1,5 +1,6 @@
 /* PR c/69669 */
 /* { dg-do compile } */
+/* { dg-options "-fdump-rtl-final" } */
 
 enum __attribute__((mode(QI))) E { F = 1 };
 
@@ -8,3 +9,5 @@ foo (enum E *x, int y)
 {
   *x = (enum E) y;
 }
+
+/* { dg-final { scan-rtl-dump-times "mem:QI" 1 "final" } } */
