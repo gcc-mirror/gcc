9e6edb946c0e9a2c530fbae3eeace148eca0de33
Jakub Jelinek <jakub@redhat.com>
2016-05-19 14:05:41 +0200

    Backport PRs 67394, 69687, 70481, 70492, 70498
    	Backported from mainline
    	2016-05-19  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/70498
    	* cp-demangle.c (d_expression_1): Formatting fix.
    
    	2016-05-02  Marcel Böhme  <boehme.marcel@gmail.com>
    
    	PR c++/70498
    	* cp-demangle.c: Parse numbers as integer instead of long to avoid
    	overflow after sanity checks. Include <limits.h> if available.
    	(INT_MAX): Define if necessary.
    	(d_make_template_param): Takes integer argument instead of long.
    	(d_make_function_param): Likewise.
    	(d_append_num): Likewise.
    	(d_identifier): Likewise.
    	(d_number): Parse as and return integer.
    	(d_compact_number): Handle overflow.
    	(d_source_name): Change variable type to integer for parsed number.
    	(d_java_resource): Likewise.
    	(d_special_name): Likewise.
    	(d_discriminator): Likewise.
    	(d_unnamed_type): Likewise.
    	* testsuite/demangle-expected: Add regression test cases.
    
    	2016-04-08  Marcel Böhme  <boehme.marcel@gmail.com>
    
    	PR c++/69687
    	* cplus-dem.c: Include <limits.h> if available.
    	(INT_MAX): Define if necessary.
    	(remember_type, remember_Ktype, register_Btype, string_need):
    	Abort if we detect cases where we the size of the allocation would
    	overflow.
    
    	PR c++/70492
    	* cplus-dem.c (gnu_special): Handle case where consume_count returns
    	-1.
    
    	2016-03-31  Mikhail Maltsev  <maltsevm@gmail.com>
    		    Marcel Bohme  <boehme.marcel@gmail.com>
    
    	PR c++/67394
    	PR c++/70481
    	* cplus-dem.c (squangle_mop_up): Zero bsize/ksize after freeing
    	btypevec/ktypevec.
    	* testsuite/demangle-expected: Add coverage tests.
    
    From-SVN: r236456

--- libiberty/cplus-dem.c
+++ libiberty/cplus-dem.c
@@ -56,6 +56,13 @@ void * malloc ();
 void * realloc ();
 #endif
 
+#ifdef HAVE_LIMITS_H
+#include <limits.h>
+#endif
+#ifndef INT_MAX
+# define INT_MAX       (int)(((unsigned int) ~0) >> 1)          /* 0x7FFFFFFF */ 
+#endif
+
 #include <demangle.h>
 #undef CURRENT_DEMANGLING_STYLE
 #define CURRENT_DEMANGLING_STYLE work->options
@@ -1224,11 +1231,13 @@ squangle_mop_up (struct work_stuff *work)
     {
       free ((char *) work -> btypevec);
       work->btypevec = NULL;
+      work->bsize = 0;
     }
   if (work -> ktypevec != NULL)
     {
       free ((char *) work -> ktypevec);
       work->ktypevec = NULL;
+      work->ksize = 0;
     }
 }
 
@@ -2986,6 +2995,11 @@ gnu_special (struct work_stuff *work, const char **mangled, string *declp)
 		      success = 1;
 		      break;
 		    }
+		  else if (n == -1)
+		    {
+		      success = 0;
+		      break;
+		    }
 		}
 	      else
 		{
@@ -4241,6 +4255,8 @@ remember_type (struct work_stuff *work, const char *start, int len)
 	}
       else
 	{
+          if (work -> typevec_size > INT_MAX / 2)
+	    xmalloc_failed (INT_MAX);
 	  work -> typevec_size *= 2;
 	  work -> typevec
 	    = XRESIZEVEC (char *, work->typevec, work->typevec_size);
@@ -4268,6 +4284,8 @@ remember_Ktype (struct work_stuff *work, const char *start, int len)
 	}
       else
 	{
+          if (work -> ksize > INT_MAX / 2)
+	    xmalloc_failed (INT_MAX);
 	  work -> ksize *= 2;
 	  work -> ktypevec
 	    = XRESIZEVEC (char *, work->ktypevec, work->ksize);
@@ -4297,6 +4315,8 @@ register_Btype (struct work_stuff *work)
 	}
       else
 	{
+          if (work -> bsize > INT_MAX / 2)
+	    xmalloc_failed (INT_MAX);
 	  work -> bsize *= 2;
 	  work -> btypevec
 	    = XRESIZEVEC (char *, work->btypevec, work->bsize);
@@ -4751,6 +4771,8 @@ string_need (string *s, int n)
   else if (s->e - s->p < n)
     {
       tem = s->p - s->b;
+      if (n > INT_MAX / 2 - tem)
+        xmalloc_failed (INT_MAX); 
       n += tem;
       n *= 2;
       s->b = XRESIZEVEC (char, s->b, n);
