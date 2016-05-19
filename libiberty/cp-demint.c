3240894f3036746c7cf0dbf1ff0e419c8c71840b
Jakub Jelinek <jakub@redhat.com>
2016-05-19 14:04:11 +0200

    backport: re PR other/61321 (demangler crash on casts in template parameters)
    	Backported from mainline
    	2015-11-27  Pedro Alves  <palves@redhat.com>
    
    	PR other/61321
    	PR other/61233
    	* demangle.h (enum demangle_component_type)
    	<DEMANGLE_COMPONENT_CONVERSION>: New value.
    
    	* cp-demangle.c (d_demangle_callback, d_make_comp): Handle
    	DEMANGLE_COMPONENT_CONVERSION.
    	(is_ctor_dtor_or_conversion): Handle DEMANGLE_COMPONENT_CONVERSION
    	instead of DEMANGLE_COMPONENT_CAST.
    	(d_operator_name): Return a DEMANGLE_COMPONENT_CONVERSION
    	component if handling a conversion.
    	(d_count_templates_scopes, d_print_comp_inner): Handle
    	DEMANGLE_COMPONENT_CONVERSION.
    	(d_print_comp_inner): Handle DEMANGLE_COMPONENT_CONVERSION instead
    	of DEMANGLE_COMPONENT_CAST.
    	(d_print_cast): Rename as ...
    	(d_print_conversion): ... this.  Adjust comments.
    	(d_print_cast): Rewrite - simply print the left subcomponent.
    	* cp-demint.c (cplus_demangle_fill_component): Handle
    	DEMANGLE_COMPONENT_CONVERSION.
    
    	* testsuite/demangle-expected: Add tests.
    
    From-SVN: r236455

--- libiberty/cp-demint.c
+++ libiberty/cp-demint.c
@@ -110,6 +110,7 @@ cplus_demangle_fill_component (struct demangle_component *p,
     case DEMANGLE_COMPONENT_IMAGINARY:
     case DEMANGLE_COMPONENT_VENDOR_TYPE:
     case DEMANGLE_COMPONENT_CAST:
+    case DEMANGLE_COMPONENT_CONVERSION:
       if (right != NULL)
 	return 0;
       break;
