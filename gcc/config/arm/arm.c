86e1c0b36f17f3a4153f982f1c5cf19bb867580e
Eric Botcazou <ebotcazou@adacore.com>
2016-05-20 20:49:33 +0000

    arm.c (arm_expand_prologue): Set the stack usage to 0 for naked functions.
    	* config/arm/arm.c (arm_expand_prologue): Set the stack usage to 0
    	for naked functions.
    	(thumb1_expand_prologue): Likewise.
    
    From-SVN: r236539

--- gcc/config/arm/arm.c
+++ gcc/config/arm/arm.c
@@ -21466,7 +21466,11 @@ arm_expand_prologue (void)
 
   /* Naked functions don't have prologues.  */
   if (IS_NAKED (func_type))
-    return;
+    {
+      if (flag_stack_usage_info)
+	current_function_static_stack_size = 0;
+      return;
+    }
 
   /* Make a copy of c_f_p_a_s as we may need to modify it locally.  */
   args_to_push = crtl->args.pretend_args_size;
@@ -24698,7 +24702,11 @@ thumb1_expand_prologue (void)
 
   /* Naked functions don't have prologues.  */
   if (IS_NAKED (func_type))
-    return;
+    {
+      if (flag_stack_usage_info)
+	current_function_static_stack_size = 0;
+      return;
+    }
 
   if (IS_INTERRUPT (func_type))
     {
