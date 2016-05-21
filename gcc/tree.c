beabe70551efb80ba423f1ba5053bf9a1f740b97
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-05-21 09:46:22 +0000

    * tree.c (array_at_struct_end_p): Look through MEM_REF.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@236557 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree.c
+++ gcc/tree.c
@@ -13076,9 +13076,28 @@ array_at_struct_end_p (tree ref)
       ref = TREE_OPERAND (ref, 0);
     }
 
+  tree size = NULL;
+
+  if (TREE_CODE (ref) == MEM_REF
+      && TREE_CODE (TREE_OPERAND (ref, 0)) == ADDR_EXPR)
+    {
+      size = TYPE_SIZE (TREE_TYPE (ref));
+      ref = TREE_OPERAND (TREE_OPERAND (ref, 0), 0);
+    }
+
   /* If the reference is based on a declared entity, the size of the array
      is constrained by its given domain.  (Do not trust commons PR/69368).  */
   if (DECL_P (ref)
+      /* Be sure the size of MEM_REF target match.  For example:
+
+	   char buf[10];
+	   struct foo *str = (struct foo *)&buf;
+
+	   str->trailin_array[2] = 1;
+
+	 is valid because BUF allocate enough space.  */
+
+      && (!size || operand_equal_p (DECL_SIZE (ref), size, 0))
       && !(flag_unconstrained_commons
 	   && TREE_CODE (ref) == VAR_DECL && DECL_COMMON (ref)))
     return false;
