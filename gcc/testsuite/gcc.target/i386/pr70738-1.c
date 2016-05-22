41a6c071413f59fb54484202b565b93afe75684a
Uros Bizjak <uros@gcc.gnu.org>
2016-05-22 19:20:10 +0200

    revert: re PR target/70738 (Add -mgeneral-regs-only option)
    Revert:
    
    gcc/
    
    	PR target/70738
    	* common/config/i386/i386-common.c
    	(OPTION_MASK_ISA_GENERAL_REGS_ONLY_UNSET): New.
    	(ix86_handle_option): Disable MPX, MMX, SSE and x87 instructions
    	for -mgeneral-regs-only.
    	* config/i386/i386.c (ix86_option_override_internal): Don't
    	enable x87 instructions if only the general registers are
    	allowed.
    	* config/i386/i386.opt: Add -mgeneral-regs-only.
    	* doc/invoke.texi: Document -mgeneral-regs-only.
    
    gcc/testsuite/
    
    	PR target/70738
    	* gcc.target/i386/pr70738-1.c: Likewise.
    	* gcc.target/i386/pr70738-2.c: Likewise.
    	* gcc.target/i386/pr70738-3.c: Likewise.
    	* gcc.target/i386/pr70738-4.c: Likewise.
    	* gcc.target/i386/pr70738-5.c: Likewise.
    	* gcc.target/i386/pr70738-6.c: Likewise.
    	* gcc.target/i386/pr70738-7.c: Likewise.
    	* gcc.target/i386/pr70738-8.c: Likewise.
    	* gcc.target/i386/pr70738-9.c: Likewise.
    
    From-SVN: r236570

--- gcc/testsuite/gcc.target/i386/pr70738-1.c
+++
@@ -1,9 +0,0 @@
-/* { dg-do compile { target { ! ia32 } } } */
-/* { dg-options "-msse2 -mgeneral-regs-only" } */
-
-typedef int int32x2_t __attribute__ ((__vector_size__ ((8))));
-
-int32x2_t test (int32x2_t a, int32x2_t b)
-{ /* { dg-error "SSE register return with SSE disabled" } */
-  return a + b;
-}
