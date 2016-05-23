ffd70e4a61cea6323d111867961763f441073c40
Christophe Lyon <christophe.lyon@linaro.org>
2016-05-23 09:05:34 +0000

    [ARM, AArch64] Add missing vrnd{,a,m,n,p,x} tests.
    2016-05-23  Christophe Lyon  <christophe.lyon@linaro.org>
    
    	* gcc.target/aarch64/advsimd-intrinsics/vrnd.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vrndX.inc: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vrnda.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vrndm.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vrndn.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vrndp.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vrndx.c: New.
    
    From-SVN: r236578

---
+++ gcc/testsuite/gcc.target/aarch64/advsimd-intrinsics/vrnd.c
@@ -0,0 +1,16 @@
+/* { dg-require-effective-target arm_v8_neon_ok } */
+/* { dg-add-options arm_v8_neon } */
+
+#include <arm_neon.h>
+#include "arm-neon-ref.h"
+#include "compute-ref-data.h"
+
+/* Expected results.  */
+VECT_VAR_DECL (expected, hfloat, 32, 2) [] = { 0xc1800000, 0xc1700000 };
+VECT_VAR_DECL (expected, hfloat, 32, 4) [] = { 0xc1800000, 0xc1700000,
+					       0xc1600000, 0xc1500000 };
+
+#define INSN vrnd
+#define TEST_MSG "VRND"
+
+#include "vrndX.inc"
