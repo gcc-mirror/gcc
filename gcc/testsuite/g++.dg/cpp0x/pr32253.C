396a1d10b3822a6da4794309a28ce3b1ea309325
Paolo Carlini <paolo@gcc.gnu.org>
2016-05-23 19:20:41 +0000

    pt.c (check_for_bare_parameter_packs): Improve error message location for expressions.
    /cp
    2016-05-23  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* pt.c (check_for_bare_parameter_packs): Improve error message
    	location for expressions.
    
    /testsuite
    2016-05-23  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* g++.dg/cpp0x/pr31445.C: Test column number too.
    	* g++.dg/cpp0x/pr32253.C: Likewise.
    	* g++.dg/cpp0x/variadic-ex13.C: Likewise.
    	* g++.dg/cpp0x/variadic36.C: Likewise.
    
    From-SVN: r236609

--- gcc/testsuite/g++.dg/cpp0x/pr32253.C
+++ gcc/testsuite/g++.dg/cpp0x/pr32253.C
@@ -1,7 +1,7 @@
 // { dg-do compile { target c++11 } }
 template<void (*... fp)()> struct A
 {
-  A() { fp(); } // { dg-error "not expanded|fp" }
+  A() { fp(); } // { dg-error "11:parameter packs not expanded|fp" }
 };
 
 void foo();
