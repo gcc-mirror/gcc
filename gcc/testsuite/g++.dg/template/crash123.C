b2dc9c3b2012f7bf7bd3e69bac8ea2fd1c470975
Paolo Carlini <paolo.carlini@oracle.com>
2016-05-24 21:32:29 +0000

    re PR c++/50436 (Crash or hang on invalid template code)
    2016-05-24  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/50436
    	* g++.dg/template/crash123.C: New.
    	* g++.dg/template/crash124.C: Likewise.
    
    From-SVN: r236671

---
+++ gcc/testsuite/g++.dg/template/crash123.C
@@ -0,0 +1,19 @@
+// PR c++/50436
+
+template <bool> struct VI {};
+template <typename T>
+struct IP
+{
+  static const bool r = IP<T>::r;  // { dg-error "depth" }
+};
+template <typename T> struct V
+{
+  VI<IP<T>::r> vi;
+};
+struct X;
+struct Y
+{
+  V<X> v;  // { dg-message "from here" }
+};
+
+// { dg-prune-output "compilation terminated" }
