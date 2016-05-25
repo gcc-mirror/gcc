d67dba4a9645daf1d479e15322ed5b6f80b2db5c
Jonathan Wakely <jwakely@redhat.com>
2016-05-25 16:43:02 +0100

    libstdc++/71036 Handle EEXIST in filesystem::create_directory
    Backport from mainline
    2016-05-10  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/71036
    	* src/filesystem/ops.cc (create_dir): Handle EEXIST from mkdir.
    	* testsuite/experimental/filesystem/operations/create_directory.cc:
    	New test.
    
    From-SVN: r236715

--- libstdc++-v3/src/filesystem/ops.cc
+++ libstdc++-v3/src/filesystem/ops.cc
@@ -660,22 +660,26 @@ namespace
   bool
   create_dir(const fs::path& p, fs::perms perm, std::error_code& ec)
   {
+    bool created = false;
 #ifdef _GLIBCXX_HAVE_SYS_STAT_H
     ::mode_t mode = static_cast<std::underlying_type_t<fs::perms>>(perm);
     if (::mkdir(p.c_str(), mode))
       {
-	ec.assign(errno, std::generic_category());
-	return false;
+	const int err = errno;
+	if (err != EEXIST || !is_directory(p))
+	  ec.assign(err, std::generic_category());
+	else
+	  ec.clear();
       }
     else
       {
 	ec.clear();
-	return true;
+	created = true;
       }
 #else
     ec = std::make_error_code(std::errc::not_supported);
-    return false;
 #endif
+    return created;
   }
 } // namespace
 
