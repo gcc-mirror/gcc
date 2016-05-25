8af01c66fb6123572bd534d8e912031fcb681a48
Jeff Law <law@redhat.com>
2016-05-25 16:25:35 -0600

    re PR tree-optimization/71272 (internal compiler error: in operator[], through tree-ssa-threadupdate.c:1981)
    	PR tree-optimization/71272
    	* tree-ssa-threadbackward.c (convert_and_register_jump_thread_path):
    	Update comments.  Add test for empty path.
    
    	PR tree-optimization/71272
    	* gcc.c-torture/compile/pr71272.c: new test.
    
    From-SVN: r236755

---
+++ gcc/testsuite/gcc.c-torture/compile/pr71272.c
@@ -0,0 +1,33 @@
+int a, b, c, d;
+
+int
+f1 (int p1, int p2)
+{
+  return p1 - p2;
+}
+
+void
+f2 (short p1, short p2)
+{
+  for (;;)
+    {
+      if (5 < (1 ^ p2))
+        for (; a;)
+          for (;;)
+            {
+              b = 0xE7BC92A3EDA01CD8 < (d = p2) || (0, 0);
+              break;
+            }
+      if (p1)
+        break;
+      p2 = 5;
+    }
+}
+
+void
+f3 (int x)
+{
+  int tmp = -2L;
+  c = f1 (90, x != 10);
+  f2 (c, tmp);
+}
