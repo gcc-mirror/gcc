2c995da92d3c973e0017e94bc7f6d782da8f8017
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-05-25 14:57:34 +0000

    PR c/71265 	* c-decl.c (c_make_fname_decl): Don't check seen_error.
    	* gcc.dg/noncompile/pr71265.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@236707 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c/c-decl.c
+++ gcc/c/c-decl.c
@@ -3989,7 +3989,7 @@ c_make_fname_decl (location_t loc, tree id, int type_dep)
 	 the __FUNCTION__ is believed to appear in K&R style function
 	 parameter declarator.  In that case we still don't have
 	 function_scope.  */
-      && (!seen_error () || current_function_scope))
+      && current_function_scope)
     {
       DECL_CONTEXT (decl) = current_function_decl;
       bind (id, decl, current_function_scope,
