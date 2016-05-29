b6d4f8e8549572f272cc03869a64d0d02af7ec70
Paolo Carlini <paolo.carlini@oracle.com>
2016-05-29 08:05:30 +0000

    re PR c++/71105 (lambdas with default captures improperly have function pointer conversions)
    /cp
    2016-05-29  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/71105
    	* lambda.c (maybe_add_lambda_conv_op): Early return also when
    	LAMBDA_EXPR_DEFAULT_CAPTURE_MODE != CPLD_NONE.
    
    /testsuite
    2016-05-29  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/71105
    	* g++.dg/cpp0x/lambda/lambda-conv11.C: New.
    	* g++.dg/cpp1y/lambda-conv1.C: Likewise.
    	* g++.dg/cpp1y/lambda-conv2.C: Likewise.
    
    From-SVN: r236859

---
+++ gcc/testsuite/g++.dg/cpp1y/lambda-conv1.C
@@ -0,0 +1,13 @@
+// PR c++/71105
+// { dg-do compile { target c++14 } }
+
+void foo()
+{
+  int i;
+  static_cast<void(*)(int)>([i](auto){});  // { dg-error "invalid static_cast" }
+  static_cast<void(*)(int)>([=](auto){});  // { dg-error "invalid static_cast" }
+  static_cast<void(*)(int)>([&](auto){});  // { dg-error "invalid static_cast" }
+  static_cast<float(*)(float)>([i](auto x){ return x; });  // { dg-error "invalid static_cast" }
+  static_cast<float(*)(float)>([=](auto x){ return x; });  // { dg-error "invalid static_cast" }
+  static_cast<float(*)(float)>([&](auto x){ return x; });  // { dg-error "invalid static_cast" }
+}
