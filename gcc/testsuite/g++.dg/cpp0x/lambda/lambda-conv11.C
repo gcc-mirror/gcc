061d916d09a1ad9c69c5969db15e64abd28d4cfe
Paolo Carlini <paolo@gcc.gnu.org>
2016-05-29 08:27:07 +0000

    re PR c++/71105 (lambdas with default captures improperly have function pointer conversions)
    /cp
    2016-05-29  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/71105
    	* lambda.c (maybe_add_lambda_conv_op): Early return also when
    	LAMBDA_EXPR_DEFAULT_CAPTURE_MODE != CPLD_NONE.
    
    /testsuite
    2016-05-29  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/71105
    	* g++.dg/cpp0x/lambda/lambda-conv11.C: New.
    	* g++.dg/cpp1y/lambda-conv1.C: Likewise.
    	* g++.dg/cpp1y/lambda-conv2.C: Likewise.
    
    From-SVN: r236860

---
+++ gcc/testsuite/g++.dg/cpp0x/lambda/lambda-conv11.C
@@ -0,0 +1,10 @@
+// PR c++/71105
+// { dg-do compile { target c++11 } }
+
+void foo()
+{
+  int i;
+  static_cast<void(*)()>([i]{});  // { dg-error "invalid static_cast" }
+  static_cast<void(*)()>([=]{});  // { dg-error "invalid static_cast" }
+  static_cast<void(*)()>([&]{});  // { dg-error "invalid static_cast" }
+}
