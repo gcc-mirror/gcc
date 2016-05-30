909ed6a89604db5baa55b62ad253a9bcd5d89c03
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-05-30 08:05:10 +0000

    backport "Remove assert in get_def_bb_for_const"
    2016-05-30  Tom de Vries  <tom@codesourcery.com>
    
    	backport:
    	2016-05-30  Tom de Vries  <tom@codesourcery.com>
    
    	PR tree-optimization/69067
    	* graphite-isl-ast-to-gimple.c (get_def_bb_for_const): Remove assert.
    
    	* gcc.dg/graphite/pr69067.c: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@236869 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/graphite/pr69067.c
@@ -0,0 +1,28 @@
+/* { dg-do link } */
+/* { dg-options " -O1 -floop-nest-optimize" } */
+/* { dg-additional-options "-flto" { target lto } } */
+
+int a1, c1, cr, kt;
+int aa[2];
+
+int
+ce (void)
+{
+  while (a1 < 1)
+    {
+      int g8;
+      for (g8 = 0; g8 < 3; ++g8)
+	if (c1 != 0)
+	  cr = aa[a1 * 2] = kt;
+      for (c1 = 0; c1 < 2; ++c1)
+	aa[c1] = cr;
+      ++a1;
+    }
+  return 0;
+}
+
+int
+main (void)
+{
+  return ce (aa);
+}
