5b8c120584ea8153ef5c230de9b6a3987f6a04da
Kirill Yukhin <kirill.yukhin@intel.com>
2016-05-31 08:05:24 +0000

    AVX-512. Limit constraint for scalar operand in split to AVX-512VL.
    PR target/71346
    gcc/
    	* config/i386/sse.md (define_insn_and_split "*vec_extractv4sf_0"): Use
    	`Yv' for scalar operand.
    testsuite/
    	* gcc.target/i386/pr71346.c: New test.
    
    From-SVN: r236909

---
+++ gcc/testsuite/gcc.target/i386/pr71346.c
@@ -0,0 +1,25 @@
+/* PR target/71346 */
+/* { dg-do compile { target { ! ia32 } }  } */
+/* { dg-options "-O2 -ftree-vectorize -ffast-math -march=knl" } */
+
+typedef int rvec[3];
+int a;
+float b, c, d, g;
+rvec *e, *f;
+void fn2(float h, float g);
+
+void
+fn1()
+{
+  float h;
+  for (; a; a++) {
+    h += e[a][0] * f[a][0];
+    b += e[a][0] * f[a][1];
+    c += e[a][2] * f[a][0];
+    d += e[a][2] * f[a][1];
+    g += e[a][2] * f[a][2];
+  }
+  fn2(h, g);
+}
+
+/* { dg-final { scan-assembler-not "vshufps\[ \\t\]+\[^\n\]*%\xmm(?:1\[6-9\]|\[2-3\]\[0-9\])" } } */
