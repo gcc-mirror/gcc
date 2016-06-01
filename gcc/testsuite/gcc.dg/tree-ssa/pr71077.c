ff66f6e835c22459bdf5b0bcd6d4d67a541cdae7
Patrick Palka <ppalka@gcc.gnu.org>
2016-06-01 02:36:27 +0000

    re PR tree-optimization/71077 (gcc -lto raises ICE)
    Fix PR tree-optimization/71077
    
    gcc/ChangeLog:
    
    	PR tree-optimization/71077
    	* tree-ssa-threadedge.c (simplify_control_stmt_condition_1): In
    	the combining step, use boolean_false_node and boolean_true_node
    	as the designated false/true return values.
    
    gcc/testsuite/ChangeLog:
    
    	PR tree-optimization/71077
    	* gcc.dg/tree-ssa/pr71077.c: New test.
    
    From-SVN: r236973

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr71077.c
@@ -0,0 +1,18 @@
+/* PR c++/71077  */
+/* { dg-do link { target { i?86-*-* x86_64-*-* } } }  */
+/* { dg-options "-O3 -flto -march=core-avx2" }  */
+
+int *a;
+int b, c, d, e;
+int sched_analyze(void) {
+ for (; b; b++) {
+   c = 0;
+   for (; c < 32; c++)
+     if (b & 1 << c)
+       a[b + c] = d;
+ }
+ return 0;
+}
+
+void schedule_insns(void) { e = sched_analyze(); }
+int main(void) { schedule_insns(); }
