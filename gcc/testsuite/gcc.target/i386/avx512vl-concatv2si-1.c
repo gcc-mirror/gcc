1e021dc33a06b5127db26a347547b97e623a0e19
Jakub Jelinek <jakub@redhat.com>
2016-06-02 13:01:31 +0200

    sse.md (*vec_concatv2si_sse4_1): Add avx512dq v=Yv,rm alternative.
    	* config/i386/sse.md (*vec_concatv2si_sse4_1): Add avx512dq v=Yv,rm
    	alternative.  Change x=x,x alternative to v=Yv,Yv and x=rm,C
    	alternative to v=rm,C.
    
    	* gcc.target/i386/avx512dq-concatv2si-1.c: New test.
    	* gcc.target/i386/avx512vl-concatv2si-1.c: New test.
    
    From-SVN: r237030

---
+++ gcc/testsuite/gcc.target/i386/avx512vl-concatv2si-1.c
@@ -0,0 +1,43 @@
+/* { dg-do compile { target { ! ia32 } } } */
+/* { dg-options "-O2 -mavx512vl -mno-avx512dq -masm=att" } */
+
+typedef int V __attribute__((vector_size (8)));
+
+void
+f1 (int x, int y)
+{
+  register int a __asm ("xmm16");
+  register int b __asm ("xmm17");
+  register V c __asm ("xmm3");
+  a = x;
+  b = y;
+  asm volatile ("" : "+v" (a), "+v" (b));
+  c = (V) { a, b };
+  asm volatile ("" : "+v" (c));
+}
+
+/* { dg-final { scan-assembler "vpunpckldq\[^\n\r]*%xmm17\[^\n\r]*%xmm16\[^\n\r]*%xmm3" } } */
+
+void
+f2 (int x, int y)
+{
+  register int a __asm ("xmm16");
+  register V c __asm ("xmm3");
+  a = x;
+  asm volatile ("" : "+v" (a));
+  c = (V) { a, y };
+  asm volatile ("" : "+v" (c));
+}
+
+void
+f3 (int x, int *y)
+{
+  register int a __asm ("xmm16");
+  register V c __asm ("xmm3");
+  a = x;
+  asm volatile ("" : "+v" (a));
+  c = (V) { a, *y };
+  asm volatile ("" : "+v" (c));
+}
+
+/* { dg-final { scan-assembler-not "vpinsrd\[^\n\r]*\\\$1\[^\n\r]*%xmm16\[^\n\r]*%xmm3" } } */
