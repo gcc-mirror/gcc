61aed6ee6bdb93f7d6c6daa52faada3b48991f5e
Jose E. Marchesi <jose.marchesi@oracle.com>
2016-06-08 21:13:37 +0200

    backport: driver-sparc.c (cpu_names): Fix the entry for the SPARC-M7 and add an entry for SPARC-S7 cpus (Sonoma).
    	Backport from mainline
    	2016-06-08  Jose E. Marchesi  <jose.marchesi@oracle.com>
    
    	* config/sparc/driver-sparc.c (cpu_names): Fix the entry for the
    	SPARC-M7 and add an entry for SPARC-S7 cpus (Sonoma).
    
    From-SVN: r237239

--- gcc/config/sparc/driver-sparc.c
+++ gcc/config/sparc/driver-sparc.c
@@ -57,7 +57,6 @@ static const struct cpu_names {
   { "UltraSPARC-T2+",	"niagara2" },
   { "SPARC-T3",		"niagara3" },
   { "SPARC-T4",		"niagara4" },
-  { "SPARC-M7",		"niagara7" },
 #else
   { "SuperSparc",	"supersparc" },
   { "HyperSparc",	"hypersparc" },
@@ -74,9 +73,10 @@ static const struct cpu_names {
   { "UltraSparc T2",	"niagara2" },
   { "UltraSparc T3",	"niagara3" },
   { "UltraSparc T4",	"niagara4" },
-  { "UltraSparc M7",	"niagara7" },
   { "LEON",		"leon3" },
 #endif
+  { "SPARC-M7",		"niagara7" },
+  { "SPARC-S7",		"niagara7" },
   { NULL,	NULL }
   };
 
