36d579866680fd2b45800b072527a9cce93235d0
Richard Biener <rguenther@suse.de>
2016-06-09 11:36:22 +0000

    re PR tree-optimization/71462 (gcc ICE at -O3  on valid code on x86_64-linux-gnu with “seg fault”)
    2016-06-09  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/71462
    	* tree-ssa-loop-manip.c (find_uses_to_rename): Guard against
    	removed blocks.
    
    	* gcc.dg/torture/pr71462.c: New testcase.
    
    From-SVN: r237254

---
+++ gcc/testsuite/gcc.dg/torture/pr71462.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+
+short a;
+long b;
+void fn1()
+{
+  int c = a = 1;
+  for (; a; a++)
+    {
+      for (; 9 <= 8;)
+	for (;;) {
+	    a = 20;
+	    for (; a <= 35; a++)
+	      ;
+line:;
+	}
+      if ((c += 264487869) == 9)
+	{
+	  unsigned *d = 0;
+	  for (; b;)
+	    d = (unsigned *)&c;
+	  if (d)
+	    for (;;)
+	      ;
+	}
+    }
+  goto line;
+}
