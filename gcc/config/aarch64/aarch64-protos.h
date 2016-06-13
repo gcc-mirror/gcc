c23895494204af3a0727d081e899347985311a34
evandro <evandro@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-06-13 19:02:52 +0000

    [AArch64] Add more choices for the reciprocal square root approximation
    Allow a target to prefer such operation depending on the operation mode.
    
    gcc/
    	* config/aarch64/aarch64-protos.h
    	(AARCH64_APPROX_MODE): New macro.
    	(AARCH64_APPROX_{NONE,ALL}): Likewise.
    	(cpu_approx_modes): New structure.
    	(tune_params): New member "approx_modes".
    	* config/aarch64/aarch64-tuning-flags.def
    	(AARCH64_EXTRA_TUNE_APPROX_RSQRT): Remove macro.
    	* config/aarch64/aarch64.c
    	({generic,exynosm1,xgene1}_approx_modes): New core
    	"cpu_approx_modes" structures.
    	(generic_tunings): New member "approx_modes".
    	(cortexa35_tunings): Likewise.
    	(cortexa53_tunings): Likewise.
    	(cortexa57_tunings): Likewise.
    	(cortexa72_tunings): Likewise.
    	(exynosm1_tunings): Likewise.
    	(thunderx_tunings): Likewise.
    	(xgene1_tunings): Likewise.
    	(use_rsqrt_p): New argument for the mode and use new member from
    	"tune_params".
    	(aarch64_builtin_reciprocal): Devise mode from builtin.
    	(aarch64_optab_supported_p): New argument for the mode.
    	* doc/invoke.texi (-mlow-precision-recip-sqrt): Reword description.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@237395 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/aarch64/aarch64-protos.h
+++ gcc/config/aarch64/aarch64-protos.h
@@ -178,6 +178,23 @@ struct cpu_branch_cost
   const int unpredictable;  /* Unpredictable branch or optimizing for speed.  */
 };
 
+/* Control approximate alternatives to certain FP operators.  */
+#define AARCH64_APPROX_MODE(MODE) \
+  ((MIN_MODE_FLOAT <= (MODE) && (MODE) <= MAX_MODE_FLOAT) \
+   ? (1 << ((MODE) - MIN_MODE_FLOAT)) \
+   : (MIN_MODE_VECTOR_FLOAT <= (MODE) && (MODE) <= MAX_MODE_VECTOR_FLOAT) \
+     ? (1 << ((MODE) - MIN_MODE_VECTOR_FLOAT \
+	      + MAX_MODE_FLOAT - MIN_MODE_FLOAT + 1)) \
+     : (0))
+#define AARCH64_APPROX_NONE (0)
+#define AARCH64_APPROX_ALL (-1)
+
+/* Allowed modes for approximations.  */
+struct cpu_approx_modes
+{
+  const unsigned int recip_sqrt; /* Reciprocal square root.  */
+};
+
 struct tune_params
 {
   const struct cpu_cost_table *insn_extra_cost;
@@ -185,6 +202,7 @@ struct tune_params
   const struct cpu_regmove_cost *regmove_cost;
   const struct cpu_vector_cost *vec_costs;
   const struct cpu_branch_cost *branch_costs;
+  const struct cpu_approx_modes *approx_modes;
   int memmov_cost;
   int issue_rate;
   unsigned int fusible_ops;
