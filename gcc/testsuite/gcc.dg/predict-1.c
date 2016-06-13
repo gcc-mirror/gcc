ad23a23ed29fb6ba8e60f63dcd879ca2284bf3ac
Martin Liska <mliska@suse.cz>
2016-06-13 16:03:51 +0200

    Make 'loop iv compare' heuristics scanning more precise
    	* gcc.dg/predict-1.c: Distinguish between "loop iv compare"
    	and "guess loop iv compared" heuristics.
    	* gcc.dg/predict-2.c: Likewise.
    	* gcc.dg/predict-3.c: Likewise.
    	* gcc.dg/predict-4.c: Likewise.
    	* gcc.dg/predict-5.c: Likewise.
    	* gcc.dg/predict-6.c: Likewise.
    
    From-SVN: r237380

--- gcc/testsuite/gcc.dg/predict-1.c
+++ gcc/testsuite/gcc.dg/predict-1.c
@@ -23,4 +23,4 @@ void foo (int bound)
     }
 }
 
-/* { dg-final { scan-tree-dump-times "loop iv compare heuristics of edge\[^:\]*: 2.0%" 5 "profile_estimate"} } */
+/* { dg-final { scan-tree-dump-times "guess loop iv compare heuristics of edge\[^:\]*: 2.0%" 5 "profile_estimate"} } */
