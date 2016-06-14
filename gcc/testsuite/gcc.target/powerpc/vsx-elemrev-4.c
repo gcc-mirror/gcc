6e433d0b2c4079f4e43363262bc1caf0b1dd0fb8
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2016-06-14 23:13:27 +0000

    vsx-elemrev-2.c: Change effective target requirements, and disable for AIX for now.
    2016-06-14  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* gcc.target/powerpc/vsx-elemrev-2.c: Change effective target
    	requirements, and disable for AIX for now.
    	* gcc.target/powerpc/vsx-elemrev-4.c: Likewise.
    
    From-SVN: r237464

--- gcc/testsuite/gcc.target/powerpc/vsx-elemrev-4.c
+++ gcc/testsuite/gcc.target/powerpc/vsx-elemrev-4.c
@@ -1,7 +1,8 @@
 /* { dg-do compile { target { powerpc64-*-* } } } */
 /* { dg-skip-if "do not override mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power9" } } */
 /* { dg-options "-mcpu=power9 -O0" } */
-/* { dg-require-effective-target p9vector_hw } */
+/* { dg-require-effective-target powerpc_p9vector_ok } */
+/* { dg-skip-if "" { powerpc*-*-aix* } { "*" } { "" } } */
 /* { dg-final { scan-assembler-times "lxvx" 40 } } */
 /* { dg-final { scan-assembler-times "stxvx" 40 } } */
 
