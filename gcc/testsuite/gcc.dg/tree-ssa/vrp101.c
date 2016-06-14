eb912adb3450436e2ec82782149d9886f2e50fa7
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-06-14 07:22:04 +0000

    2016-06-14  Richard Biener  <rguenther@suse.de>
    	PR tree-optimization/71521
    	* tree-vrp.c (extract_range_from_binary_expr_1): Guard
    	division int_const_binop against zero divisor.
    
    	* gcc.dg/tree-ssa/vrp101.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@237425 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/vrp101.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+int x = 1;
+
+int main ()
+{
+  int t = (1/(1>=x))>>1;
+  if (t != 0) __builtin_abort();
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump "<bb 2>:\[\n\r \]*return 0;" "optimized" } } */
