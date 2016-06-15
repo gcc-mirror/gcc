8f482165c975f0a8410e3c8ecb0c011d409062d9
Alan Hayward <alan.hayward@arm.com>
2016-06-15 10:53:01 +0000

    re PR tree-optimization/71439 (wrong code at -O3 in 32-bit and 64-bit mode on x86_64-linux-gnu)
    2016-06-15  Alan Hayward  <alan.hayward@arm.com>
    
    gcc/
    	PR tree-optimization/71439
    	* tree-vect-loop.c (vect_analyze_loop_operations): Additional check for
    	live PHIs.
    
    testsuite/
    	PR tree-optimization/71439
    	* gcc.dg/vect/pr71439.c: New
    
    From-SVN: r237476

---
+++ gcc/testsuite/gcc.dg/vect/pr71439.c
@@ -0,0 +1,17 @@
+#include "tree-vect.h"
+
+int a, b, c;
+short fn1(int p1, int p2) { return p1 + p2; }
+
+int main() {
+  a = 0;
+  for (; a < 30; a = fn1(a, 4)) {
+    c = b;
+    b = 6;
+  }
+
+  if (c != 6)
+    abort ();
+
+  return 0;
+}
