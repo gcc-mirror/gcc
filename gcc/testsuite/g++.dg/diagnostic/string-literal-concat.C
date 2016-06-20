ecbea05a82f27e54e4cc17a60a456fed58c51631
dmalcolm <dmalcolm@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-06-20 16:37:28 +0000

    C++ FE: Show both locations in string literal concatenation error
    gcc/cp/ChangeLog:
    	* parser.c (cp_parser_string_literal): Convert non-standard
    	concatenation error to directly use a rich_location, and
    	use that to add the location of the first literal to the
    	diagnostic.
    
    gcc/testsuite/ChangeLog:
    	* g++.dg/diagnostic/string-literal-concat.C: New test case.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@237608 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/diagnostic/string-literal-concat.C
@@ -0,0 +1,23 @@
+/* { dg-options "-fdiagnostics-show-caret -std=c++11" } */
+
+const void *s = u8"a"  u"b";  // { dg-error "24: non-standard concatenation" }
+/* { dg-begin-multiline-output "" }
+ const void *s = u8"a"  u"b";
+                 ~~~~~  ^~~~
+   { dg-end-multiline-output "" } */
+
+const void *s2 = u"a"  u"b"  u8"c";  // { dg-error "30: non-standard concatenation" }
+/* { dg-begin-multiline-output "" }
+ const void *s2 = u"a"  u"b"  u8"c";
+                        ~~~~  ^~~~~
+  { dg-end-multiline-output "" } */
+
+#define TEST_U8_LITERAL u8"a"
+
+const void *s3 = TEST_U8_LITERAL u8"b";
+
+const void *s4 = TEST_U8_LITERAL u"b"; // { dg-error "34: non-standard concatenation" }
+/* { dg-begin-multiline-output "" }
+ const void *s4 = TEST_U8_LITERAL u"b";
+                                  ^~~~
+  { dg-end-multiline-output "" } */
