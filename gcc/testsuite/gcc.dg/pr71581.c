a219950f129efff6ac5476aadfcc44ccf233edfc
Jakub Jelinek <jakub@redhat.com>
2016-06-21 09:13:52 +0200

    backport: re PR middle-end/71581 (ICE on valid code on x86_64-linux-gnu with -Wuninitialized (Segmentation fault))
    	Backported from mainline
    	2016-06-20  Jakub Jelinek  <jakub@redhat.com>
    
    	PR middle-end/71581
    	* tree-ssa-uninit.c (warn_uninit): If EXPR and VAR are NULL,
    	see if T isn't anonymous SSA_NAME with COMPLEX_EXPR created
    	for conversion of scalar user var to complex type and use the
    	underlying SSA_NAME_VAR in that case.  If EXPR is still NULL,
    	punt.
    
    	* gcc.dg/pr71581.c: New test.
    
    From-SVN: r237632

---
+++ gcc/testsuite/gcc.dg/pr71581.c
@@ -0,0 +1,24 @@
+/* PR middle-end/71581 */
+/* { dg-do compile } */
+/* { dg-options "-Wuninitialized" } */
+
+_Complex float
+f1 (void)
+{
+  float x;
+  return x;	/* { dg-warning "is used uninitialized in this function" } */
+}
+
+_Complex double
+f2 (void)
+{
+  double x;
+  return x;	/* { dg-warning "is used uninitialized in this function" } */
+}
+
+_Complex int
+f3 (void)
+{
+  int x;
+  return x;	/* { dg-warning "is used uninitialized in this function" } */
+}
