a15584d4d6a0c2ad2f96e15b6c91ea0d04390ef0
kelvin <kelvin@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-06-21 14:09:12 +0000

    gcc/testsuite/ChangeLog:
    2016-06-21  Kelvin Nilsen  <kelvin@gcc.gnu.org>
    
    	* gcc.target/powerpc/vadsdu-0.c: Replace
    	dg-require-effective-target directive to allow test to run on more
    	platforms, and add dg-skip-if directive to disable test on aix
    	platforms because of known incompatibilities.
    	* gcc.target/powerpc/vadsdu-1.c: Likewise.
    	* gcc.target/powerpc/vadsdu-2.c: Likewise.
    	* gcc.target/powerpc/vadsdu-3.c: Likewise.
    	* gcc.target/powerpc/vadsdu-4.c: Likewise.
    	* gcc.target/powerpc/vadsdu-5.c: Likewise.
    	* gcc.target/powerpc/vadsdub-1.c: Likewise.
    	* gcc.target/powerpc/vadsdub-2.c: Replace
    	dg-require-effective-target directive to allow test to run on more
    	platforms, and add dg-skip-if directive to disable test on aix
    	platforms because of known incompatibilities.
    	(doAbsoluteDifferenceUnsigned): Replace __builtin_vec_vadub call
    	with vec_absdb call to differentiate this test from vadsdub-1.c.
    	* gcc.target/powerpc/vadsduh-1.c: Replace
    	dg-require-effective-target directive to allow test to run on more
    	platforms, and add dg-skip-if directive to disable test on aix
    	platforms because of known incompatibilities.
    	* gcc.target/powerpc/vadsduh-2.c: Likewise.
    	* gcc.target/powerpc/vadsduw-1.c: Likewise.
    	* gcc.target/powerpc/vadsduw-2.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@237646 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/vadsduw-2.c
+++ gcc/testsuite/gcc.target/powerpc/vadsduw-2.c
@@ -1,6 +1,7 @@
 /* { dg-do compile { target { powerpc*-*-* } } } */
 /* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power9" } } */
-/* { dg-require-effective-target p9vector_hw } */
+/* { dg-require-effective-target powerpc_p9vector_ok } */
+/* { dg-skip-if "" { powerpc*-*-aix* } } */
 /* { dg-options "-mcpu=power9" } */
 
 /* This test should succeed on both 32- and 64-bit configurations.  */
