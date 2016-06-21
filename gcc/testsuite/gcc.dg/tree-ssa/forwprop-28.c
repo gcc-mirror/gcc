0609abdad81e26678eecdfcaaeb785b6343f51ca
Trevor Saunders <tbsaunde+gcc@tbsaunde.org>
2016-06-21 21:57:49 +0000

    remove mep-* support
    libgcc/ChangeLog:
    
    2016-06-21  Trevor Saunders  <tbsaunde+gcc@tbsaunde.org>
    
    	* config.host: Remove support for mep-*.
    	* config/mep/lib1funcs.S: Remove.
    	* config/mep/lib2funcs.c: Remove.
    	* config/mep/t-mep: Remove.
    	* config/mep/tramp.c: Remove.
    
    gcc/ChangeLog:
    
    2016-06-21  Trevor Saunders  <tbsaunde+gcc@tbsaunde.org>
    
    	* common/config/mep/mep-common.c: Remove.
    	* config.gcc: Remove mep-* support.
    	* config/mep/constraints.md: Remove.
    	* config/mep/default.h: Remove.
    	* config/mep/intrinsics.h: Remove.
    	* config/mep/intrinsics.md: Remove.
    	* config/mep/ivc2-template.h: Remove.
    	* config/mep/mep-c5.cpu: Remove.
    	* config/mep/mep-core.cpu: Remove.
    	* config/mep/mep-default.cpu: Remove.
    	* config/mep/mep-ext-cop.cpu: Remove.
    	* config/mep/mep-intrin.h: Remove.
    	* config/mep/mep-ivc2.cpu: Remove.
    	* config/mep/mep-pragma.c: Remove.
    	* config/mep/mep-protos.h: Remove.
    	* config/mep/mep.c: Remove.
    	* config/mep/mep.cpu: Remove.
    	* config/mep/mep.h: Remove.
    	* config/mep/mep.md: Remove.
    	* config/mep/mep.opt: Remove.
    	* config/mep/predicates.md: Remove.
    	* config/mep/t-mep: Remove.
    	* doc/install.texi: Remove mep-* documentation.
    	* doc/md.texi: Likewise.
    
    gcc/testsuite/ChangeLog:
    
    2016-06-21  Trevor Saunders  <tbsaunde+gcc@tbsaunde.org>
    
    	* gcc.dg/tree-ssa/forwprop-28.c: Remove mep-* support.
    	* gcc.dg/tree-ssa/reassoc-32.c: Likewise.
    	* gcc.dg/tree-ssa/reassoc-33.c: Likewise.
    	* gcc.dg/tree-ssa/reassoc-34.c: Likewise.
    	* gcc.dg/tree-ssa/reassoc-35.c: Likewise.
    	* gcc.dg/tree-ssa/reassoc-36.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-ifcombine-ccmp-1.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-ifcombine-ccmp-2.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-ifcombine-ccmp-3.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-ifcombine-ccmp-4.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-ifcombine-ccmp-5.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-ifcombine-ccmp-6.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-thread-11.c: Likewise.
    	* gcc.dg/tree-ssa/vrp87.c: Likewise.
    	* lib/target-supports.exp: Likewise.
    
    contrib/ChangeLog:
    
    2016-06-21  Trevor Saunders  <tbsaunde+gcc@tbsaunde.org>
    
    	* config-list.mk: Stop testing mep-elf.
    
    libstdc++-v3/ChangeLog:
    
    2016-06-21  Trevor Saunders  <tbsaunde+gcc@tbsaunde.org>
    
    	* configure.host: Remove mep-* support.
    
    From-SVN: r237666

--- gcc/testsuite/gcc.dg/tree-ssa/forwprop-28.c
+++ gcc/testsuite/gcc.dg/tree-ssa/forwprop-28.c
@@ -1,6 +1,6 @@
 /* Setting LOGICAL_OP_NON_SHORT_CIRCUIT to 0 leads to two conditional jumps
    when evaluating an && condition.  VRP is not able to optimize this.  */
-/* { dg-do compile { target { ! { logical_op_short_circuit || { m68k*-*-* mmix*-*-* mep*-*-* bfin*-*-* v850*-*-* moxie*-*-* cris*-*-* m32c*-*-* fr30*-*-* mcore*-*-* powerpc*-*-* xtensa*-*-* hppa*-*-* } } } } } */
+/* { dg-do compile { target { ! { logical_op_short_circuit || { m68k*-*-* mmix*-*-* bfin*-*-* v850*-*-* moxie*-*-* cris*-*-* m32c*-*-* fr30*-*-* mcore*-*-* powerpc*-*-* xtensa*-*-* hppa*-*-* } } } } } */
 /* { dg-options "-O2 -fdump-tree-forwprop1-details" } */
 
 extern char *frob (void);
