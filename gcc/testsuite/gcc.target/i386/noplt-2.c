5aaec397e6fd924a4b2fdb1d8fd3345e2a2bc407
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-06-24 11:17:14 +0000

    Enable non-PIC noplt tests on 32-bit x86 target
    Since non-PIC noplt works on 32-bit x86 target now with assembler/linker
    support, enable non-PIC noplt tests on 32-bit x86 target.  main in
    noplt-2.c and noplt-4.c are renamed to bar to avoid stack re-alignment
    in main for 32-bit target, which disables tailcall optimization.
    
    	* gcc.target/i386/noplt-1.c: Don't disable for ia32.  Scan for
    	ia32 if R_386_GOT32X relocation is supported.
    	* gcc.target/i386/noplt-3.c: Likewise.
    	* gcc.target/i386/noplt-2.c: Likewise.
    	(main): Renamed to ...
    	(bar): This.
    	* gcc.target/i386/noplt-4.c: Likewise.
    	(main): Renamed to ...
    	(bar): This.
    	* gcc.target/i386/pr67400-3.c: Don't disable for ia32.
    	* gcc.target/i386/pr67400-5.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@237756 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/noplt-2.c
+++ gcc/testsuite/gcc.target/i386/noplt-2.c
@@ -1,13 +1,14 @@
-/* { dg-do compile { target { *-*-linux* && { ! ia32 } } } } */
+/* { dg-do compile { target *-*-linux* } } */
 /* { dg-options "-O2 -fno-pic" } */
 
 
 __attribute__ ((noplt))
 int foo();
 
-int main()
+int bar()
 {
   return foo();
 }
 
-/* { dg-final { scan-assembler "jmp\[ \t\]\\*.*foo.*@GOTPCREL\\(%rip\\)" } } */ 
+/* { dg-final { scan-assembler "jmp\[ \t\]*.foo@GOTPCREL" { target { ! ia32 } } } } */
+/* { dg-final { scan-assembler "jmp\[ \t\]*.foo@GOT" { target { ia32 && got32x_reloc } } } } */
--- gcc/testsuite/gcc.target/i386/noplt-4.c
+++ gcc/testsuite/gcc.target/i386/noplt-4.c
@@ -1,11 +1,12 @@
-/* { dg-do compile { target { *-*-linux* && { ! ia32 } } } } */
+/* { dg-do compile { target *-*-linux* } } */
 /* { dg-options "-O2 -fno-pic -fno-plt" } */
 
 int foo();
 
-int main()
+int bar()
 {
   return foo();
 }
 
-/* { dg-final { scan-assembler "jmp\[ \t\]\\*.*foo.*@GOTPCREL\\(%rip\\)" } } */
+/* { dg-final { scan-assembler "jmp\[ \t\]*.foo@GOTPCREL" { target { ! ia32 } } } } */
+/* { dg-final { scan-assembler "jmp\[ \t\]*.foo@GOT" { target { ia32 && got32x_reloc } } } } */
