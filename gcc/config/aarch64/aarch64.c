ee446d9fbb8226a98a939f070186e4ac3e5e333f
Jim Wilson <jim.wilson@linaro.org>
2016-06-29 18:01:55 +0000

    Add qdf24xx base tuning support.
    	gcc/
    	* config/aarch64/aarch64-cores.def (qdf24xx): Use qdf24xx tuning.
    	* config/aarch64/aarch64.c (qdf24xx_addrcost_table,
    	qdf24xx_regmove_cost, qdf24xx_tunings): New.
    	* config/arm/aarch64-cost-tables.h (qdf24xx_extra_costs): New.
    	* config/arm/arm-cores.def (qdf24xx): Use qdf24xx tuning.
    	* config/arm/arm.c (arm_qdf24xx_tune): New.
    
    	gcc/testsuite/
    	* gcc.dg/asr_div1.c: Add aarch64 specific dg-options.
    
    From-SVN: r237857

--- gcc/config/aarch64/aarch64.c
+++ gcc/config/aarch64/aarch64.c
@@ -250,6 +250,22 @@ static const struct cpu_addrcost_table xgene1_addrcost_table =
   0, /* imm_offset  */
 };
 
+static const struct cpu_addrcost_table qdf24xx_addrcost_table =
+{
+    {
+      1, /* hi  */
+      0, /* si  */
+      0, /* di  */
+      1, /* ti  */
+    },
+  0, /* pre_modify  */
+  0, /* post_modify  */
+  0, /* register_offset  */
+  0, /* register_sextend  */
+  0, /* register_zextend  */
+  0 /* imm_offset  */
+};
+
 static const struct cpu_regmove_cost generic_regmove_cost =
 {
   1, /* GP2GP  */
@@ -306,6 +322,15 @@ static const struct cpu_regmove_cost xgene1_regmove_cost =
   8, /* GP2FP  */
   8, /* FP2GP  */
   2 /* FP2FP  */
+};
+
+static const struct cpu_regmove_cost qdf24xx_regmove_cost =
+{
+  2, /* GP2GP  */
+  /* Avoid the use of int<->fp moves for spilling.  */
+  6, /* GP2FP  */
+  6, /* FP2GP  */
+  4 /* FP2FP  */
 };
 
 /* Generic costs for vector insn classes.  */
@@ -645,6 +670,32 @@ static const struct tune_params xgene1_tunings =
   0,	/* cache_line_size.  */
   tune_params::AUTOPREFETCHER_OFF,	/* autoprefetcher_model.  */
   (AARCH64_EXTRA_TUNE_NONE)	/* tune_flags.  */
+};
+
+static const struct tune_params qdf24xx_tunings =
+{
+  &qdf24xx_extra_costs,
+  &qdf24xx_addrcost_table,
+  &qdf24xx_regmove_cost,
+  &generic_vector_cost,
+  &generic_branch_cost,
+  &generic_approx_modes,
+  4, /* memmov_cost  */
+  4, /* issue_rate  */
+  (AARCH64_FUSE_MOV_MOVK | AARCH64_FUSE_ADRP_ADD
+   | AARCH64_FUSE_MOVK_MOVK), /* fuseable_ops  */
+  16,	/* function_align.  */
+  8,	/* jump_align.  */
+  16,	/* loop_align.  */
+  2,	/* int_reassoc_width.  */
+  4,	/* fp_reassoc_width.  */
+  1,	/* vec_reassoc_width.  */
+  2,	/* min_div_recip_mul_sf.  */
+  2,	/* min_div_recip_mul_df.  */
+  0,	/* max_case_values.  */
+  64,	/* cache_line_size.  */
+  tune_params::AUTOPREFETCHER_STRONG,	/* autoprefetcher_model.  */
+  (AARCH64_EXTRA_TUNE_NONE)		/* tune_flags.  */
 };
 
 /* Support for fine-grained override of the tuning structures.  */
