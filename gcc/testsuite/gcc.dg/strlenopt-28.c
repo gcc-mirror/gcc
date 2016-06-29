83b23cdfe41e22ff26f10f628bac12b17329b9e2
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-06-29 08:47:46 +0000

    PR tree-optimization/71625 	* tree-ssa-strlen.c (get_addr_stridx): Add PTR argument.  Assume list 	is sorted by ascending list->offset.  If PTR is non-NULL and there is 	previous strinfo, call get_stridx_plus_constant. 	(get_stridx): Pass exp as second argument to get_addr_stridx. 	(addr_stridxptr): Add missing list = list->next, so that there can be 	more than one entries in the list.  Bump limit from 16 to 32.  Ensure 	the list is sorted by ascending list->offset. 	(get_stridx_plus_constant): Adjust so that it can be also called with 	ADDR_EXPR instead of SSA_NAME as PTR. 	(handle_char_store): Pass NULL_TREE as second argument to 	get_addr_stridx.
    	* gcc.dg/strlenopt-28.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@237841 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/strlenopt-28.c
@@ -0,0 +1,59 @@
+/* { dg-do run } */
+/* { dg-options "-O2 -fdump-tree-strlen" } */
+
+#include "strlenopt.h"
+
+volatile int v;
+
+size_t
+f1 (void)
+{
+  char a[30];
+  v += 1;
+  memcpy (a, "1234567", 8);
+  memcpy (a + 7, "89abcdefg", 10);
+  memcpy (a + 16, "h", 2);
+  return strlen (a);	// This strlen should be optimized into 17.
+}
+
+size_t
+f2 (void)
+{
+  char a[30];
+  v += 2;
+  strcpy (a, "1234567");
+  strcpy (a + 7, "89abcdefg");
+  strcpy (a + 16, "h");
+  return strlen (a);	// This strlen should be optimized into 17.
+}
+
+size_t
+f3 (char *a)
+{
+  v += 3;
+  memcpy (a, "1234567", 8);
+  memcpy (a + 7, "89abcdefg", 10);
+  memcpy (a + 16, "h", 2);
+  return strlen (a);	// This strlen should be optimized into 17.
+}
+
+size_t
+f4 (char *a)
+{
+  v += 4;
+  strcpy (a, "1234567");
+  strcpy (a + 7, "89abcdefg");
+  strcpy (a + 16, "h");
+  return strlen (a);	// This strlen should be optimized into 17.
+}
+
+int
+main ()
+{
+  char a[30];
+  if (f1 () != 17 || f2 () != 17 || f3 (a) != 17 || f4 (a) != 17)
+    abort ();
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "strlen \\(" 0 "strlen" } } */
