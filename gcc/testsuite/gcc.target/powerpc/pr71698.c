147f055117006f453e37c2c019a2223fe4eed867
bergner <bergner@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-01 16:06:26 +0000

    gcc/ 	Backport from trunk 	2016-07-01  Peter Bergner  <bergner@vnet.ibm.com>
    	PR target/71698
    	* config/rs6000/rs6000.c (rs6000_secondary_reload_simple_move): Disallow
    	TDmode values.
    
    gcc/testsuite/
    	Backport from trunk
    	2016-07-01  Peter Bergner  <bergner@vnet.ibm.com>
    
    	PR target/71698
    	* gcc.target/powerpc/pr71698.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@237917 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/pr71698.c
@@ -0,0 +1,13 @@
+/* Test for a reload ICE arising from trying to direct move a TDmode value.  */
+/* { dg-do compile } */
+/* { dg-require-effective-target powerpc_p9vector_ok } */
+/* { dg-require-effective-target dfp } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power9" } } */
+/* { dg-options "-O1 -mcpu=power9 -mno-lra" } */
+
+extern void testvad128 (int n, ...);
+void
+testitd128 (_Decimal128 g01d128)
+{
+  testvad128 (1, g01d128);
+}
