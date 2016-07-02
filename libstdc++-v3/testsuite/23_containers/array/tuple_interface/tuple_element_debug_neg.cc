6c539f2e6766e08351199700ce2eeaeff11e3b42
François Dumont <fdumont@gcc.gnu.org>
2016-07-02 14:55:20 +0000

    get_debug_neg.cc: Adjust dg-error line numbers.
    2016-07-02  François Dumont  <fdumont@gcc.gnu.org>
    
    	* testsuite/23_containers/array/tuple_interface/get_debug_neg.cc: Adjust
    	dg-error line numbers.
    	* testsuite/23_containers/array/tuple_interface/
    	tuple_element_debug_neg.cc: Likewise.
    	* testsuite/25_algorithms/lexicographical_compare/debug/
    	irreflexive_neg.cc: Remove.
    
    From-SVN: r237945

--- libstdc++-v3/testsuite/23_containers/array/tuple_interface/tuple_element_debug_neg.cc
+++ libstdc++-v3/testsuite/23_containers/array/tuple_interface/tuple_element_debug_neg.cc
@@ -23,4 +23,4 @@
 
 typedef std::tuple_element<1, std::array<int, 1>>::type type;
 
-// { dg-error "static assertion failed" "" { target *-*-* } 309 }
+// { dg-error "static assertion failed" "" { target *-*-* } 308 }
