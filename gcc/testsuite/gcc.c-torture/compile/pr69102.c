039a1d47d4931b243086dadc379baafbcc20434e
mrs <mrs@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-05 18:51:38 +0000

    2016-07-05  Kito Cheng <kito.cheng@gmail.com>
    	* gcc.c-torture/compile/pr69102.c: Require fpic support.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@238023 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/compile/pr69102.c
+++ gcc/testsuite/gcc.c-torture/compile/pr69102.c
@@ -1,5 +1,6 @@
 /* { dg-options "-Og -fPIC -fschedule-insns2 -fselective-scheduling2 -fno-tree-fre --param=max-sched-extend-regions-iters=10" } */
 /* { dg-require-effective-target scheduling } */
+/* { dg-require-effective-target fpic } */
 void bar (unsigned int);
 
 void
