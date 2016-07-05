7d4f48b5b85b20df4f1acc19a3f1326d37a8bf6a
Ville Voutilainen <ville.voutilainen@gmail.com>
2016-07-05 21:33:18 +0300

    Implement LWG 2509,
    	any_cast doesn't work with rvalue reference targets and cannot
    	move with a value target.
    	* include/experimental/any (any(_ValueType&&)): Constrain and
    	add an overload that doesn't forward.
    	(any_cast(any&&)): Constrain and add an overload that moves.
    	* testsuite/experimental/any/misc/any_cast.cc: Add tests for
    	the functionality added by LWG 2509.
    
    From-SVN: r238022

--- libstdc++-v3/testsuite/experimental/any/misc/any_cast.cc
+++ libstdc++-v3/testsuite/experimental/any/misc/any_cast.cc
@@ -77,8 +77,38 @@ void test02()
   }
 }
 
+static int move_count = 0;
+
+void test03()
+{
+  struct MoveEnabled
+  {
+    MoveEnabled(MoveEnabled&&)
+    {
+      ++move_count;
+    }
+    MoveEnabled() = default;
+    MoveEnabled(const MoveEnabled&) = default;
+  };
+  MoveEnabled m;
+  MoveEnabled m2 = any_cast<MoveEnabled>(any(m));
+  VERIFY(move_count == 1);
+  MoveEnabled&& m3 = any_cast<MoveEnabled&&>(any(m));
+  VERIFY(move_count == 1);
+  struct MoveDeleted
+  {
+    MoveDeleted(MoveDeleted&&) = delete;
+    MoveDeleted() = default;
+    MoveDeleted(const MoveDeleted&) = default;
+  };
+  MoveDeleted md;
+  MoveDeleted&& md2 = any_cast<MoveDeleted>(any(std::move(md)));
+  MoveDeleted&& md3 = any_cast<MoveDeleted&&>(any(std::move(md)));
+}
+
 int main()
 {
   test01();
   test02();
+  test03();
 }
