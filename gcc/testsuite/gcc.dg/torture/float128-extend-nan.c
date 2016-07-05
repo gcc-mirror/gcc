4b016ff2814057c0b41778039504f2c10d34163c
wschmidt <wschmidt@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-05 17:54:02 +0000

    2016-07-05  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    	Backport from mainline
    	2016-07-01  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* gcc.dg/const-float128-ped.c: Require __float128 effective
    	target and options.
    	* gcc.dg/const-float128.c: Likewise.
    	* gcc.dg/torture/float128-cmp-invalid.c: Require
    	__float128 and base_quadfloat_support effective targets, and
    	__float128 options.
    	* gcc.dg/torture/float128-div-underflow.c: Likewise.
    	* gcc.dg/torture/float128-extend-nan.c: Likewise.
    	* gcc.dg/torture/fp-int-convert-float128-timode-2.c: Likewise.
    	* gcc.dg/torture/fp-int-convert-float128-timode-3.c: Likewise.
    	* gcc.dg/torture/fp-int-convert-float128-timode.c: Likewise.
    	* lib/target-supports.exp (check_effective_target___float128):
    	New.
    	(add_options_for___float128): New.
    	(check_effective_target_base_quadword_support): New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@238018 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/torture/float128-extend-nan.c
+++ gcc/testsuite/gcc.dg/torture/float128-extend-nan.c
@@ -1,7 +1,10 @@
 /* Test extensions to __float128 quiet signaling NaNs.  */
-/* { dg-do run { target i?86-*-* x86_64-*-* ia64-*-* } } */
+/* { dg-do run } */
 /* { dg-options "-fsignaling-nans" } */
+/* { dg-require-effective-target __float128 } */
+/* { dg-require-effective-target base_quadfloat_support } */
 /* { dg-require-effective-target fenv_exceptions } */
+/* { dg-add-options __float128 } */
 
 #include <fenv.h>
 #include <float.h>
