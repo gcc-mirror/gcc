ebcca5bf11a3cf1c4815969f206316eff18a0b5b
Markus Trippelsdorf <markus@trippelsdorf.de>
2016-07-05 18:01:47 +0000

    re PR c++/71214 (Typo in feature test macro for rvalue references)
    Fix PR c++/71214
    
    	PR c++/71214
    	* c-cppbuiltin.c (c_cpp_builtins): Define __cpp_rvalue_references.
    
    From-SVN: r238021

--- gcc/c-family/c-cppbuiltin.c
+++ gcc/c-family/c-cppbuiltin.c
@@ -833,6 +833,7 @@ c_cpp_builtins (cpp_reader *pfile)
 	  cpp_define (pfile, "__cpp_decltype=200707");
 	  cpp_define (pfile, "__cpp_attributes=200809");
 	  cpp_define (pfile, "__cpp_rvalue_reference=200610");
+	  cpp_define (pfile, "__cpp_rvalue_references=200610");
 	  cpp_define (pfile, "__cpp_variadic_templates=200704");
 	  cpp_define (pfile, "__cpp_alias_templates=200704");
 	}
