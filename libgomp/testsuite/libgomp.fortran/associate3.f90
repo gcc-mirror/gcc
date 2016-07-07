1f159eb8463dee982eebc347076fd6bd7af5d881
Jakub Jelinek <jakub@redhat.com>
2016-07-07 23:55:37 +0200

    backport: re PR fortran/71717 (A gfortran silent "wrong code" bug in the transition from 4.9.0 -> 4.9.1, using OpenMP.)
    	Backported from mainline
    	2016-07-01  Jakub Jelinek  <jakub@redhat.com>
    
    	PR fortran/71717
    	* trans-openmp.c (gfc_omp_privatize_by_reference): Return false
    	for GFC_DECL_ASSOCIATE_VAR_P with POINTER_TYPE.
    
    	* testsuite/libgomp.fortran/associate3.f90: New test.
    
    From-SVN: r238148

---
+++ libgomp/testsuite/libgomp.fortran/associate3.f90
@@ -0,0 +1,20 @@
+! PR fortran/71717
+! { dg-do run }
+
+  type t
+    real, allocatable :: f(:)
+  end type
+  type (t) :: v
+  integer :: i, j
+  allocate (v%f(4))
+  v%f = 19.
+  i = 5
+  associate (u => v, k => i)
+  !$omp parallel do
+  do j = 1, 4
+    u%f(j) = 21.
+    if (j.eq.1) k = 7
+  end do
+  end associate
+  if (any (v%f(:).ne.21.) .or. i.ne.7) call abort
+end
