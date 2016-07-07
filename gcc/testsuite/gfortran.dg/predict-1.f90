8e976d113b9115333735c99d2f86a94fc8fce28b
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-07 13:11:05 +0000

    Add PRED_FORTRAN_LOOP_PREHEADER to DO loops with step
    	* trans-stmt.c (gfc_trans_do): Add expect builtin for DO
    	loops with step bigger than +-1.
    	* gfortran.dg/predict-1.f90: Ammend the test.
    	* gfortran.dg/predict-2.f90: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@238112 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/predict-1.f90
+++ gcc/testsuite/gfortran.dg/predict-1.f90
@@ -4,9 +4,14 @@
 subroutine test(block, array)
 integer :: i, block(9), array(2)
 
-do i = array(1), array(2)
+do i = array(1), array(2), 2
     block(i) = i
 end do
+
+do i = array(1), array(2), -2
+    block(i) = block(i) + i
+end do
+
 end subroutine test
 
-! { dg-final { scan-tree-dump-times "Fortran loop preheader heuristics of edge\[^:\]*: 99.0%" 1 "profile_estimate" } }
+! { dg-final { scan-tree-dump-times "Fortran loop preheader heuristics of edge\[^:\]*: 1.0%" 2 "profile_estimate" } }
