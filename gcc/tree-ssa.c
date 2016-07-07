c390d32d68079ad924e5b13d11fee7877a600585
Richard Biener <rguenther@suse.de>
2016-07-07 11:46:08 +0000

    Backport PRs 70022, 70484, 70931, 71452
    2016-07-07  Richard Biener  <rguenther@suse.de>
    
    	Backport from mainline
    	2016-04-04  Richard Biener  <rguenther@suse.de>
    
    	PR rtl-optimization/70484
    	* rtl.h (canon_output_dependence): Declare.
    	* alias.c (canon_output_dependence): New function.
    	* dse.c (record_store): Use canon_output_dependence rather
    	than canon_true_dependence.
    
    	* gcc.dg/torture/pr70484.c: New testcase.
    
    	2016-06-08  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/71452
    	* tree-ssa.c (non_rewritable_lvalue_p): Make sure that the
    	type used for the SSA rewrite has enough precision to cover
    	the dynamic type of the location.
    
    	* gcc.dg/torture/pr71452.c: New testcase.
    
    	2016-05-06  Richard Biener  <rguenther@suse.de>
    
    	PR middle-end/70931
    	* dwarf2out.c (native_encode_initializer): Skip zero-sized fields.
    
    	* gfortran.dg/pr70931.f90: New testcase.
    
    	2016-03-01  Richard Biener  <rguenther@suse.de>
    
    	PR middle-end/70022
    	* fold-const.c (fold_indirect_ref_1): Fix range checking for
    	vector BIT_FIELD_REF extract.
    
    	* gcc.dg/pr70022.c: New testcase.
    
    From-SVN: r238087

--- gcc/tree-ssa.c
+++ gcc/tree-ssa.c
@@ -1380,6 +1380,14 @@ non_rewritable_lvalue_p (tree lhs)
       tree decl = TREE_OPERAND (TREE_OPERAND (lhs, 0), 0);
       if (DECL_P (decl)
 	  && DECL_SIZE (decl) == TYPE_SIZE (TREE_TYPE (lhs))
+	  /* If the dynamic type of the decl has larger precision than
+	     the decl itself we can't use the decls type for SSA rewriting.  */
+	  && ((! INTEGRAL_TYPE_P (TREE_TYPE (decl))
+	       || compare_tree_int (DECL_SIZE (decl),
+				    TYPE_PRECISION (TREE_TYPE (decl))) == 0)
+	      || (INTEGRAL_TYPE_P (TREE_TYPE (lhs))
+		  && (TYPE_PRECISION (TREE_TYPE (decl))
+		      >= TYPE_PRECISION (TREE_TYPE (lhs)))))
 	  && (TREE_THIS_VOLATILE (decl) == TREE_THIS_VOLATILE (lhs)))
 	return false;
     }
