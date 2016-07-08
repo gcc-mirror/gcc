d13835b668ac64ae062fa0c765476d229b6b2c22
Vladimir Makarov <vmakarov@redhat.com>
2016-07-08 20:29:12 +0000

    re PR rtl-optimization/71621 (ICE in assign_by_spills, at lra-assigns.c:1417 (error: unable to find a register to spill) w/ -O2 -mavx2 -ftree-vectorize)
    2016-07-08  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR rtl-optimization/71621
    	* lra-constraints.c (process_alt_operands): Check combination of
    	reg class and mode.
    
    2016-07-08  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR rtl-optimization/71621
    	* gcc.target/i386/pr71621-1.c: New.
    	* gcc.target/i386/pr71621-2.c: New.
    
    From-SVN: r238178

---
+++ gcc/testsuite/gcc.target/i386/pr71621-1.c
@@ -0,0 +1,31 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -w -ftree-vectorize -mavx2" } */
+
+int cn;
+int *li;
+
+void
+y8 (void)
+{
+  int gv;
+  int *be = &gv;
+  short int v4 = 2;
+
+  while (*li != 0)
+    {
+      int sy;
+      for (sy = 0; sy < 5; ++sy)
+	{
+	  int **t6 = &be;
+	  gv |= sy ? 0 : v4;
+	  if (gv != 0)
+	    ++gv;
+	  t6 = &cn;
+	  if (gv != 0)
+	    *t6 = 0;
+	}
+      for (gv = 0; gv < 24; ++gv)
+	v4 |= 1 <= 1 % 0;
+      ++(*li);
+    }
+}
