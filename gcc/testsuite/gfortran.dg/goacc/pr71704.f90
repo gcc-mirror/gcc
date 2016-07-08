1e17a8f8c66013417c39ac6820880aba11ea73bb
Cesar Philippidis <cesar@codesourcery.com>
2016-07-08 14:58:57 -0700

    backport: parse.c (matcha): Define.
    	Backport from trunk:
    	2016-07-08  Cesar Philippidis  <cesar@codesourcery.com>
    
    	gcc/fortran/
    	* parse.c (matcha): Define.
    	(decode_oacc_directive): Add spec_only local var and set it.  Use
    	matcha to parse acc directives except for routine and declare.  Return
    	ST_GET_FCN_CHARACTERISTICS if a non-declarative directive could be
    	matched.
    
    	gcc/testsuite/
    	* gfortran.dg/goacc/pr71704.f90: New test.
    
    From-SVN: r238184

---
+++ gcc/testsuite/gfortran.dg/goacc/pr71704.f90
@@ -0,0 +1,49 @@
+! PR fortran/71704
+! { dg-do compile }
+
+real function f1 ()
+!$acc routine (f1)
+  f1 = 1
+end
+
+real function f2 (a)
+  integer a
+  !$acc enter data copyin(a)
+  f2 = 1
+end
+
+real function f3 (a)
+  integer a
+!$acc enter data copyin(a)
+  f3 = 1
+end
+
+real function f4 ()
+!$acc wait
+  f4 = 1
+end
+
+real function f5 (a)
+  integer a
+!$acc update device(a)
+  f5 = 1
+end
+
+real function f6 ()
+!$acc parallel
+!$acc end parallel
+  f6 = 1
+end
+
+real function f7 ()
+!$acc kernels
+!$acc end kernels
+  f7 = 1
+end
+
+real function f8 ()
+!$acc data
+!$acc end data
+  f8 = 1
+end
+
