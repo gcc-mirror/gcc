435e56fb5289d72bfe12068aac72e76c02bb3b3c
Ville Voutilainen <ville.voutilainen@gmail.com>
2016-07-10 20:44:21 +0300

    Implement std::optional.
    	* include/Makefile.am: Add optional to exported headers.
    	* include/Makefile.in: Likewise.
    	* include/std/optional: New.
    	* testsuite/20_util/optional/typedefs.cc: Likewise.
    	* testsuite/20_util/optional/relops/2.cc: Likewise.
    	* testsuite/20_util/optional/relops/3.cc: Likewise.
    	* testsuite/20_util/optional/relops/4.cc: Likewise.
    	* testsuite/20_util/optional/relops/5.cc: Likewise.
    	* testsuite/20_util/optional/relops/1.cc: Likewise.
    	* testsuite/20_util/optional/relops/6.cc: Likewise.
    	* testsuite/20_util/optional/nullopt.cc: Likewise.
    	* testsuite/20_util/optional/in_place.cc: Likewise.
    	* testsuite/20_util/optional/make_optional.cc: Likewise.
    	* testsuite/20_util/optional/assignment/2.cc: Likewise.
    	* testsuite/20_util/optional/assignment/3.cc: Likewise.
    	* testsuite/20_util/optional/assignment/4.cc: Likewise.
    	* testsuite/20_util/optional/assignment/5.cc: Likewise.
    	* testsuite/20_util/optional/assignment/1.cc: Likewise.
    	* testsuite/20_util/optional/assignment/6.cc: Likewise.
    	* testsuite/20_util/optional/cons/value_neg.cc: Likewise.
    	* testsuite/20_util/optional/cons/default.cc: Likewise.
    	* testsuite/20_util/optional/cons/move.cc: Likewise.
    	* testsuite/20_util/optional/cons/value.cc: Likewise.
    	* testsuite/20_util/optional/cons/copy.cc: Likewise.
    	* testsuite/20_util/optional/requirements.cc: Likewise.
    	* testsuite/20_util/optional/observers/2.cc: Likewise.
    	* testsuite/20_util/optional/observers/3.cc: Likewise.
    	* testsuite/20_util/optional/observers/4.cc: Likewise.
    	* testsuite/20_util/optional/observers/5.cc: Likewise.
    	* testsuite/20_util/optional/observers/1.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/relops/2.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/relops/3.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/relops/4.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/relops/5.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/relops/1.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/relops/6.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/nullopt.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/in_place.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/make_optional.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/cons/default.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/cons/value.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/observers/2.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/observers/3.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/observers/4.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/observers/5.cc: Likewise.
    	* testsuite/20_util/optional/constexpr/observers/1.cc: Likewise.
    	* testsuite/20_util/optional/swap/1.cc: Likewise.
    
    From-SVN: r238197

---
+++ libstdc++-v3/testsuite/20_util/optional/cons/value_neg.cc
@@ -0,0 +1,39 @@
+// { dg-options "-std=gnu++17" }
+// { dg-do compile }
+
+// Copyright (C) 2013-2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a moved_to of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <optional>
+#include <testsuite_hooks.h>
+
+#include <string>
+#include <memory>
+
+int main()
+{
+  {
+    struct X
+    {
+      explicit X(int) {}
+    };
+    std::optional<X> ox{42};
+    std::optional<X> ox2 = 42; // { dg-error "conversion" }
+    std::optional<std::unique_ptr<int>> oup{new int};
+    std::optional<std::unique_ptr<int>> oup2 = new int;  // { dg-error "conversion" }
+  }
+}
---
+++ libstdc++-v3/testsuite/20_util/optional/swap/1.cc
@@ -0,0 +1,95 @@
+// { dg-options "-std=gnu++17" }
+// { dg-do run }
+
+// Copyright (C) 2013-2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a moved_to of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <optional>
+#include <testsuite_hooks.h>
+
+struct exception {};
+
+int counter = 0;
+
+struct mixin_counter
+{
+  mixin_counter() { ++counter; }
+  mixin_counter(mixin_counter const&) { ++counter; }
+  ~mixin_counter() { --counter; }
+};
+
+namespace ns
+{
+
+struct value_type : private mixin_counter
+{
+  explicit value_type(int state) : state(state) { }
+  int state;
+};
+
+int swaps = 0;
+
+void
+swap(value_type& lhs, value_type& rhs)
+{
+  ++swaps;
+  using std::swap;
+  swap(lhs.state, rhs.state);
+}
+
+} // namespace ns
+
+int main()
+{
+  using O = std::optional<ns::value_type>;
+
+  VERIFY( ns::swaps == 0 );
+
+  {
+    O o, p;
+    swap(o, p);
+    VERIFY( !o );
+    VERIFY( !p );
+  }
+
+  {
+    O o { std::in_place, 45 }, p;
+    swap(o, p);
+    VERIFY( !o );
+    VERIFY( p && p->state == 45 );
+  }
+
+  {
+    O o, p { std::in_place, 45 };
+    swap(o, p);
+    VERIFY( o && o->state == 45 );
+    VERIFY( !p );
+  }
+
+  {
+    O o { std::in_place, 167 }, p { std::in_place, 999 };
+    VERIFY( ns::swaps == 0 );
+
+    swap(o, p);
+
+    VERIFY( o && o->state == 999 );
+    VERIFY( p && p->state == 167 );
+    VERIFY( ns::swaps == 1 );
+  }
+
+  VERIFY( counter == 0 );
+}
