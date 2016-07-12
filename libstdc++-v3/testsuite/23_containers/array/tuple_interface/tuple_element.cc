8c5a11cb7545d1a701596665392a8f4bf29a1489
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-12 14:00:26 +0000

    Add tests and docs for LWG 2212 support
    	* testsuite/20_util/pair/astuple/astuple.cc: Only include <utility>.
    	* testsuite/23_containers/array/tuple_interface/tuple_element.cc:
    	Only include <array>.
    	* testsuite/23_containers/array/tuple_interface/tuple_size.cc:
    	Likewise.
    	* doc/xml/manual/intro.xml; Document LWG 2212 support.
    	* doc/html*: Regenerate.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@238244 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/23_containers/array/tuple_interface/tuple_element.cc
+++ libstdc++-v3/testsuite/23_containers/array/tuple_interface/tuple_element.cc
@@ -18,15 +18,18 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
+// NB: Don't include any other headers in this file.
+// LWG 2212 requires <array> to define tuple_element<cv T> specializations.
 #include <array>
-#include <type_traits>
-#include <testsuite_hooks.h>
 
 void
 test01() 
 { 
   bool test __attribute__((unused)) = true;
-  using namespace std;
+  using std::array;
+  using std::tuple_element;
+  // This relies on the fact that <utility> includes <type_traits>:
+  using std::is_same;
 
   const size_t len = 3;
   typedef array<int, len> array_type;
