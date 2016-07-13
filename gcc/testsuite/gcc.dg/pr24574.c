0644c120f6e85a71fe65243de7b4269f2bbe0478
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-13 13:57:05 +0000

    2016-07-13  Richard Biener  <rguenther@suse.de>
    	PR tree-optimization/24574
    	* tree-ssa-phiopt.c (absorbing_element_p): Pass in argument
    	position and add shift, rotate, divison and modulo support
    	for left zero.
    	(value_replacement): Pass in argument position to absorbing_element_p.
    
    	* gcc.dg/pr24574.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@238299 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr24574.c
@@ -0,0 +1,36 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-phiopt1" } */
+
+int f0(int i)
+{
+  if (i == 0) return 0;
+  return i/10;
+}
+int f1(int i)
+{
+  return i?i/10:0;
+}
+
+int f2(int i)
+{
+  if (i == 0) return 0;
+  return i%10;
+}
+int f3(int i)
+{
+  return i?i%10:0;
+}
+
+int f4(int i)
+{
+  if (i == 0) return 0;
+  return i<<10;
+}
+int f5(int i)
+{
+  return i?i<<10:0;
+}
+
+/* We should if-convert all functions to carry out the operation
+   unconditionally.  */
+/* { dg-final { scan-tree-dump-not "= PHI" "phiopt1" } } */
