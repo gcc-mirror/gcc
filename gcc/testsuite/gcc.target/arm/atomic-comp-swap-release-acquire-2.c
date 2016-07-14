d62b809c11f21a96648cc15a8f3d922cc15137c4
Thomas Preud'homme <thomas.preudhomme@arm.com>
2016-07-14 17:21:21 +0000

    arm.h (TARGET_HAVE_LDACQ): Enable for ARMv8-M Mainline.
    2016-07-14  Thomas Preud'homme  <thomas.preudhomme@arm.com>
    
        gcc/
        * config/arm/arm.h (TARGET_HAVE_LDACQ): Enable for ARMv8-M Mainline.
        (TARGET_HAVE_LDACQD): New macro.
        * config/arm/sync.md (atomic_loaddi): Use TARGET_HAVE_LDACQD rather
        than TARGET_HAVE_LDACQ.
        (arm_load_acquire_exclusivedi): Likewise.
        (arm_store_release_exclusivedi): Likewise.
    
        libgcc/
        * gcc.target/arm/atomic-comp-swap-release-acquire.c: Rename into ...
        * gcc.target/arm/atomic-comp-swap-release-acquire-1.c: This.
        * gcc.target/arm/atomic-op-acq_rel.c: Rename into ...
        * gcc.target/arm/atomic-op-acq_rel-1.c: This.
        * gcc.target/arm/atomic-op-acquire.c: Rename into ...
        * gcc.target/arm/atomic-op-acquire-1.c: This.
        * gcc.target/arm/atomic-op-char.c: Rename into ...
        * gcc.target/arm/atomic-op-char-1.c: This.
        * gcc.target/arm/atomic-op-consume.c: Rename into ...
        * gcc.target/arm/atomic-op-consume-1.c: This.
        * gcc.target/arm/atomic-op-int.c: Rename into ...
        * gcc.target/arm/atomic-op-int-1.c: This.
        * gcc.target/arm/atomic-op-relaxed.c: Rename into ...
        * gcc.target/arm/atomic-op-relaxed-1.c: This.
        * gcc.target/arm/atomic-op-release.c: Rename into ...
        * gcc.target/arm/atomic-op-release-1.c: This.
        * gcc.target/arm/atomic-op-seq_cst.c: Rename into ...
        * gcc.target/arm/atomic-op-seq_cst-1.c: This.
        * gcc.target/arm/atomic-op-short.c: Rename into ...
        * gcc.target/arm/atomic-op-short-1.c: This.
        * gcc.target/arm/atomic-comp-swap-release-acquire-2.c: New test.
        * gcc.target/arm/atomic-op-acq_rel-2.c: Likewise.
        * gcc.target/arm/atomic-op-acquire-2.c: Likewise.
        * gcc.target/arm/atomic-op-char-2.c: Likewise.
        * gcc.target/arm/atomic-op-consume-2.c: Likewise.
        * gcc.target/arm/atomic-op-int-2.c: Likewise.
        * gcc.target/arm/atomic-op-relaxed-2.c: Likewise.
        * gcc.target/arm/atomic-op-release-2.c: Likewise.
        * gcc.target/arm/atomic-op-seq_cst-2.c: Likewise.
        * gcc.target/arm/atomic-op-short-2.c: Likewise.
    
    From-SVN: r238348

---
+++ gcc/testsuite/gcc.target/arm/atomic-comp-swap-release-acquire-2.c
@@ -0,0 +1,10 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target arm_arch_v8m_main_ok } */
+/* { dg-options "-O2 -fno-ipa-icf" } */
+/* { dg-add-options arm_arch_v8m_main } */
+
+#include "../aarch64/atomic-comp-swap-release-acquire.x"
+
+/* { dg-final { scan-assembler-times "ldaex" 4 } } */
+/* { dg-final { scan-assembler-times "stlex" 4 } } */
+/* { dg-final { scan-assembler-not "dmb" } } */
