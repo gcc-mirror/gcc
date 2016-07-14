52b6934ac54f8ef50366c88c0cf6a4238ba9a4c3
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2016-07-14 14:03:20 +0000

    backport: _divkc3.c: New.
    [libgcc]
    
    2016-07-14  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2016-07-12  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* config/rs6000/_divkc3.c: New.
    	* config/rs6000/_mulkc3.c: New.
    	* config/rs6000/quad-float128.h: Define TFtype; declare _mulkc3
    	and _divkc3.
    	* config/rs6000/t-float128: Add _mulkc3 and _divkc3 to
    	fp128_ppc_funcs.
    
    [gcc/testsuite]
    
    2016-07-14  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2016-07-12  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* gcc.target/powerpc/divkc3-1.c: New.
    	* gcc.target/powerpc/mulkc3-1.c: New.
    
    From-SVN: r238338

---
+++ gcc/testsuite/gcc.target/powerpc/divkc3-1.c
@@ -0,0 +1,22 @@
+/* { dg-do run { target { powerpc64*-*-* && vsx_hw } } } */
+/* { dg-options "-mfloat128 -mvsx" } */
+
+void abort ();
+
+typedef __complex float __cfloat128 __attribute__((mode(KC)));
+
+__cfloat128 divide (__cfloat128 x, __cfloat128 y)
+{
+  return x / y;
+}
+
+__cfloat128 z, a;
+
+int main ()
+{
+  z = divide (5.0q + 5.0jq, 2.0q + 1.0jq);
+  a = 3.0q + 1.0jq;
+  if (z != a)
+    abort ();
+  return 0;
+}
