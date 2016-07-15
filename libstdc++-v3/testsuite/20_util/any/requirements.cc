96de1a7fa4777a7fc0e4e71f730e307c4c82afce
ville <ville@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-15 07:44:22 +0000

    Fix the constraints for any's assignment operator template to properly reject assignment from a non-copyable lvalue.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@238365 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/20_util/any/requirements.cc
+++ libstdc++-v3/testsuite/20_util/any/requirements.cc
@@ -30,4 +30,7 @@ static_assert(std::is_assignable<any&, int>::value);
 static_assert(!std::is_assignable<any&, unique_ptr<int>>::value);
 static_assert(std::is_constructible<any, int>::value);
 static_assert(!std::is_constructible<any, unique_ptr<int>>::value);
-
+static_assert(!std::is_assignable<any&, const unique_ptr<int>&>::value);
+static_assert(!std::is_constructible<any&, const unique_ptr<int>&>::value);
+static_assert(!std::is_assignable<any&, unique_ptr<int>&>::value);
+static_assert(!std::is_constructible<any&, unique_ptr<int>&>::value);
