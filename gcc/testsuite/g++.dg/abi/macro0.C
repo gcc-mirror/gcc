0238e35ef85e0aa66fb4c179ff6a0f16e127f542
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-15 18:38:05 +0000

    Update -fabi-version default to 11.
    	* c-opts.c (c_common_post_options): Update -fabi-version default to 11.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@238388 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/abi/macro0.C
+++ gcc/testsuite/g++.dg/abi/macro0.C
@@ -1,6 +1,6 @@
 // This testcase will need to be kept in sync with c_common_post_options.
 // { dg-options "-fabi-version=0" }
 
-#if __GXX_ABI_VERSION != 1010
+#if __GXX_ABI_VERSION != 1011
 #error "Incorrect value of __GXX_ABI_VERSION"
 #endif
