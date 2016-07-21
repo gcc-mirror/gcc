1019399ab4dd28e9463e602c80b3c4cb29f39231
jgreenhalgh <jgreenhalgh@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-21 15:37:50 +0000

    [Patch 1/2 ifcvt costs] New target hook: max_noce_ifcvt_seq_cost
    gcc/
    
    	* target.def (max_noce_ifcvt_seq_cost): New.
    	* doc/tm.texi.in (TARGET_MAX_NOCE_IFCVT_SEQ_COST): Document it.
    	* doc/tm.texi: Regenerate.
    	* targhooks.h (default_max_noce_ifcvt_seq_cost): New.
    	* targhooks.c (default_max_noce_ifcvt_seq_cost): New.
    	* params.def (PARAM_MAX_RTL_IF_CONVERSION_PREDICTABLE_COST): New.
    	(PARAM_MAX_RTL_IF_CONVERSION_UNPREDICTABLE_COST): Likewise.
    	* doc/invoke.texi: Document new params.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@238593 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/targhooks.c
+++ gcc/targhooks.c
@@ -74,6 +74,8 @@ along with GCC; see the file COPYING3.  If not see
 #include "intl.h"
 #include "opts.h"
 #include "gimplify.h"
+#include "predict.h"
+#include "params.h"
 
 
 bool
@@ -1984,6 +1986,26 @@ bool
 default_optab_supported_p (int, machine_mode, machine_mode, optimization_type)
 {
   return true;
+}
+
+/* Default implementation of TARGET_MAX_NOCE_IFCVT_SEQ_COST.  */
+
+unsigned int
+default_max_noce_ifcvt_seq_cost (edge e)
+{
+  bool predictable_p = predictable_edge_p (e);
+
+  enum compiler_param param
+    = (predictable_p
+       ? PARAM_MAX_RTL_IF_CONVERSION_PREDICTABLE_COST
+       : PARAM_MAX_RTL_IF_CONVERSION_UNPREDICTABLE_COST);
+
+  /* If we have a parameter set, use that, otherwise take a guess using
+     BRANCH_COST.  */
+  if (global_options_set.x_param_values[param])
+    return PARAM_VALUE (param);
+  else
+    return BRANCH_COST (true, predictable_p) * COSTS_N_INSNS (3);
 }
 
 #include "gt-targhooks.h"
