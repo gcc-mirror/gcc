5e192a1ab70fd4e8f502b9c50e382e7df4be73b0
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-21 06:29:32 +0000

    Fix PR c++/70822 (bogus error with parenthesized SCOPE_REF)
    gcc/cp/ChangeLog:
    
    	PR c++/70822
    	PR c++/70106
    	* cp-tree.h (REF_PARENTHESIZED_P): Make this flag apply to
    	SCOPE_REFs too.
    	* pt.c (tsubst_qualified_id): If REF_PARENTHESIZED_P is set
    	on the qualified_id then propagate it to the resulting
    	expression.
    	(do_auto_deduction): Check REF_PARENTHESIZED_P on SCOPE_REFs
    	too.
    	* semantics.c (force_paren_expr): If given a SCOPE_REF, just set
    	its REF_PARENTHESIZED_P flag.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/70822
    	PR c++/70106
    	* g++.dg/cpp1y/auto-fn32.C: New test.
    	* g++.dg/cpp1y/paren4.C: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@238578 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -1647,17 +1647,10 @@ force_paren_expr (tree expr)
       && TREE_CODE (expr) != SCOPE_REF)
     return expr;
 
-  if (TREE_CODE (expr) == COMPONENT_REF)
+  if (TREE_CODE (expr) == COMPONENT_REF
+      || TREE_CODE (expr) == SCOPE_REF)
     REF_PARENTHESIZED_P (expr) = true;
-  else if (type_dependent_expression_p (expr)
-	   /* When processing_template_decl, a SCOPE_REF may actually be
-	      referring to a non-static data member of the current class, in
-	      which case its TREE_TYPE may not be properly cv-qualified (the
-	      cv-qualifiers of the implicit *this object haven't yet been taken
-	      into account) so we have to delay building a static_cast until
-	      instantiation.  */
-	   || (processing_template_decl
-	       && TREE_CODE (expr) == SCOPE_REF))
+  else if (type_dependent_expression_p (expr))
     expr = build1 (PAREN_EXPR, TREE_TYPE (expr), expr);
   else if (VAR_P (expr) && DECL_HARD_REGISTER (expr))
     /* We can't bind a hard register variable to a reference.  */;
