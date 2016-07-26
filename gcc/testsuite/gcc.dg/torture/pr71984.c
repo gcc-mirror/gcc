8c3efadf22e54cd027a4c41851446507d38945be
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-26 14:11:21 +0000

    2016-07-26  Richard Biener  <rguenther@suse.de>
    	PR rtl-optimization/71984
    	* simplify-rtx.c (simplify_subreg): Use GET_MODE_SIZE and prepare
    	for VOIDmode.
    
    	* gcc.dg/torture/pr71984.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@238757 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/pr71984.c
@@ -0,0 +1,21 @@
+/* { dg-do run { target lp64 } } */
+/* { dg-additional-options "-w -Wno-psabi" } */
+
+typedef unsigned char v64u8 __attribute__((vector_size(64)));
+typedef unsigned long v64u64 __attribute__((vector_size(64)));
+typedef unsigned char u8;
+
+static u8 __attribute__ ((noinline, noclone))
+foo (v64u64 v64u64_0)
+{
+  return ((v64u8)(v64u64){0, v64u64_0[0]})[13];
+}
+
+int
+main ()
+{
+  u8 x = foo((v64u64){0x0706050403020100UL});
+  if (x != 5)
+    __builtin_abort ();
+  return 0;
+}
