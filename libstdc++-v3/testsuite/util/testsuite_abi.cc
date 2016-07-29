62589e99d10e8fdc8de60fae1f0fafdcfa79daaf
Jonathan Wakely <jwakely@redhat.com>
2016-07-29 11:42:17 +0100

    New libstdc++ symbol version for new basic_string symbols
    	* acinclude.m4 (libtool_VERSION): Bump to 6:23:0.
    	* config/abi/pre/gnu.ver: Add 3.4.23 version for new basic_string
    	symbols.
    	* configure: Regenerate.
    	* testsuite/util/testsuite_abi.cc: Add new symbol version.
    
    From-SVN: r238853

--- libstdc++-v3/testsuite/util/testsuite_abi.cc
+++ libstdc++-v3/testsuite/util/testsuite_abi.cc
@@ -201,8 +201,9 @@ check_version(symbol& test, bool added)
       known_versions.push_back("GLIBCXX_3.4.19");
       known_versions.push_back("GLIBCXX_3.4.20");
       known_versions.push_back("GLIBCXX_3.4.21");
-      known_versions.push_back("GLIBCXX_3.4.22");
       known_versions.push_back("GLIBCXX_LDBL_3.4.21");
+      known_versions.push_back("GLIBCXX_3.4.22");
+      known_versions.push_back("GLIBCXX_3.4.23");
       known_versions.push_back("CXXABI_1.3");
       known_versions.push_back("CXXABI_LDBL_1.3");
       known_versions.push_back("CXXABI_1.3.1");
@@ -232,7 +233,7 @@ check_version(symbol& test, bool added)
 	test.version_status = symbol::incompatible;
 
       // Check that added symbols are added in the latest pre-release version.
-      bool latestp = (test.version_name == "GLIBCXX_3.4.22"
+      bool latestp = (test.version_name == "GLIBCXX_3.4.23"
 		     || test.version_name == "CXXABI_1.3.10"
 		     || test.version_name == "CXXABI_FLOAT128"
 		     || test.version_name == "CXXABI_TM_1");
