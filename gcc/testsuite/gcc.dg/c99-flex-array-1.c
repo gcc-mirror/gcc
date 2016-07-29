4ebf85beb887adfcda41872f0774680d6bac36dc
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-29 17:32:27 +0000

    PR c/71742 	* c-decl.c (finish_struct): Rephrase an error message.
    	* gcc.dg/940510-1.c: Adjust dg-error.
    	* gcc.dg/c99-flex-array-1.c: Likewise.
    	* gcc.dg/c99-flex-array-typedef-1.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@238883 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/c99-flex-array-1.c
+++ gcc/testsuite/gcc.dg/c99-flex-array-1.c
@@ -2,7 +2,7 @@
 /* { dg-do compile } */
 /* { dg-options "-std=iso9899:1999 -pedantic-errors" } */
 
-struct s1 { int x[]; }; /* { dg-error "empty struct" "empty" } */
-struct s2 { int :1; int x[]; }; /* { dg-error "empty struct" "empty" } */
+struct s1 { int x[]; }; /* { dg-error "no named members" "members" } */
+struct s2 { int :1; int x[]; }; /* { dg-error "no named members" "members" } */
 struct s3 { int x[]; int y; }; /* { dg-error "not at end" "not at end" } */
 struct s4 { int x; int y[]; };
