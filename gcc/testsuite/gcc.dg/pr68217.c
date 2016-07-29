883b6d4e1b1c826007d12b4685b4e7b0fffc5347
Kugan Vivekanandarajah <kuganv@linaro.org>
2016-07-29 00:35:23 +0000

    re PR rtl-optimization/68217 (Wrong constant folding)
    gcc/ChangeLog:
    
    2016-07-29  Kugan Vivekanandarajah  <kuganv@linaro.org>
    
    	PR middle-end/68217
    	* tree-vrp.c (extract_range_from_binary_expr_1): In case of signed
    	& sign-bit-CST, generate [-INF, 0] instead of [-INF, INF].
    
    
    gcc/testsuite/ChangeLog:
    
    2016-07-29  Kugan Vivekanandarajah  <kuganv@linaro.org>
    
    	PR middle-end/68217
    	* gcc.dg/pr68217.c: New test.
    
    From-SVN: r238846

---
+++ gcc/testsuite/gcc.dg/pr68217.c
@@ -0,0 +1,14 @@
+
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-vrp1" } */
+
+int foo (void)
+{
+    volatile int a = -1;
+    long long b = (1LL << (sizeof (b) * 8 - 1)); // LLONG_MIN
+    long long x = (a & b); // x == 0x8000000000000000
+    if (x < 1LL) { ; } else { __builtin_abort(); }
+    return 0;
+}
+
+/* { dg-final { scan-tree-dump "\\\[-INF, 0\\\]" "vrp1" } } */
