28710a875bbecce99ae7cd85b107693693a33ea0
Marek Polacek <polacek@redhat.com>
2016-07-29 11:26:46 +0000

    re PR c/7652 (-Wswitch-break : Warn if a switch case falls through)
    	PR c/7652
    	* config/rs6000/rs6000.c (altivec_expand_ld_builtin): Likewise.
    	(altivec_expand_st_builtin): Likewise.
    
    From-SVN: r238858

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -13102,6 +13102,7 @@ altivec_expand_ld_builtin (tree exp, rtx target, bool *expandedp)
       break;
     case ALTIVEC_BUILTIN_LD_INTERNAL_2di:
       icode = CODE_FOR_vector_altivec_load_v2di;
+      break;
     case ALTIVEC_BUILTIN_LD_INTERNAL_1ti:
       icode = CODE_FOR_vector_altivec_load_v1ti;
       break;
@@ -13163,6 +13164,7 @@ altivec_expand_st_builtin (tree exp, rtx target ATTRIBUTE_UNUSED,
       break;
     case ALTIVEC_BUILTIN_ST_INTERNAL_2di:
       icode = CODE_FOR_vector_altivec_store_v2di;
+      break;
     case ALTIVEC_BUILTIN_ST_INTERNAL_1ti:
       icode = CODE_FOR_vector_altivec_store_v1ti;
       break;
