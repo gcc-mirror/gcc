efa8ff039aacd4f5296a420f85adc5db11c39fad
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-30 22:31:16 +0000

    [gcc] 2016-07-30  Michael Meissner  <meissner@linux.vnet.ibm.com>
    	* config/rs6000/rs6000-protos.h (rs6000_adjust_vec_address): New
    	function that takes a vector memory address, a hard register, an
    	element number and a temporary base register, and recreates an
    	address that points to the appropriate element within the vector.
    	* config/rs6000/rs6000.c (rs6000_adjust_vec_address): Likewise.
    	(rs6000_split_vec_extract_var): Add support for the target of a
    	vec_extract with variable element number being a scalar memory
    	location.
    	(rtx_is_swappable_p): VLSO insns (UNSPEC_VSX_VSLOW) are not
    	swappable.
    	* config/rs6000/vsx.md (vsx_extract_<mode>_load): Replace
    	vsx_extract_<mode>_load insn with a new insn that optimizes
    	storing either element to a memory location, using scratch
    	registers to pick apart the vector and reconstruct the address.
    	(vsx_extract_<P:mode>_<VSX_D:mode>_load): Likewise.
    	(vsx_extract_<mode>_store): Rework alternatives to more correctly
    	support Altivec registers.  Add support for ISA 3.0 Altivec d-form
    	store instruction.
    	(vsx_extract_<mode>_var): Add support for extracting a variable
    	element number from memory.
    
    [gcc/testsuite]
    2016-07-30  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	* gcc.target/powerpc/vec-extract-2.c: New tests for vec_extract of
    	vector double or vector long where the vector is in memory.
    	* gcc.target/powerpc/vec-extract-3.c: Likewise.
    	* gcc.target/powerpc/vec-extract-4.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@238908 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/vec-extract-3.c
@@ -0,0 +1,26 @@
+/* { dg-do compile { target { powerpc*-*-* && lp64 } } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
+/* { dg-require-effective-target powerpc_p8vector_ok } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power8" } } */
+/* { dg-options "-O2 -mcpu=power8" } */
+
+#include <altivec.h>
+
+double
+add_double_n (vector double *p, double x, long n)
+{
+  return vec_extract (*p, n) + x;
+}
+
+long
+add_long_n (vector long *p, long x, long n)
+{
+  return vec_extract (*p, n) + x;
+}
+
+/* { dg-final { scan-assembler-not "lxvd2x"   } } */
+/* { dg-final { scan-assembler-not "lxvw4x"   } } */
+/* { dg-final { scan-assembler-not "lxvx"     } } */
+/* { dg-final { scan-assembler-not "lxv"      } } */
+/* { dg-final { scan-assembler-not "lvx"      } } */
+/* { dg-final { scan-assembler-not "xxpermdi" } } */
