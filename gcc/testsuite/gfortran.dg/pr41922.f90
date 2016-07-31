71b5dc234703335c6b57d8f74532c98e27e5eecb
kargl <kargl@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-31 01:51:37 +0000

    2016-07-30  Steven G. Kargl <kargl@gcc.gnu.org>
    	PR fortran/41922
    	* target-memory.c (expr_to_char): Pass in locus and use it in error
    	messages.
    	(gfc_merge_initializers): Ditto.
    	* target-memory.h: Update prototype for gfc_merge_initializers ().
    	* trans-common.c (get_init_field): Use the correct locus.
    
    2016-07-30  Steven G. Kargl <kargl@gcc.gnu.org>
    
    	PR fortran/41922
    	* gfortran.dg/equiv_constraint_5.f90: Adjust the error message.
    	* gfortran.dg/equiv_constraint_7.f90: Ditto.
    	* gfortran.dg/pr41922.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@238915 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr41922.f90
@@ -0,0 +1,11 @@
+! { dg-do compile}
+! { dg-options -std=gnu }
+      Subroutine RestoreR8Run()
+      Implicit NONE
+      Integer   ISTORE
+      Real      XSTORE
+      character   CSTORE(8)
+      data cstore/8*' '/
+      data istore/0/
+      EQUIVALENCE (CSTORE(1),XSTORE,ISTORE) ! { dg-error "Overlapping unequal" }
+      end
