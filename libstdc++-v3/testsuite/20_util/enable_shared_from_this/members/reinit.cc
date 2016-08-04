ad164f750a2c1742e1ddd99d1b1fdc8f89b27070
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-08-04 12:33:10 +0000

    Define std::enable_shared_from_this::weak_from_this
    	* testsuite/20_util/enable_shared_from_this/members/reinit.cc: Use
    	effective target not dg-options. Move check for feature-test macro to:
    	* testsuite/20_util/enable_shared_from_this/members/weak_from_this.cc:
    	New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@239121 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/20_util/enable_shared_from_this/members/reinit.cc
+++ libstdc++-v3/testsuite/20_util/enable_shared_from_this/members/reinit.cc
@@ -15,14 +15,10 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-options "-std=gnu++11" }
+// { dg-do run { target c++11 } }
 
 #include <memory>
 #include <testsuite_hooks.h>
-
-#if __cpp_lib_enable_shared_from_this < 201603
-# error "__cpp_lib_enable_shared_from_this < 201603"
-#endif
 
 struct X : public std::enable_shared_from_this<X> { };
 
