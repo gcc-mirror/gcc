42183d034d47e05814c971b2911c847e2a700941
Jonathan Wakely <jwakely@redhat.com>
2016-08-04 19:02:56 +0100

    Define std::is_callable and std::is_nothrow_callable
    	* doc/xml/manual/status_cxx2017.xml: Update status table.
    	* include/std/functional (__inv_unwrap): Move to <type_traits>.
    	(__invoke_impl): Remove exception specifications.
    	(__invoke, invoke): Add exception specifications using
    	__is_nothrow_callable.
    	* include/std/type_traits (__inv_unwrap): Move from <functional>.
    	(__is_callable_impl, __call_is_nt, __call_is_nothrow): New helpers.
    	(__is_callable, __is_nothrow_callable): New traits.
    	(is_callable, is_callable_v): New C++17 traits.
    	(is_nothrow_callable, is_nothrow_callable_v): Likewise.
    	* testsuite/20_util/is_callable/requirements/
    	explicit_instantiation.cc: New test.
    	* testsuite/20_util/is_callable/requirements/
    	explicit_instantiation_ext.cc: New test.
    	* testsuite/20_util/is_callable/requirements/typedefs.cc: New test.
    	* testsuite/20_util/is_callable/requirements/typedefs_ext.cc: New
    	test.
    	* testsuite/20_util/is_callable/value.cc: New test.
    	* testsuite/20_util/is_callable/value_ext.cc: New test.
    	* testsuite/20_util/is_nothrow_callable/requirements/
    	explicit_instantiation.cc: New test.
    	* testsuite/20_util/is_nothrow_callable/requirements/
    	explicit_instantiation_ext.cc: New test.
    	* testsuite/20_util/is_nothrow_callable/requirements/typedefs.cc:
    	New test.
    	* testsuite/20_util/is_nothrow_callable/requirements/typedefs_ext.cc:
    	New test.
    	* testsuite/20_util/is_nothrow_callable/value.cc: New test.
    	* testsuite/20_util/is_nothrow_callable/value_ext.cc: New test.
    
    From-SVN: r239145

---
+++ libstdc++-v3/testsuite/20_util/is_callable/value_ext.cc
@@ -0,0 +1,26 @@
+// Copyright (C) 2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile { target c++11 } }
+
+#include <type_traits>
+
+template<typename T, typename R = void>
+  constexpr bool is_callable() { return std::__is_callable<T, R>::value; }
+
+#define IS_CALLABLE_DEFINED
+#include "value.cc"
