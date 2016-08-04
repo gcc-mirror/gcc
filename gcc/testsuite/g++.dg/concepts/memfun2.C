ec9004d94361b87c68c91a68efd6baece7795c3c
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-08-04 16:06:59 +0000

    PR c++/72415 - member template with fold-expression constraint
    	* pt.c (tsubst_pack_expansion): Pull a single pack expansion out
    	of the TREE_VEC.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@239139 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/concepts/memfun2.C
@@ -0,0 +1,21 @@
+// PR c++/72415
+// { dg-options "-std=c++1z -fconcepts" }
+
+template<int... Indices>
+struct indices {};
+
+template<typename Dummy>
+struct foo_type {
+    template<int... Indices>
+    static void impl(indices<Indices...>)
+        requires (... && (Indices, true));
+
+    static auto caller()
+    { return impl(indices<0, 1, 2> {}); }
+};
+
+int main()
+{
+    // internal compiler error: in satisfy_predicate_constraint, at cp/constraint.cc:2013
+    foo_type<void>::caller();
+}
