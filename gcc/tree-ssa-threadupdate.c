6c1d60a219b83f17a824f5b04dd04ffa3a1c7c37
Richard Biener <rguenther@suse.de>
2016-08-05 08:00:00 +0000

    tree-ssa-threadupdate.c (thread_block_1): Remove unnecessary restriction on threading to a loop header.
    2016-08-05  Richard Biener  <rguenther@suse.de>
    
    	* tree-ssa-threadupdate.c (thread_block_1): Remove unnecessary
    	restriction on threading to a loop header.
    
    From-SVN: r239164

--- gcc/tree-ssa-threadupdate.c
+++ gcc/tree-ssa-threadupdate.c
@@ -1531,10 +1531,8 @@ thread_block_1 (basic_block bb, bool noloop_only, bool joiners)
 	     threading path that crosses loop boundaries.  We do not try
 	     and thread this elsewhere, so just cancel the jump threading
 	     request by clearing the AUX field now.  */
-	  if ((bb->loop_father != e2->src->loop_father
-	       && !loop_exit_edge_p (e2->src->loop_father, e2))
-	      || (e2->src->loop_father != e2->dest->loop_father
-		  && !loop_exit_edge_p (e2->src->loop_father, e2)))
+	  if (bb->loop_father != e2->src->loop_father
+	      && !loop_exit_edge_p (e2->src->loop_father, e2))
 	    {
 	      /* Since this case is not handled by our special code
 		 to thread through a loop header, we must explicitly
