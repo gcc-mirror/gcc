ea034e2cc96ab873921240b3443b9ca1b72b8e7c
msebor <msebor@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-08-05 18:27:46 +0000

    gcc/cp/ChangeLog:
    	* constexpr.c (cxx_eval_store_expression): Remove hyphen from
    	the spelling of "constant-expression" in diagnostic messages
    	for consistency.
    	(cxx_eval_constant_expression): Same.
    	(cxx_eval_outermost_constant_expr): Same.
    	(potential_constant_expression_1): Same.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp0x/constexpr-cast.C: Avoid assuming (void*)1 is spelled
    	1ul in diagnostics.  Remove hyphen from "constant-expression."
    	* g++.dg/cpp0x/constexpr-50060.C: Adjust.
    	* g++.dg/cpp0x/static_assert3.C: Same.
    	* g++.dg/cpp1y/constexpr-throw.C: Same.
    	* g++.dg/template/nontype3.C: Same.
    	* g++.dg/warn/overflow-warn-1.C: Same.
    	* g++.dg/warn/overflow-warn-3.C: Same.
    	* g++.dg/warn/overflow-warn-4.C: Same.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@239176 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/warn/overflow-warn-1.C
+++ gcc/testsuite/g++.dg/warn/overflow-warn-1.C
@@ -130,7 +130,7 @@ h2i (int x)
 /* { dg-error "division by zero is not a constant.expression" "division" { target c++11 } 32 } */
 /* { dg-warning "invalid conversion from" "convert" { target *-*-* } 56 } */
 /* { dg-warning "invalid conversion from" "convert" { target c++11 } 58 } */
+/* { dg-error "division by zero is not a constant expression" "division" { target c++11 } 65 } */
 /* { dg-error "is not a constant expression" "const" { target *-*-* } 65 } */
-/* { dg-error "division by zero is not a constant.expression" "division" { target c++11 } 65 } */
 /* { dg-error "width not an integer constant" "bit.field" { target c++ } 32 } */
 /* { dg-error "is not a constant expression" "division" { target c++ } 32 } */
