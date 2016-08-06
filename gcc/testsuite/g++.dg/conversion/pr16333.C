fa76f2c63032aaa2214c015d7d5f33257bd3ed66
Jonathan Wakely <jwakely@redhat.com>
2016-08-06 01:07:05 +0100

    Harmonize diagnostics for invalid reference binding
    gcc/cp:
    
    	* call.c (convert_like_real): Harmonize diagnostics for invalid
    	reference binding.
    
    gcc/testsuite:
    
    	* call.c (convert_like_real): Harmonize diagnostics for invalid
    	reference binding.
    	* g++.dg/conversion/pr16333.C: Adjust dg-error regexp.
    	* g++.dg/conversion/pr41426.C: Likewise.
    	* g++.dg/conversion/pr66211.C: Likewise.
    	* g++.dg/cpp1y/lambda-init9.C: Likewise.
    	* g++.dg/init/ref8.C: Likewise.
    	* g++.old-deja/g++.law/cvt20.C: Likewise.
    	* g++.old-deja/g++.mike/p9732c.C: Likewise.
    
    From-SVN: r239184

--- gcc/testsuite/g++.dg/conversion/pr16333.C
+++ gcc/testsuite/g++.dg/conversion/pr16333.C
@@ -7,4 +7,4 @@ struct X {
 int a[3];
 X foo1 () { return a; }
 const X &foo2 () { return a; } // { dg-warning "returning reference to temporary" }
-X &foo3 () { return a; } // { dg-error "invalid initialization" }
+X &foo3 () { return a; } // { dg-error "cannot bind non-const lvalue ref" }
