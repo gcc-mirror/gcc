0c44e080f57a7c9d334005c2ceac08139d1d276b
Marek Polacek <polacek@redhat.com>
2016-08-12 10:15:57 +0000

    re PR c/7652 (-Wswitch-break : Warn if a switch case falls through)
    	PR c/7652
    	* tree-complex.c (expand_complex_division): Add missing break.
    
    From-SVN: r239409

--- gcc/tree-complex.c
+++ gcc/tree-complex.c
@@ -1336,6 +1336,7 @@ expand_complex_division (gimple_stmt_iterator *gsi, tree inner_type,
       rr = gimplify_build2 (gsi, code, inner_type, ai, bi);
       ri = gimplify_build2 (gsi, code, inner_type, ar, bi);
       ri = gimplify_build1 (gsi, NEGATE_EXPR, inner_type, ri);
+      break;
 
     case PAIR (ONLY_REAL, VARYING):
     case PAIR (ONLY_IMAG, VARYING):
