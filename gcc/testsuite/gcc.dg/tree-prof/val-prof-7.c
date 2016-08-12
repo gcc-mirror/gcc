5cacda56cbeea80e5c5257b6716c5761d3dbd0d9
Martin Liska <mliska@suse.cz>
2016-08-12 14:12:51 +0200

    Add test coverage for PR gcov-profile/35590
    	PR gcov-profile/35590
    	* gcc.dg/tree-prof/val-prof-7.c: Improve test coverage.
    
    From-SVN: r239412

--- gcc/testsuite/gcc.dg/tree-prof/val-prof-7.c
+++ gcc/testsuite/gcc.dg/tree-prof/val-prof-7.c
@@ -1,25 +1,81 @@
 /* { dg-options "-O2 -fdump-ipa-profile -mtune=core2" } */
 /* { dg-skip-if "" { ! { i?86-*-* x86_64-*-* } } { "*" } { "" } } */
 
-extern void bzero (void *, __SIZE_TYPE__);
+char *buffer1;
+char *buffer2;
 
-int foo(int len)
-{
-  char array[1000];
-  bzero(array, len);
-  return 0;
+#define DEFINE_TEST(N) \
+__attribute__((noinline)) \
+void bzero_test_ ## N (int len) \
+{ \
+  __builtin_bzero (buffer1, len); \
+} \
+ \
+__attribute__((noinline)) \
+void memcpy_test_ ## N (int len) \
+{ \
+  __builtin_memcpy (buffer1, buffer2, len); \
+} \
+ \
+__attribute__((noinline)) \
+void mempcpy_test_ ## N (int len) \
+{ \
+  __builtin_mempcpy (buffer1, buffer2, len); \
+} \
+ \
+__attribute__((noinline)) \
+void memset_test_ ## N (int len) \
+{ \
+  __builtin_memset (buffer1, 'c', len); \
+} \
+ \
+void test_stringops_ ## N(int len) \
+{ \
+  bzero_test_ ## N (len); \
+  memcpy_test_## N (len); \
+  mempcpy_test_ ## N (len); \
+  memset_test_ ## N (len); \
+} \
+ \
+void test_stringops_with_values_ ## N (int common, int not_common) \
+{ \
+  int i; \
+  for (i = 0; i < 1000; i++) \
+    { \
+      if (i > 990) \
+	test_stringops_ ## N (not_common); \
+      else \
+	test_stringops_ ## N (common); \
+    } \
 }
+
+DEFINE_TEST(0);
+DEFINE_TEST(1);
+DEFINE_TEST(2);
 
 int main() {
-  int i;
-  for (i = 0; i < 1000; i++)
-    {
-      if (i > 990)
-	foo(16);
-      else
-	foo(8);
-    }
+  buffer1 = __builtin_malloc (1000);
+  buffer2 = __builtin_malloc (1000);
+
+  test_stringops_with_values_0 (8, 111);
+  test_stringops_with_values_1 (111, 111);
+  test_stringops_with_values_2 (257, 111);
+
   return 0;
 }
 
-/* { dg-final-use-not-autofdo { scan-ipa-dump "Single value 8 stringop transformation on bzero" "profile" } } */
+/* { dg-final-use-not-autofdo { scan-ipa-dump "Single value 8 stringop transformation on __builtin_bzero" "profile" } } */
+/* { dg-final-use-not-autofdo { scan-ipa-dump "Single value 111 stringop transformation on __builtin_bzero" "profile" } } */
+/* { dg-final-use-not-autofdo { scan-ipa-dump-times "Single value 257 stringop transformation on __builtin_bzero" 0 "profile" } } */
+
+/* { dg-final-use-not-autofdo { scan-ipa-dump "Single value 8 stringop transformation on __builtin_memcpy" "profile" } } */
+/* { dg-final-use-not-autofdo { scan-ipa-dump "Single value 111 stringop transformation on __builtin_memcpy" "profile" } } */
+/* { dg-final-use-not-autofdo { scan-ipa-dump-times "Single value 257 stringop transformation on __builtin_memcpy" 0 "profile" } } */
+
+/* { dg-final-use-not-autofdo { scan-ipa-dump "Single value 8 stringop transformation on __builtin_mempcpy" "profile" } } */
+/* { dg-final-use-not-autofdo { scan-ipa-dump "Single value 111 stringop transformation on __builtin_mempcpy" "profile" } } */
+/* { dg-final-use-not-autofdo { scan-ipa-dump-times "Single value 257 stringop transformation on __builtin_mempcpy" 0 "profile" } } */
+
+/* { dg-final-use-not-autofdo { scan-ipa-dump "Single value 8 stringop transformation on __builtin_memset" "profile" } } */
+/* { dg-final-use-not-autofdo { scan-ipa-dump "Single value 111 stringop transformation on __builtin_memset" "profile" } } */
+/* { dg-final-use-not-autofdo { scan-ipa-dump-times "Single value 257 stringop transformation on __builtin_memset" 0 "profile" } } */
