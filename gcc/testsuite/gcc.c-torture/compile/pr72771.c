66592e95db41216e6fb1d76a831f8efa6bcf0d6f
Alan Modra <amodra@gmail.com>
2016-08-18 08:11:22 +0930

    [RELOAD] Don't assume subreg mem address is ok
    This patch fixes a case where reload blindly assumes a subreg mem is
    OK if its address has been partially reloaded by legitimize_reload_address.
    
    	PR rtl-optimization/72771
    	* reload.c (find_reloads): Don't assume that a subreg mem is OK
    	when find_reloads_toplev returns address_reloaded==-1.
    	(alternative_allows_const_pool_ref): Update comment.
    testsuite/
    	* gcc.c-torture/compile/pr72771.c: New.
    
    From-SVN: r239549

---
+++ gcc/testsuite/gcc.c-torture/compile/pr72771.c
@@ -0,0 +1,29 @@
+void fn2(void);
+void fn3(unsigned long);
+
+signed char a;
+int b, c, e, f;
+float *d;
+
+void fn1(void) {
+        unsigned short g = 0;
+lbl_986:
+        for (;;) {
+                c = 0;
+                for (; c <= 2;) {
+                        *d = g;
+                        fn2();
+                        if (e)
+                                goto lbl_986;
+                }
+                g = 2;
+                for (; (short) g >= 0; g--) {
+                        for (; b;) {
+                                fn3(45360);
+                                f = 0;
+                                for (; a >= 0; a--)
+                                        ;
+                        }
+                }
+        }
+}
