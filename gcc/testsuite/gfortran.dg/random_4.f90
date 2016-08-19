91151a7334543817f1f790310a1bfc4589889cad
Janne Blomqvist <jb@gcc.gnu.org>
2016-08-19 17:12:32 +0300

    Always initialize PRNG using random data from the OS.
    libgfortran:
    
    2016-08-16  Janne Blomqvist  <jb@gcc.gnu.org>
    
            * intrinsics/random.c (master_init): New variable.
            (init_rand_state): Move below getosrandom (), maybe initialize
            master_state.
            (random_seed_i4): If called with no arguments, set master_init to
            false, and reinitialize. If called with PUT=, set master_init to
            true.
            (random_seed_i8): Likewise.
    
    testsuite:
    
    2016-08-16  Janne Blomqvist  <jb@gcc.gnu.org>
    
            * gfortran.dg/random_4.f90: Initialize seed before using, handle
            the last special seed value.
            * gfortran.dg/random_7.f90: Use size for last array member instead
            of hardcoded value.
    
    From-SVN: r239611

--- gcc/testsuite/gfortran.dg/random_4.f90
+++ gcc/testsuite/gfortran.dg/random_4.f90
@@ -6,8 +6,11 @@ program trs
   integer, allocatable, dimension(:) :: seed, check
   call test_random_seed(size)
   allocate(seed(size),check(size))
+  seed = 42
   call test_random_seed(put=seed)
   call test_random_seed(get=check)
+  ! With xorshift1024* the last seed value is special
+  seed(size) = check(size)
   if (any (seed /= check)) call abort
 contains
   subroutine test_random_seed(size, put, get)
