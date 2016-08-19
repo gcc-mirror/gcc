gcc-mirror/gcc

commit f9d97e407ff87e930c40d27f37e666fa4db79643

re PR tree-optimization/77282 (test case gcc.dg/autopar/pr46193.c fails starting with r239414)
2016-08-18  Richard Biener  <rguenther@suse.de>

	PR tree-optimization/77282
	* tree-ssa-pre.c (eliminate_dom_walker::before_dom_children):
	When doing auto-parallelizing also prevent use of PHIs that
	carry dependences across loop backedges.

From-SVN: r239565

gcc/tree-ssa-pre.c
@@ -4270,7 +4270,7 @@ eliminate_dom_walker::before_dom_children (basic_block b)
 	  if (sprime
 	      && TREE_CODE (sprime) == SSA_NAME
 	      && do_pre
-	      && flag_tree_loop_vectorize
+	      && (flag_tree_loop_vectorize || flag_tree_parallelize_loops)
 	      && loop_outer (b->loop_father)
 	      && has_zero_uses (sprime)
 	      && bitmap_bit_p (inserted_exprs, SSA_NAME_VERSION (sprime))

commit 9ca60517a094c72cff636aac20e34b1b2c2b7f9b

re PR tree-optimization/77290 (test case gcc.dg/tree-ssa/pr71347.c fails starting with r239565)
2016-08-19  Richard Biener  <rguenther@suse.de>

	PR tree-optimization/77290
	* tree-ssa-pre.c (eliminate_dom_walker::before_dom_children):
	Fix flag_tree_parallelize_loops check.

From-SVN: r239607

gcc/tree-ssa-pre.c
@@ -4270,7 +4270,7 @@ eliminate_dom_walker::before_dom_children (basic_block b)
 	  if (sprime
 	      && TREE_CODE (sprime) == SSA_NAME
 	      && do_pre
-	      && (flag_tree_loop_vectorize || flag_tree_parallelize_loops)
+	      && (flag_tree_loop_vectorize || flag_tree_parallelize_loops > 1)
 	      && loop_outer (b->loop_father)
 	      && has_zero_uses (sprime)
 	      && bitmap_bit_p (inserted_exprs, SSA_NAME_VERSION (sprime))