31c2bc2e0dbf30472c2b6501fd085bfc50ccb8ad
Jakub Jelinek <jakub@redhat.com>
2016-08-19 17:24:22 +0200

    fxsrintrin.h (_fxsave): Remove return keyword in inlines returning void.
    	* config/i386/fxsrintrin.h (_fxsave): Remove return keyword in inlines
    	returning void.
    	(_fxrstor, _fxsave64, _fxrstor64): Likewise.
    	* config/i386/xsaveintrin.h (_xsave, _xrstor, _xsave64, _xrstor64):
    	Likewise.
    	* config/i386/xsaveoptintrin.h (_xsaveopt, _xsaveopt64): Likewise.
    	* config/i386/pkuintrin.h (_wrpkru): Likewise.  Add space after
    	function name.
    	(_rdpkru_u32): Add space after function name.
    
    From-SVN: r239616

--- gcc/config/i386/xsaveintrin.h
+++ gcc/config/i386/xsaveintrin.h
@@ -38,14 +38,14 @@ extern __inline void
 __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 _xsave (void *__P, long long __M)
 {
-  return __builtin_ia32_xsave (__P, __M);
+  __builtin_ia32_xsave (__P, __M);
 }
 
 extern __inline void
 __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 _xrstor (void *__P, long long __M)
 {
-  return __builtin_ia32_xrstor (__P, __M);
+  __builtin_ia32_xrstor (__P, __M);
 }
 
 #ifdef __x86_64__
@@ -53,14 +53,14 @@ extern __inline void
 __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 _xsave64 (void *__P, long long __M)
 {
-  return __builtin_ia32_xsave64 (__P, __M);
+  __builtin_ia32_xsave64 (__P, __M);
 }
 
 extern __inline void
 __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 _xrstor64 (void *__P, long long __M)
 {
-  return __builtin_ia32_xrstor64 (__P, __M);
+  __builtin_ia32_xrstor64 (__P, __M);
 }
 #endif
 
