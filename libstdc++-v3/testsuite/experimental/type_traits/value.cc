dee8cef0c1c1ebf85fceb5c37996ed12a2bec352
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-08-19 15:42:11 +0000

    Fix ambiguities in C++17 mode
    	* include/experimental/tuple (apply): Qualify call to __apply_impl.
    	* include/std/tuple (apply): Likewise.
    	* testsuite/experimental/system_error/value.cc: Fix ambiguities in
    	C++17 mode.
    	* testsuite/experimental/tuple/tuple_size.cc: Likewise.
    	* testsuite/experimental/type_traits/value.cc: Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@239621 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/experimental/type_traits/value.cc
+++ libstdc++-v3/testsuite/experimental/type_traits/value.cc
@@ -20,8 +20,74 @@
 
 #include <experimental/type_traits>
 
-using namespace std;
-using namespace experimental;
+using std::true_type;
+using std::false_type;
+using std::nullptr_t;
+using std::is_void;
+using std::is_null_pointer;
+using std::is_integral;
+using std::is_floating_point;
+using std::is_array;
+using std::is_pointer;
+using std::is_lvalue_reference;
+using std::is_rvalue_reference;
+using std::is_member_object_pointer;
+using std::is_member_function_pointer;
+using std::is_enum;
+using std::is_union;
+using std::is_class;
+using std::is_function;
+using std::is_reference;
+using std::is_arithmetic;
+using std::is_fundamental;
+using std::is_object;
+using std::is_scalar;
+using std::is_compound;
+using std::is_member_pointer;
+using std::is_const;
+using std::is_volatile;
+using std::is_trivial;
+using std::is_trivially_copyable;
+using std::is_standard_layout;
+using std::is_pod;
+using std::is_literal_type;
+using std::is_empty;
+using std::is_polymorphic;
+using std::is_abstract;
+using std::is_final;
+using std::is_signed;
+using std::is_constructible;
+using std::is_default_constructible;
+using std::is_copy_constructible;
+using std::is_move_constructible;
+using std::is_assignable;
+using std::is_copy_assignable;
+using std::is_move_assignable;
+using std::is_destructible;
+using std::is_trivially_constructible;
+using std::is_trivially_default_constructible;
+using std::is_trivially_copy_constructible;
+using std::is_trivially_move_constructible;
+using std::is_trivially_assignable;
+using std::is_trivially_copy_assignable;
+using std::is_trivially_move_assignable;
+using std::is_trivially_destructible;
+using std::is_nothrow_constructible;
+using std::is_nothrow_default_constructible;
+using std::is_nothrow_copy_constructible;
+using std::is_nothrow_move_constructible;
+using std::is_nothrow_assignable;
+using std::is_nothrow_copy_assignable;
+using std::is_nothrow_move_assignable;
+using std::is_nothrow_destructible;
+using std::has_virtual_destructor;
+using std::alignment_of;
+using std::rank;
+using std::extent;
+using std::is_same;
+using std::is_base_of;
+using std::is_convertible;
+using namespace std::experimental;
 
 // These tests are rather simple, the front-end tests already test
 // variable templates, and the library tests for the underlying
