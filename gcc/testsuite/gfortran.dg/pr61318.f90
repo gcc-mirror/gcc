bfbc628a4e8a0626e6ccac0eec8ecfe29eb01f87
kargl <kargl@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-08-22 18:18:14 +0000

    2016-08-22  Steven G. Kargl  <kargl@gcc.gnu.org>
    	PR fortran/61318
    	* interface.c (compare_parameter): Use better locus for error message.
    
    2016-08-22  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	PR fortran/61318
    	* gfortran.dg/pr61318.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@239667 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr61318.f90
@@ -0,0 +1,23 @@
+! { dg-do compile }
+!
+module gbl_message
+  type :: mytype
+    integer(kind=4) :: e
+  end type mytype
+  type(mytype), parameter :: seve = mytype(1)
+end module gbl_message
+
+module gbl_interfaces
+  interface
+    subroutine gagout(message)
+      character(len=*), intent(in) :: message
+    end subroutine gagout
+  end interface
+end module gbl_interfaces
+
+program test
+  use gbl_message
+  use gbl_interfaces
+  call gagout(seve%e,'Some string') ! { dg-error "Type mismatch in argument" }
+end program test
+! { dg-final { cleanup-modules "gbl_interfaces gbl_message" } }
