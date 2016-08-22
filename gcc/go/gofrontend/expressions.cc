868b439e865bbc5ed8cdab23efe88bfddf7e9f67
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-08-22 21:30:49 +0000

    compiler: don't permit P.M if P is a pointer type
        When lowering method expressions of the form "P.M" where
        P is a pointer type (e.g. "type P *T") make sure we examine
        the method set of P and not T during method lookup.
        
        Fixes golang/go#15722.
        
        Reviewed-on: https://go-review.googlesource.com/24843
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@239675 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/go/gofrontend/expressions.cc
+++ gcc/go/gofrontend/expressions.cc
@@ -11439,7 +11439,8 @@ Expression*
 Selector_expression::lower_method_expression(Gogo* gogo)
 {
   Location location = this->location();
-  Type* type = this->left_->type();
+  Type* left_type = this->left_->type();
+  Type* type = left_type;
   const std::string& name(this->name_);
 
   bool is_pointer;
@@ -11469,7 +11470,8 @@ Selector_expression::lower_method_expression(Gogo* gogo)
 	imethod = it->find_method(name);
     }
 
-  if (method == NULL && imethod == NULL)
+  if ((method == NULL && imethod == NULL) 
+      || (left_type->named_type() != NULL && left_type->points_to() != NULL))
     {
       if (!is_ambiguous)
 	error_at(location, "type %<%s%s%> has no method %<%s%>",
