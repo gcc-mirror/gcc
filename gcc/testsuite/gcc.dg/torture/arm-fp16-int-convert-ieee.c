7ff4e41a19132c87c9ef6f0bd2ca809a6c2172a8
Joseph Myers <joseph@codesourcery.com>
2016-08-22 12:48:14 +0100

    Update TEST_I_F calls in ARM fp-int-convert tests.
    gcc/testsuite:
    	* gcc.dg/torture/arm-fp16-int-convert-alt.c (FP16_MAX_EXP): New
    	macro.
    	(main): Update calls to TEST_I_F.
    	* gcc.dg/torture/arm-fp16-int-convert-ieee.c (FP16_MAX_EXP): New
    	macro.
    	(main): Update calls to TEST_I_F.
    
    From-SVN: r239657

--- gcc/testsuite/gcc.dg/torture/arm-fp16-int-convert-ieee.c
+++ gcc/testsuite/gcc.dg/torture/arm-fp16-int-convert-ieee.c
@@ -4,14 +4,15 @@
 
 #include "fp-int-convert.h"
 #define FP16_MANT_DIG 11
+#define FP16_MAX_EXP 16
 
 int
 main (void)
 {
-  TEST_I_F(signed char, unsigned char, float, FP16_MANT_DIG);
-  TEST_I_F(signed short, unsigned short, float, FP16_MANT_DIG);
-  TEST_I_F(signed int, unsigned int, float, FP16_MANT_DIG);
-  TEST_I_F(signed long, unsigned long, float, FP16_MANT_DIG);
-  TEST_I_F(signed long long, unsigned long long, float, FP16_MANT_DIG);
+  TEST_I_F(signed char, unsigned char, float, FP16_MANT_DIG, FP16_MAX_EXP);
+  TEST_I_F(signed short, unsigned short, float, FP16_MANT_DIG, FP16_MAX_EXP);
+  TEST_I_F(signed int, unsigned int, float, FP16_MANT_DIG, FP16_MAX_EXP);
+  TEST_I_F(signed long, unsigned long, float, FP16_MANT_DIG, FP16_MAX_EXP);
+  TEST_I_F(signed long long, unsigned long long, float, FP16_MANT_DIG, FP16_MAX_EXP);
   exit (0);
 }
