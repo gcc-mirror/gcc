11d34611ac0f7c2edbd8595e5e10f824e95f8662
Uros Bizjak <ubizjak@gmail.com>
2016-08-28 18:30:32 +0200

    backport: re PR target/77270 (Flag -mprftchw is shared with 3dnow for -march=k8)
    	Backport from mainline
    	2016-08-23  Venkataramanan Kumar  <venkataramanan.kumar@amd.com>
    
    	* config/i386/i386.c (processor_alias_table): Enable PTA_PRFCHW
    	for targets amdfam10 and barcelona.
    
    	Backport from mainline
    	2016-08-21  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/77270
    	* config/i386/i386.md (prefetch): When TARGET_PRFCHW or
    	TARGET_PREFETCHWT1 are disabled, emit 3dNOW! write prefetches for
    	non-SSE2 athlons only, otherwise prefer SSE prefetches.
    
    	Backport from mainline
    	2016-08-19  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/77270
    	* config/i386/i386.c (ix86_option_override_internal): Remove
    	PTA_PRFCHW from entries that also have PTA_3DNOW flag.
    	Enable SSE prefetch also for TARGET_PREFETCHWT1.
    	Do not try to enable TARGET_PRFCHW ISA flag here.
    	* config/i386/i386.md (prefetch): Enable also for TARGET_3DNOW.
    	Rewrite expander function body.
    	(*prefetch_3dnow): Enable for TARGET_3DNOW and TARGET_PREFETCHWT1.
    
    testsuite/ChangeLog:
    
    	Backport from mainline
    	2016-08-24  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/77270
    	* gcc.dg/tree-ssa/loop-28.c: Also compile on 32bit x86 targets.
    	(dg-options): Use -march=amdfam10 instead of -march=athlon.
    	* gcc.dg/tree-ssa/update-unroll-1.c: Ditto.
    	* gcc.dg/tree-ssa/prefetch-3.c: Ditto.
    	* gcc.dg/tree-ssa/prefetch-4.c: Ditto.
    	* gcc.dg/tree-ssa/prefetch-5.c: Ditto.
    	* gcc.dg/tree-ssa/prefetch-6.c: Ditto.  Do not require sse2
    	effective target.  Remove scan-assembler-times directives.
    	* gcc.dg/tree-ssa/prefetch-7.c: Ditto.
    	* gcc.dg/tree-ssa/prefetch-8.c: Ditto.
    	* gcc.dg/tree-ssa/prefetch-9.c: Ditto.
    
    From-SVN: r239810

--- gcc/testsuite/gcc.dg/tree-ssa/prefetch-3.c
+++ gcc/testsuite/gcc.dg/tree-ssa/prefetch-3.c
@@ -1,7 +1,7 @@
 /* Prefetching used to prefer nonsensical unroll factor of 5 in this testcase.  */
 
-/* { dg-do compile { target { { i?86-*-* x86_64-*-* } && ia32 } } } */
-/* { dg-options "-O2 -fprefetch-loop-arrays -march=athlon -msse2 -mfpmath=sse -fdump-tree-aprefetch-details" } */
+/* { dg-do compile { target { i?86-*-* x86_64-*-* } } } */
+/* { dg-options "-O2 -fprefetch-loop-arrays -march=amdfam10 -fdump-tree-aprefetch-details" } */
 
 #define N 1000000
 
