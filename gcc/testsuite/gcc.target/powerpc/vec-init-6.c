2db6ea891ce7ea8147948e48a9f45145bfcdeb5d
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-08-29 19:38:07 +0000

    2016-08-29  Michael Meissner  <meissner@linux.vnet.ibm.com>
    	* gcc.target/powerpc/vec-init-4.c: New runtime tests for various
    	vector short/char initializations.
    	* gcc.target/powerpc/vec-init-5.c: Likewise.
    	* gcc.target/powerpc/vec-init-6.c: New compile time test for
    	vector initialization optimizations.
    	* gcc.target/powerpc/vec-init-7.c: Likewise.
    	* gcc.target/powerpc/vec-init-8.c: New runtime tests for various
    	vector float/double initializations.
    	* gcc.target/powerpc/vec-init-9.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@239837 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/vec-init-6.c
@@ -0,0 +1,16 @@
+/* { dg-do compile { target { powerpc64*-*-* && lp64 } } } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power8" } } */
+/* { dg-require-effective-target powerpc_p8vector_ok } */
+/* { dg-options "-mcpu=power8 -O2 -mupper-regs-di" } */
+
+vector int
+merge (int a, int b, int c, int d)
+{
+  return (vector int) { a, b, c, d };
+}
+
+/* { dg-final { scan-assembler     "rldicr" } } */
+/* { dg-final { scan-assembler     "rldicl" } } */
+/* { dg-final { scan-assembler     "mtvsrd" } } */
+/* { dg-final { scan-assembler-not "stw"    } } */
+/* { dg-final { scan-assembler-not "lxvw4x" } } */
