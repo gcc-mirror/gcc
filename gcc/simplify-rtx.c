26122e2d8a1aa5e8ea91f7df94513cde1e2de509
Jakub Jelinek <jakub@redhat.com>
2016-08-30 08:45:56 +0200

    re PR middle-end/77377 (c-c++-common/pr59037.c ICEs with -fpic -msse on i686)
    	PR middle-end/77377
    	* simplify-rtx.c (avoid_constant_pool_reference): For out of bounds
    	constant pool reference return x instead of c.
    
    	* gcc.target/i386/pr77377.c: New test.
    
    From-SVN: r239854

--- gcc/simplify-rtx.c
+++ gcc/simplify-rtx.c
@@ -251,15 +251,14 @@ avoid_constant_pool_reference (rtx x)
       /* If we're accessing the constant in a different mode than it was
          originally stored, attempt to fix that up via subreg simplifications.
          If that fails we have no choice but to return the original memory.  */
-      if ((offset != 0 || cmode != GET_MODE (x))
-	  && offset >= 0 && offset < GET_MODE_SIZE (cmode))
+      if (offset == 0 && cmode == GET_MODE (x))
+	return c;
+      else if (offset >= 0 && offset < GET_MODE_SIZE (cmode))
         {
           rtx tem = simplify_subreg (GET_MODE (x), c, cmode, offset);
           if (tem && CONSTANT_P (tem))
             return tem;
         }
-      else
-        return c;
     }
 
   return x;
