b30bf455ef225dc50e7be396b03ace7589de5803
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-09-04 17:39:05 +0000

    * src/pa/ffi.c (ffi_struct_type): Put type declaration on separate line. 	(ffi_prep_args_pa32): Likewise. 	(ffi_size_stack_pa32): Likewise. 	(ffi_prep_cif_machdep): Likewise. 	(ffi_call): Likewise.  Rename to ffi_call_int.  Add closure argument 	and update call to ffi_call_pa32. 	(ffi_call, ffi_call_go, ffi_prep_go_closure): New. 	(ffi_closure_inner_pa32): Update to handle go closures. 	* src/pa/ffitarget.h (FFI_GO_CLOSURES): Define. 	* src/pa/hpux32.S (ffi_call_pa32): Pass go closure argument in static 	chain register (%ret1). 	(ffi_closure_pa32): Set closure type argument to zero. 	(ffi_go_closure_pa32): New function.  Add unwind data for it. 	* src/pa/linux.S: Likewise.  Use cfi directives for unwind data. 	* testsuite/libffi.go/static-chain.h (STATIC_CHAIN_REG): Define for 	hppa.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@239978 138bc75d-0d04-0410-961f-82ee72b054a4

--- libffi/testsuite/libffi.go/static-chain.h
+++ libffi/testsuite/libffi.go/static-chain.h
@@ -4,6 +4,12 @@
 # define STATIC_CHAIN_REG  "$1"
 #elif defined(__arm__)
 # define STATIC_CHAIN_REG  "ip"
+#elif defined(__hppa__)
+# if defined(__LP64)
+#   define define STATIC_CHAIN_REG  "%r31"
+# else
+#   define define STATIC_CHAIN_REG  "%r29"   /* %ret1 */
+# endif
 #elif defined(__sparc__)
 # if defined(__arch64__) || defined(__sparcv9)
 #  define STATIC_CHAIN_REG "g5"
