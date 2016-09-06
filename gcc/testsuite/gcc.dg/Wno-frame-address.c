a8c070376794b730f125d0453537684563524f9e
Senthil Kumar Selvaraj <senthil_kumar.selvaraj@atmel.com>
2016-09-06 12:02:23 +0000

    Skip Wno-frame-address test for avr
    The avr backend supports __builtin_return_address only for the current 
    stack frame. The test expects it to work for caller frames as well and 
    therefore fails for avr.
    
    Add avr to the list of targets already excluded with dg-skip-if.
    
    gcc/testsuite/ChangeLog:
    
    2016-09-06  Senthil Kumar Selvaraj  <senthil_kumar.selvaraj@atmel.com>
    
    	* gcc.dg/Wno-frame-address.c: Skip for avr-*-*.
    
    From-SVN: r240005

--- gcc/testsuite/gcc.dg/Wno-frame-address.c
+++ gcc/testsuite/gcc.dg/Wno-frame-address.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-skip-if "Cannot access arbitrary stack frames" { arm*-*-* hppa*-*-* ia64-*-* visium-*-* } } */
+/* { dg-skip-if "Cannot access arbitrary stack frames" { arm*-*-* avr-*-* hppa*-*-* ia64-*-* visium-*-* } } */
 /* { dg-options "-Werror" } */
 /* { dg-additional-options "-mbackchain" { target { s390*-*-* } } } */
 
