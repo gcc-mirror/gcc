6ac7322b3ece979c1adf381f7c6c7e421cf9922f
Dominique d'Humieres <dominiq@lps.ens.fr>
2016-09-07 00:18:54 +0200

    re PR debug/77389 (FAIL: g++.dg/debug/dwarf2/template-params-12f.C  -std=gnu++11  scan-assembler-times DIE \\\\([^\\n]*\\\\) DW_TAG_template_value_param ...)
    2016-09-07  Dominique d'Humieres  <dominiq@lps.ens.fr>
    
    	PR debug/77389
    	* g++.dg/debug/dwarf2/template-params-12g.C: Pass -gno-strict-dwarf
    	in dg-options.
    
    	PR debug/57519
    	* g++.dg/debug/dwarf2/imported-decl-2.C: Likewise.
    
    From-SVN: r240015

--- gcc/testsuite/g++.dg/debug/dwarf2/template-params-12g.C
+++ gcc/testsuite/g++.dg/debug/dwarf2/template-params-12g.C
@@ -1,4 +1,4 @@
-// { dg-options "-gdwarf-2 -dA" }
+// { dg-options "-gdwarf-2 -gno-strict-dwarf -dA" }
 // { dg-final { scan-assembler-times "DIE \\(\[^\n\]*\\) DW_TAG_template_value_param" 1 } }
 // { dg-final { scan-assembler-times "DIE \\(\[^\n\]*\\) DW_TAG_template_value_param\[^\n\]*\n\[^\n\]* DW_AT_name\n\[^\n\]* DW_AT_type\n\[^\n\]*\[^\n\]* DW_AT_location\n\[^\n\]* DW_OP_addr\n\[^\n\]*_ZN1B1gEv\[^\n\]*\n\[^\n\]* DW_OP_stack_value\n\[^\n\]* DW_OP_piece\n\[^\n\]*\n\[^\n\]* DW_OP_lit0\n\[^\n\]* DW_OP_stack_value\n\[^\n\]* DW_OP_piece" 1 } }
 #include "template-params-12.H"
