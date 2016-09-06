8eb9e049afc30be66324513ac21f10fb4859b6a0
Jakub Jelinek <jakub@gcc.gnu.org>
2016-09-06 22:30:57 +0200

    re PR target/69255 (ICE on x86_64-linux-gnu in "emit_move_insn")
    	PR target/69255
    	* config/i386/i386.c (ix86_expand_builtin): For builtin with
    	unsupported or unknown ISA, use expand_call.
    
    	* gcc.target/i386/pr69255-1.c: New test.
    	* gcc.target/i386/pr69255-2.c: New test.
    	* gcc.target/i386/pr69255-3.c: New test.
    
    From-SVN: r240014

---
+++ gcc/testsuite/gcc.target/i386/pr69255-2.c
@@ -0,0 +1,17 @@
+/* PR target/69255 */
+/* { dg-do compile } */
+/* { dg-options "-msse4 -mno-avx" } */
+
+#pragma GCC target "avx512vl"
+#pragma GCC target ""
+__attribute__ ((__vector_size__ (32))) long long a;
+__attribute__ ((__vector_size__ (16))) int b;
+
+void
+foo (const long long *p)
+{
+  __builtin_ia32_gather3siv4di (a, p, b, 1, 1);		/* { dg-error "needs isa option -m32 -mavx512vl" } */
+}
+
+/* { dg-warning "AVX vector return without AVX enabled changes the ABI" "" { target *-*-* } 13 } */
+/* { dg-warning "AVX vector argument without AVX enabled changes the ABI" "" { target *-*-* } 13 } */
