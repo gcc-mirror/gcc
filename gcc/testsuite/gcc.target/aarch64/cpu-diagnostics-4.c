34718542cb800bc0d8f0189033912f2817198bdc
Jakub Jelinek <jakub@redhat.com>
2016-09-07 22:18:17 +0200

    PR middle-end/77475: Fix AArch64 testcases.
    2016-09-07  Jakub Jelinek  <jakub@redhat.com>
    
    	PR middle-end/77475
    	* gcc.target/aarch64/arch-diagnostics-1.c: Expect error on line 0.
    	* gcc.target/aarch64/arch-diagnostics-2.c: Likewise.
    	* gcc.target/aarch64/cpu-diagnostics-1.c: Likewise.
    	* gcc.target/aarch64/cpu-diagnostics-2.c: Likewise.
    	* gcc.target/aarch64/cpu-diagnostics-3.c: Likewise.
    	* gcc.target/aarch64/cpu-diagnostics-4.c: Likewise.
    
    From-SVN: r240030

--- gcc/testsuite/gcc.target/aarch64/cpu-diagnostics-4.c
+++ gcc/testsuite/gcc.target/aarch64/cpu-diagnostics-4.c
@@ -1,4 +1,4 @@
-/* { dg-error "missing" "" {target "aarch64*-*-*" } } */
+/* { dg-error "missing" "" {target "aarch64*-*-*" } 0 } */
 /* { dg-skip-if "do not override -mcpu" { *-*-* } { "-mcpu=*" } { "" } } */
 /* { dg-options "-O2 -mcpu=+dummy" } */
 
