7e4955774fe4e365ec762a51d90ef6365b8be315
Alexander Ivchenko <alexander.ivchenko@intel.com>
2016-09-09 21:38:06 +0000

    re PR target/77267 (MPX does not work in a presence of "-Wl,-as-needed" option (Ubuntu default))
    Fix PR target/77267
    
    2016-09-10  Alexander Ivchenko  <alexander.ivchenko@intel.com>
    
    	PR target/77267
    	* config.in: Regenerate.
    	* config/i386/linux-common.h (MPX_LD_AS_NEEDED_GUARD_PUSH):
    	New macro.
    	(MPX_LD_AS_NEEDED_GUARD_PUSH): Ditto.
    	(LIBMPXWRAPPERS_SPEC): Remove "--no-whole-archive" from
    	static-libmpxwrappers case.
    	(LIBMPX_SPEC): Add guards with MPX_LD_AS_NEEDED_GUARD_PUSH and
    	MPX_LD_AS_NEEDED_GUARD_POP.
    	* configure: Regenerate.
    	* configure.ac (HAVE_LD_PUSHPOPSTATE_SUPPORT): New variable.
    	defined if linker support "--push-state"/"--pop-state".
    
    From-SVN: r240057

--- gcc/config/i386/linux-common.h
+++ gcc/config/i386/linux-common.h
@@ -79,13 +79,23 @@ along with GCC; see the file COPYING3.  If not see
 #endif
 #endif
 
+#ifdef HAVE_LD_PUSHPOPSTATE_SUPPORT
+#define MPX_LD_AS_NEEDED_GUARD_PUSH "--push-state --no-as-needed"
+#define MPX_LD_AS_NEEDED_GUARD_POP "--pop-state"
+#else
+#define MPX_LD_AS_NEEDED_GUARD_PUSH ""
+#define MPX_LD_AS_NEEDED_GUARD_POP ""
+#endif
+
 #ifndef LIBMPX_SPEC
 #if defined(HAVE_LD_STATIC_DYNAMIC)
 #define LIBMPX_SPEC "\
 %{mmpx:%{fcheck-pointer-bounds:\
     %{static:--whole-archive -lmpx --no-whole-archive" LIBMPX_LIBS "}\
     %{!static:%{static-libmpx:" LD_STATIC_OPTION " --whole-archive}\
-    -lmpx %{static-libmpx:--no-whole-archive " LD_DYNAMIC_OPTION \
+    %{!static-libmpx:" MPX_LD_AS_NEEDED_GUARD_PUSH "} -lmpx \
+    %{!static-libmpx:" MPX_LD_AS_NEEDED_GUARD_POP "} \
+    %{static-libmpx:--no-whole-archive " LD_DYNAMIC_OPTION \
     LIBMPX_LIBS "}}}}"
 #else
 #define LIBMPX_SPEC "\
@@ -98,8 +108,8 @@ along with GCC; see the file COPYING3.  If not see
 #define LIBMPXWRAPPERS_SPEC "\
 %{mmpx:%{fcheck-pointer-bounds:%{!fno-chkp-use-wrappers:\
     %{static:-lmpxwrappers}\
-    %{!static:%{static-libmpxwrappers:" LD_STATIC_OPTION " --whole-archive}\
-    -lmpxwrappers %{static-libmpxwrappers:--no-whole-archive "\
+    %{!static:%{static-libmpxwrappers:" LD_STATIC_OPTION "}\
+    -lmpxwrappers %{static-libmpxwrappers: "\
     LD_DYNAMIC_OPTION "}}}}}"
 #else
 #define LIBMPXWRAPPERS_SPEC "\
