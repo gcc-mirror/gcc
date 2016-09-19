63012d9a57edc950c5f30242d1e19318b5708060
Joseph Myers <joseph@codesourcery.com>
2016-09-19 22:51:56 +0100

    Make max_align_t respect _Float128.
    The _FloatN, _FloatNx, _DecimalN and _DecimalNx types are specified in
    such a way that they are basic types, meaning that max_align_t must be
    at least as aligned as those types.
    
    On 32-bit x86, max_align_t is currently 8-byte aligned, but
    _Decimal128 and _Float128 are 16-byte aligned, so the alignment of
    max_align_t needs to increase to meet the standard requirements for
    these types.
    
    This patch implements such an increase.  Because max_align_t needs to
    be usable for C++ as well as for C, <stddef.h> can't actually refer to
    _Float128, but needs to use __float128 (or some other notation for the
    type) instead.  And since __float128 is architecture-specific, there
    isn't a preprocessor conditional that means "__float128 is available"
    (whereas one could test __FLT128_MANT_DIG__ to see if _Float128 is
    available; __SIZEOF_FLOAT128__ is available on x86 only).  But I
    believe the only case that actually has an alignment problem here is
    32-bit x86, and <stddef.h> already has lots of conditional specific to
    particular architectures or OSes, so this patch uses a conditional on
    __i386__; that also is the minimal change that ensures neither size
    nor alignment of max_align_t is changed in any case other than where
    it is necessary.  If any other architectures turn out to have such an
    issue, it will show up as failures of one of the testcases added by
    this patch.
    
    Such an increase is of course an ABI change, but a reasonably safe
    one, in that max_align_t doesn't tend to appear in library interfaces
    (rather, it's something to use when writing allocators and similar
    code; most matches found on codesearch.debian.net look like copies of
    the gnulib stddef.h module rather than anything actually using
    max_align_t at all).
    
    max_align_t_align has a corresponding change (adding _Float128 to the
    types considered).
    
    (I think glibc malloc alignment should also increase to 16-byte on
    32-bit x86 so it works for allocating objects of these types, which is
    now straightforward given the fix made for 32-bit powerpc.)
    
    Bootstrapped with no regressions on x86_64-pc-linux-gnu, and
    spot-tested with -m32 that the new float128-align.c test now compiles
    where previously it didn't.
    
    gcc:
    	* ginclude/stddef.h (max_align_t) [__i386__]: Add __float128
    	element.
    
    gcc/c-family:
    	* c-common.c (max_align_t_align): Also consider alignment of
    	float128_type_node.
    
    gcc/testsuite:
    	* gcc.dg/float128-align.c, gcc.dg/float128x-align.c,
    	gcc.dg/float16-align.c, gcc.dg/float32-align.c,
    	gcc.dg/float32x-align.c, gcc.dg/float64-align.c,
    	gcc.dg/float64x-align.c, gcc.dg/floatn-align.h: New tests.
    
    From-SVN: r240248

--- gcc/ginclude/stddef.h
+++ gcc/ginclude/stddef.h
@@ -426,6 +426,14 @@ typedef __WINT_TYPE__ wint_t;
 typedef struct {
   long long __max_align_ll __attribute__((__aligned__(__alignof__(long long))));
   long double __max_align_ld __attribute__((__aligned__(__alignof__(long double))));
+  /* _Float128 is defined as a basic type, so max_align_t must be
+     sufficiently aligned for it.  This code must work in C++, so we
+     use __float128 here; that is only available on some
+     architectures, but only on i386 is extra alignment needed for
+     __float128.  */
+#ifdef __i386__
+  __float128 __max_align_f128 __attribute__((__aligned__(__alignof(__float128))));
+#endif
 } max_align_t;
 #endif
 #endif /* C11 or C++11.  */
