a09b160b6e62ffb5317a608d0f39ff51db44b14b
lkrupp <lkrupp@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-09-21 08:30:51 +0000

    2016-09-21  Louis Krupp  <louis.krupp@zoho.com>
    	* gfortran.dg/pr68078.f90: Run on x86_64_*_linux* only.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@240304 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/pr68078.f90
+++ gcc/testsuite/gfortran.dg/pr68078.f90
@@ -1,6 +1,14 @@
-! { dg-do run }
-! { dg-xfail-run-if "malloc issuing error messages" { *-*-darwin*  } }
+! { dg-do run { target x86_64-*-linux* } }
 ! { dg-additional-sources set_vm_limit.c }
+!
+! This test calls set_vm_limit to set an artificially low address space
+! limit.  set_vm_limit calls setrlimit, which has some portability
+! considerations.  setrlimit gets errors on arm*linux and aarch64*linux,
+! and when the main program calls malloc(), it in turn fails on Darwin.
+! The code being tested is portable, calling ALLOCATED() or ASSOCIATED()
+! to verify that allocation was successful, so the operating assumption
+! is that as long as this test runs on at least one system, we can call
+! it good.
 
 USE :: ISO_C_BINDING !, only: C_INT
 IMPLICIT NONE
