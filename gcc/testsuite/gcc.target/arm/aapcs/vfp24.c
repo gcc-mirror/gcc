65eeae106fa9884e26993c9252c73a625f0e7dfc
mwahab <mwahab@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-09-21 13:28:53 +0000

    [ARM] FP16 ARM Alternative format variants of AAPCS tests.
    testsuite/
    2016-09-21  Matthew Wahab  <matthew.wahab@arm.com>
    
    	* gcc.target/arm/fp16-aapcs-3.c: New.
    	* gcc.target/arm/fp16-aapcs-4.c: New.
    	* gcc.target/arm/aapcs/aapcs/vfp22.c: New.
    	* gcc.target/arm/aapcs/aapcs/vfp23.c: New.
    	* gcc.target/arm/aapcs/aapcs/vfp24.c: New.
    	* gcc.target/arm/aapcs/aapcs/vfp25.c: New.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@240314 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/aapcs/vfp24.c
@@ -0,0 +1,21 @@
+/* Test AAPCS layout (VFP variant)  */
+
+/* { dg-do run { target arm_eabi } }  */
+/* { dg-require-effective-target arm_hard_vfp_ok }  */
+/* { dg-require-effective-target arm_fp16_hw }  */
+/* { dg-add-options arm_fp16_alternative }  */
+
+#ifndef IN_FRAMEWORK
+#define VFP
+#define TESTFILE "vfp24.c"
+
+#define PCSATTR __attribute__((pcs("aapcs")))
+
+#include "abitest.h"
+#else
+ARG (float, 1.0f, R0)
+ARG (double, 2.0, R2)
+ARG (float, 3.0f, STACK)
+ARG (__fp16, 2.0f, STACK+4)
+LAST_ARG (double, 4.0, STACK+8)
+#endif
---
+++ gcc/testsuite/gcc.target/arm/fp16-aapcs-3.c
@@ -0,0 +1,21 @@
+/* { dg-do compile }  */
+/* { dg-require-effective-target arm_hard_vfp_ok }  */
+/* { dg-require-effective-target arm_fp16_ok } */
+/* { dg-options "-O2" }  */
+/* { dg-add-options arm_fp16_alternative } */
+
+/* Test __fp16 arguments and return value in registers (hard-float).  */
+
+void
+swap (__fp16, __fp16);
+
+__fp16
+F (__fp16 a, __fp16 b, __fp16 c)
+{
+  swap (b, a);
+  return c;
+}
+
+/* { dg-final { scan-assembler-times {vmov\tr[0-9]+, s[0-2]} 2 } }  */
+/* { dg-final { scan-assembler-times {vmov.f32\ts1, s0} 1 } }  */
+/* { dg-final { scan-assembler-times {vmov\ts0, r[0-9]+} 2 } }  */
