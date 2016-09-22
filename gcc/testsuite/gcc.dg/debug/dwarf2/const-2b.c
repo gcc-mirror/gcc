43639d2c8f5c6b8573e801fbe155f31889c36d98
Uros Bizjak <ubizjak@gmail.com>
2016-09-22 18:23:20 +0200

    const-2b.c: Also compile for x86_64-*-*.
    	* gcc.dg/debug/dwarf2/const-2b.c: Also compile for x86_64-*-*.
    	Remove SSE effective target requirement.
    
    From-SVN: r240377

--- gcc/testsuite/gcc.dg/debug/dwarf2/const-2b.c
+++ gcc/testsuite/gcc.dg/debug/dwarf2/const-2b.c
@@ -1,6 +1,5 @@
-/* { dg-do compile { target i386*-*-* } } */
+/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
 /* { dg-options "-O -gdwarf -dA -msse" } */
-/* { dg-require-effective-target sse } */
 /* { dg-final { scan-assembler "DW_AT_const_value" } } */
 
 typedef float FloatVect __attribute__((__vector_size__(16)));
