51278c1a42eea2051ebe669f934d4b2ed1abd8d4
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-09-22 07:46:07 +0000

    2016-09-22  Paul Thomas  <pault@gcc.gnu.org>
    	* interface.c (check_dtio_interface1): Introduce errors for
    	alternate returns and incorrect numbers of arguments.
    	(gfc_find_specific_dtio_proc): Return cleanly if the derived
    	type either doesn't exist or has no namespace.
    
    2016-09-22  Paul Thomas  <pault@gcc.gnu.org>
    
    	* gfortran.dg/dtio_11.f90: Correct for changed error messages.
    	* gfortran.dg/dtio_13.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@240342 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/dtio_11.f90
+++ gcc/testsuite/gfortran.dg/dtio_11.f90
@@ -25,7 +25,7 @@ contains
    end
 end
 
-! PR77533 comment #1 - gave warning that
+! PR77533 comment #1 - gave error 'KIND = 0'
 module m3
    type t
    contains
@@ -33,7 +33,20 @@ module m3
       generic :: write(formatted) => s
    end type
 contains
-   subroutine s(x) ! { dg-error "must be of type CLASS" }
+   subroutine s(x) ! { dg-error "Too few dummy arguments" }
       class(t), intent(in) : x ! { dg-error "Invalid character in name" }
+   end
+end
+
+! PR77534
+module m4
+   type t
+   end type
+   interface read(unformatted)
+      module procedure s
+   end interface
+contains
+   subroutine s(dtv) ! { dg-error "Too few dummy arguments" }
+      type(t), intent(inout) :: dtv
    end
 end
