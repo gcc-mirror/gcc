da9b2e696a433e3d201b02d42e21e3e29c8f920a
Jiong Wang <jiong.wang@arm.com>
2016-09-23 10:54:14 +0000

    [PATCH 16/17][ARM] Add tests for VFP FP16 ACLE instrinsics.
    testsuite/
    2016-09-23  Jiong Wang  <jiong.wang@arm.com>
    	    Matthew Wahab  <matthew.wahab@arm.com>
    
    	* gcc.target/aarch64/advsimd-intrinsics/binary_scalar_op.inc: New.
    	* gcc.target/aarch64/advsimd-intrinsics/unary_scalar_op.inc: New.
    	* gcc.target/aarch64/advsimd-intrinsics/ternary_scalar_op.inc: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vabsh_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vaddh_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvtah_s32_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvtah_u32_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvth_f16_s32_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvth_f16_u32_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvth_n_f16_s32_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvth_n_f16_u32_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvth_n_s32_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvth_n_u32_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvth_s32_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvth_u32_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvtmh_s32_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvtmh_u32_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvtnh_s32_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvtnh_u32_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvtph_s32_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vcvtph_u32_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vdivh_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vfmah_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vfmsh_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vmaxnmh_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vminnmh_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vmulh_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vnegh_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vrndah_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vrndh_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vrndih_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vrndmh_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vrndnh_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vrndph_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vrndxh_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vsqrth_f16_1.c: New.
    	* gcc.target/aarch64/advsimd-intrinsics/vsubh_f16_1.c: New.
    
    
    Co-Authored-By: Matthew Wahab <matthew.wahab@arm.com>
    
    From-SVN: r240426

---
+++ gcc/testsuite/gcc.target/aarch64/advsimd-intrinsics/vcvth_n_u32_f16_1.c
@@ -0,0 +1,100 @@
+/* { dg-do run } */
+/* { dg-require-effective-target arm_v8_2a_fp16_scalar_hw } */
+/* { dg-add-options arm_v8_2a_fp16_scalar }  */
+
+#include <arm_fp16.h>
+
+/* Input values.  */
+float16_t input[] =
+{
+  0.0, -0.0,
+  123.4, -567.8,
+  -34.8, 1024,
+  663.1, 169.1,
+  -4.8, 77.0,
+  -144.5, -56.8,
+
+  (float16_t) -16, (float16_t) -15,
+  (float16_t) -14, (float16_t) -13,
+};
+
+/* Expected results (32-bit hexadecimal representation).  */
+uint32_t expected_1[] =
+{
+  0x00000000,
+  0x00000000,
+  0x000000f6,
+  0x00000000,
+  0x00000000,
+  0x00000800,
+  0x0000052e,
+  0x00000152,
+  0x00000000,
+  0x0000009a,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+};
+
+uint32_t expected_2[] =
+{
+  0x00000000,
+  0x00000000,
+  0x000001ed,
+  0x00000000,
+  0x00000000,
+  0x00001000,
+  0x00000a5c,
+  0x000002a4,
+  0x00000000,
+  0x00000134,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+};
+
+uint32_t expected_3[] =
+{
+  0x00000000,
+  0x00000000,
+  0xffffffff,
+  0x00000000,
+  0x00000000,
+  0xffffffff,
+  0xffffffff,
+  0xffffffff,
+  0x00000000,
+  0xffffffff,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+  0x00000000,
+};
+
+#define TEST_MSG "VCVTH_N_U32_F16"
+#define INSN_NAME vcvth_n_u32_f16
+
+#define INPUT input
+#define EXPECTED_1 expected_1
+#define EXPECTED_2 expected_2
+#define EXPECTED_3 expected_3
+
+#define INPUT_TYPE float16_t
+#define OUTPUT_TYPE uint32_t
+#define OUTPUT_TYPE_SIZE 32
+
+#define SCALAR_OPERANDS
+#define SCALAR_1 1
+#define SCALAR_2 2
+#define SCALAR_3 32
+
+/* Include the template for unary scalar operations.  */
+#include "unary_scalar_op.inc"
---
+++ gcc/testsuite/gcc.target/aarch64/advsimd-intrinsics/vmaxnmh_f16_1.c
@@ -0,0 +1,42 @@
+/* { dg-do run } */
+/* { dg-require-effective-target arm_v8_2a_fp16_scalar_hw } */
+/* { dg-add-options arm_v8_2a_fp16_scalar }  */
+
+#include <arm_fp16.h>
+
+#define INFF __builtin_inf ()
+
+/* Expected results (16-bit hexadecimal representation).  */
+uint16_t expected[] =
+{
+  0x3c00 /* 1.000000 */,
+  0x3c00 /* 1.000000 */,
+  0x4000 /* 2.000000 */,
+  0x5640 /* 100.000000 */,
+  0x4f80 /* 30.000000 */,
+  0x3666 /* 0.399902 */,
+  0x3800 /* 0.500000 */,
+  0x3d52 /* 1.330078 */,
+  0xc64d /* -6.300781 */,
+  0x4d00 /* 20.000000 */,
+  0x355d /* 0.335205 */,
+  0x409a /* 2.300781 */,
+  0x3c00 /* 1.000000 */,
+  0x4a91 /* 13.132812 */,
+  0x34f6 /* 0.310059 */,
+  0x4d00 /* 20.000000 */,
+  0x7c00 /* inf */,
+  0x7c00 /* inf */
+};
+
+#define TEST_MSG "VMAXNMH_F16"
+#define INSN_NAME vmaxnmh_f16
+
+#define EXPECTED expected
+
+#define INPUT_TYPE float16_t
+#define OUTPUT_TYPE float16_t
+#define OUTPUT_TYPE_SIZE 16
+
+/* Include the template for binary scalar operations.  */
+#include "binary_scalar_op.inc"
