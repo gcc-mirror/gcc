d4c65e05c8962e44a7b811c0701688fac5a1cfa0
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-09-23 07:58:07 +0000

    Support movbe as a i386 target optimization node
    	* gcc.target/i386/movbe-4.c: New test.
    	* config/i386/i386.c (ix86_valid_target_attribute_inner_p):
    	Handle movbe.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@240394 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/movbe-4.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+
+#pragma GCC target ("movbe")
+
+extern int x;
+
+void
+foo (int i)
+{
+  x = __builtin_bswap32 (i);
+}
+
+int
+bar ()
+{
+  return __builtin_bswap32 (x);
+}
+
+/* { dg-final { scan-assembler-times "movbe\[ \t\]" 2 } } */
