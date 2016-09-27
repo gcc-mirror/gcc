febf7a4513bd31e5dcff1a87f5825efbd6fc8e90
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-09-27 13:22:04 +0000

    Fix PR gcov-profile/46266
    	PR gcov-profile/46266
    	* lib/gcov.exp: Verify that <built-in>.gcov file is not
    	considered.
    	* input.h (RESERVED_LOCATION_P): New macro.
    	* profile.c (branch_prob): Use RESERVED_LOCATION_P and
    	instread of comparison with UNKNOWN_LOCATION.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@240536 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/input.h
+++ gcc/input.h
@@ -61,6 +61,8 @@ extern location_t input_location;
 #define LOCATION_BLOCK(LOC) \
   ((tree) ((IS_ADHOC_LOC (LOC)) ? get_data_from_adhoc_loc (line_table, (LOC)) \
    : NULL))
+#define RESERVED_LOCATION_P(LOC) \
+  (LOCATION_LOCUS (LOC) < RESERVED_LOCATION_COUNT)
 
 /* Return a positive value if LOCATION is the locus of a token that is
    located in a system header, O otherwise. It returns 1 if LOCATION
