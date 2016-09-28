9878f2b03d848345e88c3cf1d033f7d9c4a4a9ff
wschmidt <wschmidt@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-09-28 21:35:37 +0000

    2016-09-28  Bill Schmidt  <wschmidt@linux.vnet.ibm.com> 	    Alan Modra  <amodra@gmail.com>
    	Backport from mainline
    	2016-07-01  Alan Modra  <amodra@gmail.com>
    
    	PR rtl-optimization/71709
    	* ira-lives.c (find_call_crossed_cheap_reg): Exit loop on arg reg
    	being set, not referenced.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@240598 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ira-lives.c
+++ gcc/ira-lives.c
@@ -1014,7 +1014,7 @@ find_call_crossed_cheap_reg (rtx_insn *insn)
 		  break;
 		}
 
-	      if (reg_overlap_mentioned_p (reg, PATTERN (prev)))
+	      if (reg_set_p (reg, prev))
 		break;
 	    }
 	  prev = PREV_INSN (prev);
