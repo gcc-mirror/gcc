5424b1504484f5073c6eb4204f86f0621e5e293f
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-09-29 18:55:26 +0000

    * gcc.target/i386/adx-check.h (main): Simplify feature bit tests. 	* gcc.target/i386/avx2-check.h (main): Ditto. 	* gcc.target/i386/avx512bw-check.h (main): Ditto. 	* gcc.target/i386/avx512cd-check.h (main): Ditto. 	* gcc.target/i386/avx512dq-check.h (main): Ditto. 	* gcc.target/i386/avx512er-check.h (main): Ditto. 	* gcc.target/i386/avx512f-check.h (main): Ditto. 	* gcc.target/i386/avx512ifma-check.h (main): Ditto. 	* gcc.target/i386/avx512vbmi-check.h (main): Ditto. 	* gcc.target/i386/avx512vl-check.h (main): Ditto.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@240631 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/avx512er-check.h
+++ gcc/testsuite/gcc.target/i386/avx512er-check.h
@@ -18,14 +18,14 @@ main ()
   if (!__get_cpuid (1, &eax, &ebx, &ecx, &edx))
     return 0;
 
-  if ((ecx & bit_OSXSAVE) == (bit_OSXSAVE))
+  if (ecx & bit_OSXSAVE)
     {
       if (__get_cpuid_max (0, NULL) < 7)
 	return 0;
 
       __cpuid_count (7, 0, eax, ebx, ecx, edx);
 
-      if ((avx512f_os_support ()) && ((ebx & bit_AVX512ER) == bit_AVX512ER))
+      if ((ebx & bit_AVX512ER) && avx512f_os_support ())
 	{
 	  do_test ();
 #ifdef DEBUG
