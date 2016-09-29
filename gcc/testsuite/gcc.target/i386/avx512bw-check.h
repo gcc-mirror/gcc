4ab76933d107db95323b83e33d89bced11fa7e09
Uros Bizjak <ubizjak@gmail.com>
2016-09-29 20:55:26 +0200

    adx-check.h (main): Simplify feature bit tests.
    	* gcc.target/i386/adx-check.h (main): Simplify feature bit tests.
    	* gcc.target/i386/avx2-check.h (main): Ditto.
    	* gcc.target/i386/avx512bw-check.h (main): Ditto.
    	* gcc.target/i386/avx512cd-check.h (main): Ditto.
    	* gcc.target/i386/avx512dq-check.h (main): Ditto.
    	* gcc.target/i386/avx512er-check.h (main): Ditto.
    	* gcc.target/i386/avx512f-check.h (main): Ditto.
    	* gcc.target/i386/avx512ifma-check.h (main): Ditto.
    	* gcc.target/i386/avx512vbmi-check.h (main): Ditto.
    	* gcc.target/i386/avx512vl-check.h (main): Ditto.
    
    From-SVN: r240631

--- gcc/testsuite/gcc.target/i386/avx512bw-check.h
+++ gcc/testsuite/gcc.target/i386/avx512bw-check.h
@@ -19,14 +19,14 @@ main ()
     return 0;
 
   /* Run AVX512BW test only if host has AVX512BW support.  */
-  if ((ecx & bit_OSXSAVE) == (bit_OSXSAVE))
+  if (ecx & bit_OSXSAVE)
     {
       if (__get_cpuid_max (0, NULL) < 7)
 	return 0;
 
       __cpuid_count (7, 0, eax, ebx, ecx, edx);
 
-      if ((avx512f_os_support ()) && ((ebx & bit_AVX512BW) == bit_AVX512BW))
+      if ((ebx & bit_AVX512BW) && avx512f_os_support ())
 	{
 	  do_test ();
 #ifdef DEBUG
--- gcc/testsuite/gcc.target/i386/avx512f-check.h
+++ gcc/testsuite/gcc.target/i386/avx512f-check.h
@@ -19,14 +19,14 @@ main ()
     return 0;
 
   /* Run AVX512F test only if host has AVX512F support.  */
-  if ((ecx & bit_OSXSAVE) == (bit_OSXSAVE))
+  if (ecx & bit_OSXSAVE)
     {
       if (__get_cpuid_max (0, NULL) < 7)
 	return 0;
 
       __cpuid_count (7, 0, eax, ebx, ecx, edx);
 
-      if ((avx512f_os_support ()) && ((ebx & bit_AVX512F) == bit_AVX512F))
+      if ((ebx & bit_AVX512F) && avx512f_os_support ())
 	{
 	  do_test ();
 #ifdef DEBUG
