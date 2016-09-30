37b204de605563e7932e26099d28ea8c86cb1935
Jonathan Wakely <jwakely@redhat.com>
2016-09-30 17:07:43 +0100

    Implement LWG 2192 and LWG 2294 for std::abs
    	* doc/xml/manual/intro.xml: Document LWG 2192 changes.
    	* doc/html/*: Regenerate.
    	* include/Makefile.am: Add bits/std_abs.h.
    	* include/Makefile.in: Regenerate.
    	* include/bits/std_abs.h: New header defining all required overloads
    	of std::abs in one place (LWG 2294).
    	* include/c_global/cmath (abs(double), abs(float), abs(long double)):
    	Move to bits/std_abs.h.
    	(abs<_Tp>(_Tp)): Remove.
    	* include/c_global/cstdlib (abs(long), abs(long long), abs(__int<N>)):
    	Move to bits/std_abs.h.
    	* testsuite/26_numerics/headers/cmath/dr2192.cc: New test.
    	* testsuite/26_numerics/headers/cmath/dr2192_neg.cc: New test.
    	* testsuite/26_numerics/headers/cstdlib/dr2192.cc: New test.
    	* testsuite/26_numerics/headers/cstdlib/dr2192_neg.cc: New test.
    
    From-SVN: r240660

---
+++ libstdc++-v3/testsuite/26_numerics/headers/cmath/dr2192_neg.cc
@@ -0,0 +1,29 @@
+// Copyright (C) 2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile { target c++11 } }
+
+// NB: Don't include any other headers in this file.
+// LWG 2192 requires abs to be ill-formed for unsigned arguments.
+#include <cmath>
+
+void test()
+{
+  std::abs(0u);                 // { dg-error "ambiguous" }
+  std::abs(0lu);                // { dg-error "ambiguous" }
+  std::abs(0llu);               // { dg-error "ambiguous" }
+}
