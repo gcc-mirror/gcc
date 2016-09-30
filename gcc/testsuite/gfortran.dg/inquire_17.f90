a023997f454512a8b8bda756a4d5fe1cb7094ea7
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2016-09-30 04:49:36 +0000

    backport: re PR fortran/77707 (formatted direct access: nextrec off by one)
    2016-09-29  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	Backport from trunk
    	PR libgfortran/77707
    	io/transfer.c (next_record): Flush before calculating next_record.
    	Correctly calculate.
    
    	* gfortran.dg/inquire_17.f90: New test.
    
    From-SVN: r240645

---
+++ gcc/testsuite/gfortran.dg/inquire_17.f90
@@ -0,0 +1,10 @@
+! { dg-do run }
+! PR77707 formatted direct access: nextrec off by one
+program directaccess_formatted
+  integer nextrec
+  open(10, status='scratch', form='formatted', access='direct', recl=10*4)
+  write(10,'(10i4)',rec=9) 1,2,3,4,5,6,7,8,9,10
+  inquire(unit=10,nextrec=nextrec)
+  if (nextrec.ne.10) call abort
+  close(10)
+end
