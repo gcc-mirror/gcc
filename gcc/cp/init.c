8ff04ff92d9d88ca84671e36600572558216eab2
Martin Sebor <msebor@redhat.com>
2016-10-04 17:34:00 +0000

    PR c++/77804 - Internal compiler error on incorrect initialization of new-d array
    gcc/cp/ChangeLog:
    
    	PR c++/77804
    	* init.c (warn_placement_new_too_small): Avoid assuming an array type
    	has a constant size.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/77804
    	* g++.dg/warn/Wplacement-new-size-4.C: New test.
    
    From-SVN: r240754

--- gcc/cp/init.c
+++ gcc/cp/init.c
@@ -2504,7 +2504,7 @@ warn_placement_new_too_small (tree type, tree nelts, tree size, tree oper)
 	      && warn_placement_new < 2)
 	    return;
 	}
-	  
+
       /* The size of the buffer can only be adjusted down but not up.  */
       gcc_checking_assert (0 <= adjust);
 
@@ -2526,8 +2526,13 @@ warn_placement_new_too_small (tree type, tree nelts, tree size, tree oper)
       else if (nelts && CONSTANT_CLASS_P (nelts))
 	  bytes_need = tree_to_uhwi (nelts)
 	    * tree_to_uhwi (TYPE_SIZE_UNIT (type));
-      else
+      else if (tree_fits_uhwi_p (TYPE_SIZE_UNIT (type)))
 	bytes_need = tree_to_uhwi (TYPE_SIZE_UNIT (type));
+      else
+	{
+	  /* The type is a VLA.  */
+	  return;
+	}
 
       if (bytes_avail < bytes_need)
 	{
