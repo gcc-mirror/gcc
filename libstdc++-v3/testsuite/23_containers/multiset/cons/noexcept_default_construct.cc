0d692fc4870098ba77decf1b59046a23101768ad
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-10-05 12:01:51 +0000

    77864 Fix noexcept conditions for map/set default constructors
    	PR libstdc++/77864
    	* include/bits/stl_map.h (map::map()): Use nothrow constructibility
    	of comparison function in conditional noexcept.
    	* include/bits/stl_multimap.h (multimap::multimap()): Likewise.
    	* include/bits/stl_multiset.h (multiset::multiset()): Likewise.
    	* include/bits/stl_set.h (set::set()): Likewise.
    	* testsuite/23_containers/map/cons/noexcept_default_construct.cc:
    	New test.
    	* testsuite/23_containers/multimap/cons/noexcept_default_construct.cc:
    	Likewise.
    	* testsuite/23_containers/multiset/cons/noexcept_default_construct.cc:
    	Likewise.
    	* testsuite/23_containers/set/cons/noexcept_default_construct.cc:
    	Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@240780 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/23_containers/multiset/cons/noexcept_default_construct.cc
@@ -0,0 +1,32 @@
+// Copyright (C) 2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile { target c++11 } }
+
+#include <set>
+
+using stype1 = std::multiset<int>;
+static_assert(std::is_nothrow_default_constructible<stype1>::value, "Error");
+
+struct cmp
+{
+  cmp() { }
+  bool operator()(int, int) const;
+};
+
+using stype2 = std::multiset<int, cmp>;
+static_assert( !std::is_nothrow_default_constructible<stype2>::value, "Error");
