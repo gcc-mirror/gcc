f8b6a4889eb29c1ddf65fd172f4f2c0a7d5f7d0d
Marek Polacek <mpolacek@gcc.gnu.org>
2016-10-05 19:25:15 +0000

    Fix last commit.
    From-SVN: r240806

--- gcc/testsuite/g++.dg/cpp1z/init-statement6.C
+++ gcc/testsuite/g++.dg/cpp1z/init-statement6.C
@@ -23,28 +23,3 @@ foo ()
       // whatever
     }
 }
-// Testcase from P0305R1
-// { dg-options -std=c++1z }
-
-#include <string>
-#include <map>
-#include <algorithm>
-
-std::map<int, std::string> m;
-extern int xread (int *);
-extern void publish (int), raise (int);
-
-void
-foo ()
-{
-  if (auto it = m.find (10); it != m.end ()) { std::string s = it->second; }
-  if (char buf[10]; std::fgets(buf, 10, stdin)) { m[0] += buf; }
-  if (int s; int count = xread (&s)) { publish(count); raise(s); }
-
-  const char *s;
-  if (auto keywords = {"if", "for", "while"};
-      std::any_of(keywords.begin(), keywords.end(), [&s](const char* kw) { return s == kw; }))
-    {
-      // whatever
-    }
-}
