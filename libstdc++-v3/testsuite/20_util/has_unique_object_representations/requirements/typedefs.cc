873c7d5ade8c018b0b2ab50c0ba232473bbf86ed
Jonathan Wakely <jwakely@redhat.com>
2016-10-07 19:02:43 +0100

    Implement std::has_unique_object_representations
    	* doc/xml/manual/status_cxx2017.xml: Update status.
    	* include/std/type_traits (has_unique_object_representations): Define.
    	* testsuite/20_util/has_unique_object_representations/value.cc: New.
    	* testsuite/20_util/has_unique_object_representations/requirements/
    	explicit_instantiation.cc: New.
    	* testsuite/20_util/has_unique_object_representations/requirements/
    	typedefs.cc: New.
    
    From-SVN: r240868

---
+++ libstdc++-v3/testsuite/20_util/has_unique_object_representations/requirements/typedefs.cc
@@ -0,0 +1,32 @@
+// { dg-options "-std=gnu++1z" }
+// { dg-do compile { target c++1z } }
+
+// Copyright (C) 2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// NB: This file is for testing type_traits with NO OTHER INCLUDES.
+
+#include <type_traits>
+
+void test01()
+{
+  // Check for required typedefs
+  typedef std::has_unique_object_representations<int>	test_type;
+  static_assert( std::is_same<test_type::value_type, bool>::value );
+  typedef std::integral_constant<bool, test_type{}()> bool_type;
+  static_assert( std::is_same<test_type::type, bool_type>::value );
+}
