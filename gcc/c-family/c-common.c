627be19fe1b259a18a33f0f90f2aee63fee13384
Jason Merrill <jason@redhat.com>
2016-10-08 13:40:22 -0400

    Delay folding of bool conversion into COND_EXPR.
    gcc/c-family/
    	* c-common.c (c_common_truthvalue_conversion): Don't distribute
    	into COND_EXPR in C++.
    gcc/cp/
    	* cp-gimplify.c (cp_fold): Distribute cp_truthvalue_conversion
    	into COND_EXPR.
    
    From-SVN: r240893

--- gcc/c-family/c-common.c
+++ gcc/c-family/c-common.c
@@ -4694,21 +4694,8 @@ c_common_truthvalue_conversion (location_t location, tree expr)
 	}
       /* Distribute the conversion into the arms of a COND_EXPR.  */
       if (c_dialect_cxx ())
-	{
-	  tree op1 = TREE_OPERAND (expr, 1);
-	  tree op2 = TREE_OPERAND (expr, 2);
-	  int w = warn_int_in_bool_context;
-	  warn_int_in_bool_context = 0;
-	  /* In C++ one of the arms might have void type if it is throw.  */
-	  if (!VOID_TYPE_P (TREE_TYPE (op1)))
-	    op1 = c_common_truthvalue_conversion (location, op1);
-	  if (!VOID_TYPE_P (TREE_TYPE (op2)))
-	    op2 = c_common_truthvalue_conversion (location, op2);
-	  expr = fold_build3_loc (location, COND_EXPR, truthvalue_type_node,
-				  TREE_OPERAND (expr, 0), op1, op2);
-	  warn_int_in_bool_context = w;
-	  goto ret;
-	}
+	/* Avoid premature folding.  */
+	break;
       else
 	{
 	  int w = warn_int_in_bool_context;
