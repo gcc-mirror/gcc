74cf8d76035cd6d3a21528c8906fb2db4e5fb8c0
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-10-10 20:40:59 +0000

    PR tree-optimization/71947 	* tree-ssa-dom.c (cprop_into_stmt): Avoid replacing A with B, then 	B with A within a single statement.
    	PR tree-optimization/71947
    	* gcc.dg/tree-ssa/pr71947-1.c: New test.
    	* gcc.dg/tree-ssa/pr71947-2.c: New test.
    	* gcc.dg/tree-ssa/pr71947-3.c: New test.
    	* gcc.dg/tree-ssa/pr71947-4.c: New test.
    	* gcc.dg/tree-ssa/pr71947-5.c: New test.
    	* gcc.dg/tree-ssa/pr71947-6.c: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@240947 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr71947-4.c
@@ -0,0 +1,22 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fno-tree-vrp -fdump-tree-dom-details" } */
+
+
+
+static inline long load(long *p)
+{
+        long ret;
+        asm ("movq      %1,%0\n\t" : "=r" (ret) : "m" (*p));
+        if (ret != *p)
+                __builtin_unreachable();
+        return ret;
+}
+
+long foo(long *mem)
+{
+        long ret;
+        ret = load(mem);
+        return ret + *mem;
+}
+
+/* { dg-final { scan-tree-dump "Folded to: _\[0-9\]+ = _\[0-9\]+ \\* 2"  "dom2" } } */
