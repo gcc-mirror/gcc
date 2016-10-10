c3f9a17935773a90fecd36f30ddf1b7e7d58d115
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-10-10 15:34:28 +0000

    Implement constexpr std::addressof for C++17
    	* doc/xml/manual/intro.xml: Document DR 2296 status.
    	* doc/xml/manual/status_cxx2017.xml: Update status.
    	* include/bits/move.h (__addressof): Add _GLIBCXX_CONSTEXPR and
    	call __builtin_addressof.
    	(addressof): Add _GLIBCXX17_CONSTEXPR.
    	* testsuite/20_util/addressof/requirements/constexpr.cc: New test.
    	* testsuite/20_util/forward/c_neg.cc: Adjust dg-error lineno.
    	* testsuite/20_util/forward/f_neg.cc: Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@240929 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/20_util/forward/c_neg.cc
+++ libstdc++-v3/testsuite/20_util/forward/c_neg.cc
@@ -17,7 +17,7 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-error "static assertion failed" "" { target *-*-* } 89 }
+// { dg-error "static assertion failed" "" { target *-*-* } 86 }
 
 #include <list>
 
