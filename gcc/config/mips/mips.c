7c507664891d053d0e95e3c7f179a2043c2b1210
Robert Suchanek <robert.suchanek@imgtec.com>
2016-10-11 07:58:54 +0000

    [MIPS] Disable -mbranch-likely for -Os when targetting generic arch
    gcc/
    	* config/mips/mips-cpus.def: Replace PTF_AVOID_BRANCHLIKELY with
    	PTF_AVOID_BRANCHLIKELY_ALWAYS for generic architecture and with
    	PTF_AVOID_BRANCHLIKELY_SPEED for others.
    	(mips2, mips3, mips4): Add PTF_AVOID_BRANCHLIKELY_SIZE to tune flags.
    	* config/mips/mips.c (mips_option_override): Enable the branch likely
    	depending on the tune flags and optimization level.
    	* config/mips/mips.h (PTF_AVOID_BRANCHLIKELY): Remove.
    	(PTF_AVOID_BRANCHLIKELY_SPEED): Define.
    	(PTF_AVOID_BRANCHLIKELY_SIZE): Likewise.
    	(PTF_AVOID_BRANCHLIKELY_ALWAYS): Likewise.
    
    From-SVN: r240965

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -19652,8 +19652,15 @@ mips_option_override (void)
   if ((target_flags_explicit & MASK_BRANCHLIKELY) == 0)
     {
       if (ISA_HAS_BRANCHLIKELY
-	  && (optimize_size
-	      || (mips_tune_info->tune_flags & PTF_AVOID_BRANCHLIKELY) == 0))
+	  && ((optimize_size
+	       && (mips_tune_info->tune_flags
+		   & PTF_AVOID_BRANCHLIKELY_SIZE) == 0)
+	      || (!optimize_size
+		  && optimize > 0
+		  && (mips_tune_info->tune_flags
+		      & PTF_AVOID_BRANCHLIKELY_SPEED) == 0)
+	      || (mips_tune_info->tune_flags
+		  & PTF_AVOID_BRANCHLIKELY_ALWAYS) == 0))
 	target_flags |= MASK_BRANCHLIKELY;
       else
 	target_flags &= ~MASK_BRANCHLIKELY;
