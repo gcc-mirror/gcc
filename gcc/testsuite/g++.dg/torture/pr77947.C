3e7c21b1929359743cc2a941ec09e82be95128ce
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-10-12 14:37:53 +0000

    2016-10-12  Richard Biener  <rguenther@suse.de>
    	PR debug/77947
    	* cgraphunit.c (analyze_functions): Preserve cgraph nodes
    	function context.
    
    	* g++.dg/torture/pr77947.C: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@241053 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/torture/pr77947.C
@@ -0,0 +1,29 @@
+// { dg-do compile }
+// { dg-additional-options "-g" }
+
+class A
+{
+public:
+  virtual bool m_fn1 () const = 0;
+};
+class B
+{
+  const A *m_fn2 () const;
+};
+inline const A *
+B::m_fn2 () const
+{
+  class C : A
+  {
+    bool
+    m_fn1 () const
+    {
+    }
+    C () {}
+  };
+}
+void
+fn1 (A &p1)
+{
+  p1.m_fn1 ();
+}
