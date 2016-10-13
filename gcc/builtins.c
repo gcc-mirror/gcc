462a1917f44f11ffa36254e0fe173d7e96617d42
Tom de Vries <tom@codesourcery.com>
2016-10-13 11:40:33 +0000

    Revert backport "Remove RECORD_TYPE special-casing in std_canonical_va_list_type"
    2016-10-13  Tom de Vries  <tom@codesourcery.com>
    
    	revert:
    	2016-10-11  Tom de Vries  <tom@codesourcery.com>
    
    	backport from trunk:
    	2016-10-11  Tom de Vries  <tom@codesourcery.com>
    
    	PR middle-end/77558
    	* builtins.c (std_canonical_va_list_type): Remove RECORD_TYPE
    	special-casing.
    
    From-SVN: r241103

--- gcc/builtins.c
+++ gcc/builtins.c
@@ -4123,8 +4123,10 @@ std_canonical_va_list_type (tree type)
     type = TREE_TYPE (type);
   wtype = va_list_type_node;
   htype = type;
-
-  if (TREE_CODE (wtype) == ARRAY_TYPE)
+  /* Treat structure va_list types.  */
+  if (TREE_CODE (wtype) == RECORD_TYPE && POINTER_TYPE_P (htype))
+    htype = TREE_TYPE (htype);
+  else if (TREE_CODE (wtype) == ARRAY_TYPE)
     {
       /* If va_list is an array type, the argument may have decayed
 	 to a pointer type, e.g. by being passed to another function.
