0512a8c6031c0e04930146486216998430a6e6d9
wschmidt <wschmidt@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-10-13 19:50:41 +0000

    2016-10-13  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    	PR tree-optimization/77937
    	* gcc.dg/torture/pr77937-1.c: New.
    	* gcc.dg/torture/pr77937-2.c: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@241139 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/torture/pr77937-2.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */
+/* { dg-do options "-O3 -march=amdfam10" { target { x86_64-*-* } } } */
+
+extern int fn2(int);
+extern int fn3(int);
+int a, b, c;
+void fn1(long p1) {
+  char *d;
+  for (;; d += p1) {
+    d[0] = fn2(1 >> a);
+    fn3(0);
+    fn3(c >> a);
+    d[1] = fn3(d[1] * b + c >> a);
+    d[4] = fn3(d[4] * b + c >> a);
+    d[5] = fn3(d[5] * b + c >> a);
+  }
+}
