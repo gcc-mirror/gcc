a269531973deae79d2d3aba10ff9eab0fb5df5a9
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2016-10-13 19:50:41 +0000

    re PR tree-optimization/77937 (ICE: in replace_one_candidate, at gimple-ssa-strength-reduction.c:3370)
    2016-10-13  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	PR tree-optimization/77937
    	* gcc.dg/torture/pr77937-1.c: New.
    	* gcc.dg/torture/pr77937-2.c: New.
    
    From-SVN: r241139

---
+++ gcc/testsuite/gcc.dg/torture/pr77937-1.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-do options "-O3 -march=amdfam10" { target { x86_64-*-* } } } */
+
+int *a;
+int b, c, d;
+void fn1(char *p1, int p2) {
+  int x;
+  while (1) {
+    x = 0;
+    for (; x < 8; x++)
+      p1[0] = -a[0] * d + p1[0] * c + 1 >> b >> 1;
+    p1 += p2;
+  }
+}
