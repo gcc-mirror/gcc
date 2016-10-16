76d67a8364f437e4c46762fd952c2d35decd1059
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-10-16 13:59:07 +0000

    * system-aix.ads (Always_Compatible_Rep): Change to False. 	* system-aix64.ads (Always_Compatible_Rep): Likewise. 	* system-hpux-ia64.ads (Always_Compatible_Rep): Likewise. 	* system-hpux.ads (Always_Compatible_Rep): Likewise. 	* system-linux-alpha.ads (Always_Compatible_Rep): Likewise. 	* system-linux-hppa.ads (Always_Compatible_Rep): Likewise. 	* system-linux-ia64.ads (Always_Compatible_Rep): Likewise. 	* system-linux-mips.ads (Always_Compatible_Rep): Likewise. 	* system-linux-mips64el.ads (Always_Compatible_Rep): Likewise. 	* system-linux-mipsel.ads (Always_Compatible_Rep): Likewise. 	* system-linux-s390.ads (Always_Compatible_Rep): Likewise. 	* system-linux-s390x.ads (Always_Compatible_Rep): Likewise. 	* system-linux-sh4.ads (Always_Compatible_Rep): Likewise. 	* system-linux-sparc.ads (Always_Compatible_Rep): Likewise. 	* system-linux-sparcv9.ads (Always_Compatible_Rep): Likewise. 	* system-rtems.ads (Always_Compatible_Rep): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@241213 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/system-linux-mips.ads
+++ gcc/ada/system-linux-mips.ads
@@ -139,7 +139,7 @@ private
    Support_Composite_Assign  : constant Boolean := True;
    Support_Composite_Compare : constant Boolean := True;
    Support_Long_Shifts       : constant Boolean := True;
-   Always_Compatible_Rep     : constant Boolean := True;
+   Always_Compatible_Rep     : constant Boolean := False;
    Suppress_Standard_Library : constant Boolean := False;
    Use_Ada_Main_Program_Name : constant Boolean := False;
    Frontend_Exceptions       : constant Boolean := False;
