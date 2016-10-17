718625ad3a786a3df0405d2e080c211ccad915f4
Kugan Vivekanandarajah <kuganv@linaro.org>
2016-10-17 23:37:57 +0000

    Use get/set_ptr_nonnull in ipa-vrp
    Use get/set_ptr_nonnull in ipa-vrp
    gcc/ChangeLog:
    
    2016-10-18  Kugan Vivekanandarajah  <kuganv@linaro.org>
    
    	* ipa-prop.c (ipa_compute_jump_functions_for_edge): Set value range
    	for pointer type too.
    	(ipcp_update_vr): set_ptr_nonnull for pointer.
    
    gcc/testsuite/ChangeLog:
    
    2016-10-18  Kugan Vivekanandarajah  <kuganv@linaro.org>
    
    	* gcc.dg/ipa/vrp4.c: New test.
    
    From-SVN: r241288

---
+++ gcc/testsuite/gcc.dg/ipa/vrp4.c
@@ -0,0 +1,27 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-ipa-cp-details" } */
+
+static __attribute__((noinline, noclone))
+int foo (int *p)
+{
+  if (!p)
+    return 0;
+  *p = 1;
+}
+
+struct st
+{
+  int a;
+  int b;
+};
+
+int bar (struct st *s)
+{
+
+  if (!s)
+    return 0;
+  foo (&s->a);
+  foo (&s->b);
+}
+
+/* { dg-final { scan-ipa-dump "Setting nonnull for 0" "cp" } } */
