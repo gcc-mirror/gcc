82f4e12700d1c86ae9825d7b510bfab2375fc1ce
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-10-17 22:15:02 +0000

    * expmed.c (expand_shift_1): Add MAY_FAIL parameter and do not assert 	that the result is non-zero if it is true. 	(maybe_expand_shift): New wrapper around expand_shift_1. 	(emit_store_flag): Call maybe_expand_shift in lieu of expand_shift.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@241282 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/expmed.c
+++ gcc/expmed.c
@@ -2248,11 +2248,13 @@ expand_dec (rtx target, rtx dec)
    and AMOUNT the rtx for the amount to shift by.
    Store the result in the rtx TARGET, if that is convenient.
    If UNSIGNEDP is nonzero, do a logical shift; otherwise, arithmetic.
-   Return the rtx for where the value is.  */
+   Return the rtx for where the value is.
+   If that cannot be done, abort the compilation unless MAY_FAIL is true,
+   in which case 0 is returned.  */
 
 static rtx
 expand_shift_1 (enum tree_code code, machine_mode mode, rtx shifted,
-		rtx amount, rtx target, int unsignedp)
+		rtx amount, rtx target, int unsignedp, bool may_fail = false)
 {
   rtx op1, temp = 0;
   int left = (code == LSHIFT_EXPR || code == LROTATE_EXPR);
@@ -2449,7 +2451,7 @@ expand_shift_1 (enum tree_code code, machine_mode mode, rtx shifted,
 	 define_expand for lshrsi3 was added to vax.md.  */
     }
 
-  gcc_assert (temp);
+  gcc_assert (temp != NULL_RTX || may_fail);
   return temp;
 }
 
@@ -2466,6 +2468,16 @@ expand_shift (enum tree_code code, machine_mode mode, rtx shifted,
 {
   return expand_shift_1 (code, mode,
 			 shifted, GEN_INT (amount), target, unsignedp);
+}
+
+/* Likewise, but return 0 if that cannot be done.  */
+
+static rtx
+maybe_expand_shift (enum tree_code code, machine_mode mode, rtx shifted,
+		    int amount, rtx target, int unsignedp)
+{
+  return expand_shift_1 (code, mode,
+			 shifted, GEN_INT (amount), target, unsignedp, true);
 }
 
 /* Output a shift instruction for expression code CODE,
@@ -5754,11 +5766,12 @@ emit_store_flag (rtx target, enum rtx_code code, rtx op0, rtx op1,
       if (rtx_equal_p (subtarget, op0))
 	subtarget = 0;
 
-      tem = expand_shift (RSHIFT_EXPR, mode, op0,
-			  GET_MODE_BITSIZE (mode) - 1,
-			  subtarget, 0);
-      tem = expand_binop (mode, sub_optab, tem, op0, subtarget, 0,
-			  OPTAB_WIDEN);
+      tem = maybe_expand_shift (RSHIFT_EXPR, mode, op0,
+				GET_MODE_BITSIZE (mode) - 1,
+				subtarget, 0);
+      if (tem)
+	tem = expand_binop (mode, sub_optab, tem, op0, subtarget, 0,
+			    OPTAB_WIDEN);
     }
 
   if (code == EQ || code == NE)
@@ -5820,9 +5833,9 @@ emit_store_flag (rtx target, enum rtx_code code, rtx op0, rtx op1,
     }
 
   if (tem && normalizep)
-    tem = expand_shift (RSHIFT_EXPR, mode, tem,
-			GET_MODE_BITSIZE (mode) - 1,
-			subtarget, normalizep == 1);
+    tem = maybe_expand_shift (RSHIFT_EXPR, mode, tem,
+			      GET_MODE_BITSIZE (mode) - 1,
+			      subtarget, normalizep == 1);
 
   if (tem)
     {
