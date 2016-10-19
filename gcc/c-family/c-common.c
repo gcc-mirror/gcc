5c2ba5784a1d4d61324bc4a6e1d39c1a91734f17
edlinger <edlinger@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-10-19 21:00:39 +0000

    2016-10-19  Bernd Edlinger  <bernd.edlinger@hotmail.de>
            * c-common.c (c_common_truthvalue_conversion): Warn only for signed
            integer shift ops in boolean context.
    
    testsuite:
    2016-10-19  Bernd Edlinger  <bernd.edlinger@hotmail.de>
    
            * c-c++-common/Wint-in-bool-context-2.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@241354 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-family/c-common.c
+++ gcc/c-family/c-common.c
@@ -3328,8 +3328,15 @@ c_common_truthvalue_conversion (location_t location, tree expr)
 					       TREE_OPERAND (expr, 0));
 
     case LSHIFT_EXPR:
-      warning_at (EXPR_LOCATION (expr), OPT_Wint_in_bool_context,
-		  "<< in boolean context, did you mean '<' ?");
+      /* We will only warn on unsigned shifts here, because the majority of
+	 false positive warnings happen in code where unsigned arithmetic
+	 was used in anticipation of a possible overflow.
+	 Furthermore, if we see an unsigned type here we know that the
+	 result of the shift is not subject to integer promotion rules.  */
+      if (TREE_CODE (TREE_TYPE (expr)) == INTEGER_TYPE
+	  && !TYPE_UNSIGNED (TREE_TYPE (expr)))
+	warning_at (EXPR_LOCATION (expr), OPT_Wint_in_bool_context,
+		    "<< in boolean context, did you mean '<' ?");
       break;
 
     case COND_EXPR:
