f75036994f9d6945265d02cb29de502d6d7d538d
Kugan Vivekanandarajah <kuganv@linaro.org>
2016-10-21 20:13:55 +0000

    ADDR_EXPR and nonnull
    ADDR_EXPR and nonnull
    gcc/ChangeLog:
    
    2016-10-21  Kugan Vivekanandarajah  <kuganv@linaro.org>
    
    	* ipa-prop.c (ipa_compute_jump_functions_for_edge): Create nonzero
    	value range for pointers in more cases.
    
    gcc/testsuite/ChangeLog:
    
    2016-10-21  Kugan Vivekanandarajah  <kuganv@linaro.org>
    
    	* gcc.dg/ipa/vrp5.c: New test.
    	* gcc.dg/ipa/vrp6.c: New test.
    
    From-SVN: r241428

---
+++ gcc/testsuite/gcc.dg/ipa/vrp6.c
@@ -0,0 +1,34 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-ipa-cp-details -fdump-tree-vrp1" } */
+
+static __attribute__((noinline, noclone))
+int foo (int *p)
+{
+  if (!p)
+    return 0;
+  *p = 1;
+}
+
+struct st
+{
+  int a;
+  int b;
+};
+
+struct st s2;
+int a;
+int bar (struct st *s)
+{
+  struct st s3;
+  int b;
+  if (!s)
+    return 0;
+  foo (&s->a);
+  foo (&s2.a);
+  foo (&s3.a);
+  foo (&a);
+  foo (&b);
+}
+
+/* { dg-final { scan-ipa-dump "Setting nonnull for 0" "cp" } } */
+/* { dg-final { scan-tree-dump-times "if" 1 "vrp1" } } */
