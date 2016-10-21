20276dc9d1b4222926d4670c55933c121fa3d2e3
Jeff Law <law@redhat.com>
2016-10-21 14:41:34 -0600

    re PR tree-optimization/71947 (x ^ y not folded to 0 if x == y by DOM)
    	* PR tree-optimization/71947
    	* gcc.dg/tree-ssa/pr71947-4.c: Avoid x86 opcode.
    	* gcc.dg/tree-ssa/pr71947-5.c: Likewise.
    	* gcc.dg/tree-ssa/pr71947-6.c: Make it opt-in rather than opt-out.
    
    From-SVN: r241429

--- gcc/testsuite/gcc.dg/tree-ssa/pr71947-4.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr71947-4.c
@@ -6,7 +6,7 @@
 static inline long load(long *p)
 {
         long ret;
-        asm ("movq      %1,%0\n\t" : "=r" (ret) : "m" (*p));
+        asm ("xyzzy      %1,%0\n\t" : "=r" (ret) : "m" (*p));
         if (ret != *p)
                 __builtin_unreachable();
         return ret;
