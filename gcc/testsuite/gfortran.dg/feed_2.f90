ef14476730875df063a5788a0072984e22b7c2ec
Fritz Reese <fritzoreese@gmail.com>
2016-10-25 15:20:36 +0000

    Treat form feed as whitespace.
    	gcc/fortran/
    	* gfortran.texi: Document.
    	* gfortran.h (gfc_is_whitespace): Include form feed ('\f').
    
    	gcc/testsuite/gfortran.dg/
    	* feed_1.f90, feed_2.f90: New testcases.
    
    From-SVN: r241517

---
+++ gcc/testsuite/gfortran.dg/feed_2.f90
@@ -0,0 +1,12 @@
+      ! { dg-do compile }
+      ! { dg-options "-ffixed-form" }
+      ! Test acceptance of form feed character in fixed source.
+
+      implicit none
+      integer, volatile :: x
+
+
+
+      x = 5
+
+      end
