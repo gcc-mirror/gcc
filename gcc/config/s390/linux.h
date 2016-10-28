55c01516b302045a706fffb177bee7f660f6ed66
krebbel <krebbel@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-10-28 12:28:24 +0000

    S/390: Add support for arch<n> arch/tune options.
    This patch adds an alternate CPU level naming following the
    architecture level number in the Principles of Operations manual.  So
    instead of having z196, zEC12, and z13 you can use arch9, arch10, and
    arch11.  The old cpu names stay valid and should preferably be used.
    
    The alternate names are supposed to improve compatibility with the IBM
    XL compiler toolchain which uses the arch numbering.
    
    gcc/testsuite/ChangeLog:
    
    2016-10-28  Andreas Krebbel  <krebbel@linux.vnet.ibm.com>
    
    	* gcc.target/s390/target-attribute/tattr-m64-33.c: New test.
    
    gcc/ChangeLog:
    
    2016-10-28  Andreas Krebbel  <krebbel@linux.vnet.ibm.com>
    
    	* config/s390/s390.opt: Support alternate cpu level naming (archXX).
    	* config.gcc: Support alternate archXX cpu levels with
    	--with-arch= and --with-tune=.
    	* config/s390/linux.h: Translate new archXX cpu levels to the
    	original names when calling GAS.
    	* config/s390/tpf.h: Likewise.
    	* doc/invoke.texi: Document the alternate cpu level names.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@241643 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/s390/linux.h
+++ gcc/config/s390/linux.h
@@ -47,9 +47,19 @@ along with GCC; see the file COPYING3.  If not see
 
 
 /* Target specific assembler settings.  */
-
+/* Rewrite -march=arch* options to the original CPU name in order to
+   make it work with older binutils.  */
 #undef  ASM_SPEC
-#define ASM_SPEC "%{m31&m64}%{mesa&mzarch}%{march=*}"
+#define ASM_SPEC					\
+  "%{m31&m64}%{mesa&mzarch}%{march=z*}"			\
+  "%{march=arch3:-march=g5}"				\
+  "%{march=arch5:-march=z900}"				\
+  "%{march=arch6:-march=z990}"				\
+  "%{march=arch7:-march=z9-ec}"				\
+  "%{march=arch8:-march=z10}"				\
+  "%{march=arch9:-march=z196}"				\
+  "%{march=arch10:-march=zEC12}"			\
+  "%{march=arch11:-march=z13}"
 
 
 /* Target specific linker settings.  */
