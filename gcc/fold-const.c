c1b17c76b8f15a9751679ce89aac0e608cf1bb86
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-10-28 13:13:31 +0000

    2016-10-28  Richard Biener  <rguenther@suse.de>
    	PR middle-end/78128
    	PR middle-end/71002
    	* fold-const.c (make_bit_field_ref): Only adjust alias set
    	when the original alias set was zero.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@241646 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fold-const.c
+++ gcc/fold-const.c
@@ -3787,11 +3787,11 @@ make_bit_field_ref (location_t loc, tree inner, tree orig_inner, tree type,
 {
   tree result, bftype;
 
-  if (get_alias_set (inner) != get_alias_set (orig_inner))
+  alias_set_type iset = get_alias_set (orig_inner);
+  if (iset == 0 && get_alias_set (inner) != iset)
     inner = fold_build2 (MEM_REF, TREE_TYPE (inner),
 			 build_fold_addr_expr (inner),
-			 build_int_cst
-			  (reference_alias_ptr_type (orig_inner), 0));
+			 build_int_cst (ptr_type_node, 0));
 
   if (bitpos == 0 && !reversep)
     {
