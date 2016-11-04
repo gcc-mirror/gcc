133e5599229f17308773a4e07465381e1272e166
wschmidt <wschmidt@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-11-04 18:15:30 +0000

    [gcc]
    2016-11-04  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* config/rs6000/rs6000.c (gimple-ssa.h): New #include.
    	(TARGET_GIMPLE_FOLD_BUILTIN): Define as
    	rs6000_gimple_fold_builtin.
    	(rs6000_gimple_fold_builtin): New function.  Add handling for
    	early expansion of vector addition builtins.
    
    
    [gcc/testsuite]
    
    2016-11-04  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* gcc.target/powerpc/fold-vec-add-1.c: New.
    	* gcc.target/powerpc/fold-vec-add-2.c: New.
    	* gcc.target/powerpc/fold-vec-add-3.c: New.
    	* gcc.target/powerpc/fold-vec-add-4.c: New.
    	* gcc.target/powerpc/fold-vec-add-5.c: New.
    	* gcc.target/powerpc/fold-vec-add-6.c: New.
    	* gcc.target/powerpc/fold-vec-add-7.c: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@241857 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/fold-vec-add-4.c
@@ -0,0 +1,45 @@
+/* Verify that overloaded built-ins for vec_add with long long
+   inputs produce the right results.  */
+
+/* { dg-do compile } */
+/* { dg-require-effective-target powerpc_p8vector_ok } */
+
+#include <altivec.h>
+
+vector signed long long
+test1 (vector bool long long x, vector signed long long y)
+{
+  return vec_add (x, y);
+}
+
+vector signed long long
+test2 (vector signed long long x, vector bool long long y)
+{
+  return vec_add (x, y);
+}
+
+vector signed long long
+test3 (vector signed long long x, vector signed long long y)
+{
+  return vec_add (x, y);
+}
+
+vector unsigned long long
+test4 (vector bool long long x, vector unsigned long long y)
+{
+  return vec_add (x, y);
+}
+
+vector unsigned long long
+test5 (vector unsigned long long x, vector bool long long y)
+{
+  return vec_add (x, y);
+}
+
+vector unsigned long long
+test6 (vector unsigned long long x, vector unsigned long long y)
+{
+  return vec_add (x, y);
+}
+
+/* { dg-final { scan-assembler-times "vaddudm" 6 } } */
