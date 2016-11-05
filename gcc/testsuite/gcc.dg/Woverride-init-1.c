a3c01d6db3e9c0885c03bc2859eba0dc5d7110fd
Martin Sebor <msebor@redhat.com>
2016-11-05 22:41:18 +0000

    71115 - [5/6 Regression] Missing warning: excess elements in struct initializer Backport from trunk.
    gcc/c/ChangeLog:
    	* c-typeck.c (error_init): Use
    	expansion_point_location_if_in_system_header.
    	(warning_init): Same.
    
    gcc/testsuite/ChangeLog:
    	* gcc.dg/init-excess-2.c: New test.
    	* gcc.dg/Woverride-init-1.c: Adjust.
    	* gcc.dg/Woverride-init-2.c: Same.
    
    From-SVN: r241877

--- gcc/testsuite/gcc.dg/Woverride-init-1.c
+++ gcc/testsuite/gcc.dg/Woverride-init-1.c
@@ -10,19 +10,19 @@ union u { char a; long long b; };
 struct s s0 = {
   .a = 1,
   .b = 2,
-  .a = 3, /* { dg-warning "initialized field overwritten|near init" } */
-  4, /* { dg-warning "initialized field overwritten|near init" } */
+  .a = 3, /* { dg-warning "initialized field overwritten" } */
+  4, /* { dg-warning "initialized field overwritten" } */
   5
 };
 
 union u u0 = {
   .a = 1,
-  .b = 2, /* { dg-warning "initialized field overwritten|near init" } */
-  .a = 3 }; /* { dg-warning "initialized field overwritten|near init" } */
+  .b = 2, /* { dg-warning "initialized field overwritten" } */
+  .a = 3 }; /* { dg-warning "initialized field overwritten" } */
 
 int a[5] = {
   [0] = 1,
   [1] = 2,
-  [0] = 3, /* { dg-warning "initialized field overwritten|near init" } */
+  [0] = 3, /* { dg-warning "initialized field overwritten" } */
   [2] = 4
 };
