547c87965ee2fd4bf2615adea151ae80e716efe6
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-11-07 23:09:21 +0000

    C++ cleanups before C++17 noexcept in types.
    	* call.c (standard_conversion): Reorganize pointer conversions.
    	* pt.c (convert_nontype_argument_function): Convert to ref here.
    	(convert_nontype_argument): Not here.
    	(convert_template_argument): Add original type to error message.
    	(RECUR_AND_CHECK_FAILURE): Remove trailing semicolon.
    	(unify): Compare function-qualifiers.
    	* typeck.c (same_type_ignoring_top_level_qualifiers_p): Use
    	cp_build_qualified_type rather than TYPE_MAIN_VARIANT.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@241943 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/template/func2.C
+++ gcc/testsuite/g++.dg/template/func2.C
@@ -5,7 +5,7 @@ fptr zeroptr = 0;
 template<typename T, fptr F> struct foo { };
 template<typename T> struct foo<T,zeroptr> { };
 // { dg-error "not a valid template argument" "not valid" { target *-*-* } 6 } 
-// { dg-error "must be the address" "must be the address " { target *-*-* } 6 }
+// { dg-message "must be the address" "must be the address " { target *-*-* } 6 }
 
 // The rest is needed to trigger the ICE in 4.0 to 4.3:
 void f() { }
