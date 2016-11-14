5230c3d825aee39da28c73705bc5852fae9f92a6
Jakub Jelinek <jakub@redhat.com>
2016-11-14 08:54:50 +0100

    Implement P0217R3 - C++17 structured bindings
    	Implement P0217R3 - C++17 structured bindings
    	* g++.dg/cpp1z/decomp1.C: New test.
    	* g++.dg/cpp1z/decomp2.C: New test.
    	* g++.dg/cpp1z/decomp3.C: New test.
    	* g++.dg/cpp1z/decomp4.C: New test.
    	* g++.dg/cpp1z/decomp5.C: New test.
    	* g++.dg/cpp1z/decomp6.C: New test.
    	* g++.dg/cpp1z/decomp7.C: New test.
    	* g++.dg/cpp1z/decomp8.C: New test.
    	* g++.dg/cpp1z/decomp9.C: New test.
    	* g++.dg/cpp1z/decomp10.C: New test.
    
    Co-Authored-By: Jason Merrill <jason@redhat.com>
    
    From-SVN: r242378

---
+++ gcc/testsuite/g++.dg/cpp1z/decomp8.C
@@ -0,0 +1,88 @@
+// { dg-do run { target c++11 } }
+// { dg-options "" }
+
+struct A { int i; long long j; } a[64];
+A b[32];
+
+template <typename T>
+void
+foo (T &b)
+{
+  int i = 0;
+  for (auto &x : a)
+    {
+      x.i = i;
+      x.j = 2 * i++;
+    }
+  for (auto & [ x, y ] : a)	// { dg-warning "decomposition declaration only available with" "" { target c++14_down } }
+    {
+      x += 2;
+      y += 3;
+    }
+  i = 0;
+  for (const auto [ u, v ] : a)	// { dg-warning "decomposition declaration only available with" "" { target c++14_down } }
+    {
+      if (u != i + 2 || v != 2 * i++ + 3)
+	__builtin_abort ();
+    }
+  i = 0;
+  for (auto [ x, y ] : a)	// { dg-warning "decomposition declaration only available with" "" { target c++14_down } }
+    {
+      x += 4;
+      y += 5;
+      if (x != i + 6 || y != 2 * i++ + 8)
+	__builtin_abort ();
+    }
+  i = 0;
+  for (const auto x : a)
+    {
+      if (x.i != i + 2 || x.j != 2 * i++ + 3)
+	__builtin_abort ();
+    }
+  i = 0;
+  for (auto &x : b)
+    {
+      x.i = i;
+      x.j = 2 * i++;
+    }
+  for (auto & [ x, y ] : b)	// { dg-warning "decomposition declaration only available with" "" { target c++14_down } }
+    {
+      x -= 2;
+      y -= 3;
+    }
+  i = 0;
+  for (const auto [ u, v ] : b)	// { dg-warning "decomposition declaration only available with" "" { target c++14_down } }
+    {
+      if (u != i - 2 || v != 2 * i++ - 3)
+	__builtin_abort ();
+    }
+  i = 0;
+  for (auto [ x, y ] : b)	// { dg-warning "decomposition declaration only available with" "" { target c++14_down } }
+    {
+      x -= 4;
+      y -= 5;
+      if (x != i - 6 || y != 2 * i++ - 8)
+	__builtin_abort ();
+    }
+  i = 0;
+  for (const auto x : b)
+    {
+      if (x.i != i - 2 || x.j != 2 * i++ - 3)
+	__builtin_abort ();
+    }
+}
+
+int
+main ()
+{
+  foo (b);
+  for (int i = 0; i < 64; i++)
+    {
+      if (a[i].i != i + 2 || a[i].j != 2 * i + 3)
+	__builtin_abort ();
+      if (i >= 32)
+	continue;
+      if (b[i].i != i - 2 || b[i].j != 2 * i - 3)
+	__builtin_abort ();
+    }
+}
