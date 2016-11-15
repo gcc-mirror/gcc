5a65a911cccb7d9491ee529b687d9a2fb526ab9d
Jakub Jelinek <jakub@redhat.com>
2016-11-15 18:06:43 +0100

    decomp14.C: New test.
    	* g++.dg/cpp1z/decomp14.C: New test.
    	* g++.dg/cpp1z/decomp15.C: New test.
    	* g++.dg/cpp1z/decomp16.C: New test.
    
    From-SVN: r242435

---
+++ gcc/testsuite/g++.dg/cpp1z/decomp15.C
@@ -0,0 +1,24 @@
+// { dg-do compile }
+// { dg-options "-std=c++1z" }
+
+struct A { bool a, b; };
+struct B { int a, b; };
+
+void
+foo ()
+{
+  auto [ a, b ] = A ();
+  for (auto [ a, b ] = A (); a; )
+    ;
+  if (auto [ a, b ] = A (); a)
+    ;
+  switch (auto [ a, b ] = B (); b)
+    {
+    case 2:
+      break;
+    }
+  auto && [ c, d ] = A ();
+  [[maybe_unused]] auto [ e, f ] = A ();
+  alignas (A) auto [ g, h ] = A ();
+  __attribute__((unused)) auto [ i, j ] = A ();
+}
---
+++ gcc/testsuite/g++.dg/cpp1z/decomp16.C
@@ -0,0 +1,47 @@
+// { dg-do compile }
+// { dg-options "-std=c++1z" }
+
+struct A { bool a, b; };
+struct B { int a, b; };
+
+void
+foo ()
+{
+  auto [ a, b ] = A ();
+  for (; auto [ a, b ] = A (); )			// { dg-error "expected" }
+    ;
+  for (; false; auto [ a, b ] = A ())			// { dg-error "expected" }
+    ;
+  if (auto [ a, b ] = A ())				// { dg-error "expected" }
+    ;
+  if (auto [ a, b ] = A (); auto [ c, d ] = A ())	// { dg-error "expected" }
+    ;
+  if (int d = 5; auto [ a, b ] = A ())			// { dg-error "expected" }
+    ;
+  switch (auto [ a, b ] = B ())				// { dg-error "expected" }
+    {
+    case 2:
+      break;
+    }
+  switch (int d = 5; auto [ a, b ] = B ())		// { dg-error "expected" }
+    {
+    case 2:
+      break;
+    }
+  A e = A ();
+  auto && [ c, d ] = e;
+  auto [ i, j ] = A (), [ k, l ] = A ();		// { dg-error "expected" }
+  auto m = A (), [ n, o ] = A ();			// { dg-error "expected" }
+}
+
+template <typename T>
+auto [ a, b ] = A ();					// { dg-error "expected" }
+
+struct C
+{
+  auto [ e, f ] = A ();					// { dg-error "expected" }
+  mutable auto [ g, h ] = A ();				// { dg-error "expected" }
+  virtual auto [ i, j ] = A ();				// { dg-error "expected" }
+  explicit auto [ k, l ] = A ();			// { dg-error "expected" }
+  friend auto [ m, n ] = A ();				// { dg-error "expected" }
+};
