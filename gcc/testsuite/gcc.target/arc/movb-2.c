3e077364f3f6aa74bc662d5c2f16264dd622a4a9
Andrew Burgess <andrew.burgess@embecosm.com>
2016-11-16 11:42:43 +0000

    [ARC] Fix LE tests for nps400 variant.
    gcc/arc: New peephole2 and little endian arc test fixes
    
    Resolve some test failures introduced for little endian arc as a result
    of the recent arc/nps400 additions.
    
    There's a new peephole2 optimisation to merge together two zero_extracts
    in order that the movb instruction can be used.
    
    One of the test cases is extended so that the test does something
    meaningful in both big and little endian arc mode.
    
    Other tests have their expected results updated to reflect improvements
    in other areas of GCC.
    
    gcc/ChangeLog:
    
     Andrew Burgess  <andrew.burgess@embecosm.com>
    
    	* config/arc/arc.md (movb peephole2): New peephole2 to merge two
    	zero_extract operations to allow a movb to occur.
    	* gcc.target/arc/movb-1.c: Update little endian arc results.
    	* gcc.target/arc/movb-2.c: Likewise.
    	* gcc.target/arc/movb-5.c: Likewise.
    	* gcc.target/arc/movh_cl-1.c: Extend test to cover little endian
    	arc.
    
    From-SVN: r242484

--- gcc/testsuite/gcc.target/arc/movb-2.c
+++ gcc/testsuite/gcc.target/arc/movb-2.c
@@ -9,5 +9,5 @@ f (void)
 {
   bar.b = foo.b;
 }
-/* { dg-final { scan-assembler "movb\[ \t\]+r\[0-5\]+, *r\[0-5\]+, *r\[0-5\]+, *23, *23, *9" { target arc-*-* } } } */
+/* { dg-final { scan-assembler "movb\[ \t\]+r\[0-5\]+, *r\[0-5\]+, *r\[0-5\]+, *7, *7, *9" { target arc-*-* } } } */
 /* { dg-final { scan-assembler "movb\[ \t\]+r\[0-5\]+, *r\[0-5\]+, *r\[0-5\]+, *0, *0, *9" { target arceb-*-* } } } */
