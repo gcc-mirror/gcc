88526ee61b4bf2a3272d9ad5e88db74557196774
macro <macro@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-11-16 21:05:01 +0000

    microMIPS/GCC: Fix PIC call relaxation
    	gcc/
    	* config/mips/mips.c (mips_output_jump): Output R_MICROMIPS_JALR
    	rather than R_MIPS_JALR relocation in microMIPS code.  Do not
    	cancel short delay slots in PIC call relaxation.
    
    	gcc/testsuite/
    	* gcc.target/mips/call-1.c (dg-options): Add `-mno-micromips'.
    	(dg-final): Remove microMIPS JALRS mnemonic matching.
    	* gcc.target/mips/call-2.c (dg-options): Add `-mno-micromips'.
    	(dg-final): Remove microMIPS JALRS mnemonic matching.
    	* gcc.target/mips/call-3.c (dg-options): Add `-mno-micromips'.
    	(dg-final): Remove microMIPS JALRS mnemonic matching.
    	* gcc.target/mips/call-4.c (dg-options): Add `-mno-micromips'.
    	* gcc.target/mips/call-5.c (dg-options): Add `-mno-micromips'.
    	* gcc.target/mips/call-6.c (dg-options): Add `-mno-micromips'.
    	* gcc.target/mips/call-1u.c: New test case.
    	* gcc.target/mips/call-2u.c: New test case.
    	* gcc.target/mips/call-3u.c: New test case.
    	* gcc.target/mips/call-4u.c: New test case.
    	* gcc.target/mips/call-5u.c: New test case.
    	* gcc.target/mips/call-6u.c: New test case.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@242512 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/mips/call-5u.c
@@ -0,0 +1,53 @@
+/* Like call-1.c, but for n32.  We cannot use sibling calls for tail and tail2
+   in this case (PR target/57260).  */
+/* { dg-options "-mmicromips -mrelax-pic-calls -mshared -foptimize-sibling-calls -mabi=n32" } */
+/* { dg-skip-if "requires -foptimize-sibling-calls" { *-*-* } { "-O0" } { "" } } */
+/* { dg-final { scan-assembler "\\.reloc\t1f,R_MICROMIPS_JALR,normal\n1:\tjalrs?\t" } } */
+/* { dg-final { scan-assembler "\\.reloc\t1f,R_MICROMIPS_JALR,normal2\n1:\tjalrs?\t" } } */
+/* { dg-final { scan-assembler "\\.reloc\t1f,R_MICROMIPS_JALR,staticfunc\n1:\tjalrs?\t" } } */
+/* { dg-final { scan-assembler "\\.reloc\t1f,R_MICROMIPS_JALR,tail\n1:\tjalrs?\t" } } */
+/* { dg-final { scan-assembler "\\.reloc\t1f,R_MICROMIPS_JALR,tail2\n1:\tjalrs?\t" } } */
+/* { dg-final { scan-assembler "\\.reloc\t1f,R_MICROMIPS_JALR,tail3\n1:\tjrc?\t" } } */
+/* { dg-final { scan-assembler "\\.reloc\t1f,R_MICROMIPS_JALR,tail4\n1:\tjrc?\t" } } */
+
+__attribute__ ((noinline)) static void staticfunc () { asm (""); }
+int normal ();
+void normal2 ();
+
+int
+NOMIPS16 f (int *p)
+{
+  *p = normal ();
+  normal2 ();
+  staticfunc ();
+  return 1;
+}
+
+int tail ();
+
+int
+NOMIPS16 h ()
+{
+  return tail ();
+}
+
+void tail2 ();
+
+NOMIPS16 void g ()
+{
+  tail2 ();
+}
+
+__attribute__ ((visibility ("hidden"))) void tail3 ();
+
+NOMIPS16 void j ()
+{
+  tail3 ();
+}
+
+__attribute__ ((noinline)) static void tail4 () { asm (""); }
+
+NOMIPS16 void k ()
+{
+  tail4 ();
+}
