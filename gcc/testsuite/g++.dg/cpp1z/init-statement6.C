02e0afdc9010bb874b8cf147aea0813f188079e1
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-11-16 01:14:57 +0000

    * g++.dg/cpp1z/init-statement6.C: Rename a function.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@242467 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp1z/init-statement6.C
+++ gcc/testsuite/g++.dg/cpp1z/init-statement6.C
@@ -7,14 +7,14 @@
 
 std::map<int, std::string> m;
 extern int xread (int *);
-extern void publish (int), raise (int);
+extern void publish (int), xraise (int);
 
 void
 foo ()
 {
   if (auto it = m.find (10); it != m.end ()) { std::string s = it->second; }
   if (char buf[10]; std::fgets(buf, 10, stdin)) { m[0] += buf; }
-  if (int s; int count = xread (&s)) { publish(count); raise(s); }
+  if (int s; int count = xread (&s)) { publish(count); xraise(s); }
 
   const char *s;
   if (auto keywords = {"if", "for", "while"};
