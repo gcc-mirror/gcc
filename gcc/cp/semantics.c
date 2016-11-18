9a32634d815a4e0e41421b6b48738f2d51da98a4
Jason Merrill <jason@redhat.com>
2016-11-18 15:27:41 -0500

    PR c++/67631 - list-init and explicit conversions
    	* semantics.c (finish_compound_literal): Call digest_init_flags.
    	* typeck2.c (digest_init_flags): Add complain parm.
    	(store_init_value): Pass it.
    
    From-SVN: r242604

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -2722,7 +2722,8 @@ finish_compound_literal (tree type, tree compound_literal,
       if (type == error_mark_node)
 	return error_mark_node;
     }
-  compound_literal = digest_init (type, compound_literal, complain);
+  compound_literal = digest_init_flags (type, compound_literal, LOOKUP_NORMAL,
+					complain);
   if (TREE_CODE (compound_literal) == CONSTRUCTOR)
     TREE_HAS_CONSTRUCTOR (compound_literal) = true;
   /* Put static/constant array temporaries in static variables, but always
