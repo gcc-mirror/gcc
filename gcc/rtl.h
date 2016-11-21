2059d0e97dc98988ec5edd04cb80ed25ff1c2119
Trevor Saunders <tbsaunde+gcc@tbsaunde.org>
2016-11-21 06:16:13 +0000

    make add_int_reg_note take rtx_insn *
    gcc/ChangeLog:
    
    2016-11-21  Trevor Saunders  <tbsaunde+gcc@tbsaunde.org>
    
    	* rtl.h: Adjust prototype.
    	* rtlanal.c (add_int_reg_note): Change argument type to rtx_insn *.
    
    From-SVN: r242656

--- gcc/rtl.h
+++ gcc/rtl.h
@@ -3017,7 +3017,7 @@ extern int find_reg_fusage (const_rtx, enum rtx_code, const_rtx);
 extern int find_regno_fusage (const_rtx, enum rtx_code, unsigned int);
 extern rtx alloc_reg_note (enum reg_note, rtx, rtx);
 extern void add_reg_note (rtx, enum reg_note, rtx);
-extern void add_int_reg_note (rtx, enum reg_note, int);
+extern void add_int_reg_note (rtx_insn *, enum reg_note, int);
 extern void add_shallow_copy_of_reg_note (rtx_insn *, rtx);
 extern rtx duplicate_reg_note (rtx);
 extern void remove_note (rtx_insn *, const_rtx);
