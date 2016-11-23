d9cfff2275fd3b6fe9a3610b01673d8accde503a
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-11-23 18:45:27 +0000

    PR c++/77907 	* cp-gimplify.c (cp_fold) <case CALL_EXPR>: When calling constructor 	and maybe_constant_value returns non-CALL_EXPR, create INIT_EXPR 	with the object on lhs and maybe_constant_value returned expr on rhs.
    	* g++.dg/cpp0x/pr77907.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@242790 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp0x/pr77907.C
@@ -0,0 +1,22 @@
+// PR c++/77907
+// { dg-do run { target c++11 } }
+// { dg-options "-O2" }
+
+struct A {
+  int foo () { return 1; }
+};
+
+struct B {
+  using C = int (A::*) ();
+  constexpr explicit B (const C x) : b{x} {}
+  C b;
+};
+
+B b{&A::foo};
+
+int
+main ()
+{
+  if (!b.b)
+    __builtin_abort ();
+}
