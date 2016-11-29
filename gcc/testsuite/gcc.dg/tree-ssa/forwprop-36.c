e5c2af0835cb639037ee6c1ad64db634c6f2ef3c
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-11-29 07:48:43 +0000

    2016-11-29  Richard Biener  <rguenther@suse.de>
    	PR middle-end/78546
    	* match.pd: Add CST1 - (CST2 - A) -> CST3 + A missing case.
    
    	* gcc.dg/tree-ssa/forwprop-36.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@242953 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/forwprop-36.c
@@ -0,0 +1,24 @@
+/* { dg-do compile { target int128 } } */
+/* { dg-options "-O -fdump-tree-cddce1" } */
+
+typedef unsigned __int128 u128;
+
+u128 a, b;
+
+static inline u128
+foo (u128 p1)
+{
+  p1 += ~b;
+  return -p1;
+}
+
+int
+main ()
+{
+  u128 x = foo (~0x7fffffffffffffff);
+  if (x != 0x8000000000000001)
+    __builtin_abort();
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump "if \\(b.0_\[0-9\]+ != 0\\)" "cddce1" } } */
