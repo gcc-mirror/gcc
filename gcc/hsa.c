69a71a6d071a5eae8a06282351e53d8c383aba9a
Martin Jambor <mjambor@suse.cz>
2016-12-02 15:42:15 +0100

    [hsa] Exclude parallel outlines from hsa_callable_functions_p
    2016-12-09  Martin Jambor  <mjambor@suse.cz>
    
    	* hsa.c (hsa_callable_function_p): Return false for artificial
    	functions.
    
    From-SVN: r243184

--- gcc/hsa.c
+++ gcc/hsa.c
@@ -90,7 +90,10 @@ bool
 hsa_callable_function_p (tree fndecl)
 {
   return (lookup_attribute ("omp declare target", DECL_ATTRIBUTES (fndecl))
-	  && !lookup_attribute ("oacc function", DECL_ATTRIBUTES (fndecl)));
+	  && !lookup_attribute ("oacc function", DECL_ATTRIBUTES (fndecl))
+	  /* At this point, this is enough to identify clones for
+	     parallel, which for HSA would need to be kernels anyway.  */
+	  && !DECL_ARTIFICIAL (fndecl));
 }
 
 /* Allocate HSA structures that are are used when dealing with different
