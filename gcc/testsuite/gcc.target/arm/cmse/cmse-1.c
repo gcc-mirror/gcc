8261e476cb64cee8891fa676202d1f42decdcd14
Andre Vieira <andre.simoesdiasvieira@arm.com>
2016-12-02 15:34:36 +0000

    Added support for ARMV8-M Security Extension cmse_nonsecure_caller intrinsic
        gcc/ChangeLog:
        2016-12-02  Andre Vieira  <andre.simoesdiasvieira@arm.com>
    		Thomas Preud'homme  <thomas.preudhomme@arm.com>
    
    	* config/arm/arm-builtins.c (arm_builtins): Define
    	ARM_BUILTIN_CMSE_NONSECURE_CALLER.
    	(bdesc_2arg): Add line for cmse_nonsecure_caller.
    	(arm_init_builtins): Handle cmse_nonsecure_caller.
    	(arm_expand_builtin): Likewise.
    	* config/arm/arm_cmse.h (cmse_nonsecure_caller): New.
    
        gcc/testsuite/ChangeLog:
        2016-12-02  Andre Vieira  <andre.simoesdiasvieira@arm.com>
    		Thomas Preud'homme  <thomas.preudhomme@arm.com>
    
    	* gcc.target/arm/cmse/cmse-1.c: Add test for
    	cmse_nonsecure_caller.
    
    
    Co-Authored-By: Thomas Preud'homme <thomas.preudhomme@arm.com>
    
    From-SVN: r243193

--- gcc/testsuite/gcc.target/arm/cmse/cmse-1.c
+++ gcc/testsuite/gcc.target/arm/cmse/cmse-1.c
@@ -65,3 +65,42 @@ int foo (char * p)
 /* { dg-final { scan-assembler-times "ttat " 2 } } */
 /* { dg-final { scan-assembler-times "bl.cmse_check_address_range" 7 } } */
 /* { dg-final { scan-assembler-not "cmse_check_pointed_object" } } */
+
+int __attribute__ ((cmse_nonsecure_entry))
+baz (void)
+{
+  return cmse_nonsecure_caller ();
+}
+
+typedef int __attribute__ ((cmse_nonsecure_call)) (int_nsfunc_t) (void);
+
+int default_callback (void)
+{
+  return 0;
+}
+
+int_nsfunc_t * fp = (int_nsfunc_t *) default_callback;
+
+void __attribute__ ((cmse_nonsecure_entry))
+qux (int_nsfunc_t * callback)
+{
+  fp = cmse_nsfptr_create (callback);
+}
+
+int call_callback (void)
+{
+  if (cmse_is_nsfptr (fp))
+      return fp ();
+  else
+    return default_callback ();
+}
+/* { dg-final { scan-assembler "baz:" } } */
+/* { dg-final { scan-assembler "__acle_se_baz:" } } */
+/* { dg-final { scan-assembler "qux:" } } */
+/* { dg-final { scan-assembler "__acle_se_qux:" } } */
+/* { dg-final { scan-assembler-not "\tcmse_nonsecure_caller" } } */
+/* { dg-final { scan-rtl-dump "and.*reg.*const_int 1" expand } } */
+/* { dg-final { scan-assembler "bic" } } */
+/* { dg-final { scan-assembler "push\t\{r4, r5, r6" } } */
+/* { dg-final { scan-assembler "msr\tAPSR_nzcvq" } } */
+/* { dg-final { scan-assembler-times "bl\\s+__gnu_cmse_nonsecure_call" 1 } } */
