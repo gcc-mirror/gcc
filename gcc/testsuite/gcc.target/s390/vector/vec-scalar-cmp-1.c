081d6f26e591ad89ec4bfa1d478f0220d5d199bd
Andreas Krebbel <krebbel@linux.vnet.ibm.com>
2016-12-02 08:47:07 +0000

    S/390: Fix vector all/any cc modes.
    This fixes a problem with the vector compares producing CC mode
    results.
    
    The instructions produce condition code modes which can be either
    interpreted to check an ALL elements or an ANY element result.  As the
    modes where used before they could not be inverted by the middle-end
    by inverting the comparison code (e.g. eq to ne).  The result usually
    was just wrong.
    
    In fact inverting a comparison code on an CCVALL mode would require to
    also change the mode to CCVANY but this cannot be done easily in the
    middle-end.  With this patch the meaning of an ALL cc mode only refers
    to the not-inverted comparison code (e.g. eq, gt, ge).  With that
    change inverting the comparison code matches a not operation on the
    condition code mask again.
    
    Bootstrapped and regression tested on s390 and s390x.
    
    Bye,
    
    -Andreas-
    
    gcc/testsuite/ChangeLog:
    
    2016-12-02  Andreas Krebbel  <krebbel@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2016-12-02  Andreas Krebbel  <krebbel@linux.vnet.ibm.com>
    
    	* gcc.target/s390/vector/vec-scalar-cmp-1.c: Fix and harden the
    	pattern checks.
    	* gcc.target/s390/zvector/vec-cmp-1.c: New test.
    
    gcc/ChangeLog:
    
    2016-12-02  Andreas Krebbel  <krebbel@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2016-12-02  Andreas Krebbel  <krebbel@linux.vnet.ibm.com>
    
    	* config/s390/s390-modes.def (CCVEQANY, CCVH, CCVHANY, CCVHU)
    	(CCVHUANY): Remove modes.
    	(CCVIH, CCVIHU, CCVIALL, CCVIANY, CCVFALL, CCVFANY): Add modes and
    	documentation.
    	* config/s390/s390.c (s390_match_ccmode_set): Rename cc modes.
    	(s390_expand_vec_compare_scalar): Pick one of the cc consumer
    	modes.
    	(s390_branch_condition_mask): Adjust to use the new cc consumer
    	modes.  The new modes allow for proper reversal in the middle-end.
    	(s390_expand_vec_compare_cc): Determine the proper cc producer and
    	consumer modes for a comparison.
    	* config/s390/s390.md: Rename CCVH to CCVIH and CCVHU to CCVIHU
    	throughout the file.
    	* config/s390/vx-builtins.md: Likewise.
    
    From-SVN: r243166

--- gcc/testsuite/gcc.target/s390/vector/vec-scalar-cmp-1.c
+++ gcc/testsuite/gcc.target/s390/vector/vec-scalar-cmp-1.c
@@ -1,16 +1,7 @@
 /* Check that we use the scalar variants of vector compares.  */
 
 /* { dg-do compile { target { s390*-*-* } } } */
-/* { dg-options "-O3 -mzarch -march=z13" } */
-
-/* { dg-final { scan-assembler-times "wfcedbs\t%v\[0-9\]*,%v0,%v2" 2 } } */
-/* { dg-final { scan-assembler-times "wfchdbs\t%v\[0-9\]*,%v0,%v2" 1 } } */
-/* { dg-final { scan-assembler-times "wfchedbs\t%v\[0-9\]*,%v2,%v0" 1 } } */
-/* { dg-final { scan-assembler-times "wfchdbs\t%v\[0-9\]*,%v2,%v0" 1 } } */
-/* { dg-final { scan-assembler-times "wfchedbs\t%v\[0-9\]*,%v2,%v0" 1 } } */
-/* { dg-final { scan-assembler-times "locrne" 5 } } */
-/* { dg-final { scan-assembler-times "locrno" 1 } } */
-
+/* { dg-options "-O3 -mzarch -march=z13 -fno-asynchronous-unwind-tables" } */
 
 int
 eq (double a, double b)
@@ -18,11 +9,15 @@ eq (double a, double b)
   return a == b;
 }
 
+/* { dg-final { scan-assembler "eq:\n\twfcedbs\t%v\[0-9\]*,%v0,%v2\n\tlhi\t%r1,0\n\tlhi\t%r2,1\n\tlocrne\t%r2,%r1" } } */
+
 int
 ne (double a, double b)
 {
   return a != b;
 }
+
+/* { dg-final { scan-assembler "ne:\n\twfcedbs\t%v\[0-9\]*,%v0,%v2\n\tlhi\t%r1,0\n\tlhi\t%r2,1\n\tlocre\t%r2,%r1" } } */
 
 int
 gt (double a, double b)
@@ -30,11 +25,15 @@ gt (double a, double b)
   return a > b;
 }
 
+/* { dg-final { scan-assembler "gt:\n\twfchdbs\t%v\[0-9\]*,%v0,%v2\n\tlhi\t%r1,0\n\tlhi\t%r2,1\n\tlocrne\t%r2,%r1" } } */
+
 int
 ge (double a, double b)
 {
   return a >= b;
 }
+
+/* { dg-final { scan-assembler "ge:\n\twfchedbs\t%v\[0-9\]*,%v0,%v2\n\tlhi\t%r1,0\n\tlhi\t%r2,1\n\tlocrne\t%r2,%r1" } } */
 
 int
 lt (double a, double b)
@@ -42,8 +41,12 @@ lt (double a, double b)
   return a < b;
 }
 
+/* { dg-final { scan-assembler "lt:\n\twfchdbs\t%v\[0-9\]*,%v2,%v0\n\tlhi\t%r1,0\n\tlhi\t%r2,1\n\tlocrne\t%r2,%r1" } } */
+
 int
 le (double a, double b)
 {
   return a <= b;
 }
+
+/* { dg-final { scan-assembler "le:\n\twfchedbs\t%v\[0-9\]*,%v2,%v0\n\tlhi\t%r1,0\n\tlhi\t%r2,1\n\tlocrne\t%r2,%r1" } } */
