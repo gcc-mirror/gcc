ae58c8c0a406679ca17c2dd437514be641142b16
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-12-06 10:43:49 +0000

    Add missing noexcept on std::_Sp_locker constructors
    2016-12-06  Aditya Kumar  <hiraditya@msn.com>
    
    	* src/c++11/shared_ptr.cc (_Sp_locker::_Sp_locker(const void* p)): Add
    	noexcept on constructor.
    	(_Sp_locker::_Sp_locker(const void* p1, const void* p2)): Same.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@243291 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/src/c++11/shared_ptr.cc
+++ libstdc++-v3/src/c++11/shared_ptr.cc
@@ -56,7 +56,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
     { return _Hash_impl::hash(addr) & __gnu_internal::mask; }
   }
 
-  _Sp_locker::_Sp_locker(const void* p)
+  _Sp_locker::_Sp_locker(const void* p) noexcept
   {
     if (__gthread_active_p())
       {
@@ -67,7 +67,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       _M_key1 = _M_key2 = __gnu_internal::invalid;
   }
 
-  _Sp_locker::_Sp_locker(const void* p1, const void* p2)
+  _Sp_locker::_Sp_locker(const void* p1, const void* p2) noexcept
   {
     if (__gthread_active_p())
       {
