66972191ee05c5dd2d02e5d062fe1c942e42fcd7
Jakub Jelinek <jakub@redhat.com>
2016-12-06 10:23:51 +0100

    re PR c++/71537 (GCC rejects consetxpr boolean conversions and comparisons on the result of pointer arithmetic.)
    2016-12-06  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/71537
    	* fold-const-call.c (fold_const_call_1): Remove memchr handling here.
    	(fold_const_call) <case CFN_BUILT_IN_STRNCMP,
    	case CFN_BUILT_IN_STRNCASECMP>: Formatting improvements.
    	(fold_const_call) <case CFN_BUILT_IN_MEMCMP>: Likewise.  If s2 is 0
    	and arguments have no side-effects, return 0.
    	(fold_const_call): Handle CFN_BUILT_IN_MEMCHR.
    
    	* g++.dg/cpp0x/constexpr-memchr.C: New test.
    
    From-SVN: r243285

--- gcc/fold-const-call.c
+++ gcc/fold-const-call.c
@@ -1491,36 +1491,6 @@ fold_const_call_1 (combined_fn fn, tree type, tree arg0, tree arg1, tree arg2)
       return NULL_TREE;
     }
 
-  switch (fn)
-    {
-    case CFN_BUILT_IN_MEMCHR:
-      {
-	char c;
-	if (integer_zerop (arg2)
-	    && !TREE_SIDE_EFFECTS (arg0)
-	    && !TREE_SIDE_EFFECTS (arg1))
-	  return build_int_cst (type, 0);
-
-	if (!tree_fits_uhwi_p (arg2) || !target_char_cst_p (arg1, &c))
-	  return NULL_TREE;
-
-	unsigned HOST_WIDE_INT length = tree_to_uhwi (arg2);
-	unsigned HOST_WIDE_INT string_length;
-	const char *p1 = c_getstr (arg0, &string_length);
-	if (p1)
-	  {
-	    const char *r
-	      = (const char *)memchr (p1, c, MIN (length, string_length));
-	    if (r == NULL && length <= string_length)
-	      return build_int_cst (type, 0);
-	  }
-
-	break;
-      }
-    default:
-      break;
-    }
-
   return NULL_TREE;
 }
 
@@ -1531,45 +1501,67 @@ tree
 fold_const_call (combined_fn fn, tree type, tree arg0, tree arg1, tree arg2)
 {
   const char *p0, *p1;
+  char c;
   unsigned HOST_WIDE_INT s0, s1;
   size_t s2 = 0;
   switch (fn)
     {
     case CFN_BUILT_IN_STRNCMP:
-      {
-	bool const_size_p = host_size_t_cst_p (arg2, &s2);
-	if (const_size_p && s2 == 0
-	    && !TREE_SIDE_EFFECTS (arg0)
-	    && !TREE_SIDE_EFFECTS (arg1))
-	  return build_int_cst (type, 0);
-	else if (const_size_p
-		 && (p0 = c_getstr (arg0))
-		 && (p1 = c_getstr (arg1)))
-	  return build_int_cst (type, strncmp (p0, p1, s2));
+      if (!host_size_t_cst_p (arg2, &s2))
 	return NULL_TREE;
-      }
+      if (s2 == 0
+	  && !TREE_SIDE_EFFECTS (arg0)
+	  && !TREE_SIDE_EFFECTS (arg1))
+	return build_int_cst (type, 0);
+      else if ((p0 = c_getstr (arg0)) && (p1 = c_getstr (arg1)))
+	return build_int_cst (type, strncmp (p0, p1, s2));
+      return NULL_TREE;
+
     case CFN_BUILT_IN_STRNCASECMP:
-      {
-	bool const_size_p = host_size_t_cst_p (arg2, &s2);
-	if (const_size_p && s2 == 0
-	    && !TREE_SIDE_EFFECTS (arg0)
-	    && !TREE_SIDE_EFFECTS (arg1))
-	  return build_int_cst (type, 0);
-	else if (const_size_p
-		 && (p0 = c_getstr (arg0))
-		 && (p1 = c_getstr (arg1))
-		 && strncmp (p0, p1, s2) == 0)
-	  return build_int_cst (type, 0);
+      if (!host_size_t_cst_p (arg2, &s2))
 	return NULL_TREE;
-      }
+      if (s2 == 0
+	  && !TREE_SIDE_EFFECTS (arg0)
+	  && !TREE_SIDE_EFFECTS (arg1))
+	return build_int_cst (type, 0);
+      else if ((p0 = c_getstr (arg0))
+	       && (p1 = c_getstr (arg1))
+	       && strncmp (p0, p1, s2) == 0)
+	return build_int_cst (type, 0);
+      return NULL_TREE;
+
     case CFN_BUILT_IN_BCMP:
     case CFN_BUILT_IN_MEMCMP:
+      if (!host_size_t_cst_p (arg2, &s2))
+	return NULL_TREE;
+      if (s2 == 0
+	  && !TREE_SIDE_EFFECTS (arg0)
+	  && !TREE_SIDE_EFFECTS (arg1))
+	return build_int_cst (type, 0);
       if ((p0 = c_getstr (arg0, &s0))
 	  && (p1 = c_getstr (arg1, &s1))
-	  && host_size_t_cst_p (arg2, &s2)
 	  && s2 <= s0
 	  && s2 <= s1)
 	return build_cmp_result (type, memcmp (p0, p1, s2));
+      return NULL_TREE;
+
+    case CFN_BUILT_IN_MEMCHR:
+      if (!host_size_t_cst_p (arg2, &s2))
+	return NULL_TREE;
+      if (s2 == 0
+	  && !TREE_SIDE_EFFECTS (arg0)
+	  && !TREE_SIDE_EFFECTS (arg1))
+	return build_int_cst (type, 0);
+      if ((p0 = c_getstr (arg0, &s0))
+	  && s2 <= s0
+	  && target_char_cst_p (arg1, &c))
+	{
+	  const char *r = (const char *) memchr (p0, c, s2);
+	  if (r == NULL)
+	    return build_int_cst (type, 0);
+	  return fold_convert (type,
+			       fold_build_pointer_plus_hwi (arg0, r - p0));
+	}
       return NULL_TREE;
 
     default:
