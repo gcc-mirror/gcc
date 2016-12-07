31876bc751fe837b4ff5153db40d79af74cde61c
thopre01 <thopre01@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-12-07 17:41:47 +0000

    2016-12-07  Thomas Preud'homme  <thomas.preudhomme@arm.com>
        Backport from mainline
        2016-11-16  Thomas Preud'homme  <thomas.preudhomme@arm.com>
    
        gcc/
        * config/arm/arm.md (arm_addsi3): Add alternative for addition of
        general register with general register or ARM constant into SP
        register.
    
        gcc/testsuite/
        * gcc.target/arm/empty_fiq_handler.c: New test.
    
        Backport from mainline
        2016-11-21  Thomas Preud'homme  <thomas.preudhomme@arm.com>
    
        gcc/testsuite/
        * gcc.target/arm/empty_fiq_handler.c: Skip if -mthumb is passed in and
        target is Thumb-only.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@243372 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arm/empty_fiq_handler.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-skip-if "" { ! arm_cortex_m } { "-mthumb" } } */
+
+/* Below code used to trigger an ICE due to missing constraints for
+   sp = fp + cst pattern.  */
+
+void fiq_handler (void) __attribute__((interrupt ("FIQ")));
+
+void
+fiq_handler (void)
+{
+}
