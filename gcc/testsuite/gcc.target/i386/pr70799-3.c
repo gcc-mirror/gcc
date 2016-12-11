8c7cc1a98ff3834ac2e0a57533c08d123aeebd81
Uros Bizjak <ubizjak@gmail.com>
2016-12-11 19:59:07 +0100

    re PR target/70799 (STV pass does not convert DImode shifts)
    	PR target/70799
    	* config/i386/i386.c (dimode_scalar_to_vector_candidate_p)
    	<case ASHIFT, case LSHIFTRT>: Consider all constant shifts.
    	Add FIXME comment.
    	(dimode_scalar_chain::compute_convert_gain): Reduce gain for
    	constant shifts larger or equal than 32.
    
    testsuite/ChangeLog:
    
    	PR target/70799
    	* gcc.target/i386/pr70799-3.c: New test.
    
    From-SVN: r243530

---
+++ gcc/testsuite/gcc.target/i386/pr70799-3.c
@@ -0,0 +1,17 @@
+/* PR target/pr70799 */
+/* { dg-do compile { target { ia32 } } } */
+/* { dg-options "-O2 -march=slm -fno-split-wide-types -mno-stackrealign" } */
+/* { dg-final { scan-assembler "psllq" } } */
+/* { dg-final { scan-assembler "psrlq" } } */
+
+unsigned long long a, b, c;
+
+void test1 (void)
+{
+  a = (b << 55) | c;
+}
+
+void test2 (void)
+{
+  a = (b >> 55) | c;
+}
