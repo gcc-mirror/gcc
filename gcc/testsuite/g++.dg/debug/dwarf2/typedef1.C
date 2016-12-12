9e9034937bfc7a938a152e9fa9ecb806c7077602
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2016-12-12 11:12:17 +0000

    Fix g++.dg/debug/dwarf2/typedef1.C
    	* g++.dg/debug/dwarf2/typedef1.C: Adjust pattern for last change.
    
    From-SVN: r243544

--- gcc/testsuite/g++.dg/debug/dwarf2/typedef1.C
+++ gcc/testsuite/g++.dg/debug/dwarf2/typedef1.C
@@ -3,7 +3,7 @@
 // { dg-options "-gdwarf-2 -dA -fno-debug-types-section" }
 // { dg-do compile }
 // { dg-final { scan-assembler-times "DW_TAG_structure_type" 2 } }
-// { dg-final { scan-assembler-times "DW_AT_name: \"foo<1>\"|\"foo<1u>..\"\[^\n\]*DW_AT_name" 1 } }
+// { dg-final { scan-assembler-times "DW_AT_name: \"foo<1>\"|\"foo<1>..\"\[^\n\]*DW_AT_name" 1 } }
 // { dg-final { scan-assembler-times "DW_TAG_enumeration_type" 2 } }
 // { dg-final { scan-assembler-times "DW_AT_name: \"typedef foo<1>::type type\"|\"typedef foo<1>::type type..\"\[^\n\]*DW_AT_name" 1 } }
 // { dg-final { scan-assembler-times "DIE \\(\[^\n\]*\\) DW_TAG_enumeration_type" 1 } }
