f3362389960d3b27bd8ae830fd4c65b6fc8d19ef
Jonathan Wakely <jwakely@redhat.com>
2016-12-12 16:35:54 +0000

    Improve tests for contents of <new> header
    	Backport from mainline
    	2016-12-07  Jonathan Wakely  <jwakely@redhat.com>
    
    	* testsuite/18_support/headers/new/synopsis.cc: Add C++14 declarations
    	and use effective-target to do test for C++11 and later.
    	* testsuite/18_support/headers/new/synopsis_cxx98.cc: New test.
    
    From-SVN: r243563

--- libstdc++-v3/testsuite/18_support/headers/new/synopsis_cxx98.cc
+++ libstdc++-v3/testsuite/18_support/headers/new/synopsis_cxx98.cc
@@ -1,4 +1,5 @@
-// { dg-do compile { target c++11 } }
+// { dg-options "-std=gnu++98" }
+// { dg-do compile }
 
 // Copyright (C) 2007-2016 Free Software Foundation, Inc.
 //
@@ -21,19 +22,17 @@
 
 namespace std {
   class bad_alloc;
-  class bad_array_new_length;
   struct nothrow_t;
   extern const nothrow_t nothrow;
   typedef void (*new_handler)();
-  new_handler get_new_handler() noexcept;
-  new_handler set_new_handler(new_handler new_p) noexcept;
+  new_handler set_new_handler(new_handler new_p) throw();
 }
 
-void* operator new(std::size_t size);
-void* operator new(std::size_t size, const std::nothrow_t&) noexcept;
+void* operator new(std::size_t size) throw(std::bad_alloc);
+void* operator new(std::size_t size, const std::nothrow_t&) throw();
 void  operator delete(void* ptr) throw();
 void  operator delete(void* ptr, const std::nothrow_t&) throw();
-void* operator new[](std::size_t size);
+void* operator new[](std::size_t size) throw(std::bad_alloc);
 void* operator new[](std::size_t size, const std::nothrow_t&) throw();
 void  operator delete[](void* ptr) throw();
 void  operator delete[](void* ptr, const std::nothrow_t&) throw();
@@ -42,13 +41,3 @@ void* operator new  (std::size_t size, void* ptr) throw();
 void* operator new[](std::size_t size, void* ptr) throw();
 void  operator delete  (void* ptr, void*) throw();
 void  operator delete[](void* ptr, void*) throw();
-
-#if __cplusplus >= 201402L
-// C++14 sized deallocation functions
-void  operator delete(void* ptr, std::size_t size) noexcept;
-void  operator delete(void* ptr, std::size_t size,
-                      const std::nothrow_t&) noexcept;
-void  operator delete[](void* ptr, std::size_t size) noexcept;
-void  operator delete[](void* ptr, std::size_t size,
-                        const std::nothrow_t&) noexcept;
-#endif
