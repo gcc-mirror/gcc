c94328179741e7bdcaf59e888db809681859c6c6
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-12-14 19:40:05 +0000

    PR debug/77844 	* valtrack.c: Include rtl-iter.h. 	(struct rtx_subst_pair): Add insn field. 	(propagate_for_debug_subst): If pair->to contains at least 2 	regs, create a DEBUG_INSN with a debug temp before pair->insn 	and replace from with the debug temp instead of pair->to. 	(propagate_for_debug): Initialize p.insn. 	* combine.c (insn_uid_check): New inline function. 	(INSN_COST, LOG_LINKS): Use it instead of INSN_UID. 	(find_single_use, combine_instructions, 	cant_combine_insn_p, try_combine): Use NONDEBUG_INSN_P instead of 	INSN_P.
    	* g++.dg/opt/pr77844.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@243662 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/opt/pr77844.C
@@ -0,0 +1,32 @@
+// PR debug/77844
+// { dg-do compile }
+// { dg-options "-O3 -g" }
+
+#include <vector>
+
+void
+foo (std::vector<bool>& v, int i, int j)
+{
+  for (int k = 0; k < 5; ++k)
+    v[5 * i + k] = v[5 * i + k] | v[5 * j + k];
+}
+
+void
+bar (std::vector<bool>& v, int i, int j)
+{
+  for (int k = 0; k < 5; ++k)
+    v[5 * i + k] = v[5 * i + k] ^ v[5 * j + k];
+}
+
+void
+baz (std::vector<bool>& v)
+{
+  foo (v, 4, 1);
+  foo (v, 4, 2);
+  foo (v, 4, 3);
+  foo (v, 4, 4);
+  bar (v, 4, 1);
+  bar (v, 4, 2);
+  bar (v, 4, 3);
+  bar (v, 4, 4);
+}
