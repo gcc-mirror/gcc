202d817e6c736f86b95141f3ec0ebc33806f6bbd
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-12-15 08:22:05 +0000

    * gcc.dg/tree-ssa/forwprop-35.c: Use -Wno-psabi everywhere. 	* gcc.dg/torture/pr78515.c: Likewise. 	* gcc.dg/pr69634.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@243686 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/torture/pr78515.c
+++ gcc/testsuite/gcc.dg/torture/pr78515.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
+/* { dg-additional-options "-Wno-psabi" } */
 /* { dg-additional-options "-mavx512bw" { target x86_64-*-* i?86-*-* } } */
-/* { dg-additional-options "-Wno-psabi" { target powerpc-ibm-aix* } } */
 
 typedef unsigned V __attribute__ ((vector_size (64)));
 
