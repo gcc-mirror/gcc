7eca7f0b4450e8f57d02bc35137c071e22bea7af
Richard Earnshaw <rearnsha@arm.com>
2016-12-15 16:03:06 +0000

    [arm] Remove FEATURES field from FPU descriptions
    Now that everything uses the new ISA features, we can remove the
    FEATURES field from the FPU descriptions, along with all the macros
    and definitions associated with it.
    
    	* arm-fpus.def (ARM_FPU): Remove features field from all definitions.
    	* arm.h (arm_fpu_feature_set): Delete typedef.
    	(FPU_FL_NONE): Delete.
    	(FPU_FL_NEON): Delete.
    	(FPU_FL_FP16): Delete.
    	(FPU_FL_CRYPTO): Delete.
    	(FPU_FL_DBL): Delete.
    	(FPU_FL_D32): Delete.
    	(FPU_FL_VFPv2): Delete.
    	(FPU_FL_VFPv3): Delete.
    	(FPU_FL_VFPv4): Delete.
    	(FPU_FL_VFPv5): Delete.
    	(FPU_FL_AMRv8): Delete.
    	(FPU_VFPv2): Delete.
    	(FPU_VFPv3): Delete.
    	(FPU_VFPv4): Delete.
    	(FPU_VFPv5): Delete.
    	(FPU_ARMv8): Delete.
    	(FPU_DBL): Delete.
    	(FPU_D32): Delete.
    	(FPU_NEON): Delete.
    	(FPU_CRYPTO): Delete.
    	(FPU_FP16): Delete.
    	(arm_fpu_desc): Delete features field.
    	* arm.c (all_fpus): Don't initialize feature field.
    
    From-SVN: r243715

--- gcc/config/arm/arm.c
+++ gcc/config/arm/arm.c
@@ -2328,8 +2328,8 @@ char arm_arch_name[] = "__ARM_ARCH_PROFILE__";
 
 const struct arm_fpu_desc all_fpus[] =
 {
-#define ARM_FPU(NAME, ISA, FEATURES)		\
-  { NAME, {ISA isa_nobit}, FEATURES },
+#define ARM_FPU(NAME, ISA)		\
+  { NAME, {ISA isa_nobit} },
 #include "arm-fpus.def"
 #undef ARM_FPU
 };
