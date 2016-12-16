f79279166506406fe9a76128f277059e7e4bdcd7
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-12-16 18:13:09 +0000

    Add assertion to _Rb_tree::erase to check for end iterators
    	* include/bits/stl_tree.h (_Rb_tree::_M_erase_aux(const_iterator)):
    	Add assertion for undefined argument.
    	(_Rb_tree::_M_erase_aux(const_iterator, const_iterator)): Call
    	_M_erase_aux directly instead of through erase.
    	(_Rb_tree::_M_erase_aux(const Key&)): Likewise.
    	* testsuite/23_containers/map/modifiers/erase/end_neg.cc: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@243757 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/bits/stl_tree.h
+++ libstdc++-v3/include/bits/stl_tree.h
@@ -1104,6 +1104,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       iterator
       erase(const_iterator __position)
       {
+	__glibcxx_assert(__position != end());
 	const_iterator __result = __position;
 	++__result;
 	_M_erase_aux(__position);
@@ -1115,6 +1116,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       iterator
       erase(iterator __position)
       {
+	__glibcxx_assert(__position != end());
 	iterator __result = __position;
 	++__result;
 	_M_erase_aux(__position);
@@ -1123,11 +1125,17 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 #else
       void
       erase(iterator __position)
-      { _M_erase_aux(__position); }
+      {
+	__glibcxx_assert(__position != end());
+	_M_erase_aux(__position);
+      }
 
       void
       erase(const_iterator __position)
-      { _M_erase_aux(__position); }
+      {
+	__glibcxx_assert(__position != end());
+	_M_erase_aux(__position);
+      }
 #endif
       size_type
       erase(const key_type& __x);
@@ -2477,7 +2485,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	clear();
       else
 	while (__first != __last)
-	  erase(__first++);
+	  _M_erase_aux(__first++);
     }
 
   template<typename _Key, typename _Val, typename _KeyOfValue,
@@ -2488,7 +2496,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
     {
       pair<iterator, iterator> __p = equal_range(__x);
       const size_type __old_size = size();
-      erase(__p.first, __p.second);
+      _M_erase_aux(__p.first, __p.second);
       return __old_size - size();
     }
 
