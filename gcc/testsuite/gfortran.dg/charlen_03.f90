efa84b0163fae255cc8ca2fe9de3a2c696648bfa
Steven G. Kargl <kargl@gcc.gnu.org>
2016-12-17 23:10:01 +0000

    re PR fortran/78746 (charlen_03, charlen_10 ICE)
    2016-12-17  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	PR fortran/78746
    	* charlen_03.f90: Remove test. 
    	* charlen_10.f90: Ditto.
    
    From-SVN: r243778

--- gcc/testsuite/gfortran.dg/charlen_03.f90
+++
@@ -1,9 +0,0 @@
-! { dg-do compile }
-! PR fortran/65173
-program p
-   type t
-      character(:), allocatable :: x(n) ! { dg-error "must have a deferred shape" }
-   end type
-end
-! { dg-excess-errors "must be of INTEGER type" }
-
--- gcc/testsuite/gfortran.dg/charlen_10.f90
+++
@@ -1,9 +0,0 @@
-! { dg-do compile }
-! PR fortran/65173
-program p
-   type t
-      character(:), allocatable :: x(y)1  ! { dg-error "must have a deferred shape" }
-   end type
-end
-! { dg-excess-errors "must be of INTEGER type" } 
-
