ab955692fe6d9e459c2d27417b56f870069ad28e
Krister Walfridsson <krister.walfridsson@gmail.com>
2016-12-19 09:21:09 +0000

    config.host (*-*-netbsd*): Add t-eh-dw2-dip to tmake_file.
    2016-12-19  Krister Walfridsson  <krister.walfridsson@gmail.com>
    
    	* config.host (*-*-netbsd*): Add t-eh-dw2-dip to tmake_file.
    	* crtstuff.c (BSD_DL_ITERATE_PHDR_AVAILABLE): Define for NetBSD.
    	* unwind-dw2-fde-dip.c (USE_PT_GNU_EH_FRAME, ElfW): Likewise.
    
    From-SVN: r243790

--- libgcc/crtstuff.c
+++ libgcc/crtstuff.c
@@ -81,7 +81,7 @@ call_ ## FUNC (void)					\
 #endif
 
 #if defined(TARGET_DL_ITERATE_PHDR) && \
-   (defined(__DragonFly__) || defined(__FreeBSD__))
+   (defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__))
 #define BSD_DL_ITERATE_PHDR_AVAILABLE
 #endif
  
