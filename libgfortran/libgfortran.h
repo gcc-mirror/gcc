7113fd032de6a99e942a4362a28de0d1c7898b12
jb <jb@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-12-19 15:32:17 +0000

    Remove unused libgfortran functions
    Now that the libgfortran ABI major version has been bumped, we can
    remove functions for which the frontend nowadays generates inline
    code.
    
    This removes the malloc, free, exponent, fraction, nearest, rrspacing,
    spacing, set_exponent and transpose intrinsics. Also the unused
    store_exe_path function is removed.
    
    2016-12-19  Janne Blomqvist  <jb@gcc.gnu.org>
    
    	* Makefile.am: Remove exponent, fraction, nearest, rrspacing,
    	set_exponent, spacing, transpose, malloc, transpose_generic.
    	* Makefile.in: Regenerate.
    	* generated/exponent_r10.c: Remove.
    	* generated/exponent_r16.c: Remove.
    	* generated/exponent_r4.c: Remove.
    	* generated/exponent_r8.c: Remove.
    	* generated/fraction_r10.c: Remove.
    	* generated/fraction_r16.c: Remove.
    	* generated/fraction_r4.c: Remove.
    	* generated/fraction_r8.c: Remove.
    	* generated/nearest_r10.c: Remove.
    	* generated/nearest_r16.c: Remove.
    	* generated/nearest_r4.c: Remove.
    	* generated/nearest_r8.c: Remove.
    	* generated/rrspacing_r10.c: Remove.
    	* generated/rrspacing_r16.c: Remove.
    	* generated/rrspacing_r4.c: Remove.
    	* generated/rrspacing_r8.c: Remove.
    	* generated/set_exponent_r10.c: Remove.
    	* generated/set_exponent_r16.c: Remove.
    	* generated/set_exponent_r4.c: Remove.
    	* generated/set_exponent_r8.c: Remove.
    	* generated/spacing_r10.c: Remove.
    	* generated/spacing_r16.c: Remove.
    	* generated/spacing_r4.c: Remove.
    	* generated/spacing_r8.c: Remove.
    	* generated/transpose_c10.c: Remove.
    	* generated/transpose_c16.c: Remove.
    	* generated/transpose_c4.c: Remove.
    	* generated/transpose_c8.c: Remove.
    	* generated/transpose_i16.c: Remove.
    	* generated/transpose_i4.c: Remove.
    	* generated/transpose_i8.c: Remove.
    	* generated/transpose_r10.c: Remove.
    	* generated/transpose_r16.c: Remove.
    	* generated/transpose_r4.c: Remove.
    	* generated/transpose_r8.c: Remove.
    	* gfortran.map: Remove exponent, fraction, nearest, rrspacing,
    	set_exponent, spacing, transpose, malloc, free, transpose_generic,
    	store_exe_path.
    	* intrinsics/malloc.c: Remove.
    	* intrinsics/transpose_generic.c: Remove.
    	* libgfortran.h (store_exe_path): Remove.
    	* m4/exponent.m4: Remove.
    	* m4/fraction.m4: Remove.
    	* m4/nearest.m4: Remove.
    	* m4/rrspacing.m4: Remove.
    	* m4/set_exponent.m4: Remove.
    	* m4/spacing.m4: Remove.
    	* m4/transpose.m4: Remove.
    	* runtime/main.c (store_exe_path): Remove.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@243799 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgfortran/libgfortran.h
+++ libgfortran/libgfortran.h
@@ -650,9 +650,6 @@ iexport_proto(set_args);
 extern void get_args (int *, char ***);
 internal_proto(get_args);
 
-extern void store_exe_path (const char *);
-export_proto(store_exe_path);
-
 /* backtrace.c */
 
 extern void show_backtrace (bool);
