6b9ddad32b381091724ead4a546c4a8d2d553acc
gjl <gjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-12-21 09:20:12 +0000

    * gcc.target/avr/mmcu: New folder for compile-tests with -mmcu=. 	* gcc.target/avr/mmcu/avr-mmcu.exp: New file. 	* gcc.target/avr/pr58545.c: Move to gcc.target/avr/mmcu. 	* gcc.target/avr/tiny-caller-save.c: Dito. 	* gcc.target/avr/tiny-memx.c: Dito.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@243840 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/avr/mmcu/tiny-memx.c
+++ gcc/testsuite/gcc.target/avr/mmcu/tiny-memx.c
@@ -1,4 +1,4 @@
-/* { dg-do compile { target avr_tiny } } */
+/* { dg-do compile } */
 /* { dg-options "-mmcu=avrtiny" } */
 
 const __memx char ascmonth[] = "Jan"; /* { dg-error "not supported" } */
