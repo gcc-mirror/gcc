71d9113f9b0dd357236dcaadda0ddd9faf1607be
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2016-12-21 09:49:40 +0000

    re PR libfortran/70311 (libgfortran build dies on "implicit declaration of function strncasecmp")
    	PR libfortran/70311
    	* intrinsics/selected_char_kind.c: Include <strings.h> header.
    	* runtime/environ.c: Include <strings.h> header.
    	* runtime/string.c: Include <strings.h> header.
    
    From-SVN: r243843

--- libgfortran/runtime/environ.c
+++ libgfortran/runtime/environ.c
@@ -25,6 +25,7 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 #include "libgfortran.h"
 
 #include <string.h>
+#include <strings.h>
 #include <stdlib.h>
 #include <ctype.h>
 
