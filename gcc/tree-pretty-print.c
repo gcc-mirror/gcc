27d5c203f975b1242371f48397c061b5fc85166c
prathamesh3492 <prathamesh3492@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-12-22 20:30:33 +0000

    2016-12-22  Prathamesh Kulkarni  <prathamesh.kulkarni@linaro.org>
    	* tree-pretty-print.c (dump_generic_node): Change dump format for
    	REALPART_EXPR and IMAGPART_EXPR with TDF_GIMPLE.
    
    testsuite/
    	* gcc.dg/gimplefe-20.c: New test-case.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@243898 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-pretty-print.c
+++ gcc/tree-pretty-print.c
@@ -2451,15 +2451,31 @@ dump_generic_node (pretty_printer *pp, tree node, int spc, int flags,
       break;
 
     case REALPART_EXPR:
-      pp_string (pp, "REALPART_EXPR <");
-      dump_generic_node (pp, TREE_OPERAND (node, 0), spc, flags, false);
-      pp_greater (pp);
+      if (flags & TDF_GIMPLE)
+	{
+	  pp_string (pp, "__real ");
+	  dump_generic_node (pp, TREE_OPERAND (node, 0), spc, flags, false);
+	}
+      else
+	{
+	  pp_string (pp, "REALPART_EXPR <");
+	  dump_generic_node (pp, TREE_OPERAND (node, 0), spc, flags, false);
+	  pp_greater (pp);
+	}
       break;
 
     case IMAGPART_EXPR:
-      pp_string (pp, "IMAGPART_EXPR <");
-      dump_generic_node (pp, TREE_OPERAND (node, 0), spc, flags, false);
-      pp_greater (pp);
+      if (flags & TDF_GIMPLE)
+	{
+	  pp_string (pp, "__imag ");
+	  dump_generic_node (pp, TREE_OPERAND (node, 0), spc, flags, false);
+	}
+      else
+	{
+	  pp_string (pp, "IMAGPART_EXPR <");
+	  dump_generic_node (pp, TREE_OPERAND (node, 0), spc, flags, false);
+	  pp_greater (pp);
+	}
       break;
 
     case VA_ARG_EXPR:
