cca8d0b26549b5f3813178443a3a600aef104181
Toma Tabacu <toma.tabacu@imgtec.com>
2016-12-23 09:52:43 +0000

    MIPS: Cleanup the forcing of assembly output in error tests.
    gcc/testsuite
    
    	* gcc.target/mips/oddspreg-2.c (dg-options): Remove dg-skip-if for
    	-fno-fat-lto-objects and add the -ffat-lto-objects option, along with
    	an explanation for its purpose.
    	* gcc.target/mips/oddspreg-3.c (dg-options): Likewise.
    	* gcc.target/mips/oddspreg-6.c (dg-options): Likewise.
    	* gcc.target/mips/no-dsp-1.c: Add an explanation for the purpose of
    	-ffat-lto-objects.
    	* gcc.target/mips/pr54240.c: Likewise.
    	* gcc.target/mips/r10k-cache-barrier-14.c: Likewise.
    	* gcc.target/mips/soft-float-1.c: Likewise.
    
    From-SVN: r243908

--- gcc/testsuite/gcc.target/mips/oddspreg-3.c
+++ gcc/testsuite/gcc.target/mips/oddspreg-3.c
@@ -1,6 +1,8 @@
 /* Check that we disable odd-numbered single precision registers.  */
-/* { dg-skip-if "needs asm output" { *-*-* } { "-fno-fat-lto-objects" } { "" } } */
-/* { dg-options "-mabi=32 -mfp32 -march=loongson3a -mhard-float" } */
+/* { dg-options "-mabi=32 -mfp32 -march=loongson3a -mhard-float -ffat-lto-objects" } */
+/* This is testing for errors which can only happen in assembly generation.
+   dg-error does not guarantee assembly generation, so we need to do it
+   manually by using -ffat-lto-objects.  */
 
 NOMIPS16 void
 foo ()
