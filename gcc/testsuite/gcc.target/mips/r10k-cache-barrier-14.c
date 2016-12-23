b39416b785c008eb20f740276d188f6c63db2f3d
tomtab <tomtab@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-12-23 09:52:43 +0000

    MIPS: Cleanup the forcing of assembly output in error tests.
    gcc/testsuite
    
    	* gcc.target/mips/oddspreg-2.c (dg-options): Remove dg-skip-if for
    	-fno-fat-lto-objects and add the -ffat-lto-objects option, along with
    	an explanation for its purpose.
    	* gcc.target/mips/oddspreg-3.c (dg-options): Likewise.
    	* gcc.target/mips/oddspreg-6.c (dg-options): Likewise.
    	* gcc.target/mips/no-dsp-1.c: Add an explanation for the purpose of
    	-ffat-lto-objects.
    	* gcc.target/mips/pr54240.c: Likewise.
    	* gcc.target/mips/r10k-cache-barrier-14.c: Likewise.
    	* gcc.target/mips/soft-float-1.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@243908 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/mips/r10k-cache-barrier-14.c
+++ gcc/testsuite/gcc.target/mips/r10k-cache-barrier-14.c
@@ -1,5 +1,8 @@
 /* { dg-options "(-mips16) -mr10k-cache-barrier=store -ffat-lto-objects" } */
 
 /* Test that indirect calls are protected.  */
+/* This is testing for errors which can only happen in assembly generation.
+   dg-error does not guarantee assembly generation, so we need to do it
+   manually by using -ffat-lto-objects.  */
 
 MIPS16 void foo (void) { } /* { dg-message "sorry, unimplemented" } */
