f228d78557c1164346c62e097713c64eec11eee5
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-01-10 14:10:15 +0000

    Do not error when -E provided (PR pch/78970).
    2017-01-10  Martin Liska  <mliska@suse.cz>
    
    	Backport from mainline
    	2017-01-09  Martin Liska  <mliska@suse.cz>
    
    	PR pch/78970
    	* gcc.c (driver_handle_option): Handle OPT_E and set
    	have_E.
    	(lookup_compiler): Do not show error message with have_E.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@244270 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/gcc.c
+++ gcc/gcc.c
@@ -1919,6 +1919,9 @@ static int have_c = 0;
 /* Was the option -o passed.  */
 static int have_o = 0;
 
+/* Was the option -E passed.  */
+static int have_E = 0;
+
 /* Pointer to output file name passed in with -o. */
 static const char *output_file = 0;
 
@@ -4029,6 +4032,10 @@ driver_handle_option (struct gcc_options *opts,
 		    PREFIX_PRIORITY_B_OPT, 0, 0);
       }
       validated = true;
+      break;
+
+    case OPT_E:
+      have_E = true;
       break;
 
     case OPT_x:
@@ -8287,7 +8294,8 @@ lookup_compiler (const char *name, size_t length, const char *language)
 	  {
 	    if (name != NULL && strcmp (name, "-") == 0
 		&& (strcmp (cp->suffix, "@c-header") == 0
-		    || strcmp (cp->suffix, "@c++-header") == 0))
+		    || strcmp (cp->suffix, "@c++-header") == 0)
+		&& !have_E)
 	      fatal_error (input_location,
 			   "cannot use %<-%> as input filename for a "
 			   "precompiled header");
