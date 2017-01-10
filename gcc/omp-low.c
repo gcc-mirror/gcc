818c74cc4f738aec794333a0ddc3bbe7e6b7ccb3
Thomas Schwinge <thomas@codesourcery.com>
2017-01-10 14:00:31 +0100

    [PR tree-optimization/78024] Clear basic block flags before using BB_VISITED for OpenACC loops processing
    	gcc/
    	PR tree-optimization/78024
    	* omp-low.c (oacc_loop_discovery): Call clear_bb_flags.
    
    Backport from trunk r241334:
    
    	gcc/testsuite/
    	2016-10-19  Thomas Schwinge  <thomas@codesourcery.com>
    
    	PR tree-optimization/78024
    	* gcc.dg/goacc/loop-processing-1.c: New file.
    
    From-SVN: r244264

--- gcc/omp-low.c
+++ gcc/omp-low.c
@@ -19225,7 +19225,9 @@ oacc_loop_sibling_nreverse (oacc_loop *loop)
 static oacc_loop *
 oacc_loop_discovery ()
 {
-  basic_block bb;
+  /* Clear basic block flags, in particular BB_VISITED which we're going to use
+     in the following.  */
+  clear_bb_flags ();
   
   oacc_loop *top = new_oacc_loop_outer (current_function_decl);
   oacc_loop_discover_walk (top, ENTRY_BLOCK_PTR_FOR_FN (cfun));
@@ -19234,9 +19236,8 @@ oacc_loop_discovery ()
      that diagnostics come out in an unsurprising order.  */
   top = oacc_loop_sibling_nreverse (top);
 
-  /* Reset the visited flags.  */
-  FOR_ALL_BB_FN (bb, cfun)
-    bb->flags &= ~BB_VISITED;
+  /* Clear basic block flags again.  */
+  clear_bb_flags ();
 
   return top;
 }
