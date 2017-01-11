88df7964b5aa1b8cc2226177da25785c58b7c7e2
Jakub Jelinek <jakub@redhat.com>
2017-01-11 20:48:56 +0100

    flatten-3.c: Add quotation marks around dg-options argument.
    	* gcc.dg/tree-ssa/flatten-3.c: Add quotation marks around dg-options
    	argument.
    
    From-SVN: r244332

--- gcc/testsuite/gcc.dg/tree-ssa/flatten-3.c
+++ gcc/testsuite/gcc.dg/tree-ssa/flatten-3.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options -O2 -fno-ipa-icf } */
+/* { dg-options "-O2 -fno-ipa-icf" } */
 
 extern void do_something_usefull();
 /* Check that we finish compiling even if instructed to
