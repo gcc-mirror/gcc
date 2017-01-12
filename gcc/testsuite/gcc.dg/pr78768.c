a091a5b306d6d2ad6a1c094c8761cb599c4f6c1f
dje <dje@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-01-12 18:27:04 +0000

    * gcc.dg/pr78768.c: Require LTO.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@244379 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr78768.c
+++ gcc/testsuite/gcc.dg/pr78768.c
@@ -1,6 +1,7 @@
 /* PR c/78768 - -Walloca-larger-than and -Wformat-length warnings disabled
    by -flto
   { dg-do run }
+  { dg-require-effective-target lto }
   { dg-options "-O2 -Walloca-larger-than=10 -Wformat -Wformat-length -flto" } */
 
 int main (void)
