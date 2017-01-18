1e6c19441c3d9c4a373fc782d0a672ba540e28f6
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-01-18 17:39:56 +0000

    PR target/77416 	* gcc.target/powerpc/pr77416.c Guard the test only for ilp32 effective 	target.  Use powerpc* instead of powerpc64* in targets.  Remove -m32 	from dg-options. 	* gcc.target/powerpc/pr64205.c: Remove -m32 from dg-options of ilp32 	guarded test. 	* gcc.target/powerpc/fusion4.c: Likewise. 	* gcc.target/powerpc/pr63491.c: Remove -m64 from dg-options of lp64 	guarded test. 	* gcc.target/powerpc/pr58673-1.c: Likewise. 	* gcc.target/powerpc/pr58673-2.c: Likewise. 	* gcc.target/powerpc/pr59054.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@244585 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/fusion4.c
+++ gcc/testsuite/gcc.target/powerpc/fusion4.c
@@ -2,7 +2,7 @@
 /* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
 /* { dg-require-effective-target powerpc_p9vector_ok } */
 /* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power7" } } */
-/* { dg-options "-mcpu=power7 -mtune=power9 -O3 -msoft-float -m32" } */
+/* { dg-options "-mcpu=power7 -mtune=power9 -O3 -msoft-float" } */
 
 #define LARGE 0x12345
 
