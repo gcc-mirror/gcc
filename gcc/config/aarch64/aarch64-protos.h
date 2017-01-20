a0fe036c3f78cd5ccd286943565b7e86bf097dbd
Wilco Dijkstra <wdijkstr@arm.com>
2017-01-20 15:34:41 +0000

    This patch simplifies the handling of EH return.
    This patch simplifies the handling of EH return.  We force the use of the
    frame pointer so the return location is always at FP + 8.  This means we
    can emit a simple volatile access in EH_RETURN_HANDLER_RTX without needing md
    patterns, splitters and frame offset calculations.  The new implementation also
    fixes various bugs in aarch64_final_eh_return_addr, which does not work with
    -fomit-frame-pointer, alloca or outgoing arguments.
    
    	Backport from mainline
        gcc/
    	PR target/77455
    	* config/aarch64/aarch64.md (eh_return): Remove pattern and splitter.
    	* config/aarch64/aarch64.h (AARCH64_EH_STACKADJ_REGNUM): Remove.
    	(EH_RETURN_HANDLER_RTX): New define.
    	* config/aarch64/aarch64.c (aarch64_frame_pointer_required):
    	Force frame pointer in EH return functions.
    	(aarch64_expand_epilogue): Add barrier for eh_return.
    	(aarch64_final_eh_return_addr): Remove.
    	(aarch64_eh_return_handler_rtx): New function.
    	* config/aarch64/aarch64-protos.h (aarch64_final_eh_return_addr):
    	Remove.
    	(aarch64_eh_return_handler_rtx): New prototype.
    
        testsuite/
    	PR target/77455
    	* gcc.target/aarch64/eh_return.c: New test.
    
    From-SVN: r244724

--- gcc/config/aarch64/aarch64-protos.h
+++ gcc/config/aarch64/aarch64-protos.h
@@ -338,7 +338,7 @@ int aarch64_hard_regno_nregs (unsigned, machine_mode);
 int aarch64_simd_attr_length_move (rtx_insn *);
 int aarch64_uxt_size (int, HOST_WIDE_INT);
 int aarch64_vec_fpconst_pow_of_2 (rtx);
-rtx aarch64_final_eh_return_addr (void);
+rtx aarch64_eh_return_handler_rtx (void);
 rtx aarch64_legitimize_reload_address (rtx *, machine_mode, int, int, int);
 rtx aarch64_mask_from_zextract_ops (rtx, rtx);
 const char *aarch64_output_move_struct (rtx *operands);
