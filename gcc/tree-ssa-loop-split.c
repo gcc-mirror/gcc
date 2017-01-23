d886761fd048a6ad77dc7c927cbedfa503cfe01b
Michael Matz <matz@suse.de>
2017-01-23 13:57:31 +0000

    fix pr78384
    	PR tree-optimization/78384
    	* tree-ssa-loop-split.c (patch_loop_exit): Use correct edge.
    
    testsuite/
    	PR tree-optimization/78384
    	* gcc.dg/pr78384.c: New test.
    
    From-SVN: r244811

--- gcc/tree-ssa-loop-split.c
+++ gcc/tree-ssa-loop-split.c
@@ -159,7 +159,7 @@ patch_loop_exit (struct loop *loop, gcond *guard, tree nextval, tree newbound,
 			     nextval, newbound);
   update_stmt (stmt);
 
-  edge stay = single_pred_edge (loop->latch);
+  edge stay = EDGE_SUCC (exit->src, EDGE_SUCC (exit->src, 0) == exit);
 
   exit->flags &= ~(EDGE_TRUE_VALUE | EDGE_FALSE_VALUE);
   stay->flags &= ~(EDGE_TRUE_VALUE | EDGE_FALSE_VALUE);
