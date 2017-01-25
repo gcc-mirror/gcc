8a3ae1e655a327903db21aecef63b202e1c1e0b5
carll <carll@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-01-25 16:23:48 +0000

    gcc/ChangeLog:
    2017-01-25  Carl Love  <cel@us.ibm.com>
    
            * config/rs6000/rs6000-c (altivec_overloaded_builtins): Fix order
            of entries for ALTIVEC_BUILTIN_VEC_PACKS and P8V_BUILTIN_VEC_VGBBD.
    
    gcc/testsuite/ChangeLog:
    
    2017-01-25  Carl Love  <cel@us.ibm.com>
            * gcc.target/powerpc/builtins-3-p8.c:  Add missing tests for the
            vec_packs built-ins
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@244904 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/builtins-3-p8.c
+++ gcc/testsuite/gcc.target/powerpc/builtins-3-p8.c
@@ -22,14 +22,32 @@ test_nabs_long_long (vector long long x)
   return vec_nabs (x);
 }
 
+vector signed int
+test_vsi_packs_vsll_vsll (vector signed long long x,
+                          vector signed long long y)
+{
+  return vec_packs (x, y);
+}
+
+vector unsigned int
+test_vui_packs_vull_vull (vector unsigned long long x,
+                          vector unsigned long long y)
+{
+  return vec_packs (x, y);
+}
+
 /* Expected test results:
 
      test_eq_long_long          1 vcmpequd inst
      test_pack_float            1 vpkudum inst
-     test_nabs_long_long        1 vspltisw, 1 vsubudm, 1 vminsd */
+     test_nabs_long_long        1 vspltisw, 1 vsubudm, 1 vminsd
+     test_vsi_packs_vsll_vsll   1 vpksdss
+     test_vui_packs_vull_vull   1 vpkudus */
 
 /* { dg-final { scan-assembler-times "vcmpequd" 1 } } */
 /* { dg-final { scan-assembler-times "vpkudum"  1 } } */
 /* { dg-final { scan-assembler-times "vspltisw" 1 } } */
 /* { dg-final { scan-assembler-times "vsubudm"  1 } } */
 /* { dg-final { scan-assembler-times "vminsd"   1 } } */
+/* { dg-final { scan-assembler-times "vpksdss"  1 } } */
+/* { dg-final { scan-assembler-times "vpkudus"  1 } } */  
