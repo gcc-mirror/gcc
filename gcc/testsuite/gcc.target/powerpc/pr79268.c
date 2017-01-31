71f7a06790382422ee2b6bd242f9dec047ef8e0f
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2017-01-31 22:57:55 +0000

    backport: re PR target/79268 (Wrong code generation for vec_xl and vec_xst intrinsics)
    [gcc]
    
    2017-01-31  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2017-01-29  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	PR target/79268
    	* config/rs6000/altivec.h (vec_xl): Revise #define.
    	(vec_xst): Likewise.
    
    [gcc/testsuite]
    
    2017-01-31  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2017-01-29  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	PR target/79268
    	* gcc.target/powerpc/pr79268.c: New file.
    	* gcc.target/powerpc/vsx-elemrev-1.c: Delete file.
    	* gcc.target/powerpc/vsx-elemrev-2.c: Likewise.
    	* gcc.target/powerpc/vsx-elemrev-3.c: Likewise.
    	* gcc.target/powerpc/vsx-elemrev-4.c: Likewise.
    
    From-SVN: r245075

---
+++ gcc/testsuite/gcc.target/powerpc/pr79268.c
@@ -0,0 +1,18 @@
+/* { dg-do compile { target { powerpc64le-*-* } } } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power8" } } */
+/* { dg-options "-mcpu=power8 -O3 " } */
+
+/* Verify that vec_xl and vec_xst accept vector pixel parameters.  */
+
+/* Test case to resolve PR79268.  */
+
+#include <altivec.h>
+
+vector pixel a;
+
+vector pixel
+pr79268 (vector pixel *x)
+{
+  vec_xst (a, 0, x);
+  return vec_xl (0, x);
+}
