9b2d7a5b0b8b85ef2033f8b4d47488cf6f348983
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-02-01 12:12:34 +0000

    PR libstdc++/79195 fix make_array type deduction
    	PR libstdc++/79195
    	* include/experimental/array (make_array): Use common_type<_Dest>
    	and delay instantiation of common_type until after conditional_t.
    	Qualify std::forward call.
    	(to_array): Add exception specification.
    	* testsuite/experimental/array/make_array.cc: Test argument types
    	without a common type.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@245087 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/experimental/array/make_array.cc
+++ libstdc++-v3/testsuite/experimental/array/make_array.cc
@@ -1,7 +1,6 @@
-// { dg-options "-std=gnu++14" }
-// { dg-do compile }
+// { dg-do compile { target c++14 } }
 
-// Copyright (C) 2015-2016 Free Software Foundation, Inc.
+// Copyright (C) 2015-2017 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -19,6 +18,7 @@
 // <http://www.gnu.org/licenses/>.
 
 #include <experimental/array>
+#include <functional> // for std::ref and std::reference_wrapper
 
 struct MoveOnly
 {
@@ -27,7 +27,7 @@ struct MoveOnly
   MoveOnly& operator=(MoveOnly&&) = default;
 };
 
-int main()
+void test01()
 {
   char x[42];
   std::array<char, 42> y = std::experimental::to_array(x);
@@ -44,4 +44,14 @@ int main()
   constexpr std::array<long, 3> zz2
     = std::experimental::make_array(1,2L, 3);
   constexpr std::array<MoveOnly, 1> zzz2 = std::experimental::make_array(MoveOnly{});
+}
+
+void test02()
+{
+  // PR libstdc++/79195
+  struct A {};
+  struct B : A {};
+  struct C : A {};
+  auto arr = std::experimental::make_array<A>(B{}, C{});
+  static_assert(std::is_same<decltype(arr), std::array<A, 2>>::value, "");
 }
