55e75c7c6bcfe386d0ecbf4611cff81040af00b3
Jakub Jelinek <jakub@redhat.com>
2017-02-01 16:47:52 +0100

    re PR testsuite/79324 (The tests introduced at revision r245052 fail on darwin)
    	PR testsuite/79324
    	* gcc.dg/debug/dwarf2/align-1.c: Add -gno-strict-dwarf to dg-options.
    	* gcc.dg/debug/dwarf2/align-2.c: Likewise.
    	* gcc.dg/debug/dwarf2/align-3.c: Likewise.
    	* gcc.dg/debug/dwarf2/align-4.c: Likewise.
    	* gcc.dg/debug/dwarf2/align-5.c: Likewise.
    	* gcc.dg/debug/dwarf2/align-6.c: Likewise.
    	* gcc.dg/debug/dwarf2/align-as-1.c: Likewise.
    	* g++.dg/debug/dwarf2/align-1.C: Likewise.
    	* g++.dg/debug/dwarf2/align-2.C: Likewise.
    	* g++.dg/debug/dwarf2/align-3.C: Likewise.
    	* g++.dg/debug/dwarf2/align-4.C: Likewise.
    	* g++.dg/debug/dwarf2/align-5.C: Likewise.
    	* g++.dg/debug/dwarf2/align-6.C: Likewise.
    
    From-SVN: r245097

--- gcc/testsuite/g++.dg/debug/dwarf2/align-4.C
+++ gcc/testsuite/g++.dg/debug/dwarf2/align-4.C
@@ -1,5 +1,5 @@
 // { dg-do compile }
-// { dg-options "-O -g -dA" }
+// { dg-options "-O -g -dA -gno-strict-dwarf" }
 // { dg-final { scan-assembler-times " DW_AT_alignment" 2 { xfail { powerpc-ibm-aix* } } } }
 
 struct tt {
