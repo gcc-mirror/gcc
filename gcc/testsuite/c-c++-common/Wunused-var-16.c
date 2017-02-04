67df89e878814606099f09206d53707696ac8961
danglin <danglin@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-02-04 00:54:06 +0000

    * c-c++-common/Wunused-var-15.c: Add -fno-common additional option on 	hppa*-*-hpux*. 	* c-c++-common/Wunused-var-16.c: Likewise. 	* c-c++-common/builtin-shuffle-1.c: Likewise. 	* gcc.dg/debug/dwarf2/align-[1-6].c Likewise. 	* gcc.dg/debug/dwarf2/align-as-1.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@245179 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/c-c++-common/Wunused-var-16.c
+++ gcc/testsuite/c-c++-common/Wunused-var-16.c
@@ -1,6 +1,7 @@
 /* PR c++/78949 */
 /* { dg-do compile } */
 /* { dg-options "-Wunused" } */
+/* { dg-additional-options "-fno-common" { target hppa*-*-hpux* } } */
 
 typedef unsigned char V __attribute__((vector_size(16)));
 V v;
--- gcc/testsuite/gcc.dg/debug/dwarf2/align-4.c
+++ gcc/testsuite/gcc.dg/debug/dwarf2/align-4.c
@@ -1,5 +1,6 @@
 // { dg-do compile }
 // { dg-options "-O -g -dA -gno-strict-dwarf" }
+// { dg-additional-options "-fno-common" { target hppa*-*-hpux* } }
 // { dg-final { scan-assembler-times " DW_AT_alignment" 2 { xfail { powerpc-ibm-aix* } } } }
 
 struct tt {
