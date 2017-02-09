5d9da9f9a2d9fec32fba2b7b9f2326d1f1c4f1f0
bebbo <bebbo@bejy.net>
2017-02-09 22:05:42 +0100

    @R added a hack to support multiline __asm("...") statements

--- gcc/c/c-parser.c
+++ gcc/c/c-parser.c
@@ -3908,6 +3908,7 @@ c_parser_asm_string_literal (c_parser *parser)
 static tree
 c_parser_simple_asm_expr (c_parser *parser)
 {
+  extern int in_assembler_directive;
   tree str;
   gcc_assert (c_parser_next_token_is_keyword (parser, RID_ASM));
   /* ??? Follow the C++ parser rather than using the
@@ -3919,7 +3920,13 @@ c_parser_simple_asm_expr (c_parser *parser)
       parser->lex_untranslated_string = false;
       return NULL_TREE;
     }
+
+  // SBF: set in_assembler_directive to enable multi-line strings. And yes, it's a HACK.
+  in_assembler_directive = 1;
   str = c_parser_asm_string_literal (parser);
+  // SBF: in_assembler_directive disabled
+  in_assembler_directive = 0;
+
   parser->lex_untranslated_string = false;
   if (!c_parser_require (parser, CPP_CLOSE_PAREN, "expected %<)%>"))
     {
