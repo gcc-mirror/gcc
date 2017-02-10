7003c35ed08fd0a0cf9872482dc01ac8719fcf6b
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-02-10 20:08:39 +0000

    PR c++/78897 - constexpr union
    	* constexpr.c (cxx_eval_store_expression): A store to a union member
    	erases a previous store to another member.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@245342 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/constexpr.c
+++ gcc/cp/constexpr.c
@@ -3239,6 +3239,11 @@ cxx_eval_store_expression (const constexpr_ctx *ctx, tree t,
 	  tree fields = TYPE_FIELDS (DECL_CONTEXT (index));
 	  unsigned HOST_WIDE_INT idx;
 
+	  if (code == UNION_TYPE && CONSTRUCTOR_NELTS (*valp)
+	      && CONSTRUCTOR_ELT (*valp, 0)->index != index)
+	    /* Changing active member.  */
+	    vec_safe_truncate (CONSTRUCTOR_ELTS (*valp), 0);
+
 	  for (idx = 0;
 	       vec_safe_iterate (CONSTRUCTOR_ELTS (*valp), idx, &cep);
 	       idx++, fields = DECL_CHAIN (fields))
