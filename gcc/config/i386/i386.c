c8431fc9b528c3d2adfddeb4961e1477b9a3d4a9
Amit Pawar <amit.pawar@amd.com>
2017-02-14 09:27:06 +0000

    Fix alignemnt and max skip bytes for znver1 arch.
    2017-02-14  Amit Pawar  <amit.pawar@amd.com>
    
    	* config/i386/i386.c (znver1_cost): Fix the alignment for function and
    	max skip bytes for function, loop and jump.
    
    From-SVN: r245423

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -2672,7 +2672,7 @@ static const struct ptt processor_target_table[PROCESSOR_max] =
   {"bdver4", &bdver4_cost, 16, 10, 16, 7, 11},
   {"btver1", &btver1_cost, 16, 10, 16, 7, 11},
   {"btver2", &btver2_cost, 16, 10, 16, 7, 11},
-  {"znver1", &znver1_cost, 16, 10, 16, 7, 11}
+  {"znver1", &znver1_cost, 16, 15, 16, 15, 16}
 };
 
 static unsigned int
