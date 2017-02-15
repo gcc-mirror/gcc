3f55c60f5e81df56b9cf8ce7500d4c21c67b8c6d
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-02-15 13:38:32 +0000

    Add GDB XMethods for shared_ptr and unique_ptr<T[]>
    Backport from mainline
    2016-12-15  Jonathan Wakely  <jwakely@redhat.com>
    
    	* python/libstdcxx/v6/xmethods.py (UniquePtrGetWorker.__init__): Use
    	correct element type for unique_ptr<T[]>.
    	(UniquePtrGetWorker._supports, UniquePtrDerefWorker._supports): New
    	functions to disable unsupported operators for unique_ptr<T[]>.
    	(UniquePtrSubscriptWorker): New worker for operator[].
    	(UniquePtrMethodsMatcher.__init__): Register UniquePtrSubscriptWorker.
    	(UniquePtrMethodsMatcher.match): Call _supports on the chosen worker.
    	(SharedPtrGetWorker, SharedPtrDerefWorker, SharedPtrSubscriptWorker)
    	(SharedPtrUseCountWorker, SharedPtrUniqueWorker): New workers.
    	(SharedPtrMethodsMatcher): New matcher for shared_ptr.
    	(register_libstdcxx_xmethods): Register SharedPtrMethodsMatcher.
    	* testsuite/libstdc++-xmethods/unique_ptr.cc: Test arrays.
    	* testsuite/libstdc++-xmethods/shared_ptr.cc: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@245478 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/libstdc++-xmethods/shared_ptr.cc
+++ libstdc++-v3/testsuite/libstdc++-xmethods/shared_ptr.cc
@@ -1,7 +1,7 @@
-// { dg-do run }
-// { dg-options "-std=gnu++11 -g -O0" }
+// { dg-do run { target c++11 } }
+// { dg-options "-g -O0" }
 
-// Copyright (C) 2014-2016 Free Software Foundation, Inc.
+// Copyright (C) 2016 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -28,11 +28,9 @@ struct x_struct
 int
 main ()
 {
-  std::unique_ptr<int> p(new int(10));
+  std::shared_ptr<int> p(new int(10));
 
-  std::unique_ptr<x_struct> q(new x_struct{23});
-
-  std::unique_ptr<x_struct[]> r(new x_struct[2]{ {46}, {69} });
+  std::shared_ptr<x_struct> q(new x_struct{23});
 
 // { dg-final { note-test *p 10 } }
 // { dg-final { regexp-test p.get() 0x.* } }
@@ -47,15 +45,6 @@ main ()
 // { dg-final { whatis-test *q x_struct } }
 // { dg-final { whatis-test q.get() "x_struct \*" } }
 // { dg-final { whatis-test q->y int } }
-
-// { dg-final { note-test r\[1] {\{y = 69\}} } }
-// { dg-final { regexp-test r.get() 0x.* } }
-// { dg-final { note-test r\[1].y 69 } }
-
-// { dg-final { whatis-test r\[1] x_struct } }
-// { dg-final { whatis-test r.get() "x_struct \*" } }
-// { dg-final { whatis-test r\[1].y int } }
-
 
   return 0;  // Mark SPOT
 }
