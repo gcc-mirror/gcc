2f78ea2e3f0a990cfcd72c9cf99b45a7a9cfc47d
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-02-20 10:52:50 +0000

    2017-02-16  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/79382
    	* decl.c (access_attr_decl): Test for presence of generic DTIO
    	interface and emit error if not present.
    
    2017-02-16  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/79382
    	* io/transfer.c (check_dtio_proc): New function.
    	(formatted_transfer_scalar_read): Use it.
    	(formatted_transfer_scalar_write): ditto.
    
    2017-02-16  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/79382
    	* gfortran.dg/dtio_10.f90 : Change test of error message.
    	* gfortran.dg/dtio_23.f90 : New test.
    	* gfortran.dg/dtio_24.f90 : New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@245596 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/dtio_24.f90
@@ -0,0 +1,51 @@
+! { dg-do run }
+!
+! Test fix for the additional bug that was found in fixing PR79832.
+!
+! Contributed by Walt Brainerd  <walt.brainerd@gmail.com>
+!
+module dollar_mod
+
+   implicit none
+   private
+
+   type, public :: dollar_type
+      real :: amount
+   end type dollar_type
+
+   interface write(formatted)
+      module procedure Write_dollar
+   end interface
+
+   private :: write (formatted)
+
+contains
+
+subroutine Write_dollar &
+
+   (dollar_value, unit, b_edit_descriptor, v_list, iostat, iomsg)
+
+   class (dollar_type), intent(in) :: dollar_value
+   integer, intent(in) :: unit
+   character (len=*), intent(in) :: b_edit_descriptor
+   integer, dimension(:), intent(in) :: v_list
+   integer, intent(out) :: iostat
+   character (len=*), intent(inout) :: iomsg
+   write (unit=unit, fmt="(f9.2)", iostat=iostat) dollar_value%amount
+
+end subroutine Write_dollar
+
+end module dollar_mod
+
+program test_dollar
+
+   use :: dollar_mod
+   implicit none
+   integer  :: ios
+   character(100) :: errormsg
+
+   type (dollar_type), parameter :: wage = dollar_type(15.10)
+   write (unit=*, fmt="(DT)", iostat=ios, iomsg=errormsg) wage
+   if (ios.ne.5006) call abort
+   if (errormsg(1:22).ne."Missing DTIO procedure") call abort
+end program test_dollar
