8f712b76904c9e495d16817fa93f8edde4c1f0cd
Jason Merrill <jason@redhat.com>
2017-02-20 01:06:39 -0500

    PR c++/78282 - auto template and pack expansion
    	* pt.c (find_parameter_packs_r): Don't walk into the type of
    	templates other than template template-parameters.
    
    From-SVN: r245594

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -3576,8 +3576,12 @@ find_parameter_packs_r (tree *tp, int *walk_subtrees, void* data)
       *walk_subtrees = 0;
       return NULL_TREE;
 
-    case CONSTRUCTOR:
     case TEMPLATE_DECL:
+      if (!DECL_TEMPLATE_TEMPLATE_PARM_P (t))
+	return NULL_TREE;
+      gcc_fallthrough();
+
+    case CONSTRUCTOR:
       cp_walk_tree (&TREE_TYPE (t),
 		    &find_parameter_packs_r, ppd, ppd->visited);
       return NULL_TREE;
