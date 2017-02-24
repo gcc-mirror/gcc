4227c9adf01d5ada5eb7c868aa104167c2a01983
Jakub Jelinek <jakub@redhat.com>
2017-02-24 21:41:54 +0100

    re PR c++/79588 (ICE in warn_for_restrict with -Wrestrict)
    	PR c++/79588
    c-family/
    	* c-common.c (check_function_restrict): New function.
    	(check_function_arguments): Add FNDECL argument.  Call
    	check_function_restrict if -Wrestrict.
    	* c-warn.c (warn_for_restrict): Remove ARGS argument, add ARGARRAY
    	and NARGS.  Use auto_vec for ARG_POSITIONS, simplify.
    	* c-common.h (check_function_arguments): Add FNDECL argument.
    	(warn_for_restrict): Remove ARGS argument, add ARGARRAY and NARGS.
    c/
    	* c-parser.c (c_parser_postfix_expression_after_primary): Don't
    	handle -Wrestrict here.
    	* c-typeck.c (build_function_call_vec): Adjust
    	check_function_arguments caller.
    cp/
    	* call.c (build_over_call): Call check_function_arguments even for
    	-Wrestrict, adjust check_function_arguments caller.
    	* parser.c (cp_parser_postfix_expression): Don't handle -Wrestrict
    	here.
    	* typeck.c (cp_build_function_call_vec): Adjust
    	check_function_arguments caller.
    testsuite/
    	* g++.dg/warn/Wrestrict-1.C: New test.
    	* g++.dg/warn/Wrestrict-2.C: New test.
    
    From-SVN: r245719

---
+++ gcc/testsuite/g++.dg/warn/Wrestrict-1.C
@@ -0,0 +1,12 @@
+// PR c++/79588
+// { dg-do compile }
+// { dg-options "-Wrestrict" }
+
+void foo (char *__restrict, char *__restrict = __null);
+
+void
+bar (char *p)
+{
+  foo (p, p);	// { dg-warning "to restrict-qualified parameter aliases with" }
+  foo (p);
+}
