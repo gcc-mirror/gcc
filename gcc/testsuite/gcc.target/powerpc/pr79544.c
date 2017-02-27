c8f28a3c7ae87e3114e5789fd3a20e46c3a879f6
Pat Haugen <pthaugen@us.ibm.com>
2017-02-27 16:06:13 +0000

    re PR target/79544 (vec_sra (unsigned long long,foo) generating vsrd instead of vsrad)
    	PR target/79544
    	* config/rs6000/rs6000-c.c (struct altivec_builtin_types): Use VSRAD
    	for arithmetic shift of unsigned V2DI.
    	* gcc.target/powerpc/pr79544.c: New.
    
    From-SVN: r245762

---
+++ gcc/testsuite/gcc.target/powerpc/pr79544.c
@@ -0,0 +1,15 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-require-effective-target powerpc_p8vector_ok } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power8" } } */
+/* { dg-options "-mcpu=power8 -O2" } */
+
+#include <altivec.h>
+
+vector unsigned long long
+test_sra (vector unsigned long long x, vector unsigned long long y)
+{
+  return vec_sra (x, y);
+}
+
+/* { dg-final { scan-assembler "vsrad" } } */
+
