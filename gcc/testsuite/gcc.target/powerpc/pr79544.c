d390eb415a0c37bbf9412a78a10f6ee8db3db146
pthaugen <pthaugen@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-03-01 18:09:51 +0000

    * gcc.target/powerpc/pr79544.c: Add test for vec_vsrad and fix up 	scan string.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@245812 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/pr79544.c
+++ gcc/testsuite/gcc.target/powerpc/pr79544.c
@@ -11,5 +11,11 @@ test_sra (vector unsigned long long x, vector unsigned long long y)
   return vec_sra (x, y);
 }
 
-/* { dg-final { scan-assembler "vsrad" } } */
+vector unsigned long long
+test_vsrad (vector unsigned long long x, vector unsigned long long y)
+{
+  return vec_vsrad (x, y);
+}
+
+/* { dg-final { scan-assembler-times {\mvsrad\M} 2 } } */
 
