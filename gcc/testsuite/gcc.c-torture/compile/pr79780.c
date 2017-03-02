fe792dd82ff149208e9c7df2fab241aeda78afe3
Jakub Jelinek <jakub@redhat.com>
2017-03-02 17:02:37 +0100

    re PR rtl-optimization/79780 (ICE in rtl_verify_bb_insns, at cfgrtl.c:2661 (error: flow control insn inside a basic block))
    	PR rtl-optimization/79780
    	* cprop.c (one_cprop_pass): When second and further conditional trap
    	in a single basic block is turned into an unconditional trap, turn it
    	into a deleted note to avoid RTL verification failures.
    
    	* gcc.c-torture/compile/pr79780.c: New test.
    
    From-SVN: r245843

---
+++ gcc/testsuite/gcc.c-torture/compile/pr79780.c
@@ -0,0 +1,48 @@
+/* PR rtl-optimization/79780 */
+
+int t3, iy, f4, oi, gn;
+
+void
+foo (long long int mh)
+{
+  int pi = 0;
+
+  if (iy != 0)
+    for (;;)
+      f4 = 0;
+
+  if (t3 != 0)
+    {
+      while (mh != 0LL)
+	{
+	  while (mh < 1LL)
+	    ++mh;
+	  ++mh;
+	}
+      for (;;)
+	;
+      for (oi = 0; oi < 1; ++oi)
+	{
+	n3:;
+	}
+      gn = iy = 1;
+    }
+
+  f4 = 0;
+
+  if (pi - (mh != 0LL) == 0)
+    if (mh != 1LL)
+      {
+	oi = t3 = 0;
+	if (mh == 0LL)
+	  ++pi;
+      }
+
+  if (iy != 0 && pi != 0)
+    {
+      t3 = 0;
+      goto n3;
+    }
+
+  t3 /= 0;
+}
