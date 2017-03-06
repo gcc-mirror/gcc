0b45b93aaf8cbd77d75492739ca95effc6733e81
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-03-06 11:54:37 +0000

    2017-03-06  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/64574
    	* g++.dg/template/crash125.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@245916 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/template/crash125.C
@@ -0,0 +1,18 @@
+// PR c++/64574
+
+template<class T>
+class TraitCheckImpl;
+
+template<class T, class>
+class Swappable;
+template<class T, class=typename TraitCheckImpl<Swappable<T, void> >::Complete>
+class Swappable;
+
+template<class T>
+struct TraitCheckImpl<Swappable<T> > {
+    typedef void Complete;
+};
+
+Swappable<int> s;  // { dg-error "depth" }
+
+// { dg-prune-output "compilation terminated" }
