a8d25fd0dd7e0feffc395ff1db1f2e9d815fb78a
Richard Biener <rguenther@suse.de>
2017-03-06 13:58:57 +0000

    re PR tree-optimization/79887 (ICE in set_uid_loop_bbs, at tree-vectorizer.c:482)
    2017-03-06  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/79894
    	* tree-vectorizer.c (vectorize_loops): Set loop_vectorized_call
    	to NULL after folding it.
    
    	* gcc.dg/vect/pr79887.c: New testcase.
    
    From-SVN: r245923

--- gcc/tree-vectorizer.c
+++ gcc/tree-vectorizer.c
@@ -651,6 +651,7 @@ vectorize_loops (void)
 				     "basic block vectorized\n");
 		    fold_loop_vectorized_call (loop_vectorized_call,
 					       boolean_true_node);
+		    loop_vectorized_call = NULL;
 		    ret |= TODO_cleanup_cfg;
 		  }
 	      }
@@ -703,6 +704,7 @@ vectorize_loops (void)
 	if (loop_vectorized_call)
 	  {
 	    fold_loop_vectorized_call (loop_vectorized_call, boolean_true_node);
+	    loop_vectorized_call = NULL;
 	    ret |= TODO_cleanup_cfg;
 	  }
 
