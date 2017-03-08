f1a675e486155ebeb0abc90b73aa75d7079d9b58
Andrew Haley <aph@redhat.com>
2017-03-08 11:35:23 +0000

    re PR tree-optimization/79943 (Loop splitting breaks with loops of pointer type)
    2017-03-08  Andrew Haley  <aph@redhat.com>
    
    	PR tree-optimization/79943
    	* tree-ssa-loop-split.c (compute_new_first_bound): When
    	calculating the new upper bound, (END-BEG) should be added, not
    	subtracted.
    
    From-SVN: r245974

--- gcc/tree-ssa-loop-split.c
+++ gcc/tree-ssa-loop-split.c
@@ -436,7 +436,6 @@ compute_new_first_bound (gimple_seq *stmts, struct tree_niter_desc *niter,
   if (POINTER_TYPE_P (TREE_TYPE (guard_init)))
     {
       enddiff = gimple_convert (stmts, sizetype, enddiff);
-      enddiff = gimple_build (stmts, NEGATE_EXPR, sizetype, enddiff);
       newbound = gimple_build (stmts, POINTER_PLUS_EXPR,
 			       TREE_TYPE (guard_init),
 			       guard_init, enddiff);
