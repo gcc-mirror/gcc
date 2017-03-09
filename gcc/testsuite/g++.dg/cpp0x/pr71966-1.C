35f77c2855c08fa329638e05d29b9b0d18caa5c1
Paolo Carlini <paolo.carlini@oracle.com>
2017-03-09 11:51:29 +0000

    re PR c++/71966 (ICE on invalid C++11 code (undefined constructor used in a constant expression): in cp_build_addr_expr_1, at cp/typeck.c:5671)
    2017-03-09  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/71966
    	* g++.dg/cpp0x/pr71966-1.C: New.
    	* g++.dg/cpp0x/pr71966-2.C: Likewise.
    
    From-SVN: r246000

---
+++ gcc/testsuite/g++.dg/cpp0x/pr71966-1.C
@@ -0,0 +1,13 @@
+// PR c++/71966
+// { dg-do compile { target c++11 } }
+
+struct A
+{ 
+  constexpr A (int);  // { dg-message "never defined" }
+  constexpr operator int () const { return 0; }
+};
+
+template < int > struct B {};
+
+constexpr A a = 0;  // { dg-error "before its definition" }
+B < a > b;
