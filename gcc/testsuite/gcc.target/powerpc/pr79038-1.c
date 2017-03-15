76400239e5d5f9d14cbfe62e88d079adb8dc1156
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-03-15 21:17:35 +0000

    [gcc] 2017-03-15  Michael Meissner  <meissner@linux.vnet.ibm.com>
    	PR target/79038
    	* config/rs6000/rs6000.md (float<QHI:mode><IEEE128:mode>2): Define
    	insns to convert from signed/unsigned char/short to IEEE 128-bit
    	floating point.
    	(floatuns<QHI:mode><IEEE128:mode>2): Likewise.
    
    [gcc/testsuite]
    2017-03-15  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	PR target/79038
    	* gcc.target/powerpc/pr79038-1.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@246178 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/pr79038-1.c
@@ -0,0 +1,39 @@
+/* { dg-do compile { target { powerpc*-*-* && lp64 } } } */
+/* { dg-require-effective-target powerpc_p9vector_ok } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power9" } } */
+/* { dg-options "-mcpu=power9 -O2 -mfloat128" } */
+
+#ifndef TYPE
+#define TYPE _Float128
+#endif
+
+TYPE
+return_convert_uchar_to_float128_mem (unsigned char *p_uc)
+{
+  return (TYPE) p_uc[10];
+}
+
+TYPE
+return_convert_schar_to_float128_mem (signed char *p_sc)
+{
+  return (TYPE) p_sc[10];
+}
+
+TYPE
+return_convert_ushort_to_float128_mem (unsigned short *p_us)
+{
+  return (TYPE) p_us[10];
+}
+
+TYPE
+return_convert_sshort_to_float128_mem (short *p_ss)
+{
+  return (TYPE) p_ss[10];
+}
+
+/* { dg-final { scan-assembler     {\mlxsi[bh]zx\M}  } } */
+/* { dg-final { scan-assembler     {\mvexts[bh]2d\M} } } */
+/* { dg-final { scan-assembler-not {\mextsb\M}       } } */
+/* { dg-final { scan-assembler-not {\ml[bh][az]\M}   } } */
+/* { dg-final { scan-assembler-not {\mmtvsrw[az]\M}  } } */
+
