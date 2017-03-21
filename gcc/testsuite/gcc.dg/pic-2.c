120816e4ed287c090563bd36ebd3b7db7ef9f3e5
tomtab <tomtab@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-03-21 11:07:19 +0000

    Skip gcc.dg/pic-2.c and gcc.dg/pie-2.c for MIPS.
    gcc/testsuite/
    
    	* gcc.dg/pic-2.c: Skip for MIPS.
    	* gcc.dg/pie-2.c: Skip for MIPS.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@246311 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pic-2.c
+++ gcc/testsuite/gcc.dg/pic-2.c
@@ -1,6 +1,7 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target fpic } */
 /* { dg-options "-fPIC" } */
+/* { dg-skip-if "__PIC__ is always 1 for MIPS" { mips*-*-* } } */
 
 #if __PIC__ != 2
 # error __PIC__ is not 2!
