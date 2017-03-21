504bd413091de2fd9cc7ca6c5a26740957280063
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-03-21 18:28:06 +0000

    [PR63238] include alignment debug information in DIE checksum
    Add DW_AT_alignment to the DIE checksum.
        
    for gcc/ChangeLog
        
    	PR debug/63238
    	* dwarf2out.c (struct checksum_attributes): Add at_alignment.
    	(collect_checksum_attributes): Set it.
    	(die_checksum_ordered): Use it.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@246331 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/dwarf2out.c
+++ gcc/dwarf2out.c
@@ -6633,6 +6633,7 @@ struct checksum_attributes
   dw_attr_node *at_friend;
   dw_attr_node *at_accessibility;
   dw_attr_node *at_address_class;
+  dw_attr_node *at_alignment;
   dw_attr_node *at_allocated;
   dw_attr_node *at_artificial;
   dw_attr_node *at_associated;
@@ -6706,6 +6707,9 @@ collect_checksum_attributes (struct checksum_attributes *attrs, dw_die_ref die)
         case DW_AT_address_class:
           attrs->at_address_class = a;
           break;
+	case DW_AT_alignment:
+	  attrs->at_alignment = a;
+	  break;
         case DW_AT_allocated:
           attrs->at_allocated = a;
           break;
@@ -6912,6 +6916,7 @@ die_checksum_ordered (dw_die_ref die, struct md5_ctx *ctx, int *mark)
   CHECKSUM_ATTR (attrs.at_vtable_elem_location);
   CHECKSUM_ATTR (attrs.at_type);
   CHECKSUM_ATTR (attrs.at_friend);
+  CHECKSUM_ATTR (attrs.at_alignment);
 
   /* Checksum the child DIEs.  */
   c = die->die_child;
