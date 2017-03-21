9d60683aa0cbeb57f835e30b2e2604c2025ffd70
Toma Tabacu <toma.tabacu@imgtec.com>
2017-03-21 11:07:19 +0000

    Skip gcc.dg/pic-2.c and gcc.dg/pie-2.c for MIPS.
    gcc/testsuite/
    
    	* gcc.dg/pic-2.c: Skip for MIPS.
    	* gcc.dg/pie-2.c: Skip for MIPS.
    
    From-SVN: r246311

--- gcc/testsuite/gcc.dg/pie-2.c
+++ gcc/testsuite/gcc.dg/pie-2.c
@@ -1,6 +1,7 @@
 /* { dg-do compile } */
 /* { dg-options "-fPIE" } */
 /* { dg-require-effective-target pie } */
+/* { dg-skip-if "__PIC__ is always 1 for MIPS" { mips*-*-* } } */
 
 #if __PIC__ != 2
 # error __PIC__ is not 2!
