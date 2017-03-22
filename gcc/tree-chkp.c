21f4c507e293caf51066ce0c6ff8e4f31d00011c
Martin Liska <mliska@suse.cz>
2017-03-22 13:32:17 +0100

    Backport r246276
    2017-03-22  Martin Liska  <mliska@suse.cz>
    
    	Backport from mainline
    	2017-03-20  Martin Liska  <mliska@suse.cz>
    
    	PR middle-end/79753
    	* tree-chkp.c (chkp_build_returned_bound): Do not build
    	returned bounds for a LHS that's not a BOUNDED_P type.
    2017-03-22  Martin Liska  <mliska@suse.cz>
    
    	Backport from mainline
    	2017-03-20  Martin Liska  <mliska@suse.cz>
    
    	PR middle-end/79753
    	* gcc.target/i386/mpx/pr79753.c: New test.
    
    From-SVN: r246377

--- gcc/tree-chkp.c
+++ gcc/tree-chkp.c
@@ -2236,6 +2236,7 @@ chkp_build_returned_bound (gcall *call)
   tree bounds;
   gimple stmt;
   tree fndecl = gimple_call_fndecl (call);
+  tree lhs = gimple_call_lhs (call);
   unsigned int retflags;
 
   /* To avoid fixing alloca expands in targets we handle
@@ -2246,9 +2247,8 @@ chkp_build_returned_bound (gcall *call)
 	  || DECL_FUNCTION_CODE (fndecl) == BUILT_IN_ALLOCA_WITH_ALIGN))
     {
       tree size = gimple_call_arg (call, 0);
-      tree lb = gimple_call_lhs (call);
       gimple_stmt_iterator iter = gsi_for_stmt (call);
-      bounds = chkp_make_bounds (lb, size, &iter, true);
+      bounds = chkp_make_bounds (lhs, size, &iter, true);
     }
   /* We know bounds returned by set_bounds builtin call.  */
   else if (fndecl
@@ -2301,9 +2301,10 @@ chkp_build_returned_bound (gcall *call)
 
       bounds = chkp_find_bounds (gimple_call_arg (call, argno), &iter);
     }
-  else if (chkp_call_returns_bounds_p (call))
+  else if (chkp_call_returns_bounds_p (call)
+	   && BOUNDED_P (lhs))
     {
-      gcc_assert (TREE_CODE (gimple_call_lhs (call)) == SSA_NAME);
+      gcc_assert (TREE_CODE (lhs) == SSA_NAME);
 
       /* In general case build checker builtin call to
 	 obtain returned bounds.  */
@@ -2330,7 +2331,7 @@ chkp_build_returned_bound (gcall *call)
       print_gimple_stmt (dump_file, call, 0, TDF_VOPS|TDF_MEMSYMS);
     }
 
-  bounds = chkp_maybe_copy_and_register_bounds (gimple_call_lhs (call), bounds);
+  bounds = chkp_maybe_copy_and_register_bounds (lhs, bounds);
 
   return bounds;
 }
