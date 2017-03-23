8403bd2bbb121bf68af237c5fabd32165feecf01
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-03-23 19:40:41 +0000

    Fix Debug Mode test failures
    	* testsuite/23_containers/array/tuple_interface/
    	tuple_element_debug_neg.cc: Adjust dg-error.
    	* testsuite/23_containers/list/operations/78389.cc: Fix less-than to
    	define a valid strict weak ordering.
    	* testsuite/23_containers/priority_queue/67085.cc: Disable test for
    	Debug Mode, due to debug checks making extra copies of predicate.
    	* testsuite/ext/pb_ds/regression/priority_queue_binary_heap-62045.cc:
    	Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@246426 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/ext/pb_ds/regression/priority_queue_binary_heap-62045.cc
+++ libstdc++-v3/testsuite/ext/pb_ds/regression/priority_queue_binary_heap-62045.cc
@@ -15,7 +15,7 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-do run }
+// { dg-require-normal-mode "" }
 
 #include <ext/pb_ds/priority_queue.hpp>
 #include <testsuite_hooks.h>
