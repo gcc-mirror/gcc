7dabefa0a6f96029e6f1ac847fcb959ff2e69a21
Jakub Jelinek <jakub@redhat.com>
2017-03-24 22:43:08 +0100

    re PR target/79904 (ICE in annotate_constant_pool_refs, at config/s390/s390.c:7909)
    	PR sanitizer/79904
    	* gcc.dg/ubsan/pr79904-2.c: Add -Wno-psabi to dg-options.
    
    From-SVN: r246468

--- gcc/testsuite/gcc.dg/ubsan/pr79904-2.c
+++ gcc/testsuite/gcc.dg/ubsan/pr79904-2.c
@@ -1,6 +1,6 @@
 /* PR sanitizer/79904 */
 /* { dg-do compile } */
-/* { dg-options "-fsanitize=signed-integer-overflow" } */
+/* { dg-options "-fsanitize=signed-integer-overflow -Wno-psabi" } */
 
 typedef signed char V __attribute__((vector_size (8))); 
 
