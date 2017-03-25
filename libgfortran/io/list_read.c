1f10d710e321ad92322adf90342cf99fa3f9d356
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2017-03-25 18:48:01 +0000

    re PR fortran/78881 ([F03] reading from string with DTIO procedure does not work properly)
    2017-03-25  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/78881
    	* io/io.h (st_parameter_dt): Rename unused component last_char to
    	child_saved_iostat. Move comment to gfc_unit.
    	* io/list_read.c (list_formatted_read_scalar): After call to
    	child READ procedure, save the returned iostat value for later
    	check. (finish_list_read): Only finish READ if child_saved_iostat
    	was OK.
    	* io/transfer.c (read_sf_internal): If there is a saved character
    	in last character, seek back one. Add a new check for EOR
    	condition. (read_sf): If there is a saved character
    	in last character, seek back one. (formatted_transfer_scalar_read):
    	Initialize last character before invoking child procedure.
    	(data_transfer_init): If child dtio, set advance
    	status to nonadvancing. Move update of size and check for EOR
    	condition to before child dtio return.
    
    	* gfortran.dg/dtio_26.f90: New test.
    
    From-SVN: r246478

--- libgfortran/io/list_read.c
+++ libgfortran/io/list_read.c
@@ -2221,6 +2221,7 @@ list_formatted_read_scalar (st_parameter_dt *dtp, bt type, void *p,
 	  dtp->u.p.fdtio_ptr (p, &unit, iotype, &vlist,
 			      child_iostat, child_iomsg,
 			      iotype_len, child_iomsg_len);
+	  dtp->u.p.child_saved_iostat = *child_iostat;
 	  dtp->u.p.current_unit->child_dtio--;
       }
       break;
@@ -2352,15 +2353,18 @@ finish_list_read (st_parameter_dt *dtp)
       /* Set the next_char and push_char worker functions.  */
       set_workers (dtp);
 
-      c = next_char (dtp);
-      if (c == EOF)
+      if (likely (dtp->u.p.child_saved_iostat == LIBERROR_OK))
 	{
-	  free_line (dtp);
-	  hit_eof (dtp);
-	  return;
+	  c = next_char (dtp);
+	  if (c == EOF)
+	    {
+	      free_line (dtp);
+	      hit_eof (dtp);
+	      return;
+	    }
+	  if (c != '\n')
+	    eat_line (dtp);
 	}
-      if (c != '\n')
-	eat_line (dtp);
     }
 
   free_line (dtp);
