81b98ef7697900c12d94f7f413613b8139a16624
Claudiu Zissulescu <claziss@synopsys.com>
2017-03-27 12:56:24 +0200

    [ARC] Disable TP register when building for bare metal.
    gcc/
    2017-03-27  Claudiu Zissulescu  <claziss@synopsys.com>
    
    	* config/arc/elf.h (ARGET_ARC_TP_REGNO_DEFAULT): Define.
    	* config/arc/linux.h (ARGET_ARC_TP_REGNO_DEFAULT): Likewise.
    	* config/arc/arc.opt (mtp-regno): Use ARGET_ARC_TP_REGNO_DEFAULT.
    
    From-SVN: r246497

--- gcc/config/arc/linux.h
+++ gcc/config/arc/linux.h
@@ -74,3 +74,7 @@ along with GCC; see the file COPYING3.  If not see
 /* We do not have any MULTILIB_OPTIONS specified, so there are no
    MULTILIB_DEFAULTS.  */
 #undef  MULTILIB_DEFAULTS
+
+/* Linux toolchains use r25 as the thread pointer register.  */
+#undef TARGET_ARC_TP_REGNO_DEFAULT
+#define TARGET_ARC_TP_REGNO_DEFAULT 25
