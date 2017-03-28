cf474530613eaaa4d28534a5a53ef61fcc71180d
Janus Weil <janus@gcc.gnu.org>
2017-03-28 19:01:05 +0200

    re PR fortran/78661 ([OOP] Namelist output missing object designator under DTIO)
    2017-03-28  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/78661
    	* trans-io.c (transfer_namelist_element): Perform a polymorphic call
    	to a DTIO procedure if necessary.
    
    2017-03-28  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/78661
    	* gfortran.dg/dtio_25.f90: Modified test case.
    	* gfortran.dg/dtio_27.f90: New test case.
    
    2017-03-28  Janus Weil  <janus@gcc.gnu.org>
    
    	PR fortran/78661
    	* io/write.c (nml_write_obj): Build a class container only if necessary.
    
    From-SVN: r246546

--- gcc/testsuite/gfortran.dg/dtio_25.f90
+++ gcc/testsuite/gfortran.dg/dtio_25.f90
@@ -8,6 +8,8 @@ module m
   contains
     procedure :: write_formatted
     generic :: write(formatted) => write_formatted
+    procedure :: read_formatted
+    generic :: read(formatted) => read_formatted
   end type
 contains
   subroutine write_formatted(dtv, unit, iotype, v_list, iostat, iomsg)
@@ -18,10 +20,25 @@ contains
     integer, intent(out) :: iostat
     character(*), intent(inout) :: iomsg
     if (iotype.eq."NAMELIST") then
-      write (unit, '(a,a,a,a,i5)') 'x%c="',dtv%c,'",','x%k=', dtv%k
+      write (unit, '(a3,a1,i3)') dtv%c,',', dtv%k
     else
       write (unit,*) dtv%c, dtv%k
     end if
+  end subroutine
+  subroutine read_formatted(dtv, unit, iotype, v_list, iostat, iomsg)
+    class(t), intent(inout) :: dtv
+    integer, intent(in) :: unit
+    character(*), intent(in) :: iotype
+    integer, intent(in) :: v_list(:)
+    integer, intent(out) :: iostat
+    character(*), intent(inout) :: iomsg
+    character :: comma
+    if (iotype.eq."NAMELIST") then
+      read (unit, '(a4,a1,i3)') dtv%c, comma, dtv%k    ! FIXME: need a4 here, with a3 above
+    else
+      read (unit,*) dtv%c, comma, dtv%k
+    end if
+    if (comma /= ',') call abort()
   end subroutine
 end module
 
@@ -33,9 +50,8 @@ program p
   namelist /nml/ x
   x = t('a', 5)
   write (buffer, nml)
-  if (buffer.ne.'&NML x%c="a",x%k=    5  /') call abort
+  if (buffer.ne.'&NML  X=  a,  5  /') call abort
   x = t('x', 0)
   read (buffer, nml)
   if (x%c.ne.'a'.or. x%k.ne.5) call abort
 end
-
