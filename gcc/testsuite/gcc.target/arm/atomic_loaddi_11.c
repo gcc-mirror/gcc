a8bcfa806f00f84acf58b2fead7a87769cfadfac
thopre01 <thopre01@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-04-06 14:53:22 +0000

    [ARM] Compile atomic_loaddi_11 for Cortex-R5
    2017-04-06  Thomas Preud'homme  <thomas.preudhomme@arm.com
    
        gcc/testsuite/
        PR target/80082
        * gcc.target/arm/atomic_loaddi_11.c: Target Cortex-R5 instead of
        ARMv7-R.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@246733 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/atomic_loaddi_11.c
+++ gcc/testsuite/gcc.target/arm/atomic_loaddi_11.c
@@ -1,7 +1,7 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target arm_arch_v7r_ok } */
-/* { dg-options "-O2" } */
-/* { dg-add-options arm_arch_v7r } */
+/* { dg-skip-if "do not override -mcpu" { *-*-* } { "-mcpu=*" "-march=*" } { "-mcpu=cortex-r5" } } */
+/* { dg-options "-O2 -mcpu=cortex-r5" } */
 
 #include <stdatomic.h>
 
