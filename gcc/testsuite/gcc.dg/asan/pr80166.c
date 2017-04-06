ca23873aed881901a967372e61ee64634d634970
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-04-06 13:42:24 +0000

    Cherry-pick upstream r299036 from libsanitizer (PR sanitizer/80166).
    2017-04-06  Martin Liska  <mliska@suse.cz>
    
    	PR sanitizer/80166
    	* sanitizer_common/sanitizer_common_interceptors.inc (INTERCEPTOR):
    	Cherry-pick upstream r299036.
    2017-04-06  Martin Liska  <mliska@suse.cz>
    
    	PR sanitizer/80166
    	* gcc.dg/asan/pr80166.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@246730 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/asan/pr80166.c
@@ -0,0 +1,24 @@
+/* PR sanitizer/80166 */
+/* { dg-do run } */
+
+#include <sys/types.h>
+#include <unistd.h>
+
+int
+main (int argc, char **argv)
+{
+  gid_t groups;
+  int r = getgroups (0, &groups);
+  if (r < 0)
+    __builtin_abort ();
+
+  r = getgroups (-1, &groups);
+  if (r != -1)
+    __builtin_abort ();
+
+  r = getgroups (-1, NULL);
+  if (r != -1)
+    __builtin_abort ();
+
+  return 0;
+}
