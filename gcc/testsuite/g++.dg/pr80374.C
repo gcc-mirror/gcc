fc647d444b60b4d4a1df0a570032d1bbe54afe4a
Markus Trippelsdorf <trippels@gcc.gnu.org>
2017-04-11 06:31:58 +0000

    Fix pr80374 testcase
    From-SVN: r246832

--- gcc/testsuite/g++.dg/pr80374.C
+++ gcc/testsuite/g++.dg/pr80374.C
@@ -1,3 +1,5 @@
+// { dg-do compile }
+// { dg-options "-O1 -std=c++11" }
 void a (const char *, const char *, int, const char *)
   __attribute__ ((__noreturn__));
 template <typename b, int>
@@ -11,7 +13,8 @@ catch (b d)
     if (d)
       a ("", "", 2, __PRETTY_FUNCTION__);
   }
-main ()
+void
+foo ()
 {
   using e = decltype (nullptr);
   c<volatile e, true> ();
