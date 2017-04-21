7b9efd0304da30be598cc672ce78e82923947953
Uros Bizjak <ubizjak@gmail.com>
2017-04-21 21:09:27 +0200

    pr79804.c: Add additional dg-error directive.
    	* gcc.target/i386/pr79804.c: Add additional dg-error directive.
    
    From-SVN: r247065

--- gcc/testsuite/gcc.target/i386/pr79804.c
+++ gcc/testsuite/gcc.target/i386/pr79804.c
@@ -7,4 +7,4 @@ void foo (void)
   register int r20 asm ("20");
 
   asm volatile ("# %0" : "=r"(r20));  /* { dg-error "invalid use of register" } */
-}
+}  /* { dg-error "cannot be used in asm here" } */
