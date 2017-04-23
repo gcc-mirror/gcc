5c0207b6ae6544541c786c74d57ddfcbe48b7a48
bebbo <bebbo@bejy.net>
2017-04-23 23:08:48 +0200

    @B fix __regards definition

--- gcc/config/m68k/m68kamigaos.h
+++ gcc/config/m68k/m68kamigaos.h
@@ -194,7 +194,7 @@ amiga_declare_object = 0
       builtin_define ("__saveds=__attribute__((__saveds__))");		\
       builtin_define ("__interrupt=__attribute__((__interrupt__))");	\
       builtin_define ("__stackext=__attribute__((__stackext__))");	\
-      builtin_define ("__regargs=__attribute__((__regparm__))");	\
+      builtin_define ("__regargs=__attribute__((__regparm__(2)))");	\
       builtin_define ("__stdargs=__attribute__((__stkparm__))");	\
       builtin_define ("__aligned=__attribute__((__aligned__(4)))");	\
       builtin_define_std ("amiga");					\
