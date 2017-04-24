29bff6c0295bc3d40a1914e9e56206660c23f83c
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-04-24 07:33:20 +0000

    2017-04-24  Richard Biener  <rguenther@suse.de>
    	PR bootstrap/79814
    	* pass_manager.h (pass_manager::operator new): Remove.
    	(pass_manager::operator delete): Likewise.
    	* passes.c (pass_manager::operator new): Remove.
    	(pass_manager::operator delete): Likewise.
    	(pass_manager::pass_manager): Zero individual pass members.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@247089 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/passes.c
+++ gcc/passes.c
@@ -1528,19 +1528,6 @@ pass_manager::register_pass (struct register_pass_info *pass_info)
 				        -> all_passes
 */
 
-void *
-pass_manager::operator new (size_t sz)
-{
-  /* Ensure that all fields of the pass manager are zero-initialized.  */
-  return xcalloc (1, sz);
-}
-
-void
-pass_manager::operator delete (void *ptr)
-{
-  free (ptr);
-}
-
 pass_manager::pass_manager (context *ctxt)
 : all_passes (NULL), all_small_ipa_passes (NULL), all_lowering_passes (NULL),
   all_regular_ipa_passes (NULL),
@@ -1548,6 +1535,21 @@ pass_manager::pass_manager (context *ctxt)
   m_ctxt (ctxt)
 {
   opt_pass **p;
+
+  /* Zero-initialize pass members.  */
+#define INSERT_PASSES_AFTER(PASS)
+#define PUSH_INSERT_PASSES_WITHIN(PASS)
+#define POP_INSERT_PASSES()
+#define NEXT_PASS(PASS, NUM) PASS ## _ ## NUM = NULL
+#define NEXT_PASS_WITH_ARG(PASS, NUM, ARG) NEXT_PASS (PASS, NUM)
+#define TERMINATE_PASS_LIST(PASS)
+#include "pass-instances.def"
+#undef INSERT_PASSES_AFTER
+#undef PUSH_INSERT_PASSES_WITHIN
+#undef POP_INSERT_PASSES
+#undef NEXT_PASS
+#undef NEXT_PASS_WITH_ARG
+#undef TERMINATE_PASS_LIST
 
   /* Initialize the pass_lists array.  */
 #define DEF_PASS_LIST(LIST) pass_lists[PASS_LIST_NO_##LIST] = &LIST;
