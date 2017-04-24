84341de847199e0fb6fe3b0880b9206352a18d53
Martin Liska <mliska@suse.cz>
2017-04-24 16:48:43 +0200

    Subject: Backport r247097
    2017-04-24  Martin Liska  <mliska@suse.cz>
    
    	Backport from mainline
    	2017-04-24  Jan Hubicka  <hubicka@ucw.cz>
    
    	PR middle-end/79931
    	* ipa-devirt.c (dump_possible_polymorphic_call_targets): Fix ICE.
    2017-04-24  Martin Liska  <mliska@suse.cz>
    
    	Backport from mainline
    	2017-04-24  Martin Liska  <mliska@suse.cz>
    
    	PR middle-end/79931
    	* g++.dg/ipa/pr79931.C: New test.
    
    From-SVN: r247102

---
+++ gcc/testsuite/g++.dg/ipa/pr79931.C
@@ -0,0 +1,24 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-ipa-all" } */
+
+class DocumentImpl;
+struct NodeImpl
+{
+  virtual DocumentImpl * getOwnerDocument();
+  virtual NodeImpl * getParentNode();
+  virtual NodeImpl * removeChild(NodeImpl *oldChild);
+};
+struct AttrImpl : NodeImpl
+{
+  NodeImpl *insertBefore(NodeImpl *newChild, NodeImpl *refChild);
+};
+struct DocumentImpl : NodeImpl
+{
+  virtual NodeImpl *removeChild(NodeImpl *oldChild);
+  virtual int* getRanges();
+};
+NodeImpl *AttrImpl::insertBefore(NodeImpl *newChild, NodeImpl *refChild) {
+  NodeImpl *oldparent = newChild->getParentNode();
+  oldparent->removeChild(newChild);
+  this->getOwnerDocument()->getRanges();
+}
