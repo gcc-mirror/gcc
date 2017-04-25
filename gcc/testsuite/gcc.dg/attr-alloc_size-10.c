c9e9c3ac4d6554fc26f540b95ad3f1ef9eccfc26
Senthil Kumar Selvaraj <senthil_kumar.selvaraj@atmel.com>
2017-04-25 12:06:04 +0000

    Fix broken attr-alloc_size-10.c for avr
    Integer promotion combined with equal sizes for short and int (16 bits)
    causes overflow warnings when expanding the TEST macro for USHRT_MAX.
    
    Fix by explicitly disabling overflow warnings for targets with !int32plus.
    
    gcc/testsuite/
    
    2017-04-25  Senthil Kumar Selvaraj  <senthil_kumar.selvaraj@atmel.com>
    
    	* gcc.dg/attr-alloc_size-10.c: Ignore overflow warnings
    	for targets with int size less than 32 bits.
    
    From-SVN: r247203

--- gcc/testsuite/gcc.dg/attr-alloc_size-10.c
+++ gcc/testsuite/gcc.dg/attr-alloc_size-10.c
@@ -4,7 +4,8 @@
    range.
 
    { dg-do compile }
-   { dg-options "-O2 -Walloc-size-larger-than=12" } */
+   { dg-options "-O2 -Walloc-size-larger-than=12" } 
+   { dg-options "-Wno-overflow" { target { ! int32plus } } } */
 
 #define SCHAR_MAX __SCHAR_MAX__
 #define SCHAR_MIN (-SCHAR_MAX - 1)
