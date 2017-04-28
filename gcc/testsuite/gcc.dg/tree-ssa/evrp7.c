06984b14eb834259b37e4c6083ed909bfe121741
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-04-28 06:42:20 +0000

    2017-04-28  Richard Biener  <rguenther@suse.de>
    	* tree-vrp.c (assert_info): New struct.
    	(add_assert_info): New helper.
    	(register_edge_assert_for_2): Refactor to add asserts to a vector
    	of assert_info.
    	(register_edge_assert_for_1): Likewise.
    	(register_edge_assert_for): Likewise.
    	(finish_register_edge_assert_for): New helper actually registering
    	asserts where live on edge.
    	(find_conditional_asserts): Adjust.
    	(find_switch_asserts): Likewise.
    	(evrp_dom_walker::try_find_new_range): Generalize.
    	(evrp_dom_walker::before_dom_children): Use register_edge_assert_for.
    
    	* gcc.dg/tree-ssa/evrp7.c: New testcase.
    	* gcc.dg/tree-ssa/evrp8.c: Likewise.
    	* gcc.dg/tree-ssa/evrp9.c: Likewise.
    	* gcc.dg/tree-ssa/vrp35.c: Disable EVRP.
    	* gcc.dg/tree-ssa/vrp36.c: Likewise.
    	* gcc.dg/tree-ssa/pr49039.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@247362 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/evrp7.c
+++ gcc/testsuite/gcc.dg/tree-ssa/evrp7.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -fdisable-tree-evrp -fdump-tree-vrp1-details" } */
+/* { dg-options "-O2 -fdump-tree-evrp-details" } */
 
 int test1(int i, int k)
 {
@@ -11,4 +11,4 @@ int test1(int i, int k)
   return 1;
 }
 
-/* { dg-final { scan-tree-dump "Removing dead stmt \[^\r\n\]* = j_.* == 10" "vrp1" } } */
+/* { dg-final { scan-tree-dump "Removing dead stmt \[^\r\n\]* = j_.* == 10" "evrp" } } */
