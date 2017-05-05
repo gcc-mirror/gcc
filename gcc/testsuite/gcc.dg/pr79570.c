95fda2b9d692f15b9c364b87f022b5c4282e7ae2
Jakub Jelinek <jakub@redhat.com>
2017-05-05 23:29:10 +0200

    backport: re PR target/79570 (ICE in sel-sched-ir.c:4534 in pr69956.c)
    	Backported from mainline
    	2017-02-21  Jakub Jelinek  <jakub@redhat.com>
    
    	PR target/79570
    	* sel-sched.c (moveup_expr_cached): Don't call sel_bb_head
    	on temporarily removed DEBUG_INSNs.
    
    	* gcc.dg/pr79570.c: New test.
    
    From-SVN: r247673

---
+++ gcc/testsuite/gcc.dg/pr79570.c
@@ -0,0 +1,6 @@
+/* PR target/79570 */
+/* { dg-do compile { target powerpc*-*-* ia64-*-* i?86-*-* x86_64-*-* } } */
+/* { dg-options "-O2 -fselective-scheduling2 -fvar-tracking-assignments" } */
+/* { dg-warning "changes selective scheduling" "" { target *-*-* } 0 } */
+
+#include "pr69956.c"
