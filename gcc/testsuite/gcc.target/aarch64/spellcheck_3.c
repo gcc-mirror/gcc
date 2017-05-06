6df1349133e30539cab44681270e0ff9d67db029
Tom de Vries <tom@codesourcery.com>
2017-05-06 07:16:33 +0000

    Replace absolute line numbers in gcc.target/aarch64
    2017-05-06  Tom de Vries  <tom@codesourcery.com>
    
    	PR testsuite/80557
    	* gcc.target/aarch64/spellcheck_1.c: Replace absolute line numbers.
    	* gcc.target/aarch64/spellcheck_2.c: Same.
    	* gcc.target/aarch64/spellcheck_3.c: Same.
    
    From-SVN: r247713

--- gcc/testsuite/gcc.target/aarch64/spellcheck_3.c
+++ gcc/testsuite/gcc.target/aarch64/spellcheck_3.c
@@ -3,7 +3,7 @@
 __attribute__((target ("tune=cortex-a57-typo"))) void
 foo ()
 {
+  /* { dg-message "valid arguments are: \[^\n\r]*; did you mean 'cortex-a57?"  "" { target *-*-* } .-1 } */
+  /* { dg-error "unknown value 'cortex-a57-typo' for 'tune' target attribute"  "" { target *-*-* } .-2 } */
+  /* { dg-error "target attribute 'tune=cortex-a57-typo' is invalid"  "" { target *-*-* } .-3 } */
 }
-/* { dg-message "valid arguments are: \[^\n\r]*; did you mean 'cortex-a57?"  "" { target *-*-* } 5 } */
-/* { dg-error "unknown value 'cortex-a57-typo' for 'tune' target attribute"  "" { target *-*-* } 5 } */
-/* { dg-error "target attribute 'tune=cortex-a57-typo' is invalid"  "" { target *-*-* } 5 } */
