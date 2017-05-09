560e3ca7cd92b5f8ca97396f343c026b391a2766
Michael Meissner <meissner@linux.vnet.ibm.com>
2017-05-09 23:49:37 +0000

    backport: re PR target/79038 (Improve PowerPC ISA 3.0 conversion between integers and hardware _Float128)
    [gcc]
    2017-05-09  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	Back port from mainline
    	2017-05-05  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	PR target/79038
    	PR target/79202
    	PR target/79203
    	* config/rs6000/rs6000.md (u code attribute): Add FIX and
    	UNSIGNED_FIX.
    	(extendsi<mode>2): Add support for doing sign extension via
    	VUPKHSW and XXPERMDI if the value is in Altivec registers and we
    	don't have ISA 3.0 instructions.
    	(extendsi<mode>2 splitter): Likewise.
    	(fix_trunc<mode>si2): If we are at ISA 2.07 (VSX small integer),
    	generate the normal insns since SImode can now go in vector
    	registers.  Disallow the special UNSPECs needed for previous
    	machines to hide SImode being used.  Add new insns
    	fctiw{,w}_<mode>_smallint if SImode can go in vector registers.
    	(fix_trunc<mode>si2_stfiwx): Likewise.
    	(fix_trunc<mode>si2_internal): Likewise.
    	(fixuns_trunc<mode>si2): Likewise.
    	(fixuns_trunc<mode>si2_stfiwx): Likewise.
    	(fctiw<u>z_<mode>_smallint): Likewise.
    	(fctiw<u>z_<mode>_mem): New combiner pattern to prevent conversion
    	of floating point to 32-bit integer from doing a direct move to
    	the GPR registers to do a store.
    	(fctiwz_<mode>): Break long line.
    
    [gcc/testsuite]
    2017-05-09  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	Back port from mainline
    	2017-05-05  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	PR target/79038
    	PR target/79202
    	PR target/79203
    	* gcc.target/powerpc/ppc-round3.c: New test.
    	* gcc.target/powerpc/ppc-round2.c: Update expected code.
    
    From-SVN: r247820

--- gcc/testsuite/gcc.target/powerpc/ppc-round3.c
+++ gcc/testsuite/gcc.target/powerpc/ppc-round3.c
@@ -1,18 +1,18 @@
 /* { dg-do compile { target { powerpc*-*-* && lp64 } } } */
 /* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
-/* { dg-require-effective-target powerpc_p8vector_ok } */
+/* { dg-require-effective-target powerpc_p9vector_ok } */
 /* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power8" } } */
-/* { dg-options "-O2 -mcpu=power8" } */
+/* { dg-options "-O2 -mcpu=power9" } */
 /* { dg-final { scan-assembler-times "fcfid \|xscvsxddp "    2 } } */
 /* { dg-final { scan-assembler-times "fcfids \|xscvsxdsp "   2 } } */
 /* { dg-final { scan-assembler-times "fctiwz \|xscvdpsxws "  2 } } */
 /* { dg-final { scan-assembler-times "fctiwuz \|xscvdpuxws " 2 } } */
-/* { dg-final { scan-assembler-times {\mmfvsrwz\M}           2 } } */
-/* { dg-final { scan-assembler-times {\mmtvsrwz\M}           2 } } */
-/* { dg-final { scan-assembler-times {\mvupkhsw\M}           2 } } */
-/* { dg-final { scan-assembler-times {\mxxpermdi\M}          2 } } */
+/* { dg-final { scan-assembler-times {\mvextsw2d\M}          2 } } */
+/* { dg-final { scan-assembler-times {\mxxextractuw\M}       2 } } */
 /* { dg-final { scan-assembler-not   {\mmfvsrd\M}              } } */
+/* { dg-final { scan-assembler-not   {\mmfvsrwz\M}             } } */
 /* { dg-final { scan-assembler-not   {\mmtvsrwa\M}             } } */
+/* { dg-final { scan-assembler-not   {\mmtvsrwz\M}             } } */
 /* { dg-final { scan-assembler-not   {\mlwz\M}                 } } */
 /* { dg-final { scan-assembler-not   {\mlfiwax\M}              } } */
 /* { dg-final { scan-assembler-not   {\mlfiwzx\M}              } } */
