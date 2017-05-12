5dd0c4e81e7a79afccfc936407affbdda2e3b737
tschwinge <tschwinge@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-05-12 09:18:34 +0000

    [PR middle-end/69921] Use "oacc kernels parallelized" attribute for parallelized OpenACC kernels
    	gcc/
    	PR middle-end/69921
    	* tree-parloops.c (create_parallel_loop): Set "oacc kernels
    	parallelized" attribute for parallelized OpenACC kernels.
    	* omp-offload.c (execute_oacc_device_lower): Use it.
    	gcc/testsuite/
    	* c-c++-common/goacc/classify-kernels-unparallelized.c: Adjust.
    	* c-c++-common/goacc/classify-kernels.c: Likewise.
    	* c-c++-common/goacc/kernels-counter-vars-function-scope.c:
    	Likewise.
    	* c-c++-common/goacc/kernels-double-reduction-n.c: Likewise.
    	* c-c++-common/goacc/kernels-double-reduction.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-2.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-3.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-g.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-mod-not-zero.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-n.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-nest.c: Likewise.
    	* c-c++-common/goacc/kernels-loop.c: Likewise.
    	* c-c++-common/goacc/kernels-one-counter-var.c: Likewise.
    	* c-c++-common/goacc/kernels-reduction.c: Likewise.
    	* gfortran.dg/goacc/classify-kernels-unparallelized.f95: Likewise.
    	* gfortran.dg/goacc/classify-kernels.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-2.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data-2.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data-enter-exit-2.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data-enter-exit.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data-update.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-n.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop.f95: Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@247957 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/goacc/kernels-loop-n.f95
+++ gcc/testsuite/gfortran.dg/goacc/kernels-loop-n.f95
@@ -32,10 +32,9 @@ end module test
 
 ! Check that only one loop is analyzed, and that it can be parallelized.
 ! { dg-final { scan-tree-dump-times "SUCCESS: may be parallelized" 1 "parloops1" } }
+! TODO, PR70545.
+! { dg-final { scan-tree-dump-times "(?n)__attribute__\\(\\(oacc kernels parallelized, oacc function \\(, , \\), oacc kernels, omp target entrypoint\\)\\)" 1 "parloops1" { xfail *-*-* } } }
 ! { dg-final { scan-tree-dump-not "FAILED:" "parloops1" } }
 
 ! Check that the loop has been split off into a function.
 ! { dg-final { scan-tree-dump-times "(?n);; Function __test_MOD_foo._omp_fn.0 " 1 "optimized" } }
-
-! TODO, PR70545.
-! { dg-final { scan-tree-dump-times "(?n)oacc function \\(0," 1 "parloops1" { xfail *-*-* } } }
