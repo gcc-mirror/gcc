52e00e00b22d629c2ad1c579bda503806f76c896
Uros Bizjak <ubizjak@gmail.com>
2017-05-14 14:49:55 +0200

    backport: re PR target/80706 (peephole2 uses uninitialized stack variables on i686)
    	Backport from mainline
    	2017-05-11  Uros Bizjak  <ubizjak@gmail.com>
    
    	PR target/80706
    	* config/i386/sync.md (UNSPEC_LDX_ATOMIC): New unspec.
    	(UNSPEC_STX_ATOMIC): Ditto.
    	(loaddi_via_sse): New insn.
    	(storedi_via_sse): Ditto.
    	(atomic_loaddi_fpu): Emit loaddi_via_sse and storedi_via_sse.
    	Update corresponding peephole2 patterns.
    	(atomic_storedi_fpu): Ditto.
    
    testsuite/ChangeLog:
    
    	Backport from mainline
    	2017-05-11  Uros Bizjak  <ubizjak@gmail.com>
    		    Jakub Jelinek  <jakub@redhat.com>
    
    	PR target/80706
    	* gcc.target/i386/pr80706.c: New test.
    
    	2017-05-11  Uros Bizjak  <ubizjak@gmail.com>
    
    	* gcc.target/i386/pr22152.c: Fix undefined testcase.
    	Remove unnecessary loop.  Run on 32-bit targets only.
    
    From-SVN: r248032

--- gcc/testsuite/gcc.target/i386/pr22152.c
+++ gcc/testsuite/gcc.target/i386/pr22152.c
@@ -1,18 +1,18 @@
-/* { dg-do compile } */
+/* { dg-do compile { target ia32 } } */
 /* { dg-options "-O2 -msse2 -mtune=core2" } */
 /* { dg-additional-options "-mno-vect8-ret-in-mem" { target *-*-vxworks* } } */
-/* { dg-additional-options "-mabi=sysv" { target x86_64-*-mingw* } } */
 
 #include <mmintrin.h>
 
-__m64
-unsigned_add3 (const __m64 * a, const __m64 * b, unsigned int count)
-{
-  __m64 sum;
-  unsigned int i;
+typedef __SIZE_TYPE__ size_t;
 
-  for (i = 1; i < count; i++)
-    sum = _mm_add_si64 (a[i], b[i]);
+__m64
+unsigned_add3 (const __m64 * a, const __m64 * b, size_t count)
+{
+  __m64 sum = { 0, 0 };
+
+  if (count > 0)
+    sum = _mm_add_si64 (a[count-1], b[count-1]);
 
   return sum;
 }
