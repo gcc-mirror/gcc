727577c2300f8a1c4cf757e784267e11034f6b4a
Carl Love <cel@us.ibm.com>
2017-05-16 19:32:54 +0000

    rs6000-c: Add support for built-in functions vector unsigned long long vec_bperm (vector...
    gcc/ChangeLog:
    
    2017-05-16  Carl Love  <cel@us.ibm.com>
    
    	* config/rs6000/rs6000-c: Add support for built-in functions
    	vector unsigned long long vec_bperm (vector unsigned long long,
    	                                     vector unsigned char)
    	vector signed long long vec_mule (vector signed int,
    	                                  vector signed int)
    	vector unsigned long long vec_mule (vector unsigned int,
    	                                    vector unsigned int)
    	vector signed long long vec_mulo (vector signed int,
    	                                  vector signed int)
    	vector unsigned long long vec_mulo (vector unsigned int,
    	                                    vector unsigned int)
    	vector signed char vec_sldw (vector signed char,
    	                             vector signed char,
    	                             const int)
    	vector unsigned char vec_sldw (vector unsigned char,
    	                               vector unsigned char,
    	                               const int)
    	vector signed short vec_sldw (vector signed short,
    	                              vector signed short,
    	                              const int)
    	vector unsigned short vec_sldw (vector unsigned short,
    	                                vector unsigned short,
    	                                const int)
    	vector signed int vec_sldw (vector signed int,
    	                            vector signed int,
    	                            const int)
    	vector unsigned int vec_sldw (vector unsigned int,
    	                              vector unsigned int,
    	                              const int)
    	vector signed long long vec_sldw (vector signed long long,
    	                                  vector signed long long,
    	                                  const int)
    	vector unsigned long long vec_sldw (vector unsigned long long,
    	                                    vector unsigned long long,
    	                                    const int)
    	* config/rs6000/rs6000-c: Add support for built-in functions
    	* config/rs6000/rs6000-builtin.def: Add definition for SLDW.
    	* config/rs6000/altivec.h: Add defintion for vec_sldw.
    	* doc/extend.texi: Update the built-in documentation for the
    	new built-in functions.
    
    gcc/testsuite/ChangeLog:
    
    2017-05-16  Carl Love  <cel@us.ibm.com>
    
    	* gcc.target/powerpc/builtins-3.c: New vec_mule, vec_mulo test cases.
    	* gcc.target/powerpc/builtins-3-p8.c: Add tests for the new Power 8
    	built-ins to the test suite file.  Note, support for mradds exists
    	but no test case exists.
    	* gcc.target/powerpc/builtins-3-p9.c: Add tests for the new Power 9
    	built-ins to the test suite file.
    
    From-SVN: r248125

--- gcc/testsuite/gcc.target/powerpc/builtins-3-p8.c
+++ gcc/testsuite/gcc.target/powerpc/builtins-3-p8.c
@@ -16,12 +16,6 @@ test_pack_float (vector double x, vector double y)
   return vec_pack (x, y);
 }
 
-vector long long
-test_nabs_long_long (vector long long x)
-{
-  return vec_nabs (x);
-}
-
 vector signed int
 test_vsi_packs_vsll_vsll (vector signed long long x,
                           vector signed long long y)
@@ -34,12 +28,6 @@ test_vui_packs_vull_vull (vector unsigned long long x,
                           vector unsigned long long y)
 {
   return vec_packs (x, y);
-}
-
-vector long long
-test_neg_long_long (vector long long x)
-{
-	return vec_neg (x);
 }
 
 vector unsigned char
@@ -90,35 +78,34 @@ test_unsigned_long_long_popcnt_unsigned_long (vector unsigned long long x)
 	return vec_popcnt (x);
 }
 
+vector signed short
+test_vss_mradds_vss_vss (vector signed short x, vector signed short y,
+                         vector signed short z)
+{
+	return vec_mradds (x, y, z);
+}
+
 /* Expected test results:
 
      test_eq_long_long                         1 vcmpequd inst
      test_pack_float                           1 vpkudum inst
-     test_nabs_long_long                       1 vspltisw, 1 vsubudm, 1 vminsd
      test_vsi_packs_vsll_vsll                  1 vpksdss
      test_vui_packs_vull_vull                  1 vpkudus
-     test_neg_long_long                        1 vspltisw, 1 vsubudm 
-     test_eq_long_long                         1 vcmpequd inst
-     test_pack_float                           1 vpkudum inst
-     test_nabs_long_long                       1 vspltisw, 1 vsubudm, 1 vminsd
-     test_neg_long_long                        1 vspltisw, 1 vsubudm
      test_unsigned_char_popcnt_signed_char     1 vpopcntb
      test_unsigned_char_popcnt_unsigned_char   1 vpopcntb
      test_unsigned_short_popcnt_signed_short   1 vpopcnth
      test_unsigned_short_popcnt_unsigned_short 1 vpopcnth
-     test_unsigned_signed_popcnt_signed_int    1 vpopcntw
-     test_unsigned_signed_popcnt_unsigned_int  1 vpopcntw
-     test_unsigned_signed_popcnt_signed_long   1 vpopcntd
-     test_unsigned_signed_popcnt_unsigned_long 1 vpopcntd */
+     test_unsigned_int_popcnt_signed_int       2 vpopcntw
+     test_unsigned_int_popcnt_unsigned_int     1 vpopcntd
+     test_unsigned_long_long_popcnt_unsigned_long 1 vpopcntd
+     test_vss_mradds_vss_vsss                  1 vmhraddshs */
 
 /* { dg-final { scan-assembler-times "vcmpequd" 1 } } */
 /* { dg-final { scan-assembler-times "vpkudum"  1 } } */
-/* { dg-final { scan-assembler-times "vspltisw" 2 } } */
-/* { dg-final { scan-assembler-times "vsubudm"  2 } } */
-/* { dg-final { scan-assembler-times "vminsd"   1 } } */
 /* { dg-final { scan-assembler-times "vpksdss"  1 } } */
 /* { dg-final { scan-assembler-times "vpkudus"  1 } } */  
 /* { dg-final { scan-assembler-times "vpopcntb" 2 } } */
 /* { dg-final { scan-assembler-times "vpopcnth" 2 } } */
 /* { dg-final { scan-assembler-times "vpopcntw" 2 } } */
 /* { dg-final { scan-assembler-times "vpopcntd" 2 } } */
+/* { dg-final { scan-assembler-times "vmhraddshs"  1 } } */
--- gcc/testsuite/gcc.target/powerpc/builtins-3-p9.c
+++ gcc/testsuite/gcc.target/powerpc/builtins-3-p9.c
@@ -28,15 +28,44 @@ test_ne_long (vector bool long long x, vector bool long long y)
 	return vec_cmpne (x, y);
 }
 
+vector long long
+test_nabs_long_long (vector long long x)
+{
+  return vec_nabs (x);
+}
+
+vector long long
+test_neg_long_long (vector long long x)
+{
+	return vec_neg (x);
+}
+
+vector unsigned long long
+test_vull_bperm_vull_vuc (vector unsigned long long x,
+                          vector unsigned char y)
+{
+	return vec_bperm (x, y);
+}
+
 /* Expected test results:
 
      test_ne_char              1 vcmpneb
      test_ne_short             1 vcmpneh
      test_ne_int               1 vcmpnew
-     test_ne_long              1 vcmpequd, 1 xxlnor inst */
+     test_ne_long              1 vcmpequd, 1 xxlnor inst
+     test_nabs_long_long       1 xxspltib, 1 vsubudm, 1 vminsd
+     test_neg_long_long        1 vnegd
+     test_vull_bperm_vull_vuc  1 vbpermd
+
 
 /* { dg-final { scan-assembler-times "vcmpneb"  1 } } */
 /* { dg-final { scan-assembler-times "vcmpneh"  1 } } */
 /* { dg-final { scan-assembler-times "vcmpnew"  1 } } */
 /* { dg-final { scan-assembler-times "vcmpequd" 1 } } */
 /* { dg-final { scan-assembler-times "xxlnor"   1 } } */
+/* { dg-final { scan-assembler-times "xxspltib" 1 } } */
+/* { dg-final { scan-assembler-times "vsubudm"  1 } } */
+/* { dg-final { scan-assembler-times "vminsd"   1 } } */
+/* { dg-final { scan-assembler-times "vnegd"    1 } } */
+/* { dg-final { scan-assembler-times "vbpermd"  1 } } */
+
