26bddba34ab4735eba9f5a5da4399f348ce39674
Jonathan Wakely <jwakely@redhat.com>
2017-05-17 17:02:33 +0100

    Fixes to Doxygen comments in libstdc++ headers
    	* include/bits/refwrap.h: Fix Doxygen warning.
    	* include/bits/specfun.h: Likewise.
    	* include/bits/std_function.h: Likewise.
    	* include/bits/stl_algo.h (set_union, set_intersection)
    	(set_difference, set_symmetric_difference): Add Doxygen @param tags
    	for output iterator parameters.
    	* include/bits/stl_iterator.h (inserter): Add Doxygen @param tag for
    	iterator parameter.
    	* include/std/mutex (try_lock, lock): Change Mutex to Lockable in
    	Doxygen comments.
    
    From-SVN: r248160

--- libstdc++-v3/include/bits/std_function.h
+++ libstdc++-v3/include/bits/std_function.h
@@ -22,7 +22,7 @@
 // see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 // <http://www.gnu.org/licenses/>.
 
-/** @file include/bits/function.h
+/** @file include/bits/std_function.h
  *  This is an internal header file, included by other library headers.
  *  Do not attempt to use it directly. @headername{functional}
  */
