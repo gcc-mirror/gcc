3d4aea03a66283c39131beb17eabf15dfbbb0c30
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-05-18 14:36:15 +0000

    Don't include internal header in libstdc++ tests
    Backport from mainline
    2016-04-18  Jonathan Wakely  <jwakely@redhat.com>
    
    	* testsuite/experimental/memory_resource/null_memory_resource.cc:
    	Don't include internal header.
    	* testsuite/experimental/type_erased_allocator/2.cc: Likewise.
    	* testsuite/experimental/type_erased_allocator/uses_allocator.cc:
    	Likewise. Add licence and change to compile-only test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch@248220 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/experimental/type_erased_allocator/2.cc
+++ libstdc++-v3/testsuite/experimental/type_erased_allocator/2.cc
@@ -20,7 +20,6 @@
 #include <memory>
 #include <experimental/memory_resource>
 #include <experimental/utility>
-#include <bits/uses_allocator.h>
 #include <testsuite_hooks.h>
 #include <testsuite_allocator.h>
 
