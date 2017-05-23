c2d7c404a9ba8bc4d57907705564ed8cacd5e711
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2017-05-23 15:54:59 +0000

    re PR fortran/80256 (Cygwin test fail: bind_c_array_params_2.f90 scan-assembler-times)
    2017-05-23  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    
    	PR libgfortran/80256
    	* gfortran.dg/bind_c_array_params_2.f90: Modify to pass on
    	Cygwin.
    
    From-SVN: r248371

--- gcc/testsuite/gfortran.dg/bind_c_array_params_2.f90
+++ gcc/testsuite/gfortran.dg/bind_c_array_params_2.f90
@@ -16,7 +16,8 @@ integer :: aa(4,4)
 call test(aa)
 end
 
-! { dg-final { scan-assembler-times "\[ \t\]\[$,_0-9\]*myBindC" 1 { target { ! { hppa*-*-* s390*-*-* } } } } }
+! { dg-final { scan-assembler-times "\[ \t\]\[$,_0-9\]*myBindC" 1 { target { ! { hppa*-*-* s390*-*-* *-*-cygwin* } } } } }
 ! { dg-final { scan-assembler-times "myBindC,%r2" 1 { target { hppa*-*-* } } } }
+! { dg-final { scan-assembler-times "call\tmyBindC" 1 { target { *-*-cygwin* } } } }
 ! { dg-final { scan-assembler-times "brasl\t%r\[0-9\]*,myBindC" 1 { target { s390*-*-* } } } }
 ! { dg-final { scan-tree-dump-times "test \\\(&parm\\." 1 "original" } }
