e4d15e02dfbaf61fb5655595423baaff4a345a60
tschwinge <tschwinge@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-05-24 13:23:34 +0000

    C/C++ OpenACC: acc_pcopyin, acc_pcreate
    	libgomp/
    	* openacc.h (acc_pcopyin, acc_pcreate): Provide prototypes instead
    	of preprocessor definitions.
    	* libgomp.h (strong_alias): Guard by "#ifdef
    	HAVE_ATTRIBUTE_ALIAS".
    	* oacc-mem.c: Provide "acc_pcreate" as alias for
    	"acc_present_or_create", and "acc_pcopyin" as alias for
    	"acc_present_or_copyin".
    	* libgomp.map: New version "OACC_2.0.1".
    	(OACC_2.0.1): Add "acc_pcopyin", and "acc_pcreate".
    	* testsuite/libgomp.oacc-c-c++-common/lib-38.c: Remove, merging
    	its content into...
    	* testsuite/libgomp.oacc-c-c++-common/lib-32.c: ... this file.
    	Extend testing.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@248410 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgomp/openacc.h
+++ libgomp/openacc.h
@@ -91,8 +91,10 @@ void acc_free (void *) __GOACC_NOTHROW;
    the standard specifies otherwise.  */
 void *acc_copyin (void *, size_t) __GOACC_NOTHROW;
 void *acc_present_or_copyin (void *, size_t) __GOACC_NOTHROW;
+void *acc_pcopyin (void *, size_t) __GOACC_NOTHROW;
 void *acc_create (void *, size_t) __GOACC_NOTHROW;
 void *acc_present_or_create (void *, size_t) __GOACC_NOTHROW;
+void *acc_pcreate (void *, size_t) __GOACC_NOTHROW;
 void acc_copyout (void *, size_t) __GOACC_NOTHROW;
 void acc_delete (void *, size_t) __GOACC_NOTHROW;
 void acc_update_device (void *, size_t) __GOACC_NOTHROW;
@@ -104,11 +106,6 @@ void *acc_hostptr (void *) __GOACC_NOTHROW;
 int acc_is_present (void *, size_t) __GOACC_NOTHROW;
 void acc_memcpy_to_device (void *, void *, size_t) __GOACC_NOTHROW;
 void acc_memcpy_from_device (void *, void *, size_t) __GOACC_NOTHROW;
-
-/* Old names.  OpenACC does not specify whether these can or must
-   not be macros, inlines or aliases for the new names.  */
-#define acc_pcreate acc_present_or_create
-#define acc_pcopyin acc_present_or_copyin
 
 /* CUDA-specific routines.  */
 void *acc_get_current_cuda_device (void) __GOACC_NOTHROW;
