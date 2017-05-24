5ad4f1c81153459ce6fc8c285a39c69e060ac604
Nathan Sidwell <nathan@acm.org>
2017-05-24 23:18:58 +0000

    friend.c (do_friend): Remove check for existing decl.
    	cp/
    	* friend.c (do_friend): Remove check for existing decl.
    	* name-lookup.h (lookup_name_innermost_nonclass_level): Delete.
    	* name-lookup.c (push_local_binding): Directly look for binding.
    	(lookup_name_innermost_nonclass_level_1): Delete.
    	(lookup_name_innermost_nonclass_level): Delete.
    
    	testsuite/
    	* g++.dg/lookup/friend12.C: Adjust diagnostics.
    	* g++.dg/lookup/friend19.C: New.
    	* g++.dg/lookup/friend20.C: New.
    
    From-SVN: r248435

---
+++ gcc/testsuite/g++.dg/lookup/friend20.C
@@ -0,0 +1,16 @@
+// PR c++/80830
+
+template <int> class a;
+class b
+{
+  friend int operator>> (int, b);
+};
+template <int c> int &operator>> (int &, a<c> &);
+template <int = 3> class a
+{
+  friend int &operator>><> (int &, a &);
+  a<>
+  d ()
+  {
+  }
+};
