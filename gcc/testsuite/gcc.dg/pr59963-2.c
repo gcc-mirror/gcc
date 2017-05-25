67cbcbf60fde089ef0de49f443eb251550973e16
msebor <msebor@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-05-25 16:51:24 +0000

    gcc/testsuite/ChangeLog: 	* gcc.dg/overflow-warn-9.c: Adjust expected warnings to avoid false 	positives on ILP32. 	* gcc.dg/pr59963-2.c: Replace tabs with spaces, adjust expected 	column numbers. 	* gcc.dg/pr60114.c: Same.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@248464 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr59963-2.c
+++ gcc/testsuite/gcc.dg/pr59963-2.c
@@ -13,9 +13,9 @@ int
 g (void)
 {
   return f (0xffffffffL, /* { dg-warning "13:-Woverflow" } */
-	    0xffffffffL) /* { dg-warning "13:-Woverflow" } */
-	 && f (0xffffffffL, /* { dg-warning "9:-Woverflow" } */
-	       0xffffffffL); /* { dg-warning "9:-Woverflow" } */
+            0xffffffffL) /* { dg-warning "13:-Woverflow" } */
+         && f (0xffffffffL, /* { dg-warning "16:-Woverflow" } */
+               0xffffffffL); /* { dg-warning "16:-Woverflow" } */
 }
 
 void
@@ -24,13 +24,13 @@ foo (int i)
   bar (256); /* { dg-warning "8:-Woverflow" } */
   bar (6.66f); /* { dg-warning "8:conversion" } */
   bar8 (-1, /* { dg-warning "9:-Wsign-conversion" } */
-	 -2, /* { dg-warning "3:-Wsign-conversion" } */
-	  -3, /* { dg-warning "4:-Wsign-conversion" } */
-	   -4, /* { dg-warning "5:-Wsign-conversion" } */
-	    -5, /* { dg-warning "6:-Wsign-conversion" } */
-	     -6, /* { dg-warning "7:-Wsign-conversion" } */
-	      -7, /* { dg-warning "8:-Wsign-conversion" } */
-	       -8); /* { dg-warning "9:-Wsign-conversion" } */
+         -2, /* { dg-warning "10:-Wsign-conversion" } */
+          -3, /* { dg-warning "11:-Wsign-conversion" } */
+           -4, /* { dg-warning "12:-Wsign-conversion" } */
+            -5, /* { dg-warning "13:-Wsign-conversion" } */
+             -6, /* { dg-warning "14:-Wsign-conversion" } */
+              -7, /* { dg-warning "15:-Wsign-conversion" } */
+               -8); /* { dg-warning "16:-Wsign-conversion" } */
   bazu (i, i); /* { dg-warning "9:conversion" } */
   bazi (0x8, 0x80000000); /* { dg-warning "14:-Wsign-conversion" "" { xfail int16 } } */
 }
