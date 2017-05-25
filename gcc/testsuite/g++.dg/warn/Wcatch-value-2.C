63dbcd13e93578e21e0717d4990c1701d6957e3e
Volker Reichelt <v.reichelt@netcologne.de>
2017-05-25 17:05:07 +0000

    invoke.texi (-Wcatch-value=): Document new warning option.
            * doc/invoke.texi (-Wcatch-value=): Document new warning option.
    
            * c.opt (Wcatch-value=): New C++ warning flag.
    
            * semantics.c (finish_handler_parms): Warn about non-reference
            * type
            catch handlers.
    
            * g++.dg/warn/Wcatch-value-1.C: New test.
            * g++.dg/warn/Wcatch-value-2.C: New test.
            * g++.dg/warn/Wcatch-value-3.C: New test.
    
    From-SVN: r248466

---
+++ gcc/testsuite/g++.dg/warn/Wcatch-value-2.C
@@ -0,0 +1,64 @@
+// { dg-options "-Wcatch-value=2" }
+
+struct A { virtual ~A() {} };
+struct B : A {};
+struct C {};
+struct D : C {};
+
+void foo()
+{
+  try {}
+  catch (D)    {}  // { dg-warning "catching type" }
+  catch (C)    {}  // { dg-warning "catching type" }
+  catch (B)    {}  // { dg-warning "catching polymorphic type" }
+  catch (A)    {}  // { dg-warning "catching polymorphic type" }
+  catch (A*)   {}
+  catch (int)  {}
+
+  try {}
+  catch (D&)   {}
+  catch (C&)   {}
+  catch (B&)   {}
+  catch (A&)   {}
+  catch (A*)   {}
+  catch (int&) {}
+}
+
+template<typename T> void foo1()
+{
+  try {}
+  catch (T) {}  // { dg-warning "catching" }
+}
+
+template<typename T> void foo2()
+{
+  try {}
+  catch (T*) {}
+
+  try {}
+  catch (T&) {}
+
+  try {}
+  catch (const T&) {}
+}
+
+void bar()
+{
+  foo1<int&>();
+  foo1<const A&>();
+  foo1<B&>();
+  foo1<const C&>();
+  foo1<D&>();
+
+  foo1<int>();
+  foo1<A>();  // { dg-message "required" }
+  foo1<B>();  // { dg-message "required" }
+  foo1<C>();  // { dg-message "required" }
+  foo1<D>();  // { dg-message "required" }
+
+  foo2<int>();
+  foo2<A>();
+  foo2<B>();
+  foo2<C>();
+  foo2<D>();
+}
