932f48ac85f5111b1f3666b2c15718db2d10bcbf
Nathan Sidwell <nathan@acm.org>
2017-05-25 16:55:43 +0000

    Reimplement unqualified namespace lookup.
    	gcc/cp/
    	Reimplement unqualified namespace lookup.
    	* name-lookup.c (name_lookup::using_pair)
    	name_lookup::using_queue): New typedefs.
    	(name_lookup::queue_namespace, name_lookup::do_queue_usings,
    	name_lookup::queue_usings): New.
    	(name_lookup::search_unqualified): New.
    	(merge_functions, same_entity_p, ambiguous_decl)
    	unqualified_namespace_lookup_1, unqualified_namespace_lookup,
    	lookup_using_namespace): Delete.
    	(lookup_name_real_1): Adjust.
    
    	gcc/testsuite/
    	* g++.dg/lookup/using17.C: Adjust diagnostics.
    
    From-SVN: r248465

--- gcc/testsuite/g++.dg/lookup/using17.C
+++ gcc/testsuite/g++.dg/lookup/using17.C
@@ -3,11 +3,11 @@
 // { dg-do compile }
 
 namespace M {
-  struct S {}; // { dg-message ".struct M::S." "candidate 2" }
+  struct S {}; // { dg-message "candidates are: .struct M::S." "candidate 1" }
 }
 
 int S;
-struct S {}; // { dg-message "candidates are: .struct S." "candidate 1" }
+struct S {}; // { dg-message ".struct S." "candidate 2" }
 
 using namespace M;
 
