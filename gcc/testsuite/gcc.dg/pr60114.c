a10f9a2eaaeb9acb9ffa89fa12e248745f7fce86
Martin Sebor <msebor@redhat.com>
2017-05-25 16:51:24 +0000

    overflow-warn-9.c: Adjust expected warnings to avoid false positives on ILP32.
    gcc/testsuite/ChangeLog:
    	* gcc.dg/overflow-warn-9.c: Adjust expected warnings to avoid false
    	positives on ILP32.
    	* gcc.dg/pr59963-2.c: Replace tabs with spaces, adjust expected
    	column numbers.
    	* gcc.dg/pr60114.c: Same.
    
    From-SVN: r248464

--- gcc/testsuite/gcc.dg/pr60114.c
+++ gcc/testsuite/gcc.dg/pr60114.c
@@ -5,13 +5,13 @@
 
 struct S { int n, u[2]; };
 const signed char z[] = {
-  [0] = 0x100, /* { dg-warning "9:-Woverflow" } */
-  [2] = 0x101, /* { dg-warning "9:-Woverflow" } */
+  [0] = 0x100, /* { dg-warning "-Woverflow" } */
+  [2] = 0x101, /* { dg-warning "-Woverflow" } */
 };
 int A[] = {
-	    0, 0x80000000, /* { dg-warning "16:-Wsign-conversion" } */
-	    0xA, 0x80000000, /* { dg-warning "18:-Wsign-conversion" } */
-	    0xA, 0xA, 0x80000000 /* { dg-warning "23:-Wsign-conversion" } */
+            0, 0x80000000, /* { dg-warning "16:-Wsign-conversion" } */
+            0xA, 0x80000000, /* { dg-warning "18:-Wsign-conversion" } */
+            0xA, 0xA, 0x80000000 /* { dg-warning "23:-Wsign-conversion" } */
           };
 int *p = (int []) { 0x80000000 }; /* { dg-warning "21:-Wsign-conversion" } */
 union { int k; } u = { .k = 0x80000000 }; /* { dg-warning "29:-Wsign-conversion" } */
@@ -20,8 +20,8 @@ void
 foo (void)
 {
   signed char a[][3] = { { 0x100, /* { dg-warning "28:-Woverflow" } */
-	            1, 0x100 }, /* { dg-warning "24:-Woverflow" } */
-	          { '\0', 0x100, '\0' } /* { dg-warning "27:-Woverflow" } */
+                    1, 0x100 }, /* { dg-warning "24:-Woverflow" } */
+                  { '\0', 0x100, '\0' } /* { dg-warning "27:-Woverflow" } */
                 };
   (const signed char []) { 0x100 }; /* { dg-warning "28:-Woverflow" } */
   (const float []) { 1e0, 1e1, 1e100 }; /* { dg-warning "32:conversion" } */
