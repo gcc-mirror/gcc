5637ac627975d1173660ab6cd457f48696a6dd24
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-05-25 20:55:07 +0000

    Kill OVL_CURRENT, OVL_NEXT. 	* cp-tree.h (OVL_CURRENT, OVL_NEXT): Delete. 	* name-lookup.c (set_decl_namespace): Use ovl_iterator. 	(consider_binding_level): Use OVL_FIRST. 	(cp_emit_debug_info_for_using): Use lkp_iterator. 	* pt.c (check_explicit_specialization): Use ovl_iterator.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@248469 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/name-lookup.c
+++ gcc/cp/name-lookup.c
@@ -4275,13 +4275,13 @@ set_decl_namespace (tree decl, tree scope, bool friendp)
      friends in any namespace.  */
   if (friendp && DECL_USE_TEMPLATE (decl))
     return;
-  if (is_overloaded_fn (old))
+  if (OVL_P (old))
     {
       tree found = NULL_TREE;
-      tree elt = old;
-      for (; elt; elt = OVL_NEXT (elt))
+
+      for (ovl_iterator iter (old); iter; ++iter)
 	{
-	  tree ofn = OVL_CURRENT (elt);
+	  tree ofn = *iter;
 	  /* Adjust DECL_CONTEXT first so decls_match will return true
 	     if DECL will match a declaration in an inline namespace.  */
 	  DECL_CONTEXT (decl) = DECL_CONTEXT (ofn);
@@ -4932,10 +4932,7 @@ consider_binding_level (tree name, best_match <tree, const char *> &bm,
       /* OVERLOADs or decls from using declaration are wrapped into
 	 TREE_LIST.  */
       if (TREE_CODE (d) == TREE_LIST)
-	{
-	  d = TREE_VALUE (d);
-	  d = OVL_CURRENT (d);
-	}
+	d = OVL_FIRST (TREE_VALUE (d));
 
       /* Don't use bindings from implicitly declared functions,
 	 as they were likely misspellings themselves.  */
@@ -6290,14 +6287,18 @@ cp_emit_debug_info_for_using (tree t, tree context)
     t = BASELINK_FUNCTIONS (t);
 
   /* FIXME: Handle TEMPLATE_DECLs.  */
-  for (t = OVL_CURRENT (t); t; t = OVL_NEXT (t))
-    if (TREE_CODE (t) != TEMPLATE_DECL)
-      {
-	if (building_stmt_list_p ())
-	  add_stmt (build_stmt (input_location, USING_STMT, t));
-	else
-	  (*debug_hooks->imported_module_or_decl) (t, NULL_TREE, context, false);
-      }
+  for (lkp_iterator iter (t); iter; ++iter)
+    {
+      tree fn = *iter;
+      if (TREE_CODE (fn) != TEMPLATE_DECL)
+	{
+	  if (building_stmt_list_p ())
+	    add_stmt (build_stmt (input_location, USING_STMT, fn));
+	  else
+	    debug_hooks->imported_module_or_decl (fn,
+						  NULL_TREE, context, false);
+	}
+    }
 }
 
 #include "gt-cp-name-lookup.h"
