ac9fcfb72b38b307b5f0d5bf3c88cb56309ddfe6
Jakub Jelinek <jakub@redhat.com>
2017-05-30 10:17:54 +0200

    backport: re PR c++/80141 (ICE with pragma omp declare)
    	Backported from mainline
    	2017-03-22  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/80141
    	* semantics.c (finish_omp_clause) <case OMP_CLAUSE_SIMDLEN,
    	case OMP_CLAUSE_ALIGNED>: Call maybe_constant_value only when not
    	processing_template_decl.
    
    	* g++.dg/gomp/pr80141.C: New test.
    
    From-SVN: r248663

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -5546,9 +5546,9 @@ finish_omp_clauses (tree clauses)
 	  else
 	    {
 	      t = mark_rvalue_use (t);
-	      t = maybe_constant_value (t);
 	      if (!processing_template_decl)
 		{
+		  t = maybe_constant_value (t);
 		  if (TREE_CODE (t) != INTEGER_CST
 		      || tree_int_cst_sgn (t) != 1)
 		    {
@@ -5726,9 +5726,9 @@ finish_omp_clauses (tree clauses)
 	  else
 	    {
 	      t = mark_rvalue_use (t);
-	      t = maybe_constant_value (t);
 	      if (!processing_template_decl)
 		{
+		  t = maybe_constant_value (t);
 		  if (TREE_CODE (t) != INTEGER_CST
 		      || tree_int_cst_sgn (t) != 1)
 		    {
