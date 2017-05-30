1a21f546767e1ecdf1fb0d4cb359506a3e293921
Jakub Jelinek <jakub@redhat.com>
2017-05-30 09:17:45 +0200

    backport: re PR middle-end/77377 (c-c++-common/pr59037.c ICEs with -fpic -msse on i686)
    	Backported from mainline
    	2016-08-30  Jakub Jelinek  <jakub@redhat.com>
    
    	PR middle-end/77377
    	* simplify-rtx.c (avoid_constant_pool_reference): For out of bounds
    	constant pool reference return x instead of c.
    
    	* gcc.target/i386/pr77377.c: New test.
    
    From-SVN: r248601

---
+++ gcc/testsuite/gcc.target/i386/pr77377.c
@@ -0,0 +1,6 @@
+/* PR middle-end/77377 */
+/* { dg-do compile } */
+/* { dg-options "-O3 -msse" } */
+/* { dg-additional-options "-fpic" { target fpic } } */
+
+#include "../../c-c++-common/pr59037.c"
