7b67119fd03ebf1a3961377ac5d4e94e9b5d565b
Jakub Jelinek <jakub@redhat.com>
2017-05-30 09:37:39 +0200

    backport: re PR c++/78089 (__builtin_shuffle parsing bug)
    	Backported from mainline
    	2016-10-31  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/78089
    	* parser.c (cp_parser_postfix_expression): Replace return statement in
    	the first switch with setting postfix_expression to the return
    	expression and break;.
    
    	* c-c++-common/builtin-shuffle-1.c: New test.
    
    From-SVN: r248620

--- gcc/cp/parser.c
+++ gcc/cp/parser.c
@@ -5894,7 +5894,10 @@ cp_parser_postfix_expression (cp_parser *parser, bool address_p, bool cast_p,
 	   can be used in constant-expressions.  */
 	if (!cast_valid_in_integral_constant_expression_p (type)
 	    && cp_parser_non_integral_constant_expression (parser, NIC_CAST))
-	  return error_mark_node;
+	  {
+	    postfix_expression = error_mark_node;
+	    break;
+	  }
 
 	switch (keyword)
 	  {
@@ -5966,7 +5969,7 @@ cp_parser_postfix_expression (cp_parser *parser, bool address_p, bool cast_p,
 	parser->type_definition_forbidden_message = saved_message;
 	/* `typeid' may not appear in an integral constant expression.  */
 	if (cp_parser_non_integral_constant_expression (parser, NIC_TYPEID))
-	  return error_mark_node;
+	  postfix_expression = error_mark_node;
       }
       break;
 
@@ -6045,23 +6048,28 @@ cp_parser_postfix_expression (cp_parser *parser, bool address_p, bool cast_p,
 		    /*cast_p=*/false, /*allow_expansion_p=*/true,
 		    /*non_constant_p=*/NULL);
 	if (vec == NULL)
-	  return error_mark_node;
+	  {
+	    postfix_expression = error_mark_node;
+	    break;
+	  }
 
 	FOR_EACH_VEC_ELT (*vec, i, p)
 	  mark_exp_read (p);
 
 	if (vec->length () == 2)
-	  return build_x_vec_perm_expr (loc, (*vec)[0], NULL_TREE, (*vec)[1],
-					 tf_warning_or_error);
+	  postfix_expression
+	    = build_x_vec_perm_expr (loc, (*vec)[0], NULL_TREE, (*vec)[1],
+				     tf_warning_or_error);
 	else if (vec->length () == 3)
-	  return build_x_vec_perm_expr (loc, (*vec)[0], (*vec)[1], (*vec)[2],
-					 tf_warning_or_error);
+	  postfix_expression
+	    = build_x_vec_perm_expr (loc, (*vec)[0], (*vec)[1], (*vec)[2],
+				     tf_warning_or_error);
 	else
-	{
-	  error_at (loc, "wrong number of arguments to "
-	      "%<__builtin_shuffle%>");
-	  return error_mark_node;
-	}
+	  {
+	    error_at (loc, "wrong number of arguments to "
+			   "%<__builtin_shuffle%>");
+	    postfix_expression = error_mark_node;
+	  }
 	break;
       }
 
