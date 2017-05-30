1657d923e68a4969e1c3661a06a1f4cde30b7ae7
Uros Bizjak <uros@gcc.gnu.org>
2017-05-30 19:18:25 +0200

    re PR target/80833 (32-bit x86 causes store-forwarding stalls for int64_t -> xmm)
    	PR target/80833
    	* config/i386/constraints.md (Yd): New constraint.
    	(Ye): Ditto.
    	* config/i386/i386.md (*movti_internal): Add (?r, Ye)
    	and (?Yd, r) alternatives.  Update insn attributes.
    	* config/i386/i386.md (*movti_internal): Add (?r, *Ye)
    	and (?*Yd, r) alternatives.  Update insn attributes.
    	(double-mode inter-unit splitters): Add new GR<->XMM splitters.
    
    testsuite/ChangeLog:
    
    	PR target/80833
    	* gcc.target/i386/pr80833-1.c: New test.
    	* gcc.target/i386/pr80833-2.c: Ditto.
    
    From-SVN: r248691

---
+++ gcc/testsuite/gcc.target/i386/pr80833-1.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -msse4.1 -mtune=intel -mregparm=2" } */
+/* { dg-require-effective-target ia32 } */
+
+long long test (long long a)
+{
+  asm ("" : "+x" (a));
+  return a;
+}
+
+/* { dg-final { scan-assembler "pinsrd" } } */
+/* { dg-final { scan-assembler "pextrd" } } */
