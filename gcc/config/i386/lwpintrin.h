ec434f449ddead7da2248b6fe1018e76b71145aa
Jakub Jelinek <jakub@redhat.com>
2017-06-01 21:38:29 +0200

    backport: rdseedintrin.h (_rdseed16_step, [...]): Uglify argument names and/or local variable names in inline functions.
    	Backported from mainline
    	2016-08-19  Jakub Jelinek  <jakub@redhat.com>
    
    	* config/i386/rdseedintrin.h (_rdseed16_step, _rdseed32_step,
    	_rdseed64_step): Uglify argument names and/or local variable names
    	in inline functions.
    	* config/i386/rtmintrin.h (_xabort): Likewise.
    	* config/i386/avx512vlintrin.h (_mm256_ternarylogic_epi64,
    	_mm256_mask_ternarylogic_epi64, _mm256_maskz_ternarylogic_epi64,
    	_mm256_ternarylogic_epi32, _mm256_mask_ternarylogic_epi32,
    	_mm256_maskz_ternarylogic_epi32, _mm_ternarylogic_epi64,
    	_mm_mask_ternarylogic_epi64, _mm_maskz_ternarylogic_epi64,
    	_mm_ternarylogic_epi32, _mm_mask_ternarylogic_epi32,
    	_mm_maskz_ternarylogic_epi32): Likewise.
    	* config/i386/lwpintrin.h (__llwpcb, __lwpval32, __lwpval64,
    	__lwpins32, __lwpins64): Likewise.
    	* config/i386/avx2intrin.h (_mm_i32gather_pd, _mm_mask_i32gather_pd,
    	_mm256_i32gather_pd, _mm256_mask_i32gather_pd, _mm_i64gather_pd,
    	_mm_mask_i64gather_pd, _mm256_i64gather_pd, _mm256_mask_i64gather_pd,
    	_mm_i32gather_ps, _mm_mask_i32gather_ps, _mm256_i32gather_ps,
    	_mm256_mask_i32gather_ps, _mm_i64gather_ps, _mm_mask_i64gather_ps,
    	_mm256_i64gather_ps, _mm256_mask_i64gather_ps, _mm_i32gather_epi64,
    	_mm_mask_i32gather_epi64, _mm256_i32gather_epi64,
    	_mm256_mask_i32gather_epi64, _mm_i64gather_epi64,
    	_mm_mask_i64gather_epi64, _mm256_i64gather_epi64,
    	_mm256_mask_i64gather_epi64, _mm_i32gather_epi32,
    	_mm_mask_i32gather_epi32, _mm256_i32gather_epi32,
    	_mm256_mask_i32gather_epi32, _mm_i64gather_epi32,
    	_mm_mask_i64gather_epi32, _mm256_i64gather_epi32,
    	_mm256_mask_i64gather_epi32): Likewise.
    	* config/i386/pmm_malloc.h (_mm_malloc, _mm_free): Likewise.
    	* config/i386/ia32intrin.h (__writeeflags): Likewise.
    	* config/i386/pkuintrin.h (_wrpkru): Likewise.
    	* config/i386/avx512pfintrin.h (_mm512_mask_prefetch_i32gather_pd,
    	_mm512_mask_prefetch_i32gather_ps, _mm512_mask_prefetch_i64gather_pd,
    	_mm512_mask_prefetch_i64gather_ps, _mm512_prefetch_i32scatter_pd,
    	_mm512_prefetch_i32scatter_ps, _mm512_mask_prefetch_i32scatter_pd,
    	_mm512_mask_prefetch_i32scatter_ps, _mm512_prefetch_i64scatter_pd,
    	_mm512_prefetch_i64scatter_ps, _mm512_mask_prefetch_i64scatter_pd,
    	_mm512_mask_prefetch_i64scatter_ps): Likewise.
    	* config/i386/gmm_malloc.h (_mm_malloc, _mm_free): Likewise.
    	* config/i386/avx512fintrin.h (_mm512_ternarylogic_epi64,
    	_mm512_mask_ternarylogic_epi64, _mm512_maskz_ternarylogic_epi64,
    	_mm512_ternarylogic_epi32, _mm512_mask_ternarylogic_epi32,
    	_mm512_maskz_ternarylogic_epi32, _mm512_i32gather_ps,
    	_mm512_mask_i32gather_ps, _mm512_i32gather_pd, _mm512_i64gather_ps,
    	_mm512_i64gather_pd, _mm512_i32gather_epi32, _mm512_i32gather_epi64,
    	_mm512_i64gather_epi32, _mm512_i64gather_epi64): Likewise.
    
    From-SVN: r248799

--- gcc/config/i386/lwpintrin.h
+++ gcc/config/i386/lwpintrin.h
@@ -35,9 +35,9 @@
 #endif /* __LWP__ */
 
 extern __inline void __attribute__((__gnu_inline__, __always_inline__, __artificial__))
-__llwpcb (void *pcbAddress)
+__llwpcb (void *__pcbAddress)
 {
-  __builtin_ia32_llwpcb (pcbAddress);
+  __builtin_ia32_llwpcb (__pcbAddress);
 }
 
 extern __inline void * __attribute__((__gnu_inline__, __always_inline__, __artificial__))
@@ -48,16 +48,17 @@ __slwpcb (void)
 
 #ifdef __OPTIMIZE__
 extern __inline void __attribute__((__gnu_inline__, __always_inline__, __artificial__))
-__lwpval32 (unsigned int data2, unsigned int data1, unsigned int flags)
+__lwpval32 (unsigned int __data2, unsigned int __data1, unsigned int __flags)
 {
-  __builtin_ia32_lwpval32 (data2, data1, flags);
+  __builtin_ia32_lwpval32 (__data2, __data1, __flags);
 }
 
 #ifdef __x86_64__
 extern __inline void __attribute__((__gnu_inline__, __always_inline__, __artificial__))
-__lwpval64 (unsigned long long data2, unsigned int data1, unsigned int flags)
+__lwpval64 (unsigned long long __data2, unsigned int __data1,
+	    unsigned int __flags)
 {
-  __builtin_ia32_lwpval64 (data2, data1, flags);
+  __builtin_ia32_lwpval64 (__data2, __data1, __flags);
 }
 #endif
 #else
@@ -74,16 +75,17 @@ __lwpval64 (unsigned long long data2, unsigned int data1, unsigned int flags)
 
 #ifdef __OPTIMIZE__
 extern __inline unsigned char __attribute__((__gnu_inline__, __always_inline__, __artificial__))
-__lwpins32 (unsigned int data2, unsigned int data1, unsigned int flags)
+__lwpins32 (unsigned int __data2, unsigned int __data1, unsigned int __flags)
 {
-  return __builtin_ia32_lwpins32 (data2, data1, flags);
+  return __builtin_ia32_lwpins32 (__data2, __data1, __flags);
 }
 
 #ifdef __x86_64__
 extern __inline unsigned char __attribute__((__gnu_inline__, __always_inline__, __artificial__))
-__lwpins64 (unsigned long long data2, unsigned int data1, unsigned int flags)
+__lwpins64 (unsigned long long __data2, unsigned int __data1,
+	    unsigned int __flags)
 {
-  return __builtin_ia32_lwpins64 (data2, data1, flags);
+  return __builtin_ia32_lwpins64 (__data2, __data1, __flags);
 }
 #endif
 #else
