ef316fead76a8af3cefa2e82f40e899daea810a8
aivchenk <aivchenk@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-06-08 14:07:21 +0000

    gcc/ChangeLog:
    2017-05-09  Alexander Ivchenko  <aivchenk@gmail.com>
    
            * tree-chkp.c (chkp_get_hard_register_var_fake_base_address):
            New function.
            (chkp_get_hard_register_fake_addr_expr): Ditto.
            (chkp_build_addr_expr): Add check for hard reg case.
            (chkp_parse_array_and_component_ref): Ditto.
            (chkp_find_bounds_1): Ditto.
            (chkp_process_stmt): Don't generate bounds store for
            hard reg case.
    
    
    gcc/testsuite/ChangeLog:
    
    2017-05-09  Alexander Ivchenko  <aivchenk@gmail.com>
    
            * gcc.target/i386/mpx/hard-reg-2-lbv.c: New test.
            * gcc.target/i386/mpx/hard-reg-2-nov.c: New test.
            * gcc.target/i386/mpx/hard-reg-2-ubv.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@249015 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/mpx/hard-reg-2-ubv.c
@@ -0,0 +1,21 @@
+/* { dg-do run } */
+/* { dg-shouldfail "bounds violation" } */
+/* { dg-options "-fcheck-pointer-bounds -mmpx" } */
+
+
+#define SHOULDFAIL
+
+#include "mpx-check.h"
+
+typedef int v16 __attribute__((vector_size(16)));
+
+int foo (int i) {
+  register v16 u asm ("xmm0");
+  return u[i];
+}
+
+int mpx_test (int argc, const char **argv)
+{
+  printf ("%d\n", foo (5));
+  return 0;
+}
