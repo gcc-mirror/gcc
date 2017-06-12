754d67d5ba4a1f9994210d402893a4cf49ce6a71
Jonathan Wakely <jwakely@redhat.com>
2017-06-12 17:37:28 +0100

    PR libstdc++/55917 do not handle exceptions in std::thread
    	PR libstdc++/55917
    	* src/c++11/thread.cc (execute_native_thread_routine): Remove
    	try-block so that exceptions propagate out of the thread and terminate
    	is called by the exception-handling runtime.
    	(execute_native_thread_routine_compat): Likewise.
    	* testsuite/30_threads/thread/cons/terminate.cc: New.
    
    From-SVN: r249130

---
+++ libstdc++-v3/testsuite/30_threads/thread/cons/terminate.cc
@@ -0,0 +1,48 @@
+// Copyright (C) 2017 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do run { target *-*-freebsd* *-*-dragonfly* *-*-netbsd* *-*-linux* *-*-gnu* *-*-solaris* *-*-cygwin *-*-rtems* *-*-darwin* powerpc-ibm-aix* } }
+// { dg-options "-pthread" { target *-*-freebsd* *-*-dragonfly* *-*-netbsd* *-*-linux* *-*-gnu* *-*-solaris* powerpc-ibm-aix* } }
+// { dg-require-effective-target c++11 }
+// { dg-require-cstdint "" }
+// { dg-require-gthreads "" }
+
+#include <thread>
+#include <exception>
+#include <cstdlib>
+
+void handle_terminate()
+{
+  std::_Exit(0);
+}
+
+void f() { throw 1; }
+
+void
+test01()
+{
+  std::set_terminate(handle_terminate);
+  std::thread t(f);
+  t.join();
+  std::abort();
+}
+
+int
+main()
+{
+  test01();
+}
