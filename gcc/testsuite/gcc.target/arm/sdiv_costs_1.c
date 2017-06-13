8b9067413611345d95ea57dd74b4abac98fb0b9b
tnfchris <tnfchris@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-06-13 10:23:51 +0000

    2017-06-13  Tamar Christina  <tamar.christina@arm.com>
    	* gcc.target/arm/sdiv_costs_1.c:
    	Require arm_arch_v8a_ok and add march option.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@249148 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/sdiv_costs_1.c
+++ gcc/testsuite/gcc.target/arm/sdiv_costs_1.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
-/* { dg-options "-O3" } */
-/* { dg-require-effective-target arm_v8_vfp_ok }  */
+/* { dg-options "-O3 -march=armv8-a" } */
+/* { dg-require-effective-target arm_arch_v8a_ok } */
 
 /* Both sdiv and udiv can be used here, so prefer udiv.  */
 int f1 (unsigned char *p)
