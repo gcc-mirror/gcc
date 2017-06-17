98049775b68baa6ad9e77b3e897d790428a8460b
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-06-17 02:32:53 +0000

    * constexpr.c (potential_constant_expression_1): Allow 'this' capture.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@249324 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp1z/lambda-this3.C
@@ -0,0 +1,10 @@
+// { dg-options -std=c++1z }
+
+struct S {
+  int i;
+  constexpr S() : i(5) { 
+    ([*this] () { return i + 10; }());
+  }
+  constexpr operator int() const { return i; }
+};
+constexpr int x = S();
