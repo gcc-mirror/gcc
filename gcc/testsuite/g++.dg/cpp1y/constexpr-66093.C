431abe69f1c4ba0b4b05609f2f4f9206cc6e041a
Paolo Carlini <paolo.carlini@oracle.com>
2017-06-19 10:15:57 +0000

    re PR c++/66093 (g++ produces incorrect output on code with constexpr function initializing class with private fields)
    2017-06-19  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/66093
    	* g++.dg/cpp1y/constexpr-66093.C: New.
    
    From-SVN: r249364

---
+++ gcc/testsuite/g++.dg/cpp1y/constexpr-66093.C
@@ -0,0 +1,35 @@
+// { dg-do run { target c++14 } }
+
+#include <cassert>
+
+constexpr int n = 10;
+
+struct A {
+    constexpr operator const int*() const {
+        return data;
+    }
+  
+    constexpr operator int*() {
+        return data;
+    }
+  
+private:
+    int data[n];
+};
+
+constexpr A f() {
+    A a{};
+    for (int i = 1; i <= n; i++) {
+        a[i] = i;
+    }
+    return a;
+}
+
+A a = f();
+
+int main()
+{
+    for (int i = 0; i < n; i++) {
+        assert (a[i] == i);
+    }
+}
