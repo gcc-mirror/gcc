91437273dea529af2c42e06b87494eb8b8b24365
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-06-19 20:56:08 +0000

    PR c++/80562 - ICE with constexpr if.
    	* semantics.c (finish_if_stmt_cond): Call
    	instantiate_non_dependent_expr.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@249389 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp1z/constexpr-if12.C
@@ -0,0 +1,14 @@
+// PR c++/80562
+// { dg-options -std=c++1z }
+
+struct T {
+  constexpr auto foo() { return false; }
+};
+
+template <class MustBeTemplate>
+constexpr auto bf(T t) {
+    if constexpr(t.foo()) {
+        return false;
+    }
+    return true;
+}
