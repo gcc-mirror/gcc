648dc544240b0b5b08f3386c926ed70b76b40991
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-06-21 22:11:04 +0000

    libgo: remove old MIPS architecture names
        This removes the old names for the 3 main MIPS ABIs: mipso32, mipsn32
        and mipsn64. It also removes the mipso64 ABI which has no equivalent
        architecture name in go. This ABI has been dead for sometime and I doubt
        anyone will miss it.
        
        Reviewed-on: https://go-review.googlesource.com/46154
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@249477 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgo/go/internal/syscall/unix/getrandom_linux_mips64p32x.go
+++ libgo/go/internal/syscall/unix/getrandom_linux_mips64p32x.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build mipsn32 mips64p32 mips64p32le
+// +build mips64p32 mips64p32le
 
 package unix
 
