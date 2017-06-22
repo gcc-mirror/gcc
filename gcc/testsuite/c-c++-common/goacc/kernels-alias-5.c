896b9985f0dc944bce58878b65816d09e8e7dd04
Richard Biener <rguenther@suse.de>
2017-06-22 11:46:45 +0000

    backport: [multiple changes]
    2017-06-22  Richard Biener  <rguenther@suse.de>
    
    	Backport from mainline
    	2017-02-17  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/79552
    	* tree-ssa-structalias.c (visit_loadstore): Properly verify
    	default defs.
    
    	2016-04-18  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/43434
    	* tree-ssa-structalias.c (struct vls_data): New.
    	(visit_loadstore): Handle all pointer-based accesses.
    	(compute_dependence_clique): Compute a bitmap of restrict tags
    	assigned bases and pass it to visit_loadstore.
    
    	* gcc.dg/vect/pr43434.c: New testcase.
    	* c-c++-common/goacc/kernels-alias-3.c: Adjust.
    	* c-c++-common/goacc/kernels-alias-4.c: Likewise.
    	* c-c++-common/goacc/kernels-alias-5.c: Likewise.
    	* c-c++-common/goacc/kernels-alias-6.c: Likewise.
    	* c-c++-common/goacc/kernels-alias-7.c: Likewise.
    	* c-c++-common/goacc/kernels-alias-8.c: Likewise.
    	* gcc.dg/gomp/pr68640.c: Likewise.
    	* gfortran.dg/goacc/kernels-alias-3.f95: Likewise.
    	* gfortran.dg/goacc/kernels-alias-4.f95: Likewise.
    
    From-SVN: r249552

--- gcc/testsuite/c-c++-common/goacc/kernels-alias-5.c
+++ gcc/testsuite/c-c++-common/goacc/kernels-alias-5.c
@@ -15,5 +15,5 @@ foo (int *a)
 
 /* Only the omp_data_i related loads should be annotated with cliques.  */
 /* { dg-final { scan-tree-dump-times "clique 1 base 1" 2 "ealias" } } */
-/* { dg-final { scan-tree-dump-times "(?n)clique .* base .*" 2 "ealias" } } */
+/* { dg-final { scan-tree-dump-times "(?n)clique 1 base 0" 4 "ealias" } } */
 
