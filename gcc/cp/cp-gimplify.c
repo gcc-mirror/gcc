e38441b65ada3d4423cbd0a2d0fcf89e67591d65
Jakub Jelinek <jakub@redhat.com>
2017-06-22 13:12:43 +0200

    backport: re PR c++/80984 (ICE with label/variable ambiguity)
    	Backported from mainline
    	2017-06-13  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/80984
    	* cp-gimplify.c (cp_genericize): Only look for VAR_DECLs in
    	BLOCK_VARS (outer) chain.
    	(cxx_omp_const_qual_no_mutable): Likewise.
    
    	* g++.dg/opt/nrv18.C: New test.
    
    From-SVN: r249509

--- gcc/cp/cp-gimplify.c
+++ gcc/cp/cp-gimplify.c
@@ -1566,7 +1566,8 @@ cp_genericize (tree fndecl)
 
 	  if (outer)
 	    for (var = BLOCK_VARS (outer); var; var = DECL_CHAIN (var))
-	      if (DECL_NAME (t) == DECL_NAME (var)
+	      if (VAR_P (var)
+		  && DECL_NAME (t) == DECL_NAME (var)
 		  && DECL_HAS_VALUE_EXPR_P (var)
 		  && DECL_VALUE_EXPR (var) == t)
 		{
@@ -1813,7 +1814,8 @@ cxx_omp_const_qual_no_mutable (tree decl)
 
 	  if (outer)
 	    for (var = BLOCK_VARS (outer); var; var = DECL_CHAIN (var))
-	      if (DECL_NAME (decl) == DECL_NAME (var)
+	      if (VAR_P (var)
+		  && DECL_NAME (decl) == DECL_NAME (var)
 		  && (TYPE_MAIN_VARIANT (type)
 		      == TYPE_MAIN_VARIANT (TREE_TYPE (var))))
 		{
