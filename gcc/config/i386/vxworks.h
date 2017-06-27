e57f3ae8baeb35ba78da6c8bd00927df63105bdb
Jerome Lambourg <lambourg@adacore.com>
2017-06-27 13:55:42 +0000

    vxworks.h (ASM_SPEC): Remove definition.
    2017-06-27  Jerome Lambourg  <lambourg@adacore.com>
    
    	* config/i386/vxworks.h (ASM_SPEC): Remove definition. No target
    	specific need, just fallback on defaults.
    	(ASM_OUTPUT_ALIGNED_BSS): Add #undef before #define.
    
    From-SVN: r249691

--- gcc/config/i386/vxworks.h
+++ gcc/config/i386/vxworks.h
@@ -18,9 +18,7 @@ You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
 
-#undef  ASM_SPEC
-#define ASM_SPEC ""
-
+#undef ASM_OUTPUT_ALIGNED_BSS
 #define ASM_OUTPUT_ALIGNED_BSS(FILE, DECL, NAME, SIZE, ALIGN) \
   asm_output_aligned_bss (FILE, DECL, NAME, SIZE, ALIGN)
 
