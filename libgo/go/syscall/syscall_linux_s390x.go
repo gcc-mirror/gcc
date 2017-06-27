f9dedc3f21020bcc704fcfb7caa1b04b5dc874e7
Ian Lance Taylor <ian@gcc.gnu.org>
2017-06-27 21:51:50 +0000

    syscall: ptrace fixes for s390
        Fixes required now that we #include <linux/ptrace.h> in sysinfo.c.
        
        Patch by Andreas Krebbel.
        
        Reviewed-on: https://go-review.googlesource.com/46839
    
    From-SVN: r249712

--- libgo/go/syscall/syscall_linux_s390x.go
+++ libgo/go/syscall/syscall_linux_s390x.go
@@ -8,9 +8,9 @@ package syscall
 
 import "unsafe"
 
-func (r *PtraceRegs) PC() uint64 { return r.Psw.Addr }
+func (r *PtraceRegs) PC() uint64 { return r.Psw.addr }
 
-func (r *PtraceRegs) SetPC(pc uint64) { r.Psw.Addr = pc }
+func (r *PtraceRegs) SetPC(pc uint64) { r.Psw.addr = pc }
 
 func PtraceGetRegs(pid int, regsout *PtraceRegs) (err error) {
 	return ptrace(PTRACE_GETREGS, pid, 0, uintptr(unsafe.Pointer(regsout)))
