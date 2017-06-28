883312dc79806f513275b72502231c751c14ff72
Joseph Myers <joseph@codesourcery.com>
2017-06-28 10:21:16 +0100

    Use ucontext_t not struct ucontext in linux-unwind.h files.
    Current glibc no longer gives the ucontext_t type the tag struct
    ucontext, to conform with POSIX namespace rules.  This requires
    various linux-unwind.h files in libgcc, that were previously using
    struct ucontext, to be fixed to use ucontext_t instead.  This is
    similar to the removal of the struct siginfo tag from siginfo_t some
    years ago.
    
    This patch changes those files to use ucontext_t instead.  As the
    standard name that should be unconditionally safe, so this is not
    restricted to architectures supported by glibc, or conditioned on the
    glibc version.
    
    Tested compilation together with current glibc with glibc's
    build-many-glibcs.py.
    
    	* config/aarch64/linux-unwind.h (aarch64_fallback_frame_state),
    	config/alpha/linux-unwind.h (alpha_fallback_frame_state),
    	config/bfin/linux-unwind.h (bfin_fallback_frame_state),
    	config/i386/linux-unwind.h (x86_64_fallback_frame_state,
    	x86_fallback_frame_state), config/m68k/linux-unwind.h (struct
    	uw_ucontext), config/nios2/linux-unwind.h (struct nios2_ucontext),
    	config/pa/linux-unwind.h (pa32_fallback_frame_state),
    	config/riscv/linux-unwind.h (riscv_fallback_frame_state),
    	config/sh/linux-unwind.h (sh_fallback_frame_state),
    	config/tilepro/linux-unwind.h (tile_fallback_frame_state),
    	config/xtensa/linux-unwind.h (xtensa_fallback_frame_state): Use
    	ucontext_t instead of struct ucontext.
    
    From-SVN: r249731

--- libgcc/config/riscv/linux-unwind.h
+++ libgcc/config/riscv/linux-unwind.h
@@ -42,7 +42,7 @@ riscv_fallback_frame_state (struct _Unwind_Context *context,
   struct rt_sigframe
   {
     siginfo_t info;
-    struct ucontext uc;
+    ucontext_t uc;
   };
 
   struct rt_sigframe *rt_;
