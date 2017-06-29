f66459c1605ab66390ad5b5c54e425f96607d9eb
Peter Bergner <bergner@vnet.ibm.com>
2017-06-29 07:58:32 -0500

    re PR middle-end/81194 (ICE during RTL pass: expand)
    gcc/
    	PR middle-end/81194
    	* cfgexpand.c (expand_gimple_stmt_1): Handle switch statements
    	with only one label.
    	* stmt.c (expand_case): Assert NCASES is greater than one.
    
    gcc/testsuite/
    	PR middle-end/81194
    	* g++.dg/pr81194.C: New test.
    
    From-SVN: r249783

---
+++ gcc/testsuite/g++.dg/pr81194.C
@@ -0,0 +1,60 @@
+// { dg-do compile }
+// { dg-options "-O2 -std=c++17 -fno-exceptions" }
+
+template <class a> struct b { typedef a *c; };
+class e {};
+template <typename a> class d {
+public:
+  typedef typename b<a>::c c;
+  c begin();
+  c end();
+};
+struct f {
+  enum { g } h;
+};
+struct i {
+  d<f *> j();
+};
+struct l {
+  d<i *> k();
+};
+class ac;
+class o {
+public:
+  o(int *, int *, int *, ac *);
+};
+class ac {
+public:
+  ac(e);
+  virtual o *ae(int *, int *, int *, int *);
+};
+class p {
+  void af(f *m) {
+    switch (m->h)
+    case f::g:
+      ag();
+  }
+
+public:
+  void n() {
+    l ah;
+    for (i *ai : ah.k())
+      for (f *m : ai->j())
+        af(m);
+  }
+  virtual void ag() { __builtin_unreachable(); }
+};
+template <typename = int> class an : o {
+public:
+  an(int *, int *, int *, int *, ac *);
+};
+class q : ac {
+public:
+  q() : ac([]() -> e {}()) {}
+  o *ae(int *ap, int *aq, int *ar, int *as) { an(ap, aq, ar, as, this); }
+};
+template <typename at>
+an<at>::an(int *, int *aq, int *ar, int *as, ac *au) : o(aq, ar, as, au) {
+  p().n();
+}
+void av() { new q; }
