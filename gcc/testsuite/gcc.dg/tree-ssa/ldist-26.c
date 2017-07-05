8d95fe31028a93feb22efbd3f001cab69c000f61
amker <amker@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-07-05 12:05:44 +0000

    * tree-loop-distribution.c (classify_partition): New parameter and 	better handle reduction statement. 	(rdg_build_partitions): Revise comment. 	(distribute_loop): Compute statements in all partitions and pass it 	to classify_partition.
    	gcc/testsuite
    	* gcc.dg/tree-ssa/ldist-26.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@249993 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ldist-26.c
@@ -0,0 +1,36 @@
+/* { dg-do run } */
+/* { dg-options "-O3 -ftree-loop-distribution -fdump-tree-ldist-details" } */
+
+extern void abort (void);
+
+int a[130], b[128], c[128];
+
+int __attribute__((noinline,noclone))
+foo (int len, int x)
+{
+  int i;
+  for (i = 1; i <= len; ++i)
+    {
+      a[i] = a[i + 2] + 1;
+      b[i] = 0;
+      a[i + 1] = a[i] - 3;
+      if (i < x)
+	c[i] = a[i];
+    }
+  return i;
+}
+
+int main()
+{
+  int i;
+  for (i = 0; i < 130; ++i)
+    a[i] = i;
+  foo (127, 67);
+  if (a[0] != 0 || a[1] != 4 || a[127] != 130)
+    abort ();
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump "distributed: split to 2 loops and 0 library calls" "ldist" } } */
+/* { dg-final { scan-tree-dump "distributed: split to 1 loops and 1 library calls" "ldist" } } */
+/* { dg-final { scan-tree-dump "generated memset zero" "ldist" } } */
