14c7029b1a1ab618631851340ba3b5697f3457fd
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-07-19 12:31:59 +0000

    PR tree-optimization/81346 	* fold-const.h (fold_div_compare, range_check_type): Declare. 	* fold-const.c (range_check_type): New function. 	(build_range_check): Use range_check_type. 	(fold_div_compare): No longer static, rewritten into 	a match.pd helper function. 	(fold_comparison): Don't call fold_div_compare here. 	* match.pd (X / C1 op C2): New optimization using fold_div_compare 	as helper function.
    	* gcc.dg/tree-ssa/pr81346-1.c: New test.
    	* gcc.dg/tree-ssa/pr81346-2.c: New test.
    	* gcc.dg/tree-ssa/pr81346-3.c: New test.
    	* gcc.dg/tree-ssa/pr81346-4.c: New test.
    	* gcc.target/i386/umod-3.c: Hide comparison against 1 from the
    	compiler to avoid X / C1 op C2 optimization to trigger.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@250338 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr81346-1.c
@@ -0,0 +1,37 @@
+/* PR tree-optimization/81346 */
+/* { dg-do compile { target int32 } } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+/* { dg-final { scan-tree-dump-times "return 1;" 32 "optimized" } } */
+
+int f00 (int x) { return x / 49152 > -49152; }
+int f01 (int x) { int a = 49152, b = -49152; return x / a > b; }
+int f02 (int x) { return x / 49152 >= -49152; }
+int f03 (int x) { int a = 49152, b = -49152; return x / a >= b; }
+int f04 (int x) { return x / 49152 < 49152; }
+int f05 (int x) { int a = 49152, b = 49152; return x / a < b; }
+int f06 (int x) { return x / 49152 <= 49152; }
+int f07 (int x) { int a = 49152, b = 49152; return x / a <= b; }
+int f08 (int x) { return x / 46340 >= -46341; }
+int f09 (int x) { int a = 46340, b = -46341; return x / a >= b; }
+int f10 (int x) { return x / 46340 <= 46341; }
+int f11 (int x) { int a = 46340, b = 46341; return x / a <= b; }
+int f12 (int x) { return x / 49152 != -49152; }
+int f13 (int x) { int a = 49152, b = -49152; return x / a != b; }
+int f14 (int x) { return x / 49152 != 49152; }
+int f15 (int x) { int a = 49152, b = 49152; return x / a != b; }
+int f16 (int x) { return x / -49152 > -49152; }
+int f17 (int x) { int a = -49152, b = -49152; return x / a > b; }
+int f18 (int x) { return x / -49152 >= -49152; }
+int f19 (int x) { int a = -49152, b = -49152; return x / a >= b; }
+int f20 (int x) { return x / -49152 < 49152; }
+int f21 (int x) { int a = -49152, b = 49152; return x / a < b; }
+int f22 (int x) { return x / -49152 <= 49152; }
+int f23 (int x) { int a = -49152, b = 49152; return x / a <= b; }
+int f24 (int x) { return x / -46340 >= -46341; }
+int f25 (int x) { int a = -46340, b = -46341; return x / a >= b; }
+int f26 (int x) { return x / -46340 <= 46341; }
+int f27 (int x) { int a = -46340, b = 46341; return x / a <= b; }
+int f28 (int x) { return x / -49152 != 49152; }
+int f29 (int x) { int a = -49152, b = 49152; return x / a != b; }
+int f30 (int x) { return x / -49152 != -49152; }
+int f31 (int x) { int a = -49152, b = -49152; return x / a != b; }
