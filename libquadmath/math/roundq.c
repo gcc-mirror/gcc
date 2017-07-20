0e9284adc87e246f775ad6ab06408c023cbf2854
Jakub Jelinek <jakub@redhat.com>
2017-07-20 10:29:45 +0200

    re PR libquadmath/65757 (gfortran gives incorrect result for anint with real*16 argument)
    	PR libquadmath/65757
    	* math/roundq.c: Cherry-pick upstream glibc 2015-04-28 change.
    
    From-SVN: r250379

--- libquadmath/math/roundq.c
+++ libquadmath/math/roundq.c
@@ -1,5 +1,5 @@
 /* Round __float128 to integer away from zero.
-   Copyright (C) 1997, 1999 Free Software Foundation, Inc.
+   Copyright (C) 1997-2017 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1997 and
 		  Jakub Jelinek <jj@ultra.linux.cz>, 1999.
@@ -32,7 +32,7 @@ roundq (__float128 x)
 
   GET_FLT128_WORDS64 (i0, i1, x);
   j0 = ((i0 >> 48) & 0x7fff) - 0x3fff;
-  if (j0 < 31)
+  if (j0 < 48)
     {
       if (j0 < 0)
 	{
