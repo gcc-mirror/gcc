a7ba08407d911032da7df93d84c4837fa0119dd7
munroesj <munroesj@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-07-21 17:44:22 +0000

    Now completeting the mmintrin.h intrinsic headers for PowerPC by adding the DG tests.
    2017-07-21  Steven Munroe  <munroesj@gcc.gnu.org>
    
    	* gcc.target/powerpc/mmx-check.h: New file.
    	* gcc.target/powerpc/mmx-packs.c: New file.
    	* gcc.target/powerpc/mmx-packssdw-1.c: New file.
    	* gcc.target/powerpc/mmx-packsswb-1.c: New file.
    	* gcc.target/powerpc/mmx-packuswb-1.c: New file.
    	* gcc.target/powerpc/mmx-paddb-1.c: New file.
    	* gcc.target/powerpc/mmx-paddd-1.c: New file.
    	* gcc.target/powerpc/mmx-paddsb-1.c: New file.
    	* gcc.target/powerpc/mmx-paddsw-1.c: New file.
    	* gcc.target/powerpc/mmx-paddusb-1.c: New file.
    	* gcc.target/powerpc/mmx-paddusw-1.c: New file.
    	* gcc.target/powerpc/mmx-paddw-1.c: New file.
    	* gcc.target/powerpc/mmx-pcmpeqb-1.c: New file.
    	* gcc.target/powerpc/mmx-pcmpeqd-1.c: New file.
    	* gcc.target/powerpc/mmx-pcmpeqw-1.c: New file.
    	* gcc.target/powerpc/mmx-pcmpgtb-1.c: New file.
    	* gcc.target/powerpc/mmx-pcmpgtd-1.c: New file.
    	* gcc.target/powerpc/mmx-pcmpgtw-1.c: New file.
    	* gcc.target/powerpc/mmx-pmaddwd-1.c: New file.
    	* gcc.target/powerpc/mmx-pmulhw-1.c: New file.
    	* gcc.target/powerpc/mmx-pmullw-1.c: New file.
    	* gcc.target/powerpc/mmx-pslld-1.c: New file.
    	* gcc.target/powerpc/mmx-psllw-1.c: New file.
    	* gcc.target/powerpc/mmx-psrad-1.c: New file.
    	* gcc.target/powerpc/mmx-psraw-1.c: New file.
    	* gcc.target/powerpc/mmx-psrld-1.c: New file.
    	* gcc.target/powerpc/mmx-psrlw-1.c: New file.
    	* gcc.target/powerpc/mmx-psubb-2.c: New file.
    	* gcc.target/powerpc/mmx-psubd-2.c: New file.
    	* gcc.target/powerpc/mmx-psubsb-1.c: New file.
    	* gcc.target/powerpc/mmx-psubsw-1.c: New file.
    	* gcc.target/powerpc/mmx-psubusb-1.c: New file.
    	* gcc.target/powerpc/mmx-psubusw-1.c: New file.
    	* gcc.target/powerpc/mmx-psubw-2.c: New file.
    	* gcc.target/powerpc/mmx-punpckhbw-1.c: New file.
    	* gcc.target/powerpc/mmx-punpckhdq-1.c: New file.
    	* gcc.target/powerpc/mmx-punpckhwd-1.c: New file.
    	* gcc.target/powerpc/mmx-punpcklbw-1.c: New file.
    	* gcc.target/powerpc/mmx-punpckldq-1.c: New file.
    	* gcc.target/powerpc/mmx-punpcklwd-1.c: New file.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@250432 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/mmx-paddd-1.c
@@ -0,0 +1,42 @@
+/* { dg-do run } */
+/* { dg-options "-O3 -mpower8-vector" } */
+/* { dg-require-effective-target lp64 } */
+/* { dg-require-effective-target p8vector_hw } */
+
+#define NO_WARN_X86_INTRINSICS 1
+#ifndef CHECK_H
+#define CHECK_H "mmx-check.h"
+#endif
+
+#ifndef TEST
+#define TEST mmx_test
+#endif
+
+#include CHECK_H
+
+#include <mmintrin.h>
+
+static __m64
+__attribute__((noinline, unused))
+test (__m64 s1, __m64 s2)
+{
+  return _mm_add_pi32 (s1, s2);
+}
+
+static void
+TEST (void)
+{
+  __m64_union u, s1, s2;
+  __m64_union e;
+  int i;
+   
+  s1.as_m64 = _mm_set_pi32 (30, 90);
+  s2.as_m64 = _mm_set_pi32 (76, -100);
+  u.as_m64 = test (s1.as_m64, s2.as_m64);
+   
+  for (i = 0; i < 2; i++)
+     e.as_int[i] = s1.as_int[i] + s2.as_int[i];
+
+  if (u.as_m64 != e.as_m64)
+    abort ();
+}
