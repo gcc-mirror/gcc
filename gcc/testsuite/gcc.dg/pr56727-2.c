e2b71b5ac0542bb82ca190eed503853a5e3f1bdd
David Edelsohn <dje.gcc@gmail.com>
2017-07-23 17:55:19 +0000

    pr56727-2.c: Limit to powerpc-linux.
            * gcc.dg/pr56727-2.c: Limit to powerpc-linux.
            * gcc.dg/debug/dwarf2/stacked-qualified-types-3.c: Remove AIX XFAIL.
    
    From-SVN: r250462

--- gcc/testsuite/gcc.dg/pr56727-2.c
+++ gcc/testsuite/gcc.dg/pr56727-2.c
@@ -1,6 +1,6 @@
 /* { dg-do compile { target fpic } } */
 /* { dg-options "-O2 -fPIC" } */
-/* { dg-final { scan-assembler "@(PLT|plt)" { target i?86-*-* x86_64-*-* powerpc*-*-* } } } */
+/* { dg-final { scan-assembler "@(PLT|plt)" { target i?86-*-* x86_64-*-* powerpc*-*-linux* } } } */
 
 __attribute__((noinline, noclone))
 void f (short b)
