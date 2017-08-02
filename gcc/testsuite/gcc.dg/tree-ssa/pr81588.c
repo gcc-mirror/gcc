93b381af17082758758985185bd6c73e1bd6215e
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-08-02 10:27:24 +0000

    PR tree-optimization/81655 	PR tree-optimization/81588 	* gcc.dg/tree-ssa/pr81588.c: Use -mbranch-cost=2 where possible, 	don't run the test on branch-cost=1 targets.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@250819 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/pr81588.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr81588.c
@@ -1,7 +1,8 @@
 /* PR tree-optimization/81588 */
-/* { dg-do compile } */
+/* { dg-do compile { target { ! "m68k*-*-* mmix*-*-* bfin*-*-* v850*-*-* moxie*-*-* cris*-*-* m32c*-*-* fr30*-*-* mcore*-*-* powerpc*-*-* xtensa*-*-* hppa*-*-* nios2*-*-*" } } } */
 /* { dg-options "-O2 -fdump-tree-reassoc1-details" } */
- 	
+/* { dg-additional-options "-mbranch-cost=2" { target mips*-*-* avr-*-* s390*-*-* i?86-*-* x86_64-*-* } } */
+
 extern long long int a, c;
 extern unsigned short b;
 
