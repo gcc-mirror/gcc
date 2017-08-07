7641bb65d5e515b1194b9e4c502814986ce5c7dd
wilco <wilco@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-08-07 13:56:02 +0000

    Fix unresolved in gcc.dg/pr46932.c
    Build only if pre-increment is supported.  Given there is no config test,
    add a list of targets which have HAVE_PRE_INCREMENT set.
    
        testsuite/
    	PR middle-end/46932
    	* gcc.dg/pr46932.c: Compile on targets with pre-increment.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@250918 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr46932.c
+++ gcc/testsuite/gcc.dg/pr46932.c
@@ -1,5 +1,8 @@
 /* { dg-options "-O2 -fdump-rtl-auto_inc_dec" } */
 
+/* Build on targets which have pre increment.  */
+/* { dg-do compile { target aarch64*-*-* arm*-*-* rs6000-*-* powerpc*-*-* arc*-*-* m32r-*-* tic6x-*-* } } */
+
 /* Check that accesses based on the frame pointer do not
    use auto increment.  */
 
