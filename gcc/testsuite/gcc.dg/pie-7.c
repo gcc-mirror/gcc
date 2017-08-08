5a402d649a7cb96bbf93625335458b55fae05b1d
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-08-08 22:06:21 +0000

    PR driver/81523: Make -static override -pie
    -static and -pie together behave differently depending on whether GCC is
    configured with --enable-default-pie.  On x86, "-static -pie" fails to
    create executable when --enable-default-pie isn't used, but creates a
    static executable when --enable-default-pie is used.  This patch makes
    -static completely override -pie to create a static executable, regardless
    if --enable-default-pie is used to configure GCC.
    
    gcc/
    
    	PR driver/81523
    	* gcc.c (NO_PIE_SPEC): Delete.
    	(PIE_SPEC): Define as !no-pie/pie.  Move static|shared|r
    	exclusion..
    	(LINK_PIE_SPEC): ..to here.
    	(LINK_COMMAND_SPEC): Support -no-pie.
    	* config/gnu-user.h (GNU_USER_TARGET_STARTFILE_SPEC): Correct
    	chain of crtbegin*.o selection, update for PIE_SPEC changes and
    	format.
    	(GNU_USER_TARGET_ENDFILE_SPEC): Similarly.
    	* config/sol2.h (STARTFILE_CRTBEGIN_SPEC): Similarly.
    	(ENDFILE_CRTEND_SPEC): Similarly.
    
    gcc/testsuite/
    
    	PR driver/81523
    	* gcc.dg/pie-7.c: New test.
    	* gcc.dg/pie-static-1.c: Likewise.
    	* gcc.dg/pie-static-2.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@250974 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pie-7.c
@@ -0,0 +1,7 @@
+/* { dg-do run { target pie } } */
+/* { dg-options "-fno-pie -no-pie" } */
+
+int main(void)
+{
+  return 0;
+}
---
+++ gcc/testsuite/gcc.dg/pie-static-1.c
@@ -0,0 +1,7 @@
+/* { dg-do run { target pie } } */
+/* { dg-options "-static -fpie -pie" } */
+
+int main(void)
+{
+  return 0;
+}
