664d4b100525160140422a3d83e27c947c61c2b7
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-08-10 08:54:04 +0000

    PR testsuite/81784 	* gcc.dg/compare2.c: Update dg-bogus and dg-warning.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@251021 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/compare2.c
+++ gcc/testsuite/gcc.dg/compare2.c
@@ -41,18 +41,18 @@ void f(int x, unsigned int y)
   y > ({tf; tf?64:(tf?128:-1);}); /* { dg-warning "different signedness" "case 16" } */
 
   /* ?: branches are constants.  */
-  tf ? x : (tf?64:32); /* { dg-bogus "conditional expression" "case 17" } */
-  tf ? y : (tf?64:32); /* { dg-bogus "conditional expression" "case 18" } */
+  tf ? x : (tf?64:32); /* { dg-bogus "changes signedness" "case 17" } */
+  tf ? y : (tf?64:32); /* { dg-bogus "changes signedness" "case 18" } */
 
   /* ?: branches are signed constants.  */
-  tf ? x : (tf?64:-1); /* { dg-bogus "conditional expression" "case 19" } */
-  tf ? y : (tf?64:-1); /* { dg-warning "conditional expression" "case 20" } */
+  tf ? x : (tf?64:-1); /* { dg-bogus "changes signedness" "case 19" } */
+  tf ? y : (tf?64:-1); /* { dg-warning "changes signedness" "case 20" } */
 
   /* ?: branches are (recursively) constants.  */
-  tf ? x : (tf?64:(tf?128:256)); /* { dg-bogus "conditional expression" "case 21" } */
-  tf ? y : (tf?64:(tf?128:256)); /* { dg-bogus "conditional expression" "case 22" } */
+  tf ? x : (tf?64:(tf?128:256)); /* { dg-bogus "changes signedness" "case 21" } */
+  tf ? y : (tf?64:(tf?128:256)); /* { dg-bogus "changes signedness" "case 22" } */
 
   /* ?: branches are (recursively) signed constants.  */
-  tf ? x : (tf?64:(tf?128:-1)); /* { dg-bogus "conditional expression" "case 23" } */
-  tf ? y : (tf?64:(tf?128:-1)); /* { dg-warning "conditional expression" "case 24" } */
+  tf ? x : (tf?64:(tf?128:-1)); /* { dg-bogus "changes signedness" "case 23" } */
+  tf ? y : (tf?64:(tf?128:-1)); /* { dg-warning "changes signedness" "case 24" } */
 }
