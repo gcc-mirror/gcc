adc840379952b3a5649417d1d20d68f0d65e60ba
Richard Sandiford <richard.sandiford@linaro.org>
2017-08-10 19:58:16 +0000

    PR81738: Split vect-alias-check-6.c
    The second loop in the testcase only vectorises if we can reverse
    a vector and if aligned loads aren't required.
    
    2017-08-10  Richard Sandiford  <richard.sandiford@linaro.org>
    
    gcc/testsuite/
    	PR testsuite/81738
    	* gcc.dg/vect/vect-alias-check-6.c: Move second function to...
    	* gcc.dg/vect/vect-alias-check-7.c: ...this new file.  Require
    	vect_perm and vect_element_align for vectorization.
    
    From-SVN: r251037

--- gcc/testsuite/gcc.dg/vect/vect-alias-check-7.c
+++ gcc/testsuite/gcc.dg/vect/vect-alias-check-7.c
@@ -8,9 +8,9 @@ struct s { int x[N]; };
 void
 f1 (struct s *a, struct s *b)
 {
-  for (int i = 0; i < N - 1; ++i)
-    a->x[i + 1] += b->x[i];
+  for (int i = 0; i < N; ++i)
+    a->x[i] += b->x[N - i - 1];
 }
 
 /* { dg-final { scan-tree-dump {checking that [^\n]* and [^\n]* have different addresses} "vect" } } */
-/* { dg-final { scan-tree-dump "LOOP VECTORIZED" "vect" } } */
+/* { dg-final { scan-tree-dump "LOOP VECTORIZED" "vect" { target { vect_perm && vect_element_align } } } } */
