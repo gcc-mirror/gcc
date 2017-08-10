cd557ff63f388ad27c376d0a225e74d3594a6f9d
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-08-10 15:29:05 +0000

    i386: Don't use frame pointer without stack access
    When there is no stack access, there is no need to use frame pointer
    even if -fno-omit-frame-pointer is used and caller's frame pointer is
    unchanged.
    
    gcc/
    
    	PR target/81736
    	* config/i386/i386.c (ix86_finalize_stack_realign_flags): Renamed
    	to ...
    	(ix86_finalize_stack_frame_flags): This.  Also clear
    	frame_pointer_needed if -fno-omit-frame-pointer is used without
    	stack access.
    	(ix86_expand_prologue): Replace ix86_finalize_stack_realign_flags
    	with ix86_finalize_stack_frame_flags.
    	(ix86_expand_epilogue): Likewise.
    	(ix86_expand_split_stack_prologue): Likewise.
    	* doc/invoke.texi: Add a note for -fno-omit-frame-pointer.
    
    gcc/testsuite/
    
    	PR target/81736
    	* gcc.target/i386/pr81736-1.c: New test.
    	* gcc.target/i386/pr81736-2.c: Likewise.
    	* gcc.target/i386/pr81736-3.c: Likewise.
    	* gcc.target/i386/pr81736-4.c: Likewise.
    	* gcc.target/i386/pr81736-5.c: Likewise.
    	* gcc.target/i386/pr81736-6.c: Likewise.
    	* gcc.target/i386/pr81736-7.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@251028 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -14179,10 +14179,11 @@ output_probe_stack_range (rtx reg, rtx end)
   return "";
 }
 
-/* Finalize stack_realign_needed flag, which will guide prologue/epilogue
-   to be generated in correct form.  */
+/* Finalize stack_realign_needed and frame_pointer_needed flags, which
+   will guide prologue/epilogue to be generated in correct form.  */
+
 static void
-ix86_finalize_stack_realign_flags (void)
+ix86_finalize_stack_frame_flags (void)
 {
   /* Check if stack realign is really needed after reload, and
      stores result in cfun */
@@ -14205,13 +14206,13 @@ ix86_finalize_stack_realign_flags (void)
     }
 
   /* If the only reason for frame_pointer_needed is that we conservatively
-     assumed stack realignment might be needed, but in the end nothing that
-     needed the stack alignment had been spilled, clear frame_pointer_needed
-     and say we don't need stack realignment.  */
-  if (stack_realign
+     assumed stack realignment might be needed or -fno-omit-frame-pointer
+     is used, but in the end nothing that needed the stack alignment had
+     been spilled nor stack access, clear frame_pointer_needed and say we
+     don't need stack realignment.  */
+  if ((stack_realign || !flag_omit_frame_pointer)
       && frame_pointer_needed
       && crtl->is_leaf
-      && flag_omit_frame_pointer
       && crtl->sp_is_unchanging
       && !ix86_current_function_calls_tls_descriptor
       && !crtl->accesses_prior_frames
@@ -14402,7 +14403,7 @@ ix86_expand_prologue (void)
   if (ix86_function_naked (current_function_decl))
     return;
 
-  ix86_finalize_stack_realign_flags ();
+  ix86_finalize_stack_frame_flags ();
 
   /* DRAP should not coexist with stack_realign_fp */
   gcc_assert (!(crtl->drap_reg && stack_realign_fp));
@@ -15266,7 +15267,7 @@ ix86_expand_epilogue (int style)
       return;
     }
 
-  ix86_finalize_stack_realign_flags ();
+  ix86_finalize_stack_frame_flags ();
   frame = m->frame;
 
   m->fs.sp_realigned = stack_realign_fp;
@@ -15801,7 +15802,7 @@ ix86_expand_split_stack_prologue (void)
 
   gcc_assert (flag_split_stack && reload_completed);
 
-  ix86_finalize_stack_realign_flags ();
+  ix86_finalize_stack_frame_flags ();
   frame = cfun->machine->frame;
   allocate = frame.stack_pointer_offset - INCOMING_FRAME_SP_OFFSET;
 
