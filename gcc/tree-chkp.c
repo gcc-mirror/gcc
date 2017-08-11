f5f4a06fc5d3801eebe23f4615c5ac4926d381bf
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-08-11 10:01:13 +0000

    Do not instrument void variables with MPX (PR tree-opt/79987).
    2017-08-11  Martin Liska  <mliska@suse.cz>
    
    	PR tree-opt/79987
    	* tree-chkp.c (chkp_get_bounds_for_decl_addr): Do not instrument
    	variables of void type.
    2017-08-11  Martin Liska  <mliska@suse.cz>
    
    	PR tree-opt/79987
    	* gcc.target/i386/mpx/pr79987.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@251049 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-chkp.c
+++ gcc/tree-chkp.c
@@ -3197,6 +3197,9 @@ chkp_get_bounds_for_decl_addr (tree decl)
       && !flag_chkp_incomplete_type)
       return chkp_get_zero_bounds ();
 
+  if (VOID_TYPE_P (TREE_TYPE (decl)))
+    return chkp_get_zero_bounds ();
+
   if (flag_chkp_use_static_bounds
       && VAR_P (decl)
       && (TREE_STATIC (decl)
