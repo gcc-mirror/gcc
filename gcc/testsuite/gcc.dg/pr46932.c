639b2a42016a8df81684d1ed51b1ace19e0c083f
wilco <wilco@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-08-14 11:18:50 +0000

    Add check_effective_target_autoincdec.
    Add check_effective_target_autoincdec that returns true if a target
    runs the auto_inc_dec optimization pass.
    
        gcc/
    	* doc/sourcebuild.texi (autoincdec): Add autoincdec description.
    
        gcc/testsuite/
    	PR middle-end/46932
    	* gcc.dg/pr46932.c: Use dg-require-effective-target autoincdec.
    	* lib/target-supports.exp: Add check_effective_target_autoincdec.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@251087 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/pr46932.c
+++ gcc/testsuite/gcc.dg/pr46932.c
@@ -1,7 +1,5 @@
 /* { dg-options "-O2 -fdump-rtl-auto_inc_dec" } */
-
-/* Build on targets which have pre increment.  */
-/* { dg-do compile { target aarch64*-*-* arm*-*-* rs6000-*-* powerpc*-*-* arc*-*-* m32r-*-* tic6x-*-* } } */
+/* { dg-require-effective-target autoincdec } */
 
 /* Check that accesses based on the frame pointer do not
    use auto increment.  */
