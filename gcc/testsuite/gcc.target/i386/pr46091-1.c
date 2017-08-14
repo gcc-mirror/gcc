a6e6a4df68e9e2cfbf4680b7f794254bc533d27c
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-08-14 16:42:15 +0000

    PR target/46091 	* config/i386/i386.md (*anddi_1_btr): New insn_and_split pattern. 	(*iordi_1_bts): Ditto. 	(*xordi_1_btc): Ditto.
    testsuite/ChangeLog:
    
    	PR target/46091
    	* gcc.target/i386/pr46091-1.c: New test.
    	* gcc.target/i386/pr46091-2.c: Ditto.
    	* gcc.target/i386/pr46091-3.c: Ditto.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@251095 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr46091-1.c
@@ -0,0 +1,9 @@
+/* { dg-do compile { target { ! ia32 } } } */
+/* { dg-options "-O2" } */
+
+unsigned long long test (unsigned long long a)
+{
+  return a & ~(1ull << 55);
+}
+
+/* { dg-final { scan-assembler "btr" } } */
