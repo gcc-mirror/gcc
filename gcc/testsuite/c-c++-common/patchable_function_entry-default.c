7911a54d98bfdc117c2140cce0927a3755e0644d
Uros Bizjak <ubizjak@gmail.com>
2017-08-16 20:44:28 +0200

    patchable_function_entry-decl.c (dg-final): Adapt scan-assembler-times for alpha*-*-*.
    	* c-c++-common/patchable_function_entry-decl.c (dg-final): Adapt
    	scan-assembler-times for alpha*-*-*.
    	* c-c++-common/patchable_function_entry-default.c (dg-final): Ditto.
    	* c-c++-common/patchable_function_entry-definition.c (dg-final): Ditto.
    
    From-SVN: r251126

--- gcc/testsuite/c-c++-common/patchable_function_entry-default.c
+++ gcc/testsuite/c-c++-common/patchable_function_entry-default.c
@@ -1,6 +1,7 @@
 /* { dg-do compile { target { ! nvptx*-*-* } } } */
 /* { dg-options "-O2 -fpatchable-function-entry=3,1" } */
-/* { dg-final { scan-assembler-times "nop" 3 } } */
+/* { dg-final { scan-assembler-times "nop" 3 { target { ! alpha*-*-* } } } } */
+/* { dg-final { scan-assembler-times "bis" 3 { target alpha*-*-* } } } */
 
 extern int a;
 
