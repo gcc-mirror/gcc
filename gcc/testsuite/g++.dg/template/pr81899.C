5ec1701eae7178a140be621ac5aec4e40c1070b5
Nathan Sidwell <nathan@acm.org>
2017-08-21 13:29:20 +0000

    re PR c++/81899 (ICE: Segmentation fault)
    	PR c++/81899
    	* pt.c (instantiate_class_template_1):
    	BOUND_TEMPLATE_TEMPLATE_PARM is never friend-injected.
    
    	PR c++/81899
    	* g++.dg/template/pr81899.C: New.
    
    From-SVN: r251227

---
+++ gcc/testsuite/g++.dg/template/pr81899.C
@@ -0,0 +1,11 @@
+// PR 81899 we tried to treat a bound-tpl-tpl-parm as-if a real record-type
+
+template <template <typename> class FunctorData>
+struct functor {
+  friend FunctorData<int>;
+  void foo();
+};
+
+template <typename> struct data;
+
+template<> void functor<data>::foo();
