3951f00426bcdc09574e79ee1f26d3af99741ee4
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-08-25 16:31:50 +0000

    PR c++/81607 	* cp-gimplify.c (cp_fold): If folding exposed a branch of 	a COND_EXPR, convert it to the original type of the COND_EXPR, if 	they differ.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@251358 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/cp-gimplify.c
+++ gcc/cp/cp-gimplify.c
@@ -2311,9 +2311,9 @@ cp_fold (tree x)
 
       /* A COND_EXPR might have incompatible types in branches if one or both
 	 arms are bitfields.  If folding exposed such a branch, fix it up.  */
-      if (TREE_CODE (x) != code)
-	if (tree type = is_bitfield_expr_with_lowered_type (x))
-	  x = fold_convert (type, x);
+      if (TREE_CODE (x) != code
+	  && !useless_type_conversion_p (TREE_TYPE (org_x), TREE_TYPE (x)))
+	x = fold_convert (TREE_TYPE (org_x), x);
 
       break;
 
