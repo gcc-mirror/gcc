5e5ccf0d20b1104a1ec084f1de4dbbc312e9540e
Alexander Monakov <amonakov@ispras.ru>
2017-08-28 13:58:45 +0300

    optabs: ensure mem_thread_fence is a compiler barrier
    	PR target/80640
    	* doc/md.texi (mem_thread_fence): Remove mention of mode.  Rewrite.
    	* optabs.c (expand_mem_thread_fence): Emit a compiler barrier when
    	using targetm.gen_mem_thread_fence.
    testsuite/
    	* gcc.dg/atomic/pr80640.c: New testcase.
    
    From-SVN: r251377

---
+++ gcc/testsuite/gcc.dg/atomic/pr80640.c
@@ -0,0 +1,34 @@
+/* { dg-do run } */
+/* { dg-options "-pthread" } */
+/* { dg-require-effective-target pthread } */
+
+#include <pthread.h>
+
+static volatile int sem1;
+static volatile int sem2;
+
+static void *f(void *va)
+{
+  void **p = va;
+  if (*p) return *p;
+  sem1 = 1;
+  while (!sem2);
+  __atomic_thread_fence(__ATOMIC_ACQUIRE);
+  // GCC used to RTL-CSE this and the first load, causing 0 to be returned
+  return *p;
+}
+
+int main()
+{
+  void *p = 0;
+  pthread_t thr;
+  if (pthread_create(&thr, 0, f, &p))
+    return 2;
+  while (!sem1);
+  __atomic_thread_fence(__ATOMIC_ACQUIRE);
+  p = &p;
+  __atomic_thread_fence(__ATOMIC_RELEASE);
+  sem2 = 1;
+  pthread_join(thr, &p);
+  return !p;
+}
