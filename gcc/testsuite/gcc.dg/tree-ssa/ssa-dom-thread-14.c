14d6281388bad11de8c328be7ea825b184fc7efe
Jeff Law <law@redhat.com>
2017-08-28 23:03:36 -0600

    tree-ssa-dom.c (edge_info::record_simple_equiv): Call derive_equivalences.
    	* tree-ssa-dom.c (edge_info::record_simple_equiv): Call
    	derive_equivalences.
    	(derive_equivalences_from_bit_ior, record_temporary_equivalences):
    	Code moved into....
    	(edge_info::derive_equivalences): New private member function
    
    	* gcc.dg/torture/pr57214.c: Fix type of loop counter.
    	* gcc.dg/tree-ssa/ssa-sink-16.c: Disable DOM.
    	* gcc.dg/tree-ssa/ssa-dom-thread-11.c: New test.
    	* gcc.dg/tree-ssa/ssa-dom-thread-12.c: New test.
    	* gcc.dg/tree-ssa/ssa-dom-thread-13.c: New test.
    	* gcc.dg/tree-ssa/ssa-dom-thread-14.c: New test.
    	* gcc.dg/tree-ssa/ssa-dom-thread-15.c: New test.
    	* gcc.dg/tree-ssa/ssa-dom-thread-16.c: New test.
    	* gcc.dg/tree-ssa/ssa-dom-thread-17.c: New test.
    
    From-SVN: r251397

---
+++ gcc/testsuite/gcc.dg/tree-ssa/ssa-dom-thread-14.c
@@ -0,0 +1,40 @@
+/* { dg-do compile { target { ! logical_op_short_circuit  } } } */
+/* { dg-options "-O2 -fdump-tree-dom2-details -w" } */
+
+enum optab_methods
+{
+  OPTAB_DIRECT,
+  OPTAB_LIB,
+  OPTAB_WIDEN,
+  OPTAB_LIB_WIDEN,
+  OPTAB_MUST_WIDEN
+};
+struct optab_d { };
+typedef struct optab_d *optab;
+void
+expand_shift_1 (int code, int unsignedp, int rotate,
+		optab lshift_optab, optab rshift_arith_optab)
+{
+  int left = (code == 42 || code == 0xde);
+  int attempt;
+  enum optab_methods methods;
+  if (attempt == 0)
+    methods = OPTAB_DIRECT;
+  else if (attempt == 1)
+    methods = OPTAB_WIDEN;
+  if ((!unsignedp || (!left && methods == OPTAB_WIDEN)))
+    {
+      enum optab_methods methods1 = methods;
+      if (unsignedp)
+	methods1 = OPTAB_MUST_WIDEN;
+      expand_binop (left ? lshift_optab : rshift_arith_optab,
+			   unsignedp, methods1);
+    }
+}
+
+/* When UNSIGNEDP is true, LEFT is false and METHOD == OPTAB_WIDEN
+   we will enter the TRUE arm of the conditional and we can thread
+   the test to compute the first first argument of the expand_binop
+   call if we look backwards through the boolean logicals.  */
+/* { dg-final { scan-tree-dump-times "Threaded" 1 "dom2"} } */
+
