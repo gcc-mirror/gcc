626d40570ebb2575e0b7aed1a20979172d96e9ef
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-08-29 21:00:46 +0000

    libgo: fix Stat_t on AIX
        Reviewed-on: https://go-review.googlesource.com/59911
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@251436 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgo/mksysinfo.sh
+++ libgo/mksysinfo.sh
@@ -462,6 +462,7 @@ fi | sed -e 's/type _stat64/type Stat_t/' \
          -e 's/st_ctim/Ctim/' \
          -e 's/\([^a-zA-Z0-9_]\)_timeval\([^a-zA-Z0-9_]\)/\1Timeval\2/g' \
          -e 's/\([^a-zA-Z0-9_]\)_timespec_t\([^a-zA-Z0-9_]\)/\1Timespec\2/g' \
+         -e 's/\([^a-zA-Z0-9_]\)_st_timespec_t\([^a-zA-Z0-9_]\)/\1Timespec\2/g' \
          -e 's/\([^a-zA-Z0-9_]\)_timespec\([^a-zA-Z0-9_]\)/\1Timespec\2/g' \
          -e 's/\([^a-zA-Z0-9_]\)_timestruc_t\([^a-zA-Z0-9_]\)/\1Timestruc\2/g' \
          -e 's/Godump_[0-9] struct { \([^;]*;\) };/\1/g' \
