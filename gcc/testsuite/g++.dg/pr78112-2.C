36ef8e6fd289536162414baedf3ba9166850ceb2
Richard Biener <rguenther@suse.de>
2017-08-30 09:29:05 +0000

    dwarf2out.c (add_dwarf_attr): Check we don't add duplicate attributes.
    2017-08-30  Richard Biener  <rguenther@suse.de>
    
    	* dwarf2out.c (add_dwarf_attr): Check we don't add duplicate
    	attributes.
    	(gen_subprogram_die): Add DW_AT_object_pointer only early.
    	(dwarf2out_early_global_decl): Only generate a DIE for the
    	abstract origin if it doesn't already exist or is a declaration DIE.
    	(resolve_addr): Do not add the linkage name twice when
    	generating a stub DIE for the DW_TAG_GNU_call_site target.
    
    	* g++.dg/pr78112-2.C: Do not expect duplicate DW_AT_object_pointer.
    
    From-SVN: r251448

--- gcc/testsuite/g++.dg/pr78112-2.C
+++ gcc/testsuite/g++.dg/pr78112-2.C
@@ -2,7 +2,7 @@
 /* { dg-skip-if "No dwarf debug support" { hppa*-*-hpux* } } */
 /* { dg-options "-g -dA -gdwarf-4 -std=gnu++11" } */
 /* { dg-options "-g -dA -std=gnu++11 -gdwarf-4" } */
-/* { dg-final { scan-assembler-times DW_AT_object_pointer 18 } } */
+/* { dg-final { scan-assembler-times DW_AT_object_pointer 12 } } */
 
 void run (int *int_p, void(*func)(int *)) { func (int_p); }
 namespace foo {
