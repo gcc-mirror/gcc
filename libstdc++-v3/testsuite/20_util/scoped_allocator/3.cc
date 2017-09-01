8656885d533a8542d83f3f49b84bc966d179b76b
Jonathan Wakely <jwakely@redhat.com>
2017-09-01 13:15:44 +0100

    Make std::scoped_allocator_adaptor's OUTERMOST recursive
    Backport from mainline
    2016-10-06  Jonathan Wakely  <jwakely@redhat.com>
    
    	* doc/xml/manual/status_cxx2011.xml: Update status.
    	* doc/html/manual/status.html: Regenerate.
    	* include/std/scoped_allocator (__outer_allocator_t, __outermost_type):
    	New helpers for recursive OUTERMOST.
    	(__outermost): Use __outermost_type::_S_outermost.
    	(__do_outermost, scoped_allocator_adaptor::__outermost_type): Remove.
    	(scoped_allocator_adaptor::__outermost_alloc_traits): Use new
    	__outermost_type helper.
    	(scoped_allocator_adaptor::_Constructible): New alias template.
    	(scoped_allocator_adaptor::scoped_allocator_adaptor<_Outer2>):
    	Constrain template constructors.
    	* testsuite/20_util/scoped_allocator/3.cc: New test.
    	* testsuite/20_util/scoped_allocator/outermost.cc: New test.
    
    From-SVN: r251590

---
+++ libstdc++-v3/testsuite/20_util/scoped_allocator/3.cc
@@ -0,0 +1,67 @@
+// Copyright (C) 2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile }
+// { dg-options "-std=gnu++11" }
+
+#include <scoped_allocator>
+#include <testsuite_allocator.h>
+
+template<typename T>
+struct alloc
+{
+  using value_type = T;
+  alloc() = default;
+  template<typename U>
+    alloc(alloc<U>) { }
+  T* allocate(std::size_t);
+  void deallocate(T*, std::size_t);
+};
+
+template<typename T, typename U>
+  bool operator==(alloc<T>, alloc<U>) { return true; }
+
+template<typename T, typename U>
+  bool operator!=(alloc<T>, alloc<U>) { return false; }
+
+using scoped = std::scoped_allocator_adaptor<alloc<int>>;
+using other_alloc = __gnu_test::SimpleAllocator<int>;
+using other_scoped = std::scoped_allocator_adaptor<other_alloc>;
+
+using std::is_constructible;
+
+static_assert( is_constructible<scoped, const scoped&>::value,
+    "is_constructible<scoped, const scoped&>");
+static_assert( is_constructible<scoped, scoped>::value,
+    "is_constructible<scoped, scoped>");
+static_assert( is_constructible<scoped, const alloc<int>&>::value,
+    "is_constructible<scoped, const outer_allocator_type&>");
+static_assert( is_constructible<scoped, alloc<int>>::value,
+    "is_constructible<scoped, outer_allocator_type>");
+static_assert( is_constructible<scoped, const alloc<long>&>::value,
+    "is_constructible<scoped, const outer_allocator_type::rebind<U>::type&>");
+static_assert( is_constructible<scoped, alloc<long>>::value,
+    "is_constructible<scoped, outer_allocator_type::rebind<U>::type>");
+
+static_assert( !is_constructible<scoped, const other_alloc&>::value,
+    "!is_constructible<scoped, const other_alloc&>");
+static_assert( !is_constructible<scoped, other_alloc>::value,
+    "!is_constructible<scoped, other_alloc>");
+static_assert( !is_constructible<scoped, const other_scoped&>::value,
+    "!is_constructible<scoped, const other_scoped&>");
+static_assert( !is_constructible<scoped, other_scoped>::value,
+    "!is_constructible<scoped, other_scoped>");
