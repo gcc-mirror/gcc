de6b11059432f02e422397d75ff89bd5778cd203
charlet <charlet@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-09-06 09:55:11 +0000

    Fix typo.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@251764 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/exp_disp.ads
+++ gcc/ada/exp_disp.ads
@@ -4,9 +4,9 @@
 --                                                                          --
 --                             E X P _ D I S P                              --
 --                                                                          --
---                                 GS p e c                                  --
+--                                 S p e c                                  --
 --                                                                          --
---          Copyright (C) 1992-2015, Free Software Foundation, Inc.         --
+--          Copyright (C) 1992-2017, Free Software Foundation, Inc.         --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
