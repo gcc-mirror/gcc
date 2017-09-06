796bd467fd9b9b60eaafbc6b837466034305b504
Richard Biener <rguenther@suse.de>
2017-09-06 12:31:52 +0000

    re PR target/82108 (Wrong vectorized code generated for x86_64)
    2017-09-06  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/82108
    	* tree-vect-stmts.c (vectorizable_load): Fix pointer adjustment
    	for gap in the non-permutation SLP case.
    
    	* gcc.dg/vect/pr82108.c: New testcase.
    
    From-SVN: r251790

---
+++ gcc/testsuite/gcc.dg/vect/pr82108.c
@@ -0,0 +1,47 @@
+/* { dg-do run } */
+/* { dg-require-effective-target vect_float } */
+
+#include "tree-vect.h"
+
+void __attribute__((noinline,noclone))
+downscale_2 (const float* src, int src_n, float* dst)
+{
+  int i;
+
+  for (i = 0; i < src_n; i += 2) {
+      const float* a = src;
+      const float* b = src + 4;
+
+      dst[0] = (a[0] + b[0]) / 2;
+      dst[1] = (a[1] + b[1]) / 2;
+      dst[2] = (a[2] + b[2]) / 2;
+      dst[3] = (a[3] + b[3]) / 2;
+
+      src += 2 * 4;
+      dst +=     4;
+  }
+}
+
+int main ()
+{
+  const float in[4 * 4] = {
+      1, 2, 3, 4,
+      5, 6, 7, 8,
+
+      1, 2, 3, 4,
+      5, 6, 7, 8
+  };
+  float out[2 * 4];
+
+  check_vect ();
+
+  downscale_2 (in, 4, out);
+
+  if (out[0] != 3 || out[1] != 4 || out[2] != 5 || out[3] != 6
+      || out[4] != 3 || out[5] != 4 || out[6] != 5 || out[7] != 6)
+    __builtin_abort ();
+  
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } } */
