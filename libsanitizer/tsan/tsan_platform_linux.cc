cf35756c0ec1b30734faedd6a307a1d32ca19d1f
Matthias Klose <doko@ubuntu.com>
2017-09-07 07:15:24 +0000

    backport: re PR sanitizer/81066 (sanitizer_stoptheworld_linux_libcdep.cc:276:22: error: aggregate ‘sigaltstack handler_stack’ has incomplete type and cannot be defined)
    2017-09-07  Matthias Klose  <doko@ubuntu.com>
    
            Backported from mainline
            2017-07-14  Jakub Jelinek  <jakub@redhat.com>
    
            PR sanitizer/81066
            * sanitizer_common/sanitizer_linux.h: Cherry-pick upstream r307969.
            * sanitizer_common/sanitizer_linux.cc: Likewise.
            * sanitizer_common/sanitizer_stoptheworld_linux_libcdep.cc: Likewise.
            * tsan/tsan_platform_linux.cc: Likewise.
    
    From-SVN: r251828

--- libsanitizer/tsan/tsan_platform_linux.cc
+++ libsanitizer/tsan/tsan_platform_linux.cc
@@ -291,7 +291,7 @@ bool IsGlobalVar(uptr addr) {
 int ExtractResolvFDs(void *state, int *fds, int nfd) {
 #if SANITIZER_LINUX
   int cnt = 0;
-  __res_state *statp = (__res_state*)state;
+  struct __res_state *statp = (struct __res_state*)state;
   for (int i = 0; i < MAXNS && cnt < nfd; i++) {
     if (statp->_u._ext.nsaddrs[i] && statp->_u._ext.nssocks[i] != -1)
       fds[cnt++] = statp->_u._ext.nssocks[i];
