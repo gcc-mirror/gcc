53e7178da50ddf11bcb2fb8f2a15cb90844b9129
Jonathan Wakely <jwakely@redhat.com>
2017-09-12 17:27:09 +0100

    PR libstdc++/70483 make std::string_view fully constexpr
    Backport from mainline
    2017-09-11  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/70483
    	* include/bits/string_view.tcc (basic_string_view::find)
    	(basic_string_view::rfind, basic_string_view::find_first_of)
    	(basic_string_view::find_last_of, basic_string_view::find_first_not_of)
    	(basic_string_view::find_last_not_of): Add constexpr specifier.
    	* include/std/string_view (basic_string_view::operator=)
    	(basic_string_view::rbegin, basic_string_view::rend)
    	(basic_string_view::crbegin, basic_string_view::crend)
    	(basic_string_view::remove_prefix, basic_string_view::remove_suffix)
    	(basic_string_view::swap, basic_string_view::compare)
    	(basic_string_view::find, basic_string_view::rfind)
    	(basic_string_view::find_first_of, basic_string_view::find_last_of)
    	(basic_string_view::find_first_not_of)
    	(basic_string_view::find_last_not_of, basic_string_view::_M_check)
    	(basic_string_view::_M_limit, operator==, operator!=, operator<)
    	(operator>, operator<=, operator>=): Likewise.
    	* testsuite/21_strings/basic_string_view/modifiers/remove_prefix/
    	char/1.cc: Repeat tests in constexpr context.
    	* testsuite/21_strings/basic_string_view/modifiers/remove_prefix/
    	wchar_t/1.cc: Likewise.
    	* testsuite/21_strings/basic_string_view/modifiers/remove_suffix/
    	char/1.cc: Likewise.
    	* testsuite/21_strings/basic_string_view/modifiers/remove_suffix/
    	wchar_t/1.cc: Likewise.
    	* testsuite/21_strings/basic_string_view/operations/find/char/1.cc:
    	Likewise.
    	* testsuite/21_strings/basic_string_view/operations/find/char/2.cc:
    	Likewise.
    	* testsuite/21_strings/basic_string_view/operations/find/char/3.cc:
    	Likewise.
    	* testsuite/21_strings/basic_string_view/operations/find/wchar_t/1.cc:
    	Likewise.
    	* testsuite/21_strings/basic_string_view/operations/find/wchar_t/2.cc:
    	Likewise.
    	* testsuite/21_strings/basic_string_view/operations/find/wchar_t/3.cc:
    	Likewise.
    	* testsuite/21_strings/basic_string_view/operators/char/2.cc:
    	Likewise.
    	* testsuite/21_strings/basic_string_view/operators/wchar_t/2.cc:
    	Likewise.
    	* testsuite/21_strings/basic_string_view/range_access/char/1.cc: Test
    	cbegin, cend, rbegin, rend, crbegin and crend.
    	* testsuite/21_strings/basic_string_view/range_access/wchar_t/1.cc:
    	Likewise.
    	* testsuite/21_strings/basic_string_view/operations/compare/char/1.cc:
    	Remove trailing whitespace.
    	* testsuite/21_strings/basic_string_view/operations/compare/wchar_t/
    	1.cc: Likewise.
    	* testsuite/21_strings/basic_string_view/modifiers/swap/char/1.cc:
    	New.
    	* testsuite/21_strings/basic_string_view/modifiers/swap/wchar_t/1.cc:
    	New.
    	* testsuite/21_strings/basic_string_view/operations/compare/char/2.cc:
    	New.
    	* testsuite/21_strings/basic_string_view/operations/compare/wchar_t/
    	2.cc: New.
    
    From-SVN: r252031

--- libstdc++-v3/testsuite/21_strings/basic_string_view/operations/find/wchar_t/2.cc
+++ libstdc++-v3/testsuite/21_strings/basic_string_view/operations/find/wchar_t/2.cc
@@ -59,13 +59,13 @@ test02()
   // An empty string_view consists of no characters
   // therefore it should be found at every point in a string_view,
   // except beyond the end
-  // However, str1.find_first_of(str2,pos) finds the first character in 
+  // However, str1.find_first_of(str2,pos) finds the first character in
   // str1 (starting at pos) that exists in str2, which is none for empty str2
   csz01 = str01.find_first_of(str04, 0);
   VERIFY( csz01 == npos );
   csz01 = str01.find_first_of(str04, 5);
   VERIFY( csz01 == npos );
-  
+
   // size_type find_first_of(const wchar_t* s, size_type pos, size_type n) const;
   csz01 = str01.find_first_of(str_lit01, 0, 3);
   VERIFY( csz01 == 0 );
@@ -84,10 +84,78 @@ test02()
   VERIFY( csz01 == csz02 );
 }
 
+constexpr bool
+test04()
+{
+  typedef std::wstring_view::size_type csize_type;
+  csize_type npos = std::wstring_view::npos;
+  csize_type csz01 = 0, csz02 = 0;
+
+  const wchar_t str_lit01[] = L"mave";
+  const std::wstring_view str01(L"mavericks, santa cruz");
+  std::wstring_view str02(str_lit01);
+  std::wstring_view str03(L"s, s");
+  std::wstring_view str04;
+
+#undef VERIFY
+#define VERIFY(x) if(!(x)) return false
+
+  // size_type find_first_of(const wstring_view&, size_type pos = 0) const;
+  std::wstring_view str05(L"xena rulez");
+  csz01 = str01.find_first_of(str01);
+  VERIFY( csz01 == 0 );
+  csz01 = str01.find_first_of(str01, 4);
+  VERIFY( csz01 == 4 );
+  csz01 = str01.find_first_of(str02, 0);
+  VERIFY( csz01 == 0 );
+  csz01 = str01.find_first_of(str02, 3);
+  VERIFY( csz01 == 3 );
+  csz01 = str01.find_first_of(str03, 0);
+  VERIFY( csz01 == 8 );
+  csz01 = str01.find_first_of(str03, 3);
+  VERIFY( csz01 == 8 );
+  csz01 = str01.find_first_of(str03, 12);
+  VERIFY( csz01 == 16 );
+  csz01 = str01.find_first_of(str05, 0);
+  VERIFY( csz01 == 1 );
+  csz01 = str01.find_first_of(str05, 4);
+  VERIFY( csz01 == 4 );
+
+  // An empty string_view consists of no characters
+  // therefore it should be found at every point in a string_view,
+  // except beyond the end
+  // However, str1.find_first_of(str2,pos) finds the first character in
+  // str1 (starting at pos) that exists in str2, which is none for empty str2
+  csz01 = str01.find_first_of(str04, 0);
+  VERIFY( csz01 == npos );
+  csz01 = str01.find_first_of(str04, 5);
+  VERIFY( csz01 == npos );
+
+  // size_type find_first_of(const wchar_t* s, size_type pos, size_type n) const;
+  csz01 = str01.find_first_of(str_lit01, 0, 3);
+  VERIFY( csz01 == 0 );
+  csz01 = str01.find_first_of(str_lit01, 3, 0);
+  VERIFY( csz01 == npos );
+
+  // size_type find_first_of(const wchar_t* s, size_type pos = 0) const;
+  csz01 = str01.find_first_of(str_lit01);
+  VERIFY( csz01 == 0 );
+  csz01 = str01.find_first_of(str_lit01, 3);
+  VERIFY( csz01 == 3 );
+
+  // size_type find_first_of(wchar_t c, size_type pos = 0) const;
+  csz01 = str01.find_first_of(L'z');
+  csz02 = str01.size() - 1;
+  VERIFY( csz01 == csz02 );
+
+  return true;
+}
+
 int
 main()
-{ 
+{
   test02();
+  static_assert( test04() );
 
   return 0;
 }
