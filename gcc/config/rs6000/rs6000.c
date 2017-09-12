2afb2668ad15d17d225e97856354bf15041e5316
Carl Love <cel@us.ibm.com>
2017-09-12 15:14:32 +0000

    altivec.md (vec_widen_umult_even_v4si, [...]): Add define expands for vmuleuw, vmulesw, vmulouw, vmulosw.
    gcc/ChangeLog:
    
    2017-09-12  Carl Love  <cel@us.ibm.com>
    
            * config/rs6000/altivec.md (vec_widen_umult_even_v4si,
    	vec_widen_smult_even_v4si): Add define expands for vmuleuw, vmulesw,
    	vmulouw, vmulosw.
            * config/rs6000/rs6000-builtin.def (VMLEUW, VMULESW, VMULOUW,
            VMULOSW): Add definitions.
            * config/rs6000/rs6000-c.c (altivec_overloaded_builtins): Add
            ALTIVEC_BUILTIN_VMULESW, ALTIVEC_BUILTIN_VMULEUW,
            ALTIVEC_BUILTIN_VMULOSW, ALTIVEC_BUILTIN_VMULOUW entries.
            * config/rs6000/rs6000.c (rs6000_gimple_fold_builtin,
            builtin_function_type): Add ALTIVEC_BUILTIN_* case statements.
    
    From-SVN: r252027

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -16242,9 +16242,11 @@ rs6000_gimple_fold_builtin (gimple_stmt_iterator *gsi)
     /* Even element flavors of vec_mul (signed). */
     case ALTIVEC_BUILTIN_VMULESB:
     case ALTIVEC_BUILTIN_VMULESH:
+    case ALTIVEC_BUILTIN_VMULESW:
     /* Even element flavors of vec_mul (unsigned).  */
     case ALTIVEC_BUILTIN_VMULEUB:
     case ALTIVEC_BUILTIN_VMULEUH:
+    case ALTIVEC_BUILTIN_VMULEUW:
       {
 	arg0 = gimple_call_arg (stmt, 0);
 	arg1 = gimple_call_arg (stmt, 1);
@@ -16257,9 +16259,11 @@ rs6000_gimple_fold_builtin (gimple_stmt_iterator *gsi)
     /* Odd element flavors of vec_mul (signed).  */
     case ALTIVEC_BUILTIN_VMULOSB:
     case ALTIVEC_BUILTIN_VMULOSH:
+    case ALTIVEC_BUILTIN_VMULOSW:
     /* Odd element flavors of vec_mul (unsigned). */
     case ALTIVEC_BUILTIN_VMULOUB:
     case ALTIVEC_BUILTIN_VMULOUH:
+    case ALTIVEC_BUILTIN_VMULOUW:
       {
 	arg0 = gimple_call_arg (stmt, 0);
 	arg1 = gimple_call_arg (stmt, 1);
