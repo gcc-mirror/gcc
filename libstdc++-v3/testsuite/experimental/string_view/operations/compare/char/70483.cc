80a5b96b44dce1dde2dbad5c4ec83d4389988e7c
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-09-12 16:27:14 +0000

    PR libstdc++/70483 make std::experimental::string_view fully constexpr
    Backport from mainline
    2017-09-12  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/70483
    	* include/experimental/bits/string_view.tcc (basic_string_view::find)
    	(basic_string_view::rfind, basic_string_view::find_first_of)
    	(basic_string_view::find_last_of, basic_string_view::find_first_not_of)
    	(basic_string_view::find_last_not_of): Add constexpr specifier.
    	* include/experimental/string_view (basic_string_view::remove_prefix)
    	(basic_string_view::remove_suffix, basic_string_view::swap)
    	(basic_string_view::compare, basic_string_view::find)
    	(basic_string_view::rfind, basic_string_view::find_first_of)
    	(basic_string_view::find_last_of, basic_string_view::find_first_not_of)
    	(basic_string_view::find_last_not_of, operator==, operator!=)
    	(operator<, operator>, operator<=, operator>=): Likewise.
    	* testsuite/experimental/string_view/operations/compare/char/70483.cc:
    	New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@252032 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/experimental/string_view/operations/compare/char/70483.cc
@@ -0,0 +1,68 @@
+// Copyright (C) 2017 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile { target c++14 } }
+
+#include <experimental/string_view>
+
+struct constexpr_char_traits : std::char_traits<char>
+{
+  static constexpr size_t
+  length(const char* val)
+  {
+    size_t res = 0;
+    for (; val[res] != '\0'; ++res)
+      ;
+    return res;
+  }
+
+  static constexpr int
+  compare(const char* lhs, const char* rhs, std::size_t count)
+  {
+    for (size_t pos = 0; pos < count; ++pos)
+    {
+      if (lhs[pos] != rhs[pos])
+        return lhs[pos] - rhs[pos];
+    }
+    return 0;
+  }
+
+  static constexpr const char*
+  find(const char* p, std::size_t n, char c)
+  {
+    for (size_t pos = 0; pos < n; ++pos)
+      if (p[pos] == c)
+	return p + pos;
+    return nullptr;
+  }
+};
+
+using string_view
+  = std::experimental::basic_string_view<char, constexpr_char_traits>;
+
+constexpr
+string_view get()
+{
+    string_view res = "x::";
+    string_view start_pattern = "x";
+    res = res.substr(res.find(start_pattern) + start_pattern.size());
+    res = res.substr(0, res.find_first_of(";]"));
+    res = res.substr(res.rfind("::"));
+    return res;
+}
+
+static_assert( get() == get() );
