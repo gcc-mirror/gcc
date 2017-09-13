ddeff578df9f92083f397f03afc6e5ea3cb4c11a
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-09-13 14:07:44 +0000

    PR libstdc++/81468 constrain std::chrono::time_point constructor
    	PR libstdc++/81468
    	* include/std/chrono (__enable_if_is_duration)
    	(__disable_if_is_duration): New alias templates to simplify SFINAE.
    	(duration_cast, floor, ceil): Use __enable_if_is_duration.
    	(duration::__is_float, duration::__is_harmonic): New alias templates
    	to simplify SFINAE.
    	(duration::duration(const _Rep2&)): Use _Require, __is_float and
    	__is_harmonic.
    	(duration::duration(const duration<_Rep2, _Period2>&)): Likewise.
    	(__common_rep_type): Remove, replace with ...
    	(__common_rep_t): New alias template.
    	(operator*, operator/, operator%): Use __common_rep_t and
    	__disable_if_is_duration.
    	(time_point::time_point(const time_point<clock, _Dur2>&)): Add missing
    	constraint from LWG DR 1177.
    	* testsuite/20_util/duration/cons/dr1177.cc: New.
    	* testsuite/20_util/duration/literals/range.cc: Update dg-error line.
    	* testsuite/20_util/duration/requirements/typedefs_neg1.cc: Likewise.
    	* testsuite/20_util/duration/requirements/typedefs_neg2.cc: Likewise.
    	* testsuite/20_util/duration/requirements/typedefs_neg3.cc: Likewise.
    	* testsuite/20_util/time_point/cons/81468.cc: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@252085 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/20_util/duration/cons/dr1177.cc
@@ -0,0 +1,41 @@
+// Copyright (C) 2017 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile { target c++11 } }
+
+#include <chrono>
+#include <type_traits>
+
+using namespace std;
+using namespace std::chrono;
+
+// DR 1177
+static_assert(is_constructible<duration<float>, duration<double>>{},
+    "can convert duration with one floating point rep to another");
+static_assert(is_constructible<duration<float>, duration<int>>{},
+    "can convert duration with integral rep to one with floating point rep");
+static_assert(!is_constructible<duration<int>, duration<float>>{},
+    "cannot convert duration with floating point rep to one with integral rep");
+static_assert(is_constructible<duration<int>, duration<long>>{},
+    "can convert duration with one integral rep to another");
+
+static_assert(!is_constructible<duration<int>, duration<int, ratio<2,3>>>{},
+    "cannot convert duration to one with different period");
+static_assert(is_constructible<duration<float>, duration<int, ratio<2,3>>>{},
+    "unless it has a floating-point representation");
+static_assert(is_constructible<duration<float>, duration<int, ratio<1,3>>>{},
+    "or a period that is an integral multiple of the original");
