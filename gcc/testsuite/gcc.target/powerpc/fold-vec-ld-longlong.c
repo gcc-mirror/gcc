2a94be35291a8564154cd213f73b7e302e3f3b80
Will Schmidt <will_schmidt@vnet.ibm.com>
2017-09-14 13:56:05 +0000

    fold-vec-ld-longlong.c: Add lp64 requirement.
    [gcc/testsuite]
    
    2017-09-14  Will Schmidt  <will_schmidt@vnet.ibm.com>
    
    	* gcc.target/powerpc/fold-vec-ld-longlong.c: Add lp64 requirement.
    
    From-SVN: r252757

--- gcc/testsuite/gcc.target/powerpc/fold-vec-ld-longlong.c
+++ gcc/testsuite/gcc.target/powerpc/fold-vec-ld-longlong.c
@@ -1,7 +1,7 @@
 /* Verify that overloaded built-ins for vec_ld* with long long
    inputs produce the right code.  */
 
-/* { dg-do compile } */
+/* { dg-do compile { target lp64 } } */
 /* { dg-require-effective-target powerpc_p8vector_ok } */
 /* { dg-options "-mpower8-vector -O2" } */
 
