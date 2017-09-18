27017060ac56077cfe583e946c1437a101606239
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-09-18 18:36:47 +0000

    PR c++/81525 - wrong constant value with generic lambda
    	* pt.c (tsubst_decl) [VAR_DECL]: Avoid clobbering auto.
    	(tsubst_copy) [VAR_DECL]: Handle auto.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@252941 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp1y/lambda-generic-const4.C
@@ -0,0 +1,20 @@
+// PR c++/81525
+// { dg-do compile { target c++14 } }
+
+template <int i> struct A {
+  constexpr operator int () const { return i; }
+};
+template <int i> constexpr A<i> a = {};
+
+template <typename F> void foo (F f) {
+  f (A<0>{});
+}
+template <typename T>
+void bar (T) {
+  constexpr auto N = a<1>;
+  auto f = [&] (auto i) {
+    static_assert (static_cast<int>(N) == 1, "");
+  };
+  foo (f);
+}
+int main () { bar (0); }
