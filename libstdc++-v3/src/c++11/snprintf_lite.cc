075223648e884b11781dd69f7f73db57fba3b6f6
Jonathan Wakely <jwakely@redhat.com>
2017-09-20 13:01:44 +0100

    PR libstdc++/60936 fix length calculation
    Backport from mainline
    2017-02-16  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/60936
    	* src/c++11/snprintf_lite.cc (__concat_size_t): Calculate length
    	written to buffer, not length remaining in buffer.
    
    Backport from mainline
    2017-02-08  Gerald Pfeifer  <gerald@pfeifer.com>
    
    	* src/c++11/snprintf_lite.cc (__err): Update bug reporting URL.
    
    From-SVN: r253007

--- libstdc++-v3/src/c++11/snprintf_lite.cc
+++ libstdc++-v3/src/c++11/snprintf_lite.cc
@@ -52,7 +52,7 @@ namespace __gnu_cxx {
     const size_t __len = __bufend - __buf + 1;
 
     const char __err[] = "not enough space for format expansion "
-      "(Please submit full bug report at http://gcc.gnu.org/bugs.html):\n    ";
+      "(Please submit full bug report at https://gcc.gnu.org/bugs/):\n    ";
     const size_t __errlen = sizeof(__err) - 1;
 
     char *const __e
@@ -81,7 +81,7 @@ namespace __gnu_cxx {
 	__val /= 10;
       }
     while (__val != 0);
-    size_t __len = __out - __cs;
+    size_t __len = __cs + __ilen - __out;
     if (__bufsize < __len)
       return -1;
 
