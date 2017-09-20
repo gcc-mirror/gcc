bb66276b13450ade9eb08661fadfad4301542a47
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-09-20 22:26:26 +0000

    Fix libstdc++ testsuite failures with -D_GLIBCXX_USE_CXX11_ABI=0
    Backport from mainline
    2017-06-14  Jonathan Wakely  <jwakely@redhat.com>
    
    	* doc/xml/manual/test.xml: Correct instructions on running tests.
    	* testsuite/27_io/basic_ios/copyfmt/char/1.cc: Adjust to pass when
    	-D_GLIBCXX_USE_CXX11_ABI=0 added to RUNTESTFLAGS.
    	* testsuite/27_io/basic_ios/exceptions/char/1.cc: Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/
    	exceptions_failbit.cc: Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/wchar_t/
    	exceptions_failbit.cc: Likewise.
    	* testsuite/27_io/basic_istream/extractors_other/char/
    	exceptions_null.cc: Likewise.
    	* testsuite/27_io/basic_istream/extractors_other/wchar_t/
    	exceptions_null.cc: Likewise.
    	* testsuite/27_io/basic_istream/sentry/char/12297.cc: Likewise.
    	* testsuite/27_io/basic_istream/sentry/wchar_t/12297.cc: Likewise.
    	* testsuite/27_io/basic_ostream/inserters_other/char/
    	exceptions_null.cc: Likewise.
    	* testsuite/27_io/basic_ostream/inserters_other/wchar_t/
    	exceptions_null.cc: Likewise.
    	* testsuite/27_io/ios_base/storage/2.cc: Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@253039 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/27_io/basic_ios/copyfmt/char/1.cc
+++ libstdc++-v3/testsuite/27_io/basic_ios/copyfmt/char/1.cc
@@ -17,9 +17,6 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// The library throws the new definition of std::ios::failure
-// { dg-options "-D_GLIBCXX_USE_CXX11_ABI=1" }
-
 // 27.4.4.2 basic_ios member functions
 
 // NB: Don't include any other headers in this file.
@@ -36,29 +33,36 @@ void test02()
   // basic_ios& copyfmt(const basic_ios& rhs)
   {
     std::ios ios_01(0);
-    std::ios ios_02(0);  
+    std::ios ios_02(0);
     ios_01.exceptions(std::ios_base::eofbit);
     ios_02.exceptions(std::ios_base::eofbit);
-    
+
     try {
-    ios_01.copyfmt(ios_02);
-    }		 
+      ios_01.copyfmt(ios_02);
+    }
     catch(...) {
       VERIFY( false );
     }
   }
 
   {
+    // The library throws the new definition of std::ios::failure
+#if _GLIBCXX_USE_CXX11_ABI
+    typedef std::ios_base::failure exception_type;
+#else
+    typedef std::exception exception_type;
+#endif
+
     std::ios ios_01(0);
-    std::ios ios_02(0);  
+    std::ios ios_02(0);
     ios_01.clear(std::ios_base::eofbit);
     ios_02.exceptions(std::ios_base::eofbit);
 
     try {
       ios_01.copyfmt(ios_02);
       VERIFY( false );
-    }		 
-    catch(std::ios_base::failure& fail) {
+    }
+    catch(exception_type&) {
       VERIFY( true );
     }
     catch(...) {
@@ -67,7 +71,7 @@ void test02()
   }
 }
 
-int main() 
+int main()
 {
   test02();
   return 0;
