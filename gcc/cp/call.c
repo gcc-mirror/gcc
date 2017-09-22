9003adc732305c69346b8ae5699a250c033c31c1
David Malcolm <dmalcolm@redhat.com>
2017-09-22 14:49:52 +0000

    C++: underline parameters in mismatching function calls
    gcc/cp/ChangeLog:
    	* call.c (get_fndecl_argument_location): New function.
    	(convert_like_real): Use it  when complaining about argument type
    	mismatches.
    	* cp-tree.h (struct cp_parameter_declarator): Add "loc" field.
    	* parser.c (make_parameter_declarator): Add "loc" param and use
    	it to initialize the new field.
    	(cp_parser_translation_unit): Add UNKNOWN_LOCATION for "loc" of
    	the "no_parameters" parameter.
    	(cp_parser_parameter_declaration_list): Set the location of the
    	result of grokdeclarator to be the parameter's loc, assuming no
    	errors.
    	(cp_parser_parameter_declaration): Generate a location for the
    	parameter and pass to make_parameter_declarator.
    
    gcc/testsuite/ChangeLog:
    	* g++.dg/diagnostic/param-type-mismatch.C: Update expected results
    	to reflect highlighting of parameters; add test coverage for
    	callback parameters.
    
    From-SVN: r253096

--- gcc/cp/call.c
+++ gcc/cp/call.c
@@ -6579,6 +6579,30 @@ maybe_print_user_conv_context (conversion *convs)
 	}
 }
 
+/* Locate the parameter with the given index within FNDECL.
+   ARGNUM is zero based, -1 indicates the `this' argument of a method.
+   Return the location of the FNDECL itself if there are problems.  */
+
+static location_t
+get_fndecl_argument_location (tree fndecl, int argnum)
+{
+  int i;
+  tree param;
+
+  /* Locate param by index within DECL_ARGUMENTS (fndecl).  */
+  for (i = 0, param = FUNCTION_FIRST_USER_PARM (fndecl);
+       i < argnum && param;
+       i++, param = TREE_CHAIN (param))
+    ;
+
+  /* If something went wrong (e.g. if we have a builtin and thus no arguments),
+     return the location of FNDECL.  */
+  if (param == NULL)
+    return DECL_SOURCE_LOCATION (fndecl);
+
+  return DECL_SOURCE_LOCATION (param);
+}
+
 /* Perform the conversions in CONVS on the expression EXPR.  FN and
    ARGNUM are used for diagnostics.  ARGNUM is zero based, -1
    indicates the `this' argument of a method.  INNER is nonzero when
@@ -6680,7 +6704,7 @@ convert_like_real (conversion *convs, tree expr, tree fn, int argnum,
 	complained = permerror (loc, "invalid conversion from %qH to %qI",
 				TREE_TYPE (expr), totype);
       if (complained && fn)
-	inform (DECL_SOURCE_LOCATION (fn),
+	inform (get_fndecl_argument_location (fn, argnum),
 		"  initializing argument %P of %qD", argnum, fn);
 
       return cp_convert (totype, expr, complain);
