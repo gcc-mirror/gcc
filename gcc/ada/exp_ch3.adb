f7fb5c08f36ea1c1aeebe1710839d1c0d5e68674
Pierre-Marie de Rodat <pmderodat@gcc.gnu.org>
2017-09-25 08:46:40 +0000

    [multiple changes]
    2017-09-25  Bob Duff  <duff@adacore.com>
    
    	* exp_ch3.adb: Rename Comp_Type_Simple to be Comp_Simple_Init.
    
    2017-09-25  Doug Rupp  <rupp@adacore.com>
    
    	* libgnarl/s-taprop__linux.adb (Base_Monotonic_Clock): New variable.
    	(Compute_Base_Monotonic_Clock): New function.
    	(Timed_Sleep): Adjust to use Base_Monotonic_Clock.
    	(Timed_Delay): Likewise.
    	(Monotonic_Clock): Likewise.
    	* s-oscons-tmplt.c (CLOCK_MONOTONIC): Use on Linux.
    
    From-SVN: r253136

--- gcc/ada/exp_ch3.adb
+++ gcc/ada/exp_ch3.adb
@@ -517,11 +517,16 @@ package body Exp_Ch3 is
 
    procedure Build_Array_Init_Proc (A_Type : Entity_Id; Nod : Node_Id) is
       Comp_Type        : constant Entity_Id := Component_Type (A_Type);
-      Comp_Type_Simple : constant Boolean   :=
+      Comp_Simple_Init : constant Boolean   :=
         Needs_Simple_Initialization
           (T           => Comp_Type,
            Consider_IS =>
              not (Validity_Check_Copies and Is_Bit_Packed_Array (A_Type)));
+      --  True if the component needs simple initialization, based on its type,
+      --  plus the fact that we do not do simple initialization for components
+      --  of bit-packed arrays when validity checks are enabled, because the
+      --  initialization with deliberately out-of-range values would raise
+      --  Constraint_Error.
 
       Body_Stmts       : List_Id;
       Has_Default_Init : Boolean;
@@ -563,7 +568,7 @@ package body Exp_Ch3 is
                   Convert_To (Comp_Type,
                     Default_Aspect_Component_Value (First_Subtype (A_Type)))));
 
-         elsif Comp_Type_Simple then
+         elsif Comp_Simple_Init then
             Set_Assignment_OK (Comp);
             return New_List (
               Make_Assignment_Statement (Loc,
@@ -595,7 +600,7 @@ package body Exp_Ch3 is
          --  the dummy Init_Proc needed for Initialize_Scalars processing.
 
          if not Has_Non_Null_Base_Init_Proc (Comp_Type)
-           and then not Comp_Type_Simple
+           and then not Comp_Simple_Init
            and then not Has_Task (Comp_Type)
            and then not Has_Default_Aspect (A_Type)
          then
@@ -685,7 +690,7 @@ package body Exp_Ch3 is
       --  init_proc.
 
       Has_Default_Init := Has_Non_Null_Base_Init_Proc (Comp_Type)
-                            or else Comp_Type_Simple
+                            or else Comp_Simple_Init
                             or else Has_Task (Comp_Type)
                             or else Has_Default_Aspect (A_Type);
 
