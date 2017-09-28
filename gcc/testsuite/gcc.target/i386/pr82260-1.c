ff2994877bfa4145e355d1ad546cab769843830a
Jakub Jelinek <jakub@redhat.com>
2017-09-28 20:35:58 +0200

    re PR target/82342 (i386/pr82260-2.c fail)
    	PR target/82342
    	* gcc.target/i386/pr82260-1.c: Add -mno-bmi2 to dg-options.
    	* gcc.target/i386/pr82260-2.c: Likewise.
    
    From-SVN: r253261

--- gcc/testsuite/gcc.target/i386/pr82260-1.c
+++ gcc/testsuite/gcc.target/i386/pr82260-1.c
@@ -1,6 +1,6 @@
 /* PR target/82260 */
 /* { dg-do compile { target lp64 } } */
-/* { dg-options "-Os -mtune=generic -masm=att" } */
+/* { dg-options "-Os -mtune=generic -masm=att -mno-bmi2" } */
 /* movl %esi, %ecx is shorter than movb %sil, %cl.  While
    movl %edx, %ecx is the same size as movb %dl, %cl and
    movl %r8d, %ecx is the same size as movb %r8b, %cl, movl
