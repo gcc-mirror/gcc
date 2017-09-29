0dd263c295e853dc919cce9d026aac00fbaf67f9
kristerw <kristerw@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-09-29 09:38:08 +0000

    2017-09-29  Krister Walfridsson  <krister.walfridsson@gmail.com>
    	Backport from mainline
    	2017-09-26  Krister Walfridsson  <krister.walfridsson@gmail.com>
    
    	PR target/39570
    	* gcc/config/netbsd-protos.h: New file.
    	* gcc/config/netbsd.c: New file.
    	* gcc/config/netbsd.h (SUBTARGET_INIT_BUILTINS): Define.
    	* gcc/config/t-netbsd: New file.
    	* gcc/config.gcc (tm_p_file): Add netbsd-protos.h.
    	(tmake_file) Add t-netbsd.
    	(extra_objs) Add netbsd.o.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@253283 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/netbsd.h
+++ gcc/config/netbsd.h
@@ -178,3 +178,9 @@ along with GCC; see the file COPYING3.  If not see
 
 #undef WINT_TYPE
 #define WINT_TYPE "int"
+
+#undef  SUBTARGET_INIT_BUILTINS
+#define SUBTARGET_INIT_BUILTINS						\
+  do {									\
+    netbsd_patch_builtins ();						\
+  } while(0)
