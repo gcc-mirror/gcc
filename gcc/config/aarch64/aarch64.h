787618f1ab6c22858c262c5a9b2b5d3241a0984b
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-10-02 08:11:07 +0000

    [AArch64] PR71307: Define union class of POINTER+FP
    ALL_REGS doesn't function as a union class of POINTER_REGS and FP_REGS
    since it includes the CC register as well.  REGNO_REG_CLASS (CC_REGNUM)
    is NO_REGS, but of course NO_REGS rightly doesn't include CC_REGNUM.
    
    Adding a union class for POINTER+FP allows the RA to use it as the
    preferred or alternative class of a pseudo.  It also works as a
    union class of GENERAL+FP for modes that aren't allowed in SP.
    
    This is also needed for the SVE port, which adds predicate registers
    to the mix.
    
    2017-09-15  Richard Sandiford  <richard.sandiford@linaro.org>
    	    Alan Hayward  <alan.hayward@arm.com>
    	    David Sherwood  <david.sherwood@arm.com>
    
    gcc/
    	PR target/71307
    	* config/aarch64/aarch64.h (POINTER_AND_FP_REGS): New reg class.
    	(REG_CLASS_NAMES, REG_CLASS_CONTENTS): Update accordingly.
    	* config/aarch64/aarch64.c (aarch64_class_max_nregs): Handle
    	POINTER_AND_FP_REGS.
    
    gcc/testsuite/
    	PR target/71307
    	* gcc.target/aarch64/vect_copy_lane_1.c: Remove XFAIL.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@253337 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/aarch64/aarch64.h
+++ gcc/config/aarch64/aarch64.h
@@ -444,6 +444,7 @@ enum reg_class
   POINTER_REGS,
   FP_LO_REGS,
   FP_REGS,
+  POINTER_AND_FP_REGS,
   ALL_REGS,
   LIM_REG_CLASSES		/* Last */
 };
@@ -459,6 +460,7 @@ enum reg_class
   "POINTER_REGS",				\
   "FP_LO_REGS",					\
   "FP_REGS",					\
+  "POINTER_AND_FP_REGS",			\
   "ALL_REGS"					\
 }
 
@@ -471,6 +473,7 @@ enum reg_class
   { 0xffffffff, 0x00000000, 0x00000003 },	/* POINTER_REGS */	\
   { 0x00000000, 0x0000ffff, 0x00000000 },       /* FP_LO_REGS  */	\
   { 0x00000000, 0xffffffff, 0x00000000 },       /* FP_REGS  */		\
+  { 0xffffffff, 0xffffffff, 0x00000003 },	/* POINTER_AND_FP_REGS */\
   { 0xffffffff, 0xffffffff, 0x00000007 }	/* ALL_REGS */		\
 }
 
