3934b625ee2f522edf455191d1eaaa42e265a1d9
Thomas Koenig <tkoenig@gcc.gnu.org>
2017-10-10 16:49:32 +0000

    re PR libfortran/82233 (execute_command_line causes program to stop when command fails (or does not exist))
    2017-10-10  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR libfortran/82233
    	* intrinsics/execute_command_line.c (execute_command_line):
    	No call to runtime_error if cmdstat is present.
    
    2017-10-10  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR libfortran/82233
    	* gfortran.dg/execute_command_line_3.f90: New test.
    
    From-SVN: r253593

---
+++ gcc/testsuite/gfortran.dg/execute_command_line_3.f90
@@ -0,0 +1,24 @@
+! { dg-do  run }
+! PR 82233 - there were program aborts for some of these commands.
+! Original test case by Urban Jost.
+program boom
+implicit none
+integer                       :: i,j 
+character(len=256)            :: msg
+character(len=:), allocatable :: command
+   command='notthere'
+   msg='' ! seems to only be defined if exitstatus.ne.0
+   ! ok -- these work
+   call execute_command_line(command , wait=.false., exitstat=i, cmdstat=j, cmdmsg=msg)
+   if (j /= 0 .or. msg /= '') call abort
+   call execute_command_line(command ,               exitstat=i, cmdstat=j, cmdmsg=msg )
+   if (j /= 3 .or. msg /= "Invalid command line" ) call abort
+   msg = ''
+   call execute_command_line(command , wait=.false., exitstat=i,            cmdmsg=msg )
+   print *,msg
+   if (msg /= '') call abort
+   call execute_command_line(command ,               exitstat=i, cmdstat=j             )
+   if (j /= 3) call abort
+   call execute_command_line(command , wait=.false., exitstat=i                        )
+
+end program boom
