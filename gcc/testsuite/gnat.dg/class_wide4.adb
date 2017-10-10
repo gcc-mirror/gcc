0c6ddd1ddeccd6982ddedd3a05ea4db4769b18d5
Ed Schonberg <schonberg@adacore.com>
2017-10-10 13:23:33 +0000

    class_wide4.adb: Fix a typo.
    gcc/testsuite/
    
    	* gnat.dg/class_wide4.adb: Fix a typo.
    
    Also add ChangeLog entries missing from previous commits.
    
    From-SVN: r253584

--- gcc/testsuite/gnat.dg/class_wide4.adb
+++ gcc/testsuite/gnat.dg/class_wide4.adb
@@ -5,7 +5,7 @@ with Class_Wide4_Pkg2;
 
 procedure Class_Wide4 is
    D  : aliased Class_Wide4_Pkg.Data_Object;
-   O  : aliased Class_Wide4_Pkg.Object;
+   O  : aliased Class_Wide4_Pkg2.Object;
    IA : not null access Class_Wide4_Pkg.Conditional_Interface'Class :=
           O'Access;
    I  : Class_Wide4_Pkg.Conditional_Interface'Class renames
