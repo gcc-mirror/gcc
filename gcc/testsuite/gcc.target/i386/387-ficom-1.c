42851ff81dbe1ccaa16dbcc070513c181ff61eb7
Uros Bizjak <uros@gcc.gnu.org>
2017-10-11 20:00:55 +0200

    i386.md (*cmp<X87MODEF:mode>_<SWI24:mode>_i387): Do not use float_operator operator predicate.
    	* config/i386/i386.md (*cmp<X87MODEF:mode>_<SWI24:mode>_i387):
    	Do not use float_operator operator predicate.
    	(*cmp<X87MODEF:mode>_<SWI24:mode>_cc_i387): Ditto.
    	* config/i386/predicates.md (float_operator): Remove predicate.
    
    	* config/i386/i386.md (*jcc<mode>_0_i387): Remove insn pattern.
    	(*jccxf_i387): Ditto.
    	(*jcc<mode>_i387): Ditto.
    	(*jccu<mode>_i387): Ditto.
    	(*jcc<X87MODEF:mode>_<SWI24:mode>_i387): Ditto.
    	(*jcc_*_i387 splitters): Remove.
    	* config/i386/i386-protos.h (ix86_split_fp_branch): Remove prototype.
    	* config/i386/i386.c (ix86_split_fp_branch): Remove.
    	* config/i386/predicates.md (ix86_swapped_fp_comparison_operator):
    	Remove predicate.
    
    testsuite/ChangeLog:
    
    	* gcc.target/i386/387-ficom-1.c: New test.
    
    From-SVN: r253653

---
+++ gcc/testsuite/gcc.target/i386/387-ficom-1.c
@@ -0,0 +1,41 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target ia32 } */
+/* { dg-skip-if "" { *-*-* } { "-march=*" } { "-march=i386" } } */
+/* { dg-options "-O2 -march=i386 -ffast-math -masm=att" } */
+
+extern short s;
+
+int test_f_s (short x)
+{
+  return (float)x > s;
+}
+
+int test_d_s (short x)
+{
+  return (double)x < s;
+}
+
+int test_ld_s (short x)
+{
+  return (long double)x == s;
+}
+
+extern int i;
+
+int test_f_i (int x)
+{
+  return (float)i >= x;
+}
+
+int test_d_i (int x)
+{
+  return (double)i <= x;
+}
+
+int test_ld_i (int x)
+{
+  return (long double)i != x;
+}
+
+/* { dg-final { scan-assembler-times "ficomps" 3 } } */
+/* { dg-final { scan-assembler-times "ficompl" 3 } } */
