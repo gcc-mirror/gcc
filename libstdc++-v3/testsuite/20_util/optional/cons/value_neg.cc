7759a387e25c46fe0fe19044f1785d012a85c1f8
Jonathan Wakely <jwakely@redhat.com>
2017-10-20 16:14:29 +0100

    PR libstdc++/79433 no #error for including headers with wrong -std
    Backport from mainline
    2017-09-12  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/79433
    	* doc/xml/manual/status_cxx2017.xml: Update feature-test macros.
    	* doc/html/*: Regenerate.
    	* include/Makefile.am: Remove <bits/c++17_warning.h>.
    	* include/Makefile.in: Regenerate.
    	* include/bits/c++17_warning.h: Remove.
    	* include/bits/string_view.tcc: Do not include <bits/c++17_warning.h>
    	for pre-C++17 modes.
    	* include/std/any: Likewise.
    	(__cpp_lib_any): Define.
    	* include/std/mutex (__cpp_lib_scoped_lock): Adjust value as per new
    	SD-6 draft.
    	* include/std/numeric (__cpp_lib_gcd_lcm): Define as per new SD-6
    	draft.
    	* include/std/optional: Do not include <bits/c++17_warning.h>.
    	(__cpp_lib_optional): Define.
    	* include/std/shared_mutex: Do not include <bits/c++14_warning.h>.
    	* include/std/string_view: Do not include <bits/c++17_warning.h>.
    	(__cpp_lib_string_view): Define.
    	* include/std/variant: Do not include <bits/c++17_warning.h>.
    	(__cpp_lib_variant): Define.
    	* testsuite/20_util/optional/cons/value_neg.cc: Adjust dg-error line
    	numbers.
    	* testsuite/26_numerics/gcd/1.cc: Test for __cpp_lib_gcd_lcm.
    	* testsuite/26_numerics/gcd/gcd_neg.cc: Adjust dg-error line
    	numbers.
    	* testsuite/26_numerics/lcm/1.cc: Test for __cpp_lib_gcd_lcm.
    	* testsuite/26_numerics/lcm/lcm_neg.cc: Adjust dg-error line
    	numbers.
    	* testsuite/30_threads/scoped_lock/requirements/typedefs.cc: Adjust
    	expected value of __cpp_lib_scoped_lock.
    
    From-SVN: r253943

--- libstdc++-v3/testsuite/20_util/optional/cons/value_neg.cc
+++ libstdc++-v3/testsuite/20_util/optional/cons/value_neg.cc
@@ -37,8 +37,8 @@ int main()
     std::optional<std::unique_ptr<int>> oup2 = new int;  // { dg-error "conversion" }
     struct U { explicit U(std::in_place_t); };
     std::optional<U> ou(std::in_place); // { dg-error "no matching" }
-    // { dg-error "no type" "" { target { *-*-* } } 488 }
-    // { dg-error "no type" "" { target { *-*-* } } 498 }
-    // { dg-error "no type" "" { target { *-*-* } } 555 }
+    // { dg-error "no type" "" { target { *-*-* } } 490 }
+    // { dg-error "no type" "" { target { *-*-* } } 500 }
+    // { dg-error "no type" "" { target { *-*-* } } 557 }
   }
 }
