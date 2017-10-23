0c3d6318825778641d9a3718d9ca77d4ccab097b
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-10-23 19:06:32 +0000

    * g++.dg/opt/pr82577: Fix for C++17.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@254021 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/opt/pr82577.C
+++ gcc/testsuite/g++.dg/opt/pr82577.C
@@ -1,6 +1,11 @@
 // { dg-additional-options "-O2" }
 // PR c++/82577 ICE when optimizing
 
+#if __cplusplus > 201500L
+// register is no longer a keyword in C++17.
+#define register
+#endif
+
 class a {
 public:
   int *b();
