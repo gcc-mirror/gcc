ea3b0bfac3fe5cd2a1cadc3146243320e49ee429
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-10-23 11:10:28 +0000

    PR libstdc++/82644 document IS 29124 support
    Also fix declarations of special functions in C++17, to import them into
    the global namespace in <math.h>, and to prevent defining the
    non-standard hypergeometric functions in strict mode.
    
    	PR libstdc++/82644
    	* doc/xml/manual/intro.xml: Include new section.
    	* doc/xml/manual/status_cxxis29124.xml: New section on IS 29124
    	status.
    	* include/bits/specfun.h [__STRICT_ANSI__] (hyperg, hypergf, hypergl)
    	(conf_hyperg, conf_hypergf, conf_hypergl): Don't declare.
    	* include/c_compatibility/math.h: Import special functions into
    	global namespace for C++17.
    	* testsuite/26_numerics/headers/cmath/82644.cc: New test.
    	* testsuite/26_numerics/headers/cmath/functions_global_c++17.cc: New
    	test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@254004 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/26_numerics/headers/cmath/functions_global_c++17.cc
@@ -0,0 +1,111 @@
+// Copyright (C) 2017 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++17" }
+// { dg-do compile }
+
+#include <math.h>
+
+namespace gnu
+{
+  using ::acos;
+  using ::asin;
+  using ::atan;
+  using ::atan2;
+  using ::ceil;
+  using ::cos;
+  using ::cosh;
+  using ::exp;
+  using ::fabs;
+  using ::floor;
+  using ::fmod;
+  using ::frexp;
+  using ::ldexp;
+  using ::log;
+  using ::log10;
+  using ::modf;
+  using ::pow;
+  using ::sin;
+  using ::sinh;
+  using ::sqrt;
+  using ::tan;
+  using ::tanh;
+
+  using ::assoc_laguerre;
+  using ::assoc_laguerref;
+  using ::assoc_laguerrel;
+  using ::assoc_legendre;
+  using ::assoc_legendref;
+  using ::assoc_legendrel;
+  using ::beta;
+  using ::betaf;
+  using ::betal;
+  using ::comp_ellint_1;
+  using ::comp_ellint_1f;
+  using ::comp_ellint_1l;
+  using ::comp_ellint_2;
+  using ::comp_ellint_2f;
+  using ::comp_ellint_2l;
+  using ::comp_ellint_3;
+  using ::comp_ellint_3f;
+  using ::comp_ellint_3l;
+  using ::cyl_bessel_i;
+  using ::cyl_bessel_if;
+  using ::cyl_bessel_il;
+  using ::cyl_bessel_j;
+  using ::cyl_bessel_jf;
+  using ::cyl_bessel_jl;
+  using ::cyl_bessel_k;
+  using ::cyl_bessel_kf;
+  using ::cyl_bessel_kl;
+  using ::cyl_neumann;
+  using ::cyl_neumannf;
+  using ::cyl_neumannl;
+  using ::ellint_1;
+  using ::ellint_1f;
+  using ::ellint_1l;
+  using ::ellint_2;
+  using ::ellint_2f;
+  using ::ellint_2l;
+  using ::ellint_3;
+  using ::ellint_3f;
+  using ::ellint_3l;
+  using ::expint;
+  using ::expintf;
+  using ::expintl;
+  using ::hermite;
+  using ::hermitef;
+  using ::hermitel;
+  using ::laguerre;
+  using ::laguerref;
+  using ::laguerrel;
+  using ::legendre;
+  using ::legendref;
+  using ::legendrel;
+  using ::riemann_zeta;
+  using ::riemann_zetaf;
+  using ::riemann_zetal;
+  using ::sph_bessel;
+  using ::sph_besself;
+  using ::sph_bessell;
+  using ::sph_legendre;
+  using ::sph_legendref;
+  using ::sph_legendrel;
+  using ::sph_neumann;
+  using ::sph_neumannf;
+  using ::sph_neumannl;
+}
