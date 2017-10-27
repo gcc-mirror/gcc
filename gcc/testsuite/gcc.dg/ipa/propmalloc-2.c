bd5ef087eb390ec95e2a65597b06d298c73f3974
prathamesh3492 <prathamesh3492@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-10-27 10:48:49 +0000

    Extend ipa-pure-const pass to propagate malloc attribute.
    2017-10-27  Prathamesh Kulkarni  <prathamesh.kulkarni@linaro.org>
    
    	* cgraph.h (set_malloc_flag): Declare.
    	* cgraph.c (set_malloc_flag_1): New function.
    	(set_malloc_flag): Likewise.
    	* ipa-fnsummary.h (ipa_call_summary): Add new field is_return_callee.
    	* ipa-fnsummary.c (ipa_call_summary::reset): Set is_return_callee to
    	false.
    	(read_ipa_call_summary): Add support for reading is_return_callee.
    	(write_ipa_call_summary): Stream is_return_callee.
    	* ipa-inline.c (ipa_inline): Remove call to ipa_free_fn_summary.
    	* ipa-pure-const.c: Add headers ssa.h, alloc-pool.h, symbol-summary.h,
    	ipa-prop.h, ipa-fnsummary.h.
    	(pure_const_names): Change to static.
    	(malloc_state_e): Define.
    	(malloc_state_names): Define.
    	(funct_state_d): Add field malloc_state.
    	(varying_state): Set malloc_state to STATE_MALLOC_BOTTOM.
    	(check_retval_uses): New function.
    	(malloc_candidate_p): Likewise.
    	(analyze_function): Add support for malloc attribute.
    	(pure_const_write_summary): Stream malloc_state.
    	(pure_const_read_summary): Add support for reading malloc_state.
    	(dump_malloc_lattice): New function.
    	(propagate_malloc): New function.
    	(warn_function_malloc): New function.
    	(ipa_pure_const::execute): Call propagate_malloc and
    	ipa_free_fn_summary.
    	(pass_local_pure_const::execute): Add support for malloc attribute.
    	* ssa-iterators.h (RETURN_FROM_IMM_USE_STMT): New macro.
    	* doc/invoke.texi: Document Wsuggest-attribute=malloc.
    
    testsuite/
    	* gcc.dg/ipa/propmalloc-1.c: New test-case.
    	* gcc.dg/ipa/propmalloc-2.c: Likewise.
    	* gcc.dg/ipa/propmalloc-3.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@254140 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/ipa/propmalloc-2.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-ipa-pure-const-details" } */
+
+__attribute__((noinline, used, no_icf))
+static void *foo (__SIZE_TYPE__ n)
+{
+  return __builtin_malloc (n * 10);
+}
+
+__attribute__((noinline, used, no_icf))
+static void *bar(__SIZE_TYPE__ n, int cond)
+{
+  void *p;
+  if (cond)
+    p = foo (n);
+  else
+    p = __builtin_malloc (n);
+
+  return p;
+}
+
+/* { dg-final { scan-ipa-dump "Function foo found to be malloc" "pure-const" } } */
+/* { dg-final { scan-ipa-dump "Function bar found to be malloc" "pure-const" } } */
