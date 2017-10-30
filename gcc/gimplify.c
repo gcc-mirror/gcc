6b139f0d931c20d442eefdfb176b2c0dddd9ccfb
James E Wilson <wilson@tuliptree.org>
2017-10-30 08:38:30 -0700

    Fix ia64 build failure.
    	gcc/
    	* gimplify.c: Include memmodel.h.
    
    From-SVN: r254227

--- gcc/gimplify.c
+++ gcc/gimplify.c
@@ -27,6 +27,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "target.h"
 #include "rtl.h"
 #include "tree.h"
+#include "memmodel.h"
 #include "tm_p.h"
 #include "gimple.h"
 #include "gimple-predict.h"
