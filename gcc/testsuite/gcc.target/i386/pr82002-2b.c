e6d7851fd178be0de2414f1ca835ee21e87573fe
dansan <dansan@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-10-31 21:48:55 +0000

    PR target/82002 Part 1: Correct ICE caused by wrong calculation
    gcc:
    	config/i386/i386.c (ix86_expand_epilogue): Correct stack
    	calculation.
    
    gcc/testsuite:
    	gcc.target/i386/pr82002-1.c: New test.
    	gcc.target/i386/pr82002-2a.c: New xfail test.
    	gcc.target/i386/pr82002-2b.c: New xfail test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@254284 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr82002-2b.c
@@ -0,0 +1,14 @@
+/* { dg-do compile { target lp64 } } */
+/* { dg-options "-Ofast -mstackrealign -mabi=ms -mcall-ms2sysv-xlogues" } */
+/* { dg-xfail-if "" { *-*-* }  } */
+/* { dg-xfail-run-if "" { *-*-* }  } */
+
+void __attribute__((sysv_abi)) a (char *);
+void
+b ()
+{
+  char c[10000000000];
+  c[1099511627776] = 'b';
+  a (c);
+  a (c);
+}
