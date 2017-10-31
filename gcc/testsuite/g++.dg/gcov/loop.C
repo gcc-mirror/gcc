eb356edc35c64bd254c91d7772f7daee7ae26cb0
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-10-31 15:31:25 +0000

    GCOV: add -j argument (human readable format).
    2017-10-31  Martin Liska  <mliska@suse.cz>
    
    	* doc/gcov.texi: Document new option.
    	* gcov.c (print_usage): Likewise print it.
    	(process_args): Support the argument.
    	(format_count): New function.
    	(format_gcov): Use the function.
    2017-10-31  Martin Liska  <mliska@suse.cz>
    
    	* g++.dg/gcov/loop.C: New test.
    	* lib/gcov.exp: Support human readable format for counts.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@254269 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/gcov/loop.C
@@ -0,0 +1,27 @@
+/* { dg-options "-fprofile-arcs -ftest-coverage" } */
+/* { dg-do run { target native } } */
+
+unsigned
+loop (unsigned n, int value)		  /* count(14k) */
+{
+  for (unsigned i = 0; i < n - 1; i++)
+  {
+    value += i;				  /* count(21M) */
+  }
+
+  return value;
+}
+
+int main(int argc, char **argv)
+{
+  unsigned sum = 0;
+  for (unsigned i = 0; i < 7 * 1000; i++)
+  {
+    sum += loop (1000, sum);
+    sum += loop (2000, sum);		  /* count(7k) */
+  }
+
+  return 0;				  /* count(1) */
+}
+
+/* { dg-final { run-gcov branches { -abj loop.C } } } */
