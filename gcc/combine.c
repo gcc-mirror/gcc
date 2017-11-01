37a2c475252b26895732fefbbb0050c864ba698d
Richard Sandiford <richard.sandiford@linaro.org>
2017-11-01 17:06:17 +0000

    revert: combine.c (can_change_dest_mode): Reject changes in REGMODE_NATURAL_SIZE.
    2017-11-01  Richard Sandiford  <richard.sandiford@linaro.org>
    
    gcc/
    	Revert accidental duplicate:
    
    	* combine.c (can_change_dest_mode): Reject changes in
    	REGMODE_NATURAL_SIZE.
    
    From-SVN: r254316

--- gcc/combine.c
+++ gcc/combine.c
@@ -2478,12 +2478,6 @@ can_change_dest_mode (rtx x, int added_sets, machine_mode mode)
       != REGMODE_NATURAL_SIZE (GET_MODE (x)))
     return false;
 
-  /* Don't change between modes with different underlying register sizes,
-     since this could lead to invalid subregs.  */
-  if (REGMODE_NATURAL_SIZE (mode)
-      != REGMODE_NATURAL_SIZE (GET_MODE (x)))
-    return false;
-
   regno = REGNO (x);
   /* Allow hard registers if the new mode is legal, and occupies no more
      registers than the old mode.  */
