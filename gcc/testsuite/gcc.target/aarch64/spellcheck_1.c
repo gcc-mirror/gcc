718afa1acdb7bb247fe69995f3939ed26ab43a42
Steve Ellcey <sellcey@cavium.com>
2017-11-02 21:58:05 +0000

    re PR target/79868 (aarch64: diagnostic "malformed target %s value" not translateable)
    	PR target/79868
    	* gcc.target/aarch64/spellcheck_1.c: Update dg-error string to match
    	new format.
    	* gcc.target/aarch64/spellcheck_2.c: Ditto.
    	* gcc.target/aarch64/spellcheck_3.c: Ditto.
    	* gcc.target/aarch64/target_attr_11.c: Ditto.
    	* gcc.target/aarch64/target_attr_12.c: Ditto.
    	* gcc.target/aarch64/target_attr_17.c: Ditto.
    
    From-SVN: r254360

--- gcc/testsuite/gcc.target/aarch64/spellcheck_1.c
+++ gcc/testsuite/gcc.target/aarch64/spellcheck_1.c
@@ -4,6 +4,6 @@ __attribute__((target ("arch=armv8-a-typo"))) void
 foo ()
 {
   /* { dg-message "valid arguments are: \[^\n\r]*; did you mean 'armv8-a'?"  "" { target *-*-* } .-1 } */
-  /* { dg-error "unknown value 'armv8-a-typo' for 'arch' target attribute"  "" { target *-*-* } .-2 } */
-  /* { dg-error "target attribute 'arch=armv8-a-typo' is invalid"  "" { target *-*-* } .-3 } */
+  /* { dg-error "invalid name \\(\"armv8-a-typo\"\\) in 'target\\(\"arch=\"\\)' pragma or attribute"  "" { target *-*-* } .-2 } */
+  /* { dg-error "pragma or attribute 'target\\(\"arch=armv8-a-typo\"\\)' is not valid"  "" { target *-*-* } .-3 } */
 }
