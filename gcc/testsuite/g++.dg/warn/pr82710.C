b43bd76bcbf67d35f0ba6c322e04f2fdcdec69d6
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-11-03 13:16:06 +0000

    [PR c++/82710] false positive paren warning
    https://gcc.gnu.org/ml/gcc-patches/2017-11/msg00186.html
    	PR c++/82710
    	* decl.c (grokdeclarator): Protect MAYBE_CLASS things from paren
    	warning too.
    
    	PR c++/82710
    	* g++.dg/warn/pr82710.C: More cases.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@254371 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/warn/pr82710.C
+++ gcc/testsuite/g++.dg/warn/pr82710.C
@@ -1,7 +1,10 @@
-// { dg-additional-options -Wparentheses }
+// { dg-do compile { target c++11 } }
+// { dg-additional-options "-Wparentheses -Wno-non-template-friend" }
 
 // the MVP warning triggered on a friend decl.  */
 class X;
+enum class Q {}; // C++ 11ness
+enum R {};
 
 namespace here 
 {
@@ -9,6 +12,9 @@ namespace here
   X friendFunc1();
   X *friendFunc2 ();
   int friendFunc3 ();
+  int bob ();
+  Q bill ();
+  R ben ();
 }
 
 namespace nm
@@ -19,6 +25,9 @@ namespace nm
     void friendFunc1 ();
     void friendFunc2 ();
     void friendFunc3 ();
+    int bob ();
+    Q bill ();
+    R ben ();
   }
 
   class TestClass
@@ -27,6 +36,13 @@ namespace nm
     friend X *(::here::friendFunc2 ()); // { dg-warning "" }
     friend X *::here::friendFunc2 ();
     friend int (::here::friendFunc3 ()); // { dg-warning "" }
+  };
+
+  template <typename T> class X
+  {
+    friend typename T::frob (::here::bob ());
+    friend Q (::here::bill ());
+    friend R (::here::ben ());
   };
 }
 
