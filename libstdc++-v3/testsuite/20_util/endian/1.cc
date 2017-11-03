e641ee437f4134131e1d81a5f0e9415f3737d78b
Jonathan Wakely <jwakely@redhat.com>
2017-11-03 15:45:49 +0000

    Define std::endian for C++2a (P0463R1)
    	* include/std/type_traits (endian): Define new enumeration type.
    	* testsuite/20_util/endian/1.cc: New test.
    
    From-SVN: r254381

---
+++ libstdc++-v3/testsuite/20_util/endian/1.cc
@@ -0,0 +1,36 @@
+// Copyright (C) 2017 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++2a" }
+// { dg-do compile { target c++2a } }
+
+#include <type_traits>
+
+static_assert( std::is_enum_v<std::endian> );
+static_assert( std::endian::little != std::endian::big );
+static_assert( std::endian::native == std::endian::big
+		|| std::endian::native == std::endian::little );
+
+namespace gnu {
+  int little, big, native;
+}
+
+using namespace std;
+using namespace gnu;
+
+// std::endian is a scoped-enum so these should refer to gnu::native etc.
+int test = little + big + native;
