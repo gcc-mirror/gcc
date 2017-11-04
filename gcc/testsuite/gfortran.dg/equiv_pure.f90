e641f93ad41ce2d4078c34a21760839eebe5eb95
kargl <kargl@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-11-04 16:41:09 +0000

    2017-11-04  Steven G. Kargl  <kargl@gcc.gnu.org>
    	PR fortran/82796
    	* resolve.c (resolve_equivalence): An entity in a common block within
     	a module cannot appear in an equivalence statement if the entity is
    	with a pure procedure.
    
    2017-11-04  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	PR fortran/82796
    	* gfortran.dg/equiv_pure.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@254409 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/equiv_pure.f90
@@ -0,0 +1,50 @@
+! { dg-do compile }
+module eq
+   implicit none
+   integer :: n1, n2
+   integer, dimension(2) :: a
+   equivalence (a(1), n1)
+   equivalence (a(2), n2)
+   common /a/ a
+end module eq
+
+module m
+   use eq
+   implicit none
+   type, public :: t
+     integer :: i
+   end type t
+end module m
+
+module p
+   implicit none
+   contains
+   pure integer function d(h)
+     use m
+     implicit none
+     integer, intent(in) :: h
+     d = h
+   end function
+end module p
+
+module q
+   implicit none
+   contains
+   pure integer function d(h)
+     use m, only : t
+     implicit none
+     integer, intent(in) :: h
+     d = h
+   end function
+end module q
+
+module r
+   implicit none
+   contains
+   pure integer function d(h)
+     use m, only : a          ! { dg-error "cannot be an EQUIVALENCE object" }
+     implicit none
+     integer, intent(in) :: h
+     d = h
+   end function
+end module r
