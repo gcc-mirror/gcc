53d855e09df135ea735178521cc7c4cfbfb8a485
Jeff Law <law@redhat.com>
2017-11-06 15:44:39 -0700

    stack-check-12.c: Revert to initial version.
    	* gcc.target/i386/stack-check-12.c: Revert to initial version.  Then..
    	Add -fomit-frame-pointer.
    
    From-SVN: r254479

--- gcc/testsuite/gcc.target/i386/stack-check-12.c
+++ gcc/testsuite/gcc.target/i386/stack-check-12.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -fstack-clash-protection -mtune=generic" } */
+/* { dg-options "-O2 -fstack-clash-protection -mtune=generic -fomit-frame-pointer" } */
 /* { dg-require-effective-target supports_stack_clash_protection } */
 
 __attribute__ ((noreturn)) void exit (int);
@@ -14,6 +14,6 @@ f (void)
 /* { dg-final { scan-assembler-not "or\[ql\]" } } */
 /* { dg-final { scan-assembler "pushl	%esi" { target ia32 } } } */
 /* { dg-final { scan-assembler "popl	%esi" { target ia32 } } }*/
-/* { dg-final { scan-assembler "pushq	%rax" { target ! { ia32 } } } } */
-/* { dg-final { scan-assembler "popq	%rax" { target ! { ia32 } } } }*/
+/* { dg-final { scan-assembler "pushq	%rax" { target { ! ia32 } } } } */
+/* { dg-final { scan-assembler "popq	%rax" { target { ! ia32 } } } }*/
 
