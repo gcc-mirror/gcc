dc32d9e8c02e40adb9722ed5f24ba937e0638a66
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-11-09 15:16:55 +0000

    Add a vect_element_align_preferred target selector
    This patch adds a target selector for targets whose
    preferred_vector_alignment is the alignment of one element.  We'll never
    peel in that case, and the step of a loop that operates on normal (as
    opposed to packed) elements will always divide the preferred alignment.
    
    2017-11-09  Richard Sandiford  <richard.sandiford@linaro.org>
    	    Alan Hayward  <alan.hayward@arm.com>
    	    David Sherwood  <david.sherwood@arm.com>
    
    gcc/
    	* doc/sourcebuild.texi (vect_element_align_preferred): Document.
    
    gcc/testsuite/
    	* lib/target-supports.exp
    	(check_effective_target_vect_element_align_preferred): New proc.
    	(check_effective_target_vect_peeling_profitable): Test it.
    	* gcc.dg/vect/no-section-anchors-vect-31.c: Don't expect peeling
    	if vect_element_align_preferred.
    	* gcc.dg/vect/no-section-anchors-vect-64.c: Likewise.
    	* gcc.dg/vect/pr65310.c: Likewise.
    	* gcc.dg/vect/vect-26.c: Likewise.
    	* gcc.dg/vect/vect-54.c: Likewise.
    	* gcc.dg/vect/vect-56.c: Likewise.
    	* gcc.dg/vect/vect-58.c: Likewise.
    	* gcc.dg/vect/vect-60.c: Likewise.
    	* gcc.dg/vect/vect-89-big-array.c: Likewise.
    	* gcc.dg/vect/vect-89.c: Likewise.
    	* gcc.dg/vect/vect-92.c: Likewise.
    	* gcc.dg/vect/vect-peel-1.c: Likewise.
    	* gcc.dg/vect/vect-outer-3a-big-array.c: Expect the step to
    	divide the alignment if vect_element_align_preferred.
    	* gcc.dg/vect/vect-outer-3a.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@254593 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/vect/no-section-anchors-vect-64.c
+++ gcc/testsuite/gcc.dg/vect/no-section-anchors-vect-64.c
@@ -91,4 +91,4 @@ int main (void)
 
 /* { dg-final { scan-tree-dump-times "vectorized 3 loops" 1 "vect" } } */
 /* { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 0 "vect" } } */
-/* { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 2 "vect" } } */
+/* { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 2 "vect" { xfail vect_element_align_preferred } } } */
