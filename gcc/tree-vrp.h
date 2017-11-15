94d86adc32357dc3a46a07bcd4dc0bea40a3ed87
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-11-15 00:18:45 +0000

    * vr-values.c: New file with contents extracted from tree-vrp.c. 	* Makefile.in (OBJS): Add vr-values.o 	* tree-vrp.h (set_value_range_to_nonnull): Prototype. 	(set_value_range, set_and_canonicalize_value_range): Likewise. 	(vrp_bitmap_equal_p, range_is_nonnull): Likewise. 	(value_range_constant_singleton, symbolic_range_p): Likewise. 	(compare_values, compare_values_warnv, vrp_val_is_min): Likewise. 	(vrp_val_is_max, copy_value_range, set_value_range_to_value): Likewise. 	(extract_range_from_binary_expr_1, vrp_val_min, vrp_val_max): Likewise. 	(set_value_range_to_null, range_int_cst_p, opreand_less_p): Likewise. 	(find_case_label_range, find_case_label_index): Likewise. 	(zero_nonzero_bits_from_vr, overflow_comparison_p): Likewise. 	(range_int_cst_singleton_p, value_inside_range): Likewise. 	(get_single_symbol): Likewise. 	(switch_update): Move structure definition here. 	(to_remove_edges, to_update_switch_stmts): Provide externs. 	* tree-vrp.c: Move all methods for vr-values class to vr-values.c 	(vrp_val_max, vrp_val_min, vrp_val_is_max): Make externally visible. 	(vrp_val_is_min, set_value_range): Likewise. 	(set_and_canonicalize_value_range, copy_value_range): Likewise. 	(set_value_range_to_value, set_value_range_to_nonnull): Likewise. 	(set_value_range_to_null, vrp_bitmap_equal_p): Likewise. 	(range_is_nonnull, range_int_cst_p): Likewwise. 	(range_int_cst_singleton_p, symbolic_range_p): Likewise. 	(get_single_symbol, operand_less_p): Likewise 	(compare_values_warnv, compare_values): Likewise. 	(value_inside_range, value_range_constant_singleton): Likewise. 	(zero_nonzero_bitgs_from_vr): Likewise. 	(extract_range_from_binary_expr_1): Likewise. 	(overflow_comparison_p): Likewise. 	(to_remove_edges, to_update_switch_stmts): Likewise. 	(find_case_label-index, find_case_label_range): Likewise. 	(switch_update, set_value_range_to_nonnegative): Remove. 	(set_value_range_to_truthvalue): Likewise. 	(symbolic_range_based_on_p, gimple_assign_nonzero_p): Likewise. 	(gimple_stmt_nonzero_p, compare_ranges): Likewise. 	(compare_range_with_value, vrp_valueize, vrp_valueize_1): Likewise. 	(find_case_label_ranges, test_for_singularity): Likewise. 	(range_fits_type_p, simplify_conversion_using_ranges): LIkewise. 	(x_vr_values): Move to its remaining use site.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@254747 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-vrp.h
+++ gcc/tree-vrp.h
@@ -84,4 +84,46 @@ extern void set_value_range_to_varying (value_range *);
 extern int range_includes_zero_p (tree, tree);
 extern bool infer_value_range (gimple *, tree, tree_code *, tree *);
 
+extern void set_value_range_to_nonnull (value_range *, tree);
+extern void set_value_range (value_range *, enum value_range_type, tree,
+			     tree, bitmap);
+extern void set_and_canonicalize_value_range (value_range *,
+					      enum value_range_type,
+					      tree, tree, bitmap);
+extern bool vrp_bitmap_equal_p (const_bitmap, const_bitmap);
+extern bool range_is_nonnull (value_range *);
+extern tree value_range_constant_singleton (value_range *);
+extern bool symbolic_range_p (value_range *);
+extern int compare_values (tree, tree);
+extern int compare_values_warnv (tree, tree, bool *);
+extern bool vrp_val_is_min (const_tree);
+extern bool vrp_val_is_max (const_tree);
+extern void copy_value_range (value_range *, value_range *);
+extern void set_value_range_to_value (value_range *, tree, bitmap);
+extern void extract_range_from_binary_expr_1 (value_range *, enum tree_code,
+					      tree, value_range *,
+					      value_range *);
+extern tree vrp_val_min (const_tree);
+extern tree vrp_val_max (const_tree);
+extern void set_value_range_to_null (value_range *, tree);
+extern bool range_int_cst_p (value_range *);
+extern int operand_less_p (tree, tree);
+extern bool find_case_label_range (gswitch *, tree, tree, size_t *, size_t *);
+extern bool find_case_label_index (gswitch *, size_t, tree, size_t *);
+extern bool zero_nonzero_bits_from_vr (const tree, value_range *,
+				       wide_int *, wide_int *);
+extern bool overflow_comparison_p (tree_code, tree, tree, bool, tree *);
+extern bool range_int_cst_singleton_p (value_range *);
+extern int value_inside_range (tree, tree, tree);
+extern tree get_single_symbol (tree, bool *, tree *);
+
+
+struct switch_update {
+  gswitch *stmt;
+  tree vec;
+};
+
+extern vec<edge> to_remove_edges;
+extern vec<switch_update> to_update_switch_stmts;
+
 #endif /* GCC_TREE_VRP_H */
