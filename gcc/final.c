d3e88495d5a54c57ef80caffe232c6b3eed96a04
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-11-15 03:11:48 +0000

    introduce TDF_compare_debug, omit OBJ_TYPE_REF casts with it
    for  gcc/ChangeLog
    
    	* dumpfile.h (TDF_COMPARE_DEBUG): New.
    	* final.c (rest_of_clean_state): Set it for the
    	-fcompare-debug dump.
    	* tree-pretty-print.c (dump_generic_node): Omit OBJ_TYPE_REF
    	class when TDF_COMPARE_DEBUG is set.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@254751 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/final.c
+++ gcc/final.c
@@ -4629,7 +4629,7 @@ rest_of_clean_state (void)
 	{
 	  flag_dump_noaddr = flag_dump_unnumbered = 1;
 	  if (flag_compare_debug_opt || flag_compare_debug)
-	    dump_flags |= TDF_NOUID;
+	    dump_flags |= TDF_NOUID | TDF_COMPARE_DEBUG;
 	  dump_function_header (final_output, current_function_decl,
 				dump_flags);
 	  final_insns_dump_p = true;
