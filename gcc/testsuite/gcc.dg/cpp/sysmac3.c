69491b354f00ba7016ef5425277b4bcac10bf901
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-11-17 07:42:28 +0000

    PR testsuite/82997 	* gcc.dg/cpp/macsyntx.c (var1, rest): Don't expect 	"requires at least one" warning. 	* gcc.dg/cpp/sysmac1.c (foo): Likewise. 	* gcc.dg/cpp/macsyntx2.c: New test. 	* gcc.dg/cpp/sysmac3.c: New test. 	* gcc.dg/cpp/sysmac3.h: New file.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@254857 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cpp/sysmac3.c
+++ gcc/testsuite/gcc.dg/cpp/sysmac3.c
@@ -1,18 +1,18 @@
 /* Copyright (C) 2001-2017 Free Software Foundation, Inc.  */
 
 /* { dg-do preprocess } */
-/* { dg-options "-std=gnu99 -pedantic -Wtraditional -ftrack-macro-expansion=0" } */
+/* { dg-options "-std=c99 -pedantic -Wtraditional -ftrack-macro-expansion=0" } */
 
 /* Tests diagnostics are suppressed for some macros defined in system
    headers.  */
 
 /* Source: Neil Booth, 15 Jan 2001.  */
 
-#include "syshdr.h"
+#include "sysmac3.h"
 
 #define uint 1U
 #define str(x) x
-#define foo(x, y...) bar(x, ##y) /* { dg-warning "named variadic macros" } */
+#define foo(x, y...) bar(x, y)	/* { dg-warning "named variadic macros" } */
 
 #if uint			/* { dg-warning "traditional C rejects" } */
 #endif
@@ -22,5 +22,5 @@
 (str);				/* { dg-warning "used with arguments" } */
 (sys_str);			/* { dg-bogus "used with arguments" } */
 
-foo (one_arg);			/* { dg-bogus "requires at least one" } */
+foo (one_arg);			/* { dg-warning "requires at least one" } */
 sys_foo (one_arg);		/* { dg-bogus "requires at least one" } */
