4ced528dfc2db6a53edd6e7066c5374a4014c527
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-11-21 10:06:52 +0000

    * print-tree.h (debug_vec_tree): Remove prototype. 	* gdbinit.in (pvt): Remove macro.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@254996 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/print-tree.h
+++ gcc/print-tree.h
@@ -31,7 +31,6 @@ extern void debug_head (const tree_node &ref);
 extern void debug_head (const tree_node *ptr);
 extern void debug_body (const tree_node &ref);
 extern void debug_body (const tree_node *ptr);
-extern void debug_vec_tree (vec<tree, va_gc> *);
 extern void debug (vec<tree, va_gc> &ref);
 extern void debug (vec<tree, va_gc> *ptr);
 extern void debug_raw (vec<tree, va_gc> &ref);
