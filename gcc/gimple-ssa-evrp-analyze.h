a5de02e9e3efdcdacc5dc100b61d1fd45e719955
Jeff Law <law@redhat.com>
2017-11-22 13:31:14 -0700

    * gimple-ssa-evrp-analyze.c (evrp_range_analyzer::evrp_range_analyzer) 	Initialize vr_values. 	(evrp_range_analyzer::try_find_new_range): Call methods attached to 	vr_values via vr_values class instance rather than delegators. 	(evrp_range_analyzer::record_ranges_from_phis): Likewise. 	(evrp_range_analyzer::record_ranges_from_stmt): Likewise. 	(evrp_range_analyzer::push_value_range): Likewise. 	(evrp_range_analyzer::pop_value_range): Likewise. 	* gimple-ssa-evrp-analyze.h (class evrp_range_analyzer): Remove 	most delegators.  Those remaining are exposed as public interfaces. 	Make vr_values a pointer and private. 	(evrp_range_analyzer::~evrp_range_analyzer): Delete the attached 	vr_values. 	(evrp_range_analyzer::get_vr_value): New method. 	* gimple-ssa-evrp.c (class evrp_folder): Use DISABLE_COPY_AND_ASSIGN. 	(evrp_folder::evrp_folder): New ctor to initialize vr_values. 	(class evrp_dom_walker): Attach evrp_folder class, initialize 	it in the ctor.  Remove temporary delegators. 	(evrp_dom_walker::before_dom_children): Call methods in attached 	evrp_range_analyzer class via class instance pointer.  Use 	free value_range_constant_singleton to remove need for 	op_with_constant_singleton_value delegator method.  Do not 	create a vrp_prop class instance for every call!  Narrow 	scope of a couple variables. 	(evrp_dom_walker::cleanup): Call methods in attached 	evrp_range_analyzer class via class instance pointer. 	* vr-values.h (class vr_values): Privatize many methods and 	data members.
    From-SVN: r255074

--- gcc/gimple-ssa-evrp-analyze.h
+++ gcc/gimple-ssa-evrp-analyze.h
@@ -24,16 +24,45 @@ class evrp_range_analyzer
 {
  public:
   evrp_range_analyzer (void);
-  ~evrp_range_analyzer (void) { stack.release (); }
+  ~evrp_range_analyzer (void)
+  {
+    delete vr_values;
+    stack.release ();
+  }
 
   void enter (basic_block);
   void leave (basic_block);
   void record_ranges_from_stmt (gimple *);
 
-  class vr_values vr_values;
+  /* Main interface to retrieve range information.  */
+  value_range *get_value_range (const_tree op)
+    { return vr_values->get_value_range (op); }
+
+  /* Dump all the current value ranges.  This is primarily
+     a debugging interface.  */
+  void dump_all_value_ranges (FILE *fp)
+    { vr_values->dump_all_value_ranges (fp); }
+
+  /* A bit of a wart.  This should ideally go away.  */
+  void vrp_visit_cond_stmt (gcond *cond, edge *e)
+    { return vr_values->vrp_visit_cond_stmt (cond, e); }
+
+  /* Get the underlying vr_values class instance.  If TRANSFER is
+     true, then we are transferring ownership.  Else we keep ownership.
+
+     This should be converted to a unique_ptr.  */
+  class vr_values *get_vr_values (bool transfer)
+    {
+      class vr_values *x = vr_values;
+      if (transfer)
+	vr_values = NULL;
+      return x;
+    }
 
  private:
   DISABLE_COPY_AND_ASSIGN (evrp_range_analyzer);
+  class vr_values *vr_values;
+
   void push_value_range (tree var, value_range *vr);
   value_range *pop_value_range (tree var);
   value_range *try_find_new_range (tree, tree op, tree_code code, tree limit);
@@ -42,30 +71,6 @@ class evrp_range_analyzer
 
   /* STACK holds the old VR.  */
   auto_vec<std::pair <tree, value_range*> > stack;
-
-  /* Temporary delegators.  */
-  value_range *get_value_range (const_tree op)
-    { return vr_values.get_value_range (op); }
-  bool update_value_range (const_tree op, value_range *vr)
-    { return vr_values.update_value_range (op, vr); }
-  void extract_range_from_phi_node (gphi *phi, value_range *vr)
-    { vr_values.extract_range_from_phi_node (phi, vr); }
-  void adjust_range_with_scev (value_range *vr, struct loop *loop,
-                               gimple *stmt, tree var)
-    { vr_values.adjust_range_with_scev (vr, loop, stmt, var); }
-  void extract_range_from_stmt (gimple *stmt, edge *taken_edge_p,
-                                tree *output_p, value_range *vr)
-    { vr_values.extract_range_from_stmt (stmt, taken_edge_p, output_p, vr); }
-  void set_defs_to_varying (gimple *stmt)
-    { return vr_values.set_defs_to_varying (stmt); }
-  void set_vr_value (tree name, value_range *vr)
-    { vr_values.set_vr_value (name, vr); }
-  void extract_range_for_var_from_comparison_expr (tree var,
-						   enum tree_code cond_code,
-						   tree op, tree limit,
-						   value_range *vr_p)
-    { vr_values.extract_range_for_var_from_comparison_expr (var, cond_code,
-							    op, limit, vr_p); }
 };
 
 #endif /* GCC_GIMPLE_SSA_EVRP_ANALYZE_H */
