3b575376fbba16c3be9f607fbf53a6c841b3baae
Jakub Jelinek <jakub@gcc.gnu.org>
2017-11-23 21:16:38 +0100

    re PR fortran/81841 (THREADPRIVATE (OpenMP) wrongly rejected in BLOCK DATA)
    	PR fortran/81841
    	* parse.c (parse_spec): Allow ST_OMP_THREADPRIVATE inside of
    	BLOCK DATA.
    
    	* libgomp.fortran/pr81841.f90: New test.
    
    From-SVN: r255115

---
+++ libgomp/testsuite/libgomp.fortran/pr81841.f90
@@ -0,0 +1,26 @@
+! PR fortran/81841
+! { dg-do run }
+
+block data
+  integer :: a
+  real :: b(2)
+  common /c/ a, b
+  !$omp threadprivate (/c/)
+  data a / 32 /
+  data b /2*1./
+end
+
+program pr81841
+  use omp_lib
+  integer :: e
+  real :: f(2)
+  common /c/ e, f
+  !$omp threadprivate (/c/)
+  !$omp parallel num_threads(8)
+  if ((e /= 32) .or. any(f /= 1.)) call abort
+  e = omp_get_thread_num ()
+  f = e + 19.
+  !$omp barrier
+  if ((e /= omp_get_thread_num ()) .or. any(f /= e + 19.)) call abort
+  !$omp end parallel
+end
