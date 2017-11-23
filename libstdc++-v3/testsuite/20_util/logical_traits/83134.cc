366595fd959e7fd047372e25aa5be44dac663ef5
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-11-23 21:35:27 +0000

    PR libstdc++/83134 Ensure std::__not_ converts B::value to bool
    	PR libstdc++/83134
    	* include/std/type_traits (__not_): Explicitly convert to bool.
    	* testsuite/20_util/declval/requirements/1_neg.cc: Adjust dg-error.
    	* testsuite/20_util/logical_traits/83134.cc: New test.
    	* testsuite/20_util/make_signed/requirements/typedefs_neg.cc: Adjust
    	dg-error.
    	* testsuite/20_util/make_unsigned/requirements/typedefs_neg.cc:
    	Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@255122 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/20_util/logical_traits/83134.cc
+++ libstdc++-v3/testsuite/20_util/logical_traits/83134.cc
@@ -1,28 +1,32 @@
-// { dg-do compile { target c++11 } }
-// 2009-11-12  Paolo Carlini  <paolo.carlini@oracle.com>
-//
-// Copyright (C) 2009-2017 Free Software Foundation, Inc.
+// Copyright (C) 2017 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the
 // Free Software Foundation; either version 3, or (at your option)
 // any later version.
-//
+
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-//
+
 // You should have received a copy of the GNU General Public License along
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-error "declval.. must not be used" "" { target *-*-* } 0 }
+// { dg-options "-std=gnu++17" }
+// { dg-do compile { target c++17 } }
 
-#include <utility>
+#include <type_traits>
 
-void test01()
-{
-  std::declval<int>();		// { dg-error "required from here" }
-}
+struct X {
+  constexpr operator bool() const { return false; }
+  constexpr bool operator!() const = delete;
+};
+
+struct Y {
+  static constexpr X value{};
+};
+
+static_assert(std::negation<Y>::value);  // PR libstdc++/83134
