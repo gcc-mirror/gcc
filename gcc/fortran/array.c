8826a41a05aa81b58412408d1e8575aad7df0dc6
Soren Rasmussen <s.c.rasmussen@gmail.com>
2017-11-28 11:18:54 +0000

    Revert "Merge branch 'teams' into master"
    This reverts commit 96ce792b14349bf89efef57419976f33b5dee2d4, reversing
    changes made to afff591a39ec812224ad3c5c969bb405d7d2b55b.

--- gcc/fortran/array.c
+++ gcc/fortran/array.c
@@ -231,20 +231,7 @@ coarray:
 	return MATCH_ERROR;
 
       stat_just_seen = false;
-
-      if (gfc_match (" , team = %e", &tmp) == MATCH_YES && ar->stat == NULL)
-	{
-	  ar->team = tmp;
-	  team_just_seen = true;
-	}
-
-      if (ar->team && !team_just_seen)
-	{
-	  gfc_error ("TEAM= attribute in %C misplaced");
-	  return MATCH_ERROR;
-	}
-      
-      if (gfc_match (" , stat = %e",&tmp) == MATCH_YES && ar->stat == NULL)
+      if (gfc_match(" , stat = %e",&tmp) == MATCH_YES && ar->stat == NULL)
 	{
 	  ar->stat = tmp;
 	  stat_just_seen = true;
--- gcc/fortran/trans-intrinsic.c
+++ gcc/fortran/trans-intrinsic.c
@@ -1850,7 +1850,7 @@ conv_caf_send (gfc_code *code) {
   gfc_se lhs_se, rhs_se;
   stmtblock_t block;
   tree caf_decl, token, offset, image_index, tmp, lhs_kind, rhs_kind;
-  tree may_require_tmp, src_stat, dst_stat, dst_team;
+  tree may_require_tmp, src_stat, dst_stat;
   tree lhs_type = NULL_TREE;
   tree vec = null_pointer_node, rhs_vec = null_pointer_node;
   symbol_attribute lhs_caf_attr, rhs_caf_attr;
@@ -1866,7 +1866,6 @@ conv_caf_send (gfc_code *code) {
   lhs_caf_attr = gfc_caf_attr (lhs_expr);
   rhs_caf_attr = gfc_caf_attr (rhs_expr);
   src_stat = dst_stat = null_pointer_node;
-  dst_team = null_pointer_node;
 
   /* LHS.  */
   gfc_init_se (&lhs_se, NULL);
