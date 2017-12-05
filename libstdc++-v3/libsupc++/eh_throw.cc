0382bcfcda056493544fc1bfa25030e19f465498
Jason Merrill <jason@redhat.com>
2017-12-05 10:23:26 -0500

    Correct noexcept mismatch in declarations.
    2017-12-05  Jason Merrill  <jason@redhat.com>
    	    Jonathan Wakely  <jwakely@redhat.com>
    
    	* include/bits/fs_fwd.h (copy, copy_file): Remove noexcept.
    	(permissions): Add noexcept.
    	* include/bits/fs_ops.h (permissions): Add noexcept.
    	* libsupc++/eh_throw.cc (__cxa_init_primary_exception): Add
    	_GLIBCXX_NOTHROW.
    
    Co-Authored-By: Jonathan Wakely <jwakely@redhat.com>
    
    From-SVN: r255425

--- libstdc++-v3/libsupc++/eh_throw.cc
+++ libstdc++-v3/libsupc++/eh_throw.cc
@@ -53,8 +53,10 @@ __gxx_exception_cleanup (_Unwind_Reason_Code code, _Unwind_Exception *exc)
 }
 
 extern "C" __cxa_refcounted_exception*
-__cxxabiv1::__cxa_init_primary_exception(void *obj, std::type_info *tinfo,
-                                         void (_GLIBCXX_CDTOR_CALLABI *dest) (void *))
+__cxxabiv1::
+__cxa_init_primary_exception(void *obj, std::type_info *tinfo,
+			     void (_GLIBCXX_CDTOR_CALLABI *dest) (void *))
+_GLIBCXX_NOTHROW
 {
   __cxa_refcounted_exception *header
     = __get_refcounted_exception_header_from_obj (obj);
