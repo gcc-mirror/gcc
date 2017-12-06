25b15e953ac48e5042e27a4419ca20211403b7cb
Martin Sebor <msebor@redhat.com>
2017-12-06 17:47:45 +0000

    PR tree-optimization/83075 - Invalid strncpy optimization
    gcc/ChangeLog:
    
    	PR tree-optimization/83075
    	* tree-ssa-strlen.c (handle_builtin_stxncpy): Avoid assuming
    	strncat/strncpy don't change length of source string.
    
    gcc/testsuite/ChangeLog:
    
    	PR tree-optimization/83075
    	* gcc.dg/tree-ssa/strncat.c: New test.
    	* gcc.dg/tree-ssa/strncpy-2.c: Same.
    
    From-SVN: r255446

---
+++ gcc/testsuite/gcc.dg/tree-ssa/strncat.c
@@ -0,0 +1,19 @@
+/* PR tree-optimization/83075 - Invalid strncpy optimization
+   { dg-do run }
+   { dg-options "-O2 -Wno-stringop-overflow" } */
+
+int main (void)
+{
+  char a[8] = "";
+
+  __builtin_strcpy (a, "123");
+
+  unsigned n0 = __builtin_strlen (a);
+
+  __builtin_strncat (a + 3, a, n0);
+
+  unsigned n1 = __builtin_strlen (a);
+
+  if (n1 == n0)
+    __builtin_abort ();
+}
