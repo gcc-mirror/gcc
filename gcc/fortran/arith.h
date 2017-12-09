c01b410c541da013b69a964def3c09a93dab4480
tkoenig <tkoenig@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-12-09 08:37:17 +0000

    2017-12-09  Thomas Koenig  <tkoenig@gcc.gnu.org>
    	PR fortran/83316
    	* arith.c (gfc_character2character): New function.
    	* arith.h: Add prototype.
    	* simplify.c (gfc_convert_constant): Handle character type.
    
    2017-12-09  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/83316
    	* gfortran.dg/minval_char_5.f90: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@255522 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/arith.h
+++ gcc/fortran/arith.h
@@ -82,6 +82,7 @@ gfc_expr *gfc_hollerith2real (gfc_expr *, int);
 gfc_expr *gfc_hollerith2complex (gfc_expr *, int);
 gfc_expr *gfc_hollerith2character (gfc_expr *, int);
 gfc_expr *gfc_hollerith2logical (gfc_expr *, int);
+gfc_expr *gfc_character2character (gfc_expr *, int);
 
 #endif /* GFC_ARITH_H  */
 
