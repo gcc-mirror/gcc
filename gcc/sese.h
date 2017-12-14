e64905d8771745218738e7e9802f2a5f673afaa4
Richard Biener <rguenther@suse.de>
2017-12-14 14:53:40 +0000

    re PR tree-optimization/67842 (Incorrect check in sese.h:bb_in_region)
    2017-12-14  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/67842
    	* sese.h (bb_in_region): Remove #if 0'ed code.
    
    From-SVN: r255636

--- gcc/sese.h
+++ gcc/sese.h
@@ -120,20 +120,6 @@ sese_nb_params (sese_info_p region)
 static inline bool
 bb_in_region (const_basic_block bb, const_basic_block entry, const_basic_block exit)
 {
-  /* FIXME: PR67842.  */
-#if 0
-  if (flag_checking)
-    {
-      edge e;
-      edge_iterator ei;
-
-      /* Check that there are no edges coming in the region: all the
-	 predecessors of EXIT are dominated by ENTRY.  */
-      FOR_EACH_EDGE (e, ei, exit->preds)
-	gcc_assert (dominated_by_p (CDI_DOMINATORS, e->src, entry));
-    }
-#endif
-
   return dominated_by_p (CDI_DOMINATORS, bb, entry)
 	 && !(dominated_by_p (CDI_DOMINATORS, bb, exit)
 	      && !dominated_by_p (CDI_DOMINATORS, entry, exit));
