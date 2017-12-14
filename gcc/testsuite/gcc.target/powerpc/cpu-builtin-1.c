5a86824aaea39155a13aea2e358d71e57cef3b68
bergner <bergner@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-12-14 03:43:10 +0000

    gcc/ 	Backport from mainline 	2017-12-13  Peter Bergner  <bergner@vnet.ibm.com>
    	* config/rs6000/ppc-auxv.h (PPC_FEATURE2_HTM_NO_SUSPEND): New define.
    	* config/rs6000/rs6000.c (cpu_supports_info): Use it.
    
    gcc/testsuite/
    	Backport from mainline
    	2017-12-13  Peter Bergner  <bergner@vnet.ibm.com>
    
    	* gcc.target/powerpc/cpu-builtin-1.c (htm-no-suspend): Add test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@255622 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/cpu-builtin-1.c
+++ gcc/testsuite/gcc.target/powerpc/cpu-builtin-1.c
@@ -73,6 +73,7 @@ use_cpu_supports_builtins (unsigned int *p)
   p[37] = __builtin_cpu_supports ("vsx");
   p[38] = __builtin_cpu_supports ("darn");
   p[39] = __builtin_cpu_supports ("scv");
+  p[40] = __builtin_cpu_supports ("htm-no-suspend");
 #else
   p[0] = 0;
 #endif
