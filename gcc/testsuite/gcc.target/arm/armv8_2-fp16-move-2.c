4e3bc545b28bb80e66e1bc23866ee09662e47780
sudi <sudi@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-12-15 09:59:24 +0000

    Fix regression on soft float targets for armv8_2-fp16-move-2.c
    *** gcc/testsuite/ChangeLog ***
    
    2017-12-15  Sudakshina Das  <sudi.das@arm.com>
    
    	* gcc.target/arm/armv8_2-fp16-move-2.c: Remove dg-add-options.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@255681 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/armv8_2-fp16-move-2.c
+++ gcc/testsuite/gcc.target/arm/armv8_2-fp16-move-2.c
@@ -1,7 +1,6 @@
 /* { dg-do compile }  */
 /* { dg-require-effective-target arm_v8_2a_fp16_scalar_ok }  */
 /* { dg-options "-O2 -mfpu=fp-armv8 -march=armv8.2-a+fp16 -marm -mfloat-abi=hard" }  */
-/* { dg-add-options arm_v8_2a_fp16_scalar }  */
 
 __fp16
 test_select (__fp16 a, __fp16 b, __fp16 c)
