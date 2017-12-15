4cb8942fc5e8f0306532ac1c8efece95d5358d8a
kyukhin <kyukhin@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-12-15 04:05:17 +0000

    Enable VAES support [3/5]
    gcc/
    	* config/i386/i386-builtin.def (__builtin_ia32_vaesdeclast_v16qi,
    	__builtin_ia32_vaesdeclast_v32qi, __builtin_ia32_vaesdeclast_v64qi): New.
    	* config/i386/sse.md (vaesdeclast_<mode>): New pattern.
    	* config/i386/vaesintrin.h (_mm256_aesdeclast_epi128,
    	_mm512_aesdeclast_epi128, _mm_aesdeclast_epi128): New intrinsics.
    
    gcc/testsuite/
    	* gcc.target/i386/avx512f-aesdeclast-2.c: New test.
    	* gcc.target/i386/avx512vl-aesdeclast-2.c
    	* gcc.target/i386/avx512fvl-vaes-1.c: Handle new intrinsics.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@255674 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/vaesintrin.h
+++ gcc/config/i386/vaesintrin.h
@@ -14,6 +14,14 @@ _mm256_aesdec_epi128 (__m256i __A, __m256i __B)
   return (__m256i)__builtin_ia32_vaesdec_v32qi ((__v32qi) __A, (__v32qi) __B);
 }
 
+extern __inline __m256i
+__attribute__((__gnu_inline__, __always_inline__, __artificial__))
+_mm256_aesdeclast_epi128 (__m256i __A, __m256i __B)
+{
+  return (__m256i)__builtin_ia32_vaesdeclast_v32qi ((__v32qi) __A,
+								(__v32qi) __B);
+}
+
 #ifdef __DISABLE_VAES__
 #undef __DISABLE_VAES__
 #pragma GCC pop_options
@@ -34,6 +42,14 @@ _mm512_aesdec_epi128 (__m512i __A, __m512i __B)
   return (__m512i)__builtin_ia32_vaesdec_v64qi ((__v64qi) __A, (__v64qi) __B);
 }
 
+extern __inline __m512i
+__attribute__((__gnu_inline__, __always_inline__, __artificial__))
+_mm512_aesdeclast_epi128 (__m512i __A, __m512i __B)
+{
+  return (__m512i)__builtin_ia32_vaesdeclast_v64qi ((__v64qi) __A,
+						    (__v64qi) __B);
+}
+
 #ifdef __DISABLE_VAESF__
 #undef __DISABLE_VAESF__
 #pragma GCC pop_options
@@ -50,6 +66,14 @@ __attribute__((__gnu_inline__, __always_inline__, __artificial__))
 _mm_aesdec_epi128 (__m128i __A, __m128i __B)
 {
   return (__m128i)__builtin_ia32_vaesdec_v16qi ((__v16qi) __A, (__v16qi) __B);
+}
+
+extern __inline __m128i
+__attribute__((__gnu_inline__, __always_inline__, __artificial__))
+_mm_aesdeclast_epi128 (__m128i __A, __m128i __B)
+{
+  return (__m128i)__builtin_ia32_vaesdeclast_v16qi ((__v16qi) __A,
+						    (__v16qi) __B);
 }
 
 #ifdef __DISABLE_VAESVL__
