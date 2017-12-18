a61b14d20cbf128c03e11465e6cc14aa583ecc09
Soren Rasmussen <s.c.rasmussen@gmail.com>
2017-12-17 18:07:19 -0800

    Minimizing code per pull request recommendation

--- gcc/testsuite/gfortran.dg/teams_change_1.f90
+++ gcc/testsuite/gfortran.dg/teams_change_1.f90
@@ -6,11 +6,10 @@
   use iso_fortran_env, only : team_type
   implicit none
   type(team_type) :: team
-  integer :: orig_i, new_i
+  integer :: orig_i
 
   orig_i = this_image ()
   form team (orig_i + 1,team)
   change team (team)
-  new_i = this_image ()
-  if ( orig_i + 1 .NE. new_i ) call abort
+  if ( orig_i + 1 .NE. this_image() ) call abort
 end
