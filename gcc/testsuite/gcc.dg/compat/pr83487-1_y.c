92ca6f7403a7e26904a71b546648bb3cf54dd571
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-12-21 23:10:45 +0000

    PR middle-end/83487 	* config/i386/i386.c (ix86_function_arg_boundary): Return 	PARM_BOUNDARY for TYPE_EMPTY_P types.
    	* gcc.c-torture/compile/pr83487.c: New test.
    	* gcc.dg/compat/pr83487-1.h: New file.
    	* gcc.dg/compat/pr83487-1_main.c: New test.
    	* gcc.dg/compat/pr83487-1_x.c: New file.
    	* gcc.dg/compat/pr83487-1_y.c: New file.
    	* gcc.dg/compat/pr83487-2_main.c: New test.
    	* gcc.dg/compat/pr83487-2_x.c: New file.
    	* gcc.dg/compat/pr83487-2_y.c: New file.
    	* g++.dg/abi/pr83487.C: New test.
    	* g++.dg/compat/abi/pr83487-1_main.C: New test.
    	* g++.dg/compat/abi/pr83487-1_x.C: New file.
    	* g++.dg/compat/abi/pr83487-1_y.C: New file.
    	* g++.dg/compat/abi/pr83487-2_main.C: New test.
    	* g++.dg/compat/abi/pr83487-2_x.C: New file.
    	* g++.dg/compat/abi/pr83487-2_y.C: New file.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@255961 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/compat/pr83487-1_y.c
@@ -0,0 +1,27 @@
+#include "pr83487-1.h"
+
+struct A a;
+struct B b;
+struct C c;
+
+extern void f1 (int i, int j, int k, int l, int m, int n, int o, struct A);
+extern void f2 (int i, int j, int k, int l, int m, int n, int o, struct A, int p, int q);
+extern void f3 (int i, int j, int k, int l, int m, int n, int o, struct B, int p, int q);
+extern void f4 (int i, int j, int k, int l, int m, int n, int o, struct C, int p, int q);
+extern void f5 (int o, struct A);
+extern void f6 (int o, struct A, int p, int q);
+extern void f7 (int o, struct B, int p, int q);
+extern void f8 (int o, struct C, int p, int q);
+
+void
+do_test ()
+{
+  f1 (6, 0, 1, 2, 3, 4, 5, a);
+  f2 (6, 0, 1, 2, 3, 4, 5, a, 7, 8);
+  f3 (6, 0, 1, 2, 3, 4, 5, b, 7, 8);
+  f4 (6, 0, 1, 2, 3, 4, 5, c, 7, 8);
+  f5 (5, a);
+  f6 (5, a, 7, 8);
+  f7 (5, b, 7, 8);
+  f8 (5, c, 7, 8);
+}
