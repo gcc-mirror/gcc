0a552ae22a4b7d4a71afab9f845398597a4d230d
Jakub Jelinek <jakub@redhat.com>
2018-01-02 19:04:19 +0100

    re PR c++/83556 (ICE in gimplify_expr, at gimplify.c:12004)
    	PR c++/83556
    	* tree.c (replace_placeholders_r): Pass NULL as last argument to
    	cp_walk_tree instead of d->pset.  If non-TREE_CONSTANT and
    	non-PLACEHOLDER_EXPR tree has been seen already, set *walk_subtrees
    	to false and return.
    	(replace_placeholders): Pass NULL instead of &pset as last argument
    	to cp_walk_tree.
    
    	* g++.dg/cpp0x/pr83556.C: New test.
    
    From-SVN: r256086

--- gcc/cp/tree.c
+++ gcc/cp/tree.c
@@ -3106,6 +3106,11 @@ replace_placeholders_r (tree* t, int* walk_subtrees, void* data_)
       {
 	constructor_elt *ce;
 	vec<constructor_elt,va_gc> *v = CONSTRUCTOR_ELTS (*t);
+	if (d->pset->add (*t))
+	  {
+	    *walk_subtrees = false;
+	    return NULL_TREE;
+	  }
 	for (unsigned i = 0; vec_safe_iterate (v, i, &ce); ++i)
 	  {
 	    tree *valp = &ce->value;
@@ -3125,7 +3130,7 @@ replace_placeholders_r (tree* t, int* walk_subtrees, void* data_)
 		  valp = &TARGET_EXPR_INITIAL (*valp);
 	      }
 	    d->obj = subob;
-	    cp_walk_tree (valp, replace_placeholders_r, data_, d->pset);
+	    cp_walk_tree (valp, replace_placeholders_r, data_, NULL);
 	    d->obj = obj;
 	  }
 	*walk_subtrees = false;
@@ -3133,6 +3138,8 @@ replace_placeholders_r (tree* t, int* walk_subtrees, void* data_)
       }
 
     default:
+      if (d->pset->add (*t))
+	*walk_subtrees = false;
       break;
     }
 
@@ -3161,7 +3168,7 @@ replace_placeholders (tree exp, tree obj, bool *seen_p)
   replace_placeholders_t data = { obj, false, &pset };
   if (TREE_CODE (exp) == TARGET_EXPR)
     tp = &TARGET_EXPR_INITIAL (exp);
-  cp_walk_tree (tp, replace_placeholders_r, &data, &pset);
+  cp_walk_tree (tp, replace_placeholders_r, &data, NULL);
   if (seen_p)
     *seen_p = data.seen;
   return exp;
