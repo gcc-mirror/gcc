277184bc4daf497bf43fdee356277ee2b5eb31f1
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-01-03 09:17:54 +0000

    gcc/ 	* gcc.c (process_command): Update copyright notice dates. 	* gcov-dump.c (print_version): Ditto. 	* gcov.c (print_version): Ditto. 	* gcov-tool.c (print_version): Ditto. 	* gengtype.c (create_file): Ditto. 	* doc/cpp.texi: Bump @copying's copyright year. 	* doc/cppinternals.texi: Ditto. 	* doc/gcc.texi: Ditto. 	* doc/gccint.texi: Ditto. 	* doc/gcov.texi: Ditto. 	* doc/install.texi: Ditto. 	* doc/invoke.texi: Ditto. gcc/fortran/ 	* gfortranspec.c (lang_specific_driver): Update copyright notice 	dates. 	* gfc-internals.texi: Bump @copying's copyright year. 	* gfortran.texi: Ditto. 	* intrinsic.texi: Ditto. 	* invoke.texi: Ditto. gcc/ada/  	* gnat_ugn.texi: Bump @copying's copyright year.  	* gnat_rm.texi: Likewise. gcc/go/ 	* gccgo.texi: Bump @copyrights-go year. libitm/ 	* libitm.texi: Bump @copying's copyright year. libgomp/ 	* libgomp.texi: Bump @copying's copyright year. libquadmath/ 	* libquadmath.texi: Bump @copying's copyright year.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@256166 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/gcov-dump.c
+++ gcc/gcov-dump.c
@@ -148,7 +148,7 @@ static void
 print_version (void)
 {
   printf ("gcov-dump %s%s\n", pkgversion_string, version_string);
-  printf ("Copyright (C) 2017 Free Software Foundation, Inc.\n");
+  printf ("Copyright (C) 2018 Free Software Foundation, Inc.\n");
   printf ("This is free software; see the source for copying conditions.\n"
   	  "There is NO warranty; not even for MERCHANTABILITY or \n"
 	  "FITNESS FOR A PARTICULAR PURPOSE.\n\n");
