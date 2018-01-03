ca04a532f3fcf91939ec03da628aa94207eaa067
Martin Liska <mliska@suse.cz>
2018-01-03 15:15:58 +0100

    Do not inline variadic thunks (PR ipa/83549).
    2018-01-03  Martin Liska  <mliska@suse.cz>
    
    	PR ipa/83549
    	* cif-code.def (VARIADIC_THUNK): New enum value.
    	* ipa-fnsummary.c (compute_fn_summary): Do not inline variadic
    	thunks.
    2018-01-03  Martin Liska  <mliska@suse.cz>
    
    	PR ipa/83549
    	* g++.dg/ipa/pr83549.C: New test.
    
    From-SVN: r256177

--- gcc/ipa-fnsummary.c
+++ gcc/ipa-fnsummary.c
@@ -2422,6 +2422,11 @@ compute_fn_summary (struct cgraph_node *node, bool early)
           info->inlinable = false;
           node->callees->inline_failed = CIF_CHKP;
 	}
+      else if (stdarg_p (TREE_TYPE (node->decl)))
+	{
+	  info->inlinable = false;
+	  node->callees->inline_failed = CIF_VARIADIC_THUNK;
+	}
       else
         info->inlinable = true;
     }
