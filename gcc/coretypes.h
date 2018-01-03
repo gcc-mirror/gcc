b660eccf9b32ee3b962a77cf5565fa2771792c35
Richard Sandiford <richard.sandiford@linaro.org>
2018-01-03 21:39:45 +0000

    Add a fixed_size_mode_pod class
    This patch adds a POD version of fixed_size_mode.  The only current use
    is for storing the __builtin_apply and __builtin_result register modes,
    which were made fixed_size_modes by the previous patch.
    
    2018-01-03  Richard Sandiford  <richard.sandiford@linaro.org>
    	    Alan Hayward  <alan.hayward@arm.com>
    	    David Sherwood  <david.sherwood@arm.com>
    
    gcc/
    	* coretypes.h (fixed_size_mode): Declare.
    	(fixed_size_mode_pod): New typedef.
    	* builtins.h (target_builtins::x_apply_args_mode)
    	(target_builtins::x_apply_result_mode): Change type to
    	fixed_size_mode_pod.
    	* builtins.c (apply_args_size, apply_result_size, result_vector)
    	(expand_builtin_apply_args_1, expand_builtin_apply)
    	(expand_builtin_return): Update accordingly.
    
    Co-Authored-By: Alan Hayward <alan.hayward@arm.com>
    Co-Authored-By: David Sherwood <david.sherwood@arm.com>
    
    From-SVN: r256193

--- gcc/coretypes.h
+++ gcc/coretypes.h
@@ -59,6 +59,7 @@ class scalar_mode;
 class scalar_int_mode;
 class scalar_float_mode;
 class complex_mode;
+class fixed_size_mode;
 template<typename> class opt_mode;
 typedef opt_mode<scalar_mode> opt_scalar_mode;
 typedef opt_mode<scalar_int_mode> opt_scalar_int_mode;
@@ -66,6 +67,7 @@ typedef opt_mode<scalar_float_mode> opt_scalar_float_mode;
 template<typename> class pod_mode;
 typedef pod_mode<scalar_mode> scalar_mode_pod;
 typedef pod_mode<scalar_int_mode> scalar_int_mode_pod;
+typedef pod_mode<fixed_size_mode> fixed_size_mode_pod;
 
 /* Subclasses of rtx_def, using indentation to show the class
    hierarchy, along with the relevant invariant.
