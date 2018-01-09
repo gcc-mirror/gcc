f4f867ca3c54b1bc3d7f6bef824a2d7b78f86c59
Aaron Sawdey <acsawdey@gcc.gnu.org>
2018-01-09 14:08:43 -0600

    Part of the fix for pr82190 got undone by my newer memcmp checkin.
    From-SVN: r256392

--- gcc/config/rs6000/rs6000-string.c
+++ gcc/config/rs6000/rs6000-string.c
@@ -1429,14 +1429,14 @@ expand_block_compare (rtx operands[])
 	  rtx src1_reg = copy_addr_to_reg (XEXP (src1, 0));
 	  src1 = replace_equiv_address (src1, src1_reg);
 	}
-      set_mem_size (src1, cmp_bytes);
+      set_mem_size (src1, load_mode_size);
 
       if (!REG_P (XEXP (src2, 0)))
 	{
 	  rtx src2_reg = copy_addr_to_reg (XEXP (src2, 0));
 	  src2 = replace_equiv_address (src2, src2_reg);
 	}
-      set_mem_size (src2, cmp_bytes);
+      set_mem_size (src2, load_mode_size);
 
       do_load_for_compare (tmp_reg_src1, src1, load_mode);
       do_load_for_compare (tmp_reg_src2, src2, load_mode);
