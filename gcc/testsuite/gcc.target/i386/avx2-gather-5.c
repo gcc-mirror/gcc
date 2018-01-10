6fb2015145abfdddd31465eba22182451d2c5407
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-01-10 11:01:25 +0000

    * gcc.target/i386/avx2-gather-1.c: Add -march. 	* gcc.target/i386/avx2-gather-2.c: Add -march. 	* gcc.target/i386/avx2-gather-3.c: Add -march. 	* gcc.target/i386/avx2-gather-4.c: Add -march. 	* gcc.target/i386/avx2-gather-5.c: Add -march. 	* gcc.target/i386/avx2-gather-6.c: Add -march. 	* gcc.target/i386/avx512f-gather-1.c: Add -march. 	* gcc.target/i386/avx512f-gather-2.c: Add -march. 	* gcc.target/i386/avx512f-gather-3.c: Add -march. 	* gcc.target/i386/avx512f-gather-4.c: Add -march. 	* gcc.target/i386/avx512f-gather-5.c: Add -march. 	* gcc.target/i386/avx512f-i32gatherd512-1.c: Add -march. 	* gcc.target/i386/avx512f-i32gatherd512-2.c: Add -march. 	* gcc.target/i386/avx512f-i32gatherpd512-1.c: Add -march. 	* gcc.target/i386/avx512f-i32gatherpd512-2.c: Add -march. 	* gcc.target/i386/avx512f-i32gatherps512-1.c: Add -march.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@256423 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/avx2-gather-5.c
+++ gcc/testsuite/gcc.target/i386/avx2-gather-5.c
@@ -1,6 +1,6 @@
 /* { dg-do run } */
 /* { dg-require-effective-target avx2 } */
-/* { dg-options "-O3 -mavx2 -fno-common" } */
+/* { dg-options "-O3 -mavx2 -fno-common -mtune=skylake" } */
 
 #include "avx2-check.h"
 
