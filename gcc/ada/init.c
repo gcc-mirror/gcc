204be4888b44435a8c9854a3e3eabfffd7612f10
pmderodat <pmderodat@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-01-11 08:51:43 +0000

    [Ada] Enable sigtramp for aarch64-vx7
    2018-01-11  Doug Rupp  <rupp@adacore.com>
    
    gcc/ada/
    
    	* init.c (vxworks): Add macro checks for __aarch64__.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@256494 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/init.c
+++ gcc/ada/init.c
@@ -1937,7 +1937,7 @@ __gnat_map_signal (int sig,
   Raise_From_Signal_Handler (exception, msg);
 }
 
-#if defined (ARMEL) && (_WRS_VXWORKS_MAJOR >= 7)
+#if defined (ARMEL) && (_WRS_VXWORKS_MAJOR >= 7) || defined (__aarch64__)
 
 /* ARM-vx7 case with arm unwinding exceptions */
 #define HAVE_GNAT_ADJUST_CONTEXT_FOR_RAISE
@@ -2005,7 +2005,7 @@ __gnat_error_handler (int sig, siginfo_t *si, void *sc)
   sigdelset (&mask, sig);
   sigprocmask (SIG_SETMASK, &mask, NULL);
 
-#if defined (__ARMEL__) || defined (__PPC__) || defined (__i386__) || defined (__x86_64__)
+#if defined (__ARMEL__) || defined (__PPC__) || defined (__i386__) || defined (__x86_64__) || defined (__aarch64__)
   /* On certain targets, kernel mode, we process signals through a Call Frame
      Info trampoline, voiding the need for myriads of fallback_frame_state
      variants in the ZCX runtime.  We have no simple way to distinguish ZCX
