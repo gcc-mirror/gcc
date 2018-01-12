27af02e20efddd9db5978b8c2a88bdfdb88638ae
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-01-12 18:20:49 +0000

    PR debug/81155 	* bb-reorder.c (pass_partition_blocks::gate): In lto don't partition 	main to workaround a bug in GDB.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@256592 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/bb-reorder.c
+++ gcc/bb-reorder.c
@@ -2873,7 +2873,10 @@ pass_partition_blocks::gate (function *fun)
 	     we are going to omit the reordering.  */
 	  && optimize_function_for_speed_p (fun)
 	  && !DECL_COMDAT_GROUP (current_function_decl)
-	  && !lookup_attribute ("section", DECL_ATTRIBUTES (fun->decl)));
+	  && !lookup_attribute ("section", DECL_ATTRIBUTES (fun->decl))
+	  /* Workaround a bug in GDB where read_partial_die doesn't cope
+	     with DIEs with DW_AT_ranges, see PR81115.  */
+	  && !(in_lto_p && MAIN_NAME_P (DECL_NAME (fun->decl))));
 }
 
 unsigned
