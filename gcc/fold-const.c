a90455f4475f9dcdba4fe14ef1ab70c726ae06ed
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-01-13 17:57:17 +0000

    Fix folding of vector mask EQ/NE expressions
    fold_binary_loc assumed that if the type of the result wasn't a vector,
    the operands wouldn't be either.  This isn't necessarily true for
    EQ_EXPR and NE_EXPR of vector masks, which can return a single scalar
    for the mask as a whole.
    
    2018-01-13  Richard Sandiford  <richard.sandiford@linaro.org>
    	    Alan Hayward  <alan.hayward@arm.com>
    	    David Sherwood  <david.sherwood@arm.com>
    
    gcc/
    	* fold-const.c (fold_binary_loc): Check the argument types
    	rather than the result type when testing for a vector operation.
    
    gcc/testsuite/
    	* gcc.target/aarch64/sve/vec_bool_cmp_1.c: New test.
    	* gcc.target/aarch64/sve/vec_bool_cmp_1_run.c: Likweise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@256616 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fold-const.c
+++ gcc/fold-const.c
@@ -9323,7 +9323,7 @@ fold_binary_loc (location_t loc, enum tree_code code, tree type,
 
   if ((code == BIT_AND_EXPR || code == BIT_IOR_EXPR
        || code == EQ_EXPR || code == NE_EXPR)
-      && TREE_CODE (type) != VECTOR_TYPE
+      && !VECTOR_TYPE_P (TREE_TYPE (arg0))
       && ((truth_value_p (TREE_CODE (arg0))
 	   && (truth_value_p (TREE_CODE (arg1))
 	       || (TREE_CODE (arg1) == BIT_AND_EXPR
