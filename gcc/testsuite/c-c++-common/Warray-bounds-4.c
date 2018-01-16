d27778244bf57066baf2b47919d26e362d030d47
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-01-16 22:38:49 +0000

    * c-c++-common/Warray-bounds-4.c (test_strcpy_bounds_memarray_range): 	XFAIL last test on SPARC and Visium.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@256763 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/c-c++-common/Warray-bounds-4.c
+++ gcc/testsuite/c-c++-common/Warray-bounds-4.c
@@ -64,5 +64,5 @@ void test_strcpy_bounds_memarray_range (void)
   TM ("01", "",    ma.a5 + i, ma.a5);
   TM ("012", "",   ma.a5 + i, ma.a5);
   TM ("0123", "",  ma.a5 + i, ma.a5);     /* { dg-warning "offset 6 from the object at .ma. is out of the bounds of referenced subobject .a5. with type .char\\\[5]. at offset 0" "strcpy" { xfail *-*-* } } */
-  TM ("", "012345", ma.a7 + i, ma.a7);    /* { dg-warning "offset 13 from the object at .ma. is out of the bounds of referenced subobject .\(MA::\)?a7. with type .char ?\\\[7]. at offset 5" } */
+  TM ("", "012345", ma.a7 + i, ma.a7);    /* { dg-warning "offset 13 from the object at .ma. is out of the bounds of referenced subobject .\(MA::\)?a7. with type .char ?\\\[7]. at offset 5" "strcpy" { xfail sparc*-*-* visium-*-* } } */
 }
