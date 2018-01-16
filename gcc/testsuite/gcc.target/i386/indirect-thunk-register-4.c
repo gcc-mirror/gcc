3a205fe54c65d0fb6315162ff47b1ef27c52012a
H.J. Lu <hongjiu.lu@intel.com>
2018-01-16 11:19:51 +0000

    x86: Add 'V' register operand modifier
    Add 'V', a special modifier which prints the name of the full integer
    register without '%'.  For
    
    extern void (*func_p) (void);
    
    void
    foo (void)
    {
      asm ("call __x86_indirect_thunk_%V0" : : "a" (func_p));
    }
    
    it generates:
    
    foo:
    	movq	func_p(%rip), %rax
    	call	__x86_indirect_thunk_rax
    	ret
    
    gcc/
    
    	Backport from mainline
    	2018-01-14  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* config/i386/i386.c (print_reg): Print the name of the full
    	integer register without '%'.
    	(ix86_print_operand): Handle 'V'.
    	 * doc/extend.texi: Document 'V' modifier.
    
    gcc/testsuite/
    
    	Backport from mainline
    	2018-01-14  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* gcc.target/i386/indirect-thunk-register-4.c: New test.
    
    From-SVN: r256736

---
+++ gcc/testsuite/gcc.target/i386/indirect-thunk-register-4.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -mindirect-branch=keep -fno-pic" } */
+
+extern void (*func_p) (void);
+
+void
+foo (void)
+{
+  asm("call __x86_indirect_thunk_%V0" : : "a" (func_p));
+}
+
+/* { dg-final { scan-assembler "call\[ \t\]*__x86_indirect_thunk_eax" { target ia32 } } } */
+/* { dg-final { scan-assembler "call\[ \t\]*__x86_indirect_thunk_rax" { target { ! ia32 } } } } */
