d56301ce0da31950df7a9aaeffdf733162f1d304
wschmidt <wschmidt@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-01-18 01:34:10 +0000

    [gcc]
    2018-01-17  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2018-01-16  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* config/rs6000/rs6000.c (rs6000_opt_vars): Add entry for
    	-mspeculate-indirect-jumps.
    	* config/rs6000/rs6000.md (*call_indirect_elfv2<mode>): Disable
    	for -mno-speculate-indirect-jumps.
    	(*call_indirect_elfv2<mode>_nospec): New define_insn.
    	(*call_value_indirect_elfv2<mode>): Disable for
    	-mno-speculate-indirect-jumps.
    	(*call_value_indirect_elfv2<mode>_nospec): New define_insn.
    	(indirect_jump): Emit different RTL for
    	-mno-speculate-indirect-jumps.
    	(*indirect_jump<mode>): Disable for
    	-mno-speculate-indirect-jumps.
    	(*indirect_jump<mode>_nospec): New define_insn.
    	(tablejump): Emit different RTL for
    	-mno-speculate-indirect-jumps.
    	(tablejumpsi): Disable for -mno-speculate-indirect-jumps.
    	(tablejumpsi_nospec): New define_expand.
    	(tablejumpdi): Disable for -mno-speculate-indirect-jumps.
    	(tablejumpdi_nospec): New define_expand.
    	(*tablejump<mode>_internal1): Disable for
    	-mno-speculate-indirect-jumps.
    	(*tablejump<mode>_internal1_nospec): New define_insn.
    	* config/rs6000/rs6000.opt (mspeculate-indirect-jumps): New
    	option.
    
    	Backport from mainline
    	2018-01-17  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* config/rs6000/rs6000.md (*call_indirect_nonlocal_sysv<mode>):
    	Generate different code for -mno-speculate-indirect-jumps.
    	(*call_value_indirect_nonlocal_sysv<mode>): Likewise.
    	(*call_indirect_aix<mode>): Disable for
    	-mno-speculate-indirect-jumps.
    	(*call_indirect_aix<mode>_nospec): New define_insn.
    	(*call_value_indirect_aix<mode>): Disable for
    	-mno-speculate-indirect-jumps.
    	(*call_value_indirect_aix<mode>_nospec): New define_insn.
    	(*sibcall_nonlocal_sysv<mode>): Generate different code for
    	-mno-speculate-indirect-jumps.
    	(*sibcall_value_nonlocal_sysv<mode>): Likewise.
    
    [gcc/testsuite]
    
    2018-01-17  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	Backport from mainline
    	2018-01-16  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* gcc.target/powerpc/safe-indirect-jump-1.c: New file.
    	* gcc.target/powerpc/safe-indirect-jump-2.c: New file.
    	* gcc.target/powerpc/safe-indirect-jump-3.c: New file.
    	* gcc.target/powerpc/safe-indirect-jump-4.c: New file.
    	* gcc.target/powerpc/safe-indirect-jump-5.c: New file.
    	* gcc.target/powerpc/safe-indirect-jump-6.c: New file.
    
    	Backport from mainline
    	2018-01-17  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* gcc.target/powerpc/safe-indirect-jump-1.c: Remove endian
    	restriction, but still restrict to 64-bit.
    	* gcc.target/powerpc/safe-indirect-jump-7.c: New file.
    	* gcc.target/powerpc/safe-indirect-jump-8.c: New file.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@256832 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/safe-indirect-jump-7.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-additional-options "-mno-speculate-indirect-jumps" } */
+
+/* Test for deliberate misprediction of indirect calls.  */
+
+extern int (*f)();
+
+int bar ()
+{
+  return (*f) () * 53;
+}
+
+/* { dg-final { scan-assembler "crset eq" } } */
+/* { dg-final { scan-assembler "beqctrl-" } } */
