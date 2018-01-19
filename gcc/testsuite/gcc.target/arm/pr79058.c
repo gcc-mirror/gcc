80b21c416cf7ae760b8a96b0971414d026feb03b
Kyrylo Tkachov <kyrylo.tkachov@arm.com>
2018-01-19 09:58:37 +0000

    [arm] Fix gcc.target/arm/pr79058.c
    This testcase tests 32-bit ARM state functionality, so add the -marm to make it explicit
    as well as to avoid Thumb1 hard-float errors for certain toolchain configurations.
    
    
         * gcc.target/arm/pr79058.c: Add arm_arm_ok check and -marm to options.
    
    From-SVN: r256878

--- gcc/testsuite/gcc.target/arm/pr79058.c
+++ gcc/testsuite/gcc.target/arm/pr79058.c
@@ -1,6 +1,7 @@
 /* { dg-do compile } */
+/* { dg-require-effective-target arm_arm_ok } */
 /* { dg-skip-if "do not override -mcpu" { *-*-* } { "-mcpu=*" } { "-mcpu=arm7tdmi" } } */
-/* { dg-options "-Os -mbig-endian -mcpu=arm7tdmi" } */
+/* { dg-options "-Os -mbig-endian -marm -mcpu=arm7tdmi" } */
 
 enum { NILFS_SEGMENT_USAGE_ACTIVE, NILFS_SEGMENT_USAGE_DIRTY } a;
 
