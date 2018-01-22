de0ecff83d8639cfa0075fab7a5f9a42657dd94e
Michael Meissner <meissner@linux.vnet.ibm.com>
2018-01-22 19:36:18 +0000

    re PR target/83862 (powerpc: ICE in signbit testcase)
    [gcc]
    2018-01-22  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	PR target/83862
    	* config/rs6000/rs6000-protos.h (rs6000_split_signbit): Delete,
    	no longer used.
    	* config/rs6000/rs6000.c (rs6000_split_signbit): Likewise.
    	* config/rs6000/rs6000.md (signbit<mode>2): Change code for IEEE
    	128-bit to produce an UNSPEC move to get the double word with the
    	signbit and then a shift directly to do signbit.
    	(signbit<mode>2_dm): Replace old IEEE 128-bit signbit
    	implementation with a new version that just does either a direct
    	move or a regular move.  Move memory interface to separate insns.
    	Move insns so they are next to the expander.
    	(signbit<mode>2_dm_mem_be): New combiner insns to combine load
    	with signbit move.  Split big and little endian case.
    	(signbit<mode>2_dm_mem_le): Likewise.
    	(signbit<mode>2_dm_<su>ext): Delete, no longer used.
    	(signbit<mode>2_dm2): Likewise.
    
    [gcc/testsuite]
    2018-01-22  Michael Meissner  <meissner@linux.vnet.ibm.com>
    
    	PR target/83862
    	* gcc.target/powerpc/pr83862.c: New test.
    
    From-SVN: r256959

---
+++ gcc/testsuite/gcc.target/powerpc/pr83862.c
@@ -0,0 +1,34 @@
+/* PR target/83862.c */
+/* { dg-do compile { target { powerpc*-*-* && lp64 } } } */
+/* { dg-require-effective-target ppc_float128_sw } */
+/* { dg-require-effective-target powerpc_p8vector_ok } */
+/* { dg-options "-mpower8-vector -O2 -mabi=ieeelongdouble -Wno-psabi" } */
+
+/* On little endian systems, optimizing signbit of IEEE 128-bit values from
+   memory could abort if the memory address was indexed (reg+reg).  The
+   optimization is only on 64-bit machines with direct move.
+
+   Compile with -g -O2 -mabi=ieeelongdouble -Wno-psabi.  */
+
+#ifndef TYPE
+#define TYPE long double
+#endif
+
+int sbr (TYPE a) { return __builtin_signbit (a); }
+int sbm (TYPE *a) { return __builtin_signbit (*a); }
+int sbo (TYPE *a) { return __builtin_signbit (a[4]); }
+int sbi (TYPE *a, unsigned long n) { return __builtin_signbit (a[n]); }
+void sbs (int *p, TYPE a) { *p = __builtin_signbit (a); }
+
+/* On big endian systems, this will generate 2 LDs and 1 LDX, while on
+   little endian systems, this will generate 3 LDs and an ADD.  */
+
+/* { dg-final { scan-assembler-times {\mldx?\M}    3 } } */
+/* { dg-final { scan-assembler-times {\mmfvsrd\M}  2 } } */
+/* { dg-final { scan-assembler-times {\msrdi\M}    5 } } */
+/* { dg-final { scan-assembler-not   {\mmfvsrld\M}   } } */
+/* { dg-final { scan-assembler-not   {\mstxvx?\M}    } } */
+/* { dg-final { scan-assembler-not   {\mstxvw4x\M}   } } */
+/* { dg-final { scan-assembler-not   {\mstxvd2x\M}   } } */
+/* { dg-final { scan-assembler-not   {\mstvx\M}      } } */
+
