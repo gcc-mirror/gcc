4a5b2fc90680133f062ddbeaa62423eaa43992e6
Segher Boessenkool <segher@kernel.crashing.org>
2018-01-26 22:31:37 +0100

    rs6000: Fix safe-indirect-jump-[18].c
    Thist patch merges the safe-indirect-jump-1.c and -8.c testcases,
    since they do the same thing.  On the 64-bit and AIX ABIs the indirect
    call is not a sibcall, since there is code generated after the call
    (the restore of r2).  On the 32-bit non-AIX ABIs it is a sibcall.
    
    
    	Backport from trunk
    	2018-01-26  Segher Boessenkool  <segher@kernel.crashing.org>
    
    	* gcc.target/powerpc/safe-indirect-jump-1.c: Build on all targets.
    	Make expected output depend on whether we expect sibcalls or not.
    	* gcc.target/powerpc/safe-indirect-jump-8.c: Delete (merged into
    	safe-indirect-jump-1.c).
    
    	Backport from trunk
    	2018-01-21  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	PR target/83946
    	* gcc.target/powerpc/safe-indirect-jump-8.c: Skip for AIX.
    
    From-SVN: r257110

--- gcc/testsuite/gcc.target/powerpc/safe-indirect-jump-1.c
+++ gcc/testsuite/gcc.target/powerpc/safe-indirect-jump-1.c
@@ -1,4 +1,4 @@
-/* { dg-do compile { target { lp64 } } } */
+/* { dg-do compile } */
 /* { dg-additional-options "-mno-speculate-indirect-jumps" } */
 
 /* Test for deliberate misprediction of indirect calls.  */
@@ -11,4 +11,10 @@ int bar ()
 }
 
 /* { dg-final { scan-assembler "crset 2" } } */
-/* { dg-final { scan-assembler "beqctrl-" } } */
+
+/* The AIX and ELFv2 ABIs don't allow a sibcall here.  */
+/* { dg-final { scan-assembler "beqctrl-" { target { lp64 || { powerpc*-*-aix* } } } } } */
+
+/* The other ABIs do allow a sibcall.  */
+/* { dg-final { scan-assembler "beqctr-" { target { ilp32 && !powerpc*-*-aix* } } } } */
+/* { dg-final { scan-assembler {b \$} { target { ilp32 && !powerpc*-*-aix* } } } } */
