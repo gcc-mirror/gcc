634c3ff01847a2b0918d13d85205779075b86c31
Vladimir Makarov <vmakarov@redhat.com>
2018-01-30 20:28:59 +0000

    re PR target/84112 (powerpc64le ICE in LRA on openjdk)
    2018-01-30  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR target/84112
    	* lra-constraints.c (curr_insn_transform): Process AND in the
    	address.
    
    2018-01-30  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR target/84112
    	* pr84112.c: New.
    
    From-SVN: r257204

---
+++ gcc/testsuite/gcc.target/powerpc/pr84112.c
@@ -0,0 +1,33 @@
+/* { dg-do compile { target powerpc*-*-* } }*/
+/* { dg-options "-mcpu=power8 -O3 -fstack-protector-strong -fpic" } */
+
+char *b;
+int c, d, e, f;
+
+void
+foo (char *h, int k, int l, int m, int j, int q)
+{
+  char *i = b;
+  int n = d;
+  int s = e;
+  while (c)
+    {
+      for (; j <= 0; j += 12)
+	{
+	  i[j] = n & k - h[j] >> 31 | q & ~(k - h[j] >> 31);
+	  i[j + 1] = n & l - h[j + 1] >> 31 | q & ~(l - h[j + 1] >> 31);
+	  i[j + 2] = n & m - h[j + 2] >> 31 | s & ~(m - h[j + 2] >> 31);
+	  i[j + 3] = n & k - h[j + 3] >> 31 | q & ~(k - h[j + 3] >> 31);
+	  i[j + 4] = n & l - h[j + 4] >> 31 | q & ~(l - h[j + 4] >> 31);
+	  i[j + 5] = n & m - h[j + 5] >> 31 | s & ~(m - h[j + 5] >> 31);
+	  i[j + 6] = n & k - h[j + 6] >> 31 | q & ~(k - h[j + 6] >> 31);
+	  i[j + 7] = n & l - h[j + 7] >> 31 | q & ~(l - h[j + 7] >> 31);
+	  i[j + 8] = n & m - h[j + 8] >> 31 | s & ~(m - h[j + 8] >> 31);
+	  i[j + 9] = n & k - h[j + 9] >> 31 | q & ~(k - h[j + 9] >> 31);
+	  i[j + 10] = n & l - h[j + 10] >> 31 | q & ~(l - h[j + 10] >> 31);
+	  i[j + 11] = n & m - h[j + 11] >> 31 | s & ~(m - h[j + 11] >> 31);
+	}
+      while (j < f)
+	;
+    }
+}
