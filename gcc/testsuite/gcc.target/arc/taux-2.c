21d0c9bcff4c84ce3677dccecf1c5c83a04cceaa
claziss <claziss@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-01-31 10:01:48 +0000

    [ARC] Add 'aux' variable attribute.
    The 'aux' variable attribute is used to directly access the auxiliary
    register space from C.
    
    gcc/
    2018-01-31  Claudiu Zissulescu  <claziss@synopsys.com>
    
            * config/arc/arc.c (arc_handle_aux_attribute): New function.
            (arc_attribute_table): Add 'aux' attribute.
            (arc_in_small_data_p): Consider aux like variables.
            (arc_is_aux_reg_p): New function.
            (arc_asm_output_aligned_decl_local): Ignore 'aux' like variables.
            (arc_get_aux_arg): New function.
            (prepare_move_operands): Handle aux-register access.
            (arc_handle_aux_attribute): New function.
            * doc/extend.texi (ARC Variable attributes): Add subsection.
    
    testsuite/
    2018-01-31  Claudiu Zissulescu  <claziss@synopsys.com>
    
            * gcc.target/arc/taux-1.c: New test.
            * gcc.target/arc/taux-2.c: Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@257223 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/arc/taux-2.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 */
+
+#define __aux(r) __attribute__((aux(r)))
+static volatile __aux(0x1000) int var;
+
+int foo (void)
+{
+  var++;
+}
+
+/* { dg-final { scan-assembler-times "sr" 1 } } */
+/* { dg-final { scan-assembler-times "lr" 1 } } */
+/* { dg-final { scan-assembler "4096" } } */
+/* { dg-final { scan-assembler-not "\\.type\tvar, @object" } } */
