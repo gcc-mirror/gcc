17855935101772b79dea35abb696f205f0215e22
Will Schmidt <will_schmidt@vnet.ibm.com>
2018-02-02 15:53:30 +0000

    fold-vec-abs-int.p9.c: Add powerpc_p9vector_ok requirement.
    [testsuite]
    
    2018-01-31  Will Schmidt  <will_schmidt@vnet.ibm.com>
    
    	* gcc.target/powerpc/fold-vec-abs-int.p9.c: Add powerpc_p9vector_ok
    	requirement.
    	* gcc.target/powerpc/fold-vec-abs-int-fwrapv.p9.c: Same.
    	* gcc.target/powerpc/fold-vec-cmp-char.p9.c: Same.
    	* gcc.target/powerpc/fold-vec-neg-int.p9.c: Same.
    
    From-SVN: r257335

--- gcc/testsuite/gcc.target/powerpc/fold-vec-abs-int-fwrapv.p9.c
+++ gcc/testsuite/gcc.target/powerpc/fold-vec-abs-int-fwrapv.p9.c
@@ -2,7 +2,7 @@
    inputs produce the right results when -mcpu=power9 is specified.  */
 
 /* { dg-do compile } */
-/* { dg-require-effective-target powerpc_altivec_ok } */
+/* { dg-require-effective-target powerpc_p9vector_ok } */
 /* { dg-options "-maltivec -O2 -mcpu=power9 -fwrapv" } */
 /* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power9" } } */
 
