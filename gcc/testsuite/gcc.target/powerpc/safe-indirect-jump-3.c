b1b83f31ed828c28d18092d901cd478d6b4e7b3f
wschmidt <wschmidt@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-02-06 16:55:01 +0000

    [gcc]
    2018-02-06  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* config/rs6000/rs6000.c (rs6000_option_override_internal):
    	Display warning message for -mno-speculate-indirect-jumps.
    
    [gcc/testsuite]
    
    2018-02-06  Bill Schmidt  <wschmidt@linux.vnet.ibm.com>
    
    	* gcc.target/powerpc/safe-indirect-jump-1.c: Detect deprecation
    	warning for -mno-speculate-indirect-jumps.
    	* gcc.target/powerpc/safe-indirect-jump-2.c: Likewise.
    	* gcc.target/powerpc/safe-indirect-jump-3.c: Likewise.
    	* gcc.target/powerpc/safe-indirect-jump-4.c: Likewise.
    	* gcc.target/powerpc/safe-indirect-jump-5.c: Likewise.
    	* gcc.target/powerpc/safe-indirect-jump-6.c: Likewise.
    	* gcc.target/powerpc/safe-indirect-jump-7.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@257419 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/safe-indirect-jump-3.c
+++ gcc/testsuite/gcc.target/powerpc/safe-indirect-jump-3.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-mno-speculate-indirect-jumps" } */
+/* { dg-warning "'-mno-speculate-indirect-jumps' is deprecated" "" { target *-*-* } 0 } */
 
 /* Test for deliberate misprediction of jump tables.  */
 
