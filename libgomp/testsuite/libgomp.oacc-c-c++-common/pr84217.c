3a5a56c5d775267e2f92c07ca4bbb47eff553b58
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-02-07 10:37:55 +0000

    [openacc] Fix diff_type in expand_oacc_collapse_init
    2018-02-07  Tom de Vries  <tom@codesourcery.com>
    
    	PR libgomp/84217
    	* omp-expand.c (expand_oacc_collapse_init): Ensure diff_type is large
    	enough.
    
    	* c-c++-common/goacc/pr84217.c: New test.
    	* gfortran.dg/goacc/pr84217.f90: New test.
    
    	* testsuite/libgomp.oacc-c-c++-common/pr84217.c: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@257443 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libgomp/testsuite/libgomp.oacc-c-c++-common/pr84217.c
@@ -0,0 +1,22 @@
+extern void abort (void);
+
+#define N 10
+
+int
+main (void)
+{
+  int a[N];
+
+  for (short i = 0; i < N; ++i)
+    a[i] = -1;
+
+#pragma acc parallel loop tile (2)
+  for (short i = 0; i < N; ++i)
+    a[i] = i;
+
+  for (short i = 0; i < N; ++i)
+    if (a[i] != i)
+      abort ();
+
+  return 0;
+}
