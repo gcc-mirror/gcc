81b58bfd0b0fba2b00a04012ff60135f12036045
H.J. Lu <hongjiu.lu@intel.com>
2018-02-07 10:49:53 +0000

    Use -fcf-protection=return in cet-intrin-4.c
    Since -fcf-protection requires both -mshstk and -mibt, use
    -fcf-protection=return with -mshstk in cet-intrin-4.c.
    
    	PR target/84243
    	* gcc.target/i386/cet-intrin-4.c (dg-options): Use
    	-fcf-protection=return.
    
    From-SVN: r257445

--- gcc/testsuite/gcc.target/i386/cet-intrin-4.c
+++ gcc/testsuite/gcc.target/i386/cet-intrin-4.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O -fcf-protection -mshstk" } */
+/* { dg-options "-O -fcf-protection=return -mshstk" } */
 /* { dg-final { scan-assembler "rdsspd|incsspd\[ \t]+(%|)eax" { target ia32 } } } */
 /* { dg-final { scan-assembler "rdssp\[dq]\[ \t]+(%|)\[re]ax"  { target { ! ia32 } } } } */
 /* { dg-final { scan-assembler "incssp\[dq]\[ \t]+(%|)\[re]di" { target { ! ia32 } } } } */
