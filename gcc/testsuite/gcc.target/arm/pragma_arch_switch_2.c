6fe65bf7debb698ec1582f7182a9ddb296802eab
tnfchris <tnfchris@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-02-12 12:52:29 +0000

    2018-02-12  Tamar Christina  <tamar.christina@arm.com>
    	PR target/82641
    	* gcc.target/arm/pragma_arch_switch_2.c: Use armv5te.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@257586 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/pragma_arch_switch_2.c
+++ gcc/testsuite/gcc.target/arm/pragma_arch_switch_2.c
@@ -2,7 +2,7 @@
 /* { dg-skip-if "instruction not valid on thumb" { *-*-* } { "-mthumb" } { "" } } */
 /* { dg-do assemble } */
 /* { dg-require-effective-target arm_arm_ok } */
-/* { dg-additional-options "-Wall -O2 -march=armv5t -std=gnu99 -marm" } */
+/* { dg-additional-options "-Wall -O2 -march=armv5te -std=gnu99 -marm" } */
 
 #pragma GCC target ("arch=armv6")
 int test_assembly (int hi, int lo)
