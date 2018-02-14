dff73565df46267296c9bee0589e1f106a07a5e2
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-02-14 07:21:11 +0000

    2018-02-14  Jozef Lawrynowicz <jozefl.gcc@gmail.com>
    	PR target/79242
    	* machmode.def: Define a complex mode for PARTIAL_INT.
    	* genmodes.c (complex_class): Return MODE_COMPLEX_INT for
    	MODE_PARTIAL_INT.
    	* doc/rtl.texi: Document CSPImode.
    	* config/msp430/msp430.c (msp430_hard_regno_nregs): Add CPSImode
    	handling.
    	(msp430_hard_regno_nregs_with_padding): Likewise.
    
    	PR target/79242
    	* gcc.target/msp430/pr79242.c: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@257653 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/genmodes.c
+++ gcc/genmodes.c
@@ -119,6 +119,7 @@ complex_class (enum mode_class c)
   switch (c)
     {
     case MODE_INT: return MODE_COMPLEX_INT;
+    case MODE_PARTIAL_INT: return MODE_COMPLEX_INT;
     case MODE_FLOAT: return MODE_COMPLEX_FLOAT;
     default:
       error ("no complex class for class %s", mode_class_names[c]);
