1c9aa9c6652df0be177e1b578cc174712f474a28
itsimbal <itsimbal@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-02-14 15:06:21 +0000

    Reimplement CET intrinsics for rdssp/incssp insn.
    Introduce a couple of new CET intrinsics for reading and updating a
    shadow stack pointer (_get_ssp and _inc_ssp). They replace the existing
    _rdssp[d|q] and _incssp[d|q] instrinsics.
    
    	PR target/84239
    	* gcc/config/i386/cetintrin.h: Remove _rdssp[d|q] and
    	add _get_ssp intrinsics. Remove argument from
    	__builtin_ia32_rdssp[d|q].
    	* gcc/config/i386/i386-builtin-types.def: Add UINT_FTYPE_VOID.
    	* gcc/config/i386/i386-builtin.def: Remove argument from
    	__builtin_ia32_rdssp[d|q].
    	* gcc/config/i386/i386.c: Use UINT_FTYPE_VOID. Use
    	ix86_expand_special_args_builtin for _rdssp[d|q].
    	* gcc/config/i386/i386.md: Remove argument from rdssp[si|di] insn.
    	Clear register before usage.
    	* doc/extend.texi: Remove argument from __builtin_ia32_rdssp[d|q].
    	Add documentation for new _get_ssp and _inc_ssp intrinsics.
    	* testsuite/gcc.target/i386/cet-intrin-3.c: Use new _get_ssp and
    	_inc_ssp intrinsics.
    	* testsuite/gcc.target/i386/cet-intrin-4.c: Likewise.
    	* testsuite/gcc.target/i386/cet-rdssp-1.c: Remove argument from
    	__builtin_ia32_rdssp[d|q].
    	* libgcc/config/i386/shadow-stack-unwind.hi (_Unwind_Frames_Extra):
    	Use new _get_ssp and _inc_ssp intrinsics.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@257660 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/cet-rdssp-1.c
+++ gcc/testsuite/gcc.target/i386/cet-rdssp-1.c
@@ -5,18 +5,18 @@ void _exit(int status) __attribute__ ((__noreturn__));
 
 #ifdef __x86_64__
 # define incssp(x) __builtin_ia32_incsspq (x)
-# define rdssp(x) __builtin_ia32_rdsspq (x)
+# define rdssp() __builtin_ia32_rdsspq ()
 #else
 # define incssp(x) __builtin_ia32_incsspd (x)
-# define rdssp(x) __builtin_ia32_rdsspd (x)
+# define rdssp() __builtin_ia32_rdsspd ()
 #endif
 
 static void
 __attribute__ ((noinline, noclone))
 test (unsigned long frames)
 {
-  unsigned long ssp = 0;
-  ssp = rdssp (ssp);
+  unsigned long ssp;
+  ssp = rdssp ();
   if (ssp != 0)
     {
       unsigned long tmp = frames;
