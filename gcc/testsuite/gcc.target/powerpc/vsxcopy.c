5c0a40aec5ceeeb05e0efbf5cb35fdac04228225
Peter Bergner <bergner@vnet.ibm.com>
2018-02-14 14:03:21 -0600

    backport: re PR target/84390 (test case gcc.target/powerpc/vsxcopy.c fails for gcc 7 and gcc 6 on power9)
    	Backport from GCC 7 branch
    	2018-02-14  Peter Bergner  <bergner@vnet.ibm.com>
    
    	PR target/84390
    	* gcc.target/powerpc/vsxcopy.c: Also match lxv when compiling
    	with -mcpu=power9.
    
    From-SVN: r257671

--- gcc/testsuite/gcc.target/powerpc/vsxcopy.c
+++ gcc/testsuite/gcc.target/powerpc/vsxcopy.c
@@ -1,8 +1,8 @@
 /* { dg-do compile { target { powerpc64*-*-* } } } */
 /* { dg-require-effective-target powerpc_vsx_ok } */
 /* { dg-options "-O1 -mvsx" } */
-/* { dg-final { scan-assembler "lxvd2x" } } */
-/* { dg-final { scan-assembler "stxvd2x" } } */
+/* { dg-final { scan-assembler {\mlxvd2x\M|\mlxv\M} } } */
+/* { dg-final { scan-assembler {\mstxvd2x\M|\mstxv\M} } } */
 /* { dg-final { scan-assembler-not "xxpermdi" } } */
 
 typedef float vecf __attribute__ ((vector_size (16)));
