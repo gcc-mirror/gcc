8c76a28ef7f3e672a92eb18c5d1449a091e1a480
carll <carll@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-02-16 17:30:45 +0000

    gcc/ChangeLog:
    2018-02-16  Carl Love  <cel@us.ibm.com>
    
    	* config/rs6000/altivec.h: Add builtin names vec_extract4b
    	vec_insert4b.
    	* config/rs6000/rs6000-builtin.def: Add INSERT4B and EXTRACT4B
    	definitions.
    	* config/rs6000/rs6000-c.c: Add the definitions for
    	P9V_BUILTIN_VEC_EXTRACT4B and P9V_BUILTIN_VEC_INSERT4B.
    	* config/rs6000/rs6000.c (altivec_expand_builtin): Add
    	P9V_BUILTIN_EXTRACT4B and P9V_BUILTIN_INSERT4B case statements.
    	* config/rs6000/vsx.md: Add define_insn extract4b.  Add define_expand
    	definition for insert4b and define insn *insert3b_internal.
    	* doc/extend.texi: Add documentation for vec_extract4b.
    
    gcc/testsuite/ChangeLog:
    
    2018-02-16  Carl Love  <cel@us.ibm.com>
    	* gcc.target/powerpc/builtins-7-p9-runnable.c: New runnable test file
    	for the ABI definitions for vec_extract4b and vec_insert4b.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@257747 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -15743,6 +15743,7 @@ altivec_expand_builtin (tree exp, rtx target, bool *expandedp)
 
     case P9V_BUILTIN_VEXTRACT4B:
     case P9V_BUILTIN_VEC_VEXTRACT4B:
+    case P9V_BUILTIN_VEC_EXTRACT4B:
       arg1 = CALL_EXPR_ARG (exp, 1);
       STRIP_NOPS (arg1);
 
@@ -15760,6 +15761,7 @@ altivec_expand_builtin (tree exp, rtx target, bool *expandedp)
     case P9V_BUILTIN_VINSERT4B:
     case P9V_BUILTIN_VINSERT4B_DI:
     case P9V_BUILTIN_VEC_VINSERT4B:
+    case P9V_BUILTIN_VEC_INSERT4B:
       arg2 = CALL_EXPR_ARG (exp, 2);
       STRIP_NOPS (arg2);
 
