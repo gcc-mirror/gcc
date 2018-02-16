8ef828da20c5c799c4a6daf2c3f95764471ddb37
ygribov <ygribov@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-02-16 20:38:14 +0000

    Fix PowerPC tests in PR 81535.
    gcc/testsuite/
    
    2018-02-16  Yury Gribov  <tetra2005@gmail.com>
    
    	PR target/81535
    	* gcc.dg/pr56727-1.c: Prevent tailcalls and update for powerpc*-*-*.
    	* gcc.dg/pr56727-2.c: Ditto.
    	* gcc.target/powerpc/pr79439.c: Renamed to...
    	* gcc.target/powerpc/pr79439-1.c: ...this.
    	* gcc.target/powerpc/pr79439-2.c: New test.
    	* gcc.target/powerpc/pr79439-3.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@257760 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/pr79439-2.c
@@ -0,0 +1,31 @@
+/* { dg-do compile { target { powerpc*-*-linux* && lp64 } } } */
+/* { dg-options "-O2 -fpic -fno-reorder-blocks" } */
+
+/* On the Linux 64-bit ABIs, we should not eliminate NOP in the 'rec' call if
+   -fpic is used because rec can be interposed at link time (since it has an
+   alias), and the recursive call should call the interposed function.  The
+   Linux 32-bit ABIs do not require NOPs after the BL instruction.  */
+
+int f (void);
+
+void
+g (void)
+{
+}
+
+int
+rec (int a)
+{
+  int ret = 0;
+  if (a > 10 && f ())
+    ret += rec (a - 1);
+  g ();
+  return a + ret;
+}
+
+int rec_alias (int) __attribute__ ((alias ("rec")));
+
+/* { dg-final { scan-assembler-times {\mbl f\M}   1 } } */
+/* { dg-final { scan-assembler-times {\mbl g\M}   1 } } */
+/* { dg-final { scan-assembler-times {\mbl rec\M} 1 } } */
+/* { dg-final { scan-assembler-times {\mnop\M}    3 } } */
