af3b4514fcb92b6275ed52e47bb0dd8146f7e304
Ramana Radhakrishnan <ramana.radhakrishnan@arm.com>
2018-02-26 09:25:21 +0000

    [Patch AArch64] Turn on frame pointer / partial fix for PR84521
    This fixes a GCC-8 regression that we accidentally switched off frame
    pointers in the AArch64 backend when changing the defaults in the common
    parts of the code. This breaks an ABI decision that was made in GCC at
    the dawn of the port with respect to having a frame pointer at all
    times.  If we really want to turn this off lets have a discussion around
    that separately.
    
    For now turn this back on and I believe this will leave PR84521 latent
    again with -fomit-frame-pointer and (hopefully) make the ruby issue go
    away. I'm asking Sudi to pick that up.
    
    Bootstrapped and regression tested on AArch64-none-linux-gnu but I see
    one regression in gcc.c-torture/execute/960419-2.c which needs to be
    looked at next (PR84528, thanks Kyrill).
    
    Ok to put in and then look at PR84528 ?
    
    2018-02-26  Ramana Radhakrishnan  <ramana.radhakrishnan@arm.com>
    
            PR target/84521
    	* common/config/aarch64/aarch64-common.c
    	(aarch_option_optimization_table[]): Switch
    	off fomit-frame-pointer
    
    2018-02-26  Ramana Radhakrishnan  <ramana.radhakrishnan@arm.com>
    
    	PR target/84521
    	* gcc.target/aarch64/lr_free_2.c: Revert changes in
    	r254814 disabling -fomit-frame-pointer by default.
    	* gcc.target/aarch64/spill_1.c: Likewise.
    	* gcc.target/aarch64/test_frame_11.c: Likewise.
    	* gcc.target/aarch64/test_frame_12.c: Likewise.
    	* gcc.target/aarch64/test_frame_13.c: Likewise.
    	* gcc.target/aarch64/test_frame_14.c: Likewise.
    	* gcc.target/aarch64/test_frame_15.c: Likewise.
    	* gcc.target/aarch64/test_frame_3.c: Likewise.
    	* gcc.target/aarch64/test_frame_5.c: Likewise.
    	* gcc.target/aarch64/test_frame_9.c: Likewise.
    
    From-SVN: r257984

--- gcc/testsuite/gcc.target/aarch64/test_frame_9.c
+++ gcc/testsuite/gcc.target/aarch64/test_frame_9.c
@@ -4,15 +4,14 @@
      * total frame size > 512.
        area except outgoing <= 512
      * number of callee-saved reg = 1.
-     * Use a single stack adjustment.  */
+     * Split stack adjustment into two subtractions.
+       the first subtractions couldn't be optimized
+       into "str !" as it's > 256.  */
 
 /* { dg-do run } */
-/* { dg-options "-O2 -fomit-frame-pointer --save-temps" } */
+/* { dg-options "-O2 -fomit-frame-pointer" } */
 
 #include "test_frame_common.h"
 
 t_frame_pattern_outgoing (test9, 480, , 24, a[8], a[9], a[10])
 t_frame_run (test9)
-
-/* { dg-final { scan-assembler-times "sub\tsp, sp, #\[0-9\]+" 1 } } */
-/* { dg-final { scan-assembler "str\tx30, \\\[sp, \[0-9\]+\\\]" } } */
