defdca27d561fddec1c0ab81848961aa3709e1d4
Jakub Jelinek <jakub@redhat.com>
2018-03-03 14:21:58 +0100

    backport: re PR target/83986 (ICE in maybe_record_trace_start, at dwarf2cfi.c:2348)
    	Backported from mainline
    	2018-01-30  Jakub Jelinek  <jakub@redhat.com>
    
    	PR rtl-optimization/83986
    	* sched-deps.c (sched_analyze_insn): For frame related insns, add anti
    	dependence against last_pending_memory_flush in addition to
    	pending_jump_insns.
    
    	* gcc.dg/pr83986.c: New test.
    
    From-SVN: r258179

--- gcc/sched-deps.c
+++ gcc/sched-deps.c
@@ -2922,6 +2922,8 @@ sched_analyze_insn (struct deps_desc *deps, rtx x, rtx_insn *insn)
 	= alloc_INSN_LIST (insn, deps->sched_before_next_jump);
 
       /* Make sure epilogue insn is scheduled after preceding jumps.  */
+      add_dependence_list (insn, deps->last_pending_memory_flush, 1,
+			   REG_DEP_ANTI, true);
       add_dependence_list (insn, deps->pending_jump_insns, 1, REG_DEP_ANTI,
 			   true);
     }
