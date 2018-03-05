3917fefa9da881c3e265f824c902a10ee52c6824
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-03-05 18:44:24 +0000

    [PR c++/84702] ICE with default tmpl arg of overload set
    https://gcc.gnu.org/ml/gcc-patches/2018-03/msg00227.html
    	PR c++/84702
    	* pt.c (process_template_arg): Mark lookup_keep on a default arg.
    
    	PR c++/84702
    	* g++.dg/lookup/pr84702.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@258258 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -4425,6 +4425,9 @@ process_template_parm (tree list, location_t parm_loc, tree parm,
 
   pushdecl (decl);
 
+  if (defval && TREE_CODE (defval) == OVERLOAD)
+    lookup_keep (defval, true);
+  
   /* Build the parameter node linking the parameter declaration,
      its default argument (if any), and its constraints (if any). */
   parm = build_tree_list (defval, parm);
