35a313aaf210f8b4c901de178b46820383dbfd3b
Pádraig Brady <p@draigbrady.com>
2018-03-05 13:48:43 +0000

    [PR c++/84497] ref to undefined tls init
    https://gcc.gnu.org/ml/gcc-patches/2018-03/msg00209.html
    	PR c++/84497
    	* decl2.c (get_tls_init_fn): Check TYPE_HAS_TRIVIAL_DFLT too.
    
    	PR c++/84497
    	* g++.dg/cpp0x/pr84497.C: New.
    
    Co-Authored-By: Jason Merrill <jason@redhat.com>
    Co-Authored-By: Nathan Sidwell <nathan@acm.org>
    
    From-SVN: r258244

---
+++ gcc/testsuite/g++.dg/cpp0x/pr84497.C
@@ -0,0 +1,37 @@
+// PR 84497 mismatch with thread constructor fn weakness
+// { dg-do compile { target c++11 } }
+// { dg-require-weak "" }
+
+struct Base
+{
+  int m;
+
+  Base() noexcept = default;  // trivial but not constexpr
+  ~Base() noexcept = default;
+};
+
+struct Derived : Base {};
+struct Container {
+  Base m;
+};
+
+#ifdef DEF
+// This bit for exposition only.
+// All items placed in .tbss
+// __tls_init simply sets __tls_guard
+// no aliases to __tls_init generated
+thread_local Base base_obj;
+thread_local Derived derived_obj;
+thread_local Container container_obj;
+#else
+// Erroneously created strong undef refs to
+// _ZTH11derived_obj, _ZTH13container_obj, _ZTH8base_obj
+extern thread_local Base base_obj;
+extern thread_local Derived derived_obj;
+extern thread_local Container container_obj;
+int main() { return !(&base_obj && &derived_obj && &container_obj);}
+#endif
+
+// { dg-final { scan-assembler ".weak\[ \t\]*_ZTH8base_obj" } }
+// { dg-final { scan-assembler ".weak\[ \t\]*_ZTH11derived_obj" } }
+// { dg-final { scan-assembler ".weak\[ \t\]*_ZTH13container_obj" } }
