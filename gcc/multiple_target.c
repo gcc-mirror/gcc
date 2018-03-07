04258e15e2a3daf84a17c853025ef2a3d5ce7d8f
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-03-07 09:16:07 +0000

    PR middle-end/84723 	* multiple_target.c: Include tree-inline.h and intl.h. 	(expand_target_clones): Diagnose and fail if node->definition and 	!tree_versionable_function_p (node->decl).
    	* gcc.target/i386/pr84723-1.c: New test.
    	* gcc.target/i386/pr84723-2.c: New test.
    	* gcc.target/i386/pr84723-3.c: New test.
    	* gcc.target/i386/pr84723-4.c: New test.
    	* gcc.target/i386/pr84723-5.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@258316 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/multiple_target.c
+++ gcc/multiple_target.c
@@ -36,6 +36,8 @@ along with GCC; see the file COPYING3.  If not see
 #include "pretty-print.h"
 #include "gimple-iterator.h"
 #include "gimple-walk.h"
+#include "tree-inline.h"
+#include "intl.h"
 
 /* Walker callback that replaces all FUNCTION_DECL of a function that's
    going to be versioned.  */
@@ -309,6 +311,22 @@ expand_target_clones (struct cgraph_node *node, bool definition)
       warning_at (DECL_SOURCE_LOCATION (node->decl),
 		  0,
 		  "single target_clones attribute is ignored");
+      return false;
+    }
+
+  if (node->definition
+      && !tree_versionable_function_p (node->decl))
+    {
+      error_at (DECL_SOURCE_LOCATION (node->decl),
+		"clones for %<target_clones%> attribute cannot be created");
+      const char *reason = NULL;
+      if (lookup_attribute ("noclone", DECL_ATTRIBUTES (node->decl)))
+	reason = G_("function %q+F can never be copied "
+		    "because it has %<noclone%> attribute");
+      else
+	reason = copy_forbidden (DECL_STRUCT_FUNCTION (node->decl));
+      if (reason)
+	inform (DECL_SOURCE_LOCATION (node->decl), reason, node->decl);
       return false;
     }
 
