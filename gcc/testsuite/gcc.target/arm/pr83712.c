e0cd6bc00966de0a3a77b642f9507d9c83b398f1
Vladimir Makarov <vmakarov@redhat.com>
2018-03-09 16:00:36 +0000

    re PR target/83712 ("Unable to find a register to spill" when compiling for thumb1)
    2018-03-09  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR target/83712
    	* lra-assigns.c (assign_by_spills): Return a flag of reload
    	assignment failure.  Do not process the reload assignment
    	failures.  Do not spill other reload pseudos if they has the same
    	reg class.
    	(lra_assign): Add a return arg.  Set up from the result of
    	assign_by_spills call.
    	(find_reload_regno_insns, lra_split_hard_reg_for): New functions.
    	* lra-constraints.c (split_reg): Add a new arg.  Use it instead of
    	usage_insns if it is not NULL.
    	(spill_hard_reg_in_range): New function.
    	(split_if_necessary, inherit_in_ebb): Pass a new arg to split_reg.
    	* lra-int.h (spill_hard_reg_in_range, lra_split_hard_reg_for): New
    	function prototypes.
    	(lra_assign): Change prototype.
    	* lra.c (lra): Add code to deal with fails by splitting hard reg
    	live ranges.
    
    2018-03-09  Vladimir Makarov  <vmakarov@redhat.com>
    
    	PR target/83712
    	* gcc.target/arm/pr83712.c: New.
    
    From-SVN: r258390

---
+++ gcc/testsuite/gcc.target/arm/pr83712.c
@@ -0,0 +1,25 @@
+/* { dg-do compile } */
+/* { dg-options "-mfloat-abi=softfp -mthumb -march=armv5t -O2" }  */
+#pragma GCC optimize ("-O2")
+
+struct itrm {
+ int std_in;
+ int std_out;
+ int sock_in;
+ int sock_out;
+};
+
+struct itrm *alloc(void);
+void queue_event(struct itrm *itrm, unsigned char *data, int len);
+
+void handle_trm(int std_in, int std_out, int sock_in, int sock_out, int ctl_in, void *init_string, int init_len)
+{
+ struct itrm *itrm;
+ struct itrm ev = { 0, 80, 24, 0 };
+ itrm = alloc();
+ itrm->std_in = std_in;
+ itrm->std_out = std_out;
+ itrm->sock_in = sock_in;
+ itrm->sock_out = sock_out;
+ queue_event(itrm, (unsigned char *)&ev, sizeof(struct itrm));
+}
