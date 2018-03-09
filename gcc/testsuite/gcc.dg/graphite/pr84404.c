77a5c1b62dad78448fedb355a22be81adf5892f7
Jakub Jelinek <jakub@redhat.com>
2018-03-09 10:58:49 +0100

    re PR debug/84404 (Several "leb128 operand is an undefined symbol" in go testsuite with latest debug improvements)
    	PR debug/84404
    	* gcc.dg/graphite/pr84404.c: Only compile on pthread effective
    	targets.
    
    From-SVN: r258382

--- gcc/testsuite/gcc.dg/graphite/pr84404.c
+++ gcc/testsuite/gcc.dg/graphite/pr84404.c
@@ -1,4 +1,5 @@
-/* { dg-do compile } */
+/* PR debug/84404 */
+/* { dg-do compile { target pthread } } */
 /* { dg-options "-O2 -ftree-parallelize-loops=2 -floop-nest-optimize -g" } */
 
 int te[9];
