041bc6e3328573ab1f5fea69d0b1582395664753
Jason Merrill <jason@redhat.com>
2018-03-09 22:35:17 -0500

    PR c++/84785 - ICE with alias template and default targs.
    	* pt.c (type_unification_real): Set processing_template_decl if
    	saw_undeduced == 1.
    
    From-SVN: r258409

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -19490,7 +19490,13 @@ type_unification_real (tree tparms,
 	  location_t save_loc = input_location;
 	  if (DECL_P (parm))
 	    input_location = DECL_SOURCE_LOCATION (parm);
+
+	  if (saw_undeduced == 1)
+	    ++processing_template_decl;
 	  arg = tsubst_template_arg (arg, full_targs, complain, NULL_TREE);
+	  if (saw_undeduced == 1)
+	    --processing_template_decl;
+
 	  if (arg != error_mark_node && !uses_template_parms (arg))
 	    arg = convert_template_argument (parm, arg, full_targs, complain,
 					     i, NULL_TREE);
