8fb8d9421ad774b92afe4733fb5ae0035a268a90
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-03-15 21:09:24 +0000

    PR c++/79085 	* calls.c (expand_call): For TREE_ADDRESSABLE rettype ignore alignment 	check and use address of target always.
    	* g++.dg/opt/pr79085.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@258574 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/calls.c
+++ gcc/calls.c
@@ -3422,9 +3422,14 @@ expand_call (tree exp, rtx target, int ignore)
 	if (CALL_EXPR_RETURN_SLOT_OPT (exp)
 	    && target
 	    && MEM_P (target)
-	    && !(MEM_ALIGN (target) < TYPE_ALIGN (rettype)
-		 && targetm.slow_unaligned_access (TYPE_MODE (rettype),
-						   MEM_ALIGN (target))))
+	    /* If rettype is addressable, we may not create a temporary.
+	       If target is properly aligned at runtime and the compiler
+	       just doesn't know about it, it will work fine, otherwise it
+	       will be UB.  */
+	    && (TREE_ADDRESSABLE (rettype)
+		|| !(MEM_ALIGN (target) < TYPE_ALIGN (rettype)
+		     && targetm.slow_unaligned_access (TYPE_MODE (rettype),
+						       MEM_ALIGN (target)))))
 	  structure_value_addr = XEXP (target, 0);
 	else
 	  {
