e96b21087a4b0f80baad95f806435f73eac33c87
Andrew Macleod <amacleod@gcc.gnu.org>
2018-04-06 13:25:30 +0000

    disable rvrp for these VRP tests
    From-SVN: r259172

--- gcc/testsuite/gcc.dg/pr68217.c
+++ gcc/testsuite/gcc.dg/pr68217.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -fdisable-tree-evrp -fdump-tree-vrp1" } */
+/* { dg-options "-O2 -fdisable-tree-evrp -fdisable-tree-rvrp -fdump-tree-vrp1" } */
 
 int foo (void)
 {
