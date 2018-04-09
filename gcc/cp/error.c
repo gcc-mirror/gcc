06756ed901c35e2430f924eb6a3998d872eb1040
Jason Merrill <jason@redhat.com>
2018-04-09 17:16:05 -0400

    PR c++/85279 - dump_expr doesn't understand decltype.
    	* error.c (dump_expr): Handle DECLTYPE_TYPE.
    
    From-SVN: r259257

--- gcc/cp/error.c
+++ gcc/cp/error.c
@@ -2714,6 +2714,7 @@ dump_expr (cxx_pretty_printer *pp, tree t, int flags)
     case INTEGER_TYPE:
     case COMPLEX_TYPE:
     case VECTOR_TYPE:
+    case DECLTYPE_TYPE:
       pp_type_specifier_seq (pp, t);
       break;
 
