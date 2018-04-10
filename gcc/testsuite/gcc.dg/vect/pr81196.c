45b2bc2f04d083a6673761b3f52b64e7c7f0014d
amker <amker@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-04-10 13:11:40 +0000

    gcc/testsuite 	PR testsuite/85190 	* gcc.dg/vect/pr81196.c: Adjust pointer for aligned access.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@259272 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/vect/pr81196.c
+++ gcc/testsuite/gcc.dg/vect/pr81196.c
@@ -4,14 +4,14 @@
 
 void f(short*p){
   p=(short*)__builtin_assume_aligned(p,64);
-  short*q=p+256;
+  short*q=p+255;
   for(;p!=q;++p,--q){
     short t=*p;*p=*q;*q=t;
   }
 }
 void b(short*p){
   p=(short*)__builtin_assume_aligned(p,64);
-  short*q=p+256;
+  short*q=p+255;
   for(;p<q;++p,--q){
     short t=*p;*p=*q;*q=t;
   }
