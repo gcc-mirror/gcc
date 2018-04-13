gcc-mirror/gcc

commit e7a740068ed3cc5961101f07012314d940a97ae5

asan.c (create_cond_insert_point): Maintain profile.
	* asan.c (create_cond_insert_point): Maintain profile.
	* ipa-utils.c (ipa_merge_profiles): Be sure only IPA profiles are
	merged.
	* basic-block.h (struct basic_block_def): Remove frequency.
	(EDGE_FREQUENCY): Use to_frequency
	* bb-reorder.c (push_to_next_round_p): Use only IPA counts for global
	heuristics.
	(find_traces): Update to use to_frequency.
	(find_traces_1_round): Likewise; use only IPA counts.
	(bb_to_key): Likewise.
	(connect_traces): Use IPA counts only.
	(copy_bb_p): Update to use to_frequency.
	(fix_up_crossing_landing_pad): Likewise.
	(sanitize_hot_paths): Likewise.
	* bt-load.c (basic_block_freq): Likewise.
	* cfg.c (init_flow): Set count_max to uninitialized.
	(check_bb_profile): Remove frequencies; check counts.
	(dump_bb_info): Do not dump frequencies.
	(update_bb_profile_for_threading): Update counts only.
	(scale_bbs_frequencies_int): Likewise.
	(MAX_SAFE_MULTIPLIER): Remove.
	(scale_bbs_frequencies_gcov_type): Update counts only.
	(scale_bbs_frequencies_profile_count): Update counts only.
	(scale_bbs_frequencies): Update counts only.
	* cfg.h (struct control_flow_graph): Add count-max.
	(update_bb_profile_for_threading): Update prototype.
	* cfgbuild.c (find_bb_boundaries): Do not update frequencies.
	(find_many_sub_basic_blocks): Likewise.
	* cfgcleanup.c (try_forward_edges): Likewise.
	(try_crossjump_to_edge): Likewise.
	* cfgexpand.c (expand_gimple_cond): Likewise.
	(expand_gimple_tailcall): Likewise.
	(construct_init_block): Likewise.
	(construct_exit_block): Likewise.
	* cfghooks.c (verify_flow_info): Check consistency of counts.
	(dump_bb_for_graph): Do not dump frequencies.
	(split_block_1): Do not update frequencies.
	(split_edge): Do not update frequencies.
	(make_forwarder_block): Do not update frequencies.
	(duplicate_block): Do not update frequencies.
	(account_profile_record): Do not update frequencies.
	* cfgloop.c (find_subloop_latch_edge_by_profile): Use IPA counts

gcc/cfgloopanal.c
@@ -213,9 +213,10 @@ average_num_loop_insns (const struct loop *loop)
 	if (NONDEBUG_INSN_P (insn))
 	  binsns++;
 
-      ratio = loop->header->frequency == 0
+      ratio = loop->header->count.to_frequency (cfun) == 0
 	      ? BB_FREQ_MAX
-	      : (bb->frequency * BB_FREQ_MAX) / loop->header->frequency;
+	      : (bb->count.to_frequency (cfun) * BB_FREQ_MAX)
+		 / loop->header->count.to_frequency (cfun);
       ninsns += binsns * ratio;
     }
   free (bbs);
@@ -245,8 +246,8 @@ expected_loop_iterations_unbounded (const struct loop *loop,
   /* If we have no profile at all, use AVG_LOOP_NITER.  */
   if (profile_status_for_fn (cfun) == PROFILE_ABSENT)
     expected = PARAM_VALUE (PARAM_AVG_LOOP_NITER);
-  else if (loop->latch && (loop->latch->count.reliable_p ()
-			   || loop->header->count.reliable_p ()))
+  else if (loop->latch && (loop->latch->count.initialized_p ()
+			   || loop->header->count.initialized_p ()))
     {
       profile_count count_in = profile_count::zero (),
 		    count_latch = profile_count::zero ();
@@ -258,45 +259,25 @@ expected_loop_iterations_unbounded (const struct loop *loop,
 	  count_in += e->count ();
 
       if (!count_latch.initialized_p ())
-	;
-      else if (!(count_in > profile_count::zero ()))
+	expected = PARAM_VALUE (PARAM_AVG_LOOP_NITER);
+      else if (!count_in.nonzero_p ())
 	expected = count_latch.to_gcov_type () * 2;
       else
 	{
 	  expected = (count_latch.to_gcov_type () + count_in.to_gcov_type ()
 		      - 1) / count_in.to_gcov_type ();
-	  if (read_profile_p)
+	  if (read_profile_p
+	      && count_latch.reliable_p () && count_in.reliable_p ())
 	    *read_profile_p = true;
 	}
     }
-  if (expected == -1)
-    {
-      int freq_in, freq_latch;
-
-      freq_in = 0;
-      freq_latch = 0;
-
-      FOR_EACH_EDGE (e, ei, loop->header->preds)
-	if (flow_bb_inside_loop_p (loop, e->src))
-	  freq_latch += EDGE_FREQUENCY (e);
-	else
-	  freq_in += EDGE_FREQUENCY (e);
-
-      if (freq_in == 0)
-	{
-	  /* If we have no profile at all, use AVG_LOOP_NITER iterations.  */
-	  if (!freq_latch)
-	    expected = PARAM_VALUE (PARAM_AVG_LOOP_NITER);
-	  else
-	    expected = freq_latch * 2;
-	}
-      else
-        expected = (freq_latch + freq_in - 1) / freq_in;
-    }
+  else
+    expected = PARAM_VALUE (PARAM_AVG_LOOP_NITER);
 
   HOST_WIDE_INT max = get_max_loop_iterations_int (loop);
   if (max != -1 && max < expected)
     return max;
+ 
   return expected;
 }
 

gcc/cfgloopmanip.c
@@ -536,7 +536,6 @@ scale_loop_profile (struct loop *loop, profile_probability p,
       if (e)
 	{
 	  edge other_e;
-	  int freq_delta;
 	  profile_count count_delta;
 
           FOR_EACH_EDGE (other_e, ei, e->src->succs)
@@ -545,23 +544,18 @@ scale_loop_profile (struct loop *loop, profile_probability p,
 	      break;
 
 	  /* Probability of exit must be 1/iterations.  */
-	  freq_delta = EDGE_FREQUENCY (e);
 	  count_delta = e->count ();
 	  e->probability = profile_probability::always ()
 				.apply_scale (1, iteration_bound);
 	  other_e->probability = e->probability.invert ();
-	  freq_delta -= EDGE_FREQUENCY (e);
 	  count_delta -= e->count ();
 
-	  /* If latch exists, change its frequency and count, since we changed
+	  /* If latch exists, change its count, since we changed
 	     probability of exit.  Theoretically we should update everything from
 	     source of exit edge to latch, but for vectorizer this is enough.  */
 	  if (loop->latch
 	      && loop->latch != e->src)
 	    {
-	      loop->latch->frequency += freq_delta;
-	      if (loop->latch->frequency < 0)
-		loop->latch->frequency = 0;
 	      loop->latch->count += count_delta;
 	    }
 	}
@@ -571,7 +565,6 @@ scale_loop_profile (struct loop *loop, profile_probability p,
 	 we look at the actual profile, if it is available.  */
       p = p.apply_scale (iteration_bound, iterations);
 
-      bool determined = false;
       if (loop->header->count.initialized_p ())
 	{
 	  profile_count count_in = profile_count::zero ();
@@ -584,20 +577,7 @@ scale_loop_profile (struct loop *loop, profile_probability p,
 	    {
 	      p = count_in.probability_in (loop->header->count.apply_scale
 						 (iteration_bound, 1));
-	      determined = true;
 	    }
-	}
-      if (!determined && loop->header->frequency)
-	{
-	  int freq_in = 0;
-
-	  FOR_EACH_EDGE (e, ei, loop->header->preds)
-	    if (e->src != loop->latch)
-	      freq_in += EDGE_FREQUENCY (e);
-
-	  if (freq_in != 0)
-	    p = profile_probability::probability_in_gcov_type
-			 (freq_in * iteration_bound, loop->header->frequency);
 	}
       if (!(p > profile_probability::never ()))
 	p = profile_probability::very_unlikely ();
@@ -800,7 +780,7 @@ create_empty_loop_on_edge (edge entry_edge,
   loop->latch = loop_latch;
   add_loop (loop, outer);
 
-  /* TODO: Fix frequencies and counts.  */
+  /* TODO: Fix counts.  */
   scale_loop_frequencies (loop, profile_probability::even ());
 
   /* Update dominators.  */
@@ -866,13 +846,11 @@ loopify (edge latch_edge, edge header_edge,
   basic_block pred_bb = header_edge->src;
   struct loop *loop = alloc_loop ();
   struct loop *outer = loop_outer (succ_bb->loop_father);
-  int freq;
   profile_count cnt;
 
   loop->header = header_edge->dest;
   loop->latch = latch_edge->src;
 
-  freq = EDGE_FREQUENCY (header_edge);
   cnt = header_edge->count ();
 
   /* Redirect edges.  */
@@ -901,10 +879,9 @@ loopify (edge latch_edge, edge header_edge,
     remove_bb_from_loops (switch_bb);
   add_bb_to_loop (switch_bb, outer);
 
-  /* Fix frequencies.  */
+  /* Fix counts.  */
   if (redirect_all_edges)
     {
-      switch_bb->frequency = freq;
       switch_bb->count = cnt;
     }
   scale_loop_frequencies (loop, false_scale);
@@ -1167,7 +1144,7 @@ duplicate_loop_to_header_edge (struct loop *loop, edge e,
     {
       /* Calculate coefficients by that we have to scale frequencies
 	 of duplicated loop bodies.  */
-      freq_in = header->frequency;
+      freq_in = header->count.to_frequency (cfun);
       freq_le = EDGE_FREQUENCY (latch_edge);
       if (freq_in == 0)
 	freq_in = 1;

commit 65739a688542b637b6a9f99aed2de84d9b84460c

re PR tree-optimization/82965 (gcc.dg/vect/pr79347.c starts failing after r254379)
	PR tree-optimization/82965
	PR tree-optimization/83991
	* cfgloopanal.c (expected_loop_iterations_unbounded): Add
	by_profile_only parameter.
	* cfgloopmanip.c (scale_loop_profile): Further scale loop's profile
        information if the loop was predicted to iterate too many times.
	* cfgloop.h (expected_loop_iterations_unbounded): Update prototype

Co-Authored-By: Bin Cheng <bin.cheng@arm.com>

From-SVN: r259368

gcc/cfgloop.h
@@ -388,7 +388,7 @@ extern void verify_loop_structure (void);
 /* Loop analysis.  */
 extern bool just_once_each_iteration_p (const struct loop *, const_basic_block);
 gcov_type expected_loop_iterations_unbounded (const struct loop *,
-					      bool *read_profile_p = NULL);
+					      bool *read_profile_p = NULL, bool by_profile_only = false);
 extern unsigned expected_loop_iterations (struct loop *);
 extern rtx doloop_condition_get (rtx_insn *);
 

gcc/cfgloopanal.c
@@ -230,12 +230,17 @@ average_num_loop_insns (const struct loop *loop)
 }
 
 /* Returns expected number of iterations of LOOP, according to
-   measured or guessed profile.  No bounding is done on the
-   value.  */
+   measured or guessed profile.
+
+   This functions attempts to return "sane" value even if profile
+   information is not good enough to derive osmething.
+   If BY_PROFILE_ONLY is set, this logic is bypassed and function
+   return -1 in those scenarios.  */
 
 gcov_type
 expected_loop_iterations_unbounded (const struct loop *loop,
-				    bool *read_profile_p)
+				    bool *read_profile_p,
+				    bool by_profile_only)
 {
   edge e;
   edge_iterator ei;
@@ -246,7 +251,11 @@ expected_loop_iterations_unbounded (const struct loop *loop,
 
   /* If we have no profile at all, use AVG_LOOP_NITER.  */
   if (profile_status_for_fn (cfun) == PROFILE_ABSENT)
-    expected = PARAM_VALUE (PARAM_AVG_LOOP_NITER);
+    {
+      if (by_profile_only)
+	return -1;
+      expected = PARAM_VALUE (PARAM_AVG_LOOP_NITER);
+    }
   else if (loop->latch && (loop->latch->count.initialized_p ()
 			   || loop->header->count.initialized_p ()))
     {
@@ -260,9 +269,17 @@ expected_loop_iterations_unbounded (const struct loop *loop,
 	  count_in += e->count ();
 
       if (!count_latch.initialized_p ())
-	expected = PARAM_VALUE (PARAM_AVG_LOOP_NITER);
+	{
+          if (by_profile_only)
+	    return -1;
+	  expected = PARAM_VALUE (PARAM_AVG_LOOP_NITER);
+	}
       else if (!count_in.nonzero_p ())
-	expected = count_latch.to_gcov_type () * 2;
+	{
+          if (by_profile_only)
+	    return -1;
+	  expected = count_latch.to_gcov_type () * 2;
+	}
       else
 	{
 	  expected = (count_latch.to_gcov_type () + count_in.to_gcov_type ()
@@ -273,11 +290,18 @@ expected_loop_iterations_unbounded (const struct loop *loop,
 	}
     }
   else
-    expected = PARAM_VALUE (PARAM_AVG_LOOP_NITER);
+    {
+      if (by_profile_only)
+	return -1;
+      expected = PARAM_VALUE (PARAM_AVG_LOOP_NITER);
+    }
 
-  HOST_WIDE_INT max = get_max_loop_iterations_int (loop);
-  if (max != -1 && max < expected)
-    return max;
+  if (!by_profile_only)
+    {
+      HOST_WIDE_INT max = get_max_loop_iterations_int (loop);
+      if (max != -1 && max < expected)
+        return max;
+    }
  
   return expected;
 }

gcc/cfgloopmanip.c
@@ -502,14 +502,17 @@ scale_loop_frequencies (struct loop *loop, profile_probability p)
 
 /* Scale profile in LOOP by P.
    If ITERATION_BOUND is non-zero, scale even further if loop is predicted
-   to iterate too many times.  */
+   to iterate too many times.
+   Before caling this function, preheader block profile should be already
+   scaled to final count.  This is necessary because loop iterations are
+   determined by comparing header edge count to latch ege count and thus
+   they need to be scaled synchronously.  */
 
 void
 scale_loop_profile (struct loop *loop, profile_probability p,
 		    gcov_type iteration_bound)
 {
-  gcov_type iterations = expected_loop_iterations_unbounded (loop);
-  edge e;
+  edge e, preheader_e;
   edge_iterator ei;
 
   if (dump_file && (dump_flags & TDF_DETAILS))
@@ -517,28 +520,45 @@ scale_loop_profile (struct loop *loop, profile_probability p,
       fprintf (dump_file, ";; Scaling loop %i with scale ",
 	       loop->num);
       p.dump (dump_file);
-      fprintf (dump_file, " bounding iterations to %i from guessed %i\n",
-	       (int)iteration_bound, (int)iterations);
+      fprintf (dump_file, " bounding iterations to %i\n",
+	       (int)iteration_bound);
+    }
+
+  /* Scale the probabilities.  */
+  scale_loop_frequencies (loop, p);
+
+  if (iteration_bound == 0)
+    return;
+
+  gcov_type iterations = expected_loop_iterations_unbounded (loop, NULL, true);
+
+  if (dump_file && (dump_flags & TDF_DETAILS))
+    {
+      fprintf (dump_file, ";; guessed iterations after scaling %i\n",
+	       (int)iterations);
     }
 
   /* See if loop is predicted to iterate too many times.  */
-  if (iteration_bound && iterations > 0
-      && p.apply (iterations) > iteration_bound)
-    {
-      /* Fixing loop profile for different trip count is not trivial; the exit
-	 probabilities has to be updated to match and frequencies propagated down
-	 to the loop body.
+  if (iterations <= iteration_bound)
+    return;
 
-	 We fully update only the simple case of loop with single exit that is
-	 either from the latch or BB just before latch and leads from BB with
-	 simple conditional jump.   This is OK for use in vectorizer.  */
+  preheader_e = loop_preheader_edge (loop);
+
+  /* We could handle also loops without preheaders, but bounding is
+     currently used only by optimizers that have preheaders constructed.  */
+  gcc_checking_assert (preheader_e);
+  profile_count count_in = preheader_e->count ();
+
+  if (count_in > profile_count::zero ()
+      && loop->header->count.initialized_p ())
+    {
+      profile_count count_delta = profile_count::zero ();
+
       e = single_exit (loop);
       if (e)
 	{
 	  edge other_e;
-	  profile_count count_delta;
-
-          FOR_EACH_EDGE (other_e, ei, e->src->succs)
+	  FOR_EACH_EDGE (other_e, ei, e->src->succs)
 	    if (!(other_e->flags & (EDGE_ABNORMAL | EDGE_FAKE))
 		&& e != other_e)
 	      break;
@@ -546,52 +566,54 @@ scale_loop_profile (struct loop *loop, profile_probability p,
 	  /* Probability of exit must be 1/iterations.  */
 	  count_delta = e->count ();
 	  e->probability = profile_probability::always ()
-				.apply_scale (1, iteration_bound);
+				    .apply_scale (1, iteration_bound);
 	  other_e->probability = e->probability.invert ();
-	  count_delta -= e->count ();
 
-	  /* If latch exists, change its count, since we changed
-	     probability of exit.  Theoretically we should update everything from
-	     source of exit edge to latch, but for vectorizer this is enough.  */
-	  if (loop->latch
-	      && loop->latch != e->src)
+	  /* In code below we only handle the following two updates.  */
+	  if (other_e->dest != loop->header
+	      && other_e->dest != loop->latch
+	      && (dump_file && (dump_flags & TDF_DETAILS)))
 	    {
-	      loop->latch->count += count_delta;
+	      fprintf (dump_file, ";; giving up on update of paths from "
+		       "exit condition to latch\n");
 	    }
 	}
+      else
+        if (dump_file && (dump_flags & TDF_DETAILS))
+	  fprintf (dump_file, ";; Loop has multiple exit edges; "
+	      		      "giving up on exit condition update\n");
 
       /* Roughly speaking we want to reduce the loop body profile by the
 	 difference of loop iterations.  We however can do better if
 	 we look at the actual profile, if it is available.  */
-      p = p.apply_scale (iteration_bound, iterations);
+      p = profile_probability::always ();
 
-      if (loop->header->count.initialized_p ())
-	{
-	  profile_count count_in = profile_count::zero ();
-
-	  FOR_EACH_EDGE (e, ei, loop->header->preds)
-	    if (e->src != loop->latch)
-	      count_in += e->count ();
-
-	  if (count_in > profile_count::zero () )
-	    {
-	      p = count_in.probability_in (loop->header->count.apply_scale
-						 (iteration_bound, 1));
-	    }
-	}
+      count_in = count_in.apply_scale (iteration_bound, 1);
+      p = count_in.probability_in (loop->header->count);
       if (!(p > profile_probability::never ()))
 	p = profile_probability::very_unlikely ();
+
+      if (p == profile_probability::always ()
+	  || !p.initialized_p ())
+	return;
+
+      /* If latch exists, change its count, since we changed
+	 probability of exit.  Theoretically we should update everything from
+	 source of exit edge to latch, but for vectorizer this is enough.  */
+      if (loop->latch && loop->latch != e->src)
+	loop->latch->count += count_delta;
+
+      /* Scale the probabilities.  */
+      scale_loop_frequencies (loop, p);
+
+      /* Change latch's count back.  */
+      if (loop->latch && loop->latch != e->src)
+	loop->latch->count -= count_delta;
+
+      if (dump_file && (dump_flags & TDF_DETAILS))
+	fprintf (dump_file, ";; guessed iterations are now %i\n",
+		 (int)expected_loop_iterations_unbounded (loop, NULL, true));
     }
-
-  if (p >= profile_probability::always ()
-      || !p.initialized_p ())
-    return;
-
-  /* Scale the actual probabilities.  */
-  scale_loop_frequencies (loop, p);
-  if (dump_file && (dump_flags & TDF_DETAILS))
-    fprintf (dump_file, ";; guessed iterations are now %i\n",
-	     (int)expected_loop_iterations_unbounded (loop));
 }
 
 /* Recompute dominance information for basic blocks outside LOOP.  */