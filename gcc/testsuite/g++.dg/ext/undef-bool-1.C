558b0aa59985306c820404f4d30a05bc620117a4
wschmidt <wschmidt@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-04-16 02:00:43 +0000

    [gcc/testsuite]
    2018-04-15  Bill Schmidt  <wschmidt@linux.ibm.com>
    
    	PR testsuite/85326
    	* g++.dg/ext/undef-bool-1.C: New file.
    	* gcc.target/powerpc/powerpc.exp: Remove .C support.
    	* gcc.target/powerpc/undef-bool-1.C: Remove file.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@259393 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ext/undef-bool-1.C
+++ gcc/testsuite/g++.dg/ext/undef-bool-1.C
@@ -1,5 +1,5 @@
-/* { dg-do compile } */
-/* { dg-options "-O2 -std=c++11 -DNO_WARN_X86_INTRINSICS" } */
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-options "-O2 -DNO_WARN_X86_INTRINSICS" } */
 
 /* Test to ensure that "bool" gets undef'd in xmmintrin.h when
    we require strict ANSI.  */
