e5556251b7f7dc7dcd4f9621a986c14cba849a40
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-04-17 05:41:40 +0000

    Support bitfields in Wodr machinery (PR lto/85405).
    2018-04-17  Jan Hubicka  <jh@suse.cz>
    
    	PR lto/85405
    	* ipa-devirt.c (odr_types_equivalent_p): Handle bit fields.
    2018-04-17  Martin Liska  <mliska@suse.cz>
    
    	PR lto/85405
    	* g++.dg/lto/pr85405_0.C: New test.
    	* g++.dg/lto/pr85405_1.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@259429 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/lto/pr85405_0.C
@@ -0,0 +1,18 @@
+// { dg-lto-do link }
+// { dg-lto-options {{-fPIC -shared -flto}} }
+
+class VclReferenceBase { // { dg-lto-warning "7: type 'struct VclReferenceBase' violates the C\\+\\+ One Definition Rule" }
+  int mnRefCnt;
+  bool mbDisposed : 1;
+  virtual ~VclReferenceBase();
+};
+class a;
+class b {
+  a &e;
+  bool c();
+};
+class B {
+  VclReferenceBase d;
+};
+class a : B {};
+bool b::c() { return false; }
