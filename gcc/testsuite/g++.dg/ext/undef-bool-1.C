69c219f6065d93c25f6998c187c631dc52cd6255
wschmidt <wschmidt@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-04-18 01:56:59 +0000

    [gcc/testsuite]
    2018-04-17  Bill Schmidt  <wschmidt@linux.ibm.com>
    
    	* gcc.target/powerpc/undef-bool-2.c: Add -mvsx.
    	* gcc.target/g++.dg/ext/undef-bool-1.C: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@259456 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ext/undef-bool-1.C
+++ gcc/testsuite/g++.dg/ext/undef-bool-1.C
@@ -1,5 +1,5 @@
 /* { dg-do compile { target { powerpc*-*-* } } } */
-/* { dg-options "-O2 -DNO_WARN_X86_INTRINSICS" } */
+/* { dg-options "-O2 -DNO_WARN_X86_INTRINSICS -mvsx" } */
 
 /* Test to ensure that "bool" gets undef'd in xmmintrin.h when
    we require strict ANSI.  */
