af5975362b21c2a1268aac5b02545de482a2cc7c
Martin Liska <mliska@suse.cz>
2018-04-18 22:08:44 +0200

    Make Wodr warnings stable.
    2018-04-18  Martin Liska  <mliska@suse.cz>
    
    	PR ipa/83983
    	PR ipa/85391
    	* lto.c (cmp_type_location): New function.
    	(lto_read_decls): First collect all types, then
    	sort them according by location before register_odr_type
    	is called.
    2018-04-18  Martin Liska  <mliska@suse.cz>
    
    	PR ipa/83983
    	PR ipa/85391
    	* g++.dg/lto/pr83121_1.C (struct Environment): Adjust expected
    	output.
    
    From-SVN: r259479

--- gcc/testsuite/g++.dg/lto/pr83121_1.C
+++ gcc/testsuite/g++.dg/lto/pr83121_1.C
@@ -2,7 +2,7 @@ struct Environment {
   struct AsyncHooks {
     int providers_[1];
   };
-  AsyncHooks async_hooks_; // { dg-lto-message "a field of same name but different type is defined in another translation unit" }
+  AsyncHooks async_hooks_;
 };
 void fn1() { Environment a; }
 int main ()
