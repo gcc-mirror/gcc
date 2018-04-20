af88112e75f1167ef77011a11cdd4e5ee9fa187c
wschmidt <wschmidt@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-04-20 21:00:41 +0000

    [gcc/testsuite]
    2018-04-20  Bill Schmidt  <wschmidt@linux.ibm.com>
    
    	* g++.dg/ext/undef-bool-1.C: Require lp64.
    	* gcc.target/powerpc/undef-bool-2.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@259532 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ext/undef-bool-1.C
+++ gcc/testsuite/g++.dg/ext/undef-bool-1.C
@@ -1,5 +1,6 @@
 /* { dg-do compile { target { powerpc*-*-* } } } */
 /* { dg-options "-O2 -DNO_WARN_X86_INTRINSICS -mvsx" } */
+/* { dg-require-effective-target lp64 } */
 
 /* Test to ensure that "bool" gets undef'd in xmmintrin.h when
    we require strict ANSI.  */
--- gcc/testsuite/gcc.target/powerpc/undef-bool-2.c
+++ gcc/testsuite/gcc.target/powerpc/undef-bool-2.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -std=c11 -DNO_WARN_X86_INTRINSICS -mvsx" } */
+/* { dg-require-effective-target lp64 } */
 
 /* Test to ensure that "bool" gets undef'd in xmmintrin.h when
    we require strict ANSI.  Subsequent use of bool needs stdbool.h.
