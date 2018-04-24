48f64e053e4069a687410b1970ad85695bc73559
H.J. Lu <hongjiu.lu@intel.com>
2018-04-24 16:47:24 +0000

    Don't bootstrap GCC with -mcet
    With revision 259496:
    
    commit b1384095a7c1d06a44b70853372ebe037b2f7867
    Author: hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
    Date:   Thu Apr 19 15:15:04 2018 +0000
    
        x86: Enable -fcf-protection with multi-byte NOPs
    
    -fcf-protection no longer requires -mcet.
    
    config/
    
    	PR target/85485
    	* bootstrap-cet.mk (STAGE2_CFLAGS): Remove -mcet.
    	(STAGE3_CFLAGS): Likewise.
    
    gcc/
    
    	PR target/85485
    	* doc/install.texi: Remove -mcet from bootstrap-cet.
    
    From-SVN: r259611

--- config/bootstrap-cet.mk
+++ config/bootstrap-cet.mk
@@ -1,4 +1,4 @@
-# This option enables -fcf-protection -mcet for stage2 and stage3.
+# This option enables -fcf-protection for stage2 and stage3.
 
-STAGE2_CFLAGS += -fcf-protection -mcet
-STAGE3_CFLAGS += -fcf-protection -mcet
+STAGE2_CFLAGS += -fcf-protection
+STAGE3_CFLAGS += -fcf-protection
