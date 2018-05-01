2e93301778cc4ef33d85486ee69311837e8bb0c6
glisse <glisse@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-05-01 21:41:05 +0000

    Generalize a<b&a<c -> a<min(b,c)
    2018-05-01  Marc Glisse  <marc.glisse@inria.fr>
    
    	PR tree-optimization/85143
    gcc/
    	* match.pd (A<B&A<C): Extend to BIT_IOR_EXPR.
    
    gcc/testsuite/
    	* gcc.dg/tree-ssa/minmax-loopend.c: Extend and split...
    	* gcc.dg/tree-ssa/minmax-loopend-2.c: ... here.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@259812 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa/minmax-loopend-2.c
+++ gcc/testsuite/gcc.dg/tree-ssa/minmax-loopend-2.c
@@ -2,15 +2,15 @@
 /* { dg-options "-O2 -fdump-tree-optimized" } */
 
 int and_test(long a, long b, long c) {
-  int cmp1 = a < b;
-  int cmp2 = a < c;
+  int cmp1 = a > b;
+  int cmp2 = a > c;
   return cmp1 & cmp2;
 }
 
 int ior_test (long a, long b, long c) {
-  int cmp1 = a > b;
-  int cmp2 = a > c;
+  int cmp1 = a < b;
+  int cmp2 = a < c;
   return cmp1 | cmp2;
 }
 
-/* { dg-final { scan-tree-dump-times "MIN_EXPR" 2 "optimized" } } */
+/* { dg-final { scan-tree-dump-times "MAX_EXPR" 2 "optimized" } } */
