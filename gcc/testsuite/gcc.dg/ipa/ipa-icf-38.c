6bdd98613cbc74228ce5fe38825cd271076cebd8
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-05-02 12:16:15 +0000

    [testsuite] Add scan-wpa-ipa-dump
    2018-05-02  Tom de Vries  <tom@codesourcery.com>
    
    	PR testsuite/85106
    	* gcc.dg/ipa/ipa-icf-38.c: New test.
    	* gcc.dg/ipa/ipa-icf-38a.c: New test.
    	* lib/scandump.exp (dump-base): New proc.
    	(scan-dump, scan-dump-times, scan-dump-not, scan-dump-dem)
    	(scan-dump-dem-not): Add and handle parameter for suffix of the dump
    	base.
    	* lib/scanipa.exp: Add "" argument to scan-dump calls.
    	* lib/scanlang.exp: Same.
    	* lib/scanrtl.exp: Same.
    	* lib/scantree.exp: Same.
    	* lib/scanwpaipa.exp: New file.
    	* lib/gcc-dg.exp: Include scanwpaipa.exp.
    
    	* testsuite/lib/libatomic.exp: Include scanwpaipa.exp.
    
    	* testsuite/lib/libgomp.exp: Include scanwpaipa.exp.
    
    	* testsuite/lib/libitm.exp: Include scanwpaipa.exp.
    
    	* testsuite/lib/libvtv.exp: Include scanwpaipa.exp.
    
    	* doc/sourcebuild.texi (Commands for use in dg-final, Scan optimization
    	dump files): Add wpa-ipa.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@259837 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/ipa/ipa-icf-38.c
@@ -0,0 +1,31 @@
+/* { dg-do link } */
+/* { dg-options "-O2 -fdump-ipa-icf -flto" } */
+/* { dg-require-effective-target lto } */
+/* { dg-additional-sources "ipa-icf-38a.c" }*/
+
+/* Based on ipa-icf-3.c.  */
+
+typedef int v4si __attribute__ ((vector_size (16)));
+
+__attribute__ ((noinline))
+int foo(void)
+{
+  v4si a = {1,2,3,4};
+  v4si b = {3,2,1,4};
+  v4si c;
+
+  return 54;
+}
+
+extern int bar(void);
+
+int main()
+{
+  int volatile a = foo();
+  int volatile b = bar();
+
+  return 0;
+}
+
+/* { dg-final { scan-wpa-ipa-dump "Semantic equality hit:foo->bar" "icf"  } } */
+/* { dg-final { scan-wpa-ipa-dump "Equal symbols: 1" "icf"  } } */
