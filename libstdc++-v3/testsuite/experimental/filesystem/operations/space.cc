2e023647c8e5733305471da13cc5144e1cc3d42b
Jonathan Wakely <jwakely@redhat.com>
2018-05-03 19:58:00 +0100

    PR libstdc++/85632 fix wraparound in filesystem::space
    On 32-bit targets any values over 4GB would wrap and produce the wrong
    result.
    
    	PR libstdc++/85632 use uintmax_t for arithmetic
    	* src/filesystem/ops.cc (experimental::filesystem::space): Perform
    	arithmetic in result type.
    	* src/filesystem/std-ops.cc (filesystem::space): Likewise.
    	* testsuite/27_io/filesystem/operations/space.cc: Check total capacity
    	is greater than free space.
    	* testsuite/experimental/filesystem/operations/space.cc: New.
    
    From-SVN: r259901

--- libstdc++-v3/testsuite/experimental/filesystem/operations/space.cc
+++ libstdc++-v3/testsuite/experimental/filesystem/operations/space.cc
@@ -15,25 +15,27 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-options "-std=gnu++17 -lstdc++fs" }
-// { dg-do run { target c++17 } }
+// { dg-options "-DUSE_FILESYSTEM_TS -lstdc++fs" }
+// { dg-do run { target c++11 } }
 // { dg-require-filesystem-ts "" }
 
 // 30.10.14.3 Permissions [fs.op.space]
 
-#include <filesystem>
+#include <experimental/filesystem>
 #include <testsuite_fs.h>
 #include <testsuite_hooks.h>
+
+namespace fs = std::experimental::filesystem;
 
 void
 test01()
 {
-  std::filesystem::space_info s = std::filesystem::space("/");
+  fs::space_info s = fs::space("/");
   std::error_code ec = make_error_code(std::errc::invalid_argument);
-  s = std::filesystem::space("/", ec);
+  s = fs::space("/", ec);
   VERIFY( !ec );
 
-  s = std::filesystem::space(__gnu_test::nonexistent_path(), ec);
+  s = fs::space(__gnu_test::nonexistent_path(), ec);
   VERIFY( ec );
   VERIFY( s.capacity ==  static_cast<uintmax_t>(-1) );
   VERIFY( s.free ==  static_cast<uintmax_t>(-1) );
@@ -43,7 +45,7 @@ test01()
 void
 test02()
 {
-  std::filesystem::space_info s = std::filesystem::space(".");
+  fs::space_info s = fs::space(".");
   VERIFY( s.capacity >= s.free );
 }
 
