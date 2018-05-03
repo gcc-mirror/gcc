86f66562b73c61ae927b301d5dd907d7564c2e3f
Jonathan Wakely <jwakely@redhat.com>
2018-05-03 23:58:43 +0100

    PR libstdc++/82644 define TR1 hypergeometric functions in strict modes
    Following a recent change for PR 82644 the non-standard hypergeomtric
    functions are not defined by <cmath> when __STRICT_ANSI__ is defined
    (e.g. for -std=c++17, or -std=c++14 -D__STDCPP_WANT_MATH_SPEC_FUNCS__).
    That caused errors in <tr1/cmath> because the using-declarations for
    tr1::hyperg et al are invalid in strict modes.
    
    The solution is to define the TR1 hypergeometric functions inline in
    <tr1/cmath> if __STRICT_ANSI__ is defined.
    
    	PR libstdc++/82644
    	* include/tr1/cmath [__STRICT_ANSI__] (hypergf, hypergl, hyperg): Use
    	inline definitions instead of using-declarations.
    	[__STRICT_ANSI__] (conf_hypergf, conf_hypergl, conf_hyperg): Likewise.
    	* testsuite/tr1/5_numerical_facilities/special_functions/
    	07_conf_hyperg/compile_cxx17.cc: New.
    	* testsuite/tr1/5_numerical_facilities/special_functions/
    	17_hyperg/compile_cxx17.cc: New.
    
    From-SVN: r259912

---
+++ libstdc++-v3/testsuite/tr1/5_numerical_facilities/special_functions/17_hyperg/compile_cxx17.cc
@@ -0,0 +1,31 @@
+// Copyright (C) 2018 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=c++17" }
+// { dg-do compile { target c++17 } }
+
+#include <tr1/cmath>
+
+void
+test01()
+{
+  // For C++17 most TR1 special functions re-use the C++17 functions
+  // in namespace std, but in strict -std=c++17 mode the hypergeometric
+  // functions are not defined in namespace std. This test ensures they
+  // are still available in namespace std::tr1.
+  (void) std::tr1::hyperg(1.0, 2.0, 3.0, 4.0);
+}
