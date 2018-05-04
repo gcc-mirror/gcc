4781b6f92dafa907a26b97e0f0a16955ea24e42e
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-05-04 07:32:36 +0000

    PR ada/85635 	* link.c (BSD platforms): Add missing backslash.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@259925 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/link.c
+++ gcc/ada/link.c
@@ -104,7 +104,7 @@ unsigned char __gnat_separate_run_path_options = 0;
 const char *__gnat_default_libgcc_subdir = "lib";
 
 #elif defined (__FreeBSD__) || defined (__DragonFly__) \
-   || defined (__NetBSD__) || defined (__OpenBSD__)
+   || defined (__NetBSD__) || defined (__OpenBSD__) \
    || defined (__QNX__)
 const char *__gnat_object_file_option = "-Wl,@";
 const char *__gnat_run_path_option = "-Wl,-rpath,";
