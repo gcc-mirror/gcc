c6398a960e11eba6653b6e349793ca41c9c885d3
clyon <clyon@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-05-07 13:49:03 +0000

    [testsuite] gcc.dg/nextafter-2.c: Restrict to c99_runtime
    2018-05-07  Christophe Lyon  <christophe.lyon@linaro.org>
    
    	* gcc.dg/nextafter-2.c: Add c99_runtime effective target
    	and options.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@259994 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/nextafter-2.c
+++ gcc/testsuite/gcc.dg/nextafter-2.c
@@ -1,6 +1,8 @@
 /* PR libstdc++/85466 */
 /* { dg-do run } */
+/* { dg-require-effective-target c99_runtime } */
 /* { dg-options "-O2 -fno-builtin" } */
 /* { dg-add-options ieee } */
+/* { dg-add-options c99_runtime } */
 
 #include "nextafter-1.c"
