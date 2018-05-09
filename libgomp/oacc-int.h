2e3e0ea7e5cc2c4a850a95d79357ff83d986304f
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-05-09 13:52:49 +0000

    [openacc] Factor out async argument utility functions
    2018-05-09  Tom de Vries  <tom@codesourcery.com>
    
    	PR libgomp/83792
    	* oacc-int.h (async_valid_stream_id_p, async_valid_p)
    	(async_synchronous_p): New function.
    	* oacc-async.c (acc_async_test, acc_wait, acc_wait_all_async): Use
    	async_valid_p.
    	* oacc-cuda.c (acc_get_cuda_stream, acc_set_cuda_stream): Use
    	async_valid_stream_id_p.
    	* oacc-mem.c (gomp_acc_remove_pointer): Use async_synchronous_p.
    	* oacc-parallel.c (GOACC_parallel_keyed): Same.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@260081 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgomp/oacc-int.h
+++ libgomp/oacc-int.h
@@ -99,6 +99,28 @@ void goacc_restore_bind (void);
 void goacc_lazy_initialize (void);
 void goacc_host_init (void);
 
+static inline bool
+async_valid_stream_id_p (int async)
+{
+  return async >= 0;
+}
+
+static inline bool
+async_valid_p (int async)
+{
+  return (async == acc_async_noval || async == acc_async_sync
+	  || async_valid_stream_id_p (async));
+}
+
+static inline bool
+async_synchronous_p (int async)
+{
+  if (!async_valid_p (async))
+    return true;
+
+  return async == acc_async_sync;
+}
+
 #ifdef HAVE_ATTRIBUTE_VISIBILITY
 # pragma GCC visibility pop
 #endif
