7b24db31a41695cfd4adcee73f5edd73ae001517
Steven G. Kargl <kargl@gcc.gnu.org>
2018-05-10 22:43:00 +0000

    re PR fortran/70870 (Segmentation violation in gfc_assign_data_value)
    2018-05-10  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	PR fortran/70870
    	* data.c (gfc_assign_data_value): Check that a data object does
    	not also have default initialization.
    
    2018-05-10  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	PR fortran/70870
    	* gfortran.dg/pr70870_1.f90: New test.
    
    From-SVN: r260138

---
+++ gcc/testsuite/gfortran.dg/pr70870_1.f90
@@ -0,0 +1,9 @@
+! { dg-do compile }
+! PR fortran/70870
+! Contributed by Vittorio Zecca <zeccav at gmail dot com >
+      type t
+       integer :: g=0   ! default initialization
+      end type
+      type(t) :: v2
+      data v2/t(2)/     ! { dg-error "default initialization shall not" }
+      end
