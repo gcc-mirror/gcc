a3d084d95e82eb7623e34e598373a8cb9bc543f1
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-05-14 18:57:45 +0000

    PR libstdc++/81256 fix exception handling in basic_filebuf::close
    	PR libstdc++/81256
    	* include/bits/fstream.tcc (basic_filebuf::close): Do not swallow
    	exceptions from _M_terminate_output().
    	* include/std/fstream (basic_filebuf::~basic_filebuf): Swallow any
    	exceptions from close().
    	* testsuite/27_io/basic_filebuf/close/81256.cc: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@260236 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/27_io/basic_filebuf/close/81256.cc
@@ -0,0 +1,109 @@
+// Copyright (C) 2018 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do run { target c++11 } }
+
+#include <fstream>
+#include <locale>
+#include <testsuite_hooks.h>
+
+struct E : std::runtime_error
+{
+  E() : runtime_error("") { }
+};
+
+struct Cvt : std::codecvt<wchar_t, char, std::mbstate_t>
+{
+  explicit Cvt(size_t refs) : codecvt(refs) { }
+
+  mutable int exceptions_thrown = 0;
+
+private:
+  int
+  do_encoding() const noexcept override
+  { return -1; }
+
+  bool
+  do_always_noconv() const noexcept override
+  { return false; }
+
+  result
+  do_unshift(state_type&, char*, char*, char*&) const override
+  {
+    ++exceptions_thrown;
+    throw E();
+  }
+};
+
+struct filebuf : std::basic_filebuf<wchar_t>
+{
+  explicit filebuf(Cvt* c)
+  {
+    std::locale loc(std::locale::classic(), c);
+    imbue(loc);
+  }
+};
+
+void
+test01()
+{
+  // This facet needs to still be valid when ~basic_filebuf runs:
+  Cvt conv{1};
+  {
+    filebuf fb(&conv);
+    fb.open("output.txt", std::wios::out);
+    fb.sputn(L"x", 1);
+
+    bool caught = false;
+    try
+    {
+      /* [filebuf.members] p7: If one of these calls throws an exception,
+       * the exception is caught and rethrown after closing the file.  */
+      fb.close();
+    }
+    catch (const E&)
+    {
+      caught = true;
+    }
+    VERIFY( conv.exceptions_thrown == 1 );
+    VERIFY( caught );
+  }
+  VERIFY( conv.exceptions_thrown == 1 );
+}
+
+void
+test02()
+{
+  // This facet needs to still be valid when ~basic_filebuf runs:
+  Cvt conv{1};
+  {
+    filebuf fb(&conv);
+    fb.open("output.txt", std::wios::out);
+    fb.sputn(L"x", 1);
+    /* [filebuf.cons] p5: If an exception occurs during the destruction
+     * of the object, including the call to close(), the exception is
+     * caught but not rethrown.  */
+  }
+  VERIFY( conv.exceptions_thrown == 1 );
+}
+
+int
+main()
+{
+  test01();
+  test02();
+}
