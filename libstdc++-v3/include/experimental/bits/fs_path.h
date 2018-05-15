7330ffed52f1ec1d0a8571825daebe2ee1a58536
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-05-15 13:50:49 +0000

    PR libstdc++/83891 fix path::is_absolute() for non-POSIX targets
    The correct definition seems to be has_root_directory() for all systems
    we care about.
    
    	PR libstdc++/83891
    	* include/bits/fs_path.h (path::is_absolute()): Use same definition
    	for all operating systems.
    	* include/experimental/bits/fs_path.h (path::is_absolute()): Likewise.
    	* testsuite/27_io/filesystem/path/query/is_absolute.cc: New.
    	* testsuite/27_io/filesystem/path/query/is_relative.cc: Fix comment.
    	* testsuite/experimental/filesystem/path/query/is_absolute.cc: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@260259 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/experimental/bits/fs_path.h
+++ libstdc++-v3/include/experimental/bits/fs_path.h
@@ -368,7 +368,7 @@ _GLIBCXX_BEGIN_NAMESPACE_CXX11
     bool has_filename() const;
     bool has_stem() const;
     bool has_extension() const;
-    bool is_absolute() const;
+    bool is_absolute() const { return has_root_directory(); }
     bool is_relative() const { return !is_absolute(); }
 
     // iterators
@@ -997,16 +997,6 @@ _GLIBCXX_BEGIN_NAMESPACE_CXX11
   {
     auto ext = _M_find_extension();
     return ext.first && ext.second != string_type::npos;
-  }
-
-  inline bool
-  path::is_absolute() const
-  {
-#ifdef _GLIBCXX_FILESYSTEM_IS_WINDOWS
-    return has_root_name();
-#else
-    return has_root_directory();
-#endif
   }
 
   inline path::iterator
