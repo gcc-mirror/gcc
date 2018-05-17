9b4ef22db8686405929f0ae039683c535fd79c22
Paolo Carlini <paolo.carlini@oracle.com>
2018-05-17 09:17:56 +0000

    re PR c++/85713 (ICE in dependent_type_p, at cp/pt.c:24582 on valid code)
    2018-05-17  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/85713
    	* g++.dg/cpp1y/lambda-generic-85713-2.C: New.
    
    From-SVN: r260308

---
+++ gcc/testsuite/g++.dg/cpp1y/lambda-generic-85713-2.C
@@ -0,0 +1,7 @@
+// PR c++/85713
+// { dg-do compile { target c++14 } }
+
+auto l4 = [](auto v, auto (&array (int)) [5]) -> int { return v + array[0]; };
+auto l5 = [](auto v, auto (&array (auto)) [5]) -> int { return v + array[0]; };
+auto l6 = [](auto v, auto (&array (int int)) [5]) -> int { return v + array[0]; };  // { dg-error "two or more data types" }
+auto l7 = [](auto v, auto (&array (int auto)) [5]) -> int { return v + array[0]; };  // { dg-error "two or more data types" }
