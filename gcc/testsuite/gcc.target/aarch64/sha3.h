11e0e5fa724f9f6f979abe537d6485850abfe4d9
Tamar Christina <tamar.christina@arm.com>
2018-05-21 10:33:30 +0000

    Add missing AArch64 NEON instrinctics for Armv8.2-a to Armv8.4-a
    This patch adds the missing neon intrinsics for all 128 bit vector Integer modes for the
    three-way XOR and negate and xor instructions for Arm8.2-a to Armv8.4-a.
    
    gcc/
    	PR target/71233
    	* config/aarch64/aarch64-simd.md (aarch64_eor3qv8hi): Change to
    	eor3q<mode>4.
    	(aarch64_bcaxqv8hi): Change to bcaxq<mode>4.
    	* config/aarch64/aarch64-simd-builtins.def (veor3q_u8, veor3q_u32,
    	veor3q_u64, veor3q_s8, veor3q_s16, veor3q_s32, veor3q_s64, vbcaxq_u8,
    	vbcaxq_u32, vbcaxq_u64, vbcaxq_s8, vbcaxq_s16, vbcaxq_s32,
    	vbcaxq_s64): New.
    	* config/aarch64/arm_neon.h: Likewise.
    	* config/aarch64/iterators.md (VQ_I): New.
    
    gcc/testsuite/
    	PR target/71233
    	* gcc.target/aarch64/sha3.h (veor3q_u8, veor3q_u32,
    	veor3q_u64, veor3q_s8, veor3q_s16, veor3q_s32, veor3q_s64, vbcaxq_u8,
    	vbcaxq_u32, vbcaxq_u64, vbcaxq_s8, vbcaxq_s16, vbcaxq_s32,
    	vbcaxq_s64): New.
    	* gcc.target/aarch64/sha3_1.c: Likewise.
    	* gcc.target/aarch64/sha3_2.c: Likewise.
    	* gcc.target/aarch64/sha3_3.c: Likewise.
    
    (cherry picked from commit d21052ebd7ac9d545a26dde3229c57f872c1d5f3)

--- gcc/testsuite/gcc.target/aarch64/sha3.h
+++ gcc/testsuite/gcc.target/aarch64/sha3.h
@@ -1,10 +1,26 @@
 #include "arm_neon.h"
 
-uint16x8_t
-test_veor3q_u16 (uint16x8_t a, uint16x8_t b, uint16x8_t c)
-{
-  return veor3q_u16 (a, b, c);
-}
+#define TEST_VEOR3(T, S) T \
+test_veor3q_ ## S (T a, T b, T c) \
+{ \
+  return veor3q_ ## S (a, b, c); \
+} \
+
+#define TEST_VBCAX(T, S) T \
+test_vbcaxq_ ## S (T a, T b, T c) \
+{ \
+  return vbcaxq_ ## S (a, b, c); \
+} \
+
+
+TEST_VEOR3 (uint8x16_t, u8)
+TEST_VEOR3 (uint16x8_t, u16)
+TEST_VEOR3 (uint32x4_t, u32)
+TEST_VEOR3 (uint64x2_t, u64)
+TEST_VEOR3 (int8x16_t, s8)
+TEST_VEOR3 (int16x8_t, s16)
+TEST_VEOR3 (int32x4_t, s32)
+TEST_VEOR3 (int64x2_t, s64)
 
 uint64x2_t
 test_vrax1q_u64 (uint64x2_t a, uint64x2_t b)
@@ -18,8 +34,12 @@ test_vxarq_u64 (uint64x2_t a, uint64x2_t b)
   return vxarq_u64 (a, b, 15);
 }
 
-uint16x8_t
-test_vbcaxq_u16 (uint16x8_t a, uint16x8_t b, uint16x8_t c)
-{
-  return vbcaxq_u16 (a, b, c);
-}
+TEST_VBCAX (uint8x16_t, u8)
+TEST_VBCAX (uint16x8_t, u16)
+TEST_VBCAX (uint32x4_t, u32)
+TEST_VBCAX (uint64x2_t, u64)
+TEST_VBCAX (int8x16_t, s8)
+TEST_VBCAX (int16x8_t, s16)
+TEST_VBCAX (int32x4_t, s32)
+TEST_VBCAX (int64x2_t, s64)
+
