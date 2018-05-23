72cfbeae2b8dbc10e2738f5ccbe6328ac2111ddf
Richard Biener <rguenther@suse.de>
2018-05-23 09:06:01 +0000

    re PR middle-end/85874 (gcc points to wrong location when displaying warning for strict overflow warning)
    2018-05-23  Richard Biener  <rguenther@suse.de>
    
    	PR middle-end/85874
    	* tree-data-ref.c (create_runtime_alias_checks): Defer
    	and ignore overflow warnings.
    
    	* gcc.dg/Wstrict-overflow-27.c: New testcase.
    
    From-SVN: r260569

--- gcc/tree-data-ref.c
+++ gcc/tree-data-ref.c
@@ -1918,6 +1918,7 @@ create_runtime_alias_checks (struct loop *loop,
 {
   tree part_cond_expr;
 
+  fold_defer_overflow_warnings ();
   for (size_t i = 0, s = alias_pairs->length (); i < s; ++i)
     {
       const dr_with_seg_len& dr_a = (*alias_pairs)[i].first;
@@ -1940,6 +1941,7 @@ create_runtime_alias_checks (struct loop *loop,
       else
 	*cond_expr = part_cond_expr;
     }
+  fold_undefer_and_ignore_overflow_warnings ();
 }
 
 /* Check if OFFSET1 and OFFSET2 (DR_OFFSETs of some data-refs) are identical
