59100dfc42bbe92caff61bca1560da4a30f99906
Luis Machado <luis.machado@linaro.org>
2018-05-23 16:20:30 +0000

    [Patch 01/02] Introduce prefetch-minimum stride option
    This patch adds a new option to control the minimum stride, for a memory
    reference, after which the loop prefetch pass may issue software prefetch
    hints for. There are two motivations:
    
    * Make the pass less aggressive, only issuing prefetch hints for bigger strides
    that are more likely to benefit from prefetching. I've noticed a case in cpu2017
    where we were issuing thousands of hints, for example.
    
    * For processors that have a hardware prefetcher, like Falkor, it allows the
    loop prefetch pass to defer prefetching of smaller (less than the threshold)
    strides to the hardware prefetcher instead. This prevents conflicts between
    the software prefetcher and the hardware prefetcher.
    
    I've noticed considerable reduction in the number of prefetch hints and
    slightly positive performance numbers. This aligns GCC and LLVM in terms of
    prefetch behavior for Falkor.
    
    The default settings should guarantee no changes for existing targets. Those
    are free to tweak the settings as necessary.
    
    gcc/ChangeLog:
    
    2018-05-23  Luis Machado  <luis.machado@linaro.org>
    
    	* config/aarch64/aarch64-protos.h (cpu_prefetch_tune)
    	<minimum_stride>: New const int field.
    	* config/aarch64/aarch64.c (generic_prefetch_tune): Update to include
    	minimum_stride field defaulting to -1.
    	(exynosm1_prefetch_tune): Likewise.
    	(thunderxt88_prefetch_tune): Likewise.
    	(thunderx_prefetch_tune): Likewise.
    	(thunderx2t99_prefetch_tune): Likewise.
    	(qdf24xx_prefetch_tune) <minimum_stride>: Set to 2048.
    	<default_opt_level>: Set to 3.
    	(aarch64_override_options_internal): Update to set
    	PARAM_PREFETCH_MINIMUM_STRIDE.
    	* doc/invoke.texi (prefetch-minimum-stride): Document new option.
    	* params.def (PARAM_PREFETCH_MINIMUM_STRIDE): New.
    	* params.h (PARAM_PREFETCH_MINIMUM_STRIDE): Define.
    	* tree-ssa-loop-prefetch.c (should_issue_prefetch_p): Return false if
    	stride is constant and is below the minimum stride threshold.
    
    From-SVN: r260617

--- gcc/config/aarch64/aarch64.c
+++ gcc/config/aarch64/aarch64.c
@@ -550,6 +550,7 @@ static const cpu_prefetch_tune generic_prefetch_tune =
   -1,			/* l1_cache_size  */
   -1,			/* l1_cache_line_size  */
   -1,			/* l2_cache_size  */
+  -1,			/* minimum_stride */
   -1			/* default_opt_level  */
 };
 
@@ -559,6 +560,7 @@ static const cpu_prefetch_tune exynosm1_prefetch_tune =
   -1,			/* l1_cache_size  */
   64,			/* l1_cache_line_size  */
   -1,			/* l2_cache_size  */
+  -1,			/* minimum_stride */
   -1			/* default_opt_level  */
 };
 
@@ -568,7 +570,8 @@ static const cpu_prefetch_tune qdf24xx_prefetch_tune =
   32,			/* l1_cache_size  */
   64,			/* l1_cache_line_size  */
   512,			/* l2_cache_size  */
-  -1			/* default_opt_level  */
+  2048,			/* minimum_stride */
+  3			/* default_opt_level  */
 };
 
 static const cpu_prefetch_tune thunderxt88_prefetch_tune =
@@ -577,6 +580,7 @@ static const cpu_prefetch_tune thunderxt88_prefetch_tune =
   32,			/* l1_cache_size  */
   128,			/* l1_cache_line_size  */
   16*1024,		/* l2_cache_size  */
+  -1,			/* minimum_stride */
   3			/* default_opt_level  */
 };
 
@@ -586,6 +590,7 @@ static const cpu_prefetch_tune thunderx_prefetch_tune =
   32,			/* l1_cache_size  */
   128,			/* l1_cache_line_size  */
   -1,			/* l2_cache_size  */
+  -1,			/* minimum_stride */
   -1			/* default_opt_level  */
 };
 
@@ -595,6 +600,7 @@ static const cpu_prefetch_tune thunderx2t99_prefetch_tune =
   32,			/* l1_cache_size  */
   64,			/* l1_cache_line_size  */
   256,			/* l2_cache_size  */
+  -1,			/* minimum_stride */
   -1			/* default_opt_level  */
 };
 
@@ -10627,6 +10633,11 @@ aarch64_override_options_internal (struct gcc_options *opts)
   if (aarch64_tune_params.prefetch->l2_cache_size >= 0)
     maybe_set_param_value (PARAM_L2_CACHE_SIZE,
 			   aarch64_tune_params.prefetch->l2_cache_size,
+			   opts->x_param_values,
+			   global_options_set.x_param_values);
+  if (aarch64_tune_params.prefetch->minimum_stride >= 0)
+    maybe_set_param_value (PARAM_PREFETCH_MINIMUM_STRIDE,
+			   aarch64_tune_params.prefetch->minimum_stride,
 			   opts->x_param_values,
 			   global_options_set.x_param_values);
 
