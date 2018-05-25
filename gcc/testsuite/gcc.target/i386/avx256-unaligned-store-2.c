a1e9c80f203ffd1d420566d26b10e22bc910bf42
amker <amker@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-05-25 11:09:42 +0000

    PR tree-optimization/85720
    	* tree-loop-distribution.c (break_alias_scc_partitions): Don't merge
    	SCC if all partitions are builtins.
    	(version_loop_by_alias_check): New parameter.  Generate cancelable
    	runtime alias check if all partitions are builtins.
    	(distribute_loop): Update call to above function.
    
    	gcc/testsuite
    	* gcc.dg/tree-ssa/pr85720.c: New test.
    	* gcc.target/i386/avx256-unaligned-store-2.c: Disable loop pattern
    	distribution.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@260753 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/avx256-unaligned-store-2.c
+++ gcc/testsuite/gcc.target/i386/avx256-unaligned-store-2.c
@@ -1,5 +1,5 @@
 /* { dg-do compile { target { ! ia32 } } } */
-/* { dg-options "-O3 -mtune-ctrl=sse_typeless_stores -dp -mavx -mavx256-split-unaligned-store -mno-prefer-avx128" } */
+/* { dg-options "-O3 -mtune-ctrl=sse_typeless_stores -dp -mavx -mavx256-split-unaligned-store -mno-prefer-avx128 -fno-tree-loop-distribute-patterns" } */
 
 #define N 1024
 
