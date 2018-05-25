5a7aacd18052583ef1ed9620c20e25cc77c35938
Bob Duff <duff@adacore.com>
2018-05-25 09:03:19 +0000

    [Ada] Unbounded strings: inline Initialize and Adjust
    Procedures Initialize and Adjust in the Ada.[Wide_[Wide_]]Strings.Unbounded
    package are now inlined for nondispatching calls. No test available (efficiency
    issue only).
    
    2018-05-25  Bob Duff  <duff@adacore.com>
    
    gcc/ada/
    
    	* libgnat/a-strunb__shared.ads, libgnat/a-stwiun__shared.ads,
    	libgnat/a-stzunb__shared.ads: (Initialize, Adjust): Add pragma Inline.
    
    From-SVN: r260719

--- gcc/ada/libgnat/a-stzunb__shared.ads
+++ gcc/ada/libgnat/a-stzunb__shared.ads
@@ -503,6 +503,7 @@ private
      (Object : in out Unbounded_Wide_Wide_String);
    overriding procedure Finalize
      (Object : in out Unbounded_Wide_Wide_String);
+   pragma Inline (Initialize, Adjust);
 
    Null_Unbounded_Wide_Wide_String : constant Unbounded_Wide_Wide_String :=
                                        (AF.Controlled with
