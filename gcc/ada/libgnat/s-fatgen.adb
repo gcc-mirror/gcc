beb34f9b2ddd092afd6940e38ccbe9de45348561
pmderodat <pmderodat@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-05-29 09:42:05 +0000

    [Ada] Improper behavior of floating-point attributes
    This patch fixes an error in the handling of attributes Pred qnd Succ when
    applied to the limit values of a floating-point type. The RM mandates that
    such operations must raise constraint_error, but GNAT generated in most cases
    an infinite value, regardless of whether overflow checks were enabled.
    
    2018-05-29  Ed Schonberg  <schonberg@adacore.com>
    
    gcc/ada/
    
    	* libgnat/s-fatgen.adb (Succ, Pred):  Raise Constraint_Error
    	unconditionally when applied to the largest positive (resp. largest
    	negative) value of a floating-point type.
    
    gcc/testsuite/
    
    	* gnat.dg/float_attributes_overflows.adb: New testcase.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@260882 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/libgnat/s-fatgen.adb
+++ gcc/ada/libgnat/s-fatgen.adb
@@ -415,16 +415,7 @@ package body System.Fat_Gen is
 
       elsif X = T'First then
 
-         --  If not generating infinities, we raise a constraint error
-
-         if T'Machine_Overflows then
-            raise Constraint_Error with "Pred of largest negative number";
-
-         --  Otherwise generate a negative infinity
-
-         else
-            return X / (X - X);
-         end if;
+         raise Constraint_Error with "Pred of largest negative number";
 
       --  For infinities, return unchanged
 
@@ -671,14 +662,9 @@ package body System.Fat_Gen is
 
          --  If not generating infinities, we raise a constraint error
 
-         if T'Machine_Overflows then
-            raise Constraint_Error with "Succ of largest negative number";
+         raise Constraint_Error with "Succ of largest positive number";
 
          --  Otherwise generate a positive infinity
-
-         else
-            return X / (X - X);
-         end if;
 
       --  For infinities, return unchanged
 
