da651528995110251964fb81e4cacfe0265beb0f
Jan Hubicka <hubicka@ucw.cz>
2018-05-30 19:07:36 +0200

    20111213-1_0.c: Fix template.
    	* gcc.dg/lto/20111213-1_0.c: Fix template.
    	* gcc.dg/lto/20100603-1_0.c: Fix template.
    	* gcc.dg/lto/20100603-2_0.c: Fix template.
    	* gcc.dg/lto/20100603-3_0.c: Fix template.
    
    From-SVN: r260966

--- gcc/testsuite/gcc.dg/lto/20100603-3_0.c
+++ gcc/testsuite/gcc.dg/lto/20100603-3_0.c
@@ -1,5 +1,4 @@
 /* { dg-lto-do link } */
-/* { dg-extra-ld-options {-r -nostdlib} } */
-/* { dg-extra-ld-options "-flinker-output=nolto-rel" } */
+/* { dg-extra-ld-options {-r -nostdlib -flinker-output=nolto-rel} } */
 
 int i = 42;
