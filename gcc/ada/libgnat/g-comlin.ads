fbf04fecbd03f46d69e809a44d085ba8e4e6b7ae
pmderodat <pmderodat@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-05-30 08:58:00 +0000

    [Ada] Add support for Define_Switch with a callback in GNAT.Command_Line
    Add support for Define_Switch with a callback in GNAT.Command_Line.
    
    The callback is called for every instance of the switch found on the
    command line. This make it possible to have full control over the
    switch value and chain multiple actions if needed.
    
    2018-05-30  Pascal Obry  <obry@adacore.com>
    
    gcc/ada/
    
    	* libgnat/g-comlin.ads (Value_Callback, Define_Switch): New.
    	* libgnat/g-comlin.adb: Add corresponding implementation.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@260940 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/libgnat/g-comlin.ads
+++ gcc/ada/libgnat/g-comlin.ads
@@ -678,6 +678,20 @@ package GNAT.Command_Line is
    --  so that you can specify the default value directly in the declaration
    --  of the variable). The switch must accept an argument.
 
+   type Value_Callback is access procedure (Switch, Value : String);
+
+   procedure Define_Switch
+     (Config      : in out Command_Line_Configuration;
+      Callback    : not null Value_Callback;
+      Switch      : String := "";
+      Long_Switch : String := "";
+      Help        : String := "";
+      Section     : String := "";
+      Argument    : String := "ARG");
+   --  Call Callback for each instance of Switch. The callback is given the
+   --  actual switch and the corresponding value. The switch must accept
+   --  an argument.
+
    procedure Set_Usage
      (Config   : in out Command_Line_Configuration;
       Usage    : String := "[switches] [arguments]";
@@ -1111,7 +1125,8 @@ private
    type Switch_Type is (Switch_Untyped,
                         Switch_Boolean,
                         Switch_Integer,
-                        Switch_String);
+                        Switch_String,
+                        Switch_Callback);
 
    type Switch_Definition (Typ : Switch_Type := Switch_Untyped) is record
       Switch      : GNAT.OS_Lib.String_Access;
@@ -1135,6 +1150,8 @@ private
             Integer_Default : Integer;
          when Switch_String =>
             String_Output   : access GNAT.Strings.String_Access;
+         when Switch_Callback =>
+            Callback        : Value_Callback;
       end case;
    end record;
    type Switch_Definitions is array (Natural range <>) of Switch_Definition;
