e6a090b2d0121c0910224b2888633edb56e4b103
Steven G. Kargl <kargl@gcc.gnu.org>
2018-06-04 17:09:01 +0000

    re PR fortran/85981 (ICE in gfc_trans_string_copy, at fortran/trans-expr.c:6539)
    2018-06-04  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	PR fortran/85981
    	* resolve.c (resolve_allocate_deallocate): Check errmsg is default
    	character kind.
    
    2018-06-04  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	PR fortran/85981
    	* gfortran.dg/allocate_alloc_opt_14.f90: New test.
    	* gfortran.dg/allocate_alloc_opt_1.f90: Update error string.
    	* gfortran.dg/allocate_stat_2.f90: Ditto.
    	* gfortran.dg/deallocate_alloc_opt_1.f90: Ditto.
    
    From-SVN: r261162

--- gcc/testsuite/gfortran.dg/allocate_stat_2.f90
+++ gcc/testsuite/gfortran.dg/allocate_stat_2.f90
@@ -5,6 +5,6 @@ program main
   character(len=30), dimension(2) :: er
   integer, dimension (:), allocatable :: a
   allocate (a (16), stat = ier) ! { dg-error "must be a scalar INTEGER" }
-  allocate (a (14), stat=ier(1),errmsg=er) ! { dg-error "must be a scalar CHARACTER" }
+  allocate (a (14), stat=ier(1),errmsg=er) ! { dg-error "shall be a scalar default CHARACTER" }
 end
 
