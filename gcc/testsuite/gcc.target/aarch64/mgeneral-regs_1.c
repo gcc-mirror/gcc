aec530ac44bb5f96f1f3c63a43c53e5d5b9b5412
Steve Ellcey <sellcey@cavium.com>
2018-06-05 22:21:36 +0000

    re PR target/79924 (aarch64: untranslated diagnostics in aarch64_err_no_fpadvsimd)
    2018-06-05  Steve Ellcey  <sellcey@cavium.com>
    
    	PR target/79924
    	* gcc.target/aarch64/mgeneral-regs_1.c: Update error message.
    	* gcc.target/aarch64/mgeneral-regs_2.c: Ditto.
    	* gcc.target/aarch64/mgeneral-regs_3.c: Ditto.
    	* gcc.target/aarch64/nofp_1.c: Ditto.
    
    From-SVN: r261218

--- gcc/testsuite/gcc.target/aarch64/mgeneral-regs_1.c
+++ gcc/testsuite/gcc.target/aarch64/mgeneral-regs_1.c
@@ -2,8 +2,7 @@
 
 typedef int int32x2_t __attribute__ ((__vector_size__ ((8))));
 
-/* { dg-error "'-mgeneral-regs-only' is incompatible with vector return type" "" {target "aarch64*-*-*"} .+2 } */
-/* { dg-error "'-mgeneral-regs-only' is incompatible with vector argument" "" {target "aarch64*-*-*"} .+1 } */
+/* { dg-error "'-mgeneral-regs-only' is incompatible with the use of vector types" "" {target "aarch64*-*-*"} .+1 } */
 int32x2_t test (int32x2_t a, int32x2_t b)
 {
   return a + b;
