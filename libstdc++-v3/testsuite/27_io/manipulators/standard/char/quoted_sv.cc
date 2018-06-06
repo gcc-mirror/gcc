d30117c96381b8b97b12506d3acc8ff12ab368e4
Jonathan Wakely <jwakely@redhat.com>
2018-06-06 12:41:57 +0100

    PR libstdc++/86008 add std::quoted support for string_view
    	PR libstdc++/86008
    	* include/bits/quoted_string.h (_Quoted_string<basic_string_view, C>):
    	Define new partial specialization.
    	* include/std/iomanip (quoted(basic_string_view<C,T>, C, C)): Define
    	new overload.
    	(operator<<(basic_ostream<C,T>&, const _Quoted_string<S,C>&)): Use
    	value not reference for iteration.
    	* testsuite/27_io/manipulators/standard/char/quoted.cc: Adjust
    	comment.
    	* testsuite/27_io/manipulators/standard/char/quoted_sv.cc: New test.
    	* testsuite/27_io/manipulators/standard/wchar_t/quoted.cc: Adjust
    	comment.
    
    From-SVN: r261228

--- libstdc++-v3/testsuite/27_io/manipulators/standard/char/quoted_sv.cc
+++ libstdc++-v3/testsuite/27_io/manipulators/standard/char/quoted_sv.cc
@@ -1,6 +1,7 @@
-// { dg-do run { target c++14 } }
+// { dg-options "-std=gnu++17" }
+// { dg-do run { target c++17 } }
 
-// Copyright (C) 2013-2018 Free Software Foundation, Inc.
+// Copyright (C) 2018 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -17,9 +18,9 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// C++14 27.7.6 - Quoted manipulators		[quoted.manip]
+// C++17 30.7.8 - Quoted manipulators		[quoted.manip]
 
-#include <string>
+#include <string_view>
 #include <sstream>
 #include <iomanip>
 #include <testsuite_hooks.h>
@@ -27,11 +28,12 @@
 void
 test01()
 {
-  //  Basic test from paper.
   std::stringstream ss;
-  std::string original = "foolish me";
-  std::string round_trip;
+  const std::string_view original = R"(This "string" will be \"quoted\")";
+  std::string raw, round_trip;
   ss << std::quoted(original);
+  raw = ss.str();
+  VERIFY( raw == R"("This \"string\" will be \\\"quoted\\\"")" );
   ss >> std::quoted(round_trip);
   VERIFY( original == round_trip );
 }
@@ -39,35 +41,27 @@ test01()
 void
 test02()
 {
-  //  Test skipws correctness.
   std::stringstream ss;
-  ss << std::quoted("Hello Goodbye") << ' ' << 1 << ' ' << 2;
-  std::string song;
-  int thing1, thing2;
-  ss >> std::quoted(song) >> thing1 >> thing2;
-  VERIFY( song == "Hello Goodbye" );
-  VERIFY( thing1 == 1 );
-  VERIFY( thing2 == 2 );
+  const std::string_view original = R"(This "string" will be \"quoted\")";
+  std::string raw, round_trip;
+  ss << std::quoted(original, '\'', '!');
+  raw = ss.str();
+  VERIFY( raw == R"('This "string" will be \"quoted\"')" );
+  ss >> std::quoted(round_trip, '\'', '!');
+  VERIFY( original == round_trip );
 }
 
 void
 test03()
 {
-  //  Test read of unquoted string.
   std::stringstream ss;
-  ss << "Alpha Omega";
-  std::string testit;
-  ss >> std::quoted(testit);
-  VERIFY( testit == "Alpha" );
-}
-
-auto
-test04(const std::string& message)
-{
-  //  Test 'const basic_string&'
-  std::stringstream ss;
-  ss << "**  Error: " << std::quoted(message) << "  **";
-  return ss.str();
+  const std::string_view original = R"(This 'string' will be !'quoted!')";
+  std::string raw, round_trip;
+  ss << std::quoted(original, '\'', '!');
+  raw = ss.str();
+  VERIFY( raw == R"('This !'string!' will be !!!'quoted!!!'')" );
+  ss >> std::quoted(round_trip, '\'', '!');
+  VERIFY( original == round_trip );
 }
 
 int
@@ -76,8 +70,4 @@ main()
   test01();
   test02();
   test03();
-  auto ss = test04("My biscuits are burnin'!");
-  VERIFY( ss == "**  Error: \"My biscuits are burnin'!\"  **" );
-
-  return 0;
 }
