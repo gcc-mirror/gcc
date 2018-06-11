fafd0db149b237d2eee2f6ae420dbe3a0ca1ccbe
Paolo Carlini <paolo.carlini@oracle.com>
2018-06-11 21:13:19 +0000

    decl.c (grok_op_properties): Consistently use the location of the decl...
    /cp
    2018-06-11  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* decl.c (grok_op_properties): Consistently use the location
    	of the decl; remove special casing of POSTINCREMENT_EXPR and
    	POSTDECREMENT_EXPR wrt default arguments.
    
    /testsuite
    2018-06-11  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* g++.dg/lookup/new2.C: Test locations too.
    	* g++.dg/other/ellipsis1.C: Likewise.
    	* g++.dg/other/operator1.C: Likewise.
    	* g++.dg/other/operator2.C: Likewise.
    	* g++.dg/overload/operator2.C: Likewise.
    	* g++.dg/parse/defarg11.C: Likewise.
    	* g++.dg/parse/operator4.C: Likewise.
    	* g++.dg/template/error30.C: Likewise.
    	* g++.dg/template/explicit-args3.C: Likewise.
    	* g++.dg/warn/effc1.C: Likewise.
    	* g++.old-deja/g++.brendan/prepost2.C: Likewise.
    	* g++.old-deja/g++.brendan/prepost3.C: Likewise.
    	* g++.old-deja/g++.bugs/900215_01.C: Likewise.
    	* g++.old-deja/g++.jason/conversion5.C: Likewise.
    	* g++.old-deja/g++.jason/operator.C: Likewise.
    
    From-SVN: r261455

--- gcc/testsuite/g++.old-deja/g++.brendan/prepost3.C
+++ gcc/testsuite/g++.old-deja/g++.brendan/prepost3.C
@@ -3,5 +3,5 @@
 class Y {
 public:
    friend Y operator++ (Y&);
-   friend Y operator++ (Y&, char);	// illegal// { dg-error "" } .*
+   friend Y operator++ (Y&, char);  // { dg-error "13:postfix .Y operator\\+\\+\\(Y&, char\\). must have .int. as its second argument" }
 };
