b5a55805c6edc446a36859bdac856fe5d5854d85
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-06-11 22:01:55 +0000

    PR c++/85815 - reference to member of enclosing template.
    	* search.c (lookup_base): Use currently_open_class.
    	(lookup_member): Use it regardless of -fconcepts.
    	* parser.c (cp_parser_postfix_dot_deref_expression): Check it.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-8-branch@261460 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/parser.c
+++ gcc/cp/parser.c
@@ -7494,10 +7494,7 @@ cp_parser_postfix_dot_deref_expression (cp_parser *parser,
 	 access (5.2.5) outside the member function body.  */
       if (postfix_expression != current_class_ref
 	  && scope != error_mark_node
-	  && !(processing_template_decl
-	       && current_class_type
-	       && (same_type_ignoring_top_level_qualifiers_p
-		   (scope, current_class_type))))
+	  && !currently_open_class (scope))
 	{
 	  scope = complete_type (scope);
 	  if (!COMPLETE_TYPE_P (scope)
