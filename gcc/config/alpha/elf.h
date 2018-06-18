f96e566c3d6ea97ab431228ada9de38d8fd2f803
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-06-18 17:28:53 +0000

    2018-06-18  Maya Rashish  <coypu@sdf.org>
    	* crossconfig.m4: Handle OpenBSD just like NetBSD.
    	* configure: Rebuilt.
    
    	* config/alpha/openbsd.h (TARGET_DEFAULT): Define.
    	(LINK_SPEC, STARTFILE_SPEC, ENDFILE_SPEC): Likewise.
    	(INTMAX_TYPE, UINTMAX_TYPE, WINT_TYPE): Likewise.
    
    	* config/alpha/elf.h (STARTFILE_SPEC, ENDFILE_SPEC): Move from
    	here to ...
    	* config/alpha/linux.h (STARTFILE_SPEC, ENDFILE_SPEC): Here.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@261707 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/alpha/elf.h
+++ gcc/config/alpha/elf.h
@@ -100,32 +100,6 @@ do {									\
     ASM_OUTPUT_DEF (FILE, alias, name);				\
   } while (0)
 
-/* Provide a STARTFILE_SPEC appropriate for ELF.  Here we add the
-   (even more) magical crtbegin.o file which provides part of the
-   support for getting C++ file-scope static object constructed
-   before entering `main'.  */
-
-#undef	STARTFILE_SPEC
-#ifdef HAVE_LD_PIE
-#define STARTFILE_SPEC \
-  "%{!shared: %{pg|p:gcrt1.o%s;pie:Scrt1.o%s;:crt1.o%s}}\
-   crti.o%s %{static:crtbeginT.o%s;shared|pie:crtbeginS.o%s;:crtbegin.o%s}"
-#else
-#define STARTFILE_SPEC \
-  "%{!shared: %{pg|p:gcrt1.o%s;:crt1.o%s}}\
-   crti.o%s %{static:crtbeginT.o%s;shared|pie:crtbeginS.o%s;:crtbegin.o%s}"
-#endif
-
-/* Provide a ENDFILE_SPEC appropriate for ELF.  Here we tack on the
-   magical crtend.o file which provides part of the support for
-   getting C++ file-scope static object constructed before entering
-   `main', followed by a normal ELF "finalizer" file, `crtn.o'.  */
-
-#undef	ENDFILE_SPEC
-#define ENDFILE_SPEC \
-  "%{Ofast|ffast-math|funsafe-math-optimizations:crtfastmath.o%s} \
-   %{shared|pie:crtendS.o%s;:crtend.o%s} crtn.o%s"
-
 /* This variable should be set to 'true' if the target ABI requires
    unwinding tables even when exceptions are not used.  */
 #define TARGET_UNWIND_TABLES_DEFAULT true
