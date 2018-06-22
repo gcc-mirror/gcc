5a986e0b8b89e384d92d8848662a57e5d8d2b84d
Jakub Jelinek <jakub@redhat.com>
2018-06-22 22:54:44 +0200

    backport: re PR c++/84961 (ICE error: SSA_NAME_DEF_STMT is wrong)
    	Backported from mainline
    	2018-03-21  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/84961
    	* cp-tree.h (genericize_compound_lvalue): Declare.
    	* typeck.c (genericize_compound_lvalue): New function.
    	(unary_complex_lvalue, cp_build_modify_expr): Use it.
    	* semantics.c (finish_asm_stmt): Replace MODIFY_EXPR, PREINCREMENT_EXPR
    	and PREDECREMENT_EXPR in output and "m" constrained input operands with
    	COMPOUND_EXPR.  Call cxx_mark_addressable on the rightmost
    	COMPOUND_EXPR operand.
    
    	* c-c++-common/pr43690.c: Don't expect errors on "m" (--x) and
    	"m" (++x) in C++.
    	* g++.dg/torture/pr84961-1.C: New test.
    	* g++.dg/torture/pr84961-2.C: New test.
    
    From-SVN: r261933

--- gcc/testsuite/c-c++-common/pr43690.c
+++ gcc/testsuite/c-c++-common/pr43690.c
@@ -6,8 +6,8 @@ void
 foo (char *x)
 {
   asm ("" : : "m" (x++));	/* { dg-error "is not directly addressable" } */
-  asm ("" : : "m" (++x));	/* { dg-error "is not directly addressable" } */
+  asm ("" : : "m" (++x));	/* { dg-error "is not directly addressable" "" { target c } } */
   asm ("" : : "m" (x--));	/* { dg-error "is not directly addressable" } */
-  asm ("" : : "m" (--x));	/* { dg-error "is not directly addressable" } */
+  asm ("" : : "m" (--x));	/* { dg-error "is not directly addressable" "" { target c } } */
   asm ("" : : "m" (x + 1));	/* { dg-error "is not directly addressable" } */
 }
