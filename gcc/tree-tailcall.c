764ded997f5a0fb5ba0f65a5d537b63ff140203c
Jakub Jelinek <jakub@redhat.com>
2018-06-25 19:28:32 +0200

    backport: re PR tree-optimization/84739 (ICE in get_value_for_expr, at tree-ssa-ccp.c:649)
    	Backported from mainline
    	2018-03-08  Jakub Jelinek  <jakub@redhat.com>
    
    	PR tree-optimization/84739
    	* tree-tailcall.c (find_tail_calls): Check call arguments against
    	DECL_ARGUMENTS (current_function_decl) rather than
    	DECL_ARGUMENTS (func) when checking for tail recursion.
    
    	* gcc.dg/pr84739.c: New test.
    
    From-SVN: r262071

--- gcc/tree-tailcall.c
+++ gcc/tree-tailcall.c
@@ -467,7 +467,7 @@ find_tail_calls (basic_block bb, struct tailcall **ret)
     {
       tree arg;
 
-      for (param = DECL_ARGUMENTS (func), idx = 0;
+      for (param = DECL_ARGUMENTS (current_function_decl), idx = 0;
 	   param && idx < gimple_call_num_args (call);
 	   param = DECL_CHAIN (param), idx ++)
 	{
