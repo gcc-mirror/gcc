8a51585f2ce96ff8d4bebf168ab5127f268124ab
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-06-25 11:01:07 +0000

    2018-06-25  Richard Biener  <rguenther@suse.de>         Eric Botcazou  <ebotcazou@libertysurf.fr>
    	* tree-vect-data-refs.c (vect_check_gather_scatter): Fail
    	for reverse storage order accesses rather than asserting
    	they cannot happen here.
    
    	* gnat.dg/sso9.adb: New testcase.
    	* gnat.dg/sso9_pkg.ads: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@262007 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-vect-data-refs.c
+++ gcc/tree-vect-data-refs.c
@@ -3730,7 +3730,9 @@ vect_check_gather_scatter (gimple *stmt, loop_vec_info loop_vinfo,
      that can be gimplified before the loop.  */
   base = get_inner_reference (base, &pbitsize, &pbitpos, &off, &pmode,
 			      &punsignedp, &reversep, &pvolatilep);
-  gcc_assert (base && !reversep);
+  if (reversep)
+    return false;
+
   poly_int64 pbytepos = exact_div (pbitpos, BITS_PER_UNIT);
 
   if (TREE_CODE (base) == MEM_REF)
