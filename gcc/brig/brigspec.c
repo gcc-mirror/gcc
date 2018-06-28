62bb4a4b25f6176ad2b2902944e6a5058222b00a
Martin Liska <mliska@suse.cz>
2018-06-28 10:18:01 +0200

    Add missing header file inclusion.
    2018-06-28  Martin Liska  <mliska@suse.cz>
    
    	* brigspec.c: Add missing header file inclusion.
    
    From-SVN: r262214

--- gcc/brig/brigspec.c
+++ gcc/brig/brigspec.c
@@ -23,6 +23,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "system.h"
 #include "coretypes.h"
 #include "tm.h"
+#include "opt-suggestions.h"
 #include "gcc.h"
 #include "opts.h"
 
