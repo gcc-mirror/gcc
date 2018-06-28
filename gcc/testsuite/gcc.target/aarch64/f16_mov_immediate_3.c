cb11a2922e38bb29568aa20c505cc513c8ee4d8c
wilco <wilco@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-06-28 16:09:39 +0000

    [testsuite] Fix f16_mov_immediate_3.c
    Fix and simplify the testcase so it generates dup even on latest trunk.
    
        testsuite/
    	* gcc.target/aarch64/f16_mov_immediate_3.c: Fix testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@262222 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/aarch64/f16_mov_immediate_3.c
+++ gcc/testsuite/gcc.target/aarch64/f16_mov_immediate_3.c
@@ -1,18 +1,11 @@
 /* { dg-do compile } */
-/* { dg-options "-O0" } */
-
-extern __fp16 foo ();
+/* { dg-options "-O2" } */
 
 __fp16 f4 ()
 {
-  __fp16 a = 0;
-  __fp16 b = 1;
-  __fp16 c = 2;
-  __fp16 d = 4;
+  __fp16 a = 0.1;
 
-  __fp16 z = a + b;
-  z = z + c;
-  z = z - d;
+  __fp16 z = a * a;
   return z;
 }
 
