c48e53ece73d500c6dffd3824730d6599965b7ba
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-06-30 08:07:01 +0000

    [testsuite/guality] Use relative line numbers in gdb-test
    2018-06-30  Tom de Vries  <tdevries@suse.de>
    
    	* gcc.dg/guality/pr45882.c (foo): Use relative line numbers.
    	* lib/gcc-dg.exp (dg-final): New proc.
    	* lib/gcc-gdb-test.exp (gdb-test): Add and handle additional line number
    	argument.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@262269 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/guality/pr45882.c
+++ gcc/testsuite/gcc.dg/guality/pr45882.c
@@ -9,11 +9,11 @@ volatile short int v;
 __attribute__((noinline,noclone,used)) int
 foo (int i, int j)
 {
-  int b = i;		/* { dg-final { gdb-test bpline "b" "7" } } */
-  int c = i + 4;	/* { dg-final { gdb-test bpline "c" "11" } } */
-  int d = a[i];		/* { dg-final { gdb-test bpline "d" "112" } } */
-  int e = a[i + 6];	/* { dg-final { gdb-test bpline "e" "142" } } */
-  ++v;			/* { dg-line bpline } */
+  int b = i;		/* { dg-final { gdb-test .+4 "b" "7" } } */
+  int c = i + 4;	/* { dg-final { gdb-test .+3 "c" "11" } } */
+  int d = a[i];		/* { dg-final { gdb-test .+2 "d" "112" } } */
+  int e = a[i + 6];	/* { dg-final { gdb-test .+1 "e" "142" } } */
+  ++v;
   return ++j;
 }
 
