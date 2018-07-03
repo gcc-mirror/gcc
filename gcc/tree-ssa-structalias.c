ef773b31b6260ffc33959c485a272792d2b44b38
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-07-03 13:56:58 +0000

    2018-07-03  Richard Biener  <rguenther@suse.de>
    	PR ipa/86389
    	* tree-ssa-structalias.c (find_func_clobbers): Properly
    	handle indirect calls.
    
    	* gcc.dg/torture/pr86389.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@262341 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-structalias.c
+++ gcc/tree-ssa-structalias.c
@@ -5353,6 +5353,7 @@ find_func_clobbers (struct function *fn, gimple *origt)
       /* For callees without function info (that's external functions),
 	 ESCAPED is clobbered and used.  */
       if (cfi->decl
+	  && TREE_CODE (cfi->decl) == FUNCTION_DECL
 	  && !cfi->is_fn_info)
 	{
 	  varinfo_t vi;
