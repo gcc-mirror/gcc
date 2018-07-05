418be8984510edebc02b6bb50f6e2af7f43c1505
Martin Sebor <msebor@redhat.com>
2018-07-05 16:42:50 +0000

    backport: PR tree-optimization/86400 - set<string>::set<char (*)[2]) constructor does not
    Backport from trunk.
    
    PR tree-optimization/86400 - set<string>::set<char (*)[2]) constructor does not
    work with array argument.
    
    gcc/ChangeLog:
    	* gcc.dg/strlenopt-47.c: New test.
    	* gcc.dg/strlenopt-48.c: New test.
    
    gcc/testsuite/ChangeLog:
    	* gcc.dg/strlenopt-47.c: New test.
    	* gcc.dg/strlenopt-48.c: New test.
    
    From-SVN: r262446

---
+++ gcc/testsuite/gcc.dg/strlenopt-48.c
@@ -0,0 +1,35 @@
+/* PR tree-optimization/86400 - set<string>::set<char (*)[2]) constructor
+   does not work with array argument
+   Verify that strlen() calls with one-character array elements of
+   multidimensional arrays are still folded.
+   { dg-do compile }
+   { dg-options "-O2 -Wall -fdump-tree-optimized" } */
+
+#include "strlenopt.h"
+
+void f (void)
+{
+  extern char a[2][1];
+  int n = strlen (a[1]);
+  if (n)
+    abort();
+}
+
+void g (void)
+{
+  extern char b[3][2][1];
+  int n = strlen (b[2][1]);
+  if (n)
+    abort();
+}
+
+void h (void)
+{
+  extern char c[4][3][2][1];
+  int n = strlen (c[3][2][1]);
+  if (n)
+    abort();
+}
+
+/* { dg-final { scan-tree-dump-times "strlen" 0 "optimized" } }
+   { dg-final { scan-tree-dump-times "abort" 0 "optimized" } } */
