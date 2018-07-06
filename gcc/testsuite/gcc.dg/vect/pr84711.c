8660e7908fb884b5a0dda37f937ecced3da16070
Tamar Christina <tamar.christina@arm.com>
2018-07-06 10:44:35 +0000

    Require sse for testcase on i686.
    	PR target/84711
    	* gcc.dg/vect/pr84711.c: Add -msse for i686 targets.
    
    From-SVN: r262472

--- gcc/testsuite/gcc.dg/vect/pr84711.c
+++ gcc/testsuite/gcc.dg/vect/pr84711.c
@@ -1,6 +1,8 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target vect_int } */
+/* { dg-require-effective-target sse { target i386*-*-* } } */
 /* { dg-options "-O2" } */
+/* { dg-additional-options "-msse" { target i386*-*-* } } */
 
 typedef int v4si
   __attribute__ ((vector_size (16)));
