66a1e3c1696f4a5345fe398754e822a9aee0078a
msebor <msebor@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-07-12 22:42:00 +0000

    PR testsuite/86510 - test case g++.dg/warn/pr86453.C fails starting with r262596
    gcc/testsuite/ChangeLog:
    	g++.dg/warn/pr86453.C: Adjust.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@262609 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/warn/pr86453.C
+++ gcc/testsuite/g++.dg/warn/pr86453.C
@@ -1,5 +1,5 @@
 // { dg-do compile }
 // { dg-additional-options "-flto" { target lto } }
 struct X {
-  int *__attribute__((aligned(2), packed)) a; // { dg-warning "attribute ignored" }
+  int *__attribute__((aligned(2), packed)) a; // { dg-warning "ignoring attribute .packed. because it conflicts with attribute .aligned." }
 } b;
