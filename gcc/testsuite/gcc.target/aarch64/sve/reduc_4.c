e3798ed9f885f93206156529d61dd08a721b5f96
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-07-12 13:01:48 +0000

    Support fused multiply-adds in fully-masked reductions
    This patch adds support for fusing a conditional add or subtract
    with a multiplication, so that we can use fused multiply-add and
    multiply-subtract operations for fully-masked reductions.  E.g.
    for SVE we vectorise:
    
      double res = 0.0;
      for (int i = 0; i < n; ++i)
        res += x[i] * y[i];
    
    using a fully-masked loop in which the loop body has the form:
    
      res_1 = PHI<0(preheader), res_2(latch)>;
      avec = .MASK_LOAD (loop_mask, a)
      bvec = .MASK_LOAD (loop_mask, b)
      prod = avec * bvec;
      res_2 = .COND_ADD (loop_mask, res_1, prod, res_1);
    
    where the last statement does the equivalent of:
    
      res_2 = loop_mask ? res_1 + prod : res_1;
    
    (operating elementwise).  The point of the patch is to convert the last
    two statements into:
    
      res_s = .COND_FMA (loop_mask, avec, bvec, res_1, res_1);
    
    which is equivalent to:
    
      res_2 = loop_mask ? fma (avec, bvec, res_1) : res_1;
    
    (again operating elementwise).
    
    2018-07-12  Richard Sandiford  <richard.sandiford@linaro.org>
    	    Alan Hayward  <alan.hayward@arm.com>
    	    David Sherwood  <david.sherwood@arm.com>
    
    gcc/
    	* internal-fn.h (can_interpret_as_conditional_op_p): Declare.
    	* internal-fn.c (can_interpret_as_conditional_op_p): New function.
    	* tree-ssa-math-opts.c (convert_mult_to_fma_1): Handle conditional
    	plus and minus and convert them into IFN_COND_FMA-based sequences.
    	(convert_mult_to_fma): Handle conditional plus and minus.
    
    gcc/testsuite/
    	* gcc.dg/vect/vect-fma-2.c: New test.
    	* gcc.target/aarch64/sve/reduc_4.c: Likewise.
    	* gcc.target/aarch64/sve/reduc_6.c: Likewise.
    	* gcc.target/aarch64/sve/reduc_7.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@262588 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/aarch64/sve/reduc_4.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -ftree-vectorize -ffast-math" } */
+
+double
+f (double *restrict a, double *restrict b, int *lookup)
+{
+  double res = 0.0;
+  for (int i = 0; i < 512; ++i)
+    res += a[lookup[i]] * b[i];
+  return res;
+}
+
+/* { dg-final { scan-assembler-times {\tfmla\tz[0-9]+.d, p[0-7]/m, } 2 } } */
+/* Check that the vector instructions are the only instructions.  */
+/* { dg-final { scan-assembler-times {\tfmla\t} 2 } } */
+/* { dg-final { scan-assembler-not {\tfadd\t} } } */
+/* { dg-final { scan-assembler-times {\tfaddv\td0,} 1 } } */
+/* { dg-final { scan-assembler-not {\tsel\t} } } */
