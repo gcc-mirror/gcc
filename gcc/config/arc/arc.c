43bb0fc221b05f67c01879c741c321ad0a67fddf
Claudiu Zissulescu <claziss@synopsys.com>
2018-07-16 12:12:32 +0200

    [ARC] Add support for HS4x cpus.
    gcc/
    2017-03-10  Claudiu Zissulescu  <claziss@synopsys.com>
    
            * config/arc/arc-arch.h (arc_tune_attr): Add new tune parameters
            for ARCHS4x.
            * config/arc/arc-cpus.def (hs4x): New cpu.
            (hs4xd): Likewise.
            * config/arc/arc-tables.opt: Regenerate.
            * config/arc/arc.c (arc_sched_issue_rate): New function.
            (TARGET_SCHED_ISSUE_RATE): Define.
            (TARGET_SCHED_EXPOSED_PIPELINE): Likewise.
            * config/arc/arc.md (attr type): Add fpu_fuse, fpu_sdiv, fpu_ddiv,
            fpu_cvt.
            (attr tune): Add ARCHS4x tune values.
            (attr tune_dspmpy): Define.
            (*tst): Correct instruction type.
            * config/arc/arcHS.md: Don't use this automaton for ARCHS4x cpus.
            * config/arc/arcHS4x.md: New file.
            * config/arc/fpu.md: Update instruction type attributes.
            * config/arc/t-multilib: Regenerate.
    
    From-SVN: r262688

--- gcc/config/arc/arc.c
+++ gcc/config/arc/arc.c
@@ -483,6 +483,22 @@ arc_autovectorize_vector_sizes (vector_sizes *sizes)
     }
 }
 
+
+/* Implements target hook TARGET_SCHED_ISSUE_RATE.  */
+static int
+arc_sched_issue_rate (void)
+{
+  switch (arc_tune)
+    {
+    case TUNE_ARCHS4X:
+    case TUNE_ARCHS4XD:
+      return 3;
+    default:
+      break;
+    }
+  return 1;
+}
+
 /* TARGET_PRESERVE_RELOAD_P is still awaiting patch re-evaluation / review.  */
 static bool arc_preserve_reload_p (rtx in) ATTRIBUTE_UNUSED;
 static rtx arc_delegitimize_address (rtx);
@@ -564,6 +580,9 @@ static rtx arc_legitimize_address_0 (rtx, rtx, machine_mode mode);
 
 #undef  TARGET_SCHED_ADJUST_PRIORITY
 #define TARGET_SCHED_ADJUST_PRIORITY arc_sched_adjust_priority
+
+#undef TARGET_SCHED_ISSUE_RATE
+#define TARGET_SCHED_ISSUE_RATE arc_sched_issue_rate
 
 #undef TARGET_VECTOR_MODE_SUPPORTED_P
 #define TARGET_VECTOR_MODE_SUPPORTED_P arc_vector_mode_supported_p
