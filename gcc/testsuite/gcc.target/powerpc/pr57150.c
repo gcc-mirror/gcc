6bc66ef4207d642ca91d6d6f9d3e23a920ed9a7d
dje <dje@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-07-17 15:41:47 +0000

    * gcc.target/powerpc/pr57150.c: Require longdouble128. * gcc.target/powerpc/pr79916.c: Require dfp.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@262826 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/pr57150.c
+++ gcc/testsuite/gcc.target/powerpc/pr57150.c
@@ -1,5 +1,6 @@
 /* { dg-do compile { target { powerpc*-*-* } } } */
 /* { dg-skip-if "" { powerpc*-*-darwin* } } */
+/* { dg-require-effective-target longdouble128 } */
 /* { dg-require-effective-target powerpc_vsx_ok } */
 /* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power7" } } */
 /* { dg-options "-O3 -mcpu=power7 -fcaller-saves" } */
