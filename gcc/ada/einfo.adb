44f3169cd36f685ac6001f0cdd91a90bf467b690
pmderodat <pmderodat@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-07-17 08:05:34 +0000

    [Ada] Minor refactoring: remove extra parentheses
    2018-07-17  Piotr Trojanek  <trojanek@adacore.com>
    
    gcc/ada/
    
    	* einfo.adb (Is_Wrapper_Package): Remove extra parentheses.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@262770 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/einfo.adb
+++ gcc/ada/einfo.adb
@@ -8385,7 +8385,7 @@ package body Einfo is
 
    function Is_Wrapper_Package (Id : E) return B is
    begin
-      return (Ekind (Id) = E_Package and then Present (Related_Instance (Id)));
+      return Ekind (Id) = E_Package and then Present (Related_Instance (Id));
    end Is_Wrapper_Package;
 
    -----------------
