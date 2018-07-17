d6148817539c67f49a91e1905a2381153d7cfb92
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-07-17 18:10:57 +0000

    * lex.c (_cpp_lex_direct): Use CPP_DL_NOTE instead of CPP_DL_PEDWARN, 	CPP_DL_WARNING or CPP_DL_ERROR for note that diagnostics for C++ style 	comments is reported only once per file and guard those calls on the 	preceding cpp_error returning true.
    	* gcc.dg/cpp/pr61854-c90.c (foo): Expect a note, rather than error.
    	* gcc.dg/cpp/pr61854-c94.c (foo): Likewise.
    	* gcc.dg/cpp/pr61854-4.c (foo): Likewise.
    	* gcc.dg/cpp/pr61854-8.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@262832 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cpp/pr61854-c90.c
+++ gcc/testsuite/gcc.dg/cpp/pr61854-c90.c
@@ -7,7 +7,7 @@ foo (void)
 {
   // 1st
   /* { dg-error "C\\+\\+ style comments are not allowed in ISO C90" "comments"  { target *-*-*} .-1 } */
-  /* { dg-error "reported only once" ""  { target *-*-*} .-2 } */
+  /* { dg-message "note: \[^\n\r]*reported only once" ""  { target *-*-*} .-2 } */
   // 2nd
   // 3rd
 }
