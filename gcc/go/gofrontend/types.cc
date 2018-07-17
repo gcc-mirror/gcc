7f59a3c89badede8940dcea428a040254757b11b
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-07-17 17:21:10 +0000

    compiler: connect the concrete type and the placeholder for circular types
        Previously, when creating the backend representation of a
        circular type, we resolve the placeholder to a
        circular_pointer_type. The backend doesn't know what the
        concrete type is.
        
        This CL changes it to resolve the placeholder to the concrete
        type instead, so the backend may have better knowledge of the
        concrete type.
        
        Reviewed-on: https://go-review.googlesource.com/123738
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@262830 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/go/gofrontend/types.cc
+++ gcc/go/gofrontend/types.cc
@@ -10786,15 +10786,10 @@ Named_type::do_get_backend(Gogo* gogo)
       // Don't build a circular data structure.  GENERIC can't handle
       // it.
       if (this->seen_in_get_backend_)
-	{
-	  this->is_circular_ = true;
-	  return gogo->backend()->circular_pointer_type(bt, true);
-	}
+        return gogo->backend()->circular_pointer_type(bt, true);
       this->seen_in_get_backend_ = true;
       bt1 = Type::get_named_base_btype(gogo, base);
       this->seen_in_get_backend_ = false;
-      if (this->is_circular_)
-	bt1 = gogo->backend()->circular_pointer_type(bt, true);
       if (!gogo->backend()->set_placeholder_pointer_type(bt, bt1))
 	bt = gogo->backend()->error_type();
       return bt;
@@ -10803,15 +10798,10 @@ Named_type::do_get_backend(Gogo* gogo)
       // Don't build a circular data structure. GENERIC can't handle
       // it.
       if (this->seen_in_get_backend_)
-	{
-	  this->is_circular_ = true;
-	  return gogo->backend()->circular_pointer_type(bt, false);
-	}
+        return gogo->backend()->circular_pointer_type(bt, false);
       this->seen_in_get_backend_ = true;
       bt1 = Type::get_named_base_btype(gogo, base);
       this->seen_in_get_backend_ = false;
-      if (this->is_circular_)
-	bt1 = gogo->backend()->circular_pointer_type(bt, false);
       if (!gogo->backend()->set_placeholder_pointer_type(bt, bt1))
 	bt = gogo->backend()->error_type();
       return bt;
