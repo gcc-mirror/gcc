1c7a3c473555d3fad1a51374a19d2098a102cc89
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-07-19 19:12:37 +0000

    Simplify the base characteristics for some type traits
    	* include/std/type_traits (__is_member_object_pointer_helper): Use
    	__not_<is_function<_Tp>>::type instead of integral_constant.
    	(__is_member_function_pointer_helper): Likewise for
    	is_function<_Tp>::type.
    	(is_compund): Likewise for __not_<is_fundamental<_Tp>>::type.
    	(__do_is_nt_destructible_impl): Use __bool_constant and reindent.
    	(is_trivially_constructible): Remove redundant use of
    	is_constructible.
    	(__is_trivially_copy_assignable_impl): Remove redundant use of
    	is_copy_assignable.
    	(__is_trivially_move_assignable_impl): Remove redundant use of
    	is_move_assignable.
    	(is_trivially_destructible): Use __bool_constant.
    	* testsuite/20_util/is_trivially_assignable/value.cc: Add some more
    	tests for scalar types.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@262889 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/20_util/is_trivially_assignable/value.cc
+++ libstdc++-v3/testsuite/20_util/is_trivially_assignable/value.cc
@@ -44,91 +44,113 @@ void test01()
   using std::is_trivially_assignable;
   using namespace __gnu_test;
 
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		int, int>(false), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		int&, int>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		int&, int&>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		int&, int&&>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		int&, const int&>(true), "");
+  static_assert(test_property<is_trivially_assignable,
+		int&, int*>(false), "");
+  static_assert(test_property<is_trivially_assignable,
+		int&, void*>(false), "");
+  static_assert(test_property<is_trivially_assignable,
+		const int, int>(false), "");
+  static_assert(test_property<is_trivially_assignable,
+		const int&, int>(false), "");
+  static_assert(test_property<is_trivially_assignable,
+		const int&, const int&>(false), "");
+  static_assert(test_property<is_trivially_assignable,
+		const int*&, int*>(true), "");
+  static_assert(test_property<is_trivially_assignable,
+		int*&, const int*&>(false), "");
+  static_assert(test_property<is_trivially_assignable,
+		int*&, const int&>(false), "");
+  static_assert(test_property<is_trivially_assignable,
+		const int*&, void*>(false), "");
+  static_assert(test_property<is_trivially_assignable,
+		const void*&, void*>(true), "");
+  static_assert(test_property<is_trivially_assignable,
+		const void*&, int*>(true), "");
 
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		TType, TType>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		TType&, TType>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		TType&, TType&>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		TType&, TType&&>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		TType&, const TType&>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		PODType, PODType>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		NType&, NType&>(false), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		SLType, SLType>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::Empty, assign::Empty>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::Abstract, assign::Abstract>(false), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::Ellipsis, assign::Ellipsis>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::DelEllipsis, assign::DelEllipsis>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::Any, assign::Any>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::DelDef, assign::DelDef>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::DelCopy, assign::DelCopy>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::Nontrivial, assign::Nontrivial>(false), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::AnyAssign, assign::AnyAssign>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::DelAnyAssign, assign::DelAnyAssign>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::DelCopyAssign, assign::DelCopyAssign>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::MO, assign::MO>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::MO, assign::MO&&>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::MO, assign::MO&>(false), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		assign::MO, const assign::MO&>(false), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		CopyConsOnlyType, CopyConsOnlyType>(false), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		CopyConsOnlyType, const CopyConsOnlyType&>(false), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		MoveConsOnlyType, MoveConsOnlyType>(false), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		MoveConsOnlyType, MoveConsOnlyType&&>(false), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		HasTemplateCAssign, HasTemplateCAssign>(false), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		HasTemplateCAssign, const HasTemplateCAssign&>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		ClassType, DerivedType>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		ClassType, DerivedType&>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		ClassType, DerivedType&&>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		ClassType, const DerivedType&>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		MoveOnly, MoveOnly>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		MoveOnly, MoveOnly&&>(true), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		MoveOnly, MoveOnly&>(false), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		MoveOnly, const MoveOnly&>(false), "");
-  static_assert(test_property<is_trivially_assignable, 
+  static_assert(test_property<is_trivially_assignable,
 		MoveOnly2, MoveOnly2>(false), "");
 }
