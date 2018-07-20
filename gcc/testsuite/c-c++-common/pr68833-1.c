619837aadbd94cdf7cc73ec32af6395044a7735c
msebor <msebor@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-07-20 21:18:31 +0000

    PR middle-end/82063
    gcc/testsuite/ChangeLog:
    
    	* gcc/testsuite/c-c++-common/pr68833-1.c: Adjust.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@262911 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/c-c++-common/pr68833-1.c
+++ gcc/testsuite/c-c++-common/pr68833-1.c
@@ -3,8 +3,8 @@
 /* { dg-options "-Werror=larger-than-65536 -Werror=format -Werror=missing-noreturn" } */
 /* { dg-require-effective-target int32plus } */
 
-int a[131072];	/* { dg-error "size of 'a' is \[1-9]\[0-9]* bytes" } */
-int b[1024];	/* { dg-bogus "size of 'b' is \[1-9]\[0-9]* bytes" } */
+int a[131072];	/* { dg-error "size of .a. 524288 bytes exceeds maximum object size 65536" } */
+int b[1024];	/* { dg-bogus "size" } */
 
 void
 f1 (const char *fmt)
