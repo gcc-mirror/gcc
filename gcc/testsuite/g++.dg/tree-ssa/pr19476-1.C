ffb164c7df6931b92cf2ad2ef4bd102cec8e6be3
Jakub Jelinek <jakub@gcc.gnu.org>
2018-07-24 12:24:00 +0200

    re PR testsuite/86649 (g++.dg/tree-ssa/pr19476-1.C fails starting with r262928)
    	PR testsuite/86649
    	* g++.dg/tree-ssa-/pr19476-1.C: Check dom2 dump instead of ccp1.
    	* g++.dg/tree-ssa-/pr19476-5.C: Likewise.
    	* g++.dg/tree-ssa-/pr19476-6.C: New test.
    	* g++.dg/tree-ssa-/pr19476-7.C: New test.
    
    From-SVN: r262942

--- gcc/testsuite/g++.dg/tree-ssa/pr19476-1.C
+++ gcc/testsuite/g++.dg/tree-ssa/pr19476-1.C
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O -fdump-tree-ccp1 -fdelete-null-pointer-checks" } */
+/* { dg-options "-O -fdump-tree-dom2 -fdelete-null-pointer-checks" } */
 /* { dg-skip-if "" keeps_null_pointer_checks } */
 
 // See pr19476-5.C for a version without including <new>.
@@ -12,5 +12,5 @@ int g(){
   return 42 + (0 == new int[50]);
 }
 
-/* { dg-final { scan-tree-dump     "return 42" "ccp1" } } */
-/* { dg-final { scan-tree-dump-not "return 33" "ccp1" } } */
+/* { dg-final { scan-tree-dump     "return 42" "dom2" } } */
+/* { dg-final { scan-tree-dump-not "return 33" "dom2" } } */
