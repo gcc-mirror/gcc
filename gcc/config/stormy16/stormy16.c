01557bd44e08074f0458dd367efbc59b6c5008d8
Nick Clifton <nickc@redhat.com>
2018-08-02 16:13:32 +0000

    re PR target/86813 (xstormy16 port needs updating for CVE-2017-5753)
    	PR target/86813
    	* config/stormy16/stormy16.c (TARGET_HAVE_SPECULATION_SAFE_VALUE):
    	Define to speculation_safe_value_not_needed.
    
    From-SVN: r263266

--- gcc/config/stormy16/stormy16.c
+++ gcc/config/stormy16/stormy16.c
@@ -2728,6 +2728,9 @@ xstormy16_push_rounding (poly_int64 bytes)
 #undef TARGET_CONSTANT_ALIGNMENT
 #define TARGET_CONSTANT_ALIGNMENT constant_alignment_word_strings
 
+#undef  TARGET_HAVE_SPECULATION_SAFE_VALUE
+#define TARGET_HAVE_SPECULATION_SAFE_VALUE speculation_safe_value_not_needed
+
 struct gcc_target targetm = TARGET_INITIALIZER;
 
 #include "gt-stormy16.h"
