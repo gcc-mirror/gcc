efbf3c347591b1173b4ed5a83b71d41dec8ca732
Jeff Law <law@redhat.com>
2018-08-02 11:24:59 -0600

    re PR target/86784 (H8 port needs updating for CVE-2017-5753)
    	PR target/86784
    	* config/h8300/h8300.c (TARGET_HAVE_SPECULATION_SAFE_VALUE):
    	Define to speculation_safe_value_not_needed.
    
    From-SVN: r263270

--- gcc/config/h8300/h8300.c
+++ gcc/config/h8300/h8300.c
@@ -6148,4 +6148,7 @@ h8300_push_rounding (poly_int64 bytes)
 #undef TARGET_MODE_DEPENDENT_ADDRESS_P
 #define TARGET_MODE_DEPENDENT_ADDRESS_P h8300_mode_dependent_address_p
 
+#undef TARGET_HAVE_SPECULATION_SAFE_VALUE
+#define TARGET_HAVE_SPECULATION_SAFE_VALUE speculation_safe_value_not_needed
+
 struct gcc_target targetm = TARGET_INITIALIZER;
