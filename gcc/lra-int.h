30dc1902a777966dc1d1dad0fb5f19b7a960e5ca
Alan Hayward <alan.hayward@arm.com>
2018-08-06 09:51:01 +0000

    lra support for clobber_high
    gcc/
    	* lra-eliminations.c (lra_eliminate_regs_1): Check for clobber high.
    	(mark_not_eliminable): Likewise.
    	* lra-int.h (struct lra_insn_reg): Add clobber high marker.
    	* lra-lives.c (process_bb_lives): Check for clobber high.
    	* lra.c (new_insn_reg): Remember clobber highs.
    	(collect_non_operand_hard_regs): Check for clobber high.
    	(lra_set_insn_recog_data): Likewise.
    	(add_regs_to_insn_regno_info): Likewise.
    	(lra_update_insn_regno_info): Likewise.
    
    From-SVN: r263329

--- gcc/lra-int.h
+++ gcc/lra-int.h
@@ -168,6 +168,8 @@ struct lra_insn_reg
   /* True if there is an early clobber alternative for this
      operand.  */
   unsigned int early_clobber : 1;
+  /* True if the reg is clobber highed by the operand.  */
+  unsigned int clobber_high : 1;
   /* The corresponding regno of the register.  */
   int regno;
   /* Next reg info of the same insn.  */
