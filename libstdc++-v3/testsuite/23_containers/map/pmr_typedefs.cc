002ced10e579c67038008ed4df4de57e7f416b2d
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-08-09 23:25:53 +0000

    Define aliases for containers using polymorphic_allocator
    These aliases are placed in the top-level header, e.g. <vector> not
    <bits/stl_vector.h>. This ensures that they refer to whichever of
    std::vector or __debug::vector or __profile::vector is in use when the
    header is included.
    
    	* include/std/deque (std::pmr::deque): Declare alias.
    	* include/std/forward_list (std::pmr::forward_list): Likewise.
    	* include/std/list (std::pmr::list): Likewise.
    	* include/std/map (std::pmr::map, std::pmr::multimap): Likewise.
    	* include/std/regex (std::pmr::match_results, std::pmr::cmatch)
    	(std::pmr::smatch, std::pmr::wcmatch, std::pmr::wsmatch): Likewise.
    	* include/std/set (std::pmr::set, std::pmr::multiset): Likewise.
    	* include/std/string (std::pmr::basic_string, std::pmr::string)
    	(std::pmr::u16string, std::pmr::u32string, std::pmr::wstring):
    	Likewise.
    	* include/std/unordered_map (std::pmr::unordered_map)
    	(std::pmr::unordered_multimap): Likewise.
    	* include/std/unordered_set (std::pmr::unordered_set)
    	(std::pmr::unordered_multiset): Likewise.
    	* include/std/vector (std::pmr::vector): Likewise.
    	* testsuite/21_strings/basic_string/types/pmr_typedefs.cc: New test.
    	* testsuite/23_containers/deque/types/pmr_typedefs.cc: New test.
    	* testsuite/23_containers/forward_list/pmr_typedefs.cc: New test.
    	* testsuite/23_containers/list/pmr_typedefs.cc: New test.
    	* testsuite/23_containers/map/pmr_typedefs.cc: New test.
    	* testsuite/23_containers/multimap/pmr_typedefs.cc: New test.
    	* testsuite/23_containers/multiset/pmr_typedefs.cc: New test.
    	* testsuite/23_containers/set/pmr_typedefs.cc: New test.
    	* testsuite/23_containers/unordered_map/pmr_typedefs.cc: New test.
    	* testsuite/23_containers/unordered_multimap/pmr_typedefs.cc: New
    	test.
    	* testsuite/23_containers/unordered_multiset/pmr_typedefs.cc: New
    	test.
    	* testsuite/23_containers/unordered_set/pmr_typedefs.cc: New test.
    	* testsuite/23_containers/vector/pmr_typedefs.cc: New test.
    	* testsuite/28_regex/match_results/pmr_typedefs.cc: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@263456 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/23_containers/map/pmr_typedefs.cc
@@ -0,0 +1,33 @@
+// Copyright (C) 2018 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++17" }
+// { dg-do compile { target c++17 } }
+
+#include <map>
+#include <memory_resource>
+
+struct X { };
+struct Y { };
+struct Cmp { bool operator()(X, X) const { return false; } };
+
+static_assert(std::is_same_v<std::pmr::map<X, Y>,
+    std::map<X, Y, std::less<X>,
+	     std::pmr::polymorphic_allocator<std::pair<const X, Y>>>>);
+static_assert(std::is_same_v<std::pmr::map<X, Y, Cmp>,
+    std::map<X, Y, Cmp,
+	     std::pmr::polymorphic_allocator<std::pair<const X, Y>>>>);
