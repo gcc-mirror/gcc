3b8ba91c6fc4b1c259d72ea34ce301d0b4d92445
msebor <msebor@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-08-14 19:11:20 +0000

    PR tree-optimization/86650 - -Warray-bounds missing inlining context
    gcc/ChangeLog:
    
    	PR tree-optimization/86650
    	* tree-vrp.c (vrp_prop::check_array_ref): Print an inform message.
    	(vrp_prop::check_mem_ref): Same.
    
    gcc/testsuite/ChangeLog:
    
    	PR tree-optimization/86650
    	* gcc.dg/Warray-bounds-34.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@263541 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/Warray-bounds-34.c
@@ -0,0 +1,18 @@
+/* PR tree-optimization/86650 - -Warray-bounds missing inlining context
+   { dg-do compile }
+   { dg-options "-O2 -Warray-bounds" } */
+
+int a[3];           /* { dg-message "while referencing .a." } */
+int x;
+
+inline void foo (int i)
+{
+  a[i + 1] = 123;   /* { dg-warning "\\\[-Warray-bounds]" } */
+}
+
+int bar (void)
+{
+  foo (3);
+
+  return x;
+}
