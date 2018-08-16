ab354715c4fde24411bb2926ba33a734ca89cbbc
Martin Liska <mliska@suse.cz>
2018-08-16 12:16:32 +0200

    Backport r263248
    2018-08-16  Martin Liska  <mliska@suse.cz>
    
    	Backport from mainline
    	2018-08-02  Martin Liska  <mliska@suse.cz>
    
            PR gcov-profile/86817
    	* gcov.c (process_all_functions): New function.
    	(main): Call it.
    	(process_file): Move functions processing to
            process_all_functions.
    
    From-SVN: r263580

--- gcc/gcov.c
+++ gcc/gcov.c
@@ -536,6 +536,7 @@ static int process_args (int, char **);
 static void print_usage (int) ATTRIBUTE_NORETURN;
 static void print_version (void) ATTRIBUTE_NORETURN;
 static void process_file (const char *);
+static void process_all_functions (void);
 static void generate_results (const char *);
 static void create_file_names (const char *);
 static char *canonicalize_name (const char *);
@@ -791,6 +792,7 @@ main (int argc, char **argv)
 
       if (flag_intermediate_format || argno == argc - 1)
 	{
+	  process_all_functions ();
 	  generate_results (argv[argno]);
 	  release_structures ();
 	}
@@ -1132,11 +1134,14 @@ process_file (const char *file_name)
 {
   create_file_names (file_name);
   read_graph_file ();
-  if (functions.empty ())
-    return;
-
   read_count_file ();
+}
 
+/* Process all functions in all files.  */
+
+static void
+process_all_functions (void)
+{
   hash_map<function_start_pair_hash, function_info *> fn_map;
 
   /* Identify group functions.  */
@@ -1212,7 +1217,6 @@ process_file (const char *file_name)
 	     and end_line information of the function.  */
 	  if (fn->is_group)
 	    fn->lines.resize (fn->end_line - fn->start_line + 1);
-
 
 	  solve_flow_graph (fn);
 	  if (fn->has_catch)
