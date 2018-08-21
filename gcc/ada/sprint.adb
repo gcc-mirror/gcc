1bd9b6a5766c03bc6741d5599076476bd1c9d2b0
Ed Schonberg <schonberg@adacore.com>
2018-08-21 14:47:12 +0000

    [Ada] Sprint: add guard on printing aspects
    2018-08-21  Ed Schonberg  <schonberg@adacore.com>
    
    gcc/ada/
    
    	* sprint.adb: Add guard on printing aspects.
    
    From-SVN: r263723

--- gcc/ada/sprint.adb
+++ gcc/ada/sprint.adb
@@ -3540,8 +3540,10 @@ package body Sprint is
       --  where the aspects are printed inside the package specification.
 
       if Has_Aspects (Node)
-         and then not Nkind_In (Node, N_Package_Declaration,
-                                      N_Generic_Package_Declaration)
+        and then not Nkind_In (Node, N_Package_Declaration,
+                               N_Generic_Package_Declaration)
+        and then not Is_Empty_List (Aspect_Specifications (Node))
+        and then not Is_Empty_List (Aspect_Specifications (Node))
       then
          Sprint_Aspect_Specifications (Node, Semicolon => True);
       end if;
