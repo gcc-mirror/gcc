8073a3278173a9ab7c5ba2e01cfe1fb0a1b94c00
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-08-22 13:02:48 +0000

    [2/2] Fix bogus inner induction (PR 86725)
    This patch is the second part of the fix for PR 86725.  The problem
    in the original test is that for:
    
      outer1:
        x_1 = PHI <x_4(outer2), ...>;
        ...
    
      inner:
        x_2 = PHI <x_1(outer1), x_3(...)>;
        ...
        x_3 = ...;
        ...
    
      outer2:
        x_4 = PHI <x_3(inner)>;
        ...
    
    there are corner cases in which it is possible to classify the
    inner phi as an induction but not the outer phi.  The -4.c test
    is a more direct example.
    
    After failing to classify x_1 as an induction, we go on to
    classify it as a double reduction (which is basically true).
    But we still classified the inner phi as an induction rather
    than as part of a reduction, leading to an ICE when trying
    to vectorise the outer phi.
    
    We analyse the phis for outer loops first, so the simplest
    fix is not to classify the phi as an induction if outer loop
    analysis said that it should be a reduction.
    
    The -2.c test is from the original PR.  The -3.c test is a
    version in which "wo" really is used a reduction; this was
    already correctly rejected, but for the wrong reason ("inner-loop
    induction only used outside of the outer vectorized loop").
    The -4.c test is another way of tickling the original problem
    without relying on the undefinedness of signed overflow.
    The -5.c test shows an (uninteresting) example in which the
    patch prevents a spurious failure to vectorise the outer loop.
    
    2018-08-22  Richard Sandiford  <richard.sandiford@arm.com>
    
    gcc/
    	PR tree-optimization/86725
    	* tree-vect-loop.c (vect_inner_phi_in_double_reduction_p): New
    	function.
    	(vect_analyze_scalar_cycles_1): Check it.
    
    gcc/testsuite/
    	PR tree-optimization/86725
    	* gcc.dg/vect/no-scevccp-pr86725-2.c: New test.
    	* gcc.dg/vect/no-scevccp-pr86725-3.c: Likewise.
    	* gcc.dg/vect/no-scevccp-pr86725-4.c: Likewise.
    	* gcc.dg/vect/no-scevccp-pr86725-5.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@263774 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/no-scevccp-pr86725-4.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+/* { dg-additional-options "-O -w" } */
+
+int
+nr (unsigned int xe, unsigned int qqn)
+{
+  unsigned int oo, wo = 0;
+
+  for (oo = 0; oo < 4; ++oo)
+    {
+      unsigned int qq = qqn;
+      do
+        {
+          wo += 1;
+          xe += wo;
+        }
+      while (qq-- > 0);
+    }
+  return xe;
+}
+
+/* { dg-final { scan-tree-dump "reduction used in loop" "vect" { target vect_int } } } */
+/* { dg-final { scan-tree-dump-not "OUTER LOOP VECTORIZED" "vect" } } */
