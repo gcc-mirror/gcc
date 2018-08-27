218a51b98bf03cef934b0912bac4acc067294447
dmalcolm <dmalcolm@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-08-27 23:33:02 +0000

    C++: fix-it hint for missing "typename" (PR c++/63392)
    This patch adds a fix-it hint to missing "typename" errors in the C++
    frontend, suggesting the insertion of "typename ".
    
    This addresses part of PR c++/63392; however it does not improve the
    error-recovery for such cases.
    
    gcc/cp/ChangeLog:
    	PR c++/63392
    	* parser.c (cp_parser_diagnose_invalid_type_name): Add fix-it
    	hint.
    
    gcc/testsuite/ChangeLog:
    	PR c++/63392
    	* g++.dg/diagnostic/missing-typename.C: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@263899 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/parser.c
+++ gcc/cp/parser.c
@@ -3405,8 +3405,10 @@ cp_parser_diagnose_invalid_type_name (cp_parser *parser, tree id,
       else if (TYPE_P (parser->scope)
 	       && dependent_scope_p (parser->scope))
 	{
+	  gcc_rich_location richloc (location);
+	  richloc.add_fixit_insert_before ("typename ");
 	  if (TREE_CODE (parser->scope) == TYPENAME_TYPE)
-	    error_at (location,
+	    error_at (&richloc,
 		      "need %<typename%> before %<%T::%D::%E%> because "
 		      "%<%T::%D%> is a dependent scope",
 		      TYPE_CONTEXT (parser->scope),
@@ -3415,7 +3417,7 @@ cp_parser_diagnose_invalid_type_name (cp_parser *parser, tree id,
 		      TYPE_CONTEXT (parser->scope),
 		      TYPENAME_TYPE_FULLNAME (parser->scope));
 	  else
-	    error_at (location, "need %<typename%> before %<%T::%E%> because "
+	    error_at (&richloc, "need %<typename%> before %<%T::%E%> because "
 		      "%qT is a dependent scope",
 		      parser->scope, id, parser->scope);
 	}
