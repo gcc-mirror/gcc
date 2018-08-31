5367a58ed3e350649584843df74eb2c90825a044
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-08-31 11:19:33 +0000

    [C++ PATCH] Remove K&R declaration hack.
    https://gcc.gnu.org/ml/gcc-patches/2018-08/msg01885.html
    	gcc/
    	* doc/extend.texi (Backwards Compatibility): Remove implicit
    	extern C leeway of () being (...).
    	gcc/cp/
    	* decl.c (decls_match): Remove SYSTEM_IMPLICIT_EXTERN_C matching
    	of return types and parms.
    	* parser.c (cp_parser_parameter_declaration_clause): Likewise,
    	'()' always means '(void)'.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@264013 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/parser.c
+++ gcc/cp/parser.c
@@ -21378,16 +21378,7 @@ cp_parser_parameter_declaration_clause (cp_parser* parser)
     }
   else if (token->type == CPP_CLOSE_PAREN)
     /* There are no parameters.  */
-    {
-#ifdef SYSTEM_IMPLICIT_EXTERN_C
-      if (in_system_header_at (input_location)
-	  && current_class_type == NULL
-	  && current_lang_name == lang_name_c)
-	return NULL_TREE;
-      else
-#endif
-	return void_list_node;
-    }
+    return void_list_node;
   /* Check for `(void)', too, which is a special case.  */
   else if (token->keyword == RID_VOID
 	   && (cp_lexer_peek_nth_token (parser->lexer, 2)->type
