e00dfa841f28206591d0276d83804fa6e2739dce
Marek Polacek <polacek@redhat.com>
2018-09-08 14:27:25 +0000

    array-init.c: Add dg-prune-output.
    	* c-c++-common/array-init.c: Add dg-prune-output.
    	* g++.dg/cpp0x/lambda/lambda-const-this.C: Add dg-warning.
    	* g++.dg/cpp0x/lambda/lambda-in-class-neg.C: Likewise.
    	* g++.dg/cpp0x/lambda/lambda-in-class.C: Likewise.
    	* g++.dg/cpp0x/lambda/lambda-nested.C: Likewise.
    	* g++.dg/cpp0x/lambda/lambda-nsdmi1.C: Likewise.
    	* g++.dg/cpp0x/lambda/lambda-nsdmi4.C: Likewise.
    	* g++.dg/cpp0x/lambda/lambda-this.C: Likewise.
    	* g++.dg/cpp0x/lambda/lambda-this17.C: Likewise.
    	* g++.dg/cpp0x/lambda/lambda-this18.C: Likewise.
    	* g++.dg/cpp0x/lambda/lambda-this2.C: Likewise.
    	* g++.dg/cpp0x/lambda/lambda-this8.C: Likewise.
    	* g++.dg/cpp1y/pr64382.C: Likewise.
    	* g++.dg/cpp1y/pr77739.C: Likewise.
    	* g++.dg/cpp1z/lambda-this1.C: Likewise.
    	* g++.dg/cpp1z/lambda-this2.C: Likewise.
    	* g++.dg/template/crash84.C: Adjust dg-error.
    
    From-SVN: r264169

--- gcc/testsuite/g++.dg/cpp0x/lambda/lambda-this.C
+++ gcc/testsuite/g++.dg/cpp0x/lambda/lambda-this.C
@@ -6,8 +6,9 @@ struct A
   int i;
   void f()
   {
-    [=] { i = 0; };
+    [=] { i = 0; };		// { dg-warning "implicit capture" "" { target c++2a } }
     [&] { i = 0; };
     [=] { this = 0; };		// { dg-error "lvalue" }
+// { dg-warning "implicit capture" "" { target c++2a } .-1 }
   }
 };
