86fc6ec9f366fd95d976c01bfa24c6775537ba62
Jonathan Wakely <jwakely@redhat.com>
2018-09-11 11:55:49 +0100

    Implement LWG 2905 changes to constrain unique_ptr constructors
    LWG DR 2905 says that is_constructible_v<unique_ptr<P, D>, P, D const &>
    should be false when D is not copy constructible. This commit implements
    the changes from the DR and simplifies the signatures as per
    https://github.com/cplusplus/draft/issues/1530
    
    	* include/bits/unique_ptr.h (__uniq_ptr_impl): Add assertions to
    	check deleter type.
    	(unique_ptr::unique_ptr(pointer, const deleter_type&)): Add copy
    	constructible constraint.
    	(unique_ptr::unique_ptr(pointer, deleter_type&&)): Disable for
    	deleters of reference type and add move constructible constraint.
    	(unique_ptr::unique_ptr(pointer, remove_reference_t<deleter_type>&&)):
    	Disable for deleters of non-reference type. Define as deleted.
    	(unique_ptr<T[], D>): Likewise.
    	* testsuite/20_util/unique_ptr/assign/48635_neg.cc: Replace dg-error
    	directives with unstable line numbers with dg-prune-output.
    	* testsuite/20_util/unique_ptr/cons/cv_qual_neg.cc: Likewise.
    	* testsuite/20_util/unique_ptr/cons/lwg2905.cc: New test.
    	* testsuite/20_util/unique_ptr/specialized_algorithms/swap_cxx17.cc:
    	Make deleter types invocable.
    
    From-SVN: r264206

---
+++ libstdc++-v3/testsuite/20_util/unique_ptr/cons/lwg2905.cc
@@ -0,0 +1,78 @@
+// Copyright (C) 2017 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile { target c++11 } }
+
+#include <memory>
+
+template<typename T, typename D, typename P, typename E>
+constexpr bool check()
+{ return std::is_constructible<std::unique_ptr<T, D>, P, E>::value; }
+
+struct Del { void operator()(void*) const { } };
+
+static_assert( ! check<int, Del&, int*, Del>(), "" );
+static_assert( check<int, Del&, int*, Del&>(), "" );
+static_assert( check<int, const Del&, int*, Del&>(), "" );
+static_assert( ! check<int, Del&, int*, const Del&>(), "" );
+static_assert( check<int, Del, int*, const Del&>(), "" );
+static_assert( check<int, Del, int*, Del>(), "" );
+
+static_assert( ! check<int[], Del&, int*, Del>(), "" );
+static_assert( check<int[], Del&, int*, Del&>(), "" );
+static_assert( check<int[], const Del&, int*, Del&>(), "" );
+static_assert( ! check<int[], Del&, int*, const Del&>(), "" );
+static_assert( check<int[], Del, int*, const Del&>(), "" );
+static_assert( check<int[], Del, int*, Del>(), "" );
+
+struct DelNoCopy {
+  DelNoCopy() = default;
+  DelNoCopy(const DelNoCopy&) = delete;
+  DelNoCopy(DelNoCopy&&) = default;
+  void operator()(void*) const { }
+};
+
+static_assert( ! check<int, DelNoCopy&, int*, DelNoCopy>(), "" );
+static_assert( check<int, DelNoCopy&, int*, DelNoCopy&>(), "" );
+static_assert( check<int, const DelNoCopy&, int*, DelNoCopy&>(), "" );
+static_assert( ! check<int, DelNoCopy&, int*, const DelNoCopy&>(), "" );
+static_assert( ! check<int, DelNoCopy, int*, const DelNoCopy&>(), "" );
+static_assert( check<int, DelNoCopy, int*, DelNoCopy>(), "" );
+
+static_assert( ! check<int[], DelNoCopy&, int*, DelNoCopy>(), "" );
+static_assert( check<int[], DelNoCopy&, int*, DelNoCopy&>(), "" );
+static_assert( check<int[], const DelNoCopy&, int*, DelNoCopy&>(), "" );
+static_assert( ! check<int[], DelNoCopy&, int*, const DelNoCopy&>(), "" );
+static_assert( ! check<int[], DelNoCopy, int*, const DelNoCopy&>(), "" );
+static_assert( check<int[], DelNoCopy, int*, DelNoCopy>(), "" );
+
+struct Base { virtual ~Base() { } };
+struct Derived : Base { };
+
+static_assert( ! check<Base[], Del&, Base*, Del>(), "" );
+static_assert( check<Base[], Del&, Base*, Del&>(), "" );
+static_assert( check<Base[], const Del&, Base*, Del&>(), "" );
+static_assert( ! check<Base[], Del&, Base*, const Del&>(), "" );
+static_assert( check<Base[], Del, Base*, const Del&>(), "" );
+static_assert( check<Base[], Del, Base*, Del>(), "" );
+
+static_assert( ! check<Base[], Del&, Derived*, Del>(), "" );
+static_assert( ! check<Base[], Del&, Derived*, Del&>(), "" );
+static_assert( ! check<Base[], const Del&, Derived*, Del&>(), "" );
+static_assert( ! check<Base[], Del&, Derived*, const Del&>(), "" );
+static_assert( ! check<Base[], Del, Derived*, const Del&>(), "" );
+static_assert( ! check<Base[], Del, Derived*, Del>(), "" );
