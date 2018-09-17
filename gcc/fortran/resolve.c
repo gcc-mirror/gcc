1edfd7ddf58c3b8122b46d532b512d3d5d31ab63
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-09-17 07:18:17 +0000

    2018-09-17  Paul Thomas  <pault@gcc.gnu.org>
    	PR fortran/85954
    	* resolve.c (resolve_assoc_var): If the target expression is a
    	deferred charlen dummy and the associate name shares the
    	charlen, generate a new one. Make sure that new charlens are in
    	the namespace list so that they get cleaned up.
    	* trans-array.c (gfc_is_reallocatable_lhs): Associate names are
    	not reallocatable.
    	* trans-decl.c (gfc_get_symbol_decl): Put deferred character
    	length dummy and result arrays on the deferred initialization
    	list so that the variable length arrays can be correctly dealt
    	with.
    	* trans-expr.c (gfc_conv_string_length): Return if 'expr' is
    	NULL rather than ICEing..
    
    2018-09-17  Paul Thomas  <pault@gcc.gnu.org>
    
    	PR fortran/85954
    	* gfortran.dg/deferred_character_21.f90 : New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@264358 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/resolve.c
+++ gcc/fortran/resolve.c
@@ -8744,6 +8744,14 @@ resolve_assoc_var (gfc_symbol* sym, bool resolve_target)
       if (!sym->ts.u.cl)
 	sym->ts.u.cl = target->ts.u.cl;
 
+      if (sym->ts.deferred && target->expr_type == EXPR_VARIABLE
+	  && target->symtree->n.sym->attr.dummy
+	  && sym->ts.u.cl == target->ts.u.cl)
+	{
+	  sym->ts.u.cl = gfc_new_charlen (sym->ns, NULL);
+	  sym->ts.deferred = 1;
+	}
+
       if (!sym->ts.u.cl->length
 	  && !sym->ts.deferred
 	  && target->expr_type == EXPR_CONSTANT)
@@ -8756,7 +8764,7 @@ resolve_assoc_var (gfc_symbol* sym, bool resolve_target)
 		|| sym->ts.u.cl->length->expr_type != EXPR_CONSTANT)
 		&& target->expr_type != EXPR_VARIABLE)
 	{
-	  sym->ts.u.cl = gfc_get_charlen();
+	  sym->ts.u.cl = gfc_new_charlen (sym->ns, NULL);
 	  sym->ts.deferred = 1;
 
 	  /* This is reset in trans-stmt.c after the assignment
