0b7fb27b698da38fd13108ecc914613f85f66f9d
Allan Sandfeld Jensen <allan.jensen@qt.io>
2018-09-21 01:38:24 +0600

    Fix and document -r option
    The option has existed and been working for years,
    make sure it implies the right extra options, and list
    it in the documentation.
    
    2018-09-20  Allan Sandfeld Jensen  <allan.jensen@qt.io>
    
    gcc/
    	* gcc.c (LINK_COMMAND_SPEC): Handle -r like -nostdlib.
    	(VTABLE_VERIFICATION_SPEC): Likewise.
    	(SANITIZER_EARLY_SPEC): Likewise.
    	(SANITIZER_SPEC): Likewise.
    	* config/darwin.h (LINK_COMMAND_SPEC): Likewise.
    	* doc/invoke.texi (Link Options): Document -r.
    
    gcc/cp/
    	* g++spec.c (lang_specific_driver): Handle -r like -nostdlib.
    
    gcc/fortran/
    	* gfortranspec.c (lang_specific_driver): Handle -r like -nostdlib.
    
    gcc/go/
    	* gospec.c (lang_specific_driver): Handle -r like -nostdlib.
    
    gcc/testsuite/
    	* g++.dg/ipa/pr64059.C: Removed now redundant -nostdlib.
    	* g++.dg/lto/20081109-1_0.C: Likewise.
    	* g++.dg/lto/20090302_0.C: Likewise.
    	* g++.dg/lto/pr45621_0.C: Likewise.
    	* g++.dg/lto/pr60567_0.C: Likewise.
    	* g++.dg/lto/pr62026.C: Likewise.
    	* gcc.dg/lto/pr45736_0.c: Likewise.
    	* gcc.dg/lto/pr52634_0.c: Likewise.
    	* gfortran.dg/lto/20091016-1_0.f90: Likewise.
    	* gfortran.dg/lto/pr79108_0.f90: Likewise.
    
    From-SVN: r264451

--- gcc/cp/g++spec.c
+++ gcc/cp/g++spec.c
@@ -184,6 +184,7 @@ lang_specific_driver (struct cl_decoded_option **in_decoded_options,
 	  break;
 
 	case OPT_c:
+	case OPT_r:
 	case OPT_S:
 	case OPT_E:
 	case OPT_M:
--- gcc/testsuite/g++.dg/lto/pr60567_0.C
+++ gcc/testsuite/g++.dg/lto/pr60567_0.C
@@ -1,7 +1,7 @@
 // PR lto/60567
 // { dg-lto-do link }
 // { dg-lto-options { { -flto -fno-use-linker-plugin } } }
-// { dg-extra-ld-options "-r -nostdlib" }
+// { dg-extra-ld-options "-r" }
 
 #pragma implementation
 struct S {};
