97b8f8679d0db4b9159e1640ff34460aaccace74
H.J. Lu <hongjiu.lu@intel.com>
2018-09-25 13:51:45 +0000

    i386: Compile pr82699-5.c and pr82699-6.c with -fno-pic
    Compile pr82699-5.c and pr82699-6.c with -fno-pic to avoid
    
    FAIL: gcc.target/i386/pr82699-5.c (test for excess errors)
    Excess errors:
    cc1: sorry, unimplemented: -mfentry isn't supported for 32-bit in combination with -fpic
    FAIL: gcc.target/i386/pr82699-6.c (test for excess errors)
    Excess errors:
    cc1: error: -mnop-mcount is not implemented for -fPIC
    cc1: sorry, unimplemented: -mfentry isn't supported for 32-bit in combination with -fpic
    
    when running GCC testsuite with --target_board='unix{-fpic\ -m32,-fpic}'.
    
    	PR target/82699
    	* gcc.target/i386/pr82699-5.c: Add -fno-pic for ia32.
    	* gcc.target/i386/pr82699-6.c: Add -fno-pic.
    
    From-SVN: r264569

--- gcc/testsuite/gcc.target/i386/pr82699-5.c
+++ gcc/testsuite/gcc.target/i386/pr82699-5.c
@@ -1,5 +1,6 @@
 /* { dg-do compile { target *-*-linux* } } */
 /* { dg-options "-O2 -fcf-protection -mfentry -fasynchronous-unwind-tables" } */
+/* { dg-additional-options "-fno-pic" { target ia32 } } */
 /* { dg-final { scan-assembler-times {\t\.cfi_startproc\n\tendbr} 1 } } */
 
 extern int bar (int);
