fffa8e82b0059c9e83612dba76077fbe718153a7
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-10-01 13:10:48 +0000

    2018-10-01  Richard Biener  <rguenther@suse.de>
    	PR tree-optimization/87465
    	* tree-ssa-loop-ivcanon.c (tree_estimate_loop_size): Fix typo
    	causing branch miscounts.
    
    	* gcc.dg/tree-ssa/cunroll-15.c: New testcase.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@264758 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/cunroll-15.c
@@ -0,0 +1,22 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -funroll-loops -fdump-tree-cunroll-optimized -fdump-tree-optimized" } */
+
+int Test(void)
+{
+  int c = 0;
+  const int in[4] = {4,3,4,4};
+  for (unsigned i = 0; i < 4; i++) {
+      for (unsigned j = 0; j < i; j++) {
+	  if (in[i] == in[j])
+	    break;
+	  else 
+	    ++c;
+      }
+  }
+  return c;
+}
+
+/* { dg-final { scan-tree-dump-times "optimized:\[^\n\r\]*completely unrolled" 2 "cunroll" } } */
+/* When SLP vectorization is enabled the following will fail because DOM
+   doesn't know how to deal with the vectorized initializer of in.  */
+/* { dg-final { scan-tree-dump "return 1;" "optimized" } } */
