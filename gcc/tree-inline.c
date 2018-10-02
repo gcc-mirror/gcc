18e2a21264b0674d67af95b2db05e36cad27f7c1
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-10-02 10:07:29 +0000

    2018-10-02  Richard Biener  <rguenther@suse.de>
    	* tree-inline.c (expand_call_inline): Use the location of
    	the callee declaration for the inline-entry marker.
    	* final.c (notice_source_line): Remove special-casing of
    	NOTE_INSN_INLINE_ENTRY.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@264778 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-inline.c
+++ gcc/tree-inline.c
@@ -4592,7 +4592,8 @@ expand_call_inline (basic_block bb, gimple *stmt, copy_body_data *id)
     {
       gimple_stmt_iterator si = gsi_last_bb (bb);
       gsi_insert_after (&si, gimple_build_debug_inline_entry
-			(id->block, input_location), GSI_NEW_STMT);
+			(id->block, DECL_SOURCE_LOCATION (id->src_fn)),
+			GSI_NEW_STMT);
     }
 
   if (DECL_INITIAL (fn))
