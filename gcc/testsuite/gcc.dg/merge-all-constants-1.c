718f7174c0ec2dfa857a91518cfc7e09c359e7ac
edlinger <edlinger@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-10-04 17:34:56 +0000

    2018-10-04  Bernd Edlinger  <bernd.edlinger@hotmail.de>
            * varasm.c (output_constant): Add new parameter merge_strings.
            Make strings properly zero terminated in merge string sections.
            (mergeable_string_section): Don't fail if the last char is non-zero.
            (assemble_variable_contents): Handle merge string sections.
            (assemble_variable): Likewise.
            (assemble_constant_contents): Likewise.
            (output_constant_def_contents): Likewise.
            (output_constructor_array_range,
            output_constructor_regular_field): Adjust call to output_constant.
            (output_object_block): Adjust call to assemble_constant_contents
            and assemble_variable_contents.
    
    testsuie:
    2018-10-04  Bernd Edlinger  <bernd.edlinger@hotmail.de>
    
            * gnat.dg/string_merge1.adb: New test.
            * gnat.dg/string_merge2.adb: New test.
            * gcc.dg/merge-all-constants-1.c: Adjust test.
            * gcc.dg/merge-all-constants-2.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@264850 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/merge-all-constants-1.c
+++ gcc/testsuite/gcc.dg/merge-all-constants-1.c
@@ -1,8 +1,8 @@
 /* { dg-do compile } */
 /* { dg-options "-w -O2 -fmerge-all-constants" } */
 
-const char str1[36] = "0123456789abcdefghijklmnopqrstuvwxyz";
+const char str1[36] = "\000123456789abcdefghijklmnopqrstuvwxyz";
 const char str2[38] = "0123456789abcdefghijklmnopqrstuvwxyz";
-const char str3[10] = "0123456789abcdefghijklmnopqrstuvwxyz";
+const char str3[10] = "\000123456789abcdefghijklmnopqrstuvwxyz";
 
-/* { dg-final { scan-assembler-not "\.rodata\.str" } } */
+/* { dg-final { scan-assembler-not "\\.rodata\\.str" } } */
---
+++ gcc/testsuite/gcc.dg/merge-all-constants-2.c
@@ -0,0 +1,8 @@
+/* { dg-do compile } */
+/* { dg-options "-w -O2 -fmerge-all-constants" } */
+
+const char str1[36] = "0123456789abcdefghijklmnopqrstuvwxyz";
+const char str2[37] = "0123456789abcdefghijklmnopqrstuvwxyz";
+const char str3[10] = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+/* { dg-final { scan-assembler-not "\\.rodata\[\n\r\]" } } */
