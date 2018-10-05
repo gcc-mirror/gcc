62b06971c80d3a6824c0473548290a0f2142fded
H.J. Lu <hongjiu.lu@intel.com>
2018-10-05 11:31:45 +0000

    i386: Don't pass -msse2avx to assembler for -mavx
    With
    
    gcc -O2 -fPIC -flto -g -c -o a.o a.c
    gcc -O2 -fPIC -flto -g -mavx   -c -o b.o b.c
    gcc -shared -O2 -fPIC -flto -g -o lib1.so a.o b.o
    
    LTO correctly generates AVX for b.o and SSE for a.o.  But the GCC driver
    passes -msse2avx to assembler, which encodes SSE instructions as AVX
    instructions.  We shouldn't pass -msse2avx to assembler for -mavx.
    
    	Backport from mainline
    	PR target/87522
    	* config/i386/gnu-user.h (ASM_SPEC): Don't pass -msse2avx to
    	assembler for -mavx.
    	* config/i386/gnu-user64.h (ASM_SPEC): Likewise.
    
    From-SVN: r264865

--- gcc/config/i386/gnu-user.h
+++ gcc/config/i386/gnu-user.h
@@ -67,7 +67,7 @@ along with GCC; see the file COPYING3.  If not see
 
 #undef  ASM_SPEC
 #define ASM_SPEC \
-  "--32 %{!mno-sse2avx:%{mavx:-msse2avx}} %{msse2avx:%{!mavx:-msse2avx}}"
+  "--32 %{msse2avx:%{!mavx:-msse2avx}}"
 
 #undef  SUBTARGET_EXTRA_SPECS
 #define SUBTARGET_EXTRA_SPECS \
