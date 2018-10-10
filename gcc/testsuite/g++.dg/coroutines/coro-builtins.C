200a028cfe3457d9ca8ecb4b1366ba7962c0ed67
Iain Sandoe <iain@sandoe.co.uk>
2018-10-10 10:17:17 +0000

    c++coroutines - Add base FE builtins.
    	2018-10-10  Iain Sandoe  <iain@sandoe.co.uk>
    
    	gcc/
    	* builtin-types.def (BT_FN_BOOL_PTR, BT_FN_PTR_PTR_SIZE_BOOL): New.
    	* builtins.def (DEF_COROUTINE_BUILTIN): New.
    	Include coroutine-builtins.def.
    	* coroutine-builtins.def: New.
    	gcc/testsuite/
    	* coro-builtins.C: New.
    
    From-SVN: r265009

---
+++ gcc/testsuite/g++.dg/coroutines/coro-builtins.C
@@ -0,0 +1,18 @@
+//  { dg-additional-options "-fsyntax-only " }
+
+typedef __SIZE_TYPE__ size_t;
+
+int main ()
+{
+  void *co_h;
+  void *promise;
+  const size_t co_align = 16;
+
+  co_h = __builtin_coro_noop();
+  bool d = __builtin_coro_done(co_h);
+  __builtin_coro_resume(co_h);
+  __builtin_coro_destroy(co_h);
+  promise = __builtin_coro_promise(co_h, co_align, true);
+
+  return 0;
+}
