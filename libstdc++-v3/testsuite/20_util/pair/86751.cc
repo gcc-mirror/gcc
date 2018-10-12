147e60654722ba72460673ec90878e3e9bfb266f
Jonathan Wakely <jwakely@redhat.com>
2018-10-12 13:51:46 +0100

    PR libstdc++/86751 default assignment operators for std::pair
    The solution for PR 77537 causes ambiguities due to the extra copy
    assignment operator taking a __nonesuch_no_braces parameter. By making
    the base class non-assignable we don't need the extra deleted overload
    in std::pair. The copy assignment operator will be implicitly deleted
    (and the move assignment operator not declared) as needed. Without the
    additional user-provided operator in std::pair the ambiguity is avoided.
    
    Backport from mainline
    2018-07-31  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/86751
    	* include/bits/stl_pair.h (__pair_base): New class with deleted copy
    	assignment operator.
    	(pair): Derive from __pair_base.
    	(pair::operator=): Remove deleted overload.
    	* python/libstdcxx/v6/printers.py (StdPairPrinter): New pretty printer
    	so that new base class isn't shown in GDB.
    	* testsuite/20_util/pair/86751.cc: New test.
    	* testsuite/20_util/pair/ref_assign.cc: New test.
    
    From-SVN: r265096

---
+++ libstdc++-v3/testsuite/20_util/pair/86751.cc
@@ -0,0 +1,33 @@
+// Copyright (C) 2018 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile { target c++11 } }
+
+#include <utility>
+
+struct X {
+  template<typename T> operator T() const;
+};
+
+
+void
+test01()
+{
+  std::pair<int, int> p;
+  X x;
+  p = x;  // PR libstdc++/86751
+}
