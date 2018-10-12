4e61dba8a38e13125daeba9af250f846df1bba11
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-10-12 10:50:15 +0000

    Initial commit of Networking TS implementation
    	* include/Makefile.am: Add new headers.
    	* include/Makefile.in: Regenerate.
    	* include/experimental/bits/net.h: New header for common
    	implementation details of Networking TS.
    	* include/experimental/buffer: New header.
    	* include/experimental/executor: New header.
    	* include/experimental/internet: New header.
    	* include/experimental/io_context: New header.
    	* include/experimental/net: New header.
    	* include/experimental/netfwd: New header.
    	* include/experimental/socket: New header.
    	* include/experimental/timer: New header.
    	* testsuite/experimental/net/buffer/arithmetic.cc: New test.
    	* testsuite/experimental/net/buffer/const.cc: New test.
    	* testsuite/experimental/net/buffer/creation.cc: New test.
    	* testsuite/experimental/net/buffer/mutable.cc: New test.
    	* testsuite/experimental/net/buffer/size.cc: New test.
    	* testsuite/experimental/net/buffer/traits.cc: New test.
    	* testsuite/experimental/net/execution_context/use_service.cc: New
    	test.
    	* testsuite/experimental/net/headers.cc: New test.
    	* testsuite/experimental/net/internet/address/v4/comparisons.cc: New
    	test.
    	* testsuite/experimental/net/internet/address/v4/cons.cc: New test.
    	* testsuite/experimental/net/internet/address/v4/creation.cc: New
    	test.
    	* testsuite/experimental/net/internet/address/v4/members.cc: New
    	test.
    	* testsuite/experimental/net/internet/resolver/base.cc: New test.
    	* testsuite/experimental/net/internet/resolver/ops/lookup.cc: New
    	test.
    	* testsuite/experimental/net/internet/resolver/ops/reverse.cc: New
    	test.
    	* testsuite/experimental/net/timer/waitable/cons.cc: New test.
    	* testsuite/experimental/net/timer/waitable/dest.cc: New test.
    	* testsuite/experimental/net/timer/waitable/ops.cc: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@265080 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/experimental/net/internet/resolver/base.cc
@@ -0,0 +1,61 @@
+// Copyright (C) 2015-2018 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++14" }
+
+#include <experimental/internet>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  bool test __attribute__((unused)) = false;
+
+  using resolver = std::experimental::net::ip::resolver_base;
+
+  resolver::flags f = resolver::passive;
+
+  VERIFY( (f & resolver::numeric_host) == 0);
+  f &= resolver::numeric_host;
+  VERIFY( f == 0 );
+
+  VERIFY( (f | resolver::numeric_host) == resolver::numeric_host);
+  f |= resolver::numeric_host;
+  VERIFY( f == resolver::numeric_host );
+
+  VERIFY( (f ^ resolver::numeric_host) == 0 );
+  f ^= resolver::numeric_host;
+  VERIFY( f == 0 );
+
+  f = ~resolver::numeric_host;
+  VERIFY( (f & resolver::numeric_host) == 0);
+  VERIFY( (f | resolver::numeric_host) == ~resolver::flags{} );
+
+  (void) resolver::passive;
+  (void) resolver::canonical_name;
+  (void) resolver::numeric_host;
+  (void) resolver::numeric_service;
+  (void) resolver::v4_mapped;
+  (void) resolver::all_matching;
+  (void) resolver::address_configured;
+}
+
+int
+main()
+{
+  test01();
+}
