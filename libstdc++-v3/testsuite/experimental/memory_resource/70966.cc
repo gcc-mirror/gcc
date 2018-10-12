b860a6a1fbcb21f581d8dcc5c0073a6015fad1a0
Jonathan Wakely <jwakely@redhat.com>
2018-10-12 14:04:24 +0100

    PR libstdc++/70966 make pmr::new_delete_resource() immortal
    Construct the program-wide resource objects using placement new. This
    means they have dynamic storage duration and won't be destroyed during
    termination.
    
    Backport from mainline
    2018-07-24  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/70966
    	* include/experimental/memory_resource (__get_default_resource): Use
    	placement new to create an object with dynamic storage duration.
    
    Backport from mainline
    2018-06-20  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/70966
    	* include/experimental/memory_resource (__resource_adaptor_imp): Add
    	static assertions to enforce requirements on pointer types.
    	(__resource_adaptor_imp::get_allocator()): Add noexcept.
    	(new_delete_resource, null_memory_resource): Return address of an
    	object with dynamic storage duration.
    	(__null_memory_resource): Remove.
    	* testsuite/experimental/memory_resource/70966.cc: New.
    
    From-SVN: r265097

---
+++ libstdc++-v3/testsuite/experimental/memory_resource/70966.cc
@@ -0,0 +1,56 @@
+// Copyright (C) 2018 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do run { target c++14 } }
+
+#include <experimental/memory_resource>
+
+namespace pmr = std::experimental::pmr;
+
+struct X
+{
+  pmr::memory_resource* res = nullptr;
+  void* ptr = nullptr;
+  static constexpr std::size_t n = 64;
+
+  constexpr X() { }
+
+  explicit
+  X(pmr::memory_resource* r) : res(r), ptr(r->allocate(n)) { }
+
+  ~X() { if (ptr) res->deallocate(ptr, n); }
+};
+
+void
+swap(X& lhs, X& rhs) {
+    std::swap(lhs.res, rhs.res);
+    std::swap(lhs.ptr, rhs.ptr);
+}
+
+void
+test01()
+{
+  static X x1;
+  X x2(pmr::new_delete_resource());
+  swap(x1, x2);
+  // Now x1 will deallocate the memory during destruction of static objects.
+}
+
+int main()
+{
+  test01();
+}
