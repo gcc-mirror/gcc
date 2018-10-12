82182d876aff148dfb1c1306902353ecd5e20ff9
Jonathan Wakely <jwakely@redhat.com>
2018-10-12 13:51:29 +0100

    Fix experimental::pmr typedefs and add tests
    The typedefs in <experimental/regex> and <experimental/string> don't
    need to be in the __cxx11 namespace, because they are only aliases and
    so will have the same mangled name as the underlying types.
    
    Backport from mainline
    2018-08-23  Jonathan Wakely  <jwakely@redhat.com>
    
    	* testsuite/21_strings/basic_string/init-list.cc:
    	Require cxx11-abi.
    	* testsuite/experimental/polymorphic_allocator/pmr_typedefs_match.cc:
    	Likewise.
    	* testsuite/experimental/polymorphic_allocator/pmr_typedefs_string.cc:
    	Likewise.
    
    Backport from mainline
    2018-08-22  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/87061
    	* include/experimental/regex [!_GLIBCXX_USE_CXX11_ABI]
    	(experimental::pmr::match_results, experimental::pmr::cmatch)
    	(experimental::pmr::smatch, experimental::pmr::wcmatch)
    	(experimental::pmr::wsmatch): Do not declare for gcc4-compatible ABI,
    	because COW strings don't support C++11 allocator model.
    	* include/experimental/string [!_GLIBCXX_USE_CXX11_ABI]
    	(experimental::pmr::basic_string, experimental::pmr::string)
    	(experimental::pmr::u16string, experimental::pmr::u32string)
    	(experimental::pmr::wstring): Likewise.
    
    Backport from mainline
    2018-08-15  Jonathan Wakely  <jwakely@redhat.com>
    
    	* include/experimental/regex: Remove begin/end macros for namespace.
    	* include/experimental/string: Likewise.
    	* testsuite/experimental/polymorphic_allocator/pmr_typedefs_deque.cc:
    	New test.
    	* testsuite/experimental/polymorphic_allocator/
    	pmr_typedefs_forward_list.cc: New test.
    	* testsuite/experimental/polymorphic_allocator/pmr_typedefs_list.cc:
    	New test.
    	* testsuite/experimental/polymorphic_allocator/pmr_typedefs_map.cc:
    	New test.
    	* testsuite/experimental/polymorphic_allocator/pmr_typedefs_match.cc:
    	New test.
    	* testsuite/experimental/polymorphic_allocator/
    	pmr_typedefs_multimap.cc: New test.
    	* testsuite/experimental/polymorphic_allocator/
    	pmr_typedefs_multiset.cc: New test.
    	* testsuite/experimental/polymorphic_allocator/pmr_typedefs_set.cc:
    	New test.
    	* testsuite/experimental/polymorphic_allocator/pmr_typedefs_string.cc:
    	New test.
    	* testsuite/experimental/polymorphic_allocator/
    	pmr_typedefs_unordered_map.cc: New test.
    	* testsuite/experimental/polymorphic_allocator/
    	pmr_typedefs_unordered_multimap.cc: New test.
    	* testsuite/experimental/polymorphic_allocator/
    	pmr_typedefs_unordered_multiset.cc: New test.
    	* testsuite/experimental/polymorphic_allocator/
    	pmr_typedefs_unordered_set.cc: New test.
    	* testsuite/experimental/polymorphic_allocator/pmr_typedefs_vector.cc:
    	New test.
    
    From-SVN: r265093

---
+++ libstdc++-v3/testsuite/experimental/polymorphic_allocator/pmr_typedefs_match.cc
@@ -0,0 +1,50 @@
+// Copyright (C) 2018 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile { target c++14 } }
+// { dg-require-effective-target cxx11-abi }
+
+#include <experimental/regex>
+
+namespace xpmr = std::experimental::pmr;
+
+struct X;
+static_assert(std::is_same<xpmr::match_results<X*>,
+    std::match_results<X*,
+      xpmr::polymorphic_allocator<std::sub_match<X*>>>>::value,
+    "pmr::match_results");
+
+static_assert(std::is_same<xpmr::cmatch,
+    std::match_results<const char*,
+      xpmr::polymorphic_allocator<std::sub_match<const char*>>>>::value,
+    "pmr::cmatch");
+static_assert(std::is_same<xpmr::smatch,
+    std::match_results<xpmr::string::const_iterator,
+      xpmr::polymorphic_allocator<
+	std::sub_match<xpmr::string::const_iterator>>>>::value,
+    "pmr::smatch");
+#ifdef _GLIBCXX_USE_WCHAR_T
+static_assert(std::is_same<xpmr::wcmatch,
+    std::match_results<const wchar_t*,
+      xpmr::polymorphic_allocator<std::sub_match<const wchar_t*>>>>::value,
+    "pmr::wcmatch");
+static_assert(std::is_same<xpmr::wsmatch,
+    std::match_results<xpmr::wstring::const_iterator,
+      xpmr::polymorphic_allocator<
+	std::sub_match<xpmr::wstring::const_iterator>>>>::value,
+    "pmr::wsmatch");
+#endif
---
+++ libstdc++-v3/testsuite/experimental/polymorphic_allocator/pmr_typedefs_unordered_multimap.cc
@@ -0,0 +1,40 @@
+// Copyright (C) 2018 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile { target c++14 } }
+
+#include <experimental/unordered_map>
+
+namespace xpmr = std::experimental::pmr;
+
+struct X { };
+struct Y { };
+struct Hash { std::size_t operator()(X) const { return 0; } };
+struct Eq { bool operator()(X, X) const { return true; } };
+
+static_assert(std::is_same<xpmr::unordered_multimap<X, Y>,
+    std::unordered_multimap<X, Y, std::hash<X>, std::equal_to<X>,
+      xpmr::polymorphic_allocator<std::pair<const X, Y>>>>::value,
+    "pmr::unordered_multimap");
+static_assert(std::is_same<xpmr::unordered_multimap<X, Y, Hash>,
+    std::unordered_multimap<X, Y, Hash, std::equal_to<X>,
+      xpmr::polymorphic_allocator<std::pair<const X, Y>>>>::value,
+    "pmr::unordered_multimap");
+static_assert(std::is_same<xpmr::unordered_multimap<X, Y, Hash, Eq>,
+    std::unordered_multimap<X, Y, Hash, Eq,
+      xpmr::polymorphic_allocator<std::pair<const X, Y>>>>::value,
+    "pmr::unordered_multimap");
