0ef5554b713cb8c792500db667e51e7ff188cdde
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-10-14 20:30:02 +0000

    i386: Also disable AVX512IFMA/AVX5124FMAPS/AVX5124VNNIW
    Also disable AVX512IFMA, AVX5124FMAPS and AVX5124VNNIW when disabling
    AVX512F.
    
    gcc/
    
    	PR target/87572
    	* common/config/i386/i386-common.c (OPTION_MASK_ISA_AVX512F_UNSET):
    	Add OPTION_MASK_ISA_AVX512IFMA_UNSET,
    	OPTION_MASK_ISA_AVX5124FMAPS_UNSET and
    	OPTION_MASK_ISA_AVX5124VNNIW_UNSET.
    
    gcc/testsuite/
    
    	PR target/87572
    	* gcc.target/i386/pr87572.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@265150 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr87572.c
@@ -0,0 +1,10 @@
+/* PR target/82483 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -mavx512ifma -mno-sse2 -w -Wno-psabi" } */
+
+typedef long long __m512i __attribute__((__vector_size__(64)));
+__m512i
+foo (__m512i c, __m512i d, __m512i e, int b)
+{
+  return __builtin_ia32_vpmadd52huq512_maskz (c, d, e, b); /* { dg-error "incompatible types" } */
+}
