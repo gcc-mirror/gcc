a22584b16a3776a9f0d8b4db1a3397797ca2767d
Jonathan Wakely <jwakely@redhat.com>
2018-10-15 15:03:53 +0100

    Adjust test to pass with latest glibc
    Glibc changed the it_IT locales to use thousands separators,
    invalidating this test. Use nl_NL instead, as Dutch only uses grouping
    for money not numbers.
    
    	* testsuite/22_locale/numpunct/members/char/3.cc: Adjust test to
    	account for change to glibc it_IT localedata (glibc bz#10797).
    
    From-SVN: r265168

--- libstdc++-v3/testsuite/22_locale/numpunct/members/char/3.cc
+++ libstdc++-v3/testsuite/22_locale/numpunct/members/char/3.cc
@@ -1,4 +1,4 @@
-// { dg-require-namedlocale "it_IT.ISO8859-15" }
+// { dg-require-namedlocale "nl_NL.ISO8859-15" }
 
 // 2001-01-24 Benjamin Kosnik  <bkoz@redhat.com>
 
@@ -30,12 +30,14 @@ void test02()
   
   bool test __attribute__((unused)) = true;
 
-  locale loc_it = locale(ISO_8859(15,it_IT));
+  // nl_NL chosen because it has no thousands separator (at this time).
+  locale loc_it = locale(ISO_8859(15,nl_NL));
 
   const numpunct<char>& nump_it = use_facet<numpunct<char> >(loc_it); 
 
   string g = nump_it.grouping();
 
+  // Ensure that grouping is empty for locales with empty thousands separator.
   VERIFY( g == "" );
 }
 
