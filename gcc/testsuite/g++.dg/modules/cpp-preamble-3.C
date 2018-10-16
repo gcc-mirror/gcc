1650e24fb5cc8b2365a0991abd45e5f7b051a8b2
Nathan Sidwell <nathan@gcc.gnu.org>
2018-10-16 21:16:57 +0000

    Uncommitted tests
    From-SVN: r265216

--- gcc/testsuite/g++.dg/modules/cpp-preamble-3.C
+++
@@ -1,9 +0,0 @@
-// { dg-do preprocess }
-// { dg-additional-options -fmodule-preamble }
-
-import x
-int i;
-int j;
-
-// { dg-final { scan-file cpp-preamble-3.i "import x\nint i;" } }
-// { dg-final { scan-file-not cpp-preamble-3.i "int j;" } }
