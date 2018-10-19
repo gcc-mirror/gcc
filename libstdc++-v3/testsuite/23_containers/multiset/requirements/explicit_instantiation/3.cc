f3245887551932de6aba1aa3e9f911b78a57b5ef
Jonathan Wakely <jwakely@redhat.com>
2018-10-19 22:50:15 +0100

    Skip tests for GNU extensions when testing with strict mode
    Tests for the implicit allocator rebinding extension will fail if the
    extension is disabled, so skip them.
    
    	* testsuite/23_containers/array/requirements/explicit_instantiation/
    	3.cc: Skip test when compiled with a -std=c++NN strict mode.
    	* testsuite/23_containers/deque/requirements/explicit_instantiation/
    	3.cc: Likewise.
    	* testsuite/23_containers/forward_list/requirements/
    	explicit_instantiation/3.cc: Likewise.
    	* testsuite/23_containers/list/requirements/explicit_instantiation/
    	3.cc: Likewise.
    	* testsuite/23_containers/map/requirements/explicit_instantiation/
    	3.cc: Likewise.
    	* testsuite/23_containers/multimap/requirements/explicit_instantiation/
    	3.cc: Likewise.
    	* testsuite/23_containers/multiset/requirements/explicit_instantiation/
    	3.cc: Likewise.
    	* testsuite/23_containers/set/requirements/explicit_instantiation/
    	3.cc: Likewise.
    	* testsuite/23_containers/unordered_map/requirements/
    	explicit_instantiation/3.cc: Likewise.
    	* testsuite/23_containers/unordered_multimap/requirements/
    	explicit_instantiation/3.cc: Likewise.
    	* testsuite/23_containers/unordered_multiset/requirements/
    	explicit_instantiation/3.cc: Likewise.
    	* testsuite/23_containers/unordered_set/requirements/
    	explicit_instantiation/3.cc: Likewise.
    	* testsuite/23_containers/vector/ext_pointer/explicit_instantiation/
    	3.cc: Likewise.
    	* testsuite/23_containers/vector/requirements/explicit_instantiation/
    	3.cc: Likewise.
    
    From-SVN: r265334

--- libstdc++-v3/testsuite/23_containers/multiset/requirements/explicit_instantiation/3.cc
+++ libstdc++-v3/testsuite/23_containers/multiset/requirements/explicit_instantiation/3.cc
@@ -15,12 +15,13 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-
 // This file tests explicit instantiation of library containers
+// with an allocator for a different value_type (which is a GNU extension).
 
 #include <set>
 
 // { dg-do compile }
+// { dg-skip-if "no extensions in strict dialects" { *-*-* } { "-std=c++*" } }
 
 // libstdc++/21770
 template class std::multiset<int, std::less<int>, std::allocator<char> >;
