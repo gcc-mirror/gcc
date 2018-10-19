3ad7843329e8b98965a30bab1239fb801db974fb
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-10-19 09:13:34 +0000

    i386: Enable AVX512 memory broadcast for FP add
    Many AVX512 vector operations can broadcast from a scalar memory source.
    This patch enables memory broadcast for FP add operations.
    
    gcc/
    
    	PR target/72782
    	* config/i386/sse.md
    	(*<plusminus_insn><mode>3<mask_name>_bcst_1): New.
    	(*add<mode>3<mask_name>_bcst_2): Likewise.
    
    gcc/testsuite/
    
    	PR target/72782
    	* gcc.target/i386/avx512-binop-1.h: New file.
    	* gcc.target/i386/avx512-binop-2.h: Likewise.
    	* gcc.target/i386/avx512-binop-3.h: Likewise.
    	* gcc.target/i386/avx512-binop-4.h: Likewise.
    	* gcc.target/i386/avx512-binop-5.h: Likewise.
    	* gcc.target/i386/avx512-binop-6.h: Likewise.
    	* gcc.target/i386/avx512f-add-df-zmm-1.c: Likewise.
    	* gcc.target/i386/avx512f-add-sf-zmm-1.c: Likewise.
    	* gcc.target/i386/avx512f-add-sf-zmm-2.c: Likewise.
    	* gcc.target/i386/avx512f-add-sf-zmm-3.c: Likewise.
    	* gcc.target/i386/avx512f-add-sf-zmm-4.c: Likewise.
    	* gcc.target/i386/avx512f-add-sf-zmm-5.c: Likewise.
    	* gcc.target/i386/avx512f-add-sf-zmm-6.c: Likewise.
    	* gcc.target/i386/avx512f-sub-df-zmm-1.c: Likewise.
    	* gcc.target/i386/avx512f-sub-sf-zmm-1.c: Likewise.
    	* gcc.target/i386/avx512f-sub-sf-zmm-2.c: Likewise.
    	* gcc.target/i386/avx512f-sub-sf-zmm-3.c: Likewise.
    	* gcc.target/i386/avx512f-sub-sf-zmm-4.c: Likewise.
    	* gcc.target/i386/avx512f-sub-sf-zmm-5.c: Likewise.
    	* gcc.target/i386/avx512vl-add-sf-xmm-1.c: Likewise.
    	* gcc.target/i386/avx512vl-add-sf-ymm-1.c: Likewise.
    	* gcc.target/i386/avx512vl-sub-sf-xmm-1.c: Likewise.
    	* gcc.target/i386/avx512vl-sub-sf-ymm-1.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@265311 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/avx512f-add-df-zmm-1.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-options "-mavx512f -O2" } */
+/* { dg-final { scan-assembler-times "vaddpd\[ \\t\]+\\(%(?:eax|rdi|edi)\\)\\\{1to\[1-8\]+\\\}, %zmm\[0-9\]+, %zmm0" 1 } } */
+/* { dg-final { scan-assembler-not "vbroadcastsd\[^\n\]*%zmm\[0-9\]+" } } */
+
+#define type __m512d
+#define vec 512
+#define op add
+#define suffix pd
+#define SCALAR double
+
+#include "avx512-binop-1.h"
