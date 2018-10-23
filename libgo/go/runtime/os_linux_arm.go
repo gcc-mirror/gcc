8dd2ae4bc732f0587fb16b8bb7a5518e7f2ed8cb
Ian Lance Taylor <ian@gcc.gnu.org>
2018-10-23 19:02:29 +0000

    re PR go/87661 (libgo bootstrap failure on arm-linux-gnueabihf (redefinition of constants))
    	PR go/87661
        runtime: remove unused armArch, hwcap and hardDiv
        
        After CL 140057 these are only written but never read in gccgo.
        
        Reviewed-on: https://go-review.googlesource.com/c/141077
    
    From-SVN: r265439

--- libgo/go/runtime/os_linux_arm.go
+++ libgo/go/runtime/os_linux_arm.go
@@ -4,20 +4,7 @@
 
 package runtime
 
-import "unsafe"
-
-const (
-	_AT_PLATFORM = 15 //  introduced in at least 2.6.11
-
-	_HWCAP_VFP   = 1 << 6  // introduced in at least 2.6.11
-	_HWCAP_VFPv3 = 1 << 13 // introduced in 2.6.30
-	_HWCAP_IDIVA = 1 << 17
-)
-
 var randomNumber uint32
-var armArch uint8 = 6 // we default to ARMv6
-var hwcap uint32      // set by archauxv
-var hardDiv bool      // set if a hardware divider is available
 
 func archauxv(tag, val uintptr) {
 	switch tag {
@@ -27,15 +14,5 @@ func archauxv(tag, val uintptr) {
 		// it as a byte array.
 		randomNumber = uint32(startupRandomData[4]) | uint32(startupRandomData[5])<<8 |
 			uint32(startupRandomData[6])<<16 | uint32(startupRandomData[7])<<24
-
-	case _AT_PLATFORM: // v5l, v6l, v7l
-		t := *(*uint8)(unsafe.Pointer(val + 1))
-		if '5' <= t && t <= '7' {
-			armArch = t - '0'
-		}
-
-	case _AT_HWCAP: // CPU capability bit flags
-		hwcap = uint32(val)
-		hardDiv = (hwcap & _HWCAP_IDIVA) != 0
 	}
 }
