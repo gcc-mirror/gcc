f5690665c0e18d1c9921e2f40e3cf8dc568f8088
pc <pc@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-10-26 19:02:23 +0000

    [rs6000] Add tests for compatible implementations of x86 SSSE3 intrinsics
    This is part 2/2 for contributing PPC64LE support for X86 SSSE3 instrisics.
    This patch includes testsuite/gcc.target tests, copied from gcc.target/i386,
    for the intrinsics defined in tmmintrin.h.
    
    [gcc/testsuite]
    
    2018-10-26  Paul A. Clarke  <pc@us.ibm.com>
    
    	* gcc.target/powerpc/ssse3-check.h: New file.
    	* gcc.target/powerpc/ssse3-vals.h: New file.
    	* gcc.target/powerpc/ssse3-pabsb.c: New file.
    	* gcc.target/powerpc/ssse3-pabsd.c: New file.
    	* gcc.target/powerpc/ssse3-pabsw.c: New file.
    	* gcc.target/powerpc/ssse3-palignr.c: New file.
    	* gcc.target/powerpc/ssse3-phaddd.c: New file.
    	* gcc.target/powerpc/ssse3-phaddsw.c: New file.
    	* gcc.target/powerpc/ssse3-phaddw.c: New file.
    	* gcc.target/powerpc/ssse3-phsubd.c: New file.
    	* gcc.target/powerpc/ssse3-phsubsw.c: New file.
    	* gcc.target/powerpc/ssse3-phsubw.c: New file.
    	* gcc.target/powerpc/ssse3-pmaddubsw.c: New file.
    	* gcc.target/powerpc/ssse3-pmulhrsw.c: New file.
    	* gcc.target/powerpc/ssse3-pshufb.c: New file.
    	* gcc.target/powerpc/ssse3-psignb.c: New file.
    	* gcc.target/powerpc/ssse3-psignd.c: New file.
    	* gcc.target/powerpc/ssse3-psignw.c: New file.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@265544 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/ssse3-pshufb.c
@@ -0,0 +1,114 @@
+/* { dg-do run } */
+/* { dg-options "-O3 -mpower8-vector -Wno-psabi" } */
+/* { dg-require-effective-target p8vector_hw } */
+
+#ifndef CHECK_H
+#define CHECK_H "ssse3-check.h"
+#endif
+
+#ifndef TEST
+#define TEST ssse3_test
+#endif
+
+#include CHECK_H
+
+#include "ssse3-vals.h"
+
+#include <tmmintrin.h>
+
+#ifndef __AVX__
+/* Test the 64-bit form */
+static void
+ssse3_test_pshufb (int *i1, int *i2, int *r)
+{
+  __m64 t1 = *(__m64 *) i1;
+  __m64 t2 = *(__m64 *) i2;
+  *(__m64 *)r = _mm_shuffle_pi8 (t1, t2);
+  _mm_empty ();
+}
+#endif
+
+/* Test the 128-bit form */
+static void
+ssse3_test_pshufb128 (int *i1, int *i2, int *r)
+{
+  /* Assumes incoming pointers are 16-byte aligned */
+  __m128i t1 = *(__m128i *) i1;
+  __m128i t2 = *(__m128i *) i2;
+  *(__m128i *)r = _mm_shuffle_epi8 (t1, t2);
+}
+
+#ifndef __AVX__
+/* Routine to manually compute the results */
+static void
+compute_correct_result_64 (int *i1, int *i2, int *r)
+{
+  char *b1 = (char *) i1;
+  char *b2 = (char *) i2;
+  char *bout = (char *) r;
+  int i;
+  char select;
+
+  for (i = 0; i < 16; i++)
+    {
+      select = b2[i];
+      if (select & 0x80)
+	bout[i] = 0;
+      else if (i < 8)
+	bout[i] = b1[select & 0x7];
+      else
+	bout[i] = b1[8 + (select & 0x7)];
+    }
+}
+#endif
+
+static void
+compute_correct_result_128 (int *i1, int *i2, int *r)
+{
+  char *b1 = (char *) i1;
+  char *b2 = (char *) i2;
+  char *bout = (char *) r;
+  int i;
+  char select;
+
+  for (i = 0; i < 16; i++)
+    {
+      select = b2[i];
+      if (select & 0x80)
+	bout[i] = 0;
+      else
+	bout[i] = b1[select & 0xf];
+    }
+}
+
+static void
+TEST (void)
+{
+  int i;
+  int r [4] __attribute__ ((aligned(16)));
+  int ck [4];
+  int fail = 0;
+
+  for (i = 0; i < 256; i += 8)
+    {
+#ifndef __AVX__
+      /* Manually compute the result */
+      compute_correct_result_64 (&vals[i + 0], &vals[i + 4], ck);
+
+      /* Run the 64-bit tests */
+      ssse3_test_pshufb (&vals[i + 0], &vals[i + 4], &r[0]);
+      ssse3_test_pshufb (&vals[i + 2], &vals[i + 6], &r[2]);
+      fail += chk_128 (ck, r);
+#endif
+
+      /* Recompute the result for 128-bits */
+      compute_correct_result_128 (&vals[i + 0], &vals[i + 4], ck);
+
+      /* Run the 128-bit tests */
+      ssse3_test_pshufb128 (&vals[i + 0], &vals[i + 4], r);
+      fail += chk_128 (ck, r);
+    }
+
+  if (fail != 0)
+    abort ();
+}
