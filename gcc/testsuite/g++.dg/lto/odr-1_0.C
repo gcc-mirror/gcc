d2566ab1fd60a854c392b1d138b8aa72dfa9885b
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-10-26 07:19:07 +0000

    2018-10-26  Richard Biener  <rguenther@suse.de>
    	PR testsuite/87754
    	* g++.dg/lto/odr-1_0.C: Fix pattern.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@265518 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/lto/odr-1_0.C
+++ gcc/testsuite/g++.dg/lto/odr-1_0.C
@@ -3,6 +3,6 @@
 struct a { // { dg-lto-warning "8: type 'struct a' violates the C\\+\\+ One Definition Rule" }
   struct b *ptr; // { dg-lto-message "13: the first difference of corresponding definitions is field 'ptr'" }
 };
-void test(struct a *) // { dg-lto-warning "6: warning: 'test' violates the C++ One Definition Rule" }
+void test(struct a *) // { dg-lto-warning "6: warning: 'test' violates the C\\+\\+ One Definition Rule" }
 {
 }
