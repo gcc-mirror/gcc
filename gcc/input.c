gcc-mirror/gcc

commit 05d57d6561dc3c10cd777a5c783833b2c2e37074

Fix ICE in substring-handling building 502.gcc_r (PR 87562)
In r264887 I broke the build of 502.gcc_r due to an ICE.
The ICE occurs when generating a location for an sprintf warning within
a string literal, where the sprintf call is in a macro.

The root cause is a bug in the original commit of substring locations
(r239175).  get_substring_ranges_for_loc has code to handle the case
where the string literal is in a very long source line that exceeds the
length that the current linemap can represent: the start of the token
is in one line map, but then another line map is started, and the end
of the token is in the new linemap.  get_substring_ranges_for_loc handles
this by using the linemap of the end-point when building location_t
values within the string.  When extracting the linemap for the endpoint
in r239175 I erroneously used LRK_MACRO_EXPANSION_POINT, which should
have instead been LRK_SPELLING_LOCATION.

I believe this bug was dormant due to rejecting macro locations earlier
in the function, but in r264887 I allowed some macro locations in order
to deal with locations coming from the C++ lexer, and this uncovered
the bug: if a string literal was defined in a macro, locations within
the string literal would be looked up using the linemap of the expansion
point of the macro, rather than of the spelling point.  This would lead
to garbage location_t values, and, depending on the precise line numbers
of the two locations, an assertion failure (which was causing the build
failure in 502.gcc_r).

This patch fixes the bug by using LRK_SPELLING_LOCATION, and adds some
bulletproofing to the "two linemaps" case.

Successfully bootstrapped & regrtested on x86_64-pc-linux-gnu
(g++.sum gained 5 PASS results; gcc.sum gained 3 PASS results).
I also verified that this fixes the build of 502.gcc_r.

gcc/ChangeLog:
	PR tree-optimization/87562
	* input.c (get_substring_ranges_for_loc): Use
	LRK_SPELLING_LOCATION rather than LRK_MACRO_EXPANSION_POINT when

gcc/input.c
@@ -1457,9 +1457,17 @@ get_substring_ranges_for_loc (cpp_reader *pfile,
 	 halfway through the token.
 	 Ensure that the loc_reader uses the linemap of the
 	 *end* of the token for its start location.  */
+      const line_map_ordinary *start_ord_map;
+      linemap_resolve_location (line_table, src_range.m_start,
+				LRK_SPELLING_LOCATION, &start_ord_map);
       const line_map_ordinary *final_ord_map;
       linemap_resolve_location (line_table, src_range.m_finish,
-				LRK_MACRO_EXPANSION_POINT, &final_ord_map);
+				LRK_SPELLING_LOCATION, &final_ord_map);
+      /* Bulletproofing.  We ought to only have different ordinary maps
+	 for start vs finish due to line-length jumps.  */
+      if (start_ord_map != final_ord_map
+	  && start_ord_map->to_file != final_ord_map->to_file)
+	  return "start and finish are spelled in different ordinary maps";
       location_t start_loc
 	= linemap_position_for_line_and_column (line_table, final_ord_map,
 						start.line, start.column);

gcc/testsuite/gcc.dg/plugin/diagnostic-test-string-literals-1.c
@@ -296,3 +296,26 @@ test_backslash_continued_logical_lines (void)
  ~~~~~~                          
    { dg-end-multiline-output "" } */
 }
+
+/* Reproducer for PR 87652; this is whitespace-sensitive.  */
+
+#include "pr87562-a.h"
+
+
+
+
+#include "pr87562-b.h"
+
+void
+pr87652 (const char *stem, int counter)
+{
+  char label[100];
+  ASM_GENERATE_INTERNAL_LABEL (label, stem, counter);
+
+  /* This warning is actually in "pr87562-a.h".  */
+  /* { dg-warning "39: range" "" { target *-*-* } 5 } */
+  /* { dg-begin-multiline-output "" }
+       __emit_string_literal_range ("*.%s%u", 2, 2, 3); \
+                                       ^~
+     { dg-end-multiline-output "" } */
+}

gcc/testsuite/c-c++-common/substring-location-PR-87562-1-a.h
@@ -1,0 +1,7 @@
+#undef  ASM_GENERATE_INTERNAL_LABEL
+#define ASM_GENERATE_INTERNAL_LABEL(LABEL, PREFIX, NUM)		\
+  do								\
+    {								\
+      __builtin_sprintf (LABEL, "*.%s%u", PREFIX, (unsigned) (NUM));	\
+    }								\
+  while (0)

gcc/testsuite/c-c++-common/substring-location-PR-87562-1.c
@@ -1,0 +1,15 @@
+/* Reproducing this ICE is dependent on line numbering, hence the blank
+   lines below.  */
+#include "substring-location-PR-87562-1-a.h"
+
+
+
+
+#include "substring-location-PR-87562-1-b.h"
+
+void
+dbxout_stab_value_internal_label (const char *stem, int counter)
+{
+  char label[100];
+  ASM_GENERATE_INTERNAL_LABEL (label, stem, counter);
+}

gcc/testsuite/gcc.dg/plugin/pr87562-a.h
@@ -1,0 +1,7 @@
+#undef  ASM_GENERATE_INTERNAL_LABEL
+#define ASM_GENERATE_INTERNAL_LABEL(LABEL, PREFIX, NUM)		\
+  do								\
+    {								\
+      __emit_string_literal_range ("*.%s%u", 2, 2, 3); \
+    }								\
+  while (0)

commit 3d0a5393466ef5d2755dd0dc8e8c10750e9bb4b9

Fix ICE in get_substring_ranges_for_loc on __FILE__ (PR c++/87721)
PR c++/87721 reports a crash in get_substring_ranges_for_loc introduced
by r265271, my fix for PR 87562.

The new issue occurs when attempting to get a location with a string
literal inside a macro in which the first token is __FILE__ (formed via
concatenation).  Attempting to get the spelling location of __FILE__
fails, leading to NULL for start_ord_map and final_ord_map, and thus
a NULL pointer dereference.

Given that our "on-demand" substring locations approach reparses the
string literals, there isn't a good way to access the locations inside
such string literals: attempting to reparse __FILE__ fails with a
"missing open quote".

This patch applies the easy fix by gracefully rejecting the case where
the spelling locations for the start or finish give us NULL maps.

gcc/ChangeLog:
	PR c++/87721
	* input.c (get_substring_ranges_for_loc): Detect if
	linemap_resolve_location gives us a NULL map, and reject
	this case.

gcc/testsuite/ChangeLog:
	PR c++/87721
	* c-c++-common/substring-location-PR-87721.c: New test.
	* gcc.dg/plugin/diagnostic-test-string-literals-1.c: Add test for
	PR 87721.
	* gcc.dg/plugin/diagnostic_plugin_test_string_literals.c
	(test_string_literals): Fold the index arguments before checking
	for INTEGER_CST.

From-SVN: r265611

gcc/input.c
@@ -1463,6 +1463,8 @@ get_substring_ranges_for_loc (cpp_reader *pfile,
       const line_map_ordinary *final_ord_map;
       linemap_resolve_location (line_table, src_range.m_finish,
 				LRK_SPELLING_LOCATION, &final_ord_map);
+      if (start_ord_map == NULL || final_ord_map == NULL)
+	return "failed to get ordinary maps";
       /* Bulletproofing.  We ought to only have different ordinary maps
 	 for start vs finish due to line-length jumps.  */
       if (start_ord_map != final_ord_map

gcc/testsuite/c-c++-common/substring-location-PR-87721.c
@@ -1,0 +1,11 @@
+# define DBG_ERROR(dbg_logger, format, args...) if (1){\
+  char dbg_buffer[256]; \
+  __builtin_snprintf(dbg_buffer, sizeof(dbg_buffer)-1,\
+  __FILE__":%5d: " format  , __LINE__ , ## args); \
+};
+
+void testPasswordStore1(int argc, char **argv) {
+  const char *pw1="Secret1";
+  char pw[256];
+  DBG_ERROR(0, "Bad password, expected [%s], got [%s].", pw1, pw);
+}

gcc/testsuite/gcc.dg/plugin/diagnostic-test-string-literals-1.c
@@ -319,3 +319,33 @@ pr87652 (const char *stem, int counter)
                                        ^~
      { dg-end-multiline-output "" } */
 }
+
+/* Reproducer for PR 87721.  */
+
+# define OFFSET __builtin_strlen (__FILE__) + __builtin_strlen(":%5d: ")
+
+# define DBG_ERROR(format, caret_idx, start_idx, end_idx)	\
+  do {								\
+    __emit_string_literal_range(__FILE__":%5d: " format,	\
+				OFFSET + caret_idx,		\
+				OFFSET + start_idx,		\
+				OFFSET + end_idx);		\
+  } while (0)
+
+/* { dg-error "unable to read substring location: failed to get ordinary maps" "" { target *-*-* } 329 } */
+/* { dg-begin-multiline-output "" }
+     __emit_string_literal_range(__FILE__":%5d: " format, \
+                                 ^~~~~~~~
+     { dg-end-multiline-output "" { target c } } */
+/* { dg-begin-multiline-output "" }
+     __emit_string_literal_range(__FILE__":%5d: " format, \
+                                 ^
+     { dg-end-multiline-output "" { target c++ } } */
+
+void pr87721 (void) {
+  DBG_ERROR("Bad password, expected [%s], got [%s].", 24, 24, 25); /* { dg-message "in expansion of macro 'DBG_ERROR'" } */
+  /* { dg-begin-multiline-output "" }
+   DBG_ERROR("Bad password, expected [%s], got [%s].", 24, 24, 25);
+   ^~~~~~~~~
+     { dg-end-multiline-output "" } */
+}

gcc/testsuite/gcc.dg/plugin/diagnostic_plugin_test_string_literals.c
@@ -140,7 +140,7 @@ test_string_literals (gimple *stmt)
       return;
     }
 
-  tree t_caret_idx = gimple_call_arg (call, 1);
+  tree t_caret_idx = fold (gimple_call_arg (call, 1));
   if (TREE_CODE (t_caret_idx) != INTEGER_CST)
     {
       error_at (call->location, "integer constant required for arg 2");
@@ -148,7 +148,7 @@ test_string_literals (gimple *stmt)
     }
   int caret_idx = TREE_INT_CST_LOW (t_caret_idx);
 
-  tree t_start_idx = gimple_call_arg (call, 2);
+  tree t_start_idx = fold (gimple_call_arg (call, 2));
   if (TREE_CODE (t_start_idx) != INTEGER_CST)
     {
       error_at (call->location, "integer constant required for arg 3");
@@ -156,7 +156,7 @@ test_string_literals (gimple *stmt)
     }
   int start_idx = TREE_INT_CST_LOW (t_start_idx);
 
-  tree t_end_idx = gimple_call_arg (call, 3);
+  tree t_end_idx = fold (gimple_call_arg (call, 3));
   if (TREE_CODE (t_end_idx) != INTEGER_CST)
     {
       error_at (call->location, "integer constant required for arg 4");