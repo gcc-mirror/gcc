5c316bcae5d42d7ce10ec4de47e19ddb4e83e227
villemoes <villemoes@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-10-30 08:33:04 +0000

    libgcc: properly destroy mutexes on VxWorks
    Just as one needs run-time initialization of mutexes, one needs to
    destroy them properly to allow the OS to release resources associated
    with the semaphore.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@265616 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgcc/config/gthr-vxworks.h
+++ libgcc/config/gthr-vxworks.h
@@ -63,8 +63,9 @@ __gthread_mutex_init_function (__gthread_mutex_t *mutex)
 }
 
 static inline int
-__gthread_mutex_destroy (__gthread_mutex_t * UNUSED(mutex))
+__gthread_mutex_destroy (__gthread_mutex_t *mutex)
 {
+  semDelete(*mutex);
   return 0;
 }
 
