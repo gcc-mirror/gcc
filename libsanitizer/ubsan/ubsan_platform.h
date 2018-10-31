eac975312214dccb9d425b3e8d0b93e85c11ddf4
Martin Liska <mliska@suse.cz>
2018-10-31 12:14:23 +0100

    backport: All source files: Merge from upstream 345033.
    Merge from upstream 345033.
    
    2018-10-31  Martin Liska  <mliska@suse.cz>
    
    	* All source files: Merge from upstream 345033.
    
    From-SVN: r265665

--- libsanitizer/ubsan/ubsan_platform.h
+++ libsanitizer/ubsan/ubsan_platform.h
@@ -11,19 +11,14 @@
 #ifndef UBSAN_PLATFORM_H
 #define UBSAN_PLATFORM_H
 
-#ifndef CAN_SANITIZE_UB
 // Other platforms should be easy to add, and probably work as-is.
-#if (defined(__linux__) || defined(__FreeBSD__) || defined(__APPLE__) ||       \
-     defined(__NetBSD__)) &&                                                   \
-    (defined(__x86_64__) || defined(__i386__) || defined(__arm__) ||           \
-     defined(__aarch64__) || defined(__mips__) || defined(__powerpc64__) ||    \
-     defined(__s390__))
-# define CAN_SANITIZE_UB 1
-#elif defined(_WIN32) || defined(__Fuchsia__)
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__APPLE__) ||        \
+    defined(__NetBSD__) || defined(__OpenBSD__) || \
+    (defined(__sun__) && defined(__svr4__)) || \
+    defined(_WIN32) || defined(__Fuchsia__) || defined(__rtems__)
 # define CAN_SANITIZE_UB 1
 #else
 # define CAN_SANITIZE_UB 0
 #endif
-#endif //CAN_SANITIZE_UB
 
 #endif
