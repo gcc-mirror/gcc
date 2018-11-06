521c7eaf2b7b8c9726609f57a40b90fe3796fd07
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-11-06 15:08:46 +0000

    Fix D compilation on Solaris
    	* config/default-d.c: Include memmodel.h.
    
    	* config/sol2-d.c: New file.
    	* config/t-sol2 (sol2-d.o): New rule.
    	* config.gcc <*-*-solaris2*>: Set d_target_objs,
    	target_has_targetdm.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@265846 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sol2-d.c
+++ gcc/config/sol2-d.c
@@ -1,5 +1,5 @@
-/* Default D language target hooks initializer.
-   Copyright (C) 2017-2018 Free Software Foundation, Inc.
+/* Solaris support needed only by D front-end.
+   Copyright (C) 2018 Free Software Foundation, Inc.
 
 GCC is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free
@@ -18,9 +18,34 @@ along with GCC; see the file COPYING3.  If not see
 #include "config.h"
 #include "system.h"
 #include "coretypes.h"
+#include "tm.h"
 #include "memmodel.h"
-#include "tm_d.h"
+#include "tm_p.h"
 #include "d/d-target.h"
 #include "d/d-target-def.h"
+
+/* Implement TARGET_D_OS_VERSIONS for Solaris targets.  */
+
+static void
+solaris_d_os_builtins (void)
+{
+  d_add_builtin_version ("Posix");
+  d_add_builtin_version ("Solaris");			\
+}
+
+/* Implement TARGET_D_CRITSEC_SIZE for Solaris targets.  */
+
+static unsigned
+solaris_d_critsec_size (void)
+{
+  /* This is the sizeof pthread_mutex_t.  */
+  return 24;
+}
+
+#undef TARGET_D_OS_VERSIONS
+#define TARGET_D_OS_VERSIONS solaris_d_os_builtins
+
+#undef TARGET_D_CRITSEC_SIZE
+#define TARGET_D_CRITSEC_SIZE solaris_d_critsec_size
 
 struct gcc_targetdm targetdm = TARGETDM_INITIALIZER;
