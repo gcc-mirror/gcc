d0cb40577fb3a4cbdadd96c5454d1cfdfb64b518
Jozef Lawrynowicz <jozef.l@mittosystems.com>
2018-11-08 16:35:21 +0000

    attr-alloc_size-10.c: Fix calculation of USHRT_MAX to prevent integer overflow when...
    2018-11-08  Jozef Lawrynowicz  <jozef.l@mittosystems.com>
    
    	* gcc.dg/attr-alloc_size-10.c: Fix calculation of USHRT_MAX to prevent
    	integer overflow when sizeof(short) == sizeof(int).
    	* gcc.dg/attr-alloc_size-11.c: Likewise.
    	* gcc.dg/attr-alloc_size-3.c: Likewise.
    	* gcc.dg/attr-alloc_size-5.c: Likewise.
    	* gcc.dg/builtin-stringop-chk-7.c: Likewise.
    
    From-SVN: r265928

--- gcc/testsuite/gcc.dg/attr-alloc_size-3.c
+++ gcc/testsuite/gcc.dg/attr-alloc_size-3.c
@@ -14,7 +14,7 @@
 
 #define SHRT_MAX   __SHRT_MAX__
 #define SHRT_MIN   (-SHRT_MAX - 1)
-#define USHRT_MAX  (SHRT_MAX * 2 + 1)
+#define USHRT_MAX  (SHRT_MAX * 2U + 1)
 
 #define INT_MAX    __INT_MAX__
 #define INT_MIN    (-INT_MAX - 1)
