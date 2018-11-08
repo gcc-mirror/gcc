52bc4c0afbb8852165635cb67a7d3948d081f12f
Eric Botcazou <ebotcazou@adacore.com>
2018-11-08 15:56:57 +0000

    fe.h (Suppress_Checks): Declare.
    	* fe.h (Suppress_Checks): Declare.
    	* gcc-interface/misc.c (gnat_init_gcc_eh): Set -fnon-call-exceptions
    	only if checks are not suppressed and -faggressive-loop-optimizations
    	only if they are.
    	* gcc-interface/trans.c (struct loop_info_d): Remove has_checks and
    	warned_aggressive_loop_optimizations fields.
    	(gigi): Do not clear warn_aggressive_loop_optimizations here.
    	(Raise_Error_to_gnu): Do not set has_checks.
    	(gnat_to_gnu) <N_Indexed_Component>: Remove support for aggressive
    	loop optimizations.
    
    From-SVN: r265922

--- gcc/testsuite/gnat.dg/null_pointer_deref3.adb
+++ gcc/testsuite/gnat.dg/null_pointer_deref3.adb
@@ -1,11 +1,12 @@
 -- { dg-do run }
--- { dg-options "-O -gnatp" }
 
 -- This test requires architecture- and OS-specific support code for unwinding
 -- through signal frames (typically located in *-unwind.h) to pass.  Feel free
 -- to disable it if this code hasn't been implemented yet.
 
 procedure Null_Pointer_Deref3 is
+
+   pragma Suppress (All_Checks);
 
    procedure Leaf is
       type Int_Ptr is access all Integer;
