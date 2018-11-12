e18240ffe2b48af6e77018e5f4e797c4b57cae72
Martin Liska <mliska@suse.cz>
2018-11-12 22:01:38 +0100

    Instrument only selected files (PR gcov-profile/87442).
    2018-11-12  Martin Liska  <mliska@suse.cz>
    
    	PR gcov-profile/87442
    	* common.opt: Add -fprofile-filter-files and -fprofile-exclude-files
    	options.
    	* doc/invoke.texi: Document them.
    	* tree-profile.c (parse_profile_filter): New.
    	(parse_profile_file_filtering): Likewise.
    	(release_profile_file_filtering): Likewise.
    	(include_source_file_for_profile): Likewise.
    	(tree_profiling): Filter source files based on the
    	newly added options.
    2018-11-12  Martin Liska  <mliska@suse.cz>
    
    	PR gcov-profile/87442
    	* gcc.dg/profile-filtering-1.c: New test.
    	* gcc.dg/profile-filtering-2.c: New test.
    
    From-SVN: r266037

---
+++ gcc/testsuite/gcc.dg/profile-filtering-1.c
@@ -0,0 +1,37 @@
+/* { dg-require-profiling "-fprofile-generate" } */
+/* { dg-options "-O2 -fprofile-generate -fprofile-filter-files=.\*filtering-1.c -fdump-tree-optimized" } */
+
+extern void abort (void);
+
+int *p1;
+int *p2;
+int *p3;
+
+int ga = 100;
+
+int
+sub (int i, int j)
+{
+  int k;
+  int l;
+  int m;
+  int n;
+  p1 = &k;
+  p2 = &l;
+  p3 = &m;
+  k = 20;
+  l = 30;
+  m = 40;
+  n = i / j;
+  return n + ga;
+}
+
+int
+main(void)
+{
+  if (sub (99, 33) != 103)
+    abort ();
+  return 0;
+}
+
+/* { dg-final { scan-tree-dump "PROF_edge" "optimized" } } */
