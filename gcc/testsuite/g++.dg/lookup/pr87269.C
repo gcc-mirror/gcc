8808260dfd1f7f25e7464bcf8006235677830672
Jakub Jelinek <jakub@redhat.com>
2018-11-17 00:18:13 +0100

    re PR c++/87269 (ICE in tsubst_copy, at cp/pt.c:15475 starting from r261802)
    	PR c++/87269
    	* g++.dg/lookup/pr87269.C (std::size_t): New typedef.
    	(operator"" _a) Change unsigned long type to std::size_t.
    
    From-SVN: r266225

--- gcc/testsuite/g++.dg/lookup/pr87269.C
+++ gcc/testsuite/g++.dg/lookup/pr87269.C
@@ -1,8 +1,12 @@
 // { dg-do compile { target c++11 } }
 // PR c++/87269 ICE failing to keep a lookup
 
+namespace std {
+  typedef decltype (sizeof (0)) size_t;
+}
+
 namespace {
-  void  operator"" _a (const char *, unsigned long) {}
+  void  operator"" _a (const char *, std::size_t) {}
 }
 
 void operator"" _a (unsigned long long);
