26b2df5d4253cc9871318cf5ef9d103d1ec84671
Iain Sandoe <iain@sandoe.co.uk>
2018-11-20 00:58:54 +0000

    c++coroutines Add a tree to placehold for the frame allocator.
    2018-11-20  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* gcc/cp/cp-tree.def (CORO_ALLOCATOR): New tree.
    	* gcc/cp/cp-objcp-common.c (cp_common_init_ts): Mark CORO_ALLOCATOR
    	as typed.
    
    From-SVN: r266294

--- gcc/cp/cp-objcp-common.c
+++ gcc/cp/cp-objcp-common.c
@@ -448,6 +448,7 @@ cp_common_init_ts (void)
   MARK_TS_TYPED (BINARY_RIGHT_FOLD_EXPR);
   MARK_TS_TYPED (OMP_DEPOBJ);
   MARK_TS_TYPED (COAWAIT_EXPR);
+  MARK_TS_TYPED (CORO_ALLOCATOR);
 }
 
 #include "gt-cp-cp-objcp-common.h"
