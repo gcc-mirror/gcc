4fd723f869c976c27d9140e36cd79df49032efc6
Jakub Jelinek <jakub@redhat.com>
2018-11-21 21:45:59 +0100

    re PR rtl-optimization/85925 (compilation of masking with 257 goes wrong in combine at -02)
    	PR rtl-optimization/85925
    	* gcc.c-torture/execute/20181120-1.c: Require effective target
    	int32plus.
    	(u): New variable.
    	(main): Compare d against u.f1 rather than 0x101.  Use 0x4030201
    	instead of 0x10101.
    
    From-SVN: r266357

--- gcc/testsuite/gcc.c-torture/execute/20181120-1.c
+++ gcc/testsuite/gcc.c-torture/execute/20181120-1.c
@@ -1,4 +1,5 @@
 /* PR rtl-optimization/85925 */
+/* { dg-require-effective-target int32plus } */
 /* Testcase by <sudi@gcc.gnu.org> */
 
 int a, c, d;
@@ -9,17 +10,18 @@ union U1 {
   unsigned f0;
   unsigned f1 : 15;
 };
+volatile union U1 u = { 0x4030201 };
 
 int main (void)
 {
   for (c = 0; c <= 1; c++) {
-    union U1 f = {0x10101};
+    union U1 f = {0x4030201};
     if (c == 1)
       b;
     *e = f.f1;
   }
 
-  if (d != 0x101)
+  if (d != u.f1)
     __builtin_abort ();
 
   return 0;
