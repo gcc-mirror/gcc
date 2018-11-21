693a707794506e4b5eb4f2f571fb77a292ec4a74
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-11-21 14:19:17 +0000

    2018-11-21  Richard Biener  <rguenther@suse.de>
    	PR bootstrap/88133
    	* bitmap.c (bitmap_last_set_bit): Refactor to avoid warning.
    	* Makefile.in (bitmap.o-warn): Remove again.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@266344 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/bitmap.c
+++ gcc/bitmap.c
@@ -1186,13 +1186,13 @@ bitmap_last_set_bit (const_bitmap a)
     elt = elt->next;
 
   bit_no = elt->indx * BITMAP_ELEMENT_ALL_BITS;
-  for (ix = BITMAP_ELEMENT_WORDS - 1; ix >= 0; ix--)
+  for (ix = BITMAP_ELEMENT_WORDS - 1; ix >= 1; ix--)
     {
       word = elt->bits[ix];
       if (word)
 	goto found_bit;
     }
-  gcc_unreachable ();
+  gcc_assert (elt->bits[ix] != 0);
  found_bit:
   bit_no += ix * BITMAP_WORD_BITS;
 #if GCC_VERSION >= 3004
