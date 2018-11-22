4cc2368f267f60d5e549b8a4856cd478a85fdd57
schwab <schwab@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-11-22 10:42:19 +0000

    * g++.dg/lto/odr-2_0.C: Remove extra brace
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@266373 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/lto/odr-2_0.C
+++ gcc/testsuite/g++.dg/lto/odr-2_0.C
@@ -1,5 +1,5 @@
 // { dg-lto-do link }
-// { dg-lto-options { { -O0 -flto }  } 
+// { dg-lto-options { -O0 -flto } }
 enum a {} b; // { dg-lto-warning "6: type 'a' violates the C\\+\\+ One Definition Rule" }
 int
 main(void)
