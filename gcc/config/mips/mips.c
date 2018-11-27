32b39b86aa196f39f1647b7ca786fe066f1b9d03
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-11-27 15:26:44 +0000

    * config/mips/mips.c (mips_block_move_straight): Use RETURN_BEGIN 	in call to move_by_pieces.
    2018-11-27  Tamar Christina  <tamar.christina@arm.com>
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@266512 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -8064,7 +8064,7 @@ mips_block_move_straight (rtx dest, rtx src, HOST_WIDE_INT length)
       src = adjust_address (src, BLKmode, offset);
       dest = adjust_address (dest, BLKmode, offset);
       move_by_pieces (dest, src, length - offset,
-		      MIN (MEM_ALIGN (src), MEM_ALIGN (dest)), 0);
+		      MIN (MEM_ALIGN (src), MEM_ALIGN (dest)), RETURN_BEGIN);
     }
 }
 
