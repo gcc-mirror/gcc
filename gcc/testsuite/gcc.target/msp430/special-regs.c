fb1ed2805ed1fc39c296b477a6424843124eb1cd
jozefl <jozefl@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-11-27 00:17:42 +0000

    2018-11-27  Jozef Lawrynowicz  <jozef.l@mittosystems.com>
    	Tweak directives in GCC tests for msp430-elf
    
    	gcc/ChangeLog:
    
    	* doc/sourcebuild.texi: Document check_effective_target_int_eq_float
    	and check_effective_target_ptr_eq_long.
    
    	gcc/testsuite/ChangeLog:
    
    	* lib/target-supports.exp
    	(check_effective_target_logical_op_short_circuit): Add msp430.
    	(check_effective_target_int_eq_float): New. 
    	(check_effective_target_ptr_eq_long): New. 
    	* c-c++-common/pr41779.c: Require int_eq_float for dg-warning tests.
    	* c-c++-common/pr57371-2.c: XFAIL optimized dump scan when
    	sizeof (float) != sizeof (int).
    	* gcc.dg/pr84670-4.c: Require ptr_eq_long.
    	* gcc.dg/pr85859.c: Likewise.
    	* gcc.dg/Wno-frame-address.c: Skip for msp430-elf.
    	* gcc.dg/torture/stackalign/builtin-apply-2.c: Likewise.
    	* gcc.dg/ifcvt-4.c: Likewise.
    	* gcc.dg/pr34856.c: Likewise.
    	* gcc.dg/builtin-apply2.c: Likewise.
    	* gcc.dg/tree-ssa/ssa-dse-26.c: Likewise.
    	* gcc.dg/attr-alloc_size-11.c: Remove dg-warning XFAIL for msp430.
    	* gcc.dg/tree-ssa/20040204-1.c: Likewise.
    	* gcc.dg/compat/struct-by-value-16a_x.c: Build at -O1 for msp430
    	so it fits.
    	* gcc.dg/lto/20091013-1_0.c: Require ptr_eq_long.
    	* gcc.dg/lto/20091013-1_1.c: Remove xfail-if for when
    	sizeof(void *) != sizeof(long).
    	* gcc.dg/lto/20091013-1_2.c: Likewise.
    	* gcc.dg/tree-ssa/loop-1.c: Fix expected dg-final behaviour for msp430.
    	* gcc.dg/tree-ssa/gen-vect-25.c: Likewise.
    	* gcc.dg/tree-ssa/gen-vect-11.c: Likewise.
    	* gcc.dg/tree-ssa/loop-35.c: Likewise.
    	* gcc.dg/tree-ssa/pr23455.c: Likewise.
    	* gcc.dg/weak/typeof-2.c: Likewise.
    	* gcc.target/msp430/interrupt_fn_placement.c: Skip for 430 ISA.
    	* gcc.target/msp430/pr78818-data-region.c: Fix scan-assembler text.
    	* gcc.target/msp430/pr79242.c: Don't skip for -msmall.
    	* gcc.target/msp430/special-regs.c: Use "__asm__" instead of "asm".
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@266500 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/msp430/special-regs.c
+++ gcc/testsuite/gcc.target/msp430/special-regs.c
@@ -7,10 +7,10 @@ int foo (void)
   register int cg1 __asm__("R2"); /* { dg-error "the register specified for 'cg1' is not general enough" } */
   register int cg2 __asm__("R3"); /* { dg-error "the register specified for 'cg2' is not general enough" } */
 
-  asm("" : "=r"(pc));
-  asm("" : "=r"(sp));
-  asm("" : "=r"(cg1));
-  asm("" : "=r"(cg2));
+  __asm__("" : "=r"(pc));
+  __asm__("" : "=r"(sp));
+  __asm__("" : "=r"(cg1));
+  __asm__("" : "=r"(cg2));
 
   return pc + sp + cg1 + cg2;
 }
