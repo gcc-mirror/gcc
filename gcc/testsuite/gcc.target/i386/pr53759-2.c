c10a94358fb8d0762c9eceb5d26125582fa7971f
Jakub Jelinek <jakub@redhat.com>
2018-12-02 21:43:49 +0100

    re PR target/88278 (Fails to elide zeroing of upper vector register)
    	PR target/88278
    	* config/i386/sse.md (*vec_concatv4sf_0, *vec_concatv4si_0): New insns.
    
    	* gcc.target/i386/pr88278.c: New test.
    	* gcc.target/i386/pr53759.c: Don't expect vmovlps insn, expect vmovq
    	instead.
    	* gcc.target/i386/pr53759-2.c: New test.
    
    From-SVN: r266728

--- gcc/testsuite/gcc.target/i386/pr53759-2.c
+++ gcc/testsuite/gcc.target/i386/pr53759-2.c
@@ -7,11 +7,10 @@
 void
 foo (__m128 *x, __m64 *y)
 {
-  __m128 a = _mm_setzero_ps ();
+  __m128 a = _mm_add_ps (x[1], x[2]);
   __m128 b = _mm_loadl_pi (a, y);
   *x = _mm_add_ps (b, b);
 }
 
-/* { dg-final { scan-assembler "vmovq\[ \\t\]" } } */
-/* { dg-final { scan-assembler-not "vmovlps\[ \\t\]" } } */
+/* { dg-final { scan-assembler "vmovlps\[ \\t\]" } } */
 /* { dg-final { scan-assembler-not "vshufps\[ \\t\]" } } */
