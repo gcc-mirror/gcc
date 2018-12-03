7ed760c79399c2d0a3406f28c6fd85f95bf6097c
Fritz Reese <fritzoreese@gmail.com>
2018-12-03 15:42:51 +0000

    re PR fortran/87919 (Incorrect fortran handling of -fno-* options)
    2018-12-03  Fritz Reese  <fritzoreese@gmail.com>
                Mark Eggleston <mark.eggleston@codethink.co.uk>
    
        PR fortran/87919
    
        gcc/fortran/ChangeLog:
    
    	PR fortran/87919
    	* options.c (SET_FLAG, SET_BITFLAG, SET_BITFLAG2): New macros.
    	(set_dec_flags): Set/unset DEC and std flags according to value.
    	(set_init_local_zero): New helper for -finit-local-zero flag group.
    	(gfc_init_options): Fix disabling of init flags, array temporaries
    	check, and dec flags when value is zero (from -fno-*).
    
        gcc/testsuite/ChangeLog:
    
    	PR fortran/87919
    	* gfortran.dg/array_temporaries_5.f90: New test.
    	* gfortran.dg/dec_bitwise_ops_3.f90: Ditto.
    	* gfortran.dg/dec_d_lines_3.f: Ditto.
    	* gfortran.dg/dec_exp_4.f90: Ditto.
    	* gfortran.dg/dec_exp_5.f90: Ditto.
    	* gfortran.dg/dec_io_7.f90: Ditto.
    	* gfortran.dg/dec_structure_24.f90: Ditto.
    	* gfortran.dg/dec_structure_25.f90: Ditto.
    	* gfortran.dg/dec_structure_26.f90: Ditto.
    	* gfortran.dg/dec_structure_27.f90: Ditto.
    	* gfortran.dg/dec_type_print_3.f90: Ditto.
    	* gfortran.dg/init_flag_20.f90: Ditto.
    
    
    Co-Authored-By: Mark Eggleston <mark.eggleston@codethink.co.uk>
    
    From-SVN: r266745

---
+++ gcc/testsuite/gfortran.dg/dec_type_print_3.f90
@@ -0,0 +1,21 @@
+! { dg-do compile }
+! { dg-options "-fdec -fno-dec" }
+!
+! PR fortran/87919
+!
+! Ensure that -fno-dec disables the usage of TYPE as an alias for PRINT.
+!
+
+include 'dec_type_print.f90'
+
+! { dg-error "Invalid character in name" "" { target *-*-* } 52 }
+! { dg-error "Invalid character in name" "" { target *-*-* } 53 }
+! { dg-error "Invalid character in name" "" { target *-*-* } 54 }
+! { dg-error "Invalid character in name" "" { target *-*-* } 55 }
+! { dg-error "Invalid character in name" "" { target *-*-* } 56 }
+! { dg-error "Invalid character in name" "" { target *-*-* } 57 }
+! { dg-error "Invalid character in name" "" { target *-*-* } 58 }
+! { dg-error "Unclassifiable statement" "" { target *-*-* } 59 }
+! { dg-error "conflicts with PROCEDURE" "" { target *-*-* } 60 }
+! { dg-error "Cannot assign to a named constant" "" { target *-*-* } 80 }
+
