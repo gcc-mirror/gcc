c0c5112bcd9bd1b0846a2fc5593339a8bd690bd8
Paul Koning <ni1d@arrl.net>
2018-12-05 09:25:11 -0500

    * udivmodhi4.c (__udivmodhi4): Fix loop end check.
    From-SVN: r266826

--- libgcc/udivmodhi4.c
+++ libgcc/udivmodhi4.c
@@ -27,7 +27,7 @@ __udivmodhi4(unsigned short num, unsigned short den, int modwanted)
   unsigned short bit = 1;
   unsigned short res = 0;
 
-  while (den < num && bit && !(den & (1L<<31)))
+  while (den < num && bit && !(den & (1U<<15)))
     {
       den <<=1;
       bit <<=1;
