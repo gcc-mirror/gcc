36839963adb6a7265b4c0bed338ac0ee2046cac0
pc <pc@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-12-06 22:14:55 +0000

    [rs6000] Enable x86-compat vector intrinsics testing
    The testsuite tests for the compatibility implementations of x86 vector
    intrinsics for "powerpc" had been inadvertently made to PASS
    without actually running the test code.
    
    This patch removes the code which kept the tests from running the actual
    test code.
    
    2018-12-06  Paul A. Clarke  <pc@us.ibm.com>
    
    [gcc/testsuite]
    
    	PR target/88316
    	* gcc.target/powerpc/bmi-check.h: Remove test for
    	__BUILTIN_CPU_SUPPORTS__, thereby enabling test code to run.
    	* gcc.target/powerpc/bmi2-check.h: Likewise.
    	* gcc.target/powerpc/mmx-check.h: Likewise.
    	* gcc.target/powerpc/sse-check.h: Likewise.
    	* gcc.target/powerpc/sse2-check.h: Likewise.
    	* gcc.target/powerpc/sse3-check.h: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@266870 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/bmi-check.h
+++ gcc/testsuite/gcc.target/powerpc/bmi-check.h
@@ -13,19 +13,9 @@ do_test (void)
 int
 main ()
 {
-#ifdef __BUILTIN_CPU_SUPPORTS__
-  /* Need 64-bit for 64-bit longs as single instruction.  */
-  if ( __builtin_cpu_supports ("ppc64") )
-    {
-      do_test ();
+  do_test ();
 #ifdef DEBUG
-      printf ("PASSED\n");
+  printf ("PASSED\n");
 #endif
-    }
-#ifdef DEBUG
-  else
-    printf ("SKIPPED\n");
-#endif
-#endif /* __BUILTIN_CPU_SUPPORTS__ */
   return 0;
 }
