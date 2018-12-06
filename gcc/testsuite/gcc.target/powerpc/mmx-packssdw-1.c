98e07d5c540a3e094c4e2e0377d530042d2df72f
Paul A. Clarke <pc@us.ibm.com>
2018-12-06 22:11:01 +0000

    [rs6000] Fix x86-compat vector intrinsics testcases for BE, 32bit
    Fix general endian issues found in the test cases for thecompatibility
    implementations of the x86 vector intrinsics.  (The tests had been
    inadvertently made to PASS without actually running the test code.
    A later patch fixes this issue.)
    
    Additionally, a new <smmintrin.h> is added, as some of the APIs therein are
    now used by the test cases.  It is _not_ a complete implementation of the
    SSE4 interfaces, only the few "extract" interfaces uses by the tests.
    
    2018-12-06  Paul A. Clarke  <pc@us.ibm.com>
    
    [gcc]
    
    	PR target/88316
    	* config/rs6000/smmintrin.h: New file.
    	* config.gcc: Add smmintrin.h to extra_headers for powerpc*-*-*.
    
    [gcc/testsuite]
    
    	PR target/88316
    	* gcc.target/powerpc/mmx-packssdw-1.c: Fixes for big-endian.
    	* gcc.target/powerpc/mmx-packsswb-1.c: Likewise.
    	* gcc.target/powerpc/mmx-packuswb-1.c: Likewise.
    	* gcc.target/powerpc/mmx-pmulhw-1.c: Likewise.
    	* gcc.target/powerpc/sse-cvtpi32x2ps-1.c: Likewise.
    	* gcc.target/powerpc/sse-cvtpu16ps-1.c: Likewise.
    	* gcc.target/powerpc/sse-cvtss2si-1.c: Likewise.
    	* gcc.target/powerpc/sse-cvtss2si-2.c: Likewise.
    	* gcc.target/powerpc/sse2-pshufhw-1.c: Likewise.
    	* gcc.target/powerpc/sse2-pshuflw-1.c: Likewise.
    
    From-SVN: r266869

--- gcc/testsuite/gcc.target/powerpc/mmx-packssdw-1.c
+++ gcc/testsuite/gcc.target/powerpc/mmx-packssdw-1.c
@@ -22,37 +22,50 @@ test (__m64 s1, __m64 s2)
   return _mm_packs_pi32 (s1, s2);
 }
 
+static short
+saturate (int val)
+{
+  if (val > 32767)
+    return 32767;
+  else if (val < -32768)
+    return -32768;
+  else
+    return val;
+}
+
+static inline int
+l_mm_extract_pi32 (__m64 b, int imm8)
+{
+  unsigned int shift = imm8 & 0x1;
+#ifdef __BIG_ENDIAN__
+  shift = 1 - shift;
+#endif
+  return ((long long)b >> (shift * 32)) & 0xffffffff;
+}
+
 static void
 TEST (void)
 {
   __m64_union s1, s2;
   __m64_union u;
   __m64_union e;
-  int i;
+  int start, end, inc;
 
   s1.as_m64 = _mm_set_pi32 (2134, -128);
   s2.as_m64 = _mm_set_pi32 (41124, 234);
   u.as_m64 = test (s1.as_m64, s2.as_m64);
 
-  for (i = 0; i < 2; i++)
-    {
-      if (s1.as_int[i] > 32767)
-        e.as_short[i] = 32767;
-      else if (s1.as_int[i] < -32768)
-        e.as_short[i] = -32768;
-      else
-        e.as_short[i] = s1.as_int[i];
-    }
-  
-  for (i = 0; i < 2; i++)
-   {
-      if (s2.as_int[i] > 32767)
-        e.as_short[i+2] = 32767;
-      else if (s2.as_int[i] < -32768)
-        e.as_short[i+2] = -32768;
-      else
-        e.as_short[i+2] = s2.as_int[i];
-    }
+#ifdef __LITTLE_ENDIAN__
+  e.as_m64 = _mm_set_pi16 (saturate (l_mm_extract_pi32 (s2.as_m64, 1)),
+                           saturate (l_mm_extract_pi32 (s2.as_m64, 0)),
+                           saturate (l_mm_extract_pi32 (s1.as_m64, 1)),
+                           saturate (l_mm_extract_pi32 (s1.as_m64, 0)));
+#else
+  e.as_m64 = _mm_set_pi16 (saturate (l_mm_extract_pi32 (s1.as_m64, 1)),
+                           saturate (l_mm_extract_pi32 (s1.as_m64, 0)),
+                           saturate (l_mm_extract_pi32 (s2.as_m64, 1)),
+                           saturate (l_mm_extract_pi32 (s2.as_m64, 0)));
+#endif
 
   if (u.as_m64 != e.as_m64)
     abort ();
