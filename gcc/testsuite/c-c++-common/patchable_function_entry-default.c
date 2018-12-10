418d97a2b33bd1ae69265261cef680668e07e25e
Eric Botcazou <ebotcazou@adacore.com>
2018-12-10 11:16:47 +0000

    patchable_function_entry-decl.c: Pass -mcpu=gr6 for Visium and remove other specific handling.
    	* c-c++-common/patchable_function_entry-decl.c: Pass -mcpu=gr6 for
    	Visium and remove other specific handling.
    	* c-c++-common/patchable_function_entry-default.c: Likewise.
    	* c-c++-common/patchable_function_entry-definition.c: Likewise.
    
    From-SVN: r266948

--- gcc/testsuite/c-c++-common/patchable_function_entry-default.c
+++ gcc/testsuite/c-c++-common/patchable_function_entry-default.c
@@ -1,8 +1,8 @@
 /* { dg-do compile { target { ! nvptx*-*-* } } } */
 /* { dg-options "-O2 -fpatchable-function-entry=3,1" } */
-/* { dg-final { scan-assembler-times "nop|NOP" 3 { target { ! { alpha*-*-* visium-*-* } } } } } */
+/* { dg-additional-options "-mcpu=gr6" { target visium-*-* } }
+/* { dg-final { scan-assembler-times "nop|NOP" 3 { target { ! { alpha*-*-* } } } } } */
 /* { dg-final { scan-assembler-times "bis" 3 { target alpha*-*-* } } } */
-/* { dg-final { scan-assembler-times "nop" 4 { target visium-*-* } } } */
 
 extern int a;
 
