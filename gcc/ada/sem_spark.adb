d80ef1addc5b30061dc3a9530f96107133340948
pmderodat <pmderodat@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-12-11 11:10:48 +0000

    [Ada] Minor reformattings
    2018-12-11  Gary Dismukes  <dismukes@adacore.com>
    
    gcc/ada/
    
    	* exp_util.ads: Use preferred U.S. spelling of "honored".
    	* sem_spark.adb: Remove extra whitespace.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@266997 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/sem_spark.adb
+++ gcc/ada/sem_spark.adb
@@ -230,8 +230,8 @@ package body Sem_SPARK is
       --------------------
 
       procedure Perm_Mismatch
-        (Exp_Perm, Act_Perm  : Perm_Kind;
-         N                   : Node_Id);
+        (Exp_Perm, Act_Perm : Perm_Kind;
+         N                  : Node_Id);
       --  Issues a continuation error message about a mismatch between a
       --  desired permission Exp_Perm and a permission obtained Act_Perm. N
       --  is the node on which the error is reported.
@@ -1652,7 +1652,7 @@ package body Sem_SPARK is
 
       if Present (Iteration_Scheme (Loop_N)) then
          declare
-            Exit_Env  : constant Perm_Env_Access := new Perm_Env;
+            Exit_Env : constant Perm_Env_Access := new Perm_Env;
 
          begin
             Copy_Env (From => Current_Perm_Env, To => Exit_Env.all);
