3c0517a6531112d2dec16d18cdabb1513d387b7a
Jakub Jelinek <jakub@redhat.com>
2018-12-11 21:28:35 +0100

    re PR tree-optimization/80520 (Performance regression from missing if-conversion)
    	PR tree-optimization/80520
    	* gcc.dg/tree-ssa/split-path-11.c (foo): Make the test ilp32 target
    	clean.
    
    From-SVN: r267031

--- gcc/testsuite/gcc.dg/tree-ssa/split-path-11.c
+++ gcc/testsuite/gcc.dg/tree-ssa/split-path-11.c
@@ -1,13 +1,13 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -fsplit-paths -fdump-tree-split-paths-details -w" } */
 
-void foo(unsigned long *M)
+void foo(unsigned long long *M)
 {
-  for (unsigned long k = 0; k < 227; ++k)
+  for (unsigned long long k = 0; k < 227; ++k)
     {
-      unsigned long y =
-	((M[k] & 0xffffffff80000000) | (M[k + 1] & 0x7fffffff));
-      M[k] = (M[k + 397] ^ (y >> 1) ^ ((y & 1) ? 2567483615 : 0));
+      unsigned long long y =
+	((M[k] & 0xffffffff80000000ULL) | (M[k + 1] & 0x7fffffffULL));
+      M[k] = (M[k + 397] ^ (y >> 1) ^ ((y & 1) ? 2567483615ULL : 0));
     }
 }
 
