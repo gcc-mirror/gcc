a0bb208bb9115f1f9feb3e47886558218f1edd73
Peter Bergner <bergner@linux.ibm.com>
2018-12-12 17:14:13 +0000

    backport: re PR target/87496 (ICE in aggregate_value_p at gcc/function.c:2046)
    gcc/
    	Backport from mainline
    	2018-12-07  Peter Bergner  <bergner@linux.ibm.com>
    
    	PR target/87496
    	* config/rs6000/rs6000.c (rs6000_option_override_internal): Disallow
    	-mabi=ieeelongdouble and -mabi=ibmlongdouble without -mlong-double-128.
    	Do not error for -mabi=ibmlongdouble and no ISA 2.06 support.
    	* doc/invoke.texi: Document -mabi=ibmlongdouble and -mabi=ieeelongdouble
    	require -mlong-double-128.
    
    gcc/testsuite/
    	Backport from mainline
    	2018-12-07  Peter Bergner  <bergner@linux.ibm.com>
    
    	PR target/87496
    	* gcc.target/powerpc/pr87496.c: Rename from this...
    	* gcc.target/powerpc/pr87496-1.c: ...to this.  Update comment.
    	* gcc.target/powerpc/pr87496-2.c: New test.
    	* gcc.target/powerpc/pr87496-3.c: New test.
    
    From-SVN: r267061

---
+++ gcc/testsuite/gcc.target/powerpc/pr87496-2.c
@@ -0,0 +1,9 @@
+/* PR target/87496 */
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power7" } } */
+/* { dg-options "-O2 -mcpu=power7 -mabi=ieeelongdouble -mlong-double-64 -Wno-psabi" } */
+
+int i;
+
+/* { dg-error "'-mabi=ieeelongdouble' requires '-mlong-double-128'" "PR87496" { target *-*-* } 0 } */
