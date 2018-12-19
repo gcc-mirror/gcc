43e458822f36b7d70df6e66b224d7b3a51a7d106
pc <pc@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-12-19 15:06:00 +0000

    [rs6000] Fix x86 SSSE3 compatibility implementations and testcases
    This patch is the analog to r266868-r266870, but for SSSE3.
    The SSSE3 tests had been inadvertently made to PASS without actually running
    the test code. Actually running the code turned up some previously undetected
    issues.
    
    This patch fixes some issues in the implementations, fixes up the tests
    to use a union for the test data, which avoids strict aliasing issues,
    and enables the tests to actually run (by removing a dependency on
    __BUILTIN_CPU_SUPPORTS).
    
    Also, there's a fairly insignificant change in the testcases that walk
    through the data as pairs of vectors from:
      [0] and [1]
      [2] and [3]
      ...
      [n-4] and [n-3]
      [n-2] and [n-1]
    
    to:
      [0] and [1]
      [1] and [2]
      ...
      [n-3] and [n-2]
      [n-2] and [n-1]
    
    Since the testcases compute the correct answers based on the input, no
    other changes were necessary to effect the change.
    
    2018-12-19  Paul A. Clarke  <pc@us.ibm.com>
    
    [gcc]
    
    	* config/rs6000/tmmintrin.h (_mm_hadds_epi16): Vector lanes swapped.
    	(_mm_hsub_epi32): Likewise.
    	(_mm_shuffle_epi8): Fix reversed interpretation of parameters.
    	(_mm_shuffle_pi8): Likewise.
    	(_mm_addubs_pi16): Likewise.
    
    [gcc/testsuite]
    
    	* gcc.target/powerpc/ssse3-check.h: Enable tests to run.
    	* gcc.target/powerpc/ssse3-pabsb.c: Code fixes for strict aliasing
    	issues.
    	* gcc.target/powerpc/ssse3-pabsd.c: Likewise.
    	* gcc.target/powerpc/ssse3-palignr.c: Likewise.
    	* gcc.target/powerpc/ssse3-phaddd.c: Likewise.
    	* gcc.target/powerpc/ssse3-phaddsw.c: Likewise.
    	* gcc.target/powerpc/ssse3-phaddw.c: Likewise.
    	* gcc.target/powerpc/ssse3-phsubd.c: Likewise.
    	* gcc.target/powerpc/ssse3-phsubw.c: Likewise.
    	* gcc.target/powerpc/ssse3-pmulhrsw.c: Likewise.
    	* gcc.target/powerpc/ssse3-pshufb.c: Likewise.
    	* gcc.target/powerpc/ssse3-psignb.c: Likewise.
    	* gcc.target/powerpc/ssse3-psignd.c: Likewise.
    	* gcc.target/powerpc/ssse3-psignw.c: Likewise.
    	* gcc.target/powerpc/ssse3-vals.h: Provide input data as a union.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@267271 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/ssse3-psignw.c
+++ gcc/testsuite/gcc.target/powerpc/ssse3-psignw.c
@@ -18,66 +18,59 @@
 #ifndef __AVX__
 /* Test the 64-bit form */
 static void
-ssse3_test_psignw (int *i1, int *i2, int *r)
+ssse3_test_psignw (__m64 *i1, __m64 *i2, __m64 *r)
 {
-  __m64 t1 = *(__m64 *) i1;
-  __m64 t2 = *(__m64 *) i2;
-  *(__m64 *) r = _mm_sign_pi16 (t1, t2);
+  *r = _mm_sign_pi16 (*i1, *i2);
   _mm_empty ();
 }
 #endif
 
 /* Test the 128-bit form */
 static void
-ssse3_test_psignw128 (int *i1, int *i2, int *r)
+ssse3_test_psignw128 (__m128i *i1, __m128i *i2, __m128i *r)
 {
   /* Assumes incoming pointers are 16-byte aligned */
-  __m128i t1 = *(__m128i *) i1;
-  __m128i t2 = *(__m128i *) i2;
- *(__m128i *) r = _mm_sign_epi16 (t1, t2);
+ *r = _mm_sign_epi16 (*i1, *i2);
 }
 
 /* Routine to manually compute the results */
 static void
-compute_correct_result (int *i1, int *i2, int *r)
+compute_correct_result (short *i1, short *i2, short *r)
 {
-  short *s1 = (short *) i1;
-  short *s2 = (short *) i2;
-  short *sout = (short *) r;
   int i;
 
   for (i = 0; i < 8; i++)
-    if (s2[i] < 0)
-      sout[i] = -s1[i];
-    else if (s2[i] == 0)
-      sout[i] = 0;
+    if (i2[i] < 0)
+      r[i] = -i1[i];
+    else if (i2[i] == 0)
+      r[i] = 0;
     else
-      sout[i] = s1[i];
+      r[i] = i1[i];
 }
 
 static void
 TEST (void)
 {
   int i;
-  int r [4] __attribute__ ((aligned(16)));
-  int ck [4];
+  union data r __attribute__ ((aligned(16)));
+  union data ck;
   int fail = 0;
 
-  for (i = 0; i < 256; i += 8)
+  for (i = 0; i < ARRAY_SIZE (vals) - 1; i++)
     {
       /* Manually compute the result */
-      compute_correct_result (&vals[i + 0], &vals[i + 4], ck);
+      compute_correct_result (&vals[i + 0].h[0], &vals[i + 1].h[0], &ck.h[0]);
 
 #ifndef __AVX__
       /* Run the 64-bit tests */
-      ssse3_test_psignw (&vals[i + 0], &vals[i + 4], &r[0]);
-      ssse3_test_psignw (&vals[i + 2], &vals[i + 6], &r[2]);
-      fail += chk_128 (ck, r);
+      ssse3_test_psignw (&vals[i + 0].ll[0], &vals[i + 1].ll[0], &r.ll[0]);
+      ssse3_test_psignw (&vals[i + 0].ll[1], &vals[i + 1].ll[1], &r.ll[1]);
+      fail += chk_128 (ck.m[0], r.m[0]);
 #endif
 
       /* Run the 128-bit tests */
-      ssse3_test_psignw128 (&vals[i + 0], &vals[i + 4], r);
-      fail += chk_128 (ck, r);
+      ssse3_test_psignw128 (&vals[i + 0].m[0], &vals[i + 1].m[0], &r.m[0]);
+      fail += chk_128 (ck.m[0], r.m[0]);
     }
 
   if (fail != 0)
