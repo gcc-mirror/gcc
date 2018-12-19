a152954ea4fee516e83b4f75a17818fbc8d555bb
Tom de Vries <tdevries@suse.de>
2018-12-19 14:20:54 +0000

    [nvptx] Commit passing pr85381-*.c test-cases
    Add pr85381*.c test-cases that are already passing without the fix for PR85381.
    
    Build and reg-tested on x86_64 with nvptx accelerator.
    
    2018-12-19  Tom de Vries  <tdevries@suse.de>
    
    	* testsuite/libgomp.oacc-c-c++-common/pr85381-2.c: New test.
    	* testsuite/libgomp.oacc-c-c++-common/pr85381-3.c: New test.
    	* testsuite/libgomp.oacc-c-c++-common/pr85381-4.c: New test.
    
    From-SVN: r267268

---
+++ libgomp/testsuite/libgomp.oacc-c-c++-common/pr85381-4.c
@@ -0,0 +1,27 @@
+/* { dg-additional-options "-save-temps -w" } */
+/* { dg-do run { target openacc_nvidia_accel_selected } }
+   { dg-skip-if "" { *-*-* } { "*" } { "-O2" } } */
+
+#define n 1024
+
+int
+main (void)
+{
+  #pragma acc parallel
+  {
+    #pragma acc loop worker
+    for (int i = 0; i < n; i++)
+      ;
+
+    #pragma acc loop worker
+    for (int i = 0; i < n; i++)
+      ;
+  }
+
+  return 0;
+}
+
+/* Atm, %ntid.y is broadcast from one loop to the next, so there are 2 bar.syncs
+   for that (the other two are there for the same reason as in pr85381-2.c).
+   Todo: Recompute %ntid.y instead of broadcasting it. */
+/* { dg-final { scan-assembler-times "bar.sync" 4 } } */
