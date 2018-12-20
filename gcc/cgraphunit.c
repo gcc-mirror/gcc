f2d0c6ad5dcff052c53e24f6461bf06ee5be2870
luoxhu <luoxhu@138bc75d-0d04-0410-961f-82ee72b054a4>
2018-12-20 08:44:42 +0000

    fix typo.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@267294 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cgraphunit.c
+++ gcc/cgraphunit.c
@@ -95,7 +95,7 @@ along with GCC; see the file COPYING3.  If not see
 	      both reduce linking times and linktime memory usage by	
 	      not having to represent whole program in memory.
 
-	   d) LTO sreaming.  When doing LTO, everything important gets
+	   d) LTO streaming.  When doing LTO, everything important gets
 	      streamed into the object file.
 
        Compile time and or linktime analysis stage (WPA):
