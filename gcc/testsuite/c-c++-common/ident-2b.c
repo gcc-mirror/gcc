224246175fca6e27015b14afe1dadbc2fe88a4c9
Iain Sandoe <iain@sandoe.co.uk>
2018-12-24 08:55:01 +0000

    powerpc-darwin, skip ident tests.
    The c-c++-common tests fail (or XPASS depending on which) on
    powerpc Darwin because it doesn't currently emit .ident marker. 
    
    Since there's no support for .ident in the assembler, we can't
    enable the output and therefore these tests should be skipped.
    
    2018-12-24  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* c-c++-common/ident-0a.c: Skip for powerpc-darwin.
    	* c-c++-common/ident-0b.c: Likewise.
    	* c-c++-common/ident-1a.c: Likewise.
    	* c-c++-common/ident-1b.c: Likewise.
    	* c-c++-common/ident-2a.c: Likewise.
    	* c-c++-common/ident-2b.c: Likewise.
    
    From-SVN: r267394

--- gcc/testsuite/c-c++-common/ident-2b.c
+++ gcc/testsuite/c-c++-common/ident-2b.c
@@ -2,6 +2,7 @@
  * Make sure scan-assembler-times turns off .ident unless -fident in testcase */
 /* { dg-do compile } */
 /* { dg-options "-fident" } */
+/* { dg-skip-if "no assembler .ident support" { powerpc*-*-darwin* } } */
 int ident;
 
 /* { dg-final { scan-assembler-times "GCC: " 1 } } */
