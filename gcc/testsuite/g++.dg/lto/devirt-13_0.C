d2014992c0b7c4090cfd030c02e792927ab674fc
Jan Hubicka <hubicka@ucw.cz>
2019-01-01 14:10:39 +0100

    devirt-13_0.C: Drop broken scan of ssa dump.
    	* g++.dg/lto/devirt-13_0.C: Drop broken scan of ssa dump.
    	* g++.dg/lto/devirt-14_0.C: Drop broken scan of ssa dump.
    	* g++.dg/lto/devirt-23_0.C: Add -fdump-ipa-cp.
    
    From-SVN: r267496

--- gcc/testsuite/g++.dg/lto/devirt-13_0.C
+++ gcc/testsuite/g++.dg/lto/devirt-13_0.C
@@ -1,5 +1,5 @@
 /* { dg-lto-do run } */
 /* Call to foo should be devirtualized because there are no derived types of A.  */
-/* { dg-lto-options "-O2 -flto -fdump-tree-ssa"  } */
+/* { dg-lto-options "-O2 -flto"  } */
 #include "../ipa/devirt-13.C"
-/* { dg-final { scan-tree-dump-times "OBJ_TYPE_REF" 0 "ssa"} } */
+/* Ideally we should also { scan-tree-dump-times "OBJ_TYPE_REF" 0 "ssa"}.  */
