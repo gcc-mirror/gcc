27ebfa0d1f055987fcee0b60de51a1f0f70f4a3f
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-01-01 13:10:39 +0000

    * g++.dg/lto/devirt-13_0.C: Drop broken scan of ssa dump. 	* g++.dg/lto/devirt-14_0.C: Drop broken scan of ssa dump. 	* g++.dg/lto/devirt-23_0.C: Add -fdump-ipa-cp.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@267496 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/lto/devirt-14_0.C
+++ gcc/testsuite/g++.dg/lto/devirt-14_0.C
@@ -1,4 +1,4 @@
 /* { dg-lto-do run } */
 /* { dg-lto-options "-O2 -fdump-tree-ssa"  } */
 #include "../ipa/devirt-14.C"
-/* { dg-final { scan-tree-dump-not "A.*foo" "ssa"} } */
+/* Ideally we should also { scan-tree-dump-not "A.*foo" "ssa"} } */
