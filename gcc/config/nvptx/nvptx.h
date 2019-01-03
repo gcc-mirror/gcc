0d06e8c794f37ded34e902ead35e7c70c3b112d1
Tom de Vries <tdevries@suse.de>
2019-01-03 15:08:36 +0000

    [nvptx] Add nvptx_mach_vector_length, nvptx_mach_max_workers
    The vector length and maximum number of workers are known compile-time.  Make
    these easily available during code generation via new functions.
    
    2019-01-03  Tom de Vries  <tdevries@suse.de>
    
    	* config/nvptx/nvptx.c (MACH_VECTOR_LENGTH, MACH_MAX_WORKERS): Define.
    	(init_axis_dim, nvptx_mach_max_workers, nvptx_mach_vector_length): New
    	function.
    	* config/nvptx/nvptx.h (struct machine_function): Add axis_dims.
    
    From-SVN: r267558

--- gcc/config/nvptx/nvptx.h
+++ gcc/config/nvptx/nvptx.h
@@ -218,6 +218,9 @@ struct GTY(()) machine_function
   int return_mode; /* Return mode of current fn.
 		      (machine_mode not defined yet.) */
   rtx axis_predicate[2]; /* Neutering predicates.  */
+  int axis_dim[2]; /* Maximum number of threads on each axis, dim[0] is
+		      vector_length, dim[1] is num_workers.  */
+  bool axis_dim_init_p;
   rtx unisimt_master; /* 'Master lane index' for -muniform-simt.  */
   rtx unisimt_predicate; /* Predicate for -muniform-simt.  */
   rtx unisimt_location; /* Mask location for -muniform-simt.  */
