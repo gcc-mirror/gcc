fdcef314bcb0728d883cd87d6950d85217e82755
Ian Lance Taylor <ian@gcc.gnu.org>
2019-01-07 21:44:06 +0000

    compiler: move slice construction to callers of makeslice
        This is the gccgo version of https://golang.org/cl/141822:
        
            Only return a pointer p to the new slices backing array from makeslice.
            Makeslice callers then construct sliceheader{p, len, cap} explictly
            instead of makeslice returning the slice.
        
        This change caused the GCC backend to break the runtime/pprof test by
        merging together the identical functions allocateReflectTransient and
        allocateTransient2M.  This caused the traceback to be other than
        expected.  Fix that by making the functions not identical.
        
        This is a step toward updating libgo to the Go1.12beta1 release.
        
        Reviewed-on: https://go-review.googlesource.com/c/155937
    
    From-SVN: r267660

--- libgo/go/runtime/pprof/mprof_test.go
+++ libgo/go/runtime/pprof/mprof_test.go
@@ -45,7 +45,7 @@ func allocatePersistent1K() {
 // Allocate transient memory using reflect.Call.
 
 func allocateReflectTransient() {
-	memSink = make([]byte, 2<<20)
+	memSink = make([]byte, 3<<20)
 }
 
 func allocateReflect() {
@@ -106,7 +106,7 @@ func TestMemoryProfiler(t *testing.T) {
 		// GC means that sometimes the value is not collected.
 		fmt.Sprintf(`(0|%v): (0|%v) \[%v: %v\] @( 0x[0-9,a-f]+)+
 #	0x[0-9,a-f]+	pprof\.allocateReflectTransient\+0x[0-9,a-f]+	.*/mprof_test.go:48
-`, memoryProfilerRun, (2<<20)*memoryProfilerRun, memoryProfilerRun, (2<<20)*memoryProfilerRun),
+`, memoryProfilerRun, (3<<20)*memoryProfilerRun, memoryProfilerRun, (3<<20)*memoryProfilerRun),
 	}
 
 	for _, test := range tests {
