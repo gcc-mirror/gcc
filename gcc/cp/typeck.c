6001aba1132f2b82bb413168f58767c16bc48dcf
Jakub Jelinek <jakub@redhat.com>
2019-01-08 11:06:33 +0100

    backport: re PR c++/88103 (Wrong value category when conditional expression result is used as object expression)
    	Backported from mainline
    	2018-12-04  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/88103
    	* typeck.c (build_class_member_access_expr): If unary_complex_lvalue
    	turned xvalue_p into non-xvalue_p, call move on it.
    
    	* g++.dg/cpp0x/rv-cond3.C: New test.
    
    From-SVN: r267695

--- gcc/cp/typeck.c
+++ gcc/cp/typeck.c
@@ -2421,7 +2421,13 @@ build_class_member_access_expr (cp_expr object, tree member,
   {
     tree temp = unary_complex_lvalue (ADDR_EXPR, object);
     if (temp)
-      object = cp_build_fold_indirect_ref (temp);
+      {
+	temp = cp_build_fold_indirect_ref (temp);
+	if (xvalue_p (object) && !xvalue_p (temp))
+	  /* Preserve xvalue kind.  */
+	  temp = move (temp);
+	object = temp;
+      }
   }
 
   /* In [expr.ref], there is an explicit list of the valid choices for
