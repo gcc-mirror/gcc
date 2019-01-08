7fe9e5be6f04e299573a0d783473d37392245019
Jakub Jelinek <jakub@redhat.com>
2019-01-08 10:58:48 +0100

    backport: re PR c++/88181 (ICE: verify_type failed (error: type variant differs by TYPE_PACKED))
    	Backported from mainline
    	2018-11-27  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/88181
    	* class.c (fixup_attribute_variants): Also propagate TYPE_PACKED
    	to variants.
    
    	* g++.dg/debug/pr88181.C: New test.
    
    From-SVN: r267689

--- gcc/cp/class.c
+++ gcc/cp/class.c
@@ -1957,6 +1957,7 @@ fixup_attribute_variants (tree t)
   unsigned align = TYPE_ALIGN (t);
   bool user_align = TYPE_USER_ALIGN (t);
   bool may_alias = lookup_attribute ("may_alias", attrs);
+  bool packed = TYPE_PACKED (t);
 
   if (may_alias)
     fixup_may_alias (t);
@@ -1974,6 +1975,7 @@ fixup_attribute_variants (tree t)
       else
 	TYPE_USER_ALIGN (variants) = user_align;
       SET_TYPE_ALIGN (variants, valign);
+      TYPE_PACKED (variants) = packed;
       if (may_alias)
 	fixup_may_alias (variants);
     }
