efb56ae82bd871b64861e991466ce616688a88e9
Tom de Vries <tdevries@suse.de>
2019-01-12 22:19:31 +0000

    [nvptx] Enable setting vector length using -fopenacc-dim -- testcases
    Add some test-cases that set vector length using -fopenacc-dim.
    
    2019-01-12  Tom de Vries  <tdevries@suse.de>
    
    	* testsuite/libgomp.oacc-c-c++-common/pr85486-2.c: New test.
    	* testsuite/libgomp.oacc-c-c++-common/vector-length-128-2.c: New test.
    	* testsuite/libgomp.oacc-c-c++-common/vector-length-128-5.c: New test.
    	* testsuite/libgomp.oacc-fortran/gemm-2.f90: New test.
    
    From-SVN: r267897

---
+++ libgomp/testsuite/libgomp.oacc-c-c++-common/pr85486-2.c
@@ -0,0 +1,52 @@
+/* { dg-do run { target openacc_nvidia_accel_selected } } */
+/* { dg-additional-options "-fopenacc-dim=::128" } */
+
+/* Minimized from ref-1.C.  */
+
+#include <stdio.h>
+
+#pragma acc routine vector
+void __attribute__((noinline, noclone))
+Vector (int *ptr, int n, const int inc)
+{
+  #pragma acc loop vector
+  for (unsigned ix = 0; ix < n; ix++)
+    ptr[ix] += inc;
+}
+
+int
+main (void)
+{
+  const int n = 32, m=32;
+
+  int ary[m][n];
+  unsigned ix,  iy;
+
+  for (ix = m; ix--;)
+    for (iy = n; iy--;)
+      ary[ix][iy] = (1 << 16) + (ix << 8) + iy;
+
+  int err = 0;
+
+#pragma acc parallel copy (ary)
+  {
+    Vector (&ary[0][0], m * n, (1 << 24) - (1 << 16));
+  }
+
+  for (ix = m; ix--;)
+    for (iy = n; iy--;)
+      if (ary[ix][iy] != ((1 << 24) + (ix << 8) + iy))
+	{
+	  printf ("ary[%u][%u] = %x expected %x\n",
+		  ix, iy, ary[ix][iy], ((1 << 24) + (ix << 8) + iy));
+	  err++;
+	}
+
+  if (err)
+    {
+      printf ("%d failed\n", err);
+      return 1;
+    }
+
+  return 0;
+}
