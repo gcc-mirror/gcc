bf4eb32bc1a32e1cabf28281d3251ea67864b79b
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-01-14 18:40:34 +0000

    Fix location of tls_wrapper_fn (PR gcov-profile/88263).
    2019-01-14  Martin Liska  <mliska@suse.cz>
    
    	PR gcov-profile/88263
    	* decl2.c (get_tls_wrapper_fn): Use DECL_SOURCE_LOCATION
    	as location of the TLS wrapper.
    2019-01-14  Martin Liska  <mliska@suse.cz>
    
    	PR gcov-profile/88263
    	* g++.dg/gcov/pr88263-2.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@267921 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/gcov/pr88263-2.C
@@ -0,0 +1,25 @@
+// PR gcov-profile/88263
+// { dg-options "-fprofile-arcs -ftest-coverage -std=c++11" }
+// { dg-do run { target native } }
+
+#include <sstream>
+
+namespace logging {
+    class Logstream {
+	~Logstream();
+	static thread_local std::ostringstream os_;
+    };
+}
+namespace logging {
+    thread_local std::ostringstream Logstream::os_;
+    Logstream::~Logstream() {
+	os_.clear();
+    }
+}
+
+int main()
+{
+  return 0;
+}
+
+// { dg-final { run-gcov pr88263-2.C } }
