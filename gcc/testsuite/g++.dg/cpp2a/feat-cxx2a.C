478fe3c07d951d1671586769e4b9239419f635dd
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-01-14 20:12:11 +0000

    * c-cppbuiltin.c (c_cpp_builtin): Define __cpp_guaranteed_copy_elision 	and __cpp_nontype_template_parameter_auto.  Add a comment that 	__cpp_template_auto is deprecated.
    	* g++.dg/cpp1z/feat-cxx1z.C: Add tests for
    	__cpp_guaranteed_copy_elision and __cpp_nontype_template_parameter_auto
    	feature test macros.
    	* g++.dg/cpp2a/feat-cxx2a.C: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@267925 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp2a/feat-cxx2a.C
+++ gcc/testsuite/g++.dg/cpp2a/feat-cxx2a.C
@@ -416,6 +416,18 @@
 #  error "__cpp_variadic_using != 201611"
 #endif
 
+#ifndef __cpp_guaranteed_copy_elision
+#  error "__cpp_guaranteed_copy_elision"
+#elif __cpp_guaranteed_copy_elision != 201606
+#  error "__cpp_guaranteed_copy_elision != 201606"
+#endif
+
+#ifndef __cpp_nontype_template_parameter_auto
+#  error "__cpp_nontype_template_parameter_auto"
+#elif __cpp_nontype_template_parameter_auto != 201606
+#  error "__cpp_nontype_template_parameter_auto != 201606"
+#endif
+
 // C++20 features
 
 #if __cpp_conditional_explicit != 201806
