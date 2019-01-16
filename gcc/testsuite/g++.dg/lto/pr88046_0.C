4e7cbff5c2a391f025b29cc0ce05e1d7d01b5d59
Tamar Christina <tamar.christina@arm.com>
2019-01-16 17:50:38 +0000

    Fix PR88046 on AArch64 and Arm bare metal targets.
    gcc/testsuite/ChangeLog:
    
    	PR debug/88046
    	* g++.dg/lto/pr88046_0.C: Check for shared and fPIC.
    
    From-SVN: r267980

--- gcc/testsuite/g++.dg/lto/pr88046_0.C
+++ gcc/testsuite/g++.dg/lto/pr88046_0.C
@@ -1,5 +1,7 @@
 // { dg-lto-do link }
 // { dg-lto-options { { -O2 -fPIC -flto } } }
+// { dg-require-effective-target shared }
+// { dg-require-effective-target fpic }
 // { dg-extra-ld-options "-shared -g" }
 
 class a {};
