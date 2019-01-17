742b6dc476c544389f109b03d221262a4cc55aaf
Paolo Carlini <paolo.carlini@oracle.com>
2019-01-17 18:01:43 +0000

    decl.c (grokdeclarator): Use typespec_loc in error messages about 'auto' and trailing return type.
    /cp
    2019-01-17  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* decl.c (grokdeclarator): Use typespec_loc in error messages
    	about 'auto' and trailing return type.
    
    /testsuite
    2019-01-17  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* g++.dg/cpp0x/auto52.C: Test locations too.
    	* g++.dg/cpp0x/trailing2.C: Likewise.
    	* g++.dg/cpp1y/auto-fn18.C: Likewise.
    	* g++.dg/cpp1y/auto-fn25.C: Likewise.
    	* g++.dg/cpp1y/auto-fn52.C: Likewise.
    	* g++.dg/cpp1y/auto-fn53.C: Likewise.
    	* g++.dg/cpp1y/auto-fn54.C: Likewise.
    
    From-SVN: r268043

--- gcc/testsuite/g++.dg/cpp1y/auto-fn53.C
+++ gcc/testsuite/g++.dg/cpp1y/auto-fn53.C
@@ -1,4 +1,4 @@
 // PR c++/86942
 // { dg-do compile { target c++14 } }
 
-using T = decltype(auto) () -> int; // { dg-error "invalid use of" }
+using T = decltype(auto) () -> int; // { dg-error "11:invalid use of" }
