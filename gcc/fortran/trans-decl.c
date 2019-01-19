7b60c1315b59398b6360cda8df73e4b68b2151a8
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-01-19 08:43:12 +0000

    PR fortran/88902 	* trans-decl.c (gfc_get_symbol_decl): Don't add length to function 	or parent function if it has been added there already.
    	* gfortran.dg/pr88902.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@268091 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/trans-decl.c
+++ gcc/fortran/trans-decl.c
@@ -1572,13 +1572,17 @@ gfc_get_symbol_decl (gfc_symbol * sym)
 	  if (VAR_P (length) && DECL_FILE_SCOPE_P (length))
 	    {
 	      /* Add the string length to the same context as the symbol.  */
-	      if (DECL_CONTEXT (sym->backend_decl) == current_function_decl)
-	        gfc_add_decl_to_function (length);
-	      else
-		gfc_add_decl_to_parent_function (length);
+	      if (DECL_CONTEXT (length) == NULL_TREE)
+		{
+		  if (DECL_CONTEXT (sym->backend_decl)
+		      == current_function_decl)
+		    gfc_add_decl_to_function (length);
+		  else
+		    gfc_add_decl_to_parent_function (length);
+		}
 
-	      gcc_assert (DECL_CONTEXT (sym->backend_decl) ==
-			    DECL_CONTEXT (length));
+	      gcc_assert (DECL_CONTEXT (sym->backend_decl)
+			  == DECL_CONTEXT (length));
 
 	      gfc_defer_symbol_init (sym);
 	    }
