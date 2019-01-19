e047844bd6af7b167eb06c570c2c79b800165bfb
Richard Sandiford <richard.sandiford@arm.com>
2019-01-19 12:59:51 +0000

    Claw back some of the code size regression in 548.exchange2_r
    This patch tries harder to detect cases in which the inner dimension
    of an array access is invariant, such as:
    
         x(i, :) = 100
    
    It fixes some of the code size regression in 548.exchange2_r, with
    size improving by 5% compared to before the patch.  Of the two other
    SPEC 2017 tests affected by loop versioning, 554.roms_r improved by a
    trivial amount (0.3%) and 549.fotonik3d_r didn't change.  All three
    results are with -Ofast -flto.
    
    2019-01-19  Richard Sandiford  <richard.sandiford@arm.com>
    
    gcc/
    	* gimple-loop-versioning.cc (loop_versioning::dump_inner_likelihood):
    	New function, split out from...
    	(loop_versioning::analyze_stride): ...here.
    	(loop_versioning::find_per_loop_multiplication): Use gassign.
    	(loop_versioning::analyze_term_using_scevs): Return a success code.
    	(loop_versioning::analyze_arbitrary_term): New function.
    	(loop_versioning::analyze_address_fragment): Use
    	analyze_arbitrary_term if all else fails.
    
    gcc/testsuite/
    	* gfortran.dg/loop_versioning_1.f90: Bump the number of identified
    	inner strides.
    	* gfortran.dg/loop_versioning_9.f90: New test.
    	* gfortran.dg/loop_versioning_10.f90: Likewise.
    
    From-SVN: r268093

---
+++ gcc/testsuite/gfortran.dg/loop_versioning_10.f90
@@ -0,0 +1,31 @@
+! { dg-options "-O3 -fdump-tree-lversion-details" }
+
+subroutine f1(x)
+  real :: x(:, :)
+  x(:, 1) = 100
+end subroutine f1
+
+subroutine f2(x, i)
+  real :: x(:, :)
+  integer :: i
+  x(:, i) = 100
+end subroutine f2
+
+subroutine f3(x)
+  real :: x(:, :)
+  do j = lbound(x, 1), ubound(x, 1)
+     x(j, 1) = 100
+  end do
+end subroutine f3
+
+subroutine f4(x, i)
+  real :: x(:, :)
+  integer :: i
+  do j = lbound(x, 1), ubound(x, 1)
+     x(j, i) = 100
+  end do
+end subroutine f4
+
+! { dg-final { scan-tree-dump-times {likely to be the innermost dimension} 6 "lversion" } }
+! { dg-final { scan-tree-dump-times {want to version} 4 "lversion" } }
+! { dg-final { scan-tree-dump-times {versioned} 4 "lversion" } }
