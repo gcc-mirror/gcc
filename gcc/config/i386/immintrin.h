ca55c217ce6025bac034f31b8f6510ac097ee69b
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-01-21 12:23:49 +0000

    i386: Move Intel intrinsics head files to <immintrin.h>
    According to Intel Intrinsics Guide:
    
    https://software.intel.com/sites/landingpage/IntrinsicsGuide/
    
    Intel intrinsics should be available by including <immintrin.h>.  This
    patch moves remaining Intel intrinsics head files from <x86intrin.h> to
    <immintrin.h>.
    
    	PR target/71659
    	* config/i386/adxintrin.h: Just check _IMMINTRIN_H_INCLUDED.
    	* config/i386/clflushoptintrin.h: Check _IMMINTRIN_H_INCLUDED
    	instead of _X86INTRIN_H_INCLUDED.
    	* onfig/i386/clwbintrin.h: Likewise.
    	* config/i386/pkuintrin.h: Likewise.
    	* config/i386/prfchwintrin.h: Likewise.
    	* config/i386/rdseedintrin.h: Likewise.
    	* config/i386/wbnoinvdintrin.h: Likewise.
    	* config/i386/xsavecintrin.h: Likewise.
    	* config/i386/xsavesintrin.h: Likewise.
    	* config/i386/fxsrintrin.h: Enable _IMMINTRIN_H_INCLUDED check.
    	* config/i386/xsaveintrin.h: Likewise.
    	* config/i386/xsaveoptintrin.h: Likewise.
    	* config/i386/x86intrin.h: Move "#include" <rdseedintrin.h>,
    	<prfchwintrin.h>, <fxsrintrin.h>, <xsaveintrin.h>,
    	<xsaveoptintrin.h>, <adxintrin.h>, <clwbintrin.h>,
    	<clflushoptintrin.h>, <xsavesintrin.h>, <xsavecintrin.h>,
    	<wbnoinvdintrin.h> and <pkuintrin.h> to ...
    	* config/i386/immintrin.h: Here.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@268113 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/immintrin.h
+++ gcc/config/i386/immintrin.h
@@ -38,6 +38,16 @@
 
 #include <wmmintrin.h>
 
+#include <fxsrintrin.h>
+
+#include <xsaveintrin.h>
+
+#include <xsaveoptintrin.h>
+
+#include <xsavesintrin.h>
+
+#include <xsavecintrin.h>
+
 #include <avxintrin.h>
 
 #include <avx2intrin.h>
@@ -119,6 +129,20 @@
 #include <waitpkgintrin.h>
 
 #include <cldemoteintrin.h>
+
+#include <rdseedintrin.h>
+
+#include <prfchwintrin.h>
+
+#include <adxintrin.h>
+
+#include <clwbintrin.h>
+
+#include <clflushoptintrin.h>
+
+#include <wbnoinvdintrin.h>
+
+#include <pkuintrin.h>
 
 extern __inline void
 __attribute__((__gnu_inline__, __always_inline__, __artificial__))
