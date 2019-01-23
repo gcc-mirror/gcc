d351dc7bc2fcfbf41ca12049b9ca578ff5c9e46b
Gergö Barany <gergo@codesourcery.com>
2019-01-23 14:32:57 -0800

    Handle conditional execution of loops in OpenACC kernels regions
    Any OpenACC loop controlled by an if statement or a non-OpenACC loop must be
    executed in a gang-single region. Detecting such loops is not trivial as
    OpenACC kernels expansion is done on GIMPLE but before computation of the
    control flow graph. This patch adds an auxiliary analysis for determining
    whether a statement is inside a conditionally executed region (relative to
    the kernels region's entry).
    
        gcc/
        * omp-oacc-kernels.c (control_flow_regions): New class.
        (control_flow_regions::control_flow_regions): New constructor.
        (control_flow_regions::is_unconditional_oacc_for_loop): New method.
        (control_flow_regions::find_rep): Likewise.
        (control_flow_regions::union_reps): Likewise.
        (control_flow_regions::compute_regions): Likewise.
        (decompose_kernels_region_body): Use test for conditional execution.
    
        gcc/testsuite/
        * c-c++-common/goacc/kernels-conversion.c: Add test for conditionally
        executed code.
        * gfortran.dg/goacc/kernels-conversion.f95: Likewise.

--- gcc/testsuite/c-c++-common/goacc/kernels-conversion.c
+++ gcc/testsuite/c-c++-common/goacc/kernels-conversion.c
@@ -12,6 +12,7 @@ main (void)
   unsigned int sum = 1;
 
 #pragma acc kernels copyin(a[0:N]) copy(sum)
+  /* { dg-bogus "region contains gang partitoned code but is not gang partitioned" "gang partitioned" { xfail *-*-* } .-1 } */
   {
     #pragma acc loop
     for (i = 0; i < N; ++i)
@@ -19,6 +20,17 @@ main (void)
 
     sum++;
     a[0]++;
+
+    #pragma acc loop
+    for (i = 0; i < N; ++i)
+      sum += a[i];
+
+    if (sum > 10)
+      { 
+        #pragma acc loop
+        for (i = 0; i < N; ++i)
+          sum += a[i];
+      }
 
     #pragma acc loop
     for (i = 0; i < N; ++i)
@@ -32,10 +44,10 @@ main (void)
    parallel regions.  */ 
 /* { dg-final { scan-tree-dump-times "oacc_data_kernels" 1 "convert_oacc_kernels" } } */
 
-/* The two loop regions are parallelized, the sequential part in between is
-   made gang-single.  */
-/* { dg-final { scan-tree-dump-times "oacc_parallel_kernels_parallelized" 2 "convert_oacc_kernels" } } */
-/* { dg-final { scan-tree-dump-times "oacc_parallel_kernels_gang_single" 1 "convert_oacc_kernels" } } */
+/* The three unconditional loop regions are parallelized, the sequential
+   part in between and the conditional loop are made gang-single.  */
+/* { dg-final { scan-tree-dump-times "oacc_parallel_kernels_parallelized" 3 "convert_oacc_kernels" } } */
+/* { dg-final { scan-tree-dump-times "oacc_parallel_kernels_gang_single" 2 "convert_oacc_kernels" } } */
 
 /* Check that the original kernels region is removed.  */
 /* { dg-final { scan-tree-dump-not "oacc_kernels" "convert_oacc_kernels" } } */
