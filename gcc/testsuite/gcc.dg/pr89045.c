f4b7e754cc9db79e4239ffcdf56acc780543ed05
Jakub Jelinek <jakub@redhat.com>
2019-01-29 00:49:51 +0100

    re PR c/89045 (ICE in get_parm_info, at c/c-decl.c:7518)
    	PR c/89045
    	* c-decl.c (build_compound_literal): Don't pushdecl if in parameter
    	scope.
    
    	* gcc.dg/pr89045.c: New test.
    
    From-SVN: r268349

---
+++ gcc/testsuite/gcc.dg/pr89045.c
@@ -0,0 +1,15 @@
+/* PR c/89045 */
+/* { dg-do compile } */
+/* { dg-options "" } */
+
+int
+foo (int x)
+{
+  int v[(int){ x }];
+  v[0] = 0;
+  int bar (int p[(int){ x }])
+  {
+    return p[0];
+  }
+  return bar (v);
+}
