c98f255154798847bdd1fc6ce33266c1a1ddc13a
Jonathan Wakely <jwakely@redhat.com>
2019-01-28 23:58:57 +0000

    PR libstdc++/68737 Do not use vsnprintf on HPUX
    It doesn't conform to the spec, so use vsprintf with a large buffer
    instead.
    
    	PR libstdc++/68737
    	* config/locale/generic/c_locale.h (__convert_from_v)
    	[_GLIBCXX_USE_C99_STDIO]: Also check _GLIBCXX_HAVE_BROKEN_VSNPRINTF.
    	* config/os/hpux/os_defines.h: Define _GLIBCXX_HAVE_BROKEN_VSNPRINTF.
    	* include/bits/locale_facets.tcc (num_put::_M_insert_float)
    	[_GLIBCXX_USE_C99_STDIO]: Also check _GLIBCXX_HAVE_BROKEN_VSNPRINTF.
    
    From-SVN: r268350

--- libstdc++-v3/config/locale/generic/c_locale.h
+++ libstdc++-v3/config/locale/generic/c_locale.h
@@ -70,7 +70,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
     __builtin_va_list __args;
     __builtin_va_start(__args, __fmt);
 
-#if _GLIBCXX_USE_C99_STDIO
+#if _GLIBCXX_USE_C99_STDIO && !_GLIBCXX_HAVE_BROKEN_VSNPRINTF
     const int __ret = __builtin_vsnprintf(__out, __size, __fmt, __args);
 #else
     const int __ret = __builtin_vsprintf(__out, __fmt, __args);
