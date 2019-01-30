bd5092043090e89d693cde98c619e8064f393893
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-01-30 02:32:33 +0000

    PR testsuite/89110 	* g++.dg/other/nontype-1.C: Expect error in all modes. 	* g++.dg/parse/crash13.C: Likewise. 	* g++.dg/parse/error36.C: Likewise. 	* g++.dg/template/error29.C: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@268376 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/other/nontype-1.C
+++ gcc/testsuite/g++.dg/other/nontype-1.C
@@ -1,7 +1,7 @@
 template <class Op>
 bool asfun(Op f,
-           Op::first_argument_type a, // { dg-error "not a type" "" { target c++17_down } }
-           Op::second_argument_type b) // { dg-error "not a type" "" { target c++17_down } }
+           Op::first_argument_type a, // { dg-error "not a type" }
+           Op::second_argument_type b) // { dg-error "not a type" }
 {
    return Op(a, b);
 }
