7ad97d17775832ca57166a8bf302f48ea07a785e
Richard Biener <rguenther@suse.de>
2019-01-31 11:51:59 +0000

    re PR tree-optimization/89135 (internal compiler error: in gimple_split_edge, at tree-cfg.c:2747)
    2019-01-31  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/89135
    	* tree-ssa-phiprop.c (pass_phiprop::execute): Skip blocks
    	with abnormal preds.
    
    	* gcc.dg/torture/pr89135.c: New testcase.
    
    From-SVN: r268417

---
+++ gcc/testsuite/gcc.dg/torture/pr89135.c
@@ -0,0 +1,33 @@
+/* { dg-do compile } */
+
+typedef __INTPTR_TYPE__ intptr_t;
+intptr_t a, b, c, d;
+int foo (void) { return 0; }
+int baz (void);
+
+void
+bar (void)
+{
+  intptr_t g = (intptr_t) &&h;
+  void *i = &&j, *k = &&l;
+j:
+  if (baz ())
+    {
+      intptr_t **n = (intptr_t **) &a;
+l:
+      b = 0;
+      for (; b >= 0;)
+	goto *k;
+h:
+      **n = 0;
+      for (;;)
+	{
+	  intptr_t *o = &c;
+	  g = foo ();
+	  *o = g;
+	  if (c)
+	    goto *d;
+	}
+    }
+  goto *i;
+}
