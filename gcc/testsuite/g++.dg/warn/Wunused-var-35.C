0b841214a8c8a56d4c4d391fddf780faba3f5748
Martin Sebor <msebor@redhat.com>
2019-02-03 21:48:27 +0000

    PR c++/44648 - missing -Wunused warning on a const variable in if statement
    gcc/testsuite/ChangeLog:
    	* g++.dg/warn/Wunused-var-35.C: New test.
    
    From-SVN: r268503

---
+++ gcc/testsuite/g++.dg/warn/Wunused-var-35.C
@@ -0,0 +1,19 @@
+// PR c++/44648 - missing -Wunused warning on a const variable in if statement
+// { dg-do compile }
+// { dg-options "-Wunused" }
+
+int main()
+{
+  bool b0 = 1;           // { dg-warning "\\\[-Wunused-variable\\\]" }
+  const bool b00 = 1;    // { dg-warning "\\\[-Wunused-variable\\\]" }
+  if (bool b1 = 1)       // { dg-warning "\\\[-Wunused-variable\\\]" }
+    return 0;
+  else
+    return 1;
+
+  if (const bool b2 = 1) // { dg-warning "\\\[-Wunused-variable\\\]" "bug" { xfail c++98_only } }
+    return 0;
+  else
+    return 1;
+}
+
