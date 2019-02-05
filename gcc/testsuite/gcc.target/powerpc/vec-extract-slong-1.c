6df7c3ed3172dd4ccd94dbf5f073c8a1be116b7d
Kelvin Nilsen <kelvin@gcc.gnu.org>
2019-02-05 19:42:57 +0000

    backport: rs6000-c.c (altivec-resolve_overloaded_builtin): Change handling of ALTIVEC_BUILTIN_VEC_EXTRACT.
    gcc/ChangeLog:
    
    2019-02-05  Kelvin Nilsen  <kelvin@gcc.gnu.org>
    
    	Backport from mainline.
    	2019-01-30  Kelvin Nilsen  <kelvin@gcc.gnu.org>
    	* config/rs6000/rs6000-c.c (altivec-resolve_overloaded_builtin):
    	Change handling of ALTIVEC_BUILTIN_VEC_EXTRACT.  Coerce result to
    	type of vector element when vec_extract is implemented by direct
    	move.
    
    gcc/testsuite/ChangeLog:
    
    2019-02-05  Kelvin Nilsen  <kelvin@gcc.gnu.org>
    
    	2019-02-05  Kelvin Nilsen  <kelvin@gcc.gnu.org>
    	* gcc.target/powerpc/vec-extract-slong-1.c: Require p8 execution
    	hardware.
    	* gcc.target/powerpc/vec-extract-schar-1.c: Likewise.
    	* gcc.target/powerpc/vec-extract-sint128-1.c: Likewise.
    	* gcc.target/powerpc/vec-extract-sshort-1.c: Likewise.
    	* gcc.target/powerpc/vec-extract-ulong-1.c: Likewise.
    	* gcc.target/powerpc/vec-extract-uchar-1.c: Likewise.
    	* gcc.target/powerpc/vec-extract-sint-1.c: Likewise.
    	* gcc.target/powerpc/vec-extract-uint128-1.c: Likewise.
    	* gcc.target/powerpc/vec-extract-ushort-1.c: Likewise.
    	* gcc.target/powerpc/vec-extract-uint-1.c: Likewise.
    
    	Backport from mainline.
    	2019-01-30  Kelvin Nilsen  <kelvin@gcc.gnu.org>
    	* gcc.target/powerpc/vec-extract-schar-1.c: New test.
    	* gcc.target/powerpc/vec-extract-sint-1.c: New test.
    	* gcc.target/powerpc/vec-extract-sint128-1.c: New test.
    	* gcc.target/powerpc/vec-extract-slong-1.c: New test.
    	* gcc.target/powerpc/vec-extract-sshort-1.c: New test.
    	* gcc.target/powerpc/vec-extract-uchar-1.c: New test.
    	* gcc.target/powerpc/vec-extract-uint-1.c: New test.
    	* gcc.target/powerpc/vec-extract-uint128-1.c: New test.
    	* gcc.target/powerpc/vec-extract-ulong-1.c: New test.
    	* gcc.target/powerpc/vec-extract-ushort-1.c: New test.
    
    From-SVN: r268556

---
+++ gcc/testsuite/gcc.target/powerpc/vec-extract-slong-1.c
@@ -0,0 +1,27 @@
+/* Test to verify that the vec_extract from a vector of
+   signed longs remains signed.  */
+/* { dg-do run } */
+/* { dg-options "-ansi -mcpu=power8 " } */
+/* { dg-require-effective-target p8vector_hw } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power8" } } */
+
+#include <altivec.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int test1(signed long long int sl) {
+
+  vector signed long long int v = vec_splats(sl);
+
+  if (vec_extract (v, 0) > sl)
+    abort();
+  return 0;
+}
+
+int main()
+{
+  test1 (0xf600000000000000LL);
+  test1 (0x7600000000000000LL);
+  test1 (0x0600000000000000LL);
+  return 0;
+}
---
+++ gcc/testsuite/gcc.target/powerpc/vec-extract-uchar-1.c
@@ -0,0 +1,29 @@
+/* Test to verify that the vec_extract from a vector of
+   unsigned chars remains unsigned.  */
+/* { dg-do run } */
+/* { dg-options "-ansi -mcpu=power8 " } */
+/* { dg-require-effective-target p8vector_hw } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power8" } } */
+
+#include <altivec.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int test1(unsigned char uc) {
+  int uce;
+
+  vector unsigned char v = vec_splats(uc);
+  uce = vec_extract(v,0);
+
+  if (uce != uc)
+    abort();
+  return 0;
+}
+
+int main()
+{
+  test1 (0xf6);
+  test1 (0x76);
+  test1 (0x06);
+  return 0;
+}
