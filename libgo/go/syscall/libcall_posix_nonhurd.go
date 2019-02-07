4321f202f985f659e0150ee72ed3e14e554f9735
Ian Lance Taylor <ian@gcc.gnu.org>
2019-02-07 03:11:47 +0000

    syscall: add Hurd support
        Loosely based on a patch by Svante Signell.
        
        Reviewed-on: https://go-review.googlesource.com/c/161518
    
    From-SVN: r268603

---
+++ libgo/go/syscall/libcall_posix_nonhurd.go
@@ -0,0 +1,17 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !hurd
+
+package syscall
+
+// Removed the mount call for GNU/Hurd, it exists but use translators.
+// Functionality is not the same as descibed in Linux <sys/mount.h>.
+// Removed the madvise call for GNU/Hurd, not yet implemented.
+
+//sys	Mount(source string, target string, fstype string, flags uintptr, data string) (err error)
+//mount(source *byte, target *byte, fstype *byte, flags _C_long, data *byte) _C_int
+
+//sys Madvise(b []byte, advice int) (err error)
+//madvise(addr *byte, len Size_t, advice _C_int) _C_int
