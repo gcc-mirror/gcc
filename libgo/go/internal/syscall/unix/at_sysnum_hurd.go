9fa4b2aa45d3ceec8f13cc6b1415a84d13254018
Ian Lance Taylor <ian@gcc.gnu.org>
2019-02-07 02:22:57 +0000

    internal/syscall/unix: add constants for hurd
        Patch by Svante Signell.
        
        Reviewed-on: https://go-review.googlesource.com/c/161517
    
    From-SVN: r268602

---
+++ libgo/go/internal/syscall/unix/at_sysnum_hurd.go
@@ -0,0 +1,8 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package unix
+
+const AT_REMOVEDIR = 0x200
+const AT_SYMLINK_NOFOLLOW = 0x100
