25030590b128f579d91f266b5a1eaeb7d50f0c89
Jakub Jelinek <jakub@redhat.com>
2019-02-09 09:51:41 +0100

    re PR middle-end/89243 (ICE in new test case g++.dg/opt/pr89188.C from r268647)
    	PR middle-end/89243
    	* g++.dg/opt/pr89188.C: Include ../torture/pr88861.C.
    
    	Backported from mainline
    	2019-01-16  David Malcolm  <dmalcolm@redhat.com>
    
    	PR target/88861
    	* combine.c (delete_noop_moves): Convert to "bool" return,
    	returning true if any edges are eliminated.
    	(combine_instructions): Also return true if delete_noop_moves
    	returns true.
    
    	* g++.dg/torture/pr88861.C: New test.
    
    From-SVN: r268717

--- gcc/testsuite/g++.dg/opt/pr89188.C
+++ gcc/testsuite/g++.dg/opt/pr89188.C
@@ -2,12 +2,4 @@
 // { dg-do compile { target c++11 } }
 // { dg-options "-Og -flive-range-shrinkage -fnon-call-exceptions" }
 
-struct Ax {
-  int n, a[];
-};
-
-int i = 12345678;
-int main() {
-  static Ax s{456, i};
-  ((s.a[0]) ? (void)0 : (void)0);
-}
+#include "../torture/pr88861.C"
