49c45748e747a2ccad0ed00db88b47f23d4bd5b5
Thomas Koenig <tkoenig@gcc.gnu.org>
2019-02-10 18:30:01 +0000

    re PR fortran/71723 ([F08] ICE on invalid pointer initialization)
    2019-02-10  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/71237
    	Backport from trunk
    	* expr.c (gfc_check_assign): Add argument is_init_expr.  If we are
    	looking at an init expression, issue error if the target is not a
    	TARGET and we are not looking at a procedure pointer.
    	* gfortran.h (gfc_check_assign): Add optional argument
    	is_init_expr.
    
    2019-02-10  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/71237
    	Backport from trunk
    	* gfortran.dg/pointer_init_2.f90: Adjust error messages.
    	* gfortran.dg/pointer_init_6.f90: Likewise.
    	* gfortran.dg/pointer_init_9.f90: New test.
    
    From-SVN: r268750

--- gcc/fortran/gfortran.h
+++ gcc/fortran/gfortran.h
@@ -3200,7 +3200,7 @@ int gfc_kind_max (gfc_expr *, gfc_expr *);
 
 bool gfc_check_conformance (gfc_expr *, gfc_expr *, const char *, ...) ATTRIBUTE_PRINTF_3;
 bool gfc_check_assign (gfc_expr *, gfc_expr *, int, bool c = true);
-bool gfc_check_pointer_assign (gfc_expr *, gfc_expr *);
+bool gfc_check_pointer_assign (gfc_expr *, gfc_expr *, bool is_init_expr = false);
 bool gfc_check_assign_symbol (gfc_symbol *, gfc_component *, gfc_expr *);
 
 gfc_expr *gfc_build_default_init_expr (gfc_typespec *, locus *);
