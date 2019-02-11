dd9bf21a80f9a035fa8667abf80b02b167a4e015
tnfchris <tnfchris@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-02-11 16:54:18 +0000

    Arm: Update tests after register allocation changes. (PR/target 88560)
    After the register allocator changes of r268705 we need to update a few tests
    with new output.
    
    In all cases the compiler is now generating the expected code, since the tests
    are all float16 testcases using a hard-floar abi, we expect that actual fp16
    instructions are used rather than using integer loads and stores.  Because of
    we also save on some mov.f16s that were being emitted before to move between
    the two.
    
    The aapcs cases now match the f32 cases in using floating point operations.
    
    gcc/testsuite/Changelog
    
    	PR middle-end/88560
    	* gcc.target/arm/armv8_2-fp16-move-1.c: Update assembler scans.
    	* gcc.target/arm/fp16-aapcs-1.c: Likewise.
    	* gcc.target/arm/fp16-aapcs-3.c: Likewise.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@268772 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/fp16-aapcs-1.c
+++ gcc/testsuite/gcc.target/arm/fp16-aapcs-1.c
@@ -16,6 +16,7 @@ F (__fp16 a, __fp16 b, __fp16 c)
   return c;
 }
 
-/* { dg-final { scan-assembler {vmov(\.f16)?\tr[0-9]+, s[0-9]+} } }  */
-/* { dg-final { scan-assembler {vmov(\.f32)?\ts1, s0} } }  */
-/* { dg-final { scan-assembler {vmov(\.f16)?\ts0, r[0-9]+} } }  */
+/* { dg-final { scan-assembler {vmov\.f32\ts[0-9]+, s1} } }  */
+/* { dg-final { scan-assembler {vmov\.f32\ts1, s0} } }  */
+/* { dg-final { scan-assembler {vmov\.f32\ts[0-9]+, s2+} } }  */
+/* { dg-final { scan-assembler-times {vmov\.f32\ts0, s[0-9]+} 2 } }  */
