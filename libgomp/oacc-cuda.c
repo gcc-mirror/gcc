5c357d4f2c835f9b4f432ab03847beb11aec8888
Julian Brown <julian@codesourcery.com>
2019-02-12 06:36:03 -0800

    Tweak error return value for acc_set_cuda_stream.
    The return value of acc_set_cuda_stream is unspecified in OpenACC 2.6.
    The testsuite changes might be unnecessary with the current async code.
    
    	libgomp/
    	* oacc-cuda.c (acc_set_cuda_stream): Return 0 on error/invalid
    	arguments.
    	* testsuite/libgomp.oacc-c-c++-common/lib-84.c: Handle unnumbered
    	async stream being an alias for a numbered async stream.
    	* testsuite/libgomp.oacc-c-c++-common/lib-85.c: Likewise.

--- libgomp/oacc-cuda.c
+++ libgomp/oacc-cuda.c
@@ -115,6 +115,9 @@ acc_get_cuda_stream (int async)
   return ret;
 }
 
+/* As of OpenACC 2.6, the return code of this function appears to be
+   unspecified.  We choose to return 1 for success, or 0 for failure.  */
+
 int
 acc_set_cuda_stream (int async, void *stream)
 {
@@ -127,7 +130,7 @@ acc_set_cuda_stream (int async, void *stream)
 
   thr = goacc_thread ();
 
-  int ret = -1;
+  int ret = 0;
   if (thr && thr->dev && thr->dev->openacc.cuda.set_stream_func)
     {
       acc_prof_info prof_info;
