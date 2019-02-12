3a5e525489f2f808093ae1f12b5d2b406f571ec7
Julian Brown <julian@codesourcery.com>
2019-02-12 14:56:12 -0800

    Various OpenACC reduction enhancements - FE changes
    This version differs somewhat from the last version posted upstream
    (and addresses some of Jakub's review comments).
    
    2018-12-13  Cesar Philippidis  <cesar@codesourcery.com>
    	    Nathan Sidwell  <nathan@acm.org>
    	    Julian Brown  <julian@codesourcery.com>
    
    	gcc/c/
    	* c-parser.cc (c_parser_omp_variable_list): New c_omp_region_type
    	argument.  Use it to specialize handling of OMP_CLAUSE_REDUCTION for
    	OpenACC.
    	(c_parser_oacc_data_clause): Add region-type argument.
    	(c_parser_oacc_data_clause_deviceptr): Likewise.
    	(c_parser_omp_clause_reduction): Change is_omp boolean parameter to
    	c_omp_region_type.  Update call to c_parser_omp_variable_list.
    	(c_parser_oacc_all_clauses): Update calls to
    	c_parser_omp_clause_reduction.
    	(c_parser_omp_all_clauses): Likewise.
    	(c_parser_oacc_cache): Update call to c_parser_omp_var_list_parens.
    	* c-typeck.cc (c_finish_omp_clauses): Emit an error on orphan OpenACC
    	gang reductions.  Suppress user-defined reduction error for OpenACC.
    
    	gcc/cp/
    	* parser.cc (cp_parser_omp_var_list_no_open):  New c_omp_region_type
    	argument.  Use it to specialize handling of OMP_CLAUSE_REDUCTION for
    	OpenACC.
    	(cp_parser_omp_var_list): Add c_omp_region_type argument. Update call
    	to cp_parser_omp_var_list_parens.
    	(cp_parser_oacc_data_clause): Update call to cp_parser_omp_var_list.
    	(cp_parser_omp_clause_reduction): Change is_omp boolean parameter to
    	c_omp_region_type.  Update call to cp_parser_omp_var_list_no_open.
    	(cp_parser_oacc_all_clauses): Update call to
    	cp_parser_omp_clause_reduction.
    	(cp_parser_omp_all_clauses): Likewise.
    	* semantics.cc (finish_omp_reduction_clause): Add c_omp_region_type
    	argument.  Suppress user-defined reduction error for OpenACC.
    	(finish_omp_clauses): Emit an error on orphan OpenACC gang reductions.
    
    	gcc/fortran/
    	* openmp.cc (oacc_is_parallel): New.
    	(resolve_oacc_loop_blocks): Emit an error on orphan OpenACC
    	gang reductions.
    	* trans-openmp.cc (gfc_omp_clause_copy_ctor): Permit reductions.
    
    2022-02-03  Kwok Cheung Yeung  <kcy@codesourcery.com>
    
    	gcc/c/
    	* c-parser.cc (c_parser_omp_clause_map): Update call to
    	c_parser_omp_variable_list.
    	(c_parser_omp_clause_to): Update call to c_parser_omp_var_list_parens.
    	(c_parser_omp_clause_from): Likewise.
    
    	gcc/cp/
    	* parser.cc (cp_parser_omp_clause_map): Update call to
    	cp_parser_omp_var_list_no_open.
    	(cp_parser_omp_all_clauses): Update calls to cp_parser_omp_var_list.

--- gcc/cp/semantics.cc
+++ gcc/cp/semantics.cc
@@ -6183,7 +6183,8 @@ find_omp_placeholder_r (tree *tp, int *, void *data)
    Return true if there is some error and the clause should be removed.  */
 
 static bool
-finish_omp_reduction_clause (tree c, bool *need_default_ctor, bool *need_dtor)
+finish_omp_reduction_clause (tree c, enum c_omp_region_type ort,
+			     bool *need_default_ctor, bool *need_dtor)
 {
   tree t = OMP_CLAUSE_DECL (c);
   bool predefined = false;
@@ -6429,9 +6430,11 @@ finish_omp_reduction_clause (tree c, bool *need_default_ctor, bool *need_dtor)
     *need_dtor = true;
   else
     {
-      error_at (OMP_CLAUSE_LOCATION (c),
-		"user defined reduction not found for %qE",
-		omp_clause_printable_decl (t));
+      /* There are no user-defined reductions for OpenACC (as of 2.6).  */
+      if (ort & C_ORT_OMP)
+	error_at (OMP_CLAUSE_LOCATION (c),
+		  "user defined reduction not found for %qE",
+		  omp_clause_printable_decl (t));
       return true;
     }
   if (TREE_CODE (OMP_CLAUSE_DECL (c)) == MEM_REF)
@@ -6728,6 +6731,7 @@ finish_omp_clauses (tree clauses, enum c_omp_region_type ort)
   bool mergeable_seen = false;
   bool implicit_moved = false;
   bool target_in_reduction_seen = false;
+  bool oacc_gang_seen = false;
 
   bitmap_obstack_initialize (NULL);
   bitmap_initialize (&generic_head, &bitmap_default_obstack);
@@ -6745,10 +6749,15 @@ finish_omp_clauses (tree clauses, enum c_omp_region_type ort)
 
   if (ort & C_ORT_ACC)
     for (c = clauses; c; c = OMP_CLAUSE_CHAIN (c))
-      if (OMP_CLAUSE_CODE (c) == OMP_CLAUSE_ASYNC)
-	{
+      switch (OMP_CLAUSE_CODE (c))
+        {
+	case OMP_CLAUSE_ASYNC:
 	  oacc_async = true;
 	  break;
+	case OMP_CLAUSE_GANG:
+	  oacc_gang_seen = true;
+	  break;
+	default:;
 	}
 
   for (pc = &clauses, c = clauses; c ; c = *pc)
@@ -6765,6 +6774,13 @@ finish_omp_clauses (tree clauses, enum c_omp_region_type ort)
 	  field_ok = ((ort & C_ORT_OMP_DECLARE_SIMD) == C_ORT_OMP);
 	  goto check_dup_generic;
 	case OMP_CLAUSE_REDUCTION:
+	  if (oacc_gang_seen && oacc_get_fn_attrib (current_function_decl))
+	    {
+	      error_at (OMP_CLAUSE_LOCATION (c),
+			"gang reduction on an orphan loop");
+	      remove = true;
+	      break;
+	    }
 	  if (reduction_seen == 0)
 	    reduction_seen = OMP_CLAUSE_REDUCTION_INSCAN (c) ? -1 : 1;
 	  else if (reduction_seen != -2
@@ -8935,7 +8951,7 @@ finish_omp_clauses (tree clauses, enum c_omp_region_type ort)
 	  if (processing_template_decl
 	      && !VAR_P (t) && TREE_CODE (t) != PARM_DECL)
 	    break;
-	  if (finish_omp_reduction_clause (c, &need_default_ctor,
+	  if (finish_omp_reduction_clause (c, ort, &need_default_ctor,
 					   &need_dtor))
 	    remove = true;
 	  else
