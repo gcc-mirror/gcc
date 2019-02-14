31de5e4d91c8b348dd23ed2042983b9b057c882c
Jakub Jelinek <jakub@redhat.com>
2019-02-14 08:31:14 +0100

    re PR middle-end/89284 (gcc -fsanitize=undefined inhibits -Wuninitialized)
    	PR middle-end/89284
    	* passes.def: Swap pass_ubsan and pass_early_warn_uninitialized.
    
    	* gcc.dg/ubsan/pr89284.c: New test.
    
    From-SVN: r268862

---
+++ gcc/testsuite/gcc.dg/ubsan/pr89284.c
@@ -0,0 +1,23 @@
+/* PR middle-end/89284 */
+/* { dg-do compile } */
+/* { dg-options "-fsanitize=undefined -O0 -Wuninitialized" } */
+
+struct A { _Bool a; int i; };
+
+int
+foo (void)
+{
+  struct A a;
+  if (a.i)	/* { dg-warning "'a.i' is used uninitialized in this function" } */
+    return 1;
+  return 0;
+}
+
+int
+bar (void)
+{
+  struct A a;
+  if (a.a)	/* { dg-warning "'a.a' is used uninitialized in this function" } */
+    return 1;
+  return 0;
+}
