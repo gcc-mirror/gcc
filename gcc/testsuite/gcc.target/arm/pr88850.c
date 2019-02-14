07a9d3db682994d92fc0f67b5efd6fe3254de7bc
tnfchris <tnfchris@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-02-14 18:29:06 +0000

    Arm: Fix testism in pr88850 testcase.
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/arm/pr88850.c: change options to additional option.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@268887 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/pr88850.c
+++ gcc/testsuite/gcc.target/arm/pr88850.c
@@ -1,6 +1,6 @@
 /* PR target/88850 */
 /* { dg-do compile } */
-/* { dg-options "-O2 -march=armv7-a -mfloat-abi=softfp -mfpu=neon -fdump-rtl-final" } */
+/* { dg-additional-options "-O2 -march=armv7-a -mfloat-abi=softfp -mfpu=neon -fdump-rtl-final" } */
 /* { dg-require-effective-target arm_neon_ok } */
 
 typedef __builtin_neon_qi int8x8_t __attribute__ ((__vector_size__ (8)));
