9703fa32c66189af1959731bab12b3751189f900
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-02-15 10:50:27 +0000

    * c-c++-common/patchable_function_entry-decl.c: Do not run on Visium. 	* c-c++-common/patchable_function_entry-default.c: Likewise. 	* c-c++-common/patchable_function_entry-definition.c: Likewise. 	* gcc.dg/tree-ssa/pr84859.c: Add -ftree-cselim switch.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@268932 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/c-c++-common/patchable_function_entry-default.c
+++ gcc/testsuite/c-c++-common/patchable_function_entry-default.c
@@ -1,6 +1,5 @@
-/* { dg-do compile { target { ! nvptx*-*-* } } } */
+/* { dg-do compile { target { ! { nvptx*-*-* visium-*-* } } } } */
 /* { dg-options "-O2 -fpatchable-function-entry=3,1" } */
-/* { dg-additional-options "-mcpu=gr6" { target visium-*-* } }
 /* { dg-final { scan-assembler-times "nop|NOP" 3 { target { ! { alpha*-*-* } } } } } */
 /* { dg-final { scan-assembler-times "bis" 3 { target alpha*-*-* } } } */
 
