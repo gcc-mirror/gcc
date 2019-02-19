e8350f09ab8413b550fa3fa1720d1e5038eb5277
Wilco Dijkstra <wdijkstr@arm.com>
2019-02-19 18:43:43 +0000

    Fix pr88850 test
    Fix pr88850.c testcase which was failing in hardfp environments.
    
    Committed as obvious.
    
        gcc/testsuite/
    	* gcc.target/arm/pr88850.c: Block -mfloat-abi override.
    
    From-SVN: r269026

--- gcc/testsuite/gcc.target/arm/pr88850.c
+++ gcc/testsuite/gcc.target/arm/pr88850.c
@@ -1,6 +1,7 @@
 /* PR target/88850 */
 /* { dg-do compile } */
-/* { dg-additional-options "-O2 -march=armv7-a -mfloat-abi=softfp -mfpu=neon -fdump-rtl-final" } */
+/* { dg-skip-if "do not override -mfloat-abi" { *-*-* } { "-mfloat-abi=*" } {"-mfloat-abi=soft" } } */
+/* { dg-options "-O2 -march=armv7-a -mfloat-abi=softfp -mfpu=neon -fdump-rtl-final" } */
 /* { dg-require-effective-target arm_neon_ok } */
 
 typedef __builtin_neon_qi int8x8_t __attribute__ ((__vector_size__ (8)));
