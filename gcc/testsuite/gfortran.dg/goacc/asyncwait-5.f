3b8f48d6f009cbd22b39966413421ad3a9e17ee3
Thomas Schwinge <thomas@codesourcery.com>
2019-02-19 17:04:17 +0100

    [PR87924] OpenACC wait clauses without async-arguments: remove XFAILs
    ... which the recent r269016 didn't do.
    
    	gcc/testsuite/
    	PR c/87924
    	* c-c++-common/goacc/asyncwait-5.c: Remove XFAILs.
    	* gfortran.dg/goacc/asyncwait-5.f: Likewise.
    
    From-SVN: r269020

--- gcc/testsuite/gfortran.dg/goacc/asyncwait-5.f
+++ gcc/testsuite/gfortran.dg/goacc/asyncwait-5.f
@@ -12,22 +12,22 @@
 
 !$ACC PARALLEL ASYNC (3) WAIT
 !$ACC END PARALLEL
-! { dg-final { scan-tree-dump-times "(?n)#pragma acc parallel async\\(3\\) wait\\(-1\\)$" 1 "original" { xfail *-*-* } } }
+! { dg-final { scan-tree-dump-times "(?n)#pragma acc parallel async\\(3\\) wait\\(-1\\)$" 1 "original" } }
 
 !$ACC PARALLEL ASYNC (4) WAIT (100) WAIT
 !$ACC END PARALLEL
-! { dg-final { scan-tree-dump-times "(?n)#pragma acc parallel async\\(4\\) wait\\(100\\) wait\\(-1\\)$" 1 "original" { xfail *-*-* } } }
+! { dg-final { scan-tree-dump-times "(?n)#pragma acc parallel async\\(4\\) wait\\(100\\) wait\\(-1\\)$" 1 "original" } }
 
 !$ACC PARALLEL ASYNC (5) WAIT WAIT (101)
 !$ACC END PARALLEL
-! { dg-final { scan-tree-dump-times "(?n)#pragma acc parallel async\\(5\\) wait\\(-1\\) wait\\(101\\)$" 1 "original" { xfail *-*-* } } }
+! { dg-final { scan-tree-dump-times "(?n)#pragma acc parallel async\\(5\\) wait\\(-1\\) wait\\(101\\)$" 1 "original" } }
 
 !$ACC PARALLEL ASYNC (6) WAIT WAIT (102, 103) WAIT WAIT
 !$ACC END PARALLEL
-! { dg-final { scan-tree-dump-times "(?n)#pragma acc parallel async\\(6\\) wait\\(-1\\) wait\\(102\\) wait\\(103\\) wait\\(-1\\) wait\\(-1\\)$" 1 "original" { xfail *-*-* } } }
+! { dg-final { scan-tree-dump-times "(?n)#pragma acc parallel async\\(6\\) wait\\(-1\\) wait\\(102\\) wait\\(103\\) wait\\(-1\\) wait\\(-1\\)$" 1 "original" } }
 
 !$ACC PARALLEL ASYNC (7) WAIT (104) WAIT WAIT (105, 106)
 !$ACC END PARALLEL
-! { dg-final { scan-tree-dump-times "(?n)#pragma acc parallel async\\(7\\) wait\\(104\\) wait\\(-1\\) wait\\(105\\) wait\\(106\\)$" 1 "original" { xfail *-*-* } } }
+! { dg-final { scan-tree-dump-times "(?n)#pragma acc parallel async\\(7\\) wait\\(104\\) wait\\(-1\\) wait\\(105\\) wait\\(106\\)$" 1 "original" } }
 
       END
