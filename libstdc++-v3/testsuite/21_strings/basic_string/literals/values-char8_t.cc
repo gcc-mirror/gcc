4acc5060446fe653f823973bd0fda52ac09d461b
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-02-19 02:55:05 +0000

    P0482R5 char8_t: New standard library tests
    2019-02-19  Tom Honermann  <tom@honermann.net>
    
    	* testsuite/18_support/numeric_limits/char8_t.cc: New test cloned
    	from char16_32_t.cc; validates numeric_limits<char8_t>.
    	* testsuite/21_strings/basic_string/literals/types-char8_t.cc: New
    	test cloned from types.cc; validates operator""s for char8_t
    	returns u8string.
    	* testsuite/21_strings/basic_string/literals/values-char8_t.cc: New
    	test cloned from values.cc; validates construction and comparison
    	of u8string values.
    	* testsuite/21_strings/basic_string/requirements/
    	/explicit_instantiation/char8_t/1.cc: New test cloned from
    	char16_t/1.cc; validates explicit instantiation of
    	basic_string<char8_t>.
    	* testsuite/21_strings/basic_string_view/literals/types-char8_t.cc:
    	New test cloned from types.cc; validates operator""sv for char8_t
    	returns u8string_view.
    	* testsuite/21_strings/basic_string_view/literals/
    	values-char8_t.cc: New test cloned from values.cc; validates
    	construction and comparison of u8string_view values.
    	* testsuite/21_strings/basic_string_view/requirements/
    	explicit_instantiation/char8_t/1.cc: New test cloned from
    	char16_t/1.cc; validates explicit instantiation of
    	basic_string_view<char8_t>.
    	* testsuite/21_strings/char_traits/requirements/char8_t/65049.cc:
    	New test cloned from char16_t/65049.cc; validates that
    	char_traits<char8_t> is not vulnerable to the concerns in PR65049.
    	* testsuite/21_strings/char_traits/requirements/char8_t/
    	typedefs.cc: New test cloned from char16_t/typedefs.cc; validates
    	that char_traits<char8_t> member typedefs are present and correct.
    	* testsuite/21_strings/char_traits/requirements/
    	explicit_instantiation/char8_t/1.cc: New test cloned from
    	char16_t/1.cc; validates explicit instantiation of
    	char_traits<char8_t>.
    	* testsuite/22_locale/codecvt/char16_t-char8_t.cc: New test cloned
    	from char16_t.cc: validates
    	codecvt<char16_t, char8_t, mbstate_t>.
    	* testsuite/22_locale/codecvt/char32_t-char8_t.cc: New test cloned
    	from char32_t.cc: validates
    	codecvt<char32_t, char8_t, mbstate_t>.
    	* testsuite/22_locale/codecvt/utf8-char8_t.cc: New test cloned from
    	utf8.cc; validates codecvt<char16_t, char8_t, std::mbstate_t> and
    	codecvt<char32_t, char8_t, std::mbstate_t>.
    	* testsuite/27_io/filesystem/path/native/string-char8_t.cc: New
    	test cloned from string.cc; validates filesystem::path construction
    	from char8_t input.
    	* testsuite/experimental/feat-char8_t.cc: New test; validates that
    	the __cpp_lib_char8_t feature test macro is defined with the
    	correct value.
    	* testsuite/experimental/filesystem/path/native/string-char8_t.cc:
    	New test cloned from string.cc; validates filesystem::path
    	construction from char8_t input.
    	* testsuite/experimental/string_view/literals/types-char8_t.cc: New
    	test cloned from types.cc; validates operator""sv for char8_t
    	returns u8string_view.
    	* testsuite/experimental/string_view/literals/values-char8_t.cc:
    	New test cloned from values.cc; validates construction and
    	comparison of u8string_view values.
    	* testsuite/experimental/string_view/requirements/
    	explicit_instantiation/char8_t/1.cc: New test cloned from
    	char16_t/1.cc; validates explicit instantiation of
    	basic_string_view<char8_t>.
    	* testsuite/ext/char8_t/atomic-1.cc: New test; validates that
    	ATOMIC_CHAR8_T_LOCK_FREE is not defined if char8_t support is not
    	enabled.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@269005 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/21_strings/basic_string/literals/values-char8_t.cc
@@ -0,0 +1,58 @@
+// { dg-do run { target c++14 } }
+// { dg-options "-fchar8_t" }
+
+// Copyright (C) 2013-2017 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <string>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  using namespace std::literals::string_literals;
+
+  std::string planet = "Mercury"s;
+#ifdef _GLIBCXX_USE_WCHAR_T
+  std::wstring wplanet = L"Venus"s;
+#endif
+#ifdef _GLIBCXX_USE_CHAR8_T
+  std::u8string u8planet = u8"Mars"s;
+#else
+  std::string u8planet = u8"Mars"s;
+#endif
+  std::u16string u16planet = u"Jupiter"s;
+  std::u32string u32planet = U"Saturn"s;
+
+  VERIFY( planet == std::string("Mercury") );
+#ifdef _GLIBCXX_USE_WCHAR_T
+  VERIFY( wplanet == std::wstring(L"Venus") );
+#endif
+#ifdef _GLIBCXX_USE_CHAR8_T
+  VERIFY( u8planet == std::u8string(u8"Mars") );
+#else
+  VERIFY( u8planet == std::string(u8"Mars") );
+#endif
+  VERIFY( u16planet == std::u16string(u"Jupiter") );
+  VERIFY( u32planet == std::u32string(U"Saturn") );
+}
+
+int
+main()
+{
+  test01();
+}
