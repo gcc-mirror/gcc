46ca1dd73c97c5bf85057d450adf4b885a427e77
Tom Honermann <tom@honermann.net>
2019-02-19 02:55:05 +0000

    P0482R5 char8_t: New standard library tests
    2019-02-19  Tom Honermann  <tom@honermann.net>
    
    	* testsuite/18_support/numeric_limits/char8_t.cc: New test cloned
    	from char16_32_t.cc; validates numeric_limits<char8_t>.
    	* testsuite/21_strings/basic_string/literals/types-char8_t.cc: New
    	test cloned from types.cc; validates operator""s for char8_t
    	returns u8string.
    	* testsuite/21_strings/basic_string/literals/values-char8_t.cc: New
    	test cloned from values.cc; validates construction and comparison
    	of u8string values.
    	* testsuite/21_strings/basic_string/requirements/
    	/explicit_instantiation/char8_t/1.cc: New test cloned from
    	char16_t/1.cc; validates explicit instantiation of
    	basic_string<char8_t>.
    	* testsuite/21_strings/basic_string_view/literals/types-char8_t.cc:
    	New test cloned from types.cc; validates operator""sv for char8_t
    	returns u8string_view.
    	* testsuite/21_strings/basic_string_view/literals/
    	values-char8_t.cc: New test cloned from values.cc; validates
    	construction and comparison of u8string_view values.
    	* testsuite/21_strings/basic_string_view/requirements/
    	explicit_instantiation/char8_t/1.cc: New test cloned from
    	char16_t/1.cc; validates explicit instantiation of
    	basic_string_view<char8_t>.
    	* testsuite/21_strings/char_traits/requirements/char8_t/65049.cc:
    	New test cloned from char16_t/65049.cc; validates that
    	char_traits<char8_t> is not vulnerable to the concerns in PR65049.
    	* testsuite/21_strings/char_traits/requirements/char8_t/
    	typedefs.cc: New test cloned from char16_t/typedefs.cc; validates
    	that char_traits<char8_t> member typedefs are present and correct.
    	* testsuite/21_strings/char_traits/requirements/
    	explicit_instantiation/char8_t/1.cc: New test cloned from
    	char16_t/1.cc; validates explicit instantiation of
    	char_traits<char8_t>.
    	* testsuite/22_locale/codecvt/char16_t-char8_t.cc: New test cloned
    	from char16_t.cc: validates
    	codecvt<char16_t, char8_t, mbstate_t>.
    	* testsuite/22_locale/codecvt/char32_t-char8_t.cc: New test cloned
    	from char32_t.cc: validates
    	codecvt<char32_t, char8_t, mbstate_t>.
    	* testsuite/22_locale/codecvt/utf8-char8_t.cc: New test cloned from
    	utf8.cc; validates codecvt<char16_t, char8_t, std::mbstate_t> and
    	codecvt<char32_t, char8_t, std::mbstate_t>.
    	* testsuite/27_io/filesystem/path/native/string-char8_t.cc: New
    	test cloned from string.cc; validates filesystem::path construction
    	from char8_t input.
    	* testsuite/experimental/feat-char8_t.cc: New test; validates that
    	the __cpp_lib_char8_t feature test macro is defined with the
    	correct value.
    	* testsuite/experimental/filesystem/path/native/string-char8_t.cc:
    	New test cloned from string.cc; validates filesystem::path
    	construction from char8_t input.
    	* testsuite/experimental/string_view/literals/types-char8_t.cc: New
    	test cloned from types.cc; validates operator""sv for char8_t
    	returns u8string_view.
    	* testsuite/experimental/string_view/literals/values-char8_t.cc:
    	New test cloned from values.cc; validates construction and
    	comparison of u8string_view values.
    	* testsuite/experimental/string_view/requirements/
    	explicit_instantiation/char8_t/1.cc: New test cloned from
    	char16_t/1.cc; validates explicit instantiation of
    	basic_string_view<char8_t>.
    	* testsuite/ext/char8_t/atomic-1.cc: New test; validates that
    	ATOMIC_CHAR8_T_LOCK_FREE is not defined if char8_t support is not
    	enabled.
    
    From-SVN: r269005

---
+++ libstdc++-v3/testsuite/18_support/numeric_limits/char8_t.cc
@@ -0,0 +1,71 @@
+// { dg-do run { target c++11 } }
+// { dg-require-cstdint "" }
+// { dg-options "-fchar8_t" }
+
+// Copyright (C) 2017 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <limits>
+#include <cstdint>
+#include <testsuite_hooks.h>
+
+// Test specializations for char8_t.
+template<typename T, typename R>
+  void
+  do_test()
+  {
+    typedef std::numeric_limits<T> char_type;
+    typedef std::numeric_limits<R> impl_type;
+
+    VERIFY( char_type::is_specialized == impl_type::is_specialized );
+    VERIFY( char_type::min() == impl_type::min() );
+    VERIFY( char_type::max() == impl_type::max() );
+    VERIFY( char_type::digits == impl_type::digits );
+    VERIFY( char_type::digits10 == impl_type::digits10 );
+    VERIFY( char_type::is_signed == impl_type::is_signed );
+    VERIFY( char_type::is_integer == impl_type::is_integer );
+    VERIFY( char_type::is_exact == impl_type::is_exact );
+    VERIFY( char_type::radix == impl_type::radix );
+    VERIFY( char_type::epsilon() == impl_type::epsilon() );
+    VERIFY( char_type::round_error() == impl_type::round_error() );
+    VERIFY( char_type::min_exponent == impl_type::min_exponent );
+    VERIFY( char_type::min_exponent10 == impl_type::min_exponent10 );
+    VERIFY( char_type::max_exponent == impl_type::max_exponent );
+    VERIFY( char_type::max_exponent10 == impl_type::max_exponent10 );
+    VERIFY( char_type::has_infinity == impl_type::has_infinity );
+    VERIFY( char_type::has_quiet_NaN == impl_type::has_quiet_NaN );
+    VERIFY( char_type::has_signaling_NaN == impl_type::has_signaling_NaN );
+    VERIFY( char_type::has_denorm == impl_type::has_denorm );
+    VERIFY( char_type::has_denorm_loss == impl_type::has_denorm_loss );
+    VERIFY( char_type::infinity() == impl_type::infinity() );
+    VERIFY( char_type::quiet_NaN() == impl_type::quiet_NaN() );
+    VERIFY( char_type::signaling_NaN() == impl_type::signaling_NaN() );
+    VERIFY( char_type::denorm_min() == impl_type::denorm_min() );
+    VERIFY( char_type::is_iec559 == impl_type::is_iec559 );
+    VERIFY( char_type::is_bounded == impl_type::is_bounded );
+    VERIFY( char_type::is_modulo == impl_type::is_modulo );
+    VERIFY( char_type::traps == impl_type::traps );
+    VERIFY( char_type::tinyness_before == impl_type::tinyness_before );
+    VERIFY( char_type::round_style == impl_type::round_style );
+  }
+
+int main()
+{
+  do_test<char8_t, unsigned char>();
+
+  return 0;
+}
