d331c5f10d5e6c9f41a24ff7cb7a8c6493790885
Jonathan Wakely <jwakely@redhat.com>
2019-02-21 20:47:43 +0000

    PR libstdc++/89416 fix __is_move_insertable trait
    The common base class for __is_move_insertable and __is_copy_insertable
    instantiates both the copy and move tests, when only one is needed. The
    unneeded one might cause errors outside the immediate context.
    
    The solution used in this patch is to replace them with alias templates,
    which will only be instantiated as needed.
    
    	PR libstdc++/89416
    	* include/bits/alloc_traits.h (__is_alloc_insertable_impl): Replace
    	class template with class. Replace move and copy member types with
    	member alias templates, so they are only instantiated when needed.
    	(__is_copy_insertable, __is_move_insertable): Adjust base class.
    	* testsuite/23_containers/vector/modifiers/push_back/89130.cc: Enable
    	test for C++11/14/17 as well.
    	* testsuite/23_containers/vector/modifiers/push_back/89416.cc: New
    	test.
    
    From-SVN: r269075

---
+++ libstdc++-v3/testsuite/23_containers/vector/modifiers/push_back/89416.cc
@@ -0,0 +1,44 @@
+// Copyright (C) 2019 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile { target c++11 } }
+
+// PR libstdc++/89416
+
+#include <vector>
+
+template<typename T>
+  struct Alloc : std::allocator<T>
+  {
+    using std::allocator<T>::allocator;
+
+    template<typename U>
+      struct rebind { using other = Alloc<U>;  };
+  };
+
+struct X
+{
+  X(int);
+  X(X&&);
+};
+
+void test01()
+{
+  std::vector<X, Alloc<X>> V;
+  V.push_back(X(1));
+  V.emplace_back(1);
+}
