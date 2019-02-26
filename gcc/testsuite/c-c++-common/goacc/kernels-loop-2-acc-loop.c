30025fc576f5bca8901ab4cceb328e03f1f783ae
Julian Brown <julian@codesourcery.com>
2019-02-26 13:39:03 -0800

    Generate sequential loop for OpenACC loop directive inside kernels
    2019-09-20  Chung-Lin Tang <cltang@codesourcery.com>
    	    Cesar Philippidis  <cesar@codesourcery.com>
    
    	gcc/
    	* omp-expand.cc (struct omp_region): Add inside_kernels_p field.
    	(expand_omp_for_generic): Adjust to generate a 'sequential' loop
    	when GOMP builtin arguments are BUILT_IN_NONE.
    	(expand_omp_for): Use expand_omp_for_generic to generate a
    	non-parallelized loop for OMP_FORs inside OpenACC kernels regions.
    	(expand_omp): Mark inside_kernels_p field true for regions
    	nested inside OpenACC kernels constructs.
    	gcc/testsuite/
    	* c-c++-common/goacc/kernels-loop-acc-loop.c: New test.
    	* c-c++-common/goacc/kernels-loop-2-acc-loop.c: New test.
    	* c-c++-common/goacc/kernels-loop-3-acc-loop.c: New test.
    	* c-c++-common/goacc/kernels-loop-n-acc-loop.c: New test.
    	* c-c++-common/goacc/kernels-acc-loop-reduction.c: New test.
    	* c-c++-common/goacc/kernels-acc-loop-smaller-equal.c: New test.

---
+++ gcc/testsuite/c-c++-common/goacc/kernels-loop-2-acc-loop.c
@@ -0,0 +1,18 @@
+/* { dg-additional-options "-O2" } */
+/* { dg-additional-options "-fdump-tree-parloops1-all" } */
+/* { dg-additional-options "-fdump-tree-optimized" } */
+
+/* Check that loops with '#pragma acc loop' tagged gets properly parallelized.  */
+#define ACC_LOOP
+#include "kernels-loop-2.c"
+
+/* Check that only three loops are analyzed, and that all can be
+   parallelized.  */
+/* { dg-final { scan-tree-dump-times "SUCCESS: may be parallelized" 3 "parloops1" } } */
+/* { dg-final { scan-tree-dump-times "(?n)__attribute__\\(\\(oacc kernels parallelized, oacc function \\(, , \\), oacc kernels, omp target entrypoint\\)\\)" 3 "parloops1" } } */
+/* { dg-final { scan-tree-dump-not "FAILED:" "parloops1" } } */
+
+/* Check that the loop has been split off into a function.  */
+/* { dg-final { scan-tree-dump-times "(?n);; Function .*main._omp_fn.0" 1 "optimized" } } */
+/* { dg-final { scan-tree-dump-times "(?n);; Function .*main._omp_fn.1" 1 "optimized" } } */
+/* { dg-final { scan-tree-dump-times "(?n);; Function .*main._omp_fn.2" 1 "optimized" } } */
