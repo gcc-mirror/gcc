ac9ef95aece80fe25f72800f281f73c18150c5de
Julian Brown <julian@codesourcery.com>
2019-02-26 14:22:41 -0800

    Fix implicit mapping for array slices on lexically-enclosing data constructs (PR70828)
    2018-08-28  Julian Brown  <julian@codesourcery.com>
                Cesar Philippidis  <cesar@codesourcery.com>
    
    	gcc/
    	* gimplify.c (oacc_array_mapping_info): New struct.
    	(gimplify_omp_ctx): Add decl_data_clause hash map.
    	(new_omp_context): Zero-initialise above.
    	(delete_omp_context): Delete above if allocated.
    	(gimplify_scan_omp_clauses): Scan for array mappings on data constructs,
    	and record in above map.
    	(gomp_oacc_needs_data_present): New function.
    	(gimplify_adjust_omp_clauses_1): Handle data mappings (e.g. array
    	slices) declared in lexically-enclosing data constructs.
    	* omp-low.c (lower_omp_target): Allow decl for bias not to be present
    	in OpenACC context.
    
    	gcc/testsuite/
    	* c-c++-common/goacc/acc-data-chain.c: New test.
    	* gfortran.dg/goacc/pr70828.f90: New test.
    	* gfortran.dg/goacc/pr70828-2.f90: New test.
    
    	libgomp/
    	* testsuite/libgomp.oacc-c-c++-common/pr70828.c: New test.
    	* testsuite/libgomp.oacc-fortran/implicit_copy.f90: New test.
    	* testsuite/libgomp.oacc-fortran/pr70828.f90: New test.
    	* testsuite/libgomp.oacc-fortran/pr70828-2.f90: New test.
    	* testsuite/libgomp.oacc-fortran/pr70828-3.f90: New test.
    	* testsuite/libgomp.oacc-fortran/pr70828-4.f90: New test.
    	* testsuite/libgomp.oacc-fortran/pr70828-5.f90: New test.
    	* testsuite/libgomp.oacc-fortran/pr70828-6.f90: New test.

---
+++ libgomp/testsuite/libgomp.oacc-fortran/pr70828-6.f90
@@ -0,0 +1,28 @@
+! Subarrays declared on data construct: allocatable array (with array
+! descriptor).
+
+program test
+  integer, parameter :: n = 100
+  integer i
+  integer, allocatable :: data(:)
+
+  allocate (data(1:n))
+
+  data(:) = 0
+
+  !$acc data copy(data(5:n-10))
+  !$acc parallel loop
+  do i = 10, n - 10
+     data(i) = i
+  end do
+  !$acc end parallel loop
+  !$acc end data
+
+  do i = 1, n
+     if ((i < 10 .or. i > n-10)) then
+        if ((data(i) .ne. 0)) call abort
+     else if (data(i) .ne. i) then
+        call abort
+     end if
+  end do
+end program test
