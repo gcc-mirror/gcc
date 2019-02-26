65be1389eeda9b3b97f6587721215c3f31bd7f98
Julian Brown <julian@codesourcery.com>
2019-02-26 15:48:00 -0800

    Fortran "declare create"/allocate support for OpenACC
    2018-10-04  Cesar Philippidis  <cesar@codesourcery.com>
                Julian Brown  <julian@codesourcery.com>
    
    	gcc/
    	* omp-low.cc (scan_sharing_clauses): Update handling of OpenACC declare
    	create, declare copyin and declare deviceptr to have local lifetimes.
    	(convert_to_firstprivate_int): Handle pointer types.
    	(convert_from_firstprivate_int): Likewise.  Create local storage for
    	the values being pointed to.  Add new orig_type argument.
    	(lower_omp_target): Handle GOMP_MAP_DECLARE_{ALLOCATE,DEALLOCATE}.
    	Add orig_type argument to convert_from_firstprivate_int call.
    	Allow pointer types with GOMP_MAP_FIRSTPRIVATE_INT.  Don't privatize
    	firstprivate VLAs.
    	* tree-pretty-print.cc (dump_omp_clause): Handle
    	GOMP_MAP_DECLARE_{ALLOCATE,DEALLOCATE}.
    
    	gcc/fortran/
    	* gfortran.h (enum gfc_omp_map_op): Add OMP_MAP_DECLARE_ALLOCATE,
    	OMP_MAP_DECLARE_DEALLOCATE.
    	(gfc_omp_clauses): Add update_allocatable.
    	* trans-array.cc (gfc_array_allocate): Call
    	gfc_trans_oacc_declare_allocate for decls that have oacc_declare_create
    	attribute set.
    	* trans-decl.cc (find_module_oacc_declare_clauses): Relax
    	oacc_declare_create to OMP_MAP_ALLOC, and oacc_declare_copyin to
    	OMP_MAP_TO, in order to	match OpenACC 2.5 semantics.
    	* trans-openmp.cc (gfc_trans_omp_clauses): Use GOMP_MAP_ALWAYS_POINTER
    	(for update directive) or GOMP_MAP_FIRSTPRIVATE_POINTER (otherwise) for
    	allocatable scalar decls.  Handle OMP_MAP_DECLARE_{ALLOCATE,DEALLOCATE}
    	clauses.
    	(gfc_trans_oacc_executable_directive): Use GOMP_MAP_ALWAYS_POINTER
    	for allocatable scalar data clauses inside acc update directives.
    	(gfc_trans_oacc_declare_allocate): New function.
    	* trans-stmt.cc (gfc_trans_allocate): Call
    	gfc_trans_oacc_declare_allocate for decls with oacc_declare_create
    	attribute set.
    	(gfc_trans_deallocate): Likewise.
    	* trans.h (gfc_trans_oacc_declare_allocate): Declare.
    
    	gcc/testsuite/
    	* gfortran.dg/goacc/declare-allocatable-1.f90: New test.
    
    	include/
    	* gomp-constants.h (enum gomp_map_kind): Define
    	GOMP_MAP_DECLARE_{ALLOCATE,DEALLOCATE} and GOMP_MAP_FLAG_SPECIAL_4.
    
    	libgomp/
    	* oacc-mem.c (gomp_acc_declare_allocate): New function.
    	* oacc-parallel.c (GOACC_enter_exit_data): Handle
    	GOMP_MAP_DECLARE_{ALLOCATE,DEALLOCATE}.
    	* testsuite/libgomp.oacc-fortran/allocatable-scalar.f90: New test.
    	* testsuite/libgomp.oacc-fortran/declare-allocatable-2.f90: New test.
    	* testsuite/libgomp.oacc-fortran/declare-allocatable-3.f90: New test.
    	* testsuite/libgomp.oacc-fortran/declare-allocatable-4.f90: New test.
    
    2020-02-19  Julian Brown  <julian@codesourcery.com>
    
    	gcc/fortran/
    	* trans-openmp.cc (gfc_omp_check_optional_argument): Handle non-decl
    	case.
    
    	gcc/
    	* gimplify.cc (gimplify_scan_omp_clauses): Handle
    	GOMP_MAP_DECLARE_ALLOCATE and GOMP_MAP_DECLARE_DEALLOCATE.
    
    	libgomp/
    	* libgomp.h (gomp_acc_declare_allocate): Remove prototype.
    	* oacc-mem.c (gomp_acc_declare_allocate): Make static.  Add POINTER
    	argument. Use acc_delete instead of acc_free.  Handle scalar
    	mappings.
    	(find_group_last): Handle GOMP_MAP_DECLARE_ALLOCATE and
    	GOMP_MAP_DECLARE_DEALLOCATE groupings.
    	(goacc_enter_data_internal): Fix kind check for
    	GOMP_MAP_DECLARE_ALLOCATE. Pass new pointer argument to
    	gomp_acc_declare_allocate.
    	(goacc_exit_data_internal): Unlock device mutex around
    	gomp_acc_declare_allocate call. Pass new pointer argument. Handle
    	group pointer mapping for deallocate.
    
    2021-04-07  Kwok Cheung Yeung  <kcy@codesourcery.com>
    
    	libgomp/
    	* oacc-mem.c (goacc_enter_data_internal): Unlock mutex before calling
    	gomp_acc_declare_allocate and relock it afterwards.
    
    2023-04-17  Kwok Cheung Yeung  <kcy@codesourcery.com>
    
    	* gimplify.cc (omp_group_base): Handle GOMP_MAP_DECLARE_ALLOCATE
    	and GOMP_MAP_DECLARE_DEALLOCATE.

--- gcc/fortran/trans-stmt.cc
+++ gcc/fortran/trans-stmt.cc
@@ -7063,6 +7063,10 @@ gfc_trans_allocate (gfc_code * code, gfc_omp_namelist *omp_allocate)
 	  else
 	    gfc_allocate_using_malloc (&se.pre, se.expr, memsz, stat,
 				      omp_cond, omp_alt_alloc, succ_add_expr);
+
+	  /* Allocate memory for OpenACC declared variables.  */
+	  if (expr->symtree->n.sym->attr.oacc_declare_create)
+	    gfc_trans_oacc_declare_allocate (&se.pre, expr, true);
 	}
       else
 	{
@@ -7569,6 +7573,10 @@ gfc_trans_deallocate (gfc_code *code)
 
 	  if (GFC_DESCRIPTOR_TYPE_P (TREE_TYPE (se.expr)))
 	    {
+	      if (!is_coarray
+		  && expr->symtree->n.sym->attr.oacc_declare_create)
+		gfc_trans_oacc_declare_allocate (&se.pre, expr, false);
+
 	      gfc_coarray_deregtype caf_dtype;
 
 	      if (is_coarray)
@@ -7622,6 +7630,10 @@ gfc_trans_deallocate (gfc_code *code)
 	}
       else
 	{
+	  /* Deallocate memory for OpenACC declared variables.  */
+	  if (expr->symtree->n.sym->attr.oacc_declare_create)
+	    gfc_trans_oacc_declare_allocate (&se.pre, expr, false);
+
 	  tmp = gfc_deallocate_scalar_with_status (se.expr, pstat, label_finish,
 						   false, al->expr,
 						   al->expr->ts, NULL_TREE,
