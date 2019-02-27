c167de66371fddca36996a255fc4ae407c65964d
sje <sje@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-02-27 00:02:53 +0000

    2018-02-26  Steve Ellcey  <sellcey@marvell.com>
    	* gfortran.dg/simd-builtins-1.f90: Update for aarch64*-*-*.
    	* gfortran.dg/simd-builtins-2.f90: Ditto.
    	* gfortran.dg/simd-builtins-6.f90: Ditto.
    	* gfortran.dg/simd-builtins-8.f90: New test.
    	* gfortran.dg/simd-builtins-8.h: New header file.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@269236 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/simd-builtins-2.f90
+++ gcc/testsuite/gfortran.dg/simd-builtins-2.f90
@@ -1,11 +1,12 @@
-! { dg-do compile { target { i?86-*-linux* x86_64-*-linux* } } }
-! { dg-additional-options "-msse2 -nostdinc -Ofast -fdump-tree-optimized" }
+! { dg-do compile { target { i?86-*-linux* x86_64-*-linux* aarch64*-*-linux* } } }
+! { dg-additional-options "-nostdinc -Ofast -fdump-tree-optimized" }
+! { dg-additional-options "-msse2" { target i?86-*-linux* x86_64-*-linux* } }
 
 program test_overloaded_intrinsic
   real(4) :: x4(3200), y4(3200)
   real(8) :: x8(3200), y8(3200)
 
-  ! this should be using simd clone
+  ! this should not be using simd clone
   y4 = sin(x4)
   print *, y4
 
@@ -18,3 +19,4 @@ end
 ! { dg-final { scan-tree-dump "__builtin_sin" "optimized" } } */
 ! { dg-final { scan-tree-dump-not "simdclone" "optimized" } } */
 ! { dg-final { scan-assembler-not "call.*_ZGVbN4v_sinf" } }
+! { dg-final { scan-assembler-not "bl.*_ZGVnN4v_sinf" } }
