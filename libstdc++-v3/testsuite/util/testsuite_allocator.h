5b313df9cb738aee920eeb8b8b5cc12464e98ef1
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-03-01 13:50:41 +0000

    Fix test memory_resource to work without sized deallocation
    The checking memory_resource in the testsuite assumes sized deallocation
    is supported, which might not be true for other compilers.
    
    	* testsuite/util/testsuite_allocator.h (__gnu_test::memory_resource)
    	[!__cpp_sized_deallocation]: Do not pass size to operator delete.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@269312 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/util/testsuite_allocator.h
+++ libstdc++-v3/testsuite/util/testsuite_allocator.h
@@ -780,7 +780,11 @@ namespace __gnu_test
 		  throw bad_size();
 		if (alignment != a->alignment)
 		  throw bad_alignment();
+#if __cpp_sized_deallocation
 		::operator delete(p, bytes, std::align_val_t(alignment));
+#else
+		::operator delete(p, std::align_val_t(alignment));
+#endif
 		*aptr = a->next;
 		a->next = lists->freed;
 		lists->freed = a;
