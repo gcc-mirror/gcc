1738b52201b1ce28c9f869646f5412b8937ccfe6
Martin Jambor <mjambor@suse.cz>
2019-03-07 17:03:34 +0100

    Relax cgraph_node::clone_of_p to also look through former clones
    2019-03-07  Martin Jambor  <mjambor@suse.cz>
    
    	PR ipa/88235
    	* cgraph.h (cgraph_node): New inline method former_thunk_p.
    	* cgraph.c (cgraph_node::dump): Dump a note if node is a former thunk.
    	(clone_of_p): Treat expanded thunks like thunks, be optimistic if they
    	have multiple callees.  At the end check if declarations match as
    	opposed to cgraph_nodes.
    
    	testsuite/
    	* g++.dg/ipa/pr88235.C: New test.
    
    From-SVN: r269462

---
+++ gcc/testsuite/g++.dg/ipa/pr88235.C
@@ -0,0 +1,55 @@
+// { dg-do compile }
+// { dg-options "-O1 -fdevirtualize -finline-small-functions -fipa-cp -fipa-cp-clone --param ipa-cp-eval-threshold=125 --param max-inline-insns-single=4" }
+
+extern "C" int printf (const char *, ...);
+enum E { vf_request, vf_event } want;
+
+int errs = 0;
+
+class ivResource {
+public:
+  virtual ~ivResource () { }
+};
+
+class ivHandler   : public ivResource   {
+public:
+  virtual void event() { }
+};
+
+class ivGlyph   : public ivResource   {
+public:
+  virtual ~ivGlyph  () { }
+  virtual void request () {
+    if (want!=vf_request)
+      ++errs;
+  }
+};
+
+class ItemView : public ivGlyph, public ivHandler {
+public:
+  virtual void event () {
+    if (want!=vf_event)
+      ++errs;
+  }
+} a;
+
+ivGlyph *bar() {
+  return &a;
+}
+
+ivHandler *bar2() {
+  return &a;
+}
+
+int main() {
+  want=vf_request;
+  bar()->request();
+  want=vf_event;
+  bar2()->event();
+  if (errs) {
+    printf("FAIL\n");
+    return 1;
+  }
+  printf("PASS\n");
+  return 0;
+}
