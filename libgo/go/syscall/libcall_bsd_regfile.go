ee973155b2fd00dfe0ef6a3563c9623512f6632e
Ian Lance Taylor <ian@gcc.gnu.org>
2019-03-09 02:10:22 +0000

    re PR go/89447 (libgo largefile support is incomplete and inconsistent)
    	PR go/89447
        syscall, internal/syscall: adjust use of largefile functions
        
        Consistently call __go_openat for openat.  Use fstatat64, creat64,
        sendfile64, and getdents64 where needed.
        
        Based on patch by Rainer Orth.
        
        Fixes https://gcc.gnu.org/PR89447
        
        Reviewed-on: https://go-review.googlesource.com/c/gofrontend/+/166420
    
    From-SVN: r269521

---
+++ libgo/go/syscall/libcall_bsd_regfile.go
@@ -0,0 +1,10 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build darwin dragonfly freebsd netbsd openbsd solaris,amd64 solaris,sparc64
+
+package syscall
+
+//sys	sendfile(outfd int, infd int, offset *Offset_t, count int) (written int, err error)
+//sendfile(outfd _C_int, infd _C_int, offset *Offset_t, count Size_t) Ssize_t
