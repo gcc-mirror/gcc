161c01072e1221fc571b669f1458b17f7258583c
Nathan Sidwell <nathan@gcc.gnu.org>
2019-03-11 17:55:32 +0000

    avoid spurious error
    From-SVN: r269593

--- gcc/testsuite/g++.dg/other/pr28114.C
+++ gcc/testsuite/g++.dg/other/pr28114.C
@@ -1,7 +1,7 @@
 
 // Test to make sure we do not ICE on this invalid program.
 
-template<int> void foo(struct {}*); // { dg-error "may not be defined" }
+template<int> void foo(struct {}*){} // { dg-error "may not be defined" }
 
 void bar()
 {
