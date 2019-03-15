0922e5086151ce5c86defc9834198d33ca684743
kelvin <kelvin@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-03-15 19:52:43 +0000

    gcc/ChangeLog:
    2019-03-15  Kelvin Nilsen  <kelvin@gcc.gnu.org>
    
    	PR target/87532
    	* config/rs6000/rs6000-c.c (altivec_resolve_overloaded_builtin):
    	When handling vec_extract, use modular arithmetic to allow
    	constant selectors greater than vector length.
    	* config/rs6000/rs6000.c (rs6000_expand_vector_extract): Allow
    	V1TImode vectors to have constant selector values greater than 0.
    	Use modular arithmetic to compute vector index.
    	(rs6000_split_vec_extract_var): Use modular arithmetic to compute
    	index for in-memory vectors.  Correct code generation for
    	in-register vectors.
    	(altivec_expand_vec_ext_builtin): Use modular arithmetic to
    	compute index.
    
    gcc/testsuite/ChangeLog:
    
    2019-03-15  Kelvin Nilsen  <kelvin@gcc.gnu.org>
    
    	PR target/87532
    	* gcc.target/powerpc/fold-vec-extract-char.p8.c: Modify expected
    	instruction selection.
    	* gcc.target/powerpc/fold-vec-extract-int.p8.c: Likewise.
    	* gcc.target/powerpc/fold-vec-extract-short.p8.c: Likewise.
    	* gcc.target/powerpc/pr87532-mc.c: New test.
    	* gcc.target/powerpc/pr87532.c: New test.
    	* gcc.target/powerpc/vec-extract-v16qiu-v2.h: New test.
    	* gcc.target/powerpc/vec-extract-v16qiu-v2a.c: New test.
    	* gcc.target/powerpc/vec-extract-v16qiu-v2b.c: New test.
    	* gcc.target/powerpc/vsx-builtin-10a.c: New test.
    	* gcc.target/powerpc/vsx-builtin-10b.c: New test.
    	* gcc.target/powerpc/vsx-builtin-11a.c: New test.
    	* gcc.target/powerpc/vsx-builtin-11b.c: New test.
    	* gcc.target/powerpc/vsx-builtin-12a.c: New test.
    	* gcc.target/powerpc/vsx-builtin-12b.c: New test.
    	* gcc.target/powerpc/vsx-builtin-13a.c: New test.
    	* gcc.target/powerpc/vsx-builtin-13b.c: New test.
    	* gcc.target/powerpc/vsx-builtin-14a.c: New test.
    	* gcc.target/powerpc/vsx-builtin-14b.c: New test.
    	* gcc.target/powerpc/vsx-builtin-15a.c: New test.
    	* gcc.target/powerpc/vsx-builtin-15b.c: New test.
    	* gcc.target/powerpc/vsx-builtin-16a.c: New test.
    	* gcc.target/powerpc/vsx-builtin-16b.c: New test.
    	* gcc.target/powerpc/vsx-builtin-17a.c: New test.
    	* gcc.target/powerpc/vsx-builtin-17b.c: New test.
    	* gcc.target/powerpc/vsx-builtin-18a.c: New test.
    	* gcc.target/powerpc/vsx-builtin-18b.c: New test.
    	* gcc.target/powerpc/vsx-builtin-19a.c: New test.
    	* gcc.target/powerpc/vsx-builtin-19b.c: New test.
    	* gcc.target/powerpc/vsx-builtin-20a.c: New test.
    	* gcc.target/powerpc/vsx-builtin-20b.c: New test.
    	* gcc.target/powerpc/vsx-builtin-9a.c: New test.
    	* gcc.target/powerpc/vsx-builtin-9b.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@269715 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/powerpc/vsx-builtin-12b.c
@@ -0,0 +1,109 @@
+/* { dg-do run { target int128 } } */
+/* { dg-require-effective-target vsx_hw } */
+/* { dg-options "-mvsx -O3" } */
+
+/* This test should run the same on any target that supports vsx
+   instructions.  Intentionally not specifying cpu in order to test
+   all code generation paths.  */
+
+#include <altivec.h>
+
+extern void abort (void);
+
+#define CONST0		(31415926539LL)
+#define CONST1		(2 * 31415926539LL)
+
+/* Test that indices > length of vector are applied modulo the vector
+   length.  */
+
+/* Test for vector residing in register.  */
+long long int e0 (vector long long int v)
+{
+  return __builtin_vec_ext_v2di (v, 0);
+}
+
+long long int e3 (vector long long int v)
+{
+  return __builtin_vec_ext_v2di (v, 3);
+}
+
+/* Test for vector residing in memory.  */
+long long int me0 (vector long long int *vp)
+{
+  return __builtin_vec_ext_v2di (*vp, 0);
+}
+
+long long int me3 (vector long long int *vp)
+{
+  return __builtin_vec_ext_v2di (*vp, 3);
+}
+
+/* Test the same with variable indices.  */
+
+/* Test for variable selector and vector residing in register.  */
+__attribute__((noinline))
+long long int ei (vector long long int v, int i)
+{
+  return __builtin_vec_ext_v2di (v, i);
+}
+
+/* Test for variable selector and vector residing in memory.  */
+long long int mei (vector long long int *vp, int i)
+{
+  return __builtin_vec_ext_v2di (*vp, i);
+}
+
+int main (int argc, char *argv[]) {
+  vector long long int dv = { CONST0, CONST1 };
+  long long int d;
+
+  d = e0 (dv);
+  if (d != CONST0)
+    abort ();
+
+  d = e3 (dv);
+  if (d != CONST1)
+    abort ();
+
+  d = me0 (&dv);
+  if (d != CONST0)
+    abort ();
+
+  d = me3 (&dv);
+  if (d != CONST1)
+    abort ();
+
+  d = ei (dv, 0);
+  if (d != CONST0)
+    abort ();
+
+  d = ei (dv, 1);
+  if (d != CONST1)
+    abort ();
+
+  d = ei (dv, 2);
+  if (d != CONST0)
+    abort ();
+
+  d = ei (dv, 3);
+  if (d != CONST1)
+    abort ();
+
+  d = mei (&dv, 0);
+  if (d != CONST0)
+    abort ();
+
+  d = mei (&dv, 1);
+  if (d != CONST1)
+    abort ();
+
+  d = mei (&dv, 2);
+  if (d != CONST0)
+    abort ();
+
+  d = mei (&dv, 3);
+  if (d != CONST1)
+    abort ();
+
+  return 0;
+}
