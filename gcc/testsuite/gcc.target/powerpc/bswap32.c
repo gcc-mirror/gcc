4221d1bb79624fc6b70aaf7e2d64dd0eed3514e5
segher <segher@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-03-18 17:44:56 +0000

    rs6000: Use pointers in bswap testcases
    Currently these bswap testcases use global variables, which causes
    problems with -m32: the memory access is a D-form access, and when
    combine tries to combine that with the bswap it tries a D-form store
    with byte reverse.  That instruction does not exist, and since combine
    started with only two insns here it will not try splitting this.
    
    This should be improved, but it is not what this test is testing, and
    the "load" case already uses a pointer, so let's do that for the store
    case as well.
    
    
    	* gcc.target/powerpc/bswap16.c: Use a pointer instead of a global for
    	the "store" test as well.
    	* gcc.target/powerpc/bswap32.c: Ditto.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@269771 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/bswap32.c
+++ gcc/testsuite/gcc.target/powerpc/bswap32.c
@@ -3,6 +3,5 @@
 /* { dg-final { scan-assembler "lwbrx" } } */
 /* { dg-final { scan-assembler "stwbrx" } } */
 
-unsigned int ui;
 unsigned int load_bswap32 (unsigned int *p) { return __builtin_bswap32 (*p); }
-void store_bswap32 (unsigned int a) { ui = __builtin_bswap32 (a); }
+void store_bswap32 (unsigned int *p, unsigned int a) { *p = __builtin_bswap32 (a); }
