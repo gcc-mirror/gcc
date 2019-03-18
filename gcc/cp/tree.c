7834e5a0e103c7f07c789cbe7293c6c1400a1216
Jason Merrill <jason@redhat.com>
2019-03-18 15:37:00 -0400

    PR c++/89630 - ICE with dependent using-decl as template arg.
    Even though these two using-declarations have the same effect, they are not
    the same declaration, and we don't need to work to treat them as the same
    like we do for typedefs.  If we did need to, we would need to handle them
    specially in iterative_hash_template_arg as well as here.
    
    	* tree.c (cp_tree_equal): Always return false for USING_DECL.
    
    From-SVN: r269777

--- gcc/cp/tree.c
+++ gcc/cp/tree.c
@@ -3661,6 +3661,7 @@ cp_tree_equal (tree t1, tree t2)
     case TEMPLATE_DECL:
     case IDENTIFIER_NODE:
     case SSA_NAME:
+    case USING_DECL:
       return false;
 
     case BASELINK:
@@ -3786,14 +3787,6 @@ cp_tree_equal (tree t1, tree t2)
 	      && comp_template_args (DEFERRED_NOEXCEPT_ARGS (t1),
 				     DEFERRED_NOEXCEPT_ARGS (t2)));
       break;
-
-    case USING_DECL:
-      if (DECL_DEPENDENT_P (t1) && DECL_DEPENDENT_P (t2))
-	return (cp_tree_equal (USING_DECL_SCOPE (t1),
-			       USING_DECL_SCOPE (t2))
-		&& cp_tree_equal (DECL_NAME (t1),
-				  DECL_NAME (t2)));
-      return false;
 
     case LAMBDA_EXPR:
       /* Two lambda-expressions are never considered equivalent.  */
