2214085affb80feb19e79882ee769e28c2a85a5a
Martin Liska <mliska@suse.cz>
2019-03-19 18:08:28 +0100

    Fix set of even probabilities (PR middle-end/89737).
    2019-03-19  Martin Liska  <mliska@suse.cz>
    
    	PR middle-end/89737
    	* predict.c (combine_predictions_for_bb): Empty likely_edges and
    	unlikely_edges if there's an edge that belongs to both these sets.
    2019-03-19  Martin Liska  <mliska@suse.cz>
    
    	PR middle-end/89737
    	* gcc.dg/pr89737.c: New test.
    
    From-SVN: r269804

---
+++ gcc/testsuite/gcc.dg/pr89737.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-profile_estimate" } */
+
+int a, b;
+
+void c() {
+  &&d;
+  void *e = &&f, *g = &&h;
+f:
+  __attribute__((hot)) h : __attribute__((cold)) for (; a;) goto *g;
+d:
+  for (; b;)
+    goto *e;
+}
+
+/* { dg-final { scan-tree-dump-times "predicted to even probabilities" 4 "profile_estimate"} } */
+
