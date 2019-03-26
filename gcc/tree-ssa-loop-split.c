c300b1333030597725efedcf3020c41e833635aa
Richard Biener <rguenther@suse.de>
2019-03-26 13:18:23 +0000

    backport: [multiple changes]
    2019-02-26  Richard Biener  <rguenther@suse.de>
    
    	Backport from mainline
    	2019-02-12  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/89253
    	* tree-ssa-loop-split.c (tree_ssa_split_loops): Check we can
    	duplicate the loop.
    
    	* gfortran.dg/pr89253.f: New testcase.
    
    	2019-02-08  Richard Biener  <rguenther@suse.de>
    
    	PR middle-end/89223
    	* tree-data-ref.c (initialize_matrix_A): Fail if constant
    	doesn't fit in HWI.
    	(analyze_subscript_affine_affine): Handle failure from
    	initialize_matrix_A.
    
    	* gcc.dg/torture/pr89223.c: New testcase.
    
    	2019-01-28  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/88739
    	* tree-ssa-sccvn.c (vn_reference_lookup_3): Avoid generating
    	BIT_FIELD_REFs of non-mode-precision integral operands.
    
    	* gcc.c-torture/execute/pr88739.c: New test.
    
    From-SVN: r269942

--- gcc/tree-ssa-loop-split.c
+++ gcc/tree-ssa-loop-split.c
@@ -649,7 +649,8 @@ tree_ssa_split_loops (void)
 					false, true)
 	  && niter.cmp != ERROR_MARK
 	  /* We can't yet handle loops controlled by a != predicate.  */
-	  && niter.cmp != NE_EXPR)
+	  && niter.cmp != NE_EXPR
+	  && can_duplicate_loop_p (loop))
 	{
 	  if (split_loop (loop, &niter))
 	    {
