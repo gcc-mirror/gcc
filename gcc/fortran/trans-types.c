24709509deabcbcd5285ef362cd55f94379987a9
anlauf <anlauf@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-03-31 18:33:51 +0000

    2019-03-31  Harald Anlauf  <anlauf@gmx.de>
    	PR fortran/83515
    	PR fortran/85797
    	* trans-types.c (gfc_typenode_for_spec): Handle conversion for
    	procedure pointers.
    	* target-memory.c (gfc_element_size): Handle size determination
    	for procedure pointers.
    
    	PR fortran/83515
    	PR fortran/85797
    	* gfortran.dg/pr85797.f90: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@270045 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/trans-types.c
+++ gcc/fortran/trans-types.c
@@ -1194,6 +1194,9 @@ gfc_typenode_for_spec (gfc_typespec * spec, int codim)
 	    basetype = pfunc_type_node;
 	}
        break;
+    case BT_PROCEDURE:
+      basetype = pfunc_type_node;
+      break;
     default:
       gcc_unreachable ();
     }
