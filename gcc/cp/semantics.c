7651759baf5fc32daaf4ce7bae2e66b6fd480763
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-04-08 08:13:50 +0000

    /cp 2019-04-08  Paolo Carlini  <paolo.carlini@oracle.com>
    	PR c++/89914
    	* semantics.c (trait_expr_value): Don't use TYPE_NOTHROW_P
    	when maybe_instantiate_noexcept fails.
    	(classtype_has_nothrow_assign_or_copy_p): Likewise.
    	* method.c (implicitly_declare_fn): Avoid passing error_mark_node
    	to build_exception_variant.
    
    /testsuite
    2019-04-08  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	PR c++/89914
    	* g++.dg/ext/has_nothrow_constructor-3.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@270201 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -9551,8 +9551,8 @@ classtype_has_nothrow_assign_or_copy_p (tree type, bool assign_p)
       if (copy_fn_p (fn) > 0)
 	{
 	  saw_copy = true;
-	  maybe_instantiate_noexcept (fn);
-	  if (!TYPE_NOTHROW_P (TREE_TYPE (fn)))
+	  if (!maybe_instantiate_noexcept (fn)
+	      || !TYPE_NOTHROW_P (TREE_TYPE (fn)))
 	    return false;
 	}
     }
@@ -9594,8 +9594,8 @@ trait_expr_value (cp_trait_kind kind, tree type1, tree type2)
       return (trait_expr_value (CPTK_HAS_TRIVIAL_CONSTRUCTOR, type1, type2) 
 	      || (CLASS_TYPE_P (type1)
 		  && (t = locate_ctor (type1))
-		  && (maybe_instantiate_noexcept (t),
-		      TYPE_NOTHROW_P (TREE_TYPE (t)))));
+		  && maybe_instantiate_noexcept (t)
+		  && TYPE_NOTHROW_P (TREE_TYPE (t))));
 
     case CPTK_HAS_TRIVIAL_CONSTRUCTOR:
       type1 = strip_array_types (type1);
