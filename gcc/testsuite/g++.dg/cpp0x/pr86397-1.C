1fdc37a3a1b56c69e24a8c54307c788c4dc55301
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-04-12 15:26:16 +0000

    * except.c (build_noexcept_spec): Use build_converted_constant_bool_expr 	instead of perform_implicit_conversion_flags.
    	* g++.dg/cpp0x/noexcept30.C: Tweak dg-error.
    	* g++.dg/cpp0x/pr86397-1.C: Likewise.
    	* g++.dg/cpp0x/pr86397-2.C: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@270319 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp0x/pr86397-1.C
+++ gcc/testsuite/g++.dg/cpp0x/pr86397-1.C
@@ -1,5 +1,5 @@
 // { dg-do compile { target c++11 } }
 // { dg-options "-fdelete-null-pointer-checks" }
 void e();
-template <bool> void f(int() noexcept(e)) {}
-template void f<false>(int()); // { dg-error "does not match" "" { target c++17 } }
+template <bool> void f(int() noexcept(e)) {} // { dg-error "convert" }
+template void f<false>(int());
