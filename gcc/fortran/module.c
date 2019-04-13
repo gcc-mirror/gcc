8d0bceca13045ec8562d757694b6a485561a0940
dominiq <dominiq@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-04-13 11:32:49 +0000

    2019-04-13  Dominique d'Humieres  <dominiq@gcc.gnu.org>
    	PR fortran/79842
    	* module.c (gfc_use_module): use complete sentences.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@270338 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/module.c
+++ gcc/fortran/module.c
@@ -7144,8 +7144,12 @@ gfc_use_module (gfc_use_list *module)
   for (p = gfc_state_stack; p; p = p->previous)
     if ((p->state == COMP_MODULE || p->state == COMP_SUBMODULE)
 	 && strcmp (p->sym->name, module_name) == 0)
-      gfc_fatal_error ("Cannot USE the same %smodule we're building",
-		       p->state == COMP_SUBMODULE ? "sub" : "");
+      {
+	if (p->state == COMP_SUBMODULE)
+	  gfc_fatal_error ("Cannot USE a submodule that is currently built");
+	else
+	  gfc_fatal_error ("Cannot USE a module that is currently built");
+      }
 
   init_pi_tree ();
   init_true_name_tree ();
