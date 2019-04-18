ff146a59dd0cb24043897cf90ea011eb2aa3d38b
Jakub Jelinek <jakub@redhat.com>
2019-04-18 12:28:21 +0200

    re PR translation/79183 (Hard coded plurals in gimple-ssa-sprintf.c:2050)
    	PR translation/79183
    	* gimple-ssa-sprintf.c (format_directive): Use inform_n instead of
    	inform where appropriate.
    
    From-SVN: r270438

--- gcc/gimple-ssa-sprintf.c
+++ gcc/gimple-ssa-sprintf.c
@@ -3016,12 +3016,10 @@ format_directive (const sprintf_dom_walker::call_info &info,
 	     help the user figure out how big a buffer they need.  */
 
 	  if (min == max)
-	    inform (callloc,
-		    (min == 1
-		     ? G_("%qE output %wu byte into a destination of size %wu")
-		     : G_("%qE output %wu bytes into a destination of size "
-			  "%wu")),
-		    info.func, min, info.objsize);
+	    inform_n (callloc, min,
+		      "%qE output %wu byte into a destination of size %wu",
+		      "%qE output %wu bytes into a destination of size %wu",
+		      info.func, min, info.objsize);
 	  else if (max < HOST_WIDE_INT_MAX)
 	    inform (callloc,
 		    "%qE output between %wu and %wu bytes into "
@@ -3044,11 +3042,9 @@ format_directive (const sprintf_dom_walker::call_info &info,
 	     of printf with no destination size just print the computed
 	     result.  */
 	  if (min == max)
-	    inform (callloc,
-		    (min == 1
-		     ? G_("%qE output %wu byte")
-		     : G_("%qE output %wu bytes")),
-		    info.func, min);
+	    inform_n (callloc, min,
+		      "%qE output %wu byte", "%qE output %wu bytes",
+		      info.func, min);
 	  else if (max < HOST_WIDE_INT_MAX)
 	    inform (callloc,
 		    "%qE output between %wu and %wu bytes",
