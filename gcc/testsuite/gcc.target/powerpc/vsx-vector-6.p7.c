226a15e6cd18369e65eb34bce360f4dbe6341c70
Xiong Hu Luo <luoxhu@linux.ibm.com>
2019-04-19 03:30:33 +0000

    backport r257541, r259936, r260294, r260623, r261098, r261333, r268585 from trunk
    These patches are followed changes for r255555 on testcases
    vsx-vector-6*.c.  Backport them to update file names and fix regressions
    for GCC7 on power9.
    See e.g. https://gcc.gnu.org/ml/gcc-testresults/2019-04/msg00159.html
    for the failures this patch fixes; the patch is for GCC 7.
    
    gcc/ChangeLog:
    
    2019-04-19  Xiong Hu Luo  <luoxhu@linux.ibm.com>
    
    	Backport from trunk
    	2018-05-23  Segher Boessenkool  <segher@kernel.crashing.org>
    
    	* doc/sourcebuild.texi (Endianness): New subsubsection.
    
    gcc/testsuite/ChangeLog:
    
    2019-04-19  Xiong Hu Luo  <luoxhu@linux.ibm.com>
    
    	Backport from trunk
    	2018-02-07  Will Schmidt  <will_schmidt@vnet.ibm.com>
    
    	* gcc.target/powerpc/vsx-vector-6-le.c: Update CPU target.
    	* gcc.target/powerpc/vsx-vector-6-le.p9.c: New.
    
    	Backport from trunk
    	2018-05-04  Carl Love  <cel@us.ibm.com>
    	* gcc.target/powerpc/vsx-vector-6.h (foo): Add test for vec_max,
    	vec_trunc.
    	* gcc.target/powerpc/vsx-vector-6-le.c (dg-final): Update xvcmpeqdp,
    	xvcmpgtdp, xvcmpgedp counts. Add xxsel counts.
    	* gcc.target/powerpc/vsx-vector-6-be.c (dg-final): Update xvcmpgtdp,
    	xvcmpgedp counts. Add xxsel counts.
    
    	Backport from trunk
    	2018-05-16  Carl Love  <cel@us.ibm.com>
    	* gcc.target/powerpc/vsx-vector-6-be.c: Remove file.
    	* gcc.target/powerpc/vsx-vector-6-be.p7.c: New test file.
    	* gcc.target/powerpc/vsx-vector-6-be.p8.c: New test file.
    	* gcc.target/powerpc/vsx-vector-6-le.c (dg-final): Update counts for
    	xvcmpeqdp., xvcmpgtdp., xvcmpgedp., xxlxor, xvrdpi.
    
    	Backport from trunk
    	2018-05-23  Segher Boessenkool  <segher@kernel.crashing.org>
    
    	* lib/target-supports.exp (check_effective_target_be): New.
    	(check_effective_target_le): New.
    
    	Backport from trunk
    	2018-06-01  Carl Love  <cel@us.ibm.com>
    
    	* gcc.target/powerpc/altivec-7-be.c: Delete file.
    	* gcc.target/powerpc/altivec-7-le.c: Delete file.
    	* gcc.target/powerpc/vsx-7-be.c: Remove file.
    
    	Backport from trunk
    	2018-06-01  Carl Love  <cel@us.ibm.com>
    
    	Commit 260294 on 2018-05-16 by Carl Love was supposed to add the
    	following files.
    
    	* gcc.target/powerpc/vsx-vector-6-be.p7.c: New test file.
    	* gcc.target/powerpc/vsx-vector-6-be.p8.c: New test file.
    
    	Backport from trunk
    	2018-06-08  Carl Love  <cel@us.ibm.com>
    
    	* gcc.target/powerpc/vsx-vector-6-be.p7.c: Rename this file to
    	vsx-vector-6.p7.c.
    	* gcc.target/powerpc/vsx-vector-6-le.p9.c: Rename this file to
    	vsx-vector-6.p9.c.
    	* gcc.target/powerpc/vsx-vector-6-be.p8.c: Move instruction counts
    	for BE system that are different then for an LE system from this file
    	into vsx-vector-6-le.c using be target qualifier.  Remove this file.
    	* gcc.target/powerpc/vsx-vector-6-le.c: Add le qualifiers as needed for
    	the various instruction counts.  Rename file to vsx-vector-6.p8.c.
    
    	Backport from trunk
    	2019-02-06  Bill Seurer  <seurer@linux.vnet.ibm.com>
    
    	* gcc.target/powerpc/vsx-vector-6.p7.c: Update instruction
    	counts and target.
    	* gcc.target/powerpc/vsx-vector-6.p8.c: Update instruction
    	counts and target.
    	* gcc.target/powerpc/vsx-vector-6.p9.c: Update instruction
    	counts and target.
    
    From-SVN: r270452

---
+++ gcc/testsuite/gcc.target/powerpc/vsx-vector-6.p7.c
@@ -0,0 +1,42 @@
+/* { dg-do compile { target { lp64 && be } } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } } */
+/* { dg-require-effective-target powerpc_vsx_ok } */
+/* { dg-options "-mvsx -O2 -mcpu=power7 -dp" } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power7" } } */
+
+/* Expected instruction counts for Power 7 */
+
+/* { dg-final { scan-assembler-times "xvabsdp" 1 } } */
+/* { dg-final { scan-assembler-times "xvadddp" 1 } } */
+/* { dg-final { scan-assembler-times "xxlnor" 5 } } */
+/* { dg-final { scan-assembler-times {\mxvcmpeqdp\s} 1 } } */
+/* { dg-final { scan-assembler-times {\mxvcmpeqdp\.\s} 5 } } */
+/* { dg-final { scan-assembler-times {\mxvcmpgtdp\s} 2 } } */
+/* { dg-final { scan-assembler-times {\mxvcmpgtdp\.\s} 5 } } */
+/* { dg-final { scan-assembler-times {\mxvcmpgedp\s} 1 } } */
+/* { dg-final { scan-assembler-times {\mxvcmpgedp\.\s} 6 } } */
+/* { dg-final { scan-assembler-times "xvrdpim" 1 } } */
+/* { dg-final { scan-assembler-times "xvmaddadp" 1 } } */
+/* { dg-final { scan-assembler-times "xvmsubadp" 1 } } */
+/* { dg-final { scan-assembler-times "xvsubdp" 1 } } */
+/* { dg-final { scan-assembler-times "xvmaxdp" 1 } } */
+/* { dg-final { scan-assembler-times "xvmindp" 1 } } */
+/* { dg-final { scan-assembler-times "xvmuldp" 1 } } */
+/* { dg-final { scan-assembler-times "vperm" 2 } } */
+/* { dg-final { scan-assembler-times "xvrdpic" 2 } } */
+/* { dg-final { scan-assembler-times "xvsqrtdp" 1 } } */
+/* { dg-final { scan-assembler-times "xvrdpiz" 1 } } */
+/* { dg-final { scan-assembler-times "xvmsubasp" 1 } } */
+/* { dg-final { scan-assembler-times "xvnmaddasp" 1 } } */
+/* { dg-final { scan-assembler-times "xvnmaddadp" 1 } } */
+/* { dg-final { scan-assembler-times "xvnmsubadp" 1 } } */
+/* { dg-final { scan-assembler-times "vmsumshs" 2 } } */
+/* { dg-final { scan-assembler-times "xxland" 13 } } */
+/* { dg-final { scan-assembler-times "xxlxor" 2 } } */
+/* { dg-final { scan-assembler-times "xxsel" 4 } } */
+/* { dg-final { scan-assembler-times "xvrdpip" 1 } } */
+/* { dg-final { scan-assembler-times "xvdivdp" 1 } } */
+/* { dg-final { scan-assembler-times "xvrdpi" 7 } } */
+
+/* Source code for the test in vsx-vector-6.h */
+#include "vsx-vector-6.h"
---
+++ gcc/testsuite/gcc.target/powerpc/vsx-vector-6.p8.c
@@ -0,0 +1,54 @@
+/* { dg-do compile { target lp64 } } */
+/* { dg-skip-if "" { powerpc*-*-darwin* } } */
+/* { dg-require-effective-target powerpc_vsx_ok } */
+/* { dg-options "-mvsx -O2 -mcpu=power8" } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power8" } } */
+
+/* Expected instruction counts for Power 8.  */
+
+/* { dg-final { scan-assembler-times "xvabsdp" 1 } } */
+/* { dg-final { scan-assembler-times "xvadddp" 1 } } */
+/* { dg-final { scan-assembler-times "xxlnor" 6 { target le } } } */
+/* { dg-final { scan-assembler-times "xxlnor" 5 { target be } } } */
+
+/* We generate xxlor instructions for many reasons other than or'ing vector
+   operands or calling __builtin_vec_or(), which  means we cannot rely on
+   their usage counts being stable.  Therefore, we just ensure at least one
+   xxlor instruction was generated.  */
+/* { dg-final { scan-assembler "xxlor" } } */
+
+/* { dg-final { scan-assembler-times {\mxvcmpeqdp\s} 0 { target le } } } */
+/* { dg-final { scan-assembler-times {\mxvcmpeqdp\s} 1 { target be } } } */
+/* { dg-final { scan-assembler-times {\mxvcmpeqdp\.\s} 6 { target le } } } */
+/* { dg-final { scan-assembler-times {\mxvcmpeqdp\.\s} 5 { target be } } } */
+/* { dg-final { scan-assembler-times {\mxvcmpgtdp\s} 2 } } */
+/* { dg-final { scan-assembler-times {\mxvcmpgtdp\.\s} 5 { target le } } } */
+/* { dg-final { scan-assembler-times {\mxvcmpgtdp\.\s} 6 { target le } } } */
+/* { dg-final { scan-assembler-times {\mxvcmpgedp\s} 2 } } */
+/* { dg-final { scan-assembler-times {\mxvcmpgedp\.\s} 4 } } */
+/* { dg-final { scan-assembler-times "xvrdpim" 1 } } */
+/* { dg-final { scan-assembler-times "xvmaddadp" 1 } } */
+/* { dg-final { scan-assembler-times "xvmsubadp" 1 } } */
+/* { dg-final { scan-assembler-times "xvsubdp" 1 } } */
+/* { dg-final { scan-assembler-times "xvmaxdp" 1 } } */
+/* { dg-final { scan-assembler-times "xvmindp" 1 } } */
+/* { dg-final { scan-assembler-times "xvmuldp" 1 } } */
+/* { dg-final { scan-assembler-times "vperm" 1 } } */
+/* { dg-final { scan-assembler-times "xvrdpic" 1 } } */
+/* { dg-final { scan-assembler-times "xvsqrtdp" 1 } } */
+/* { dg-final { scan-assembler-times "xvrdpiz" 1 } } */
+/* { dg-final { scan-assembler-times "xvmsubasp" 1 } } */
+/* { dg-final { scan-assembler-times "xvnmaddasp" 1 } } */
+/* { dg-final { scan-assembler-times "xvnmaddadp" 1 } } */
+/* { dg-final { scan-assembler-times "xvnmsubadp" 1 } } */
+/* { dg-final { scan-assembler-times "vmsumshs" 1 } } */
+/* { dg-final { scan-assembler-times "xxland" 9 { target le } } } */
+/* { dg-final { scan-assembler-times "xxland" 13 { target be } } } */
+/* { dg-final { scan-assembler-times "xxlxor" 2 } } */
+/* { dg-final { scan-assembler-times "xxsel" 2 } } */
+/* { dg-final { scan-assembler-times "xvrdpip" 1 } } */
+/* { dg-final { scan-assembler-times "xvdivdp" 1 } } */
+/* { dg-final { scan-assembler-times "xvrdpi" 5 } } */
+
+/* Source code for the test in vsx-vector-6.h */
+#include "vsx-vector-6.h"
