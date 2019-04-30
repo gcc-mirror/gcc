e622e833a441fdf842f4e6fe5ecffccadf53f663
Jakub Jelinek <jakub@redhat.com>
2019-04-30 22:31:26 +0200

    backport: re PR c++/89405 (ICE in import_export_decl, at cp/decl2.c:2959)
    	Backported from mainline
    	2019-02-20  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/89405
    	* decl.c (maybe_commonize_var): When clearing TREE_PUBLIC and
    	DECL_COMMON, set DECL_INTERFACE_KNOWN.
    
    	* g++.dg/cpp1z/inline-var5.C: New test.
    
    From-SVN: r270715

--- gcc/testsuite/g++.dg/cpp1z/inline-var5.C
+++ gcc/testsuite/g++.dg/cpp1z/inline-var5.C
@@ -1,3 +1,14 @@
+// PR c++/89405
+// { dg-do compile { target c++17 } }
+// { dg-options "-fno-weak" }
+
+template <int N>
+struct S
+{
+  static constexpr int a = N;	// { dg-warning "semantics of inline variable" }
+};				// { dg-message "you can work around this" "" { target *-*-* } .-1 }
+
+const int *x = &S<0>::a;
 // PR c++/87921
 // { dg-do compile { target c++17 } }
 
