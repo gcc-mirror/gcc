e884ced05231a04b1a3c70ece7237d0b1eeebf19
Clément Chigot <clement.chigot@atos.net>
2019-05-03 16:53:13 +0200

    syscall: fix TestForeground for AIX
    Syscall function can't be used on AIX. Therefore, Ioctl in
    TestForeground must call raw_ioctl.
    
    Reviewed-on: https://go-review.googlesource.com/c/gofrontend/+/175080

--- libgo/go/syscall/export_aix_test.go
+++ libgo/go/syscall/export_aix_test.go
@@ -1,12 +1,16 @@
-// Copyright 2015 The Go Authors. All rights reserved.
+// Copyright 2019 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin dragonfly freebsd hurd linux netbsd openbsd solaris
+// +build aix
 
 package syscall
 
+import (
+	"unsafe"
+)
+
 func Ioctl(fd, req, arg uintptr) (err Errno) {
-	_, _, err = Syscall(SYS_IOCTL, fd, req, arg)
+	_, err = raw_ioctl_ptr(int(fd), req, unsafe.Pointer(arg))
 	return err
 }
