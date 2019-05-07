57481cdfdc4aa2a0c61e732f9fff312a9ddfb7b6
segher <segher@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-05-07 22:25:05 +0000

    rs6000: Remove reload leftovers
    There are a few things left in the rs6000 port that are unused now
    that we do not support old reload anymore.  This removes those.
    
    
    	* config/rs6000/rs6000-protos.h (rs6000_legitimize_reload_address_ptr):
    	Delete declaration.
    	* config/rs6000/rs6000.c (rs6000_legitimize_reload_address): Delete.
    	(rs6000_debug_legitimize_reload_address): Delete.
    	(rs6000_legitimize_reload_address_ptr): Delete.
    	(rs6000_option_override_internal): Adjust.
    	(mem_operand_gpr): Adjust comment.
    	(legitimate_lo_sum_address_p): Ditto.
    	(rs6000_legitimize_reload_address): Delete.
    	(rs6000_debug_legitimize_reload_address): Delete.
    	* config/rs6000/rs6000.h (LEGITIMIZE_RELOAD_ADDRESS): Delete.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@270983 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/rs6000/rs6000-protos.h
+++ gcc/config/rs6000/rs6000-protos.h
@@ -137,8 +137,6 @@ extern void rs6000_emit_le_vsx_permute (rtx, rtx, machine_mode);
 extern void rs6000_emit_le_vsx_move (rtx, rtx, machine_mode);
 extern bool valid_sf_si_move (rtx, rtx, machine_mode);
 extern void rs6000_emit_move (rtx, rtx, machine_mode);
-extern rtx (*rs6000_legitimize_reload_address_ptr) (rtx, machine_mode,
-						    int, int, int, int *);
 extern bool rs6000_legitimate_offset_address_p (machine_mode, rtx,
 						bool, bool);
 extern void rs6000_output_tlsargs (rtx *);
