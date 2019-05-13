93ee143d1888051fbf3156212accc154af3a667b
Ian Lance Taylor <ian@gcc.gnu.org>
2019-05-13 20:26:24 +0000

    libgo: drop Solaris 10 support
        Based on patch by Rainer Orth.
        
        Reviewed-on: https://go-review.googlesource.com/c/gofrontend/+/176938
    
    From-SVN: r271135

--- libgo/go/runtime/signal_gccgo.go
+++ libgo/go/runtime/signal_gccgo.go
@@ -60,11 +60,6 @@ type sigctxt struct {
 }
 
 func (c *sigctxt) sigcode() uint64 {
-	if c.info == nil {
-		// This can happen on Solaris 10.  We don't know the
-		// code, just avoid a misleading value.
-		return _SI_USER + 1
-	}
 	return uint64(c.info.si_code)
 }
 
