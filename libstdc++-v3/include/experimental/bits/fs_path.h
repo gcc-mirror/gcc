fecfaafd91e88b8156694b6ae795d94d4914f488
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-05-13 20:12:06 +0000

    PR libstdc++/90454.cc path construction from void*
    Make the filesystem::path constructors SFINAE away for void* arguments,
    instead of giving an error due to iterator_traits<void*>::reference.
    
    	PR libstdc++/90454.cc path construction from void*
    	* include/bits/fs_path.h (path::_Path): Use remove_pointer so that
    	pointers to void are rejected as well as void.
    	* include/experimental/bits/fs_path.h (path::_Path): Likewise.
    	* testsuite/27_io/filesystem/path/construct/80762.cc: Also check
    	pointers to void.
    	* testsuite/experimental/filesystem/path/construct/80762.cc: Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@271134 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/experimental/bits/fs_path.h
+++ libstdc++-v3/include/experimental/bits/fs_path.h
@@ -128,11 +128,12 @@ _GLIBCXX_BEGIN_NAMESPACE_CXX11
       : decltype(__is_path_src(std::declval<_Source>(), 0))
       { };
 
-    template<typename _Tp1, typename _Tp2 = void>
+    template<typename _Tp1, typename _Tp2 = void,
+	     typename _Tp1_nocv = typename remove_cv<_Tp1>::type,
+	     typename _Tp1_noptr = typename remove_pointer<_Tp1>::type>
       using _Path = typename
-	std::enable_if<__and_<__not_<is_same<typename remove_cv<_Tp1>::type,
-					     path>>,
-			      __not_<is_void<_Tp1>>,
+	std::enable_if<__and_<__not_<is_same<_Tp1_nocv, path>>,
+			      __not_<is_void<_Tp1_noptr>>,
 			      __constructible_from<_Tp1, _Tp2>>::value,
 		       path>::type;
 
