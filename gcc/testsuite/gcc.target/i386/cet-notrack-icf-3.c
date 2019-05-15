92bde79946cff05b65eec25847a76d3778cf811d
Iain Sandoe <iain@sandoe.co.uk>
2019-05-15 07:10:04 +0000

    darwin, testsuite, laste piece to fix PR82920
    These ae test adjustments to the scan assembler strings
    mostly just to catch missing __USER_LABEL_PREFIX__s  on
    symbols.
    
    2019-05-15  Iain Sandoe  <iain@sandoe.co.uk>
    
    	PR target/82920
    	* g++.dg/cet-notrack-1.C: Adjust scan assembler for Darwin.
    	* gcc.target/i386/cet-notrack-5a.c: Likewise.
    	* gcc.target/i386/cet-notrack-5b.c: Likewise.
    	* gcc.target/i386/cet-notrack-6b.c: Likewise.
    	* gcc.target/i386/cet-notrack-icf-1.c: Likewise.
    	* gcc.target/i386/cet-notrack-icf-2.c: Likewise.
    	* gcc.target/i386/cet-notrack-icf-3.c: Likewise.
    	* gcc.target/i386/cet-notrack-icf-4.c: Likewise.
    	* gcc.target/i386/cet-sjlj-3.c: Likewise.
    	* gcc.target/i386/cet-sjlj-5.c: Likewise.
    
    From-SVN: r271199

--- gcc/testsuite/gcc.target/i386/cet-notrack-icf-3.c
+++ gcc/testsuite/gcc.target/i386/cet-notrack-icf-3.c
@@ -3,8 +3,8 @@
 /* { dg-options "-O2 -fcf-protection=none" } */
 /* { dg-final { scan-assembler-not "endbr" } } */
 /* { dg-final { scan-assembler-not "fn2:" } } */
-/* { dg-final { scan-assembler "set\[ \t]+fn2,fn1" } } */
-/* { dg-final { scan-assembler "set\[ \t]+fn3,fn1" } } */
+/* { dg-final { scan-assembler "set\[ \t]+fn2,fn1" { target { ! *-*-darwin* } } } } */
+/* { dg-final { scan-assembler "set\[ \t]+fn3,fn1" { target { ! *-*-darwin* } } } } */
 
 int (*foo)(int);
 
