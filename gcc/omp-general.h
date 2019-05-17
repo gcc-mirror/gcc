5f7ea2eebbef8715d22695470221dff57fe45c42
tschwinge <tschwinge@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-05-17 19:13:15 +0000

    [PR89433] Use 'oacc_verify_routine_clauses' for C/C++ OpenACC 'routine' directives
    	gcc/
    	PR middle-end/89433
    	* omp-general.c (oacc_build_routine_dims): Move some of its
    	processing into...
    	(oacc_verify_routine_clauses): ... this new function.
    	* omp-general.h (oacc_verify_routine_clauses): New prototype.
    	gcc/c/
    	PR c/89433
    	* c-parser.c (c_parser_oacc_routine): Normalize order of clauses.
    	(c_finish_oacc_routine): Call oacc_verify_routine_clauses.
    	gcc/cp/
    	PR c++/89433
    	* parser.c (cp_parser_oacc_routine)
    	(cp_parser_late_parsing_oacc_routine): Normalize order of clauses.
    	(cp_finalize_oacc_routine): Call oacc_verify_routine_clauses.
    	gcc/testsuite/
    	PR testsuite/89433
    	* c-c++-common/goacc/routine-2.c: Update, and move some test
    	into...
    	* c-c++-common/goacc/routine-level-of-parallelism-1.c: ... this
    	new file.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@271344 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/omp-general.h
+++ gcc/omp-general.h
@@ -84,6 +84,7 @@ extern tree oacc_launch_pack (unsigned code, tree device, unsigned op);
 extern tree oacc_replace_fn_attrib_attr (tree attribs, tree dims);
 extern void oacc_replace_fn_attrib (tree fn, tree dims);
 extern void oacc_set_fn_attrib (tree fn, tree clauses, vec<tree> *args);
+extern void oacc_verify_routine_clauses (tree *, location_t);
 extern tree oacc_build_routine_dims (tree clauses);
 extern tree oacc_get_fn_attrib (tree fn);
 extern bool offloading_function_p (tree fn);
