6e059f2c994c890d13c83047ea6a2673485b299b
msebor <msebor@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-05-17 23:14:09 +0000

    gcc/testsuite/ChangeLog:
    	* g++.dg/overload/conv-op1.C: Adjust text of expected messages.
    	* g++.dg/ubsan/pr63956.C: Same.
    	* g++.old-deja/g++.pt/assign1.C: Same.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@271364 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/overload/conv-op1.C
+++ gcc/testsuite/g++.dg/overload/conv-op1.C
@@ -10,7 +10,7 @@ struct A
 void f()
 {
   const int i = 42;
-  A()(i);			// { dg-message "<conversion>" }
+  A()(i);			// { dg-message "(conversion)" }
   // { dg-error "qualifiers" "" { target *-*-* } .-1 }
 }
 
