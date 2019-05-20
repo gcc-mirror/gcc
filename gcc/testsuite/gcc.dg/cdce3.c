5a12868e24592f445f98138b04670a2875ee00c3
clyon <clyon@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-05-20 15:01:46 +0000

    [testsuite] PR90106 Fix cdce3.c testcase
    2019-05-20  Christophe Lyon  <christophe.lyon@linaro.org>
    
    	PR tree-optimization/90106
    	* gcc.dg/cdce3.c: Add hard_float effective target.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@271424 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cdce3.c
+++ gcc/testsuite/gcc.dg/cdce3.c
@@ -1,6 +1,7 @@
 /* { dg-do compile } */
+/* { dg-require-effective-target hard_float } */
 /* { dg-options "-O2 -fmath-errno -fdump-tree-cdce-details -fdump-tree-optimized" } */
-/* { dg-final { scan-tree-dump "cdce3.c:9: \[^\n\r]* function call is shrink-wrapped into error conditions\." "cdce" } } */
+/* { dg-final { scan-tree-dump "cdce3.c:10: \[^\n\r]* function call is shrink-wrapped into error conditions\." "cdce" } } */
 /* { dg-final { scan-tree-dump "sqrtf \\(\[^\n\r]*\\); \\\[tail call\\\]" "optimized" } } */
 
 float sqrtf (float);
