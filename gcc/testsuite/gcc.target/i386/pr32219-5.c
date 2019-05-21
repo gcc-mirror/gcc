5f53d1f41a266a494e23cf7a0dd8af0261b693bf
iains <iains@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-05-21 16:33:48 +0000

    darwin, testsuite - fix PR 67958
    These tests require specific scan-asms in some cases because
    of the different codegen for Dawin.  Added some explanations
    too.
    
    2019-05-21  Iain Sandoe  <iain@sandoe.co.uk>
    
    	PR testsuite/67958
    	* gcc.target/i386/pr32219-1.c: Adjust scan-asms for Darwin, comment
    	the differences.
    	* gcc.target/i386/pr32219-2.c: Likewise.
    	* gcc.target/i386/pr32219-3.c: Likewise.
    	* gcc.target/i386/pr32219-4.c: Likewise.
    	* gcc.target/i386/pr32219-5.c: Likewise.
    	* gcc.target/i386/pr32219-6.c: Likewise.
    	* gcc.target/i386/pr32219-7.c: Likewise.
    	* gcc.target/i386/pr32219-8.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@271475 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/pr32219-5.c
+++ gcc/testsuite/gcc.target/i386/pr32219-5.c
@@ -11,7 +11,11 @@ foo ()
   return xxx;
 }
 
-/* { dg-final { scan-assembler "movl\[ \t\]xxx\\(%rip\\), %eax" { target { ! ia32 } } } } */
+/* { dg-final { scan-assembler "movl\[ \t\]_?xxx\\(%rip\\), %eax" { target { ! ia32 } } } } */
 /* { dg-final { scan-assembler-not "xxx@GOTPCREL" { target { ! ia32 } } } } */
-/* { dg-final { scan-assembler "movl\[ \t\]xxx@GOTOFF\\(%\[^,\]*\\), %eax" { target ia32 } } } */
-/* { dg-final { scan-assembler-not "movl\[ \t\]xxx@GOT\\(%\[^,\]*\\), %eax" { target ia32 } } } */
+
+/* { dg-final { scan-assembler "movl\[ \t\]xxx@GOTOFF\\(%\[^,\]*\\), %eax" { target { ia32 && { ! *-*-darwin* } } } } } */
+/* { dg-final { scan-assembler-not "movl\[ \t\]xxx@GOT\\(%\[^,\]*\\), %eax" { target { ia32 && { ! *-*-darwin* } } } } } */
+
+/* For Darwin m32, we need PIC (the default) to allow PIE.  */
+/* { dg-final { scan-assembler {movl[ \t]_xxx-L1\$pb\(%eax\),[ \t]%eax} { target { ia32 && *-*-darwin* } } } } */
