647551f1bd7a8964130b494b1a713914a34c4233
jb <jb@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-05-22 11:56:01 +0000

    fortran/89100: Default widths with -fdec-format-defaults
    gcc/fortran ChangeLog:
    
    2019-05-22  Jeff Law  <law@redhat.com>
                Mark Eggleston  <mark.eggleston@codethink.com>
    
            PR fortran/89100
            * gfortran.texi: Add Default widths for F, G and I format
            descriptors to Extensions section.
            * invoke.texi: Add -fdec-format-defaults
            * io.c (check_format): Use default widths for i, f and g when
            flag_dec_format_defaults is enabled.
            * lang.opt: Add new option.
            * options.c (set_dec_flags): Add SET_BITFLAG for
            flag_dec_format_defaults.
    
    
    gcc/testsuite ChangeLog:
    
    2019-05-22  Mark Eggleston  <mark.eggleston@codethink.com>
    
            PR fortran/89100
            * gfortran.dg/fmt_f_default_field_width_1.f90: New test.
            * gfortran.dg/fmt_f_default_field_width_2.f90: New test.
            * gfortran.dg/fmt_f_default_field_width_3.f90: New test.
            * gfortran.dg/fmt_g_default_field_width_1.f90: New test.
            * gfortran.dg/fmt_g_default_field_width_2.f90: New test.
            * gfortran.dg/fmt_g_default_field_width_3.f90: New test.
            * gfortran.dg/fmt_i_default_field_width_1.f90: New test.
            * gfortran.dg/fmt_i_default_field_width_2.f90: New test.
            * gfortran.dg/fmt_i_default_field_width_3.f90: New test.
    
    
    libgfortran ChangeLog:
    
    2019-05-22  Jeff Law  <law@redhat.com>
    
            PR fortran/89100
            * io/format.c (parse_format_list): set default width when the
            IOPARM_DT_DEC_EXT flag is set for i, f and g.
            * io/io.h: add default_width_for_integer, default_width_for_float
            and default_precision_for_float.
            * io/write.c (write_boz): extra parameter giving length of data
            corresponding to the type's kind.
            (write_b): pass data length as extra parameter in calls to
            write_boz.
            (write_o): pass data length as extra parameter in calls to
            write_boz.
            (write_z): pass data length as extra parameter in calls to
            write_boz.
            (size_from_kind): also set size is default width is set.
            * io/write_float.def (build_float_string): new paramter inserted
            before result parameter. If default width use values passed
            instead of the values in fnode.
            (FORMAT_FLOAT): macro modified to check for default width and
            calls to build_float_string to pass in default width.
            (get_float_string): set width and precision to defaults when
            needed.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@271511 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/fmt_g_default_field_width_2.f90
@@ -0,0 +1,48 @@
+! { dg-do run }
+! { dg-options -fdec-format-defaults }
+!
+! Test case for the default field widths enabled by the -fdec-format-defaults flag.
+!
+! This feature is not part of any Fortran standard, but it is supported by the
+! Oracle Fortran compiler and others.
+!
+! Test case added by Mark Eggleston <mark.eggleston@codethink.com> to check
+! use of -fdec-format-defaults
+!
+
+program test
+    character(50) :: buffer
+
+    real(4) :: real_4
+    real(8) :: real_8
+    real(16) :: real_16
+    integer :: len
+    character(*), parameter :: fmt = "(A, G, A)"
+
+    real_4 = 4.18
+    write(buffer, fmt) ':',real_4,':'
+    print *,buffer
+    if (buffer.ne.":   4.180000    :") stop 1
+
+    real_4 = 0.00000018
+    write(buffer, fmt) ':',real_4,':'
+    print *,buffer
+    if (buffer.ne.":  0.1800000E-06:") stop 2
+
+    real_4 = 18000000.4
+    write(buffer, fmt) ':',real_4,':'
+    print *,buffer
+    if (buffer.ne.":  0.1800000E+08:") stop 3
+
+    real_8 = 4.18
+    write(buffer, fmt) ':',real_8,':'
+    print *,buffer
+    len = len_trim(buffer)
+    if (len /= 27) stop 4
+
+    real_16 = 4.18
+    write(buffer, fmt) ':',real_16,':'
+    print *,buffer
+    len = len_trim(buffer)
+    if (len /= 44) stop 5
+end
