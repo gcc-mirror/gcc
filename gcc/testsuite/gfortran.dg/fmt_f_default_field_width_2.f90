532beba3aad23146e1b544d010adc00abb4ec58e
kargl <kargl@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-05-23 17:20:58 +0000

    2019-05-23  Mark Eggleston  <mark.eggleston@codethink.com>
    	* gfortran.dg/fmt_f_default_field_width_1.f90: Hide REAL(16) behind
    	__GFC_REAL_16__.  Add -cpp to dg-options.
    	* gfortran.dg/fmt_f_default_field_width_2.f90: Ditto.
    	* gfortran.dg/fmt_f_default_field_width_3.f90: Ditto.
    	* gfortran.dg/fmt_g_default_field_width_1.f90: Ditto.
    	* gfortran.dg/fmt_g_default_field_width_2.f90: Ditto.
    	* gfortran.dg/fmt_g_default_field_width_3.f90: Ditto.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@271573 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/fmt_f_default_field_width_2.f90
+++ gcc/testsuite/gfortran.dg/fmt_f_default_field_width_2.f90
@@ -1,5 +1,5 @@
 ! { dg-do run }
-! { dg-options -fdec-format-defaults }
+! { dg-options "-cpp -fdec-format-defaults" }
 !
 ! Test case for the default field widths enabled by the -fdec-format-defaults flag.
 !
@@ -11,11 +11,14 @@
 !
 
 program test
+    implicit none
     character(50) :: buffer
 
     real(4) :: real_4
     real(8) :: real_8
+#ifdef __GFC_REAL_16__
     real(16) :: real_16
+#endif
     integer :: len
     character(*), parameter :: fmt = "(A, F, A)"
 
@@ -35,9 +38,11 @@ program test
     len = len_trim(buffer)
     if (len /= 27) stop 3
 
+#ifdef __GFC_REAL_16__
     real_16 = 4.18
     write(buffer, fmt) ':',real_16,':'
     print *,buffer
     len = len_trim(buffer)
     if (len /= 44) stop 4
+#endif
 end
