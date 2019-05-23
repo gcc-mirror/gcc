8a6d0554342d6495414866a24ba7e0cecb7ee48d
iains <iains@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-05-23 09:23:47 +0000

    x86, testsuite - update fuse-caller-save tests.
    These tests had started to XPASS on pic targets where
    the codegen is now as expected.
    
    gcc/testsuite/
    
    2019-05-23  Iain Sandoe  <iain@sandoe.co.uk>
    
    	PR rtl-optimisation/64895
    	* gcc.target/i386/fuse-caller-save-rec.c: Remove XFAILs.
    	* gcc.target/i386/fuse-caller-save.c: Likewise.
    	* gcc.target/i386/fuse-caller-save-xmm.c: Adjust tests for
    	PIC cases, remove XFAILs.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@271544 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/fuse-caller-save.c
+++ gcc/testsuite/gcc.target/i386/fuse-caller-save.c
@@ -16,11 +16,9 @@ foo (int y)
   return y + bar (y);
 }
 
-/* For !nonpic && ia32 xfails, see PR64895.  */
-
 /* Check that no registers are saved/restored. */
-/* { dg-final { scan-assembler-not "push" { xfail { { ! nonpic } && ia32 } } } } */
-/* { dg-final { scan-assembler-not "pop" { xfail { { ! nonpic } && ia32 } } } } */
+/* { dg-final { scan-assembler-not "push" } } */
+/* { dg-final { scan-assembler-not "pop" } } */
 
 /* PR61605.  If the first argument register and the return register differ, then
    bar leaves the first argument register intact.  That means in foo that the
@@ -31,4 +29,4 @@ foo (int y)
 /* { dg-final { scan-assembler-not "movl" { target { ! ia32 } } } } */
 
 /* Check that addition uses di (in case of no copy) or dx (in case of copy). */
-/* { dg-final { scan-assembler-times "addl\t%\[re\]?d\[ix\], %\[re\]?ax" 1 { xfail { { ! nonpic } && ia32 } } } } */
+/* { dg-final { scan-assembler-times "addl\t%\[re\]?d\[ix\], %\[re\]?ax" 1 } } */
