392d2ff3bd6fc66a6683540fc33910ddef9bf6f1
Segher Boessenkool <segher@kernel.crashing.org>
2019-05-28 13:45:48 +0200

    rs6000: Improve p9-dimode* testcases
    This removes the unnecessary restriction to 32-bit (all three ways).
    It also scans for mtvsr*, not just mtvsrd.  Finally, it uses the "wa"
    constraints instead of "wi" in the inline asm statements.
    
    
    gcc/testsuite/
    	* gcc.target/powerpc/p9-dimode1.c: Don't restrict to -m64.  Check for
    	all mtvsr*, not just mtvsrd.  Use "wa" instead of "wi" constraints.
    	* gcc.target/powerpc/p9-dimode2.c: Ditto.
    
    From-SVN: r271696

--- gcc/testsuite/gcc.target/powerpc/p9-dimode2.c
+++ gcc/testsuite/gcc.target/powerpc/p9-dimode2.c
@@ -1,13 +1,9 @@
-/* { dg-do compile { target { powerpc64*-*-* && lp64 } } } */
+/* { dg-do compile { target { powerpc*-*-* } } } */
 /* { dg-require-effective-target powerpc_p9vector_ok } */
 /* { dg-options "-mdejagnu-cpu=power9 -O2" } */
 
-/* Verify that large integer constants are loaded via direct move instead of being
-   loaded from memory.  */
-
-#ifndef _ARCH_PPC64
-#error "This code is 64-bit."
-#endif
+/* Verify that large integer constants are loaded via direct move instead of
+   being loaded from memory.  */
 
 double
 p9_large (void)
@@ -15,12 +11,12 @@ p9_large (void)
   long l = 0x12345678;
   double ret;
 
-  __asm__ ("xxlor %x0,%x1,%x1" : "=&d" (ret) : "wi" (l));
+  __asm__ ("xxlor %x0,%x1,%x1" : "=&d" (ret) : "wa" (l));
 
   return ret;
 }
 
-/* { dg-final { scan-assembler     {\mmtvsrd\M} } } */
+/* { dg-final { scan-assembler     {\mmtvsr}    } } */
 /* { dg-final { scan-assembler-not {\mld\M}     } } */
 /* { dg-final { scan-assembler-not {\mlfd\M}    } } */
 /* { dg-final { scan-assembler-not {\mlxsd\M}   } } */
