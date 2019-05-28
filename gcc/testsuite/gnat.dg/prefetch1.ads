804f7d390a7c68875f8f6a5ca53a7e8eb77f5ed7
ebotcazou <ebotcazou@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-05-28 07:21:07 +0000

    * gcc-interface/decl.c (intrin_arglists_compatible_p): Do not return 	false if the internal builtin uses a variable list.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@271679 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gnat.dg/prefetch1.ads
@@ -0,0 +1,9 @@
+with System;
+
+package Prefetch1 is
+
+  procedure My_Proc1 (Addr : System.Address);
+  procedure My_Proc2 (Addr : System.Address);
+  procedure My_Proc3 (Addr : System.Address);
+
+end Prefetch1;
