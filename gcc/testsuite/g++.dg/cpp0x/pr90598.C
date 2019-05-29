874f88c43bc504d49228f53365d2fd565077ecdd
Jakub Jelinek <jakub@redhat.com>
2019-05-29 23:33:18 +0200

    re PR c++/90598 (Return type of explicit destructor call wrong)
    	PR c++/90598
    	* tree.c (lvalue_kind): Return clk_none for expressions with
    	with VOID_TYPE_P.
    
    	* g++.dg/cpp0x/pr90598.C: New test.
    
    From-SVN: r271752

---
+++ gcc/testsuite/g++.dg/cpp0x/pr90598.C
@@ -0,0 +1,16 @@
+// PR c++/90598
+// { dg-do compile { target c++11 } }
+
+struct A {};
+using B = decltype(A ().~A ());
+template <typename T> struct C;
+template <> struct C<void> {};
+C<B> t;
+// PR c++/90598
+// { dg-do compile { target c++11 } }
+
+struct A {};
+using B = decltype(A ().~A ());
+template <typename T> struct C;
+template <> struct C<void> {};
+C<B> t;
