6fbba0c7ce093dac744a30f7cd549e122009c135
Iain Sandoe <iain@sandoe.co.uk>
2019-05-30 08:00:45 +0000

    Darwin, backport fix for PR82920 part3 (other CET test fixes).
    gcc/testsuite/
    
    2019-05-30  Iain Sandoe  <iain@sandoe.co.uk>
    
    	Backport from mainline.
    	2019-05-15  Iain Sandoe  <iain@sandoe.co.uk>
    
    	PR target/82920
    	* g++.dg/cet-notrack-1.C: Adjust scan assembler for Darwin.
    	* gcc.target/i386/cet-notrack-5a.c: Likewise.
    	* gcc.target/i386/cet-notrack-5b.c: Likewise.
    	* gcc.target/i386/cet-notrack-6b.c: Likewise.
    	* gcc.target/i386/cet-notrack-icf-1.c: Likewise.
    	* gcc.target/i386/cet-notrack-icf-2.c: Likewise.
    	* gcc.target/i386/cet-notrack-icf-3.c: Likewise.
    	* gcc.target/i386/cet-notrack-icf-4.c: Likewise.
    	* gcc.target/i386/cet-sjlj-3.c: Likewise.
    	* gcc.target/i386/cet-sjlj-5.c: Likewise.
    
    From-SVN: r271766

--- gcc/testsuite/gcc.target/i386/cet-notrack-6b.c
+++ gcc/testsuite/gcc.target/i386/cet-notrack-6b.c
@@ -2,7 +2,8 @@
 /* { dg-options "-O0 -fcf-protection" } */
 /* { dg-final { scan-assembler-times "endbr32" 1 { target ia32 } } } */
 /* { dg-final { scan-assembler-times "endbr64" 1 { target { ! ia32 } } } } */
-/* { dg-final { scan-assembler-not "\tcall\[ \t]+" } } */
+/* { dg-final { scan-assembler-not "\tcall\[ \t]+" { target { ! ia32 } || { ! *-*-darwin* } } } } */
+/* { dg-final { scan-assembler-times "\tcall\[ \t]+" 1 { target { ia32 && *-*-darwin* } } } } */
 /* { dg-final { scan-assembler-times "notrack call\[ \t]+" 1 } } */
 
 int foo (int arg);
