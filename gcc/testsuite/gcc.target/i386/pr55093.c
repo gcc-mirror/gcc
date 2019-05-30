3b0eb909e04f79043d796acdbd5402099cac0b48
iains <iains@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-05-30 07:56:10 +0000

    Darwin, backport fix for PR82920 part2 (mx32 is not supported)
    gcc/
    
    2019-05-30  Iain Sandoe  <iain@sandoe.co.uk>
    
    	Backport from mainline.
    	2019-05-12  Iain Sandoe  <iain@sandoe.co.uk>
    
    	PR target/82920
    	* config/i386/darwin.h (CC1_SPEC): Report -mx32 as an error for
    	Darwin.
    
    gcc/testsuite/
    
    2019-05-30  Iain Sandoe  <iain@sandoe.co.uk>
    
    	Backport from mainline.
    	2019-05-14  Iain Sandoe  <iain@sandoe.co.uk>
    
    	PR target/82920
    	* gcc.target/i386/cet-sjlj-6b.c: Require effective target x32.
    	* gcc.target/i386/pr52146.c: Likewise.
    	* gcc.target/i386/pr52698.c: Likewise.
    	* gcc.target/i386/pr52857-1.c: Likewise.
    	* gcc.target/i386/pr52857-2.c: Likewise.
    	* gcc.target/i386/pr52876.c: Likewise.
    	* gcc.target/i386/pr53698.c: Likewise.
    	* gcc.target/i386/pr54157.c: Likewise.
    	* gcc.target/i386/pr55049-1.c: Likewise.
    	* gcc.target/i386/pr55093.c: Likewise.
    	* gcc.target/i386/pr55116-1.c: Likewise.
    	* gcc.target/i386/pr55116-2.c: Likewise.
    	* gcc.target/i386/pr55597.c: Likewise.
    	* gcc.target/i386/pr59929.c: Likewise.
    	* gcc.target/i386/pr66470.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-9-branch@271765 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/pr55093.c
+++ gcc/testsuite/gcc.target/i386/pr55093.c
@@ -1,4 +1,5 @@
 /* { dg-do compile { target { ! ia32 } } } */
+/* { dg-require-effective-target maybe_x32 } */
 /* { dg-options "-O2 -mx32 -maddress-mode=long" } */
 /* { dg-skip-if "different ABI" { x86_64-*-mingw* } } */
 
