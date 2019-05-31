3a401f9ba1040a7a3bc6fe2c5acbcec6c2620748
Kwok Cheung Yeung <kcy@codesourcery.com>
2019-05-31 12:25:03 -0700

    Fix expected messages in goacc tests
    The expected messages in the OpenACC kernel-related tests should be prefixed
    with 'optimized:' rather than 'note:'.
    
    2019-05-31  Kwok Cheung Yeung  <kcy@codesourcery.com>
    
    	gcc/testsuite/
    	* c-c++-common/goacc/kernels-decompose-1.c: Change 'note:' to
    	'optimized:'.  Fix typo.
    	* c-c++-common/goacc/note-parallelism-1-kernels-conditional-loop-independent_seq.c:
    	Change 'note:' to 'optimized:'.
    	* c-c++-common/goacc/note-parallelism-1-kernels-loop-auto.c: Likewise.
    	* c-c++-common/goacc/note-parallelism-1-kernels-loop-independent_seq.c:
    	Likewise.
    	* c-c++-common/goacc/note-parallelism-1-kernels-loops.c: Likewise.
    	* c-c++-common/goacc/note-parallelism-1-kernels-straight-line.c:
    	Likewise.
    	* c-c++-common/goacc/note-parallelism-combined-kernels-loop-auto.c:
    	Likewise.
    	* c-c++-common/goacc/note-parallelism-combined-kernels-loop-independent_seq.c:
    	Likewise.
    	* c-c++-common/goacc/note-parallelism-kernels-conditional-loop-independent_seq.c:
    	Likewise.
    	* c-c++-common/goacc/note-parallelism-kernels-loop-auto.c: Likewise.
    	* c-c++-common/goacc/note-parallelism-kernels-loop-independent_seq.c:
    	Likewise.
    	* c-c++-common/goacc/note-parallelism-kernels-loops.c: Likewise.
    
    (cherry picked from openacc-gcc-9-branch commit
    09d9f4e78c1f68227fccd75d7d5b8b4a952c2b97)

--- gcc/testsuite/c-c++-common/goacc/kernels-decompose-1.c
+++ gcc/testsuite/c-c++-common/goacc/kernels-decompose-1.c
@@ -30,92 +30,92 @@ main ()
 
 #pragma acc kernels
   {
-    x = 0; /* { dg-message "note: beginning .gang-single. region in OpenACC .kernels. construct" } */
+    x = 0; /* { dg-message "optimized: beginning .gang-single. region in OpenACC .kernels. construct" } */
     y = x < 10;
     z = x++;
     ;
   }
 
 #pragma acc kernels
-  for (int i = 0; i < N; i++) /* { dg-message "note: beginning .parloops. region in OpenACC .kernels. construct" } */
+  for (int i = 0; i < N; i++) /* { dg-message "optimized: beginning .parloops. region in OpenACC .kernels. construct" } */
     a[i] = 0;
 
 #pragma acc kernels loop
-  /* { dg-message "note: forwarded loop nest in OpenACC .kernels. construct to .parloops. for analysis" "" { target *-*-* } .-1 } */
+  /* { dg-message "optimized: forwarded loop nest in OpenACC .kernels. construct to .parloops. for analysis" "" { target *-*-* } .-1 } */
   for (int i = 0; i < N; i++)
     b[i] = a[N - i - 1];
 
 #pragma acc kernels
   {
 #pragma acc loop
-    /* { dg-message "note: forwarded loop nest in OpenACC .kernels. construct to .parloops. for analysis" "" { target *-*-* } .-1 } */
+    /* { dg-message "optimized: forwarded loop nest in OpenACC .kernels. construct to .parloops. for analysis" "" { target *-*-* } .-1 } */
     for (int i = 0; i < N; i++)
       b[i] = a[N - i - 1];
 
 #pragma acc loop
-    /* { dg-message "note: forwarded loop nest in OpenACC .kernels. construct to .parloops. for analysis" "" { target *-*-* } .-1 } */
+    /* { dg-message "optimized: forwarded loop nest in OpenACC .kernels. construct to .parloops. for analysis" "" { target *-*-* } .-1 } */
     for (int i = 0; i < N; i++)
       c[i] = a[i] * b[i];
 
-    a[z] = 0; /* { dg-message "note: beginning .gang-single. region in OpenACC .kernels. construct" } */
+    a[z] = 0; /* { dg-message "optimized: beginning .gang-single. region in OpenACC .kernels. construct" } */
 
 #pragma acc loop
-    /* { dg-message "note: forwarded loop nest in OpenACC .kernels. construct to .parloops. for analysis" "" { target *-*-* } .-1 } */
+    /* { dg-message "optimized: forwarded loop nest in OpenACC .kernels. construct to .parloops. for analysis" "" { target *-*-* } .-1 } */
     for (int i = 0; i < N; i++)
       c[i] += a[i];
 
-#pragma acc loop seq /* { dg-message "note: assigned OpenACC seq loop parallelism" } */
-    /* { dg-message "note: parallelized loop nest in OpenACC .kernels. construct" "" { target *-*-* } .-1 } */
+#pragma acc loop seq /* { dg-message "optimized: assigned OpenACC seq loop parallelism" } */
+    /* { dg-message "optimized: parallelized loop nest in OpenACC .kernels. construct" "" { target *-*-* } .-1 } */
     for (int i = 0 + 1; i < N; i++)
       c[i] += c[i - 1];
   }
 
 #pragma acc kernels
   {
-#pragma acc loop independent /* { dg-message "note: assigned OpenACC gang loop parallelism" } */
-    /* { dg-message "note: parallelized loop nest in OpenACC .kernels. construct" "" { target *-*-* } .-1 } */
+#pragma acc loop independent /* { dg-message "optimized: assigned OpenACC gang loop parallelism" } */
+    /* { dg-message "optimized: parallelized loop nest in OpenACC .kernels. construct" "" { target *-*-* } .-1 } */
     for (int i = 0; i < N; ++i)
-#pragma acc loop independent /* { dg-message "note: assigned OpenACC worker loop parallelism" } */
+#pragma acc loop independent /* { dg-message "optimized: assigned OpenACC worker loop parallelism" } */
       for (int j = 0; j < N; ++j)
-#pragma acc loop independent /* { dg-message "note: assigned OpenACC seq loop parallelism" } */
+#pragma acc loop independent /* { dg-message "optimized: assigned OpenACC seq loop parallelism" } */
 	 /* { dg-warning "insufficient partitioning available to parallelize loop" "" { target *-*-* } .-1 } */
 	for (int k = 0; k < N; ++k)
 	  a[(i + j + k) % N]
 	    = b[j]
-	    + f_v (c[k]); /* { dg-message "note: assigned OpenACC vector loop parallelism" } */
+	    + f_v (c[k]); /* { dg-message "optimized: assigned OpenACC vector loop parallelism" } */
 
     //TODO Should the following turn into "gang-single" instead of "parloops"?
     //TODO The problem is that the first STMT is "if (y <= 4) goto <D.2547>; else goto <D.2548>;", thus "parloops".
-    if (y < 5) /* { dg-message "note: beginning .parloops. region in OpenACC .kernels. construct" } */
-#pragma acc loop independent /* { dg-message "note: unparallelized loop nest in OpenACC .kernels. region: it's executed conditionally" } */
+    if (y < 5) /* { dg-message "optimized: beginning .parloops. region in OpenACC .kernels. construct" } */
+#pragma acc loop independent /* { dg-message "optimized: unparallelized loop nest in OpenACC .kernels. region: it's executed conditionally" } */
       for (int j = 0; j < N; ++j)
 	b[j] = f_w (c[j]);
   }
 
-#pragma acc kernels /* { dg-warning "region contains gang partitoned code but is not gang partitioned" } */
+#pragma acc kernels /* { dg-warning "region contains gang partitioned code but is not gang partitioned" } */
   {
-    /* { dg-message "note: beginning .gang-single. region in OpenACC .kernels. construct" "" { target *-*-* } .+1 } */
-    y = f_g (a[5]); /* { dg-message "note: assigned OpenACC gang worker vector loop parallelism" } */
+    /* { dg-message "optimized: beginning .gang-single. region in OpenACC .kernels. construct" "" { target *-*-* } .+1 } */
+    y = f_g (a[5]); /* { dg-message "optimized: assigned OpenACC gang worker vector loop parallelism" } */
 
-#pragma acc loop independent /* { dg-message "note: assigned OpenACC gang loop parallelism" } */
-    /* { dg-message "note: parallelized loop nest in OpenACC .kernels. construct" "" { target *-*-* } .-1 } */
+#pragma acc loop independent /* { dg-message "optimized: assigned OpenACC gang loop parallelism" } */
+    /* { dg-message "optimized: parallelized loop nest in OpenACC .kernels. construct" "" { target *-*-* } .-1 } */
     for (int j = 0; j < N; ++j)
-      b[j] = y + f_w (c[j]); /* { dg-message "note: assigned OpenACC worker vector loop parallelism" } */
+      b[j] = y + f_w (c[j]); /* { dg-message "optimized: assigned OpenACC worker vector loop parallelism" } */
   }
 
 #pragma acc kernels
   {
-    y = 3; /* { dg-message "note: beginning .gang-single. region in OpenACC .kernels. construct" } */
+    y = 3; /* { dg-message "optimized: beginning .gang-single. region in OpenACC .kernels. construct" } */
 
-#pragma acc loop independent /* { dg-message "note: assigned OpenACC gang worker loop parallelism" } */
-    /* { dg-message "note: parallelized loop nest in OpenACC .kernels. construct" "" { target *-*-* } .-1 } */
+#pragma acc loop independent /* { dg-message "optimized: assigned OpenACC gang worker loop parallelism" } */
+    /* { dg-message "optimized: parallelized loop nest in OpenACC .kernels. construct" "" { target *-*-* } .-1 } */
     for (int j = 0; j < N; ++j)
-      b[j] = y + f_v (c[j]); /* { dg-message "note: assigned OpenACC vector loop parallelism" } */
+      b[j] = y + f_v (c[j]); /* { dg-message "optimized: assigned OpenACC vector loop parallelism" } */
 
-    z = 2; /* { dg-message "note: beginning .gang-single. region in OpenACC .kernels. construct" } */
+    z = 2; /* { dg-message "optimized: beginning .gang-single. region in OpenACC .kernels. construct" } */
   }
 
-#pragma acc kernels /* { dg-message "note: beginning .gang-single. region in OpenACC .kernels. construct" } */
+#pragma acc kernels /* { dg-message "optimized: beginning .gang-single. region in OpenACC .kernels. construct" } */
   ;
 
   return 0;
