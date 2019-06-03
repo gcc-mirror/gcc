fbd4b7f39ee26f1258aff5b86ad0fba06f3edd5e
Segher Boessenkool <segher@kernel.crashing.org>
2019-06-04 00:36:24 +0200

    rs6000: Delete -mmfpgpr
    This patch makes the -mmfpgpr option not do anything except warn that
    the option is deprecated.
    
    
    	* config/rs6000/rs6000.h (MASK_MFPGPR): Delete.
    	* config/rs6000/rs6000.c (direct_move_p): Adjust.
    	(rs6000_secondary_reload_simple_move): Adjust.
    	(rs6000_opt_masks): Neuter the "mfpgpr" option.
    	* config/rs6000/rs6000-c.c (rs6000_target_modify_macros): Adjust.
    	* config/rs6000/rs6000-cpus.def (ISA_2_5_MASKS_EMBEDDED): Adjust
    	comment.
    	(power6x): Adjust.
    	* config/rs6000/rs6000.md (floatsi<mode>2_lfiwax): Adjust.
    	(floatunssi<mode>2_lfiwzx): Adjust.
    	(fix_trunc<mode>si2_stfiwx): Adjust.
    	(fixuns_trunc<mode>si2_stfiwx): Adjust.
    	* config/rs6000/rs6000.opt (mno-mfpgpr): New.
    	(mfpgpr): Mark as deprecated.
    	* doc/extend.texi (PowerPC Function Attributes): Delete mfpgpr.
    	(Basic PowerPC Built-in Functions Available on ISA 2.05): Adjust.
    	* doc/invoke.texi (RS/6000 and PowerPC Options): Delete -mmfpgpr.
    
    gcc/testsuite/
    	* gcc.target/powerpc/mmfpgpr.c: Delete.
    
    From-SVN: r271889

--- gcc/config/rs6000/rs6000-c.c
+++ gcc/config/rs6000/rs6000-c.c
@@ -430,8 +430,6 @@ rs6000_target_modify_macros (bool define_p, HOST_WIDE_INT flags,
     rs6000_define_or_undefine_macro (define_p, "_ARCH_PWR5X");
   if ((flags & OPTION_MASK_CMPB) != 0)
     rs6000_define_or_undefine_macro (define_p, "_ARCH_PWR6");
-  if ((flags & OPTION_MASK_MFPGPR) != 0)
-    rs6000_define_or_undefine_macro (define_p, "_ARCH_PWR6X");
   if ((flags & OPTION_MASK_POPCNTD) != 0)
     rs6000_define_or_undefine_macro (define_p, "_ARCH_PWR7");
   /* Note that the OPTION_MASK_DIRECT_MOVE flag is automatically
