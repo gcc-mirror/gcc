b8e2acff118bfa0dcd3aaf299e1f78dfc5266cbd
Martin Liska <mliska@suse.cz>
2019-06-07 07:33:11 +0200

    Add warn_unused_result for malloc-like functions (PR tree-optimization/78902).
    2019-06-07  Martin Liska  <mliska@suse.cz>
    
    	PR tree-optimization/78902
    	* builtin-attrs.def (ATTR_WARN_UNUSED_RESULT): New.
    	(ATTR_MALLOC_NOTHROW_LEAF_LIST): Remove.
    	(ATTR_WARN_UNUSED_RESULT_NOTHROW_LEAF_LIST): New.
    	(ATTR_MALLOC_WARN_UNUSED_RESULT_NOTHROW_LEAF_LIST): New.
    	(ATTR_ALLOC_SIZE_2_NOTHROW_LIST): Remove.
    	(ATTR_MALLOC_SIZE_1_NOTHROW_LEAF_LIST): Remove.
    	(ATTR_MALLOC_WARN_UNUSED_RESULT_NOTHROW_LIST): New.
    	(ATTR_ALLOC_WARN_UNUSED_RESULT_SIZE_2_NOTHROW_LIST): New.
    	(ATTR_MALLOC_WARN_UNUSED_RESULT_SIZE_1_NOTHROW_LEAF_LIST): New.
    	(ATTR_ALLOCA_SIZE_1_NOTHROW_LEAF_LIST): Remove.
    	(ATTR_ALLOCA_WARN_UNUSED_RESULT_SIZE_1_NOTHROW_LEAF_LIST): New.
    	(ATTR_MALLOC_SIZE_1_2_NOTHROW_LEAF_LIST):  Remove.
    	(ATTR_MALLOC_WARN_UNUSED_RESULT_SIZE_1_2_NOTHROW_LEAF_LIST):
    	New.
    	(ATTR_ALLOC_SIZE_2_NOTHROW_LEAF_LIST): Remove.
    	(ATTR_ALLOC_WARN_UNUSED_RESULT_SIZE_2_NOTHROW_LEAF_LIST): New.
    	(ATTR_MALLOC_NOTHROW_NONNULL): Remove.
    	(ATTR_WARN_UNUSED_RESULT_NOTHROW_NONNULL): New.
    	(ATTR_MALLOC_WARN_UNUSED_RESULT_NOTHROW_NONNULL): New.
    	(ATTR_MALLOC_NOTHROW_NONNULL_LEAF): Remove.
    	(ATTR_WARN_UNUSED_RESULT_NOTHROW_NONNULL_LEAF): New.
    	(ATTR_MALLOC_WARN_UNUSED_RESULT_NOTHROW_NONNULL_LEAF): New.
    	* builtins.def (BUILT_IN_ALIGNED_ALLOC): Change to use
    	warn_unused_result attribute.
    	(BUILT_IN_STRDUP): Likewise.
    	(BUILT_IN_STRNDUP): Likewise.
    	(BUILT_IN_ALLOCA): Likewise.
    	(BUILT_IN_CALLOC): Likewise.
    	(BUILT_IN_MALLOC): Likewise.
    	(BUILT_IN_REALLOC): Likewise.
    2019-06-07  Martin Liska  <mliska@suse.cz>
    
    	PR tree-optimization/78902
    	* c-c++-common/asan/alloca_loop_unpoisoning.c: Use result
    	of __builtin_alloca.
    	* c-c++-common/asan/pr88619.c: Likewise.
    	* g++.dg/overload/using2.C: Likewise for malloc.
    	* gcc.dg/attr-alloc_size-5.c: Add new dg-warning.
    	* gcc.dg/nonnull-3.c: Use result of __builtin_strdup.
    	* gcc.dg/pr43643.c: Likewise.
    	* gcc.dg/pr59717.c: Likewise for calloc.
    	* gcc.dg/torture/pr71816.c: Likewise.
    	* gcc.dg/tree-ssa/pr78886.c: Likewise.
    	* gcc.dg/tree-ssa/pr79697.c: Likewise.
    	* gcc.dg/pr78902.c: New test.
    
    From-SVN: r272028

--- gcc/testsuite/gcc.dg/tree-ssa/pr79697.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr79697.c
@@ -3,17 +3,17 @@
 
 void f(void)
 {
-  __builtin_strdup ("abc");
+  __builtin_strdup ("abc"); /* { dg-warning "ignoring return value of '__builtin_strdup' declared with attribute 'warn_unused_result'" } */
 }
 
 void g(void)
 {
-  __builtin_strndup ("abc", 3);
+  __builtin_strndup ("abc", 3); /* { dg-warning "ignoring return value of '__builtin_strndup' declared with attribute 'warn_unused_result'" } */
 }
 
 void h(void)
 {
-  __builtin_realloc (0, 10);
+  __builtin_realloc (0, 10); /* { dg-warning "ignoring return value of '__builtin_realloc' declared with attribute 'warn_unused_result'" } */
 }
 
 /* { dg-final { scan-tree-dump "Deleting : __builtin_strdup" "cddce1" } } */
