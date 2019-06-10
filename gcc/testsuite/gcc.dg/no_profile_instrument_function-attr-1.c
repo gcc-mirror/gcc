bf4c25cb334f1449db611d833b817069646daf3f
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-06-10 11:28:49 +0000

    Update __gcov_* scanned function name in a test-case.
    2019-06-10  Martin Liska  <mliska@suse.cz>
    
    	* gcc.dg/no_profile_instrument_function-attr-1.c: Fix
    	function name.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@272116 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/no_profile_instrument_function-attr-1.c
+++ gcc/testsuite/gcc.dg/no_profile_instrument_function-attr-1.c
@@ -19,6 +19,6 @@ int main ()
 }
 
 /* { dg-final { scan-tree-dump-times "__gcov0\\.main.* = PROF_edge_counter" 1 "optimized"} } */
-/* { dg-final { scan-tree-dump-times "__gcov_indirect_call_profiler_v3" 1 "optimized" } } */
+/* { dg-final { scan-tree-dump-times "__gcov_indirect_call_profiler_v" 1 "optimized" } } */
 /* { dg-final { scan-tree-dump-times "__gcov_time_profiler_counter = " 1 "optimized" } } */
 /* { dg-final { scan-tree-dump-times "__gcov_init" 1 "optimized" } } */
