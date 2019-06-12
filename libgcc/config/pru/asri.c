ae7deb4b85aad67c64228c19e226b902669032f4
Dimitar Dimitrov <dimitar@dinux.eu>
2019-06-12 19:28:51 +0000

    Initial TI PRU libgcc port
    libgcc/ChangeLog:
    
    2019-06-12  Dimitar Dimitrov  <dimitar@dinux.eu>
    
    	* config.host: Add PRU target.
    	* config/pru/asri.c: New file.
    	* config/pru/eqd.c: New file.
    	* config/pru/eqf.c: New file.
    	* config/pru/ged.c: New file.
    	* config/pru/gef.c: New file.
    	* config/pru/gtd.c: New file.
    	* config/pru/gtf.c: New file.
    	* config/pru/led.c: New file.
    	* config/pru/lef.c: New file.
    	* config/pru/lib2bitcountHI.c: New file.
    	* config/pru/lib2divHI.c: New file.
    	* config/pru/lib2divQI.c: New file.
    	* config/pru/lib2divSI.c: New file.
    	* config/pru/libgcc-eabi.ver: New file.
    	* config/pru/ltd.c: New file.
    	* config/pru/ltf.c: New file.
    	* config/pru/mpyll.S: New file.
    	* config/pru/pru-abi.h: New file.
    	* config/pru/pru-asm.h: New file.
    	* config/pru/pru-divmod.h: New file.
    	* config/pru/sfp-machine.h: New file.
    	* config/pru/t-pru: New file.
    
    From-SVN: r272204

---
+++ libgcc/config/pru/asri.c
@@ -0,0 +1,33 @@
+/* PRU ABI compatibility functions
+   Arithmetic right shift
+   Copyright (C) 2017-2019 Free Software Foundation, Inc.
+   Contributed by Dimitar Dimitrov <dimitar@dinux.eu>
+
+   This file is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   In addition to the permissions in the GNU Lesser General Public
+   License, the Free Software Foundation gives you unlimited
+   permission to link the compiled version of this file into
+   combinations with other programs, and to distribute those
+   combinations without any restriction coming from the use of this
+   file.  (The Lesser General Public License restrictions do apply in
+   other respects; for example, they cover modification of the file,
+   and distribution when not linked into a combine executable.)
+
+   This file is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with GCC; see the file COPYING.LIB.  If not see
+   <http://www.gnu.org/licenses/>.  */
+
+int __pruabi_asri(int a, unsigned char shift)
+{
+  /* GCC has a built-in op.  */
+  return a >> shift;
+}
---
+++ libgcc/config/pru/pru-asm.h
@@ -0,0 +1,35 @@
+/*  Copyright (C) 2014-2019 Free Software Foundation, Inc.
+    Contributed by Dimitar Dimitrov <dimitar@dinux.eu>
+
+  This file is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the
+  Free Software Foundation; either version 3, or (at your option) any
+  later version.
+
+  This file is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  Under Section 7 of GPL version 3, you are granted additional
+  permissions described in the GCC Runtime Library Exception, version
+  3.1, as published by the Free Software Foundation.
+
+  You should have received a copy of the GNU General Public License and
+  a copy of the GCC Runtime Library Exception along with this program;
+  see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+  <http://www.gnu.org/licenses/>.  */
+
+/* ANSI concatenation macros.  */
+
+#define CONCAT1(a, b) CONCAT2(a, b)
+#define CONCAT2(a, b) a ## b
+
+/* Use the right prefix for global labels.  */
+
+#define SYM(x) CONCAT1 (__USER_LABEL_PREFIX__, x)
+
+#define FUNC(X)		.type SYM(X),@function
+#define HIDDEN_FUNC(X)	FUNC(X)` .hidden SYM(X)
+#define ENDFUNC0(X)	CONCAT1(.Lfe_,X): .size X,CONCAT1(.Lfe_,X)-X
+#define ENDFUNC(X)	ENDFUNC0(SYM(X))
