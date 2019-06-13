0a6c3056e8c7000cfa8bc87160aa1b10c891f8f5
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-06-13 18:55:55 +0000

    PR tree-optimization/77820 	* tree-ssa-threadedge.c 	(edge_forwards_cmp_to_conditional_jump_through_empty_bb_p): New 	function. 	(thread_across_edge): Add call to 	edge_forwards_cmp_to_conditional_jump_through_empty_bb_p.
    	PR tree-optimization/77820
    	* gcc.dg/tree-ssa/phi_on_compare-1.c: New testcase.
    	* gcc.dg/tree-ssa/phi_on_compare-2.c: New testcase.
    	* gcc.dg/tree-ssa/phi_on_compare-3.c: New testcase.
    	* gcc.dg/tree-ssa/phi_on_compare-4.c: New testcase.
    	* gcc.dg/tree-ssa/split-path-6.c: Update testcase.
    	* gcc.target/sh/pr51244-20.c: Update testcase.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@272261 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/phi_on_compare-1.c
@@ -0,0 +1,30 @@
+/* { dg-do compile } */
+/* { dg-options "-Ofast -fdump-tree-vrp1" } */
+
+void g (int);
+void g1 (int);
+
+void
+f (long a, long b, long c, long d, long x)
+{
+  _Bool t;
+  if (x)
+    {
+      g (a + 1);
+      t = a < b;
+      c = d + x;
+    }
+  else
+    {
+      g (b + 1);
+      a = c + d;
+      t = c > d;
+    }
+
+  if (t)
+    g1 (c);
+
+  g (a);
+}
+
+/* { dg-final { scan-tree-dump-times "Removing basic block" 1 "vrp1" } } */
---
+++ gcc/testsuite/gcc.dg/tree-ssa/phi_on_compare-3.c
@@ -0,0 +1,25 @@
+/* { dg-do compile } */
+/* { dg-options "-Ofast -fdump-tree-vrp1" } */
+
+void g (void);
+void g1 (void);
+
+void
+f (long a, long b, long c, long d, int x)
+{
+  int t;
+  if (x)
+    t = a < b;
+  else if (d == x)
+    t = c < b;
+  else
+    t = d > c;
+
+  if (t)
+    {
+      g1 ();
+      g ();
+    }
+}
+
+/* { dg-final { scan-tree-dump-times "Removing basic block" 1 "vrp1" } } */
