aeb834b7f7aa1a530f321742f232aad4d57e47fc
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-06-15 12:01:30 +0000

    [nvptx, libgomp] Update pr85381-{2,4}.c test-cases
    After the fix for "PR tree-optimization/89713 - Assume loop with an exit is
    finite" ( r272234 ) empty oacc loops are removed before expand.
    
    Update pr85381-{2,4}.c accordingly.
    
    2019-06-15  Tom de Vries  <tdevries@suse.de>
    
    	PR tree-optimization/89713
    	* testsuite/libgomp.oacc-c-c++-common/pr85381-2.c: Expect no bar.sync.
    	* testsuite/libgomp.oacc-c-c++-common/pr85381-4.c: Same.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@272324 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgomp/testsuite/libgomp.oacc-c-c++-common/pr85381-2.c
+++ libgomp/testsuite/libgomp.oacc-c-c++-common/pr85381-2.c
@@ -15,22 +15,4 @@ main (void)
   return 0;
 }
 
-/* Todo: Boths bar.syncs can be removed.
-   Atm we generate this dead code inbetween forked and joining:
-
-                     mov.u32 %r28, %ntid.y;
-                     mov.u32 %r29, %tid.y;
-                     add.u32 %r30, %r29, %r29;
-                     setp.gt.s32     %r31, %r30, 19;
-             @%r31   bra     $L2;
-                     add.u32 %r25, %r28, %r28;
-                     mov.u32 %r24, %r30;
-     $L3:
-                     add.u32 %r24, %r24, %r25;
-                     setp.le.s32     %r33, %r24, 19;
-             @%r33   bra     $L3;
-     $L2:
-
-   so the loop is not recognized as empty loop (which we detect by seeing if
-   joining immediately follows forked).  */
-/* { dg-final { scan-assembler-times "bar.sync" 2 } } */
+/* { dg-final { scan-assembler-times "bar.sync" 0 } } */
