2789efe3ee85f85ec83139fab6930cf2175be06a
Steven G. Kargl <kargl@gcc.gnu.org>
2019-06-15 00:38:55 +0000

    arith.c (arith_power): Rework overflow of an integer to an integer exponent.
    2019-06-14  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	* arith.c (arith_power): Rework overflow of an integer to an integer
    	exponent.
    
    2019-06-14  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	* gfortran.dg/integer_exponentiation_4.f90: Update test.
    	* gfortran.dg/integer_exponentiation_5.F90: Ditto.
    	* gfortran.dg/no_range_check_1.f90: Ditto.
    
    From-SVN: r272320

--- gcc/testsuite/gfortran.dg/no_range_check_1.f90
+++ gcc/testsuite/gfortran.dg/no_range_check_1.f90
@@ -4,11 +4,8 @@
 ! This testcase arose from PR 31262
   integer :: a
   integer(kind=8) :: b
-  a = -3
   b = -huge(b) / 7
-  a = a ** 73
   b = 7894_8 * b - 78941_8
-  if ((-3)**73 /= a) STOP 1
   if (7894_8 * (-huge(b) / 7) - 78941_8 /= b) STOP 2
 
   a = 1234789786453123
