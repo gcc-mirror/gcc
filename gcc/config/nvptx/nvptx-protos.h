8b243438b3b2f6410360fb1d26e527aba70accf7
Tom de Vries <tdevries@suse.de>
2019-06-17 14:50:21 +0000

    [nvptx] Fix missing mode warnings in nvptx.md, omp part
    Fix these warnings:
    ...
    gcc/config/nvptx/nvptx.md:1074:1: warning: operand 0 missing mode?
    gcc/config/nvptx/nvptx.md:1240:1: warning: operand 0 missing mode?
    gcc/config/nvptx/nvptx.md:1240:1: warning: operand 1 missing mode?
    gcc/config/nvptx/nvptx.md:1240:1: warning: operand 2 missing mode?
    gcc/config/nvptx/nvptx.md:1268:1: warning: operand 0 missing mode?
    ...
    
    Build and reg-tested on x86_64 with nvptx accelerator.
    
    2019-06-17  Tom de Vries  <tdevries@suse.de>
    
    	* config/nvptx/nvptx-protos.h (gen_set_softstack_insn): Declare.
    	* config/nvptx/nvptx.c (gen_set_softstack_insn): New function.
    	* config/nvptx/nvptx.md (define_insn "set_softstack_insn"): Rename to
    	...
    	(define_insn "set_softstack_<mode>"): ... this.  Use P iterator on
    	match_operand 0.
    	(define_insn "omp_simt_enter_insn"): Rename to ...
    	(define_insn "omp_simt_enter_<mode>"): ... this.  Use P iterator on
    	match_operand 0, 1 and 2, as well as the unspec_volatile result.
    	(define_expand "omp_simt_enter): Use gen_omp_simt_enter_di and
    	gen_omp_simt_enter_si.
    	(define_expand "omp_simt_exit"): New.
    	(define_insn "omp_simt_exit"): Rename to ...
    	(define_insn "omp_simt_exit_<mode>"): ... this.  Use P iterator on
    	match_operand 0.
    
    From-SVN: r272388

--- gcc/config/nvptx/nvptx-protos.h
+++ gcc/config/nvptx/nvptx-protos.h
@@ -57,5 +57,6 @@ extern const char *nvptx_output_set_softstack (unsigned);
 extern const char *nvptx_output_simt_enter (rtx, rtx, rtx);
 extern const char *nvptx_output_simt_exit (rtx);
 extern const char *nvptx_output_red_partition (rtx, rtx);
+extern rtx gen_set_softstack_insn (rtx);
 #endif
 #endif
