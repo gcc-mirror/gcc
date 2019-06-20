1194e1971ac9ab84b633ee14c5a77f0e475990ab
Tom de Vries <tdevries@suse.de>
2019-06-20 10:37:18 +0000

    [testsuite] Add missing dg-require-effective-target global_constructor
    Add missing dg-require-effective-target global_constructor.
    
    Tested on nvptx.
    
    2019-06-20  Tom de Vries  <tdevries@suse.de>
    
    	* gcc.dg/pr90866-2.c: Require global_constructor.
    
    From-SVN: r272505

--- gcc/testsuite/gcc.dg/pr90866-2.c
+++ gcc/testsuite/gcc.dg/pr90866-2.c
@@ -1,6 +1,8 @@
 /* PR tree-optimization/90866 - ICE in fold_binary_loc, at fold-const.c:9827
    { dg-do compile  }
-   { dg-options "-O2 -fsanitize=thread" } */
+   { dg-require-effective-target global_constructor }
+   { dg-options "-O2 -fsanitize=thread" }
+ */
 
 typedef enum { a } b;
 typedef struct {
