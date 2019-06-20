f39d033c3b550940e906b3ee3227342bb5100368
rearnsha <rearnsha@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-06-20 17:27:06 +0000

    [aarch64] Refactor common errata work-around specs
    With the addition of the AArch64 NetBSD support code we now have four
    ports all using and defining the same errata work-around headers.
    That's silly and long-term becomes a maintenance burden.
    
    This patch factors all that code into a single header to eliminate all
    the duplication.
    
    	* config/aarch64/aarch64-errata.h: New file.
    	* config/aarch64/aarch64-elf-raw.h (CA53_ERR_835769_SPEC): Delete.
    	(CA53_ERR_843419_SPEC): Delete.
    	(LINK_SPEC): Use AARCH64_ERRATA_LINK_SPEC instead of above.
    	* config/aarch64/aarch64-linux.h: Likewise.
    	* config/aarch64/aarch64-netbsd.h: Likewise.
    	* config/aarch64/aarch64-freebsd.h: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@272515 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/aarch64/aarch64-netbsd.h
+++ gcc/config/aarch64/aarch64-netbsd.h
@@ -39,27 +39,10 @@
   "%{mlittle-endian:-EL -m " TARGET_LINKER_LITTLE_EMULATION "} "	\
   "%(netbsd_link_spec)"
 
-#if TARGET_FIX_ERR_A53_835769_DEFAULT
-#define CA53_ERR_835769_SPEC \
-  " %{!mno-fix-cortex-a53-835769:--fix-cortex-a53-835769}"
-#else
-#define CA53_ERR_835769_SPEC \
-  " %{mfix-cortex-a53-835769:--fix-cortex-a53-835769}"
-#endif
-
-#ifdef TARGET_FIX_ERR_A53_843419_DEFAULT
-#define CA53_ERR_843419_SPEC \
-  " %{!mno-fix-cortex-a53-843419:--fix-cortex-a53-843419}"
-#else
-#define CA53_ERR_843419_SPEC \
-  " %{mfix-cortex-a53-843419:--fix-cortex-a53-843419}"
-#endif
-
 #undef  LINK_SPEC
 #define LINK_SPEC NETBSD_LINK_SPEC_ELF		\
 		  NETBSD_TARGET_LINK_SPEC	\
-		  CA53_ERR_835769_SPEC		\
-		  CA53_ERR_843419_SPEC
+		  AARCH64_ERRATA_LINK_SPEC
 
 #undef TARGET_OS_CPP_BUILTINS
 #define TARGET_OS_CPP_BUILTINS()		\
