1c6ffbab63f55187909b6d71cfca6080e9cf43cf
Qing Zhao <qing.zhao@oracle.com>
2019-07-02 20:23:30 +0000

    re PR preprocessor/90581 (provide an option to adjust the maximum depth of nested #include)
    PR preprocessor/90581
    Add a cpp option -fmax-include-depth to set the maximum depth of the nested #include.
    
    From-SVN: r272948

--- gcc/c-family/c-opts.c
+++ gcc/c-family/c-opts.c
@@ -460,6 +460,10 @@ c_common_handle_option (size_t scode, const char *arg, HOST_WIDE_INT value,
       cpp_opts->extended_identifiers = value;
       break;
 
+    case OPT_fmax_include_depth_:
+	cpp_opts->max_include_depth = value;
+      break;
+
     case OPT_foperator_names:
       cpp_opts->operator_names = value;
       break;
