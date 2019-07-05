ac18b2fa170a6d774a008c8ad0a03ba5250b281c
Nathan Sidwell <nathan@acm.org>
2019-07-05 15:44:29 +0000

    iostream-1_[ab].[HC]: New.
    	gcc/testsuite/
    	* g++.dg/modules/iostream-1_[ab].[HC]: New.
    
    From-SVN: r273142

---
+++ gcc/testsuite/g++.dg/modules/iostream-1_b.C
@@ -0,0 +1,13 @@
+// { dg-additional-options -fmodules-ts }
+
+import "iostream-1_a.H";
+
+// This hack is needed for the moment.  iostream contains a static var
+// definition by which it invokes its global ctors.
+static std::ios_base::Init __ioinit;
+
+int main ()
+{
+  std::cout << "hello world!\n";
+  return 0;
+}
