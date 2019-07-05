26ea500302b5595b0bab3837766d8b7b5d08856c
pmderodat <pmderodat@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-07-05 07:03:35 +0000

    [Ada] Ada.Wide_Wide_Characters.Handling: fix comments
    2019-07-05  Vasiliy Fofanov  <fofanov@adacore.com>
    
    gcc/ada/
    
    	* libgnat/a-wichha.ads (Is_Alphanumeric): Replace comment with
    	the correct one.  Also capitalize references to False
    	throughout.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@273125 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/libgnat/a-wichha.ads
+++ gcc/ada/libgnat/a-wichha.ads
@@ -25,28 +25,28 @@ package Ada.Wide_Characters.Handling is
    function Is_Control (Item : Wide_Character) return Boolean;
    pragma Inline (Is_Control);
    --  Returns True if the Wide_Character designated by Item is categorized as
-   --  other_control, otherwise returns false.
+   --  other_control, otherwise returns False.
 
    function Is_Letter (Item : Wide_Character) return Boolean;
    pragma Inline (Is_Letter);
    --  Returns True if the Wide_Character designated by Item is categorized as
    --  letter_uppercase, letter_lowercase, letter_titlecase, letter_modifier,
-   --  letter_other, or number_letter. Otherwise returns false.
+   --  letter_other, or number_letter. Otherwise returns False.
 
    function Is_Lower (Item : Wide_Character) return Boolean;
    pragma Inline (Is_Lower);
    --  Returns True if the Wide_Character designated by Item is categorized as
-   --  letter_lowercase, otherwise returns false.
+   --  letter_lowercase, otherwise returns False.
 
    function Is_Upper (Item : Wide_Character) return Boolean;
    pragma Inline (Is_Upper);
    --  Returns True if the Wide_Character designated by Item is categorized as
-   --  letter_uppercase, otherwise returns false.
+   --  letter_uppercase, otherwise returns False.
 
    function Is_Digit (Item : Wide_Character) return Boolean;
    pragma Inline (Is_Digit);
    --  Returns True if the Wide_Character designated by Item is categorized as
-   --  number_decimal, otherwise returns false.
+   --  number_decimal, otherwise returns False.
 
    function Is_Decimal_Digit (Item : Wide_Character) return Boolean
      renames Is_Digit;
@@ -54,51 +54,51 @@ package Ada.Wide_Characters.Handling is
    function Is_Hexadecimal_Digit (Item : Wide_Character) return Boolean;
    --  Returns True if the Wide_Character designated by Item is categorized as
    --  number_decimal, or is in the range 'A' .. 'F' or 'a' .. 'f', otherwise
-   --  returns false.
+   --  returns False.
 
    function Is_Alphanumeric (Item : Wide_Character) return Boolean;
    pragma Inline (Is_Alphanumeric);
    --  Returns True if the Wide_Character designated by Item is categorized as
-   --  number_decimal, or is in the range 'A' .. 'F' or 'a' .. 'f', otherwise
-   --  returns false.
+   --  letter_uppercase, letter_lowercase, letter_titlecase, letter_modifier,
+   --  letter_other, number_letter, or number_decimal; otherwise returns False.
 
    function Is_Special (Item : Wide_Character) return Boolean;
    pragma Inline (Is_Special);
    --  Returns True if the Wide_Character designated by Item is categorized
    --  as graphic_character, but not categorized as letter_uppercase,
    --  letter_lowercase, letter_titlecase, letter_modifier, letter_other,
-   --  number_letter, or number_decimal. Otherwise returns false.
+   --  number_letter, or number_decimal. Otherwise returns False.
 
    function Is_Line_Terminator (Item : Wide_Character) return Boolean;
    pragma Inline (Is_Line_Terminator);
    --  Returns True if the Wide_Character designated by Item is categorized as
    --  separator_line or separator_paragraph, or if Item is a conventional line
-   --  terminator character (CR, LF, VT, or FF). Otherwise returns false.
+   --  terminator character (CR, LF, VT, or FF). Otherwise returns False.
 
    function Is_Mark (Item : Wide_Character) return Boolean;
    pragma Inline (Is_Mark);
    --  Returns True if the Wide_Character designated by Item is categorized as
-   --  mark_non_spacing or mark_spacing_combining, otherwise returns false.
+   --  mark_non_spacing or mark_spacing_combining, otherwise returns False.
 
    function Is_Other_Format (Item : Wide_Character) return Boolean;
    pragma Inline (Is_Other_Format);
    --  Returns True if the Wide_Character designated by Item is categorized as
-   --  other_format, otherwise returns false.
+   --  other_format, otherwise returns False.
 
    function Is_Punctuation_Connector (Item : Wide_Character) return Boolean;
    pragma Inline (Is_Punctuation_Connector);
    --  Returns True if the Wide_Character designated by Item is categorized as
-   --  punctuation_connector, otherwise returns false.
+   --  punctuation_connector, otherwise returns False.
 
    function Is_Space (Item : Wide_Character) return Boolean;
    pragma Inline (Is_Space);
    --  Returns True if the Wide_Character designated by Item is categorized as
-   --  separator_space, otherwise returns false.
+   --  separator_space, otherwise returns False.
 
    function Is_Graphic (Item : Wide_Character) return Boolean;
    pragma Inline (Is_Graphic);
    --  Returns True if the Wide_Character designated by Item is categorized as
-   --  graphic_character, otherwise returns false.
+   --  graphic_character, otherwise returns False.
 
    function To_Lower (Item : Wide_Character) return Wide_Character;
    pragma Inline (To_Lower);
