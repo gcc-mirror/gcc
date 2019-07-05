f28a3d6c447309f05fd4031622e6d709058fa8cc
pmderodat <pmderodat@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-07-05 07:02:41 +0000

    [Ada] Typo fixes
    2019-07-05  Piotr Trojanek  <trojanek@adacore.com>
    
    gcc/ada/
    
    	* einfo.ads, sem_res.adb: Typo fixes in comments.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@273114 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/sem_res.adb
+++ gcc/ada/sem_res.adb
@@ -6070,7 +6070,7 @@ package body Sem_Res is
       --  is frozen in the usual fashion, by the appearance of a real body,
       --  or at the end of a declarative part. However an implicit call to
       --  an expression function may appear when it is part of a default
-      --  expression in a call to an initialiation procedure, and must be
+      --  expression in a call to an initialization procedure, and must be
       --  frozen now, even if the body is inserted at a later point.
 
       if Is_Entity_Name (Subp)
