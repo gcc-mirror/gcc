8c93b71cb9623dad07a1fc5a2073e968245814e3
pmderodat <pmderodat@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-07-11 08:01:44 +0000

    [Ada] Fix crash on dynamic predicate when generating SCOs
    A pragma Check for Dynamic_Predicate does not correspond to any source
    construct that has a provisionally-disabled SCO.
    
    2019-07-11  Thomas Quinot  <quinot@adacore.com>
    
    gcc/ada/
    
    	* sem_prag.adb (Analyze_Pragma, case pragma Check): Do not call
    	Set_SCO_Pragma_Enabled for the dynamic predicate case.
    
    gcc/testsuite/
    
    	* gnat.dg/scos1.adb: New testcase.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@273384 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/sem_prag.adb
+++ gcc/ada/sem_prag.adb
@@ -14113,9 +14113,14 @@ package body Sem_Prag is
 
             Expr := Get_Pragma_Arg (Arg2);
 
-            --  Deal with SCO generation
+            --  Mark the pragma (or, if rewritten from an aspect, the original
+            --  aspect) as enabled. Nothing to do for an internally generated
+            --  check for a dynamic predicate.
 
-            if Is_Checked (N) and then not Split_PPC (N) then
+            if Is_Checked (N)
+              and then not Split_PPC (N)
+              and then Cname /= Name_Dynamic_Predicate
+            then
                Set_SCO_Pragma_Enabled (Loc);
             end if;
 
