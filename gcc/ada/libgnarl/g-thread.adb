a1a8b1726cf8de2ed244353a9c8cd2fab12e4c71
Arnaud Charlet <charlet@adacore.com>
2019-07-11 08:01:25 +0000

    [Ada] GNAT.Threads: Add a Get_Thread overload taking a Task_Id
    2019-07-11  Arnaud Charlet  <charlet@adacore.com>
    
    gcc/ada/
    
    	* libgnarl/g-thread.ads, libgnarl/g-thread.adb (Get_Thread):
    	Update comments. Add new version taking a Task_Id.
    
    From-SVN: r273380

--- gcc/ada/libgnarl/g-thread.adb
+++ gcc/ada/libgnarl/g-thread.adb
@@ -168,9 +168,14 @@ package body GNAT.Threads is
    ----------------
 
    procedure Get_Thread (Id : Address; Thread : Address) is
-      Thr : constant Thread_Id_Ptr := To_Thread (Thread);
    begin
-      Thr.all := Task_Primitives.Operations.Get_Thread_Id (To_Id (Id));
+      To_Thread (Thread).all :=
+        Task_Primitives.Operations.Get_Thread_Id (To_Id (Id));
+   end Get_Thread;
+
+   procedure Get_Thread (Id : Task_Id; Thread : Address) is
+   begin
+      Get_Thread (To_Addr (Id), Thread);
    end Get_Thread;
 
    ----------------------
