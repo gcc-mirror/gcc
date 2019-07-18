b92fd6eb44a465113644cfcc017f711fb1595ba6
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-07-18 17:17:31 +0000

    PR target/91188 	* config/i386/i386.md (*addqi_1_slp): Use register_operand predicate 	for operand 0.  Do not use (match_dup) to match operand 1 with 	operand 0.  Add check in insn constraint that either input operand 	matches operand 0.  Use SWI12 mode iterator to also handle 	HImode operands. 	(*and<mode>_1_slp): Ditto. 	(*<code>qi_1_slp): Ditto. 	(*sub<mode>_1_slp): Use register_operand predicate for operand 0. 	Do not use (match_dup) to match operand 1 with operand 0.  Add 	check in insn constraint that operand 1 matches operand 0. 	Use SWI12 mode iterator to also handle HImode operands. 	(*ashl<mode>3_1_slp): Ditto. 	(*<shift_insn><mode>3_1_slp): Ditto. 	(*<rotate_insn><mode>3_1_slp): Ditto.
    testsuite/ChangeLog:
    
    	PR target/91188
    	* gcc.target/i386/pr91188-1a.c: New test.
    	* gcc.target/i386/pr91188-1b.c: Ditto.
    	* gcc.target/i386/pr91188-1c.c: Ditto.
    	* gcc.target/i386/pr91188-2a.c: Ditto.
    	* gcc.target/i386/pr91188-2b.c: Ditto.
    	* gcc.target/i386/pr91188-2c.c: Ditto.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@273578 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr91188-2b.c
@@ -0,0 +1,64 @@
+/* PR target/91188 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -masm=att" } */
+/* { dg-additional-options "-mregparm=3" { target ia32 } } */
+/* { dg-final { scan-assembler-not "movzwl" } } */
+/* { dg-final { scan-assembler-not "movw" } } */
+
+struct S
+{
+  unsigned short val;
+  unsigned short pad;
+};
+
+unsigned short b;
+
+struct S
+test_and (struct S a)
+{
+  a.val &= b;
+
+  return a;
+}
+
+/* { dg-final { scan-assembler "\[ \t\]andw" } } */
+
+struct S
+test_or (struct S a)
+{
+  a.val |= b;
+
+  return a;
+}
+
+/* { dg-final { scan-assembler "\[ \t\]orw" } } */
+
+struct S
+test_xor (struct S a)
+{
+  a.val ^= b;
+
+  return a;
+}
+
+/* { dg-final { scan-assembler "\[ \t\]xorw" } } */
+
+struct S
+test_add (struct S a)
+{
+  a.val += b;
+
+  return a;
+}
+
+/* { dg-final { scan-assembler "\[ \t\]addw" } } */
+
+struct S
+test_sub (struct S a)
+{
+  a.val -= b;
+
+  return a;
+}
+
+/* { dg-final { scan-assembler "\[ \t\]subw" } } */
