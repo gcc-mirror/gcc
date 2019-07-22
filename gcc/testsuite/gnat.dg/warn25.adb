e869060dcfdf9bd66f7cb0f7136066ab53659929
pmderodat <pmderodat@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-07-22 13:57:51 +0000

    [Ada] Misleading warning on variable not assigned
    This patch removes a warning on a referenced entity with no explicit
    prior assignment, if the type of the entity has
    Preelaborable_Initialixation, such as Exception_Occurrence.
    
    2019-07-22  Ed Schonberg  <schonberg@adacore.com>
    
    gcc/ada/
    
    	* sem_warn.adb (Check_References): Do not emit s warning on a
    	referenced entity with no explicit assignment if the type of the
    	entity has Preelaborable_Initialixation, such as
    	Exception_Occurrence.
    
    gcc/testsuite/
    
    	* gnat.dg/warn25.adb: New testcase.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@273686 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gnat.dg/warn25.adb
@@ -0,0 +1,23 @@
+--  { dg-do compile }
+--  { dg-options "-gnatwa" }
+
+with Ada.Exceptions;
+procedure Warn25 is
+    CASA_Unavailable : Ada.Exceptions.Exception_Occurrence;
+    use Ada.Exceptions;
+begin
+   while True loop
+    declare
+    begin
+       if Exception_Identity (CASA_Unavailable) = Null_Id then
+         exit;
+     end if;
+     exception
+       when E : others =>
+         Save_Occurrence (Source => E, Target => CASA_Unavailable);
+     end;
+   end loop;
+   if Exception_Identity (CASA_Unavailable) /= Null_Id then
+      Reraise_Occurrence (CASA_Unavailable);
+   end if;
+end;
