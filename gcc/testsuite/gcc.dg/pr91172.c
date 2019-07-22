2a245bc81b9e09cba4930d12d0417da229c37d67
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-07-22 07:34:10 +0000

    Make a warning for -Werror=wrong-language (PR driver/91172).
    2019-07-22  Martin Liska  <mliska@suse.cz>
    
    	PR driver/91172
    	* opts-common.c (decode_cmdline_option): Decode
    	argument of -Werror and check it for a wrong language.
    	* opts-global.c (complain_wrong_lang): Remove such case.
    2019-07-22  Martin Liska  <mliska@suse.cz>
    
    	PR driver/91172
    	* gcc.dg/pr91172.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@273660 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/pr91172.c
@@ -0,0 +1,3 @@
+/* { dg-do compile } */
+/* { dg-options "-Werror=target-lifetime" } */
+/* { dg-warning "'-Werror\=' argument '-Werror=target-lifetime' is not valid for C" "" { target *-*-* } 0 } */
