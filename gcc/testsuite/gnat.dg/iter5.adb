441fbb64d5df0e4e52035abbf642a75666ad8456
pmderodat <pmderodat@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-07-23 08:13:15 +0000

    [Ada] Iterators are view-specific
    Operational aspects, such as Default_Iterator, are view-specific, and if
    such an aspect appears on the full view of a private type, an object of
    the type cannot be iterated upon if it is not in the scope of the full
    view, This patch diagnoses properly an attempt to iterate over such an
    object.
    
    2019-07-23  Ed Schonberg  <schonberg@adacore.com>
    
    gcc/ada/
    
    	* aspects.ads: New table Operational_Aspect, used to distinguish
    	between aspects that are view-specific, such as those related to
    	iterators, and representation aspects that apply to all views of
    	a type.
    	* aspects.adb (Find_Aspect): If the aspect being sought is
    	operational, do not ecamine the full view of a private type to
    	retrieve it.
    	* sem_ch5.adb (Analyze_Iterator_Specification): Improve error
    	message when the intended domain of iteration does not implement
    	the required iterator aspects.
    
    gcc/testsuite/
    
    	* gnat.dg/iter5.adb: Add an expected error.
    	* gnat.dg/iter6.adb: New testcase.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@273722 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gnat.dg/iter5.adb
+++ gcc/testsuite/gnat.dg/iter5.adb
@@ -4,7 +4,7 @@ with Iter5_Pkg;
 
 procedure Iter5 is
 begin
-   for The_Filename of Iter5_Pkg.Iterator_For ("C:\Program_Files") loop
+   for The_Filename of Iter5_Pkg.Iterator_For ("C:\Program_Files") loop  --  { dg-error "cannot iterate over \"Item\"" }
       null;
    end loop;
 end Iter5;
