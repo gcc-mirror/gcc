9c4b070dc4623900707dde48b822f02af33fb83a
Nathan Sidwell <nathan@acm.org>
2019-07-24 15:55:57 +0000

    alias-2_a.H: Add dg-module-header.
    	gcc/testsuite/
    	* g++.dg/modules/alias-2_a.H: Add dg-module-header.
    	* g++.dg/modules/alias-3_a.H: Likewise.
    	* g++.dg/modules/mod-decl-0-2a.C: std=c++2a.
    	* g++.dg/modules/mod-decl-0.C: std=c++17.
    	* g++.dg/modules/modules.exp: Add dg-module-header, iterate over
    	different c++ stds.
    
    From-SVN: r273764

--- gcc/testsuite/g++.dg/modules/mod-decl-0-2a.C
+++ gcc/testsuite/g++.dg/modules/mod-decl-0-2a.C
@@ -1,6 +1,6 @@
-// { dg-options "-fno-modules -std=c++17" }
+// { dg-options "-fno-modules -std=c++2a" }
 
-export // { dg-message "ignored" }
+export // { dg-message "enabled with" }
 module nope; // { dg-error "" }
 // { dg-message "only available with -fmodules" "" { target *-*-* } .-1 }
 // { dg-module-cmi "!nope" }
