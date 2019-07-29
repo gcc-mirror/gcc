4d706ff86ea86868615558e92407674a4f4b4af9
Richard Sandiford <richard.sandiford@arm.com>
2019-07-29 08:46:46 +0000

    Add dg test for matching function bodies
    There isn't a 1:1 mapping from SVE intrinsics to SVE instructions,
    but the intrinsics are still close enough to the instructions for
    there to be a specific preferred sequence (or sometimes choice of
    preferred sequences) for a given combination of operands.  Sometimes
    these sequences will be one instruction, sometimes they'll be several.
    
    I therefore wanted a convenient way of matching the exact assembly
    implementation of a given function.  It's possible to do that using
    single scan-assembler lines, but:
    
    (a) they become hard to read for multiline matches
    (b) the PASS/FAIL lines tend to be overly long
    (c) it's useful to have a single place that skips over uninteresting
        lines, such as entry block labels and .cfi_* directives, without
        being overly broad
    
    This patch therefore adds a new check-function-bodies dg-final test
    that looks for specially-formatted comments.  As a demo, the patch
    converts the SVE vec_init tests to use the new harness instead of
    scan-assembler.
    
    The regexps in parse_function_bodies are fairly general, but might
    still need to be extended in future for targets like Darwin or AIX.
    
    2019-07-29  Richard Sandiford  <richard.sandiford@arm.com>
    
    gcc/
    	* doc/sourcebuild.texi (check-function-bodies): Document.
    
    gcc/testsuite/
    	* lib/scanasm.exp (parse_function_bodies, check_function_body)
    	(check-function-bodies): New procedures.
    	* gcc.target/aarch64/sve/init_1.c: Use check-function-bodies
    	instead of scan-assembler.
    	* gcc.target/aarch64/sve/init_2.c: Likewise.
    	* gcc.target/aarch64/sve/init_3.c: Likewise.
    	* gcc.target/aarch64/sve/init_4.c: Likewise.
    	* gcc.target/aarch64/sve/init_5.c: Likewise.
    	* gcc.target/aarch64/sve/init_6.c: Likewise.
    	* gcc.target/aarch64/sve/init_7.c: Likewise.
    	* gcc.target/aarch64/sve/init_8.c: Likewise.
    	* gcc.target/aarch64/sve/init_9.c: Likewise.
    	* gcc.target/aarch64/sve/init_10.c: Likewise.
    	* gcc.target/aarch64/sve/init_11.c: Likewise.
    	* gcc.target/aarch64/sve/init_12.c: Likewise.
    
    From-SVN: r273869

--- gcc/testsuite/gcc.target/aarch64/sve/init_2.c
+++ gcc/testsuite/gcc.target/aarch64/sve/init_2.c
@@ -1,5 +1,6 @@
 /* { dg-do assemble { target aarch64_asm_sve_ok } } */
 /* { dg-options "-O -msve-vector-bits=256 --save-temps" } */
+/* { dg-final { check-function-bodies "**" "" } } */
 
 /* Case 1.2: Trailing constants with repeating sequence.  */
 
@@ -7,23 +8,16 @@
 
 typedef int32_t vnx4si __attribute__((vector_size (32)));
 
+/*
+** foo:
+**	...
+**	ld1w	(z[0-9]+\.s), p[0-9]+/z, \[x[0-9]+\]
+**	insr	\1, w1
+**	insr	\1, w0
+**	...
+*/
 __attribute__((noipa))
 vnx4si foo(int a, int b)
 {
   return (vnx4si) { a, b, 2, 3, 2, 3, 2, 3 };
 }
-
-/*
-foo:
-.LFB0:
-        .cfi_startproc
-        ptrue   p0.s, vl8
-        adrp    x2, .LANCHOR0
-        add     x2, x2, :lo12:.LANCHOR0
-        ld1w    z0.s, p0/z, [x2]
-        insr    z0.s, w1
-        insr    z0.s, w0
-        ret
-*/
-
-/* { dg-final { scan-assembler {\tld1w\t(z[0-9]+\.s), p[0-9]+/z, \[x[0-9]+\]\n\tinsr\t\1, w1\n\tinsr\t\1, w0} } } */
--- gcc/testsuite/gcc.target/aarch64/sve/init_6.c
+++ gcc/testsuite/gcc.target/aarch64/sve/init_6.c
@@ -1,5 +1,6 @@
 /* { dg-do assemble { target aarch64_asm_sve_ok } } */
 /* { dg-options "-O -msve-vector-bits=256 --save-temps" } */
+/* { dg-final { check-function-bodies "**" "" } } */
 
 /* Case 3: Trailing same element.  */ 
 
@@ -7,21 +8,16 @@
 
 typedef int32_t vnx4si __attribute__((vector_size (32)));
 
+/*
+** foo:
+**	mov	(z[0-9]+\.s), w2
+**	insr	\1, w1
+**	insr	\1, w0
+**	rev	\1, \1
+**	...
+*/
 __attribute__((noipa))
 vnx4si foo(int a, int b, int c)
 {
   return (vnx4si) { c, c, c, c, c, c, b, a };
 }
-
-/*
-foo:
-.LFB0:
-        .cfi_startproc
-        mov     z0.s, w2
-        insr    z0.s, w1
-        insr    z0.s, w0
-        rev     z0.s, z0.s
-        ret
-*/
-
-/* { dg-final { scan-assembler {\tmov\t(z[0-9]+\.s), w2\n\tinsr\t\1, w1\n\tinsr\t\1, w0\n\trev\t\1, \1} } } */
