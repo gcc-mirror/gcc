32a388df6c6a847bfe820f2f602070e05a7cead1
jozefl <jozefl@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-07-29 19:21:18 +0000

    Allow both upper and lower case 'r' in register names
    2019-07-29  Jozef Lawrynowicz  <jozef.l@mittosystems.com>
    
    	PR target/70320
    	* config/msp430/msp430.h: Define ADDITIONAL_REGISTER_NAMES.
    
    2019-07-29  Jozef Lawrynowicz  <jozef.l@mittosystems.com>
    
    	PR target/70320
    	* gcc.target/msp430/asm-register-names-lower-case.c: New test.
    	* gcc.target/msp430/asm-register-names-upper-case.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@273883 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/msp430/msp430.h
+++ gcc/config/msp430/msp430.h
@@ -225,6 +225,28 @@ extern const char * msp430_select_hwmult_lib (int, const char **);
   "argptr"							\
 }
 
+/* Allow lowercase "r" to be used in register names instead of upper
+   case "R".  */
+#define ADDITIONAL_REGISTER_NAMES	\
+{					\
+    { "r0",  0 },			\
+    { "r1",  1 },			\
+    { "r2",  2 },			\
+    { "r3",  3 },			\
+    { "r4",  4 },			\
+    { "r5",  5 },			\
+    { "r6",  6 },			\
+    { "r7",  7 },			\
+    { "r8",  8 },			\
+    { "r9",  9 },			\
+    { "r10", 10 },			\
+    { "r11", 11 },			\
+    { "r12", 12 },			\
+    { "r13", 13 },			\
+    { "r14", 14 },			\
+    { "r15", 15 }			\
+}
+
 enum reg_class
 {
   NO_REGS,
