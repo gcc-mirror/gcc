960b9ae05ac46f51370c30d30575e7a7fe6a3a99
Jonathan Wakely <jwakely@redhat.com>
2019-07-31 17:40:39 +0100

    Implement "P0631R4 Math Constants" for C++20
    The values of the constants are taken from Glibc where the equivalent
    constant exists, or by rounding the actual constant to the same number
    of digits as the Glibc constants have.
    
    	P0631R4 Math Constants
    	* include/Makefile.am: Add new header.
    	* include/Makefile.in: Regenerate.
    	* include/precompiled/stdc++.h: Include new header.
    	* include/std/numbers: New header.
    	* include/std/version (__cpp_lib_math_constants): Define.
    	* testsuite/26_numerics/numbers/1.cc: New test.
    	* testsuite/26_numerics/numbers/2.cc: New test.
    	* testsuite/26_numerics/numbers/3.cc: New test.
    	* testsuite/26_numerics/numbers/nonfloat_neg.cc: New test.
    
    From-SVN: r273940

---
+++ libstdc++-v3/testsuite/26_numerics/numbers/nonfloat_neg.cc
@@ -0,0 +1,36 @@
+// Copyright (C) 2019 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++2a" }
+// { dg-do compile { target c++2a } }
+
+#include <numbers>
+
+int
+test01()
+{
+  return std::numbers::pi_v<int>; // { dg-error "here" }
+}
+
+void
+test02()
+{
+  struct S { };
+  auto s = std::numbers::egamma_v<S>; // { dg-error "here" }
+}
+
+// { dg-prune-output "no type named 'type' in" }
