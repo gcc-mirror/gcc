27eb6799718407070649d2784d40a04b57e7210f
Iain Sandoe <iain@sandoe.co.uk>
2019-08-04 10:06:33 +0000

    Darwin, X86, backport testsuite fix for 67958.
    2019-08-04  Iain Sandoe  <iain@sandoe.co.uk>
    
    	Backport from mainline.
    	2019-05-21  Iain Sandoe  <iain@sandoe.co.uk>
    
    	PR testsuite/67958
    	* gcc.target/i386/pr32219-1.c: Adjust scan-asms for Darwin, comment
    	the differences.
    	* gcc.target/i386/pr32219-2.c: Likewise.
    	* gcc.target/i386/pr32219-3.c: Likewise.
    	* gcc.target/i386/pr32219-4.c: Likewise.
    	* gcc.target/i386/pr32219-5.c: Likewise.
    	* gcc.target/i386/pr32219-6.c: Likewise.
    	* gcc.target/i386/pr32219-7.c: Likewise.
    	* gcc.target/i386/pr32219-8.c: Likewise.
    
    From-SVN: r274060

--- gcc/testsuite/gcc.target/i386/pr32219-6.c
+++ gcc/testsuite/gcc.target/i386/pr32219-6.c
@@ -11,7 +11,14 @@ foo ()
   return xxx;
 }
 
-/* { dg-final { scan-assembler-not "movl\[ \t\]xxx\\(%rip\\), %" { target { ! ia32 } } } } */
-/* { dg-final { scan-assembler "xxx@GOTPCREL" { target { ! ia32 } } } } */
-/* { dg-final { scan-assembler-not "movl\[ \t\]xxx@GOTOFF\\(%\[^,\]*\\), %" { target ia32 } } } */
-/* { dg-final { scan-assembler "movl\[ \t\]xxx@GOT\\(%\[^,\]*\\), %" { target ia32 } } } */
+/* { dg-final { scan-assembler-not "movl\[ \t\]xxx\\(%rip\\), %" { target { { ! ia32 } && { ! *-*-darwin* } } } } } */
+/* { dg-final { scan-assembler "xxx@GOTPCREL" { target { { ! ia32 } && { ! *-*-darwin* } } } } } */
+
+/* Darwin m64 is always PIC, and the dynamic linker doesn't need an indirection.  */
+/* { dg-final { scan-assembler {movl[ \t]_xxx\(%rip\),[ \t]%eax} { target { { ! ia32 } && *-*-darwin* } } } } */
+
+/* { dg-final { scan-assembler-not "movl\[ \t\]xxx@GOTOFF\\(%\[^,\]*\\), %" { target { ia32 && { ! *-*-darwin* } } } } } */
+/* { dg-final { scan-assembler "movl\[ \t\]xxx@GOT\\(%\[^,\]*\\), %" { target { ia32 && { ! *-*-darwin* } } } } } */
+
+/* Darwin m32 defaults to PIC, so no change.  */
+/* { dg-final { scan-assembler {movl[ \t]_xxx-L1\$pb\(%eax\),[ \t]%eax} { target { ia32 && *-*-darwin* } } } } */
