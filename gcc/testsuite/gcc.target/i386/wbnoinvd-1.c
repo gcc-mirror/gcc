d7b3a854cd0db9891be3dd703c7636ca06f59d7e
iains <iains@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-08-04 10:40:16 +0000

    Darwin, X86, testsuite, backport matching fixes.
    Some of the i386.exp tests fail on Darwin (and at least one passes
    incorrectly on Linux) because their scan-asm match strings are too
    general.  In some cases the strings also match instances in the .file
    and size directives or in comment output.  This patch makes the match
    strings more specific.
    
    2019-08-04  Iain Sandoe  <iain@sandoe.co.uk>
    
    	Backport from mainline.
    	2019-05-24  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* gcc.target/i386/pconfig-1.c: Scan for the string in the generated
    	code, not in comments or miscellaneous directives.
    	* gcc.target/i386/pr18041-1.c: Likewise.
    	* gcc.target/i386/pr18041-2.c: Likewise.
    	* gcc.target/i386/wbinvd-1.c: Likewise.
    	* gcc.target/i386/wbnoinvd-1.c: Likewise.
    	* gcc.target/i386/pr66819-3.c: Specifically, check that there is no
    	call to "bar".
    	* gcc.target/i386/pr66819-4.c: Likewise.
    	* gcc.target/i386/pr82662.c
    	* gcc.target/i386/ptwrite2.c: Make the checks look for the specific
    	destination register, don't try the m32 test on m64 targets.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-9-branch@274065 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/wbnoinvd-1.c
+++ gcc/testsuite/gcc.target/i386/wbnoinvd-1.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -mwbnoinvd" } */
-/* { dg-final { scan-assembler-times "wbnoinvd" 2 } } */
+/* { dg-final { scan-assembler-times {\twbnoinvd} 1 } } */
 
 #include "x86intrin.h"
 
