b4f6d7224000aab896d5e0d8ea398f9619ee5467
Julian Brown <julian@codesourcery.com>
2019-08-05 15:05:35 -0700

    Add missing exec_params libgomp plugin entry points
    	libgomp/
    	* plugin/plugin-gcn.c (GOMP_OFFLOAD_openacc_exec_params,
    	GOMP_OFFLOAD_openacc_async_exec_params): New functions.

--- libgomp/plugin/plugin-gcn.c
+++ libgomp/plugin/plugin-gcn.c
@@ -3867,6 +3867,14 @@ GOMP_OFFLOAD_openacc_exec (void (*fn_ptr) (void *),
 /* Run an asynchronous OpenACC kernel on the specified queue.  */
 
 void
+GOMP_OFFLOAD_openacc_exec_params (void (*fn_ptr) (void *), size_t mapnum,
+				  void **hostaddrs, void **devaddrs,
+				  unsigned *dims, void *targ_mem_desc)
+{
+  GOMP_PLUGIN_fatal ("OpenACC exec params unimplemented.");
+}
+
+void
 GOMP_OFFLOAD_openacc_async_exec (void (*fn_ptr) (void *),
 				 size_t mapnum __attribute__((unused)),
 				 void **hostaddrs __attribute__((unused)),
@@ -3880,6 +3888,15 @@ GOMP_OFFLOAD_openacc_async_exec (void (*fn_ptr) (void *),
 }
 
 /* Create a new asynchronous thread and queue for running future kernels.  */
+
+void
+GOMP_OFFLOAD_openacc_async_exec_params (void (*fn) (void *), size_t mapnum,
+					void **hostaddrs, void **devaddrs,
+					unsigned *dims, void *targ_mem_desc,
+					struct goacc_asyncqueue *aq)
+{
+  GOMP_PLUGIN_fatal ("OpenACC async exec params unimplemented.");
+}
 
 struct goacc_asyncqueue *
 GOMP_OFFLOAD_openacc_async_construct (int device)
