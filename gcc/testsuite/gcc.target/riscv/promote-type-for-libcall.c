78978dd25597ec4acc651a28d55814092c406712
kito <kito@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-08-05 03:32:38 +0000

    RISC-V: Promote type correctly for libcalls
     - argument and return value for libcall won't promote at
       default_promote_function_mode_always_promote, however we expect it
       should sign-extend as normal function.
    
     - Witout this patch, this test case will fail at -march=rv64i -mabi=lp64.
    
     - The implementation of riscv_promote_function_mode is borrowed from MIPS.
    
    gcc/ChangeLog
    
    	* config/riscv/riscv.c (riscv_promote_function_mode): New.
    	(TARGET_PROMOTE_FUNCTION_MODE): Use riscv_promote_function_mode.
    
    gcc/testsuite/ChangeLog
    
    	* gcc.target/riscv/promote-type-for-libcall.c: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@274107 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/riscv/promote-type-for-libcall.c
@@ -0,0 +1,37 @@
+/* { dg-do run } */
+/* { dg-options "-O1 -ftree-slp-vectorize -funroll-loops" } */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define N 4
+volatile float f[N];
+int x[N] __attribute__((aligned(8)));
+int main() {
+  int i;
+  x[0] = -1;
+  x[1] = 2;
+  x[2] = -2;
+  x[3] = 2;
+
+  for (i=0;i<N;++i){
+    f[i] = x[i];
+  }
+
+  if (f[0] != -1.0f) {
+    abort();
+  }
+
+  if (f[1] != 2.0f) {
+    abort();
+  }
+
+  if (f[2] != -2.0f) {
+    abort();
+  }
+
+  if (f[3] != 2.0f) {
+    abort();
+  }
+
+  return 0;
+}
