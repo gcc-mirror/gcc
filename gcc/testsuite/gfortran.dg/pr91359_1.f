cffb2c3f73e4b1d49b3b9d9ea6487b1b8831d17e
kargl <kargl@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-08-12 20:09:00 +0000

    2019-08-12  Steven G. Kargl  <kargl@gcc.gnu.org>
    	PR fortran/91359
    	* trans-decl.c (gfc_generate_return): Ensure something is returned
    	from a function.
    
    2019-08-12  Steven G. Kargl  <kargl@gcc.gnu.org>
    
    	PR fortran/91359
    	* gfortran.dg/pr91359_1.f: New test.
    	* gfortran.dg/pr91359_2.f: Ditto.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-9-branch@274319 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/pr91359_1.f
@@ -0,0 +1,17 @@
+! { dg-do run }
+! PR fortran/91359
+! Orginal code contributed by Brian T. Carcich <briantcarcich at gmail dot com>
+!
+      logical function zero()
+         goto 2
+1        return
+2        zero = .false.
+         if (.not.zero) goto 1
+         return
+      end
+
+      program test_zero
+         logical zero
+         if (zero()) stop 'FAIL:  zero() returned .TRUE.'
+         stop 'OKAY:  zero() returned .FALSE.'
+      end
