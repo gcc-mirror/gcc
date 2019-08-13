4167b0752365c69e5895b5c8097e7dba34f735f5
Gary Dismukes <dismukes@adacore.com>
2019-08-13 08:07:46 +0000

    [Ada] Compiler may blow up on array aggregates whose size is very large
    The compiler may crash when compiling array aggregates where the
    computation of the size produces a very large number that overflows
    (possibly producing a small result), such as with an aggregate of a type
    that is an array of arrays, where each array range has close to
    Integer'Last elements. That can lead to Aggr_Size_OK returning
    incorrectly returning True, following on to allocating a very large
    array in function Flatten that blows the stack.  The size computation
    was being performed using type Int, so this was changed to use universal
    arithmetic.
    
    2019-08-13  Gary Dismukes  <dismukes@adacore.com>
    
    gcc/ada/
    
    	* exp_aggr.adb (Aggr_Size_OK): Compute the aggregate size using
    	universal arithmetic, to avoid situations where the size
    	computation overflows.
    
    gcc/testsuite/
    
    	* gnat.dg/aggr26.adb: New testcase.
    
    From-SVN: r274348

---
+++ gcc/testsuite/gnat.dg/aggr26.adb
@@ -0,0 +1,10 @@
+--  { dg-do compile }
+
+procedure Aggr26 is
+
+    type Row is array (Positive) of Integer;
+    H : array (Positive) of Row := (others => (others => 0));  --  { dg-warning "\"Storage_Error\" will be raised at run time" }
+
+begin
+   null;
+end Aggr26;
