342216cb41a9dda145f37a7d4920d8474213052d
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-08-15 08:12:41 +0000

    [AArch64] Use SVE FABD in conditional arithmetic
    This patch extends the FABD support so that it handles conditional
    arithmetic.  We're relying on combine for this, since there's no
    associated IFN_COND_* (yet?).
    
    2019-08-15  Richard Sandiford  <richard.sandiford@arm.com>
    	    Kugan Vivekanandarajah  <kugan.vivekanandarajah@linaro.org>
    
    gcc/
    	* config/aarch64/aarch64-sve.md (*aarch64_cond_abd<SVE_F:mode>_2)
    	(*aarch64_cond_abd<SVE_F:mode>_3)
    	(*aarch64_cond_abd<SVE_F:mode>_any): New patterns.
    
    gcc/testsuite/
    	* gcc.target/aarch64/sve/cond_fabd_1.c: New test.
    	* gcc.target/aarch64/sve/cond_fabd_1_run.c: Likewise.
    	* gcc.target/aarch64/sve/cond_fabd_2.c: Likewise.
    	* gcc.target/aarch64/sve/cond_fabd_2_run.c: Likewise.
    	* gcc.target/aarch64/sve/cond_fabd_3.c: Likewise.
    	* gcc.target/aarch64/sve/cond_fabd_3_run.c: Likewise.
    	* gcc.target/aarch64/sve/cond_fabd_4.c: Likewise.
    	* gcc.target/aarch64/sve/cond_fabd_4_run.c: Likewise.
    	* gcc.target/aarch64/sve/cond_fabd_5.c: Likewise.
    	* gcc.target/aarch64/sve/cond_fabd_5_run.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@274507 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/aarch64/sve/cond_fabd_2.c
@@ -0,0 +1,29 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -ftree-vectorize -fno-trapping-math" } */
+
+#include <stdint.h>
+
+#define DEF_LOOP(TYPE, ABS)				\
+  void __attribute__ ((noinline, noclone))		\
+  test_##TYPE (TYPE *__restrict r, TYPE *__restrict a,	\
+	       TYPE *__restrict b, TYPE *__restrict c,	\
+	       int n)					\
+  {							\
+    for (int i = 0; i < n; ++i)				\
+      r[i] = a[i] < 20 ? ABS (b[i] - c[i]) : c[i];	\
+  }
+
+#define TEST_ALL(T) \
+  T (_Float16, __builtin_fabsf16) \
+  T (float, __builtin_fabsf) \
+  T (double, __builtin_fabs)
+
+TEST_ALL (DEF_LOOP)
+
+/* { dg-final { scan-assembler-times {\tfabd\tz[0-9]+\.h, p[0-7]/m,} 1 } } */
+/* { dg-final { scan-assembler-times {\tfabd\tz[0-9]+\.s, p[0-7]/m,} 1 } } */
+/* { dg-final { scan-assembler-times {\tfabd\tz[0-9]+\.d, p[0-7]/m,} 1 } } */
+
+/* { dg-final { scan-assembler-not {\tmov\tz[^,]*z} } } */
+/* { dg-final { scan-assembler-not {\tmovprfx\t} } } */
+/* { dg-final { scan-assembler-not {\tsel\t} } } */
