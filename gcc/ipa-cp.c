97ecc8d5769e947e8659d32be51846d40c90f50c
Aldy Hernandez <aldyh@redhat.com>
2019-08-16 06:54:23 +0000

    Add type to VR_VARYING.
    From-SVN: r274561

--- gcc/ipa-cp.c
+++ gcc/ipa-cp.c
@@ -977,7 +977,12 @@ ipcp_vr_lattice::set_to_bottom ()
 {
   if (m_vr.varying_p ())
     return false;
-  m_vr.set_varying ();
+  /* ?? We create all sorts of VARYING ranges for floats, structures,
+     and other types which we cannot handle as ranges.  We should
+     probably avoid handling them throughout the pass, but it's easier
+     to create a sensible VARYING here and let the lattice
+     propagate.  */
+  m_vr.set_varying (integer_type_node);
   return true;
 }
 
