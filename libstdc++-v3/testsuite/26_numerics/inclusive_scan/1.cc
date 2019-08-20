14cbcae81a30ce09c3e45a34c68a8bb9c97090b2
Jonathan Wakely <jwakely@redhat.com>
2019-08-20 22:03:11 +0100

    Implement new serial algorithms from Parallelism TS (P0024R2)
    These new (non-parallel) algorithms were added to C++17 along with the
    parallel algorithms, but were missing from libstdc++.
    
    Backported for PR libstdc++/91495, replacing the use of
    std::__size_to_integer which is not present on the branch.
    
    Backport from mainline
    2019-06-19  Jonathan Wakely  <jwakely@redhat.com>
    
    	* include/std/numeric (reduce(Iter, Iter, T, BinOp)): Fix value
    	category used in invocable check.
    	(reduce(Iter, Iter, T)): Pass initial value as rvalue.
    	* testsuite/26_numerics/reduce/2.cc: New test.
    
    Backport from mainline
    2019-06-18  Jonathan Wakely  <jwakely@redhat.com>
    
    	* include/bits/algorithmfwd.h: Change title of doc group.
    	* include/bits/stl_algo.h (for_each_n): Add new C++17 algorithm from
    	P0024R2.
    	* include/bits/stl_numeric.h: Define doc group and add algos to it.
    	* include/std/numeric (__is_random_access_iter): New internal trait.
    	(reduce, transform_reduce, exclusive_scan, inclusive_scan)
    	(transform_exclusive_scan, transform_inclusive_scan): Likewise.
    	* testsuite/25_algorithms/for_each/for_each_n.cc: New test.
    	* testsuite/26_numerics/exclusive_scan/1.cc: New test.
    	* testsuite/26_numerics/inclusive_scan/1.cc: New test.
    	* testsuite/26_numerics/reduce/1.cc: New test.
    	* testsuite/26_numerics/transform_exclusive_scan/1.cc: New test.
    	* testsuite/26_numerics/transform_inclusive_scan/1.cc: New test.
    	* testsuite/26_numerics/transform_reduce/1.cc: New test.
    	* testsuite/util/testsuite_iterators.h (test_container::size()): New
    	member function.
    
    From-SVN: r274754

---
+++ libstdc++-v3/testsuite/26_numerics/inclusive_scan/1.cc
@@ -0,0 +1,123 @@
+// { dg-options "-std=gnu++17" }
+// { dg-do run { target c++17 } }
+
+// Copyright (C) 2019 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// C++17 29.8.8 [inclusive.scan]
+
+#include <numeric>
+#include <iterator>
+#include <testsuite_hooks.h>
+#include <testsuite_iterators.h>
+
+int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+using __gnu_test::test_container;
+using __gnu_test::input_iterator_wrapper;
+using __gnu_test::output_iterator_wrapper;
+
+/*
+template<class InputIterator, class OutputIterator>
+  OutputIterator
+  inclusive_scan(InputIterator, InputIterator, OutputIterator);
+*/
+void
+test01()
+{
+  int out[10];
+  test_container<int, output_iterator_wrapper> co(out);
+  test_container<int, input_iterator_wrapper> ca(a);
+  auto end = std::inclusive_scan(ca.begin(), ca.end(), co.begin());
+  static_assert(std::is_same_v<decltype(end), decltype(co.begin())>);
+  VERIFY( end.ptr == out+10 );
+  VERIFY( out[0] == 1 );
+  VERIFY( out[1] == (1+2) );
+  VERIFY( out[2] == (1+2+3) );
+  VERIFY( out[3] == (1+2+3+4) );
+  VERIFY( out[4] == (1+2+3+4+5) );
+  VERIFY( out[5] == (1+2+3+4+5+6) );
+  VERIFY( out[6] == (1+2+3+4+5+6+7) );
+  VERIFY( out[7] == (1+2+3+4+5+6+7+8) );
+  VERIFY( out[8] == (1+2+3+4+5+6+7+8+9) );
+  VERIFY( out[9] == (1+2+3+4+5+6+7+8+9+10) );
+}
+
+/*
+template<class InputIterator, class OutputIterator, class BinaryOperation>
+  OutputIterator
+  inclusive_scan(InputIterator, InputIterator, OutputIterator,
+		 BinaryOperation);
+*/
+void
+test02()
+{
+  int out[10];
+  test_container<int, output_iterator_wrapper> co(out);
+  test_container<int, input_iterator_wrapper> ca(a);
+  auto end = std::inclusive_scan(ca.begin(), ca.end(), co.begin(),
+				 [](int i, int j) { return 2*i + 2*j; });
+  static_assert(std::is_same_v<decltype(end), decltype(co.begin())>);
+  VERIFY( end.ptr == out+10 );
+  VERIFY( out[0] == 1 );
+  VERIFY( out[1] == (2*1+2*2) );
+  VERIFY( out[2] == (2*6+2*3) );
+  VERIFY( out[3] == (2*18+2*4) );
+  VERIFY( out[4] == (2*44+2*5) );
+  VERIFY( out[5] == (2*98+2*6));
+  VERIFY( out[6] == (2*208+2*7) );
+  VERIFY( out[7] == (2*430+2*8) );
+  VERIFY( out[8] == (2*876+2*9) );
+  VERIFY( out[9] == (2*1770+2*10) );
+}
+
+/*
+template<class InputIterator, class OutputIterator, class BinaryOperation, T>
+  OutputIterator
+  inclusive_scan(InputIterator, InputIterator, OutputIterator,
+		 BinaryOperation, T);
+*/
+void
+test03()
+{
+  int out[10];
+  test_container<int, output_iterator_wrapper> co(out);
+  test_container<int, input_iterator_wrapper> ca(a);
+  auto end = std::inclusive_scan(ca.begin(), ca.end(), co.begin(),
+				 [](int i, int j) { return 2*i + 2*j; },
+				 1);
+  static_assert(std::is_same_v<decltype(end), decltype(co.begin())>);
+  VERIFY( end.ptr == out+10 );
+  VERIFY( out[0] == 4 );
+  VERIFY( out[1] == (2*4+2*2) );
+  VERIFY( out[2] == (2*12+2*3) );
+  VERIFY( out[3] == (2*30+2*4) );
+  VERIFY( out[4] == (2*68+2*5) );
+  VERIFY( out[5] == (2*146+2*6) );
+  VERIFY( out[6] == (2*304+2*7));
+  VERIFY( out[7] == (2*622+2*8) );
+  VERIFY( out[8] == (2*1260+2*9) );
+  VERIFY( out[9] == (2*2538+2*10) );
+}
+
+int
+main()
+{
+  test01();
+  test02();
+  test03();
+}
