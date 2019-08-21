00391db3c7af0b3ef2a0698bfd9d8e3a1ee47b85
prathamesh3492 <prathamesh3492@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-08-21 20:41:41 +0000

    2019-08-22  Prathamesh Kulkarni  <prathamesh.kulkarni@linaro.org> 	    Richard Sandiford  <richard.sandiford@arm.com>
    	PR target/88839
    	* config/aarch64/aarch64.c (aarch64_evpc_sel): New function.
    	(aarch64_expand_vec_perm_const_1): Call aarch64_evpc_sel.
    
    testsuite/
    	* gcc.target/aarch64/sve/sel_1.c: New test.
    	* gcc.target/aarch64/sve/sel_2.c: Likewise.
    	* gcc.target/aarch64/sve/sel_3.c: Likewise.
    	* gcc.target/aarch64/sve/sel_4.c: Likewise.
    	* gcc.target/aarch64/sve/sel_5.c: Likewise.
    	* gcc.target/aarch64/sve/sel_6.c: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@274810 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/aarch64/sve/sel_3.c
@@ -0,0 +1,50 @@
+/* { dg-do assemble { target aarch64_asm_sve_ok } } */
+/* { dg-options "-O2 -msve-vector-bits=256 --save-temps" } */
+
+#include <stdint.h>
+
+typedef int8_t vnx16qi __attribute__((vector_size (32)));
+typedef int16_t vnx8hi __attribute__((vector_size (32)));
+typedef int32_t vnx4si __attribute__((vector_size (32)));
+typedef _Float16 vnx8hf __attribute__((vector_size (32)));
+typedef float vnx4sf __attribute__((vector_size (32)));
+
+/* Predicate vector: 1 0 0 0 0 0 0 0 ... */
+
+#define MASK_32		{ 0, 33, 34, 35, 36, 37, 38, 39,  \
+			  8, 41, 42, 43, 44, 45, 46, 47,  \
+			  16, 49, 50, 51, 52, 53, 54, 55, \
+			  24, 57, 58, 59, 60, 61, 62, 63  }
+
+/* Predicate vector: 1 0 0 0 ... */
+
+#define MASK_16		{ 0, 17, 18, 19, 4, 21, 22, 23, \
+			  8, 25, 26, 27, 12, 29, 30, 31 } 
+
+/* Predicate vector: 1 0 ... */
+
+#define MASK_8		{ 0, 9, 2, 11, 4, 13, 6, 15 }
+
+#define INDEX_32 vnx16qi
+#define INDEX_16 vnx8hi
+#define INDEX_8 vnx4si
+
+#define PERMUTE(type, nunits)						\
+type permute_##type (type x, type y)					\
+{									\
+  return __builtin_shuffle (x, y, (INDEX_##nunits) MASK_##nunits);	\
+}
+
+PERMUTE(vnx16qi, 32)
+PERMUTE(vnx8hi, 16)
+PERMUTE(vnx4si, 8)
+PERMUTE(vnx8hf, 16)
+PERMUTE(vnx4sf, 8)
+
+/* { dg-final { scan-assembler-not {\ttbl\t} } } */
+
+/* { dg-final { scan-assembler-times {\tsel\tz[0-9]+\.b, p[0-9]+, z[0-9]+\.b, z[0-9]+\.b\n} 1 } } */
+/* { dg-final { scan-assembler-times {\tsel\tz[0-9]+\.h, p[0-9]+, z[0-9]+\.h, z[0-9]+\.h\n} 2 } } */
+/* { dg-final { scan-assembler-times {\tsel\tz[0-9]+\.s, p[0-9]+, z[0-9]+\.s, z[0-9]+\.s\n} 2 } } */
+
+/* { dg-final { scan-assembler-times {\tptrue\tp[0-9]+\.d, vl4\n} 5 } } */
