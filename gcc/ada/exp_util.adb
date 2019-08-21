78170c8ea108d76c9ed44b9a59546aadf64e9c3e
Eric Botcazou <ebotcazou@adacore.com>
2019-08-21 08:29:42 +0000

    [Ada] Fix assertion failure on derived private protected type
    This fixes an assertion failure on the instantiation of a generic
    package on a type derived from the private view of a protected type,
    ultimately caused by Finalize_Address returning Empty for the subtype
    built for the generic actual type of the instantiation.
    
    Finalize_Address has a special processing for untagged derivations of
    private views, but it would no longer trigger for the subtype because
    this subtype is now represented as a subtype of an implicit derived base
    type instead of as the derived type of an implicit subtype previously.
    
    2019-08-21  Eric Botcazou  <ebotcazou@adacore.com>
    
    gcc/ada/
    
    	* exp_util.adb (Finalize_Address): Deal consistently with
    	subtypes of private protected types.
    
    gcc/testsuite/
    
    	* gnat.dg/prot9.adb, gnat.dg/prot9_gen.ads,
    	gnat.dg/prot9_pkg1.ads, gnat.dg/prot9_pkg2.ads: New testcase.
    
    From-SVN: r274778

--- gcc/ada/exp_util.adb
+++ gcc/ada/exp_util.adb
@@ -5347,6 +5347,7 @@ package body Exp_Util is
    ----------------------
 
    function Finalize_Address (Typ : Entity_Id) return Entity_Id is
+      Btyp : constant Entity_Id := Base_Type (Typ);
       Utyp : Entity_Id := Typ;
 
    begin
@@ -5386,12 +5387,12 @@ package body Exp_Util is
       --  records do not automatically inherit operations, but maybe they
       --  should???)
 
-      if Is_Untagged_Derivation (Typ) then
-         if Is_Protected_Type (Typ) then
-            Utyp := Corresponding_Record_Type (Root_Type (Base_Type (Typ)));
+      if Is_Untagged_Derivation (Btyp) then
+         if Is_Protected_Type (Btyp) then
+            Utyp := Corresponding_Record_Type (Root_Type (Btyp));
 
          else
-            Utyp := Underlying_Type (Root_Type (Base_Type (Typ)));
+            Utyp := Underlying_Type (Root_Type (Btyp));
 
             if Is_Protected_Type (Utyp) then
                Utyp := Corresponding_Record_Type (Utyp);
