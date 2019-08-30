74870a4e38a1d829748a404c836ca3f31767a0e4
Jakub Jelinek <jakub@redhat.com>
2019-08-30 14:39:09 +0200

    backport: re PR tree-optimization/90208 (error: EH landing pad label)
    	Backported from mainline
    	2019-04-24  Jakub Jelinek  <jakub@redhat.com>
    
    	PR tree-optimization/90208
    	* tree-cfg.c (remove_bb): Move forced labels from removed bbs
    	after labels of new_bb, not before them.
    
    	* gcc.dg/tsan/pr90208-2.c: New test.
    
    From-SVN: r275151

--- gcc/tree-cfg.c
+++ gcc/tree-cfg.c
@@ -2170,7 +2170,7 @@ remove_bb (basic_block bb)
 		}
 
 	      new_bb = bb->prev_bb;
-	      new_gsi = gsi_start_bb (new_bb);
+	      new_gsi = gsi_after_labels (new_bb);
 	      gsi_remove (&i, false);
 	      gsi_insert_before (&new_gsi, stmt, GSI_NEW_STMT);
 	    }
