6b8d77b92690197285b7999ed4743d0d7ed6a31a
Jakub Jelinek <jakub@redhat.com>
2019-08-30 14:27:21 +0200

    backport: re PR target/89752 (ICE in emit_move_insn, at expr.c:3723)
    	Backported from mainline
    	2019-03-19  Jakub Jelinek  <jakub@redhat.com>
    
    	PR target/89752
    	* gimplify.c (gimplify_asm_expr): For output argument with
    	TREE_ADDRESSABLE type, clear allows_reg if it allows memory, otherwise
    	diagnose error.
    
    	* g++.dg/ext/asm15.C: Check for particular diagnostic wording.
    	* g++.dg/ext/asm16.C: Likewise.
    	* g++.dg/ext/asm17.C: New test.
    
    From-SVN: r275136

--- gcc/testsuite/g++.dg/ext/asm15.C
+++ gcc/testsuite/g++.dg/ext/asm15.C
@@ -6,5 +6,6 @@ struct S { S (); ~S (); int s; };
 void
 foo (S &s)
 {
-  __asm volatile ("" : "+r" (s) : : "memory");	// { dg-error "" }
+  __asm volatile ("" : "+r" (s) : : "memory");	// { dg-error "impossible constraint" }
+						// { dg-error "must stay in memory" "" { target *-*-* } .-1 }
 }
