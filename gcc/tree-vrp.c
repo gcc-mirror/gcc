2084eeb84e3d3075005ae90cdbd252f212d5311b
msebor <msebor@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-08-30 17:42:57 +0000

    PR middle-end/91584 - Bogus warning from -Warray-bounds during string assignment
    gcc/ChangeLog:
    
    	PR middle-end/91584
    	* tree-vrp.c (vrp_prop::check_mem_ref): Normalize type domain bounds
    	before using them to validate MEM_REF offset.
    
    gcc/testsuite/ChangeLog:
    	* gfortran.dg/char_array_constructor_4.f90: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@275210 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-vrp.c
+++ gcc/tree-vrp.c
@@ -4703,31 +4703,23 @@ vrp_prop::check_mem_ref (location_t location, tree ref,
       || RECORD_OR_UNION_TYPE_P (reftype))
     return false;
 
+  arrbounds[0] = 0;
+
   offset_int eltsize;
   if (TREE_CODE (reftype) == ARRAY_TYPE)
     {
       eltsize = wi::to_offset (TYPE_SIZE_UNIT (TREE_TYPE (reftype)));
-
       if (tree dom = TYPE_DOMAIN (reftype))
 	{
 	  tree bnds[] = { TYPE_MIN_VALUE (dom), TYPE_MAX_VALUE (dom) };
-	  if (array_at_struct_end_p (arg)
-	      || !bnds[0] || !bnds[1])
-	    {
-	      arrbounds[0] = 0;
-	      arrbounds[1] = wi::lrshift (maxobjsize, wi::floor_log2 (eltsize));
-	    }
+	  if (array_at_struct_end_p (arg) || !bnds[0] || !bnds[1])
+	    arrbounds[1] = wi::lrshift (maxobjsize, wi::floor_log2 (eltsize));
 	  else
-	    {
-	      arrbounds[0] = wi::to_offset (bnds[0]) * eltsize;
-	      arrbounds[1] = (wi::to_offset (bnds[1]) + 1) * eltsize;
-	    }
+	    arrbounds[1] = (wi::to_offset (bnds[1]) - wi::to_offset (bnds[0])
+			    + 1) * eltsize;
 	}
       else
-	{
-	  arrbounds[0] = 0;
-	  arrbounds[1] = wi::lrshift (maxobjsize, wi::floor_log2 (eltsize));
-	}
+	arrbounds[1] = wi::lrshift (maxobjsize, wi::floor_log2 (eltsize));
 
       if (TREE_CODE (ref) == MEM_REF)
 	{
@@ -4742,7 +4734,6 @@ vrp_prop::check_mem_ref (location_t location, tree ref,
   else
     {
       eltsize = 1;
-      arrbounds[0] = 0;
       arrbounds[1] = wi::to_offset (TYPE_SIZE_UNIT (reftype));
     }
 
