a427504b6e16f0ea9fe766b48598b09882c53ac1
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-08-31 02:55:09 +0000

    runtime: always build panic32.go
        Avoids problems with arm64 ILP32 mode.  We might want to handle that
        mode better in general, but always building panic32.go is simple and
        fixes the build.
        
        Reviewed-on: https://go-review.googlesource.com/c/gofrontend/+/192723
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@275237 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgo/go/runtime/panic32.go
+++ libgo/go/runtime/panic32.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build 386 amd64p32 arm mips mipsle m68k nios2 sh shbe
-
 package runtime
 
 import _ "unsafe" // for go:linkname
