fae9df4a5d740933d227799c7f3385e4f8de67a4
Julian Brown <julian@codesourcery.com>
2019-09-03 13:37:50 -0700

    Fix up tests for oaccdevlow pass splitting
    	gcc/testsuite/
    	* c-c++-common/goacc/classify-kernels-unparallelized.c,
    	c-c++-common/goacc/classify-kernels.c,
    	c-c++-common/goacc/classify-parallel.c,
    	c-c++-common/goacc/classify-routine.c,
    	gfortran.dg/goacc/classify-kernels-unparallelized.f95,
    	gfortran.dg/goacc/classify-kernels.f95,
    	gfortran.dg/goacc/classify-parallel.f95,
    	gfortran.dg/goacc/classify-routine.f95: Scan oaccloops dump instead of
    	oaccdevlow pass.

--- gcc/testsuite/gfortran.dg/goacc/classify-routine.f95
+++ gcc/testsuite/gfortran.dg/goacc/classify-routine.f95
@@ -4,7 +4,7 @@
 ! { dg-additional-options "-O2" }
 ! { dg-additional-options "-fopt-info-optimized-omp" }
 ! { dg-additional-options "-fdump-tree-ompexp" }
-! { dg-additional-options "-fdump-tree-oaccdevlow" }
+! { dg-additional-options "-fdump-tree-oaccloops" }
 
 subroutine ROUTINE
   !$acc routine worker
@@ -25,6 +25,6 @@ end subroutine ROUTINE
 
 ! Check the offloaded function's classification and compute dimensions (will
 ! always be 1 x 1 x 1 for non-offloading compilation).
-! { dg-final { scan-tree-dump-times "(?n)Function is OpenACC routine level 1" 1 "oaccdevlow" } }
-! { dg-final { scan-tree-dump-times "(?n)Compute dimensions \\\[1, 1, 1\\\]" 1 "oaccdevlow" } }
-! { dg-final { scan-tree-dump-times "(?n)__attribute__\\(\\(oacc function \\(0 1, 1 1, 1 1\\), omp declare target \\(worker\\)\\)\\)" 1 "oaccdevlow" } }
+! { dg-final { scan-tree-dump-times "(?n)Function is OpenACC routine level 1" 1 "oaccloops" } }
+! { dg-final { scan-tree-dump-times "(?n)Compute dimensions \\\[1, 1, 1\\\]" 1 "oaccloops" } }
+! { dg-final { scan-tree-dump-times "(?n)__attribute__\\(\\(oacc function \\(0 1, 1 1, 1 1\\), omp declare target \\(worker\\)\\)\\)" 1 "oaccloops" } }
