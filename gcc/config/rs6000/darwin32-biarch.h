f8b3e1ef29ed71f0836b8ac54d8651e73ceffd9e
Iain Sandoe <iain@sandoe.co.uk>
2019-09-05 19:14:35 +0000

    [Darwin] Fix PR87030 and tidy config fragments.
    This is about 32/64b host and multilib support across the range of Darwin
    systems.
    
    Prior to Darwin8 (OS X 10.4), the toolchains support only PowerPC and only 32b.
    
    On Darwin8 it is possible to target a 64b multilib, but with support limited
    to a few of the main libraries on the system (not a recommended configuration).
    
    From Darwin9 to Darwin17 (OSX 10.5 to 10.13) it is possible to have either
    32 or 64b hosted toolchains, with support for a 64 or 32b multilib respectively.
    
    On Darwin9 the kernel is 32b, but with support for 64b executables, so it's
    conventional to build a 32b host toolchain supporting a 64b multilib. However
    this is not enforced (merely a convention).
    
    There is also some platform hardware supporting Darwin10/11 which is only 32b
    and for which the same situation applies. However, from Darwin10 to Darwin17,
    the majority of platform hardware supports a 64b kernel and it's conventional
    to build a 64b host toolchain with support for a 32b multilib.
    
    On/from Darwin18 (OS X 10.14), the development headers (in the SDK) no longer
    expose the interfaces for the 32b multilib support (although sufficient runtime
    support remains installed that the testsuite can be run for a 32b multilib).
    
    The PR is raised against this latter situation since the absence of exposed
    interfaces causes a 'default' bootstrap fail regardless of the availability of
    the runtimes. Given the number of permutations, I felt it warranted a general
    solution, especially since the current scheme of target headers and t-make
    fragments has become somewhat messy.
    
    The changes here enforce the single 32b PowerPC multilib for Darwin < 8 and the
    single X86 64b multilib for Darwin >= 18. This means that there is no longer
    any need to configure Darwin18+ '--disable-multilib', but also that if you want
    to use the ability to continue to test the compiler's 32b multilib there, you
    need to make a configuration targeting an earlier OS version (and using the
    SDK from that).
    
    2019-09-05  Iain Sandoe  <iain@sandoe.co.uk>
    
    	Backport from mainline
    	2019-07-24  Iain Sandoe  <iain@sandoe.co.uk>
    
    	PR bootstrap/87030
    	* config/i386/darwin.h (REAL_LIBGCC_SPEC): Revert change from r273749.
    
    	PR bootstrap/87030
    	* config/i386/darwin.h (REAL_LIBGCC_SPEC): Move from here...
    	* config/i386/darwin32-biarch.h .. to here.
    	* config/i386/darwin64-biarch.h: Adjust comments.
    	* config/rs6000/darwin32-biarch.h: Likewise.
    	* config/rs6000/darwin64-biarch.h: Likewise.
    	* config.gcc: Missed commit from r273746
    	(*-*-darwin*): Don't include CPU t-darwin here.
    	(i[34567]86-*-darwin*): Adjust to use biarch files. Produce
    	an error message if i686-darwin configuration is attempted for
    	Darwin >= 18.
    
    	Backport from mainline
    	2019-07-23  Iain Sandoe  <iain@sandoe.co.uk>
    
    	PR bootstrap/87030
    	* config.gcc (*-*-darwin*): Don't include CPU t-darwin here.
    	(i[34567]86-*-darwin*): Adjust to use biarch files. Produce
    	an error message if i686-darwin configuration is attempted for
    	Darwin >= 18.
    	(x86_64-*-darwin*): Switch to single multilib for Darwin >= 18.
    	(powerpc-*-darwin*): Use biarch files where needed.
    	(powerpc64-*-darwin*): Likewise.
    	* config/i386/darwin.h (REAL_LIBGCC_SPEC): Move to new biarch file.
    	(DARWIN_ARCH_SPEC, DARWIN_SUBARCH_SPEC): Revise for default single
    	arch case.
    	* config/i386/darwin32-biarch.h: New.
    	* config/i386/darwin64.h: Rename.
    	* gcc/config/i386/darwin64-biarch.h: To this.
    	* config/i386/t-darwin: Rename.
    	* gcc/config/i386/t-darwin32-biarch: To this.
    	* config/i386/t-darwin64: Rename.
    	* gcc/config/i386/t-darwin64-biarch: To this.
    	* config/rs6000/darwin32-biarch.h: New.
    	* config/rs6000/darwin64.h: Rename.
    	* config/rs6000/darwin64-biarch.h: To this.
    	(DARWIN_ARCH_SPEC, DARWIN_SUBARCH_SPEC): Revise for default single
    	arch case.
    	* config/rs6000/t-darwin8: Rename.
    	* config/rs6000/t-darwin32-biarch: To this.
    	* config/rs6000/t-darwin64 Rename.
    	* config/rs6000/t-darwin64-biarch: To this.
    
    From-SVN: r275429

---
+++ gcc/config/rs6000/darwin32-biarch.h
@@ -0,0 +1,49 @@
+/* Target definitions for PowerPC running Darwin (Mac OS X) for a 32b host
+   with a 64b miultilib.
+   Copyright (C) 2019 Free Software Foundation, Inc.
+
+   This file is part of GCC.
+
+   GCC is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 3, or (at your
+   option) any later version.
+
+   GCC is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GCC; see the file COPYING3.  If not see
+   <http://www.gnu.org/licenses/>.  */
+
+#undef DARWIN_ARCH_SPEC
+#define DARWIN_ARCH_SPEC "%{m64:ppc64;:ppc}"
+
+#undef DARWIN_SUBARCH_SPEC
+#define DARWIN_SUBARCH_SPEC "			\
+ %{m64: ppc64}					\
+ %{!m64:					\
+ %{mcpu=601:ppc601;				\
+   mcpu=603:ppc603;				\
+   mcpu=603e:ppc603;				\
+   mcpu=604:ppc604;				\
+   mcpu=604e:ppc604e;				\
+   mcpu=740:ppc750;				\
+   mcpu=750:ppc750;				\
+   mcpu=G3:ppc750;				\
+   mcpu=7400:ppc7400;				\
+   mcpu=G4:ppc7400;				\
+   mcpu=7450:ppc7450;				\
+   mcpu=970:ppc970;				\
+   mcpu=power4:ppc970;				\
+   mcpu=G5:ppc970;				\
+   :ppc}}"
+
+#undef SUBTARGET_EXTRA_SPECS
+#define SUBTARGET_EXTRA_SPECS			\
+  DARWIN_EXTRA_SPECS                            \
+  { "darwin_arch", DARWIN_ARCH_SPEC },		\
+  { "darwin_crt2", DARWIN_CRT2_SPEC },		\
+  { "darwin_subarch", DARWIN_SUBARCH_SPEC },
