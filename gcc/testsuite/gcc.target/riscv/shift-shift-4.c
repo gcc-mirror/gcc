36ec3f57d305e343ad1bbffa53e3484661a176a6
Jakub Jelinek <jakub@redhat.com>
2019-09-05 22:32:55 +0200

    RISC-V: Fix bad insn splits with paradoxical subregs.
    Shifting by more than the size of a SUBREG_REG doesn't work, so we either
    need to disable splits if an input is paradoxical, or else we need to
    generate a clean temporary for intermediate results.
    
    Jakub wrote the first version of this patch, so gets primary credit for it.
    
    	gcc/
    	PR target/91635
    	* config/riscv/riscv.md (zero_extendsidi2, zero_extendhi<GPR:mode>2,
    	extend<SHORT:mode><SUPERQI:mode>2): Don't split if
    	paradoxical_subreg_p (operands[0]).
    	(*lshrsi3_zero_extend_3+1, *lshrsi3_zero_extend_3+2): Add clobber and
    	use as intermediate value.
    
    	gcc/testsuite/
    	PR target/91635
    	* gcc.c-torture/execute/pr91635.c: New test.
    	* gcc.target/riscv/shift-shift-4.c: New test.
    	* gcc.target/riscv/shift-shift-5.c: New test.
    
    Co-Authored-By: Jim Wilson <jimw@sifive.com>
    
    From-SVN: r275444

---
+++ gcc/testsuite/gcc.target/riscv/shift-shift-4.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv32i -mabi=ilp32 -O2" } */
+
+/* One zero-extend shift can be eliminated by modifying the constant in the
+   greater than test.  Started working after modifying the splitter
+   lshrsi3_zero_extend_3+1 to use a temporary reg for the first split dest.  */
+int
+sub (int i)
+{
+  i &= 0x7fffffff;
+  return i > 0x7f800000;
+}
+/* { dg-final { scan-assembler-not "srli" } } */
