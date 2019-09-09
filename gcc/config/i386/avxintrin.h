837823f08ac2a6240815782614566f84e54c281a
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-09-09 10:51:04 +0000

    PR target/91704 	* config/i386/avxintrin.h (__v32qs): New typedef. 	* config/i386/avx2intrin.h (_mm256_cmpgt_epi8): Use casts to __v32qs 	instead of __v32qi.
    	* gcc.target/i386/pr91704.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-9-branch@275509 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/avxintrin.h
+++ gcc/config/i386/avxintrin.h
@@ -47,6 +47,7 @@ typedef unsigned int __v8su __attribute__ ((__vector_size__ (32)));
 typedef short __v16hi __attribute__ ((__vector_size__ (32)));
 typedef unsigned short __v16hu __attribute__ ((__vector_size__ (32)));
 typedef char __v32qi __attribute__ ((__vector_size__ (32)));
+typedef signed char __v32qs __attribute__ ((__vector_size__ (32)));
 typedef unsigned char __v32qu __attribute__ ((__vector_size__ (32)));
 
 /* The Intel API is flexible enough that we must allow aliasing with other
