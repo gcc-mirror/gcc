c16504f6eabad7e173e4fbcfacf52820fffcb9ec
Li Jia He <helijia@linux.ibm.com>
2019-09-16 14:22:16 +0000

    Fix PR88784, middle end is missing some optimizations about unsigned
    2019-09-16  Li Jia He  <helijia@linux.ibm.com>
    	    Qi Feng  <ffengqi@linux.ibm.com>
    
    	PR middle-end/88784
    	* match.pd (x >  y  &&  x != XXX_MIN): Optimize into 'x > y'.
    	(x >  y  &&  x == XXX_MIN): Optimize into 'false'.
    	(x <= y  &&  x == XXX_MIN): Optimize into 'x == XXX_MIN'.
    	(x <  y  &&  x != XXX_MAX): Optimize into 'x < y'.
    	(x <  y  &&  x == XXX_MAX): Optimize into 'false'.
    	(x >= y  &&  x == XXX_MAX): Optimize into 'x == XXX_MAX'.
    	(x >  y  ||  x != XXX_MIN): Optimize into 'x != XXX_MIN'.
    	(x <= y  ||  x != XXX_MIN): Optimize into 'true'.
    	(x <= y  ||  x == XXX_MIN): Optimize into 'x <= y'.
    	(x <  y  ||  x != XXX_MAX): Optimize into 'x != XXX_MAX'.
    	(x >= y  ||  x != XXX_MAX): Optimize into 'true'.
    	(x >= y  ||  x == XXX_MAX): Optimize into 'x >= y'.
    2019-09-16  Li Jia He  <helijia@linux.ibm.com>
    	    Qi Feng  <ffengqi@linux.ibm.com>
    
    	PR middle-end/88784
    	* gcc.dg/pr88784-1.c: New testcase.
    	* gcc.dg/pr88784-2.c: New testcase.
    	* gcc.dg/pr88784-3.c: New testcase.
    	* gcc.dg/pr88784-4.c: New testcase.
    	* gcc.dg/pr88784-5.c: New testcase.
    	* gcc.dg/pr88784-6.c: New testcase.
    	* gcc.dg/pr88784-7.c: New testcase.
    	* gcc.dg/pr88784-8.c: New testcase.
    	* gcc.dg/pr88784-9.c: New testcase.
    	* gcc.dg/pr88784-10.c: New testcase.
    	* gcc.dg/pr88784-11.c: New testcase.
    	* gcc.dg/pr88784-12.c: New testcase.
    
    Co-Authored-By: Qi Feng <ffengqi@linux.ibm.com>
    
    From-SVN: r275749

---
+++ gcc/testsuite/gcc.dg/pr88784-4.c
@@ -0,0 +1,32 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+#include <limits.h>
+
+_Bool and1(unsigned x, unsigned y)
+{
+  /* x > y && x == 0 --> false */
+  return x > y && x == 0;
+}
+
+_Bool and2(unsigned x, unsigned y)
+{
+  /* x < y && x == UINT_MAX --> false */
+  return x < y && x == UINT_MAX;
+}
+
+_Bool and3(signed x, signed y)
+{
+  /* x > y && x == INT_MIN --> false */
+  return x > y && x == INT_MIN;
+}
+
+_Bool and4(signed x, signed y)
+{
+  /* x < y && x == INT_MAX --> false */
+  return x < y && x == INT_MAX;
+}
+
+/* { dg-final { scan-tree-dump-not " == " "optimized" } } */
+/* { dg-final { scan-tree-dump-not " > " "optimized" } } */
+/* { dg-final { scan-tree-dump-not " < " "optimized" } } */
