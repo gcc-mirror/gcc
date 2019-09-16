224850c73e8f4e7ce289e8738ed3eb1d073f81df
iains <iains@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-09-16 00:30:59 +0000

    [Darwin, testsuite] Fix isysroot-1.c.
    For the test to succeed there needs to be some header that is to be found in
    the 'expected' place i.e. <sysroot>/usr/include/. It's important that it is
    not the name of a header for which fixincludes have been applied, since such
    headers will be found in the gcc include-fixed dir and, in general, reference
    additional headers. The dummy sysroot will prevent the additional headers
    from being found, resulting in a failed test. The fix is to use a header name
    that isn't expected to be present in a real sysroot.
    
    2019-09-16  Iain Sandoe  <iain@sandoe.co.uk>
    
    	Backport from mainline.
    	2019-06-24  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* gcc.dg/cpp/isysroot-1.c: Use <example.h> as the test header.
    	* gcc.dg/cpp/usr/include/stdio.h: Rename...
    	* gcc.dg/cpp/usr/include/example.h: ... to this.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@275742 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/cpp/isysroot-1.c
+++ gcc/testsuite/gcc.dg/cpp/isysroot-1.c
@@ -1,10 +1,17 @@
 /* { dg-options "-isysroot ${srcdir}/gcc.dg/cpp" } */
 /* { dg-do compile  { target *-*-darwin* } } */
 
-#include <stdio.h>
+/* For the test to succeed there needs to be some header that is to be found
+   in the 'expected' place i.e. <sysroot>/usr/include/.  It's important that
+   it is not the name of a header for which fixincludes have been applied,
+   since such headers will be found in the gcc include-fixed dir and, in
+   general, reference additional headers.  The dummy sysroot will prevent the
+   additional headers from being found, resulting in a failed test.  So use
+   a header name we don't expect to see. */
+#include <example.h>
 int main()
 {
-  /* Special stdio.h supplies function foo.  */
+  /* Special example.h supplies function foo.  */
   void (*x)(void) = foo;
   return 0;
 }
