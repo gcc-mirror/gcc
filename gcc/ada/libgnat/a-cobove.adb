5ce1c7733b720d583eb46910e738adc932ecf7ce
Bob Duff <duff@adacore.com>
2019-09-18 08:31:37 +0000

    [Ada] Avoid uninitialized variable in bounded containers
    In function Copy in Ada.Containers.Bounded_Ordered_Sets and other
    bounded containers packages, remove a possible use of an uninitialized
    variable. This was not a bug, because the uninitialized variable could
    be used only if checks are suppressed, and the checks would have failed,
    leading to erroneous execution.
    
    However, it seems more robust this way, and is probably equally
    efficient, and avoids a warning that is given if checks are suppressed,
    and the -Wall switch is given, and optimization is turned on.
    
    2019-09-18  Bob Duff  <duff@adacore.com>
    
    gcc/ada/
    
    	* libgnat/a-cbhama.adb, libgnat/a-cbhase.adb,
    	libgnat/a-cbmutr.adb, libgnat/a-cborma.adb,
    	libgnat/a-cborse.adb, libgnat/a-cobove.adb (Copy): Avoid reading
    	the uninitialized variable C in the Checks = False case. Change
    	variable to be a constant.
    
    gcc/testsuite/
    
    	* gnat.dg/containers1.adb, gnat.dg/containers1.ads: New
    	testcase.
    
    From-SVN: r275839

--- gcc/ada/libgnat/a-cobove.adb
+++ gcc/ada/libgnat/a-cobove.adb
@@ -451,18 +451,12 @@ package body Ada.Containers.Bounded_Vectors is
      (Source   : Vector;
       Capacity : Count_Type := 0) return Vector
    is
-      C : Count_Type;
-
+      C : constant Count_Type :=
+        (if Capacity = 0 then Source.Length
+         else Capacity);
    begin
-      if Capacity = 0 then
-         C := Source.Length;
-
-      elsif Capacity >= Source.Length then
-         C := Capacity;
-
-      elsif Checks then
-         raise Capacity_Error
-           with "Requested capacity is less than Source length";
+      if Checks and then C < Source.Length then
+         raise Capacity_Error with "Capacity too small";
       end if;
 
       return Target : Vector (C) do
