3aacb9ed526c68e49424ec4dc4833de8353d2a7c
Raphael Amiard <amiard@adacore.com>
2019-09-19 08:13:06 +0000

    [Ada] Add comments wrt. deallocation of bounded sets/maps
    2019-09-19  Raphael Amiard  <amiard@adacore.com>
    
    gcc/ada/
    
    	* libgnat/a-cbhama.ads, libgnat/a-cbhase.ads,
    	libgnat/a-chtgop.ads (Clear): Refine comments
    
    From-SVN: r275936

--- gcc/ada/libgnat/a-chtgop.ads
+++ gcc/ada/libgnat/a-chtgop.ads
@@ -107,8 +107,9 @@ package Ada.Containers.Hash_Tables.Generic_Operations is
 
    procedure Clear (HT : in out Hash_Table_Type);
    --  Deallocates each node in hash table HT. (Note that it only deallocates
-   --  the nodes, not the buckets array.) Program_Error is raised if the hash
-   --  table is busy.
+   --  the nodes, not the buckets array. Also note that for bounded containers,
+   --  the buckets array is not dynamically allocated). Program_Error is raised
+   --  if the hash table is busy.
 
    procedure Move (Target, Source : in out Hash_Table_Type);
    --  Moves (not copies) the buckets array and nodes from Source to
