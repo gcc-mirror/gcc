3950b229a5ed6710f30241c2ddc3c74909bf4740
Richard Henderson <richard.henderson@linaro.org>
2019-09-19 14:36:43 +0000

    aarch64: Implement -moutline-atomics
    	* config/aarch64/aarch64.opt (-moutline-atomics): New.
    	* config/aarch64/aarch64.c (aarch64_atomic_ool_func): New.
    	(aarch64_ool_cas_names, aarch64_ool_swp_names): New.
    	(aarch64_ool_ldadd_names, aarch64_ool_ldset_names): New.
    	(aarch64_ool_ldclr_names, aarch64_ool_ldeor_names): New.
    	(aarch64_expand_compare_and_swap): Honor TARGET_OUTLINE_ATOMICS.
    	* config/aarch64/atomics.md (atomic_exchange<ALLI>): Likewise.
    	(atomic_<atomic_op><ALLI>): Likewise.
    	(atomic_fetch_<atomic_op><ALLI>): Likewise.
    	(atomic_<atomic_op>_fetch<ALLI>): Likewise.
    	* doc/invoke.texi: Document -moutline-atomics.
    testsuite/
    	* gcc.target/aarch64/atomic-op-acq_rel.c: Use -mno-outline-atomics.
    	* gcc.target/aarch64/atomic-comp-swap-release-acquire.c: Likewise.
    	* gcc.target/aarch64/atomic-op-acquire.c: Likewise.
    	* gcc.target/aarch64/atomic-op-char.c: Likewise.
    	* gcc.target/aarch64/atomic-op-consume.c: Likewise.
    	* gcc.target/aarch64/atomic-op-imm.c: Likewise.
    	* gcc.target/aarch64/atomic-op-int.c: Likewise.
    	* gcc.target/aarch64/atomic-op-long.c: Likewise.
    	* gcc.target/aarch64/atomic-op-relaxed.c: Likewise.
    	* gcc.target/aarch64/atomic-op-release.c: Likewise.
    	* gcc.target/aarch64/atomic-op-seq_cst.c: Likewise.
    	* gcc.target/aarch64/atomic-op-short.c: Likewise.
    	* gcc.target/aarch64/atomic_cmp_exchange_zero_reg_1.c: Likewise.
    	* gcc.target/aarch64/atomic_cmp_exchange_zero_strong_1.c: Likewise.
    	* gcc.target/aarch64/sync-comp-swap.c: Likewise.
    	* gcc.target/aarch64/sync-op-acquire.c: Likewise.
    	* gcc.target/aarch64/sync-op-full.c: Likewise.
    
    From-SVN: r275968

--- gcc/testsuite/gcc.target/aarch64/atomic-comp-swap-release-acquire.c
+++ gcc/testsuite/gcc.target/aarch64/atomic-comp-swap-release-acquire.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-march=armv8-a+nolse -O2 -fno-ipa-icf" } */
+/* { dg-options "-march=armv8-a+nolse -O2 -fno-ipa-icf -mno-outline-atomics" } */
 
 #include "atomic-comp-swap-release-acquire.x"
 
