3686c5158dd58602f87af7c33d2d409e3ee7b53e
Andreas Krebbel <krebbel@linux.ibm.com>
2019-09-20 09:23:50 +0000

    re PR rtl-optimization/88751 (Performance regression reload vs lra)
    Fix PR88751
        
    This patch implements a small improvement for the heuristic in lra
    which decides when it has to activate the simpler register allocation
    algorithm.
        
    gcc/ChangeLog:
    
    2019-09-20  Andreas Krebbel  <krebbel@linux.ibm.com>
    
    	Backport from mainline
    	2019-06-06  Andreas Krebbel  <krebbel@linux.ibm.com>
    
    	PR rtl-optimization/88751
    	* ira.c (ira): Use the number of the actually referenced registers
    	when calculating the threshold.
    
    From-SVN: r275993

--- gcc/ira.c
+++ gcc/ira.c
@@ -5160,6 +5160,8 @@ ira (FILE *f)
   int ira_max_point_before_emit;
   bool saved_flag_caller_saves = flag_caller_saves;
   enum ira_region saved_flag_ira_region = flag_ira_region;
+  unsigned int i;
+  int num_used_regs = 0;
 
   clear_bb_flags ();
 
@@ -5175,12 +5177,17 @@ ira (FILE *f)
 
   ira_conflicts_p = optimize > 0;
 
+  /* Determine the number of pseudos actually requiring coloring.  */
+  for (i = FIRST_PSEUDO_REGISTER; i < DF_REG_SIZE (df); i++)
+    num_used_regs += !!(DF_REG_USE_COUNT (i) + DF_REG_DEF_COUNT (i));
+
   /* If there are too many pseudos and/or basic blocks (e.g. 10K
      pseudos and 10K blocks or 100K pseudos and 1K blocks), we will
      use simplified and faster algorithms in LRA.  */
   lra_simple_p
     = (ira_use_lra_p
-       && max_reg_num () >= (1 << 26) / last_basic_block_for_fn (cfun));
+       && num_used_regs >= (1 << 26) / last_basic_block_for_fn (cfun));
+
   if (lra_simple_p)
     {
       /* It permits to skip live range splitting in LRA.  */
