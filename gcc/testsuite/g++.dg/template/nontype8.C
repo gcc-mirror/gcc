0788210f804a2ba451bc9cbda26c256ad9f7c5f3
Paolo Carlini <paolo.carlini@oracle.com>
2019-09-23 19:29:55 +0000

    pt.c (check_explicit_specialization): Use cp_expr_loc_or_input_loc.
    /cp
    2019-09-23  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* pt.c (check_explicit_specialization): Use cp_expr_loc_or_input_loc.
    	(process_partial_specialization): Likewise.
    	(convert_nontype_argument_function): Likewise.
    	(invalid_tparm_referent_p): Likewise.
    	(convert_template_argument): Likewise.
    	(check_valid_ptrmem_cst_expr): Tidy.
    
    /testsuite
    2019-09-23  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* g++.dg/cpp0x/pr68724.C: Check location(s) too.
    	* g++.dg/cpp0x/variadic38.C: Likewise.
    	* g++.dg/cpp1z/nontype2.C: Likewise.
    	* g++.dg/parse/explicit1.C: Likewise.
    	* g++.dg/template/crash11.C: Likewise.
    	* g++.dg/template/non-dependent8.C: Likewise.
    	* g++.dg/template/nontype-array1.C: Likewise.
    	* g++.dg/template/nontype3.C: Likewise.
    	* g++.dg/template/nontype8.C: Likewise.
    	* g++.dg/template/partial5.C: Likewise.
    	* g++.dg/template/spec33.C: Likewise.
    	* g++.old-deja/g++.pt/memtemp64.C: Likewise.
    	* g++.old-deja/g++.pt/spec20.C: Likewise.
    	* g++.old-deja/g++.pt/spec21.C: Likewise.
    	* g++.old-deja/g++.robertl/eb103.C: Likewise.
    
    From-SVN: r276064

--- gcc/testsuite/g++.dg/template/nontype8.C
+++ gcc/testsuite/g++.dg/template/nontype8.C
@@ -6,8 +6,10 @@ template<int* p> class X { };
 int a[10];
 struct S { int m; static int s; } s;
 
-X<&a[2]> x3;                    // { dg-error "" } address of array element
-X<&s.m> x4;                     // { dg-error "" } address of non-static member
+X<&a[2]> x3;                    // { dg-error "3:.& a\\\[2\\\]. is not a valid template argument" "" { target c++17 } }
+// { dg-error "" "" { target c++14_down } .-1 }
+X<&s.m> x4;                     // { dg-error "3:.& s.S::m. is not a valid template argument" "" { target c++17 } }
+// { dg-error "" "" { target c++14_down } .-1 }
 X<&s.s> x5;                     // { dg-error "" "" { target { ! c++17 } } } &S::s must be used
 X<&S::s> x6;                    // OK: address of static member
 
