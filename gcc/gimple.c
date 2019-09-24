5944c363ebec1e5451fa3157351796231910faaa
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-09-24 11:38:29 +0000

    Use more switch statements.
    2019-09-24  Martin Liska  <mliska@suse.cz>
    
    	* cfgexpand.c (gimple_assign_rhs_to_tree): Use switch statement
    	instead of if-elseif-elseif-...
    	* gimple-expr.c (extract_ops_from_tree): Likewise.
    	* gimple.c (get_gimple_rhs_num_ops): Likewise.
    	* tree-ssa-forwprop.c (rhs_to_tree): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@276095 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/gimple.c
+++ gcc/gimple.c
@@ -2225,16 +2225,18 @@ dump_gimple_statistics (void)
 unsigned
 get_gimple_rhs_num_ops (enum tree_code code)
 {
-  enum gimple_rhs_class rhs_class = get_gimple_rhs_class (code);
-
-  if (rhs_class == GIMPLE_UNARY_RHS || rhs_class == GIMPLE_SINGLE_RHS)
-    return 1;
-  else if (rhs_class == GIMPLE_BINARY_RHS)
-    return 2;
-  else if (rhs_class == GIMPLE_TERNARY_RHS)
-    return 3;
-  else
-    gcc_unreachable ();
+  switch (get_gimple_rhs_class (code))
+    {
+    case GIMPLE_UNARY_RHS:
+    case GIMPLE_SINGLE_RHS:
+      return 1;
+    case GIMPLE_BINARY_RHS:
+      return 2;
+    case GIMPLE_TERNARY_RHS:
+      return 3;
+    default:
+      gcc_unreachable ();
+    }
 }
 
 #define DEFTREECODE(SYM, STRING, TYPE, NARGS)   			    \
