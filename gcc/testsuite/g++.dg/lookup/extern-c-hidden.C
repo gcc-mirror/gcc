085b46f9e4fd5a2ffeaf28419565f4cfa30c3e27
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-09-25 08:50:29 +0000

    /cp 2019-09-25  Paolo Carlini  <paolo.carlini@oracle.com>
    	* name-lookup.c (check_extern_c_conflict): Use DECL_SOURCE_LOCATION.
    	(check_local_shadow): Use it in three additional places.
    
    /testsuite
    2019-09-25  Paolo Carlini  <paolo.carlini@oracle.com>
    
    	* g++.dg/diagnostic/redeclaration-1.C: New.
    	* g++.dg/lookup/extern-c-hidden.C: Test location(s) too.
    	* g++.dg/lookup/extern-c-redecl.C: Likewise.
    	* g++.dg/lookup/extern-c-redecl6.C: Likewise.
    	* g++.old-deja/g++.other/using9.C: Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@276119 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/lookup/extern-c-hidden.C
+++ gcc/testsuite/g++.dg/lookup/extern-c-hidden.C
@@ -4,8 +4,8 @@ extern "C" float fabsf (float);  // { dg-message "previous declaration" }
 
 namespace Bob 
 {
-  extern "C" float fabsf (float, float); // { dg-error "C language" }
+  extern "C" float fabsf (float, float); // { dg-error "20:conflicting C language" }
   extern "C" double fabs (double, double); // { dg-message "previous declaration" }
 }
 
-extern "C" double fabs (double); // { dg-error "C language" }
+extern "C" double fabs (double); // { dg-error "19:conflicting C language" }
