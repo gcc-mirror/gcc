17404fb2aa244327e7aa8bda1736f4607127c587
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-09-25 12:31:53 +0000

    Implement LWG 3296 for basic_regex::assign
    	* include/bits/regex.h
    	(basic_regex::assign(const C*, size_t, flag_type)): Add default
    	argument (LWG 3296).
    	* testsuite/28_regex/basic_regex/assign/char/lwg3296.cc: New test.
    	* testsuite/28_regex/basic_regex/assign/wchar_t/lwg3296.cc: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@276121 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/28_regex/basic_regex/assign/char/lwg3296.cc
@@ -0,0 +1,36 @@
+// Copyright (C) 2019 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do run { target c++11 } }
+
+#include <regex>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  std::regex r("", std::regex_constants::grep);
+  r.assign("(.)[", 3);  // LWG 3296
+  VERIFY( r.flags() == std::regex_constants::ECMAScript );
+  VERIFY( r.mark_count() == 1 );
+}
+
+int
+main()
+{
+  test01();
+}
