f5c1bea71dce1fa87870b010f0821dee68b13102
Iain Sandoe <iain@sandoe.co.uk>
2019-09-28 20:05:38 +0000

    [X86, Darwin] Backport fix for pr82920 (part 4).
    As part of the backport for pr82920, the following three testcases
    that are only present on the 7 and 8 branch, also needed amendment.
    
    2019-09-28  Iain Sandoe  <iain@sandoe.co.uk>
    
    	PR target/82920
    	* gcc.target/i386/indirect-thunk-bnd-1.c: Adjust scan-asms for Darwin,
    	do not use -fno-pic on Darwin.
    	* gcc.target/i386/indirect-thunk-bnd-2.c: Likewise.
    	* gcc.target/i386/ret-thunk-25.c: Skip for Darwin, which has a
    	different ABI for returning this category of complex value.
    
    From-SVN: r276259

--- gcc/testsuite/gcc.target/i386/indirect-thunk-bnd-1.c
+++ gcc/testsuite/gcc.target/i386/indirect-thunk-bnd-1.c
@@ -1,5 +1,6 @@
 /* { dg-do compile { target { ! x32 } } } */
-/* { dg-options "-O2 -mno-indirect-branch-register -mfunction-return=keep -mindirect-branch=thunk -fcheck-pointer-bounds -mmpx -fno-pic" } */
+/* { dg-options "-O2 -mno-indirect-branch-register -mfunction-return=keep -mindirect-branch=thunk -fcheck-pointer-bounds -mmpx" } */
+/* { dg-additional-options "-fno-pic" { target { ! *-*-darwin* } } } */
 
 void (*dispatch) (char *);
 char buf[10];
@@ -11,8 +12,8 @@ foo (void)
 }
 
 /* { dg-final { scan-assembler "mov(?:l|q)\[ \t\]*_?dispatch" { target *-*-linux* } } } */
-/* { dg-final { scan-assembler "bnd jmp\[ \t\]*__x86_indirect_thunk_bnd_rax" { target lp64 } } } */
-/* { dg-final { scan-assembler "bnd call\[ \t\]*__x86_indirect_thunk_bnd_eax" { target ia32 } } } */
+/* { dg-final { scan-assembler "bnd jmp\[ \t\]*_?__x86_indirect_thunk_bnd_rax" { target lp64 } } } */
+/* { dg-final { scan-assembler "bnd call\[ \t\]*_?__x86_indirect_thunk_bnd_eax" { target ia32 } } } */
 /* { dg-final { scan-assembler "jmp\[ \t\]*\.LIND" } } */
 /* { dg-final { scan-assembler "bnd call\[ \t\]*\.LIND" } } */
 /* { dg-final { scan-assembler "bnd ret" } } */
