492c602a8109a97c90882fb6966b0c6673c53fb2
olegendo <olegendo@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-09-28 08:53:27 +0000

    gcc/ 2019-09-28  Oleg Endo  <olegendo@gcc.gnu.org>
    	PR target/86805
    	* config/sh/sh.c (TARGET_HAVE_SPECULATION_SAFE_VALUE): Define.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@276244 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sh/sh.c
+++ gcc/config/sh/sh.c
@@ -661,6 +661,9 @@ static const struct attribute_spec sh_attribute_table[] =
 #undef TARGET_CONSTANT_ALIGNMENT
 #define TARGET_CONSTANT_ALIGNMENT constant_alignment_word_strings
 
+#undef  TARGET_HAVE_SPECULATION_SAFE_VALUE
+#define TARGET_HAVE_SPECULATION_SAFE_VALUE speculation_safe_value_not_needed
+
 struct gcc_target targetm = TARGET_INITIALIZER;
 
 
