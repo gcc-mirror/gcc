8ab38f6cbc16025515571cd40f0f0d872d39c5ba
François Dumont <fdumont@gcc.gnu.org>
2019-10-06 15:29:04 +0000

    Add std::copy_n istreambuf_iterator specialization
    	* include/bits/stl_algo.h
    	(__copy_n_a(_IIte, _Size, _OIte)): New.
    	(__copy_n_a(istreambuf_iterator<>, _Size, _CharT*)): New declaration.
    	(__copy_n(_IIte, _Size, _OIte, input_iterator_tag)): Adapt to use
    	latter.
    	* include/bits/streambuf_iterator.h (istreambuf_iterator<>): Declare
    	std::__copy_n_a friend.
    	(__copy_n_a(istreambuf_iterator<>, _Size, _CharT*)): New.
    	* testsuite/25_algorithms/copy_n/istreambuf_iterator/1.cc: New.
    	* testsuite/25_algorithms/copy_n/istreambuf_iterator/1_neg.cc: New.
    	* testsuite/25_algorithms/copy_n/istreambuf_iterator/2_neg.cc: New.
    
    From-SVN: r276638

---
+++ libstdc++-v3/testsuite/25_algorithms/copy_n/istreambuf_iterator/1.cc
@@ -0,0 +1,73 @@
+// Copyright (C) 2019 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do run { target c++11 } }
+
+#include <algorithm>
+#include <sstream>
+#include <iterator>
+
+#include <testsuite_hooks.h>
+
+void test01()
+{
+  std::stringstream ss("12345");
+
+  std::string ostr(5, '0');
+  typedef std::istreambuf_iterator<char> istrb_ite;
+  auto res = std::copy_n(istrb_ite(ss), 0, ostr.begin());
+  VERIFY( res == ostr.begin() );
+  VERIFY( ostr.front() == '0' );
+
+  res = std::copy_n(istrb_ite(ss), 2, ostr.begin());
+  VERIFY( res == ostr.begin() + 2 );
+  VERIFY( ostr == "12000" );
+
+  res = std::copy_n(istrb_ite(ss), 3, ostr.begin() + 2);
+  VERIFY( res == ostr.begin() + 5 );
+  VERIFY( ostr == "12345" );
+}
+
+void test02()
+{
+  std::stringstream ss("12345");
+
+  std::string ostr(5, '0');
+  typedef std::istreambuf_iterator<char> istrb_ite;
+
+  istrb_ite ibfit(ss);
+  auto res = std::copy_n(ibfit, 3, std::copy_n(ibfit, 2, ostr.begin()));
+  VERIFY( res == ostr.begin() + 5 );
+  VERIFY( ostr == "12345" );
+}
+
+void test03()
+{
+  std::string ostr(5, '0');
+  typedef std::istreambuf_iterator<char> istrb_ite;
+
+  auto res = std::copy_n(istrb_ite(), 0, ostr.begin());
+  VERIFY( res == ostr.begin() );
+}
+
+int main()
+{
+  test01();
+  test02();
+  test03();
+  return 0;
+}
