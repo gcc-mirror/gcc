ea6e1dcb1afbe9cb338eec9408e718c4887a58a0
uros <uros@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-10-08 17:01:55 +0000

    PR target/91994 	* config/i386/i386.c (x86_avx_u128_mode_needed): Use SSE_REG 	instead of ALL_SSE_REG to check if function call preserves some 	256-bit SSE registers.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@276707 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -13530,7 +13530,7 @@ ix86_avx_u128_mode_needed (rtx_insn *insn)
 	 modes wider than 256 bits.  It's only safe to issue a
 	 vzeroupper if all SSE registers are clobbered.  */
       const function_abi &abi = insn_callee_abi (insn);
-      if (!hard_reg_set_subset_p (reg_class_contents[ALL_SSE_REGS],
+      if (!hard_reg_set_subset_p (reg_class_contents[SSE_REGS],
 				  abi.mode_clobbers (V4DImode)))
 	return AVX_U128_ANY;
 
