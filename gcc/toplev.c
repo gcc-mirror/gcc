019f36a648fd3f35e562eb7ddd3ff6393b30b4c7
Jan Hubicka <hubicka@ucw.cz>
2019-10-10 15:04:44 +0200

    re PR middle-end/92037 (LTO bootstrap broken in selftests)
    	PR middle-end/92037
    	* cgraph.c (symbol_table_test::symbol_table_test): Use ggc_alloc
    	rather than ggc_alloc_cleared to alloc symbol table.
    	* toplev.c (general_init): Likewise.
    	* cgraph.h (symbol_table): Explicitly construct every field.
    
    From-SVN: r276804

--- gcc/toplev.c
+++ gcc/toplev.c
@@ -1170,7 +1170,7 @@ general_init (const char *argv0, bool init_signals)
   /* Create the passes.  */
   g->set_passes (new gcc::pass_manager (g));
 
-  symtab = new (ggc_cleared_alloc <symbol_table> ()) symbol_table ();
+  symtab = new (ggc_alloc <symbol_table> ()) symbol_table ();
 
   statistics_early_init ();
   debuginfo_early_init ();
