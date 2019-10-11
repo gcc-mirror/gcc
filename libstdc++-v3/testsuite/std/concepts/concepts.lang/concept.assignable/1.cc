6dc6cd99d8bb4313585a1d9b93fc7a43fb2016a9
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-10-11 15:53:52 +0000

    Implement <concepts> header for C++20
    There are currently no tests for [concepts.compare], but they will be
    added ASAP.
    
    	* include/Makefile.am: Add new header.
    	* include/Makefile.in: Regenerate.
    	* include/precompiled/stdc++.h: Include <concepts>.
    	* include/std/concepts: New header for C++20.
    	* include/std/version (__cpp_lib_concepts): Define.
    	* scripts/create_testsuite_files: Look for test files in new std
    	directory.
    	* testsuite/libstdc++-dg/conformance.exp: Likewise.
    	* testsuite/std/concepts/concepts.callable/invocable.cc: New test.
    	* testsuite/std/concepts/concepts.callable/regular_invocable.cc: New
    	test.
    	* testsuite/std/concepts/concepts.callable/relation.cc: New test.
    	* testsuite/std/concepts/concepts.callable/strictweakorder.cc: New
    	test.
    	* testsuite/std/concepts/concepts.lang/concept.arithmetic/
    	floating_point.cc: New test.
    	* testsuite/std/concepts/concepts.lang/concept.arithmetic/integral.cc:
    	New test.
    	* testsuite/std/concepts/concepts.lang/concept.arithmetic/
    	signed_integral.cc: New test.
    	* testsuite/std/concepts/concepts.lang/concept.arithmetic/
    	unsigned_integral.cc: New test.
    	* testsuite/std/concepts/concepts.lang/concept.assignable/1.cc: New
    	test.
    	* testsuite/std/concepts/concepts.lang/concept.common/1.cc: New test.
    	* testsuite/std/concepts/concepts.lang/concept.commonref/1.cc: New
    	test.
    	* testsuite/std/concepts/concepts.lang/concept.constructible/1.cc:
    	New test.
    	* testsuite/std/concepts/concepts.lang/concept.convertible/1.cc:
    	New test.
    	* testsuite/std/concepts/concepts.lang/concept.copyconstructible/1.cc:
    	New test.
    	* testsuite/std/concepts/concepts.lang/concept.defaultconstructible/
    	1.cc: New test.
    	* testsuite/std/concepts/concepts.lang/concept.derived/1.cc:
    	New test.
    	* testsuite/std/concepts/concepts.lang/concept.destructible/1.cc:
    	New test.
    	* testsuite/std/concepts/concepts.lang/concept.moveconstructible/1.cc:
    	New test.
    	* testsuite/std/concepts/concepts.lang/concept.same/1.cc:
    	New test.
    	* testsuite/std/concepts/concepts.lang/concept.swappable/swap.cc:
    	New test.
    	* testsuite/std/concepts/concepts.lang/concept.swappable/swappable.cc:
    	New test.
    	* testsuite/std/concepts/concepts.lang/concept.swappable/
    	swappable_with.cc: New test.
    	* testsuite/std/concepts/concepts.object/copyable.cc: New test.
    	* testsuite/std/concepts/concepts.object/movable.cc: New test.
    	* testsuite/std/concepts/concepts.object/regular.cc: New test.
    	* testsuite/std/concepts/concepts.object/semiregular.cc: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@276892 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/std/concepts/concepts.lang/concept.assignable/1.cc
@@ -0,0 +1,85 @@
+// Copyright (C) 2019 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++2a" }
+// { dg-do compile { target c++2a } }
+
+#include <concepts>
+
+static_assert( !std::assignable_from<void, void> );
+static_assert( !std::assignable_from<void*, int*> );
+static_assert( std::assignable_from<void*&, int*> );
+static_assert( !std::assignable_from<void*&, const int*> );
+static_assert( std::assignable_from<const void*&, const int*> );
+static_assert( !std::assignable_from<char, char> );
+static_assert( std::assignable_from<char&, char> );
+static_assert( !std::assignable_from<float, float> );
+static_assert( std::assignable_from<float&, double> );
+static_assert( std::assignable_from<int*&, int*> );
+static_assert( std::assignable_from<int&, int&> );
+static_assert( std::assignable_from<int&, int&&> );
+static_assert( !std::assignable_from<int&&, int> );
+static_assert( !std::assignable_from<const int&, int> );
+static_assert( !std::assignable_from<const int&, int&> );
+static_assert( !std::assignable_from<const int&, const int> );
+static_assert( !std::assignable_from<const int&, const int&> );
+static_assert( !std::assignable_from<int(&)[], int(&)[]> );
+static_assert( !std::assignable_from<int(&)[], int> );
+static_assert( !std::assignable_from<int(&)[2], int(&)[2]> );
+static_assert( !std::assignable_from<int(), int()> );
+static_assert( !std::assignable_from<int(*)(), int(*)()> );
+static_assert( std::assignable_from<int(*&)(), int(*)()> );
+static_assert( std::assignable_from<int(*&)(), std::nullptr_t> );
+static_assert( std::assignable_from<int(*&)(), int(*)() noexcept> );
+static_assert( std::assignable_from<int(*&)(), int(&)() noexcept> );
+static_assert( !std::assignable_from<int(&)(), std::nullptr_t> );
+static_assert( !std::assignable_from<int(&)(), int(&)() noexcept> );
+
+enum E { };
+static_assert( !std::assignable_from<E, E> );
+static_assert( std::assignable_from<E&, E> );
+static_assert( std::assignable_from<E&, E&> );
+static_assert( std::assignable_from<E&, const E&> );
+static_assert( !std::assignable_from<const E&, const E&> );
+enum class CE { };
+static_assert( !std::assignable_from<CE, CE> );
+static_assert( std::assignable_from<CE&, CE&> );
+static_assert( std::assignable_from<CE&, const CE&> );
+static_assert( !std::assignable_from<const CE&, const CE&> );
+struct A { };
+static_assert( !std::assignable_from<A, A> );
+static_assert( std::assignable_from<A&, A> );
+static_assert( !std::assignable_from<A, A&> );
+static_assert( std::assignable_from<A&, const A&> );
+union B { };
+static_assert( !std::assignable_from<B, B> );
+static_assert( std::assignable_from<B&, B> );
+static_assert( !std::assignable_from<B, B&> );
+static_assert( std::assignable_from<B&, const B&> );
+
+struct C
+{
+  C(int) { }
+  C& operator=(const C&) { return *this; }
+  C& operator=(int) { return *this; }
+  void operator=(void*) { }
+};
+static_assert( std::assignable_from<C&, C> );
+static_assert( std::assignable_from<C&, const C&> );
+static_assert( std::assignable_from<C&, int> );
+static_assert( !std::assignable_from<C&, void*> );
+static_assert( !std::assignable_from<C&, std::nullptr_t> );
