971f73e0c923df2d8422519a3b40b8fe39295dde
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-10-14 08:48:42 +0000

    PR libgomp/92081 	* testsuite/libgomp.fortran/target-simd.f90: Iterate from 1 rather 	than 0.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@276956 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgomp/testsuite/libgomp.fortran/target-simd.f90
+++ libgomp/testsuite/libgomp.fortran/target-simd.f90
@@ -10,14 +10,14 @@ program test
   b = 0
 
   !$omp target simd map(to:a) map(from:b)
-  do i = 0, size(a)
+  do i = 1, size(a)
     b(i) = 5.0 * a(i)
   end do
 
   if (any (b - 5.0 *a > 10.0*epsilon(a))) call abort()
 
   !$omp target simd map(to:a) map(from:b)
-  do i = 0, size(a)
+  do i = 1, size(a)
     b(i) = 2.0 * a(i)
   end do
   !$omp end target simd
