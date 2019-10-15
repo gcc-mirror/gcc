a7c4d6d1c29b62d97014d30848ee542e309dbe9c
Hongtao Liu <liuhongt@gcc.gnu.org>
2019-10-15 07:44:15 +0000

    Add missing mask[z]_roundscale_[round]_s[d,s] intrinsics
    gcc/
    	* config/i386/avx512fintrin.h (_mm_mask_roundscale_ss,
    	_mm_maskz_roundscale_ss, _mm_maskz_roundscale_round_ss,
    	_mm_maskz_roundscale_round_ss, _mm_mask_roundscale_sd,
    	_mm_maskz_roundscale_sd, _mm_mask_roundscale_round_sd,
    	_mm_maskz_roundscale_round_sd): New intrinsics.
    	(_mm_roundscale_ss, _mm_roundscale_round_ss): Use
    	__builtin_ia32_rndscales?_mask_round builtins instead of
    	__builtin_ia32_rndscales?_round.
    	* config/i386/i386-builtin.def (__builtin_ia32_rndscaless_round,
    	__builtin_ia32_rndscalesd_round): Remove.
    	(__builtin_ia32_rndscaless_mask_round,
    	__builtin_ia32_rndscalesd_mask_round): New intrinsics.
    	* config/i386/sse.md
    	(avx512f_rndscale<mode><round_saeonly_name>): Renamed to ...
    	(avx512f_rndscale<mode><mask_scalar_name><round_saeonly_scalar_name>):
    	 ... this, adjust and add subst atrributes to make it maskable.
    
    gcc/testsuite/
    	* gcc.target/i386/avx512f-vrndscaless-1.c: Add scan-assembler-times
    	directives for newly expected instructions.
    	* gcc.target/i386/avx512f-vrndscalesd-1.c: Likewise.
    	* gcc.target/i386/avx512f-vrndscaless-2.c
    	(avx512f_test): Add tests for new intrinsics.
    	* gcc.target/i386/avx512f-vrndscalesd-2.c: Likewise.
    	* gcc.target/i386/avx-1.c (__builtin_ia32_rndscalefss_round,
    	__builtin_ia32_rndscalefsd_round): Remove.
    	(__builtin_ia32_rndscalefss_mask_round,
    	__builtin_ia32_rndscalefsd_mask_round): Define.
    	* gcc.target/i386/sse-13.c: Ditto.
    	* gcc.target/i386/sse-23.c: Ditto.
    
    From-SVN: r276986

--- gcc/config/i386/sse.md
+++ gcc/config/i386/sse.md
@@ -9694,18 +9694,17 @@
    (set_attr "prefix" "evex")
    (set_attr "mode" "<MODE>")])
 
-(define_insn "avx512f_rndscale<mode><round_saeonly_name>"
+(define_insn "avx512f_rndscale<mode><mask_scalar_name><round_saeonly_scalar_name>"
   [(set (match_operand:VF_128 0 "register_operand" "=v")
 	(vec_merge:VF_128
 	  (unspec:VF_128
-	    [(match_operand:VF_128 1 "register_operand" "v")
-	     (match_operand:VF_128 2 "<round_saeonly_nimm_predicate>" "<round_saeonly_constraint>")
+	    [(match_operand:VF_128 2 "<round_saeonly_scalar_nimm_predicate>" "<round_saeonly_scalar_constraint>")
 	     (match_operand:SI 3 "const_0_to_255_operand")]
 	    UNSPEC_ROUND)
-	  (match_dup 1)
+	  (match_operand:VF_128 1 "register_operand" "v")
 	  (const_int 1)))]
   "TARGET_AVX512F"
-  "vrndscale<ssescalarmodesuffix>\t{%3, <round_saeonly_op4>%2, %1, %0|%0, %1, %<iptr>2<round_saeonly_op4>, %3}"
+  "vrndscale<ssescalarmodesuffix>\t{%3, <round_saeonly_scalar_mask_op4>%2, %1, %0<mask_scalar_operand4>|%0<mask_scalar_operand4>, %1, %<iptr>2<round_saeonly_scalar_mask_op4>, %3}"
   [(set_attr "length_immediate" "1")
    (set_attr "prefix" "evex")
    (set_attr "mode" "<MODE>")])
--- gcc/testsuite/gcc.target/i386/avx512f-vrndscalesd-1.c
+++ gcc/testsuite/gcc.target/i386/avx512f-vrndscalesd-1.c
@@ -1,14 +1,24 @@
 /* { dg-do compile } */
 /* { dg-options "-mavx512f -O2" } */
-/* { dg-final { scan-assembler-times "vrndscalesd\[ \\t\]+\\S*,\[ \\t\]+\{sae\}\[^\n\]*%xmm\[0-9\]+(?:\n|\[ \\t\]+#)" 1 } } */
+/* { dg-final { scan-assembler-times "vrndscalesd\[ \\t\]+\[^\{\n\]*%xmm\[0-9\]+\[^\n\]*%xmm\[0-9\]+\[^\n\]*%xmm\[0-9\]+(?:\n|\[ \\t\]+#)" 1 } } */
+/* { dg-final { scan-assembler-times "vrndscalesd\[ \\t\]+\[^\n\]*\{sae\}\[^\{\n\]*%xmm\[0-9\]+\[^\n\]*%xmm\[0-9\]+\[^\n\]*%xmm\[0-9\]+(?:\n|\[ \\t\]+#)" 1 } } */
+/* { dg-final { scan-assembler-times "vrndscalesd\[ \\t\]+\[^\{\n\]*%xmm\[0-9\]+\[^\n\]*%xmm\[0-9\]+\[^\n\]*%xmm\[0-9\]+\{%k\[1-7\]\}(?:\n|\[ \\t\]+#)" 1 } } */
+/* { dg-final { scan-assembler-times "vrndscalesd\[ \\t\]+\[^\n\]*\{sae\}\[^\{\n\]*%xmm\[0-9\]+\[^\n\]*%xmm\[0-9\]+\[^\n\]*%xmm\[0-9\]+\{%k\[1-7\]\}(?:\n|\[ \\t\]+#)" 1 } } */
+/* { dg-final { scan-assembler-times "vrndscalesd\[ \\t\]+\[^\{\n\]*%xmm\[0-9\]+\[^\n\]*%xmm\[0-9\]+\[^\n\]*%xmm\[0-9\]+\{%k\[1-7\]\}\{z\}(?:\n|\[ \\t\]+#)" 1 } } */
+/* { dg-final { scan-assembler-times "vrndscalesd\[ \\t\]+\[^\n\]*\{sae\}\[^\{\n\]*%xmm\[0-9\]+\[^\n\]*%xmm\[0-9\]+\[^\n\]*%xmm\[0-9\]+\{%k\[1-7\]\}\{z\}(?:\n|\[ \\t\]+#)" 1 } } */
 
 #include <immintrin.h>
 
 volatile __m128d x1, x2;
+volatile __mmask8 m;
 
 void extern
 avx512f_test (void)
 {
   x1 = _mm_roundscale_sd (x1, x2, 0x42);
   x1 = _mm_roundscale_round_sd (x1, x2, 0x42, _MM_FROUND_NO_EXC);
+  x1 = _mm_mask_roundscale_sd (x1, m, x1, x2, 0x42);
+  x1 = _mm_mask_roundscale_round_sd (x1, m, x1, x2, 0x42, _MM_FROUND_NO_EXC);
+  x1 = _mm_maskz_roundscale_sd (m, x1, x2, 0x42);
+  x1 = _mm_maskz_roundscale_round_sd (m, x1, x2, 0x42, _MM_FROUND_NO_EXC);
 }
