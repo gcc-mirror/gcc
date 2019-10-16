7c3dbd220797599172e5982eadce7fcfb860361c
msebor <msebor@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-10-16 19:24:36 +0000

    PR tree-optimization/83821 - local aggregate initialization defeats strlen optimization
    gcc/ChangeLog:
    
    	PR tree-optimization/83821
    	* tree-ssa-strlen.c (maybe_invalidate): Add argument.  Consider
    	the length of a string when available.
    	(handle_builtin_memset) Add argument.
    	(handle_store, strlen_check_and_optimize_call): Same.
    	(check_and_optimize_stmt): Same.  Pass it to callees.
    
    gcc/testsuite/ChangeLog:
    
    	PR tree-optimization/83821
    	* c-c++-common/Warray-bounds-4.c: Remove XFAIL.
    	* gcc.dg/strlenopt-82.c: New test.
    	* gcc.dg/strlenopt-83.c: Same.
    	* gcc.dg/strlenopt-84.c: Same.
    	* gcc.dg/strlenopt-85.c: Same.
    	* gcc.dg/strlenopt-86.c: Same.
    	* gcc.dg/tree-ssa/calloc-4.c: Same.
    	* gcc.dg/tree-ssa/calloc-5.c: Same.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@277080 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/strlenopt-85.c
@@ -0,0 +1,44 @@
+/* PR tree-optimization/83821 - local aggregate initialization defeats
+   strlen optimization
+   Verify that a strlen() call is eliminated for a pointer to a region
+   of memory allocated by calloc() even if one or more nul bytes are
+   written into it.
+   { dg-do compile }
+   { dg-options "-O2 -fdump-tree-optimized" } */
+
+unsigned n0, n1;
+
+void* elim_strlen_calloc_store_memset_1 (unsigned a, unsigned b)
+{
+  char *p = __builtin_calloc (a, 1);
+
+  p[0] = '\0';
+  p[1] = '\0';
+  p[2] = '\0';
+  p[3] = '\0';
+
+  __builtin_memset (p, 0, b);
+
+  n0 = __builtin_strlen (p);
+
+  return p;
+}
+
+void* elim_strlen_calloc_store_memset_2 (unsigned a, unsigned b, unsigned c)
+{
+  char *p = __builtin_calloc (a, 1);
+
+  p[1] = '\0';
+  __builtin_memset (p, 0, b);
+
+  n0 = __builtin_strlen (p);
+
+  p[3] = 0;
+  __builtin_memset (p, 0, c);
+
+  n1 = __builtin_strlen (p);
+
+  return p;
+}
+
+/* { dg-final { scan-tree-dump-not "__builtin_strlen" "optimized" } } */
