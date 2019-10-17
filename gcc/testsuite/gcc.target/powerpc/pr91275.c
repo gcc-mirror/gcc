865f5907a572efe85661a37e69670905bd97d0e5
wschmidt <wschmidt@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-10-17 15:35:28 +0000

    2019-10-17  Bill Schmidt  <wschmidt@linux.ibm.com>
    	Backport from mainline
    	2019-10-15  Bill Schmidt  <wschmidt@linux.ibm.com>
    
    	PR target/92093
    	* gcc.target/powerpc/pr91275.c: Fix type and endian issues.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-7-branch@277119 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/pr91275.c
+++ gcc/testsuite/gcc.target/powerpc/pr91275.c
@@ -10,12 +10,17 @@ int main() {
   const unsigned long long r0l = 0x8e7dfceac070e3a0;
   vector unsigned long long r0 = (vector unsigned long long) {r0l, 0}, v;
   const vector unsigned long long pd
-    = (vector unsigned long) {0xc2LLU << 56, 0};
+    = (vector unsigned long long) {0xc2LLU << 56, 0};
 
   v = __builtin_crypto_vpmsumd ((vector unsigned long long) {r0[0], 0}, pd);
 
+#if __LITTLE_ENDIAN__
   if (v[0] != 0x4000000000000000 || v[1] != 0x65bd7ab605a4a8ff)
     __builtin_abort ();
+#else
+  if (v[1] != 0x4000000000000000 || v[0] != 0x65bd7ab605a4a8ff)
+    __builtin_abort ();
+#endif
 
   return 0;
 }
