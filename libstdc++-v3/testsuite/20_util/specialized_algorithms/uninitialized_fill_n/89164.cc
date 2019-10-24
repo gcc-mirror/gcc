6192703ef919da1ddd68d4477783827243b5322a
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-10-24 12:54:58 +0000

    PR libstdc++/89164 enforce constraints for uninitialized algos
    The memmove optimizations for std::uninitialized_copy/fill/_n will
    compile even if the type is not copy constructible, because std::copy
    doesn't require copy construction to work. But the uninitialized
    algorithms do require it.
    
    This adds explicit static assertions to ensure we don't allow ill-formed
    initializations.
    
    Backport from mainline
    2019-08-30  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/89164
    	* include/bits/stl_algobase.h (__copy_move): Give descriptive names
    	to template parameters.
    	* include/bits/stl_uninitialized.h (uninitialized_copy)
    	(uninitialized_fill, uninitialized_fill_n): Add static assertions to
    	diagnose invalid uses.
    	* testsuite/20_util/specialized_algorithms/uninitialized_copy/1.cc:
    	Adjust expected error.
    	* testsuite/20_util/specialized_algorithms/uninitialized_copy/89164.cc:
    	New test.
    	* testsuite/20_util/specialized_algorithms/uninitialized_copy_n/
    	89164.cc: New test.
    	* testsuite/20_util/specialized_algorithms/uninitialized_fill/89164.cc:
    	New test.
    	* testsuite/20_util/specialized_algorithms/uninitialized_fill_n/
    	89164.cc: New test.
    	* testsuite/23_containers/vector/cons/89164.cc: New test.
    	* testsuite/23_containers/vector/cons/89164_c++17.cc: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-9-branch@277388 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/20_util/specialized_algorithms/uninitialized_fill_n/89164.cc
+++ libstdc++-v3/testsuite/20_util/specialized_algorithms/uninitialized_fill_n/89164.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2018-2019 Free Software Foundation, Inc.
+// Copyright (C) 2019 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -17,21 +17,19 @@
 
 // { dg-do compile { target c++11 } }
 
-#include <memory>
+#include <vector>
 
-struct T
-{
-  T() { }
-  T(const T&) = delete;
+struct X {
+  X() = default;
+  X(const X&) = delete;
 };
 
-static_assert(__is_trivially_assignable(T&, const T&) &&
-  !__is_trivial(T), "T is only trivially copy assignable");
-
-void
-test01(T* result)
+void test01()
 {
-  T t[1];
-  std::uninitialized_copy(t, t+1, result); // { dg-error "here" }
+  X x;
+  alignas(X) unsigned char buf[sizeof(X)];
+  X* p = (X*)buf;
+
+  std::uninitialized_fill_n(p, 1, x); // { dg-error "here" }
 }
-// { dg-error "constructible from value" "" { target *-*-* } 0 }
+// { dg-error "must be constructible" "" { target *-*-* } 0 }
