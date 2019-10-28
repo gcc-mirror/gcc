dc11b59dfb71ccdcd15b1366fdf51dda07367f34
pault <pault@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-10-28 18:28:48 +0000

    2019-10-28  Paul Thomas  <pault@gcc.gnu.org>
    	Backport from trunk
    	PR fortran/91926
    	* trans-expr.c (gfc_conv_gfc_desc_to_cfi_desc): Correct the
    	assignment of the attribute field to account correctly for an
    	assumed shape dummy. Assign separately to the gfc and cfi
    	descriptors since the atribute can be different. Add branch to
    	correctly handle missing optional dummies.
    
    2019-10-28  Paul Thomas  <pault@gcc.gnu.org>
    
    	Backport from trunk
    	PR fortran/91926
    	* gfortran.dg/ISO_Fortran_binding_13.f90 : New test.
    	* gfortran.dg/ISO_Fortran_binding_13.c : Additional source.
    	* gfortran.dg/ISO_Fortran_binding_14.f90 : New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-9-branch@277531 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/ISO_Fortran_binding_14.f90
@@ -0,0 +1,41 @@
+! { dg-do run }
+!
+! Correct an error in the eveluation of the CFI descriptor attribute for
+! the case where the bind_C formal argument is not an assumed shape array
+! and not allocatable or pointer.
+!
+! Contributed by Gilles Gouaillardet  <gilles@rist.or.jp>
+!
+MODULE FOO
+INTERFACE
+SUBROUTINE dummy(buf) BIND(C, name="sync")
+type(*), dimension(..) :: buf
+END SUBROUTINE
+END INTERFACE
+END MODULE
+
+PROGRAM main
+    USE FOO
+    IMPLICIT NONE
+    integer(8) :: before, after
+
+    INTEGER, parameter :: n = 1
+
+    INTEGER, ALLOCATABLE :: buf(:)
+    INTEGER :: buf2(n)
+    INTEGER :: i
+
+    ALLOCATE(buf(n))
+    before = LOC(buf(1))
+    CALL dummy (buf)
+    after = LOC(buf(1))
+
+    if (before .NE. after) stop 1
+
+    before = LOC(buf2(1))
+    CALL dummy (buf)
+    after = LOC(buf2(1))
+
+    if (before .NE. after) stop 2
+
+END PROGRAM
