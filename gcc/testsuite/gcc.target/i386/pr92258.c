38fb3ed7aaccf05e1d45c517970fd26480adf216
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-10-29 07:49:46 +0000

    PR target/92258 	* config/i386/sse.md (iptr): Revert 2019-10-27 change.
    	* gcc.target/i386/pr92258.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@277551 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/i386/pr92258.c
@@ -0,0 +1,11 @@
+/* PR target/92258 */
+/* { dg-do compile } */
+/* { dg-options "-masm=intel -msse2" } */
+
+typedef double V __attribute__ ((__vector_size__ (16)));
+
+int
+foo (V x, V y)
+{
+  return __builtin_ia32_ucomisdeq (x, y);
+}
