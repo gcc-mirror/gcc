35cd1262dcfb99006bb01b0f481487ccd6dfe437
Iain Sandoe <iain@sandoe.co.uk>
2019-10-29 20:01:59 +0000

    [Darwin] Update machopic_legitimize_pic_address.
    Some changes were missed here in the transition to LRA. The Darwin
    archs are all using LRA now, testing for reload is not correct.
    
    2019-10-18  Iain Sandoe  <iain@sandoe.co.uk>
    
    	Backport from mainline
    	2019-09-21  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* config/darwin.c (machopic_legitimize_pic_address): Check
    	for lra, rather than reload.
    
    From-SVN: r277583

--- gcc/config/darwin.c
+++ gcc/config/darwin.c
@@ -841,7 +841,7 @@ machopic_legitimize_pic_address (rtx orig, machine_mode mode, rtx reg)
 	{
 	  if (reg == 0)
 	    {
-	      gcc_assert (!reload_in_progress);
+	      gcc_assert (!lra_in_progress);
 	      reg = gen_reg_rtx (Pmode);
 	    }
 
@@ -925,7 +925,7 @@ machopic_legitimize_pic_address (rtx orig, machine_mode mode, rtx reg)
 	      emit_use (gen_rtx_REG (Pmode, PIC_OFFSET_TABLE_REGNUM));
 #endif
 
-	      if (reload_in_progress)
+	      if (lra_in_progress)
 		df_set_regs_ever_live (REGNO (pic), true);
 	      pic_ref = gen_rtx_PLUS (Pmode, pic,
 				      machopic_gen_offset (XEXP (orig, 0)));
@@ -949,7 +949,7 @@ machopic_legitimize_pic_address (rtx orig, machine_mode mode, rtx reg)
 
 	      if (reg == 0)
 		{
-		  gcc_assert (!reload_in_progress);
+		  gcc_assert (!lra_in_progress);
 		  reg = gen_reg_rtx (Pmode);
 		}
 
@@ -995,7 +995,7 @@ machopic_legitimize_pic_address (rtx orig, machine_mode mode, rtx reg)
 #if 0
 		  emit_use (pic_offset_table_rtx);
 #endif
-		  if (reload_in_progress)
+		  if (lra_in_progress)
 		    df_set_regs_ever_live (REGNO (pic), true);
 		  pic_ref = gen_rtx_PLUS (Pmode,
 					  pic,
