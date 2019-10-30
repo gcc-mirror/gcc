961daeb6e62b06d81e9e8552dfc5872c2ab6879c
iains <iains@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-10-30 20:48:44 +0000

    [Darwin, machopic] Back out part of PR71767 fix.
    This is part of making our codegen match what the system tools produce.
    
    We applied a conservative, but fairly large, hammer to fix 71767.
    However, ideally, we want minimise the number of symbols visible to
    ld64 and to match the cases emitted by clang (since that's what ld64
    is expecting). Now we've improved the handling of indirections, we
    can make the indirection symbols local when they are in the regular
    non-lazy symbol pointers section. We will continue to make any
    indirections in the data section visible (since right now we have no
    way to track if a given symbol follows a weak global).
    This change makes no difference to handling of labels for constants
    (to be revised in a future patch).
    
    There's a mechanical change to a number of tests (allowing 'l' or 'L'
    as the indirection symbol prefix).
    
    gcc/
    
    2019-10-30  Iain Sandoe  <iain@sandoe.co.uk>
    
    	Backport from mainline
    	2019-10-13  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* config/darwin.c (machopic_indirection_name): Rework the
    	function to emit linker-visible symbols only for indirections
    	in the data section.  Clean up the code and update comments.
    
    gcc/testsuite/
    
    2019-10-30  Iain Sandoe  <iain@sandoe.co.uk>
    
    	Backport from mainline.
    	2019-10-13  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* gcc.target/i386/indirect-thunk-1.c: Allow 'l' or 'L' in
    	indirection label prefix, for Darwin.
    	* gcc.target/i386/indirect-thunk-2.c: Likewise.
    	* gcc.target/i386/indirect-thunk-3.c: Likewise.
    	* gcc.target/i386/indirect-thunk-4.c: Likewise.
    	* gcc.target/i386/indirect-thunk-attr-1.c: Likewise.
    	* gcc.target/i386/indirect-thunk-attr-2.c: Likewise.
    	* gcc.target/i386/indirect-thunk-attr-3.c: Likewise.
    	* gcc.target/i386/indirect-thunk-attr-4.c: Likewise.
    	* gcc.target/i386/indirect-thunk-attr-5.c: Likewise.
    	* gcc.target/i386/indirect-thunk-attr-6.c: Likewise.
    	* gcc.target/i386/indirect-thunk-extern-1.c: Likewise.
    	* gcc.target/i386/indirect-thunk-extern-2.c: Likewise.
    	* gcc.target/i386/indirect-thunk-extern-3.c: Likewise.
    	* gcc.target/i386/indirect-thunk-extern-4.c: Likewise.
    	* gcc.target/i386/indirect-thunk-inline-1.c: Likewise.
    	* gcc.target/i386/indirect-thunk-inline-2.c: Likewise.
    	* gcc.target/i386/indirect-thunk-inline-3.c: Likewise.
    	* gcc.target/i386/indirect-thunk-inline-4.c: Likewise.
    	* gcc.target/i386/pr32219-2.c: Likewise.
    	* gcc.target/i386/pr32219-3.c: Likewise.
    	* gcc.target/i386/pr32219-4.c: Likewise.
    	* gcc.target/i386/pr32219-7.c: Likewise.
    	* gcc.target/i386/pr32219-8.c: Likewise.
    	* gcc.target/i386/ret-thunk-14.c: Likewise.
    	* gcc.target/i386/ret-thunk-15.c: Likewise.
    	* gcc.target/i386/ret-thunk-9.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-9-branch@277646 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/i386/ret-thunk-9.c
+++ gcc/testsuite/gcc.target/i386/ret-thunk-9.c
@@ -17,7 +17,7 @@ foo (void)
 /* { dg-final { scan-assembler "_?__x86_return_thunk:" } } */
 /* { dg-final { scan-assembler "mov(?:l|q)\[ \t\]*bar" { target *-*-linux* } } } */
 /* { dg-final { scan-assembler {movq[ \t]*_bar} { target { lp64 && *-*-darwin* } } } } */
-/* { dg-final { scan-assembler {movl[ \t]*l_bar\$non_lazy_ptr-L[0-9]+\$pb} { target { ia32 && *-*-darwin* } } } } */
+/* { dg-final { scan-assembler {movl[ \t]*[Ll]_bar\$non_lazy_ptr-L[0-9]+\$pb} { target { ia32 && *-*-darwin* } } } } */
 /* { dg-final { scan-assembler-times {\tpause} 2 } } */
 /* { dg-final { scan-assembler-times {\tlfence} 2 } } */
 /* { dg-final { scan-assembler "call\[ \t\]*_?__x86_indirect_thunk_(r|e)ax" } } */
