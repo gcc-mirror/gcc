0092b21f24a6778a54caac4015db1d0ad6291a45
Jozef Lawrynowicz <jozef.l@mittosystems.com>
2019-10-31 17:36:53 +0000

    devices-main.c: Move to devices subdirectory.
    2019-10-31  Jozef Lawrynowicz  <jozef.l@mittosystems.com>
    
    	* gcc.target/msp430/devices-main.c: Move to devices subdirectory.
    	* gcc.target/msp430/devices/bad-devices-1.c: Update #include path to
    	devices-main.c.
    	* gcc.target/msp430/devices/bad-devices-2.c: Likewise.
    	* gcc.target/msp430/devices/bad-devices-3.c: Likewise.
    	* gcc.target/msp430/devices/bad-devices-4.c: Likewise.
    	* gcc.target/msp430/devices/bad-devices-5.c: Likewise.
    	* gcc.target/msp430/devices/bad-devices-6.c: Likewise.
    	* gcc.target/msp430/devices/csv-device-order.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_00.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_01.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_02.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_04.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_08.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_10.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_11.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_12.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_14.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_18.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_20.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_21.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_22.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_24.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430_28.c: Likewise.
    	* gcc.target/msp430/devices/csv-msp430fr5969.c: Likewise.
    	* gcc.target/msp430/devices/hard-cc430f5123.c: Likewise.
    	* gcc.target/msp430/devices/hard-foo.c: Likewise.
    	* gcc.target/msp430/devices/hard-msp430afe253.c: Likewise.
    	* gcc.target/msp430/devices/hard-msp430cg4616.c: Likewise.
    	* gcc.target/msp430/devices/hard-msp430f4783.c: Likewise.
    	* gcc.target/msp430/devices/hard-rf430frl154h_rom.c: Likewise.
    
    From-SVN: r277684

--- gcc/testsuite/gcc.target/msp430/devices/bad-devices-5.c
+++ gcc/testsuite/gcc.target/msp430/devices/bad-devices-5.c
@@ -2,4 +2,4 @@
 /* { dg-additional-options "-mmcu=msp430_00 -mno-warn-mcu" } */
 /* { dg-warning "invalid 'CPU_TYPE' value of '5' read from 'devices.csv' for 'msp430_00'" "" { target *-*-* } 0 } */
 
-#include "../devices-main.c"
+#include "devices-main.c"
--- gcc/testsuite/gcc.target/msp430/devices/csv-msp430_28.c
+++ gcc/testsuite/gcc.target/msp430/devices/csv-msp430_28.c
@@ -3,4 +3,4 @@
 /* { dg-warning "supports 430X ISA but '-mcpu' option is set to 430" "" { target msp430_430_selected } 0 } */
 /* { dg-warning "supports 32-bit .5xx. hardware multiply" "" { target msp430_hwmul_not_f5 } 0 } */
 
-#include "../devices-main.c"
+#include "devices-main.c"
