b8dd4aa14d1eca6d0f4baa5213fd8853313eef0b
Thomas Koenig <tkoenig@gcc.gnu.org>
2019-11-03 22:33:53 +0000

    re PR fortran/92113 (r276673 causes segfault in gfortran.dg/pr51434.f90)
    2019-11-03  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
        PR fortran/92133
        * trans-decl.c (gfc_get_symbol_decl): If __def_init actually
        contains a value, put it into  the read-only section.
    
    From-SVN: r277760

--- gcc/fortran/trans-decl.c
+++ gcc/fortran/trans-decl.c
@@ -1904,13 +1904,20 @@ gfc_get_symbol_decl (gfc_symbol * sym)
   if (sym->attr.associate_var)
     GFC_DECL_ASSOCIATE_VAR_P (decl) = 1;
 
-  /* We no longer mark __def_init as read-only so it does not take up
-     space in the read-only section and dan go into the BSS instead,
-     see PR 84487.  Marking this as artificial means that OpenMP will
-     treat this as predetermined shared.  */
-  if (sym->attr.vtab
-      || (sym->name[0] == '_' && gfc_str_startswith (sym->name, "__def_init")))
-    DECL_ARTIFICIAL (decl) = 1;
+  /* We only longer mark __def_init as read-only if it actually has an
+     initializer, it does not needlessly take up space in the
+     read-only section and can go into the BSS instead, see PR 84487.
+     Marking this as artificial means that OpenMP will treat this as
+     predetermined shared.  */
+
+  bool def_init = gfc_str_startswith (sym->name, "__def_init");
+
+  if (sym->attr.vtab || def_init)
+    {
+      DECL_ARTIFICIAL (decl) = 1;
+      if (def_init && sym->value)
+	TREE_READONLY (decl) = 1;
+    }
 
   return decl;
 }
