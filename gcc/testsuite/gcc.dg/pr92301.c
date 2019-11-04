b76f4e6c06bd494d2383c4c16d1e1a034da74641
Richard Biener <rguenther@suse.de>
2019-11-04 10:08:22 +0000

    re PR bootstrap/92301 (Wrong vectorizer code since  r277566)
    2019-11-04  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/92301
    	* tree-vect-stmts.c (process_use): Force reduction PHI defs live
    	as required by epilogue generation
    
    	* gcc.dg/pr92301.c: New testcase.
    
    From-SVN: r277771

---
+++ gcc/testsuite/gcc.dg/pr92301.c
@@ -0,0 +1,35 @@
+/* { dg-do run } */
+/* { dg-options "-O3" } */
+
+unsigned int m;
+
+#define N 128
+unsigned int a[N];
+
+unsigned int
+__attribute__((noipa))
+df_count_refs (_Bool include_defs)
+{
+  int size = 0;
+
+  for (unsigned int regno = 0; regno < m; regno++)
+    if (include_defs)
+      size += a[regno];
+  return size;
+}
+
+int main(int argc, char **argv)
+{
+  for (unsigned i = 0; i < N; i++)
+    a[i] = i;
+
+  if (argc == 1)
+    m = 17;
+
+  unsigned int r = df_count_refs(1);
+  __builtin_printf ("r: %d\n", r);
+  if (r != 136)
+    __builtin_abort ();
+
+  return 0;
+}
