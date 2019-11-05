94555328ad9dd616eb728b265ede63e5889f1fed
msebor <msebor@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-11-05 16:20:44 +0000

    PR middle-end/92341 - missing -Warray-bounds indexing past the end of a compound literal PR middle-end/82612 - missing -Warray-bounds on a non-zero offset from the address of a non-array object
    gcc/testsuite/ChangeLog:
    
    	PR middle-end/92341
    	PR middle-end/82612
    	* g++.dg/warn/Warray-bounds-4.C: Adjust text of expected warning.
    	* gcc.dg/Warray-bounds-53.c: New test.
    	* gcc.dg/Warray-bounds-54.c: New test.
    
    gcc/ChangeLog:
    
    	PR middle-end/92341
    	PR middle-end/82612
    	* tree-sra.c (get_access_for_expr): Fail for out-of-bounds offsets.
    	* tree-vrp.c (vrp_prop::check_array_ref): Correct index and text
    	of message printed in a warning for empty arrays.
    	(vrp_prop::check_mem_ref): Also handle function parameters and
    	empty arrays.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@277851 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/Warray-bounds-54.c
@@ -0,0 +1,24 @@
+/* PR middle-end/82612 - missing -Warray-bounds on a non-zero offset
+   from the address of a non-array object
+   { dg-do compile }
+   { dg-options "-O2 -Wall" } */
+
+int i;
+int f0 (void)
+{
+  int *p = &i;
+  return p[2];      // { dg-warning "-Warray-bounds" }
+}
+
+int f1 (void)
+{
+  int i;
+  int *p = &i;
+  return p[2];      // { dg-warning "-Warray-bounds" }
+}
+
+int f2 (int i)
+{
+  int *p = &i;
+  return p[2];      // { dg-warning "-Warray-bounds" }
+}
