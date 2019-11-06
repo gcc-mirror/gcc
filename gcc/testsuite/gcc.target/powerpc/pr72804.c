a3f9c6f666e622f30eda441cd7ad8657a7a64bfe
luoxhu <luoxhu@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-11-06 03:36:46 +0000

    PR92090: Fix part of testcase failures by r276469
    -finline-functions is enabled by default for O2 since r276469, update the
    test cases with -fno-inline-functions.
    c11-atomic-exec-5.c stills hit ICE of LRA on BE systems in PR92090.
    This commit is NOT a fix for the bug and so it must NOT be closed.
    
    gcc/testsuite/ChangeLog:
    
    	2019-11-06  Xiong Hu Luo  <luoxhu@linux.ibm.com>
    
    	PR92090
    	* gcc.target/powerpc/pr72804.c: Add -fno-inline-functions --param
    	max-inline-insns-single-O2=200.
    	* gcc.target/powerpc/pr79439-1.c: Add -fno-inline-functions.
    	* gcc.target/powerpc/vsx-builtin-7.c: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@277872 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/powerpc/pr72804.c
+++ gcc/testsuite/gcc.target/powerpc/pr72804.c
@@ -1,6 +1,6 @@
 /* { dg-do compile { target { lp64 } } } */
 /* { dg-skip-if "" { powerpc*-*-darwin* } } */
-/* { dg-require-effective-target powerpc_vsx_ok } */
+/* { dg-require-effective-target powerpc_vsx_ok  -fno-inline-functions --param max-inline-insns-single-O2=200 } */
 /* { dg-options "-O2 -mvsx" } */
 
 __int128_t
