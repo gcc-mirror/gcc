87496ad88e0fa227fcdb2cd88e3e501f96aaf2f2
Iain Sandoe <iain@sandoe.co.uk>
2019-11-07 21:17:31 +0000

    [Darwin, X86, testsuite] Fix pr92258.c.
    This test uses -masm=intel, which isn't supported by Darwin.  Add the
    necessary dg-require-effective-target.
    
    gcc/testsuite/ChangeLog:
    
    2019-11-07  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* gcc.target/i386/pr92258.c: Add dg-requires for masm_intel.
    
    From-SVN: r277930

--- gcc/testsuite/gcc.target/i386/pr92258.c
+++ gcc/testsuite/gcc.target/i386/pr92258.c
@@ -1,5 +1,6 @@
 /* PR target/92258 */
 /* { dg-do compile } */
+/* { dg-require-effective-target masm_intel } */
 /* { dg-options "-masm=intel -msse2" } */
 
 typedef double V __attribute__ ((__vector_size__ (16)));
