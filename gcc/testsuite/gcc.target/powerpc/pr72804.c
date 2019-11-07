ce6c0a20b5875c18a8416d60950febea76a4b9d3
Xiong Hu Luo <luoxhu@gcc.gnu.org>
2019-11-07 01:24:03 +0000

    Fix copy-paste typo syntax error by r277872
    gcc/testsuite/ChangeLog:
    
    	2019-11-07  Xiong Hu Luo  <luoxhu@linux.ibm.com>
    
    	* gcc.target/powerpc/pr72804.c: Move inline options from
    	dg-require-effective-target to dg-options.
    
    From-SVN: r277904

--- gcc/testsuite/gcc.target/powerpc/pr72804.c
+++ gcc/testsuite/gcc.target/powerpc/pr72804.c
@@ -1,7 +1,7 @@
 /* { dg-do compile { target { lp64 } } } */
 /* { dg-skip-if "" { powerpc*-*-darwin* } } */
-/* { dg-require-effective-target powerpc_vsx_ok  -fno-inline-functions --param max-inline-insns-single-O2=200 } */
-/* { dg-options "-O2 -mvsx" } */
+/* { dg-require-effective-target powerpc_vsx_ok } */
+/* { dg-options "-O2 -mvsx -fno-inline-functions --param max-inline-insns-single-O2=200" } */
 
 __int128_t
 foo (__int128_t *src)
