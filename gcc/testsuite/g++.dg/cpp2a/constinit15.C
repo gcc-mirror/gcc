7031eb30850025d0978fa2004a07d4ee939e11e0
mpolacek <mpolacek@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-11-08 21:40:45 +0000

    PR c++/92058 - constinit malfunction in static data member. 	* g++.dg/cpp2a/constinit15.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@277990 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/cpp2a/constinit15.C
@@ -0,0 +1,14 @@
+// PR c++/92058 - constinit malfunction in static data member.
+// { dg-do compile { target c++2a } }
+
+struct B {
+    B() {}
+};
+
+struct A {
+    constinit static inline B b1{}; // { dg-error "does not have a constant initializer|call to non-.constexpr. function" }
+};
+
+int main() {
+    A a;
+}
