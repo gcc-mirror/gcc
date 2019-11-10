2806ecbdc8054ef9f3968577806b007baefad4e6
Jason Merrill <jason@redhat.com>
2019-11-10 15:30:03 -0500

    Implement D1957R0, T* to bool should be considered narrowing.
    This paper was delayed until the February meeting in Prague so that we could
    get a better idea of what the impact on existing code would actually be.  To
    that end, I'm implementing it now.
    
    	* typeck2.c (check_narrowing): Treat pointer->bool as a narrowing
    	conversion with -std=c++2a.
    
    From-SVN: r278026

--- gcc/cp/typeck2.c
+++ gcc/cp/typeck2.c
@@ -1018,6 +1018,11 @@ check_narrowing (tree type, tree init, tsubst_flags_t complain,
 	    ok = true;
 	}
     }
+  else if (TREE_CODE (type) == BOOLEAN_TYPE
+	   && (TYPE_PTR_P (ftype) || TYPE_PTRMEM_P (ftype)))
+    /* This hasn't actually made it into C++20 yet, but let's add it now to get
+       an idea of the impact.  */
+    ok = (cxx_dialect < cxx2a);
 
   bool almost_ok = ok;
   if (!ok && !CONSTANT_CLASS_P (init) && (complain & tf_warning_or_error))
