7c01b4f72c9423da4280e7f6a525ae5f7ad2fde1
ams <ams@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-11-13 12:37:54 +0000

    GCN mkoffload
    2019-11-13  Andrew Stubbs  <ams@codesourcery.com>
    	    Kwok Cheung Yeung  <kcy@codesourcery.com>
    	    Julian Brown  <julian@codesourcery.com>
    	    Tom de Vries  <tom@codesourcery.com>
    
    	gcc/
    	* config/gcn/mkoffload.c: New file.
    	* config/gcn/offload.h: New file.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@278133 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/config/gcn/offload.h
@@ -0,0 +1,35 @@
+/* Support for AMD GCN offloading.
+
+   Copyright (C) 2014-2019 Free Software Foundation, Inc.
+
+   This file is part of GCC.
+
+   GCC is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+
+   GCC is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   Under Section 7 of GPL version 3, you are granted additional
+   permissions described in the GCC Runtime Library Exception, version
+   3.1, as published by the Free Software Foundation.
+
+   You should have received a copy of the GNU General Public License and
+   a copy of the GCC Runtime Library Exception along with this program;
+   see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#ifndef GCC_GCN_OFFLOAD_H
+#define GCC_GCN_OFFLOAD_H
+
+/* Support for OpenACC acc_on_device.  */
+
+#include "gomp-constants.h"
+
+#define ACCEL_COMPILER_acc_device GOMP_DEVICE_GCN
+
+#endif
