3b41e0d0a729f816241f5dfd6b4e2db3aca82a46
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-11-14 15:33:49 +0000

    Consider building nodes from scalars in vect_slp_analyze_node_operations
    If the statements in an SLP node aren't similar enough to be vectorised,
    or aren't something the vectoriser has code to handle, the BB vectoriser
    tries building the vector from scalars instead.  This patch does the
    same thing if we're able to build a viable-looking tree but fail later
    during the analysis phase, e.g. because the target doesn't support a
    particular vector operation.
    
    This is needed to avoid regressions with a later patch.
    
    2019-11-14  Richard Sandiford  <richard.sandiford@arm.com>
    
    gcc/
    	* tree-vect-slp.c (vect_contains_pattern_stmt_p): New function.
    	(vect_slp_convert_to_external): Likewise.
    	(vect_slp_analyze_node_operations): If analysis fails, try building
    	the node from scalars instead.
    
    gcc/testsuite/
    	* gcc.dg/vect/bb-slp-div-2.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@278246 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/bb-slp-div-2.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+
+int x[4], y[4], z[4];
+
+void
+f (void)
+{
+  x[0] += y[0] / z[0] * 2;
+  x[1] += y[1] / z[1] * 2;
+  x[2] += y[2] / z[2] * 2;
+  x[3] += y[3] / z[3] * 2;
+}
+
+/* { dg-final { scan-tree-dump "basic block vectorized" "slp2" { target vect_int } } } */
