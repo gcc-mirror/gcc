9338c3b0ccade91fcf0ec852d9f11b38aee96129
marxin <marxin@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-11-14 08:35:35 +0000

    Handle FIELD_DECL in IPA ICF.
    2019-11-14  Martin Liska  <mliska@suse.cz>
    
    	* ipa-icf-gimple.c (func_checker::hash_operand): Improve
    	func_checker::hash_operand by handling of FIELD_DECLs.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@278208 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ipa-icf-gimple.c
+++ gcc/ipa-icf-gimple.c
@@ -234,7 +234,10 @@ func_checker::hash_operand (const_tree arg, inchash::hash &hstate,
     case CONST_DECL:
     case SSA_NAME:
       return;
-
+    case FIELD_DECL:
+      inchash::add_expr (DECL_FIELD_OFFSET (arg), hstate, flags);
+      inchash::add_expr (DECL_FIELD_BIT_OFFSET (arg), hstate, flags);
+      return;
     default:
       break;
     }
