55f925d015878202f271983d9759b59fb6e251cf
emsr <emsr@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-11-16 03:16:35 +0000

    2019-11-15  Edward Smith-Rowland  <3dw4rd@verizon.net>
    	Implement the <tuple> part of C++20 p1032 Misc constexpr bits.
    	* include/std/tuple (_Head_base, _Tuple_impl(allocator_arg_t,...)
    	(_M_assign, tuple(allocator_arg_t,...), _Inherited, operator=, _M_swap)
    	(swap, pair(piecewise_construct_t,): Constexpr.
    	* (__uses_alloc0::_Sink::operator=, __uses_alloc_t): Constexpr.
    	* testsuite/20_util/tuple/cons/constexpr_allocator_arg_t.cc: New test.
    	* testsuite/20_util/tuple/constexpr_swap.cc : New test.
    	* testsuite/20_util/uses_allocator/69293_neg.cc: Extra error for C++20.
    	* testsuite/20_util/uses_allocator/cons_neg.cc: : Extra error for C++20.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@278331 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/20_util/tuple/constexpr_swap.cc
+++ libstdc++-v3/testsuite/20_util/tuple/constexpr_swap.cc
@@ -1,47 +1,36 @@
-// { dg-do compile { target c++11 } }
-
-// Copyright (C) 2011-2019 Free Software Foundation, Inc.
+// { dg-options "-std=gnu++2a" }
+// { dg-do compile { target c++2a } }
+//
+// Copyright (C) 2019 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the
 // Free Software Foundation; either version 3, or (at your option)
 // any later version.
-
+//
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-
+//
 // You should have received a copy of the GNU General Public License along
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// 20.4.2.1 [tuple.cnstr] Allocator-extended constructors
-
-#include <memory>
 #include <tuple>
 
-struct MyAlloc { };
-
-struct Type
+constexpr bool
+test_tuple()
 {
-  typedef MyAlloc allocator_type; // uses_allocator<Type, MyAlloc> is true
+  auto ok = true;
 
-  explicit Type(int) { }
+  std::tuple<int, double, double> t1(1, 3.1415, 2.7182);
+  std::tuple<int, double, double> t2(1, 4.1234, 3.3333);
+  t1.swap(t2);
+  std::swap(t1, t2);
 
-  Type(std::allocator_arg_t, MyAlloc) { }
-  Type(MyAlloc) { }
-};
-
-void test01()
-{
-  using std::allocator_arg;
-  using std::tuple;
-
-  MyAlloc a;
-
-  tuple<Type> t(allocator_arg, a, 1);
+  return ok;
 }
-// { dg-error "failed: .* uses_allocator is true" "" { target *-*-* } 0 }
-// { dg-error "no matching function for call" "" { target c++2a } 0 }
+
+static_assert(test_tuple());
