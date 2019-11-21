1d22a845a482203c5b0469b9eb0cc923e4c5f1d8
Jakub Jelinek <jakub@redhat.com>
2019-11-21 18:08:49 +0100

    backport: re PR c/91401 (schedule + dist_schedule clauses rejected on distribute parallel for)
    	Backported from mainline
    	2019-08-09  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c/91401
    	* c-parser.c (c_parser_omp_clause_dist_schedule): Fix up typos in the
    	check_no_duplicate_clause call.  Comment it out, instead emit a
    	warning for duplicate dist_schedule clauses.
    
    	* parser.c (cp_parser_omp_clause_dist_schedule): Comment out the
    	check_no_duplicate_clause call, instead emit a warning for duplicate
    	dist_schedule clauses.
    
    	* c-c++-common/gomp/pr91401-1.c: New test.
    	* c-c++-common/gomp/pr91401-2.c: New test.
    
    From-SVN: r278573

---
+++ gcc/testsuite/c-c++-common/gomp/pr91401-2.c
@@ -0,0 +1,15 @@
+#pragma omp declare target
+void f0 (void);
+
+void
+f1 (void)
+{
+  int i;
+  #pragma omp distribute dist_schedule(static) dist_schedule(static)	/* { dg-warning "too many 'dist_schedule' clauses" } */
+  for (i = 0; i < 8; ++i)
+    f0 ();
+  #pragma omp distribute dist_schedule(static,2) dist_schedule(static,4) /* { dg-warning "too many 'dist_schedule' clauses" } */
+  for (i = 0; i < 8; ++i)
+    f0 ();
+}
+#pragma omp end declare target
