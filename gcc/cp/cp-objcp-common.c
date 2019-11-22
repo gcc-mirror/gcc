bdaf8be1b2e3514b8bbcdd1042136e7d91b3c3f3
Jakub Jelinek <jakub@redhat.com>
2019-11-22 22:45:27 +0100

    re PR c/90677 (gcc-9.1.0 fails to build __gcc_diag__ souce: error: 'cgraph_node' is not defined as a type)
    	PR c/90677
    	* c-common.h (identifier_global_tag): Declare.
    	* c-format.c (get_pointer_to_named_type): Renamed to ...
    	(get_named_type): ... this.  Use identifier_global_tag instead of
    	identifier_global_value, handle the return value being a TYPE_P.
    	(init_dynamic_diag_info): Adjust get_pointer_to_named_type callers
    	to call get_named_type instead.  Formatting fixes.
    c/
    	* c-decl.c (identifier_global_tag): Define.
    cp/
    	* cp-objcp-common.c (identifier_global_tag): Define.
    testsuite/
    	* c-c++-common/pr90677.c: New test.
    
    From-SVN: r278634

--- gcc/cp/cp-objcp-common.c
+++ gcc/cp/cp-objcp-common.c
@@ -349,6 +349,15 @@ identifier_global_value (tree name)
   return get_global_binding (name);
 }
 
+/* Similarly, but return struct/class/union NAME instead.  */
+
+tree
+identifier_global_tag (tree name)
+{
+  return lookup_qualified_name (global_namespace, name, /*prefer_type*/2,
+				/*complain*/false);
+}
+
 /* Returns true if NAME refers to a built-in function or function-like
    operator.  */
 
