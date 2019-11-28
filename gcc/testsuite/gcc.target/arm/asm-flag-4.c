f88ed7de657124226ee4d59253200763963ca360
clyon <clyon@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-11-28 09:16:55 +0000

    [testsuite][arm] Force use of -mfloat-abi=softfp in asm-flag-4.c
    The asm-flag-4.c test forces the generation of Thumb code for armv4t,
    which is not supported when using the 'hard' FP ABI. This patch
    ensures the test uses -mfloat-abi=softfp safely, to avoid irrelevant
    failures.
    
    2019-11-28  Christophe Lyon  <christophe.lyon@linaro.org>
    
    	* gcc.target/arm/asm-flag-4.c: Use -mfloat-abi=softfp.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@278804 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/arm/asm-flag-4.c
+++ gcc/testsuite/gcc.target/arm/asm-flag-4.c
@@ -1,6 +1,8 @@
 /* Test that we do not ice in thumb1 mode */
 /* { dg-do compile } */
-/* { dg-options "-march=armv4t" } */
+/* { dg-require-effective-target arm_arch_v4t_thumb_ok } */
+/* { dg-skip-if "do not override -mfloat-abi" { *-*-* } { "-mfloat-abi=*" } { "-mfloat-abi=softfp" } } */
+/* { dg-options "-march=armv4t -mfloat-abi=softfp" } */
 
 void __attribute__((target("arm"))) f(char *out)
 {
