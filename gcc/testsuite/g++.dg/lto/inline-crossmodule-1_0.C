f87c23751ab207ee44c2956a444c2c80a797d1fe
Jan Hubicka <hubicka@ucw.cz>
2019-12-02 12:11:34 +0100

    Fix dg failure
    	* g++.dg/lto/inline-crossmodule-1_0.C: fix template.
    
    From-SVN: r278897

--- gcc/testsuite/g++.dg/lto/inline-crossmodule-1_0.C
+++ gcc/testsuite/g++.dg/lto/inline-crossmodule-1_0.C
@@ -5,7 +5,7 @@ int a::key ()
 {
   return 0;
 }
-/* { dg-final { scan-wpa-ipa-times "Inlined ret1" 1 "inlined"  } } */
-/* { dg-final { scan-wpa-ipa-times "Inlined ret2" 1 "inlined"  } } */
-/* { dg-final { scan-wpa-ipa-times "Inlined key\[^\\n\]*(cross module)" 1 "inlined"  } } */
-/* { dg-final { scan-wpa-ipa-times "(cross module)" 1 "inlined"  } } */
+/* { dg-final { scan-wpa-ipa-dump-times "Inlined ret1" 1 "inlined"  } } */
+/* { dg-final { scan-wpa-ipa-dump-times "Inlined ret2" 1 "inlined"  } } */
+/* { dg-final { scan-wpa-ipa-dump-times "Inlined key\[^\\n\]*(cross module)" 1 "inlined"  } } */
+/* { dg-final { scan-wpa-ipa-dump-times "(cross module)" 1 "inlined"  } } */
