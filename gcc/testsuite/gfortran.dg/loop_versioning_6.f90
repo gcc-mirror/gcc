7c5c29384cc747e4092f19331cb32800fb8b544a
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-12-03 10:11:58 +0000

    XFAIL loop_versioning_6.f90 for ! lp64
    2019-12-03  Richard Sandiford  <richard.sandiford@arm.com>
    
    gcc/testsuite/
    	* gfortran.dg/loop_versioning_6.f90: XFAIL the scans for ! lp64.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@278928 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/loop_versioning_6.f90
+++ gcc/testsuite/gfortran.dg/loop_versioning_6.f90
@@ -89,7 +89,5 @@ subroutine f9(x, limit, step)
   end do
 end subroutine f9
 
-! { dg-final { scan-tree-dump-times {want to version containing loop} 9 "lversion" { target lp64 } } }
-! { dg-final { scan-tree-dump-times {versioned this loop} 9 "lversion" { target lp64 } } }
-! { dg-final { scan-tree-dump-times {want to version containing loop} 8 "lversion" { target { ! lp64 } } } }
-! { dg-final { scan-tree-dump-times {versioned this loop} 8 "lversion" { target { ! lp64 } } } }
+! { dg-final { scan-tree-dump-times {want to version containing loop} 9 "lversion" { xfail { ! lp64 } } } }
+! { dg-final { scan-tree-dump-times {versioned this loop} 9 "lversion" { xfail { ! lp64 } } } }
