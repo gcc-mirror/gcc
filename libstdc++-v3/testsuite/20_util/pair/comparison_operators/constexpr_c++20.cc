52f510dea48f3775917fb1ec817e42044a2c18ad
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-12-03 23:57:46 +0000

    libstdc++: Implement spaceship for std::pair (P1614R2)
    This defines operator<=> as a non-member function template and does not
    alter operator==. This contradicts the changes made by P1614R2, which
    specify both as hidden friends, but that specification of operator<=> is
    broken and the subject of a soon-to-be-published LWG issue.
    
    	* include/bits/stl_pair.h [__cpp_lib_three_way_comparison]
    	(operator<=>): Define for C++20.
    	* libsupc++/compare (__cmp2way_res_t): Rename to __cmp3way_res_t,
    	move into __detail namespace. Do not turn argument types into lvalues.
    	(__cmp3way_helper): Rename to __cmp3way_res_impl, move into __detail
    	namespace. Constrain with concepts instead of using void_t.
    	(compare_three_way_result): Adjust name of base class.
    	(compare_three_way_result_t): Use __cmp3way_res_impl directly.
    	(__detail::__3way_cmp_with): Add workaround for PR 91073.
    	(compare_three_way): Use workaround.
    	(__detail::__synth3way, __detail::__synth3way_t): Define new helpers
    	implementing synth-three-way and synth-three-way-result semantics.
    	* testsuite/20_util/pair/comparison_operators/constexpr_c++20.cc: New
    	test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@278951 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libstdc++-v3/testsuite/20_util/pair/comparison_operators/constexpr_c++20.cc
@@ -0,0 +1,31 @@
+// { dg-options "-std=gnu++2a" }
+// { dg-do compile { target c++2a } }
+
+// Copyright (C) 2019 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <utility>
+#include <testsuite_common_types.h>
+
+int main()
+{
+  __gnu_test::constexpr_comparison_operators test;
+  test.operator()<std::pair<int, int>>();
+
+  constexpr std::pair<int, int> p{1, 2}, q{3, 0};
+  static_assert( p <=> q == std::strong_ordering::less );
+}
