851e611cac2751e356ff56531a098f2fd18a052a
Iain Sandoe <iain@sandoe.co.uk>
2019-12-08 20:58:50 +0000

    c++-coroutines - Update operator overload tests.
    Make the simple case participate so that it is checked.
    Add a case where we have two possible overloads.
    
    2019-12-08  Iain Sandoe  <iain@sandoe.co.uk>
    
    	gcc/testsuite/
    	* g++.dg/coroutines/torture/co-await-12-operator-2.C: New test.
    	* g++.dg/coroutines/torture/co-await-6-ovl.C: Make the operator
    	result participate in the output.
    
    From-SVN: r279099

--- gcc/testsuite/g++.dg/coroutines/torture/co-await-12-operator-2.C
+++ gcc/testsuite/g++.dg/coroutines/torture/co-await-12-operator-2.C
@@ -10,31 +10,29 @@
 /* A very simple overload.  */
 struct empty 
 {
-  auto operator co_await() const & noexcept { 
+  auto operator co_await() & noexcept { 
     return coro1::suspend_always_intprt{};
+  }
+  auto operator co_await() && noexcept { 
+    return coro1::suspend_always_longprtsq(3L);
   }
 };
 
-int gX = 1;
 empty e{};
 
 coro1
 f ()
 {
-  int a = co_await(e); /* operator ovl. */
-  co_return gX + 5 + a;
+  int a = co_await(e); /* operator ovl lv. */
+  int b = co_await(empty{}); /* operator ovl rv. */
+  co_return b + a;
 }
 
 int main ()
 {
   PRINT ("main: create coro1");
   struct coro1 f_coro = f ();
-  PRINT ("main: got coro1 - checking gX");
-  if (gX != 1)
-    {
-      PRINTF ("main: gX is wrong : %d, should be 1\n", gX);
-      abort ();
-    }
+
   if (f_coro.handle.done())
     {
       PRINT ("main: we should not be 'done'");
@@ -44,10 +42,13 @@ int main ()
   PRINT ("main: resuming [1] initial suspend");
   f_coro.handle.resume();
 
-  PRINT ("main: resuming [2] co_await");
+  PRINT ("main: resuming [2] co_await a");
   f_coro.handle.resume();
 
-  /* we should now have returned with the co_return (11) */
+  PRINT ("main: resuming [3] co_await b");
+  f_coro.handle.resume();
+
+  /* we should now have returned with the co_return (14) */
   if (!f_coro.handle.done())
     {
       PRINT ("main: we should be 'done' ");
@@ -55,9 +56,9 @@ int main ()
     }
 
   int y = f_coro.handle.promise().get_value();
-  if (y != 11)
+  if (y != 14)
     {
-      PRINTF ("main: y is wrong : %d, should be 11\n", y);
+      PRINTF ("main: y is wrong : %d, should be 14\n", y);
       abort ();
     }
   puts ("main: done");
