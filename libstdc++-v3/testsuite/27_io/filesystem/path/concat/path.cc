01af17ba2b00819b409eba4058ffd3927bc0e655
redi <redi@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-12-09 11:16:29 +0000

    libstdc++: fix buffer overflow in path::operator+= (PR92853)
    When concatenating a path ending in a root-directory onto another path,
    we added an empty filename to the end of the path twice, but only
    reserved space for one. That meant the second write went past the end of
    the allocated buffer.
    
    	PR libstdc++/92853
    	* src/c++17/fs_path.cc (filesystem::path::operator+=(const path&)):
    	Do not process a trailing directory separator twice.
    	* testsuite/27_io/filesystem/path/concat/92853.cc: New test.
    	* testsuite/27_io/filesystem/path/concat/path.cc: Test more cases.
    	* testsuite/27_io/filesystem/path/concat/strings.cc: Test more cases.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-9-branch@279115 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/27_io/filesystem/path/concat/path.cc
+++ libstdc++-v3/testsuite/27_io/filesystem/path/concat/path.cc
@@ -55,6 +55,8 @@ test02()
     path x("//blah/di/blah");
     p += x;
     VERIFY( p.native() == prior_native + x.native() );
+    path copy(p);
+    compare_paths( copy, p );
   }
 }
 
@@ -66,10 +68,28 @@ test03()
   compare_paths(p, "a//b");
 }
 
+void
+test04()
+{
+  // Concat every test path onto every test path.
+  for (path p : __gnu_test::test_paths)
+  {
+    for (path x : __gnu_test::test_paths)
+    {
+      auto prior_native = p.native();
+      p += x;
+      VERIFY( p.native() == prior_native + x.native() );
+      path copy(p); // PR libstdc++/98523
+      compare_paths( copy, p );
+    }
+  }
+}
+
 int
 main()
 {
   test01();
   test02();
   test03();
+  test04();
 }
