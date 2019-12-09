a74d1c85921f0828075a6bf35e94df411d110673
tschwinge <tschwinge@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-12-09 22:52:56 +0000

    [PR92116, PR92877] [OpenACC] Replace 'openacc.data_environ' by standard libgomp mechanics
    	libgomp/
    	PR libgomp/92116
    	PR libgomp/92877
    	* oacc-mem.c (lookup_dev): Reimplement.  Adjust all users.
    	* libgomp.h (struct acc_dispatch_t): Remove 'data_environ' member.
    	Adjust all users.
    	* testsuite/libgomp.oacc-c-c++-common/acc_free-pr92503-4-2.c:
    	Remove XFAIL.
    	* testsuite/libgomp.oacc-c-c++-common/acc_free-pr92503-4.c:
    	Likewise.
    	* testsuite/libgomp.oacc-c-c++-common/pr92877-1.c: New file.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@279147 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgomp/testsuite/libgomp.oacc-c-c++-common/acc_free-pr92503-4-2.c
+++ libgomp/testsuite/libgomp.oacc-c-c++-common/acc_free-pr92503-4-2.c
@@ -25,7 +25,5 @@ main ()
 }
 
 /* { dg-output "CheCKpOInT(\n|\r\n|\r).*" }
-   TODO PR92877
-   { dg-output "libgomp: cuMemGetAddressRange_v2 error: named symbol not found" { target openacc_nvidia_accel_selected } }
-   { dg-output "refusing to free device memory space at \[0-9a-fA-FxX\]+ that is still mapped at \\\[\[0-9a-fA-FxX\]+,\\\+64\\\]" { xfail *-*-* } }
+   { dg-output "refusing to free device memory space at \[0-9a-fA-FxX\]+ that is still mapped at \\\[\[0-9a-fA-FxX\]+,\\\+64\\\]" }
    { dg-shouldfail "" } */
