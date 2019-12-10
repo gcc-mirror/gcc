0cc063af32311944d66b6537aeade08038cabba9
Thomas Koenig <tkoenig@gcc.gnu.org>
2019-12-10 21:59:09 +0000

    re PR fortran/91643 (ICE in gfc_trans_create_temp_array, at fortran/trans-array.c:1265)
    2019-12-10  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
        PR fortran/91643
        * trans-array.c (gfc_conv_array_parameter): Do not repack
        an assumed rank dummy argument.
    
    2019-12-10  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
        PR fortran/91643
        * gfortran.dg/assumed_rank_18.f90: New test.
    
    From-SVN: r279203

--- gcc/fortran/trans-array.c
+++ gcc/fortran/trans-array.c
@@ -8141,6 +8141,8 @@ gfc_conv_array_parameter (gfc_se * se, gfc_expr * expr, bool g77,
 
       if (g77 && optimize && !optimize_size && expr->expr_type == EXPR_VARIABLE
 	  && !is_pointer (expr) && ! gfc_has_dimen_vector_ref (expr)
+	  && !(expr->symtree->n.sym->as
+	       && expr->symtree->n.sym->as->type == AS_ASSUMED_RANK)
 	  && (fsym == NULL || fsym->ts.type != BT_ASSUMED))
 	{
 	  gfc_conv_subref_array_arg (se, expr, g77,
