60961fcea72b2acdf0e9b0cd9f47e599f764fd89
frederik <frederik@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-12-11 08:26:18 +0000

    Fix PR92901: Change test expectation for C++ in OpenACC test clause-locations.c
    The columns of the clause locations that are reported for C and C++ are
    different and hence we need separate test expectations for both languages.
    
    2019-12-11  Frederik Harwath  <frederik@codesourcery.com>
    
    	PR other/92901
    	/gcc/testsuite/
    	* c-c++-common/clause-locations.c: Adjust test expectation for C++.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@279215 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/c-c++-common/goacc/clause-locations.c
+++ gcc/testsuite/c-c++-common/goacc/clause-locations.c
@@ -9,7 +9,9 @@ check_clause_columns() {
     #pragma acc loop reduction(+:sum)
     for (i = 1; i <= 10; i++)
       {
-        #pragma acc loop reduction(-:diff) reduction(-:sum)  /* { dg-warning "53: conflicting reduction operations for .sum." } */
+        #pragma acc loop reduction(-:diff) reduction(-:sum)
+	/* { dg-warning "53: conflicting reduction operations for .sum." "" { target c } .-1 } */
+	/* { dg-warning "56: conflicting reduction operations for .sum." "" { target c++ } .-2 } */
 	for (j = 1; j <= 10; j++)
 	  sum = 1;
       }
