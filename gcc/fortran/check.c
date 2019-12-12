14e94b008115079f2adf3e0a302fd0d17ea73ab4
anlauf <anlauf@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-12-12 20:39:48 +0000

    2019-12-12  Harald Anlauf  <anlauf@gmx.de>
    	PR fortran/92898
    	* check.c (gfc_check_is_contiguous): Simplify check to handle
    	arbitrary NULL() argument.
    
    	PR fortran/92898
    	* gfortran.dg/pr92898.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@279314 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/check.c
+++ gcc/fortran/check.c
@@ -7153,8 +7153,7 @@ gfc_check_ttynam_sub (gfc_expr *unit, gfc_expr *name)
 bool
 gfc_check_is_contiguous (gfc_expr *array)
 {
-  if (array->expr_type == EXPR_NULL
-      && array->symtree->n.sym->attr.pointer == 1)
+  if (array->expr_type == EXPR_NULL)
     {
       gfc_error ("Actual argument at %L of %qs intrinsic shall be an "
 		 "associated pointer", &array->where, gfc_current_intrinsic);
