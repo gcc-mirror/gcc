3ee501a7d4911e0a142e660af036c459857d6f2c
jamborm <jamborm@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-12-18 16:08:09 +0000

    IPA-CP: Remove bogus static keyword (PR 92971)
    2019-12-18  Martin Jambor  <mjambor@suse.cz>
    
    	PR ipa/92971
    	* ipa-cp.c (cgraph_edge_brings_all_agg_vals_for_node): Fix
              definition of values, release memory on exit.
    
    	testsuite/
    	* gcc.dg/ipa/ipcp-agg-12.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@279525 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ipa-cp.c
+++ gcc/ipa-cp.c
@@ -5117,7 +5117,6 @@ cgraph_edge_brings_all_agg_vals_for_node (struct cgraph_edge *cs,
 
   for (i = 0; i < count; i++)
     {
-      static vec<ipa_agg_value> values = vNULL;
       class ipcp_param_lattices *plats;
       bool interesting = false;
       for (struct ipa_agg_replacement_value *av = aggval; av; av = av->next)
@@ -5133,7 +5132,7 @@ cgraph_edge_brings_all_agg_vals_for_node (struct cgraph_edge *cs,
       if (plats->aggs_bottom)
 	return false;
 
-      values = intersect_aggregates_with_edge (cs, i, values);
+      vec<ipa_agg_value> values = intersect_aggregates_with_edge (cs, i, vNULL);
       if (!values.exists ())
 	return false;
 
@@ -5157,6 +5156,7 @@ cgraph_edge_brings_all_agg_vals_for_node (struct cgraph_edge *cs,
 		return false;
 	      }
 	  }
+      values.release ();
     }
   return true;
 }
