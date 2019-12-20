0dd1f694b7ecaa97db780ce5dd5cb75ee2817626
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-12-20 11:35:20 +0000

    Fortran] PR 92996 – fix rank resolution EXPR_ARRAY
            PR fortran/92996
            gcc/fortran/
            * expr.c (simplify_parameter_variable): Call gfc_resolve_ref and
            gfc_expression_rank; fix location info.
            * gfortran.h (gfc_resolve_ref, gfc_expression_rank): Declare.
            * match.c (gfc_match_stopcode): Remove redundant setting of
            gfc_init_expr_flag; early return if gfc_simplify_expr has an error.
            * resolve.c (gfc_expression_rank): Renamed from expression_rank;
            minor cleanup.
            (gfc_resolve_ref): Removed static and renamed from resolve_ref.
            (resolve_variable, resolve_typebound_function,
            resolve_typebound_subroutine, resolve_ppc_call, resolve_expr_ppc,
            gfc_resolve_expr, resolve_procedure): Update calls.
    
            PR fortran/92996
            gcc/testsuite/
            * gfortran.dg/array_simplify_4.f90: New.
            * gfortran.dg/pr91565.f90: Update dg-error.
            * gfortran.dg/pr91801.f90: Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@279638 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/pr91565.f90
+++ gcc/testsuite/gfortran.dg/pr91565.f90
@@ -2,16 +2,16 @@
 ! PR fortran/91565
 ! Contributed by Gerhard Steinmetz
 program p
-   integer, parameter :: a(2) = [2,2]              ! { dg-error "\(1\)" }
-   print *, reshape([1,2,3,4,5,6], [2,3], order=a) ! { dg-error "not a permutation" }
+   integer, parameter :: a(2) = [2,2]
+   print *, reshape([1,2,3,4,5,6], [2,3], order=a) ! { dg-error "ORDER at .1. is not a permutation of the size of SHAPE at .2." }
 end
 
 subroutine foo
-   integer, parameter :: a(1) = 1                  ! { dg-error "\(1\)" }
+   integer, parameter :: a(1) = 1
    print *, reshape([1,2,3,4,5,6], [2,3], order=a) ! { dg-error "are different" }
 end
 
 subroutine bar
-   integer, parameter :: a(1,2) = 1                ! { dg-error "\(1\)" }
+   integer, parameter :: a(1,2) = 1
    print *, reshape([1,2,3,4,5,6], [2,3], order=a) ! { dg-error "are different" }
 end
