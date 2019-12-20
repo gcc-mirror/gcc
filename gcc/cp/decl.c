45f3739d14aa215d6572827c21d00ce92c4b2bee
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2019-12-20 17:30:38 +0000

    Backported from mainline 	2019-11-29  Jakub Jelinek  <jakub@redhat.com>
    	PR c++/60228
    	* parser.c (cp_parser_omp_declare_reduction_exprs): If
    	processing_template_decl, wrap the combiner or initializer
    	into EXPR_STMT.
    	* decl.c (start_preparsed_function): Don't start a lambda scope
    	for DECL_OMP_DECLARE_REDUCTION_P functions.
    	(finish_function): Don't finish a lambda scope for
    	DECL_OMP_DECLARE_REDUCTION_P functions, nor cp_fold_function
    	them nor cp_genericize them.
    	* mangle.c (decl_mangling_context): Look through
    	DECL_OMP_DECLARE_REDUCTION_P functions.
    	* semantics.c (expand_or_defer_fn_1): For DECL_OMP_DECLARE_REDUCTION_P
    	functions, use tentative linkage, don't keep their bodies with
    	-fkeep-inline-functions and return false at the end.
    
    	* g++.dg/gomp/openmp-simd-2.C: Don't expect bodies for
    	DECL_OMP_DECLARE_REDUCTION_P functions.
    
    	* testsuite/libgomp.c++/udr-20.C: New test.
    	* testsuite/libgomp.c++/udr-21.C: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-9-branch@279662 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/decl.c
+++ gcc/cp/decl.c
@@ -15632,7 +15632,8 @@ start_preparsed_function (tree decl1, tree attrs, int flags)
       && !implicit_default_ctor_p (decl1))
     cp_ubsan_maybe_initialize_vtbl_ptrs (current_class_ptr);
 
-  start_lambda_scope (decl1);
+  if (!DECL_OMP_DECLARE_REDUCTION_P (decl1))
+    start_lambda_scope (decl1);
 
   return true;
 }
@@ -16040,7 +16041,8 @@ finish_function (bool inline_p)
   if (fndecl == NULL_TREE)
     return error_mark_node;
 
-  finish_lambda_scope ();
+  if (!DECL_OMP_DECLARE_REDUCTION_P (fndecl))
+    finish_lambda_scope ();
 
   if (c_dialect_objc ())
     objc_finish_function ();
@@ -16157,7 +16159,7 @@ finish_function (bool inline_p)
     invoke_plugin_callbacks (PLUGIN_PRE_GENERICIZE, fndecl);
 
   /* Perform delayed folding before NRV transformation.  */
-  if (!processing_template_decl)
+  if (!processing_template_decl && !DECL_OMP_DECLARE_REDUCTION_P (fndecl))
     cp_fold_function (fndecl);
 
   /* Set up the named return value optimization, if we can.  Candidate
@@ -16280,7 +16282,8 @@ finish_function (bool inline_p)
   if (!processing_template_decl)
     {
       struct language_function *f = DECL_SAVED_FUNCTION_DATA (fndecl);
-      cp_genericize (fndecl);
+      if (!DECL_OMP_DECLARE_REDUCTION_P (fndecl))
+	cp_genericize (fndecl);
       /* Clear out the bits we don't need.  */
       f->x_current_class_ptr = NULL;
       f->x_current_class_ref = NULL;
