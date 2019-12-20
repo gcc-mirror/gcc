d9c7be4ce3b0590ee1c143e29635c084e2704c62
Jakub Jelinek <jakub@redhat.com>
2019-12-20 18:32:23 +0100

    backport: re PR c++/92695 (P1064R0 - virtual constexpr fails if object taken from array)
    	Backported from mainline
    	2019-12-02  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/92695
    	* constexpr.c (cxx_eval_constant_expression) <case OBJ_TYPE_REF>: Use
    	STRIP_NOPS before checking for ADDR_EXPR.
    
    	* g++.dg/cpp2a/constexpr-virtual15.C: New test.
    
    From-SVN: r279663

---
+++ gcc/testsuite/g++.dg/cpp2a/constexpr-virtual15.C
@@ -0,0 +1,7 @@
+// PR c++/92695
+// { dg-do compile { target c++2a } }
+
+struct A { virtual int get() = 0; };
+struct B : A { constexpr int get() override { return 10; } };
+struct D { B b[2]; A* c{&(b[0])}; };
+static_assert(D{}.c->get() == 10);
