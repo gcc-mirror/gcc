66b01cc3421055806f51ac36a8d237536970504c
Andrew Stubbs <ams@codesourcery.com>
2020-01-07 14:35:21 +0000

    Disallow 'B' constraints on amdgcn addc/subb
    2020-01-07  Andrew Stubbs  <ams@codesourcery.com>
    
    	gcc/
    	* config/gcn/constraints.md (DA): Update description and match.
    	(DB): Likewise.
    	(Db): New constraint.
    	* config/gcn/gcn-protos.h (gcn_inline_constant64_p): Add second
    	parameter.
    	* config/gcn/gcn.c (gcn_inline_constant64_p): Add 'mixed' parameter.
    	Implement 'Db' mixed immediate type.
    	* config/gcn/gcn-valu.md (addcv64si3<exec_vcc>): Rework constraints.
    	(addcv64si3_dup<exec_vcc>): Delete.
    	(subcv64si3<exec_vcc>): Rework constraints.
    	(addv64di3): Rework constraints.
    	(addv64di3_exec): Rework constraints.
    	(subv64di3): Rework constraints.
    	(addv64di3_dup): Delete.
    	(addv64di3_dup_exec): Delete.
    	(addv64di3_zext): Rework constraints.
    	(addv64di3_zext_exec): Rework constraints.
    	(addv64di3_zext_dup): Rework constraints.
    	(addv64di3_zext_dup_exec): Rework constraints.
    	(addv64di3_zext_dup2): Rework constraints.
    	(addv64di3_zext_dup2_exec): Rework constraints.
    	(addv64di3_sext_dup2): Rework constraints.
    	(addv64di3_sext_dup2_exec): Rework constraints.
    
    From-SVN: r279959

--- gcc/config/gcn/gcn-protos.h
+++ gcc/config/gcn/gcn-protos.h
@@ -51,7 +51,7 @@ extern int gcn_hard_regno_nregs (int regno, machine_mode mode);
 extern void gcn_hsa_declare_function_name (FILE *file, const char *name,
 					   tree decl);
 extern HOST_WIDE_INT gcn_initial_elimination_offset (int, int);
-extern bool gcn_inline_constant64_p (rtx);
+extern bool gcn_inline_constant64_p (rtx, bool);
 extern bool gcn_inline_constant_p (rtx);
 extern int gcn_inline_fp_constant_p (rtx, bool);
 extern reg_class gcn_mode_code_base_reg_class (machine_mode, addr_space_t,
