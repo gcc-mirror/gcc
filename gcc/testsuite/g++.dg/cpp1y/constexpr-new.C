f74f6092ace420bd8a4498697754b06fc6da63bd
Jakub Jelinek <jakub@redhat.com>
2020-01-07 08:13:50 +0100

    re PR c++/91369 (Implement P0784R7: constexpr new)
    	PR c++/91369
    	* constexpr.c (struct constexpr_global_ctx): Add heap_alloc_count
    	member, initialize it to zero in ctor.
    	(cxx_eval_call_expression): Bump heap_dealloc_count when deleting
    	a heap object.  Don't cache calls to functions which allocate some
    	heap objects and don't deallocate them or deallocate some heap
    	objects they didn't allocate.
    
    	* g++.dg/cpp1y/constexpr-new.C: Expect an error explaining why
    	static_assert failed for C++2a.
    	* g++.dg/cpp2a/constexpr-new9.C: New test.
    
    From-SVN: r279943

--- gcc/testsuite/g++.dg/cpp1y/constexpr-new.C
+++ gcc/testsuite/g++.dg/cpp1y/constexpr-new.C
@@ -5,7 +5,7 @@ constexpr int *f4(bool b) {
     return nullptr;
   } else {
     return new int{42}; // { dg-error "call to non-.constexpr." "" { target c++17_down } }
-  }
+  }			// { dg-error "is not a constant expression because allocated storage has not been deallocated" "" { target c++2a } .-1 }
 }
 static_assert(f4(true) == nullptr, "");
 static_assert(f4(false) == nullptr, ""); // { dg-error "non-.constant. condition|" }
