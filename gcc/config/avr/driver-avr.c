e3da34115b54ee6d47c7458a82dd5367b928ee29
Georg-Johann Lay <avr@gjlay.de>
2020-01-08 09:46:07 +0000

    backport: Add -nodevicespecs option for avr.
    	Backport from 2020-01-08 trunk r279995.
    
    	Add -nodevicespecs option for avr.
    
    	PR target/93182
    	* config/avr/avr.opt (-nodevicespecs): New driver option.
    	* config/avr/driver-avr.c (avr_devicespecs_file): Only issue
    	"-specs=device-specs/..." if that option is not set.
    	* doc/invoke.texi (AVR Options) <-nodevicespecs>: Document.
    
    From-SVN: r279996

--- gcc/config/avr/driver-avr.c
+++ gcc/config/avr/driver-avr.c
@@ -26,8 +26,8 @@ along with GCC; see the file COPYING3.  If not see
 #include "diagnostic.h"
 #include "tm.h"
 
-// Remove -nodevicelib from the command line if not needed
-#define X_NODEVLIB "%<nodevicelib"
+// Remove -nodevicelib and -nodevicespecs from the command line if not needed.
+#define X_NODEVLIB "%<nodevicelib %<nodevicespecs"
 
 static const char dir_separator_str[] = { DIR_SEPARATOR, 0 };
 
@@ -102,8 +102,8 @@ avr_devicespecs_file (int argc, const char **argv)
         return X_NODEVLIB;
       }
 
-  return concat ("-specs=device-specs", dir_separator_str, "specs-",
-                 mmcu, "%s"
+  return concat ("%{!nodevicespecs:-specs=device-specs", dir_separator_str,
+		 "specs-", mmcu, "%s} %<nodevicespecs"
 #if defined (WITH_AVRLIBC)
                  " %{mmcu=avr*:" X_NODEVLIB "} %{!mmcu=*:" X_NODEVLIB "}",
 #else
