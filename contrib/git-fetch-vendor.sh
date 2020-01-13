b8f59c4b30ac1761e6f4b63666ca7f5d68649ed5
Richard Earnshaw <rearnsha@arm.com>
2020-01-13 16:14:32 +0000

    contrib: script to setup git to pull a vendors branches
    This simple script is intended to setup a new git configuration to
    pull the branches and tags for a specific vendor.  This should
    simplify some of the steps needed for working with a vendor's
    branches.
    
    	* git-fetch-vendor.sh: New file.

---
+++ contrib/git-fetch-vendor.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ $# != 1 ]
+then
+    echo "Usage: $0 <vendor>"
+    exit 1
+fi
+
+vendor=$1
+upstream=`git config --get "gcc-config.upstream"`
+if [ x"$upstream" = x ]
+then
+    echo "Config gcc-config.upstream not set, run contrib/gcc-git-customization"
+    exit 1
+fi
+
+echo "setting up git to fetch vendor ${vendor} to remotes/${upstream}/${vendor}"
+
+git config --replace-all "remote.${upstream}.fetch" "+refs/vendors/${vendor}/heads/*:refs/remotes/${upstream}/${vendor}/*" ":refs/remotes/${upstream}/${vendor}/"
+git config --replace-all "remote.${upstream}.fetch" "+refs/vendors/${vendor}/tags/*:refs/tags/${vendor}/*" ":refs/tags/${vendor}/"
+git config --replace-all "remote.${upstream}.push" "+refs/heads/${vendor}/*:refs/vendors/${vendor}/heads/*" "^\+refs/heads/${vendor}/"
+git fetch
