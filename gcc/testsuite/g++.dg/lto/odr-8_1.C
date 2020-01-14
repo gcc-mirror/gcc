000c7a93bdf4040d7d0672fbb9b064eae3d78f5d
Jakub Jelinek <jakub@redhat.com>
2020-01-15 00:35:58 +0100

    Remove bogus initial lines from test.
    2020-01-15  Jakub Jelinek  <jakub@redhat.com>
    
    	PR lto/91576
    	* g++.dg/lto/odr-8_1.C: Remove bogus initial lines.

--- gcc/testsuite/g++.dg/lto/odr-8_1.C
+++ gcc/testsuite/g++.dg/lto/odr-8_1.C
@@ -1,6 +1,3 @@
---- a/gcc/testsuite/g++.dg/lto/odr-8_1.C
-+++ b/gcc/testsuite/g++.dg/lto/odr-8_1.C
-@@ -1,9 +1,9 @@
 struct a {char c; a() {} a(struct a &) {}}; // { dg-lto-message "one type needs to be constructed while other not" }
 extern int test (struct a *a);
 int
