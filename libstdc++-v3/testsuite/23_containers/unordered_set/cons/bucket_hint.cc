eb56f5cc01e9ca5484b80553d4ea0eaf03665c8f
François Dumont <fdumont@gcc.gnu.org>
2020-01-20 19:01:18 +0100

    libstdc++: Do not over-size hashtable buckets on range insertion
    We used to consider range size on insertion but on unique keys container
    not all range values might be inserted resulting in over-sizing. In this
    case we just consider user reservation and if none then the container will
    adapt to actually inserted elements.
    
    libstdc++-v3/ChangeLog:
    
    	* include/bits/hashtable.h
    	(_Hashtable<>(_InputIterator, _InputIterator, size_t, const _H1&,
    	const _H2&, const _Hash&, const _Equal&, const _ExtractKey&,
    	const allocator_type&, true_type)): New.
    	(_Hashtable<>(_InputIterator, _InputIterator, size_t, const _H1&,
    	const _H2&, const _Hash&, const _Equal&, const _ExtractKey&,
    	const allocator_type&, false_type)): New.
    	(_Hashtable<>(_InputIterator, _InputIterator, size_t, const _H1&,
    	const _H2&, const _Hash&, const _Equal&, const _ExtractKey&,
    	const allocator_type&)): Delegate to latters.
    	(operator=(initializer_list<value_type>)): Rehash if too small.
    	(_M_insert(_Arg&&, const _NodeGenerator&, true_type)): Remove
    	size_t len parameter.
    	* include/bits/hashtable_policy.h (_Insert_base<>::_M_insert_range):
    	Do not try to get input range distance.
    	* testsuite/23_containers/unordered_set/cons/bucket_hint.cc: New test.
    	* testsuite/23_containers/unordered_set/modifiers/insert.cc: New test.

---
+++ libstdc++-v3/testsuite/23_containers/unordered_set/cons/bucket_hint.cc
@@ -0,0 +1,63 @@
+// { dg-do run { target c++11 } }
+
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <vector>
+#include <forward_list>
+#include <unordered_set>
+
+#include <testsuite_hooks.h>
+
+void test01()
+{
+  std::unordered_set<int> a;
+  a.reserve(2);
+
+  std::unordered_set<int> b({ 0, 1, 0, 1, 0, 1, 0, 1 }, a.bucket_count());
+  VERIFY( b.bucket_count() == a.bucket_count() );
+}
+
+void test02()
+{
+  std::unordered_set<int> a;
+  a.reserve(2);
+
+  std::vector<int> v { 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 };
+
+  std::unordered_set<int> b(v.begin(), v.end(), a.bucket_count());
+  VERIFY( b.bucket_count() == a.bucket_count() );
+}
+
+void test03()
+{
+  std::unordered_set<int> a;
+  a.reserve(2);
+
+  std::forward_list<int> fl { 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 };
+
+  std::unordered_set<int> b(fl.begin(), fl.end(), a.bucket_count());
+  VERIFY( b.bucket_count() == a.bucket_count() );
+}
+
+int main()
+{
+  test01();
+  test02();
+  test03();
+  return 0;
+}
