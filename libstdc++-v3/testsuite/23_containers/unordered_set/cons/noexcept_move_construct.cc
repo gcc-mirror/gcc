5f63261c3e54995b45dd411ad76526870c4b8be3
François Dumont <fdumont@gcc.gnu.org>
2020-01-20 19:15:43 +0100

    libstdc++: Fix unordered containers move constructors noexcept qualification
    _Hashtable move constructor is wrongly qualified as noexcept(true) regardless of
    _Equal and _H1 copy constructor qualifications.
    _Hashtable allocator-aware move constructor is missing its noexcept
    qualification like the depending unordered containers ones.
    
    This backport also includes the changes from r11-8062 and r11-2438.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/96029
    	* include/bits/hashtable.h
    	(_Hashtable(_Hashtable&& __ht, __node_alloc_type&& __a, true_type)):
    	Add noexcept qualification.
    	(_Hashtable(_Hashtable&&)): Fix noexcept qualification.
    	(_Hashtable(_Hashtable&&, const allocator_type&)): Add noexcept
    	qualification.
    	* include/bits/unordered_map.h
    	(unordered_map(unordered_map&&, const allocator_type&)): Add noexcept
    	qualification.
    	(unordered_multimap(unordered_multimap&&, const allocator_type&)):
    	Likewise.
    	* include/bits/unordered_set.h
    	(unordered_set(unordered_set&&, const allocator_type&)): Likewise.
    	(unordered_multiset(unordered_multiset&&, const allocator_type&)):
    	Likewise.
    	* include/debug/unordered_map
    	(unordered_map(unordered_map&&, const allocator_type&)): Likewise.
    	(unordered_multimap(unordered_multimap&&, const allocator_type&)):
    	Likewise.
    	* include/debug/unordered_set
    	(unordered_set(unordered_set&&, const allocator_type&)): Likewise.
    	(unordered_multiset(unordered_multiset&&, const allocator_type&)):
    	Likewise.
    	* testsuite/23_containers/unordered_map/allocator/default_init.cc:
    	New test.
    	* testsuite/23_containers/unordered_map/cons/noexcept_default_construct.cc:
    	New test.
    	* testsuite/23_containers/unordered_map/cons/noexcept_move_construct.cc:
    	New test.
    	* testsuite/23_containers/unordered_map/modifiers/move_assign.cc:
    	New test.
    	* testsuite/23_containers/unordered_multimap/cons/noexcept_default_construct.cc:
    	New test.
    	* testsuite/23_containers/unordered_multimap/cons/noexcept_move_construct.cc:
    	New test.
    	* testsuite/23_containers/unordered_multiset/cons/noexcept_default_construct.cc:
    	New test.
    	* testsuite/23_containers/unordered_multiset/cons/noexcept_move_construct.cc:
    	New test.
    	* testsuite/23_containers/unordered_set/allocator/default_init.cc:
    	New test.
    	* testsuite/23_containers/unordered_set/cons/noexcept_default_construct.cc:
    	New test.
    	* testsuite/23_containers/unordered_set/cons/noexcept_move_construct.cc:
    	New test.
    
    (cherry picked from commit 12324b9a934654a5c3bf4a614853ded2e0a958af)

---
+++ libstdc++-v3/testsuite/23_containers/unordered_set/cons/noexcept_move_construct.cc
@@ -0,0 +1,65 @@
+// { dg-do compile { target c++11 } }
+
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <unordered_set>
+
+using type1 = std::unordered_set<int>;
+
+static_assert( std::is_nothrow_move_constructible<type1>::value,
+	       "noexcept move constructor" );
+static_assert( std::is_nothrow_constructible<type1,
+	       type1&&, const typename type1::allocator_type&>::value,
+	       "noexcept move constructor with allocator" );
+
+struct not_noexcept_copy_cons_hash
+{
+  not_noexcept_copy_cons_hash() noexcept;
+  not_noexcept_copy_cons_hash(const not_noexcept_copy_cons_hash&) /* noexcept */;
+  not_noexcept_copy_cons_hash(not_noexcept_copy_cons_hash&&) noexcept;
+
+  std::size_t
+  operator()(int) const noexcept;
+};
+
+using type2 = std::unordered_set<int, not_noexcept_copy_cons_hash>;
+
+static_assert( !std::is_nothrow_move_constructible<type2>::value,
+	       "not noexcept move constructor" );
+static_assert( !std::is_nothrow_constructible<type2, type2&&,
+	       const typename type2::allocator_type&>::value,
+	       "not noexcept move constructor with allocator" );
+
+struct not_noexcept_copy_cons_equal_to
+{
+  not_noexcept_copy_cons_equal_to() noexcept;
+  not_noexcept_copy_cons_equal_to(const not_noexcept_copy_cons_equal_to&) /* noexcept */;
+  not_noexcept_copy_cons_equal_to(not_noexcept_copy_cons_equal_to&&) noexcept;
+
+  bool
+  operator()(int, int) const noexcept;
+};
+
+using type3 = std::unordered_set<int, std::hash<int>,
+				  not_noexcept_copy_cons_equal_to>;
+
+static_assert( !std::is_nothrow_move_constructible<type3>::value,
+	       "not noexcept move constructor" );
+static_assert( !std::is_nothrow_constructible<type3, type3&&,
+	       const typename type3::allocator_type&>::value,
+	       "not noexcept move constructor with allocator" );
