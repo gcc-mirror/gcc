9c4fb56578e0a66ec222e58bb89c1e8bc742ef5e
Richard Sandiford <richard.sandiford@arm.com>
2020-01-21 16:52:49 +0000

    Skip gcc.target/aarch64/sve/tls_preserve* for emultls targets
    These tests are supposed to be testing the tlsdesc handling and
    so don't apply to emultls targets.
    
    2020-01-22  Richard Sandiford  <richard.sandiford@arm.com>
    
    gcc/testsuite/
    	* gcc.target/aarch64/sve/tls_preserve_1.c: Require tls_native.
    	* gcc.target/aarch64/sve/tls_preserve_2.c: Likewise.
    	* gcc.target/aarch64/sve/tls_preserve_3.c: Likewise.

--- gcc/testsuite/gcc.target/aarch64/sve/tls_preserve_1.c
+++ gcc/testsuite/gcc.target/aarch64/sve/tls_preserve_1.c
@@ -1,8 +1,7 @@
 /* { dg-do compile } */
 /* { dg-options "-O3 -fpic" } */
 /* { dg-require-effective-target fpic } */
-
-/* Clobber highs do not need to be spilled around tls usage.  */
+/* { dg-require-effective-target tls_native } */
 
 typedef float v4si __attribute__ ((vector_size (16)));
 
