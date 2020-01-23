7175893ad8010df1270af884a1810476e51a8cd9
Jason Merrill <jason@redhat.com>
2020-01-23 15:45:36 -0500

    c++: Unshare expressions from constexpr cache.
    Another place we need to unshare cached expressions.
    
    	PR c++/92852 - ICE with generic lambda and reference var.
    	* constexpr.c (maybe_constant_value): Likewise.

--- gcc/cp/constexpr.c
+++ gcc/cp/constexpr.c
@@ -5107,7 +5107,7 @@ maybe_constant_value (tree t, tree decl)
   if (cv_cache == NULL)
     cv_cache = hash_map<tree, tree>::create_ggc (101);
   if (tree *cached = cv_cache->get (t))
-    return *cached;
+    return unshare_expr_without_location (*cached);
 
   r = cxx_eval_outermost_constant_expr (t, true, true, decl);
   gcc_checking_assert (r == t
