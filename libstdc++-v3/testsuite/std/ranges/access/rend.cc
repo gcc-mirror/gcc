5cd2e126f5c5705fa1aa7fafef3d6b94a99593da
Jonathan Wakely <jwakely@redhat.com>
2020-01-29 13:36:15 +0000

    libstdc++: Make tests for std::ranges access functions more robust
    	* testsuite/std/ranges/access/end.cc: Do not assume test_range::end()
    	returns the same type as test_range::begin(). Add comments.
    	* testsuite/std/ranges/access/rbegin.cc: Likewise.
    	* testsuite/std/ranges/access/rend.cc: Likewise.
    	* testsuite/std/ranges/range.cc: Do not assume the sentinel for
    	test_range is the same as its iterator type.
    	* testsuite/util/testsuite_iterators.h (test_range::sentinel): Add
    	operator- overloads to satisfy sized_sentinel_for when the iterator
    	satisfies random_access_iterator.

--- libstdc++-v3/testsuite/std/ranges/access/rend.cc
+++ libstdc++-v3/testsuite/std/ranges/access/rend.cc
@@ -40,11 +40,41 @@ void
 test01()
 {
   constexpr R1 r;
+
+  // decay-copy(t.rend()) if it is a valid expression
+  // and its type S models sentinel_for<decltype(ranges::rbegin(E))>
+
   static_assert( std::ranges::rend(r) == &r.i + 1 );
   static_assert( std::ranges::rend(std::move(r)) == &r.i + 1 );
 }
 
 struct R2
+{
+  int i = 0;
+
+  int* rbegin() noexcept { return &i + 1; }
+  long* rend() noexcept { return nullptr; } // not a sentinel for rbegin()
+
+  friend long* rbegin(R2&) noexcept { return nullptr; }
+  friend int* rend(R2& r) { return &r.i; }
+};
+
+void
+test02()
+{
+  R2 r;
+
+  // Otherwise, decay-copy(rend(t)) if it is a valid expression
+  // and its type S models sentinel_for<decltype(ranges::rbegin(E))>
+
+  auto i1 = std::ranges::rbegin(r);
+  auto i2 = rend(r);
+  static_assert( std::sentinel_for<decltype(i2), decltype(i1)> );
+  VERIFY( std::ranges::rend(r) == &r.i );
+  static_assert( !noexcept(std::ranges::rend(r)) );
+}
+
+struct R3
 {
   int a[2] = { };
   long l[2] = { };
@@ -52,53 +82,49 @@ struct R2
   constexpr const int* begin() const { return a; }
   constexpr const int* end() const { return a + 2; }
 
-  friend constexpr const long* begin(const R2&& r) { return r.l; }
-  friend constexpr const long* end(const R2&& r) { return r.l + 2; }
+  friend constexpr const long* begin(const R3&& r) { return r.l; }
+  friend constexpr const long* end(const R3&& r) { return r.l + 2; }
 };
 
-// N.B. this is a lie, begin/end on an R2 rvalue will return a dangling pointer.
-template<> constexpr bool std::ranges::enable_safe_range<R2> = true;
+// N.B. this is a lie, begin/end on an R3 rvalue will return a dangling pointer.
+template<> constexpr bool std::ranges::enable_safe_range<R3> = true;
 
 void
-test02()
+test03()
 {
-  constexpr R2 r;
+  constexpr R3 r;
+
+  // Otherwise, make_reverse_iterator(ranges::begin(t)) if both
+  // ranges::begin(t) and ranges::end(t) are valid expressions
+  // of the same type I which models bidirectional_iterator.
+
   static_assert( std::ranges::rend(r)
       == std::make_reverse_iterator(std::ranges::begin(r)) );
   static_assert( std::ranges::rend(std::move(r))
       == std::make_reverse_iterator(std::ranges::begin(std::move(r))) );
 }
 
-struct R3
-{
-  int i = 0;
-
-  int* rbegin() noexcept { return &i + 1; }
-  long* rend() noexcept { return nullptr; } // not a sentinel for rbegin()
-
-  friend long* rbegin(R3&) noexcept { return nullptr; }
-  friend int* rend(R3& r) { return &r.i; }
-};
-
-void
-test03()
-{
-  R3 r;
-  auto i1 = std::ranges::rbegin(r);
-  auto i2 = rend(r);
-  static_assert( std::sentinel_for<decltype(i2), decltype(i1)> );
-  // VERIFY( std::ranges::rend(r) == r.i );
-  // static_assert( !noexcept(std::ranges::rend(r)) );
-}
-
 void
 test04()
 {
-  using __gnu_test::test_range;
-  using __gnu_test::bidirectional_iterator_wrapper;
+  struct R4
+  : __gnu_test::test_range<int, __gnu_test::bidirectional_iterator_wrapper>
+  {
+    R4(int (&a)[2]) : test_range(a) { }
+
+    using test_range::begin;
+
+    // Replace test_range::end() to return same type as begin()
+    // so ranges::rend will wrap it in a reverse_iterator.
+    auto end() &
+    {
+      using __gnu_test::bidirectional_iterator_wrapper;
+      return bidirectional_iterator_wrapper<int>(bounds.last, &bounds);
+    }
+  };
 
   int a[2] = { };
-  test_range<int, bidirectional_iterator_wrapper> r(a);
+  R4 r(a);
   VERIFY( std::ranges::rend(r) == std::make_reverse_iterator(std::ranges::begin(r)) );
 }
 
@@ -108,4 +134,5 @@ main()
   test01();
   test02();
   test03();
+  test04();
 }
