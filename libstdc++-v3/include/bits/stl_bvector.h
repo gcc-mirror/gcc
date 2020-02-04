72a54e5e8157508c87594d1a6b53de90b76524ce
François Dumont <fdumont@gcc.gnu.org>
2020-02-04 19:07:06 +0100

    libstdc++ std::fill overload for std::vector<bool>::iterator
      Extend the overload so that it is used even when _GLIBCXX_DEBUG mode
    is activated.
    
    	* include/bits/stl_algobase.h (struct _Bit_iterator): New declaration.
    	(std::__fill_a1(_Bit_iterator, _Bit_iterator, const bool&)): Likewise.
    	* include/bits/stl_bvector.h (__fill_bvector): Move outside
    	_GLIBCXX_STD_C namespace.
    	(fill(_Bit_iterator, _Bit_iterator, const bool&)): Likewise and rename
    	into...
    	(__fill_a1): ...this.
    	* testsuite/25_algorithms/fill/bvector/1.cc: New.

--- libstdc++-v3/include/bits/stl_bvector.h
+++ libstdc++-v3/include/bits/stl_bvector.h
@@ -416,39 +416,6 @@ _GLIBCXX_BEGIN_NAMESPACE_CONTAINER
     { return __x + __n; }
   };
 
-  inline void
-  __fill_bvector(_Bit_type * __v,
-		 unsigned int __first, unsigned int __last, bool __x)
-  {
-    const _Bit_type __fmask = ~0ul << __first;
-    const _Bit_type __lmask = ~0ul >> (_S_word_bit - __last);
-    const _Bit_type __mask = __fmask & __lmask;
-
-    if (__x)
-      *__v |= __mask;
-    else
-      *__v &= ~__mask;
-  }
-
-  inline void
-  fill(_Bit_iterator __first, _Bit_iterator __last, const bool& __x)
-  {
-    if (__first._M_p != __last._M_p)
-      {
-	_Bit_type* __first_p = __first._M_p;
-	if (__first._M_offset != 0)
-	  __fill_bvector(__first_p++, __first._M_offset, _S_word_bit, __x);
-
-	__builtin_memset(__first_p, __x ? ~0 : 0,
-			 (__last._M_p - __first_p) * sizeof(_Bit_type));
-
-	if (__last._M_offset != 0)
-	  __fill_bvector(__last._M_p, 0, __last._M_offset, __x);
-      }
-    else if (__first._M_offset != __last._M_offset)
-      __fill_bvector(__first._M_p, __first._M_offset, __last._M_offset, __x);
-  }
-
   template<typename _Alloc>
     struct _Bvector_base
     {
@@ -1336,15 +1303,46 @@ _GLIBCXX_BEGIN_NAMESPACE_CONTAINER
   };
 
 _GLIBCXX_END_NAMESPACE_CONTAINER
-_GLIBCXX_END_NAMESPACE_VERSION
-} // namespace std
+
+  inline void
+  __fill_bvector(_GLIBCXX_STD_C::_Bit_type * __v,
+		 unsigned int __first, unsigned int __last, bool __x)
+  {
+    using _GLIBCXX_STD_C::_Bit_type;
+    using _GLIBCXX_STD_C::_S_word_bit;
+    const _Bit_type __fmask = ~0ul << __first;
+    const _Bit_type __lmask = ~0ul >> (_S_word_bit - __last);
+    const _Bit_type __mask = __fmask & __lmask;
+
+    if (__x)
+      *__v |= __mask;
+    else
+      *__v &= ~__mask;
+  }
+
+  inline void
+  __fill_a1(_GLIBCXX_STD_C::_Bit_iterator __first,
+	    _GLIBCXX_STD_C::_Bit_iterator __last, const bool& __x)
+  {
+    using _GLIBCXX_STD_C::_Bit_type;
+    using _GLIBCXX_STD_C::_S_word_bit;
+    if (__first._M_p != __last._M_p)
+      {
+	_Bit_type* __first_p = __first._M_p;
+	if (__first._M_offset != 0)
+	  __fill_bvector(__first_p++, __first._M_offset, _S_word_bit, __x);
+
+	__builtin_memset(__first_p, __x ? ~0 : 0,
+			 (__last._M_p - __first_p) * sizeof(_Bit_type));
+
+	if (__last._M_offset != 0)
+	  __fill_bvector(__last._M_p, 0, __last._M_offset, __x);
+      }
+    else if (__first._M_offset != __last._M_offset)
+      __fill_bvector(__first._M_p, __first._M_offset, __last._M_offset, __x);
+  }
 
 #if __cplusplus >= 201103L
-
-namespace std _GLIBCXX_VISIBILITY(default)
-{
-_GLIBCXX_BEGIN_NAMESPACE_VERSION
-
   // DR 1182.
   /// std::hash specialization for vector<bool>.
   template<typename _Alloc>
@@ -1354,10 +1352,9 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       size_t
       operator()(const _GLIBCXX_STD_C::vector<bool, _Alloc>&) const noexcept;
     };
+#endif // C++11
 
 _GLIBCXX_END_NAMESPACE_VERSION
-}// namespace std
-
-#endif // C++11
+} // namespace std
 
 #endif
