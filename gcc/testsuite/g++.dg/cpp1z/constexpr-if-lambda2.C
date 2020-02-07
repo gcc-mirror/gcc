8496e135b111424e3c84f3f44c74f2cbe48eed90
Jason Merrill <jason@redhat.com>
2020-02-07 15:09:14 -0500

    c++: Make PR92654 testcase valid.

--- gcc/testsuite/g++.dg/cpp1z/constexpr-if-lambda2.C
+++ gcc/testsuite/g++.dg/cpp1z/constexpr-if-lambda2.C
@@ -1,12 +1,17 @@
 // PR c++/92654
 // { dg-do compile { target c++17 } }
 
-template <unsigned long> struct C;
+struct A {
+  constexpr operator int () { return 42; }
+};
+template <int I> struct C {
+  static const bool ap = I;
+};
 template <auto I>
 void am() {
   [](auto an)
      {
-       if constexpr (C<an>::ap) ; // { dg-error "constant" }
-     }(42);
+       if constexpr (C<an>::ap) ;
+     }(A{});
 }
 void fn() { am<42>(); }
