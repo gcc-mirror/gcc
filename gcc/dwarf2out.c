794bb8c2f5d328d9d4cfa55efd41cdd420ebb13a
Richard Biener <rguenther@suse.de>
2020-02-14 09:17:57 +0100

    debug/92763 keep DIEs that might be used in DW_TAG_inlined_subroutine
    We were pruning type-local subroutine DIEs if their context is unused
    despite us later needing those DIEs as abstract origins for inlines.
    The patch makes code already present for -fvar-tracking-assignments
    unconditional.
    
    2020-02-14  Richard Biener  <rguenther@suse.de>
    
    	Backport from mainline
    	2020-01-20  Richard Biener  <rguenther@suse.de>
    
    	PR debug/92763
    	* dwarf2out.c (prune_unused_types): Unconditionally mark
    	called function DIEs.
    
    	* g++.dg/debug/pr92763.C: New testcase.

--- gcc/dwarf2out.c
+++ gcc/dwarf2out.c
@@ -29580,9 +29580,9 @@ prune_unused_types (void)
   for (i = 0; base_types.iterate (i, &base_type); i++)
     prune_unused_types_mark (base_type, 1);
 
-  /* For -fvar-tracking-assignments, also set the mark on nodes that could be
-     referenced by DW_TAG_call_site DW_AT_call_origin (i.e. direct call
-     callees).  */
+  /* Also set the mark on nodes that could be referenced by
+     DW_TAG_call_site DW_AT_call_origin (i.e. direct call callees) or
+     by DW_TAG_inlined_subroutine origins.  */
   cgraph_node *cnode;
   FOR_EACH_FUNCTION (cnode)
     if (cnode->referred_to_p (false))
@@ -29591,8 +29591,7 @@ prune_unused_types (void)
 	if (die == NULL || die->die_mark)
 	  continue;
 	for (cgraph_edge *e = cnode->callers; e; e = e->next_caller)
-	  if (e->caller != cnode
-	      && opt_for_fn (e->caller->decl, flag_var_tracking_assignments))
+	  if (e->caller != cnode)
 	    {
 	      prune_unused_types_mark (die, 1);
 	      break;
