ecd0c303b0553e2be89379e9b7d4f4821ac03020
Bernd Edlinger <bernd.edlinger@hotmail.de>
2020-02-17 17:40:07 +0100

    Avoid collect2 calling signal unsafe functions and/or unlink with uninitialized memory
    2020-02-20  Bernd Edlinger  <bernd.edlinger@hotmail.de>
    
    	* collect2.c (tool_cleanup): Avoid calling not signal-safe
    	functions.
    	(maybe_run_lto_and_relink): Avoid possible signal handler
    	access to unintialzed memory (lto_o_files).

--- gcc/collect2.c
+++ gcc/collect2.c
@@ -382,6 +382,10 @@ static void scan_prog_file (const char *, scanpass, scanfilter);
 void
 tool_cleanup (bool from_signal)
 {
+  /* maybe_unlink may call notice, which is not signal safe.  */
+  if (from_signal)
+    debug = false;
+
   if (c_file != 0 && c_file[0])
     maybe_unlink (c_file);
 
@@ -741,7 +745,10 @@ maybe_run_lto_and_relink (char **lto_ld_argv, char **object_lst,
 	      ++num_files;
 	  }
 
-	lto_o_files = XNEWVEC (char *, num_files + 1);
+	/* signal handler may access uninitialized memory
+	   and delete whatever it points to, if lto_o_files
+	   is not allocated with calloc.  */
+	lto_o_files = XCNEWVEC (char *, num_files + 1);
 	lto_o_files[num_files] = NULL;
 	start = XOBFINISH (&temporary_obstack, char *);
 	for (i = 0; i < num_files; ++i)
