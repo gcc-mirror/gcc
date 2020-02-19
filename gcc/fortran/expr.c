44ea6508f1009086018d0db4347a14b9c4eec2c0
Mark Eggleston <markeggleston@gcc.gnu.org>
2020-02-19 09:36:42 +0000

    [Fortran] ICE assign character pointer to non target PR93714
    An ICE occurred if an attempt was made to assign a pointer to a
    character variable that has an length incorrectly specified using
    a real constant and does not have the target attribute.
    
    	Backported from mainline
    	2020-02-18  Mark Eggleston  <markeggleston@gcc.gnu.org>
    
    	PR fortran/93714
    	* expr.c (gfc_check_pointer_assign): Move check for
    	matching character length to after checking the lvalue
    	attributes for target or pointer.
    
    	PR fortran/93714
    	* gfortran.dg/char_pointer_assign_6.f90: Look for no target
    	message instead of length mismatch.
    	* gfortran.dg/pr93714_1.f90
    	* gfortran.dg/pr93714_2.f90

--- gcc/fortran/expr.c
+++ gcc/fortran/expr.c
@@ -4196,13 +4196,6 @@ gfc_check_pointer_assign (gfc_expr *lvalue, gfc_expr *rvalue,
   if (rvalue->expr_type == EXPR_NULL)
     return true;
 
-  if (lvalue->ts.type == BT_CHARACTER)
-    {
-      bool t = gfc_check_same_strlen (lvalue, rvalue, "pointer assignment");
-      if (!t)
-	return false;
-    }
-
   if (rvalue->expr_type == EXPR_VARIABLE && is_subref_array (rvalue))
     lvalue->symtree->n.sym->attr.subref_array_pointer = 1;
 
@@ -4256,6 +4249,13 @@ gfc_check_pointer_assign (gfc_expr *lvalue, gfc_expr *rvalue,
 		     "nor POINTER at %L", &rvalue->where);
 	  return false;
 	}
+    }
+
+  if (lvalue->ts.type == BT_CHARACTER)
+    {
+      bool t = gfc_check_same_strlen (lvalue, rvalue, "pointer assignment");
+      if (!t)
+	return false;
     }
 
   if (is_pure && gfc_impure_variable (rvalue->symtree->n.sym))
