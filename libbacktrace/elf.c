8600440e1479a8bd0394509b86a4d85d98ca6c8a
Ian Lance Taylor <iant@golang.org>
2020-02-19 10:30:51 -0800

    libbacktrace: don't free ELF strtab if error occurs after saving syminfo
    	* elf.c (elf_add): Don't free strtab if an error occurs after
    	recording symbol information.

--- libbacktrace/elf.c
+++ libbacktrace/elf.c
@@ -3011,6 +3011,7 @@ elf_add (struct backtrace_state *state, const char *filename, int descriptor,
 	 string table permanently.  */
       backtrace_release_view (state, &symtab_view, error_callback, data);
       symtab_view_valid = 0;
+      strtab_view_valid = 0;
 
       *found_sym = 1;
 
