26b070a3f6e5c7ccee7491fb758dba4c72bd7026
Jonathan Wakely <jwakely@redhat.com>
2020-02-26 09:43:59 +0000

    PR libstdc++/78552 only construct std::locale for C locale once
    Backport from mainline
    2019-10-09  Jonathan Wakely  <jwakely@redhat.com>
    
    	PR libstdc++/78552
    	* src/c++98/locale_init.cc (locale::classic()): Do not construct a new
    	locale object for every call.
    	(locale::_S_initialize_once()): Construct C locale here.

--- libstdc++-v3/src/c++98/locale_init.cc
+++ libstdc++-v3/src/c++98/locale_init.cc
@@ -296,7 +296,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
   locale::classic()
   {
     _S_initialize();
-    return *(new (&c_locale) locale(_S_classic));
+    return *(const locale*)c_locale;
   }
 
   void
@@ -306,6 +306,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
     // One reference for _S_classic, one for _S_global
     _S_classic = new (&c_locale_impl) _Impl(2);
     _S_global = _S_classic;
+    new (&c_locale) locale(_S_classic);
   }
 
   void
