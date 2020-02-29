c7dbc54958321d296ca4e283f26f279f6a5342a7
Jeff Law <law@redhat.com>
2020-02-29 13:45:37 -0700

    Make STATIC_CHAIN_REGNUM a call used register.
    	* config/v850/v850.h (STATIC_CHAIN_REGNUM): Change to r19.
    	* config/v850/v850.c (v850_asm_trampoline_template): Update
    	accordingly.

--- gcc/config/v850/v850.h
+++ gcc/config/v850/v850.h
@@ -438,8 +438,9 @@ enum reg_class
 /* Base register for access to arguments of the function.  */
 #define ARG_POINTER_REGNUM 35
 
-/* Register in which static-chain is passed to a function.  */
-#define STATIC_CHAIN_REGNUM 20
+/* Register in which static-chain is passed to a function.
+   This must be a call used register.  */
+#define STATIC_CHAIN_REGNUM 19
 
 /* If defined, this macro specifies a table of register pairs used to
    eliminate unneeded registers that point into the stack frame.  If
