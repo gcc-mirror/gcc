2f3318dbcfd2390b49231b66a47ebed505af18f9
liuhongt <hongtao.liu@intel.com>
2020-03-02 18:07:36 +0800

    AVX512FP16: Add testcase for vector init and broadcast intrinsics.
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/i386/m512-check.h: Add union128h, union256h, union512h.
    	* gcc.target/i386/avx512fp16-10a.c: New test.
    	* gcc.target/i386/avx512fp16-10b.c: Ditto.
    	* gcc.target/i386/avx512fp16-1a.c: Ditto.
    	* gcc.target/i386/avx512fp16-1b.c: Ditto.
    	* gcc.target/i386/avx512fp16-1c.c: Ditto.
    	* gcc.target/i386/avx512fp16-1d.c: Ditto.
    	* gcc.target/i386/avx512fp16-1e.c: Ditto.
    	* gcc.target/i386/avx512fp16-2a.c: Ditto.
    	* gcc.target/i386/avx512fp16-2b.c: Ditto.
    	* gcc.target/i386/avx512fp16-2c.c: Ditto.
    	* gcc.target/i386/avx512fp16-3a.c: Ditto.
    	* gcc.target/i386/avx512fp16-3b.c: Ditto.
    	* gcc.target/i386/avx512fp16-3c.c: Ditto.
    	* gcc.target/i386/avx512fp16-4.c: Ditto.
    	* gcc.target/i386/avx512fp16-5.c: Ditto.
    	* gcc.target/i386/avx512fp16-6.c: Ditto.
    	* gcc.target/i386/avx512fp16-7.c: Ditto.
    	* gcc.target/i386/avx512fp16-8.c: Ditto.
    	* gcc.target/i386/avx512fp16-9a.c: Ditto.
    	* gcc.target/i386/avx512fp16-9b.c: Ditto.
    	* gcc.target/i386/pr54855-13.c: Ditto.
    	* gcc.target/i386/avx512fp16-vec_set_var.c: Ditto.

---
+++ gcc/testsuite/gcc.target/i386/pr54855-13.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -mavx512fp16" } */
+/* { dg-final { scan-assembler-times "vmaxsh\[ \\t\]" 1 } } */
+/* { dg-final { scan-assembler-not "vcomish\[ \\t\]" } } */
+/* { dg-final { scan-assembler-not "vmovsh\[ \\t\]" { target { ! ia32 } } } } */
+
+#include <immintrin.h>
+
+__m128h
+foo (__m128h x, __m128h y)
+{
+  x[0] = x[0] > y[0] ? x[0] : y[0];
+  return x;
+}
