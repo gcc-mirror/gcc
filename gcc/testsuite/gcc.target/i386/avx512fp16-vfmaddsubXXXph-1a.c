b6c24eab08d9649b4229b4b0396ce91f97f88dfb
liuhongt <hongtao.liu@intel.com>
2020-03-02 17:29:15 +0800

    AVX512FP16: Add testcase for vfmaddsub[132,213,231]ph/vfmsubadd[132,213,231]ph.
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/i386/avx512fp16-vfmaddsubXXXph-1a.c: New test.
    	* gcc.target/i386/avx512fp16-vfmaddsubXXXph-1b.c: Ditto.
    	* gcc.target/i386/avx512fp16-vfmsubaddXXXph-1a.c: Ditto.
    	* gcc.target/i386/avx512fp16-vfmsubaddXXXph-1b.c: Ditto.
    	* gcc.target/i386/avx512fp16vl-vfmaddsubXXXph-1a.c: Ditto.
    	* gcc.target/i386/avx512fp16vl-vfmaddsubXXXph-1b.c: Ditto.
    	* gcc.target/i386/avx512fp16vl-vfmsubaddXXXph-1a.c: Ditto.
    	* gcc.target/i386/avx512fp16vl-vfmsubaddXXXph-1b.c: Ditto.

---
+++ gcc/testsuite/gcc.target/i386/avx512fp16-vfmaddsubXXXph-1a.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+/* { dg-options "-mavx512fp16 -O2" } */
+/* { dg-final { scan-assembler-times "vfmaddsub...ph\[ \\t\]+\[^\{\n\]*%zmm\[0-9\]+(?:\n|\[ \\t\]+#)"  1 } } */
+/* { dg-final { scan-assembler-times "vfmaddsub...ph\[ \\t\]+\[^\{\n\]*%zmm\[0-9\]+\{%k\[1-7\]\}(?:\n|\[ \\t\]+#)"  2 } } */
+/* { dg-final { scan-assembler-times "vfmaddsub231ph\[ \\t\]+\[^\{\n\]*%zmm\[0-9\]+\{%k\[1-7\]\}(?:\n|\[ \\t\]+#)"  1 } } */
+/* { dg-final { scan-assembler-times "vfmaddsub...ph\[ \\t\]+\[^\{\n\]*%zmm\[0-9\]+\{%k\[1-7\]\}\{z\}(?:\n|\[ \\t\]+#)"  1 } } */
+/* { dg-final { scan-assembler-times "vfmaddsub...ph\[ \\t\]+\[^\n\]*\{rn-sae\}\[^\{\n\]*%zmm\[0-9\]+(?:\n|\[ \\t\]+#)" 1 } } */
+/* { dg-final { scan-assembler-times "vfmaddsub...ph\[ \\t\]+\[^\n\]*\{rd-sae\}\[^\{\n\]*%zmm\[0-9\]+\{%k\[1-7\]\}(?:\n|\[ \\t\]+#)" 1 } } */
+/* { dg-final { scan-assembler-times "vfmaddsub231ph\[ \\t\]+\[^\n\]*\{ru-sae\}\[^\{\n\]*%zmm\[0-9\]+\{%k\[1-7\]\}(?:\n|\[ \\t\]+#)" 1 } } */
+/* { dg-final { scan-assembler-times "vfmaddsub...ph\[ \\t\]+\[^\n\]*\{rz-sae\}\[^\{\n\]*%zmm\[0-9\]+\{%k\[1-7\]\}\{z\}(?:\n|\[ \\t\]+#)" 1 } } */
+
+#include <immintrin.h>
+
+volatile __m512h x1, x2, x3;
+volatile __mmask32 m;
+
+void extern
+avx512f_test (void)
+{
+  x1 = _mm512_fmaddsub_ph (x1, x2, x3);
+  x1 = _mm512_mask_fmaddsub_ph (x1, m, x2, x3);
+  x3 = _mm512_mask3_fmaddsub_ph (x1, x2, x3, m);
+  x1 = _mm512_maskz_fmaddsub_ph (m, x1, x2, x3);
+  x1 = _mm512_fmaddsub_round_ph (x1, x2, x3, _MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC);
+  x1 = _mm512_mask_fmaddsub_round_ph (x1, m, x2, x3, _MM_FROUND_TO_NEG_INF | _MM_FROUND_NO_EXC);
+  x3 = _mm512_mask3_fmaddsub_round_ph (x1, x2, x3, m, _MM_FROUND_TO_POS_INF | _MM_FROUND_NO_EXC);
+  x1 = _mm512_maskz_fmaddsub_round_ph (m, x1, x2, x3, _MM_FROUND_TO_ZERO | _MM_FROUND_NO_EXC);
+}
