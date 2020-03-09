157e23d8803dda38d7d38404bcc4284ce2dce3b5
Christophe Lyon <christophe.lyon@linaro.org>
2020-03-09 13:15:10 +0000

    [testsuite][arm] Fix typo in fuse-caller-save.c
    2020-03-09  Christophe Lyon  <christophe.lyon@linaro.org>
    
    	* gcc.target/arm/fuse-caller-save.c: Fix DejaGnu typo.

--- gcc/testsuite/gcc.target/arm/fuse-caller-save.c
+++ gcc/testsuite/gcc.target/arm/fuse-caller-save.c
@@ -22,4 +22,4 @@ main (void)
 
 /* For thumb1, r3 is considered likely spilled, and treated differently in
    ira_build_conflicts, which inhibits the fipa-ra optimization.  */
-/* { dg-final { scan-assembler-times "mov\tr[123], r0" 1 { target { ! arm_thumb1 } } } } */
+/* { dg-final { scan-assembler-times "mov\tr\[123\], r0" 1 { target { ! arm_thumb1 } } } } */
