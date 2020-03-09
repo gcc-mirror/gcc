314b91220a07bd63f13c58e37f1b5b9430a3702b
Martin Liska <mliska@suse.cz>
2020-03-09 14:13:04 +0100

    Restore alignment in rs6000 target.
    	PR target/93800
    	* config/rs6000/rs6000.c (rs6000_option_override_internal):
    	Remove set of str_align_loops and str_align_jumps as these
    	should be set in previous 2 conditions in the function.
    	PR target/93800
    	* gcc.target/powerpc/pr93800.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/pr93800.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-options "-mcpu=860 -O2" } */
+/* { dg-require-effective-target ilp32 } */
+/* { dg-final { scan-assembler-not "\\.p2align 4" } } */
+
+volatile int g;
+int f(int a, int b)
+{
+	int i;
+
+	for (i = 0; i < b; i++)
+		a += g;
+	return a;
+}
