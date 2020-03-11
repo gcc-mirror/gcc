0c3519218fb11bdde5356aec9fcac133b4988698
Alan Modra <amodra@gmail.com>
2020-03-11 21:22:37 +1030

    [RS6000] PR94145, make PLT loads volatile
    	PR target/94145
    	* config/rs6000/rs6000.c (rs6000_longcall_ref): Use unspec_volatile
    	for PLT16_LO.
    	* config/rs6000/rs6000.md (UNSPEC_PLT16_LO): Remove.
    	(UNSPECV_PLT16_LO): Define.
    	(pltseq_plt16_lo_): Use unspec_volatile.
    
    (cherry picked from commit 19e5389debb03c3623f6a2ce8a8f6f4aa2118901)
    minus the PLT_PCREL parts

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -33049,8 +33049,9 @@ rs6000_longcall_ref (rtx call_ref, rtx arg)
       rtx reg = gen_rtx_REG (Pmode, regno);
       rtx hi = gen_rtx_UNSPEC (Pmode, gen_rtvec (3, base, call_ref, arg),
 			       UNSPEC_PLT16_HA);
-      rtx lo = gen_rtx_UNSPEC (Pmode, gen_rtvec (3, reg, call_ref, arg),
-			       UNSPEC_PLT16_LO);
+      rtx lo = gen_rtx_UNSPEC_VOLATILE (Pmode,
+					gen_rtvec (3, reg, call_ref, arg),
+					UNSPECV_PLT16_LO);
       emit_insn (gen_rtx_SET (reg, hi));
       emit_insn (gen_rtx_SET (reg, lo));
       return reg;
