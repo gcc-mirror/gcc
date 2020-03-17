e675277214018c96dbb016e4cb9c3bd8d170c882
Sandra Loosemore <sandra@codesourcery.com>
2020-03-16 18:08:01 -0700

    Kernels loops annotation: Fortran.
    This patch implements the Fortran support for adding "#pragma acc loop auto"
    annotations to loops in OpenACC kernels regions.  It implements the same
    -fopenacc-kernels-annotate-loops and -Wopenacc-kernels-annotate-loops options
    that were previously added (and documented) for the C/C++ front ends.
    
    2020-03-27  Sandra Loosemore  <sandra@codesourcery.com>
    	    Gergö Barany <gergo@codesourcery.com>
    
    	gcc/fortran/
    	* gfortran.h (gfc_oacc_annotate_loops_in_kernels_regions): Declare.
    	* lang.opt (Wopenacc-kernels-annotate-loops): New.
    	(fopenacc-kernels-annotate-loops): New.
    	* openmp.c: Include options.h.
    	(enum annotation_state, enum annotation_result): New.
    	(check_code_for_invalid_calls): New.
    	(check_expr_for_invalid_calls): New.
    	(check_for_invalid_calls): New.
    	(annotate_do_loop): New.
    	(annotate_do_loops_in_kernels): New.
    	(compute_goto_targets): New.
    	(gfc_oacc_annotate_loops_in_kernels_regions): New.
    	* parse.c (gfc_parse_file): Handle -fopenacc-kernels-annotate-loops.
    
    	gcc/testsuite/
    	* gfortran.dg/goacc/classify-kernels-unparallelized.f95: Add
    	-fno-openacc-kernels-annotate-loops option.
    	* gfortran.dg/goacc/classify-kernels.f95: Likewise.
    	* gfortran.dg/goacc/common-block-3.f90: Likewise.
    	* gfortran.dg/goacc/kernels-loop-2.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data-2.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data-enter-exit-2.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data-enter-exit.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data-update.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-n.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop.f95: Likewise.
    	* gfortran.dg/goacc/kernels-parallel-loop-data-enter-exit.f95:
    	Likewise.
    	* gfortran.dg/goacc/kernels-loop-annotation-1.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-2.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-3.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-4.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-5.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-6.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-7.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-8.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-9.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-10.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-11.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-12.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-13.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-14.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-15.f95: New.
    	* gfortran.dg/goacc/kernels-loop-annotation-16.f95: New.

---
+++ gcc/testsuite/gfortran.dg/goacc/kernels-loop-annotation-14.f95
@@ -0,0 +1,35 @@
+! { dg-additional-options "-fopenacc -fopenacc-kernels-annotate-loops" }
+! { dg-additional-options "-Wopenacc-kernels-annotate-loops" }
+! { dg-additional-options "-fdump-tree-original" }
+! { dg-do compile }
+
+! Test that an explicit annotation on an outer loop suppresses annotation
+!  of inner loops, and produces a diagnostic.
+
+function f (a, b)
+  implicit none
+
+  real :: f
+  real, intent (in), dimension (16) :: a, b
+
+  integer :: i, j
+  real :: t
+
+  t = 0.0
+
+!$acc kernels
+
+!$acc loop seq  ! { dg-warning "Explicit loop annotation" }
+  do i = 1, 16
+    do j = 1, 16
+      t = t + a(i) * b(j)
+    end do
+  end do
+
+  f = t
+
+!$acc end kernels
+
+end function f
+
+! { dg-final { scan-tree-dump-times "acc loop private.* auto" 0 "original" } }
---
+++ gcc/testsuite/gfortran.dg/goacc/kernels-loop-annotation-4.f95
@@ -0,0 +1,34 @@
+! { dg-additional-options "-fopenacc -fopenacc-kernels-annotate-loops" }
+! { dg-additional-options "-Wopenacc-kernels-annotate-loops" }
+! { dg-additional-options "-fdump-tree-original" }
+! { dg-do compile }
+
+! Test that a loop with a case construct in the body can be annotated. 
+
+function f (a, b)
+  implicit none
+
+  real :: f
+  real, intent (in), dimension (16) :: a, b
+
+  integer :: i
+  real :: t
+
+!$acc kernels
+
+  do i = 1, 16
+    select case (i)
+      case (1)
+        t = a(i) * b(i)
+      case default
+        t = t + a(i) * b(i)
+    end select
+  end do
+
+  f = t
+
+!$acc end kernels
+
+end function f
+
+! { dg-final { scan-tree-dump-times "acc loop private.* auto" 1 "original" } }
