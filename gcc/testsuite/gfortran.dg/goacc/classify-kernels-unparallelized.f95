bf0b96ecd4c9dc9ab363e2384c2c84c3d3a45c6a
Sandra Loosemore <sandra@codesourcery.com>
2020-03-19 08:32:24 -0700

    Additional Fortran testsuite fixes for kernels loops annotation pass.
    These testsuite fixes are specific to the og10 branch, so are being
    segregated from the ones that apply to mainline in a separate commit
    from the main Fortran kernels loop annotation patch.
    
    2020-03-27  Sandra Loosemore  <sandra@codesourcery.com>
    
    	gcc/testsuite/
    	* gfortran.dg/goacc/classify-kernels-unparallelized.f95: Adjust
    	line numbering.
    	* gfortran.dg/goacc/classify-kernels.f95: Likewise.
    	* gfortran.dg/goacc/kernels-decompose-2.f95: Add
    	-fno-openacc-kernels-annotate-loops.

--- gcc/testsuite/gfortran.dg/goacc/classify-kernels-unparallelized.f95
+++ gcc/testsuite/gfortran.dg/goacc/classify-kernels-unparallelized.f95
@@ -22,7 +22,7 @@ program main
 
   !$acc kernels copyin (a(0:n-1), b(0:n-1)) copyout (c(0:n-1))
   do i = 0, n - 1 ! { dg-message "optimized: assigned OpenACC seq loop parallelism" }
-                  ! { dg-message "optimized: beginning .parloops. region in OpenACC .kernels. construct" "" { target *-*-* } 23 }
+                  ! { dg-message "optimized: beginning .parloops. region in OpenACC .kernels. construct" "" { target *-*-* } 24 }
      c(i) = a(f (i)) + b(f (i))
   end do
   !$acc end kernels
