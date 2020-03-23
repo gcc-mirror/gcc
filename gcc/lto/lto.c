ce6413087deba5888a0a9a24f95f3b796e46add9
Tobias Burnus <tobias@codesourcery.com>
2020-03-23 14:41:58 +0100

    lto/lto.c – used $ or . in generated linkptr name
    	* lto.c (offload_handle_link_vars): Reduce chance of
    	naming clashes of generated linkptr variable.

--- gcc/lto/lto.c
+++ gcc/lto/lto.c
@@ -561,11 +561,9 @@ offload_handle_link_vars (void)
 			  DECL_ATTRIBUTES (var->decl)))
       {
 	tree type = build_pointer_type (TREE_TYPE (var->decl));
-	tree var_name = DECL_ASSEMBLER_NAME (var->decl);
-	char *new_name
-	  = ACONCAT ((IDENTIFIER_POINTER (var_name), "_linkptr", NULL));
 	tree link_ptr_var = build_decl (UNKNOWN_LOCATION, VAR_DECL,
-					get_identifier (new_name), type);
+					clone_function_name (var->decl,
+							     "linkptr"), type);
 	TREE_USED (link_ptr_var) = 1;
 	TREE_STATIC (link_ptr_var) = 1;
 	DECL_ARTIFICIAL (link_ptr_var) = 1;
