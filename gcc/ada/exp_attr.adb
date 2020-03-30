eb72521915b1f109b3b65aa384005c2527f76c31
Bob Duff <duff@adacore.com>
2020-03-30 10:14:27 -0400

    [Ada] Put_Image improvements for strings
    2020-06-15  Bob Duff  <duff@adacore.com>
    
    gcc/ada/
    
    	* exp_attr.adb (Put_Image): Use underlying type for strings.
    	Remove unchecked union processing.
    	* exp_put_image.adb (Tagged_Put_Image_Enabled): Use -gnatd_z to
    	enable default Put_Image for tagged types.  This allows testing
    	that feature.
    	(Build_String_Put_Image_Call): Set Conversion_OK flag.
    	(Make_Component_List_Attributes): Remove unchecked union
    	processing.
    	(Enable_Put_Image): Disable for unchecked unions.  Enable for
    	nonscalar types (which were mistakenly disabled in earlier
    	changes).
    	* debug.adb: Document -gnatd_z switch.
    	* libgnat/s-putima.adb (Put_Image_String, Put_Image_Wide_String,
    	Put_Image_Wide_Wide_String): Double double-quote characters.
    	Forget about special handling of control characters for now --
    	that's rare enough to not be a priority, and it's not clear what
    	the right thing to do is anyway.
    	* namet.adb: Minor: Improve debugger-friendliness.
    	* sinfo.ads: Minor: Add "???" comment.

--- gcc/ada/exp_attr.adb
+++ gcc/ada/exp_attr.adb
@@ -5505,20 +5505,7 @@ package body Exp_Attr is
                Analyze (N);
                return;
 
-            --  ???It would be nice to call Build_String_Put_Image_Call below
-            --  if U_Type is a standard string type, but it currently generates
-            --  something like:
-            --
-            --     Put_Image_String (Sink, String (X));
-            --
-            --  so if X is of a private type whose full type is "new String",
-            --  then the type conversion is illegal. To fix that, we would need
-            --  to do unchecked conversions of access values, taking care to
-            --  deal with thin and fat pointers properly. For now, we just fall
-            --  back to Build_Array_Put_Image_Procedure in these cases, so the
-            --  following says "Root_Type (Entity (Pref))" instead of "U_Type".
-
-            elsif Is_Standard_String_Type (Root_Type (Entity (Pref))) then
+            elsif Is_Standard_String_Type (U_Type) then
                Rewrite (N, Build_String_Put_Image_Call (N));
                Analyze (N);
                return;
@@ -5558,21 +5545,6 @@ package body Exp_Attr is
 
             else
                pragma Assert (Is_Record_Type (U_Type));
-
-               --  Program_Error is raised when calling the default
-               --  implementation of the Put_Image attribute of an
-               --  Unchecked_Union type. ???It would be friendlier to print a
-               --  canned string. See handling of unchecked unions in
-               --  exp_put_image.adb (which is not reachable).
-
-               if Is_Unchecked_Union (Base_Type (U_Type)) then
-                  Rewrite (N,
-                    Make_Raise_Program_Error (Loc,
-                      Reason => PE_Unchecked_Union_Restriction));
-                  Set_Etype (N, Standard_Void_Type);
-                  return;
-               end if;
-
                Build_Record_Put_Image_Procedure
                  (Loc, Full_Base (U_Type), Decl, Pname);
                Insert_Action (N, Decl);
