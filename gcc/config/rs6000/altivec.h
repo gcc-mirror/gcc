02a201f7158b4d2d2b937ff3b37640126ce936d2
Carl Love <carll@us.ibm.com>
2020-03-31 11:41:26 -0500

    rs6000: vec_rlnm fix to make builtin work according to ABI
    	Backport from mainline:
    	  commit e97929e20b2f52e6cfc046c1302324d1b24d95e3
    	  Author: Carl Love <carll@us.ibm.com>
    	  Date:   Wed Mar 25 18:33:37 2020 -0500
    
    	gcc/ChangeLog
    
    	PR target/93819
                * gcc/config/rs6000/altivec.h:
                Fixed swapped arguments for vec_rlnm define.

--- gcc/config/rs6000/altivec.h
+++ gcc/config/rs6000/altivec.h
@@ -180,7 +180,7 @@
 #define vec_recipdiv __builtin_vec_recipdiv
 #define vec_rlmi __builtin_vec_rlmi
 #define vec_vrlnm __builtin_vec_rlnm
-#define vec_rlnm(a,b,c) (__builtin_vec_rlnm((a),((b)<<8)|(c)))
+#define vec_rlnm(a,b,c) (__builtin_vec_rlnm((a),((c)<<8)|(b)))
 #define vec_rsqrt __builtin_vec_rsqrt
 #define vec_rsqrte __builtin_vec_rsqrte
 #define vec_signed __builtin_vec_vsigned
