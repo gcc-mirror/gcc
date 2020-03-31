8fb217e34984588da26bfcb2981a58625f5ced9f
Sandra Loosemore <sandra@codesourcery.com>
2020-03-31 14:29:09 -0700

    Fix bug in processing of array dimensions in data clauses.
    The g++ front end wraps the array length and low_bound values in
    NON_LVALUE_EXPR, causing the subsequent tests for INTEGER_CST to fail.
    The test case c-c++-common/goacc/kernels-loop-annotation-1.c was
    tickling this bug and giving bogus errors in g++ because it was falling
    through to dynamic array code instead of recognizing the constant bounds.
    
    This patch was posted upstream here
    https://gcc.gnu.org/pipermail/gcc-patches/2020-March/542694.html
    but not yet committed.  It may be that some other fix for this problem
    is implemented on mainline instead; check before merging this patch.
    
    2020-03-31  Sandra Loosemore  <sandra@codesourcery.com>
    
    	gcc/cp/
    	* semantics.c (handle_omp_array_sections_1): Call STRIP_NOPS
    	on length and low_bound;
    	(handle_omp_array_sections): Likewise.

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -5057,6 +5057,10 @@ handle_omp_array_sections_1 (tree c, tree t, vec<tree> &types,
     length = mark_rvalue_use (length);
   /* We need to reduce to real constant-values for checks below.  */
   if (length)
+    STRIP_NOPS (length);
+  if (low_bound)
+    STRIP_NOPS (low_bound);
+  if (length)
     length = fold_simple (length);
   if (low_bound)
     low_bound = fold_simple (low_bound);
@@ -5369,6 +5373,11 @@ handle_omp_array_sections (tree c, enum c_omp_region_type ort)
 	{
 	  tree low_bound = TREE_PURPOSE (t);
 	  tree length = TREE_VALUE (t);
+
+	  if (length)
+	    STRIP_NOPS (length);
+	  if (low_bound)
+	    STRIP_NOPS (low_bound);
 
 	  i--;
 	  if (low_bound
