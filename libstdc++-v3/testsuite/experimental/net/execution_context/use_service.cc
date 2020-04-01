5e4128fd723d6ff7b19600e96839d2bd9c9a69e4
Andrea Corallo <andrea.corallo@arm.com>
2020-04-01 10:19:04 +0100

    libstdc++-v3/test: Better skip for "use_service.cc"
    2020-04-01  Andrea Corallo  <andrea.corallo@arm.com>
    
    	* testsuite/experimental/net/execution_context/use_service.cc:
    	Require pthread and gthreads.
    
    (cherry picked from commit c1effaa209f9f9b4bcf4cd7c6fcfccaf5e59a2b2)

--- libstdc++-v3/testsuite/experimental/net/execution_context/use_service.cc
+++ libstdc++-v3/testsuite/experimental/net/execution_context/use_service.cc
@@ -15,7 +15,11 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-do run { target c++14 } }
+// { dg-do run }
+// { dg-options "-pthread"  }
+// { dg-require-effective-target c++14 }
+// { dg-require-effective-target pthread }
+// { dg-require-gthreads "" }
 
 #include <experimental/executor>
 #include <testsuite_hooks.h>
