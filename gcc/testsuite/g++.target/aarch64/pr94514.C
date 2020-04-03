fb22faf48f6eb518932f24005f8606e5f19a7304
Szabolcs Nagy <szabolcs.nagy@arm.com>
2020-04-03 09:35:41 +0000

    aarch64, libgcc: Fix unwinding from pac-ret to normal frames [PR94514]
    With -mbranch-protection=pac-ret the debug info toggles the
    signedness state of the return address so the unwinder knows when
    the return address needs pointer authentication.
    
    The unwind context flags were not updated according to the dwarf
    frame info.
    
    This causes unwinding across frames that were built without pac-ret
    to incorrectly authenticate the return address wich corrupts the
    return address on a system where PAuth is enabled.
    
    Note: This even affects systems where all code use pac-ret because
    unwinding across a signal frame the return address is not signed.
    
    gcc/testsuite/ChangeLog:
    
    	PR target/94514
    	* g++.target/aarch64/pr94514.C: New test.
    	* gcc.target/aarch64/pr94514.c: New test.
    
    libgcc/ChangeLog:
    
    	PR target/94514
    	* config/aarch64/aarch64-unwind.h (aarch64_frob_update_context):
    	Update context->flags accroding to the frame state.

---
+++ gcc/testsuite/g++.target/aarch64/pr94514.C
@@ -0,0 +1,26 @@
+/* PR target/94514. Unwind across mixed pac-ret and non-pac-ret frames.  */
+/* { dg-do run } */
+
+__attribute__((noinline, target("branch-protection=pac-ret")))
+static void do_throw (void)
+{
+  throw 42;
+  __builtin_abort ();
+}
+
+__attribute__((noinline, target("branch-protection=none")))
+static void no_pac_ret (void)
+{
+  do_throw ();
+  __builtin_abort ();
+}
+
+int main ()
+{
+  try {
+    no_pac_ret ();
+  } catch (...) {
+    return 0;
+  }
+  __builtin_abort ();
+}
