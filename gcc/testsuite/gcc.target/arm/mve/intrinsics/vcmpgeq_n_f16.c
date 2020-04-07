6a90680bfff0a3803b3dc950c847046abb2d7d54
Andre Simoes Dias Vieira <andre.simoesdiasvieira@arm.com>
2020-04-07 15:40:15 +0100

    arm: MVE: Add C++ polymorphism and fix some more issues
    This patch adds C++ polymorphism for the MVE intrinsics, by using the native C++
    polymorphic functions when C++ is used.
    
    It also moves the PRESERVE name macro definitions to the right place so that the
    variants without the '__arm_' prefix are not available if we define the PRESERVE
    NAMESPACE macro.
    
    This patch further fixes two testisms that were brought to light by C++ testing
    added in this patch.
    
    gcc/ChangeLog:
    2020-04-07  Andre Vieira  <andre.simoesdiasvieira@arm.com>
    
    	* config/arm/arm_mve.h: Add C++ polymorphism and fix preserve MACROs.
    
    gcc/testsuite/ChangeLog:
    2020-04-07  Andre Vieira  <andre.simoesdiasvieira@arm.com>
    
    	* g++.target/arm/mve.exp: New.
    	* gcc.target/arm/mve/intrinsics/vcmpneq_n_f16: Fix testism.
    	* gcc.target/arm/mve/intrinsics/vcmpneq_n_f32: Likewise.

--- gcc/testsuite/gcc.target/arm/mve/intrinsics/vcmpgeq_n_f16.c
+++ gcc/testsuite/gcc.target/arm/mve/intrinsics/vcmpgeq_n_f16.c
@@ -15,7 +15,7 @@ foo (float16x8_t a, float16_t b)
 mve_pred16_t
 foo1 (float16x8_t a, float16_t b)
 {
-  return vcmpgeq_n (a, b);
+  return vcmpgeq (a, b);
 }
 
 /* { dg-final { scan-assembler "vcmp.f16"  }  } */
