9a385bd124dc0272f1ea028f4e2e8026d6ea989b
Will Schmidt <will_schmidt@vnet.ibm.com>
2020-04-07 16:07:03 -0500

    rs6000 pragma fix backport from mainline to gcc-9
    2020-04-07  Will Schmidt  <will_schmidt@vnet.ibm.com>
    
    Backport from mainline.
        2020-03-23  Will Schmidt  <will_schmidt@vnet.ibm.com>
    
        * config/rs6000/rs6000-call.c altivec_init_builtins(): Remove
        code to skip defining builtins based on builtin_mask.
    
        * gcc.target/powerpc/pragma_power6.c: New.
        * gcc.target/powerpc/pragma_power7.c: New.
        * gcc.target/powerpc/pragma_power8.c: New.
        * gcc.target/powerpc/pragma_power9.c: New.
        * gcc.target/powerpc/pragma_misc9.c: New.
        * gcc.target/powerpc/vsu/pragma_misc9.c: New.
        * gcc.target/powerpc/vsu/vec-all-nez-7.c: Update.
        * gcc.target/powerpc/vsu/vec-any-eqz-7.c: Update.

---
+++ gcc/testsuite/gcc.target/powerpc/pragma_misc9.c
@@ -0,0 +1,47 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target powerpc_p9vector_ok } */
+/* { dg-require-effective-target lp64 } */
+/* { dg-options "-mdejagnu-cpu=power9 -maltivec -O2" } */
+
+/* Ensure that if we set a pragma gcc target for an
+   older processor, we do not compile builtins that
+   the older target does not support.  */
+
+#include <altivec.h>
+
+vector bool int
+test1 (vector signed int a, vector signed int b)
+{
+  return vec_cmpnez (a, b);
+}
+
+#pragma GCC target ("cpu=power8")
+vector bool int
+test2 (vector signed int a, vector signed int b)
+{
+  return vec_cmpnez (a, b);
+  /* { dg-error "'__builtin_altivec_vcmpnezw' requires the '-mcpu=power9' option" "" { target *-*-* } .-1 } */
+}
+
+#pragma GCC target ("cpu=power7")
+vector signed int
+test3 (vector signed int a, vector signed int b)
+{
+  return vec_mergee (a, b);
+  /* { dg-error "'__builtin_altivec_vmrgew_v4si' requires the '-mpower8-vector' option" "" { target *-*-* } .-1 } */
+}
+
+#pragma GCC target ("cpu=power6")
+vector signed int
+test4 (vector int a, vector int b)
+{
+  return vec_sldw (a, b, 2);
+  /* { dg-error "'__builtin_vsx_xxsldwi_4si' requires the '-mvsx' option" "" { target *-*-* } .-1 } */
+}
+
+vector int
+test5 (vector int a, vector int b)
+{
+  return vec_add (a, b);
+}
+
