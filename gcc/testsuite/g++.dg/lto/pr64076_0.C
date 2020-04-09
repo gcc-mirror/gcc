bb40460646ce4e6ad27a2f6795106d004d405314
Richard Biener <rguenther@suse.de>
2020-04-09 13:54:01 +0200

    testsuite/93369 - use -shared to avoid issue with ODR violation
    The testcase contains an ODR violation and thus the observed
    link failure is an accepted outcome (it originally was for
    an ICE during WPA).  Thus the following adds -shared to the link.
    
    2020-04-09  Richard Biener  <rguenther@suse.de>
    
    	PR testsuite/93369
    	* g++.dg/lto/pr64076_0.C: Add -shared -fPIC.
    	* g++.dg/lto/pr64076_1.C: Add -fPIC.

--- gcc/testsuite/g++.dg/lto/pr64076_0.C
+++ gcc/testsuite/g++.dg/lto/pr64076_0.C
@@ -1,4 +1,8 @@
 // { dg-lto-do link }
+// { dg-lto-options { { -O0 -flto -shared -fPIC } } }
+// { dg-require-effective-target fpic }
+// { dg-require-effective-target shared }
+// { dg-extra-ld-options "-shared" }
 
 #define XXX
 #include "pr64076.H"
