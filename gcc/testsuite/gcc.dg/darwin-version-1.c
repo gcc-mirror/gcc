a7af3434de07c07c83636991ade56f762121e18c
Iain Sandoe <iain@sandoe.co.uk>
2020-04-13 19:27:21 +0100

    Darwin, testsuite: Fix darwin-version-1.c fails with XCode 11.4.
    From XCode 11.4 on 10.14/15 use of 10.6 and 10.7 is deprecated.
    The tools issue diagnostics if -mmacosx-version-min= < 10.8
    
    Adjust the testcase to avoid that usage on 10.14, 10.15 for now.
    
    gcc/testsuite/ChangeLog:
    
    2020-04-13  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* gcc.dg/darwin-version-1.c: Use -mmacosx-version-min= 10.8
    	for system versions 10.14 and 10.15.

--- gcc/testsuite/gcc.dg/darwin-version-1.c
+++ gcc/testsuite/gcc.dg/darwin-version-1.c
@@ -5,7 +5,9 @@
 /* Later Darwin linkers decline to link for less than Darwin8/MacOS 10.4.
    However, we need to make the link for 10.6 because the relevant libgcc_s
    shim files for 10.4 and 10.5 are also not installed in later SDKs.  */
-/* { dg-options "-mmacosx-version-min=10.6" { target *-*-darwin[123]* } } */
+/* { dg-options "-mmacosx-version-min=10.6" { target *-*-darwin1[01234567]* } } */
+/* From XCode 11.4 on 10.14/15 10.6 and 10.7 are also deprecated.  */
+/* { dg-options "-mmacosx-version-min=10.8" { target *-*-darwin1[89]* } } */
 /* { dg-do link { target *-*-darwin* } } */
 
 int main()
