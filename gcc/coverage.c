5c09a1b71bb96d1fd6488d040b65001807fd16c2
Martin Liska <mliska@suse.cz>
2020-04-20 11:26:58 +0200

    Backport e9f799d25973fc38022c5ea71ed5a2bca58a847f
    	Backport from mainline
    	2020-04-17  Martin Liska  <mliska@suse.cz>
    		    Jonathan Yong <10walls@gmail.com>
    
    	PR gcov-profile/94570
    	* ltmain.sh: Do not define HAVE_DOS_BASED_FILE_SYSTEM
    	for CYGWIN.
    	Backport from mainline
    	2020-04-17  Martin Liska  <mliska@suse.cz>
    		    Jonathan Yong <10walls@gmail.com>
    
    	PR gcov-profile/94570
    	* coverage.c (coverage_init): Use separator properly.
    	Backport from mainline
    	2020-04-17  Martin Liska  <mliska@suse.cz>
    		    Jonathan Yong <10walls@gmail.com>
    
    	PR gcov-profile/94570
    	* filenames.h (defined): Do not define HAVE_DOS_BASED_FILE_SYSTEM
    	for CYGWIN.
    
    Co-Authored-By: Jonathan Yong <10walls@gmail.com>

--- gcc/coverage.c
+++ gcc/coverage.c
@@ -1201,6 +1201,11 @@ coverage_obj_finish (vec<constructor_elt, va_gc> *ctor)
 void
 coverage_init (const char *filename)
 {
+#if HAVE_DOS_BASED_FILE_SYSTEM
+  const char *separator = "\\";
+#else
+  const char *separator = "/";
+#endif
   int len = strlen (filename);
   int prefix_len = 0;
 
@@ -1217,11 +1222,6 @@ coverage_init (const char *filename)
 	 of filename in order to prevent file path clashing.  */
       if (profile_data_prefix)
 	{
-#if HAVE_DOS_BASED_FILE_SYSTEM
-	  const char *separator = "\\";
-#else
-	  const char *separator = "/";
-#endif
 	  filename = concat (getpwd (), separator, filename, NULL);
 	  filename = mangle_path (filename);
 	  len = strlen (filename);
@@ -1240,7 +1240,7 @@ coverage_init (const char *filename)
   if (profile_data_prefix)
     {
       memcpy (da_file_name, profile_data_prefix, prefix_len);
-      da_file_name[prefix_len++] = '/';
+      da_file_name[prefix_len++] = *separator;
     }
   memcpy (da_file_name + prefix_len, filename, len);
   strcpy (da_file_name + prefix_len + len, GCOV_DATA_SUFFIX);
