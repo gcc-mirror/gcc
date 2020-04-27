62ab8b9114b0bdae508ed76fa9028e0040d35e6b
Szabolcs Nagy <szabolcs.nagy@arm.com>
2020-04-27 09:07:15 +0100

    aarch64: Fix .cfi_window_save with pac-ret [PR94515]
    On aarch64 -mbranch-protection=pac-ret reuses the dwarf
    opcode for window_save to mean "toggle the return address
    mangle state", but in the dwarf2cfi internal logic the
    state was not updated when an opcode was emitted, the
    currently present update logic is only valid for the
    original sparc use of window_save so a separate bool is
    used on aarch64 to track the state.
    
    This bug can cause the unwinder not to authenticate return
    addresses that were signed (or vice versa) which means a
    runtime crash on a pauth enabled system.
    
    Currently only aarch64 pac-ret uses REG_CFA_TOGGLE_RA_MANGLE.
    
    This should be backported to gcc-9 and gcc-8 branches.
    
    Changed branch-protection=pac-ret to sign-return-address=all etc
    in the tests for the backport and adjusted the dwarf2cfi.c
    changes because the sparc change was missing.
    
    gcc/ChangeLog:
    
    	Backport from mainline.
    	2020-04-27  Szabolcs Nagy  <szabolcs.nagy@arm.com>
    
    	PR target/94515
    	* dwarf2cfi.c (struct GTY): Add ra_mangled.
    	(cfi_row_equal_p): Check ra_mangled.
    	(dwarf2out_frame_debug_cfa_window_save): Remove the argument,
    	this only handles the sparc logic now.
    	(dwarf2out_frame_debug_cfa_toggle_ra_mangle): New function for
    	the aarch64 specific logic.
    	(dwarf2out_frame_debug): Update to use the new subroutines.
    	(change_cfi_row): Check ra_mangled.
    
    gcc/testsuite/ChangeLog:
    
    	Backport from mainline.
    	2020-04-27  Szabolcs Nagy  <szabolcs.nagy@arm.com>
    
    	PR target/94515
    	* g++.target/aarch64/pr94515-1.C: New test.
    	* g++.target/aarch64/pr94515-2.C: New test.

--- gcc/dwarf2cfi.c
+++ gcc/dwarf2cfi.c
@@ -68,6 +68,9 @@ struct GTY(()) dw_cfi_row
 
   /* The expressions for any register column that is saved.  */
   cfi_vec reg_save;
+
+  /* True if the return address is in a mangled state.  */
+  bool ra_mangled;
 };
 
 /* The caller's ORIG_REG is saved in SAVED_IN_REG.  */
@@ -763,6 +766,9 @@ cfi_row_equal_p (dw_cfi_row *a, dw_cfi_row *b)
         return false;
     }
 
+  if (a->ra_mangled != b->ra_mangled)
+    return false;
+
   return true;
 }
 
@@ -1368,6 +1374,21 @@ dwarf2out_frame_debug_cfa_window_save (void)
 
   cfi->dw_cfi_opc = DW_CFA_GNU_window_save;
   add_cfi (cfi);
+}
+
+/* A subroutine of dwarf2out_frame_debug, process a REG_CFA_TOGGLE_RA_MANGLE.
+   Note: DW_CFA_GNU_window_save dwarf opcode is reused for toggling RA mangle
+   state, this is a target specific operation on AArch64 and can only be used
+   on other targets if they don't use the window save operation otherwise.  */
+
+static void
+dwarf2out_frame_debug_cfa_toggle_ra_mangle (void)
+{
+  dw_cfi_ref cfi = new_cfi ();
+
+  cfi->dw_cfi_opc = DW_CFA_GNU_window_save;
+  add_cfi (cfi);
+  cur_row->ra_mangled = !cur_row->ra_mangled;
 }
 
 /* Record call frame debugging information for an expression EXPR,
@@ -2127,8 +2148,11 @@ dwarf2out_frame_debug (rtx_insn *insn)
 	break;
 
       case REG_CFA_TOGGLE_RA_MANGLE:
+	dwarf2out_frame_debug_cfa_toggle_ra_mangle ();
+	handled_one = true;
+	break;
+
       case REG_CFA_WINDOW_SAVE:
-	/* We overload both of these operations onto the same DWARF opcode.  */
 	dwarf2out_frame_debug_cfa_window_save ();
 	handled_one = true;
 	break;
@@ -2192,6 +2216,15 @@ change_cfi_row (dw_cfi_row *old_row, dw_cfi_row *new_row)
 	add_cfi_restore (i);
       else if (!cfi_equal_p (r_old, r_new))
         add_cfi (r_new);
+    }
+
+  if (old_row->ra_mangled != new_row->ra_mangled)
+    {
+      dw_cfi_ref cfi = new_cfi ();
+
+      /* DW_CFA_GNU_window_save is reused for toggling RA mangle state.  */
+      cfi->dw_cfi_opc = DW_CFA_GNU_window_save;
+      add_cfi (cfi);
     }
 }
 
