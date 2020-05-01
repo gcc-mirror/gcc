a5f2fb1ff1742685a91dfdf78da871fd4d3292e5
Jonathan Wakely <jwakely@redhat.com>
2020-05-01 14:27:25 +0100

    libstdc++: Replace deduced return type in ranges::iter_move (PR 92894)
    The deduced return type causes the instantiation of the function body,
    which can then require the instantiation of std::projected::operator*
    which is intentionally not defined.
    
    This patch uses a helper trait to define the return type, so that the
    function body doesn't need to be instantiated. That helper trait can
    then also be used in other places that currently check the return type
    of ranges::iter_move (iter_rvalue_reference_t and indirectly_readable).
    
    2020-05-01  Jonathan Wakely  <jwakely@redhat.com>
    	    Patrick Palka  <ppalka@redhat.com>
    
    	PR libstdc++/92894
    	* include/bits/iterator_concepts.h (ranges::__cust_imove::_IMove):
    	Add trait to determine return type and an alias for it.
    	(ranges::__cust_imove::_IMove::operator()): Use __result instead of
    	deduced return type.
    	(iter_rvalue_reference_t): Use _IMove::__type instead of checking
    	the result of ranges::iter_move.
    	(__detail::__indirectly_readable_impl): Use iter_rvalue_reference_t
    	instead of checking the result of ranges::iter_move.
    	* testsuite/24_iterators/customization_points/92894.cc: New test.
    	* testsuite/24_iterators/indirect_callable/92894.cc: New test.

---
+++ libstdc++-v3/testsuite/24_iterators/customization_points/92894.cc
@@ -0,0 +1,52 @@
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++2a" }
+// { dg-do compile { target c++2a } }
+
+#include <iterator>
+
+using namespace std;
+
+// Define our own of version of indirectly_readable_impl here,
+// to check the use of iter_move even if the real concept in
+// <bits/iterator_concepts.h> no longer uses iter_move.
+template<class In>
+concept indirectly_readable_impl
+  = requires(const In in)
+      {
+	typename iter_value_t<In>;
+	typename iter_reference_t<In>;
+	typename iter_rvalue_reference_t<In>;
+	{ *in } -> same_as<iter_reference_t<In>>;
+	{ ranges::iter_move(in) } -> same_as<iter_rvalue_reference_t<In>>;
+      };
+
+template<class T> requires indirectly_readable_impl<projected<T*, identity>>
+  void algo(T)
+  { }
+
+void
+test01()
+{
+  // PR libstdc++/92894
+  // Verify that the use of range::iter_move above doesn't cause odr-use of
+  // projected<local-class-type, identity>::operator* (which is not defined).
+  struct X { };
+  X a;
+  algo(a);
+}
