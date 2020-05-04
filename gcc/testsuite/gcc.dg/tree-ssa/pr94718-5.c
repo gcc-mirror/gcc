6b5c7ee0df6b87780f2fd6f2c5759a04e6eed1fe
Jakub Jelinek <jakub@redhat.com>
2020-05-04 11:03:32 +0200

    match.pd: Optimize (x < 0) != (y < 0) into (x ^ y) < 0 [PR94718]
    The following patch (on top of the two other PR94718 patches) performs the
    actual optimization requested in the PR.
    
    2020-05-04  Jakub Jelinek  <jakub@redhat.com>
    
    	PR tree-optimization/94718
    	* match.pd ((X < 0) != (Y < 0) into (X ^ Y) < 0): New simplification.
    
    	* gcc.dg/tree-ssa/pr94718-4.c: New test.
    	* gcc.dg/tree-ssa/pr94718-5.c: New test.

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr94718-5.c
@@ -0,0 +1,61 @@
+/* PR tree-optimization/94718 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fno-ipa-icf -fdump-tree-optimized" } */
+/* { dg-final { scan-tree-dump-times "= \[xy]_\[0-9]+\\\(D\\\) \\^ \[xy]_\[0-9]+\\\(D\\\);" 8 "optimized" } } */
+/* { dg-final { scan-tree-dump-times "\[0-9]+ >= 0;" 8 "optimized" } } */
+
+int
+f1 (int x, int y)
+{
+  return (x < 0) == (y < 0);
+}
+
+int
+f2 (int x, int y)
+{
+  return (x >= 0) == (y >= 0);
+}
+
+int
+f3 (int x, int y)
+{
+  return (x < 0) != (y >= 0);
+}
+
+int
+f4 (int x, int y)
+{
+  return (x >= 0) != (y < 0);
+}
+
+int
+f5 (int x, int y)
+{
+  int s = (x < 0);
+  int t = (y < 0);
+  return s == t;
+}
+
+int
+f6 (int x, int y)
+{
+  int s = (x >= 0);
+  int t = (y >= 0);
+  return s == t;
+}
+
+int
+f7 (int x, int y)
+{
+  int s = (x < 0);
+  int t = (y >= 0);
+  return s != t;
+}
+
+int
+f8 (int x, int y)
+{
+  int s = (x >= 0);
+  int t = (y < 0);
+  return s != t;
+}
