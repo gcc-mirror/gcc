f629b8dd43fc8aa2bf2a38cb75c0d8d499ac6d44
Richard Biener <rguenther@suse.de>
2020-05-05 13:09:50 +0200

    ipa/94947 - fix test for externally visible variables for IPA PTA
    This fixes lack of an escape point of externally declared variables.
    
    2020-05-05  Richard Biener  <rguenther@suse.de>
    
    	PR ipa/94947
    	* tree-ssa-structalias.c (ipa_pta_execute): Use
    	varpool_node::externally_visible_p ().
    	(refered_from_nonlocal_var): Likewise.
    
    	* gcc.dg/torture/pr94947-1.c: New testcase.
    	* gcc.dg/torture/pr94947-2.c: Likewise.

---
+++ gcc/testsuite/gcc.dg/torture/pr94947-1.c
@@ -0,0 +1,23 @@
+/* { dg-do run } */
+/* { dg-additional-sources "pr94947-2.c" } */
+/* { dg-additional-options "-fipa-pta -flto-partition=1to1" } */
+
+extern void abort ();
+extern void baz ();
+extern void (*baz_call)();
+static int *p;
+
+static void foo ()
+{
+  if (*p != 1)
+    abort ();
+}
+
+int main()
+{
+  int x = 1;
+  p = &x;
+  baz_call = foo;
+  baz ();
+  return 0;
+}
