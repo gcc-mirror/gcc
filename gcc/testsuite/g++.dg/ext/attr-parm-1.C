702d7b71a86a29d52b10d33ac8f0ea0eedcab27f
Nathan Sidwell <nathan@acm.org>
2020-05-06 12:07:30 -0700

    c++: QT overload regression with attribute [PR94946]
    Jason's fix for 90570 & 79585 was a bit overzealous.  Dependent attribs should still
    attach to a parameter decl.
    
    	* decl.c (grokdeclarator): Don't splice template attributes in
    	parm context -- they can apply to the parm.

---
+++ gcc/testsuite/g++.dg/ext/attr-parm-1.C
@@ -0,0 +1,7 @@
+// { dg-do compile { target { i?86-*-* x86_64-*-* } } }
+// { dg-options -m32 }
+// PR 94946
+class a {
+  template <typename b> a(b (*)());
+  template <typename b> a(b(__attribute__((fastcall)) *c)());
+};
