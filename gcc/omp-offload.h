a0875043b032087d169e7dd726c34f3659ded205
Jakub Jelinek <jakub@redhat.com>
2020-05-12 09:17:09 +0200

    openmp: Implement discovery of implicit declare target to clauses
    This attempts to implement what the OpenMP 5.0 spec in declare target section
    says as ammended by the 5.1 changes so far (related to device_type(host)), except
    that it doesn't have the device(ancestor: ...) handling yet because we do not
    support it yet, and I've left so far out the except lambda note, because I need
    that clarified.
    
    2020-05-12  Jakub Jelinek  <jakub@redhat.com>
    
    	* omp-offload.h (omp_discover_implicit_declare_target): Declare.
    	* omp-offload.c: Include context.h.
    	(omp_declare_target_fn_p, omp_declare_target_var_p,
    	omp_discover_declare_target_fn_r, omp_discover_declare_target_var_r,
    	omp_discover_implicit_declare_target): New functions.
    	* cgraphunit.c (analyze_functions): Call
    	omp_discover_implicit_declare_target.
    
    	* testsuite/libgomp.c/target-39.c: New test.

--- gcc/omp-offload.h
+++ gcc/omp-offload.h
@@ -30,5 +30,6 @@ extern GTY(()) vec<tree, va_gc> *offload_funcs;
 extern GTY(()) vec<tree, va_gc> *offload_vars;
 
 extern void omp_finish_file (void);
+extern void omp_discover_implicit_declare_target (void);
 
 #endif /* GCC_OMP_DEVICE_H */
