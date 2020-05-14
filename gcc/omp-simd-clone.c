38484dac88b157151db984790be0582bf66adcfb
Jakub Jelinek <jakub@redhat.com>
2020-05-14 09:51:05 +0200

    openmp: Fix placement of 2nd+ preparation statement for PHIs in simd clone lowering [PR95108]
    For normal stmts, preparation statements are inserted before the stmt, so if we need multiple,
    they are in the correct order, but for PHIs we emit them after labels in the entry successor
    bb, and we used to emit them in the reverse order that way.
    
    2020-05-14  Jakub Jelinek  <jakub@redhat.com>
    
    	PR middle-end/95108
    	* omp-simd-clone.c (struct modify_stmt_info): Add after_stmt member.
    	(ipa_simd_modify_stmt_ops): For PHIs, only add before first stmt in
    	entry block if info->after_stmt is NULL, otherwise add after that stmt
    	and update it after adding each stmt.
    	(ipa_simd_modify_function_body): Initialize info.after_stmt.
    
    	* gcc.dg/gomp/pr95108.c: New test.
    
    (cherry picked from commit d0fb9ffc1b8f3b86bbdf0e915cec2136141b329b)

--- gcc/omp-simd-clone.c
+++ gcc/omp-simd-clone.c
@@ -816,6 +816,7 @@ simd_clone_init_simd_arrays (struct cgraph_node *node,
 struct modify_stmt_info {
   ipa_parm_adjustment_vec adjustments;
   gimple *stmt;
+  gimple *after_stmt;
   /* True if the parent statement was modified by
      ipa_simd_modify_stmt_ops.  */
   bool modified;
@@ -899,7 +900,10 @@ ipa_simd_modify_stmt_ops (tree *tp, int *walk_subtrees, void *data)
       gimple_stmt_iterator gsi;
       if (gimple_code (info->stmt) == GIMPLE_PHI)
 	{
-	  gsi = gsi_after_labels (single_succ (ENTRY_BLOCK_PTR_FOR_FN (cfun)));
+	  if (info->after_stmt)
+	    gsi = gsi_for_stmt (info->after_stmt);
+	  else
+	    gsi = gsi_after_labels (single_succ (ENTRY_BLOCK_PTR_FOR_FN (cfun)));
 	  /* Cache SSA_NAME for next time.  */
 	  if (cand
 	      && TREE_CODE (*orig_tp) == ADDR_EXPR
@@ -908,7 +912,12 @@ ipa_simd_modify_stmt_ops (tree *tp, int *walk_subtrees, void *data)
 	}
       else
 	gsi = gsi_for_stmt (info->stmt);
-      gsi_insert_before (&gsi, stmt, GSI_SAME_STMT);
+      if (info->after_stmt)
+	gsi_insert_after (&gsi, stmt, GSI_SAME_STMT);
+      else
+	gsi_insert_before (&gsi, stmt, GSI_SAME_STMT);
+      if (gimple_code (info->stmt) == GIMPLE_PHI)
+	info->after_stmt = stmt;
       *orig_tp = repl;
     }
   else if (!useless_type_conversion_p (TREE_TYPE (*tp), TREE_TYPE (repl)))
@@ -1013,6 +1022,7 @@ ipa_simd_modify_function_body (struct cgraph_node *node,
 	  gphi *phi = as_a <gphi *> (gsi_stmt (gsi));
 	  int i, n = gimple_phi_num_args (phi);
 	  info.stmt = phi;
+	  info.after_stmt = NULL;
 	  struct walk_stmt_info wi;
 	  memset (&wi, 0, sizeof (wi));
 	  info.modified = false;
@@ -1038,6 +1048,7 @@ ipa_simd_modify_function_body (struct cgraph_node *node,
 	{
 	  gimple *stmt = gsi_stmt (gsi);
 	  info.stmt = stmt;
+	  info.after_stmt = NULL;
 	  struct walk_stmt_info wi;
 
 	  memset (&wi, 0, sizeof (wi));
