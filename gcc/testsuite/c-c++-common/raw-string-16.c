ed63c387aa0bc1846082524455a6ff1fcec40f9d
Nathan Sidwell <nathan@acm.org>
2020-05-19 11:28:05 -0700

    preprocessor: Reimplement raw string lexing [pr95149]
    pr95149 is a false positive static analysis checker.  But it
    encouranged me to fix raw string lexing, which does contain a
    complicated macro and pointers to local variables.  The
    reimplementation does away with that macro.  Part of the complication
    is we need to undo some of the fresh line processing -- trigraph notes
    and escaped line continuations.  But the undone characters need to go
    through the raw string processing, as they can legitimately be part of
    the prefix marker.  however, in this reformulation we only process one
    line marker at a time[*], so there's a limited number of undone
    characters.  We can arrange the buffering to make sure we don't split
    such an append sequence, and then simply take the characters from the
    append buffer.
    
    The prefix scanner had a switch statement, which I discovered was not
    optimized as well as an if of a bunch of explicit comparisons (pr
    95208 filed).
    
    Finally I adjusted the failure mode.  When we get a bad prefix, we lex
    up until the next '"', thus often swallowing the whole raw string.
    Previously we'd bail and then the lexer would usually generate stupid
    tokens, particularly when meeting the ending '"'.
    
    	libcpp/
    	* lex.c (struct lit_accum): New.
    	(bufring_append): Replace by lit_accum::append.
    	(lex_raw_string): Reimplement, using fragments of the old version.
    	(lex_string): Adjust lex_raw_string call.
    
    	gcc/testsuite/
    	* c-c++-common/raw-string-14.c: Adjust errors.
    	* c-c++-common/raw-string-16.c: Likewise.
    	* c-c++-common/raw-string-5.c: Likewise.

--- gcc/testsuite/c-c++-common/raw-string-16.c
+++ gcc/testsuite/c-c++-common/raw-string-16.c
@@ -4,29 +4,29 @@
 // { dg-options "-std=gnu++11 -Wtrigraphs" { target c++ } }
 
 const void *s0 = R"abc\
-def()abcdef";
+def()abcdef" 0;
 	// { dg-error "invalid character" "invalid" { target *-*-* } 6 }
 	// { dg-error "stray" "stray" { target *-*-* } 6 }
-const void *s1 = R"abcdefghijklmn??/(a)abcdefghijklmn???";
+const void *s1 = R"abcdefghijklmn??/(a)abcdefghijklmn???" 0;
 	// { dg-error "raw string delimiter longer" "longer" { target *-*-* } .-1 }
 	// { dg-error "stray" "stray" { target *-*-* } .-2 }
 
-const void *s2 = R"abcdefghijklmno??/(a)abcdefghijklmno???";
+const void *s2 = R"abcdefghijklmno??/(a)abcdefghijklmno???" 0;
 	// { dg-error "raw string delimiter longer" "longer" { target *-*-* } .-1 }
 	// { dg-error "stray" "stray" { target *-*-* } .-2 }
-const void *s3 = R"abcdefghijklmnop??=(a)abcdefghijklmnop??=?"; // { dg-warning "trigraph ..= ignored" }
+const void *s3 = R"abcdefghijklmnop??=(a)abcdefghijklmnop??=?" 0;
 	// { dg-error "raw string delimiter longer" "longer" { target *-*-* } .-1 }
 	// { dg-error "stray" "stray" { target *-*-* } .-2 }
 const void *s4 = R"abc\
-()abcdef";
+()abcdef" 0;
 	// { dg-error "invalid character" "invalid" { target *-*-* } 20 }
 	// { dg-error "stray" "stray" { target *-*-* } 20 }
 const void *s5 = R"\
-()";
+()" 0;
 	// { dg-error "invalid character" "invalid" { target *-*-* } 24 }
 	// { dg-error "stray" "stray" { target *-*-* } 24 }
 const void *s6 = R"\
-a()a";
+a()a" 0;
 	// { dg-error "invalid character" "invalid" { target *-*-* } 28 }
 	// { dg-error "stray" "stray" { target *-*-* } 28 }
 
