ca21d0126befaf0e3ad3d011b82028bc3d9b3935
Nathan Sidwell <nathan@acm.org>
2020-05-20 11:15:34 -0700

    preprocessor: cleanups in c-common handling
    	* c-common.c (try_to_locate_new_include_insertion_point): Use
    	strcmp to compare filenames.
    	* c-lex.c (init_c_lex): Move declaration to initialization.
    	* c-opts.c (handle_deferred_opts): Move cpp_get_deps call into
    	deferred count loop.

--- gcc/c-family/c-common.c
+++ gcc/c-family/c-common.c
@@ -8708,7 +8708,8 @@ try_to_locate_new_include_insertion_point (const char *file, location_t loc)
 	    last_ord_map_after_include = NULL;
 	  }
 
-      if (ord_map->to_file == file)
+      if (0 == strcmp (ord_map->to_file, file)
+	  && ord_map->to_line)
 	{
 	  if (!first_ord_map_in_file)
 	    first_ord_map_in_file = ord_map;
