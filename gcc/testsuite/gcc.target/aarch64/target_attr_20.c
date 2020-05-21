a7a0531e37f9c17503dc8aaf937afe46a0e340d7
Martin Liska <mliska@suse.cz>
2020-05-21 12:45:47 +0200

    Add outline-atomics to target attribute.
    	* common/config/aarch64/aarch64-common.c (aarch64_handle_option):
    	Handle OPT_moutline_atomics.
    	* config/aarch64/aarch64.c: Add outline-atomics to
    	aarch64_attributes.
    
    	* doc/extend.texi: Document the newly added target attribute.
    
    	* gcc.target/aarch64/target_attr_20.c: New test.
    	* gcc.target/aarch64/target_attr_21.c: New test.

---
+++ gcc/testsuite/gcc.target/aarch64/target_attr_20.c
@@ -0,0 +1,27 @@
+/* { dg-do compile } */
+/* { dg-options "-march=armv8-a+nolse -moutline-atomics" } */
+
+int b, c, d, e, f, h;
+short g;
+int foo (int) __attribute__ ((__const__));
+
+__attribute__ ((target ("no-outline-atomics")))
+void
+bar (void)
+{
+  while (1)
+    {
+      while (1)
+	{
+	  __atomic_load_n (&e, 0);
+	  if (foo (2))
+	    __sync_val_compare_and_swap (&c, 0, f);
+	  b = 1;
+	  if (h == e)
+	    break;
+	}
+      __sync_val_compare_and_swap (&g, -1, f);
+    }
+}
+
+/* { dg-final { scan-assembler-not "bl.*__aarch64_cas2_acq_rel" } } */
