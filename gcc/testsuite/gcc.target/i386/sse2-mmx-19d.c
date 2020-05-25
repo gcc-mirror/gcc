4f4cf8690099eeac9fd7ab20a8403dcfc4b640f9
Uros Bizjak <ubizjak@gmail.com>
2020-05-25 16:11:18 +0200

    i386: Remove broadcasts from TARGET_MMX_WITH_SSE vec_dup insn patterns
    XMM broadcast instructions broadcast value from general reg to all
    elements of the vector.  This is not allowed for TARGET_MMX_WITH_SSE,
    where it is expected that bits outside lower 64bits load or retain
    zero value.  Following testcases expect broadcast, and are thus invalid:
    
    FAIL: gcc.target/i386/sse2-mmx-18b.c scan-assembler-not movd
    FAIL: gcc.target/i386/sse2-mmx-18b.c scan-assembler-times pbroadcastd 1
    FAIL: gcc.target/i386/sse2-mmx-19b.c scan-assembler-not movd
    FAIL: gcc.target/i386/sse2-mmx-19b.c scan-assembler-times pbroadcastw 1
    FAIL: gcc.target/i386/sse2-mmx-19d.c scan-assembler-times pbroadcastw 1
    FAIL: gcc.target/i386/sse2-mmx-19e.c scan-assembler-times pbroadcastw 1
    
    These testcases are removed entirely.
    
    2020-05-25  Uroš Bizjak  <ubizjak@gmail.com>
    
    gcc/ChangeLog:
    	* config/i386/mmx.md (*vec_dupv2sf): Redefine as define_insn.
    	(mmx_pshufw_1): Change Yv constraint to xYw.  Correct type attribute.
    	(*vec_dupv4hi): Redefine as define_insn.
    	Remove alternative with general register input.
    	(*vec_dupv2si): Ditto.
    
    gcc/testsuite/ChangeLog:
    	* gcc.target/i386/sse2-mmx-18a.c (dg-options): Remove -mno-avx512vl.
    	* gcc.target/i386/sse2-mmx-19a.c (dg-options): Remove -mno-avx.
    	* gcc.target/i386/sse2-mmx-18b.c: Remove.
    	* gcc.target/i386/sse2-mmx-18c.c: Ditto.
    	* gcc.target/i386/sse2-mmx-19b.c: Ditto.
    	* gcc.target/i386/sse2-mmx-19c.c: Ditto.
    	* gcc.target/i386/sse2-mmx-19d.c: Ditto.
    	* gcc.target/i386/sse2-mmx-19e.c: Ditto.

--- gcc/testsuite/gcc.target/i386/sse2-mmx-19d.c
+++
@@ -1,7 +0,0 @@
-/* { dg-do compile { target { ! ia32 } } } */
-/* { dg-options "-O2 -mno-mmx -mavx512bw -mno-avx512vl -mtune=intel" } */
-/* { dg-final { scan-assembler-times "pbroadcastw" 1 } } */
-/* { dg-final { scan-assembler-times "movd" 1 } } */
-/* { dg-final { scan-assembler-not "movl" } } */
-
-#include "sse2-mmx-19a.c"
--- gcc/testsuite/gcc.target/i386/sse2-mmx-19e.c
+++
@@ -1,7 +0,0 @@
-/* { dg-do compile { target { ! ia32 } } } */
-/* { dg-options "-O2 -mno-mmx -mavx2 -mno-avx512f -mtune=intel" } */
-/* { dg-final { scan-assembler-times "pbroadcastw" 1 } } */
-/* { dg-final { scan-assembler-times "movd" 1 } } */
-/* { dg-final { scan-assembler-not "movl" } } */
-
-#include "sse2-mmx-19a.c"
