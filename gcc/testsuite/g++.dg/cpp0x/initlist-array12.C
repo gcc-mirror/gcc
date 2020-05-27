8bab5c832ed9749f5f93cc2b86b1f797d6838c31
Jason Merrill <jason@redhat.com>
2020-05-27 10:27:25 -0400

    c++: Handle multiple aggregate overloads [PR95319].
    Here, when considering the two 'insert' overloads, we look for aggregate
    conversions from the same initializer-list to B<3> or
    initializer_list<B<3>>.  But since my fix for reshape_init overhead on the
    PR14179 testcase we reshaped the initializer-list directly, leading to an
    error when we then tried to reshape it differently for the second overload.
    
    gcc/cp/ChangeLog:
    
    	PR c++/95319
    	* decl.c (reshape_init_array_1): Don't reuse in overload context.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/95319
    	* g++.dg/cpp0x/initlist-array12.C: New test.

---
+++ gcc/testsuite/g++.dg/cpp0x/initlist-array12.C
@@ -0,0 +1,24 @@
+// PR c++/95319
+// { dg-do compile { target c++11 } }
+
+namespace std {
+template <class> class initializer_list {
+  int *_M_array;
+  unsigned long _M_len;
+};
+template <int _Nm> struct A { typedef int _Type[_Nm]; };
+template <int _Nm> struct B { typename A<_Nm>::_Type _M_elems; };
+class C {
+public:
+  void insert(int, B<3>);
+  void insert(int, initializer_list<B<3>>);
+};
+} // namespace std
+int a;
+int
+main() {
+  using ArrayVector = std::C;
+  auto b = ArrayVector();
+  b.insert(a, {{2}});
+  return 0;
+}
