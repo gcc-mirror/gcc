4b2fc5f7fc3593f5526a12b25cd0437f41fc047f
Jason Merrill <jason@redhat.com>
2020-05-28 00:35:56 -0400

    c++: Immediately deduce auto member [PR94926].
    In r9-297 I was trying to be more flexible and treat static data members of
    class templates more like variable templates, where the type need not be
    determined until the variable is instantiated, but I suppose that in a class
    the types of all the non-template members need to be determined at the time
    of class instantiation.
    
    gcc/cp/ChangeLog:
    
    	PR c++/94926
    	* decl.c (cp_finish_decl): Revert r9-297 change.
    	(check_static_variable_definition): Likewise.
    	* constexpr.c (ensure_literal_type_for_constexpr_object): Likewise.
    	* pt.c (instantiate_decl): Return early on type error.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp1z/pr86648.C: Expect error.
    	* g++.dg/cpp1z/static2.C: Expect error.
    	* g++.dg/cpp0x/nsdmi16.C: New test.

--- gcc/cp/decl.c
+++ gcc/cp/decl.c
@@ -7467,18 +7467,24 @@ cp_finish_decl (tree decl, tree init, bool init_const_expr_p,
       && (DECL_INITIAL (decl) || init))
     DECL_INITIALIZED_IN_CLASS_P (decl) = 1;
 
-  /* Do auto deduction unless decl is a function or an uninstantiated
-     template specialization.  */
   if (TREE_CODE (decl) != FUNCTION_DECL
-      && !(init == NULL_TREE
-	   && DECL_LANG_SPECIFIC (decl)
-	   && DECL_TEMPLATE_INSTANTIATION (decl)
-	   && !DECL_TEMPLATE_INSTANTIATED (decl))
       && (auto_node = type_uses_auto (type)))
     {
       tree d_init;
       if (init == NULL_TREE)
-	gcc_assert (CLASS_PLACEHOLDER_TEMPLATE (auto_node));
+	{
+	  if (DECL_LANG_SPECIFIC (decl)
+	      && DECL_TEMPLATE_INSTANTIATION (decl)
+	      && !DECL_TEMPLATE_INSTANTIATED (decl))
+	    {
+	      /* init is null because we're deferring instantiating the
+		 initializer until we need it.  Well, we need it now.  */
+	      instantiate_decl (decl, /*defer_ok*/true, /*expl*/false);
+	      return;
+	    }
+
+	  gcc_assert (CLASS_PLACEHOLDER_TEMPLATE (auto_node));
+	}
       d_init = init;
       if (d_init)
 	{
@@ -10171,7 +10177,6 @@ check_static_variable_definition (tree decl, tree type)
      in check_initializer.  Similarly for inline static data members.  */
   else if (DECL_P (decl)
       && (DECL_DECLARED_CONSTEXPR_P (decl)
-	  || undeduced_auto_decl (decl)
 	  || DECL_VAR_DECLARED_INLINE_P (decl)))
     ;
   else if (cxx_dialect >= cxx11 && !INTEGRAL_OR_ENUMERATION_TYPE_P (type))
