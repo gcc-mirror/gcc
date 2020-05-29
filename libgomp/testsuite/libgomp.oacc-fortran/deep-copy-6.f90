2e25babc5c94e30b7a218856434f01150a6c2ae2
Thomas Schwinge <thomas@codesourcery.com>
2020-05-29 15:22:42 +0200

    [OpenACC] Repair/restore 'is_tgt_unmapped' checking
    	libgomp/
    	* oacc-mem.c (goacc_exit_datum): Repair 'is_tgt_unmapped'
    	checking.
    	(acc_unmap_data, goacc_exit_data_internal): Restore
    	'is_tgt_unmapped' checking.
    	* testsuite/libgomp.oacc-c-c++-common/struct-refcount-1.c: New
    	file.
    	* testsuite/libgomp.oacc-fortran/deep-copy-6.f90: Adjust.
    	* testsuite/libgomp.oacc-fortran/mdc-refcount-1-1-1.f90: Likewise.
    	* testsuite/libgomp.oacc-fortran/mdc-refcount-1-2-1.f90: Likewise.
    	* testsuite/libgomp.oacc-fortran/mdc-refcount-1-2-2.f90: Likewise.
    	* testsuite/libgomp.oacc-fortran/mdc-refcount-1-3-1.f90: Likewise.
    	* testsuite/libgomp.oacc-fortran/mdc-refcount-1-4-1.f90: Likewise.
    
    Co-Authored-By: Julian Brown <julian@codesourcery.com>

--- libgomp/testsuite/libgomp.oacc-fortran/deep-copy-6.f90
+++ libgomp/testsuite/libgomp.oacc-fortran/deep-copy-6.f90
@@ -40,19 +40,20 @@ program dtype
   if (.not. acc_is_present(var%a(5:n - 5))) stop 11
   if (.not. acc_is_present(var%b(5:n - 5))) stop 12
   if (.not. acc_is_present(var)) stop 13
-!$acc exit data copyout(var%a(5:n - 5), var%b(5:n - 5)) finalize
-  if (acc_get_device_type() .ne. acc_device_host) then
-     if (acc_is_present(var%a(5:n - 5))) stop 21
-     if (acc_is_present(var%b(5:n - 5))) stop 22
-  end if
   print *, "CheCKpOInT1"
   ! { dg-output ".*CheCKpOInT1(\n|\r\n|\r)" }
-  if (.not. acc_is_present(var)) stop 23
-  !TODO { dg-output "STOP 23(\n|\r\n|\r)$" { target { ! openacc_host_selected } } } ! Scan for what we expect in the "XFAILed" case (without actually XFAILing).
+!$acc exit data copyout(var%a(5:n - 5), var%b(5:n - 5)) finalize
+  !TODO     goacc_exit_data_internal: Assertion `is_tgt_unmapped || num_mappings > 1' failed.
+  !TODO { dg-output ".*\[Aa\]ssert.*is_tgt_unmapped" { target { ! openacc_host_selected } } } ! Scan for what we expect in the "XFAILed" case (without actually XFAILing).
   !TODO { dg-shouldfail "XFAILed" { ! openacc_host_selected } } ! ... instead of 'dg-xfail-run-if' so that 'dg-output' is evaluated at all.
   !TODO { dg-final { if { [dg-process-target { xfail { ! openacc_host_selected } }] == "F" } { xfail "[testname-for-summary] really is XFAILed" } } } ! ... so that we still get an XFAIL visible in the log.
   print *, "CheCKpOInT2"
   ! { dg-output ".CheCKpOInT2(\n|\r\n|\r)" { target { openacc_host_selected } } }
+  if (acc_get_device_type() .ne. acc_device_host) then
+     if (acc_is_present(var%a(5:n - 5))) stop 21
+     if (acc_is_present(var%b(5:n - 5))) stop 22
+  end if
+  if (.not. acc_is_present(var)) stop 23
 
 !$acc end data
 
