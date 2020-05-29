95625d3580489b27c6e2b156a0bbc71842f9b59e
Iain Buclaw <ibuclaw@gdcproject.org>
2020-05-29 18:12:55 +0200

    contrib: Remove cris-linux and crisv32-* from config-list.mk
    Support for crisv32-*-* and cris-*-linux* was removed in git r11-214.
    
    contrib/ChangeLog:
    
    	* config-list.mk (LIST): Remove cris-linux, crisv32-elf, and
    	crisv32-linux.

--- contrib/config-list.mk
+++ contrib/config-list.mk
@@ -41,7 +41,7 @@ LIST = aarch64-elf aarch64-linux-gnu aarch64-rtems \
   arm-symbianelf avr-elf \
   bfin-elf bfin-uclinux bfin-linux-uclibc bfin-rtems bfin-openbsd \
   bpf-unknown-none \
-  c6x-elf c6x-uclinux cr16-elf cris-elf cris-linux crisv32-elf crisv32-linux \
+  c6x-elf c6x-uclinux cr16-elf cris-elf \
   csky-elf csky-linux-gnu \
   epiphany-elf epiphany-elfOPT-with-stack-offset=16 fido-elf \
   fr30-elf frv-elf frv-linux ft32-elf h8300-elf hppa-linux-gnu \
