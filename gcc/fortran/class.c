75f5824b77632ef583a7a65302b326e8bf1b851c
Thomas Koenig <tkoenig@gcc.gnu.org>
2020-05-31 10:26:40 +0200

    Finalization depends on the expression, not on the component.
    This patch fixes a 8/9/10/11 regression, where finalized types
    were not finalized (and deallocated), which led to memory
    leaks.
    
    gcc/fortran/ChangeLog:
    
    2020-05-24  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/94361
    	* class.c (finalize_component): Use expr->finalized instead of
    	comp->finalized.
    	* gfortran.h (gfc_component): Remove finalized member.
    	(gfc_expr): Add it here instead.
    
    gcc/testsuite/ChangeLog:
    
    2020-05-24  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/94361
    	* gfortran.dg/finalize_28.f90: Adjusted free counts.
    	* gfortran.dg/finalize_34.f90: Likewise.
    	* gfortran.dg/finalize_35.f90: New test.
    
    (cherry picked from commit 2a0de88ccf2ecc6659b0f1e4580c1ce5cff976b3)

--- gcc/fortran/class.c
+++ gcc/fortran/class.c
@@ -907,7 +907,7 @@ finalize_component (gfc_expr *expr, gfc_symbol *derived, gfc_component *comp,
   if (!comp_is_finalizable (comp))
     return;
 
-  if (comp->finalized)
+  if (expr->finalized)
     return;
 
   e = gfc_copy_expr (expr);
@@ -998,6 +998,7 @@ finalize_component (gfc_expr *expr, gfc_symbol *derived, gfc_component *comp,
 	}
       else
 	(*code) = cond;
+
     }
   else if (comp->ts.type == BT_DERIVED
 	    && comp->ts.u.derived->f2k_derived
@@ -1037,7 +1038,7 @@ finalize_component (gfc_expr *expr, gfc_symbol *derived, gfc_component *comp,
 			    sub_ns);
       gfc_free_expr (e);
     }
-  comp->finalized = true;
+  expr->finalized = 1;
 }
 
 
