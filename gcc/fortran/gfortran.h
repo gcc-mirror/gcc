49e29cdd6193a2f2d3cbb6c40a732d7e433f99a0
Thomas Koenig <tkoenig@gcc.gnu.org>
2020-05-31 10:26:40 +0200

    Finalization depends on the expression, not on the component.
    This patch fixes a 8/9/10/11 regression, where finalized types
    were not finalized (and deallocated), which led to memory
    leaks.
    
    gcc/fortran/ChangeLog:
    
    2020-05-24  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/94361
    	* class.c (finalize_component): Use expr->finalized instead of
    	comp->finalized.
    	* gfortran.h (gfc_component): Remove finalized member.
    	(gfc_expr): Add it here instead.
    
    gcc/testsuite/ChangeLog:
    
    2020-05-24  Thomas Koenig  <tkoenig@gcc.gnu.org>
    
    	PR fortran/94361
    	* gfortran.dg/finalize_28.f90: Adjusted free counts.
    	* gfortran.dg/finalize_33.f90: Likewise.
    	* gfortran.dg/finalize_34.f90: Likewise.
    	* gfortran.dg/finalize_35.f90: New test.

--- gcc/fortran/gfortran.h
+++ gcc/fortran/gfortran.h
@@ -1107,7 +1107,6 @@ typedef struct gfc_component
   struct gfc_typebound_proc *tb;
   /* When allocatable/pointer and in a coarray the associated token.  */
   tree caf_token;
-  bool finalized;
 }
 gfc_component;
 
@@ -2217,6 +2216,9 @@ typedef struct gfc_expr
 
   /* Set this if the expression came from expanding an array constructor.  */
   unsigned int from_constructor : 1;
+
+  /* Set this if the expression has already been finalized.  */
+  unsigned int finalized : 1;
 
   /* If an expression comes from a Hollerith constant or compile-time
      evaluation of a transfer statement, it may have a prescribed target-
