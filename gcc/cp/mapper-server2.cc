d34b6baae9112cb69e1c9ae2e7f44077fcd01400
Nathan Sidwell <nathan@acm.org>
2020-06-02 07:32:58 -0700

    Add libcody with stub users
    	* libcody: New.  Currently a symlink to cody repo.
    	* Makefile.def: Add libcody.
    	* configure.ac: Add libcody.
    	* Makefile.in: Rebuilt.
    	* configure: Rebuilt.
    	gcc/
    	* Makefile.in: Add libcody.
    	gcc/cp/
    	* Make-lang.in: Add libcody
    	* mapper.h: New stub/
    	* mapper-resolver.cc: New stub.
    	* mapper-server2.cc: New stub.

---
+++ gcc/cp/mapper-server2.cc
@@ -0,0 +1,25 @@
+/* C++ modules.  Experimental!
+   Copyright (C) 2018-2020 Free Software Foundation, Inc.
+   Written by Nathan Sidwell <nathan@acm.org> while at FaceBook
+
+   This file is part of GCC.
+
+   GCC is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+
+   GCC is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+// FIXME: temporarily named server2 while the original is still around
+
+#include "config.h"
+#include "mapper.h"
+
