7761ccb7361f322538318f364dbcbd1dd3db72b2
Iain Buclaw <ibuclaw@gdcproject.org>
2020-06-04 22:05:49 +0200

    d: Merge upstream dmd 47ed0330f
    Updates the copyright years of the front-end sources.
    
    Reviewed-on: https://github.com/dlang/dmd/pull/11221
    
    gcc/d/ChangeLog:
    
    	* dmd/MERGE: Merge upstream dmd 47ed0330f.

--- gcc/d/dmd/root/array.h
+++ gcc/d/dmd/root/array.h
@@ -1,4 +1,5 @@
-/* Copyright (C) 2011-2019 by The D Language Foundation, All Rights Reserved
+/* Copyright (C) 2011-2020 by The D Language Foundation, All Rights Reserved
+ * written by Walter Bright
  * http://www.digitalmars.com
  * Distributed under the Boost Software License, Version 1.0.
  * http://www.boost.org/LICENSE_1_0.txt
