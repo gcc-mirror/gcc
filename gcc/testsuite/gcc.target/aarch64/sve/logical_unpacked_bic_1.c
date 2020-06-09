bb3ab62a8b4a108f01ea2eddfe31e9f733bd9cb6
Joe Ramsay <joe.ramsay@arm.com>
2020-06-09 12:23:56 +0100

    AArch64+SVE: Add support for unpacked unary ops and BIC
    MD patterns extended for unary ops ABS, CLS, CLZ, CNT, NEG and NOT
    to support unpacked vectors. Also extended patterns for BIC to
    support unpacked vectors where input elements are of the same width.
    
    gcc/ChangeLog:
    
    2020-06-09  Joe Ramsay  <joe.ramsay@arm.com>
    
    	* config/aarch64/aarch64-sve.md (<optab><mode>2): Add support for
    	unpacked vectors.
    	(@aarch64_pred_<optab><mode>): Add support for unpacked vectors.
    	(@aarch64_bic<mode>): Enable unpacked BIC.
    	(*bic<mode>3): Enable unpacked BIC.
    
    gcc/testsuite/ChangeLog:
    
    2020-06-09  Joe Ramsay  <joe.ramsay@arm.com>
    
    	* gcc.target/aarch64/sve/logical_unpacked_abs.c: New test.
    	* gcc.target/aarch64/sve/logical_unpacked_bic_1.c: New test.
    	* gcc.target/aarch64/sve/logical_unpacked_bic_2.c: New test.
    	* gcc.target/aarch64/sve/logical_unpacked_bic_3.c: New test.
    	* gcc.target/aarch64/sve/logical_unpacked_bic_4.c: New test.
    	* gcc.target/aarch64/sve/logical_unpacked_neg.c: New test.
    	* gcc.target/aarch64/sve/logical_unpacked_not.c: New test.

---
+++ gcc/testsuite/gcc.target/aarch64/sve/logical_unpacked_bic_1.c
@@ -0,0 +1,15 @@
+/* { dg-options "-O3 -msve-vector-bits=256" } */
+
+#include <stdint.h>
+
+void
+f (uint64_t *restrict dst, uint32_t *restrict src1, uint32_t *restrict src2)
+{
+  for (int i = 0; i < 3; ++i)
+    dst[i] = (uint32_t) (src1[i] & ~src2[i]);
+}
+
+/* { dg-final { scan-assembler-times {\tld1w\tz[0-9]+\.d,} 2 } } */
+/* { dg-final { scan-assembler-times {\tbic\tz[0-9]+\.d, z[0-9]+\.d, z[0-9]+\.d\n} 1 } } */
+/* { dg-final { scan-assembler-times {\tuxtw\tz[0-9]+\.d,} 1 } } */
+/* { dg-final { scan-assembler-times {\tst1d\tz[0-9]+\.d,} 1 } } */
