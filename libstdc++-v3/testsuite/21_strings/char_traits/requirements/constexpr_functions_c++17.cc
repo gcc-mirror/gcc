6e4c7cc8d90317a8e706fe35db0d4290367474e7
Jonathan Wakely <jwakely@redhat.com>
2020-06-15 14:31:26 +0100

    libstdc++: Update value of __cpp_lib_constexpr_char_traits for C++20
    Although not required by SD-6 or the C++20 draft, we define the macro
    __cpp_lib_constexpr_char_traits to indicate support for P0432R1. This
    updates the value in C++20 mode for the P1032R1 changes to char_traits.
    
    	* include/bits/char_traits.h (__cpp_lib_constexpr_char_traits):
    	Update value for C++20.
    	* include/std/version (__cpp_lib_constexpr_char_traits): Likewise.
    	* testsuite/21_strings/char_traits/requirements/constexpr_functions_c++17.cc:
    	Update expected value.
    
    (cherry picked from commit b6ab9ecd550227684643b41e9e33a4d3466724d8)

--- libstdc++-v3/testsuite/21_strings/char_traits/requirements/constexpr_functions_c++17.cc
+++ libstdc++-v3/testsuite/21_strings/char_traits/requirements/constexpr_functions_c++17.cc
@@ -75,8 +75,10 @@ template<typename CT>
 
 #ifndef __cpp_lib_constexpr_char_traits
 # error Feature-test macro for constexpr char_traits is missing
-#elif __cpp_lib_constexpr_char_traits != 201611
+#elif __cpp_lib_constexpr_char_traits < 201611
 # error Feature-test macro for constexpr char_traits has the wrong value
+#elif __cpp_lib_constexpr_char_traits > 201611 && __cplusplus == 201703
+# error Feature-test macro for constexpr char_traits has wrong value for C++17
 #endif
 
 static_assert( test_assign<std::char_traits<char>>() );
