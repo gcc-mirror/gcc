8c1347013941b49c861d3e3a027195cf4a3409f4
Jonathan Wakely <jwakely@redhat.com>
2020-06-16 22:34:55 +0100

    libstdc++: Enforce copyable/movable checks in std::atomic
    C++20 adds some new preconditions to std::atomic, which weren't
    previously checked by our implementation.
    
    	* include/std/atomic (atomic): Add static assertions.
    	* testsuite/29_atomics/atomic/requirements/types_neg.cc: New test.
    
    (cherry picked from commit cc799df98f6eaf73763a069f7854e490a0416573)

---
+++ libstdc++-v3/testsuite/29_atomics/atomic/requirements/types_neg.cc
@@ -0,0 +1,43 @@
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++2a" }
+// { dg-do compile { target c++2a } }
+
+#include <atomic>
+
+std::atomic<const int> a; // { dg-error "here" }
+
+struct MoveOnly
+{
+  MoveOnly() = default;
+  MoveOnly(MoveOnly&&) = default;
+  MoveOnly& operator=(MoveOnly&&) = default;
+};
+std::atomic<MoveOnly> b; // { dg-error "here" }
+
+struct NoMove
+{
+  NoMove() = default;
+  NoMove(const NoMove&) = default;
+  NoMove& operator=(const NoMove&) = default;
+  NoMove(NoMove&&) = delete;
+  NoMove& operator=(NoMove&&) = delete;
+};
+std::atomic<NoMove> c; // { dg-error "here" }
+
+// { dg-error "static assertion failed" "" { target *-*-* } 0 }
