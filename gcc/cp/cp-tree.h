ae2ebf011fec926e003645c33c07a03619ea216a
Marek Polacek <polacek@redhat.com>
2020-06-17 09:19:02 -0400

    c++: ICE with IMPLICIT_CONV_EXPR in array subscript [PR95508]
    Since r10-7096 convert_like, when called in a template, creates an
    IMPLICIT_CONV_EXPR when we're converting to/from array type.
    
    In this test, we have e[f], and we're converting f (of type class A) to
    int, so convert_like in build_new_op_1 created the IMPLICIT_CONV_EXPR
    that got into cp_build_array_ref which calls maybe_constant_value.  My
    patch above failed to adjust this spot to call fold_non_dependent_expr
    instead, which can handle codes like I_C_E in a template.  Fixed by
    using a new function maybe_fold_non_dependent_expr, which, if the expr
    can't be evaluated to a constant, returns the original expression.
    
    gcc/cp/ChangeLog:
    
    	PR c++/95508
    	* constexpr.c (maybe_fold_non_dependent_expr): New.
    	* cp-tree.h (maybe_fold_non_dependent_expr): Declare.
    	* typeck.c (cp_build_array_ref): Call maybe_fold_non_dependent_expr
    	instead of maybe_constant_value.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/95508
    	* g++.dg/template/conv16.C: New test.

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -7933,6 +7933,8 @@ extern tree maybe_constant_init			(tree, tree = NULL_TREE, bool = false);
 extern tree fold_non_dependent_expr		(tree,
 						 tsubst_flags_t = tf_warning_or_error,
 						 bool = false, tree = NULL_TREE);
+extern tree maybe_fold_non_dependent_expr	(tree,
+						 tsubst_flags_t = tf_warning_or_error);
 extern tree fold_non_dependent_init		(tree,
 						 tsubst_flags_t = tf_warning_or_error,
 						 bool = false);
