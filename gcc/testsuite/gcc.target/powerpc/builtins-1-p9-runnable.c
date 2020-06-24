972c78d3f65354aa9ea23d5e503835d68881f494
Will Schmidt <will_schmidt@vnet.ibm.com>
2020-06-24 13:59:34 -0500

    Backport to gcc-9
    	[PATCH, PR target/94954] Fix wrong codegen for vec_pack_to_short_fp32() builtin
    
    Hi,
      Fix codegen for builtin vec_pack_to_short_fp32.  This includes adding
    a define_insn for xvcvsphp, and adding a new define_expand for
    convert_4f32_8f16.
    
    [v2]
       Comment on altivec.md "convert_4f32_8f16" enhanced.
       Testsuite builtins-1-p9-runnable.c updated.
    
    OK for trunk and backports?
    
    Thanks
    -Will
    
    	PR target/94954
    
    2020-07-06  Will Schmidt  <will_schmidt@vnet.ibm.com>
    
    gcc/ChangeLog:
    
    	* config/rs6000/altivec.h (vec_pack_to_short_fp32): Update.
    	* config/rs6000/altivec.md (UNSPEC_CONVERT_4F32_8F16): New unspec.
    	(convert_4f32_8f16): New define_expand
    	* config/rs6000/rs6000-builtin.def (convert_4f32_8f16): New builtin define
    	and overload.
    	* config/rs6000/rs6000-c.c (P9V_BUILTIN_VEC_CONVERT_4F32_8F16): New
    	overloaded builtin entry.
    	* config/rs6000/vsx.md (UNSPEC_VSX_XVCVSPHP): New unspec.
    	(vsx_xvcvsphp): New define_insn.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/powerpc/builtins-1-p9-runnable.c: Update.

--- gcc/testsuite/gcc.target/powerpc/builtins-1-p9-runnable.c
+++ gcc/testsuite/gcc.target/powerpc/builtins-1-p9-runnable.c
@@ -1,25 +1,50 @@
-/* { dg-do run { target { powerpc*-*-linux* && { lp64 && p9vector_hw } } } } */
-/* { dg-require-effective-target powerpc_p9vector_ok } */
+/* { dg-do run { target { powerpc*-*-linux* && p9vector_hw } } } */
+/* { dg-require-effective-target p9vector_hw } */
 /* { dg-options "-O2 -mdejagnu-cpu=power9" } */
 
 #include <altivec.h>
+#include <stdio.h>
 
 void abort (void);
 
 int main() {
   int i;
   vector float vfa, vfb;
-  vector unsigned short vur, vuexpt;
+  vector unsigned short vresult, vexpected;
 
-  vfa = (vector float){3.4, 5.0, 20.0, 50.9 };
-  vfb = (vector float){10.0, 40.0, 70.0, 100.0 };
-  vuexpt = (vector unsigned short){ 3, 5, 20, 50,
-                                    10, 40, 70, 100};
+  vfa = (vector float){0.4, 1.6, 20.0, 99.9 };
+  vfb = (vector float){10.0, -2.0, 70.0, 999.0 };
 
-  vur = vec_pack_to_short_fp32 (vfa, vfb);
+  /* Expected results.  */
+  vexpected = (vector unsigned short) { 0x3666, 0x3e66, 0x4d00, 0x563e,
+					0x4900, 0xc000, 0x5460, 0x63ce};
+
+/*
+     vresult = vec_pack_to_short_fp32 (vfa, vfb);
+  This built-in converts a pair of vector floats into a single vector of
+  packed half-precision (F16) values.  The result type is a vector of
+  signed shorts.
+  The expected codegen for this builtin is
+    xvcvsphp t, vfa
+    xvcvsphp u, vfb
+    if (little endian)
+      vpkuwum vresult, t, u
+    else
+      vpkuwum vresult, u, t
+*/
+
+  vresult = vec_pack_to_short_fp32 (vfa, vfb);
+
+#ifdef DEBUG
+  for(i = 0; i< 4; i++) { printf("i=[%d] %f  \n",i,vfa[i]); }
+  for(i = 0; i< 4; i++) { printf("i=[%d] %f  \n",i+4,vfb[i]); }
+  for(i = 0; i< 8; i++) { printf("i=[%d] %d  \n",i,vresult[i]); }
+#endif
 
   for(i = 0; i< 8; i++) {
-    if (vur[i] != vuexpt[i])
+    if (vresult[i] != vexpected[i]) {
+	printf("i=[%d] 0x%x != 0x%x \n",i,vresult[i],vexpected[i]);
       abort();
+    }
   }
 }
