a543cf8b6b96ba034fac65d0b7876791b58022fc
Richard Biener <rguenther@suse.de>
2020-06-25 12:47:20 +0200

    tree-optimization/95839 - allow CTOR vectorization without loads
    This removes a premature check for enough datarefs in a basic-block
    before we consider vectorizing it which leaves basic-blocks with
    just vectorizable vector constructors unvectorized.  The check
    is effectively done by the following check for store groups
    which then also include constructors.
    
    2020-06-25  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/95839
    	* tree-vect-slp.c (vect_slp_analyze_bb_1): Remove premature
    	check on the number of datarefs.
    
    	* gcc.dg/vect/bb-slp-pr95839.c: New testcase.

--- gcc/tree-vect-slp.c
+++ gcc/tree-vect-slp.c
@@ -3149,15 +3149,6 @@ vect_slp_analyze_bb_1 (bb_vec_info bb_vinfo, int n_stmts, bool &fatal)
       return false;
     }
 
-  if (BB_VINFO_DATAREFS (bb_vinfo).length () < 2)
-    {
-      if (dump_enabled_p ())
-        dump_printf_loc (MSG_MISSED_OPTIMIZATION, vect_location,
-			 "not vectorized: not enough data-refs in "
-			 "basic block.\n");
-      return false;
-    }
-
   if (!vect_analyze_data_ref_accesses (bb_vinfo))
     {
      if (dump_enabled_p ())
@@ -3169,9 +3160,9 @@ vect_slp_analyze_bb_1 (bb_vec_info bb_vinfo, int n_stmts, bool &fatal)
 
   vect_slp_check_for_constructors (bb_vinfo);
 
-  /* If there are no grouped stores in the region there is no need
-     to continue with pattern recog as vect_analyze_slp will fail
-     anyway.  */
+  /* If there are no grouped stores and no constructors in the region
+     there is no need to continue with pattern recog as vect_analyze_slp
+     will fail anyway.  */
   if (bb_vinfo->grouped_stores.is_empty ())
     {
       if (dump_enabled_p ())
