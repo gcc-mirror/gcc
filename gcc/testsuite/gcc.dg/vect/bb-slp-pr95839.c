628b78f9794a2eefcbc578011806bfa8e09b9ef7
Richard Biener <rguenther@suse.de>
2020-06-25 12:47:20 +0200

    tree-optimization/95839 - allow CTOR vectorization without loads
    This removes a premature check for enough datarefs in a basic-block
    before we consider vectorizing it which leaves basic-blocks with
    just vectorizable vector constructors unvectorized.  The check
    is effectively done by the following check for store groups
    which then also include constructors.
    
    2020-06-25  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/95839
    	* tree-vect-slp.c (vect_slp_analyze_bb_1): Remove premature
    	check on the number of datarefs.
    
    	* gcc.dg/vect/bb-slp-pr95839.c: New testcase.

---
+++ gcc/testsuite/gcc.dg/vect/bb-slp-pr95839.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target vect_float } */
+/* { dg-additional-options "-w -Wno-psabi" } */
+
+typedef float __attribute__((vector_size(16))) v4f32;
+
+v4f32 f(v4f32 a, v4f32 b)
+{
+  /* Check that we vectorize this CTOR without any loads.  */
+  return (v4f32){a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3]};
+}
+
+/* { dg-final { scan-tree-dump "basic block vectorized" "slp2" } } */
