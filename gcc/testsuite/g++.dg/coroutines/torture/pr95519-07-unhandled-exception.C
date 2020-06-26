e74c76073092f4715007584edb1fe6b7a17274db
Iain Sandoe <iain@sandoe.co.uk>
2020-06-26 10:48:35 +0100

    coroutines: Handle non-method promise expressions [PR95519]
    The PR  points out that the standard does not restrict promise
    expressions to methods, but the current implementation does.
    
    The patch factors out the building of a general promise expression,
    and then uses it in a fairly mechanical replacement of each case
    that we need such an expressions.
    
    This extends the handling for p.xxxxxx() expressions to cover the
    cases where the promise member is some form callable.
    
    Tests are added for each of the promise expressions.
    
    It's somewhat tortuous to find good uses for this for the
    get-return-object and get-return-object-on-allocation-failure
    cases, but they are included anyway.
    
    gcc/cp/ChangeLog:
    
    	PR c++/95519
    	* coroutines.cc (struct coroutine_info):Add a field
    	to hold computed p.return_void expressions.
    	(coro_build_promise_expression): New.
    	(get_coroutine_return_void_expr): New.
    	(finish_co_yield_expr): Build the promise expression
    	using coro_build_promise_expression.
    	(finish_co_return_stmt): Likewise.
    	(build_init_or_final_await): Likewise.
    	(morph_fn_to_coro): Likewise, for several cases.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/95519
    	* g++.dg/coroutines/torture/pr95519-00-return_void.C: New test.
    	* g++.dg/coroutines/torture/pr95519-01-initial-suspend.C: New test.
    	* g++.dg/coroutines/torture/pr95519-02-final_suspend.C: New test.
    	* g++.dg/coroutines/torture/pr95519-03-return-value.C: New test.
    	* g++.dg/coroutines/torture/pr95519-04-yield-value.C: New test.
    	* g++.dg/coroutines/torture/pr95519-05-gro.C: New test.
    	* g++.dg/coroutines/torture/pr95519-06-grooaf.C: New test.
    	* g++.dg/coroutines/torture/pr95519-07-unhandled-exception.C: New test.

---
+++ gcc/testsuite/g++.dg/coroutines/torture/pr95519-07-unhandled-exception.C
@@ -0,0 +1,69 @@
+//  { dg-do run }
+
+#include "../coro.h"
+
+struct pt_b
+{
+  constexpr std::suspend_never initial_suspend() const noexcept { return {}; }
+  constexpr std::suspend_never final_suspend() const noexcept { return {}; }
+  constexpr void return_void () {};
+};
+
+int called_ueh_op = 0;
+struct ueh
+{
+  void operator ()() noexcept {
+    PRINT("call to operator UEH");
+    called_ueh_op++;
+  }
+};
+
+struct pt_c : pt_b
+{
+  using handle_t = std::coroutine_handle<pt_c>;
+  auto get_return_object() noexcept { return handle_t::from_promise(*this); }
+  ueh unhandled_exception;
+};
+
+int lambda_ueh = 0;
+
+struct pt_d : pt_b
+{
+  using handle_t = std::coroutine_handle<pt_d>;
+  auto get_return_object() noexcept { return handle_t::from_promise(*this); }
+  static constexpr auto unhandled_exception
+    = [] () noexcept { PRINT("call to lambda UEH"); lambda_ueh++; };
+};
+
+template <>
+struct std::coroutine_traits<pt_c::handle_t>
+    { using promise_type = pt_c; };
+
+static pt_c::handle_t
+foo ()
+{
+  throw ("foo");
+  co_return;
+}
+
+template <> 
+struct std::coroutine_traits<pt_d::handle_t>
+    { using promise_type = pt_d; };
+
+static pt_d::handle_t
+bar ()
+{
+  throw ("bar");
+  co_return;
+}
+
+int main ()
+{
+  foo ();
+  bar ();
+  if (called_ueh_op != 1 || lambda_ueh != 1)
+    {
+      PRINT ("Failed to call one of the UEH cases");
+      abort ();
+    }
+}
