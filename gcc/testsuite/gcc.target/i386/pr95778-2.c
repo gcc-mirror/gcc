f3eccbe3bd91e4156583805a97ce4d3058e2119d
Yichao Yu <yyc1992@gmail.com>
2020-06-26 15:46:15 -0600

    Fix target clone indirection elimination
    The current logic seems to be comparing the whole attribute tree between
    the callee and caller (or at least the tree starting from the target attribute).
    This is unnecessary and causes strange dependency of the indirection
    elimination on unrelated properties like `noinline`(PR95780) and
    `visibility`(PR95778).
    
    This changes the comparison to be only on the `target` attribute which should
    be the intent of the code.
    
    gcc
    
    	* multiple_target.c (redirect_to_specific_clone): Fix tests
    	to check individual attribute rather than an attribute list.
    
    gcc/testsuite
    
    	* gcc.target/i386/pr95778-1.c: New test.
    	* gcc.target/i386/pr95778-2.c: New test.

---
+++ gcc/testsuite/gcc.target/i386/pr95778-2.c
@@ -0,0 +1,21 @@
+/* { dg-do compile { target fpic } } */
+/* { dg-options "-O3 -fPIC -fno-asynchronous-unwind-tables" } */
+/* { dg-require-ifunc "" } */
+
+__attribute__((visibility("internal"),target_clones("default,avx2")))
+int
+f2(int *p)
+{
+  asm volatile ("" :: "r"(p) : "memory");
+  return *p;
+}
+
+__attribute__((target_clones("default,avx2")))
+int
+g2(int *p)
+{
+  return f2(p);
+}
+
+/* { dg-final { scan-assembler "g2.default.1:\n\tjmp\tf2.default.1\n" } } */
+/* { dg-final { scan-assembler "g2.avx2.0:\n\tjmp\tf2.avx2.0\n" } } */
