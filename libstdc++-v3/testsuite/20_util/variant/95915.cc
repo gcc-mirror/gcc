deb9c51ca71d3cb507dd91ab8bfd8c1021effd9f
Ville Voutilainen <ville.voutilainen@gmail.com>
2020-06-29 00:36:38 +0300

    PR libstdc++/95915
    	PR libstdc++/95915
    	* include/std/variant (_Uninitialized):
    	Adjust the condition and the comment.
    	* testsuite/20_util/variant/95915.cc: New.
    	* testsuite/20_util/variant/compile.cc: Add new test.
    
    (cherry picked from commit 24b54628cfa95194a8de4071c28cb56e6e81c08e)

---
+++ libstdc++-v3/testsuite/20_util/variant/95915.cc
@@ -0,0 +1,35 @@
+// { dg-options "-std=gnu++20" }
+// { dg-do compile { target c++20 } }
+
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <variant>
+
+using namespace std;
+
+struct virtual_default_dtor {
+   virtual ~virtual_default_dtor() = default;
+};
+
+void default_ctor()
+{
+  {
+    variant<virtual_default_dtor> a;
+  }
+}
+
