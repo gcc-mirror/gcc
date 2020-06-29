008842d741e5a88b8f46c692cf09e83d1f51252c
Richard Biener <rguenther@suse.de>
2020-06-29 13:38:19 +0200

    do not include <utility> from tree-vectorizer.h
    This removes the duplicate <utility> include from tree-vectorizer.h.
    
    2020-06-29  Richard Biener  <rguenther@suse.de>
    
    	* tree-vectorizer.h: Do not include <utility>.

--- gcc/tree-vectorizer.h
+++ gcc/tree-vectorizer.h
@@ -26,7 +26,7 @@ typedef class _stmt_vec_info *stmt_vec_info;
 #include "tree-data-ref.h"
 #include "tree-hash-traits.h"
 #include "target.h"
-#include <utility>
+
 
 /* Used for naming of new temporaries.  */
 enum vect_var_kind {
