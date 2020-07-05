6f49c66ed4e060c333d8bcd5e4ae127ab7bca15b
Hans-Peter Nilsson <hp@axis.com>
2020-07-05 20:50:52 +0200

    PR94600: fix volatile access to the whole of a compound object.
    The store to the whole of each volatile object was picked apart
    like there had been an individual assignment to each of the
    fields.  Reads were added as part of that; see PR for details.
    The reads from volatile memory were a clear bug; individual
    stores questionable.  A separate patch clarifies the docs.
    
    gcc:
    
    2020-07-09  Richard Biener  <rguenther@suse.de>
    
    	PR middle-end/94600
    	* expr.c (expand_constructor): Make a temporary also if we're
    	storing to volatile memory.
    
    gcc/testsuite:
    
    2020-07-09  Hans-Peter Nilsson  <hp@axis.com>
    
    	PR middle-end/94600
    	* gcc.dg/pr94600-1.c, gcc.dg/pr94600-2.c, gcc.dg/pr94600-3.c,
    	gcc.dg/pr94600-4.c, gcc.dg/pr94600-5.c, gcc.dg/pr94600-6.c,
    	gcc.dg/pr94600-7.c, gcc.dg/pr94600-8.c: New tests.
    
    (cherry picked from commit a4aca1edaf37d43b2b7e9111825837a7a317b1b0)

---
+++ gcc/testsuite/gcc.dg/pr94600-8.c
@@ -0,0 +1,33 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target size32plus } */
+/* { dg-options "-fdump-rtl-final -O2" } */
+
+/* Unrolled version of pr94600-2.c. */
+
+typedef struct {
+  unsigned int f0 : 4;
+  unsigned int f1 : 11;
+  unsigned int f2 : 10;
+  unsigned int f3 : 7;
+} t0;
+
+void
+bar(volatile t0 *b)
+{
+  t0 a00 = { .f0 = 7, .f1 = 99, .f3 = 1, };
+  t0 a01 = { .f0 = 7, .f1 = 251, .f3 = 1, };
+  t0 a02 = { .f0 = 8, .f1 = 127, .f3 = 5, };
+  t0 a03 = { .f0 = 5, .f1 = 1, .f3 = 1, };
+  t0 a04 = { .f0 = 5, .f1 = 1, .f3 = 1, };
+  t0 a05 = { .f0 = 5, .f1 = 1, .f3 = 1, };
+
+  b[11] = a00;
+  b[11] = a01;
+  b[11] = a02;
+  b[11] = a03;
+  b[11] = a04;
+  b[11] = a05;
+}
+
+/* { dg-final { scan-rtl-dump-times {\(mem/v} 6 "final" } } */
+/* { dg-final { scan-rtl-dump-times {\(set \(mem/v} 6 "final" } } */
