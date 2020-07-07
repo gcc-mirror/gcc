5888d2c2eeaeda72a4c366efb735dfab1d343de2
Marek Polacek <polacek@redhat.com>
2020-07-07 19:28:04 -0400

    c++: Add new test [PR92427]
    Fixed in r10-5578.
    
    	PR c++/92427
    	* g++.dg/ext/flexary37.C: New test.

---
+++ gcc/testsuite/g++.dg/ext/flexary37.C
@@ -0,0 +1,15 @@
+// PR c++/92427
+// { dg-do compile { target c++11 } }
+// { dg-options "" }
+
+class C {
+private:
+    int a; int b;
+public:
+    C(int A, int B) : a(A), b(B) { }
+    ~C() { }
+};
+
+struct y { // { dg-error "unknown array size in delete" }
+    int a; C b[];
+} y = { 1, { { 2, 3 } } }; // { dg-error "unknown array size in delete" }
