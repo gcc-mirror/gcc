28b0ea877e2d0c72656489baad2d81637ba540c7
Tamar Christina <tamar.christina@arm.com>
2020-07-08 14:32:34 +0100

    AArch64: Add test for -mcpu=native
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/aarch64/cpunative/aarch64-cpunative.exp: New file.
    	* gcc.target/aarch64/cpunative/info_0: New test.
    	* gcc.target/aarch64/cpunative/info_1: New test.
    	* gcc.target/aarch64/cpunative/info_10: New test.
    	* gcc.target/aarch64/cpunative/info_11: New test.
    	* gcc.target/aarch64/cpunative/info_12: New test.
    	* gcc.target/aarch64/cpunative/info_13: New test.
    	* gcc.target/aarch64/cpunative/info_14: New test.
    	* gcc.target/aarch64/cpunative/info_15: New test.
    	* gcc.target/aarch64/cpunative/info_2: New test.
    	* gcc.target/aarch64/cpunative/info_3: New test.
    	* gcc.target/aarch64/cpunative/info_4: New test.
    	* gcc.target/aarch64/cpunative/info_5: New test.
    	* gcc.target/aarch64/cpunative/info_6: New test.
    	* gcc.target/aarch64/cpunative/info_7: New test.
    	* gcc.target/aarch64/cpunative/info_8: New test.
    	* gcc.target/aarch64/cpunative/info_9: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_0.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_1.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_10.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_13.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_14.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_2.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_3.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_4.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_5.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_6.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_7.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_8.c: New test.
    
    (cherry picked from commit 8bc83ee378e1cac65d75752b5137ec35d9e1aca1)

---
+++ gcc/testsuite/gcc.target/aarch64/cpunative/native_cpu_1.c
@@ -0,0 +1,12 @@
+/* { dg-do compile { target { { aarch64*-*-linux*} && native } } } */
+/* { dg-set-compiler-env-var GCC_CPUINFO "$srcdir/gcc.target/aarch64/cpunative/info_1" } */
+/* { dg-additional-options "-mcpu=native" } */
+
+int main()
+{
+  return 0;
+}
+
+/* { dg-final { scan-assembler {\.arch armv8-a\+nosimd} } } */
+
+/* Test one where fp is on by default so turn off simd.  */
