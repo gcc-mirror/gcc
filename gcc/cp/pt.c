ab1a1e6506f3ad9dd960146d1979efca6c647eed
Marek Polacek <polacek@redhat.com>
2020-07-08 19:45:34 -0400

    c++: Diagnose cv-qualified decltype(auto) [PR79815]
    "If the placeholder is the decltype(auto) type-specifier, T shall be the
    placeholder alone." but we weren't detecting "const decltype(auto)".
    
    I've just expanded the existing diagnostic detecting "decltype(auto) &"
    and similar.
    
    gcc/cp/ChangeLog:
    
    	PR c++/79815
    	* decl.c (grokdeclarator): Detect cv-qual decltype(auto).
    	* pt.c (do_auto_deduction): Likewise.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/79815
    	* g++.dg/cpp1y/auto-fn59.C: New test.

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -28993,6 +28993,12 @@ do_auto_deduction (tree type, tree init, tree auto_node,
 	    error ("%qT as type rather than plain %<decltype(auto)%>", type);
 	  return error_mark_node;
 	}
+      else if (TYPE_QUALS (type) != TYPE_UNQUALIFIED)
+	{
+	  if (complain & tf_error)
+	    error ("%<decltype(auto)%> cannot be cv-qualified");
+	  return error_mark_node;
+	}
     }
   else
     {
