8314e36e0d3b8207f286425f476d3ce366959ada
Nathan Sidwell <nathan@acm.org>
2020-07-09 09:54:12 -0700

    aarch64 and powerpc va_lists look different
    	gcc/testuite/
    	* g++.dg/modules/builtin-3_[ab].C: Add va_list scans for
    	aarch64 and ppowerpc ABIs.

--- gcc/testsuite/g++.dg/modules/builtin-3_a.C
+++ gcc/testsuite/g++.dg/modules/builtin-3_a.C
@@ -27,9 +27,14 @@ export inline int count (int a, ...)
 // { dg-final { scan-lang-dump {Writing:-[0-9]*'s named merge key \(decl\) function_decl:'::__builtin_strlen'} module } }
 // { dg-final { scan-lang-dump-not {Writing tree:-[0-9]* function_decl:'__builtin_strlen'\(strlen\)} module } }
 
-// { dg-final { scan-lang-dump-not { Cluster members:\n  \[0\]=decl declaration '::__builtin_va_list'\n  \[1\]=binding '::__builtin_va_list'\n} module } }
-// { dg-final { scan-lang-dump {Wrote GMF:-[0-9]* type_decl:'::__builtin_va_list'@builtins} module } }
-// { dg-final { scan-lang-dump {Writing:-[0-9]*'s named merge key \(decl\) type_decl:'::__builtin_va_list'} module } }
+// The implementation details of va_list's are target-specific.
+// Usually one of two patterns though
+// { dg-final { scan-lang-dump-not { Cluster members:\n  \[0\]=decl declaration '::__builtin_va_list'\n  \[1\]=binding '::__builtin_va_list'\n} module { target { i?86-*-* x86_64-*-* } } } }
+// { dg-final { scan-lang-dump {Wrote GMF:-[0-9]* type_decl:'::__builtin_va_list'@builtins} module { target { i?86-*-* x86_64-*-* } } } }
+// { dg-final { scan-lang-dump {Writing:-[0-9]*'s named merge key \(decl\) type_decl:'::__builtin_va_list'} module { target { i?86-*-* x86_64-*-* } } } }
+
+// { dg-final { scan-lang-dump {Wrote fixed:[0-9]* record_type:'__va_list'} module { target { aarch64*-*-* } } } }
+// { dg-final { scan-lang-dump {Wrote fixed:[0-9]* pointer_type:'::__gnuc_va_list'} module { target { powerpc*-*-* } } } }
 
 // { dg-final { scan-lang-dump-not { Cluster members:\n  \[0\]=decl declaration '::va_list'\n  \[1\]=binding '::va_list'\n} module } }
 // { dg-final { scan-lang-dump {Wrote GMF:-[0-9]* type_decl:'::va_list'@builtins} module } }
