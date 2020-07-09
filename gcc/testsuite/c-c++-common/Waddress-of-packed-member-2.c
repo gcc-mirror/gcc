6741411b033a7e8eae96d5a85e24e5be214757dd
Dimitar Dimitrov <dimitar@dinux.eu>
2020-07-09 22:00:46 +0300

    testsuite: Filter unaligned pointer value warning
    Targets which pack structures by default will not get warnings about
    unaligned access to structure members.
    
    gcc/testsuite/ChangeLog:
    
    	* c-c++-common/Waddress-of-packed-member-1.c: Filter dg-warning
    	for targets who pack by default.
    	* c-c++-common/Waddress-of-packed-member-2.c: Ditto.
    	* c-c++-common/pr51628-13.c: Ditto.
    	* c-c++-common/pr51628-15.c: Ditto.
    	* c-c++-common/pr51628-16.c: Ditto.
    	* c-c++-common/pr51628-26.c: Ditto.
    	* c-c++-common/pr51628-27.c: Ditto.
    	* c-c++-common/pr51628-28.c: Ditto.
    	* c-c++-common/pr51628-29.c: Ditto.
    	* c-c++-common/pr51628-3.c: Ditto.
    	* c-c++-common/pr51628-30.c: Ditto.
    	* c-c++-common/pr51628-31.c: Ditto.
    	* c-c++-common/pr51628-32.c: Ditto.
    	* c-c++-common/pr51628-33.c: Ditto.
    	* c-c++-common/pr51628-35.c: Ditto.
    	* c-c++-common/pr51628-4.c: Ditto.
    	* c-c++-common/pr51628-5.c: Ditto.
    	* c-c++-common/pr51628-6.c: Ditto.
    	* c-c++-common/pr51628-8.c: Ditto.
    	* c-c++-common/pr51628-9.c: Ditto.
    	* c-c++-common/pr88664-2.c: Ditto.
    	* gcc.dg/pr51628-17.c: Ditto.
    	* gcc.dg/pr51628-19.c: Ditto.
    	* gcc.dg/pr51628-20.c: Ditto.
    	* gcc.dg/pr51628-21.c: Ditto.
    	* gcc.dg/pr51628-22.c: Ditto.
    	* gcc.dg/pr51628-24.c: Ditto.
    	* gcc.dg/pr51628-25.c: Ditto.
    	* gcc.dg/pr51628-34.c: Ditto.
    	* gcc.dg/pr88928.c: Ditto.
    
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- gcc/testsuite/c-c++-common/Waddress-of-packed-member-2.c
+++ gcc/testsuite/c-c++-common/Waddress-of-packed-member-2.c
@@ -24,24 +24,24 @@ int *i0;
 
 void foo (void)
 {
-  i0 = s0.p.a;               /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = t0.p.a;               /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = s0.p.b[0];            /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = t0.p.b[0];            /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = &s0.p.a[0];           /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = &t0.p.a[0];           /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = &s0.p.b[0][0];        /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = &t0.p.b[0][0];        /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = *s0.p.b;              /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = *t0.p.b;              /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = &**s0.p.b;            /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = &**t0.p.b;            /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = **&s0.p.b;            /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = **&t0.p.b;            /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = &*s0.p.a;             /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = &*t0.p.a;             /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = *&s0.p.a;             /* { dg-warning "may result in an unaligned pointer value" } */
-  i0 = *&t0.p.a;             /* { dg-warning "may result in an unaligned pointer value" } */
+  i0 = s0.p.a;               /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = t0.p.a;               /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = s0.p.b[0];            /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = t0.p.b[0];            /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = &s0.p.a[0];           /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = &t0.p.a[0];           /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = &s0.p.b[0][0];        /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = &t0.p.b[0][0];        /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = *s0.p.b;              /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = *t0.p.b;              /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = &**s0.p.b;            /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = &**t0.p.b;            /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = **&s0.p.b;            /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = **&t0.p.b;            /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = &*s0.p.a;             /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = &*t0.p.a;             /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = *&s0.p.a;             /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
+  i0 = *&t0.p.a;             /* { dg-warning "may result in an unaligned pointer value" ""  { target { ! default_packed } } } */
   i0 = t0.u.a;                 /* { dg-bogus "may result in an unaligned pointer value" } */
   i0 = t0.u.b[0];              /* { dg-bogus "may result in an unaligned pointer value" } */
   i0 = &t0.u.a[0];             /* { dg-bogus "may result in an unaligned pointer value" } */
