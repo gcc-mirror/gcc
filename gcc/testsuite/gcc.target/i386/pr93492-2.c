267891bd3c7ce3e16662eb2be7273f0bc8be9e7e
H.J. Lu <hjl.tools@gmail.com>
2020-07-10 14:50:03 -0700

    x86: Require Linux target for PR target/93492 tests
    Since -fpatchable-function-entry is only supported on Linux and used by
    Linux kernel, require Linux target for PR target/93492 tests.
    
    	PR target/93492
    	* gcc.target/i386/pr93492-1.c: Require Linux target.
    	* gcc.target/i386/pr93492-2.c: Likewise.
    	* gcc.target/i386/pr93492-3.c: Likewise.
    	* gcc.target/i386/pr93492-4.c: Likewise.
    	* gcc.target/i386/pr93492-5.c: Likewise.

--- gcc/testsuite/gcc.target/i386/pr93492-2.c
+++ gcc/testsuite/gcc.target/i386/pr93492-2.c
@@ -1,4 +1,4 @@
-/* { dg-do "compile" } */
+/* { dg-do "compile" { target *-*-linux* } } */
 /* { dg-options "-O1 -fcf-protection -mmanual-endbr -fasynchronous-unwind-tables" } */
 
 /* Test the placement of the .LPFE1 label.  */
--- gcc/testsuite/gcc.target/i386/pr93492-4.c
+++ gcc/testsuite/gcc.target/i386/pr93492-4.c
@@ -1,4 +1,4 @@
-/* { dg-do "compile" } */
+/* { dg-do "compile" { target *-*-linux* } } */
 /* { dg-options "-O1 -fpatchable-function-entry=1 -fasynchronous-unwind-tables" } */
 
 /* Test the placement of the .LPFE1 label.  */
