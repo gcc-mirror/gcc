70c884a4b82733027ac0e2620d09169b177080d7
Harald Anlauf <anlauf@gmx.de>
2020-07-10 21:35:35 +0200

    PR fortran/95980 - ICE in get_unique_type_string, at fortran/class.c:485
    In SELECT TYPE, the argument may be an incorrectly specified unlimited
    CLASS variable.  Avoid NULL pointer dereferences for clean error
    recovery.
    
    gcc/fortran/
    	PR fortran/95980
    	* class.c (gfc_add_component_ref, gfc_build_class_symbol):
    	Add checks for NULL pointer dereference.
    	* primary.c (gfc_variable_attr): Likewise.
    	* resolve.c (resolve_variable, resolve_assoc_var)
    	(resolve_fl_var_and_proc, resolve_fl_variable_derived)
    	(resolve_symbol): Likewise.

---
+++ gcc/testsuite/gfortran.dg/pr95980_2.f90
@@ -0,0 +1,11 @@
+! { dg-do compile }
+! PR fortran/95980 - ICE in get_unique_type_string, at fortran/class.c:485
+
+program p
+  type t
+     integer :: a
+  end type t
+  class(t) :: x         ! { dg-error "must be dummy, allocatable or pointer" }
+  select type (y => x)
+  end select
+end
