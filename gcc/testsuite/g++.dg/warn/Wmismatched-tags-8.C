5e4c9ebbab7bec3b5994f85aebce13bf37cf46e9
Martin Sebor <msebor@redhat.com>
2020-07-13 09:32:40 -0600

    Avoid printing informational notes when -Wmismatched-tags is suppressed in system headers (PR c++/96063)
    Related:
    PR c++/96063 - mismatched-tags warnings in stdlib headers
    
    gcc/cp/ChangeLog:
    
    	PR c++/96063
    	* parser.c (class_decl_loc_t::diag_mismatched_tags): Print notes only
    	if warning_at returns nonzero.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/96063
    	* g++.dg/warn/Wmismatched-tags-7.C: New test.
    	* g++.dg/warn/Wmismatched-tags-8.C: New test.

---
+++ gcc/testsuite/g++.dg/warn/Wmismatched-tags-8.C
@@ -0,0 +1,22 @@
+/* Verify that #pragma GCC diagnostic works for -Wmismatched-tags.
+   { dg-do "compile" }
+   { dg-options "-Wmismatched-tags" } */
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic error "-Wmismatched-tags"
+class A;            // { dg-message "first declared"
+struct A;           // { dg-error "\\\[-Werror=mismatched-tags" }
+
+#pragma GCC diagnostic ignored "-Wmismatched-tags"
+class B;            // { dg-bogus "first declared" }
+struct B;
+
+#pragma GCC diagnostic warning "-Wmismatched-tags"
+class C;            // { dg-message "first declared"
+struct C;           // { dg-warning "\\\[-Wmismatched-tags" }
+#pragma GCC diagnostic pop
+
+class D;            // { dg-message "first declared"
+struct D;           // { dg-warning "\\\[-Wmismatched-tags" }
+
+// { dg-prune-output "some warnings being treated as errors" }
