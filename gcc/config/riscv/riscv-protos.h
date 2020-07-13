ba0c8e0c3603f2d76cfbba9f488fc14e05a4fc14
SimplyTheOther <simplytheother@gmail.com>
2020-07-13 22:05:28 +0800

    Added RISC-V target hook (or at least provisional one)

--- gcc/config/riscv/riscv-protos.h
+++ gcc/config/riscv/riscv-protos.h
@@ -81,6 +81,9 @@ void riscv_cpu_cpp_builtins (cpp_reader *);
 /* Routines implemented in riscv-d.c  */
 extern void riscv_d_target_versions (void);
 
+/* Routines implemented in riscv-rust.c  */
+extern void riscv_rust_target_cpu_info (void);
+
 /* Routines implemented in riscv-builtins.c.  */
 extern void riscv_atomic_assign_expand_fenv (tree *, tree *, tree *);
 extern rtx riscv_expand_builtin (tree, rtx, rtx, machine_mode, int);
--- gcc/config/riscv/riscv.h
+++ gcc/config/riscv/riscv.h
@@ -30,6 +30,9 @@ along with GCC; see the file COPYING3.  If not see
 /* Target CPU versions for D.  */
 #define TARGET_D_CPU_VERSIONS riscv_d_target_versions
 
+/* Target CPU info for Rust.  */
+#define TARGET_RUST_CPU_INFO riscv_rust_target_cpu_info
+
 /* Default target_flags if no switches are specified  */
 
 #ifndef TARGET_DEFAULT
