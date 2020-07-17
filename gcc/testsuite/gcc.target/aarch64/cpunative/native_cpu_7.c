933b2766c8767d27b9c2b3b14cb9c0f08418889a
Tamar Christina <tamar.christina@arm.com>
2020-07-17 13:13:12 +0100

    AArch64: Add test for -mcpu=native
    This adds some tests to the GCC testsuite for testing the
    -mcpu=native code.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/aarch64/cpunative/aarch64-cpunative.exp: New test.
    	* gcc.target/aarch64/cpunative/info_0: New test.
    	* gcc.target/aarch64/cpunative/info_1: New test.
    	* gcc.target/aarch64/cpunative/info_10: New test.
    	* gcc.target/aarch64/cpunative/info_11: New test.
    	* gcc.target/aarch64/cpunative/info_12: New test.
    	* gcc.target/aarch64/cpunative/info_13: New test.
    	* gcc.target/aarch64/cpunative/info_14: New test.
    	* gcc.target/aarch64/cpunative/info_15: New test.
    	* gcc.target/aarch64/cpunative/info_2: New test.
    	* gcc.target/aarch64/cpunative/info_3: New test.
    	* gcc.target/aarch64/cpunative/info_4: New test.
    	* gcc.target/aarch64/cpunative/info_5: New test.
    	* gcc.target/aarch64/cpunative/info_6: New test.
    	* gcc.target/aarch64/cpunative/info_7: New test.
    	* gcc.target/aarch64/cpunative/info_8: New test.
    	* gcc.target/aarch64/cpunative/info_9: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_0.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_1.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_10.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_11.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_12.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_13.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_14.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_15.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_2.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_3.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_4.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_5.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_6.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_7.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_8.c: New test.
    	* gcc.target/aarch64/cpunative/native_cpu_9.c: New test.

---
+++ gcc/testsuite/gcc.target/aarch64/cpunative/native_cpu_7.c
@@ -0,0 +1,13 @@
+/* { dg-do compile { target { { aarch64*-*-linux*} && native } } } */
+/* { dg-set-compiler-env-var GCC_CPUINFO "$srcdir/gcc.target/aarch64/cpunative/info_7" } */
+/* { dg-additional-options "-mcpu=native" } */
+
+int main()
+{
+  return 0;
+}
+
+/* { dg-final { scan-assembler {\.arch armv8-a\+crypto\+fp16} } } */
+
+/* Test one where the crypto and fp16 options are specified in different
+   order from what is in the options file.  */
