ec97d2e842022a3f112e27d6d85e1bc6c4754ea4
Richard Biener <rguenther@suse.de>
2020-07-20 13:38:16 +0200

    Compute RPO with adjacent SCC members, expose toplevel SCC extents
    This produces a more optimal RPO order for iteration processing
    by making sure that SCC exits are processed before SCC members
    themselves..  This avoids iterating blocks unrelated to the current
    iteration for RPO VN and has the chance to improve code-generation
    for the non-iterative mode of RPO VN.  The patch also exposes toplevel
    SCCs and gets rid of the ad-hoc max_rpo computation in RPO VN.
    
    For simplicity it also removes the odd reverse ordering of the RPO
    array returned from rev_post_order_and_mark_dfs_back_seme.
    
    Overall reduction in the number of visited blocks isn't spectacular
    for bootstrap (~2.5%) but single cases see up to a 10% reduction.
    
    The same function can be used to optimize var-tracking iteration order
    as seen in the followup.
    
    2020-07-28  Richard Biener  <rguenther@suse.de>
    
    	* cfganal.h (rev_post_order_and_mark_dfs_back_seme): Adjust
    	prototype.
    	* cfganal.c (rpoamdbs_bb_data): New struct with pre BB data.
    	(tag_header): New helper.
    	(cmp_edge_dest_pre): Likewise.
    	(rev_post_order_and_mark_dfs_back_seme): Compute SCCs,
    	find SCC exits and perform a DFS walk with extra edges to
    	compute a RPO with adjacent SCC members when requesting an
    	iteration optimized order and populate the toplevel SCC array.
    	* tree-ssa-sccvn.c (do_rpo_vn): Remove ad-hoc computation
    	of max_rpo and fill it in from SCC extent info instead.
    
    	* gcc.dg/torture/20200727-0.c: New testcase.
    
    (cherry picked from commit 3e61a2056335ca7d4e2009823efae4ee2dc950ee)

--- gcc/tree-ssa-sccvn.c
+++ gcc/tree-ssa-sccvn.c
@@ -7351,11 +7351,9 @@ do_rpo_vn (function *fn, edge entry, bitmap exit_bbs,
     e->flags &= ~EDGE_DFS_BACK;
 
   int *rpo = XNEWVEC (int, n_basic_blocks_for_fn (fn) - NUM_FIXED_BLOCKS);
+  auto_vec<std::pair<int, int> > toplevel_scc_extents;
   int n = rev_post_order_and_mark_dfs_back_seme
-    (fn, entry, exit_bbs, !loops_state_satisfies_p (LOOPS_NEED_FIXUP), rpo);
-  /* rev_post_order_and_mark_dfs_back_seme fills RPO in reverse order.  */
-  for (int i = 0; i < n / 2; ++i)
-    std::swap (rpo[i], rpo[n-i-1]);
+    (fn, entry, exit_bbs, true, rpo, !iterate ? &toplevel_scc_extents : NULL);
 
   if (!do_region)
     BITMAP_FREE (exit_bbs);
@@ -7433,12 +7431,20 @@ do_rpo_vn (function *fn, edge entry, bitmap exit_bbs,
   vn_valueize = rpo_vn_valueize;
 
   /* Initialize the unwind state and edge/BB executable state.  */
-  bool need_max_rpo_iterate = false;
+  unsigned curr_scc = 0;
   for (int i = 0; i < n; ++i)
     {
       basic_block bb = BASIC_BLOCK_FOR_FN (fn, rpo[i]);
       rpo_state[i].visited = 0;
       rpo_state[i].max_rpo = i;
+      if (!iterate && curr_scc < toplevel_scc_extents.length ())
+	{
+	  if (i >= toplevel_scc_extents[curr_scc].first
+	      && i <= toplevel_scc_extents[curr_scc].second)
+	    rpo_state[i].max_rpo = toplevel_scc_extents[curr_scc].second;
+	  if (i == toplevel_scc_extents[curr_scc].second)
+	    curr_scc++;
+	}
       bb->flags &= ~BB_EXECUTABLE;
       bool has_backedges = false;
       edge e;
@@ -7450,50 +7456,11 @@ do_rpo_vn (function *fn, edge entry, bitmap exit_bbs,
 	  e->flags &= ~EDGE_EXECUTABLE;
 	  if (iterate || e == entry || (skip_entry_phis && bb == entry->dest))
 	    continue;
-	  if (bb_to_rpo[e->src->index] > i)
-	    {
-	      rpo_state[i].max_rpo = MAX (rpo_state[i].max_rpo,
-					  bb_to_rpo[e->src->index]);
-	      need_max_rpo_iterate = true;
-	    }
-	  else
-	    rpo_state[i].max_rpo
-	      = MAX (rpo_state[i].max_rpo,
-		     rpo_state[bb_to_rpo[e->src->index]].max_rpo);
 	}
       rpo_state[i].iterate = iterate && has_backedges;
     }
   entry->flags |= EDGE_EXECUTABLE;
   entry->dest->flags |= BB_EXECUTABLE;
-
-  /* When there are irreducible regions the simplistic max_rpo computation
-     above for the case of backedges doesn't work and we need to iterate
-     until there are no more changes.  */
-  unsigned nit = 0;
-  while (need_max_rpo_iterate)
-    {
-      nit++;
-      need_max_rpo_iterate = false;
-      for (int i = 0; i < n; ++i)
-	{
-	  basic_block bb = BASIC_BLOCK_FOR_FN (fn, rpo[i]);
-	  edge e;
-	  edge_iterator ei;
-	  FOR_EACH_EDGE (e, ei, bb->preds)
-	    {
-	      if (e == entry || (skip_entry_phis && bb == entry->dest))
-		continue;
-	      int max_rpo = MAX (rpo_state[i].max_rpo,
-				 rpo_state[bb_to_rpo[e->src->index]].max_rpo);
-	      if (rpo_state[i].max_rpo != max_rpo)
-		{
-		  rpo_state[i].max_rpo = max_rpo;
-		  need_max_rpo_iterate = true;
-		}
-	    }
-	}
-    }
-  statistics_histogram_event (cfun, "RPO max_rpo iterations", nit);
 
   /* As heuristic to improve compile-time we handle only the N innermost
      loops and the outermost one optimistically.  */
