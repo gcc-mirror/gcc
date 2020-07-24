52cdac6823237d0ff46f245f35b44a24b8c9fc63
Hans-Peter Nilsson <hp@bitrange.com>
2020-07-24 23:50:05 +0200

    testsuite: Adjust patchable_function tests for mmix.
    There's no reason anyone would want to use the "patchable function"
    feature for MMIX and also no reason to exclude those tests.  For MMIX,
    the NOP equivalent is SWYM ("swymming" is a healthy exercise).
    Text-wise, making the tests pass by adjusting the regexp, is shorter,
    and it seems unlikely to both appear as a mnemonic for other targets
    *and* being emitted in uppercase.
    
    gcc/testsuite:
    
    	* c-c++-common/patchable_function_entry-decl.c,
    	c-c++-common/patchable_function_entry-default.c,
    	c-c++-common/patchable_function_entry-definition.c: Adjust for mmix.

--- gcc/testsuite/c-c++-common/patchable_function_entry-default.c
+++ gcc/testsuite/c-c++-common/patchable_function_entry-default.c
@@ -1,7 +1,7 @@
 /* { dg-do compile { target { ! { nvptx*-*-* visium-*-* } } } } */
 /* { dg-options "-O2 -fpatchable-function-entry=3,1" } */
 /* { dg-additional-options "-fno-pie" { target sparc*-*-* } } */
-/* { dg-final { scan-assembler-times "nop|NOP" 3 { target { ! { alpha*-*-* } } } } } */
+/* { dg-final { scan-assembler-times "nop|NOP|SWYM" 3 { target { ! { alpha*-*-* } } } } } */
 /* { dg-final { scan-assembler-times "bis" 3 { target alpha*-*-* } } } */
 
 extern int a;
