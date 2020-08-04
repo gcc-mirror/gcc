39231d22f240a45f876c3143f985fae9aeb5c15b
Andrew Stubbs <ams@codesourcery.com>
2020-08-04 17:50:38 +0100

    amdgcn: Remove dead defines from gcn-run
    Nothing uses these since the switch to HSACOv3.
    
    gcc/ChangeLog:
    
    	* config/gcn/gcn-run.c (R_AMDGPU_NONE): Delete.
    	(R_AMDGPU_ABS32_LO): Delete.
    	(R_AMDGPU_ABS32_HI): Delete.
    	(R_AMDGPU_ABS64): Delete.
    	(R_AMDGPU_REL32): Delete.
    	(R_AMDGPU_REL64): Delete.
    	(R_AMDGPU_ABS32): Delete.
    	(R_AMDGPU_GOTPCREL): Delete.
    	(R_AMDGPU_GOTPCREL32_LO): Delete.
    	(R_AMDGPU_GOTPCREL32_HI): Delete.
    	(R_AMDGPU_REL32_LO): Delete.
    	(R_AMDGPU_REL32_HI): Delete.
    	(reserved): Delete.
    	(R_AMDGPU_RELATIVE64): Delete.

--- gcc/config/gcn/gcn-run.c
+++ gcc/config/gcn/gcn-run.c
@@ -34,24 +34,6 @@
 #include <elf.h>
 #include <signal.h>
 
-/* These probably won't be in elf.h for a while.  */
-#ifndef R_AMDGPU_NONE
-#define R_AMDGPU_NONE		0
-#define R_AMDGPU_ABS32_LO	1	/* (S + A) & 0xFFFFFFFF  */
-#define R_AMDGPU_ABS32_HI	2	/* (S + A) >> 32  */
-#define R_AMDGPU_ABS64		3	/* S + A  */
-#define R_AMDGPU_REL32		4	/* S + A - P  */
-#define R_AMDGPU_REL64		5	/* S + A - P  */
-#define R_AMDGPU_ABS32		6	/* S + A  */
-#define R_AMDGPU_GOTPCREL	7	/* G + GOT + A - P  */
-#define R_AMDGPU_GOTPCREL32_LO	8	/* (G + GOT + A - P) & 0xFFFFFFFF  */
-#define R_AMDGPU_GOTPCREL32_HI	9	/* (G + GOT + A - P) >> 32  */
-#define R_AMDGPU_REL32_LO	10	/* (S + A - P) & 0xFFFFFFFF  */
-#define R_AMDGPU_REL32_HI	11	/* (S + A - P) >> 32  */
-#define reserved		12
-#define R_AMDGPU_RELATIVE64	13	/* B + A  */
-#endif
-
 #include "hsa.h"
 
 #ifndef HSA_RUNTIME_LIB
