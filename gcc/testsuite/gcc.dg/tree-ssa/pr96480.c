eb136c60756739504aa938b426603aa3b2821d36
Jakub Jelinek <jakub@redhat.com>
2020-08-06 15:47:25 +0200

    reassoc: Improve maybe_optimize_range_tests [PR96480]
    On the following testcase, if the IL before reassoc would be:
    ...
      <bb 4> [local count: 354334800]:
      if (x_3(D) == 2)
        goto <bb 7>; [34.00%]
      else
        goto <bb 5>; [66.00%]
    
      <bb 5> [local count: 233860967]:
      if (x_3(D) == 3)
        goto <bb 7>; [34.00%]
      else
        goto <bb 6>; [66.00%]
    
      <bb 6> [local count: 79512730]:
    
      <bb 7> [local count: 1073741824]:
      # prephitmp_7 = PHI <1(3), 1(4), 1(5), 1(2), 0(6)>
    then we'd optimize it properly, but as bb 5-7 is instead:
      <bb 5> [local count: 233860967]:
      if (x_3(D) == 3)
        goto <bb 6>; [34.00%]
      else
        goto <bb 7>; [66.00%]
    
      <bb 6> [local count: 79512730]:
    
      <bb 7> [local count: 1073741824]:
      # prephitmp_7 = PHI <1(3), 1(4), 0(5), 1(2), 1(6)>
    (i.e. the true/false edges on the last bb with condition swapped
    and ditto for the phi args), we don't recognize it.  If bb 6
    is empty, there should be no functional difference between the two IL
    representations.
    
    This patch handles those special cases.
    
    2020-08-06  Jakub Jelinek  <jakub@redhat.com>
    
    	PR tree-optimization/96480
    	* tree-ssa-reassoc.c (suitable_cond_bb): Add TEST_SWAPPED_P argument.
    	If TEST_BB ends in cond and has one edge to *OTHER_BB and another
    	through an empty bb to that block too, if PHI args don't match, retry
    	them through the other path from TEST_BB.
    	(maybe_optimize_range_tests): Adjust callers.  Handle such LAST_BB
    	through inversion of the condition.
    
    	* gcc.dg/tree-ssa/pr96480.c: New test.

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr96480.c
@@ -0,0 +1,23 @@
+/* PR tree-optimization/96480 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+/* { dg-final { scan-tree-dump " = _\[0-9]* <= 3;" "optimized" } } */
+
+int v[4];
+
+int
+foo (int x)
+{
+  int *p;
+  if (x == 0)
+    p = &v[0];
+  else if (x == 1)
+    p = &v[1];
+  else if (x == 2)
+    p = &v[2];
+  else if (x == 3)
+    p = &v[3];
+  else
+    p = &v[4];
+  return p != &v[4];
+}
