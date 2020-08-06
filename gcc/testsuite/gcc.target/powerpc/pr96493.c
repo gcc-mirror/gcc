61e46440e8124ba605f7ff81aaf1a19176d0c14b
Alan Modra <amodra@gmail.com>
2020-08-06 14:12:21 +0930

    PR96493, powerpc local call linkage failure
    This corrects current_file_function_operand, an operand predicate used
    to determine whether a symbol_ref is safe to use with the local_call
    patterns.  Calls between pcrel and non-pcrel code need to go via
    linker stubs.  In the case of non-pcrel code to pcrel the stub saves
    r2 but there needs to be a nop after the branch for the r2 restore.
    So the local_call patterns can't be used there.  For pcrel code to
    non-pcrel the local_call patterns could still be used, but I thought
    it better to not use them since the call isn't direct.  Code generated
    by the corresponding call_nonlocal_aix for pcrel is identical anyway.
    
    Incidentally, without the TREE_CODE () == FUNCTION_DECL test,
    gcc.c-torture/compile/pr37433.c and pr37433-1.c ICE.  Also, if you
    make the test more strict by disallowing an op without a
    SYMBOL_REF_DECL then a bunch of go and split-stack tests fail.  That's
    because a prologue call to __morestack can't have a following nop.
    (__morestack calls its caller at a fixed offset from the __morestack
    call!)
    
    gcc/
    	PR target/96493
    	* config/rs6000/predicates.md (current_file_function_operand): Don't
    	accept functions that differ in r2 usage.
    
    gcc/testsuite/
    	* gcc.target/powerpc/pr96493.c: New file.
    
    (cherry picked from commit f8ac30f1653ff69706c35af6d725f1d330600f11)

---
+++ gcc/testsuite/gcc.target/powerpc/pr96493.c
@@ -0,0 +1,28 @@
+/* { dg-do run } */
+/* { dg-options "-mdejagnu-cpu=power8 -O2" } */
+/* { dg-require-effective-target powerpc_elfv2 } */
+
+/* Test local calls between pcrel and non-pcrel code.
+
+   Despite the cpu=power10 option, the code generated here should just
+   be plain powerpc64, even the necessary linker stubs.  */
+
+int one = 1;
+
+int __attribute__ ((target("cpu=power8"),noclone,noinline))
+p8_func (int x)
+{
+  return x - one;
+}
+
+int __attribute__ ((target("cpu=power10"),noclone,noinline))
+p10_func (int x)
+{
+  return p8_func (x);
+}
+
+int
+main (void)
+{
+  return p10_func (1);
+}
