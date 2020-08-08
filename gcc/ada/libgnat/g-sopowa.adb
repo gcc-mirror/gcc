d08d481912b9a2dc3388f8e1183cea0bf3ffae9f
Dmitriy Anisimkov <anisimko@adacore.com>
2020-08-08 18:49:27 +0600

    [Ada] Sockets.Poll implementation
    gcc/ada/
    
    	* Makefile.rtl (GNATRTL_SOCKETS_OBJS): New object
    	g-socpol$(objext) New source files noted: g-socpol.adb,
    	g-socpol.ads, g-socpol__dummy.adb, g-socpol__dummy.ads,
    	g-sopowa.adb, g-sopowa__posix.adb, g-sopowa__mingw.adb,
    	g-spogwa.adb, g-spogwa.ads.
    	* impunit.adb (Non_Imp_File_Names_95): New base filename
    	g-socpol in "GNAT Library Units" section for GNAT.Sockets.Poll
    	unit.
    	* libgnat/g-socket.ads, libgnat/g-socket.adb:
    	(Raise_Socket_Error): Moved from body to private part of
    	specification to use in GNAT.Sockets.Poll.
    	* libgnat/g-socpol.ads, libgnat/g-socpol.adb: Main unit of the
    	implementation.
    	* libgnat/g-socpol__dummy.ads, libgnat/g-socpol__dummy.adb:
    	Empty unit for the systems without sockets support.
    	* libgnat/g-spogwa.ads, libgnat/g-spogwa.adb: Generic unit
    	implementing sockets poll on top of select system call.
    	* libgnat/g-sopowa.adb (Wait): Separate implementation for
    	operation systems with poll system call support.
    	* libgnat/g-sopowa__posix.adb (Wait): Separate implementation
    	for POSIX select system call.
    	* libgnat/g-sopowa__mingw.adb (Wait): Separate implementation
    	for Windows select system call.
    	* gsocket.h (_WIN32_WINNT): Increase to 0x0600 for winsock2.h to
    	allow WSAPoll related definitions.
    	* s-oscons-tmplt.c: Fix comment next to #endif for
    	#if defined (__linux__) || defined (__ANDROID__) line.  Include
    	<poll.h> for all except VxWorks and Windows.
    	(SIZEOF_nfds_t): New definition.
    	(SIZEOF_fd_type): New definition.
    	(SIZEOF_pollfd_events): New definition.
    	(POLLIN, POLLPRI, POLLOUT, POLLERR, POLLHUP, POLLNVAL): New
    	definitions for VxWorks to be able to emulate poll on top of
    	select in it.  Define POLLPRI as zero on Windows as it is not
    	supported there.
    	(Poll_Linkname): New definition, because the poll system call
    	has different name in Windows and POSIX.

---
+++ gcc/ada/libgnat/g-sopowa.adb
@@ -0,0 +1,56 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                         GNAT COMPILER COMPONENTS                         --
+--                                                                          --
+--                G N A T . S O C K E T S . P O L L . W A I T               --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--                       Copyright (C) 2020, AdaCore                        --
+--                                                                          --
+-- GNAT is free software;  you can  redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.                                     --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+-- GNAT was originally developed  by the GNAT team at  New York University. --
+-- Extensive contributions were provided by Ada Core Technologies Inc.      --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  Wait implementation on top of native poll call
+--
+--  This submodule can be used on systems where poll system call is natively
+--  supported. Microsoft Windows supports WSAPoll system call from Vista
+--  version and this submodule can be used on such Windows versions too, the
+--  System.OS_Constants.Poll_Linkname constant defines appropriate link name
+--  for Windows. But we do not use WSAPoll in GNAT.Sockets.Poll implementation
+--  for now because it is much slower than select system call, at least in
+--  Windows version 10.0.18363.1016.
+
+separate (GNAT.Sockets.Poll)
+
+procedure Wait
+  (Fds : in out Set; Timeout : Interfaces.C.int; Result : out Integer)
+is
+
+   function Poll
+     (Fds     : Poll_Set;
+      Nfds    : nfds_t;
+      Timeout : Interfaces.C.int) return Integer
+     with Import, Convention => Stdcall, External_Name => SOC.Poll_Linkname;
+
+begin
+   Result := Poll (Fds.Fds, nfds_t (Fds.Length), Timeout);
+end Wait;
---
+++ gcc/ada/libgnat/g-spogwa.ads
@@ -0,0 +1,50 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                         GNAT COMPILER COMPONENTS                         --
+--                                                                          --
+--             G N A T . S O C K E T S . P O L L . G _ W A I T              --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--                       Copyright (C) 2020, AdaCore                        --
+--                                                                          --
+-- GNAT is free software;  you can  redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.                                     --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+-- GNAT was originally developed  by the GNAT team at  New York University. --
+-- Extensive contributions were provided by Ada Core Technologies Inc.      --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Interfaces.C;
+
+private generic
+   type FD_Set_Type is private;
+   with procedure Reset_Socket_Set (Set : in out FD_Set_Type);
+   with procedure Insert_Socket_In_Set
+          (Set : in out FD_Set_Type; FD : FD_Type);
+   with function Is_Socket_In_Set
+          (Set : FD_Set_Type; FD : FD_Type) return Interfaces.C.int;
+procedure GNAT.Sockets.Poll.G_Wait
+  (Fds : in out Set; Timeout : Interfaces.C.int; Result : out Integer);
+--  Common code to implement GNAT.Sockets.Poll.Wait routine on top of posix or
+--  win32 select API.
+--  Posix and Win32 select has the same API but different socket set structure.
+--  C API for select has socket set size defined at compilation stage. This Ada
+--  implementation allow to define size of socket set at the execution time.
+--  Unlike C select API we do not need allocate socket set for maximum number
+--  of sockets when we need to check only few of them. And we are not limited
+--  with FD_SETSIZE when we need more sockets to check.
