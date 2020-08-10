cee4e70e77efe0c85ee27232223b706d983ad3e2
Aldy Hernandez <aldyh@redhat.com>
2020-08-10 09:39:03 +0200

    Declare gt_* functions inline in value-range.h.
    gcc/ChangeLog:
    
    	* value-range.h (gt_ggc_mx): Declare inline.
    	(gt_pch_nx): Same.

--- gcc/value-range.h
+++ gcc/value-range.h
@@ -343,7 +343,7 @@ range_includes_zero_p (const irange *vr)
 }
 
 template<unsigned N>
-static inline void
+inline void
 gt_ggc_mx (int_range<N> *x)
 {
   for (unsigned i = 0; i < N; ++i)
@@ -354,7 +354,7 @@ gt_ggc_mx (int_range<N> *x)
 }
 
 template<unsigned N>
-static inline void
+inline void
 gt_pch_nx (int_range<N> *x)
 {
   for (unsigned i = 0; i < N; ++i)
@@ -365,7 +365,7 @@ gt_pch_nx (int_range<N> *x)
 }
 
 template<unsigned N>
-static inline void
+inline void
 gt_pch_nx (int_range<N> *x, gt_pointer_operator op, void *cookie)
 {
   for (unsigned i = 0; i < N; ++i)
