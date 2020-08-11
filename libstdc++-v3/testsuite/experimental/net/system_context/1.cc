6531087573216bf2404fbf682eeae4a4866a18b7
Jonathan Wakely <jwakely@redhat.com>
2020-08-11 16:16:21 +0100

    libstdc++: Fix net::system_context stop condition
    libstdc++-v3/ChangeLog:
    
    	* include/experimental/executor (system_context::_M_run()):
    	Fix predicate.
    	(system_context::join()): Do not join unless joinable.
    	* testsuite/experimental/net/system_context/1.cc: New test.
    
    (cherry picked from commit 61759518adc7679a6f46369543e30a761a16490a)

---
+++ libstdc++-v3/testsuite/experimental/net/system_context/1.cc
@@ -0,0 +1,42 @@
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do run }
+// { dg-options "-pthread"  }
+// { dg-require-effective-target c++14 }
+// { dg-require-effective-target pthread }
+// { dg-require-gthreads "" }
+
+#include <experimental/executor>
+#include <testsuite_hooks.h>
+
+namespace net = std::experimental::net;
+
+void
+test01()
+{
+  net::system_context& c = net::system_executor{}.context();
+  net::post( [&c] { c.stop(); } );
+  c.join();
+  VERIFY( c.stopped() );
+}
+
+int
+main()
+{
+  test01();
+}
