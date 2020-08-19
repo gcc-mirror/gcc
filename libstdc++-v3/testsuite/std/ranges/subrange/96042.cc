a65261443a9b9cfac876c0b7e47393587eb1ce5c
Jonathan Wakely <jwakely@redhat.com>
2020-08-19 20:36:10 +0100

    libstdc++: Make make-unsigned-like-t<__int128> work [PR 96042]
    As well as ensuring that numeric_limits<__int128> is defined, we need to
    ensure that make-unsigned-like-t and to-unsigned-like work correctly for
    128-bit integers in strict mode. This ensures that a subrange created
    from an iota_view's iterator and sentinel can represent its size.
    
    Co-authored-by: Patrick Palka  <ppalka@redhat.com>
    
    libstdc++-v3/ChangeLog:
    
    2020-08-19  Jonathan Wakely  <jwakely@redhat.com>
    	    Patrick Palka  <ppalka@redhat.com>
    
    	PR libstdc++/96042
    	* include/bits/range_access.h (__detail::__to_unsigned_like):
    	Do not use make_unsigned_t<T> in the return type, as it can
    	result in an error before the integral<T> constraint is checked.
    	[__STRICT_ANSI__]: Add overloads for 128-bit integer types.
    	(__detail::__make_unsigned_like_t): Define as the return type
    	of __to_unsigned_like.
    	* testsuite/std/ranges/subrange/96042.cc: New test.
    
    (cherry picked from commit e6e01618e83bcd9eb3a2b27df30ed87106a748b4)

---
+++ libstdc++-v3/testsuite/std/ranges/subrange/96042.cc
@@ -0,0 +1,34 @@
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=c++20" }
+// { dg-do compile { target c++2a } }
+
+#include <ranges>
+
+constexpr bool
+test01()
+{
+  using I = unsigned long long;
+  // view with a difference type that doesn't fit in long long:
+  std::ranges::iota_view<I, I> v(0, std::numeric_limits<I>::max());
+  // view with a size type that doesn't fit in unsigned long long:
+  std::ranges::subrange sr{v.begin(), v.end()};
+  auto sz = std::ranges::size(sr);
+  return sz == std::numeric_limits<I>::max();
+}
+static_assert( test01() );
