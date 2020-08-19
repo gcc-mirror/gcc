91d206adfe39ce063f6a5731b92a03c05e82e94a
Joe Ramsay <Joe.Ramsay@arm.com>
2020-08-19 12:34:06 +0000

    arm: Require MVE memory operand for destination of vst1q intrinsic
    Previously, the machine description patterns for vst1q accepted a generic memory
    operand for the destination, which could lead to an unrecognised builtin when
    expanding vst1q* intrinsics. This change fixes the pattern to only accept MVE
    memory operands.
    
    gcc/ChangeLog:
    
    	PR target/96683
    	* config/arm/mve.md (mve_vst1q_f<mode>): Require MVE memory operand for
    	destination.
    	(mve_vst1q_<supf><mode>): Likewise.
    
    gcc/testsuite/ChangeLog:
    
    	PR target/96683
    	* gcc.target/arm/mve/intrinsics/vst1q_f16.c: New test.
    	* gcc.target/arm/mve/intrinsics/vst1q_s16.c: New test.
    	* gcc.target/arm/mve/intrinsics/vst1q_s8.c: New test.
    	* gcc.target/arm/mve/intrinsics/vst1q_u16.c: New test.
    	* gcc.target/arm/mve/intrinsics/vst1q_u8.c: New test.

--- gcc/testsuite/gcc.target/arm/mve/intrinsics/vst1q_u8.c
+++ gcc/testsuite/gcc.target/arm/mve/intrinsics/vst1q_u8.c
@@ -10,12 +10,16 @@ foo (uint8_t * addr, uint8x16_t value)
   vst1q_u8 (addr, value);
 }
 
-/* { dg-final { scan-assembler "vstrb.8"  }  } */
-
 void
 foo1 (uint8_t * addr, uint8x16_t value)
 {
   vst1q (addr, value);
 }
 
-/* { dg-final { scan-assembler "vstrb.8"  }  } */
+/* { dg-final { scan-assembler-times "vstrb.8" 2 }  } */
+
+void
+foo2 (uint8_t a, uint8x16_t x)
+{
+  vst1q (&a, x);
+}
