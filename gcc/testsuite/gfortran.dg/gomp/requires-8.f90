cee7f178c9b06461530ef7079c4daa1dfbbcc7a5
Tobias Burnus <tobias@codesourcery.com>
2020-08-21 18:01:33 +0200

    OpenMP: Add 'omp requires' to Fortran (mostly parsing)
    gcc/fortran/ChangeLog:
    
    	* gfortran.h (enum gfc_statement): Add ST_OMP_REQUIRES.
    	(enum gfc_omp_requires_kind): New.
    	(enum gfc_omp_atomic_op): Add GFC_OMP_ATOMIC_ACQ_REL.
    	(struct gfc_namespace): Add omp_requires and omp_target_seen.
    	(gfc_omp_requires_add_clause,
    	(gfc_check_omp_requires): New.
    	* match.h (gfc_match_omp_requires): New.
    	* module.c (enum ab_attribute, attr_bits): Add omp requires clauses.
    	(mio_symbol_attribute): Read/write them.
    	* openmp.c (gfc_check_omp_requires, (gfc_omp_requires_add_clause,
    	gfc_match_omp_requires): New.
    	(gfc_match_omp_oacc_atomic): Use requires's default mem-order.
    	* parse.c (decode_omp_directive): Match requires, set omp_target_seen.
    	(gfc_ascii_statement): Handle ST_OMP_REQUIRES.
    	* trans-openmp.c (gfc_trans_omp_atomic): Handle GFC_OMP_ATOMIC_ACQ_REL.
    
    gcc/testsuite/ChangeLog:
    
    	* gfortran.dg/gomp/requires-1.f90: New test.
    	* gfortran.dg/gomp/requires-2.f90: New test.
    	* gfortran.dg/gomp/requires-3.f90: New test.
    	* gfortran.dg/gomp/requires-4.f90: New test.
    	* gfortran.dg/gomp/requires-5.f90: New test.
    	* gfortran.dg/gomp/requires-6.f90: New test.
    	* gfortran.dg/gomp/requires-7.f90: New test.
    	* gfortran.dg/gomp/requires-8.f90: New test.
    	* gfortran.dg/gomp/requires-9.f90: New test.
    
    (cherry picked from commit 269322ece17202632bc354e9c510e4a5bd6ad84b)

---
+++ gcc/testsuite/gfortran.dg/gomp/requires-8.f90
@@ -0,0 +1,22 @@
+module m  !  { dg-error "has OpenMP device constructs/routines but does not set !.OMP REQUIRES UNIFIED_SHARED_MEMORY but other program units do" }
+  !$omp requires reverse_offload
+contains
+ subroutine foo
+  interface
+   subroutine bar2
+     !$!omp requires dynamic_allocators
+   end subroutine
+  end interface
+  !$omp target
+     call bar2()
+  !$omp end target
+ end subroutine foo
+end module m
+
+subroutine bar  ! { dg-error "has OpenMP device constructs/routines but does not set !.OMP REQUIRES REVERSE_OFFSET but other program units do" }
+  !use m
+  !$omp requires unified_shared_memory
+  !$omp declare target
+end subroutine bar
+
+! { dg-prune-output "not yet supported" }
