0f5c98b6a1a7eed281e359f40bc2e4326f2a2f56
Iain Buclaw <ibuclaw@gdcproject.org>
2020-08-25 11:44:48 +0200

    d: Merge upstream dmd cb4a96fae
    Fixes both a bug where compilation would hang, and an issue where recursive
    template limits are hit too early.
    
    Reviewed-on: https://github.com/dlang/dmd/pull/11621
    
    gcc/d/ChangeLog:
    
    	* dmd/MERGE: Merge upstream dmd cb4a96fae

--- gcc/d/dmd/globals.h
+++ gcc/d/dmd/globals.h
@@ -244,6 +244,8 @@ struct Global
     Array<class Identifier*>* versionids; // command line versions and predefined versions
     Array<class Identifier*>* debugids;   // command line debug versions and predefined versions
 
+    enum { recursionLimit = 500 }; // number of recursive template expansions before abort
+
     /* Start gagging. Return the current number of gagged errors
      */
     unsigned startGagging();
