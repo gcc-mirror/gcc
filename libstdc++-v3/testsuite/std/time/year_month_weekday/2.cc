7b743c67f04471a0129390ad2808e61e5538e0d3
Patrick Palka <ppalka@redhat.com>
2020-08-27 14:09:52 -0400

    libstdc++: Fix operator overload ambiguity with calendar types
    We currently don't enforce a constraint on some of the calendar types'
    addition/subtraction operator overloads that take a 'months' arguments:
    
      Constraints: If the argument supplied by the caller for the months
      parameter is convertible to years, its implicit conversion sequence to
      years is worse than its implicit conversion sequence to months.
    
    This constraint is relevant when adding/subtracting a duration to/from,
    say, a year_month where the given duration is convertible to both
    'months' and to 'years' (as in the new testcases below).  The correct
    behavior here in light of this constraint is to perform the operation
    through the (more efficient) 'years'-based overload, but we currently
    emit an ambiguous overload error.
    
    This patch templatizes the 'months'-based addition/subtraction operator
    overloads so that in the event of an implicit-conversion tie, we select
    the non-template 'years'-based overload.  This is the same approach
    that the date library takes for enforcing this constraint.
    
    libstdc++-v3/ChangeLog:
    
    	* include/std/chrono
    	(__detail::__months_years_conversion_disambiguator): Define.
    	(year_month::operator+=): Templatize the 'months'-based overload
    	so that the 'years'-based overload is selected in case of
    	equally-ranked implicit conversion sequences to both 'months'
    	and 'years' from the supplied argument.
    	(year_month::operator-=): Likewise.
    	(year_month::operator+): Likewise.
    	(year_month::operator-): Likewise.
    	(year_month_day::operator+=): Likewise.
    	(year_month_day::operator-=): Likewise.
    	(year_month_day::operator+): Likewise.
    	(year_month_day::operator-): Likewise.
    	(year_month_day_last::operator+=): Likewise.
    	(year_month_day_last::operator-=): Likewise.
    	(year_month_day_last::operator+): Likewise
    	(year_month_day_last::operator-): Likewise.
    	(year_month_day_weekday::operator+=): Likewise
    	(year_month_day_weekday::operator-=): Likewise.
    	(year_month_day_weekday::operator+): Likewise.
    	(year_month_day_weekday::operator-): Likewise.
    	(year_month_day_weekday_last::operator+=): Likewise
    	(year_month_day_weekday_last::operator-=): Likewise.
    	(year_month_day_weekday_last::operator+): Likewise.
    	(year_month_day_weekday_last::operator-): Likewise.
    	(testsuite/std/time/year_month/2.cc): New test.
    	(testsuite/std/time/year_month_day/2.cc): New test.
    	(testsuite/std/time/year_month_day_last/2.cc): New test.
    	(testsuite/std/time/year_month_weekday/2.cc): New test.
    	(testsuite/std/time/year_month_weekday_last/2.cc): New test.

---
+++ libstdc++-v3/testsuite/std/time/year_month_weekday/2.cc
@@ -0,0 +1,40 @@
+// { dg-options "-std=gnu++2a" }
+// { dg-do compile { target c++2a } }
+
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// Class template year_month_weekday [time.cal.year_month_weekday]
+
+#include <chrono>
+
+constexpr void
+constexpr_year_month_weekday_op_overload_disambiguation()
+{
+  using namespace std::chrono;
+  using decades = duration<long long, std::ratio<31556952 * 10>>;
+  static_assert(std::convertible_to<decades, months>
+		&& std::convertible_to<decades, years>);
+  using ymwd = year_month_weekday;
+
+  constexpr ymwd ymwd1 = 2015y/June/Monday[3];
+  static_assert(ymwd1 + decades{1} == 2025y/June/Monday[3]);
+  static_assert(ymwd1 - decades{1} == 2005y/June/Monday[3]);
+  static_assert(decades{1} + ymwd1 == 2025y/June/Monday[3]);
+  static_assert((ymwd{ymwd1} += decades{1}) == 2025y/June/Monday[3]);
+  static_assert((ymwd{ymwd1} -= decades{1}) == 2005y/June/Monday[3]);
+}
