31782bd45331ef3006f624d7b1cc9cd11b4abb84
Jonathan Wakely <jwakely@redhat.com>
2020-08-28 22:45:24 +0100

    libstdc++: Fix std::gcd and std::lcm for unsigned integers [PR 92978]
    This fixes a bug with mixed signed and unsigned types, where converting
    a negative value to the unsigned result type alters the value. The
    solution is to obtain the absolute values of the arguments immediately
    and to perform the actual GCD or LCM algorithm on two arguments of the
    same type.
    
    In order to operate on the most negative number without overflow when
    taking its absolute, use an unsigned type for the result of the abs
    operation. For example, -INT_MIN will overflow, but -(unsigned)INT_MIN
    is (unsigned)INT_MAX+1U which is the correct value.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/92978
    	* include/std/numeric (__abs_integral): Replace with ...
    	(__detail::__absu): New function template that returns an
    	unsigned type, guaranteeing it can represent the most
    	negative signed value.
    	(__detail::__gcd, __detail::__lcm): Require arguments to
    	be unsigned and therefore already non-negative.
    	(gcd, lcm): Convert arguments to absolute value as unsigned
    	type before calling __detail::__gcd or __detail::__lcm.
    	* include/experimental/numeric (gcd, lcm): Likewise.
    	* testsuite/26_numerics/gcd/gcd_neg.cc: Adjust expected
    	errors.
    	* testsuite/26_numerics/lcm/lcm_neg.cc: Likewise.
    	* testsuite/26_numerics/gcd/92978.cc: New test.
    	* testsuite/26_numerics/lcm/92978.cc: New test.
    	* testsuite/experimental/numeric/92978.cc: New test.
    
    (cherry picked from commit 82db1a42e9254c9009bbf8ac01366da4d1ab6df5)

--- libstdc++-v3/testsuite/26_numerics/gcd/gcd_neg.cc
+++ libstdc++-v3/testsuite/26_numerics/gcd/gcd_neg.cc
@@ -46,9 +46,9 @@ test01()
   std::gcd<const int&, const int&>(0.1, 0.1);   // { dg-error "from here" }
 }
 
-// { dg-error "integers" "" { target *-*-* } 133 }
-// { dg-error "integers" "" { target *-*-* } 134 }
-// { dg-error "not bools" "" { target *-*-* } 135 }
-// { dg-error "not bools" "" { target *-*-* } 137 }
+// { dg-error "must be integers" "" { target *-*-* } 134 }
+// { dg-error "must be integers" "" { target *-*-* } 135 }
+// { dg-error "must not be bool" "" { target *-*-* } 136 }
+// { dg-error "must not be bool" "" { target *-*-* } 137 }
 // { dg-prune-output "deleted function" }
-// { dg-prune-output "invalid operands" }
+// { dg-prune-output "incomplete type .*make_unsigned" }
