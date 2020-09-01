14f8307cf4261efd5ee4475b3c7f7c42c48557d6
Jonathan Wakely <jwakely@redhat.com>
2020-09-01 18:18:26 +0100

    libstdc++: Constrain chrono::duration conversions [LWG 2094]
    The chrono::duration constructor that converts from another duration
    type is meant to be constrained so that it doesn't participate in
    overload resolution if the ratio of the periods cannot be represented as
    a std::ratio.
    
    Because our std::ratio_divide is not SFINAE-friendly the evaluation of
    __is_harmonic results in an error outside the immediate context when an
    overflow occurs. I intend to make ratio_divide (and ratio_multiply)
    SFINAE-friendly in a future patch, but for now this patch just
    introduces a new SFINAE-friendly alias template for the division.
    
    The standard doesn't require it, but it also seems right to constrain
    the constructor with std::is_convertible_v<_Rep2, rep>.
    
    libstdc++-v3/ChangeLog:
    
    	* include/std/chrono (duration::_S_gcd(intmax_t, intmax_t)):
    	New helper function for finding GCD of two positive intmax_t
    	values.
    	(duration::__divide): New helper alias for dividing one period
    	by another.
    	(duration::__is_harmonic): Use __divide not ratio_divide.
    	(duration(const duration<R2, P2>&)): Require the duration rep
    	types to be convertible.
    	* testsuite/20_util/duration/cons/dr2094.cc: New test.
    	* testsuite/20_util/duration/requirements/reduced_period.cc:
    	Fix definition of unused member functions in test type.
    	* testsuite/20_util/duration/requirements/typedefs_neg2.cc:
    	Adjust expected errors.
    
    (cherry picked from commit b1850c617b14eedaf60b358f3b7d4707cff73b8a)

---
+++ libstdc++-v3/testsuite/20_util/duration/cons/dr2094.cc
@@ -0,0 +1,64 @@
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do compile { target c++11 } }
+
+#include <chrono>
+
+void
+test01()
+{
+  using namespace std::chrono;
+  using std::exa;
+
+  // LWG 2094
+  // duration conversion overflow shouldn't participate in overload resolution
+  bool f(milliseconds);
+  void f(seconds);
+  duration<int,exa> r(1);
+  f(r);
+}
+
+void
+test02()
+{
+  struct Number
+  {
+    explicit
+    Number(int t = 0) : i(t)
+    { }
+
+    int i = 0;
+
+    Number& operator+=(Number n) { i += n.i; return *this; }
+    Number& operator-=(Number n) { i -= n.i; return *this; }
+    Number& operator*=(Number n) { i *= n.i; return *this; }
+    Number& operator/=(Number n) { i /= n.i; return *this; }
+    Number& operator%=(Number n) { i %= n.i; return *this; }
+
+    Number operator+(Number n) { return Number{ i + n.i }; }
+    Number operator-(Number n) { return Number{ i - n.i }; }
+    Number operator*(Number n) { return Number{ i * n.i }; }
+    Number operator/(Number n) { return Number{ i / n.i }; }
+    Number operator%(Number n) { return Number{ i % n.i }; }
+  };
+
+  using std::chrono::duration;
+
+  static_assert( ! std::is_constructible<duration<int>, duration<Number>>(),
+      "duration(const duration<R2, P2>&) constrained on R2 -> R conversion" );
+}
