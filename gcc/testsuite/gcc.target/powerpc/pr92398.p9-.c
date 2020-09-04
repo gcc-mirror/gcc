e1336703f8220dcffdeddb5e19dd032c766fbb8f
Kewen Lin <linkw@gcc.gnu.org>
2020-09-03 21:58:39 -0500

    test/rs6000: Replace test targets p8 and p9+
    This patch is to clean existing rs6000 test targets p8 and p9+
    with existing has_arch_pwr8 and has_arch_pwr9 targets combination
    or only one of them.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/powerpc/pr92398.p9+.c: Replace p9+ with has_arch_pwr9.
    	* gcc.target/powerpc/pr92398.p9-.c: Replace p9+ with has_arch_pwr9,
    	and replace p8 with has_arch_pwr8 && !has_arch_pwr9.
    	* lib/target-supports.exp (check_effective_target_p8): Remove.
    	(check_effective_target_p9+): Remove.

--- gcc/testsuite/gcc.target/powerpc/pr92398.p9-.c
+++ gcc/testsuite/gcc.target/powerpc/pr92398.p9-.c
@@ -1,9 +1,9 @@
-/* { dg-do compile { target { lp64 && {! p9+} } } } */
+/* { dg-do compile { target { lp64 && {! has_arch_pwr9} } } } */
 /* { dg-require-effective-target powerpc_vsx_ok } */
 /* { dg-options "-O2 -mvsx" } */
 
 /* { dg-final { scan-assembler-times {\mnot\M} 2 { xfail be } } } */
-/* { dg-final { scan-assembler-times {\mstd\M} 2 { xfail { p8 && be } } } } */
+/* { dg-final { scan-assembler-times {\mstd\M} 2 { xfail { { {! has_arch_pwr9} && has_arch_pwr8 } && be } } } } */
 
 /* Source code for the test in pr92398.h */
 #include "pr92398.h"
