29ff25e70671ce2046aab5cf89bf519ec6cf2bed
Iain Buclaw <ibuclaw@gdcproject.org>
2020-09-08 17:15:33 +0200

    d: Warn when casting from a D class to a C++ class.
    Before, the warning was only issued when casting in the other direction.
    Now a warning is printed for both directions.
    
    gcc/d/ChangeLog:
    
    	* d-convert.cc (convert_expr): Warn when casting from a D class to a
    	C++ class.
    
    gcc/testsuite/ChangeLog:
    
    	* gdc.dg/Waddress.d: New test.
    	* gdc.dg/Wcastresult1.d: New test.
    	* gdc.dg/Wcastresult2.d: New test.

---
+++ gcc/testsuite/gdc.dg/Wcastresult2.d
@@ -0,0 +1,12 @@
+// { dg-do compile }
+// { dg-options "-Wcast-result" }
+
+void test()
+{
+    auto imvalue = 1.23i;
+    auto revalue = 1.23;
+
+    auto imtore = cast(double)imvalue; // { dg-warning "cast from 'idouble' to 'double' will produce zero result" }
+    auto retoim = cast(idouble)revalue; // { dg-warning "cast from 'double' to 'idouble' will produce zero result" }
+    return;
+}
