2c1d809e93e983fa9dde6d4a14c23193608fed68
Andrew Stubbs <ams@codesourcery.com>
2020-09-10 14:58:15 +0100

    testsuite: gimplefe-44 requires exceptions
    This avoids an ICE on amdgcn.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/gimplefe-44.c: Require exceptions.

--- gcc/testsuite/gcc.dg/gimplefe-44.c
+++ gcc/testsuite/gcc.dg/gimplefe-44.c
@@ -1,4 +1,5 @@
 /* { dg-do compile } */
+/* { dg-require-effective-target exceptions } */
 /* { dg-options "-fexceptions -fgimple -fdump-tree-eh-eh" } */
 /* { dg-require-effective-target nonlocal_goto } */
 
