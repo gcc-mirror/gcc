04a9b796436c68a07c052805631e962a1126dcca
Will Schmidt <will_schmidt@vnet.ibm.com>
2020-09-15 15:06:08 -0500

    [PATCH, rs6000] Fix vector long long subtype (PR96139)
    Hi,
      This corrects an issue with the powerpc vector long long subtypes.
    As reported by SjMunroe, when building some code with -Wall, and
    attempting to print an element of a "long long vector" with a
    long long printf format string, we will report an error because
    the vector sub-type was improperly defined as int.
    
    When defining a V2DI_type_node we use a TARGET_POWERPC64 ternary to
    define the V2DI_type_node with "vector long" or "vector long long".
    We also need to specify the proper sub-type when we define the type.
    
    Due to some file renames, This is a backport and rework of both
        [PATCH, rs6000] Fix vector long long subtype (PR96139)
    and
        [PATCH, rs6000] Testsuite fixup pr96139 tests
    
    
    PR target/96139
    
    2020-09-03  Will Schmidt  <will_schmidt@vnet.ibm.com>
    
    gcc/ChangeLog:
    	* config/rs6000/rs6000.c (rs6000_init_builtin): Update V2DI_type_node
    	and unsigned_V2DI_type_node definitions.
    
    gcc/testsuite/ChangeLog:
    	* gcc.target/powerpc/pr96139-a.c: New test.
    	* gcc.target/powerpc/pr96139-b.c: New test.
    	* gcc.target/powerpc/pr96139-c.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/pr96139-a.c
@@ -0,0 +1,32 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -Wall -m32 -mvsx" } */
+/* { dg-require-effective-target ilp32 } */
+/* { dg-require-effective-target powerpc_vsx_ok } */
+
+#include <stdio.h>
+#include <altivec.h>
+
+void
+try_printing_longlong_a (
+                        __vector signed char cval,
+                        __vector signed int ival,
+                        __vector signed long long int llval,
+                        int x, int y, int z)
+{
+  printf (" %016llx \n", llval[x]);
+  printf (" %016x \n", ival[z]);
+  printf (" %c \n", cval[y]);
+}
+
+void
+try_printing_unsigned_longlong_a (
+                        __vector unsigned char cval,
+                        __vector unsigned int ival,
+                        __vector unsigned long long int llval,
+                        int x, int y, int z)
+{
+  printf (" %016llx \n", llval[x]);
+  printf (" %016x \n", ival[z]);
+  printf (" %c \n", cval[y]);
+}
+
