9e89fa0e2d7da77236cad50c0a9a33b7a4b97367
H.J. Lu <hjl.tools@gmail.com>
2020-09-15 07:21:58 -0700

    gcc.target/i386/pr78904-4a.c: Compile with -mtune=generic
    commit e95395926a84a2406faefe0995295d199d595440
    Author: Uros Bizjak <ubizjak@gmail.com>
    Date:   Thu Jun 18 20:12:48 2020 +0200
    
        i386: Fix mode of ZERO_EXTRACT RTXes, remove ext_register_operand predicate.
    
    caused
    
    FAIL: gcc.target/i386/pr78904-4a.c scan-assembler [ \t]movb[\t ]+%.h, t
    
    when compiled with --target_board='unix{-m32\ -march=cascadelake}'.  With
    -mtune=generic:
    
    	movzwl	4(%esp), %edx
    	movl	8(%esp), %eax
    	movb	%dh, t(%eax)
    	ret
    
    With -mtune=cascadelake:
    
    	movzbl	5(%esp), %edx
    	movl	8(%esp), %eax
    	movb	%dl, t(%eax)
    	ret
    
    Add -mtune=generic for --target_board='unix{-m32\ -march=cascadelake}'.
    
    	* gcc.target/i386/pr78904-4a.c: Compile with -mtune=generic.

--- gcc/testsuite/gcc.target/i386/pr78904-4a.c
+++ gcc/testsuite/gcc.target/i386/pr78904-4a.c
@@ -1,7 +1,7 @@
 /* PR target/78904 */
 /* { dg-do compile } */
 /* { dg-require-effective-target nonpic } */
-/* { dg-options "-O2 -masm=att" } */
+/* { dg-options "-O2 -masm=att -mtune=generic" } */
 
 typedef __SIZE_TYPE__ size_t;
 
