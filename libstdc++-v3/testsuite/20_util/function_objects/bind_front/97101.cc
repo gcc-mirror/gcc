3c755b428e188228d0bad90625c995fd25a02322
Jonathan Wakely <jwakely@redhat.com>
2020-09-21 00:17:02 +0100

    libstdc++: Fix noexcept-specifier for std::bind_front [PR 97101]
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/97101
    	* include/std/functional (bind_front): Fix order of parameters
    	in is_nothrow_constructible_v specialization.
    	* testsuite/20_util/function_objects/bind_front/97101.cc: New test.

---
+++ libstdc++-v3/testsuite/20_util/function_objects/bind_front/97101.cc
@@ -0,0 +1,41 @@
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++2a" }
+// { dg-do compile { target c++2a } }
+
+#include <functional>
+
+void
+test01()
+{
+  struct F1
+  {
+    void operator()() { }
+  };
+
+  struct F2
+  {
+    F2() = default;
+    F2(const F2&) noexcept(false) { }
+    void operator()() { }
+  };
+
+  // PR libstdc++/97101
+  static_assert( noexcept(std::bind_front(F1{})) );
+  static_assert( ! noexcept(std::bind_front(F2{})) );
+}
