77923ad01415f6e72af844cbef5227f5b5a9fb4b
Jonathan Wakely <jwakely@redhat.com>
2020-09-21 14:28:58 +0100

    libstdc++: Make std::assume_aligned a constexpr function [PR 97132]
    The cast from void* to T* in std::assume_aligned is not valid in a
    constexpr function. The optimization hint is redundant during constant
    evaluation anyway (the compiler can see the object and knows its
    alignment). Simply return the original pointer without applying the
    __builtin_assume_aligned hint to it when doing constant evaluation.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/97132
    	* include/std/memory (assume_aligned): Do not use
    	__builtin_assume_aligned during constant evaluation.
    	* testsuite/20_util/assume_aligned/1.cc: Improve test.
    	* testsuite/20_util/assume_aligned/97132.cc: New test.
    
    (cherry picked from commit f10ed928e2f8ecc2c859abff8f2f9296b11b8d95)

--- libstdc++-v3/testsuite/20_util/assume_aligned/1.cc
+++ libstdc++-v3/testsuite/20_util/assume_aligned/1.cc
@@ -15,7 +15,7 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-options "-std=gnu++2a" }
+// { dg-options "-std=gnu++2a -O2" }
 // { dg-do run { target c++2a } }
 
 #include <memory>
@@ -29,7 +29,28 @@ test01()
   VERIFY( p == &i );
 }
 
+[[gnu::noipa,gnu::noinline]]
+int*
+create_aligned(std::size_t alignment, void* p, std::size_t n)
+{
+  return ::new(std::align(alignment, sizeof(int), p, n)) int(42);
+}
+
+extern "C" void undefined(); // call to this should be optimized away
+
+void
+test02()
+{
+  unsigned char buf[sizeof(int) * 128];
+  int* p = create_aligned(64, buf + 1, sizeof(buf) - 1);
+  int* q = std::assume_aligned<64>(p);
+  if ((std::uintptr_t)q % 64)
+    undefined();
+  VERIFY( p == q );
+}
+
 int main()
 {
   test01();
+  test02();
 }
