39a27bb01aa223ce89946f0a4de6b60c4c0b03d2
Richard Biener <rguenther@suse.de>
2020-09-29 15:02:47 +0200

    tree-optimization/97241 - fix ICE in reduction vectorization
    The following moves an ad-hoc attempt at discovering the SLP node
    for a stmt to the place where we can find it in lock-step when
    we find the stmt itself.
    
    2020-09-29  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/97241
    	* tree-vect-loop.c (vectorizable_reduction): Move finding
    	the SLP node for the reduction stmt to a better place.
    
    	* gcc.dg/vect/pr97241.c: New testcase.

---
+++ gcc/testsuite/gcc.dg/vect/pr97241.c
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 --param max-loop-header-insns=2" } */
+
+short int *ev;
+int l4;
+
+short int
+a7 (void)
+{
+  short int uo = ev[0], ie = uo;
+
+  for (int kp = 0; kp < l4; kp += 4)
+    {
+      uo += ev[kp + 1];
+      ie += ev[kp];
+    }
+
+  return uo + ie;
+}
