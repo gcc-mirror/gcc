679dbc9dcec0d1240951442947199ba155f494b6
Nathan Sidwell <nathan@acm.org>
2020-10-02 12:21:08 -0700

    c++: Kill DECL_ANTICIPATED
    Here's the patch to remove DECL_ANTICIPATED, and with it hiddenness is
    managed entirely in the symbol table.  Sadly I couldn't get rid of the
    actual field without more investigation -- it's repurposed for
    OMP_PRIVATIZED_MEMBER.  It looks like a the VAR-related flags in
    lang_decl_base are not completely orthogonal, so perhaps some can be
    turned into an enumeration or something.  But that's more than I want
    to do right now.
    
    DECL_FRIEND_P Is still slightly suspect as it appears to mean more
    than just in-class definition.  However, I'm leaving that for now.
    
    	gcc/cp/
    	* cp-tree.h (lang_decl_base): anticipated_p is not used for
    	anticipatedness.
    	(DECL_ANTICIPATED): Delete.
    	* decl.c (duplicate_decls): Delete DECL_ANTICIPATED_management,
    	use was_hidden.
    	(cxx_builtin_function): Drop DECL_ANTICIPATED setting.
    	(xref_tag_1): Drop DECL_ANTICIPATED assert.
    	* name-lookup.c (name_lookup::adl_class_only): Drop
    	DECL_ANTICIPATED check.
    	(name_lookup::search_adl): Always dedup.
    	(anticipated_builtin_p): Reimplement.
    	(do_pushdecl): Drop DECL_ANTICIPATED asserts & update.
    	(lookup_elaborated_type_1): Drop DECL_ANTICIPATED update.
    	(do_pushtag): Drop DECL_ANTICIPATED setting.
    	* pt.c (push_template_decl): Likewise.
    	(tsubst_friend_class): Likewise.
    	libcc1/
    	* libcp1plugin.cc (libcp1plugin.cc): Drop DECL_ANTICIPATED test.

--- gcc/cp/name-lookup.c
+++ gcc/cp/name-lookup.c
@@ -842,12 +842,6 @@ name_lookup::adl_class_only (tree type)
 	  if (CP_DECL_CONTEXT (fn) != context)
 	    continue;
 
-	  /* Only interested in anticipated friends.  (Non-anticipated
-	     ones will have been inserted during the namespace
-	     adl.)  */
-	  if (!DECL_ANTICIPATED (fn))
-	    continue;
-
 	  /* Template specializations are never found by name lookup.
 	     (Templates themselves can be found, but not template
 	     specializations.)  */
@@ -1079,11 +1073,8 @@ name_lookup::adl_template_arg (tree arg)
 tree
 name_lookup::search_adl (tree fns, vec<tree, va_gc> *args)
 {
-  if (fns)
-    {
-      deduping = true;
-      lookup_mark (fns, true);
-    }
+  deduping = true;
+  lookup_mark (fns, true);
   value = fns;
 
   unsigned ix;
@@ -2136,19 +2127,9 @@ strip_using_decl (tree decl)
 static bool
 anticipated_builtin_p (tree ovl)
 {
-  if (TREE_CODE (ovl) != OVERLOAD)
-    return false;
-
-  if (!OVL_HIDDEN_P (ovl))
-    return false;
-
-  tree fn = OVL_FUNCTION (ovl);
-  gcc_checking_assert (DECL_ANTICIPATED (fn));
-
-  if (DECL_BUILTIN_P (fn))
-    return true;
-
-  return false;
+  return (TREE_CODE (ovl) == OVERLOAD
+	  && OVL_HIDDEN_P (ovl)
+	  && DECL_BUILTIN_P (OVL_FUNCTION (ovl)));
 }
 
 /* BINDING records an existing declaration for a name in the current scope.
@@ -3079,14 +3060,6 @@ do_pushdecl (tree decl, bool hiding)
       tree *slot = NULL; /* Binding slot in namespace.  */
       tree old = NULL_TREE;
 
-      if (!hiding)
-	/* We should never unknownly push an anticipated decl.  */
-	gcc_checking_assert (!((TREE_CODE (decl) == TYPE_DECL
-				|| TREE_CODE (decl) == FUNCTION_DECL
-				|| TREE_CODE (decl) == TEMPLATE_DECL)
-			       && DECL_LANG_SPECIFIC (decl)
-			       && DECL_ANTICIPATED (decl)));
-
       if (level->kind == sk_namespace)
 	{
 	  /* We look in the decl's namespace for an existing
@@ -3171,8 +3144,6 @@ do_pushdecl (tree decl, bool hiding)
 		  /* Don't attempt to push it.  */
 		  return error_mark_node;
 		}
-	      /* Hide it from ordinary lookup.  */
-	      DECL_ANTICIPATED (decl) = true;
 	    }
 	}
 
@@ -6730,7 +6701,6 @@ lookup_elaborated_type_1 (tree name, TAG_how how)
 		HIDDEN_TYPE_BINDING_P (iter) = false;
 
 		/* Unanticipate the decl itself.  */
-		DECL_ANTICIPATED (found) = false;
 		DECL_FRIEND_P (found) = false;
 
 		gcc_checking_assert (TREE_CODE (found) != TEMPLATE_DECL);
@@ -6738,7 +6708,6 @@ lookup_elaborated_type_1 (tree name, TAG_how how)
 		if (tree ti = TYPE_TEMPLATE_INFO (TREE_TYPE (found)))
 		  {
 		    tree tmpl = TI_TEMPLATE (ti);
-		    DECL_ANTICIPATED (tmpl) = false;
 		    DECL_FRIEND_P (tmpl) = false;
 		  }
 	      }
@@ -6799,18 +6768,17 @@ lookup_elaborated_type_1 (tree name, TAG_how how)
 	  if (reveal)
 	    {
 	      /* Reveal the previously hidden thing.  */
-	      DECL_ANTICIPATED (found) = false;
 	      DECL_FRIEND_P (found) = false;
 
 	      if (TREE_CODE (found) == TEMPLATE_DECL)
 		{
-		  DECL_ANTICIPATED (DECL_TEMPLATE_RESULT (found)) = false;
-		  DECL_FRIEND_P (DECL_TEMPLATE_RESULT (found)) = false;
+		  tree res = DECL_TEMPLATE_RESULT (found);
+		  if (DECL_LANG_SPECIFIC (res))
+		    DECL_FRIEND_P (res) = false;
 		}
 	      else if (tree ti = TYPE_TEMPLATE_INFO (TREE_TYPE (found)))
 		{
 		  tree tmpl = TI_TEMPLATE (ti);
-		  DECL_ANTICIPATED (tmpl) = false;
 		  DECL_FRIEND_P (tmpl) = false;
 		}
 	    }
@@ -7019,7 +6987,6 @@ do_pushtag (tree name, tree type, TAG_how how)
 	     ordinary name lookup.  Its corresponding TEMPLATE_DECL
 	     will be marked in push_template_decl.  */
 	  retrofit_lang_decl (tdef);
-	  DECL_ANTICIPATED (tdef) = 1;
 	  DECL_FRIEND_P (tdef) = 1;
 	}
 
