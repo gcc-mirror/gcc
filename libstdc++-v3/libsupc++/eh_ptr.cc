1352ea192513e9a45808b8034df62b9434c674a7
Jonathan Wakely <jwakely@redhat.com>
2020-10-06 16:55:06 +0100

    libstdc++: Inline std::exception_ptr members [PR 90295]
    This inlines most members of std::exception_ptr so that all operations
    on a null exception_ptr can be optimized away. This benefits code like
    std::future and coroutines where an exception_ptr object is present to
    cope with exceptional cases, but is usually not used and remains null.
    
    Since those functions were previously non-inline we have to continue to
    export them from the library, for objects that were compiled against the
    old headers and expect to find definitions in the library.
    
    In order to inline the copy constructor and destructor we need to export
    the _M_addref() and _M_release() members that increment/decrement the
    reference count when copying/destroying a non-null exception_ptr. The
    copy ctor and dtor check for null and don't call _M_addref and
    _M_release unless they need to. The checks for null pointers in
    _M_addref and _M_release are still needed because old code might call
    them without checking for null first. But we can use __builtin_expect to
    predict that they are usually called for the non-null case.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/90295
    	* config/abi/pre/gnu.ver (CXXABI_1.3.13): New symbol version.
    	(exception_ptr::_M_addref(), exception_ptr::_M_release()):
    	Export symbols.
    	* libsupc++/eh_ptr.cc (exception_ptr::exception_ptr()):
    	Remove out-of-line definition.
    	(exception_ptr::exception_ptr(const exception_ptr&)):
    	Likewise.
    	(exception_ptr::~exception_ptr()): Likewise.
    	(exception_ptr::operator=(const exception_ptr&)):
    	Likewise.
    	(exception_ptr::swap(exception_ptr&)): Likewise.
    	(exception_ptr::_M_addref()): Add branch prediction.
    	* libsupc++/exception_ptr.h (exception_ptr::operator bool):
    	Add noexcept.
    	[!_GLIBCXX_EH_PTR_COMPAT] (operator==, operator!=): Define
    	inline as hidden friends. Remove declarations at namespace
    	scope.
    	(exception_ptr::exception_ptr()): Define inline.
    	(exception_ptr::exception_ptr(const exception_ptr&)):
    	Likewise.
    	(exception_ptr::~exception_ptr()): Likewise.
    	(exception_ptr::operator=(const exception_ptr&)):
    	Likewise.
    	(exception_ptr::swap(exception_ptr&)): Likewise.
    	* testsuite/util/testsuite_abi.cc: Add CXXABI_1.3.13.
    	* testsuite/18_support/exception_ptr/90295.cc: New test.

--- libstdc++-v3/libsupc++/eh_ptr.cc
+++ libstdc++-v3/libsupc++/eh_ptr.cc
@@ -61,41 +61,18 @@ static_assert( adjptr<__cxa_exception>()
 #endif
 }
 
-std::__exception_ptr::exception_ptr::exception_ptr() noexcept
-: _M_exception_object(0) { }
-
-
 std::__exception_ptr::exception_ptr::exception_ptr(void* obj) noexcept
 : _M_exception_object(obj)  { _M_addref(); }
 
 
 std::__exception_ptr::exception_ptr::exception_ptr(__safe_bool) noexcept
-: _M_exception_object(0) { }
-
-
-std::__exception_ptr::
-exception_ptr::exception_ptr(const exception_ptr& other) noexcept
-: _M_exception_object(other._M_exception_object)
-{ _M_addref(); }
-
-
-std::__exception_ptr::exception_ptr::~exception_ptr() noexcept
-{ _M_release(); }
-
-
-std::__exception_ptr::exception_ptr&
-std::__exception_ptr::
-exception_ptr::operator=(const exception_ptr& other) noexcept
-{
-  exception_ptr(other).swap(*this);
-  return *this;
-}
+: _M_exception_object(nullptr) { }
 
 
 void
 std::__exception_ptr::exception_ptr::_M_addref() noexcept
 {
-  if (_M_exception_object)
+  if (__builtin_expect(_M_exception_object != nullptr, true))
     {
       __cxa_refcounted_exception *eh =
 	__get_refcounted_exception_header_from_obj (_M_exception_object);
@@ -107,7 +84,7 @@ std::__exception_ptr::exception_ptr::_M_addref() noexcept
 void
 std::__exception_ptr::exception_ptr::_M_release() noexcept
 {
-  if (_M_exception_object)
+  if (__builtin_expect(_M_exception_object != nullptr, true))
     {
       __cxa_refcounted_exception *eh =
 	__get_refcounted_exception_header_from_obj (_M_exception_object);
@@ -117,7 +94,7 @@ std::__exception_ptr::exception_ptr::_M_release() noexcept
 	    eh->exc.exceptionDestructor (_M_exception_object);
 
           __cxa_free_exception (_M_exception_object);
-          _M_exception_object = 0;
+          _M_exception_object = nullptr;
         }
     }
 }
@@ -128,14 +105,6 @@ std::__exception_ptr::exception_ptr::_M_get() const noexcept
 { return _M_exception_object; }
 
 
-void
-std::__exception_ptr::exception_ptr::swap(exception_ptr &other) noexcept
-{
-  void *tmp = _M_exception_object;
-  _M_exception_object = other._M_exception_object;
-  other._M_exception_object = tmp;
-}
-
 
 // Retained for compatibility with CXXABI_1.3.
 void
@@ -145,15 +114,14 @@ std::__exception_ptr::exception_ptr::_M_safe_bool_dummy() noexcept { }
 // Retained for compatibility with CXXABI_1.3.
 bool
 std::__exception_ptr::exception_ptr::operator!() const noexcept
-{ return _M_exception_object == 0; }
+{ return _M_exception_object == nullptr; }
 
 
 // Retained for compatibility with CXXABI_1.3.
 std::__exception_ptr::exception_ptr::operator __safe_bool() const noexcept
 {
-  return _M_exception_object ? &exception_ptr::_M_safe_bool_dummy : 0;
+  return _M_exception_object ? &exception_ptr::_M_safe_bool_dummy : nullptr;
 }
-
 
 const std::type_info*
 std::__exception_ptr::exception_ptr::__cxa_exception_type() const noexcept
@@ -162,15 +130,17 @@ std::__exception_ptr::exception_ptr::__cxa_exception_type() const noexcept
   return eh->exceptionType;
 }
 
-
-bool std::__exception_ptr::operator==(const exception_ptr& lhs,
-				      const exception_ptr& rhs) noexcept
+// Retained for compatibility with CXXABI_1.3.12.
+bool
+std::__exception_ptr::operator==(const exception_ptr& lhs,
+				 const exception_ptr& rhs) noexcept
 { return lhs._M_exception_object == rhs._M_exception_object; }
 
-
-bool std::__exception_ptr::operator!=(const exception_ptr& lhs,
-				      const exception_ptr& rhs) noexcept
-{ return !(lhs == rhs);}
+// Retained for compatibility with CXXABI_1.3.12.
+bool
+std::__exception_ptr::operator!=(const exception_ptr& lhs,
+				 const exception_ptr& rhs) noexcept
+{ return !(lhs == rhs); }
 
 
 std::exception_ptr
