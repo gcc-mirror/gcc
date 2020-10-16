b0e5c3b84ef2c477fe797da59a1aadfbed8445fe
Thomas Schwinge <thomas@codesourcery.com>
2020-10-16 11:20:21 +0200

    Adjust 'gfortran.dg/gomp/order-4.f90' for og10
    Testcase added in og10 commit 835160b024ce34bc6f258e7df366bb5c15e12a4b "OpenMP:
    Handle order(concurrent) clause in gfortran" (cherry picked from commit
    d8140b9ed3c0fed041aedaff3fa4a603984ca10f), but og10 doesn't have commit
    4f2ab6b89e170f1343f935761481c3745fe603b1 "[OpenMP, gimplifier] 'inform' after
    'error' diagnostic".
    
    	gcc/testsuite/
    	* gfortran.dg/gomp/order-4.f90: Adjust.

--- gcc/testsuite/gfortran.dg/gomp/order-4.f90
+++ gcc/testsuite/gfortran.dg/gomp/order-4.f90
@@ -7,7 +7,7 @@ subroutine f1
   use m
   implicit none
   integer :: i
-  !$omp simd order(concurrent)  ! { dg-message "note: enclosing region" } */
+  !$omp simd order(concurrent)  ! { dg-error "enclosing region" }
   do i = 1, 64
     t = t + 1  ! { dg-error "threadprivate variable 't' used in a region with 'order\\(concurrent\\)' clause" } */
   end do
@@ -17,9 +17,10 @@ subroutine f2
   use m
   implicit none
   integer :: i
-  !$omp do simd order(concurrent) ! { dg-message "note: enclosing region" } */
+  !$omp do simd order(concurrent) ! { dg-error "enclosing region" }
   do i = 1, 64
     t = t + 1  ! { dg-error "threadprivate variable 't' used in a region with 'order\\(concurrent\\)' clause" } */
+    ! { dg-error "enclosing region" "" { target *-*-* } .-1 }
   end do
 end
 
@@ -27,7 +28,7 @@ subroutine f3
   use m
   implicit none
   integer :: i
-  !$omp do order(concurrent)  ! { dg-message "note: enclosing region" } */
+  !$omp do order(concurrent)  ! { dg-error "enclosing region" }
   do i = 1, 64
     t = t + 1  ! { dg-error "threadprivate variable 't' used in a region with 'order\\(concurrent\\)' clause" } */
   end do
