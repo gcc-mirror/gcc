853067ac5e2662ce0fd32539ab6b3f3e1792834a
Nathan Sidwell <nathan@acm.org>
2020-10-21 12:37:20 -0400

    Trivial darwin test fix
    	gcc/testsuite/
    	* g++.dg/modules/builtin-3_[ab].C: Scans for darwin, thanks Iain

--- gcc/testsuite/g++.dg/modules/builtin-3_a.C
+++ gcc/testsuite/g++.dg/modules/builtin-3_a.C
@@ -29,12 +29,15 @@ export inline int count (int a, ...)
 
 // The implementation details of va_list's are target-specific.
 // Usually one of two patterns though
-// { dg-final { scan-lang-dump-not { Cluster members:\n  \[0\]=decl declaration '::__builtin_va_list'\n  \[1\]=binding '::__builtin_va_list'\n} module { target i?86-*-* x86_64-*-* } } }
-// { dg-final { scan-lang-dump {Wrote GMF:-[0-9]* type_decl:'::__builtin_va_list'@builtins} module { target i?86-*-* x86_64-*-* } } }
-// { dg-final { scan-lang-dump {Writing:-[0-9]*'s named merge key \(decl\) type_decl:'::__builtin_va_list'} module { target i?86-*-* x86_64-*-* } } }
+// { dg-final { scan-lang-dump-not { Cluster members:\n  \[0\]=decl declaration '::__builtin_va_list'\n  \[1\]=binding '::__builtin_va_list'\n} module { target i?86-*-linux* x86_64-*-linux* } } }
+// { dg-final { scan-lang-dump {Wrote GMF:-[0-9]* type_decl:'::__builtin_va_list'@builtins} module { target i?86-*-linux* x86_64-*-linux* } } }
+// { dg-final { scan-lang-dump {Writing:-[0-9]*'s named merge key \(decl\) type_decl:'::__builtin_va_list'} module { target i?86-*-linux* x86_64-*-linux* } } }
 
-// { dg-final { scan-lang-dump {Wrote fixed:[0-9]* record_type:'__va_list'} module { target aarch64*-*-* } } }
-// { dg-final { scan-lang-dump {Wrote fixed:[0-9]* pointer_type:'::__builtin_va_list'} module { target powerpc*-*-* } } }
+// { dg-final { scan-lang-dump {Writing:-1's named merge key \(decl\) type_decl:'::__gnuc_va_list'} module { target *-*-darwin* } } }
+// { dg-final { scan-lang-dump {Wrote GMF:-3 type_decl:'::__gnuc_va_list'@builtins} module { target *-*-darwin* } } }
+
+// { dg-final { scan-lang-dump {Wrote fixed:[0-9]* record_type:'__va_list'} module { target aarch64*-*-linux* } } }
+// { dg-final { scan-lang-dump {Wrote fixed:[0-9]* pointer_type:'::__builtin_va_list'} module { target powerpc*-*-linux* } } }
 
 // { dg-final { scan-lang-dump-not { Cluster members:\n  \[0\]=decl declaration '::va_list'\n  \[1\]=binding '::va_list'\n} module } }
 // { dg-final { scan-lang-dump {Wrote GMF:-[0-9]* type_decl:'::va_list'@builtins} module } }
