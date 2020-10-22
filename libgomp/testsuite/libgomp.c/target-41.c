17c5b7e1dc47bab6e6cedbf4b2d88cef3283533e
Jakub Jelinek <jakub@redhat.com>
2020-10-22 09:33:17 +0200

    openmp: Add test for OMP_TARGET_OFFLOAD=mandatory for cases where it must not fail
    2020-10-22  Jakub Jelinek  <jakub@redhat.com>
    
    	* testsuite/libgomp.c/target-41.c: New test.

---
+++ libgomp/testsuite/libgomp.c/target-41.c
@@ -0,0 +1,33 @@
+/* { dg-set-target-env-var OMP_TARGET_OFFLOAD "mandatory" } */
+
+#include <omp.h>
+#include <stdlib.h>
+
+int v;
+
+void
+foo (void)
+{
+  v++;
+}
+
+#pragma omp declare target to (v, foo)
+
+int
+main ()
+{
+  /* OMP_TARGET_OFFLOAD=mandatory shouldn't fail for host fallback
+     if it is because the program explicitly asked for the host
+     fallback through if(false) or omp_get_initial_device () as
+     the device.  */
+  #pragma omp target if (v)
+  foo ();
+  #pragma omp target device (omp_get_initial_device ())
+  foo ();
+  omp_set_default_device (omp_get_initial_device ());
+  #pragma omp target
+  foo ();
+  if (v != 3)
+    abort ();
+  return 0;
+}
