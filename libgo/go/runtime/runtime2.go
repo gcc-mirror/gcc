668894d7b584b40ddb46e9e2e2ffa637f4d732e5
Ian Lance Taylor <iant@golang.org>
2020-10-24 14:47:44 -0700

    libgo: update to Go 1.15.3 release
    Reviewed-on: https://go-review.googlesource.com/c/gofrontend/+/265717

--- libgo/go/runtime/runtime2.go
+++ libgo/go/runtime/runtime2.go
@@ -450,6 +450,10 @@ type g struct {
 	// copying needs to acquire channel locks to protect these
 	// areas of the stack.
 	activeStackChans bool
+	// parkingOnChan indicates that the goroutine is about to
+	// park on a chansend or chanrecv. Used to signal an unsafe point
+	// for stack shrinking. It's a boolean value, but is updated atomically.
+	parkingOnChan uint8
 
 	raceignore     int8     // ignore race detection events
 	sysblocktraced bool     // StartTrace has emitted EvGoInSyscall about this goroutine
@@ -940,11 +944,6 @@ type _defer struct {
 
 // panics
 // This is the gccgo version.
-//
-// This is marked go:notinheap because _panic values must only ever
-// live on the stack.
-//
-//go:notinheap
 type _panic struct {
 	// The next entry in the stack.
 	link *_panic
