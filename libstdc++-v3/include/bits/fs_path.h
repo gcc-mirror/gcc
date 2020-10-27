51d19b9b06cef05a5368c8f07c2c6269e6b1f8b0
Jonathan Wakely <jwakely@redhat.com>
2020-10-27 14:48:50 +0000

    libstdc++: Add noexcept to declaration of path::_List members
    libstdc++-v3/ChangeLog:
    
    	* include/bits/fs_path.h (path::_List::begin, path::_List::end):
    	Add noexcept to match definitions in src/c++17/fs_path.cc.
    
    (cherry picked from commit fe9d058ca28e0e3a50c44096ce112763057e686e)

--- libstdc++-v3/include/bits/fs_path.h
+++ libstdc++-v3/include/bits/fs_path.h
@@ -583,10 +583,10 @@ _GLIBCXX_BEGIN_NAMESPACE_CXX11
       // All the member functions below here have a precondition !empty()
       // (and they should only be called from within the library).
 
-      iterator begin();
-      iterator end();
-      const_iterator begin() const;
-      const_iterator end() const;
+      iterator begin() noexcept;
+      iterator end() noexcept;
+      const_iterator begin() const noexcept;
+      const_iterator end() const noexcept;
 
       value_type& front() noexcept;
       value_type& back() noexcept;
