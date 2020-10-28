a0e4d7b44c544c84cffc7ff9c64b6f1af14fb08d
Thomas Rodgers <trodgers@redhat.com>
2020-10-28 11:52:17 -0700

    libstdc++: Implement C++20 features for <sstream>
    New ctors and ::view() accessor for -
      * basic_stingbuf
      * basic_istringstream
      * basic_ostringstream
      * basic_stringstreamm
    
    New ::get_allocator() accessor for basic_stringbuf.
    
    libstdc++-v3/ChangeLog:
    	* acinclude.m4 (glibcxx_SUBDIRS): Add src/c++20.
    	* config/abi/pre/gnu.ver (GLIBCXX_3.4.29): New symbols.
    	* configure: Regenerate.
    	* include/std/sstream:
    	(basic_stringbuf::basic_stringbuf(allocator const&)): New constructor.
    	(basic_stringbuf::basic_stringbuf(openmode, allocator const&)): Likewise.
    	(basic_stringbuf::basic_stringbuf(basic_string&&, openmode)): Likewise.
    	(basic_stringbuf::basic_stringbuf(basic_stringbuf&&, allocator const&)):
    	Likewise.
    	(basic_stringbuf::get_allocator()): New method.
    	(basic_stringbuf::view()): Likewise.
    	(basic_istringstream::basic_istringstream(basic_string&&, openmode)):
    	New constructor.
    	(basic_istringstream::basic_istringstream(openmode, allocator const&)):
    	Likewise
    	(basic_istringstream::view()): New method.
    	(basic_ostringstream::basic_ostringstream(basic_string&&, openmode)):
    	New constructor.
    	(basic_ostringstream::basic_ostringstream(openmode, allocator const&)):
    	Likewise
    	(basic_ostringstream::view()): New method.
    	(basic_stringstream::basic_stringstream(basic_string&&, openmode)):
    	New constructor.
    	(basic_stringstream::basic_stringstream(openmode, allocator const&)):
    	Likewise
    	(basic_stringstream::view()): New method.
    	* src/Makefile.in: Add c++20 directory.
    	* src/Makefile.am: Regenerate.
    	* src/c++20/Makefile.am: Add makefile for new sub-directory.
    	* src/c++20/Makefile.in: Generate.
    	* src/c++20/sstream-inst.cc: New file defining explicit
    	instantiations for basic_stringbuf, basic_istringstream,
    	basic_ostringstream, and basic_stringstream member functions
    	added in C++20.
    	* testsuite/27_io/basic_stringbuf/cons/char/2.cc: New test.
    	* testsuite/27_io/basic_stringbuf/cons/wchar_t/2.cc: Likewise.
    	* testsuite/27_io/basic_stringbuf/view/char/1.cc: Likewise.
    	* testsuite/27_io/basic_stringbuf/view/wchar_t/1.cc: Likewise.
    	* testsuite/27_io/basic_istringstream/cons/char/1.cc: Likewise.
    	* testsuite/27_io/basic_istringstream/cons/wchar_t/1.cc: Likewise.
    	* testsuite/27_io/basic_istringstream/view/char/1.cc: Likewise.
    	* testsuite/27_io/basic_istringstream/view/wchar_t/1.cc: Likewise.
    	* testsuite/27_io/basic_ostringstream/cons/char/1.cc: Likewise.
    	* testsuite/27_io/basic_ostringstream/cons/wchar_t/1.cc: Likewise.
    	* testsuite/27_io/basic_ostringstream/view/char/1.cc: Likewise.
    	* testsuite/27_io/basic_ostringstream/view/wchar_t/1.cc: Likewise.
    	* testsuite/27_io/basic_stringstream/cons/char/1.cc: Likewise.
    	* testsuite/27_io/basic_stringstream/cons/wchar_t/1.cc: Likewise.
    	* testsuite/27_io/basic_stringstream/view/char/1.cc: Likewise.
    	* testsuite/27_io/basic_stringstream/view/wchar_t/1.cc: Likewise.

---
+++ libstdc++-v3/testsuite/27_io/basic_istringstream/view/wchar_t/1.cc
@@ -0,0 +1,35 @@
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// 27.7.1.1  basic_stringbuf constructors  [lib.stringbuf.cons]
+
+// { dg-options "-std=gnu++2a" }
+// { dg-do run { target c++2a } }
+// { dg-require-effective-target cxx11-abi }
+
+#include <sstream>
+#include <string_view>
+#include <testsuite_hooks.h>
+
+int
+main()
+{
+  std::wstring s(L"This is a test");
+  std::wistringstream stm(s);
+  VERIFY( stm.view() == s );
+  return 0;
+}
---
+++ libstdc++-v3/testsuite/27_io/basic_ostringstream/cons/char/1.cc
@@ -0,0 +1,85 @@
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// C++20 29.8.2.2  basic_stringbuf constructors  [stringbuf.cons
+
+// { dg-options "-std=gnu++2a" }
+// { dg-do run { target c++2a } }
+// { dg-require-effective-target cxx11-abi }
+
+#include <sstream>
+#include <string>
+#include <testsuite_allocator.h>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  std::ostringstream::allocator_type a;
+  std::ostringstream stm(std::ios_base::in, a);
+}
+
+auto const cstr = "This is a test";
+
+void
+test02()
+{
+  std::string s1(cstr);
+  std::ostringstream stm(std::move(s1));
+  VERIFY( s1.empty() );
+
+  std::string s2(cstr);
+  VERIFY( stm.str() == s2 );
+}
+
+void
+test03()
+{
+  using alloc_type = __gnu_test::tracker_allocator<char>;
+  using str_type = std::basic_string<char, std::char_traits<char>, alloc_type>;
+
+  auto const mode = std::ios_base::out;
+  str_type s1(cstr);
+
+  {
+    std::ostringstream::allocator_type a;
+    std::ostringstream sbuf(s1, mode, a);
+    std::string s2(cstr);
+    VERIFY( sbuf.str() == s2 );
+  }
+
+  {
+    std::ostringstream sbuf(s1, mode);
+    std::string s2(cstr);
+    VERIFY( sbuf.str() == s2 );
+  }
+
+  {
+    std::ostringstream sbuf(s1);
+    std::string s2(cstr);
+    VERIFY( sbuf.str() == s2 );
+  }
+}
+
+int
+main()
+{
+  test01();
+  test02();
+  test03();
+  return 0;
+}
