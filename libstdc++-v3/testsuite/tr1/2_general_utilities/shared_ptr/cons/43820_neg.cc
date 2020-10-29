52ddf0d458f063f6c9e07e726f9b46206f71e4c4
Jonathan Wakely <jwakely@redhat.com>
2020-10-29 22:47:22 +0000

    libstdc++: Prevent deprecation warnings from <tr1/shared_ptr>
    libstdc++-v3/ChangeLog:
    
    	* include/tr1/shared_ptr.h (__shared_count, __shared_ptr)
    	(shared_ptr): Add diagnostic pragmas around uses of auto_ptr.
    	* testsuite/tr1/2_general_utilities/shared_ptr/cons/43820_neg.cc:
    	Adust dg-error line numbers.

--- libstdc++-v3/testsuite/tr1/2_general_utilities/shared_ptr/cons/43820_neg.cc
+++ libstdc++-v3/testsuite/tr1/2_general_utilities/shared_ptr/cons/43820_neg.cc
@@ -32,10 +32,10 @@ void test01()
 {
   X* px = 0;
   std::tr1::shared_ptr<X> p1(px);   // { dg-error "here" }
-  // { dg-error "incomplete" "" { target *-*-* } 554 }
+  // { dg-error "incomplete" "" { target *-*-* } 559 }
 
   std::tr1::shared_ptr<X> p9(ap());  // { dg-error "here" }
-  // { dg-error "incomplete" "" { target *-*-* } 593 }
+  // { dg-error "incomplete" "" { target *-*-* } 600 }
 }
 
 // Ignore additional diagnostic given with -Wsystem-headers:
