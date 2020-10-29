caee9e676a567b1e64579e0cf10493ada40f76bf
Andrea Corallo <andrea.corallo@arm.com>
2020-10-29 13:56:17 +0100

    arm: Add vld1_bf16 + vld1q_bf16 intrinsics
    gcc/ChangeLog
    
    2020-10-29  Andrea Corallo  <andrea.corallo@arm.com>
    
    	* config/arm/arm-builtins.c (VAR14): Define macro.
    	* config/arm/arm_neon_builtins.def: Touch for:
    	__builtin_neon_vld1v4bf, __builtin_neon_vld1v8bf.
    	* config/arm/arm_neon.h (vld1_bf16, vld1q_bf16): Add intrinsics.
    
    gcc/testsuite/ChangeLog
    
    2020-10-29  Andrea Corallo  <andrea.corallo@arm.com>
    
    	* gcc.target/arm/simd/vld1_bf16_1.c: New test.

--- gcc/config/arm/arm-builtins.c
+++ gcc/config/arm/arm-builtins.c
@@ -946,6 +946,9 @@ typedef struct {
 #define VAR13(T, N, A, B, C, D, E, F, G, H, I, J, K, L, M) \
   VAR12 (T, N, A, B, C, D, E, F, G, H, I, J, K, L) \
   VAR1 (T, N, M)
+#define VAR14(T, N, A, B, C, D, E, F, G, H, I, J, K, L, M, O) \
+  VAR13 (T, N, A, B, C, D, E, F, G, H, I, J, K, L, M) \
+  VAR1 (T, N, O)
 
 /* The builtin data can be found in arm_neon_builtins.def, arm_vfp_builtins.def
    and arm_acle_builtins.def.  The entries in arm_neon_builtins.def require
