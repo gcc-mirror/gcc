3553c658533e430b232997bdfd97faf6606fb102
Dennis Zhang <denzha01@e124712.cambridge.arm.com>
2020-11-03 16:56:02 +0000

    aarch64: intrinsics extract half of bf16 vector
    This patch implements ACLE intrinsics vget_low_bf16 and vget_high_bf16
    to extract lower or higher half from a bfloat16x8 vector. The
    vget_high_bf16 is done by 'dup' instruction. The vget_low_bf16 is just
    to return the lower half of a vector register. Tests include both big-
    and little-endian cases.
    
    gcc/ChangeLog:
    
    2020-11-03  Dennis Zhang  <dennis.zhang@arm.com>
    
    	* config/aarch64/aarch64-simd-builtins.def (vget_lo_half): New entry.
    	(vget_hi_half): Likewise.
    	* config/aarch64/aarch64-simd.md (aarch64_vget_lo_halfv8bf): New entry.
    	(aarch64_vget_hi_halfv8bf): Likewise.
    	* config/aarch64/arm_neon.h (vget_low_bf16): New intrinsic.
    	(vget_high_bf16): Likewise.
    
    gcc/testsuite/ChangeLog
    
    	* gcc.target/aarch64/advsimd-intrinsics/bf16_get.c: New test.
    	* gcc.target/aarch64/advsimd-intrinsics/bf16_get-be.c: New test.

---
+++ gcc/testsuite/gcc.target/aarch64/advsimd-intrinsics/bf16_get.c
@@ -0,0 +1,27 @@
+/* { dg-do assemble { target { aarch64*-*-* } } } */
+/* { dg-require-effective-target arm_v8_2a_bf16_neon_ok } */
+/* { dg-add-options arm_v8_2a_bf16_neon } */
+/* { dg-additional-options "-save-temps" } */
+/* { dg-final { check-function-bodies "**" "" {-O[^0]} } } */
+/* { dg-skip-if "" { *-*-* } { "-fno-fat-lto-objects" } } */
+
+#include <arm_neon.h>
+
+/*
+**test_vget_low_bf16:
+**     ret
+*/
+bfloat16x4_t test_vget_low_bf16 (bfloat16x8_t a)
+{
+  return vget_low_bf16 (a);
+}
+
+/*
+**test_vget_high_bf16:
+**     dup	d0, v0.d\[1\]
+**     ret
+*/
+bfloat16x4_t test_vget_high_bf16 (bfloat16x8_t a)
+{
+  return vget_high_bf16 (a);
+}
