c71d97549d4dca17d452b4a47e406c5f6b2a1a56
Thomas Schwinge <thomas@codesourcery.com>
2020-11-06 09:18:06 +0100

    [testsuite] Enable column location checking for 'dg-optimized', 'dg-missed'
    'process-message' would like the 'msgprefix' argument without trailing space.
    
    This is a small bug-fix for commit ed2d9d3720adef3a260b8a55e17e744352a901fc
    "dumpfile.c: use prefixes other than 'note: ' for
    MSG_{OPTIMIZED_LOCATIONS|MISSED_OPTIMIZATION}", which added 'dg-optimized',
    'dg-missed'.
    
    	gcc/testsuite/
    	* lib/gcc-dg.exp (dg-optimized, dg-missed): Fix 'process-message'
    	call.
    	* gcc.dg/vect/nodump-vect-opt-info-1.c: Demonstrate.
    	* gcc.dg/vect/nodump-vect-opt-info-2.c: Likewise.
    
    (cherry picked from commit 24b553d0f73ffea2551a77c67859ad6fe44110a6)

--- gcc/testsuite/gcc.dg/vect/nodump-vect-opt-info-2.c
+++ gcc/testsuite/gcc.dg/vect/nodump-vect-opt-info-2.c
@@ -6,7 +6,7 @@ extern void accumulate (int x, int *a);
 int test_missing_function_defn (int *arr, int n) /* { dg-message "vectorized 0 loops in function" } */
 {
   int sum = 0;
-  for (int i = 0; i < n; ++i) /* { dg-missed "couldn't vectorize loop" } */
-    accumulate (arr[i], &sum); /* { dg-missed "statement clobbers memory: accumulate \\(.*\\);" } */
+  for (int i = 0; i < n; ++i) /* { dg-missed "3: couldn't vectorize loop" } */
+    accumulate (arr[i], &sum); /* { dg-missed "5: statement clobbers memory: accumulate \\(.*\\);" } */
   return sum;
 }
