b2a28b8bc17e591572200b7089c2992ab02034c1
Iain Sandoe <iain@sandoe.co.uk>
2020-11-07 08:43:37 +0000

    Ada : Fix bootstrap after r11-4793.
    The patch omitted a change for Ada, fixed thus.
    
    gcc/ada/ChangeLog:
    
    	* gcc-interface/misc.c (gnat_printable_name): Change
    	DECL_IS_BUILTIN -> DECL_IS_UNDECLARED_BUILTIN.

--- gcc/ada/gcc-interface/misc.c
+++ gcc/ada/gcc-interface/misc.c
@@ -559,7 +559,7 @@ gnat_printable_name (tree decl, int verbosity)
 
   __gnat_decode (coded_name, ada_name, 0);
 
-  if (verbosity == 2 && !DECL_IS_BUILTIN (decl))
+  if (verbosity == 2 && !DECL_IS_UNDECLARED_BUILTIN (decl))
     {
       Set_Identifier_Casing (ada_name, DECL_SOURCE_FILE (decl));
       return ggc_strdup (Name_Buffer);
