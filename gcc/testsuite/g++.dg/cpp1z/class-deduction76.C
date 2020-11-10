a210d404d08e363af4b2e2a60986c3cb08f8ebc5
Marek Polacek <polacek@redhat.com>
2020-11-10 14:57:19 -0500

    c++: Add 5 unfixed tests.
    A couple of dg-ice tests.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/52830
    	PR c++/88982
    	PR c++/90799
    	PR c++/87765
    	PR c++/89565
    	* g++.dg/cpp0x/constexpr-52830.C: New test.
    	* g++.dg/cpp0x/vt-88982.C: New test.
    	* g++.dg/cpp1z/class-deduction76.C: New test.
    	* g++.dg/cpp1z/constexpr-lambda26.C: New test.
    	* g++.dg/cpp2a/nontype-class39.C: New test.

---
+++ gcc/testsuite/g++.dg/cpp1z/class-deduction76.C
@@ -0,0 +1,25 @@
+// PR c++/90799
+// { dg-do compile { target c++17 } }
+// { dg-ice "unify" }
+
+template<class T>
+void foo() noexcept(T::value);
+
+struct S {
+    static constexpr const bool value = true;
+
+    template<class T>
+    void bar() noexcept(T::value);
+};
+
+template<class... Args, bool is_noexcept>
+constexpr bool is_noexcept_function(void(Args...) noexcept(is_noexcept)) noexcept {
+    return is_noexcept;
+}
+
+template<class... Args, bool is_noexcept>
+constexpr bool is_noexcept_member_function(void(S::*)(Args...) noexcept(is_noexcept)) noexcept {
+    return is_noexcept;
+}
+
+static_assert(is_noexcept_function(foo<S>));
