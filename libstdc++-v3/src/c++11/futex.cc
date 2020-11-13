730e1357dfb9aff481d6c47a21ef748f0d810d4f
Jonathan Wakely <jwakely@redhat.com>
2020-11-13 19:11:02 +0000

    libstdc++: Remove redundant overflow check for futex timeout [PR 93456]
    The relative_timespec function already checks for the case where the
    specified timeout is in the past, so the difference can never be
    negative. That means we dn't need to check if it's more negative than
    the minimum time_t value.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/93456
    	* src/c++11/futex.cc (relative_timespec): Remove redundant check
    	negative values.
    	* testsuite/30_threads/future/members/93456.cc: New.
    
    (cherry picked from commit b8d36dcc917e8a06d8c20b9f5ecc920ed2b9e947)

--- libstdc++-v3/src/c++11/futex.cc
+++ libstdc++-v3/src/c++11/futex.cc
@@ -42,6 +42,8 @@ namespace std _GLIBCXX_VISIBILITY(default)
 {
 _GLIBCXX_BEGIN_NAMESPACE_VERSION
 
+  using __gnu_cxx::__int_traits;
+
 namespace
 {
   // Return the relative duration from (now_s + now_ns) to (abs_s + abs_ns)
@@ -59,21 +61,23 @@ namespace
 	return rt;
       }
 
-    auto rel_s = abs_s.count() - now_s;
+    const auto rel_s = abs_s.count() - now_s;
 
-    // Avoid overflows
-    if (rel_s > __gnu_cxx::__int_traits<time_t>::__max)
-      rel_s = __gnu_cxx::__int_traits<time_t>::__max;
-    else if (rel_s < __gnu_cxx::__int_traits<time_t>::__min)
-      rel_s = __gnu_cxx::__int_traits<time_t>::__min;
-
-    // Convert the absolute timeout value to a relative timeout
-    rt.tv_sec = rel_s;
-    rt.tv_nsec = abs_ns.count() - now_ns;
-    if (rt.tv_nsec < 0)
+    // Convert the absolute timeout to a relative timeout, without overflow.
+    if (rel_s > __int_traits<time_t>::__max) [[unlikely]]
       {
-	rt.tv_nsec += 1000000000;
-	--rt.tv_sec;
+	rt.tv_sec = __int_traits<time_t>::__max;
+	rt.tv_nsec = 999999999;
+      }
+    else
+      {
+	rt.tv_sec = rel_s;
+	rt.tv_nsec = abs_ns.count() - now_ns;
+	if (rt.tv_nsec < 0)
+	  {
+	    rt.tv_nsec += 1000000000;
+	    --rt.tv_sec;
+	  }
       }
 
     return rt;
---
+++ libstdc++-v3/testsuite/30_threads/future/members/93456.cc
@@ -0,0 +1,48 @@
+// { dg-do run }
+// { dg-additional-options "-pthread" { target pthread } }
+// { dg-require-effective-target c++11 }
+// { dg-require-gthreads "" }
+
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+
+#include <future>
+#include <chrono>
+#include <climits>
+#include <testsuite_hooks.h>
+
+namespace chrono = std::chrono;
+
+void test01()
+{
+  std::future<void> fut = std::async(std::launch::async, [] {
+    std::this_thread::sleep_for(chrono::seconds(4));
+  });
+
+  // A time in the distant future, but which overflows 32-bit time_t:
+  auto then = chrono::system_clock::now() + chrono::seconds(UINT_MAX + 2LL);
+  auto status = fut.wait_until(then);
+  // The wait_until call should have waited for the result to be ready.
+  // If converting the time_point to time_t overflows, it will timeout.
+  VERIFY(status == std::future_status::ready);
+}
+
+int main()
+{
+  test01();
+}
