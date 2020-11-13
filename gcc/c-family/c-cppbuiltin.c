d4a3152d3f3f62ccd614df5da6c3c0d2f6e5fa0d
Jason Merrill <jason@redhat.com>
2020-11-13 14:15:46 -0500

    c++: Add feature test macro for C++20 using enum.
    Missing piece from the 'using enum' implementation patch.
    
    gcc/c-family/ChangeLog:
    
    	* c-cppbuiltin.c (c_cpp_builtins): Define __cpp_using_enum.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp2a/feat-cxx2a.C: Check it.

--- gcc/c-family/c-cppbuiltin.c
+++ gcc/c-family/c-cppbuiltin.c
@@ -1005,6 +1005,7 @@ c_cpp_builtins (cpp_reader *pfile)
 	  cpp_define (pfile, "__cpp_constexpr_dynamic_alloc=201907L");
 	  cpp_define (pfile, "__cpp_impl_three_way_comparison=201907L");
 	  cpp_define (pfile, "__cpp_aggregate_paren_init=201902L");
+	  cpp_define (pfile, "__cpp_using_enum=201907L");
 	}
       if (flag_concepts)
         {
