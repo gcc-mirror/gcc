9243f0fba68339fa6d8bcac35b0df6e88c1ad099
David Edelsohn <dje.gcc@gmail.com>
2020-11-15 14:56:47 -0500

    testsuite: fix ipa/modref-2.c on ilp32
    ipa/modref-2.c output for Parm 1 depends on the word size.  This patch
    updates the testcase to expect the appropriate result for ilp32 and lp64.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/ipa/modref-2.c: Add ilp32 expected result.

--- gcc/testsuite/gcc.dg/ipa/modref-2.c
+++ gcc/testsuite/gcc.dg/ipa/modref-2.c
@@ -11,5 +11,6 @@ test2 (double x, double *y)
  __builtin_modf (x,y);
 }
 /* 321*8 */
-/* { dg-final { scan-ipa-dump "Parm 0 param offset:0 offset:0 size:-1 max_size:2568" "modref"  } } */
-/* { dg-final { scan-ipa-dump "Parm 1 param offset:0 offset:0 size:-1 max_size:64" "modref"  } } */
+/* { dg-final { scan-ipa-dump "Parm 0 param offset:0 offset:0 size:-1 max_size:2568" "modref" } } */
+/* { dg-final { scan-ipa-dump "Parm 1 param offset:0 offset:0 size:-1 max_size:64" "modref" { target lp64 } } } */
+/* { dg-final { scan-ipa-dump "Parm 1 param offset:0 offset:0 size:-1 max_size:32" "modref" { target ilp32 } } } */
