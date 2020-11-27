Rust-GCC/gccrs

Initial target hook construction
Creation of basic target_os, target_env, target_vendor, and target_family for all GCC targets. Also target_cpu and target_features for i386 - will do target_features for other targets when I can find exactly what is considered a target_feature by rustc

gcc/config/i386/linux-common.h
@@ -30,6 +30,20 @@ along with GCC; see the file COPYING3.  If not see
 #define EXTRA_TARGET_D_OS_VERSIONS()		\
   ANDROID_TARGET_D_OS_VERSIONS();
 
+#define EXTRA_TARGET_RUST_OS_INFO()		\
+  ANDROID_TARGET_RUST_OS_INFO();
+// TODO: decide on whether following c frontend style or d one - leaning towards c
+
+#ifdef TARGET_RUST_OS_INFO
+# error "TARGET_RUST_OS_INFO already defined in linux-common.h (i386) - c++ undefines it and redefines it."
+# error "note that this above error (linux-common-i386) is expected due to already defining EXTRA_TARGET stuff"
+#endif
+#define TARGET_RUST_OS_INFO()               \
+  do {                                      \
+    GNU_USER_TARGET_RUST_OS_INFO();         \
+    ANDROID_TARGET_RUST_OS_INFO();          \
+  } while (0)
+
 #define GNU_USER_TARGET_D_CRITSEC_SIZE		\
   (TARGET_64BIT ? (POINTER_SIZE == 64 ? 40 : 32) : 24)
 

gcc-mirror/gcc

gcc/config/i386/linux-common.h
@@ -30,6 +30,17 @@ along with GCC; see the file COPYING3.  If not see
 #define EXTRA_TARGET_D_OS_VERSIONS()		\
   ANDROID_TARGET_D_OS_VERSIONS();
 
+#define EXTRA_TARGET_RUST_OS_INFO()		\
+  ANDROID_TARGET_RUST_OS_INFO();
+// TODO: decide on whether following c frontend style or d one - leaning towards c
+
+#undef TARGET_RUST_OS_INFO
+#define TARGET_RUST_OS_INFO()               \
+  do {                                      \
+    GNU_USER_TARGET_RUST_OS_INFO();         \
+    ANDROID_TARGET_RUST_OS_INFO();          \
+  } while (0)
+
 #undef CC1_SPEC
 #define CC1_SPEC \
   LINUX_OR_ANDROID_CC (GNU_USER_TARGET_CC1_SPEC, \