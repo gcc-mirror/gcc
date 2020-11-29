56f83e64e179f2ff5bb4161db076d7e6ae1c0a3f
Harald Anlauf <anlauf@gmx.de>
2020-11-29 23:23:16 +0100

    PR fortran/98017 - Suspected regression using PACK
    When substituting a parameter variable of type character, the character
    length was reset to 1.  Fix this by copying the length.
    
    gcc/fortran/ChangeLog:
    
    	* expr.c (simplify_parameter_variable): Fix up character length
    	after copying an array-valued expression.
    
    gcc/testsuite/ChangeLog:
    
    	* gfortran.dg/pr98017.f90: New test.
    
    (cherry picked from commit bb67ad5cff58a707aaae645d4f45a913d8511c86)

--- gcc/fortran/expr.c
+++ gcc/fortran/expr.c
@@ -1898,6 +1898,9 @@ simplify_parameter_variable (gfc_expr *p, int type)
 
   e->rank = p->rank;
 
+  if (e->ts.type == BT_CHARACTER && p->ts.u.cl)
+    e->ts = p->ts;
+
   /* Do not copy subobject refs for constant.  */
   if (e->expr_type != EXPR_CONSTANT && p->ref != NULL)
     e->ref = gfc_copy_ref (p->ref);
