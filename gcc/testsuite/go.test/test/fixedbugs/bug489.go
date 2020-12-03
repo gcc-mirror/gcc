3089f5feef36810c625b5813370a97b4ecc841f8
Ian Lance Taylor <iant@golang.org>
2020-12-03 12:07:04 -0800

    Go testsuite: update new tests to version in source repo

--- gcc/testsuite/go.test/test/fixedbugs/bug489.go
+++ gcc/testsuite/go.test/test/fixedbugs/bug489.go
@@ -1,12 +1,22 @@
-// compiledir
+// compile
 
-// Copyright 2014 The Go Authors.  All rights reserved.
+// Copyright 2014 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// The gccgo compiler had a bug: if one file in a package did a dot
-// import, then an earlier file in the package would incorrectly
-// resolve to the imported names rather than reporting undefined
-// errors.
+// The gccgo compiler had a bug: mentioning a function type in an
+// expression in a function literal messed up the list of variables
+// referenced in enclosing functions.
 
-package ignored
+package main
+
+func main() {
+	v1, v2 := 0, 0
+	f := func() {
+		a := v1
+		g := (func())(nil)
+		b := v2
+		_, _, _ = a, g, b
+	}
+	_, _, _ = v1, v2, f
+}
