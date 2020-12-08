0bd4fecbea3b3da9befe24906f699d63fb28ed71
Nathan Sidwell <nathan@acm.org>
2020-12-08 06:31:31 -0800

    c++: Fix MODULE_VERSION breakage
    Adding includes to module.cc triggered the kind of build failure I
    wanted to check for.  In this case it was MODULE_VERSION not being
    defined, and module.cc's internal #error triggering.  I've relaxed the
    check in Make-lang, so we proviude MODULE_VERSION when DEVPHASE is not
    empty (rather than when it is 'experimental').  AFAICT devphase is
    empty for release builds, and the #error will force us to decide
    whether modules is sufficiently baked at that point.
    
    	gcc/cp
    	* Make-lang.in (MODULE_VERSION): Override when DEVPHASE not empty.
    	* module.cc: Comment.

--- gcc/cp/module.cc
+++ gcc/cp/module.cc
@@ -32,6 +32,7 @@ along with GCC; see the file COPYING3.  If not see
 #define MODULE_MINOR(V) ((V) % 10000)
 #define EXPERIMENT(A,B) (IS_EXPERIMENTAL (MODULE_VERSION) ? (A) : (B))
 #ifndef MODULE_VERSION
+// Be sure you're ready!  Remove #error this before release!
 #error "Shtopp! What are you doing? This is not ready yet."
 #include "bversion.h"
 #define MODULE_VERSION (BUILDING_GCC_MAJOR * 10000U + BUILDING_GCC_MINOR)
