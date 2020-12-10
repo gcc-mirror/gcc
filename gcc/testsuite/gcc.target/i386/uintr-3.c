078c7498908d9b74caff34d12099b5c85470e277
H.J. Lu <hjl.tools@gmail.com>
2020-12-09 18:14:24 -0800

    x86: Update user interrupt handler stack frame
    User interrupt handler stack frame is similar to exception interrupt
    handler stack frame.  Instead of error code, the second argument is
    user interrupt request register vector.
    
    gcc/
    
    	PR target/98219
    	* config/i386/uintrintrin.h (__uintr_frame): Remove uirrv.
    
    gcc/testsuite/
    
    	PR target/98219
    	* gcc.dg/guality/pr98219-1.c: New test.
    	* gcc.dg/guality/pr98219-2.c: Likewise.
    	* gcc.dg/torture/pr98219-1.c: Likewise.
    	* gcc.dg/torture/pr98219-2.c: Likewise.
    	* gcc.target/i386/uintr-2.c: Scan "add[lq] $8, %[er]sp".
    	(uword_t): New.
    	(foo): Add a uword_t argument.
    	(UINTR_hanlder): Likewise.
    	* gcc.target/i386/uintr-3.c: Scan "add[lq] $8, %[er]sp".
    	(uword_t): New.
    	(UINTR_hanlder): Add a uword_t argument.
    	* gcc.target/i386/uintr-4.c (uword_t): New.
    	(UINTR_hanlder): Add a uword_t argument.
    	* gcc.target/i386/uintr-5.c (uword_t): New.
    	(UINTR_hanlder): Add a uword_t argument.

--- gcc/testsuite/gcc.target/i386/uintr-3.c
+++ gcc/testsuite/gcc.target/i386/uintr-3.c
@@ -1,9 +1,13 @@
 /* { dg-do compile { target { ! ia32 } } } */
 /* { dg-options "-O2 -muintr" } */
 /* { dg-final { scan-assembler "uiret" } } */
+/* { dg-final { scan-assembler "add\[lq]\[ \t]\+\\\$8, %\[er\]sp" } } */
+
 #include <x86gprintrin.h>
 
+typedef unsigned int uword_t __attribute__ ((mode (__word__)));
+
 void __attribute__ ((target("general-regs-only"), interrupt))
-UINTR_handler (struct __uintr_frame *p)
+UINTR_handler (struct __uintr_frame *p, uword_t uirrv)
 {
 }
