ace51190c628d06c12af458dd0b46b99c4e30a4b
Arnaud Charlet <charlet@adacore.com>
2020-12-11 03:57:08 -0500

    [Ada] AI12-0407: Fixups on Big_Integers and Big_Reals
    gcc/ada/
    
    	* libgnat/a-nbnbin.ads (From_Universal_Image): New.
    	(Big_Integer): Update definition.
    	* libgnat/a-nbnbre.ads, libgnat/a-nbnbre.adb
    	(From_Universal_Image): New.
    	(From_String): Remove alternate body, replaced by
    	From_Universal_Image.
    	(Big_Real): Update definition.

--- gcc/ada/libgnat/a-nbnbin.ads
+++ gcc/ada/libgnat/a-nbnbin.ads
@@ -22,7 +22,7 @@ package Ada.Numerics.Big_Numbers.Big_Integers
   with Preelaborate
 is
    type Big_Integer is private
-     with Integer_Literal => From_String,
+     with Integer_Literal => From_Universal_Image,
           Put_Image       => Put_Image;
 
    function Is_Valid (Arg : Big_Integer) return Boolean
@@ -115,6 +115,9 @@ is
 
    function From_String (Arg : String) return Valid_Big_Integer
      with Global => null;
+
+   function From_Universal_Image (Arg : String) return Valid_Big_Integer
+     renames From_String;
 
    procedure Put_Image (S : in out Sink'Class; V : Big_Integer);
 
