0fa4787bf34b173ce6f198e99b6f6dd8a3f98014
liuhongt <hongtao.liu@intel.com>
2020-12-11 19:02:43 +0800

    Fold more shuffle builtins to VEC_PERM_EXPR.
    A follow-up to https://gcc.gnu.org/pipermail/gcc-patches/2019-May/521983.html
    
    gcc/
    	PR target/98167
    	PR target/43147
    	* config/i386/i386.c (ix86_gimple_fold_builtin): Fold
    	IX86_BUILTIN_SHUFPD512, IX86_BUILTIN_SHUFPS512,
    	IX86_BUILTIN_SHUFPD256， IX86_BUILTIN_SHUFPS，
    	IX86_BUILTIN_SHUFPS256.
    	(ix86_masked_all_ones): New function.
    
    gcc/testsuite/
    	* gcc.target/i386/avx512f-vshufpd-1.c: Adjust testcase.
    	* gcc.target/i386/avx512f-vshufps-1.c: Adjust testcase.
    	* gcc.target/i386/pr43147.c: New test.

--- gcc/testsuite/gcc.target/i386/avx512f-vshufpd-1.c
+++ gcc/testsuite/gcc.target/i386/avx512f-vshufpd-1.c
@@ -7,11 +7,12 @@
 #include <immintrin.h>
 
 __m512d x;
+__m512d y;
 
 void extern
 avx512f_test (void)
 {
-  x = _mm512_shuffle_pd (x, x, 56);
+  x = _mm512_shuffle_pd (x, y, 56);
   x = _mm512_mask_shuffle_pd (x, 2, x, x, 56);
   x = _mm512_maskz_shuffle_pd (2, x, x, 56);
 }
