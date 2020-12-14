ebc15a4e1eee66f68b106e93d4fbf4ed9c8ef07a
Christophe Lyon <christophe.lyon@linaro.org>
2020-12-14 10:40:45 +0000

    arm,testsuite: Fix vect-half-floats.c test
    This patch fixes typos in effective targets which otherwise lead to
    DejaGnu errors.
    
    It also replaces dg-additional-options with dg-options to avoid
    compiling with -ansi -pedantic-errors, resulting in
    error: ISO C does not support the '_Float16' type [-Wpedantic]
    
    2020-12-14  Christophe Lyon  <christophe.lyon@linaro.org>
    
    	gcc/testsuite/
    	* gcc.target/arm/vect-half-floats.c: Fix typos.

--- gcc/testsuite/gcc.target/arm/vect-half-floats.c
+++ gcc/testsuite/gcc.target/arm/vect-half-floats.c
@@ -1,8 +1,8 @@
 /* { dg-do compile }  */
-/* { dg-require-effective-target target_float16 } */ 
+/* { dg-options "-Ofast -ftree-vectorize -fdump-tree-vect-all -std=c11" } */
+/* { dg-require-effective-target float16 } */ 
 /* { dg-require-effective-target arm_fp16_ok } */
-/* { dg-add-options for_float16 } */
-/* { dg-additional-options "-Ofast -ftree-vectorize -fdump-tree-vect-all -std=c11" } */
+/* { dg-add-options float16 } */
 
 void foo (_Float16 n1[], _Float16 n2[], _Float16 r[], int n)
 {
