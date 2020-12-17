4a7a3110c70da8bad6978a36d9da3836538a0cc3
H.J. Lu <hjl.tools@gmail.com>
2020-12-17 11:00:02 -0800

    Update default_estimated_poly_value prototype in targhooks.h
    commit 64432b680eab0bddbe9a4ad4798457cf6a14ad60
    Author: Kyrylo Tkachov <kyrylo.tkachov@arm.com>
    Date:   Thu Dec 17 18:02:37 2020 +0000
    
        vect, aarch64: Extend SVE vs Advanced SIMD costing decisions in vect_better_loop_vinfo_p
    
    changed default_estimated_poly_value to
    
    HOST_WIDE_INT
    default_estimated_poly_value (poly_int64 x, poly_value_estimate_kind)
    {
      return x.coeffs[0];
    }
    
    Update default_estimated_poly_value prototype in targhooks.h to match it.
    
    	* targhooks.h (default_estimated_poly_value): Updated.

--- gcc/targhooks.h
+++ gcc/targhooks.h
@@ -221,7 +221,8 @@ extern int default_memory_move_cost (machine_mode, reg_class_t, bool);
 extern int default_register_move_cost (machine_mode, reg_class_t,
 				       reg_class_t);
 extern bool default_slow_unaligned_access (machine_mode, unsigned int);
-extern HOST_WIDE_INT default_estimated_poly_value (poly_int64);
+extern HOST_WIDE_INT default_estimated_poly_value (poly_int64,
+						   poly_value_estimate_kind);
 
 extern bool default_use_by_pieces_infrastructure_p (unsigned HOST_WIDE_INT,
 						    unsigned int,
