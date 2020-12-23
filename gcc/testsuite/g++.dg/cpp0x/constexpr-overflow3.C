0a541e746c71fe21eb72bae61a9ead541ed7e98d
Jason Merrill <jason@redhat.com>
2020-12-23 17:06:39 -0500

    c++: Improve testcase [PR98332]
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp0x/constexpr-overflow3.C: Use INT_MAX.

--- gcc/testsuite/g++.dg/cpp0x/constexpr-overflow3.C
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-overflow3.C
@@ -1,5 +1,7 @@
 // PR c++/98332
 // { dg-do compile { target c++11 } }
 
-struct S { int a = 2147483647 + 1; }; // { dg-warning "overflow" }
+#include <limits.h>
+
+struct S { int a = INT_MAX + 1; }; // { dg-warning "overflow" }
 struct { S b[2][1][1][1]; } c;
