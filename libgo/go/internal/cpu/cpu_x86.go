abca6705969b59edae86f11233f9d207cbdd1e06
Ian Lance Taylor <iant@golang.org>
2020-12-31 11:23:30 -0800

    internal/cpu, golang.org/x/sys/cpu: support other GOARCH values
    Add support (mostly dummy support) for GOARCH values supported by
    gofrontend but not gc.  Fix PPC handling.
    
    Fixes https://gcc.gnu.org/PR98493
    
    Reviewed-on: https://go-review.googlesource.com/c/gofrontend/+/280932

--- libgo/go/internal/cpu/cpu_x86.go
+++ libgo/go/internal/cpu/cpu_x86.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build 386 amd64
+// +build 386 amd64 amd64p32
 
 package cpu
 
@@ -56,7 +56,7 @@ func doinit() {
 		{Name: "ssse3", Feature: &X86.HasSSSE3},
 
 		// These capabilities should always be enabled on amd64:
-		{Name: "sse2", Feature: &X86.HasSSE2, Required: GOARCH == "amd64"},
+		{Name: "sse2", Feature: &X86.HasSSE2, Required: GOARCH == "amd64" || GOARCH == "amd64p32"},
 	}
 
 	maxID, _, _, _ := cpuid(0, 0)
