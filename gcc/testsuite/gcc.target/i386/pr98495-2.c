af60b0ec79e9c5d7116122b185e44927aca5aa07
H.J. Lu <hjl.tools@gmail.com>
2021-01-01 05:30:34 -0800

    x86: Cast to unsigned short first for _mm_extract_pi16
    _mm_extract_pi16 is intrinsic for pextrw, which should be zero-extended,
    not sign-extended.
    
    gcc/
    
    	PR target/98495
    	* config/i386/xmmintrin.h (_mm_extract_pi16): Cast to unsigned
    	short first.
    
    gcc/testsuite/
    
    	PR target/98495
    	* gcc.target/i386/pr98495-1.c: New test.
    	* gcc.target/i386/pr98495-2.c: New test.
    	* gcc.target/i386/pr98495-3.c: New test.
    	* gcc.target/i386/pr98495-4.c: New test.
    	* gcc.target/i386/pr98495-5.c: New test.

---
+++ gcc/testsuite/gcc.target/i386/pr98495-2.c
@@ -0,0 +1,11 @@
+/* { dg-do compile { target { ! ia32 } } } */
+/* { dg-options "-O2 -mmmx -msse2 -mtune=generic" } */
+/* { dg-final { scan-assembler-not "movswl" } } */
+/* { dg-final { scan-assembler-not "movzwl" } } */
+/* { dg-final { scan-assembler-not "cwtl" } } */
+/* { dg-final { scan-assembler-not "cltq" } } */
+/* { dg-final { scan-assembler "pextrw" } } */
+
+#include <xmmintrin.h>
+
+unsigned long long int foo16(__m64 x) { return _mm_extract_pi16(x, 3); }
