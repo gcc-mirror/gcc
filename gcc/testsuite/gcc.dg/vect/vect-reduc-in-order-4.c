8837f82e4bab1b5405cf034eab9b3e83afc563ad
Richard Biener <rguenther@suse.de>
2021-01-04 09:53:11 +0100

    tree-optimization/98291 - allow SLP more vectorization of reductions
    When the VF is one a SLP reduction is in-order and thus we can
    vectorize even when the reduction op is not associative.
    
    2021-01-04  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/98291
    	* tree-vect-loop.c (vectorizable_reduction): Bypass
    	associativity check for SLP reductions with VF 1.
    
    	* gcc.dg/vect/slp-reduc-11.c: New testcase.
    	* gcc.dg/vect/vect-reduc-in-order-4.c: Adjust.

--- gcc/testsuite/gcc.dg/vect/vect-reduc-in-order-4.c
+++ gcc/testsuite/gcc.dg/vect/vect-reduc-in-order-4.c
@@ -41,6 +41,4 @@ main ()
   return 0;
 }
 
-/* { dg-final { scan-tree-dump {in-order unchained SLP reductions not supported} "vect" } } */
-/* { dg-final { scan-tree-dump-not {vectorizing stmts using SLP} "vect" } } */
 /* { dg-final { scan-tree-dump-times "VECT_PERM_EXPR" 0 "vect" } } */
