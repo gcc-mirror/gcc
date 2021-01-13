cf7a335306153a5b01f3e1d081456ea4c32baefc
Richard Sandiford <richard.sandiford@arm.com>
2021-01-13 13:00:12 +0000

    aarch64: Add support for unpacked SVE MLA and MAD
    This patch extends the MLA/MAD patterns to support unpacked
    integer vectors.  The type suffix could be either the element
    size or the container size, but using the element size should
    be more efficient.
    
    gcc/
    	* config/aarch64/aarch64-sve.md (fma<mode>4): Extend from SVE_FULL_I
    	to SVE_I.
    	(@aarch64_pred_fma<mode>, cond_fma<mode>, *cond_fma<mode>_2)
    	(*cond_fma<mode>_4, *cond_fma<mode>_any): Likewise.
    
    gcc/testsuite/
    	* gcc.target/aarch64/sve/mla_2.c: New test.
    	* g++.target/aarch64/sve/cond_mla_1.C: Likewise.
    	* g++.target/aarch64/sve/cond_mla_2.C: Likewise.
    	* g++.target/aarch64/sve/cond_mla_3.C: Likewise.
    	* g++.target/aarch64/sve/cond_mla_4.C: Likewise.
    	* g++.target/aarch64/sve/cond_mla_5.C: Likewise.

---
+++ gcc/testsuite/g++.target/aarch64/sve/cond_mla_2.C
@@ -0,0 +1,33 @@
+/* { dg-do assemble { target aarch64_asm_sve_ok } } */
+/* { dg-options "-O -msve-vector-bits=2048 -save-temps" } */
+
+#include <stdint.h>
+
+#define TEST_OP(TYPE) \
+  TYPE \
+  test##_##TYPE##_reg (TYPE a, TYPE b, TYPE c, TYPE d) \
+  { \
+    return d == 0 ? a + b * c : b; \
+  }
+
+#define TEST_TYPE(TYPE, SIZE) \
+  typedef TYPE TYPE##SIZE __attribute__((vector_size(SIZE))); \
+  TEST_OP (TYPE##SIZE)
+
+TEST_TYPE (uint8_t, 32)
+
+TEST_TYPE (uint8_t, 64)
+TEST_TYPE (uint16_t, 64)
+
+TEST_TYPE (uint8_t, 128)
+TEST_TYPE (uint16_t, 128)
+TEST_TYPE (uint32_t, 128)
+
+/* { dg-final { scan-assembler {\tld1b\t(z[0-9]+)\.h, p[0-7]/z, \[x1\][^L]*\tmad\t\1\.b,} } } */
+/* { dg-final { scan-assembler {\tld1b\t(z[0-9]+)\.s, p[0-7]/z, \[x1\][^L]*\tmad\t\1\.b,} } } */
+/* { dg-final { scan-assembler {\tld1b\t(z[0-9]+)\.d, p[0-7]/z, \[x1\][^L]*\tmad\t\1\.b,} } } */
+/* { dg-final { scan-assembler {\tld1h\t(z[0-9]+)\.s, p[0-7]/z, \[x1\][^L]*\tmad\t\1\.h,} } } */
+/* { dg-final { scan-assembler {\tld1h\t(z[0-9]+)\.d, p[0-7]/z, \[x1\][^L]*\tmad\t\1\.h,} } } */
+/* { dg-final { scan-assembler {\tld1w\t(z[0-9]+)\.d, p[0-7]/z, \[x1\][^L]*\tmad\t\1\.s,} } } */
+
+/* { dg-final { scan-assembler-not {\tsel\t} } } */
