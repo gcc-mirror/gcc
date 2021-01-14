7624c58c6b7b2b313c92b25ebbd267cf0ed7f7c4
Martin Liska <mliska@suse.cz>
2021-01-14 17:08:32 +0100

    gcov: add one more pytest
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/gcov/gcov-17.C: New test.
    	* g++.dg/gcov/test-gcov-17.py: New test.

---
+++ gcc/testsuite/g++.dg/gcov/gcov-17.C
@@ -0,0 +1,40 @@
+/* { dg-options "--coverage -std=c++11" } */
+/* { dg-do run { target native } } */
+
+template <class T> class Foo
+{
+public:
+  Foo () : b (1000) {}
+
+  void inc () { b++; }
+
+private:
+  int b;
+};
+
+template class Foo<int>;
+template class Foo<char>;
+
+int
+main (void)
+{
+  int i, total;
+  Foo<int> counter;
+
+  counter.inc ();
+  counter.inc ();
+  total = 0;
+
+  for (i = 0; i < 10; i++)
+    total += i;
+
+  int v = total > 100 ? 1 : 2;
+
+  if (total != 45)
+    __builtin_printf ("Failure\n");
+  else
+    __builtin_printf ("Success\n");
+  return 0;
+}
+
+/* { dg-final { run-gcov-pytest gcov-17.C "test-gcov-17.py" } } */
