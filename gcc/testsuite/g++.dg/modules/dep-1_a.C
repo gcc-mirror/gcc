e1efa6af61ab54faf0d8d091328e0c6a1141050c
Nathan Sidwell <nathan@acm.org>
2021-01-15 11:02:38 -0800

    preprocessor: Make quoting : [PR 95253]
    I missed some testsuite fall out with my patch to fix mkdeps file
    mangling.
    
    	PR preprocessor/95253
    	gcc/testsuite/
    	* g++.dg/modules/dep-1_a.C: Adjust expected output.
    	* g++.dg/modules/dep-1_b.C: Likewise.
    	* g++.dg/modules/dep-2.C: Likewise.

--- gcc/testsuite/g++.dg/modules/dep-1_a.C
+++ gcc/testsuite/g++.dg/modules/dep-1_a.C
@@ -4,6 +4,6 @@ export module m:part;
 // { dg-module-cmi m:part }
 
 // All The Backslashes!
-// { dg-final { scan-file dep-1_a.d {\nm\\:part\.c\+\+m: gcm.cache/m-part\.gcm} } }
+// { dg-final { scan-file dep-1_a.d {\nm:part\.c\+\+m: gcm.cache/m-part\.gcm} } }
 // { dg-final { scan-file dep-1_a.d {\ngcm.cache/m-part\.gcm:| dep-1_a\.o} } }
-// { dg-final { scan-file dep-1_a.d {\n\.PHONY: m\\:part\.c\+\+m} } }
+// { dg-final { scan-file dep-1_a.d {\n\.PHONY: m:part\.c\+\+m} } }
