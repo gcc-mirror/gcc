ce5720447c69286599b96bae53ae854b1bbe41fa
Martin Sebor <msebor@redhat.com>
2021-02-04 14:50:23 -0700

    PR c/97882 - Segmentation Fault on improper redeclaration of function
    gcc/c/ChangeLog:
    
    	PR c/97882
    	* c-decl.c (locate_old_decl): Add type to diagnostic output.
    	(diagnose_mismatched_decls): Same.
    	(start_function): Introduce temporaries for better readability.
    	* c-typeck.c (comptypes_internal): Only consider complete enum
    	types in comparisons with integers.
    
    gcc/testsuite/ChangeLog:
    
    	PR c/97882
    	* gcc.dg/decl-8.c: Adjust text of expected diagnostic.
    	* gcc.dg/label-decl-4.c: Same.
    	* gcc.dg/mismatch-decl-1.c: Same.
    	* gcc.dg/old-style-then-proto-1.c: Same.
    	* gcc.dg/parm-mismatch-1.c: Same.
    	* gcc.dg/pr35445.c: Same.
    	* gcc.dg/redecl-11.c: Same.
    	* gcc.dg/redecl-12.c: Same.
    	* gcc.dg/redecl-13.c: Same.
    	* gcc.dg/redecl-15.c: Same.
    	* gcc.dg/tls/thr-init-1.c: Same.
    	* objc.dg/id-1.m: Same.
    	* objc.dg/tls/diag-3.m: Same.
    	* gcc.dg/pr97882.c: New test.
    	* gcc.dg/qual-return-7.c: New test.
    	* gcc.dg/qual-return-8.c: New test.

--- gcc/testsuite/gcc.dg/label-decl-4.c
+++ gcc/testsuite/gcc.dg/label-decl-4.c
@@ -7,8 +7,8 @@ void
 f (void)
 {
   __label__ a, b, a; /* { dg-error "duplicate label declaration 'a'" } */
-  /* { dg-message "note: previous declaration of 'a' was here" "previous" { target *-*-* } .-1 } */
-  __label__ c; /* { dg-message "note: previous declaration of 'c' was here" } */
+  /* { dg-message "note: previous declaration of 'a'" "previous" { target *-*-* } .-1 } */
+  __label__ c; /* { dg-message "note: previous declaration of 'c'" "note" } */
   __label__ c; /* { dg-error "duplicate label declaration 'c'" } */
   return;
 }
