86d8ecf28e6c20b3e6866a5b1551a339edc8d14d
Jason Merrill <jason@redhat.com>
2021-02-08 17:16:14 -0500

    c++: consteval and explicit instantiation [PR96905]
    Normally, an explicit instantiation means we want to write out the
    instantiation.  But not for a consteval function.
    
    gcc/cp/ChangeLog:
    
    	PR c++/96905
    	* pt.c (mark_decl_instantiated): Exit early if consteval.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/96905
    	* g++.dg/cpp2a/consteval-expinst1.C: New test.

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -23927,6 +23927,11 @@ mark_decl_instantiated (tree result, int extern_p)
   if (TREE_ASM_WRITTEN (result))
     return;
 
+  /* consteval functions are never emitted.  */
+  if (TREE_CODE (result) == FUNCTION_DECL
+      && DECL_IMMEDIATE_FUNCTION_P (result))
+    return;
+
   /* For anonymous namespace we don't need to do anything.  */
   if (decl_anon_ns_mem_p (result))
     {
