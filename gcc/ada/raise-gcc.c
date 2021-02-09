385484ef92f34ff7b420312927aaa174e5a8a696
Eric Botcazou <ebotcazou@adacore.com>
2021-02-09 12:52:08 +0100

    [Ada] Small cleanup in exception handling C code
    gcc/ada/
    
    	* raise-gcc.c: Include <cstdarg> instead of <stdarg.h> in C++.
    	Include <stdbool.h> and unconditionally <stdlib.h> in C.

--- gcc/ada/raise-gcc.c
+++ gcc/ada/raise-gcc.c
@@ -48,14 +48,13 @@
 # endif
 #endif
 
-#include <stdarg.h>
-
 #ifdef __cplusplus
+# include <cstdarg>
 # include <cstdlib>
 #else
-typedef char bool;
-# define true 1
-# define false 0
+# include <stdarg.h>
+# include <stdbool.h>
+# include <stdlib.h>
 #endif
 
 #include "raise.h"
@@ -123,7 +122,6 @@ extern void __gnat_raise_abort (void) __attribute__ ((noreturn));
 #define abort() __gnat_raise_abort()
 
 #elif defined(STANDALONE)
-#include <stdlib.h>
 #define inhibit_libc
 #endif
 
