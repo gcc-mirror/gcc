bd0e37f68a3aed944df4eb739a0734bb87153749
Jakub Jelinek <jakub@redhat.com>
2021-02-10 10:34:58 +0100

    openmp: Temporarily disable into_ssa when gimplifying OpenMP reduction clauses [PR99007]
    gimplify_scan_omp_clauses was already calling gimplify_expr with false as
    last argument to make sure it is not an SSA_NAME, but as the testcases show,
    that is not enough, SSA_NAME temporaries created during that gimplification
    can be reused too and we can't allow SSA_NAMEs to be used across OpenMP
    region boundaries, as we can only firstprivatize decls.
    
    Fixed by temporarily disabling into_ssa.
    
    2021-02-10  Jakub Jelinek  <jakub@redhat.com>
    
    	PR middle-end/99007
    	* gimplify.c (gimplify_scan_omp_clauses): For MEM_REF on reductions,
    	temporarily disable gimplify_ctxp->into_ssa around gimplify_expr
    	calls.
    
    	* g++.dg/gomp/pr99007.C: New test.
    	* gcc.dg/gomp/pr99007-1.c: New test.
    	* gcc.dg/gomp/pr99007-2.c: New test.
    	* gcc.dg/gomp/pr99007-3.c: New test.

---
+++ gcc/testsuite/gcc.dg/gomp/pr99007-2.c
@@ -0,0 +1,15 @@
+/* PR middle-end/99007 */
+
+int
+bar (int n)
+{
+  int s[n];
+  int i, j;
+  for (i = 0; i < n; i++)
+    s[i] = 0;
+  #pragma omp teams distribute parallel for reduction(+:s) private (j)
+  for (i = 0; i < 8; i++)
+    for (j = 0; j < n; j++)
+      s[j] += i;
+  return s[0] + s[n - 1];
+}
