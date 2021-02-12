d1a821b93c45bfe7606b5dee8d160c7172b37e3e
Jonathan Wakely <jwakely@redhat.com>
2021-02-12 15:08:29 +0000

    libstdc++: Include scope ID in net::internet::address_v6::to_string()
    libstdc++-v3/ChangeLog:
    
    	* include/experimental/internet (address_v6::to_string): Include
    	scope ID in string.
    	* testsuite/experimental/net/internet/address/v6/members.cc:
    	Test to_string() results.

--- libstdc++-v3/testsuite/experimental/net/internet/address/v6/members.cc
+++ libstdc++-v3/testsuite/experimental/net/internet/address/v6/members.cc
@@ -86,16 +86,30 @@ static_assert(test02(), "");
 void
 test03()
 {
+  // Assume these addresses use the preferred forms:
   VERIFY( address_v6::any().to_string() == "::" );
   VERIFY( address_v6::loopback().to_string() == "::1" );
+
+  // Choose values with no leading zeros, so output is portable:
+  address_v6 a{address_v6::bytes_type{21,22,23,24,25,26,27,28,29}, 42};
+  const std::string s = a.to_string();
+  if (s.find("::") != s.npos)
+    VERIFY( s == "1516:1718:191a:1b1c:1d00::%42" );
+  else
+  {
+    // Contiguous zeros were not shortened to "::"
+    VERIFY( s.substr(0, 25) == "1516:1718:191a:1b1c:1d00:" );
+    VERIFY( s.substr(s.size() - 3) == "%42" );
+  }
 }
 
 void
 test04()
 {
+  address_v6 a{address_v6::bytes_type{1,2,3,4,5,6,7,8,9}, 42};
   std::ostringstream ss;
-  ss << address_v6::any() << ' ' << address_v6::loopback();
-  VERIFY( ss.str() == ":: ::1" );
+  ss << address_v6::any() << ' ' << address_v6::loopback() << ' ' << a;
+  VERIFY( ss.str() == (":: ::1 " + a.to_string()) );
 }
 
 int
