21a7edf3a0cea656f2de834ea8320870496d5a2f
Jakub Jelinek <jakub@redhat.com>
2021-02-23 09:30:18 +0100

    libstdc++: Fix up constexpr std::char_traits<char>::compare [PR99181]
    Because of LWG 467, std::char_traits<char>::lt compares the values
    cast to unsigned char rather than char, so even when char is signed
    we get unsigned comparision.  std::char_traits<char>::compare uses
    __builtin_memcmp and that works the same, but during constexpr evaluation
    we were calling __gnu_cxx::char_traits<char_type>::compare.  As
    char_traits::lt is not virtual, __gnu_cxx::char_traits<char_type>::compare
    used __gnu_cxx::char_traits<char_type>::lt rather than
    std::char_traits<char>::lt and thus compared chars as signed if char is
    signed.
    This change fixes it by inlining __gnu_cxx::char_traits<char_type>::compare
    into std::char_traits<char>::compare by hand, so that it calls the right
    lt method.
    
    2021-02-23  Jakub Jelinek  <jakub@redhat.com>
    
    	PR libstdc++/99181
    	* include/bits/char_traits.h (char_traits<char>::compare): For
    	constexpr evaluation don't call
    	__gnu_cxx::char_traits<char_type>::compare but do the comparison loop
    	directly.
    
    	* testsuite/21_strings/char_traits/requirements/char/99181.cc: New
    	test.
    
    (cherry picked from commit 311c57f6d8f285d69e44bf94152c753900cb1a0a)

--- libstdc++-v3/include/bits/char_traits.h
+++ libstdc++-v3/include/bits/char_traits.h
@@ -305,7 +305,14 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	if (__builtin_constant_p(__n)
 	    && __constant_char_array_p(__s1, __n)
 	    && __constant_char_array_p(__s2, __n))
-	  return __gnu_cxx::char_traits<char_type>::compare(__s1, __s2, __n);
+	  {
+	    for (size_t __i = 0; __i < __n; ++__i)
+	      if (lt(__s1[__i], __s2[__i]))
+		return -1;
+	      else if (lt(__s2[__i], __s1[__i]))
+		return 1;
+	    return 0;
+	  }
 #endif
 	if (__n == 0)
 	  return 0;
