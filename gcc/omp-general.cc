81d73ad786759aadd926273e8ddd4adc7e6015bc
Kwok Cheung Yeung <kcy@codesourcery.com>
2021-03-01 14:15:30 -0800

    openmp: Scale type precision of collapsed iterator variable
    This sets the type precision of the collapsed iterator variable to the
    sum of the precision of the collapsed loop variables, up to a maximum of
    sizeof(long long) (i.e. 64-bits).
    
    gcc/ChangeLog
    	* omp-expand.cc (expand_oacc_for): Convert .tile variable to
    	diff_type before multiplying.
    	* omp-general.cc (omp_extract_for_data): Use accumulated precision
    	of all collapsed for-loops as precision of iteration variable, up
    	to the precision of a long long.
    
    libgomp/ChangeLog
    	* testsuite/libgomp.c-c++-common/collapse-4.c: New.
    	* testsuite/libgomp.fortran/collapse5.f90: New.

--- gcc/omp-general.cc
+++ gcc/omp-general.cc
@@ -406,6 +406,7 @@ omp_extract_for_data (gomp_for *for_stmt, struct omp_for_data *fd,
 	  fd->non_rect = true;
 	}
     }
+  int accum_iter_precision = 0;
   for (i = 0; i < cnt; i++)
     {
       if (i == 0
@@ -489,19 +490,33 @@ omp_extract_for_data (gomp_for *for_stmt, struct omp_for_data *fd,
 	{
 	  if (fd->collapse == 1 && !fd->tiling)
 	    iter_type = TREE_TYPE (loop->v);
-	  else if (i == 0
-		   || TYPE_PRECISION (iter_type)
-		      < TYPE_PRECISION (TREE_TYPE (loop->v)))
+	  else
 	    {
-	      if (TREE_CODE (iter_type) == BITINT_TYPE
-		  || TREE_CODE (TREE_TYPE (loop->v)) == BITINT_TYPE)
-		iter_type
-		  = build_bitint_type (TYPE_PRECISION (TREE_TYPE (loop->v)),
-				       1);
-	      else
-		iter_type
-		  = build_nonstandard_integer_type
-			(TYPE_PRECISION (TREE_TYPE (loop->v)), 1);
+	      int loop_precision = TYPE_PRECISION (TREE_TYPE (loop->v));
+	      int iter_type_precision = 0;
+	      const int max_accum_precision
+		= TYPE_PRECISION (long_long_unsigned_type_node);
+
+	      accum_iter_precision += loop_precision;
+
+	      if (i == 0
+		  || (loop_precision >= max_accum_precision
+		      && loop_precision >= TYPE_PRECISION (iter_type)))
+		iter_type_precision = loop_precision;
+	      else if (TYPE_PRECISION (iter_type) < max_accum_precision)
+		iter_type_precision
+		  = MIN (1 << ceil_log2 (accum_iter_precision),
+			 max_accum_precision);
+
+	      if (iter_type_precision)
+		{
+		  if (TREE_CODE (iter_type) == BITINT_TYPE
+		      || TREE_CODE (TREE_TYPE (loop->v)) == BITINT_TYPE)
+		    iter_type = build_bitint_type (iter_type_precision, 1);
+		  else
+		    iter_type
+		      = build_nonstandard_integer_type (iter_type_precision, 1);
+		}
 	    }
 	}
       else if (iter_type != long_long_unsigned_type_node)
