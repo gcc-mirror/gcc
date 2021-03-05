b5bc39317bf4da1d51cb6eccf8afcacc90514602
Bill Schmidt <wschmidt@linux.ibm.com>
2021-03-04 19:54:00 -0600

    rs6000: Darwin builtin support
    2021-03-04  Bill Schmidt  <wschmidt@linux.ibm.com>
    
    gcc/
    	* config/rs6000/darwin.h (SUBTARGET_INIT_BUILTINS): Use the new
    	decl when new_builtins_are_live.
    	* config/rs6000/rs6000-builtin-new.def (__builtin_cfstring): New
    	built-in.

--- gcc/config/rs6000/darwin.h
+++ gcc/config/rs6000/darwin.h
@@ -504,8 +504,12 @@
 #define SUBTARGET_INIT_BUILTINS						\
 do {									\
   darwin_patch_builtins ();						\
-  rs6000_builtin_decls[(unsigned) (RS6000_BUILTIN_CFSTRING)]		\
-    = darwin_init_cfstring_builtins ((unsigned) (RS6000_BUILTIN_CFSTRING)); \
+  if (new_builtins_are_live)						\
+    rs6000_builtin_decls_x[(unsigned) (RS6000_BIF_CFSTRING)]		\
+      = darwin_init_cfstring_builtins ((unsigned) (RS6000_BIF_CFSTRING)); \
+  else									\
+    rs6000_builtin_decls[(unsigned) (RS6000_BUILTIN_CFSTRING)]		\
+      = darwin_init_cfstring_builtins ((unsigned) (RS6000_BUILTIN_CFSTRING)); \
 } while(0)
 
 /* So far, there is no rs6000_fold_builtin, if one is introduced, then
