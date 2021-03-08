81fee438512460f1be50d91ee5de452f8fe5cc18
Martin Liska <mliska@suse.cz>
2021-03-08 15:52:03 +0100

    libsanitizer: cherry-pick ad294e572bc5c16f9dc420cc994322de6ca3fbfb
    libsanitizer/ChangeLog:
    
    	PR sanitizer/98920
    	* asan/asan_interceptors.cpp (COMMON_INTERCEPT_FUNCTION_VER):
    	Cherry pick.
    	(COMMON_INTERCEPT_FUNCTION_VER_UNVERSIONED_FALLBACK): Likewise.
    	* asan/asan_interceptors.h (ASAN_INTERCEPT_FUNC_VER_UNVERSIONED_FALLBACK): Likewise.
    	* sanitizer_common/sanitizer_common_interceptors.inc
    	(COMMON_INTERCEPT_FUNCTION_GLIBC_VER_MIN): Likewise.
    	(INIT_REGEX): Likewise.
    	* tsan/tsan_interceptors_posix.cpp (COMMON_INTERCEPT_FUNCTION_VER_UNVERSIONED_FALLBACK):
    	Likewise.
    
    gcc/testsuite/ChangeLog:
    
    	PR sanitizer/98920
    	* c-c++-common/asan/pr98920.c: New test.

---
+++ gcc/testsuite/c-c++-common/asan/pr98920.c
@@ -0,0 +1,24 @@
+/* PR sanitizer/98920 */
+/* { dg-do run } */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <regex.h>
+
+int main(void)
+{
+    regex_t r;
+    const char s[] = "ban\0ana";
+    regmatch_t pmatch[10];
+    pmatch[0].rm_so = 0;
+    pmatch[0].rm_eo = sizeof(s);
+    if (regcomp(&r, "ana", 0))
+        return 2;
+    if (regexec(&r, s, sizeof(pmatch)/sizeof(pmatch[0]), pmatch, REG_STARTEND)) {
+        fprintf(stderr, "failed to match\n");
+        regfree(&r);
+        return 3;
+    }
+    regfree(&r);
+    return 0;
+}
