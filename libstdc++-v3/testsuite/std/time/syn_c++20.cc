e7afb82c358855510aa90ae9869663b614a732cb
Jonathan Wakely <jwakely@redhat.com>
2021-03-10 11:14:17 +0000

    libstdc++: Uncomment more parts of <chrono> synopsis test
    libstdc++-v3/ChangeLog:
    
    	* testsuite/std/time/syn_c++20.cc: Enable synopsis checks for
    	C++20 calendar types.

--- libstdc++-v3/testsuite/std/time/syn_c++20.cc
+++ libstdc++-v3/testsuite/std/time/syn_c++20.cc
@@ -75,6 +75,7 @@ namespace __gnu_test
   using std::chrono::clock_cast;
 
   using std::chrono::last_spec;
+#endif
 
   using std::chrono::day;
   using std::chrono::month;
@@ -100,6 +101,8 @@ namespace __gnu_test
   using std::chrono::make12;
   using std::chrono::make24;
 
+  // FIXME
+#if 0
   using std::chrono::tzdb;
   using std::chrono::tzdb_list;
   using std::chrono::get_tzdb;
@@ -130,6 +133,7 @@ namespace __gnu_test
   using std::chrono::local_time_format;
 
   using std::chrono::parse;
+#endif
 
   using std::chrono::last;
   using std::chrono::Sunday;
@@ -155,7 +159,6 @@ namespace __gnu_test
 
   using std::chrono_literals::operator""d;
   using std::chrono_literals::operator""y;
-#endif
 
   template<typename>
     constexpr bool is_duration = false;
