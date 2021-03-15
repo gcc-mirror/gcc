dce586ff83e5bf86956cb24e646c7b9a21283ce4
Patrick Palka <ppalka@redhat.com>
2021-03-15 10:31:45 -0400

    libstdc++: Implement missing operator overloads in max_size_type.h
    This implements operator++, operator-- and operator<=> for the
    integer-class types defined in max_size_type.h, which I overlooked
    when originally implementing the class.
    
    libstdc++-v3/ChangeLog:
    
    	* include/bits/max_size_type.h (__max_size_type::operator _Tp):
    	Fix formatting.
    	(__max_size_type::operator++): Define.
    	(__max_size_type::operator--): Likewise.
    	(__max_size_type::operator<=>): Conditionally define (in place
    	of the other comparison operators).
    	(__max_diff_type::operator _Tp): Fix formatting.
    	(__max_diff_type::operator++): Define.
    	(__max_diff_type::operator--): Likewise.
    	(__max_diff_type::operator<=>): Conditionally define (in place
    	of the other comparison operators).
    	* testsuite/std/ranges/iota/max_size_type.cc (test01): Test
    	these operator overloads.

--- libstdc++-v3/include/bits/max_size_type.h
+++ libstdc++-v3/include/bits/max_size_type.h
@@ -71,7 +71,8 @@ namespace ranges
       __max_size_type(const __max_diff_type& __d) noexcept;
 
       template<typename _Tp> requires integral<_Tp> || __is_int128<_Tp>
-	constexpr explicit operator _Tp() const noexcept
+	constexpr explicit
+	operator _Tp() const noexcept
 	{ return _M_val; }
 
       constexpr explicit
@@ -89,6 +90,30 @@ namespace ranges
       constexpr __max_size_type
       operator-() const noexcept
       { return operator~() + 1; }
+
+      constexpr __max_size_type&
+      operator++() noexcept
+      { return *this += 1; }
+
+      constexpr __max_size_type
+      operator++(int) noexcept
+      {
+	auto __tmp = *this;
+	++*this;
+	return __tmp;
+      }
+
+      constexpr __max_size_type&
+      operator--() noexcept
+      { return *this -= 1; }
+
+      constexpr __max_size_type
+      operator--(int) noexcept
+      {
+	auto __tmp = *this;
+	--*this;
+	return __tmp;
+      }
 
       constexpr __max_size_type&
       operator+=(const __max_size_type& __r) noexcept
@@ -355,6 +380,16 @@ namespace ranges
       operator==(const __max_size_type& __l, const __max_size_type& __r) noexcept
       { return __l._M_val == __r._M_val && __l._M_msb == __r._M_msb; }
 
+#if __cpp_lib_three_way_comparison
+      friend constexpr strong_ordering
+      operator<=>(const __max_size_type& __l, const __max_size_type& __r) noexcept
+      {
+	if (__l._M_msb ^ __r._M_msb)
+	  return __l._M_msb ? strong_ordering::greater : strong_ordering::less;
+	else
+	  return __l._M_val <=> __r._M_val;
+      }
+#else
       friend constexpr bool
       operator!=(const __max_size_type& __l, const __max_size_type& __r) noexcept
       { return !(__l == __r); }
@@ -379,6 +414,7 @@ namespace ranges
       friend constexpr bool
       operator>=(const __max_size_type& __l, const __max_size_type& __r) noexcept
       { return __r <= __l; }
+#endif
 
 #if __SIZEOF_INT128__
       using __rep = unsigned __int128;
@@ -417,7 +453,8 @@ namespace ranges
       { }
 
       template<typename _Tp> requires integral<_Tp> || __is_int128<_Tp>
-	constexpr explicit operator _Tp() const noexcept
+	constexpr explicit
+	operator _Tp() const noexcept
 	{ return static_cast<_Tp>(_M_rep); }
 
       constexpr explicit
@@ -435,6 +472,30 @@ namespace ranges
       constexpr __max_diff_type
       operator~() const noexcept
       { return __max_diff_type(~_M_rep); }
+
+      constexpr __max_diff_type&
+      operator++() noexcept
+      { return *this += 1; }
+
+      constexpr __max_diff_type
+      operator++(int) noexcept
+      {
+	auto __tmp = *this;
+	++*this;
+	return __tmp;
+      }
+
+      constexpr __max_diff_type&
+      operator--() noexcept
+      { return *this -= 1; }
+
+      constexpr __max_diff_type
+      operator--(int) noexcept
+      {
+	auto __tmp = *this;
+	--*this;
+	return __tmp;
+      }
 
       constexpr __max_diff_type&
       operator+=(const __max_diff_type& __r) noexcept
@@ -647,6 +708,18 @@ namespace ranges
       operator==(const __max_diff_type& __l, const __max_diff_type& __r) noexcept
       { return __l._M_rep == __r._M_rep; }
 
+#if __cpp_lib_three_way_comparison
+      constexpr strong_ordering
+      operator<=>(const __max_diff_type& __r) const noexcept
+      {
+	const auto __lsign = _M_rep._M_msb;
+	const auto __rsign = __r._M_rep._M_msb;
+	if (__lsign ^ __rsign)
+	  return __lsign ? strong_ordering::less : strong_ordering::greater;
+	else
+	  return _M_rep <=> __r._M_rep;
+      }
+#else
       friend constexpr bool
       operator!=(const __max_diff_type& __l, const __max_diff_type& __r) noexcept
       { return !(__l == __r); }
@@ -673,6 +746,7 @@ namespace ranges
       friend constexpr bool
       operator>=(const __max_diff_type& __l, const __max_diff_type& __r) noexcept
       { return !(__l < __r); }
+#endif
 
     private:
       __max_size_type _M_rep = 0;
