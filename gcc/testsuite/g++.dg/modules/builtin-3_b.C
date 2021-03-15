770d3487ef18a71f65626c182625889eee29f580
Nathan Sidwell <nathan@acm.org>
2021-03-15 15:25:32 -0400

    c++: Fix 2 testcases [PR 99601]
    I'd failed to correctly restrict some checks to lp64 x86 targets.
    
    	PR c++/99601
    	gcc/testsuite/
    	* g++.dg/modules/builtin-3_a.C: Fix lp64 x86 detection.
    	* g++.dg/modules/builtin-3_b.C: Fix lp64 x86 detection.

--- gcc/testsuite/g++.dg/modules/builtin-3_b.C
+++ gcc/testsuite/g++.dg/modules/builtin-3_b.C
@@ -8,6 +8,6 @@ int main ()
 }
 
 // { dg-final { scan-lang-dump {Read:-[0-9]*'s named merge key \(matched\) function_decl:'::__builtin_strlen'} module } }
-// { dg-final { scan-lang-dump {Read:-[0-9]*'s named merge key \(matched\) type_decl:'::__builtin_va_list'} module { target { x86_64-*-linux* } } } }
+// { dg-final { scan-lang-dump {Read:-[0-9]*'s named merge key \(matched\) type_decl:'::__builtin_va_list'} module { target { { x86_64-*-linux* i?86-*-linux* } && lp64 } } }
 // { dg-final { scan-lang-dump {Read:-[0-9]*'s named merge key \(new\) type_decl:'::va_list'} module } }
 // { dg-final { scan-lang-dump {Read:-[0-9]*'s named merge key \(new\) type_decl:'::__gnuc_va_list'} module } }
