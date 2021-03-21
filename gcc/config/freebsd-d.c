74ed3fc27966b07d701e1dead1cc37b53af227b4
Iain Buclaw <ibuclaw@gdcproject.org>
2021-03-21 17:51:39 +0100

    [freebsd] d: Fix build failures on sparc64-*-freebsd*
    All target platforms that could run on SPARC should include this header
    in order to avoid errors from memmodel being used in sparc-protos.h.
    
    gcc/ChangeLog:
    
    	* config/freebsd-d.c: Include memmodel.h.

--- gcc/config/freebsd-d.c
+++ gcc/config/freebsd-d.c
@@ -18,6 +18,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "config.h"
 #include "system.h"
 #include "coretypes.h"
+#include "memmodel.h"
 #include "tm.h"
 #include "tm_p.h"
 #include "d/d-target.h"
