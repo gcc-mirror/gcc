129afa080badc7884c08668ed67a1e4da61bb7da
Tobias Burnus <tobias@codesourcery.com>
2021-03-22 09:49:48 +0100

    Fortran: Fix 'name' bound size [PR99688]
    gcc/fortran/ChangeLog:
    
    	PR fortran/99688
    	* match.c (select_type_set_tmp, gfc_match_select_type,
    	gfc_match_select_rank): Fix 'name' buffersize to avoid out of bounds.
    	* resolve.c (resolve_select_type): Likewise.
    
    (cherry picked from commit 0e792ee11aa6ebb6f61e9ed33eb06e260f0ec703)

--- gcc/fortran/match.c
+++ gcc/fortran/match.c
@@ -6311,7 +6311,7 @@ select_intrinsic_set_tmp (gfc_typespec *ts)
 static void
 select_type_set_tmp (gfc_typespec *ts)
 {
-  char name[GFC_MAX_SYMBOL_LEN];
+  char name[GFC_MAX_SYMBOL_LEN + 12 + 1];
   gfc_symtree *tmp = NULL;
   gfc_symbol *selector = select_type_stack->selector;
   gfc_symbol *sym;
@@ -6390,7 +6390,7 @@ gfc_match_select_type (void)
 {
   gfc_expr *expr1, *expr2 = NULL;
   match m;
-  char name[GFC_MAX_SYMBOL_LEN];
+  char name[GFC_MAX_SYMBOL_LEN + 1];
   bool class_array;
   gfc_symbol *sym;
   gfc_namespace *ns = gfc_current_ns;
@@ -6615,7 +6615,7 @@ gfc_match_select_rank (void)
 {
   gfc_expr *expr1, *expr2 = NULL;
   match m;
-  char name[GFC_MAX_SYMBOL_LEN];
+  char name[GFC_MAX_SYMBOL_LEN + 1];
   gfc_symbol *sym, *sym2;
   gfc_namespace *ns = gfc_current_ns;
   gfc_array_spec *as = NULL;
