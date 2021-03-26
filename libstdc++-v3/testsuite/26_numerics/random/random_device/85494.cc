5f070ba29803c99a5fe94ed7632d7b8c55593df3
Jonathan Wakely <jwakely@redhat.com>
2021-03-26 18:39:49 +0000

    libstdc++: Add PRNG fallback to std::random_device
    This makes std::random_device usable on VxWorks when running on older
    x86 hardware. Since the r10-728 fix for PR libstdc++/85494 the library
    will use the new code unconditionally on x86, but the cpuid checks for
    RDSEED and RDRAND can fail at runtime, depending on the hardware where
    the code is executing. If the OS does not provide /dev/urandom then this
    means the std::random_device constructor always fails. In previous
    releases if /dev/urandom is unavailable then std::mt19937 was used
    unconditionally.
    
    This patch adds a fallback for the case where the runtime cpuid checks
    for x86 hardware instructions fail, and no /dev/urandom is available.
    When this happens a std::linear_congruential_engine object will be used,
    with a seed based on hashing the engine's address and the current time.
    Distinct std::random_device objects will use different seeds, unless an
    object is created and destroyed and a new object created at the same
    memory location within the clock tick. This is not great, but is better
    than always throwing from the constructor, and better than always using
    std::mt19937 with the same seed (as GCC 9 and earlier do).
    
    libstdc++-v3/ChangeLog:
    
    	* src/c++11/random.cc (USE_LCG): Define when a pseudo-random
    	fallback is needed.
    	[USE_LCG] (bad_seed, construct_lcg_at, destroy_lcg_at, __lcg):
    	New helper functions and callback.
    	(random_device::_M_init): Add 'prng' and 'all' enumerators.
    	Replace switch with fallthrough with a series of 'if' statements.
    	[USE_LCG]: Construct an lcg_type engine and use __lcg when cpuid
    	checks fail.
    	(random_device::_M_init_pretr1) [USE_MT19937]: Accept "prng"
    	token.
    	(random_device::_M_getval): Check for callback unconditionally
    	and always pass _M_file pointer.
    	* testsuite/26_numerics/random/random_device/85494.cc: Remove
    	effective-target check. Use new random_device_available helper.
    	* testsuite/26_numerics/random/random_device/94087.cc: Likewise.
    	* testsuite/26_numerics/random/random_device/cons/default-cow.cc:
    	Remove effective-target check.
    	* testsuite/26_numerics/random/random_device/cons/default.cc:
    	Likewise.
    	* testsuite/26_numerics/random/random_device/cons/token.cc: Use
    	new random_device_available helper. Test "prng" token.
    	* testsuite/util/testsuite_random.h (random_device_available):
    	New helper function.

--- libstdc++-v3/testsuite/26_numerics/random/random_device/85494.cc
+++ libstdc++-v3/testsuite/26_numerics/random/random_device/85494.cc
@@ -16,14 +16,21 @@
 // <http://www.gnu.org/licenses/>.
 
 // { dg-do run { target c++11 } }
-// { dg-require-effective-target random_device }
 
 #include <random>
 #include <testsuite_hooks.h>
+#include <testsuite_random.h>
 
 void
 test01()
 {
+  if (__gnu_test::random_device_available("mt19937"))
+  {
+    // std::random_device uses a Mersenne Twister with default seed,
+    // and the test below will fail.  No point trying to test it.
+    return;
+  }
+
   unsigned v1[3], v2[3];
   std::random_device d1, d2;
   for (auto& v : v1)
