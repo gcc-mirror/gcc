099fcbd107ad6139de2c387c62327d0d189a5261
Philip Herron <philip.herron@embecosm.com>
2021-03-29 12:04:21 +0100

    Add missing testcase for generics

---
+++ gcc/testsuite/rust.test/compile/generics10.rs
@@ -0,0 +1,18 @@
+struct Foo<T>(T);
+
+struct Bar<T> {
+    a: Foo<T>,
+    b: bool,
+}
+
+fn test<T>(a: Bar<T>) -> Foo<T> {
+    a.a
+}
+
+fn main() {
+    let a: Bar<i32> = Bar::<i32> {
+        a: Foo::<i32>(123),
+        b: true,
+    };
+    let b: Foo<i32> = test(a);
+}
