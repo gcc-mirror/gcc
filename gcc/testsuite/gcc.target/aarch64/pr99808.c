41d57b2a97c44ae7b0a5b01ae703a8f0d0495238
Kyrylo Tkachov <kyrylo.tkachov@arm.com>
2021-03-30 14:07:50 +0100

    aarch64: Fix gcc.target/aarch64/pr99808.c for ILP32
    Fix test for -mabi=ilp32
    
    gcc/testsuite/ChangeLog:
    
    	PR target/99808
    	* gcc.target/aarch64/pr99808.c: Use ULL constant suffix.

--- gcc/testsuite/gcc.target/aarch64/pr99808.c
+++ gcc/testsuite/gcc.target/aarch64/pr99808.c
@@ -8,7 +8,7 @@
 float32x4_t
 foo (void)
 {
-  float64x2_t arg2 = vcombine_f64 ((float64x1_t) 0UL, (float64x1_t) 1UL);
-  return vcvt_high_f32_f64 ((float32x2_t) 1UL, arg2);
+  float64x2_t arg2 = vcombine_f64 ((float64x1_t) 0ULL, (float64x1_t) 1ULL);
+  return vcvt_high_f32_f64 ((float32x2_t) 1ULL, arg2);
 }
 
