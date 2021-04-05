07f56824fd4da14a48030e698c8eb58de951c741
Jason Merrill <jason@redhat.com>
2021-04-05 15:50:48 -0400

    c++: reinterpret_cast from prvalue to rvalue ref [PR98440]
    In r260622 I allowed this under the general principle that [basic.lval]
    "Whenever a prvalue appears as an operand of an operator that expects a
    glvalue for that operand, the temporary materialization conversion (7.3.4)
    is applied to convert the expression to an xvalue."  But
    [expr.reinterpret.cast] specifically excludes creating a temporary in this
    case.
    
    gcc/cp/ChangeLog:
    
    	PR c++/98440
    	* typeck.c (build_reinterpret_cast_1): Don't perform
    	temporary materialization.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp0x/rv-cast6.C: Expect reinterpret_cast error.
    	* g++.dg/cpp0x/reinterpret_cast2.C: Adjust message.
    	* g++.old-deja/g++.jason/rvalue3.C: Likewise.

--- gcc/testsuite/g++.dg/cpp0x/rv-cast6.C
+++ gcc/testsuite/g++.dg/cpp0x/rv-cast6.C
@@ -5,7 +5,7 @@ struct A { virtual void f(); };
 struct B : A {};
 
 auto && a = static_cast<A&&>(B());
-auto && b = reinterpret_cast<A&&>(B());
+auto && b = reinterpret_cast<A&&>(B()); // { dg-error "prvalue" }
 auto && c = dynamic_cast<A&&>(B());
 auto && d = dynamic_cast<B&&>(static_cast<A&&>(B()));
 auto && e = const_cast<B&&>(B());
