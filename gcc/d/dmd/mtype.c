d48f87d5c1927b1bf2009af3251fe8757e823713
Iain Buclaw <ibuclaw@gdcproject.org>
2021-04-06 13:12:54 +0200

    d: Fix missing call to va_end in getMatchError [PR99917]
    Reviewed-on: https://github.com/dlang/dmd/pull/12380
    
    gcc/d/ChangeLog:
    
    	PR d/99917
    	* dmd/MERGE: Merge upstream dmd d16195406.

--- gcc/d/dmd/mtype.c
+++ gcc/d/dmd/mtype.c
@@ -5220,6 +5220,7 @@ static const char *getMatchError(const char *format, ...)
     va_list ap;
     va_start(ap, format);
     buf.vprintf(format, ap);
+    va_end(ap);
     return buf.extractChars();
 }
 
