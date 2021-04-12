a694a02125f181afe2ae461dd8f35c6f5f54ad22
Martin Liska <mliska@suse.cz>
2021-04-12 13:16:22 +0200

    mklog: support long filenames
    contrib/ChangeLog:
    
    	* gcc-changelog/git_commit.py: Support long filenames
    	in entries.
    	* gcc-changelog/test_email.py: Test it.
    	* gcc-changelog/test_patches.txt: Likewise.

--- contrib/gcc-changelog/git_commit.py
+++ contrib/gcc-changelog/git_commit.py
@@ -415,8 +415,10 @@ class GitCommit:
             if line != line.rstrip():
                 self.errors.append(Error('trailing whitespace', line))
             if len(line.replace('\t', ' ' * TAB_WIDTH)) > LINE_LIMIT:
-                self.errors.append(Error('line exceeds %d character limit'
-                                         % LINE_LIMIT, line))
+                # support long filenames
+                if not line.startswith('\t* ') or not line.endswith(':'):
+                    self.errors.append(Error('line exceeds %d character limit'
+                                             % LINE_LIMIT, line))
             m = changelog_regex.match(line)
             if m:
                 last_entry = ChangeLogEntry(m.group(1).rstrip('/'),
