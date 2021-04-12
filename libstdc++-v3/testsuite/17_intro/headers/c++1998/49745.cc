436891d97aef6d7a8e107d2d3e02e8ca4c52a51a
Jonathan Wakely <jwakely@redhat.com>
2021-04-12 11:12:47 +0100

    libstdc++: Fix test for libstdc++ not including <unistd.h> [PR100117]
    The <cxxx> headers for the C library are not under our control, so we
    can't prevent them from including <unistd.h>. Change the PR 49745 test
    to only include the C++ library headers, not the <cxxx> ones.
    
    To ensure <bits/stdc++.h> isn't included automatically we need to use
    no_pch to disable PCH.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/100117
    	* testsuite/17_intro/headers/c++1998/49745.cc: Explicitly list
    	all C++ headers instead of including <bits/stdc++.h>
    
    (cherry picked from commit 77963796aef8aa07993c0bc757c15848fab7432a)

--- libstdc++-v3/testsuite/17_intro/headers/c++1998/49745.cc
+++ libstdc++-v3/testsuite/17_intro/headers/c++1998/49745.cc
@@ -1,4 +1,5 @@
 // { dg-do compile { target *-*-linux* *-*-gnu* } }
+// { dg-add-options no_pch }
 
 // Copyright (C) 2011-2020 Free Software Foundation, Inc.
 //
@@ -18,5 +19,115 @@
 // <http://www.gnu.org/licenses/>.
 
 // libstdc++/49745
+// error: 'int truncate' redeclared as different kind of symbol
+
+// This tests that no libstdc++ headers include <unistd.h>.
+// However, as discussed in PR libstdc++/100117 we cannot guarantee that
+// no libc headers include it indirectly, so only include the C++ headers.
+// That means we can't use <bits/stdc++.h> because that includes <cstdio> etc.
+// so we list the C++ headers explicitly here. This list is unfortunately
+// doomed to get out of date as new headers are added to the library.
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
 #include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <codecvt>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#if __cplusplus >= 201402L
+#include <shared_mutex>
+#endif
+
+#if __cplusplus >= 201703L
+#include <any>
+#include <charconv>
+// #include <execution>
+#include <filesystem>
+#include <optional>
+#include <memory_resource>
+#include <string_view>
+#include <variant>
+#endif
+
+#if __cplusplus >= 202002L
+#include <barrier>
+#include <bit>
+#include <compare>
+#include <concepts>
+#if __cpp_impl_coroutine
+# include <coroutine>
+#endif
+#if __has_include (<format>)
+# include <format>
+#endif
+#include <latch>
+#include <numbers>
+#include <ranges>
+#include <span>
+#include <stop_token>
+#include <semaphore>
+#include <source_location>
+#include <syncstream>
+#include <version>
+#endif
+
+#if __cplusplus > 202002L
+#if __has_include(<spanstream>)
+# include <spanstream>
+#endif
+#if __has_include(<stacktrace>)
+# include <stacktrace>
+#endif
+#endif
+
 int truncate = 0;
