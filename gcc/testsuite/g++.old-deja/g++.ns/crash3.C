96d73645c77e7784ebd12527322d52b6b7eedb06
Patrick Palka <ppalka@redhat.com>
2021-04-13 15:40:08 -0400

    c++: Adjust expected diagnostics for old-deja tests [PR99008]
    I missed adjusting these tests in the recently committed r11-8155.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/99008
    	* g++.old-deja/g++.ns/crash3.C: Adjust expected diagnostic.
    	* g++.old-deja/g++.ns/template7.C: Likewise.
    	* g++.old-deja/g++.pt/crash8.C: Likewise.

--- gcc/testsuite/g++.old-deja/g++.ns/crash3.C
+++ gcc/testsuite/g++.old-deja/g++.ns/crash3.C
@@ -6,6 +6,6 @@ namespace N {
 
 void f()
 {
-  N::S(); // { dg-error "6:cannot deduce template arguments" "" { target c++17 } } invalid use of template
+  N::S(); // { dg-error "8:class template argument deduction failed|no match" "" { target c++17 } } invalid use of template
   // { dg-error "7:missing template arguments" "" { target c++14_down } .-1 }
 }
