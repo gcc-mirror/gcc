785209fc464ee3efec2b2a8e8244b7292c251ad8
Martin Sebor <msebor@redhat.com>
2021-04-14 10:43:22 -0600

    PR testsuite/100073 - test case gcc.dg/pr86058.c fails on arm, powerpc64
    gcc/testsuite/ChangeLog:
    	* gcc.dg/pr86058.c: Limit to just x86_64.

--- gcc/testsuite/gcc.dg/pr86058.c
+++ gcc/testsuite/gcc.dg/pr86058.c
@@ -1,8 +1,9 @@
 /* PR middle-end/86058 - TARGET_MEM_REF causing incorrect message for
    -Wmaybe-uninitialized warning
-   { dg-do compile } 
+   The test fails on a number of non-x86_64 targets due to pr100073.
+   { dg-do compile { target x86_64-*-* } }
    { dg-options "-O2 -Wuninitialized -Wmaybe-uninitialized" } */
-    
+
 extern void foo (int *);
 
 void zip (int *out, int indx)
@@ -10,9 +11,9 @@ void zip (int *out, int indx)
   int arr[10];
 
   for (int i = 0; i < indx; ++i)
-    out[i] = arr[i] + 1;  // { dg-warning "'arr\\\[i]' may be used uninitialized" "pr?????" { xfail *-*-* } }
+    out[i] = arr[i] + 1;  // { dg-warning "'arr\\\[i]' may be used uninitialized" "pr99944" { xfail *-*-* } }
                           // { dg-warning "'arr' may be used uninitialized" "actual" { target *-*-* } .-1 }
-  
+
   foo (arr);
   foo (out);
 }
