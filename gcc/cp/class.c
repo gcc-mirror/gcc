f59cfce8ba6612c063a8f55267dc2caee7e0d381
Jason Merrill <jason@redhat.com>
2021-04-23 16:41:35 -0400

    c++: -Wdeprecated-copy and using operator= [PR92145]
    For the purpose of [depr.impldec] "if the class has a user-declared copy
    assignment operator", an operator= brought in from a base class with 'using'
    may be a copy-assignment operator, but it isn't a copy-assignment operator
    for the derived class.
    
    gcc/cp/ChangeLog:
    
    	PR c++/92145
    	* class.c (classtype_has_depr_implicit_copy): Check DECL_CONTEXT
    	of operator=.
    
    gcc/testsuite/ChangeLog:
    
    	PR c++/92145
    	* g++.dg/cpp0x/depr-copy3.C: New test.
    
    (cherry picked from commit 37846c42f1f5ac4d9ba190d49c4373673c89c8b5)

--- gcc/cp/class.c
+++ gcc/cp/class.c
@@ -5685,7 +5685,8 @@ classtype_has_depr_implicit_copy (tree t)
 	 iter; ++iter)
       {
 	tree fn = *iter;
-	if (user_provided_p (fn) && copy_fn_p (fn))
+	if (DECL_CONTEXT (fn) == t
+	    && user_provided_p (fn) && copy_fn_p (fn))
 	  return fn;
       }
 
