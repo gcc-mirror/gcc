06c86a4f210c76a157512a2963e6c31302d161cb
Jonathan Wakely <jwakely@redhat.com>
2021-04-26 21:16:21 +0100

    libstdc++: Fix socket option classes
    This fixes some flaws in the socket option types defined in
    net::socket_base:
    
    - The constructors were not noexcept.
    - The __sockopt_base<T>::value() member function was present
      unconditionally (so was defined for socket_base::linger which is
      incorrect).
    - The __socket_crtp<C, T>::operator=(T) assignment operator was not
      noexcept, and was hidden in the derived classes.
    
    Also:
    
    - Use class instead of struct for the socket option types.
    - Define the _S_level and _S_name constants as private.
    - Declare the __socket_crtp base as a friend.
    
    libstdc++-v3/ChangeLog:
    
    	* include/experimental/bits/net.h (__socket_base): Add
    	bool template parameter to allow BooleanSocketOption and
    	IntegerSocketOption to have different __socket_base<int>
    	base classes.
    	(__socket_base<bool>): Adjust base class.
    	(__socket_base<int>): Add partial specialization.
    	(__socket_crtp::operator=(_Tp)): Add noexcept-specifier.
    	* include/experimental/socket (socket_base::broadcast)
    	(socket_base::debug, socket_base::do_not_route)
    	(socket_base::keep_alive, socket_base::linger)
    	(socket_base::out_of_band_inline)
    	(socket_base::receive_buffer_size)
    	(socket_base::receive_low_watermark)
    	(socket_base::reuse_address, socket_base::send_buffer_size)
    	(socket_base::send_low_watermark): Add using-declaration for
    	__socket_crtp::operator=(_Tp).
    	* testsuite/experimental/net/socket/socket_base.cc: Check
    	properties of socket option types.

--- libstdc++-v3/testsuite/experimental/net/socket/socket_base.cc
+++ libstdc++-v3/testsuite/experimental/net/socket/socket_base.cc
@@ -15,13 +15,155 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-do compile { target c++14 } }
+// { dg-do run { target c++14 } }
 
 #include <experimental/socket>
 #include <testsuite_common_types.h>
+#include <testsuite_hooks.h>
 
 using S = std::experimental::net::socket_base;
 using namespace std;
+
+// Dummy protocol
+struct P
+{
+  struct endpoint
+  {
+    using protocol_type = P;
+    P protocol() const;
+  };
+};
+
+static_assert( ! is_default_constructible<S>(), "" );
+static_assert( ! is_destructible<S>(), "" );
+
+template<typename C, typename T>
+void check_gettable_sockopt()
+{
+  P p;
+  static_assert( is_same<decltype(declval<const C&>().level(p)), int>(), "" );
+  static_assert( noexcept(declval<const C&>().level(p)), "" );
+
+  static_assert( is_same<decltype(declval<const C&>().name(p)), int>(), "" );
+  static_assert( noexcept(declval<const C&>().name(p)), "" );
+
+  static_assert( is_same<decltype(declval<C&>().data(p)), void*>(), "" );
+  static_assert( noexcept(declval<C&>().data(p)), "" );
+
+  static_assert( is_same<decltype(declval<const C&>().size(p)), size_t>(), "" );
+  static_assert( noexcept(declval<const C&>().size(p)), "" );
+
+  static_assert( is_same<decltype(declval<C&>().resize(p, 0)), void>(), "" );
+  static_assert( ! noexcept(declval<C&>().resize(p, 0)), "" );
+
+  C opt;
+  VERIFY(opt.size(p) == sizeof(T));
+}
+
+template<typename C, typename T>
+void check_settable_sockopt()
+{
+  P p;
+  static_assert( is_same<decltype(declval<const C&>().level(p)), int>(), "" );
+  static_assert( noexcept(declval<const C&>().level(p)), "" );
+
+  static_assert( is_same<decltype(declval<const C&>().name(p)), int>(), "" );
+  static_assert( noexcept(declval<const C&>().name(p)), "" );
+
+  static_assert( is_same<decltype(declval<const C&>().data(p)), const void*>(), "" );
+  static_assert( noexcept(declval<const C&>().data(p)), "" );
+
+  static_assert( is_same<decltype(declval<C&>().size(p)), size_t>(), "" );
+  static_assert( noexcept(declval<C&>().size(p)), "" );
+
+  C opt;
+  VERIFY(opt.size(p) == sizeof(T));
+}
+
+template<typename C, typename T = int>
+void check_boolean_sockopt()
+{
+  check_gettable_sockopt<C, T>();
+  check_settable_sockopt<C, T>();
+
+  static_assert( is_destructible<C>(), "" );
+  static_assert( is_nothrow_default_constructible<C>(), "" );
+  static_assert( is_nothrow_copy_constructible<C>(), "" );
+  static_assert( is_nothrow_copy_assignable<C>(), "" );
+
+  static_assert( is_nothrow_constructible<C, bool>(), "" );
+  static_assert( is_nothrow_assignable<C&, bool>(), "" );
+
+  static_assert( is_same<decltype(declval<const C&>().value()), bool>(), "" );
+  static_assert( noexcept(declval<const C&>().value()), "" );
+
+  static_assert( is_same<decltype(static_cast<bool>(declval<const C&>())), bool>(), "" );
+  static_assert( noexcept(static_cast<bool>(declval<const C&>())), "" );
+
+  static_assert( is_same<decltype(!declval<const C&>()), bool>(), "" );
+  static_assert( noexcept(!declval<const C&>()), "" );
+}
+
+template<typename C, typename T = int>
+void check_integer_sockopt()
+{
+  check_gettable_sockopt<C, T>();
+  check_settable_sockopt<C, T>();
+
+  static_assert( is_destructible<C>(), "" );
+  static_assert( is_nothrow_default_constructible<C>(), "" );
+  static_assert( is_nothrow_copy_constructible<C>(), "" );
+  static_assert( is_nothrow_copy_assignable<C>(), "" );
+
+  static_assert( is_nothrow_constructible<C, int>(), "" );
+  static_assert( is_nothrow_assignable<C&, int>(), "" );
+
+  static_assert( is_same<decltype(declval<const C&>().value()), int>(), "" );
+  static_assert( noexcept(declval<const C&>().value()), "" );
+}
+
+void test_option_types()
+{
+  check_boolean_sockopt<S::broadcast>();
+
+  check_boolean_sockopt<S::debug>();
+
+  check_boolean_sockopt<S::do_not_route>();
+
+  check_boolean_sockopt<S::keep_alive>();
+
+  check_gettable_sockopt<S::linger, ::linger>();
+  check_settable_sockopt<S::linger, ::linger>();
+  static_assert( is_destructible<S::linger>(), "" );
+  static_assert( is_nothrow_default_constructible<S::linger>(), "" );
+  static_assert( is_nothrow_copy_constructible<S::linger>(), "" );
+  static_assert( is_nothrow_copy_assignable<S::linger>(), "" );
+  static_assert( is_nothrow_constructible<S::linger, bool, chrono::seconds>(), "" );
+
+  static_assert( is_same<decltype(declval<const S::linger&>().enabled()), bool>(), "" );
+  static_assert( noexcept(declval<const S::linger&>().enabled()), "" );
+
+  static_assert( is_void<decltype(declval<S::linger&>().enabled(true))>(), "" );
+  static_assert( noexcept(declval<S::linger&>().enabled(true)), "" );
+
+  static_assert( is_same<decltype(declval<const S::linger&>().timeout()), chrono::seconds>(), "" );
+  static_assert( noexcept(declval<const S::linger&>().timeout()), "" );
+
+  static_assert( is_void<decltype(declval<S::linger&>().timeout(chrono::seconds()))>(), "" );
+  static_assert( noexcept(declval<S::linger&>().timeout(chrono::seconds())), "" );
+
+  check_boolean_sockopt<S::out_of_band_inline>();
+
+  check_integer_sockopt<S::receive_buffer_size>();
+
+  check_integer_sockopt<S::receive_low_watermark>();
+
+  check_boolean_sockopt<S::reuse_address>();
+
+  check_integer_sockopt<S::send_buffer_size>();
+
+  check_integer_sockopt<S::send_low_watermark>();
+}
 
 void test_constants()
 {
@@ -43,3 +185,7 @@ void test_constants()
   static_assert( is_same<decltype(m), const int*>::value, "" );
 }
 
+int main()
+{
+  test_option_types();
+}
