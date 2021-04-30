6e00d9bb113b531be15bc71cad2f1742648ba18a
Patrick Palka <ppalka@redhat.com>
2021-04-30 10:59:20 -0400

    libstdc++: Implement P2367 changes to avoid some list-initialization
    This implements the wording changes of P2367R0 "Remove misuses of
    list-initialization from Clause 24", modulo the parts that depend
    on P1739R4 which we don't yet implement (due to LWG 3407).
    
    libstdc++-v3/ChangeLog:
    
    	* include/bits/ranges_util.h (subrange::subrange): Avoid
    	list-initialization in delegating constructor.
    	* include/std/ranges (single_view): Replace implicit guide
    	with explicit deduction guide that decays its argument.
    	(_Single::operator()): Avoid CTAD when constructing the
    	single_view object.
    	(_Iota::operator()): Avoid list-initialization.
    	(__detail::__can_filter_view, _Filter::operator()): Likewise.
    	(__detail::__can_transform_view, _Transform::operator()): Likewise.
    	(take_view::begin): Likewise.
    	(__detail::__can_take_view, _Take::operator()): Likewise.
    	(__detail::__can_take_while_view, _TakeWhile::operator()): Likewise.
    	(__detail::__can_drop_view, _Drop::operator()): Likewise.
    	(__detail::__can_drop_while_view, _DropWhile::operator()): Likewise.
    	(split_view::split_view): Use views::single when initializing
    	_M_pattern.
    	(__detail::__can_split_view, _Split::operator()): Avoid
    	list-initialization.
    	(_Counted::operator()): Likewise.
    	* testsuite/std/ranges/p2367.cc: New test.

---
+++ libstdc++-v3/testsuite/std/ranges/p2367.cc
@@ -0,0 +1,48 @@
+// Copyright (C) 2021 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-options "-std=gnu++2a" }
+// { dg-do compile { target c++2a } }
+
+// Verify P2367 changes.
+
+#include <ranges>
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+void
+test01()
+{
+  extern int (&x)[5];
+
+  // Verify changes to views::single.
+  using ranges::single_view;
+  using std::same_as;
+  same_as<single_view<int*>> auto v1 = views::single(x);
+  same_as<single_view<int>> auto v2 = views::single((const int)5);
+  same_as<single_view<single_view<int>>> auto v3 = views::single(v2);
+
+  // Verify changes to views::take.
+  auto v4 = views::take(x, 0ull);
+
+  // Verify changes to views::drop.
+  auto v5 = views::drop(x, 0ull);
+
+  // Verify changes to views::split.
+  auto v6 = views::split(x, 5u);
+}
