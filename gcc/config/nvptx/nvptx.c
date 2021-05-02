af08d7265bb5a82f3728737476c8098458a8d210
Jakub Jelinek <jakub@redhat.com>
2021-05-02 14:17:23 +0200

    nvptx: Fix up nvptx build against latest libstdc++ [PR100375]
    The r12-220-gd96db15967e78d7cecea3b1cf3169ceb924678ac change
    deprecated some non-standard std::pair constructors and that apparently
    broke nvptx.c build, where pseudo_node_t is std::pair<struct basic_block_def *, int>
    and so nullptr (or NULL) needs to be used for the first argument of the
    ctors instead of 0.
    
    2021-05-02  Jakub Jelinek  <jakub@redhat.com>
    
    	PR target/100375
    	* config/nvptx/nvptx.c (nvptx_sese_pseudo): Use NULL instead of 0
    	as first argument of pseudo_node_t constructors.
    
    (cherry picked from commit 7911a905276781c20f704f5a91b5125e0184d072)

--- gcc/config/nvptx/nvptx.c
+++ gcc/config/nvptx/nvptx.c
@@ -3576,9 +3576,9 @@ nvptx_sese_pseudo (basic_block me, bb_sese *sese, int depth, int dir,
   edge e;
   edge_iterator (ei);
   int hi_back = depth;
-  pseudo_node_t node_back (0, depth);
+  pseudo_node_t node_back (NULL, depth);
   int hi_child = depth;
-  pseudo_node_t node_child (0, depth);
+  pseudo_node_t node_child (NULL, depth);
   basic_block child = NULL;
   unsigned num_children = 0;
   int usd = -dir * sese->dir;
@@ -3645,7 +3645,7 @@ nvptx_sese_pseudo (basic_block me, bb_sese *sese, int depth, int dir,
       else
 	{ /* Fallen off graph, backlink to entry node.  */
 	  hi_back = 0;
-	  node_back = pseudo_node_t (0, 0);
+	  node_back = pseudo_node_t (NULL, 0);
 	}
     }
 
@@ -3666,7 +3666,7 @@ nvptx_sese_pseudo (basic_block me, bb_sese *sese, int depth, int dir,
       else
 	{
 	  /* back edge to entry node */
-	  sese->push (pseudo_node_t (0, 0));
+	  sese->push (pseudo_node_t (NULL, 0));
 	}
     }
   
@@ -3675,7 +3675,7 @@ nvptx_sese_pseudo (basic_block me, bb_sese *sese, int depth, int dir,
   if (!sese->brackets.length () || !edges || !edges->length ())
     {
       hi_back = 0;
-      node_back = pseudo_node_t (0, 0);
+      node_back = pseudo_node_t (NULL, 0);
       sese->push (node_back);
     }
 
