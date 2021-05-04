cea2b1a52b3c5fe9f70daf460afe067e245bb1d7
Jonathan Wakely <jwakely@redhat.com>
2021-05-04 15:28:39 +0100

    libstdc++: Fix undefined behaviour in testsuite
    Fix some test bugs found by ubsan.
    
    libstdc++-v3/ChangeLog:
    
    	* testsuite/20_util/from_chars/3.cc: Use unsigned type to avoid
    	overflow.
    	* testsuite/24_iterators/reverse_iterator/2.cc: Do not add
    	non-zero value to null pointer.
    	* testsuite/25_algorithms/copy_backward/move_iterators/69478.cc:
    	Use past-the-end iterator for result.
    	* testsuite/25_algorithms/move_backward/69478.cc: Likewise.
    	* testsuite/25_algorithms/move_backward/93872.cc: Likewise.
    
    (cherry picked from commit 6fb8b67089119b737ccb38f75f403b8d279e2229)

--- libstdc++-v3/testsuite/24_iterators/reverse_iterator/2.cc
+++ libstdc++-v3/testsuite/24_iterators/reverse_iterator/2.cc
@@ -27,7 +27,7 @@ void test02()
   iterator_type it01;
   iterator_type it02;
 
-  // Sanity check non-member operators and functions can be instantiated. 
+  // Sanity check non-member operators and functions can be instantiated.
   it01 == it02;
   it01 != it02;
   it01 < it02;
@@ -35,11 +35,11 @@ void test02()
   it01 > it02;
   it01 >= it02;
   it01 - it02;
-  5 + it02;
+  0 + it02;
 }
 
-int main() 
-{ 
+int main()
+{
   test02();
   return 0;
 }
