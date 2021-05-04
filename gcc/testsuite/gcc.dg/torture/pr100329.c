a310bb73edc9548e08d1fa28e7a56246caf27757
Richard Biener <rguenther@suse.de>
2021-05-04 10:07:35 +0200

    tree-optimization/100329 - avoid reassociating asm goto defs
    This avoids reassociating asm goto defs because we have no idea
    on which outgoing edge to insert defs.
    
    2021-05-04  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/100329
    	* tree-ssa-reassoc.c (can_reassociate_p): Do not reassociate
    	asm goto defs.
    	(insert_stmt_after): Assert we're not running into asm goto.
    
    	* gcc.dg/torture/pr100329.c: New testcase.

---
+++ gcc/testsuite/gcc.dg/torture/pr100329.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-additional-options "--param tree-reassoc-width=2" } */
+
+unsigned int a0;
+
+unsigned int
+foo (unsigned int a1, unsigned int a2)
+{
+  unsigned int x;
+
+  asm goto ("" : "=r" (x) : : : lab);
+  a0 = x;
+
+ lab:
+  return x + a1 + a2 + 1;
+}
