a8b79cc939d6786293f654c42a2d1b0ab040de0e
Harald Anlauf <anlauf@gmx.de>
2021-05-05 15:25:50 +0200

    PR fortran/100274 - ICE in gfc_conv_procedure_call, at fortran/trans-expr.c:6131
    When the check for the length of formal and actual character arguments
    found a mismatch and emitted a warning, it would skip further checks
    like that could lead to errors.  Fix that by continuing the checking.
    Also catch a NULL pointer dereference.
    
    gcc/fortran/ChangeLog:
    
    	PR fortran/100274
    	* interface.c (gfc_compare_actual_formal): Continue checks after
    	emitting warning for argument length mismatch.
    	* trans-expr.c (gfc_conv_procedure_call): Check for NULL pointer
    	dereference.
    
    gcc/testsuite/ChangeLog:
    
    	PR fortran/100274
    	* gfortran.dg/argument_checking_25.f90: New test.

--- gcc/fortran/trans-expr.c
+++ gcc/fortran/trans-expr.c
@@ -6125,6 +6125,7 @@ gfc_conv_procedure_call (gfc_se * se, gfc_symbol * sym,
 		      bool add_clobber;
 		      add_clobber = fsym && fsym->attr.intent == INTENT_OUT
 			&& !fsym->attr.allocatable && !fsym->attr.pointer
+			&& e->symtree && e->symtree->n.sym
 			&& !e->symtree->n.sym->attr.dimension
 			&& !e->symtree->n.sym->attr.pointer
 			&& !e->symtree->n.sym->attr.allocatable
