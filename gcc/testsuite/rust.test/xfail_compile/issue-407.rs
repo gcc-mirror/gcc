613eeee78a1ffd29332e3b0f500bfd476f8bfa9c
Thomas Schwinge <thomas@codesourcery.com>
2021-05-05 22:31:52 +0200

    Add note to 'rust.test/xfail_compile/issue-407.rs' [#407, #409]

--- gcc/testsuite/rust.test/xfail_compile/issue-407.rs
+++ gcc/testsuite/rust.test/xfail_compile/issue-407.rs
@@ -1,5 +1,6 @@
 // { dg-excess-errors "Noisy error and debug" }
 
+// This already worked before the #409 code changes.
 fn test()  {
     let mut a = 1;
     a + = 1; // { dg-error "found unexpected token '=' in null denotation" }
