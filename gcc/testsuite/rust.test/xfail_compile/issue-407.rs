d29b5c5ac3a90665bf5d897159ede20cb765d4a3
Thomas Schwinge <thomas@codesourcery.com>
2021-05-05 22:30:46 +0200

    Use standard 'dg-excess-errors' directive comment in 'rust.test/xfail_compile/issue-407.rs' [#409]

--- gcc/testsuite/rust.test/xfail_compile/issue-407.rs
+++ gcc/testsuite/rust.test/xfail_compile/issue-407.rs
@@ -1,4 +1,5 @@
-// { dg-excess-errors "failed to parse" }
+// { dg-excess-errors "Noisy error and debug" }
+
 fn test()  {
     let mut a = 1;
     a + = 1; // { dg-error "found unexpected token '=' in null denotation" }
