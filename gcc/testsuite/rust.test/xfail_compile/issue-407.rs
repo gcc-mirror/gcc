843547eb48549342f464c39a658c759ac1e42f58
Thomas Schwinge <thomas@codesourcery.com>
2021-05-05 22:29:39 +0200

    Fix 'dg-error' directive in 'rust.test/xfail_compile/issue-407.rs' [#409]
    ..., so that this actually does verify the error diagnostic.

--- gcc/testsuite/rust.test/xfail_compile/issue-407.rs
+++ gcc/testsuite/rust.test/xfail_compile/issue-407.rs
@@ -1,5 +1,5 @@
 // { dg-excess-errors "failed to parse" }
 fn test()  {
     let mut a = 1;
-    a + = 1; // { dg-error "found unexpected token ‘=’ in null denotation"
+    a + = 1; // { dg-error "found unexpected token '=' in null denotation" }
 }
