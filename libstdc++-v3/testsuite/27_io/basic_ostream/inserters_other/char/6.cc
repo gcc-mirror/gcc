a87ceadf17b4a899f3e74e2da8b6b209461d2742
Jonathan Wakely <jwakely@redhat.com>
2021-05-06 19:14:42 +0100

    libstdc++: Fix constraints for rvalue stream insertion/extraction
    The __rval_streamable() function was an attempt to test for
    convertibility cheaply and without confusing diagnostics. It doesn't
    work with Clang though, and is probably ill-formed.
    
    Replace that helper function with a check for derivation from ios_base,
    and use that in the alias templates __rvalue_stream_insertion_t and
    __rvalue_stream_extraction_t. Use concepts for the constraints when
    available.
    
    libstdc++-v3/ChangeLog:
    
    	* include/std/istream (__rvalue_stream_extraction_t): Replace
    	use of __rval_streamable.
    	* include/std/ostream (__rvalue_stream_insertion_t): Likewise.
    	(__rval_streamable): Remove.
    	(_Require_derived_from_ios_base, __derived_from_ios_base): New
    	helper for checking constraints.
    	* testsuite/27_io/basic_istream/extractors_other/char/4.cc: Fix
    	reference to the wrong subclause of the standard.
    	* testsuite/27_io/basic_istream/extractors_other/wchar_t/4.cc:
    	Likewise.
    	* testsuite/27_io/basic_ostream/inserters_other/char/6.cc:
    	Likewise.
    	* testsuite/27_io/basic_ostream/inserters_other/wchar_t/6.cc:
    	Likewise.
    	* testsuite/27_io/basic_ostream/inserters_other/char/99692.cc:
    	New test.
    	* testsuite/27_io/filesystem/path/io/dr2989.cc: Adjust pruned
    	errors.

--- libstdc++-v3/testsuite/27_io/basic_ostream/inserters_other/char/6.cc
+++ libstdc++-v3/testsuite/27_io/basic_ostream/inserters_other/char/6.cc
@@ -17,7 +17,7 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// 27.6.2.5.3 basic_ostream manipulator inserters
+// C++11 27.7.3.9 Rvalue stream insertion [ostream.rvalue]
 
 #include <sstream>
 
