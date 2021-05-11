9d6ee0d3c42807596c50569fc01b3f31a6e0900a
Patrick Palka <ppalka@redhat.com>
2021-05-11 13:19:46 -0400

    libstdc++: Remove extern "C" from Ryu sources
    floating_to_chars.cc includes the Ryu sources into an anonymous
    namespace as a convenient way to give all its symbols internal linkage.
    But an entity declared extern "C" always has external linkage even
    from within an anonymous namespace, so this trick doesn't work in the
    presence of extern "C", and it causes the Ryu function generic_to_chars
    to be visible from libstdc++.a.
    
    This patch removes the only use of extern "C" from our local copy of
    Ryu along with some declarations for never-defined functions that GCC
    now warns about.
    
    libstdc++-v3/ChangeLog:
    
    	* src/c++17/ryu/LOCAL_PATCHES: Update.
    	* src/c++17/ryu/ryu_generic_128.h: Remove extern "C".
    	Remove declarations for never-defined functions.
    	* testsuite/20_util/to_chars/4.cc: New test.
    
    (cherry picked from commit 84b384a16f388c68166b0603e50d0b477d0830a7)

---
+++ libstdc++-v3/testsuite/20_util/to_chars/4.cc
@@ -0,0 +1,36 @@
+// Copyright (C) 2021 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do link { target c++17 } }
+// { dg-require-effective-target ieee-floats }
+// { dg-require-static-libstdcxx }
+// { dg-additional-options "-static-libstdc++" }
+
+// Verify the Ryu symbol generic_to_chars doesn't inadvertently leak into
+// libstdc++.a.  If it did, this test would fail at link time with a multiple
+// definition error.
+
+#include <charconv>
+
+extern "C" void generic_to_chars(void) { }
+
+int
+main()
+{
+  char x[64];
+  std::to_chars(x, x+64, 42.L, std::chars_format::scientific);
+}
