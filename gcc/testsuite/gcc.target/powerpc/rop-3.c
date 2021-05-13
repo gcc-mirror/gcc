5bd66c6826e5f3a38ba393702923feb88959f845
Bill Schmidt <wschmidt@linux.ibm.com>
2021-05-13 13:33:35 -0500

    rs6000: Add ROP tests
    2021-05-13  Bill Schmidt  <wschmidt@linux.ibm.com>
    
    gcc/testsuite/
    	* gcc.target/powerpc/rop-1.c: New.
    	* gcc.target/powerpc/rop-2.c: New.
    	* gcc.target/powerpc/rop-3.c: New.
    	* gcc.target/powerpc/rop-4.c: New.
    	* gcc.target/powerpc/rop-5.c: New.

---
+++ gcc/testsuite/gcc.target/powerpc/rop-3.c
@@ -0,0 +1,18 @@
+/* { dg-do run { target { power10_hw } } } */
+/* { dg-require-effective-target powerpc_elfv2 } */
+/* { dg-options "-O2 -mdejagnu-cpu=power10 -mrop-protect" } */
+
+/* Verify that ROP-protect instructions execute correctly when a
+   call is present.  */
+
+void __attribute__((noipa)) foo ()
+{
+  asm ("");
+}
+
+int main ()
+{
+  foo ();
+  return 0;
+}
+
