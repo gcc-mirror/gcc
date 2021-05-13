810afb0b5fbb9da1e0e51ee9607f275f14c17459
Martin Liska <mliska@suse.cz>
2021-05-13 09:23:30 +0200

    testsuite: prune new LTO warning
    libgomp/ChangeLog:
    
    	PR testsuite/100569
    	* testsuite/libgomp.c/omp-nested-3.c: Prune new LTO warning.
    	* testsuite/libgomp.c/pr46032-2.c: Likewise.
    	* testsuite/libgomp.oacc-c-c++-common/data-clauses-kernels-ipa-pta.c: Likewise.
    	* testsuite/libgomp.oacc-c-c++-common/data-clauses-parallel-ipa-pta.c: Likewise.
    
    gcc/testsuite/ChangeLog:
    
    	PR testsuite/100569
    	* gcc.dg/atomic/c11-atomic-exec-2.c: Prune new LTO warning.
    	* gcc.dg/torture/pr94947-1.c: Likewise.

--- libgomp/testsuite/libgomp.c/omp-nested-3.c
+++ libgomp/testsuite/libgomp.c/omp-nested-3.c
@@ -1,4 +1,5 @@
 // { dg-do run { target lto } }
 // { dg-additional-options "-fipa-pta -flto -flto-partition=max" }
+// { dg-prune-output "warning: using serial compilation" }
 
 #include "omp-nested-1.c"
