8552cfa658dcfbd3ed85db356f0782ba69041d14
Jakub Jelinek <jakub@redhat.com>
2021-05-14 13:24:12 +0200

    testsuite: Add testcase for already fixed PR
    2021-05-14  Jakub Jelinek  <jakub@redhat.com>
    
    	* g++.dg/cpp1y/pr88872.C: New test.
    
    (cherry picked from commit f05627d404038368b99e92ac4df4c29f4ae4a5fa)

---
+++ gcc/testsuite/g++.dg/cpp1y/pr88872.C
@@ -0,0 +1,24 @@
+// PR c++/88872
+// { dg-do compile { target c++14 } }
+
+struct a {
+  template <typename b> constexpr a(b) : c() {}
+  int c;
+};
+void d();
+template <char...> constexpr a operator"" _n() { return d; }
+struct e;
+struct f {
+  e operator[](int);
+};
+struct g {
+  void h();
+  f i;
+};
+template <typename> struct j {
+  void k() { [](auto) { constexpr auto l = 2_n; }(keywords); }
+  int keywords;
+};
+using m = j<int>;
+class e : public m {};
+void g::h() { i[0].k(); }
