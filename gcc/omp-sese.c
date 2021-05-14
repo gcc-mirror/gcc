36d5a174f82ac554014da4352f3f08ddfb3e561c
Tobias Burnus <tobias@codesourcery.com>
2021-05-14 09:44:19 +0200

    Fix fallout from merge from releases/gcc-11
    Re-apply commit f963d6c79b1d52ce565c772166c3c3e1b1d0aa78 on nvptx.c:
    "Remove duplicate SESE code in NVPTX backend"
    The code got readded by a wrongly resolved merge conflict.
    
    Apply to the OG11 code the change of the merge-conflict causing change:
    commit da9c085ddbfe61e5954c8ec4e996240fa3a994c0
    "nvptx: Fix up nvptx build against latest libstdc++ [PR100375]"
    
    gcc/ChangeLog
    
    	* omp-sese.c (omp_sese_pseudo): Use nullptr instead of 0
    	as first argument of pseudo_node_t constructors.
    	* config/nvptx/nvptx.c (bb_pair_t, bb_pair_vec_t,
    	pseudo_node_t, bracket, bracket_vec_t,
            bb_sese, bb_sese::~bb_sese, bb_sese::append, bb_sese::remove,
    	BB_SET_SESE, BB_GET_SESE, nvptx_sese_number, nvptx_sese_pseudo,
    	nvptx_sese_color, nvptx_find_sese): Remove again.

--- gcc/omp-sese.c
+++ gcc/omp-sese.c
@@ -2084,9 +2084,9 @@ omp_sese_pseudo (basic_block me, bb_sese *sese, int depth, int dir,
   edge e;
   edge_iterator ei;
   int hi_back = depth;
-  pseudo_node_t node_back (0, depth);
+  pseudo_node_t node_back (nullptr, depth);
   int hi_child = depth;
-  pseudo_node_t node_child (0, depth);
+  pseudo_node_t node_child (nullptr, depth);
   basic_block child = NULL;
   unsigned num_children = 0;
   int usd = -dir * sese->dir;
@@ -2153,7 +2153,7 @@ omp_sese_pseudo (basic_block me, bb_sese *sese, int depth, int dir,
       else
 	{ /* Fallen off graph, backlink to entry node.  */
 	  hi_back = 0;
-	  node_back = pseudo_node_t (0, 0);
+	  node_back = pseudo_node_t (nullptr, 0);
 	}
     }
 
@@ -2174,7 +2174,7 @@ omp_sese_pseudo (basic_block me, bb_sese *sese, int depth, int dir,
       else
 	{
 	  /* back edge to entry node */
-	  sese->push (pseudo_node_t (0, 0));
+	  sese->push (pseudo_node_t (nullptr, 0));
 	}
     }
 
@@ -2183,7 +2183,7 @@ omp_sese_pseudo (basic_block me, bb_sese *sese, int depth, int dir,
   if (!sese->brackets.length () || !edges || !edges->length ())
     {
       hi_back = 0;
-      node_back = pseudo_node_t (0, 0);
+      node_back = pseudo_node_t (nullptr, 0);
       sese->push (node_back);
     }
 
