99b76adb94d9363d0ca31a7d34ce850e2ca9ffc5
Andrew Pinski <apinski@marvell.com>
2021-05-16 10:40:16 -0700

    Don't simplify (A & C) != 0 ? D : 0 for pointer types.
    While rewriting part of PHI-OPT to use match-and-simplify,
    I ran into a bug where this pattern in match.pd would hit
    and would produce invalid gimple; a shift of a pointer type.
    
    This just disables this simplification for pointer types similarly
    to what is already done in PHI-OPT for the generic A ? D : 0 case.
    
    OK? Bootstrapped and tested on x86_64-linux-gnu with no regressions.
    
    Thanks,
    Andrew Pinski
    
    2021-5-23  Andrew Pinski  <apinski@marvell.com>
    
    gcc/
    	* match.pd ((A & C) != 0 ? D : 0): Limit to non pointer types.
    
    gcc/testsuite/
    	* gcc.dg/gimplefe-45.c: New test.

---
+++ gcc/testsuite/gcc.dg/gimplefe-45.c
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fgimple" } */
+
+/* This used to ICE when simplifying (A & C) != 0 ? D : 0
+   for pointer types. */
+
+int *__GIMPLE ()
+p (int n)
+{
+  int *_2;
+  int *_t;
+  int *_t1;
+  _t = (int*)8;
+  _t1 = 0;
+  n = n & 2;
+  _2 = n != 0 ? _t : _t1;
+  return _2;
+}
+
