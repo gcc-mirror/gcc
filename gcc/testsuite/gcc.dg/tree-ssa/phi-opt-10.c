1040a44aa23f7b106e1c96e6a247ea6fd4021258
Andrew Pinski <apinski@marvell.com>
2021-05-23 17:35:40 +0000

    Fix two testcases for ssa names which are more than 1 digit
    phi-opt-10.c and phi-opt-7.c both depend on currently that some ssa name
    versions are one digit long which is not always correct. This fixes the
    problem by detecting digits rather than just using '.'.
    
    Committed as obvious after a bootstrap/test.
    
    Thanks,
    Andrew Pinski
    
    gcc/testsuite/ChangeLog
    	* gcc.dg/tree-ssa/phi-opt-10.c: Use "\[0-9\]*" instead of '.'
    	when matching ssa name version.
    	* gcc.dg/tree-ssa/phi-opt-7.c: Likewise.

--- gcc/testsuite/gcc.dg/tree-ssa/phi-opt-10.c
+++ gcc/testsuite/gcc.dg/tree-ssa/phi-opt-10.c
@@ -7,4 +7,4 @@ int eqm1_phi (unsigned long a) { return a ? 0 : -1; }
 int spaceship1 (long a) { return a > 0 ? 1 : a < 0 ? -1 : 0; }
 int spaceship2 (long a) { return a > 0 ? 1 : a == 0 ? 0 : -1; }
 
-/* { dg-final { scan-tree-dump-times " = -\[^\r\n\]*_.;" 4 "optimized"} } */
+/* { dg-final { scan-tree-dump-times " = -\[^\r\n\]*_\[0-9\]*;" 4 "optimized"} } */
