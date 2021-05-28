359c0a86e2974a9f3036bc05b6e6c661f2c463cf
Richard Biener <rguenther@suse.de>
2021-05-28 13:31:35 +0200

    ipa/100791 - copy fntype when processing __builtin_va_arg_pack
    This missing copying exposed a type mismatch in the IL.
    
    2021-05-28  Richard Biener  <rguenther@suse.de>
    
    	PR ipa/100791
    	* tree-inline.c (copy_bb): When processing __builtin_va_arg_pack
    	copy fntype from original call.
    
    	* gcc.dg/pr100791.c: New testcase.

---
+++ gcc/testsuite/gcc.dg/pr100791.c
@@ -0,0 +1,9 @@
+/* { dg-do compile } */
+/* { dg-options "" } */
+
+static inline int __attribute__((__always_inline__))
+foo ()
+{
+  return log_bad_request(0, __builtin_va_arg_pack()); /* { dg-warning "implicit" } */
+}
+void log_bad_request() { foo (0); } /* { dg-warning "conflicting types" } */
