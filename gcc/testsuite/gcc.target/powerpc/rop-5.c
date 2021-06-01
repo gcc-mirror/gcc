3d75b5cd9a362172838825c7083a3afa2403735a
Bill Schmidt <wschmidt@linux.ibm.com>
2021-06-01 15:47:22 -0500

    PR100750: Require ELFv2 ABI for ROP test
    2021-06-01  Bill Schmidt  <wschmidt@linux.ibm.com>
    
    gcc/testsuite/
    	PR testsuite/100750
    	* gcc.target/powerpc/rop-5.c: Require ELFv2 ABI.

--- gcc/testsuite/gcc.target/powerpc/rop-5.c
+++ gcc/testsuite/gcc.target/powerpc/rop-5.c
@@ -1,4 +1,5 @@
 /* { dg-do compile } */
+/* { dg-require-effective-target powerpc_elfv2 } */
 /* { dg-options "-O2 -mdejagnu-cpu=power10 -mrop-protect" } */
 
 /* Verify that __ROP_PROTECT__ is predefined for -mrop-protect.  */
