61fc01806f376a780978a6dea165ec3dadef085b
Jason Merrill <jason@redhat.com>
2021-06-08 17:48:26 -0400

    c++: update diagnostic messages
    These tests needed updating for the diagnostic change in r12-1306.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp0x/pr60209-neg.C: Update diagnostic.
    	* g++.dg/diagnostic/string-literal-concat.C: Likewise.
    	* g++.dg/ext/utf-badconcat.C: Likewise.
    	* g++.dg/ext/utf-badconcat2.C: Likewise.

--- gcc/testsuite/g++.dg/diagnostic/string-literal-concat.C
+++ gcc/testsuite/g++.dg/diagnostic/string-literal-concat.C
@@ -1,12 +1,12 @@
 /* { dg-options "-fdiagnostics-show-caret -std=c++11" } */
 
-const void *s = u8"a"  u"b";  // { dg-error "24: non-standard concatenation" }
+const void *s = u8"a"  u"b";  // { dg-error "24: concatenation" }
 /* { dg-begin-multiline-output "" }
  const void *s = u8"a"  u"b";
                  ~~~~~  ^~~~
    { dg-end-multiline-output "" } */
 
-const void *s2 = u"a"  u"b"  u8"c";  // { dg-error "30: non-standard concatenation" }
+const void *s2 = u"a"  u"b"  u8"c";  // { dg-error "30: concatenation" }
 /* { dg-begin-multiline-output "" }
  const void *s2 = u"a"  u"b"  u8"c";
                         ~~~~  ^~~~~
@@ -16,7 +16,7 @@ const void *s2 = u"a"  u"b"  u8"c";  // { dg-error "30: non-standard concatenati
 
 const void *s3 = TEST_U8_LITERAL u8"b";
 
-const void *s4 = TEST_U8_LITERAL u"b"; // { dg-error "34: non-standard concatenation" }
+const void *s4 = TEST_U8_LITERAL u"b"; // { dg-error "34: concatenation" }
 /* { dg-begin-multiline-output "" }
  const void *s4 = TEST_U8_LITERAL u"b";
                                   ^~~~
