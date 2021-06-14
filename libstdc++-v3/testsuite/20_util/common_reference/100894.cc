0e54986854831a4c66b308f98aff949ca7d3ce84
Jonathan Wakely <jwakely@redhat.com>
2021-06-14 20:31:00 +0100

    libstdc++: Fix common_reference for non-reference results [PR100894]
    The result of COMMON-REF(A&, B&&) where they have no common reference
    type should be ill-formed. Our implementation fails to check that the
    COMMON-REF result is a reference, so is well-formed when it shouldn't
    be. This means that common_reference uses that result when it shouldn't
    do.
    
    The fix is to reject the result of COMMON-REF(A, B) if it's not a
    reference, so that common_reference falls through to the next case,
    which uses COND-RES, which yields the correct non-reference result.
    
    Signed-off-by: Jonathan Wakely <jwakely@redhat.com>
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/100894
    	* include/std/type_traits (__common_ref_impl<X&, Y&>): Only
    	use the type if it's a reference.
    	* testsuite/20_util/common_reference/100894.cc: New test.
    
    (cherry picked from commit c37b5ddcc88e0cc0f6a4ad609eda51021df0f6bb)

---
+++ libstdc++-v3/testsuite/20_util/common_reference/100894.cc
@@ -0,0 +1,9 @@
+// { dg-options "-std=gnu++20" }
+// { dg-do compile { target c++20 } }
+// PR libstdc++/100894 - common_reference implementation seems to be wrong
+
+#include <type_traits>
+
+struct A {};
+struct B { B(A); };
+static_assert( std::is_same_v<std::common_reference_t<A&, B&&>, B> );
