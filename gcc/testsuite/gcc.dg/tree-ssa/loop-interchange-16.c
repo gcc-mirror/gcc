e46ec6e243c704f0858d16af380a7d9c36fc4244
Richard Biener <rguenther@suse.de>
2021-06-23 09:59:28 +0200

    tree-optimization/101173 - fix interchange dependence checking
    This adjusts the loop interchange dependence checking to properly
    guard all dependence checks with DDR_REVERSED_P or its inverse.
    
    2021-07-07  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/101173
    	PR tree-optimization/101280
    	* gimple-loop-interchange.cc
    	(tree_loop_interchange::valid_data_dependences): Properly
    	guard all dependence checks with DDR_REVERSED_P or its
    	inverse.
    
    	* gcc.dg/torture/pr101173.c: New testcase.

---
+++ gcc/testsuite/gcc.dg/tree-ssa/loop-interchange-16.c
@@ -0,0 +1,22 @@
+/* PR/101280 */
+/* { dg-do compile } */
+/* { dg-options "-O3 -fdump-tree-linterchange-details" } */
+
+void dummy (double *, double *);
+#define LEN_2D 32
+double aa[LEN_2D][LEN_2D], bb[LEN_2D][LEN_2D];
+double s231(int iterations)
+{
+//    loop interchange
+//    loop with data dependency
+    for (int nl = 0; nl < 100*(iterations/LEN_2D); nl++) {
+        for (int i = 0; i < LEN_2D; ++i) {
+            for (int j = 1; j < LEN_2D; j++) {
+                aa[j][i] = aa[j - 1][i] + bb[j][i];
+            }
+        }
+        dummy(aa[0],bb[0]);
+    }
+}
+
+/* { dg-final { scan-tree-dump "loops interchanged" "linterchange" } } */
