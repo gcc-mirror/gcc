17bc3848e065c0980523e1a1592f2f03b24b4f1c
Jonathan Wakely <jwakely@redhat.com>
2021-06-24 12:56:20 +0100

    libstdc++: Implement LWG 2762 for std::unique_ptr::operator*
    The LWG issue proposes to add a conditional noexcept-specifier to
    std::unique_ptr's dereference operator. The issue is currently in
    Tentatively Ready status, but even if it isn't voted into the draft, we
    can do it as a conforming extensions. This commit also adds a similar
    noexcept-specifier to operator[] for the unique_ptr<T[], D> partial
    specialization.
    
    Also ensure that all dereference operators for shared_ptr are noexcept,
    and adds tests for the std::optional accessors modified by the issue,
    which were already noexcept in our implementation.
    
    Signed-off-by: Jonathan Wakely <jwakely@redhat.com>
    
    libstdc++-v3/ChangeLog:
    
    	* include/bits/shared_ptr_base.h (__shared_ptr_access::operator[]):
    	Add noexcept.
    	* include/bits/unique_ptr.h (unique_ptr::operator*): Add
    	conditional noexcept as per LWG 2762.
    	* testsuite/20_util/shared_ptr/observers/array.cc: Check that
    	dereferencing cannot throw.
    	* testsuite/20_util/shared_ptr/observers/get.cc: Likewise.
    	* testsuite/20_util/optional/observers/lwg2762.cc: New test.
    	* testsuite/20_util/unique_ptr/lwg2762.cc: New test.

--- libstdc++-v3/include/bits/unique_ptr.h
+++ libstdc++-v3/include/bits/unique_ptr.h
@@ -402,7 +402,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 
       /// Dereference the stored pointer.
       typename add_lvalue_reference<element_type>::type
-      operator*() const
+      operator*() const noexcept(noexcept(*std::declval<pointer>()))
       {
 	__glibcxx_assert(get() != pointer());
 	return *get();
@@ -655,6 +655,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       /// Access an element of owned array.
       typename std::add_lvalue_reference<element_type>::type
       operator[](size_t __i) const
+      noexcept(noexcept(std::declval<pointer>()[std::declval<size_t&>()]))
       {
 	__glibcxx_assert(get() != pointer());
 	return get()[__i];
