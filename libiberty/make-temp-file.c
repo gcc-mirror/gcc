13eaeffde5d53638664364a6107f4b6c605b5168
Gerald Pfeifer <gerald@pfeifer.com>
2021-06-29 00:39:15 +0200

    libiberty: No longer use /usr/tmp
    /usr/tmp is antiquated and not present on decently modern systems.
    Remove it from consideration when choosing a directory for temporary
    files.
    
    libiberty:
    
    2021-06-29  Gerald Pfeifer  <gerald@pfeifer.com>
    
    	* make-temp-file.c (usrtmp): Remove.
    	(choose_tmpdir): Remove use of usrtmp.

--- libiberty/make-temp-file.c
+++ libiberty/make-temp-file.c
@@ -81,8 +81,6 @@ try_dir (const char *dir, const char *base)
 }
 
 static const char tmp[] = { DIR_SEPARATOR, 't', 'm', 'p', 0 };
-static const char usrtmp[] =
-{ DIR_SEPARATOR, 'u', 's', 'r', DIR_SEPARATOR, 't', 'm', 'p', 0 };
 static const char vartmp[] =
 { DIR_SEPARATOR, 'v', 'a', 'r', DIR_SEPARATOR, 't', 'm', 'p', 0 };
 
@@ -129,9 +127,8 @@ choose_tmpdir (void)
 	base = try_dir (P_tmpdir, base);
 #endif
 
-      /* Try /var/tmp, /usr/tmp, then /tmp.  */
+      /* Try /var/tmp, then /tmp.  */
       base = try_dir (vartmp, base);
-      base = try_dir (usrtmp, base);
       base = try_dir (tmp, base);
       
       /* If all else fails, use the current directory!  */
