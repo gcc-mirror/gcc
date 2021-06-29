59897751aa797c9c6e7e1d795bbc1afbb9c81e32
Philip Herron <philip.herron@embecosm.com>
2021-06-29 21:24:47 +0100

    Revert change to ScanUnused to not scan Types
    The scan dead-code pass will take over this role, but lets keep both
    running for a while and then we can update ScanUnused to only scan for
    unused labels and variables. Its not possible to rely on name resolution
    to know what symbols are used or not used. Further resolution occurs during
    type resolution and the dead code pass runs after type resolution.

--- gcc/testsuite/rust/compile/torture/cfg_attr.rs
+++ gcc/testsuite/rust/compile/torture/cfg_attr.rs
@@ -3,6 +3,6 @@ use std::env; // Add one line so gccrs doesn't believe we're parsing a shebang
 #[cfg_attr(feature = "somefeature", attribute = "someattr")]
 struct Feature;
 // { dg-warning "struct is never constructed" "" { target *-*-* } .-1 }
+// { dg-warning "unused name" "" { target *-*-* } .-2 }
 
-fn main() {
-}
+fn main() {}
--- gcc/testsuite/rust/compile/torture/unused_struct.rs
+++ gcc/testsuite/rust/compile/torture/unused_struct.rs
@@ -1,8 +1,8 @@
 struct Foo {
-// { dg-warning "struct is never constructed" "" { target *-*-* } .-1 }
+    // { dg-warning "struct is never constructed" "" { target *-*-* } .-1 }
+    // { dg-warning "unused name" "" { target *-*-* } .-2 }
     one: i32,
-    two: i32
+    two: i32,
 }
 
-fn main() {
-}
+fn main() {}
