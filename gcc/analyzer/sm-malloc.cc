8d58bfb78c8dc6f5bdf7786b96f26329e0d36b80
David Malcolm <dmalcolm@redhat.com>
2021-07-02 15:19:43 -0400

    analyzer: fix missing leak after call to strsep [PR100615]
    PR analyzer/100615 reports a missing leak diagnostic.
    The issue is that the code calls strsep which the analyzer doesn't
    have special knowledge of, and so conservatively assumes that it
    could free the pointer, so drops malloc state for it.
    
    Properly "teaching" the analyzer about strsep would require it
    to support bifurcating state at a call, which is currently fiddly to
    do, so for now this patch notes that strsep doesn't affect the
    malloc state machine, allowing the analyzer to correctly detect the leak.
    
    gcc/analyzer/ChangeLog:
    	PR analyzer/100615
    	* sm-malloc.cc: Include "analyzer/function-set.h".
    	(malloc_state_machine::on_stmt): Call unaffected_by_call_p and
    	bail on the functions it recognizes.
    	(malloc_state_machine::unaffected_by_call_p): New.
    
    gcc/testsuite/ChangeLog:
    	PR analyzer/100615
    	* gcc.dg/analyzer/pr100615.c: New test.
    
    Signed-off-by: David Malcolm <dmalcolm@redhat.com>

--- gcc/analyzer/sm-malloc.cc
+++ gcc/analyzer/sm-malloc.cc
@@ -44,6 +44,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "analyzer/region-model.h"
 #include "stringpool.h"
 #include "attribs.h"
+#include "analyzer/function-set.h"
 
 #if ENABLE_ANALYZER
 
@@ -383,6 +384,8 @@ public:
 
   bool reset_when_passed_to_unknown_fn_p (state_t s,
 					  bool is_mutable) const FINAL OVERRIDE;
+
+  static bool unaffected_by_call_p (tree fndecl);
 
   standard_deallocator_set m_free;
   standard_deallocator_set m_scalar_delete;
@@ -1569,6 +1572,9 @@ malloc_state_machine::on_stmt (sm_context *sm_ctxt,
 	    return true;
 	  }
 
+	if (unaffected_by_call_p (callee_fndecl))
+	  return true;
+
 	/* Cast away const-ness for cache-like operations.  */
 	malloc_state_machine *mutable_this
 	  = const_cast <malloc_state_machine *> (this);
@@ -1923,6 +1929,28 @@ malloc_state_machine::reset_when_passed_to_unknown_fn_p (state_t s,
 
   /* Otherwise, pointers passed as non-const can be freed.  */
   return is_mutable;
+}
+
+/* Return true if calls to FNDECL are known to not affect this sm-state.  */
+
+bool
+malloc_state_machine::unaffected_by_call_p (tree fndecl)
+{
+  /* A set of functions that are known to not affect allocation
+     status, even if we haven't fully modelled the rest of their
+     behavior yet.  */
+  static const char * const funcnames[] = {
+    /* This array must be kept sorted.  */
+    "strsep",
+  };
+  const size_t count
+    = sizeof(funcnames) / sizeof (funcnames[0]);
+  function_set fs (funcnames, count);
+
+  if (fs.contains_decl_p (fndecl))
+    return true;
+
+  return false;
 }
 
 /* Shared logic for handling GIMPLE_ASSIGNs and GIMPLE_PHIs that
