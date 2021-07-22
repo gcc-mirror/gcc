8ed6cfbbee74ec9e03f2558b9c36f61dd7d4dcfd
Jonathan Wakely <jwakely@redhat.com>
2021-07-22 18:49:57 +0100

    libstdc++: Fix non-default constructors for hash containers [PR101583]
    When I added the new mixin to _Hashtable, I forgot to explicitly
    construct it in each non-default constructor. That means you can't
    use any constructors unless all three of the hash function, equality
    function, and allocator are all default constructible.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/101583
    	* include/bits/hashtable.h (_Hashtable): Replace mixin with
    	_Enable_default_ctor. Construct it explicitly in all
    	non-forwarding, non-defaulted constructors.
    	* testsuite/23_containers/unordered_map/cons/default.cc: Check
    	non-default constructors can be used.
    	* testsuite/23_containers/unordered_set/cons/default.cc:
    	Likewise.

--- libstdc++-v3/testsuite/23_containers/unordered_map/cons/default.cc
+++ libstdc++-v3/testsuite/23_containers/unordered_map/cons/default.cc
@@ -31,3 +31,18 @@ static_assert( ! std::is_default_constructible<Map2>{}, "PR libstdc++/100863" );
 struct Equal : std::equal_to<int> { Equal(int) { } };
 using Map3 = std::unordered_map<int, int, std::hash<int>, Equal>;
 static_assert( ! std::is_default_constructible<Map3>{}, "PR libstdc++/100863" );
+
+// PR libstdc++/101583
+// verify non-default ctors can still be used
+using Map4 = std::unordered_map<int, int, Hash, Equal,
+			        NoDefaultConsAlloc<std::pair<const int, int>>>;
+Hash h(1);
+Equal eq(1);
+Map4::allocator_type a(1);
+Map4 m{1, h, eq, a};
+Map4 m2{m.begin(), m.end(), m.size(), h, eq, a};
+Map4 m3{{{1,1}, {2,2}, {3,3}}, 3, h, eq, a};
+Map4 m4{m};
+Map4 m5{m, a};
+Map4 m6{std::move(m)};
+Map4 m7{std::move(m6), a};
