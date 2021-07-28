8cd27a3b25558e5be7f8595fc1c828bc46641671
Jonathan Wright <jonathan.wright@arm.com>
2021-07-28 15:49:29 +0100

    aarch64: Don't include vec_select high-half in SIMD add cost
    The Neon add-long/add-widen instructions can select the top or bottom
    half of the operand registers. This selection does not change the
    cost of the underlying instruction and this should be reflected by
    the RTL cost function.
    
    This patch adds RTL tree traversal in the Neon add cost function to
    match vec_select high-half of its operands. This traversal prevents
    the cost of the vec_select from being added into the cost of the
    subtract - meaning that these instructions can now be emitted in the
    combine pass as they are no longer deemed prohibitively expensive.
    
    gcc/ChangeLog:
    
    2021-07-28  Jonathan Wright  <jonathan.wright@arm.com>
    
    	* config/aarch64/aarch64.c: Traverse RTL tree to prevent cost
    	of vec_select high-half from being added into Neon add cost.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/aarch64/vaddX_high_cost.c: New test.

--- gcc/config/aarch64/aarch64.c
+++ gcc/config/aarch64/aarch64.c
@@ -13161,6 +13161,21 @@ cost_minus:
 	op1 = XEXP (x, 1);
 
 cost_plus:
+	if (VECTOR_MODE_P (mode))
+	  {
+	    /* ADDL2 and ADDW2.  */
+	    unsigned int vec_flags = aarch64_classify_vector_mode (mode);
+	    if (vec_flags & VEC_ADVSIMD)
+	      {
+		/* The select-operand-high-half versions of the add instruction
+		   have the same cost as the regular three vector version -
+		   don't add the costs of the select into the costs of the add.
+		   */
+		op0 = aarch64_strip_extend_vec_half (op0);
+		op1 = aarch64_strip_extend_vec_half (op1);
+	      }
+	  }
+
 	if (GET_RTX_CLASS (GET_CODE (op0)) == RTX_COMPARE
 	    || GET_RTX_CLASS (GET_CODE (op0)) == RTX_COMM_COMPARE)
 	  {
