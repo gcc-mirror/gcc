d55d3f5b04e81b79f34ccf23f7e2c1e736ad3b0d
Martin Jambor <mjambor@suse.cz>
2021-08-09 17:35:39 +0200

    ipa: Fix testsuite/gcc.dg/ipa/remref-6.c
    I forgot to add -fdump-ipa-inline to options of
    testsuite/gcc.dg/ipa/remref-6.c and so the dump scan test were not
    PASSing but ended up as UNRESOLVED.  Fixing that revealed that the one
    of the dumps it was looking for had a double space, so I removed it
    too.
    
    gcc/ChangeLog:
    
    2021-08-09  Martin Jambor  <mjambor@suse.cz>
    
    	PR testsuite/101654
    	* ipa-prop.c (propagate_controlled_uses): Removed a spurious space.
    
    gcc/testsuite/ChangeLog:
    
    2021-08-09  Martin Jambor  <mjambor@suse.cz>
    
    	PR testsuite/101654
    	* gcc.dg/ipa/remref-6.c: Added missing -fdump-ipa-inline option.

--- gcc/testsuite/gcc.dg/ipa/remref-6.c
+++ gcc/testsuite/gcc.dg/ipa/remref-6.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -fno-early-inlining -fno-ipa-cp -fdump-tree-optimized"  }  */
+/* { dg-options "-O2 -fno-early-inlining -fno-ipa-cp -fdump-ipa-inline -fdump-tree-optimized"  }  */
 
 static double global = 0.0;
 
