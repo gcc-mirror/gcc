01f8a8b48e50cbaa68b878d9f8a330b8c0736bed
Jakub Jelinek <jakub@redhat.com>
2021-08-12 09:26:27 +0200

    openmp: Diagnose syntax mismatches between declare target and end declare target
    OpenMP 5.1 says:
    For any directive that has a paired end directive, including those with a begin
    and end pair, both directives must use either the attribute syntax or the
    pragma syntax.
    
    The following patch enforces it with the only pair so far recognized in C++
    (Fortran has many, but on the other side doesn't have attribute syntax).
    
    While I initially wanted to use vec<bool, va_gc> *member; in there, that
    unfortunately doesn't work, one gets linker errors and I guess it is fixable,
    but for begin declare target we'll need a struct anyway to store device_type
    etc.
    
    2021-08-12  Jakub Jelinek  <jakub@redhat.com>
    
    	* cp-tree.h (omp_declare_target_attr): New type.
    	(struct saved_scope): Change type of omp_declare_target_attribute
    	from int to vec<omp_declare_target_attr, va_gc> * and move it.
    	* parser.c (cp_parser_omp_declare_target): Instead of
    	incrementing scope_chain->omp_declare_target_attribute, push
    	a struct containing parser->lexer->in_omp_attribute_pragma to
    	the vector.
    	(cp_parser_omp_end_declare_target): Instead of decrementing
    	scope_chain->omp_declare_target_attribute, pop a structure
    	from it.  Diagnose mismatching declare target vs.
    	end declare target syntax.
    	* semantics.c (finish_translation_unit): Use vec_safe_length
    	and vec_safe_truncate on scope_chain->omp_declare_target_attributes.
    	* decl2.c (cplus_decl_attributes): Use vec_safe_length
    	on scope_chain->omp_declare_target_attributes.
    
    	* g++.dg/gomp/attrs-12.C: New test.

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -1789,6 +1789,10 @@ union GTY((desc ("cp_tree_node_structure (&%h)"),
 };
 
 
+struct GTY(()) omp_declare_target_attr {
+  bool attr_syntax;
+};
+
 /* Global state.  */
 
 struct GTY(()) saved_scope {
@@ -1826,9 +1830,6 @@ struct GTY(()) saved_scope {
   int unevaluated_operand;
   int inhibit_evaluation_warnings;
   int noexcept_operand;
-  /* If non-zero, implicit "omp declare target" attribute is added into the
-     attribute lists.  */
-  int omp_declare_target_attribute;
   int ref_temp_count;
 
   struct stmt_tree_s x_stmt_tree;
@@ -1837,6 +1838,7 @@ struct GTY(()) saved_scope {
   cp_binding_level *bindings;
 
   hash_map<tree, tree> *GTY((skip)) x_local_specializations;
+  vec<omp_declare_target_attr, va_gc> *omp_declare_target_attribute;
 
   struct saved_scope *prev;
 };
