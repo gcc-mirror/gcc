9fcc3a1dd2372deea8856c55d25337b06e201203
Thomas Schwinge <thomas@codesourcery.com>
2021-08-24 11:14:10 +0200

    Host and offload targets have no common meaning of address spaces
    	gcc/
    	* tree-streamer-out.c (pack_ts_base_value_fields): Don't pack
    	'TYPE_ADDR_SPACE' for offloading.
    	* tree-streamer-in.c (unpack_ts_base_value_fields): Don't unpack
    	'TYPE_ADDR_SPACE' for offloading.
    	libgomp/
    	* testsuite/libgomp.c/address-space-1.c: Remove 'dg-xfail-run-if'
    	for 'offload_device_intel_mic'.

--- gcc/tree-streamer-in.c
+++ gcc/tree-streamer-in.c
@@ -146,7 +146,9 @@ unpack_ts_base_value_fields (struct bitpack_d *bp, tree expr)
 	TYPE_REVERSE_STORAGE_ORDER (expr) = (unsigned) bp_unpack_value (bp, 1);
       else
 	TYPE_SATURATING (expr) = (unsigned) bp_unpack_value (bp, 1);
+#ifndef ACCEL_COMPILER
       TYPE_ADDR_SPACE (expr) = (unsigned) bp_unpack_value (bp, 8);
+#endif
     }
   else if (TREE_CODE (expr) == BIT_FIELD_REF || TREE_CODE (expr) == MEM_REF)
     {
