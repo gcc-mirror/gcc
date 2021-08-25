29c77454e5ab33ce06a741eacdfbd5348fbccc95
Richard Biener <rguenther@suse.de>
2021-08-25 10:06:01 +0200

    tree-optimization/102046 - fix SLP build from scalars with patterns
    When we swap operands for SLP builds we lose track where exactly
    pattern defs are - but we fail to update the any_pattern member
    of the operands info.  Do so conservatively.
    
    2021-08-25  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/102046
    	* tree-vect-slp.c (vect_build_slp_tree_2): Conservatively
    	update ->any_pattern when swapping operands.
    
    	* gcc.dg/vect/pr102046.c: New testcase.

--- gcc/tree-vect-slp.c
+++ gcc/tree-vect-slp.c
@@ -2311,6 +2311,10 @@ out:
 	      }
 	  if (dump_enabled_p ())
 	    dump_printf (MSG_NOTE, "\n");
+	  /* After swapping some operands we lost track whether an
+	     operand has any pattern defs so be conservative here.  */
+	  if (oprnds_info[0]->any_pattern || oprnds_info[1]->any_pattern)
+	    oprnds_info[0]->any_pattern = oprnds_info[1]->any_pattern = true;
 	  /* And try again with scratch 'matches' ... */
 	  bool *tem = XALLOCAVEC (bool, group_size);
 	  if ((child = vect_build_slp_tree (vinfo, oprnd_info->def_stmts,
