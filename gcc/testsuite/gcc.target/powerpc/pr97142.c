68d525ee859041b21d87b23030d1e829a9cc3b6f
Xionghu Luo <luoxhu@linux.ibm.com>
2021-09-06 20:22:50 -0500

    rs6000: Expand fmod and remainder when built with fast-math [PR97142]
    fmod/fmodf and remainder/remainderf could be expanded instead of library
    call when fast-math build, which is much faster.
    
    fmodf:
         fdivs   f0,f1,f2
         friz    f0,f0
         fnmsubs f1,f2,f0,f1
    
    remainderf:
         fdivs   f0,f1,f2
         frin    f0,f0
         fnmsubs f1,f2,f0,f1
    
    SPEC2017 Ofast P8LE: 511.povray_r +1.14%,  526.blender_r +1.72%
    
    gcc/ChangeLog:
    
    2021-09-07  Xionghu Luo  <luoxhu@linux.ibm.com>
    
    	PR target/97142
    	* config/rs6000/rs6000.md (fmod<mode>3): New define_expand.
    	(remainder<mode>3): Likewise.
    
    gcc/testsuite/ChangeLog:
    
    2021-09-07  Xionghu Luo  <luoxhu@linux.ibm.com>
    
    	PR target/97142
    	* gcc.target/powerpc/pr97142.c: New test.
    
    (cherry picked from commit 546ecb0054af302acf0839c7f3eb78598f8c0672)

---
+++ gcc/testsuite/gcc.target/powerpc/pr97142.c
@@ -0,0 +1,35 @@
+/* { dg-do compile } */
+/* { dg-options "-Ofast" } */
+
+#include <math.h>
+
+float test1 (float x, float y)
+{
+  return fmodf (x, y);
+}
+
+double test2 (double x, double y)
+{
+  return fmod (x, y);
+}
+
+float test3 (float x, float y)
+{
+  return remainderf (x, y);
+}
+
+double test4 (double x, double y)
+{
+  return remainder (x, y);
+}
+
+/* { dg-final { scan-assembler-not {(?n)\mb.*fmod} } } */
+/* { dg-final { scan-assembler-not {(?n)\mb.*fmodf} } } */
+/* { dg-final { scan-assembler-not {(?n)\mb.*remainder} } } */
+/* { dg-final { scan-assembler-not {(?n)\mb.*remainderf} } } */
+/* { dg-final { scan-assembler-times {\mfdiv\M} 2 } } */
+/* { dg-final { scan-assembler-times {\mfdivs\M} 2 } } */
+/* { dg-final { scan-assembler-times {\mfnmsub\M} 2 } } */
+/* { dg-final { scan-assembler-times {\mfnmsubs\M} 2 } } */
+/* { dg-final { scan-assembler-times {\mfriz\M} 2 } } */
+/* { dg-final { scan-assembler-times {\mfrin\M} 2 } } */
