640bda34f0886fd30396981780557096b4c4d6ed
Claire Dross <dross@adacore.com>
2021-09-10 12:05:27 +0200

    [Ada] Disable contract cases on formal containers
    gcc/ada/
    
    	* libgnat/a-cfdlli.ads: Use pragma Assertion_Policy to disable
    	contract cases at execution.
    	* libgnat/a-cfinve.ads: Idem.
    	* libgnat/a-cofove.ads: Idem.
    	* libgnat/a-cfhase.ads: Idem.
    	* libgnat/a-cfhama.ads: Idem.
    	* libgnat/a-cforse.ads: Idem.
    	* libgnat/a-cforma.ads: Idem.

--- gcc/ada/libgnat/a-cfhama.ads
+++ gcc/ada/libgnat/a-cfhama.ads
@@ -69,6 +69,7 @@ is
 
    pragma Assertion_Policy (Pre => Ignore);
    pragma Assertion_Policy (Post => Ignore);
+   pragma Assertion_Policy (Contract_Cases => Ignore);
    pragma Annotate (CodePeer, Skip_Analysis);
 
    type Map (Capacity : Count_Type; Modulus : Hash_Type) is private with
