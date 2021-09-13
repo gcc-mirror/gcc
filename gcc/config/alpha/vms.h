f42e95a830ab48e59389065ce79a013a519646f1
Jan-Benedict Glaw <jbglaw@ug-owl.de>
2021-09-13 12:08:25 +0200

    Fix multi-statment macro
    INIT_CUMULATIVE_ARGS() expands to multiple statements, which will break right
    after an `if` statement. Wrap it into a block.
    
    gcc/ChangeLog:
    
    	* config/alpha/vms.h (INIT_CUMULATIVE_ARGS): Wrap multi-statment
    	define into a block.

--- gcc/config/alpha/vms.h
+++ gcc/config/alpha/vms.h
@@ -145,9 +145,13 @@ typedef struct {int num_args; enum avms_arg_type atypes[6];} avms_arg_info;
 
 #undef INIT_CUMULATIVE_ARGS
 #define INIT_CUMULATIVE_ARGS(CUM, FNTYPE, LIBNAME, INDIRECT, N_NAMED_ARGS) \
-  (CUM).num_args = 0;						\
-  (CUM).atypes[0] = (CUM).atypes[1] = (CUM).atypes[2] = I64;	\
-  (CUM).atypes[3] = (CUM).atypes[4] = (CUM).atypes[5] = I64;
+  do                                                                       \
+    {                                                                      \
+      (CUM).num_args = 0;                                                  \
+      (CUM).atypes[0] = (CUM).atypes[1] = (CUM).atypes[2] = I64;           \
+      (CUM).atypes[3] = (CUM).atypes[4] = (CUM).atypes[5] = I64;           \
+    }                                                                      \
+  while (0)
 
 #define DEFAULT_PCC_STRUCT_RETURN 0
 
