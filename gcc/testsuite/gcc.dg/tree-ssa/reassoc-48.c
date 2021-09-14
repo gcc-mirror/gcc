92cdd338fdbfd33f72e645af1caaa1a357fe9839
Ilya Leoshkevich <iii@linux.ibm.com>
2021-09-14 21:31:31 +0200

    reassoc: Test rank biasing
    Add both positive and negative tests.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/tree-ssa/reassoc-46.c: New test.
    	* gcc.dg/tree-ssa/reassoc-46.h: Common code for new tests.
    	* gcc.dg/tree-ssa/reassoc-47.c: New test.
    	* gcc.dg/tree-ssa/reassoc-48.c: New test.
    	* gcc.dg/tree-ssa/reassoc-49.c: New test.
    	* gcc.dg/tree-ssa/reassoc-50.c: New test.
    	* gcc.dg/tree-ssa/reassoc-51.c: New test.

---
+++ gcc/testsuite/gcc.dg/tree-ssa/reassoc-48.c
@@ -0,0 +1,9 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized -ftree-vectorize" } */
+
+#define STORE
+#include "reassoc-46.h"
+
+/* Check that if the loop accumulator is modified using a chain of operations
+   other than addition, its new value is still added last.  */
+/* { dg-final { scan-tree-dump-times {(?:vect_)?sum_[\d._]+ = (?:(?:vect_)?_[\d._]+ \+ (?:vect_)?sum_[\d._]+|(?:vect_)?sum_[\d._]+ \+ (?:vect_)?_[\d._]+)} 1 "optimized" } } */
