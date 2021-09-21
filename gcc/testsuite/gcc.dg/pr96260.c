6e6bf4cd21af39a7923bae007517ab43a4c3b36a
Eric Botcazou <ebotcazou@adacore.com>
2021-09-21 09:25:47 +0200

    Fix no_fsanitize_address effective target
    The implementation of the no_fsanitize_address effective target was copied
    from asan-dg.exp without realizing that it does not work outside of this
    context (there is a comment explaining why).  As a consequence, it always
    returns 0, so for example the directive in gnat.dg/asan1.adb:
    
    { dg-skip-if "no address sanitizer" { no_fsanitize_address } }
    
    does not work.  This led some people to add the nonsensical:
    
    { dg-require-effective-target no_fsanitize_address }
    
    to sanitizer tests, e.g. g++.dg/warn/uninit-pr93100.C, thus disabling them
    everywhere instead of just for the problematic targets.
    
    gcc/testsuite/
    	* lib/target-supports.exp (no_fsanitize_address): Add missing bits.
    	* gcc.dg/uninit-pr93100.c: Skip if no_fsanitize_address.
    	* gcc.dg/pr91441.c: Likewise.
    	* gcc.dg/pr96260.c: Likewise.
    	* gcc.dg/pr96307.c: Likewise.
    	* g++.dg/warn/uninit-pr93100.C: Likewise.
    	* gnat.dg/asan1.adb: Likewise.
    
    	* gcc.dg/Wstringop-overflow-70.c: Adjust for SPARC.
    	* g++.dg/abi/anon4.C: Likewise.

--- gcc/testsuite/gcc.dg/pr96260.c
+++ gcc/testsuite/gcc.dg/pr96260.c
@@ -1,7 +1,7 @@
 /* PR target/96260 */
 /* { dg-do compile } */
-/* { dg-require-effective-target no_fsanitize_address }*/
 /* { dg-options "--param asan-stack=1 -fsanitize=kernel-address -fasan-shadow-offset=0x100000" } */
+/* { dg-skip-if "no address sanitizer" { no_fsanitize_address } } */
 
 int *bar(int *);
 int *f( int a)
