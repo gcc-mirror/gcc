a6ff720460fa38436476045cb7ac2881d1e9d0b5
Jakub Jelinek <jakub@redhat.com>
2021-09-23 09:36:01 +0200

    c++: Fix up parsing of attributes for using-directive
    As I've said earlier and added xfails in gen-attrs-76.C test,
    https://eel.is/c++draft/namespace.udir#nt:using-directive
    has attribute-specifier-seq[opt] at the start, not at the end before ;
    as gcc is expecting.
    IMHO we should continue parsing at the end the GNU attributes
    because using namespace N __attribute__((strong));, while not supported
    anymore, used to be supported in the past, but my code searches for
    using namespace N [[gnu::strong]]; didn't reveal anything at all.
    
    2021-08-12  Jakub Jelinek  <jakub@redhat.com>
    
    	* parser.c (cp_parser_block_declaration): Call
    	cp_parser_using_directive for C++11 attributes followed by
    	using namespace tokens.
    	(cp_parser_using_directive): Parse C++11 attributes at the start
    	of the directive rather than at the end, only parse GNU attributes
    	at the end.
    
    	* g++.dg/lookup/strong-using.C: Add test using [[gnu::strong]]
    	as well.
    	* g++.dg/lookup/strong-using2.C: Likewise.
    	* g++.dg/cpp0x/gen-attrs-58.C: Move alignas(int) before
    	using namespace.
    	* g++.dg/cpp0x/gen-attrs-59.C: Move alignas(X) before
    	using namespace, add tests for alignas before semicolon.
    	* g++.dg/cpp0x/gen-attrs-76.C: Remove xfails.  Add test for
    	C++11 attributes on using directive before semicolon.
    
    (cherry picked from commit 3890c28ac5bd03ba334a20fbf9518a37dcdbfe5d)

--- gcc/testsuite/g++.dg/cpp0x/gen-attrs-76.C
+++ gcc/testsuite/g++.dg/cpp0x/gen-attrs-76.C
@@ -3,6 +3,7 @@
 
 namespace N {}
 namespace O { typedef int T; };
+namespace P {}
 
 void
 foo ()
@@ -11,7 +12,8 @@ foo ()
   [[]] __extension__ asm ("");			// { dg-error "expected" }
   __extension__ [[]] asm ("");			// { dg-error "expected" }
   [[]] namespace M = ::N;			// { dg-error "expected" }
-  [[]] using namespace N;			// { dg-bogus "expected" "" { xfail *-*-* } }
+  [[]] using namespace N;			// { dg-bogus "expected" }
+  using namespace P [[]];			// { dg-error "expected" }
   [[]] using O::T;				// { dg-error "expected" }
   [[]] __label__ foo;				// { dg-error "expected" }
   [[]] static_assert (true, "");		// { dg-error "expected" }
@@ -24,7 +26,8 @@ bar ()
   [[gnu::unused]] __extension__ asm ("");	// { dg-error "expected" }
   __extension__ [[gnu::unused]] asm ("");	// { dg-error "expected" }
   [[gnu::unused]] namespace M = ::N;		// { dg-error "expected" }
-  [[gnu::unused]] using namespace N;		// { dg-bogus "expected" "" { xfail *-*-* } }
+  [[gnu::unused]] using namespace N;		// { dg-bogus "expected" }
+  using namespace P [[gnu::unused]];		// { dg-error "expected" }
   [[gnu::unused]] using O::T;			// { dg-error "expected" }
   [[gnu::unused]] __label__ foo;		// { dg-error "expected" }
   [[gnu::unused]] static_assert (true, "");	// { dg-error "expected" }
--- gcc/testsuite/g++.dg/lookup/strong-using2.C
+++ gcc/testsuite/g++.dg/lookup/strong-using2.C
@@ -9,3 +9,12 @@ namespace A
 
   using namespace B __attribute__ ((strong)); // { dg-bogus "no longer supported" }
 }
+
+namespace C
+{
+  namespace D // { dg-bogus "inline namespace" }
+  {
+  }
+
+  [[gnu::strong]] using namespace D; // { dg-bogus "no longer supported" }
+}
