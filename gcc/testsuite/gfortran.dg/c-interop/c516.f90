1d9aaea802f9ccd6be1a8c7ff57679e75a2ab7c3
Sandra Loosemore <sandra@codesourcery.com>
2021-09-24 10:50:54 -0700

    Fortran: Diagnose default-initialized pointer/allocatable dummies
    TS29113 changed what was then C516 in the 2010 Fortran standard (now
    C1557 in F2018) from disallowing all of pointer, allocatable, and
    optional attributes on dummy arguments to BIND(C) functions, to
    disallowing only pointer/allocatable with default-initialization.
    gfortran was previously failing to diagnose violations of this
    constraint.
    
    2021-09-23  Sandra Loosemore  <sandra@codesourcery.com>
    
    	PR fortran/101320
    
    gcc/fortran/
    	* decl.c (gfc_verify_c_interop_param): Handle F2018 C1557,
    	aka TS29113 C516.
    
    gcc/testsuite/
    	* gfortran.dg/c-interop/c516.f90: Remove xfails.  Add more
    	tests.
    
    (cherry picked from commit 2646d0e06b170569be1da28fce1d6e2f03a15f60)

--- gcc/testsuite/gfortran.dg/c-interop/c516.f90
+++ gcc/testsuite/gfortran.dg/c-interop/c516.f90
@@ -27,6 +27,10 @@ module m2
 
   interface
 
+    ! First test versions with optional attributes on the argument.
+    ! TS29113 removed the constraint disallowing optional arguments
+    ! that previously used to be in C516.
+
     ! good, no default initialization, no pointer/allocatable attribute
     subroutine s1a (x) bind (c)
       use m1
@@ -52,15 +56,53 @@ module m2
     end subroutine
 
     ! bad, default initialization + allocatable
-    subroutine s2b (x) bind (c)  ! { dg-error "BIND\\(C\\)" "pr101320" { xfail *-*-* } }
+    subroutine s2b (x) bind (c)  ! { dg-error "BIND\\(C\\)" }
       use m1
       type(t2), allocatable, optional :: x
     end subroutine
 
     ! bad, default initialization + pointer
-    subroutine s2c (x) bind (c)  ! { dg-error "BIND\\(C\\)" "pr101320" { xfail *-*-* } }
+    subroutine s2c (x) bind (c)  ! { dg-error "BIND\\(C\\)" }
       use m1
       type(t2), pointer, optional :: x
+    end subroutine
+
+    ! Now do all the same tests without the optional attribute.
+
+    ! good, no default initialization, no pointer/allocatable attribute
+    subroutine s3a (x) bind (c)
+      use m1
+      type(t1) :: x
+    end subroutine
+
+    ! good, no default initialization
+    subroutine s3b (x) bind (c)
+      use m1
+      type(t1), allocatable :: x
+    end subroutine
+
+    ! good, no default initialization
+    subroutine s3c (x) bind (c)
+      use m1
+      type(t1), pointer :: x
+    end subroutine
+
+    ! good, default initialization but no pointer/allocatable attribute
+    subroutine s4a (x) bind (c)
+      use m1
+      type(t2) :: x
+    end subroutine
+
+    ! bad, default initialization + allocatable
+    subroutine s4b (x) bind (c)  ! { dg-error "BIND\\(C\\)" }
+      use m1
+      type(t2), allocatable :: x
+    end subroutine
+
+    ! bad, default initialization + pointer
+    subroutine s4c (x) bind (c)  ! { dg-error "BIND\\(C\\)" }
+      use m1
+      type(t2), pointer :: x
     end subroutine
 
   end interface
