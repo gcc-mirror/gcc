ce450af5087b95001b003184b8ecc2c9bbf65378
Tobias Burnus <tobias@codesourcery.com>
2021-09-28 09:49:12 +0200

    gfortran.dg/include_15.f90: Add dg-prune-output [PR102500]
    gcc/testsuite/
    	PR fortran/102500
    	* gfortran.dg/include_15.f90: Add 'dg-prune-output' to prune
    	-Wmissing-include-dirs output printed or not depending on
    	how the testsuite is run.

--- gcc/testsuite/gfortran.dg/include_15.f90
+++ gcc/testsuite/gfortran.dg/include_15.f90
@@ -4,3 +4,6 @@ end
 ! { dg-warning " /fdaf/: No such file or directory" "" { target *-*-* } 0 }
 ! { dg-warning " bar: No such file or directory" "" { target *-*-* } 0 }
 ! { dg-warning " foo/bar: No such file or directory" "" { target *-*-* } 0 }
+
+! Depending how the testsuite is run, it may or may not print the following warning:
+! { dg-prune-output "Warning: finclude: No such file or directory" }
