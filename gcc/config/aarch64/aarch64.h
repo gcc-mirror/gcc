f0688d42c9b74a6999548ff2e79ae440b049b87f
Przemyslaw Wirkus <przemyslaw.wirkus@arm.com>
2021-10-01 10:06:45 +0100

    aarch64: add armv9-a to -march
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64-arches.def (AARCH64_ARCH): Added
    	armv9-a.
    	* config/aarch64/aarch64.h (AARCH64_FL_V9): New.
    	(AARCH64_FL_FOR_ARCH9): New flags for Armv9-A.
    	(AARCH64_ISA_V9): New ISA flag.
    	* doc/invoke.texi: Update docs.

--- gcc/config/aarch64/aarch64.h
+++ gcc/config/aarch64/aarch64.h
@@ -230,6 +230,8 @@ extern unsigned aarch64_architecture_version;
 
 /* Pointer Authentication (PAUTH) extension.  */
 #define AARCH64_FL_PAUTH      (1ULL << 40)
+/* Armv9.0-A.  */
+#define AARCH64_FL_V9         (1ULL << 41)  /* Armv9.0-A Architecture.  */
 
 /* 64-byte atomic load/store extensions.  */
 #define AARCH64_FL_LS64      (1ULL << 41)
@@ -266,6 +268,8 @@ extern unsigned aarch64_architecture_version;
 
 #define AARCH64_FL_FOR_ARCH8_R     \
   (AARCH64_FL_FOR_ARCH8_4 | AARCH64_FL_V8_R)
+#define AARCH64_FL_FOR_ARCH9       \
+  (AARCH64_FL_FOR_ARCH8_5 | AARCH64_FL_SVE | AARCH64_FL_SVE2 | AARCH64_FL_V9)
 
 /* Macros to test ISA flags.  */
 
@@ -304,6 +308,7 @@ extern unsigned aarch64_architecture_version;
 #define AARCH64_ISA_SB		   (aarch64_isa_flags & AARCH64_FL_SB)
 #define AARCH64_ISA_V8_R	   (aarch64_isa_flags & AARCH64_FL_V8_R)
 #define AARCH64_ISA_PAUTH	   (aarch64_isa_flags & AARCH64_FL_PAUTH)
+#define AARCH64_ISA_V9		   (aarch64_isa_flags & AARCH64_FL_V9)
 
 /* Crypto is an optional extension to AdvSIMD.  */
 #define TARGET_CRYPTO (TARGET_SIMD && AARCH64_ISA_CRYPTO)
