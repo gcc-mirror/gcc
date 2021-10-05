313193edfc3986c40dedce3d0b41455d0bcdbe43
Jonathan Wakely <jwakely@redhat.com>
2021-10-05 14:45:11 +0100

    libstdc++: Improve test for printing volatile pointers
    libstdc++-v3/ChangeLog:
    
    	* testsuite/27_io/basic_ostream/inserters_other/char/volatile_ptr.cc:
    	Check result matches non-volatile pointer.

--- libstdc++-v3/testsuite/27_io/basic_ostream/inserters_other/char/volatile_ptr.cc
+++ libstdc++-v3/testsuite/27_io/basic_ostream/inserters_other/char/volatile_ptr.cc
@@ -1,11 +1,15 @@
 // { dg-options "-std=gnu++23 -fno-inline" }
-// { dg-do link { target c++23 } }
+// { dg-do run { target c++23 } }
 
-#include <iostream>
+#include <sstream>
+#include <testsuite_hooks.h>
 
 int main()
 {
   int i = 0;
-  volatile void* p = &i;
-  std::cout << p << std::endl;
+  volatile void* vp = &i;
+  std::ostringstream s1, s2;
+  s1 << &i;
+  s2 << vp;
+  VERIFY( s1.str() == s2.str() );
 }
