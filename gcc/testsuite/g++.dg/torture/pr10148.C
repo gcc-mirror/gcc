09a0affdb0598a54835ac4bb0dd6b54122c12916
Richard Biener <rguenther@suse.de>
2021-10-11 16:06:03 +0200

    middle-end/101480 - overloaded global new/delete
    The following fixes the issue of ignoring side-effects on memory
    from overloaded global new/delete operators by not marking them
    as effectively 'const' apart from other explicitely specified
    side-effects.
    
    This will cause
    
    FAIL: g++.dg/warn/Warray-bounds-16.C  -std=gnu++1? (test for excess errors)
    
    because we now no longer statically see the initialization loop
    never executes because the call to operator new can now clobber 'a.m'.
    This seems to be an issue with the warning code and/or ranger so
    I'm leaving this FAIL to be addressed as followup.
    
    2021-10-11  Richard Biener  <rguenther@suse.de>
    
    	PR middle-end/101480
    	* gimple.c (gimple_call_fnspec): Do not mark operator new/delete
    	as const.
    
    	* g++.dg/torture/pr10148.C: New testcase.

---
+++ gcc/testsuite/g++.dg/torture/pr10148.C
@@ -0,0 +1,52 @@
+/* { dg-do run } */
+
+#include <stdlib.h>
+#include <assert.h>
+
+static bool flag = false;
+
+class C
+{
+  bool prev;
+
+public:
+  C() : prev(flag)
+  {
+    flag = true;
+  }
+
+  ~C() {
+    flag = prev;
+  }
+};
+
+void* operator new(size_t size)
+{
+  assert(flag);
+  return malloc(size);
+}
+
+void operator delete(void *p)
+{
+  free(p);
+}
+
+void g(int* p)
+{
+  delete p;
+}
+
+void f()
+{
+  int* p;
+  {
+    C c;
+    p = new int;
+  }
+  g(p);
+}
+
+int main(int, char**)
+{
+  f();
+}
