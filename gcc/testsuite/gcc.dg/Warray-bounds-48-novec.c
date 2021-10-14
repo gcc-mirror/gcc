3c8d8c0be95e99dc0cba7f6fad2429243582119f
liuhongt <hongtao.liu@intel.com>
2021-10-14 09:31:03 +0800

    Adjust testcase for O2 vectorization.
    As discussed in [1], this patch add xfail/target selector to those
    testcases, also make a copy of them so that they can be tested w/o
    vectorization.
    
    Newly added xfail/target selectors are used to check the vectorization
    capability of continuous byte/double bytes storage, these scenarios
    are exactly the part of the testcases that regressed after O2
    vectorization.
    
    [1] https://gcc.gnu.org/pipermail/gcc-patches/2021-October/581456.html.
    
    2021-10-19  Hongtao Liu  <hongtao.liu@intel.com>
    	    Kewen Lin  <linkw@linux.ibm.com>
    
    gcc/ChangeLog
    
    	* doc/sourcebuild.texi (Effective-Target Keywords): Document
    	vect_slp_v2qi_store, vect_slp_v4qi_store, vect_slp_v8qi_store,
    	vect_slp_v16qi_store, vect_slp_v2hi_store,
    	vect_slp_v4hi_store, vect_slp_v2si_store, vect_slp_v4si_store.
    
    gcc/testsuite/ChangeLog
    
    	PR middle-end/102722
    	PR middle-end/102697
    	PR middle-end/102462
    	PR middle-end/102706
    	PR middle-end/102744
    	* c-c++-common/Wstringop-overflow-2.c: Adjust testcase with new
    	xfail/target selector.
    	* gcc.dg/Warray-bounds-51.c: Ditto.
    	* gcc.dg/Warray-parameter-3.c: Ditto.
    	* gcc.dg/Wstringop-overflow-14.c: Ditto.
    	* gcc.dg/Wstringop-overflow-21.c: Ditto.
    	* gcc.dg/Wstringop-overflow-68.c: Ditto.
    	* gcc.dg/Wstringop-overflow-76.c: Ditto.
    	* gcc.dg/Warray-bounds-48.c: Ditto.
    	* gcc.dg/Wzero-length-array-bounds-2.c: Ditto.
    	* lib/target-supports.exp (check_vect_slp_aligned_store_usage):
    	New function.
    	(check_effective_target_vect_slp_v2qi_store): Ditto.
    	(check_effective_target_vect_slp_v4qi_store): Ditto.
    	(check_effective_target_vect_slp_v8qi_store): Ditto.
    	(check_effective_target_vect_slp_v16qi_store): Ditto.
    	(check_effective_target_vect_slp_v2hi_store): Ditto.
    	(check_effective_target_vect_slp_v4hi_store): Ditto.
    	(check_effective_target_vect_slp_v2si_store): Ditto.
    	(check_effective_target_vect_slp_v4si_store): Ditto.
    	* c-c++-common/Wstringop-overflow-2-novec.c: New test.
    	* gcc.dg/Warray-bounds-51-novec.c: New test.
    	* gcc.dg/Warray-bounds-48-novec.c: New test.
    	* gcc.dg/Warray-parameter-3-novec.c: New test.
    	* gcc.dg/Wstringop-overflow-14-novec.c: New test.
    	* gcc.dg/Wstringop-overflow-21-novec.c: New test.
    	* gcc.dg/Wstringop-overflow-76-novec.c: New test.
    	* gcc.dg/Wzero-length-array-bounds-2-novec.c: New test.

--- gcc/testsuite/gcc.dg/Warray-bounds-48-novec.c
+++ gcc/testsuite/gcc.dg/Warray-bounds-48-novec.c
@@ -1,7 +1,7 @@
 /* PR middle-end/91647 - missing -Warray-bounds accessing a zero-length array
    of a declared object
    { dg-do "compile" }
-   { dg-options "-O2 -Wall" }
+   { dg-options "-O2 -Wall -fno-tree-vectorize" }
    { dg-require-effective-target alloca } */
 
 typedef __INT16_TYPE__ int16_t;
@@ -30,7 +30,7 @@ static void nowarn_ax_extern (struct AX *p)
 
 static void warn_ax_local_buf (struct AX *p)
 {
-  p->ax[0] = 4; p->ax[1] = 5;  // { dg-warning "\\\[-Wstringop-overflow" "pr102706" { target { vect_slp_v2hi_store &&  { ! vect_slp_v4hi_store } } } }
+  p->ax[0] = 4; p->ax[1] = 5;
 
   p->ax[2] = 6;     // { dg-warning "\\\[-Warray-bounds" }
   p->ax[3] = 7;     // { dg-warning "\\\[-Warray-bounds" }
@@ -130,7 +130,7 @@ static void warn_a0_extern (struct A0 *p)
 
 static void warn_a0_local_buf (struct A0 *p)
 {
-  p->a0[0] = 4; p->a0[1] = 5;  // { dg-warning "\\\[-Wstringop-overflow" "pr102706" { target { vect_slp_v2hi_store && { ! vect_slp_v4hi_store } } } }
+  p->a0[0] = 4; p->a0[1] = 5;
 
   p->a0[2] = 6;     // { dg-warning "\\\[-Warray-bounds" }
   p->a0[3] = 7;     // { dg-warning "\\\[-Warray-bounds" }
