017665f63047ce47b087b0b283548a60e5abf3d2
Tobias Burnus <tobias@codesourcery.com>
2021-10-18 09:49:05 +0200

    Fortran: Fix CLASS conversion check [PR102745]
    	PR fortran/102745
    gcc/fortran/ChangeLog
    	* intrinsic.c (gfc_convert_type_warn): Fix checks by checking CLASS
    	and do typcheck in correct order for type extension.
    	* misc.c (gfc_typename): Print proper not internal CLASS type name.
    
    gcc/testsuite/ChangeLog
    	* gfortran.dg/class_72.f90: New.

--- gcc/fortran/misc.c
+++ gcc/fortran/misc.c
@@ -130,7 +130,6 @@ gfc_typename (gfc_typespec *ts, bool for_hash)
   static char buffer2[GFC_MAX_SYMBOL_LEN + 8];
   static int flag = 0;
   char *buffer;
-  gfc_typespec *ts1;
   gfc_charlen_t length = 0;
 
   buffer = flag ? buffer1 : buffer2;
@@ -180,16 +179,17 @@ gfc_typename (gfc_typespec *ts, bool for_hash)
       sprintf (buffer, "TYPE(%s)", ts->u.derived->name);
       break;
     case BT_CLASS:
-      if (ts->u.derived == NULL)
+      if (!ts->u.derived || !ts->u.derived->components
+	  || !ts->u.derived->components->ts.u.derived)
 	{
 	  sprintf (buffer, "invalid class");
 	  break;
 	}
-      ts1 = ts->u.derived->components ? &ts->u.derived->components->ts : NULL;
-      if (ts1 && ts1->u.derived && ts1->u.derived->attr.unlimited_polymorphic)
+      if (ts->u.derived->components->ts.u.derived->attr.unlimited_polymorphic)
 	sprintf (buffer, "CLASS(*)");
       else
-	sprintf (buffer, "CLASS(%s)", ts->u.derived->name);
+	sprintf (buffer, "CLASS(%s)",
+		 ts->u.derived->components->ts.u.derived->name);
       break;
     case BT_ASSUMED:
       sprintf (buffer, "TYPE(*)");
