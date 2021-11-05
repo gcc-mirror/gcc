db89d474ad812e57895cb8866ebaeedad0cc3e3f
John David Anglin <danglin@gcc.gnu.org>
2021-11-05 16:07:35 +0000

    Support TI mode and soft float on PA64
    This change implements TI mode on PA64.  Various new patterns are
    added to pa.md.  The libgcc build needed modification to build both
    DI and TI routines.  We also need various softfp routines to
    convert to and from TImode.
    
    I added full softfp for the -msoft-float option.  At the moment,
    this doesn't completely eliminate all use of the floating-point
    co-processor.  For this, libgcc needs to be built with -msoft-mult.
    The floating-point exception support also needs a soft option.
    
    2021-11-05  John David Anglin  <danglin@gcc.gnu.org>
    
    	PR libgomp/96661
    
    gcc/ChangeLog:
    
    	* config/pa/pa-modes.def: Add OImode integer type.
    	* config/pa/pa.c (pa_scalar_mode_supported_p): Allow TImode
    	for TARGET_64BIT.
    	* config/pa/pa.h (MIN_UNITS_PER_WORD) Define to MIN_UNITS_PER_WORD
    	to UNITS_PER_WORD if IN_LIBGCC2.
    	* config/pa/pa.md (addti3, addvti3, subti3, subvti3, negti2,
    	negvti2, ashlti3, shrpd_internal): New patterns.
    	Change some multi instruction types to multi.
    
    libgcc/ChangeLog:
    
    	* config.host (hppa*64*-*-linux*): Revise tmake_file.
    	(hppa*64*-*-hpux11*): Likewise.
    	* config/pa/sfp-exceptions.c: New.
    	* config/pa/sfp-machine.h: New.
    	* config/pa/t-dimode: New.
    	* config/pa/t-softfp-sfdftf: New.

--- gcc/config/pa/pa.h
+++ gcc/config/pa/pa.h
@@ -258,11 +258,17 @@ typedef struct GTY(()) machine_function
    is UNITS_PER_WORD.  Otherwise, it is the constant value that is the
    smallest value that UNITS_PER_WORD can have at run-time.
 
-   FIXME: This needs to be 4 when TARGET_64BIT is true to suppress the
-   building of various TImode routines in libgcc.  The HP runtime
-   specification doesn't provide the alignment requirements and calling
-   conventions for TImode variables.  */
-#define MIN_UNITS_PER_WORD 4
+   This needs to be 8 when TARGET_64BIT is true to allow building various
+   TImode routines in libgcc.  However, we also need the DImode DIVMOD
+   routines because they are not currently implemented in pa.md.
+   
+   The HP runtime specification doesn't provide the alignment requirements
+   and calling conventions for TImode variables.  */
+#ifdef IN_LIBGCC2
+#define MIN_UNITS_PER_WORD      UNITS_PER_WORD
+#else
+#define MIN_UNITS_PER_WORD      4
+#endif
 
 /* The widest floating point format supported by the hardware.  Note that
    setting this influences some Ada floating point type sizes, currently
