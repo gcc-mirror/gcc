44d9d55c6d0e3a1e26427662d30f350a80282634
Gerald Pfeifer <gerald@pfeifer.com>
2021-11-05 12:56:07 +0100

    include: Allow for our md5.h to defer to the system header
    This came up in the context of libsanitizer, where platform-specific
    support for FreeBSD relies on aspects provided by FreeBSD's own md5.h.
    
    Address this by allowing GCC's md5.h to pull in the system header
    instead, controlled by a new macro USE_SYSTEM_MD5.
    
    2021-11-05  Gerald Pfeifer  <gerald@pfeifer.com>
    	    Jakub Jelinek  <jakub@redhat.com>
    
    include/
    	* md5.h (USE_SYSTEM_MD5): Introduce.

--- include/md5.h
+++ include/md5.h
@@ -21,6 +21,10 @@
 #ifndef _MD5_H
 #define _MD5_H 1
 
+#ifdef USE_SYSTEM_MD5
+#include_next <md5.h>
+#else
+
 #include <stdio.h>
 
 #if defined HAVE_LIMITS_H || _LIBC
@@ -150,5 +154,7 @@ extern void *md5_buffer (const char *buffer, size_t len, void *resblock);
 #ifdef __cplusplus
 }
 #endif
+
+#endif // USE_SYSTEM_MD5
 
 #endif
