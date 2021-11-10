0cf6065ce4997774de66db4de83d461013e0f0e1
Tamar Christina <tamar.christina@arm.com>
2021-11-10 12:03:33 +0000

    testsuite: change vect_long to vect_long_long in complex tests.
    These tests are still failing on SPARC and it looks like this is because I need
    to use vect_long_long instead of vect_long.
    
    gcc/testsuite/ChangeLog:
    
    	PR testsuite/103042
    	* gcc.dg/vect/complex/bb-slp-complex-add-pattern-long.c: Use
    	vect_long_long instead of vect_long.
    	* gcc.dg/vect/complex/bb-slp-complex-add-pattern-unsigned-long.c:
    	Likewise.
    	* gcc.dg/vect/complex/vect-complex-add-pattern-long.c: Likewise.
    	* gcc.dg/vect/complex/vect-complex-add-pattern-unsigned-long.c:
    	Likewise.

--- gcc/testsuite/gcc.dg/vect/complex/bb-slp-complex-add-pattern-long.c
+++ gcc/testsuite/gcc.dg/vect/complex/bb-slp-complex-add-pattern-long.c
@@ -1,6 +1,5 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target vect_complex_add_long } */
-/* { dg-require-effective-target vect_long } */
 /* { dg-require-effective-target stdint_types } */
 /* { dg-add-options arm_v8_3a_complex_neon } */
 
@@ -13,6 +12,6 @@
 
 /* { dg-final { scan-tree-dump-times "stmt.*COMPLEX_ADD_ROT90" 1 "slp1" { target { vect_complex_add_long } } } } */
 /* { dg-final { scan-tree-dump-times "stmt.*COMPLEX_ADD_ROT270" 1 "slp1" { target { vect_complex_add_long } && ! target { aarch64_sve2 } } } } */
-/* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT270" "slp1" } } */
-/* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT90" "slp1" } } */
-/* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT90" "slp1" } } */
+/* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT270" "slp1" { target { vect_long_long } } } } */
+/* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT90" "slp1" { target { vect_long_long } } } } */
+/* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT90" "slp1" { target { vect_long_long } } } } */
--- gcc/testsuite/gcc.dg/vect/complex/bb-slp-complex-add-pattern-unsigned-long.c
+++ gcc/testsuite/gcc.dg/vect/complex/bb-slp-complex-add-pattern-unsigned-long.c
@@ -1,6 +1,5 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target stdint_types } */
-/* { dg-require-effective-target vect_long } */
 /* { dg-additional-options "-fno-tree-loop-vectorize" } */
 /* { dg-add-options arm_v8_3a_complex_neon } */
 
@@ -13,5 +12,5 @@
 
 /* { dg-final { scan-tree-dump-times "stmt.*COMPLEX_ADD_ROT90" 1 "slp1" { target { vect_complex_add_long } } } } */
 /* { dg-final { scan-tree-dump-times "stmt.*COMPLEX_ADD_ROT270" 1 "slp1" { target { vect_complex_add_long } && ! target { aarch64_sve2 } } } } */
-/* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT270" "slp1" } } */
-/* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT90" "slp1" } } */
+/* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT270" "slp1" { target { vect_long_long } } } } */
+/* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT90" "slp1" { target { vect_long_long } } } } */
