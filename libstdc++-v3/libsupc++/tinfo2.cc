054bf99841aad3869c70643b2ba2d9f85770c980
Jonathan Wakely <jwakely@redhat.com>
2021-11-16 21:03:21 +0000

    libstdc++: Fix std::type_info::before for ARM [PR103240]
    The r179236 fix for std::type_info::operator== should also have been
    applied to std::type_info::before. Otherwise two distinct types can
    compare equivalent due to using a string comparison, when they should do
    a pointer comparison.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/103240
    	* libsupc++/tinfo2.cc (type_info::before): Use unadjusted name
    	to check for the '*' prefix.
    	* testsuite/util/testsuite_shared.cc: Add type_info object for
    	use in new test.
    	* testsuite/18_support/type_info/103240.cc: New test.

--- libstdc++-v3/libsupc++/tinfo2.cc
+++ libstdc++-v3/libsupc++/tinfo2.cc
@@ -36,7 +36,10 @@ type_info::before (const type_info &arg) const _GLIBCXX_NOEXCEPT
 #if __GXX_MERGED_TYPEINFO_NAMES
   return name () < arg.name ();
 #else
-  return (name ()[0] == '*') ? name () < arg.name ()
+  /* The name() method will strip any leading '*' prefix. Therefore
+     take care to look at __name rather than name() when looking for
+     the "pointer" prefix.  */
+  return (__name[0] == '*') ? name () < arg.name ()
     :  __builtin_strcmp (name (), arg.name ()) < 0;
 #endif
 }
