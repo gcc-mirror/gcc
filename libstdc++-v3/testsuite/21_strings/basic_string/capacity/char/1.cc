b8f2efaed02e8b03d215d74e42d3707761772f64
Jonathan Wakely <jwakely@redhat.com>
2021-11-19 18:13:10 +0000

    libstdc++: Suppress -Wstringop warnings [PR103332]
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/103332
    	PR libstdc++/102958
    	* testsuite/21_strings/basic_string/capacity/char/1.cc: Add
    	-Wno-stringop-overflow.
    	* testsuite/21_strings/basic_string/operators/char/1.cc:
    	Likewise.
    	* testsuite/experimental/filesystem/path/factory/u8path-char8_t.cc:
    	Add -Wno-stringop-overread.

--- libstdc++-v3/testsuite/21_strings/basic_string/capacity/char/1.cc
+++ libstdc++-v3/testsuite/21_strings/basic_string/capacity/char/1.cc
@@ -17,6 +17,8 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
+// { dg-options "-Wno-stringop-overflow" }
+
 // 21.3.3 string capacity
 
 #include <string>
@@ -59,6 +61,7 @@ void test01()
 
   std::string str05(30, 'q');
   std::string str06 = str05;
+  // The following triggers -Wstringop-overflow.  See PR 103332.
   str05 = str06 + str05;
   VERIFY( str05.capacity() >= str05.size() );
   VERIFY( str06.capacity() >= str06.size() );
