487d604b6fa0f0a981eadc216d9e481d08ed7e7b
Tamar Christina <tamar.christina@arm.com>
2021-11-19 15:12:38 +0000

    middle-end: Handle FMA_CONJ correctly after SLP layout update.
    Apologies, I got dinged by the i386 regressions bot for a test I didn't have in
    my tree at the time I made the previous patch.  The bot was telling me that FMA
    stopped working after I strengthened the FMA check in the previous patch.
    
    The reason is that the check is slightly early.  The first check can indeed only
    exit early when either node isn't a mult.  However we need to delay till we know
    if the node is a MUL or FMA before enforcing that both nodes must be a MULT
    since the node to inspect is different if the operation is a MUL or FMA.
    
    Also with the update patch for GCC 11 tree layout update to the new GCC 12 one
    I had missed that the difference in which node is conjucated is not symmetrical.
    
    So the test for it can just be testing the inverse order.  It was Currently
    no detecting when the first node was conjucated instead of the second one.
    
    This also made me wonder why my own test didn't detect this.  It turns out that
    the tests, being copied from the _Float16 ones were incorrectly marked as
    xfail.  The _Float16 ones are marked as xfail since C doesn't have a conj
    operation for _Float16, which means you get extra type-casts in between.
    
    While you could use the GCC _Complex extension here I opted to mark them xfail
    since I wanted to include detection over the widenings next year.
    
    Secondly the double tests were being skipped because Adv. SIMD was missing from
    targets supporting Complex Double vectorization.
    
    With these changes all other tests run and pass and only XFAIL ones are
    correctly the _Float16 ones.  Sorry for missing this before, testing should now
    cover all cases.
    
    gcc/ChangeLog:
    
    	PR tree-optimization/103311
    	PR target/103330
    	* tree-vect-slp-patterns.c (vect_validate_multiplication): Fix CONJ
    	test to new codegen.
    	(complex_mul_pattern::matches): Move check downwards.
    
    gcc/testsuite/ChangeLog:
    
    	PR tree-optimization/103311
    	PR target/103330
    	* gcc.dg/vect/complex/fast-math-bb-slp-complex-mla-double.c: Fix it.
    	* gcc.dg/vect/complex/fast-math-bb-slp-complex-mla-float.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-bb-slp-complex-mls-double.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-bb-slp-complex-mls-float.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-bb-slp-complex-mul-double.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-bb-slp-complex-mul-float.c: Likewise.
    	* lib/target-supports.exp
    	(check_effective_target_vect_complex_add_double): Add Adv. SIMD.

--- gcc/testsuite/gcc.dg/vect/complex/fast-math-bb-slp-complex-mls-float.c
+++ gcc/testsuite/gcc.dg/vect/complex/fast-math-bb-slp-complex-mls-float.c
@@ -1,11 +1,11 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target vect_complex_add_float } */
-/* { dg-additional-options "-fno-tree-loop-vectorize" } */
+/* { dg-additional-options "-fdump-tree-vect-details" } */
 /* { dg-add-options arm_v8_3a_complex_neon } */
 
 #define TYPE float
 #define N 16
 #include "complex-mls-template.c"
 
-/* { dg-final { scan-tree-dump "Found COMPLEX_FMS_CONJ" "slp1"  { xfail *-*-* } } } */
-/* { dg-final { scan-tree-dump "Found COMPLEX_FMS" "slp1"  { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump "Found COMPLEX_FMS_CONJ" "vect" } } */
+/* { dg-final { scan-tree-dump "Found COMPLEX_FMS" "vect" } } */
