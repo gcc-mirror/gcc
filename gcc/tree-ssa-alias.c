f4e470d44e8104bada6a95c12765f6ed8bd505e2
Jan Hubicka <jh@suse.cz>
2021-11-25 14:42:47 +0100

    Do not check gimple_static_cahin in ref_maybe_used_by_call_p_1
    gcc/ChangeLog:
    
    2021-11-25  Jan Hubicka  <hubicka@ucw.cz>
    
    	* tree-ssa-alias.c (ref_maybe_used_by_call_p_1): Do not check
    	gimple_static_chain.

--- gcc/tree-ssa-alias.c
+++ gcc/tree-ssa-alias.c
@@ -2743,9 +2743,7 @@ ref_maybe_used_by_call_p_1 (gcall *call, ao_ref *ref, bool tbaa_p)
   unsigned i;
   int flags = gimple_call_flags (call);
 
-  /* Const functions without a static chain do not implicitly use memory.  */
-  if (!gimple_call_chain (call)
-      && (flags & (ECF_CONST|ECF_NOVOPS)))
+  if (flags & (ECF_CONST|ECF_NOVOPS))
     goto process_args;
 
   /* A call that is not without side-effects might involve volatile
