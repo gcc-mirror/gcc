7057b8f8c2fbb7a2112705c2962d92b8ccef7c30
Richard Sandiford <richard.sandiford@arm.com>
2021-11-30 17:32:36 +0000

    vect: Fix ncopies calculation for emulated gather/scatter [PR103494]
    I was too eager about removing ncopies calculations in g:10833849b55.
    When emulating gather/scatter, the offset ncopies can be different from
    the data ncopies.  This patch restores the original calculation.
    
    gcc/
    	PR tree-optimization/103494
    	* tree-vect-stmts.c (vect_get_gather_scatter_ops): Remove ncopies
    	argument and calculate ncopies from gs_info->offset_vectype
    	where necessary.
    	(vectorizable_store, vectorizable_load): Update accordingly.
    
    gcc/testsuite/
    	PR tree-optimization/103494
    	* gcc.dg/vect/pr103494.c: New test.
    	* g++.dg/vect/pr103494.cc: Likewise.

--- gcc/tree-vect-stmts.c
+++ gcc/tree-vect-stmts.c
@@ -2962,8 +2962,7 @@ vect_build_gather_load_calls (vec_info *vinfo, stmt_vec_info stmt_info,
 static void
 vect_get_gather_scatter_ops (loop_vec_info loop_vinfo,
 			     class loop *loop, stmt_vec_info stmt_info,
-			     slp_tree slp_node, unsigned int ncopies,
-			     gather_scatter_info *gs_info,
+			     slp_tree slp_node, gather_scatter_info *gs_info,
 			     tree *dataref_ptr, vec<tree> *vec_offset)
 {
   gimple_seq stmts = NULL;
@@ -2978,9 +2977,13 @@ vect_get_gather_scatter_ops (loop_vec_info loop_vinfo,
   if (slp_node)
     vect_get_slp_defs (SLP_TREE_CHILDREN (slp_node)[0], vec_offset);
   else
-    vect_get_vec_defs_for_operand (loop_vinfo, stmt_info, ncopies,
-				   gs_info->offset, vec_offset,
-				   gs_info->offset_vectype);
+    {
+      unsigned ncopies
+	= vect_get_num_copies (loop_vinfo, gs_info->offset_vectype);
+      vect_get_vec_defs_for_operand (loop_vinfo, stmt_info, ncopies,
+				     gs_info->offset, vec_offset,
+				     gs_info->offset_vectype);
+    }
 }
 
 /* Prepare to implement a grouped or strided load or store using
@@ -8149,8 +8152,8 @@ vectorizable_store (vec_info *vinfo,
 	  else if (STMT_VINFO_GATHER_SCATTER_P (stmt_info))
 	    {
 	      vect_get_gather_scatter_ops (loop_vinfo, loop, stmt_info,
-					   slp_node, ncopies, &gs_info,
-					   &dataref_ptr, &vec_offsets);
+					   slp_node, &gs_info, &dataref_ptr,
+					   &vec_offsets);
 	      vec_offset = vec_offsets[0];
 	    }
 	  else
@@ -9454,8 +9457,8 @@ vectorizable_load (vec_info *vinfo,
 	  else if (STMT_VINFO_GATHER_SCATTER_P (stmt_info))
 	    {
 	      vect_get_gather_scatter_ops (loop_vinfo, loop, stmt_info,
-					   slp_node, ncopies, &gs_info,
-					   &dataref_ptr, &vec_offsets);
+					   slp_node, &gs_info, &dataref_ptr,
+					   &vec_offsets);
 	    }
 	  else
 	    dataref_ptr
