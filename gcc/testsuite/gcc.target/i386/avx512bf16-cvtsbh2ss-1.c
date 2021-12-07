61e53698a08dc1d9a54d785218af687a6751c1b3
konglin1 <lingling.kong@intel.com>
2021-12-07 17:08:23 +0800

    i386: Enable intrinsics that convert float and bf16 data to each other.
    gcc/ChangeLog:
    
    	* config/i386/avx512bf16intrin.h (_mm_cvtsbh_ss): Add new intrinsic.
    	(_mm512_cvtpbh_ps): Likewise.
    	(_mm512_maskz_cvtpbh_ps): Likewise.
    	(_mm512_mask_cvtpbh_ps): Likewise.
    	* config/i386/avx512bf16vlintrin.h (_mm_cvtness_sbh): Likewise.
    	(_mm_cvtpbh_ps): Likewise.
    	(_mm256_cvtpbh_ps): Likewise.
    	(_mm_maskz_cvtpbh_ps): Likewise.
    	(_mm256_maskz_cvtpbh_ps): Likewise.
    	(_mm_mask_cvtpbh_ps): Likewise.
    	(_mm256_mask_cvtpbh_ps): Likewise.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/i386/avx512bf16-cvtsbh2ss-1.c: New test.
    	* gcc.target/i386/avx512bf16-vcvtpbh2ps-1.c: Ditto.
    	* gcc.target/i386/avx512bf16vl-cvtness2sbh-1.c: Ditto.
    	* gcc.target/i386/avx512bf16vl-vcvtpbh2ps-1.c: Ditto.

---
+++ gcc/testsuite/gcc.target/i386/avx512bf16-cvtsbh2ss-1.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-options "-mavx512bf16 -O2" } */
+/* { dg-final { scan-assembler-times "sall\[ \\t\]+\[^\{\n\]*16" 1 } } */
+/* { dg-final { scan-assembler-times "movl" 1 } } */
+
+#include <immintrin.h>
+
+volatile __bfloat16 x1;
+volatile float res;
+
+void extern
+avx512bf16_test (void)
+{
+  res = _mm_cvtsbh_ss (x1);
+}
