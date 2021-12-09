02b601ad7ea06460e7bc42d7251e895d9031bc6e
Jonathan Wakely <jwakely@redhat.com>
2021-12-09 13:54:39 +0000

    libstdc++: Fix std::exception_ptr regressions [PR103630]
    This restores support for std::make_exception_ptr<E&> and for using
    std::exception_ptr in C++98.
    
    Because the new non-throwing implementation needs to use std::decay to
    handle references the original throwing implementation is used for
    C++98.
    
    We also need to change the typeid expression so it doesn't yield the
    dynamic type when the function parameter is a reference to a polymorphic
    type. Otherwise the new exception object could be caught by any handler
    matching the dynamic type, even though the actual exception object is
    only a copy of the base class, sliced to the static type.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/103630
    	* libsupc++/exception_ptr.h (make_exception_ptr): Decay the
    	template parameter. Use typeid of the static type.
    	* testsuite/18_support/exception_ptr/103630.cc: New test.
    
    (cherry picked from commit a1ca039fc0fe934ef36c25d8284e6e116bcaffa7)

--- libstdc++-v3/libsupc++/exception_ptr.h
+++ libstdc++-v3/libsupc++/exception_ptr.h
@@ -39,6 +39,10 @@
 #include <typeinfo>
 #include <new>
 
+#if __cplusplus >= 201103L
+# include <bits/move.h>
+#endif
+
 extern "C++" {
 
 namespace std 
@@ -185,14 +189,16 @@ namespace std
     exception_ptr 
     make_exception_ptr(_Ex __ex) _GLIBCXX_USE_NOEXCEPT
     {
-#if __cpp_exceptions && __cpp_rtti && !_GLIBCXX_HAVE_CDTOR_CALLABI
+#if __cpp_exceptions && __cpp_rtti && !_GLIBCXX_HAVE_CDTOR_CALLABI \
+      && __cplusplus >= 201103L
+      using _Ex2 = typename remove_reference<_Ex>::type;
       void* __e = __cxxabiv1::__cxa_allocate_exception(sizeof(_Ex));
       (void) __cxxabiv1::__cxa_init_primary_exception(
-	  __e, const_cast<std::type_info*>(&typeid(__ex)),
-	  __exception_ptr::__dest_thunk<_Ex>);
+	  __e, const_cast<std::type_info*>(&typeid(_Ex)),
+	  __exception_ptr::__dest_thunk<_Ex2>);
       try
 	{
-          ::new (__e) _Ex(__ex);
+	  ::new (__e) _Ex2(std::forward<_Ex>(__ex));
           return exception_ptr(__e);
 	}
       catch(...)
