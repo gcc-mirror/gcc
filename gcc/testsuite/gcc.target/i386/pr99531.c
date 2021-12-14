973f6aedeb6489a9fdc7aeceaed0514348ee1e1a
Vladimir N. Makarov <vmakarov@redhat.com>
2021-12-14 08:57:30 -0500

    [PR99531] Do not scan push insn for ia32 in the test
    The patch prohibits scanning push insn for ia32 as push are expected not to be generated only for x86_64 Linux ABI.
    
    gcc/testsuite/ChangeLog:
    
    	PR target/99531
    	* gcc.target/i386/pr99531.c: Do not scan for ia32.

--- gcc/testsuite/gcc.target/i386/pr99531.c
+++ gcc/testsuite/gcc.target/i386/pr99531.c
@@ -4,4 +4,4 @@
 int func(int, int, int, int, int, int);
 int caller(int a, int b, int c, int d, int e) { return func(0, a, b, c, d, e); }
 
-/* { dg-final { scan-assembler-not "push" } } */
+/* { dg-final { scan-assembler-not "push"  { target { ! ia32 } } } } */
