87ae8d7613a8b15d0d729b38ffd49153f1314799
Segher Boessenkool <segher@kernel.crashing.org>
2021-12-17 17:01:16 +0000

    rs6000: Update darn testcases
    Make the darn testcases work (and be tested) in 32-bit mode as well.
    They used to ICE, but they no longer do.
    
    2021-12-17  Segher Boessenkool <segher@kernel.crashing.org>
    
    gcc/testsuite/
    	PR target/103624
    	* gcc.target/powerpc/darn-0.c: Remove target clause.
    	* gcc.target/powerpc/darn-1.c: Remove target clause. Remove lp64
    	requirement.  Change return type to long.
    	* gcc.target/powerpc/darn-2.c: Ditto.
    	* gcc.target/powerpc/darn-3.c: Remove target clause.

--- gcc/testsuite/gcc.target/powerpc/darn-2.c
+++ gcc/testsuite/gcc.target/powerpc/darn-2.c
@@ -1,12 +1,11 @@
-/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-do compile } */
 /* { dg-require-effective-target powerpc_p9vector_ok } */
-/* { dg-require-effective-target lp64 } */
 /* { dg-skip-if "" { powerpc*-*-aix* } } */
 /* { dg-options "-mdejagnu-cpu=power9" } */
 
 #include <altivec.h>
 
-long long get_raw_random ()
+long get_raw_random ()
 {
   return __builtin_darn_raw ();
 }
