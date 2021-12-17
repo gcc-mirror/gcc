303bd2a8428b75f2467578a933e729d5672789d1
Yannick Moy <moy@adacore.com>
2021-12-17 17:45:14 +0100

    [Ada] Proof of unit System.Case_Util
    gcc/ada/
    
    	* libgnat/s-casuti.adb: Add ghost code.
    	* libgnat/s-casuti.ads: Add contracts.

--- gcc/ada/libgnat/s-casuti.adb
+++ gcc/ada/libgnat/s-casuti.adb
@@ -29,8 +29,17 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
-package body System.Case_Util is
+--  Ghost code, loop invariants and assertions in this unit are meant for
+--  analysis only, not for run-time checking, as it would be too costly
+--  otherwise. This is enforced by setting the assertion policy to Ignore.
 
+pragma Assertion_Policy (Ghost          => Ignore,
+                         Loop_Invariant => Ignore,
+                         Assert         => Ignore);
+
+package body System.Case_Util
+  with SPARK_Mode
+is
    --------------
    -- To_Lower --
    --------------
@@ -53,6 +62,9 @@ package body System.Case_Util is
    begin
       for J in A'Range loop
          A (J) := To_Lower (A (J));
+
+         pragma Loop_Invariant
+           (for all K in A'First .. J => A (K) = To_Lower (A'Loop_Entry (K)));
       end loop;
    end To_Lower;
 
@@ -77,6 +89,15 @@ package body System.Case_Util is
          else
             A (J) := To_Lower (A (J));
          end if;
+
+         pragma Loop_Invariant
+           (for all K in A'First .. J =>
+              (if K = A'First
+                 or else A'Loop_Entry (K - 1) = '_'
+               then
+                 A (K) = To_Upper (A'Loop_Entry (K))
+               else
+                 A (K) = To_Lower (A'Loop_Entry (K))));
 
          Ucase := A (J) = '_';
       end loop;
@@ -111,6 +132,9 @@ package body System.Case_Util is
    begin
       for J in A'Range loop
          A (J) := To_Upper (A (J));
+
+         pragma Loop_Invariant
+           (for all K in A'First .. J => A (K) = To_Upper (A'Loop_Entry (K)));
       end loop;
    end To_Upper;
 
