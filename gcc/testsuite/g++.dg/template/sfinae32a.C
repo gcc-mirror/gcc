916ec36d0a3ef3fe44c1657746922a5f18b60326
Patrick Palka <ppalka@redhat.com>
2021-12-27 10:01:42 -0500

    c++: Add testcase for SFINAE w/ p[N] and incomplete type [PR101239]
    The r12-6123 fix for SFINAE with p+N and incomplete type also fixed
    the analogous issue with p[N].
    
    	PR c++/101239
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/template/sfinae32a.C: New test.

---
+++ gcc/testsuite/g++.dg/template/sfinae32a.C
@@ -0,0 +1,24 @@
+// PR c++/101239
+// { dg-do compile { target c++11 } }
+
+template<class T, int N> auto f(T* p) -> decltype(p[N]);
+template<class T, int N> auto f(T* p) -> decltype(p[-N]);
+template<class T, int N> auto f(T* p) -> decltype(N[p]);
+template<class T, int N> void f(T* p);
+
+template<class T> auto g(T* p, int n) -> decltype(p[n]);
+template<class T> auto g(T* p, int n) -> decltype(p[-n]);
+template<class T> auto g(T* p, int n) -> decltype(n[p]);
+template<class T> void g(T* p, int n);
+
+struct Incomplete;
+
+int main() {
+  f<Incomplete,  0>(nullptr);
+  f<Incomplete,  1>(nullptr);
+  f<Incomplete, -1>(nullptr);
+  f<Incomplete,  7>(nullptr);
+  f<Incomplete, -7>(nullptr);
+
+  g<Incomplete>(nullptr, 0);
+}
