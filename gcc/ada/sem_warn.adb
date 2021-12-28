1be42e5907524af490afdec8ff2e200e69bfa8aa
Justin Squirek <squirek@adacore.com>
2021-12-28 19:32:51 +0000

    [Ada] Task arrays trigger spurious unreferenced warnings
    gcc/ada/
    
    	* sem_warn.adb (Check_References): Add call to Has_Task instead
    	of checking component type.

--- gcc/ada/sem_warn.adb
+++ gcc/ada/sem_warn.adb
@@ -1701,13 +1701,10 @@ package body Sem_Warn is
                           and then Ekind (E1) /= E_Constant
                           and then Ekind (E1) /= E_Component)
 
-                         --  Check that E1T is not a task or an array of them
+                         --  Check that E1T is not a task or a composite type
+                         --  with a task component.
 
-                         or else not
-                           (Is_Task_Type (E1T)
-                             or else (Ekind (E1T) in Array_Kind
-                                       and then Is_Task_Type
-                                                  (Component_Type (E1T)))))
+                         or else not Has_Task (E1T))
 
               --  For subunits, only place warnings on the main unit itself,
               --  since parent units are not completely compiled.
