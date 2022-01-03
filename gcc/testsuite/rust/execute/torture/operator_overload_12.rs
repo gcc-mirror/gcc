41cdd94998955b80d03d7f5dd9c5b51571ef8113
Philip Herron <philip.herron@embecosm.com>
2022-01-03 15:51:42 +0000

    Add missing bitwise lang items for operator overloading
    This adds the missing bitwise lang items:
    
    - bitand: libcore/ops/bit.rs
    - bitor: libcore/ops/bit.rs
    - bitxor: libcore/ops/bit.rs
    - shl: libcore/ops/bit.rs
    - shr: libcore/ops/bit.rs
    - bitand_assign: libcore/ops/bit.rs
    - bitor_assign: libcore/ops/bit.rs
    - bitxor_assign: libcore/ops/bit.rs
    - shl_assign: libcore/ops/bit.rs
    - shr_assign: libcore/ops/bit.rs
    
    Addresses #742

---
+++ gcc/testsuite/rust/execute/torture/operator_overload_12.rs
@@ -0,0 +1,31 @@
+// { dg-output "1\n" }
+// { dg-additional-options "-w" }
+extern "C" {
+    fn printf(s: *const i8, ...);
+}
+
+#[lang = "bitand_assign"]
+pub trait BitAndAssign<Rhs = Self> {
+    fn bitand_assign(&mut self, rhs: Rhs);
+}
+
+impl BitAndAssign for i32 {
+    fn bitand_assign(&mut self, other: i32) {
+        *self &= other;
+
+        unsafe {
+            let a = "%i\n\0";
+            let b = a as *const str;
+            let c = b as *const i8;
+
+            printf(c, *self);
+        }
+    }
+}
+
+fn main() -> i32 {
+    let mut a = 1;
+    a &= 1;
+
+    0
+}
