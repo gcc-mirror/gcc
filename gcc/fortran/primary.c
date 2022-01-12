0b8464365b15ac108cd1d00d5bc56d229c1340de
Harald Anlauf <anlauf@gmx.de>
2022-01-12 21:24:49 +0100

    Fortran: fix error recovery on bad structure constructor in DATA statement
    gcc/fortran/ChangeLog:
    
    	PR fortran/67804
    	* primary.c (gfc_match_structure_constructor): Recover from errors
    	that occurred while checking for a valid structure constructor in
    	a DATA statement.
    
    gcc/testsuite/ChangeLog:
    
    	PR fortran/67804
    	* gfortran.dg/pr93604.f90: Adjust to changed diagnostics.
    	* gfortran.dg/pr67804.f90: New test.

--- gcc/fortran/primary.c
+++ gcc/fortran/primary.c
@@ -3364,6 +3364,7 @@ gfc_match_structure_constructor (gfc_symbol *sym, gfc_expr **result)
   match m;
   gfc_expr *e;
   gfc_symtree *symtree;
+  bool t = true;
 
   gfc_get_ha_sym_tree (sym->name, &symtree);
 
@@ -3394,10 +3395,18 @@ gfc_match_structure_constructor (gfc_symbol *sym, gfc_expr **result)
      in the structure constructor must be a constant.  Try to reduce the
      expression here.  */
   if (gfc_in_match_data ())
-    gfc_reduce_init_expr (e);
+    t = gfc_reduce_init_expr (e);
 
-  *result = e;
-  return MATCH_YES;
+  if (t)
+    {
+      *result = e;
+      return MATCH_YES;
+    }
+  else
+    {
+      gfc_free_expr (e);
+      return MATCH_ERROR;
+    }
 }
 
 
