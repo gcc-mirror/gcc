080a06fcb076b3586ee4b00d415ae177f0b76b18
Xionghu Luo <luoxhu@linux.ibm.com>
2022-01-12 18:36:54 -0600

    rs6000: Add split pattern to replace
    7: r120:V4SI=const_vector
    8: r121:V4SI=unspec[r120:V4SI,r120:V4SI,0xc] 260
    
    with r121:v4SI = r120:V4SI when r120 is a vector with same element.
    
    gcc/ChangeLog:
    
    	* config/rs6000/altivec.md (sldoi_to_mov<mode>): New.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/powerpc/sldoi_to_mov.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/sldoi_to_mov.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+
+#include <altivec.h>
+vector signed int foo1 (vector signed int a) {
+    vector signed int b = {0};
+    return vec_sum2s(a, b);
+}
+
+vector signed int foo2 (vector signed int a) {
+    vector signed int b = {0};
+    return vec_sld(b, b, 4);
+}
+
+/* { dg-final { scan-assembler-times {\mvsldoi\M} 1 {target le} } } */
