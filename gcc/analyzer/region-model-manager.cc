2aefe248aa4160205c44808166393a42031d2dea
David Malcolm <dmalcolm@redhat.com>
2022-01-18 11:41:19 -0500

    analyzer: fix ICE on unary ops folding to casts of constants [PR104089]
    gcc/analyzer/ChangeLog:
    	PR analyzer/104089
    	* region-model-manager.cc
    	(region_model_manager::get_or_create_constant_svalue): Assert that
    	we have a CONSTANT_CLASS_P.
    	(region_model_manager::maybe_fold_unaryop): Only fold a constant
    	when fold_unary's result is a constant or a cast of a constant.
    
    gcc/testsuite/ChangeLog:
    	* gcc.dg/analyzer/pr104089.c: New test.
    	PR analyzer/104089
    
    Signed-off-by: David Malcolm <dmalcolm@redhat.com>

--- gcc/analyzer/region-model-manager.cc
+++ gcc/analyzer/region-model-manager.cc
@@ -209,6 +209,7 @@ const svalue *
 region_model_manager::get_or_create_constant_svalue (tree cst_expr)
 {
   gcc_assert (cst_expr);
+  gcc_assert (CONSTANT_CLASS_P (cst_expr));
 
   constant_svalue **slot = m_constants_map.get (cst_expr);
   if (slot)
@@ -426,7 +427,23 @@ region_model_manager::maybe_fold_unaryop (tree type, enum tree_code op,
   /* Constants.  */
   if (tree cst = arg->maybe_get_constant ())
     if (tree result = fold_unary (op, type, cst))
-      return get_or_create_constant_svalue (result);
+      {
+	if (CONSTANT_CLASS_P (result))
+	  return get_or_create_constant_svalue (result);
+
+	/* fold_unary can return casts of constants; try to handle them.  */
+	if (op != NOP_EXPR
+		 && type
+		 && TREE_CODE (result) == NOP_EXPR
+		 && CONSTANT_CLASS_P (TREE_OPERAND (result, 0)))
+	  {
+	    const svalue *inner_cst
+	      = get_or_create_constant_svalue (TREE_OPERAND (result, 0));
+	    return get_or_create_cast (type,
+				       get_or_create_cast (TREE_TYPE (result),
+							   inner_cst));
+	  }
+      }
 
   return NULL;
 }
