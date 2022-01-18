fe3ed885cda5ab920d361b694ee539242052022f
Jonathan Wakely <jwakely@redhat.com>
2022-01-18 16:26:45 +0000

    libstdc++: Limit new basic_string(nullptr_t) constructor to C++23 [PR104099]
    The new deleted constructors added by P2166R1 are a breaking change,
    making previously valid code ill-formed in C++23. As a result, they
    should only be defined for C++23 and not for C++11 and up.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/104099
    	* include/bits/basic_string.h (basic_string(nullptr_t)): Only
    	define for C++23.
    	(operator=(nullptr_t)): Likewise.
    	* include/bits/cow_string.h: Likewise.
    	* include/std/string_view (basic_string_view(nullptr_t)):
    	Likewise.
    	* testsuite/21_strings/basic_string/cons/char/nullptr.cc: Adjust
    	expected error. Add examples that become ill-formed in C++23.
    	* testsuite/21_strings/basic_string_view/cons/char/nonnull.cc:
    	Adjust expected errors.
    	* testsuite/21_strings/basic_string_view/cons/wchar_t/nonnull.cc:
    	Likewise.

--- libstdc++-v3/include/bits/basic_string.h
+++ libstdc++-v3/include/bits/basic_string.h
@@ -728,10 +728,12 @@ _GLIBCXX_BEGIN_NAMESPACE_CXX11
 	else
 	  _M_construct(__str.begin(), __str.end(), std::forward_iterator_tag());
       }
+#endif // C++11
 
+#if __cplusplus >= 202100L
       basic_string(nullptr_t) = delete;
       basic_string& operator=(nullptr_t) = delete;
-#endif // C++11
+#endif // C++23
 
       /**
        *  @brief  Construct string as copy of a range.
