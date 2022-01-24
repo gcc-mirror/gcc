fe5cee6f62a0b229d9d51616b7490331d39b5ddd
Jason Merrill <jason@redhat.com>
2022-01-24 00:01:40 -0500

    c++: assignment to temporary [PR59950]
    Given build_this of a TARGET_EXPR, cp_build_fold_indirect_ref returns the
    TARGET_EXPR.  But that's the wrong value category for the result of the
    defaulted class assignment operator, which returns an lvalue, so we need to
    actually build the INDIRECT_REF.
    
    	PR c++/59950
    
    gcc/cp/ChangeLog:
    
    	* call.cc (build_over_call): Use cp_build_indirect_ref.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/init/assign2.C: New test.

--- gcc/cp/call.cc
+++ gcc/cp/call.cc
@@ -9789,7 +9789,10 @@ build_over_call (struct z_candidate *cand, int flags, tsubst_flags_t complain)
 	   && DECL_OVERLOADED_OPERATOR_IS (fn, NOP_EXPR)
 	   && trivial_fn_p (fn))
     {
-      tree to = cp_build_fold_indirect_ref (argarray[0]);
+      /* Don't use cp_build_fold_indirect_ref, op= returns an lvalue even if
+	 the object argument isn't one.  */
+      tree to = cp_build_indirect_ref (input_location, argarray[0],
+				       RO_ARROW, complain);
       tree type = TREE_TYPE (to);
       tree as_base = CLASSTYPE_AS_BASE (type);
       tree arg = argarray[1];
