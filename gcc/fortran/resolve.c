3a426e474125c37e5f5268aaa42600b0ef4192e4
Mikael Morin <mikael@gcc.gnu.org>
2022-01-28 22:00:57 +0100

    fortran: Backport associate character length fixes [PR104228]
    Backport commits:
    r12-7217-g57da34939703a6e6d3267a0d25d1fb9369d3ac0e
    r12-7726-g907811ddc35da6c1701ed22355ece63a8c3ed7fb
    
    --
    
    fortran: Unshare associate var charlen [PR104228]
    
    PR104228 showed that character lengths were shared between associate
    variable and associate targets.  This is problematic when the associate
    target is itself a variable and gets a variable to hold the length, as
    the length variable is added (and all the variables following it in the chain)
    to both the associate variable scope and the target variable scope.
    This caused an ICE when compiling with -O0 -fsanitize=address.
    
    This change forces the creation of a separate character length for the
    associate variable.  It also forces the initialization of the character
    length variable to avoid regressing associate_32 and associate_47 tests.
    
    --
    
    fortran: Separate associate character lengths earlier [PR104570]
    
    This change workarounds an ICE in the evaluation of the character length
    of an array expression referencing an associate variable; the code is
    not prepared to see a non-scalar expression as it doesn’t initialize the
    scalarizer.
    
    Before this change, associate length symbols get a new gfc_charlen at
    resolution stage to unshare them from the associate expression, so that
    at translation stage it is a decl specific to the associate symbol that
    is initialized, not the decl of some other symbol.  This
    reinitialization of gfc_charlen happens after expressions referencing
    the associate symbol have been parsed, so that those expressions retain
    the original gfc_charlen they have copied from the symbol.
    At translation stage, the gfc_charlen for the associate symbol is setup
    with the decl holding the actual length value, but the expressions have
    retained the original gfc_charlen without any decl.  So they need to
    evaluate the character length, and this is where the ICE happens.
    
    This change moves the reinitialization of gfc_charlen earlier at parsing
    stage, so that at resolution stage the gfc_charlen can be retained as
    it’s already not shared with any other symbol, and the expressions which
    now share their gfc_charlen with the symbol are automatically updated
    when the length decl is setup at translation stage.  There is no need
    any more to evaluate the character length as it has all the required
    information, and the ICE doesn’t happen.
    
    The first resolve.c hunk is necessary to avoid regressing on the
    associate_35.f90 testcase.
    
    --
    
    	PR fortran/104228
    	PR fortran/104570
    
    gcc/fortran/ChangeLog:
    
    	* parse.c (parse_associate): Use a new distinct gfc_charlen if
    	the copied type has one whose length is not known to be
    	constant.
    	* resolve.c (resolve_assoc_var): Also create a new character
    	length for non-dummy associate targets.  Reset charlen if it’s
    	shared with the associate target regardless of the expression
    	type.  Don’t reinitialize charlen if it’s deferred.
    	* trans-stmt.c (trans_associate_var): Initialize character
    	length even if no temporary is used for the associate variable.
    
    gcc/testsuite/ChangeLog:
    
    	* gfortran.dg/asan_associate_58.f90: New test.
    	* gfortran.dg/asan_associate_59.f90: New test.
    	* gfortran.dg/associate_58.f90: New test.
    
    (cherry picked from commit 4c8e037d32e91c0ec4ac577d1d0723ab9d6ec809)

--- gcc/fortran/resolve.c
+++ gcc/fortran/resolve.c
@@ -8942,8 +8942,7 @@ resolve_assoc_var (gfc_symbol* sym, bool resolve_target)
       if (!sym->ts.u.cl)
 	sym->ts.u.cl = target->ts.u.cl;
 
-      if (sym->ts.deferred && target->expr_type == EXPR_VARIABLE
-	  && target->symtree->n.sym->attr.dummy
+      if (sym->ts.deferred
 	  && sym->ts.u.cl == target->ts.u.cl)
 	{
 	  sym->ts.u.cl = gfc_new_charlen (sym->ns, NULL);
@@ -8962,8 +8961,11 @@ resolve_assoc_var (gfc_symbol* sym, bool resolve_target)
 		|| sym->ts.u.cl->length->expr_type != EXPR_CONSTANT)
 		&& target->expr_type != EXPR_VARIABLE)
 	{
-	  sym->ts.u.cl = gfc_new_charlen (sym->ns, NULL);
-	  sym->ts.deferred = 1;
+	  if (!sym->ts.deferred)
+	    {
+	      sym->ts.u.cl = gfc_new_charlen (sym->ns, NULL);
+	      sym->ts.deferred = 1;
+	    }
 
 	  /* This is reset in trans-stmt.c after the assignment
 	     of the target expression to the associate name.  */
