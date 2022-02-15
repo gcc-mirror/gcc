14f0b450f7976f824428f0d35b4e7e6dc162f708
Jakub Jelinek <jakub@redhat.com>
2022-02-15 10:22:30 +0100

    openmp: Make finalize_task_copyfn order reproduceable [PR104517]
    The following testcase fails -fcompare-debug, because finalize_task_copyfn
    was invoked from splay tree destruction, whose order can in some cases
    depend on -g/-g0.  The fix is to queue the task stmts that need copyfn
    in a vector and run finalize_task_copyfn on elements of that vector.
    
    2022-02-15  Jakub Jelinek  <jakub@redhat.com>
    
    	PR debug/104517
    	* omp-low.c (task_cpyfns): New variable.
    	(delete_omp_context): Don't call finalize_task_copyfn from here.
    	(create_task_copyfn): Push task_stmt into task_cpyfns.
    	(execute_lower_omp): Call finalize_task_copyfn here on entries from
    	task_cpyfns vector and release the vector.
    
    	* gcc.dg/gomp/pr104517.c: New test.
    
    (cherry picked from commit 6a0d6e7ca9b9e338e82572db79c26168684a7441)

---
+++ gcc/testsuite/gcc.dg/gomp/pr104517.c
@@ -0,0 +1,53 @@
+/* PR debug/104517 */
+/* { dg-do compile } */
+/* { dg-options "-O1 -fcompare-debug -fopenmp -fno-tree-ter -save-temps" } */
+
+enum {
+  omp_default_mem_alloc,
+  omp_large_cap_mem_alloc,
+  omp_const_mem_alloc,
+  omp_high_bw_mem_alloc
+} omp_allocator_handle_t;
+
+int t, bar_nte, bar_tl, bar_i3, bar_dd;
+
+#pragma omp threadprivate(t)
+#pragma omp declare target
+int f, l, ll, r, r2;
+#pragma omp end declare target
+
+void
+bar (int *idp, int s, int nth, int g, int nta, int fi, int pp, int *q,
+     int ntm)
+{
+  int p = 0, i2 = 0, i1 = 0, m = 0, d = 0;
+
+#pragma omp target parallel for                               \
+  device(p) firstprivate (f) allocate (f)
+  for (int i = 0; i < 4; i++)
+    ll++;
+
+#pragma omp target parallel for                                         \
+  device(d) map (m)                                                     \
+  if (target: p) firstprivate (f) defaultmap(tofrom: scalar) is_device_ptr (idp) \
+  if (parallel: i2) reduction(+:r) num_threads (nth) linear (ll)        \
+  schedule(static) collapse(1) nowait depend(inout: d) allocate (f)
+  for (int i = 0; i < 4; i++)
+    ll++;
+
+#pragma omp taskloop simd firstprivate(f) lastprivate(s) grainsize(g) \
+  collapse(1) untied if (i1) final(fi) mergeable nogroup              \
+  priority(pp) linear(ll) aligned(q) allocate(f)
+  for (int i = 0; i < 4; i++)
+    ll++;
+
+#pragma omp taskloop simd firstprivate(f) lastprivate(s) num_tasks(nta) \
+  collapse(1) if (i1) final(fi) priority(pp) safelen(8) simdlen(4)      \
+  linear(ll) aligned(q) nontemporal(ntm) order(concurrent) allocate(f)
+  for (int i = 0; i < 4; i++)
+    ll++;
+
+#pragma omp parallel master firstprivate(f) shared(nth) proc_bind(spread) \
+  copyin(t) allocate(f)
+  ;
+}
