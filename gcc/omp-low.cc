6a0d6e7ca9b9e338e82572db79c26168684a7441
Jakub Jelinek <jakub@redhat.com>
2022-02-15 10:22:30 +0100

    openmp: Make finalize_task_copyfn order reproduceable [PR104517]
    The following testcase fails -fcompare-debug, because finalize_task_copyfn
    was invoked from splay tree destruction, whose order can in some cases
    depend on -g/-g0.  The fix is to queue the task stmts that need copyfn
    in a vector and run finalize_task_copyfn on elements of that vector.
    
    2022-02-15  Jakub Jelinek  <jakub@redhat.com>
    
    	PR debug/104517
    	* omp-low.cc (task_cpyfns): New variable.
    	(delete_omp_context): Don't call finalize_task_copyfn from here.
    	(create_task_copyfn): Push task_stmt into task_cpyfns.
    	(execute_lower_omp): Call finalize_task_copyfn here on entries from
    	task_cpyfns vector and release the vector.
    
    	* gcc.dg/gomp/pr104517.c: New test.

--- gcc/omp-low.cc
+++ gcc/omp-low.cc
@@ -191,6 +191,7 @@ static int target_nesting_level;
 static bitmap task_shared_vars;
 static bitmap global_nonaddressable_vars;
 static vec<omp_context *> taskreg_contexts;
+static vec<gomp_task *> task_cpyfns;
 
 static void scan_omp (gimple_seq *, omp_context *);
 static tree scan_omp_1_op (tree *, int *, void *);
@@ -1081,9 +1082,6 @@ delete_omp_context (splay_tree_value value)
       for (t = TYPE_FIELDS (ctx->srecord_type); t ; t = DECL_CHAIN (t))
 	DECL_ABSTRACT_ORIGIN (t) = NULL;
     }
-
-  if (is_task_ctx (ctx))
-    finalize_task_copyfn (as_a <gomp_task *> (ctx->stmt));
 
   if (ctx->task_reduction_map)
     {
@@ -11951,6 +11949,7 @@ create_task_copyfn (gomp_task *task_stmt, omp_context *ctx)
   size_t looptempno = 0;
 
   child_fn = gimple_omp_task_copy_fn (task_stmt);
+  task_cpyfns.safe_push (task_stmt);
   child_cfun = DECL_STRUCT_FUNCTION (child_fn);
   gcc_assert (child_cfun->cfg == NULL);
   DECL_SAVED_TREE (child_fn) = alloc_stmt_list ();
@@ -14475,6 +14474,10 @@ execute_lower_omp (void)
       && (TREE_CODE (TREE_TYPE (DECL_ARGUMENTS (current_function_decl)))
 	  == POINTER_TYPE))
     remove_member_access_dummy_vars (DECL_INITIAL (current_function_decl));
+
+  for (auto task_stmt : task_cpyfns)
+    finalize_task_copyfn (task_stmt);
+  task_cpyfns.release ();
   return 0;
 }
 
