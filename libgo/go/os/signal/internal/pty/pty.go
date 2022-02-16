24ca97325cab7bc454c785d55f37120fe7ea6f74
Ian Lance Taylor <iant@golang.org>
2022-02-16 11:30:04 -0800

    libgo: restore building on Solaris
    Add build tags and a few other changes so that libgo builds on Solaris.
    
    Patch partially from Rainer Orth.
    
    Reviewed-on: https://go-review.googlesource.com/c/gofrontend/+/386215

--- libgo/go/os/signal/internal/pty/pty.go
+++ libgo/go/os/signal/internal/pty/pty.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (aix || darwin || dragonfly || freebsd || hurd || (linux && !android) || netbsd || openbsd) && cgo
+//go:build (aix || darwin || dragonfly || freebsd || hurd || (linux && !android) || netbsd || openbsd || solaris) && cgo
 
 // Package pty is a simple pseudo-terminal package for Unix systems,
 // implemented by calling C functions via cgo.
