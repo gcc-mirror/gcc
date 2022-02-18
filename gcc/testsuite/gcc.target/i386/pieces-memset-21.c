1931cbad498e625b1e24452dcfffe02539b12224
H.J. Lu <hjl.tools@gmail.com>
2022-02-18 10:36:53 -0800

    pieces-memset-21.c: Expect vzeroupper for ia32
    Update gcc.target/i386/pieces-memset-21.c to expect vzeroupper for ia32
    caused by
    
    commit fe79d652c96b53384ddfa43e312cb0010251391b
    Author: Richard Biener <rguenther@suse.de>
    Date:   Thu Feb 17 14:40:16 2022 +0100
    
        target/104581 - compile-time regression in mode-switching
    
    	PR target/104581
    	* gcc.target/i386/pieces-memset-21.c: Expect vzeroupper for ia32.

--- gcc/testsuite/gcc.target/i386/pieces-memset-21.c
+++ gcc/testsuite/gcc.target/i386/pieces-memset-21.c
@@ -11,7 +11,8 @@ foo (void)
 
 /* { dg-final { scan-assembler-times "vpxor(?:d|)\[ \\t\]+\[^\n\]*%xmm" 1 } } */
 /* { dg-final { scan-assembler-times "vmovdqu(?:64|8)\[ \\t\]+\[^\n\]*%zmm" 1 } } */
-/* { dg-final { scan-assembler-not "vzeroupper" } } */
+/* { dg-final { scan-assembler-not "vzeroupper" { target { ! ia32 } } } } */
+/* { dg-final { scan-assembler "vzeroupper" { target ia32 } } } */
 /* No need to dynamically realign the stack here.  */
 /* { dg-final { scan-assembler-not "and\[^\n\r]*%\[re\]sp" } } */
 /* Nor use a frame pointer.  */
