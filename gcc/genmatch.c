bfa476528ceeac96865a48c49f3f1a15d566d209
Richard Biener <rguenther@suse.de>
2022-02-23 13:47:01 +0100

    middle-end/109505 - backport match.pd ! support for GENERIC
    The patch adds support for the ! modifier to GENERIC, backported
    from r12-7361-gfdc46830f1b793.
    
    2023-06-02  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/109505
    	* doc/match-and-simplify.texi: Amend ! documentation.
    	* genmatch.c (expr::gen_transform): Code-generate ! support
    	for GENERIC.
    	(parser::parse_expr): Allow ! for GENERIC.

--- gcc/genmatch.c
+++ gcc/genmatch.c
@@ -2554,19 +2554,20 @@ expr::gen_transform (FILE *f, int indent, const char *dest, bool gimple,
 	fprintf_indent (f, indent, "_r%d = fold_build%d_loc (loc, %s, %s",
 			depth, ops.length(), opr_name, type);
       else
-	{
-	  fprintf_indent (f, indent, "{\n");
-	  fprintf_indent (f, indent, "  _r%d = maybe_build_call_expr_loc (loc, "
-			  "%s, %s, %d", depth, opr_name, type, ops.length());
-	}
+	fprintf_indent (f, indent, "_r%d = maybe_build_call_expr_loc (loc, "
+			"%s, %s, %d", depth, opr_name, type, ops.length());
       for (unsigned i = 0; i < ops.length (); ++i)
 	fprintf (f, ", _o%d[%u]", depth, i);
       fprintf (f, ");\n");
       if (opr->kind != id_base::CODE)
 	{
-	  fprintf_indent (f, indent, "  if (!_r%d)\n", depth);
-	  fprintf_indent (f, indent, "    goto %s;\n", fail_label);
-	  fprintf_indent (f, indent, "}\n");
+	  fprintf_indent (f, indent, "if (!_r%d)\n", depth);
+	  fprintf_indent (f, indent, "  goto %s;\n", fail_label);
+	}
+      if (force_leaf)
+	{
+	  fprintf_indent (f, indent, "if (EXPR_P (_r%d))\n", depth);
+	  fprintf_indent (f, indent, "  goto %s;\n", fail_label);
 	}
       if (*opr == CONVERT_EXPR)
 	{
@@ -4300,9 +4301,6 @@ parser::parse_expr ()
       && token->type == CPP_NOT
       && !(token->flags & PREV_WHITE))
     {
-      if (!gimple)
-	fatal_at (token, "forcing simplification to a leaf is not supported "
-		  "for GENERIC");
       eat_token (CPP_NOT);
       e->force_leaf = true;
     }
