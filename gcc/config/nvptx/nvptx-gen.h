d59d13c89503baf92d14b04c05708a6296916fad
Tom de Vries <tdevries@suse.de>
2022-02-25 11:49:01 +0100

    [nvptx] Add nvptx-gen.h and nvptx-gen.opt
    Use nvptx-sm.def to generate new files nvptx-gen.h and nvptx-gen.opt, and:
    - include nvptx-gen.h in nvptx.h, and
    - add nvptx-gen.opt to extra_options (before nvptx.opt, in case that matters).
    
    Tested on nvptx.
    
    gcc/ChangeLog:
    
    2022-02-25  Tom de Vries  <tdevries@suse.de>
    
    	* config.gcc (nvptx*-*-*): Add nvptx/nvptx-gen.opt to extra_options.
    	* config/nvptx/gen-copyright.sh: New file.
    	* config/nvptx/gen-h.sh: New file.
    	* config/nvptx/gen-opt.sh: New file.
    	* config/nvptx/nvptx.h (TARGET_SM35, TARGET_SM53, TARGET_SM70)
    	(TARGET_SM75, TARGET_SM80): Move ...
    	* config/nvptx/nvptx-gen.h: ... here.  New file, generate.
    	* config/nvptx/nvptx.opt (Enum ptx_isa): Move ...
    	* config/nvptx/nvptx-gen.opt: ... here.  New file, generate.
    	* config/nvptx/t-nvptx ($(srcdir)/config/nvptx/nvptx-gen.h)
    	($(srcdir)/config/nvptx/nvptx-gen.opt): New make target.

---
+++ gcc/config/nvptx/nvptx-gen.h
@@ -0,0 +1,29 @@
+/* -*- buffer-read-only: t -*-
+   Generated automatically by gen-h.sh from nvptx-sm.def.
+*/
+
+/* Copyright (C) 2022 Free Software Foundation, Inc.
+
+   This file is part of GCC.
+
+   GCC is free software; you can redistribute it and/or modify it under
+   the terms of the GNU General Public License as published by the Free
+   Software Foundation; either version 3, or (at your option) any later
+   version.
+
+   GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+   for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GCC; see the file COPYING3.  If not see
+   <http://www.gnu.org/licenses/>.
+*/
+
+#define TARGET_SM30 (ptx_isa_option >= PTX_ISA_SM30)
+#define TARGET_SM35 (ptx_isa_option >= PTX_ISA_SM35)
+#define TARGET_SM53 (ptx_isa_option >= PTX_ISA_SM53)
+#define TARGET_SM70 (ptx_isa_option >= PTX_ISA_SM70)
+#define TARGET_SM75 (ptx_isa_option >= PTX_ISA_SM75)
+#define TARGET_SM80 (ptx_isa_option >= PTX_ISA_SM80)
