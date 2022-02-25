3f5a3b7539e066b539e81b901687facdea4e1bac
Thomas Schwinge <thomas@codesourcery.com>
2022-02-25 12:55:05 +0100

    Fix 'libgomp.c/declare-variant-3-*.c' compilation for configurations where GCN offloading is enabled in addition to nvptx
    The GCN offloading compiler doesn't like '-misa=sm_30' etc.; restrict to
    '-foffload=nvptx-none' compilation only.
    
    Fix-up for commit 59b8ade88774b4dcf1691a8f650cdbb86cc30862
    "[libgomp, testsuite, nvptx] Add libgomp.c/declare-variant-3-sm*.c".
    
    	libgomp/
    	* testsuite/libgomp.c/declare-variant-3-sm30.c:
    	'dg-additional-options -foffload=nvptx-none'.
    	* testsuite/libgomp.c/declare-variant-3-sm35.c: Likewise.
    	* testsuite/libgomp.c/declare-variant-3-sm53.c: Likewise.
    	* testsuite/libgomp.c/declare-variant-3-sm70.c: Likewise.
    	* testsuite/libgomp.c/declare-variant-3-sm75.c: Likewise.
    	* testsuite/libgomp.c/declare-variant-3-sm80.c: Likewise.

--- libgomp/testsuite/libgomp.c/declare-variant-3-sm30.c
+++ libgomp/testsuite/libgomp.c/declare-variant-3-sm30.c
@@ -1,4 +1,5 @@
 /* { dg-do run { target { offload_target_nvptx } } } */
+/* { dg-additional-options -foffload=nvptx-none } */
 /* { dg-additional-options "-foffload=-misa=sm_30 -foffload=-mptx=_" } */
 /* { dg-additional-options "-foffload=-fdump-tree-optimized" } */
 
