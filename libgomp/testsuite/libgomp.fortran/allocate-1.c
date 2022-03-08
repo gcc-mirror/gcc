wenyiwang-us/gnu-openmp-xtask

Add support for allocate clause (OpenMP 5.0).
This patch adds support for OpenMP 5.0 allocate clause for fortran. It does not
yet support the allocator-modifier as specified in OpenMP 5.1. The allocate
clause is already supported in C/C++.

gcc/fortran/ChangeLog:

	* dump-parse-tree.c (show_omp_clauses): Handle OMP_LIST_ALLOCATE.
	* gfortran.h (OMP_LIST_ALLOCATE): New enum value.
	* openmp.c (enum omp_mask1): Add OMP_CLAUSE_ALLOCATE.
	(gfc_match_omp_clauses): Handle OMP_CLAUSE_ALLOCATE
	(OMP_PARALLEL_CLAUSES, OMP_DO_CLAUSES, OMP_SECTIONS_CLAUSES)
	(OMP_TASK_CLAUSES, OMP_TASKLOOP_CLAUSES, OMP_TARGET_CLAUSES)
	(OMP_TEAMS_CLAUSES, OMP_DISTRIBUTE_CLAUSES)
	(OMP_SINGLE_CLAUSES): Add OMP_CLAUSE_ALLOCATE.
	(OMP_TASKGROUP_CLAUSES): New.
	(gfc_match_omp_taskgroup): Use OMP_TASKGROUP_CLAUSES instead of
	OMP_CLAUSE_TASK_REDUCTION.
	(resolve_omp_clauses): Handle OMP_LIST_ALLOCATE.
	(resolve_omp_do): Avoid warning when loop iteration variable is
	in allocate clause.
	* trans-openmp.c (gfc_trans_omp_clauses): Handle translation of
	allocate clause.
	(gfc_split_omp_clauses): Update for OMP_LIST_ALLOCATE.

gcc/testsuite/ChangeLog:

	* gfortran.dg/gomp/allocate-1.f90: New test.
	* gfortran.dg/gomp/allocate-2.f90: New test.
	* gfortran.dg/gomp/allocate-3.f90: New test.
	* gfortran.dg/gomp/collapse1.f90: Update error message.
	* gfortran.dg/gomp/openmp-simd-4.f90: Likewise.
	* gfortran.dg/gomp/clauses-1.f90: Uncomment allocate clause.

libgomp/ChangeLog:

	* testsuite/libgomp.fortran/allocate-1.c: New test.
	* testsuite/libgomp.fortran/allocate-1.f90: New test.
	* libgomp.texi: Remove string that says that allocate clause
	support is for C/C++ only.

libgomp/testsuite/libgomp.fortran/allocate-1.c
@@ -1,0 +1,7 @@
+#include <stdint.h>
+
+int
+is_64bit_aligned_ (uintptr_t a)
+{
+  return ( (a & 0x3f) == 0);
+}

gcc-mirror/gcc

This commit contains 4 following upstream commits.
69561fc781a Add support for allocate clause (OpenMP 5.0).
deb9f18f677 Change kind of integer literal to fix a testcase.
6d498135016 libgomp.fortran/allocate-1.f90: Minor cleanup
f62156eab7b libgomp.fortran/allocate-1.f90: Fix minor cleanup
(cherry picked from commit 69561fc781aca3dea3aa4d5d562ef5a502965924)
Change kind of integer literal to fix a testcase.
As Thomas reported in
https://gcc.gnu.org/pipermail/gcc-patches/2022-January/588448.html
a test added in my recent allocate clause patch fails on m32. It was due
to default kind for integer matching c_intptr_t for m32. I have now
changed it to 0_1 so that always integer with kind=1 is used.
	* gfortran.dg/gomp/allocate-2.f90: Change 0 to 0_1.
(cherry picked from commit deb9f18f67788c36f4652bca101d93faf07ecf39)
libgomp.fortran/allocate-1.f90: Minor cleanup
	* testsuite/libgomp.fortran/allocate-1.c (is_64bit_aligned): Renamed
	from is_64bit_aligned_.
	* testsuite/libgomp.fortran/allocate-1.f90: Fix interface decl
	and use it, more implicit none, remove unused argument.
(cherry picked from commit 6d4981350168f1eb3f72149bd7e05b9ba6bec1fd)
libgomp.fortran/allocate-1.f90: Fix minor cleanup
	* testsuite/libgomp.fortran/allocate-1.f90: Remove spurious
	STOP of previous commit.
(cherry picked from commit f62156eab7b757d1ee03a11d5c96c72bd3de079c)

libgomp/testsuite/libgomp.fortran/allocate-1.c
@@ -1,0 +1,7 @@
+#include <stdint.h>
+
+int
+is_64bit_aligned (uintptr_t a)
+{
+  return ( (a & 0x3f) == 0);
+}