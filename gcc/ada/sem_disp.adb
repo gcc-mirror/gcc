e6bdaa0299d459e4f0486793bcbeff1bbe75f8a1
Javier Miranda <miranda@adacore.com>
2022-03-08 14:55:16 -0800

    [Ada] Accept calls to abstract subprograms in class-wide pre/post-conditions
    Fix a regression in the support for Ada 2022's treatment of calls to
    abstract subprograms in pre/post-conditions (thanks to Javier Miranda
    for producing this patch).
    
    gcc/ada/
    
    	* sem_disp.adb (Check_Dispatching_Context): When checking to see
    	whether an expression occurs in a class-wide pre/post-condition,
    	also check for the possibility that it occurs in a class-wide
    	preconditions subprogram that was introduced as part of
    	expansion. Without this fix, some legal calls occuring in
    	class-wide preconditions may be incorrectly flagged as violating
    	the "a call to an abstract subprogram must be dispatching" rule.

--- gcc/ada/sem_disp.adb
+++ gcc/ada/sem_disp.adb
@@ -751,14 +751,22 @@ package body Sem_Disp is
 
             elsif Is_Subprogram (Scop)
               and then not Is_Tag_Indeterminate (N)
-              and then In_Pre_Post_Condition (Call, Class_Wide_Only => True)
-
-              --  The tagged type associated with the called subprogram must be
-              --  the same as that of the subprogram with a class-wide aspect.
-
-              and then Is_Dispatching_Operation (Scop)
               and then
-                Find_Dispatching_Type (Subp) = Find_Dispatching_Type (Scop)
+               --  The context is an internally built helper or an indirect
+               --  call wrapper that handles class-wide preconditions
+                 (Present (Class_Preconditions_Subprogram (Scop))
+
+               --  ... or the context is a class-wide pre/postcondition.
+                   or else
+                     (In_Pre_Post_Condition (Call, Class_Wide_Only => True)
+
+                       --  The tagged type associated with the called
+                       --  subprogram must be the same as that of the
+                       --  subprogram with a class-wide aspect.
+
+                       and then Is_Dispatching_Operation (Scop)
+                       and then Find_Dispatching_Type (Subp)
+                                  = Find_Dispatching_Type (Scop)))
             then
                null;
 
