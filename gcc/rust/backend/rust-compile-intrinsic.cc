9e23c29cd0f95f4e6102f770965079546aebd96c
Philip Herron <philip.herron@embecosm.com>
2022-03-09 16:15:19 +0000

    Add builtin unreachable intrinsic mapping
    This demonstrates how we can add in the simple intrinsics in a single
    patch.
    
    Addresses #658

--- gcc/rust/backend/rust-compile-intrinsic.cc
+++ gcc/rust/backend/rust-compile-intrinsic.cc
@@ -108,6 +108,11 @@ private:
 
     define_builtin ("sqrtf32", BUILT_IN_SQRTF, "__builtin_sqrtf", "sqrtf",
 		    math_function_type_f32, builtin_const);
+
+    define_builtin ("unreachable", BUILT_IN_UNREACHABLE,
+		    "__builtin_unreachable", NULL,
+		    build_function_type (void_type_node, void_list_node),
+		    builtin_const | builtin_noreturn);
   }
 
   // Define a builtin function.  BCODE is the builtin function code
