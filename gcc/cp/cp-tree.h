9413bb55185b9e88d84e91d5145d59f9f83b884a
Patrick Palka <ppalka@redhat.com>
2022-03-12 15:00:40 -0500

    c++: return-type-req in constraint using only outer tparms [PR104527]
    Here the template context for the atomic constraint has two levels of
    template parameters, but since it depends only on the innermost parameter
    T we use a single-level argument vector (built by get_mapped_args) during
    substitution into the atom.  We eventually pass this vector to
    do_auto_deduction as part of checking the return-type-requirement within
    the atom, but do_auto_deduction expects outer_targs to be a full set of
    arguments for sake of satisfaction.
    
    This patch fixes this by making get_mapped_args always return an
    argument vector whose depth corresponds to the template depth of the
    context in which the atomic constraint expression was written, instead
    of the highest parameter level that the expression happens to use.
    
    	PR c++/104527
    
    gcc/cp/ChangeLog:
    
    	* constraint.cc (normalize_atom): Set
    	ATOMIC_CONSTR_EXPR_FROM_CONCEPT_P appropriately.
    	(get_mapped_args):  Make static, adjust parameters.  Always
    	return a vector whose depth corresponds to the template depth of
    	the context of the atomic constraint expression.  Micro-optimize
    	by passing false as exact to safe_grow_cleared and by collapsing
    	a multi-level depth-one argument vector.
    	(satisfy_atom): Adjust call to get_mapped_args and
    	diagnose_atomic_constraint.
    	(diagnose_atomic_constraint): Replace map parameter with an args
    	parameter.
    	* cp-tree.h (ATOMIC_CONSTR_EXPR_FROM_CONCEPT_P): Define.
    	(get_mapped_args): Remove declaration.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp2a/concepts-return-req4.C: New test.

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -466,6 +466,7 @@ extern GTY(()) tree cp_global_trees[CPTI_MAX];
       IMPLICIT_CONV_EXPR_NONTYPE_ARG (in IMPLICIT_CONV_EXPR)
       BASELINK_FUNCTIONS_MAYBE_INCOMPLETE_P (in BASELINK)
       BIND_EXPR_VEC_DTOR (in BIND_EXPR)
+      ATOMIC_CONSTR_EXPR_FROM_CONCEPT_P (in ATOMIC_CONSTR)
    2: IDENTIFIER_KIND_BIT_2 (in IDENTIFIER_NODE)
       ICS_THIS_FLAG (in _CONV)
       DECL_INITIALIZED_BY_CONSTANT_EXPRESSION_P (in VAR_DECL)
@@ -1678,6 +1679,11 @@ check_constraint_info (tree t)
    Used only in satisfy_atom and in the satisfaction cache.  */
 #define ATOMIC_CONSTR_MAP_INSTANTIATED_P(NODE) \
   TREE_LANG_FLAG_0 (ATOMIC_CONSTR_CHECK (NODE))
+
+/* Whether the expression for this atomic constraint belongs to a
+   concept definition.  */
+#define ATOMIC_CONSTR_EXPR_FROM_CONCEPT_P(NODE) \
+  TREE_LANG_FLAG_1 (ATOMIC_CONSTR_CHECK (NODE))
 
 /* The expression of an atomic constraint. */
 #define ATOMIC_CONSTR_EXPR(NODE) \
@@ -8315,7 +8321,6 @@ extern tree evaluate_requires_expr		(tree);
 extern tree tsubst_constraint                   (tree, tree, tsubst_flags_t, tree);
 extern tree tsubst_constraint_info              (tree, tree, tsubst_flags_t, tree);
 extern tree tsubst_parameter_mapping		(tree, tree, tsubst_flags_t, tree);
-extern tree get_mapped_args			(tree);
 
 struct processing_constraint_expression_sentinel
 {
