bec69ac548b0f37b41d07082d6ee52b52d356536
H.J. Lu <hjl.tools@gmail.com>
2022-03-21 13:57:31 -0700

    x86: Disable AVX on pr86722.c and pr90356.c
    SSE/SSE2 are enabled explicitly on pr86722.c and pr90356.c.  Disable AVX
    to avoid AVX with -march=native.
    
    	PR target/86722
    	PR tree-optimization/90356
    	* gcc.target/i386/pr86722.c: Add -mno-avx.
    	* gcc.target/i386/pr90356.c: Likewise.

--- gcc/testsuite/gcc.target/i386/pr86722.c
+++ gcc/testsuite/gcc.target/i386/pr86722.c
@@ -1,5 +1,5 @@
 /* { dg-do compile { target { ! ia32 } } } */
-/* { dg-options "-O2 -msse" } */
+/* { dg-options "-O2 -mno-avx -msse" } */
 
 void f(double*d,double*e){
   for(;d<e;++d)
@@ -8,4 +8,3 @@ void f(double*d,double*e){
 
 /* { dg-final { scan-assembler-not "andnpd" } } */
 /* { dg-final { scan-assembler-not "orpd" } } */
-
--- gcc/testsuite/gcc.target/i386/pr90356.c
+++ gcc/testsuite/gcc.target/i386/pr90356.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -msse2 -mfpmath=sse" } */
+/* { dg-options "-O2 -mno-avx -msse2 -mfpmath=sse" } */
 float doit(float k){
     float c[2]={0.0};
     c[1]+=k;
