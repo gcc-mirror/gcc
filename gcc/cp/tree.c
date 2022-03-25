3bf2e1bfc9858516e028137b313e4c689b0c8cd4
Jason Merrill <jason@redhat.com>
2022-03-25 11:26:06 -0400

    c++: ICE with alias in pack expansion [PR103769]
    This was breaking because when we stripped the 't' typedef in s<t<Args>...>
    to be s<Args...>, the TYPE_MAIN_VARIANT of "Args..." was still
    "t<Args>...", because type pack expansions are treated as types.  Fixed by
    using the right function to copy a "type".
    
    	PR c++/99445
    	PR c++/103769
    
    gcc/cp/ChangeLog:
    
    	* tree.c (strip_typedefs): Use build_distinct_type_copy.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp0x/variadic-alias5.C: New test.

--- gcc/cp/tree.c
+++ gcc/cp/tree.c
@@ -1745,7 +1745,7 @@ strip_typedefs (tree t, bool *remove_attributes, unsigned int flags)
 	    type = strip_typedefs (pat, remove_attributes, flags);
 	    if (type != pat)
 	      {
-		result = copy_node (t);
+		result = build_distinct_type_copy (t);
 		PACK_EXPANSION_PATTERN (result) = type;
 	      }
 	  }
