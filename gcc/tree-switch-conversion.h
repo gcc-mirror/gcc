57b62cd299a0ec257141349a114e52f7233be67b
Richard Biener <rguenther@suse.de>
2022-03-28 10:07:53 +0200

    tree-optimization/105070 - annotate bit cluster tests with locations
    The following makes sure to annotate the tests generated by
    switch lowering bit-clustering with locations which otherwise
    can be completely lost even at -O0.
    
    2022-03-28  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/105070
    	* tree-switch-conversion.h
    	(bit_test_cluster::hoist_edge_and_branch_if_true): Add location
    	argument.
    	* tree-switch-conversion.c
    	(bit_test_cluster::hoist_edge_and_branch_if_true): Annotate
    	cond with location.
    	(bit_test_cluster::emit): Annotate all generated expressions
    	with location.
    
    (cherry picked from commit bc86a86a4f2c057bc0e0be94dcbb8c128ae7f717)

--- gcc/tree-switch-conversion.h
+++ gcc/tree-switch-conversion.h
@@ -429,7 +429,8 @@ public:
   static basic_block hoist_edge_and_branch_if_true (gimple_stmt_iterator *gsip,
 						    tree cond,
 						    basic_block case_bb,
-						    profile_probability prob);
+						    profile_probability prob,
+						    location_t);
 
   /* Return whether bit test expansion is allowed.  */
   static inline bool is_enabled (void)
