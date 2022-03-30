e3d2b0d040e9baf6c0548b865ed5244dec464cc1
Alexandre Oliva <oliva@adacore.com>
2022-03-29 22:47:19 -0300

    analyzer/strndup-1.c: skip on *-*-vxworks*
    Add vxworks to the set of operating systems whose C libraries don't
    support strndup.
    
    
    for  gcc/testsuite/ChangeLog
    
    	* gcc.dg/analyzer/strndup-1.c: Add *-*-vxworks* to no-strndup
    	in libc.

--- gcc/testsuite/gcc.dg/analyzer/strndup-1.c
+++ gcc/testsuite/gcc.dg/analyzer/strndup-1.c
@@ -1,4 +1,5 @@
-/* { dg-skip-if "no strndup in libc" { *-*-darwin[789]* *-*-darwin10* *-*-mingw* } } */
+/* { dg-skip-if "no strndup in libc" { *-*-darwin[789]* *-*-darwin10* *-*-mingw* *-*-vxworks* } } */
+
 #include <string.h>
 #include <stdlib.h>
 
