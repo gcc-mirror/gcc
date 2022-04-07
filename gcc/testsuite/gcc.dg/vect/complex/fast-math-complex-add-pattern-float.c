68163cc2de1d3125ed9dbb8eac3294d1f9118641
Tamar Christina <tamar.christina@arm.com>
2022-04-07 19:53:43 +0100

    testsuite: delete slp scan from loop vect test.
    I accidentally left in an slp1 check in the vect test which showed up as
    UNRESOLVED and had missed it in the sum file.  This deletes that line.
    
    gcc/testsuite/ChangeLog:
    
    	PR testsuite/105196
    	* gcc.dg/vect/complex/fast-math-complex-add-pattern-float.c: Remove slp1 check.

--- gcc/testsuite/gcc.dg/vect/complex/fast-math-complex-add-pattern-float.c
+++ gcc/testsuite/gcc.dg/vect/complex/fast-math-complex-add-pattern-float.c
@@ -9,5 +9,4 @@
 /* { dg-final { scan-tree-dump-times "stmt.*COMPLEX_ADD_ROT90" 1 "vect" { target { vect_complex_add_float } } } } */
 /* { dg-final { scan-tree-dump-times "stmt.*COMPLEX_ADD_ROT270" 1 "vect" { target { vect_complex_add_float } } } } */
 /* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT270" "vect" } } */
-/* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT90" "slp1" } } */
 /* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT90" "vect" } } */
