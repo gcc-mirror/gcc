82a4c5c704433249aa2adc89ef58b6b70e50c930
Andreas Krebbel <krebbel@linux.ibm.com>
2022-04-12 07:41:33 +0200

    IBM zSystems: Add support for z16 as CPU name.
    So far z16 was identified as arch14. After the machine has been
    announced we can now add the real name.
    
    gcc/ChangeLog:
    
    	* common/config/s390/s390-common.cc: Rename PF_ARCH14 to PF_Z16.
    	* config.gcc: Add z16 as march/mtune switch.
    	* config/s390/driver-native.cc (s390_host_detect_local_cpu):
    	Recognize z16 with -march=native.
    	* config/s390/s390-opts.h (enum processor_type): Rename
    	PROCESSOR_ARCH14 to PROCESSOR_3931_Z16.
    	* config/s390/s390.cc (PROCESSOR_ARCH14): Rename to ...
    	(PROCESSOR_3931_Z16): ... throughout the file.
    	(s390_processor processor_table): Add z16 as cpu string.
    	* config/s390/s390.h (enum processor_flags): Rename PF_ARCH14 to
    	PF_Z16.
    	(TARGET_CPU_ARCH14): Rename to ...
    	(TARGET_CPU_Z16): ... this.
    	(TARGET_CPU_ARCH14_P): Rename to ...
    	(TARGET_CPU_Z16_P): ... this.
    	(TARGET_ARCH14): Rename to ...
    	(TARGET_Z16): ... this.
    	(TARGET_ARCH14_P): Rename to ...
    	(TARGET_Z16_P): ... this.
    	* config/s390/s390.md (cpu_facility): Rename arch14 to z16 and
    	check TARGET_Z16 instead of TARGET_ARCH14.
    	* config/s390/s390.opt: Add z16 to processor_type.
    	* doc/invoke.texi: Document z16 and arch14.

--- gcc/config/s390/s390.cc
+++ gcc/config/s390/s390.cc
@@ -337,7 +337,7 @@ const struct s390_processor processor_table[] =
   { "z13",    "z13",    PROCESSOR_2964_Z13,    &zEC12_cost,  11 },
   { "z14",    "arch12", PROCESSOR_3906_Z14,    &zEC12_cost,  12 },
   { "z15",    "arch13", PROCESSOR_8561_Z15,    &zEC12_cost,  13 },
-  { "arch14", "arch14", PROCESSOR_ARCH14,      &zEC12_cost,  14 },
+  { "z16",    "arch14", PROCESSOR_3931_Z16,    &zEC12_cost,  14 },
   { "native", "",       PROCESSOR_NATIVE,      NULL,         0  }
 };
 
@@ -851,12 +851,6 @@ s390_expand_builtin (tree exp, rtx target, rtx subtarget ATTRIBUTE_UNUSED,
       if ((bflags & B_VXE2) && !TARGET_VXE2)
 	{
 	  error ("Builtin %qF requires z15 or higher", fndecl);
-	  return const0_rtx;
-	}
-
-      if ((bflags & B_NNPA) && !TARGET_NNPA)
-	{
-	  error ("Builtin %qF requires arch14 or higher.", fndecl);
 	  return const0_rtx;
 	}
     }
@@ -8525,7 +8519,7 @@ s390_issue_rate (void)
     case PROCESSOR_2827_ZEC12:
     case PROCESSOR_2964_Z13:
     case PROCESSOR_3906_Z14:
-    case PROCESSOR_ARCH14:
+    case PROCESSOR_3931_Z16:
     default:
       return 1;
     }
@@ -14879,7 +14873,7 @@ s390_get_sched_attrmask (rtx_insn *insn)
 	mask |= S390_SCHED_ATTR_MASK_GROUPOFTWO;
       break;
     case PROCESSOR_8561_Z15:
-    case PROCESSOR_ARCH14:
+    case PROCESSOR_3931_Z16:
       if (get_attr_z15_cracked (insn))
 	mask |= S390_SCHED_ATTR_MASK_CRACKED;
       if (get_attr_z15_expanded (insn))
@@ -14927,7 +14921,7 @@ s390_get_unit_mask (rtx_insn *insn, int *units)
 	mask |= 1 << 3;
       break;
     case PROCESSOR_8561_Z15:
-    case PROCESSOR_ARCH14:
+    case PROCESSOR_3931_Z16:
       *units = 4;
       if (get_attr_z15_unit_lsu (insn))
 	mask |= 1 << 0;
