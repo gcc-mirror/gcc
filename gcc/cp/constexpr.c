d939233ef460133e012d8f40f9d8c8fcb73bb7b8
Jason Merrill <jason@redhat.com>
2022-04-14 15:34:14 -0400

    c++: constexpr trivial -fno-elide-ctors [PR104646]
    The constexpr constructor checking code got confused by the expansion of a
    trivial copy constructor; we don't need to do that checking for defaulted
    ctors, anyway.
    
    	PR c++/104646
    
    gcc/cp/ChangeLog:
    
    	* constexpr.c (maybe_save_constexpr_fundef): Don't do extra
    	checks for defaulted ctors.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp0x/constexpr-fno-elide-ctors1.C: New test.

--- gcc/cp/constexpr.c
+++ gcc/cp/constexpr.c
@@ -898,7 +898,7 @@ register_constexpr_fundef (tree fun, tree body)
   if (!potential && !DECL_GENERATED_P (fun))
     require_potential_rvalue_constant_expression (massaged);
 
-  if (DECL_CONSTRUCTOR_P (fun)
+  if (DECL_CONSTRUCTOR_P (fun) && !DECL_DEFAULTED_FN (fun)
       && cx_check_missing_mem_inits (DECL_CONTEXT (fun),
 				     massaged, !DECL_GENERATED_P (fun)))
     potential = false;
