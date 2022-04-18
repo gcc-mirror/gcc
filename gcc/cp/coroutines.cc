6cae3bb65c873a2191613f7888fe949553a21f9e
Iain Sandoe <iain@sandoe.co.uk>
2022-04-18 09:21:52 +0100

    c++, coroutines: Account for overloaded promise return_value() [PR105301].
    Whether it was intended or not, it is possible to define a coroutine promise
    with multiple return_value() methods [which need not even have the same type].
    
    We were not accounting for this possibility in the check to see whether both
    return_value and return_void are specifier (which is prohibited by the
    standard).  Fixed thus and provided an adjusted diagnostic for the case that
    multiple return_value() methods are present.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>
    
    	PR c++/105301
    
    gcc/cp/ChangeLog:
    
    	* coroutines.cc (coro_promise_type_found_p): Account for possible
    	mutliple overloads of the promise return_value() method.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/coroutines/pr105301.C: New test.

--- gcc/cp/coroutines.cc
+++ gcc/cp/coroutines.cc
@@ -513,8 +513,14 @@ coro_promise_type_found_p (tree fndecl, location_t loc)
 		      coro_info->promise_type);
 	  inform (DECL_SOURCE_LOCATION (BASELINK_FUNCTIONS (has_ret_void)),
 		  "%<return_void%> declared here");
-	  inform (DECL_SOURCE_LOCATION (BASELINK_FUNCTIONS (has_ret_val)),
-		  "%<return_value%> declared here");
+	  has_ret_val = BASELINK_FUNCTIONS (has_ret_val);
+	  const char *message = "%<return_value%> declared here";
+	  if (TREE_CODE (has_ret_val) == OVERLOAD)
+	    {
+	      has_ret_val = OVL_FIRST (has_ret_val);
+	      message = "%<return_value%> first declared here";
+	    }
+	  inform (DECL_SOURCE_LOCATION (has_ret_val), message);
 	  coro_info->coro_co_return_error_emitted = true;
 	  return false;
 	}
