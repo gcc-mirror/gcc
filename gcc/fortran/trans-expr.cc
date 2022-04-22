89ca0fffa48b799b228beee48a16e26e24d8e199
Mikael Morin <mikael@gcc.gnu.org>
2022-04-22 22:52:12 +0200

    fortran: Pre-evaluate string pointers. [PR102043]
    This avoids a regression on deferred_character_23.f90 later in the
    patch series when array references are rewritten to use pointer
    arithmetic.
    
    The problem is a SAVE_EXPR tree as TYPE_SIZE_UNIT of one array element
    type, which is used by the pointer arithmetic expressions.  As these
    expressions appear in both branches of an if-then-else block, the tree
    is lowered to a variable in one of the branches but it’s used in both
    branches, which is invalid middle-end code.
    
    This change pre-evaluates the array references or pointer arithmetics
    to variables before the if-then-else block, so that the SAVE_EXPR are
    expanded to variables in the parent scope of the if-then-else block,
    and expressions referencing the variables remain valid in both
    branches.
    
    	PR fortran/102043
    
    gcc/fortran/ChangeLog:
    	* trans-expr.cc: Pre-evaluate src and dest to variables
    	before using them.
    
    gcc/testsuite/ChangeLog:
    	* gfortran.dg/dependency_49.f90: Update variable occurence
    	count.

--- gcc/fortran/trans-expr.cc
+++ gcc/fortran/trans-expr.cc
@@ -8093,6 +8093,13 @@ gfc_trans_string_copy (stmtblock_t * block, tree dlength, tree dest,
   cond2 = fold_build2_loc (input_location, LT_EXPR, logical_type_node, slen,
 			   dlen);
 
+  /* Pre-evaluate pointers unless one of the IF arms will be optimized away.  */
+  if (!CONSTANT_CLASS_P (cond2))
+    {
+      dest = gfc_evaluate_now (dest, block);
+      src = gfc_evaluate_now (src, block);
+    }
+
   /* Copy and pad with spaces.  */
   tmp3 = build_call_expr_loc (input_location,
 			      builtin_decl_explicit (BUILT_IN_MEMMOVE),
