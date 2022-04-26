288e4c64f6b4806358aabc9b99b2fba72bf04bf6
Patrick Palka <ppalka@redhat.com>
2022-04-25 21:46:56 -0400

    c++: partial ordering with dependent NTTP type [PR105289]
    Here ever since r11-6483-ge2e2f3f2c9400f we're rejecting and crashing
    on (respectively) two testcases that we used to accept in C++17 mode
    since r8-1437-g3da557ec145823.  Both testcases declare a partial
    specialization of a primary template that has an NTTP with dependent
    type, and the validity of these partial specializations is unclear and
    the subject of PR86193 / CWG 455.
    
    So for now, this minimal patch just aims to fix the crash in the second
    testcase.  During deduction, when checking whether the type of an NTTP
    uses still-undeduced parameters, we were incorrectly substituting into
    the previously substituted type instead of into its original type.
    
    In passing this patch also downgrades the "not more specialized"
    diagnostic from a permerror to a pedwarn.
    
    	PR c++/105289
    	PR c++/86193
    
    gcc/cp/ChangeLog:
    
    	* pt.cc (process_partial_specialization): Downgrade "partial
    	specialization isn't more specialized" diagnostic from permerror
    	to an on-by-default pedwarn.
    	(unify) <case TEMPLATE_PARM_INDEX>: When substituting into the
    	NTTP type a second time, use the original type not the
    	substituted type.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/template/partial-specialization11.C: New test.
    	* g++.dg/template/partial-specialization12.C: New test.

--- gcc/cp/pt.cc
+++ gcc/cp/pt.cc
@@ -5227,8 +5227,9 @@ process_partial_specialization (tree decl)
 	   && !get_partial_spec_bindings (maintmpl, maintmpl, specargs))
     {
       auto_diagnostic_group d;
-      if (permerror (input_location, "partial specialization %qD is not "
-		     "more specialized than", decl))
+      if (pedwarn (input_location, 0,
+		   "partial specialization %qD is not more specialized than",
+		   decl))
 	inform (DECL_SOURCE_LOCATION (maintmpl), "primary template %qD",
 		maintmpl);
     }
@@ -24299,7 +24300,7 @@ unify (tree tparms, tree targs, tree parm, tree arg, int strict,
 	      /* Now check whether the type of this parameter is still
 		 dependent, and give up if so.  */
 	      ++processing_template_decl;
-	      tparm = tsubst (tparm, targs, tf_none, NULL_TREE);
+	      tparm = tsubst (TREE_TYPE (parm), targs, tf_none, NULL_TREE);
 	      --processing_template_decl;
 	      if (uses_template_parms (tparm))
 		return unify_success (explain_p);
