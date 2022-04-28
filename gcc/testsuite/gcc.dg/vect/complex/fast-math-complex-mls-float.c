73ceece5363b1a06196191aa6a7254ceab5b9cf4
Tamar Christina <tamar.christina@arm.com>
2022-04-28 12:32:26 +0100

    testsuite: Backport testsuite changes from GCC 12 to GCC 11
    This backports the testsuite changes from mainline to GCC 11.
    This enables some tests that were not running and fix some
    unresolved cases. The test output are adjusted to what GCC 11 could detect.
    
    gcc/testsuite/ChangeLog:
    
    	PR testsuite/105095
    	* gcc.dg/vect/complex/fast-math-bb-slp-complex-mul-float.c: Update for
    	codegen.
    	* gcc.dg/vect/complex/fast-math-complex-add-double.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-complex-add-float.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-complex-add-half-float.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-complex-add-pattern-double.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-complex-add-pattern-float.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-complex-add-pattern-half-float.c:
    	Likewise.
    	* gcc.dg/vect/complex/fast-math-complex-mla-half-float.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-complex-mls-double.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-complex-mls-float.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-complex-mls-half-float.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-complex-mul-double.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-complex-mul-float.c: Likewise.
    	* gcc.dg/vect/complex/fast-math-complex-mul-half-float.c: Likewise.
    	* gcc.dg/vect/vect.exp: Add extra letter to filter.

--- gcc/testsuite/gcc.dg/vect/complex/fast-math-complex-mls-float.c
+++ gcc/testsuite/gcc.dg/vect/complex/fast-math-complex-mls-float.c
@@ -1,10 +1,10 @@
 /* { dg-do compile } */
+/* { dg-require-effective-target vect_complex_add_float } */
 /* { dg-add-options arm_v8_3a_complex_neon } */
 
 #define TYPE float
 #define N 200
 #include "complex-mls-template.c"
-/* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT270" "vect" } } */
 /* { dg-final { scan-tree-dump "Found COMPLEX_FMA" "vect" } } */
 /* { dg-final { scan-tree-dump "Found COMPLEX_FMS_CONJ" "vect" } } */
 /* { dg-final { scan-tree-dump "Found COMPLEX_FMS" "vect" } } */
--- gcc/testsuite/gcc.dg/vect/complex/fast-math-complex-mls-half-float.c
+++ gcc/testsuite/gcc.dg/vect/complex/fast-math-complex-mls-half-float.c
@@ -1,9 +1,10 @@
 /* { dg-do compile } */
+/* { dg-require-effective-target vect_complex_add_half } */
+/* { dg-require-effective-target float16 } */
 /* { dg-add-options arm_v8_3a_fp16_complex_neon } */
 
 #define TYPE _Float16
 #define N 200
 #include "complex-mls-template.c"
-/* { dg-final { scan-tree-dump "Found COMPLEX_ADD_ROT270" "vect" } } */
 /* { dg-final { scan-tree-dump "Found COMPLEX_FMS_CONJ" "vect" } } */
 /* { dg-final { scan-tree-dump "Found COMPLEX_FMS" "vect" } } */
