2fd2e6c1dffe305b10477b83a6c350a3f19e4058
Jakub Jelinek <jakub@redhat.com>
2022-05-04 10:07:36 +0200

    c++: Don't emit deprecated warnings on lambda declarations
    On the following testcase, we emit deprecated warnings
    even on merge declarations of those lambdas (the dg-bogus directives), while
    IMHO we should emit them only when something actually calls those lambdas.
    
    The following patch temporarily disables that diagnostics during
    maybe_add_lambda_conv_op.
    
    PR2173R1 also says that ambiguity between attribute-specifier-seq at the
    end of requires-clause and attribute-specifier-seq from lambda-expression
    should be resolved to attribute-specifier-seq for the latter.  Do we need
    to do anything about that?  I mean, can a valid requires-clause end with
    an attribute-specifier-seq?  Say operator int [[]] is valid primary
    expression, but requires operator int [[]] isn't valid, nor is
    requires operator int, no?
    
    2022-05-04  Jakub Jelinek  <jakub@redhat.com>
    
    	* lambda.c: Include decl.h.
    	(maybe_add_lambda_conv_op): Temporarily override deprecated_state to
    	DEPRECATED_SUPPRESS.
    
    	* g++.dg/cpp23/lambda-attr1.C: New test.
    
    (cherry picked from commit 1c8e9bed9b9d46d479b83ae05b334543f66961fb)

--- gcc/cp/lambda.c
+++ gcc/cp/lambda.c
@@ -31,6 +31,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "toplev.h"
 #include "gimplify.h"
 #include "target.h"
+#include "decl.h"
 
 /* Constructor for a lambda expression.  */
 
@@ -1177,9 +1178,13 @@ maybe_add_lambda_conv_op (tree type)
 	}
     }
   else
-    call = build_call_a (callop,
-			 direct_argvec->length (),
-			 direct_argvec->address ());
+    {
+      /* Don't warn on deprecated lambda declarations, unless
+	 the lambda is actually called.  */
+      auto du = make_temp_override (deprecated_state, DEPRECATED_SUPPRESS);
+      call = build_call_a (callop, direct_argvec->length (),
+			   direct_argvec->address ());
+    }
 
   CALL_FROM_THUNK_P (call) = 1;
   SET_EXPR_LOCATION (call, UNKNOWN_LOCATION);
