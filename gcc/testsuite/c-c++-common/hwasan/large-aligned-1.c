3771486daa1e904ceae6f3e135b28e58af33849f
Martin Liska <mliska@suse.cz>
2022-05-04 09:57:17 +0200

    hwasan: support new dg-output format.
    Supports change in libsanitizer where it newly reports:
    READ of size 4 at 0xffffffffc3d4 tags: 02/01(00) (ptr/mem) in thread T0
    
    So the 'tags' contains now 3 entries compared to 2 entries.
    
    gcc/testsuite/ChangeLog:
    
    	* c-c++-common/hwasan/alloca-outside-caught.c: Update dg-output.
    	* c-c++-common/hwasan/heap-overflow.c: Likewise.
    	* c-c++-common/hwasan/hwasan-thread-access-parent.c: Likewise.
    	* c-c++-common/hwasan/large-aligned-1.c: Likewise.
    	* c-c++-common/hwasan/stack-tagging-basic-1.c: Likewise.

--- gcc/testsuite/c-c++-common/hwasan/large-aligned-1.c
+++ gcc/testsuite/c-c++-common/hwasan/large-aligned-1.c
@@ -9,6 +9,6 @@
 /* { dg-output "HWAddressSanitizer: tag-mismatch on address 0x\[0-9a-f\]*.*" } */
 /* NOTE: This assumes the current tagging mechanism (one at a time from the
    base and large aligned variables being handled first).  */
-/* { dg-output "READ of size 4 at 0x\[0-9a-f\]* tags: \[\[:xdigit:\]\]\[\[:xdigit:\]\]/\[\[:xdigit:\]\]\[\[:xdigit:\]\] \\(ptr/mem\\) in thread T0.*" } */
+/* { dg-output "READ of size 4 at 0x\[0-9a-f\]* tags: \[\[:xdigit:\]\]\[\[:xdigit:\]\]/\[\[:xdigit:\]\]\[\[:xdigit:\]\].* \\(ptr/mem\\) in thread T0.*" } */
 /* { dg-output "Address 0x\[0-9a-f\]* is located in stack of thread T0.*" } */
 /* { dg-output "SUMMARY: HWAddressSanitizer: tag-mismatch \[^\n\]*.*" } */
