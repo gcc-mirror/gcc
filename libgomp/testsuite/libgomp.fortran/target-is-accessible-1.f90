caa8134568c639dfe980ebab26a514e7e1fa5199
Marcel Vollweiler <marcel@codesourcery.com>
2022-05-06 07:28:26 -0700

    OpenMP, libgomp: Add new runtime routine omp_target_is_accessible.
    gcc/ChangeLog:
    
    	* omp-low.cc (omp_runtime_api_call): Added target_is_accessible to
    	omp_runtime_apis array.
    
    libgomp/ChangeLog:
    
    	* libgomp.map: Added omp_target_is_accessible.
    	* libgomp.texi: Tagged omp_target_is_accessible as supported.
    	* omp.h.in: Added omp_target_is_accessible.
    	* omp_lib.f90.in: Added interface for omp_target_is_accessible.
    	* omp_lib.h.in: Likewise.
    	* target.c (omp_target_is_accessible): Added implementation of
    	omp_target_is_accessible.
    	* testsuite/libgomp.c-c++-common/target-is-accessible-1.c: New test.
    	* testsuite/libgomp.fortran/target-is-accessible-1.f90: New test.
    
    (cherry picked from commit 4043f53cb4a3541f7a6e4f4132419f78ab7ec4f7)

---
+++ libgomp/testsuite/libgomp.fortran/target-is-accessible-1.f90
@@ -0,0 +1,50 @@
+program main
+  use omp_lib
+  use iso_c_binding
+  implicit none (external, type)
+  integer :: d, id, n, shared_mem, i
+  integer, target :: a(1:128)
+  type(c_ptr) :: p
+
+  d = omp_get_default_device ()
+  id = omp_get_initial_device ()
+  n = omp_get_num_devices ()
+
+  if (d < 0 .or. d >= n) &
+    d = id
+
+  if (omp_target_is_accessible (p, c_sizeof (d), n) /= 1) &
+    stop 1
+
+  if (omp_target_is_accessible (p, c_sizeof (d), id) /= 1) &
+    stop 2
+
+  if (omp_target_is_accessible (p, c_sizeof (d), -1) /= 0) &
+    stop 3
+
+  if (omp_target_is_accessible (p, c_sizeof (d), n + 1) /= 0) &
+    stop 4
+
+  ! Currently, a host pointer is accessible if the device supports shared
+  ! memory or omp_target_is_accessible is executed on the host. This
+  ! test case must be adapted when unified shared memory is avialable.
+  do d = 0, omp_get_num_devices ()
+    shared_mem = 0;
+    !$omp target map (alloc: shared_mem) device (d)
+      shared_mem = 1;
+    !$omp end target
+
+    if (omp_target_is_accessible (p, c_sizeof (d), d) /= shared_mem) &
+      stop 5;
+
+    if (omp_target_is_accessible (c_loc (a), 128 * sizeof (a(1)), d) /= shared_mem) &
+      stop 6;
+
+    do i = 1, 128
+      if (omp_target_is_accessible (c_loc (a(i)), sizeof (a(i)), d) /= shared_mem) &
+        stop 7;
+    end do
+
+  end do
+
+end program main
