wenyiwang-us/gnu-openmp-xtask

c-family: Initialize ridpointers for __int128 etc. [PR105186]
The following testcase ICEs with C++ and is incorrectly rejected with C.
The reason is that both FEs use ridpointers identifiers for CPP_KEYWORD
and value or u.value for CPP_NAME e.g. when parsing attributes or OpenMP
directives etc., like:
         /* Save away the identifier that indicates which attribute
            this is.  */
         identifier = (token->type == CPP_KEYWORD)
           /* For keywords, use the canonical spelling, not the
              parsed identifier.  */
           ? ridpointers[(int) token->keyword]
           : id_token->u.value;

         identifier = canonicalize_attr_name (identifier);
I've tried to change those to use ridpointers only if non-NULL and otherwise
use the value/u.value even for CPP_KEYWORDS, but that was a large 10 hunks
patch.

The following patch instead just initializes ridpointers for the __intNN
keywords.  It can't be done earlier before we record_builtin_type as there
are 2 different spellings and if we initialize those ridpointers early, the
second record_builtin_type fails miserably.

2022-04-11  Jakub Jelinek  <jakub@redhat.com>

	PR c++/105186
	* c-common.cc (c_common_nodes_and_builtins): After registering __int%d
	and __int%d__ builtin types, initialize corresponding ridpointers
	entry.

	* c-c++-common/pr105186.c: New test.

gcc/testsuite/c-c++-common/pr105186.c
@@ -1,0 +1,5 @@
+/* PR c++/105186 */
+/* { dg-do compile } */
+
+__attribute__((__int128)) int i;	/* { dg-warning "'__int128' attribute directive ignored" } */
+__attribute__((__int128__)) int j;	/* { dg-warning "'__int128' attribute directive ignored" } */

gcc-mirror/gcc

	* c-common.c (c_common_nodes_and_builtins): After registering __int%d
(cherry picked from commit 083e8e66d2e90992fa83a53bfc3553dfa91abda1)

gcc/testsuite/c-c++-common/pr105186.c
@@ -1,0 +1,5 @@
+/* PR c++/105186 */
+/* { dg-do compile } */
+
+__attribute__((__int128)) int i;	/* { dg-warning "'__int128' attribute directive ignored" } */
+__attribute__((__int128__)) int j;	/* { dg-warning "'int128' attribute directive ignored" } */