6d79958a50eb5419ebb9baa5ef880aabeef05467
Jakub Jelinek <jakub@redhat.com>
2022-05-11 20:37:01 +0200

    testsuite: Fix up pr102860.f90 for gcc 9 [PR105570]
    Apparently -mcpu=power10 is gcc 10+, but the PR102860 change otherwise
    made sense also for 9.x.  So just adjusting testcase...
    
    2022-05-11  Jakub Jelinek  <jakub@redhat.com>
    
    	PR middle-end/102860
    	PR testsuite/105570
    	* gfortran.dg/pr102860.f90: Use -mcpu=power9 instead of -mcpu=power10.

--- gcc/testsuite/gfortran.dg/pr102860.f90
+++ gcc/testsuite/gfortran.dg/pr102860.f90
@@ -1,8 +1,8 @@
 ! PR middle-end/102860
 ! { dg-do compile { target { powerpc*-*-* } } }
 ! { dg-require-effective-target powerpc_vsx_ok } 
-! { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power10" } } 
-! { dg-options "-O2 -mcpu=power10" } 
+! { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power9" } } 
+! { dg-options "-O2 -mcpu=power9" } 
 
 function foo(a)
   integer(kind=4) :: a(1024)
