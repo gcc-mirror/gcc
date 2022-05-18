16e02662c092c6e3087628ef0030e7daa2f71485
Giuliano Belinassi <gbelinassi@suse.de>
2022-05-18 16:16:48 -0300

    PR105647 Update pr105169* so it does not fail on powerpc64le
    On powerpc64le, the tests related to pr105169 failed because the
    .localentry was not on a power of two address due to the extra nop
    instruction taking one byte and thus moving its position one byte
    further. Generating two nops instead moves .localentry to a valid
    position.
    
    gcc/testsuite/ChangeLog
    2022-05-18  Giuliano Belinassi  <gbelinassi@suse.de>
    
    	PR target/105647
    	* g++.dg/modules/pr105169_a.C: Change -fpatchable-function-entry to 2.
    	* g++.dg/modules/pr105169_b.C: Likewise.

--- gcc/testsuite/g++.dg/modules/pr105169_b.C
+++ gcc/testsuite/g++.dg/modules/pr105169_b.C
@@ -1,6 +1,6 @@
 /* { dg-module-do link } */
-/* { dg-options "-std=c++11 -fpatchable-function-entry=1 -O2" } */
-/* { dg-additional-options "-std=c++11 -fpatchable-function-entry=1 -O2" } */
+/* { dg-options "-std=c++11 -fpatchable-function-entry=2 -O2" } */
+/* { dg-additional-options "-std=c++11 -fpatchable-function-entry=2 -O2" } */
 
 /* This test is in the "modules" package because it supports multiple files
    linkage.  */
