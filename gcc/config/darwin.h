8fe73ef2785837dbebee7c56d5f30211571b2773
Iain Sandoe <iain@sandoe.co.uk>
2022-05-28 10:16:27 +0100

    Darwin, rpaths: Add --with-darwin-extra-rpath.
    This is provided to allow distributions to add a single additional
    runpath to allow for cases where the installed GCC library directories
    are then symlinked to a common dirctory outside of any of the GCC
    installations.
    
    For example:
    
    /opt/distro/lib:
      libgfortran.dylib -> /opt/distro/lib/gcc-11.3/lib/libgfortran.dylib
    
    So that libraries which are designed to be found in the runpath we would then
    add --with-darwin-add-rpath=/opt/distro/lib to the configure line.
    
    This patch makes the configuration a little more forgiving of using
    --disable-darwin-at-rpath (although for platform versions >= 10.11 this will
    result in misconfigured target libraries).
    
    gcc/ChangeLog:
    
    	* configure.ac: Add --with-darwin-extra-rpath option.
    	* config/darwin.h: Handle DARWIN_EXTRA_RPATH.
    	* config.in: Regenerate.
    	* configure: Regenerate.

--- gcc/config/darwin.h
+++ gcc/config/darwin.h
@@ -321,6 +321,19 @@ extern GTY(()) int darwin_ms_struct;
  %:version-compare(>= 10.11 mmacosx-version-min= -lemutls_w) "
 #endif
 
+/* We might elect to add a path even when this compiler does not use embedded
+   run paths, so that we can use libraries from an alternate compiler that is
+   using embedded runpaths.  */
+#if DARWIN_DO_EXTRA_RPATH
+# define DARWIN_EXTRA_RPATH \
+"%{!r:%{!nostdlib:%{!nodefaultrpaths:\
+    %:version-compare(>= 10.5 mmacosx-version-min= -rpath) \
+    %:version-compare(>= 10.5 mmacosx-version-min= " DARWIN_ADD_RPATH ") \
+  }}}"
+#else
+# define DARWIN_EXTRA_RPATH ""
+#endif
+
 #define SUBSUBTARGET_OVERRIDE_OPTIONS					\
   do {									\
     darwin_override_options ();						\
@@ -415,6 +428,7 @@ extern GTY(()) int darwin_ms_struct;
     DARWIN_NOPIE_SPEC \
     DARWIN_RDYNAMIC \
     DARWIN_NOCOMPACT_UNWIND \
+    DARWIN_EXTRA_RPATH \
     DARWIN_RPATH_LINK \
     "}}}}}}} %<pie %<no-pie %<rdynamic %<X %<rpath %<nodefaultrpaths "
 
