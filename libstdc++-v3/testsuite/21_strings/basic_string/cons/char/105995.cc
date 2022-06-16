e562236851e06091256593aa0d3fbda60a28e45b
Jonathan Wakely <jwakely@redhat.com>
2022-06-16 14:57:32 +0100

    libstdc++: Support constexpr global std::string for size < 15 [PR105995]
    I don't think this is required by the standard, but it's easy to
    support.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/105995
    	* include/bits/basic_string.h (_M_use_local_data): Initialize
    	the entire SSO buffer.
    	* testsuite/21_strings/basic_string/cons/char/105995.cc: New test.
    
    (cherry picked from commit 98a0d72a610a87e8e383d366e50253ddcc9a51dd)

---
+++ libstdc++-v3/testsuite/21_strings/basic_string/cons/char/105995.cc
@@ -0,0 +1,11 @@
+// { dg-options "-std=gnu++20" }
+// { dg-do compile { target c++20 } }
+// { dg-require-effective-target cxx11_abi }
+
+#include <string>
+
+// PR libstdc++/105995
+// Not required by the standard, but supported for QoI.
+constexpr std::string pr105995_empty;
+constexpr std::string pr105995_partial = "0";
+constexpr std::string pr105995_full = "0123456789abcde";
