fc259b522c0f8b7bbca8e7adcd3da63330094a34
Jeff Law <jeffreyalaw@gmail.com>
2022-06-25 12:36:55 -0400

    Remove long deprecated tilegx and tilepro ports
    /
    	* MAINTAINERS: Remove tilegx and tilepro entries.
    	* configure.ac: Remove tilegx and tilepro stanzas.
    	* configure: Rebuilt.
    
    contrib/
    	* config-list.mk: Remove tilegx and tilepro entries.
    	* gcc_update: Remove tilegx and tilepro entries.
    
    gcc/
    	* common/config/tilegx/tilegx-common.cc: Removed.
    	* common/config/tilepro/tilepro-common.cc: Removed.
    	* config.gcc: Remove tilegx and tilepro entries.
    	* config/tilegx/constraints.md: Removed.
    	* config/tilegx/feedback.h: Removed.
    	* config/tilegx/linux.h: Removed.
    	* config/tilegx/mul-tables.cc: Removed.
    	* config/tilegx/predicates.md: Removed.
    	* config/tilegx/sync.md: Removed.
    	* config/tilegx/t-tilegx: Removed.
    	* config/tilegx/tilegx-builtins.h: Removed.
    	* config/tilegx/tilegx-c.cc: Removed.
    	* config/tilegx/tilegx-generic.md: Removed.
    	* config/tilegx/tilegx-modes.def: Removed.
    	* config/tilegx/tilegx-multiply.h: Removed.
    	* config/tilegx/tilegx-opts.h: Removed.
    	* config/tilegx/tilegx-protos.h: Removed.
    	* config/tilegx/tilegx.cc: Removed.
    	* config/tilegx/tilegx.h: Removed.
    	* config/tilegx/tilegx.md: Removed.
    	* config/tilegx/tilegx.opt: Removed.
    	* config/tilepro/constraints.md: Removed.
    	* config/tilepro/feedback.h: Removed.
    	* config/tilepro/gen-mul-tables.cc: Removed.
    	* config/tilepro/linux.h: Removed.
    	* config/tilepro/mul-tables.cc: Removed.
    	* config/tilepro/predicates.md: Removed.
    	* config/tilepro/t-tilepro: Removed.
    	* config/tilepro/tilepro-builtins.h: Removed.
    	* config/tilepro/tilepro-c.cc: Removed.
    	* config/tilepro/tilepro-generic.md: Removed.
    	* config/tilepro/tilepro-modes.def: Removed.
    	* config/tilepro/tilepro-multiply.h: Removed.
    	* config/tilepro/tilepro-protos.h: Removed.
    	* config/tilepro/tilepro.cc: Removed.
    	* config/tilepro/tilepro.h: Removed.
    	* config/tilepro/tilepro.md: Removed.
    	* config/tilepro/tilepro.opt: Removed.
    	* configure.ac: Remove tilegx and tilepro entries.
    	* configure: Rebuilt.
    	* doc/extend.texi: Remove tilegx and tilepro entries.
    	* doc/install.texi: Remove tilegx and tilepro entries.
    	* doc/invoke.texi: Remove tilegx and tilepro entries.
    	* doc/md.texi: Remove tilegx and tilepro entries.
    
    gcc/testsuite/
    	* gcc.dg/lower-subreg-1.c: Remove tilegx and tilepro entries.
    	* gcc.misc-tests/linkage.exp: Remove tilegx and
    	tilepro entries.
    
    libgcc/
    	* config.host: Removed tilegx and tilepro entries.
    	* config/tilegx/sfp-machine.h: Removed.
    	* config/tilegx/sfp-machine32.h: Removed.
    	* config/tilegx/sfp-machine64.h: Removed.
    	* config/tilegx/t-crtstuff: Removed.
    	* config/tilegx/t-softfp: Removed.
    	* config/tilegx/t-tilegx: Removed.
    	* config/tilepro/atomic.c: Removed.
    	* config/tilepro/atomic.h: Removed.
    	* config/tilepro/linux-unwind.h: Removed.
    	* config/tilepro/sfp-machine.h: Removed.
    	* config/tilepro/softdivide.c: Removed.
    	* config/tilepro/softmpy.S: Removed.
    	* config/tilepro/t-crtstuff: Removed.
    	* config/tilepro/t-tilepro: Removed.

--- gcc/testsuite/gcc.dg/lower-subreg-1.c
+++ gcc/testsuite/gcc.dg/lower-subreg-1.c
@@ -1,4 +1,4 @@
-/* { dg-do compile { target { ! { mips64 || { aarch64*-*-* arm*-*-* i?86-*-* ia64-*-* pru-*-* sparc*-*-* tilegx-*-* x86_64-*-* } } } } } */
+/* { dg-do compile { target { ! { mips64 || { aarch64*-*-* arm*-*-* i?86-*-* ia64-*-* pru-*-* sparc*-*-* x86_64-*-* } } } } } */
 /* { dg-options "-O -fdump-rtl-subreg1" } */
 /* { dg-require-effective-target ilp32 } */
 
