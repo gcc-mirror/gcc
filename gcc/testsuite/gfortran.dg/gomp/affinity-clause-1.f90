88c73dbc2de129c5d4f1af95c6e5433447d847a2
Tobias Burnus <tobias@codesourcery.com>
2022-06-27 13:44:36 +0200

    Fix gfortran.dg/gomp/affinity-clause-1.f90
    The patch
        [OG11][committed][PATCH 01/22] Fortran: delinearize multi-dimensional array accesses
        https://gcc.gnu.org/pipermail/gcc-patches/2021-November/584716.html
    OG11 commit e208eefbaa3f9f590171fce79c7366636770b348 causes a different
    dump - update the testcase accordingly.
    
    gcc/testsuite/
    	* gfortran.dg/gomp/affinity-clause-1.f90: Fix scan-tree-dump-times.

--- gcc/testsuite/gfortran.dg/gomp/affinity-clause-1.f90
+++ gcc/testsuite/gfortran.dg/gomp/affinity-clause-1.f90
@@ -22,7 +22,7 @@ end
 
 ! { dg-final { scan-tree-dump-times "D\\.\[0-9\]+ = .integer.kind=4.. __builtin_cosf ..real.kind=4.. a \\+ 1.0e\\+0\\);" 2 "original" } }
 
-! { dg-final { scan-tree-dump-times "#pragma omp task affinity\\(iterator\\(integer\\(kind=4\\) jj=2:5:2, integer\\(kind=4\\) i=D\\.\[0-9\]+:5:1\\):b\\\[.* <?i>? \\+ -1\\\]\\) affinity\\(iterator\\(integer\\(kind=4\\) jj=2:5:2, integer\\(kind=4\\) i=D\\.\[0-9\]+:5:1\\):d\\\[\\(.*jj \\* 5 \\+ .* <?i>?\\) \\+ -6\\\]\\)" 1 "original" } }
+! { dg-final { scan-tree-dump-times "#pragma omp task affinity\\(iterator\\(integer\\(kind=4\\) jj=2:5:2, integer\\(kind=4\\) i=D\\.\[0-9\]+:5:1\\):b\\\[.* <?i>? \\+ -1\\\]\\) affinity\\(iterator\\(integer\\(kind=4\\) jj=2:5:2, integer\\(kind=4\\) i=D\\.\[0-9\]+:5:1\\):\\(\\(integer\\(kind=4\\)\\\[1:5\\\]\\\[1:5\\\]\\) d\\)\\\[\[^ \]* *jj\\\]{lb: 1 sz: 20}\\\[\[^ \]* *i\\\]{lb: 1 sz: 4}\\)" 1 "original" } }
 
 ! { dg final { scan-tree-dump-times "#pragma omp task affinity\\(iterator\\(integer\\(kind=4\\) i=D\\.\[0-9\]+:5:1\\):b\\\[\\(.* <?i>? \\+ -1\\\]\\) affinity\\(iterator\\(integer\\(kind=4\\) i=D\\.\[0-9\]+:5:1\\):d\\\[\\(\\(integer\\(kind=8\\)\\) i \\+ -1\\) \\* 6\\\]\\)"  1 "original" } }
 
