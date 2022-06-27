2adb0ec35cd47b34d47c961f6ae46089e3e02cbc
Tobias Burnus <tobias@codesourcery.com>
2022-06-27 13:26:43 +0200

    Fix gfortran.dg/gomp/num-teams-2.f90
    OG11 contrary to mainline issues an error for resolve_positive_int_expr
    (-> OG11 commit a14b3f29681da1d2465e15f98b8cf8d5c64a2c3c). Update
    testcase accordingly.
    
    gcc/testsuite/
    	* gfortran.dg/gomp/num-teams-2.f90: Use dg-error not dg-warning.

--- gcc/testsuite/gfortran.dg/gomp/num-teams-2.f90
+++ gcc/testsuite/gfortran.dg/gomp/num-teams-2.f90
@@ -9,13 +9,13 @@ subroutine foo (i)
   !$omp teams num_teams (6 : 4)		! { dg-warning "NUM_TEAMS lower bound at .1. larger than upper bound at .2." }
   !$omp end teams
 
-  !$omp teams num_teams (-7)		! { dg-warning "INTEGER expression of NUM_TEAMS clause at .1. must be positive" }
+  !$omp teams num_teams (-7)		! { dg-error "INTEGER expression of NUM_TEAMS clause at .1. must be positive" }
   !$omp end teams
 
-  !$omp teams num_teams (i : -7)		! { dg-warning "INTEGER expression of NUM_TEAMS clause at .1. must be positive" }
+  !$omp teams num_teams (i : -7)		! { dg-error "INTEGER expression of NUM_TEAMS clause at .1. must be positive" }
   !$omp end teams
 
-  !$omp teams num_teams (-7 : 8)		! { dg-warning "INTEGER expression of NUM_TEAMS clause at .1. must be positive" }
+  !$omp teams num_teams (-7 : 8)		! { dg-error "INTEGER expression of NUM_TEAMS clause at .1. must be positive" }
   !$omp end teams
 end
 
@@ -25,13 +25,13 @@ subroutine bar (i)
   !$omp target teams num_teams (6 : 4)	! { dg-warning "NUM_TEAMS lower bound at .1. larger than upper bound at .2." }
   !$omp end target teams
 
-  !$omp target teams num_teams (-7)	! { dg-warning "INTEGER expression of NUM_TEAMS clause at .1. must be positive" }
+  !$omp target teams num_teams (-7)	! { dg-error "INTEGER expression of NUM_TEAMS clause at .1. must be positive" }
   !$omp end target teams
 
-  !$omp target teams num_teams (i : -7)	! { dg-warning "INTEGER expression of NUM_TEAMS clause at .1. must be positive" }
+  !$omp target teams num_teams (i : -7)	! { dg-error "INTEGER expression of NUM_TEAMS clause at .1. must be positive" }
   !$omp end target teams
 
-  !$omp target teams num_teams (-7 : 8)	! { dg-warning "INTEGER expression of NUM_TEAMS clause at .1. must be positive" }
+  !$omp target teams num_teams (-7 : 8)	! { dg-error "INTEGER expression of NUM_TEAMS clause at .1. must be positive" }
   !$omp end target teams
 end
 end module
