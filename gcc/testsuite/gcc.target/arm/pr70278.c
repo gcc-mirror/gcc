f6d791a4cc6d7f2f24f5aec02dc4fff1a7ea9101
Li kunyu <kunyu@nfschina.com>
2022-07-01 10:44:50 +0800

    testsuite: gcc.target: arm: Optimize symbols and styles
    Signed-off-by: Li kunyu <kunyu@nfschina.com>

--- gcc/testsuite/gcc.target/arm/pr70278.c
+++ gcc/testsuite/gcc.target/arm/pr70278.c
@@ -27,10 +27,10 @@ double __ieee754_hypot(double x, double y)
 {
  double a=x,b=y,t1,t2,y1,y2,w;
  int j,k,ha,hb;
- do { ieee_double_shape_type gh_u; gh_u.value = (x); (ha) = gh_u.parts.msw; } while (0);;
- if(hb > ha) {a=y;b=x;j=ha; ha=hb;hb=j;} else {a=x;b=y;}
+ do { ieee_double_shape_type gh_u; gh_u.value = (x); (ha) = gh_u.parts.msw; } while (0);
+ if(hb > ha) {a=y; b=x; j=ha; ha=hb; hb=j;} else {a=x; b=y;}
  if(ha > 0x5f300000) {
-    do { ieee_double_shape_type sh_u; sh_u.value = (a); sh_u.parts.msw = (ha); (a) = sh_u.value; } while (0);;
+    do { ieee_double_shape_type sh_u; sh_u.value = (a); sh_u.parts.msw = (ha); (a) = sh_u.value; } while (0);
  }
  w = a-b;
  if (w <= b)
