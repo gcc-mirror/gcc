faa0c328ee65f0d6d65d6e20181d26e336071919
Thomas Schwinge <thomas@codesourcery.com>
2022-07-07 15:11:03 +0200

    Fix one issue in OpenMP 'requires' directive diagnostics
    Fix-up for recent commit 683f11843974f0bdf42f79cdcbb0c2b43c7b81b0
    "OpenMP: Move omp requires checks to libgomp".
    
    	gcc/
    	* lto-cgraph.cc (input_offload_tables) <LTO_symtab_edge>: Correct
    	'fn2' computation.
    	libgomp/
    	* testsuite/libgomp.c-c++-common/requires-1.c: Add 'dg-note's.
    	* testsuite/libgomp.c-c++-common/requires-2.c: Likewise.
    	* testsuite/libgomp.c-c++-common/requires-3.c: Likewise.
    	* testsuite/libgomp.c-c++-common/requires-7.c: Likewise.
    	* testsuite/libgomp.fortran/requires-1.f90: Likewise.

--- libgomp/testsuite/libgomp.c-c++-common/requires-7.c
+++ libgomp/testsuite/libgomp.c-c++-common/requires-7.c
@@ -20,5 +20,13 @@ main (void)
   return 0;
 }
 
-/* { dg-error "OpenMP 'requires' directive with non-identical clauses in multiple compilation units: 'unified_shared_memory' vs. 'unified_address'" "" { target *-*-* } 0 }  */
+/* { dg-error "OpenMP 'requires' directive with non-identical clauses in multiple compilation units: 'unified_shared_memory' vs. 'unified_address'" "" { target *-*-* } 0 }
+     { dg-note {requires-7\.c' has 'unified_shared_memory'} {} { target *-*-* } 0 }
+     TODO We're currently not streaming location information for the OpenMP
+     directives used in 'requires-7-aux.c', so we're not seeing the source file
+     name here (but a temporary '*.o' instead; for details, see
+     <https://gcc.gnu.org/pipermail/gcc-patches/2022-July/598011.html>):
+     { dg-note {requires-7-aux\.c' has 'unified_address'} {} { xfail *-*-* } 0 }
+     ..., but we may still verify that the rest of the diagnostic is correct:
+     { dg-note {' has 'unified_address'} {} { target *-*-* } 0 } */
 /* { dg-excess-errors "Ignore messages like: errors during merging of translation units|mkoffload returned 1 exit status" } */
