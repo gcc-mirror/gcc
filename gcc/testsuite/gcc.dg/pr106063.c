4ebbf3906895bcb40d7ff2729cf46deae66bc268
Dimitar Dimitrov <dimitar@dinux.eu>
2022-07-10 11:15:39 +0300

    testsuite: Require int128 for gcc.dg/pr106063.c
    Require effective target int128 for gcc.dg/pr106063.c.
    
    	PR tree-optimization/106063
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/pr106063.c: Require effective target int128.
    
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- gcc/testsuite/gcc.dg/pr106063.c
+++ gcc/testsuite/gcc.dg/pr106063.c
@@ -1,4 +1,4 @@
-/* { dg-do compile } */
+/* { dg-do compile { target int128 } } */
 /* { dg-options "-O2 -fno-tree-forwprop --disable-tree-evrp" } */
 typedef __int128 __attribute__((__vector_size__ (16))) V;
 
