3723aedaad20a129741c2f6f3c22b3dd1220a3fc
Thomas Schwinge <thomas@codesourcery.com>
2022-07-12 08:17:37 +0200

    XFAIL 'offloading_enabled' diagnostics issue in 'libgomp.oacc-c-c++-common/reduction-5.c' [PR101551]
    Fix-up for recent commit 06b2a2abe26554c6f9365676683d67368cbba206
    "Enhance '_Pragma' diagnostics verification in OMP C/C++ test cases".
    Supposedly it's the same issue as in
    <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=101551#c2>, where I'd
    noted that:
    
    | [...] with an offloading-enabled build of GCC we're losing
    | "note: in expansion of macro '[...]'" diagnostics.
    | (Effectively '-ftrack-macro-expansion=0'?)
    
    	PR middle-end/101551
    	libgomp/
    	* testsuite/libgomp.oacc-c-c++-common/reduction-5.c: XFAIL
    	'offloading_enabled' diagnostics issue.

--- libgomp/testsuite/libgomp.oacc-c-c++-common/reduction-5.c
+++ libgomp/testsuite/libgomp.oacc-c-c++-common/reduction-5.c
@@ -45,10 +45,11 @@ main (void)
 
   /* Nvptx targets require a vector_length or 32 in to allow spinlocks with
      gangs.  */
-  check_reduction (num_workers (nw) vector_length (vl), worker); /* { dg-line check_reduction_loc }
+  check_reduction (num_workers (nw) vector_length (vl), worker); /* { dg-line check_reduction_loc } */
   /* { dg-warning "22:region is vector partitioned but does not contain vector partitioned code" "" { target *-*-* } pragma_loc }
-     { dg-note "1:in expansion of macro 'DO_PRAGMA'" "" { target *-*-* } DO_PRAGMA_loc }
-     { dg-note "3:in expansion of macro 'check_reduction'" "" { target *-*-* } check_reduction_loc } */
+     { dg-note "1:in expansion of macro 'DO_PRAGMA'" "" { target *-*-* xfail offloading_enabled } DO_PRAGMA_loc }
+     { dg-note "3:in expansion of macro 'check_reduction'" "" { target *-*-* xfail offloading_enabled } check_reduction_loc }
+     TODO See PR101551 for 'offloading_enabled' XFAILs.  */
   check_reduction (vector_length (vl), vector);
   check_reduction (num_gangs (ng) num_workers (nw) vector_length (vl), gang
 		   worker vector);
