9d7c64fa996fc32dc2ae5c9af7f83c3a3e08f34a
Christophe Lyon <christophe.lyon@arm.com>
2022-07-15 10:10:14 +0100

    arm: move builtin function codes into general numberspace
    This patch introduces a separate numberspace for general arm builtin
    function codes. The intent of this patch is to separate the space of
    function codes that may be assigned to general builtins and future
    MVE intrinsic functions by using the first bit of each function code
    to differentiate them. This is identical to how SVE intrinsic functions
    are currently differentiated from general aarch64 builtins.
    
    Future intrinsics implementations may also make use of numberspacing by
    changing the values of ARM_BUILTIN_SHIFT and ARM_BUILTIN_CLASS, and
    adding themselves to the arm_builtin_class enum.
    
    2022-09-08  Murray Steele  <murray.steele@arm.com>
    	    Christophe Lyon  <christophe.lyon@arm.com>
    
    gcc/ChangeLog:
    
    	* config/arm/arm-builtins.cc (arm_general_add_builtin_function):
    	New function.
    	(arm_init_builtin): Use arm_general_add_builtin_function instead
    	of arm_add_builtin_function.
    	(arm_init_acle_builtins): Likewise.
    	(arm_init_mve_builtins): Likewise.
    	(arm_init_crypto_builtins): Likewise.
    	(arm_init_builtins): Likewise.
    	(arm_general_builtin_decl): New function.
    	(arm_builtin_decl): Defer to numberspace-specialized functions.
    	(arm_expand_builtin_args): Rename into arm_general_expand_builtin_args.
    	(arm_expand_builtin_1): Rename into arm_general_expand_builtin_1 and ...
    	(arm_general_expand_builtin_1): ... specialize for general builtins.
    	(arm_expand_acle_builtin): Use arm_general_expand_builtin
    	instead of arm_expand_builtin.
    	(arm_expand_mve_builtin): Likewise.
    	(arm_expand_neon_builtin): Likewise.
    	(arm_expand_vfp_builtin): Likewise.
    	(arm_general_expand_builtin): New function.
    	(arm_expand_builtin): Specialize for general builtins.
    	(arm_general_check_builtin_call): New function.
    	(arm_check_builtin_call): Specialize for general builtins.
    	(arm_describe_resolver): Validate numberspace.
    	(arm_cde_end_args): Likewise.
    	* config/arm/arm-protos.h (enum arm_builtin_class): New enum.
    	(ARM_BUILTIN_SHIFT, ARM_BUILTIN_CLASS): New constants.
    
    Co-authored-by: Christophe Lyon  <christophe.lyon@arm.com>

--- gcc/config/arm/arm-protos.h
+++ gcc/config/arm/arm-protos.h
@@ -210,6 +210,22 @@ extern opt_machine_mode arm_get_mask_mode (machine_mode mode);
 
 #endif /* RTX_CODE */
 
+/* It's convenient to divide the built-in function codes into groups,
+   rather than having everything in a single enum.  This type enumerates
+   those groups.  */
+enum arm_builtin_class
+{
+  ARM_BUILTIN_GENERAL
+};
+
+/* Built-in function codes are structured so that the low
+   ARM_BUILTIN_SHIFT bits contain the arm_builtin_class
+   and the upper bits contain a group-specific subcode.  */
+const unsigned int ARM_BUILTIN_SHIFT = 1;
+
+/* Mask that selects the arm part of a function code.  */
+const unsigned int ARM_BUILTIN_CLASS = (1 << ARM_BUILTIN_SHIFT) - 1;
+
 /* MVE functions.  */
 namespace arm_mve {
   void handle_arm_mve_types_h ();
