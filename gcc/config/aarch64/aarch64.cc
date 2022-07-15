4cbebddc2ce1e88216fec6aa89d111d8596bd34c
Prathamesh Kulkarni <prathamesh.kulkarni@linaro.org>
2022-07-15 06:26:50 +0530

    [aarch64] Use op_mode instead of vmode in aarch64_vectorize_vec_perm_const.
    gcc/ChangeLog:
    	* config/aarch64/aarch64.cc (aarch64_vectorize_vec_perm_const): Use
    	op_mode instead of vmode in calls to force_reg for op0 and op1.

--- gcc/config/aarch64/aarch64.cc
+++ gcc/config/aarch64/aarch64.cc
@@ -24129,11 +24129,11 @@ aarch64_vectorize_vec_perm_const (machine_mode vmode, machine_mode op_mode,
   d.op_mode = op_mode;
   d.op_vec_flags = aarch64_classify_vector_mode (d.op_mode);
   d.target = target;
-  d.op0 = op0 ? force_reg (vmode, op0) : NULL_RTX;
+  d.op0 = op0 ? force_reg (op_mode, op0) : NULL_RTX;
   if (op0 == op1)
     d.op1 = d.op0;
   else
-    d.op1 = op1 ? force_reg (vmode, op1) : NULL_RTX;
+    d.op1 = op1 ? force_reg (op_mode, op1) : NULL_RTX;
   d.testing_p = !target;
 
   if (!d.testing_p)
