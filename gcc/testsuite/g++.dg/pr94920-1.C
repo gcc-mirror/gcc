633e9920589ddfaf2d6da1c24ce99b18a2638db4
Sam Feifer <sfeifer@redhat.com>
2022-07-21 16:31:41 -0400

    match.pd: Add new abs pattern [PR94920]
    This patch is intended to fix a missed optimization in match.pd. It optimizes (x >= 0 ? x : 0) + (x <= 0 ? -x : 0) to just abs(x). Additionally, the pattern (x <= 0 ? -x : 0) now gets optimized to max(-x, 0), which helps with the other simplification rule.
    
    Tests are also included to be added to the testsuite.
    
    Bootstrapped/regtested on x86_64-pc-linux-gnu, ok for trunk?
    
    	PR tree-optimization/94920
    
    gcc/ChangeLog:
    
    	* match.pd (x >= 0 ? x : 0) + (x <= 0 ? -x : 0): New simplification.
    	           (x <= 0 ? -x : 0): New simplification.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/pr94920-1.C: New test.
    	* g++.dg/pr94920.C: New test.
    	* gcc.dg/pr94920-2.c: New test.

---
+++ gcc/testsuite/g++.dg/pr94920-1.C
@@ -0,0 +1,17 @@
+/* PR tree-optimization/94920 */
+/* { dg-do run } */
+
+#include "pr94920.C"
+
+int main() {
+
+    if (foo(0) != 0
+        || foo(-42) != 42
+        || foo(42) != 42
+        || baz(-10) != 10
+        || baz(-10) != 10) {
+            __builtin_abort();
+        }
+    
+    return 0;
+}
