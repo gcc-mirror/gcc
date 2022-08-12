4ceaae1f5c970fc53e16e94ce6a5216d7d4b2d2a
Srinath Parvathaneni <srinath.parvathaneni@arm.com>
2022-08-12 20:47:42 +0100

    [GCC][PATCH] arm: Add pacbti related multilib support for armv8.1-m.main.
    Hi,
    
    This patch supports following -march/-mbranch-protection combination by linking them
    to existing pacbti multilibs.
    
    $ -march=armv8.1-m.main+pacbti+fp.dp+mve.fp -mbranch-protection=standard -mfloat-abi=hard -mthumb
    $ -march=armv8.1-m.main+pacbti+fp.dp+mve -mbranch-protection=standard -mfloat-abi=hard -mthumb
    $ -march=armv8.1-m.main+dsp+pacbti+fp.dp -mbranch-protection=standard -mfloat-abi=hard -mthumb
    
    Regression tested on arm-none-eabi and bootstrapped on arm-none-linux-gnueabihf.
    
    Ok for master?
    
    Regards,
    Srinath.
    
    gcc/ChangeLog:
    
    2022-08-12  Srinath Parvathaneni  <srinath.parvathaneni@arm.com>
    
            * config/arm/t-rmprofile: Add pacbti multililb variants.
    
    gcc/testsuite/ChangeLog:
    
    2022-08-12  Srinath Parvathaneni  <srinath.parvathaneni@arm.com>
    
            * gcc.target/arm/pac-10.c: New test.
            * gcc.target/arm/pac-11.c: Likewise.
            * gcc.target/arm/pac-12.c: Likewise.

---
+++ gcc/testsuite/gcc.target/arm/pac-10.c
@@ -0,0 +1,7 @@
+/* Testing PACBTI multilib matches.  */
+/* { dg-do run } */
+/* { dg-require-effective-target arm_pacbti_hw } */
+/* { dg-skip-if "need fp instructions" { *-*-* } { "" } { "-mfloat-abi=hard" } } */
+/* { dg-options "-march=armv8.1-m.main+mve.fp+fp.dp+pacbti -mbranch-protection=standard -mthumb -mfloat-abi=hard" } */
+
+#include "pac.h"
