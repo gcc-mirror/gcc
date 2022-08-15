d2d189985d1f9fa09802c4a14856d442786f4bf8
Nathan Sidwell <nathan@acm.org>
2022-08-15 07:19:36 -0700

    c++: Fix module line no testcase
    Not all systems have the same injected headers, leading to line
    location table differences that are immaterial to the test.  Fix the
    regexp more robustly.
    
    	gcc/testsuite/
    	* g++.dg/modules/loc-prune-4.C: Adjust regexp

--- gcc/testsuite/g++.dg/modules/loc-prune-4.C
+++ gcc/testsuite/g++.dg/modules/loc-prune-4.C
@@ -18,5 +18,5 @@ int baz (int);
 
 // { dg-final { scan-lang-dump {Ordinary maps:2 locs:12288 range_bits:5} module } }
 // { dg-final { scan-lang-dump { 1 source file names\n Source file...=[^\n]*loc-prune-4.C\n} module } }
-// { dg-final { scan-lang-dump { Span:0 ordinary \[2.....\+12288,\+4096\)->\[0,\+4096\)} module } }
-// { dg-final { scan-lang-dump { Span:1 ordinary \[2.....\+40960,\+8192\)->\[4096,\+8192\)} module } }
+// { dg-final { scan-lang-dump { Span:0 ordinary \[[0-9]+\+12288,\+4096\)->\[0,\+4096\)} module } }
+// { dg-final { scan-lang-dump { Span:1 ordinary \[[0-9]+\+40960,\+8192\)->\[4096,\+8192\)} module } }
