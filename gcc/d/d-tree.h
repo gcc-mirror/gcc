dc230a0c690794cd45cc4065fa13303fd6cdfc40
Iain Buclaw <ibuclaw@gdcproject.org>
2022-08-15 17:51:03 +0200

    d: Fix internal compiler error: Segmentation fault at gimple-expr.cc:88
    Because complex types are deprecated in the language, the new way to
    expose native complex types is by defining an enum with a basetype of a
    library-defined struct that is implicitly treated as-if it is native.
    As casts are not implicitly added by the front-end when downcasting from
    enum to its underlying type, we must insert an explicit cast during the
    code generation pass.
    
    	PR d/106623
    
    gcc/d/ChangeLog:
    
    	* d-codegen.cc (underlying_complex_expr): New function.
    	(d_build_call): Handle passing native complex objects as the
    	library-defined equivalent.
    	* d-tree.h (underlying_complex_expr): Declare.
    	* expr.cc (ExprVisitor::visit (DotVarExp *)): Call
    	underlying_complex_expr instead of build_vconvert.
    
    gcc/testsuite/ChangeLog:
    
    	* gdc.dg/torture/pr106623.d: New test.
    
    (cherry picked from commit e206fecaac29f559f4990312b875604eb1ce3ef3)

--- gcc/d/d-tree.h
+++ gcc/d/d-tree.h
@@ -564,6 +564,7 @@ extern tree size_mult_expr (tree, tree);
 extern tree real_part (tree);
 extern tree imaginary_part (tree);
 extern tree complex_expr (tree, tree, tree);
+extern tree underlying_complex_expr (tree, tree);
 extern tree indirect_ref (tree, tree);
 extern tree build_deref (tree);
 extern tree build_array_index (tree, tree);
