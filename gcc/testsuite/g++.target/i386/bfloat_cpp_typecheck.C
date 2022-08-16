bf13a13c65bd06c75d0e40e27f5ad34c7373fc55
Jason Merrill <jason@redhat.com>
2022-08-16 15:19:46 -0400

    c++: remove some xfails
    These tests are now passing.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/warn/Wstringop-overflow-4.C: Only xfail for C++98.
    	* g++.target/i386/bfloat_cpp_typecheck.C: Remove xfail.

--- gcc/testsuite/g++.target/i386/bfloat_cpp_typecheck.C
+++ gcc/testsuite/g++.target/i386/bfloat_cpp_typecheck.C
@@ -3,8 +3,8 @@
 
 void foo (void)
 {
-  __bf16 (); /* { dg-bogus {invalid conversion to type '__bf16'} "" { xfail *-*-* } } */
-  __bf16 a = __bf16(); /* { dg-bogus {invalid conversion to type '__bf16'} "" { xfail *-*-* } } */
+  __bf16 (); /* { dg-bogus {invalid conversion to type '__bf16'} } */
+  __bf16 a = __bf16(); /* { dg-bogus {invalid conversion to type '__bf16'} } */
   __bf16 (0x1234); /* { dg-error {invalid conversion to type '__bf16'} } */
   __bf16 (0.1); /* { dg-error {invalid conversion to type '__bf16'} } */
 }
