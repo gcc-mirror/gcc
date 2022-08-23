a3e1361d1ac1ae892d25737e0ee21e71db423a07
Philip Herron <philip.herron@embecosm.com>
2022-08-23 17:42:17 +0100

    Fix port of NOP_EXPR
    NOP_EXPR should fall through into convert and view_convert_exprs.

--- gcc/rust/backend/rust-constexpr.cc
+++ gcc/rust/backend/rust-constexpr.cc
@@ -2080,15 +2080,6 @@ eval_constant_expression (const constexpr_ctx *ctx, tree t, bool lval,
 				    non_constant_p, overflow_p);
       break;
 
-    case NOP_EXPR:
-      if (REINTERPRET_CAST_P (t))
-	{
-	  if (!ctx->quiet)
-	    error_at (loc, "%<reinterpret_cast%> is not a constant expression");
-	  *non_constant_p = true;
-	  return t;
-	}
-      /* FALLTHROUGH.  */
     case MODIFY_EXPR:
       r = eval_store_expression (ctx, t, false, non_constant_p, overflow_p);
       break;
@@ -2347,6 +2338,7 @@ eval_constant_expression (const constexpr_ctx *ctx, tree t, bool lval,
       break;
 
       /* FALLTHROUGH.  */
+    case NOP_EXPR:
     case CONVERT_EXPR:
       case VIEW_CONVERT_EXPR: {
 	tree oldop = TREE_OPERAND (t, 0);
