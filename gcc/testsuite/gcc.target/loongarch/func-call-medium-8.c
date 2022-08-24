feeb9314002a5e304dc552781ed93f06d7f51c93
Xi Ruoyao <xry111@xry111.site>
2022-08-24 19:34:47 +0800

    LoongArch: testsuite: refine __tls_get_addr tests with tls_native
    If GCC is not built with a working linker for the target (developers
    occansionally build such a "minimal" GCC for testing and debugging),
    TLS will be emulated and __tls_get_addr won't be used.  Refine those
    tests depending on __tls_get_addr with tls_native to avoid test
    failures.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/loongarch/func-call-medium-1.c: Refine test
    	depending on __tls_get_addr with { target tls_native }.
    	* gcc.target/loongarch/func-call-medium-2.c: Likewise.
    	* gcc.target/loongarch/func-call-medium-3.c: Likewise.
    	* gcc.target/loongarch/func-call-medium-4.c: Likewise.
    	* gcc.target/loongarch/func-call-medium-5.c: Likewise.
    	* gcc.target/loongarch/func-call-medium-6.c: Likewise.
    	* gcc.target/loongarch/func-call-medium-7.c: Likewise.
    	* gcc.target/loongarch/func-call-medium-8.c: Likewise.
    	* gcc.target/loongarch/tls-gd-noplt.c: Likewise.

--- gcc/testsuite/gcc.target/loongarch/func-call-medium-8.c
+++ gcc/testsuite/gcc.target/loongarch/func-call-medium-8.c
@@ -3,7 +3,8 @@
 /* { dg-final { scan-assembler "test:.*pcalau12i\t.*%got_pc_hi20\\(g\\)\n\tld\.d\t.*%got_pc_lo12\\(g\\)\n\tjirl" } } */
 /* { dg-final { scan-assembler "test1:.*pcalau12i\t.*%pc_hi20\\(f\\)\n\tjirl.*%pc_lo12\\(f\\)" } } */
 /* { dg-final { scan-assembler "test2:.*pcalau12i\t.*%pc_hi20\\(l\\)\n\tjirl.*%pc_lo12\\(l\\)" } } */
-/* { dg-final { scan-assembler "test3:.*pcalau12i.*%got_pc_hi20\\(__tls_get_addr\\)\n\tld\.d.*%got_pc_lo12\\(__tls_get_addr\\)" } } */
+/* { dg-final { scan-assembler "test3:.*pcalau12i.*%got_pc_hi20\\(__tls_get_addr\\)\n\tld\.d.*%got_pc_lo12\\(__tls_get_addr\\)" { target tls_native } } } */
+/* { dg-final { scan-assembler "test3:.*pcalau12i.*%got_pc_hi20\\(__tls_get_addr\\)\n\tld\.d.*%got_pc_lo12\\(__tls_get_addr\\)" { target tls_native } } } */
 
 extern void g (void);
 
