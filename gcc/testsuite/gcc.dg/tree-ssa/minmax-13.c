37ebaabde2b88d446369240ae8f03b8e6a284a7b
Tamar Christina <tamar.christina@arm.com>
2022-08-30 07:49:02 +0100

    middle-end: fix min/max phiopts reduction [PR106744]
    This corrects the argument usage to use them in the order that they occur in
    the comparisons in gimple.
    
    gcc/ChangeLog:
    
    	PR tree-optimization/106744
    	* tree-ssa-phiopt.cc (minmax_replacement): Correct arguments.
    
    gcc/testsuite/ChangeLog:
    
    	PR tree-optimization/106744
    	* gcc.dg/tree-ssa/minmax-10.c: Make runtime test.
    	* gcc.dg/tree-ssa/minmax-11.c: Likewise.
    	* gcc.dg/tree-ssa/minmax-12.c: Likewise.
    	* gcc.dg/tree-ssa/minmax-13.c: Likewise.
    	* gcc.dg/tree-ssa/minmax-14.c: Likewise.
    	* gcc.dg/tree-ssa/minmax-15.c: Likewise.
    	* gcc.dg/tree-ssa/minmax-16.c: Likewise.
    	* gcc.dg/tree-ssa/minmax-3.c: Likewise.
    	* gcc.dg/tree-ssa/minmax-4.c: Likewise.
    	* gcc.dg/tree-ssa/minmax-5.c: Likewise.
    	* gcc.dg/tree-ssa/minmax-6.c: Likewise.
    	* gcc.dg/tree-ssa/minmax-7.c: Likewise.
    	* gcc.dg/tree-ssa/minmax-8.c: Likewise.
    	* gcc.dg/tree-ssa/minmax-9.c: Likewise.

--- gcc/testsuite/gcc.dg/tree-ssa/minmax-13.c
+++ gcc/testsuite/gcc.dg/tree-ssa/minmax-13.c
@@ -1,8 +1,9 @@
-/* { dg-do compile } */
+/* { dg-do run } */
 /* { dg-options "-O -fdump-tree-phiopt" } */
 
 #include <stdint.h>
 
+__attribute__ ((noipa, noinline))
 uint8_t three_minmax2 (uint8_t xc, uint8_t xm, uint8_t xy) {
 	uint8_t	 xk;
     xc=~xc;
@@ -15,5 +16,17 @@ uint8_t three_minmax2 (uint8_t xc, uint8_t xm, uint8_t xy) {
     }
     return xk;
 }
+
+int
+main (void)
+{
+  volatile uint8_t xy = 255;
+  volatile uint8_t xm = 127;
+  volatile uint8_t xc = 0;
+  if (three_minmax2 (xc, xm, xy) != 255)
+    __builtin_abort ();
+  return 0;
+}
+
 /* { dg-final { scan-tree-dump-times "MIN_EXPR" 1 "phiopt1" } } */
 /* { dg-final { scan-tree-dump-times "MAX_EXPR" 1 "phiopt1" } } */
