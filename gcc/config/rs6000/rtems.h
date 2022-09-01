595b3d18164953a09dc901a71d6c1b9c61ed7156
Sebastian Huber <sebastian.huber@embedded-brains.de>
2022-09-01 10:22:47 +0200

    RTEMS: Add -mvrsave multilibs
    gcc/ChangeLog:
    
    	* config/rs6000/rtems.h (CPP_OS_DEFAULT_SPEC): Define __PPC_VRSAVE__ if
    	-mvrsave is present.
    	* config/rs6000/t-rtems: Add -mvrsave multilib variants for
    	-mcpu=e6500.

--- gcc/config/rs6000/rtems.h
+++ gcc/config/rs6000/rtems.h
@@ -255,7 +255,8 @@
 %{mcpu=821:  %{!Dppc*: %{!Dmpc*: -Dmpc821}  } } \
 %{mcpu=860:  %{!Dppc*: %{!Dmpc*: -Dmpc860}  } } \
 %{mcpu=8540: %{!Dppc*: %{!Dmpc*: -Dppc8540}  } } \
-%{mcpu=e6500: -D__PPC_CPU_E6500__}"
+%{mcpu=e6500: -D__PPC_CPU_E6500__} \
+%{mvrsave: -D__PPC_VRSAVE__}"
 
 #undef	ASM_SPEC
 #define	ASM_SPEC "%{!m64:%(asm_spec32)}%{m64:%(asm_spec64)} %(asm_spec_common)"
