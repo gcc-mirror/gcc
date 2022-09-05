09b91bbcddd4900d433ea72f08dd11719bceaa58
Piotr Trojanek <trojanek@adacore.com>
2022-09-05 11:40:50 +0200

    ada: Deconstruct build support for ancient MinGW
    Remove conditional C code for building GNAT with MinGW earlier than 2.0,
    which was released in 2007.
    
    gcc/ada/
    
    	* adaint.c: Remove conditional #include directives for old MinGW.
    	* cal.c: Always include winsock.h, since it is part of modern
    	MinGW.
    	* cstreams.c: Remove workaround for old MinGW.
    	* expect.c: Remove conditional #include directive for old MinGW.
    	* mingw32.h: Remove STD_MINGW and OLD_MINGW declarations.
    	* sysdep.c: Remove conditional #include directive for old MinGW.

--- gcc/ada/mingw32.h
+++ gcc/ada/mingw32.h
@@ -101,23 +101,6 @@ extern UINT __gnat_current_ccs_encoding;
 
 #include <stdlib.h>
 
-/* STD_MINGW: standard if MINGW32 version > 1.3, we have switched to this
-   version instead of the previous enhanced version to ease building GNAT on
-   Windows platforms. By using STD_MINGW or OLD_MINGW it is possible to build
-   GNAT using both MingW include files (Old MingW + ACT changes and standard
-   MingW starting with version 1.3.
-   For w64 Mingw the define STD_MINGW is always set to value 1, because
-   there is no old header set present.  */
-#ifdef _WIN64
-#define STD_MINGW 1
-#else
-#define STD_MINGW ((__MINGW32_MAJOR_VERSION == 1 \
-		   && __MINGW32_MINOR_VERSION >= 3) \
-     || (__MINGW32_MAJOR_VERSION >= 2))
-#endif
-
-#define OLD_MINGW (!(STD_MINGW))
-
 #ifndef MAXPATHLEN
 #define MAXPATHLEN MAX_PATH
 #endif
