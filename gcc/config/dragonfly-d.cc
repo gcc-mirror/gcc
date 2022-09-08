490b0c669d0f8fb14931cdcf7f5318c6a842c666
Iain Buclaw <ibuclaw@gdcproject.org>
2022-09-08 12:29:32 +0200

    d: Include tm.h in all D target platform sources, remove memmodel.h
    The tm.h header would pull in config/elfos.h, which defines
    TARGET_D_MINFO_SECTION needed for the D module support in the front-end
    to emit data to the correct section for the run-time library to pick up.
    
    The removal of it in r13-2385 caused a stage2 bootstrap failure on all
    Solaris targets.
    
    The memmodel header has also been removed as it is no longer required
    now tm_p.h is no longer used by these sources.
    
    gcc/ChangeLog:
    
    	* config/darwin-d.cc: Include tm.h.
    	* config/dragonfly-d.cc: Likewise.
    	* config/freebsd-d.cc: Remove memmodel.h.
    	* config/glibc-d.cc: Likewise.
    	* config/netbsd-d.cc: Include tm.h.
    	* config/openbsd-d.cc: Likewise.
    	* config/sol2-d.cc: Likewise.
    
    (cherry picked from commit a7852bd30a19d29ff7986869453786d460d17877)

--- gcc/config/dragonfly-d.cc
+++ gcc/config/dragonfly-d.cc
@@ -18,6 +18,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "config.h"
 #include "system.h"
 #include "coretypes.h"
+#include "tm.h"
 #include "tm_d.h"
 #include "d/d-target.h"
 #include "d/d-target-def.h"
