f92f8ee75bf0f4a653218a52698263b559c463a4
Philip Herron <philip.herron@embecosm.com>
2022-09-18 10:37:06 +0100

    gccrs: Add testcase for const-eval issue from rust-blog
    see:
    https://blog.rust-lang.org/2022/09/15/const-eval-safety-rule-revision.html
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/rust-const-blog-issue.rs: New test.

---
+++ gcc/testsuite/rust/compile/rust-const-blog-issue.rs
@@ -0,0 +1,12 @@
+// { dg-excess-errors "accessing value of"  }
+mod mem {
+    extern "rust-intrinsic" {
+        #[rustc_const_stable(feature = "const_transmute", since = "1.46.0")]
+        fn transmute<T, U>(_: T) -> U;
+    }
+}
+
+pub static FOO: () = unsafe {
+    let illegal_ptr2int: usize = mem::transmute(&());
+    let _copy = illegal_ptr2int;
+};
