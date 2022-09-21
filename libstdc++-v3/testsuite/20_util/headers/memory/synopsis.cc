c0c1207d49d2870542866368da3ae30da7e99baa
Jonathan Wakely <jwakely@redhat.com>
2022-09-21 09:29:39 +0100

    libstdc++: Update <memory> synopsis test for C++11 and later
    libstdc++-v3/ChangeLog:
    
    	* testsuite/20_util/headers/memory/synopsis.cc: Add declarations
    	from C++11 and later.
    
    (cherry picked from commit 980aa91e9cefe64245b7c32ee81ba372b7229aeb)

--- libstdc++-v3/testsuite/20_util/headers/memory/synopsis.cc
+++ libstdc++-v3/testsuite/20_util/headers/memory/synopsis.cc
@@ -26,20 +26,35 @@
 # define NOTHROW
 #endif
 
-namespace std {
+namespace std
+{
+#if __cplusplus >= 201103L
+  template<class Ptr> struct pointer_traits;
+  template<class T> struct pointer_traits<T*>;
+
+  void* align(size_t alignment, size_t size, void*& ptr, size_t& space);
+
+  struct allocator_arg_t;
+  extern const allocator_arg_t allocator_arg;
+
+  template<class T, class Alloc> struct uses_allocator;
+
+  template<class Alloc> struct allocator_traits;
+#endif // C++11
+
+#if __STDC_HOSTED__
   //  lib.default.allocator, the default allocator:
   template <class T> class allocator;
+#if __cplusplus >= 202002L
+  template <class T, class U>
+  constexpr bool operator==(const allocator<T>&, const allocator<U>&) throw();
+#else
   template <> class allocator<void>;
   template <class T, class U>
-#if __cplusplus > 201703L
-  constexpr
-#endif
   bool operator==(const allocator<T>&, const allocator<U>&) throw();
   template <class T, class U>
-#if __cplusplus > 201703L
-  constexpr
-#endif
   bool operator!=(const allocator<T>&, const allocator<U>&) throw();
+#endif
 
   //  lib.storage.iterator, raw storage iterator:
   template <class OutputIterator, class T> class raw_storage_iterator;
@@ -49,17 +64,54 @@ namespace std {
   pair<T*,ptrdiff_t> get_temporary_buffer(ptrdiff_t n) NOTHROW;
   template <class T>
   void return_temporary_buffer(T* p);
+#endif // HOSTED
 
   //  lib.specialized.algorithms, specialized algorithms:
+#if __cplusplus >= 201703L
+  template <class T> constexpr T* addressof(T&) noexcept;
+#elif __cplusplus >= 201402L
+  template <class T> T* addressof(T&) noexcept;
+#endif
   template <class InputIterator, class ForwardIterator>
   ForwardIterator
   uninitialized_copy(InputIterator first, InputIterator last,
 		     ForwardIterator result);
+#if __cplusplus >= 201103L
+  template <class InputIterator, class Size, class ForwardIterator>
+  ForwardIterator
+  uninitialized_copy_n(InputIterator first, Size n, ForwardIterator result);
+#endif
   template <class ForwardIterator, class T>
   void uninitialized_fill(ForwardIterator first, ForwardIterator last,
 			  const T& x);
   template <class ForwardIterator, class Size, class T>
   void uninitialized_fill_n(ForwardIterator first, Size n, const T& x);
+
+#if __cplusplus >= 201103L
+  template<class T> class default_delete;
+  template<class T> class default_delete<T[]>;
+  template<class T, class D> class unique_ptr;
+  template<class T, class D> class unique_ptr<T[], D>;
+  template<class T, class D>
+    void swap(unique_ptr<T, D>&, unique_ptr<T, D>&) noexcept;
+#if __cplusplus >= 201402L
+  template<class T, class... Args> unique_ptr<T> make_unique(Args&&...);
+#endif
+
+  class bad_weak_ptr;
+  template<class T> class shared_ptr;
+  template<class T, class... Args> shared_ptr<T> make_shared(Args&&... args);
+  template<class T, class A, class... Args>
+  shared_ptr<T> allocate_shared(const A& a, Args&&... args);
+  template<class T> void swap(shared_ptr<T>&, shared_ptr<T>&) noexcept;
+  template<class T> class weak_ptr;
+  template<class T> void swap(weak_ptr<T>&, weak_ptr<T>&) noexcept;
+  template<class T> class owner_less;
+  template<class T> class enable_shared_from_this;
+
+  template<class T, class D> struct hash<unique_ptr<T, D>>;
+  template<class T> struct hash<shared_ptr<T>>;
+#endif
 
   //  lib.auto.ptr, pointers:
   template<class X> class auto_ptr;
