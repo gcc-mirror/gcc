1c19256b70634ab62eeddefed58556bfc4912a90
liushuyu <liushuyu011@gmail.com>
2022-09-26 22:30:33 -0600

    testsuite/rust: add a testcase for testing ...
    ... builtin macro and decl macro mixed expansion

---
+++ gcc/testsuite/rust/compile/builtin_macro_recurse.rs
@@ -0,0 +1,21 @@
+// { dg-additional-options "-fdump-tree-gimple" }
+
+#[rustc_builtin_macro]
+macro_rules! concat {
+    () => {{}};
+}
+
+macro_rules! a {
+    () => { "test" };
+}
+
+macro_rules! b {
+    () => { "canary" };
+}
+
+fn main() {
+    // { dg-final { scan-tree-dump-times {"test1canary"} 1 gimple } }
+    let _ = concat!(a!(), 1, b!());
+    // should not error
+    concat!(a!(), true, b!(),);
+}
