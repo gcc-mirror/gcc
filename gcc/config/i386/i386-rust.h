22e3557e2d52f129f2bbfdc98688b945dba28dc9
Iain Buclaw <ibuclaw@gdcproject.org>
2022-09-29 10:34:18 +0200

    rust: Implement TARGET_RUST_CPU_INFO for i[34567]86-*-* and x86_64-*-*
    There are still quite a lot of the previously reverted i386-rust.cc
    missing, so it's only a partial reimplementation.
    
    gcc/ChangeLog:
    
    	* config/i386/t-i386 (i386-rust.o): New rule.
    	* config/i386/i386-rust.cc: New file.
    	* config/i386/i386-rust.h: New file.

---
+++ gcc/config/i386/i386-rust.h
@@ -0,0 +1,22 @@
+/* Definitions for the Rust front end on the x86 architecture.
+   Copyright (C) 2022 Free Software Foundation, Inc.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+/* In i386-rust.cc  */
+extern void ix86_rust_target_cpu_info (void);
+
+/* Target CPU info for Rust.  */
+#define TARGET_RUST_CPU_INFO ix86_rust_target_cpu_info
