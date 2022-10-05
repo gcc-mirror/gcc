0b7cd54d63c2f8728711f5cb27f6ee66f4862eca
Arthur Cohen <arthur.cohen@embecosm.com>
2022-10-05 09:49:50 +0200

    optional: Remove system includes

--- gcc/rust/util/rust-optional-test.cc
+++ gcc/rust/util/rust-optional-test.cc
@@ -16,9 +16,8 @@
 // along with GCC; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
+#include "rust-system.h"
 #include "rust-optional.h"
-
-#include "config.h"
 #include "selftest.h"
 
 #if CHECKING_P
