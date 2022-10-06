f3f4295ad19f205862fe21ab49db090e7d295557
Andrea Corallo <andrea.corallo@arm.com>
2022-10-06 16:36:28 +0200

    arm: improve tests and fix vddupq*
    gcc/ChangeLog:
    
    	* config/arm/mve.md (mve_vddupq_u<mode>_insn): Fix 'vddup.u'
    	spacing.
    	(mve_vddupq_m_wb_u<mode>_insn): Likewise.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/arm/mve/intrinsics/vddupq_m_n_u16.c: Improve test.
    	* gcc.target/arm/mve/intrinsics/vddupq_m_n_u32.c : Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_m_n_u8.c : Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_m_wb_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_m_wb_u32.c : Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_m_wb_u8.c : Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_n_u16.c : Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_n_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_n_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_wb_u16.c : Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_wb_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_wb_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_x_n_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_x_n_u32.c : Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_x_n_u8.c : Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_x_wb_u16.c : Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_x_wb_u32.c : Likewise.
    	* gcc.target/arm/mve/intrinsics/vddupq_x_wb_u8.c : Likewise.

--- gcc/testsuite/gcc.target/arm/mve/intrinsics/vddupq_wb_u16.c
+++ gcc/testsuite/gcc.target/arm/mve/intrinsics/vddupq_wb_u16.c
@@ -1,21 +1,45 @@
 /* { dg-require-effective-target arm_v8_1m_mve_ok } */
 /* { dg-add-options arm_v8_1m_mve } */
 /* { dg-additional-options "-O2" } */
+/* { dg-final { check-function-bodies "**" "" } } */
 
 #include "arm_mve.h"
 
+/*
+**foo:
+**	...
+**	vddup.u16	q[0-9]+, (?:ip|fp|r[0-9]+), #[0-9]+(?:	@.*|)
+**	...
+*/
 uint16x8_t
 foo (uint32_t *a)
 {
-  return vddupq_wb_u16 (a, 4);
+  return vddupq_wb_u16 (a, 1);
 }
 
-/* { dg-final { scan-assembler "vddup.u16"  }  } */
 
+/*
+**foo1:
+**	...
+**	vddup.u16	q[0-9]+, (?:ip|fp|r[0-9]+), #[0-9]+(?:	@.*|)
+**	...
+*/
 uint16x8_t
 foo1 (uint32_t *a)
 {
-  return vddupq_u16 (a, 4);
+  return vddupq_u16 (a, 1);
 }
 
-/* { dg-final { scan-assembler "vddup.u16"  }  } */
+/*
+**foo2:
+**	...
+**	vddup.u16	q[0-9]+, (?:ip|fp|r[0-9]+), #[0-9]+(?:	@.*|)
+**	...
+*/
+uint16x8_t
+foo2 ()
+{
+  return vddupq_u16 (1, 1);
+}
+
+/* { dg-final { scan-assembler-not "__ARM_undef" } } */
