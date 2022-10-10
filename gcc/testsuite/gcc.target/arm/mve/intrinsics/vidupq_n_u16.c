e9fe7b23b82942faffcdf4803eda4ade7b708a8a
Andrea Corallo <andrea.corallo@arm.com>
2022-10-10 10:23:39 +0200

    arm: improve vidupq* tests
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/arm/mve/intrinsics/vidupq_m_n_u16.c: Improve tests.
    	* gcc.target/arm/mve/intrinsics/vidupq_m_n_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_m_n_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_m_wb_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_m_wb_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_m_wb_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_n_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_n_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_n_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_wb_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_wb_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_wb_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_x_n_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_x_n_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_x_n_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_x_wb_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_x_wb_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vidupq_x_wb_u8.c: Likewise.

--- gcc/testsuite/gcc.target/arm/mve/intrinsics/vidupq_n_u16.c
+++ gcc/testsuite/gcc.target/arm/mve/intrinsics/vidupq_n_u16.c
@@ -1,21 +1,45 @@
 /* { dg-require-effective-target arm_v8_1m_mve_ok } */
 /* { dg-add-options arm_v8_1m_mve } */
 /* { dg-additional-options "-O2" } */
+/* { dg-final { check-function-bodies "**" "" } } */
 
 #include "arm_mve.h"
 
+/*
+**foo:
+**	...
+**	vidup.u16	q[0-9]+, (?:ip|fp|r[0-9]+), #[0-9]+(?:	@.*|)
+**	...
+*/
 uint16x8_t
 foo (uint32_t a)
 {
-  return vidupq_n_u16 (a, 4);
+  return vidupq_n_u16 (a, 1);
 }
 
-/* { dg-final { scan-assembler "vidup.u16"  }  } */
 
+/*
+**foo1:
+**	...
+**	vidup.u16	q[0-9]+, (?:ip|fp|r[0-9]+), #[0-9]+(?:	@.*|)
+**	...
+*/
 uint16x8_t
 foo1 (uint32_t a)
 {
-  return vidupq_u16 (a, 4);
+  return vidupq_u16 (a, 1);
 }
 
-/* { dg-final { scan-assembler "vidup.u16"  }  } */
+/*
+**foo2:
+**	...
+**	vidup.u16	q[0-9]+, (?:ip|fp|r[0-9]+), #[0-9]+(?:	@.*|)
+**	...
+*/
+uint16x8_t
+foo2 ()
+{
+  return vidupq_u16 (1, 1);
+}
+
+/* { dg-final { scan-assembler-not "__ARM_undef" } } */
