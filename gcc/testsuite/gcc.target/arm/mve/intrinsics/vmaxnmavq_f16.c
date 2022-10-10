2c5bb2513f2cfaf0c28aa933fb68dc09a4b01862
Andrea Corallo <andrea.corallo@arm.com>
2022-10-10 16:23:21 +0200

    arm: improve tests for vmax*
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/arm/mve/intrinsics/vmaxaq_m_s16.c: Improve test.
    	* gcc.target/arm/mve/intrinsics/vmaxaq_m_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxaq_m_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxaq_s16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxaq_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxaq_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxavq_p_s16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxavq_p_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxavq_p_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxavq_s16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxavq_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxavq_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmaq_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmaq_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmaq_m_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmaq_m_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmavq_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmavq_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmavq_p_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmavq_p_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmq_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmq_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmq_m_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmq_m_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmq_x_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmq_x_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmvq_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmvq_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmvq_p_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxnmvq_p_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_m_s16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_m_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_m_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_m_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_m_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_m_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_s16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_x_s16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_x_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_x_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_x_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_x_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxq_x_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxvq_p_s16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxvq_p_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxvq_p_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxvq_p_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxvq_p_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxvq_p_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxvq_s16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxvq_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxvq_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxvq_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxvq_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmaxvq_u8.c: Likewise.

--- gcc/testsuite/gcc.target/arm/mve/intrinsics/vmaxnmavq_f16.c
+++ gcc/testsuite/gcc.target/arm/mve/intrinsics/vmaxnmavq_f16.c
@@ -1,9 +1,16 @@
 /* { dg-require-effective-target arm_v8_1m_mve_fp_ok } */
 /* { dg-add-options arm_v8_1m_mve_fp } */
 /* { dg-additional-options "-O2" } */
+/* { dg-final { check-function-bodies "**" "" } } */
 
 #include "arm_mve.h"
 
+/*
+**foo:
+**	...
+**	vmaxnmav.f16	(?:ip|fp|r[0-9]+), q[0-9]+(?:	@.*|)
+**	...
+*/
 float16_t
 foo (float16_t a, float16x8_t b)
 {
@@ -11,18 +18,28 @@ foo (float16_t a, float16x8_t b)
 }
 
 
+/*
+**foo1:
+**	...
+**	vmaxnmav.f16	(?:ip|fp|r[0-9]+), q[0-9]+(?:	@.*|)
+**	...
+*/
 float16_t
 foo1 (float16_t a, float16x8_t b)
 {
   return vmaxnmavq (a, b);
 }
 
-
+/*
+**foo2:
+**	...
+**	vmaxnmav.f16	(?:ip|fp|r[0-9]+), q[0-9]+(?:	@.*|)
+**	...
+*/
 float16_t
-foo2 (float32_t a, float16x8_t b)
+foo2 (float16x8_t b)
 {
-  return vmaxnmavq (a, b);
+  return vmaxnmavq (1.1, b);
 }
 
 /* { dg-final { scan-assembler-not "__ARM_undef" } } */
-/* { dg-final { scan-assembler-times "vmaxnmav.f16" 3 } } */
--- gcc/testsuite/gcc.target/arm/mve/intrinsics/vmaxvq_s16.c
+++ gcc/testsuite/gcc.target/arm/mve/intrinsics/vmaxvq_s16.c
@@ -1,9 +1,16 @@
 /* { dg-require-effective-target arm_v8_1m_mve_ok } */
 /* { dg-add-options arm_v8_1m_mve } */
 /* { dg-additional-options "-O2" } */
+/* { dg-final { check-function-bodies "**" "" } } */
 
 #include "arm_mve.h"
 
+/*
+**foo:
+**	...
+**	vmaxv.s16	(?:ip|fp|r[0-9]+), q[0-9]+(?:	@.*|)
+**	...
+*/
 int16_t
 foo (int16_t a, int16x8_t b)
 {
@@ -11,18 +18,16 @@ foo (int16_t a, int16x8_t b)
 }
 
 
+/*
+**foo1:
+**	...
+**	vmaxv.s16	(?:ip|fp|r[0-9]+), q[0-9]+(?:	@.*|)
+**	...
+*/
 int16_t
 foo1 (int16_t a, int16x8_t b)
 {
   return vmaxvq (a, b);
 }
 
-
-int16_t
-foo2 (int8_t a, int16x8_t b)
-{
-  return vmaxvq (a, b);
-}
-
 /* { dg-final { scan-assembler-not "__ARM_undef" } } */
-/* { dg-final { scan-assembler-times "vmaxv.s16" 3 } } */
