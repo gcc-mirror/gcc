c70b44f2c1adf9a9b1c64105d07f177e86139fca
Patrick Palka <ppalka@redhat.com>
2022-10-18 10:51:52 -0400

    libstdc++: Implement ranges::stride_view from P1899R3
    libstdc++-v3/ChangeLog:
    
    	* include/std/ranges (stride_view): Define.
    	(stride_view::_Iterator): Define.
    	(views::__detail::__can_stride_view): Define.
    	(views::_Stride, views::stride): Define.
    	* testsuite/std/ranges/adaptors/stride/1.cc: New test.

---
+++ libstdc++-v3/testsuite/std/ranges/adaptors/stride/1.cc
@@ -0,0 +1,73 @@
+// { dg-options "-std=gnu++23" }
+// { dg-do run { target c++23 } }
+
+#include <ranges>
+#include <algorithm>
+#include <testsuite_hooks.h>
+#include <testsuite_iterators.h>
+
+namespace ranges = std::ranges;
+namespace views = std::views;
+
+constexpr bool
+test01()
+{
+  int x[] = {1, 2, 3, 4, 5, 6, 7};
+
+  auto v2 = x | views::stride(2);
+  const auto i0 = v2.begin(), i1 = v2.begin() + 1;
+  VERIFY( i0 + 1 - 1 == i0 );
+  VERIFY( i0 != i1 );
+  VERIFY( i0 < i1 );
+  VERIFY( i0 <= i0 );
+  VERIFY( i0 >= i0 );
+  VERIFY( v2.end() > i1 );
+  VERIFY( i1 - i0 == 1 );
+  VERIFY( i0 - i1 == -1 );
+  VERIFY( v2.end() - i1 == 3 );
+  VERIFY( i1 - v2.end() == -3 );
+  auto i2 = v2.begin();
+  i2 += 2;
+  i2 -= -2;
+  VERIFY( i2 == v2.end() );
+  VERIFY( ranges::size(v2) == 4 );
+  VERIFY( ranges::equal(v2, (int[]){1, 3, 5, 7}) );
+  VERIFY( ranges::equal(v2 | views::reverse, (int[]){7, 5, 3, 1}) );
+  VERIFY( v2.stride() == 2 );
+
+  auto v1 = x | views::stride(1);
+  VERIFY( ranges::size(v1) == ranges::size(x) );
+  VERIFY( ranges::equal(v1, x) );
+  VERIFY( ranges::equal(v1 | views::reverse, x | views::reverse) );
+  VERIFY( v1.stride() == 1 );
+
+  auto v5 = x | views::stride(5);
+  VERIFY( ranges::equal(v5, (int[]){1, 6}) );
+  VERIFY( ranges::equal(v5 | views::reverse, (int[]){6, 1}) );
+  VERIFY( v5.stride() == 5 );
+
+  auto v10 = x | views::stride(10);
+  VERIFY( ranges::equal(v10, (int[]){1}) );
+  VERIFY( ranges::equal(v10 | views::reverse, (int[]){1}) );
+  VERIFY( v10.stride() == 10 );
+
+  return true;
+}
+
+template<class container>
+void
+test02()
+{
+  int x[] = {1, 2, 3, 4, 5, 6, 7, 8};
+  container rx(x);
+  auto v = rx | views::stride(3);
+  VERIFY( ranges::equal(v, (int[]){1, 4, 7}) );
+}
+
+int
+main()
+{
+  static_assert(test01());
+  test02<__gnu_test::test_input_range<int>>();
+  test02<__gnu_test::test_forward_range<int>>();
+}
