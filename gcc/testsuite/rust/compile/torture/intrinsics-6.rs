1c586a1d1c12dce2a65fb117b577a73ad5cf45ea
Arthur Cohen <arthur.cohen@embecosm.com>
2022-10-26 15:57:54 +0200

    gccrs: intrinsics: Add unchecked operation intrinsics
    gcc/rust/ChangeLog:
    
    	* backend/rust-compile-intrinsic.cc (is_basic_integer_type): New function.
    	(unchecked_op_inner): New handler.
    	(unchecked_op_handler): New handler.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/torture/intrinsics-6.rs: New test.
    	* rust/compile/torture/intrinsics-7.rs: New test.

---
+++ gcc/testsuite/rust/compile/torture/intrinsics-6.rs
@@ -0,0 +1,21 @@
+extern "rust-intrinsic" {
+    pub fn unchecked_add<T>(x: T, y: T) -> T;
+    pub fn unchecked_sub<T>(x: T, y: T) -> T;
+    pub fn unchecked_mul<T>(x: T, y: T) -> T;
+    pub fn unchecked_div<T>(x: T, y: T) -> T;
+    pub fn unchecked_rem<T>(x: T, y: T) -> T;
+    pub fn unchecked_shl<T>(x: T, y: T) -> T;
+    pub fn unchecked_shr<T>(x: T, y: T) -> T;
+}
+
+fn main() -> i32 {
+    let zero0 = unsafe { (1 + 5) - unchecked_add(1, 5) };
+    let zero1 = unsafe { (1 - 5) - unchecked_sub(1, 5) };
+    let zero2 = unsafe { (1 * 5) - unchecked_mul(1, 5) };
+    let zero3 = unsafe { (1 / 5) - unchecked_div(1, 5) };
+    let zero4 = unsafe { (1 % 5) - unchecked_rem(1, 5) };
+    let zero5 = unsafe { (1 << 5) - unchecked_shl(1, 5) };
+    let zero6 = unsafe { (1 >> 5) - unchecked_shr(1, 5) };
+
+    zero0 + zero1 + zero2 + zero3 + zero4 + zero5 + zero6
+}
