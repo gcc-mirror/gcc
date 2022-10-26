b0bde7e2b5bafb111552e634b520cd908a767554
Arthur Cohen <arthur.cohen@embecosm.com>
2022-10-26 17:00:55 +0200

    intrinsics: Implement atomic_load intrinsics
    This commit also comments out the definition of the generic atomic_store
    builtins, with an explanation as to why
    
    Co-authored-by: Marc Poulhies <dkm@kataplop.net>

---
+++ gcc/testsuite/rust/execute/torture/atomic_load.rs
@@ -0,0 +1,31 @@
+trait Copy {}
+
+extern "rust-intrinsic" {
+    pub fn atomic_load_seqcst<T: Copy>(src: *const T) -> T;
+    pub fn atomic_load_acquire<T: Copy>(src: *const T) -> T;
+    pub fn atomic_load_relaxed<T: Copy>(src: *const T) -> T;
+    pub fn atomic_load_unordered<T: Copy>(src: *const T) -> T;
+}
+
+fn main() -> i32 {
+    let one;
+    let two;
+    let three;
+    let four;
+
+    unsafe {
+        let mut src = 1;
+        one = atomic_load_seqcst(&src);
+
+        src = 2;
+        two = atomic_load_acquire(&src);
+
+        src = 3;
+        three = atomic_load_relaxed(&src);
+
+        src = 4;
+        four = atomic_load_unordered(&src);
+    }
+
+    (four + three + two + one) - 10
+}
