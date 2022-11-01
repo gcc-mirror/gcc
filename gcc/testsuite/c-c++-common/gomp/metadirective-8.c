3ae3f87962bf6338d1f52dd0ee6d890d040efb1f
Marcel Vollweiler <marcel@codesourcery.com>
2022-11-01 13:05:18 +0000

    openmp: Bugfix in omp_expand_metadirective for same blocks/edges to be deleted.
    This patch handles an ICE that is thrown in omp_expand_metadirective when a
    basic_block for a metadirective label is tried to be deleted multiple times.
    To avoid this situation, processed labels are added to the already existing
    list of labels that are not intended to be deleted.
    
    The issue occured in the attached test case.
    
    gcc/ChangeLog:
    
    	* omp-expand-metadirective.cc (omp_expand_metadirective): Add already
    	processed labels to "labels" (the list of labels not to be deleted).
    
    gcc/testsuite/ChangeLog:
    
    	* c-c++-common/gomp/metadirective-8.c: New test.

---
+++ gcc/testsuite/c-c++-common/gomp/metadirective-8.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+
+#define N 256
+
+void f ()
+{
+  int i;
+  int a[N];
+
+  #pragma omp metadirective \
+      when( device={kind(nohost)}: nothing ) \
+      when( device={arch("nvptx")}: nothing) \
+      default( parallel for)
+    for (i = 0; i < N; i++)
+      a[i] = i;
+}
