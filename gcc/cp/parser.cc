2b0ae7fb91f64fb005abf7d7903fd4c0764bb45c
Jason Merrill <jason@redhat.com>
2022-11-26 11:13:55 -0500

    c++: simple-requirement starting with 'typename' [PR101733]
    Usually a requirement starting with 'typename' is a type-requirement, but it
    might be a simple-requirement such as a functional cast to a typename-type.
    
    	PR c++/101733
    
    gcc/cp/ChangeLog:
    
    	* parser.cc (cp_parser_requirement): Parse tentatively for the
    	'typename' case.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp2a/concepts-requires32.C: New test.

--- gcc/cp/parser.cc
+++ gcc/cp/parser.cc
@@ -30737,7 +30737,20 @@ cp_parser_requirement (cp_parser *parser)
   if (cp_lexer_next_token_is (parser->lexer, CPP_OPEN_BRACE))
     return cp_parser_compound_requirement (parser);
   else if (cp_lexer_next_token_is_keyword (parser->lexer, RID_TYPENAME))
-    return cp_parser_type_requirement (parser);
+    {
+      /* It's probably a type-requirement.  */
+      cp_parser_parse_tentatively (parser);
+      tree req = cp_parser_type_requirement (parser);
+      if (cp_parser_parse_definitely (parser))
+	return req;
+      /* No, maybe it's something like typename T::type(); */
+      cp_parser_parse_tentatively (parser);
+      req = cp_parser_simple_requirement (parser);
+      if (cp_parser_parse_definitely (parser))
+	return req;
+      /* Non-tentative for the error.  */
+      return cp_parser_type_requirement (parser);
+    }
   else if (cp_lexer_next_token_is_keyword (parser->lexer, RID_REQUIRES))
     return cp_parser_nested_requirement (parser);
   else
