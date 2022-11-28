4fdbf8cb806575df6e6c8f249ab2e778bee70899
Andrea Corallo <andrea.corallo@arm.com>
2022-11-28 17:11:01 +0100

    arm: improve tests for vmulhq*
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/arm/mve/intrinsics/vmulhq_m_s16.c: Use
    	check-function-bodies instead of scan-assembler checks.  Use
    	extern "C" for C++ testing.
    	* gcc.target/arm/mve/intrinsics/vmulhq_m_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_m_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_m_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_m_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_m_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_s16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_x_s16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_x_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_x_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_x_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_x_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmulhq_x_u8.c: Likewise.
    
    (cherry picked from commit 6c61fac9016891c5444be859b080872ce96b63cf)

--- gcc/testsuite/gcc.target/arm/mve/intrinsics/vmulhq_u16.c
+++ gcc/testsuite/gcc.target/arm/mve/intrinsics/vmulhq_u16.c
@@ -1,21 +1,41 @@
 /* { dg-require-effective-target arm_v8_1m_mve_ok } */
 /* { dg-add-options arm_v8_1m_mve } */
 /* { dg-additional-options "-O2" } */
+/* { dg-final { check-function-bodies "**" "" } } */
 
 #include "arm_mve.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+**foo:
+**	...
+**	vmulh.u16	q[0-9]+, q[0-9]+, q[0-9]+(?:	@.*|)
+**	...
+*/
 uint16x8_t
 foo (uint16x8_t a, uint16x8_t b)
 {
   return vmulhq_u16 (a, b);
 }
 
-/* { dg-final { scan-assembler "vmulh.u16"  }  } */
 
+/*
+**foo1:
+**	...
+**	vmulh.u16	q[0-9]+, q[0-9]+, q[0-9]+(?:	@.*|)
+**	...
+*/
 uint16x8_t
 foo1 (uint16x8_t a, uint16x8_t b)
 {
   return vmulhq (a, b);
 }
 
-/* { dg-final { scan-assembler "vmulh.u16"  }  } */
+#ifdef __cplusplus
+}
+#endif
+
+/* { dg-final { scan-assembler-not "__ARM_undef" } } */
