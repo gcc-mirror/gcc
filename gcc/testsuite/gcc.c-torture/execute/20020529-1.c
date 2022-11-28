dcae798b26864d12e43840f3cae75c01eaa11eae
Thomas Schwinge <thomas@codesourcery.com>
2022-11-28 10:37:26 +0100

    nvptx: Re-enable all variants of 'gcc.c-torture/execute/20020529-1.c'
    Generally PASSes with:
    
        $ ptxas --version
        ptxas: NVIDIA (R) Ptx optimizing assembler
        Copyright (c) 2005-2018 NVIDIA Corporation
        Built on Sun_Sep__9_21:06:46_CDT_2018
        Cuda compilation tools, release 10.0, V10.0.145
    
    ..., and execution with 'Driver Version: 361.93.02'.
    
    Only the '-O1' execution test FAILs (pre-existing; to be analyzed later):
    
        nvptx-run: error getting kernel result: an illegal memory access was encountered (CUDA_ERROR_ILLEGAL_ADDRESS, 700)
    
    	gcc/testsuite/
    	* gcc.c-torture/execute/20020529-1.c: Re-enable all variants for
    	nvptx.

--- gcc/testsuite/gcc.c-torture/execute/20020529-1.c
+++ gcc/testsuite/gcc.c-torture/execute/20020529-1.c
@@ -12,10 +12,6 @@
    forced a splitter through the output pattern "#", but there was no
    matching splitter.  */
 
-/* The ptx assembler appears to clobber 'b' inside foo during the f1 call.
-   Reported to nvidia 2016-05-18.  */
-/* { dg-skip-if "PTX assembler bug" { nvptx-*-* } { "-O0" } { "" } } */
-
 void abort (void);
 void exit (int);
 
