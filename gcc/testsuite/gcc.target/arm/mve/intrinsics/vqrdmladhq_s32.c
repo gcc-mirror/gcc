98c45781be3fed8ed8c8819cf196faa2b5753892
Andrea Corallo <andrea.corallo@arm.com>
2022-11-28 17:42:09 +0100

    arm: improve tests for vqrdmladhq*
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/arm/mve/intrinsics/vqrdmladhq_m_s16.c: Use
    	check-function-bodies instead of scan-assembler checks.  Use
    	extern "C" for C++ testing.
    	* gcc.target/arm/mve/intrinsics/vqrdmladhq_m_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vqrdmladhq_m_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vqrdmladhq_s16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vqrdmladhq_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vqrdmladhq_s8.c: Likewise.
    
    (cherry picked from commit 6c43db2480f0e50878ba4da4d47c6621513fe91f)

--- gcc/testsuite/gcc.target/arm/mve/intrinsics/vqrdmladhq_s32.c
+++ gcc/testsuite/gcc.target/arm/mve/intrinsics/vqrdmladhq_s32.c
@@ -1,21 +1,41 @@
 /* { dg-require-effective-target arm_v8_1m_mve_ok } */
 /* { dg-add-options arm_v8_1m_mve } */
 /* { dg-additional-options "-O2" } */
+/* { dg-final { check-function-bodies "**" "" } } */
 
 #include "arm_mve.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+**foo:
+**	...
+**	vqrdmladh.s32	q[0-9]+, q[0-9]+, q[0-9]+(?:	@.*|)
+**	...
+*/
 int32x4_t
 foo (int32x4_t inactive, int32x4_t a, int32x4_t b)
 {
   return vqrdmladhq_s32 (inactive, a, b);
 }
 
-/* { dg-final { scan-assembler "vqrdmladh.s32"  }  } */
 
+/*
+**foo1:
+**	...
+**	vqrdmladh.s32	q[0-9]+, q[0-9]+, q[0-9]+(?:	@.*|)
+**	...
+*/
 int32x4_t
 foo1 (int32x4_t inactive, int32x4_t a, int32x4_t b)
 {
   return vqrdmladhq (inactive, a, b);
 }
 
-/* { dg-final { scan-assembler "vqrdmladh.s32"  }  } */
+#ifdef __cplusplus
+}
+#endif
+
+/* { dg-final { scan-assembler-not "__ARM_undef" } } */
