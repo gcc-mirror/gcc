455461dfccc0747c4731c4f69d8ae3a353cd9798
Andrea Corallo <andrea.corallo@arm.com>
2022-11-28 17:34:41 +0100

    arm: improve tests for vcmlaq*
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/arm/mve/intrinsics/vcmlaq_f16.c: Use
    	check-function-bodies instead of scan-assembler checks.  Use
    	extern "C" for C++ testing.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_m_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_m_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_rot180_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_rot180_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_rot180_m_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_rot180_m_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_rot270_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_rot270_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_rot270_m_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_rot270_m_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_rot90_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_rot90_f32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_rot90_m_f16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vcmlaq_rot90_m_f32.c: Likewise.
    
    (cherry picked from commit 0ea30b2a83c908323d73309b6f698331607eb2e0)

--- gcc/testsuite/gcc.target/arm/mve/intrinsics/vcmlaq_rot270_m_f32.c
+++ gcc/testsuite/gcc.target/arm/mve/intrinsics/vcmlaq_rot270_m_f32.c
@@ -1,23 +1,49 @@
 /* { dg-require-effective-target arm_v8_1m_mve_fp_ok } */
 /* { dg-add-options arm_v8_1m_mve_fp } */
 /* { dg-additional-options "-O2" } */
+/* { dg-final { check-function-bodies "**" "" } } */
 
 #include "arm_mve.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+**foo:
+**	...
+**	vmsr	p0, (?:ip|fp|r[0-9]+)(?:	@.*|)
+**	...
+**	vpst(?:	@.*|)
+**	...
+**	vcmlat.f32	q[0-9]+, q[0-9]+, q[0-9]+, #270(?:	@.*|)
+**	...
+*/
 float32x4_t
 foo (float32x4_t a, float32x4_t b, float32x4_t c, mve_pred16_t p)
 {
   return vcmlaq_rot270_m_f32 (a, b, c, p);
 }
 
-/* { dg-final { scan-assembler "vpst" } } */
-/* { dg-final { scan-assembler "vcmlat.f32"  }  } */
 
+/*
+**foo1:
+**	...
+**	vmsr	p0, (?:ip|fp|r[0-9]+)(?:	@.*|)
+**	...
+**	vpst(?:	@.*|)
+**	...
+**	vcmlat.f32	q[0-9]+, q[0-9]+, q[0-9]+, #270(?:	@.*|)
+**	...
+*/
 float32x4_t
 foo1 (float32x4_t a, float32x4_t b, float32x4_t c, mve_pred16_t p)
 {
   return vcmlaq_rot270_m (a, b, c, p);
 }
 
-/* { dg-final { scan-assembler "vpst" } } */
-/* { dg-final { scan-assembler "vcmlat.f32"  }  } */
+#ifdef __cplusplus
+}
+#endif
+
+/* { dg-final { scan-assembler-not "__ARM_undef" } } */
--- gcc/testsuite/gcc.target/arm/mve/intrinsics/vcmlaq_rot90_m_f16.c
+++ gcc/testsuite/gcc.target/arm/mve/intrinsics/vcmlaq_rot90_m_f16.c
@@ -1,23 +1,49 @@
 /* { dg-require-effective-target arm_v8_1m_mve_fp_ok } */
 /* { dg-add-options arm_v8_1m_mve_fp } */
 /* { dg-additional-options "-O2" } */
+/* { dg-final { check-function-bodies "**" "" } } */
 
 #include "arm_mve.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+**foo:
+**	...
+**	vmsr	p0, (?:ip|fp|r[0-9]+)(?:	@.*|)
+**	...
+**	vpst(?:	@.*|)
+**	...
+**	vcmlat.f16	q[0-9]+, q[0-9]+, q[0-9]+, #90(?:	@.*|)
+**	...
+*/
 float16x8_t
 foo (float16x8_t a, float16x8_t b, float16x8_t c, mve_pred16_t p)
 {
   return vcmlaq_rot90_m_f16 (a, b, c, p);
 }
 
-/* { dg-final { scan-assembler "vpst" } } */
-/* { dg-final { scan-assembler "vcmlat.f16"  }  } */
 
+/*
+**foo1:
+**	...
+**	vmsr	p0, (?:ip|fp|r[0-9]+)(?:	@.*|)
+**	...
+**	vpst(?:	@.*|)
+**	...
+**	vcmlat.f16	q[0-9]+, q[0-9]+, q[0-9]+, #90(?:	@.*|)
+**	...
+*/
 float16x8_t
 foo1 (float16x8_t a, float16x8_t b, float16x8_t c, mve_pred16_t p)
 {
   return vcmlaq_rot90_m (a, b, c, p);
 }
 
-/* { dg-final { scan-assembler "vpst" } } */
-/* { dg-final { scan-assembler "vcmlat.f16"  }  } */
+#ifdef __cplusplus
+}
+#endif
+
+/* { dg-final { scan-assembler-not "__ARM_undef" } } */
