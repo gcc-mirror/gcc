e6f52130d0db50ca3f59e45676db9431fa44457c
Andrea Corallo <andrea.corallo@arm.com>
2022-11-28 17:12:08 +0100

    arm: improve tests for vmullbq*
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_m_s16.c: Use
    	check-function-bodies instead of scan-assembler checks.  Use
    	extern "C" for C++ testing.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_m_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_m_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_m_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_m_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_m_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_s16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_x_s16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_x_s32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_x_s8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_x_u16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_x_u32.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_int_x_u8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_poly_m_p16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_poly_m_p8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_poly_p16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_poly_p8.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_poly_x_p16.c: Likewise.
    	* gcc.target/arm/mve/intrinsics/vmullbq_poly_x_p8.c: Likewise.

--- gcc/testsuite/gcc.target/arm/mve/intrinsics/vmullbq_int_s32.c
+++ gcc/testsuite/gcc.target/arm/mve/intrinsics/vmullbq_int_s32.c
@@ -1,21 +1,41 @@
 /* { dg-require-effective-target arm_v8_1m_mve_ok } */
 /* { dg-add-options arm_v8_1m_mve } */
 /* { dg-additional-options "-O2" } */
+/* { dg-final { check-function-bodies "**" "" } } */
 
 #include "arm_mve.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+**foo:
+**	...
+**	vmullb.s32	q[0-9]+, q[0-9]+, q[0-9]+(?:	@.*|)
+**	...
+*/
 int64x2_t
 foo (int32x4_t a, int32x4_t b)
 {
   return vmullbq_int_s32 (a, b);
 }
 
-/* { dg-final { scan-assembler "vmullb.s32"  }  } */
 
+/*
+**foo1:
+**	...
+**	vmullb.s32	q[0-9]+, q[0-9]+, q[0-9]+(?:	@.*|)
+**	...
+*/
 int64x2_t
 foo1 (int32x4_t a, int32x4_t b)
 {
   return vmullbq_int (a, b);
 }
 
-/* { dg-final { scan-assembler "vmullb.s32"  }  } */
+#ifdef __cplusplus
+}
+#endif
+
+/* { dg-final { scan-assembler-not "__ARM_undef" } } */
