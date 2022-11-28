81dcca1c24835c6c9e06e6aa917c40e0f4b1fdd5
Thomas Schwinge <thomas@codesourcery.com>
2022-11-28 10:37:26 +0100

    nvptx: Re-enable 'gcc.dg/special/weak-2.c'
    PASSes with:
    
        $ ptxas --version
        ptxas: NVIDIA (R) Ptx optimizing assembler
        Copyright (c) 2005-2018 NVIDIA Corporation
        Built on Sun_Sep__9_21:06:46_CDT_2018
        Cuda compilation tools, release 10.0, V10.0.145
    
    ..., and execution with 'Driver Version: 361.93.02'.
    
    	gcc/testsuite/
    	* gcc.dg/special/weak-2.c: Re-enable for nvptx.

--- gcc/testsuite/gcc.dg/special/weak-2.c
+++ gcc/testsuite/gcc.dg/special/weak-2.c
@@ -2,10 +2,6 @@
 /* { dg-require-weak "" } */
 /* { dg-additional-sources "weak-2a.c weak-2b.c" } */
 
-/* NVPTX's implementation of weak is broken when a strong symbol is in
-   a later object file than the weak definition.   */
-/* { dg-skip-if "" { "nvptx-*-*" } } */
-
 #include <stdlib.h>
 
 extern int foo(void);
