b6c6a3d64f2e4e9347733290aca3c75898c44b2e
Ian Lance Taylor <iant@golang.org>
2022-11-29 15:31:39 -0800

    syscall, runtime: always call XSI strerror_r
    This does the right thing for either glibc or musl on GNU/Linux.
    
    Based on patch by Sören Tempel.
    
    Reviewed-on: https://go-review.googlesource.com/c/gofrontend/+/454176

--- libgo/go/syscall/errstr.go
+++ libgo/go/syscall/errstr.go
@@ -4,23 +4,19 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !hurd && !linux
-// +build !hurd,!linux
-
 package syscall
 
-//sysnb	strerror_r(errnum int, buf []byte) (err Errno)
-//strerror_r(errnum _C_int, buf *byte, buflen Size_t) _C_int
+import "internal/bytealg"
+
+//extern go_strerror
+func go_strerror(_C_int, *byte, Size_t) _C_int
 
 func Errstr(errnum int) string {
-	for len := 128; ; len *= 2 {
-		b := make([]byte, len)
-		errno := strerror_r(errnum, b)
+	for size := 128; ; size *= 2 {
+		b := make([]byte, size)
+		errno := go_strerror(_C_int(errnum), &b[0], Size_t(len(b)))
 		if errno == 0 {
-			i := 0
-			for b[i] != 0 {
-				i++
-			}
+			i := bytealg.IndexByte(b, 0)
 			// Lowercase first letter: Bad -> bad, but
 			// STREAM -> STREAM.
 			if i > 1 && 'A' <= b[0] && b[0] <= 'Z' && 'a' <= b[1] && b[1] <= 'z' {
@@ -29,7 +25,7 @@ func Errstr(errnum int) string {
 			return string(b[:i])
 		}
 		if errno != ERANGE {
-			return "errstr failure"
+			return "strerror_r failure"
 		}
 	}
 }
