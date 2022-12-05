31dd14e197c00891751994839b16a5a193a71820
Philip Herron <philip.herron@embecosm.com>
2022-12-05 00:31:18 +0000

    Skip this debug test case which is failing on the latest mac-os devtools and its only for debug info

--- gcc/testsuite/rust/debug/chartype.rs
+++ gcc/testsuite/rust/debug/chartype.rs
@@ -1,10 +1,11 @@
-// 'char' should use DW_ATE_UTF
-fn main () {
-    let c = 'x';
 // { dg-do compile }
-// Use -w to avoid warnings about the unused variables
-// DW_ATE_UTF entered in DWARF 4.
+// { dg-skip-if "see https://github.com/Rust-GCC/gccrs/pull/1632" { *-*-darwin* } }
 // { dg-options "-w -gdwarf-4 -dA" }
-// DW_ATE_UTF = 0x10
-// { dg-final { scan-assembler "0x10\[ \t]\[^\n\r]* DW_AT_encoding" } } */
+// 'char' should use DW_ATE_UTF
+fn main() {
+    let c = 'x';
+    // Use -w to avoid warnings about the unused variables
+    // DW_ATE_UTF entered in DWARF 4.
+    // DW_ATE_UTF = 0x10
+    // { dg-final { scan-assembler "0x10\[ \t]\[^\n\r]* DW_AT_encoding" } } */
 }
