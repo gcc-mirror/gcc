68e51bd0a85794cd437d3e740357dfef84dc560d
Marek Polacek <polacek@redhat.com>
2022-12-16 12:28:43 -0500

    c-family: Fix ICE with -Wsuggest-attribute [PR98487]
    Here we crash because check_function_format was using TREE_PURPOSE
    directly rather than using get_attribute_name.
    
    	PR c/98487
    
    gcc/c-family/ChangeLog:
    
    	* c-format.cc (check_function_format): Use get_attribute_name.
    
    gcc/testsuite/ChangeLog:
    
    	* c-c++-common/Wsuggest-attribute-1.c: New test.

--- gcc/c-family/c-format.cc
+++ gcc/c-family/c-format.cc
@@ -1215,7 +1215,7 @@ check_function_format (const_tree fn, tree attrs, int nargs,
 	      for (c = TYPE_ATTRIBUTES (TREE_TYPE (current_function_decl));
 		   c;
 		   c = TREE_CHAIN (c))
-		if (is_attribute_p ("format", TREE_PURPOSE (c))
+		if (is_attribute_p ("format", get_attribute_name (c))
 		    && (decode_format_type (IDENTIFIER_POINTER
 					    (TREE_VALUE (TREE_VALUE (c))))
 			== info.format_type))
