308d4efc00da65ecef294e863183b569fbeaee32
Thomas Schwinge <thomas@codesourcery.com>
2022-12-19 17:10:52 +0100

    Add 'gcc.target/nvptx/softstack-decl-1.c', 'gcc.target/nvptx/uniform-simt-decl-1.c'
    ... to document the status quo re implicit (via 'need_softstack_decl',
    'need_unisimt_decl') and explicit declarations of '__nvptx_stacks',
    '__nvptx_uni'.
    
    	gcc/testsuite/
    	* gcc.target/nvptx/softstack-decl-1.c: New.
    	* gcc.target/nvptx/uniform-simt-decl-1.c: Likewise.

---
+++ gcc/testsuite/gcc.target/nvptx/softstack-decl-1.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-options {-save-temps -O0 -msoft-stack} } */
+
+extern void *__nvptx_stacks[32] __attribute__((shared,nocommon));
+
+void *f()
+{
+  /* Implicit '__nvptx_stacks' usage for frame; per 'init_softstack_frame':
+     { dg-final { scan-assembler-times {mov\.u64 %fstmp2, __nvptx_stacks;} 1 } }
+  */
+  void *stack_array[123];
+  /* Explicit '__nvptx_stacks' usage.  */ 
+  stack_array[5] = __nvptx_stacks[0];
+  return stack_array[5];
+}
+
+/* The implicit (via 'need_softstack_decl') and explicit declarations of
+   '__nvptx_stacks' are both emitted:
+   { dg-final { scan-assembler-times {(?n)\.extern .* __nvptx_stacks\[32\];} 2 } }
+*/
