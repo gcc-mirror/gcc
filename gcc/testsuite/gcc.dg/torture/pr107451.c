7b2cf5041460859ca4f58e5da1308b7ef9129d8b
Richard Biener <rguenther@suse.de>
2022-12-22 09:36:17 +0100

    tree-optimization/107451 - SLP load vectorization issue
    When vectorizing SLP loads with permutations we can access excess
    elements when the load vector type is bigger than the group size
    and the vectorization factor covers less groups than necessary
    to fill it.  Since we know the code will only access up to
    group_size * VF elements in the unpermuted vector we can simply
    fill the rest of the vector with whatever we want.  For simplicity
    this patch chooses to repeat the last group.
    
    	PR tree-optimization/107451
    	* tree-vect-stmts.cc (vectorizable_load): Avoid loading
    	SLP group members from group numbers in excess of the
    	vectorization factor.
    
    	* gcc.dg/torture/pr107451.c: New testcase.

---
+++ gcc/testsuite/gcc.dg/torture/pr107451.c
@@ -0,0 +1,27 @@
+/* { dg-do run } */
+/* { dg-additional-options "-ftree-vectorize -fno-vect-cost-model" } */
+/* { dg-additional-options "-mavx2" { target avx2_runtime } } */
+
+double getdot(int n, const double *x, int inc_x, const double *y)
+{
+  int i, ix = 0;
+  double dot[4] = { 0.0, 0.0, 0.0, 0.0 } ;
+
+  for(i = 0; i < n; i++) {
+      dot[0] += x[ix]   * y[ix]   ;
+      dot[1] += x[ix+1] * y[ix+1] ;
+      dot[2] += x[ix]   * y[ix+1] ;
+      dot[3] += x[ix+1] * y[ix]   ;
+      ix += inc_x ;
+  }
+
+  return dot[0] + dot[1] + dot[2] + dot[3];
+}
+
+int main()
+{
+  double x[2] = {0, 0}, y[2] = {0, 0};
+  if (getdot(1, x, 4096*4096, y) != 0.)
+    __builtin_abort ();
+  return 0;
+}
