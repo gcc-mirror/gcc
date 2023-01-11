83e5f634d746a9b57b1c63eb33e072f53f61995d
Owen Avery <powerboat9.gamer@gmail.com>
2023-01-11 11:56:05 -0500

    gccrs: Add type resolution for grouped patterns
    gcc/rust/ChangeLog:
    
    	* typecheck/rust-hir-type-check-pattern.cc (TypeCheckPattern::visit): Add proper
    	visitor.
    
    Signed-off-by: Owen Avery <powerboat9.gamer@gmail.com>

--- gcc/rust/typecheck/rust-hir-type-check-pattern.cc
+++ gcc/rust/typecheck/rust-hir-type-check-pattern.cc
@@ -387,10 +387,12 @@ TypeCheckPattern::visit (HIR::IdentifierPattern &)
 }
 
 void
-TypeCheckPattern::visit (HIR::GroupedPattern &)
+TypeCheckPattern::visit (HIR::GroupedPattern &pattern)
 {
-  // TODO
-  gcc_unreachable ();
+  TyTy::BaseType *inner_ty
+    = TypeCheckPattern::Resolve (pattern.get_item ().get (), parent);
+  infered = inner_ty->clone ();
+  infered->set_ref (pattern.get_pattern_mappings ().get_hirid ());
 }
 
 void
