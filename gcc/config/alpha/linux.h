31949fba1222692548b501e4dfa4ddcf7b7fc7c6
Richard Biener <rguenther@suse.de>
2023-01-13 08:50:14 +0100

    alpha: Don't add crtfastmath.o for -shared
    Don't add crtfastmath.o for -shared to avoid altering the FP
    environment when loading a shared library.
    
    	PR target/55522
    	* config/alpha/linux.h (ENDFILE_SPEC): Don't add
    	crtfastmath.o for -shared.

--- gcc/config/alpha/linux.h
+++ gcc/config/alpha/linux.h
@@ -106,7 +106,7 @@ along with GCC; see the file COPYING3.  If not see
 
 #undef	ENDFILE_SPEC
 #define ENDFILE_SPEC \
-  "%{Ofast|ffast-math|funsafe-math-optimizations:crtfastmath.o%s} \
+  "%{Ofast|ffast-math|funsafe-math-optimizations:%{!shared:crtfastmath.o%s}} \
    %{shared|pie:crtendS.o%s;:crtend.o%s} crtn.o%s"
 
 #define LINK_GCC_C_SEQUENCE_SPEC \
