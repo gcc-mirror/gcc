159b0f41adc4c8ead45e12a523470381ce716ff2
liuhongt <hongtao.liu@intel.com>
2023-01-13 16:19:47 +0800

    Don't add crtfastmath.o for -shared.
    Patches [1] and [2] fixed PR55522 for x86-linux but left all other x86
    targets unfixed (x86-cygwin, x86-darwin and x86-mingw32).
    This patch applies a similar change to other specs using crtfastmath.o.
    
    [1] https://gcc.gnu.org/pipermail/gcc-patches/2022-December/608528.html
    [2] https://gcc.gnu.org/pipermail/gcc-patches/2022-December/608529.html
    
    gcc/ChangeLog:
    
    	PR target/55522
    	* config/i386/cygwin.h (ENDFILE_SPEC): Link crtfastmath.o
    	whenever -mdaz-ftz is specified. Don't link crtfastmath.o when
    	-share or -mno-daz-ftz is specified.
    	* config/i386/darwin.h (ENDFILE_SPEC): Ditto.
    	* config/i386/mingw32.h (ENDFILE_SPEC): Ditto.

--- gcc/config/i386/darwin.h
+++ gcc/config/i386/darwin.h
@@ -110,7 +110,7 @@ along with GCC; see the file COPYING3.  If not see
 
 #undef ENDFILE_SPEC
 #define ENDFILE_SPEC \
-  "%{Ofast|ffast-math|funsafe-math-optimizations:crtfastmath.o%s} \
+  "%{mdaz-ftz:crtfastmath.o%s;Ofast|ffast-math|funsafe-math-optimizations:%{!shared:%{!mno-daz-ftz:crtfastmath.o%s}}} \
    %{mpc32:crtprec32.o%s} \
    %{mpc64:crtprec64.o%s} \
    %{mpc80:crtprec80.o%s}" TM_DESTRUCTOR
