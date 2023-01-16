79a81d526babb6ffb6d85b4a05b29269470ab49d
Kewen Lin <linkw@linux.ibm.com>
2023-01-16 02:15:39 -0600

    rs6000: Teach rs6000_opaque_type_invalid_use_p about inline asm [PR108272]
    As PR108272 shows, there are some invalid uses of MMA opaque
    types in inline asm statements.  This patch is to teach the
    function rs6000_opaque_type_invalid_use_p for inline asm,
    check and error any invalid use of MMA opaque types in input
    and output operands.
    
    	PR target/108272
    
    gcc/ChangeLog:
    
    	* config/rs6000/rs6000.c (rs6000_opaque_type_invalid_use_p): Add the
    	support for invalid uses in inline asm, factor out the checking and
    	erroring to lambda function check_and_error_invalid_use.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/powerpc/pr108272-1.c: New test.
    	* gcc.target/powerpc/pr108272-2.c: New test.
    	* gcc.target/powerpc/pr108272-3.c: New test.
    	* gcc.target/powerpc/pr108272-4.c: New test.
    
    (cherry picked from commit 074b0c03eabeb8e9c8de813c81bf87a1f88fdb65)

---
+++ gcc/testsuite/gcc.target/powerpc/pr108272-4.c
@@ -0,0 +1,18 @@
+/* { dg-require-effective-target powerpc_p9modulo_ok } */
+/* If the default cpu type is power10 or later, type __vector_pair is
+   supported.  To keep the test point available all the time, this case
+   specifies -mdejagnu-cpu=power9 here.  */
+/* { dg-options "-mdejagnu-cpu=power9" } */
+
+/* Verify there is no ICE and don't check the error messages on unsupported
+   type since they could be fragile and are not test points of this case.  */
+
+/* { dg-excess-errors "pr108272-4" } */
+
+typedef __vector_pair vpair_t;
+void
+foo (void)
+{
+  vpair_t acc;
+  asm("#..." : "=d"(acc));
+}
