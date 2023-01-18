0e41d8a77887b838de5493c491f411274376227a
Kewen Lin <linkw@linux.ibm.com>
2023-01-18 02:34:19 -0600

    rs6000: Teach rs6000_opaque_type_invalid_use_p about gcall [PR108348]
    PR108348 shows one special case that MMA opaque types are
    used in function arguments and treated as pass by reference,
    it results in one copying from argument to a temp variable,
    since this copying happens before rs6000_function_arg check,
    it can cause ICE without MMA support then.  This patch is to
    teach function rs6000_opaque_type_invalid_use_p to check if
    any function argument in a gcall stmt has the invalid use of
    MMA opaque types.
    
    btw, I checked the handling on return value, it doesn't have
    this kind of issue as its checking and error emission is quite
    early, so this doesn't handle function return value.
    
    	PR target/108348
    
    gcc/ChangeLog:
    
    	* config/rs6000/rs6000.c (rs6000_opaque_type_invalid_use_p): Add the
    	support for invalid uses of MMA opaque type in function arguments.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/powerpc/pr108348-1.c: New test.
    	* gcc.target/powerpc/pr108348-2.c: New test.
    
    (cherry picked from commit 5d9529687deb9ed009361a16c02a7f6c3e2ebbf3)

---
+++ gcc/testsuite/gcc.target/powerpc/pr108348-2.c
@@ -0,0 +1,23 @@
+/* { dg-require-effective-target powerpc_altivec_ok } */
+/* If the default cpu type is power10 or later, type __vector_pair is
+   supported.  To keep the test point available all the time, this case
+   specifies -mdejagnu-cpu=power9 here.  This needs -mabi=no-altivec
+   to do the copying for pass-by-reference function argument on 32 bit
+   environment.  */
+/* { dg-options "-mdejagnu-cpu=power9 -mabi=no-altivec" } */
+
+/* Verify there is no ICE on 32 bit and don't check the error messages
+   on unsupported type since they could be fragile and are not test
+   points of this case.  */
+
+/* { dg-excess-errors "pr108348-2" } */
+
+extern void bar (__vector_pair v);
+
+void
+foo (void)
+{
+  __vector_pair v;
+  bar (v);
+}
+
