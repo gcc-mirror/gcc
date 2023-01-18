7abcef725e40589553a079df9258ae094b811751
Kong Lingling <lingling.kong@intel.com>
2023-01-18 17:23:29 +0800

    [APX NDD] Support APX NDD for optimization patterns of add
    gcc/ChangeLog:
    
    	* config/i386/i386.md: (addsi_1_zext): Add new alternatives for
    	NDD and adjust output templates.
    	(*add<mode>_2): Likewise.
    	(*addsi_2_zext): Likewise.
    	(*add<mode>_3): Likewise.
    	(*addsi_3_zext): Likewise.
    	(*adddi_4): Likewise.
    	(*add<mode>_4): Likewise.
    	(*add<mode>_5): Likewise.
    	(*addv<mode>4): Likewise.
    	(*addv<mode>4_1): Likewise.
    	(*add<mode>3_cconly_overflow_1): Likewise.
    	(*add<mode>3_cc_overflow_1): Likewise.
    	(*addsi3_zext_cc_overflow_1): Likewise.
    	(*add<mode>3_cconly_overflow_2): Likewise.
    	(*add<mode>3_cc_overflow_2): Likewise.
    	(*addsi3_zext_cc_overflow_2): Likewise.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/i386/apx-ndd.c: Add more test.

--- gcc/testsuite/gcc.target/i386/apx-ndd.c
+++ gcc/testsuite/gcc.target/i386/apx-ndd.c
@@ -2,20 +2,43 @@
 /* { dg-options "-mapxf -march=x86-64 -O2" } */
 /* { dg-final { scan-assembler-not "movl"} } */
 
-int foo (int *a)
-{
-  int b = *a - 1;
-  return b;
-}
+#define FOO(TYPE, OP_NAME, OP)   \
+TYPE				 \
+__attribute__ ((noipa)) 	 \
+foo_##OP_NAME##_##TYPE (TYPE *a) \
+{				 \
+  TYPE b = *a OP 1;		 \
+  return b;			 \
+}			
 
-int foo2 (int a, int b)
-{
-  int c = a + b;
-  return c;
-}
+#define FOO1(TYPE, OP_NAME, OP)		 \
+TYPE				  	 \
+__attribute__ ((noipa)) 	  	 \
+foo1_##OP_NAME##_##TYPE (TYPE a, TYPE b) \
+{				 	 \
+  TYPE c = a OP b;		 	 \
+  return c;			 	 \
+}			
 
-int foo3 (int *a, int b)
-{
-  int c = *a + b;
-  return c;
-}
+#define FOO2(TYPE, OP_NAME, OP)		  \
+TYPE				  	  \
+__attribute__ ((noipa)) 	  	  \
+foo2_##OP_NAME##_##TYPE (TYPE *a, TYPE b) \
+{				 	  \
+  TYPE c = *a OP b;		 	  \
+  return c;			 	  \
+}			
+
+FOO (char, add, +)
+FOO1 (char, add, +)
+FOO2 (char, add, +)
+FOO (short, add, +)
+FOO1 (short, add, +)
+FOO2 (short, add, +)
+FOO (int, add, +)
+FOO1 (int, add, +)
+FOO2 (int, add, +)
+FOO (long, add, +)
+FOO1 (long, add, +)
+FOO2 (long, add, +)
+
