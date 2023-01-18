b5ea0f071aca505c82cc8c062e57bf9892900277
Lulu Cheng <chenglulu@loongson.cn>
2023-01-18 11:06:56 +0800

    LoongArch: Fixed a compilation failure with '%c' in inline assembly [PR107731].
    Co-authored-by: Yang Yujie <yangyujie@loongson.cn>
    
    	PR target/107731
    
    gcc/ChangeLog:
    
    	* config/loongarch/loongarch.cc (loongarch_classify_address):
    	Add precessint for CONST_INT.
    	(loongarch_print_operand_reloc): Operand modifier 'c' is supported.
    	(loongarch_print_operand): Increase the processing of '%c'.
    	* doc/extend.texi: Adds documents for LoongArch operand modifiers.
    	And port the public operand modifiers information to this document.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/loongarch/tst-asm-const.c: Moved to...
    	* gcc.target/loongarch/pr107731.c: ...here.

--- gcc/testsuite/gcc.target/loongarch/pr107731.c
+++ gcc/testsuite/gcc.target/loongarch/pr107731.c
@@ -1,13 +1,13 @@
-/* Test asm const. */
 /* { dg-do compile } */
 /* { dg-final { scan-assembler-times "foo:.*\\.long 1061109567.*\\.long 52" 1 } } */
+
 int foo ()
 {
   __asm__ volatile (
           "foo:"
           "\n\t"
-	  ".long %a0\n\t"
-	  ".long %a1\n\t"
+	  ".long %c0\n\t"
+	  ".long %c1\n\t"
 	  :
 	  :"i"(0x3f3f3f3f), "i"(52)
 	  :
