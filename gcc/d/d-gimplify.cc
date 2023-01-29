7e9dd9de169034810b92d47bf78284db731fa5da
Iain Buclaw <ibuclaw@gdcproject.org>
2023-01-29 21:25:40 +0100

    d: Only handle the left-to-right evaluation of a call expression during gimplify
    Removes an unnecessary rewriting of the front-end AST during lowering.
    
    gcc/d/ChangeLog:
    
    	* d-codegen.cc (d_build_call): Remove front-end expansion of
    	side-effects in a call expression.
    	* d-gimplify.cc (d_gimplify_call_expr): Gimplify the callee before its
    	arguments.

--- gcc/d/d-gimplify.cc
+++ gcc/d/d-gimplify.cc
@@ -162,6 +162,15 @@ d_gimplify_call_expr (tree *expr_p, gimple_seq *pre_p)
   if (!has_side_effects)
     return GS_UNHANDLED;
 
+  /* Evaluate the callee before calling it.  */
+  tree new_call_fn = CALL_EXPR_FN (*expr_p);
+
+  if (gimplify_expr (&new_call_fn, pre_p, NULL,
+		     is_gimple_call_addr, fb_rvalue) == GS_ERROR)
+    return GS_ERROR;
+
+  CALL_EXPR_FN (*expr_p) = new_call_fn;
+
   /* Leave the last argument for gimplify_call_expr.  */
   for (int i = 0; i < nargs - 1; i++)
     {
