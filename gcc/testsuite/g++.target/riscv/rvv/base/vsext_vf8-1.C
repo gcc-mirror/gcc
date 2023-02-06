74595dd5181813e6cca9464ec7ca04dccf029d4e
Ju-Zhe Zhong <juzhe.zhong@rivai.ai>
2023-02-06 13:25:47 +0800

    RISC-V: Add vsext C++ API tests
    gcc/testsuite/ChangeLog:
    
    	* g++.target/riscv/rvv/base/vsext_vf2-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2_mu-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2_mu-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2_mu-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2_tu-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2_tu-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2_tu-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2_tum-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2_tum-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2_tum-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2_tumu-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2_tumu-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf2_tumu-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4_mu-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4_mu-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4_mu-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4_tu-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4_tu-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4_tu-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4_tum-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4_tum-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4_tum-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4_tumu-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4_tumu-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf4_tumu-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8_mu-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8_mu-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8_mu-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8_tu-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8_tu-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8_tu-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8_tum-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8_tum-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8_tum-3.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8_tumu-1.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8_tumu-2.C: New test.
    	* g++.target/riscv/rvv/base/vsext_vf8_tumu-3.C: New test.

---
+++ gcc/testsuite/g++.target/riscv/rvv/base/vsext_vf8-1.C
@@ -0,0 +1,62 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gcv -mabi=lp64d -O3 -fno-schedule-insns -fno-schedule-insns2" } */
+
+#include "riscv_vector.h"
+
+vint64m1_t test___riscv_vsext_vf8(vint8mf8_t op1,size_t vl)
+{
+    return __riscv_vsext_vf8(op1,vl);
+}
+
+
+vint64m2_t test___riscv_vsext_vf8(vint8mf4_t op1,size_t vl)
+{
+    return __riscv_vsext_vf8(op1,vl);
+}
+
+
+vint64m4_t test___riscv_vsext_vf8(vint8mf2_t op1,size_t vl)
+{
+    return __riscv_vsext_vf8(op1,vl);
+}
+
+
+vint64m8_t test___riscv_vsext_vf8(vint8m1_t op1,size_t vl)
+{
+    return __riscv_vsext_vf8(op1,vl);
+}
+
+
+vint64m1_t test___riscv_vsext_vf8(vbool64_t mask,vint8mf8_t op1,size_t vl)
+{
+    return __riscv_vsext_vf8(mask,op1,vl);
+}
+
+
+vint64m2_t test___riscv_vsext_vf8(vbool32_t mask,vint8mf4_t op1,size_t vl)
+{
+    return __riscv_vsext_vf8(mask,op1,vl);
+}
+
+
+vint64m4_t test___riscv_vsext_vf8(vbool16_t mask,vint8mf2_t op1,size_t vl)
+{
+    return __riscv_vsext_vf8(mask,op1,vl);
+}
+
+
+vint64m8_t test___riscv_vsext_vf8(vbool8_t mask,vint8m1_t op1,size_t vl)
+{
+    return __riscv_vsext_vf8(mask,op1,vl);
+}
+
+
+
+/* { dg-final { scan-assembler-times {vsetvli\s+zero,\s*[a-x0-9]+,\s*e64,\s*m1,\s*t[au],\s*m[au]\s+vsext\.vf8\s+v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetvli\s+zero,\s*[a-x0-9]+,\s*e64,\s*m2,\s*t[au],\s*m[au]\s+vsext\.vf8\s+v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetvli\s+zero,\s*[a-x0-9]+,\s*e64,\s*m4,\s*t[au],\s*m[au]\s+vsext\.vf8\s+v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetvli\s+zero,\s*[a-x0-9]+,\s*e64,\s*m8,\s*t[au],\s*m[au]\s+vsext\.vf8\s+v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetvli\s+zero,\s*[a-x0-9]+,\s*e64,\s*m1,\s*t[au],\s*m[au]\s+vsext\.vf8\s+v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetvli\s+zero,\s*[a-x0-9]+,\s*e64,\s*m2,\s*t[au],\s*m[au]\s+vsext\.vf8\s+v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetvli\s+zero,\s*[a-x0-9]+,\s*e64,\s*m4,\s*t[au],\s*m[au]\s+vsext\.vf8\s+v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetvli\s+zero,\s*[a-x0-9]+,\s*e64,\s*m8,\s*t[au],\s*m[au]\s+vsext\.vf8\s+v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
---
+++ gcc/testsuite/g++.target/riscv/rvv/base/vsext_vf8_tumu-2.C
@@ -0,0 +1,34 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gcv -mabi=lp64d -O3 -fno-schedule-insns -fno-schedule-insns2" } */
+
+#include "riscv_vector.h"
+
+vint64m1_t test___riscv_vsext_vf8_tumu(vbool64_t mask,vint64m1_t merge,vint8mf8_t op1,size_t vl)
+{
+    return __riscv_vsext_vf8_tumu(mask,merge,op1,31);
+}
+
+
+vint64m2_t test___riscv_vsext_vf8_tumu(vbool32_t mask,vint64m2_t merge,vint8mf4_t op1,size_t vl)
+{
+    return __riscv_vsext_vf8_tumu(mask,merge,op1,31);
+}
+
+
+vint64m4_t test___riscv_vsext_vf8_tumu(vbool16_t mask,vint64m4_t merge,vint8mf2_t op1,size_t vl)
+{
+    return __riscv_vsext_vf8_tumu(mask,merge,op1,31);
+}
+
+
+vint64m8_t test___riscv_vsext_vf8_tumu(vbool8_t mask,vint64m8_t merge,vint8m1_t op1,size_t vl)
+{
+    return __riscv_vsext_vf8_tumu(mask,merge,op1,31);
+}
+
+
+
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e64,\s*m1,\s*tu,\s*mu\s+vsext\.vf8\s+v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e64,\s*m2,\s*tu,\s*mu\s+vsext\.vf8\s+v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e64,\s*m4,\s*tu,\s*mu\s+vsext\.vf8\s+v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e64,\s*m8,\s*tu,\s*mu\s+vsext\.vf8\s+v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
