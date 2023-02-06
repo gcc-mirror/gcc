1d66166b0f7185ac8e54b97fcf5528ec1fd430e9
Ju-Zhe Zhong <juzhe.zhong@rivai.ai>
2023-02-06 13:09:49 +0800

    RISC-V: Add vzext.vf4 C API tests
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/base/vzext_vf4-1.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4-2.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4-3.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_m-1.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_m-2.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_m-3.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_mu-1.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_mu-2.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_mu-3.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_tu-1.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_tu-2.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_tu-3.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_tum-1.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_tum-2.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_tum-3.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_tumu-1.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_tumu-2.c: New test.
    	* gcc.target/riscv/rvv/base/vzext_vf4_tumu-3.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/rvv/base/vzext_vf4-2.c
@@ -0,0 +1,69 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gcv -mabi=lp64d -O3 -fno-schedule-insns -fno-schedule-insns2" } */
+
+#include "riscv_vector.h"
+
+vuint32mf2_t test___riscv_vzext_vf4_u32mf2(vuint8mf8_t op1,size_t vl)
+{
+    return __riscv_vzext_vf4_u32mf2(op1,31);
+}
+
+
+vuint32m1_t test___riscv_vzext_vf4_u32m1(vuint8mf4_t op1,size_t vl)
+{
+    return __riscv_vzext_vf4_u32m1(op1,31);
+}
+
+
+vuint32m2_t test___riscv_vzext_vf4_u32m2(vuint8mf2_t op1,size_t vl)
+{
+    return __riscv_vzext_vf4_u32m2(op1,31);
+}
+
+
+vuint32m4_t test___riscv_vzext_vf4_u32m4(vuint8m1_t op1,size_t vl)
+{
+    return __riscv_vzext_vf4_u32m4(op1,31);
+}
+
+
+vuint32m8_t test___riscv_vzext_vf4_u32m8(vuint8m2_t op1,size_t vl)
+{
+    return __riscv_vzext_vf4_u32m8(op1,31);
+}
+
+
+vuint64m1_t test___riscv_vzext_vf4_u64m1(vuint16mf4_t op1,size_t vl)
+{
+    return __riscv_vzext_vf4_u64m1(op1,31);
+}
+
+
+vuint64m2_t test___riscv_vzext_vf4_u64m2(vuint16mf2_t op1,size_t vl)
+{
+    return __riscv_vzext_vf4_u64m2(op1,31);
+}
+
+
+vuint64m4_t test___riscv_vzext_vf4_u64m4(vuint16m1_t op1,size_t vl)
+{
+    return __riscv_vzext_vf4_u64m4(op1,31);
+}
+
+
+vuint64m8_t test___riscv_vzext_vf4_u64m8(vuint16m2_t op1,size_t vl)
+{
+    return __riscv_vzext_vf4_u64m8(op1,31);
+}
+
+
+
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*mf2,\s*t[au],\s*m[au]\s+vzext\.vf4\s+v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m1,\s*t[au],\s*m[au]\s+vzext\.vf4\s+v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m2,\s*t[au],\s*m[au]\s+vzext\.vf4\s+v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m4,\s*t[au],\s*m[au]\s+vzext\.vf4\s+v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m8,\s*t[au],\s*m[au]\s+vzext\.vf4\s+v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e64,\s*m1,\s*t[au],\s*m[au]\s+vzext\.vf4\s+v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e64,\s*m2,\s*t[au],\s*m[au]\s+vzext\.vf4\s+v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e64,\s*m4,\s*t[au],\s*m[au]\s+vzext\.vf4\s+v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e64,\s*m8,\s*t[au],\s*m[au]\s+vzext\.vf4\s+v[0-9]+,\s*v[0-9]+} 1 } } */
