2c9a25a82ead1379b3a1f4f4a77a23104ea238b8
Christophe Lyon <christophe.lyon@arm.com>
2023-02-08 21:18:28 +0000

    arm: [MVE intrinsics] add support for MODE_r
    A few intrinsics have an additional mode (MODE_r), which does not
    always support the same set of predicates as MODE_none and MODE_n.
    For vqshlq they are the same, but for vshlq they are not.
    
    Indeed we have:
    vqshlq
    vqshlq_m
    vqshlq_n
    vqshlq_m_n
    vqshlq_r
    vqshlq_m_r
    
    vshlq
    vshlq_m
    vshlq_x
    vshlq_n
    vshlq_m_n
    vshlq_x_n
    vshlq_r
    vshlq_m_r
    
    This patch adds support for it.
    
    2022-09-08  Christophe Lyon <christophe.lyon@arm.com>
    
    	gcc/
    	* config/arm/arm-mve-builtins.cc (has_inactive_argument)
    	(finish_opt_n_resolution): Handle MODE_r.
    	* config/arm/arm-mve-builtins.def (r): New mode.

--- gcc/config/arm/arm-mve-builtins.cc
+++ gcc/config/arm/arm-mve-builtins.cc
@@ -669,7 +669,8 @@ function_instance::has_inactive_argument () const
   if (pred != PRED_m)
     return false;
 
-  if ((base == functions::vorrq && mode_suffix_id == MODE_n)
+  if (mode_suffix_id == MODE_r
+      || (base == functions::vorrq && mode_suffix_id == MODE_n)
       || (base == functions::vqrshlq && mode_suffix_id == MODE_n)
       || (base == functions::vrshlq && mode_suffix_id == MODE_n))
     return false;
@@ -1522,7 +1523,10 @@ finish_opt_n_resolution (unsigned int argno, unsigned int first_argno,
 {
   if (inferred_type == NUM_TYPE_SUFFIXES)
     inferred_type = first_type;
-  tree scalar_form = lookup_form (MODE_n, inferred_type);
+  mode_suffix_index scalar_mode = MODE_n;
+  if (mode_suffix_id == MODE_r)
+    scalar_mode = MODE_r;
+  tree scalar_form = lookup_form (scalar_mode, inferred_type);
 
   /* Allow the final argument to be scalar, if an _n form exists.  */
   if (scalar_argument_p (argno))
