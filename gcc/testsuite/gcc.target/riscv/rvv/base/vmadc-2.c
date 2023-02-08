c8c7b4b32d86ecf5f8768a5c162484ac2a6b0130
Ju-Zhe Zhong <juzhe.zhong@rivai.ai>
2023-02-09 04:54:48 +0800

    RISC-V: Add vmadc C API tests
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/base/vmadc-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc-3.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc-4.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc-5.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc-6.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc-7.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc-8.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vv-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vv-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vv-3.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vvm-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vvm-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vvm-3.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vx_rv32-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vx_rv32-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vx_rv32-3.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vx_rv64-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vx_rv64-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vx_rv64-3.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vxm_rv32-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vxm_rv32-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vxm_rv32-3.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vxm_rv64-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vxm_rv64-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmadc_vxm_rv64-3.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/rvv/base/vmadc-2.c
@@ -0,0 +1,43 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv32gcv -mabi=ilp32d -O3" } */
+#include "riscv_vector.h"
+
+void f1 (void * in, void *out, int32_t x)
+{
+  vbool32_t mask = __riscv_vlm_v_b32 (in + 100, 4);
+  vint32m1_t v = __riscv_vle32_v_i32m1 (in, 4);
+  vint32m1_t v2 = __riscv_vle32_v_i32m1_tu (v, in, 4);
+  vbool32_t v3 = __riscv_vmadc_vxm_i32m1_b32 (v2, -16, mask, 4);
+  __riscv_vsm_v_b32 (out, v3, 4);
+}
+
+void f2 (void * in, void *out, int32_t x)
+{
+  vbool32_t mask = __riscv_vlm_v_b32 (in + 100, 4);
+  vint32m1_t v = __riscv_vle32_v_i32m1 (in, 4);
+  vint32m1_t v2 = __riscv_vle32_v_i32m1_tu (v, in, 4);
+  vbool32_t v3 = __riscv_vmadc_vxm_i32m1_b32 (v2, 15, mask, 4);
+  __riscv_vsm_v_b32 (out, v3, 4);
+}
+
+void f3 (void * in, void *out, int32_t x)
+{
+  vbool32_t mask = __riscv_vlm_v_b32 (in + 100, 4);
+  vint32m1_t v = __riscv_vle32_v_i32m1 (in, 4);
+  vint32m1_t v2 = __riscv_vle32_v_i32m1_tu (v, in, 4);
+  vbool32_t v3 = __riscv_vmadc_vxm_i32m1_b32 (v2, -17, mask, 4);
+  __riscv_vsm_v_b32 (out, v3, 4);
+}
+
+void f4 (void * in, void *out, int32_t x)
+{
+  vbool32_t mask = __riscv_vlm_v_b32 (in + 100, 4);
+  vint32m1_t v = __riscv_vle32_v_i32m1 (in, 4);
+  vint32m1_t v2 = __riscv_vle32_v_i32m1_tu (v, in, 4);
+  vbool32_t v3 = __riscv_vmadc_vxm_i32m1_b32 (v2, 16, mask, 4);
+  __riscv_vsm_v_b32 (out, v3, 4);
+}
+
+/* { dg-final { scan-assembler-times {vmadc\.vim\s+v[0-9]+,\s*v[0-9]+,\s*-16,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vmadc\.vim\s+v[0-9]+,\s*v[0-9]+,\s*15,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vmadc\.vxm\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+} 2 } } */
