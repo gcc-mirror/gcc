525713ed9db904ed2504decc5bde9d58bd5d98b4
liuhongt <hongtao.liu@intel.com>
2023-02-08 22:27:54 +0800

    Adjust testcases after better RA decision.
    After optimization for RA, memory op is not propagated into
    instructions(>1), and it make testcases not generate vxorps since
    the memory is loaded into the dest, and the dest is never unused now.
    
    So rewrite testcases to make the codegen more stable.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/i386/avx2-dest-false-dep-for-glc.c: Rewrite
    	testcase to make the codegen more stable.
    	* gcc.target/i386/avx512dq-dest-false-dep-for-glc.c: Ditto
    	* gcc.target/i386/avx512f-dest-false-dep-for-glc.c: Ditto.
    	* gcc.target/i386/avx512fp16-dest-false-dep-for-glc.c: Ditto.
    	* gcc.target/i386/avx512vl-dest-false-dep-for-glc.c: Ditto.

--- gcc/testsuite/gcc.target/i386/avx512fp16-dest-false-dep-for-glc.c
+++ gcc/testsuite/gcc.target/i386/avx512fp16-dest-false-dep-for-glc.c
@@ -11,32 +11,98 @@ __mmask32 m32;
 __mmask16 m16;
 __mmask8 m8;
 
-void complex_mul_test (void)
+__m512h
+foo1 (__m512h h2, __m512h h1)
 {
-  h1 = _mm512_fmul_pch (h1, h1);
-  h1 = _mm512_fmul_round_pch (h1, h1, 8);
-  h1 = _mm512_mask_fmul_pch (h1, m32, h1, h1);
-  h1 = _mm512_mask_fmul_round_pch (h1, m32, h1, h1, 8);
-  h1 = _mm512_maskz_fmul_pch (m32, h1, h1);
-  h1 = _mm512_maskz_fmul_round_pch (m32, h1, h1, 11);
-
-  h3 = _mm_fmul_sch (h3, h3);
-  h3 = _mm_fmul_round_sch (h3, h3, 8);
-  h3 = _mm_mask_fmul_sch (h3, m8, h3, h3);
-  h3 = _mm_mask_fmul_round_sch (h3, m8, h3, h3, 8);
-  h3 = _mm_maskz_fmul_sch (m8, h3, h3);
-  h3 = _mm_maskz_fmul_round_sch (m8, h3, h3, 11);
+  return _mm512_fmul_pch (h1, h1);
 }
 
-void vgetmant_test (void)
+__m512h
+foo2 (__m512h h2, __m512h h1)
 {
-  h3 = _mm_getmant_sh (h3, h3, _MM_MANT_NORM_p75_1p5,
-		       _MM_MANT_SIGN_src);
-  h3 = _mm_mask_getmant_sh (h3, m8, h3, h3, _MM_MANT_NORM_p75_1p5,
-			    _MM_MANT_SIGN_src);
-  h3 = _mm_maskz_getmant_sh (m8, h3, h3, _MM_MANT_NORM_p75_1p5,
-			     _MM_MANT_SIGN_src);
-}    
+  return _mm512_fmul_round_pch (h1, h1, 8);
+}
+
+__m512h
+foo3 (__m512h h2, __m512h h1)
+{
+  return _mm512_mask_fmul_pch (h1, m32, h1, h1);
+}
+
+__m512h
+foo4 (__m512h h2, __m512h h1)
+{
+  return _mm512_mask_fmul_round_pch (h1, m32, h1, h1, 8);
+}
+
+__m512h
+foo5 (__m512h h2, __m512h h1)
+{
+  return _mm512_maskz_fmul_pch (m32, h1, h1);
+}
+
+__m512h
+foo6 (__m512h h2, __m512h h1)
+{
+  return _mm512_maskz_fmul_round_pch (m32, h1, h1, 11);
+}
+
+__m128h
+bar1 (__m128h h2, __m128h h1)
+{
+  return _mm_fmul_sch (h1, h1);
+}
+
+__m128h
+bar2 (__m128h h2, __m128h h1)
+{
+  return _mm_fmul_round_sch (h1, h1, 8);
+}
+
+__m128h
+bar3 (__m128h h2, __m128h h1)
+{
+  return _mm_mask_fmul_sch (h1, m8, h1, h1);
+}
+
+__m128h
+bar4 (__m128h h2, __m128h h1)
+{
+  return _mm_mask_fmul_round_sch (h1, m8, h1, h1, 8);
+}
+
+__m128h
+bar5 (__m128h h2, __m128h h1)
+{
+  return _mm_maskz_fmul_sch (m8, h1, h1);
+}
+
+__m128h
+bar6 (__m128h h2, __m128h h1)
+{
+  return _mm_maskz_fmul_round_sch (m8, h1, h1, 11);
+}
+
+__m128h
+zoo1 (__m128h h1, __m128h h3)
+{
+  return _mm_getmant_sh (h3, h3, _MM_MANT_NORM_p75_1p5,
+			 _MM_MANT_SIGN_src);
+}
+
+__m128h
+zoo2 (__m128h h1, __m128h h3)
+{
+  return _mm_mask_getmant_sh (h3, m8, h3, h3, _MM_MANT_NORM_p75_1p5,
+			      _MM_MANT_SIGN_src);
+}
+
+__m128h
+zoo3 (__m128h h1, __m128h h3)
+{
+  return _mm_maskz_getmant_sh (m8, h3, h3, _MM_MANT_NORM_p75_1p5,
+			       _MM_MANT_SIGN_src);
+}
 
 /* { dg-final { scan-assembler-times "vxorps" 10 } } */
 /* { dg-final { scan-assembler-times "vfmulcph" 6 } } */
--- gcc/testsuite/gcc.target/i386/avx512vl-dest-false-dep-for-glc.c
+++ gcc/testsuite/gcc.target/i386/avx512vl-dest-false-dep-for-glc.c
@@ -13,60 +13,203 @@ extern __m128 f2, *pf2;
 __mmask16 m16;
 __mmask8 m8;
 
-void vperm_test (void)
+__m256d
+foo1 (__m256d d2, __m256d d1)
 {
-  d1 = _mm256_permutex_pd (d1, 12);
-  d1 = _mm256_mask_permutex_pd (d1, m8, d1, 12);
-  d1 = _mm256_maskz_permutex_pd (m8, d1, 12);
-  d11 = _mm256_permutexvar_pd (i1, d11);
-  d11 = _mm256_mask_permutexvar_pd (d11, m8, i1, d11);
-  d11 = _mm256_maskz_permutexvar_pd (m8, i1, d11);
-
-  f1 = _mm256_permutexvar_ps (i1, f1);
-  f1 = _mm256_mask_permutexvar_ps (f1, m8, i1, f1);
-  f1 = _mm256_maskz_permutexvar_ps (m8, i1, f1);
-
-  i1 = _mm256_permutexvar_epi64 (i1, i1);
-  i1 = _mm256_mask_permutexvar_epi64 (i1, m8, i1, i1);
-  i1 = _mm256_maskz_permutexvar_epi64 (m8, i1, i1);
-  i1 = _mm256_permutex_epi64 (i1, 12);
-  i1 = _mm256_mask_permutex_epi64 (i1, m8, i1, 12);
-  i1 = _mm256_maskz_permutex_epi64 (m8, i1, 12);
-
-  i2 = _mm256_permutexvar_epi32 (i2, i2);
-  i2 = _mm256_mask_permutexvar_epi32 (i2, m8, i2, i2);
-  i3 = _mm256_maskz_permutexvar_epi32 (m8, i3, i3);
-} 
-
-void getmant_test (void)
-{
-  d1 = _mm256_getmant_pd (*pd1, _MM_MANT_NORM_p75_1p5,
-			  _MM_MANT_SIGN_src);
-  d1 = _mm256_mask_getmant_pd (d1, m8, *pd1, _MM_MANT_NORM_p75_1p5,
-			       _MM_MANT_SIGN_src);
-  d1 = _mm256_maskz_getmant_pd (m8, *pd1, _MM_MANT_NORM_p75_1p5,
-				_MM_MANT_SIGN_src);
-  d2 = _mm_getmant_pd (*pd2, _MM_MANT_NORM_p75_1p5,
-		       _MM_MANT_SIGN_src);
-  d2 = _mm_mask_getmant_pd (d2, m8, *pd2, _MM_MANT_NORM_p75_1p5,
-			    _MM_MANT_SIGN_src);
-  d2 = _mm_maskz_getmant_pd (m8, *pd2, _MM_MANT_NORM_p75_1p5,
-			     _MM_MANT_SIGN_src);
-  f1 = _mm256_getmant_ps (*pf1, _MM_MANT_NORM_p75_1p5,
-			  _MM_MANT_SIGN_src);
-  f1 = _mm256_mask_getmant_ps (f1, m8, *pf1, _MM_MANT_NORM_p75_1p5,
-			       _MM_MANT_SIGN_src);
-  f1 = _mm256_maskz_getmant_ps (m8, *pf1, _MM_MANT_NORM_p75_1p5,
-				_MM_MANT_SIGN_src);
-  f2 = _mm_getmant_ps (*pf2, _MM_MANT_NORM_p75_1p5,
-		       _MM_MANT_SIGN_src);
-  f2 = _mm_mask_getmant_ps (f2, m8, *pf2, _MM_MANT_NORM_p75_1p5,
-			    _MM_MANT_SIGN_src);
-  f2 = _mm_maskz_getmant_ps (m8, *pf2, _MM_MANT_NORM_p75_1p5,
-			     _MM_MANT_SIGN_src);
+  return _mm256_permutex_pd (d1, 12);
 }
 
-/* { dg-final { scan-assembler-times "vxorps" 19 } } */
+__m256d
+foo2 (__m256d d2, __m256d d1)
+{
+  return _mm256_mask_permutex_pd (d1, m8, d1, 13);
+}
+
+__m256d
+foo3 (__m256d d2, __m256d d1)
+{
+  return _mm256_maskz_permutex_pd (m8, d1, 14);
+}
+
+__m256d
+foo4 (__m256d d2, __m256d d11, __m256i i1)
+{
+  return _mm256_permutexvar_pd (i1, d11);
+}
+
+__m256d
+foo5 (__m256d d2, __m256d d11, __m256i i2)
+{
+  return _mm256_mask_permutexvar_pd (d11, m8, i2, d11);
+}
+
+__m256d
+foo6 (__m256d d2, __m256d d11, __m256i i3)
+{
+  return _mm256_maskz_permutexvar_pd (m8, i3, d11);
+}
+
+__m256i
+ioo1 (__m256i d2, __m256i d1)
+{
+  return _mm256_permutex_epi64 (d1, 12);
+}
+
+__m256i
+ioo2 (__m256i d2, __m256i d1)
+{
+  return _mm256_mask_permutex_epi64 (d1, m8, d1, 13);
+}
+
+__m256i
+ioo3 (__m256i d2, __m256i d1)
+{
+  return _mm256_maskz_permutex_epi64 (m8, d1, 14);
+}
+
+__m256i
+ioo4 (__m256i d2, __m256i d11, __m256i i1)
+{
+  return _mm256_permutexvar_epi64 (i1, d11);
+}
+
+__m256i
+ioo5 (__m256i d2, __m256i d11, __m256i i2)
+{
+  return _mm256_mask_permutexvar_epi64 (d11, m8, i2, d11);
+}
+
+__m256i
+ioo6 (__m256i d2, __m256i d11, __m256i i3)
+{
+  return _mm256_maskz_permutexvar_epi64 (m8, i3, d11);
+}
+
+__m256
+koo1 (__m256 f2, __m256i i1, __m256 f1)
+{
+  return _mm256_permutexvar_ps (i1, f1);
+}
+
+__m256
+koo2 (__m256 f2, __m256i i1, __m256 f1)
+{
+  return _mm256_mask_permutexvar_ps (f1, m8, i1, f1);
+}
+
+__m256
+koo3 (__m256 f2, __m256i i1, __m256 f1)
+{
+  return _mm256_maskz_permutexvar_ps (m8, i1, f1);
+}
+
+__m256i
+hoo1 (__m256i f2, __m256i i1, __m256i f1)
+{
+  return _mm256_permutexvar_epi32 (i1, f1);
+}
+
+__m256i
+hoo2 (__m256i f2, __m256i i1, __m256i f1)
+{
+  return _mm256_mask_permutexvar_epi32 (f1, m8, i1, f1);
+}
+
+__m256i
+hoo3 (__m256i f2, __m256i i1, __m256i f1)
+{
+  return _mm256_maskz_permutexvar_epi32 (m8, i1, f1);
+}
+
+__m256d
+moo1 (__m256d d2, __m256d* d1)
+{
+  return _mm256_getmant_pd (*d1, _MM_MANT_NORM_p75_1p5,
+			    _MM_MANT_SIGN_src);
+}
+
+__m256d
+moo3 (__m256d d2, __m256d d1, __m256d* d3)
+{
+
+  return _mm256_mask_getmant_pd (d1, m8, *d3, _MM_MANT_NORM_p75_1p5,
+				 _MM_MANT_SIGN_src);
+}
+
+__m256d
+moo5 (__m256d d2, __m256d* d1)
+{
+  return _mm256_maskz_getmant_pd (m8, *d1, _MM_MANT_NORM_p75_1p5,
+				  _MM_MANT_SIGN_src);
+}
+
+__m128d
+moo2 (__m128d d2, __m128d* d1)
+{
+  return _mm_getmant_pd (*d1, _MM_MANT_NORM_p75_1p5,
+			    _MM_MANT_SIGN_src);
+}
+
+__m128d
+moo4 (__m128d d2, __m128d d1, __m128d* d3)
+{
+
+  return _mm_mask_getmant_pd (d1, m8, *d3, _MM_MANT_NORM_p75_1p5,
+				 _MM_MANT_SIGN_src);
+}
+
+__m128d
+moo6 (__m128d d2, __m128d* d1)
+{
+  return _mm_maskz_getmant_pd (m8, *d1, _MM_MANT_NORM_p75_1p5,
+				  _MM_MANT_SIGN_src);
+}
+
+__m256
+noo1 (__m256 d2, __m256* d1)
+{
+  return _mm256_getmant_ps (*d1, _MM_MANT_NORM_p75_1p5,
+			    _MM_MANT_SIGN_src);
+}
+
+__m256
+noo3 (__m256 d2, __m256 d1, __m256* d3)
+{
+
+  return _mm256_mask_getmant_ps (d1, m8, *d3, _MM_MANT_NORM_p75_1p5,
+				 _MM_MANT_SIGN_src);
+}
+
+__m256
+noo5 (__m256 d2, __m256* d1)
+{
+  return _mm256_maskz_getmant_ps (m8, *d1, _MM_MANT_NORM_p75_1p5,
+				  _MM_MANT_SIGN_src);
+}
+
+__m128
+noo2 (__m128 d2, __m128* d1)
+{
+  return _mm_getmant_ps (*d1, _MM_MANT_NORM_p75_1p5,
+			    _MM_MANT_SIGN_src);
+}
+
+__m128
+noo4 (__m128 d2, __m128 d1, __m128* d3)
+{
+
+  return _mm_mask_getmant_ps (d1, m8, *d3, _MM_MANT_NORM_p75_1p5,
+				 _MM_MANT_SIGN_src);
+}
+
+__m128
+noo6 (__m128 d2, __m128* d1)
+{
+  return _mm_maskz_getmant_ps (m8, *d1, _MM_MANT_NORM_p75_1p5,
+				  _MM_MANT_SIGN_src);
+}
+
+/* { dg-final { scan-assembler-times "vxorps" 20 } } */
 /* { dg-final { scan-assembler-times "vpermpd" 6 } } */
 /* { dg-final { scan-assembler-times "vpermps" 3 } } */
 /* { dg-final { scan-assembler-times "vpermq" 6 } } */
