c0ea34be11c0da54c18b010ab51b97fc95657a4a
Ju-Zhe Zhong <juzhe.zhong@rivai.ai>
2023-02-10 05:57:13 +0800

    RISC-V: Add vmerge C API tests
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/base/vmerge-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge-3.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge-4.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vvm-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vvm-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vvm-3.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vvm-4.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vvm-5.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vvm-6.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vvm_tu-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vvm_tu-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vvm_tu-3.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vvm_tu-4.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vvm_tu-5.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vvm_tu-6.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vxm_rv32-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vxm_rv32-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vxm_rv32-3.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vxm_rv64-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vxm_rv64-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vxm_rv64-3.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vxm_tu_rv32-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vxm_tu_rv32-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vxm_tu_rv32-3.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vxm_tu_rv64-1.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vxm_tu_rv64-2.c: New test.
    	* gcc.target/riscv/rvv/base/vmerge_vxm_tu_rv64-3.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/rvv/base/vmerge-2.c
@@ -0,0 +1,47 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv32gcv -mabi=ilp32d -O3" } */
+#include "riscv_vector.h"
+
+void f1 (void * in, void *out, int32_t x)
+{
+  vbool32_t mask = __riscv_vlm_v_b32 (in + 100, 4);
+  vint32m1_t v = __riscv_vle32_v_i32m1 (in, 4);
+  vint32m1_t v2 = __riscv_vle32_v_i32m1_tu (v, in, 4);
+  vint32m1_t v3 = __riscv_vmerge_vxm_i32m1 (v2, -16, mask, 4);
+  vint32m1_t v4 = __riscv_vmerge_vxm_i32m1_tu (v3, v2, -16, mask, 4);
+  __riscv_vse32_v_i32m1 (out, v4, 4);
+}
+
+void f2 (void * in, void *out, int32_t x)
+{
+  vbool32_t mask = __riscv_vlm_v_b32 (in + 100, 4);
+  vint32m1_t v = __riscv_vle32_v_i32m1 (in, 4);
+  vint32m1_t v2 = __riscv_vle32_v_i32m1_tu (v, in, 4);
+  vint32m1_t v3 = __riscv_vmerge_vxm_i32m1 (v2, 15, mask, 4);
+  vint32m1_t v4 = __riscv_vmerge_vxm_i32m1_tu (v3, v2, 15, mask, 4);
+  __riscv_vse32_v_i32m1 (out, v4, 4);
+}
+
+void f3 (void * in, void *out, int32_t x)
+{
+  vbool32_t mask = __riscv_vlm_v_b32 (in + 100, 4);
+  vint32m1_t v = __riscv_vle32_v_i32m1 (in, 4);
+  vint32m1_t v2 = __riscv_vle32_v_i32m1_tu (v, in, 4);
+  vint32m1_t v3 = __riscv_vmerge_vxm_i32m1 (v2, -17, mask, 4);
+  vint32m1_t v4 = __riscv_vmerge_vxm_i32m1_tu (v3, v2, -17, mask, 4);
+  __riscv_vse32_v_i32m1 (out, v4, 4);
+}
+
+void f4 (void * in, void *out, int32_t x)
+{
+  vbool32_t mask = __riscv_vlm_v_b32 (in + 100, 4);
+  vint32m1_t v = __riscv_vle32_v_i32m1 (in, 4);
+  vint32m1_t v2 = __riscv_vle32_v_i32m1_tu (v, in, 4);
+  vint32m1_t v3 = __riscv_vmerge_vxm_i32m1 (v2, 16, mask, 4);
+  vint32m1_t v4 = __riscv_vmerge_vxm_i32m1_tu (v3, v2, 16, mask, 4);
+  __riscv_vse32_v_i32m1 (out, v4, 4);
+}
+
+/* { dg-final { scan-assembler-times {vmerge\.vim\s+v[0-9]+,\s*v[0-9]+,\s*-16,\s*v[0-9]+} 2 } } */
+/* { dg-final { scan-assembler-times {vmerge\.vim\s+v[0-9]+,\s*v[0-9]+,\s*15,\s*v[0-9]+} 2 } } */
+/* { dg-final { scan-assembler-times {vmerge\.vxm\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+} 4 } } */
