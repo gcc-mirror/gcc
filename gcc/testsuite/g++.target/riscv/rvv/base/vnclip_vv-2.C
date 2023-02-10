0906435e2b6d1f226679b6eac145d9f247559cdf
Ju-Zhe Zhong <juzhe.zhong@rivai.ai>
2023-02-10 14:53:44 +0800

    RISC-V: Add vnclip C++ API tests
    gcc/testsuite/ChangeLog:
    
    	* g++.target/riscv/rvv/base/vnclip_vv-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv_mu-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv_mu-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv_mu-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv_tu-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv_tu-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv_tu-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv_tum-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv_tum-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv_tum-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv_tumu-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv_tumu-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vv_tumu-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx_mu-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx_mu-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx_mu-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx_tu-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx_tu-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx_tu-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx_tum-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx_tum-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx_tum-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx_tumu-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx_tumu-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclip_vx_tumu-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv_mu-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv_mu-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv_mu-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv_tu-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv_tu-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv_tu-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv_tum-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv_tum-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv_tum-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv_tumu-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv_tumu-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vv_tumu-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx_mu-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx_mu-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx_mu-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx_tu-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx_tu-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx_tu-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx_tum-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx_tum-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx_tum-3.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx_tumu-1.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx_tumu-2.C: New test.
    	* g++.target/riscv/rvv/base/vnclipu_vx_tumu-3.C: New test.

---
+++ gcc/testsuite/g++.target/riscv/rvv/base/vnclip_vv-2.C
@@ -0,0 +1,216 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gcv -mabi=lp64d -O3 -fno-schedule-insns -fno-schedule-insns2" } */
+
+#include "riscv_vector.h"
+
+vint8mf8_t test___riscv_vnclip(vint16mf4_t op1,vuint8mf8_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint8mf4_t test___riscv_vnclip(vint16mf2_t op1,vuint8mf4_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint8mf2_t test___riscv_vnclip(vint16m1_t op1,vuint8mf2_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint8m1_t test___riscv_vnclip(vint16m2_t op1,vuint8m1_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint8m2_t test___riscv_vnclip(vint16m4_t op1,vuint8m2_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint8m4_t test___riscv_vnclip(vint16m8_t op1,vuint8m4_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint16mf4_t test___riscv_vnclip(vint32mf2_t op1,vuint16mf4_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint16mf2_t test___riscv_vnclip(vint32m1_t op1,vuint16mf2_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint16m1_t test___riscv_vnclip(vint32m2_t op1,vuint16m1_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint16m2_t test___riscv_vnclip(vint32m4_t op1,vuint16m2_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint16m4_t test___riscv_vnclip(vint32m8_t op1,vuint16m4_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint32mf2_t test___riscv_vnclip(vint64m1_t op1,vuint32mf2_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint32m1_t test___riscv_vnclip(vint64m2_t op1,vuint32m1_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint32m2_t test___riscv_vnclip(vint64m4_t op1,vuint32m2_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint32m4_t test___riscv_vnclip(vint64m8_t op1,vuint32m4_t shift,size_t vl)
+{
+    return __riscv_vnclip(op1,shift,31);
+}
+
+
+vint8mf8_t test___riscv_vnclip(vbool64_t mask,vint16mf4_t op1,vuint8mf8_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint8mf4_t test___riscv_vnclip(vbool32_t mask,vint16mf2_t op1,vuint8mf4_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint8mf2_t test___riscv_vnclip(vbool16_t mask,vint16m1_t op1,vuint8mf2_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint8m1_t test___riscv_vnclip(vbool8_t mask,vint16m2_t op1,vuint8m1_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint8m2_t test___riscv_vnclip(vbool4_t mask,vint16m4_t op1,vuint8m2_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint8m4_t test___riscv_vnclip(vbool2_t mask,vint16m8_t op1,vuint8m4_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint16mf4_t test___riscv_vnclip(vbool64_t mask,vint32mf2_t op1,vuint16mf4_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint16mf2_t test___riscv_vnclip(vbool32_t mask,vint32m1_t op1,vuint16mf2_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint16m1_t test___riscv_vnclip(vbool16_t mask,vint32m2_t op1,vuint16m1_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint16m2_t test___riscv_vnclip(vbool8_t mask,vint32m4_t op1,vuint16m2_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint16m4_t test___riscv_vnclip(vbool4_t mask,vint32m8_t op1,vuint16m4_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint32mf2_t test___riscv_vnclip(vbool64_t mask,vint64m1_t op1,vuint32mf2_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint32m1_t test___riscv_vnclip(vbool32_t mask,vint64m2_t op1,vuint32m1_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint32m2_t test___riscv_vnclip(vbool16_t mask,vint64m4_t op1,vuint32m2_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+vint32m4_t test___riscv_vnclip(vbool8_t mask,vint64m8_t op1,vuint32m4_t shift,size_t vl)
+{
+    return __riscv_vnclip(mask,op1,shift,31);
+}
+
+
+
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf8,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf4,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf2,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m1,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m2,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m4,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*mf4,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*mf2,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m1,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m2,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m4,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*mf2,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m1,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m2,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m4,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf8,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf4,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf2,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m1,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m2,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m4,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*mf4,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*mf2,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m1,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m2,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m4,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*mf2,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m1,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m2,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m4,\s*t[au],\s*m[au]\s+vnclip\.wv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
