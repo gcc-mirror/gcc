ecdbebde7cb9ab63ae62bdf9296fe118a507b172
Ju-Zhe Zhong <juzhe.zhong@rivai.ai>
2023-02-13 16:26:29 +0800

    RISC-V: Add vmsne vx C++ tests
    gcc/testsuite/ChangeLog:
    
    	* g++.target/riscv/rvv/base/vmsne_vx_m_rv32-1.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_m_rv32-2.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_m_rv32-3.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_m_rv64-1.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_m_rv64-2.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_m_rv64-3.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_mu_rv32-1.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_mu_rv32-2.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_mu_rv32-3.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_mu_rv64-1.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_mu_rv64-2.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_mu_rv64-3.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_rv32-1.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_rv32-2.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_rv32-3.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_rv64-1.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_rv64-2.C: New test.
    	* g++.target/riscv/rvv/base/vmsne_vx_rv64-3.C: New test.

---
+++ gcc/testsuite/g++.target/riscv/rvv/base/vmsne_vx_mu_rv32-2.C
@@ -0,0 +1,289 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv32gcv -mabi=ilp32d -O3 -fno-schedule-insns -fno-schedule-insns2" } */
+
+#include "riscv_vector.h"
+
+vbool64_t test___riscv_vmsne_mu(vbool64_t mask,vbool64_t merge,vint8mf8_t op1,int8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool32_t test___riscv_vmsne_mu(vbool32_t mask,vbool32_t merge,vint8mf4_t op1,int8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool16_t test___riscv_vmsne_mu(vbool16_t mask,vbool16_t merge,vint8mf2_t op1,int8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool8_t test___riscv_vmsne_mu(vbool8_t mask,vbool8_t merge,vint8m1_t op1,int8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool4_t test___riscv_vmsne_mu(vbool4_t mask,vbool4_t merge,vint8m2_t op1,int8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool2_t test___riscv_vmsne_mu(vbool2_t mask,vbool2_t merge,vint8m4_t op1,int8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool1_t test___riscv_vmsne_mu(vbool1_t mask,vbool1_t merge,vint8m8_t op1,int8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool64_t test___riscv_vmsne_mu(vbool64_t mask,vbool64_t merge,vint16mf4_t op1,int16_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool32_t test___riscv_vmsne_mu(vbool32_t mask,vbool32_t merge,vint16mf2_t op1,int16_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool16_t test___riscv_vmsne_mu(vbool16_t mask,vbool16_t merge,vint16m1_t op1,int16_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool8_t test___riscv_vmsne_mu(vbool8_t mask,vbool8_t merge,vint16m2_t op1,int16_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool4_t test___riscv_vmsne_mu(vbool4_t mask,vbool4_t merge,vint16m4_t op1,int16_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool2_t test___riscv_vmsne_mu(vbool2_t mask,vbool2_t merge,vint16m8_t op1,int16_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool64_t test___riscv_vmsne_mu(vbool64_t mask,vbool64_t merge,vint32mf2_t op1,int32_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool32_t test___riscv_vmsne_mu(vbool32_t mask,vbool32_t merge,vint32m1_t op1,int32_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool16_t test___riscv_vmsne_mu(vbool16_t mask,vbool16_t merge,vint32m2_t op1,int32_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool8_t test___riscv_vmsne_mu(vbool8_t mask,vbool8_t merge,vint32m4_t op1,int32_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool4_t test___riscv_vmsne_mu(vbool4_t mask,vbool4_t merge,vint32m8_t op1,int32_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool64_t test___riscv_vmsne_mu(vbool64_t mask,vbool64_t merge,vint64m1_t op1,int64_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool32_t test___riscv_vmsne_mu(vbool32_t mask,vbool32_t merge,vint64m2_t op1,int64_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool16_t test___riscv_vmsne_mu(vbool16_t mask,vbool16_t merge,vint64m4_t op1,int64_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool8_t test___riscv_vmsne_mu(vbool8_t mask,vbool8_t merge,vint64m8_t op1,int64_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool64_t test___riscv_vmsne_mu(vbool64_t mask,vbool64_t merge,vuint8mf8_t op1,uint8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool32_t test___riscv_vmsne_mu(vbool32_t mask,vbool32_t merge,vuint8mf4_t op1,uint8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool16_t test___riscv_vmsne_mu(vbool16_t mask,vbool16_t merge,vuint8mf2_t op1,uint8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool8_t test___riscv_vmsne_mu(vbool8_t mask,vbool8_t merge,vuint8m1_t op1,uint8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool4_t test___riscv_vmsne_mu(vbool4_t mask,vbool4_t merge,vuint8m2_t op1,uint8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool2_t test___riscv_vmsne_mu(vbool2_t mask,vbool2_t merge,vuint8m4_t op1,uint8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool1_t test___riscv_vmsne_mu(vbool1_t mask,vbool1_t merge,vuint8m8_t op1,uint8_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool64_t test___riscv_vmsne_mu(vbool64_t mask,vbool64_t merge,vuint16mf4_t op1,uint16_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool32_t test___riscv_vmsne_mu(vbool32_t mask,vbool32_t merge,vuint16mf2_t op1,uint16_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool16_t test___riscv_vmsne_mu(vbool16_t mask,vbool16_t merge,vuint16m1_t op1,uint16_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool8_t test___riscv_vmsne_mu(vbool8_t mask,vbool8_t merge,vuint16m2_t op1,uint16_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool4_t test___riscv_vmsne_mu(vbool4_t mask,vbool4_t merge,vuint16m4_t op1,uint16_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool2_t test___riscv_vmsne_mu(vbool2_t mask,vbool2_t merge,vuint16m8_t op1,uint16_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool64_t test___riscv_vmsne_mu(vbool64_t mask,vbool64_t merge,vuint32mf2_t op1,uint32_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool32_t test___riscv_vmsne_mu(vbool32_t mask,vbool32_t merge,vuint32m1_t op1,uint32_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool16_t test___riscv_vmsne_mu(vbool16_t mask,vbool16_t merge,vuint32m2_t op1,uint32_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool8_t test___riscv_vmsne_mu(vbool8_t mask,vbool8_t merge,vuint32m4_t op1,uint32_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool4_t test___riscv_vmsne_mu(vbool4_t mask,vbool4_t merge,vuint32m8_t op1,uint32_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool64_t test___riscv_vmsne_mu(vbool64_t mask,vbool64_t merge,vuint64m1_t op1,uint64_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool32_t test___riscv_vmsne_mu(vbool32_t mask,vbool32_t merge,vuint64m2_t op1,uint64_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool16_t test___riscv_vmsne_mu(vbool16_t mask,vbool16_t merge,vuint64m4_t op1,uint64_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+vbool8_t test___riscv_vmsne_mu(vbool8_t mask,vbool8_t merge,vuint64m8_t op1,uint64_t op2,size_t vl)
+{
+    return __riscv_vmsne_mu(mask,merge,op1,op2,31);
+}
+
+
+
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf8,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf4,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf2,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m1,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m2,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m4,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m8,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*mf4,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*mf2,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m1,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m2,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m4,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m8,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*mf2,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m1,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m2,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m4,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m8,\s*t[au],\s*mu\s+vmsne\.vx\s+v[0-9]+,\s*v[0-9]+,\s*[a-x0-9]+,\s*v0.t\s+} 2 } } */
+/* { dg-final { scan-assembler-times {vmsne\.vv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+,\s*v0.t\s+} 8 } } */
