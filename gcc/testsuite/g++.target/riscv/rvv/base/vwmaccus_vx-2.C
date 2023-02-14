c4e2a63e4620ab85f84b06e6e5f1553caae4a576
Ju-Zhe Zhong <juzhe.zhong@rivai.ai>
2023-02-14 22:12:36 +0800

    RISC-V: Add vwmacc vx C++ api tests
    gcc/testsuite/ChangeLog:
    
    	* g++.target/riscv/rvv/base/vwmacc_vx-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx_mu-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx_mu-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx_mu-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx_tu-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx_tu-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx_tu-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx_tum-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx_tum-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx_tum-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx_tumu-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx_tumu-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmacc_vx_tumu-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx_mu-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx_mu-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx_mu-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx_tu-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx_tu-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx_tu-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx_tum-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx_tum-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx_tum-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx_tumu-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx_tumu-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccsu_vx_tumu-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx_mu-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx_mu-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx_mu-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx_tu-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx_tu-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx_tu-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx_tum-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx_tum-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx_tum-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx_tumu-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx_tumu-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccu_vx_tumu-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx_mu-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx_mu-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx_mu-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx_tu-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx_tu-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx_tu-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx_tum-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx_tum-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx_tum-3.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx_tumu-1.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx_tumu-2.C: New test.
    	* g++.target/riscv/rvv/base/vwmaccus_vx_tumu-3.C: New test.

---
+++ gcc/testsuite/g++.target/riscv/rvv/base/vwmaccus_vx-2.C
@@ -0,0 +1,216 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gcv -mabi=lp64d -O3 -fno-schedule-insns -fno-schedule-insns2" } */
+
+#include "riscv_vector.h"
+
+vint16mf4_t test___riscv_vwmaccus(vint16mf4_t vd,uint8_t rs1,vint8mf8_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint16mf2_t test___riscv_vwmaccus(vint16mf2_t vd,uint8_t rs1,vint8mf4_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint16m1_t test___riscv_vwmaccus(vint16m1_t vd,uint8_t rs1,vint8mf2_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint16m2_t test___riscv_vwmaccus(vint16m2_t vd,uint8_t rs1,vint8m1_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint16m4_t test___riscv_vwmaccus(vint16m4_t vd,uint8_t rs1,vint8m2_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint16m8_t test___riscv_vwmaccus(vint16m8_t vd,uint8_t rs1,vint8m4_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint32mf2_t test___riscv_vwmaccus(vint32mf2_t vd,uint16_t rs1,vint16mf4_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint32m1_t test___riscv_vwmaccus(vint32m1_t vd,uint16_t rs1,vint16mf2_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint32m2_t test___riscv_vwmaccus(vint32m2_t vd,uint16_t rs1,vint16m1_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint32m4_t test___riscv_vwmaccus(vint32m4_t vd,uint16_t rs1,vint16m2_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint32m8_t test___riscv_vwmaccus(vint32m8_t vd,uint16_t rs1,vint16m4_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint64m1_t test___riscv_vwmaccus(vint64m1_t vd,uint32_t rs1,vint32mf2_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint64m2_t test___riscv_vwmaccus(vint64m2_t vd,uint32_t rs1,vint32m1_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint64m4_t test___riscv_vwmaccus(vint64m4_t vd,uint32_t rs1,vint32m2_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint64m8_t test___riscv_vwmaccus(vint64m8_t vd,uint32_t rs1,vint32m4_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(vd,rs1,vs2,31);
+}
+
+
+vint16mf4_t test___riscv_vwmaccus(vbool64_t mask,vint16mf4_t vd,uint8_t rs1,vint8mf8_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint16mf2_t test___riscv_vwmaccus(vbool32_t mask,vint16mf2_t vd,uint8_t rs1,vint8mf4_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint16m1_t test___riscv_vwmaccus(vbool16_t mask,vint16m1_t vd,uint8_t rs1,vint8mf2_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint16m2_t test___riscv_vwmaccus(vbool8_t mask,vint16m2_t vd,uint8_t rs1,vint8m1_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint16m4_t test___riscv_vwmaccus(vbool4_t mask,vint16m4_t vd,uint8_t rs1,vint8m2_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint16m8_t test___riscv_vwmaccus(vbool2_t mask,vint16m8_t vd,uint8_t rs1,vint8m4_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint32mf2_t test___riscv_vwmaccus(vbool64_t mask,vint32mf2_t vd,uint16_t rs1,vint16mf4_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint32m1_t test___riscv_vwmaccus(vbool32_t mask,vint32m1_t vd,uint16_t rs1,vint16mf2_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint32m2_t test___riscv_vwmaccus(vbool16_t mask,vint32m2_t vd,uint16_t rs1,vint16m1_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint32m4_t test___riscv_vwmaccus(vbool8_t mask,vint32m4_t vd,uint16_t rs1,vint16m2_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint32m8_t test___riscv_vwmaccus(vbool4_t mask,vint32m8_t vd,uint16_t rs1,vint16m4_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint64m1_t test___riscv_vwmaccus(vbool64_t mask,vint64m1_t vd,uint32_t rs1,vint32mf2_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint64m2_t test___riscv_vwmaccus(vbool32_t mask,vint64m2_t vd,uint32_t rs1,vint32m1_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint64m4_t test___riscv_vwmaccus(vbool16_t mask,vint64m4_t vd,uint32_t rs1,vint32m2_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+vint64m8_t test___riscv_vwmaccus(vbool8_t mask,vint64m8_t vd,uint32_t rs1,vint32m4_t vs2,size_t vl)
+{
+    return __riscv_vwmaccus(mask,vd,rs1,vs2,31);
+}
+
+
+
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf8,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf4,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf2,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m1,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m2,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m4,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*mf4,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*mf2,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m1,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m2,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m4,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*mf2,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m1,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m2,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m4,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf8,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf4,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf2,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m1,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m2,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m4,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*mf4,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*mf2,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m1,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m2,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e16,\s*m4,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*mf2,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m1,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m2,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e32,\s*m4,\s*t[au],\s*m[au]\s+vwmaccus\.vx\s+v[0-9]+,\s*[a-x0-9]+,\s*v[0-9]+,\s*v0.t\s+} 1 } } */
