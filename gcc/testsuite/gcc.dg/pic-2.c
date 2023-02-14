38945ee3b2884e5514272c4b41a507ebdfddbb3d
Jonathan Yong <10walls@gmail.com>
2023-02-14 10:37:03 +0000

    gcc/testsuite/gcc.dg: Fix pic test case for PE targets
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/pic-2.c: Fix expected __PIC__ value.
    	* gcc.dg/pic-3.c: Ditto.
    	* gcc.dg/pic-4.c: Ditto.
    
    Signed-off-by: Jonathan Yong <10walls@gmail.com>

--- gcc/testsuite/gcc.dg/pic-2.c
+++ gcc/testsuite/gcc.dg/pic-2.c
@@ -4,7 +4,11 @@
 /* { dg-skip-if "__PIC__ is always 1 for MIPS" { mips*-*-* } } */
 /* { dg-skip-if "__PIE__ is always defined for GCN" { amdgcn*-*-* } } */
 
-#if __PIC__ != 2
+#if defined(__CYGWIN__) || defined(__WIN32__)
+# if __PIC__ != 1
+#  error __PIC__ is not 1!
+# endif
+#elif __PIC__ != 2
 # error __PIC__ is not 2!
 #endif
 
