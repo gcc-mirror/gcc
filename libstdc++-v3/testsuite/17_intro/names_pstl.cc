7478278f88ba1753e68d6962b7f38d1be5b43d56
Jonathan Wakely <jwakely@redhat.com>
2023-02-16 12:18:25 +0000

    libstdc++: Make names_pstl.cc require et tbb_backend
    The https://github.com/oneapi-src/oneTBB/pull/833 fix is needed for TBB
    headers to avoid an error with GCC 13. The new names_pstl.cc test needs
    to check effective target tbb_backend so that it's UNSUPPORTED on
    systems without fixed headers.
    
    libstdc++-v3/ChangeLog:
    
    	* testsuite/17_intro/names_pstl.cc: Require et tbb_backend.

--- libstdc++-v3/testsuite/17_intro/names_pstl.cc
+++ libstdc++-v3/testsuite/17_intro/names_pstl.cc
@@ -1,10 +1,12 @@
 // { dg-do compile { target c++17 } }
-// { dg-additional-options "-DTBB_SUPPRESS_DEPRECATED_MESSAGES=1" { target tbb_backend } }
+// { dg-require-effective-target tbb_backend }
+// { dg-add-options no_pch }
 
 // The TBB headers use non-reserved names (because they're not part of the
 // implementation) so we need to include them before the macro definitions
 // in names.cc:
 #if __has_include(<tbb/tbb.h>)
+# define TBB_SUPPRESS_DEPRECATED_MESSAGES 1
 # include <tbb/tbb.h>
 #endif
 // Now we can define the macros to poison uses of non-reserved names:
