fa88446bd539415fcb8301ba25048f8499422c88
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2023-02-20 15:48:13 +0100

    libproc_macro: Implement Display for Group
    Implement the Display trait on internal rust Group structure.
    
    ChangeLog:
    
    	* librust/proc_macro/rust/bridge/group.rs: Implement Display.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

--- librust/proc_macro/rust/bridge/group.rs
+++ librust/proc_macro/rust/bridge/group.rs
@@ -1,4 +1,5 @@
 use bridge;
+use std::fmt;
 use Delimiter;
 
 #[repr(C)]
@@ -6,4 +7,25 @@ use Delimiter;
 pub struct Group {
     delimiter: Delimiter,
     stream: bridge::token_stream::TokenStream,
+}
+impl fmt::Display for Group {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.delimiter {
+            Delimiter::Parenthesis => f.write_str("(")?,
+            Delimiter::Brace => f.write_str("{")?,
+            Delimiter::Bracket => f.write_str("[")?,
+            Delimiter::None => (),
+        }
+
+        self.stream.fmt(f)?;
+
+        match self.delimiter {
+            Delimiter::Parenthesis => f.write_str(")")?,
+            Delimiter::Brace => f.write_str("}")?,
+            Delimiter::Bracket => f.write_str("]")?,
+            Delimiter::None => (),
+        }
+
+        Ok(())
+    }
 }
