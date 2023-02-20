8901323c390b71b41ba11d1bcb9f166f2e15226c
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2023-02-20 16:34:43 +0100

    libproc_macro: Implement Display for Group
    Implement the Display trait for the rust Group external structure.
    
    ChangeLog:
    
    	* librust/proc_macro/rust/group.rs: Implement Display.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

--- librust/proc_macro/rust/group.rs
+++ librust/proc_macro/rust/group.rs
@@ -76,8 +76,8 @@ impl Group {
 }
 
 impl fmt::Display for Group {
-    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        todo!("Implement this function")
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
     }
 }
 
