82c0c5c5d9128ea173477638c5312f01d73837a1
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2023-02-20 16:14:51 +0100

    libproc_macro: Implement Display on Literal
    Implement the Display trait on external rust structure Literal.
    
    ChangeLog:
    
    	* librust/proc_macro/rust/literal.rs: Implement Display trait.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

--- librust/proc_macro/rust/literal.rs
+++ librust/proc_macro/rust/literal.rs
@@ -175,8 +175,8 @@ impl fmt::Debug for Literal {
 }
 
 impl fmt::Display for Literal {
-    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        todo!("Implement this function")
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
     }
 }
 
