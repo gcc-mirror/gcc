ab76d71179be6e4e09ee3d5b7e8fa4265717a34c
Roger Sayle <roger@nextmovesoftware.com>
2023-02-27 17:26:54 +0000

    Fix RTL simplifications of FFS, POPCOUNT and PARITY.
    In 2011, the rtl.texi documentation was updated to reflect that the
    modes of the RTX unary operations FFS, POPCOUNT and PARITY should
    match those of their operands.  Unfortunately, some of the transformations
    in simplify-rtx.cc predate this tightening of RTL semantics, and have
    not (until now) been updated/fixed.  i.e. The POPCOUNT and PARITY
    optimizations were "correct" when I added them back in 2007.
    
    2023-02-27  Roger Sayle  <roger@nextmovesoftware.com>
    
    gcc/ChangeLog
    	* simplify-rtx.cc (simplify_unary_operation_1) <case FFS>: Avoid
    	generating FFS with mismatched operand and result modes, by using
    	an explicit SIGN_EXTEND/ZERO_EXTEND.
    	<case POPCOUNT>: Likewise, for POPCOUNT of ZERO_EXTEND.
    	<case PARITY>: Likewise, for PARITY of {ZERO,SIGN}_EXTEND.

--- gcc/simplify-rtx.cc
+++ gcc/simplify-rtx.cc
@@ -1404,21 +1404,31 @@ simplify_context::simplify_unary_operation_1 (rtx_code code, machine_mode mode,
       break;
 
     case FFS:
-      /* (ffs (*_extend <X>)) = (ffs <X>) */
+      /* (ffs (*_extend <X>)) = (*_extend (ffs <X>)).  */
       if (GET_CODE (op) == SIGN_EXTEND
 	  || GET_CODE (op) == ZERO_EXTEND)
-	return simplify_gen_unary (FFS, mode, XEXP (op, 0),
-				   GET_MODE (XEXP (op, 0)));
+	{
+	  temp = simplify_gen_unary (FFS, GET_MODE (XEXP (op, 0)),
+				     XEXP (op, 0), GET_MODE (XEXP (op, 0)));
+	  return simplify_gen_unary (GET_CODE (op), mode, temp,
+				     GET_MODE (temp));
+	}
       break;
 
     case POPCOUNT:
       switch (GET_CODE (op))
 	{
 	case BSWAP:
-	case ZERO_EXTEND:
-	  /* (popcount (zero_extend <X>)) = (popcount <X>) */
+	  /* (popcount (bswap <X>)) = (popcount <X>).  */
 	  return simplify_gen_unary (POPCOUNT, mode, XEXP (op, 0),
 				     GET_MODE (XEXP (op, 0)));
+
+	case ZERO_EXTEND:
+	  /* (popcount (zero_extend <X>)) = (zero_extend (popcount <X>)).  */
+	  temp = simplify_gen_unary (POPCOUNT, GET_MODE (XEXP (op, 0)),
+				     XEXP (op, 0), GET_MODE (XEXP (op, 0)));
+	  return simplify_gen_unary (ZERO_EXTEND, mode, temp,
+				     GET_MODE (temp));
 
 	case ROTATE:
 	case ROTATERT:
@@ -1438,10 +1448,15 @@ simplify_context::simplify_unary_operation_1 (rtx_code code, machine_mode mode,
 	{
 	case NOT:
 	case BSWAP:
-	case ZERO_EXTEND:
-	case SIGN_EXTEND:
 	  return simplify_gen_unary (PARITY, mode, XEXP (op, 0),
 				     GET_MODE (XEXP (op, 0)));
+
+	case ZERO_EXTEND:
+	case SIGN_EXTEND:
+	  temp = simplify_gen_unary (PARITY, GET_MODE (XEXP (op, 0)),
+				     XEXP (op, 0), GET_MODE (XEXP (op, 0)));
+	  return simplify_gen_unary (GET_CODE (op), mode, temp,
+				     GET_MODE (temp));
 
 	case ROTATE:
 	case ROTATERT:
