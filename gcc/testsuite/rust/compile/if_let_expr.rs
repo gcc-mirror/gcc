d344a64be5b8f985acdccf644e0959a0564a54d5
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2023-03-01 14:20:23 +0100

    gccrs: testsuite: Add a test for if let syntax
    Add a new test to check the if let expression syntax parsing.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/if_let_expr.rs: New test.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

---
+++ gcc/testsuite/rust/compile/if_let_expr.rs
@@ -0,0 +1,19 @@
+// { dg-options "-fsyntax-only" }
+
+pub enum Option<T> {
+    None,
+    Some(T),
+}
+
+fn main() {
+    let x = Option::Some(3);
+    let a = if let Option::Some(1) = x {
+        1
+    } else if x == Option::Some(2) {
+        2
+    } else if let Option::Some(y) = x {
+        y
+    } else {
+        -1
+    };
+}
