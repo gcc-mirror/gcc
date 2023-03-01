745775eb2c47dad4f2ce30f5559ae39a9b73f184
Paul-Antoine Arras <pa@codesourcery.com>
2023-03-01 17:20:21 +0100

    amdgcn: Add instruction patterns for conditional min/max operations
    gcc/ChangeLog:
    
    	* config/gcn/gcn-valu.md (<expander><mode>3_exec): Add patterns for
    	{s|u}{max|min} in QI, HI and DI modes.
    	(<expander><mode>3): Add pattern for {s|u}{max|min} in DI mode.
    	(cond_<fexpander><mode>): Add pattern for cond_f{max|min}.
    	(cond_<expander><mode>): Add pattern for cond_{s|u}{max|min}.
    	* config/gcn/gcn.cc (gcn_spill_class): Allow the exec register to be
    	saved in SGPRs.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/gcn/cond_fmaxnm_1.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_1_run.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_2.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_2_run.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_3.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_3_run.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_4.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_4_run.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_5.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_5_run.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_6.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_6_run.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_7.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_7_run.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_8.c: New test.
    	* gcc.target/gcn/cond_fmaxnm_8_run.c: New test.
    	* gcc.target/gcn/cond_fminnm_1.c: New test.
    	* gcc.target/gcn/cond_fminnm_1_run.c: New test.
    	* gcc.target/gcn/cond_fminnm_2.c: New test.
    	* gcc.target/gcn/cond_fminnm_2_run.c: New test.
    	* gcc.target/gcn/cond_fminnm_3.c: New test.
    	* gcc.target/gcn/cond_fminnm_3_run.c: New test.
    	* gcc.target/gcn/cond_fminnm_4.c: New test.
    	* gcc.target/gcn/cond_fminnm_4_run.c: New test.
    	* gcc.target/gcn/cond_fminnm_5.c: New test.
    	* gcc.target/gcn/cond_fminnm_5_run.c: New test.
    	* gcc.target/gcn/cond_fminnm_6.c: New test.
    	* gcc.target/gcn/cond_fminnm_6_run.c: New test.
    	* gcc.target/gcn/cond_fminnm_7.c: New test.
    	* gcc.target/gcn/cond_fminnm_7_run.c: New test.
    	* gcc.target/gcn/cond_fminnm_8.c: New test.
    	* gcc.target/gcn/cond_fminnm_8_run.c: New test.
    	* gcc.target/gcn/cond_smax_1.c: New test.
    	* gcc.target/gcn/cond_smax_1_run.c: New test.
    	* gcc.target/gcn/cond_smin_1.c: New test.
    	* gcc.target/gcn/cond_smin_1_run.c: New test.
    	* gcc.target/gcn/cond_umax_1.c: New test.
    	* gcc.target/gcn/cond_umax_1_run.c: New test.
    	* gcc.target/gcn/cond_umin_1.c: New test.
    	* gcc.target/gcn/cond_umin_1_run.c: New test.
    	* gcc.target/gcn/smax_1.c: New test.
    	* gcc.target/gcn/smax_1_run.c: New test.
    	* gcc.target/gcn/smin_1.c: New test.
    	* gcc.target/gcn/smin_1_run.c: New test.
    	* gcc.target/gcn/umax_1.c: New test.
    	* gcc.target/gcn/umax_1_run.c: New test.
    	* gcc.target/gcn/umin_1.c: New test.
    	* gcc.target/gcn/umin_1_run.c: New test.
    
    (cherry picked from commit 553ff2524f412be4e02e2ffb1a0a3dc3e2280742)

---
+++ gcc/testsuite/gcc.target/gcn/cond_fmaxnm_2.c
@@ -0,0 +1,33 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -ftree-vectorize -ffast-math -dp" } */
+
+#include <stdint.h>
+
+#ifndef FN
+#define FN(X) __builtin_fmax##X
+#endif
+
+#define DEF_LOOP(FN, TYPE, NAME, CONST)                                        \
+  void __attribute__ ((noipa))                                                 \
+  test_##TYPE##_##NAME (TYPE *__restrict x, TYPE *__restrict y,                \
+			TYPE *__restrict z, int n)                             \
+  {                                                                            \
+    for (int i = 0; i < n; ++i)                                                \
+      x[i] = y[i] < 8 ? FN (z[i], CONST) : y[i];                               \
+  }
+
+#define TEST_TYPE(T, FN, TYPE)                                                 \
+  T (FN, TYPE, zero, 0)                                                        \
+  T (FN, TYPE, one, 1)                                                         \
+  T (FN, TYPE, two, 2)
+
+#define TEST_ALL(T)                                                            \
+  TEST_TYPE (T, FN (f32), float)                                               \
+  TEST_TYPE (T, FN (f64), double)
+
+TEST_ALL (DEF_LOOP)
+
+/* { dg-final { scan-assembler-times {smaxv64sf3_exec} 3 } } */
+/* { dg-final { scan-assembler-times {smaxv64df3_exec} 3 } } */
+
+/* { dg-final { scan-assembler-not {\tv_writelane_b32\tv[0-9]+, vcc_..} } } */
---
+++ gcc/testsuite/gcc.target/gcn/cond_fmaxnm_6.c
@@ -0,0 +1,9 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -ftree-vectorize -dp" } */
+
+#include "cond_fmaxnm_2.c"
+
+/* { dg-final { scan-assembler-times {smaxv64sf3_exec} 3 } } */
+/* { dg-final { scan-assembler-times {smaxv64df3_exec} 3 } } */
+
+/* { dg-final { scan-assembler-not {\tv_writelane_b32\tv[0-9]+, vcc_..} } } */
