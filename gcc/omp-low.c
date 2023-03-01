e7d995cf9bff2ca4a54b94c4a38455ebd1c2883e
Tobias Burnus <tobias@codesourcery.com>
2023-03-01 13:53:09 +0100

    OpenMP/Fortran: Fix handling of optional is_device_ptr + bind(C) [PR108546]
    For is_device_ptr, optional checks should only be done before calling
    libgomp, afterwards they are NULL either because of absent or, by
    chance, because it is unallocated or unassociated (for pointers/allocatables).
    
    Additionally, it fixes an issue with explicit mapping for 'type(c_ptr)'.
    
    	PR middle-end/108546
    
    gcc/fortran/ChangeLog:
    
    	* trans-openmp.c (gfc_trans_omp_clauses): Fix mapping of
    	type(C_ptr) variables.
    
    gcc/ChangeLog:
    
    	* omp-low.c (lower_omp_target): Remove optional handling
    	on the receiver side, i.e. inside target (data), for
    	use_device_ptr.
    
    libgomp/ChangeLog:
    
    	* testsuite/libgomp.fortran/is_device_ptr-3.f90: New test.
    	* testsuite/libgomp.fortran/use_device_ptr-optional-4.f90: New test.
    
    (cherry picked from commit 96ff97ff6574666a5509ae9fa596e7f2b6ad4f88)

--- gcc/omp-low.c
+++ gcc/omp-low.c
@@ -12910,7 +12910,8 @@ lower_omp_target (gimple_stmt_iterator *gsi_p, omp_context *ctx)
 				     gimple_build_assign (new_var, x));
 	      }
 	    tree present;
-	    present = (do_optional_check
+	    present = ((do_optional_check
+			&& OMP_CLAUSE_CODE (c) != OMP_CLAUSE_IS_DEVICE_PTR)
 		       ? omp_check_optional_argument (OMP_CLAUSE_DECL (c), true)
 		       : NULL_TREE);
 	    if (present)
