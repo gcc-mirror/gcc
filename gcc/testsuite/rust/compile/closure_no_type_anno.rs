ae91a0a3f2b44e5b011d3742a71c3c5030558bc8
Owen Avery <powerboat9.gamer@gmail.com>
2023-03-03 20:05:59 -0500

    Resolve closure parameters without given type
    gcc/rust/ChangeLog:
    
    	* resolve/rust-early-name-resolver.cc
    	(EarlyNameResolver::visit):
    	Check for closure parameter without given type.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/closure_no_type_anno.rs: Extend test.
    
    Signed-off-by: Owen Avery <powerboat9.gamer@gmail.com>

--- gcc/testsuite/rust/compile/closure_no_type_anno.rs
+++ gcc/testsuite/rust/compile/closure_no_type_anno.rs
@@ -1,5 +1,10 @@
-// { dg-additional-options "-fsyntax-only" }
+#[lang = "fn_once"]
+trait FnOnce<Args> {
+  type Output;
+
+  fn call_once(self, args: Args) -> Self::Output;
+}
 
 pub fn foo() {
-  let a = |_| 15;
+  let a = |_| 15; // { dg-error "type annotations needed" }
 }
