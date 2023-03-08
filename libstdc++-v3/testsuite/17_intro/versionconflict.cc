6f0d67b912900009bff5018bd58e17ea1db1de69
Arsen Arsenović <arsen@aarsen.me>
2023-03-08 17:01:24 +0100

    libstdc++: Add a test for <version> FTM redefinitions
    This test detects redefinitions by compiling stdc++.h and <version>, by
    disabling the system_header pragma on the latter, to allow warnings in
    it.  Thanks Patrick Palka for the suggestion.
    
    libstdc++-v3/ChangeLog:
    
    	* testsuite/17_intro/versionconflict.cc: New test.
    	* include/std/version: Allow disabling the system_header pragma
    	via _GLIBCXX_TESTING_SYSHDR.

---
+++ libstdc++-v3/testsuite/17_intro/versionconflict.cc
@@ -0,0 +1,7 @@
+// { dg-do preprocess }
+// { dg-additional-options "-Werror" }
+
+// Test for redefinitions of FTMs using bits/stdc++.h.
+#define _GLIBCXX_TESTING_SYSHDR
+#include <bits/stdc++.h>
+#include <version>
