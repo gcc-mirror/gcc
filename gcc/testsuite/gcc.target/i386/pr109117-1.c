a9ae16db8cbb2c0ec8e8384ac38ce618f9af1e3a
Hu, Lin1 <lin1.hu@intel.com>
2023-03-13 15:50:11 +0800

    i386:Add missing OPTION_MASK_ISA_AVX512VL in i386-builtin.def for VAES builtins
    gcc/ChangeLog:
    
    	PR target/109117
    	* config/i386/i386-builtin.def (__builtin_ia32_vaesdec_v16qi,
    	__builtin_ia32_vaesdeclast_v16qi,__builtin_ia32_vaesenc_v16qi,
    	__builtin_ia32_vaesenclast_v16qi): Require OPTION_MASK_ISA_AVX512VL.
    
    gcc/testsuite/ChangeLog:
    
    	PR target/109117
    	* gcc.target/i386/pr109117-1.c: New test.

---
+++ gcc/testsuite/gcc.target/i386/pr109117-1.c
@@ -0,0 +1,14 @@
+/* PR target/109117 */
+/* { dg-do compile } */
+/* { dg-options "-mvaes -mno-avx512vl" } */
+
+typedef char __v16qi __attribute__ ((__vector_size__(16)));
+typedef long long __m128i __attribute__((__vector_size__(16), __aligned__(16)));
+volatile __v16qi x, y;
+volatile __m128i res;
+
+void
+foo (void)
+{
+      res = __builtin_ia32_vaesdec_v16qi (x, y); /* { dg-warning "implicit declaration of function" } */
+}     /* { dg-error "incompatible types when assigning to type" "" { target *-*-* } .-1 } */
