0251292d2da4039d4b12ecee81588614bf99b546
Eric Botcazou <ebotcazou@adacore.com>
2023-03-14 12:42:53 +0100

    ada: Add missing guards in Selected_Range_Checks
    gcc/ada/
    
    	* checks.adb (Selected_Range_Checks): Add guards to protect calls
    	to Expr_Value on bounds.

--- gcc/ada/checks.adb
+++ gcc/ada/checks.adb
@@ -10826,6 +10826,8 @@ package body Checks is
 
                if not Check_Added
                  and then Is_Fixed_Lower_Bound_Index_Subtype (T_Typ)
+                 and then Known_LB
+                 and then Known_T_LB
                  and then Expr_Value (LB) /= Expr_Value (T_LB)
                then
                   Add_Check
