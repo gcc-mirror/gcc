6f7f90113bc78440a248bef4b917583fde3e6e5f
Jason Merrill <jason@redhat.com>
2023-03-16 13:11:32 -0400

    c++: &enum::enumerator [PR101869]
    We don't want to call build_offset_ref with an enum.
    
    	PR c++/101869
    
    gcc/cp/ChangeLog:
    
    	* semantics.c (finish_qualified_id_expr): Don't try to build a
    	pointer-to-member if the scope is an enumeration.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp0x/enum43.C: New test.

--- gcc/cp/semantics.c
+++ gcc/cp/semantics.c
@@ -2143,7 +2143,8 @@ finish_qualified_id_expr (tree qualifying_class,
 
   /* If EXPR occurs as the operand of '&', use special handling that
      permits a pointer-to-member.  */
-  if (address_p && done)
+  if (address_p && done
+      && TREE_CODE (qualifying_class) != ENUMERAL_TYPE)
     {
       if (TREE_CODE (expr) == SCOPE_REF)
 	expr = TREE_OPERAND (expr, 1);
