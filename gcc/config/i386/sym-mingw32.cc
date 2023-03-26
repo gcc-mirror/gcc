304c7d44a2212e6fd618587331cea2c266dc10bf
Costas Argyris <costas.argyris@gmail.com>
2023-03-26 11:32:13 +0100

    Extend UTF-8 support to the 32-bit mingw host.
    Prevent any name mangling in HOST_EXTRA_OBJS_SYMBOL
    such that the linker always finds it by that name.
    
    Also add the .manifest file as an explicit
    dependency in the make rule such that the
    object gets re-built if it changes.
    
    gcc/ChangeLog:
    
    	* config.host: Pull in i386/x-mingw32-utf8 Makefile
    	fragment and reference utf8rc-mingw32.o explicitly
    	for mingw hosts.
    	* config/i386/sym-mingw32.cc: prevent name mangling of
    	stub symbol.
    	* config/i386/x-mingw32-utf8: Make utf8rc-mingw32.o
    	depend on manifest file explicitly.
    
    Signed-off-by: Jonathan Yong <10walls@gmail.com>

--- gcc/config/i386/sym-mingw32.cc
+++ gcc/config/i386/sym-mingw32.cc
@@ -1,1 +1,3 @@
-char HOST_EXTRA_OBJS_SYMBOL;
+/* Prevent any name mangling to make sure that the linker
+   will always find the symbol. */
+char HOST_EXTRA_OBJS_SYMBOL asm ("HOST_EXTRA_OBJS_SYMBOL");
