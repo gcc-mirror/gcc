82ec75a726b3f8f874dacb0cb342c9bbd1233cc0
Jerry DeLisle <jvdelisle@gcc.gnu.org>
2023-03-26 18:44:35 -0700

    Fortran: Modify checks to avoid referencing NULL pointer.
    Backport from mainline.
    
    gcc/fortran/ChangeLog:
    
    	PR fortran/102331
    	* decl.cc (attr_decl1): Guard against NULL pointer.
    	* parse.cc (match_deferred_characteristics): Include BT_CLASS in check
    	for derived being undefined.
    
    gcc/testsuite/ChangeLog:
    
    	PR fortran/102331
    	* gfortran.dg/class_result_4.f90: Update error message check.
    	* gfortran.dg/pr85779_3.f90: Update error message check.

--- gcc/fortran/parse.cc
+++ gcc/fortran/parse.cc
@@ -3911,7 +3911,7 @@ match_deferred_characteristics (gfc_typespec * ts)
   m = gfc_match_prefix (ts);
   gfc_buffer_error (false);
 
-  if (ts->type == BT_DERIVED)
+  if (ts->type == BT_DERIVED || ts->type == BT_CLASS)
     {
       ts->kind = 0;
 
@@ -4195,7 +4195,7 @@ declSt:
   if (bad_characteristic)
     {
       ts = &gfc_current_block ()->result->ts;
-      if (ts->type != BT_DERIVED)
+      if (ts->type != BT_DERIVED && ts->type != BT_CLASS)
 	gfc_error ("Bad kind expression for function %qs at %L",
 		   gfc_current_block ()->name,
 		   &gfc_current_block ()->declared_at);
--- gcc/testsuite/gfortran.dg/class_result_4.f90
+++ gcc/testsuite/gfortran.dg/class_result_4.f90
@@ -1,6 +1,6 @@
 ! { dg-do compile }
 ! PR fortran/78500
-class(t) function f() ! { dg-error "must be dummy, allocatable or pointer" }
+class(t) function f() ! { dg-error "is not accessible" }
    f = 1              ! { dg-error "variable must not be polymorphic" }
 end
 
