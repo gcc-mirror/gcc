d79d1073c444ceb85b3cd2f55be7b4dfe598d287
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2023-03-28 10:40:05 +0200

    testsuite: Fix weak_undefined handling on Darwin
    The patch that introduced the weak_undefined effective-target keyword
    and corresponding dg-add-options support
    
    commit 378ec7b87a5265dbe2d489c245fac98ef37fa638
    Author: Alexandre Oliva <oliva@adacore.com>
    Date:   Thu Mar 23 00:45:05 2023 -0300
    
        [testsuite] test for weak_undefined support and add options
    
    badly broke the affected tests on macOS like so:
    
    ERROR: gcc.dg/addr_equal-1.c: unknown dg option: 89 for " dg-add-options 5 weak_undefined "
    ERROR: gcc.dg/addr_equal-1.c: unknown dg option: 89 for " dg-add-options 5 weak_undefined "
    
    add_options_for_weak_undefined tries to call an non-existant proc "89".
    Even after fixing this by escaping the brackets, two tests still failed to
    link since they lacked the corresponding calls do dg-add-options
    weak_undefined.
    
    Tested on x86_64-apple-darwin20.6.0 and i386-pc-solaris2.11.
    
    2023-03-27  Rainer Orth  <ro@CeBiTec.Uni-Bielefeld.DE>
    
    	gcc/testsuite:
    	* lib/target-supports.exp (add_options_for_weak_undefined): Escape
    	brackets.
    	* gcc.dg/visibility-22.c: Add weak_undefined options.
    
    (cherry picked from commit 8443f42f05f9026dadad1236b9e44ec294c70337)

--- gcc/testsuite/gcc.dg/visibility-22.c
+++ gcc/testsuite/gcc.dg/visibility-22.c
@@ -3,6 +3,7 @@
 /* { dg-require-visibility "" } */
 /* { dg-require-effective-target weak_undefined } */
 /* { dg-options "-O2 -fPIC" { target fpic } } */
+/* { dg-add-options weak_undefined } */
 
 extern void foo () __attribute__((weak,visibility("hidden")));
 int
