721bc1b926bbcd019bfdc19b626b3db45e8b01c5
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2023-04-03 20:44:45 +0200

    gccrs: ast: Move pattern implementation out of rust-ast
    Move rust-pattern implementation out of rust-ast.cc in their own file.
    
    gcc/rust/ChangeLog:
    
    	* Make-lang.in: Add two new files to the list of sources.
    	* ast/rust-ast-full.h: Add new ast formatting header.
    	* ast/rust-ast.cc (enum indent_mode): Move formatting function
    	to it's own file.
    	(indent_spaces): Likewise.
    	(get_string_in_delims): Likewise.
    	(enum AttrMode): Likewise.
    	(get_mode_dump_desc): Likewise.
    	(append_attributes): Likewise.
    	(unquote_string): Likewise.
    	(GroupedExpr::as_string): Move pattern implementation to it's
    	own file.
    	(RangePattern::as_string): Likewise.
    	(RangePatternBoundLiteral::as_string): Likewise.
    	(SlicePattern::as_string): Likewise.
    	(AltPattern::as_string): Likewise.
    	(TuplePatternItemsMultiple::as_string): Likewise.
    	(TuplePatternItemsRanged::as_string): Likewise.
    	(TuplePattern::as_string): Likewise.
    	(StructPatternField::as_string): Likewise.
    	(StructPatternFieldIdent::as_string): Likewise.
    	(StructPatternFieldTuplePat::as_string): Likewise.
    	(StructPatternFieldIdentPat::as_string): Likewise.
    	(StructPatternElements::as_string): Likewise.
    	(StructPattern::as_string): Likewise.
    	(LiteralPattern::as_string): Likewise.
    	(ReferencePattern::as_string): Likewise.
    	(IdentifierPattern::as_string): Likewise.
    	(TupleStructItemsNoRange::as_string): Likewise.
    	(TupleStructItemsRange::as_string): Likewise.
    	(TupleStructPattern::as_string): Likewise.
    	(GroupedExpr::accept_vis): Likewise.
    	(LiteralPattern::accept_vis): Likewise.
    	(IdentifierPattern::accept_vis): Likewise.
    	(WildcardPattern::accept_vis): Likewise.
    	(RestPattern::accept_vis): Likewise.
    	(RangePatternBoundLiteral::accept_vis): Likewise.
    	(RangePatternBoundPath::accept_vis): Likewise.
    	(RangePatternBoundQualPath::accept_vis): Likewise.
    	(RangePattern::accept_vis): Likewise.
    	(ReferencePattern::accept_vis): Likewise.
    	(StructPatternFieldTuplePat::accept_vis): Likewise.
    	(StructPatternFieldIdentPat::accept_vis): Likewise.
    	(StructPatternFieldIdent::accept_vis): Likewise.
    	(StructPattern::accept_vis): Likewise.
    	(TupleStructItemsNoRange::accept_vis): Likewise.
    	(TupleStructItemsRange::accept_vis): Likewise.
    	(TupleStructPattern::accept_vis): Likewise.
    	(TuplePatternItemsMultiple::accept_vis): Likewise.
    	(TuplePatternItemsRanged::accept_vis): Likewise.
    	(TuplePattern::accept_vis): Likewise.
    	(GroupedPattern::accept_vis): Likewise.
    	(SlicePattern::accept_vis): Likewise.
    	(AltPattern::accept_vis): Likewise.
    	* ast/rust-ast-formatting.cc: New file.
    	* ast/rust-ast-formatting.h: New file.
    	* ast/rust-pattern.cc: New file.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

--- gcc/rust/ast/rust-ast-full.h
+++ gcc/rust/ast/rust-ast-full.h
@@ -20,6 +20,7 @@
 #define RUST_AST_FULL_H
 // Use as a fast way of including all aspects of the AST (i.e. all headers)
 #include "rust-ast.h"
+#include "rust-ast-formatting.h"
 #include "rust-expr.h"
 #include "rust-item.h"
 #include "rust-path.h"
