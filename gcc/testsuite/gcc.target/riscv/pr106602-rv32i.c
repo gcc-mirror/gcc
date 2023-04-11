8c010f6fe5ebe80d2e054b31e04ae0e9f12ae368
Palmer Dabbelt <palmer@rivosinc.com>
2023-04-11 11:04:56 -0700

    RISC-V: Clean up the pr106602.c testcase
    The test case that was added is rv64i-specific, as there's better ways
    to generate this code on rv32i (where the long/int cast is a NOP) and on
    rv64i_zba (where we have word shifts).  This renames the original test
    case and adds two more for those targets.
    
    gcc/testsuite/ChangeLog:
    	PR target/106602
    	* gcc.target/riscv/pr106602.c: Moved to...
    	* gcc.target/riscv/pr106602-rv64i.c: ...here.
    	* gcc.target/riscv/pr106602-rv32i.c: New test.
    	* gcc.target/riscv/pr106602-rv64i_zba.c: New test.

--- gcc/testsuite/gcc.target/riscv/pr106602-rv32i.c
+++ gcc/testsuite/gcc.target/riscv/pr106602-rv32i.c
@@ -1,5 +1,5 @@
 /* { dg-do compile { target { riscv64*-*-* } } } */
-/* { dg-options "-O2" } */
+/* { dg-options "-O2 -march=rv32i -mabi=ilp32" } */
 
 unsigned long
 foo2 (unsigned long a)
@@ -8,7 +8,7 @@ foo2 (unsigned long a)
 }
 
 /* { dg-final { scan-assembler-times "slli\t" 1 } } */
-/* { dg-final { scan-assembler-times "srli\t" 1 } } */
+/* { dg-final { scan-assembler-not "srli\t" } } */
 /* { dg-final { scan-assembler-not "\tli\t" } } */
 /* { dg-final { scan-assembler-not "addi\t" } } */
 /* { dg-final { scan-assembler-not "and\t" } } */
