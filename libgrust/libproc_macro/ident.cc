81544c237851a542046fa06f437f77b30562d2da
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2023-04-11 12:18:54 +0200

    libproc_macro: Add ident implementation
    Add implementation and representation for ident cpp structure as well as
    a high level wrapper interface.
    
    ChangeLog:
    
    	* libgrust/libproc_macro/Makefile.am: Add ident to source list.
    	* libgrust/libproc_macro/Makefile.in: Regenerate.
    	* libgrust/libproc_macro/ident.cc: New file.
    	* libgrust/libproc_macro/ident.h: New file.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

---
+++ libgrust/libproc_macro/ident.cc
@@ -0,0 +1,56 @@
+// Copyright (C) 2023 Free Software Foundation, Inc.
+//
+// This file is part of the GNU Proc Macro Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// Under Section 7 of GPL version 3, you are granted additional
+// permissions described in the GCC Runtime Library Exception, version
+// 3.1, as published by the Free Software Foundation.
+
+// You should have received a copy of the GNU General Public License and
+// a copy of the GCC Runtime Library Exception along with this program;
+// see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+// <http://www.gnu.org/licenses/>.
+#include "ident.h"
+
+#include <cstring>
+
+extern "C" {
+Ident
+Ident__new (unsigned char *str, std::uint64_t len)
+{
+  unsigned char *val = new unsigned char[len];
+  std::memcpy (val, str, len);
+  return {false, val, len};
+}
+
+Ident
+Ident__new_raw (unsigned char *str, std::uint64_t len)
+{
+  unsigned char *val = new unsigned char[len];
+  std::memcpy (val, str, len);
+  return {true, val, len};
+}
+
+void
+Ident__drop (Ident *ident)
+{
+  delete[] ident->val;
+}
+
+Ident
+Ident__clone (const Ident *ident)
+{
+  unsigned char *val = new unsigned char[ident->len];
+  std::memcpy (val, ident->val, ident->len);
+  return {ident->is_raw, val, ident->len};
+}
+}
