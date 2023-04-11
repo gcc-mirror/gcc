5a15a78b919c43954fbfcc90f53f34d7e2700c97
Michael Meissner <meissner@linux.ibm.com>
2023-04-11 10:11:53 -0400

    Backport from master
    2023-04-11  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	PR target/109067
    	* config/rs6000/rs6000.c (create_complex_muldiv): Delete.
    	(init_float128_ieee): Delete code to switch complex multiply and divide
    	for long double.  Backport from master, 3/20/2023.
    	(complex_multiply_builtin_code): New helper function.
    	(complex_divide_builtin_code): Likewise.
    	(rs6000_mangle_decl_assembler_name): Add support for mangling the name
    	of complex 128-bit multiply and divide built-in functions.
    
    gcc/testsuite/
    
    	PR target/109067
    	* gcc.target/powerpc/divic3-1.c: New test.  Backport from master,
    	3/20/2023.
    	* gcc.target/powerpc/divic3-2.c: Likewise.
    	* gcc.target/powerpc/mulic3-1.c: Likewise.
    	* gcc.target/powerpc/mulic3-2.c: Likewise.

---
+++ gcc/testsuite/gcc.target/powerpc/mulic3-2.c
@@ -0,0 +1,25 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target ppc_float128_sw } */
+/* { dg-require-effective-target longdouble128 } */
+/* { dg-options "-O2 -mabi=ibmlongdouble -Wno-psabi" } */
+
+/* When GCC is configured with an older library that does not support IEEE
+   128-bit, it issues a warning if you change the long double type. We use
+   -Wno-psabi to silence this warning.  Since this is a code generation test,
+   it does not matter if the library has full IEEE 128-bit support.
+
+   We also need to require that the default long double is 128-bits, otherwise
+   the TC/TF modes might not be available.  */
+
+/* Check that complex multiply generates the right call for __ibm128 when long
+   double is IBM 128-bit floating point.  */
+
+typedef _Complex long double c_ibm128_t __attribute__((mode(__TC__)));
+
+void
+multiply (c_ibm128_t *p, c_ibm128_t *q, c_ibm128_t *r)
+{
+  *p = *q * *r;
+}
+
+/* { dg-final { scan-assembler {\mbl .*__multc3\M} } } */
