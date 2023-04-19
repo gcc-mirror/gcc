13c5de145f027ade74ad1d890aa6118662a0c024
Peng Fan <fanpeng@loongson.cn>
2023-04-19 16:23:42 +0800

    LoongArch: Fix MUSL_DYNAMIC_LINKER
    The system based on musl has no '/lib64', so change it.
    
    https://wiki.musl-libc.org/guidelines-for-distributions.html,
    "Multilib/multi-arch" section of this introduces it.
    
    gcc/
    	* config/loongarch/gnu-user.h (MUSL_DYNAMIC_LINKER): Redefine.
    
    Signed-off-by: Peng Fan <fanpeng@loongson.cn>
    Suggested-by: Xi Ruoyao <xry111@xry111.site>
    
    (cherry picked from commit a80c68a08604b0ac625ac7fc59eae40b551b1176)

--- gcc/config/loongarch/gnu-user.h
+++ gcc/config/loongarch/gnu-user.h
@@ -33,9 +33,14 @@ along with GCC; see the file COPYING3.  If not see
 #define GLIBC_DYNAMIC_LINKER \
   "/lib" ABI_GRLEN_SPEC "/ld-linux-loongarch-" ABI_SPEC ".so.1"
 
+#define MUSL_ABI_SPEC \
+  "%{mabi=lp64d:-lp64d}" \
+  "%{mabi=lp64f:-lp64f}" \
+  "%{mabi=lp64s:-lp64s}"
+
 #undef MUSL_DYNAMIC_LINKER
 #define MUSL_DYNAMIC_LINKER \
-  "/lib" ABI_GRLEN_SPEC "/ld-musl-loongarch-" ABI_SPEC ".so.1"
+  "/lib/ld-musl-loongarch" ABI_GRLEN_SPEC MUSL_ABI_SPEC ".so.1"
 
 #undef GNU_USER_TARGET_LINK_SPEC
 #define GNU_USER_TARGET_LINK_SPEC \
