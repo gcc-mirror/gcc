91a86375d9869c8652734ce28dd30c2cdb21656e
Philip Herron <herron.philip@googlemail.com>
2023-04-20 12:33:55 +0100

    gccrs: fix ICE with recursive function calls
    Fixes #2136
    
    gcc/rust/ChangeLog:
    
    	* backend/rust-compile-item.cc (CompileItem::visit): remove bad checks
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/issue-2136-1.rs: New test.
    	* rust/compile/issue-2136-2.rs: New test.
    
    Signed-off-by: Philip Herron <herron.philip@googlemail.com>

---
+++ gcc/testsuite/rust/compile/issue-2136-1.rs
@@ -0,0 +1,14 @@
+pub trait Foo {
+    fn foo();
+}
+
+impl Foo for u16 {
+    fn foo() {
+        // { dg-warning "infinite recursion detected" "" { target *-*-* } .-1 }
+        <u16 as Foo>::foo()
+    }
+}
+
+fn main() {
+    <u16>::foo();
+}
