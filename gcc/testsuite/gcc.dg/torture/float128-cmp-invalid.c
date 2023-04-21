6afa7d31a0e8865e17b317ada5cc5014b5d07da3
Haochen Gui <guihaoc@gcc.gnu.org>
2023-04-21 16:42:31 +0800

    rs6000: xfail float128 comparison test case that fails on powerpc64.
    This patch xfails a float128 comparison test case on powerpc64 that
    fails due to a longstanding issue with floating-point compares.
    
    See PR58684 for more information.
    
    When float128 hardware is enabled (-mfloat128-hardware), xscmpuqp is
    generated for comparison which is unexpected.   When float128 software
    emulation is enabled (-mno-float128-hardware), we still have to xfail
    the hardware version (__lekf2_hw) which finally generates xscmpuqp.
    
    gcc/testsuite/
    	PR target/108728
    	* gcc.dg/torture/float128-cmp-invalid.c: Add xfail.

--- gcc/testsuite/gcc.dg/torture/float128-cmp-invalid.c
+++ gcc/testsuite/gcc.dg/torture/float128-cmp-invalid.c
@@ -1,5 +1,6 @@
 /* Test for "invalid" exceptions from __float128 comparisons.  */
 /* { dg-do run } */
+/* { dg-xfail-run-if "xfail for ppc float128_hw" { ppc_float128_hw || { ppc_cpu_supports_hw && p9vector_hw } } } */
 /* { dg-options "" } */
 /* { dg-require-effective-target __float128 } */
 /* { dg-require-effective-target base_quadfloat_support } */
