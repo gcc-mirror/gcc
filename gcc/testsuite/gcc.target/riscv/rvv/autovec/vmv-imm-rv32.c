6445f1224509861f2d471a66134b98f26290586e
Robin Dapp <rdapp@ventanamicro.com>
2023-04-26 20:32:36 +0200

    RISC-V: Allow vector constants in riscv_const_insns.
    This patch adds various vector constants to riscv_const_insns in order
    for them to be properly recognized as immediate operands.  This then
    allows to emit vmv.v.i instructions via autovectorization.
    
    gcc/ChangeLog:
    
    	* config/riscv/riscv.cc (riscv_const_insns): Add permissible
    	vector constants.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/vmv-imm-rv32.c: New test.
    	* gcc.target/riscv/rvv/autovec/vmv-imm-rv64.c: New test.
    	* gcc.target/riscv/rvv/autovec/vmv-imm-template.h: New test.
    	* gcc.target/riscv/rvv/autovec/vmv-imm-run.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/vmv-imm-rv32.c
@@ -0,0 +1,6 @@
+/* { dg-do compile } */
+/* { dg-additional-options "-std=c99 -march=rv32gcv -mabi=ilp32d -fno-vect-cost-model --param=riscv-autovec-preference=scalable -fno-builtin" } */
+
+#include "vmv-imm-template.h"
+
+/* { dg-final { scan-assembler-times "vmv.v.i" 32 } } */
