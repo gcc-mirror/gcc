977a3be3ccbc7f177316b2b349523023cac37bcd
Jeff Law <jlaw@ventanamicro>
2023-05-01 07:40:38 -0600

    Convert xstormy16 to LRA
    This patch converts the xstormy16 patch to LRA.  It introduces a code
    quality regression in the shiftsi testcase, but it also fixes numerous
    aborts/errors.  IMHO it's a good tradeoff.
    
    gcc/
    
    	* config/stormy16/stormy16.cc (TARGET_LRA_P): Remove defintion.

--- gcc/config/stormy16/stormy16.cc
+++ gcc/config/stormy16/stormy16.cc
@@ -2894,9 +2894,6 @@ xstormy16_push_rounding (poly_int64 bytes)
 #undef  TARGET_PREFERRED_OUTPUT_RELOAD_CLASS
 #define TARGET_PREFERRED_OUTPUT_RELOAD_CLASS xstormy16_preferred_reload_class
 
-#undef TARGET_LRA_P
-#define TARGET_LRA_P hook_bool_void_false
-
 #undef TARGET_LEGITIMATE_ADDRESS_P
 #define TARGET_LEGITIMATE_ADDRESS_P	xstormy16_legitimate_address_p
 #undef TARGET_MODE_DEPENDENT_ADDRESS_P
