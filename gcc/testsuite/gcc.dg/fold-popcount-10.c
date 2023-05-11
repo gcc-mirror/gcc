5fdcfe3c577657cb4d6dbc394a3c10b6a8e8cbd3
Roger Sayle <roger@nextmovesoftware.com>
2023-05-11 08:15:21 +0100

    match.pd: Simplify popcount(X&Y)+popcount(X|Y) as popcount(X)+popcount(Y)
    This patch teaches match.pd to simplify popcount(X&Y)+popcount(X|Y) as
    popcount(X)+popcount(Y), and the related simplifications that
    popcount(X)+popcount(Y)-popcount(X&Y) is popcount(X|Y).  As surprising
    as it might seem, this idiom is common in cheminformatics codes
    (for Tanimoto coefficient calculations).
    
    2023-05-11  Roger Sayle  <roger@nextmovesoftware.com>
    
    gcc/ChangeLog
    	* match.pd <popcount optimizations>: Simplify popcount(X|Y) +
    	popcount(X&Y) as popcount(X)+popcount(Y).  Likewise, simplify
    	popcount(X)+popcount(Y)-popcount(X&Y) as popcount(X|Y), and
    	vice versa.
    
    gcc/testsuite/ChangeLog
    	* gcc.dg/fold-popcount-8.c: New test case.
    	* gcc.dg/fold-popcount-9.c: Likewise.
    	* gcc.dg/fold-popcount-10.c: Likewise.

---
+++ gcc/testsuite/gcc.dg/fold-popcount-10.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+#define popc __builtin_popcount
+
+int foo1(unsigned int x, unsigned int y)
+{
+  return popc (x) + popc (y) - popc (x|y);
+}
+
+int foo2(unsigned int x, unsigned int y)
+{
+  return popc (y) + popc (x) - popc (x|y);
+}
+
+int foo3(unsigned int x, unsigned int y)
+{
+  return (popc (x) - popc (x|y)) + popc (y);
+}
+
+int foo4(unsigned int x, unsigned int y)
+{
+  return (popc (y) - popc (x|y)) + popc (x);
+}
+
+/* { dg-final { scan-tree-dump-not " \\| " "optimized" } } */
+/* { dg-final { scan-tree-dump-times " & " 4 "optimized" } } */
+/* { dg-final { scan-tree-dump-times "popcount " 4 "optimized" } } */
