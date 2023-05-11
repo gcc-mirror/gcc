e24fe8e416ec68a0498c470f8b81716ff55bbc57
Haochen Gui <guihaoc@gcc.gnu.org>
2023-05-11 10:13:06 +0800

    rs6000: Change ilp32 target check for scalar-extract-sig and scalar-insert-exp test cases
    gcc/testsuite/
    	* gcc.target/powerpc/bfp/scalar-extract-sig-2.c: Replace ilp32 check
    	with dg-skip-if has_arch_ppc64.
    	* gcc.target/powerpc/bfp/scalar-insert-exp-2.c: Likewise.
    	* gcc.target/powerpc/bfp/scalar-insert-exp-5.c: Likewise.

--- gcc/testsuite/gcc.target/powerpc/bfp/scalar-extract-sig-2.c
+++ gcc/testsuite/gcc.target/powerpc/bfp/scalar-extract-sig-2.c
@@ -1,5 +1,5 @@
 /* { dg-do compile { target { powerpc*-*-* } } } */
-/* { dg-require-effective-target ilp32 } */
+/* { dg-skip-if "" { has_arch_ppc64 } } */
 /* { dg-require-effective-target powerpc_p9vector_ok } */
 /* { dg-options "-mdejagnu-cpu=power9" } */
 
