842a432b02238361ecc601d301ac400a7f30f4fa
Paul Thomas <pault@gcc.gnu.org>
2023-05-23 06:46:37 +0100

    Fortran: Fix assumed length chars and len inquiry [PR103716]
    2023-05-23  Paul Thomas  <pault@gcc.gnu.org>
    
    gcc/fortran
    	PR fortran/103716
    	* resolve.cc (gfc_resolve_ref): Conversion of array_ref into an
    	element should be done for all characters without a len expr,
    	not just deferred lens, and for integer expressions.
    	* trans-expr.cc (conv_inquiry): For len and kind inquiry refs,
    	set the se string_length to NULL_TREE.
    
    gcc/testsuite/
    	PR fortran/103716
    	* gfortran.dg/pr103716.f90 : New test.

--- gcc/fortran/resolve.cc
+++ gcc/fortran/resolve.cc
@@ -5526,7 +5526,9 @@ gfc_resolve_ref (gfc_expr *expr)
 	case REF_INQUIRY:
 	  /* Implement requirement in note 9.7 of F2018 that the result of the
 	     LEN inquiry be a scalar.  */
-	  if (ref->u.i == INQUIRY_LEN && array_ref && expr->ts.deferred)
+	  if (ref->u.i == INQUIRY_LEN && array_ref
+	      && ((expr->ts.type == BT_CHARACTER && !expr->ts.u.cl->length)
+		  || expr->ts.type == BT_INTEGER))
 	    {
 	      array_ref->u.ar.type = AR_ELEMENT;
 	      expr->rank = 0;
