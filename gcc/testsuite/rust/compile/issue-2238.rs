bc2cac14138c5276562e8df83c01646e99a117ff
Philip Herron <herron.philip@googlemail.com>
2023-05-30 13:45:58 +0100

    gccrs: Add missing name resolution to item statements
    This fixes the issue but there are two cleanups to do at some point.
    
      1. misc namesapce this is a scope AST namespace where we dump resolution
         info when its not defined here. This occurs in the case such as nested
         scopes where the nested scope is popped and we hit an assertion.
         Outside of name resolution this requirement shouldnt really apply
         it should be permissive to allow for this
    
      2. We reuse our existing name resolution pieces here for Traits and impl
         blocks we should start doing this for the other statements.
    
    Fixes #2238
    
    gcc/rust/ChangeLog:
    
    	* resolve/rust-ast-resolve-stmt.cc (ResolveStmt::visit): add name resolution
    	* resolve/rust-ast-resolve-stmt.h: likewise
    	* typecheck/rust-hir-type-check-expr.cc (TypeCheckExpr::visit): insert resolved node
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/issue-2238.rs: New test.
    
    Signed-off-by: Philip Herron <herron.philip@googlemail.com>

---
+++ gcc/testsuite/rust/compile/issue-2238.rs
@@ -0,0 +1,15 @@
+fn main() {
+    struct Foo;
+
+    trait Bar {
+        fn foo(&self);
+    }
+
+    impl Bar for Foo {
+        fn foo(&self) {}
+        // { dg-warning "unused name" "" { target *-*-* } .-1 }
+    }
+
+    let s = Foo;
+    s.foo();
+}
