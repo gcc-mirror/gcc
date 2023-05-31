efa6a82b978fd6aecd3b8184a0f0d0e5fba816f7
Haochen Jiang <haochen.jiang@intel.com>
2023-05-31 10:45:00 +0800

    Support Intel AMX-COMPLEX
    gcc/ChangeLog:
    
    	* common/config/i386/cpuinfo.h (get_available_features):
    	Detect AMX-COMPLEX.
    	* common/config/i386/i386-common.cc
    	(OPTION_MASK_ISA2_AMX_COMPLEX_SET,
    	OPTION_MASK_ISA2_AMX_COMPLEX_UNSET): New.
    	(ix86_handle_option): Handle -mamx-complex.
    	* common/config/i386/i386-cpuinfo.h (enum processor_features):
    	Add FEATURE_AMX_COMPLEX.
    	* common/config/i386/i386-isas.h: Add ISA_NAME_TABLE_ENTRY for
    	amx-complex.
    	* config.gcc: Add amxcomplexintrin.h.
    	* config/i386/cpuid.h (bit_AMX_COMPLEX): New.
    	* config/i386/i386-c.cc (ix86_target_macros_internal): Define
    	__AMX_COMPLEX__.
    	* config/i386/i386-isa.def (AMX_COMPLEX): Add DEF_PTA(AMX_COMPLEX).
    	* config/i386/i386-options.cc (ix86_valid_target_attribute_inner_p):
    	Handle amx-complex.
    	* config/i386/i386.opt: Add option -mamx-complex.
    	* config/i386/immintrin.h: Include amxcomplexintrin.h.
    	* doc/extend.texi: Document amx-complex.
    	* doc/invoke.texi: Document -mamx-complex.
    	* doc/sourcebuild.texi: Document target amx-complex.
    	* config/i386/amxcomplexintrin.h: New file.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/other/i386-2.C: Add -mamx-complex.
    	* g++.dg/other/i386-3.C: Ditto.
    	* gcc.target/i386/amx-check.h: Add cpu check for AMX-COMPLEX.
    	* gcc.target/i386/amx-helper.h: Add amx-complex support.
    	* gcc.target/i386/funcspec-56.inc: Add new target attribute.
    	* gcc.target/i386/sse-12.c: Add -mamx-complex.
    	* gcc.target/i386/sse-13.c: Ditto.
    	* gcc.target/i386/sse-14.c: Ditto.
    	* gcc.target/i386/sse-22.c: Add amx-complex.
    	* gcc.target/i386/sse-23.c: Ditto.
    	* lib/target-supports.exp (check_effective_target_amx_complex): New.
    	* gcc.target/i386/amxcomplex-asmatt-1.c: New test.
    	* gcc.target/i386/amxcomplex-asmintel-1.c: Ditto.
    	* gcc.target/i386/amxcomplex-cmmimfp16ps-2.c: Ditto.
    	* gcc.target/i386/amxcomplex-cmmrlfp16ps-2.c: Ditto.

---
+++ gcc/testsuite/gcc.target/i386/amxcomplex-cmmrlfp16ps-2.c
@@ -0,0 +1,53 @@
+/* { dg-do run { target { ! ia32 } } } */
+/* { dg-require-effective-target amx_complex } */
+/* { dg-require-effective-target avx512fp16 } */
+/* { dg-options "-O2 -mamx-complex -mavx512fp16" } */
+#define AMX_COMPLEX
+#define DO_TEST test_amx_complex_cmmrlfp16ps
+void test_amx_complex_cmmrlfp16ps();
+#include "amx-helper.h"
+
+void calc_matrix_cmmrlfp16ps (__tile *dst, __tile *src1, __tile *src2)
+{
+  uint16_t *src1_buf = (uint16_t *) src1->buf;
+  uint16_t *src2_buf = (uint16_t *) src2->buf;
+  float *dst_buf = (float *) dst->buf;
+  
+  int M = src1->rows;
+  int N = src1->colsb / 4;
+  int K = src2->colsb / 4;
+  int i, j, k, t;
+
+  for (i = 0; i < M; i++)
+    for (j = 0; j < N; j++)
+      for (k = 0; k < K; k++)
+	for (t = 0; t < 2; t+=2)
+	  dst_buf[i * N + k] += 
+	    (make_fp16_f32 (src1_buf[i * 2 * N + 2 * j + t]) *
+	      make_fp16_f32 (src2_buf[j * 2 * K + 2 * k + t])) -
+	    (make_fp16_f32 (src1_buf[i * 2 * N + 2 * j + t + 1]) *
+	      make_fp16_f32 (src2_buf[j * 2 * K + 2 * k + t + 1]));
+}
+
+void test_amx_complex_cmmrlfp16ps ()
+{
+  __tilecfg_u cfg;
+  __tile dst, dst_ref, src1, src2;
+  uint8_t tmp_dst_buf[1024], tmp_dst_zero_buf[1024];
+
+  init_fp16_max_tile_buffer (tmp_dst_buf);
+  init_fp16_max_tile_zero_buffer (tmp_dst_zero_buf);
+
+  init_tile_config (&cfg);
+  init_tile_reg_and_src_with_buffer (1, dst, tmp_dst_zero_buf);
+  init_tile_reg_and_src_with_buffer (2, src1, tmp_dst_buf);
+  init_tile_reg_and_src_with_buffer (3, src2, tmp_dst_buf);
+
+  calc_matrix_cmmrlfp16ps (&dst, &src1, &src2);
+  
+  _tile_cmmrlfp16ps (1, 2, 3);
+  _tile_stored (1, dst_ref.buf, _STRIDE);
+
+  if (!check_tile_register (&dst_ref, &dst))
+        abort ();
+}
