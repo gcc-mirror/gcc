8e1e1fc4fcb0d7d93222a707f9b83ae400c5a3fa
Uros Bizjak <ubizjak@gmail.com>
2023-06-05 14:51:02 +0200

    print-rtl: Change return type of two print functions from int to void
    Also change one internal variable to bool.
    
    gcc/ChangeLog:
    
    	* rtl.h (print_rtl_single): Change return type from int to void.
    	(print_rtl_single_with_indent): Ditto.
    	* print-rtl.h (class rtx_writer): Ditto.  Change m_sawclose to bool.
    	* print-rtl.cc (rtx_writer::rtx_writer): Update for m_sawclose change.
    	(rtx_writer::print_rtx_operand_code_0): Ditto.
    	(rtx_writer::print_rtx_operand_codes_E_and_V): Ditto.
    	(rtx_writer::print_rtx_operand_code_i): Ditto.
    	(rtx_writer::print_rtx_operand_code_u): Ditto.
    	(rtx_writer::print_rtx_operand): Ditto.
    	(rtx_writer::print_rtx): Ditto.
    	(rtx_writer::finish_directive): Ditto.
    	(print_rtl_single): Change return type from int to void
    	and adjust function body accordingly.
    	(rtx_writer::print_rtl_single_with_indent): Ditto.

--- gcc/rtl.h
+++ gcc/rtl.h
@@ -4241,8 +4241,8 @@ extern const rtx_insn *debug_rtx_find (const rtx_insn *, int);
 extern void print_mem_expr (FILE *, const_tree);
 extern void print_rtl (FILE *, const_rtx);
 extern void print_simple_rtl (FILE *, const_rtx);
-extern int print_rtl_single (FILE *, const_rtx);
-extern int print_rtl_single_with_indent (FILE *, const_rtx, int);
+extern void print_rtl_single (FILE *, const_rtx);
+extern void print_rtl_single_with_indent (FILE *, const_rtx, int);
 extern void print_inline_rtx (FILE *, const_rtx, int);
 
 /* In stmt.cc */
