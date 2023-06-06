aa087355c724b883189a3d2964e3050abef3dadf
Jonathan Wakely <jwakely@redhat.com>
2023-06-06 16:09:29 +0100

    libstdc++: Fix some tests that fail with -fexcess-precision=standard
    libstdc++-v3/ChangeLog:
    
    	* testsuite/20_util/duration/cons/2.cc: Use values that aren't
    	affected by rounding.
    	* testsuite/20_util/from_chars/5.cc: Cast arithmetic result to
    	double before comparing for equality.
    	* testsuite/20_util/from_chars/6.cc: Likewise.
    	* testsuite/20_util/variant/86874.cc: Use values that aren't
    	affected by rounding.
    	* testsuite/25_algorithms/lower_bound/partitioned.cc: Compare to
    	original value instead of to floating-point-literal.
    	* testsuite/26_numerics/random/discrete_distribution/cons/range.cc:
    	Cast arithmetic result to double before comparing for equality.
    	* testsuite/26_numerics/random/piecewise_constant_distribution/cons/range.cc:
    	Likewise.
    	* testsuite/26_numerics/random/piecewise_linear_distribution/cons/range.cc:
    	Likewise.
    	* testsuite/26_numerics/valarray/transcend.cc (eq): Check that
    	the absolute difference is less than 0.01 instead of comparing
    	to two decimal places.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/01.cc:
    	Cast arithmetic result to double before comparing for equality.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/09.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/char/10.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/wchar_t/01.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/wchar_t/09.cc:
    	Likewise.
    	* testsuite/27_io/basic_istream/extractors_arithmetic/wchar_t/10.cc:
    	Likewise.
    	* testsuite/ext/random/hoyt_distribution/cons/parms.cc: Likewise.
    
    (cherry picked from commit ae12aced97b41c7498ffc5b4dd314878289547ab)

--- libstdc++-v3/testsuite/25_algorithms/lower_bound/partitioned.cc
+++ libstdc++-v3/testsuite/25_algorithms/lower_bound/partitioned.cc
@@ -84,10 +84,10 @@ test02()
 
   auto part3 = std::lower_bound(c.begin(), c.end(), Y{1.0});
   VERIFY( part3 != c.end() );
-  VERIFY( part3->val == 1.2 );
+  VERIFY( part3->val == seq[1].val );
   auto part4 = std::lower_bound(c.begin(), c.end(), Y{1.0}, std::less<Y>{});
   VERIFY( part4 != c.end() );
-  VERIFY( part4->val == 1.2 );
+  VERIFY( part4->val == seq[1].val );
 }
 
 int
