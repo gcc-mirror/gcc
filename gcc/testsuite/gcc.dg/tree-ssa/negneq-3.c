cc155ff9c38848a8e6a7125dd0b66ac0aef47880
Andrew Pinski <apinski@marvell.com>
2023-06-05 19:12:43 -0700

    For the `-A CMP -B -> B CMP A` pattern allow EQ/NE for all integer types
    I noticed while looking at some code generation issue, that forwprop
    was not handling `-a == 0` for unsigned types and I was confused why
    it was not.
    r6-1814-g66e1cacf608045 removed these from fold because they
    were supposed to be already handled by the match.pd patterns
    but it was missed that the match.pd patterns checked
    TYPE_OVERFLOW_UNDEFINED while fold didn't do that for NE/EQ.
    This patch removes the restriction on NE/EQ on TYPE_OVERFLOW_UNDEFINED.
    
    OK? Bootstrapped and tested on x86_64-linux-gnu.
    
    gcc/ChangeLog:
    
    	PR tree-optimization/110134
    	* match.pd (-A CMP -B -> B CMP A): Allow EQ/NE for all integer
    	types.
    	(-A CMP CST -> B CMP (-CST)): Likewise.
    
    gcc/testsuite/ChangeLog:
    
    	PR tree-optimization/110134
    	* gcc.dg/tree-ssa/negneq-1.c: New test.
    	* gcc.dg/tree-ssa/negneq-2.c: New test.
    	* gcc.dg/tree-ssa/negneq-3.c: New test.
    	* gcc.dg/tree-ssa/negneq-4.c: New test.

---
+++ gcc/testsuite/gcc.dg/tree-ssa/negneq-3.c
@@ -0,0 +1,20 @@
+/* PR tree-optimization/110134 */
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-optimized" } */
+
+int fu(unsigned a, unsigned b)
+{
+  a = -a;
+  b = -b;
+  return a == b;
+}
+int fs(signed a, signed b)
+{
+  a = -a;
+  b = -b;
+  return a == b;
+}
+
+/* We should have optimized out the a = -; statements. */
+/* { dg-final { scan-tree-dump-not "= -a" "optimized" } } */
+/* { dg-final { scan-tree-dump-not "= -b" "optimized" } } */
