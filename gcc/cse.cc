c0bd79300e8fad52ec0fac7c65838a525d544a25
Jiufu Guo <guojiufu@linux.ibm.com>
2023-06-09 16:30:54 +0800

    Check SCALAR_INT_MODE_P in try_const_anchors
    The const_anchor in cse.cc supports integer constants only.
    There is a "gcc_assert (SCALAR_INT_MODE_P (mode))" in
    try_const_anchors.
    
    In the latest code, some non-integer modes are used with const int.
    For examples:
    "set (mem/c:BLK (xx) (const_int 0 [0])" occur in md files of
    rs6000, i386, arm, and pa. For this, the mode may be BLKmode.
    Pattern "(set (strict_low_part (xx)) (const_int xx))" could
    be generated in a few ports. For this, the mode may be VOIDmode.
    
    So, avoid mode other than SCALAR_INT_MODE in try_const_anchors
    would be needed.
    
    Some discussions in the previous thread:
    https://gcc.gnu.org/pipermail/gcc-patches/2023-June/621097.html
    
    gcc/ChangeLog:
    
    	* cse.cc (try_const_anchors): Check SCALAR_INT_MODE.

--- gcc/cse.cc
+++ gcc/cse.cc
@@ -1312,11 +1312,10 @@ try_const_anchors (rtx src_const, machine_mode mode)
   rtx lower_exp = NULL_RTX, upper_exp = NULL_RTX;
   unsigned lower_old, upper_old;
 
-  /* CONST_INT is used for CC modes, but we should leave those alone.  */
-  if (GET_MODE_CLASS (mode) == MODE_CC)
+  /* CONST_INT may be in various modes, avoid non-scalar-int mode. */
+  if (!SCALAR_INT_MODE_P (mode))
     return NULL_RTX;
 
-  gcc_assert (SCALAR_INT_MODE_P (mode));
   if (!compute_const_anchors (src_const, &lower_base, &lower_offs,
 			      &upper_base, &upper_offs))
     return NULL_RTX;
