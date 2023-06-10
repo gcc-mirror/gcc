4e14dd9c20db032fb77cc59fa3e5dc9a18ca533f
Julian Brown <julian@codesourcery.com>
2023-06-10 07:53:57 +0000

    OpenACC: "declare create" fixes wrt. "allocatable" variables
    This patch fixes a case revealed by the previous patch where a synthetic
    "acc data" region created for a "declare create" variable could interact
    strangely with lexical inheritance behaviour.  In fact, it doesn't seem
    right to create the "acc data" region for allocatable variables at all
    -- doing so means that a data region is likely to be created for an
    unallocated variable.
    
    The fix is not to add such variables to the synthetic "acc data" region
    at all, and defer to the code that performs "enter data"/"exit data"
    for them when allocated/deallocated on the host instead. Then, "declare
    create" variables are implicitly turned into "present" clauses on in-scope
    offload regions.
    
    2023-06-16  Julian Brown  <julian@codesourcery.com>
    
    gcc/fortran/
    	* trans-openmp.cc (gfc_omp_finish_clause): Handle "declare create" for
    	scalar allocatable variables.
    	(gfc_trans_omp_clauses): Don't include allocatable vars in synthetic
    	"acc data" region created for "declare create" variables.  Mark such
    	variables with the "oacc declare create" attribute instead.  Don't
    	create ALWAYS_POINTER mapping for target-to-host updates of declare
    	create variables.
    	(gfc_trans_oacc_declare): Handle empty clause list.
    
    gcc/
    	* gimplify.cc (gimplify_adjust_omp_clauses_1): Handle "oacc declare
    	create" attribute.
    
    libgomp/
    	* testsuite/libgomp.oacc-fortran/declare-create-1.f90: New test.
    	* testsuite/libgomp.oacc-fortran/declare-create-2.f90: New test.
    	* testsuite/libgomp.oacc-fortran/declare-create-3.f90: New test.

---
+++ libgomp/testsuite/libgomp.oacc-fortran/declare-create-2.f90
@@ -0,0 +1,25 @@
+! { dg-do run }
+
+module m
+integer, allocatable :: mint
+!$acc declare create (mint)
+end module m
+
+program p
+use m
+
+allocate(mint)
+
+mint = 0
+
+!$acc serial
+mint = 5
+!$acc end serial
+
+!$acc update host(mint)
+
+if (mint.ne.5) stop 1
+
+deallocate(mint)
+
+end program p
