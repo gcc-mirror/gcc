be1f186f0f3a45937be7e96bf6d65f372b045be7
Julian Brown <julian@codesourcery.com>
2023-06-15 19:23:10 +0000

    OpenACC: Reimplement "inheritance" for lexically-nested offload regions
    This patch reimplements "lexical inheritance" for OpenACC offload regions
    inside "data" regions, allowing e.g. this to work:
    
      int *ptr;
      [...]
      #pragma acc data copyin(ptr[10:2])
      {
        #pragma acc parallel
        { ... }
      }
    
    here, the "copyin" is mirrored on the inner "acc parallel" as
    "present(ptr[10:2])" -- allowing code within the parallel to use that
    section of the array even though the mapping is implicit.
    
    In terms of implementation, this works by expanding mapping nodes for
    "acc data" to include pointer mappings that might be needed by inner
    offload regions. The resulting mapping group is then copied to the inner
    offload region as needed, rewriting the first node to "force_present".
    The pointer mapping nodes are then removed from the "acc data" later
    during gimplification.
    
    For OpenMP, pointer mapping nodes on equivalent "omp data" regions are
    not needed, so remain suppressed during expansion.
    
    2023-06-16  Julian Brown  <julian@codesourcery.com>
    
    gcc/c-family/
    	* c-omp.cc (c_omp_address_inspector::expand_array_base): Don't omit
    	pointer nodes for OpenACC.
    
    gcc/
    	* gimplify.cc (omp_tsort_mark, omp_mapping_group): Move before
    	gimplify_omp_ctx. Add constructor to omp_mapping_group.
    	(gimplify_omp_ctx): Add DECL_DATA_CLAUSE field.
    	(new_omp_context, delete_omp_context): Initialise and free above field.
    	(omp_gather_mapping_groups_1): Use constructor for omp_mapping_group.
    	(gimplify_scan_omp_clauses): Record mappings that might be lexically
    	inherited.  Don't remove
    	GOMP_MAP_FIRSTPRIVATE_POINTER/GOMP_MAP_FIRSTPRIVATE_REFERENCE yet.
    	(gomp_oacc_needs_data_present): New function.
    	(gimplify_adjust_omp_clauses_1): Implement lexical inheritance
    	behaviour for OpenACC.
    	(gimplify_adjust_omp_clauses): Remove
    	GOMP_MAP_FIRSTPRIVATE_POINTER/GOMP_MAP_FIRSTPRIVATE_REFERENCE here
    	instead, after lexical inheritance is done.
    
    gcc/testsuite/
    	* c-c++-common/goacc/acc-data-chain.c: New test.
    	* gfortran.dg/goacc/pr70828.f90: Likewise.
    	* gfortran.dg/goacc/assumed-size.f90: Likewise.
    
    libgomp/
    	* testsuite/libgomp.oacc-c-c++-common/pr70828.c: New test.
    	* testsuite/libgomp.oacc-c-c++-common/pr70828-2.c: Likewise.
    	* testsuite/libgomp.oacc-fortran/pr70828.f90: Likewise.
    	* testsuite/libgomp.oacc-fortran/pr70828-2.f90: Likewise.
    	* testsuite/libgomp.oacc-fortran/pr70828-3.f90: Likewise.
    	* testsuite/libgomp.oacc-fortran/pr70828-4.f90: Likewise.
    	* testsuite/libgomp.oacc-fortran/pr70828-5.f90: Likewise.
    	* testsuite/libgomp.oacc-fortran/pr70828-6.f90: Likewise.

---
+++ libgomp/testsuite/libgomp.oacc-c-c++-common/pr70828.c
@@ -0,0 +1,27 @@
+/* Subarray declared on enclosing data construct.  */
+
+#include <assert.h>
+
+int
+main ()
+{
+  int a[100], i;
+
+  for (i = 0; i < 100; i++)
+    a[i] = 0;
+
+#pragma acc data copy(a[10:80])
+  {
+    #pragma acc parallel loop
+    for (i = 10; i < 90; i++)
+      a[i] = i;
+  }
+
+  for (i = 0; i < 100; i++)
+    if (i >= 10 && i < 90)
+      assert (a[i] == i);
+    else
+      assert (a[i] == 0);
+
+  return 0;
+}
