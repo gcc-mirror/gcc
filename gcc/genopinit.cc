ccfdda34341423c6034876856cd8ba50f0ca0ef3
Ju-Zhe Zhong <juzhe.zhong@rivai.ai>
2023-06-19 10:07:09 +0200

    VECT: Support LEN_MASK_{LOAD,STORE} ifn && optabs
    This patch adds LEN_MASK_ LOAD/STORE to support flow control for targets
    like RISC-V that uses length in loop control.
    Normalize load/store into LEN_MASK_ LOAD/STORE as long as either length
    or mask is valid. Length is the outcome of SELECT_VL or MIN_EXPR.
    Mask is the outcome of comparison.
    
    LEN_MASK_ LOAD/STORE format is defined as follows:
    1). LEN_MASK_LOAD (ptr, align, length, mask).
    2). LEN_MASK_STORE (ptr, align, length, mask, vec).
    
    Consider these 4 following cases:
    
    VLA: Variable-length auto-vectorization
    VLS: Specific-length auto-vectorization
    
    Case 1 (VLS): -mrvv-vector-bits=128   IR (Does not use LEN_MASK_*):
    Code:					v1 = MEM (...)
      for (int i = 0; i < 4; i++)           v2 = MEM (...)
        a[i] = b[i] + c[i];                 v3 = v1 + v2
                                            MEM[...] = v3
    
    Case 2 (VLS): -mrvv-vector-bits=128   IR (LEN_MASK_* with length = VF, mask = comparison):
    Code:                                   mask = comparison
      for (int i = 0; i < 4; i++)           v1 = LEN_MASK_LOAD (length = VF, mask)
        if (cond[i])                        v2 = LEN_MASK_LOAD (length = VF, mask)
          a[i] = b[i] + c[i];               v3 = v1 + v2
                                            LEN_MASK_STORE (length = VF, mask, v3)
    
    Case 3 (VLA):
    Code:                                   loop_len = SELECT_VL or MIN
      for (int i = 0; i < n; i++)           v1 = LEN_MASK_LOAD (length = loop_len, mask = {-1,-1,...})
          a[i] = b[i] + c[i];               v2 = LEN_MASK_LOAD (length = loop_len, mask = {-1,-1,...})
                                            v3 = v1 + v2
                                            LEN_MASK_STORE (length = loop_len, mask = {-1,-1,...}, v3)
    
    Case 4 (VLA):
    Code:                                   loop_len = SELECT_VL or MIN
      for (int i = 0; i < n; i++)           mask = comparison
          if (cond[i])                      v1 = LEN_MASK_LOAD (length = loop_len, mask)
          a[i] = b[i] + c[i];               v2 = LEN_MASK_LOAD (length = loop_len, mask)
                                            v3 = v1 + v2
                                            LEN_MASK_STORE (length = loop_len, mask, v3)
    
    Co-authored-by: Robin Dapp <rdapp.gcc@gmail.com>
    
    gcc/ChangeLog:
    
    	* doc/md.texi: Add len_mask{load,store}.
    	* genopinit.cc (main): Ditto.
    	(CMP_NAME): Ditto.
    	* internal-fn.cc (len_maskload_direct): Ditto.
    	(len_maskstore_direct): Ditto.
    	(expand_call_mem_ref): Ditto.
    	(expand_partial_load_optab_fn): Ditto.
    	(expand_len_maskload_optab_fn): Ditto.
    	(expand_partial_store_optab_fn): Ditto.
    	(expand_len_maskstore_optab_fn): Ditto.
    	(direct_len_maskload_optab_supported_p): Ditto.
    	(direct_len_maskstore_optab_supported_p): Ditto.
    	* internal-fn.def (LEN_MASK_LOAD): Ditto.
    	(LEN_MASK_STORE): Ditto.
    	* optabs.def (OPTAB_CD): Ditto.

--- gcc/genopinit.cc
+++ gcc/genopinit.cc
@@ -376,7 +376,8 @@ main (int argc, const char **argv)
 
   fprintf (s_file,
 	   "/* Returns TRUE if the target supports any of the partial vector\n"
-	   "   optabs: while_ult_optab, len_load_optab or len_store_optab,\n"
+	   "   optabs: while_ult_optab, len_load_optab, len_store_optab,\n"
+	   "   len_maskload_optab or len_maskstore_optab,\n"
 	   "   for any mode.  */\n"
 	   "bool\npartial_vectors_supported_p (void)\n{\n");
   bool any_match = false;
@@ -386,7 +387,8 @@ main (int argc, const char **argv)
     {
 #define CMP_NAME(N) !strncmp (p->name, (N), strlen ((N)))
       if (CMP_NAME("while_ult") || CMP_NAME ("len_load")
-	  || CMP_NAME ("len_store"))
+	  || CMP_NAME ("len_store")|| CMP_NAME ("len_maskload")
+	  || CMP_NAME ("len_maskstore"))
 	{
 	  if (first)
 	    fprintf (s_file, " HAVE_%s", p->name);
