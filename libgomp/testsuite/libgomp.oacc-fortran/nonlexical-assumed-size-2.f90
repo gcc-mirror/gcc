5813266ee2fc9f9f9e90e44f72bde348411318e4
Julian Brown <julian@codesourcery.com>
2023-06-19 09:10:14 +0000

    OpenACC: Allow implicit uses of assumed-size arrays in offload regions
    This patch reimplements the functionality of the previously-reverted
    patch "Assumed-size arrays with non-lexical data mappings". The purpose
    is to support implicit uses of assumed-size arrays for Fortran when those
    arrays have already been mapped on the target some other way (e.g. by
    "acc enter data").
    
    This relates to upstream OpenACC issue 489 (not yet resolved).
    
    2023-06-16  Julian Brown  <julian@codesourcery.com>
    
    gcc/fortran/
    	* trans-openmp.cc (gfc_omp_finish_clause): Treat implicitly-mapped
    	assumed-size arrays as zero-sized for OpenACC, rather than an error.
    
    gcc/testsuite/
    	* gfortran.dg/goacc/assumed-size.f90: Don't expect error.
    
    libgomp/
    	* testsuite/libgomp.oacc-fortran/nonlexical-assumed-size-1.f90: New
    	test.
    	* testsuite/libgomp.oacc-fortran/nonlexical-assumed-size-2.f90: New
    	test.

---
+++ libgomp/testsuite/libgomp.oacc-fortran/nonlexical-assumed-size-2.f90
@@ -0,0 +1,40 @@
+! { dg-do run }
+
+program p
+implicit none
+integer :: myarr(10)
+
+myarr = 0
+
+call subr(myarr)
+
+if (myarr(5).ne.5) stop 1
+
+contains
+
+subroutine subr(arr)
+implicit none
+integer :: arr(*)
+
+! At first glance, it might not be obvious how this works.  The "enter data"
+! and "exit data" operations expand to a pair of mapping nodes for OpenACC,
+! GOMP_MAP_{TO/FROM} and GOMP_MAP_POINTER.  The former maps the array data,
+! and the latter creates a separate mapping on the target for the pointer
+! itself with a bias so it represents the "zeroth" element.
+
+!$acc enter data copyin(arr(2:8))
+
+! ...then this implicit mapping creates a zero-length array section
+! (GOMP_MAP_ZERO_LEN_ARRAY_SECTION) followed by another GOMP_MAP_POINTER for
+! 'arr'.  But now that pointer is already "present" on the target, so is not
+! overwritten.
+
+!$acc serial
+! This access is then done via the on-target pointer.
+arr(5) = 5
+!$acc end serial
+
+!$acc exit data copyout(arr(2:8))
+
+end subroutine subr
+end program p
