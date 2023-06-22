2183235d6eb00f9e6d6cad341c3084f34de5b1a3
Paul E. Murphy <murphyp@linux.ibm.com>
2023-06-22 17:53:46 -0500

    go: Update usage of TARGET_AIX to TARGET_AIX_OS
    TARGET_AIX is defined to a non-zero value on linux and maybe other
    powerpc64le targets.  This leads to unexpected behavior such as
    dropping the .go_export section when linking a shared library
    on linux/powerpc64le.
    
    Instead, use TARGET_AIX_OS to toggle AIX specific behavior.
    
    Fixes golang/go#60798.
    
    2023-06-22  Paul E. Murphy  <murphyp@linux.ibm.com>
    
    gcc/go/
    	* go-backend.cc [TARGET_AIX]: Rename and update usage to TARGET_AIX_OS.
    	* go-lang.cc: Likewise.
    
    (cherry picked from commit b76cd1ec361712e1ac9ca5e0246da24ea2b78916)

--- gcc/go/go-lang.cc
+++ gcc/go/go-lang.cc
@@ -39,8 +39,8 @@ along with GCC; see the file COPYING3.  If not see
 #include "go-c.h"
 #include "go-gcc.h"
 
-#ifndef TARGET_AIX
-#define TARGET_AIX 0
+#ifndef TARGET_AIX_OS
+#define TARGET_AIX_OS 0
 #endif
 
 /* Language-dependent contents of a type.  */
@@ -116,9 +116,9 @@ go_langhook_init (void)
   args.compiling_runtime = go_compiling_runtime;
   args.debug_escape_level = go_debug_escape_level;
   args.debug_escape_hash = go_debug_escape_hash;
-  args.nil_check_size_threshold = TARGET_AIX ? -1 : 4096;
+  args.nil_check_size_threshold = TARGET_AIX_OS ? -1 : 4096;
   args.debug_optimization = go_debug_optimization;
-  args.need_eqtype = TARGET_AIX ? true : false;
+  args.need_eqtype = TARGET_AIX_OS ? true : false;
   args.linemap = go_get_linemap();
   args.backend = go_get_backend();
   go_create_gogo (&args);
