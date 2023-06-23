7515e1f72c832cf7c527f621465b69307afaf1d0
Arthur Cohen <arthur.cohen@embecosm.com>
2023-06-23 16:17:51 +0200

    name resolution 2.0: Add base for our context data structure
    This commit adds a first simple `NameResolutionContext` data structure, which will
    be used by all passes of the name resolution to store definitions and access
    them. Namely, it contains a few instances of `ForeverStack`, for each
    namespace we will be using.
    
    gcc/rust/ChangeLog:
    
    	* Make-lang.in: Add new object file.
    	* resolve/rust-name-resolution-context.cc: New file.
    	* resolve/rust-name-resolution-context.h: New file.

---
+++ gcc/rust/resolve/rust-name-resolution-context.cc
@@ -0,0 +1,93 @@
+// Copyright (C) 2020-2023 Free Software Foundation, Inc.
+
+// This file is part of GCC.
+
+// GCC is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3, or (at your option) any later
+// version.
+
+// GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with GCC; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include "rust-name-resolution-context.h"
+
+namespace Rust {
+namespace Resolver2_0 {
+
+tl::expected<NodeId, DuplicateNameError>
+NameResolutionContext::insert (Identifier name, NodeId id, Namespace ns)
+{
+  switch (ns)
+    {
+    case Namespace::Values:
+      return values.insert (name, id);
+    case Namespace::Types:
+      return types.insert (name, id);
+    case Namespace::Labels:
+    case Namespace::Macros:
+    default:
+      rust_unreachable ();
+    }
+}
+
+void
+NameResolutionContext::scoped (Rib rib, NodeId id,
+			       std::function<void (void)> lambda,
+			       tl::optional<Identifier> path)
+{
+  values.push (rib, id, path);
+  types.push (rib, id, path);
+  macros.push (rib, id, path);
+  // labels.push (rib, id);
+
+  lambda ();
+
+  values.pop ();
+  types.pop ();
+  macros.pop ();
+  // labels.pop (rib);
+}
+
+void
+NameResolutionContext::scoped (Rib rib, Namespace ns, NodeId scope_id,
+			       std::function<void (void)> lambda,
+			       tl::optional<Identifier> path)
+{
+  switch (ns)
+    {
+    case Namespace::Values:
+      values.push (rib, scope_id, path);
+      break;
+    case Namespace::Types:
+      types.push (rib, scope_id, path);
+      break;
+    case Namespace::Labels:
+    case Namespace::Macros:
+      gcc_unreachable ();
+    }
+
+  lambda ();
+
+  switch (ns)
+    {
+    case Namespace::Values:
+      values.pop ();
+      break;
+    case Namespace::Types:
+      types.pop ();
+      break;
+    case Namespace::Labels:
+    case Namespace::Macros:
+      gcc_unreachable ();
+    }
+}
+
+} // namespace Resolver2_0
+} // namespace Rust
