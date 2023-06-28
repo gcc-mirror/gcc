237e83e2158a3d9b875f8775805d04d97e8b36c1
Richard Biener <rguenther@suse.de>
2023-06-28 13:36:59 +0200

    tree-optimization/110451 - hoist invariant compare after interchange
    The following adjusts the cost model of invariant motion to consider
    [VEC_]COND_EXPRs and comparisons producing a data value as expensive.
    For 503.bwaves_r this avoids an unnecessarily high vectorization
    factor because of an integer comparison besides data operations on
    double.
    
    	PR tree-optimization/110451
    	* tree-ssa-loop-im.cc (stmt_cost): [VEC_]COND_EXPR and
    	tcc_comparison are expensive.
    
    	* gfortran.dg/vect/pr110451.f: New testcase.

--- gcc/tree-ssa-loop-im.cc
+++ gcc/tree-ssa-loop-im.cc
@@ -617,7 +617,8 @@ stmt_cost (gimple *stmt)
   if (gimple_code (stmt) != GIMPLE_ASSIGN)
     return 1;
 
-  switch (gimple_assign_rhs_code (stmt))
+  enum tree_code code = gimple_assign_rhs_code (stmt);
+  switch (code)
     {
     case MULT_EXPR:
     case WIDEN_MULT_EXPR:
@@ -645,6 +646,11 @@ stmt_cost (gimple *stmt)
       /* Shifts and rotates are usually expensive.  */
       return LIM_EXPENSIVE;
 
+    case COND_EXPR:
+    case VEC_COND_EXPR:
+      /* Conditionals are expensive.  */
+      return LIM_EXPENSIVE;
+
     case CONSTRUCTOR:
       /* Make vector construction cost proportional to the number
          of elements.  */
@@ -658,6 +664,9 @@ stmt_cost (gimple *stmt)
       return 0;
 
     default:
+      /* Comparisons are usually expensive.  */
+      if (TREE_CODE_CLASS (code) == tcc_comparison)
+	return LIM_EXPENSIVE;
       return 1;
     }
 }
