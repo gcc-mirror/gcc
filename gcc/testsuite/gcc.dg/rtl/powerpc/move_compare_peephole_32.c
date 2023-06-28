1554ab958afd4f4a94f776f3a7a03b40918bf424
Haochen Gui <guihaoc@gcc.gnu.org>
2023-06-28 16:45:50 +0800

    rs6000: Add two peephole patterns for "mr." insn
    When investigating the issue mentioned in PR87871#c30 - if compare
    and move pattern benefits before RA, I checked the assembly generated
    for SPEC2017 and found that certain insn sequences aren't converted to
    "mr." instructions.
    Following two sequence are never to be combined to "mr." pattern as
    there is no register link between them. This patch adds two peephole2
    patterns to convert them to "mr." instructions.
    
    cmp 0,3,0
    mr 4,3
    
    mr 4,3
    cmp 0,3,0
    
    The patch also creates a new mode iterator which decided by
    TARGET_POWERPC64.  This mode iterator is used in "mr." and its split
    pattern.  The original P iterator is improper when -m32/-mpowerpc64 is
    set.  In this situation, the "mr." should compares the whole 64-bit
    register with 0 other than the low 32-bit one.
    
    gcc/
    	* config/rs6000/rs6000.md (peephole2 for compare_and_move): New.
    	(peephole2 for move_and_compare): New.
    	(mode_iterator WORD): New.  Set the mode to SI/DImode by
    	TARGET_POWERPC64.
    	(*mov<mode>_internal2): Change the mode iterator from P to WORD.
    	(split pattern for compare_and_move): Likewise.
    
    gcc/testsuite/
    	* gcc.dg/rtl/powerpc/move_compare_peephole_32.c: New.
    	* gcc.dg/rtl/powerpc/move_compare_peephole_64.c: New.

---
+++ gcc/testsuite/gcc.dg/rtl/powerpc/move_compare_peephole_32.c
@@ -0,0 +1,60 @@
+/* { dg-do compile { target powerpc*-*-linux* } } */
+/* { dg-skip-if "" { has_arch_ppc64 } } */
+/* { dg-options "-O2 -mregnames" } */
+
+/* Following instruction sequence is found in assembly of
+   Perl_block_start, which is a function of op.c in SPEC2017
+   perlbench.  It can be never combined to a move and compare
+   instruction in combine pass.  A peephole pattern is needed to
+   converted the sequence to a "mr." instruction.
+
+	cmpdi 0,9,0
+	mr 12,9
+
+   This test case is an analogue of the source code and verifies
+   if the peephole2 patterns work.
+*/
+
+int __RTL (startwith ("peephole2")) compare_move_peephole ()
+{
+(function "compare_move_peephole"
+  (insn-chain
+    (block 2
+      (edge-from entry (flags "FALLTHRU"))
+      (cnote 3 [bb 2] NOTE_INSN_BASIC_BLOCK)
+      (cinsn 8 (set (reg:CC %cr0)
+                    (compare:CC (reg:SI %r3)
+                        (const_int 0))))
+      (cinsn 2 (set (reg:SI %r4)
+                    (reg:SI %r3)))
+      ;; Extra insn to avoid the above being deleted by DCE.
+      (cinsn 18 (use (reg:SI %r4)))
+      (cinsn 19 (use (reg:CC %cr0)))
+      (edge-to exit (flags "FALLTHRU"))
+    ) ;; block 2
+  ) ;; insn-chain
+) ;; function "main"
+}
+
+int __RTL (startwith ("peephole2")) move_compare_peephole ()
+{
+(function "move_compare_peephole"
+  (insn-chain
+    (block 2
+      (edge-from entry (flags "FALLTHRU"))
+      (cnote 3 [bb 2] NOTE_INSN_BASIC_BLOCK)
+      (cinsn 2 (set (reg:SI %r4)
+                    (reg:SI %r3)))
+      (cinsn 8 (set (reg:CC %cr0)
+                    (compare:CC (reg:SI %r3)
+                        (const_int 0))))
+      ;; Extra insn to avoid the above being deleted by DCE.
+      (cinsn 18 (use (reg:SI %r4)))
+      (cinsn 19 (use (reg:CC %cr0)))
+      (edge-to exit (flags "FALLTHRU"))
+    ) ;; block 2
+  ) ;; insn-chain
+) ;; function "main"
+}
+
+/* { dg-final { scan-assembler-times {\mmr\.} 2 } } */
