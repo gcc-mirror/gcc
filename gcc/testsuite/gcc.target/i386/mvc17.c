c5703272e9e3d7dce0c45c52f1922daa77e436e8
Hongyu Wang <hongyu.wang@intel.com>
2023-06-28 09:13:29 +0800

    i386: Fix mvc17.c test for default target clone under --with-arch
    For target clones, the default clone follows the default march
    so adjust the testcase to avoid test failure on --with-arch=native
    build.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/i386/mvc17.c: Add -march=x86-64 to dg-options.

--- gcc/testsuite/gcc.target/i386/mvc17.c
+++ gcc/testsuite/gcc.target/i386/mvc17.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 /* { dg-require-ifunc "" } */
-/* { dg-options "-O2" } */
+/* { dg-options "-O2 -march=x86-64" } */
 /* { dg-final { scan-assembler-times "rep mov" 1 } } */
 
 __attribute__((target_clones("default","arch=icelake-server")))
