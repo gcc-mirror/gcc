5a13caf2666bdf586272cc24a08ab90499771c95
Christoph Müllner <christoph.muellner@vrull.eu>
2023-07-06 08:16:39 +0200

    riscv: thead: Fix failing XTheadCondMov tests (indirect-rv[32|64])
    Recently, two identical XTheadCondMov tests have been added, which both fail.
    Let's fix that by changing the following:
    * Merge both files into one (no need for separate tests for rv32 and rv64)
    * Drop unrelated attribute check test (we already test for `th.mveqz`
      and `th.mvnez` instructions, so there is little additional value)
    * Fix the pattern to allow matching
    
    Fixes: a1806f0918c0 ("RISC-V: Optimize TARGET_XTHEADCONDMOV")
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/xtheadcondmov-indirect-rv32.c: Moved to...
    	* gcc.target/riscv/xtheadcondmov-indirect.c: ...here.
    	* gcc.target/riscv/xtheadcondmov-indirect-rv64.c: Removed.
    
    Signed-off-by: Christoph Müllner <christoph.muellner@vrull.eu>

---
+++ gcc/testsuite/gcc.target/riscv/xtheadcondmov-indirect.c
@@ -0,0 +1,118 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv32gc_xtheadcondmov" { target { rv32 } } } */
+/* { dg-options "-march=rv64gc_xtheadcondmov" { target { rv64 } } } */
+/* { dg-skip-if "" { *-*-* } {"-O0" "-Os" "-Og" "-Oz" "-flto" } } */
+/* { dg-final { check-function-bodies "**" "" } } */
+
+/*
+** ConEmv_imm_imm_reg:
+**	addi	a[0-9]+,a[0-9]+,-1000
+**	li	a[0-9]+,10
+**	th\.mvnez	a[0-9]+,a[0-9]+,a[0-9]+
+**	ret
+*/
+int ConEmv_imm_imm_reg(int x, int y)
+{
+  if (x == 1000)
+    return 10;
+  return y;
+}
+
+/*
+** ConEmv_imm_reg_reg:
+**	addi	a[0-9]+,a[0-9]+,-1000
+**	th.mveqz	a[0-9]+,a[0-9]+,a[0-9]+
+**	mv	a[0-9]+,a[0-9]+
+**	ret
+*/
+int ConEmv_imm_reg_reg(int x, int y, int z)
+{
+  if (x == 1000)
+    return y;
+  return z;
+}
+
+/*
+** ConEmv_reg_imm_reg:
+**	sub	a[0-9]+,a[0-9]+,a[0-9]+
+**	li	a[0-9]+,10
+**	th.mvnez	a[0-9]+,a[0-9]+,a[0-9]+
+**	ret
+*/
+int ConEmv_reg_imm_reg(int x, int y, int z)
+{
+  if (x == y)
+    return 10;
+  return z;
+}
+
+/*
+** ConEmv_reg_reg_reg:
+**	sub	a[0-9]+,a[0-9]+,a[0-9]+
+**	th.mveqz	a[0-9]+,a[0-9]+,a[0-9]+
+**	mv	a[0-9]+,a[0-9]+
+**	ret
+*/
+int ConEmv_reg_reg_reg(int x, int y, int z, int n)
+{
+  if (x == y)
+    return z;
+  return n;
+}
+
+/*
+** ConNmv_imm_imm_reg:
+**	addi	a[0-9]+,a[0-9]+,-1000+
+**	li	a[0-9]+,9998336+
+**	addi	a[0-9]+,a[0-9]+,1664+
+**	th.mveqz	a[0-9]+,a[0-9]+,a[0-9]+
+**	ret
+*/
+int ConNmv_imm_imm_reg(int x, int y)
+{
+  if (x != 1000)
+    return 10000000;
+  return y;
+}
+
+/*
+**ConNmv_imm_reg_reg:
+**	addi	a[0-9]+,a[0-9]+,-1000+
+**	th.mvnez	a[0-9]+,a[0-9]+,a[0-9]+
+**	mv	a[0-9]+,a[0-9]+
+**	ret
+*/
+int ConNmv_imm_reg_reg(int x, int y, int z)
+{
+  if (x != 1000)
+    return y;
+  return z;
+}
+
+/*
+**ConNmv_reg_imm_reg:
+**	sub	a[0-9]+,a[0-9]+,a[0-9]+
+**	li	a[0-9]+,10+
+**	th.mveqz	a[0-9]+,a[0-9]+,a[0-9]+
+**	ret
+*/
+int ConNmv_reg_imm_reg(int x, int y, int z)
+{
+  if (x != y)
+    return 10;
+  return z;
+}
+
+/*
+**ConNmv_reg_reg_reg:
+**	sub	a[0-9]+,a[0-9]+,a[0-9]+
+**	th.mvnez	a[0-9]+,a[0-9]+,a[0-9]+
+**	mv	a[0-9]+,a[0-9]+
+**	ret
+*/
+int ConNmv_reg_reg_reg(int x, int y, int z, int n)
+{
+  if (x != y)
+    return z;
+  return n;
+}
