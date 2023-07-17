3228e5c078ed2b505e4ad238b09c1817b38f9cfb
Richard Biener <rguenther@suse.de>
2023-07-17 09:20:33 +0200

    tree-optimization/110669 - bogus matching of loop bitop
    The matching code lacked a check that we end up with a PHI node
    in the loop header.  This caused us to match a random PHI argument
    now catched by the extra PHI_ARG_DEF_FROM_EDGE checking.
    
    	PR tree-optimization/110669
    	* tree-scalar-evolution.cc (analyze_and_compute_bitop_with_inv_effect):
    	Check we matched a header PHI.
    
    	* gcc.dg/torture/pr110669.c: New testcase.

---
+++ gcc/testsuite/gcc.dg/torture/pr110669.c
@@ -0,0 +1,15 @@
+/* { dg-do compile } */
+
+int g_29, func_47_p_48, func_47_p_51_l_129;
+void func_47_p_51()
+{
+  for (;;)
+    {
+      func_47_p_51_l_129 = 0;
+      for (; func_47_p_51_l_129 <= 1; func_47_p_51_l_129 += 1)
+	{
+	  short *l_160 = (short *)(__UINTPTR_TYPE__)(func_47_p_48 || *l_160);
+	  *l_160 &= g_29;
+	}
+    }
+}
