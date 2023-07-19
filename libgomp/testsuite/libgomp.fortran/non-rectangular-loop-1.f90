85da0b40538fb0d17d89de1e7905984668e3dfef
Tobias Burnus <tobias@codesourcery.com>
2023-07-19 10:18:49 +0200

    OpenMP/Fortran: Non-rectangular loops with constant steps other than 1 or -1 [PR107424]
    Before this commit, gfortran produced with OpenMP for 'do i = 1,10,2'
    the code
      for (count.0 = 0; count.0 < 5; count.0 = count.0 + 1)
        i = count.0 * 2 + 1;
    
    While such an inner loop can be collapsed, a non-rectangular could not.
    With this commit and for all constant loop steps, a simple loop such
    as 'for (i = 1; i <= 10; i = i + 2)' is created. (Before only for the
    constant steps of 1 and -1.)
    
    The constant step permits to know the direction (increasing/decreasing)
    that is required for the loop condition.
    
    The new code is only valid if one assumes no overflow of the loop variable.
    However, the Fortran standard can be read that this must be ensured by
    the user. Namely, the Fortran standard requires (F2023, 10.1.5.2.4):
    "The execution of any numeric operation whose result is not defined by
    the arithmetic used by the processor is prohibited."
    
    And, for DO loops, F2023's "11.1.7.4.3 The execution cycle" has the
    following: The number of loop iterations handled by an iteration count,
    which would permit code like 'do i = huge(i)-5, huge(i),4'. However,
    in step (3), this count is not only decremented by one but also:
      "... The DO variable, if any, is incremented by the value of the
      incrementation parameter m3."
    And for the example above, 'i' would be 'huge(i)+3' in the last
    execution cycle, which exceeds the largest model number and should
    render the example as invalid.
    
    	PR fortran/107424
    
    gcc/fortran/ChangeLog:
    
    	* trans-openmp.cc (gfc_nonrect_loop_expr): Accept all
    	constant loop steps.
    	(gfc_trans_omp_do): Likewise; use sign to determine
    	loop direction.
    
    libgomp/ChangeLog:
    
    	* libgomp.texi (Impl. Status 5.0): Add link to new PR110735.
    	* testsuite/libgomp.fortran/non-rectangular-loop-1.f90: Enable
    	commented tests.
    	* testsuite/libgomp.fortran/non-rectangular-loop-1a.f90: Remove
    	test file; tests are in non-rectangular-loop-1.f90.
    	* testsuite/libgomp.fortran/non-rectangular-loop-5.f90: Change
    	testcase to use a non-constant step to retain the 'sorry' test.
    	* testsuite/libgomp.fortran/non-rectangular-loop-6.f90: New test.
    
    gcc/testsuite/ChangeLog:
    
    	* gfortran.dg/gomp/linear-2.f90: Update dump to remove
    	the additional count variable.

--- libgomp/testsuite/libgomp.fortran/non-rectangular-loop-1.f90
+++ libgomp/testsuite/libgomp.fortran/non-rectangular-loop-1.f90
@@ -6,9 +6,6 @@
 
 ! Nonrectangular loop nests checks
 
-! See PR or non-rectangular-loop-1a.f90 for the commented tests
-! Hint: Those use step for loop vars part of nonrectangular loop nests
-
 module m
   implicit none (type, external)
 contains
@@ -26,32 +23,32 @@ subroutine lastprivate_check_simd_1
   ! Use 'i' or 'j', unit step on 'i' or on 'j' -> 4 loops
   ! Then same, except use non-unit step for 'k'
 
-!  !$omp simd collapse(3) lastprivate(k)
-!  do i = 1, n
-!    do j = 1, m, 2
-!      do k = j - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
+  !$omp simd collapse(3) lastprivate(k)
+  do i = 1, n
+    do j = 1, m, 2
+      do k = j - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
 
-!  !$omp simd collapse(3) lastprivate(k)
-!  do i = 1, n, 2
-!    do j = 1, m
-!      do k = i - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
+  !$omp simd collapse(3) lastprivate(k)
+  do i = 1, n, 2
+    do j = 1, m
+      do k = i - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
 
   !$omp simd collapse(3) lastprivate(k)
   do i = 1, n, 2
     do j = 1, m
       do k = j - 41, p
         if (k < 1 - 41 .or. k > p) then
-          print *, i, j, k,p, " -> i, j, k, p   (k < 1 - 41 .or. k > p)"
+          ! print *, i, j, k,p, " -> i, j, k, p   (k < 1 - 41 .or. k > p)"
           error stop
         end if
       end do
@@ -66,7 +63,7 @@ subroutine lastprivate_check_simd_1
     do j = 1, m
       do k = j - 41, p
         if (k < 1 - 41 .or. k > p) then
-          print *, i, j, k,p, " -> i, j, k, p   (k < 1 - 41 .or. k > p)"
+          ! print *, i, j, k,p, " -> i, j, k, p   (k < 1 - 41 .or. k > p)"
           error stop
         end if
       end do
@@ -102,25 +99,25 @@ subroutine lastprivate_check_simd_1
 
   ! Same but 'private' for all (i,j) vars
 
-!  !$omp simd collapse(3) lastprivate(k) private(i,j)
-!  do i = 1, n
-!    do j = 1, m, 2
-!      do k = j - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
-!
-!  !$omp simd collapse(3) lastprivate(k) private(i,j)
-!  do i = 1, n, 2
-!    do j = 1, m
-!      do k = i - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
+  !$omp simd collapse(3) lastprivate(k) private(i,j)
+  do i = 1, n
+    do j = 1, m, 2
+      do k = j - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
+
+  !$omp simd collapse(3) lastprivate(k) private(i,j)
+  do i = 1, n, 2
+    do j = 1, m
+      do k = i - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
 
   !$omp simd collapse(3) lastprivate(k) private(i,j)
   do i = 1, n, 2
@@ -144,27 +141,27 @@ subroutine lastprivate_check_simd_1
 
   ! Same - but with lastprivate(i,j)
 
-!  !$omp simd collapse(3) lastprivate(k) lastprivate(i,j)
-!  do i = 1, n
-!    do j = 1, m, 2
-!      do k = j - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
-!  if (i /= n + 1 .or. j /= m + 2) error stop
+  !$omp simd collapse(3) lastprivate(k) lastprivate(i,j)
+  do i = 1, n
+    do j = 1, m, 2
+      do k = j - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
+  if (i /= n + 1 .or. j /= m + 2) error stop
 
-!  !$omp simd collapse(3) lastprivate(k) lastprivate(i,j)
-!  do i = 1, n, 2
-!    do j = 1, m
-!      do k = i - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
-!  if (i /= n + 2 .or. j /= m + 1) error stop
+  !$omp simd collapse(3) lastprivate(k) lastprivate(i,j)
+  do i = 1, n, 2
+    do j = 1, m
+      do k = i - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
+  if (i /= n + 2 .or. j /= m + 1) error stop
 
   !$omp simd collapse(3) lastprivate(k) lastprivate(i,j)
   do i = 1, n, 2
@@ -201,25 +198,25 @@ subroutine lastprivate_check_do_simd_1
   ! Use 'i' or 'j', unit step on 'i' or on 'j' -> 4 loops
   ! Then same, except use non-unit step for 'k'
 
-!  !$omp parallel do simd collapse(3) lastprivate(k)
-!  do i = 1, n
-!    do j = 1, m, 2
-!      do k = j - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
+  !$omp parallel do simd collapse(3) lastprivate(k)
+  do i = 1, n
+    do j = 1, m, 2
+      do k = j - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
 
-!  !$omp parallel do simd collapse(3) lastprivate(k)
-!  do i = 1, n, 2
-!    do j = 1, m
-!      do k = i - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
+  !$omp parallel do simd collapse(3) lastprivate(k)
+  do i = 1, n, 2
+    do j = 1, m
+      do k = i - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
 
   !$omp parallel do simd collapse(3) lastprivate(k)
   do i = 1, n, 2
@@ -243,25 +240,25 @@ subroutine lastprivate_check_do_simd_1
 
   ! Same but 'private' for all (i,j) vars
 
-!  !$omp parallel do simd collapse(3) lastprivate(k) private(i,j)
-!  do i = 1, n
-!    do j = 1, m, 2
-!      do k = j - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
+  !$omp parallel do simd collapse(3) lastprivate(k) private(i,j)
+  do i = 1, n
+    do j = 1, m, 2
+      do k = j - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
 
-!  !$omp parallel do simd collapse(3) lastprivate(k) private(i,j)
-!  do i = 1, n, 2
-!    do j = 1, m
-!      do k = i - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
+  !$omp parallel do simd collapse(3) lastprivate(k) private(i,j)
+  do i = 1, n, 2
+    do j = 1, m
+      do k = i - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
 
   !$omp parallel do simd collapse(3) lastprivate(k) private(i,j)
   do i = 1, n, 2
@@ -285,27 +282,27 @@ subroutine lastprivate_check_do_simd_1
 
   ! Same - but with lastprivate(i,j)
 
-!  !$omp parallel do simd collapse(3) lastprivate(k) lastprivate(i,j)
-!  do i = 1, n
-!    do j = 1, m, 2
-!      do k = j - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
-!  if (i /= n + 1 .or. j /= m + 2) error stop
+  !$omp parallel do simd collapse(3) lastprivate(k) lastprivate(i,j)
+  do i = 1, n
+    do j = 1, m, 2
+      do k = j - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
+  if (i /= n + 1 .or. j /= m + 2) error stop
 
-!  !$omp parallel do simd collapse(3) lastprivate(k) lastprivate(i,j)
-!  do i = 1, n, 2
-!    do j = 1, m
-!      do k = i - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
-!  if (i /= n + 2 .or. j /= m + 1) error stop
+  !$omp parallel do simd collapse(3) lastprivate(k) lastprivate(i,j)
+  do i = 1, n, 2
+    do j = 1, m
+      do k = i - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
+  if (i /= n + 2 .or. j /= m + 1) error stop
 
   !$omp parallel do simd collapse(3) lastprivate(k) lastprivate(i,j)
   do i = 1, n, 2
@@ -343,25 +340,25 @@ subroutine lastprivate_check_do_1
   ! Use 'i' or 'j', unit step on 'i' or on 'j' -> 4 loops
   ! Then same, except use non-unit step for 'k'
 
-!  !$omp parallel do collapse(3) lastprivate(k)
-!  do i = 1, n
-!    do j = 1, m, 2
-!      do k = j - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
+  !$omp parallel do collapse(3) lastprivate(k)
+  do i = 1, n
+    do j = 1, m, 2
+      do k = j - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
 
-!  !$omp parallel do collapse(3) lastprivate(k)
-!  do i = 1, n, 2
-!    do j = 1, m
-!      do k = i - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
+  !$omp parallel do collapse(3) lastprivate(k)
+  do i = 1, n, 2
+    do j = 1, m
+      do k = i - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
 
   !$omp parallel do collapse(3) lastprivate(k)
   do i = 1, n, 2
@@ -385,25 +382,25 @@ subroutine lastprivate_check_do_1
 
   ! Same but 'private' for all (i,j) vars
 
-!  !$omp parallel do collapse(3) lastprivate(k) private(i,j)
-!  do i = 1, n
-!    do j = 1, m, 2
-!      do k = j - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
+  !$omp parallel do collapse(3) lastprivate(k) private(i,j)
+  do i = 1, n
+    do j = 1, m, 2
+      do k = j - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
 
-!  !$omp parallel do collapse(3) lastprivate(k) private(i,j)
-!  do i = 1, n, 2
-!    do j = 1, m
-!      do k = i - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
+  !$omp parallel do collapse(3) lastprivate(k) private(i,j)
+  do i = 1, n, 2
+    do j = 1, m
+      do k = i - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
 
   !$omp parallel do collapse(3) lastprivate(k) private(i,j)
   do i = 1, n, 2
@@ -427,27 +424,27 @@ subroutine lastprivate_check_do_1
 
   ! Same - but with lastprivate(i,j)
 
-!  !$omp parallel do collapse(3) lastprivate(k) lastprivate(i,j)
-!  do i = 1, n
-!    do j = 1, m, 2
-!      do k = j - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
-!  if (i /= n + 1 .or. j /= m + 2) error stop
+  !$omp parallel do collapse(3) lastprivate(k) lastprivate(i,j)
+  do i = 1, n
+    do j = 1, m, 2
+     do k = j - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
+  if (i /= n + 1 .or. j /= m + 2) error stop
 
-!  !$omp parallel do collapse(3) lastprivate(k) lastprivate(i,j)
-!  do i = 1, n, 2
-!    do j = 1, m
-!      do k = i - 41, p
-!        if (k < 1 - 41 .or. k > p) error stop
-!      end do
-!    end do
-!  end do
-!  if (k /= p + 1) error stop
-!  if (i /= n + 2 .or. j /= m + 1) error stop
+  !$omp parallel do collapse(3) lastprivate(k) lastprivate(i,j)
+  do i = 1, n, 2
+    do j = 1, m
+      do k = i - 41, p
+        if (k < 1 - 41 .or. k > p) error stop
+      end do
+    end do
+  end do
+  if (k /= p + 1) error stop
+  if (i /= n + 2 .or. j /= m + 1) error stop
 
   !$omp parallel do collapse(3) lastprivate(k) lastprivate(i,j)
   do i = 1, n, 2
@@ -481,42 +478,42 @@ subroutine lastprivate_check_2
   m = 23
   p = 27
 
-!  !$omp parallel do simd collapse(3) lastprivate(p)
-!  do i = 1, n
-!    do j = 1, m,2
-!      do k = 1, j + 41
-!        do ll = 1, p, 2
-!          if (k > 23 + 41 .or. k < 1) error stop
-!        end do
-!      end do
-!    end do
-!  end do
-!  if (ll /= 29) error stop
+  !$omp parallel do simd collapse(3) lastprivate(ll)
+  do i = 1, n
+    do j = 1, m,2
+      do k = 1, j + 41
+        do ll = 1, p, 2
+          if (k > 23 + 41 .or. k < 1) error stop
+        end do
+      end do
+    end do
+  end do
+  if (ll /= 29) error stop
 
-!  !$omp simd collapse(3) lastprivate(p)
-!  do i = 1, n
-!    do j = 1, m,2
-!      do k = 1, j + 41
-!        do ll = 1, p, 2
-!          if (k > 23 + 41 .or. k < 1) error stop
-!        end do
-!      end do
-!    end do
-!  end do
-!  if (ll /= 29) error stop
+  !$omp simd collapse(3) lastprivate(ll)
+  do i = 1, n
+    do j = 1, m,2
+      do k = 1, j + 41
+        do ll = 1, p, 2
+          if (k > 23 + 41 .or. k < 1) error stop
+        end do
+      end do
+    end do
+  end do
+  if (ll /= 29) error stop
 
-!  !$omp simd collapse(3) lastprivate(k)
-!  do i = 1, n,2
-!    do j = 1, m
-!      do k = 1, i + 41
-!        if (k > 11 + 41 .or. k < 1) error stop
-!      end do
-!    end do
-!  end do
-!if (k /= 53) then
-!  print *, k, 53
-!  error stop
-!endif
+  !$omp simd collapse(3) lastprivate(k)
+  do i = 1, n,2
+    do j = 1, m
+      do k = 1, i + 41
+        if (k > 11 + 41 .or. k < 1) error stop
+      end do
+    end do
+  end do
+if (k /= 53) then
+  print *, k, 53
+  error stop
+endif
 
 !$omp simd collapse(3) lastprivate(k)
 do i = 1, n,2
@@ -546,32 +543,32 @@ if (k /= 53) then
 endif
 
 ! - Same but without 'private':
-!!$omp simd collapse(3) lastprivate(k)
-!do i = 1, n
-!  do j = 1, m,2
-!    do k = 1, j + 41
-!      if (k > 23 + 41 .or. k < 1) error stop
-!    end do
-!  end do
-!end do
-!if (k /= 65) then
-!  print *, k, 65
-!  error stop
-!endif
+!$omp simd collapse(3) lastprivate(k)
+do i = 1, n
+  do j = 1, m,2
+    do k = 1, j + 41
+      if (k > 23 + 41 .or. k < 1) error stop
+    end do
+  end do
+end do
+if (k /= 65) then
+  print *, k, 65
+  error stop
+endif
 
 
-!!$omp simd collapse(3) lastprivate(k)
-!do i = 1, n,2
-!  do j = 1, m
-!    do k = 1, i + 41
-!      if (k > 11 + 41 .or. k < 1) error stop
-!    end do
-!  end do
-!end do
-!if (k /= 53) then
-!  print *, k, 53
-!  error stop
-!endif
+!$omp simd collapse(3) lastprivate(k)
+do i = 1, n,2
+  do j = 1, m
+    do k = 1, i + 41
+      if (k > 11 + 41 .or. k < 1) error stop
+    end do
+  end do
+end do
+if (k /= 53) then
+  print *, k, 53
+  error stop
+endif
 
 !$omp simd collapse(3) lastprivate(k)
 do i = 1, n,2
@@ -601,32 +598,32 @@ if (k /= 53) then
 endif
 
 ! - all with lastprivate
-!!$omp simd collapse(3) lastprivate(k) lastprivate(i, j)
-!do i = 1, n
-!  do j = 1, m,2
-!    do k = 1, j + 41
-!      if (k > 23 + 41 .or. k < 1) error stop
-!    end do
-!  end do
-!end do
-!if (k /= 65) then
-!  print *, k, 65
-!  error stop
-!endif
+!$omp simd collapse(3) lastprivate(k) lastprivate(i, j)
+do i = 1, n
+  do j = 1, m,2
+    do k = 1, j + 41
+      if (k > 23 + 41 .or. k < 1) error stop
+    end do
+  end do
+end do
+if (k /= 65) then
+  print *, k, 65
+  error stop
+endif
 
 
-!!$omp simd collapse(3) lastprivate(k) lastprivate(i, j)
-!do i = 1, n,2
-!  do j = 1, m
-!    do k = 1, i + 41
-!      if (k > 11 + 41 .or. k < 1) error stop
-!    end do
-!  end do
-!end do
-!if (k /= 53) then
-!  print *, k, 53
-!  error stop
-!endif
+!$omp simd collapse(3) lastprivate(k) lastprivate(i, j)
+do i = 1, n,2
+  do j = 1, m
+    do k = 1, i + 41
+      if (k > 11 + 41 .or. k < 1) error stop
+    end do
+  end do
+end do
+if (k /= 53) then
+  print *, k, 53
+  error stop
+endif
 
 !$omp simd collapse(3) lastprivate(k) lastprivate(i, j)
 do i = 1, n,2
