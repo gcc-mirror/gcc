5e48542f93de0dd4b74c74924ed90f3cd37f6738
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2023-07-20 12:52:50 +0200

    testsuite: Replace dg-excess-errors with dg-error
    Some test were using dg-excess-errors instead of dg-error, this is now
    fixed.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/proc_macro_attribute_crate_type.rs: Replace
    	dg-excess-errors with dg-error and fix the line so the test
    	can pass.
    	* rust/compile/proc_macro_crate_type.rs: Likewise.
    	* rust/compile/proc_macro_derive_crate_type.rs: Likewise.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

--- gcc/testsuite/rust/compile/proc_macro_derive_crate_type.rs
+++ gcc/testsuite/rust/compile/proc_macro_derive_crate_type.rs
@@ -2,5 +2,5 @@
 
 trait Dungeness {}
 
-#[proc_macro_derive(Dungeness)] // { dg-excess-errors "the '#\[proc_macro_derive\]' attribute is only usable with crates of the 'proc-macro' crate type" }
+#[proc_macro_derive(Dungeness)] // { dg-error "the .#.proc_macro_derive.. attribute is only usable with crates of the .proc-macro. crate type" }
 pub fn my_invalid_macro() {}
