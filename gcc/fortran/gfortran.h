09dda270380fe13e7b4722305cb1122df1f779a0
Tobias Burnus <tobias@codesourcery.com>
2023-07-25 15:43:58 +0200

    OpenMP/Fortran: Reject declarations between target + teams
    While commit r14-2754-g2e31fe431b08b0302e1fa8a1c18ee51adafd41df
    detected executable statements, declarations do not show up as
    executable statements.  Hence, we now check whether the first
    statement after TARGET is TEAMS - such that we can detect data
    statements like type or variable declarations.  Fortran semantics
    ensures that only executable directives/statemens can come after
    '!$omp end teams' such that those can be detected with the
    previous check.
    
    Note that statements returning ST_NONE such as 'omp nothing' or
    'omp error at(compilation)' will still slip through.
    
    	PR fortran/110725
    	PR middle-end/71065
    
    gcc/fortran/ChangeLog:
    
    	* gfortran.h (gfc_omp_clauses): Add target_first_st_is_teams.
    	* parse.cc (parse_omp_structured_block): Set it if the first
    	statement in the structured block of a TARGET is TEAMS or
    	a combined/composite starting with TEAMS.
    	* openmp.cc (resolve_omp_target): Also show an error for
    	contains_teams_construct without target_first_st_is_teams.
    
    gcc/testsuite/ChangeLog:
    
    	* gfortran.dg/gomp/teams-6.f90: New test.

--- gcc/fortran/gfortran.h
+++ gcc/fortran/gfortran.h
@@ -1575,7 +1575,7 @@ typedef struct gfc_omp_clauses
   unsigned order_unconstrained:1, order_reproducible:1, capture:1;
   unsigned grainsize_strict:1, num_tasks_strict:1, compare:1, weak:1;
   unsigned non_rectangular:1, order_concurrent:1;
-  unsigned contains_teams_construct:1;
+  unsigned contains_teams_construct:1, target_first_st_is_teams:1;
   ENUM_BITFIELD (gfc_omp_sched_kind) sched_kind:3;
   ENUM_BITFIELD (gfc_omp_device_type) device_type:2;
   ENUM_BITFIELD (gfc_omp_memorder) memorder:3;
--- gcc/fortran/parse.cc
+++ gcc/fortran/parse.cc
@@ -5766,7 +5766,7 @@ parse_openmp_allocate_block (gfc_statement omp_st)
 static gfc_statement
 parse_omp_structured_block (gfc_statement omp_st, bool workshare_stmts_only)
 {
-  gfc_statement st, omp_end_st;
+  gfc_statement st, omp_end_st, first_st;
   gfc_code *cp, *np;
   gfc_state_data s;
 
@@ -5857,7 +5857,7 @@ parse_omp_structured_block (gfc_statement omp_st, bool workshare_stmts_only)
   gfc_namespace *my_ns = NULL;
   gfc_namespace *my_parent = NULL;
 
-  st = next_statement ();
+  first_st = st = next_statement ();
 
   if (st == ST_BLOCK)
     {
@@ -5876,8 +5876,27 @@ parse_omp_structured_block (gfc_statement omp_st, bool workshare_stmts_only)
       new_st.ext.block.ns = my_ns;
       new_st.ext.block.assoc = NULL;
       accept_statement (ST_BLOCK);
-      st = parse_spec (ST_NONE);
+      first_st = next_statement ();
+      st = parse_spec (first_st);
     }
+
+  if (omp_end_st == ST_OMP_END_TARGET)
+    switch (first_st)
+      {
+      case ST_OMP_TEAMS:
+      case ST_OMP_TEAMS_DISTRIBUTE:
+      case ST_OMP_TEAMS_DISTRIBUTE_SIMD:
+      case ST_OMP_TEAMS_DISTRIBUTE_PARALLEL_DO:
+      case ST_OMP_TEAMS_DISTRIBUTE_PARALLEL_DO_SIMD:
+      case ST_OMP_TEAMS_LOOP:
+	{
+	  gfc_state_data *stk = gfc_state_stack->previous;
+	  stk->tail->ext.omp_clauses->target_first_st_is_teams = true;
+	  break;
+	}
+      default:
+	break;
+      }
 
   do
     {
