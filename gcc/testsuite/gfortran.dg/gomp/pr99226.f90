50656980497d77ac12a5e7179013a6af09ba32f7
Tobias Burnus <tobias@codesourcery.com>
2023-07-25 09:28:43 +0200

    gfortran.dg/gomp/pr99226.f90: Add missing dg-error
    Follow up to r14-2754-g2e31fe431b08b0302e1fa8a1c18ee51adafd41df
    which added a check that a target region with teams does not
    have anything anything else strictly nested in the target.
    
    When changing the dg-error for this PR, somehow the addition of a
    dg-error in a second line was lost (the message uses (1) and (2) as
    location, showing two lines, both need a dg-error with the same message).
    
    gcc/testsuite/ChangeLog:
    
    	* gfortran.dg/gomp/pr99226.f90: Update dg-error.

--- gcc/testsuite/gfortran.dg/gomp/pr99226.f90
+++ gcc/testsuite/gfortran.dg/gomp/pr99226.f90
@@ -3,7 +3,7 @@
 subroutine sub (n)
    integer :: n, i
    !$omp target	! { dg-error "OMP TARGET region at .1. with a nested TEAMS at .2. may not contain any other statement, declaration or directive outside of the single TEAMS construct" }
-   !$omp teams distribute dist_schedule (static,n+4)
+   !$omp teams distribute dist_schedule (static,n+4)	! { dg-error "OMP TARGET region at .1. with a nested TEAMS at .2. may not contain any other statement, declaration or directive outside of the single TEAMS construct" }
    do i = 1, 8
    end do
    !$omp teams distribute dist_schedule (static,n+4)
