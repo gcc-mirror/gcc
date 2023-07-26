27ba73644f53c118e3f9b3fe9cd792210696ec55
Harald Anlauf <anlauf@gmx.de>
2023-07-26 21:12:45 +0200

    Fortran: diagnose strings of non-constant length in DATA statements [PR68569]
    gcc/fortran/ChangeLog:
    
    	PR fortran/68569
    	* resolve.cc (check_data_variable): Do not accept strings with
    	deferred length or non-constant length in a DATA statement.
    	Reject also substrings of string variables of non-constant length.
    
    gcc/testsuite/ChangeLog:
    
    	PR fortran/68569
    	* gfortran.dg/data_char_4.f90: Adjust expected diagnostic.
    	* gfortran.dg/data_char_5.f90: Likewise.
    	* gfortran.dg/data_char_6.f90: New test.

--- gcc/testsuite/gfortran.dg/data_char_5.f90
+++ gcc/testsuite/gfortran.dg/data_char_5.f90
@@ -4,12 +4,12 @@
 subroutine sub ()
   integer :: ll = 4
   block
-    character(ll) :: c(2) ! { dg-error "non-constant" }
-    data c /'a', 'b'/
+    character(ll) :: c(2)
+    data c /'a', 'b'/     ! { dg-error "Non-constant character length" }
   end block
 contains
   subroutine sub1 ()
-    character(ll) :: d(2) ! { dg-error "non-constant" }
-    data d /'a', 'b'/
+    character(ll) :: d(2)
+    data d /'a', 'b'/     ! { dg-error "Non-constant character length" }
   end subroutine sub1
 end subroutine sub
