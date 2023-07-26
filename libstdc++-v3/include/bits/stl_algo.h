c01b344e814001e07fd304ce98d013d811e90192
Jonathan Wakely <jwakely@redhat.com>
2023-07-26 14:05:58 +0100

    libstdc++: Add deprecated attribute to std::random_shuffle declarations
    We already have these attributes on the definitions in <bits/stl_algo.h>
    but they don't work due to PR c++/84542. Add the attributes to the
    declarations in <bits/algorithmfwd.h> as well, and add a test.
    
    libstdc++-v3/ChangeLog:
    
    	* include/bits/algorithmfwd.h (random_shuffle): Add deprecated
    	attribute.
    	* include/bits/stl_algo.h (random_shuffle): Correct comments.
    	* testsuite/25_algorithms/random_shuffle/1.cc: Disable
    	deprecated warnings.
    	* testsuite/25_algorithms/random_shuffle/59603.cc: Likewise.
    	* testsuite/25_algorithms/random_shuffle/moveable.cc: Likewise.
    	* testsuite/25_algorithms/random_shuffle/deprecated.cc: New
    	test.

--- libstdc++-v3/include/bits/stl_algo.h
+++ libstdc++-v3/include/bits/stl_algo.h
@@ -4479,7 +4479,7 @@ _GLIBCXX_BEGIN_NAMESPACE_ALGO
    *  equally likely.
    *
    *  @deprecated
-   *  Since C++14 `std::random_shuffle` is not part of the C++ standard.
+   *  Since C++17, `std::random_shuffle` is not part of the C++ standard.
    *  Use `std::shuffle` instead, which was introduced in C++11.
   */
   template<typename _RandomAccessIterator>
@@ -4518,7 +4518,7 @@ _GLIBCXX_BEGIN_NAMESPACE_ALGO
    *  range `[0, N)`.
    *
    *  @deprecated
-   *  Since C++14 `std::random_shuffle` is not part of the C++ standard.
+   *  Since C++17, `std::random_shuffle` is not part of the C++ standard.
    *  Use `std::shuffle` instead, which was introduced in C++11.
   */
   template<typename _RandomAccessIterator, typename _RandomNumberGenerator>
@@ -4546,7 +4546,7 @@ _GLIBCXX_BEGIN_NAMESPACE_ALGO
 	}
     }
 #endif // HOSTED
-#endif // C++11 || USE_DEPRECATED
+#endif // <= C++11 || USE_DEPRECATED
 
   /**
    *  @brief Move elements for which a predicate is true to the beginning
