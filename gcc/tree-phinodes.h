0460c1221627938baa56c4b574a43ff19a6a8499
Richard Biener <rguenther@suse.de>
2023-08-02 13:26:06 +0200

    Make add_phi_node_to_bb static
    The only exported PHI allocation already adds the PHI node to a block.
    
    	* tree-phinodes.h (add_phi_node_to_bb): Remove.
    	* tree-phinodes.cc  (add_phi_node_to_bb): Make static.

--- gcc/tree-phinodes.h
+++ gcc/tree-phinodes.h
@@ -22,7 +22,6 @@ along with GCC; see the file COPYING3.  If not see
 
 extern void phinodes_print_statistics (void);
 extern void reserve_phi_args_for_new_edge (basic_block);
-extern void add_phi_node_to_bb (gphi *phi, basic_block bb);
 extern gphi *create_phi_node (tree, basic_block);
 extern void add_phi_arg (gphi *, tree, edge, location_t);
 extern void remove_phi_args (edge);
