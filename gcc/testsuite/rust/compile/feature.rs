f0193accb7bbf8240a5beae3ddef7a102d68fb47
Muhammad Mahad <mahadtxt@gmail.com>
2023-08-03 13:24:13 +0500

    gccrs: [E0635] Use of Unknown feature.
    Added ErrorCode support for use of unknown
    feature.
    
    gcc/rust/ChangeLog:
    
    	* checks/errors/rust-feature-gate.cc (FeatureGate::check):
    	errorcode support for unknown feature.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/feature.rs: Added new unknown feature.
    
    Signed-off-by: Muhammad Mahad <mahadtxt@gmail.com>

--- gcc/testsuite/rust/compile/feature.rs
+++ gcc/testsuite/rust/compile/feature.rs
@@ -1,4 +1,6 @@
-#![feature(AA)] //{ dg-error "unknown feature 'AA'" }
-                   
+// ErrorCode - E0635
+#![feature(AA)] //{ dg-error "unknown feature .AA." }
+#![feature(iamcrabby)] // { dg-error "unknown feature .iamcrabby." }
+#![feature(nonexistent_gccrs_feature)] // { dg-error "unknown feature .nonexistent_gccrs_feature." }
 
-fn main(){}
+fn main() {}
