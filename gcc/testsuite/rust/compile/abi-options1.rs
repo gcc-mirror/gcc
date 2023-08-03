9f09284d8fc0a218e6fe936591d4ba35f716408f
Muhammad Mahad <mahadtxt@gmail.com>
2023-08-03 20:46:50 +0500

    gccrs: [E0703] Use of Invalid ABI
    Added error code support for invalid ABI.
    
    gcc/rust/ChangeLog:
    
    	* hir/rust-ast-lower-base.cc (ASTLoweringBase::lower_qualifiers):
    	called error function.
    	(ASTLoweringBase::lower_extern_block): likewise.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/abi-options1.rs: updated comment for testcase.
    
    Signed-off-by: Muhammad Mahad <mahadtxt@gmail.com>

--- gcc/testsuite/rust/compile/abi-options1.rs
+++ gcc/testsuite/rust/compile/abi-options1.rs
@@ -1,7 +1,9 @@
 extern "foobar" {
-    // { dg-error "unknown ABI option" "" { target *-*-* } .-1 }
+    // { dg-error "invalid ABI: found .foobar." "" { target *-*-* } .-1 }
     fn printf(s: *const i8, ...);
 }
 
 pub extern "baz" fn test() {}
-// { dg-error "unknown ABI option" "" { target *-*-* } .-1 }
+// { dg-error "invalid ABI: found .baz." "" { target *-*-* } .-1 }
+
+// extern "Rust" fn foo() {} // OK!
