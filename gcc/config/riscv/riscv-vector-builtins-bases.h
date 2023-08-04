62d9c1dd8eb1152d8fbe0e1df101b99c9141417a
Pan Li <pan2.li@intel.com>
2023-08-04 10:17:35 +0800

    RISC-V: Support RVV VFNMACC rounding mode intrinsic API
    This patch would like to support the rounding mode API for the
    VFNMACC for the below samples.
    
    * __riscv_vfnmacc_vv_f32m1_rm
    * __riscv_vfnmacc_vv_f32m1_rm_m
    * __riscv_vfnmacc_vf_f32m1_rm
    * __riscv_vfnmacc_vf_f32m1_rm_m
    
    Signed-off-by: Pan Li <pan2.li@intel.com>
    
    gcc/ChangeLog:
    
    	* config/riscv/riscv-vector-builtins-bases.cc
    	(class vfnmacc_frm): New class for vfnmacc.
    	(vfnmacc_frm_obj): New declaration.
    	(BASE): Ditto.
    	* config/riscv/riscv-vector-builtins-bases.h: Ditto.
    	* config/riscv/riscv-vector-builtins-functions.def
    	(vfnmacc_frm): New function definition.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/base/float-point-single-negate-multiply-add.c:
    	New test.

--- gcc/config/riscv/riscv-vector-builtins-bases.h
+++ gcc/config/riscv/riscv-vector-builtins-bases.h
@@ -165,6 +165,7 @@ extern const function_base *const vfnmsac;
 extern const function_base *const vfmadd;
 extern const function_base *const vfnmsub;
 extern const function_base *const vfnmacc;
+extern const function_base *const vfnmacc_frm;
 extern const function_base *const vfmsac;
 extern const function_base *const vfnmadd;
 extern const function_base *const vfmsub;
