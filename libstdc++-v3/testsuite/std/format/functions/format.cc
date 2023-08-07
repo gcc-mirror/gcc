6cf214b4fc97f5320fff8f4d5d262ef858f34a8d
Jonathan Wakely <jwakely@redhat.com>
2023-08-07 14:06:59 +0100

    libstdc++: Add std::formatter specializations for extended float types
    This makes it possible to format _Float32, _Float64 etc. in C++20 mode.
    Previously it was only possible to format them in C++23 when the
    <stdfloat> typedefs and the std::to_chars overloads were defined.
    
    Instead of relying on std::to_chars for those types, we can just reuse
    the formatters for float, double and long double. This also avoids
    template bloat by reusing the same specializations instead of
    instantiating __formatter_fp for every different type.
    
    libstdc++-v3/ChangeLog:
    
    	* include/std/format (formatter): Add partial specializations
    	for extended floating-point types.
    	* testsuite/std/format/functions/format.cc: Move test_float128()
    	to ...
    	* testsuite/std/format/formatter/ext_float.cc: New test.

--- libstdc++-v3/testsuite/std/format/functions/format.cc
+++ libstdc++-v3/testsuite/std/format/functions/format.cc
@@ -347,38 +347,6 @@ test_p1652r1() // printf corner cases in std::format
   VERIFY( s == "3.31" );
 }
 
-template<typename T>
-bool format_float()
-{
-    auto s = std::format("{:#} != {:<+7.3f}", (T)-0.0, (T)0.5);
-    return s == "-0. != +0.500 ";
-}
-
-#if __cplusplus > 202002L
-template<typename T>
-concept formattable = std::formattable<T, char>;
-#else
-template<typename T>
-concept formattable = requires (T t, char* p) { std::to_chars(p, p, t); };
-#endif
-
-void
-test_float128()
-{
-#ifdef __SIZEOF_FLOAT128__
-  if constexpr (formattable<__float128>)
-    VERIFY( format_float<__float128>() );
-  else
-    std::puts("Cannot format __float128 on this target");
-#endif
-#if __FLT128_DIG__
-  if constexpr (formattable<_Float128>)
-    VERIFY( format_float<_Float128>() );
-  else
-    std::puts("Cannot format _Float128 on this target");
-#endif
-}
-
 void
 test_pointer()
 {
@@ -429,6 +397,5 @@ int main()
   test_wchar();
   test_minmax();
   test_p1652r1();
-  test_float128();
   test_pointer();
 }
