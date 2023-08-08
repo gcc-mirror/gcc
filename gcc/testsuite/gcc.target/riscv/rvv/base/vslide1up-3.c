ffd676ef2c9849231626a532343c7ec908558c33
yulong <shiyulong@iscas.ac.cn>
2023-08-08 12:12:32 +0800

    RISC-V: Fix a bug that causes an error insn.
    I test the following rvv intrinsics.
    vint64m1_t test_vslide1up_vx_i64m1_m(vbool64_t mask, vint64m1_t src, int64_t value, size_t vl) {
      return __riscv_vslide1up_vx_i64m1_m(mask, src, value, vl);
    }
    And I got an error info,t hat is error:
      unrecognizable insn:(insn 17 16 18 2
        (set (reg:RVVMIDI 134 [ _1 ])(if_then_else:RVVMIDI
          (unspec:RVVMF64BI [(reg/v:SI 142 [ vl ])(const_int 2 [x2])(const_int 日 [o])(reg:SI 66 vl)(reg:SI 67 vtype)] UNSPEC_VPREDICATE
       (vec_merge:RVVMIDI (reg:RVVMIDI 134 [ _1 ])(unspec:RVVMIDI [(reg:sI 日 zero)] UNSPEC_VUNDEF)
       (reg/v:RVVMF64BI 137 [ mask ]))
       (unspec:RVVM1DI[(reg:sI 日 zero)] UNSPEC_VUNDEF)))
    
    This patch fix it.
    
    gcc/ChangeLog:
    
    	* config/riscv/riscv-v.cc (slide1_sew64_helper): Modify.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/base/vslide1down-1.c: New test.
    	* gcc.target/riscv/rvv/base/vslide1down-2.c: New test.
    	* gcc.target/riscv/rvv/base/vslide1down-3.c: New test.
    	* gcc.target/riscv/rvv/base/vslide1up-1.c: New test.
    	* gcc.target/riscv/rvv/base/vslide1up-2.c: New test.
    	* gcc.target/riscv/rvv/base/vslide1up-3.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/rvv/base/vslide1up-3.c
@@ -0,0 +1,22 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv32gc_zve64f -mabi=ilp32d -Wno-psabi -O3 -fno-schedule-insns -fno-schedule-insns2" } */
+
+#include "riscv_vector.h"
+
+vint64m1_t test_vslide1up_vx_i64m1_m(vbool64_t mask, vint64m1_t src, int64_t value, size_t vl) {
+  return __riscv_vslide1up_vx_i64m1_m(mask, src, value, vl);
+}
+
+vint64m2_t test_vslide1up_vx_i64m2_m(vbool32_t mask, vint64m2_t src, int64_t value, size_t vl) {
+  return __riscv_vslide1up_vx_i64m2_m(mask, src, value, vl);
+}
+
+vint64m4_t test_vslide1up_vx_i64m4_m(vbool16_t mask, vint64m4_t src, int64_t value, size_t vl) {
+  return __riscv_vslide1up_vx_i64m4_m(mask, src, value, vl);
+}
+
+vint64m8_t test_vslide1up_vx_i64m8_m(vbool8_t mask, vint64m8_t src, int64_t value, size_t vl) {
+  return __riscv_vslide1up_vx_i64m8_m(mask, src, value, vl);
+}
+
+/* { dg-final { scan-assembler-times {vseti?vli\s+[a-z0-9]+,\s*[a-z0-9]+,\s*e[0-9]+,\s*mf?[1248],\s*t[au],\s*m[au]\s+vslide1up\.[ivxfswum.]+\s+} 4 } } */
