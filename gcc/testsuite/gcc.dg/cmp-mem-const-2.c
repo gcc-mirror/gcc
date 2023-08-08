f3a8dde08b420dbe9cacbc1a1febaaf77357f4d4
Stefan Schulze Frielinghaus <stefansf@linux.ibm.com>
2023-08-08 08:53:12 +0200

    rtl-optimization/110869 Fix tests cmp-mem-const-*.c for sparc
    This fixes the rather new tests cmp-mem-const-{1,2,3,4,5,6}.c for sparc.
    For -1 and -2 we need at least optimization level 2 on sparc.  For the
    sake of homogeneity, change all test cases to -O2.  For -3 and -4 we do
    not end up with a comparison of memory and a constant, and finally for
    -5 and -6 the constants are reduced by a prior optimization which means
    there is nothing left to do.  Thus excluding sparc from those tests.
    
    gcc/testsuite/ChangeLog:
    
    	PR rtl-optimization/110869
    	* gcc.dg/cmp-mem-const-1.c: Use optimization level 2.
    	* gcc.dg/cmp-mem-const-2.c: Dito.
    	* gcc.dg/cmp-mem-const-3.c: Exclude sparc from this test.
    	* gcc.dg/cmp-mem-const-4.c: Dito.
    	* gcc.dg/cmp-mem-const-5.c: Dito.
    	* gcc.dg/cmp-mem-const-6.c: Dito.

--- gcc/testsuite/gcc.dg/cmp-mem-const-2.c
+++ gcc/testsuite/gcc.dg/cmp-mem-const-2.c
@@ -1,5 +1,5 @@
 /* { dg-do compile { target { lp64 } } } */
-/* { dg-options "-O1 -fdump-rtl-combine-details" } */
+/* { dg-options "-O2 -fdump-rtl-combine-details" } */
 /* { dg-final { scan-rtl-dump "narrow comparison from mode .I to QI" "combine" } } */
 
 typedef __UINT64_TYPE__ uint64_t;
