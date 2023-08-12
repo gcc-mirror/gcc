fea5442127daf8472966360279d402023dba3379
Tsukasa OI <research_trasio@irq.a4lg.com>
2023-08-12 00:38:18 +0000

    RISC-V: Add stub support for existing extensions (vendor)
    After commit c283c4774d1c ("RISC-V: Throw compilation error for unknown
    extensions") changed how do we handle unknown extensions, we have no
    guarantee that we can share the same architectural string with Binutils
    (specifically, the assembler).
    
    To avoid compilation errors on shared Assembler-C/C++ projects or programs
    with inline assembler, GCC should support almost all extensions that
    Binutils support, even if the GCC itself does not touch a thing.
    
    This commit adds stub supported vendor extensions to
    riscv_ext_version_table (no riscv_implied_info entries to add; all
    information is copied from Binutils' bfd/elfxx-riscv.c).
    
    gcc/ChangeLog:
    
    	* common/config/riscv/riscv-common.cc (riscv_ext_version_table):
    	Add stub support for all vendor extensions supported by Binutils.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/predef-30.c: New test for a stub
    	vendor extension 'XVentanaCondOps'.

--- gcc/common/config/riscv/riscv-common.cc
+++ gcc/common/config/riscv/riscv-common.cc
@@ -322,6 +322,8 @@ static const struct riscv_ext_version riscv_ext_version_table[] =
   {"xtheadmempair", ISA_SPEC_CLASS_NONE, 1, 0},
   {"xtheadsync", ISA_SPEC_CLASS_NONE, 1, 0},
 
+  {"xventanacondops", ISA_SPEC_CLASS_NONE, 1, 0},
+
   /* Terminate the list.  */
   {NULL, ISA_SPEC_CLASS_NONE, 0, 0}
 };
