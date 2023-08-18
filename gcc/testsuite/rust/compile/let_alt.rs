5c339c29f9aa61a2bb0488ca2b3456b2e5c53480
Muhammad Mahad <mahadtxt@gmail.com>
2023-08-18 17:06:02 +0500

    gccrs: [E0658] top level or-patterns are not allowed let binding
    gcc/rust/ChangeLog:
    
    	* hir/rust-ast-lower-pattern.cc (ASTLoweringPattern::visit):
    	Added richlocation & error code.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/let_alt.rs: Updated comment.
    
    Signed-off-by: Muhammad Mahad <mahadtxt@gmail.com>

--- gcc/testsuite/rust/compile/let_alt.rs
+++ gcc/testsuite/rust/compile/let_alt.rs
@@ -1,4 +1,4 @@
 fn main() {
     let _a | _a = 12;
-    // { dg-error "top level alternate patterns are not allowed" "" { target *-*-* } .-1 }
+    // { dg-error "top level or-patterns are not allowed for .let. bindings" "" { target *-*-* } .-1 }
 }
