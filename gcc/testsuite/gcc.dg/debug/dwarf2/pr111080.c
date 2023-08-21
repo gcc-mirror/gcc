80ded4e8f871c98481bab912997034b9d24b1c96
Richard Biener <rguenther@suse.de>
2023-08-21 10:34:30 +0200

    debug/111080 - avoid outputting debug info for unused restrict qualified type
    The following applies some maintainance with respect to type qualifiers
    and kinds added by later DWARF standards to prune_unused_types_walk.
    The particular case in the bug is not handling (thus marking required)
    all restrict qualified type DIEs.  I've found more DW_TAG_*_type that
    are unhandled, looked up the DWARF docs and added them as well based
    on common sense.
    
    	PR debug/111080
    	* dwarf2out.c (prune_unused_types_walk): Handle
    	DW_TAG_restrict_type, DW_TAG_shared_type, DW_TAG_atomic_type,
    	DW_TAG_immutable_type, DW_TAG_coarray_type, DW_TAG_unspecified_type
    	and DW_TAG_dynamic_type as to only output them when referenced.
    
    	* gcc.dg/debug/dwarf2/pr111080.c: New testcase.

---
+++ gcc/testsuite/gcc.dg/debug/dwarf2/pr111080.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-save-temps -gdwarf-3 -dA" } */
+
+struct foo {
+        int field_number_1;
+        int field_number_2;
+        int field_number_3;
+        int field_number_4;
+        int field_number_5;
+};
+
+typedef int fun_t(struct foo *restrict);
+
+int main() {
+        return 0;
+}
+
+/* { dg-final { scan-assembler-not "DW_TAG_structure_type" } } */
