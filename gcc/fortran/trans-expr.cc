44bcb51eb0d5cac6eb2de54541ca8e6c2d738160
Paul Thomas <pault@gcc.gnu.org>
2023-08-26 14:37:49 +0100

    Fortran: Supply a missing dereference [PR92586]
    2023-08-26  Paul Thomas  <pault@gcc.gnu.org>
    
    gcc/fortran
    	PR fortran/92586
    	* trans-expr.cc (gfc_trans_arrayfunc_assign): Supply a missing
    	dereference for the call to gfc_deallocate_alloc_comp_no_caf.
    
    gcc/testsuite/
    	PR fortran/92586
    	* gfortran.dg/pr92586.f90 : New test

--- gcc/fortran/trans-expr.cc
+++ gcc/fortran/trans-expr.cc
@@ -11171,7 +11171,8 @@ gfc_trans_arrayfunc_assign (gfc_expr * expr1, gfc_expr * expr2)
   if (expr1->ts.type == BT_DERIVED
 	&& expr1->ts.u.derived->attr.alloc_comp)
     {
-      tmp = gfc_deallocate_alloc_comp_no_caf (expr1->ts.u.derived, se.expr,
+      tmp = build_fold_indirect_ref_loc (input_location, se.expr);
+      tmp = gfc_deallocate_alloc_comp_no_caf (expr1->ts.u.derived, tmp,
 					      expr1->rank);
       gfc_add_expr_to_block (&se.pre, tmp);
     }
