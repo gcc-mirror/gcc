9afdebbd5f581bdfb1f94dd944636c09ca553c5c
Richard Biener <rguenther@suse.de>
2023-08-31 09:06:24 +0200

    Adjust gcc.target/i386/pr52252-{atom,core}.c
    The following adjusts the testcases to force 128bit vectorization
    to make them more robust when for example adding -march=cascadelake
    
    	* gcc.target/i386/pr52252-atom.c: Add -mprefer-vector-width=128.
    	* gcc.target/i386/pr52252-core.c: Likewise.

--- gcc/testsuite/gcc.target/i386/pr52252-core.c
+++ gcc/testsuite/gcc.target/i386/pr52252-core.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -ftree-vectorize -mssse3 -mtune=corei7" } */
+/* { dg-options "-O2 -ftree-vectorize -mssse3 -mtune=corei7 -mprefer-vector-width=128" } */
 #define byte unsigned char
 
 void
