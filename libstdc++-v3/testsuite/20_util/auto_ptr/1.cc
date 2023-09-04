678834e9ff541f4a5b1065357e94e863b6f34913
Jonathan Wakely <jwakely@redhat.com>
2023-09-04 14:25:14 +0100

    libstdc++: Enable std::auto_ptr tests for C++11 and later
    There is no reason to only test std::auto_ptr with -std=c++03, we just
    need to handle the deprecated warnings for C++11 and later.
    
    libstdc++-v3/ChangeLog:
    
    	* testsuite/20_util/auto_ptr/1.cc: Remove dg-options -std=c++03
    	and add dg-warning for deprecation warnings.
    	* testsuite/20_util/auto_ptr/2.cc: Likewise.
    	* testsuite/20_util/auto_ptr/3.cc: Likewise.
    	* testsuite/20_util/auto_ptr/3946.cc: Likewise.
    	* testsuite/20_util/auto_ptr/4.cc: Likewise.
    	* testsuite/20_util/auto_ptr/5.cc: Likewise.
    	* testsuite/20_util/auto_ptr/6.cc: Likewise.
    	* testsuite/20_util/auto_ptr/7.cc: Likewise.
    	* testsuite/20_util/auto_ptr/assign_neg.cc: Likewise.
    	* testsuite/20_util/auto_ptr/requirements/explicit_instantiation/1.cc:
    	Likewise.
    	* testsuite/tr1/2_general_utilities/shared_ptr/assign/auto_ptr.cc:
    	Likewise.
    	* testsuite/tr1/2_general_utilities/shared_ptr/assign/auto_ptr_neg.cc:
    	Likewise.
    	* testsuite/tr1/2_general_utilities/shared_ptr/assign/auto_ptr_rvalue_neg.cc:
    	Likewise.
    	* testsuite/tr1/2_general_utilities/shared_ptr/cons/43820_neg.cc:
    	Likewise.
    	* testsuite/tr1/2_general_utilities/shared_ptr/cons/auto_ptr.cc:
    	Likewise.
    	* testsuite/tr1/2_general_utilities/shared_ptr/cons/auto_ptr_neg.cc:
    	Likewise.

--- libstdc++-v3/testsuite/20_util/auto_ptr/1.cc
+++ libstdc++-v3/testsuite/20_util/auto_ptr/1.cc
@@ -15,9 +15,9 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// 20.4.5 Template class auto_ptr [lib.auto.ptr]
+// C++03 20.4.5 Template class auto_ptr [lib.auto.ptr]
 
-// { dg-options "-std=c++98" }
+// { dg-add-options using-deprecated }
 
 #include <memory>
 #include <testsuite_hooks.h>
@@ -63,21 +63,21 @@ test01()
 {
   reset_count_struct __attribute__((unused)) reset;
 
-  std::auto_ptr<A> A_default;
+  std::auto_ptr<A> A_default; // { dg-warning "deprecated" "" { target c++11 } }
   VERIFY( A_default.get() == 0 );
   VERIFY( A::ctor_count == 0 );
   VERIFY( A::dtor_count == 0 );
   VERIFY( B::ctor_count == 0 );
   VERIFY( B::dtor_count == 0 );
 
-  std::auto_ptr<A> A_from_A(new A);
+  std::auto_ptr<A> A_from_A(new A); // { dg-warning "deprecated" "" { target c++11 } }
   VERIFY( A_from_A.get() != 0 );
   VERIFY( A::ctor_count == 1 );
   VERIFY( A::dtor_count == 0 );
   VERIFY( B::ctor_count == 0 );
   VERIFY( B::dtor_count == 0 );
 
-  std::auto_ptr<A> A_from_B(new B);
+  std::auto_ptr<A> A_from_B(new B); // { dg-warning "deprecated" "" { target c++11 } }
   VERIFY( A_from_B.get() != 0 );
   VERIFY( A::ctor_count == 2 );
   VERIFY( A::dtor_count == 0 );
