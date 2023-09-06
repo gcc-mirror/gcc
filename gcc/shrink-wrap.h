66d89a43a7b6bafca1d4675744808be53ef2736f
Fei Gao <gaofei@eswincomputing.com>
2023-09-06 09:39:08 +0000

    Allow targets to check shrink-wrap-separate enabled or not
    No functional changes but restructure and expose use_shrink_wrapping_separate
    to the TARGETs.
    
    gcc/ChangeLog:
    
    	* shrink-wrap.cc (try_shrink_wrapping_separate):call
    	use_shrink_wrapping_separate.
    	(use_shrink_wrapping_separate): wrap the condition
    	check in use_shrink_wrapping_separate.
    	* shrink-wrap.h (use_shrink_wrapping_separate): add to extern

--- gcc/shrink-wrap.h
+++ gcc/shrink-wrap.h
@@ -26,6 +26,7 @@ along with GCC; see the file COPYING3.  If not see
 extern bool requires_stack_frame_p (rtx_insn *, HARD_REG_SET, HARD_REG_SET);
 extern void try_shrink_wrapping (edge *entry_edge, rtx_insn *prologue_seq);
 extern void try_shrink_wrapping_separate (basic_block first_bb);
+extern bool use_shrink_wrapping_separate (void);
 #define SHRINK_WRAPPING_ENABLED \
   (flag_shrink_wrap && targetm.have_simple_return ())
 
