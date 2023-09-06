c62c82dc98dcb7420498b7114bf4cd2ec1a81405
Jakub Jelinek <jakub@redhat.com>
2023-09-06 17:47:49 +0200

    Add further _BitInt <-> floating point tests [PR102989]
    Here are just the testsuite additions from libgcc _BitInt patch review.
    
    On Fri, Sep 01, 2023 at 09:48:22PM +0000, Joseph Myers wrote:
    > 1. Test overflowing conversions to integers (including from inf or NaN)
    > raise FE_INVALID.  (Note: it's not specified in the standard whether
    > inexact conversions to integers raise FE_INEXACT or not, so testing that
    > seems less important.)
    
    This is in gcc.dg/bitint-28.c (FE_INVALID) and gcc.dg/bitint-29.c
    (FE_INEXACT) for binary and dfp/bitint-8.c new tests.
    
    > 2. Test conversions from integers to floating point raise FE_INEXACT when
    > inexact, together with FE_OVERFLOW when overflowing (while exact
    > conversions don't raise exceptions).
    
    This is in gcc.dg/bitint-30.c new test.
    
    > 3. Test conversions from integers to floating point respect the rounding
    > mode.
    
    This is in gcc.dg/bitint-31.c new test.
    
    > 4. Test converting floating-point values in the range (-1.0, 0.0] to both
    > unsigned and signed _BitInt; I didn't see such tests for binary floating
    > types, only for decimal types, and the decimal tests didn't include tests
    > of negative zero itself as the value converted to _BitInt.
    
    This is done as incremental changes to existing tests.
    
    > 5. Test conversions of noncanonical BID zero to integers (these tests
    > would be specific to BID).  See below for a bug in this area.
    
    This is done in dfp/bitint-7.c test.
    
    2023-09-06  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c/102989
    	* gcc.dg/torture/bitint-21.c (main): Add tests for -1 for signed only,
    	-1 + epsilon, another (-1, 0) range value and -0.
    	* gcc.dg/torture/bitint-22.c (main): Likewise.
    	* gcc.dg/bitint-28.c: New test.
    	* gcc.dg/bitint-29.c: New test.
    	* gcc.dg/bitint-30.c: New test.
    	* gcc.dg/bitint-31.c: New test.
    	* gcc.dg/dfp/bitint-1.c (main): Add tests for -1 for signed only,
    	-1 + epsilon and -0.
    	* gcc.dg/dfp/bitint-2.c (main): Likewise.
    	* gcc.dg/dfp/bitint-3.c (main): Likewise.
    	* gcc.dg/dfp/bitint-7.c: New test.
    	* gcc.dg/dfp/bitint-8.c: New test.

---
+++ gcc/testsuite/gcc.dg/dfp/bitint-7.c
@@ -0,0 +1,110 @@
+/* PR c/102989 */
+/* Test non-canonical BID significands.  */
+/* { dg-do run { target bitint } } */
+/* { dg-require-effective-target dfp_bid } */
+/* { dg-options "-std=gnu2x -O2" } */
+
+union U32
+{
+  _Decimal32 d;
+  unsigned int u;
+};
+
+union U64
+{
+  _Decimal64 d;
+  unsigned long long int u;
+};
+
+union U128
+{
+  _Decimal128 d;
+  unsigned long long int u[2];
+};
+
+int
+main ()
+{
+  volatile union U32 u32;
+  u32.d = 0.9999999e+27DF;
+  u32.u++;
+  volatile union U64 u64;
+  u64.d = 0.9999999999999999e+90DD;
+  u64.u++;
+  volatile union U128 u128;
+  u128.d = 0.9999999999999999999999999999999999e+39DL;
+  if (u128.u[0] == 0x378d8e63ffffffffULL)
+    u128.u[0]++;
+  else if (u128.u[1] == 0x378d8e63ffffffffULL)
+    u128.u[1]++;
+  else
+    u128.d = 0.DL;
+#if __BITINT_MAXWIDTH__ >= 192
+  if ((_BitInt(192)) u32.d != 0wb
+      || (unsigned _BitInt(192)) u32.d != 0uwb
+      || (_BitInt(192)) u64.d != 0wb
+      || (unsigned _BitInt(192)) u64.d != 0uwb
+      || (_BitInt(192)) u128.d != 0wb
+      || (unsigned _BitInt(192)) u128.d != 0uwb)
+    __builtin_abort ();
+#endif
+#if __BITINT_MAXWIDTH__ >= 575
+  if ((_BitInt(575)) u32.d != 0wb
+      || (unsigned _BitInt(575)) u32.d != 0uwb
+      || (_BitInt(575)) u64.d != 0wb
+      || (unsigned _BitInt(575)) u64.d != 0uwb
+      || (_BitInt(575)) u128.d != 0wb
+      || (unsigned _BitInt(575)) u128.d != 0uwb)
+    __builtin_abort ();
+#endif
+  u32.u = 0xe59fffffU;
+  u64.u = 0xe3ffffffffffffffULL;
+  if (u128.u[0] == 0x378d8e6400000000ULL)
+    {
+      u128.u[0] = -1ULL;
+      u128.u[1] = 0xe1be7fffffffffffULL;
+    }
+  else if (u128.u[1] == 0x378d8e6400000000ULL)
+    {
+      u128.u[1] = -1ULL;
+      u128.u[0] = 0xe1be7fffffffffffULL;
+    }
+#if __BITINT_MAXWIDTH__ >= 192
+  if ((_BitInt(192)) u32.d != 0wb
+      || (unsigned _BitInt(192)) u32.d != 0uwb
+      || (_BitInt(192)) u64.d != 0wb
+      || (unsigned _BitInt(192)) u64.d != 0uwb
+      || (_BitInt(192)) u128.d != 0wb
+      || (unsigned _BitInt(192)) u128.d != 0uwb)
+    __builtin_abort ();
+#endif
+#if __BITINT_MAXWIDTH__ >= 575
+  if ((_BitInt(575)) u32.d != 0wb
+      || (unsigned _BitInt(575)) u32.d != 0uwb
+      || (_BitInt(575)) u64.d != 0wb
+      || (unsigned _BitInt(575)) u64.d != 0uwb
+      || (_BitInt(575)) u128.d != 0wb
+      || (unsigned _BitInt(575)) u128.d != 0uwb)
+    __builtin_abort ();
+#endif
+  if (u128.u[0] == -1ULL)
+    {
+      u128.u[0] = 0;
+      u128.u[1] = 0xe629800000000000ULL;
+    }
+  else if (u128.u[1] == -1ULL)
+    {
+      u128.u[1] = 0;
+      u128.u[0] = 0xe629800000000000ULL;
+    }
+#if __BITINT_MAXWIDTH__ >= 192
+  if ((_BitInt(192)) u128.d != 0wb
+      || (unsigned _BitInt(192)) u128.d != 0uwb)
+    __builtin_abort ();
+#endif
+#if __BITINT_MAXWIDTH__ >= 575
+  if ((_BitInt(575)) u128.d != 0wb
+      || (unsigned _BitInt(575)) u128.d != 0uwb)
+    __builtin_abort ();
+#endif
+}
