1c135ab169fd6924292258ab107732b565ee1ee3
Eric Botcazou <ebotcazou@adacore.com>
2023-09-07 16:44:36 +0200

    ada: Fix wrong optimization of extended return for discriminated record type
    This happens when the discriminants of the record type have default values.
    
    gcc/ada/ChangeLog:
    
    	* inline.adb (Expand_Inlined_Call): In the case of a function call
    	that returns an unconstrained type and initializes an object, set
    	the No_Initialization flag on the new declaration of the object.

--- gcc/ada/inline.adb
+++ gcc/ada/inline.adb
@@ -4105,6 +4105,7 @@ package body Inline is
               Object_Definition   =>
                 New_Copy_Tree (Object_Definition (Parent (Targ1))));
          Replace_Formals (Decl);
+         Set_No_Initialization (Decl);
          Rewrite (Parent (N), Decl);
          Analyze (Parent (N));
 
