fbbd9001e9b6f2c59b542cc53a8f9183514091ce
Tobias Burnus <tobias@codesourcery.com>
2023-09-07 12:40:57 +0200

    Revert "contrib/gcc-changelog: Check whether revert-commit exists"
    This reverts commit ffffffffffffffffffffffffffffffffffffffff.

--- contrib/gcc-changelog/git_email.py
+++ contrib/gcc-changelog/git_email.py
@@ -89,7 +89,8 @@ class GitEmail(GitCommit):
                 t = 'M'
             modified_files.append((target if t != 'D' else source, t))
         git_info = GitInfo(None, date, author, message, modified_files)
-        super().__init__(git_info, commit_to_info_hook=None)
+        super().__init__(git_info,
+                         commit_to_info_hook=lambda x: None)
 
 
 def show_help():
