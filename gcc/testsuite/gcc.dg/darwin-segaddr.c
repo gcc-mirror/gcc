83f10a9ac04a616a9c7c7d3f35401653afe3296c
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2023-09-08 21:55:56 +0200

    testsuite: adjust for darwin linker warning
    On recent macOS versions, no_pie is deprecated and the linker complains
    about it: "-no_pie is deprecated when targeting new OS versions"
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/darwin-segaddr.c: Adjust for darwin linker warning.
    	* gcc.dg/pie-7.c: Likewise.
    
    (cherry picked from commit d9926c0d974646dc6024d5a881fe1bee2f499139)

--- gcc/testsuite/gcc.dg/darwin-segaddr.c
+++ gcc/testsuite/gcc.dg/darwin-segaddr.c
@@ -2,6 +2,7 @@
 /* { dg-do run { target *-*-darwin* } } */
 /* { dg-options "-O0 -segaddr __TEST 0x200000 -fno-pie" { target { *-*-darwin* && { ! lp64 } } } } */
 /* { dg-options "-O0 -segaddr __TEST 0x110000000 -fno-pie" { target { *-*-darwin* && lp64 } } } */
+/* { dg-prune-output "-no_pie is deprecated when targeting new OS versions" } */
 
 extern void abort ();
 
