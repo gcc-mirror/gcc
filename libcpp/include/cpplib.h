00dea7e8c41b672730d6e2c891b6012a83d8842c
Raiki Tamura <tamaron1203@gmail.com>
2023-09-08 16:59:09 +0200

    libcpp: add function to check XID properties
    This commit adds a new function intended for checking the XID properties
    of a possibly unicode character, as well as the accompanying enum
    describing the possible properties.
    
    libcpp/ChangeLog:
    
    	* charset.cc (cpp_check_xid_property): New.
    	* include/cpplib.h
    	(cpp_check_xid_property): New.
    	(enum cpp_xid_property): New.
    
    Signed-off-by: Raiki Tamura <tamaron1203@gmail.com>

--- libcpp/include/cpplib.h
+++ libcpp/include/cpplib.h
@@ -1631,4 +1631,11 @@ bool cpp_valid_utf8_p (const char *data, size_t num_bytes);
 bool cpp_is_combining_char (cppchar_t c);
 bool cpp_is_printable_char (cppchar_t c);
 
+enum cpp_xid_property {
+  CPP_XID_START = 1,
+  CPP_XID_CONTINUE = 2
+};
+
+unsigned int cpp_check_xid_property (cppchar_t c);
+
 #endif /* ! LIBCPP_CPPLIB_H */
