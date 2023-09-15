0ff31212eb6dd0f7b64f9c51be9850eee2b8f83b
Jonathan Wakely <jwakely@redhat.com>
2023-09-15 14:49:36 +0100

    libstdc++: Do not require effective target pthread for some tests
    We can remove { dg-require-effective-target pthread } and allow these
    tests non-pthread targets and conditionally adding -pthread only for
    pthread targets.
    
    Also remove the { dg-options "-std=gnu++20" } that is no longer needed.
    
    libstdc++-v3/ChangeLog:
    
    	* testsuite/29_atomics/atomic_ref/wait_notify.cc: Do not
    	restrict to pthreads targets.
    	* testsuite/30_threads/jthread/3.cc: Likewise.
    	* testsuite/30_threads/semaphore/100806.cc: Likewise.
    	* testsuite/30_threads/semaphore/try_acquire.cc: Likewise.
    	* testsuite/30_threads/semaphore/try_acquire_for.cc: Likewise.
    	* testsuite/30_threads/semaphore/try_acquire_posix.cc: Likewise.

--- libstdc++-v3/testsuite/30_threads/jthread/3.cc
+++ libstdc++-v3/testsuite/30_threads/jthread/3.cc
@@ -15,9 +15,8 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-options "-std=gnu++2a -pthread" }
-// { dg-do compile { target c++2a } }
-// { dg-require-effective-target pthread }
+// { dg-do compile { target c++20 } }
+// { dg-additional-options "-pthread" { target pthread } }
 // { dg-require-gthreads "" }
 
 #include <thread>
