4aca1cfd6235090e48a53dab734437740671bbf3
Qing Zhao <qing.zhao@oracle.com>
2023-09-15 13:46:52 +0000

    Fix PR111407--SSA corruption due to widening_mul opt on conflict across an abnormal edge
    This is a bug in tree-ssa-math-opts.cc, when applying the widening mul
    optimization, the compiler needs to check whether the operand is in a
    ABNORMAL PHI, if YES, we should avoid the transformation.
    
    	PR tree-optimization/111407
    
    gcc/ChangeLog:
    
    	* tree-ssa-math-opts.cc (convert_mult_to_widen): Avoid the transform
    	when one of the operands is subject to abnormal coalescing.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/pr111407.c: New test.

---
+++ gcc/testsuite/gcc.dg/pr111407.c
@@ -0,0 +1,21 @@
+/* PR tree-optimization/111407*/
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+enum { SEND_TOFILE } __sigsetjmp();
+void fclose();
+void foldergets();
+void sendpart_stats(int *p1, int a1, int b1) {
+ int *a = p1;
+ fclose();
+ p1 = 0;
+ long t = b1;
+ if (__sigsetjmp()) {
+   {
+     long t1 = a1;
+     a1+=1;
+     fclose(a1*(long)t1);
+   }
+ }
+ if (p1)
+   fclose();
+}
