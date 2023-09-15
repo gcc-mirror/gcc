9b80311cdc685e6f27cf4f8625ac3d24dcc59d7f
Juzhe-Zhong <juzhe.zhong@rivai.ai>
2023-09-15 17:03:49 +0800

    test: Block slp-16.c check for target support vect_strided6
    This testcase FAIL in RISC-V because RISC-V support vect_load_lanes with 6.
    FAIL: gcc.dg/vect/slp-16.c -flto -ffat-lto-objects  scan-tree-dump-times vect "vectorizing stmts using SLP" 2
    FAIL: gcc.dg/vect/slp-16.c scan-tree-dump-times vect "vectorizing stmts using SLP" 2
    
    Since it use vlseg6 (vect_load_lanes with array size = 6)
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/vect/slp-16.c: Block vect_strided6.
    	* lib/target-supports.exp: Add strided type.

--- gcc/testsuite/gcc.dg/vect/slp-16.c
+++ gcc/testsuite/gcc.dg/vect/slp-16.c
@@ -67,5 +67,5 @@ int main (void)
 }
 
 /* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect"  { target vect_int_mult } } } */
-/* { dg-final { scan-tree-dump-times "vectorizing stmts using SLP" 2 "vect" { target vect_int_mult } } } */
+/* { dg-final { scan-tree-dump-times "vectorizing stmts using SLP" 2 "vect" { target { vect_int_mult && {! vect_strided6 } } } } } */
   
