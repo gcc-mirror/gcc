1a554a2c9f33fdb3c170f1c37274037ece050114
Tobias Burnus <tobias@codesourcery.com>
2023-09-20 16:03:19 +0200

    OpenMP: Add ME support for 'omp allocate' stack variables
    Call GOMP_alloc/free for 'omp allocate' allocated variables. This is
    for C only as C++ and Fortran show a sorry already in the FE. Note that
    this only applies to stack variables as the C FE shows a sorry for
    static variables.
    
    gcc/ChangeLog:
    
    	* gimplify.cc (gimplify_bind_expr): Call GOMP_alloc/free for
    	'omp allocate' variables; move stack cleanup after other
    	cleanup.
    	(omp_notice_variable): Process original decl when decl
    	of the value-expression for a 'omp allocate' variable is passed.
    	* omp-low.cc (scan_omp_1_op): Handle 'omp allocate' variables
    
    libgomp/ChangeLog:
    
    	* libgomp.texi (OpenMP 5.1 Impl.): Mark 'omp allocate' as
    	implemented for C only.
    	* testsuite/libgomp.c/allocate-4.c: New test.
    	* testsuite/libgomp.c/allocate-5.c: New test.
    	* testsuite/libgomp.c/allocate-6.c: New test.
    
    gcc/testsuite/ChangeLog:
    
    	* c-c++-common/gomp/allocate-11.c: Remove C-only dg-message
    	for 'sorry, unimplemented'.
    	* c-c++-common/gomp/allocate-12.c: Likewise.
    	* c-c++-common/gomp/allocate-15.c: Likewise.
    	* c-c++-common/gomp/allocate-9.c: Likewise.
    	* c-c++-common/gomp/allocate-10.c: New test.
    	* c-c++-common/gomp/allocate-17.c: New test.

--- gcc/omp-low.cc
+++ gcc/omp-low.cc
@@ -3951,6 +3951,7 @@ scan_omp_1_op (tree *tp, int *walk_subtrees, void *data)
   struct walk_stmt_info *wi = (struct walk_stmt_info *) data;
   omp_context *ctx = (omp_context *) wi->info;
   tree t = *tp;
+  tree tmp;
 
   switch (TREE_CODE (t))
     {
@@ -3960,11 +3961,36 @@ scan_omp_1_op (tree *tp, int *walk_subtrees, void *data)
     case RESULT_DECL:
       if (ctx)
 	{
+	  tmp = NULL_TREE;
+	  if (TREE_CODE (t) == VAR_DECL
+	      && (tmp = lookup_attribute ("omp allocate var",
+					  DECL_ATTRIBUTES (t))) != NULL_TREE)
+	    t = TREE_VALUE (TREE_VALUE (tmp));
 	  tree repl = remap_decl (t, &ctx->cb);
 	  gcc_checking_assert (TREE_CODE (repl) != ERROR_MARK);
-	  *tp = repl;
+	  if (tmp != NULL_TREE  && t != repl)
+	    *tp = build_fold_addr_expr (repl);
+	  else if (tmp == NULL_TREE)
+	    *tp = repl;
 	}
       break;
+
+    case INDIRECT_REF:
+    case MEM_REF:
+      if (ctx
+	  && TREE_CODE (TREE_OPERAND (t, 0)) == VAR_DECL
+	  && ((tmp = lookup_attribute ("omp allocate var",
+				       DECL_ATTRIBUTES (TREE_OPERAND (t, 0))))
+	       != NULL_TREE))
+	{
+	  tmp = TREE_VALUE (TREE_VALUE (tmp));
+	  tree repl = remap_decl (tmp, &ctx->cb);
+	  gcc_checking_assert (TREE_CODE (repl) != ERROR_MARK);
+	  if (tmp != repl)
+	    *tp = repl;
+	  break;
+	}
+      gcc_fallthrough ();
 
     default:
       if (ctx && TYPE_P (t))
