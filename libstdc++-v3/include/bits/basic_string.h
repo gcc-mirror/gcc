28adad7a32ed92a3c0906b38411c4ed686bbacc5
Nathaniel Shead <nathanieloshead@gmail.com>
2023-09-29 10:30:41 +0100

    libstdc++: Ensure active union member is correctly set
    This patch ensures that the union members for std::string and
    std::variant are always properly set when a change occurs.
    
    libstdc++-v3/ChangeLog:
    
    	* include/bits/basic_string.h: (basic_string(basic_string&&)):
    	Activate _M_local_buf when needed.
    	(basic_string(basic_string&&, const _Alloc&)): Likewise.
    	* include/bits/basic_string.tcc: (basic_string::swap): Likewise.
    	* include/std/variant: (__detail::__variant::__construct_n): New.
    	(__detail::__variant::__emplace): Use __construct_n.
    
    Signed-off-by: Nathaniel Shead <nathanieloshead@gmail.com>

--- libstdc++-v3/include/bits/basic_string.h
+++ libstdc++-v3/include/bits/basic_string.h
@@ -678,6 +678,7 @@ _GLIBCXX_BEGIN_NAMESPACE_CXX11
       {
 	if (__str._M_is_local())
 	  {
+	    (void)_M_use_local_data();
 	    traits_type::copy(_M_local_buf, __str._M_local_buf,
 			      __str.length() + 1);
 	  }
@@ -691,7 +692,7 @@ _GLIBCXX_BEGIN_NAMESPACE_CXX11
 	// basic_stringbuf relies on writing into unallocated capacity so
 	// we mess up the contents if we put a '\0' in the string.
 	_M_length(__str.length());
-	__str._M_data(__str._M_local_data());
+	__str._M_data(__str._M_use_local_data());
 	__str._M_set_length(0);
       }
 
@@ -717,6 +718,7 @@ _GLIBCXX_BEGIN_NAMESPACE_CXX11
       {
 	if (__str._M_is_local())
 	  {
+	    (void)_M_use_local_data();
 	    traits_type::copy(_M_local_buf, __str._M_local_buf,
 			      __str.length() + 1);
 	    _M_length(__str.length());
@@ -728,7 +730,7 @@ _GLIBCXX_BEGIN_NAMESPACE_CXX11
 	    _M_data(__str._M_data());
 	    _M_length(__str.length());
 	    _M_capacity(__str._M_allocated_capacity);
-	    __str._M_data(__str._M_local_buf);
+	    __str._M_data(__str._M_use_local_data());
 	    __str._M_set_length(0);
 	  }
 	else
