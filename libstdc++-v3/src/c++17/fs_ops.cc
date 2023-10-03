7caac4e650f38f3bc341ef3513864605747af986
Jonathan Wakely <jwakely@redhat.com>
2023-10-03 16:08:41 +0100

    Revert "libstdc++: Horrible macro hacks to allow building on avr"
    This reverts commit 061700c9f6f0f9f4219ddeb236d55764b42869b6.
    
    This hack will be unnecessary, as I'm backporting the __unsupported()
    utility from the later branches.
    
    libstdc++-v3/ChangeLog:
    
    	* src/c++17/fs_ops.cc (not_supported):
    	* src/filesystem/ops-common.h (ENOTSUP):

--- libstdc++-v3/src/c++17/fs_ops.cc
+++ libstdc++-v3/src/c++17/fs_ops.cc
@@ -61,10 +61,6 @@
 #define _GLIBCXX_END_NAMESPACE_FILESYSTEM }
 #include "../filesystem/ops-common.h"
 
-#ifdef __AVR__
-# define not_supported function_not_supported
-#endif
-
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
 namespace fs = std::filesystem;
