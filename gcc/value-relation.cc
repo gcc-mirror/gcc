f0efc4b25cba1bd35b08b7dfbab0f8fc81b55c66
Andrew MacLeod <amacleod@redhat.com>
2023-10-09 13:40:15 -0400

    Ensure float equivalences include + and - zero.
    A floating point equivalence may not properly reflect both signs of
    zero, so be pessimsitic and ensure both signs are included.
    
    	PR tree-optimization/111694
    	gcc/
    	* gimple-range-cache.cc (ranger_cache::fill_block_cache): Adjust
    	equivalence range.
    	* value-relation.cc (adjust_equivalence_range): New.
    	* value-relation.h (adjust_equivalence_range): New prototype.
    
    	gcc/testsuite/
    	* gcc.dg/pr111694.c: New.

--- gcc/value-relation.cc
+++ gcc/value-relation.cc
@@ -183,6 +183,25 @@ relation_transitive (relation_kind r1, relation_kind r2)
   return relation_kind (rr_transitive_table[r1][r2]);
 }
 
+// When one name is an equivalence of another, ensure the equivalence
+// range is correct.  Specifically for floating point, a +0 is also
+// equivalent to a -0 which may not be reflected.  See PR 111694.
+
+void
+adjust_equivalence_range (vrange &range)
+{
+  if (range.undefined_p () || !is_a<frange> (range))
+    return;
+
+  frange fr = as_a<frange> (range);
+  REAL_VALUE_TYPE dconstm0 = dconst0;
+  dconstm0.sign = 1;
+  frange zeros (range.type (), dconstm0, dconst0);
+  // If range includes a 0 make sure both signs of zero are included.
+  if (fr.intersect (zeros) && !fr.undefined_p ())
+    range.union_ (zeros);
+ }
+
 // This vector maps a relation to the equivalent tree code.
 
 static const tree_code relation_to_code [VREL_LAST] = {
