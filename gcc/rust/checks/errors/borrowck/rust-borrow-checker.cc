3b51f04f140922fdd3d151215df7f55292a18eeb
Jakub Dupak <dev@jakubdupak.com>
2023-10-18 19:09:34 +0200

    gccrs: borrowck: Add initial structure for borrowchecking
    gcc/rust/ChangeLog:
    
    	* Make-lang.in: Build borrowck.
    	* checks/errors/borrowck/rust-borrow-checker.cc: New file.
    	* checks/errors/borrowck/rust-borrow-checker.h: New file.
    	* checks/errors/borrowck/rust-function-collector.h: New file.
    
    Signed-off-by: Jakub Dupak <dev@jakubdupak.com>

---
+++ gcc/rust/checks/errors/borrowck/rust-borrow-checker.cc
@@ -0,0 +1,43 @@
+// Copyright (C) 2020-2023 Free Software Foundation, Inc.
+
+// This file is part of GCC.
+
+// GCC is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3, or (at your option) any later
+// version.
+
+// GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with GCC; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include "rust-borrow-checker.h"
+#include "rust-function-collector.h"
+
+namespace Rust {
+namespace HIR {
+
+BorrowChecker::BorrowChecker () = default;
+
+void
+BorrowChecker::go (HIR::Crate &crate)
+{
+  FunctionCollector collector;
+  collector.go (crate);
+
+  for (auto func ATTRIBUTE_UNUSED : collector.get_functions ())
+    {
+    }
+
+  for (auto closure ATTRIBUTE_UNUSED : collector.get_closures ())
+    {
+    }
+}
+
+} // namespace HIR
+} // namespace Rust
