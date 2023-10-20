a663fe63889db486d577b20aff0730034400bfe9
David Edelsohn <dje.gcc@gmail.com>
2023-10-20 14:12:54 -0400

    bootstrap: Include tm_p.h
    ASM_GENERATE_INTERNAL_LABEL may use target-specific functions, so
    tm_p.h must be included.  This fixes bootstrap breakage on AIX, at least.
    
    gcc/ChangeLog
    
    	* gimple-harden-control-flow.cc: Include tm_p.h.
    
    Signed-off-by: David Edelsohn <dje.gcc@gmail.com>

--- gcc/gimple-harden-control-flow.cc
+++ gcc/gimple-harden-control-flow.cc
@@ -23,6 +23,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "system.h"
 #include "coretypes.h"
 #include "backend.h"
+#include "tm_p.h"
 #include "tree.h"
 #include "fold-const.h"
 #include "gimple.h"
