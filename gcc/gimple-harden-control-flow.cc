8f75e6cf583cf8db16334c0ba323801cbc9f4e7f
David Edelsohn <dje.gcc@gmail.com>
2023-10-20 15:51:51 -0400

    bootstrap: tm_p.h requires memmodel.h on SPARC.
    SPARC target header references memmodel, which requires memmodel.h.
    
    gcc/ChangeLog:
    
    	* gimple-harden-control-flow.cc: Include memmodel.h.
    
    Signed-off-by: David Edelsohn <dje.gcc@gmail.com>

--- gcc/gimple-harden-control-flow.cc
+++ gcc/gimple-harden-control-flow.cc
@@ -23,6 +23,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "system.h"
 #include "coretypes.h"
 #include "backend.h"
+#include "memmodel.h"
 #include "tm_p.h"
 #include "tree.h"
 #include "fold-const.h"
