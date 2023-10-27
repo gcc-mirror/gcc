f0d163da425049242cc327d7036873040572836a
Tobias Burnus <tobias@codesourcery.com>
2023-10-27 12:41:31 +0200

    OpenMP: Avoid ICE with LTO and 'omp allocate'
    gcc/ChangeLog:
    
    	* gimplify.cc (gimplify_bind_expr): Remove "omp allocate" attribute
    	to avoid that auxillary statement list reaches LTO.
    
    gcc/testsuite/ChangeLog:
    
    	* gfortran.dg/gomp/allocate-13a.f90: New test.
    
    (cherry picked from commit af4bb221153359f5948da917d5ef2df738bb1e61)

---
+++ gcc/testsuite/gfortran.dg/gomp/allocate-13a.f90
@@ -0,0 +1,34 @@
+! { dg-do compile { target lto } }
+! { dg-additional-options "-flto" }
+
+! Same as allocate-13.f90 but compiled with -flto.
+
+! This was failing before as the statement list,
+! used for placing the GOMP_alloc/GOMP_free leaked
+! through to LTO.
+
+module m
+  implicit none
+  !$omp requires dynamic_allocators
+contains
+subroutine f ()
+  !$omp declare target
+  integer :: var
+  !$omp allocate(var)
+  var = 5
+end
+
+subroutine h ()
+  !$omp target
+   !$omp parallel
+    !$omp single
+      block
+       integer :: var2(5)
+       !$omp allocate(var2)
+       var2(1) = 7
+      end block
+    !$omp end single
+   !$omp end parallel
+  !$omp end target
+end
+end module
