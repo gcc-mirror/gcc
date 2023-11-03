c034b8f53243175f04935fd653ce15bd7700495c
Richard Earnshaw <rearnsha@arm.com>
2023-11-03 18:11:10 +0000

    arm: testsuite: improve compatibility of pr88648-asm-syntax-unified.c
    Fix another test that was trying to set the architecture directly
    rather than using the infrastructure as intended.
    
    gcc/testsuite:
    
    	* gcc.target/arm/pr88648-asm-syntax-unified.c: It isn't necessary
    	to try to override the architecture flags specified by arm_arch_v7a.

--- gcc/testsuite/gcc.target/arm/pr88648-asm-syntax-unified.c
+++ gcc/testsuite/gcc.target/arm/pr88648-asm-syntax-unified.c
@@ -1,8 +1,8 @@
 /* Test for unified syntax assembly generation.  */
 /* { dg-do compile } */
 /* { dg-require-effective-target arm_arch_v7a_ok } */
+/* { dg-options "-marm -masm-syntax-unified" } */
 /* { dg-add-options arm_arch_v7a } */
-/* { dg-options "-marm -march=armv7-a -masm-syntax-unified" } */
 
 void test ()
 {
