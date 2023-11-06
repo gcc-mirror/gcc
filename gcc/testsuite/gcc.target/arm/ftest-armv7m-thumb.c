b9bed256c9772bcc13de6760919ecc963b865c32
Richard Earnshaw <rearnsha@arm.com>
2023-11-06 14:57:26 +0000

    arm: testsuite: improve compatibility of ftest-armv7m-thumb.c
    This test is specific to armv7m cores which do not support hardware
    floating-point.  We can improve its compatibility by having the default
    options for this core specify -mfloat-abi=soft.
    
    gcc/testsuite:
    
    	* lib/target-supports.exp (check_effective_target_arm_arch_FUNC_ok):
    	Use soft-float ABI for armv7m.
    	* gcc.target/arm/ftest-armv7m-thumb.c: Use dg-require-effective-target
    	to check flag compatibility.

--- gcc/testsuite/gcc.target/arm/ftest-armv7m-thumb.c
+++ gcc/testsuite/gcc.target/arm/ftest-armv7m-thumb.c
@@ -1,6 +1,5 @@
 /* { dg-do compile } */
-/* { dg-skip-if "avoid conflicting multilib options" { *-*-* } { "-march=*" } { "-march=arm7-m" } } */
-/* { dg-skip-if "avoid conflicting multilib options" { *-*-* } { "-marm" } { "" } } */
+/* { dg-require-effective-target arm_arch_v7m_ok }
 /* { dg-options "-mthumb" } */
 /* { dg-add-options arm_arch_v7m } */
 
