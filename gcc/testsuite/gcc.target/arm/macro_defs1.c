5bc424925a3678d7821ab3512f49a66e98085768
Richard Earnshaw <rearnsha@arm.com>
2023-11-06 15:26:20 +0000

    arm: testsuite: improve compatibility of gcc.target/arm/macro_defs*.c
    Convert these tests to use dg-add-options for increased compatibilty.
    Since they also result in an empty translation unit, override the
    default testsuite options.
    
    gcc/testsuite:
    
    	* gcc.target/arm/macro_defs0.c: Use dg-effective-target and
    	dg-add-options.
    	* gcc.target/arm/macro_defs1.c: Likewise.
    	* gcc.target/arm/macro_defs2.c: Likewise.

--- gcc/testsuite/gcc.target/arm/macro_defs1.c
+++ gcc/testsuite/gcc.target/arm/macro_defs1.c
@@ -1,10 +1,8 @@
 /* { dg-do compile } */
-/* { dg-skip-if "avoid conflicting multilib options" { *-*-* } { "-march=*" } { "-march=armv6-m" } } */
-/* { dg-skip-if "avoid conflicting multilib options" { *-*-* } { "-marm" } { "" } } */
 /* { dg-require-effective-target arm_arch_v6m_ok } */
-/* { dg-options "-march=armv6-m -mthumb" } */
+/* { dg-options "" } */
+/* { dg-add-options arm_arch_v6m } */
 
 #ifdef __ARM_NEON_FP
 #error __ARM_NEON_FP should not be defined
 #endif
-
