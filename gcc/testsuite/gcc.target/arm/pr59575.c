47b3f2c84dea6460b1cd14b5dbca4c2ad609f2ca
Richard Earnshaw <rearnsha@arm.com>
2023-11-06 17:04:03 +0000

    arm: testsuite: improve compatibility of gcc.target/arm/pr59575.c
    Use dg-require-effective-target/dg-add-options to improve
    compatibility of this test with various compiler configurations.
    
    gcc/testsuite:
    
    	* gcc.target/arm/pr59575.c: Use dg-require-effective-target and
    	dg-add-options.

--- gcc/testsuite/gcc.target/arm/pr59575.c
+++ gcc/testsuite/gcc.target/arm/pr59575.c
@@ -1,7 +1,9 @@
 /* PR target/59575 */
 /* { dg-do compile } */
+/* { dg-require-effective-target arm_arch_v7a_ok } */
 /* { dg-skip-if "-mpure-code supports M-profile only" { *-*-* } { "-mpure-code" } } */
-/* { dg-options "-Os -g -march=armv7-a" } */
+/* { dg-options "-Os -g" } */
+/* { dg-add-options arm_arch_v7a } */
 
 void foo (int *);
 int *bar (int, long long, int);
