0b94e9cc060906806d1aec5e19498dece5e2e729
Marek Polacek <polacek@redhat.com>
2023-11-07 10:41:41 -0500

    testsuite: nodiscard-reason-nonstring.C FAIL in C++26
    Since r14-5071, we emit an extra error for this test (the first one):
    
    nodiscard-reason-nonstring.C:5:13: error: expected string-literal before numeric constant
    nodiscard-reason-nonstring.C:5:36: error: 'nodiscard' attribute argument must be a string constant
    
    so the test needs adjusting.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp2a/nodiscard-reason-nonstring.C: Adjust dg-error.

--- gcc/testsuite/g++.dg/cpp2a/nodiscard-reason-nonstring.C
+++ gcc/testsuite/g++.dg/cpp2a/nodiscard-reason-nonstring.C
@@ -2,7 +2,7 @@
 /* { dg-do compile { target c++20 } } */
 /* { dg-options "-O" } */
 
-[[nodiscard(123)]] int check1 (void); /* { dg-error "nodiscard\[^\n\r]*must be a string constant" } */
+[[nodiscard(123)]] int check1 (void); /* { dg-error "nodiscard\[^\n\r]*must be a string constant|expected string-literal" } */
 
 void
 test (void)
