71b1efd910d47362558defc31b92fc4cc80c0ec6
Lehua Ding <lehua.ding@rivai.ai>
2023-11-07 15:33:20 +0800

    RISC-V: Fixed failed rvv combine testcases
    This patch fixed the fellowing failed testcases on the trunk:
    FAIL: gcc.target/riscv/rvv/autovec/cond/cond_widen_reduc-2.c scan-assembler-times \\tvfwredusum\\.vs\\tv[0-9]+,v[0-9]+,v[0-9]+,v0\\.t 2
    ...
    FAIL: gcc.target/riscv/rvv/autovec/cond/cond_widen_reduc-2.c scan-assembler-times \\tvwredsumu\\.vs\\tv[0-9]+,v[0-9]+,v[0-9]+,v0\\.t 3
    ...
    
    The reason for these failed testcases is the introduce of .VCOND_MASK_LEN
    in midend for other bugfix and further leads to a new vcond_mask_len rtl
    pattern after expand. So we need add new combine patterns handle this case.
    
    Consider this code:
    
    int16_t foo (int8_t *restrict a, int8_t *restrict pred)
    {
      int16_t sum = 0;
      for (int i = 0; i < 16; i += 1)
        if (pred[i])
          sum += a[i];
      return sum;
    }
    
    Before this patch:
    foo:
            vsetivli        zero,16,e8,m1,ta,ma
            vle8.v  v0,0(a1)
            vsetvli a5,zero,e8,m1,ta,ma
            vmsne.vi        v0,v0,0
            vsetvli zero,zero,e16,m2,ta,ma
            li      a3,0
            vmv.v.i v2,0
            vsetivli        zero,16,e16,m2,ta,ma
            vle8.v  v6,0(a0),v0.t
            vmv.s.x v1,a3
            vsetvli a5,zero,e16,m2,ta,ma
            vsext.vf2       v4,v6
            vsetivli        zero,16,e16,m2,tu,ma
            vmerge.vvm      v2,v2,v4,v0
            vsetvli a5,zero,e16,m2,ta,ma
            vredsum.vs      v2,v2,v1
            vmv.x.s a0,v2
            slliw   a0,a0,16
            sraiw   a0,a0,16
            ret
    
    After this patch:
    foo:
    	vsetivli	zero,16,e16,m2,ta,ma
    	li	a5,0
    	vle8.v	v0,0(a1)
    	vmv.s.x	v1,a5
    	vsetvli	zero,zero,e8,m1,ta,ma
    	vmsne.vi	v0,v0,0
    	vle8.v	v2,0(a0),v0.t
    	vwredsum.vs	v1,v2,v1,v0.t
    	vsetvli	zero,zero,e16,m1,ta,ma
    	vmv.x.s	a0,v1
    	slliw	a0,a0,16
    	sraiw	a0,a0,16
    	ret
    
    Combine the vsext.vf2, vmerge.vvm, and vredsum.vs instructions while
    reducing the corresponding vsetvl instructions.
    
    gcc/ChangeLog:
    
    	* config/riscv/autovec-opt.md (*cond_len_<optab><v_double_trunc><mode>):
    	New combine pattern.
    	(*cond_len_<optab><v_quad_trunc><mode>): Ditto.
    	(*cond_len_<optab><v_oct_trunc><mode>): Ditto.
    	(*cond_len_extend<v_double_trunc><mode>): Ditto.
    	(*cond_len_widen_reduc_plus_scal_<mode>): Ditto.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/cond/cond_widen_reduc-1.c:
    	* gcc.target/riscv/rvv/autovec/cond/cond_widen_reduc-2.c:

--- gcc/testsuite/gcc.target/riscv/rvv/autovec/cond/cond_widen_reduc-1.c
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/cond/cond_widen_reduc-1.c
@@ -15,16 +15,27 @@
 
 #define TEST_ALL(TEST)                                                         \
   TEST (int16_t, int8_t, 16)                                                   \
+  TEST (int32_t, int8_t, 8)                                                    \
   TEST (int32_t, int16_t, 8)                                                   \
+  TEST (int64_t, int8_t, 4)                                                    \
+  TEST (int64_t, int16_t, 4)                                                   \
   TEST (int64_t, int32_t, 4)                                                   \
   TEST (uint16_t, uint8_t, 16)                                                 \
+  TEST (uint32_t, uint8_t, 8)                                                  \
   TEST (uint32_t, uint16_t, 8)                                                 \
+  TEST (uint64_t, uint8_t, 4)                                                  \
+  TEST (uint64_t, uint16_t, 4)                                                 \
   TEST (uint64_t, uint32_t, 4)                                                 \
   TEST (float, _Float16, 8)                                                    \
+  TEST (double, _Float16, 4)                                                   \
   TEST (double, float, 4)
 
 TEST_ALL (TEST_TYPE)
 
-/* { dg-final { scan-assembler-times {\tvfwredusum\.vs\tv[0-9]+,v[0-9]+,v[0-9]+,v0\.t} 2 } } */
+/* { dg-final { scan-assembler-times {\tvfwredusum\.vs\tv[0-9]+,v[0-9]+,v[0-9]+,v0\.t} 3 } } */
 /* { dg-final { scan-assembler-times {\tvwredsum\.vs\tv[0-9]+,v[0-9]+,v[0-9]+,v0\.t} 3 } } */
 /* { dg-final { scan-assembler-times {\tvwredsumu\.vs\tv[0-9]+,v[0-9]+,v[0-9]+,v0\.t} 3 } } */
+/* { dg-final { scan-assembler-times {\tvsext\.vf4\tv[0-9]+,v[0-9]+,v0\.t} 2 } } */
+/* { dg-final { scan-assembler-times {\tvsext\.vf8\tv[0-9]+,v[0-9]+,v0\.t} 1 } } */
+/* { dg-final { scan-assembler-times {\tvzext\.vf4\tv[0-9]+,v[0-9]+,v0\.t} 2 } } */
+/* { dg-final { scan-assembler-times {\tvzext\.vf8\tv[0-9]+,v[0-9]+,v0\.t} 1 } } */
