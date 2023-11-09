f88b78b1196d71042cf129be7a0bf02b9a4de0d3
Florian Weimer <fweimer@redhat.com>
2023-11-09 09:50:53 +0100

    gcc.dg/Wmissing-parameter-type*: Test the intended warning
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/Wmissing-parameter-type.c: Build with -std=gnu89
    	to trigger the -Wmissing-parameter-type warning
    	and not the default -Wimplicit warning.  Also match
    	against -Wmissing-parameter-type.
    	* gcc.dg/Wmissing-parameter-type-Wextra.c: Likewise.

--- gcc/testsuite/gcc.dg/Wmissing-parameter-type-Wextra.c
+++ gcc/testsuite/gcc.dg/Wmissing-parameter-type-Wextra.c
@@ -1,7 +1,7 @@
 /* Test -Wmissing-parameter-type is enabled by -Wextra */
 /* { dg-do compile } */
-/* { dg-options "-Wextra" } */
+/* { dg-options "-std=gnu89 -Wextra" } */
 
-int foo(bar) { return bar;} /* { dg-warning "type of 'bar' defaults to 'int'" } */
+int foo(bar) { return bar;} /* { dg-warning "type of 'bar' defaults to 'int' \\\[-Wmissing-parameter-type\\\]" } */
 
 
