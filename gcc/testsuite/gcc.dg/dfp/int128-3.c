f172b9d38db426d2b102c0f9c1fd58672acc6c9b
Jakub Jelinek <jakub@redhat.com>
2023-11-09 09:14:07 +0100

    libgcc: Add {unsigned ,}__int128 <-> _Decimal{32,64,128} conversion support [PR65833]
    The following patch adds the missing
    {unsigned ,}__int128 <-> _Decimal{32,64,128}
    conversion support into libgcc.a on top of the _BitInt support
    (doing it without that would be larger amount of code and I hope all
    the targets which support __int128 will eventually support _BitInt,
    after all it is a required part of C23) and because it is in libgcc.a
    only, it doesn't hurt that much if it is added for some architectures
    only in GCC 15.
    Initially I thought about doing this on the compiler side, but doing
    it on the library side seems to be easier and more -Os friendly.
    The tests currently require bitint effective target, that can be
    removed when all the int128 targets support bitint.
    
    2023-11-09  Jakub Jelinek  <jakub@redhat.com>
    
    	PR libgcc/65833
    libgcc/
    	* config/t-softfp (softfp_bid_list): Add
    	{U,}TItype <-> _Decimal{32,64,128} conversions.
    	* soft-fp/floattisd.c: New file.
    	* soft-fp/floattidd.c: New file.
    	* soft-fp/floattitd.c: New file.
    	* soft-fp/floatuntisd.c: New file.
    	* soft-fp/floatuntidd.c: New file.
    	* soft-fp/floatuntitd.c: New file.
    	* soft-fp/fixsdti.c: New file.
    	* soft-fp/fixddti.c: New file.
    	* soft-fp/fixtdti.c: New file.
    	* soft-fp/fixunssdti.c: New file.
    	* soft-fp/fixunsddti.c: New file.
    	* soft-fp/fixunstdti.c: New file.
    gcc/testsuite/
    	* gcc.dg/dfp/int128-1.c: New test.
    	* gcc.dg/dfp/int128-2.c: New test.
    	* gcc.dg/dfp/int128-3.c: New test.
    	* gcc.dg/dfp/int128-4.c: New test.

---
+++ gcc/testsuite/gcc.dg/dfp/int128-3.c
@@ -0,0 +1,81 @@
+/* PR libgcc/65833 */
+/* Test non-canonical BID significands.  */
+/* { dg-do run { target { int128 && bitint } } } */
+/* { dg-options "-O2 -std=gnu2x" } */
+/* { dg-require-effective-target dfp_bid } */
+
+union U32
+{
+  _Decimal32 d;
+  unsigned int u;
+};
+
+union U64
+{
+  _Decimal64 d;
+  unsigned long long int u;
+};
+
+union U128
+{
+  _Decimal128 d;
+  unsigned long long int u[2];
+};
+
+int
+main ()
+{
+  volatile union U32 u32;
+  u32.d = 0.9999999e+27DF;
+  u32.u++;
+  volatile union U64 u64;
+  u64.d = 0.9999999999999999e+90DD;
+  u64.u++;
+  volatile union U128 u128;
+  u128.d = 0.9999999999999999999999999999999999e+39DL;
+  if (u128.u[0] == 0x378d8e63ffffffffULL)
+    u128.u[0]++;
+  else if (u128.u[1] == 0x378d8e63ffffffffULL)
+    u128.u[1]++;
+  else
+    u128.d = 0.DL;
+  if ((__int128) u32.d != 0
+      || (unsigned __int128) u32.d != 0U
+      || (__int128) u64.d != 0
+      || (unsigned __int128) u64.d != 0U
+      || (__int128) u128.d != 0
+      || (unsigned __int128) u128.d != 0U)
+    __builtin_abort ();
+  u32.u = 0xe59fffffU;
+  u64.u = 0xe3ffffffffffffffULL;
+  if (u128.u[0] == 0x378d8e6400000000ULL)
+    {
+      u128.u[0] = -1ULL;
+      u128.u[1] = 0xe1be7fffffffffffULL;
+    }
+  else if (u128.u[1] == 0x378d8e6400000000ULL)
+    {
+      u128.u[1] = -1ULL;
+      u128.u[0] = 0xe1be7fffffffffffULL;
+    }
+  if ((__int128) u32.d != 0
+      || (unsigned __int128) u32.d != 0U
+      || (__int128) u64.d != 0
+      || (unsigned __int128) u64.d != 0U
+      || (__int128) u128.d != 0
+      || (unsigned __int128) u128.d != 0U)
+    __builtin_abort ();
+  if (u128.u[0] == -1ULL)
+    {
+      u128.u[0] = 0;
+      u128.u[1] = 0xe629800000000000ULL;
+    }
+  else if (u128.u[1] == -1ULL)
+    {
+      u128.u[1] = 0;
+      u128.u[0] = 0xe629800000000000ULL;
+    }
+  if ((__int128) u128.d != 0
+      || (unsigned __int128) u128.d != 0U)
+    __builtin_abort ();
+}
