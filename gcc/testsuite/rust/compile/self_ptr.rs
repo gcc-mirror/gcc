9735aca65a40fe1f45ba4c63f8a1d0afca4a3885
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2023-11-15 13:53:23 +0100

    Add new test for parsing errors on self pointers
    Add new tests to highlight the behavior of errors thrown when meeting a
    self pointer.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/self_const_ptr.rs: New test.
    	* rust/compile/self_mut_ptr.rs: New test.
    	* rust/compile/self_ptr.rs: New test.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

---
+++ gcc/testsuite/rust/compile/self_ptr.rs
@@ -0,0 +1,8 @@
+struct MyStruct;
+
+impl MyStruct {
+    pub fn do_something(*self) {}
+    // { dg-error "cannot pass .self. by raw pointer" "" { target *-*-* } .-1 }
+    // { dg-error "failed to parse inherent impl item in inherent impl" "" { target *-*-* } .-2 }
+    // { dg-error "failed to parse item in crate" "" { target *-*-* } .-3 }
+}
