cbd0fe22a5ced9751d2450dc4fd6fe3525c2fc02
Jonathan Wakely <jwakely@redhat.com>
2023-11-15 09:17:49 +0000

    libstdc++: std::stacktrace tweaks
    Fix a typo in a string literal and make the new hash.cc test gracefully
    handle missing stacktrace data (see PR 112541).
    
    libstdc++-v3/ChangeLog:
    
    	* include/std/stacktrace (basic_stacktrace::at): Fix class name
    	in exception message.
    	* testsuite/19_diagnostics/stacktrace/hash.cc: Do not fail if
    	current() returns a non-empty stacktrace.

--- libstdc++-v3/testsuite/19_diagnostics/stacktrace/hash.cc
+++ libstdc++-v3/testsuite/19_diagnostics/stacktrace/hash.cc
@@ -12,9 +12,10 @@ test_hash()
   using Alloc = __gnu_test::uneq_allocator<std::stacktrace_entry>;
   using S = std::basic_stacktrace<Alloc>;
   S s;
+  S cur = S::current();
   std::size_t h = std::hash<S>()(s);
-  std::size_t h2 = std::hash<S>()(S::current());
-  VERIFY( h != h2 );
+  std::size_t h2 = std::hash<S>()(cur);
+  VERIFY( cur.empty() == (h == h2) );
 }
 
 int main()
