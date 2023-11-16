9bd6ee8a82e53ae7e3cb8c83830b3b3e106e2dc0
Thomas Schwinge <thomas@codesourcery.com>
2023-11-16 23:17:36 +0100

    GCN: Generally enable the 'gcc.target/gcn/avgpr-[...]' test cases
    ... added in commit ae0d2c240213c5a7f6959c032bfc9f0703cab787
    "amdgcn: Add Accelerator VGPR registers".  This way, they're correctly tested
    no matter what '-march=[...]' is used with 'make check'.
    
    	gcc/testsuite/
    	* gcc.target/gcn/avgpr-mem-double.c: Remove
    	'dg-skip-if "incompatible ISA" [...]'.
    	* gcc.target/gcn/avgpr-mem-int.c: Likewise.
    	* gcc.target/gcn/avgpr-mem-long.c: Likewise.
    	* gcc.target/gcn/avgpr-mem-short.c: Likewise.
    	* gcc.target/gcn/avgpr-spill-double.c: Likewise.
    	* gcc.target/gcn/avgpr-spill-int.c: Likewise.
    	* gcc.target/gcn/avgpr-spill-long.c: Likewise.
    	* gcc.target/gcn/avgpr-spill-short.c: Likewise.

--- gcc/testsuite/gcc.target/gcn/avgpr-mem-double.c
+++ gcc/testsuite/gcc.target/gcn/avgpr-mem-double.c
@@ -1,6 +1,5 @@
 /* { dg-do compile } */
 /* { dg-additional-options "-march=gfx90a -O1" } */
-/* { dg-skip-if "incompatible ISA" { *-*-* } { "-march=gfx90[068]" } } */
 /* { dg-final { scan-assembler {load[^\n]*a[0-9[]} } } */
 /* { dg-final { scan-assembler {store[^\n]*a[0-9[]} } } */
 
--- gcc/testsuite/gcc.target/gcn/avgpr-spill-int.c
+++ gcc/testsuite/gcc.target/gcn/avgpr-spill-int.c
@@ -1,6 +1,5 @@
 /* { dg-do compile } */
 /* { dg-additional-options "-march=gfx908 -O1" } */
-/* { dg-skip-if "incompatible ISA" { *-*-* } { "-march=gfx90[06]" } } */
 /* { dg-final { scan-assembler "accvgpr" } } */
 
 #ifndef TYPE
