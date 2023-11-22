0f4ce86eebd031d1d8ad5bd8fc92333030ce56a1
Maciej W. Rozycki <macro@embecosm.com>
2023-11-22 01:18:30 +0000

    RISC-V/testsuite: Add branchless cases for generic FP cond adds
    Verify, for generic floating-point conditional-add operations that have
    a corresponding conditional-set machine instruction, that if-conversion
    triggers via `noce_try_addcc' at `-mbranch-cost=3' setting, which makes
    branchless code sequences emitted by if-conversion cheaper than their
    original branched equivalents, and that extraneous instructions such as
    SNEZ, etc. are not present in output.
    
    The reason to XFAIL SImode tests for RV64 targets is the compiler thinks
    it has to sign-extend addends, which causes if-conversion to give up.
    
    	gcc/testsuite/
    	* gcc.target/riscv/adddifeq.c: New test.
    	* gcc.target/riscv/adddifge.c: New test.
    	* gcc.target/riscv/adddifgt.c: New test.
    	* gcc.target/riscv/adddifle.c: New test.
    	* gcc.target/riscv/adddiflt.c: New test.
    	* gcc.target/riscv/addsifeq.c: New test.
    	* gcc.target/riscv/addsifge.c: New test.
    	* gcc.target/riscv/addsifgt.c: New test.
    	* gcc.target/riscv/addsifle.c: New test.
    	* gcc.target/riscv/addsiflt.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/adddiflt.c
@@ -0,0 +1,26 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target rv64 } */
+/* { dg-skip-if "" { *-*-* } { "-O0" "-Og" "-Os" "-Oz" } } */
+/* { dg-options "-march=rv64gc -mtune=sifive-5-series -mbranch-cost=3 -mmovcc -ffinite-math-only -fdump-rtl-ce1" } */
+
+typedef int __attribute__ ((mode (DI))) int_t;
+
+int_t
+adddiflt (double w, double x, int_t y, int_t z)
+{
+  return w < x ? y + z : y;
+}
+
+/* Expect branchless assembly like:
+
+	flt.d	a5,fa0,fa1
+	neg	a5,a5
+	and	a5,a5,a1
+	add	a0,a5,a0
+ */
+
+/* { dg-final { scan-rtl-dump-times "Conversion succeeded on pass 1\\." 1 "ce1" } } */
+/* { dg-final { scan-rtl-dump-times "if-conversion succeeded through noce_try_addcc" 1 "ce1" } } */
+/* { dg-final { scan-assembler-times "\\s(?:fge\\.d|fgt\\.d|fle\\.d|flt\\.d)\\s" 1 } } */
+/* { dg-final { scan-assembler-not "\\s(?:seqz|snez)\\s" } } */
+/* { dg-final { scan-assembler-not "\\s(?:beq|bne)\\s" } } */
