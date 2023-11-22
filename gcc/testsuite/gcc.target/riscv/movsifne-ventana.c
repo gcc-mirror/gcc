814485b2564a453ce9c0731344282f18e12ed0b5
Maciej W. Rozycki <macro@embecosm.com>
2023-11-22 01:18:25 +0000

    RISC-V/testsuite: Add branchless cases for FP cond-move operations
    Verify, for short forward branch, T-Head, Ventana and Zicond targets and
    the ordered floating-point conditional-move operations that already work
    as expected, that if-conversion triggers via `noce_try_cmove' at the
    respective sufficiently high `-mbranch-cost=' settings that make
    branchless code sequences produced by if-conversion cheaper than their
    original branched equivalents, and that extraneous instructions such as
    SNEZ, etc. are not present in output.  Cover all ordered floating-point
    relational operations to make sure no corner case escapes.
    
    	gcc/testsuite/
    	* gcc.target/riscv/movdifge-sfb.c: New test.
    	* gcc.target/riscv/movdifge-thead.c: New test.
    	* gcc.target/riscv/movdifge-ventana.c: New test.
    	* gcc.target/riscv/movdifge-zicond.c: New test.
    	* gcc.target/riscv/movdifgt-sfb.c: New test.
    	* gcc.target/riscv/movdifgt-thead.c: New test.
    	* gcc.target/riscv/movdifgt-ventana.c: New test.
    	* gcc.target/riscv/movdifgt-zicond.c: New test.
    	* gcc.target/riscv/movdifle-sfb.c: New test.
    	* gcc.target/riscv/movdifle-thead.c: New test.
    	* gcc.target/riscv/movdifle-ventana.c: New test.
    	* gcc.target/riscv/movdifle-zicond.c: New test.
    	* gcc.target/riscv/movdiflt-sfb.c: New test.
    	* gcc.target/riscv/movdiflt-thead.c: New test.
    	* gcc.target/riscv/movdiflt-ventana.c: New test.
    	* gcc.target/riscv/movdiflt-zicond.c: New test.
    	* gcc.target/riscv/movdifne-sfb.c: New test.
    	* gcc.target/riscv/movdifne-thead.c: New test.
    	* gcc.target/riscv/movdifne-ventana.c: New test.
    	* gcc.target/riscv/movdifne-zicond.c: New test.
    	* gcc.target/riscv/movsifge-sfb.c: New test.
    	* gcc.target/riscv/movsifge-thead.c: New test.
    	* gcc.target/riscv/movsifge-ventana.c: New test.
    	* gcc.target/riscv/movsifge-zicond.c: New test.
    	* gcc.target/riscv/movsifgt-sfb.c: New test.
    	* gcc.target/riscv/movsifgt-thead.c: New test.
    	* gcc.target/riscv/movsifgt-ventana.c: New test.
    	* gcc.target/riscv/movsifgt-zicond.c: New test.
    	* gcc.target/riscv/movsifle-sfb.c: New test.
    	* gcc.target/riscv/movsifle-thead.c: New test.
    	* gcc.target/riscv/movsifle-ventana.c: New test.
    	* gcc.target/riscv/movsifle-zicond.c: New test.
    	* gcc.target/riscv/movsiflt-sfb.c: New test.
    	* gcc.target/riscv/movsiflt-thead.c: New test.
    	* gcc.target/riscv/movsiflt-ventana.c: New test.
    	* gcc.target/riscv/movsiflt-zicond.c: New test.
    	* gcc.target/riscv/movsifne-sfb.c: New test.
    	* gcc.target/riscv/movsifne-thead.c: New test.
    	* gcc.target/riscv/movsifne-ventana.c: New test.
    	* gcc.target/riscv/movsifne-zicond.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/movsifne-ventana.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target rv64 } */
+/* { dg-skip-if "" { *-*-* } { "-O0" "-Og" "-Os" "-Oz" } } */
+/* { dg-options "-march=rv64gc_xventanacondops -mtune=rocket -mbranch-cost=3 -fdump-rtl-ce1" } */
+
+typedef int __attribute__ ((mode (SI))) int_t;
+
+int_t
+movsifne (double w, double x, int_t y, int_t z)
+{
+  return w != x ? y : z;
+}
+
+/* Expect branchless assembly like:
+
+	feq.d	a5,fa0,fa1
+	vt.maskc	a1,a1,a5
+	vt.maskcn	a0,a0,a5
+	or	a0,a0,a1
+ */
+
+/* { dg-final { scan-rtl-dump-times "Conversion succeeded on pass 1\\." 1 "ce1" } } */
+/* { dg-final { scan-rtl-dump-times "if-conversion succeeded through noce_try_cmove" 1 "ce1" } } */
+/* { dg-final { scan-assembler-times "\\sfeq\\.d\\s" 1 } } */
+/* { dg-final { scan-assembler-times "\\svt\\.maskc\\s" 1 } } */
+/* { dg-final { scan-assembler-times "\\svt\\.maskcn\\s" 1 } } */
+/* { dg-final { scan-assembler-not "\\s(?:beq|bne)\\s" } } */
+/* { dg-final { scan-assembler-not "\\s(?:seqz|snez)\\s" } } */
