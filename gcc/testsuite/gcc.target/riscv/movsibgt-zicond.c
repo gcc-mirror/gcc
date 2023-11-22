dcf4395fc67a6fc83e50329d92bea82b2fdf4254
Maciej W. Rozycki <macro@embecosm.com>
2023-11-22 01:18:25 +0000

    RISC-V/testsuite: Add branched cases for integer cond-move operations
    Verify, for T-Head, Ventana and Zicond targets and the integer
    conditional-move operations that already work as expected, that
    if-conversion does *not* trigger at the respective sufficiently low
    `-mbranch-cost=' settings that make original branched code sequences
    cheaper than their branchless equivalents if-conversion would emit.
    Cover all integer relational operations to make sure no corner case
    escapes.
    
    The reason to XFAIL movdibne-thead.c and movsibne-thead.c is the
    branchless T-Head sequence:
    
    	sub	a1,a0,a1
    	th.mveqz	a2,a3,a1
    	mv	a0,a2
    	ret
    
    produced rather than its original branched counterpart:
    
    	beq	a0,a1,.L3
    	mv	a0,a2
    	ret
    .L3:
    	mv	a0,a3
    	ret
    
    at `-mbranch-cost=1', even though under this setting the latter sequence
    is obviously cheaper performance-wise.  This is because the final move
    instruction in the branchless sequence is not counted towards its cost
    and consequently the cost of both sequences works out at 8 each, making
    if-conversion prefer the branchless variant.  Use the XFAIL mark to keep
    track of these cases for future consideration.
    
    	gcc/testsuite/
    	* gcc.target/riscv/movdibeq-thead.c: New test.
    	* gcc.target/riscv/movdibge-ventana.c: New test.
    	* gcc.target/riscv/movdibge-zicond.c: New test.
    	* gcc.target/riscv/movdibgeu-ventana.c: New test.
    	* gcc.target/riscv/movdibgeu-zicond.c: New test.
    	* gcc.target/riscv/movdibgt-ventana.c: New test.
    	* gcc.target/riscv/movdibgt-zicond.c: New test.
    	* gcc.target/riscv/movdible-ventana.c: New test.
    	* gcc.target/riscv/movdible-zicond.c: New test.
    	* gcc.target/riscv/movdibleu-ventana.c: New test.
    	* gcc.target/riscv/movdibleu-zicond.c: New test.
    	* gcc.target/riscv/movdiblt-ventana.c: New test.
    	* gcc.target/riscv/movdiblt-zicond.c: New test.
    	* gcc.target/riscv/movdibne-thead.c: New test.
    	* gcc.target/riscv/movsibeq-thead.c: New test.
    	* gcc.target/riscv/movsibge-ventana.c: New test.
    	* gcc.target/riscv/movsibge-zicond.c: New test.
    	* gcc.target/riscv/movsibgeu-ventana.c: New test.
    	* gcc.target/riscv/movsibgeu-zicond.c: New test.
    	* gcc.target/riscv/movsibgt-ventana.c: New test.
    	* gcc.target/riscv/movsibgt-zicond.c: New test.
    	* gcc.target/riscv/movsible-ventana.c: New test.
    	* gcc.target/riscv/movsible-zicond.c: New test.
    	* gcc.target/riscv/movsibleu-ventana.c: New test.
    	* gcc.target/riscv/movsibleu-zicond.c: New test.
    	* gcc.target/riscv/movsiblt-ventana.c: New test.
    	* gcc.target/riscv/movsiblt-zicond.c: New test.
    	* gcc.target/riscv/movsibne-thead.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/movsibgt-zicond.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+/* { dg-skip-if "" { *-*-* } { "-O0" "-Og" "-Os" "-Oz" } } */
+/* { dg-options "-march=rv64gc_zicond -mtune=rocket -mbranch-cost=3 -fdump-rtl-ce1" { target { rv64 } } } */
+/* { dg-options "-march=rv32gc_zicond -mtune=rocket -mbranch-cost=3 -fdump-rtl-ce1" { target { rv32 } } } */
+
+typedef int __attribute__ ((mode (SI))) int_t;
+
+int_t
+movsigt (int_t w, int_t x, int_t y, int_t z)
+{
+  return w > x ? y : z;
+}
+
+/* Expect branched assembly like:
+
+	ble	a0,a1,.L2
+	mv	a3,a2
+.L2:
+	mv	a0,a3
+ */
+
+/* { dg-final { scan-rtl-dump-not "Conversion succeeded on pass \[0-9\]+\\." "ce1" } } */
+/* { dg-final { scan-rtl-dump-not "if-conversion succeeded through" "ce1" } } */
+/* { dg-final { scan-assembler-times "\\s(?:bge|bgt|ble|blt)\\s" 1 } } */
+/* { dg-final { scan-assembler-not "\\s(?:sgt|slt)\\s" } } */
+/* { dg-final { scan-assembler-not "\\sczero\\.eqz\\s" } } */
+/* { dg-final { scan-assembler-not "\\sczero\\.nez\\s" } } */
+/* { dg-final { scan-assembler-not "\\s(?:seqz|snez)\\s" } } */
