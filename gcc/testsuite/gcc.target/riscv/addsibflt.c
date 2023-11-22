2278c6443aa6aaa12b3682afb8ad0774575ae1b4
Maciej W. Rozycki <macro@embecosm.com>
2023-11-22 01:18:30 +0000

    RISC-V/testsuite: Add branched cases for generic FP cond adds
    Verify, for generic floating-point conditional-add operations that have
    a corresponding conditional-set machine instruction, that if-conversion
    does *not* trigger at `-mbranch-cost=2' setting, which makes original
    branched code sequences cheaper than their branchless equivalents
    if-conversion would emit.  Cover all the relevant floating-point
    relational operations to make sure no corner case escapes.
    
    	gcc/testsuite/
    	* gcc.target/riscv/adddibfeq.c: New test.
    	* gcc.target/riscv/adddibfge.c: New test.
    	* gcc.target/riscv/adddibfgt.c: New test.
    	* gcc.target/riscv/adddibfle.c: New test.
    	* gcc.target/riscv/adddibflt.c: New test.
    	* gcc.target/riscv/addsibfeq.c: New test.
    	* gcc.target/riscv/addsibfge.c: New test.
    	* gcc.target/riscv/addsibfgt.c: New test.
    	* gcc.target/riscv/addsibfle.c: New test.
    	* gcc.target/riscv/addsibflt.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/addsibflt.c
@@ -0,0 +1,26 @@
+/* { dg-do compile } */
+/* { dg-skip-if "" { *-*-* } { "-O0" "-Og" "-Os" "-Oz" } } */
+/* { dg-options "-march=rv32gc -mtune=sifive-5-series -mbranch-cost=2 -mmovcc -ffinite-math-only -fdump-rtl-ce1" { target { rv32 } } } */
+/* { dg-options "-march=rv64gc -mtune=sifive-5-series -mbranch-cost=2 -mmovcc -ffinite-math-only -fdump-rtl-ce1" { target { rv64 } } } */
+
+typedef int __attribute__ ((mode (SI))) int_t;
+
+int_t
+addsiflt (double w, double x, int_t y, int_t z)
+{
+  return w < x ? y + z : y;
+}
+
+/* Expect branched assembly like:
+
+	flt.d	a5,fa0,fa1
+	beq	a5,zero,.L2
+	addw	a0,a0,a1
+.L2:
+ */
+
+/* { /* { dg-final { scan-rtl-dump-not "Conversion succeeded on pass \[0-9\]+\\." "ce1" } } */
+/* { dg-final { scan-rtl-dump-not "if-conversion succeeded through" "ce1" } } */
+/* { dg-final { scan-assembler-times "\\s(?:fge\\.d|fgt\\.d|fle\\.d|flt\\.d)\\s" 1 } } */
+/* { dg-final { scan-assembler-times "\\s(?:beq|bne)\\s" 1 } } */
+/* { dg-final { scan-assembler-not "\\s(?:seqz|snez)\\s" } } */
