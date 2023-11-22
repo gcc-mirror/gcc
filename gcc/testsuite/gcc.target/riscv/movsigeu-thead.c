6e3237321454a48f37c43e9b71f7c1ffd1879b96
Maciej W. Rozycki <macro@embecosm.com>
2023-11-22 01:18:27 +0000

    RISC-V/testsuite: Add branchless cases for T-Head non-equality cond moves
    Verify, for T-Head targets and the non-equality integer conditional-move
    operations, that if-conversion triggers via `noce_try_cmove' at
    `-mbranch-cost=2' setting, which makes branchless code sequences
    produced by if-conversion cheaper than their original branched
    equivalents, and that extraneous instructions such as SNEZ, etc. are not
    present in output.
    
    	gcc/testsuite/
    	* gcc.target/riscv/movdige-thead.c: New test.
    	* gcc.target/riscv/movdigeu-thead.c: New test.
    	* gcc.target/riscv/movdigt-thead.c: New test.
    	* gcc.target/riscv/movdigtu-thead.c: New test.
    	* gcc.target/riscv/movdile-thead.c: New test.
    	* gcc.target/riscv/movdileu-thead.c: New test.
    	* gcc.target/riscv/movdilt-thead.c: New test.
    	* gcc.target/riscv/movdiltu-thead.c: New test.
    	* gcc.target/riscv/movsige-thead.c: New test.
    	* gcc.target/riscv/movsigeu-thead.c: New test.
    	* gcc.target/riscv/movsigt-thead.c: New test.
    	* gcc.target/riscv/movsigtu-thead.c: New test.
    	* gcc.target/riscv/movsile-thead.c: New test.
    	* gcc.target/riscv/movsileu-thead.c: New test.
    	* gcc.target/riscv/movsilt-thead.c: New test.
    	* gcc.target/riscv/movsiltu-thead.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/movsigeu-thead.c
@@ -0,0 +1,26 @@
+/* { dg-do compile } */
+/* { dg-skip-if "" { *-*-* } { "-O0" "-Og" "-Os" "-Oz" } } */
+/* { dg-options "-march=rv64gc_xtheadcondmov -mtune=thead-c906 -mbranch-cost=2 -fdump-rtl-ce1" { target { rv64 } } } */
+/* { dg-options "-march=rv32gc_xtheadcondmov -mtune=thead-c906 -mbranch-cost=2 -fdump-rtl-ce1" { target { rv32 } } } */
+
+typedef unsigned int __attribute__ ((mode (SI))) int_t;
+
+int_t
+movsigeu (int_t w, int_t x, int_t y, int_t z)
+{
+  return w >= x ? y : z;
+}
+
+/* Expect branchless assembly like:
+
+	sltu	a0,a0,a1
+	th.mvnez	a2,a3,a0
+	mv	a0,a2
+ */
+
+/* { dg-final { scan-rtl-dump-times "Conversion succeeded on pass 1\\." 1 "ce1" } } */
+/* { dg-final { scan-rtl-dump-times "if-conversion succeeded through noce_try_cmove" 1 "ce1" } } */
+/* { dg-final { scan-assembler-times "\\s(?:sgtu|sltu)\\s" 1 } } */
+/* { dg-final { scan-assembler-times "\\s(?:th\\.mveqz|th\\.mvnez)\\s" 1 } } */
+/* { dg-final { scan-assembler-not "\\s(?:seqz|snez)\\s" } } */
+/* { dg-final { scan-assembler-not "\\s(?:bgeu|bgtu|bleu|bltu)\\s" } } */
