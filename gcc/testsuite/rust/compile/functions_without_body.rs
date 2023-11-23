a7991de39ff3ef42cd48bb460a02134af73d7b9d
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2023-11-23 14:08:50 +0100

    gccrs: Add a regression test for function body check
    Function body check emit an error message when a required function body
    is missing.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/functions_without_body.rs: New test.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

---
+++ gcc/testsuite/rust/compile/functions_without_body.rs
@@ -0,0 +1,21 @@
+struct MyStruct;
+
+trait X {}
+
+fn test_a();
+// { dg-error "free function without a body" "" { target *-*-* } .-1 }
+
+impl MyStruct {
+    fn test_b<T>()
+    // { dg-error "associated function in .impl. without body" "" { target *-*-* } .-1 }
+    where
+        T: Copy;
+
+    fn test_c<T>();
+    // { dg-error "associated function in .impl. without body" "" { target *-*-* } .-1 }
+}
+
+impl X for MyStruct {
+    fn test_d();
+    // { dg-error "associated function in .impl. without body" "" { target *-*-* } .-1 }
+}
