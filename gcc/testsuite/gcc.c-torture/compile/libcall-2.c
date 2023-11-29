4ed0740c6e807460ce79a351094329fdeb551545
Jose E. Marchesi <jose.marchesi@oracle.com>
2023-11-29 07:44:59 +0100

    testsuite: fix gcc.c-torture/compile/libcall-2.c in -m32
    This test relies on having __int128 in x86_64 targets, which is only
    available in -m64.
    
    gcc/testsuite/ChangeLog
    
    	* gcc.c-torture/compile/libcall-2.c: Skip test in -m32.

--- gcc/testsuite/gcc.c-torture/compile/libcall-2.c
+++ gcc/testsuite/gcc.c-torture/compile/libcall-2.c
@@ -2,6 +2,8 @@
    indirect calls.  */
 
 /* { dg-do compile } */
+/* __int128 is not supported in x86 -m32.  */
+/* { dg-skip-if "" { ! { x86_64-*-* && { ! ilp32 } } } } */
 /* { dg-options "-O2 -mcmodel=large" { target x86_64-*-* } } */
 /* { dg-final { scan-assembler "globl\t__divti3" } } */
 
