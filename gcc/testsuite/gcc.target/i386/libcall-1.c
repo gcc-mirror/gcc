86903dd94e61b6db8ac525f4ae6c266165533c90
Jose E. Marchesi <jose.marchesi@oracle.com>
2023-11-29 09:16:48 +0100

    testsuite: move gcc.c-torture/compile/libcall-2.c to gcc.target/i386/libcall-1.c
    This patch relocates a test that is really x86 specific, and changes
    it to use check_effective_target_int128.
    
    gcc/testsuite/ChangeLog
    
    	* gcc.c-torture/compile/libcall-2.c: Remove.
    	* gcc.target/i386/libcall-1.c: Moved from
    	gcc.c-torture/compile/libcall-2.c and adapted to use
    	effective-target for int128_t.

--- gcc/testsuite/gcc.target/i386/libcall-1.c
+++ gcc/testsuite/gcc.target/i386/libcall-1.c
@@ -1,10 +1,8 @@
 /* Make sure that external refences for libcalls are generated even for
    indirect calls.  */
 
-/* { dg-do compile } */
-/* __int128 is not supported in x86 -m32.  */
-/* { dg-skip-if "" { ! { x86_64-*-* && { ! ilp32 } } } } */
-/* { dg-options "-O2 -mcmodel=large" { target x86_64-*-* } } */
+/* { dg-do compile { target int128 } } */
+/* { dg-options "-O2 -mcmodel=large" } */
 /* { dg-final { scan-assembler "globl\t__divti3" } } */
 
 __int128 a, b; void foo () { a = a / b; }
