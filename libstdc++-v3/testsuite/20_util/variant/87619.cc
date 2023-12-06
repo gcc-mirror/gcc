7d11d813583efad652c83c7631ef8d6d18489119
Jonathan Wakely <jwakely@redhat.com>
2023-12-06 18:32:21 +0000

    libstdc++: Remove dg-timeout-factor from test
    As the comment notes, the increased timeout was needed because of PR
    102780, but that was fixed long ago.
    
    libstdc++-v3/ChangeLog:
    
    	* testsuite/20_util/variant/87619.cc: Remove dg-timeout-factor.

--- libstdc++-v3/testsuite/20_util/variant/87619.cc
+++ libstdc++-v3/testsuite/20_util/variant/87619.cc
@@ -16,8 +16,6 @@
 // <http://www.gnu.org/licenses/>.
 
 // { dg-do compile { target c++17 } }
-// FIXME: Need increased timeout due to PR c++/102780
-// { dg-timeout-factor 2 { target c++20 } }
 
 #include <variant>
 #include <utility>
