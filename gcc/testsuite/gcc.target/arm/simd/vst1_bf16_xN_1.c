bdd0a50833e2a30ad5795f0c81925c9cea46f9d1
Richard Earnshaw <rearnsha@arm.com>
2023-12-08 16:04:18 +0000

    Revert "arm: vst1_types_x4 ACLE intrinsics"
    This reverts commit 2f48d846c794ba091b266133f73717361096d454.

--- gcc/testsuite/gcc.target/arm/simd/vst1_bf16_xN_1.c
+++ gcc/testsuite/gcc.target/arm/simd/vst1_bf16_xN_1.c
@@ -15,8 +15,4 @@ void test_vst1_bf16_x3 (bfloat16_t * ptr, bfloat16x4x3_t val)
     vst1_bf16_x3 (ptr, val);
 }
 
-void test_vst1_bf16_x4 (bfloat16_t * ptr, bfloat16x4x4_t val)
-{
-    vst1_bf16_x4 (ptr, val);
-}
-/* { dg-final { scan-assembler-times {vst1.16\t\{d[0-9]+-d[0-9]+\}, \[r[0-9]+\]\n} 3 } }  */
+/* { dg-final { scan-assembler-times {vst1.16\t\{d[0-9]+-d[0-9]+\}, \[r[0-9]+\]\n} 2 } }  */
--- gcc/testsuite/gcc.target/arm/simd/vst1_fp16_xN_1.c
+++ gcc/testsuite/gcc.target/arm/simd/vst1_fp16_xN_1.c
@@ -15,9 +15,4 @@ void test_vst1_f16_x3 (float16_t * ptr, float16x4x3_t val)
     vst1_f16_x3 (ptr, val);
 }
 
-void test_vst1_f16_x4 (float16_t * ptr, float16x4x4_t val)
-{
-    vst1_f16_x4 (ptr, val);
-}
-
-/* { dg-final { scan-assembler-times {vst1.16\t\{d[0-9]+-d[0-9]+\}, \[r[0-9]+\]\n} 3 } }  */
+/* { dg-final { scan-assembler-times {vst1.16\t\{d[0-9]+-d[0-9]+\}, \[r[0-9]+\]\n} 2 } }  */
