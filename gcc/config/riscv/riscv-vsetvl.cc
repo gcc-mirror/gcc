330bb06485b18bae3effe83b45b97dbdcd543367
Juzhe-Zhong <juzhe.zhong@rivai.ai>
2023-12-11 14:43:34 +0800

    RISC-V: Fix ICE in extract_single_source
    This patch fixes the following ICE in VSETVL PASS:
    bug.c:39:1: internal compiler error: Segmentation fault
       39 | }
          | ^
    0x1ad5a08 crash_signal
            ../../../../gcc/gcc/toplev.cc:316
    0x7f7f55feb90f ???
            ./signal/../sysdeps/unix/sysv/linux/x86_64/libc_sigaction.c:0
    0x218d7c7 extract_single_source
            ../../../../gcc/gcc/config/riscv/riscv-vsetvl.cc:583
    0x218d95d extract_single_source
            ../../../../gcc/gcc/config/riscv/riscv-vsetvl.cc:604
    0x218fbc5 pre_vsetvl::compute_lcm_local_properties()
            ../../../../gcc/gcc/config/riscv/riscv-vsetvl.cc:2703
    0x2190ef4 pre_vsetvl::earliest_fuse_vsetvl_info()
            ../../../../gcc/gcc/config/riscv/riscv-vsetvl.cc:2890
    0x2193e62 pass_vsetvl::lazy_vsetvl()
            ../../../../gcc/gcc/config/riscv/riscv-vsetvl.cc:3537
    0x219406a pass_vsetvl::execute(function*)
            ../../../../gcc/gcc/config/riscv/riscv-vsetvl.cc:3584
    
    The rootcause we have a case that the def info can not be traced:
    
    (insn 208 327 333 27 (use (reg/i:DI 10 a0)) "bug.c":36:1 -1
         (nil))
    
    It's obvious, we conservatively disable any optimization in this situation if
    AVL def_info can not be tracded.
    
    Committed as it is obvious.
    
    gcc/ChangeLog:
    
    	* config/riscv/riscv-vsetvl.cc (extract_single_source): Fix ICE.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/vsetvl/avl_use_bug-1.c: New test.

--- gcc/config/riscv/riscv-vsetvl.cc
+++ gcc/config/riscv/riscv-vsetvl.cc
@@ -579,6 +579,8 @@ extract_single_source (set_info *set)
   if (!set->insn ()->is_phi ())
     return nullptr;
   hash_set<set_info *> sets = get_all_sets (set, true, false, true);
+  if (sets.is_empty ())
+    return nullptr;
 
   insn_info *first_insn = (*sets.begin ())->insn ();
   if (first_insn->is_artificial ())
