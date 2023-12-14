0715ed49b40311e9b2715f63308a970195dd2417
Eric Botcazou <ebotcazou@adacore.com>
2023-12-14 13:05:52 +0100

    ada: Fix oversight in previous change
    In rare cases, types using structural equality may reach relate_alias_sets.
    
    gcc/ada/
    
    	* gcc-interface/utils.cc (relate_alias_sets): Restore previous code
    	when the type uses structural equality.

--- gcc/ada/gcc-interface/utils.cc
+++ gcc/ada/gcc-interface/utils.cc
@@ -1867,8 +1867,11 @@ relate_alias_sets (tree new_type, tree old_type, enum alias_set_op op)
 		      && TYPE_NONALIASED_COMPONENT (new_type)
 			 != TYPE_NONALIASED_COMPONENT (old_type)));
 
-      /* The alias set always lives on the TYPE_CANONICAL.  */
-      TYPE_ALIAS_SET (TYPE_CANONICAL (new_type)) = get_alias_set (old_type);
+      /* The alias set is a property of the TYPE_CANONICAL if it exists.  */
+      if (TYPE_STRUCTURAL_EQUALITY_P (new_type))
+	TYPE_ALIAS_SET (new_type) = get_alias_set (old_type);
+      else
+	TYPE_ALIAS_SET (TYPE_CANONICAL (new_type)) = get_alias_set (old_type);
       break;
 
     case ALIAS_SET_SUBSET:
