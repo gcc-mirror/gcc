afead7fb9b3113dbc3dc84e514847f8b688742bf
Juzhe-Zhong <juzhe.zhong@rivai.ai>
2023-12-15 06:22:06 +0800

    RISC-V: Adjust test
    Since middle-end patch:
    https://gcc.gnu.org/pipermail/gcc-patches/2023-December/640595.html
    
    will change vectorization code.
    
    Adapt tests for ths patch.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/vsetvl/pr112988-1.c: Adapt test.

--- gcc/testsuite/gcc.target/riscv/rvv/vsetvl/pr112988-1.c
+++ gcc/testsuite/gcc.target/riscv/rvv/vsetvl/pr112988-1.c
@@ -62,8 +62,7 @@ int main() {
   return 0;
 }
 
-/* { dg-final { scan-assembler-times {vsetvli} 1 } } */
-/* { dg-final { scan-assembler-times {vsetivli} 1 } } */
-/* { dg-final { scan-assembler-times {vsetivli\tzero,\s*4,\s*e32,\s*m1,\s*t[au],\s*m[au]} 1 } } */
+/* { dg-final { scan-assembler-times {vsetvli} 4 } } */
+/* { dg-final { scan-assembler-not {vsetivli} } } */
 /* { dg-final { scan-assembler-times {vsetvli\tzero,\s*[a-x0-9]+,\s*e8,\s*m8,\s*t[au],\s*m[au]} 1 } } */
 /* { dg-final { scan-assembler-times {li\t[a-x0-9]+,\s*32} 1 } } */
