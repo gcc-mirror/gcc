35914e5bba0dff600ce3e74a8fdc441c58b74330
Alexandre Oliva <oliva@adacore.com>
2023-12-20 01:05:45 -0300

    -finline-stringops: copy timeout factor from memcmp-1.c test
    I added some -finline-stringops tests that included memcmp-1.c, but
    carried over the timeout factor onto only one such test.  Jeff Law
    kindly pointed that out (thanks!), so here's the fix.
    
    
    for  gcc/testsuite/ChangeLog
    
    	* gcc.dg/torture/inline-mem-cmp-1.c: Copy timeout factor from
    	mem-cmp-1.c.
    	* gcc.dg/torture/inline-mem-cpy-1.c: Likewise.

--- gcc/testsuite/gcc.dg/torture/inline-mem-cpy-1.c
+++ gcc/testsuite/gcc.dg/torture/inline-mem-cpy-1.c
@@ -1,5 +1,6 @@
 /* { dg-do run } */
 /* { dg-options "-finline-stringops=memcpy -save-temps -g0 -fno-lto" } */
+/* { dg-timeout-factor 2 } */
 
 #include "../memcmp-1.c"
 /* Yeah, this memcmp test exercises plenty of memcpy, more than any of the
