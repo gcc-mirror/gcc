b3ec98d458f2b285bb7b3fa4fcd93fd830fee069
Feng Wang <wangfeng@eswincomputing.com>
2024-01-03 05:21:45 +0000

    RISC-V: Add crypto vector api-testing cases.
    This patch add crypto vector api-testing cases based on
    https://github.com/riscv-non-isa/rvv-intrinsic-doc/blob/eopc/vector-crypto/auto-generated/vector-crypto
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/base/zvbb-intrinsic.c: New test.
    	* gcc.target/riscv/rvv/base/zvbb_vandn_vx_constraint.c: New test.
    	* gcc.target/riscv/rvv/base/zvbc-intrinsic.c: New test.
    	* gcc.target/riscv/rvv/base/zvbc_vx_constraint-1.c: New test.
    	* gcc.target/riscv/rvv/base/zvbc_vx_constraint-2.c: New test.
    	* gcc.target/riscv/rvv/base/zvkg-intrinsic.c: New test.
    	* gcc.target/riscv/rvv/base/zvkned-intrinsic.c: New test.
    	* gcc.target/riscv/rvv/base/zvknha-intrinsic.c: New test.
    	* gcc.target/riscv/rvv/base/zvknhb-intrinsic.c: New test.
    	* gcc.target/riscv/rvv/base/zvksed-intrinsic.c: New test.
    	* gcc.target/riscv/rvv/base/zvksh-intrinsic.c: New test.
    	* gcc.target/riscv/zvkb.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/zvkb.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gc_zvkb" { target { rv64 } } } */
+/* { dg-options "-march=rv32gc_zvkb" { target { rv32 } } } */
+
+#ifndef __riscv_zvkb
+#error "Feature macro not defined"
+#endif
+
+int
+foo (int a)
+{
+  return a;
+}
