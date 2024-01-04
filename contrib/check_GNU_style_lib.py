29abd09a744970bf2b93d078bad9946899ef2b05
Jonathan Wakely <jwakely@redhat.com>
2024-01-04 15:01:20 +0000

    contrib: Remove C-style comments from Python files
    These Python scripts have "*/" at the end of the license header comment
    blocks, presumably copy&pasted from C files.
    
    contrib/ChangeLog:
    
    	* analyze_brprob.py: Remove stray text at end of comment.
    	* analyze_brprob_spec.py: Likewise.
    	* check-params-in-docs.py: Likewise.
    	* check_GNU_style.py: Likewise.
    	* check_GNU_style_lib.py: Likewise.
    	* filter-clang-warnings.py: Likewise.
    	* gcc-changelog/git_check_commit.py: Likewise.
    	* gcc-changelog/git_commit.py: Likewise.
    	* gcc-changelog/git_email.py: Likewise.
    	* gcc-changelog/git_repository.py: Likewise.
    	* gcc-changelog/git_update_version.py: Likewise.
    	* gcc-changelog/test_email.py: Likewise.
    	* gen_autofdo_event.py: Likewise.
    	* mark_spam.py: Likewise.
    	* unicode/gen-box-drawing-chars.py: Likewise.
    	* unicode/gen-combining-chars.py: Likewise.
    	* unicode/gen-printable-chars.py: Likewise.
    	* unicode/gen_wcwidth.py: Likewise.

--- contrib/check_GNU_style_lib.py
+++ contrib/check_GNU_style_lib.py
@@ -20,7 +20,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with GCC; see the file COPYING3.  If not see
-# <http://www.gnu.org/licenses/>.  */
+# <http://www.gnu.org/licenses/>.
 #
 # The script requires python packages, which can be installed via pip3
 # like this:
