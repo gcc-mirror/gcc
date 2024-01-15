4d7c4288a90ab949f70c47a4b3e97c52f4f39f11
Liao Shihua <shihua@iscas.ac.cn>
2024-01-15 16:31:33 +0800

    RISC-V: Remove the Scalar Bitmanip and Crypto Built-In function testsuites
    The serials patch provides a mapping from the RV intrinsics to the builtin names.
    There are some duplicates testsuites between intrinsic and built-in function.
    Remove the Scalar Bitmanip and Scalar Crypto Built-In function testsuites
    that will be included in the intrinsic functions.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/zbb_32_bswap-2.c: Moved to...
    	* gcc.target/riscv/zbb_bswap16.c: ...here.
    	* gcc.target/riscv/zbkb32.c: Remove __builtin_riscv_(un)zip and
    	zip,__builtin_riscv_brev8.
    	* gcc.target/riscv/zbkb64.c: Remove __builtin_riscv_brev8.
    	* gcc.target/riscv/zbb_32_bswap-1.c: Removed.
    	* gcc.target/riscv/zbb_bswap-1.c: Removed.
    	* gcc.target/riscv/zbb_bswap-2.c: Removed.
    	* gcc.target/riscv/zbbw.c: Removed.
    	* gcc.target/riscv/zbc32.c: Removed.
    	* gcc.target/riscv/zbc64.c: Removed.
    	* gcc.target/riscv/zbkc32.c: Removed.
    	* gcc.target/riscv/zbkc64.c: Removed.
    	* gcc.target/riscv/zbkx32.c: Removed.
    	* gcc.target/riscv/zbkx64.c: Removed.
    	* gcc.target/riscv/zknd32-2.c: Removed.
    	* gcc.target/riscv/zknd64-2.c: Removed.
    	* gcc.target/riscv/zkne32-2.c: Removed.
    	* gcc.target/riscv/zkne64-2.c: Removed.
    	* gcc.target/riscv/zknh-sha256-32.c: Removed.
    	* gcc.target/riscv/zknh-sha256-64.c: Removed.
    	* gcc.target/riscv/zknh-sha512-32.c: Removed.
    	* gcc.target/riscv/zknh-sha512-64.c: Removed.
    	* gcc.target/riscv/zksed32-2.c: Removed.
    	* gcc.target/riscv/zksed64-2.c: Removed.
    	* gcc.target/riscv/zksh32.c: Removed.
    	* gcc.target/riscv/zksh64.c: Removed.

--- gcc/testsuite/gcc.target/riscv/zbb_bswap16.c
+++ gcc/testsuite/gcc.target/riscv/zbb_bswap16.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
-/* { dg-options "-march=rv32gc_zbb -mabi=ilp32" } */
+/* { dg-options "-march=rv64gc_zbb -mabi=lp64d"  { target { rv64 } } } */
+/* { dg-options "-march=rv32gc_zbb -mabi=ilp32d" { target { rv32 } } } */
 /* { dg-skip-if "" { *-*-* } { "-O0" } } */
 
 int foo(int n)
--- gcc/testsuite/gcc.target/riscv/zbkb32.c
+++ gcc/testsuite/gcc.target/riscv/zbkb32.c
@@ -14,23 +14,5 @@ uint32_t foo2(uint8_t rs1, uint8_t rs2)
     return __builtin_riscv_packh(rs1, rs2);
 }
 
-uint32_t foo3(uint32_t rs1)
-{
-    return __builtin_riscv_brev8(rs1);
-}
-
-uint32_t foo4(uint32_t rs1)
-{
-    return __builtin_riscv_zip(rs1);
-}
-
-uint32_t foo5(uint32_t rs1)
-{
-    return __builtin_riscv_unzip(rs1);
-}
-
 /* { dg-final { scan-assembler-times "pack\t" 1 } } */
 /* { dg-final { scan-assembler-times {\mpackh} 1 } } */
-/* { dg-final { scan-assembler-times {\mbrev8} 1 } } */
-/* { dg-final { scan-assembler-times "\tzip\t" 1 } } */
-/* { dg-final { scan-assembler-times {\munzip} 1 } } */
