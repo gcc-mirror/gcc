3474059707a939cf2fece418daef83e6f5bf1494
Arthur Cohen <arthur.cohen@embecosm.com>
2024-01-18 17:24:01 +0100

    testsuite: Fix missing handling of little endian.
    Some failures occur in the testsuite because we
    did not account for the little-endian case.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/issue-1446.rs: Add swap_bytes function.
    	* rust/compile/iterators1.rs: Remove unused {to, from}_le functions.

--- gcc/testsuite/rust/compile/issue-1446.rs
+++ gcc/testsuite/rust/compile/issue-1446.rs
@@ -1,3 +1,11 @@
+// fake function
+pub fn swap_bytes(this: u32) -> u32 {
+    (((this) & 0xff000000) >> 24)
+        | (((this) & 0x00ff0000) >> 8)
+        | (((this) & 0x0000ff00) << 8)
+        | (((this) & 0x000000ff) << 24)
+}
+
 pub fn to_le(this: u32) -> u32 {
     #[cfg(target_endian = "little")]
     {
@@ -5,6 +13,6 @@ pub fn to_le(this: u32) -> u32 {
     }
     #[cfg(not(target_endian = "little"))]
     {
-        this.swap_bytes()
+        swap_bytes(this)
     }
 }
