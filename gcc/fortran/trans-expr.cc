68862e5c75ef0e875e690f0880a96fc6200d1682
Harald Anlauf <anlauf@gmx.de>
2024-01-20 22:18:02 +0100

    Fortran: passing of optional scalar arguments with VALUE attribute [PR113377]
    gcc/fortran/ChangeLog:
    
    	PR fortran/113377
    	* trans-expr.cc (gfc_conv_procedure_call): Fix handling of optional
    	scalar arguments of intrinsic type with the VALUE attribute.
    
    gcc/testsuite/ChangeLog:
    
    	PR fortran/113377
    	* gfortran.dg/optional_absent_9.f90: New test.

--- gcc/fortran/trans-expr.cc
+++ gcc/fortran/trans-expr.cc
@@ -7256,6 +7256,7 @@ gfc_conv_procedure_call (gfc_se * se, gfc_symbol * sym,
 	      && e->symtree->n.sym->attr.optional
 	      && (((e->rank != 0 && elemental_proc)
 		   || e->representation.length || e->ts.type == BT_CHARACTER
+		   || (e->rank == 0 && e->symtree->n.sym->attr.value)
 		   || (e->rank != 0
 		       && (fsym == NULL
 			   || (fsym->as
