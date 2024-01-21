a5aca83ca9c7fac895d10eb7b3e14b1927ec1eac
Jonathan Wakely <jwakely@redhat.com>
2024-01-21 18:16:14 +0000

    libstdc++: Fix std::format for floating-point chrono::time_point [PR113500]
    Currently trying to use std::format with certain specializations of
    std::chrono::time_point is ill-formed, due to one member function of the
    __formatter_chrono type which tries to write a time_point to an ostream.
    For sys_time<floating-point> or sys_time with a period greater than days
    there is no operator<< that can be used.
    
    That operator<< is only needed when using an empty chrono-specs in the
    format string, like "{}", but the ill-formed expression gives an error
    even if not actually used. This means it's not possible to format some
    other specializations of chrono::time_point, even when using a non-empty
    chrono-specs.
    
    This fixes it by avoiding using 'os << t' for all chrono::time_point
    specializations, and instead using std::format("{:L%F %T}", t). So that
    we continue to reject std::format("{}", sys_time{1.0s}) a check for
    empty chrono-specs is added to the formatter<sys_time<D>, C>
    specialization.
    
    While testing this I noticed that the output for %S with a
    floating-point duration was incorrect, as the subseconds part was being
    appended to the seconds without a decimal point, and without the correct
    number of leading zeros.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/113500
    	* include/bits/chrono_io.h (__formatter_chrono::_M_S): Fix
    	printing of subseconds with floating-point rep.
    	(__formatter_chrono::_M_format_to_ostream): Do not write
    	time_point specializations directly to the ostream.
    	(formatter<chrono::sys_time<D>, C>::parse): Do not allow an
    	empty chrono-spec if the type fails to meet the constraints for
    	writing to an ostream with operator<<.
    	* testsuite/std/time/clock/file/io.cc: Check formatting
    	non-integral times with empty chrono-specs.
    	* testsuite/std/time/clock/gps/io.cc: Likewise.
    	* testsuite/std/time/clock/utc/io.cc: Likewise.
    	* testsuite/std/time/hh_mm_ss/io.cc: Likewise.
    
    (cherry picked from commit 7431fcea6b72beb54abb1932c254ac0e76bd0bde)

--- libstdc++-v3/testsuite/std/time/clock/file/io.cc
+++ libstdc++-v3/testsuite/std/time/clock/file/io.cc
@@ -18,7 +18,25 @@ test_ostream()
   VERIFY( ss1.str() == ss2.str() );
 }
 
+void
+test_format()
+{
+  using namespace std::chrono;
+  auto t = file_clock::now();
+
+  auto s = std::format("{}", t);
+  std::ostringstream ss;
+  ss << t;
+  VERIFY( s == ss.str() );
+
+  // PR libstdc++/113500
+  auto ft = clock_cast<file_clock>(sys_days(2024y/January/21)) + 0ms + 2.5s;
+  s = std::format("{}", ft);
+  VERIFY( s == "2024-01-21 00:00:02.500");
+}
+
 int main()
 {
   test_ostream();
+  test_format();
 }
--- libstdc++-v3/testsuite/std/time/clock/gps/io.cc
+++ libstdc++-v3/testsuite/std/time/clock/gps/io.cc
@@ -4,6 +4,7 @@
 
 #include <chrono>
 #include <format>
+#include <sstream>
 #include <testsuite_hooks.h>
 
 void
@@ -17,9 +18,36 @@ test01()
 
   auto s = format("{0:%F %T %Z} == {1:%F %T %Z}", st, gt);
   VERIFY( s == "2000-01-01 00:00:00 UTC == 2000-01-01 00:00:13 GPS" );
+
+  std::ostringstream ss;
+  ss << gt;
+  VERIFY( ss.str() == "2000-01-01 00:00:13" );
+
+  gps_time<duration<float>> gtf = gt;
+  ss.str("");
+  ss.clear();
+  ss << (gps_time<duration<long double>>(gt) + 20ms);
+  VERIFY( ss.str() == "2000-01-01 00:00:13.020" );
+}
+
+void
+test_format()
+{
+  using std::format;
+  using namespace std::chrono;
+
+  auto st = sys_days{2000y/January/1};
+  auto gt = clock_cast<gps_clock>(st);
+  auto s = std::format("{}", gt);
+  VERIFY( s == "2000-01-01 00:00:13" );
+
+  // PR libstdc++/113500
+  s = std::format("{}", gt + 150ms + 10.5s);
+  VERIFY( s == "2000-01-01 00:00:23.650" );
 }
 
 int main()
 {
   test01();
+  test_format();
 }
