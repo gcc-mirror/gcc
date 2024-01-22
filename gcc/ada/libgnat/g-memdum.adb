06c565cc0105db5d1cb25655671586b47ea5ecf7
Marc Poulhiès <poulhies@adacore.com>
2024-01-22 10:57:42 +0100

    Update copyright years.

--- gcc/ada/libgnat/g-memdum.adb
+++ gcc/ada/libgnat/g-memdum.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---                     Copyright (C) 2003-2023, AdaCore                     --
+--                     Copyright (C) 2003-2024, AdaCore                     --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
