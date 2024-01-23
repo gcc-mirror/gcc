36ac388bf967a820c8cfc43191fc352ac917f97a
Arthur Cohen <arthur.cohen@embecosm.com>
2024-01-23 17:19:31 +0100

    gccrs: Fix scan-gimple testcases on LE platforms.
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/macros/builtin/eager1.rs: Switch to scan-assembler directive as the
    	GIMPLE dump does not contain strings on LE.
    	* rust/compile/macros/builtin/recurse2.rs: Likewise.

--- gcc/testsuite/rust/compile/macros/builtin/recurse2.rs
+++ gcc/testsuite/rust/compile/macros/builtin/recurse2.rs
@@ -15,7 +15,29 @@ macro_rules! a {
     };
 }
 
+extern "C" {
+    fn printf(fmt: *const i8, ...);
+}
+
+fn print_ptr(s: &str) {
+    unsafe {
+        printf("%p\n\0" as *const str as *const i8, s as *const str);
+    }
+}
+
+fn print_str(s: &str) {
+    unsafe {
+        printf(
+            "%s\n\0" as *const str as *const i8,
+            s as *const str as *const i8,
+        );
+    }
+}
+
+// { dg-final { scan-assembler {"abheyho"} } }
+static S: &str = concat!("a", 'b', a!(), a!(b c d e f a!()), '\0');
+
 fn main() {
-    // { dg-final { scan-tree-dump-times {"abheyho"} 1 gimple } }
-    let _ = concat!("a", 'b', a!(), a!(b c d e f a!()), '\0');
+    print_ptr(S);
+    print_str(S);
 }
