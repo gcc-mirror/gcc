209fc1e5f6c67e55e579b69f617b0b678b1bfdf0
Jason Merrill <jason@redhat.com>
2024-01-30 12:07:21 -0500

    testsuite: fix anon6 mangling [PR112846]
    As with r14-6796-g2fa122cae50cd8, avoid mangling compatibility aliases in
    mangling tests, and test the new mangling.
    
    	PR c++/112846
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/abi/anon6.C: Specify ABI v18.
    	* g++.dg/abi/anon6a.C: New test for ABI v19.

--- gcc/testsuite/g++.dg/abi/anon6a.C
+++ gcc/testsuite/g++.dg/abi/anon6a.C
@@ -1,6 +1,6 @@
 // PR c++/108566
 // { dg-do compile { target c++20 } }
-// { dg-additional-options "-fabi-version=18 -fabi-compat-version=18" }
+// { dg-additional-options "-fabi-compat-version=0" }
 
 template<typename T>
 struct wrapper1 {
@@ -17,4 +17,4 @@ void uses() {
   dummy<wrapper1<double>{123.0}>();
 }
 
-// { dg-final { scan-assembler "_Z5dummyIXtl8wrapper1IdEtlNS1_Ut_Edi9RightNametlNS2_Ut_Edi9RightNameLd405ec00000000000EEEEEEvv" } }
+// { dg-final { scan-assembler "_Z5dummyITnDaXtl8wrapper1IdEtlNS1_Ut_Edi9RightNametlNS2_Ut_Edi9RightNameLd405ec00000000000EEEEEEvv" } }
