1725714f1667958e39f67a74e9ab5f41bdbde4af
Sahil Yeole <sahilyeole93@gmail.com>
2024-02-16 02:15:17 +0530

    contrib: Add libgrust to update-copyright.py script
    contrib/ChangeLog:
    	* update-copyright.py: Add libgrust folder.
    
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- contrib/update-copyright.py
+++ contrib/update-copyright.py
@@ -775,6 +775,7 @@ class GCCCmdLine (CmdLine):
         self.add_dir ('libgfortran')
         # libgo is imported from upstream.
         self.add_dir ('libgomp')
+        self.add_dir ('libgrust')
         self.add_dir ('libiberty')
         self.add_dir ('libitm')
         self.add_dir ('libobjc')
@@ -802,6 +803,7 @@ class GCCCmdLine (CmdLine):
             'libgcc',
             'libgfortran',
             'libgomp',
+            'libgrust',
             'libiberty',
             'libitm',
             'libobjc',
