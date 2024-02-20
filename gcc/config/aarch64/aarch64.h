38e422e2ef539ccf6db1bdd340079631b1141637
Zac Walker <zacwalker@microsoft.com>
2024-02-20 18:10:08 +0100

    aarch64: Add SEH to machine_function
    SEH is not enabled in aarch64-w64-mingw32 target yet. However, it is
    needed to be declared in machine_function for reusing winnt.cc.
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64.h (struct seh_frame_state): Declare SEH
    	structure in machine_function.
    	(GTY): Add SEH field.

--- gcc/config/aarch64/aarch64.h
+++ gcc/config/aarch64/aarch64.h
@@ -1046,6 +1046,9 @@ struct GTY (()) aarch64_frame
   bool is_scs_enabled;
 };
 
+/* Private to winnt.cc.  */
+struct seh_frame_state;
+
 #ifdef hash_set_h
 typedef struct GTY (()) machine_function
 {
@@ -1086,6 +1089,9 @@ typedef struct GTY (()) machine_function
      still exists and still fulfils its original purpose. the same register
      can be reused by other code.  */
   rtx_insn *advsimd_zero_insn;
+
+  /* During SEH output, this is non-null.  */
+  struct seh_frame_state * GTY ((skip (""))) seh;
 } machine_function;
 #endif
 #endif
