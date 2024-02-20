ecfcc362b7f3f796c72f3525c4e3a6dd8ab3beac
Jakub Jelinek <jakub@redhat.com>
2024-02-20 10:31:46 +0100

    testsuite: Fix up analyzer/torture/vector-extract-1.c test for i686 [PR113983]
    The testcase fails on i686-linux with
    .../gcc/testsuite/gcc.dg/analyzer/torture/vector-extract-1.c:11:1: warning: MMX vector return without MMX enabled changes the ABI [-Wpsabi]
    Added -Wno-psabi to silence the warning.
    
    2024-02-20  Jakub Jelinek  <jakub@redhat.com>
    
    	PR analyzer/113983
    	* gcc.dg/analyzer/torture/vector-extract-1.c: Add -Wno-psabi as
    	dg-additional-options.

--- gcc/testsuite/gcc.dg/analyzer/torture/vector-extract-1.c
+++ gcc/testsuite/gcc.dg/analyzer/torture/vector-extract-1.c
@@ -1,4 +1,5 @@
 /* PR analyzer/113983  */
+/* { dg-additional-options "-Wno-psabi" } */
 
 /* maybe_undo_optimize_bit_field_compare used to ICE on this
    because it was not checking for only integer types. */
