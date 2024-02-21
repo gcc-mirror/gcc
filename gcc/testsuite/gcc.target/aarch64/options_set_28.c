d0f8cb17bbd35e5eb25fc3b2119756b3e103e798
Tamar Christina <tamar.christina@arm.com>
2024-02-21 11:42:53 +0000

    AArch64: remove ls64 from being mandatory on armv8.7-a..
    The Arm Architectural Reference Manual (Version J.a, section A2.9 on FEAT_LS64)
    shows that ls64 is an optional extensions and should not be enabled by default
    for Armv8.7-a.
    
    This drops it from the mandatory bits for the architecture and brings GCC inline
    with LLVM and the achitecture.
    
    Note that we will not be changing binutils to preserve compatibility with older
    released compilers.
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64-arches.def (AARCH64_ARCH): Remove LS64 from
    	Armv8.7-a.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.target/aarch64/acle/ls64.C: Add +ls64.
    	* g++.target/aarch64/acle/ls64_lto.C: Likewise.
    	* gcc.target/aarch64/acle/ls64_lto.c: Likewise.
    	* gcc.target/aarch64/acle/pr110100.c: Likewise.
    	* gcc.target/aarch64/acle/pr110132.c: Likewise.
    	* gcc.target/aarch64/options_set_28.c: Drop check for nols64.
    	* gcc.target/aarch64/pragma_cpp_predefs_2.c: Correct header checks.

--- gcc/testsuite/gcc.target/aarch64/options_set_28.c
+++ gcc/testsuite/gcc.target/aarch64/options_set_28.c
@@ -1,9 +1,9 @@
 /* { dg-do compile } */
-/* { dg-additional-options "-march=armv9.3-a+nopredres+nols64+nomops" } */
+/* { dg-additional-options "-march=armv9.3-a+nopredres+nomops" } */
 
 int main ()
 {
   return 0;
 }
 
-/* { dg-final { scan-assembler-times {\.arch armv9\.3\-a\+crc\+nopredres\+nols64\+nomops\n} 1 } } */
+/* { dg-final { scan-assembler-times {\.arch armv9\.3\-a\+crc\+nopredres\+nomops\n} 1 } } */
