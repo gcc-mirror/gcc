1f5787e4b803a4294eeb80e048f56ccdb99c1b3b
Harald Anlauf <anlauf@gmx.de>
2024-02-25 21:18:23 +0100

    Fortran: do not evaluate polymorphic functions twice in assignment [PR114012]
    	PR fortran/114012
    
    gcc/fortran/ChangeLog:
    
    	* trans-expr.cc (gfc_conv_procedure_call): Evaluate non-trivial
    	arguments just once before assigning to an unlimited polymorphic
    	dummy variable.
    
    gcc/testsuite/ChangeLog:
    
    	* gfortran.dg/pr114012.f90: New test.
    
    (cherry picked from commit 2f71e801ad0bb1f620334aadbd7c99cc4efe6309)

--- gcc/fortran/trans-expr.cc
+++ gcc/fortran/trans-expr.cc
@@ -6518,6 +6518,10 @@ gfc_conv_procedure_call (gfc_se * se, gfc_symbol * sym,
 			    {
 			      tree efield;
 
+			      /* Evaluate arguments just once.  */
+			      if (e->expr_type != EXPR_VARIABLE)
+				parmse.expr = save_expr (parmse.expr);
+
 			      /* Set the _data field.  */
 			      tmp = gfc_class_data_get (var);
 			      efield = fold_convert (TREE_TYPE (tmp),
