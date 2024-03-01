13bad1ac7a6ea4dbbde67c69d31c218a2f2d7a5d
Zac Walker <zacwalker@microsoft.com>
2024-03-01 01:40:53 +0100

    Introduce aarch64-w64-mingw32 target
    Add the initial aarch64-w64-mingw32 target for gcc.
    
    This is the first commit in a sequence of patch series to add
    new aarch64-w64-mingw32 target.
    
    Coauthors: Zac Walker <zacwalker@microsoft.com>,
    Mark Harmstone <mark@harmstone.com>  and
    Ron Riddle <ron.riddle@microsoft.com>
    
    Refactored, prepared, and validated by
    Radek Barton <radek.barton@microsoft.com> and
    Evgeny Karpov <evgeny.karpov@microsoft.com>
    
    fixincludes/ChangeLog:
    
    	* mkfixinc.sh: Extend for *-mingw32* targets.
    
    gcc/ChangeLog:
    
    	* config.gcc: Add aarch64-w64-mingw32 target.

--- fixincludes/mkfixinc.sh
+++ fixincludes/mkfixinc.sh
@@ -12,8 +12,7 @@ target=fixinc.sh
 # Check for special fix rules for particular targets
 case $machine in
     i?86-*-cygwin* | \
-    i?86-*-mingw32* | \
-    x86_64-*-mingw32* | \
+    *-mingw32* | \
     powerpc-*-eabisim* | \
     powerpc-*-eabi*    | \
     powerpc-*-rtems*   | \
