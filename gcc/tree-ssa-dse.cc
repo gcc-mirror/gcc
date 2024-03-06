0249744a9fe0775c2c895727aeebec4c59fd5f95
Richard Biener <rguenther@suse.de>
2024-03-06 09:02:31 +0100

    tree-optimization/114246 - invalid call argument from DSE
    The following makes sure to strip type conversions added by
    build_fold_addr_expr before placing the result in a call argument.
    
    	PR tree-optimization/114246
    	* tree-ssa-dse.cc (increment_start_addr): Strip useless
    	type conversions from the adjusted address.
    
    	* gcc.dg/torture/pr114246.c: New testcase.

--- gcc/tree-ssa-dse.cc
+++ gcc/tree-ssa-dse.cc
@@ -49,6 +49,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "cfgloop.h"
 #include "tree-data-ref.h"
 #include "internal-fn.h"
+#include "tree-ssa.h"
 
 /* This file implements dead store elimination.
 
@@ -658,6 +659,7 @@ increment_start_addr (gimple *stmt, tree *where, int increment)
 					      *where,
 					      build_int_cst (ptr_type_node,
 							     increment)));
+  STRIP_USELESS_TYPE_CONVERSION (*where);
 }
 
 /* STMT is builtin call that writes bytes in bitmap ORIG, some bytes are dead
