6cb5ef37c2fac240b68d8ee438aba4885956269f
Nathaniel Shead <nathanieloshead@gmail.com>
2024-03-16 00:11:25 +1100

    testsuite: Fix excess errors for new modules testcases on powerpc [PR114320]
    On some configurations, PowerPC emits -Wpsabi warnings when using IEEE
    long doubles on a machine configured with IBM long double by default.
    This patch suppresses these warnings for this testcase.
    
    	PR testsuite/114320
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/modules/target-powerpc-1_a.C: Suppress -Wpsabi.
    	* g++.dg/modules/target-powerpc-1_b.C: Likewise.
    
    Signed-off-by: Nathaniel Shead <nathanieloshead@gmail.com>

--- gcc/testsuite/g++.dg/modules/target-powerpc-1_b.C
+++ gcc/testsuite/g++.dg/modules/target-powerpc-1_b.C
@@ -1,7 +1,7 @@
 // PR c++/98645
 // { dg-module-do compile { target powerpc*-*-* } }
 // { dg-require-effective-target ppc_float128_sw }
-// { dg-additional-options "-fmodules-ts -mfloat128 -mabi=ieeelongdouble" }
+// { dg-additional-options "-fmodules-ts -mfloat128 -mabi=ieeelongdouble -Wno-psabi" }
 
 import M;
 
