5213047b1d50af63dfabb5e5649821a6cb157e33
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2024-03-16 09:50:00 +0100

    libcc1: fix <vector> include
    Use INCLUDE_VECTOR before including system.h, instead of directly
    including <vector>, to avoid running into poisoned identifiers.
    
    Signed-off-by: Dimitry Andric <dimitry@andric.com>
    
    libcc1/ChangeLog:
    
    	PR middle-end/111632
    	* libcc1plugin.cc: Fix include.
    	* libcp1plugin.cc: Fix include.

--- libcc1/libcp1plugin.cc
+++ libcc1/libcp1plugin.cc
@@ -33,6 +33,7 @@
 #undef PACKAGE_VERSION
 
 #define INCLUDE_MEMORY
+#define INCLUDE_VECTOR
 #include "gcc-plugin.h"
 #include "system.h"
 #include "coretypes.h"
@@ -70,8 +71,6 @@
 #include "marshall-cp.hh"
 #include "rpc.hh"
 #include "context.hh"
-
-#include <vector>
 
 using namespace cc1_plugin;
 
