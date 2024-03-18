729170a45040e96d567be79bda00604a7645a23d
Iain Sandoe <iain@sandoe.co.uk>
2024-03-18 10:06:44 +0000

    testsuite, Darwin: Use the IOKit framework in framework-1.c [PR114049].
    The intent of the test is to show that we find a framework that
    is installed in /System/Library/Frameworks when the user has added
    a '-F' option.  The trick is to choose some header that is present
    for all the Darwin versions we support and that does not contain any
    content we cannot parse.  We had been using the Kernel framework for
    this, but recent SDK versions have revealed that this is not suitable.
    
    Replacing with a use of IOKit.
    
    	PR target/114049
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/framework-1.c: Use an IOKit header instead of a
    	Kernel one.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>
    (cherry picked from commit 4adb1a5839e7a3310a127c1776f1f95d7edaa6ff)

--- gcc/testsuite/gcc.dg/framework-1.c
+++ gcc/testsuite/gcc.dg/framework-1.c
@@ -1,4 +1,10 @@
 /* { dg-do compile { target *-*-darwin* } } */
 /* { dg-options "-F." } */
 
-#include <Kernel/string.h>
+/* The intent of the test is to show that we find a framework that
+   is installed in /System/Library/Frameworks when the user has added
+   a '-F' option.  The trick is to choose some header that is present
+   for all the Darwin versions we support and that does not contain any
+   content we cannot parse.  */
+
+#include <IOKit/IOReturn.h>
