94c3508c5a14d1948fe3bffa9e16c6f3d9c2836a
Richard Biener <rguenther@suse.de>
2024-03-18 12:39:03 +0100

    tree-optimization/114375 - disallow SLP discovery of permuted mask loads
    We cannot currently handle permutations of mask loads in code generation
    or permute optimization.  But we simply drop any permutation on the
    floor, so the following instead rejects the SLP build rather than
    producing wrong-code.  I've also made sure to reject them in
    vectorizable_load for completeness.
    
    	PR tree-optimization/114375
    	* tree-vect-slp.cc (vect_build_slp_tree_2): Compute the
    	load permutation for masked loads but reject it when any
    	such is necessary.
    	* tree-vect-stmts.cc (vectorizable_load): Reject masked
    	VMAT_ELEMENTWISE and VMAT_STRIDED_SLP as those are not
    	supported.
    
    	* gcc.dg/vect/vect-pr114375.c: New testcase.

---
+++ gcc/testsuite/gcc.dg/vect/vect-pr114375.c
@@ -0,0 +1,44 @@
+/* { dg-additional-options "-mavx2" { target avx2_runtime } } */
+
+#include "tree-vect.h"
+
+int a[512];
+int b[512];
+int c[512];
+
+void __attribute__((noipa))
+foo(int * __restrict p)
+{
+  for (int i = 0; i < 64; ++i)
+    {
+      int tem = 2, tem2 = 2;
+      if (a[4*i + 1])
+        tem = p[4*i];
+      if (a[4*i])
+        tem2 = p[4*i + 2];
+      b[2*i] = tem2;
+      b[2*i+1] = tem;
+      if (a[4*i + 2])
+        tem = p[4*i + 1];
+      if (a[4*i + 3])
+        tem2 = p[4*i + 3];
+      c[2*i] = tem2;
+      c[2*i+1] = tem;
+    }
+}
+int main()
+{
+  check_vect ();
+
+  for (int i = 0; i < 512; ++i)
+    a[i] = (i >> 1) & 1;
+
+  foo (a);
+
+  if (c[0] != 1 || c[1] != 0 || c[2] != 1 || c[3] != 0
+      || b[0] != 2 || b[1] != 2 || b[2] != 2 || b[3] != 2)
+    abort ();
+
+  return 0;
+}
+
