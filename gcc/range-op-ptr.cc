76fae4051a72b2d417d50f1980dff8ab0c50d0c5
Aldy Hernandez <aldyh@redhat.com>
2024-03-20 11:10:03 +0100

    Implement operator_gt for prange.
    gcc/ChangeLog:
    
    	* range-op-mixed.h: Add overloaded declarations for pointer variants.
    	* range-op-ptr.cc (operator_gt::fold_range): New.
    	(operator_gt::op1_range): New.
    	(operator_gt::op2_range): New.
    	(operator_gt::op1_op2_relation): New.
    	(operator_gt::pointers_handled_p): New.

--- gcc/range-op-ptr.cc
+++ gcc/range-op-ptr.cc
@@ -1747,6 +1747,112 @@ operator_le::pointers_handled_p (range_op_dispatch_type type,
     }
 }
 
+bool
+operator_gt::fold_range (irange &r, tree type,
+			 const prange &op1, const prange &op2,
+			 relation_trio rel) const
+{
+  if (relop_early_resolve (r, type, op1, op2, rel, VREL_GT))
+    return true;
+
+  signop sign = TYPE_SIGN (op1.type ());
+  gcc_checking_assert (sign == TYPE_SIGN (op2.type ()));
+
+  if (wi::gt_p (op1.lower_bound (), op2.upper_bound (), sign))
+    r = range_true ();
+  else if (!wi::gt_p (op1.upper_bound (), op2.lower_bound (), sign))
+    r = range_false ();
+  else
+    r = range_true_and_false ();
+
+  //update_known_bitmask (r, GT_EXPR, op1, op2);
+  return true;
+}
+
+bool
+operator_gt::op1_range (prange &r, tree type,
+			const irange &lhs, const prange &op2,
+			relation_trio) const
+{
+  if (op2.undefined_p ())
+    return false;
+
+  switch (get_bool_state (r, lhs, type))
+    {
+    case BRS_TRUE:
+      build_gt (r, type, op2);
+      break;
+
+    case BRS_FALSE:
+      build_le (r, type, op2);
+      break;
+
+    default:
+      break;
+    }
+  return true;
+}
+
+bool
+operator_gt::op2_range (prange &r, tree type,
+			const irange &lhs,
+			const prange &op1,
+			relation_trio) const
+{
+  if (op1.undefined_p ())
+    return false;
+
+  switch (get_bool_state (r, lhs, type))
+    {
+    case BRS_TRUE:
+      build_lt (r, type, op1);
+      break;
+
+    case BRS_FALSE:
+      build_ge (r, type, op1);
+      break;
+
+    default:
+      break;
+    }
+  return true;
+}
+
+relation_kind
+operator_gt::op1_op2_relation (const irange &lhs, const prange &,
+			       const prange &) const
+{
+  if (lhs.undefined_p ())
+    return VREL_UNDEFINED;
+
+  // FALSE = op1 > op2 indicates LE_EXPR.
+  if (lhs.zero_p ())
+    return VREL_LE;
+
+  // TRUE = op1 > op2 indicates GT_EXPR.
+  if (!range_includes_zero_p (lhs))
+    return VREL_GT;
+  return VREL_VARYING;
+}
+
+bool
+operator_gt::pointers_handled_p (range_op_dispatch_type type,
+				 unsigned dispatch) const
+{
+  switch (type)
+    {
+    case DISPATCH_FOLD_RANGE:
+      return dispatch == RO_IPP;
+    case DISPATCH_OP1_RANGE:
+    case DISPATCH_OP2_RANGE:
+      return dispatch == RO_PIP;
+    case DISPATCH_OP1_OP2_RELATION:
+      return dispatch == RO_IPP;
+    default:
+      return true;
+    }
+}
+
 // Initialize any pointer operators to the primary table
 
 void
