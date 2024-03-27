3435f4e92b06747eb7d3777e8f60aa80bc495263
Owen Avery <powerboat9.gamer@gmail.com>
2024-03-26 20:16:08 -0400

    gccrs: Improve parsing of raw byte string literals
    gcc/rust/ChangeLog:
    
    	* lex/rust-lex.cc
    	(Lexer::parse_raw_byte_string):
    	Bring handling of edge cases to par with parse_byte_string.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/raw-byte-string-loc.rs: New test.
    
    Signed-off-by: Owen Avery <powerboat9.gamer@gmail.com>

---
+++ gcc/testsuite/rust/compile/raw-byte-string-loc.rs
@@ -0,0 +1,6 @@
+const X: &'static u8 = br#"12
+12"#;
+
+BREAK
+// { dg-error "unrecognised token" "" { target *-*-* } .-1 }
+// { dg-excess-errors "error 'failed to parse item' does not have location" }
