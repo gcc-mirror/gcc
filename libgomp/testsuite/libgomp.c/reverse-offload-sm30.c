679f81a32f706645f45900fdb1659fb5fe607f77
Thomas Schwinge <tschwinge@baylibre.com>
2024-04-05 14:04:53 +0200

    nvptx: In mkoffload.cc, call diagnostic_color_init + gcc_init_libintl: Restore 'libgomp.c/reverse-offload-sm30.c' testing
    With commit 7520a4992c94254016085a461c58c972497c4483
    "nvptx: In mkoffload.cc, call diagnostic_color_init + gcc_init_libintl",
    we regressed:
    
        [-PASS:-]{+FAIL:+} libgomp.c/reverse-offload-sm30.c  at line 15 (test for warnings, line )
        [-PASS:-]{+FAIL:+} libgomp.c/reverse-offload-sm30.c (test for excess errors)
    
    	libgomp/
    	* testsuite/libgomp.c/reverse-offload-sm30.c: Set 'GCC_COLORS' to the empty string.

--- libgomp/testsuite/libgomp.c/reverse-offload-sm30.c
+++ libgomp/testsuite/libgomp.c/reverse-offload-sm30.c
@@ -12,4 +12,7 @@ main ()
   return 0;
 }
 
+/* The 'mkoffload's currently don't obey '-fno-diagnostics-color' etc., so use a different way to effect the same thing:
+   { dg-set-compiler-env-var GCC_COLORS "" }
+   ..., so that the following regexp doesn't have to deal with color code escape sequences.  */
 /* { dg-warning "'omp requires reverse_offload' requires at least 'sm_35' for '-foffload-options=nvptx-none=-march=' - disabling offload-code generation for this device type" "" { target *-*-* } 0 } */
