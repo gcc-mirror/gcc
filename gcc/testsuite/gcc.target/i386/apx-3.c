18e94e04dae724c61cbc13ace85fa68f2deda900
H.J. Lu <hjl.tools@gmail.com>
2024-04-08 18:57:49 -0700

    x86: Define __APX_INLINE_ASM_USE_GPR32__
    Define __APX_INLINE_ASM_USE_GPR32__ for -mapx-inline-asm-use-gpr32.
    When __APX_INLINE_ASM_USE_GPR32__ is defined, inline asm statements
    should contain only instructions compatible with r16-r31.
    
    gcc/
    
    	PR target/114587
    	* config/i386/i386-c.cc (ix86_target_macros_internal): Define
    	__APX_INLINE_ASM_USE_GPR32__ for -mapx-inline-asm-use-gpr32.
    
    gcc/testsuite/
    
    	PR target/114587
    	* gcc.target/i386/apx-3.c: Likewise.

---
+++ gcc/testsuite/gcc.target/i386/apx-3.c
@@ -0,0 +1,6 @@
+/* { dg-do compile { target { ! ia32 } } } */
+/* { dg-options "-mapx-inline-asm-use-gpr32" } */
+
+#ifndef __APX_INLINE_ASM_USE_GPR32__
+# error __APX_INLINE_ASM_USE_GPR32__ not defined
+#endif
