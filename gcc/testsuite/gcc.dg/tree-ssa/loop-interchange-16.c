b6c8259076a336e8082853ed6dda083c25a465d0
Stefan Schulze Frielinghaus <stefansf@linux.ibm.com>
2024-04-12 09:20:53 +0200

    testsuite: Fix loop-interchange-16.c
    Prevent loop unrolling of the innermost loop because otherwise we are
    left with no loop interchange for targets like s390 which have a more
    aggressive loop unrolling strategy.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/tree-ssa/loop-interchange-16.c: Prevent loop unrolling
    	of the innermost loop.

--- gcc/testsuite/gcc.dg/tree-ssa/loop-interchange-16.c
+++ gcc/testsuite/gcc.dg/tree-ssa/loop-interchange-16.c
@@ -11,6 +11,7 @@ double s231(int iterations)
 //    loop with data dependency
     for (int nl = 0; nl < 100*(iterations/LEN_2D); nl++) {
         for (int i = 0; i < LEN_2D; ++i) {
+#pragma GCC unroll 0
             for (int j = 1; j < LEN_2D; j++) {
                 aa[j][i] = aa[j - 1][i] + bb[j][i];
             }
