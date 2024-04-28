818ec41d50bc906523f8fd86d7c463dd90e16d24
Alexandre Oliva <oliva@adacore.com>
2024-04-28 04:30:00 -0300

    [testsuite] [ppc64] expect error on vxworks too
    These ppc lp64 tests check for errors or warnings on -mno-powerpc64.
    On powerpc64-*-vxworks* we get the same errors as on most other
    covered platforms, but the tests did not mark them as expected for
    this target.  On powerpc-*-vxworks*, the tests are skipped because
    lp64 is not satisfied, so I'm naming powerpc*-*-vxworks* rather than
    something more specific.
    
    
    for  gcc/testsuite/ChangeLog
    
    	* gcc.target/powerpc/pr106680-1.c: Error on vxworks too.
    	* gcc.target/powerpc/pr106680-2.c: Likewise.
    	* gcc.target/powerpc/pr106680-3.c: Likewise.

--- gcc/testsuite/gcc.target/powerpc/pr106680-1.c
+++ gcc/testsuite/gcc.target/powerpc/pr106680-1.c
@@ -8,6 +8,6 @@ int foo ()
   return 1;
 }
 
-/* { dg-error "'-m64' requires a PowerPC64 cpu" "PR106680" { target powerpc*-*-linux* powerpc*-*-freebsd* powerpc-*-rtems* } 0 } */
+/* { dg-error "'-m64' requires a PowerPC64 cpu" "PR106680" { target powerpc*-*-linux* powerpc*-*-freebsd* powerpc-*-rtems* powerpc*-*-vxworks* } 0 } */
 /* { dg-warning "'-m64' requires PowerPC64 architecture, enabling" "PR106680" { target powerpc*-*-darwin* } 0 } */
 /* { dg-warning "'-maix64' requires PowerPC64 architecture remain enabled" "PR106680" { target powerpc*-*-aix* } 0 } */
