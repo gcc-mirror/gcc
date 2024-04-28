bc07fa6af54cc67f7fc85cc144f9686fad07f205
Alexandre Oliva <oliva@adacore.com>
2024-04-28 04:30:30 -0300

    [testsuite] require sqrt_insn effective target where needed
    Some tests fail on ppc and ppc64 when testing a compiler [with options
    for] for a CPU [emulator] that doesn't support the sqrt insn.
    
    The gcc.dg/cdce3.c is one in which the expected shrink-wrap
    optimization only takes place when the target CPU supports a sqrt
    insn.
    
    The gcc.target/powerpc/pr46728-1[0-4].c tests use -mpowerpc-gpopt and
    call sqrt(), which involves the sqrt insn that the target CPU under
    test may not support.
    
    Require a sqrt_insn effective target for all the affected tests.
    
    
    for  gcc/testsuite/ChangeLog
    
    	* gcc.dg/cdce3.c: Require sqrt_insn effective target.
    	* gcc.target/powerpc/pr46728-10.c: Likewise.  Drop darwin
    	explicit skipping.
    	* gcc.target/powerpc/pr46728-11.c: Likewise.  Likewise.
    	* gcc.target/powerpc/pr46728-13.c: Likewise.  Likewise.
    	* gcc.target/powerpc/pr46728-14.c: Likewise.  Likewise.

--- gcc/testsuite/gcc.dg/cdce3.c
+++ gcc/testsuite/gcc.dg/cdce3.c
@@ -1,7 +1,8 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target hard_float } */
+/* { dg-require-effective-target sqrt_insn } */
 /* { dg-options "-O2 -fmath-errno -fdump-tree-cdce-details -fdump-tree-optimized" } */
-/* { dg-final { scan-tree-dump "cdce3.c:11: \[^\n\r]* function call is shrink-wrapped into error conditions\." "cdce" } } */
+/* { dg-final { scan-tree-dump "cdce3.c:12: \[^\n\r]* function call is shrink-wrapped into error conditions\." "cdce" } } */
 /* { dg-final { scan-tree-dump "sqrtf \\(\[^\n\r]*\\); \\\[tail call\\\]" "optimized" } } */
 /* { dg-skip-if "doesn't have a sqrtf insn" { mmix-*-* } } */
 
--- gcc/testsuite/gcc.target/powerpc/pr46728-11.c
+++ gcc/testsuite/gcc.target/powerpc/pr46728-11.c
@@ -1,6 +1,6 @@
 /* { dg-do run } */
-/* { dg-skip-if "-mpowerpc-gpopt not supported" { powerpc*-*-darwin* } } */
 /* { dg-options "-O2 -ffast-math -fno-inline -fno-unroll-loops -lm -mpowerpc-gpopt" } */
+/* { dg-require-effective-target sqrt_insn } */
 
 #include <math.h>
 
