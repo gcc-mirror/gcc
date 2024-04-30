61bc95e26db6fb58d98a119a04f433d5f14d9550
Julian Brown <julian@codesourcery.com>
2024-04-30 19:21:22 +0200

    OpenMP: Allow complete replacement of clause during map/to/from expansion
    At present, map/to/from clauses on OpenMP "target" directives may be
    expanded into several mapping nodes if they describe array sections with
    pointer or reference bases, or similar.  This patch allows the original
    clause to be replaced during that expansion, mostly by passing the list
    pointer to the node to various functions rather than the node itself.
    
    This is needed by the following patch. There shouldn't be any functional
    changes introduced by this patch itself.
    
    2023-09-05  Julian Brown  <julian@codesourcery.com>
    
    gcc/c-family/
    	* c-common.h (expand_array_base, expand_component_selector,
    	expand_map_clause): Adjust member declarations.
    	* c-omp.cc (omp_expand_access_chain): Pass and return pointer to
    	clause.
    	(c_omp_address_inspector::expand_array_base): Likewise.
    	(c_omp_address_inspector::expand_component_selector): Likewise.
    	(c_omp_address_inspector::expand_map_clause): Likewise.
    
    gcc/c/
    	* c-typeck.cc (handle_omp_array_sections): Pass pointer to clause to
    	process instead of clause.
    	(c_finish_omp_clauses): Update calls to handle_omp_array_sections.
    	Handle cases where initial clause might be replaced.
    
    gcc/cp/
    	* semantics.cc (handle_omp_array_sections): Pass pointer to clause
    	instead of clause.  Add PNEXT return parameter for next clause in list
    	to process.
    	(finish_omp_clauses): Update calls to handle_omp_array_sections.
    	Handle cases where initial clause might be replaced.

--- gcc/c/c-typeck.cc
+++ gcc/c/c-typeck.cc
@@ -14234,8 +14234,9 @@ handle_omp_array_sections_1 (tree c, tree t, vec<tree> &types,
 /* Handle array sections for clause C.  */
 
 static bool
-handle_omp_array_sections (tree &c, enum c_omp_region_type ort)
+handle_omp_array_sections (tree *pc, tree **pnext, enum c_omp_region_type ort)
 {
+  tree c = *pc;
   bool maybe_zero_len = false;
   unsigned int first_non_one = 0;
   bool non_contiguous = false;
@@ -14481,10 +14482,12 @@ handle_omp_array_sections (tree &c, enum c_omp_region_type ort)
 
       c_omp_address_inspector ai (OMP_CLAUSE_LOCATION (c), t);
 
-      tree nc = ai.expand_map_clause (c, first, addr_tokens, ort);
-      if (nc != error_mark_node)
+      tree *npc = ai.expand_map_clause (pc, first, addr_tokens, ort);
+      if (npc != NULL)
 	{
 	  using namespace omp_addr_tokenizer;
+
+	  c = *pc;
 
 	  if (ai.maybe_zero_length_array_section (c))
 	    OMP_CLAUSE_MAP_MAYBE_ZERO_LENGTH_ARRAY_SECTION (c) = 1;
@@ -14492,12 +14495,14 @@ handle_omp_array_sections (tree &c, enum c_omp_region_type ort)
 	  /* !!! If we're accessing a base decl via chained access
 	     methods (e.g. multiple indirections), duplicate clause
 	     detection won't work properly.  Skip it in that case.  */
-	  if ((addr_tokens[0]->type == STRUCTURE_BASE
-	       || addr_tokens[0]->type == ARRAY_BASE)
+	  if (pnext
+	      && (addr_tokens[0]->type == STRUCTURE_BASE
+		  || addr_tokens[0]->type == ARRAY_BASE)
 	      && addr_tokens[0]->u.structure_base_kind == BASE_DECL
 	      && addr_tokens[1]->type == ACCESS_METHOD
 	      && omp_access_chain_p (addr_tokens, 1))
-	    c = nc;
+	    /* NPC points to the last node in the new sequence.  */
+	    *pnext = npc;
 
 	  return false;
 	}
@@ -14845,12 +14850,13 @@ c_finish_omp_clauses (tree clauses, enum c_omp_region_type ort)
 	  t = OMP_CLAUSE_DECL (c);
 	  if (TREE_CODE (t) == OMP_ARRAY_SECTION)
 	    {
-	      if (handle_omp_array_sections (c, ort))
+	      if (handle_omp_array_sections (pc, NULL, ort))
 		{
 		  remove = true;
 		  break;
 		}
 
+	      c = *pc;
 	      t = OMP_CLAUSE_DECL (c);
 	      if (OMP_CLAUSE_CODE (c) == OMP_CLAUSE_REDUCTION
 		  && OMP_CLAUSE_REDUCTION_INSCAN (c))
@@ -15574,10 +15580,12 @@ c_finish_omp_clauses (tree clauses, enum c_omp_region_type ort)
 	    last_iterators = NULL_TREE;
 	  if (TREE_CODE (t) == OMP_ARRAY_SECTION)
 	    {
-	      if (handle_omp_array_sections (c, ort))
+	      if (handle_omp_array_sections (pc, NULL, ort))
 		remove = true;
-	      else if (OMP_CLAUSE_CODE (c) == OMP_CLAUSE_DEPEND
-		       && OMP_CLAUSE_DEPEND_KIND (c) == OMP_CLAUSE_DEPEND_DEPOBJ)
+	      else if ((c = *pc)
+		       && OMP_CLAUSE_CODE (c) == OMP_CLAUSE_DEPEND
+		       && (OMP_CLAUSE_DEPEND_KIND (c)
+			   == OMP_CLAUSE_DEPEND_DEPOBJ))
 		{
 		  error_at (OMP_CLAUSE_LOCATION (c),
 			    "%<depend%> clause with %<depobj%> dependence "
@@ -15693,10 +15701,12 @@ c_finish_omp_clauses (tree clauses, enum c_omp_region_type ort)
 		grp_start_p = pc;
 		grp_sentinel = OMP_CLAUSE_CHAIN (c);
 
-		if (handle_omp_array_sections (c, ort))
+		tree *pnext = NULL;
+		if (handle_omp_array_sections (pc, &pnext, ort))
 		  remove = true;
 		else
 		  {
+		    c = *pc;
 		    t = OMP_CLAUSE_DECL (c);
 		    if (!omp_mappable_type (TREE_TYPE (t)))
 		      {
@@ -15792,6 +15802,8 @@ c_finish_omp_clauses (tree clauses, enum c_omp_region_type ort)
 		     clauses, reset the OMP_CLAUSE_SIZE (representing a bias)
 		     to zero here.  */
 		  OMP_CLAUSE_SIZE (c) = size_zero_node;
+		if (pnext)
+		  c = *pnext;
 		break;
 	      }
 	    else if (!omp_parse_expr (addr_tokens, t))
@@ -15990,10 +16002,10 @@ c_finish_omp_clauses (tree clauses, enum c_omp_region_type ort)
 	      {
 		grp_start_p = pc;
 		grp_sentinel = OMP_CLAUSE_CHAIN (c);
-		tree nc = ai.expand_map_clause (c, OMP_CLAUSE_DECL (c),
-						addr_tokens, ort);
-		if (nc != error_mark_node)
-		  c = nc;
+		tree *npc = ai.expand_map_clause (pc, OMP_CLAUSE_DECL (c),
+						  addr_tokens, ort);
+		if (npc != NULL)
+		  c = *npc;
 	      }
 	  }
 	  break;
@@ -16093,10 +16105,11 @@ c_finish_omp_clauses (tree clauses, enum c_omp_region_type ort)
 	  t = OMP_CLAUSE_DECL (c);
 	  if (TREE_CODE (t) == OMP_ARRAY_SECTION)
 	    {
-	      if (handle_omp_array_sections (c, ort))
+	      if (handle_omp_array_sections (pc, NULL, ort))
 		remove = true;
 	      else
 		{
+		  c = *pc;
 		  t = OMP_CLAUSE_DECL (c);
 		  while (TREE_CODE (t) == ARRAY_REF)
 		    t = TREE_OPERAND (t, 0);
