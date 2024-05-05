8facb98b23ab21368b753edc1210c36e4a2230d4
Iain Sandoe <iain@sandoe.co.uk>
2024-05-05 14:16:36 +0100

    testsuite, darwin: XFAIL tests with -g3 on systems using dyly-ld.
    Currently, Darwin's static linker does not handle the output of -g3.
    We have no way to remedy this, so xfail the tests for current OS versions.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/debug/dwarf2/dwarf2-macro.c: XFAIL for Darwin23+.
    	* gcc.dg/debug/dwarf2/dwarf2-macro2.c: Likewise.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>
    (cherry picked from commit fe881353f13d8b7212faf1f91e6eb022f9edd233)

--- gcc/testsuite/gcc.dg/debug/dwarf2/dwarf2-macro.c
+++ gcc/testsuite/gcc.dg/debug/dwarf2/dwarf2-macro.c
@@ -1,7 +1,7 @@
 /* Test to make sure the mcaro info includes a start file command for the main source */
 /* { dg-do compile } */
 /* { dg-options "-g3 -gdwarf -dA -fverbose-asm" } */
-/* { dg-final { scan-assembler "Start new file" } } */
+/* { dg-final { scan-assembler "Start new file"  { xfail *-*-darwin23* } } } */
 
 #define ADD(x) (M + x)
 
--- gcc/testsuite/gcc.dg/debug/dwarf2/dwarf2-macro2.c
+++ gcc/testsuite/gcc.dg/debug/dwarf2/dwarf2-macro2.c
@@ -1,7 +1,7 @@
 /* Test to make sure the macro info includes the predefined macros with line number 0.  */
 /* { dg-do compile } */
 /* { dg-options "-g3 -gdwarf -dA -fverbose-asm" } */
-/* { dg-final { scan-assembler "At line number 0" } } */
+/* { dg-final { scan-assembler "At line number 0" { xfail *-*-darwin23* } } } */
 
 #define FOO 1
 int i;
