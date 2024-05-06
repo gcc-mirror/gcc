3a5acd2583056e8cd0e5fda83e7c34be65415c62
Paul Thomas <pault@gcc.gnu.org>
2024-05-06 08:21:14 +0100

    Fortran: Fix ICE and clear incorrect error messages [PR114739]
    2024-05-06  Paul Thomas  <pault@gcc.gnu.org>
    
    gcc/fortran
    	PR fortran/114739
    	* primary.cc (gfc_match_varspec): Check for default type before
    	checking for derived types with the right component name.
    
    gcc/testsuite/
    	PR fortran/114739
    	* gfortran.dg/pr114739.f90: New test.
    	* gfortran.dg/derived_comp_array_ref_8.f90: Add 'implicit none'
    	for consistency with expected error message.
    	* gfortran.dg/nullify_4.f90: ditto
    	* gfortran.dg/pointer_init_6.f90: ditto
    	* gfortran.dg/pr107397.f90: ditto
    	* gfortran.dg/pr88138.f90: ditto
    
    (cherry picked from commit 094f8a36dffea52fe6a64596203b82648a3f6121)

--- gcc/testsuite/gfortran.dg/derived_comp_array_ref_8.f90
+++ gcc/testsuite/gfortran.dg/derived_comp_array_ref_8.f90
@@ -2,6 +2,7 @@
 !
 ! PR fortran/52325
 !
+implicit none
 real :: f
 cc%a = 5 ! { dg-error "Symbol 'cc' at .1. has no IMPLICIT type" }
 f%a = 5  ! { dg-error "Unexpected '%' for nonderived-type variable 'f' at" }
