1073469074ff132478ec8d923ed8635c672f7d9b
Iain Sandoe <iain@sandoe.co.uk>
2024-05-11 09:24:33 +0100

    testsuite, C++, Darwin: Skip cxa_atexit-6, which is not applicable.
    For Darwin, non-weak functions defined in a TU always bind locally
    and so cxa_atexit-6.C is not applicable here.
    
    	PR testsuite/114982
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/tree-ssa/cxa_atexit-6.C: Skip for Darwin.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/g++.dg/tree-ssa/cxa_atexit-6.C
+++ gcc/testsuite/g++.dg/tree-ssa/cxa_atexit-6.C
@@ -2,10 +2,14 @@
 /* { dg-require-effective-target fpic } */
 /* { dg-options "-O2 -fdump-tree-cddce1-details -fdump-tree-optimized -fPIC" } */
 // { dg-require-effective-target cxa_atexit }
+/* This test is not appropriate for targets where non-weak functions defined
+   in the TU always bind locally; see PR114982.  */
+/* { dg-skip-if "PR114982" { *-*-darwin* } } */
 /* PR tree-optimization/19661 */
 
 /* The call to axexit should not be removed as A::~A() cannot be figured if it
-   is a pure/const function call as the function call g does not bind locally. */
+   is a pure/const function call for platforms where the function call g does
+   not bind locally. */
 
 __attribute__((noinline))
 void g() {}
