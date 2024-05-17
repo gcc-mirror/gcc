bc6e336cb7c85094ddc77757be97c3d8588f35ca
Aldy Hernandez <aldyh@redhat.com>
2024-05-17 10:30:03 +0200

    [prange] Avoid looking at type() for undefined ranges
    Undefined ranges have no type.  This patch fixes the thinko.
    
    gcc/ChangeLog:
    
    	PR middle-end/115128
    	* ipa-cp.cc (ipa_value_range_from_jfunc): Check for undefined_p
    	before looking at type.
    	(propagate_vr_across_jump_function): Same.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/tree-ssa/pr115128.c: New test.

--- gcc/ipa-cp.cc
+++ gcc/ipa-cp.cc
@@ -1744,6 +1744,8 @@ ipa_value_range_from_jfunc (vrange &vr,
 		 pointer type to hold the result instead of a boolean
 		 type.  Avoid trapping in the sanity check in
 		 fold_range until this is fixed.  */
+	      || srcvr.undefined_p ()
+	      || op_vr.undefined_p ()
 	      || !handler.operand_check_p (vr_type, srcvr.type (), op_vr.type ())
 	      || !handler.fold_range (op_res, vr_type, srcvr, op_vr))
 	    op_res.set_varying (vr_type);
@@ -2556,6 +2558,8 @@ propagate_vr_across_jump_function (cgraph_edge *cs, ipa_jump_func *jfunc,
 		 pointer type to hold the result instead of a boolean
 		 type.  Avoid trapping in the sanity check in
 		 fold_range until this is fixed.  */
+	      || src_lats->m_value_range.m_vr.undefined_p ()
+	      || op_vr.undefined_p ()
 	      || !handler.operand_check_p (operand_type,
 					   src_lats->m_value_range.m_vr.type (),
 					   op_vr.type ())
