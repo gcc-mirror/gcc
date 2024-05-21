7a9a6091b81d8579ab0470e4e21b5682d4ee4ef4
Kewen Lin <linkw@linux.ibm.com>
2024-05-20 21:01:08 -0500

    testsuite, rs6000: Remove effective target powerpc_405_nocache
    With the introduction of -mdejagnu-cpu=, when the test case
    is specifying -mdejagnu-cpu=405, it would override the other
    possibly given -mcpu=, so it would compile for PowerPC 405
    for sure.  This patch is to remove the effective target
    powerpc_405_nocache and update all its uses.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/powerpc/405-dlmzb-strlen-1.c: Remove the line using
    	powerpc_405_nocache check.
    	* gcc.target/powerpc/405-macchw-1.c: Likewise.
    	* gcc.target/powerpc/405-macchw-2.c: Likewise.
    	* gcc.target/powerpc/405-macchwu-1.c: Likewise.
    	* gcc.target/powerpc/405-macchwu-2.c: Likewise.
    	* gcc.target/powerpc/405-machhw-1.c: Likewise.
    	* gcc.target/powerpc/405-machhw-2.c: Likewise.
    	* gcc.target/powerpc/405-machhwu-1.c: Likewise.
    	* gcc.target/powerpc/405-machhwu-2.c: Likewise.
    	* gcc.target/powerpc/405-maclhw-1.c: Likewise.
    	* gcc.target/powerpc/405-maclhw-2.c: Likewise.
    	* gcc.target/powerpc/405-maclhwu-1.c: Likewise.
    	* gcc.target/powerpc/405-maclhwu-2.c: Likewise.
    	* gcc.target/powerpc/405-mulchw-1.c: Likewise.
    	* gcc.target/powerpc/405-mulchw-2.c: Likewise.
    	* gcc.target/powerpc/405-mulchwu-1.c: Likewise.
    	* gcc.target/powerpc/405-mulchwu-2.c: Likewise.
    	* gcc.target/powerpc/405-mulhhw-1.c: Likewise.
    	* gcc.target/powerpc/405-mulhhw-2.c: Likewise.
    	* gcc.target/powerpc/405-mulhhwu-1.c: Likewise.
    	* gcc.target/powerpc/405-mulhhwu-2.c: Likewise.
    	* gcc.target/powerpc/405-mullhw-1.c: Likewise.
    	* gcc.target/powerpc/405-mullhw-2.c: Likewise.
    	* gcc.target/powerpc/405-mullhwu-1.c: Likewise.
    	* gcc.target/powerpc/405-mullhwu-2.c: Likewise.
    	* gcc.target/powerpc/405-nmacchw-1.c: Likewise.
    	* gcc.target/powerpc/405-nmacchw-2.c: Likewise.
    	* gcc.target/powerpc/405-nmachhw-1.c: Likewise.
    	* gcc.target/powerpc/405-nmachhw-2.c: Likewise.
    	* gcc.target/powerpc/405-nmaclhw-1.c: Likewise.
    	* gcc.target/powerpc/405-nmaclhw-2.c: Likewise.
    	* lib/target-supports.exp
    	(check_effective_target_powerpc_405_nocache): Remove.

--- gcc/testsuite/gcc.target/powerpc/405-dlmzb-strlen-1.c
+++ gcc/testsuite/gcc.target/powerpc/405-dlmzb-strlen-1.c
@@ -4,7 +4,6 @@
 /* { dg-skip-if "" { powerpc*-*-aix* } } */
 /* { dg-require-effective-target ilp32 } */
 /* { dg-options "-O2 -mdejagnu-cpu=405" } */
-/* { dg-skip-if "other options override -mcpu=405" { ! powerpc_405_nocache } } */
 
 /* { dg-final { scan-assembler "dlmzb\\. " } } */
 
