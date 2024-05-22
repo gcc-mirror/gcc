de7dbe64f859ab67ee0160cff6f8463569dd9739
jjasmine <tanghocle456@gmail.com>
2024-05-21 19:45:35 -0700

    Added faulty tests for inline asm cloberring
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/inline_asm_faulty_clobber.rs: New test.
    	* rust/compile/inline_asm_faulty_clobber_1.rs: New test.
    	* rust/compile/inline_asm_faulty_clobber_2.rs: New test.

---
+++ gcc/testsuite/rust/compile/inline_asm_faulty_clobber.rs
@@ -0,0 +1,10 @@
+#![feature(rustc_attrs)]
+
+#[rustc_builtin_macro]
+macro_rules! asm {
+    () => {}
+}
+
+fn main() {
+    asm!("nop", clobber_abi());  // { dg-error "at least one abi must be provided as an argument to `clobber_abi`" }
+}
