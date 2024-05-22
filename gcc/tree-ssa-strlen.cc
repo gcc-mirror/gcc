dbc9b45a3c2468ad134b3a9bd4961f7ae6bc1e67
Jakub Jelinek <jakub@redhat.com>
2024-05-22 09:13:50 +0200

    strlen: Fix up !si->full_string_p handling in count_nonzero_bytes_addr [PR115152]
    The following testcase is miscompiled because
    strlen_pass::count_nonzero_bytes_addr doesn't handle correctly
    the !si->full_string_p case.
    If si->full_string_p, it correctly computes minlen and maxlen as
    minimum and maximum length of the '\0' terminated stgring and
    clears *nulterm (ie. makes sure !full_string_p in the ultimate
    caller) if minlen is equal or larger than nbytes and so
    '\0' isn't guaranteed to be among those bytes.
    But in the !si->full_string_p case, all we know is that there
    are [minlen,maxlen] non-zero bytes followed by unknown bytes,
    so effectively the maxlen is infinite (but caller cares about only
    the first nbytes bytes) and furthermore, we never know if there is
    any '\0' char among those, so *nulterm needs to be always cleared.
    
    2024-05-22  Jakub Jelinek  <jakub@redhat.com>
    
    	PR tree-optimization/115152
    	* tree-ssa-strlen.cc (strlen_pass::count_nonzero_bytes_addr): If
    	!si->full_string_p, clear *nulterm and set maxlen to nbytes.
    
    	* gcc.dg/pr115152.c: New test.

--- gcc/tree-ssa-strlen.cc
+++ gcc/tree-ssa-strlen.cc
@@ -4829,7 +4829,7 @@ strlen_pass::count_nonzero_bytes_addr (tree exp, tree vuse, gimple *stmt,
       if (maxlen + 1 < nbytes)
 	return false;
 
-      if (nbytes <= minlen)
+      if (nbytes <= minlen || !si->full_string_p)
 	*nulterm = false;
 
       if (nbytes < minlen)
@@ -4838,6 +4838,9 @@ strlen_pass::count_nonzero_bytes_addr (tree exp, tree vuse, gimple *stmt,
 	  if (nbytes < maxlen)
 	    maxlen = nbytes;
 	}
+
+      if (!si->full_string_p)
+	maxlen = nbytes;
 
       if (minlen < lenrange[0])
 	lenrange[0] = minlen;
