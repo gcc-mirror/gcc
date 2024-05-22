f5c832e2f75d72b04fe84f7c5edd4d0351bfdb18
jjasmine <tanghocle456@gmail.com>
2024-05-21 17:54:57 -0700

    gccrs: Top level parsing test for asm!
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/inline_asm_ident_first.rs: New test.

---
+++ gcc/testsuite/rust/compile/inline_asm_ident_first.rs
@@ -0,0 +1,10 @@
+#![feature(rustc_attrs)]
+
+#[rustc_builtin_macro]
+macro_rules! asm {
+    () => {}
+}
+
+fn main() {
+    asm!(i_am_a_dummy); // { dg-error "asm template must be a string literal" }
+}
