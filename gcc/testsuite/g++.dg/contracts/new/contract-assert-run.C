7ca227f96d45f1582cdad16238950056c609cef4
Nina Ranns <dinka.ranns@gmail.com>
2024-05-28 10:50:29 +0100

    double error tests still not fixed

--- gcc/testsuite/g++.dg/contracts/new/contract-assert-run.C
+++ gcc/testsuite/g++.dg/contracts/new/contract-assert-run.C
@@ -7,23 +7,53 @@
 //   ensure that a missing colon after contract level errors
 //   ensure that an invalid contract role 'invalid' errors
 //   ensure that a missing colon after contract role errors
-// { dg-do compile }
-// { dg-options "-std=c++2a -fcontracts -fcontracts-nonattr" }
+// { dg-do run }
+// { dg-options "-std=c++2a -fcontracts -fcontracts-nonattr -fcontract-continuation-mode=on" }
+#include <iostream>
+#include <experimental/contract>
+
+#define VERIFY_ASSERT(statement, asserts)  \
+	{ \
+		bool violation = false;\
+		try{ \
+			statement; \
+		} catch(int &ex) { \
+			violation = true; \
+		} \
+		if ((asserts && !violation) || (!(asserts) && violation)) __builtin_abort(); \
+	} \
 
 static_assert (__cpp_contracts >= 201906);
 static_assert (__cpp_contracts_literal_semantics >= 201906);
 static_assert (__cpp_contracts_roles >= 201906);
 
+void handle_contract_violation(const std::experimental::contract_violation &violation) {
+  std::cerr << "custom std::handle_contract_violation called:"
+    << " " << violation.line_number()
+    << " " << violation.file_name()
+    << std::endl;
+  throw -(int)violation.line_number();
+}
+
 void foo(int x) pre (x>10){};
 
 int main()
 {
-  int x = 3;
+  VERIFY_ASSERT([[assert: false]], true);
+  VERIFY_ASSERT([[assert: true]], false);
 
-  contract_assert( x > 0 ? true : false);
-  contract_assert( x < 0 ? true : false);
+  VERIFY_ASSERT(contract_assert(true), false);
+  VERIFY_ASSERT(contract_assert(false), true);
 
-  foo(4);
+  int i = 4;
+  VERIFY_ASSERT(foo(i), true);
+  VERIFY_ASSERT(contract_assert( i == 4 ? true : false), false)
+  VERIFY_ASSERT(contract_assert( i > 4 ? true : false), true)
+
+  i = 18;
+  VERIFY_ASSERT(foo(i), false);
+  VERIFY_ASSERT(contract_assert( i == 4 ? true : false), true)
+  VERIFY_ASSERT(contract_assert( i > 4 ? true : false), false)
 
   return 0;
 }
