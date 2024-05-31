dd03ef700b718738955e53701b90a36f89588f28
jjasmine <tanghocle456@gmail.com>
2024-05-31 02:06:18 -0700

    Slim down the test cases
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/inline_asm_faulty_clobber.rs: compress
    	the test
    	* rust/compile/inline_asm_nop.rs: compress
    	the test
    	* rust/compile/inline_asm_faulty_clobber_1.rs: Removed.
    	* rust/compile/inline_asm_faulty_clobber_2.rs: Removed.
    	* rust/compile/inline_asm_nop_2.rs: Removed.

--- gcc/testsuite/rust/compile/inline_asm_faulty_clobber.rs
+++ gcc/testsuite/rust/compile/inline_asm_faulty_clobber.rs
@@ -8,5 +8,7 @@ macro_rules! asm {
 fn main() {
     unsafe {
         asm!("nop", clobber_abi());  // { dg-error "at least one abi must be provided as an argument to 'clobber_abi'" }
+        asm!("nop", clobber_abi+);  // { dg-error "expected '\\(', found '\\+'" }
+        asm!("nop", clobber_abi);  // { dg-error "expected '\\(', found end of macro arguments" }
     }
 }
