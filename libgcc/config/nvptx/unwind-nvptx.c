a29c5852a606588175d11844db84da0881227100
Thomas Schwinge <tschwinge@baylibre.com>
2024-06-05 13:11:04 +0200

    nvptx, libgcc: Stub unwinding implementation
    Adding stub '_Unwind_Backtrace', '_Unwind_GetIPInfo' functions is necessary
    for linking libbacktrace, as a normal (non-'LIBGFOR_MINIMAL') configuration
    of libgfortran wants to do, for example.
    
    The file 'libgcc/config/nvptx/unwind-nvptx.c' is copied from
    'libgcc/config/gcn/unwind-gcn.c'.
    
    libgcc/ChangeLog:
    
    	* config/nvptx/t-nvptx: Add unwind-nvptx.c.
    	* config/nvptx/unwind-nvptx.c: New file.
    
    Co-authored-by: Andrew Stubbs <ams@gcc.gnu.org>

---
+++ libgcc/config/nvptx/unwind-nvptx.c
@@ -0,0 +1,37 @@
+/* Stub unwinding implementation.
+
+   Copyright (C) 2019-2024 Free Software Foundation, Inc.
+   Contributed by Mentor Graphics
+
+   This file is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 3, or (at your option) any
+   later version.
+
+   This file is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   Under Section 7 of GPL version 3, you are granted additional
+   permissions described in the GCC Runtime Library Exception, version
+   3.1, as published by the Free Software Foundation.
+
+   You should have received a copy of the GNU General Public License and
+   a copy of the GCC Runtime Library Exception along with this program;
+   see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include "unwind.h"
+
+_Unwind_Reason_Code
+_Unwind_Backtrace(_Unwind_Trace_Fn trace, void * trace_argument)
+{
+  return 0;
+}
+
+_Unwind_Ptr
+_Unwind_GetIPInfo (struct _Unwind_Context *c, int *ip_before_insn)
+{
+  return 0;
+}
