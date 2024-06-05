3a4775d4403f2e88b589e88a9937cc1fd45a0e87
Thomas Schwinge <tschwinge@baylibre.com>
2024-06-05 13:13:24 +0200

    nvptx, libgfortran: Switch out of "minimal" mode
    ..., in order to enable (portions of) Fortran I/O, for example.
    
    	libgfortran/
    	* configure.ac: No longer set 'LIBGFOR_MINIMAL' for nvptx.
    	* configure: Regenerate.
    	libgomp/
    	* libgomp.texi (nvptx): Update.
    	* testsuite/libgomp.fortran/target-print-1-nvptx.f90: Remove.
    	* testsuite/libgomp.fortran/target-print-1.f90: Adjust.
    	* testsuite/libgomp.oacc-fortran/error_stop-2-nvptx.f: New.
    	* testsuite/libgomp.oacc-fortran/error_stop-2.f: Adjust.
    	* testsuite/libgomp.oacc-fortran/print-1-nvptx.f90: Adjust.
    	* testsuite/libgomp.oacc-fortran/print-1.f90: Adjust.
    	* testsuite/libgomp.oacc-fortran/stop-2-nvptx.f: New.
    	* testsuite/libgomp.oacc-fortran/stop-2.f: Adjust.
    
    Co-authored-by: Andrew Stubbs <ams@gcc.gnu.org>

--- libgomp/testsuite/libgomp.oacc-fortran/error_stop-2-nvptx.f
+++ libgomp/testsuite/libgomp.oacc-fortran/error_stop-2-nvptx.f
@@ -1,4 +1,12 @@
-! { dg-do run }
+! 'error_stop-2.f' nvptx offloading variant
+
+! { dg-do run { target openacc_nvidia_accel_selected } }
+!
+! The PTX JIT doesn't understand the 'ERROR STOP' call graph, and therefore:
+!     warning : Stack size for entry function 'main$_omp_fn$0' cannot be statically determined
+! Running with default 1024-bytes GPU thread stack size overflows the stack,
+! so raise it to an arbitrarily higher value:
+! { dg-set-target-env-var GOMP_NVPTX_NATIVE_GPU_THREAD_STACK_SIZE 3333 }
 
       PROGRAM MAIN
       IMPLICIT NONE
@@ -17,8 +25,7 @@
 
 ! { dg-output "CheCKpOInT(\n|\r\n|\r)+" }
 
-! See 'error_stop-2-nvptx.f' regarding the nvptx offloading XFAIL.
-! { dg-output "ERROR STOP 35(\n|\r\n|\r)+" { xfail openacc_nvidia_accel_selected } }
+! { dg-output "ERROR STOP 35(\n|\r\n|\r)+" }
 !
 ! In gfortran's main program, libfortran's set_options is called - which sets
 ! compiler_options.backtrace = 1 by default.  For an offload libgfortran, this
