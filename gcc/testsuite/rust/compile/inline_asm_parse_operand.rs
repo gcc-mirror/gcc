afc905c2d7bbb771f5663e043f93f82cbf9f005b
jjasmine <tanghocle456@gmail.com>
2024-06-05 08:24:50 -0700

    gccrs: Add potentially_nonpromoted_keywords set str
    gcc/rust/ChangeLog:
    
    	* expand/rust-macro-builtins-asm.cc (parse_reg_operand):
    	Add potentially_nonpromoted_keywords set str
    	(check_identifier): likewise
    	* expand/rust-macro-builtins-asm.h (parse_format_string):
    	likewise
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/inline_asm_parse_operand.rs: fix warnings

--- gcc/testsuite/rust/compile/inline_asm_parse_operand.rs
+++ gcc/testsuite/rust/compile/inline_asm_parse_operand.rs
@@ -16,13 +16,22 @@ fn main() {
     // This adds two numbers num1 and num2 into num1, giving us 30
     // {0} stands for num1
     // {1} stands for num2
-    let mut num1: i32 = 10;
-    let num2: i32 = 20;
+    let mut _num1: i32 = 10;
+    let _num2: i32 = 20;
     unsafe {
         asm!(
             "add {0}, {0}",
-            inout(reg) num1 => num1,
-            in(reg) num2,
+            inout(reg) num1 =>_num1,
+            in(reg) _num2,
+        );
+    }
+
+    let mut _output_testing : u32 = 0;
+    unsafe {
+        asm!(
+            "add {0}, {0}",
+            in(reg) _num1,
+            out(reg) _,
         );
     }
 }
