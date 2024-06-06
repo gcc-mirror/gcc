43530bc40b1d0465911e493e56a6631202ce85b1
Richard Ball <richard.ball@arm.com>
2024-06-06 16:28:00 +0100

    aarch64: Add missing ACLE macro for NEON-SVE Bridge
    __ARM_NEON_SVE_BRIDGE was missed in the original patch and is
    added by this patch.
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64-c.cc (aarch64_define_unconditional_macros):
    	Add missing __ARM_NEON_SVE_BRIDGE.

--- gcc/config/aarch64/aarch64-c.cc
+++ gcc/config/aarch64/aarch64-c.cc
@@ -75,6 +75,7 @@ aarch64_define_unconditional_macros (cpp_reader *pfile)
 
   builtin_define ("__ARM_STATE_ZA");
   builtin_define ("__ARM_STATE_ZT0");
+  builtin_define ("__ARM_NEON_SVE_BRIDGE");
 
   /* Define keyword attributes like __arm_streaming as macros that expand
      to the associated [[...]] attribute.  Use __extension__ in the attribute
