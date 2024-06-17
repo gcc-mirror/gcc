a84945e521e5687cdc46fc1f963d64d0b7f26cdd
Pan Li <pan2.li@intel.com>
2024-06-17 14:39:10 +0800

    RISC-V: Add testcases for unsigned .SAT_ADD vector form 2
    After the middle-end support the form 2 of unsigned SAT_ADD and
    the RISC-V backend implement the .SAT_ADD for vector mode, add
    more test case to cover the form 2.
    
    Form 2:
      #define DEF_VEC_SAT_U_ADD_FMT_2(T)                                   \
      void __attribute__((noinline))                                       \
      vec_sat_u_add_##T##_fmt_2 (T *out, T *op_1, T *op_2, unsigned limit) \
      {                                                                    \
        unsigned i;                                                        \
        for (i = 0; i < limit; i++)                                        \
          {                                                                \
            T x = op_1[i];                                                 \
            T y = op_2[i];                                                 \
            out[i] = (T)(x + y) >= x ? (x + y) : -1;                       \
          }                                                                \
      }
    
    Passed the rv64gcv regression tests.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_arith.h: Add helper
    	macro for testing.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-5.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-6.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-7.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-8.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-run-5.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-run-6.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-run-7.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-run-8.c: New test.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

--- gcc/testsuite/gcc.target/riscv/rvv/autovec/binop/vec_sat_arith.h
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/binop/vec_sat_arith.h
@@ -19,8 +19,24 @@ vec_sat_u_add_##T##_fmt_1 (T *out, T *op_1, T *op_2, unsigned limit) \
     }                                                                \
 }
 
+#define DEF_VEC_SAT_U_ADD_FMT_2(T)                                   \
+void __attribute__((noinline))                                       \
+vec_sat_u_add_##T##_fmt_2 (T *out, T *op_1, T *op_2, unsigned limit) \
+{                                                                    \
+  unsigned i;                                                        \
+  for (i = 0; i < limit; i++)                                        \
+    {                                                                \
+      T x = op_1[i];                                                 \
+      T y = op_2[i];                                                 \
+      out[i] = (T)(x + y) >= x ? (x + y) : -1;                       \
+    }                                                                \
+}
+
 #define RUN_VEC_SAT_U_ADD_FMT_1(T, out, op_1, op_2, N) \
   vec_sat_u_add_##T##_fmt_1(out, op_1, op_2, N)
+
+#define RUN_VEC_SAT_U_ADD_FMT_2(T, out, op_1, op_2, N) \
+  vec_sat_u_add_##T##_fmt_2(out, op_1, op_2, N)
 
 /******************************************************************************/
 /* Saturation Sub (Unsigned and Signed)                                       */
---
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-5.c
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gcv -mabi=lp64d -O3 -ftree-vectorize -fdump-rtl-expand-details -fno-schedule-insns -fno-schedule-insns2" } */
+/* { dg-skip-if "" { *-*-* } { "-flto" } } */
+/* { dg-final { check-function-bodies "**" "" } } */
+
+#include "vec_sat_arith.h"
+
+/*
+** vec_sat_u_add_uint8_t_fmt_2:
+** ...
+** vsetvli\s+[atx][0-9]+,\s*[atx][0-9]+,\s*e8,\s*m1,\s*ta,\s*ma
+** vle8\.v\s+v[0-9]+,\s*0\([atx][0-9]+\)
+** vle8\.v\s+v[0-9]+,\s*0\([atx][0-9]+\)
+** vsaddu\.vv\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+
+** ...
+*/
+DEF_VEC_SAT_U_ADD_FMT_2(uint8_t)
+
+/* { dg-final { scan-rtl-dump-times ".SAT_ADD " 2 "expand" } } */
