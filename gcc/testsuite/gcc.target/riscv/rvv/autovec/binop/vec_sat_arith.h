24ae0a0a3dea27d8c81f2f102d637cf09424b4b9
Pan Li <pan2.li@intel.com>
2024-06-17 16:09:13 +0800

    RISC-V: Add testcases for unsigned .SAT_ADD vector form 4
    After the middle-end support the form 4 of unsigned SAT_ADD and
    the RISC-V backend implement the .SAT_ADD for vector mode, add
    more test case to cover the form 4.
    
    Form 4:
      #define DEF_VEC_SAT_U_ADD_FMT_4(T)                                   \
      void __attribute__((noinline))                                       \
      vec_sat_u_add_##T##_fmt_4 (T *out, T *op_1, T *op_2, unsigned limit) \
      {                                                                    \
        unsigned i;                                                        \
        for (i = 0; i < limit; i++)                                        \
          {                                                                \
            T x = op_1[i];                                                 \
            T y = op_2[i];                                                 \
            T ret;                                                         \
            out[i] = __builtin_add_overflow (x, y, &ret) ? -1 : ret;       \
          }                                                                \
      }
    
    Passed the rv64gcv regression tests.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_arith.h: Add helper
    	macro for testing.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-13.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-14.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-15.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-16.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-run-13.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-run-14.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-run-15.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_u_add-run-16.c: New test.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

--- gcc/testsuite/gcc.target/riscv/rvv/autovec/binop/vec_sat_arith.h
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/binop/vec_sat_arith.h
@@ -47,6 +47,20 @@ vec_sat_u_add_##T##_fmt_3 (T *out, T *op_1, T *op_2, unsigned limit) \
     }                                                                \
 }
 
+#define DEF_VEC_SAT_U_ADD_FMT_4(T)                                   \
+void __attribute__((noinline))                                       \
+vec_sat_u_add_##T##_fmt_4 (T *out, T *op_1, T *op_2, unsigned limit) \
+{                                                                    \
+  unsigned i;                                                        \
+  for (i = 0; i < limit; i++)                                        \
+    {                                                                \
+      T x = op_1[i];                                                 \
+      T y = op_2[i];                                                 \
+      T ret;                                                         \
+      out[i] = __builtin_add_overflow (x, y, &ret) ? -1 : ret;       \
+    }                                                                \
+}
+
 #define RUN_VEC_SAT_U_ADD_FMT_1(T, out, op_1, op_2, N) \
   vec_sat_u_add_##T##_fmt_1(out, op_1, op_2, N)
 
@@ -55,6 +69,9 @@ vec_sat_u_add_##T##_fmt_3 (T *out, T *op_1, T *op_2, unsigned limit) \
 
 #define RUN_VEC_SAT_U_ADD_FMT_3(T, out, op_1, op_2, N) \
   vec_sat_u_add_##T##_fmt_3(out, op_1, op_2, N)
+
+#define RUN_VEC_SAT_U_ADD_FMT_4(T, out, op_1, op_2, N) \
+  vec_sat_u_add_##T##_fmt_4(out, op_1, op_2, N)
 
 /******************************************************************************/
 /* Saturation Sub (Unsigned and Signed)                                       */
