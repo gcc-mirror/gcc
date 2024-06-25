d4db77ce37a65207baea88859fd9c191469187f8
Eric Botcazou <ebotcazou@adacore.com>
2024-06-25 11:47:48 +0200

    SPARC: fix internal error with -mv8plus on 64-bit Linux
    This passes -m32 when -mv8plus is specified on Linux (like on Solaris).
    
    gcc/
    	PR target/115608
    	* config/sparc/linux64.h (CC1_SPEC): Pass -m32 for -mv8plus.

--- gcc/config/sparc/linux64.h
+++ gcc/config/sparc/linux64.h
@@ -162,7 +162,7 @@ extern const char *host_detect_local_cpu (int argc, const char **argv);
 "%{m32:%{m64:%emay not use both -m32 and -m64}} \
 %{m32:-mptr32 -mno-stack-bias %{!mlong-double-128:-mlong-double-64} \
   %{!mcpu*:-mcpu=cypress}} \
-%{mv8plus:-mptr32 -mno-stack-bias %{!mlong-double-128:-mlong-double-64} \
+%{mv8plus:-m32 -mptr32 -mno-stack-bias %{!mlong-double-128:-mlong-double-64} \
   %{!mcpu*:-mcpu=v9}} \
 %{!m32:%{!mcpu*:-mcpu=ultrasparc}} \
 %{!mno-vis:%{!m32:%{!mcpu=v9:-mvis}}}"
