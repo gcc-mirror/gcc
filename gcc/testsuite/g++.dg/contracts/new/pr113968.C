032d1d1ef81454072e914d76dd8ca463fca7ca12
Nina Ranns <dinka.ranns@gmail.com>
2024-07-03 15:55:52 +0100

    wron test description removed

--- gcc/testsuite/g++.dg/contracts/new/pr113968.C
+++ gcc/testsuite/g++.dg/contracts/new/pr113968.C
@@ -1,12 +1,3 @@
-// generic assert contract parsing checks
-//   check omitted, 'default', 'audit', and 'axiom' contract levels parse
-//   check that all concrete semantics parse
-//   check omitted, '%default' contract roles parse
-//   ensure that an invalid contract level 'invalid' errors
-//   ensure that a predicate referencing an undefined variable errors
-//   ensure that a missing colon after contract level errors
-//   ensure that an invalid contract role 'invalid' errors
-//   ensure that a missing colon after contract role errors
 // { dg-do compile }
 // { dg-options "-std=c++23 -fcontracts -fcontracts-nonattr" }
 struct A {
