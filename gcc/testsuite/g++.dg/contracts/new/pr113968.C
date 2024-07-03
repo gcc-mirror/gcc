05eb24d327fc20b954491c5d5bbf110cbecaca2b
Nina Ranns <dinka.ranns@gmail.com>
2024-07-03 15:47:09 +0100

    pr113968 and minor natural syntax parsing fix

---
+++ gcc/testsuite/g++.dg/contracts/new/pr113968.C
@@ -0,0 +1,27 @@
+// check that an invalid contract condition doesn't cause an ICE
+// { dg-do compile }
+// { dg-options "-std=c++23 -fcontracts -fcontracts-nonattr" }
+struct A {
+  A(A &);
+};
+struct S{
+  void f(A a) pre(a) // { dg-error "could not convert" }
+              pre(a.b) // { dg-error "has no member" }
+  {
+
+  }
+};
+void f(A a) pre(a) // { dg-error "could not convert" }
+	    pre(a.b) // { dg-error "has no member" }
+	    {
+  contract_assert(a); // { dg-error "could not convert" }
+  contract_assert(a.b); // { dg-error "has no member" }
+}
+
+int main()
+{
+}
+
+
+
+
