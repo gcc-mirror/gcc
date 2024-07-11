8dbc02ba43d9e89056f4ba21d664118377f7da40
Jonathan Wakely <jwakely@redhat.com>
2024-07-11 09:40:12 +0100

    libstdc++: Disable expensive test for debug mode [PR108636]
    This test uses -fkeep-inline-functions and with debug mode enabled that
    compiles much slower and times out if the system is under heavy load.
    
    The original problem being tested is independent of debug mode, so just
    require normal mode for the test.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/108636
    	* testsuite/27_io/filesystem/path/108636.cc: Require normal
    	mode.

--- libstdc++-v3/testsuite/27_io/filesystem/path/108636.cc
+++ libstdc++-v3/testsuite/27_io/filesystem/path/108636.cc
@@ -1,6 +1,7 @@
 // { dg-do link { target c++17 } }
 // { dg-options "-fkeep-inline-functions" }
 // { dg-require-filesystem-ts "" }
+// { dg-require-normal-mode "too slow with debug mode" }
 
 #include <filesystem>
 int main()
