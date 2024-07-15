25cf4d2a2200903fe868f8cbd9d24f35768041c1
Peter Bergner <bergner@linux.ibm.com>
2024-07-15 16:57:32 -0500

    rs6000: Error on CPUs and ABIs that don't support the ROP protection insns [PR114759]
    We currently silently ignore the -mrop-protect option for old CPUs we don't
    support with the ROP hash insns, but we throw an error for unsupported ABIs.
    This patch treats unsupported CPUs and ABIs similarly by throwing an error
    both both.  This matches clang behavior and allows us to simplify our tests
    in the code that generates our prologue and epilogue code.
    
    2024-06-26  Peter Bergner  <bergner@linux.ibm.com>
    
    gcc/
    	PR target/114759
    	* config/rs6000/rs6000.cc (rs6000_option_override_internal): Disallow
    	CPUs and ABIs that do no support the ROP protection insns.
    	* config/rs6000/rs6000-logue.cc (rs6000_stack_info): Remove now
    	unneeded tests.
    	(rs6000_emit_prologue): Likewise.
    	Remove unneeded gcc_assert.
    	(rs6000_emit_epilogue): Likewise.
    	* config/rs6000/rs6000.md: Likewise.
    
    gcc/testsuite/
    	PR target/114759
    	* gcc.target/powerpc/pr114759-3.c: New test.
    
    (cherry picked from commit 6f2bab9b5d1ce1914c748b7dcd8638dafaa98df7)

---
+++ gcc/testsuite/gcc.target/powerpc/pr114759-3.c
@@ -0,0 +1,19 @@
+/* PR target/114759 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -mdejagnu-cpu=power7 -mrop-protect" } */
+
+/* Verify we emit an error if we use -mrop-protect with an unsupported cpu.  */
+
+extern void foo (void);
+
+int
+bar (void)
+{
+  foo ();
+  return 5;
+}
+
+/* The correct line number is in the preamble to the error message, not
+   in the final line (which is all that dg-error inspects). Hence, we have
+   to tell dg-error to ignore the line number.  */
+/* { dg-error "'-mrop-protect' requires '-mcpu=power8'" "PR114759" { target *-*-* } 0 } */
