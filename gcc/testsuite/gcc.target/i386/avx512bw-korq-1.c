4b58697cecbd72fd7db5a0fcdf7af8deb3be2b14
Haochen Jiang <haochen.jiang@intel.com>
2024-07-17 16:26:35 +0800

    i386: Fix testcases generating invalid asm
    For compile test, we should generate valid asm except for special purposes.
    Fix the compile test that generates invalid asm.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/i386/apx-egprs-names.c: Use ax for short and
    	al for char instead of eax.
    	* gcc.target/i386/avx512bw-kandnq-1.c: Do not run the test
    	under -m32 since kmovq with register is invalid. Use long
    	long to use 64 bit register instead of 32 bit register for
    	kmovq.
    	* gcc.target/i386/avx512bw-kandq-1.c: Ditto.
    	* gcc.target/i386/avx512bw-knotq-1.c: Ditto.
    	* gcc.target/i386/avx512bw-korq-1.c: Ditto.
    	* gcc.target/i386/avx512bw-kshiftlq-1.c: Ditto.
    	* gcc.target/i386/avx512bw-kshiftrq-1.c: Ditto.
    	* gcc.target/i386/avx512bw-kxnorq-1.c: Ditto.
    	* gcc.target/i386/avx512bw-kxorq-1.c: Ditto.

--- gcc/testsuite/gcc.target/i386/avx512bw-korq-1.c
+++ gcc/testsuite/gcc.target/i386/avx512bw-korq-1.c
@@ -1,4 +1,4 @@
-/* { dg-do compile } */
+/* { dg-do compile { target { ! ia32 } } } */
 /* { dg-options "-mavx512bw -O2" } */
 /* { dg-final { scan-assembler-times "korq\[ \\t\]+\[^\{\n\]*%k\[0-7\](?:\n|\[ \\t\]+#)" 1 } } */
 
@@ -10,8 +10,8 @@ avx512bw_test ()
   __mmask64 k1, k2, k3;
   volatile __m512i x = _mm512_setzero_si512 ();
 
-  __asm__( "kmovq %1, %0" : "=k" (k1) : "r" (1) );
-  __asm__( "kmovq %1, %0" : "=k" (k2) : "r" (2) );
+  __asm__( "kmovq %1, %0" : "=k" (k1) : "r" (1ULL) );
+  __asm__( "kmovq %1, %0" : "=k" (k2) : "r" (2ULL) );
 
   k3 = _kor_mask64 (k1, k2);
   x = _mm512_mask_add_epi8 (x, k3, x, x);
