43a7ece873eba47a11c0b21b0068eee53740551a
Richard Sandiford <richard.sandiford@arm.com>
2024-07-17 19:38:12 +0100

    rtl-ssa: Fix move range canonicalisation [PR115929]
    In this PR, canonicalize_move_range walked off the end of a list
    and triggered a null dereference.  There are multiple ways of fixing
    that, but I think the approach taken in the patch should be
    relatively efficient.
    
    gcc/
    	PR rtl-optimization/115929
    	* rtl-ssa/movement.h (canonicalize_move_range): Check for null prev
    	and next insns and create an invalid move range for them.
    
    gcc/testsuite/
    	PR rtl-optimization/115929
    	* gcc.dg/torture/pr115929-2.c: New test.

---
+++ gcc/testsuite/gcc.dg/torture/pr115929-2.c
@@ -0,0 +1,22 @@
+/* { dg-additional-options "-fschedule-insns" } */
+
+int a, b, c, d, e, f;
+int main() {
+  if (e && f)
+    while (1)
+      while (a)
+        a = 0;
+  if (c) {
+    if (b)
+      goto g;
+    int h = a;
+  i:
+    b = ~((b ^ h) | 1 % b);
+    if (a)
+    g:
+      b = 0;
+  }
+  if (d)
+    goto i;
+  return 0;
+}
