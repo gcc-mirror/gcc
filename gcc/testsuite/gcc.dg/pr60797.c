9d0651b9a15d6ed27acb589ba6d082bea54c7172
Thomas Schwinge <tschwinge@baylibre.com>
2024-07-21 22:23:40 +0200

    nvptx: Specify '-mno-alias' for 'gcc.dg/pr60797.c' [PR60797, PR104957]
    2014 Subversion r209299 (Git commit 8330537b5b58bd0532a0a49f9cbd59bf526a7847)
    "Fix PR60797" added this test case, which we now amend so that it's able to
    test its thing also in '--target=nvptx-none' configurations with symbol alias
    support enabled (..., and test nvptx '-mno-alias').
    
    	PR middle-end/60797
    	PR target/104957
    	gcc/testsuite/
    	* gcc.dg/pr60797.c: For nvptx, specify '-mno-alias'.
    
    (cherry picked from commit b9be3113a9365e8380397abb23abc71f049cf4f1)

--- gcc/testsuite/gcc.dg/pr60797.c
+++ gcc/testsuite/gcc.dg/pr60797.c
@@ -1,5 +1,7 @@
-/* { dg-do compile } */
-/* { dg-skip-if "" { alias } } */
+/* If there's support for symbol aliases, have to 'dg-skip-if' -- unless
+   there's a way to disable this support.
+   { dg-additional-options -mno-alias { target nvptx-*-* } }
+   { dg-skip-if "" { { ! nvptx-*-* } && alias } } */
 
 extern int foo __attribute__((alias("bar"))); /* { dg-error "supported" } */
 int main()
