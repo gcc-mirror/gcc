05f0e9eec9f9beeffe024ecd6566b41200d79cff
Michael Meissner <meissner@linux.ibm.com>
2024-07-22 12:20:43 -0400

    Add -mcpu=power11 support.
    This patch adds the power11 option to the -mcpu= and -mtune= switches.
    
    This patch treats the power11 like a power10 in terms of costs and reassociation
    width.
    
    This patch issues a ".machine power11" to the assembly file if you use
    -mcpu=power11.
    
    This patch defines _ARCH_PWR11 if the user uses -mcpu=power11.
    
    This patch allows GCC to be configured with the --with-cpu=power11 and
    --with-tune=power11 options.
    
    This patch passes -mpwr11 to the assembler if the user uses -mcpu=power11.
    
    This patch adds support for using "power11" in the __builtin_cpu_is built-in
    function.
    
    2024-07-22  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config.gcc (powerpc*-*-*): Add support for power11.
    	* config/rs6000/aix71.h (ASM_CPU_SPEC): Add support for -mcpu=power11.
    	* config/rs6000/aix72.h (ASM_CPU_SPEC): Likewise.
    	* config/rs6000/aix73.h (ASM_CPU_SPEC): Likewise.
    	* config/rs6000/driver-rs6000.cc (asm_names): Likewise.
    	* config/rs6000/ppc-auxv.h (PPC_PLATFORM_POWER11): New define.
    	* config/rs6000/rs6000-builtin.cc (cpu_is_info): Add power11.
    	* config/rs6000/rs6000-c.cc (rs6000_target_modify_macros): Define
    	_ARCH_PWR11 if -mcpu=power11.
    	* config/rs6000/rs6000-cpus.def (POWER11_MASKS_SERVER): New define.
    	(POWERPC_MASKS): Add power11.
    	(power11 cpu): Add power11 definition.
    	* config/rs6000/rs6000-opts.h (PROCESSOR_POWER11): Add power11 processor.
    	* config/rs6000/rs6000-string.cc (expand_compare_loop): Likewise.
    	* config/rs6000/rs6000-tables.opt: Regenerate.
    	* config/rs6000/rs6000.cc (rs6000_option_override_internal): Add power11
    	support.
    	(rs6000_machine_from_flags): Likewise.
    	(rs6000_reassociation_width): Likewise.
    	(rs6000_adjust_cost): Likewise.
    	(rs6000_issue_rate): Likewise.
    	(rs6000_sched_reorder): Likewise.
    	(rs6000_sched_reorder2): Likewise.
    	(rs6000_register_move_cost): Likewise.
    	(rs6000_opt_masks): Likewise.
    	* config/rs6000/rs6000.h (ASM_CPU_SPEC): Likewise.
    	* config/rs6000/rs6000.md (cpu attribute): Add power11.
    	* config/rs6000/rs6000.opt (-mpower11): Add internal power11 flag.
    	* doc/invoke.texi (RS/6000 and PowerPC Options): Document -mcpu=power11.
    	* config/rs6000/power10.md (all reservations): Add power11 support.
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/power11-1.c: New test.
    	* gcc.target/powerpc/power11-2.c: Likewise.
    	* gcc.target/powerpc/power11-3.c: Likewise.

--- gcc/config/rs6000/rs6000.cc
+++ gcc/config/rs6000/rs6000.cc
@@ -1067,7 +1067,7 @@ struct processor_costs power9_cost = {
   COSTS_N_INSNS (3),	/* SF->DF convert */
 };
 
-/* Instruction costs on POWER10 processors.  */
+/* Instruction costs on Power10/Power11 processors.  */
 static const
 struct processor_costs power10_cost = {
   COSTS_N_INSNS (2),	/* mulsi */
@@ -4385,7 +4385,8 @@ rs6000_option_override_internal (bool global_init_p)
      generating power10 instructions.  */
   if (!(rs6000_isa_flags_explicit & OPTION_MASK_P10_FUSION))
     {
-      if (rs6000_tune == PROCESSOR_POWER10)
+      if (rs6000_tune == PROCESSOR_POWER10
+	  || rs6000_tune == PROCESSOR_POWER11)
 	rs6000_isa_flags |= OPTION_MASK_P10_FUSION;
       else
 	rs6000_isa_flags &= ~OPTION_MASK_P10_FUSION;
@@ -4414,6 +4415,7 @@ rs6000_option_override_internal (bool global_init_p)
 			&& rs6000_tune != PROCESSOR_POWER8
 			&& rs6000_tune != PROCESSOR_POWER9
 			&& rs6000_tune != PROCESSOR_POWER10
+			&& rs6000_tune != PROCESSOR_POWER11
 			&& rs6000_tune != PROCESSOR_PPCA2
 			&& rs6000_tune != PROCESSOR_CELL
 			&& rs6000_tune != PROCESSOR_PPC476);
@@ -4428,6 +4430,7 @@ rs6000_option_override_internal (bool global_init_p)
 				 || rs6000_tune == PROCESSOR_POWER8
 				 || rs6000_tune == PROCESSOR_POWER9
 				 || rs6000_tune == PROCESSOR_POWER10
+				 || rs6000_tune == PROCESSOR_POWER11
 				 || rs6000_tune == PROCESSOR_PPCE500MC
 				 || rs6000_tune == PROCESSOR_PPCE500MC64
 				 || rs6000_tune == PROCESSOR_PPCE5500
@@ -4727,6 +4730,7 @@ rs6000_option_override_internal (bool global_init_p)
 	break;
 
       case PROCESSOR_POWER10:
+      case PROCESSOR_POWER11:
 	rs6000_cost = &power10_cost;
 	break;
 
@@ -5899,6 +5903,8 @@ rs6000_machine_from_flags (void)
   flags &= ~(OPTION_MASK_PPC_GFXOPT | OPTION_MASK_PPC_GPOPT | OPTION_MASK_ISEL
 	     | OPTION_MASK_ALTIVEC);
 
+  if ((flags & (POWER11_MASKS_SERVER & ~ISA_3_1_MASKS_SERVER)) != 0)
+    return "power11";
   if ((flags & (ISA_3_1_MASKS_SERVER & ~ISA_3_0_MASKS_SERVER)) != 0)
     return "power10";
   if ((flags & (ISA_3_0_MASKS_SERVER & ~ISA_2_7_MASKS_SERVER)) != 0)
@@ -10147,6 +10153,7 @@ rs6000_reassociation_width (unsigned int opc ATTRIBUTE_UNUSED,
     case PROCESSOR_POWER8:
     case PROCESSOR_POWER9:
     case PROCESSOR_POWER10:
+    case PROCESSOR_POWER11:
       if (DECIMAL_FLOAT_MODE_P (mode))
 	return 1;
       if (VECTOR_MODE_P (mode))
@@ -18228,7 +18235,8 @@ rs6000_adjust_cost (rtx_insn *insn, int dep_type, rtx_insn *dep_insn, int cost,
 
 	/* Separate a load from a narrower, dependent store.  */
 	if ((rs6000_sched_groups || rs6000_tune == PROCESSOR_POWER9
-	     || rs6000_tune == PROCESSOR_POWER10)
+	     || rs6000_tune == PROCESSOR_POWER10
+	     || rs6000_tune == PROCESSOR_POWER11)
 	    && GET_CODE (PATTERN (insn)) == SET
 	    && GET_CODE (PATTERN (dep_insn)) == SET
 	    && MEM_P (XEXP (PATTERN (insn), 1))
@@ -18267,6 +18275,7 @@ rs6000_adjust_cost (rtx_insn *insn, int dep_type, rtx_insn *dep_insn, int cost,
 		 || rs6000_tune == PROCESSOR_POWER8
 		 || rs6000_tune == PROCESSOR_POWER9
 		 || rs6000_tune == PROCESSOR_POWER10
+		 || rs6000_tune == PROCESSOR_POWER11
                  || rs6000_tune == PROCESSOR_CELL)
                 && recog_memoized (dep_insn)
                 && (INSN_CODE (dep_insn) >= 0))
@@ -18841,6 +18850,7 @@ rs6000_issue_rate (void)
   case PROCESSOR_POWER9:
     return 6;
   case PROCESSOR_POWER10:
+  case PROCESSOR_POWER11:
     return 8;
   default:
     return 1;
@@ -19556,8 +19566,10 @@ rs6000_sched_reorder (FILE *dump ATTRIBUTE_UNUSED, int sched_verbose,
   if (rs6000_tune == PROCESSOR_POWER6)
     load_store_pendulum = 0;
 
-  /* Do Power10 dependent reordering.  */
-  if (rs6000_tune == PROCESSOR_POWER10 && last_scheduled_insn)
+  /* Do Power10/Power11 dependent reordering.  */
+  if (last_scheduled_insn
+      && (rs6000_tune == PROCESSOR_POWER10
+	  || rs6000_tune == PROCESSOR_POWER11))
     power10_sched_reorder (ready, n_ready - 1);
 
   return rs6000_issue_rate ();
@@ -19581,8 +19593,10 @@ rs6000_sched_reorder2 (FILE *dump, int sched_verbose, rtx_insn **ready,
       && recog_memoized (last_scheduled_insn) >= 0)
     return power9_sched_reorder2 (ready, *pn_ready - 1);
 
-  /* Do Power10 dependent reordering.  */
-  if (rs6000_tune == PROCESSOR_POWER10 && last_scheduled_insn)
+  /* Do Power10/Power11 dependent reordering.  */
+  if (last_scheduled_insn
+      && (rs6000_tune == PROCESSOR_POWER10
+	  || rs6000_tune == PROCESSOR_POWER11))
     return power10_sched_reorder (ready, *pn_ready - 1);
 
   return cached_can_issue_more;
@@ -22799,7 +22813,8 @@ rs6000_register_move_cost (machine_mode mode,
 		 allocation a move within the same class might turn
 		 out to be a nop.  */
 	      if (rs6000_tune == PROCESSOR_POWER9
-		  || rs6000_tune == PROCESSOR_POWER10)
+		  || rs6000_tune == PROCESSOR_POWER10
+		  || rs6000_tune == PROCESSOR_POWER11)
 		ret = 3 * hard_regno_nregs (FIRST_GPR_REGNO, mode);
 	      else
 		ret = 4 * hard_regno_nregs (FIRST_GPR_REGNO, mode);
@@ -24470,6 +24485,7 @@ static struct rs6000_opt_mask const rs6000_opt_masks[] =
   { "float128-hardware",	OPTION_MASK_FLOAT128_HW,	false, true  },
   { "fprnd",			OPTION_MASK_FPRND,		false, true  },
   { "power10",			OPTION_MASK_POWER10,		false, true  },
+  { "power11",			OPTION_MASK_POWER11,		false, false },
   { "hard-dfp",			OPTION_MASK_DFP,		false, true  },
   { "htm",			OPTION_MASK_HTM,		false, true  },
   { "isel",			OPTION_MASK_ISEL,		false, true  },
