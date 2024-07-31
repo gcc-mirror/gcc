34d947134403e7482ba4f153d8faabee0bc4933e
Marek Polacek <polacek@redhat.com>
2024-07-31 17:33:55 -0400

    c++: fix -Wdangling-reference false positive [PR115987]
    This fixes another false positive.  When a function is taking a
    temporary of scalar type that couldn't be bound to the return type
    of the function, don't warn, such a program would be ill-formed.
    
    Thanks to Jonathan for reporting the problem.
    
    	PR c++/115987
    
    gcc/cp/ChangeLog:
    
    	* call.cc (do_warn_dangling_reference): Don't consider a
    	temporary with a scalar type that cannot bind to the return type.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/ext/attr-no-dangling6.C: Adjust.
    	* g++.dg/ext/attr-no-dangling7.C: Likewise.
    	* g++.dg/warn/Wdangling-reference22.C: New test.

--- gcc/testsuite/g++.dg/ext/attr-no-dangling6.C
+++ gcc/testsuite/g++.dg/ext/attr-no-dangling6.C
@@ -12,26 +12,26 @@ struct SF { static constexpr bool value = false; };
 
 template<typename T>
 [[gnu::no_dangling(T::value)]]
-const X& get (const int& i)
+const X& get (const int& i, const X&)
 {
    return i == 0 ? x1 : x2;
 }
 
 template<bool B = true>
 [[gnu::no_dangling(B)]]
-const X& foo (const int& i)
+const X& foo (const int& i, const X&)
 {
    return i == 0 ? x1 : x2;
 }
 
 [[gnu::no_dangling(val ())]]
-const X& bar (const int& i)
+const X& bar (const int& i, const X&)
 {
    return i == 0 ? x1 : x2;
 }
 
 [[gnu::no_dangling(!val ())]]
-const X& baz (const int& i)
+const X& baz (const int& i, const X&)
 {
    return i == 0 ? x1 : x2;
 }
@@ -52,13 +52,13 @@ auto gety() -> Span<SF>;
 void
 test ()
 {
-  [[maybe_unused]] const X& x1 = get<ST> (10);	  // { dg-bogus "dangling" }
-  [[maybe_unused]] const X& x2 = get<SF> (10);	  // { dg-warning "dangling" }
-  [[maybe_unused]] const X& x3 = foo<true> (10);  // { dg-bogus "dangling" }
-  [[maybe_unused]] const X& x4 = foo<false> (10); // { dg-warning "dangling" }
-  [[maybe_unused]] const X& x7 = foo<> (10);	  // { dg-bogus "dangling" }
-  [[maybe_unused]] const X& x5 = bar (10);	  // { dg-bogus "dangling" }
-  [[maybe_unused]] const X& x6 = baz (10);	  // { dg-warning "dangling" }
+  [[maybe_unused]] const X& x1 = get<ST> (10, X{});	  // { dg-bogus "dangling" }
+  [[maybe_unused]] const X& x2 = get<SF> (10, X{});	  // { dg-warning "dangling" }
+  [[maybe_unused]] const X& x3 = foo<true> (10, X{});  // { dg-bogus "dangling" }
+  [[maybe_unused]] const X& x4 = foo<false> (10, X{}); // { dg-warning "dangling" }
+  [[maybe_unused]] const X& x7 = foo<> (10, X{});	  // { dg-bogus "dangling" }
+  [[maybe_unused]] const X& x5 = bar (10, X{});	  // { dg-bogus "dangling" }
+  [[maybe_unused]] const X& x6 = baz (10, X{});	  // { dg-warning "dangling" }
 
   [[maybe_unused]] const auto &b1 = geti()[0];	  // { dg-bogus "dangling" }
   [[maybe_unused]] const auto &b2 = gety()[0];	  // { dg-warning "dangling" }
