488395f9513233944e488fae59372da4de4324c3
Tamar Christina <tamar.christina@arm.com>
2024-08-01 16:54:15 +0100

    AArch64: Add Neoverse N3 and Cortex-A725 core definition and cost model
    This adds a cost model and core definition for Neoverse N3 and Cortex-A725.
    
    It also makes Cortex-A725 use the Neoverse N3 cost model.
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64-cores.def (neoverse-n3, cortex-a725): New.
    	* config/aarch64/aarch64-tune.md: Regenerate.
    	* config/aarch64/tuning_models/neoversen3.h: New file.
    	* config/aarch64/aarch64.cc: Use it.
    	* doc/invoke.texi: Document it.

--- gcc/config/aarch64/aarch64.cc
+++ gcc/config/aarch64/aarch64.cc
@@ -409,6 +409,7 @@ static const struct aarch64_flag_desc aarch64_tuning_flags[] =
 #include "tuning_models/neoversev1.h"
 #include "tuning_models/neoverse512tvb.h"
 #include "tuning_models/neoversen2.h"
+#include "tuning_models/neoversen3.h"
 #include "tuning_models/neoversev2.h"
 #include "tuning_models/neoversev3.h"
 #include "tuning_models/neoversev3ae.h"
