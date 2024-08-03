ba45573c8072f06f67af22a2ca60da5f5c12beae
Andrew Pinski <quic_apinski@quicinc.com>
2024-08-03 09:30:57 -0700

    sh: Don't call make_insn_raw in sh_recog_treg_set_expr [PR116189]
    This was an interesting compare debug failure to debug. The first symptom
    was in gcse which would produce different order of creating psedu-registers. This
    was caused by a different order of a hashtable walk, due to the hash table having different
    number of entries. Which in turn was due to the number of max insn being different between
    the 2 runs. The place max insn uid comes from was in sh_recog_treg_set_expr which is called
    via rtx_costs and fwprop would cause rtx_costs in some cases for debug insn related stuff.
    
    Build and tested for sh4-linux-gnu.
    
    	PR target/116189
    
    gcc/ChangeLog:
    
    	* config/sh/sh.cc (sh_recog_treg_set_expr): Don't call make_insn_raw,
    	make the insn with a fake uid.
    
    gcc/testsuite/ChangeLog:
    
    	* c-c++-common/torture/pr116189-1.c: New test.
    
    Signed-off-by: Andrew Pinski <quic_apinski@quicinc.com>
    (cherry picked from commit 0355c943b9e954e8f59068971d934f1b91ecb729)

--- gcc/config/sh/sh.cc
+++ gcc/config/sh/sh.cc
@@ -12279,7 +12279,17 @@ sh_recog_treg_set_expr (rtx op, machine_mode mode)
      have to capture its current state and restore it afterwards.  */
   recog_data_d prev_recog_data = recog_data;
 
-  rtx_insn* i = make_insn_raw (gen_rtx_SET (get_t_reg_rtx (), op));
+  /* Note we can't use insn_raw here since that increases the uid
+     and could cause debug compare differences; this insn never leaves
+     this function so create a dummy one. */
+  rtx_insn* i = as_a <rtx_insn *> (rtx_alloc (INSN));
+
+  INSN_UID (i) = 1;
+  PATTERN (i) = gen_rtx_SET (get_t_reg_rtx (), op);
+  INSN_CODE (i) = -1;
+  REG_NOTES (i) = NULL;
+  INSN_LOCATION (i) = curr_insn_location ();
+  BLOCK_FOR_INSN (i) = NULL;
   SET_PREV_INSN (i) = NULL;
   SET_NEXT_INSN (i) = NULL;
 
