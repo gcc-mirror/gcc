3a51aaf5f4ccd3d2ed871727c16f9c6f9ed54e50
Ian Lance Taylor <iant@golang.org>
2024-08-05 16:46:03 -0700

    libbacktrace: avoid -Wpointer-arith errors
    Based on patch from Kirill Müller.
    
    	* configure.ac (ACX_PROG_CC_WARNING_OPTS): Add -Wpointer-arith.
    	* pecoff.c (coff_add): Cast void pointers.
    	* xcoff.c (xcoff_add): Likewise.
    	* configure: Regenerate.

--- libbacktrace/pecoff.c
+++ libbacktrace/pecoff.c
@@ -704,7 +704,7 @@ coff_add (struct backtrace_state *state, int descriptor,
       magic_ok = memcmp (magic, "PE\0", 4) == 0;
       fhdr_off += 4;
 
-      memcpy (&fhdr, fhdr_view.data + 4, sizeof fhdr);
+      memcpy (&fhdr, (const unsigned char *) fhdr_view.data + 4, sizeof fhdr);
     }
   else
     {
@@ -738,7 +738,7 @@ coff_add (struct backtrace_state *state, int descriptor,
   sects_view_valid = 1;
   opt_hdr = (const b_coff_optional_header *) sects_view.data;
   sects = (const b_coff_section_header *)
-    (sects_view.data + fhdr.size_of_optional_header);
+    ((const unsigned char *) sects_view.data + fhdr.size_of_optional_header);
 
   is_64 = 0;
   memset (&image_base, 0, sizeof image_base);
@@ -781,7 +781,8 @@ coff_add (struct backtrace_state *state, int descriptor,
 	goto fail;
       syms_view_valid = 1;
 
-      str_size = coff_read4 (syms_view.data + syms_size);
+      str_size = coff_read4 ((const unsigned char *) syms_view.data
+			     + syms_size);
 
       str_off = syms_off + syms_size;
 
