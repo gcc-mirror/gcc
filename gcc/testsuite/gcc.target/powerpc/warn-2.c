cdca23875296edd78327d3da6890bb334c28f2fd
Kewen Lin <linkw@linux.ibm.com>
2024-08-07 02:03:55 -0500

    testsuite, rs6000: Replace powerpc_vsx_ok with powerpc_vsx
    Following up the previous r15-886, this patch to clean up
    the remaining powerpc_vsx_ok which actually should use
    powerpc_vsx instead.
    
    	PR testsuite/114842
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/powerpc/error-1.c: Replace powerpc_vsx_ok check with
    	powerpc_vsx.
    	* gcc.target/powerpc/warn-2.c: Likewise.
    	* gcc.target/powerpc/fold-vec-logical-ors-longlong.c: Likewise.
    	* gcc.target/powerpc/ppc-fortran/pr80108-1.f90: Replace powerpc_vsx_ok
    	check with powerpc_vsx and remove useless -mfloat128.
    	* gcc.target/powerpc/pragma_power8.c: Replace powerpc_vsx_ok check with
    	powerpc_vsx.

--- gcc/testsuite/gcc.target/powerpc/warn-2.c
+++ gcc/testsuite/gcc.target/powerpc/warn-2.c
@@ -1,6 +1,6 @@
 /* { dg-do compile { target { powerpc*-*-* } } } */
 /* { dg-skip-if "" { powerpc*-*-darwin* } } */
-/* { dg-require-effective-target powerpc_vsx_ok } */
+/* { dg-require-effective-target powerpc_vsx } */
 /* { dg-options "-O -mdejagnu-cpu=power7 -mno-altivec" } */
 
 /* { dg-warning "'-mno-altivec' disables vsx" "" { target *-*-* } 0 } */
