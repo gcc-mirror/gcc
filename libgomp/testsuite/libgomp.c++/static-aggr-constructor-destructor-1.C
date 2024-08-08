e55aa018ba1c5ce3f756e56489b7007e736aa5a8
Tobias Burnus <tburnus@baylibre.com>
2024-08-08 10:42:25 +0200

    libgomp.c++/static-aggr-constructor-destructor-{1,2}.C: Fix scan-tree-dump
    In principle, the optimized dump should be the same on the host, but as
    'nohost' is not handled, is is present. However when ENABLE_OFFLOADING is
    false, it is handled early enough to remove the function.
    
    libgomp/ChangeLog:
    
    	* testsuite/libgomp.c++/static-aggr-constructor-destructor-1.C: Split
    	scan-tree-dump into with and without target offload_target_any.
    	* testsuite/libgomp.c++/static-aggr-constructor-destructor-2.C:
    	Likewise.
    
    (cherry picked from commit e3a6dec326a127ad549246435b9d3835e9a32407)

--- libgomp/testsuite/libgomp.c++/static-aggr-constructor-destructor-1.C
+++ libgomp/testsuite/libgomp.c++/static-aggr-constructor-destructor-1.C
@@ -9,9 +9,18 @@
 
 // { dg-final { scan-tree-dump-not "omp_is_initial_device" "optimized" } }
 // { dg-final { scan-tree-dump-not "__omp_target_static_init_and_destruction" "optimized" } }
-// FIXME: should be '-not' not '-times' 1:
-// { dg-final { scan-tree-dump-times "void _GLOBAL__off_I_v1" 1 "optimized" } }
-// { dg-final { scan-tree-dump-times "__attribute__\\(\\(\[^\n\r]*omp declare target nohost" 1 "optimized" } }
+
+// (A) No offloading configured: The symbols aren't present
+//     Caveat: They are present with -foffload=disable - or offloading
+//     configured but none of the optional offload packages/binaries installed.
+//     But the 'offload_target_any' check cannot distinguish those
+// { dg-final { scan-tree-dump-not "void _GLOBAL__off_I_v1" "optimized" { target { ! offload_target_any } } } }
+// { dg-final { scan-tree-dump-not "__attribute__\\(\\(\[^\n\r]*omp declare target nohost" "optimized" { target { ! offload_target_any } } } }
+
+// (B) With offload configured (and compiling for an offload target)
+//     the symbols are present (missed optimization). Hence: FIXME.
+// { dg-final { scan-tree-dump-times "void _GLOBAL__off_I_v1" 1 "optimized" { target offload_target_any } } }
+// { dg-final { scan-tree-dump-times "__attribute__\\(\\(\[^\n\r]*omp declare target nohost" 1 "optimized" { target offload_target_any } } }
 
 // { dg-final { only_for_offload_target amdgcn-amdhsa scan-offload-tree-dump-not "omp_initial_device;" "optimized" { target offload_target_amdgcn } } }
 // { dg-final { only_for_offload_target amdgcn-amdhsa scan-offload-tree-dump "v1\\._x = 5;" "optimized" { target offload_target_amdgcn } } }
