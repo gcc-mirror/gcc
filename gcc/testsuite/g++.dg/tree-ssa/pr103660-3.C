b73373520f0ed5d131d2cd6ee9078939a98d7a0d
Andrew Pinski <quic_apinski@quicinc.com>
2024-08-12 16:00:45 -0700

    match: extend the `((a CMP b) ? c : 0) | ((a CMP' b) ? d : 0)` patterns to support ^ and + [PR103660]
    r13-4620-g4d9db4bdd458 Added a few patterns and some of them can be extended to support XOR and PLUS.
    This extends the patterns to support XOR and PLUS instead of just IOR.
    
    Bootstrapped and tested on x86_64-linux-gnu.
    
    	PR tree-optimization/103660
    
    gcc/ChangeLog:
    
    	* match.pd (`((a CMP b) ? c : 0) | ((a CMP' b) ? d : 0)`): Extend to support
    	XOR and PLUS.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/tree-ssa/pr103660-2.C: New test.
    	* g++.dg/tree-ssa/pr103660-3.C: New test.
    	* gcc.dg/tree-ssa/pr103660-2.c: New test.
    	* gcc.dg/tree-ssa/pr103660-3.c: New test.
    
    Signed-off-by: Andrew Pinski <quic_apinski@quicinc.com>

---
+++ gcc/testsuite/g++.dg/tree-ssa/pr103660-3.C
@@ -0,0 +1,30 @@
+/* PR tree-optimization/103660 */
+/* Vector type version. */
+/* { dg-do compile } */
+/* { dg-options "-O1 -fdump-tree-forwprop1-raw -Wno-psabi" } */
+
+typedef int v4si __attribute((__vector_size__(4 * sizeof(int))));
+#define funcs(OP,n)			\
+v4si min_##n(v4si a, v4si b) {		\
+  v4si X = -(a < b) * a;		\
+  v4si Y = -(a >= b) * b;		\
+  return (X OP Y);			\
+}					\
+v4si f_##n(v4si a, v4si b,		\
+	   v4si c, v4si d) {		\
+  v4si X = -(a < b) * c;		\
+  v4si Y = -(a >= b) * d;		\
+  return (X OP Y);			\
+}
+
+
+funcs(^, xor)
+funcs(+, plus)
+
+/* min_xor/min_plus should produce min<a,b> or `a < b ? a : b` depending on if the target
+   supports min on the vector type or not. */
+/* f_xor/f_plus should produce (a < b) ? c : d */
+/* { dg-final { scan-tree-dump-not   "bit_xor_expr, "     "forwprop1" } } */
+/* { dg-final { scan-tree-dump-not   "plus_expr, "        "forwprop1" } } */
+/* { dg-final { scan-tree-dump-times "(?:lt_expr|min_expr), "        4 "forwprop1" } } */
+/* { dg-final { scan-tree-dump-times "(?:vec_cond_expr|min_expr), "  4 "forwprop1" } } */
