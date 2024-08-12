5c2ecc630d95077e533d6cac6bf6c4e0f16ba623
Kushal Pal <kushalpal109@gmail.com>
2024-08-12 05:48:27 +0000

    gccrs: Move errors with locations
    gcc/rust/ChangeLog:
    
    	* checks/errors/borrowck/rust-borrow-checker-diagnostics.cc
    	(BorrowCheckerDiagnostics::report_move_errors): Specify
    	locations for code causing errors and related moves.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/borrowck/test_move.rs: Test rich-errors related to moves.
    	* rust/borrowck/test_move_conditional.rs: Likewise.
    
    Signed-off-by: Kushal Pal <kushalpal109@gmail.com>

--- gcc/testsuite/rust/borrowck/test_move.rs
+++ gcc/testsuite/rust/borrowck/test_move.rs
@@ -1,11 +1,27 @@
-// { dg-additional-options "-frust-compile-until=compilation -frust-borrowcheck" }
-fn test_move() { // { dg-error "Found move errors in function test_move" }
+// { dg-additional-options "-frust-compile-until=compilation -frust-borrowcheck -fdiagnostics-show-caret -fdiagnostics-show-line-numbers" }
+// { dg-enable-nn-line-numbers "" }
+
+fn test_move() {
     struct A {
         i: i32,
     }
     let a = A { i: 1 };
     let b = a;
-    let c = a;
+    let c = a; //~ ERROR
+    // { dg-error "use of moved value" "" { target *-*-* } .-1 }
+    /*
+     { dg-begin-multiline-output "" }
+   NN |     let b = a;
+      |             ~
+      |             |
+      |             value moved here
+   NN |     let c = a; //~ ERROR
+      |             ^
+      |             |
+      |             moved value used here
+     { dg-end-multiline-output "" }
+     */
+    
 }
 
 fn test_move_fixed() {
--- gcc/testsuite/rust/borrowck/test_move_conditional.rs
+++ gcc/testsuite/rust/borrowck/test_move_conditional.rs
@@ -1,6 +1,7 @@
-// { dg-additional-options "-frust-compile-until=compilation -frust-borrowcheck" }
+// { dg-additional-options "-frust-compile-until=compilation -frust-borrowcheck -fdiagnostics-show-caret -fdiagnostics-show-line-numbers" }
+// { dg-enable-nn-line-numbers "" }
 
-fn test_move_conditional(b1: bool, b2:bool) { // { dg-error "Found move errors in function test_move" }
+fn test_move_conditional(b1: bool, b2:bool) {
     struct A {
         i: i32,
     }
@@ -9,9 +10,47 @@ fn test_move_conditional(b1: bool, b2:bool) { // { dg-error "Found move errors i
     let b = a;
     if b1 {
         let b = a;
+        // { dg-error "use of moved value" "" { target *-*-* } .-1 }
+    /*
+     { dg-begin-multiline-output "" }
+   NN |     let b = a;
+      |             ~    
+      |             |
+      |             value moved here
+   NN |     if b1 {
+   NN |         let b = a;
+      |                 ~
+      |                 |
+      |                 value moved here
+......
+   NN |         let c = a;
+      |                 ^
+      |                 |
+      |                 moved value used here
+     { dg-end-multiline-output "" }
+     */
     }
     if b2 {
         let c = a;
+        // { dg-error "use of moved value" "" { target *-*-* } .-1 }
+    /*
+     { dg-begin-multiline-output "" }
+   NN |     let b = a;
+      |             ~    
+      |             |
+      |             value moved here
+   NN |     if b1 {
+   NN |         let b = a;
+      |                 ^
+      |                 |
+      |                 moved value used here
+......
+   NN |         let c = a;
+      |                 ~
+      |                 |
+      |                 value moved here
+     { dg-end-multiline-output "" }
+     */
     }
 }
 
