d08a5f211135374b3ad700780c46a198cd320328
Lingling Kong <lingling.kong@intel.com>
2024-08-14 16:42:29 +0800

    i386: Optimization for APX NDD is always zero-uppered for logic
    gcc/ChangeLog:
    
    	PR target/113729
    	* config/i386/i386.md (*andqi_1_zext<mode><nf_name>): New
    	define_insn.
    	(*andhi_1_zext<mode><nf_name>): Ditto.
    	(*<code>qi_1_zext<mode><nf_name>): Ditto.
    	(*<code>hi_1_zext<mode><nf_name>): Ditto.
    	(*negqi_1_zext<mode><nf_name>): Ditto.
    	(*neghi_1_zext<mode><nf_name>): Ditto.
    	(*one_cmplqi2_1_zext<mode>): Ditto.
    	(*one_cmplhi2_1_zext<mode>): Ditto.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/i386/pr113729.c: Add more tests.

--- gcc/testsuite/gcc.target/i386/pr113729.c
+++ gcc/testsuite/gcc.target/i386/pr113729.c
@@ -31,3 +31,43 @@ F (int, char, sub, -)
 F (int64_t, char, sub, -)
 F (int, short, sub, -)
 F (int64_t, short, sub, -)
+
+#define F1(TYPE1, TYPE2,  OP_NAME, OP)                \
+TYPE1                                                 \
+__attribute__ ((noipa))                               \
+f3_##OP_NAME##_##TYPE2##_##TYPE1 (unsigned TYPE2 *a)  \
+{                                                     \
+  unsigned TYPE2 b = OP*a;                            \
+  return b;                                           \
+}			
+/* neghi_1_zext<mode> */
+F1 (short, char, neg, -)
+F1 (int, char, neg, -)
+F1 (int64_t, char, neg, -)
+F1 (int, short, neg, -)
+F1 (int64_t, short, neg, -)
+/* one_cmplqi2_1_zext<mode> */
+F1 (short, char, not, ~)
+F1 (int, char, not, ~)
+F1 (int64_t, char, not, ~)
+F1 (int, short, not, ~)
+F1 (int64_t, short, not, ~)
+
+/* andqi_1_zext<mode> */
+F (short, char, and, &)
+F (int, char, and, &)
+F (int64_t, char, and, &)
+F (int, short, and, &)
+F (int64_t, short, and, &)
+/* iorqi_1_zext<mode> */
+F (short, char, or, |)
+F (int, char, or, |)
+F (int64_t, char, or, |)
+F (int, short, or, |)
+F (int64_t, short, or, |)
+/* xorqi_1_zext<mode> */
+F (short, char, xor, ^)
+F (int, char, xor, ^)
+F (int64_t, char, xor, ^)
+F (int, short, xor, ^)
+F (int64_t, short, xor, ^)
