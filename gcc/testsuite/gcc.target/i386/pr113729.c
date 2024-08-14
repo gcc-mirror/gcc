a302cd6abbeccc93784b7ecc86f2e9106aa412ca
Lingling Kong <lingling.kong@intel.com>
2024-08-14 16:35:29 +0800

    i386: Optimization for APX NDD is always zero-uppered for ADD
    gcc/ChangeLog:
    
    	PR target/113729
    	* config/i386/i386.md (*addqi_1_zext<mode><nf_name>): New
    	define.
    	(*addhi_1_zext<mode><nf_name>): Ditto.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/i386/pr113729.c: New test.

---
+++ gcc/testsuite/gcc.target/i386/pr113729.c
@@ -0,0 +1,27 @@
+/* { dg-do compile { target { ! ia32 } } } */
+/* { dg-options "-mapx-features=ndd -march=x86-64 -O2" } */
+/* { dg-final { scan-assembler-not "movz"} } */
+
+#include <stdint.h>
+
+#define F(TYPE1, TYPE2, OP_NAME, OP)                \
+TYPE1                                               \
+__attribute__ ((noipa))                             \
+f_##OP_NAME##_##TYPE2##_##TYPE1 (unsigned TYPE2 b)  \
+{                                                   \
+  return (unsigned TYPE2) (200 OP b);               \
+}                                                   \
+TYPE1                                               \
+__attribute__ ((noipa))                             \
+f1_##OP_NAME##_##TYPE2##_##TYPE1                    \
+(unsigned TYPE2 a, unsigned TYPE2 b)                \
+{                                                   \
+  return (unsigned TYPE2) (a OP b);                 \
+}					
+
+/* addqi_1_zext<mode> */
+F (short, char, add, +)
+F (int, char, add, +)
+F (int64_t, char, add, +)
+F (int, short, add, +)
+F (int64_t, short, add, +)
