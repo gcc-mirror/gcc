469602619d32c24382c6f1c3e3d95c3db606c770
Dhruv Chawla <dhruvc@nvidia.com>
2024-08-26 11:09:19 +0530

    libstdc++: Add missing feature-test macro in various headers
    version.syn#2 requires various headers to define
    __cpp_lib_allocator_traits_is_always_equal. Currently, only <memory> was
    defining this macro. Implement fixes for the other headers as well.
    
    Signed-off-by: Dhruv Chawla <dhruvc@nvidia.com>
    
    libstdc++-v3/ChangeLog:
    
    	* include/std/deque: Define macro
    	__glibcxx_want_allocator_traits_is_always_equal.
    	* include/std/forward_list: Likewise.
    	* include/std/list: Likewise.
    	* include/std/map: Likewise.
    	* include/std/scoped_allocator: Likewise.
    	* include/std/set: Likewise.
    	* include/std/string: Likewise.
    	* include/std/unordered_map: Likewise.
    	* include/std/unordered_set: Likewise.
    	* include/std/vector: Likewise.
    	* testsuite/20_util/headers/memory/version.cc: New test.
    	* testsuite/20_util/scoped_allocator/version.cc: Likewise.
    	* testsuite/21_strings/headers/string/version.cc: Likewise.
    	* testsuite/23_containers/deque/version.cc: Likewise.
    	* testsuite/23_containers/forward_list/version.cc: Likewise.
    	* testsuite/23_containers/list/version.cc: Likewise.
    	* testsuite/23_containers/map/version.cc: Likewise.
    	* testsuite/23_containers/set/version.cc: Likewise.
    	* testsuite/23_containers/unordered_map/version.cc: Likewise.
    	* testsuite/23_containers/unordered_set/version.cc: Likewise.
    	* testsuite/23_containers/vector/version.cc: Likewise.
    
    (cherry picked from commit efe6efb6f315c7f97be8a850e0a84ff7f6651d85)

---
+++ libstdc++-v3/testsuite/23_containers/set/version.cc
@@ -0,0 +1,8 @@
+// { dg-do preprocess { target c++17 } }
+// { dg-add-options no_pch }
+
+#include <set>
+
+#if __cpp_lib_allocator_traits_is_always_equal != 201411L
+# error "Feature-test macro __cpp_lib_allocator_traits_is_always_equal has wrong value in <version>"
+#endif
