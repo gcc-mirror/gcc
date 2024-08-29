00ec6bd805924b2d7d72cf03b200b3b4b7831835
Richard Sandiford <richard.sandiford@arm.com>
2024-08-29 14:00:23 +0100

    Make some smallest_int_mode_for_size calls cope with failure
    smallest_int_mode_for_size now returns an optional mode rather
    than aborting on failure.  This patch adjusts a couple of callers
    so that they fail gracefully when no mode exists.
    
    There should be no behavioural change, since anything that triggers
    the new return paths would previously have aborted.  I just think
    this is how the code would have been written if the option had been
    available earlier.
    
    gcc/
    	* dse.cc (find_shift_sequence): Allow smallest_int_mode_for_size
    	to failure.
    	* optabs.cc (expand_twoval_binop_libfunc): Likewise.

--- gcc/optabs.cc
+++ gcc/optabs.cc
@@ -2551,8 +2551,10 @@ expand_twoval_binop_libfunc (optab binoptab, rtx op0, rtx op1,
 
   /* The value returned by the library function will have twice as
      many bits as the nominal MODE.  */
-  libval_mode
-    = smallest_int_mode_for_size (2 * GET_MODE_BITSIZE (mode)).require ();
+  auto return_size = 2 * GET_MODE_BITSIZE (mode);
+  if (!smallest_int_mode_for_size (return_size).exists (&libval_mode))
+    return false;
+
   start_sequence ();
   libval = emit_library_call_value (libfunc, NULL_RTX, LCT_CONST,
 				    libval_mode,
