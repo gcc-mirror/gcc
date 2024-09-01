e4953b4d1d85e5aede98985d93a152a6335796ec
badumbatish <tanghocle456@gmail.com>
2024-08-31 23:37:19 -0700

    Provide new asm test case for amd64
    gcc/testsuite/ChangeLog:
    
    	* rust/execute/torture/inline_asm_mov_x_5.rs: Move to...
    	* rust/execute/torture/inline_asm_mov_x_5_ARM.rs: ...here.
    	* rust/execute/torture/inline_asm_mov_x_5_x86_64.rs: New test.

---
+++ gcc/testsuite/rust/execute/torture/inline_asm_mov_x_5_x86_64.rs
@@ -0,0 +1,24 @@
+/* { dg-do run { target x86_64*-*-* } } */
+/* { dg-output "5\r*\n" }*/
+
+#![feature(rustc_attrs)]
+#[rustc_builtin_macro]
+macro_rules! asm {
+    () => {};
+}
+
+extern "C" {
+    fn printf(s: *const i8, ...);
+}
+
+fn main() -> i32 {
+    let mut _x: i32 = 0;
+    unsafe {
+        asm!(
+            "mov $5, {}",
+            out(reg) _x
+        );
+        printf("%d\n\0" as *const str as *const i8, _x);
+    }
+    0
+}
