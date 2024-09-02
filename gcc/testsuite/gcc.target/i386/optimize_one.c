86f5031c804220274a9bbebd26b8ebf47a2207ac
Hu, Lin1 <lin1.hu@intel.com>
2024-09-02 10:24:31 +0800

    i386: Optimize ordered and nonequal
    Currently, when we input !__builtin_isunordered (a, b) && (a != b), gcc
    will emit
      ucomiss %xmm1, %xmm0
      movl $1, %ecx
      setp %dl
      setnp %al
      cmovne %ecx, %edx
      andl %edx, %eax
      movzbl %al, %eax
    
    In fact,
      xorl %eax, %eax
      ucomiss %xmm1, %xmm0
      setne %al
    is better.
    
    gcc/ChangeLog:
    
    	* match.pd: Optimize (and ordered non-equal) to
    	(not (or unordered  equal))
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/i386/optimize_one.c: New test.

---
+++ gcc/testsuite/gcc.target/i386/optimize_one.c
@@ -0,0 +1,9 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -mfpmath=sse" } */
+/* { dg-final { scan-assembler-times "comi" 1 } } */
+/* { dg-final { scan-assembler-times "set" 1 } } */
+
+int is_ordered_or_nonequal_sh (float a, float b)
+{
+  return !__builtin_isunordered (a, b) && (a != b);
+}
