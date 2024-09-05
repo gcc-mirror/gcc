de3ca363811a3974e4398ecdb1db2274efd61a1c
Palmer Dabbelt <palmer@rivosinc.com>
2024-09-04 21:34:31 -0600

    [PATCH] RISC-V: Make the setCC/REE tests robust to instruction selection
    These tests were checking that the output of the setCC instruction was bit
    flipped, but it looks like they're really designed to test that
    redundant sign extension elimination fires on conditionals from function
    inputs.  Jeff just posed a patch to clean this code up with trips up on
    the arbitrary xori/snez instruction selection decision changing, so
    let's just robustify the tests.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/sge.c: Adjust regex to match the input.
    	* gcc.target/riscv/sgeu.c: Likewise.
    	* gcc.target/riscv/sle.c: Likewise.
    	* gcc.target/riscv/sleu.c: Likewise.

--- gcc/testsuite/gcc.target/riscv/sge.c
+++ gcc/testsuite/gcc.target/riscv/sge.c
@@ -8,5 +8,5 @@ sge (int x, int y)
   return x >= y;
 }
 
-/* { dg-final { scan-assembler "\\sxori\\sa0,a0,1\n\\sret\n" } } */
+/* { dg-final { scan-assembler "slt\\sa0,a0,a1" } } */
 /* { dg-final { scan-assembler-not "andi|sext\\.w" } } */
--- gcc/testsuite/gcc.target/riscv/sle.c
+++ gcc/testsuite/gcc.target/riscv/sle.c
@@ -8,5 +8,5 @@ sle (int x, int y)
   return x <= y;
 }
 
-/* { dg-final { scan-assembler "\\sxori\\sa0,a0,1\n\\sret\n" } } */
+/* { dg-final { scan-assembler "sgt\\sa0,a0,a1" } } */
 /* { dg-final { scan-assembler-not "andi|sext\\.w" } } */
