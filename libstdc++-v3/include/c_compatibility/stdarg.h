2484ba167e1c4a52d4989b71e1f5d4d27755500f
Jason Merrill <jason@redhat.com>
2024-09-09 12:35:37 -0400

    libstdc++: fix C header include guards
    Ever since the c_global and c_compatibility directories were added in
    r122533, the include guards have been oddly late in the files, with no
    comment about why that might be either in the commit message or the files
    themselves.  I don't see any justification for this; it seems like a
    scripting error in creating these files based on the ones in include/c.
    
    libstdc++-v3/ChangeLog:
    
    	* include/c_compatibility/ctype.h
    	* include/c_compatibility/errno.h
    	* include/c_compatibility/float.h
    	* include/c_compatibility/iso646.h
    	* include/c_compatibility/limits.h
    	* include/c_compatibility/locale.h
    	* include/c_compatibility/setjmp.h
    	* include/c_compatibility/signal.h
    	* include/c_compatibility/stdarg.h
    	* include/c_compatibility/stdbool.h
    	* include/c_compatibility/stddef.h
    	* include/c_compatibility/stdio.h
    	* include/c_compatibility/string.h
    	* include/c_compatibility/tgmath.h
    	* include/c_compatibility/time.h
    	* include/c_compatibility/uchar.h
    	* include/c_compatibility/wchar.h
    	* include/c_compatibility/wctype.h
    	* include/c_global/ccomplex
    	* include/c_global/cctype
    	* include/c_global/cerrno
    	* include/c_global/cfloat
    	* include/c_global/climits
    	* include/c_global/clocale
    	* include/c_global/cmath
    	* include/c_global/csetjmp
    	* include/c_global/csignal
    	* include/c_global/cstdalign
    	* include/c_global/cstdarg
    	* include/c_global/cstdbool
    	* include/c_global/cstdio
    	* include/c_global/cstdlib
    	* include/c_global/cstring
    	* include/c_global/ctgmath
    	* include/c_global/ctime
    	* include/c_global/cwchar
    	* include/c_global/cwctype: Move header guard before #includes.

--- libstdc++-v3/include/c_compatibility/stdarg.h
+++ libstdc++-v3/include/c_compatibility/stdarg.h
@@ -26,10 +26,10 @@
  *  This is a Standard C++ Library header.
  */
 
-#include <cstdarg>
-
 #ifndef _GLIBCXX_STDARG_H
 #define _GLIBCXX_STDARG_H 1
+
+#include <cstdarg>
 
 #ifdef _GLIBCXX_NAMESPACE_C
 using std::va_list;
--- libstdc++-v3/include/c_compatibility/tgmath.h
+++ libstdc++-v3/include/c_compatibility/tgmath.h
@@ -26,6 +26,9 @@
  *  This is a Standard C++ Library header.
  */
 
+#ifndef _GLIBCXX_TGMATH_H
+#define _GLIBCXX_TGMATH_H 1
+
 #include <bits/c++config.h>
 
 #if __cplusplus >= 201103L
@@ -35,8 +38,5 @@
 #  include_next <tgmath.h>
 # endif
 #endif
-
-#ifndef _GLIBCXX_TGMATH_H
-#define _GLIBCXX_TGMATH_H 1
 
 #endif
