4645aa79a6dfe39cb58d21ee251bfde59a6b27d4
Eric Botcazou <ebotcazou@adacore.com>
2024-09-09 12:23:42 +0200

    Remove problematic declaration for 32-bit targets
    gcc/testsuite
    	PR ada/115246
    	* gnat.dg/alignment14.adb (My_Int2): Delete.
    	(Arr2): Likewise.

--- gcc/testsuite/gnat.dg/alignment14.adb
+++ gcc/testsuite/gnat.dg/alignment14.adb
@@ -7,11 +7,6 @@ procedure Alignment14 is
 
   type Arr1 is array (1 .. 2) of My_Int1;
 
-  type My_Int2 is new Integer;
-  for My_Int2'Alignment use 16;
-
-  type Arr2 is array (1 .. 2) of My_Int2;
-
 begin
    null;
 end Alignment14;
