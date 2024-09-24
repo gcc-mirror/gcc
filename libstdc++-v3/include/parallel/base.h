2407dbe1d887068a13c6ee9b6922560f1c89def5
Jason Merrill <jason@redhat.com>
2024-09-23 23:38:34 -0400

    libstdc++: more #pragma diagnostic
    The CI saw failures on 17_intro/headers/c++2011/parallel_mode.cc due to
    -Wdeprecated-declarations warnings in some parallel/ headers.
    
    libstdc++-v3/ChangeLog:
    
    	* include/parallel/base.h: Suppress -Wdeprecated-declarations.
    	* include/parallel/multiseq_selection.h: Likewise.

--- libstdc++-v3/include/parallel/base.h
+++ libstdc++-v3/include/parallel/base.h
@@ -166,6 +166,8 @@ namespace __gnu_parallel
       { return !_M_comp(__a, __b) && !_M_comp(__b, __a); }
     };
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations" // *nary_function
 
   /** @brief Similar to std::unary_negate,
    *  but giving the argument types explicitly. */
@@ -296,6 +298,8 @@ namespace __gnu_parallel
   template<typename _Tp>
     struct _Multiplies<_Tp, _Tp, _Tp>
     : public std::multiplies<_Tp> { };
+
+#pragma GCC diagnostic pop // -Wdeprecated-declarations
 
   /** @brief _Iterator associated with __gnu_parallel::_PseudoSequence.
    *  If features the usual random-access iterator functionality.
