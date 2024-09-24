7e560ffd7562cbd1a51ae6298c515b89ebed1363
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2024-09-24 09:59:56 +0200

    Testsuite, darwin: account for macOS 15
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/darwin-minversion-link.c: Account for macOS 15.

--- gcc/testsuite/gcc.dg/darwin-minversion-link.c
+++ gcc/testsuite/gcc.dg/darwin-minversion-link.c
@@ -19,6 +19,7 @@
 /* { dg-additional-options "-mmacosx-version-min=012.000.00 -DCHECK=120000" { target *-*-darwin21* } } */
 /* { dg-additional-options "-mmacosx-version-min=013.000.00 -DCHECK=130000" { target *-*-darwin22* } } */
 /* { dg-additional-options "-mmacosx-version-min=014.000.00 -DCHECK=140000" { target *-*-darwin23* } } */
+/* { dg-additional-options "-mmacosx-version-min=015.000.00 -DCHECK=150000" { target *-*-darwin24* } } */
 
 int
 main ()
