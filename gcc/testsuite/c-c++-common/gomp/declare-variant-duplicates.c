6935bddd8f90dde6009a1b8dea9745788ceeefb1
Sandra Loosemore <sloosemore@baylibre.com>
2024-09-25 02:59:53 +0000

    OpenMP: Fix testsuite failure on x86 with -m32
    The testcase decare-variant-duplicates.c added in commit
    96246bff0bcd9e5cdec9e6cf811ee3db4997f6d4 failed on 32-bit x86
    because on that target "i386" is defined as a preprocessor macro
    and cannot be used as an identifier.  Fixed by rewriting that test
    not to do that.
    
    gcc/testsuite/ChangeLog
    	* c-c++-common/gomp/declare-variant-duplicates.c: Avoid using
    	"i386" as an identifier.

--- gcc/testsuite/c-c++-common/gomp/declare-variant-duplicates.c
+++ gcc/testsuite/c-c++-common/gomp/declare-variant-duplicates.c
@@ -8,6 +8,6 @@ extern int f4 (int);
 
 #pragma omp declare variant (f1) match (device={kind(cpu,gpu,"cpu")})  /* { dg-error "trait-property .cpu. specified more than once" } */
 #pragma omp declare variant (f2) match (device={isa(sse4,"avx",avx)})  /* { dg-error "trait-property .avx. specified more than once" } */
-#pragma omp declare variant (f3) match (device={arch(x86_64,i386,aarch64,"i386")})  /* { dg-error "trait-property .i386. specified more than once" } */
+#pragma omp declare variant (f3) match (device={arch(x86_64,"i386",aarch64,"x86_64")})  /* { dg-error "trait-property .x86_64. specified more than once" } */
 #pragma omp declare variant (f4) match (implementation={vendor(llvm,gnu,"arm",gnu)})  /* { dg-error "trait-property .gnu. specified more than once" } */
 int f (int);
