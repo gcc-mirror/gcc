819098dc71f2079aedc15a904ab5f17f0788d991
Sam James <sam@gentoo.org>
2024-09-26 15:43:33 +0100

    testsuite: XFAIL gfortran.dg/initialization_25.f90 properly
    The test was disabled/XFAIL'd informally in r0-100012-gcdc6637d7c78ec,
    but r15-3890-g34bf6aa41ba539 didn't realize this, causing a FAIL.
    
    Fix that by marking it as XFAIL per the original intent.
    
    gcc/testsuite/ChangeLog:
    	PR fortran/35779
    	PR fortran/116858
    
    	* gfortran.dg/initialization_25.f90: Mark as XFAIL.

--- gcc/testsuite/gfortran.dg/initialization_25.f90
+++ gcc/testsuite/gfortran.dg/initialization_25.f90
@@ -5,8 +5,8 @@
 ! Dick Hendrickson <dick DOT hendrickson AT gmail DOT com>
 !
 ! Initial patch was reverted as it broke nested loops (see initialization_26.f90).
-!
+! XFAIL is for PR35779
 
 !   INTEGER :: J1
-!   INTEGER,PARAMETER :: I2(10) = (/(J1,J1=its_bad,1,-1)/) ! { dg-error "does not reduce" }
+!   INTEGER,PARAMETER :: I2(10) = (/(J1,J1=its_bad,1,-1)/) ! { dg-error "does not reduce" { xfail *-*-* } }
 END
