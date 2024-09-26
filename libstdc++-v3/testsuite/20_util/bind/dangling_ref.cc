c580b8a2b59208efc687e2b706e3a40225167854
Jonathan Wakely <jwakely@redhat.com>
2024-09-26 23:43:20 +0100

    libstdc++: Fix test FAILs due to -Wreturn-local-addr
    This fixes two FAILs due to -Wpointer-arith warnings when testing with
    c++11 or c++14 dialects.
    
    libstdc++-v3/ChangeLog:
    
    	* testsuite/20_util/bind/dangling_ref.cc: Add an additional
    	dg-warning for -Wreturn-local-addr warning.
    	* testsuite/30_threads/packaged_task/cons/dangling_ref.cc:
    	Likewise.

--- libstdc++-v3/testsuite/20_util/bind/dangling_ref.cc
+++ libstdc++-v3/testsuite/20_util/bind/dangling_ref.cc
@@ -5,5 +5,6 @@ int f();
 auto b = std::bind<const int&>(f);
 int i = b(); // { dg-error "here" "" { target { c++14_down } } }
 // { dg-error "dangling reference" "" { target { c++14_down } } 0 }
+// { dg-error "reference to temporary" "" { target { c++14_down } } 0 }
 // { dg-error "no matching function" "" { target c++17 } 0 }
 // { dg-error "enable_if" "" { target c++17 } 0 }
