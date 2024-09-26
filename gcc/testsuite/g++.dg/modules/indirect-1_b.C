064d5c67d7ad2be446c19e84f0cd993ecab784c3
Nathaniel Shead <nathanieloshead@gmail.com>
2024-09-26 11:12:02 +1000

    testsuite: Fix testcase g++.dg/modules/indirect-1_b.C [PR116846]
    r15-3878 exposed a mistake in the testcase, probably from an older
    version of the dumping logic.
    
    Apart from the slightly different syntax for the dump line, also check
    for importing the type_decl rather than the const_decl (we need the type
    anyway and importing the type also brings along the enumerators so it
    would be unnecessary to seed an import for them as well).
    
    	PR c++/116846
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/modules/indirect-1_b.C: Fix testcase.
    
    Signed-off-by: Nathaniel Shead <nathanieloshead@gmail.com>

--- gcc/testsuite/g++.dg/modules/indirect-1_b.C
+++ gcc/testsuite/g++.dg/modules/indirect-1_b.C
@@ -48,8 +48,5 @@ namespace bar
 // { dg-final { scan-lang-dump {Lazily binding '::foo::Scoped'@'foo' section:} module } }
 // { dg-final { scan-lang-dump-not {Lazily binding '::foo::Scoped@foo:.::[ABCD]'@'foo' section:} module } }
 
-// XFAIL is for PR116846
-// { dg-final { scan-lang-dump {Wrote named import:-[0-9]* const_decl:'::foo::Plain@\(foo\)::C'@foo} module { xfail *-*-* } } }
-// { dg-final { scan-lang-dump {Wrote named import:-[0-9]* const_decl:'::foo::Plain@\(foo\)::B'@foo} module { xfail *-*-* } } }
-// { dg-final { scan-lang-dump {Wrote named import:-[0-9]* const_decl:'::foo::Scoped@\(foo\)::C'@foo} module { xfail *-*-* } } }
-// { dg-final { scan-lang-dump {Wrote named import:-[0-9]* const_decl:'::foo::Scoped@\(foo\)::B'@foo} module { xfail *-*-* } } }
+// { dg-final { scan-lang-dump-times {Wrote import:-[0-9]* type_decl:'::foo::Plain@foo:.'@foo} 2 module } }
+// { dg-final { scan-lang-dump-times {Wrote import:-[0-9]* type_decl:'::foo::Scoped@foo:.'@foo} 2 module } }
