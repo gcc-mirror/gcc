141b2f9c7bb9b4060d39a0ae404c50b0c71b5ba0
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2024-09-26 22:59:48 +0200

    gccrs: Add box definition to avoid error
    Box definition is part of the standard library and cannot be found during
    name resolution. This simple definition prevent any error from being
    emitted.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/box_syntax_feature_gate.rs: Add box land item
    	definition.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

--- gcc/testsuite/rust/compile/box_syntax_feature_gate.rs
+++ gcc/testsuite/rust/compile/box_syntax_feature_gate.rs
@@ -1,4 +1,6 @@
 // { dg-options "-frust-compile-until=lowering" }
+#[lang = "owned_box"]
+pub struct Box<T>;
 
 fn main() {
     let x: Box<_> = box 1; //{ dg-error "box expression syntax is experimental." "" { target *-*-* }  }
