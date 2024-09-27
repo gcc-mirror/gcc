7040c207baa6b5d5f6065a47dd3559f3d3974a1b
Jonathan Wakely <jwakely@redhat.com>
2024-09-27 15:51:56 +0100

    libstdc++: Fix -Wsign-compare warning in std::string::resize_for_overwrite
    libstdc++-v3/ChangeLog:
    
    	* include/bits/basic_string.tcc (resize_for_overwrite): Fix
    	-Wsign-compare warning.
    	* include/bits/cow_string.h (resize_for_overwrite): Likewise.

--- libstdc++-v3/include/bits/cow_string.h
+++ libstdc++-v3/include/bits/cow_string.h
@@ -3800,7 +3800,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       static_assert(__gnu_cxx::__is_integer_nonstrict<decltype(__r)>::__value,
 		    "resize_and_overwrite operation must return an integer");
 #endif
-      _GLIBCXX_DEBUG_ASSERT(__r >= 0 && __r <= __n);
+      _GLIBCXX_DEBUG_ASSERT(__r >= 0 && size_type(__r) <= __n);
       __term._M_r = size_type(__r);
       if (__term._M_r > __n)
 	__builtin_unreachable();
