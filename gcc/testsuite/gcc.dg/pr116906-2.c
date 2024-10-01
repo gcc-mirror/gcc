9ade99bfbc7f47975d7f8765366904f4d2496afc
Richard Biener <rguenther@suse.de>
2024-10-01 10:37:16 +0200

    tree-optimization/116906 - unsafe PRE with never executed edges
    When we're computing ANTIC for PRE we treat edges to not yet visited
    blocks as having a maximum ANTIC solution to get at an optimistic
    solution in the iteration.  That assumes the edges visted eventually
    execute.  This is a wrong assumption that can lead to wrong code
    (and not only non-optimality) when possibly trapping expressions
    are involved as the testcases in the PR show.  The following mitigates
    this by pruning trapping expressions from ANTIC computed when
    maximum sets are involved.
    
    	PR tree-optimization/116906
    	* tree-ssa-pre.cc (prune_clobbered_mems): Add clean_traps
    	argument.
    	(compute_antic_aux): Direct prune_clobbered_mems to prune
    	all traps when any MAX solution was involved in the ANTIC
    	computation.
    	(compute_partial_antic_aux): Adjust.
    
    	* gcc.dg/pr116906-1.c: New testcase.
    	* gcc.dg/pr116906-2.c: Likewise.
    
    (cherry picked from commit 3e1bd6470e4deba1a3ad14621037098311ad1350)

---
+++ gcc/testsuite/gcc.dg/pr116906-2.c
@@ -0,0 +1,40 @@
+/* { dg-do run { target *-*-linux* *-*-gnu* *-*-uclinux* } } */
+/* { dg-options "-O2 -fno-tree-ch" } */
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+int x;
+
+void __attribute__((noipa))
+foo (int *p, unsigned n)
+{
+  unsigned i = 0;
+  do
+    {
+      if (i == n)
+        break;
+      if (p)
+        x = *p;
+      i += 2;
+    }
+  while (1);
+  x = *p;
+}
+
+void do_exit (int i)
+{
+  exit (0);
+}
+
+int main()
+{
+  struct sigaction s;
+  sigemptyset (&s.sa_mask);
+  s.sa_handler = do_exit;
+  s.sa_flags = 0;
+  sigaction (SIGALRM, &s, NULL);
+  alarm (1);
+  foo ((int *)0, 1);
+}
