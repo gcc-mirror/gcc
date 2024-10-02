79ea0aab75732c26c38d4b64f1d97acedf80155a
Richard Biener <rguenther@suse.de>
2024-10-02 11:27:09 +0200

    testsuite/116660 - adjust testcases unexpectedly failing on 32bit sparc
    Both testcases miss some effective target requires.
    
    	PR testsuite/116660
    	* gcc.dg/vect/no-scevccp-outer-12.c: Add vect_pack_trunc.
    	* gcc.dg/vect/vect-multitypes-6.c: Add vect_char_add, remove
    	explicit 32bit sparc XFAIL.

--- gcc/testsuite/gcc.dg/vect/vect-multitypes-6.c
+++ gcc/testsuite/gcc.dg/vect/vect-multitypes-6.c
@@ -1,6 +1,7 @@
 /* Disabling epilogues until we find a better way to deal with scans.  */
 /* { dg-additional-options "--param vect-epilogues-nomask=0" } */
 /* { dg-require-effective-target vect_int } */
+/* { dg-require-effective-target vect_char_add } */
 /* { dg-add-options double_vectors } */
 
 #include <stdarg.h>
@@ -67,7 +68,7 @@ int main (void)
   return 0;
 }
 
-/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { xfail { sparc*-*-* && ilp32 } }} } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } } */
 /* { dg-final { scan-tree-dump-times "Alignment of access forced using versioning" 6 "vect" { target { vect_no_align && { ! vect_hw_misalign } } } } } */
 /* { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 6 "vect" { xfail { ! { vect_unaligned_possible && vect_align_stack_vars } } } } } */
 
