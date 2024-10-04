0f39769ab39ba74c9890c1553c016469c043965c
Georg-Johann Lay <avr@gjlay.de>
2024-10-04 17:43:47 +0200

    testsuite - Some float64 and float32x test require double64plus.
    Some of the float64 and float32x test cases are using double built-ins
    and hence require double64plus resp. that double is at least as good
    as float32x (double_float32xplus).
    
    gcc/testsuite/
    	* lib/target-supports.exp (check_effective_target_double_float32xplus):
    	New proc.
    	* gcc.dg/torture/float32x-builtin.c: Add
    	dg-require-effective-target double_float32xplus.
    	* gcc.dg/torture/float32x-tg-2.c: Same.
    	* gcc.dg/torture/float32x-tg.c: Same.
    	* gcc.dg/torture/float64-builtin.c: Add
    	dg-require-effective-target double64plus.
    	* gcc.dg/torture/float64-tg-2.c: Same.
    	* gcc.dg/torture/float64-tg.c: Same.

--- gcc/testsuite/gcc.dg/torture/float32x-tg-2.c
+++ gcc/testsuite/gcc.dg/torture/float32x-tg-2.c
@@ -4,6 +4,7 @@
 /* { dg-add-options float32x } */
 /* { dg-add-options ieee } */
 /* { dg-require-effective-target float32x_runtime } */
+/* { dg-require-effective-target double_float32xplus } */
 
 #define WIDTH 32
 #define EXT 1
--- gcc/testsuite/gcc.dg/torture/float64-tg-2.c
+++ gcc/testsuite/gcc.dg/torture/float64-tg-2.c
@@ -4,6 +4,7 @@
 /* { dg-add-options float64 } */
 /* { dg-add-options ieee } */
 /* { dg-require-effective-target float64_runtime } */
+/* { dg-require-effective-target double64plus } */
 
 #define WIDTH 64
 #define EXT 0
