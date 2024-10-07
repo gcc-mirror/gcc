b95ad25f9c9376575dcde4bcb529d3ca31b27359
Tobias Burnus <tburnus@baylibre.com>
2024-10-07 23:57:42 +0200

    Move gfortran.dg/gomp/allocate-static.f90 to libgomp.fortran/
    The testcase was turned into a 'dg-do run' check to check for the alignment,
    but this only works in testsuite/gfortran.dg, causing link errors for
    out-of-tree testing. The test was added in r15-4104-ga8caeaacf499d5.
    
    gcc/testsuite/:
    
    	* gfortran.dg/gomp/allocate-static.f90: Move to libgomp/testsuite/.
    
    libgomp/:
    
    	* testsuite/libgomp.fortran/allocate-static.f90: Moved from
    	gcc/testsuite/ as it is a dg-do run test; use real omp_lib_kinds
    	instead of local definition

--- libgomp/testsuite/libgomp.fortran/allocate-static.f90
+++ libgomp/testsuite/libgomp.fortran/allocate-static.f90
@@ -1,31 +1,3 @@
-! { dg-do run }
-
-module omp_lib_kinds
-  use iso_c_binding, only: c_int, c_intptr_t
-  implicit none
-  private :: c_int, c_intptr_t
-  integer, parameter :: omp_allocator_handle_kind = c_intptr_t
-
-  integer (kind=omp_allocator_handle_kind), &
-     parameter :: omp_null_allocator = 0
-  integer (kind=omp_allocator_handle_kind), &
-     parameter :: omp_default_mem_alloc = 1
-  integer (kind=omp_allocator_handle_kind), &
-     parameter :: omp_large_cap_mem_alloc = 2
-  integer (kind=omp_allocator_handle_kind), &
-     parameter :: omp_const_mem_alloc = 3
-  integer (kind=omp_allocator_handle_kind), &
-     parameter :: omp_high_bw_mem_alloc = 4
-  integer (kind=omp_allocator_handle_kind), &
-     parameter :: omp_low_lat_mem_alloc = 5
-  integer (kind=omp_allocator_handle_kind), &
-     parameter :: omp_cgroup_mem_alloc = 6
-  integer (kind=omp_allocator_handle_kind), &
-     parameter :: omp_pteam_mem_alloc = 7
-  integer (kind=omp_allocator_handle_kind), &
-     parameter :: omp_thread_mem_alloc = 8
-end module
-
 module m
   use iso_c_binding, only: c_intptr_t
   use omp_lib_kinds, only: omp_default_mem_alloc
