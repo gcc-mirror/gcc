cf08dd297ca9e13b46ba4ff203dbcdce49dbc067
Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>
2024-10-07 09:06:37 +0200

    testsuite: arm: use effective-target for vsel*, mod* and pr65647.c tests
    Update test cases to use -mcpu=unset/-march=unset feature introduced in
    r15-3606-g7d6c6a0d15c.
    
    gcc/testsuite/ChangeLog
    
    	* gcc.target/arm/pr65647.c: Use effective-target arm_arch_v6m.
    	Removed unneeded dg-skip-if.
    	* gcc.target/arm/mod_2.c: Use effective-target arm_cpu_cortex_a57.
    	* gcc.target/arm/mod_256.c: Likewise.
    	* gcc.target/arm/vseleqdf.c: Likewise.
    	* gcc.target/arm/vseleqsf.c: Likewise.
    	* gcc.target/arm/vselgedf.c: Likewise.
    	* gcc.target/arm/vselgesf.c: Likewise.
    	* gcc.target/arm/vselgtdf.c: Likewise.
    	* gcc.target/arm/vselgtsf.c: Likewise.
    	* gcc.target/arm/vselledf.c: Likewise.
    	* gcc.target/arm/vsellesf.c: Likewise.
    	* gcc.target/arm/vselltdf.c: Likewise.
    	* gcc.target/arm/vselltsf.c: Likewise.
    	* gcc.target/arm/vselnedf.c: Likewise.
    	* gcc.target/arm/vselnesf.c: Likewise.
    	* gcc.target/arm/vselvcdf.c: Likewise.
    	* gcc.target/arm/vselvcsf.c: Likewise.
    	* gcc.target/arm/vselvsdf.c: Likewise.
    	* gcc.target/arm/vselvssf.c: Likewise.
    	* lib/target-supports.exp: Define effective-target arm_cpu_cortex_a57.
    	Update effective-target arm_v8_1_lob_ok to use -mcpu=unset.
    
    Signed-off-by: Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>

--- gcc/testsuite/gcc.target/arm/mod_2.c
+++ gcc/testsuite/gcc.target/arm/mod_2.c
@@ -1,7 +1,9 @@
 /* { dg-do compile } */
 /* { dg-skip-if "-mpure-code supports M-profile only" { *-*-* } { "-mpure-code" } } */
 /* { dg-require-effective-target arm32 } */
-/* { dg-options "-O2 -mcpu=cortex-a57 -save-temps" } */
+/* { dg-require-effective-target arm_cpu_cortex_a57 } */
+/* { dg-options "-O2 -save-temps" } */
+/* { dg-add-options arm_cpu_cortex_a57 } */
 
 #include "../aarch64/mod_2.x"
 
--- gcc/testsuite/gcc.target/arm/vselvcsf.c
+++ gcc/testsuite/gcc.target/arm/vselvcsf.c
@@ -1,7 +1,8 @@
 /* { dg-do compile } */
-/* { dg-require-effective-target arm_arch_v8a_ok } */
+/* { dg-require-effective-target arm_cpu_cortex_a57_ok } */
 /* { dg-require-effective-target arm_v8_vfp_ok } */
-/* { dg-options "-O2 -mcpu=cortex-a57" } */
+/* { dg-options "-O2" } */
+/* { dg-add-options arm_cpu_cortex_a57 } */
 /* { dg-add-options arm_v8_vfp } */
 
 float
