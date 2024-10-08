0199e91f5e897985aaa587610f37962e62fc3473
Ville Voutilainen <ville.voutilainen@gmail.com>
2024-10-08 13:06:31 +0300

    Parser loop braino

--- gcc/cp/parser.cc
+++ gcc/cp/parser.cc
@@ -13239,6 +13239,12 @@ cp_parser_statement (cp_parser* parser, tree in_statement_expr,
 		  if (!flag_contracts_nonattr_mutable_keyword &&
 		      !flag_contracts_nonattr_const_keyword)
 		    break;
+		  if (tok->keyword == RID_CONST &&
+		      !flag_contracts_nonattr_const_keyword)
+		    break;
+		  if (tok->keyword == RID_MUTABLE &&
+		      !flag_contracts_nonattr_mutable_keyword)
+		    break;
 		  if (flag_contracts_nonattr_mutable_keyword)
 		    {
 		      if (tok && tok->keyword == RID_MUTABLE)
@@ -31542,6 +31548,12 @@ cp_parser_contract_attribute_spec (cp_parser *parser, tree attribute,
 	{
 	  if (!flag_contracts_nonattr_mutable_keyword &&
 	      !flag_contracts_nonattr_const_keyword)
+	    break;
+	  if (tok->keyword == RID_CONST &&
+	      !flag_contracts_nonattr_const_keyword)
+	    break;
+	  if (tok->keyword == RID_MUTABLE &&
+	      !flag_contracts_nonattr_mutable_keyword)
 	    break;
 	  if (flag_contracts_nonattr_mutable_keyword)
 	    {
