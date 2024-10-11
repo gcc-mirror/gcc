c4af4fe11e71c686ee06c1eebe9e64ad5a94410a
Pan Li <pan2.li@intel.com>
2024-10-11 12:12:03 +0800

    RISC-V: Add testcases for form 1 of vector signed SAT_SUB
    Form 1:
      #define DEF_VEC_SAT_S_SUB_FMT_1(T, UT, MIN, MAX)                     \
      void __attribute__((noinline))                                       \
      vec_sat_s_add_##T##_fmt_1 (T *out, T *op_1, T *op_2, unsigned limit) \
      {                                                                    \
        unsigned i;                                                        \
        for (i = 0; i < limit; i++)                                        \
          {                                                                \
            T x = op_1[i];                                                 \
            T y = op_2[i];                                                 \
            T minus = (UT)x - (UT)y;                                       \
            out[i] = (x ^ y) >= 0                                          \
              ? minus                                                      \
              : (minus ^ x) >= 0                                           \
                ? minus                                                    \
                : x < 0 ? MIN : MAX;                                       \
          }                                                                \
      }
    
    DEF_VEC_SAT_S_SUB_FMT_1(int8_t, uint8_t, INT8_MIN, INT8_MAX)
    
    The below test are passed for this patch.
    * The rv64gcv fully regression test.
    
    It is test only patch and obvious up to a point, will commit it
    directly if no comments in next 48H.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_data.h: Add test
    	data for run test.
    	* gcc.target/riscv/rvv/autovec/vec_sat_arith.h: Add test helper
    	macros.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_s_sub-1-i16.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_s_sub-1-i32.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_s_sub-1-i64.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_s_sub-1-i8.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_s_sub-run-1-i16.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_s_sub-run-1-i32.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_s_sub-run-1-i64.c: New test.
    	* gcc.target/riscv/rvv/autovec/binop/vec_sat_s_sub-run-1-i8.c: New test.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

--- gcc/testsuite/gcc.target/riscv/rvv/autovec/vec_sat_arith.h
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/vec_sat_arith.h
@@ -446,6 +446,26 @@ vec_sat_u_sub_##T1##_##T2##_fmt_zip (T1 *x, T2 b, unsigned limit) \
 }
 #define DEF_VEC_SAT_U_SUB_ZIP_WRAP(T1, T2) DEF_VEC_SAT_U_SUB_ZIP(T1, T2)
 
+#define DEF_VEC_SAT_S_SUB_FMT_1(T, UT, MIN, MAX)                     \
+void __attribute__((noinline))                                       \
+vec_sat_s_sub_##T##_fmt_1 (T *out, T *op_1, T *op_2, unsigned limit) \
+{                                                                    \
+  unsigned i;                                                        \
+  for (i = 0; i < limit; i++)                                        \
+    {                                                                \
+      T x = op_1[i];                                                 \
+      T y = op_2[i];                                                 \
+      T minus = (UT)x - (UT)y;                                       \
+      out[i] = (x ^ y) >= 0                                          \
+        ? minus                                                      \
+        : (minus ^ x) >= 0                                           \
+          ? minus                                                    \
+          : x < 0 ? MIN : MAX;                                       \
+    }                                                                \
+}
+#define DEF_VEC_SAT_S_SUB_FMT_1_WRAP(T, UT, MIN, MAX) \
+  DEF_VEC_SAT_S_SUB_FMT_1(T, UT, MIN, MAX)
+
 #define RUN_VEC_SAT_U_SUB_FMT_1(T, out, op_1, op_2, N) \
   vec_sat_u_sub_##T##_fmt_1(out, op_1, op_2, N)
 
@@ -480,6 +500,11 @@ vec_sat_u_sub_##T1##_##T2##_fmt_zip (T1 *x, T2 b, unsigned limit) \
   vec_sat_u_sub_##T1##_##T2##_fmt_zip(x, b, N)
 #define RUN_VEC_SAT_U_SUB_FMT_ZIP_WRAP(T1, T2, x, b, N) \
   RUN_VEC_SAT_U_SUB_FMT_ZIP(T1, T2, x, b, N) \
+
+#define RUN_VEC_SAT_S_SUB_FMT_1(T, out, op_1, op_2, N) \
+  vec_sat_s_sub_##T##_fmt_1(out, op_1, op_2, N)
+#define RUN_VEC_SAT_S_SUB_FMT_1_WRAP(T, out, op_1, op_2, N) \
+  RUN_VEC_SAT_S_SUB_FMT_1(T, out, op_1, op_2, N)
 
 /******************************************************************************/
 /* Saturation Sub Truncated (Unsigned and Signed)                             */
