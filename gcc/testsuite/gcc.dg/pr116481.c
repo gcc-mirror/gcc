451efdb6b075e16efb9e41b034671ca55b54a11b
Richard Biener <rguenther@suse.de>
2024-10-13 11:42:27 +0200

    tree-optimization/116481 - avoid building function_type[]
    The following avoids building an array type with function or method
    element type during diagnosing an array bound violation as this
    will result in an error, rejecting a program with a not too useful
    error message.  Instead build such array type manually.
    
    	PR tree-optimization/116481
    	* pointer-query.cc (build_printable_array_type):
    	Build an array types with function or method element type
    	manually to avoid bogus diagnostic.
    
    	* gcc.dg/pr116481.c: New testcase.
    
    (cherry picked from commit 1506027347776a2f6ec5b92d56ef192e85944e2e)

---
+++ gcc/testsuite/gcc.dg/pr116481.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -Warray-bounds" } */
+
+extern void tramp ();
+
+int is_trampoline (void* function) /* { dg-bogus "arrays of functions are not meaningful" } */
+{
+  void* tramp_address = tramp;
+  if (!(((unsigned long)function & 3) == 2))
+    return 0;
+  return (((long *) ((char*)function - 2))[0]
+	  == ((long *) ((char*)tramp_address-2))[0]); /* { dg-warning "outside array bounds" } */
+}
