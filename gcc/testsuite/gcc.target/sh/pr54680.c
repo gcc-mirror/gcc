7ec8b4bf42fe9e27f7cf65fafae62e57be784db2
Oleg Endo <olegendo@gcc.gnu.org>
2024-10-13 10:33:17 +0900

    SH: Add -fno-math-errno to fsca,fsrra tests.
    Without -fno-math-errno some of the test might fail because the expected insns
    will not be generated.
    
    gcc/testsuite/ChangeLog:
    	* gcc.target/sh/pr53512-1.c: Add -fno-math-errno option.
    	* gcc.target/sh/pr53512-2.c: Likewise.
    	* gcc.target/sh/pr53512-3.c: Likewise.
    	* gcc.target/sh/pr53512-4.c: Likewise.
    	* gcc.target/sh/pr54680.c: Likewise.

--- gcc/testsuite/gcc.target/sh/pr54680.c
+++ gcc/testsuite/gcc.target/sh/pr54680.c
@@ -2,7 +2,7 @@
    to int.  Notice that we can't count just "lds" insns because mode switches
    use "lds.l".  */
 /* { dg-do compile { target { has_fsca } } }  */
-/* { dg-options "-O2 -mfsca -funsafe-math-optimizations -fno-ipa-icf" }  */
+/* { dg-options "-O2 -mfsca -funsafe-math-optimizations -fno-ipa-icf -fno-math-errno" }  */
 /* { dg-final { scan-assembler-times "fsca" 7 } } */
 /* { dg-final { scan-assembler-times "shad" 1 } } */
 /* { dg-final { scan-assembler-times "lds\tr\[0-9\],fpul" 6 } } */
