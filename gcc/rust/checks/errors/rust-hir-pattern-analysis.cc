a3049c8f050e6fbf0acd5e64263a14f67e93864f
Owen Avery <powerboat9.gamer@gmail.com>
2024-10-15 15:34:06 -0400

    gccrs: Use name resolver 2.0 in pattern checker
    gcc/rust/ChangeLog:
    
    	* checks/errors/rust-hir-pattern-analysis.cc: Add includes.
    	(PatternChecker::visit): Use name resolver 2.0 when enabled.
    
    Signed-off-by: Owen Avery <powerboat9.gamer@gmail.com>

--- gcc/rust/checks/errors/rust-hir-pattern-analysis.cc
+++ gcc/rust/checks/errors/rust-hir-pattern-analysis.cc
@@ -26,6 +26,10 @@
 #include "rust-mapping-common.h"
 #include "rust-system.h"
 #include "rust-tyty.h"
+#include "rust-immutable-name-resolution-context.h"
+
+// for flag_name_resolution_2_0
+#include "options.h"
 
 namespace Rust {
 namespace Analysis {
@@ -234,7 +238,17 @@ PatternChecker::visit (CallExpr &expr)
 
   NodeId ast_node_id = expr.get_fnexpr ()->get_mappings ().get_nodeid ();
   NodeId ref_node_id;
-  if (!resolver.lookup_resolved_name (ast_node_id, &ref_node_id))
+  if (flag_name_resolution_2_0)
+    {
+      auto &nr_ctx
+	= Resolver2_0::ImmutableNameResolutionContext::get ().resolver ();
+
+      if (auto id = nr_ctx.lookup (ast_node_id))
+	ref_node_id = *id;
+      else
+	return;
+    }
+  else if (!resolver.lookup_resolved_name (ast_node_id, &ref_node_id))
     return;
 
   if (auto definition_id = mappings.lookup_node_to_hir (ref_node_id))
