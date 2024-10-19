a8f36de5aa52db2864a4c068dd0b4c9464385159
Thomas Koenig <tkoenig@gcc.gnu.org>
2024-10-19 16:21:48 +0200

    Add missing dg-error to unsigned_38.f90.
    gcc/testsuite/ChangeLog:
    
    	PR fortran/117225
    	* gfortran.dg/unsigned_38.f90: Add missing dg-error directive.

--- gcc/testsuite/gfortran.dg/unsigned_38.f90
+++ gcc/testsuite/gfortran.dg/unsigned_38.f90
@@ -2,5 +2,5 @@
 ! { dg-options "-funsigned" }
 program main
   unsigned, parameter :: u = 7u
-  print *,mod(-(u+1u),u)
+  print *,mod(-(u+1u),u) ! { dg-error "Operand of unary numeric operator" }
 end program main
