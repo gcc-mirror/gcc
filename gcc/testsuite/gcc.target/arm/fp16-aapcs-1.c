205515da82a2914d765e74ba73fd2765e1254112
Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>
2024-10-20 11:48:42 +0200

    testsuite: arm: Use check-function-bodies in fp16-aapcs-* tests
    Converted the tests to use check-function-bodies in order to ensure that
    the sequence is correct.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/arm/fp16-aapcs-1.c: Use check-function-bodies.
    	* gcc.target/arm/fp16-aapcs-2.c: Likewise.
    	* gcc.target/arm/fp16-aapcs-3.c: Likewise.
    	* gcc.target/arm/fp16-aapcs-4.c: Likewise.
    
    Signed-off-by: Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>

--- gcc/testsuite/gcc.target/arm/fp16-aapcs-1.c
+++ gcc/testsuite/gcc.target/arm/fp16-aapcs-1.c
@@ -3,20 +3,63 @@
 /* { dg-require-effective-target arm_fp16_ok } */
 /* { dg-options "-O2" }  */
 /* { dg-add-options arm_fp16_ieee } */
+/* { dg-final { check-function-bodies "**" "" "" } } */
 
 /* Test __fp16 arguments and return value in registers (hard-float).  */
 
 void
 swap (__fp16, __fp16);
 
+/*
+** F:
+** ...
+** (
+
+Below block is for non-armv8.1
+** (
+**	vmov\.f32	(s[3-9]|s1[0-5]), s0
+** ...
+**	vmov\.f32	s0, s1
+** ...
+**	vmov\.f32	s1, \1
+** |
+**	vmov\.f32	(s[3-9]|s1[0-5]), s1
+** ...
+**	vmov\.f32	s1, s0
+** ...
+**	vmov\.f32	s0, \2
+** )
+**	vstr\.32	s2, \[sp, #4\]	@ int
+**	bl	swap
+**	vldr\.32	s2, \[sp, #4\]	@ int
+**	vmov\.f32	s0, s2
+
+** |
+
+Below block is for armv8.1
+** (
+**	vmov	(s[3-9]|s1[0-5]), s0	@ __fp16
+** ...
+**	vmov	s0, s1	@ __fp16
+** ...
+**	vmov	s1, \3	@ __fp16
+** |
+**	vmov	(s[3-9]|s1[0-5]), s1	@ __fp16
+** ...
+**	vmov	s1, s0	@ __fp16
+** ...
+**	vmov	s0, \4	@ __fp16
+** )
+**	vstr\.32	s2, \[sp, #4\]	@ int
+**	bl	swap
+**	vldr\.16	s0, \[sp, #4\]
+
+** )
+** ...
+*/
 __fp16
 F (__fp16 a, __fp16 b, __fp16 c)
 {
   swap (b, a);
   return c;
 }
-
-/* { dg-final { scan-assembler {vmov\.f32\ts[0-9]+, s1} } }  */
-/* { dg-final { scan-assembler {vmov\.f32\ts1, s0} } }  */
-/* { dg-final { scan-assembler {vmov\.f32\ts[0-9]+, s2+} } }  */
-/* { dg-final { scan-assembler-times {vmov\.f32\ts0, s[0-9]+} 2 } }  */
