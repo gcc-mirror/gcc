66a3827379e8863d9025b7881543d2bffe771aee
Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>
2024-10-24 10:40:27 +0200

    testsuite: arm: Use effective-target for memset-inline* tests
    As these tests are set to execute and require neon hardware to do so,
    add the missing dg-require-effective-target arm_neon_hw.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/arm/memset-inline-4.c: Use effective-target
    	arm_neon_hw.
    	* gcc.target/arm/memset-inline-5.c: Likewise.
    	* gcc.target/arm/memset-inline-6.c: Likewise.
    
    Signed-off-by: Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>

--- gcc/testsuite/gcc.target/arm/memset-inline-6.c
+++ gcc/testsuite/gcc.target/arm/memset-inline-6.c
@@ -1,5 +1,6 @@
 /* { dg-do run } */
 /* { dg-skip-if "Don't inline memset using neon instructions" { ! arm_tune_string_ops_prefer_neon } } */
+/* { dg-require-effective-target arm_neon_hw } */
 /* { dg-options "-save-temps -O2 -fno-inline" } */
 /* { dg-add-options "arm_neon" } */
 
