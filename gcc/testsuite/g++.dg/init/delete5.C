fe919da3c395d2214ffc81ad4f51f8344ad5a5c1
Simon Martin <simon@nasilyan.com>
2024-10-31 15:41:11 +0100

    c++: Add testcase for now fixed issue [PR101887]
    The testcase in PR101887 has been working since the fix for PR104846,
    via r12-7599-gac8310dd122172.
    
    This patch simply adds the case to the testsuite.
    
    	PR c++/101887
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/init/delete5.C: Add testcase from PR c++/101887.

--- gcc/testsuite/g++.dg/init/delete5.C
+++ gcc/testsuite/g++.dg/init/delete5.C
@@ -1,8 +1,9 @@
 // PR c++/104846
+// PR c++/101887
 // { dg-do compile { target c++14 } }
 
 struct S {
   auto operator delete (void *) {} // { dg-error ".operator delete. must return type .void'" }
+  auto operator delete (void*, __SIZE_TYPE__); // { dg-error ".operator delete. must return type .void'" }
   auto operator delete[] (void *) {} // { dg-error ".operator delete. must return type .void'" }
 };
-
