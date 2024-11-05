5c19ba52519be975d4464b063d3d5a2c700dd241
Di Zhao <dizhao@os.amperecomputing.com>
2024-11-05 12:28:54 +0800

    testsuite: fix testcase pr110279-1.c
    The test case is for targets that support FMA. Previously
    the "target" selector is missed in dg-final command.
    
    gcc/testsuite/ChangeLog:
    	PR tree-optimization/110279
    	* gcc.dg/pr110279-1.c: add target selector.

--- gcc/testsuite/gcc.dg/pr110279-1.c
+++ gcc/testsuite/gcc.dg/pr110279-1.c
@@ -1,4 +1,4 @@
-/* { dg-do compile } */
+/* { dg-do compile { target { scalar_all_fma || { i?86-*-* x86_64-*-* } } } } */
 /* { dg-options "-Ofast --param avoid-fma-max-bits=512 --param tree-reassoc-width=4 -fdump-tree-widening_mul-details" } */
 /* { dg-additional-options "-mcpu=generic" { target aarch64*-*-* } } */
 /* { dg-additional-options "-mfma" { target i?86-*-* x86_64-*-* } } */
@@ -64,4 +64,4 @@ foo3 (data_e a, data_e b, data_e c, data_e d)
   return result;
 }
 
-/* { dg-final { scan-tree-dump-times "Generated FMA" 3 "widening_mul"} } */
+/* { dg-final { scan-tree-dump-times "Generated FMA" 3 "widening_mul" } } */
