ee6711ead30876daf2a8a66f8647cad95470fe79
Thomas Schwinge <tschwinge@baylibre.com>
2024-11-10 18:29:25 +0100

    nvptx: Enhance '-march-map=[...]' test cases
    This expands upon the one test case added in
    commit de0ef04419e90eacf0d1ddb265552a1b08c18d4b "[nvptx] Add march-map".
    
    	gcc/testsuite/
    	* gcc.target/nvptx/march-map.c: Remove; expanded into...
    	* gcc.target/nvptx/march-map=sm_50.c: ... this.
    	* gcc.target/nvptx/march-map=sm_30.c: New.
    	* gcc.target/nvptx/march-map=sm_32.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_35.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_37.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_52.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_53.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_60.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_61.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_62.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_70.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_72.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_75.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_80.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_86.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_87.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_89.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_90.c: Likewise.
    	* gcc.target/nvptx/march-map=sm_90a.c: Likewise.
    	* gcc.target/nvptx/main.c: Remove.

--- gcc/testsuite/gcc.target/nvptx/march-map.c
+++
@@ -1,5 +0,0 @@
-/* { dg-options "-march-map=sm_50" } */
-
-#include "main.c"
-
-/* { dg-final { scan-assembler-times "\\.target\tsm_35" 1 } } */
---
+++ gcc/testsuite/gcc.target/nvptx/march-map=sm_75.c
@@ -0,0 +1,19 @@
+/* { dg-do assemble } */
+/* { dg-options {-march-map=sm_75 -mptx=_} } */
+/* { dg-additional-options -save-temps } */
+/* { dg-final { scan-assembler-times {(?n)^	\.version	6\.3$} 1 } } */
+/* { dg-final { scan-assembler-times {(?n)^	\.target	sm_75$} 1 } } */
+
+#if __PTX_ISA_VERSION_MAJOR__ != 6
+#error wrong value for __PTX_ISA_VERSION_MAJOR__
+#endif
+
+#if __PTX_ISA_VERSION_MINOR__ != 3
+#error wrong value for __PTX_ISA_VERSION_MINOR__
+#endif
+
+#if __PTX_SM__ != 750
+#error wrong value for __PTX_SM__
+#endif
+
+int dummy;
