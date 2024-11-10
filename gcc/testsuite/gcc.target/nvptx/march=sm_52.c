1ced50b3a0b8d822286792a63fdfff3402646916
Thomas Schwinge <tschwinge@baylibre.com>
2024-11-10 17:47:16 +0100

    nvptx: Support '-march=sm_52'
    	gcc/
    	* config/nvptx/nvptx-sm.def: Add '52'.
    	* config/nvptx/nvptx-gen.h: Regenerate.
    	* config/nvptx/nvptx-gen.opt: Likewise.
    	* config/nvptx/nvptx.cc (first_ptx_version_supporting_sm): Adjust.
    	* config/nvptx/nvptx.opt (-march-map=sm_52): Likewise.
    	* config.gcc: Likewise.
    	* doc/invoke.texi (Nvidia PTX Options): Document '-march=sm_52'.
    	* config/nvptx/gen-multilib-matches-tests: Extend.
    	gcc/testsuite/
    	* gcc.target/nvptx/march-map=sm_52.c: Adjust.
    	* gcc.target/nvptx/march=sm_52.c: New.
    	libgomp/
    	* testsuite/libgomp.c/declare-variant-3-sm52.c: New.
    	* testsuite/libgomp.c/declare-variant-3.h: Adjust.
    
    (cherry picked from commit 7cff338e5ae478cf841a4394741587bdb6ca76a8)

--- gcc/testsuite/gcc.target/nvptx/march=sm_52.c
+++ gcc/testsuite/gcc.target/nvptx/march=sm_52.c
@@ -1,5 +1,5 @@
 /* { dg-do assemble } */
-/* { dg-options {-march-map=sm_52 -mptx=_} } */
+/* { dg-options {-march=sm_52 -mptx=_} } */
 /* { dg-additional-options -save-temps } */
 /* { dg-final { scan-assembler-times {(?n)^	\.version	6\.0$} 1 } } */
 /* { dg-final { scan-assembler-times {(?n)^	\.target	sm_52$} 1 } } */
