a2467372e7241be661c5fde80b751d84df4d567b
Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>
2024-11-10 14:46:51 +0100

    testsuite: arm: fast-math-complex-add-half-float.c test should not xfail
    With the change in 15-3128-gde1923f9f4d, this test case no longer xfail.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/vect/complex/fast-math-complex-add-half-float.c: Remove
    	xfail from test.
    
    Signed-off-by: Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>

--- gcc/testsuite/gcc.dg/vect/complex/fast-math-complex-add-half-float.c
+++ gcc/testsuite/gcc.dg/vect/complex/fast-math-complex-add-half-float.c
@@ -8,7 +8,5 @@
 #define N 200
 #include "complex-add-template.c"
 
-/* Vectorization is failing for these cases.  They should work but for now ignore.  */
-
-/* { dg-final { scan-tree-dump-times "stmt.*COMPLEX_ADD_ROT270" 1 "vect" { xfail *-*-* } } } */
-/* { dg-final { scan-tree-dump-times "stmt.*COMPLEX_ADD_ROT90" 1 "vect" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-times "stmt.*COMPLEX_ADD_ROT270" 1 "vect" } } */
+/* { dg-final { scan-tree-dump-times "stmt.*COMPLEX_ADD_ROT90" 1 "vect" } } */
