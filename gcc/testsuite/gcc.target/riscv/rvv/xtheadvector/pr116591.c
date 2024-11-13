07d4c264a000b6448d6b519110c05c3b8a64d23b
Jin Ma <jinma@linux.alibaba.com>
2024-11-13 15:19:29 -0700

    [PATCH] RISC-V: Bugfix for unrecognizable insn for XTheadVector
    error: unrecognizable insn:
    
    (insn 35 34 36 2 (set (subreg:RVVM1SF (reg/v:RVVM1x4SF 142 [ _r ]) 0)
            (unspec:RVVM1SF [
                    (const_vector:RVVM1SF repeat [
                            (const_double:SF 0.0 [0x0.0p+0])
                        ])
                    (reg:DI 0 zero)
                    (const_int 1 [0x1])
                    (reg:SI 66 vl)
                    (reg:SI 67 vtype)
                ] UNSPEC_TH_VWLDST)) -1
         (nil))
    during RTL pass: mode_sw
    
    	PR target/116591
    
    gcc/ChangeLog:
    
    	* config/riscv/vector.md: Add restriction to call pred_th_whole_mov.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/xtheadvector/pr116591.c: New test.
    
    (cherry picked from commit 8564d0948c72df0a66d7eb47e15c6ab43e9b25ce)

---
+++ gcc/testsuite/gcc.target/riscv/rvv/xtheadvector/pr116591.c
@@ -0,0 +1,14 @@
+/* { dg-do assemble } */
+/* { dg-options "-march=rv32gc_xtheadvector -mabi=ilp32d -O2 -save-temps" { target { rv32 } } } */
+/* { dg-options "-march=rv64gc_xtheadvector -mabi=lp64d -O2 -save-temps" { target { rv64 } } } */
+
+#include <riscv_vector.h>
+
+void
+foo (float *a, int b)
+{
+  vfloat32m1x4_t c;
+  __riscv_vsseg4e32_v_f32m1x4(a, c, b);
+}
+
+/* { dg-final { scan-assembler-times {th\.vmv\.v\.i\tv[0-9]+,0} 4 } } */
