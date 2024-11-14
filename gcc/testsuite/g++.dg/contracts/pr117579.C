fa81ee665834b8669735f0104163db6e45bdc2f6
Nina Ranns <dinka.ranns@gmail.com>
2024-11-14 14:20:02 +0000

    github issue 17 fix - contract_assert not followed by  causes a crash

--- gcc/testsuite/g++.dg/contracts/pr117579.C
+++ gcc/testsuite/g++.dg/contracts/pr117579.C
@@ -6,4 +6,5 @@ void f();
 int main ()
 {
   [[assert: true]] f(); // { dg-error "assertions must be followed by" }
+  // { dg-error "contract assertion on a non empty statement" "" {target *-*-* } .-1 }
 }
