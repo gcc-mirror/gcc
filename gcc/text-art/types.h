76c202329458aad027ececc59d666e4995e3644e
Andrew Pinski <quic_apinski@quicinc.com>
2024-11-22 09:25:41 -0800

    test-art: Fix comment in types.h
    The comment references INCLUDE_MEMORY but the code actually
    checks INCLUDE_VECTOR. So fix up the comment to mention
    INCLUDE_VECTROR.
    
    Pushed as obvious.
    
    gcc/ChangeLog:
    
    	* text-art/types.h: Fix comment.
    
    Signed-off-by: Andrew Pinski <quic_apinski@quicinc.com>

--- gcc/text-art/types.h
+++ gcc/text-art/types.h
@@ -23,7 +23,7 @@ along with GCC; see the file COPYING3.  If not see
 
 /* This header uses std::vector, but <vector> can't be directly
    included due to issues with macros.  Hence it must be included from
-   system.h by defining INCLUDE_MEMORY in any source file using it.  */
+   system.h by defining INCLUDE_VECTOR in any source file using it.  */
 
 #ifndef INCLUDE_VECTOR
 # error "You must define INCLUDE_VECTOR before including system.h to use text-art/types.h"
