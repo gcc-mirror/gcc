08bb92d642757f1d4374b947f45f871cc04b8c65
David Malcolm <dmalcolm@redhat.com>
2024-11-26 10:39:12 -0500

    plugin: add missing colon in error message [PR93746]
    gcc/ChangeLog:
    	PR plugins/93746
    	* plugin.cc (try_init_one_plugin): Add missing colon in error
    	message.
    
    Signed-off-by: David Malcolm <dmalcolm@redhat.com>

--- gcc/plugin.cc
+++ gcc/plugin.cc
@@ -714,7 +714,7 @@ try_init_one_plugin (struct plugin_name_args *plugin)
   /* Check the plugin license.  */
   if (dlsym (dl_handle, str_license) == NULL)
     fatal_error (input_location,
-		 "plugin %s is not licensed under a GPL-compatible license"
+		 "plugin %s is not licensed under a GPL-compatible license:"
 		 " %s", plugin->full_name, dlerror ());
 
   PTR_UNION_AS_VOID_PTR (plugin_init_union)
