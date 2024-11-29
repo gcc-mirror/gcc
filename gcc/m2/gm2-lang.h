59f5d4ef99702619d94b475de06bb937fa6d22af
Gaius Mulley <gaiusmod2@gmail.com>
2024-11-29 18:34:33 +0000

    [PATCH] PR modula2/116557 Remove physical address from the GPL header comment
    This patch removes the physical address from all the header comments
    in the m2 subdirectory.  The physical address is replaced with the
    text "You should have received a copy of the GNU General Public License
    along with GCC; see the file COPYING3.  If not see
    <http://www.gnu.org/licenses/>." instead.
    
    gcc/m2/ChangeLog:
    
    	PR modula2/116557
    	* gm2-lang.cc: Replace physical address with URL in GPL header.
    	* gm2-lang.h: Ditto.
    	* images/LICENSE.IMG: Ditto.
    	* m2-tree.def: Ditto.
    	* mc-boot/GIndexing.cc: Ditto.
    	* mc-boot/Gkeyc.cc: Ditto.
    	* mc-boot/Glists.cc: Ditto.
    	* mc-boot/GmcComp.cc: Ditto.
    	* mc-boot/GmcDebug.cc: Ditto.
    	* mc-boot/GmcFileName.cc: Ditto.
    	* mc-boot/GmcMetaError.cc: Ditto.
    	* mc-boot/GmcOptions.cc: Ditto.
    	* mc-boot/GmcPreprocess.cc: Ditto.
    	* mc-boot/GmcPretty.cc: Ditto.
    	* mc-boot/GmcPrintf.cc: Ditto.
    	* mc-boot/GmcQuiet.cc: Ditto.
    	* mc-boot/GmcReserved.cc: Ditto.
    	* mc-boot/GmcSearch.cc: Ditto.
    	* mc-boot/GmcStack.cc: Ditto.
    	* mc/Indexing.mod: Ditto.
    	* mc/keyc.mod: Ditto.
    	* mc/lists.mod: Ditto.
    	* mc/mcComp.mod: Ditto.
    	* mc/mcDebug.mod: Ditto.
    	* mc/mcFileName.mod: Ditto.
    	* mc/mcMetaError.mod: Ditto.
    	* mc/mcOptions.mod: Ditto.
    	* mc/mcPreprocess.mod: Ditto.
    	* mc/mcPretty.mod: Ditto.
    	* mc/mcPrintf.mod: Ditto.
    	* mc/mcQuiet.mod: Ditto.
    	* mc/mcReserved.mod: Ditto.
    	* mc/mcSearch.mod: Ditto.
    	* mc/mcStack.mod: Ditto.
    	* tools-src/buildpg: Ditto.
    	* tools-src/calcpath: Ditto.
    	* tools-src/checkmeta.py: Ditto.
    	* tools-src/def2doc.py: Ditto.
    	* tools-src/makeSystem: Ditto.
    	* tools-src/tidydates.py: Ditto.
    
    (cherry picked from commit 78dc2e2575c602c62d50a9522ea976020a5bf7aa)
    
    Signed-off-by: Gaius Mulley <gaiusmod2@gmail.com>

--- gcc/m2/gm2-lang.h
+++ gcc/m2/gm2-lang.h
@@ -15,9 +15,9 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with GNU CC; see the file COPYING.  If not, write to
-the Free Software Foundation, 51 Franklin Street, Fifth Floor,
-Boston, MA 02110-1301, USA.  */
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
 
 #if !defined(GM2_LANG_H)
 #  define GM2_LANG_H
--- gcc/m2/tools-src/tidydates.py
+++ gcc/m2/tools-src/tidydates.py
@@ -17,9 +17,8 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with GNU Modula-2; see the file COPYING.  If not, write to the
-# Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
+# along with GCC; see the file COPYING3.  If not see
+# <http://www.gnu.org/licenses/>.
 
 import os
 import pathlib
