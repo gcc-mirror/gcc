aedb306a457496bac3b0029c6b4a1e63c738ddde
Pan Li <pan2.li@intel.com>
2024-11-29 12:01:33 +0800

    RISC-V: Fix RVV strided load/store testcases failure
    This patch would like to fix the testcases failures of strided
    load/store after sorts of optimization option passing to testcase.
    
    * Add no strict align for vector option.
    * Adjust dg-final by any-opts and/or no-opts if the rtl dump changes
      on different optimization options (like O2, O3, zvl).
    
    The below test suites are passed for this patch.
    * The rv64gcv fully regression test.
    
    It is test only patch and obvious up to a point, will commit it
    directly if no comments in next 48H.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/strided/strided_ld_st-1-f16.c: Fix
    	the failed test by target any-opts and/or no-opts.
    	* gcc.target/riscv/rvv/autovec/strided/strided_ld_st-1-f32.c: Ditto
    	* gcc.target/riscv/rvv/autovec/strided/strided_ld_st-1-f64.c: Ditto
    	* gcc.target/riscv/rvv/autovec/strided/strided_ld_st-1-i16.c: Ditto
    	* gcc.target/riscv/rvv/autovec/strided/strided_ld_st-1-i32.c: Ditto
    	* gcc.target/riscv/rvv/autovec/strided/strided_ld_st-1-i64.c: Ditto
    	* gcc.target/riscv/rvv/autovec/strided/strided_ld_st-1-i8.c: Ditto
    	* gcc.target/riscv/rvv/autovec/strided/strided_ld_st-1-u16.c: Ditto
    	* gcc.target/riscv/rvv/autovec/strided/strided_ld_st-1-u32.c: Ditto
    	* gcc.target/riscv/rvv/autovec/strided/strided_ld_st-1-u64.c: Ditto
    	* gcc.target/riscv/rvv/autovec/strided/strided_ld_st-1-u8.c: Ditto
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

--- gcc/testsuite/gcc.target/riscv/rvv/autovec/strided/strided_ld_st-1-i32.c
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/strided/strided_ld_st-1-i32.c
@@ -1,13 +1,45 @@
 /* { dg-do compile } */
-/* { dg-options "-march=rv64gcv -mabi=lp64d -fno-vect-cost-model -fdump-rtl-expand-details" } */
+/* { dg-options "-march=rv64gcv -mabi=lp64d -mno-vector-strict-align -fno-vect-cost-model -fdump-rtl-expand-details" } */
 
 #include "strided_ld_st.h"
 
 DEF_STRIDED_LD_ST_FORM_1(int32_t)
 
-/* { dg-final { scan-rtl-dump-times ".MASK_LEN_STRIDED_LOAD " 4 "expand" { target { any-opts "-O3" } } } } */
-/* { dg-final { scan-rtl-dump-times ".MASK_LEN_STRIDED_STORE " 4 "expand" { target { any-opts "-O3" } } } } */
-/* { dg-final { scan-rtl-dump-times ".MASK_LEN_STRIDED_LOAD " 2 "expand" { target { any-opts "-O2" } } } } */
-/* { dg-final { scan-rtl-dump-times ".MASK_LEN_STRIDED_STORE " 2 "expand" { target { any-opts "-O2" } } } } */
-/* { dg-final { scan-assembler-times {vlse32.v} 1 } } */
-/* { dg-final { scan-assembler-times {vsse32.v} 1 } } */
+/* { dg-final { scan-rtl-dump-times ".MASK_LEN_STRIDED_LOAD " 4 "expand" { target {
+     any-opts "-O3"
+     no-opts "-mrvv-vector-bits=zvl"
+   } } } } */
+/* { dg-final { scan-rtl-dump-times ".MASK_LEN_STRIDED_STORE " 4 "expand" { target {
+     any-opts "-O3"
+     no-opts "-mrvv-vector-bits=zvl"
+   } } } } */
+
+/* { dg-final { scan-rtl-dump-times ".MASK_LEN_STRIDED_LOAD " 8 "expand" { target {
+     any-opts "-O3" "-mrvv-vector-bits=zvl"
+   } } } } */
+/* { dg-final { scan-rtl-dump-times ".MASK_LEN_STRIDED_STORE " 8 "expand" { target {
+     any-opts "-O3" "-mrvv-vector-bits=zvl"
+   } } } } */
+
+/* { dg-final { scan-rtl-dump-times ".MASK_LEN_STRIDED_LOAD " 2 "expand" { target {
+     any-opts "-O2"
+     no-opts "-mrvv-vector-bits=zvl"
+   } } } } */
+/* { dg-final { scan-rtl-dump-times ".MASK_LEN_STRIDED_STORE " 2 "expand" { target {
+     any-opts "-O2"
+     no-opts "-mrvv-vector-bits=zvl"
+   } } } } */
+
+/* { dg-final { scan-assembler-times {vlse32.v} 1 { target {
+     no-opts "-mrvv-vector-bits=zvl"
+   } } } } */
+/* { dg-final { scan-assembler-times {vsse32.v} 1 { target {
+     no-opts "-mrvv-vector-bits=zvl"
+   } } } } */
+
+/* { dg-final { scan-assembler-times {vlse32.v} 2 { target {
+     any-opts "-mrvv-vector-bits=zvl"
+   } } } } */
+/* { dg-final { scan-assembler-times {vsse32.v} 2 { target {
+     any-opts "-mrvv-vector-bits=zvl"
+   } } } } */
