d361be35d2cf8587bebb394a2e6d878940841703
Iain Sandoe <iain@sandoe.co.uk>
2024-12-04 20:07:03 +0000

    c++, contracts: Use contract scope for each contract condition.
    This just wraps each contract condition check in a new scope (as
    required by P2900).  I've actually just let it happen for c++2a
    contracts as well, since we are going to deprecate them (and it
    apparently does not alter their behaviour).
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -1954,6 +1954,8 @@ struct GTY(()) saved_scope {
   int x_processing_contract_condition;
   int x_processing_contract_postcondition;
   int suppress_location_wrappers;
+  BOOL_BITFIELD x_processing_postcondition : 1;
+  BOOL_BITFIELD x_should_constify_contract : 1;
   BOOL_BITFIELD x_processing_explicit_instantiation : 1;
   BOOL_BITFIELD need_pop_function_context : 1;
   BOOL_BITFIELD x_processing_omp_trait_property_expr : 1;
@@ -2037,6 +2039,9 @@ extern GTY(()) struct saved_scope *scope_chain;
 #define processing_contract_condition scope_chain->x_processing_contract_condition
 
 #define processing_contract_postcondition scope_chain->x_processing_contract_postcondition
+
+#define processing_postcondition scope_chain->x_processing_postcondition
+#define should_constify_contract scope_chain->x_should_constify_contract
 
 #define in_discarded_stmt scope_chain->discarded_stmt
 #define in_consteval_if_p scope_chain->consteval_if_p
