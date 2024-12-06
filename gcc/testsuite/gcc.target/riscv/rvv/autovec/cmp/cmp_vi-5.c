6205144d6b566f32a58c4fa4f5e7e2343ad07c62
Pan Li <pan2.li@intel.com>
2024-12-06 12:22:53 +0800

    RISC-V: Refactor the testcases for rvv binop and cmp
    This patch would like to refactor the testcases for rvv binop
    and cmp after sorts of optimization option passing to testcase.
    To fits different optimization option asm dump checks.
    
    The below test suites are passed for this patch.
    * The rv64gcv fully regression test.
    
    It is test only patch and obvious up to a point, will commit it
    directly if no comments in next 48H.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/binop/vcompress-avlprop-1.c: Skip
    	m8 as it has different body layout.
    	* gcc.target/riscv/rvv/autovec/cmp/cmp_vi-1.c: Add build option
    	condition when check asm dumps.
    	* gcc.target/riscv/rvv/autovec/cmp/cmp_vi-2.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/cmp/cmp_vi-5.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/cmp/cmp_vi-6.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/cmp/cmp_vi-9.c: Ditto.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

--- gcc/testsuite/gcc.target/riscv/rvv/autovec/cmp/cmp_vi-5.c
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/cmp/cmp_vi-5.c
@@ -12,5 +12,16 @@ CMP_VI (eq_unsigned_short, unsigned short, n, ==, 15)
 CMP_VI (eq_unsigned_int, unsigned int, n, ==, 15)
 CMP_VI (eq_unsigned_long, unsigned long, n, ==, 15)
 
-/* { dg-final { scan-assembler-times {vmseq\.vi} 16 } } */
+/* { dg-final { scan-assembler-times {vmseq\.vi} 16 { target {
+     any-opts "-mrvv-max-lmul=m1"
+   } } } } */
+
+/* { dg-final { scan-assembler-times {vmseq\.vi} 10 { target {
+     any-opts "-mrvv-max-lmul=m2"
+   } } } } */
+
+/* { dg-final { scan-assembler-times {vmseq\.vi} 8 { target {
+     any-opts "-mrvv-max-lmul=m4" "-mrvv-max-lmul=m8" "-mrvv-max-lmul=dynamic"
+   } } } } */
+
 /* { dg-final { scan-assembler-not {vmseq\.vv} } } */
