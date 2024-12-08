e3c378aabb4120cd78f9a2b4cfddca2a1f71b7f5
Pan Li <pan2.li@intel.com>
2024-12-08 09:32:28 +0800

    RISC-V: Refine signed SAT_ADD testcase dump check to tree optimized
    The sat alu related testcase check the rtl dump for the standard name
    like .SAT_ADD exist or not.  But the rtl pass expand is somehow
    impressionable by the middle-end change or debug information.  Like
    below new appearance recently.
    
    Replacing Expressions
    _5 replace with --> _5 = .SAT_ADD (x_3(D), y_4(D)); [tail call]
    
    After that we need to adjust the dump check time and again.  This
    patch would like to switch to tree optimized pass for the standard
    name check, which is more stable up to a point.
    
    The below test suites are passed for this patch.
    * The rv64gcv fully regression test.
    
    It is test only patch and obvious up to a point, will commit it
    directly if no comments in next 48H.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/sat/sat_s_add-1-i16.c: Take tree-optimized
    	pass for standard name check, and adjust the times.
    	* gcc.target/riscv/sat/sat_s_add-1-i32.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-1-i64.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-1-i8.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-2-i16.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-2-i32.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-2-i64.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-2-i8.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-3-i16.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-3-i32.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-3-i64.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-3-i8.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-4-i16.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-4-i32.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-4-i64.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add-4-i8.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add_imm-1-1.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add_imm-1.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add_imm-2-1.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add_imm-2.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add_imm-3-1.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add_imm-3.c: Ditto.
    	* gcc.target/riscv/sat/sat_s_add_imm-4.c: Ditto.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

--- gcc/testsuite/gcc.target/riscv/sat/sat_s_add_imm-2-1.c
+++ gcc/testsuite/gcc.target/riscv/sat/sat_s_add_imm-2-1.c
@@ -1,9 +1,9 @@
 /* { dg-do compile } */
-/* { dg-options "-march=rv64gc -mabi=lp64d -fdump-rtl-expand-details" } */
+/* { dg-options "-march=rv64gc -mabi=lp64d -fdump-tree-optimized" } */
 
 #include "sat_arith.h"
 
 DEF_SAT_S_ADD_IMM_FMT_1(0, int16_t, uint16_t, -32769, INT16_MIN, INT16_MAX)
 DEF_SAT_S_ADD_IMM_FMT_1(1, int16_t, uint16_t, 32768, INT16_MIN, INT16_MAX)
 
-/* { dg-final { scan-rtl-dump-not ".SAT_ADD " "expand" } } */
+/* { dg-final { scan-tree-dump-not ".SAT_ADD " "optimized" } } */
