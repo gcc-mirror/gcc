3537aa694d7d817ac13c3c2908bda45adfb95511
Mariam Arutunian <mariamarutunian@gmail.com>
2024-12-09 07:29:36 -0700

    [committed] RISC-V testsuite changes to test clmul expansion of CRCs
    This testsuite only patch allows us to test code generation for CRC functions
    using clmul instructions.
    
    Conceptually it's trivial.  We already have various execution tests in
    gcc.dg/torture.  We just define a new set of dg directives and include the
    testcase in gcc.dg/torture.
    
    The only gotcha in here is the need to change target-supports.exp.  It was
    passing the default set of arguments down to the check_runtime routine, so they
    always failed to assemble the testcase and we never claimed the ability to
    execute Zbc, Zbkb or Zbkc extension code.
    
    Again, NFC, just testsuite bits.  Pushing to the trunk.
    
    Only aarch64 and x86 bits left ;-)
    
    gcc/testsuite
    	* gcc.target/riscv/crc-1-zbc.c: New test.
    	* gcc.target/riscv/crc-1-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-10-zbc.c: Likewise.
    	* gcc.target/riscv/crc-10-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-12-zbc.c: Likewise.
    	* gcc.target/riscv/crc-12-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-13-zbc.c: Likewise.
    	* gcc.target/riscv/crc-13-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-14-zbc.c: Likewise.
    	* gcc.target/riscv/crc-14-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-17-zbc.c: Likewise.
    	* gcc.target/riscv/crc-17-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-18-zbc.c: Likewise.
    	* gcc.target/riscv/crc-18-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-21-rv64-zbc.c: Likewise.
    	* gcc.target/riscv/crc-21-rv64-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-22-zbc.c: Likewise.
    	* gcc.target/riscv/crc-22-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-23-zbc.c: Likewise.
    	* gcc.target/riscv/crc-23-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-4-zbc.c: Likewise.
    	* gcc.target/riscv/crc-4-zbkb.c: Likewise.
    	* gcc.target/riscv/crc-4-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-5-zbc.c: Likewise.
    	* gcc.target/riscv/crc-5-zbkb.c: Likewise.
    	* gcc.target/riscv/crc-5-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-6-zbc.c: Likewise.
    	* gcc.target/riscv/crc-6-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-7-zbc.c: Likewise.
    	* gcc.target/riscv/crc-7-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-8-zbc.c: Likewise.
    	* gcc.target/riscv/crc-8-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-9-zbc.c: Likewise.
    	* gcc.target/riscv/crc-9-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-CCIT-data16-zbc.c: Likewise.
    	* gcc.target/riscv/crc-CCIT-data16-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-CCIT-data8-zbc.c: Likewise.
    	* gcc.target/riscv/crc-CCIT-data8-zbkc.c: Likewise.
    	* gcc.target/riscv/crc-coremark-16bitdata-zbc.c: Likewise.
    	* gcc.target/riscv/crc-coremark-16bitdata-zbkc.c: Likewise.
    	* lib/target-supports.exp (check_effective_target_riscv_zbc_ok): Set
    	gcc_march before compiling test program.
    	(check_effective_target_riscv_zbkc_ok): Likewise.
    	(check_effective_target_riscv_zbkb_ok): Likewise.
    	Co-authored-by: Jeff Law <jlaw@ventanamicro.com>

---
+++ gcc/testsuite/gcc.target/riscv/crc-1-zbkc.c
@@ -0,0 +1,11 @@
+/* { dg-do run { target riscv_zbkc_ok } } */
+/* { dg-options "-march=rv64gc_zbkc -fdump-tree-crc -fdump-rtl-dfinish  -fdisable-tree-phiopt2 -fdisable-tree-phiopt3" { target { rv64 } } } */
+/* { dg-options "-march=rv32gc_zbkc -fdump-tree-crc -fdump-rtl-dfinish  -fdisable-tree-phiopt2 -fdisable-tree-phiopt3" { target { rv32 } } } */
+
+/* { dg-skip-if "" { *-*-* } { "-O0" "-O1" "-Og" } } */
+
+#include "../../gcc.dg/torture/crc-1.c"
+
+/* { dg-final { scan-tree-dump "calculates CRC!" "crc"} } */
+/* { dg-final { scan-tree-dump-times "Couldn't generate faster CRC code." 0 "crc"} } */
+/* { dg-final { scan-rtl-dump "clmul" "dfinish"} } */
---
+++ gcc/testsuite/gcc.target/riscv/crc-21-rv64-zbc.c
@@ -0,0 +1,10 @@
+/* { dg-do run { target { riscv64*-*-* && riscv_zbc_ok } } } */
+/* { dg-options "-march=rv64gc_zbc -fdump-tree-crc -fdump-rtl-dfinish" { target { rv64 } } } */
+/* { dg-options "-march=rv32gc_zbc -fdump-tree-crc -fdump-rtl-dfinish" { target { rv32 } } } */
+/* { dg-skip-if "" { *-*-* } { "-O0" "-O1" "-Og" } } */
+
+#include "../../gcc.dg/torture/crc-21.c"
+
+/* { dg-final { scan-tree-dump "calculates CRC!" "crc"} } */
+/* { dg-final { scan-tree-dump-times "Couldn't generate faster CRC code." 0 "crc"} } */
+/* { dg-final { scan-rtl-dump "clmul" "dfinish"} } */
