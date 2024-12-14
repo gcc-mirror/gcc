6866547e2421cc40606c13c2c3621a3d9121e893
Jose E. Marchesi <jose.marchesi@oracle.com>
2024-12-14 19:15:34 +0100

    bpf: fix build adding new required arg to RESOLVE_OVERLOADED_BUILTIN
    gcc/ChangeLog
    
    	* config/bpf/bpf.cc (bpf_resolve_overloaded_builtin): Add argument
    	`complain'.

--- gcc/config/bpf/bpf.cc
+++ gcc/config/bpf/bpf.cc
@@ -1084,7 +1084,8 @@ bpf_expand_builtin (tree exp, rtx target ATTRIBUTE_UNUSED,
 #define TARGET_EXPAND_BUILTIN bpf_expand_builtin
 
 static tree
-bpf_resolve_overloaded_builtin (location_t loc, tree fndecl, void *arglist)
+bpf_resolve_overloaded_builtin (location_t loc, tree fndecl, void *arglist,
+				bool complain ATTRIBUTE_UNUSED)
 {
   int code = DECL_MD_FUNCTION_CODE (fndecl);
   if (code > BPF_CORE_BUILTINS_MARKER)
