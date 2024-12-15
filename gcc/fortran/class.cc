1572e634dec4a09593f68645939b5b5043de8de6
Paul Thomas <pault@gcc.gnu.org>
2024-12-15 14:48:59 +0000

    Fortran: Fix non_overridable typebound proc problems [PR84674/117730].
    2024-12-15  Paul Thomas  <pault@gcc.gnu.org>
    
    gcc/fortran/ChangeLog
    
    	PR fortran/117730
    	PR fortran/84674
    	* class.cc (add_proc_comp): If the present typebound procedure
    	component is abstract, unconditionally check the replacement.
    	Only reject a non_overridable if it has no overridden procedure
    	and the component is already present in the vtype.
    
    gcc/testsuite/ChangeLog
    
    	PR fortran/117730
    	* gfortran.dg/pr117730_a.f90: New test.
    	* gfortran.dg/pr117730_b.f90: New test.
    
    	PR fortran/84674
    	* gfortran.dg/pr84674.f90: New test.

--- gcc/fortran/class.cc
+++ gcc/fortran/class.cc
@@ -872,11 +872,22 @@ static void
 add_proc_comp (gfc_symbol *vtype, const char *name, gfc_typebound_proc *tb)
 {
   gfc_component *c;
-
-  if (tb->non_overridable && !tb->overridden)
-    return;
+  bool is_abstract = false;
 
   c = gfc_find_component (vtype, name, true, true, NULL);
+
+  /* If the present component typebound proc is abstract, the new version
+     should unconditionally be tested if it is a suitable replacement.  */
+  if (c && c->tb && c->tb->u.specific
+      && c->tb->u.specific->n.sym->attr.abstract)
+    is_abstract = true;
+
+  /* Pass on the new tb being not overridable if a component is found and
+     either there is not an overridden specific or the present component
+     tb is abstract. This ensures that possible, viable replacements are
+     loaded.  */
+  if (tb->non_overridable && !tb->overridden && !is_abstract && c)
+    return;
 
   if (c == NULL)
     {
