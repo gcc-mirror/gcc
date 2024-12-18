e2f6ed54f75bbf8dd0292af90304890f06a9be17
Prathamesh Kulkarni <prathameshk@nvidia.com>
2024-12-18 17:03:51 +0530

    PR81358: Enable automatic linking of libatomic.
    ChangeLog:
    	PR driver/81358
    	* Makefile.def: Add dependencies so libatomic is built before target
    	libraries are configured.
    	* Makefile.tpl: Export TARGET_CONFIGDIRS.
    	* configure.ac: Add libatomic to bootstrap_target_libs.
    	* Makefile.in: Regenerate.
    	* configure: Regenerate.
    
    gcc/ChangeLog:
    	PR driver/81358
    	* common.opt: New option -flink-libatomic.
    	* gcc.cc (LINK_LIBATOMIC_SPEC): New macro.
    	* config/gnu-user.h (GNU_USER_TARGET_LINK_GCC_C_SEQUENCE_SPEC): Use
    	LINK_LIBATOMIC_SPEC.
    	* doc/invoke.texi: Document -flink-libatomic.
    	* configure.ac: Define TARGET_PROVIDES_LIBATOMIC.
    	* configure: Regenerate.
    	* config.in: Regenerate.
    
    libatomic/ChangeLog:
    	PR driver/81358
    	* Makefile.am: Pass -fno-link-libatomic.
    	New rule all.
    	* configure.ac: Assert that CFLAGS is set and pass -fno-link-libatomic.
    	* Makefile.in: Regenerate.
    	* configure: Regenerate.
    
    Signed-off-by: Prathamesh Kulkarni <prathameshk@nvidia.com>
    Co-authored-by: Matthew Malcolmson <mmalcolmson@nvidia.com>

--- gcc/gcc.cc
+++ gcc/gcc.cc
@@ -980,6 +980,13 @@ proper position among the other output files.  */
 
 /* Here is the spec for running the linker, after compiling all files.  */
 
+#if defined(TARGET_PROVIDES_LIBATOMIC) && defined(USE_LD_AS_NEEDED)
+#define LINK_LIBATOMIC_SPEC "%{!fno-link-libatomic:" LD_AS_NEEDED_OPTION \
+			    " -latomic " LD_NO_AS_NEEDED_OPTION "} "
+#else
+#define LINK_LIBATOMIC_SPEC ""
+#endif
+
 /* This is overridable by the target in case they need to specify the
    -lgcc and -lc order specially, yet not require them to override all
    of LINK_COMMAND_SPEC.  */
