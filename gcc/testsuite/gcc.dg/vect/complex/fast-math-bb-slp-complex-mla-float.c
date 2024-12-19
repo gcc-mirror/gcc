7cc573017274acfd5a276d959a8297ba04e98002
Christophe Lyon <christophe.lyon@linaro.org>
2024-12-19 10:07:28 +0000

    arm, testsuite: fix fast-math-bb-slp-complex-mla-float.c dg-add-options
    The test uses floats, not fp16 so it should use arm_v8_3a_complex_neon
    instead of arm_v8_3a_fp16_complex_neon.
    
    This makes it PASS on arm-linux-gnueabihf instead of being UNRESOLVED.
    
    	gcc/testsuite/ChangeLog:
    	* gcc.dg/vect/complex/fast-math-bb-slp-complex-mla-float.c: Use
    	arm_v8_3a_complex_neon.

--- gcc/testsuite/gcc.dg/vect/complex/fast-math-bb-slp-complex-mla-float.c
+++ gcc/testsuite/gcc.dg/vect/complex/fast-math-bb-slp-complex-mla-float.c
@@ -1,7 +1,7 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target vect_complex_add_float } */
 /* { dg-additional-options "-ffast-math -fdump-tree-vect-details" } */
-/* { dg-add-options arm_v8_3a_fp16_complex_neon } */
+/* { dg-add-options arm_v8_3a_complex_neon } */
 
 #define TYPE float
 #define N 16
