7c50564627e42c619cc64c09cce8a42fd7932166
Christoph Müllner <christoph.muellner@vrull.eu>
2024-12-20 14:46:51 +0100

    testsuite: Add tests for PR118149
    A recent bugfix (eee2891312) for PR117830 also addressed PR118149.
    This patch adds two test cases for PR118149.
    These tests are different than other tests in that one of the
    vec-perm selectors contains indices in descending order (1, 1, 0, 0),
    which is the root cause for the ICE observed in PR118149.
    
    	PR118149
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/tree-ssa/pr118149-2.c: New test.
    	* gcc.dg/tree-ssa/pr118149.c: New test.
    
    Signed-off-by: Christoph Müllner <christoph.muellner@vrull.eu>

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr118149.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-options "-O3 -fdump-tree-forwprop4-details -Wno-psabi" } */
+/* { dg-additional-options "-msse2" { target i?86-*-* x86_64-*-* } } */
+
+float *fastconv_parse_dst;
+
+void fastconv_parse ()
+{
+  float r3k = fastconv_parse_dst[1] - fastconv_parse_dst[3],
+        i0k = fastconv_parse_dst[4] + fastconv_parse_dst[6],
+        i1k = fastconv_parse_dst[4] - fastconv_parse_dst[6],
+        i2k = fastconv_parse_dst[5] + fastconv_parse_dst[7];
+  fastconv_parse_dst[1] = fastconv_parse_dst[0];
+  fastconv_parse_dst[4] = fastconv_parse_dst[5] = i0k - i2k;
+  fastconv_parse_dst[6] = fastconv_parse_dst[7] = i1k + r3k;
+}
+
+/* { dg-final { scan-tree-dump "Vec perm simplify sequences have been blended" "forwprop4" { target { aarch64*-*-* i?86-*-* x86_64-*-* } } } } */
+/* { dg-final { scan-tree-dump "VEC_PERM_EXPR.*{ 0, 0, 6, 6 }" "forwprop4" { target { aarch64*-*-* i?86-*-* x86_64-*-* } } } } */
+/* { dg-final { scan-tree-dump "VEC_PERM_EXPR.*{ 1, 1, 7, 7 }" "forwprop4" { target { aarch64*-*-* i?86-*-* x86_64-*-* } } } } */
