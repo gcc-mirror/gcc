681934aead9c1310e3b910d18986b76272168a80
Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>
2024-12-22 16:19:17 +0100

    testsuite: arm: Use -Os in memset-inline-8* tests
    When the test was initially created, -fcommon was the default, but in
    commit r10-4867-g6271dd984d7 the default value changed to -fno-common.
    This change made the test start failing. To counter the over-alignment
    caused by 'a' no longer being common, use -Os.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/arm/memset-inline-8.c: Use -Os and prefix assembler
    	instructions with a tab to improve test stability.
    	* gcc.target/arm/memset-inline-8-exe.c: Use -Os.
    
    Signed-off-by: Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>

--- gcc/testsuite/gcc.target/arm/memset-inline-8-exe.c
+++ gcc/testsuite/gcc.target/arm/memset-inline-8-exe.c
@@ -1,7 +1,7 @@
 /* { dg-do run } */
 /* { dg-skip-if "Don't inline memset using neon instructions" { ! arm_tune_string_ops_prefer_neon } } */
 /* { dg-require-effective-target arm_neon_hw } */
-/* { dg-options "-save-temps -O2 -fno-inline"  } */
+/* { dg-options "-save-temps -Os -fno-inline"  } */
 /* { dg-add-options "arm_neon" } */
 
 #include "./memset-inline-8.c"
