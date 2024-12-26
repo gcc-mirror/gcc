533abf7e53c6edb699d67418a84b662bd41cf381
Arthur Cohen <arthur.cohen@embecosm.com>
2024-12-26 10:57:07 +0000

    derive(Clone): Mark PhantomData as a lang item
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/derive_macro4.rs: Make PhantomData a lang item.

--- gcc/testsuite/rust/compile/derive_macro4.rs
+++ gcc/testsuite/rust/compile/derive_macro4.rs
@@ -6,11 +6,8 @@ pub trait Clone {
     fn clone(&self) -> Self;
 }
 
+#[lang = "phantom_data"]
 struct PhantomData<T>;
-
-pub struct AssertParamIsCopy<T: Copy> {
-    _field: PhantomData<T>,
-}
 
 #[derive(Clone)] // { dg-error "bounds not satisfied for U .Copy. is not satisfied" }
 union U {
