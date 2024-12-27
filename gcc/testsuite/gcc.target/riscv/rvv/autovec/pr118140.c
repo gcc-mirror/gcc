14cb0610559fa33f211e1546260458496fdc5e71
Robin Dapp <rdapp@ventanamicro.com>
2024-12-27 17:29:25 +0100

    match: Keep conditional in simplification to constant [PR118140].
    In PR118140 we simplify
    
      _ifc__33 = .COND_IOR (_41, d_lsm.7_11, _46, d_lsm.7_11);
    
    to 1:
    
    Match-and-simplified .COND_IOR (_41, d_lsm.7_11, _46, d_lsm.7_11) to 1
    
    when _46 == 1.  This happens by removing the conditional and applying
    a | 1 = 1.  Normally we re-introduce the conditional and its else value
    if needed but that does not happen here as we're not dealing with a
    vector type.  For correctness's sake, we must not remove the conditional
    even for non-vector types.
    
    This patch re-introduces a COND_EXPR in such cases.  For PR118140 this
    result in a non-vectorized loop.
    
    	PR middle-end/118140
    
    gcc/ChangeLog:
    
    	* gimple-match-exports.cc (maybe_resimplify_conditional_op): Add
    	COND_EXPR when we simplified to a scalar gimple value but still
    	have an else value.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/vect/pr118140.c: New test.
    	* gcc.target/riscv/rvv/autovec/pr118140.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/pr118140.c
@@ -0,0 +1,29 @@
+/* { dg-do run } */
+/* { dg-require-effective-target riscv_v_ok } */
+/* { dg-add-options riscv_v } */
+/* { dg-additional-options "-std=gnu99 -Wno-pedantic" } */
+
+long long a;
+_Bool d;
+char e;
+_Bool f[17];
+_Bool f_3;
+
+int main() {
+  for (char g = 3; g < 16; g++) {
+      d |= ({
+        int h = f[g - 1] ? 2 : 0;
+        _Bool t;
+        if (f[g - 1])
+          t = f_3;
+        else
+          t = 0;
+        int i = t;
+        h > i;
+      });
+    e += f[g + 1];
+  }
+
+  if (d != 0)
+    __builtin_abort ();
+}
