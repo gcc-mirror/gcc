31f1bec65ba257adc346f952fd79a1ec3165a2e6
Estevan Castilho (Tevo) <estevan.cps@gmail.com>
2024-12-28 20:37:37 +0000

    Ada: Fix build for dummy s-taprop
    gcc/ada
    	* libgnarl/s-taprop__dummy.adb: Remove use clause for
    	System.Parameters.
    	(Unlock): Remove Global_Lock formal parameter.
    	(Write_Lock): Likewise.

--- gcc/ada/libgnarl/s-taprop__dummy.adb
+++ gcc/ada/libgnarl/s-taprop__dummy.adb
@@ -37,7 +37,6 @@
 package body System.Task_Primitives.Operations is
 
    use System.OS_Locks;
-   use System.Parameters;
    use System.Tasking;
 
    pragma Warnings (Off);
@@ -483,10 +482,7 @@ package body System.Task_Primitives.Operations is
       null;
    end Unlock;
 
-   procedure Unlock
-     (L           : not null access RTS_Lock;
-      Global_Lock : Boolean := False)
-   is
+   procedure Unlock (L : not null access RTS_Lock) is
    begin
       null;
    end Unlock;
@@ -525,10 +521,7 @@ package body System.Task_Primitives.Operations is
       Ceiling_Violation := False;
    end Write_Lock;
 
-   procedure Write_Lock
-     (L           : not null access RTS_Lock;
-      Global_Lock : Boolean := False)
-   is
+   procedure Write_Lock (L : not null access RTS_Lock) is
    begin
       null;
    end Write_Lock;
