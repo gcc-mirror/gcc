d369ddca549b5ff7d868b8f5ee139835b1f9382a
Jeff Law <jlaw@ventanamicro.com>
2024-12-30 23:40:58 -0700

    [RISC-V][PR target/115375] Fix expected dump output
    Several months ago changes were made to the vectorizer which mucked up several
    of the scan tests.  All but one of the cases in pr115375 have since been fixed.
    
    The remaining failure seems to be primarily a debugging dump issue -- we're
    still selecting the same lmul values.  This patch adjusts the dump scan
    appropriately.
    
    	PR target/115375
    
    gcc/testsuite
    
    	* gcc.dg/vect/costmodel/riscv/rvv/dynamic-lmul8-9.c: Adjust expected
    	output.

--- gcc/testsuite/gcc.dg/vect/costmodel/riscv/rvv/dynamic-lmul8-9.c
+++ gcc/testsuite/gcc.dg/vect/costmodel/riscv/rvv/dynamic-lmul8-9.c
@@ -14,6 +14,6 @@ foo (int64_t *__restrict a, int64_t init, int n)
 /* { dg-final { scan-assembler {e64,m8} } } */
 /* { dg-final { scan-assembler-not {csrr} } } */
 /* { dg-final { scan-tree-dump-not "Preferring smaller LMUL loop because it has unexpected spills" "vect" } } */
-/* { dg-final { scan-tree-dump-times "Maximum lmul = 8" 2 "vect" } } */
+/* { dg-final { scan-tree-dump-times "Maximum lmul = 8" 1 "vect" } } */
 /* { dg-final { scan-tree-dump-times "Maximum lmul = 4" 1 "vect" } } */
 /* { dg-final { scan-tree-dump-times "Maximum lmul = 2" 1 "vect" } } */
