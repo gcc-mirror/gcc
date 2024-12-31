f8b559626025d2466c2780af6b62560dda468647
Richard Biener <rguenther@suse.de>
2024-12-31 14:47:03 +0100

    middle-end/118174 - bogus TER of tailcall
    The following avoids applying TER to direct internal functions that
    are tailcall since the involved expansion code path doesn't honor
    TER constraints.
    
    	PR middle-end/118174
    	* tree-outof-ssa.cc (ssa_is_replaceable_p): Exclude tailcalls.
    
    	* gcc.dg/torture/pr118174.c: New testcase.

--- gcc/tree-outof-ssa.cc
+++ gcc/tree-outof-ssa.cc
@@ -61,11 +61,14 @@ ssa_is_replaceable_p (gimple *stmt)
   tree def;
   gimple *use_stmt;
 
-  /* Only consider modify stmts and direct internal fn calls.  */
+  /* Only consider modify stmts and direct internal fn calls that are
+     not also tail-calls.  */
+  gcall *call;
   if (!is_gimple_assign (stmt)
-      && (!is_gimple_call (stmt)
-	  || !gimple_call_internal_p (stmt)
-	  || !direct_internal_fn_p (gimple_call_internal_fn (stmt))))
+      && (!(call = dyn_cast <gcall *> (stmt))
+	  || gimple_call_tail_p (call)
+	  || !gimple_call_internal_p (call)
+	  || !direct_internal_fn_p (gimple_call_internal_fn (call))))
     return false;
 
   /* If the statement may throw an exception, it cannot be replaced.  */
