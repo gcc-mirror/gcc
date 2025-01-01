7fd30073101a94bcbe29bab6ca45b05ab74542aa
Om Swaroop Nayak <96killerat96@gmail.com>
2025-01-01 09:02:02 -0800

    gccrs: gcc/rust/ChangeLog:
    	* ast/rust-collect-lang-items.cc (get_lang_item_attr): "removed checker fn"
    	* util/rust-attributes.cc (Attributes::is_lang_item): "added fn"
    	* util/rust-attributes.h: "added fn"
    
    Signed-off-by: Om Swaroop Nayak <96killerat96@gmail.com>

--- gcc/rust/util/rust-attributes.h
+++ gcc/rust/util/rust-attributes.h
@@ -29,6 +29,8 @@ class Attributes
 {
 public:
   static bool is_known (const std::string &attribute_path);
+  static bool is_lang_item (const std::string &attribute_path,
+			    const AST::Attribute &attr);
 };
 
 enum CompilerPass
