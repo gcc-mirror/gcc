a856b4d97b8d328fdcb169b792ac5456e40f8c00
Jeff Law <jlaw@ventanamicro.com>
2025-01-07 07:43:19 -0700

    [PR testsuite/118055] Trivial testsuite adjustment for m68k target
    After a bit of a prod from Hans...
    
    Make the obvious change to these tests to get them passing again on m68k.
    
    	PR testsuite/118055
    
    gcc/testsuite
    
    	* gcc.dg/tree-ssa/pr83403-1.c: Add m68k*-*-* to targets needing
    	additional arguments for peeling.
    	* gcc.dg/tree-ssa/pr83403-2.c: Similarly.

--- gcc/testsuite/gcc.dg/tree-ssa/pr83403-2.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr83403-2.c
@@ -1,7 +1,7 @@
 /* { dg-do compile } */
 /* { dg-options "-O3 -funroll-loops -fdump-tree-lim2-details" } */
 /* { dg-additional-options "--param max-completely-peeled-insns=200" { target { s390*-*-* } } } */
-/* { dg-additional-options "--param max-completely-peeled-insns=300" { target { arm*-*-* cris-*-* } } } */
+/* { dg-additional-options "--param max-completely-peeled-insns=300" { target { arm*-*-* cris-*-* m68k*-*-* } } } */
 
 #define TYPE int
 
