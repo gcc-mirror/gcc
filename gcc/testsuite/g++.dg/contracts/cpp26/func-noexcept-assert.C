ae945833a14c0ad85cae4d7b77d79465ef7dcddd
Nina Ranns <dinka.ranns@gmail.com>
2025-01-09 10:18:05 +0000

    adding more noexcept function tests, renaming existing tests

---
+++ gcc/testsuite/g++.dg/contracts/cpp26/func-noexcept-assert.C
@@ -0,0 +1,54 @@
+// Throwing violation handler in an assert check in a noexcept function
+// can be caught by the function.
+// { dg-do run }
+// { dg-options "-std=c++2a -fcontracts -fcontracts-nonattr -fcontract-evaluation-semantic=observe " }
+
+#include <experimental/contract>
+#include <exception>
+#include <cstdlib>
+
+struct MyException{};
+
+
+void handle_contract_violation(const std::experimental::contract_violation& violation)
+{
+  throw MyException{};
+}
+
+void free_f(const int x) {
+  try {
+	  contract_assert(x>1);
+	  int i = 1;
+  }
+  catch(...){}
+}
+
+struct X
+{
+    void f(const int x) {
+      try {
+	  contract_assert(x>1);
+	  int i = 1;
+      }
+      catch(...){}
+    }
+
+    virtual void virt_f(const int x) {
+      try {
+	  contract_assert(x>1);
+	  int i = 1;
+      }
+      catch(...){}
+    }
+
+};
+
+int main()
+{
+  free_f(-42);
+
+  X x;
+  x.f(-42);
+  x.virt_f(-42);
+
+}
