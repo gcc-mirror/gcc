24eb432e26fbb5f3ac45a7c642f608073387ed31
Nina Ranns <dinka.ranns@gmail.com>
2025-01-09 10:18:05 +0000

    adding more noexcept function tests, renaming existing tests

---
+++ gcc/testsuite/g++.dg/contracts/cpp26/memberfunc-noexcept-pre.C
@@ -0,0 +1,46 @@
+// Throwing violation handler in a pre/post check on a noexcept function
+// behaves as if the function exited via an exception.
+// This tests the behaviour of a pre condition on a member function
+// { dg-do run }
+// { dg-options "-std=c++2a -fcontracts -fcontracts-nonattr -fcontract-evaluation-semantic=observe " }
+
+#include <experimental/contract>
+#include <exception>
+#include <cstdlib>
+
+struct MyException{};
+
+// Test that there is an active exception when we reach the terminate handler.
+void my_term()
+{
+  try { throw; }
+  catch(MyException) { std::exit(0); }
+}
+
+
+void handle_contract_violation(const std::experimental::contract_violation& violation)
+{
+  throw MyException{};
+}
+
+struct X
+{
+    void f(int x) noexcept pre(x>1) {
+       int i = 1;
+    }
+};
+
+int main()
+{
+  std::set_terminate (my_term);
+  try
+  {
+      X x;
+      x.f(-42);
+  } catch (...) {
+  }
+
+  // We should not get here
+  return 1;
+
+}
