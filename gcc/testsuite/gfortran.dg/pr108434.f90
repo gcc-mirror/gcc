4d4c5ec93d65752a4aeda6bf2c9efe429e637969
Paul Thomas <pault@gcc.gnu.org>
2025-01-11 08:23:48 +0000

    Fortran: Fix error recovery for bad component arrayspecs [PR108434]
    2025-01-11  Paul Thomas  <pault@gcc.gnu.org>
    
    gcc/fortran/
    	PR fortran/108434
    	* class.cc (generate_finalization_wrapper): To avoid memory
    	leaks from callocs, return immediately if the derived type
    	error flag is set.
    	* decl.cc (build_struct): If the declaration of a derived type
    	or class component does not have a deferred arrayspec, correct,
    	set the error flag of the derived type and emit an immediate
    	error.
    
    gcc/testsuite/
    	PR fortran/108434
    	* gfortran.dg/pr108434.f90 : Add tests from comment 1.
    
    (cherry picked from commit d64ca15351029164bac30b49fb3c4f9723e755de)

--- gcc/testsuite/gfortran.dg/pr108434.f90
+++ gcc/testsuite/gfortran.dg/pr108434.f90
@@ -1,11 +1,19 @@
 ! { dg-do compile }
 ! PR fortran/108434 - ICE in class_allocatable
-! Contributed by G.Steinmetz
+! Contributed by G.Steinmetz  <gscfq@t-online.de>
 
 program p
   type t
      class(c), pointer :: a(2) ! { dg-error "must have a deferred shape" }
   end type t
+  type s
+     class(d), allocatable :: a(2) ! { dg-error "must have a deferred shape|not been declared" }
+  end type
+  type u
+     type(e),  allocatable :: b(2) ! { dg-error "must have a deferred shape|not been declared" }
+  end type
   class(t), allocatable :: x
   class(t), pointer     :: y
+  class(s), allocatable :: x2
+  class(s), pointer :: y2
 end
