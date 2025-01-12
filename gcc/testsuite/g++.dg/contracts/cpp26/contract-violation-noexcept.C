04bf921701f24926a061a9662e10fcdbeacece1b
Ville Voutilainen <ville.voutilainen@gmail.com>
2025-01-12 18:51:08 +0200

    Make the built-in contract violation handler noexcept

---
+++ gcc/testsuite/g++.dg/contracts/cpp26/contract-violation-noexcept.C
@@ -0,0 +1,107 @@
+// test that the default contract violation handler can't throw
+// { dg-do run }
+// { dg-options "-std=c++2a -fcontracts -fcontract-continuation-mode=on -fcontracts-nonattr " }
+
+#include <experimental/contract>
+#include <iostream>
+#include <exception>
+#include <cstdlib>
+
+// Throws on all overflow and underflow calls.
+struct underflow_error: std::exception { };
+struct overflow_error: std::exception { };
+struct positioning_error: std::exception { };
+
+template<typename T>
+struct fail_buf
+  : public T
+{
+  typedef typename T::char_type   char_type;
+  typedef typename T::int_type    int_type;
+  typedef typename T::off_type    off_type;
+  typedef typename T::pos_type    pos_type;
+
+private:
+  char_type p[2];
+
+public:
+  fail_buf()
+  {
+    p[0] = char_type('s');
+    p[1] = char_type();
+    this->setg(p, p, p + 1);
+  }
+
+  virtual int_type underflow()
+  {
+    throw underflow_error();
+    return int_type();
+  }
+
+  virtual int_type uflow()
+  {
+    throw underflow_error();
+    return int_type();
+  }
+
+  virtual int_type
+  overflow(int_type)
+  {
+    throw overflow_error();
+    return int_type();
+  }
+
+  virtual pos_type
+  seekoff(off_type, std::ios_base::seekdir, std::ios_base::openmode)
+  {
+    throw positioning_error();
+    return pos_type(off_type(-1));
+  }
+
+  virtual pos_type
+  seekpos(pos_type, std::ios_base::openmode)
+  {
+    throw positioning_error();
+    return pos_type(off_type(-1));
+  }
+
+  virtual int
+  sync()
+  {
+    throw positioning_error();
+    return 0;
+  }
+};
+
+typedef  fail_buf<std::streambuf>   fail_streambuf;
+
+
+// Test that there is an active exception when we reach the terminate handler.
+void my_term()
+{
+  try { throw; }
+  catch(...) { std::exit(0); }
+}
+
+void f(int x) noexcept pre(x >= 0)
+{
+  try{
+   int i = 1;
+  }
+  catch(...) {
+  }
+}
+
+int main()
+{
+  std::set_terminate (my_term);
+  fail_streambuf buf;
+  std::cerr.rdbuf(&buf);
+  try
+  {
+      f(-42);
+  } catch (...) {
+  }
+  // We should not get here
+  return 1;
+}
