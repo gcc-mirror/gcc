8d565a24186dc38265764eff0ed0c74418f511a7
Nina Ranns <dinka.ranns@gmail.com>
2025-01-16 11:31:49 +0000

    thunk thinko

---
+++ gcc/testsuite/g++.dg/contracts/cpp26/callerside-checks/callerside-checks-non-trivial.C
@@ -0,0 +1,18 @@
+// { dg-do compile }
+// { dg-options "-std=c++2a -fcontracts -fcontracts-nonattr -fcontract-evaluation-semantic=observe -fcontracts-nonattr-client-contracts=all" }
+
+
+struct S{
+  S(){};
+  S(const S&){}
+  ~S(){};
+  int x = 0;
+};
+
+void f(S s) pre(s.x == 1 ) {};
+
+int main()
+{
+  S s;
+  f(s);
+}
---
+++ gcc/testsuite/g++.dg/contracts/cpp26/virtual-func-nontrivial.C
@@ -0,0 +1,25 @@
+// Test that non trivial types work ok with a contract wrapper for virtual
+// functions.
+// { dg-do compile }
+// { dg-options "-std=c++2a -fcontracts -fcontract-continuation-mode=on -fcontracts-nonattr -g3" }
+
+struct NonTrivial{
+  NonTrivial(){};
+  NonTrivial(const NonTrivial&){}
+  ~NonTrivial(){};
+  int x = 0;
+};
+
+struct S
+{
+
+  virtual void f(const NonTrivial s) post(s.x >1 ) {};
+
+};
+
+int main()
+{
+  NonTrivial nt;
+  S s;
+  s.f(nt);
+}
