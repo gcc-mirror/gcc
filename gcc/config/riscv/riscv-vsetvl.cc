b9493e98da58c7689645b4ee1a2f653b86a5d758
Jin Ma <jinma@linux.alibaba.com>
2025-01-18 07:43:17 -0700

    [PR target/118357] RISC-V: Disable fusing vsetvl instructions by VSETVL_VTYPE_CHANGE_ONLY for XTheadVector.
    In RVV 1.0, the instruction "vsetvli	zero,zero,*" indicates that the
    available vector length (avl) does not change. However, in XTheadVector,
    this same instruction signifies that the avl should take the maximum value.
    Consequently, when fusing vsetvl instructions, the optimization labeled
    "VSETVL_VTYPE_CHANGE_ONLY" is disabled for XTheadVector.
    
    	PR target/118357
    
    gcc/ChangeLog:
    
    	* config/riscv/riscv-vsetvl.cc: Function change_vtype_only_p always
    	returns false for XTheadVector.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/xtheadvector/pr118357.c: New test.

--- gcc/config/riscv/riscv-vsetvl.cc
+++ gcc/config/riscv/riscv-vsetvl.cc
@@ -903,7 +903,8 @@ public:
   bool valid_p () const { return m_state == state_type::VALID; }
   bool unknown_p () const { return m_state == state_type::UNKNOWN; }
   bool empty_p () const { return m_state == state_type::EMPTY; }
-  bool change_vtype_only_p () const { return m_change_vtype_only; }
+  bool change_vtype_only_p () const { return m_change_vtype_only
+					     && !TARGET_XTHEADVECTOR; }
 
   void set_valid () { m_state = state_type::VALID; }
   void set_unknown () { m_state = state_type::UNKNOWN; }
