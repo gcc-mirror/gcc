86a1acfd86f881c32e0ae57036df65edd7d1d441
Stefan Schulze Frielinghaus <stefansf@gcc.gnu.org>
2025-01-20 10:01:09 +0100

    s390: arch15: Load indexed address
    Add instructions lxa and llxa.
    
    gcc/ChangeLog:
    
    	* config/s390/s390.md (*lxa<LXAMODE>_index): Add.
    	(*lxa<LXAMODE>_displacement_index): Add.
    	(*lxa<LXAMODE>_index_base): Add.
    	(*lxa<LXAMODE>_displacement_index_base): Add.
    	(*lxab_displacement_index_base): Add.
    	(*llxa<LXAMODE>_displacement_index): Add.
    	(*llxa<LXAMODE>_index_base): Add.
    	(*llxa<LXAMODE>_displacement_index_base): Add.
    	(*llxab_displacement_index_base): Add.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/s390/llxa-1.c: New test.
    	* gcc.target/s390/llxa-2.c: New test.
    	* gcc.target/s390/llxa-3.c: New test.
    	* gcc.target/s390/lxa-1.c: New test.
    	* gcc.target/s390/lxa-2.c: New test.
    	* gcc.target/s390/lxa-3.c: New test.
    	* gcc.target/s390/lxa-4.c: New test.

---
+++ gcc/testsuite/gcc.target/s390/lxa-2.c
@@ -0,0 +1,34 @@
+/* { dg-do compile { target { lp64 } } } */
+/* { dg-options "-O1 -march=arch15" } */
+/* { dg-final { scan-assembler {\tlxah\t%r[0-9]+,42\(%r[0-9]+,0\)} } } */
+/* { dg-final { scan-assembler {\tlxaf\t%r[0-9]+,42\(%r[0-9]+,0\)} } } */
+/* { dg-final { scan-assembler {\tlxag\t%r[0-9]+,42\(%r[0-9]+,0\)} } } */
+/* { dg-final { scan-assembler {\tlxaq\t%r[0-9]+,42\(%r[0-9]+,0\)} } } */
+
+long
+lxah (long j)
+{
+  long i = (int)j + 42;
+  return i << 1;
+}
+
+long
+lxaf (long j)
+{
+  long i = (int)j + 42;
+  return i << 2;
+}
+
+long
+lxag (long j)
+{
+  long i = (int)j + 42;
+  return i << 3;
+}
+
+long
+lxaq (long j)
+{
+  long i = (int)j + 42;
+  return i << 4;
+}
