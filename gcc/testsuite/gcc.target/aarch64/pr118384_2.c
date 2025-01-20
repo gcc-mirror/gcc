09c8aa319e1da30c5629024bfd3bcfc6cf42770e
Richard Sandiford <richard.sandiford@arm.com>
2025-01-20 20:05:04 +0000

    Revert "vect: Preserve OMP info for conditional stores [PR118384]"
    This reverts commit 8edf8b552313951cb4f2f97821ee4b3820c9506b.

--- gcc/testsuite/gcc.target/aarch64/pr118384_2.c
+++
@@ -1,4 +0,0 @@
-/* { dg-do run { target aarch64_sve256_hw } } */
-/* { dg-options "-O2 -fopenmp-simd -fno-trapping-math -msve-vector-bits=256 --param aarch64-autovec-preference=sve-only -fstack-protector-strong" } */
-
-#include "pr118384_1.c"
