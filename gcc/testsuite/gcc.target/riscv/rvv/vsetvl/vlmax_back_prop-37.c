1edd93fbaddce9b2938e2927014272fa621ade9c
Jeff Law <jlaw@ventanamicro.com>
2025-01-20 15:05:34 -0700

    [PR target/116256] Adjust expected output in a couple testcases
    I've had a long standing TODO to review the RISC-V testsuite regressions from
    enabling the late-combine pass (pr116256).  I adjusted a few cases months ago,
    this adjusts a couple more were it looks like the right thing to do.
    
    All that's left after this are the vls/dup-? tests which regress in meaningful
    ways and I'm still investigating reasonable approaches to fix them (they play
    into the whole mvconst_internal pattern situation), late-combine isn't doing
    anything wrong.
    
    	PR target/116256
    gcc/testsuite
    	* gcc.target/riscv/rvv/vsetvl/vlmax_back_prop-37.c: Update expected
    	output.
    	* gcc.target/riscv/rvv/vsetvl/vsetvl-15.c: Likewise.

--- gcc/testsuite/gcc.target/riscv/rvv/vsetvl/vlmax_back_prop-37.c
+++ gcc/testsuite/gcc.target/riscv/rvv/vsetvl/vlmax_back_prop-37.c
@@ -38,4 +38,4 @@ void f (int32_t * restrict in, int32_t * restrict out, int32_t * restrict in2, i
 
 /* { dg-final { scan-assembler-times {vsetvli\s+[a-x0-9]+,\s*zero,\s*e32,\s*mf2,\s*t[au],\s*m[au]} 1 { target { no-opts "-O0" no-opts "-O1"  no-opts "-Os" no-opts "-Oz" no-opts "-funroll-loops" no-opts "-g" } } } } */
 /* { dg-final { scan-assembler-times {vsetvli\s+[a-x0-9]+,\s*zero,\s*e8,\s*mf8,\s*t[au],\s*m[au]} 1 { target { no-opts "-O0" no-opts "-O1"  no-opts "-Os" no-opts "-Oz" no-opts "-funroll-loops" no-opts "-g" } } } } */
-/* { dg-final { scan-assembler-times {vsetvli} 2 { target { no-opts "-O0"  no-opts "-O1"  no-opts "-Os" no-opts "-Oz" no-opts "-funroll-loops" no-opts "-g" } } } } */
+/* { dg-final { scan-assembler-times {vsetvli} 3 { target { no-opts "-O0"  no-opts "-O1"  no-opts "-Os" no-opts "-Oz" no-opts "-funroll-loops" no-opts "-g" } } } } */
