cad542b63b4a8ecc1088bdcb8e4aa2d200c851f2
Nina Ranns <dinka.ranns@gmail.com>
2025-01-22 15:06:00 +0000

    adding flag_contract_nonattr_client_check flag

---
+++ gcc/testsuite/g++.dg/contracts/cpp26/definition-checks/virtual-func-no-def-check.C
@@ -0,0 +1,29 @@
+// check that an invocation of a virtual function through the base class does not
+// check contracts of the derived function, which are definition side contracts
+// { dg-do run }
+// { dg-options "-std=c++2a -fcontracts -fcontracts-nonattr -fcontracts-nonattr-def-contracts=off " }
+
+struct Base
+{
+  virtual int f(const int a){ return 0;};
+};
+
+struct Child : Base
+{
+  virtual int f(const int a) pre (a > 14) post(r:r >2){ return 1;}
+};
+
+int fooBase(Base& b)
+{
+    return b.f(1);
+}
+
+int main(int, char**)
+{
+  Base b;
+  Child c;
+
+  fooBase (c);
+
+  return 0;
+}
