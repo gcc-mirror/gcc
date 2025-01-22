14d04e93cf83d167c650709c9f09aa5267cf71c5
Arthur Cohen <arthur.cohen@embecosm.com>
2025-01-22 12:37:58 +0000

    gccrs: mappings: Improve error message for get_lang_item_node
    gcc/rust/ChangeLog:
    
    	* util/rust-hir-map.cc (Mappings::get_lang_item_node): Better formatting when a lang
    	item does not exist when it should.

--- gcc/rust/util/rust-hir-map.cc
+++ gcc/rust/util/rust-hir-map.cc
@@ -1305,8 +1305,8 @@ Mappings::get_lang_item_node (LangItem::Kind item_type)
   if (auto lookup = lookup_lang_item_node (item_type))
     return *lookup;
 
-  rust_fatal_error (UNKNOWN_LOCATION, "failed to find lang item %qs",
-		    LangItem::ToString (item_type).c_str ());
+  rust_fatal_error (UNKNOWN_LOCATION, "undeclared lang item: %qs",
+		    LangItem::PrettyString (item_type).c_str ());
 }
 
 void
