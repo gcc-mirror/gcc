9e6d06e93df566046225afc541511893bbeab845
Arthur Cohen <arthur.cohen@embecosm.com>
2025-01-23 11:46:09 +0000

    session-manager: Call into DesugarQuestionMark
    gcc/rust/ChangeLog:
    
    	* rust-session-manager.cc (Session::compile_crate): Call DesugarQuestionMark::go().

--- gcc/rust/rust-session-manager.cc
+++ gcc/rust/rust-session-manager.cc
@@ -19,6 +19,7 @@
 #include "rust-session-manager.h"
 #include "rust-collect-lang-items.h"
 #include "rust-desugar-for-loops.h"
+#include "rust-desugar-question-mark.h"
 #include "rust-diagnostics.h"
 #include "rust-hir-pattern-analysis.h"
 #include "rust-immutable-name-resolution-context.h"
@@ -610,6 +611,7 @@ Session::compile_crate (const char *filename)
     return;
 
   AST::CollectLangItems ().go (parsed_crate);
+  AST::DesugarQuestionMark ().go (parsed_crate);
 
   auto name_resolution_ctx = Resolver2_0::NameResolutionContext ();
   // expansion pipeline stage
