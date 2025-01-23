f73fb802ed2b0d2c6a29fad6036b36452157bcf5
Nina Ranns <dinka.ranns@gmail.com>
2025-01-23 14:44:06 +0000

    fixing typos

--- gcc/testsuite/g++.dg/contracts/cpp26/definition-checks/contract-assert-no-def-check.C
+++ gcc/testsuite/g++.dg/contracts/cpp26/definition-checks/contract-assert-no-def-check.C
@@ -1,16 +1,16 @@
-// Check that contract asserts are checkef when the definition side contracts
+// Check that contract asserts are checked when the definition side contracts
 // are turned off
 // { dg-do run }
 // { dg-options "-std=c++2a -fcontracts -fcontracts-nonattr -fcontracts-nonattr-definition-check=off -fcontract-evaluation-semantic=observe" }
 
 #include <cstdlib>
 
-bool termintating_check(){
+bool terminating_check(){
   std::exit(-1);
   return true;
 }
 // pre and post check would cause termination
-void foo(int i) noexcept pre(termintating_check()) post(termintating_check()) {
+void foo(int i) noexcept pre(terminating_check()) post(terminating_check()) {
 
     contract_assert(i > 4);
 
