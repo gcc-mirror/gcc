e3da7e68880473a53ff900734fbca77531bf888d
Iain Sandoe <iain@sandoe.co.uk>
2025-01-27 07:16:26 +0000

    c++, contracts, Darwin: Fix local symbols for linker.
    Darwin partitions objects into 'atoms' based on linker-visible
    symbols.  If these are not present, and weak and non-weak sections
    overlap we get spurious warnings.  We need the contract violation
    constants and the source locations to be visible.
    
    In addition, this fixes those symbols such that we do not
    unconditionally emit periods into the names (some assemblers do
    not accept this).
    
    TODO: fix other cases at some stage.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/config/darwin.h
+++ gcc/config/darwin.h
@@ -1005,6 +1005,10 @@ extern GTY(()) section * darwin_sections[NUM_DARWIN_SECTIONS];
       sprintf (LABEL, "*%s%ld", "lASAN", (long)(NUM));\
     else if (strcmp ("LTRAMP", PREFIX) == 0)	\
       sprintf (LABEL, "*%s%ld", "lTRAMP", (long)(NUM));\
+    else if (strcmp ("Lcontract_violation", PREFIX) == 0)	\
+      sprintf (LABEL, "*%s%ld", "lcontract_violation", (long)(NUM));\
+    else if (strcmp ("Lsrc_loc_impl.", PREFIX) == 0)	\
+      sprintf (LABEL, "*%s%ld", "lsrc_loc_impl", (long)(NUM));\
     else						\
       sprintf (LABEL, "*%s%ld", PREFIX, (long)(NUM));	\
   } while (0)
