be2ead6d3eb315b4c1e20c34044f20ff47589b81
Tobias Burnus <tburnus@baylibre.com>
2025-01-27 12:41:53 +0100

    C++: reject OpenMP directives in constexpr functions
    gcc/cp/ChangeLog:
    
    	* parser.cc (cp_parser_omp_construct, cp_parser_pragma): Reject
    	OpenMP expressions in constexpr functions.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/gomp/pr108607.C: Update dg-error.
    	* g++.dg/gomp/pr79664.C: Update dg-error.
    	* g++.dg/gomp/omp-constexpr.C: New test.
    
    (cherry picked from commit 46dd8acffe8264c5586cf5dc489157ea254dc152)

--- gcc/testsuite/g++.dg/gomp/pr79664.C
+++ gcc/testsuite/g++.dg/gomp/pr79664.C
@@ -6,7 +6,7 @@ constexpr int
 f1 ()
 {
   int i = 0;
-#pragma omp parallel for			// { dg-error "is not a constant expression" }
+#pragma omp parallel for			// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   for (i = 0; i < 10; ++i)
     ;
   return 0;
@@ -16,7 +16,7 @@ constexpr int
 f2 ()
 {
   int i = 0;
-#pragma omp parallel				// { dg-error "is not a constant expression" }
+#pragma omp parallel				// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   i = 5;
   return 0;
 }
@@ -25,7 +25,7 @@ constexpr int
 f3 ()
 {
   int i = 0;
-#pragma omp task				// { dg-error "is not a constant expression" }
+#pragma omp task				// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   i = 5;
   return 0;
 }
@@ -34,7 +34,7 @@ constexpr int
 f4 ()
 {
   int i = 0;
-#pragma omp for					// { dg-error "is not a constant expression" }
+#pragma omp for					// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   for (i = 0; i < 10; ++i)
     ;
   return 0;
@@ -44,7 +44,7 @@ constexpr int
 f5 ()
 {
   int i = 0;
-#pragma omp taskloop				// { dg-error "is not a constant expression" }
+#pragma omp taskloop				// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   for (i = 0; i < 10; ++i)
     ;
   return 0;
@@ -54,7 +54,7 @@ constexpr int
 f6 ()
 {
   int i = 0;
-#pragma omp target teams			// { dg-error "is not a constant expression" }
+#pragma omp target teams			// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   i = 5;
   return 0;
 }
@@ -63,7 +63,7 @@ constexpr int
 f7 ()
 {
   int i = 0;
-#pragma omp target data map(tofrom:i)		// { dg-error "is not a constant expression" }
+#pragma omp target data map(tofrom:i)		// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   i = 5;
   return 0;
 }
@@ -72,7 +72,7 @@ constexpr int
 f8 ()
 {
   int i = 0;
-#pragma omp target				// { dg-error "is not a constant expression" }
+#pragma omp target				// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   i = 5;
   return 0;
 }
@@ -81,9 +81,9 @@ constexpr int
 f9 ()
 {
   int i = 0;
-#pragma omp sections				// { dg-error "is not a constant expression" }
+#pragma omp sections				// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   {
-#pragma omp section
+#pragma omp section				// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
     i = 5;
   }
   return 0;
@@ -93,7 +93,7 @@ constexpr int
 f10 ()
 {
   int i = 0;
-#pragma omp ordered				// { dg-error "is not a constant expression" }
+#pragma omp ordered				// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   i = 1;
   return 0;
 }
@@ -102,7 +102,7 @@ constexpr int
 f11 ()
 {
   int i = 0;
-#pragma omp critical				// { dg-error "is not a constant expression" }
+#pragma omp critical				// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   i = 1;
   return 0;
 }
@@ -111,7 +111,7 @@ constexpr int
 f12 ()
 {
   int i = 0;
-#pragma omp single				// { dg-error "is not a constant expression" }
+#pragma omp single				// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   i = 1;
   return 0;
 }
@@ -120,7 +120,7 @@ constexpr int
 f13 ()
 {
   int i = 0;
-#pragma omp master				// { dg-error "is not a constant expression" }
+#pragma omp master				// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   i = 1;
   return 0;
 }
@@ -129,7 +129,7 @@ constexpr int
 f14 ()
 {
   int i = 0;
-#pragma omp taskgroup				// { dg-error "is not a constant expression" }
+#pragma omp taskgroup				// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   i = 1;
   return 0;
 }
@@ -138,7 +138,7 @@ constexpr int
 f15 ()
 {
   int i = 0;
-#pragma omp target update to(i)			// { dg-error "is not a constant expression" }
+#pragma omp target update to(i)			// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   i = 1;
   return 0;
 }
@@ -147,7 +147,7 @@ constexpr int
 f16 ()
 {
   int i = 0;
-#pragma omp target update to(i)			// { dg-error "is not a constant expression" }
+#pragma omp target update to(i)			// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   return 0;
 }
 
@@ -155,7 +155,7 @@ constexpr int
 f17 ()
 {
   int i = 0;
-#pragma omp target enter data map(to:i)		// { dg-error "is not a constant expression" }
+#pragma omp target enter data map(to:i)		// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   return 0;
 }
 
@@ -163,6 +163,6 @@ constexpr int
 f18 ()
 {
   int i = 0;
-#pragma omp target exit data map(from:i)	// { dg-error "is not a constant expression" }
+#pragma omp target exit data map(from:i)	// { dg-error "OpenMP directives may not appear in 'constexpr' functions" }
   return 0;
 }
