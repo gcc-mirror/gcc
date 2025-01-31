a9172b107a24b244e0b71c2575dd6448d48b3ae3
Patrick Palka <ppalka@redhat.com>
2025-01-31 15:53:12 -0500

    libstdc++: Fix flat_foo::insert_range for non-common ranges [PR118156]
    This fixes flat_map/multimap::insert_range by just generalizing the
    insert implementation to handle heterogenous iterator/sentinel pair.
    I'm not sure we can do better than this, e.g. we can't implement it in
    terms of the adapted containers' insert_range because that'd require two
    passes over the range.
    
    For flat_set/multiset, we can implement insert_range directly in terms
    of the adapted container's insert_range.  A fallback implementation
    is also provided if insert_range isn't available, as is the case for
    std::deque currently.
    
    	PR libstdc++/118156
    
    libstdc++-v3/ChangeLog:
    
    	* include/std/flat_map (_Flat_map_impl::_M_insert): Generalized
    	version of insert taking heterogenous iterator/sentinel pair.
    	(_Flat_map_impl::insert): Dispatch to _M_insert.
    	(_Flat_map_impl::insert_range): Likewise.
    	(flat_map): Export _Flat_map_impl::insert_range.
    	(flat_multimap): Likewise.
    	* include/std/flat_set (_Flat_set_impl::insert_range):
    	Reimplement directly, not in terms of insert.
    	(flat_set): Export _Flat_set_impl::insert_range.
    	(flat_multiset): Likewise.
    	* testsuite/23_containers/flat_map/1.cc (test06): New test.
    	* testsuite/23_containers/flat_multimap/1.cc (test06): New test.
    	* testsuite/23_containers/flat_multiset/1.cc (test06): New test.
    	* testsuite/23_containers/flat_set/1.cc (test06): New test.
    
    Reviewed-by: Jonathan Wakely <jwakely@redhat.com>

--- libstdc++-v3/testsuite/23_containers/flat_multiset/1.cc
+++ libstdc++-v3/testsuite/23_containers/flat_multiset/1.cc
@@ -2,6 +2,7 @@
 
 #include <flat_set>
 #include <deque>
+#include <ranges>
 #include <vector>
 #include <testsuite_allocator.h>
 #include <testsuite_hooks.h>
@@ -128,6 +129,20 @@ test05()
   VERIFY( std::ranges::equal(m, (int[]){1, 2, 3, 3, 4, 5}) );
 }
 
+void
+test06()
+{
+  // PR libstdc++/118156 - flat_foo::insert_range cannot handle non-common ranges
+  std::flat_multiset<int> s;
+  auto r = std::views::iota(1) | std::views::take(5);
+  static_assert(!std::ranges::common_range<decltype(r)>);
+  s.insert_range(r);
+  VERIFY( std::ranges::equal(s, (int[]){1, 2, 3, 4, 5}) );
+  s.clear();
+  s.insert_range(r | std::views::reverse);
+  VERIFY( std::ranges::equal(s, (int[]){1, 2, 3, 4, 5}) );
+}
+
 int
 main()
 {
@@ -137,4 +152,5 @@ main()
   test03();
   test04();
   test05();
+  test06();
 }
