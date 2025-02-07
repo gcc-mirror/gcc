670f83c0b9f50e54b082ad566049f812fe47ae65
Alexandre Oliva <oliva@adacore.com>
2025-02-07 04:10:57 -0300

    [testsuite] [sparc] use -mtune in alignment tuning test
    If -mcpu=leon3 is present in the command line for a test run,
    overriding it with -mcpu=niagara7 is not enough to override the tuning
    for leon3 selected by the previous -mcpu option.
    
    niagara7-align.c tests for niagara7 alignment tuning, so use -mtune
    rather than -mcpu.
    
    
    for  gcc/testsuite/ChangeLog
    
    	* gcc.target/sparc/niagara7-align.c: Use -mtune.

--- gcc/testsuite/gcc.target/sparc/niagara7-align.c
+++ gcc/testsuite/gcc.target/sparc/niagara7-align.c
@@ -1,4 +1,4 @@
 /* { dg-do compile } */
-/* { dg-options "-falign-functions -mcpu=niagara7" } */
+/* { dg-options "-falign-functions -mtune=niagara7" } */
 /* { dg-final { scan-assembler "\.align 64" } } */
 void foo(void) {}
