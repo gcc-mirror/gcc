c1af05f3d0320b5af98f27a76abfc9263bd42bb8
Lulu Cheng <chenglulu@loongson.cn>
2025-02-11 20:05:13 +0800

    LoongArch: Split the function loongarch_cpu_cpp_builtins into two functions.
    Split the implementation of the function loongarch_cpu_cpp_builtins into two parts:
      1. Macro definitions that do not change (only considering 64-bit architecture)
      2. Macro definitions that change with different compilation options.
    
    gcc/ChangeLog:
    
    	* config/loongarch/loongarch-c.cc (builtin_undef): New macro.
    	(loongarch_cpu_cpp_builtins): Split to loongarch_update_cpp_builtins
    	and loongarch_define_unconditional_macros.
    	(loongarch_def_or_undef): New functions.
    	(loongarch_define_unconditional_macros): Likewise.
    	(loongarch_update_cpp_builtins): Likewise.

--- gcc/config/loongarch/loongarch-c.cc
+++ gcc/config/loongarch/loongarch-c.cc
@@ -31,26 +31,22 @@ along with GCC; see the file COPYING3.  If not see
 
 #define preprocessing_asm_p() (cpp_get_options (pfile)->lang == CLK_ASM)
 #define builtin_define(TXT) cpp_define (pfile, TXT)
+#define builtin_undef(TXT) cpp_undef (pfile, TXT)
 #define builtin_assert(TXT) cpp_assert (pfile, TXT)
 
-void
-loongarch_cpu_cpp_builtins (cpp_reader *pfile)
+static void
+loongarch_def_or_undef (bool def_p, const char *macro, cpp_reader *pfile)
 {
-  builtin_assert ("machine=loongarch");
-  builtin_assert ("cpu=loongarch");
+  if (def_p)
+    cpp_define (pfile, macro);
+  else
+    cpp_undef (pfile, macro);
+}
+
+static void
+loongarch_define_unconditional_macros (cpp_reader *pfile)
+{
   builtin_define ("__loongarch__");
-
-  builtin_define_with_value ("__loongarch_arch",
-			     loongarch_arch_strings[la_target.cpu_arch], 1);
-
-  builtin_define_with_value ("__loongarch_tune",
-			     loongarch_tune_strings[la_target.cpu_tune], 1);
-
-  builtin_define_with_value ("_LOONGARCH_ARCH",
-			     loongarch_arch_strings[la_target.cpu_arch], 1);
-
-  builtin_define_with_value ("_LOONGARCH_TUNE",
-			     loongarch_tune_strings[la_target.cpu_tune], 1);
 
   /* Base architecture / ABI.  */
   if (TARGET_64BIT)
@@ -64,68 +60,6 @@ loongarch_cpu_cpp_builtins (cpp_reader *pfile)
       builtin_define ("_ABILP64=3");
       builtin_define ("_LOONGARCH_SIM=_ABILP64");
       builtin_define ("__loongarch_lp64");
-    }
-
-  /* These defines reflect the ABI in use, not whether the
-     FPU is directly accessible.  */
-  if (TARGET_DOUBLE_FLOAT_ABI)
-    builtin_define ("__loongarch_double_float=1");
-  else if (TARGET_SINGLE_FLOAT_ABI)
-    builtin_define ("__loongarch_single_float=1");
-
-  if (TARGET_DOUBLE_FLOAT_ABI || TARGET_SINGLE_FLOAT_ABI)
-    builtin_define ("__loongarch_hard_float=1");
-  else
-    builtin_define ("__loongarch_soft_float=1");
-
-
-  /* ISA Extensions.  */
-  if (TARGET_DOUBLE_FLOAT)
-    builtin_define ("__loongarch_frlen=64");
-  else if (TARGET_SINGLE_FLOAT)
-    builtin_define ("__loongarch_frlen=32");
-  else
-    builtin_define ("__loongarch_frlen=0");
-
-  if (TARGET_HARD_FLOAT && ISA_HAS_FRECIPE)
-    builtin_define ("__loongarch_frecipe");
-
-  if (ISA_HAS_LSX)
-    {
-      builtin_define ("__loongarch_simd");
-      builtin_define ("__loongarch_sx");
-
-      if (!ISA_HAS_LASX)
-	builtin_define ("__loongarch_simd_width=128");
-    }
-
-  if (ISA_HAS_LASX)
-    {
-      builtin_define ("__loongarch_asx");
-      builtin_define ("__loongarch_simd_width=256");
-    }
-
-  /* ISA evolution features */
-  int max_v_major = 1, max_v_minor = 0;
-
-  for (int i = 0; i < N_EVO_FEATURES; i++)
-    if (la_target.isa.evolution & la_evo_feature_masks[i])
-      {
-	builtin_define (la_evo_macro_name[i]);
-
-	int major = la_evo_version_major[i],
-	    minor = la_evo_version_minor[i];
-
-	max_v_major = major > max_v_major ? major : max_v_major;
-	max_v_minor = major == max_v_major
-	  ? (minor > max_v_minor ? minor : max_v_minor) : max_v_minor;
-      }
-
-  /* Find the minimum ISA version required to run the target program.  */
-  if (!(max_v_major == 1 && max_v_minor <= 1 && ISA_HAS_LASX))
-    {
-      builtin_define_with_int_value ("__loongarch_version_major", max_v_major);
-      builtin_define_with_int_value ("__loongarch_version_minor", max_v_minor);
     }
 
   /* Add support for FLOAT128_TYPE on the LoongArch architecture.  */
@@ -145,7 +79,105 @@ loongarch_cpu_cpp_builtins (cpp_reader *pfile)
   builtin_define_with_int_value ("_LOONGARCH_SZPTR", POINTER_SIZE);
   builtin_define_with_int_value ("_LOONGARCH_FPSET", 32);
   builtin_define_with_int_value ("_LOONGARCH_SPFPSET", 32);
+}
 
+static void
+loongarch_update_cpp_builtins (cpp_reader *pfile)
+{
+  /* Since the macros in this function might be redefined, it's necessary to
+     undef them first.*/
+  builtin_undef ("__loongarch_arch");
+  builtin_define_with_value ("__loongarch_arch",
+			     loongarch_arch_strings[la_target.cpu_arch], 1);
+
+  builtin_undef ("__loongarch_tune");
+  builtin_define_with_value ("__loongarch_tune",
+			     loongarch_tune_strings[la_target.cpu_tune], 1);
+
+  builtin_undef ("_LOONGARCH_ARCH");
+  builtin_define_with_value ("_LOONGARCH_ARCH",
+			     loongarch_arch_strings[la_target.cpu_arch], 1);
+
+  builtin_undef ("_LOONGARCH_TUNE");
+  builtin_define_with_value ("_LOONGARCH_TUNE",
+			     loongarch_tune_strings[la_target.cpu_tune], 1);
+
+  builtin_undef ("__loongarch_double_float");
+  builtin_undef ("__loongarch_single_float");
+  /* These defines reflect the ABI in use, not whether the
+     FPU is directly accessible.  */
+  if (TARGET_DOUBLE_FLOAT_ABI)
+    builtin_define ("__loongarch_double_float=1");
+  else if (TARGET_SINGLE_FLOAT_ABI)
+    builtin_define ("__loongarch_single_float=1");
+
+  builtin_undef ("__loongarch_soft_float");
+  builtin_undef ("__loongarch_hard_float");
+  if (TARGET_DOUBLE_FLOAT_ABI || TARGET_SINGLE_FLOAT_ABI)
+    builtin_define ("__loongarch_hard_float=1");
+  else
+    builtin_define ("__loongarch_soft_float=1");
+
+
+  /* ISA Extensions.  */
+  builtin_undef ("__loongarch_frlen");
+  if (TARGET_DOUBLE_FLOAT)
+    builtin_define ("__loongarch_frlen=64");
+  else if (TARGET_SINGLE_FLOAT)
+    builtin_define ("__loongarch_frlen=32");
+  else
+    builtin_define ("__loongarch_frlen=0");
+
+  loongarch_def_or_undef (TARGET_HARD_FLOAT && ISA_HAS_FRECIPE,
+			  "__loongarch_frecipe", pfile);
+
+  loongarch_def_or_undef (ISA_HAS_LSX, "__loongarch_simd", pfile);
+  loongarch_def_or_undef (ISA_HAS_LSX, "__loongarch_sx", pfile);
+  loongarch_def_or_undef (ISA_HAS_LASX, "__loongarch_asx", pfile);
+
+  builtin_undef ("__loongarch_simd_width");
+  if (ISA_HAS_LSX)
+    {
+      if (ISA_HAS_LASX)
+	builtin_define ("__loongarch_simd_width=256");
+      else
+	builtin_define ("__loongarch_simd_width=128");
+    }
+
+  /* ISA evolution features */
+  int max_v_major = 1, max_v_minor = 0;
+
+  for (int i = 0; i < N_EVO_FEATURES; i++)
+    if (la_target.isa.evolution & la_evo_feature_masks[i])
+      {
+	builtin_define (la_evo_macro_name[i]);
+
+	int major = la_evo_version_major[i],
+	    minor = la_evo_version_minor[i];
+
+	max_v_major = major > max_v_major ? major : max_v_major;
+	max_v_minor = major == max_v_major
+	  ? (minor > max_v_minor ? minor : max_v_minor) : max_v_minor;
+      }
+
+  /* Find the minimum ISA version required to run the target program.  */
+  builtin_undef ("__loongarch_version_major");
+  builtin_undef ("__loongarch_version_minor");
+  if (!(max_v_major == 1 && max_v_minor <= 1 && ISA_HAS_LASX))
+    {
+      builtin_define_with_int_value ("__loongarch_version_major", max_v_major);
+      builtin_define_with_int_value ("__loongarch_version_minor", max_v_minor);
+    }
+}
+
+void
+loongarch_cpu_cpp_builtins (cpp_reader *pfile)
+{
+  builtin_assert ("machine=loongarch");
+  builtin_assert ("cpu=loongarch");
+
+  loongarch_define_unconditional_macros (pfile);
+  loongarch_update_cpp_builtins (pfile);
 }
 
 /* Hook to validate the current #pragma GCC target and set the state, and
