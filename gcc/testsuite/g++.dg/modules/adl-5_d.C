1bfab1dc79bef1f1120bda37556f2e372a2378f5
Nathaniel Shead <nathanieloshead@gmail.com>
2025-02-11 22:24:55 +1100

    testsuite: Fix g++.dg/modules/adl-5
    This testcase wasn't running, because adl-5_a had the wrong extension.
    adl-5_d should have been reporting an error because 'frob' is only
    visible from within the 'hidden' module but this was missed.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/modules/adl-5_a.c: Move to...
    	* g++.dg/modules/adl-5_a.C: ...here.
    	* g++.dg/modules/adl-5_d.C: Add errors.
    
    Signed-off-by: Nathaniel Shead <nathanieloshead@gmail.com>

--- gcc/testsuite/g++.dg/modules/adl-5_d.C
+++ gcc/testsuite/g++.dg/modules/adl-5_d.C
@@ -7,10 +7,11 @@ int main ()
 {
   X x (2);
 
-  if (frob (x) != 2)
+  if (frob (x) != 2)  // { dg-error "not declared in" }
     return 1;
 
-  if (TPL (x) != 2)
+  // { dg-regexp "\n\[^\n]*adl-5_a.C:8:15: error: 'frob' was not declared in this scope$" }
+  if (TPL (x) != 2)  // { dg-message "required from here" }
     return 2;
 
   return 0;
