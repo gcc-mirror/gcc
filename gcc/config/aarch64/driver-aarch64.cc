fa5aedd841105329b2f65cb0ff418cb4427f255e
Tamar Christina <tamar.christina@arm.com>
2025-02-12 10:38:21 +0000

    AArch64: Fix GCC 13 backport of big.Little CPU detection [PR118800]
    On the GCC-13 branch the backport caused a failure due to the branch not having
    generic-armv8-a and also it still treating the generic cpu special.  This made
    it return NULL when trying to find the default CPU.
    
    In GCC 13 we still had multiple structures with the same information and in this
    case aarch64_cpu_data was missing the generic CPU which is in all_cores.
    
    This corrects it by using "generc" instead and also adding it to
    aarch64_cpu_data.
    
    gcc/ChangeLog:
    
    	PR target/118800
    	* config/aarch64/driver-aarch64.cc (DEFAULT_CPU): Use generic instead of
    	generic-armv8-a.
    	(aarch64_cpu_data): Add generic.
    
    gcc/testsuite/ChangeLog:
    
    	PR target/118800
    	* gcc.target/aarch64/cpunative/native_cpu_34.c: Update order.

--- gcc/config/aarch64/driver-aarch64.cc
+++ gcc/config/aarch64/driver-aarch64.cc
@@ -60,7 +60,7 @@ struct aarch64_core_data
 #define ALL_VARIANTS ((unsigned)-1)
 /* Default architecture to use if -mcpu=native did not detect a known CPU.  */
 #define DEFAULT_ARCH "8A"
-#define DEFAULT_CPU "generic-armv8-a"
+#define DEFAULT_CPU "generic"
 
 #define AARCH64_CORE(CORE_NAME, CORE_IDENT, SCHED, ARCH, FLAGS, COSTS, IMP, PART, VARIANT) \
   { CORE_NAME, #ARCH, IMP, PART, VARIANT, feature_deps::cpu_##CORE_IDENT },
@@ -68,6 +68,7 @@ struct aarch64_core_data
 static CONSTEXPR const aarch64_core_data aarch64_cpu_data[] =
 {
 #include "aarch64-cores.def"
+  { "generic", "armv8-a", 0, 0, ALL_VARIANTS, 0},
   { NULL, NULL, INVALID_IMP, INVALID_CORE, ALL_VARIANTS, 0 }
 };
 
