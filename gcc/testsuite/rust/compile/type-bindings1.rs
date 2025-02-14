f21bf4bd6c415884c3b24c96a381d9cbc6c1cde2
Philip Herron <herron.philip@googlemail.com>
2025-02-14 17:32:20 +0000

    gccrs: improve error diagnostic for bad type-resolution in CallExpr
    We have the type information for the resolved call lets tell the user about
    it in the diagnostic and apply the correct error code.
    
    Fixes Rust-GCC#2035
    
    gcc/rust/ChangeLog:
    
    	* typecheck/rust-hir-type-check-expr.cc (TypeCheckExpr::visit): improve error diag
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/generics4.rs: cleanup
    	* rust/compile/generics6.rs: likewise
    	* rust/compile/type-bindings1.rs: likewise
    	* rust/compile/unconstrained_type_param.rs: likewise
    	* rust/compile/issue-2035.rs: New test.
    
    Signed-off-by: Philip Herron <herron.philip@googlemail.com>

--- gcc/testsuite/rust/compile/type-bindings1.rs
+++ gcc/testsuite/rust/compile/type-bindings1.rs
@@ -7,5 +7,4 @@ fn main() {
     let a;
     a = Foo::<A = i32, B = f32>(123f32);
     // { dg-error "associated type bindings are not allowed here" "" { target *-*-* } .-1 }
-    // { dg-error {Failed to resolve expression of function call} "" { target *-*-* } .-2 }
 }
