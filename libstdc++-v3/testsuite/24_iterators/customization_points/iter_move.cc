58a2b7f05587485f846490e21e4de3d51d83afb9
Jonathan Wakely <jwakely@redhat.com>
2025-02-28 21:44:41 +0000

    libstdc++: Fix ranges::iter_move handling of rvalues [PR106612]
    The specification for std::ranges::iter_move apparently requires us to
    handle types which do not satisfy std::indirectly_readable, for example
    with overloaded operator* which behaves differently for different value
    categories.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/106612
    	* include/bits/iterator_concepts.h (_IterMove::__iter_ref_t):
    	New alias template.
    	(_IterMove::__result): Use __iter_ref_t instead of
    	std::iter_reference_t.
    	(_IterMove::__type): Remove incorrect __dereferenceable
    	constraint.
    	(_IterMove::operator()): Likewise. Add correct constraints. Use
    	__iter_ref_t instead of std::iter_reference_t. Forward parameter
    	as correct value category.
    	(iter_swap): Add comments.
    	* testsuite/24_iterators/customization_points/iter_move.cc: Test
    	that iter_move is found by ADL and that rvalue arguments are
    	handled correctly.
    
    Reviewed-by: Patrick Palka <ppalka@redhat.com>
    
    (cherry picked from commit a8ee522c5923ba17851e4b71316a2dff19d6368f)

--- libstdc++-v3/testsuite/24_iterators/customization_points/iter_move.cc
+++ libstdc++-v3/testsuite/24_iterators/customization_points/iter_move.cc
@@ -63,8 +63,103 @@ test01()
   VERIFY( test_X(3, 4) );
 }
 
+template<typename T>
+using rval_ref = std::iter_rvalue_reference_t<T>;
+
+static_assert(std::same_as<rval_ref<int*>, int&&>);
+static_assert(std::same_as<rval_ref<const int*>, const int&&>);
+static_assert(std::same_as<rval_ref<std::move_iterator<int*>>, int&&>);
+
+template<typename T>
+concept iter_movable = requires { std::ranges::iter_move(std::declval<T>()); };
+
+struct Iter
+{
+  friend int& iter_move(Iter&) { static int i = 1; return i; }
+  friend long iter_move(Iter&&) { return 2; }
+  const short& operator*() const & { static short s = 3; return s; }
+  friend float operator*(const Iter&&) { return 4.0f; }
+};
+
+void
+test_adl()
+{
+  Iter it;
+  const Iter& cit = it;
+
+  VERIFY( std::ranges::iter_move(it) == 1 );
+  VERIFY( std::ranges::iter_move(std::move(it)) == 2 );
+  VERIFY( std::ranges::iter_move(cit) == 3 );
+  VERIFY( std::ranges::iter_move(std::move(cit)) == 4.0f );
+
+  // The return type should be unchanged for ADL iter_move:
+  static_assert(std::same_as<decltype(std::ranges::iter_move(it)), int&>);
+  static_assert(std::same_as<decltype(std::ranges::iter_move(std::move(it))),
+			     long>);
+  // When ADL iter_move is not used, return type should be an rvalue:
+  static_assert(std::same_as<decltype(std::ranges::iter_move(cit)),
+			     const short&&>);
+  static_assert(std::same_as<decltype(std::ranges::iter_move(std::move(cit))),
+			     float>);
+
+  // std::iter_rvalue_reference_t always considers the argument as lvalue.
+  static_assert(std::same_as<rval_ref<Iter>, int&>);
+  static_assert(std::same_as<rval_ref<Iter&>, int&>);
+  static_assert(std::same_as<rval_ref<const Iter>, const short&&>);
+  static_assert(std::same_as<rval_ref<const Iter&>, const short&&>);
+}
+
+void
+test_pr106612()
+{
+  // Bug 106612 ranges::iter_move does not consider iterator's value categories
+
+  struct I
+  {
+    int i{};
+    int& operator*() & { return i; }
+    int operator*() const & { return i; }
+    void operator*() && = delete;
+  };
+
+  static_assert( iter_movable<I&> );
+  static_assert( iter_movable<I const&> );
+  static_assert( ! iter_movable<I> );
+  static_assert( std::same_as<std::iter_rvalue_reference_t<I>, int&&> );
+  static_assert( std::same_as<std::iter_rvalue_reference_t<const I>, int> );
+
+  struct I2
+  {
+    int i{};
+    int& operator*() & { return i; }
+    int operator*() const & { return i; }
+    void operator*() &&;
+  };
+
+  static_assert( iter_movable<I2&> );
+  static_assert( iter_movable<I2 const&> );
+  static_assert( iter_movable<I2> );
+  static_assert( std::is_void_v<decltype(std::ranges::iter_move(I2{}))> );
+  static_assert( std::same_as<std::iter_rvalue_reference_t<I2>, int&&> );
+  static_assert( std::same_as<std::iter_rvalue_reference_t<I2 const>, int> );
+
+  enum E { e };
+  enum F { f };
+
+  struct I3
+  {
+    E operator*() const & { return e; }
+    F operator*() && { return f; }
+  };
+
+  static_assert( iter_movable<I3&> );
+  static_assert( iter_movable<I3> );
+  static_assert( std::same_as<decltype(std::ranges::iter_move(I3{})), F> );
+}
+
 int
 main()
 {
   test01();
+  test_adl();
 }
