671702b29f252b417810b7a1dc7506f096339577
Lulu Cheng <chenglulu@loongson.cn>
2025-03-03 16:52:43 +0800

    LoongArch: testsuite: Fix pr112325.c and pr117888-1.c.
    By default, vectorization is not enabled on LoongArch,
    resulting in the failure of these two test cases.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/vect/pr112325.c: Add the vector compilation
    	option '-mlsx' for LoongArch.
    	* gcc.dg/vect/pr117888-1.c: Likewise.

--- gcc/testsuite/gcc.dg/vect/pr112325.c
+++ gcc/testsuite/gcc.dg/vect/pr112325.c
@@ -4,6 +4,7 @@
 /* { dg-require-effective-target vect_shift } */
 /* { dg-additional-options "-mavx2" { target x86_64-*-* i?86-*-* } } */
 /* { dg-additional-options "--param max-completely-peeled-insns=200" { target powerpc64*-*-* } } */
+/* { dg-additional-options "-mlsx" { target loongarch64-*-* } } */
 
 typedef unsigned short ggml_fp16_t;
 static float table_f32_f16[1 << 16];
