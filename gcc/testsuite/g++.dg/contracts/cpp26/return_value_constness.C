b047572b5d07d714b920a40093c6e07a6633a0df
Nina Ranns <dinka.ranns@gmail.com>
2025-03-04 14:15:32 +0000

    addressing return value constness

---
+++ gcc/testsuite/g++.dg/contracts/cpp26/return_value_constness.C
@@ -0,0 +1,59 @@
+// check that the return value has correct const qualification
+// { dg-do run }
+// { dg-options "-std=c++2b -fcontracts -fcontracts-nonattr -fcontract-evaluation-semantic=observe  " }
+
+
+#include <type_traits>
+
+bool is_constified(int &){ return false;}
+
+bool is_constified(const int &){ return true;}
+
+
+int f1(int i)
+     pre (is_constified(i))
+     pre (std::is_same<decltype(i), int>::value)
+     pre (std::is_same<decltype((i)), const int&>::value)
+
+     post (r: is_constified(r))
+     post (r: std::is_same<decltype(r), int>::value)
+     post (r: std::is_same<decltype((r)), const int&>::value)
+     { return i; };
+
+int& f2(int& i)
+    pre (is_constified(i))
+    pre (std::is_same<decltype(i), int&>::value)
+    pre (std::is_same<decltype((i)), const int&>::value)
+
+    post (r: is_constified(r))
+    post (r: std::is_same<decltype(r), int&>::value)
+    post (r: std::is_same<decltype((r)), const int&>::value)
+    {
+      static_assert(std::is_same<decltype(i), int&>::value);
+      return i;
+    }
+
+int* f2(int* i)
+    pre (std::is_same<decltype(i), int*>::value)
+    pre (std::is_same<decltype((i)),int * const>::value)
+
+    post (r: std::is_same<decltype(r), int *>::value)
+    post (r: std::is_same<decltype((r)),int * const>::value)
+    { return i;}
+
+int const * f2(int const * i)
+    pre (std::is_same<decltype(i), int const *>::value)
+    pre (std::is_same<decltype((i)),int const * const>::value)
+
+    post (r: std::is_same<decltype(r), int const *>::value)
+    post (r: std::is_same<decltype((r)),int const * const>::value)
+    { return i;}
+
+int main()
+{
+
+  int i = 4;
+  f1(i);
+  f2(i);
+
+}
