316eaca17ee11f575fc72e139e8cc3f9f5ccb067
Jeff Law <jlaw@ventanamicro.com>
2025-03-05 22:24:05 -0700

    Improve coverage of ext-dce tests in risc-v testsuite
    Inspired by Liao Shihua, this adjusts two tests in the RISC-V testsuite
    to get more coverage.  Drop the -O1 argument and replace it with -fext-dce.
    That way the test gets run across the full set of flags.  We just need to
    make sure to skip -O0.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/core_list_init.c: Use -fext-dce rather than
    	-O1.  Skip for -O0.
    	* gcc.target/riscv/pr111384.c: Ditto.

--- gcc/testsuite/gcc.target/riscv/pr111384.c
+++ gcc/testsuite/gcc.target/riscv/pr111384.c
@@ -1,6 +1,7 @@
 /* { dg-do compile } */
-/* { dg-options "-O1 -fdump-rtl-ext_dce" } */
+/* { dg-options "-fext-dce -fdump-rtl-ext_dce" } */
 /* { dg-final { scan-rtl-dump {Successfully transformed} "ext_dce" } } */
+/* { dg-skip-if "" { *-*-* } { "-O0"} } */
 
 void
 foo(unsigned int src, unsigned short *dst1, unsigned short *dst2)
