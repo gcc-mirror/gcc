ce54bffdae2971a906fb0f64bfc02af8955d3fd3
Nina Ranns <dinka.ranns@gmail.com>
2025-03-06 11:38:30 +0000

    fixing a lambda ICE

---
+++ gcc/testsuite/g++.dg/contracts/cpp26/lambda.C
@@ -0,0 +1,6 @@
+// check that we do not crash when capturing a constified entity in a contract assertion lambda
+// { dg-do compile }
+// { dg-options "-std=c++2b -fcontracts -fcontracts-nonattr  " }
+void f(int i, int j) pre( [il = i, jl = &j](){ return true;} ( ))
+{
+};
