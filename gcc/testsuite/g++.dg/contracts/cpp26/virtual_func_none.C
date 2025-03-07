782263f6bb0e3797a9918fb1d231cdf57b27b1a6
Nina Ranns <dinka.ranns@gmail.com>
2025-03-07 13:39:34 +0000

    making contracts on virtual functions invalid

--- gcc/testsuite/g++.dg/contracts/cpp26/virtual_func_none.C
+++ gcc/testsuite/g++.dg/contracts/cpp26/virtual_func_none.C
@@ -1,132 +1,16 @@
-// test that contracts on overriding functions are found correctly
-// { dg-do run }
-// { dg-options "-std=c++2a -fcontracts -fcontract-continuation-mode=off -fcontracts-nonattr -fcontracts-nonattr-inheritance-mode=none " }
-#include <cassert>
-
-struct contract{
-  int checked = 0;
-};
-
-
-contract a,b,c;
-
-bool checkA(){
-    a.checked++;
-    return true;
-}
-
-bool checkB(){
-    b.checked++;
-    return true;
-}
-
-bool checkC(){
-    c.checked++;
-    return true;
-}
-
-void clear_checks(){
-  a.checked = b.checked = c.checked = 0;
-
-}
+// { dg-do compile }
+// { dg-options "-std=c++2b -fcontracts -fcontracts-nonattr -fcontracts-nonattr-inheritance-mode=none " }
 
 struct Base
 {
-  virtual void f() pre (checkA()){};
+  virtual int f1() pre(true); // { dg-error "Contracts can not be added to virtual functions" }
+  virtual int f2();
+
 };
-
-
-struct Child0 : Base
+struct Child : Base
 {
-  virtual void f() {};  //no contract
+  int f2() pre(true); // { dg-error "Contracts can not be added to virtual functions" }
+
 };
-
-struct Child1 : Base
-{
-  virtual void f() pre (checkB()){};
-};
-
-
-struct GChild1 : Child0
-{
-  virtual void f() pre (checkC()){};
-};
-
-struct GChild2 : Child1
-{
-  virtual void f() {};
-};
-
-
-void fooBase(Base* b)
-{
-    b->f();
-}
-
-int main(int, char**)
-{
-  Base b0;
-  Child0 c0;
-  Child1 c1;
-  GChild1 g1;
-  GChild2 g2;
-
-
-  clear_checks();
-  fooBase(&b0);
-  assert(a.checked > 0);
-
-  clear_checks();
-  fooBase(&c0);
-  assert(a.checked == 0);
-
-  clear_checks();
-  fooBase(&c1);
-  assert(a.checked == 0);
-  assert(b.checked > 0);
-
-
-  clear_checks();
-  fooBase(&g1);
-  assert(a.checked == 0);
-  assert(b.checked == 0);
-  assert(c.checked > 0);
-
-
-  clear_checks();
-  fooBase(&g2);
-  assert(a.checked == 0);
-  assert(b.checked == 0);
-  assert(c.checked == 0);
-
-  clear_checks();
-  b0.f();
-  assert(a.checked > 0);
-
-  clear_checks();
-  c0.f();
-  assert(a.checked == 0);
-
-  clear_checks();
-  c1.f();
-  assert(a.checked == 0);
-  assert(b.checked > 0);
-
-
-  clear_checks();
-  g1.f();;
-  assert(a.checked == 0);
-  assert(b.checked == 0);
-  assert(c.checked > 0);
-
-
-  clear_checks();
-  g2.f();
-  assert(a.checked == 0);
-  assert(b.checked == 0);
-  assert(c.checked == 0);
-
-  return 0;
-}
 
 
