be671ec1f30ecd55aaff09048afb2a619018cb8a
liuhongt <hongtao.liu@intel.com>
2025-03-16 22:28:44 -0700

    Mark gcc.target/i386/apx-ndd-tls-1b.c as xfail.
    It looks like the testcase is fragile, it's supposed to check the
    compiler ability of generating code_6_gottpoff_reloc instruction, but
    failed since there's a seg_prefixed memory
    usage(r14-6242-gd564198f960a2f).
    
            mov     r13, QWORD PTR j@gottpoff[rip]
            mov     r12, QWORD PTR a@gottpoff[rip]
            mov     rbp, QWORD PTR [rsp+1040]
            lea     rbx, [rsp+1040]
            add     r14, QWORD PTR fs:0, r12
    
    gcc/testsuite/ChangeLog:
    
    	PR target/117069
    	* gcc.target/i386/apx-ndd-tls-1b.c: Add xfail.

--- gcc/testsuite/gcc.target/i386/apx-ndd-tls-1b.c
+++ gcc/testsuite/gcc.target/i386/apx-ndd-tls-1b.c
@@ -3,7 +3,10 @@
 /* { dg-require-effective-target tls } */
 /* { dg-require-effective-target code_6_gottpoff_reloc } */
 /* { dg-options "-save-temps -std=gnu17 -mapxf -O3 -w" } */
-
+/* The testcase is fragile, it's supposed to check the compiler
+   ability of generating code_6_gottpoff_reloc instruction, but
+   failed since there's a seg_prefixed memory
+   usage(r14-6242-gd564198f960a2f).  */
 #include "apx-ndd-tls-1a.c"
 
-/* { dg-final { scan-assembler-times "addq\[ \t]+%r\[a-z0-9\]+, a@gottpoff\\(%rip\\), %r\[a-z0-9\]+" 1 { target lp64 } } } */
+/* { dg-final { scan-assembler-times "addq\[ \t]+%r\[a-z0-9\]+, a@gottpoff\\(%rip\\), %r\[a-z0-9\]+" 1 { xfail lp64 } } } */
