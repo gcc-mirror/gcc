ae5cecdad0dde10673254ca7a276a7845a797d8a
Nina Ranns <dinka.ranns@gmail.com>
2025-03-18 11:57:39 +0000

    fixing detection of non cont parameters in redeclarations. Diagnostic messages are minimum effort and can be improved

--- gcc/cp/semantics.cc
+++ gcc/cp/semantics.cc
@@ -5030,7 +5030,7 @@ finish_id_expression_1 (tree id_expression,
 	}
     }
 
-  maybe_reject_param_in_postcondition (decl);
+  maybe_reject_param_in_postcondition (decl, location);
   if (flag_contracts_nonattr && should_constify_contract
 	&& processing_contract_condition)
     decl = constify_contract_access(decl);
