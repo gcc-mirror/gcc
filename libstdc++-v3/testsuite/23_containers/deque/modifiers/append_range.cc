157c14a387fc880b31e9e42deb6960b3143d3c2d
Tomasz Kamiński <tkaminsk@redhat.com>
2025-03-21 09:03:54 +0100

    libstdc++: Add P1206R7 range operations to std::deque [PR111055]
    This is another piece of P1206R7, adding from_range constructor, append_range,
    prepend_range, insert_range, and assign_range members to std::deque.
    
    For append_front of input non-sized range, we are emplacing element at the front and
    then reverse inserted elements. This does not existing elements, and properly handle
    aliasing ranges.
    
    For insert_range, the handling of insertion in the middle of input-only ranges
    that are sized could be optimized, we still insert nodes one-by-one in such case.
    For forward and stronger ranges, we reduce them to common_range case, by computing
    the iterator when computing the distance. This is slightly suboptimal, as it require
    range to be iterated for non-common forward ranges that are sized, but reduces
    number of instantiations.
    
    This patch extract _M_range_prepend, _M_range_append helper functions that accepts
    (iterator, sentinel) pair. This all used in all standard modes.
    
    	PR libstdc++/111055
    
    libstdc++-v3/ChangeLog:
    
    	* include/bits/deque.tcc (deque::prepend_range, deque::append_range)
    	(deque::insert_range, __advance_dist): Define.
    	(deque::_M_range_prepend, deque::_M_range_append):
    	Extract from _M_range_insert_aux for _ForwardIterator(s).
    	* include/bits/stl_deque.h (deque::assign_range): Define.
    	(deque::prepend_range, deque::append_range, deque::insert_range):
    	Declare.
    	(deque(from_range_t, _Rg&&, const allocator_type&)): Define constructor
    	and deduction guide.
    	* include/debug/deque (deque::prepend_range, deque::append_range)
    	(deque::assign_range):	Define.
    	(deque(from_range_t, _Rg&&, const allocator_type&)): Define constructor
    	and deduction guide.
    	* testsuite/23_containers/deque/cons/from_range.cc: New test.
    	* testsuite/23_containers/deque/modifiers/append_range.cc: New test.
    	* testsuite/23_containers/deque/modifiers/assign/assign_range.cc:
    	New test.
    	* testsuite/23_containers/deque/modifiers/prepend_range.cc: New test.
    
    Reviewed-by: Jonathan Wakely <jwakely@redhat.com>
    Signed-off-by: Tomasz Kamiński <tkaminsk@redhat.com>

---
+++ libstdc++-v3/testsuite/23_containers/deque/modifiers/append_range.cc
@@ -0,0 +1,128 @@
+// { dg-do run { target c++23 } }
+
+#include <deque>
+#include <span>
+#include <testsuite_hooks.h>
+#include <testsuite_iterators.h>
+#include <testsuite_allocator.h>
+
+template<typename Range, typename Alloc>
+constexpr void
+do_test()
+{
+  // The deque's value_type.
+  using V = typename std::allocator_traits<Alloc>::value_type;
+
+  // The range's value_type.
+  using T = std::ranges::range_value_t<Range>;
+  T a[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+
+  auto eq = [](const std::deque<V, Alloc>& l, std::span<T> r) {
+    if (l.size() != r.size())
+      return false;
+    for (auto i = 0u; i < l.size(); ++i)
+      if (l[i] != r[i])
+	return false;
+    return true;
+  };
+
+  V const* p[std::size(a)]{};
+  auto stable = [](const std::deque<V, Alloc>& l, std::span<V const*> r) {
+    if (l.size() != r.size())
+      return false;
+    for (auto i = 0u; i < l.size(); ++i)
+    {
+      if (r[i] == nullptr)
+	r[i] = &l[i];
+      else if (&l[i] != r[i])
+	return false;
+    }
+    return true;
+  };
+
+  std::deque<V, Alloc> d;
+  d.append_range(Range(a, a));
+  VERIFY( d.empty() );
+  d.append_range(Range(a, a+4));
+  VERIFY( eq(d, {a, 4}) );
+  VERIFY( stable(d, {p, 4} ) );
+  d.append_range(Range(a+4, a+8));
+  VERIFY( eq(d, {a, 8}) );
+  VERIFY( stable(d, {p, 8}) );
+  d.append_range(Range(a+8, a+14));
+  VERIFY( eq(d, a) );
+  VERIFY( stable(d, p) );
+
+  d.clear();
+  std::ranges::fill(p, nullptr);
+  d.append_range(Range(a, a+4));
+  VERIFY( eq(d, {a, 4}) );
+  VERIFY( stable(d, {p, 4} ) );
+  d.append_range(Range(a+4, a+14));
+  VERIFY( eq(d, a) );
+  VERIFY( stable(d, p) );
+
+  d.clear();
+  std::ranges::fill(p, nullptr);
+  d.append_range(Range(a, a+14));
+  VERIFY( eq(d, a) );
+}
+
+struct EightInBuf
+{
+  EightInBuf(int x) : elems{x}
+  { }
+
+private:
+  int elems[512 / (sizeof(int) * 8)];
+
+  friend constexpr bool operator==(EightInBuf const& lhs, int rhs)
+  { return lhs.elems[0] == rhs; }
+};
+
+template<typename Range>
+void
+do_test_a()
+{
+  do_test<Range, std::allocator<int>>();
+  do_test<Range, __gnu_test::SimpleAllocator<int>>();
+  do_test<Range, std::allocator<EightInBuf>>();
+}
+
+bool
+test_ranges()
+{
+  using namespace __gnu_test;
+
+  do_test_a<test_forward_range<int>>();
+  do_test_a<test_forward_sized_range<int>>();
+  do_test_a<test_sized_range_sized_sent<int, forward_iterator_wrapper>>();
+
+  do_test_a<test_input_range<int>>();
+  do_test_a<test_input_sized_range<int>>();
+  do_test_a<test_sized_range_sized_sent<int, input_iterator_wrapper>>();
+
+  do_test_a<test_range_nocopy<int, input_iterator_wrapper_nocopy>>();
+  do_test_a<test_sized_range<int, input_iterator_wrapper_nocopy>>();
+  do_test_a<test_sized_range_sized_sent<int, input_iterator_wrapper_nocopy>>();
+
+  do_test_a<test_forward_range<short>>();
+  do_test_a<test_input_range<short>>();
+
+  // Not lvalue-convertible to int
+  struct C {
+    C(int v) : val(v) { }
+    operator int() && { return val; }
+    bool operator==(int b) const { return b == val; }
+    int val;
+  };
+  using rvalue_input_range = test_range<C, input_iterator_wrapper_rval>;
+  do_test<rvalue_input_range, std::allocator<int>>();
+
+  return true;
+}
+
+int main()
+{
+  test_ranges();
+}
