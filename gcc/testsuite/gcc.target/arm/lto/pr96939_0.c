927cfea902c330092848bd7a228b714b07d08f6b
Richard Earnshaw <rearnsha@arm.com>
2025-03-25 13:48:06 +0000

    arm: testsuite: avoid dg-options in primary LTO file
    As the primary LTO file in this test, it cannot use dg-options.  Move
    the flags from there to dg-lto-options.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/arm/lto/pr96939_0.c (dg-options):  Delete.  Move the
    	options from here ...
    	(dg-lto-options): ... to here.

--- gcc/testsuite/gcc.target/arm/lto/pr96939_0.c
+++ gcc/testsuite/gcc.target/arm/lto/pr96939_0.c
@@ -1,8 +1,7 @@
 /* PR target/96939 */
 /* { dg-lto-do link } */
 /* { dg-require-effective-target arm_arch_v8a_link } */
-/* { dg-options "-mcpu=unset -march=armv8-a+simd -mfpu=auto" } */
-/* { dg-lto-options { { -flto -O2 } } } */
+/* { dg-lto-options { { -flto -O2 -mcpu=unset -march=armv8-a+simd -mfpu=auto} } } */
 
 extern unsigned crc (unsigned, const void *);
 typedef unsigned (*fnptr) (unsigned, const void *);
