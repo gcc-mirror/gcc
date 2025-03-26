Rust-GCC/gccrs

testsuite: Add more testcases for cfg() in core
gcc/testsuite/ChangeLog:

	* rust/compile/cfg-core1.rs: New test.
	* rust/compile/cfg-core2.rs: New test.

gcc/testsuite/rust/compile/cfg-core1.rs
@@ -1,0 +1,12 @@
+// { dg-additional-options "-frust-cfg=A -frust-cfg=B" }
+
+#[cfg_attr(A, cfg(B))]
+struct Foo0;
+
+#[cfg_attr(A, cfg(C))]
+struct Bar0;
+
+fn main() {
+    let a = Foo0; // { dg-error "cannot find value" }
+    let a = Bar0;
+}

gcc-mirror/gcc

gcc/testsuite/rust/compile/cfg-core1.rs
@@ -1,0 +1,12 @@
+// { dg-additional-options "-frust-cfg=A -frust-cfg=B" }
+
+#[cfg_attr(A, cfg(B))]
+struct Foo0;
+
+#[cfg_attr(A, cfg(C))]
+struct Bar0;
+
+fn main() {
+    let a = Foo0;
+    let a = Bar0; // { dg-error "cannot find value" }
+}