aa3e72f943032e5f074b2bd2fd06d130dda8760b
Thomas Schwinge <tschwinge@baylibre.com>
2025-03-27 23:06:37 +0100

    Add test cases for exception handling constructs in dead code for GCN, nvptx target and OpenMP 'target' offloading [PR118794]
    	PR target/118794
    	gcc/testsuite/
    	* g++.target/gcn/exceptions-pr118794-1.C: New.
    	* g++.target/nvptx/exceptions-pr118794-1.C: Likewise.
    	libgomp/
    	* testsuite/libgomp.c++/target-exceptions-pr118794-1.C: New.
    	* testsuite/libgomp.c++/target-exceptions-pr118794-1-offload-sorry-GCN.C:
    	Likewise.
    	* testsuite/libgomp.c++/target-exceptions-pr118794-1-offload-sorry-nvptx.C:
    	Likewise.

---
+++ libgomp/testsuite/libgomp.c++/target-exceptions-pr118794-1-offload-sorry-GCN.C
@@ -0,0 +1,23 @@
+/* Exception handling constructs in dead code.  */
+
+/* As this test case involves an expected offload compilation failure, we have to handle each offload target individually.
+   { dg-do link { target offload_target_amdgcn } }
+   { dg-additional-options -foffload=amdgcn-amdhsa } */
+/* { dg-require-effective-target exceptions }
+   { dg-additional-options -fexceptions } */
+/* { dg-additional-options -O0 } */
+/* { dg-additional-options -fdump-tree-optimized-raw }
+   { dg-additional-options -foffload-options=-fdump-tree-optimized-raw } */
+
+#include "target-exceptions-pr118794-1.C"
+
+/* In this specific C++ arrangement, distilled from PR118794, GCC synthesizes
+   '__builtin_eh_pointer', '__builtin_unwind_resume' calls as dead code in 'f':
+   { dg-final { scan-tree-dump-times {gimple_call <__builtin_eh_pointer, } 1 optimized } }
+   { dg-final { scan-tree-dump-times {gimple_call <__builtin_unwind_resume, } 1 optimized } }
+   { dg-final { only_for_offload_target amdgcn-amdhsa scan-offload-tree-dump-times {gimple_call <__builtin_eh_pointer, } 1 optimized } }
+   { dg-final { only_for_offload_target amdgcn-amdhsa scan-offload-tree-dump-times {gimple_call <__builtin_unwind_resume, } 1 optimized } }
+   Given '-O0', offload compilation fails:
+   { dg-regexp {[^\r\n]+: In function 'f':[\r\n]+(?:[^\r\n]+: sorry, unimplemented: exception handling not supported[\r\n]+)+} }
+   (Note, using 'dg-regexp' instead of 'dg-message', as the former runs before the auto-mark-UNSUPPORTED.)
+   { dg-excess-errors {'mkoffload' failure etc.} } */
---
+++ libgomp/testsuite/libgomp.c++/target-exceptions-pr118794-1.C
@@ -0,0 +1,58 @@
+/* Exception handling constructs in dead code.  */
+
+/* { dg-require-effective-target exceptions }
+   { dg-additional-options -fexceptions } */
+/* { dg-additional-options -foffload=disable }
+   Offloading compilation not yet supported; see
+   'target-exceptions-pr118794-1-offload-sorry-GCN.C',
+   'target-exceptions-pr118794-1-offload-sorry-nvptx.C'.  */
+/* { dg-additional-options -O0 } */
+/* { dg-additional-options -fdump-tree-optimized-raw } */
+
+/* See also '../../../gcc/testsuite/g++.target/gcn/exceptions-pr118794-1.C',
+   '../../../gcc/testsuite/g++.target/nvptx/exceptions-pr118794-1.C'.  */
+
+#pragma omp begin declare target
+
+bool ok = false;
+
+template <typename T>
+struct C
+{
+  C()
+  {
+    ok = true;
+  }
+  C(int) {};
+  ~C() {};
+
+  __attribute__((noipa))
+  void m()
+  {
+    C c;
+  }
+};
+
+inline void f()
+{
+  C<double> c(1);
+  c.m();
+}
+
+#pragma omp end declare target
+
+int main()
+{
+#pragma omp target
+  {
+    f();
+  }
+#pragma omp target update from(ok)
+  if (!ok)
+    __builtin_abort();
+}
+
+/* In this specific C++ arrangement, distilled from PR118794, GCC synthesizes
+   '__builtin_eh_pointer', '__builtin_unwind_resume' calls as dead code in 'f':
+   { dg-final { scan-tree-dump-times {gimple_call <__builtin_eh_pointer, } 1 optimized } }
+   { dg-final { scan-tree-dump-times {gimple_call <__builtin_unwind_resume, } 1 optimized } } */
