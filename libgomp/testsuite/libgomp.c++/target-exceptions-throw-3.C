6c0ea840265454b01d9ef4eb8850a4f015788788
Thomas Schwinge <tschwinge@baylibre.com>
2025-03-27 14:46:20 +0100

    Add 'throw', dead code test cases for GCN, nvptx target and OpenACC, OpenMP 'target' offloading
    	gcc/testsuite/
    	* g++.target/gcn/exceptions-throw-3.C: New.
    	* g++.target/nvptx/exceptions-throw-3.C: Likewise.
    	libgomp/
    	* testsuite/libgomp.c++/target-exceptions-throw-3.C: New.
    	* testsuite/libgomp.oacc-c++/exceptions-throw-3.C: Likewise.

---
+++ libgomp/testsuite/libgomp.c++/target-exceptions-throw-3.C
@@ -0,0 +1,19 @@
+/* 'throw' in OpenMP 'target' region, dead code.  */
+
+/* { dg-require-effective-target exceptions }
+   { dg-additional-options -fexceptions } */
+/* { dg-additional-options -DDEFAULT=defaultmap(to) }
+   ... to avoid wrong code for offloading execution; PR119692.
+   With this, the device code still isn't correct, but the defects are in dead code.
+   { dg-additional-options -fdump-tree-gimple } */
+/* { dg-additional-options -fdump-tree-optimized-raw }
+   { dg-additional-options -foffload-options=-fdump-tree-optimized-raw } */
+
+#include "../libgomp.oacc-c++/exceptions-throw-3.C"
+
+/* { dg-final { scan-tree-dump-not {(?n)#pragma omp target .* defaultmap\(to\) map\(to:_ZTI11MyException \[len: [0-9]+\] \[runtime_implicit\]\)$} gimple { xfail *-*-* } } } */
+
+/* { dg-final { scan-tree-dump-times {gimple_call <__cxa_allocate_exception, } 1 optimized } }
+   { dg-final { scan-tree-dump-times {gimple_call <__cxa_throw, } 1 optimized } }
+   { dg-final { scan-offload-tree-dump-times {gimple_call <__cxa_allocate_exception, } 1 optimized } }
+   { dg-final { scan-offload-tree-dump-times {gimple_call <__cxa_throw, } 1 optimized } } */
