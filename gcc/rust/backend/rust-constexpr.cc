a0b0d2a58ec8b830d0a88039709e2002b32dc2a8
Philip Herron <herron.philip@googlemail.com>
2025-03-28 16:59:33 +0000

    gccrs: Fix ICE in array ref constexpr
    Since 898d55ad7e2 was fixed to remove the VIEW_CONVERT_EXPR from
    array expressions we can now turn on the array element access
    const expr.
    
    Fixes Rust-GCC#3563
    
    gcc/rust/ChangeLog:
    
    	* backend/rust-constexpr.cc (eval_store_expression): turn this back on
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/issue-3563.rs: New test.
    
    Signed-off-by: Philip Herron <herron.philip@googlemail.com>

--- gcc/rust/backend/rust-constexpr.cc
+++ gcc/rust/backend/rust-constexpr.cc
@@ -2697,10 +2697,8 @@ eval_store_expression (const constexpr_ctx *ctx, tree t, bool lval,
 	      }
 	    if (TREE_CODE (probe) == ARRAY_REF)
 	      {
-		// TODO
-		rust_unreachable ();
-		// elt = eval_and_check_array_index (ctx, probe, false,
-		// 				  non_constant_p, overflow_p);
+		elt = eval_and_check_array_index (ctx, probe, false,
+						  non_constant_p, overflow_p);
 		if (*non_constant_p)
 		  return t;
 	      }
