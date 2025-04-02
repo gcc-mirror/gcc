a179c05f228cace7bcaba8c550e0550993ff5a46
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2025-04-02 18:20:54 +0200

    Adapt testcase to name resolution 2.0
    Name resolution 2.0 behavior matches more closely rustc's behavior
    and trying to prevent undeclared label error emission would break some
    other test.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/invalid_label_name.rs: Adapt test to nr2.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

--- gcc/testsuite/rust/compile/invalid_label_name.rs
+++ gcc/testsuite/rust/compile/invalid_label_name.rs
@@ -1,20 +1,24 @@
+// { dg-additional-options "-frust-name-resolution-2.0" }
 pub fn function() {
     'continue: loop {
         // { dg-error "invalid label name .'continue." "" { target *-*-* } .-1 }
         break 'extern;
         // { dg-error "invalid label name .'extern." "" { target *-*-* } .-1 }
+        // { dg-error "use of undeclared label .'extern." "" { target *-*-* } .-2 }
     }
 
     'break: loop {
         // { dg-error "invalid label name .'break." "" { target *-*-* } .-1 }
         break 'for;
         // { dg-error "invalid label name .'for." "" { target *-*-* } .-1 }
+        // { dg-error "use of undeclared label .'for." "" { target *-*-* } .-2 }
     }
 
     'crate: loop {
         // { dg-error "invalid label name .'crate." "" { target *-*-* } .-1 }
         break 'loop;
         // { dg-error "invalid label name .'loop." "" { target *-*-* } .-1 }
+        // { dg-error "use of undeclared label .'loop." "" { target *-*-* } .-2 }
     }
 
     'a: loop {
