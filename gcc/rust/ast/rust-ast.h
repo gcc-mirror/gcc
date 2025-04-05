a65c3b497fd0ca28f35cb7e63f5a1ce95baff8ad
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2025-04-06 01:44:18 +0200

    gccrs: Add hash function for Identifiers
    gcc/rust/ChangeLog:
    
    	* ast/rust-ast.h: Add hash function.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

--- gcc/rust/ast/rust-ast.h
+++ gcc/rust/ast/rust-ast.h
@@ -2102,6 +2102,19 @@ template <> struct less<Rust::Identifier>
     return lhs.as_string () < rhs.as_string ();
   }
 };
+
+template <> struct hash<Rust::Identifier>
+{
+  std::size_t operator() (const Rust::Identifier &k) const
+  {
+    using std::hash;
+    using std::size_t;
+    using std::string;
+
+    return hash<string> () (k.as_string ()) ^ (hash<int> () (k.get_locus ()));
+  }
+};
+
 } // namespace std
 
 #endif
