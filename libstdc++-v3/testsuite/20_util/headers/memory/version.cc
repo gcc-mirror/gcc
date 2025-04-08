0d46cee2fe38b9a1aa576c8031ed22d298d88954
Dhruv Chawla <dhruvc@nvidia.com>
2025-04-08 01:01:24 -0700

    libstdc++: Add missing feature-test macro in <memory>
    Per version.syn#2, <memory> is required to define
    __cpp_lib_addressof_constexpr as 201603L.
    
    Bootstrapped and tested on aarch64-linux-gnu.
    
    Signed-off-by: Dhruv Chawla <dhruvc@nvidia.com>
    
    libstdc++-v3/ChangeLog:
    	* include/std/memory: Define __glibcxx_want_addressof_constexpr.
    	* testsuite/20_util/headers/memory/version.cc: Test for macro
    	value.
    
    (cherry picked from commit 0e65fef8717f404cf9c85bff51bf87d534f87828)

--- libstdc++-v3/testsuite/20_util/headers/memory/version.cc
+++ libstdc++-v3/testsuite/20_util/headers/memory/version.cc
@@ -6,3 +6,7 @@
 #if __cpp_lib_allocator_traits_is_always_equal != 201411L
 # error "Feature-test macro __cpp_lib_allocator_traits_is_always_equal has wrong value in <version>"
 #endif
+
+#if __cpp_lib_addressof_constexpr != 201603L
+# error "Feature-test macro __cpp_lib_addressof_constexpr has wrong value in <version>"
+#endif
