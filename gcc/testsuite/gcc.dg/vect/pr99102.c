c3e1c23e8bd80284c4b7206ec260bb273d86da22
Richard Sandiford <richard.sandiford@arm.com>
2025-04-08 22:03:39 +0100

    testsuite: Fix gcc.dg/vect/pr99102.c command line
    The aarch64_sve256_hw line forced the vector length, but didn't force
    SVE itself.  This meant that the associated:
    
      /* { dg-final { scan-tree-dump "MASK_SCATTER_STORE" "vect"  { target aarch64_sve256_hw } } } */
    
    wouldn't always fire.  I imagine this was tested with SVE enabled by
    default, which would have masked the problem.
    
    gcc/testsuite/
    	* gcc.dg/vect/pr99102.c: Force SVE when forcing the vector length.

--- gcc/testsuite/gcc.dg/vect/pr99102.c
+++ gcc/testsuite/gcc.dg/vect/pr99102.c
@@ -1,5 +1,5 @@
 /* { dg-additional-options "-O2 -ftree-vectorize -fno-vect-cost-model -fdump-tree-vect-details" } */
-/* { dg-additional-options "-msve-vector-bits=256" { target aarch64_sve256_hw } } */
+/* { dg-additional-options "-march=armv8-a+sve -msve-vector-bits=256" { target aarch64_sve256_hw } } */
 long a[44];
 short d, e = -7;
 __attribute__((noipa)) void b(char f, short j, short k, unsigned l) {
