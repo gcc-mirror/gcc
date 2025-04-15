3d03428400552156abd0d639dfc8fe9bb00158cb
Jonathan Wakely <jwakely@redhat.com>
2025-04-15 14:01:55 +0100

    libstdc++: Add test for not using reserved name 'ranges' before C++20
    This is a test for a bug that was present on trunk, because 'ranges' is
    not a reserved name before C++20.
    
    libstdc++-v3/ChangeLog:
    
    	* testsuite/17_intro/names.cc: Check ranges is not used as an
    	identifier before C++20.

--- libstdc++-v3/testsuite/17_intro/names.cc
+++ libstdc++-v3/testsuite/17_intro/names.cc
@@ -140,6 +140,10 @@
 #define try_emplace (
 #endif
 
+#if __cplusplus < 202002L
+#define ranges (
+#endif
+
 // These clash with newlib so don't use them.
 # define __lockable		cannot be used as an identifier
 # define __null_sentinel	cannot be used as an identifier
