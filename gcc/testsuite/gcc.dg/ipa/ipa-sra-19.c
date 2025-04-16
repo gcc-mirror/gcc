7b9d8d43154efcb56cee1787e3267183dd6a372e
Alexandre Oliva <oliva@adacore.com>
2025-04-16 19:30:48 -0300

    [testsuite] [ppc] ipa-sra-19.c: pass -Wno-psabi on powerpc-*-elf as well
    Like other ppc targets, powerpc-*-elf needs -Wno-psabi to compile
    gcc.dg/ipa/ipa-sra-19.c without an undesired warning about vector
    argument passing.
    
    
    for  gcc/testsuite/ChangeLog
    
    	* gcc.dg/ipa/ipa-sra-19.c: Add -Wno-psabi on ppc-elf too.

--- gcc/testsuite/gcc.dg/ipa/ipa-sra-19.c
+++ gcc/testsuite/gcc.dg/ipa/ipa-sra-19.c
@@ -1,7 +1,7 @@
 /* { dg-do compile } */
 /* { dg-options "-O2"  } */
 /* { dg-additional-options "-msse2" { target ia32 } } */
-/* { dg-additional-options "-Wno-psabi" { target powerpc-ibm-aix* powerpc-wrs-vxworks* } } */
+/* { dg-additional-options "-Wno-psabi" { target powerpc-ibm-aix* powerpc-wrs-vxworks* powerpc-*-elf } } */
 
 typedef int __attribute__((__vector_size__(16))) vectype;
 
