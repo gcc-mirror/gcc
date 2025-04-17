533c13aeb4a394853884f43ed43d53e4589108c5
Philip Herron <herron.philip@googlemail.com>
2025-04-17 13:50:55 +0100

    gccrs: Fix ICE when handling case of unknown field in HIR::FieldAccess
    We were wrongly adding the assertion that this must not be an enum but
    this is a pointless assertion we only care that there are variant in the
    ADT and if the field exists in the first variant.
    
    Fixes Rust-GCC#3581
    
    gcc/rust/ChangeLog:
    
    	* typecheck/rust-hir-type-check-expr.cc (TypeCheckExpr::visit): fix bad assertion
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/nonexistent-field.rs: fix bad error message
    	* rust/compile/issue-3581-1.rs: New test.
    	* rust/compile/issue-3581-2.rs: New test.
    
    Signed-off-by: Philip Herron <herron.philip@googlemail.com>

--- gcc/testsuite/rust/compile/nonexistent-field.rs
+++ gcc/testsuite/rust/compile/nonexistent-field.rs
@@ -6,7 +6,7 @@ fn main() {
 
     let s = StructWithFields { x: 0 };
     s.foo;
-    // { dg-error "no field .foo. on type .StructWithFields.StructWithFields .x.u32... .E0609." "" { target *-*-* } .-1 }
+    // { dg-error "no field .foo. on type .StructWithFields. .E0609." "" { target *-*-* } .-1 }
 
     let numbers = (1, 2, 3);
     numbers.3;
