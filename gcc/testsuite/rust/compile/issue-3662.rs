838b583c6600758d2a2f537c8379ff69e336a848
Philip Herron <herron.philip@googlemail.com>
2025-04-17 16:29:05 +0100

    gccrs: Add test case to show ICE is fixed
    Fixes Rust-GCC#3662
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/issue-3662.rs: New test.
    
    Signed-off-by: Philip Herron <herron.philip@googlemail.com>

---
+++ gcc/testsuite/rust/compile/issue-3662.rs
@@ -0,0 +1,8 @@
+pub fn rlib() {
+    let _ = ((-1 as i8) << 8 - 1) as f32;
+    let _ = 0u8 as char;
+    let _ = true > false;
+    let _ = true >= false;
+    let _ = true < false;
+    let _ = true >= false;
+}
