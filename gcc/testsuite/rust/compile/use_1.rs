f84a0888c14025dbef90f9c74256a3ec255cb0bd
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2025-04-24 14:28:59 +0200

    gccrs: Change expected error output to match nr2
    Name resolution 2.0 message describes the context around the unresolved
    items and should therefore be kept.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/nr2/exclude: Remove test from exclusion list.
    	* rust/compile/use_1.rs: Change expected output and remove test from
    	nr1.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

--- gcc/testsuite/rust/compile/use_1.rs
+++ gcc/testsuite/rust/compile/use_1.rs
@@ -1,7 +1,8 @@
+// { dg-additional-options "-frust-name-resolution-2.0" }
 mod frob {}
 
-use foo::bar::baz; // { dg-error "cannot find simple path segment .foo." }
-use frob::ulator; // { dg-error "cannot find simple path segment .ulator." }
+use foo::bar::baz; // { dg-error "unresolved import .foo::bar::baz." }
+use frob::ulator; // { dg-error "unresolved import .frob::ulator." }
 
 mod sain {
     mod doux {}
@@ -9,8 +10,8 @@ mod sain {
     mod dron {}
 }
 
-use not_sain::*; // { dg-error "cannot find simple path segment .not_sain." }
+use not_sain::*; // { dg-error "unresolved import .not_sain." }
 
 use sain::*;
 use sain::{doux, dron};
-use sain::{doux, dron, graal}; // { dg-error "cannot find simple path segment .graal." }
+use sain::{doux, dron, graal}; // { dg-error "unresolved import .sain::graal." }
