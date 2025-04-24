8396c80be153b5207d992b7ea6dd4fdb536ce973
Jonathan Wakely <jwakely@redhat.com>
2025-04-24 14:50:36 +0100

    libstdc++: Remove unnecessary dg-prune-output from tests
    There are no errors matching this pattern in these tests (only in the
    deque/48101_neg.cc and vector/48101_neg.cc tests).
    
    libstdc++-v3/ChangeLog:
    
    	* testsuite/23_containers/forward_list/48101_neg.cc: Remove
    	dg-prune-output that doesn't match anything.
    	* testsuite/23_containers/list/48101_neg.cc: Likewise.
    	* testsuite/23_containers/multiset/48101_neg.cc: Likewise.
    	* testsuite/23_containers/set/48101_neg.cc: Likewise.

--- libstdc++-v3/testsuite/23_containers/set/48101_neg.cc
+++ libstdc++-v3/testsuite/23_containers/set/48101_neg.cc
@@ -29,7 +29,6 @@ test01()
 
 // { dg-error "non-const, non-volatile value_type" "" { target *-*-* } 0 }
 // { dg-error "comparison object must be invocable" "" { target *-*-* } 0 }
-// { dg-prune-output "std::allocator<.* has no member named " }
 // { dg-prune-output "must have the same value_type as its allocator" }
 // { dg-prune-output "no match for call" }
 // { dg-prune-output "invalid conversion" }
