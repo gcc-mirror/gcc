1ca1c1fc3b58ae5e1d3db4f5a2014132fe69f82a
Georg-Johann Lay <avr@gjlay.de>
2025-04-30 08:43:51 +0200

    AVR: target/119989 - Add missing clobbers to xload_<mode>_libgcc.
    libgcc's __xload_1...4 is clobbering Z (and also R21 is some cases),
    but avr.md had clobbers of respective GPRs only up to reload.
    Outcome was that code reading from the same __memx address twice
    could be wrong.  This patch adds respective clobbers.
    
    gcc/
    	* config/avr/avr.md (xload_<mode>_libgcc): Clobber R21, Z.
    
    gcc/testsuite/
    	* gcc.target/avr/torture/pr119989.h: New file.
    	* gcc.target/avr/torture/pr119989-memx-1.c: New test.
    	* gcc.target/avr/torture/pr119989-memx-2.c: New test.
    	* gcc.target/avr/torture/pr119989-memx-3.c: New test.
    	* gcc.target/avr/torture/pr119989-memx-4.c: New test.

---
+++ gcc/testsuite/gcc.target/avr/torture/pr119989-memx-3.c
@@ -0,0 +1,6 @@
+/* { dg-do run { target { ! avr_tiny } } } */
+/* { dg-additional-options "-std=gnu99" } */
+
+__extension__ typedef __uint24 TYP;
+
+#include "pr119989.h"
---
+++ gcc/testsuite/gcc.target/avr/torture/pr119989-memx-4.c
@@ -0,0 +1,6 @@
+/* { dg-do run { target { ! avr_tiny } } } */
+/* { dg-additional-options "-std=gnu99" } */
+
+typedef __UINT32_TYPE__ TYP;
+
+#include "pr119989.h"
