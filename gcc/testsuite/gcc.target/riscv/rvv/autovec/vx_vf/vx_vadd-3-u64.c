b3a32804bebb9520b327b7cbf3e8f8b4730f9bd6
Pan Li <pan2.li@intel.com>
2025-05-03 11:37:09 +0800

    RISC-V: Add testcases for vec_duplicate + vadd.vv combine when GR2VR cost 15
    Add asm dump check and for vec_duplicate + vadd.vv combine to vadd.vx.
    The late-combine will not take action when GR2VR cost is 15.
    
    The below test suites are passed for this patch.
    * The rv64gcv fully regression test.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_vadd-3-i16.c: New test.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_vadd-3-i32.c: New test.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_vadd-3-i64.c: New test.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_vadd-3-i8.c: New test.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_vadd-3-u16.c: New test.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_vadd-3-u32.c: New test.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_vadd-3-u64.c: New test.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_vadd-3-u8.c: New test.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

---
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/vx_vf/vx_vadd-3-u64.c
@@ -0,0 +1,8 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gcv -mabi=lp64d --param=gpr2vr-cost=15" } */
+
+#include "vx_binary.h"
+
+DEF_VX_BINARY(uint64_t, +)
+
+/* { dg-final { scan-assembler-not {vadd.vx} } } */
