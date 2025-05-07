7b53b88381179c5c8152bcb890460f66d9c88fac
Thomas Schwinge <tschwinge@baylibre.com>
2025-05-07 16:02:16 +0200

    nvptx: Support '-march=sm_61'
    	gcc/
    	* config/nvptx/nvptx-sm.def: Add '61'.
    	* config/nvptx/nvptx-gen.h: Regenerate.
    	* config/nvptx/nvptx-gen.opt: Likewise.
    	* config/nvptx/nvptx.cc (first_ptx_version_supporting_sm): Adjust.
    	* config/nvptx/nvptx.opt (-march-map=sm_61, -march-map=sm_62):
    	Likewise.
    	* config.gcc: Likewise.
    	* doc/invoke.texi (Nvidia PTX Options): Document '-march=sm_61'.
    	* config/nvptx/gen-multilib-matches-tests: Extend.
    	gcc/testsuite/
    	* gcc.target/nvptx/march-map=sm_61.c: Adjust.
    	* gcc.target/nvptx/march-map=sm_62.c: Likewise.
    	* gcc.target/nvptx/march=sm_61.c: New.
    	libgomp/
    	* testsuite/libgomp.c/declare-variant-3-sm61.c: New.
    	* testsuite/libgomp.c/declare-variant-3.h: Adjust.

--- gcc/testsuite/gcc.target/nvptx/march=sm_61.c
+++ gcc/testsuite/gcc.target/nvptx/march=sm_61.c
@@ -1,5 +1,5 @@
 /* { dg-do assemble } */
-/* { dg-options {-march-map=sm_62 -mptx=_} } */
+/* { dg-options {-march=sm_61 -mptx=_} } */
 /* { dg-additional-options -save-temps } */
 /* { dg-final { scan-assembler-times {(?n)^	\.version	7\.3$} 1 } } */
 /* { dg-final { scan-assembler-times {(?n)^	\.target	sm_61$} 1 } } */
