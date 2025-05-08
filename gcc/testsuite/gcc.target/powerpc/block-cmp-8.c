319c472fdabc9bed84676dd0a95f1e3ef633bb06
Alexandre Oliva <oliva@adacore.com>
2025-05-08 02:17:38 -0300

    [testsuite] [ppc] block-cmp-8 should require powerpc64
    gcc.target/powerpc/block-cmp-8.c is an execution test on ilp32.  It
    tests for support for the 64-bit ISA in the compiler, but not for the
    ability to execute powerpc64 instructions, so the test fails on 32-bit
    hardware.  Require powerpc64 instead.
    
    
    for  gcc/testsuite/ChangeLog
    
    	* gcc.target/powerpc/block-cmp-8.c: Require powerpc64
    	instruction execution support.

--- gcc/testsuite/gcc.target/powerpc/block-cmp-8.c
+++ gcc/testsuite/gcc.target/powerpc/block-cmp-8.c
@@ -1,6 +1,6 @@
 /* { dg-do run { target ilp32 } } */
 /* { dg-options "-O2 -mpowerpc64" } */
-/* { dg-require-effective-target has_arch_ppc64 } */
+/* { dg-require-effective-target powerpc64 } */
 /* { dg-timeout-factor 2 } */
 
 /* Verify memcmp on m32 mpowerpc64 */
