621cb3dc9996a160e3e65bce4a8e61da26caafa7
Pan Li <pan2.li@intel.com>
2025-05-13 22:32:03 +0800

    RISC-V: Add test for vec_duplicate + vsub.vv combine case 1 with GR2VR cost 0
    Add asm dump check test for vec_duplicate + vsub.vv combine to vsub.vx.
    
    The below test suites are passed for this patch.
    * The rv64gcv fully regression test.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-4-i16.c: Add test cases
    	for vsub vx combine case 1 with GR2VR cost 0.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-4-i32.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-4-i64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-4-i8.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-4-u16.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-4-u32.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-4-u64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-4-u8.c: Ditto.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

--- gcc/testsuite/gcc.target/riscv/rvv/autovec/vx_vf/vx-4-i8.c
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/vx_vf/vx-4-i8.c
@@ -4,5 +4,7 @@
 #include "vx_binary.h"
 
 DEF_VX_BINARY_CASE_1(int8_t, +, add, VX_BINARY_BODY_X16)
+DEF_VX_BINARY_CASE_1(int8_t, -, sub, VX_BINARY_BODY_X16)
 
 /* { dg-final { scan-assembler {vadd.vx} } } */
+/* { dg-final { scan-assembler {vsub.vx} } } */
