0babc6f29f7c29215674b3ee58d5dd17ce7b3849
Luc Grosheintz <luc.grosheintz@gmail.com>
2025-05-14 21:13:52 +0200

    libstdc++: Fix class mandate for extents.
    The standard states that the IndexType must be a signed or unsigned
    integer. This mandate was implemented using `std::is_integral_v`. Which
    also includes (among others) char and bool, which neither signed nor
    unsigned integers.
    
    libstdc++-v3/ChangeLog:
    
    	* include/std/mdspan: Implement the mandate for extents as
    	signed or unsigned integer and not any interal type. Remove
    	leading underscores from names in static_assert message.
    	* testsuite/23_containers/mdspan/extents/class_mandates_neg.cc:
    	Check that extents<char,...> and extents<bool,...> are invalid.
    	Adjust dg-prune-output pattern.
    	* testsuite/23_containers/mdspan/extents/misc.cc: Update
    	tests to avoid `char` and `bool` as IndexType.
    
    Reviewed-by: Tomasz Kamiński <tkaminsk@redhat.com>
    Reviewed-by: Jonathan Wakely <jwakely@redhat.com>

--- libstdc++-v3/testsuite/23_containers/mdspan/extents/class_mandates_neg.cc
+++ libstdc++-v3/testsuite/23_containers/mdspan/extents/class_mandates_neg.cc
@@ -1,8 +1,12 @@
 // { dg-do compile { target c++23 } }
 #include<mdspan>
 
-std::extents<char, size_t(1) << 9> e1; // { dg-error "from here" }
-std::extents<double, 1> e2;            // { dg-error "from here" }
-// { dg-prune-output "dynamic or representable as _IndexType" }
-// { dg-prune-output "must be integral" }
+#include <cstdint>
+
+std::extents<uint8_t, size_t(1) << 9> e1; // { dg-error "from here" }
+std::extents<char, 1> e2;                 // { dg-error "from here" }
+std::extents<bool, 1> e3;                 // { dg-error "from here" }
+std::extents<double, 1> e4;               // { dg-error "from here" }
+// { dg-prune-output "dynamic or representable as IndexType" }
+// { dg-prune-output "signed or unsigned integer" }
 // { dg-prune-output "invalid use of incomplete type" }
