a2328db215cea44c2b3ad57a533b900723f43c10
Iain Sandoe <iain@sandoe.co.uk>
2025-05-29 16:50:44 +0100

    c++, coroutines: Improve diagnostics for awaiter/promise.
    At present, we can issue diagnostics about missing or malformed
    awaiter or promise methods when we encounter their uses in the
    body of a user's function.  We might then re-issue the same
    diagnostics when processing the initial or final await expressions.
    
    This change avoids such duplication, and also attempts to
    identify issues with the initial or final expressions specifically
    since diagnostics for those do not have any useful line number.
    
    gcc/cp/ChangeLog:
    
    	* coroutines.cc (build_co_await): Identify diagnostics
    	for initial and final await expressions.
    	(cp_coroutine_transform::wrap_original_function_body): Do
    	not handle initial and final await expressions here ...
    	(cp_coroutine_transform::apply_transforms): ... handle them
    	here and avoid duplicate diagnostics.
    	* coroutines.h: Declare inital and final await expressions
    	in the transform class.  Save the function closing brace
    	location.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/coroutines/coro1-missing-await-method.C: Adjust for
    	improved diagnostics.
    	* g++.dg/coroutines/coro-missing-final-suspend.C: Likewise.
    	* g++.dg/coroutines/pr104051.C: Move to...
    	* g++.dg/coroutines/pr104051-0.C: ...here.
    	* g++.dg/coroutines/pr104051-1.C: New test.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>
    (cherry picked from commit 4c014a9db521b24bd900eb9a6ac70988322a57da)

--- gcc/testsuite/g++.dg/coroutines/pr104051-1.C
+++ gcc/testsuite/g++.dg/coroutines/pr104051-1.C
@@ -1,21 +1,15 @@
 // { dg-additional-options "-fsyntax-only" }
 // { dg-skip-if "requires hosted libstdc++ for vector" { ! hostedlib } }
 #include <coroutine>
-#include <vector>
-template <typename> struct promise {
-  struct final_awaitable {
-    bool await_ready() noexcept;
-    template <typename Promise>
-    std::coroutine_handle<>
-        await_suspend(std::coroutine_handle<Promise>) noexcept;
-    void await_resume() noexcept;
-  };
+template <typename>
+struct promise {
   auto get_return_object() {
     return std::coroutine_handle<promise>::from_promise(*this);
   }
-  auto initial_suspend() { return std::suspend_always(); }
+  auto initial_suspend() { return 42.0; }
   auto final_suspend() noexcept { return true; }
   void unhandled_exception();
+  void return_void ();
 };
 template <typename T> struct task {
   using promise_type = promise<T>;
@@ -24,7 +18,6 @@ template <typename T> struct task {
   std::coroutine_handle<> await_suspend(std::coroutine_handle<>);
   T await_resume();
 };
-task<std::vector<int>> foo() { // { dg-error {awaitable type 'bool' is not a structure} }
-  while ((co_await foo()).empty())
-    ;
-}
+task<double> foo() { // { dg-error {'initial_suspend\(\)' awaitable type 'double' is not a structure} }
+  co_return;
+} 
