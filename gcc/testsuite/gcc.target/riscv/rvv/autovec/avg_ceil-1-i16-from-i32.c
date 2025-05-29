72972bc3a1b35bcfc83d801a9da45121210bc3c3
Pan Li <pan2.li@intel.com>
2025-05-29 21:33:44 +0800

    RISC-V: Add test cases for avg_ceil vaadd implementation
    Add asm and run testcase for avg_ceil vaadd implementation.
    
    The below test suites are passed for this patch series.
    * The rv64gcv fully regression test.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/avg.h: Add test helper macros.
    	* gcc.target/riscv/rvv/autovec/avg_data.h: Add test data for
    	avg_ceil.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-1-i16-from-i32.c: New test.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-1-i16-from-i64.c: New test.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-1-i32-from-i64.c: New test.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-1-i8-from-i16.c: New test.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-1-i8-from-i32.c: New test.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-1-i8-from-i64.c: New test.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-run-1-i16-from-i32.c: New test.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-run-1-i16-from-i64.c: New test.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-run-1-i32-from-i64.c: New test.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-run-1-i8-from-i16.c: New test.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-run-1-i8-from-i32.c: New test.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-run-1-i8-from-i64.c: New test.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

---
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/avg_ceil-1-i16-from-i32.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gcv -mabi=lp64d" } */
+
+#include "avg.h"
+
+#define NT int16_t
+#define WT int32_t
+
+DEF_AVG_1(NT, WT, avg_ceil)
+
+/* { dg-final { scan-assembler-times {csrwi\s*vxrm,\s*0} 1 } } */
+/* { dg-final { scan-assembler-times {vaadd.vv} 1 } } */
