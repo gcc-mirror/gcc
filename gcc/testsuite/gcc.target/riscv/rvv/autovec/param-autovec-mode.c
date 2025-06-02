d3ddc93a9daa0924cf939508d192999487a9dd4c
Liao Shihua <shihua@iscas.ac.cn>
2025-06-01 21:11:25 -0600

    [PATCH] testsuite: RISC-V: Fix the typo in param-autovec-mode.c
    This patch fixes the typo in the test case `param-autovec-mode.c` in the RISC-V
    autovec testsuite.
    
    The option `autovec-mode` is changed to `riscv-autovec-mode` to match the
    expected parameter name.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/param-autovec-mode.c: Change
    	`autovec-mode` to `riscv-autovec-mode` in dg-options.

--- gcc/testsuite/gcc.target/riscv/rvv/autovec/param-autovec-mode.c
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/param-autovec-mode.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-O3 -march=rv64gcv -mabi=lp64d --param=autovec-mode=V4QI -fdump-tree-vect-details" } */
+/* { dg-options "-O3 -march=rv64gcv -mabi=lp64d --param=riscv-autovec-mode=V4QI -fdump-tree-vect-details" } */
 
 /* By default we will use RVVM1SI mode for vectorization because N is not
    known.  Check that we use V4QI and create an epilogue when the autovec-mode
