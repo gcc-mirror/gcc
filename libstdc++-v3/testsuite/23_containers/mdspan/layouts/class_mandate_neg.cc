90f7bbfe2219770ac8b25d0f99320ed3a4fd7736
Tomasz Kamiński <tkaminsk@redhat.com>
2025-06-05 10:40:10 +0200

    libstdc++: Uglify __mapping_alike template parameter and fix test and typo in comment.
    When the static assert was generated from instantiations of default member
    initializer of class B, the error was not generated for B<1, std::layout_left,
    std::layout_left> case, only when -D_GLIBCXX_DEBUG was set. Changing B calls to
    functions fixes that.
    
    We also replace class with typename in template head of layout_right::mapping
    constructors.
    
    libstdc++-v3/ChangeLog:
    
    	* include/std/mdspan (__mdspan::__mapping_alike): Rename template
    	parameter from M to _M_p.
    	(layout_right::mapping): Replace class with typename in template
    	head.
    	(layout_stride::mapping): Fix typo in comment.
    	* testsuite/23_containers/mdspan/layouts/class_mandate_neg.cc:
    	Changed B to function.
    
    Reviewed-by: Jonathan Wakely <jwakely@redhat.com>
    Signed-off-by: Tomasz Kamiński <tkaminsk@redhat.com>

--- libstdc++-v3/testsuite/23_containers/mdspan/layouts/class_mandate_neg.cc
+++ libstdc++-v3/testsuite/23_containers/mdspan/layouts/class_mandate_neg.cc
@@ -18,7 +18,8 @@ template<typename Layout>
   };
 
 template<size_t Count, typename Layout, typename OLayout>
-  struct B
+  bool
+  B()
   {
     using Extents = std::extents<uint8_t, dyn, dyn, Count>;
     using OExtents = std::extents<uint16_t, n, 4, Count>;
@@ -27,20 +28,21 @@ template<size_t Count, typename Layout, typename OLayout>
     using OMapping = typename OLayout::mapping<OExtents>;
 
     Mapping m{OMapping{}};
+    return true;
   };
 
 A<std::layout_left> a_left;                      // { dg-error "required from" }
 A<std::layout_right> a_right;                    // { dg-error "required from" }
 A<std::layout_stride> a_stride;                  // { dg-error "required from" }
 
-B<1, std::layout_left, std::layout_left> b0;     // { dg-error "required here" }
-B<2, std::layout_left, std::layout_stride> b1;   // { dg-error "required here" }
+auto b1 = B<1, std::layout_left, std::layout_left>();     // { dg-error "required from" }
+auto b2 = B<2, std::layout_left, std::layout_stride>();   // { dg-error "required from" }
 
-B<3, std::layout_right, std::layout_right> b2;   // { dg-error "required here" }
-B<4, std::layout_right, std::layout_stride> b3;  // { dg-error "required here" }
+auto b3 = B<3, std::layout_right, std::layout_right>();   // { dg-error "required from" }
+auto b4 = B<4, std::layout_right, std::layout_stride>();  // { dg-error "required from" }
 
-B<5, std::layout_stride, std::layout_right> b4;  // { dg-error "required here" }
-B<6, std::layout_stride, std::layout_left> b5;   // { dg-error "required here" }
-B<7, std::layout_stride, std::layout_stride> b6; // { dg-error "required here" }
+auto b5 = B<5, std::layout_stride, std::layout_right>();  // { dg-error "required from" }
+auto b6 = B<6, std::layout_stride, std::layout_left>();   // { dg-error "required from" }
+auto b7 = B<7, std::layout_stride, std::layout_stride>(); // { dg-error "required from" }
 
 // { dg-prune-output "must be representable as index_type" }
