f3001dd5fe57738be222c3af690f7fdd86f531a1
Pan Li <pan2.li@intel.com>
2025-06-12 10:42:39 +0800

    RISC-V: Add test for vec_dup + vmax.vv combine case 1 with max func 1 and GR2VR cost 0, 1 and 2
    Add asm dump check test for vec_duplicate + vmax.vv combine to vmax.vx,
    with the GR2VR cost is 0, 1 and 2.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-4-i16.c: Add asm check
    	for vmax.vx combine.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-4-i32.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-4-i64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-4-i8.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-5-i16.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-5-i32.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-5-i64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-5-i8.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-6-i16.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-6-i32.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-6-i64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-6-i8.c: Ditto.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

--- gcc/testsuite/gcc.target/riscv/rvv/autovec/vx_vf/vx-5-i8.c
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/vx_vf/vx-5-i8.c
@@ -15,6 +15,7 @@ DEF_VX_BINARY_CASE_1_WRAP(T, *, mul, VX_BINARY_BODY_X16)
 DEF_VX_BINARY_CASE_1_WRAP(T, /, div, VX_BINARY_BODY_X8)
 DEF_VX_BINARY_CASE_1_WRAP(T, %, rem, VX_BINARY_BODY_X8)
 DEF_VX_BINARY_CASE_3_WRAP(T, MAX_FUNC_0_WARP(T), max, VX_BINARY_FUNC_BODY_X8)
+DEF_VX_BINARY_CASE_3_WRAP(T, MAX_FUNC_1_WARP(T), max, VX_BINARY_FUNC_BODY_X8)
 
 /* { dg-final { scan-assembler-not {vadd.vx} } } */
 /* { dg-final { scan-assembler {vsub.vx} } } */
