e7fb2459b00cde4fb14062076df29320efafdb98
Lili Cui <lili.cui@intel.com>
2025-06-28 06:19:19 -0700

    Relax the testcase check for Solaris [PR120818]
    gcc/testsuite/ChangeLog:
    
    	PR target/120818
    	* g++.target/i386/shrink_wrap_separate.C: Relax the check.

--- gcc/testsuite/g++.target/i386/shrink_wrap_separate.C
+++ gcc/testsuite/g++.target/i386/shrink_wrap_separate.C
@@ -21,5 +21,4 @@ bool k() {
   b *n;
  return h(l->g, n);
 }
-/* { dg-final { scan-rtl-dump "The components we wrap separately are \\\[sep 3 4\\\]" "pro_and_epilogue" { target { ia32 } } } } */
-/* { dg-final { scan-rtl-dump "The components we wrap separately are \\\[sep 40 41 42 43\\\]" "pro_and_epilogue" { target { ! ia32 } } } } */
+/* { dg-final { scan-rtl-dump "The components we wrap separately are" "pro_and_epilogue" } } */
