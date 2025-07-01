f85212ad89ab2d0a66cc802d8fbb7b381bd05847
Patrick Palka <ppalka@redhat.com>
2025-07-01 13:43:09 -0400

    libstdc++: Use ranges::iter_move in ranges::unique [PR120789]
    	PR libstdc++/120789
    
    libstdc++-v3/ChangeLog:
    
    	* include/bits/ranges_algo.h (__unique_fn::operator()): Use
    	ranges::iter_move(iter) instead of std::move(*iter).
    	* testsuite/25_algorithms/unique/120789.cc: New test.
    
    Reviewed-by: Tomasz Kamiński <tkaminsk@redhat.com>
    Reviewed-by: Jonathan Wakely <jwakely@redhat.com>
    (cherry picked from commit 6f69a68998f601cdb86c65113eb1feddfa9da31a)

---
+++ libstdc++-v3/testsuite/25_algorithms/unique/120789.cc
@@ -0,0 +1,36 @@
+// PR libstdc++/120789 - ranges::unique should use ranges::iter_move
+// { dg-do compile { target c++20 } }
+
+#include <algorithm>
+
+struct A
+{
+  bool operator==(const A&) const;
+};
+
+struct B
+{
+  B(B&&) = delete;
+  B& operator=(const A&) const;
+
+  operator A() const;
+  bool operator==(const B&) const;
+};
+
+struct I
+{
+  using value_type = A;
+  using difference_type = int;
+  B operator*() const;
+  I& operator++();
+  I operator++(int);
+  bool operator==(const I&) const;
+  friend A iter_move(const I&);
+};
+
+void
+test01()
+{
+  std::ranges::subrange<I, I> r;
+  auto [begin, end] = std::ranges::unique(r);
+}
