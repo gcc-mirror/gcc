13c766066e23eb6ddf6bad7a5664b9d3ca8c1974
Thomas Schwinge <tschwinge@baylibre.com>
2025-07-03 14:44:21 +0200

    OpenMP: Add omp_get_initial_device/omp_get_num_devices builtins: Fix test cases
    With this fix-up for commit 387209938d2c476a67966c6ddbdbf817626f24a2
    "OpenMP: Add omp_get_initial_device/omp_get_num_devices builtins", we progress:
    
         PASS: c-c++-common/gomp/omp_get_num_devices_initial_device.c (test for excess errors)
         PASS: c-c++-common/gomp/omp_get_num_devices_initial_device.c scan-tree-dump-not optimized "abort"
        -FAIL: c-c++-common/gomp/omp_get_num_devices_initial_device.c scan-tree-dump-times optimized "omp_get_num_devices;" 1
        +PASS: c-c++-common/gomp/omp_get_num_devices_initial_device.c scan-tree-dump-times optimized "omp_get_num_devices" 1
         PASS: c-c++-common/gomp/omp_get_num_devices_initial_device.c scan-tree-dump optimized "_1 = __builtin_omp_get_num_devices \\(\\);[\\r\\n]+[ ]+return _1;"
    
    ... etc. for offloading configurations.
    
    	gcc/testsuite/
    	* c-c++-common/gomp/omp_get_num_devices_initial_device.c: Fix.
    	* gfortran.dg/gomp/omp_get_num_devices_initial_device.f90: Likewise.

--- gcc/testsuite/c-c++-common/gomp/omp_get_num_devices_initial_device.c
+++ gcc/testsuite/c-c++-common/gomp/omp_get_num_devices_initial_device.c
@@ -25,8 +25,8 @@ int f()
 
 /* { dg-final { scan-tree-dump-not "abort" "optimized" } }  */
 
-/* { dg-final { scan-tree-dump-not "omp_get_num_devices;" "optimized" { target { ! offloading_enabled } } } }  */
+/* { dg-final { scan-tree-dump-not "omp_get_num_devices" "optimized" { target { ! offloading_enabled } } } }  */
 /* { dg-final { scan-tree-dump "return 0;" "optimized" { target { ! offloading_enabled } } } }  */
 
-/* { dg-final { scan-tree-dump-times "omp_get_num_devices;" 1 "optimized" { target offloading_enabled } } }  */
+/* { dg-final { scan-tree-dump-times "omp_get_num_devices" 1 "optimized" { target offloading_enabled } } }  */
 /* { dg-final { scan-tree-dump "_1 = __builtin_omp_get_num_devices \\(\\);\[\\r\\n\]+\[ \]+return _1;" "optimized" { target offloading_enabled } } }  */
