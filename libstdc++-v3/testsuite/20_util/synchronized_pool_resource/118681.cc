7a878ba615c2c51ea9cf89142033c5e1ad71795e
Jonathan Wakely <jwakely@redhat.com>
2025-07-09 10:14:23 +0100

    libstdc++: Fix memory_resource.cc bootstrap failure for non-gthreads targets
    The new choose_block_size function added in r16-2112-gac2fb60a67d6d1 was
    defined inside an #ifdef _GLIBCXX_HAS_GTHREADS group, which means that
    it's not available for single-threaded targets, and so can't be used by
    unsynchronized_pool_resource. Move it before that preprocessor group so
    it's always defined.
    
    libstdc++-v3/ChangeLog:
    
    	* src/c++17/memory_resource.cc: Adjust indentation of unnamed
    	namespaces.
    	(pool_sizes): Add comment.
    	(choose_block_size): Move outside preprocessor group for
    	gthreads targets.
    	* testsuite/20_util/synchronized_pool_resource/118681.cc:
    	Require gthreads.

--- libstdc++-v3/testsuite/20_util/synchronized_pool_resource/118681.cc
+++ libstdc++-v3/testsuite/20_util/synchronized_pool_resource/118681.cc
@@ -1,4 +1,5 @@
 // { dg-do run { target c++17 } }
+// { dg-require-gthreads "" }
 // Bug 118681 - unsynchronized_pool_resource may fail to respect alignment
 
 #define RESOURCE std::pmr::synchronized_pool_resource
