1e77645795fdf25ffdb5e9931fd9c946f2261fe2
Pan Li <pan2.li@intel.com>
2025-07-19 10:49:15 +0800

    RISC-V: Refine the test case for vector avg_floor and avg_ceil [NFC]
    The previous test case doesn't leverage the right test helper macro,
    it should be DEF_AVG_0_WRAP instead of DEF_AVG_0.  We prefer the
    test function name is test_avg_floor_int64_t_int32_t_0 instead
    of test_avg_floor_WT_NT_0 for DEF_AVG_0(WT, NT).
    
    The below test suites are passed for this patch.
    * The rv64gcv fully regression test.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/avg_floor-1-i16-from-i32.c:
    	Leverage DEF_AVG_0_WRAP to generate the correct func name.
    	* gcc.target/riscv/rvv/autovec/avg_floor-1-i16-from-i64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/avg_floor-1-i32-from-i64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/avg_floor-1-i64-from-i128.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/avg_floor-1-i8-from-i16.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/avg_floor-1-i8-from-i32.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/avg_floor-1-i8-from-i64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-1-i16-from-i32.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-1-i16-from-i64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-1-i32-from-i64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-1-i8-from-i16.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-1-i8-from-i32.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-1-i8-from-i64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/avg_ceil-1-i64-from-i128.c: Ditto.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

--- gcc/testsuite/gcc.target/riscv/rvv/autovec/avg_floor-1-i8-from-i32.c
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/avg_floor-1-i8-from-i32.c
@@ -6,7 +6,7 @@
 #define NT int8_t
 #define WT int32_t
 
-DEF_AVG_0(NT, WT, avg_floor)
+DEF_AVG_0_WRAP(NT, WT, avg_floor)
 
 /* { dg-final { scan-assembler-times {csrwi\s*vxrm,\s*2} 1 } } */
 /* { dg-final { scan-assembler-times {vaadd.vv} 1 } } */
