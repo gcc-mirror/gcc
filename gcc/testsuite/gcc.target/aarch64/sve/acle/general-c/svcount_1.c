ef4c2666f2504c57b4e25dd5e7fc431ca05616b5
Tejas Belagod <tejas.belagod@arm.com>
2025-07-29 11:35:59 +0100

    AArch64: Update test to reflect new message
    Update test error message as svbool_t is now treated as a GNU vector.
    
    gcc/testsuite/ChangeLog
    
    	* gcc.target/aarch64/sve/acle/general-c/svcount_1.c: Update message.

--- gcc/testsuite/gcc.target/aarch64/sve/acle/general-c/svcount_1.c
+++ gcc/testsuite/gcc.target/aarch64/sve/acle/general-c/svcount_1.c
@@ -6,5 +6,5 @@ void f3 (svbool_t *p, svcount_t x) { *p = x; } /* { dg-error {incompatible types
 void f4 (svcount_t *p, svbool_t x) { *p = x; } /* { dg-error {incompatible types} } */
 svbool_t *f5 (svcount_t *p) { return p; } /* { dg-error {incompatible return type} } */
 svcount_t *f6 (svbool_t *p) { return p; } /* { dg-error {incompatible return type} } */
-svbool_t f7 (svcount_t x) { return (svbool_t) x; } /* { dg-error {conversion to non-scalar} } */
+svbool_t f7 (svcount_t x) { return (svbool_t) x; } /* { dg-error {cannot convert value to a vector} } */
 svcount_t f8 (svbool_t x) { return (svcount_t) x; } /* { dg-error {conversion to non-scalar} } */
