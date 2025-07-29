71a121c2c65a423beafb370307afcc5163217c1a
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2025-07-29 15:15:31 +0200

    gccrs: Add execute test for variable and identifiers
    gcc/testsuite/ChangeLog:
    
    	* rust/execute/inline_asm_inout_ident.rs: New test.
    	* rust/execute/inline_asm_inout_var.rs: New test.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

---
+++ gcc/testsuite/rust/execute/inline_asm_inout_ident.rs
@@ -0,0 +1,23 @@
+/* { dg-output "Value is: 5\r*\n" } */
+#![feature(rustc_attrs)]
+
+extern "C" {
+    fn printf(s: *const i8, ...);
+}
+
+#[rustc_builtin_macro]
+macro_rules! asm {
+    () => {};
+}
+
+fn main() -> i32 {
+    let x: i32;
+    // `inout` can also move values to different places
+    unsafe {
+        asm!("inc {}", inout(reg) 4u64=>x);
+    }
+    unsafe {
+        printf("Value is: %i\n\0" as *const str as *const i8, x);
+    }
+    0
+}
