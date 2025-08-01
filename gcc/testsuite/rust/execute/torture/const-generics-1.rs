3d42d4fdf1a06da9d04c1f8ca4574def333f1c91
Philip Herron <herron.philip@googlemail.com>
2025-08-01 21:54:36 +0100

    gccrs: Add test case showing method resolution with const-generics
    gcc/testsuite/ChangeLog:
    
    	* rust/execute/torture/const-generics-1.rs: New test.
    
    Signed-off-by: Philip Herron <herron.philip@googlemail.com>

---
+++ gcc/testsuite/rust/execute/torture/const-generics-1.rs
@@ -0,0 +1,24 @@
+#[lang = "sized"]
+trait Sized {}
+
+struct Foo<const N: usize>;
+
+impl Foo<1> {
+    fn call(&self) -> i32 {
+        10
+    }
+}
+
+impl Foo<2> {
+    fn call(&self) -> i32 {
+        20
+    }
+}
+
+fn main() -> i32 {
+    let a = Foo::<1> {};
+    let b = Foo::<2> {};
+    let aa = a.call();
+    let bb = b.call();
+    bb - aa - 10
+}
