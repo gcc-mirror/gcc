28a4dfe807afb292ef726a82d40c351743c3e345
Richard Sandiford <richard.sandiford@arm.com>
2025-08-04 11:45:34 +0100

    aarch64: Use VNx16BI for sv(n)match*
    This patch continues the work of making ACLE intrinsics use VNx16BI
    for svbool_t results.  It deals with the svmatch* and svnmatch*
    intrinsics.
    
    gcc/
    	* config/aarch64/aarch64-sve2.md (@aarch64_pred_<sve_int_op><mode>):
    	Split SVE2_MATCH pattern into a VNx16QI_ONLY define_ins and a
    	VNx8HI_ONLY define_expand.  Use a VNx16BI destination for the latter.
    	(*aarch64_pred_<sve_int_op><mode>): New SVE2_MATCH pattern for
    	VNx8HI_ONLY.
    	(*aarch64_pred_<sve_int_op><mode>_cc): Likewise.
    
    gcc/testsuite/
    	* gcc.target/aarch64/sve2/acle/general/match_4.c: New test.
    	* gcc.target/aarch64/sve2/acle/general/nmatch_1.c: Likewise.

---
+++ gcc/testsuite/gcc.target/aarch64/sve2/acle/general/nmatch_1.c
@@ -0,0 +1,30 @@
+/* { dg-do compile } */
+/* { dg-options "-O2" } */
+
+#include <arm_sve.h>
+
+svbool_t
+test1 (svbool_t pg, svint16_t x, svint16_t y)
+{
+  return svand_z (svptrue_b8 (),
+		  svnmatch (pg, x, y),
+		  svptrue_b16 ());
+}
+
+svbool_t
+test2 (svbool_t pg, svint16_t x, svint16_t y)
+{
+  return svand_z (svptrue_b16 (),
+		  svnmatch (pg, x, y),
+		  svptrue_b8 ());
+}
+
+svbool_t
+test3 (svbool_t pg, svint16_t x, svint16_t y)
+{
+  return svand_z (svptrue_b16 (),
+		  svnmatch (pg, x, y),
+		  svptrue_b16 ());
+}
+
+/* { dg-final { scan-assembler-not {\tand\t} } } */
