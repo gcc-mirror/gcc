gcc-mirror/gcc

commit ac4e7455a3323750d7f07a3f652b090a1bc2b498

diagnostics: revamp of dumping of "diagnostics" internal state
The diagnostics subsystem has a handy dump feature, usable
during debugging via

  (gdb) call global_dc->dump ()

which prints copious amounts of information about the state
of the diagnostics subsystem to stderr.

This patch consolidates the implementation and extends it, adding
various per-sink data (generation options specific to each of text,
SARIF, and HTML).

No functional difference intended outside of the debugger.

gcc/ChangeLog:
	* Makefile.in (OBJS-libcommon): Add diagnostics/dumping.o.
	* diagnostics/buffering.cc: Include "diagnostics/dumping.h".
	(buffer::dump): Reimplement using diagnostics::dumping.
	* diagnostics/context.cc: Include "diagnostics/dumping.h".
	(context::dump): Reimplement using diagnostics::dumping.
	Use sink::dump_kind when listing the sinks.
	(sink::dump): Reimplement using diagnostics::dumping.
	(counters::dump): Likewise.
	* diagnostics/dumping.cc: New file.
	* diagnostics/dumping.h: New file.
	* diagnostics/file-cache.cc: Include "diagnostics/dumping.h".
	(file_cache::dump): Reimplement using diagnostics::dumping.
	(file_cache_slot::dump): Likewise.
	* diagnostics/html-sink.cc: Include "diagnostics/dumping.h".
	(html_generation_options::dump): New.
	(html_sink_buffer::dump): Reimplement using diagnostics::dumping.
	(html_builder::dump): New.
	(html_sink::dump): Reimplement using diagnostics::dumping.
	Add dump of the html_builder.
	(html_file_sink::dump): Replace with...
	(html_file_sink::dump_kind): ...this.
	(html_buffered_sink::dump_kind): New.
	* diagnostics/html-sink.h (html_generation_options::dump): New
	decl.
	* diagnostics/sarif-sink.cc: Include "diagnostics/dumping.h".
	(sarif_serialization_format_json::dump): New.
	(sarif_builder::dump): New.
	(sarif_sink_buffer::dump): Reimplement using diagnostics::dumping.
	(sarif_sink::dump): Likewise.  Add dump of the sarif_builder.
	(sarif_stream_sink::dump_kind): New.
	(sarif_file_sink::dump): Replace with...

gcc/diagnostics/context.cc
@@ -50,6 +50,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "diagnostics/logical-locations.h"
 #include "diagnostics/buffering.h"
 #include "diagnostics/file-cache.h"
+#include "diagnostics/dumping.h"
 
 #ifdef HAVE_TERMIOS_H
 # include <termios.h>
@@ -358,33 +359,37 @@ context::finish ()
 /* Dump state of this diagnostics::context to OUT, for debugging.  */
 
 void
-context::dump (FILE *out) const
+context::dump (FILE *outfile) const
 {
-  fprintf (out, "diagnostics::context:\n");
-  m_diagnostic_counters.dump (out, 2);
-  fprintf (out, "  reference printer:\n");
-  m_reference_printer->dump (out, 4);
-  fprintf (out, "  output sinks:\n");
+  dumping::emit_heading (outfile, 0, "diagnostics::context");
+  m_diagnostic_counters.dump (outfile, 2);
+  dumping::emit_heading (outfile, 2, "reference printer");
+  m_reference_printer->dump (outfile, 4);
+  dumping::emit_heading (outfile, 2, "output sinks");
   if (m_sinks.length () > 0)
     {
       for (unsigned i = 0; i < m_sinks.length (); ++i)
 	{
-	  fprintf (out, "  sink %i:\n", i);
-	  m_sinks[i]->dump (out, 4);
+	  dumping::emit_indent (outfile, 4);
+	  const sink *s = m_sinks[i];
+	  fprintf (outfile, "sink %i (", i);
+	  s->dump_kind (outfile);
+	  fprintf (outfile, "):\n");
+	  s->dump (outfile, 6);
 	}
     }
   else
-    fprintf (out, "    (none):\n");
-  fprintf (out, "  diagnostic buffer:\n");
+    dumping::emit_none (outfile, 4);
+  dumping::emit_heading (outfile, 2, "diagnostic buffer");
   if (m_diagnostic_buffer)
-    m_diagnostic_buffer->dump (out, 4);
+    m_diagnostic_buffer->dump (outfile, 4);
   else
-    fprintf (out, "    (none):\n");
-  fprintf (out, "  file cache:\n");
+    dumping::emit_none (outfile, 4);
+  dumping::emit_heading (outfile, 2, "file cache");
   if (m_file_cache)
-    m_file_cache->dump (out, 4);
+    m_file_cache->dump (outfile, 4);
   else
-    fprintf (out, "    (none):\n");
+    dumping::emit_none (outfile, 4);
 }
 
 /* Return true if sufficiently severe diagnostics have been seen that
@@ -1702,7 +1707,7 @@ context::set_nesting_level (int new_level)
 void
 sink::dump (FILE *out, int indent) const
 {
-  fprintf (out, "%*sprinter:\n", indent, "");
+  dumping::emit_heading (out, indent, "printer");
   m_printer->dump (out, indent + 2);
 }
 
@@ -1789,19 +1794,19 @@ counters::counters ()
 void
 counters::dump (FILE *out, int indent) const
 {
-  fprintf (out, "%*scounts:\n", indent, "");
+  dumping::emit_heading (out, indent, "counts");
   bool none = true;
   for (int i = 0; i < static_cast<int> (kind::last_diagnostic_kind); i++)
     if (m_count_for_kind[i] > 0)
       {
-	fprintf (out, "%*s%s%i\n",
-		 indent + 2, "",
+	dumping::emit_indent (out, indent + 2);
+	fprintf (out, "%s%i\n",
 		 get_text_for_kind (static_cast<enum kind> (i)),
 		 m_count_for_kind[i]);
 	none = false;
       }
   if (none)
-    fprintf (out, "%*s(none)\n", indent + 2, "");
+    dumping::emit_none (out, indent + 2);
 }
 
 void

gcc/diagnostics/dumping.cc
@@ -1,0 +1,116 @@
+/* Utilities for implementing "dump" functions for the diagnostics subsystem.
+   Copyright (C) 2025 Free Software Foundation, Inc.
+   Contributed by David Malcolm <dmalcolm@redhat.com>.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+
+#include "config.h"
+#include "system.h"
+#include "coretypes.h"
+#include "diagnostics/dumping.h"
+
+namespace diagnostics {
+namespace dumping {
+
+/* Emit indentation to OUTFILE for the start of a dump line.  */
+
+void
+emit_indent (FILE *outfile, int indent)
+{
+  fprintf (outfile, "%*s", indent, "");
+}
+
+/* Emit an indented line to OUTFILE showing a heading.  */
+
+void
+emit_heading (FILE *outfile, int indent,
+	      const char *text)
+{
+  emit_indent (outfile, indent);
+  fprintf (outfile, "%s:\n", text);
+}
+
+/* Various specializattions that emit an indented line to OUTFILE
+   showing "label: value".  */
+
+template <>
+void
+emit_field<const char *> (FILE *outfile, int indent,
+			  const char *label, const char *value)
+{
+  emit_indent (outfile, indent);
+  fprintf (outfile, "%s: %s\n", label, value);
+}
+
+template <>
+void
+emit_field<char *> (FILE *outfile, int indent,
+		    const char *label, char *value)
+{
+  emit_indent (outfile, indent);
+  fprintf (outfile, "%s: %s\n", label, value);
+}
+
+template <>
+void
+emit_field<bool> (FILE *outfile, int indent,
+		  const char *label, bool value)
+{
+  emit_field<const char *> (outfile, indent, label,
+			    value ? "true" : "false");
+}
+
+template <>
+void
+emit_field<size_t> (FILE *outfile, int indent,
+		    const char *label, size_t value)
+{
+  emit_indent (outfile, indent);
+  fprintf (outfile, "%s: %zi\n", label, value);
+}
+
+template <>
+void
+emit_field<int> (FILE *outfile, int indent,
+		 const char *label, int value)
+{
+  emit_indent (outfile, indent);
+  fprintf (outfile, "%s: %i\n", label, value);
+}
+
+template <>
+void
+emit_field<unsigned> (FILE *outfile, int indent,
+		      const char *label, unsigned value)
+{
+  emit_indent (outfile, indent);
+  fprintf (outfile, "%s: %u\n", label, value);
+}
+
+/* Emit an indented line to OUTFILE reading "(none)".  */
+
+void
+emit_none (FILE *outfile, int indent)
+{
+  emit_indent (outfile, indent);
+  fprintf (outfile, "(none)\n");
+}
+
+
+} // namespace dumping {
+} // namespace diagnostics

gcc/diagnostics/dumping.h
@@ -1,0 +1,43 @@
+/* Utilities for implementing "dump" functions for the diagnostics subsystem.
+   Copyright (C) 2025 Free Software Foundation, Inc.
+   Contributed by David Malcolm <dmalcolm@redhat.com>.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef GCC_DIAGNOSTICS_DUMP_H
+#define GCC_DIAGNOSTICS_DUMP_H
+
+namespace diagnostics {
+namespace dumping {
+
+extern void emit_indent (FILE *outfile, int indent);
+extern void emit_heading (FILE *outfile, int indent,
+			  const char *text);
+
+template <typename T>
+extern void emit_field (FILE *outfile, int indent,
+			const char *label, T value);
+
+extern void emit_none (FILE *outfile, int indent);
+
+#define DIAGNOSTICS_DUMPING_EMIT_FIELD(FLAG) \
+  dumping::emit_field (outfile, indent, #FLAG, FLAG)
+
+} // namespace dumping
+} // namespace diagnostics
+
+#endif /* ! GCC_DIAGNOSTICS_DUMP_H */

gcc/diagnostics/file-cache.cc
@@ -22,6 +22,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "coretypes.h"
 #include "cpplib.h"
 #include "diagnostics/file-cache.h"
+#include "diagnostics/dumping.h"
 #include "selftest.h"
 
 #ifndef HAVE_ICONV
@@ -473,7 +474,8 @@ file_cache::dump (FILE *out, int indent) const
 {
   for (size_t i = 0; i < m_num_file_slots; ++i)
     {
-      fprintf (out, "%*sslot[%i]:\n", indent, "", (int)i);
+      dumping::emit_indent (out, indent);
+      fprintf (out, "slot[%i]:\n", (int)i);
       m_file_slots[i].dump (out, indent + 2);
     }
 }
@@ -541,27 +543,35 @@ file_cache_slot::dump (FILE *out, int indent) const
 {
   if (!m_file_path)
     {
-      fprintf (out, "%*s(unused)\n", indent, "");
+      dumping::emit_indent (out, indent);
+      fprintf (out, "(unused)\n");
       return;
     }
-  fprintf (out, "%*sfile_path: %s\n", indent, "", m_file_path);
-  fprintf (out, "%*sfp: %p\n", indent, "", (void *)m_fp);
-  fprintf (out, "%*sneeds_read_p: %i\n", indent, "", (int)needs_read_p ());
-  fprintf (out, "%*sneeds_grow_p: %i\n", indent, "", (int)needs_grow_p ());
-  fprintf (out, "%*suse_count: %i\n", indent, "", m_use_count);
-  fprintf (out, "%*ssize: %zi\n", indent, "", m_size);
-  fprintf (out, "%*snb_read: %zi\n", indent, "", m_nb_read);
-  fprintf (out, "%*sstart_line_idx: %zi\n", indent, "", m_line_start_idx);
-  fprintf (out, "%*sline_num: %zi\n", indent, "", m_line_num);
-  fprintf (out, "%*smissing_trailing_newline: %i\n",
-	   indent, "", (int)m_missing_trailing_newline);
-  fprintf (out, "%*sline records (%i):\n",
-	   indent, "", m_line_record.length ());
+  dumping::emit_field (out, indent, "file_path", m_file_path);
+  {
+    dumping::emit_indent (out, indent);
+    fprintf (out, "fp: %p\n", (void *)m_fp);
+  }
+  dumping::emit_field (out, indent, "needs_read_p", needs_read_p ());
+  dumping::emit_field (out, indent, "needs_grow_p", needs_grow_p ());
+  dumping::emit_field (out, indent, "use_count", m_use_count);
+  dumping::emit_field (out, indent, "size", m_size);
+  dumping::emit_field (out, indent, "nb_read", m_nb_read);
+  dumping::emit_field (out, indent, "start_line_idx", m_line_start_idx);
+  dumping::emit_field (out, indent, "line_num", m_line_num);
+  dumping::emit_field (out, indent, "missing_trailing_newline",
+		       (int)m_missing_trailing_newline);
+  {
+    dumping::emit_indent (out, indent);
+    fprintf (out, "line records (%i):\n", m_line_record.length ());
+  }
   int idx = 0;
   for (auto &line : m_line_record)
-    fprintf (out, "%*s[%i]: line %zi: byte offsets: %zi-%zi\n",
-	     indent + 2, "",
-	     idx++, line.line_num, line.start_pos, line.end_pos);
+    {
+      dumping::emit_indent (out, indent);
+      fprintf (out, "[%i]: line %zi: byte offsets: %zi-%zi\n",
+	       idx++, line.line_num, line.start_pos, line.end_pos);
+    }
 }
 
 /* Returns TRUE iff the cache would need to be filled with data coming

gcc/diagnostics/html-sink.cc
@@ -33,6 +33,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "diagnostics/output-file.h"
 #include "diagnostics/buffering.h"
 #include "diagnostics/paths.h"
+#include "diagnostics/dumping.h"
 #include "diagnostics/client-data-hooks.h"
 #include "selftest.h"
 #include "diagnostics/selftest-context.h"
@@ -59,6 +60,16 @@ html_generation_options::html_generation_options ()
   m_show_state_diagrams_sarif (false),
   m_show_state_diagrams_dot_src (false)
 {
+}
+
+void
+html_generation_options::dump (FILE *outfile, int indent) const
+{
+  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_css);
+  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_javascript);
+  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_show_state_diagrams);
+  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_show_state_diagrams_sarif);
+  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_show_state_diagrams_dot_src);
 }
 
 class html_builder;
@@ -115,6 +126,8 @@ public:
 		pretty_printer &pp,
 		const line_maps *line_maps,
 		const html_generation_options &html_gen_opts);
+
+  void dump (FILE *out, int indent) const;
 
   void
   set_main_input_filename (const char *name);
@@ -223,11 +236,12 @@ make_span (std::string class_)
 void
 html_sink_buffer::dump (FILE *out, int indent) const
 {
-  fprintf (out, "%*shtml_sink_buffer:\n", indent, "");
+  dumping::emit_heading (out, indent, "html_sink_buffer");
   int idx = 0;
   for (auto &result : m_results)
     {
-      fprintf (out, "%*sresult[%i]:\n", indent + 2, "", idx);
+      dumping::emit_indent (out, indent + 2);
+      fprintf (out, "result[%i]:\n", idx);
       result->dump (out);
       fprintf (out, "\n");
       ++idx;
@@ -467,6 +481,13 @@ html_builder::html_builder (context &dc,
       }
     }
   }
+}
+
+void
+html_builder::dump (FILE *out, int indent) const
+{
+  dumping::emit_heading (out, indent, "HTML generation options");
+  m_html_gen_opts.dump (out, indent + 2);
 }
 
 void
@@ -1333,8 +1354,9 @@ public:
 
   void dump (FILE *out, int indent) const override
   {
-    fprintf (out, "%*shtml_sink\n", indent, "");
     sink::dump (out, indent);
+    dumping::emit_heading (out, indent, "html_builder");
+    m_builder.dump (out, indent + 2);
   }
 
   void
@@ -1439,12 +1461,10 @@ public:
   {
     m_builder.flush_to_file (m_output_file.get_open_file ());
   }
-  void dump (FILE *out, int indent) const override
+  void dump_kind (FILE *out) const override
   {
-    fprintf (out, "%*shtml_file_sink: %s\n",
-	     indent, "",
+    fprintf (out, "html_file_sink: %s",
 	     m_output_file.get_filename ());
-    sink::dump (out, indent);
   }
   bool machine_readable_stderr_p () const final override
   {
@@ -1606,6 +1626,10 @@ private:
 			const html_generation_options &html_gen_opts)
     : html_sink (dc, line_maps, html_gen_opts)
     {
+    }
+    void dump_kind (FILE *out) const final override
+    {
+      fprintf (out, "html_buffered_sink");
     }
     bool machine_readable_stderr_p () const final override
     {

commit 7c02f011502ea65bb03891239a7317a95c6a00dd

diagnostics: fix build on hosts where unsigned == size_t
Looks like I broke the build with r16-3091-gac4e7455a33237 on hosts
where unsigned == size_t.

../../gcc/gcc/diagnostics/dumping.cc:98:1: error: redefinition of ‘void diagnostics::dumping::emit_field(FILE*, int, const char*, T) [with T = unsigned int; FILE = FILE]’
   98 | emit_field<unsigned> (FILE *outfile, int indent,
      | ^~~~~~~~~~~~~~~~~~~~
../../gcc/gcc/diagnostics/dumping.cc:80:1: note: ‘void diagnostics::dumping::emit_field(FILE*, int, const char*, T) [with T = unsigned int; FILE = FILE]’ previously declared here
   80 | emit_field<size_t> (FILE *outfile, int indent,
      | ^~~~~~~~~~~~~~~~~~

Sorry about this.

Should be fixed by the following patch, which avoids templates here
in favor of being explicit about types, avoids the use of "%zi" with
fprintf in various places, and fixes some other minor issues in the
dumping logic that I noticed whilst testing the patch.

gcc/ChangeLog:
	* diagnostics/context.cc (context::dump): Bulletproof against
	m_reference_printer being null.
	* diagnostics/dumping.cc (emit_field<const char *>): Replace
	with...
	(emit_string_field): ...this.
	(emit_field<char *>): Eliminate.
	(emit_field<bool>): Replace with...
	(emit_bool_field): ...this.
	(emit_field<size_t>): Replace with...
	(emit_size_t_field): ...this, and use HOST_SIZE_T_PRINT_DEC rather
	than %zi in fprintf call.
	(emit_field<int>): Replace with...
	(emit_int_field): ...this.
	(emit_field<unsigned>): Replace with...
	(emit_unsigned_field): ...this.
	* diagnostics/dumping.h (emit_field): Replace this template decl
	with...
	(emit_string_field): ...this,
	(emit_bool_field): ...this,
	(emit_size_t_field): ...this,
	(emit_int_field): ...this,
	(emit_unsigned_field): ... and this.
	(DIAGNOSTICS_DUMPING_EMIT_FIELD): Rename to...
	(DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD): ...this and update for
	above change.
	* diagnostics/file-cache.cc (file_cache_slot::dump): Replace

gcc/diagnostics/context.cc
@@ -391,7 +391,10 @@ context::dump (FILE *outfile) const
   dumping::emit_heading (outfile, 0, "diagnostics::context");
   m_diagnostic_counters.dump (outfile, 2);
   dumping::emit_heading (outfile, 2, "reference printer");
-  m_reference_printer->dump (outfile, 4);
+  if (m_reference_printer)
+    m_reference_printer->dump (outfile, 4);
+  else
+    dumping::emit_none (outfile, 4);
   dumping::emit_heading (outfile, 2, "output sinks");
   if (m_sinks.length () > 0)
     {

gcc/diagnostics/dumping.cc
@@ -45,58 +45,44 @@ emit_heading (FILE *outfile, int indent,
   fprintf (outfile, "%s:\n", text);
 }
 
-/* Various specializattions that emit an indented line to OUTFILE
+/* Various functions that emit an indented line to OUTFILE
    showing "label: value".  */
 
-template <>
 void
-emit_field<const char *> (FILE *outfile, int indent,
-			  const char *label, const char *value)
+emit_string_field (FILE *outfile, int indent,
+		      const char *label, const char *value)
 {
   emit_indent (outfile, indent);
   fprintf (outfile, "%s: %s\n", label, value);
 }
 
-template <>
 void
-emit_field<char *> (FILE *outfile, int indent,
-		    const char *label, char *value)
+emit_bool_field (FILE *outfile, int indent,
+		 const char *label, bool value)
+{
+  emit_string_field (outfile, indent, label,
+		     value ? "true" : "false");
+}
+
+void
+emit_size_t_field (FILE *outfile, int indent,
+		   const char *label, size_t value)
 {
   emit_indent (outfile, indent);
-  fprintf (outfile, "%s: %s\n", label, value);
+  fprintf (outfile, "%s: " HOST_SIZE_T_PRINT_DEC "\n", label, value);
 }
 
-template <>
 void
-emit_field<bool> (FILE *outfile, int indent,
-		  const char *label, bool value)
-{
-  emit_field<const char *> (outfile, indent, label,
-			    value ? "true" : "false");
-}
-
-template <>
-void
-emit_field<size_t> (FILE *outfile, int indent,
-		    const char *label, size_t value)
-{
-  emit_indent (outfile, indent);
-  fprintf (outfile, "%s: %zi\n", label, value);
-}
-
-template <>
-void
-emit_field<int> (FILE *outfile, int indent,
-		 const char *label, int value)
+emit_int_field (FILE *outfile, int indent,
+		const char *label, int value)
 {
   emit_indent (outfile, indent);
   fprintf (outfile, "%s: %i\n", label, value);
 }
 
-template <>
 void
-emit_field<unsigned> (FILE *outfile, int indent,
-		      const char *label, unsigned value)
+emit_unsigned_field (FILE *outfile, int indent,
+		     const char *label, unsigned value)
 {
   emit_indent (outfile, indent);
   fprintf (outfile, "%s: %u\n", label, value);

gcc/diagnostics/dumping.h
@@ -28,14 +28,21 @@ extern void emit_indent (FILE *outfile, int indent);
 extern void emit_heading (FILE *outfile, int indent,
 			  const char *text);
 
-template <typename T>
-extern void emit_field (FILE *outfile, int indent,
-			const char *label, T value);
+extern void emit_string_field (FILE *outfile, int indent,
+			       const char *label, const char *value);
+extern void emit_bool_field (FILE *outfile, int indent,
+				const char *label, bool value);
+extern void emit_size_t_field (FILE *outfile, int indent,
+			       const char *label, size_t value);
+extern void emit_int_field (FILE *outfile, int indent,
+			    const char *label, int value);
+extern void emit_unsigned_field (FILE *outfile, int indent,
+				 const char *label, unsigned value);
 
 extern void emit_none (FILE *outfile, int indent);
 
-#define DIAGNOSTICS_DUMPING_EMIT_FIELD(FLAG) \
-  dumping::emit_field (outfile, indent, #FLAG, FLAG)
+#define DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD(FLAG) \
+  dumping::emit_bool_field (outfile, indent, #FLAG, FLAG)
 
 } // namespace dumping
 } // namespace diagnostics

gcc/diagnostics/file-cache.cc
@@ -547,20 +547,20 @@ file_cache_slot::dump (FILE *out, int indent) const
       fprintf (out, "(unused)\n");
       return;
     }
-  dumping::emit_field (out, indent, "file_path", m_file_path);
+  dumping::emit_string_field (out, indent, "file_path", m_file_path);
   {
     dumping::emit_indent (out, indent);
     fprintf (out, "fp: %p\n", (void *)m_fp);
   }
-  dumping::emit_field (out, indent, "needs_read_p", needs_read_p ());
-  dumping::emit_field (out, indent, "needs_grow_p", needs_grow_p ());
-  dumping::emit_field (out, indent, "use_count", m_use_count);
-  dumping::emit_field (out, indent, "size", m_size);
-  dumping::emit_field (out, indent, "nb_read", m_nb_read);
-  dumping::emit_field (out, indent, "start_line_idx", m_line_start_idx);
-  dumping::emit_field (out, indent, "line_num", m_line_num);
-  dumping::emit_field (out, indent, "missing_trailing_newline",
-		       (int)m_missing_trailing_newline);
+  dumping::emit_bool_field (out, indent, "needs_read_p", needs_read_p ());
+  dumping::emit_bool_field (out, indent, "needs_grow_p", needs_grow_p ());
+  dumping::emit_unsigned_field (out, indent, "use_count", m_use_count);
+  dumping::emit_size_t_field (out, indent, "size", m_size);
+  dumping::emit_size_t_field (out, indent, "nb_read", m_nb_read);
+  dumping::emit_size_t_field (out, indent, "start_line_idx", m_line_start_idx);
+  dumping::emit_size_t_field (out, indent, "line_num", m_line_num);
+  dumping::emit_bool_field (out, indent, "missing_trailing_newline",
+			    m_missing_trailing_newline);
   {
     dumping::emit_indent (out, indent);
     fprintf (out, "line records (%i):\n", m_line_record.length ());
@@ -569,7 +569,10 @@ file_cache_slot::dump (FILE *out, int indent) const
   for (auto &line : m_line_record)
     {
       dumping::emit_indent (out, indent);
-      fprintf (out, "[%i]: line %zi: byte offsets: %zi-%zi\n",
+      fprintf (out, ("[%i]:"
+		     " line " HOST_SIZE_T_PRINT_DEC ":"
+		     " byte offsets: " HOST_SIZE_T_PRINT_DEC
+		     "-" HOST_SIZE_T_PRINT_DEC "\n"),
 	       idx++, line.line_num, line.start_pos, line.end_pos);
     }
 }

gcc/diagnostics/html-sink.cc
@@ -65,11 +65,11 @@ html_generation_options::html_generation_options ()
 void
 html_generation_options::dump (FILE *outfile, int indent) const
 {
-  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_css);
-  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_javascript);
-  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_show_state_diagrams);
-  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_show_state_diagrams_sarif);
-  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_show_state_diagrams_dot_src);
+  DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD (m_css);
+  DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD (m_javascript);
+  DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD (m_show_state_diagrams);
+  DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD (m_show_state_diagrams_sarif);
+  DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD (m_show_state_diagrams_dot_src);
 }
 
 class html_builder;

gcc/diagnostics/sarif-sink.cc
@@ -710,7 +710,7 @@ sarif_serialization_format_json::dump (FILE *outfile, int indent) const
 {
   dumping::emit_indent (outfile, indent);
   fprintf (outfile, "json\n");
-  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_formatted);
+  DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD (m_formatted);
 }
 
 /* A class for managing SARIF output (for -fdiagnostics-format=sarif-stderr
@@ -4360,10 +4360,10 @@ get_dump_string_for_sarif_version (enum sarif_version version)
 void
 sarif_generation_options::dump (FILE *outfile, int indent) const
 {
-  dumping::emit_field (outfile, indent,
-		       "m_version",
-		       get_dump_string_for_sarif_version (m_version));
-  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_state_graph);
+  dumping::emit_string_field (outfile, indent,
+			      "m_version",
+			      get_dump_string_for_sarif_version (m_version));
+  DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD (m_state_graph);
 }
 
 #if CHECKING_P

gcc/diagnostics/text-sink.cc
@@ -159,10 +159,10 @@ text_sink::~text_sink ()
 void
 text_sink::dump (FILE *outfile, int indent) const
 {
-  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_follows_reference_printer);
-  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_show_nesting);
-  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_show_locations_in_nesting);
-  DIAGNOSTICS_DUMPING_EMIT_FIELD (m_show_nesting_levels);
+  DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD (m_follows_reference_printer);
+  DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD (m_show_nesting);
+  DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD (m_show_locations_in_nesting);
+  DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD (m_show_nesting_levels);
 
   sink::dump (outfile, indent);
   dumping::emit_heading (outfile, indent, "saved_output_buffer");

gcc/libgdiagnostics.cc
@@ -1939,7 +1939,8 @@ diagnostic_manager_debug_dump_file (diagnostic_manager *,
 	fprintf (out, ", sarif_source_language=\"%s\"",
 		 file->get_sarif_source_language ());
       if (const content_buffer *buf = file->get_content ())
-	fprintf (out, ", content=(size=%zi)", buf->m_sz);
+	fprintf (out, ", content=(size=" HOST_SIZE_T_PRINT_DEC ")",
+		 buf->m_sz);
       fprintf (out, ")");
     }
   else

gcc/pretty-print.cc
@@ -29,6 +29,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "pretty-print-urlifier.h"
 #include "diagnostics/color.h"
 #include "diagnostics/event-id.h"
+#include "diagnostics/dumping.h"
 #include "diagnostic-highlight-colors.h"
 #include "auto-obstack.h"
 #include "selftest.h"
@@ -1600,9 +1601,8 @@ pp_formatted_chunks::dump (FILE *out, int indent) const
 {
   for (size_t idx = 0; m_args[idx]; ++idx)
     {
-      fprintf (out, "%*s%i: ",
-	       indent, "",
-	       (int)idx);
+      diagnostics::dumping::emit_indent (out, indent);
+      fprintf (out, "%i: ", (int)idx);
       m_args[idx]->dump (out);
     }
 }
@@ -3100,34 +3100,39 @@ pretty_printer::end_url ()
     pp_string (this, get_end_url_string (this));
 }
 
-/* Dump state of this pretty_printer to OUT, for debugging.  */
-
-void
-pretty_printer::dump (FILE *out, int indent) const
+static const char *
+get_url_format_as_string (diagnostic_url_format url_format)
 {
-  fprintf (out, "%*sm_show_color: %s\n",
-	   indent, "",
-	   m_show_color ? "true" : "false");
-
-  fprintf (out, "%*sm_url_format: ", indent, "");
-  switch (m_url_format)
+  switch (url_format)
     {
     case URL_FORMAT_NONE:
-      fprintf (out, "none");
-      break;
+      return "none";
     case URL_FORMAT_ST:
-      fprintf (out, "st");
-      break;
+      return "st";
     case URL_FORMAT_BEL:
-      fprintf (out, "bel");
-      break;
+      return "bel";
     default:
       gcc_unreachable ();
     }
-  fprintf (out, "\n");
+}
 
-  fprintf (out, "%*sm_buffer:\n", indent, "");
-  m_buffer->dump (out, indent + 2);
+/* Dump state of this pretty_printer to OUT, for debugging.  */
+
+void
+pretty_printer::dump (FILE *outfile, int indent) const
+{
+  namespace dumping = diagnostics::dumping;
+
+  DIAGNOSTICS_DUMPING_EMIT_BOOL_FIELD (m_show_color);
+  dumping::emit_string_field
+    (outfile, indent,
+     "m_url_format",
+     get_url_format_as_string (m_url_format));
+  dumping::emit_heading (outfile, indent, "m_buffer");
+  if (m_buffer)
+    m_buffer->dump (outfile, indent + 2);
+  else
+    dumping::emit_none (outfile, indent + 2);
 }
 
 /* class pp_markup::context.  */