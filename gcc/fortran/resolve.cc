e6f4543f63366433493b3870845b555fd00be7e6
Paul Thomas <pault@gcc.gnu.org>
2025-08-13 07:16:31 +0100

    Fortran: Use associated TBP subroutine not found [PR89092]
    2025-08-13  Paul Thomas  <pault@gcc.gnu.org>
    
    gcc/fortran
    	PR fortran/89092
    	* resolve.cc (was_declared): Add subroutine attribute.
    
    gcc/testsuite/
    	PR fortran/89092
    	* gfortran.dg/pr89092.f90: New test.

--- gcc/fortran/resolve.cc
+++ gcc/fortran/resolve.cc
@@ -1630,7 +1630,7 @@ was_declared (gfc_symbol *sym)
   if (a.allocatable || a.dimension || a.dummy || a.external || a.intrinsic
       || a.optional || a.pointer || a.save || a.target || a.volatile_
       || a.value || a.access != ACCESS_UNKNOWN || a.intent != INTENT_UNKNOWN
-      || a.asynchronous || a.codimension)
+      || a.asynchronous || a.codimension || a.subroutine)
     return 1;
 
   return 0;
