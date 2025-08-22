65db613b6ef6ea81eb662820d815dfaa36d86187
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2025-08-22 12:35:58 +0200

    gccrs: Add fn pointer implementation test
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/impl_fnptr.rs: New test.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

---
+++ gcc/testsuite/rust/compile/impl_fnptr.rs
@@ -0,0 +1,18 @@
+#[lang = "sized"]
+pub trait Sized {}
+
+#[lang = "eq"]
+pub trait PartialEq<Rhs: ?Sized = Self> {
+    fn eq(&self, other: &Rhs) -> bool;
+
+    fn ne(&self, other: &Rhs) -> bool {
+        !self.eq(other)
+    }
+}
+
+impl<Ret> PartialEq for extern "C" fn() -> Ret {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        *self as usize == *other as usize
+    }
+}
