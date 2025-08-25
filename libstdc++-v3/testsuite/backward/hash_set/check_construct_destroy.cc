04e8f8ac9401352ec3ddc1dd4d226ba8fc947207
Tomasz Kamiński <tkaminsk@redhat.com>
2025-08-25 13:15:35 +0200

    libstdc++: Do not require assignment for vector::resize(n, v) [PR90192]
    This patch introduces a new function, _M_fill_append, which is invoked when
    copies of the same value are appended to the end of a vector. Unlike
    _M_fill_insert(end(), n, v), _M_fill_append never permute elements in place,
    so it does not require:
    * vector element type to be assignable;
    * a copy of the inserted value, in the case where it points to an
      element of the vector.
    
    vector::resize(n, v) now uses _M_fill_append, fixing the non-conformance where
    element types were required to be assignable.
    
    In addition, _M_fill_insert(end(), n, v) now delegates to _M_fill_append, which
    eliminates an unnecessary copy of v when the existing capacity is used.
    
    	PR libstdc++/90192
    
    libstdc++-v3/ChangeLog:
    
    	* include/bits/stl_vector.h (vector<T>::_M_fill_append): Declare.
    	(vector<T>::fill): Use _M_fill_append instead of _M_fill_insert.
    	* include/bits/vector.tcc (vector<T>::_M_fill_append): Define
    	(vector<T>::_M_fill_insert): Delegate to _M_fill_append when
    	elements are appended.
    	* testsuite/23_containers/vector/modifiers/moveable.cc: Updated
    	copycount for inserting at the end (appending).
    	* testsuite/23_containers/vector/modifiers/resize.cc: New test.
    	* testsuite/backward/hash_set/check_construct_destroy.cc: Updated
    	copycount, the hash_set constructor uses insert to fill buckets
    	with nullptrs.
    
    Reviewed-by: Jonathan Wakely <jwakely@redhat.com>
    Signed-off-by: Tomasz Kamiński <tkaminsk@redhat.com>
    (cherry picked from commit 390f3a690cf5c6bc8f7290484cafa6617c1757d4)

--- libstdc++-v3/testsuite/backward/hash_set/check_construct_destroy.cc
+++ libstdc++-v3/testsuite/backward/hash_set/check_construct_destroy.cc
@@ -39,50 +39,45 @@ int main()
 
   int buckets;
 
-  // For C++11 and later add 1 to all counts, because the std::vector used
-  // internally by the hashtable creates and destroys a temporary object
-  // using its allocator.
-  const int extra = __cplusplus >= 201102L ? 1 : 0;
-
   tracker_allocator_counter::reset();
   {
     Container c;
     buckets = c.bucket_count();
-    ok = check_construct_destroy("empty container", buckets+extra, extra) && ok;
+    ok = check_construct_destroy("empty container", buckets, 0) && ok;
   }
-  ok = check_construct_destroy("empty container", buckets+extra, buckets+extra) && ok;
+  ok = check_construct_destroy("empty container", buckets, buckets) && ok;
 
 
   tracker_allocator_counter::reset();
   {
     Container c(arr10, arr10 + 10);
-    ok = check_construct_destroy("Construct from range", buckets+10+extra, extra) && ok;
+    ok = check_construct_destroy("Construct from range", buckets+10, 0) && ok;
   }
-  ok = check_construct_destroy("Construct from range", buckets+10+extra, buckets+10+extra) && ok;
+  ok = check_construct_destroy("Construct from range", buckets+10, buckets+10) && ok;
 
   tracker_allocator_counter::reset();
   {
     Container c(arr10, arr10 + 10);
     c.insert(arr10a[0]);
-    ok = check_construct_destroy("Insert element", buckets+11+extra, extra) && ok;
+    ok = check_construct_destroy("Insert element", buckets+11, 0) && ok;
   }
-  ok = check_construct_destroy("Insert element", buckets+11+extra, buckets+11+extra) && ok;
+  ok = check_construct_destroy("Insert element", buckets+11, buckets+11) && ok;
 
   tracker_allocator_counter::reset();
   {
     Container c(arr10, arr10 + 10);
     c.insert(arr10a, arr10a+3);
-    ok = check_construct_destroy("Insert short range", buckets+13+extra, extra) && ok;
+    ok = check_construct_destroy("Insert short range", buckets+13, 0) && ok;
   }
-  ok = check_construct_destroy("Insert short range", buckets+13+extra, buckets+13+extra) && ok;
+  ok = check_construct_destroy("Insert short range", buckets+13, buckets+13) && ok;
 
   tracker_allocator_counter::reset();
   {
     Container c(arr10, arr10 + 10);
     c.insert(arr10a, arr10a+10);
-    ok = check_construct_destroy("Insert long range", buckets+20+extra, extra) && ok;
+    ok = check_construct_destroy("Insert long range", buckets+20, 0) && ok;
   }
-  ok = check_construct_destroy("Insert long range", buckets+20+extra, buckets+20+extra) && ok;
+  ok = check_construct_destroy("Insert long range", buckets+20, buckets+20) && ok;
 
   return ok ? 0 : 1;
 }
