58a8ecc1e9a9e54c2605e80adca7659f475ab10f
Jeff Law <jlaw@ventanamicro.com>
2025-08-27 16:37:19 -0600

    Remove xfail marker on RISC-V test
    So yet another testsuite hygiene patch.  This time turning XPASS -> PASS.  My
    tester treats those cases the same so I didn't get notified that nozicond-2.c
    was passing after some recent changes.
    
    This removes the xfail marker on that test and thus the test is expected to
    pass now.
    
    Pushing to the trunk momentarily.
    
    gcc/testsuite/
    	* gcc.target/riscv/nozicond-2.c: Remove xfails.

--- gcc/testsuite/gcc.target/riscv/nozicond-2.c
+++ gcc/testsuite/gcc.target/riscv/nozicond-2.c
@@ -9,7 +9,7 @@ long foo2 (long c) { return c >= 0 ? -1 : 1; }
 /* We don't support 4->3 splitters, so this fails.  We could perhaps
    try to catch it in the expander as a special case rather than waiting
    for combine.  */
-/* { dg-final { scan-assembler-times {srai\t} 2 { xfail *-*-* } } } */
-/* { dg-final { scan-assembler-times {ori\t} 2 { xfail *-*-* } } } */
-/* { dg-final { scan-assembler-times {not\t} 2 { xfail *-*-* } } } */
+/* { dg-final { scan-assembler-times {srai\t} 2 } } */
+/* { dg-final { scan-assembler-times {ori\t} 2 } } */
+/* { dg-final { scan-assembler-times {not\t} 2 } } */
 
