b996d4509f1724d92a08dae70d8354fbb5561fb7
Wilco Dijkstra <wilco.dijkstra@arm.com>
2025-08-27 17:20:21 +0000

    AArch64: Add isfinite expander [PR 66462]
    Add an expander for isfinite using integer arithmetic.  This is
    typically faster and avoids generating spurious exceptions on
    signaling NaNs.  This fixes part of PR66462.
    
    int isfinite1 (float x) { return __builtin_isfinite (x); }
    
    Before:
    	fabs    s0, s0
    	mov     w0, 2139095039
    	fmov    s31, w0
    	fcmp    s0, s31
    	cset    w0, hi
    	eor     w0, w0, 1
    	ret
    
    After:
    	fmov    w1, s0
    	mov     w0, -16777216
    	cmp     w0, w1, lsl 1
    	cset    w0, hi
    	ret
    
    gcc:
    	PR middle-end/66462
    	* config/aarch64/aarch64.md (isfinite<mode>2): Add new expander.
    
    gcc/testsuite:
    	PR middle-end/66462
    	* gcc.target/aarch64/pr66462.c: Add tests for isfinite.

--- gcc/testsuite/gcc.target/aarch64/pr66462.c
+++ gcc/testsuite/gcc.target/aarch64/pr66462.c
@@ -24,6 +24,26 @@ static void t_inf (double x, bool res)
     __builtin_abort ();
 }
 
+static void t_finf (float x, bool res)
+{
+  if (__builtin_isfinite (x) != res)
+    __builtin_abort ();
+  if (__builtin_isfinite (-x) != res)
+    __builtin_abort ();
+  if (fetestexcept (FE_INVALID))
+    __builtin_abort ();
+}
+
+static void t_fin (double x, bool res)
+{
+  if (__builtin_isfinite (x) != res)
+    __builtin_abort ();
+  if (__builtin_isfinite (-x) != res)
+    __builtin_abort ();
+  if (fetestexcept (FE_INVALID))
+    __builtin_abort ();
+}
+
 int
 main ()
 {
@@ -40,6 +60,18 @@ main ()
   t_inf (__builtin_inf (), 1);
   t_inf (__builtin_nans (""), 0);
   t_inf (__builtin_nan (""), 0);
+
+  t_finf (0.0f, 1);
+  t_finf (1.0f, 1);
+  t_finf (__builtin_inff (), 0);
+  t_finf (__builtin_nansf (""), 0);
+  t_finf (__builtin_nanf (""), 0);
+
+  t_fin (0.0, 1);
+  t_fin (1.0, 1);
+  t_fin (__builtin_inf (), 0);
+  t_fin (__builtin_nans (""), 0);
+  t_fin (__builtin_nan (""), 0);
 
   return 0;
 }
