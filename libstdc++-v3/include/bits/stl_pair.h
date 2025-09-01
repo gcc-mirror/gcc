314490946370821584ce5a01c42a278f57885541
Jonathan Wakely <jwakely@redhat.com>
2025-09-01 18:12:27 +0100

    libstdc++: Fix std::get<T> for std::pair with reference members [PR121745]
    Make the std::get<T> overloads for rvalues use std::forward<T>(p.first)
    not std::move(p.first), so that lvalue reference members are not
    incorrectly converted to rvalues.
    
    It might appear that std::move(p).first would also work, but the
    language rules say that for std::pair<T&&, U> that would produce T&
    rather than the expected T&& (see the discussion in P2445R1 §8.2).
    
    Additional tests are added to verify all combinations of reference
    members, value categories, and const-qualification.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/121745
    	* include/bits/stl_pair.h (get): Use forward instead of move in
    	std::get<T> overloads for rvalue pairs.
    	* testsuite/20_util/pair/astuple/get_by_type.cc: Check all value
    	categories and cv-qualification.
    
    Reviewed-by: Tomasz Kamiński <tkaminsk@redhat.com>
    (cherry picked from commit c8a24f60b6874fca4fb3adb153f8d5f1dd72b951)

--- libstdc++-v3/include/bits/stl_pair.h
+++ libstdc++-v3/include/bits/stl_pair.h
@@ -1067,12 +1067,12 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
   template <typename _Tp, typename _Up>
     constexpr _Tp&&
     get(pair<_Tp, _Up>&& __p) noexcept
-    { return std::move(__p.first); }
+    { return std::forward<_Tp>(__p.first); }
 
   template <typename _Tp, typename _Up>
     constexpr const _Tp&&
     get(const pair<_Tp, _Up>&& __p) noexcept
-    { return std::move(__p.first); }
+    { return std::forward<const _Tp>(__p.first); }
 
   template <typename _Tp, typename _Up>
     constexpr _Tp&
@@ -1087,12 +1087,12 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
   template <typename _Tp, typename _Up>
     constexpr _Tp&&
     get(pair<_Up, _Tp>&& __p) noexcept
-    { return std::move(__p.second); }
+    { return std::forward<_Tp>(__p.second); }
 
   template <typename _Tp, typename _Up>
     constexpr const _Tp&&
     get(const pair<_Up, _Tp>&& __p) noexcept
-    { return std::move(__p.second); }
+    { return std::forward<const _Tp>(__p.second); }
 
 #if __cplusplus > 202002L
   template<typename _T1, typename _T2, typename _U1, typename _U2,
