193b28649933685f4dd3824c01aee4f843cc66b5
Jakub Jelinek <jakub@redhat.com>
2025-09-05 10:54:53 +0200

    testsuite, powerpc, v2: Fix vsx-vectorize-* after alignment peeling [PR118567]
    On Tue, Jul 01, 2025 at 02:50:40PM -0500, Segher Boessenkool wrote:
    > No tests become good tests without effort.  And tests that are not good
    > tests require constant maintenance!
    
    Here are two patches, either just the first one or both can be used
    and both were tested on powerpc64le-linux.
    
    The first one removes all the checking etc. stuff from the testcases,
    as they are just dg-do compile, for the vectorize dump checks all we
    care about are the vectorized loops they want to test.
    
    2025-09-05  Jakub Jelinek  <jakub@redhat.com>
    
    	PR testsuite/118567
    	* gcc.target/powerpc/vsx-vectorize-1.c: Remove includes, checking
    	part of main1 and main.
    	* gcc.target/powerpc/vsx-vectorize-2.c: Remove includes, replace
    	bar definition with declaration, remove main.
    	* gcc.target/powerpc/vsx-vectorize-3.c: Likewise.
    	* gcc.target/powerpc/vsx-vectorize-4.c: Likewise.
    	* gcc.target/powerpc/vsx-vectorize-5.c: Likewise.
    	* gcc.target/powerpc/vsx-vectorize-6.c: Likewise.
    	* gcc.target/powerpc/vsx-vectorize-7.c: Likewise.
    	* gcc.target/powerpc/vsx-vectorize-8.c: Likewise.

--- gcc/testsuite/gcc.target/powerpc/vsx-vectorize-7.c
+++ gcc/testsuite/gcc.target/powerpc/vsx-vectorize-7.c
@@ -4,28 +4,10 @@
 /* { dg-require-effective-target powerpc_vsx } */
 
 /* Taken from vect/vect-95.c.  */
-#include <stdlib.h>
-#include <stdarg.h>
 
 #define N 256
 
-__attribute__ ((noinline))
-void bar (int *pd, int *pa, int *pb, int *pc) 
-{
-  int i;
-
-  /* check results:  */
-  for (i = 0; i < N; i++)
-    {
-      if (pa[i] != (pb[i] * pc[i]))
-	abort ();
-      if (pd[i] != 5.0)
-	abort ();
-    }
-
-  return;
-}
-
+void bar (int *pd, int *pa, int *pb, int *pc);
 
 __attribute__ ((noinline)) int
 main1 (int n, int * __restrict__ pd, int * __restrict__ pa, int * __restrict__ pb, int * __restrict__ pc)
@@ -39,20 +21,6 @@ main1 (int n, int * __restrict__ pd, int * __restrict__ pa, int * __restrict__ p
     }
 
   bar (pd,pa,pb,pc);
-
-  return 0;
-}
-
-int main (void)
-{
-  int i;
-  int a[N] __attribute__ ((__aligned__(__BIGGEST_ALIGNMENT__)));
-  int d[N+1] __attribute__ ((__aligned__(__BIGGEST_ALIGNMENT__)));
-  int b[N] = {0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57};
-  int c[N] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
-
-  main1 (N,&d[1],a,b,c);
-  main1 (N-2,&d[1],a,b,c);
 
   return 0;
 }
