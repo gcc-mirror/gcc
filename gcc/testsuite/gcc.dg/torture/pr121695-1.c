052c1d8284b1eb16474473b341fd83f99e8664c8
Georg-Johann Lay <avr@gjlay.de>
2025-09-05 15:45:24 +0200

    testsuite: Fix gcc.dg/torture/pr121695-1.c
    This test case fails on int < 32-bit platforms obviously.
    This patch undoes the macro expansion from stdint.h.
    
    gcc/testsuite/
    	PR testsuite/121695
    	PR testsuite/52641
    	* gcc.dg/torture/pr121695-1.c: int -> int32_t etc.

--- gcc/testsuite/gcc.dg/torture/pr121695-1.c
+++ gcc/testsuite/gcc.dg/torture/pr121695-1.c
@@ -1,17 +1,17 @@
 /* { dg-do compile } */
 /* PR tree-optimization/121695 */
 
-int ac;
+__INT32_TYPE__ ac;
 char p;
-int *r;
-static unsigned t = 7;
-int q() {
-  int v;
+__INT32_TYPE__ *r;
+static __UINT32_TYPE__ t = 7;
+__INT32_TYPE__ q() {
+  __INT32_TYPE__ v;
 af: {
-  int ag[3];
-  int *ah = &ag[1];
+  __INT32_TYPE__ ag[3];
+  __INT32_TYPE__ *ah = &ag[1];
   for (; ac;) {
-    int ai = 3971866093;
+    __INT32_TYPE__ ai = 3971866093;
     if (0 >= *ah && (*r = 1))
       *ah &= ai;
     else {
