8d8b6249d83609e56752ee51d2686b2f5fb062e6
Jakub Jelinek <jakub@redhat.com>
2025-09-05 10:59:42 +0200

    testsuite, powerpc, v2: Fix vsx-vectorize-* after alignment peeling [PR118567]
    On Tue, Jul 01, 2025 at 02:50:40PM -0500, Segher Boessenkool wrote:
    > No tests become good tests without effort.  And tests that are not good
    > tests require constant maintenance!
    
    Here are two patches, either just the first one or both can be used
    and both were tested on powerpc64le-linux.
    
    The second one adds further 8 tests, which are dg-do run which #include
    the former tests, don't do any dump tests and just define the checking/main
    for those.
    
    2025-09-05  Jakub Jelinek  <jakub@redhat.com>
    
    	PR testsuite/118567
    	* gcc.target/powerpc/vsx-vectorize-9.c: New test.
    	* gcc.target/powerpc/vsx-vectorize-10.c: New test.
    	* gcc.target/powerpc/vsx-vectorize-11.c: New test.
    	* gcc.target/powerpc/vsx-vectorize-12.c: New test.
    	* gcc.target/powerpc/vsx-vectorize-13.c: New test.
    	* gcc.target/powerpc/vsx-vectorize-14.c: New test.
    	* gcc.target/powerpc/vsx-vectorize-15.c: New test.
    	* gcc.target/powerpc/vsx-vectorize-16.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/vsx-vectorize-16.c
@@ -0,0 +1,39 @@
+/* { dg-do run } */
+/* { dg-options "-O2 -ftree-vectorize -fno-vect-cost-model -fdump-tree-vect-details" } */
+
+/* Taken from vect/vect-95.c.  */
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include "vsx-vectorize-8.c"
+
+__attribute__ ((noinline))
+void bar (short *pd, short *pa, short *pb, short *pc) 
+{
+  int i;
+
+  /* check results:  */
+  for (i = 0; i < N; i++)
+    {
+      if (pa[i] != (pb[i] * pc[i]))
+	abort ();
+      if (pd[i] != 5.0)
+	abort ();
+    }
+
+  return;
+}
+
+int main (void)
+{
+  int i;
+  short a[N] __attribute__ ((__aligned__(__BIGGEST_ALIGNMENT__)));
+  short d[N+1] __attribute__ ((__aligned__(__BIGGEST_ALIGNMENT__)));
+  short b[N] = {0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57};
+  short c[N] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+
+  main1 (N,&d[1],a,b,c);
+  main1 (N-2,&d[1],a,b,c);
+
+  return 0;
+}
