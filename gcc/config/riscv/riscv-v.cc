9da7ccf12e6d5f8c4d73e99cd9d9013e84d634e1
Robin Dapp <rdapp@ventanamicro.com>
2025-10-07 17:17:22 +0200

    RISC-V: Remove gather scale and offset handling.
    With the recent vectorizer changes upstream the vectorizer can take care
    of offset extension and scaling (and its proper costing) itself.
    Thus, we can remove all related handling in expand_gather_scatter and
    set the predicates in the gather/scatter expanders to what our
    instructions actually support.
    
    gcc/ChangeLog:
    
    	* config/riscv/autovec.md: Use const_1_operand for scale and
    	extend predicates.
    	* config/riscv/riscv-v.cc (expand_gather_scatter): Remove scale
    	and extension handling.

--- gcc/config/riscv/riscv-v.cc
+++ gcc/config/riscv/riscv-v.cc
@@ -4747,8 +4747,6 @@ void
 expand_gather_scatter (rtx *ops, bool is_load)
 {
   rtx ptr, vec_offset, vec_reg;
-  bool zero_extend_p;
-  int shift;
   rtx mask = ops[5];
   rtx len = ops[6];
   if (is_load)
@@ -4758,77 +4756,17 @@ expand_gather_scatter (rtx *ops, bool is_load)
       vec_reg = ops[0];
       ptr = ops[1];
       vec_offset = ops[2];
-      zero_extend_p = INTVAL (ops[3]);
-      shift = exact_log2 (INTVAL (ops[4]));
     }
   else
     {
       vec_reg = ops[4];
       ptr = ops[0];
       vec_offset = ops[1];
-      zero_extend_p = INTVAL (ops[2]);
-      shift = exact_log2 (INTVAL (ops[3]));
     }
 
   machine_mode vec_mode = GET_MODE (vec_reg);
   machine_mode idx_mode = GET_MODE (vec_offset);
-  scalar_mode inner_idx_mode = GET_MODE_INNER (idx_mode);
-  unsigned inner_offsize = GET_MODE_BITSIZE (inner_idx_mode);
-  poly_int64 nunits = GET_MODE_NUNITS (vec_mode);
   bool is_vlmax = is_vlmax_len_p (vec_mode, len);
-
-  bool use_widening_shift = false;
-
-  /* Extend the offset element to address width.  */
-  if (inner_offsize < BITS_PER_WORD)
-    {
-      use_widening_shift = TARGET_ZVBB && zero_extend_p && shift == 1;
-      /* 7.2. Vector Load/Store Addressing Modes.
-	 If the vector offset elements are narrower than XLEN, they are
-	 zero-extended to XLEN before adding to the ptr effective address. If
-	 the vector offset elements are wider than XLEN, the least-significant
-	 XLEN bits are used in the address calculation. An implementation must
-	 raise an illegal instruction exception if the EEW is not supported for
-	 offset elements.
-
-	 RVV spec only refers to the shift == 0 case.  */
-      if (!zero_extend_p || shift)
-	{
-	  if (zero_extend_p)
-	    inner_idx_mode
-	      = int_mode_for_size (inner_offsize * 2, 0).require ();
-	  else
-	    inner_idx_mode = int_mode_for_size (BITS_PER_WORD, 0).require ();
-	  machine_mode new_idx_mode
-	    = get_vector_mode (inner_idx_mode, nunits).require ();
-	  if (!use_widening_shift)
-	    {
-	      rtx tmp = gen_reg_rtx (new_idx_mode);
-	      emit_insn (gen_extend_insn (tmp, vec_offset, new_idx_mode, idx_mode,
-					  zero_extend_p ? true : false));
-	      vec_offset = tmp;
-	    }
-	  idx_mode = new_idx_mode;
-	}
-    }
-
-  if (shift)
-    {
-      rtx tmp;
-      if (!use_widening_shift)
-	tmp = expand_binop (idx_mode, ashl_optab, vec_offset,
-			    gen_int_mode (shift, Pmode), NULL_RTX, 0,
-			    OPTAB_DIRECT);
-      else
-	{
-	  tmp = gen_reg_rtx (idx_mode);
-	  insn_code icode = code_for_pred_vwsll_scalar (idx_mode);
-	  rtx ops[] = {tmp, vec_offset, const1_rtx};
-	  emit_vlmax_insn (icode, BINARY_OP, ops);
-	}
-
-      vec_offset = tmp;
-    }
 
   insn_code icode = prepare_gather_scatter (vec_mode, idx_mode, is_load);
   if (is_vlmax)
