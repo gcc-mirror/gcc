f90f1c1a0166375144978ed16817d34065175d44
Jose E. Marchesi <jose.marchesi@oracle.com>
2025-10-11 19:43:16 +0200

    a68: powerpc specific support
    Some code in the rs6000 port relies on parsing the language name.
    This commit makes that code to recognize "GNU Algol 68".
    
    Signed-off-by: Jose E. Marchesi <jemarch@gnu.org>
    
    gcc/ChangeLog
    
    	* config/rs6000/rs6000-logue.cc (rs6000_output_function_epilogue):
    	Handle "GNU Algol 68" in language_string.

--- gcc/config/rs6000/rs6000-logue.cc
+++ gcc/config/rs6000/rs6000-logue.cc
@@ -5332,18 +5332,18 @@ rs6000_output_function_epilogue (FILE *file)
       /* Tbtab format type.  Use format type 0.  */
       fputs ("\t.byte 0,", file);
 
-      /* Language type.  Unfortunately, there does not seem to be any
-	 official way to discover the language being compiled, so we
-	 use language_string.
-	 C is 0.  Fortran is 1.  Ada is 3.  Modula-2 is 8.  C++ is 9.
-	 Java is 13.  Objective-C is 14.  Objective-C++ isn't assigned
-	 a number, so for now use 9.  LTO, Go, D, and JIT aren't assigned
-	 numbers either, so for now use 0.  */
+      /* Language type.  Unfortunately, there does not seem to be any official
+	 way to discover the language being compiled, so we use
+	 language_string.  C is 0.  Fortran is 1.  Ada is 3.  Modula-2 is 8.
+	 C++ is 9.  Java is 13.  Objective-C is 14.  Objective-C++ isn't
+	 assigned a number, so for now use 9.  LTO, Go, D, Algol 68 and JIT
+	 aren't assigned numbers either, so for now use 0.  */
       if (lang_GNU_C ()
 	  || ! strcmp (language_string, "GNU GIMPLE")
 	  || ! strcmp (language_string, "GNU Go")
 	  || ! strcmp (language_string, "GNU D")
 	  || ! strcmp (language_string, "GNU Rust")
+	  || ! strcmp (language_string, "GNU Algol 68")
 	  || ! strcmp (language_string, "libgccjit"))
 	i = 0;
       else if (! strcmp (language_string, "GNU F77")
