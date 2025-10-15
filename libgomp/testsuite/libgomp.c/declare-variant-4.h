b3c0e9aadbee55636f76057b4eecb01807156fae
Tobias Burnus <tburnus@baylibre.com>
2025-10-15 19:15:15 +0200

    gcn: Add missing GFX9_4_GENERIC, OpenMP context-selector update
    The definition for gfx942 and gfx950 missed the GFX9_4_GENERIC
    family flag.
    
    For OpenMP context selectors: The t-omp-device file missed the
    generic selectors.
    
    Additionally, there is now a note in the OpenMP documentation that
    there is a one-to-one match for ISA names, ignoring any compatibility.
    For instance, for Nvidia GPUs 'isa("sm_70")' is only true when compiling
    for 'sm_70', even though sm < 7.0 code also runs on sm_70 hardware.
    And, for AMD GPUs, gfx9-4-generic neither matches 'gfx942'
    (even though such generic code runs on gfx942) - nor the reverse
    (although all gfx9-4-generic code runs on gfx942).
    
    gcc/ChangeLog:
    
    	* config/gcn/gcn-devices.def (gfx942, gfx950): Set generic name
    	to GFX9_4_GENERIC.
    	* config/gcn/t-omp-device: Include generic names for OpenMP's
    	ISA trait.
    
    libgomp/ChangeLog:
    
    	* libgomp.texi (OpenMP Context Selectors): Add note that there is
    	currently an exact match between ISA and compilation, ignoring
    	compatibilities in both ways.
    	* testsuite/libgomp.c/declare-variant-4.h: Add missing variant
    	functions for specific and generic AMD GPUs.
    	* testsuite/libgomp.c/declare-variant-4-gfx10-3-generic.c: New test.
    	* testsuite/libgomp.c/declare-variant-4-gfx11-generic.c: New test.
    	* testsuite/libgomp.c/declare-variant-4-gfx9-4-generic.c: New test.
    	* testsuite/libgomp.c/declare-variant-4-gfx9-generic.c: New test.

--- libgomp/testsuite/libgomp.c/declare-variant-4.h
+++ libgomp/testsuite/libgomp.c/declare-variant-4.h
@@ -9,6 +9,20 @@ gfx900 (void)
 
 __attribute__ ((noipa))
 int
+gfx902 (void)
+{
+  return 0x902;
+}
+
+__attribute__ ((noipa))
+int
+gfx904 (void)
+{
+  return 0x904;
+}
+
+__attribute__ ((noipa))
+int
 gfx906 (void)
 {
   return 0x906;
@@ -19,6 +33,13 @@ int
 gfx908 (void)
 {
   return 0x908;
+}
+
+__attribute__ ((noipa))
+int
+gfx909 (void)
+{
+  return 0x909;
 }
 
 __attribute__ ((noipa))
@@ -44,9 +65,51 @@ gfx942 (void)
 
 __attribute__ ((noipa))
 int
+gfx950 (void)
+{
+  return 0x950;
+}
+
+__attribute__ ((noipa))
+int
 gfx1030 (void)
 {
   return 0x1030;
+}
+
+__attribute__ ((noipa))
+int
+gfx1031 (void)
+{
+  return 0x1031;
+}
+
+__attribute__ ((noipa))
+int
+gfx1032 (void)
+{
+  return 0x1032;
+}
+
+__attribute__ ((noipa))
+int
+gfx1033 (void)
+{
+  return 0x1033;
+}
+
+__attribute__ ((noipa))
+int
+gfx1034 (void)
+{
+  return 0x1034;
+}
+
+__attribute__ ((noipa))
+int
+gfx1035 (void)
+{
+  return 0x1035;
 }
 
 __attribute__ ((noipa))
@@ -65,21 +128,111 @@ gfx1100 (void)
 
 __attribute__ ((noipa))
 int
+gfx1101 (void)
+{
+  return 0x1101;
+}
+
+__attribute__ ((noipa))
+int
+gfx1102 (void)
+{
+  return 0x1102;
+}
+
+__attribute__ ((noipa))
+int
 gfx1103 (void)
 {
   return 0x1103;
 }
 
+__attribute__ ((noipa))
+int
+gfx1150 (void)
+{
+  return 0x1150;
+}
+
+__attribute__ ((noipa))
+int
+gfx1151 (void)
+{
+  return 0x1151;
+}
+
+__attribute__ ((noipa))
+int
+gfx1152 (void)
+{
+  return 0x1152;
+}
+
+__attribute__ ((noipa))
+int
+gfx1153 (void)
+{
+  return 0x1153;
+}
+
+__attribute__ ((noipa))
+int
+gfx9_generic (void)
+{
+  return 0x90ff;
+}
+
+__attribute__ ((noipa))
+int
+gfx9_4_generic (void)
+{
+  return 0x94ff;
+}
+
+__attribute__ ((noipa))
+int
+gfx10_3_generic (void)
+{
+  return 0x103ff;
+}
+
+__attribute__ ((noipa))
+int
+gfx11_generic (void)
+{
+  return 0x110ff;
+}
+
+
 #pragma omp declare variant(gfx900) match(device = {isa("gfx900")})
+#pragma omp declare variant(gfx902) match(device = {isa("gfx902")})
+#pragma omp declare variant(gfx904) match(device = {isa("gfx904")})
 #pragma omp declare variant(gfx906) match(device = {isa("gfx906")})
 #pragma omp declare variant(gfx908) match(device = {isa("gfx908")})
+#pragma omp declare variant(gfx909) match(device = {isa("gfx909")})
 #pragma omp declare variant(gfx90a) match(device = {isa("gfx90a")})
 #pragma omp declare variant(gfx90c) match(device = {isa("gfx90c")})
 #pragma omp declare variant(gfx942) match(device = {isa("gfx942")})
+#pragma omp declare variant(gfx950) match(device = {isa("gfx950")})
 #pragma omp declare variant(gfx1030) match(device = {isa("gfx1030")})
+#pragma omp declare variant(gfx1031) match(device = {isa("gfx1031")})
+#pragma omp declare variant(gfx1032) match(device = {isa("gfx1032")})
+#pragma omp declare variant(gfx1033) match(device = {isa("gfx1033")})
+#pragma omp declare variant(gfx1034) match(device = {isa("gfx1034")})
+#pragma omp declare variant(gfx1035) match(device = {isa("gfx1035")})
 #pragma omp declare variant(gfx1036) match(device = {isa("gfx1036")})
 #pragma omp declare variant(gfx1100) match(device = {isa("gfx1100")})
+#pragma omp declare variant(gfx1101) match(device = {isa("gfx1101")})
+#pragma omp declare variant(gfx1102) match(device = {isa("gfx1102")})
 #pragma omp declare variant(gfx1103) match(device = {isa("gfx1103")})
+#pragma omp declare variant(gfx1150) match(device = {isa("gfx1150")})
+#pragma omp declare variant(gfx1151) match(device = {isa("gfx1151")})
+#pragma omp declare variant(gfx1152) match(device = {isa("gfx1152")})
+#pragma omp declare variant(gfx1153) match(device = {isa("gfx1153")})
+#pragma omp declare variant(gfx9_generic) match(device = {isa("gfx9-generic")})
+#pragma omp declare variant(gfx9_4_generic) match(device = {isa("gfx9-4-generic")})
+#pragma omp declare variant(gfx10_3_generic) match(device = {isa("gfx10-3-generic")})
+#pragma omp declare variant(gfx11_generic) match(device = {isa("gfx11-generic")})
 __attribute__ ((noipa))
 int
 f (void)
