be848078841cbd937ff0ce7bb1564e09d5e2959e
Pan Li <pan2.li@intel.com>
2025-10-20 21:08:46 +0800

    RISC-V: Add testcase for unsigned scalar SAT_MUL form 7
    The form 7 of unsigned scalar SAT_MUL has supported from the
    previous change.  Thus, add the test cases to make sure it
    works well.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/sat/sat_arith.h: Add test helper macros.
    	* gcc.target/riscv/sat/sat_u_mul-8-u16-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-8-u16-from-u32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-8-u16-from-u64.rv32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-8-u16-from-u64.rv64.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-8-u32-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-8-u32-from-u64.rv32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-8-u32-from-u64.rv64.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-8-u64-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-8-u8-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-8-u8-from-u16.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-8-u8-from-u32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-8-u8-from-u64.rv32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-8-u8-from-u64.rv64.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-8-u16-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-8-u16-from-u32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-8-u16-from-u64.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-8-u32-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-8-u32-from-u64.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-8-u64-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-8-u8-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-8-u8-from-u16.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-8-u8-from-u32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-8-u8-from-u64.c: New test.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

--- gcc/testsuite/gcc.target/riscv/sat/sat_arith.h
+++ gcc/testsuite/gcc.target/riscv/sat/sat_arith.h
@@ -770,4 +770,19 @@ sat_u_mul_##NT##_from_##WT##_fmt_6 (NT a, NT b) \
   sat_u_mul_##NT##_from_##WT##_fmt_6 (a, b)
 #define RUN_SAT_U_MUL_FMT_6_WRAP(NT, WT, a, b) RUN_SAT_U_MUL_FMT_6(NT, WT, a, b)
 
+#define DEF_SAT_U_MUL_FMT_7(NT, WT)             \
+NT __attribute__((noinline))                    \
+sat_u_mul_##NT##_from_##WT##_fmt_7 (NT a, NT b) \
+{                                               \
+  WT x = (WT)a * (WT)b;                         \
+  NT max = -1;                                  \
+  bool overflow_p = x > (WT)(max);              \
+  return -(NT)(overflow_p) | (NT)x;             \
+}
+
+#define DEF_SAT_U_MUL_FMT_7_WRAP(NT, WT) DEF_SAT_U_MUL_FMT_7(NT, WT)
+#define RUN_SAT_U_MUL_FMT_7(NT, WT, a, b) \
+  sat_u_mul_##NT##_from_##WT##_fmt_7 (a, b)
+#define RUN_SAT_U_MUL_FMT_7_WRAP(NT, WT, a, b) RUN_SAT_U_MUL_FMT_7(NT, WT, a, b)
+
 #endif
---
+++ gcc/testsuite/gcc.target/riscv/sat/sat_u_mul-run-8-u8-from-u64.c
@@ -0,0 +1,16 @@
+/* { dg-do run { target { rv32 || rv64 } } } */
+/* { dg-additional-options "-std=c99" } */
+
+#include "sat_arith.h"
+#include "sat_arith_data.h"
+
+#define NT               uint8_t
+#define WT               uint64_t
+#define NAME             usmul
+#define DATA             TEST_BINARY_DATA_WRAP(NT, NAME)
+#define T                TEST_BINARY_STRUCT_DECL_WRAP(NT, NAME)
+#define RUN_BINARY(x, y) RUN_SAT_U_MUL_FMT_7_WRAP(NT, WT, x, y)
+
+DEF_SAT_U_MUL_FMT_7_WRAP(NT, WT)
+
+#include "scalar_sat_binary_run_xxx.h"
