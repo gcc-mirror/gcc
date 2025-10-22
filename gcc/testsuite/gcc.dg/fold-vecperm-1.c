a46dffee33a3a4bf52eb2d493aace3e77068318f
Pengfei Li <Pengfei.Li2@arm.com>
2025-10-22 11:17:07 +0000

    match.pd: Fold VEC_PERM_EXPR chains implementing concat-and-extract
    When compiling the following code with SIMDe on AArch64:
    
    	__m128i lo = _mm_srli_si128(a, 12);
    	__m128i hi = _mm_slli_si128(b, 4);
    	__m128i res = _mm_blend_epi16(hi, lo, 3);
    
    current GCC produces:
    
    	mov     v31.4s, 0
    	ext     v30.16b, v0.16b, v31.16b, #12
    	ext     v0.16b, v31.16b, v1.16b, #12
    	ins     v0.s[0], v30.s[0]
    
    instead of the more efficient:
    
    	ext     v0.16b, v0.16b, v1.16b, #12
    
    GCC builds three VEC_PERM_EXPRs for the intrinsic calls. The first two
    implement vector shifts and the final one implements the blend, but they
    use different vector modes. The forward propagation fails to optimize
    this case because VIEW_CONVERT_EXPRs in between block the folding.
    
    This patch adds a match.pd pattern to recognize the concat-and-extract
    idiom and folds the VEC_PERM_EXPR chain, even when VIEW_CONVERT_EXPRs
    split the chain.
    
    Bootstrapped and tested on aarch64-linux-gnu and x86_64-linux-gnu.
    
    gcc/ChangeLog:
    
    	* match.pd: Fold VEC_PERM_EXPR chains implementing vector
    	concat-and-extract.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/fold-vecperm-1.c: New test.

---
+++ gcc/testsuite/gcc.dg/fold-vecperm-1.c
@@ -0,0 +1,23 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+typedef int v4si __attribute__((vector_size(16)));
+typedef short v8hi __attribute__((vector_size(16)));
+
+typedef union {
+  v4si s;
+  v8hi h;
+} int128;
+
+int128 concat (int128 a, int128 b) {
+  int128 x, y, res;
+  v4si zero = { 0, 0, 0, 0 };
+  v4si sel0 = { 3, 4, 5, 6 };
+  v8hi sel1 = { 0, 1, 10, 11, 12, 13, 14, 15 };
+  x.s = __builtin_shuffle (a.s, zero, sel0);
+  y.s = __builtin_shuffle (zero, b.s, sel0);
+  res.h = __builtin_shuffle (x.h, y.h, sel1);
+  return res;
+}
+
+/* { dg-final { scan-tree-dump-times "VEC_PERM_EXPR" 1 "optimized" } } */
