83e96a566890d9044a328c71e76b503c2efd1ba0
Pan Li <pan2.li@intel.com>
2025-11-16 19:12:35 +0800

    RISC-V: Add test for vec_duplicate + vmsne.vv combine case 0 with GR2VR cost 0, 1 and 15
    Add asm dump check and run test for vec_duplicate + vmsne.vv
    combine to vmseq.vx, with the GR2VR cost is 0, 2 and 15.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-1-i16.c: Add asm check
    	for vmsne.vx.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-1-i32.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-1-i64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-1-i8.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-2-i16.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-2-i32.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-2-i64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-2-i8.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-3-i16.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-3-i32.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-3-i64.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx-3-i8.c: Ditto.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_binary.h: Add test
    	helper macros.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_binary_data.h: Add test
    	data for run test.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_vmsne-run-1-i16.c: New test.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_vmsne-run-1-i32.c: New test.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_vmsne-run-1-i64.c: New test.
    	* gcc.target/riscv/rvv/autovec/vx_vf/vx_vmsne-run-1-i8.c: New test.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

--- gcc/testsuite/gcc.target/riscv/rvv/autovec/vx_vf/vx-2-i32.c
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/vx_vf/vx-2-i32.c
@@ -28,3 +28,4 @@ TEST_TERNARY_VX_SIGNED_0(T)
 /* { dg-final { scan-assembler-not {vmadd.vx} } } */
 /* { dg-final { scan-assembler-not {vnmsub.vx} } } */
 /* { dg-final { scan-assembler-not {vmseq.vx} } } */
+/* { dg-final { scan-assembler-not {vmsne.vx} } } */
