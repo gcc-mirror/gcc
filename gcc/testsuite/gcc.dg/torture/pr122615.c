73824bffde7a2226a864313546f8f0bd127a22b7
Daniel Barboza <dbarboza@ventanamicro.com>
2025-11-27 22:03:18 -0300

    match.pd: x&c ?(x op c):(x|c) -> x^c [PR122615, PR122616]
    Add a single pattern to reduce these patterns to "x ^ c":
    
    x & c ? (x - c) | (x | c)
    x & c ? (x & ~c) | (x | c)
    
    As long as "c" has a single bit set.
    
            PR tree-optimization/122615
            PR tree-optimization/122616
    
    gcc/ChangeLog:
    
    	* match.pd (`x & c ? (x - c) | (x | c)`): New pattern.
    	(`x & c ? (x & ~c) | (x | c)`): Likewise.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/torture/pr122615.c: New test.
    	* gcc.dg/torture/pr122616.c: Likewise.
    
    Co-authored-by: Jeff Law <jlaw@ventanamicro.com>
    Signed-off-by: Daniel Barboza <dbarboza@ventanamicro.com>

---
+++ gcc/testsuite/gcc.dg/torture/pr122615.c
@@ -0,0 +1,22 @@
+/* { dg-do compile } */
+/* { dg-options "-fdump-tree-original" } */
+/* { dg-skip-if "" { *-*-* } { "-O0" } { "" } } */
+
+int f1 (int x)
+{
+  return x & 1 ? (x & ~1) : (x | 1);
+}
+
+int f2 (int x)
+{
+  return x & 2 ? (x & ~2) : (x | 2);
+}
+
+int f3 (int x)
+{
+  return x & 3 ? (x & ~3) : (x | 3);
+}
+
+/* { dg-final { scan-tree-dump-times "x \\^ 1" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "x \\^ 2" 1 "original" } } */
+/* { dg-final { scan-tree-dump-times "x \\^ 3" 0 "original" } } */
