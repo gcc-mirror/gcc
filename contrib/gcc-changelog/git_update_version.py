be86130982ae2cc50e64c7359b536c9945026f21
Jakub Jelinek <jakub@redhat.com>
2025-12-02 10:43:52 +0100

    Add e69bea9b4ccfe79efeec6e87d7d24e6350f31375 to ignored_commits
    This fails because we've changed git_commit.py mid day and one commit
    was using the old (incorrect) ChangeLog location.
    
    	* gcc-changelog/git_update_version.py (ignored_commits): Add
    	e69bea9b4ccfe79efeec6e87d7d24e6350f31375.

--- contrib/gcc-changelog/git_update_version.py
+++ contrib/gcc-changelog/git_update_version.py
@@ -48,7 +48,8 @@ ignored_commits = {
         '10d76b7f1e5b63ad6d2b92940c39007913ced037',
         'de3b277247ce98d189f121155b75f490725a42f6',
         '13cf22eb557eb5e3d796822247d8d4957bdb25da',
-        'b40ef6e9dc096c8c19399e94947a1965258a6942'}
+        'b40ef6e9dc096c8c19399e94947a1965258a6942',
+        'e69bea9b4ccfe79efeec6e87d7d24e6350f31375'}
 
 FORMAT = '%(asctime)s:%(levelname)s:%(name)s:%(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT,
