756e32a160ed353709b70e36c32edb5edffc0865
Patrick Palka <ppalka@redhat.com>
2025-12-05 12:14:23 -0500

    libstdc++: Use deducing this in std::not_fn when available [PR111327]
    Implement the perfect forwarding required by std::not_fn using deducing
    this when available, instead of needing 8 operator() overloads.  This
    also fixes Jiang An's test from this PR which would be messy to fix in
    the old implementation.
    
    	PR libstdc++/111327
    
    libstdc++-v3/ChangeLog:
    
    	* include/std/functional (_Not_fn::operator())
    	[_GLIBCXX_EXPLICIT_THIS_PARAMETER]: Define as a single
    	overload using deducing this.
    	* testsuite/20_util/function_objects/not_fn/111327.cc: Extend test.
    
    Reviewed-by: Tomasz Kamiński <tkaminsk@redhat.com>
    Reviewed-by: Jonathan Wakely <jwakely@redhat.com>

--- libstdc++-v3/testsuite/20_util/function_objects/not_fn/111327.cc
+++ libstdc++-v3/testsuite/20_util/function_objects/not_fn/111327.cc
@@ -15,15 +15,28 @@ struct G {
   bool operator()(...) const &&;
 };
 
+struct Weird {
+  void operator()();
+  bool operator()() const { return true; }
+};
+
 int main() {
   auto f = std::not_fn(F{});
-  f(); // { dg-error "deleted" }
+  f(); // { dg-error "no match" }
   std::move(f)();
   std::as_const(f)();
   std::move(std::as_const(f))();
 
   auto g = std::not_fn(G{});
-  g(); // { dg-error "deleted" }
-  std::move(g)(); // { dg-error "deleted" }
+  g(); // { dg-error "no match" }
+  std::move(g)(); // { dg-error "no match" }
   std::move(std::as_const(g))();
+
+  auto h = std::not_fn(Weird{});
+  h(); // { dg-error "no match" }
 }
+
+// { dg-error "no type named 'type' in 'struct std::__invoke_result<" "" { target *-*-* } 0 }
+// { dg-error "no matching function for call to 'std::_Not_fn<Weird>" "" { target *-*-* } 0 }
+// { dg-error "could not convert 'std::declval<void>\\(\\)' from 'void' to 'bool'" "" { target *-*-* } 0 }
+// { dg-error "in argument to unary !" "" { target *-*-* } 0 }
