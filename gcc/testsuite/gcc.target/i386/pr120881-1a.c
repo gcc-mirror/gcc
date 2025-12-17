bd986ddbded2cfcbe3ccdc076e4cba15a7643b65
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2025-12-17 13:20:20 +0100

    testsuite: i386: Restrict gcc.target/i386/pr120881-1?.c to Linux
    Two tests FAIL on Solaris x86:
    
    FAIL: gcc.target/i386/pr120881-1a.c  at line 4 (test for warnings, line )
    FAIL: gcc.target/i386/pr120881-1b.c  at line 4 (test for warnings, line )
    
    producing no message contrary to what the tests expect.  The reason is
    similar to previous instances: Solaris doesn't support -mfentry.  Like
    previous cases, this patch thus restricts them to Linux.
    
    Tested on i386-pc-solaris2.11 and x86_64-pc-linux-gnu.
    
    2025-12-15  Rainer Orth  <ro@CeBiTec.Uni-Bielefeld.DE>
    
    	gcc/testsuite:
    	* gcc.target/i386/pr120881-1a.c: Restrict to Linux.
    	* gcc.target/i386/pr120881-1b.c: Likewise.

--- gcc/testsuite/gcc.target/i386/pr120881-1a.c
+++ gcc/testsuite/gcc.target/i386/pr120881-1a.c
@@ -1,4 +1,4 @@
-/* { dg-do compile { target fpic } } */
+/* { dg-do compile { target { *-*-linux* && fpic } } } */
 /* { dg-require-profiling "-pg" } */
 /* { dg-options "-O2 -pg -mno-fentry -fno-pic" } */
 /* { dg-message "'-pg' without '-mfentry' may be unreliable with shrink wrapping" "" { target *-*-* } 0 } */
--- gcc/testsuite/gcc.target/i386/pr120881-1b.c
+++ gcc/testsuite/gcc.target/i386/pr120881-1b.c
@@ -1,4 +1,4 @@
-/* { dg-do compile { target { fpic && { ! ia32 } } } } */
+/* { dg-do compile { target { { *-*-linux* && { ! ia32 } } && fpic } } } */
 /* { dg-require-profiling "-pg" } */
 /* { dg-options "-O2 -pg -mno-fentry -fpic" } */
 /* { dg-message "'-pg' without '-mfentry' may be unreliable with shrink wrapping" "" { target *-*-* } 0 } */
