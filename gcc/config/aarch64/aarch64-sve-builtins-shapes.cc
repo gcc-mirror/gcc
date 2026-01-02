5e968b9fe53b5f7ffde8521f1c65ebb64b4d9c43
Iain Sandoe <iain@sandoe.co.uk>
2026-01-02 17:48:06 +0000

    aarch64: Define INCLUDE_ALGORITHM for std::max in sve-builtins-shape.
    std::max needs <algorithm>, so arrange for that.
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64-sve-builtins-shapes.cc
    	(INCLUDE_ALGORITHM): Define.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/config/aarch64/aarch64-sve-builtins-shapes.cc
+++ gcc/config/aarch64/aarch64-sve-builtins-shapes.cc
@@ -17,6 +17,7 @@
    along with GCC; see the file COPYING3.  If not see
    <http://www.gnu.org/licenses/>.  */
 
+#define INCLUDE_ALGORITHM
 #include "config.h"
 #include "system.h"
 #include "coretypes.h"
