e474dbc00cf70e7939a67ea26182c1843288b0a1
Rainer Orth <ro@CeBiTec.Uni-Bielefeld.DE>
2026-01-07 14:41:23 +0100

    testsuite: Require vect_hw_misalign in gcc.dg/vect/vect-71.c [PR123197]
    The gcc.dg/vect/vect-71.c test currently FAILs on SPARC (32 and 64-bit):
    
    FAIL: gcc.dg/vect/vect-71.c scan-tree-dump-times vect "vectorized 1 loops" 1
    
    The dump shows
    
    gcc.dg/vect/vect-71.c:18:17: missed:   unsupported unaligned access
    gcc.dg/vect/vect-71.c:20:17: missed:   not vectorized: relevant stmt not supported: ia[_1] = 0;
    
    so the test needs to require vect_hw_misalign.
    
    Tested on sparc-sun-solaris2.11 and i386-pc-solaris2.11.
    
    2026-01-05  Rainer Orth  <ro@CeBiTec.Uni-Bielefeld.DE>
    
    	gcc/testsuite:
    	PR tree-optimization/123197
    	* gcc.dg/vect/vect-71.c: Require vect_hw_misalign.

--- gcc/testsuite/gcc.dg/vect/vect-71.c
+++ gcc/testsuite/gcc.dg/vect/vect-71.c
@@ -1,4 +1,5 @@
 /* { dg-require-effective-target vect_int } */
+/* { dg-require-effective-target vect_hw_misalign } */
 /* { dg-additional-options "-fdump-tree-optimized-details-blocks" } */
 
 #include <stdarg.h>
