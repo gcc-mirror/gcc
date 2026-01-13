80c77d9a1cfbfde3a437361fa95be326fcdf3e58
Andrew Pinski <andrew.pinski@oss.qualcomm.com>
2026-01-12 18:58:47 -0800

    match: Add simplification of `(a*zero_one_valued_p) & b` if `a & b` simplifies [PR119402]
    This is a small reassociation for `a*bool & b` into `(a & b) * bool` checking if
    `a & b` simplifies. Since it could be the case `b` is `~a` or `a` or something
    else that might simplify when anding with `a`.
    
    Note this fixes a regression for aarch64 where the cost of a multiply vs `&-` changed
    in GCC 14 and can no longer optimize some cases at the RTL level.
    
    Bootstrapped and tested on x86_64-linux-gnu.
    
    	PR tree-optimization/119402
    gcc/ChangeLog:
    
    	* match.pd (`(a*zero_one_valued_p) & b`): New pattern.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/tree-ssa/bitops-14.c: New test.
    	* gcc.dg/tree-ssa/bitops-15.c: New test.
    
    Signed-off-by: Andrew Pinski <andrew.pinski@oss.qualcomm.com>

---
+++ gcc/testsuite/gcc.dg/tree-ssa/bitops-15.c
@@ -0,0 +1,24 @@
+/* { dg-do compile }  */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+
+__attribute__((noipa))
+int x(_Bool iftmp, unsigned _6)
+{
+        return ((-iftmp) & _6) & (_6);
+}
+__attribute__((noipa))
+int y(_Bool iftmp, unsigned _6)
+{
+        return (iftmp * _6) & (_6);
+}
+__attribute__((noipa))
+int z(_Bool iftmp, unsigned _6)
+{
+        unsigned t = _6;
+        unsigned t1 = (iftmp ? _6 : 0);
+        return t1 & t;
+}
+
+/* In this case AND should be removed.  */
+/* { dg-final { scan-tree-dump-not " & " "optimized" } } */
