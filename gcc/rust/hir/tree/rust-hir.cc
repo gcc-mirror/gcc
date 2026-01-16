Rust-GCC/gccrs

gccrs: remove match arm pattern vector to single pattern
This patch refactors all uses of vector patterns since a vector of
patterns would be considered as an alt pattern, a vector is considered
useless.

gcc/rust/ChangeLog:

	* ast/rust-ast-builder.cc (Builder::match_arm): Moves the vector of patterns
	to a single pattern.
	* ast/rust-ast-collector.cc (TokenCollector::visit):Likewise.
	* ast/rust-ast-pointer-visitor.cc (PointerVisitor::visit):Likewise.
	* ast/rust-ast-visitor.cc (DefaultASTVisitor::visit):Likewise.
	* ast/rust-ast.cc (IfLetExpr::as_string):Likewise.
	(WhileLetLoopExpr::as_string):Likewise.
	(MatchArm::as_string):Likewise.
	* ast/rust-desugar-question-mark.cc (make_match_arm):Likewise.
	* ast/rust-desugar-while-let.cc (DesugarWhileLet::desugar):Likewise.
	* ast/rust-expr.h (class WhileLetLoopExpr):Likewise.
	(class IfLetExpr):Likewise.
	* backend/rust-compile-expr.cc (CompileExpr::visit):Likewise.
	* checks/errors/rust-hir-pattern-analysis.cc (lower_arm):Likewise.
	* expand/rust-cfg-strip.cc (CfgStrip::visit):Likewise.
	* hir/rust-ast-lower.cc (ASTLoweringIfLetBlock::desugar_iflet):Likewise.
	(ASTLoweringExprWithBlock::visit):Likewise.
	* hir/rust-hir-dump.cc (Dump::do_matcharm):Likewise.
	(Dump::visit):Likewise.
	* hir/tree/rust-hir-expr.cc (OperatorExpr::operator=):Likewise.
	(ArithmeticOrLogicalExpr::operator=):Likewise.
	(ComparisonExpr::operator=):Likewise.
	(LazyBooleanExpr::operator=):Likewise.
	(TypeCastExpr::operator=):Likewise.
	(AssignmentExpr::operator=):Likewise.
	(CompoundAssignmentExpr::operator=):Likewise.
	(GroupedExpr::operator=):Likewise.
	(ArrayExpr::operator=):Likewise.
	(ArrayIndexExpr::operator=):Likewise.
	(CallExpr::operator=):Likewise.
	(MethodCallExpr::operator=):Likewise.
	(FieldAccessExpr::operator=):Likewise.
	(BlockExpr::operator=):Likewise.
	(BreakExpr::operator=):Likewise.
	(ReturnExpr::operator=):Likewise.
	(UnsafeBlockExpr::operator=):Likewise.
	(BaseLoopExpr::operator=):Likewise.
	(WhileLetLoopExpr::WhileLetLoopEx

gcc/rust/hir/tree/rust-hir.cc
@@ -1762,16 +1762,13 @@ WhileLetLoopExpr::as_string () const
     }
 
   str += "\n Match arm patterns: ";
-  if (match_arm_patterns.empty ())
+  if (match_arm_pattern == nullptr)
     {
       str += "none";
     }
   else
     {
-      for (const auto &pattern : match_arm_patterns)
-	{
-	  str += "\n  " + pattern->as_string ();
-	}
+      str += "\n  " + match_arm_pattern->as_string ();
     }
 
   str += "\n Scrutinee expr: " + condition->as_string ();
@@ -1885,17 +1882,10 @@ MatchArm::as_string () const
     }
 
   str += "\nPatterns: ";
-  if (match_arm_patterns.empty ())
-    {
-      str += "none";
-    }
+  if (match_arm_pattern == nullptr)
+    str += "none";
   else
-    {
-      for (const auto &pattern : match_arm_patterns)
-	{
-	  str += "\n " + pattern->as_string ();
-	}
-    }
+    str += "\n " + match_arm_pattern->as_string ();
 
   str += "\nGuard expr: ";
   if (!has_match_arm_guard ())

gcc-mirror/gcc

	* parse/rust-parse-impl-expr.hxx: Likewise.
	* parse/rust-parse-impl.hxx: Likewise.

gcc/rust/hir/tree/rust-hir.cc
@@ -1775,16 +1775,13 @@ WhileLetLoopExpr::to_string () const
     }
 
   str += "\n Match arm patterns: ";
-  if (match_arm_patterns.empty ())
+  if (match_arm_pattern == nullptr)
     {
       str += "none";
     }
   else
     {
-      for (const auto &pattern : match_arm_patterns)
-	{
-	  str += "\n  " + pattern->to_string ();
-	}
+      str += "\n  " + match_arm_pattern->to_string ();
     }
 
   str += "\n Scrutinee expr: " + condition->to_string ();
@@ -1898,17 +1895,10 @@ MatchArm::to_string () const
     }
 
   str += "\nPatterns: ";
-  if (match_arm_patterns.empty ())
-    {
-      str += "none";
-    }
+  if (match_arm_pattern == nullptr)
+    str += "none";
   else
-    {
-      for (const auto &pattern : match_arm_patterns)
-	{
-	  str += "\n " + pattern->to_string ();
-	}
-    }
+    str += "\n " + match_arm_pattern->to_string ();
 
   str += "\nGuard expr: ";
   if (!has_match_arm_guard ())