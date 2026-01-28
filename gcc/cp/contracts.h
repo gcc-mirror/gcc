iains/gcc-git

c++, contracts: C++26 base implementation as per P2900R14.
This implementation performs the contracts application in three
phases:
    1. Parsing and semantic checks.
       Most of the code for this is in the parser, with helpers provided here.
    2. Emitting contract assertion AST nodes into function bodies.
       This is initiated from "finish_function ()"
    3. Lowering the contract assertion AST nodes to control flow, constant
       data and calls to the violation handler.
       This is initiated from "cp_genericize ()".

  Contract Assertion State
  ========================

  contract_assert () does not require any special handling and can be
  represented directly by AST inserted in the function body.

  'pre' and 'post' function contract specifiers require most of the special
  handling, since they must be tracked across re-declarations of functions and
  there are contraints on how such specifiers may change in these cases.

  The contracts specification identifies a "first declaration" of any given
  function - which is the first encountered when parsing a given TU.
  Subsequent re-declarations may not add or change the function contract
  specifiers from any introduced on this first declaration.  It is, however,
  permitted to omit specifiers on re-declarations.

  Since the implementation of GCC's (re-)declarations is a destructive merge
  we need to keep some state on the side to determine whether the re-declaration
  rules are met.  In this current design we have chosen not to add another tree
  to each function decl but, instead, keep a map from function decl to contract
  specifier state.  In this state we record the 'first declaration' specifiers
  which are used to validate re-declaration(s) and to report the initial state
  in diagnostics.

  We need (for example) to compare
    pre ( x > 2 ) equal to
    pre ( z > 2 ) when x and z refer to the same function parameter in a
    re-declaration.

  The mechanism used to determine if two contrac

gcc/cp/contracts.h
@@ -1,7 +1,12 @@
-/* Definitions for C++ contract levels.  Implements functionality described in
-   the N4820 working draft version of contracts, P1290, P1332, and P1429.
+/* Definitions for C++26 contracts.
+
    Copyright (C) 2020-2025 Free Software Foundation, Inc.
-   Contributed by Jeff Chapman II (jchapman@lock3software.com)
+   Originally by Jeff Chapman II (jchapman@lock3software.com) for proposed
+   C++20 contracts.
+   Rewritten for C++26 contracts by:
+     Nina Ranns (dinka.ranns@googlemail.com)
+     Iain Sandoe (iain@sandoe.co.uk)
+     Ville Voutilainen (ville.voutilainen@gmail.com).
 
 This file is part of GCC.
 
@@ -22,5 +27,161 @@ along with GCC; see the file COPYING3.  If not see
 #ifndef GCC_CP_CONTRACT_H
 #define GCC_CP_CONTRACT_H
 
+#include <cstdint>
 
+/* Now our contracts are stored separately from other attributes we
+   can iterate simply... */
+#define NEXT_CONTRACT_ATTR(NODE) \
+  TREE_CHAIN (NODE)
+
+/* Contract assertion kind */
+/* Must match relevant enums in <contracts> header  */
+
+enum contract_assertion_kind : uint16_t {
+  CAK_INVALID = 0 ,
+  CAK_PRE = 1 ,
+  CAK_POST = 2 ,
+  CAK_ASSERT = 3,
+};
+
+/* Per P2900R14 + D3290R3 + extensions.  */
+enum contract_evaluation_semantic : uint16_t {
+  CES_INVALID = 0,
+  CES_IGNORE = 1,
+  CES_OBSERVE = 2,
+  CES_ENFORCE = 3,
+  CES_QUICK = 4,
+};
+
+enum detection_mode : uint16_t {
+  CDM_UNSPECIFIED = 0,
+  CDM_PREDICATE_FALSE = 1,
+  CDM_EVAL_EXCEPTION = 2
+};
+
+/* Contract evaluation_semantic */
+#define CONTRACT_EVALUATION_SEMANTIC(NODE) \
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 0))
+
+#define CONTRACT_ASSERTION_KIND(NODE) \
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 1))
+
+#define CONTRACT_CHECK(NODE) \
+  (TREE_CHECK3 (NODE, ASSERTION_STMT, PRECONDITION_STMT, POSTCONDITION_STMT))
+
+/* True if NODE is any kind of contract.  */
+#define CONTRACT_P(NODE)			\
+  (TREE_CODE (NODE) == ASSERTION_STMT		\
+   || TREE_CODE (NODE) == PRECONDITION_STMT	\
+   || TREE_CODE (NODE) == POSTCONDITION_STMT)
+
+/* True if NODE is a contract condition.  */
+#define CONTRACT_CONDITION_P(NODE)		\
+  (TREE_CODE (NODE) == PRECONDITION_STMT	\
+   || TREE_CODE (NODE) == POSTCONDITION_STMT)
+
+/* True if NODE is a precondition.  */
+#define PRECONDITION_P(NODE)           \
+  (TREE_CODE (NODE) == PRECONDITION_STMT)
+
+/* True if NODE is a postcondition.  */
+#define POSTCONDITION_P(NODE)          \
+  (TREE_CODE (NODE) == POSTCONDITION_STMT)
+
+/* True iff the FUNCTION_DECL NODE currently has any contracts.  */
+#define DECL_HAS_CONTRACTS_P(NODE) \
+  (get_fn_contract_specifiers (NODE) != NULL_TREE)
+
+/* The wrapper of the original source location of a list of contracts.  */
+#define CONTRACT_SOURCE_LOCATION_WRAPPER(NODE) \
+  (TREE_PURPOSE (TREE_VALUE (NODE)))
+
+/* The original source location of a list of contracts.  */
+#define CONTRACT_SOURCE_LOCATION(NODE) \
+  (EXPR_LOCATION (CONTRACT_SOURCE_LOCATION_WRAPPER (NODE)))
+
+/* The actual code _STMT for a contract attribute.  */
+#define CONTRACT_STATEMENT(NODE) \
+  (TREE_VALUE (TREE_VALUE (NODE)))
+
+/* The parsed condition of the contract.  */
+#define CONTRACT_CONDITION(NODE) \
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 2))
+
+/* True iff the condition of the contract NODE is not yet parsed.  */
+#define CONTRACT_CONDITION_DEFERRED_P(NODE) \
+  (TREE_CODE (CONTRACT_CONDITION (NODE)) == DEFERRED_PARSE)
+
+/* The raw comment of the contract.  */
+#define CONTRACT_COMMENT(NODE) \
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 3))
+
+/* A std::source_location, if provided.  */
+#define CONTRACT_STD_SOURCE_LOC(NODE) \
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 4))
+
+/* The VAR_DECL of a postcondition result. For deferred contracts, this
+   is an IDENTIFIER.  */
+#define POSTCONDITION_IDENTIFIER(NODE) \
+  (TREE_OPERAND (POSTCONDITION_STMT_CHECK (NODE), 5))
+
+/* contracts.cc */
+
+extern tree grok_contract			(tree, tree, tree, cp_expr, location_t);
+extern tree finish_contract_attribute		(tree, tree);
+extern tree finish_contract_condition		(cp_expr);
+extern void update_late_contract		(tree, tree, cp_expr);
+extern void check_redecl_contract		(tree, tree);
+extern tree invalidate_contract			(tree);
+extern tree copy_and_remap_contracts		(tree, tree);
+
+extern void set_fn_contract_specifiers		(tree, tree);
+extern void update_fn_contract_specifiers	(tree, tree);
+extern tree get_fn_contract_specifiers		(tree);
+extern void remove_decl_with_fn_contracts_specifiers (tree);
+extern void remove_fn_contract_specifiers	(tree);
+extern void update_contract_arguments		(tree, tree);
+
+extern tree make_postcondition_variable		(cp_expr);
+extern tree make_postcondition_variable		(cp_expr, tree);
+extern void check_param_in_postcondition	(tree, location_t);
+extern void check_postconditions_in_redecl	(tree, tree);
+extern void maybe_update_postconditions		(tree);
+extern void rebuild_postconditions		(tree);
+extern bool check_postcondition_result		(tree, tree, location_t);
+
+extern bool contract_any_deferred_p 		(tree);
+
+extern void start_function_contracts		(tree);
+extern void maybe_apply_function_contracts	(tree);
+
+extern void maybe_emit_violation_handler_wrappers (void);
+
+extern tree init_builtin_contract_violation_type (void);
+extern tree build_contract_check		(tree);
+
+inline void
+set_decl_contracts (tree decl, tree contract_attrs)
+{
+  set_fn_contract_specifiers (decl, contract_attrs);
+}
+
+/* TODO : decide if we should push the tests into contracts.cc  */
+extern contract_evaluation_semantic get_evaluation_semantic (const_tree);
+
+/* Will this contract be ignored.  */
+
+inline bool
+contract_ignored_p (const_tree contract)
+{
+  return (get_evaluation_semantic (contract) <= CES_IGNORE);
+}
+
+/* Will this contract be evaluated?  */
+
+inline bool
+contract_evaluated_p (const_tree contract)
+{
+  return (get_evaluation_semantic (contract) >= CES_OBSERVE);
+}
 #endif /* ! GCC_CP_CONTRACT_H */

gcc-mirror/gcc

	* c.opt.urls: Regenerate.
	* doc/invoke.texi: Document -fcontracts and
	-fc

gcc/cp/contracts.h
@@ -1,7 +1,12 @@
-/* Definitions for C++ contract levels.  Implements functionality described in
-   the N4820 working draft version of contracts, P1290, P1332, and P1429.
+/* Definitions for C++26 contracts.
+
    Copyright (C) 2020-2026 Free Software Foundation, Inc.
-   Contributed by Jeff Chapman II (jchapman@lock3software.com)
+   Originally by Jeff Chapman II (jchapman@lock3software.com) for proposed
+   C++20 contracts.
+   Rewritten for C++26 contracts by:
+     Nina Ranns (dinka.ranns@googlemail.com)
+     Iain Sandoe (iain@sandoe.co.uk)
+     Ville Voutilainen (ville.voutilainen@gmail.com).
 
 This file is part of GCC.
 
@@ -22,5 +27,167 @@ along with GCC; see the file COPYING3.  If not see
 #ifndef GCC_CP_CONTRACT_H
 #define GCC_CP_CONTRACT_H
 
+#include <cstdint>
+
+/* Contract assertion kind */
+/* Must match relevant enums in <contracts> header  */
+
+enum contract_assertion_kind : uint16_t {
+  CAK_INVALID = 0 ,
+  CAK_PRE = 1 ,
+  CAK_POST = 2 ,
+  CAK_ASSERT = 3,
+};
+
+/* Per P2900R14 + D3290R3 + extensions.  */
+enum contract_evaluation_semantic : uint16_t {
+  CES_INVALID = 0,
+  CES_IGNORE = 1,
+  CES_OBSERVE = 2,
+  CES_ENFORCE = 3,
+  CES_QUICK = 4,
+};
+
+enum detection_mode : uint16_t {
+  CDM_UNSPECIFIED = 0,
+  CDM_PREDICATE_FALSE = 1,
+  CDM_EVAL_EXCEPTION = 2
+};
+
+/* Contract evaluation_semantic */
+#define CONTRACT_EVALUATION_SEMANTIC(NODE) \
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 0))
+
+#define CONTRACT_ASSERTION_KIND(NODE) \
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 1))
+
+#define CONTRACT_CHECK(NODE) \
+  (TREE_CHECK3 (NODE, ASSERTION_STMT, PRECONDITION_STMT, POSTCONDITION_STMT))
+
+/* True if NODE is any kind of contract.  */
+#define CONTRACT_P(NODE)			\
+  (TREE_CODE (NODE) == ASSERTION_STMT		\
+   || TREE_CODE (NODE) == PRECONDITION_STMT	\
+   || TREE_CODE (NODE) == POSTCONDITION_STMT)
+
+/* True if NODE is a contract condition.  */
+#define CONTRACT_CONDITION_P(NODE)		\
+  (TREE_CODE (NODE) == PRECONDITION_STMT	\
+   || TREE_CODE (NODE) == POSTCONDITION_STMT)
+
+/* True if NODE is a precondition.  */
+#define PRECONDITION_P(NODE)           \
+  (TREE_CODE (NODE) == PRECONDITION_STMT)
+
+/* True if NODE is a postcondition.  */
+#define POSTCONDITION_P(NODE)          \
+  (TREE_CODE (NODE) == POSTCONDITION_STMT)
+
+/* True iff the FUNCTION_DECL NODE currently has any contracts.  */
+#define DECL_HAS_CONTRACTS_P(NODE) \
+  (get_fn_contract_specifiers (NODE) != NULL_TREE)
+
+/* The wrapper of the original source location of a list of contracts.  */
+#define CONTRACT_SOURCE_LOCATION_WRAPPER(NODE) \
+  (TREE_PURPOSE (TREE_VALUE (NODE)))
+
+/* The original source location of a list of contracts.  */
+#define CONTRACT_SOURCE_LOCATION(NODE) \
+  (EXPR_LOCATION (CONTRACT_SOURCE_LOCATION_WRAPPER (NODE)))
+
+/* The actual code _STMT for a contract specifier.  */
+#define CONTRACT_STATEMENT(NODE) \
+  (TREE_VALUE (TREE_VALUE (NODE)))
+
+/* The parsed condition of the contract.  */
+#define CONTRACT_CONDITION(NODE) \
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 2))
+
+/* True iff the condition of the contract NODE is not yet parsed.  */
+#define CONTRACT_CONDITION_DEFERRED_P(NODE) \
+  (TREE_CODE (CONTRACT_CONDITION (NODE)) == DEFERRED_PARSE)
+
+/* The raw comment of the contract.  */
+#define CONTRACT_COMMENT(NODE) \
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 3))
+
+/* A std::source_location, if provided.  */
+#define CONTRACT_STD_SOURCE_LOC(NODE) \
+  (TREE_OPERAND (CONTRACT_CHECK (NODE), 4))
+
+/* The VAR_DECL of a postcondition result. For deferred contracts, this
+   is an IDENTIFIER.  */
+#define POSTCONDITION_IDENTIFIER(NODE) \
+  (TREE_OPERAND (POSTCONDITION_STMT_CHECK (NODE), 5))
+
+/* contracts.cc */
+
+extern void init_contracts			(void);
+
+extern tree grok_contract			(tree, tree, tree, cp_expr, location_t);
+extern tree finish_contract_specifier 		(tree, tree);
+extern tree finish_contract_condition		(cp_expr);
+extern void update_late_contract		(tree, tree, cp_expr);
+extern void check_redecl_contract		(tree, tree);
+extern tree invalidate_contract			(tree);
+extern tree copy_and_remap_contracts		(tree, tree);
+
+extern void set_fn_contract_specifiers		(tree, tree);
+extern void update_fn_contract_specifiers	(tree, tree);
+extern tree get_fn_contract_specifiers		(tree);
+extern void remove_decl_with_fn_contracts_specifiers (tree);
+extern void remove_fn_contract_specifiers	(tree);
+extern void update_contract_arguments		(tree, tree);
+
+extern tree make_postcondition_variable		(cp_expr);
+extern tree make_postcondition_variable		(cp_expr, tree);
+extern void check_param_in_postcondition	(tree, location_t);
+extern void check_postconditions_in_redecl	(tree, tree);
+extern void maybe_update_postconditions		(tree);
+extern void rebuild_postconditions		(tree);
+extern bool check_postcondition_result		(tree, tree, location_t);
+
+extern bool contract_any_deferred_p 		(tree);
+
+extern void start_function_contracts		(tree);
+extern void maybe_apply_function_contracts	(tree);
+
+extern void maybe_emit_violation_handler_wrappers (void);
+
+extern tree build_contract_check		(tree);
+
+inline void
+set_decl_contracts (tree decl, tree contract_attrs)
+{
+  set_fn_contract_specifiers (decl, contract_attrs);
+}
+
+/* TODO : decide if we should push the tests into contracts.cc  */
+extern contract_evaluation_semantic get_evaluation_semantic (const_tree);
+
+/* Will this contract be ignored.  */
+
+inline bool
+contract_ignored_p (const_tree contract)
+{
+  return (get_evaluation_semantic (contract) <= CES_IGNORE);
+}
+
+/* Will this contract be evaluated?  */
+
+inline bool
+contract_evaluated_p (const_tree contract)
+{
+  return (get_evaluation_semantic (contract) >= CES_OBSERVE);
+}
+
+/* Is the contract terminating?  */
+
+inline bool
+contract_terminating_p (const_tree contract)
+{
+  return (get_evaluation_semantic (contract) == CES_ENFORCE
+	  || get_evaluation_semantic (contract) == CES_QUICK);
+}
 
 #endif /* ! GCC_CP_CONTRACT_H */