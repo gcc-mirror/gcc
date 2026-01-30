a28bb06b3e20a26579e06dc1b5bd6344ce4f88f0
Eric Botcazou <ebotcazou@adacore.com>
2026-01-30 11:58:58 +0100

    Ada: Fix spurious visibility error from limited_with clause in hierarchy
    The problem is that the compiler installs the limited view of a package that
    is already installed by the virtue of being an ancestor of the main unit.
    
    gcc/ada/
    	PR ada/123867
    	* sem_ch10.adb (Analyze_Compilation_Unit): Output info message
    	when -gnatdi is specified.
    	(Install_Parents): Likewise.  Set the Is_Visible_Lib_Unit flag
    	on the unit.
    	(Install_Private_With_Clauses): Do not output info message here.
    	(Remove_Parents): Output info message when -gnatdi is specified
    	and clear the Is_Visible_Lib_Unit flag on the unit.
    
    gcc/testsuite/
    	* gnat.dg/specs/limited_with3.ads: New test.
    	* gnat.dg/specs/limited_with3-child.ads: New helper.
    	* gnat.dg/specs/limited_with3-child-grandchild.ads: Likewise.
    	* gnat.dg/specs/limited_with3-child-grandchild-grandgrandchild.ads:
    	Likewise.

---
+++ gcc/testsuite/gnat.dg/specs/limited_with3-child-grandchild.ads
@@ -0,0 +1,5 @@
+package Limited_With3.Child.Grandchild is
+
+  function F return T is (Two);
+
+end Limited_With3.Child.Grandchild;
---
+++ gcc/testsuite/gnat.dg/specs/limited_with3-child.ads
@@ -0,0 +1,7 @@
+package Limited_With3.Child is
+
+  type T is (One, Two, Three);
+
+  function F return T is (One);
+
+end Limited_With3.Child;
