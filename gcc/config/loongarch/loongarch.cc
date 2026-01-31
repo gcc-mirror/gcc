c88be070dcec3b8c6ea996f79466f24fe65c798f
Xi Ruoyao <xry111@xry111.site>
2026-02-01 00:04:23 +0800

    Revert "LoongArch: Fix PR122097 (2)."
    This reverts commit e753fa38bb612a3bebe0fc596a44a104bbd2739b.
    
    The commit is no longer needed with the new copysign/xorsign
    implementaton.

--- gcc/config/loongarch/loongarch.cc
+++ gcc/config/loongarch/loongarch.cc
@@ -1851,27 +1851,7 @@ loongarch_const_vector_same_bytes_p (rtx op, machine_mode mode)
 
   first = CONST_VECTOR_ELT (op, 0);
   bytes = GET_MODE_UNIT_SIZE (mode);
-
-  if (GET_MODE_CLASS (mode) == MODE_VECTOR_FLOAT)
-    {
-      rtx val_s = CONST_VECTOR_ELT (op, 0);
-      const REAL_VALUE_TYPE *x = CONST_DOUBLE_REAL_VALUE (val_s);
-      if (GET_MODE (val_s) == DFmode)
-	{
-	  long tmp[2];
-	  REAL_VALUE_TO_TARGET_DOUBLE (*x, tmp);
-	  val = (unsigned HOST_WIDE_INT) tmp[1] << 32 | tmp[0];
-	}
-      else
-	{
-	  long tmp;
-	  REAL_VALUE_TO_TARGET_SINGLE (*x, tmp);
-	  val = (unsigned HOST_WIDE_INT) tmp;
-	}
-    }
-  else
-    val = UINTVAL (first);
-
+  val = INTVAL (first);
   first_byte = val & 0xff;
   for (i = 1; i < bytes; i++)
     {
