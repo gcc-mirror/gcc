bf2450527e5909ab2fbd008e6933415310ab8aa6
Alexandre Oliva <oliva@adacore.com>
2026-02-01 04:35:41 -0300

    testsuite: riscv: pr118170 goes 64-bit without choosing a 64-bit abi
    The test selects a 64-bit cpu explicitly, which enables a 64-bit arch,
    but the test doesn't set the abi, so it fails on riscv32-elf.  Set a
    64-bit abi explicitly.
    
    
    for  gcc/testsuite/ChangeLog
    
    	* gcc.target/riscv/pr118170.c: Set the abi explicitly.

--- gcc/testsuite/gcc.target/riscv/pr118170.c
+++ gcc/testsuite/gcc.target/riscv/pr118170.c
@@ -1,5 +1,5 @@
 /* { dg-do "compile" { target { ! riscv_abi_e } } } */
-/* { dg-options "-O2 -mcpu=tt-ascalon-d8" } */
+/* { dg-options "-O2 -mcpu=tt-ascalon-d8 -mabi=lp64d" } */
 _Float16 f;
 
 void
