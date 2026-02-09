7d429e9faa41258d5a4196ef11ddcedb3e35f555
Jonathan Wakely <jwakely@redhat.com>
2026-02-09 10:48:44 +0000

    libstdc++: Fix incorrect noexcept on string::compare overloads [PR123991]
    These compare overloads throw when the pos index is out of range, not
    only when the const T& parameter throws on conversion to string_view.
    
    Remove the incorrect conditional noexcept-specifier from the two
    overloads that can throw.
    
    libstdc++-v3/ChangeLog:
    
    	PR libstdc++/123991
    	* include/bits/basic_string.h (compare(size_type, size_type, T)):
    	Remove noexcept-specifier.
    	(compare(size_type, size_type, T, size_type, size_type)):
    	Likewise.
    	* include/bits/cow_string.h (compare(size_type, size_type, T)):
    	Remove noexcept-specifier.
    	(compare(size_type, size_type, T, size_type, size_type)):
    	Likewise.
    	* testsuite/21_strings/basic_string/operations/compare/char/123991.cc:
    	New test.
    	* testsuite/21_strings/basic_string/operations/compare/wchar_t/123991.cc:
    	New test.
    
    Reviewed-by: Nathan Myers <nmyers@redhat.com>
    (cherry picked from commit 0912dfcd1e901d7dc1ff5e10528eefe3f3ff7b22)

---
+++ libstdc++-v3/testsuite/21_strings/basic_string/operations/compare/wchar_t/123991.cc
@@ -0,0 +1,56 @@
+// { dg-do run { target c++17 } }
+
+// Bug 123991 - std::string::compare crashes instead of throwing
+
+#include <string>
+#include <string_view>
+#include <stdexcept>
+#include <testsuite_hooks.h>
+
+void
+test_compare_3arg()
+{
+  std::wstring_view sv;
+  std::wstring s;
+  static_assert( ! noexcept(s.compare(0, 0, sv)) );
+#ifdef __cpp_exceptions
+  try
+  {
+    (void) s.compare(1, 0, sv);
+    VERIFY(false);
+  }
+  catch (const std::out_of_range&)
+  { }
+#endif
+}
+
+void
+test_compare_5arg()
+{
+  std::wstring_view sv;
+  std::wstring s;
+  static_assert( ! noexcept(s.compare(0, 0, sv, 0, 0)) );
+#ifdef __cpp_exceptions
+  try
+  {
+    (void) s.compare(1, 0, sv, 0, 0);
+    VERIFY(false);
+  }
+  catch (const std::out_of_range&)
+  { }
+
+  try
+  {
+    (void) s.compare(0, 0, sv, 1, 0);
+    VERIFY(false);
+  }
+  catch (const std::out_of_range&)
+  { }
+#endif
+}
+
+int main()
+{
+  test_compare_3arg();
+  test_compare_5arg();
+}
