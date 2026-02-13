186fd25041417c86d74095c20a1de5ad5b29127f
Tobias Burnus <tburnus@baylibre.com>
2026-02-13 12:29:03 +0100

    Fix gcc/testsuite/*/gomp/pr113436* [PR113436]
    Testcases in gcc/testsuite/ may not '#include <omp.h>' or 'use omp_lib';
    instead, now a local declaration of the enum/Fortran-parameter values is
    used.
    
    gcc/testsuite/ChangeLog:
    
    	PR middle-end/113436
    	* c-c++-common/gomp/pr113436-1.c: Replace '#include <omp.h>' by
    	local declaration for omp_allocator_handle_t.
    	* c-c++-common/gomp/pr113436-2.c: Likewise.
    	* g++.dg/gomp/pr113436.C: Likewise.
    	* gfortran.dg/gomp/pr113436-1.f90: Replace 'use omp_lib' by local
    	omp_*_mem_alloc parameter declarations.
    	* gfortran.dg/gomp/pr113436-2.f90: Likewise.
    	* gfortran.dg/gomp/pr113436-3.f90: Likewise.
    	* gfortran.dg/gomp/pr113436-4.f90: Likewise.
    
    (cherry picked from commit eb311dc3db842d5e2bd4215184a01099e0f26d6c)

--- gcc/testsuite/gfortran.dg/gomp/pr113436-3.f90
+++ gcc/testsuite/gfortran.dg/gomp/pr113436-3.f90
@@ -2,8 +2,32 @@
 ! { dg-do compile }
 ! { dg-options "-fopenmp -fno-automatic -fdump-tree-omplower" }
 
+module m
+use iso_c_binding
+integer, parameter :: omp_allocator_handle_kind = c_intptr_t
+        integer (kind=omp_allocator_handle_kind), &
+                 parameter :: omp_null_allocator = 0
+        integer (kind=omp_allocator_handle_kind), &
+                 parameter :: omp_default_mem_alloc = 1
+        integer (kind=omp_allocator_handle_kind), &
+                 parameter :: omp_large_cap_mem_alloc = 2
+        integer (kind=omp_allocator_handle_kind), &
+                 parameter :: omp_const_mem_alloc = 3
+        integer (kind=omp_allocator_handle_kind), &
+                 parameter :: omp_high_bw_mem_alloc = 4
+        integer (kind=omp_allocator_handle_kind), &
+                 parameter :: omp_low_lat_mem_alloc = 5
+        integer (kind=omp_allocator_handle_kind), &
+                 parameter :: omp_cgroup_mem_alloc = 6
+        integer (kind=omp_allocator_handle_kind), &
+                 parameter :: omp_pteam_mem_alloc = 7
+        integer (kind=omp_allocator_handle_kind), &
+                 parameter :: omp_thread_mem_alloc = 8
+end
+
 program g
-  use omp_lib
+!  use omp_lib
+  use m
   implicit none
 
   integer :: A(10)
