b02f9495dcf635e3a7a6a5215eb0cccbb949b8e9
Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>
2026-03-02 11:12:21 +0100

    testsuite: arm: adjust inline assembler for arm-none-eabi [PR124320]
    gcc/testsuite/ChangeLog:
    
    	PR testsuite/124320
    	* gcc.dg/lto/toplevel-extended-asm-1_0.c: Adjust inline
    	assembler for arm-none-eabi.
    
    Signed-off-by: Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>

--- gcc/testsuite/gcc.dg/lto/toplevel-extended-asm-1_0.c
+++ gcc/testsuite/gcc.dg/lto/toplevel-extended-asm-1_0.c
@@ -4,8 +4,22 @@
 void asm_fn();
 void asm_fn_used();
 
-asm(".global %cc0\n%cc0:" :: ":" (asm_fn));
-asm(".global %cc0\n%cc0:" :: ":" (asm_fn_used));
+#ifdef __ELF__
+#ifdef __sparc__
+#define TYPE_PFX "#"
+#elif defined (__arm__) || defined (__aarch64__)
+#define TYPE_PFX "%%"
+#else
+#define TYPE_PFX "@"
+#endif
+
+#define ASM_FUNCTION ".global %cc0\n\t.type %cc0, " TYPE_PFX "function\n%cc0:"
+#else
+#define ASM_FUNCTION ".global %cc0\n%cc0:"
+#endif
+
+asm(ASM_FUNCTION :: ":" (asm_fn));
+asm(ASM_FUNCTION :: ":" (asm_fn_used));
 
 
 __attribute__((noinline))
