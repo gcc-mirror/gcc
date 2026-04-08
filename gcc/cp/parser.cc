5140c1683f1655a4163464688007b38a57c62633
Yang Kun <yangkun@disroot.org>
2026-04-08 15:36:27 +0200

    c++, openmp: Fix wrong error messages
    Error messages wrongly said "invalid depend kind"
    
    gcc/cp/ChangeLog:
    	* parser.cc (cp_parser_omp_clause_proc_bind): Fix error message
    	(cp_parser_omp_clause_device_type): Likewise.
    
    Signed-off-by: Yang Kun <yangkun@disroot.org>
    (cherry picked from commit 691ad6ef9e14e2234ff296fd69bf7d324e71d135)

--- gcc/cp/parser.cc
+++ gcc/cp/parser.cc
@@ -40782,7 +40782,7 @@ cp_parser_omp_clause_proc_bind (cp_parser *parser, tree list,
   return c;
 
  invalid_kind:
-  cp_parser_error (parser, "invalid depend kind");
+  cp_parser_error (parser, "invalid proc_bind kind");
  resync_fail:
   cp_parser_skip_to_closing_parenthesis (parser, /*recovering=*/true,
 					 /*or_comma=*/false,
@@ -40832,7 +40832,7 @@ cp_parser_omp_clause_device_type (cp_parser *parser, tree list,
   return c;
 
  invalid_kind:
-  cp_parser_error (parser, "invalid depend kind");
+  cp_parser_error (parser, "expected %<host%>, %<nohost%> or %<any%>");
  resync_fail:
   cp_parser_skip_to_closing_parenthesis (parser, /*recovering=*/true,
 					 /*or_comma=*/false,
