1c8ca0108746d3a7452134f9d1c2e5d89857fed7
Eikansh Gupta <eikansh.gupta@oss.qualcomm.com>
2026-06-17 09:58:16 +0530

    MATCH: fold signbit comparison and conditional negate to copysign [PR109843]
    Fold (signbit (x) cmp1 0) cmp (signbit (y) cmp2 0) ? y : -y
    to copysign (y, +-x).  The result keeps the magnitude of Y and takes its
    sign from X (or -X).  Emitted as IFN_COPYSIGN when the target supports it.
    
    	PR tree-optimization/109843
    
    gcc/ChangeLog:
    
    	* match.pd ((signbit (x) cmp 0) cmp (signbit (y) cmp 0) ? y : -y):
    	New simplification to copysign (y, +-x).
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/tree-ssa/pr109843.c: New test.
    
    Signed-off-by: Eikansh Gupta <eikansh.gupta@oss.qualcomm.com>

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr109843.c
@@ -0,0 +1,76 @@
+/* PR tree-optimization/109843 */
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+#include <stdbool.h>
+
+/* Transforms to copysign (y, x)  */
+
+float copysign1 (float x, float y)
+{
+  bool t = __builtin_signbit (x) == 0;
+  bool t1 = __builtin_signbit (y) == 0;
+  return (t == t1) ? y : -y;
+}
+
+float copysign2 (float x, float y)
+{
+  bool t = __builtin_signbit (x) != 0;
+  bool t1 = __builtin_signbit (y) != 0;
+  return (t == t1) ? y : -y;
+}
+
+float copysign3 (float x, float y)
+{
+  bool t = __builtin_signbit (x) != 0;
+  bool t1 = __builtin_signbit (y) == 0;
+  return (t != t1) ? y : -y;
+}
+
+float copysign4 (float x, float y)
+{
+  bool t = __builtin_signbit (x) == 0;
+  bool t1 = __builtin_signbit (y) != 0;
+  return (t != t1) ? y : -y;
+}
+
+float copysign5 (float x, float y)
+{
+  bool t = __builtin_signbit (y) == 0;
+  bool t1 = __builtin_signbit (x) == 0;
+  return (t == t1) ? y : -y;
+}
+
+/* Transforms to copysign (y, -x)  */
+
+float copysign6 (float x, float y)
+{
+  bool t = __builtin_signbit (x) == 0;
+  bool t1 = __builtin_signbit (y) == 0;
+  return (t != t1) ? y : -y;
+}
+
+float copysign7 (float x, float y)
+{
+  bool t = __builtin_signbit (x) != 0;
+  bool t1 = __builtin_signbit (y) == 0;
+  return (t == t1) ? y : -y;
+}
+
+float copysign8 (float x, float y)
+{
+  bool t = __builtin_signbit (x) == 0;
+  bool t1 = __builtin_signbit (y) != 0;
+  return (t == t1) ? y : -y;
+}
+
+float copysign9 (float x, float y)
+{
+  bool t = __builtin_signbit (x) != 0;
+  bool t1 = __builtin_signbit (y) != 0;
+  return (t != t1) ? y : -y;
+}
+
+/* { dg-final { scan-tree-dump-not "signbit" "optimized" { target ifn_copysign } } } */
+/* { dg-final { scan-tree-dump-times "= \\.COPYSIGN" 9 "optimized" { target ifn_copysign } } } */
+/* { dg-final { scan-tree-dump-times " = -" 4 "optimized" { target ifn_copysign } } } */
