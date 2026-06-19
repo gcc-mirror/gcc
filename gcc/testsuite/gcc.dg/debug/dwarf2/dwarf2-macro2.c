6ba6bc80ac8d62f7fef74d9a8af7b84387450aa7
Iain Sandoe <iain@sandoe.co.uk>
2026-06-19 19:40:37 +0100

    testsuite, Darwin: Extend skipping of -g3 for more OS versions.
    The dyld-based linker does not work with GCC's output for -g3 and that
    is now in force from darwin23 onwards.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/debug/dwarf2/dwarf2-macro.c: Skip for Darwin23+.
    	* gcc.dg/debug/dwarf2/dwarf2-macro2.c: Likewise.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>
    (cherry picked from commit da0aa94c68ad225f3f92cece2f3b8a2b7f1aee1f)

--- gcc/testsuite/gcc.dg/debug/dwarf2/dwarf2-macro2.c
+++ gcc/testsuite/gcc.dg/debug/dwarf2/dwarf2-macro2.c
@@ -1,7 +1,7 @@
 /* Test to make sure the macro info includes the predefined macros with line number 0.  */
 /* { dg-do compile } */
 /* { dg-options "-g3 -gdwarf -dA -fverbose-asm" } */
-/* { dg-final { scan-assembler "At line number 0" { xfail *-*-darwin23* } } } */
+/* { dg-final { scan-assembler "At line number 0" { xfail *-*-darwin2[3-5]* } } } */
 
 #define FOO 1
 int i;
