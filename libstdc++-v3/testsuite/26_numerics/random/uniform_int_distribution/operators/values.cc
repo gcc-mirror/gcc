430812242779ce8fefac725ce51fcf004ebe382d
Tomasz Kamiński <tkaminsk@redhat.com>
2026-07-27 16:39:46 +0200

    libstdc++: Handle generators with non-zero min in uniform_int_distribution [PR118665]
    The commit r11-3757-g98c37d3bacbb2f that introduced Lemire's algorithm in
    uniform_int_distribution, assumed that any engine that generates range of
    size 2^32 or 2^64, returns a range starting from zero. While this is true
    for all standard providede engines, it may not be true for user provided
    range. In such case the produced output is no longer uniform:
    testDiscreteDist was failing for such engine.
    
    This patch simply substract __min (_Urbg::min()) from the generated number
    (__g()). As this value is compile time constant, this has no performance
    impact for generators producing ranges starting from zero.
    
    	PR libstdc++/118665
    
    libstdc++-v3/ChangeLog:
    
    	* include/bits/uniform_int_dist.h (uniform_int_distribution::_S_nd):
    	Substract __min (_Urbg::min()) from each generator invocation.
    	* testsuite/26_numerics/random/uniform_int_distribution/operators/values.cc:
    	Add testDiscreteDist for shifted (with non-zero min()) engine.
    
    Reviewed-by: Jonathan Wakely <jwakely@redhat.com>
    Signed-off-by: Tomasz Kamiński <tkaminsk@redhat.com>
    (cherry picked from commit 9d93ec2ac1b13ab09ca80bf0c684cb035e36a431)

--- libstdc++-v3/testsuite/26_numerics/random/uniform_int_distribution/operators/values.cc
+++ libstdc++-v3/testsuite/26_numerics/random/uniform_int_distribution/operators/values.cc
@@ -21,6 +21,7 @@
 // 26.5.8.2.1 Class template uniform_int_distribution [rand.dist.uni.int]
 
 #include <random>
+#include <cstdint>
 #include <functional>
 #include <testsuite_random.h>
 
@@ -31,6 +32,19 @@
 #else
 # define ARGS
 #endif
+
+template<std::uint64_t Offset>
+struct shifted
+{
+  using result_type = std::uint64_t;
+  static constexpr result_type min() { return Offset; }
+  static constexpr result_type max() { return Offset + std::mt19937::max(); }
+
+  result_type operator()()
+  { return Offset + eng(); }
+
+  std::mt19937 eng;
+};
 
 void test01()
 {
@@ -49,6 +63,14 @@ void test01()
   std::uniform_int_distribution<> uid3(1, 20);
   auto buid3 = std::bind(uid3, eng);
   testDiscreteDist<ARGS>(buid3, [](int n) { return uniform_int_pdf(n, 1, 20); } );
+
+  shifted<(std::uint64_t(1) << 16)> s16e;
+  auto buid4 = std::bind(uid3, s16e);
+  testDiscreteDist<ARGS>(buid4, [](int n) { return uniform_int_pdf(n, 1, 20); } );
+
+  shifted<(std::uint64_t(1) << 32)> s32e;
+  auto buid5 = std::bind(uid3, s32e);
+  testDiscreteDist<ARGS>(buid5, [](int n) { return uniform_int_pdf(n, 1, 20); } );
 }
 
 int main()
