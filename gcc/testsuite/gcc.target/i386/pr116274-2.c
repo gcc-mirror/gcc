30024f2093e4a234fa0e1b159d6a24514aa364cb
H.J. Lu <hjl.tools@gmail.com>
2026-07-29 06:51:24 +0800

    gcc.target/i386/pr116274-2.c: Check function body
    gcc.target/i386/pr116274-2.c has been optimized to:
    
    test:
    	.cfi_startproc
    	leaq	(%rdi,%rsi), %rax
    	ret
    
    for x86-64 since GCC 15.  Add check-function-bodies to verify it.
    
    	PR target/116274
    	* gcc.target/i386/pr116274-2.c: Add check-function-bodies.
    
    Signed-off-by: H.J. Lu <hjl.tools@gmail.com>

--- gcc/testsuite/gcc.target/i386/pr116274-2.c
+++ gcc/testsuite/gcc.target/i386/pr116274-2.c
@@ -1,5 +1,17 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -fdump-tree-slp2-optimized" } */
+/* Keep labels and directives ('.cfi_startproc', '.cfi_endproc').  */
+/* { dg-final { check-function-bodies "**" "" "" { target  { *-*-linux* && lp64 } } {^\t?\.}  } } */
+
+/*
+**test:
+**.LFB[0-9]+:
+**	.cfi_startproc
+**	leaq	\(%rdi,%rsi\), %rax
+**	ret
+**	.cfi_endproc
+**...
+*/
 
 struct a { long x,y; };
 long test(struct a a) { return a.x+a.y; }
