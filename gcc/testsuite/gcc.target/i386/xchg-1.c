baf658d80576de061320f136945fff90359ff7fd
Uros Bizjak <ubizjak@gmail.com>
2026-07-28 08:41:57 +0200

    i386: Avoid XCHGB %rH, %rL in bswaphi2 on non-pentium4 targets [PR126433]
    XCHGB has higher latency than ROLW for non-pentium4 targets, so it
    should be avoided. Switch from the preferred_for_size/preferred_for_speed
    attributes to the "enabled" attribute to hard-disable unwanted alternatives.
    
    	PR target/126433
    
    gcc/ChangeLog:
    
    	* config/i386/i386.md (*bswaphi2_movbe) Use enabled attribute.
    	Remove preferred_for_speed and preferred_for_size attributes.
    	(*bswaphi2): Ditto.
    	(*bswaphisi2_lowpart): Ditto.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/i386/xchg-1.c: Scan for ROL instead of XCHG.
    	* gcc.target/i386/xchg-2.c: Use -O2 -march=pentium4 to emit XCHG.

--- gcc/testsuite/gcc.target/i386/xchg-1.c
+++ gcc/testsuite/gcc.target/i386/xchg-1.c
@@ -6,4 +6,4 @@ unsigned short good(unsigned short a)
        return (a >> 8 | a << 8);
 }
 
-/* { dg-final { scan-assembler "\[ \t\]xchg" } } */
+/* { dg-final { scan-assembler "\[ \t\]rol" } } */
--- gcc/testsuite/gcc.target/i386/xchg-2.c
+++ gcc/testsuite/gcc.target/i386/xchg-2.c
@@ -1,9 +1,9 @@
-/* { dg-do compile } */
-/* { dg-options "-Os" } */
+/* { dg-do compile { target ia32 } } */
+/* { dg-options "-O2 -march=pentium4" } */
 
 unsigned short good(unsigned short a)
 {
        return (a >> 8 | a << 8);
 }
 
-/* { dg-final { scan-assembler "xchgb" } } */
+/* { dg-final { scan-assembler "\[ \t\]xchg" } } */
