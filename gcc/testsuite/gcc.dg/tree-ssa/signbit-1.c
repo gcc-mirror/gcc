1c37c631abe1de02e9b843a264b9923c12e9a68d
Kyrylo Tkachov <ktkachov@nvidia.com>
2026-08-08 12:36:55 +0200

    testsuite: Remove unrelated checks from signbit-1.c
    signbit-1.c tests the X + (X | -X) simplification.  The test also
    contains three expressions for the separate (X | -X) sign-bit fold.
    That fold belongs to a separate patch, so the dump scans fail when this
    patch is tested by itself.
    
    Remove the unrelated functions and scan.  The sign-bit fold patch has
    its own tests.
    
    Tested on x86_64-pc-linux-gnu.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/tree-ssa/signbit-1.c (f1, f2, f3): Remove.
    	Remove the corresponding dump scan.
    
    Signed-off-by: Kyrylo Tkachov <ktkachov@nvidia.com>

--- gcc/testsuite/gcc.dg/tree-ssa/signbit-1.c
+++ gcc/testsuite/gcc.dg/tree-ssa/signbit-1.c
@@ -1,14 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -fdump-tree-optimized" } */
 
-/* X | -X has the sign bit set exactly when X is non-zero.  */
-
-int f1 (int x)
-{ return (x | -x) >> (__SIZEOF_INT__ * __CHAR_BIT__ - 1); }
-unsigned int f2 (unsigned int x)
-{ return (x | -x) >> (__SIZEOF_INT__ * __CHAR_BIT__ - 1); }
-long f3 (long x) { return (x | -x) >> (__SIZEOF_LONG__ * __CHAR_BIT__ - 1); }
-
 /* X + (X | -X) clears the lowest set bit of X.  The identity holds for a
    signed operand too, X - 1 overflows only where the source does.  */
 unsigned int f4 (unsigned int x) { return x + (x | -x); }
@@ -22,5 +14,4 @@ unsigned int f6 (unsigned int x, unsigned int *p)
 }
 
 /* { dg-final { scan-tree-dump-times " \\| " 1 "optimized" } } */
-/* { dg-final { scan-tree-dump-times " != 0" 3 "optimized" } } */
 /* { dg-final { scan-tree-dump-times " & " 2 "optimized" } } */
