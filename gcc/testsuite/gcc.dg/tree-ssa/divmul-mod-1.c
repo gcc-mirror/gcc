23797f88d1635d38ce68c8d180a0f90a1ea34740
Kyrylo Tkachov <ktkachov@nvidia.com>
2026-08-10 20:41:36 +0200

    match.pd: fold the inequality spelling of divisibility
    GCC folds:
    
      x / y * y == x
    
    to:
    
      x % y == 0
    
    Apply the same simplification to inequality.
    
      int divisible_by_3 (unsigned int x)
      {
        return x / 3 * 3 != x;
      }
    
    aarch64 -O2:
    
    before:
    
    	divisible_by_3:
    		mov	w1, 43691
    		movk	w1, 0xaaaa, lsl 16
    		umull	x1, w0, w1
    		lsr	x1, x1, 33
    		add	w1, w1, w1, lsl 1
    		cmp	w1, w0
    		cset	w0, ne
    		ret
    
    after:
    
    	divisible_by_3:
    		mov	w1, 43691
    		movk	w1, 0xaaaa, lsl 16
    		mul	w0, w0, w1
    		mov	w1, 1431655765
    		cmp	w0, w1
    		cset	w0, hi
    		ret
    
    Bootstrapped and tested on aarch64-none-linux-gnu.
    
    gcc/ChangeLog:
    
    	* match.pd (x / y * y == x): Extend to inequality.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/tree-ssa/divmul-mod-1.c: New test.
    
    Signed-off-by: Kyrylo Tkachov <ktkachov@nvidia.com>

---
+++ gcc/testsuite/gcc.dg/tree-ssa/divmul-mod-1.c
@@ -0,0 +1,38 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+typedef int v4si __attribute__ ((vector_size (16)));
+typedef unsigned int v4ui __attribute__ ((vector_size (16)));
+
+int
+eq (unsigned int a, unsigned int b)
+{
+  return a / b * b == a;
+}
+
+int
+ne (unsigned int a, unsigned int b)
+{
+  return a / b * b != a;
+}
+
+int
+nes (int a, int b)
+{
+  return a / b * b != a;
+}
+
+v4si
+veq (v4ui a, v4ui b)
+{
+  return a / b * b == a;
+}
+
+v4si
+vne (v4ui a, v4ui b)
+{
+  return a / b * b != a;
+}
+
+/* { dg-final { scan-tree-dump-not " \\* " "optimized" } } */
+/* { dg-final { scan-tree-dump " % " "optimized" } } */
