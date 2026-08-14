0e9f2f58879de6c14221f423bad224e4ec96238f
Kito Cheng <kito.cheng@sifive.com>
2026-08-14 15:56:09 +0800

    RISC-V: Move the hwprobe interface description into a .def file
    Extract the hwprobe key numbers and extension bits into
    common/config/riscv/riscv-hwprobe.def, with the syscall wrapper in
    riscv-hwprobe.h, so that the -march=native implementation can share the
    description libgcc already needs.
    
    gcc/ChangeLog:
    
    	* common/config/riscv/riscv-hwprobe.def: New file.
    	* common/config/riscv/riscv-hwprobe.h: New file.
    
    libgcc/ChangeLog:
    
    	* config/riscv/feature_bits.c: Include
    	common/config/riscv/riscv-hwprobe.h, and drop the hwprobe key
    	numbers, extension bit positions, struct riscv_hwprobe and
    	syscall_5_args that moved there.
    	(__init_riscv_features_bits_linux): Set the extension bits from
    	riscv-hwprobe.def.
    	(__init_riscv_feature_bits): Add a declaration.

---
+++ gcc/common/config/riscv/riscv-hwprobe.h
@@ -0,0 +1,85 @@
+/* Definitions for the Linux riscv_hwprobe interface.
+
+   Copyright (C) 2026 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+Under Section 7 of GPL version 3, you are granted additional
+permissions described in the GCC Runtime Library Exception, version
+3.1, as published by the Free Software Foundation.
+
+You should have received a copy of the GNU General Public License and
+a copy of the GCC Runtime Library Exception along with this program;
+see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef GCC_RISCV_HWPROBE_H
+#define GCC_RISCV_HWPROBE_H
+
+#ifndef __NR_riscv_hwprobe
+#define __NR_riscv_hwprobe 258
+#endif
+
+enum {
+#define RISCV_HWPROBE_KEY(NAME, VALUE) RISCV_HWPROBE_KEY_##NAME = VALUE,
+#include "common/config/riscv/riscv-hwprobe.def"
+#define RISCV_HWPROBE_EXT(NAME, UPPERCASE_NAME, KEY, BIT, XLEN) \
+  RISCV_HWPROBE_EXT_##UPPERCASE_NAME = (1ULL << BIT),
+#include "common/config/riscv/riscv-hwprobe.def"
+};
+
+#define RISCV_HWPROBE_BASE_BEHAVIOR_IMA (1ULL << 0)
+
+struct riscv_hwprobe {
+  long long key;
+  unsigned long long value;
+};
+
+static inline long
+syscall_5_args (long number, long arg1, long arg2, long arg3,
+		long arg4, long arg5)
+{
+  register long a7 __asm__ ("a7") = number;
+  register long a0 __asm__ ("a0") = arg1;
+  register long a1 __asm__ ("a1") = arg2;
+  register long a2 __asm__ ("a2") = arg3;
+  register long a3 __asm__ ("a3") = arg4;
+  register long a4 __asm__ ("a4") = arg5;
+  __asm__ __volatile__ ("ecall\n\t"
+			: "=r"(a0)
+			: "r"(a7), "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4)
+			: "memory");
+  return a0;
+}
+
+static inline long
+riscv_hwprobe (struct riscv_hwprobe *hwprobes, long npairs)
+{
+  return syscall_5_args (__NR_riscv_hwprobe, (long)hwprobes, npairs, 0,
+			 0, 0);
+}
+
+static inline unsigned long long
+riscv_hwprobe_value (const struct riscv_hwprobe *pairs, int npairs,
+		     long long key)
+{
+  int i;
+
+  for (i = 0; i < npairs; i++)
+    if (pairs[i].key == key)
+      return pairs[i].value;
+
+  return 0;
+}
+
+#endif
--- libgcc/config/riscv/feature_bits.c
+++ libgcc/config/riscv/feature_bits.c
@@ -190,119 +190,7 @@ struct {
 #define SET_EXT(EXT) features[EXT##_GROUPID] |= EXT##_BITMASK
 
 #ifdef __linux
-
-#define __NR_riscv_hwprobe 258
-#define RISCV_HWPROBE_KEY_MVENDORID 0
-#define RISCV_HWPROBE_KEY_MARCHID 1
-#define RISCV_HWPROBE_KEY_MIMPID 2
-#define RISCV_HWPROBE_KEY_BASE_BEHAVIOR 3
-#define RISCV_HWPROBE_BASE_BEHAVIOR_IMA (1ULL << 0)
-#define RISCV_HWPROBE_KEY_IMA_EXT_0 4
-#define RISCV_HWPROBE_IMA_FD (1ULL << 0)
-#define RISCV_HWPROBE_IMA_C (1ULL << 1)
-#define RISCV_HWPROBE_IMA_V (1ULL << 2)
-#define RISCV_HWPROBE_EXT_ZBA (1ULL << 3)
-#define RISCV_HWPROBE_EXT_ZBB (1ULL << 4)
-#define RISCV_HWPROBE_EXT_ZBS (1ULL << 5)
-#define RISCV_HWPROBE_EXT_ZICBOZ (1ULL << 6)
-#define RISCV_HWPROBE_EXT_ZBC (1ULL << 7)
-#define RISCV_HWPROBE_EXT_ZBKB (1ULL << 8)
-#define RISCV_HWPROBE_EXT_ZBKC (1ULL << 9)
-#define RISCV_HWPROBE_EXT_ZBKX (1ULL << 10)
-#define RISCV_HWPROBE_EXT_ZKND (1ULL << 11)
-#define RISCV_HWPROBE_EXT_ZKNE (1ULL << 12)
-#define RISCV_HWPROBE_EXT_ZKNH (1ULL << 13)
-#define RISCV_HWPROBE_EXT_ZKSED (1ULL << 14)
-#define RISCV_HWPROBE_EXT_ZKSH (1ULL << 15)
-#define RISCV_HWPROBE_EXT_ZKT (1ULL << 16)
-#define RISCV_HWPROBE_EXT_ZVBB (1ULL << 17)
-#define RISCV_HWPROBE_EXT_ZVBC (1ULL << 18)
-#define RISCV_HWPROBE_EXT_ZVKB (1ULL << 19)
-#define RISCV_HWPROBE_EXT_ZVKG (1ULL << 20)
-#define RISCV_HWPROBE_EXT_ZVKNED (1ULL << 21)
-#define RISCV_HWPROBE_EXT_ZVKNHA (1ULL << 22)
-#define RISCV_HWPROBE_EXT_ZVKNHB (1ULL << 23)
-#define RISCV_HWPROBE_EXT_ZVKSED (1ULL << 24)
-#define RISCV_HWPROBE_EXT_ZVKSH (1ULL << 25)
-#define RISCV_HWPROBE_EXT_ZVKT (1ULL << 26)
-#define RISCV_HWPROBE_EXT_ZFH (1ULL << 27)
-#define RISCV_HWPROBE_EXT_ZFHMIN (1ULL << 28)
-#define RISCV_HWPROBE_EXT_ZIHINTNTL (1ULL << 29)
-#define RISCV_HWPROBE_EXT_ZVFH (1ULL << 30)
-#define RISCV_HWPROBE_EXT_ZVFHMIN (1ULL << 31)
-#define RISCV_HWPROBE_EXT_ZFA (1ULL << 32)
-#define RISCV_HWPROBE_EXT_ZTSO (1ULL << 33)
-#define RISCV_HWPROBE_EXT_ZACAS (1ULL << 34)
-#define RISCV_HWPROBE_EXT_ZICOND (1ULL << 35)
-#define RISCV_HWPROBE_EXT_ZIHINTPAUSE (1ULL << 36)
-#define RISCV_HWPROBE_EXT_ZVE32X (1ULL << 37)
-#define RISCV_HWPROBE_EXT_ZVE32F (1ULL << 38)
-#define RISCV_HWPROBE_EXT_ZVE64X (1ULL << 39)
-#define RISCV_HWPROBE_EXT_ZVE64F (1ULL << 40)
-#define RISCV_HWPROBE_EXT_ZVE64D (1ULL << 41)
-#define RISCV_HWPROBE_EXT_ZIMOP  (1ULL << 42)
-#define RISCV_HWPROBE_EXT_ZCA    (1ULL << 43)
-#define RISCV_HWPROBE_EXT_ZCB    (1ULL << 44)
-#define RISCV_HWPROBE_EXT_ZCD    (1ULL << 45)
-#define RISCV_HWPROBE_EXT_ZCF    (1ULL << 46)
-#define RISCV_HWPROBE_EXT_ZCMOP  (1ULL << 47)
-#define RISCV_HWPROBE_EXT_ZAWRS  (1ULL << 48)
-#define RISCV_HWPROBE_EXT_SUPM   (1ULL << 49)
-#define RISCV_HWPROBE_EXT_ZICNTR (1ULL << 50)
-#define RISCV_HWPROBE_EXT_ZIHPM  (1ULL << 51)
-#define RISCV_HWPROBE_EXT_ZFBFMIN (1ULL << 52)
-#define RISCV_HWPROBE_EXT_ZVFBFMIN (1ULL << 53)
-#define RISCV_HWPROBE_EXT_ZVFBFWMA (1ULL << 54)
-#define RISCV_HWPROBE_EXT_ZICBOM (1ULL << 55)
-#define RISCV_HWPROBE_EXT_ZAAMO  (1ULL << 56)
-#define RISCV_HWPROBE_EXT_ZALRSC (1ULL << 57)
-#define RISCV_HWPROBE_EXT_ZABHA  (1ULL << 58)
-#define RISCV_HWPROBE_EXT_ZALASR (1ULL << 59)
-#define RISCV_HWPROBE_EXT_ZICBOP (1ULL << 60)
-#define RISCV_HWPROBE_EXT_ZILSD  (1ULL << 61)
-#define RISCV_HWPROBE_EXT_ZCLSD  (1ULL << 62)
-#define RISCV_HWPROBE_EXT_ZICFILP (1ULL << 63)
-#define RISCV_HWPROBE_KEY_CPUPERF_0 5
-#define RISCV_HWPROBE_MISALIGNED_UNKNOWN (0 << 0)
-#define RISCV_HWPROBE_MISALIGNED_EMULATED (1ULL << 0)
-#define RISCV_HWPROBE_MISALIGNED_SLOW (2 << 0)
-#define RISCV_HWPROBE_MISALIGNED_FAST (3 << 0)
-#define RISCV_HWPROBE_MISALIGNED_UNSUPPORTED (4 << 0)
-#define RISCV_HWPROBE_MISALIGNED_MASK (7 << 0)
-#define RISCV_HWPROBE_KEY_ZICBOZ_BLOCK_SIZE 6
-#define RISCV_HWPROBE_KEY_IMA_EXT_1 16
-#define RISCV_HWPROBE_EXT_ZICFISS (1ULL << 0)
-
-struct riscv_hwprobe {
-  long long key;
-  unsigned long long value;
-};
-
-static long syscall_5_args (long number, long arg1, long arg2, long arg3,
-			    long arg4, long arg5)
-{
-  register long a7 __asm__ ("a7") = number;
-  register long a0 __asm__ ("a0") = arg1;
-  register long a1 __asm__ ("a1") = arg2;
-  register long a2 __asm__ ("a2") = arg3;
-  register long a3 __asm__ ("a3") = arg4;
-  register long a4 __asm__ ("a4") = arg5;
-  __asm__ __volatile__ ("ecall\n\t"
-			: "=r"(a0)
-			: "r"(a7), "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4)
-			: "memory");
-  return a0;
-}
-
-#define SET_FROM_HWPROBE(HWPROBE_VAR, EXT) \
-  if (HWPROBE_VAR.value & RISCV_HWPROBE_EXT_##EXT) \
-    SET_EXT (EXT)
-
-#define SET_FROM_IMA_EXT(EXT) \
-  SET_FROM_HWPROBE (hwprobe_ima_ext, EXT)
-
-#define SET_FROM_IMA_EXT_1(EXT) \
-  SET_FROM_HWPROBE (hwprobe_ima_ext_1, EXT)
+#include "common/config/riscv/riscv-hwprobe.h"
 
 static void __init_riscv_features_bits_linux ()
 {
@@ -314,10 +202,9 @@ static void __init_riscv_features_bits_linux ()
     {RISCV_HWPROBE_KEY_IMA_EXT_0, 0},
     {RISCV_HWPROBE_KEY_IMA_EXT_1, 0},
   };
+  const int npairs = sizeof (hwprobes) / sizeof (hwprobes[0]);
 
-  long res = syscall_5_args (__NR_riscv_hwprobe, (long)&hwprobes,
-			     sizeof (hwprobes) / sizeof (hwprobes[0]), 0,
-			     0, 0);
+  long res = riscv_hwprobe (hwprobes, npairs);
 
   if (res)
     return;
@@ -354,18 +241,17 @@ static void __init_riscv_features_bits_linux ()
      1.  The new extension implies a previous extension (e.g., Zve32f -> F).
      2.  The extensions imply some other extensions appear in the same release
 	 version of Linux Kernel (e.g., Zbc - > Zbkc).  */
-
-  if (hwprobe_ima_ext.value & RISCV_HWPROBE_IMA_FD)
+  if (hwprobe_ima_ext.value & (RISCV_HWPROBE_EXT_F | RISCV_HWPROBE_EXT_D))
     {
       SET_EXT (F);
       SET_EXT (D);
     }
 
-  if (hwprobe_ima_ext.value & RISCV_HWPROBE_IMA_C)
+  if (hwprobe_ima_ext.value & RISCV_HWPROBE_EXT_C)
     {
       SET_EXT (C);
       SET_EXT (ZCA);
-      if (hwprobe_ima_ext.value & RISCV_HWPROBE_IMA_FD)
+      if (hwprobe_ima_ext.value & (RISCV_HWPROBE_EXT_F | RISCV_HWPROBE_EXT_D))
 	{
 #if __riscv_xlen == 32
 	  SET_EXT (ZCF);
@@ -375,7 +261,7 @@ static void __init_riscv_features_bits_linux ()
     }
 
   /* Added since Linux v6.5.  */
-  if (hwprobe_ima_ext.value & RISCV_HWPROBE_IMA_V)
+  if (hwprobe_ima_ext.value & RISCV_HWPROBE_EXT_V)
     {
       SET_EXT (V);
       SET_EXT (ZVE32X);
@@ -385,74 +271,10 @@ static void __init_riscv_features_bits_linux ()
       SET_EXT (ZVE64D);
     }
 
-  SET_FROM_IMA_EXT (ZBA);
-  SET_FROM_IMA_EXT (ZBB);
-  SET_FROM_IMA_EXT (ZBS);
-  /* Added since Linux v6.7.  */
-  SET_FROM_IMA_EXT (ZICBOZ);
-  /* Added since Linux v6.8.  */
-  SET_FROM_IMA_EXT (ZBC);
-  SET_FROM_IMA_EXT (ZBKB);
-  SET_FROM_IMA_EXT (ZBKC);
-  SET_FROM_IMA_EXT (ZBKX);
-  SET_FROM_IMA_EXT (ZKND);
-  SET_FROM_IMA_EXT (ZKNE);
-  SET_FROM_IMA_EXT (ZKNH);
-  SET_FROM_IMA_EXT (ZKSED);
-  SET_FROM_IMA_EXT (ZKSH);
-  SET_FROM_IMA_EXT (ZKT);
-  SET_FROM_IMA_EXT (ZVBB);
-  SET_FROM_IMA_EXT (ZVBC);
-  SET_FROM_IMA_EXT (ZVKB);
-  SET_FROM_IMA_EXT (ZVKG);
-  SET_FROM_IMA_EXT (ZVKNED);
-  SET_FROM_IMA_EXT (ZVKNHA);
-  SET_FROM_IMA_EXT (ZVKNHB);
-  SET_FROM_IMA_EXT (ZVKSED);
-  SET_FROM_IMA_EXT (ZVKSH);
-  SET_FROM_IMA_EXT (ZVKT);
-  SET_FROM_IMA_EXT (ZFH);
-  SET_FROM_IMA_EXT (ZFHMIN);
-  SET_FROM_IMA_EXT (ZIHINTNTL);
-  SET_FROM_IMA_EXT (ZVFH);
-  SET_FROM_IMA_EXT (ZVFHMIN);
-  SET_FROM_IMA_EXT (ZFA);
-  SET_FROM_IMA_EXT (ZTSO);
-  SET_FROM_IMA_EXT (ZACAS);
-  SET_FROM_IMA_EXT (ZICOND);
-  /* Added since Linux v6.10.  */
-  SET_FROM_IMA_EXT (ZIHINTPAUSE);
-  /* Added since Linux v6.11.  */
-  SET_FROM_IMA_EXT (ZVE32X);
-  SET_FROM_IMA_EXT (ZVE32F);
-  SET_FROM_IMA_EXT (ZVE64X);
-  SET_FROM_IMA_EXT (ZVE64F);
-  SET_FROM_IMA_EXT (ZVE64D);
-  SET_FROM_IMA_EXT (ZIMOP);
-  SET_FROM_IMA_EXT (ZCA);
-  SET_FROM_IMA_EXT (ZCB);
-  SET_FROM_IMA_EXT (ZCD);
-  SET_FROM_IMA_EXT (ZCF);
-  SET_FROM_IMA_EXT (ZCMOP);
-  SET_FROM_IMA_EXT (ZAWRS);
-  SET_FROM_IMA_EXT (SUPM);
-  SET_FROM_IMA_EXT (ZICNTR);
-  SET_FROM_IMA_EXT (ZIHPM);
-  SET_FROM_IMA_EXT (ZFBFMIN);
-  SET_FROM_IMA_EXT (ZVFBFMIN);
-  SET_FROM_IMA_EXT (ZVFBFWMA);
-  SET_FROM_IMA_EXT (ZICBOM);
-  SET_FROM_IMA_EXT (ZAAMO);
-  SET_FROM_IMA_EXT (ZALRSC);
-  SET_FROM_IMA_EXT (ZABHA);
-  SET_FROM_IMA_EXT (ZALASR);
-  SET_FROM_IMA_EXT (ZICBOP);
-  SET_FROM_IMA_EXT (ZILSD);
-  SET_FROM_IMA_EXT (ZCLSD);
-  SET_FROM_IMA_EXT (ZICFILP);
-
-  const struct riscv_hwprobe hwprobe_ima_ext_1 = hwprobes[5];
-  SET_FROM_IMA_EXT_1 (ZICFISS);
+#define RISCV_HWPROBE_EXT(NAME, UPPERCASE_NAME, KEY, BIT, XLEN)		\
+  if (riscv_hwprobe_value (hwprobes, npairs, KEY) & (1ULL << (BIT)))	\
+    SET_EXT (UPPERCASE_NAME);
+#include "common/config/riscv/riscv-hwprobe.def"
 
   for (i = 0; i < RISCV_FEATURE_BITS_LENGTH; ++i)
     __riscv_feature_bits.features[i] = features[i];
@@ -463,6 +285,8 @@ static void __init_riscv_features_bits_linux ()
 
 
 static int __init = 0;
+
+void __init_riscv_feature_bits ();
 
 void
 __attribute__ ((constructor (101)))
