dcf34067a80904e66a91db127f39f27fdb1b6ee5
Iain Sandoe <iain@sandoe.co.uk>
2026-08-14 15:14:12 +0100

    testsuite, X86: Fix missing U_L_P on pr126293-1b.c.
    These new tests were failing on darwin because of the missing user
    label prefix.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/i386/pr126293-1b.c: Account for USER_LABEL_PREFIX.
    	* gcc.target/i386/pr126293-1c.c: Likwise.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/gcc.target/i386/pr126293-1c.c
+++ gcc/testsuite/gcc.target/i386/pr126293-1c.c
@@ -1,6 +1,6 @@
 /* { dg-do compile { target { ! ia32 } } } */
 /* { dg-additional-options "-O2 -march=x86-64 -m128bit-atomic -mno-cx16" } */
-/* { dg-final { scan-assembler-times "call\[ \\t\]+__atomic_fetch_" 4 } } */
-/* { dg-final { scan-assembler-times "jmp\[ \\t\]+__atomic_fetch_" 4 } } */
+/* { dg-final { scan-assembler-times "call\[ \\t\]+_?__atomic_fetch_" 4 } } */
+/* { dg-final { scan-assembler-times "jmp\[ \\t\]+_?__atomic_fetch_" 4 } } */
 
 #include "pr126293-1a.c"
