ec5e102d66f182c9028fb88a7354a32a73ed0347
Matt Turner <mattst88@gmail.com>
2026-08-18 08:40:49 -0600

    [PATCH 6/7] testsuite: skip cmp-mem-const on alpha
    Alpha does not narrow memory comparisons in combine, so the
    "narrow comparison from mode .I to HI" scan never matches.  This is not
    specific to byte/word memory access: -mbwx does not change it either.
    Skip these tests the same way sparc and hppa already do.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/cmp-mem-const-3.c: Skip on alpha.
    	* gcc.dg/cmp-mem-const-4.c: Likewise.
    	* gcc.dg/cmp-mem-const-5.c: Likewise.
    	* gcc.dg/cmp-mem-const-6.c: Likewise.

--- gcc/testsuite/gcc.dg/cmp-mem-const-3.c
+++ gcc/testsuite/gcc.dg/cmp-mem-const-3.c
@@ -1,6 +1,7 @@
-/* { dg-do compile { target { lp64 && { ! { sparc*-*-* hppa*-*-* } } } } } */
+/* { dg-do compile { target { lp64 && { ! { sparc*-*-* hppa*-*-* alpha*-*-* } } } } } */
 /* Excluding sparc since there we do not end up with a comparison of memory and
    a constant which means that the optimization is not applicable.  */
+/* Excluding alpha since memory comparisons are not narrowed there.  */
 /* { dg-options "-O2 -fdump-rtl-combine-details" } */
 /* { dg-final { scan-rtl-dump "narrow comparison from mode .I to HI" "combine" } } */
 
--- gcc/testsuite/gcc.dg/cmp-mem-const-6.c
+++ gcc/testsuite/gcc.dg/cmp-mem-const-6.c
@@ -1,6 +1,7 @@
-/* { dg-do compile { target { lp64 && { ! { sparc*-*-* hppa*-*-* } } } } } */
+/* { dg-do compile { target { lp64 && { ! { sparc*-*-* hppa*-*-* alpha*-*-* } } } } } */
 /* Excluding sparc since there a prior optimization already reduced the
    constant, i.e., nothing left for us.  */
+/* Excluding alpha since memory comparisons are not narrowed there.  */
 /* { dg-options "-O2 -fdump-rtl-combine-details" } */
 /* { dg-final { scan-rtl-dump "narrow comparison from mode .I to SI" "combine" } } */
 
