929cafe124d0020c66dea03d069374c9e4c5aec2
Eric Botcazou <ebotcazou@adacore.com>
2026-08-19 15:19:41 +0200

    Ada: Fix assertion failure on nested formal packages with box notation
    The compiler is trying to instantiate a generic body for an instantiation
    present in a transient package that it has discarded.
    
    gcc/ada/
    	PR ada/126928
    	* sem_ch12.adb (In_Local_Package_Of_Formal_Package): New predicate.
    	(Analyze_Package_Instantiation): Do not register the instantiation
    	of the body if it is present in the local package of a formal one.
    	(Need_Subprogram_Instance_Body): Likewise.
    
    gcc/testsuite/
    	* gnat.dg/specs/generic_inst10.ads: New test.
    	* gnat.dg/specs/generic_inst10_g1.ads: New helper.
    	* gnat.dg/specs/generic_inst10_g2.ads: Likewise.
    	* gnat.dg/specs/generic_inst10_g3.ads: Likewise.
    	* gnat.dg/specs/generic_inst10_pkg.ads: Likewise.

---
+++ gcc/testsuite/gnat.dg/specs/generic_inst10.ads
@@ -0,0 +1,6 @@
+-- { dg-do compile }
+
+with Generic_Inst10_G3;
+with Generic_Inst10_Pkg;
+
+package Generic_Inst10 is new Generic_Inst10_G3 (Generic_Inst10_Pkg);
---
+++ gcc/testsuite/gnat.dg/specs/generic_inst10_g1.ads
@@ -0,0 +1,3 @@
+generic
+package Generic_Inst10_G1 is
+end Generic_Inst10_G1;
