1a92288ed6cf63c5b9e030506aa31c633af2ac14
Pan Li <pan2.li@intel.com>
2026-08-20 08:49:16 +0800

    RISC-V: Add test cases for vwmul.vx reg overlap
    Add test cases for register group overlap, please
    note it is not overlap as much as possible.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i16-m1.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i16-m2.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i16-m4.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i16-mf2.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i16-mf4.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i32-m1.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i32-m2.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i32-m4.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i32-mf2.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i8-m1.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i8-m2.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i8-m4.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i8-mf2.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i8-mf4.c: New test.
    	* gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i8-mf8.c: New test.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

---
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/group_overlap/vwmul_vx-i8-m4.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gcv -mabi=lp64d" } */
+
+#include "group_overlap.h"
+
+DEF_GROUP_OVERLAP_BINARY_3(
+  __riscv_vsetvlmax_e8m4,
+  vint8m4_t,
+  vint16m8_t,
+  int8_t,
+  __riscv_vle8_v_i8m4,
+  __riscv_vwmul_vx_i16m8,
+  __riscv_vse16_v_i16m8,
+  vwmul_vx,
+  LOOP_DUAL_WIDEN_BINARY_VX_BODY_X4)
+
+/* { dg-final { scan-assembler-times {vwmul\.vx\s+v0,v4,} 1 } } */
+/* { dg-final { scan-assembler-times {vwmul\.vx\s+v8,v12,} 1 } } */
