f9c50f4ac496d96439ff8a5b7d10fe0e5b7aa1f5
H.J. Lu <hjl.tools@gmail.com>
2026-08-24 19:43:55 +0800

    vcvtneps2bf16 emulation: Flush input denormal to zero
    Update vcvtneps2bf16 emulation in truncsfbf-5.c and truncsfbf-6.c to
    flush input denormal to zero.
    
    	PR target/126933
    	* gcc.target/i386/truncsfbf-5.c (CALC): Flush input denormal to
    	zero.
    	(main): Add tests for float denormal inputs.
    	* gcc.target/i386/truncsfbf-6.c (CALC): Flush input denormal to
    	zero.
    	(main): Add tests for float denormal inputs.
    
    Signed-off-by: H.J. Lu <hjl.tools@gmail.com>

--- gcc/testsuite/gcc.target/i386/truncsfbf-6.c
+++ gcc/testsuite/gcc.target/i386/truncsfbf-6.c
@@ -20,14 +20,20 @@ CALC (float *a)
 {
   uint32_t bits;
   memcpy (&bits, a, sizeof (bits));
-  uint32_t rounding_bias = 0x7FFF + ((bits >> 16) & 1);
-  bits += rounding_bias;
-  bits >>= 16;
-  /* Flush denormal to zero like vcvtneps2bf16.  */
-  if ((bits & 0x7f80) == 0)
-    bits &= 0x8000;
-  uint16_t bfloat16_bits = (uint16_t) bits;
+  /* Flush input denormal to zero like vcvtneps2bf16.  */
+  if ((bits & 0x7f800000) == 0)
+    {
+      bits >>= 16;
+      bits &= 0x8000;
+    }
+  else
+    {
+      uint32_t rounding_bias = 0x7FFF + ((bits >> 16) & 1);
+      bits += rounding_bias;
+      bits >>= 16;
+    }
   __bf16 bf16;
+  uint16_t bfloat16_bits = (uint16_t) bits;
   memcpy (&bf16, &bfloat16_bits, sizeof (bf16));
   return bf16;
 }
@@ -36,9 +42,13 @@ __attribute__ ((noipa, noinline))
 static void
 do_test (void)
 {
-  float test_values[] = { 0.0f, -0.0f, 1.0f, -1.0f, 0.5f, -0.5f, 1000.0f, -1000.0f,
-                          3.1415926f, -3.1415926f, 1e-8f, -1e-8f,
-                          1.0e+38f, -1.0e+38f, 1.0e-38f, -1.0e-38f };
+  float test_values[] =
+    {
+      0.0f, -0.0f, 1.0f, -1.0f, 0.5f, -0.5f, 1000.0f, -1000.0f,
+      3.1415926f, -3.1415926f, 1e-8f, -1e-8f, 1.0e+38f, -1.0e+38f,
+      1.0e-38f, -1.0e-38f, 1.170903e-38, -1.170903e-38, 1.175494e-38,
+      -1.175494e-38, 1.170902e-38, 4.999295e-39, 1.175494e-38
+    };
   size_t num_values = sizeof (test_values) / sizeof (test_values[0]);
   bool failed = false;
 
