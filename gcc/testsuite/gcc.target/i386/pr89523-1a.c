e623ab6a336f40191e0137471d3cc775d5d64a33
Richard Biener <rguenther@suse.de>
2026-08-26 10:26:40 +0200

    Adjust gcc.target/i386/pr89523-1{a,b}.c
    The following removes the scan for a addr32 vgather, but keeps the
    negative scan for vgather.  There is no reason for using a gather
    in the first place and after hoisting the invariant load of the
    ncost pointer load from the unswitched loop body we don't use one.
    
    	PR testsuite/127048
    	* gcc.target/i386/pr89523-1a.c: Remove scan for addr32 vgather.
    	* gcc.target/i386/pr89523-1b.c: Likewise.

--- gcc/testsuite/gcc.target/i386/pr89523-1a.c
+++ gcc/testsuite/gcc.target/i386/pr89523-1a.c
@@ -2,7 +2,6 @@
 /* { dg-require-effective-target maybe_x32 } */
 /* { dg-options "-maddress-mode=short -mx32 -Ofast -funroll-loops -march=haswell" } */
 /* { dg-final { scan-assembler-not "\tvgather" } } */
-/* { dg-final { scan-assembler "addr32 vgather" } } */
 
 void foo (void);
 
--- gcc/testsuite/gcc.target/i386/pr89523-1b.c
+++ gcc/testsuite/gcc.target/i386/pr89523-1b.c
@@ -2,6 +2,5 @@
 /* { dg-require-effective-target maybe_x32 } */
 /* { dg-options "-maddress-mode=long -mx32 -Ofast -funroll-loops -march=haswell" } */
 /* { dg-final { scan-assembler-not "\tvgather" } } */
-/* { dg-final { scan-assembler "addr32 vgather" } } */
 
 #include "pr89523-1a.c"
