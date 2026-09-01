c500635955ce2cba307cc174e3081367911efddc
Georg-Johann Lay <avr@gjlay.de>
2026-09-01 18:52:04 +0200

    AVR: Skip gcc.dg/pr28796-2.c
    avr double is not fully ieee, so skip this failing test.
    
    gcc/testsuite/
    	* gcc.dg/pr28796-2.c: Skip on avr.

--- gcc/testsuite/gcc.dg/pr28796-2.c
+++ gcc/testsuite/gcc.dg/pr28796-2.c
@@ -1,6 +1,7 @@
 /* { dg-do run } */
 /* { dg-options "-O2 -funsafe-math-optimizations -fno-finite-math-only -DUNSAFE" } */
 /* { dg-add-options ieee } */
+/* { dg-skip-if "not fully ieee" { avr-*-* } } */
 
 #include "tg-tests.h"
 
