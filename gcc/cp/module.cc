dad0fca70f5a35b1016189eb544ea729b0144417
Patrick Palka <ppalka@redhat.com>
2026-09-04 23:00:29 -0400

    c++/modules: handle tinfo with identifier as template [PR127207]
    A couple of sanity checks need to be made robust to TEMPLATE_INFO that
    has IDENTIFIER_NODE as its TI_TEMPLATE, which is how we express the
    friend template specialization createFoo<T>() when there is no existing
    declaration of createFoo.
    
    	PR c++/127207
    
    gcc/cp/ChangeLog:
    
    	* module.cc (trees_out::decl_value): Relax checking assert
    	to accept IDENTIFIER_NODE as TI_TEMPLATE.
    	(get_originating_module_decl): Likewise.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/modules/friend-15_a.H: New test.
    	* g++.dg/modules/friend-15_b.C: New test.
    
    Reviewed-by: Jason Merrill <jason@redhat.com>

--- gcc/cp/module.cc
+++ gcc/cp/module.cc
@@ -8386,6 +8386,7 @@ trees_out::decl_value (tree decl, depset *dep)
       int use_tpl = -1;
       if (tree ti = node_template_info (decl, use_tpl))
 	gcc_checking_assert (TREE_CODE (TI_TEMPLATE (ti)) == OVERLOAD
+			     || TREE_CODE (TI_TEMPLATE (ti)) == IDENTIFIER_NODE
 			     || TREE_CODE (TI_TEMPLATE (ti)) == FIELD_DECL
 			     || (DECL_TEMPLATE_RESULT (TI_TEMPLATE (ti))
 				 != decl));
@@ -21918,7 +21919,7 @@ get_originating_module_decl (tree decl)
 	  if (TREE_CODE (decl) != TEMPLATE_DECL)
 	    {
 	      /* A friend template specialization.  */
-	      gcc_checking_assert (OVL_P (decl));
+	      gcc_checking_assert (OVL_P (decl) || identifier_p (decl));
 	      return global_namespace;
 	    }
 	}
