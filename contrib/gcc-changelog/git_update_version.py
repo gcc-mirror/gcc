21764d36e7ffcc529c0b58c21cd3a19eb8e8497f
Richard Biener <rguenther@suse.de>
2026-09-11 09:27:54 +0200

    Remove gcc-13 branch from active branches
    Close the gcc-13 branch.
    
    contrib/
    	* gcc-changelog/git_update_version.py: Remove gcc-13 branch
    	from active branches.

--- contrib/gcc-changelog/git_update_version.py
+++ contrib/gcc-changelog/git_update_version.py
@@ -71,7 +71,7 @@ def prepend_to_changelog_files(repo, folder, git_commit, add_to_git):
             repo.git.add(full_path)
 
 
-active_refs = ['master', 'releases/gcc-13',
+active_refs = ['master',
                'releases/gcc-14', 'releases/gcc-15', 'releases/gcc-16']
 
 parser = argparse.ArgumentParser(description='Update DATESTAMP and generate '
