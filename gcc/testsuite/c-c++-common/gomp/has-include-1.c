5b3356d8fafc56d380693b7c55e06f8f84830e5a
Andrea Pinski <andrew.pinski@oss.qualcomm.com>
2026-09-21 19:56:09 -0700

    testsuite: Fix has-include-1.c
    After r17-4537-g059231fb8a42ea, two error messages instead of
    just one is emitted. So let's look for both of them.
    
    Pushed as obvious after testing to make sure the testcase passes
    now for both C and C++.
    
    gcc/testsuite/ChangeLog:
    
    	* c-c++-common/gomp/has-include-1.c: Add second dg-error.
    
    Signed-off-by: Andrea Pinski <andrew.pinski@oss.qualcomm.com>

--- gcc/testsuite/c-c++-common/gomp/has-include-1.c
+++ gcc/testsuite/c-c++-common/gomp/has-include-1.c
@@ -4,5 +4,6 @@ void
 foo (void)
 {
 #pragma omp parallel if (__has_include ("<stdlib.h>"))	/* { dg-error "used outside of preprocessing directive" } */
+/* { dg-error "a function" "" { target *-*-* } .-1 } */
   ;
 }
