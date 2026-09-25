e8add4e4e24a9cb4ee05fb0889b7eaaed3a218ee
Georg-Johann Lay <avr@gjlay.de>
2026-09-25 10:52:52 +0200

    AVR: Add new option -moptimize for avr-specific passes.
    This is a collection option that can be used to disable / enable
    the avr-specific optimization passes -- currently 8 of them.
    
    gcc/
    	* config/avr/avr.opt (-moptimize): New target option.
    	* config/avr/avr.opt.urls: Add resprective URL.
    	* common/config/avr/avr-common.cc (avr_enable_optimizations): New
    	static function.
    	(avr_handle_option) [OPT_moptimize]: Call it.
    	* doc/invoke.texi (AVR Options) <-moptimize>: Document it.

--- gcc/common/config/avr/avr-common.cc
+++ gcc/common/config/avr/avr-common.cc
@@ -80,6 +80,22 @@ static const struct default_options avr_option_optimization_table[] =
   };
 
 
+/* Let -m[no-]optimize [de-]activate avr-specific optimization (passes).  */
+
+static void
+avr_enable_optimizations (gcc_options *opts, bool on)
+{
+  opts->x_avropt_demote_switch = on;
+  opts->x_avropt_fuse_add = on ? 2 : 0;
+  opts->x_avropt_fuse_ifelse = on;
+  opts->x_avropt_fuse_move = on ? 23 : 0;
+  opts->x_avropt_fuse_move2 = on;
+  opts->x_avropt_split_ldst = on;
+  opts->x_avropt_split_bit_shift = on;
+  opts->x_avropt_use_nonzero_bits = on;
+}
+
+
 /* Implement `TARGET_HANDLE_OPTION'.  */
 
 /* This is the same logic that driver-avr.cc:avr_double_lib() applies
@@ -93,6 +109,9 @@ avr_handle_option (struct gcc_options *opts, struct gcc_options*,
 		   location_t loc ATTRIBUTE_UNUSED)
 {
   int value = decoded->value;
+
+  if (decoded->opt_index == OPT_moptimize)
+    avr_enable_optimizations (opts, value);
 
   switch (decoded->opt_index)
     {
